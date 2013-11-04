79a91ca4f315553f738af367d82bb80a07e1ccb9
axelcasauran <axel.casauran@irely.com>
2013-11-04 16:09:56 +0800

    FIN-354

--- Manual Scripts/13.4 Create Scripts/3. GL/3. CreateSP.sql
+++ Manual Scripts/13.4 Create Scripts/3. GL/3. CreateSP.sql
@@ -756,6 +756,35 @@ DELETE FROM tblGLTempAccount WHERE intUserID = @intUserID
 
 EXEC usp_SyncAccounts @intUserID
 
+-- +++++ RECREATE tblGLTempCOASegment  +++++ --
+IF EXISTS (SELECT *  FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'tblGLTempCOASegment') DROP TABLE tblGLTempCOASegment 
+    DECLARE @Segments NVARCHAR(MAX)
+    SELECT @Segments = SUBSTRING((SELECT '],[' + strStructureName FROM tblGLAccountStructure WHERE strType <> 'Divider' FOR XML PATH('')),3,200000) + ']'
+    DECLARE @Query NVARCHAR(MAX)
+    SET @Query = 
+    'SELECT A.intAccountID, DAS.* INTO tblGLTempCOASegment FROM tblGLAccount A
+    INNER JOIN (
+     SELECT *  FROM (
+       SELECT DISTINCT
+       A.strAccountID 
+       ,C.strCode
+       ,D.strStructureName
+        from tblGLAccount A INNER JOIN tblGLAccountSegmentMapping B 
+          ON A.intAccountID = B.intAccountID
+       INNER JOIN tblGLAccountSegment C
+        ON B.intAccountSegmentID = C.intAccountSegmentID
+       INNER JOIN  tblGLAccountStructure D 
+        ON C.intAccountStructureID = D.intAccountStructureID
+      ) AS tempTable
+     PIVOT
+     (
+     MIN(strCode)
+     FOR strStructureName IN (' + @Segments + ')) AS PVT
+     ) AS DAS
+    ON A.strAccountID = DAS.strAccountID
+    '
+    EXEC sp_executesql @Query
+
 
 select 1
 
