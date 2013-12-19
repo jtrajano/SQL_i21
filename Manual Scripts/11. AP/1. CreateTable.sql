237a8033715f492353b62276c1e1c5ca3574ffeb
Darwin Estrada <darwin.estrada@irely.com>
2013-12-19 14:17:38 +0800

    remove the view from AP 1. CreateTable.sql to 2. CreateViews.sql

--- Manual Scripts/11. AP/1. CreateTable.sql
+++ Manual Scripts/11. AP/1. CreateTable.sql
@@ -301,28 +301,7 @@ ADD CONSTRAINT APVendorId_Unique UNIQUE NONCLUSTERED(strVendorId)
 --ALTER TABLE tblAPVendors
 --ADD CONSTRAINT APVendorId_Unique UNIQUE NONCLUSTERED(strVendorId)
 
---VIEWS
-IF EXISTS (SELECT * FROM sys.sysobjects WHERE ID = OBJECT_ID(N'vyu_VendorHistory') AND OBJECTPROPERTY(ID, N'IsView') = 1) 
-DROP VIEW vyu_VendorHistory
 
-GO
-
-CREATE VIEW vyu_VendorHistory
-AS
-SELECT 
-	strVendorId = A.strVendorId
-	,A.dtmDate
-	,intTransactionId = A.intBillId 
-	,strTransactionType = 'Bill'
-	,dblTotal = ISNULL(A.dblTotal,0)
-	,dblAmountPaid = ISNULL(SUM(B.dblPayment),0)
-	,CAST((CASE WHEN (A.dblTotal - SUM(B.dblPayment) = 0) THEN 1 ELSE 0 END) AS BIT) AS ysnPaid
-	,dblAmountDue = ISNULL(A.dblTotal,0) - ISNULL(SUM(B.dblPayment),0)
-FROM tblAPBills A
-		LEFT JOIN tblAPPaymentDetails B ON A.intBillId = B.intBillId
-GROUP BY A.intBillId, A.dtmDate, A.dblTotal, A.strVendorId
-
-GO
 
 --IF EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[ImportBills]') and OBJECTPROPERTY(id, N'IsProcedure') = 1)
 --DROP PROCEDURE [dbo].ImportBills
