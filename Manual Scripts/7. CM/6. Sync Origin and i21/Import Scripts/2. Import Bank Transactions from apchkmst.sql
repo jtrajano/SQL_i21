83e4893b5c4111dbce993b53a856e1293e5fcb75
Feb Montefrio <feb.montefrio@irely.com>
2014-01-02 13:11:51 +0800

    Moved and rename the import script files

--- Manual Scripts/7. CM/6. Sync Origin and i21/Import Scripts/2. Import Bank Transactions from apchkmst.sql
+++ Manual Scripts/7. CM/6. Sync Origin and i21/Import Scripts/2. Import Bank Transactions from apchkmst.sql
@@ -1,3 +1,20 @@
+
+/*
+ '====================================================================================================================================='
+   SCRIPT CREATED BY: Feb Montefrio	DATE CREATED: December 27, 2013
+  -------------------------------------------------------------------------------------------------------------------------------------						
+   Script Name	:	Cash Management - Bank Transaction Import from apchkmst
+   
+   Description	:	The purpose of this script is to the bank transactions from the origin system to i21. 
+					Transactions from origin are group as: 
+					
+					1. Origin Deposit - if apchk_chk_amt is negative amount. 
+					2. Origin Checks - if apchk_chk_amt is a positive amount, apchk_trx_ind is a 'C', and apchk_chk_no is a number. 
+					3. Origin EFT - if apchk_chk_amt is a positive amount and apchk_chk_no starts with a 'E'. 
+					4. Origin Wire - if apchk_chk_amt is a positive amount and apchk_chk_no starts with a 'W'. 
+					5. Origin Withdrawal - if apchk_chk_amt is a positive amount and it is not a check, EFT, nor a Wire transaction.
+*/
+
 -- Declare the transaction types (constant)
 DECLARE @BANK_DEPOSIT AS INT = 1
 		,@BANK_WITHDRAWAL AS INT = 2
@@ -9,30 +26,14 @@ DECLARE @BANK_DEPOSIT AS INT = 1
 		,@CREDIT_CARD_PAYMENTS AS INT = 8
 		,@BANK_TRANSFER_WD AS INT = 9
 		,@BANK_TRANSFER_DEP AS INT = 10
-		,@ORIGIN_DEPOSIT AS INT = 11 -- NEGATIVE AMOUNT
-		,@ORIGIN_WITHDRAWAL AS INT = 14 -- POSITIVE AMOUNT
+		,@ORIGIN_DEPOSIT AS INT = 11		-- NEGATIVE AMOUNT, INDICATOR: O, APCHK_CHK_NO PREFIX: NONE
+		,@ORIGIN_CHECKS AS INT = 12			-- POSITIVE AMOUNT, INDICATOR: C, APCHK_CHK_NO PREFIX: N/A, IT MUST BE A VALID NUMBER
+		,@ORIGIN_EFT AS INT = 13			-- POSITIVE AMOUNT, INDICATOR: N/A, APCHK_CHK_NO PREFIX: 'E'		
+		,@ORIGIN_WITHDRAWAL AS INT = 14		-- POSITIVE AMOUNT, INDICATOR: N/A, APCHK_CHK_NO PREFIX: NONE
+		,@ORIGIN_WIRE AS INT = 15			-- POSITIVE AMOUNT, INDICATOR: N/A, APCHK_CHK_NO PREFIX: 'W'
 		
 		-- Declare the local variables. 
-		,@intBankAccountID AS INT
-		
--- Get the proper transaction prefix to use: 
-DECLARE @ORIGIN_DEPOSIT_PREFIX AS NVARCHAR(10),
-		@ORIGIN_WITHDRAWAL_PREFIX AS NVARCHAR(10)
-
-IF @@ERROR <> 0 GOTO EXIT_INSERT
-
--- Get the prefix from the transaction type table
-SELECT	TOP 1 
-		@ORIGIN_DEPOSIT_PREFIX = strTransactionPrefix
-FROM	dbo.[tblCMBankTransactionType]
-WHERE	intBankTransactionTypeID = @ORIGIN_DEPOSIT
-IF @@ERROR <> 0 GOTO EXIT_INSERT
-		
-SELECT	TOP 1 
-		@ORIGIN_WITHDRAWAL_PREFIX = strTransactionPrefix
-FROM	dbo.[tblCMBankTransactionType]
-WHERE	intBankTransactionTypeID = @ORIGIN_WITHDRAWAL
-IF @@ERROR <> 0 GOTO EXIT_INSERT
+		,@intBankAccountID AS INT	
 
 -- Insert the record from the origin system to i21. 
 INSERT INTO tblCMBankTransaction (
@@ -52,7 +53,7 @@ INSERT INTO tblCMBankTransaction (
 		,dblAmount
 		,strAmountInWords
 		,strMemo
-		,intReferenceNo
+		,strReferenceNo
 		,ysnCheckPrinted
 		,ysnCheckToBePrinted
 		,ysnCheckVoid
@@ -67,24 +68,19 @@ INSERT INTO tblCMBankTransaction (
 		,intConcurrencyID	
 )
 SELECT 
-		strTransactionID			=	
-										CASE 
-											WHEN i.apchk_chk_amt > 0 THEN
-												@ORIGIN_WITHDRAWAL_PREFIX + '-' 
-												+ CAST(i.apchk_cbk_no AS NVARCHAR(2)) 
-												+ CAST(i.apchk_rev_dt AS NVARCHAR(10))
-												+ CAST(i.apchk_trx_ind AS NVARCHAR(1))
-												+ CAST(i.apchk_chk_no AS NVARCHAR(8)) 
-											WHEN i.apchk_chk_amt < 0 THEN 
-												@ORIGIN_DEPOSIT_PREFIX + '-' 
-												+ CAST(i.apchk_cbk_no AS NVARCHAR(2)) 
-												+ CAST(i.apchk_rev_dt AS NVARCHAR(10))
-												+ CAST(i.apchk_trx_ind AS NVARCHAR(1))
-												+ CAST(i.apchk_chk_no AS NVARCHAR(8))  
-										END
+		strTransactionID			=	CAST(i.apchk_cbk_no AS NVARCHAR(2)) + '-'
+										+ CAST(i.apchk_rev_dt AS NVARCHAR(10)) + '-'
+										+ CAST(i.apchk_trx_ind AS NVARCHAR(1)) + '-'
+										+ CAST(i.apchk_chk_no AS NVARCHAR(8))
 		,intBankTransactionTypeID	=	
-										CASE	
-											WHEN i.apchk_chk_amt > 0 THEN @ORIGIN_WITHDRAWAL
+										CASE
+											WHEN i.apchk_chk_amt > 0 THEN 
+												CASE 
+													WHEN LEFT(i.apchk_chk_no, 1) = 'E' THEN @ORIGIN_EFT
+													WHEN LEFT(i.apchk_chk_no, 1) = 'W' THEN @ORIGIN_WIRE
+													WHEN i.apchk_trx_ind = 'C' THEN @ORIGIN_CHECKS
+													ELSE @ORIGIN_WITHDRAWAL
+												END
 											WHEN i.apchk_chk_amt < 0 THEN @ORIGIN_DEPOSIT
 										END
 		,intBankAccountID			=	f.intBankAccountID
@@ -108,7 +104,7 @@ SELECT
 		,strMemo					=	RTRIM(LTRIM(ISNULL(i.apchk_comment_1, ''))) + CASE WHEN LEN(LTRIM(RTRIM(i.apchk_comment_2))) > 0 THEN CHAR(13) ELSE '' END +
 										RTRIM(LTRIM(ISNULL(i.apchk_comment_2, ''))) + CASE WHEN LEN(LTRIM(RTRIM(i.apchk_comment_3))) > 0 THEN CHAR(13) ELSE '' END +
 										RTRIM(LTRIM(ISNULL(i.apchk_comment_3, ''))) 
-		,intReferenceNo				=	dbo.fn_GetNumbersFromString(i.apchk_chk_no)
+		,strReferenceNo				=	RTRIM(LTRIM(i.apchk_chk_no))
 		,ysnCheckPrinted			=	1 
 		,ysnCheckToBePrinted		=	1
 		,ysnCheckVoid				=	CASE
