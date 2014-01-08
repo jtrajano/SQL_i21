7041d622195a8c223faebdd72b3bc5c56b15d190
Jayson E. Ramos <jayson.ramos@iRely.com>
2014-01-08 16:36:13 +0800

    Update Invoices Credits View.

--- Database Project/dbo/Views/vwCPInvoicesCredits.sql
+++ Database Project/dbo/Views/vwCPInvoicesCredits.sql
@@ -27,7 +27,7 @@ select
 	,agivc_disc_rev_dt = (case len(convert(varchar, a.agivc_disc_rev_dt)) when 8 then convert(date, cast(convert(varchar, a.agivc_disc_rev_dt) AS CHAR(12)), 112) else null end)
 	,agivc_net_rev_dt = (case len(convert(varchar, a.agivc_net_rev_dt)) when 8 then convert(date, cast(convert(varchar, a.agivc_net_rev_dt) AS CHAR(12)), 112) else null end)
 	,a.agivc_src_sys
-	,a.agivc_orig_rev_dt--agivc_orig_rev_dt = (case len(convert(varchar, a.agivc_orig_rev_dt)) when 8 then convert(date, cast(convert(varchar, a.agivc_orig_rev_dt) AS CHAR(12)), 112) else null end)
+	,agivc_orig_rev_dt = (case len(convert(varchar, a.agivc_orig_rev_dt)) when 8 then convert(date, cast(convert(varchar, a.agivc_orig_rev_dt) AS CHAR(12)), 112) else null end)
     ,a.agivc_split_no
 	,a.agivc_pd_days_old
 	,a.agivc_eft_ivc_paid_yn
