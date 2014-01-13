1a052b15bb984a0c0004138f6abf145930cb25c1
Feb Montefrio <feb.montefrio@irely.com>
2014-01-13 12:56:21 +0800

    Print Check 1. Add stored procedure to validate if a check number is a duplicate.
    *Add things in the ignore list.

---
+++ Database Project/dbo/Stored Procedures/GetDuplicateCheckNumber.sql
@@ -0,0 +1,44 @@
+﻿
+CREATE PROCEDURE GetDuplicateCheckNumber
+	@intBankAccountID INT = NULL,
+	@strTransactionID NVARCHAR(40) = NULL,
+	@strCheckNumber NVARCHAR(20) = NULL,
+	@isDuplicate BIT = 0 OUTPUT
+AS
+
+SET QUOTED_IDENTIFIER OFF
+SET ANSI_NULLS ON
+SET NOCOUNT ON
+SET XACT_ABORT ON
+SET ANSI_WARNINGS OFF
+
+DECLARE @BANK_DEPOSIT INT = 1
+		,@BANK_WITHDRAWAL INT = 2
+		,@MISC_CHECKS INT = 3
+		,@BANK_TRANSFER INT = 4
+		,@BANK_TRANSACTION INT = 5
+		,@CREDIT_CARD_CHARGE INT = 6
+		,@CREDIT_CARD_RETURNS INT = 7
+		,@CREDIT_CARD_PAYMENTS INT = 8
+		,@BANK_TRANSFER_WD INT = 9
+		,@BANK_TRANSFER_DEP INT = 10
+		,@ORIGIN_DEPOSIT AS INT = 11
+		,@ORIGIN_CHECKS AS INT = 12
+		,@ORIGIN_EFT AS INT = 13
+		,@ORIGIN_WITHDRAWAL AS INT = 14
+		,@ORIGIN_WIRE AS INT = 15
+		
+SELECT	TOP 1 
+		@isDuplicate = 1
+FROM	dbo.tblCMBankTransaction 
+WHERE	intBankAccountID = @intBankAccountID
+		AND strTransactionID <> @strTransactionID
+		AND 1 = 
+				CASE	WHEN ISNUMERIC(strReferenceNo) = 1 AND ISNUMERIC(@strCheckNumber) = 1THEN 
+							CASE WHEN CAST(strReferenceNo AS INT) = CAST(@strCheckNumber AS INT) THEN 1 ELSE 0 END 
+						ELSE 
+							CASE WHEN strReferenceNo = @strCheckNumber THEN 1 ELSE 0 END 							
+				END 
+		AND	intBankTransactionTypeID IN (@MISC_CHECKS, @ORIGIN_CHECKS)
+
+SET @isDuplicate = ISNULL(@isDuplicate, 0)
