9ed874ec4871bd0ff1ae7dacac9e40e2b48abcf9
Erick Dela Cruz <erick.delacruz@irely.com>
2014-01-14 09:41:55 +0800

    AP-8 Added recurring table.

---
+++ Database Project/dbo/Tables/tblAPRecurringTransaction.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[tblAPRecurringTransaction]
+(
+	[intRecurringId] INT IDENTITY(1, 1) NOT NULL PRIMARY KEY, 
+    [strTransactonId] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
+    [intTransactionId] INT NULL, 
+    [strFrequency] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
+    [dtmLastProcess] DATETIME NULL, 
+    [dtmNextProcess] DATETIME NULL, 
+    [dtmStartDate] DATETIME NULL, 
+    [dtmEndDate] DATETIME NULL, 
+    [ysnActive] BIT NULL, 
+    [ysnDue] BIT NULL, 
+    [intGroupId] INT NULL, 
+    [intDayofMonth] INT NULL
+)
+
+GO
+
+
