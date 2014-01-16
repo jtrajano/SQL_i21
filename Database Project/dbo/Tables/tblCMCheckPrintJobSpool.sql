fbd8379a7611f6147bed62c7b105b06c95e5293a
Feb Montefrio <feb.montefrio@irely.com>
2014-01-16 10:47:22 +0800

    Schema change for Cash Management 1. Add new table called tblCMCheckPrintJobSpool 2. Fix the address fields in CM. Make sure all are NVARCHAR(65).

---
+++ Database Project/dbo/Tables/tblCMCheckPrintJobSpool.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[tblCMCheckPrintJobSpool] (
+    [intBankAccountID]           INT            NOT NULL,
+    [strTransactionID]     NVARCHAR(40)            NOT NULL,
+    [strBatchID] NVARCHAR(20)            NOT NULL,
+    [strCheckNumber]           NVARCHAR (20) COLLATE Latin1_General_CI_AS NOT NULL,
+    [dtmPrintJobCreated]     DATETIME            NULL,
+    [dtmCheckPrinted] DATETIME NULL, 
+    [intCreatedUserID] INT NULL, 
+    CONSTRAINT [PK_tblCMCheckPrintJobSpool] PRIMARY KEY ([strTransactionID])
+);
+
+GO
+CREATE NONCLUSTERED INDEX [IX_FK_tblCMBankAccounttblCMCheckNumberAudit]
+    ON [dbo].[tblCMCheckPrintJobSpool]([strTransactionID] ASC);
+
