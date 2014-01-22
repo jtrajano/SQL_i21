e16ed4732c1567829553f2833cd145fb0486914b
Jayson E. Ramos <jayson.ramos@iRely.com>
2014-01-22 11:55:42 +0800

    Create identity for tblCPUserConfiguration.

--- Database Project/dbo/Tables/tblCPUserConfiguration.sql
+++ Database Project/dbo/Tables/tblCPUserConfiguration.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[tblCPUserConfiguration]
 (
-	[intUserConfigId] INT NOT NULL PRIMARY KEY, 
+	[intUserConfigId] INT NOT NULL PRIMARY KEY IDENTITY, 
     [intUserSecurityId] INT NOT NULL, 
     [strCustomerNo] NVARCHAR(10) NOT NULL, 
     CONSTRAINT [FK_tblCPUserConfiguration_tblSMUserSecurity] FOREIGN KEY ([intUserSecurityId]) REFERENCES [tblSMUserSecurity]([intUserSecurityID]) 
