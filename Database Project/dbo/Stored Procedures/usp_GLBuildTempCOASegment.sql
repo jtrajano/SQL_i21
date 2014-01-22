366f7dee541e16aaff01a52ec67ef3a3114bcde6
axelcasauran <axel.casauran@irely.com>
2014-01-22 14:49:19 +0800

    FIN-456

--- Database Project/dbo/Stored Procedures/usp_GLBuildTempCOASegment.sql
+++ Database Project/dbo/Stored Procedures/usp_GLBuildTempCOASegment.sql
@@ -33,4 +33,20 @@ BEGIN
 			'
 			
 			EXEC sp_executesql @Query
+			
+			
+
+	IF EXISTS (SELECT top 1 1  FROM INFORMATION_SCHEMA.VIEWS WHERE TABLE_NAME = 'vyu_GLDetailView') DROP VIEW vyu_GLDetailView
+	
+		EXEC ('CREATE VIEW [dbo].[vyu_GLDetailView]
+		AS
+		SELECT		A.intGLDetailID, A.dtmDate, A.strBatchID, A.dblDebit, A.dblCredit, A.dblDebitUnit, A.dblCreditUnit, A.strDescription AS GLDescription, A.strCode, 
+					A.strTransactionID, A.strReference, A.strNum, A.ysnIsUnposted, A.intUserID, A.strTransactionForm, A.strUOMCode,             
+					C.strAccountGroup, C.strAccountType, D.*            
+		FROM         dbo.tblGLDetail AS A 
+						INNER JOIN dbo.tblGLAccount AS B ON B.intAccountID = A.intAccountID 
+						INNER JOIN dbo.tblGLAccountGroup AS C ON C.intAccountGroupID = B.intAccountGroupID
+						LEFT JOIN dbo.tblGLTempCOASegment AS D ON D.intAccountID = B.intAccountID')
+
+				
 END
