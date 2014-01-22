205309551cbc2eb28557e60545291cdc38b54098
Erick Dela Cruz <erick.delacruz@irely.com>
2014-01-22 14:37:19 +0800

    AP-81

--- Database Project/dbo/Tables/tblAPInvalidTransaction.sql
+++ Database Project/dbo/Tables/tblAPInvalidTransaction.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[tblAPInvalidTransaction]
 (
-	[intId] INT NOT NULL PRIMARY KEY, 
+	[intId] INT IDENTITY(1,1) NOT NULL PRIMARY KEY, 
     [strError] NVARCHAR(100) NULL, 
     [strTransactionType] NVARCHAR(50) NULL, 
     [strTransactionId] NVARCHAR(50) NULL
