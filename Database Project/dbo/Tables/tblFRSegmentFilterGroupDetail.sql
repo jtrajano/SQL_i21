42b576f23866076d25936d15f5e4d895325f042b
axelcasauran <axel.casauran@irely.com>
2014-01-22 15:17:32 +0800

    FIN-456

--- Database Project/dbo/Tables/tblFRSegmentFilterGroupDetail.sql
+++ Database Project/dbo/Tables/tblFRSegmentFilterGroupDetail.sql
@@ -1,11 +1,10 @@
 ﻿CREATE TABLE [dbo].[tblFRSegmentFilterGroupDetail] (
     [intSegmentFilterGroupDetailID] INT            IDENTITY (1, 1) NOT NULL,
+	[intSegmentFilterGroupID]		INT            NOT NULL,
     [strJoin]                       NVARCHAR (10)  COLLATE Latin1_General_CI_AS NULL,
     [strSegmentCode]                NVARCHAR (100) COLLATE Latin1_General_CI_AS NULL,
-    [strSegmentFilterGroup]         NVARCHAR (75)  COLLATE Latin1_General_CI_AS NOT NULL,
     [strSegmentName]                NVARCHAR (100) COLLATE Latin1_General_CI_AS NULL,
     [ysnDisplayToHeader]            BIT            DEFAULT ((0)) NULL,
     [intConcurrencyID]              INT            DEFAULT ((1)) NULL,
-    CONSTRAINT [PK_tblFRSegmentFilterGroupDetail] PRIMARY KEY CLUSTERED ([intSegmentFilterGroupDetailID] ASC, [strSegmentFilterGroup] ASC)
+    CONSTRAINT [PK_tblFRSegmentFilterGroupDetail] PRIMARY KEY CLUSTERED ([intSegmentFilterGroupDetailID] ASC, [intSegmentFilterGroupID] ASC)
 );
-
