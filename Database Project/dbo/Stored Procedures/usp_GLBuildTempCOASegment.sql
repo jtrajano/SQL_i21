d93815178412920d3467d6c73809f2ed28973d37
axelcasauran <axel.casauran@irely.com>
2014-01-22 16:53:45 +0800

    FIN-456

--- Database Project/dbo/Stored Procedures/usp_GLBuildTempCOASegment.sql
+++ Database Project/dbo/Stored Procedures/usp_GLBuildTempCOASegment.sql
@@ -5,7 +5,7 @@ BEGIN
 	--CREATE DYNAMIC ACCOUNT STRUCTURE
 	IF EXISTS (SELECT top 1 1  FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'tempDASTable') DROP TABLE tempDASTable 
 	IF EXISTS (SELECT top 1 1  FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'tblGLTempCOASegment') DROP TABLE tblGLTempCOASegment
-
+	BEGIN
 			DECLARE @Segments NVARCHAR(MAX)
 			SELECT @Segments = SUBSTRING((SELECT '],[' + strStructureName FROM tblGLAccountStructure WHERE strType <> 'Divider' FOR XML PATH('')),3,200000) + ']'
 			DECLARE @Query NVARCHAR(MAX)
@@ -33,11 +33,12 @@ BEGIN
 			'
 			
 			EXEC sp_executesql @Query
+	END
 			
 			
-
 	IF EXISTS (SELECT top 1 1  FROM INFORMATION_SCHEMA.VIEWS WHERE TABLE_NAME = 'vyu_GLDetailView') DROP VIEW vyu_GLDetailView
-	
+	IF EXISTS (SELECT top 1 1  FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'tblGLTempCOASegment')
+	BEGIN
 		EXEC ('CREATE VIEW [dbo].[vyu_GLDetailView]
 		AS
 		SELECT		A.intGLDetailID, A.dtmDate, A.strBatchID, A.dblDebit, A.dblCredit, A.dblDebitUnit, A.dblCreditUnit, A.strDescription AS GLDescription, A.strCode, 
@@ -47,17 +48,19 @@ BEGIN
 						INNER JOIN dbo.tblGLAccount AS B ON B.intAccountID = A.intAccountID 
 						INNER JOIN dbo.tblGLAccountGroup AS C ON C.intAccountGroupID = B.intAccountGroupID
 						LEFT JOIN dbo.tblGLTempCOASegment AS D ON D.intAccountID = B.intAccountID')
-						
-						
-						
-	IF EXISTS (SELECT top 1 1  FROM INFORMATION_SCHEMA.VIEWS WHERE TABLE_NAME = 'vyu_GLAccountView') DROP VIEW vyu_GLAccountView
+	END
+
 	
+	IF EXISTS (SELECT top 1 1  FROM INFORMATION_SCHEMA.VIEWS WHERE TABLE_NAME = 'vyu_GLAccountView') DROP VIEW vyu_GLAccountView
+	IF EXISTS (SELECT top 1 1  FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'tblGLTempCOASegment')
+	BEGIN
 		EXEC ('CREATE VIEW [dbo].[vyu_GLAccountView]
 		AS
 		SELECT     C.*, A.strDescription, B.strAccountGroup, B.strAccountType, A.intAccountGroupID, A.ysnIsUsed, A.ysnActive, A.dblOpeningBalance, A.intAccountUnitID
 		FROM		dbo.tblGLAccount AS A 
 					INNER JOIN dbo.tblGLAccountGroup AS B ON B.intAccountGroupID = A.intAccountGroupID
 					LEFT JOIN dbo.tblGLTempCOASegment AS C ON C.intAccountID = A.intAccountID')
+	END
 
 				
 END
