85091d02ff2b5ba45d6d47c8ed14c0c10e3368f5
Feb Montefrio <feb.montefrio@irely.com>
2014-01-24 11:36:05 +0800

    Fix issue on adding deposit and payments in Bank Reconciliation.

--- Database Project/dbo/Stored Procedures/AddPayment.sql
+++ Database Project/dbo/Stored Procedures/AddPayment.sql
@@ -1,7 +1,4 @@
 ﻿
---=====================================================================================================================================
--- 	CREATE THE STORED PROCEDURE AFTER DELETING IT
----------------------------------------------------------------------------------------------------------------------------------------
 CREATE PROCEDURE AddPayment
 	@intBankAccountID INT
 	,@dtmDate DATETIME 
@@ -55,7 +52,7 @@ IF @@ERROR <> 0	GOTO AddPayment_Rollback
 UPDATE	dbo.tblSMStartingNumber
 SET		intNumber += 1
 WHERE	strTransactionType = @STARTING_NUMBER_BANK_TRANSACTION
-IF @@ERROR <> 0	GOTO AddDeposit_Rollback
+IF @@ERROR <> 0	GOTO AddPayment_Rollback
 
 -- Create the Bank Deposit HEADER
 INSERT INTO tblCMBankTransaction(
