a309f280d7631e91c9e03af956dfb902cc21b76a
bonnpoaloponce <bonn.irely$1>
2014-01-29 16:02:24 +0800

    Renamed fields from ID to Id

--- Database Project/dbo/Tables/tblRMArchive.sql
+++ Database Project/dbo/Tables/tblRMArchive.sql
@@ -1,13 +1,15 @@
 ﻿CREATE TABLE [dbo].[tblRMArchive] (
-    [intArchiveID]     INT             IDENTITY (1, 1) NOT NULL,
+    [intArchiveId]     INT             IDENTITY (1, 1) NOT NULL,
     [blbDocument]      VARBINARY (MAX) NULL,
     [strName]          NVARCHAR (100)  COLLATE Latin1_General_CI_AS NULL,
     [dtmDate]          DATETIME        NULL,
     [strDescription]   NVARCHAR (100)  COLLATE Latin1_General_CI_AS NULL,
-    [strUserID]        NVARCHAR (50)   COLLATE Latin1_General_CI_AS NULL,
-    [intConcurrencyId] INT              NOT NULL DEFAULT 1,
-    CONSTRAINT [PK_tblRMArchive] PRIMARY KEY CLUSTERED ([intArchiveID] ASC)
+    [strUserId]        NVARCHAR (50)   COLLATE Latin1_General_CI_AS NULL,
+    [intConcurrencyId] INT             DEFAULT ((1)) NOT NULL,
+    CONSTRAINT [PK_tblRMArchive] PRIMARY KEY CLUSTERED ([intArchiveId] ASC)
 );
+
+
 
 
 
