fb6e4959bf3118c67f9c2402bbaca1fb866748c6
darwin_estrada <darwin.estrada@irely.com>
2014-02-10 19:38:29 +0800

    separated pre-deployment to a different project

---
+++ i21PreDeployment/Scripts/Pre-Deployment/PreDeployment.sql
@@ -0,0 +1,26 @@
+﻿/*
+ Pre-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be executed before the build script.	
+ Use SQLCMD syntax to include a file in the pre-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the pre-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+:r .\DeleteScripts.sql
+
+-- TM
+:r .\1_TM\1_1320_to_1340.sql
+:r .\1_TM\2_DropUniqueConstraints.sql
+
+-- CM
+:r .\2_CM\1_CM.sql
+
+-- DB
+:r .\3_DB\1_1340_to_1410.sql
+
+-- SM
+:r .\4_SM\1_DataCleanup.sql
