4229faad9211f5fbdcfcc7d7617936de1606f5ac
Jayson E. Ramos <jayson.ramos@iRely.com>
2014-02-13 17:43:03 +0800

    Add Petro query to vwCPPayments view.

--- Integration/dbo/Views/vwCPPayments.sql
+++ Integration/dbo/Views/vwCPPayments.sql
@@ -28,7 +28,21 @@ IF  (SELECT TOP 1 ysnUsed FROM ##tblOriginMod WHERE strPrefix = 'PT' and strDBNa
 	EXEC ('
 		CREATE VIEW [dbo].[vwCPPayments]
 		AS
-		select ''PETRO HERE'' XX
+			select
+			p.A4GLIdentity
+			,agloc_name = l.ptloc_name
+			,agpay_chk_no = p.ptpay_check_no
+			,agpay_ref_no = p.ptpay_ref_no
+			,agpay_batch_no = p.ptpay_batch_no
+			,agpay_orig_rev_dt = (case len(convert(varchar, p.ptpay_orig_rev_dt)) when 8 then convert(date, cast(convert(varchar, p.ptpay_orig_rev_dt) AS CHAR(12)), 112) else null end)
+			,agpay_ivc_no = p.ptpay_invc_no
+			,agpay_seq_no = p.ptpay_orig_cr_seq_no
+			,agpay_amt = p.ptpay_amt
+		from
+			ptpaymst p --agpaymst p
+			,ptlocmst l --aglocmst l
+		where
+		 p.ptpay_loc_no = l.ptloc_loc_no
 		')
 GO
 
