d1d6ae1df4b0c354bbda88e2596757f8e7997b83
Jayson E. Ramos <jayson.ramos@iRely.com>
2014-02-13 18:10:09 +0800

    Add Petro query to vwCPPendingPayments view.

--- Integration/dbo/Views/vwCPPendingPayments.sql
+++ Integration/dbo/Views/vwCPPendingPayments.sql
@@ -30,6 +30,20 @@ IF  (SELECT TOP 1 ysnUsed FROM ##tblOriginMod WHERE strPrefix = 'PT' and strDBNa
 	EXEC ('
 		CREATE VIEW [dbo].[vwCPPendingPayments]
 		AS
-		select ''PETRO HERE'' XX
+		select
+			a.ptpye_check_no
+			,agpye_rev_dt = (case len(convert(varchar, a.ptpye_rev_dt)) when 8 then convert(date, cast(convert(varchar, a.ptpye_rev_dt) AS CHAR(12)), 112) else null end)
+			,agpye_amt = sum(a.ptpye_amt)
+			,b.ptivc_cus_no
+			,A4GLIdentity = row_number() over (order by a.ptpye_check_no)
+		from ptpyemst a --agpyemst a
+		left outer join ptivcmst b --agivcmst b
+			on a.ptpye_cus_no = b.ptivc_cus_no
+			and a.ptpye_inc_ref = b.ptivc_invc_no
+			and a.ptpye_ivc_loc_no = b.ptivc_loc_no 
+		group by 
+			a.ptpye_check_no
+			,ptpye_rev_dt
+			,b.ptivc_cus_no
 		')
 GO
