fd0d6894f67d2762a4f1cbfdb0113a461b46fff5
Feb Montefrio <feb.montefrio@irely.com>
2014-02-18 09:39:31 +0800

    1. Convert the CM import scripts into stored procedures. 2. Add a new table (and its default data) for tracking the execution of the data import.

---
+++ i21Database/dbo/Tables/tblCMDataImportStatus.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[tblCMDataImportStatus]
+(
+	[intDataImportStatusId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [strDescription] NVARCHAR(250) NULL, 
+    [dtmImportRun] DATETIME NULL, 
+    [intUserId] INT NULL
+)
