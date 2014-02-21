b6f0d641869436912d1a798840f2efbea4e63cf9
darwin_estrada <darwin.estrada@irely.com>
2014-02-21 09:42:21 -0500

    Revert "Revert "SM-745""
    This reverts commit 1334ea3d4e7b6cf70c7e1160a96d5d9e11007e05.

---
+++ i21Database/dbo/Tables/tblSMUserRoleDashboardPermission.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[tblSMUserRoleDashboardPermission]
+(
+	[intUserRoleDashboardPermissionId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [intUserRoleId] INT NOT NULL, 
+    [intPanelId] INT NOT NULL, 
+    [strPermission] NVARCHAR(20) NOT NULL, 
+    [intConcurrencyId] INT NOT NULL DEFAULT (1), 
+    CONSTRAINT [FK_tblSMUserRoleDashboardPermission_tblSMUserRole] FOREIGN KEY ([intUserRoleId]) REFERENCES [tblSMUserRole]([intUserRoleID]), 
+    CONSTRAINT [FK_tblSMUserRoleDashboardPermission_tblDBPanel] FOREIGN KEY ([intPanelId]) REFERENCES [tblDBPanel]([intPanelId])
+)
+
+GO
+
+CREATE INDEX [IX_tblSMUserRoleDashboardPermission_intPanelId] ON [dbo].[tblSMUserRoleDashboardPermission] ([intPanelId])
+
+GO
+
+CREATE INDEX [IX_tblSMUserRoleDashboardPermission_intUserRoleId] ON [dbo].[tblSMUserRoleDashboardPermission] ([intUserRoleId])
---
+++ i21Database/dbo/Tables/tblSMUserRoleScreenPermission.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[tblSMUserRoleScreenPermission]
+(
+	[intUserRoleScreenPermissionId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [intUserRoleId] INT NOT NULL, 
+    [strScreenName] NVARCHAR(100) NOT NULL, 
+    [strScreenNameSpace] NVARCHAR(150) NOT NULL, 
+    [strModuleName] NVARCHAR(50) NOT NULL, 
+    [strPermission] NVARCHAR(20) NOT NULL, 
+    [intConcurrencyId] INT NOT NULL DEFAULT (1), 
+    CONSTRAINT [FK_tblSMUserRoleScreenPermission_tblSMUserRole] FOREIGN KEY ([intUserRoleId]) REFERENCES [tblSMUserRole]([intUserRoleID]) 
+)
+
+GO
+
+CREATE INDEX [IX_tblSMUserRoleScreenPermission_intUserRoleId] ON [dbo].[tblSMUserRoleScreenPermission] ([intUserRoleId])
