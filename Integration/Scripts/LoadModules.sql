3e63fe05d959b674ad7adfb426843a3ff5650630
darwin_estrada <darwin.estrada@irely.com>
2014-02-25 19:25:05 +0800

    added e-commerce on the origin modules table. changed ##tblOriginMod.strPrefix and ##tblOriginMod.strName to unique fields.

--- Integration/Scripts/LoadModules.sql
+++ Integration/Scripts/LoadModules.sql
@@ -17,8 +17,8 @@ CREATE TABLE ##tblOriginMod
 (
 	 intModId INT IDENTITY(1,1)
 	, strDBName nvarchar(50) NOT NULL 
-	, strPrefix NVARCHAR(5) NOT NULL 
-	, strName NVARCHAR(30) NOT NULL 
+	, strPrefix NVARCHAR(5) NOT NULL UNIQUE
+	, strName NVARCHAR(30) NOT NULL UNIQUE
 	, ysnUsed BIT NOT NULL 
 )
 
@@ -73,7 +73,11 @@ BEGIN
 	EXEC ('INSERT INTO ##tblOriginMod (strDBName, strPrefix, strName, ysnUsed) SELECT TOP 1 db_name(), N''TF'', N''TAX FORMS'', CASE ISNULL(coctl_px_yn, ''N'') WHEN ''Y'' THEN 1 else 0 END FROM coctlmst')
 END
 
-
+-- E-COMMERCE
+IF EXISTS (SELECT TOP 1 1 from INFORMATION_SCHEMA.COLUMNS where COLUMN_NAME = 'coctl_ec')
+BEGIN
+	EXEC ('INSERT INTO ##tblOriginMod (strDBName, strPrefix, strName, ysnUsed) SELECT TOP 1 db_name(), N''EC'', N''E-COMMERCE'', CASE ISNULL(coctl_ec, ''N'') WHEN ''Y'' THEN 1 else 0 END FROM coctlmst')
+END
 
 
 
