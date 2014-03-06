3b5a08e1647f7f8e0f427dc7edf6edc18a35d9bc
darwin_estrada <darwin.estrada@irely.com>
2014-03-06 13:56:58 +0800

    Revert "revert 88139e7 (accidentally merged 1410 to master)"
    This reverts commit 5c64a7b88eeba6029ad4dbaef499f6bceea4671e.

---
+++ i21Database/dbo/Tables/tblSMControl.sql
@@ -0,0 +1,26 @@
+﻿CREATE TABLE [dbo].[tblSMControl]
+(
+	[intControlId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [intScreenId] INT NOT NULL, 
+    [strControlId] NVARCHAR(50) NOT NULL, 
+    [strControlName] NVARCHAR(100) NULL, 
+    [strControlType] NCHAR(10) NOT NULL, 
+    [intConcurrencyId] INT NOT NULL DEFAULT (1), 
+    CONSTRAINT [FK_tblSMControl_tblSMScreen] FOREIGN KEY ([intScreenId]) REFERENCES [tblSMScreen]([intScreenId])
+)
+
+GO
+
+CREATE INDEX [IX_tblSMControl_intScreenId] ON [dbo].[tblSMControl] ([intScreenId])
+
+GO
+
+CREATE INDEX [IX_tblSMControl_strControlId] ON [dbo].[tblSMControl] ([strControlId])
+
+GO
+
+CREATE INDEX [IX_tblSMControl_strControlName] ON [dbo].[tblSMControl] ([strControlName])
+
+GO
+
+CREATE INDEX [IX_tblSMControl_strControlType] ON [dbo].[tblSMControl] ([strControlType])
---
+++ i21Database/dbo/Tables/tblSMUserSecurityScreenPermission.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[tblSMUserSecurityScreenPermission]
+(
+	[intUserSecurityScreenPermissionId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [intUserSecurityId] INT NOT NULL, 
+    [intScreenId] INT NOT NULL, 
+	[strPermission] NVARCHAR(20) NOT NULL, 
+    [intConcurrencyId] INT NOT NULL DEFAULT (1), 
+    CONSTRAINT [FK_tblSMUserSecurityScreenPermission_tblSMUserSecurity] FOREIGN KEY ([intUserSecurityId]) REFERENCES [tblSMUserSecurity]([intUserSecurityID]), 
+    CONSTRAINT [FK_tblSMUserSecurityScreenPermission_tblSMScreen] FOREIGN KEY ([intScreenId]) REFERENCES [tblSMScreen]([intScreenId]) 
+)
+
+GO
+
+CREATE INDEX [IX_tblSMUserSecurityScreenPermission_intUserSecurityId] ON [dbo].[tblSMUserSecurityScreenPermission] ([intUserSecurityId])
+
+GO
+
+CREATE INDEX [IX_tblSMUserSecurityScreenPermission_intScreenId] ON [dbo].[tblSMUserSecurityScreenPermission] ([intScreenId])
