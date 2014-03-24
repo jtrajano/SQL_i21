e237d181126900812f4db716fca7e5bb5349bc01
kenneth lew lagunero <kenneth.lagunero@irely.com>
2014-03-24 13:02:44 +0800

    normalization of tblTMInventoryStatus table

--- i21Database/dbo/Tables/tblTMInventoryStatusType.sql
+++ i21Database/dbo/Tables/tblTMInventoryStatusType.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [dbo].[tblTMInventoryStatusType] (
     [intConcurrencyId]         INT           DEFAULT 1 NOT NULL,
-    [intInventoryStatusTypeID] INT           IDENTITY (1, 1) NOT NULL,
+    [intInventoryStatusTypeId] INT           IDENTITY (1, 1) NOT NULL,
     [strInventoryStatusType]   NVARCHAR (70) COLLATE Latin1_General_CI_AS DEFAULT ('') NOT NULL,
     [ysnDefault]               BIT           DEFAULT 0 NOT NULL,
-    CONSTRAINT [PK_tblTMInventoryStatusType] PRIMARY KEY CLUSTERED ([intInventoryStatusTypeID] ASC)
+    CONSTRAINT [PK_tblTMInventoryStatusType] PRIMARY KEY CLUSTERED ([intInventoryStatusTypeId] ASC)
 );
 
--- i21PreDeployment/Scripts/Pre-Deployment/1_TM/3_1410_to_1420.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/1_TM/3_1410_to_1420.sql
@@ -14,3 +14,12 @@ BEGIN
         EXEC sp_rename 'tblTMDevice.intMeterTypeID', 'intMeterTypeId' , 'COLUMN'
     END
 END
+
+IF EXISTS (SELECT TOP 1 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[tblTMDevice]') AND type in (N'U')) 
+BEGIN
+	IF NOT EXISTS (SELECT TOP 1 1 FROM sys.columns WHERE NAME  = N'intInventoryStatusTypeId' AND OBJECT_ID = OBJECT_ID(N'tblTMDevice')) AND EXISTS (SELECT TOP 1 1 FROM sys.columns WHERE NAME  = N'intInventoryStatusTypeID' AND OBJECT_ID = OBJECT_ID(N'tblTMDevice'))
+    BEGIN
+        EXEC sp_rename 'tblTMDevice.intInventoryStatusTypeID', 'intInventoryStatusTypeId' , 'COLUMN'
+    END
+END
+
