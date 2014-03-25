7e77c2efd5dba0ca7a55f95e8d2263087cc4f66c
Smith Wilson de Jesus <smith.dejesus@irely.com>
2014-03-26 00:50:03 +0800

    Added tblARServiceCharge and fix collation of entity tables

--- i21Database/dbo/Tables/tblEntityNote.sql
+++ i21Database/dbo/Tables/tblEntityNote.sql
@@ -4,9 +4,9 @@
     [dtmDate]          DATETIME       NULL,
     [dtmTime]          DATETIME       NULL,
     [intDuration]      INT            NULL,
-    [strName]          NVARCHAR (100) NULL,
-    [strSubject]       NVARCHAR (100) NULL,
-    [strNotes]         NVARCHAR (MAX) NULL,
+    [strName]          NVARCHAR (100) COLLATE Latin1_General_CI_AS NULL,
+    [strSubject]       NVARCHAR (100) COLLATE Latin1_General_CI_AS NULL,
+    [strNotes]         NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NULL,
     [intConcurrencyId] INT            CONSTRAINT [DF_tblEntityNotes_intConcurrencyId] DEFAULT ((0)) NOT NULL,
     CONSTRAINT [PK_tblEntityNote] PRIMARY KEY CLUSTERED ([intEntityNoteId] ASC)
 );
