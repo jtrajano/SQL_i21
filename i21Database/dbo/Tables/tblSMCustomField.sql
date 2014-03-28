d7721257320b2a61cf75c5e397f9b3c1f3da7ff5
Von Pasion <von.pasion@irely.com>
2014-03-28 15:09:42 +0800

    COM-108

--- i21Database/dbo/Tables/tblSMCustomField.sql
+++ i21Database/dbo/Tables/tblSMCustomField.sql
@@ -1,11 +1,14 @@
 ﻿CREATE TABLE [dbo].[tblSMCustomField] (
     [intCustomFieldId] INT            IDENTITY (1, 1) NOT NULL,
-    [strScreen]        NVARCHAR (50)  COLLATE Latin1_General_CI_AS NULL,
-    [strModule]        NVARCHAR (50)  COLLATE Latin1_General_CI_AS NULL,
-    [strLayout]        NVARCHAR (50)  COLLATE Latin1_General_CI_AS NULL,
-    [strTabName]       NVARCHAR (50)  COLLATE Latin1_General_CI_AS NULL,
+    [strScreen]        NVARCHAR (50)  COLLATE Latin1_General_CI_AS NOT NULL,
+    [strModule]        NVARCHAR (50)  COLLATE Latin1_General_CI_AS NOT NULL,
+    [strLayout]        NVARCHAR (50)  COLLATE Latin1_General_CI_AS NOT NULL,
+    [strTabName]       NVARCHAR (50)  COLLATE Latin1_General_CI_AS NOT NULL,
     [strDescription]   NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NULL,
+    [ysnBuild]         BIT            NOT NULL,
     [intConcurrencyId] INT            NOT NULL,
     CONSTRAINT [PK_tblSMCustomField] PRIMARY KEY CLUSTERED ([intCustomFieldId] ASC)
 );
+
+
 
