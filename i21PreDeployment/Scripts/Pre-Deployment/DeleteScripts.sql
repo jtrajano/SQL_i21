cb572737ba95ea8241fe1f959fcbae25cd20c6cc
bonnpoaloponce <bonn.irely$1>
2014-03-31 15:15:29 +0800

    Check if db object is existing before deleting -Reports -Views -Procedures

--- i21PreDeployment/Scripts/Pre-Deployment/DeleteScripts.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/DeleteScripts.sql
@@ -2,154 +2,136 @@
 
 
 PRINT N' BEGIN CONSOLIDATED DELETE PATH: 13.4 to 14.1'
+	
+	GO
+	/*******************  BEGIN DROP 13.4 REPORTS TABLE CONSTRAINTS  *******************/
 
+	PRINT('*******************  BEGIN DROP 13.4 REPORTS TABLE CONSTRAINTS  *******************')
+	SELECT * INTO #TEMPConstraints 
+	FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS 
+	WHERE CONSTRAINT_TYPE = 'FOREIGN KEY'
+	AND TABLE_NAME 
+	IN ('tblRMArchives'
+	,'tblRMCompanyInformations'
+	,'tblRMConfigurations'
+	,'tblRMConnections'
+	,'tblRMCriteriaFields'
+	,'tblRMCriteriaFieldSelections'
+	,'tblRMDatasources'
+	,'tblRMFieldSelectionFilters'
+	,'tblRMFilters'
+	,'tblRMOptions'
+	,'tblRMReports'
+	,'tblRMSorts'
+	,'tblRMSubreportSettings'
+	,'tblRMUsers')
+	
+	
+	DECLARE @tableName NVARCHAR(100)
+	DECLARE @constraintName NVARCHAR(100)
 
-	GO
-	PRINT N'Dropping FK_dbo.Reports_dbo.CompanyInformations_intCompanyInformationId...';
-	GO
-	ALTER TABLE [dbo].[tblRMReports] DROP CONSTRAINT [FK_dbo.Reports_dbo.CompanyInformations_intCompanyInformationId];
-	GO
-	PRINT N'Dropping FK_dbo.CriteriaFieldSelections_dbo.Connections_intConnectionId...';
-	GO
-	ALTER TABLE [dbo].[tblRMCriteriaFieldSelections] DROP CONSTRAINT [FK_dbo.CriteriaFieldSelections_dbo.Connections_intConnectionId];
-	GO
-	PRINT N'Dropping FK_dbo.Datasources_dbo.Connections_intConnectionId...';
-	GO
-	ALTER TABLE [dbo].[tblRMDatasources] DROP CONSTRAINT [FK_dbo.Datasources_dbo.Connections_intConnectionId];
-	GO
-	PRINT N'Dropping FK_dbo.FieldSelectionFilters_dbo.CriteriaFields_intCriteriaFieldId...';
-	GO
-	ALTER TABLE [dbo].[tblRMFieldSelectionFilters] DROP CONSTRAINT [FK_dbo.FieldSelectionFilters_dbo.CriteriaFields_intCriteriaFieldId];
-	GO
-	PRINT N'Dropping FK_dbo.CriteriaFields_dbo.CriteriaFieldSelections_intCriteriaFieldSelectionId...';
-	GO
-	ALTER TABLE [dbo].[tblRMCriteriaFields] DROP CONSTRAINT [FK_dbo.CriteriaFields_dbo.CriteriaFieldSelections_intCriteriaFieldSelectionId];
-	GO
-	PRINT N'Dropping FK_dbo.Options_dbo.Reports_intReportId...';
-	GO
-	ALTER TABLE [dbo].[tblRMOptions] DROP CONSTRAINT [FK_dbo.Options_dbo.Reports_intReportId];
-	GO
-	PRINT N'Dropping FK_dbo.Sorts_dbo.Reports_intReportId...';
-	GO
-	ALTER TABLE [dbo].[tblRMSorts] DROP CONSTRAINT [FK_dbo.Sorts_dbo.Reports_intReportId];
-	GO
-	PRINT N'Dropping FK_dbo.SubreportSettings_dbo.Reports_intReportId...';
-	GO
-	ALTER TABLE [dbo].[tblRMSubreportSettings] DROP CONSTRAINT [FK_dbo.SubreportSettings_dbo.Reports_intReportId];
-	GO
-	PRINT N'Dropping FK_dbo.Configurations_dbo.Reports_intReportId...';
-	GO
-	ALTER TABLE [dbo].[tblRMConfigurations] DROP CONSTRAINT [FK_dbo.Configurations_dbo.Reports_intReportId];
-	GO
-	PRINT N'Dropping FK_dbo.CriteriaFields_dbo.Reports_intReportId...';
-	GO
-	ALTER TABLE [dbo].[tblRMCriteriaFields] DROP CONSTRAINT [FK_dbo.CriteriaFields_dbo.Reports_intReportId];
-	GO
-	PRINT N'Dropping FK_dbo.Datasources_dbo.Reports_intReportId...';
-	GO
-	ALTER TABLE [dbo].[tblRMDatasources] DROP CONSTRAINT [FK_dbo.Datasources_dbo.Reports_intReportId];
-	GO
-	PRINT N'Dropping FK_dbo.Filters_dbo.Reports_intReportId...';
-	GO
-	ALTER TABLE [dbo].[tblRMFilters] DROP CONSTRAINT [FK_dbo.Filters_dbo.Reports_intReportId];
-	GO
-	PRINT N'Dropping [dbo].[tblRMArchives]...';
-	GO
-	DROP TABLE [dbo].[tblRMArchives];
-	GO
-	PRINT N'Dropping [dbo].[tblRMCompanyInformations]...';
-	GO
-	DROP TABLE [dbo].[tblRMCompanyInformations];
-	GO
-	PRINT N'Dropping [dbo].[tblRMConfigurations]...';
-	GO
-	DROP TABLE [dbo].[tblRMConfigurations];
-	GO
-	PRINT N'Dropping [dbo].[tblRMFieldSelectionFilters]...';
-	GO
-	DROP TABLE [dbo].[tblRMFieldSelectionFilters];
-	GO
-	PRINT N'Dropping [dbo].[tblRMFilters]...';
-	GO
-	DROP TABLE [dbo].[tblRMFilters];
-	GO
-	PRINT N'Dropping [dbo].[tblRMOptions]...';
-	GO
-	DROP TABLE [dbo].[tblRMOptions];
-	GO
-	PRINT N'Dropping [dbo].[tblRMSorts]...';
-	GO
-	DROP TABLE [dbo].[tblRMSorts];
-	GO
-	PRINT N'Dropping [dbo].[tblRMSubreportSettings]...';
-	GO
-	DROP TABLE [dbo].[tblRMSubreportSettings];
-	GO
-	PRINT N'Dropping [dbo].[tblRMUsers]...';
-	GO
-	DROP TABLE [dbo].[tblRMUsers];
-	GO
-	PRINT N'Dropping [dbo].[vwapivcmst]...';
-	GO
-	DROP VIEW [dbo].[vwapivcmst];
-	GO
-	PRINT N'Dropping [dbo].[vwcoctlmst]...';
-	GO
-	DROP VIEW [dbo].[vwcoctlmst];
-	GO
-	PRINT N'Dropping [dbo].[vwticmst]...';
-	GO
-	DROP VIEW [dbo].[vwticmst];
-	GO
-	PRINT N'Dropping [dbo].[vyu_GLAccountView]...';
-	GO
-	DROP VIEW [dbo].[vyu_GLAccountView];
-	GO
-	PRINT N'Dropping [dbo].[vyu_GLDetailView]...';
-	GO
-	DROP VIEW [dbo].[vyu_GLDetailView];
-	GO
-	PRINT N'Dropping [dbo].[usp_BuildGLAccount]...';
-	GO
-	DROP PROCEDURE [dbo].[usp_BuildGLAccount];
-	GO
-	PRINT N'Dropping [dbo].[usp_BuildGLAccountTemporary]...';
-	GO
-	DROP PROCEDURE [dbo].[usp_BuildGLAccountTemporary];
-	GO
-	PRINT N'Dropping [dbo].[usp_RMInsertDynamicParameterFields]...';
-	GO
-	DROP PROCEDURE [dbo].[usp_RMInsertDynamicParameterFields];
-	GO
-	PRINT N'Dropping [dbo].[usp_SyncAccounts]...';
-	GO
-	DROP PROCEDURE [dbo].[usp_SyncAccounts];
-	GO
-	PRINT N'Dropping [dbo].[tblRMConnections]...';
-	GO
-	DROP TABLE [dbo].[tblRMConnections];
-	GO
-	PRINT N'Dropping [dbo].[tblRMCriteriaFields]...';
-	GO
-	DROP TABLE [dbo].[tblRMCriteriaFields];
-	GO
-	PRINT N'Dropping [dbo].[tblRMCriteriaFieldSelections]...';
-	GO
-	DROP TABLE [dbo].[tblRMCriteriaFieldSelections];
-	GO
-	PRINT N'Dropping [dbo].[tblRMDatasources]...';
-	GO
-	DROP TABLE [dbo].[tblRMDatasources];
-	GO
-	PRINT N'Dropping [dbo].[tblRMReports]...';
-	GO
-	DROP TABLE [dbo].[tblRMReports];
-	GO
-	PRINT N'Dropping [dbo].[usp_BuildGLTempCOASegment]...';
-	GO
-	DROP PROCEDURE [dbo].[usp_BuildGLTempCOASegment];
-	GO
+	WHILE exists(SELECT TOP 1 1 FROM #TEMPConstraints)
+	BEGIN
+		SELECT TOP 1 @tableName= TABLE_NAME, @constraintName = CONSTRAINT_NAME FROM #TEMPConstraints
+		PRINT('ALTER TABLE ' +  @tableName + ' DROP CONSTRAINT [' + @constraintName + ']')
+		EXEC ('ALTER TABLE ' +  @tableName + ' DROP CONSTRAINT [' + @constraintName + ']')
+		DELETE FROM #TEMPConstraints WHERE CONSTRAINT_NAME = @constraintName
+	END
 
+	DROP TABLE #TEMPConstraints
+	
+	PRINT('*******************  END DROP 13.4 REPORTS TABLE CONSTRAINTS  *******************')
+	
+	
+	/*******************  END DROP 13.4 REPORTS TABLE CONSTRAINTS  *******************/
 
+	/*******************  BEGIN DROP 13.4 REPORTS TABLE  *******************/
+
+	PRINT('*******************  BEGIN DROP 13.4 REPORTS TABLE  *******************')
+	SELECT * INTO #TEMPReportTables 
+	FROM INFORMATION_SCHEMA.TABLES 
+	WHERE TABLE_NAME 
+	IN ('tblRMArchives'
+	,'tblRMCompanyInformations'
+	,'tblRMConfigurations'
+	,'tblRMConnections'
+	,'tblRMCriteriaFields'
+	,'tblRMCriteriaFieldSelections'
+	,'tblRMDatasources'
+	,'tblRMFieldSelectionFilters'
+	,'tblRMFilters'
+	,'tblRMOptions'
+	,'tblRMReports'
+	,'tblRMSorts'
+	,'tblRMSubreportSettings'
+	,'tblRMUsers')
+
+	WHILE exists(SELECT TOP 1 1 FROM #TEMPReportTables)
+		BEGIN
+			SELECT TOP 1 @tableName= TABLE_NAME FROM #TEMPReportTables
+			PRINT('DROP TABLE [' + @tableName + ']')
+			EXEC ('DROP TABLE [' + @tableName + ']')
+			DELETE FROM #TEMPReportTables WHERE TABLE_NAME = @tableName
+		END
+	
+	DROP TABLE #TEMPReportTables
+	
+	PRINT('*******************  END DROP 13.4 REPORTS TABLE  *******************')
+	/*******************  END DROP 13.4 REPORTS TABLE  *******************/
+
+	/*******************  BEGIN DROP 13.4 VIEWS  *******************/
+	PRINT('*******************  BEGIN DROP 13.4 VIEWS  *******************')
+	SELECT * INTO #TEMPViews 
+	FROM INFORMATION_SCHEMA.VIEWS 
+	WHERE TABLE_NAME 
+	IN('vwapivcmst'
+	,'vwcoctlmst'
+	,'vwticmst'
+	,'vyu_GLAccountView'
+	,'vyu_GLDetailView')
+	
+	DECLARE @viewName NVARCHAR(100)
+	
+	WHILE exists(SELECT TOP 1 1 FROM #TEMPViews)
+	BEGIN
+		SELECT TOP 1 @viewName= TABLE_NAME FROM #TEMPViews
+		PRINT('DROP VIEW [' + @viewName + ']')
+		EXEC ('DROP VIEW [' + @viewName + ']')
+		DELETE FROM #TEMPViews WHERE TABLE_NAME = @viewName
+	END
+
+	DROP TABLE #TEMPViews
+
+	PRINT('*******************  END DROP 13.4 VIEWS  *******************')
+	/*******************  END DROP 13.4 VIEWS  *******************/
+
+	/*******************  BEGIN DROP 13.4 PROCEDURES  *******************/
+	PRINT('*******************  BEGIN DROP 13.4 PROCEDURES  *******************')
+	
+	SELECT * INTO #TEMPProcedures 
+	FROM INFORMATION_SCHEMA.ROUTINES 
+	WHERE ROUTINE_TYPE = 'PROCEDURE' 
+	AND ROUTINE_NAME 
+	IN ('usp_BuildGLAccount'
+	,'usp_BuildGLAccountTemporary'
+	,'usp_RMInsertDynamicParameterFields'
+	,'usp_SyncAccounts'
+	,'usp_BuildGLTempCOASegment')
+	
+	DECLARE @procedureName NVARCHAR(100)
+	
+	WHILE exists(SELECT TOP 1 1 FROM #TEMPProcedures)
+	BEGIN
+		SELECT TOP 1 @procedureName= ROUTINE_NAME FROM #TEMPProcedures
+		PRINT('DROP PROCEDURE [' + @procedureName + ']')
+		EXEC ('DROP PROCEDURE [' + @procedureName + ']')
+		DELETE FROM #TEMPProcedures WHERE ROUTINE_NAME = @procedureName
+	END
+
+	DROP TABLE #TEMPProcedures
+
+	PRINT('*******************  END DROP 13.4 PROCEDURES  *******************')
+	/*******************  END DROP 13.4 PROCEDURES  *******************/
+	GO
 
 PRINT N' END CONSOLIDATED DELETE PATH: 13.4 to 14.1'
