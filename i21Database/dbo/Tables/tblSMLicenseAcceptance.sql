aa8865b19ffe74d34a2cd2c50f783982d3ec935c
Lawrence Zabala <christopher_z@comtechsolutions.com>
2014-04-01 17:56:49 +0800

    SM-808 SM-746

---
+++ i21Database/dbo/Tables/tblSMLicenseAcceptance.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[tblSMLicenseAcceptance]
+(
+	[intLicenseAcceptanceId] INT NOT NULL PRIMARY KEY, 
+    [intUserSecurityId] INT NOT NULL, 
+    [strEULAVersion] NVARCHAR(50) NOT NULL, 
+    [dtmDateAccepted] DATE NOT NULL, 
+    [intConcurrencyId] INT NOT NULL DEFAULT (1), 
+    CONSTRAINT [FK_tblSMLicenseAcceptance_tblSMUserSecurity] FOREIGN KEY ([intUserSecurityId]) REFERENCES [tblSMUserSecurity]([intUserSecurityID])
+)
+
+GO
+
+CREATE INDEX [IX_tblSMLicenseAcceptance_strEULAVersion] ON [dbo].[tblSMLicenseAcceptance] ([strEULAVersion])
