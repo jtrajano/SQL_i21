a372e1ee6ff26c2fcd96331f939bd985f00c056e
Feb Montefrio <feb.montefrio@irely.com>
2014-04-02 16:09:50 +0800

    Resolve CM-241

--- i21Database/dbo/Stored Procedures/uspCMPostBankTransfer.sql
+++ i21Database/dbo/Stored Procedures/uspCMPostBankTransfer.sql
@@ -195,6 +195,14 @@ BEGIN
 	END
 END 
 
+-- Check if amount is zero. 
+IF @dblAmount = 0 AND @ysnPost = 1 AND @ysnRecap = 0
+BEGIN 
+	-- Cannot post a zero-value transaction.
+	RAISERROR(50020, 11, 1)
+	GOTO Post_Rollback
+END 
+
 --=====================================================================================================================================
 -- 	PROCESSING OF THE G/L ENTRIES. 
 ---------------------------------------------------------------------------------------------------------------------------------------
