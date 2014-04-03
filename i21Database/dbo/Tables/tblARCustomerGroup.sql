b3e076abb6f567886c8b5f23a06c92f9e4522744
Smith Wilson de Jesus <smith.dejesus@irely.com>
2014-04-03 15:19:29 +0800

    Modified AR tables and remove intEntityContactId field on EntityContact table

---
+++ i21Database/dbo/Tables/tblARCustomerGroup.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[tblARCustomerGroup] (
+    [intCustomerGroupId] INT            IDENTITY (1, 1) NOT NULL,
+    [strGroupName]       NVARCHAR (50)  NOT NULL,
+    [strDescription]     NVARCHAR (100) NULL,
+    [intConcurrencyId]   INT            NOT NULL,
+    CONSTRAINT [PK_tblARCustomerGroup] PRIMARY KEY CLUSTERED ([intCustomerGroupId] ASC),
+    CONSTRAINT [UKstrGroupName] UNIQUE NONCLUSTERED ([strGroupName] ASC)
+);
+
