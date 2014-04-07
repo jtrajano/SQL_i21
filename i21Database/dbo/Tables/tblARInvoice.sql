048a80e59b1e7be4317c2a23cd4b3e797aef4ec2
Smith Wilson de Jesus <smith.dejesus@irely.com>
2014-04-07 15:37:57 +0800

    Added tblARInvoice used in AR

---
+++ i21Database/dbo/Tables/tblARInvoice.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[tblARInvoice] (
+    [intInvoiceId]     INT             IDENTITY (1, 1) NOT NULL,
+    [strInvoiceNumber] NVARCHAR (25)   NOT NULL,
+    [intEntityId]      INT             NOT NULL,
+    [dtmDate]          DATETIME        NOT NULL,
+    [strType]          NVARCHAR (25)   NULL,
+    [dblTotal]         NUMERIC (18, 6) NULL,
+    [dblAmountPaid]    NUMERIC (18, 6) NULL,
+    [ysnPaid]          BIT             NOT NULL,
+    [dblAmountDue]     NUMERIC (18, 6) NULL,
+    [intConcurrencyId] INT             CONSTRAINT [DF_tblARInvoice_intConcurrencyId] DEFAULT ((0)) NOT NULL,
+    CONSTRAINT [PK_tblARInvoice] PRIMARY KEY CLUSTERED ([intInvoiceId] ASC)
+);
+
