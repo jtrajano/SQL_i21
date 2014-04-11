434e62f48c4ea610244e95b7eaeef28aa9dcf136
Smith Wilson de Jesus <smith.dejesus@irely.com>
2014-04-11 13:34:39 +0800

    Added EntityTableDataFix and renaming of some fields on tblAPVendor

---
+++ i21PreDeployment/Scripts/Pre-Deployment/8_AP/1_1410_to_1420.sql
@@ -0,0 +1,12 @@
+﻿IF EXISTS (SELECT TOP 1 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[tblAPVendor]') AND type in (N'U')) 
+BEGIN
+	IF NOT EXISTS (SELECT TOP 1 1 FROM sys.columns WHERE NAME  = N'intDefaultLocationId' AND OBJECT_ID = OBJECT_ID(N'tblAPVendor')) AND EXISTS (SELECT TOP 1 1 FROM sys.columns WHERE NAME  = N'intEntityLocationId' AND OBJECT_ID = OBJECT_ID(N'tblAPVendor'))
+    BEGIN
+        EXEC sp_rename 'tblAPVendor.intEntityLocationId', 'intDefaultLocationId' , 'COLUMN'
+    END
+
+	IF NOT EXISTS (SELECT TOP 1 1 FROM sys.columns WHERE NAME  = N'intDefaultContactId' AND OBJECT_ID = OBJECT_ID(N'tblAPVendor')) AND EXISTS (SELECT TOP 1 1 FROM sys.columns WHERE NAME  = N'intEntityContactId' AND OBJECT_ID = OBJECT_ID(N'tblAPVendor'))
+    BEGIN
+        EXEC sp_rename 'tblAPVendor.intEntityContactId', 'intDefaultContactId' , 'COLUMN'
+    END
+END
