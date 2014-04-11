a44ca0b06aafd62cbd193f2ba53564d4e8c69e54
Jayson Ramos <jayson.ramos@iRely.com>
2014-04-11 17:30:18 +0800

    Add Customer Portal Preference table. Add Help Desk File Upload table.

---
+++ i21Database/dbo/Tables/tblHDUpload.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[tblHDUpload]
+(
+		[intUploadId] [int] IDENTITY(1,1) NOT NULL,
+	[intTicketCommentId] [int] NULL,
+	[strFileIdentifier] [uniqueidentifier] NOT NULL,
+	[strFileName] [nvarchar](100) COLLATE Latin1_General_CI_AS NOT NULL,
+	[strFileLocation] [nvarchar](max) COLLATE Latin1_General_CI_AS NOT NULL,
+	[blbFile] [varbinary](max) NULL,
+	[intConcurrencyId] [int] NOT NULL, 
+		CONSTRAINT [PK_tblHDUpload] PRIMARY KEY CLUSTERED ([intUploadId] ASC),
+    CONSTRAINT [FK_HDUpload_Ticket] FOREIGN KEY ([intTicketCommentId]) REFERENCES [dbo].[tblHDTicketComment] ([intTicketCommentId]) on delete cascade
+)
