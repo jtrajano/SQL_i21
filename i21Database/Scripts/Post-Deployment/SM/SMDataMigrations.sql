ec7a4ddafb88818fecc80d82068683600a823a79
Lawrence Zabala <christopher_z@comtechsolutions.com>
2014-04-15 13:31:48 +0800

    SM-833 SM-800

--- i21Database/Scripts/Post-Deployment/SM/SMDataMigrations.sql
+++ i21Database/Scripts/Post-Deployment/SM/SMDataMigrations.sql
@@ -3,3 +3,10 @@
 EXEC uspSMBuildSecurityMenus
 
 GO
+
+--Set default Dashboard Role to all users
+UPDATE tblSMUserSecurity
+SET strDashboardRole = 'User'
+WHERE ISNULL(strDashboardRole, '') = ''
+
+GO
