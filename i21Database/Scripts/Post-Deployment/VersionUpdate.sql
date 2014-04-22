f6bd6e49c8fa892be53d5f07b34d990aab1fb59f
darwin_estrada <darwin.estrada@irely.com>
2014-04-22 14:42:22 +0800

    Database Version update to 14.2

--- i21Database/Scripts/Post-Deployment/VersionUpdate.sql
+++ i21Database/Scripts/Post-Deployment/VersionUpdate.sql
@@ -2,7 +2,7 @@
 	PRINT N'BEGIN INSERT VERSION UPDATE'
 GO
 	INSERT INTO tblSMBuildNumber (strVersionNo, dtmLastUpdate)
-	SELECT '14.1', getdate()
+	SELECT '14.2', getdate()
 GO
 	PRINT N'END INSERT VERSION UPDATE'
 GO
