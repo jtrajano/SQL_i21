d65418cc8bf655fc3298368da787ea25d97e8ad3
Lawrence Zabala <christopher_z@comtechsolutions.com>
2014-04-25 14:06:39 +0800

    SM-861

---
+++ i21Database/dbo/Tables/tblSMUserSecurityMenuFavorite.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[tblSMUserSecurityMenuFavorite]
+(
+	[intUserSecurityMenuFavoriteId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [intUserSecurityMenuId] INT NOT NULL, 
+    [intSort] INT NULL DEFAULT (1), 
+	[intConcurrencyId] INT NOT NULL DEFAULT (1), 
+    CONSTRAINT [FK_tblSMUserSecurityMenuFavorite_tblSMUserSecurityMenu] FOREIGN KEY ([intUserSecurityMenuId]) REFERENCES [tblSMUserSecurityMenu]([intUserSecurityMenuId]) ON DELETE CASCADE
+)
