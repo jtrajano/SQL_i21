f3561bf4993df4a297c2d8859e4efb2727723422
Smith Wilson de Jesus <smith.dejesus@irely.com>
2014-05-02 20:42:07 +0800

    TM-623

--- i21Database/dbo/Tables/tblEntityToContact.sql
+++ i21Database/dbo/Tables/tblEntityToContact.sql
@@ -3,10 +3,10 @@
     [intEntityId]          INT NULL,
     [intContactId]         INT NULL,
     [intLocationId]        INT NULL,
-    [intConcurrencyId]     INT NOT NULL DEFAULT 0,
+    [intConcurrencyId]     INT DEFAULT ((0)) NOT NULL,
     CONSTRAINT [PK_tblEntity2Contact] PRIMARY KEY CLUSTERED ([intEntityToContactId] ASC),
-    CONSTRAINT [FK_tblEntityToContact_tblEntity] FOREIGN KEY ([intEntityId]) REFERENCES [dbo].[tblEntity] ([intEntityId]),
-    CONSTRAINT [FK_tblEntityToContact_tblEntityContact] FOREIGN KEY ([intContactId]) REFERENCES [dbo].[tblEntityContact] ([intEntityId]) ON DELETE SET DEFAULT,
-    CONSTRAINT [FK_tblEntityToContact_tblEntityLocation] FOREIGN KEY ([intLocationId]) REFERENCES [dbo].[tblEntityLocation] ([intEntityLocationId])
+    CONSTRAINT [FK_tblEntityToContact_tblEntityContact] FOREIGN KEY ([intContactId]) REFERENCES [dbo].[tblEntityContact] ([intEntityId]) ON DELETE SET DEFAULT
 );
+
+
 
