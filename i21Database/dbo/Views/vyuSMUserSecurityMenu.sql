58fe14906116635996ed5e117a467ae7d11b90e3
Lawrence Zabala <christopher_z@comtechsolutions.com>
2014-05-07 12:06:47 +0800

    SM-910

---
+++ i21Database/dbo/Views/vyuSMUserSecurityMenu.sql
@@ -0,0 +1,22 @@
+﻿CREATE VIEW [dbo].vyuSMUserSecurityMenu
+AS 
+
+SELECT 
+intUserSecurityMenuId,
+intUserSecurityId,
+intMenuId,
+intParentMenuId,
+UserMenu.ysnVisible,
+UserMenu.intSort,
+strMenuName,
+strModuleName,
+strDescription,
+strType,
+strCommand,
+strIcon,
+ysnExpanded,
+ysnIsLegacy,
+ysnLeaf,
+UserMenu.intConcurrencyId
+FROM tblSMUserSecurityMenu UserMenu
+LEFT JOIN tblSMMasterMenu Menu ON Menu.intMenuID = UserMenu.intMenuId
