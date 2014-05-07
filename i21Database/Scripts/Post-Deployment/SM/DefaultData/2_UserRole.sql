dcafd6f344a918cfbf486da99ee6971c81c5aad5
Lawrence Zabala <christopher_z@comtechsolutions.com>
2014-05-07 17:04:07 +0800

    SM-907

--- i21Database/Scripts/Post-Deployment/SM/DefaultData/2_UserRole.sql
+++ i21Database/Scripts/Post-Deployment/SM/DefaultData/2_UserRole.sql
@@ -3,14 +3,12 @@
 GO
 	ALTER TABLE tblSMUserSecurity NOCHECK CONSTRAINT FK_UserSecurity_UserRole
 GO
+	DELETE FROM tblSMUserRole WHERE intUserRoleID IN (1, 2)
+GO
 	SET IDENTITY_INSERT [dbo].[tblSMUserRole] ON
 	IF NOT EXISTS (SELECT TOP 1 1 FROM tblSMUserRole WHERE strName = 'ADMIN') INSERT [dbo].[tblSMUserRole] ([intUserRoleID], [strName], [strDescription], [ysnAdmin]) VALUES (1, N'ADMIN', N'Do not use in Production! For Demo Purposes Only!', 1)
 	IF NOT EXISTS (SELECT TOP 1 1 FROM tblSMUserRole WHERE strName = 'USER') INSERT [dbo].[tblSMUserRole] ([intUserRoleID], [strName], [strDescription], [ysnAdmin]) VALUES (2, N'USER', N'Do not use in Production! For Demo Purposes Only!', 0)
 	SET IDENTITY_INSERT [dbo].[tblSMUserRole] OFF
-GO
-	UPDATE tblSMUserRole
-	SET strDescription = 'Do not use in Production! For Demo Purposes Only!'
-	WHERE strName IN ('USER', 'ADMIN')
 GO
 	ALTER TABLE tblSMUserSecurity CHECK CONSTRAINT FK_UserSecurity_UserRole
 GO
--- i21Database/dbo/Tables/tblSMUserRole.sql
+++ i21Database/dbo/Tables/tblSMUserRole.sql
@@ -1,12 +1,13 @@
 ﻿CREATE TABLE [dbo].[tblSMUserRole] (
     [intUserRoleID]     INT            IDENTITY (1, 1) NOT NULL,
-    [strName]           NVARCHAR (50)  COLLATE Latin1_General_CI_AS NULL,
+    [strName]           NVARCHAR (50)  COLLATE Latin1_General_CI_AS NULL ,
     [strDescription]    NVARCHAR (50)  COLLATE Latin1_General_CI_AS NULL,
     [strMenu]           NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NOT NULL,
     [strMenuPermission] NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NULL,
     [strForm]           NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NULL,
     [ysnAdmin]          BIT            DEFAULT ((0)) NOT NULL,
     [intConcurrencyId]  INT            DEFAULT (1) NOT NULL,
-    CONSTRAINT [PK_Role] PRIMARY KEY CLUSTERED ([intUserRoleID] ASC)
+    CONSTRAINT [PK_Role] PRIMARY KEY CLUSTERED ([intUserRoleID] ASC), 
+    CONSTRAINT [UQ_tblSMUserRole_strName] UNIQUE ([strName]) 
 );
 
