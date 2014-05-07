c40bcbad2a0b348a5e39c7a382bd8d95b6d17a87
Lawrence Zabala <christopher_z@comtechsolutions.com>
2014-05-07 16:06:38 +0800

    SM-907

--- i21Database/Scripts/Post-Deployment/SM/SMDataMigrations.sql
+++ i21Database/Scripts/Post-Deployment/SM/SMDataMigrations.sql
@@ -1,4 +1,20 @@
 ﻿GO
+	-- Reset Demo User Roles and permissions
+
+	DECLARE @AdminId INT
+	DECLARE @UserId INT
+
+	SELECT TOP 1 @AdminId = intUserRoleID FROM tblSMUserRole WHERE strName = 'ADMIN'
+	SELECT TOP 1 @UserId = intUserRoleID FROM tblSMUserRole WHERE strName = 'USER'
+
+	UPDATE tblSMUserRoleMenu
+	SET ysnVisible = 1
+	WHERE intUserRoleId IN (@AdminId, @UserId)
+
+	EXEC uspSMUpdateUserRoleMenus @AdminId
+	EXEC uspSMUpdateUserRoleMenus @UserId
+
+GO
 
 --Set default Dashboard Role to all users
 UPDATE tblSMUserSecurity
