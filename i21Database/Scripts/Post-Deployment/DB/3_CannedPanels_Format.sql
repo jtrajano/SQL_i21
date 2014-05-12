4a65f790ce9c88d04103bbb81b12c41fd0dd099e
Randy Honorato <randy_h@comtechsolutions.com>
2014-05-12 18:40:09 +0800

    IG-537 - Script out the latest canned panel list.

--- i21Database/Scripts/Post-Deployment/DB/3_CannedPanels_Format.sql
+++ i21Database/Scripts/Post-Deployment/DB/3_CannedPanels_Format.sql
@@ -23,15 +23,13 @@ Create TABLE #TampCannedPanelFormat
     [intSort]           SMALLINT       NOT NULL,
     [strType]           NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NULL,
     [ysnVisible]        BIT            NOT NULL,
-    [intCannedPanelId] INT NOT NULL DEFAULT ((0)), 
+	[intConcurrencyId] INT			   NOT NULL,
+    [intCannedPanelId]  INT			   NOT NULL DEFAULT ((0)), 
 	
-	--[intConcurrencyId ] INT            DEFAULT ((1)) NOT NULL,
 )
 
 print('/*******************  BEGIN INSERTING canned panels on temporary panel format table  *******************/')
-INSERT INTO #TampCannedPanelFormat VALUES (1, N'stphy_diff_qty', N'>', N'0', N'', -8388652, N'Bold', 0, N'Cell', 12, 0, 2, N'', 0, 12)
-INSERT INTO #TampCannedPanelFormat VALUES (2, N'stphy_diff_qty', N'<', N'0', N'', -65536, N'Bold', 0, N'Cell', 12, 0, 3, N'', 0, 12)
-INSERT INTO #TampCannedPanelFormat VALUES (3, N'gacnt_due_rev_dt', N'>', N'0', N'', -657931, N'Regular', 0, N'Cell', 17, 0, 2, N'', 0, 17)
+INSERT INTO #TampCannedPanelFormat VALUES (4, N'gacnt_due_rev_dt', N'>', N'0', N'', -657931, N'Regular', 0, N'Cell', 129, 0, 2, N'', 0, 1, 17)
 print('/*******************  END INSERTING canned panels on temporary panel format table  *******************/')
 
 print('/*******************  BEGIN DELETE old panel format records  *******************/')
@@ -58,8 +56,8 @@ BEGIN
 	SET @intCurrentPanelId = (SELECT TOP 1 intPanelId FROM tblDBPanel WHERE intCannedPanelId = @intCannedPanelId)
 		
 	INSERT INTO [dbo].[tblDBPanelFormat] 
-		([strColumn], [strCondition], [strValue1], [strValue2], [intBackColor], [strFontStyle], [intFontColor], [strApplyTo], [intPanelId], [intUserId], [intSort], [strType], [ysnVisible], [intCannedPanelId])
-	SELECT [strColumn], [strCondition], [strValue1], [strValue2], [intBackColor], [strFontStyle], [intFontColor], [strApplyTo], @intCurrentPanelId, [intUserId], [intSort], [strType], [ysnVisible], [intCannedPanelId]
+		([strColumn], [strCondition], [strValue1], [strValue2], [intBackColor], [strFontStyle], [intFontColor], [strApplyTo], [intPanelId], [intUserId], [intSort], [strType], [ysnVisible], [intConcurrencyId], [intCannedPanelId])
+	SELECT [strColumn], [strCondition], [strValue1], [strValue2], [intBackColor], [strFontStyle], [intFontColor], [strApplyTo], @intCurrentPanelId, [intUserId], [intSort], [strType], [ysnVisible], [intConcurrencyId], [intCannedPanelId]
 	FROM #TampCannedPanelFormat 
 	WHERE intPanelFormatId = @intPanelFormatId
 
