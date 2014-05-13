ed5fa0eabcd9cfb058b70f28c656971778f0a2d5
kenneth lew lagunero <kenneth.lagunero@irely.com>
2014-05-13 14:34:12 +0800

    TM-580

--- i21Database/dbo/Tables/tblTMRegulatorType.sql
+++ i21Database/dbo/Tables/tblTMRegulatorType.sql
@@ -2,6 +2,7 @@
     [intConcurrencyId]   INT           DEFAULT 1 NOT NULL,
     [intRegulatorTypeId] INT           IDENTITY (1, 1) NOT NULL,
     [strRegulatorType]   NVARCHAR (50) COLLATE Latin1_General_CI_AS DEFAULT ('') NOT NULL,
+    [ysnDefault] BIT NOT NULL DEFAULT 0, 
     CONSTRAINT [PK_tblTMRegulatorType] PRIMARY KEY CLUSTERED ([intRegulatorTypeId] ASC)
 );
 
