f72ab62615dcfa0d8676089566df008ae0d631f6
Smith Wilson de Jesus <smith.dejesus@irely.com>
2014-06-04 16:25:53 +0800

    AR-236

--- i21Database/dbo/Tables/tblARCustomerPortalMenu.sql
+++ i21Database/dbo/Tables/tblARCustomerPortalMenu.sql
@@ -3,6 +3,7 @@
     [strCustomerPortalMenuName]     NVARCHAR (50) COLLATE Latin1_General_CI_AS NULL,
     [intCustomerPortalParentMenuId] INT           NOT NULL,
     [strType]                       NVARCHAR (10) COLLATE Latin1_General_CI_AS NULL,
+    [strCommand] NVARCHAR(250) COLLATE Latin1_General_CI_AS NULL, 
     CONSTRAINT [PK_tblARCustomerPortalMenu] PRIMARY KEY CLUSTERED ([intCustomerPortalMenuId] ASC)
 );
 
