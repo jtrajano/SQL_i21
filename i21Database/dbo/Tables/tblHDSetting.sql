b29e43c221e251b74cecc80fdb282939aefa7c01
Jayson Ramos <jayson.ramos@iRely.com>
2014-06-06 14:15:21 +0800

    HD-83;

--- i21Database/dbo/Tables/tblHDSetting.sql
+++ i21Database/dbo/Tables/tblHDSetting.sql
@@ -3,6 +3,7 @@
 	[intSettingId] [int] IDENTITY(1,1) NOT NULL,
 	[strHelpDeskName] [nvarchar](max) COLLATE Latin1_General_CI_AS NOT NULL,
 	[strHelpDeskURL] [nvarchar](max) COLLATE Latin1_General_CI_AS NOT NULL,
+	[strJIRAURL] [nvarchar](max) COLLATE Latin1_General_CI_AS NULL,
 	[strTimeZone] [nvarchar](100) COLLATE Latin1_General_CI_AS NULL,
 	[intConcurrencyId] [int] NOT NULL DEFAULT 1,
  CONSTRAINT [PK_tblHDSettings] PRIMARY KEY CLUSTERED 
