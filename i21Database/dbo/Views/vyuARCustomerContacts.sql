f3c71ef8da83a0c0f450ef7979b3435d5fd018c7
Smith Wilson de Jesus <smith.dejesus@irely.com>
2014-06-06 19:26:21 +0800

    Added view for AR use to get customer contacts

---
+++ i21Database/dbo/Views/vyuARCustomerContacts.sql
@@ -0,0 +1,17 @@
+﻿CREATE VIEW [dbo].[vyuARCustomerContacts]
+AS
+SELECT 
+B2.intEntityId, 
+B2.strName, 
+B2.strEmail, 
+E.strLocationName, 
+D.strPhone, 
+D.strTimezone, 
+D.strTitle, 
+D.ysnPortalAccess
+FROM dbo.tblARCustomer AS A 
+INNER JOIN dbo.tblEntity AS B ON A.intEntityId = B.intEntityId 
+INNER JOIN dbo.tblEntityToContact AS C ON B.intEntityId = C.intEntityId 
+INNER JOIN dbo.tblEntity AS B2 ON C.intContactId = B2.intEntityId 
+INNER JOIN dbo.tblEntityContact AS D ON B2.intEntityId = D.intEntityId 
+LEFT OUTER JOIN dbo.tblEntityLocation AS E ON C.intLocationId = E.intEntityLocationId
