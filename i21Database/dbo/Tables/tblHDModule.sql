47518fa456e1afba426eafa8f47f40fbf4ab9047
Jayson Ramos <jayson.ramos@iRely.com>
2014-06-06 18:01:13 +0800

    Add strJIRAProject column to tblHDModule.

--- i21Database/dbo/Tables/tblHDModule.sql
+++ i21Database/dbo/Tables/tblHDModule.sql
@@ -5,6 +5,7 @@
 	[intTicketGroupId] [int] NOT NULL,
 	[strModule] [nvarchar](100) COLLATE Latin1_General_CI_AS NOT NULL,
 	[strDescription] [nvarchar](150) COLLATE Latin1_General_CI_AS NOT NULL,
+	[strJIRAProject] [nvarchar](150) COLLATE Latin1_General_CI_AS NULL,
 	[intSort] [int] NULL,
 	[intConcurrencyId] [int] NOT NULL DEFAULT 1,
 	CONSTRAINT [PK_tblHDModule] PRIMARY KEY CLUSTERED ([intModuleId] ASC),
