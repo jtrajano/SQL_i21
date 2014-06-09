d35b8d975d3b1360037aa3fe15ce4fa4a380b6ec
Jayson Ramos <jayson.ramos@iRely.com>
2014-06-09 14:18:11 +0800

    HD-85;

--- i21Database/dbo/Tables/tblHDVersion.sql
+++ i21Database/dbo/Tables/tblHDVersion.sql
@@ -2,12 +2,13 @@
 (
 	[intVersionId] [int] IDENTITY(1,1) NOT NULL,
 	[intTicketProductId] [int] NOT NULL,
-	[strVersionNo] [nvarchar](max) COLLATE Latin1_General_CI_AS NOT NULL,
-	[strDescription] [nvarchar](max) COLLATE Latin1_General_CI_AS NOT NULL,
+	[strVersionNo] [nvarchar](100) COLLATE Latin1_General_CI_AS NOT NULL,
+	[strDescription] [nvarchar](150) COLLATE Latin1_General_CI_AS NOT NULL,
 	[dtmReleaseDate] [date] NULL,
 	[ysnSupported] [bit] NULL,
 	[dtmEOLDate] [date] NULL,
 	[intConcurrencyId] [int] NOT NULL,
 	CONSTRAINT [PK_tblHDVersion] PRIMARY KEY CLUSTERED ([intVersionId] ASC),
+	CONSTRAINT [UNQ_tblHDVersion] UNIQUE ([intTicketProductId],[strVersionNo]),
     CONSTRAINT [FK_Version_TicketProduct] FOREIGN KEY ([intTicketProductId]) REFERENCES [dbo].[tblHDTicketProduct] ([intTicketProductId]) on delete cascade
 )
