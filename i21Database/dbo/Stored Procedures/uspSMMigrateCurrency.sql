fbed7812782edd97593127e5963bb7f7e093b3d6
Lawrence Zabala <christopher_z@comtechsolutions.com>
2014-06-10 16:30:26 +0800

    SM-841

---
+++ i21Database/dbo/Stored Procedures/uspSMMigrateCurrency.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[uspSMMigrateCurrency]
+
+AS
+
+SET QUOTED_IdENTIFIER OFF
+SET ANSI_NULLS ON
+SET NOCOUNT ON
+SET XACT_ABORT ON
+SET ANSI_WARNINGS OFF
+
+BEGIN
+	INSERT INTO tblSMCurrency(strCurrency, strDescription, dblDailyRate, dblMinRate, dblMaxRate)
+	SELECT sscur_key,
+	sscur_desc,
+	sscur_daily_rt,
+	sscur_min_rt,
+	sscur_max_rt
+	FROM sscurmst
+	WHERE RTRIM(sscur_key)COLLATE DATABASE_DEFAULT  NOT IN (SELECT strCurrency COLLATE DATABASE_DEFAULT  FROM tblSMCurrency)
+END
