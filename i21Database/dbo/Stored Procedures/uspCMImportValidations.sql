47b83865973d1f065cb161c6cb8480b7ee146d50
Feb Montefrio <feb.montefrio@irely.com>
2014-06-12 14:12:31 -0400

    Resolve CM-299 (for 1420)

---
+++ i21Database/dbo/Stored Procedures/uspCMImportValidations.sql
@@ -0,0 +1,48 @@
+﻿/*
+ '====================================================================================================================================='
+   SCRIPT CREATED BY: Feb Montefrio	DATE CREATED: June 12, 2014
+  -------------------------------------------------------------------------------------------------------------------------------------						
+   Script Name         :  Cash Management - Import Validation 
+   
+   Description		   :  The purpose of this script to validate data before import takes place. It will allow the user to perform the 
+						  corrections to avoid bad data imports. 
+
+*/
+
+
+CREATE PROCEDURE [dbo].[uspCMImportValidations]
+	@Invalid_UserId_Found AS BIT OUTPUT
+	,@Invalid_GL_Account_Id_Found AS BIT OUTPUT
+	,@Invalid_Currency_Id_Found AS BIT OUTPUT
+AS
+
+DECLARE @CASH_ACCOUNT AS NVARCHAR(20) = 'Cash Accounts'
+DECLARE @ASSET AS NVARCHAR(20) = 'Asset'
+	
+-- Check for invalid user id's (WARN)
+SELECT	TOP 1 
+		@Invalid_UserId_Found = 1  
+FROM	apcbkmst 
+WHERE	dbo.fnConvertOriginUserIdtoi21(apcbk_user_id) IS NULL
+
+-- Check for invalid GL accounts. It must be moved under the Cash Account Group before import can be done. (ERR)
+SELECT	TOP 1 
+		@Invalid_GL_Account_Id_Found = 1  
+FROM	apcbkmst o INNER JOIN tblGLAccount accnt
+			ON dbo.fnGetGLAccountIdFromOriginToi21(o.apcbk_gl_cash) = accnt.intAccountId
+		INNER JOIN tblGLAccountGroup grp
+			ON accnt.intAccountGroupId = grp.intAccountGroupId
+WHERE	grp.strAccountGroup <> @CASH_ACCOUNT
+		OR grp.strAccountType <> @ASSET
+
+-- Check for invalid currency id's. It must be defined first before import can be done (ERR). 
+SELECT	TOP 1 
+		@Invalid_Currency_Id_Found = 1  
+FROM	apcbkmst
+WHERE	dbo.fnGetCurrencyIdFromOriginToi21(apcbk_currency) IS NULL
+
+SELECT	@Invalid_UserId_Found = ISNULL(@Invalid_UserId_Found, 0)
+		,@Invalid_GL_Account_Id_Found = ISNULL(@Invalid_GL_Account_Id_Found, 0)
+		,@Invalid_Currency_Id_Found = ISNULL(@Invalid_Currency_Id_Found,0)
+
+GO
