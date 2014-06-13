f6ec92be141474b8320d37559e3724c3d4ed1d17
Jayson Ramos <jayson.ramos@iRely.com>
2014-06-13 18:59:57 +0800

    HD-88;

--- i21Database/dbo/Tables/tblHDGroupUserConfig.sql
+++ i21Database/dbo/Tables/tblHDGroupUserConfig.sql
@@ -5,5 +5,6 @@
 	[intUserSecurityId] [int] NOT NULL,
 	[intConcurrencyId] [int] NOT NULL DEFAULT 1,
 	PRIMARY KEY CLUSTERED (	[intGroupUserConfigId] ASC),
+	CONSTRAINT [UNQ_tblHDGroupUserConfig] UNIQUE ([intTicketGroupId],[intUserSecurityId]),
     CONSTRAINT [FK_GroupUserConfig_TicketGroup] FOREIGN KEY ([intTicketGroupId]) REFERENCES [dbo].[tblHDTicketGroup] ([intTicketGroupId]) on delete cascade
 )
