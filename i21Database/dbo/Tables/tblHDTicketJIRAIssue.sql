f10ab33774704a5453f0d9bb426c588f5305e5ea
Jayson Ramos <jayson.ramos@iRely.com>
2014-06-16 19:40:47 +0800

    Add Help Desk [tblHDTicketJIRAIssue] table for JIRA issues.

---
+++ i21Database/dbo/Tables/tblHDTicketJIRAIssue.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[tblHDTicketJIRAIssue]
+(
+	[intTicketJIRAIssueId] [int] IDENTITY(1,1) NOT NULL,
+	[intTicketId] [int] NOT NULL,
+	[strKey] [nvarchar](10) COLLATE Latin1_General_CI_AS NOT NULL,
+	[intConcurrencyId] [int] NOT NULL DEFAULT 1,
+	CONSTRAINT [PK_tblHDTicketJIRAIssue] PRIMARY KEY CLUSTERED ([intTicketJIRAIssueId] ASC),
+    CONSTRAINT [FK_Ticket_icketJIRAIssue] FOREIGN KEY ([intTicketId]) REFERENCES [dbo].[tblHDTicket] ([intTicketId])
+)
