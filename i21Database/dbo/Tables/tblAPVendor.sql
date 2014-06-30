77e2ade626d8721b76582130288c9e9145933bea
Erick Dela Cruz <erick.delacruz@irely.com>
2014-06-30 14:32:49 +0800

    AP-121

--- i21Database/dbo/Tables/tblAPVendor.sql
+++ i21Database/dbo/Tables/tblAPVendor.sql
@@ -23,6 +23,8 @@
     [strTaxState]               NVARCHAR (50)  NULL,
     [intConcurrencyId] INT NOT NULL DEFAULT 0, 
     [strTaxNumber] NVARCHAR(20) NULL, 
+    [intBillToId] INT NULL, 
+    [intShipFromId] INT NULL, 
     CONSTRAINT [PK_dbo.tblAPVendor] PRIMARY KEY CLUSTERED ([intEntityId] ASC),
     CONSTRAINT [FK_dbo.tblAPVendor_dbo.tblEntities_intEntityId] FOREIGN KEY ([intEntityId]) REFERENCES [dbo].[tblEntity] ([intEntityId]),
     CONSTRAINT [APstrVendorId_Unique] UNIQUE NONCLUSTERED ([strVendorId] ASC)
