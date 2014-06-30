e2ee2ed15146aac65e2f97671fb6c589b89c062b
von.pasion <von.pasion@irely.com>
2014-06-30 23:58:40 +0800

    COM-211

---
+++ i21Database/dbo/Tables/tblSMSearch.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[tblSMSearch] (
+    [intSearchId]      INT           IDENTITY (1, 1) NOT NULL,
+    [strScreen]        NVARCHAR (50) COLLATE Latin1_General_CI_AS NULL,
+    [strModule]        NVARCHAR (50) COLLATE Latin1_General_CI_AS NULL,
+    [intEntityId]      INT           NOT NULL,
+    [intConcurrencyId] INT           NOT NULL,
+    CONSTRAINT [PK_tblSMSearch] PRIMARY KEY CLUSTERED ([intSearchId] ASC),
+    CONSTRAINT [FK_tblSMSearch_tblEntity] FOREIGN KEY ([intEntityId]) REFERENCES [dbo].[tblEntity] ([intEntityId]) ON DELETE CASCADE
+);
+
+
+
---
+++ i21Database/dbo/Tables/tblSMSearchFilter.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[tblSMSearchFilter] (
+    [intSearchFilterId] INT            IDENTITY (1, 1) NOT NULL,
+    [intSearchFieldId]  INT            NOT NULL,
+    [strValue]          NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NULL,
+    [strCondition]      NVARCHAR (50)  COLLATE Latin1_General_CI_AS NULL,
+    [strConjunction]    NVARCHAR (50)  COLLATE Latin1_General_CI_AS NULL,
+    [intConcurrencyId]  INT            NOT NULL,
+    CONSTRAINT [PK_tblSMSearchFilter] PRIMARY KEY CLUSTERED ([intSearchFilterId] ASC),
+    CONSTRAINT [FK_tblSMSearchFilter_tblSMSearchField] FOREIGN KEY ([intSearchFieldId]) REFERENCES [dbo].[tblSMSearchField] ([intSearchFieldId]) ON DELETE CASCADE
+);
+
+
+
