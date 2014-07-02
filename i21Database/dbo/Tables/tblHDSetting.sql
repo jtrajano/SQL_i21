29c52082ee0c4eef1d7a8e10df004530551c768d
Jayson Ramos <jayson.ramos@iRely.com>
2014-07-02 15:17:59 +0800

    Limit the tblHDSetting fields strHelpDeskName = 100; strHelpDeskURL = 255; strJIRAURL =255;

--- i21Database/dbo/Tables/tblHDSetting.sql
+++ i21Database/dbo/Tables/tblHDSetting.sql
@@ -1,9 +1,9 @@
 ﻿CREATE TABLE [dbo].[tblHDSetting]
 (
 	[intSettingId] [int] IDENTITY(1,1) NOT NULL,
-	[strHelpDeskName] [nvarchar](max) COLLATE Latin1_General_CI_AS NOT NULL,
-	[strHelpDeskURL] [nvarchar](max) COLLATE Latin1_General_CI_AS NOT NULL,
-	[strJIRAURL] [nvarchar](max) COLLATE Latin1_General_CI_AS NULL,
+	[strHelpDeskName] [nvarchar](100) COLLATE Latin1_General_CI_AS NOT NULL,
+	[strHelpDeskURL] [nvarchar](255) COLLATE Latin1_General_CI_AS NOT NULL,
+	[strJIRAURL] [nvarchar](255) COLLATE Latin1_General_CI_AS NULL,
 	[strTimeZone] [nvarchar](100) COLLATE Latin1_General_CI_AS NULL,
 	[intConcurrencyId] [int] NOT NULL DEFAULT 1,
  CONSTRAINT [PK_tblHDSettings] PRIMARY KEY CLUSTERED 
