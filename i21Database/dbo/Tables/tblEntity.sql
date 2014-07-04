452873893aba37f400f48d8790a05512862d273f
Smith Wilson de Jesus <smith.dejesus@irely.com>
2014-07-04 16:04:28 +0800

    AR-85, AR-86

--- i21Database/dbo/Tables/tblEntity.sql
+++ i21Database/dbo/Tables/tblEntity.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[tblEntity] (
     [intEntityId]      INT            IDENTITY (1, 1) NOT NULL,
     [strName]          NVARCHAR (100) COLLATE Latin1_General_CI_AS NOT NULL,
-	[strEmail]         NVARCHAR (75) COLLATE Latin1_General_CI_AS NOT NULL DEFAULT (''),
+    [strEmail]         NVARCHAR (75)  COLLATE Latin1_General_CI_AS DEFAULT ('') NOT NULL,
     [strWebsite]       NVARCHAR (100) COLLATE Latin1_General_CI_AS NULL,
     [strInternalNotes] NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NULL,
     [ysnPrint1099]     BIT            NULL,
@@ -11,9 +11,10 @@
     [strFederalTaxId]  NVARCHAR (50)  COLLATE Latin1_General_CI_AS NULL,
     [dtmW9Signed]      DATETIME       NULL,
     [intConcurrencyId] INT            DEFAULT ((0)) NOT NULL,
-    CONSTRAINT [PK_dbo.tblEntity] PRIMARY KEY CLUSTERED ([intEntityId] ASC),
-    CONSTRAINT [FK_tblEntity_tblEntity] FOREIGN KEY ([intEntityId]) REFERENCES [dbo].[tblEntity] ([intEntityId])
+    CONSTRAINT [PK_dbo.tblEntity] PRIMARY KEY CLUSTERED ([intEntityId] ASC)
 );
+
+
 
 
 
