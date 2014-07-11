b6db99ce03b8e2f8784c7ff1673da5c07511101d
Smith Wilson de Jesus <smith.dejesus@irely.com>
2014-07-11 09:38:20 +0800

    Fix collation issue on tblEntityCredential

--- i21Database/dbo/Tables/tblEntityCredential.sql
+++ i21Database/dbo/Tables/tblEntityCredential.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [dbo].[tblEntityCredential] (
     [intEntityCredentialId] INT            IDENTITY (1, 1) NOT NULL,
     [intEntityId]           INT            NOT NULL,
-    [strUserName]           NVARCHAR (100) NOT NULL,
-    [strPassword]           NVARCHAR (100) NOT NULL,
+    [strUserName]           NVARCHAR (100) COLLATE Latin1_General_CI_AS NOT NULL,
+    [strPassword]           NVARCHAR (100) COLLATE Latin1_General_CI_AS NOT NULL,
     [intConcurrencyId]      INT            DEFAULT ((1)) NOT NULL,
     CONSTRAINT [PK_tblEntityCredential] PRIMARY KEY CLUSTERED ([intEntityCredentialId] ASC),
     CONSTRAINT [FK_tblEntityCredential_tblEntity] FOREIGN KEY ([intEntityId]) REFERENCES [dbo].[tblEntity] ([intEntityId]),
