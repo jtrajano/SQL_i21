446ba04f7bdf365030aef65a9ceb054a193cfeb0
kenneth lew lagunero <kenneth.lagunero@irely.com>
2014-07-15 16:46:35 +0800

    TM-32

--- i21Database/dbo/Tables/tblTMInventoryStatusType.sql
+++ i21Database/dbo/Tables/tblTMInventoryStatusType.sql
@@ -1,8 +1,11 @@
 ﻿CREATE TABLE [dbo].[tblTMInventoryStatusType] (
-    [intConcurrencyId]         INT           DEFAULT 1 NOT NULL,
+    [intConcurrencyId]         INT           DEFAULT ((1)) NOT NULL,
     [intInventoryStatusTypeId] INT           IDENTITY (1, 1) NOT NULL,
     [strInventoryStatusType]   NVARCHAR (70) COLLATE Latin1_General_CI_AS DEFAULT ('') NOT NULL,
-    [ysnDefault]               BIT           DEFAULT 0 NOT NULL,
-    CONSTRAINT [PK_tblTMInventoryStatusType] PRIMARY KEY CLUSTERED ([intInventoryStatusTypeId] ASC)
+    [ysnDefault]               BIT           DEFAULT ((0)) NOT NULL,
+    CONSTRAINT [PK_tblTMInventoryStatusType] PRIMARY KEY CLUSTERED ([intInventoryStatusTypeId] ASC),
+    CONSTRAINT [IX_tblTMInventoryStatusType] UNIQUE NONCLUSTERED ([strInventoryStatusType] ASC)
 );
+
+
 
--- i21Database/dbo/Tables/tblTMMeterType.sql
+++ i21Database/dbo/Tables/tblTMMeterType.sql
@@ -1,9 +1,12 @@
 ﻿CREATE TABLE [dbo].[tblTMMeterType] (
-    [intConcurrencyId]    INT             DEFAULT 1 NOT NULL,
+    [intConcurrencyId]    INT             DEFAULT ((1)) NOT NULL,
     [intMeterTypeId]      INT             IDENTITY (1, 1) NOT NULL,
     [strMeterType]        NVARCHAR (50)   COLLATE Latin1_General_CI_AS DEFAULT ('') NOT NULL,
-    [dblConversionFactor] NUMERIC (18, 8) DEFAULT 0 NULL,
-    [ysnDefault]          BIT             DEFAULT 0 NULL,
-    CONSTRAINT [PK_tblTMMeterType] PRIMARY KEY CLUSTERED ([intMeterTypeId] ASC)
+    [dblConversionFactor] NUMERIC (18, 8) DEFAULT ((0)) NULL,
+    [ysnDefault]          BIT             DEFAULT ((0)) NULL,
+    CONSTRAINT [PK_tblTMMeterType] PRIMARY KEY CLUSTERED ([intMeterTypeId] ASC),
+    CONSTRAINT [IX_tblTMMeterType] UNIQUE NONCLUSTERED ([strMeterType] ASC)
 );
+
+
 
