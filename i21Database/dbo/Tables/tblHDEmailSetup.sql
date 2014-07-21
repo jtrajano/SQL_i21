062f4651702c39ba2a7b9a4909328aef493938a8
Jayson Ramos <jayson.ramos@iRely.com>
2014-07-21 19:11:43 +0800

    HD-113;

--- i21Database/dbo/Tables/tblHDEmailSetup.sql
+++ i21Database/dbo/Tables/tblHDEmailSetup.sql
@@ -1,15 +1,15 @@
 ﻿CREATE TABLE [dbo].[tblHDEmailSetup]
 (
 	[intEmailSetupId] [int] IDENTITY(1,1) NOT NULL,
-	[strFromEmail] [nvarchar](max) COLLATE Latin1_General_CI_AS NOT NULL,
-	[strFromName] [nvarchar](max) COLLATE Latin1_General_CI_AS NOT NULL,
-	[strSubjectPrefix] [nvarchar](max) COLLATE Latin1_General_CI_AS NULL,
-	[strSMTPServer] [nvarchar](max) COLLATE Latin1_General_CI_AS NULL,
+	[strFromEmail] [nvarchar](50) COLLATE Latin1_General_CI_AS NOT NULL,
+	[strFromName] [nvarchar](100) COLLATE Latin1_General_CI_AS NOT NULL,
+	[strSubjectPrefix] [nvarchar](255) COLLATE Latin1_General_CI_AS NULL,
+	[strSMTPServer] [nvarchar](150) COLLATE Latin1_General_CI_AS NULL,
 	[intSMTPPort] [int] NULL,
-	[strEncryptedConnection] [nvarchar](max) COLLATE Latin1_General_CI_AS NULL,
+	[strEncryptedConnection] [nvarchar](50) COLLATE Latin1_General_CI_AS NULL,
 	[ysnRequiresAuthentication] [bit] NULL,
-	[strUserName] [nvarchar](max) COLLATE Latin1_General_CI_AS NULL,
-	[strPassword] [nvarchar](max) COLLATE Latin1_General_CI_AS NULL,
+	[strUserName] [nvarchar](50) COLLATE Latin1_General_CI_AS NULL,
+	[strPassword] [nvarchar](50) COLLATE Latin1_General_CI_AS NULL,
 	[intConcurrencyId] [int] NOT NULL,
  CONSTRAINT [PK_tblHDEmailSetup] PRIMARY KEY CLUSTERED 
 (
