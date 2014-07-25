89d2710200096ca823b34703e3c6e47c002b0643
Lawrence Zabala <christopher_z@comtechsolutions.com>
2014-07-25 16:39:23 +0800

    SM-1077

--- i21Database/dbo/Views/vyuCPContactMenu.sql
+++ i21Database/dbo/Views/vyuCPContactMenu.sql
@@ -1,22 +1,28 @@
-﻿CREATE VIEW [dbo].[vyuCPContactMenu]
-AS
-SELECT
-intUserSecurityMenuId = Permission.intCustomerPortalMenuId,
-intUserSecurityId = Permission.intContactId,
-intMenuId = Menu.intCustomerPortalMenuId,
-intParentMenuId = Menu.intCustomerPortalParentMenuId,
-strMenuName = Menu.strCustomerPortalMenuName,
-strModuleName = 'Customer Portal',
-strIcon = (CASE WHEN Menu.strType = 'Folder' THEN 'small-folder' ELSE 'small-screen' END),
-strDescription = Menu.strCustomerPortalMenuName,
-strType = Menu.strType,
-strCommand = Menu.strCommand,
-iconCls = Menu.strCustomerPortalMenuName,
-ysnLeaf = (CASE WHEN Menu.strType = 'Folder' THEN CAST(0 AS BIT) ELSE CAST(1 AS BIT) END),
-leaf = (CASE WHEN Menu.strType = 'Folder' THEN CAST(0 AS BIT) ELSE CAST(1 AS BIT) END),
-expanded = CAST(0 AS BIT),
-ysnVisible = (CASE WHEN ISNULL(Permission.intCustomerPortalMenuId, 0) = 0 THEN CAST(0 AS BIT) ELSE CAST(1 AS BIT) END),
-ysnIsLegacy = CAST(0 AS BIT),
-intSort = Menu.intCustomerPortalMenuId
-FROM tblARCustomerPortalMenu Menu
+﻿CREATE VIEW [dbo].[vyuCPContactMenu]  
+AS  
+SELECT  
+intUserSecurityMenuId = Permission.intCustomerPortalPermissionId,  
+intUserSecurityId = Permission.intContactId,  
+intMenuId = Menu.intCustomerPortalMenuId,  
+intParentMenuId = (CASE WHEN Menu.intCustomerPortalParentMenuId = 0 THEN 0 ELSE (
+					SELECT intCustomerPortalPermissionId
+					FROM tblARCustomerPortalPermission
+					WHERE tblARCustomerPortalPermission.intContactId = Permission.intContactId
+					
+					AND	tblARCustomerPortalPermission.intCustomerPortalMenuId = Menu.intCustomerPortalParentMenuId
+					) END),
+strMenuName = Menu.strCustomerPortalMenuName,  
+strModuleName = 'Customer Portal',  
+strIcon = (CASE WHEN Menu.strType = 'Folder' THEN 'small-folder' ELSE 'small-screen' END),  
+strDescription = Menu.strCustomerPortalMenuName,  
+strType = Menu.strType,  
+strCommand = Menu.strCommand,  
+iconCls = Menu.strCustomerPortalMenuName,  
+ysnLeaf = (CASE WHEN Menu.strType = 'Folder' THEN CAST(0 AS BIT) ELSE CAST(1 AS BIT) END),  
+leaf = (CASE WHEN Menu.strType = 'Folder' THEN CAST(0 AS BIT) ELSE CAST(1 AS BIT) END),  
+expanded = CAST(0 AS BIT),  
+ysnVisible = (CASE WHEN ISNULL(Permission.intCustomerPortalMenuId, 0) = 0 THEN CAST(0 AS BIT) ELSE CAST(1 AS BIT) END),  
+ysnIsLegacy = CAST(0 AS BIT),  
+intSort = Menu.intCustomerPortalMenuId  
+FROM tblARCustomerPortalMenu Menu  
 LEFT JOIN tblARCustomerPortalPermission Permission ON Menu.intCustomerPortalMenuId = Permission.intCustomerPortalMenuId
