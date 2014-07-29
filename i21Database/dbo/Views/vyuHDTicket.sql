735e6ac65a575b65b3261afc1912b074889fadb5
Jayson Ramos <jayson.ramos@iRely.com>
2014-07-29 17:47:29 +0800

    HD-190 - Duplicate Ticket Nos are appearing on the Ticket Search Screen;

--- i21Database/dbo/Views/vyuHDTicket.sql
+++ i21Database/dbo/Views/vyuHDTicket.sql
@@ -1,5 +1,30 @@
 ﻿CREATE VIEW [dbo].[vyuHDTicket]
-	AS 
+	AS
+	select
+      tic.intTicketId
+      ,tic.strTicketNumber
+      ,tic.strSubject
+      ,typ.strType
+      ,sta.strStatus
+      ,pri.strPriority
+      ,pro.strProduct
+      ,mo.strModule
+      ,ver.strVersionNo
+      ,strCreateBy = (select top 1 strUserName from tblEntityCredential where intEntityId = tic.intCreatedUserEntityId)
+      ,tic.dtmCreated
+      ,tic.dtmLastModified
+      ,strCustomer = tic.strCustomerNumber
+      ,strAssignedTo = (select top 1 strUserName from tblEntityCredential where intEntityId = tic.intAssignedToEntity)
+      ,tic.intConcurrencyId
+  from
+      tblHDTicket tic
+      left outer join tblHDTicketType typ on typ.intTicketTypeId = tic.intTicketTypeId
+      left outer join tblHDTicketStatus sta on sta.intTicketStatusId = tic.intTicketStatusId
+      left outer join tblHDTicketPriority pri on pri.intTicketPriorityId = tic.intTicketPriorityId
+      left outer join tblHDTicketProduct pro on pro.intTicketProductId = tic.intTicketProductId
+      left outer join tblHDModule mo on mo.intModuleId = tic.intModuleId
+      left outer join tblHDVersion ver on ver.intVersionId = tic.intVersionId 
+	/*
 	 select
       tic.intTicketId
       ,tic.strTicketNumber
@@ -26,3 +51,4 @@
       left outer join tblHDVersion ver on ver.intVersionId = tic.intVersionId
       left outer join vyuHDUserDetail cre on cre.intEntityId = tic.intCreatedUserEntityId
       left outer join vyuHDUserDetail assi on assi.intEntityId = tic.intAssignedToEntity
+	  */
