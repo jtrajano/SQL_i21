e1020863a4702209cca7088530b447222fa842b2
Erick Dela Cruz <erick.delacruz@irely.com>
2014-08-05 03:33:19 +0800

    AP-360

--- i21Database/dbo/Stored Procedures/uspAPPostPayment.sql
+++ i21Database/dbo/Stored Procedures/uspAPPostPayment.sql
@@ -564,7 +564,12 @@ BEGIN
 		--Unposting Process
 		UPDATE tblAPPayment
 			SET ysnPosted = 0
-		FROM tblAPPayment WHERE intPaymentId IN (SELECT intPaymentId FROM #tmpPayablePostData)
+			,ysnVoid = CASE WHEN A.ysnPrinted = 1 AND ISNULL(A.strPaymentInfo,'') <> '' THEN 1 ELSE 0 END
+			,strPaymentRecordNum = CASE WHEN A.ysnPrinted = 1 AND ISNULL(A.strPaymentInfo,'') <> '' THEN B.strReferenceNo ELSE A.strPaymentInfo END
+		FROM tblAPPayment A 
+			INNER JOIN tblCMBankTransaction B
+				ON A.strPaymentRecordNum = B.strTransactionId
+		WHERE intPaymentId IN (SELECT intPaymentId FROM #tmpPayablePostData)
 
 		UPDATE tblAPPaymentDetail
 			SET tblAPPaymentDetail.dblAmountDue = (CASE WHEN B.dblAmountDue = 0 THEN B.dblDiscount + B.dblPayment ELSE B.dblPayment END)
