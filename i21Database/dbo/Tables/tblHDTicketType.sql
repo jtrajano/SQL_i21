90935f9a5dcbb313902b9f9b7c87218a6d32082f
Jayson Ramos <jayson.ramos@iRely.com>
2014-08-06 10:36:08 +0800

    HD-188;

--- i21Database/dbo/Tables/tblHDTicketType.sql
+++ i21Database/dbo/Tables/tblHDTicketType.sql
@@ -3,6 +3,7 @@
 	[intTicketTypeId] [int] IDENTITY(1,1) NOT NULL,
 	[strType] [nvarchar](100) COLLATE Latin1_General_CI_AS NOT NULL,
 	[strDescription] [nvarchar](150) COLLATE Latin1_General_CI_AS NOT NULL,
+	[strJIRAType] [nvarchar](255) COLLATE Latin1_General_CI_AS NULL,
 	[strIcon] [nvarchar](50) COLLATE Latin1_General_CI_AS NULL,
 	[intSort] [int] NULL,
 	[intConcurrencyId] [int] NOT NULL,
