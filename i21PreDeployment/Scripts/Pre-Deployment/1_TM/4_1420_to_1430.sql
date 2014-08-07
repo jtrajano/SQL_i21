8ca247f4aba0b63aae7d1fe024f2fc913ee90278
kenneth lagunero <kenneth.lagunero@irely.com>
2014-08-07 16:20:14 +0800

    TM-829 Changes on table schema and default values and update script on predeployment

---
+++ i21PreDeployment/Scripts/Pre-Deployment/1_TM/4_1420_to_1430.sql
@@ -0,0 +1,24 @@
+﻿
+PRINT N'BEGIN Update of data in tblSMSite'
+GO
+
+IF EXISTS (SELECT TOP 1 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[tblTMSite]') AND type in (N'U')) 
+BEGIN
+	IF EXISTS (SELECT TOP 1 1 FROM sys.columns WHERE NAME  = N'dblWinterDailyUse' AND OBJECT_ID = OBJECT_ID(N'tblTMSite')) 
+    BEGIN
+        UPDATE tblTMSite
+		SET dblWinterDailyUse = 0.0
+		WHERE dblWinterDailyUse IS NULL
+    END
+
+	IF EXISTS (SELECT TOP 1 1 FROM sys.columns WHERE NAME  = N'dblSummerDailyUse' AND OBJECT_ID = OBJECT_ID(N'tblTMSite')) 
+    BEGIN
+        UPDATE tblTMSite
+		SET dblSummerDailyUse = 0.0
+		WHERE dblSummerDailyUse IS NULL
+    END
+END
+GO
+PRINT N'END Update of data in tblSMSite'
+GO
+
