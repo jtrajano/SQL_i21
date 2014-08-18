80c82b28b468fe72789a3330d81c4ee68f7a1ef3
Smith Wilson de Jesus <smith.dejesus@irely.com>
2014-08-18 18:29:52 +0800

    Added vyuARCustomer

---
+++ i21Database/dbo/Views/vyuARCustomer.sql
@@ -0,0 +1,16 @@
+﻿CREATE VIEW [dbo].[vyuARCustomer]
+AS
+SELECT     
+ Entity.intEntityId
+,Entity.strName
+,Cus.strCustomerNumber
+,Con.strPhone
+,Loc.strAddress
+,Loc.strCity
+,Loc.strState
+,Loc.strZipCode 
+FROM tblEntity as Entity
+INNER JOIN tblARCustomer as Cus ON Entity.intEntityId = Cus.intEntityId
+INNER JOIN tblARCustomerToContact as CusToCon ON Cus.intDefaultContactId = CusToCon.intARCustomerToContactId
+INNER JOIN tblEntityContact as Con ON CusToCon.intContactId = Con.intContactId
+INNER JOIN tblEntityLocation as Loc ON Cus.intDefaultLocationId = Loc.intEntityLocationId
