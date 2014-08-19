550bb39214cdf80d68f2d3cab4896121fad5e430
Jayson Ramos <jayson.ramos@iRely.com>
2014-08-19 15:18:51 +0800

    HD-226 - tickets reported by me on customer side is not showing all;

--- i21Database/dbo/Views/vyuHDTicket.sql
+++ i21Database/dbo/Views/vyuHDTicket.sql
@@ -17,6 +17,8 @@
       ,strAssignedTo = (select top 1 strUserName from tblEntityCredential where intEntityId = tic.intAssignedToEntity)
       ,tic.intConcurrencyId
 	  ,intAssignToEntity = tic.intAssignedToEntity
+	  ,strContactName = (select top 1 strName from tblEntity where intEntityId = tic.intCustomerContactId)
+	  ,tic.intCustomerContactId
   from
       tblHDTicket tic
       left outer join tblHDTicketType typ on typ.intTicketTypeId = tic.intTicketTypeId
