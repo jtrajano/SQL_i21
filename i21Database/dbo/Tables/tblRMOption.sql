06934396837e5cc32305413444dcf94f0889f148
Bonn Paolo Ponce <bonn.ponce@irely.com>
2014-08-22 14:54:00 +0800

    RPT-491 Click NO on loading report defaults will display blank Report Parameter

--- i21Database/dbo/Tables/tblRMOption.sql
+++ i21Database/dbo/Tables/tblRMOption.sql
@@ -1,20 +1,22 @@
 ﻿CREATE TABLE [dbo].[tblRMOption] (
-    [intOptionId]        INT            IDENTITY (1, 1) NOT NULL,
-    [strName]            NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NULL,
-    [strDescription]     NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NULL,
-    [intType]            INT            NOT NULL,
-    [strSettings]        NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NULL,
-    [ysnShow]            BIT            NOT NULL,
-    [intReportId]        INT            NOT NULL,
-    [ysnEnable]          BIT            NOT NULL,
-    [intSortId]          INT            NOT NULL,
-    [intUserId]          INT            NULL,
-    [ysnDefault]         BIT            NULL,
-    [intDefaultOptionId] INT            NULL,
-    [intConcurrencyId]   INT            DEFAULT ((1)) NOT NULL,
+    [intOptionId]            INT            IDENTITY (1, 1) NOT NULL,
+    [strName]                NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NULL,
+    [strDescription]         NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NULL,
+    [intType]                INT            NOT NULL,
+    [strSettings]            NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NULL,
+    [ysnShow]                BIT            NULL,
+    [intReportId]            INT            NOT NULL,
+    [ysnEnable]              BIT            NOT NULL,
+    [intSortId]              INT            NOT NULL,
+    [ysnDefault]             BIT            NULL,
+    [intUserId]              INT            NULL,
+    [intOptionConcurrencyId] INT            NULL,
+    [intConcurrencyId]       INT            CONSTRAINT [DF__tblRMOpti__intCo__703483B9] DEFAULT ((1)) NOT NULL,
     CONSTRAINT [PK_dbo.Options] PRIMARY KEY CLUSTERED ([intOptionId] ASC),
     CONSTRAINT [FK_tblRMOption_tblRMReport] FOREIGN KEY ([intReportId]) REFERENCES [dbo].[tblRMReport] ([intReportId]) ON DELETE CASCADE
 );
+
+
 
 
 
