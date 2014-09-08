d182882571c50b3c601cac51d8d6f7f71d1bad1b
Feb Montefrio <feb.montefrio@irely.com>
2014-09-08 14:02:18 +0800

    Resolve all warnings when compiling the SSDT project.

--- i21Database/dbo/Stored Procedures/uspGLBuildAccount.sql
+++ i21Database/dbo/Stored Procedures/uspGLBuildAccount.sql
@@ -3,7 +3,7 @@
    Stub stored procedure
   -------------------------------------------------------------------------------------------------------------------------------------						
 	A stub is created in case GL module is not enabled in origin. 
-
+	The real stored procedure is in the integration project. 
 */
 CREATE PROCEDURE  [dbo].[uspGLBuildAccount]
 	@intUserId nvarchar(50)
--- i21Database/dbo/Stored Procedures/uspSMMigrateCurrency.sql
+++ i21Database/dbo/Stored Procedures/uspSMMigrateCurrency.sql
@@ -3,7 +3,7 @@
    Stub stored procedure
   -------------------------------------------------------------------------------------------------------------------------------------						
 	A stub is created in case the origin table sscurmst is missing. 
-
+	The real stored procedure is in the integration project. 
 */
 CREATE PROCEDURE [dbo].[uspSMMigrateCurrency]
 AS
