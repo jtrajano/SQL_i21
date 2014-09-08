df6b1c6984acdf019db812f8e48916bf39542bc9
Feb Montefrio <feb.montefrio@irely.com>
2014-09-08 16:00:40 +0800

    Implement the tSQLt test project - for a stored procedure.

--- tSQLt_UnitTesting/testi21Database/Stored Procedures/test the uspCMAddDeposit stored procedure.sql
+++ tSQLt_UnitTesting/testi21Database/Stored Procedures/test the uspCMAddDeposit stored procedure.sql
@@ -2,7 +2,8 @@
 AS
 BEGIN
 	-- Prepare the fake table 
-	EXEC tSQLt.FakeTable 'dbo.tblCMBankTransaction';
+	EXEC tSQLt.FakeTable 'dbo.tblCMBankTransaction', @Identity = 1;
+	EXEC tSQLt.FakeTable 'dbo.tblCMBankTransactionDetail';
 
 	DECLARE @p1 AS INT
 			,@p2 AS DATETIME
@@ -12,13 +13,23 @@ BEGIN
 			,@p6 AS INT 
 			,@p7 AS BIT;
 
-	SET @p1 = 19
+	-- SET @p1 = 1
 	SET @p2 = '02/28/2012'
-	SET @p3 = 1099
+	-- SET @p3 = 1099
 	SET @p4 = 496.88
 	SET @p5 = 'this is the description'
 	SET @p6 = 4546
 	SET @p7 = 0
+
+	SELECT	TOP 1 
+			@p1 = intBankAccountId
+	FROM	tblCMBankAccount
+
+	SELECT TOP 1 
+			@p3 = tblGLAccount.intAccountId
+	FROM	tblGLAccount INNER JOIN tblGLAccountGroup  
+				ON tblGLAccount.intAccountGroupId = tblGLAccountGroup.intAccountGroupId
+	WHERE	tblGLAccountGroup.strAccountGroup = 'Expenses'
 
 	-- Act
 	EXEC dbo.uspCMAddDeposit 
@@ -43,7 +54,21 @@ BEGIN
 		[intCreatedUserId]         INT              NULL
 	)
 
-	INSERT INTO expected (intBankAccountId, dtmDate, dblAmount, strMemo, intCreatedUserId) SELECT 19, '02/28/2012', 496.88, 'this is the description', 4546
+	INSERT INTO expected (intBankAccountId, dtmDate, dblAmount, strMemo, intCreatedUserId) SELECT 1, '02/28/2012', 496.88, 'this is the description', 4546
 	 
 	EXEC tSQLt.AssertEqualsTable 'expected', 'actual';
+
+	SELECT	intGLAccountId, dblDebit, dblCredit
+	INTO	actualDetail
+	FROM	dbo.tblCMBankTransactionDetail
+
+	CREATE TABLE expectedDetail (
+		[intGLAccountId]         INT              NOT NULL,
+		[dblDebit]                DECIMAL (18, 6)  DEFAULT 0 NOT NULL,
+		[dblCredit]                DECIMAL (18, 6)  DEFAULT 0 NOT NULL
+	)
+
+	INSERT INTO expectedDetail (intGLAccountId, dblDebit, dblCredit) SELECT @p3, 0, 496.88
+
+	EXEC tSQLt.AssertEqualsTable 'expectedDetail', 'actualDetail';
 END 
