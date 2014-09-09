2432e3d1888fa723f1d95915d63ae507c9575fb4
Feb Montefrio <feb.montefrio@irely.com>
2014-09-09 17:17:00 +0800

    1. Add a function for getting the costing method of an item. 2. Add the table for tracking inventory transactions.

---
+++ i21Database/dbo/Functions/fnGetItemCostingMethod.sql
@@ -0,0 +1,13 @@
+﻿
+
+CREATE FUNCTION [dbo].[fnGetItemCostingMethod](
+	@intItemId int
+)
+RETURNS INT
+AS
+BEGIN 
+	DECLARE @intCostingMethodId AS INT 
+	SELECT @intCostingMethodId = intCostingMethodId FROM tblICItem
+
+	RETURN @intCostingMethodId
+END 
---
+++ i21Database/dbo/Tables/tblICInventoryTransaction.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE [dbo].[tblICInventoryTransaction]
+(
+	[intInventoryTransactionId] INT NOT NULL PRIMARY KEY IDENTITY, 
+	[intItemId] INT NOT NULL,
+	[intItemLocationStoreId] INT NOT NULL,
+	[dtmDate] DATE NOT NULL, 
+    [dblQuantity] NUMERIC(18, 6) NOT NULL DEFAULT 0, 
+	[dblUOMQty] NUMERIC(18,6) NOT NULL DEFAULT 1,
+    [dblCost] NUMERIC(18, 6) NOT NULL DEFAULT 0, 
+	[dblSalesPrice] NUMERIC(18, 6) NOT NULL DEFAULT 0, 
+	[dblExchangeRate] DECIMAL (38, 20) DEFAULT 1 NOT NULL,
+    [intTransactionTypeId] INT NOT NULL, 
+    [intLotId] INT NULL, 
+    [dtmCreated] DATE NULL, 
+    [intCreatedUserId] INT NULL    
+)
+
+GO
+CREATE NONCLUSTERED INDEX [IX_tblICInventoryTransaction_intItemId]
+    ON [dbo].[tblICInventoryTransaction]([intItemId] ASC);
+
+GO
+CREATE NONCLUSTERED INDEX [IX_tblICInventoryTransaction_intItemLocationStoreId]
+    ON [dbo].[tblICInventoryTransaction]([intItemLocationStoreId] ASC);
