70459394b1fb5ad7503dc91d86da71a6dc09f37b
Lawrence Zabala <lawrence.zabala@irely.com>
2014-09-16 17:26:03 +0800

    Fixed Server side routing of API calls

--- server/iRely.Inventory.WebAPI/App_Start/WebApiConfig.cs
+++ server/iRely.Inventory.WebAPI/App_Start/WebApiConfig.cs
@@ -11,7 +11,7 @@ namespace iRely.Inventory.WebAPI
         {
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
