85d7ccb9f36d863fc760000cf0498179be922baf
Ravikumar <ravikumar.k@irely.com>
2014-09-16 08:27:59 +0530

    IC-5 server side changes

---
+++ server/iRely.Inventory.BRL/PatronageCategory.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using iRely.Common;
+using iRely.Inventory.Model;
+using IdeaBlade.Core;
+using IdeaBlade.Linq;
+
+namespace iRely.Inventory.BRL
+{
+    public class PatronageCategory : IDisposable
+    {
+        private Repository _db;
+
+        public PatronageCategory()
+        {
+            _db = new Repository(new Inventory.Model.InventoryEntities());
+        }
+
+        public IQueryable<tblICPatronageCategory> GetSearchQuery()
+        {
+            return _db.GetQuery<tblICPatronageCategory>();
+        }
+
+        public object GetSearchQuery(int page, int start, int limit, IProjectionSelector selector, CompositeSortSelector sortSelector, Expression<Func<tblICPatronageCategory, bool>> predicate)
+        {
+            return GetSearchQuery()
+                .Where(predicate)
+                .OrderBySelector(sortSelector)
+                .Skip(start)
+                .Take(limit)
+                .Select(selector)
+                .AsNoTracking();
+        }
+
+        public int GetCount(Expression<Func<tblICPatronageCategory, bool>> predicate)
+        {
+            return GetSearchQuery().Where(predicate).Count();
+        }
+
+        public IQueryable<tblICPatronageCategory> GetPatronageCategories(int page, int start, int limit, CompositeSortSelector sortSelector, Expression<Func<tblICPatronageCategory, bool>> predicate)
+        {
+            var query = GetSearchQuery(); //Get Search Query
+            return _db.GetQuery<tblICPatronageCategory>()
+                      .Where(w => query.Where(predicate).Any(a => a.intPatronageCategoryId == w.intPatronageCategoryId)) //Filter the Main DataSource Based on Search Query
+                      .OrderBySelector(sortSelector)
+                      .Skip(start)
+                      .Take(limit)
+                      .AsNoTracking();
+        }
+
+        public void AddPatronageCategory(tblICPatronageCategory PatronageCategory)
+        {
+            _db.AddNew<tblICPatronageCategory>(PatronageCategory);
+        }
+
+        public void UpdatePatronageCategory(tblICPatronageCategory PatronageCategory)
+        {
+            _db.UpdateBatch<tblICPatronageCategory>(PatronageCategory);
+        }
+
+        public void DeletePatronageCategory(tblICPatronageCategory PatronageCategory)
+        {
+            _db.Delete<tblICPatronageCategory>(PatronageCategory);
+        }
+
+        public SaveResult Save(bool continueOnConflict)
+        {
+            return _db.Save(continueOnConflict);
+        }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+    }
+}
---
+++ server/iRely.Inventory.WebAPI/Controllers/PatronageCategoryController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using Newtonsoft.Json;
+using IdeaBlade.Core;
+using IdeaBlade.Linq;
+
+using iRely.Common;
+using iRely.Inventory.Model;
+using iRely.Inventory.BRL;
+
+namespace iRely.Invetory.WebAPI.Controllers
+{
+    public class PatronageCategoryController : ApiController
+    {
+        private PatronageCategory _PatronageCategoryBRL = new PatronageCategory();
+
+        public HttpResponseMessage SearchPatronageCategories(int page, int start, int limit, string columns = "", string sort = "", string filter = "")
+        {
+            var searchFilters = JsonConvert.DeserializeObject<IEnumerable<SearchFilter>>(filter);
+            var searchSorts = JsonConvert.DeserializeObject<IEnumerable<SearchSort>>(sort);
+            var predicate = ExpressionBuilder.True<tblICPatronageCategory>();
+            var selector = ExpressionBuilder.GetSelector(columns);
+
+            var sortSelector = ExpressionBuilder.GetSortSelector(searchSorts);
+
+            if (searchFilters != null)
+                predicate = ExpressionBuilder.GetPredicateBasedOnSearch<tblICPatronageCategory>(searchFilters);
+
+            var data = _PatronageCategoryBRL.GetSearchQuery(page, start, limit, selector, sortSelector, predicate);
+
+            var total = _PatronageCategoryBRL.GetCount(predicate);
+            return Request.CreateResponse(HttpStatusCode.OK, new
+            {
+                data = data,
+                total = total
+            });
+        }
+
+        [HttpGet]
+        [ActionName("GetPatronageCategories")]
+        public HttpResponseMessage GetPatronageCategories(int start = 0, int limit = 1, int page = 0, string sort = "", string filter = "")
+        {
+            filter = string.IsNullOrEmpty(filter) ? "" : filter;
+
+            var searchFilters = JsonConvert.DeserializeObject<IEnumerable<SearchFilter>>(filter);
+            var searchSorts = JsonConvert.DeserializeObject<IEnumerable<SearchSort>>(sort);
+            var predicate = ExpressionBuilder.True<tblICPatronageCategory>();
+            var sortSelector = ExpressionBuilder.GetSortSelector(searchSorts, "intPatronageCategoryId", "DESC");
+
+            if (searchFilters != null)
+                predicate = ExpressionBuilder.GetPredicateBasedOnSearch<tblICPatronageCategory>(searchFilters, true);
+
+            var total = _PatronageCategoryBRL.GetCount(predicate);
+            var data = _PatronageCategoryBRL.GetPatronageCategories(page, start, page == 0 ? total : limit, sortSelector, predicate);
+
+            return Request.CreateResponse(HttpStatusCode.OK, new
+            {
+                data = data.ToList(),
+                total = total
+            });
+        }
+
+        [HttpPost]
+        public HttpResponseMessage PostPatronageCategories(IEnumerable<tblICPatronageCategory> PatronageCategories, bool continueOnConflict = false)
+        {
+            foreach (var PatronageCategory in PatronageCategories)
+                _PatronageCategoryBRL.AddPatronageCategory(PatronageCategory);
+
+            var result = _PatronageCategoryBRL.Save(continueOnConflict);
+            _PatronageCategoryBRL.Dispose();
+
+            return Request.CreateResponse(HttpStatusCode.Accepted, new
+            {
+                data = PatronageCategories,
+                success = !result.HasError,
+                message = new
+                {
+                    statusText = result.Exception.Message,
+                    status = result.Exception.Error,
+                    button = result.Exception.Button.ToString()
+                }
+            });
+        }
+
+        [AcceptVerbs("POST", "PUT")]
+        [HttpPost]
+        [HttpPut]
+        public HttpResponseMessage PutPatronageCategories(IEnumerable<tblICPatronageCategory> PatronageCategories, bool continueOnConflict = false)
+        {
+            foreach (var PatronageCategory in PatronageCategories)
+                _PatronageCategoryBRL.UpdatePatronageCategory(PatronageCategory);
+
+            var result = _PatronageCategoryBRL.Save(continueOnConflict);
+            _PatronageCategoryBRL.Dispose();
+
+            return Request.CreateResponse(HttpStatusCode.Accepted, new
+            {
+                data = PatronageCategories,
+                success = !result.HasError,
+                message = new
+                {
+                    statusText = result.Exception.Message,
+                    status = result.Exception.Error,
+                    button = result.Exception.Button.ToString()
+                }
+            });
+        }
+
+        [AcceptVerbs("POST", "DELETE")]
+        [HttpPost]
+        [HttpDelete]
+        public HttpResponseMessage DeletePatronageCategories(IEnumerable<tblICPatronageCategory> PatronageCategories, bool continueOnConflict = false)
+        {
+            foreach (var PatronageCategory in PatronageCategories)
+                _PatronageCategoryBRL.DeletePatronageCategory(PatronageCategory);
+
+            var result = _PatronageCategoryBRL.Save(continueOnConflict);
+            _PatronageCategoryBRL.Dispose();
+
+            return Request.CreateResponse(HttpStatusCode.Accepted, new
+            {
+                data = PatronageCategories,
+                success = !result.HasError,
+                message = new
+                {
+                    statusText = result.Exception.Message,
+                    status = result.Exception.Error,
+                    button = result.Exception.Button.ToString()
+                }
+            });
+        }
+    }
+}
