25c8e838a0e4fc800178e1a57e360fdf4d5ad2f9
Abdul Rahman <m.rahman@irely.com>
2014-09-16 08:49:59 +0530

    IC-4

---
+++ server/iRely.Inventory.BRL/Tag.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+using iRely.Common;
+using iRely.Inventory.Model;
+using IdeaBlade.Core;
+using IdeaBlade.Linq;
+
+namespace iRely.Inventory.BRL
+{
+    public class Tag : IDisposable
+    {
+        private Repository _db;
+
+        public Tag()
+        {
+            _db = new Repository(new Inventory.Model.InventoryEntities());
+        }
+
+        public IQueryable<tblICTag> GetSearchQuery()
+        {
+            return _db.GetQuery<tblICTag>();
+        }
+
+        public object GetSearchQuery(int page, int start, int limit, IProjectionSelector selector, CompositeSortSelector sortSelector, Expression<Func<tblICTag, bool>> predicate)
+        {
+            return GetSearchQuery()
+                .Where(predicate)
+                .OrderBySelector(sortSelector)
+                .Skip(start)
+                .Take(limit)
+                .Select(selector)
+                .AsNoTracking();
+        }
+
+        public int GetCount(Expression<Func<tblICTag, bool>> predicate)
+        {
+            return GetSearchQuery().Where(predicate).Count();
+        }
+
+        public IQueryable<tblICTag> GetTags(int page, int start, int limit, CompositeSortSelector sortSelector, Expression<Func<tblICTag, bool>> predicate)
+        {
+            var query = GetSearchQuery(); //Get Search Query
+            return _db.GetQuery<tblICTag>()
+                      .Where(w => query.Where(predicate).Any(a => a.intTagId == w.intTagId)) //Filter the Main DataSource Based on Search Query
+                      .OrderBySelector(sortSelector)
+                      .Skip(start)
+                      .Take(limit)
+                      .AsNoTracking();
+        }
+
+        public void AddTag(tblICTag tag)
+        {
+            _db.AddNew<tblICTag>(tag);
+        }
+
+        public void UpdateTag(tblICTag tag)
+        {
+            _db.UpdateBatch<tblICTag>(tag);
+        }
+
+        public void DeleteTag(tblICTag tag)
+        {
+            _db.Delete<tblICTag>(tag);
+        }
+
+        public SaveResult Save(bool continueOnConflict)
+        {
+            return _db.Save(continueOnConflict);
+        }
+        
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+    }
+}
---
+++ server/iRely.Inventory.WebAPI/Controllers/TagController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using Newtonsoft.Json;
+using IdeaBlade.Core;
+using IdeaBlade.Linq;
+
+using iRely.Common;
+using iRely.Inventory.Model;
+using iRely.Inventory.BRL;
+
+namespace iRely.Invetory.WebAPI.Controllers
+{
+    public class TagController : ApiController
+    {
+        private Tag _TagBRL = new Tag();
+
+        public HttpResponseMessage SearchTags(int page, int start, int limit, string columns = "", string sort = "", string filter = "")
+        {
+            var searchFilters = JsonConvert.DeserializeObject<IEnumerable<SearchFilter>>(filter);
+            var searchSorts = JsonConvert.DeserializeObject<IEnumerable<SearchSort>>(sort);
+            var predicate = ExpressionBuilder.True<tblICTag>();
+            var selector = ExpressionBuilder.GetSelector(columns);
+
+            var sortSelector = ExpressionBuilder.GetSortSelector(searchSorts);
+
+            if (searchFilters != null)
+                predicate = ExpressionBuilder.GetPredicateBasedOnSearch<tblICTag>(searchFilters);
+
+            var data = _TagBRL.GetSearchQuery(page, start, limit, selector, sortSelector, predicate);
+
+            var total = _TagBRL.GetCount(predicate);
+            return Request.CreateResponse(HttpStatusCode.OK, new
+            {
+                data = data,
+                total = total
+            });
+        }
+
+        [HttpGet]
+        [ActionName("GetTags")]
+        public HttpResponseMessage GetTags(int start = 0, int limit = 1, int page = 0, string sort = "", string filter = "")
+        {
+            filter = string.IsNullOrEmpty(filter) ? "" : filter;
+
+            var searchFilters = JsonConvert.DeserializeObject<IEnumerable<SearchFilter>>(filter);
+            var searchSorts = JsonConvert.DeserializeObject<IEnumerable<SearchSort>>(sort);
+            var predicate = ExpressionBuilder.True<tblICTag>();
+            var sortSelector = ExpressionBuilder.GetSortSelector(searchSorts, "intTagId", "DESC");
+
+            if (searchFilters != null)
+                predicate = ExpressionBuilder.GetPredicateBasedOnSearch<tblICTag>(searchFilters, true);
+
+            var total = _TagBRL.GetCount(predicate);
+            var data = _TagBRL.GetTags(page, start, page == 0 ? total : limit, sortSelector, predicate);
+
+            return Request.CreateResponse(HttpStatusCode.OK, new
+            {
+                data = data.ToList(),
+                total = total
+            });
+        }
+
+        [HttpPost]
+        public HttpResponseMessage PostTags(IEnumerable<tblICTag> tags, bool continueOnConflict = false)
+        {
+            foreach (var tag in tags)
+                _TagBRL.AddTag(tag);
+
+            var result = _TagBRL.Save(continueOnConflict);
+            _TagBRL.Dispose();
+
+            return Request.CreateResponse(HttpStatusCode.Accepted, new
+            {
+                data = tags,
+                success = !result.HasError,
+                message = new
+                {
+                    statusText = result.Exception.Message,
+                    status = result.Exception.Error,
+                    button = result.Exception.Button.ToString()
+                }
+            });
+        }
+
+        [AcceptVerbs("POST", "PUT")]
+        [HttpPost]
+        [HttpPut]
+        public HttpResponseMessage PutTags(IEnumerable<tblICTag> tags, bool continueOnConflict = false)
+        {
+            foreach (var tag in tags)
+                _TagBRL.UpdateTag(tag);
+
+            var result = _TagBRL.Save(continueOnConflict);
+            _TagBRL.Dispose();
+
+            return Request.CreateResponse(HttpStatusCode.Accepted, new
+            {
+                data = tags,
+                success = !result.HasError,
+                message = new
+                {
+                    statusText = result.Exception.Message,
+                    status = result.Exception.Error,
+                    button = result.Exception.Button.ToString()
+                }
+            });
+        }
+
+        [AcceptVerbs("POST", "DELETE")]
+        [HttpPost]
+        [HttpDelete]
+        public HttpResponseMessage DeleteTags(IEnumerable<tblICTag> tags, bool continueOnConflict = false)
+        {
+            foreach (var tag in tags)
+                _TagBRL.DeleteTag(tag);
+
+            var result = _TagBRL.Save(continueOnConflict);
+            _TagBRL.Dispose();
+
+            return Request.CreateResponse(HttpStatusCode.Accepted, new
+            {
+                data = tags,
+                success = !result.HasError,
+                message = new
+                {
+                    statusText = result.Exception.Message,
+                    status = result.Exception.Error,
+                    button = result.Exception.Button.ToString()
+                }
+            });
+        }
+    }
+}
