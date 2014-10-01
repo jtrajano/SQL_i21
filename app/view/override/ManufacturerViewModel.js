0e88eda91f826c03e22e0a75ae271afb1c393a36
Lawrence Zabala <lawrence.zabala@irely.com>
2014-10-01 14:18:11 +0800

    Fix Manufacturer ViewModel override

--- app/view/override/ManufacturerViewModel.js
+++ app/view/override/ManufacturerViewModel.js
@@ -1,9 +1,8 @@
 /**
  * Created by kkarthick on 24-09-2014.
  */
-Ext.define('Inventory.view.ManufacturerViewModel', {
-    extend: 'Ext.app.ViewModel',
-    alias: 'viewmodel.manufacturer',
+Ext.define('Inventory.view.override.ManufacturerViewModel', {
+    override: 'Inventory.view.ManufacturerViewModel',
 
     requires:['i21.store.ZipCodeBuffered',
               'i21.store.CountryBuffered']
