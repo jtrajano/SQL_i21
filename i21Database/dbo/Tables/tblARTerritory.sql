236e48c3a9665a464304864bfcbc76db78b53697
Smith Wilson de Jesus <smith.dejesus@irely.com>
2014-10-01 15:31:29 +0800

    AR-252

--- i21Database/dbo/Tables/tblARTerritory.sql
+++ i21Database/dbo/Tables/tblARTerritory.sql
@@ -2,6 +2,13 @@
     [intTerritoryId]   INT            IDENTITY (1, 1) NOT NULL,
     [strDescription]   NVARCHAR (255) COLLATE Latin1_General_CI_AS NULL,
     [intConcurrencyId] INT            NOT NULL,
-    CONSTRAINT [PK_tblARTerritory] PRIMARY KEY CLUSTERED ([intTerritoryId] ASC)
+    CONSTRAINT [PK_tblARTerritory_intTerritoryId] PRIMARY KEY CLUSTERED ([intTerritoryId] ASC)
 );
+
+
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [UQ_tblARTerritory_strDescription]
+    ON [dbo].[tblARTerritory]([strDescription] ASC);
 
