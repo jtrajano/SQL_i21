47b921f06a8721dbe7081093f0ff26836b596c28
Lawrence Zabala <lawrence.zabala@irely.com>
2014-10-03 14:43:18 +0800

    Inventory Receipt tables

---
+++ i21Database/dbo/Tables/tblICInventoryReceiptItem.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[tblICInventoryReceiptItem]
+(
+	[intInventoryReceiptItemId] INT NOT NULL IDENTITY, 
+    [intLineNo] INT NOT NULL, 
+    [intItemId] INT NOT NULL, 
+    [intUnitMeasureId] INT NOT NULL, 
+    [intNoPackages] INT NULL, 
+    [dblExpPackageWeight] NUMERIC(18, 6) NULL DEFAULT ((0)), 
+    [dblUnitCost] NUMERIC(18, 6) NULL DEFAULT ((0)), 
+    [dblUnitRetail] NUMERIC(18, 6) NULL DEFAULT ((0)), 
+	[dblLineTotal] NUMERIC(18, 6) NULL DEFAULT ((0)), 
+	[dblGrossMargin] NUMERIC(18, 6) NULL DEFAULT ((0)), 
+
+    CONSTRAINT [PK_tblICInventoryReceiptItem] PRIMARY KEY ([intInventoryReceiptItemId])
+)
