0ae7f262a1cfc5dd9789fdcac2e4de76b7c81ead
Smith Wilson de Jesus <smith.dejesus@irely.com>
2014-10-03 14:51:37 +0800

    AR-390

--- i21Database/dbo/Tables/tblARMarketZone.sql
+++ i21Database/dbo/Tables/tblARMarketZone.sql
@@ -2,7 +2,7 @@
     [intMarketZoneId]   INT            IDENTITY (1, 1) NOT NULL,
     [strMarketZoneCode] NVARCHAR (20)  COLLATE Latin1_General_CI_AS NULL,
     [strDescription]    NVARCHAR (255) COLLATE Latin1_General_CI_AS NULL,
-    [intConcurrencyId]  INT            NOT NULL,
+    [intConcurrencyId]  INT            NOT NULL DEFAULT ((0)),
     CONSTRAINT [PK_tblARMarketZone] PRIMARY KEY CLUSTERED ([intMarketZoneId] ASC),
 	CONSTRAINT [UKstrMarketZoneCode] UNIQUE NONCLUSTERED ([strMarketZoneCode] ASC)
 );
