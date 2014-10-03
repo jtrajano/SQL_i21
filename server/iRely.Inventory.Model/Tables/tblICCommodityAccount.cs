19c1a1299c9dd1d29036655ddb060a0fbfb4da9e
Lawrence Zabala <lawrence.zabala@irely.com>
2014-10-03 14:46:58 +0800

    IC-9

---
+++ server/iRely.Inventory.Model/Tables/tblICCommodityAccount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using iRely.Common;
+
+namespace iRely.Inventory.Model
+{
+    public class tblICCommodityAccount : BaseEntity
+    {
+
+
+        public tblICCommodity tblICCommodity { get; set; }
+        
+    }
+}
