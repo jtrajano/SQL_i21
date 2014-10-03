71d52e8568ff35f9c25e0a83dc0c5ca579937e1a
Lawrence Zabala <lawrence.zabala@irely.com>
2014-10-03 11:31:01 +0800

    IC-9

---
+++ i21Database/dbo/Tables/tblICCommodityAccount.sql
@@ -0,0 +1,78 @@
+﻿CREATE TABLE [dbo].[tblICCommodityAccount]
+(
+	[intCommodityAccountId] INT NOT NULL IDENTITY, 
+    [intCommodityId] INT NOT NULL, 
+    [intLocationId] INT NOT NULL, 
+    [strAccountDescription] NVARCHAR(50) NULL, 
+    [intAccountId] INT NULL, 
+    [intSort] INT NULL, 
+    [intConcurrencyId] INT NULL DEFAULT ((0)), 
+    CONSTRAINT [PK_tblICCommodityAccount] PRIMARY KEY ([intCommodityAccountId]), 
+    CONSTRAINT [FK_tblICCommodityAccount_tblICCommodity] FOREIGN KEY ([intCommodityId]) REFERENCES [tblICCommodity]([intCommodityId]), 
+    CONSTRAINT [FK_tblICCommodityAccount_tblSMCompanyLocation] FOREIGN KEY ([intLocationId]) REFERENCES [tblSMCompanyLocation]([intCompanyLocationId]), 
+    CONSTRAINT [FK_tblICCommodityAccount_tblGLAccount] FOREIGN KEY ([intAccountId]) REFERENCES [tblGLAccount]([intAccountId])
+)
+
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Identity Field',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'tblICCommodityAccount',
+    @level2type = N'COLUMN',
+    @level2name = N'intCommodityAccountId'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Commodity Id',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'tblICCommodityAccount',
+    @level2type = N'COLUMN',
+    @level2name = N'intCommodityId'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Location Id',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'tblICCommodityAccount',
+    @level2type = N'COLUMN',
+    @level2name = N'intLocationId'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Account Description',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'tblICCommodityAccount',
+    @level2type = N'COLUMN',
+    @level2name = N'strAccountDescription'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Account Id',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'tblICCommodityAccount',
+    @level2type = N'COLUMN',
+    @level2name = N'intAccountId'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Sort Field',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'tblICCommodityAccount',
+    @level2type = N'COLUMN',
+    @level2name = N'intSort'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Concurrency Field',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'tblICCommodityAccount',
+    @level2type = N'COLUMN',
+    @level2name = N'intConcurrencyId'
