f1cb1d8f3ea50be18ca6112691bbbc21d8822819
Erick Dela Cruz <erick.delacruz@irely.com>
2014-10-08 17:41:12 +0800

    AP-512

---
+++ i21Database/dbo/Tables/tblPOPurchase.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[tblPOPurchase]
+(
+	[intPurchaseId] INT NOT NULL PRIMARY KEY, 
+    [intVendorId] INT NOT NULL, 
+    [intAccountId] INT NOT NULL, 
+    [intFreightId] INT NOT NULL, 
+    [intCurrencyId] INT NOT NULL, 
+    [intTermId] INT NOT NULL, 
+    [intOrderById] INT NOT NULL, 
+    [intApprovedById] INT NOT NULL, 
+    [intShipViaId] INT NOT NULL, 
+    [intShipFromId] INT NOT NULL, 
+    [intShipToId] INT NOT NULL, 
+    [intStoreId] INT NULL, 
+    [strOrderNumber] NVARCHAR(100) NULL, 
+    [dtmDate] DATETIME NOT NULL DEFAULT GETDATE(), 
+    [dtmExpectedDate] DATETIME NOT NULL DEFAULT GETDATE()
+)
---
+++ i21Database/dbo/Tables/tblPOPurchaseDetail.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[tblPOPurchaseDetail]
+(
+	[intPurchaseDetailId] INT NOT NULL PRIMARY KEY, 
+    [intPurchaseId] INT NOT NULL, 
+    [intProductId] INT NULL, 
+    [intUnitOfMeasureId] INT NOT NULL, 
+    [intAccountId] INT NOT NULL, 
+    [dblQtyOrdered] NUMERIC(18, 6) NOT NULL DEFAULT 0, 
+    [dblQtyReceived] NUMERIC(18, 6) NOT NULL DEFAULT 0, 
+    [dblVolume] NUMERIC(18, 6) NOT NULL DEFAULT 0, 
+    [dblWeight] NUMERIC(18, 6) NOT NULL DEFAULT 0, 
+    [dblDiscount] NUMERIC(18, 6) NOT NULL DEFAULT 0, 
+    [dblCost] NUMERIC(18, 6) NOT NULL DEFAULT 0, 
+    [strDescription] NVARCHAR(250) NULL
+)
