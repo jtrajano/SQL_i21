61ee6c9af7e69b86f225148f9139bb3501a11feb
Lawrence Zabala <lawrence.zabala@irely.com>
2014-10-09 11:24:24 +0800

    IC-66

--- server/iRely.Inventory.Model/Tables/tblICBrand.cs
+++ server/iRely.Inventory.Model/Tables/tblICBrand.cs
@@ -13,7 +13,7 @@ namespace iRely.Inventory.Model
         public int intBrandId { get; set; }
         public string strBrandCode { get; set; }
         public string strBrandName { get; set; }
-        public int intManufacturerId { get; set; }
+        public int? intManufacturerId { get; set; }
         public int intSort { get; set; }
 
         public tblICManufacturer tblICManufacturer { get; set; }
