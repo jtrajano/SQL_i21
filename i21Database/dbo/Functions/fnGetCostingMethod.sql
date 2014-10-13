66feb954d083a53c2648c06caf3379f555e86baf
Feb Montefrio <feb.montefrio@irely.com>
2014-10-13 17:15:33 +0800

    1. Add new functions common in inventory costing 2. Add unit test for the newly added functions.
    Note: These are just initial functions. Business rules are yet to be finalized.

---
+++ i21Database/dbo/Functions/fnGetCostingMethod.sql
@@ -0,0 +1,21 @@
+﻿
+-- This function will retrieve the costing method configured in an item (and per location).
+CREATE FUNCTION [dbo].[fnGetCostingMethod](
+	@intItemId INT
+	,@intLocationId INT
+)
+RETURNS NVARCHAR(40)
+AS 
+BEGIN 
+	DECLARE @strCostingMethod AS INT
+
+	-- TODO: Change "strCostingMethod" to "intCostingMethod"
+	-- See this comment. http://www.inet.irelyserver.com/display/INV/Location+Tab?focusedCommentId=38209014#comment-38209014
+	SELECT	@strCostingMethod = strCostingMethod
+	FROM	tblICItemLocationStore
+	WHERE	intItemId = @intItemId
+			AND intLocationId = @intLocationId
+
+	RETURN @strCostingMethod;
+END
+GO
