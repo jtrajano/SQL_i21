28b1f6cd6e9a11dd5cff4e1dea2c2ae381aa466b
Erick Dela Cruz <erick.delacruz@irely.com>
2014-10-20 22:56:31 +0800

    AP-6

--- i21Database/dbo/Views/vyuAPPaidBillsHistory.sql
+++ i21Database/dbo/Views/vyuAPPaidBillsHistory.sql
@@ -15,3 +15,4 @@ SELECT
 FROM tblAPPayment A
 	INNER JOIN (tblAPVendor B INNER JOIN tblEntity B1 ON B.intEntityId = B1.intEntityId) ON A.intVendorId = B.intVendorId
 	INNER JOIN (tblCMBankTransaction C INNER JOIN tblCMBankAccount C1 ON C.intBankAccountId = C1.intBankAccountId) ON A.strPaymentRecordNum = C.strTransactionId
+WHERE A.ysnPosted = 1 AND A.ysnVoid = 0
