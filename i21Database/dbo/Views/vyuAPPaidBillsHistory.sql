c0d2ae630a175a6707d6fc715827893827baa3d3
Erick Dela Cruz <erick.delacruz@irely.com>
2014-10-20 22:24:24 +0800

    AP-6

---
+++ i21Database/dbo/Views/vyuAPPaidBillsHistory.sql
@@ -0,0 +1,17 @@
+﻿CREATE VIEW [dbo].[vyuAPPaidBillsHistory]
+AS
+SELECT
+	intPaymentId
+	,B.intVendorId
+	,C.intBankAccountId
+	,B.strVendorId
+	,B1.strName as strVendorName
+	,C1.strCbkNo
+	,A.strPaymentRecordNum
+	,A.strPaymentInfo
+	,A.dblAmountPaid
+	,C.dtmDateReconciled
+	,C.ysnClr as ysnCleared
+FROM tblAPPayment A
+	INNER JOIN (tblAPVendor B INNER JOIN tblEntity B1 ON B.intEntityId = B1.intEntityId) ON A.intVendorId = B.intVendorId
+	INNER JOIN (tblCMBankTransaction C INNER JOIN tblCMBankAccount C1 ON C.intBankAccountId = C1.intBankAccountId) ON A.strPaymentRecordNum = C.strTransactionId
