d96de5063fc4ae85ce13312fb1514f14373008b6
kenneth lagunero <kenneth.lagunero@irely.com>
2014-10-21 00:41:09 +0800

    TM-901

--- i21Database/dbo/Tables/tblTMCustomer.sql
+++ i21Database/dbo/Tables/tblTMCustomer.sql
@@ -3,6 +3,7 @@
     [intCustomerID]        INT IDENTITY (1, 1) NOT NULL,
     [intCurrentSiteNumber] INT DEFAULT 0 NOT NULL,
     [intCustomerNumber]    INT DEFAULT 0 NOT NULL,
+    [strOriginCustomerKey] NVARCHAR(15) COLLATE Latin1_General_CI_AS DEFAULT ('') NOT NULL, 
     CONSTRAINT [PK_tblTMCustomer] PRIMARY KEY CLUSTERED ([intCustomerID] ASC)
 );
 
