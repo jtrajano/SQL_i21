3c5507584761cfc9f73cab70f6a41da452f46253
kenneth lagunero <kenneth.lagunero@irely.com>
2014-10-21 00:41:09 +0800

    Revert "TM-901"
    This reverts commit 5d1d52555ad6f257ea506c93afad8eaebdf6e483.

--- i21PreDeployment/Scripts/Pre-Deployment/1_TM/4_1420_to_1430.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/1_TM/4_1420_to_1430.sql
@@ -258,25 +258,6 @@ BEGIN
 END
 GO
 
-PRINT N'BEGIN Update of data in tblTMCustomer Populate strOriginCustomerKey'
-GO
-
-IF EXISTS(SELECT TOP 1 1 FROM INFORMATION_SCHEMA.VIEWS WHERE TABLE_NAME = 'vwcusmst') 
-	AND EXISTS(SELECT TOP 1 1 FROM sys.columns WHERE NAME  = N'strOriginCustomerKey' AND OBJECT_ID = OBJECT_ID(N'tblTMCustomer'))
-BEGIN
-		EXEC ('
-				UPDATE tblTMCustomer
-				SET strOriginCustomerKey = ISNULL(A.vwcus_key,'')
-				FROM vwcusmst A
-				WHERE tblTMCustomer.intCustomerNumber = A.A4GLIdentity
-				AND tblTMCustomer.strOriginCustomerKey IS NULL OR tblTMCustomer.strOriginCustomerKey = ''
-			  ')
-END
-GO
-
-PRINT N'END Update of data in tblTMCustomer Populate strOriginCustomerKey'
-GO
-
 
 
 
