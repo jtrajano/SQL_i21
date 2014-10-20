ae17bb6b457e5aaf39a565e93139ea0a22321cea
Erick Dela Cruz <erick.delacruz@irely.com>
2014-10-20 16:46:27 +0800

    Add constraint unit test on AP tables.

---
+++ tSQLt_UnitTesting/testi21Database/Scripts/test the constraints of tblAPBill.sql
@@ -0,0 +1,19 @@
+﻿--Since some of the constraints were just newly added, adding it requires to fix some data
+--and causing it to not actually add in on the table definition
+--with this test, we will know that the constraint should exists
+CREATE PROCEDURE testi21Database.[test the constraints of tblAPBill]
+AS
+BEGIN
+	--Primary Key [PK_dbo.tblAPBill]
+	IF NOT EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE CONSTRAINT_NAME='PK_dbo.tblAPBill')
+	EXEC tSQLt.Fail 'Primary key on tblAPBill does not exists'
+	--Foreign key to tblAPBillBatch
+	IF NOT EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE CONSTRAINT_NAME='FK_dbo.tblAPBill_dbo.tblAPBillBatch_intBillBatchId')
+	EXEC tSQLt.Fail 'Foreign key to tblAPBillBatch on tblAPBill does not exists'
+	--Foreign key to tblSMTerm
+	IF NOT EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE CONSTRAINT_NAME='FK_dbo.tblAPBill_dbo.tblSMTerm_intTermId')
+	EXEC tSQLt.Fail 'Foreign key to tblSMTerm on tblAPBill does not exists'
+	--Foreign key to tblEntity
+	IF NOT EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE CONSTRAINT_NAME='FK_dbo.tblAPBill_dbo.tblEntity_intEntityId')
+	EXEC tSQLt.Fail 'Foreign key to tblEntity on tblAPBill does not exists'
+END
---
+++ tSQLt_UnitTesting/testi21Database/Scripts/test the constraints of tblAPVendor.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE testi21Database.[test the constraints of tblAPVendor]
+AS
+BEGIN
+
+	--Primary Key 
+	IF NOT EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE CONSTRAINT_NAME='PK_dbo.tblAPVendor')
+	EXEC tSQLt.Fail 'Primary key on tblAPVendor does not exists'
+	--Unique key strVendorId to tblAPVendor
+	IF NOT EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE CONSTRAINT_NAME='UK_strVendorId')
+	EXEC tSQLt.Fail 'Unique key strVendorId to tblAPVendor on tblAPVendor does not exists'
+	--Unique key intVendorId to tblAPVendor
+	IF NOT EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE CONSTRAINT_NAME='UK_intVendorId')
+	EXEC tSQLt.Fail 'Unique key intVendorId to tblAPVendor on tblAPVendor does not exists'
+	--Foreign key to tblEntity
+	IF NOT EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE CONSTRAINT_NAME='FK_dbo.tblAPVendor_dbo.tblEntities_intEntityId')
+	EXEC tSQLt.Fail 'Foreign key to tblAPVendor on tblEntity does not exists'
+
+END
