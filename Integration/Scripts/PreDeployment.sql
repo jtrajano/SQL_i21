d1b0390cf24fa6d712bb158fd66ba6e22791af44
kenneth lagunero <kenneth.lagunero@irely.com>
2014-10-22 03:38:34 +0800

    TM-901

--- Integration/Scripts/PreDeployment.sql
+++ Integration/Scripts/PreDeployment.sql
@@ -82,7 +82,8 @@
 :r "..\dbo\Stored Procedures\uspTMGetConsumptionWithGasCheck.sql"
 :r "..\dbo\Stored Procedures\uspTMGetConsumptionWithLeakCheck.sql"
 :r "..\dbo\Stored Procedures\uspTMGetConsumptionWithoutLeakCheck.sql"
-:r ..\dbo\Views\vyuTMOriginDegreeOption.sql
+:r "..\dbo\Views\vyuTMOriginDegreeOption.sql"
+:r "..\Scripts\TM\Customer.sql"
 
 :r "..\dbo\Functions\fnGetVendorLastName.sql"
 :r "..\dbo\Stored Procedures\uspAPImportBillTransactions.sql"
