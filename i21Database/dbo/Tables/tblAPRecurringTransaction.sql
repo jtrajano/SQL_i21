6967d8293d12218b8e834a5c3d2e75fa5699fed9
Erick Dela Cruz <erick.delacruz@irely.com>
2014-10-22 15:48:15 +0800

    AP-8

--- i21Database/dbo/Tables/tblAPRecurringTransaction.sql
+++ i21Database/dbo/Tables/tblAPRecurringTransaction.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[tblAPRecurringTransaction] (
     [intRecurringId]   INT           IDENTITY (1, 1) NOT NULL,
-    [strTransactonId]  NVARCHAR (50) COLLATE Latin1_General_CI_AS NULL,
-    [intTransactionId] INT           NULL,
+    [intTransactionId] INT           NOT NULL,
+	[intTransactionType] INT           NOT NULL,
     [strFrequency]     NVARCHAR (50) COLLATE Latin1_General_CI_AS NULL,
     [dtmLastProcess]   DATETIME      NULL,
     [dtmNextProcess]   DATETIME      NULL,
@@ -11,6 +11,7 @@
     [ysnDue]           BIT           NULL,
     [intGroupId]       INT           NULL,
     [intDayofMonth]    INT           NULL,
+	[intEntityId]		INT			NOT NULL, 
     [intConcurrencyId] INT NOT NULL DEFAULT 0, 
     PRIMARY KEY CLUSTERED ([intRecurringId] ASC)
 );
