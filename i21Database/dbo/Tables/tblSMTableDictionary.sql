ef2906c2c252896256afa61b2772fdc763729fa3
von.pasion <von.pasion@irely.com>
2014-10-23 23:29:00 +0800

    FRM-1516

---
+++ i21Database/dbo/Tables/tblSMTableDictionary.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tblSMTableDictionary] (
+    [intTableDictionaryId] INT            IDENTITY (1, 1) NOT NULL,
+    [strModuleName]        NVARCHAR (50)  COLLATE Latin1_General_CI_AS NOT NULL,
+    [strTableName]         NVARCHAR (50)  COLLATE Latin1_General_CI_AS NOT NULL,
+    [intManagerId]         INT            NOT NULL,
+    [strLink]              NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NULL,
+    [strDescription]       NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NULL,
+    [intConcurrencyId]     INT            NOT NULL,
+    CONSTRAINT [PK__tblSMTableDictionary] PRIMARY KEY CLUSTERED ([intTableDictionaryId] ASC)
+);
+
