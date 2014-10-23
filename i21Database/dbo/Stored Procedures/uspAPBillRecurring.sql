993701f0af332c4ea2f143fa9ecb1920c09fb2f7
Erick Dela Cruz <erick.delacruz@irely.com>
2014-10-23 20:56:10 +0800

    AP-6

---
+++ i21Database/dbo/Stored Procedures/uspAPBillRecurring.sql
@@ -0,0 +1,87 @@
+﻿CREATE PROCEDURE [dbo].[uspAPBillRecurring]
+	@recurrings NVARCHAR(MAX),
+	@userId INT
+AS
+BEGIN
+
+CREATE TABLE #tmpRecurringData (
+	[intRecurringId] [int] PRIMARY KEY,
+	UNIQUE ([intRecurringId])
+);
+
+INSERT INTO #tmpRecurringData SELECT [intID] FROM [dbo].fnGetRowsFromDelimitedValues(@recurrings)
+
+SELECT intTransactionId INTO #tmpRecurringBill FROM tblAPRecurringTransaction
+WHERE intRecurringId IN (SELECT intRecurringId FROM #tmpRecurringData)
+
+INSERT INTO tblAPBill(
+		[strVendorOrderNumber], 
+		[intTermsId],
+		[intTaxId],
+		[dtmDate],            
+		[dtmDueDate],
+		[intAccountId],
+		[strDescription],
+		[dblTotal],
+		[dblSubtotal],
+		[ysnPosted],
+		[ysnPaid],
+		[strBillId],
+		[dblAmountDue],
+		[dtmDatePaid],
+		[dtmDiscountDate],
+		[intUserId],
+		[intConcurrencyId],
+		[dtmBillDate],
+		[intVendorId],
+		[dblWithheld],
+		[dblDiscount],
+		[dblBillTax],
+		[dblPayment],
+		[dblInterest],
+		[intTransactionType],
+		[intPurchaseOrderId],
+		[intShipFromId],
+		[intShipToId],
+		[intStoreLocationId],
+		[intContactId],
+		[intOrderById],
+		[intEntityId]
+	)
+	SELECT 
+		[strVendorOrderNumber], 
+		[intTermsId],
+		[intTaxId],
+		[dtmDate],            
+		[dtmDueDate],
+		[intAccountId],
+		[strDescription],
+		[dblTotal],
+		[dblSubtotal],
+		[ysnPosted],
+		[ysnPaid],
+		[strBillId],
+		[dblAmountDue],
+		[dtmDatePaid],
+		[dtmDiscountDate],
+		[intUserId],
+		[intConcurrencyId],
+		GETDATE(),
+		[intVendorId],
+		[dblWithheld],
+		[dblDiscount],
+		[dblBillTax],
+		[dblPayment],
+		[dblInterest],
+		[intTransactionType],
+		[intPurchaseOrderId],
+		[intShipFromId],
+		[intShipToId],
+		[intStoreLocationId],
+		[intContactId],
+		[intOrderById],
+		@userId
+	FROM tblAPBill
+	WHERE intBillId IN (SELECT intTransactionId FROM #tmpRecurringBill)
+
+END
