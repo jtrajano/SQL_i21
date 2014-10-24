e8f6dbdb5e6698791396cdc5ce64274d49ac4fa3
Feb Montefrio <feb.montefrio@irely.com>
2014-10-24 09:46:07 +0800

    Refactor fnGetItemGLAccounts Add unit test for fnGetItemGLAccounts

--- i21Database/dbo/Functions/fnGetItemGLAccounts.sql
+++ i21Database/dbo/Functions/fnGetItemGLAccounts.sql
@@ -9,33 +9,21 @@ RETURNS TABLE
 AS 
 RETURN 
 	SELECT	-- Get the inventory g/l account id
-			intInventoryAccount =  dbo.fnGetGLAccountIdFromProfitCenter(
+			Inventory =  dbo.fnGetGLAccountIdFromProfitCenter(
 				dbo.fnGetItemBaseGLAccount(@intItemId, @intLocationId, 1)
 				,dbo.fnGetItemProfitCenter(@intItemId, @intLocationId, 1)
 			)
 
 			-- Get the COGS g/l account id
-			,intCOGSAccount = dbo.fnGetGLAccountIdFromProfitCenter(
+			,Sales = dbo.fnGetGLAccountIdFromProfitCenter(
 				dbo.fnGetItemBaseGLAccount(@intItemId, @intLocationId, 2)
 				,dbo.fnGetItemProfitCenter(@intItemId, @intLocationId, 2)
 			)
 
 			-- Get the Revalue Cost g/l account id
-			,intRevalueCostAccount = dbo.fnGetGLAccountIdFromProfitCenter(
+			,Purchases = dbo.fnGetGLAccountIdFromProfitCenter(
 				dbo.fnGetItemBaseGLAccount(@intItemId, @intLocationId, 3)
 				,dbo.fnGetItemProfitCenter(@intItemId, @intLocationId, 3)
-			)
-
-			-- Get the Write-Off Cost g/l account id
-			,intWriteOffCostAccount = dbo.fnGetGLAccountIdFromProfitCenter(
-				dbo.fnGetItemBaseGLAccount(@intItemId, @intLocationId, 4)
-				,dbo.fnGetItemProfitCenter(@intItemId, @intLocationId, 4)
-			)
-
-			-- Get the Auto-negative g/l account  id 
-			,intAutoNegativeAccount = dbo.fnGetGLAccountIdFromProfitCenter(
-				dbo.fnGetItemBaseGLAccount(@intItemId, @intLocationId, 5)
-				,dbo.fnGetItemProfitCenter(@intItemId, @intLocationId, 5)
 			)
 
 GO
