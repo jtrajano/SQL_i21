b0eceb2ed99eca8b356abdb1dd437e0b4d8bb06e
Feb Montefrio <feb.montefrio@irely.com>
2014-10-24 15:14:28 +0800

    IC-100 Implement fnGetNegativeInventoryOption Add unit test for it as well.

--- i21Database/dbo/Functions/fnGetNegativeInventoryOption.sql
+++ i21Database/dbo/Functions/fnGetNegativeInventoryOption.sql
@@ -1,19 +1,25 @@
-﻿
--- Returns the selected option for an item when handling negative stocks. 
+﻿--------------------------------------------------------------------------------------------------------------------------------------------------
+-- Returns the selected option for an item and for a particular location. 
+-- Values for "Allow Negative Inventory". 
+--
+-- 1 > Yes (Inventory is allowed to go less than zero and stay there) 
+-- 2 > Yes with Auto Write-Off (Inventory is allowed to go negative. However as soon as it does, the system automatically adjusts the inventory back to 0 resulting in a  debit to inventory  and a credit to P&L (gain). )
+-- 3 > No (The system will block any transaction that would result in inventory going into the negative)
+--------------------------------------------------------------------------------------------------------------------------------------------------
 CREATE FUNCTION [dbo].[fnGetNegativeInventoryOptions] (
 	@intItemId INT
+	,@intLocationId INT 
 )
 RETURNS INT 
 AS
 BEGIN
 	DECLARE @AllowNegativeInventoryType AS INT
 
-	-- TODO: Replace it with the correct business rule 
-	-- See: http://inet.irelyserver.com/display/INV/Inbound+Stock+Process?focusedCommentId=38209060#comment-38209060
 
-	SELECT	@AllowNegativeInventoryType = NULL
-	FROM	tblICItem
+	SELECT	@AllowNegativeInventoryType = intAllowNegativeInventory 
+	FROM	tblICItemLocation
 	WHERE	intItemId = @intItemId
+			AND intLocationId = @intLocationId
 
 	RETURN @AllowNegativeInventoryType
 END
