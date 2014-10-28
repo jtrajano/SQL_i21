7bfb46bd8d803052a9496a0c83a0b3fbf820453f
Feb Montefrio <feb.montefrio@irely.com>
2014-10-28 09:52:55 +0800

    IC-100 Add test cases for uspICProcesCosting. It is for checking for any errors thrown by all the stored procedures it uses.

---
+++ tSQLt_UnitTesting/testi21Database/Stored Procedures/test uspICProcessCosting for errors thrown by uspICUnpostCosting.sql
@@ -0,0 +1,76 @@
+﻿
+-- Note: This stored procedure is monolithic (huge)
+-- Unit test involved here is to check if the stored procedures called by this SP is called correctly. 
+
+CREATE PROCEDURE [testi21Database].[test uspICProcessCosting for errors thrown by uspICUnpostCosting]
+AS
+BEGIN
+	-- Arrange 
+	BEGIN 
+		DECLARE @isCalledUspICValidateCostingOnPost AS BIT
+		DECLARE @isCalledUspICPostCosting AS BIT
+		DECLARE @isCalledUspICValidateCostingOnUnpost AS BIT
+		DECLARE @isCalledUspICUnpostCosting AS BIT
+
+		DECLARE @errorCommand AS NVARCHAR(MAX)
+		DECLARE @command AS NVARCHAR(MAX)
+		SET @errorCommand = 'RAISERROR(''Error raised inside uspICUnpostCosting'', 16, 1);';
+
+		EXEC tSQLt.SpyProcedure 'dbo.uspICValidateCostingOnPost';
+		EXEC tSQLt.SpyProcedure 'dbo.uspICPostCosting';
+		EXEC tSQLt.SpyProcedure 'dbo.uspICValidateCostingOnUnpost';
+		EXEC tSQLt.SpyProcedure 'dbo.uspICUnpostCosting', @errorCommand;
+
+
+		DECLARE @Items AS ItemCostingTableType
+		
+		INSERT @Items (intItemId, intItemLocationId, dtmDate, dblUnitQty, dblUOMQty, dblCost, dblSalesPrice, intCurrencyId, dblExchangeRate, intTransactionId, strTransactionId, intTransactionTypeId, intLotId)
+		SELECT intItemId = 1, intItemLocationId = 1, dtmDate = GETDATE(), dblUnitQty = 1, dblUOMQty = 1, dblCost = 1.00, dblSalesPrice = 2.00 , intCurrencyId = 1, dblExchangeRate = 1, intTransactionId = 1, strTransactionId = 'TRANSACTION-ID', intTransactionTypeId = 1, intLotId = 1
+	END 
+	
+	-- Test case 1: Test error handling when uspICValidateCostingOnPost raise an error. 
+	BEGIN 
+		-- Act
+		EXEC tSQLt.ExpectException @ExpectedErrorNumber = 50000 
+		EXEC dbo.uspICProcessCosting @ItemsToProcess = @Items, @strBatchId = 'BATCH-XXXX', @ysnPost = 0, @intTransactionId = 1, @intTransactionTypeId = 1;
+	
+		-- Assert
+		-- CALLED
+			-- Check if the Validate Costing on unpost sp is NOT called. 
+			SELECT	@isCalledUspICValidateCostingOnUnpost = 1 
+			FROM	uspICValidateCostingOnUnpost_SpyProcedureLog 
+			WHERE	_id_ = 1
+					AND intTransactionId = 1
+					AND intTransactionTypeId = 1
+				
+			EXEC tSQLt.AssertEquals 1, @isCalledUspICValidateCostingOnUnpost;			
+			
+			-- Check if the Unpost Costing is NOT called 
+			SELECT	@isCalledUspICUnpostCosting = 1 
+			FROM	uspICUnpostCosting_SpyProcedureLog 
+			WHERE	_id_ = 1
+					AND strBatchId = 'BATCH-XXXX'
+					AND intTransactionId = 1
+					AND intTransactionTypeId = 1	
+			
+			EXEC tSQLt.AssertEquals 1, @isCalledUspICUnpostCosting;				
+	
+		-- NOT CALLED
+			-- Check if the validate costing on post is CALLED. 
+			SELECT	@isCalledUspICValidateCostingOnPost = 1 
+			FROM	uspICValidateCostingOnPost_SpyProcedureLog 
+			WHERE	_id_ = 1 
+					AND CAST(ItemsToValidate AS NVARCHAR(MAX)) = (SELECT * FROM @Items FOR XML PATH(''))
+
+			EXEC tSQLt.AssertEquals 0 ,@isCalledUspICValidateCostingOnPost;			
+
+			-- Check if the post costing sp is NOT called. 
+			SELECT	@isCalledUspICPostCosting = 1 
+			FROM	uspICPostCosting_SpyProcedureLog 
+			WHERE	_id_ = 1 
+					AND CAST(ItemsToProcess AS NVARCHAR(MAX)) = (SELECT * FROM @Items FOR XML PATH(''))
+					AND strBatchId = 'BATCH-XXXX'
+
+			EXEC tSQLt.AssertEquals 0, @isCalledUspICPostCosting;
+	END 
+END 
