608ae9317d616630141bf4df6b4a68b93c9a10c7
darwin_estrada <darwin.estrada@irely.com>
2014-10-29 17:25:20 +0800

    AR-424 One time import completed

---
+++ Integration/dbo/Stored Procedures/uspARImportCustomerContacts.sql
@@ -0,0 +1,179 @@
+﻿IF EXISTS(select top 1 1 from sys.procedures where name = 'uspARImportCustomerContacts')
+	DROP PROCEDURE uspARImportCustomerContacts
+GO
+
+CREATE PROCEDURE [dbo].[uspARImportCustomerContacts]
+AS 
+
+BEGIN
+
+-- Temp Table with Autonumber to hold the Contacts to be imported
+DECLARE @Contacts TABLE
+(
+	 id int identity(1,1)
+	, sscon_contact_id nvarchar(max)
+	, sscon_cus_no nvarchar(max)
+	, sscon_email nvarchar(max)
+	, sscon_first_name nvarchar(max)
+	, sscon_last_name nvarchar(max)
+	, sscon_work_no nvarchar(max)
+	, sscon_work_ext nvarchar(max)
+	, sscon_cell_no nvarchar(max)
+	, sscon_cell_ext nvarchar(max))
+
+	
+	BEGIN -- Get Customers to be imported to #tmpContacts
+		INSERT INTO @Contacts
+		(
+			sscon_contact_id
+			, sscon_cus_no 
+			, sscon_email 
+			, sscon_first_name 
+			, sscon_last_name 
+			, sscon_work_no 
+			, sscon_work_ext 
+			, sscon_cell_no 
+			, sscon_cell_ext 
+		)
+		SELECT top 10
+			isnull(sscon_contact_id, '')
+			, isnull(sscon_cus_no, '')
+			, isnull(sscon_email, '')
+			, isnull(sscon_first_name, '')
+			, isnull(sscon_last_name, '')
+			, isnull(sscon_work_no, '')
+			, isnull(sscon_work_ext, '')
+			, isnull(sscon_cell_no, '')
+			, isnull(sscon_cell_ext, '')
+		FROM ssconmst sscon
+		WHERE 
+			sscon_cus_no LIKE '%[a-z0-9]%'
+			AND 
+				(
+					sscon_email not in 
+					(
+						select isnull(strEmail, '') COLLATE SQL_Latin1_General_CP1_CS_AS 
+						from tblEntity E inner join tblEntityContact EC on E.intEntityId = EC.intEntityId
+						WHERE isnull(strEmail, '') like '%[a-z0-9]%' 
+					) 
+					OR isnull(sscon_email, '') = ''
+				)
+			AND sscon_cus_no in (select strCustomerNumber collate SQL_Latin1_General_CP1_CS_AS from tblARCustomer)
+
+	END
+		-- LOOP Insertions
+		WHILE exists (select top 1 1 from @Contacts)
+		BEGIN
+			DECLARE @id int
+			select top 1 @id = id from @Contacts
+
+			BEGIN -- Create entity record for Contacts
+
+				declare @Entity as Entity
+
+				insert @Entity
+				select top 1 
+					sscon_last_name + ', ' + sscon_first_name, sscon_email, '', '' , 0, '', '', '', '', null, null
+				from @Contacts where id = @id
+
+					declare @intEntityId int
+					exec uspEntityCreateEntity @Entity , @intEntityId OUT
+			
+			END
+
+			BEGIN -- Create Contact record
+				declare @EntityContact as EntityContact
+				insert @EntityContact
+				select top 1
+					@intEntityId
+					, sscon_contact_id
+					, ''
+					, ''
+					, sscon_cell_no + ' x' + sscon_cell_ext
+					, sscon_work_no + ' x' + sscon_work_ext
+					, '' , '', '', '', '', ''
+				from @Contacts where id = @id
+
+					declare @intContactId int
+					exec [uspEntityCreateEntityContact] @EntityContact, @intContactId OUT
+			END
+
+			BEGIN -- Create Customer to Contact
+
+				declare @CustomerNo nvarchar(max), @intCustomerId int
+
+				select top 1 @CustomerNo = sscon_cus_no from @Contacts where id = @id
+				select top 1 @intCustomerId = intCustomerId from tblARCustomer where strCustomerNumber = @CustomerNo 
+
+				insert into tblARCustomerToContact
+				(
+					 intCustomerId
+					, intContactId
+					, intEntityLocationId
+					, strUserType
+					, ysnPortalAccess
+				)
+				select
+					@intCustomerId
+					, @intContactId
+					, null
+					, 'User'
+					, 0
+
+			END
+			
+			--TODO1: remove this select and delete tblEntity and tblEntityContact
+				select * from tblEntity where intEntityId = @intEntityId
+				select * from tblEntityContact where intContactId = @intContactId 
+				
+				delete from tblEntityContact where intContactId = @intContactId 
+				delete from tblEntity where intEntityId = @intEntityId
+			-- End of TODO1
+			
+				delete from @Entity
+				delete from @EntityContact 
+				delete from @Contacts where id = @id
+
+		END
+END
+	
+
+GO
+
+
+
+
+--exec [uspARImportCustomerContacts]
+
+
+-- 3. Insert tbl tblARCustomerToContact
+
+
+
+
+--tblEntityContact
+
+---- TEST
+--declare @EntityContact as EntityContact
+--insert @EntityContact
+--select 
+--	1
+--	, 'contactNo'
+--	, 'title'
+--	, 'department'
+--	, 'mobile'
+--	, 'phone'
+--	, 'phone2'
+--	, 'email2'
+--	, 'fax'
+--	, 'notes'
+--	, 'contact method'
+--	, 'timezone'
+
+--declare @intContactId int
+--exec [uspEntityCreateEntityContact] @EntityContact, @intContactId OUT
+--select @intContactId
+--select * from tblEntityContact where intContactId = @intContactId
+
+--delete from tblEntityContact where intContactId = @intContactId
+
---
+++ i21Database/dbo/Stored Procedures/uspEntityCreateEntityContact.sql
@@ -0,0 +1,83 @@
+﻿
+CREATE PROCEDURE [dbo].[uspEntityCreateEntityContact]
+	@EntityContact EntityContact READONLY
+	,@ContactId int = null OUT
+AS 
+
+BEGIN
+	
+			
+	BEGIN TRANSACTION;	
+		BEGIN TRY
+
+			BEGIN -- Validation: only one entity at a time
+				IF (select count(*) from @EntityContact) > 1 
+				BEGIN
+					RAISERROR('Insertion of more than one contact not allowed', 16,1)
+				END
+			END
+
+			set @ContactId = NULL
+
+			BEGIN -- DML EntityContact
+				insert into tblEntityContact
+				(
+					intEntityId
+					,strTitle
+					,strDepartment
+					,strMobile
+					,strPhone
+					,strPhone2
+					,strEmail2
+					,strFax
+					,strNotes
+					,strContactMethod
+					,strTimezone
+					,strContactNumber
+				)
+				select
+					EntityId
+					,Title
+					,Department
+					,Mobile
+					,Phone
+					,Phone2
+					,Email2
+					,Fax
+					,Notes
+					,ContactMethod
+					,Timezone
+					,ContactNumber					
+				from @EntityContact
+			END
+
+			set @ContactId = SCOPE_IDENTITY()
+		END TRY
+
+		BEGIN CATCH
+			IF @@TRANCOUNT > 0
+				ROLLBACK TRANSACTION;
+			--THROW;
+			-- all the code below will be obsolete (but will still work) with SQL2012
+			-- a single THROW will throw this error
+			    DECLARE @ErrorMessage NVARCHAR(4000);
+				DECLARE @ErrorSeverity INT;
+				DECLARE @ErrorState INT;
+
+				SELECT 
+					@ErrorMessage = ERROR_MESSAGE(),
+					@ErrorSeverity = ERROR_SEVERITY(),
+					@ErrorState = ERROR_STATE();
+
+				RAISERROR (@ErrorMessage, -- Message text.
+						   @ErrorSeverity, -- Severity.
+						   @ErrorState -- State.
+						   );
+		END CATCH
+
+		IF @@TRANCOUNT > 0
+			COMMIT TRANSACTION;			
+			
+END
+
+GO
