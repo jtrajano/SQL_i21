d2779ca7dbf1758488c12c3a2e4d22224c84ccb3
Feb Montefrio <feb.montefrio@irely.com>
2014-10-30 16:24:13 +0800

    IC-100 Add a new UDTT (User-defined table type) called ItemGLAccount Add a new stored procedure: uspICProcessMovingAverageCost Refactor uspICPostCosting. It is the initial approach... more will be changed in the next commit.

--- i21Database/dbo/Stored Procedures/uspICPostCosting.sql
+++ i21Database/dbo/Stored Procedures/uspICPostCosting.sql
@@ -1,42 +1,123 @@
-﻿CREATE PROCEDURE [dbo].[uspICPostCosting]
+﻿
+/*
+	It process each the items submitted for costing. There is cursor used in this procedure. 
+	It iterates over the list of items in @ItemsToProcess. 
+
+	The each iteration, it does the following: 
+		1. Determines if a stock is for incoming or outgoing then calls the appropriate stored procedure. 
+		2. Determines the type of inventory transaction it is, whether an in, out, or cost adjustment. 
+*/
+
+CREATE PROCEDURE [dbo].[uspICPostCosting]
 	@ItemsToProcess AS ItemCostingTableType READONLY
 	,@strBatchId AS NVARCHAR(20)
 AS
 
--- TODO: Loop for each item to post. 
+SET QUOTED_IDENTIFIER OFF
+SET ANSI_NULLS ON
+SET NOCOUNT ON
+SET XACT_ABORT ON
+SET ANSI_WARNINGS OFF
 
-	-- TODO: If item is for receiving, call the Process In-Stock
+-- Declare the variables to use for the cursor
+DECLARE @intItemId AS INT
+		,@intItemLocationId AS INT 
+		,@dtmDate AS DATETIME
+		,@dblUnitQty AS NUMERIC(18, 6) 
+		,@dblUOMQty AS NUMERIC(18, 6)
+		,@dblCost AS NUMERIC(18, 6)
+		,@dblSalesPrice AS NUMERIC(18, 6)
+		,@intCurrencyId AS INT 
+		,@dblExchangeRate AS DECIMAL (38, 20) 
+		,@intTransactionId AS INT
+		,@strTransactionId AS NVARCHAR(40) 
+		,@intTransactionTypeId AS INT 
+		,@intLotId AS INT
 
-	-- TODO: If item is for selling or outgoing, call the Process Out-Stock
+DECLARE @CostingMethod AS INT 
+DECLARE @NegativeInventoryOption AS INT
+DECLARE @GLAccounts AS ItemGLAccount
 
-	-- TODO: If item is for cost adjustment, call Process Cost Adjustment
+-- Create the cursor
+-- Use LOCAL. It specifies that the scope of the cursor is local to the stored procedure where it was created. The cursor name is only valid within this scope. 
+-- Use FAST_FORWARD. It specifies a FORWARD_ONLY, READ_ONLY cursor with performance optimizations enabled. 
+DECLARE loopItems CURSOR LOCAL FAST_FORWARD
+FOR 
+SELECT  intItemId
+		,intItemLocationId
+		,dtmDate
+		,dblUnitQty
+		,dblUOMQty
+		,dblCost
+		,dblSalesPrice
+		,intCurrencyId
+		,dblExchangeRate
+		,intTransactionId
+		,strTransactionId
+		,intTransactionTypeId
+		,intLotId
+FROM	@ItemsToProcess
 
--- TODO: End Loop 
+OPEN loopItems;
+
+-- Initial fetch attempt
+FETCH NEXT FROM loopItems INTO @intItemId, @intItemLocationId, @dtmDate, @dblUnitQty, @dblUOMQty, @dblCost, @dblSalesPrice, @intCurrencyId, @dblExchangeRate, @intTransactionId, @strTransactionId, @intTransactionTypeId, @intLotId;
+
+-- The loop
+WHILE @@FETCH_STATUS = 0
+BEGIN 
+	-- Initialize the costing method and negative inventory option. 
+	SET @CostingMethod = NULL;
+	SET @NegativeInventoryOption = NULL;
+	DELETE FROM @GLAccounts;
+
+	-- Get the costing method of an item and the negative stock option
+	SELECT @CostingMethod = dbo.fnGetCostingMethod(@intItemId, @intItemLocationId)
+			,@NegativeInventoryOption = dbo.fnGetNegativeInventoryOptions(@intItemId, @intItemLocationId);
+
+	-- Get the g/l accounts for the item based on its item level setup, category setup, or location setup. 
+	INSERT INTO @GLAccounts (Inventory, Sales, Purchases, COGS)	
+	SELECT	Inventory, Sales, Purchases, NULL 
+	FROM	dbo.fnGetItemGLAccounts(@intItemId, @intItemLocationId); 
+
+	-- Moving Average Cost
+
+	-- FIFO
+
+	-- LIFO
+
+
+	-- Attempt to fetch the next row from cursor. 
+	FETCH NEXT FROM loopItems INTO @intItemId, @intItemLocationId, @dtmDate, @dblUnitQty, @dblUOMQty, @dblCost, @dblSalesPrice, @intCurrencyId, @dblExchangeRate, @intTransactionId, @strTransactionId, @intTransactionTypeId, @intLotId;
+END;
+
+CLOSE loopItems;
+DEALLOCATE loopItems;
 
 
 -- Generate the GL entries
---SELECT	[strTransactionId]		= A.strTransactionId
---		,[intTransactionId]		= A.intTransactionId
---		,[dtmDate]				= A.dtmDate
---		,[strBatchId]			= @strBatchId
---		,[intAccountId]			= 
---		,[dblDebit]				= 
---		,[dblCredit]			= 
---		,[dblDebitUnit]			= 
---		,[dblCreditUnit]		= 
---		,[strDescription]		= 
---		,[strCode]				= 
---		,[strReference]			= 
---		,[intCurrencyId]		= 
---		,[dblExchangeRate]		= 
---		,[dtmDateEntered]		= GETDATE()
---		,[dtmTransactionDate]	= A.dtmDate
---		,[strJournalLineDescription] = NULL 
---		,[ysnIsUnposted]		= 0 
---		,[intConcurrencyId]		= 1
---		,[intUserId]			= 
---		,[strTransactionForm]	= @TRANSACTION_FORM
---		,[strModuleName]		= @MODULE_NAME
---		,[intEntityId]			= A.intEntityId
+--SELECT	[strTransactionId		= A.strTransactionId
+--		,@intTransactionId		= A.intTransactionId
+--		,@dtmDate				= A.dtmDate
+--		,@strBatchId			= @strBatchId
+--		,@intAccountId			= 
+--		,@dblDebit				= 
+--		,@dblCredit			= 
+--		,@dblDebitUnit			= 
+--		,@dblCreditUnit		= 
+--		,@strDescription		= 
+--		,@strCode				= 
+--		,@strReference			= 
+--		,@intCurrencyId		= 
+--		,@dblExchangeRate		= 
+--		,@dtmDateEntered		= GETDATE()
+--		,@dtmTransactionDate	= A.dtmDate
+--		,@strJournalLineDescription = NULL 
+--		,@ysnIsUnposted		= 0 
+--		,@intConcurrencyId		= 1
+--		,@intUserId			= 
+--		,@strTransactionForm	= @TRANSACTION_FORM
+--		,@strModuleName		= @MODULE_NAME
+--		,@intEntityId			= A.intEntityId
 --FROM	tblICInventoryTransaction A INNER JOIN @ItemsForProcessing B
 --			ON A.intTransactionId = B.intTransactionId
---
+++ i21Database/dbo/User Defined Types/ItemGLAccount.sql
@@ -0,0 +1,11 @@
+﻿/*
+	This is a user-defined table type used to hold all the g/l account used in item costing. 
+*/
+
+CREATE TYPE [dbo].[ItemGLAccount] AS TABLE
+(
+	[Inventory] INT NULL
+	,[Sales] INT NULL
+	,[Purchases] INT NULL
+	,[COGS] INT NULL
+)
