4dbc8f54a49e69a6c0684cd1483c582c00dbf7e9
rufil cabangal <rufil.cabangal@irely.com>
2014-11-03 20:41:59 +0800

    TS-458

--- tests-ui/FeedStockUOM/AddFeedStockUOM.js
+++ tests-ui/FeedStockUOM/AddFeedStockUOM.js
@@ -4,12 +4,7 @@
 /**
  * Created by RQuidato on 10/30/14.
  */
-/**
- * Created by RQuidato on 10/30/14.
- */
-/**
- * Created by RQuidato on 10/29/14.
- */
+
 StartTest (function (t) {
 
     var engine = new iRely.TestEngine();
@@ -26,8 +21,8 @@ StartTest (function (t) {
         .checkScreenShown ('feedstockuom').wait(100)
         .checkScreenWindow({alias: 'feedstockuom', title: 'Feed Stock UOM', collapse: true, maximize: true, minimize: false, restore: false, close: true })
         .checkToolbarButton({new: false, save: true, search: false, delete: false, undo: true, close: true})
-        .checkControlVisible(['#colUOM', '#colUOMCode' ], true)
-        .checkControlVisible('#btnDeleteFeedStockUom', true)
+        .checkControlVisible(['#colUOM', '#colRinFeedStockUOMCode' ], true)
+        .checkControlVisible('#btnDelete', true)
         .checkControlVisible(['#btnHelp', '#btnSupport', '#btnFieldName'], true)
         .checkStatusBar()
         .checkStatusMessage('Ready')
@@ -35,10 +30,8 @@ StartTest (function (t) {
 
         /* 2. Multiple Data entry */
         .addFunction(function(next){t.diag("Scenario 1. Add new records > 2. Multiple Data entry"); next();}).wait(100)
-        .enterGridData('#grdFeedStockUom', 0, 'colUOM', 'u01').wait(100)
-        .enterGridData('#grdFeedStockUom', 0, 'colUOMCode', 'uom code 01').wait(100)
-        .enterGridData('#grdFeedStockUom', 1, 'colUOM', 'u02').wait(100)
-        .enterGridData('#grdFeedStockUom', 1, 'colUOMCode', 'uom code 02').wait(100)
+        .selectGridComboRowByFilter('#grdGridTemplate',0, 'collUOM','gram').wait(100)
+        .selectGridComboRowByFilter('#grdGridTemplate',1, 'colUOM','kilogram').wait(100)
         .checkStatusMessage('Edited')
         .clickButton('#btnSave').wait(500)
         .checkStatusMessage('Saved').wait(100)
@@ -50,76 +43,76 @@ StartTest (function (t) {
 //        .addFunction(function(next){t.diag("Scenario 1. Add new records > 3. Verify record added"); next();}).wait(100)
 //        .openScreen('Feed Stock UOM').wait(200)
 //        .checkScreenShown ('feedstockuom').wait(100)
-//        .checkGridData('#grdFeedStockUom', 0, 'colUOM','u01').wait(100)
-//        .checkGridData('#grdFeedStockUom', 0, 'colUOMCode','uom code 01').wait(100)
-//        .checkGridData('#grdFeedStockUom', 1, 'colUOM','u02').wait(100)
-//        .checkGridData('#grdFeedStockUom', 1, 'colUOMCode','uom code 02').wait(100)
+//        .checkGridData('#grdGridTemplate', 0, 'colUOM','u01').wait(100)
+//        .checkGridData('#grdGridTemplate', 0, 'colRinFeedStockUOMCode','uom code 01').wait(100)
+//        .checkGridData('#grdGridTemplate', 1, 'colUOM','u02').wait(100)
+//        .checkGridData('#grdGridTemplate', 1, 'colRinFeedStockUOMCode','uom code 02').wait(100)
 //
 
-        /* Scenario 2. Add another record, click Close button, do NOT save the changes */
-        /* 1. Data entry. */
-        .addFunction(function(next){t.diag("Scenario 2. Add another record, click Close button, do NOT save the changes"); next();}).wait(100)
-        .openScreen('Feed Stock UOM').wait(200)
-        .checkScreenShown ('feedstockuom').wait(100)
-        .enterGridData('#grdFeedStockUom', 2, 'colUOM', 'u03').wait(100)
-        .enterGridData('#grdFeedStockUom', 2, 'colUOMCode', 'uom code 03').wait(100)
-        .clickButton('#btnClose').wait(100)
-        .checkMessageBox('iRely i21','Do you want to save the changes you made?','yesnocancel', 'question')
-//        .checkControlVisible(['#colFeedStockCode', '#colDescription' ], true)
-        .clickMessageBoxButton('no').wait(10)
-        .clickButton('#btnClose').wait(100)
-        .checkIfScreenClosed('feedstockuom').wait(100)
+//        /* Scenario 2. Add another record, click Close button, do NOT save the changes */
+//        /* 1. Data entry. */
+//        .addFunction(function(next){t.diag("Scenario 2. Add another record, click Close button, do NOT save the changes"); next();}).wait(100)
+//        .openScreen('Feed Stock UOM').wait(200)
+//        .checkScreenShown ('feedstockuom').wait(100)
+//        .enterGridData('#grdGridTemplate', 2, 'colUOM', 'pounds').wait(100)
+////        .enterGridData('#grdGridTemplate', 2, 'colRinFeedStockUOMCode', 'uom code 03').wait(100)
+//        .clickButton('#btnClose').wait(100)
+//        .checkMessageBox('iRely i21','Do you want to save the changes you made?','yesnocancel', 'question')
+////        .checkControlVisible(['#colFeedStockCode', '#colDescription' ], true)
+//        .clickMessageBoxButton('no').wait(10)
+//        .clickButton('#btnClose').wait(100)
+//        .checkIfScreenClosed('feedstockuom').wait(100)
 
 
 
-        /* Scenario 3. Add another record, click Close button, Cancel*/
-        /* 1. Data entry. */
-        .addFunction(function(next){t.diag("Scenario 3. Add another record, click Close button, Cancel"); next();}).wait(100)
-        .openScreen('Feed Stock UOM').wait(200)
-        .checkScreenShown ('feedstockuom').wait(100)
-        .enterGridData('#grdFeedStockUom', 2, 'colUOM', 'u03').wait(100)
-        .enterGridData('#grdFeedStockUom', 2, 'colUOMCode', 'uom code 03').wait(100)
-        .clickButton('#btnClose').wait(100)
-        .checkMessageBox('iRely i21','Do you want to save the changes you made?','yesnocancel', 'question')
-        .clickMessageBoxButton('cancel').wait(10)
-        .clickButton('#btnClose').wait(100)
-
-
-        /* Scenario 4. Add another record, click Close button, SAVE the changes*/
-        .addFunction(function(next){t.diag("Scenario 4. Add another record, click Close button, SAVE the changes"); next();}).wait(100)
-        .checkMessageBox('iRely i21','Do you want to save the changes you made?','yesnocancel', 'question')
-        .clickMessageBoxButton('yes').wait(500)
-        .checkIfScreenClosed('feedstockuom').wait(100) /*issue - FRM-1547*/
+//        /* Scenario 3. Add another record, click Close button, Cancel*/
+//        /* 1. Data entry. */
+//        .addFunction(function(next){t.diag("Scenario 3. Add another record, click Close button, Cancel"); next();}).wait(100)
+//        .openScreen('Feed Stock UOM').wait(200)
+//        .checkScreenShown ('feedstockuom').wait(100)
+//        .enterGridData('#grdGridTemplate', 2, 'colUOM', 'pounds').wait(100)
+////        .enterGridData('#grdGridTemplate', 2, 'colRinFeedStockUOMCode', 'uom code 03').wait(100)
+//        .clickButton('#btnClose').wait(100)
+//        .checkMessageBox('iRely i21','Do you want to save the changes you made?','yesnocancel', 'question')
+//        .clickMessageBoxButton('cancel').wait(10)
+//        .clickButton('#btnClose').wait(100)
+//
+//
+//        /* Scenario 4. Add another record, click Close button, SAVE the changes*/
+//        .addFunction(function(next){t.diag("Scenario 4. Add another record, click Close button, SAVE the changes"); next();}).wait(100)
+//        .checkMessageBox('iRely i21','Do you want to save the changes you made?','yesnocancel', 'question')
+//        .clickMessageBoxButton('yes').wait(500)
+//        .checkIfScreenClosed('feedstockuom').wait(100) /*issue - FRM-1547*/
 //
 //        /* 2. Verify record not added*/
 //        .addFunction(function(next){t.diag("Scenario 2. Add another record, click Close button, do NOT save the changes > 2. Verify record added"); next();}).wait(100)
 //        .openScreen('Feed Stock UOM').wait(200)
 //        .checkScreenShown ('feedstockuom').wait(100)
-//        .checkGridData('#grdFeedStockUom', 2, 'colUOM','u03')
-//        .checkGridData('#grdFeedStockUom', 2, 'colUOMCode','uom code 03')
+//        .checkGridData('#grdGridTemplate', 2, 'colUOM','pounds')
+//        .checkGridData('#grdGridTemplate', 2, 'colRinFeedStockUOMCode','uom code 03')
 //
 //
 //
-        /*Scenario 5. Add duplicate record > FuelCode*/
-        .addFunction(function(next){t.diag("Scenario 5. Add duplicate record > FuelTypeCode"); next();}).wait(100)
-        .openScreen('Feed Stock UOM').wait(200)
-        .checkScreenShown ('feedstockuom').wait(100)
-        .enterGridData('#grdFeedStockUom', 3, 'colUOM', 'u03').wait(100)
-        .enterGridData('#grdFeedStockUom', 3, 'colUOMCode', 'uom code 04').wait(100)
-        .clickButton('#btnSave').wait(100)
-        .checkMessageBox('iRely i21','Fuel Category already exists.','ok','error') /* IC-84 */
-        .clickMessageBoxButton('ok').wait(10)
+//        /*Scenario 5. Add duplicate record > FuelCode*/
+//        .addFunction(function(next){t.diag("Scenario 5. Add duplicate record > FuelTypeCode"); next();}).wait(100)
+//        .openScreen('Feed Stock UOM').wait(200)
+//        .checkScreenShown ('feedstockuom').wait(100)
+//        .enterGridData('#grdGridTemplate', 3, 'colUOM', 'u03').wait(100)
+//        .enterGridData('#grdGridTemplate', 3, 'colRinFeedStockUOMCode', 'uom code 04').wait(100)
+//        .clickButton('#btnSave').wait(100)
+//        .checkMessageBox('iRely i21','Fuel Category already exists.','ok','error') /* IC-84 */
+//        .clickMessageBoxButton('ok').wait(10)
+//
+//        /*Scenario 6. Modify duplicate ProcessCode to correct it*/
+//        .addFunction(function(next){t.diag("Scenario 6. Modify duplicate ProcessCode to correct it"); next();}).wait(100)
+//        .enterGridData('#grdGridTemplate', 3, 'colUOM', 'u04').wait(100) /* IC-81 */
+//        .clickButton('#closeButton').wait(100)
+//        .checkMessageBox('iRely i21','Do you want to save the changes you made?','yesnocancel', 'question')
+//        .clickMessageBoxButton('yes').wait(500)
+//        .checkIfScreenClosed('feedstockuom').wait(100)
 
-        /*Scenario 6. Modify duplicate ProcessCode to correct it*/
-        .addFunction(function(next){t.diag("Scenario 6. Modify duplicate ProcessCode to correct it"); next();}).wait(100)
-        .enterGridData('#grdFeedStockUom', 3, 'colUOM', 'u04').wait(100) /* IC-81 */
-        .clickButton('#closeButton').wait(100)
-        .checkMessageBox('iRely i21','Do you want to save the changes you made?','yesnocancel', 'question')
-        .clickMessageBoxButton('yes').wait(500)
-        .checkIfScreenClosed('feedstockuom').wait(100)
 
-
-//        .enterDummyRowData('#grdFeedStockUom', [{ column: 'strRinFuelTypeCode', data: '10Et'}, {column: 'strDescription', data: '10-Ethanol'}]) /*issue - FRM-1535*/
+//        .enterDummyRowData('#grdGridTemplate', [{ column: 'strRinFuelTypeCode', data: '10Et'}, {column: 'strDescription', data: '10-Ethanol'}]) /*issue - FRM-1535*/
 
 
 
