f0c19890d5d741102a6bac397575a7b5754a187a
Lawrence Zabala <lawrence.zabala@irely.com>
2014-11-06 18:55:56 +0800

    IC-122

--- app/view/InventoryTagViewController.js
+++ app/view/InventoryTagViewController.js
@@ -17,7 +17,68 @@ Ext.define('Inventory.view.InventoryTagViewController', {
     extend: 'Ext.app.ViewController',
     alias: 'controller.inventorytag',
 
-    requires: [
-        'Inventory.view.override.InventoryTagViewController'
-    ]
+    config: {
+        searchConfig: {
+            title:  'Search Inventory Tag',
+            type: 'Inventory.Tag',
+            api: {
+                read: '../Inventory/api/Tag/SearchTags'
+            },
+            columns: [
+                {dataIndex: 'intTagId',text: "Tag Id", flex: 1, defaultSort:true, dataType: 'numeric', key: true, hidden: true},
+                {dataIndex: 'strTagNumber', text: 'Tag Number', flex: 1,  dataType: 'string'},
+                {dataIndex: 'strDescription', text: 'Description', flex: 1,  dataType: 'string'},
+                {dataIndex: 'ysnHazMat',text: 'HAZMAT', flex: 1,  dataType: 'boolean', xtype: 'checkcolumn'}
+            ]
+        },
+        binding: {
+            txtTagNumber: '{current.strTagNumber}',
+            txtDescription: '{current.strDescription}',
+            chkHAZMATMessage: '{current.ysnHazMat}',
+            txtMessage: '{current.strMessage}'
+        }
+    },
+
+    setupContext : function(options){
+        "use strict";
+        var me = this,
+            win = options.window,
+            store = Ext.create('Inventory.store.InventoryTag', { pageSize: 1 });
+
+        win.context = Ext.create('iRely.mvvm.Engine', {
+            window : win,
+            store  : store,
+            createRecord : me.createRecord,
+            binding: me.config.binding
+        });
+
+        return win.context;
+    },
+
+    show : function(config) {
+        "use strict";
+
+        var me = this,
+            win = this.getView();
+
+        if (config) {
+            win.show();
+
+            var context = me.setupContext( {window : win} );
+
+            if (config.action === 'new') {
+                context.data.addRecord();
+            } else {
+                if (config.id) {
+                    config.filters = [{
+                        column: 'intTagId',
+                        value: config.id
+                    }];
+                }
+                context.data.load({
+                    filters: config.filters
+                });
+            }
+        }
+    }
 });
