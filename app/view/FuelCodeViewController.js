a5d267231a0ed406cd66e8fbcb505a05e0d322e4
Lawrence Zabala <lawrence.zabala@irely.com>
2014-11-06 15:54:54 +0800

    IC-119

--- app/view/FuelCodeViewController.js
+++ app/view/FuelCodeViewController.js
@@ -25,7 +25,7 @@ Ext.define('Inventory.view.FuelCodeViewController', {
             store: Ext.create('Inventory.store.FuelCode'),
             singleGridMgr: Ext.create('iRely.mvvm.grid.Manager', {
                 grid: win.down('grid'),
-                title: 'Fuel',
+                title: 'Fuel Code',
                 columns: [
                     {
                         itemId: 'colRinFuelCode',
