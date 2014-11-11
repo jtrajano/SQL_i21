02d6f3b5f6309999b569ef2cfdca55e9f48dd7c2
Dan Michael Jalimao <Dan.Jalimao@irely.com>
2014-11-11 15:57:12 +0800

    FRM-1590

--- i21Database/dbo/Tables/tblSMPaymentMethod.sql
+++ i21Database/dbo/Tables/tblSMPaymentMethod.sql
@@ -2,7 +2,7 @@
     [intPaymentMethodID] INT            IDENTITY (1, 1) NOT NULL,
     [strPaymentMethod]   NVARCHAR (100) COLLATE Latin1_General_CI_AS NOT NULL,
     [ysnActive]          BIT            DEFAULT ((1)) NOT NULL,
-    [intSort]            INT            NULL,
+    [intSort]            INT            NOT NULL DEFAULT 0,
     [intConcurrencyId] INT NOT NULL DEFAULT (1), 
     CONSTRAINT [PK_tblSMPaymentMethod] PRIMARY KEY CLUSTERED ([intPaymentMethodID] ASC)
 );
