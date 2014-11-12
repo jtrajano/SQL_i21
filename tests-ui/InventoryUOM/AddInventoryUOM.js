bcd312f1039a3a631d8cf17f6fd626b9200f4c42
rufil cabangal <rufil.cabangal@irely.com>
2014-11-12 20:04:15 +0800

    TS-477

--- tests-ui/InventoryUOM/AddInventoryUOM.js
+++ tests-ui/InventoryUOM/AddInventoryUOM.js
@@ -1,7 +1,6 @@
 /**
  * Created by RQuidato on 10/30/14.
  */
-
 StartTest (function (t) {
 
     var engine = new iRely.TestEngine();
@@ -9,121 +8,185 @@ StartTest (function (t) {
 
         /*Scenario 1. Add new records */
         /* 1. Open screen and check default controls' state */
-        .login('ssiadmin','summit','eo').wait(1500)
+        .login('ssiadmin','summit','ag').wait(1500)
         .addFunction(function(next){t.diag("Scenario 1. Add new records > 1. Open screen and check default controls' state"); next();}).wait(100)
         .expandMenu('Inventory').wait(100)
         .expandMenu('Maintenance').wait(200)
         .openScreen('Inventory UOM').wait(200)
         .checkScreenShown ('inventoryuom').wait(100)
-        .checkScreenWindow({alias: 'inventoryuom', title: 'Inventory UOM', collapse: true, maximize: true, minimize: false, restore: false, close: true }).wait(100)
-        .checkToolbarButton({new: true, save: true, search: true, delete: true, undo: true, close: true}).wait(100) /*issue - IC-20*/
-        /*Control checking*/
-        .checkControlVisible([
-            '#txtUnitMeasure',
-            '#txtSymbol',
-            '#cboUnitType',
-            '#chkDefault',
-            ], true)
-        .checkControlVisible(['#btnDeleteConversion', '#txtFilterGrid'],true) /* x button on filter does not have item id*/
+        .checkScreenWindow({alias: 'inventoryuom',title: 'Inventory UOM',collapse: true,maximize: true,minimize: false,restore: false,close: true})
+        .checkToolbarButton({new: false, save: true, search: false, delete: false, undo: true, close: true})
+        .checkControlVisible(['#txtUnitMeasure', '#txtSymbol', '#cboUnitType', '#chkDefault'], true)
+        .checkControlVisible(['#btnDeleteConversion', '#txtFilterGrid'], true)
+        .checkControlVisible(['#colConversionStockUOM', '#colConversionToStockUOM', '#colConversionFromStockUOM'], true)
         .checkControlVisible(['#btnHelp', '#btnSupport', '#btnFieldName'], true)
-        .checkControlVisible(['#colConversionStockUOM','#colConversionToStockUOM','#colConversionFromStockUOM'],true)
         .checkStatusBar()
         .checkStatusMessage('Ready')
-        .checkControlVisible(['#first','#prev','#inputItem','#next','#last','#refresh'],true)
-        //        /*label checking*/ not yet available. Liz asked for this method be done on test framework. will continue this once available.
-//        .checkFieldLabel('#cboFuelType', 'Fuel Type')
+        .checkControlVisible(['#first', '#prev', '#inputItem', '#next', '#last', '#refresh'], true)
 
-//
-//        /* 2. Data entry */
-//        .addFunction(function(next){t.diag("Scenario 1. Add new records > 2. Data entry"); next();}).wait(100)
-//        .selectComboRowByFilter()
-//        .enterGridData('#grdFeedStockUom', 0, 'colUOM', 'u01').wait(100)
-//        .enterGridData('#grdFeedStockUom', 0, 'colUOMCode', 'uom code 01').wait(100)
-//        .enterGridData('#grdFeedStockUom', 1, 'colUOM', 'u02').wait(100)
-//        .enterGridData('#grdFeedStockUom', 1, 'colUOMCode', 'uom code 02').wait(100)
-//        .checkStatusMessage('Edited')
-//        .clickButton('#btnSave').wait(500)
-//        .checkStatusMessage('Saved').wait(100)
-//        .clickButton('#btnClose').wait(200)
-//        .checkIfScreenClosed('feedstockuom').wait(100)
 
-//
-////        /* 3. Verify record added*/
-////        .addFunction(function(next){t.diag("Scenario 1. Add new records > 3. Verify record added"); next();}).wait(100)
-////        .openScreen('Feed Stock UOM').wait(200)
-////        .checkScreenShown ('feedstockuom').wait(100)
-////        .checkGridData('#grdFeedStockUom', 0, 'colUOM','u01').wait(100)
-////        .checkGridData('#grdFeedStockUom', 0, 'colUOMCode','uom code 01').wait(100)
-////        .checkGridData('#grdFeedStockUom', 1, 'colUOM','u02').wait(100)
-////        .checkGridData('#grdFeedStockUom', 1, 'colUOMCode','uom code 02').wait(100)
-////
-//
-//        /* Scenario 2. Add another record, click Close button, do NOT save the changes */
-//        /* 1. Data entry. */
-//        .addFunction(function(next){t.diag("Scenario 2. Add another record, click Close button, do NOT save the changes"); next();}).wait(100)
-//        .openScreen('Feed Stock UOM').wait(200)
-//        .checkScreenShown ('feedstockuom').wait(100)
-//        .enterGridData('#grdFeedStockUom', 2, 'colUOM', 'u03').wait(100)
-//        .enterGridData('#grdFeedStockUom', 2, 'colUOMCode', 'uom code 03').wait(100)
-//        .clickButton('#btnClose').wait(100)
-//        .checkMessageBox('iRely i21','Do you want to save the changes you made?','yesnocancel', 'question')
-////        .checkControlVisible(['#colFeedStockCode', '#colDescription' ], true)
-//        .clickMessageBoxButton('no').wait(10)
-//        .clickButton('#btnClose').wait(100)
-//        .checkIfScreenClosed('feedstockuom').wait(100)
-//
-//
-//
-//        /* Scenario 3. Add another record, click Close button, Cancel*/
-//        /* 1. Data entry. */
-//        .addFunction(function(next){t.diag("Scenario 3. Add another record, click Close button, Cancel"); next();}).wait(100)
-//        .openScreen('Feed Stock UOM').wait(200)
-//        .checkScreenShown ('feedstockuom').wait(100)
-//        .enterGridData('#grdFeedStockUom', 2, 'colUOM', 'u03').wait(100)
-//        .enterGridData('#grdFeedStockUom', 2, 'colUOMCode', 'uom code 03').wait(100)
-//        .clickButton('#btnClose').wait(100)
-//        .checkMessageBox('iRely i21','Do you want to save the changes you made?','yesnocancel', 'question')
-//        .clickMessageBoxButton('cancel').wait(10)
-//        .clickButton('#btnClose').wait(100)
-//
-//
-//        /* Scenario 4. Add another record, click Close button, SAVE the changes*/
-//        .addFunction(function(next){t.diag("Scenario 4. Add another record, click Close button, SAVE the changes"); next();}).wait(100)
-//        .checkMessageBox('iRely i21','Do you want to save the changes you made?','yesnocancel', 'question')
-//        .clickMessageBoxButton('yes').wait(500)
-//        .checkIfScreenClosed('feedstockuom').wait(100) /*issue - FRM-1547*/
-////
-////        /* 2. Verify record not added*/
-////        .addFunction(function(next){t.diag("Scenario 2. Add another record, click Close button, do NOT save the changes > 2. Verify record added"); next();}).wait(100)
-////        .openScreen('Feed Stock UOM').wait(200)
-////        .checkScreenShown ('feedstockuom').wait(100)
-////        .checkGridData('#grdFeedStockUom', 2, 'colUOM','u03')
-////        .checkGridData('#grdFeedStockUom', 2, 'colUOMCode','uom code 03')
-////
-////
-////
-//        /*Scenario 5. Add duplicate record > FuelCode*/
-//        .addFunction(function(next){t.diag("Scenario 5. Add duplicate record > FuelTypeCode"); next();}).wait(100)
-//        .openScreen('Feed Stock UOM').wait(200)
-//        .checkScreenShown ('feedstockuom').wait(100)
-//        .enterGridData('#grdFeedStockUom', 3, 'colUOM', 'u03').wait(100)
-//        .enterGridData('#grdFeedStockUom', 3, 'colUOMCode', 'uom code 04').wait(100)
+        /* 2. Data entry */
+        .addFunction(function(next){t.diag("Scenario 1. Add new records > 2. Data entry"); next();}).wait(100)
+        .enterData('#txtUnitMeasure','gram').wait(100)
+        .enterData('#txtSymbol','g').wait(100)
+//        .selectComboRowByIndex('cboUnitType',5)
+        .clickCheckBox('#chkDefault')
+        .checkStatusMessage('Edited')
+        .clickButton('#btnSave').wait(100)
+        .checkStatusMessage('Saved').wait(100)
+        .clickButton('#btnClose').wait(100)
+        .checkIfScreenClosed('inventoryuom').wait(100)
+
+
+        /* 3. Verify record added*/
+        .addFunction(function(next){t.diag("Scenario 1. Add new records > 3. Verify records added"); next();}).wait(100)
+        .openScreen('Inventory UOM').wait(200)
+        .addFunction(function(next){t.diag("Scenario 1. Add new records > 3.a Check Search screen "); next();}).wait(100)
+        .checkScreenShown ('search').wait(200)
+        .checkScreenWindow({alias: 'search', title: 'Search Inventory UOMs', collapse: true, maximize: true, minimize: false, restore: false, close: true })
+        .checkToolbarButton({new: true, save: false, search: false, delete: false, undo: false, close: true})
+        .checkControlVisible(['#btnOpenSelected', '#btnRefresh'], true)
+//        no checking if correct icon is used
+//        no checking if button label is correct
+        .checkControlVisible(['#btnInsertCriteria', '#txtFilterGrid','#lblTotalRecords'],true)
+        .selectSearchRowByFilter('gram')
+        .clickButton('#btnOpenSelected').wait(100)
+        .addFunction(function(next){t.diag("3.a Opens selected record = Passed"); next();}).wait(100)
+        .checkControlData('#txtUnitMeasure','gram')
+        .checkControlData('#txtSymbol','g')
+        .checkCheckboxValue('#chkDefault',true)
+//         need to add select combo box
+        .checkStatusMessage('Ready')
+        .addFunction(function(next){t.diag("3.b Correct record is selected and data entered is intact = Passed"); next();}).wait(100)
+        .clickButton('#btnClose').wait(100)
+        .checkIfScreenClosed('inventoryuom').wait(100)
+
+
+        /* Scenario 2. Add another record, click Close button, do NOT save the changes */
+        /* 1. Data entry. */
+        .addFunction(function(next){t.diag("Scenario 2. Add another record, click Close button, do NOT save the changes"); next();}).wait(100)
+        .openScreen('Inventory UOM').wait(200)
+        .checkScreenShown ('search').wait(200)
+        .checkScreenWindow({alias: 'search', title: 'Search Inventory UOMs', collapse: true, maximize: true, minimize: false, restore: false, close: true })
+        .clickButton('#btnNew').wait(100)
+        .enterData('#txtUnitMeasure','hour').wait(100)
+        .enterData('#txtSymbol','hr').wait(100)
+        //        .selectComboRowByIndex('cboUnitType',5)
+        .clickButton('#btnClose').wait(100)
+        .checkMessageBox('iRely i21','Do you want to save the changes you made?','yesnocancel', 'question')
+        .clickMessageBoxButton('no').wait(10)
+        .clickButton('#btnClose').wait(100)
+        .checkIfScreenClosed('inventoryuom').wait(100)
+
+
+        /* Scenario 3. Add another record, click Close button, Cancel*/
+        /* 1. Data entry. */
+        .addFunction(function(next){t.diag("Scenario 3. Add another record, click Close button, Cancel"); next();}).wait(100)
+        .openScreen('Inventory UOM').wait(200)
+        .checkScreenShown ('search').wait(200)
+        .checkScreenWindow({alias: 'search', title: 'Search Inventory UOMs', collapse: true, maximize: true, minimize: false, restore: false, close: true })
+        .clickButton('#btnNew').wait(100)
+        .enterData('#txtUnitMeasure','hour').wait(100)
+        .enterData('#txtSymbol','hr').wait(100)
+        //        .selectComboRowByIndex('cboUnitType',5)
+        .clickButton('#btnClose').wait(100)
+        .checkMessageBox('iRely i21','Do you want to save the changes you made?','yesnocancel', 'question')
+        .clickMessageBoxButton('cancel').wait(10)
+        .clickButton('#btnClose').wait(100)
+
+
+        /* Scenario 4. Add another record, click Close button, SAVE the changes*/
+        .addFunction(function(next){t.diag("Scenario 4. Add another record, click Close button, SAVE the changes"); next();}).wait(100)
+        .checkMessageBox('iRely i21','Do you want to save the changes you made?','yesnocancel', 'question')
+        .clickMessageBoxButton('yes').wait(500)
+        .checkIfScreenClosed('inventoryuom').wait(100) /*issue - FRM-1547 or TS-445 or FRM-1560*/
+
+        /* 2. Verify record added*/
+        .addFunction(function(next){t.diag("Scenario 4. Add another record, SAVE the changes > 2. Verify record added"); next();}).wait(100)
+        .openScreen('Inventory UOM').wait(200)
+        .addFunction(function(next){t.diag("Scenario 4. Add new records > 3.a Check Search screen "); next();}).wait(100)
+        .checkScreenShown ('search').wait(200)
+        .checkScreenWindow({alias: 'search', title: 'Search Inventory UOMs', collapse: true, maximize: true, minimize: false, restore: false, close: true })
+        .checkToolbarButton({new: true, save: false, search: false, delete: false, undo: false, close: true})
+        .checkControlVisible(['#btnOpenSelected', '#btnRefresh'], true)
+//        no checking if correct icon is used
+//        no checking if button label is correct
+        .checkControlVisible(['#btnInsertCriteria', '#txtFilterGrid','#lblTotalRecords'],true)
+        .selectSearchRowByFilter('hour')
+        .clickButton('#btnOpenSelected').wait(100)
+        .addFunction(function(next){t.diag("3.a Opens selected record = Passed"); next();}).wait(100)
+        .checkControlData('#txtUnitMeasure','hour')
+        .checkControlData('#txtSymbol','hr')
+        .checkCheckboxValue('#chkDefault',false)
+//         need to add select combo box
+        .checkStatusMessage('Ready')
+        .addFunction(function(next){t.diag("3.b Correct record is selected and data entered is intact = Passed"); next();}).wait(100)
+        .clickButton('#btnClose').wait(100)
+        .checkIfScreenClosed('inventoryuom').wait(100)
+
+
+        /*Scenario 5. Add duplicate record */
+        .addFunction(function(next){t.diag("Scenario 5. Add duplicate record "); next();}).wait(100)
+        .openScreen('Inventory UOM').wait(200)
+        .checkScreenShown ('search').wait(200)
+        .checkScreenWindow({alias: 'search', title: 'Search Inventory UOMs', collapse: true, maximize: true, minimize: false, restore: false, close: true })
+        .clickButton('#btnNew').wait(100)
+        .enterData('#txtUnitMeasure','hour').wait(100)
+        .enterData('#txtSymbol','doz').wait(100)
+        //        .selectComboRowByIndex('cboUnitType',5)
+        .clickButton('#btnSave').wait(100)
+        .clickButton('#btnClose').wait(100)
+        .checkMessageBox('iRely i21','Inventory UOM already exists.','ok','error') /*issue - IC-84 */
+        .clickMessageBoxButton('ok').wait(10)
+
+        /*Scenario 6. Modify duplicate record to correct it*/
+        .addFunction(function(next){t.diag("Scenario 6. Modify duplicate record to correct it"); next();}).wait(100)
+        .enterData('#txtUnitMeasure','dozen').wait(100)
+        .clickButton('#btnClose').wait(100)
+        .checkMessageBox('iRely i21','Do you want to save the changes you made?','yesnocancel', 'question').wait(100)
+        .clickMessageBoxButton('yes').wait(10)
+        .checkIfScreenClosed('inventoryuom').wait(100)
+
+
+
+        /* 2. Verify record added*/
+        .addFunction(function(next){t.diag("Scenario 6. Modify duplicate record to correct it > 2. Verify record added"); next();}).wait(100)
+        .openScreen('Inventory UOM').wait(200)
+        .addFunction(function(next){t.diag("6.a Check Search screen "); next();}).wait(100)
+        .checkScreenShown ('search').wait(200)
+        .checkScreenWindow({alias: 'search', title: 'Search Inventory UOMs', collapse: true, maximize: true, minimize: false, restore: false, close: true })
+        .checkToolbarButton({new: true, save: false, search: false, delete: false, undo: false, close: true})
+        .checkControlVisible(['#btnOpenSelected', '#btnRefresh'], true)
+//        no checking if correct icon is used
+//        no checking if button label is correct
+        .checkControlVisible(['#btnInsertCriteria', '#txtFilterGrid','#lblTotalRecords'],true)
+        .selectSearchRowByFilter('dozen')
+        .clickButton('#btnOpenSelected').wait(100)
+        .addFunction(function(next){t.diag("6.b Opens selected record = Passed"); next();}).wait(100)
+        .checkControlData('#txtUnitMeasure','dozen')
+        .checkControlData('#txtSymbol','doz')
+        .checkCheckboxValue('#chkDefault',false)
+//         need to add select combo box
+        .checkStatusMessage('Ready')
+        .addFunction(function(next){t.diag("6.c Correct record is selected and data entered is intact = Passed"); next();}).wait(100)
+        .checkIfScreenClosed('fuelcode').wait(100)
+
+//        .enterDummyRowData('#grdFuelCategory', [{ column: 'strRinFuelTypeCode', data: '10Et'}, {column: 'strDescription', data: '10-Ethanol'}]) /*issue - FRM-1535 or TS-446 or FRM-1559*/
+
+//        /*Scenario 7. Add records > Fuel Code */
+//        .addFunction(function(next){t.diag("Scenario 7. Add records "); next();}).wait(100)
+//        .openScreen('Fuel Code').wait(200)
+//        .checkScreenShown ('fuelcode').wait(100)
+//        .enterGridData('#grdGridTemplate', 4, 'colRinFuelCode', 'f05').wait(100)
+//        .enterGridData('#grdGridTemplate', 4, 'colDescription', 'fuel 05').wait(100)
+//        .enterGridData('#grdGridTemplate', 5, 'colRinFuelCode', 'f06').wait(100)
+//        .enterGridData('#grdGridTemplate', 5, 'colDescription', 'fuel 06').wait(100)
+//        .enterGridData('#grdGridTemplate', 6, 'colRinFuelCode', 'f07').wait(100)
+//        .enterGridData('#grdGridTemplate', 6, 'colDescription', 'fuel 07').wait(100)
 //        .clickButton('#btnSave').wait(100)
-//        .checkMessageBox('iRely i21','Fuel Category already exists.','ok','error') /* IC-84 */
-//        .clickMessageBoxButton('ok').wait(10)
-//
-//        /*Scenario 6. Modify duplicate ProcessCode to correct it*/
-//        .addFunction(function(next){t.diag("Scenario 6. Modify duplicate ProcessCode to correct it"); next();}).wait(100)
-//        .enterGridData('#grdFeedStockUom', 3, 'colUOM', 'u04').wait(100) /* IC-81 */
-//        .clickButton('#closeButton').wait(100)
-//        .checkMessageBox('iRely i21','Do you want to save the changes you made?','yesnocancel', 'question')
-//        .clickMessageBoxButton('yes').wait(500)
-//        .checkIfScreenClosed('feedstockuom').wait(100)
-//
-//
-////        .enterDummyRowData('#grdFeedStockUom', [{ column: 'strRinFuelTypeCode', data: '10Et'}, {column: 'strDescription', data: '10-Ethanol'}]) /*issue - FRM-1535*/
-
-
+//        .checkStatusMessage('Edited')
+//        .clickButton('#btnClose').wait(100)
+//        .checkIfScreenClosed('fuelcode').wait(100)
 
         .done()
 })
