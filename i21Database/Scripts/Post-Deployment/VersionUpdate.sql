3f0c66d4c9f3be83641f04b4ccdcef6cbd80427d
darwin_estrada <darwin.estrada@irely.com>
2014-11-17 23:33:35 +0800

    FRM-1417

--- i21Database/Scripts/Post-Deployment/VersionUpdate.sql
+++ i21Database/Scripts/Post-Deployment/VersionUpdate.sql
@@ -2,7 +2,7 @@
 	PRINT N'BEGIN INSERT VERSION UPDATE'
 GO
 	INSERT INTO tblSMBuildNumber (strVersionNo, dtmLastUpdate)
-	SELECT '14.3', getdate()
+	SELECT '14.4', getdate()
 GO
 	PRINT N'END INSERT VERSION UPDATE'
 GO
