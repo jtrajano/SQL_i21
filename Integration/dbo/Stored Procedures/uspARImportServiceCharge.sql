c4f89b0f57b2104f9132b189ddff6435056bd8d6
Smith Wilson de Jesus <smith.dejesus@irely.com>
2014-11-18 14:05:06 +0800

    AR-487 Service Charge was successfully imported, but the records are not found in i2

--- Integration/dbo/Stored Procedures/uspARImportServiceCharge.sql
+++ Integration/dbo/Stored Procedures/uspARImportServiceCharge.sql
@@ -3,17 +3,13 @@
 IF EXISTS(select top 1 1 from sys.procedures where name = 'uspARImportServiceCharge')
 	DROP PROCEDURE uspARImportServiceCharge
 GO
-EXEC('CREATE PROCEDURE [dbo].[uspARImportServiceCharge]
+CREATE PROCEDURE [dbo].[uspARImportServiceCharge]
 	@ServiceChargeCode NVARCHAR(2) = NULL,
 	@Update BIT = 0,
 	@Total INT = 0 OUTPUT
 
 	AS
-
-	--Make first a copy of agsrvmst. This will use to track all service charge already imported
-	IF(OBJECT_ID(''dbo.tblARTempServiceCharge'') IS NULL)
-		SELECT * INTO tblARTempServiceCharge FROM agsrvmst
-	
+BEGIN	
 	--================================================
 	--     UPDATE/INSERT IN ORIGIN	
 	--================================================
@@ -55,38 +51,12 @@ EXEC('CREATE PROCEDURE [dbo].[uspARImportServiceCharge]
 	BEGIN
 	
 		--1 Time synchronization here
-		PRINT ''1 Time Service Charge Synchronization''
+		PRINT '1 Time Service Charge Synchronization'
 
-		DECLARE @originServiceChargeCode	NVARCHAR(2)
-		DECLARE @strServiceChargeCode		NVARCHAR(2)
-		DECLARE	@strDescription				NVARCHAR(MAX)
-		DECLARE @dblPercentage				NUMERIC(18,6)
-	
-		DECLARE @Counter INT = 0
-	
-    
-		--Import only those are not yet imported
-		SELECT agsrv_srv_no INTO #tmpagsrvmst 
-			FROM agsrvmst
-		LEFT JOIN tblARServiceCharge
-			ON agsrvmst.agsrv_srv_no  = tblARServiceCharge.strServiceChargeCode COLLATE Latin1_General_CI_AS
-		WHERE tblARServiceCharge.strServiceChargeCode IS NULL
-		ORDER BY agsrvmst.agsrv_srv_no DESC
-
-		WHILE (EXISTS(SELECT 1 FROM #tmpagsrvmst))
-		BEGIN
-		
-			SELECT @originServiceChargeCode = agsrv_srv_no FROM #tmpagsrvmst
-
-			SELECT TOP 1
-				@strServiceChargeCode = agsrv_srv_no,
-				@strDescription = agsrv_desc,
-				@dblPercentage = agsrv_pct
-			FROM agsrvmst
-			WHERE agsrv_srv_no = @originServiceChargeCode
-		
-			--Insert into tblARServiceCharge
-			INSERT [dbo].[tblARServiceCharge]
+		--================================================
+		--     Insert into tblARServiceCharge only those exist in agsrvmst
+		--================================================
+		INSERT [dbo].[tblARServiceCharge]
 			([strServiceChargeCode],
 			 [strDescription],
 			 [strCalculationType],
@@ -94,31 +64,18 @@ EXEC('CREATE PROCEDURE [dbo].[uspARImportServiceCharge]
 			 [dblServiceChargeAPR],
 			 [dblMinimumCharge],
 			 [intGracePeriod])
-			VALUES
-			(@strServiceChargeCode,
-			 @strDescription,
-			 ''Percent'',
-			 @dblPercentage,
-			 0,
-			 0,
-			 0)				
-	
-		
-			IF(@@ERROR <> 0) 
-			BEGIN
-				PRINT @@ERROR;
-				RETURN;
-			END
-
-			DELETE FROM #tmpagsrvmst WHERE agsrv_srv_no = @originServiceChargeCode
-		
-		
-			SET @Counter += 1
-		END
-	
-	SET @Total = @Counter
-	--To delete all record on temp table to determine if there are still record to import
-	DELETE FROM tblARTempServiceCharge
+		 SELECT 
+			 agsrv_srv_no
+			,agsrv_desc
+			,'Percent'
+			,agsrv_pct
+			,0
+			,0
+			,0
+		FROM agsrvmst LEFT JOIN 
+		tblARServiceCharge ON agsrvmst.agsrv_srv_no  = tblARServiceCharge.strServiceChargeCode COLLATE Latin1_General_CI_AS
+		WHERE tblARServiceCharge.strServiceChargeCode IS NULL
+						
 	END
 
 	--================================================
@@ -126,6 +83,11 @@ EXEC('CREATE PROCEDURE [dbo].[uspARImportServiceCharge]
 	--================================================
 	IF(@Update = 1 AND @ServiceChargeCode IS NULL) 
 	BEGIN
-		SELECT @Total = COUNT(agsrv_srv_no) from tblARTempServiceCharge
-	END'
-)
+		SELECT 
+			 @Total = COUNT(agsrv_srv_no)
+		FROM agsrvmst LEFT JOIN 
+		tblARServiceCharge ON agsrvmst.agsrv_srv_no  = tblARServiceCharge.strServiceChargeCode COLLATE Latin1_General_CI_AS
+		WHERE tblARServiceCharge.strServiceChargeCode IS NULL
+	END
+	
+END
