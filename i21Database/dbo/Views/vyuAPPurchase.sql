be87e0450529303323b05c287bb5d8a1f26adc3f
Erick Dela Cruz <erick.delacruz@irely.com>
2014-11-21 16:20:32 +0800

    AP-659

---
+++ i21Database/dbo/Views/vyuAPPurchase.sql
@@ -0,0 +1,11 @@
+﻿CREATE VIEW [dbo].[vyuAPPurchase]
+WITH SCHEMABINDING
+AS 
+SELECT 
+A.intPurchaseId
+,A.dtmDate
+,A.intVendorId
+,A.strPONumber
+,B.strVendorId 
+FROM dbo.tblPOPurchase A
+	INNER JOIN dbo.tblAPVendor B ON A.intVendorId = B.intVendorId
