ee645ebc9f49383aec9c3ef38aa3d47d254690a6
unknown <REscarcha@IBSRUBEN.COMTECH.COM>
2014-11-24 18:48:54 +0800

    AR-500

---
+++ i21Database/dbo/Functions/fnGetDueDateBasedonTerm.sql
@@ -0,0 +1,51 @@
+﻿CREATE FUNCTION [dbo].[fnGetDueDateBasedOnTerm]
+(
+	@TransactionDate		datetime
+	,@TermId				int
+)
+RETURNS datetime
+AS
+BEGIN
+
+DECLARE @Type nvarchar(100)
+DECLARE @BalanceDue int, @DayMonthDue int, @DueNextMonth int
+DECLARE @DueDate datetime
+
+SELECT 
+	@Type = strType 
+	,@BalanceDue = ISNULL(intBalanceDue, 0)
+	,@DayMonthDue = ISNULL(intDayofMonthDue, 0)
+	,@DueNextMonth = ISNULL(intDueNextMonth, 0)
+	,@DueDate = ISNULL(dtmDueDate, @TransactionDate) 
+FROM
+	tblSMTerm
+WHERE
+	intTermID = @TermId
+
+
+IF (@Type = 'Standard')
+	BEGIN
+		RETURN DATEADD(DAY, @BalanceDue, @TransactionDate);
+	END	
+ELSE IF (@Type = 'Date Driven')
+	BEGIN
+		DECLARE @TransactionMonth int, @TransactionDay int, @TransactionYear int
+		SELECT @TransactionMonth = DATEPART(MONTH,@TransactionDate), @TransactionDay = DATEPART(DAY,@TransactionDate) ,@TransactionYear = DATEPART(YEAR,@TransactionDate)
+		
+		DECLARE @TempDueDate datetime
+		Set @TempDueDate = CONVERT(datetime, (CAST(@TransactionMonth AS nvarchar(10)) + '/' + CAST(@DayMonthDue AS nvarchar(10)) + '/' + CAST(@TransactionYear AS nvarchar(10))), 101)
+			
+		IF (DATEDIFF(DAY,@TransactionDate,@TempDueDate) < @DueNextMonth)
+			RETURN DATEADD(MONTH, 0, @TempDueDate);
+		ELSE	
+			RETURN DATEADD(MONTH, 1, @TempDueDate);
+		
+	END	
+ELSE
+	BEGIN
+		RETURN ISNULL(@DueDate, @TransactionDate);
+	END	
+
+RETURN @TransactionDate;
+
+END
---
+++ i21Database/dbo/Stored Procedures/uspARDuplicateInvoice.sql
@@ -0,0 +1,119 @@
+﻿CREATE PROCEDURE dbo.uspARDuplicateInvoice
+	 @InvoiceId			int
+	,@InvoiceDate		datetime
+	,@UserId			int
+AS
+
+DECLARE @InvoiceNumber nvarchar(25)
+EXEC uspSMGetStartingNumber 19, @InvoiceNumber OUT
+
+INSERT INTO tblARInvoice(
+	strInvoiceNumber
+	,strInvoiceOriginId
+	,intCustomerId
+	,dtmDate
+	,dtmDueDate
+	,intCurrencyId
+	,intCompanyLocationId
+	,intSalespersonId
+	,dtmShipDate
+	,intShipViaId
+	,strPONumber
+	,intTermId
+	,dblInvoiceSubtotal
+	,dblShipping
+	,dblTax
+	,dblInvoiceTotal
+	,dblDiscount
+	,dblAmountDue
+	,dblPayment
+	,strTransactionType
+	,intPaymentMethodId
+	,strComments
+	,intAccountId
+	,ysnPosted
+	,ysnPaid
+	,strShipToAddress
+	,strShipToCity
+	,strShipToState
+	,strShipToZipCode
+	,strShipToCountry
+	,strBillToAddress
+	,strBillToCity
+	,strBillToState
+	,strBillToZipCode
+	,strBillToCountry
+	,intConcurrencyId)
+SELECT 
+	@InvoiceNumber
+	,strInvoiceOriginId
+	,intCustomerId
+	,@InvoiceDate
+	,dbo.fnGetDueDateBasedOnTerm(@InvoiceDate, intTermId)
+	,intCurrencyId
+	,intCompanyLocationId
+	,intSalespersonId
+	,dtmShipDate
+	,intShipViaId
+	,strPONumber
+	,intTermId
+	,dblInvoiceSubtotal
+	,dblShipping
+	,dblTax
+	,dblInvoiceTotal
+	,dblDiscount
+	,dblAmountDue
+	,dblPayment
+	,strTransactionType
+	,intPaymentMethodId
+	,strComments
+	,intAccountId
+	,0
+	,0
+	,strShipToAddress
+	,strShipToCity
+	,strShipToState
+	,strShipToZipCode
+	,strShipToCountry
+	,strBillToAddress
+	,strBillToCity
+	,strBillToState
+	,strBillToZipCode
+	,strBillToCountry
+	,0
+FROM 
+	tblARInvoice
+WHERE
+	intInvoiceId = @InvoiceId
+	
+	
+
+DECLARE @NewInvoiceId int
+
+SET @NewInvoiceId = SCOPE_IDENTITY()
+
+INSERT INTO tblARInvoiceDetail(
+	intInvoiceId
+    ,intItemId
+    ,strItemDescription
+    ,dblQtyOrdered
+    ,dblQtyShipped
+    ,dblPrice
+    ,dblTotal
+    ,intConcurrencyId)
+SELECT
+	@NewInvoiceId
+	,intItemId
+	,strItemDescription
+	,dblQtyOrdered
+	,dblQtyShipped
+	,dblPrice
+	,dblTotal
+	,intConcurrencyId
+FROM
+	tblARInvoiceDetail
+WHERE
+	intInvoiceId = @InvoiceId	
+
+
+
