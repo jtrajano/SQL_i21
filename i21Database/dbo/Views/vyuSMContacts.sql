4d4860244f8aca4b30422af17266e1e5db83dbfc
Drew Palaganas <drew.palaganas@irely.com>
2014-11-26 14:05:49 +0800

    FRM-1536

---
+++ i21Database/dbo/Views/vyuSMContacts.sql
@@ -0,0 +1,8 @@
+﻿CREATE VIEW [dbo].[vyuSMContacts]
+AS
+SELECT     dbo.vyuARCustomerContacts.intCustomerId, dbo.vyuARCustomerContacts.intEntityId, dbo.vyuARCustomerContacts.intContactId, dbo.vyuARCustomerContacts.strName, 
+           dbo.vyuARCustomerContacts.strEmail, dbo.vyuARCustomerContacts.strLocationName, dbo.vyuARCustomerContacts.strPhone, dbo.vyuARCustomerContacts.strTimezone, 
+           dbo.vyuARCustomerContacts.strTitle, dbo.vyuARCustomerContacts.ysnPortalAccess, dbo.vyuARCustomerContacts.ysnDefaultContact, dbo.tblEntityCredential.strUserName, 
+           dbo.tblEntityCredential.strPassword
+FROM       dbo.vyuARCustomerContacts INNER JOIN
+           dbo.tblEntityCredential ON dbo.vyuARCustomerContacts.intEntityId = dbo.tblEntityCredential.intEntityId
