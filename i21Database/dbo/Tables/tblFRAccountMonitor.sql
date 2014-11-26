055837adbdcc83a3b83d95d2713150d5f50dbb37
axel_casauran <axel.casauran@irely.com>
2014-11-26 08:20:02 +0800

    FRD-326

--- i21Database/dbo/Tables/tblFRAccountMonitor.sql
+++ i21Database/dbo/Tables/tblFRAccountMonitor.sql
@@ -1,13 +1,13 @@
 ﻿CREATE TABLE [dbo].[tblFRAccountMonitor](
 	[cntId]					INT             IDENTITY (1, 1) NOT NULL,
-	[intRowId]				INT             NOT NULL,
-	[intAccountId]			INT             NOT NULL,
-	[strAccountId]			NVARCHAR (70)  COLLATE Latin1_General_CI_AS NOT NULL,
-	[strPrimary]			NVARCHAR (50)  COLLATE Latin1_General_CI_AS NOT NULL,
-	[strDescription]		NVARCHAR (300)  COLLATE Latin1_General_CI_AS NOT NULL,
-	[strAccountGroup]		NVARCHAR (250)  COLLATE Latin1_General_CI_AS NOT NULL,
-	[strAccountType]		NVARCHAR (150)  COLLATE Latin1_General_CI_AS NOT NULL,
+	[intRowId]				INT             NULL,
+	[intAccountId]			INT             NULL,
+	[strAccountId]			NVARCHAR (70)	COLLATE Latin1_General_CI_AS NULL,
+	[strPrimary]			NVARCHAR (50)	COLLATE Latin1_General_CI_AS NULL,
+	[strDescription]		NVARCHAR (300)  COLLATE Latin1_General_CI_AS NULL,
+	[strAccountGroup]		NVARCHAR (250)  COLLATE Latin1_General_CI_AS NULL,
+	[strAccountType]		NVARCHAR (150)  COLLATE Latin1_General_CI_AS NULL,
 	[intConcurrencyId]		INT             NULL,
-	[dtmEntered]			DATETIME       CONSTRAINT [DF_tblFRAccountMonitor_dtmEntered] DEFAULT (getdate()) NOT NULL,
+	[dtmEntered]			DATETIME		CONSTRAINT [DF_tblFRAccountMonitor_dtmEntered] DEFAULT (getdate()) NOT NULL,
  CONSTRAINT [PK_tblFRAccountMonitor] PRIMARY KEY CLUSTERED ([cntId] ASC)
 );
