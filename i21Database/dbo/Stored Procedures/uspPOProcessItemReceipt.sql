c521084d6bbb9b579c83f50c7d8064528c537b1d
unknown <EDelaCruz@ErickNB.COMTECH.COM>
2014-12-02 17:04:25 +0800

    AP-659

---
+++ i21Database/dbo/Stored Procedures/uspPOProcessItemReceipt.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE [dbo].[uspPOProcessItemReceipt]
+	@poIds NVARCHAR(MAX),
+	@userId NVARCHAR(50)
+AS
+BEGIN
+
+SET QUOTED_IDENTIFIER OFF
+SET ANSI_NULLS ON
+SET NOCOUNT ON
+SET XACT_ABORT ON
+SET ANSI_WARNINGS OFF
+ 
+-- Implement your code that validates the transaction you need to process.
+ 
+-- Add code to lock-out editing of the purchase order after it has been processed.
+  
+-- Call inventory stored procedure to process your transaction into "Item Receipt"
+EXEC dbo.uspICProcessToItemReceipt
+	@intSourceTransactionId = 1
+	,@strSourceType = 'Purchase Order'
+	,@intUserId = 1
+
+END
