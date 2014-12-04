86670765840d01eb4723a2d6deaac58f5b050c2b
Feb Montefrio <feb.montefrio@irely.com>
2014-12-04 15:50:46 +0800

    IC-100 Add unit tests for uspICReduceStockInLIFO

---
+++ tSQLt_UnitTesting/testi21Database/Stored Procedures/test uspICReduceStockInLIFO from positive stock to negative stock.sql
@@ -0,0 +1,219 @@
+﻿CREATE PROCEDURE [testi21Database].[test uspICReduceStockInLIFO from positive stock to negative stock]
+AS
+BEGIN
+	-- Arrange 
+	BEGIN 
+		-- Fake the table 
+		EXEC tSQLt.FakeTable 'dbo.tblICInventoryLIFO', @Identity = 1;		
+
+		-- Re-add the clustered index. This is critical for the LIFO table because it arranges the data physically by that order. 
+		CREATE CLUSTERED INDEX [Fake_IDX_tblICInventoryLIFO]
+			ON [dbo].[tblICInventoryLIFO]([dtmDate] DESC, [intItemId] ASC, [intLocationId] ASC, [intInventoryLIFOId] DESC);
+
+		-- Declare the variables for grains (item)
+		DECLARE @WetGrains AS INT = 1
+				,@StickyGrains AS INT = 2
+				,@PremiumGrains AS INT = 3
+				,@ColdGrains AS INT = 4
+				,@HotGrains AS INT = 5
+				,@InvalidItem AS INT = -1
+
+		-- Declare the variables for location
+		DECLARE @Default_Location AS INT = 1
+				,@NewHaven AS INT = 2
+				,@BetterHaven AS INT = 3
+
+		-- Create a fake data for tblICInventoryLIFO
+			/***************************************************************************************************************************************************************************************************************
+			The initial data in tblICInventoryLIFO
+			intItemId   intLocationId dtmDate                 dblStockIn                              dblStockOut                             dblCost                                 intCreatedUserId intConcurrencyId
+			----------- ----------------- ----------------------- --------------------------------------- --------------------------------------- --------------------------------------- ---------------- ----------------
+			1           1                 2014-01-01 00:00:00.000 100.000000                              0.000000                                11.440000                               1                1
+			***************************************************************************************************************************************************************************************************************/
+		INSERT INTO dbo.tblICInventoryLIFO (
+			[intItemId]
+			,[intLocationId]
+			,[dtmDate]
+			,[dblStockIn]
+			,[dblStockOut]
+			,[dblCost]
+			,[dtmCreated]
+			,[intCreatedUserId]
+			,[intConcurrencyId]
+		)
+		SELECT	[intItemId] = @WetGrains
+				,[intLocationId] = @Default_Location
+				,[dtmDate] = 'January 1, 2014'
+				,[dblStockIn] = 100
+				,[dblStockOut] = 0
+				,[dblCost] = 11.44
+				,[dtmCreated] = GETDATE()
+				,[intCreatedUserId] = 1
+				,[intConcurrencyId] = 1
+
+		-- Create the expected and actual tables 
+		CREATE TABLE expected (
+			[intItemId] INT 
+			,[intLocationId] INT 
+			,[dtmDate] DATETIME
+			,[dblStockIn] NUMERIC(18,6)
+			,[dblStockOut] NUMERIC(18,6)
+			,[dblCost] NUMERIC(18,6)
+			,[intCreatedUserId] INT 
+			,[intConcurrencyId]	INT
+		)
+
+		CREATE TABLE actual (
+			[intItemId] INT 
+			,[intLocationId] INT 
+			,[dtmDate] DATETIME
+			,[dblStockIn] NUMERIC(18,6)
+			,[dblStockOut] NUMERIC(18,6)
+			,[dblCost] NUMERIC(18,6)
+			,[intCreatedUserId] INT 
+			,[intConcurrencyId]	INT
+		)
+
+		-- Create the variables used by uspICReduceStockInLIFO
+		DECLARE @intItemId AS INT = @WetGrains
+				,@intLocationId AS INT = @Default_Location
+				,@dtmDate AS DATETIME = 'January 12, 2014'
+				,@dblSoldQty NUMERIC(18,6) = -125
+				,@dblCost AS NUMERIC(18,6) = 45.66
+				,@strTransactionId AS NVARCHAR(40)
+				,@intTransactionId AS INT
+				,@intUserId AS INT = 1
+				,@dtmCreated AS DATETIME
+				,@dblReduceQty AS NUMERIC(18,6)
+				,@RemainingQty AS NUMERIC(18,6) 
+				,@CostUsed AS NUMERIC(18,6) 
+				,@QtyOffset AS NUMERIC(18,6)
+				,@LIFOId AS INT 
+
+		-- Setup the expected values 
+		INSERT INTO expected (
+				[intItemId] 
+				,[intLocationId] 
+				,[dtmDate] 
+				,[dblStockIn] 
+				,[dblStockOut]
+				,[dblCost] 
+				,[intCreatedUserId] 
+				,[intConcurrencyId]
+		)
+		SELECT	[intItemId] = @WetGrains
+				,[intLocationId] = @Default_Location
+				,[dtmDate] = 'January 1, 2014'
+				,[dblStockIn] = 100
+				,[dblStockOut] = 100
+				,[dblCost] = 11.44
+				,[intCreatedUserId] = 1
+				,[intConcurrencyId] = 2
+		UNION ALL
+		SELECT	[intItemId] = @WetGrains
+				,[intLocationId] = @Default_Location
+				,[dtmDate] = 'January 12, 2014'
+				,[dblStockIn] = 0
+				,[dblStockOut] = 25
+				,[dblCost] = 45.66
+				,[intCreatedUserId] = 1
+				,[intConcurrencyId] = 1
+
+				/***************************************************************************************************************************************************************************************************************
+				The following are the expected records to be affected. Here is how it should look like:  
+		_m_		intItemId   intLocationId dtmDate                 dblStockIn                              dblStockOut                             dblCost                                 intCreatedUserId intConcurrencyId
+		-----	----------- ----------------- ----------------------- --------------------------------------- --------------------------------------- --------------------------------------- ---------------- ----------------
+		upt		1           1                 2014-01-01 00:00:00.000 100.000000                              100.000000                              11.440000                               1                1
+		new		1           1                 2014-01-12 00:00:00.000 0.000000                                25.000000                               45.660000                               1                1
+				***************************************************************************************************************************************************************************************************************/
+	END 
+	
+	-- Act
+	BEGIN 
+		SET @dblReduceQty = @dblSoldQty
+		DECLARE @intIterationCounter AS INT = 0;
+
+		-- Repeat call on uspICReduceStockInLIFO until @dblReduceQty is completely distributed to all of the available LIFO buckets
+		WHILE (ISNULL(@dblReduceQty, 0) < 0)
+		BEGIN 	
+			SET @intIterationCounter += 1;
+						
+			EXEC dbo.uspICReduceStockInLIFO
+				@intItemId
+				,@intLocationId
+				,@dtmDate
+				,@dblReduceQty
+				,@dblCost
+				,@strTransactionId
+				,@intTransactionId
+				,@intUserId
+				,@RemainingQty OUTPUT
+				,@CostUsed OUTPUT 
+				,@QtyOffset OUTPUT 
+				,@LIFOId OUTPUT 
+
+			-- Assert on first pass
+			IF (@intIterationCounter = 1) 
+			BEGIN 
+				-- the cost used is 11.44. 
+				EXEC tSQLt.AssertEquals 11.44, @CostUsed
+
+				-- the qty offset is 100
+				EXEC tSQLt.AssertEquals 100, @QtyOffset
+
+				-- the LIFO id used is 1
+				EXEC tSQLt.AssertEquals 1, @LIFOId
+			END 
+				
+			-- Assert on 2nd pass
+			IF (@intIterationCounter = 2) 
+			BEGIN 
+				-- there is no cost used is NULL (no cost used).
+				EXEC tSQLt.AssertEquals NULL, @CostUsed
+
+				-- the qty offset is also NULL 
+				EXEC tSQLt.AssertEquals NULL, @QtyOffset
+
+				-- the LIFO id used is also NULL 
+				EXEC tSQLt.AssertEquals NULL, @LIFOId
+			END 
+
+			SET @dblReduceQty = @RemainingQty;
+		END 
+
+		INSERT INTO actual (
+				[intItemId] 
+				,[intLocationId] 
+				,[dtmDate] 
+				,[dblStockIn] 
+				,[dblStockOut]
+				,[dblCost] 
+				,[intCreatedUserId] 
+				,[intConcurrencyId]
+		)
+		SELECT
+				[intItemId] 
+				,[intLocationId] 
+				,[dtmDate] 
+				,[dblStockIn] 
+				,[dblStockOut]
+				,[dblCost] 
+				,[intCreatedUserId] 
+				,[intConcurrencyId]
+		FROM	dbo.tblICInventoryLIFO
+		WHERE	intItemId = @intItemId
+				AND intLocationId = @intLocationId
+	END 
+
+	-- Assert
+	BEGIN 
+		EXEC tSQLt.AssertEqualsTable 'expected', 'actual';
+	END
+
+	-- Clean-up: remove the tables used in the unit test
+	IF OBJECT_ID('actual') IS NOT NULL 
+		DROP TABLE actual
+
+	IF OBJECT_ID('expected') IS NOT NULL 
+		DROP TABLE dbo.expected
+END
---
+++ tSQLt_UnitTesting/testi21Database/Stored Procedures/test uspICReduceStockInLIFO from zero to negative stock.sql
@@ -0,0 +1,156 @@
+﻿CREATE PROCEDURE [testi21Database].[test uspICReduceStockInLIFO from zero to negative stock]
+AS
+BEGIN
+	-- Arrange 
+	BEGIN 
+		-- Fake the table 
+		EXEC tSQLt.FakeTable 'dbo.tblICInventoryLIFO', @Identity = 1;
+
+		-- Re-add the clustered index. This is critical for the LIFO table because it arranges the data physically by that order. 
+		CREATE CLUSTERED INDEX [Fake_IDX_tblICInventoryLIFO]
+			ON [dbo].[tblICInventoryLIFO]([dtmDate] DESC, [intItemId] ASC, [intLocationId] ASC, [intInventoryLIFOId] DESC);
+
+		-- Create the expected and actual tables 
+		CREATE TABLE expected (
+			[intItemId] INT 
+			,[intLocationId] INT 
+			,[dtmDate] DATETIME
+			,[dblStockIn] NUMERIC(18,6)
+			,[dblStockOut] NUMERIC(18,6)
+			,[dblCost] NUMERIC(18,6)
+			,[intCreatedUserId] INT 
+			,[intConcurrencyId]	INT
+		)
+
+		CREATE TABLE actual (
+			[intItemId] INT 
+			,[intLocationId] INT 
+			,[dtmDate] DATETIME
+			,[dblStockIn] NUMERIC(18,6)
+			,[dblStockOut] NUMERIC(18,6)
+			,[dblCost] NUMERIC(18,6)
+			,[intCreatedUserId] INT 
+			,[intConcurrencyId]	INT
+		)
+
+		-- Create a fake data for tblICInventoryLIFO
+		/***************************************************************************************************************************************************************************************************************
+		The initial data in tblICInventoryLIFO
+		intItemId   intLocationId dtmDate                 dblStockIn                              dblStockOut                             dblCost                                 intCreatedUserId intConcurrencyId
+		----------- ----------------- ----------------------- --------------------------------------- --------------------------------------- --------------------------------------- ---------------- ----------------
+		No record.
+		***************************************************************************************************************************************************************************************************************/
+
+		-- Create the variables used by uspICReduceStockInLIFO
+		DECLARE @intItemId AS INT = 1
+				,@intLocationId AS INT = 1
+				,@dtmDate AS DATETIME = 'January 3, 2014'
+				,@dblSoldQty NUMERIC(18,6) = -10
+				,@dblCost AS NUMERIC(18,6) = 33.19
+				,@strTransactionId AS NVARCHAR(40)
+				,@intTransactionId AS INT
+				,@intUserId AS INT = 1
+				,@dtmCreated AS DATETIME
+				,@dblReduceQty AS NUMERIC(18,6)
+				,@RemainingQty AS NUMERIC(18,6)
+				,@CostUsed AS NUMERIC(18,6)
+				,@QtyOffset AS NUMERIC(18,6)
+				,@LIFOId AS INT 
+
+		-- Setup the expected values 
+		INSERT INTO expected (
+				[intItemId] 
+				,[intLocationId] 
+				,[dtmDate] 
+				,[dblStockIn] 
+				,[dblStockOut]
+				,[dblCost] 
+				,[intCreatedUserId] 
+				,[intConcurrencyId]
+		)
+		SELECT	[intItemId] = @intItemId
+				,[intLocationId] = @intLocationId
+				,[dtmDate] = @dtmDate
+				,[dblStockIn] = 0
+				,[dblStockOut] = ABS(@dblSoldQty)
+				,[dblCost] = @dblCost
+				,[intCreatedUserId] = @intUserId
+				,[intConcurrencyId] = 1
+
+				/***************************************************************************************************************************************************************************************************************
+				The following are the expected records to be affected. Here is how it should look like:  
+		_m_		intItemId   intLocationId dtmDate                 dblStockIn                              dblStockOut                             dblCost                                 intCreatedUserId intConcurrencyId
+		-----	----------- ----------------- ----------------------- --------------------------------------- --------------------------------------- --------------------------------------- ---------------- ----------------
+		new		1           1                 2014-01-03 00:00:00.000 0.000000                                10.000000                               33.190000                               1                1
+				***************************************************************************************************************************************************************************************************************/
+	END 
+	
+	-- Act
+	BEGIN 
+		SET @dblReduceQty = @dblSoldQty
+
+		-- Repeat call on uspICReduceStockInLIFO until @dblReduceQty is completely distributed to all the available LIFO buckets
+		WHILE (ISNULL(@dblReduceQty, 0) < 0)
+		BEGIN 					
+			EXEC [dbo].[uspICReduceStockInLIFO]
+				@intItemId
+				,@intLocationId
+				,@dtmDate
+				,@dblReduceQty
+				,@dblCost
+				,@strTransactionId
+				,@intTransactionId
+				,@intUserId
+				,@RemainingQty OUTPUT
+				,@CostUsed OUTPUT
+				,@QtyOffset OUTPUT 
+				,@LIFOId OUTPUT 
+
+			SET @dblReduceQty = @RemainingQty;
+
+			-- Assert that no cost was used (NULL)
+			EXEC tSQLt.AssertEquals NULL, @CostUsed;
+
+			-- Assert Qty offset is NULL
+			EXEC tSQLt.AssertEquals NULL, @QtyOffset;
+
+			-- Assert LIFO Id is NULL
+			EXEC tSQLt.AssertEquals NULL, @LIFOId;
+		END 
+
+		INSERT INTO actual (
+				[intItemId] 
+				,[intLocationId] 
+				,[dtmDate] 
+				,[dblStockIn] 
+				,[dblStockOut]
+				,[dblCost] 
+				,[intCreatedUserId] 
+				,[intConcurrencyId]
+		)
+		SELECT
+				[intItemId] 
+				,[intLocationId] 
+				,[dtmDate] 
+				,[dblStockIn] 
+				,[dblStockOut]
+				,[dblCost] 
+				,[intCreatedUserId] 
+				,[intConcurrencyId]
+		FROM	dbo.tblICInventoryLIFO
+		WHERE	intItemId = @intItemId
+				AND intLocationId = @intLocationId
+	END 
+
+	-- Assert
+	BEGIN 
+		EXEC tSQLt.AssertEqualsTable 'expected', 'actual';
+	END
+
+	-- Clean-up: remove the tables used in the unit test
+	IF OBJECT_ID('actual') IS NOT NULL 
+		DROP TABLE actual
+
+	IF OBJECT_ID('expected') IS NOT NULL 
+		DROP TABLE dbo.expected
+END 
