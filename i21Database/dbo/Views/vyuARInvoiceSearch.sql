b71c7ee60f222db2ee3e2d1855e335a9c3dfd6bf
Smith Wilson de Jesus <smith.dejesus@irely.com>
2014-12-05 17:32:31 +0800

    AR-541 Receive Payments: Functional issues in grid area

--- i21Database/dbo/Views/vyuARInvoiceSearch.sql
+++ i21Database/dbo/Views/vyuARInvoiceSearch.sql
@@ -19,9 +19,16 @@ Inv.dblAmountDue,
 Inv.dblPayment, 
 Inv.intPaymentMethodId, 
 Inv.intCompanyLocationId, 
-Inv.strComments
+Inv.strComments,
+Inv.intCurrencyId,
+CompLoc.strLocationName,
+PayMthd.strPaymentMethod
 FROM         
 dbo.tblARInvoice AS Inv INNER JOIN
 dbo.tblARCustomer AS Cus ON Inv.intCustomerId = Cus.intCustomerId INNER JOIN
 dbo.tblEntity AS NTT ON Cus.intEntityId = NTT.intEntityId LEFT OUTER JOIN
-dbo.tblSMTerm AS Term ON Inv.intTermId = Term.intTermID
+dbo.tblSMTerm AS Term ON Inv.intTermId = Term.intTermID LEFT OUTER JOIN
+dbo.tblSMCompanyLocation AS CompLoc ON Inv.intCompanyLocationId  = CompLoc.intCompanyLocationId LEFT OUTER JOIN
+dbo.tblSMPaymentMethod AS PayMthd ON Inv.intPaymentMethodId = PayMthd.intPaymentMethodID
+
+
