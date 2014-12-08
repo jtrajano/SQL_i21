859fda2098296134c848c65b78c6588b9a0ebabd
Smith Wilson de Jesus <smith.dejesus@irely.com>
2014-12-08 16:03:29 +0800

    AR-542 Import of duplicate terms

--- Integration/dbo/Stored Procedures/uspARImportTerm.sql
+++ Integration/dbo/Stored Procedures/uspARImportTerm.sql
@@ -18,10 +18,61 @@ BEGIN
 		--1 Time synchronization here
 		PRINT '1 Time Term Synchronization'
 		
+		DECLARE @intTermId			INT
+		DECLARE @strTerm			NVARCHAR(200)
+		DECLARE @strTermCode		NVARCHAR(10)
+		DECLARE @strType			NVARCHAR(50)
+		DECLARE @dblDiscountEP		NUMERIC(18,6)
+		DECLARE @intDiscountDay		INT
+		DECLARE @dtmDiscountDate	DATETIME
+		DECLARE @intBalanceDue		INT
+		DECLARE @dtmDueDate			DATETIME
+		DECLARE @ysnAllowEFT		BIT
+		DECLARE @isTermExists		BIT
 		
-			--================================================
-			--     Insert into tblSMTerm only those exist in agivcmst
-			--================================================
+		SELECT 
+		 LTRIM(RTRIM(agtrm_desc)) as strTerm
+		,agtrm_key_n as strTermCode
+		,(CASE 
+			WHEN agtrm_net_rev_dt <> 0  OR agtrm_net_rev_dt <> 0
+				THEN 'Specific Date'
+			ELSE
+				'Standard'
+			END) as strType
+		,agtrm_disc_pct as dblDiscountEP
+		,agtrm_disc_days as intDiscountDay
+		,(CASE WHEN ISDATE(agtrm_disc_rev_dt) = 1 THEN CONVERT(DATE, CAST(agtrm_disc_rev_dt AS CHAR(12)), 112) ELSE NULL END) as dtmDiscountDate
+		,agtrm_net_days as intBalanceDue
+		,(CASE WHEN ISDATE(agtrm_net_rev_dt) = 1 THEN CONVERT(DATE, CAST(agtrm_net_rev_dt AS CHAR(12)), 112) ELSE NULL END) as dtmDueDate
+		,(CASE WHEN RTRIM(LTRIM(ISNULL(agtrm_eft_yn,'N'))) = 'N' THEN 0 ELSE 1 END) as ysnAllowEFT
+		INTO #tmpTerm
+		FROM agtrmmst 
+		LEFT JOIN tblSMTerm
+					ON CAST(agtrmmst.agtrm_key_n as CHAR(10)) COLLATE Latin1_General_CI_AS = tblSMTerm.strTermCode COLLATE Latin1_General_CI_AS
+		 WHERE tblSMTerm.strTermCode is  null
+		 
+		WHILE (EXISTS(SELECT 1 FROM #tmpTerm))
+		BEGIN
+			SET @isTermExists = 0;
+			
+			SELECT TOP 1 
+			 @strTerm			= strTerm			
+			,@strTermCode		= strTermCode		 
+			,@strType			= strType			 
+			,@dblDiscountEP		= dblDiscountEP		 
+			,@intDiscountDay	= intDiscountDay		 
+			,@dtmDiscountDate	= dtmDiscountDate	 
+			,@intBalanceDue		= intBalanceDue		 
+			,@dtmDueDate		= dtmDueDate			 
+			,@ysnAllowEFT		= ysnAllowEFT		
+			FROM #tmpTerm
+			
+			IF(EXISTS(SELECT 1 FROM tblSMTerm WHERE LTRIM(RTRIM(strTerm)) = @strTerm))
+			BEGIN
+				SET @strTerm = @strTerm + '*'
+				SET @isTermExists = 1
+			END
+			
 			INSERT INTO [dbo].[tblSMTerm]
 			   ([strTerm]
 			   ,[strTermCode]
@@ -31,28 +82,33 @@ BEGIN
 			   ,[dtmDiscountDate]
 			   ,[intBalanceDue]
 			   ,[dtmDueDate]
-			   ,[ysnAllowEFT])
-			SELECT 
-				agtrm_desc
-				,agtrm_key_n
-				,(CASE 
-					WHEN agtrm_net_rev_dt <> 0  OR agtrm_net_rev_dt <> 0
-						THEN 'Specific Date'
-					ELSE
-						'Standard'
-					END)
-				,agtrm_disc_pct
-				,agtrm_disc_days
-				,(CASE WHEN ISDATE(agtrm_disc_rev_dt) = 1 THEN CONVERT(DATE, CAST(agtrm_disc_rev_dt AS CHAR(12)), 112) ELSE NULL END)
-				,agtrm_net_days
-				,(CASE WHEN ISDATE(agtrm_net_rev_dt) = 1 THEN CONVERT(DATE, CAST(agtrm_net_rev_dt AS CHAR(12)), 112) ELSE NULL END)
-				,(CASE WHEN RTRIM(LTRIM(ISNULL(agtrm_eft_yn,'N'))) = 'N' THEN 0 ELSE 1 END)
-			FROM agtrmmst 
-			LEFT JOIN tblSMTerm Term ON CONVERT(NVARCHAR(20),agtrmmst.agtrm_key_n) COLLATE Latin1_General_CI_AS = Term.strTermCode COLLATE Latin1_General_CI_AS
-			WHERE agtrm_key_n in (SELECT DISTINCT agivc_terms_code FROM agivcmst) 
-			AND Term.strTermCode IS NULL AND agtrmmst.agtrm_key_n = UPPER(agtrmmst.agtrm_key_n) COLLATE Latin1_General_CS_AS
-			 
-
+			   ,[ysnAllowEFT]
+			   ,[dblAPR]
+			   ,[intDayofMonthDue]
+			   ,[intDueNextMonth])
+			VALUES
+				(@strTerm			
+				,@strTermCode		
+				,@strType			
+				,@dblDiscountEP		
+				,@intDiscountDay	
+				,@dtmDiscountDate	
+				,@intBalanceDue		
+				,@dtmDueDate		
+				,@ysnAllowEFT
+				,0 -- dblAPR	
+				,0 -- intDayofMonthDue
+				,0) -- intDueNextMonth
+			SET @intTermId = SCOPE_IDENTITY()
+			
+			IF(@isTermExists = 1)
+			BEGIN
+				UPDATE tblSMTerm SET strTerm = SUBSTRING(@strTerm,0,CHARINDEX('*',@strTerm)) + ' - ' +  CAST(@intTermId AS NVARCHAR(100)) WHERE strTermCode = @strTermCode
+			END
+			
+			DELETE FROM #tmpTerm WHERE strTermCode = @strTermCode
+			
+		END
 	END
 
 	--================================================
@@ -61,20 +117,13 @@ BEGIN
 	--================================================
 	IF(@Checking = 1) 
 	BEGIN
-		DECLARE @fromTerm INT
-        DECLARE @fromOriginTerm INT
-        
-        select @fromTerm = COUNT(strTermCode) from tblSMTerm where strTermCode  IN (SELECT CONVERT(nvarchar(10),agtrm_key_n) FROM agtrmmst WHERE agtrm_key_n in (SELECT DISTINCT agivc_terms_code FROM agivcmst))
-		
-		SELECT @fromOriginTerm = COUNT(DISTINCT agivc_terms_code) FROM agivcmst
-        
-        IF(@fromTerm = @fromOriginTerm)
-        BEGIN
-            SET @Total = 0
-        END
-        ELSE
-            Set @Total = @fromOriginTerm - @fromTerm
+		SELECT 
+		@Total = COUNT(agtrm_key_n)		
+		FROM agtrmmst 
+		LEFT JOIN tblSMTerm
+					ON CAST(agtrmmst.agtrm_key_n as CHAR(10)) COLLATE Latin1_General_CI_AS = tblSMTerm.strTermCode COLLATE Latin1_General_CI_AS
+		 WHERE tblSMTerm.strTermCode is  null
 		
 	END
 		
-END	
+END
