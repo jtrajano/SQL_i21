25901ecfb03a12be8e63b970bb42f6d748332da5
Lawrence Zabala <lawrence.zabala@irely.com>
2014-12-10 17:16:17 +0800

    IC-91

--- i21Database/Scripts/Post-Deployment/IC/3_DefaultData.sql
+++ i21Database/Scripts/Post-Deployment/IC/3_DefaultData.sql
@@ -24,3 +24,24 @@ IF NOT EXISTS(SELECT TOP 1 1 FROM tblICReadingPoint WHERE strReadingPoint = 'Pro
 BEGIN
 	INSERT INTO tblICReadingPoint (strReadingPoint) VALUES ('Produce')
 END
+
+IF NOT EXISTS(SELECT TOP 1 1 FROM tblICEquipmentLength WHERE strEquipmentLength = '02000')
+BEGIN
+	INSERT INTO tblICEquipmentLength (strEquipmentLength, strDescription, intSort) VALUES ('02000', '20 ft container', 1)
+END
+IF NOT EXISTS(SELECT TOP 1 1 FROM tblICEquipmentLength WHERE strEquipmentLength = '02400')
+BEGIN
+	INSERT INTO tblICEquipmentLength (strEquipmentLength, strDescription, intSort) VALUES ('02400', '24 ft container', 2)
+END
+IF NOT EXISTS(SELECT TOP 1 1 FROM tblICEquipmentLength WHERE strEquipmentLength = '04500')
+BEGIN
+	INSERT INTO tblICEquipmentLength (strEquipmentLength, strDescription, intSort) VALUES ('04500', '45 ft trailer/container', 3)
+END
+IF NOT EXISTS(SELECT TOP 1 1 FROM tblICEquipmentLength WHERE strEquipmentLength = '04800')
+BEGIN
+	INSERT INTO tblICEquipmentLength (strEquipmentLength, strDescription, intSort) VALUES ('04800', '48 ft trailer', 4)
+END
+IF NOT EXISTS(SELECT TOP 1 1 FROM tblICEquipmentLength WHERE strEquipmentLength = '05300')
+BEGIN
+	INSERT INTO tblICEquipmentLength (strEquipmentLength, strDescription, intSort) VALUES ('05300', '53 ft trailer', 5)
+END
