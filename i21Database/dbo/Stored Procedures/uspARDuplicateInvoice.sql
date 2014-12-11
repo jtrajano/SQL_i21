84d61b570f155288ea73d91278f9de40af137962
Ruben  Escarcha <ruben.escarcha@irely.com>
2014-12-11 13:34:29 +0800

    AR-500 Create a stored procedure that duplicates an invoice - Additional requirement requested by SM

--- i21Database/dbo/Stored Procedures/uspARDuplicateInvoice.sql
+++ i21Database/dbo/Stored Procedures/uspARDuplicateInvoice.sql
@@ -2,120 +2,127 @@
 	 @InvoiceId			int
 	,@InvoiceDate		datetime
 	,@UserId			int
+	,@NewInvoiceId		int			= NULL		OUTPUT
 AS
 
-DECLARE @InvoiceNumber nvarchar(25)
-EXEC uspSMGetStartingNumber 19, @InvoiceNumber OUT
+BEGIN
+	DECLARE @InvoiceNumber nvarchar(25)
+	EXEC uspSMGetStartingNumber 19, @InvoiceNumber OUT
 
-DECLARE @EntityId int
+	DECLARE @EntityId int
 
-SET @EntityId = ISNULL((SELECT TOP 1 intEntityId FROM tblSMUserSecurity WHERE intUserSecurityID = @UserId), 0)
+	SET @EntityId = ISNULL((SELECT TOP 1 intEntityId FROM tblSMUserSecurity WHERE intUserSecurityID = @UserId), 0)
 
-INSERT INTO tblARInvoice(
-	strInvoiceNumber
-	,strInvoiceOriginId
-	,intCustomerId
-	,dtmDate
-	,dtmDueDate
-	,intCurrencyId
-	,intCompanyLocationId
-	,intSalespersonId
-	,dtmShipDate
-	,intShipViaId
-	,strPONumber
-	,intTermId
-	,dblInvoiceSubtotal
-	,dblShipping
-	,dblTax
-	,dblInvoiceTotal
-	,dblDiscount
-	,dblAmountDue
-	,dblPayment
-	,strTransactionType
-	,intPaymentMethodId
-	,strComments
-	,intAccountId
-	,ysnPosted
-	,ysnPaid
-	,strShipToAddress
-	,strShipToCity
-	,strShipToState
-	,strShipToZipCode
-	,strShipToCountry
-	,strBillToAddress
-	,strBillToCity
-	,strBillToState
-	,strBillToZipCode
-	,strBillToCountry
-	,intConcurrencyId
-	,intEntityId)
-SELECT 
-	@InvoiceNumber
-	,strInvoiceOriginId
-	,intCustomerId
-	,@InvoiceDate
-	,dbo.fnGetDueDateBasedOnTerm(@InvoiceDate, intTermId)
-	,intCurrencyId
-	,intCompanyLocationId
-	,intSalespersonId
-	,dtmShipDate
-	,intShipViaId
-	,strPONumber
-	,intTermId
-	,dblInvoiceSubtotal
-	,dblShipping
-	,dblTax
-	,dblInvoiceTotal
-	,dblDiscount
-	,dblAmountDue
-	,dblPayment
-	,strTransactionType
-	,intPaymentMethodId
-	,strComments
-	,intAccountId
-	,0
-	,0
-	,strShipToAddress
-	,strShipToCity
-	,strShipToState
-	,strShipToZipCode
-	,strShipToCountry
-	,strBillToAddress
-	,strBillToCity
-	,strBillToState
-	,strBillToZipCode
-	,strBillToCountry
-	,0
-	,@EntityId
-FROM 
-	tblARInvoice
-WHERE
-	intInvoiceId = @InvoiceId
-	
-	
-DECLARE @NewInvoiceId int
+	INSERT INTO tblARInvoice(
+		strInvoiceNumber
+		,strInvoiceOriginId
+		,intCustomerId
+		,dtmDate
+		,dtmDueDate
+		,intCurrencyId
+		,intCompanyLocationId
+		,intSalespersonId
+		,dtmShipDate
+		,intShipViaId
+		,strPONumber
+		,intTermId
+		,dblInvoiceSubtotal
+		,dblShipping
+		,dblTax
+		,dblInvoiceTotal
+		,dblDiscount
+		,dblAmountDue
+		,dblPayment
+		,strTransactionType
+		,intPaymentMethodId
+		,strComments
+		,intAccountId
+		,ysnPosted
+		,ysnPaid
+		,strShipToAddress
+		,strShipToCity
+		,strShipToState
+		,strShipToZipCode
+		,strShipToCountry
+		,strBillToAddress
+		,strBillToCity
+		,strBillToState
+		,strBillToZipCode
+		,strBillToCountry
+		,intConcurrencyId
+		,intEntityId)
+	SELECT 
+		@InvoiceNumber
+		,strInvoiceOriginId
+		,intCustomerId
+		,@InvoiceDate
+		,dbo.fnGetDueDateBasedOnTerm(@InvoiceDate, intTermId)
+		,intCurrencyId
+		,intCompanyLocationId
+		,intSalespersonId
+		,dtmShipDate
+		,intShipViaId
+		,strPONumber
+		,intTermId
+		,dblInvoiceSubtotal
+		,dblShipping
+		,dblTax
+		,dblInvoiceTotal
+		,dblDiscount
+		,dblAmountDue
+		,dblPayment
+		,strTransactionType
+		,intPaymentMethodId
+		,strComments
+		,intAccountId
+		,0
+		,0
+		,strShipToAddress
+		,strShipToCity
+		,strShipToState
+		,strShipToZipCode
+		,strShipToCountry
+		,strBillToAddress
+		,strBillToCity
+		,strBillToState
+		,strBillToZipCode
+		,strBillToCountry
+		,0
+		,@EntityId
+	FROM 
+		tblARInvoice
+	WHERE
+		intInvoiceId = @InvoiceId
+		
+		
+	DECLARE @NewId int
 
-SET @NewInvoiceId = SCOPE_IDENTITY()
+	SET @NewId = SCOPE_IDENTITY()
 
-INSERT INTO tblARInvoiceDetail(
-	intInvoiceId
-    ,intItemId
-    ,strItemDescription
-    ,dblQtyOrdered
-    ,dblQtyShipped
-    ,dblPrice
-    ,dblTotal
-    ,intConcurrencyId)
-SELECT
-	@NewInvoiceId
-	,intItemId
-	,strItemDescription
-	,dblQtyOrdered
-	,dblQtyShipped
-	,dblPrice
-	,dblTotal
-	,intConcurrencyId
-FROM
-	tblARInvoiceDetail
-WHERE
-	intInvoiceId = @InvoiceId	
+	INSERT INTO tblARInvoiceDetail(
+		intInvoiceId
+		,intItemId
+		,strItemDescription
+		,dblQtyOrdered
+		,dblQtyShipped
+		,dblPrice
+		,dblTotal
+		,intConcurrencyId)
+	SELECT
+		@NewId
+		,intItemId
+		,strItemDescription
+		,dblQtyOrdered
+		,dblQtyShipped
+		,dblPrice
+		,dblTotal
+		,intConcurrencyId
+	FROM
+		tblARInvoiceDetail
+	WHERE
+		intInvoiceId = @InvoiceId	
+
+	SET @NewInvoiceId = @NewId
+
+	Return @NewInvoiceId
+END
