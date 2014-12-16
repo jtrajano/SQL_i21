a6e441a97a4b0e03b73c0f0374adc361932fc666
Jayson Ramos <jayson.ramos@iRely.com>
2014-12-16 16:26:33 +0800

    HD-479;

---
+++ i21Database/dbo/Views/vyuHDAgentDetail.sql
@@ -0,0 +1,14 @@
+﻿CREATE VIEW [dbo].[vyuHDAgentDetail]
+	AS
+	select
+		strName = ec.strUserName,
+		intId = us.intUserSecurityID,
+		strFullName = en.strName,
+		strEmail = en.strEmail,
+		intEntityId = us.intEntityId
+	from
+		tblSMUserSecurity us, tblEntity en, tblEntityCredential ec
+	where
+		us.intEntityId is not null
+		and en.intEntityId = us.intEntityId
+		and ec.intEntityId = us.intEntityId
