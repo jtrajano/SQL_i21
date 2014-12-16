15f42ab31e2560208560d2c415569ab6208ac5ae
kenneth lagunero <kenneth.lagunero@irely.com>
2014-12-17 07:11:51 +0800

    TM-1077

--- i21Database/dbo/Tables/tblTMFillGroup.sql
+++ i21Database/dbo/Tables/tblTMFillGroup.sql
@@ -2,7 +2,7 @@
     [intFillGroupId]   INT           IDENTITY (1, 1) NOT NULL,
     [strFillGroupCode] NVARCHAR (6)  COLLATE Latin1_General_CI_AS NOT NULL,
     [strDescription]   NVARCHAR (50) COLLATE Latin1_General_CI_AS NULL,
-    [ysnActive]        BIT           NULL,
+    [ysnActive]        BIT           NULL DEFAULT 1,
     [intConcurrencyId] INT           DEFAULT 1 NOT NULL,
     CONSTRAINT [PK_tblTMFillGroup] PRIMARY KEY CLUSTERED ([intFillGroupId] ASC),
     CONSTRAINT [UQ_tblTMFillGroup_strFillGroupCode] UNIQUE NONCLUSTERED ([strFillGroupCode] ASC)
