2763201c3c366849c1e66bf8e3700f52e9787c69
Lawrence Zabala <lawrence.zabala@irely.com>
2014-12-17 11:36:04 +0800

    Fixed Merged issues

--- server/iRely.Inventory.BRL/Activities/Receipts/Receipt_Posting.cs
+++ server/iRely.Inventory.BRL/Activities/Receipts/Receipt_Posting.cs
@@ -14,15 +14,33 @@ namespace iRely.Inventory.BRL
 {
     public partial class Receipt : IDisposable
     {
-        public void PostTransaction(tblICInventoryReceipt receipt, bool isRecap)
+        public SaveResult PostTransaction(tblICInventoryReceipt receipt, bool isRecap)
         {
             // TODO for Lawrence
             // Save the record first 
+            var result = _db.Save(false);
 
+            if (result.HasError)
+            {
+                return result;
+            }
 
             // Post the receipt transaction 
-            var db = (Inventory.Model.InventoryEntities)_db.ContextManager;
-            db.PostInventoryReceipt(isRecap, receipt.strReceiptNumber, iRely.Common.Security.GetUserId(), iRely.Common.Security.GetEntityId()); 
+            var postResult = new SaveResult();
+            try
+            {
+                var db = (Inventory.Model.InventoryEntities)_db.ContextManager;
+                db.PostInventoryReceipt(isRecap, receipt.strReceiptNumber, iRely.Common.Security.GetUserId(), iRely.Common.Security.GetEntityId());
+
+                postResult.HasError = false;
+            }
+            catch (Exception ex)
+            {
+                postResult.BaseException = ex;
+                postResult.HasError = true;
+                postResult.Exception = new ServerException(ex, Error.OtherException, Button.Ok);
+            }
+            return postResult;
         }
 
         public void UnpostTransaction(string transaction, bool isRecap)
