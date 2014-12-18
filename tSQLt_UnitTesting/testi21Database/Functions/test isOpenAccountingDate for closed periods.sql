551adca8100adb450a265a74962dac3c235e2f91
Feb Montefrio <feb.montefrio@irely.com>
2014-12-18 09:20:03 +0800

    IC-100 Add unit tests for isOpenAccountingDate

---
+++ tSQLt_UnitTesting/testi21Database/Functions/test isOpenAccountingDate for closed periods.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE testi21Database.[test isOpenAccountingDate for closed periods]
+AS 
+BEGIN
+	-- Arrange
+	DECLARE @result AS BIT
+	DECLARE @expected AS BIT = 0
+
+	-- Fake the inventory transaction table 
+	EXEC tSQLt.FakeTable 'dbo.tblGLFiscalYearPeriod', @Identity = 1;
+	INSERT INTO tblGLFiscalYearPeriod (dtmStartDate, dtmEndDate, ysnOpen) VALUES (GETDATE(), GETDATE(), 0)
+
+	-- Act
+	SELECT @result = dbo.isOpenAccountingDate(GETDATE());
+
+	-- Assert 
+	-- Result must be 0 since the date is closed
+	EXEC tSQLt.AssertEquals @expected, @result;
+END 
