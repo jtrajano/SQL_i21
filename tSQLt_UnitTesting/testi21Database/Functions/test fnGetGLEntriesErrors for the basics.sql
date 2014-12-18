d57d0e59e3dc57fbd03f73e4177340a8ad0d41b2
Feb Montefrio <feb.montefrio@irely.com>
2014-12-18 14:03:46 +0800

    IC-100 Add unit testing for fnGetGLEntriesErrors

---
+++ tSQLt_UnitTesting/testi21Database/Functions/test fnGetGLEntriesErrors for the basics.sql
@@ -0,0 +1,41 @@
+﻿CREATE PROCEDURE testi21Database.[test fnGetGLEntriesErrors for the basics]
+AS 
+BEGIN
+	-- Arrange
+	BEGIN 	
+		CREATE TABLE expected (
+			strTransactionId NVARCHAR(40)
+			,strText NVARCHAR(MAX) NULL
+			,intErrorCode INT
+		)
+
+		CREATE TABLE actual (
+			strTransactionId NVARCHAR(40)
+			,strText NVARCHAR(MAX) NULL
+			,intErrorCode INT
+		)
+
+		DECLARE @GLEntries AS RecapTableType
+
+		-- Call the fake data for GL Account 
+		EXEC testi21Database.[Fake data for simple COA];
+	END 
+
+	-- Act
+	BEGIN
+		INSERT INTO actual
+		SELECT * FROM dbo.fnGetGLEntriesErrors(@GLEntries)
+	END 
+
+	-- Assert
+	BEGIN 		
+		EXEC tSQLt.AssertEqualsTable 'expected', 'actual';
+	END
+
+	-- Clean-up: remove the tables used in the unit test
+	IF OBJECT_ID('actual') IS NOT NULL 
+		DROP TABLE actual
+
+	IF OBJECT_ID('expected') IS NOT NULL 
+		DROP TABLE expected
+END 
