1fe87f516dc35a8ac3aa5af6c70d46e6ffbcabc2
Lawrence Zabala <lawrence.zabala@irely.com>
2014-12-22 13:44:41 +0800

    IC-241

---
+++ i21Database/dbo/Tables/tblICInventoryShipmentItem.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE [dbo].[tblICInventoryShipmentItem]
+(
+	[intInventoryShipmentItemId] INT NOT NULL IDENTITY, 
+	[intInventoryShipmentId] INT NOT NULL, 
+    [strReferenceNumber] NVARCHAR(50) NULL, 
+    [intItemId] INT NOT NULL, 
+    [intSubLocationId] INT NULL, 
+    [dblQuantity] NUMERIC(18, 6) NOT NULL DEFAULT ((0)), 
+    [intUnitMeasureId] INT NOT NULL, 
+    [intWeightUomId] INT NULL, 
+    [dblTareWeight] NUMERIC(18, 6) NULL DEFAULT ((0)), 
+    [dbNetWeight] NUMERIC(18, 6) NULL DEFAULT ((0)), 
+    [dblUnitPrice] NUMERIC(18, 6) NULL DEFAULT ((0)), 
+    [intDockDoorId] INT NULL, 
+    [strNotes] NVARCHAR(MAX) NULL, 
+    [intSort] INT NULL, 
+    [intConcurrencyId] INT NULL DEFAULT ((0)), 
+    CONSTRAINT [PK_tblICInventoryShipmentItem] PRIMARY KEY ([intInventoryShipmentItemId]), 
+    CONSTRAINT [FK_tblICInventoryShipmentItem_tblICInventoryShipment] FOREIGN KEY ([intInventoryShipmentId]) REFERENCES [tblICInventoryShipment]([intInventoryShipmentId]) ON DELETE CASCADE, 
+    CONSTRAINT [FK_tblICInventoryShipmentItem_tblICItem] FOREIGN KEY ([intItemId]) REFERENCES [tblICItem]([intItemId])
+)
---
+++ i21Database/dbo/Tables/tblICInventoryShipmentItemLot.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[tblICInventoryShipmentItemLot]
+(
+	[intInventoryShipmentItemLotId] INT NOT NULL IDENTITY, 
+	[intInventoryShipmentItemId] INT NOT NULL, 
+    [intLotId] INT NOT NULL, 
+    [dblQuantityShipped] NUMERIC(18, 6) NULL, 
+    [strWhseCargoNumber] NVARCHAR(50) NULL, 
+    [intSort] INT NULL, 
+    [intConcurrencyId] INT NULL DEFAULT ((0)), 
+    CONSTRAINT [PK_tblICInventoryShipmentItemLot] PRIMARY KEY ([intInventoryShipmentItemLotId]), 
+    CONSTRAINT [FK_tblICInventoryShipmentItemLot_tblICInventoryShipmentItem] FOREIGN KEY ([intInventoryShipmentItemId]) REFERENCES [tblICInventoryShipmentItem]([intInventoryShipmentItemId]) ON DELETE CASCADE, 
+    CONSTRAINT [FK_tblICInventoryShipmentItemLot_tblICLot] FOREIGN KEY ([intLotId]) REFERENCES [tblICLot]([intLotId]) 
+)
