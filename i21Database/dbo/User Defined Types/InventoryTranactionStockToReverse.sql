94efe29e6cf52aa926f298901123dff650050b65
Feb Montefrio <feb.montefrio@irely.com>
2014-12-22 19:01:12 +0800

    IC-100 Add uspICUnpostFIFOOut Add a basic unit test for uspICUnpostFIFOOut Add new type - InventoryTransactionStockToReverse

---
+++ i21Database/dbo/User Defined Types/InventoryTranactionStockToReverse.sql
@@ -0,0 +1,16 @@
+﻿/*
+	This is a user-defined table type used unposting an inventory costing. 
+*/
+
+CREATE TYPE [dbo].[InventoryTranactionStockToReverse] AS TABLE
+(
+	intInventoryTransactionId INT NOT NULL 
+	,intTransactionId INT NULL 
+	,strTransactionId NVARCHAR(40) COLLATE Latin1_General_CI_AS NULL
+	,strRelatedInventoryTransactionId NVARCHAR(40) COLLATE Latin1_General_CI_AS NULL
+	,intRelatedInventoryTransactionId INT NULL 
+	,intTransactionTypeId INT NOT NULL 
+)
+
+
+
---
+++ tSQLt_UnitTesting/testi21Database/Stored Procedures/test uspICUnpostFIFOOut for the basics.sql
@@ -0,0 +1,41 @@
+﻿CREATE PROCEDURE [testi21Database].[test uspICUnpostFIFOOut for the basics]
+AS
+BEGIN
+	-- Arrange 
+	BEGIN 
+		DECLARE @strTransactionId AS NVARCHAR(40)
+		DECLARE @intTransactionId AS INT
+
+		CREATE TABLE actual (
+			strTransactionId NVARCHAR(40) COLLATE Latin1_General_CI_AS NULL
+			,intTransactionId INT
+			,dblStockIn NUMERIC(18,6)
+			,dblStockOut NUMERIC(18,6)
+		)
+
+		CREATE TABLE expected (
+			strTransactionId NVARCHAR(40) COLLATE Latin1_General_CI_AS NULL
+			,intTransactionId INT
+			,dblStockIn NUMERIC(18,6)
+			,dblStockOut NUMERIC(18,6)
+		)
+	END 
+	
+	-- Act
+	BEGIN 
+		EXEC dbo.uspICUnpostFIFOOut @strTransactionId, @intTransactionId
+	END 
+
+	-- Assert
+	BEGIN 
+		EXEC tSQLt.AssertEqualsTable 'expected', 'actual';
+	END
+
+	-- Clean-up: remove the tables used in the unit test
+	IF OBJECT_ID('actual') IS NOT NULL 
+		DROP TABLE actual
+
+	IF OBJECT_ID('expected') IS NOT NULL 
+		DROP TABLE dbo.expected
+
+END
