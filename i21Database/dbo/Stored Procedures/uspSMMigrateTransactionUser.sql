b0adf2240259978a86a23a1f743ec57552ec3253
Feb Montefrio <feb.montefrio@irely.com>
2015-01-05 11:28:46 +0800

    Fix bug caused by commit in 04f340355b671e2c1032e4a1514d9f8fff5e4ae3

--- i21Database/dbo/Stored Procedures/uspSMMigrateTransactionUser.sql
+++ i21Database/dbo/Stored Procedures/uspSMMigrateTransactionUser.sql
@@ -20,14 +20,14 @@ BEGIN
 	
 	WHILE EXISTS(SELECT TOP 1 1 FROM #tmpGLTables)
 	BEGIN
-		SELECT TOP 1 @tablename = [Table_Name] FROM #tmpGLTables
+		SELECT TOP 1 @tablename = [TABLE_NAME] FROM #tmpGLTables
 		
 		IF EXISTS(SELECT * FROM sys.columns WHERE name = N'intUserId' AND object_id = OBJECT_ID(@tablename))
 		BEGIN
 			EXEC('UPDATE ' + @tablename + ' SET intEntityId = dbo.fnGetEntityIdFromUser(intUserId) WHERE ISNULL(intEntityId, -1) = -1')
 		END
 		
-		DELETE FROM #tmpGLTables WHERE [Table_Name] = @tablename
+		DELETE FROM #tmpGLTables WHERE [TABLE_NAME] = @tablename
 	END
 	
 	DROP TABLE #tmpGLTables

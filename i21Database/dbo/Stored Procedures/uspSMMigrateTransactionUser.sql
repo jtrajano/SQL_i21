jtrajano/SQL_i21

commit 04f340355b671e2c1032e4a1514d9f8fff5e4ae3

Suppress errors for SQL70588 Fix or suppress errors for SQL70512

i21Database/dbo/Stored Procedures/uspSMMigrateTransactionUser.sql
@@ -12,17 +12,17 @@ BEGIN
 
 	DECLARE @tablename NVARCHAR(100)
 	
-	SELECT [Table_Name]
+	SELECT TABLE_NAME
 	INTO #tmpGLTables
 	FROM INFORMATION_SCHEMA.COLUMNS
-	WHERE [Column_Name] = 'intEntityId'
-	AND [Table_Name] LIKE 'tbl' + @Module + '%'
+	WHERE COLUMN_NAME = 'intEntityId'
+	AND TABLE_NAME LIKE 'tbl' + @Module + '%'
 	
 	WHILE EXISTS(SELECT TOP 1 1 FROM #tmpGLTables)
 	BEGIN
 		SELECT TOP 1 @tablename = [Table_Name] FROM #tmpGLTables
 		
-		IF EXISTS(SELECT * FROM sys.columns WHERE Name = N'intUserId' AND OBJECT_ID = OBJECT_ID(@tablename))
+		IF EXISTS(SELECT * FROM sys.columns WHERE name = N'intUserId' AND object_id = OBJECT_ID(@tablename))
 		BEGIN
 			EXEC('UPDATE ' + @tablename + ' SET intEntityId = dbo.fnGetEntityIdFromUser(intUserId) WHERE ISNULL(intEntityId, -1) = -1')
 		END

Integration/Integration.sqlproj
@@ -224,6 +224,14 @@
     <None Include="dbo\Stored Procedures\uspARImportTaxAuthority.sql" />
     <None Include="dbo\Stored Procedures\uspARImportTerm.sql" />
     <None Include="dbo\Stored Procedures\uspSMImportCompanyLocation.sql" />
-    <Build Include="dbo\Stored Procedures\uspSMSyncCompanyLocation.sql" />
+    <Build Include="dbo\Stored Procedures\uspSMSyncCompanyLocation.sql">
+      <SuppressTSqlWarnings>71502</SuppressTSqlWarnings>
+    </Build>
+  </ItemGroup>
+  <ItemGroup>
+    <SqlCmdVariable Include="i21Database">
+      <DefaultValue>i21Database</DefaultValue>
+      <Value>$(SqlCmdVar__1)</Value>
+    </SqlCmdVariable>
   </ItemGroup>
 </Project>

i21Database/dbo/Stored Procedures/uspGLBuildTempCOASegment.sql
@@ -3,8 +3,16 @@
 AS
 BEGIN
 	--CREATE DYNAMIC ACCOUNT STRUCTURE
-	IF EXISTS (SELECT top 1 1  FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'tempDASTable') DROP TABLE tempDASTable 
-	IF EXISTS (SELECT top 1 1  FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'tblGLTempCOASegment') DROP TABLE tblGLTempCOASegment
+	IF EXISTS (SELECT top 1 1  FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'tempDASTable') 
+	BEGIN 
+		EXEC ('DROP TABLE tempDASTable ');
+	END
+
+	IF EXISTS (SELECT top 1 1  FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'tblGLTempCOASegment') 
+	BEGIN 
+		EXEC ('DROP TABLE tblGLTempCOASegment');
+	END 
+
 	BEGIN
 			DECLARE @Segments NVARCHAR(MAX)
 			SELECT @Segments = ISNULL(SUBSTRING((SELECT '],[' + strStructureName FROM tblGLAccountStructure WHERE strType <> 'Divider' FOR XML PATH('')),3,200000) + ']','[Primary Account]')
@@ -36,7 +44,11 @@ BEGIN
 	END
 			
 			
-	IF EXISTS (SELECT top 1 1  FROM INFORMATION_SCHEMA.VIEWS WHERE TABLE_NAME = 'vyuGLDetailView') DROP VIEW vyuGLDetailView
+	IF EXISTS (SELECT top 1 1  FROM INFORMATION_SCHEMA.VIEWS WHERE TABLE_NAME = 'vyuGLDetailView') 
+	BEGIN 
+		EXEC ('DROP VIEW vyuGLDetailView');
+	END 
+
 	IF EXISTS (SELECT top 1 1  FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'tblGLTempCOASegment')
 	BEGIN
 		EXEC ('CREATE VIEW [dbo].[vyuGLDetailView]
@@ -51,7 +63,11 @@ BEGIN
 	END
 
 	
-	IF EXISTS (SELECT top 1 1  FROM INFORMATION_SCHEMA.VIEWS WHERE TABLE_NAME = 'vyuGLAccountView') DROP VIEW vyuGLAccountView
+	IF EXISTS (SELECT top 1 1  FROM INFORMATION_SCHEMA.VIEWS WHERE TABLE_NAME = 'vyuGLAccountView') 
+	BEGIN 
+		EXEC ('DROP VIEW vyuGLAccountView');
+	END 
+
 	IF EXISTS (SELECT top 1 1  FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'tblGLTempCOASegment')
 	BEGIN
 		EXEC ('CREATE VIEW [dbo].[vyuGLAccountView]

i21Database/dbo/Stored Procedures/uspRMInsertDynamicParameterFields.sql
@@ -43,7 +43,7 @@ BEGIN
 	INSERT INTO @DynamicColumns
 	SELECT COLUMN_NAME
 	FROM   INFORMATION_SCHEMA.COLUMNS
-	WHERE  table_name = 'tblGLTempCOASegment'
+	WHERE  TABLE_NAME = 'tblGLTempCOASegment'
 
 	--CRITERIA FIELDSELECTION VARIABLES
 		DECLARE @CFSId NVARCHAR(MAX)

i21Database/dbo/Stored Procedures/uspSMBuildCustomTable.sql
@@ -55,8 +55,8 @@ BEGIN
 	FROM #tmpCustomFieldDetail
 	
 	SELECT @existingCount = COUNT(*) FROM sys.columns 
-	WHERE OBJECT_ID = ( SELECT OBJECT_ID FROM sys.tables 
-						WHERE NAME = @tableName) AND NAME = @fieldName
+	WHERE object_id = ( SELECT object_id FROM sys.tables 
+						WHERE name = @tableName) AND name = @fieldName
 	
 	IF (@existingCount = 0)
 		BEGIN

i21Database/i21Database.sqlproj
@@ -339,8 +339,12 @@
     <Build Include="dbo\Stored Procedures\uspCMApplyCheckChangeBankReconciliation.sql" />
     <Build Include="dbo\Stored Procedures\uspCMApplyCheckChangeForCheckPrint.sql" />
     <Build Include="dbo\Stored Procedures\uspCMApplyCheckChangeForPrintFail.sql" />
-    <Build Include="dbo\Stored Procedures\uspCMBookGLEntries.sql" />
-    <Build Include="dbo\Stored Procedures\uspCMCheckPrint_QueuePrintJobs.sql" />
+    <Build Include="dbo\Stored Procedures\uspCMBookGLEntries.sql">
+      <SuppressTSqlWarnings>71502</SuppressTSqlWarnings>
+    </Build>
+    <Build Include="dbo\Stored Procedures\uspCMCheckPrint_QueuePrintJobs.sql">
+      <SuppressTSqlWarnings>71502</SuppressTSqlWarnings>
+    </Build>
     <Build Include="dbo\Stored Procedures\uspCMCheckPrint_ValidatePrintJobs.sql" />
     <Build Include="dbo\Stored Procedures\uspCMCommitPrintCheckVerification.sql" />
     <Build Include="dbo\Stored Procedures\uspCMGenerateCheckNumbers.sql" />
@@ -360,8 +364,12 @@
     <Build Include="dbo\Stored Procedures\uspCMPostMiscChecks.sql" />
     <Build Include="dbo\Stored Procedures\uspSMErrorMessages.sql" />
     <Build Include="dbo\Stored Procedures\uspCMPostRecap.sql" />
-    <Build Include="dbo\Stored Procedures\uspCMReconcileBankRecords.sql" />
-    <Build Include="dbo\Stored Procedures\uspCMReverseGLEntries.sql" />
+    <Build Include="dbo\Stored Procedures\uspCMReconcileBankRecords.sql">
+      <SuppressTSqlWarnings>71502</SuppressTSqlWarnings>
+    </Build>
+    <Build Include="dbo\Stored Procedures\uspCMReverseGLEntries.sql">
+      <SuppressTSqlWarnings>71502</SuppressTSqlWarnings>
+    </Build>
     <Build Include="dbo\Stored Procedures\uspGLAccountClone.sql" />
     <Build Include="dbo\Stored Procedures\uspGLAccountOriginSync.sql" />
     <Build Include="dbo\Stored Procedures\uspGLBuildAccount.sql" />
@@ -369,7 +377,9 @@
     <Build Include="dbo\Stored Procedures\uspGLBuildOriginAccount.sql" />
     <Build Include="dbo\Stored Procedures\uspGLBuildTempCOASegment.sql" />
     <Build Include="dbo\Stored Procedures\uspGLImportOriginCOA.sql" />
-    <Build Include="dbo\Stored Procedures\uspGLImportOriginHistoricalJournal.sql" />
+    <Build Include="dbo\Stored Procedures\uspGLImportOriginHistoricalJournal.sql">
+      <SuppressTSqlWarnings>71502</SuppressTSqlWarnings>
+    </Build>
     <Build Include="dbo\Stored Procedures\uspGLPostFiscalYear.sql" />
     <Build Include="dbo\Stored Procedures\uspAPPostBill.sql" />
     <Build Include="dbo\Stored Procedures\uspAPPostBillBatch.sql" />
@@ -556,7 +566,9 @@
     <Build Include="dbo\Tables\tblFRMappingDetail.sql" />
     <Build Include="dbo\Views\vyuAPBill.sql" />
     <Build Include="dbo\Stored Procedures\uspGLRecreateCOASegmentMapping.sql" />
-    <Build Include="dbo\Stored Procedures\uspCMBankTransactionReversal.sql" />
+    <Build Include="dbo\Stored Procedures\uspCMBankTransactionReversal.sql">
+      <SuppressTSqlWarnings>71502</SuppressTSqlWarnings>
+    </Build>
     <Build Include="dbo\Functions\fnGetBankTransactionReversalErrors.sql" />
     <Build Include="dbo\Stored Procedures\uspGLFixStartingNumbers.sql" />
     <Build Include="dbo\Tables\tblSMSearchFilter.sql" />
@@ -570,7 +582,9 @@
     <Build Include="dbo\Functions\fnTrim.sql" />
     <Build Include="dbo\Views\vyuHDExportHoursWork.sql" />
     <Build Include="dbo\Tables\tblARPostResult.sql" />
-    <Build Include="dbo\Stored Procedures\uspARPostPayment.sql" />
+    <Build Include="dbo\Stored Procedures\uspARPostPayment.sql">
+      <SuppressTSqlWarnings>71502</SuppressTSqlWarnings>
+    </Build>
     <Build Include="dbo\Stored Procedures\uspARFixStartingNumbers.sql" />
     <Build Include="dbo\Tables\cstGLJournal.sql" />
     <Build Include="dbo\Tables\cstARCustomer.sql" />
@@ -790,7 +804,9 @@
     <Build Include="dbo\User Defined Types\Entity.sql" />
     <Build Include="dbo\User Defined Types\EntityContact.sql" />
     <Build Include="dbo\Stored Procedures\uspEntityCreateEntityContact.sql" />
-    <Build Include="dbo\Stored Procedures\uspARContactOriginSync.sql" />
+    <Build Include="dbo\Stored Procedures\uspARContactOriginSync.sql">
+      <SuppressTSqlWarnings>71502</SuppressTSqlWarnings>
+    </Build>
     <Build Include="dbo\Tables\tblNRAdjustmentType.sql" />
     <Build Include="dbo\Tables\tblNRNote.sql" />
     <Build Include="dbo\Tables\tblNRNoteDescription.sql" />
@@ -800,7 +816,9 @@
     <Build Include="dbo\Tables\tblNRNoteTransType.sql" />
     <Build Include="dbo\Tables\tblNRScheduleTransaction.sql" />
     <Build Include="dbo\Tables\tblNRUCC.sql" />
-    <Build Include="dbo\Stored Procedures\uspARValidations.sql" />
+    <Build Include="dbo\Stored Procedures\uspARValidations.sql">
+      <SuppressTSqlWarnings>71502</SuppressTSqlWarnings>
+    </Build>
     <Build Include="dbo\Stored Procedures\uspSMCreateImport.sql" />
     <Build Include="dbo\Stored Procedures\uspSMBuildTable.sql" />
     <Build Include="dbo\Tables\tblICInventoryFIFOOut.sql" />
@@ -834,19 +852,37 @@
     <Build Include="dbo\Views\vyuARInvoiceSearch.sql" />
     <Build Include="dbo\Tables\tblCTBuySell.sql" />
     <Build Include="dbo\Tables\tblCTCashOrFuture.sql" />
-    <Build Include="dbo\Tables\tblCTContractCost.sql" />
-    <Build Include="dbo\Tables\tblCTContractDetail.sql" />
-    <Build Include="dbo\Tables\tblCTContractHeader.sql" />
-    <Build Include="dbo\Tables\tblCTContractOptDetail.sql" />
+    <Build Include="dbo\Tables\tblCTContractCost.sql">
+      <SuppressTSqlWarnings>70588</SuppressTSqlWarnings>
+    </Build>
+    <Build Include="dbo\Tables\tblCTContractDetail.sql">
+      <SuppressTSqlWarnings>70588</SuppressTSqlWarnings>
+    </Build>
+    <Build Include="dbo\Tables\tblCTContractHeader.sql">
+      <SuppressTSqlWarnings>70588</SuppressTSqlWarnings>
+    </Build>
+    <Build Include="dbo\Tables\tblCTContractOptDetail.sql">
+      <SuppressTSqlWarnings>70588</SuppressTSqlWarnings>
+    </Build>
     <Build Include="dbo\Tables\tblCTContractOptHeader.sql" />
-    <Build Include="dbo\Tables\tblCTContractOption.sql" />
+    <Build Include="dbo\Tables\tblCTContractOption.sql">
+      <SuppressTSqlWarnings>70588</SuppressTSqlWarnings>
+    </Build>
     <Build Include="dbo\Tables\tblCTContractText.sql" />
     <Build Include="dbo\Tables\tblCTCostMethod.sql" />
-    <Build Include="dbo\Tables\tblCTCostType.sql" />
-    <Build Include="dbo\Tables\tblCTCropYear.sql" />
+    <Build Include="dbo\Tables\tblCTCostType.sql">
+      <SuppressTSqlWarnings>70588</SuppressTSqlWarnings>
+    </Build>
+    <Build Include="dbo\Tables\tblCTCropYear.sql">
+      <SuppressTSqlWarnings>70588</SuppressTSqlWarnings>
+    </Build>
     <Build Include="dbo\Tables\tblCTDeferredPay.sql" />
-    <Build Include="dbo\Tables\tblCTDeferredPayRate.sql" />
-    <Build Include="dbo\Tables\tblCTFreightRate.sql" />
+    <Build Include="dbo\Tables\tblCTDeferredPayRate.sql">
+      <SuppressTSqlWarnings>70588</SuppressTSqlWarnings>
+    </Build>
+    <Build Include="dbo\Tables\tblCTFreightRate.sql">
+      <SuppressTSqlWarnings>70588</SuppressTSqlWarnings>
+    </Build>
     <Build Include="dbo\Tables\tblCTPremFee.sql" />
     <Build Include="dbo\Tables\tblCTPricingType.sql" />
     <Build Include="dbo\Tables\tblCTPutCall.sql" />
@@ -902,7 +938,7 @@
     <Reference Include="System" />
   </ItemGroup>
   <ItemGroup>
-    <Compile Include="C:\SharedAssemblyInfo.cs" >
+    <Compile Include="C:\SharedAssemblyInfo.cs">
       <Link>Properties\SharedAssemblyInfo.cs</Link>
     </Compile>
   </ItemGroup>

commit b0adf2240259978a86a23a1f743ec57552ec3253

Fix bug caused by commit in 04f340355b671e2c1032e4a1514d9f8fff5e4ae3

i21Database/dbo/Stored Procedures/uspSMMigrateTransactionUser.sql
@@ -20,14 +20,14 @@ BEGIN
 	
 	WHILE EXISTS(SELECT TOP 1 1 FROM #tmpGLTables)
 	BEGIN
-		SELECT TOP 1 @tablename = [Table_Name] FROM #tmpGLTables
+		SELECT TOP 1 @tablename = [TABLE_NAME] FROM #tmpGLTables
 		
 		IF EXISTS(SELECT * FROM sys.columns WHERE name = N'intUserId' AND object_id = OBJECT_ID(@tablename))
 		BEGIN
 			EXEC('UPDATE ' + @tablename + ' SET intEntityId = dbo.fnGetEntityIdFromUser(intUserId) WHERE ISNULL(intEntityId, -1) = -1')
 		END
 		
-		DELETE FROM #tmpGLTables WHERE [Table_Name] = @tablename
+		DELETE FROM #tmpGLTables WHERE [TABLE_NAME] = @tablename
 	END
 	
 	DROP TABLE #tmpGLTables