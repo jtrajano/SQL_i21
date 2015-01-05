dffee22f13cf51eba987a0e0d3228b1e7051655f
Lawrence Zabala <lawrence.zabala@irely.com>
2015-01-05 20:16:15 +0800

    IC-262

---
+++ i21Database/dbo/Views/vyuSMGetLocationPricingLevel.sql
@@ -0,0 +1,14 @@
+﻿CREATE VIEW [dbo].[vyuSMGetLocationPricingLevel]
+
+AS 
+
+SELECT * FROM (
+	SELECT intCompanyLocationId, strPriceLevel1 AS strPriceLevel from tblSMCompanyLocation
+	UNION ALL SELECT intCompanyLocationId, strPriceLevel2 AS strPriceLevel from tblSMCompanyLocation
+	UNION ALL SELECT intCompanyLocationId, strPriceLevel3 AS strPriceLevel from tblSMCompanyLocation
+	UNION ALL SELECT intCompanyLocationId, strPriceLevel4 AS strPriceLevel from tblSMCompanyLocation
+	UNION ALL SELECT intCompanyLocationId, strPriceLevel5 AS strPriceLevel from tblSMCompanyLocation
+	) tblLocationPriceLevel
+WHERE ISNULL(strPriceLevel, '') <> ''
+
+GO
