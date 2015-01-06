df1c6e12a9813fc4b18f8c3c0180dd02926be2bc
Lawrence Zabala <lawrence.zabala@irely.com>
2015-01-06 11:00:16 +0800

    IC-262

--- i21Database/dbo/Views/vyuSMGetLocationPricingLevel.sql
+++ i21Database/dbo/Views/vyuSMGetLocationPricingLevel.sql
@@ -2,7 +2,8 @@
 
 AS 
 
-SELECT * FROM (
+SELECT intKey = CAST(ROW_NUMBER() OVER(ORDER BY tblLocationPriceLevel.intCompanyLocationId, tblLocationPriceLevel.strPriceLevel) AS INT),
+* FROM (
 	SELECT intCompanyLocationId, strPriceLevel1 AS strPriceLevel from tblSMCompanyLocation
 	UNION ALL SELECT intCompanyLocationId, strPriceLevel2 AS strPriceLevel from tblSMCompanyLocation
 	UNION ALL SELECT intCompanyLocationId, strPriceLevel3 AS strPriceLevel from tblSMCompanyLocation
