ca2432de979e873456de5c1e35946480fadb0e8c
Ravikumar <ravikumar.k@irely.com>
2015-01-07 21:02:52 +0530

    Added 'tblSTRegister' table and modified some of the existing tables.

---
+++ i21Database/dbo/Tables/tblSTPaymentOption.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[tblSTPaymentOption]
+(
+	[intPaymentOptionId] INT NOT NULL IDENTITY, 
+    [strPaymentOption] NVARCHAR COLLATE Latin1_General_CI_AS NOT NULL, 
+    [intStoreId] INT NOT NULL, 
+    [strPaymentOptionId] NVARCHAR(15) COLLATE Latin1_General_CI_AS NOT NULL, 
+    [strDescription] NVARCHAR(40) COLLATE Latin1_General_CI_AS NULL, 
+    [intAccountId] INT NULL, 
+    [intPaymentMethodId] INT NULL, 
+    [intEntityId] INT NULL, 
+    [dblFeePerTransction] NUMERIC(6, 4) NULL, 
+    [dblDiscountFeePercent] NUMERIC(6, 4) NULL, 
+    [intConcurrencyId] INT NOT NULL, 
+    CONSTRAINT [PK_tblSTPaymentOption] PRIMARY KEY CLUSTERED ([intPaymentOptionId]), 
+    CONSTRAINT [AK_tblSTPaymentOption_intStoreId_strPaymentOptionId] UNIQUE NONCLUSTERED ([intStoreId],[strPaymentOptionId]), 
+	CONSTRAINT [FK_tblSTPaymentOption_tblSTStore] FOREIGN KEY ([intStoreId]) REFERENCES [tblSTStore]([intStoreId]),
+    CONSTRAINT [FK_tblSTPaymentOption_tblGLAccount] FOREIGN KEY ([intAccountId]) REFERENCES [tblGLAccount]([intAccountId]), 
+    CONSTRAINT [FK_tblSTPaymentOption_tblSTPaymentMethod] FOREIGN KEY ([intPaymentMethodId]) REFERENCES [tblSTPaymentMethod]([intPaymentMethodId]), 
+    CONSTRAINT [FK_tblSTPaymentOption_tblARCustomer] FOREIGN KEY ([intEntityId]) REFERENCES [tblARCustomer]([intEntityId]) 
+  );
---
+++ i21Database/dbo/Tables/tblSTPromotionItemListDetail.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[tblSTPromotionItemListDetail]
+(
+	[intPromoItemListDetailId] INT NOT NULL IDENTITY, 
+    [intPromoItemListId] INT NOT NULL, 
+    [intItemUOMId] INT NOT NULL, 
+    [intUpcModifier] INT NOT NULL, 
+    [dblRetailPrice] NUMERIC(7, 2) NULL, 
+    [intConcurrencyId] INT NOT NULL, 
+    CONSTRAINT [PK_tblSTPromotionItemListDetail] PRIMARY KEY ([intPromoItemListDetailId]), 
+    CONSTRAINT [FK_tblSTPromotionItemListDetail_tblICItemUOM] FOREIGN KEY ([intItemUOMId]) REFERENCES [tblICItemUOM]([intItemUOMId])
+);
+Go
+ALTER TABLE [dbo].[tblSTPromotionItemListDetail]  WITH CHECK ADD  CONSTRAINT [FK_tblSTPromotionItemListDetail_tblSTPromotionItemList_intPromoItemListId] FOREIGN KEY([intPromoItemListId])
+REFERENCES [dbo].[tblSTPromotionItemList] ([intPromoItemListId]) ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[tblSTPromotionItemListDetail] CHECK CONSTRAINT [FK_tblSTPromotionItemListDetail_tblSTPromotionItemList_intPromoItemListId]
+GO
