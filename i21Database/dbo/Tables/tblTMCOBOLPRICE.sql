2322ec1012088f8990f1b2b971cd1741453072d1
kenneth lagunero <kenneth.lagunero@irely.com>
2015-01-09 10:44:22 +0800

    TM-973

--- i21Database/dbo/Tables/tblTMCOBOLPRICE.sql
+++ i21Database/dbo/Tables/tblTMCOBOLPRICE.sql
@@ -1,8 +1,20 @@
 ﻿CREATE TABLE [dbo].[tblTMCOBOLPRICE] (
-    [CustomerNumber]       CHAR (10)       CONSTRAINT [DEF_tblTMCOBOLPRICE_CustomerNumber] DEFAULT ((0)) NOT NULL,
-    [SiteNumber]           CHAR (4)        CONSTRAINT [DEF_tblTMCOBOLPRICE_SiteNumber] DEFAULT ((0)) NOT NULL,
-    [Price]        DECIMAL (18, 6) CONSTRAINT [DEF_tblTMCOBOLPRICE_TotalCapacity] DEFAULT ((0)) NOT NULL,
-	[LastUpdateDate]        CHAR (8) CONSTRAINT [DEF_tblTMCOBOLPRICE_LastUpdateDate] DEFAULT (('00000000')) NOT NULL,
-	[LastUpdateTime]        CHAR (8) CONSTRAINT [DEF_tblTMCOBOLPRICE_LastUpdateTime] DEFAULT (('00000000')) NOT NULL,
+    [CustomerNumber] CHAR (10)       CONSTRAINT [DEF_tblTMCOBOLPRICE_CustomerNumber] DEFAULT ((0)) NOT NULL,
+    [SiteNumber]     CHAR (4)        CONSTRAINT [DEF_tblTMCOBOLPRICE_SiteNumber] DEFAULT ((0)) NOT NULL,
+    [Price]          DECIMAL (18, 6) CONSTRAINT [DEF_tblTMCOBOLPRICE_TotalCapacity] DEFAULT ((0)) NOT NULL,
+    [LastUpdateDate] CHAR (8)        CONSTRAINT [DEF_tblTMCOBOLPRICE_LastUpdateDate] DEFAULT ('00000000') NOT NULL,
+    [LastUpdateTime] CHAR (8)        CONSTRAINT [DEF_tblTMCOBOLPRICE_LastUpdateTime] DEFAULT ('00000000') NOT NULL
 );
+
+
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_tblTMCOBOLPRICE_SiteNumber]
+    ON [dbo].[tblTMCOBOLPRICE]([SiteNumber] ASC);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_tblTMCOBOLPRICE_CustomerNumber]
+    ON [dbo].[tblTMCOBOLPRICE]([CustomerNumber] ASC);
 
