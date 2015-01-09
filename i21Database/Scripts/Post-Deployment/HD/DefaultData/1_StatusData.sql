66aee4ff09f5df6263b580ea1cc6dc0e291b4262
Jayson Ramos <jayson.ramos@iRely.com>
2015-01-09 18:40:54 +0800

    HD-517;

--- i21Database/Scripts/Post-Deployment/HD/DefaultData/1_StatusData.sql
+++ i21Database/Scripts/Post-Deployment/HD/DefaultData/1_StatusData.sql
@@ -16,6 +16,7 @@ GO
 
 IF NOT EXISTS (SELECT TOP 1 1 FROM [tblHDTicketStatus] WHERE strStatus = 'Open') INSERT [dbo].[tblHDTicketStatus] ([strStatus], [strDescription], [strIcon], [strFontColor], [strBackColor], [intSort], [intConcurrencyId]) VALUES (N'Open', N'Open', NULL, NULL, NULL, 1, 1)
 IF NOT EXISTS (SELECT TOP 1 1 FROM [tblHDTicketStatus] WHERE strStatus = 'Closed') INSERT [dbo].[tblHDTicketStatus] ([strStatus], [strDescription], [strIcon], [strFontColor], [strBackColor], [intSort], [intConcurrencyId]) VALUES (N'Closed', N'Closed', NULL, NULL, NULL, 2, 1)
+IF NOT EXISTS (SELECT TOP 1 1 FROM [tblHDTicketStatus] WHERE strStatus = 'Reopen') INSERT [dbo].[tblHDTicketStatus] ([strStatus], [strDescription], [strIcon], [strFontColor], [strBackColor], [intSort], [intConcurrencyId]) VALUES (N'Reopen', N'Reopen', NULL, NULL, NULL, 3, 1)
 
 GO
 	PRINT N'END INSERT DEFAULT HELP DESK STATUS'
