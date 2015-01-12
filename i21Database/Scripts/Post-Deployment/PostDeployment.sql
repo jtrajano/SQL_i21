ecc6600b11bb57479d4dba175029a23dbb2ef617
jeffrey trajano <jeffrey.trajano@irely.com>
2015-01-12 19:32:27 +0800

    GL-1395

--- i21Database/Scripts/Post-Deployment/PostDeployment.sql
+++ i21Database/Scripts/Post-Deployment/PostDeployment.sql
@@ -55,6 +55,7 @@ print 'BEGIN POST DEPLOYMENT'
 -- General Ledger
 :r .\GL\DefaultData\AccountStructure.sql
 :r .\GL\DefaultData\AccountGroup.sql
+:r .\GL\DefaultData\AccountCategory.sql
 :r .\GL\DefaultData\AccountTemplate.sql
 :r .\GL\DefaultData\AccountSegmentTemplate.sql
 :r .\GL\GLEntryDataFix.sql
---
+++ i21Database/dbo/Tables/tblGLAccountCategory.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[tblGLAccountCategory]
+(
+	[intAccountCategoryId] [int] IDENTITY(1,1) NOT NULL,
+	[strAccountCategory] [nvarchar](50) COLLATE Latin1_General_CI_AS NULL ,
+	[intAccountGroupId] [int] NULL, 
+	[intConcurrencyId] [int] NOT NULL
+    CONSTRAINT [PK_tblGLAccountCategory] PRIMARY KEY ([intAccountCategoryId]),
+)
