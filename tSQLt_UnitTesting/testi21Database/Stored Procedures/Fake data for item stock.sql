jtrajano/SQL_i21

commit dd5039e9dff5341c3dbb880eb9fb996b1ecc210d

IC-362

tSQLt_UnitTesting/testi21Database/Stored Procedures/Fake data for item stock.sql
@@ -26,7 +26,6 @@ BEGIN
 				[intItemId]
 				,[intLocationId]
 				,[intSubLocationId]
-				,[intUnitMeasureId]
 				,[dblAverageCost]
 				,[dblUnitOnHand]
 				,[dblOrderCommitted]
@@ -39,7 +38,6 @@ BEGIN
 		SELECT	[intItemId] = @WetGrains
 				,[intLocationId] = @Default_Location
 				,[intSubLocationId] = NULL
-				,[intUnitMeasureId] = @EACH
 				,[dblAverageCost] = 0
 				,[dblUnitOnHand] = 0 
 				,[dblOrderCommitted] = 0

i21Database/dbo/Stored Procedures/uspICIncreaseOnOrderQty.sql
@@ -49,7 +49,6 @@ WHEN NOT MATCHED THEN
 		intItemId
 		,intLocationId
 		,intSubLocationId
-		,intUnitMeasureId
 		,dblAverageCost
 		,dblUnitOnHand
 		,dblOrderCommitted
@@ -61,7 +60,6 @@ WHEN NOT MATCHED THEN
 	VALUES (
 		Source_Query.intItemId
 		,Source_Query.intLocationId
-		,NULL 
 		,NULL 
 		,0
 		,0

i21Database/dbo/Stored Procedures/uspICPostCosting.sql
@@ -208,7 +208,6 @@ BEGIN
 				intItemId
 				,intLocationId
 				,intSubLocationId
-				,intUnitMeasureId
 				,dblAverageCost
 				,dblUnitOnHand
 				,dblOrderCommitted
@@ -220,7 +219,6 @@ BEGIN
 			VALUES (
 				StockToUpdate.intItemId
 				,StockToUpdate.intLocationId
-				,NULL 
 				,NULL 
 				,dbo.fnCalculateAverageCost(StockToUpdate.Qty, StockToUpdate.Cost, 0, 0) -- dblAverageCost
 				,StockToUpdate.Qty -- dblUnitOnHand

i21Database/dbo/Tables/tblICItemStock.sql
@@ -4,7 +4,6 @@
     [intItemId] INT NOT NULL, 
     [intLocationId] INT NOT NULL, 
     [intSubLocationId] INT NULL, 
-    [intUnitMeasureId] INT NULL, 
 	[dblAverageCost] NUMERIC(18, 6) NULL DEFAULT ((0)), 
     [dblUnitOnHand] NUMERIC(18, 6) NULL DEFAULT ((0)), 
 	[dblOrderCommitted] NUMERIC(18, 6) NULL DEFAULT ((0)), 
@@ -14,8 +13,7 @@
     [intConcurrencyId] INT NULL DEFAULT ((0)), 
     CONSTRAINT [PK_tblICItemStock] PRIMARY KEY ([intItemStockId]), 
     CONSTRAINT [FK_tblICItemStock_tblICItem] FOREIGN KEY ([intItemId]) REFERENCES [tblICItem]([intItemId]) ON DELETE CASCADE, 
-    CONSTRAINT [FK_tblICItemStock_tblSMCompanyLocation] FOREIGN KEY ([intLocationId]) REFERENCES [tblSMCompanyLocation]([intCompanyLocationId]), 
-    CONSTRAINT [FK_tblICItemStock_tblICUnitMeasure] FOREIGN KEY ([intUnitMeasureId]) REFERENCES [tblICUnitMeasure]([intUnitMeasureId])
+    CONSTRAINT [FK_tblICItemStock_tblSMCompanyLocation] FOREIGN KEY ([intLocationId]) REFERENCES [tblSMCompanyLocation]([intCompanyLocationId])
 )
 GO
 
@@ -61,14 +59,7 @@ EXEC sp_addextendedproperty @name = N'MS_Description',
     @level2type = N'COLUMN',
     @level2name = 'intSubLocationId'
 GO
-EXEC sp_addextendedproperty @name = N'MS_Description',
-    @value = N'Unit of Measure Id',
-    @level0type = N'SCHEMA',
-    @level0name = N'dbo',
-    @level1type = N'TABLE',
-    @level1name = N'tblICItemStock',
-    @level2type = N'COLUMN',
-    @level2name = N'intUnitMeasureId'
+
 GO
 EXEC sp_addextendedproperty @name = N'MS_Description',
     @value = N'Units on Hand',

i21Database/dbo/Views/vyuICGetItemStock.sql
@@ -25,8 +25,6 @@ SELECT
 	strCostingMethod = (CASE WHEN ItemLocation.intCostingMethod = 1 THEN 'AVG'
 							 WHEN ItemLocation.intCostingMethod = 2 THEN 'FIFO'
 							 WHEN ItemLocation.intCostingMethod = 3 THEN 'LIFO' END),
-	ItemStock.intUnitMeasureId,
-	strStockUOM = (SELECT TOP 1 strUnitMeasure FROM tblICUnitMeasure WHERE intUnitMeasureId = ItemStock.intUnitMeasureId),
 	ItemStock.dblUnitOnHand,
 	ItemStock.dblAverageCost,
 	ItemStock.dblOnOrder,

tSQLt_UnitTesting/testi21Database/Stored Procedures/Fake data for item costing.sql
@@ -202,25 +202,25 @@ BEGIN
 	-- Fake data for Item Stock
 	BEGIN 
 		-- Add stock information for items under location 1 ('Default')
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@WetGrains, @Default_Location, 100, @EACH, 22)
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@StickyGrains, @Default_Location, 150, @EACH, 33)
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@PremiumGrains, @Default_Location, 200, @EACH, 44)
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@ColdGrains, @Default_Location, 250, @EACH, 55)
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@HotGrains, @Default_Location, 300, @EACH, 66)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@WetGrains, @Default_Location, 100, 22)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@StickyGrains, @Default_Location, 150, 33)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@PremiumGrains, @Default_Location, 200, 44)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@ColdGrains, @Default_Location, 250, 55)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@HotGrains, @Default_Location, 300, 66)
 
 		-- Add stock information for items under location 2 ('NEW HAVEN')
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@WetGrains, @NewHaven, 0, @EACH, 0)
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@StickyGrains, @NewHaven, 0, @EACH, 0)
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@PremiumGrains, @NewHaven, 0, @EACH, 0)
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@ColdGrains, @NewHaven, 0, @EACH, 0)
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@HotGrains, @NewHaven, 0, @EACH, 0)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@WetGrains, @NewHaven, 0, 0)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@StickyGrains, @NewHaven, 0, 0)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@PremiumGrains, @NewHaven, 0, 0)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@ColdGrains, @NewHaven, 0, 0)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@HotGrains, @NewHaven, 0, 0)
 
 		-- Add stock information for items under location 3 ('BETTER HAVEN')
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@WetGrains, @BetterHaven, 0, @EACH, 0)
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@StickyGrains, @BetterHaven, 0, @EACH, 0)
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@PremiumGrains, @BetterHaven, 0, @EACH, 0)
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@ColdGrains, @BetterHaven, 0, @EACH, 0)
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@HotGrains, @BetterHaven, 0, @EACH, 0)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@WetGrains, @BetterHaven, 0, 0)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@StickyGrains, @BetterHaven, 0, 0)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@PremiumGrains, @BetterHaven, 0, 0)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@ColdGrains, @BetterHaven, 0, 0)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@HotGrains, @BetterHaven, 0, 0)
 	END
 
 	-- Fake data for tblICInventoryFIFO

tSQLt_UnitTesting/testi21Database/Stored Procedures/Fake data for unposting item costing.sql
@@ -215,25 +215,25 @@ BEGIN
 	-- Fake data for Item Stock
 	BEGIN 
 		-- Add stock information for items under location 1 ('Default')
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@WetGrains, @Default_Location, 100, @EACH, 22)
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@StickyGrains, @Default_Location, 150, @EACH, 33)
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@PremiumGrains, @Default_Location, 200, @EACH, 44)
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@ColdGrains, @Default_Location, 250, @EACH, 55)
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@HotGrains, @Default_Location, 300, @EACH, 66)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@WetGrains, @Default_Location, 100, 22)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@StickyGrains, @Default_Location, 150, 33)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@PremiumGrains, @Default_Location, 200, 44)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@ColdGrains, @Default_Location, 250, 55)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@HotGrains, @Default_Location, 300, 66)
 
 		-- Add stock information for items under location 2 ('NEW HAVEN')
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@WetGrains, @NewHaven, 0, @EACH, 0)
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@StickyGrains, @NewHaven, 0, @EACH, 0)
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@PremiumGrains, @NewHaven, 0, @EACH, 0)
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@ColdGrains, @NewHaven, 0, @EACH, 0)
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@HotGrains, @NewHaven, 0, @EACH, 0)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@WetGrains, @NewHaven, 0, 0)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@StickyGrains, @NewHaven, 0, 0)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@PremiumGrains, @NewHaven, 0, 0)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@ColdGrains, @NewHaven, 0, 0)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@HotGrains, @NewHaven, 0, 0)
 
 		-- Add stock information for items under location 3 ('BETTER HAVEN')
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@WetGrains, @BetterHaven, 0, @EACH, 0)
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@StickyGrains, @BetterHaven, 0, @EACH, 0)
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@PremiumGrains, @BetterHaven, 0, @EACH, 0)
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@ColdGrains, @BetterHaven, 0, @EACH, 0)
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@HotGrains, @BetterHaven, 0, @EACH, 0)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@WetGrains, @BetterHaven, 0, 0)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@StickyGrains, @BetterHaven, 0, 0)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@PremiumGrains, @BetterHaven, 0, 0)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@ColdGrains, @BetterHaven, 0, 0)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@HotGrains, @BetterHaven, 0, 0)
 	END
 
 	-- Fake data for tblICInventoryFIFO

tSQLt_UnitTesting/testi21Database/Stored Procedures/Fake items for posted transactions.sql
@@ -178,25 +178,25 @@ BEGIN
 	-- Fake data for Item Stock
 	BEGIN 
 		-- Add stock information for items under location 1 ('Default')
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@WetGrains, @Default_Location, 100, @EACH, 22)
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@StickyGrains, @Default_Location, 150, @EACH, 33)
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@PremiumGrains, @Default_Location, 200, @EACH, 44)
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@ColdGrains, @Default_Location, 250, @EACH, 55)
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@HotGrains, @Default_Location, 300, @EACH, 66)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@WetGrains, @Default_Location, 100, 22)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@StickyGrains, @Default_Location, 150, 33)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@PremiumGrains, @Default_Location, 200, 44)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@ColdGrains, @Default_Location, 250, 55)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@HotGrains, @Default_Location, 300, 66)
 
 		-- Add stock information for items under location 2 ('NEW HAVEN')
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@WetGrains, @NewHaven, 0, @EACH, 0)
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@StickyGrains, @NewHaven, 0, @EACH, 0)
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@PremiumGrains, @NewHaven, 0, @EACH, 0)
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@ColdGrains, @NewHaven, 0, @EACH, 0)
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@HotGrains, @NewHaven, 0, @EACH, 0)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@WetGrains, @NewHaven, 0, 0)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@StickyGrains, @NewHaven, 0, 0)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@PremiumGrains, @NewHaven, 0, 0)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@ColdGrains, @NewHaven, 0, 0)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@HotGrains, @NewHaven, 0, 0)
 
 		-- Add stock information for items under location 3 ('BETTER HAVEN')
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@WetGrains, @BetterHaven, 0, @EACH, 0)
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@StickyGrains, @BetterHaven, 0, @EACH, 0)
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@PremiumGrains, @BetterHaven, 0, @EACH, 0)
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@ColdGrains, @BetterHaven, 0, @EACH, 0)
-		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, intUnitMeasureId, dblAverageCost) VALUES (@HotGrains, @BetterHaven, 0, @EACH, 0)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@WetGrains, @BetterHaven, 0, 0)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@StickyGrains, @BetterHaven, 0, 0)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@PremiumGrains, @BetterHaven, 0, 0)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@ColdGrains, @BetterHaven, 0, 0)
+		INSERT INTO dbo.tblICItemStock (intItemId, intLocationId, dblUnitOnHand, dblAverageCost) VALUES (@HotGrains, @BetterHaven, 0, 0)
 	END
 
 

commit bdab25e59682bab8b1179e25456e53032557dbd1

Fix error caused by commit in dd5039e9dff5341c3dbb880eb9fb996b1ecc210d

tSQLt_UnitTesting/testi21Database/Stored Procedures/Fake data for item stock.sql
@@ -49,7 +49,6 @@ BEGIN
 		SELECT	[intItemId] = @WetGrains
 				,[intLocationId] = @NewHaven
 				,[intSubLocationId] = NULL
-				,[intUnitMeasureId] = @EACH
 				,[dblAverageCost] = 0
 				,[dblUnitOnHand] = 0 
 				,[dblOrderCommitted] = 0
@@ -60,8 +59,7 @@ BEGIN
 		UNION ALL 
 		SELECT	[intItemId] = @WetGrains
 				,[intLocationId] = @BetterHaven
-				,[intSubLocationId] = NULL
-				,[intUnitMeasureId] = @EACH
+				,[intSubLocationId] = NULL				
 				,[dblAverageCost] = 0
 				,[dblUnitOnHand] = 0 
 				,[dblOrderCommitted] = 0
@@ -74,8 +72,7 @@ BEGIN
 		UNION ALL 
 		SELECT	[intItemId] = @StickyGrains
 				,[intLocationId] = @Default_Location
-				,[intSubLocationId] = NULL
-				,[intUnitMeasureId] = @EACH
+				,[intSubLocationId] = NULL				
 				,[dblAverageCost] = 0
 				,[dblUnitOnHand] = 0 
 				,[dblOrderCommitted] = 0
@@ -86,8 +83,7 @@ BEGIN
 		UNION ALL 
 		SELECT	[intItemId] = @StickyGrains
 				,[intLocationId] = @NewHaven
-				,[intSubLocationId] = NULL
-				,[intUnitMeasureId] = @EACH
+				,[intSubLocationId] = NULL				
 				,[dblAverageCost] = 0
 				,[dblUnitOnHand] = 0 
 				,[dblOrderCommitted] = 0
@@ -98,8 +94,7 @@ BEGIN
 		UNION ALL 
 		SELECT	[intItemId] = @StickyGrains
 				,[intLocationId] = @BetterHaven
-				,[intSubLocationId] = NULL
-				,[intUnitMeasureId] = @EACH
+				,[intSubLocationId] = NULL				
 				,[dblAverageCost] = 0
 				,[dblUnitOnHand] = 0 
 				,[dblOrderCommitted] = 0
@@ -112,8 +107,7 @@ BEGIN
 		UNION ALL 
 		SELECT	[intItemId] = @PremiumGrains
 				,[intLocationId] = @Default_Location
-				,[intSubLocationId] = NULL
-				,[intUnitMeasureId] = @EACH
+				,[intSubLocationId] = NULL				
 				,[dblAverageCost] = 0
 				,[dblUnitOnHand] = 0 
 				,[dblOrderCommitted] = 0
@@ -124,8 +118,7 @@ BEGIN
 		UNION ALL 
 		SELECT	[intItemId] = @PremiumGrains
 				,[intLocationId] = @NewHaven
-				,[intSubLocationId] = NULL
-				,[intUnitMeasureId] = @EACH
+				,[intSubLocationId] = NULL				
 				,[dblAverageCost] = 0
 				,[dblUnitOnHand] = 0 
 				,[dblOrderCommitted] = 0
@@ -136,8 +129,7 @@ BEGIN
 		UNION ALL 
 		SELECT	[intItemId] = @PremiumGrains
 				,[intLocationId] = @BetterHaven
-				,[intSubLocationId] = NULL
-				,[intUnitMeasureId] = @EACH
+				,[intSubLocationId] = NULL				
 				,[dblAverageCost] = 0
 				,[dblUnitOnHand] = 0 
 				,[dblOrderCommitted] = 0
@@ -150,8 +142,7 @@ BEGIN
 		UNION ALL 
 		SELECT	[intItemId] = @ColdGrains
 				,[intLocationId] = @Default_Location
-				,[intSubLocationId] = NULL
-				,[intUnitMeasureId] = @EACH
+				,[intSubLocationId] = NULL				
 				,[dblAverageCost] = 0
 				,[dblUnitOnHand] = 0 
 				,[dblOrderCommitted] = 0
@@ -162,8 +153,7 @@ BEGIN
 		UNION ALL 
 		SELECT	[intItemId] = @ColdGrains
 				,[intLocationId] = @NewHaven
-				,[intSubLocationId] = NULL
-				,[intUnitMeasureId] = @EACH
+				,[intSubLocationId] = NULL				
 				,[dblAverageCost] = 0
 				,[dblUnitOnHand] = 0 
 				,[dblOrderCommitted] = 0
@@ -174,8 +164,7 @@ BEGIN
 		UNION ALL 
 		SELECT	[intItemId] = @ColdGrains
 				,[intLocationId] = @BetterHaven
-				,[intSubLocationId] = NULL
-				,[intUnitMeasureId] = @EACH
+				,[intSubLocationId] = NULL				
 				,[dblAverageCost] = 0
 				,[dblUnitOnHand] = 0 
 				,[dblOrderCommitted] = 0
@@ -188,8 +177,7 @@ BEGIN
 		UNION ALL 
 		SELECT	[intItemId] = @HotGrains
 				,[intLocationId] = @Default_Location
-				,[intSubLocationId] = NULL
-				,[intUnitMeasureId] = @EACH
+				,[intSubLocationId] = NULL				
 				,[dblAverageCost] = 0
 				,[dblUnitOnHand] = 0 
 				,[dblOrderCommitted] = 0
@@ -200,8 +188,7 @@ BEGIN
 		UNION ALL 
 		SELECT	[intItemId] = @HotGrains
 				,[intLocationId] = @NewHaven
-				,[intSubLocationId] = NULL
-				,[intUnitMeasureId] = @EACH
+				,[intSubLocationId] = NULL				
 				,[dblAverageCost] = 0
 				,[dblUnitOnHand] = 0 
 				,[dblOrderCommitted] = 0
@@ -212,8 +199,7 @@ BEGIN
 		UNION ALL 
 		SELECT	[intItemId] = @HotGrains
 				,[intLocationId] = @BetterHaven
-				,[intSubLocationId] = NULL
-				,[intUnitMeasureId] = @EACH
+				,[intSubLocationId] = NULL				
 				,[dblAverageCost] = 0
 				,[dblUnitOnHand] = 0 
 				,[dblOrderCommitted] = 0