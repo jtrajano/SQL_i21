e24932f0313ee07d880d4a17fa790a9de58f3ca0
Dan Michael Jalimao <Dan.Jalimao@irely.com>
2015-01-13 21:06:19 +0800

    FCOM-5

--- i21Database/dbo/Tables/tblSMCompanyLocationSubLocation.sql
+++ i21Database/dbo/Tables/tblSMCompanyLocationSubLocation.sql
@@ -1,10 +1,12 @@
 ﻿CREATE TABLE [dbo].[tblSMCompanyLocationSubLocation]
 (
 	[intCompanyLocationSubLocationId] INT NOT NULL IDENTITY , 
+	[intCompanyLocationId] INT NOT NULL, 
     [strSubLocationName] NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL, 
     [strSubLocationDescription] NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL, 
     [strClassification] NVARCHAR(50) NOT NULL, 
     [strAddressKey] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
     [intConcurrencyId] INT NOT NULL DEFAULT 1, 
-    CONSTRAINT [PK_tblSMCompanyLocationSubLocation] PRIMARY KEY ([intCompanyLocationSubLocationId])
+    CONSTRAINT [PK_tblSMCompanyLocationSubLocation] PRIMARY KEY ([intCompanyLocationSubLocationId]), 
+    CONSTRAINT [FK_tblSMCompanyLocationSubLocation_tblSMCompanyLocation] FOREIGN KEY (intCompanyLocationId) REFERENCES tblSMCompanyLocation(intCompanyLocationId)
 )
