14df3fa4c001f19d855c92481e54ced717f9034c
Arman Arenas <arman.arenas@irely.com>
2015-01-14 06:01:19 +0800

    CM-477 CM MVVM to Conversion - Updating strCommand for CM screens pt1

--- i21Database/Scripts/Post-Deployment/SM/DefaultData/1_MasterMenu.sql
+++ i21Database/Scripts/Post-Deployment/SM/DefaultData/1_MasterMenu.sql
@@ -1370,3 +1370,23 @@ GO
 	/* ----------------------------------------------- */
 
 GO
+
+	/* ------------------------------------------------- */
+	/* - Update Cash Management Menu Commands for MVVM - */
+	/* ------------------------------------------------- */
+
+	UPDATE tblSMMasterMenu SET strCommand = 'CashManagement.view.BankDeposit' WHERE strCommand = 'CashManagement.controller.BankDeposit'
+	UPDATE tblSMMasterMenu SET strCommand = 'CashManagement.view.BankTransactions' WHERE strCommand = 'CashManagement.controller.BankTransactions'
+	UPDATE tblSMMasterMenu SET strCommand = 'CashManagement.view.BankTransfer' WHERE strCommand = 'CashManagement.controller.BankTransfer'
+	UPDATE tblSMMasterMenu SET strCommand = 'CashManagement.view.MiscellaneousChecks' WHERE strCommand = 'CashManagement.controller.MiscellaneousChecks'
+	--UPDATE tblSMMasterMenu SET strCommand = 'CashManagement.view.BankAccountRegister' WHERE strCommand = 'CashManagement.controller.BankAccountRegister'
+	--UPDATE tblSMMasterMenu SET strCommand = 'CashManagement.view.BankReconciliation' WHERE strCommand = 'CashManagement.controller.BankReconciliation'
+	UPDATE tblSMMasterMenu SET strCommand = 'CashManagement.view.Banks' WHERE strCommand = 'CashManagement.controller.Banks'
+	--UPDATE tblSMMasterMenu SET strCommand = 'CashManagement.view.BankAccounts' WHERE strCommand = 'CashManagement.controller.BankAccounts'
+	--UPDATE tblSMMasterMenu SET strCommand = 'CashManagement.view.BankFileFormat' WHERE strCommand = 'CashManagement.controller.BankFileFormat'
+
+	/* ------------------------------------------------- */
+	/* End Update Cash Management Menu Commands for MVVM */
+	/* ------------------------------------------------- */
+
+GO
