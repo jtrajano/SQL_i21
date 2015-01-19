2a90904f445f79dd5df65e1244c19a2774e09018
Abdul Rahman <m.rahman@irely.com>
2015-01-19 17:21:49 +0530

    GRN-43

---
+++ i21Database/dbo/Tables/tblGRDiscountId.sql
@@ -0,0 +1,67 @@
+﻿CREATE TABLE [dbo].[tblGRDiscountId]
+(
+	[intDiscountId] INT NOT NULL IDENTITY, 
+    [intCurrencyId] INT NOT NULL, 
+    [strDiscountId] NVARCHAR(3) COLLATE Latin1_General_CI_AS NOT NULL, 
+    [strDiscountDescription] NVARCHAR(30) COLLATE Latin1_General_CI_AS NOT NULL, 
+    [ysnDiscountIdActive] BIT NOT NULL, 
+    [intConcurrencyId] INT NOT NULL DEFAULT 1, 
+    CONSTRAINT [PK_tblGRDiscountId_intDiscountId] PRIMARY KEY ([intDiscountId]), 
+    CONSTRAINT [FK_tblGRDiscountId_tblSMCurrency_intCurrencyId] FOREIGN KEY ([intCurrencyId]) REFERENCES [tblSMCurrency]([intCurrencyID]), 
+    CONSTRAINT [UK_tblGRDiscountId_strDiscountId_intCurrencyId] UNIQUE ([strDiscountId], [intCurrencyId]) 
+)
+
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Currency Id',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'tblGRDiscountId',
+    @level2type = N'COLUMN',
+    @level2name = N'intCurrencyId'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Identity Field',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'tblGRDiscountId',
+    @level2type = N'COLUMN',
+    @level2name = N'intDiscountId'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Discount Id',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'tblGRDiscountId',
+    @level2type = N'COLUMN',
+    @level2name = 'strDiscountId'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Discount Description',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'tblGRDiscountId',
+    @level2type = N'COLUMN',
+    @level2name = N'strDiscountDescription'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Discount Active Indicator',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'tblGRDiscountId',
+    @level2type = N'COLUMN',
+    @level2name = N'ysnDiscountIdActive'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Concurrency Field',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'tblGRDiscountId',
+    @level2type = N'COLUMN',
+    @level2name = N'intConcurrencyId'
---
+++ i21Database/dbo/Tables/tblGRDiscountLocationUse.sql
@@ -0,0 +1,58 @@
+﻿CREATE TABLE [dbo].[tblGRDiscountLocationUse]
+(
+	[intDiscountLocationUseId] INT NOT NULL  IDENTITY, 
+    [intDiscountId] INT NOT NULL, 
+    [intCompanyLocationId] INT NOT NULL, 
+    [ysnDiscountLocationActive] BIT NOT NULL, 
+    [intConcurrencyId] INT NOT NULL DEFAULT 1, 
+    CONSTRAINT [PK_tblGRDiscountLocationUse_intDiscountLocationUseId] PRIMARY KEY ([intDiscountLocationUseId]), 
+    CONSTRAINT [FK_tblGRDiscountLocationUse_tblGRDiscountId_intDiscountId] FOREIGN KEY ([intDiscountId]) REFERENCES [tblGRDiscountId]([intDiscountId]), 
+    CONSTRAINT [FK_tblGRDiscountLocationUse_tblSMCompanyLocation_intCompanyLocationId] FOREIGN KEY ([intCompanyLocationId]) REFERENCES [tblSMCompanyLocation]([intCompanyLocationId]), 
+    CONSTRAINT [AK_tblGRDiscountLocationUse_intCompanyLocationId] UNIQUE ([intCompanyLocationId])
+)
+
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Identity Column',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'tblGRDiscountLocationUse',
+    @level2type = N'COLUMN',
+    @level2name = N'intDiscountLocationUseId'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Discount Id',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'tblGRDiscountLocationUse',
+    @level2type = N'COLUMN',
+    @level2name = N'intDiscountId'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Location Id',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'tblGRDiscountLocationUse',
+    @level2type = N'COLUMN',
+    @level2name = N'intCompanyLocationId'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Discount Location Active',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'tblGRDiscountLocationUse',
+    @level2type = N'COLUMN',
+    @level2name = N'ysnDiscountLocationActive'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Concurrency Field',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'tblGRDiscountLocationUse',
+    @level2type = N'COLUMN',
+    @level2name = N'intConcurrencyId'
