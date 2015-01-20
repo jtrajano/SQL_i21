5f0686fad491b9037fbe2df4f486825698f3ca72
Dan Michael Jalimao <Dan.Jalimao@irely.com>
2015-01-20 18:42:46 +0800

    FCOM-5

--- i21Database/dbo/Tables/tblSMCompanyLocationSubLocation.sql
+++ i21Database/dbo/Tables/tblSMCompanyLocationSubLocation.sql
@@ -4,9 +4,13 @@
 	[intCompanyLocationId] INT NOT NULL, 
     [strSubLocationName] NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL, 
     [strSubLocationDescription] NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL, 
-    [strClassification] NVARCHAR(50) NOT NULL, 
+    [strClassification] NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL, 
+	[intNewLotBin] INT NULL, 
+    [intAuditBin] INT NULL, 
     [strAddressKey] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
     [intConcurrencyId] INT NOT NULL DEFAULT 1, 
     CONSTRAINT [PK_tblSMCompanyLocationSubLocation] PRIMARY KEY ([intCompanyLocationSubLocationId]), 
-    CONSTRAINT [FK_tblSMCompanyLocationSubLocation_tblSMCompanyLocation] FOREIGN KEY (intCompanyLocationId) REFERENCES tblSMCompanyLocation(intCompanyLocationId)
+    CONSTRAINT [FK_tblSMCompanyLocationSubLocation_tblSMCompanyLocation] FOREIGN KEY (intCompanyLocationId) REFERENCES tblSMCompanyLocation(intCompanyLocationId), 
+    CONSTRAINT [FK_tblSMCompanyLocationSubLocation_tblICStorageLocation_new] FOREIGN KEY (intNewLotBin) REFERENCES tblICStorageLocation(intStorageLocationId),
+	CONSTRAINT [FK_tblSMCompanyLocationSubLocation_tblICStorageLocation_audit] FOREIGN KEY (intAuditBin) REFERENCES tblICStorageLocation(intStorageLocationId)
 )
