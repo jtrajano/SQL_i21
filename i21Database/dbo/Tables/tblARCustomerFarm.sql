eea87057210d43b22ef1dda184b1fe9019882b40
Smith Wilson de Jesus <smith.dejesus@irely.com>
2015-01-20 20:24:58 +0800

    AR-687 Buffered store for CustomerFarm

--- i21Database/dbo/Tables/tblARCustomerFarm.sql
+++ i21Database/dbo/Tables/tblARCustomerFarm.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[tblARCustomerFarm] (
     [intFarmFieldId]      INT            IDENTITY (1, 1) NOT NULL,
-    [intEntityId]         INT            NOT NULL,
+    [intCustomerId]         INT            NOT NULL,
     [strFarmNumber]           NVARCHAR (10)  COLLATE Latin1_General_CI_AS NULL,
     [strFarmDescription]  NVARCHAR (30)  COLLATE Latin1_General_CI_AS NULL,
     [strFieldNumber]          NVARCHAR (10)  COLLATE Latin1_General_CI_AS NULL,
