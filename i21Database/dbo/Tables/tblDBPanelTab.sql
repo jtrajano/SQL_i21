2d067a571477d870c0bb76c4544721ac1bca1293
Randy Honorato <randy.honorato@irely.com>
2015-01-21 16:04:26 +0800

    IG-955 - Added new field on tblDBPanelTab for mvvm changes

--- i21Database/dbo/Tables/tblDBPanelTab.sql
+++ i21Database/dbo/Tables/tblDBPanelTab.sql
@@ -12,6 +12,7 @@
     [strTabName]        NVARCHAR (MAX) COLLATE Latin1_General_CI_AS DEFAULT ('') NOT NULL,
     [strRenameTabName]  NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NULL,
     [intConcurrencyId ] INT            DEFAULT ((1)) NOT NULL,
+    [ysnDefaultTab] BIT NULL DEFAULT ((0)), 
     CONSTRAINT [PK_dbo.tblDBPanelTab] PRIMARY KEY CLUSTERED ([intPanelTabId] ASC)
 );
 
