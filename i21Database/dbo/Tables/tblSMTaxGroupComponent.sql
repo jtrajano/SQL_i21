77b7b6b5687793fd1349af5870e51d4d1b6e0d61
Dan Michael Jalimao <Dan.Jalimao@irely.com>
2015-01-22 14:28:06 +0800

    FCOM-1

---
+++ i21Database/dbo/Tables/tblSMTaxGroupComponent.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[tblSMTaxGroupComponent]
+(
+	[intTaxGroupComponentId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [intTaxGroupId] INT NOT NULL, 
+    [intTaxComponentId] INT NOT NULL, 
+    [intConcurrencyId] INT NOT NULL DEFAULT 1, 
+    CONSTRAINT [FK_tblSMTaxGroupComponent_tblSMTaxGroup] FOREIGN KEY (intTaxGroupId) REFERENCES tblSMTaxGroup(intTaxGroupId),
+    CONSTRAINT [FK_tblSMTaxGroupComponent_tblSMTaxComponent] FOREIGN KEY (intTaxComponentId) REFERENCES tblSMTaxComponent(intTaxComponentId)
+)
