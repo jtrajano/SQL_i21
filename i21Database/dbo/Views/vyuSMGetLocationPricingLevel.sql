ae8d062c5c498e7c7092fa2a107181b121bf9033
Dan Michael Jalimao <Dan.Jalimao@irely.com>
2015-01-23 19:25:41 +0800

    FCOM-7

--- i21Database/dbo/Views/vyuSMGetLocationPricingLevel.sql
+++ i21Database/dbo/Views/vyuSMGetLocationPricingLevel.sql
@@ -1,15 +1,21 @@
 ﻿CREATE VIEW [dbo].[vyuSMGetLocationPricingLevel]
 
 AS 
+	SELECT TOP 100 PERCENT 
+		[intSort] AS ['intKey'], 
+		[intCompanyLocationId], 
+		[strPricingLevelName] AS [strPriceLevel]
+	FROM tblSMCompanyLocationPricingLevel
+	ORDER BY intCompanyLocationId, intSort ASC
 
-SELECT intKey = CAST(ROW_NUMBER() OVER(ORDER BY tblLocationPriceLevel.intCompanyLocationId, tblLocationPriceLevel.strPriceLevel) AS INT),
-* FROM (
-	SELECT intCompanyLocationId, strPriceLevel1 AS strPriceLevel from tblSMCompanyLocation
-	UNION ALL SELECT intCompanyLocationId, strPriceLevel2 AS strPriceLevel from tblSMCompanyLocation
-	UNION ALL SELECT intCompanyLocationId, strPriceLevel3 AS strPriceLevel from tblSMCompanyLocation
-	UNION ALL SELECT intCompanyLocationId, strPriceLevel4 AS strPriceLevel from tblSMCompanyLocation
-	UNION ALL SELECT intCompanyLocationId, strPriceLevel5 AS strPriceLevel from tblSMCompanyLocation
-	) tblLocationPriceLevel
-WHERE ISNULL(strPriceLevel, '') <> ''
+--SELECT intKey = CAST(ROW_NUMBER() OVER(ORDER BY tblLocationPriceLevel.intCompanyLocationId, tblLocationPriceLevel.strPriceLevel) AS INT),
+--* FROM (
+--	SELECT intCompanyLocationId, strPriceLevel1 AS strPriceLevel from tblSMCompanyLocation
+--	UNION ALL SELECT intCompanyLocationId, strPriceLevel2 AS strPriceLevel from tblSMCompanyLocation
+--	UNION ALL SELECT intCompanyLocationId, strPriceLevel3 AS strPriceLevel from tblSMCompanyLocation
+--	UNION ALL SELECT intCompanyLocationId, strPriceLevel4 AS strPriceLevel from tblSMCompanyLocation
+--	UNION ALL SELECT intCompanyLocationId, strPriceLevel5 AS strPriceLevel from tblSMCompanyLocation
+--	) tblLocationPriceLevel
+--WHERE ISNULL(strPriceLevel, '') <> ''
 
 GO
