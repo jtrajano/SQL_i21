4e7353e18f30a53c4eeb7cf0fcaa4af7d8a851a3
Lawrence Zabala <lawrence.zabala@irely.com>
2015-01-27 17:12:59 +0800

    IC-417

---
+++ i21Database/dbo/Views/vyuICGetItemAccount.sql
@@ -0,0 +1,19 @@
+﻿CREATE VIEW [dbo].[vyuICGetItemAccount]
+	AS 
+
+SELECT 
+intAccountKey = CAST(ROW_NUMBER() OVER(ORDER BY Item.intKey, ItemAccount.intItemAccountId) AS INT),
+Item.intKey,
+intItemAccountId,
+ItemAccount.intItemId,
+ItemAccount.intAccountId,
+GLAccount.strAccountId,
+GLAccount.intAccountGroupId,
+GLAccountGroup.strAccountGroup,
+GLAccountGroup.strAccountType,
+ItemAccount.strAccountDescription AS strAccountCategory,
+ItemAccount.intSort
+FROM vyuICGetItemStock Item
+INNER JOIN tblICItemAccount ItemAccount ON ItemAccount.intItemId = Item.intItemId
+LEFT JOIN tblGLAccount GLAccount ON GLAccount.intAccountId = ItemAccount.intAccountId
+LEFT JOIN tblGLAccountGroup GLAccountGroup ON GLAccountGroup.intAccountGroupId = GLAccount.intAccountGroupId
---
+++ i21Database/dbo/Views/vyuICGetItemPricing.sql
@@ -0,0 +1,33 @@
+﻿CREATE VIEW [dbo].[vyuICGetItemPricing]
+	AS 
+
+SELECT 
+intPricingKey = CAST(ROW_NUMBER() OVER(ORDER BY Item.intKey, ItemPricing.intItemPricingId) AS INT),
+Item.intKey,
+ItemPricing.intItemPricingId,
+ItemPricing.intItemId,
+ItemPricing.intLocationId,
+Item.strLocationName,
+Item.strLocationType,
+ItemPricing.intItemUnitMeasureId,
+UOM.strUnitMeasure,
+UOM.strUnitType,
+ItemUOM.ysnStockUnit,
+ItemPricing.dblRetailPrice,
+ItemPricing.dblWholesalePrice,
+ItemPricing.dblLargeVolumePrice,
+ItemPricing.dblAmountPercent,
+ItemPricing.dblSalePrice,
+ItemPricing.dblMSRPPrice,
+ItemPricing.strPricingMethod,
+ItemPricing.dblLastCost,
+ItemPricing.dblStandardCost,
+ItemPricing.dblMovingAverageCost,
+ItemPricing.dblEndMonthCost,
+ItemPricing.dtmBeginDate,
+ItemPricing.dtmEndDate,
+ItemPricing.intSort
+FROM vyuICGetItemStock Item
+INNER JOIN tblICItemPricing ItemPricing ON ItemPricing.intItemId = Item.intItemId and ItemPricing.intLocationId = Item.intLocationId
+LEFT JOIN tblICItemUOM ItemUOM ON ItemUOM.intItemUOMId = ItemPricing.intItemUnitMeasureId
+LEFT JOIN tblICUnitMeasure UOM ON UOM .intUnitMeasureId= ItemUOM.intUnitMeasureId
