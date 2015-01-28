7d1649af30013f1441953b68409f6595c383c2b6
Dan Michael Jalimao <Dan.Jalimao@irely.com>
2015-01-28 12:00:43 +0800

    FCOM-117

--- i21Database/dbo/Tables/tblSMCompanyLocationSubLocationCategory.sql
+++ i21Database/dbo/Tables/tblSMCompanyLocationSubLocationCategory.sql
@@ -4,6 +4,6 @@
     [intCompanyLocationSubLocationId] INT NOT NULL, 
     [intCategoryId] INT NOT NULL, 
     [intConcurrencyId] INT NOT NULL DEFAULT 1, 
-    CONSTRAINT [FK_tblSMCompanyLocationSubLocationCategory_tblSMCompanyLocationSubLocation] FOREIGN KEY ([intCompanyLocationSubLocationId]) REFERENCES tblSMCompanyLocationSubLocation([intCompanyLocationSubLocationId]), 
+    CONSTRAINT [FK_tblSMCompanyLocationSubLocationCategory_tblSMCompanyLocationSubLocation] FOREIGN KEY ([intCompanyLocationSubLocationId]) REFERENCES tblSMCompanyLocationSubLocation([intCompanyLocationSubLocationId]) ON DELETE CASCADE, 
     CONSTRAINT [FK_tblSMCompanyLocationSubLocationCategory_tblICCategory] FOREIGN KEY ([intCategoryId]) REFERENCES tblICCategory(intCategoryId) 
 )
