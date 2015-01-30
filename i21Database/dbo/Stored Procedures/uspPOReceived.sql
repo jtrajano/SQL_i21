791a5da489161168a0746060aafaaeb0224743d0
Erick F. Dela Cruz <erick.delacruz@irely.com>
2015-01-30 11:36:48 +0800

    AP-858

---
+++ i21Database/dbo/Stored Procedures/uspPOReceived.sql
@@ -0,0 +1,37 @@
+﻿CREATE PROCEDURE [dbo].[uspPOReceived]
+	@purchaseId INT,
+	@receivedNum DECIMAL(18,6),
+	@itemId INT,
+	@lineNo INT
+AS
+BEGIN
+
+	--Validate
+	IF(NOT EXISTS(SELECT 1 FROM tblPOPurchase WHERE intPurchaseId = @purchaseId))
+	BEGIN
+		RAISERROR(51033, 11, 1); --Not Exists
+	END
+
+	IF(NOT EXISTS(SELECT 1 FROM tblPOPurchaseDetail WHERE intPurchaseId = @purchaseId AND intItemId = @itemId AND intLineNo = @lineNo))
+	BEGIN
+		RAISERROR(51034, 11, 1); --PO item not exists
+	END
+
+	IF(NOT EXISTS(SELECT 1 FROM tblPOPurchaseDetail WHERE intPurchaseId = @purchaseId AND intItemId = @itemId AND intLineNo = @lineNo AND (dblQtyReceived + @receivedNum) > dblQtyOrdered))
+	BEGIN
+		RAISERROR(51035, 11, 1); --received item exceeds
+	END
+
+	UPDATE A
+		SET dblQtyReceived = (dblQtyReceived + @receivedNum)
+	FROM tblPOPurchaseDetail A
+	WHERE intPurchaseId = @purchaseId AND intItemId = @itemId AND intLineNo = @lineNo
+
+	UPDATE A
+		SET intOrderStatusId = CASE WHEN (SELECT SUM(dblQtyReceived) FROM tblPOPurchaseDetail WHERE intPurchaseId = @purchaseId) 
+											= (SELECT SUM(dblQtyOrdered) FROM tblPOPurchaseDetail WHERE intPurchaseId = @purchaseId)
+									THEN 3 ELSE 2 END
+	FROM tblPOPurchase A
+	WHERE intPurchaseId = @purchaseId
+
+END
