9cbb4dda37434cc7cc033c759c5b7038b98ec606
Smith Wilson de Jesus <smith.dejesus@irely.com>
2015-02-04 11:47:22 +0800

    AR-702 Code Sales Order functionalities Added duplicate functionality

---
+++ i21Database/dbo/Stored Procedures/uspSODuplicateSalesOrder.sql
@@ -0,0 +1,128 @@
+﻿CREATE PROCEDURE uspSODuplicateSalesOrder
+	@SalesOrderId	INT = 0,
+	@UserId			INT = 0
+
+	AS
+BEGIN
+
+	DECLARE @NewSalesOrderId INT
+	
+	INSERT INTO tblSOSalesOrder
+		(   [intCustomerId]
+           ,[dtmDate]
+           ,[dtmDueDate]
+           ,[intCurrencyId]
+           ,[intCompanyLocationId]
+           ,[intSalespersonId]
+           ,[intShipViaId]
+           ,[strPONumber]
+           ,[intTermId]
+           ,[dblSalesOrderSubtotal]
+           ,[dblShipping]
+           ,[dblTax]
+           ,[dblSalesOrderTotal]
+           ,[dblDiscount]
+           ,[dblAmountDue]
+           ,[dblPayment]
+           ,[strTransactionType]
+           ,[strOrderStatus]
+           ,[intAccountId]
+           ,[dtmProcessDate]
+           ,[ysnProcessed]
+           ,[strComments]
+           ,[strShipToLocationName]
+           ,[strShipToAddress]
+           ,[strShipToCity]
+           ,[strShipToState]
+           ,[strShipToZipCode]
+           ,[strShipToCountry]
+           ,[strBillToLocationName]
+           ,[strBillToAddress]
+           ,[strBillToCity]
+           ,[strBillToState]
+           ,[strBillToZipCode]
+           ,[strBillToCountry]
+           ,[intEntityId]
+        )
+	SELECT
+			[intCustomerId]
+           ,GETDATE() --Date
+           ,[dbo].fnGetDueDateBasedOnTerm(GETDATE(),intTermId) --Due Date
+           ,[intCurrencyId]
+           ,[intCompanyLocationId]
+           ,[intSalespersonId]
+           ,[intShipViaId]
+           ,[strPONumber]
+           ,[intTermId]
+           ,[dblSalesOrderSubtotal]
+           ,[dblShipping]
+           ,[dblTax]
+           ,[dblSalesOrderTotal]
+           ,[dblDiscount]
+           ,[dblAmountDue]
+           ,[dblPayment]
+           ,[strTransactionType]
+           ,'Pending'
+           ,[intAccountId]
+           ,NULL --Processed Date
+           ,0 --Processed
+           ,[strComments]
+           ,[strShipToLocationName]
+           ,[strShipToAddress]
+           ,[strShipToCity]
+           ,[strShipToState]
+           ,[strShipToZipCode]
+           ,[strShipToCountry]
+           ,[strBillToLocationName]
+           ,[strBillToAddress]
+           ,[strBillToCity]
+           ,[strBillToState]
+           ,[strBillToZipCode]
+           ,[strBillToCountry]
+           ,@UserId
+	FROM
+	tblSOSalesOrder
+	WHERE intSalesOrderId = @SalesOrderId
+
+	SET @NewSalesOrderId = SCOPE_IDENTITY()
+
+	INSERT INTO [dbo].[tblSOSalesOrderDetail]
+	   (	[intSalesOrderId]
+           ,[intCompanyLocationId]
+           ,[intItemId]
+           ,[strItemDescription]
+           ,[intItemUOMId]
+           ,[dblQtyOrdered]
+           ,[dblQtyAllocated]
+           ,[dblDiscount]
+           ,[intTaxId]
+           ,[dblPrice]
+           ,[dblTotal]
+           ,[strComments]
+           ,[intAccountId]
+           ,[intCOGSAccountId]
+           ,[intSalesAccountId]
+           ,[intInventoryAccountId]
+        )
+	SELECT 
+			@NewSalesOrderId
+		   ,[intCompanyLocationId]
+           ,[intItemId]
+           ,[strItemDescription]
+           ,[intItemUOMId]
+           ,[dblQtyOrdered]
+           ,[dblQtyAllocated]
+           ,[dblDiscount]
+           ,[intTaxId]
+           ,[dblPrice]
+           ,[dblTotal]
+           ,[strComments]
+           ,[intAccountId]
+           ,[intCOGSAccountId]
+           ,[intSalesAccountId]
+           ,[intInventoryAccountId]
+	FROM
+	tblSOSalesOrderDetail
+	WHERE intSalesOrderId = @SalesOrderId
+	
+END
