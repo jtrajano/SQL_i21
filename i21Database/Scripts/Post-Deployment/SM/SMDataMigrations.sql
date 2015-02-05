9f38421775500e4b0405fa2ebea98580ff187497
Dan Michael Jalimao <Dan.Jalimao@irely.com>
2015-02-05 17:48:23 +0800

    FCOM-148

--- i21Database/Scripts/Post-Deployment/SM/SMDataMigrations.sql
+++ i21Database/Scripts/Post-Deployment/SM/SMDataMigrations.sql
@@ -27,3 +27,29 @@ GO
 -- Add the SQL Server custom messages
 EXEC dbo.uspSMErrorMessages
 GO
+
+-- Update User Role and User Security Menus
+DECLARE @currentRow INT
+DECLARE @totalRows INT
+
+SET @currentRow = 1
+SELECT @totalRows = Count(*) FROM [dbo].[tblSMUserRole]
+
+WHILE (@currentRow <= @totalRows)
+BEGIN
+
+Declare @roleId INT
+SELECT @roleId = intUserRoleID FROM (  
+	SELECT ROW_NUMBER() OVER(ORDER BY intUserRoleID ASC) AS 'ROWID', *
+	FROM [dbo].[tblSMUserRole]
+) a
+WHERE ROWID = @currentRow
+
+PRINT N'Executing uspSMUpdateUserRoleMenus'
+Exec uspSMUpdateUserRoleMenus @roleId, 1, 0
+
+
+SET @currentRow = @currentRow + 1
+END
+
+GO
