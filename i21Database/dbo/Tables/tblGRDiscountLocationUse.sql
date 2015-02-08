fc9399e66e81d983df866951f60e516f21cd7fdd
Satya <ssahoo@BLRSATYAWIN7.irely.net>
2015-02-08 09:22:29 +0530

    GR

--- i21Database/dbo/Tables/tblGRDiscountLocationUse.sql
+++ i21Database/dbo/Tables/tblGRDiscountLocationUse.sql
@@ -6,9 +6,8 @@
     [ysnDiscountLocationActive] BIT NOT NULL, 
     [intConcurrencyId] INT NOT NULL DEFAULT 1, 
     CONSTRAINT [PK_tblGRDiscountLocationUse_intDiscountLocationUseId] PRIMARY KEY ([intDiscountLocationUseId]), 
-    CONSTRAINT [FK_tblGRDiscountLocationUse_tblGRDiscountId_intDiscountId] FOREIGN KEY ([intDiscountId]) REFERENCES [tblGRDiscountId]([intDiscountId]), 
-    CONSTRAINT [FK_tblGRDiscountLocationUse_tblSMCompanyLocation_intCompanyLocationId] FOREIGN KEY ([intCompanyLocationId]) REFERENCES [tblSMCompanyLocation]([intCompanyLocationId]), 
-    CONSTRAINT [AK_tblGRDiscountLocationUse_intCompanyLocationId] UNIQUE ([intCompanyLocationId])
+    CONSTRAINT [FK_tblGRDiscountLocationUse_tblGRDiscountId_intDiscountId] FOREIGN KEY ([intDiscountId]) REFERENCES [tblGRDiscountId]([intDiscountId]) ON DELETE CASCADE, 
+    CONSTRAINT [FK_tblGRDiscountLocationUse_tblSMCompanyLocation_intCompanyLocationId] FOREIGN KEY ([intCompanyLocationId]) REFERENCES [tblSMCompanyLocation]([intCompanyLocationId])
 )
 
 GO
