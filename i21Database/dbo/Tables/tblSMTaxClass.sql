1cd4d43c3845e80bbb959aba88ee982836867de6
Dan Michael Jalimao <Dan.Jalimao@irely.com>
2015-02-10 19:40:24 +0800

    FCOM-107

---
+++ i21Database/dbo/Tables/tblSMTaxClass.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[tblSMTaxClass]
+(
+	[intTaxClassId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [strTaxClass] NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL, 
+    [strDescription] NVARCHAR(150) COLLATE Latin1_General_CI_AS NULL, 
+    [ysnTaxable] BIT NOT NULL, 
+    [intConcurrencyId] INT NOT NULL DEFAULT 1
+)
