9d966fdbf743ff0caa95e6d59208960e72e14684
Lawrence Zabala <lawrence.zabala@irely.com>
2015-02-11 11:08:11 +0800

    IC-469

--- i21Database/dbo/Views/vyuICGetReceiptItemSource.sql
+++ i21Database/dbo/Views/vyuICGetReceiptItemSource.sql
@@ -7,16 +7,31 @@ SELECT
 	strSourceId = 
 		(
 			CASE WHEN Receipt.strReceiptType = 'Contract'
-				THEN 'Not Yet Implemented'
+				THEN NULL
 			WHEN Receipt.strReceiptType = 'Purchase Order'
 				THEN (SELECT ISNULL(strPurchaseOrderNumber, 'PO Number not found!') FROM tblPOPurchase WHERE intPurchaseId = ReceiptItem.intSourceId)
 			WHEN Receipt.strReceiptType = 'Transfer Receipt'
-				THEN 'Not Yet Implemented'
+				THEN NULL
 			WHEN Receipt.strReceiptType = 'Direct Transfer'
-				THEN 'Not Yet Implemented'
+				THEN NULL
 			WHEN Receipt.strReceiptType = 'Direct'
-				THEN ''
-			ELSE ''
+				THEN NULL
+			ELSE NULL
+			END
+		),
+	dtmDate = 
+		(
+			CASE WHEN Receipt.strReceiptType = 'Contract'
+				THEN NULL
+			WHEN Receipt.strReceiptType = 'Purchase Order'
+				THEN (SELECT ISNULL(dtmDate, 'PO Number not found!') FROM tblPOPurchase WHERE intPurchaseId = ReceiptItem.intSourceId)
+			WHEN Receipt.strReceiptType = 'Transfer Receipt'
+				THEN NULL
+			WHEN Receipt.strReceiptType = 'Direct Transfer'
+				THEN NULL
+			WHEN Receipt.strReceiptType = 'Direct'
+				THEN NULL
+			ELSE NULL
 			END
 		)
 FROM tblICInventoryReceiptItem ReceiptItem
