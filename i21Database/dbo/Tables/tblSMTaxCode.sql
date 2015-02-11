d42cfcef5a58e075b8e0eb0e66532f37909c9665
Dan Michael Jalimao <Dan.Jalimao@irely.com>
2015-02-11 17:38:14 +0800

    FCOM-108

--- i21Database/dbo/Tables/tblSMTaxCode.sql
+++ i21Database/dbo/Tables/tblSMTaxCode.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[tblSMTaxCode]
 (
     [intTaxCodeId]			INT				NOT NULL PRIMARY KEY IDENTITY, 
-    [strCodeName]			NVARCHAR(50)    COLLATE Latin1_General_CI_AS NULL, 
+    [strTaxCode]			NVARCHAR(50)    COLLATE Latin1_General_CI_AS NULL, 
     --[strType]                NVARCHAR(50)    COLLATE Latin1_General_CI_AS NULL, 
     [strDescription]        NVARCHAR(100)	COLLATE Latin1_General_CI_AS NULL, 
     [strCalculationMethod]	NVARCHAR(15)	COLLATE Latin1_General_CI_AS NULL, 
@@ -14,8 +14,8 @@
     [strCountry]			NVARCHAR (25)	COLLATE Latin1_General_CI_AS NOT NULL,
     [intSalesAccountId]		INT				NULL, 
     [intPurchasingAccountId] INT			NULL, 
-    [ysnTaxable]			BIT				NULL	DEFAULT 0, 
-    [ysnSeparateOnInvoice]	BIT				NULL	DEFAULT 0, 
+    [ysnTaxable]			BIT				NOT NULL	DEFAULT 0, 
+    [ysnSeparateOnInvoice]	BIT				NOT NULL	DEFAULT 0, 
     [intConcurrencyId]		INT				NOT NULL DEFAULT 1, 
     CONSTRAINT [FK_tblSMTaxCode_tblGLAccount_sales] FOREIGN KEY (intSalesAccountId) REFERENCES tblGLAccount(intAccountId),
     CONSTRAINT [FK_tblSMTaxCode_tblGLAccount_purchasing] FOREIGN KEY (intPurchasingAccountId) REFERENCES tblGLAccount(intAccountId)
