9319c94738b91a7dafc5747fcb558d9a92528868
jeffrey trajano <jeffrey.trajano@irely.com>
2015-02-11 11:48:39 +0800

    GL-1518 UPGRADING ERROR: Invalid object name 'tblGLAccountStructure' when running the installer on Fresh db

--- i21PreDeployment/Scripts/Pre-Deployment/6_GL/3_1440_to_1510.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/6_GL/3_1440_to_1510.sql
@@ -1,5 +1,8 @@
 ﻿--CHECKS FOR PROFIT CENTER OR LOCATION IS PRESENT IN ACCOUNT STRUCTURE TABLE
 GO
-EXEC('IF NOT EXISTS (SELECT TOP 1 1 FROM tblGLAccountStructure WHERE strType = ''Segment'' and (LOWER(strStructureName) like ''profit center%'' OR strStructureName = ''Location''))
-	RAISERROR(N''Missing valid structure (i.e. location/profit center) in tblGLAccountStructure, Deployment Terminated.'', 16,1)')
+EXEC('IF EXISTS(SELECT  *  FROM  sys.objects WHERE    object_id = OBJECT_ID(N''[dbo].[tblGLAccountStructure]'') AND type in (N''U''))
+		BEGIN
+			IF NOT EXISTS (SELECT TOP 1 1 FROM tblGLAccountStructure WHERE strType = ''Segment'' and (LOWER(strStructureName) like ''profit center%'' OR strStructureName = ''Location''))
+			RAISERROR(N''Missing valid structure (i.e. location/profit center) in tblGLAccountStructure, Deployment Terminated.'', 16,1)
+		END')
 GO
