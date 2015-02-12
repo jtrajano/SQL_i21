70d9750caa28c50e022d5d8e99cd439bdeed12ba
Jayson Ramos <jayson.ramos@iRely.com>
2015-02-12 11:09:09 +0800

    HD-603;

--- i21Database/dbo/Tables/tblHDAnnouncement.sql
+++ i21Database/dbo/Tables/tblHDAnnouncement.sql
@@ -1,77 +1,76 @@
-﻿CREATE TABLE [dbo].[tblHDAnnouncementType]
+﻿CREATE TABLE [dbo].[tblHDAnnouncement]
 (
-	[intAnnouncementTypeId] INT IDENTITY (1, 1) NOT NULL,
-    [strAnnouncementType] NVARCHAR(255) COLLATE Latin1_General_CI_AS NOT NULL, 
-    [strDescription] NVARCHAR(255) COLLATE Latin1_General_CI_AS NOT NULL, 
-	[strDisplayTo] NVARCHAR(255) COLLATE Latin1_General_CI_AS NOT NULL, 
-	[strFontColor] NVARCHAR(255) COLLATE Latin1_General_CI_AS NULL, 
-	[strBackColor] NVARCHAR(255) COLLATE Latin1_General_CI_AS NULL, 
+		[intAnnouncementId] INT IDENTITY (1, 1) NOT NULL,
+		[intAnnouncementTypeId] INT NOT NULL,
+		[dtmStartDate] datetime NOT NULL,
+		[dtmEndDate] datetime NOT NULL,
+    [strAnnouncement] NVARCHAR(max) COLLATE Latin1_General_CI_AS NOT NULL,
     [intSort] INT NULL,
     [intConcurrencyId] INT NOT NULL DEFAULT 1,
-	CONSTRAINT [PK_tblHDAnnouncementType] PRIMARY KEY CLUSTERED ([intAnnouncementTypeId] ASC),
-	CONSTRAINT [UNQ_AnnouncementType] UNIQUE ([strAnnouncementType])
+	CONSTRAINT [PK_tblHDAnnouncement] PRIMARY KEY CLUSTERED ([intAnnouncementId] ASC),
+    CONSTRAINT [FK_Announcement_Type] FOREIGN KEY ([intAnnouncementTypeId]) REFERENCES [dbo].[tblHDAnnouncementType] ([intAnnouncementTypeId])
 )
 
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Announcement Id',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'tblHDAnnouncement',
+    @level2type = N'COLUMN',
+    @level2name = N'intAnnouncementId'
 GO
 EXEC sp_addextendedproperty @name = N'MS_Description',
     @value = N'Announcement Type Id',
     @level0type = N'SCHEMA',
     @level0name = N'dbo',
     @level1type = N'TABLE',
-    @level1name = N'tblHDAnnouncementType',
+    @level1name = N'tblHDAnnouncement',
     @level2type = N'COLUMN',
     @level2name = N'intAnnouncementTypeId'
 GO
 EXEC sp_addextendedproperty @name = N'MS_Description',
-    @value = N'Announcement Type',
+    @value = N'Start Date',
     @level0type = N'SCHEMA',
     @level0name = N'dbo',
     @level1type = N'TABLE',
-    @level1name = N'tblHDAnnouncementType',
+    @level1name = N'tblHDAnnouncement',
     @level2type = N'COLUMN',
-    @level2name = N'strAnnouncementType'
+    @level2name = N'dtmStartDate'
 GO
 EXEC sp_addextendedproperty @name = N'MS_Description',
-    @value = N'Description',
+    @value = N'End Date',
     @level0type = N'SCHEMA',
     @level0name = N'dbo',
     @level1type = N'TABLE',
-    @level1name = N'tblHDAnnouncementType',
+    @level1name = N'tblHDAnnouncement',
     @level2type = N'COLUMN',
-    @level2name = N'strDescription'
+    @level2name = N'dtmEndDate'
 GO
 EXEC sp_addextendedproperty @name = N'MS_Description',
-    @value = N'Display To',
+    @value = N'Announcement',
     @level0type = N'SCHEMA',
     @level0name = N'dbo',
     @level1type = N'TABLE',
-    @level1name = N'tblHDAnnouncementType',
+    @level1name = N'tblHDAnnouncement',
     @level2type = N'COLUMN',
-    @level2name = N'strDisplayTo'
+    @level2name = N'strAnnouncement'
 GO
 EXEC sp_addextendedproperty @name = N'MS_Description',
-    @value = N'Font Color',
+    @value = N'Sort Order',
     @level0type = N'SCHEMA',
     @level0name = N'dbo',
     @level1type = N'TABLE',
-    @level1name = N'tblHDAnnouncementType',
+    @level1name = N'tblHDAnnouncement',
     @level2type = N'COLUMN',
-    @level2name = N'strFontColor'
-GO
-EXEC sp_addextendedproperty @name = N'MS_Description',
-    @value = N'Back COlor',
-    @level0type = N'SCHEMA',
-    @level0name = N'dbo',
-    @level1type = N'TABLE',
-    @level1name = N'tblHDAnnouncementType',
-    @level2type = N'COLUMN',
-    @level2name = N'strBackColor'
+    @level2name = N'intSort'
 GO
 EXEC sp_addextendedproperty @name = N'MS_Description',
     @value = N'Concurrency Id',
     @level0type = N'SCHEMA',
     @level0name = N'dbo',
     @level1type = N'TABLE',
-    @level1name = N'tblHDAnnouncementType',
+    @level1name = N'tblHDAnnouncement',
     @level2type = N'COLUMN',
     @level2name = N'intConcurrencyId'
