677ae79a38d834a8d6015507b9b40b7a40ce4a84
Dan Michael Jalimao <Dan.Jalimao@irely.com>
2015-02-12 17:07:58 +0800

    FCOM-108

--- i21Database/dbo/Tables/tblSMTaxCode.sql
+++ i21Database/dbo/Tables/tblSMTaxCode.sql
@@ -2,7 +2,7 @@
 (
     [intTaxCodeId]			INT				NOT NULL PRIMARY KEY IDENTITY, 
     [strTaxCode]			NVARCHAR(50)    COLLATE Latin1_General_CI_AS NULL, 
-    --[intTaxTypeId]          INT				NULL, 
+    [intTaxTypeId]          INT				NULL, 
     [strDescription]        NVARCHAR(100)	COLLATE Latin1_General_CI_AS NULL, 
     [strCalculationMethod]	NVARCHAR(15)	COLLATE Latin1_General_CI_AS NULL, 
     [numRate]				NUMERIC(18, 6)	NULL, 
@@ -17,6 +17,7 @@
     [ysnTaxable]			BIT				NOT NULL	DEFAULT 0, 
     [ysnSeparateOnInvoice]	BIT				NOT NULL	DEFAULT 0, 
     [intConcurrencyId]		INT				NOT NULL DEFAULT 1, 
+    CONSTRAINT [FK_tblSMTaxCode_tblSMTaxType] FOREIGN KEY (intTaxTypeId) REFERENCES tblSMTaxType(intTaxTypeId),
     CONSTRAINT [FK_tblSMTaxCode_tblGLAccount_salesTax] FOREIGN KEY ([intSalesTaxAccountId]) REFERENCES tblGLAccount(intAccountId),
     CONSTRAINT [FK_tblSMTaxCode_tblGLAccount_purchaseTax] FOREIGN KEY ([intPurchaseTaxAccountId]) REFERENCES tblGLAccount(intAccountId)
 )
