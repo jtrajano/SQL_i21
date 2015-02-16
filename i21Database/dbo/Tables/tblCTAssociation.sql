1f91bedcf48283cc202ac165dc1ad3ca073fa530
Satya <ssahoo@BLRSATYAWIN7.irely.net>
2015-02-16 15:24:26 +0530

    Length changed

--- i21Database/dbo/Tables/tblCTAssociation.sql
+++ i21Database/dbo/Tables/tblCTAssociation.sql
@@ -3,6 +3,6 @@
 	[intAssociationId] INT NOT NULL PRIMARY KEY, 
     [intConcurrencyId] INT NOT NULL, 
     [strName] NVARCHAR(30) COLLATE Latin1_General_CI_AS NOT NULL, 
-    [strComment] NVARCHAR(250) COLLATE Latin1_General_CI_AS NULL, 
+    [strComment] NVARCHAR(500) COLLATE Latin1_General_CI_AS NULL, 
     [ysnActive] BIT NOT NULL
 )
