4dcb1167669d388d37ec18321eab0b1cdbb187fa
Lawrence Zabala <lawrence.zabala@irely.com>
2015-02-17 17:09:56 -0500

    Fixes on Account Category reference on Item Account

--- i21PreDeployment/Scripts/Pre-Deployment/11_IC/Account_Category_Change_14_4.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/11_IC/Account_Category_Change_14_4.sql
@@ -9,10 +9,10 @@ BEGIN
 		
 		EXEC('
 		UPDATE tblICItemAccount 
-		SET intAccountCategoryId = CASE WHEN intAccountCategoryId IN (''COGS'', ''Cost of Goods'') THEN ''10''
-								   WHEN intAccountCategoryId IN (''Sales'', ''Sales Account'') THEN ''33''
-								   WHEN intAccountCategoryId IN (''Purchase'', ''Purchase Account'') THEN ''30''
-								   WHEN intAccountCategoryId IN (''Variance'', ''Variance Account'') THEN ''40''
+		SET intAccountCategoryId = CASE WHEN strAccountDescription IN (''COGS'', ''Cost of Goods'') THEN ''10''
+								   WHEN strAccountDescription IN (''Sales'', ''Sales Account'') THEN ''33''
+								   WHEN strAccountDescription IN (''Purchase'', ''Purchase Account'') THEN ''30''
+								   WHEN strAccountDescription IN (''Variance'', ''Variance Account'') THEN ''40''
 								   ELSE NULL END')
 	END
 
