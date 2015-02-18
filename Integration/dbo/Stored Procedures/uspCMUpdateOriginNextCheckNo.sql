e9281256fd7313b30aed27ad67f430f5d0a4c888
Feb Montefrio <feb.montefrio@irely.com>
2015-02-18 09:31:52 -0500

    CM-579 Also add a stored procedure that will update the next check number. It will prevent errors when the origin integration is not executed.

---
+++ Integration/dbo/Stored Procedures/uspCMUpdateOriginNextCheckNo.sql
@@ -0,0 +1,22 @@
+﻿-- Create a integration compliant stored procedure. 
+-- There is another stored procedure of the same name in the i21Database project. 
+-- If there is an integration with the origin system, this stored procedure will be used. 
+-- Otherwise, the stored procedure in the i21Database will be used. 
+CREATE PROCEDURE uspCMUpdateOriginNextCheckNo
+	@strNextCheckNumber NVARCHAR(20)
+	,@intBankAccountId INT 
+AS
+
+SET QUOTED_IDENTIFIER OFF
+SET ANSI_NULLS ON
+SET NOCOUNT ON
+SET XACT_ABORT ON
+SET ANSI_WARNINGS OFF
+  
+UPDATE	dbo.apcbkmst_origin
+SET		apcbk_next_chk_no = CAST(dbo.fnAddZeroPrefixes(@strNextCheckNumber) AS INT) + 1
+FROM	dbo.apcbkmst_origin O INNER JOIN dbo.tblCMBankAccount f
+			ON f.strCbkNo = O.apcbk_no COLLATE Latin1_General_CI_AS
+WHERE	f.intBankAccountId = @intBankAccountId
+		AND ISNULL(@strNextCheckNumber, '') <> ''
+		AND O.apcbk_next_chk_no <= CAST(dbo.fnAddZeroPrefixes(@strNextCheckNumber) AS INT)  
