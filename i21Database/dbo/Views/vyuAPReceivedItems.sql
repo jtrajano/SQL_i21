ee521ae3794437c42229c7d5da493c31a00498cc
Erick F. Dela Cruz <erick.delacruz@irely.com>
2015-02-19 06:17:39 +0800

    AP-889

--- i21Database/dbo/Views/vyuAPReceivedItems.sql
+++ i21Database/dbo/Views/vyuAPReceivedItems.sql
@@ -20,13 +20,14 @@ SELECT
 	,D.strPurchaseOrderNumber
 	,F.strShipVia
 	,G.strTerm
-	,intAccountId = [dbo].[fnGetItemGLAccount](B.intItemId, A.intLocationId, 'AP Clearing')
-	,strAccountId = (SELECT strAccountId FROM tblGLAccount WHERE intAccountId = dbo.fnGetItemGLAccount(B.intItemId, A.intLocationId, 'AP Clearing'))
+	,intAccountId = [dbo].[fnGetItemGLAccount](B.intItemId, loc.intItemLocationId, 'AP Clearing')
+	,strAccountId = (SELECT strAccountId FROM tblGLAccount WHERE intAccountId = dbo.fnGetItemGLAccount(B.intItemId, loc.intItemLocationId, 'AP Clearing'))
 FROM tblICInventoryReceipt A
 	INNER JOIN tblICInventoryReceiptItem B ON A.intInventoryReceiptId = B.intInventoryReceiptId
 	INNER JOIN tblICItem C ON B.intItemId = C.intItemId
 	INNER JOIN tblPOPurchase D ON B.intSourceId = D.intPurchaseId
 	INNER JOIN  (tblAPVendor E1 INNER JOIN tblEntity E2 ON E1.intEntityId = E2.intEntityId) ON D.intVendorId = E1.intVendorId
+	INNER JOIN tblICItemLocation loc ON B.intItemId = loc.intItemId AND A.intLocationId = loc.intLocationId
 	LEFT JOIN tblSMShipVia F ON D.intShipViaId = F.intShipViaID
 	LEFT JOIN tblSMTerm G ON D.intTermsId = G.intTermID
 WHERE strReceiptType = 'Purchase Order' AND ysnPosted = 1
