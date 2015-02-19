2e0d2c3da145aa40c58be31e7cdf5622e724ad03
Arman Arenas <arman.arenas@irely.com>
2015-02-20 02:34:37 +0800

    Fix for http://bamboo.i21build.com/deployment-download/12713986/build_logs/12615682-12713986-11796523.log

--- Integration/dbo/Stored Procedures/uspCMUpdateOriginNextCheckNo.sql
+++ Integration/dbo/Stored Procedures/uspCMUpdateOriginNextCheckNo.sql
@@ -4,21 +4,33 @@
 -- Otherwise, the stored procedure in the i21Database will be used. 
 
 
-CREATE PROCEDURE uspCMUpdateOriginNextCheckNo
-	@strNextCheckNumber NVARCHAR(20)
-	,@intBankAccountId INT 
-AS
+IF (SELECT TOP 1 ysnUsed FROM ##tblOriginMod WHERE strPrefix = 'AP') = 1
+BEGIN
 
-SET QUOTED_IDENTIFIER OFF
-SET ANSI_NULLS ON
-SET NOCOUNT ON
-SET XACT_ABORT ON
-SET ANSI_WARNINGS OFF
+	EXEC('
+		IF EXISTS (SELECT 1 FROM sys.objects WHERE name = ''uspCMUpdateOriginNextCheckNo'' and type = ''P'') 
+			DROP PROCEDURE [dbo].[uspCMUpdateOriginNextCheckNo];
+	')
+
+	EXEC ('
+		CREATE PROCEDURE uspCMUpdateOriginNextCheckNo
+			@strNextCheckNumber NVARCHAR(20)
+			,@intBankAccountId INT 
+		AS
+
+		SET QUOTED_IDENTIFIER OFF
+		SET ANSI_NULLS ON
+		SET NOCOUNT ON
+		SET XACT_ABORT ON
+		SET ANSI_WARNINGS OFF
   
-UPDATE	dbo.apcbkmst_origin
-SET		apcbk_next_chk_no = CAST(dbo.fnAddZeroPrefixes(@strNextCheckNumber) AS INT) + 1
-FROM	dbo.apcbkmst_origin O INNER JOIN dbo.tblCMBankAccount f
-			ON f.strCbkNo = O.apcbk_no COLLATE Latin1_General_CI_AS
-WHERE	f.intBankAccountId = @intBankAccountId
-		AND ISNULL(@strNextCheckNumber, '') <> ''
-		AND O.apcbk_next_chk_no <= CAST(dbo.fnAddZeroPrefixes(@strNextCheckNumber) AS INT)  
+		UPDATE	dbo.apcbkmst_origin
+		SET		apcbk_next_chk_no = CAST(dbo.fnAddZeroPrefixes(@strNextCheckNumber) AS INT) + 1
+		FROM	dbo.apcbkmst_origin O INNER JOIN dbo.tblCMBankAccount f
+					ON f.strCbkNo = O.apcbk_no COLLATE Latin1_General_CI_AS
+		WHERE	f.intBankAccountId = @intBankAccountId
+				AND ISNULL(@strNextCheckNumber, '''') <> ''''
+				AND O.apcbk_next_chk_no <= CAST(dbo.fnAddZeroPrefixes(@strNextCheckNumber) AS INT)
+	')
+
+END
