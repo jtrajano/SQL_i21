ba055163c7d70758fb1edce4843b41beae114457
Lawrence Zabala <lawrence.zabala@irely.com>
2015-02-23 16:59:36 -0500

    IC-497

--- app/view/InventoryReceiptViewController.js
+++ app/view/InventoryReceiptViewController.js
@@ -1147,6 +1147,31 @@ Ext.define('Inventory.view.InventoryReceiptViewController', {
         };
     },
 
+    onSpecialKeyTab: function(component, e, eOpts) {
+        var win = component.up('window');
+        if(win) {
+            if (e.getKey() === Ext.event.Event.TAB) {
+                var gridObj = win.query('#grdInventoryReceipt')[0],
+                    sel = gridObj.getStore().getAt(0);
+
+                if(sel && gridObj){
+                    gridObj.setSelection(sel);
+
+                    var task = new Ext.util.DelayedTask(function(){
+                        gridObj.plugins[0].startEditByPosition({
+                            row: 0,
+                            column: 1
+                        });
+                        var txtNotes = gridObj.query('#txtNotes')[0];
+                        txtNotes.focus();
+                    });
+
+                    task.delay(10);
+                }
+            }
+        }
+    },
+
     init: function(application) {
         this.control({
             "#cboVendor": {
@@ -1202,6 +1227,9 @@ Ext.define('Inventory.view.InventoryReceiptViewController', {
             },
             "#colItemNo": {
                 beforerender: this.onItemGridColumnBeforeRender
+            },
+            "#txtNotes": {
+                specialKey: this.onSpecialKeyTab
             }
         })
     }
