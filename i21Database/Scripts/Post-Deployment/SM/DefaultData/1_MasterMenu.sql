124af3628972fccff3fdd3e8e3750b8a84f7eb7d
jeffrey trajano <jeffrey.trajano@irely.com>
2015-02-25 15:11:05 +0800

    GL-1429 MVVM Conversions update tblSMMasterMenu

--- i21Database/Scripts/Post-Deployment/SM/DefaultData/1_MasterMenu.sql
+++ i21Database/Scripts/Post-Deployment/SM/DefaultData/1_MasterMenu.sql
@@ -1469,8 +1469,11 @@ GO
 		UPDATE tblSMMasterMenu SET strCommand = REPLACE (strCommand,'controller','view') WHERE strMenuName = 'Build Accounts' AND strModuleName = 'General Ledger' AND strCommand = 'GeneralLedger.controller.BuildAccounts'
 		UPDATE tblSMMasterMenu SET strCommand = 'GeneralLedger.view.BatchPosting' WHERE strMenuName = 'Batch Posting' AND strModuleName = 'General Ledger' AND strMenuName = 'Batch Posting'
 		UPDATE tblSMMasterMenu SET strCommand = 'GeneralLedger.view.AccountGroups' WHERE strMenuName = 'Account Groups' AND strModuleName = 'General Ledger' AND strCommand = 'GeneralLedger.controller.AccountGroups'
-
-
+		UPDATE tblSMMasterMenu SET strCommand = 'GeneralLedger.view.GLAccountAdjustment' WHERE strModuleName = 'General Ledger' AND  strCommand  = 'GeneralLedger.controller.GLAccountAdjustment'
+		UPDATE tblSMMasterMenu SET strCommand = 'GeneralLedger.view.Reallocation' WHERE strMenuName = 'Reallocation' AND strModuleName = 'General Ledger' AND strCommand = 'GeneralLedger.controller.Reallocation'
+		UPDATE tblSMMasterMenu SET strCommand = 'GeneralLedger.view.FiscalYear' WHERE strMenuName = 'Fiscal Year' AND strModuleName = 'General Ledger' AND strCommand = 'GeneralLedger.controller.FiscalYear'
+		UPDATE tblSMMasterMenu SET strCommand = 'GeneralLedger.view.RecurringJournalHistory' WHERE strMenuName = 'Recurring Journal History' AND strModuleName = 'General Ledger' AND strCommand = 'GeneralLedger.controller.RecurringJournalHistory'
+		UPDATE tblSMMasterMenu SET strCommand = 'GeneralLedger.view.RecurringJournal' WHERE strMenuName = 'Recurring Journal' AND strModuleName = 'General Ledger' AND strCommand = 'GeneralLedger.controller.RecurringJournal'
 
 	/* ---------------------------------------- */
 	/* --   End General Ledger Module Menu   -- */
