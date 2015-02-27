4b357fdb47a94da2349a2257b2aaf122721b3f56
Lawrence Zabala <lawrence.zabala@irely.com>
2015-02-27 11:59:23 -0500

    IC-522

--- i21Database/dbo/Views/vyuICGetItemPricing.sql
+++ i21Database/dbo/Views/vyuICGetItemPricing.sql
@@ -2,32 +2,32 @@
 	AS 
 
 SELECT 
-intPricingKey = CAST(ROW_NUMBER() OVER(ORDER BY Item.intKey, ItemPricing.intItemPricingId) AS INT),
-Item.intKey,
-Item.strDescription,
-ItemUOM.strUpcCode,
-ItemPricing.intItemPricingId,
-ItemPricing.intItemId,
-Item.intLocationId,
-ItemPricing.intItemLocationId,
-Item.strLocationName,
-Item.strLocationType,
-ItemUOM.intItemUOMId as intItemUnitMeasureId,
-UOM.intUnitMeasureId,
-UOM.strUnitMeasure,
-UOM.strUnitType,
-ItemUOM.ysnStockUnit,
-ItemUOM.dblUnitQty,
-ItemPricing.dblAmountPercent,
-dblSalePrice = ItemPricing.dblSalePrice * ItemUOM.dblUnitQty,
-dblMSRPPrice = ItemPricing.dblMSRPPrice * ItemUOM.dblUnitQty,
-ItemPricing.strPricingMethod,
-dblLastCost = ItemPricing.dblLastCost * ItemUOM.dblUnitQty,
-dblStandardCost = ItemPricing.dblStandardCost * ItemUOM.dblUnitQty,
-dblAverageCost = ItemPricing.dblAverageCost * ItemUOM.dblUnitQty,
-dblEndMonthCost = ItemPricing.dblEndMonthCost * ItemUOM.dblUnitQty,
-ItemPricing.intSort
+	intPricingKey = CAST(ROW_NUMBER() OVER(ORDER BY Item.intKey, ItemPricing.intItemPricingId) AS INT),
+	Item.intKey,
+	Item.strDescription,
+	ItemUOM.strUpcCode,
+	ItemPricing.intItemPricingId,
+	Item.intItemId,
+	Item.intLocationId,
+	ItemPricing.intItemLocationId as intItemLocationId,
+	Item.strLocationName,
+	Item.strLocationType,
+	ItemUOM.intItemUOMId as intItemUnitMeasureId,
+	UOM.intUnitMeasureId,
+	UOM.strUnitMeasure,
+	UOM.strUnitType,
+	ItemUOM.ysnStockUnit,
+	ItemUOM.dblUnitQty,
+	dblAmountPercent = ISNULL(ItemPricing.dblAmountPercent, 0),
+	dblSalePrice = ISNULL(ItemPricing.dblSalePrice * ItemUOM.dblUnitQty, 0),
+	dblMSRPPrice = ISNULL(ItemPricing.dblMSRPPrice * ItemUOM.dblUnitQty, 0),
+	ItemPricing.strPricingMethod,
+	dblLastCost = ISNULL(ItemPricing.dblLastCost * ItemUOM.dblUnitQty, 0),
+	dblStandardCost = ISNULL(ItemPricing.dblStandardCost * ItemUOM.dblUnitQty, 0),
+	dblAverageCost = ISNULL(ItemPricing.dblAverageCost * ItemUOM.dblUnitQty, 0),
+	dblEndMonthCost = ISNULL(ItemPricing.dblEndMonthCost * ItemUOM.dblUnitQty, 0),
+	ItemPricing.intSort
 FROM vyuICGetItemStock Item
-INNER JOIN tblICItemPricing ItemPricing ON ItemPricing.intItemId = Item.intItemId and ItemPricing.intItemLocationId = Item.intItemLocationId
+LEFT JOIN tblICItemPricing ItemPricing ON ItemPricing.intItemId = Item.intItemId and ItemPricing.intItemLocationId = Item.intItemLocationId
 LEFT JOIN tblICItemUOM ItemUOM ON ItemUOM.intItemId = Item.intItemId
 LEFT JOIN tblICUnitMeasure UOM ON UOM .intUnitMeasureId= ItemUOM.intUnitMeasureId
