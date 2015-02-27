762ece0ed82ec3a2f40f5a30cbeaa0b380181121
Chakra <chakradhar.behera@irely.com>
2015-02-27 22:20:14 +0530

    Work Order Status Data

--- i21Database/Scripts/Post-Deployment/MF/1_MasterTables.sql
+++ i21Database/Scripts/Post-Deployment/MF/1_MasterTables.sql
@@ -58,3 +58,93 @@ BEGIN
     VALUES(2,'Closed')
 END
 GO
+IF NOT EXISTS(SELECT * FROM tblMFWorkOrderProductionType WHERE intProductionTypeId = 1)
+BEGIN
+    INSERT INTO tblMFWorkOrderProductionType(intProductionTypeId,strName)
+    VALUES(1,'Make To Order')
+END
+GO
+IF NOT EXISTS(SELECT * FROM tblMFWorkOrderProductionType WHERE intProductionTypeId = 2)
+BEGIN
+    INSERT INTO tblMFWorkOrderProductionType(intProductionTypeId,strName)
+    VALUES(2,'Stock')
+END
+GO
+IF NOT EXISTS(SELECT * FROM tblMFWorkOrderStatus WHERE intStatusId = 1)
+BEGIN
+    INSERT INTO tblMFWorkOrderStatus(intStatusId,strName)
+    VALUES(1,'New')
+END
+GO
+IF NOT EXISTS(SELECT * FROM tblMFWorkOrderStatus WHERE intStatusId = 2)
+BEGIN
+    INSERT INTO tblMFWorkOrderStatus(intStatusId,strName)
+    VALUES(2,'Not Released')
+END
+GO
+IF NOT EXISTS(SELECT * FROM tblMFWorkOrderStatus WHERE intStatusId = 3)
+BEGIN
+    INSERT INTO tblMFWorkOrderStatus(intStatusId,strName)
+    VALUES(3,'Open')
+END
+GO
+IF NOT EXISTS(SELECT * FROM tblMFWorkOrderStatus WHERE intStatusId = 4)
+BEGIN
+    INSERT INTO tblMFWorkOrderStatus(intStatusId,strName)
+    VALUES(4,'Frozen')
+END
+GO
+IF NOT EXISTS(SELECT * FROM tblMFWorkOrderStatus WHERE intStatusId = 5)
+BEGIN
+    INSERT INTO tblMFWorkOrderStatus(intStatusId,strName)
+    VALUES(5,'Hold')
+END
+GO
+IF NOT EXISTS(SELECT * FROM tblMFWorkOrderStatus WHERE intStatusId = 6)
+BEGIN
+    INSERT INTO tblMFWorkOrderStatus(intStatusId,strName)
+    VALUES(6,'Pre Kitted')
+END
+GO
+IF NOT EXISTS(SELECT * FROM tblMFWorkOrderStatus WHERE intStatusId = 7)
+BEGIN
+    INSERT INTO tblMFWorkOrderStatus(intStatusId,strName)
+    VALUES(7,'Kitted')
+END
+GO
+IF NOT EXISTS(SELECT * FROM tblMFWorkOrderStatus WHERE intStatusId = 8)
+BEGIN
+    INSERT INTO tblMFWorkOrderStatus(intStatusId,strName)
+    VALUES(8,'Kit Transferred')
+END
+GO
+IF NOT EXISTS(SELECT * FROM tblMFWorkOrderStatus WHERE intStatusId = 9)
+BEGIN
+    INSERT INTO tblMFWorkOrderStatus(intStatusId,strName)
+    VALUES(9,'Released')
+END
+GO
+IF NOT EXISTS(SELECT * FROM tblMFWorkOrderStatus WHERE intStatusId = 10)
+BEGIN
+    INSERT INTO tblMFWorkOrderStatus(intStatusId,strName)
+    VALUES(10,'Started')
+END
+GO
+IF NOT EXISTS(SELECT * FROM tblMFWorkOrderStatus WHERE intStatusId = 11)
+BEGIN
+    INSERT INTO tblMFWorkOrderStatus(intStatusId,strName)
+    VALUES(11,'Paused')
+END
+GO
+IF NOT EXISTS(SELECT * FROM tblMFWorkOrderStatus WHERE intStatusId = 12)
+BEGIN
+    INSERT INTO tblMFWorkOrderStatus(intStatusId,strName)
+    VALUES(12,'Staged')
+END
+GO
+IF NOT EXISTS(SELECT * FROM tblMFWorkOrderStatus WHERE intStatusId = 13)
+BEGIN
+    INSERT INTO tblMFWorkOrderStatus(intStatusId,strName)
+    VALUES(13,'Completed')
+END
+GO
