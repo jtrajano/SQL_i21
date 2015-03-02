34ad8d8a3bdefa51ebcc24c73e2b17fcac723e82
unknown <rshanmuganathan@BLRRAMW8PRON.irely.net>
2015-03-02 12:32:39 +0530

    RM-3 and RM-6.  Risk Management changes

---
+++ i21Database/dbo/Tables/tblRKBrokerageAccount.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tblRKBrokerageAccount]
+(
+	[intBrokerageAccountId] INT IDENTITY(1,1) NOT NULL, 
+    [intBrokerId] INT NOT NULL, 
+    [strAccountNumber] NVARCHAR(50) COLLATE Latin1_General_CI_AS  NOT NULL, 
+    [intInstrumentTypeId] INT NOT NULL, 
+    [strDescription] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
+    [intConcurrencyId] INT NOT NULL, 
+    CONSTRAINT [PK_tblRKBrokerageAccount_intBrokerageAccountId] PRIMARY KEY ([intBrokerageAccountId]), 
+    CONSTRAINT [UK_tblRKBrokerageAccount_intBrokerId_strAccountNumber] UNIQUE ([intBrokerId], [strAccountNumber])
+)
