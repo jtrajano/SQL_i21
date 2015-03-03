f3ed614d55425da9cfc8e251c4e6f1661ef16d89
Ruben  Escarcha <ruben.escarcha@irely.com>
2015-03-04 03:58:47 +0800

    AR-864 Invoice Tax Calculation - first cut

---
+++ i21Database/dbo/Views/vyuARCustomerTaxCode.sql
@@ -0,0 +1,33 @@
+﻿CREATE VIEW [dbo].[vyuARCustomerTaxCode]
+	AS 
+	
+
+SELECT 
+	 C.intCustomerId
+	,C.strCustomerNumber 
+	,L.intEntityLocationId
+	,L.strLocationName 
+	,G.intTaxGroupId
+	,G.strTaxGroup 
+	,TG.ysnSeparateOnInvoice
+	,TC.intTaxCodeId
+	,TC.strTaxCode
+	,TC.intSalesTaxAccountId  
+	,TC.strCalculationMethod
+	,TC.numRate
+FROM
+	tblEntityLocation L
+INNER JOIN
+	tblARCustomer C
+		ON L.intEntityId = C.intEntityId 
+		AND L.intEntityLocationId = C.intDefaultLocationId
+INNER JOIN
+	tblSMTaxGroup G
+		ON L.intTaxCodeId = G.intTaxGroupId		
+INNER JOIN
+	tblSMTaxGroupCode TG
+		ON G.intTaxGroupId = TG.intTaxGroupId
+LEFT OUTER JOIN
+	tblSMTaxCode TC
+		ON TG.intTaxCodeId = TC.intTaxCodeId 		
+
