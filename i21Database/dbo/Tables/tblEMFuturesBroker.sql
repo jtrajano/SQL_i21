576516fa204640b9557801bce615262331e04413
Mon Gonzales <mon.gonzales@irely.com>
2015-03-03 19:03:52 +0800

    EM-35 Code Backup

--- i21Database/dbo/Tables/tblEMFuturesBroker.sql
+++ i21Database/dbo/Tables/tblEMFuturesBroker.sql
@@ -1,5 +1,9 @@
 ﻿CREATE TABLE [dbo].[tblEMFuturesBroker]
 (
 	[intEntityId] INT NOT NULL PRIMARY KEY, 
-    [strVendorId] NVARCHAR(50) NULL
+    [strVendorId] NVARCHAR(50) NOT NULL,
+	[intContactId] INT NOT NULL, 
+    CONSTRAINT [FK_tblEMFutureBroker_tblEntityContact] FOREIGN KEY ([intContactId]) REFERENCES [tblEntityContact]([intContactId]),
+	CONSTRAINT [UQ_tblEMFuturesBroker_strVendorId] UNIQUE NONCLUSTERED ([strVendorId] ASC)
+	
 )
