cb7e3e7ecfa0059727cb1a4c34dd2097096178ec
Erick F. Dela Cruz <erick.delacruz@irely.com>
2015-03-10 03:59:53 +0800

    IC-567

---
+++ i21Database/dbo/Stored Procedures/uspPOUpdateStatus.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[uspPOUpdateStatus]
+    @poId int,
+    @status int
+AS
+BEGIN
+
+    UPDATE A
+        SET A.intOrderStatusId = @status
+    FROM tblPOPurchase
+    WHERE intPurchaseId = @poId
+
+END
