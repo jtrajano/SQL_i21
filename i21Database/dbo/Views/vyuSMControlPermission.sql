7f2bb02b90083cc7da3218734009c13103e452b6
von.pasion <von.pasion@irely.com>
2015-03-10 20:55:08 +0800

    FRM-1984

---
+++ i21Database/dbo/Views/vyuSMControlPermission.sql
@@ -0,0 +1,13 @@
+﻿CREATE VIEW [dbo].[vyuSMControlPermission]
+AS 
+	SELECT 
+		A.intUserSecurityControlPermissionId AS intControlPermissionId,
+		B.strControlId, 
+		B.strControlType,
+		A.strPermission, 
+		A.intUserSecurityId, 
+		C.strNamespace,
+		A.intConcurrencyId
+	FROM tblSMUserSecurityControlPermission A 
+		INNER JOIN tblSMControl B ON A.intControlId = B.intControlId
+		INNER JOIN tblSMScreen C ON B.intScreenId = C.intScreenId 
