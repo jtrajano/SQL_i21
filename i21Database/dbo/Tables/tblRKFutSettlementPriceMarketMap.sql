8b45554dd542bd84c0e08a937efa5931314c0a7a
unknown <rshanmuganathan@BLRRAMW8PRON.irely.net>
2015-03-11 16:31:12 +0530

    RK- 10 Program Futures Price Settlement

---
+++ i21Database/dbo/Tables/tblRKFutSettlementPriceMarketMap.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[tblRKFutSettlementPriceMarketMap]
+(
+	[intFutSettlementPriceMonthId] INT IDENTITY(1,1) NOT NULL, 
+	[intConcurrencyId] INT NOT NULL,
+    [intFutureSettlementPriceId] INT NOT NULL, 
+    [intFutureMonthId] INT NOT NULL, 
+    [dblLastSettle] NUMERIC(18, 6) NOT NULL, 
+    [dblLow] NUMERIC(18, 6) NOT NULL, 
+    [dblHigh] NUMERIC(18, 6) NOT NULL, 
+    [strComments] NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NULL,	
+    CONSTRAINT [PK_tblRKFutSettlementPriceMarketMap_intFutSettlementPriceMonthId] PRIMARY KEY ([intFutSettlementPriceMonthId]), 
+	CONSTRAINT [FK_tblRKFutSettlementPriceMarketMap_tblRKFuturesSettlementPrice_intFutureSettlementPriceId] FOREIGN KEY ([intFutureSettlementPriceId]) REFERENCES [tblRKFuturesSettlementPrice]([intFutureSettlementPriceId])ON DELETE CASCADE,
+	CONSTRAINT [FK_tblRKFutSettlementPriceMarketMap_tblRKFuturesMonth_intFutureMonthId] FOREIGN KEY ([intFutureMonthId]) REFERENCES [tblRKFuturesMonth]([intFutureMonthId])
+	
+)
---
+++ i21Database/dbo/Tables/tblRKOptSettlementPriceMarketMap.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[tblRKOptSettlementPriceMarketMap]
+(
+	[intOptSettlementPriceMonthId] INT IDENTITY(1,1) NOT NULL, 
+	[intConcurrencyId] INT NOT NULL,
+    [intFutureSettlementPriceId] INT NOT NULL, 
+    [intOptionMonthId] INT NOT NULL, 
+    [dblStrike] NUMERIC(18, 6) NOT NULL, 
+    [intType] int NOT NULL, 
+    [dblSettle] NUMERIC(18, 6) NOT NULL, 
+	[dblDelta] NUMERIC(18, 6) NOT NULL, 
+    [strComments] NVARCHAR(MAX)  COLLATE Latin1_General_CI_AS NULL,	
+	CONSTRAINT [PK_tblRKOptSettlementPriceMarketMap_intOptSettlementPriceMonthId] PRIMARY KEY ([intOptSettlementPriceMonthId]), 
+	CONSTRAINT [FK_tblRKOptSettlementPriceMarketMap_tblRKFutureSettlementPrice_intFutureSettlementPriceId] FOREIGN KEY ([intFutureSettlementPriceId]) REFERENCES [tblRKFuturesSettlementPrice]([intFutureSettlementPriceId])ON DELETE CASCADE,
+	CONSTRAINT [FK_tblRKOptSettlementPriceMarketMap_tblRKOptionsMonth_intFutureMonthId] FOREIGN KEY ([intOptionMonthId]) REFERENCES [tblRKOptionsMonth]([intOptionMonthId])
+	
+)
