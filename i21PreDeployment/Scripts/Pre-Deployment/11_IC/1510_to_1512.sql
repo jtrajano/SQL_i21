a4d0c58e9c081176024d9fa11dff5d8637cb8282
Feb Montefrio <feb.montefrio@irely.com>
2015-03-13 10:06:19 -0400

    Move the Lot Number fix to the post deployment table.

--- i21PreDeployment/Scripts/Pre-Deployment/11_IC/1510_to_1512.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/11_IC/1510_to_1512.sql
@@ -19,21 +19,4 @@ BEGIN
 	END
 END
 
-IF EXISTS (SELECT * FROM sys.tables WHERE object_id = object_id('tblICInventoryReceiptItemLot'))
-BEGIN
-	IF EXISTS(SELECT * FROM sys.columns WHERE object_id = object_id('tblICInventoryReceiptItemLot') AND name = 'strLotNumber')
-	BEGIN
-		-- Repopulate the missing strLotNumber value
-		EXEC('
-			UPDATE	ItemLot
-			SET		strLotNumber = Lot.strLotNumber
-			FROM	dbo.tblICInventoryReceiptItemLot ItemLot INNER JOIN tblICLot Lot
-						ON ItemLot.intLotId = Lot.intLotId
-			WHERE	ItemLot.intLotId IS NOT NULL 
-					AND ISNULL(ItemLot.strLotNumber, '''') = ''''
-		')
-	END
-END
-
-
 PRINT N'END INVENTORY PATH from 15.10.x.x to 15.12.x.x'
