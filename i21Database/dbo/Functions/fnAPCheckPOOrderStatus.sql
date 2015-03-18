c426d36d622923aa4875b562bb5af4b2228e25b7
Erick F. Dela Cruz <erick.delacruz@irely.com>
2015-03-18 15:47:30 +0800

    AP-1008

--- i21Database/dbo/Functions/fnAPCheckPOOrderStatus.sql
+++ i21Database/dbo/Functions/fnAPCheckPOOrderStatus.sql
@@ -9,23 +9,34 @@ BEGIN
 
 	DECLARE @success AS BIT = 0;
 
-	SET @success = 
-	(
-		CASE 
-			WHEN @orderStatus = 1 AND EXISTS
-			(
-				SELECT 1 FROM tblICInventoryReceiptItem 
-				WHERE intSourceId = @poId
-			)
-			THEN 0
-			WHEN @orderStatus = 7 AND NOT EXISTS
-			(
-				SELECT 1 FROM tblICInventoryReceiptItem 
-				WHERE intSourceId = @poId
-			)
-			THEN 0
-		ELSE 1 END
-	)
+	IF EXISTS(SELECT 1 FROM tblPOPurchaseDetail A
+				INNER JOIN tblICItem B ON A.intItemId = B.intItemId 
+				WHERE strType NOT IN ('Non-Inventory', 'Other Charge', 'Service') AND intPurchaseId = @poId)
+	BEGIN
+
+		SET @success = 
+		(
+			CASE 
+				WHEN @orderStatus = 1 AND EXISTS
+				(
+					SELECT 1 FROM tblICInventoryReceiptItem 
+					WHERE intSourceId = @poId
+				)
+				THEN 0
+				WHEN @orderStatus = 7 AND NOT EXISTS
+				(
+					SELECT 1 FROM tblICInventoryReceiptItem 
+					WHERE intSourceId = @poId
+				)
+				THEN 0
+			ELSE 1 END
+		)
+
+	END
+	ELSE
+	BEGIN
+		SET @success = 1;
+	END
 
 	RETURN(@success)
 END
