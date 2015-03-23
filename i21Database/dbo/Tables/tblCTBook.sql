1320f0de847c6dbab0548c148e0bdfcee464d995
Satya <ssahoo@BLRSATYAWIN7.irely.net>
2015-03-23 16:55:55 +0530

    CT - Book and SubBook tables added

---
+++ i21Database/dbo/Tables/tblCTBook.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[tblCTBook]
+(
+	[intBookId] INT NOT NULL IDENTITY, 
+    [strBook] NVARCHAR(10) COLLATE Latin1_General_CI_AS NOT NULL, 
+    [strBookDescription] NVARCHAR(30) COLLATE Latin1_General_CI_AS NOT NULL, 
+    [ysnActive] BIT NOT NULL,
+	CONSTRAINT [PK_tblCTBook_intBookId] PRIMARY KEY CLUSTERED ([intBookId] ASC), 
+    CONSTRAINT [UK_tblCTBook_strBook] UNIQUE ([strBook])
+)
---
+++ i21Database/dbo/Tables/tblCTSubBook.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tblCTSubBook]
+(
+	[intSubBookId] INT NOT NULL IDENTITY, 
+	intBookId INT NOT NULL,
+    [strSubBook] NVARCHAR(10) COLLATE Latin1_General_CI_AS NOT NULL, 
+    [strSubBookDescription] NVARCHAR(30) COLLATE Latin1_General_CI_AS NOT NULL, 
+    [ysnActive] BIT NOT NULL,
+	CONSTRAINT [PK_tblCTSubBook_intSubBookId] PRIMARY KEY CLUSTERED ([intSubBookId] ASC), 
+    CONSTRAINT [UK_tblCTSubBook_strSubBook] UNIQUE ([strSubBook]), 
+    CONSTRAINT [FK_tblCTSubBook_tblCTBook_intBookId] FOREIGN KEY ([intBookId]) REFERENCES [tblCTBook]([intBookId])
+)
