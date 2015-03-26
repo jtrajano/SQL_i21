1c724f01527de90ad2d06ea1331db18242ae40d3
unknown <jbalachander@BLRJAMESBWIN7.irely.net>
2015-03-26 15:19:24 +0530

    LG-14 Developed Equipment Type Maintenance screen

---
+++ i21Database/dbo/Tables/tblLGEquipmentType.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[tblLGEquipmentType]
+(
+ [intEquipmentTypeId] INT NOT NULL IDENTITY, 
+ [intConcurrencyId] INT NOT NULL, 
+ [strEquipmentType] NVARCHAR(100) COLLATE Latin1_General_CI_AS NOT NULL
+ 
+ CONSTRAINT [PK_tblLGEquipmentType_intEquipmentTypeId] PRIMARY KEY ([intEquipmentTypeId])
+)
