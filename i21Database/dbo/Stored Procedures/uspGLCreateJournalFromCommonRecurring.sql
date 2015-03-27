b99c8a5b3fc35a53888231290cb0b76335047cb5
jeffrey trajano <jeffrey.trajano@irely.com>
2015-03-27 11:52:35 +0800

    GL-1416 Create Journal enteries from the Common Recurring screen

--- i21Database/dbo/Stored Procedures/uspGLCreateJournalFromCommonRecurring.sql
+++ i21Database/dbo/Stored Procedures/uspGLCreateJournalFromCommonRecurring.sql
@@ -7,12 +7,11 @@ CREATE PROCEDURE [dbo].[uspGLCreateJournalFromCommonRecurring]
 	@journalId  INT, 
 	@journalDate DATETIME,
 	@userId  INT,
-	@strJournalId NVARCHAR(50) OUTPUT 
+	@strJournalId NVARCHAR(100) OUTPUT 
 AS
 BEGIN
 	SET NOCOUNT ON;
 	DECLARE @entityid INT ,@intNumber INT, @strPrefix VARCHAR(10),@smID varchar(10),@intJournalID INT
-	SELECT  @journalDate  = CONVERT(DATE ,@journalDate ,101)
 	BEGIN TRY
 		BEGIN TRANSACTION
 			SELECT TOP 1 @entityid =intEntityId FROM tblSMUserSecurity WHERE intUserSecurityID = @userId
@@ -20,13 +19,15 @@ BEGIN
 			SET @smID = @strPrefix + CONVERT(VARCHAR(5),@intNumber)
 			UPDATE tblSMStartingNumber SET intNumber = @intNumber + 1 WHERE intStartingNumberId = 4
 			INSERT INTO tblGLJournal(strDescription,intCurrencyId,dtmDate,dtmReverseDate,strJournalId,strJournalType,strTransactionType,strSourceType,intEntityId,ysnPosted,strRecurringStatus)
-			SELECT strDescription,intCurrencyId,@journalDate,dtmReverseDate,@smID,'Common Recurring Journal','General Journal','GJ',@entityid,0,'Locked' FROM tblGLJournal
+			SELECT strDescription,intCurrencyId,dtmDate,dtmReverseDate,@smID,'Common Recurring Journal','General Journal','GJ',@entityid,0,'Locked' FROM tblGLJournal
 			WHERE intJournalId = @journalId
 			SELECT  @intJournalID = SCOPE_IDENTITY() 
 			INSERT INTO tblGLJournalDetail (intJournalId,intAccountId,intLineNo,dblCredit,dblCreditUnit,dblCreditRate,dblDebit,dblDebitUnit, dblDebitRate,strDescription,strReference,strComments,strDocument,dtmDate)
-			SELECT @intJournalID,intAccountId,intLineNo,dblCredit,dblCreditUnit,dblCreditRate,dblDebit,dblDebitUnit,dblDebitRate,strDescription,strReference,strComments,strDocument,@journalDate
+			SELECT @intJournalID,intAccountId,intLineNo,dblCredit,dblCreditUnit,dblCreditRate,dblDebit,dblDebitUnit,dblDebitRate,strDescription,strReference,strComments,strDocument,dtmDate
 			FROM tblGLJournalDetail
 			WHERE intJournalId = @journalId
+		CLOSE cursor_id
+		DEALLOCATE cursor_id
 		COMMIT TRANSACTION
 		SET @strJournalId = @smID
 	END TRY
