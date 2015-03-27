e0979ae04a449ecbe00806173477b0b524e7f44a
unknown <DJalimao@DanMichael.COMTECH.COM>
2015-03-27 11:02:00 +0800

    FCOM-301

--- i21PreDeployment/Scripts/Pre-Deployment/4_SM/3_DataMigration.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/4_SM/3_DataMigration.sql
@@ -12,8 +12,12 @@ GO
 	PRINT N'Check if intUserSecurityMenuID is existing'
 	IF EXISTS(SELECT TOP 1 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE [TABLE_NAME] = 'tblSMUserSecurityMenuFavorite' AND [COLUMN_NAME] = 'intUserSecurityMenuId') 
 	BEGIN
-		PRINT N'Drop FK_tblSMUserSecurityMenuFavorite_tblSMUserSecurityMenu'
-		ALTER TABLE tblSMUserSecurityMenuFavorite DROP CONSTRAINT FK_tblSMUserSecurityMenuFavorite_tblSMUserSecurityMenu;
+
+		IF EXISTS(SELECT TOP 1 1 FROM sys.objects where name = 'FK_tblSMUserSecurityMenuFavorite_tblSMUserSecurityMenu')
+		BEGIN
+			PRINT N'Drop FK_tblSMUserSecurityMenuFavorite_tblSMUserSecurityMenu'
+			ALTER TABLE tblSMUserSecurityMenuFavorite DROP CONSTRAINT FK_tblSMUserSecurityMenuFavorite_tblSMUserSecurityMenu;
+		END
 
 		PRINT N'UPDATE intUserSecurityMenuId with intUserRoleMenuId data AND INSERT the intUserSecurityId'
 
