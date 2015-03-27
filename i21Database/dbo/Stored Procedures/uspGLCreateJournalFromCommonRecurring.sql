2e6868aa7f7f36eb072235049e72a207b4150b30
jeffrey trajano <jeffrey.trajano@irely.com>
2015-03-27 09:17:21 +0800

    GL-1416 Create Journal enteries from the Common Recurring screen

--- i21Database/dbo/Stored Procedures/uspGLCreateJournalFromCommonRecurring.sql
+++ i21Database/dbo/Stored Procedures/uspGLCreateJournalFromCommonRecurring.sql
@@ -4,51 +4,35 @@
 -- Description:	Creates journal from Common Info 
 -- =============================================
 CREATE PROCEDURE [dbo].[uspGLCreateJournalFromCommonRecurring] 
-	@delimitedIds  varchar (max), 
-	@delimiter nvarchar(1),
-	@entityid  int
+	@journalId  INT, 
+	@journalDate DATETIME,
+	@userId  INT
 AS
 BEGIN
-	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
-	DECLARE @journalIDS varchar(max)
-	DECLARE	@IntegerTable TABLE (intID int ,intValue INT)
-	DECLARE @id INT, @intNumber INT, @strPrefix VARCHAR(10),@smID varchar(10),@intJournalID INT
-	DECLARE @dateNow DATE = CONVERT(DATE, GETDATE(),101)
-	
-	INSERT INTO @IntegerTable SELECT * FROM fnCreateTableFromDelimitedValues(@delimitedIds,@delimiter)	
+	DECLARE @entityid INT ,@intNumber INT, @strPrefix VARCHAR(10),@smID varchar(10),@intJournalID INT
+	SELECT  @journalDate  = CONVERT(DATE ,@journalDate ,101)
 	BEGIN TRY
 		BEGIN TRANSACTION
-		DECLARE cursor_id CURSOR FOR SELECT intValue FROM @IntegerTable
-		OPEN cursor_id
-		FETCH NEXT FROM cursor_id INTO @id
-		WHILE @@FETCH_STATUS = 0
-		BEGIN
+			SELECT TOP 1 @entityid =intEntityId FROM tblSMUserSecurity WHERE intUserSecurityID = @userId
 			SELECT  @intNumber = intNumber, @strPrefix = strPrefix FROM tblSMStartingNumber WHERE intStartingNumberId = 4
 			SET @smID = @strPrefix + CONVERT(VARCHAR(5),@intNumber)
 			UPDATE tblSMStartingNumber SET intNumber = @intNumber + 1 WHERE intStartingNumberId = 4
-					
 			INSERT INTO tblGLJournal(strDescription,intCurrencyId,dtmDate,dtmReverseDate,strJournalId,strJournalType,strTransactionType,strSourceType,intEntityId,ysnPosted,strRecurringStatus)
-			SELECT strDescription,intCurrencyId,@dateNow,dtmReverseDate,@smID,'Common Recurring Journal','General Journal','GJ',@entityid,0,'Locked' FROM tblGLJournal
-			WHERE intJournalId = @id
+			SELECT strDescription,intCurrencyId,@journalDate,dtmReverseDate,@smID,'Common Recurring Journal','General Journal','GJ',@entityid,0,'Locked' FROM tblGLJournal
+			WHERE intJournalId = @journalId
 			SELECT  @intJournalID = SCOPE_IDENTITY() 
-			SET @journalIDS = CONVERT(VARCHAR(5), @intJournalID) + @delimiter
-			
 			INSERT INTO tblGLJournalDetail (intJournalId,intAccountId,intLineNo,dblCredit,dblCreditUnit,dblCreditRate,dblDebit,dblDebitUnit, dblDebitRate,strDescription,strReference,strComments,strDocument,dtmDate)
-			SELECT @intJournalID,intAccountId,intLineNo,dblCredit,dblCreditUnit,dblCreditRate,dblDebit,dblDebitUnit,dblDebitRate,strDescription,strReference,strComments,strDocument,@dateNow
+			SELECT @intJournalID,intAccountId,intLineNo,dblCredit,dblCreditUnit,dblCreditRate,dblDebit,dblDebitUnit,dblDebitRate,strDescription,strReference,strComments,strDocument,@journalDate
 			FROM tblGLJournalDetail
-			WHERE intJournalId = @id
-			FETCH NEXT FROM cursor_id INTO @id
-		END
+			WHERE intJournalId = @journalId
 		CLOSE cursor_id
 		DEALLOCATE cursor_id
 		COMMIT TRANSACTION
-		
-		SELECT SUBSTRING(@journalIDS,0, LEN(@journalIDS))
+		RETURN @smID
 	END TRY
 	BEGIN CATCH
 		IF @@TRANCOUNT > 0 ROLLBACK TRANSACTION
-		SELECT ''
+		RETURN ''
 	END CATCH
 END
