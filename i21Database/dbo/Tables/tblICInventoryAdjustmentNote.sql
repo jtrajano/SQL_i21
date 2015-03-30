e4d81e3cc9a5110d27c9b243c0b2b0f47a1a2dcf
Lawrence Zabala <lawrence.zabala@irely.com>
2015-03-30 10:19:54 +0800

    IC-96

---
+++ i21Database/dbo/Tables/tblICInventoryAdjustmentNote.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tblICInventoryAdjustmentNote]
+(
+	[intInventoryAdjustmentNoteId] INT NOT NULL IDENTITY, 
+    [intInventoryAdjustmentId] INT NOT NULL, 
+    [strDescription] NVARCHAR(100) NULL, 
+    [strNotes] NVARCHAR(MAX) NULL, 
+    [intSort] INT NULL, 
+    [intConcurrencyId] INT NULL DEFAULT ((0)), 
+    CONSTRAINT [PK_tblICInventoryAdjustmentNote] PRIMARY KEY ([intInventoryAdjustmentNoteId]), 
+    CONSTRAINT [FK_tblICInventoryAdjustmentNote_tblICInventoryAdjustment] FOREIGN KEY ([intInventoryAdjustmentId]) REFERENCES [tblICInventoryAdjustment]([intInventoryAdjustmentId]) ON DELETE CASCADE
+)
