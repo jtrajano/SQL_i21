0ba474ddda8758065f0fa24505f1f5733ce2f47b
unknown <rshanmuganathan@BLRRAMW8PRON.irely.net>
2015-03-30 13:56:45 +0530

    RK-12 Future month and option month table script

--- i21Database/dbo/Tables/tblRKOptionsMonth.sql
+++ i21Database/dbo/Tables/tblRKOptionsMonth.sql
@@ -2,9 +2,13 @@
 (
 	[intOptionMonthId] INT IDENTITY(1,1) NOT NULL, 
 	[intConcurrencyId] INT NOT NULL,	
-	[strOptionMonth] nvarchar(20)  COLLATE Latin1_General_CI_AS NOT NULL,
-   	[intFutureMarketId] INT NOT NULL, 
+	[intFutureMarketId] INT NOT NULL,
+	[strOptionMonth] NVARCHAR(20)  COLLATE Latin1_General_CI_AS NOT NULL ,  
+    [intFutureMonthId] INT NOT NULL, 
+    [strOptionMonthId] NVARCHAR(20)  COLLATE Latin1_General_CI_AS NOT NULL, 
+    [ysnMonthExpired] BIT NULL, 
+    [Expiration Date] DATETIME NULL, 
     CONSTRAINT [PK_tblRKOptionsMonth_intOptionMonthId] PRIMARY KEY ([intOptionMonthId]), 
-    CONSTRAINT [FK_tblRKOptionsMonth_tblRKFutureMarket_intFutureMarketId] FOREIGN KEY ([intFutureMarketId]) REFERENCES [tblRKFutureMarket]([intFutureMarketId])
-
+    CONSTRAINT [FK_tblRKOptionsMonth_tblRKFutureMarket_intFutureMarketId] FOREIGN KEY ([intFutureMarketId]) REFERENCES [tblRKFutureMarket]([intFutureMarketId]), 
+    CONSTRAINT [FK_tblRKOptionsMonth_tblRKFuturesMonth_intFutureMonthId] FOREIGN KEY ([intFutureMonthId]) REFERENCES [tblRKFuturesMonth]([intFutureMonthId])
 )
