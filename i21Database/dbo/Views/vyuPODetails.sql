fffcd032dd497bc69147ef0654d97886927a06c9
Erick F. Dela Cruz <erick.delacruz@irely.com>
2015-03-30 14:53:41 +0800

    IC-642

--- i21Database/dbo/Views/vyuPODetails.sql
+++ i21Database/dbo/Views/vyuPODetails.sql
@@ -28,14 +28,14 @@ SELECT
 	,C.intVendorId
 	,D.strItemNo
 	,D.strLotTracking
-	,E.strUnitMeasure AS strUOM
+	,H.strUnitMeasure AS strUOM
 	,F.strSubLocationName
 	,G.strName AS strStorageName
 FROM tblPOPurchase A
 	INNER JOIN  tblPOPurchaseDetail B ON A.intPurchaseId = B.intPurchaseId
 	INNER JOIN tblAPVendor C ON A.intVendorId = C.intVendorId
 	LEFT JOIN tblICItem D ON B.intItemId = D.intItemId
-	LEFT JOIN tblICUnitMeasure E ON B.intUnitOfMeasureId = E.intUnitMeasureId
+	LEFT JOIN tblICItemUOM E ON B.intUnitOfMeasureId = E.intItemUOMId
+	LEFT JOIN tblICUnitMeasure H ON E.intUnitMeasureId = H.intUnitMeasureId
 	LEFT JOIN tblSMCompanyLocationSubLocation F ON B.intSubLocationId = F.intCompanyLocationSubLocationId
 	LEFT JOIN tblICStorageLocation G ON B.intStorageLocationId = G.intStorageLocationId
-	
