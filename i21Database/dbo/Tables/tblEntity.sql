e2adc989e64cb84880623c8c19f7d779e890d048
Mon Gonzales <mon.gonzales@irely.com>
2015-03-31 09:33:25 +0800

    EM-68

--- i21Database/dbo/Tables/tblEntity.sql
+++ i21Database/dbo/Tables/tblEntity.sql
@@ -22,8 +22,10 @@
     [strNotes]         NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NULL,
     [strContactMethod] NVARCHAR (20)  COLLATE Latin1_General_CI_AS NULL,
     [strTimezone]      NVARCHAR (100) COLLATE Latin1_General_CI_AS NULL,
+	[intDefaultLocationId]       INT            NULL,
 	[ysnActive]        BIT             CONSTRAINT [DF_tblEntity_ysnActive] DEFAULT ((1)) NOT NULL,    	
     [intConcurrencyId] INT             CONSTRAINT [DF__tmp_ms_xx__intCo__5132705A] DEFAULT ((0)) NOT NULL,
+	CONSTRAINT [FK_tblEntity_tblEntityLocation] FOREIGN KEY ([intDefaultLocationId]) REFERENCES [tblEntityLocation]([intEntityLocationId]),
     CONSTRAINT [PK_dbo.tblEntity] PRIMARY KEY CLUSTERED ([intEntityId] ASC)
 );
 
