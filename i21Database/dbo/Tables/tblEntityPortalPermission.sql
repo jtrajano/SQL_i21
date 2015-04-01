12d669562445dafe89683d9f45f2edde02cff511
Mon Gonzales <mon.gonzales@irely.com>
2015-04-01 11:12:41 +0800

    EM-68 Portal Permission EM-8

---
+++ i21Database/dbo/Tables/tblEntityPortalPermission.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[tblEntityPortalPermission] (
+    [intEntityPortalPermissionId]	INT IDENTITY (1, 1) NOT NULL,
+    [intEntityToContactId]			INT NOT NULL,
+    [intEntityPortalMenuId]			INT NOT NULL,
+    [intConcurrencyId]              INT CONSTRAINT [DF_tblEntityPortalPermission_intConcurrencyId] DEFAULT ((0)) NOT NULL,
+    CONSTRAINT [PK_tblEntityPortalPermission] PRIMARY KEY CLUSTERED ([intEntityPortalPermissionId] ASC),
+    CONSTRAINT [FK_tblEntityPortalPermission_tblEntityPortalMenu] FOREIGN KEY ([intEntityPortalMenuId]) REFERENCES [dbo].[tblEntityPortalMenu]([intEntityPortalMenuId]),
+    CONSTRAINT [FK_tblEntityPortalPermission_tblEntityToContact] FOREIGN KEY ([intEntityToContactId]) REFERENCES [dbo].[tblEntityToContact]([intEntityToContactId]) ON DELETE CASCADE
+);
+
