b4edbae723ca64b482f5c846a7051cc6283052ad
unknown <gnayak@BLRGOPINWIN7.irely.net>
2015-04-01 14:58:27 +0530

    GRN - 74

---
+++ i21Database/dbo/Tables/tblRKMarketExchange.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[tblRKMarketExchange]
+(
+	[intMarketExchangeId] INT NOT NULL IDENTITY, 
+    [intConcurrencyId] INT NOT NULL, 
+    [strMarketSymbol] NVARCHAR(20) NOT NULL, 
+    [strExchangeCode] NVARCHAR(5) NOT NULL, 
+    CONSTRAINT [PK_tblRKMarketExchange_intMarketExchangeId] PRIMARY KEY ([intMarketExchangeId]) 
+)
