646c17f79167553fe877efc72c6018651471a487
Arman Arenas <arman.arenas@irely.com>
2015-04-02 01:30:11 +0800

    NR-87 comment out SET ANSI_WARNINGS OFF

--- i21Database/dbo/Stored Procedures/uspCMBookGLEntries.sql
+++ i21Database/dbo/Stored Procedures/uspCMBookGLEntries.sql
@@ -10,7 +10,7 @@ SET QUOTED_IDENTIFIER OFF
 SET ANSI_NULLS ON
 SET NOCOUNT ON
 SET XACT_ABORT ON
-SET ANSI_WARNINGS OFF
+--SET ANSI_WARNINGS OFF
 
 --=====================================================================================================================================
 -- 	DECLARATION 
