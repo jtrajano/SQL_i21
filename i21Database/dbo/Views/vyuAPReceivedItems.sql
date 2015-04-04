42fb895ea7ad1bb6ed1738c3314a729581a44b75
Erick F. Dela Cruz <erick.delacruz@irely.com>
2015-04-04 15:32:36 +0800

    AP-1045

--- i21Database/dbo/Views/vyuAPReceivedItems.sql
+++ i21Database/dbo/Views/vyuAPReceivedItems.sql
@@ -36,8 +36,7 @@ FROM tblPOPurchase A
 			INNER JOIN tblICItemLocation loc ON B.intItemId = loc.intItemId AND A.intLocationId = loc.intLocationId
 		WHERE A.ysnPosted = 1 AND B.dblOpenReceive != B.dblBillQty
 		GROUP BY
-			B.intInventoryReceiptItemId
-			,B.intItemId 
+			B.intItemId 
 			,B.dblUnitCost
 			,intLineNo
 			,dblOrderQty
@@ -48,3 +47,33 @@ FROM tblPOPurchase A
 	INNER JOIN  (tblAPVendor D1 INNER JOIN tblEntity D2 ON D1.intEntityId = D2.intEntityId) ON A.intVendorId = D1.intVendorId
 	LEFT JOIN tblSMShipVia E ON A.intShipViaId = E.intShipViaID
 	LEFT JOIN tblSMTerm F ON A.intTermsId = F.intTermID
+UNION ALL
+--Miscellaneous items
+SELECT
+A.intVendorId
+,A.dtmDate
+,A.strReference
+,A.strPurchaseOrderNumber
+,B.intPurchaseDetailId
+,B.intItemId
+,C.strItemNo
+,C.strDescription
+,B.dblQtyOrdered
+,B.dblQtyOrdered
+,B.intPurchaseDetailId
+,B.dblCost
+,intAccountId = [dbo].[fnGetItemGLAccount](B.intItemId, loc.intItemLocationId, 'Inventory')
+,strAccountId = (SELECT strAccountId FROM tblGLAccount WHERE intAccountId = dbo.fnGetItemGLAccount(B.intItemId, loc.intItemLocationId, 'Inventory'))
+,D2.strName
+,D1.strVendorId
+,E.strShipVia
+,F.strTerm
+FROM tblPOPurchase A
+	INNER JOIN tblPOPurchaseDetail B ON A.intPurchaseId = B.intPurchaseId
+	INNER JOIN tblICItem C ON B.intItemId = C.intItemId
+	INNER JOIN tblICItemLocation loc ON C.intItemId = loc.intItemId AND loc.intLocationId = A.intShipToId
+	INNER JOIN  (tblAPVendor D1 INNER JOIN tblEntity D2 ON D1.intEntityId = D2.intEntityId) ON A.intVendorId = D1.intVendorId
+	LEFT JOIN tblSMShipVia E ON A.intShipViaId = E.intShipViaID
+	LEFT JOIN tblSMTerm F ON A.intTermsId = F.intTermID
+WHERE C.strType IN ('Service','Software','Non-Inventory','Other Charge')
+AND B.dblQtyOrdered != B.dblQtyReceived
