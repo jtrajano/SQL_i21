c9570543f215ebe5cd35c4d7b51072a39a151899
unknown <LDorado@Liz.COMTECH.COM>
2015-04-06 11:49:20 +0800

    Grn - 74 Reversed commit and merged the i21db.sqlpropject. With merge conflict on 21db.sqlpropject

---
+++ i21Database/dbo/Tables/tblRKElectonicPricing.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[tblRKElectonicPricing]
+(
+	[intElectronicPricingId] INT NOT NULL IDENTITY, 
+    [intConcurrencyId] INT NOT NULL, 
+    [intFutureMarketId] INT NOT NULL, 
+    [intNumberofForwardPeriods] INT NOT NULL DEFAULT 0, 
+    [intReturnCurrency] INT NULL, 
+    [intConvertCurrency] INT NULL, 
+    [strSymbolPrefix] NVARCHAR(5) NOT NULL, 
+    [strMarketSymbol] NVARCHAR(20) NOT NULL, 
+    CONSTRAINT [PK_tblRKElectonicPricing_intElectronicPricingId] PRIMARY KEY ([intElectronicPricingId]), 
+    CONSTRAINT [FK_tblRKElectonicPricing_tblRKFutureMarket_intFutureMarketId] FOREIGN KEY (intFutureMarketId) REFERENCES [tblRKFutureMarket]([intFutureMarketId]) 
+)
--- i21Database/dbo/Tables/tblRKMarketExchange.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[tblRKMarketExchange]
-(
-	[intMarketExchangeId] INT NOT NULL IDENTITY, 
-    [intConcurrencyId] INT NOT NULL, 
-    [strMarketSymbol] NVARCHAR(20) NOT NULL, 
-    [strExchangeCode] NVARCHAR(5) NOT NULL, 
-    CONSTRAINT [PK_tblRKMarketExchange_intMarketExchangeId] PRIMARY KEY ([intMarketExchangeId]) 
-)
