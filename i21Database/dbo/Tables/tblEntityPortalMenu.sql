524b2fc90937e426757347bf90c5719eefeb7a71
Mon Gonzales <mon.gonzales@irely.com>
2015-04-06 10:05:32 +0800

    EM-68 EM-8

--- i21Database/dbo/Tables/tblEntityPortalMenu.sql
+++ i21Database/dbo/Tables/tblEntityPortalMenu.sql
@@ -4,6 +4,7 @@
     [strPortalMenuName]     NVARCHAR (50) COLLATE Latin1_General_CI_AS NULL,
     [intPortalParentMenuId] INT           NOT NULL,
     [strType]                       NVARCHAR (10) COLLATE Latin1_General_CI_AS NULL,
+	[strEntityType]                       NVARCHAR (50) COLLATE Latin1_General_CI_AS NULL,
     [strCommand] NVARCHAR(250) COLLATE Latin1_General_CI_AS NULL, 
     CONSTRAINT [PK_tblEntityPortalMenu] PRIMARY KEY CLUSTERED ([intEntityPortalMenuId] ASC)
 );
---
+++ i21Database/dbo/Views/vyuEPContactMenu.sql
@@ -0,0 +1,36 @@
+﻿CREATE VIEW vyuEPContactMenu
+AS
+SELECT    
+intUserSecurityMenuId = Permission.intEntityPortalPermissionId,
+intUserSecurityId = Contact.[intEntityId],    
+intMenuId = Menu.intEntityPortalMenuId,    
+intParentMenuId = (CASE WHEN Menu.intPortalParentMenuId = 0 THEN 0 ELSE (  
+     SELECT intEntityPortalPermissionId  
+     FROM tblEntityPortalPermission  
+     LEFT JOIN tblEntityToContact  ON tblEntityPortalPermission.intEntityToContactId = tblEntityToContact.intEntityToContactId       
+     LEFT JOIN tblEntity  ON tblEntityToContact.[intEntityContactId] = tblEntity.[intEntityId]       
+     WHERE tblEntity.[intEntityId] = Contact.[intEntityId]  
+  
+       
+     AND tblEntityPortalPermission.intEntityPortalMenuId = Menu.intPortalParentMenuId  
+     ) END),  
+strMenuName = Menu.strPortalMenuName,    
+strModuleName = 'Customer Portal',    
+strIcon = (CASE WHEN Menu.strType = 'Folder' THEN 'small-folder' ELSE 'small-screen' END),    
+strDescription = Menu.strPortalMenuName,    
+strType = Menu.strType,    
+strCommand = Menu.strCommand,    
+iconCls = Menu.strPortalMenuName,    
+ysnLeaf = (CASE WHEN Menu.strType = 'Folder' THEN CAST(0 AS BIT) ELSE CAST(1 AS BIT) END),    
+leaf = (CASE WHEN Menu.strType = 'Folder' THEN CAST(0 AS BIT) ELSE CAST(1 AS BIT) END),    
+expanded = CAST(0 AS BIT),    
+ysnVisible = (CASE WHEN ISNULL(Permission.intEntityPortalMenuId, 0) = 0 THEN CAST(0 AS BIT) ELSE CAST(1 AS BIT) END),    
+ysnIsLegacy = CAST(0 AS BIT),    
+intSort = Menu.intEntityPortalMenuId    
+FROM tblEntityPortalMenu Menu    
+	LEFT JOIN tblEntityPortalPermission Permission 
+		ON Menu.intEntityPortalMenuId = Permission.intEntityPortalMenuId  
+	LEFT JOIN tblEntityToContact EntityToContact 
+		ON Permission.intEntityToContactId = EntityToContact.intEntityToContactId  
+	LEFT JOIN tblEntity Contact 
+		ON EntityToContact.[intEntityContactId] = Contact.[intEntityId]
