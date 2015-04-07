ea0e1d04ed738764371d6fec6b96553b9b46bbd4
von.pasion <von.pasion@irely.com>
2015-04-08 00:05:01 +0800

    FRM-2010

---
+++ i21Database/dbo/Stored Procedures/uspSMUpdateStageListing.sql
@@ -0,0 +1,134 @@
+﻿CREATE PROCEDURE [dbo].[uspSMUpdateStageListing]  
+AS  
+  
+SET QUOTED_IDENTIFIER OFF  
+SET ANSI_NULLS ON  
+SET NOCOUNT ON  
+SET XACT_ABORT ON  
+SET ANSI_WARNINGS OFF  
+  
+BEGIN TRANSACTION  
+	-- Set change to Added to all new screen(s)
+	UPDATE A 
+	SET A.strChange = 'Added'
+	FROM tblSMScreenStage A
+		LEFT OUTER JOIN tblSMScreen B ON A.strNamespace = B.strNamespace
+	WHERE ISNULL(B.strScreenName, '') = '' 
+
+	-- Add entry to deleted screen(s)
+	INSERT INTO tblSMScreenStage (
+		strScreenId, 
+		strScreenName, 
+		strNamespace, 
+		strModule, 
+		strChange, 
+		intConcurrencyId
+	)
+	SELECT A.strScreenId, 
+		A.strScreenName, 
+		A.strNamespace, 
+		A.strModule, 
+		'Deleted', 
+		0 
+	FROM tblSMScreen A 
+		LEFT OUTER JOIN tblSMScreenStage B ON A.strNamespace = B.strNamespace
+	WHERE ISNULL(B.strScreenName, '') = '' 
+
+	-- Set change to Added to all new control(s)
+	UPDATE A
+	SET A.strChange = 'Added'
+	FROM (
+		SELECT tblSMControlStage.strControlId, 
+			tblSMControlStage.strControlName, 
+			tblSMControlStage.strControlType, 
+			tblSMScreenStage.strNamespace,
+			tblSMControlStage.strChange  
+		FROM tblSMControlStage 
+		INNER JOIN tblSMScreenStage 
+			ON tblSMControlStage.intScreenStageId = tblSMScreenStage.intScreenStageId
+		WHERE tblSMControlStage.intScreenStageId IN (SELECT intScreenStageId FROM tblSMScreenStage WHERE ISNULL(strChange, '') = '')
+	) A
+	LEFT OUTER JOIN (
+		SELECT tblSMControl.strControlId, 
+			tblSMControl.strControlName, 
+			tblSMControl.strControlType, 
+			tblSMScreen.strNamespace 
+		FROM tblSMControl 
+		INNER JOIN tblSMScreen
+			ON tblSMControl.intScreenId = tblSMScreen.intScreenId
+		WHERE tblSMControl.intScreenId 
+		IN (SELECT intScreenId FROM tblSMScreen 
+			WHERE strNamespace
+			IN (SELECT strNamespace FROM tblSMScreenStage 
+				WHERE ISNULL(strChange, '') = '')
+		)
+	) B
+	ON A.strControlId = B.strControlId AND A.strNamespace = B.strNamespace
+	WHERE ISNULL(B.strControlName, '') = '' 
+
+	-- Add entry to deleted control(s)
+	INSERT INTO tblSMControlStage (
+		strControlId, 
+		strControlName, 
+		strControlType, 
+		strContainer, 
+		intScreenStageId, 
+		strChange, 
+		intConcurrencyId)
+	SELECT 
+		A.strControlId, 
+		A.strControlName, 
+		A.strControlType, 
+		'' As strContainer, 
+		(SELECT TOP 1 intScreenStageId FROM tblSMScreenStage WHERE strNamespace = A.strNamespace) AS intScreenStageId,
+		'Deleted' AS strChange,
+		0
+	FROM (
+		SELECT tblSMControl.strControlId, 
+			tblSMControl.strControlName, 
+			tblSMControl.strControlType, 
+			tblSMScreen.strNamespace 
+		FROM tblSMControl 
+		INNER JOIN tblSMScreen 
+			ON tblSMControl.intScreenId = tblSMScreen.intScreenId
+		WHERE tblSMControl.intScreenId 
+		IN (SELECT intScreenId FROM tblSMScreen 
+			WHERE strNamespace
+			IN (SELECT strNamespace FROM tblSMScreenStage 
+				WHERE ISNULL(strChange, '') = '')
+		)
+	) A
+	LEFT OUTER JOIN (
+		SELECT tblSMControlStage.strControlId, 
+			tblSMControlStage.strControlName, 
+			tblSMControlStage.strControlType, 
+			tblSMScreenStage.strNamespace,
+			tblSMControlStage.strChange,
+			tblSMControlStage.intScreenStageId  
+		FROM tblSMControlStage 
+		INNER JOIN tblSMScreenStage 
+			ON tblSMControlStage.intScreenStageId = tblSMScreenStage.intScreenStageId
+		WHERE tblSMControlStage.intScreenStageId IN (SELECT intScreenStageId FROM tblSMScreenStage WHERE ISNULL(strChange, '') = '')
+	) B
+	ON A.strControlId = B.strControlId AND A.strNamespace = B.strNamespace
+	WHERE ISNULL(B.strControlName, '') = '' 
+
+	-- Delete control(s) staging that doesn't have conflicts
+	DELETE FROM tblSMControlStage WHERE intControlStageId IN (
+		SELECT tblSMControlStage.intControlStageId 
+		FROM tblSMControlStage 
+		INNER JOIN tblSMScreenStage 
+			ON tblSMControlStage.intScreenStageId = tblSMScreenStage.intScreenStageId
+		WHERE ISNULL(tblSMScreenStage.strChange, '') = '' AND ISNULL(tblSMControlStage.strChange, '') = ''
+	)
+
+	-- Delete screen(s) staging that doesn't have conflicts
+	DELETE FROM tblSMScreenStage WHERE intScreenStageId IN (
+		SELECT tblSMControlStage.intControlStageId 
+		FROM tblSMControlStage 
+		INNER JOIN tblSMScreenStage 
+			ON tblSMControlStage.intScreenStageId = tblSMScreenStage.intScreenStageId
+		WHERE ISNULL(tblSMControlStage.strChange, '') <> ''
+	) AND ISNULL(strChange, '') = ''
+ 
+COMMIT TRANSACTION
