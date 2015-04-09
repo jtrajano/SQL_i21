dd72efbd396ec6a933af576f98c1f68dc1915a63
kenneth lagunero <kenneth.lagunero@irely.com>
2015-04-09 16:09:44 +0800

    TM-1271

--- Integration/Scripts/PreDeployment.sql
+++ Integration/Scripts/PreDeployment.sql
@@ -111,6 +111,7 @@
 :r "..\dbo\Views\vyuTMOriginDegreeOption.sql"
 :r "..\Scripts\TM\Customer.sql"
 :r "..\dbo\Views\vyuTMConsumptionSiteSearch.sql"
+:r "..\dbo\Views\vyuTMSiteOrder.sql"
 
 :r "..\dbo\Functions\fnGetVendorLastName.sql"
 :r "..\dbo\Stored Procedures\uspAPImportBillTransactions.sql"
