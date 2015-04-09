0ec2826eb29700a4de1d935874886cf1b8d7cbbf
Feb Montefrio <feb.montefrio@irely.com>
2015-04-09 16:44:18 +0800

    Fix the issue on receiving multiple stocks on the same lot number.

--- i21Database/dbo/Functions/fnCalculateWeightUnitQty.sql
+++ i21Database/dbo/Functions/fnCalculateWeightUnitQty.sql
@@ -2,16 +2,15 @@
 -- This function returns the weight per uom 
 CREATE FUNCTION [dbo].[fnCalculateWeightUnitQty] (
 	@dblQty AS NUMERIC(18,6)
-	,@dblTotalWeight AS NUMERIC(18,6)
+	,@dblTotalWeight AS NUMERIC(38,20)
 )
 RETURNS NUMERIC(38,20)
 AS
 BEGIN 
-	DECLARE @result AS NUMERIC(38,20)
-
 	-- formula is Total Weight / Qty
 	-- If qty is zero, return zero. 
-	SET @result = CASE WHEN ISNULL(@dblQty, 0) = 0 THEN 0 ELSE ISNULL(@dblTotalWeight, 0) / @dblQty END 
-
-	RETURN ISNULL(@result, 0);
+	RETURN CASE		WHEN ISNULL(@dblQty, 0) = 0 THEN 
+						0 
+					ELSE CAST(ISNULL(@dblTotalWeight, 0) AS FLOAT) / CAST(@dblQty AS FLOAT)
+			END 
 END
