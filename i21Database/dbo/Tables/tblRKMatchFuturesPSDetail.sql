d8d7c54f5aa89a60a4aec17d379251164312962c
Ramachandran <rshanmuganathan@BLRRAMW8PRON.irely.net>
2015-04-10 17:45:24 +0530

    RM -14 P&S changes

---
+++ i21Database/dbo/Tables/tblRKMatchFuturesPSDetail.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[tblRKMatchFuturesPSDetail]
+(
+	[intMatchFuturesPSDetailId]  INT IDENTITY(1,1) NOT NULL,
+	[intConcurrencyId] INT NOT NULL, 
+	[intMatchFuturesPSHeaderId] INT NOT NULL, 
+	[dblMatchQty] NUMERIC(18, 6) NOT NULL,
+	[intLFutOptTransactionId] INT NOT NULL,
+	[intSFutOptTransactionId] INT NOT NULL		 
+	 CONSTRAINT [PK_tblRKMatchFuturesPSDetail_intMatchFuturesPSDetailId] PRIMARY KEY (intMatchFuturesPSDetailId), 
+	 CONSTRAINT [FK_tblRKMatchFuturesPSDetail_tblRKFutOptTransaction_intLFutOptTransactionId] FOREIGN KEY ([intLFutOptTransactionId]) REFERENCES [tblRKFutOptTransaction]([intFutOptTransactionId]),
+	 CONSTRAINT [FK_tblRKMatchFuturesPSDetail_tblRKFutOptTransaction_intSFutOptTransactionId] FOREIGN KEY ([intSFutOptTransactionId]) REFERENCES [tblRKFutOptTransaction]([intFutOptTransactionId]),
+)
---
+++ i21Database/dbo/Tables/tblRKMatchFuturesPSHeader.sql
@@ -0,0 +1,26 @@
+﻿CREATE TABLE [dbo].[tblRKMatchFuturesPSHeader]
+(
+	[intMatchFuturesPSHeaderId] INT IDENTITY(1,1) NOT NULL,
+	[intConcurrencyId] INT NOT NULL, 
+	[intMatchNo] INT NOT NULL, 
+    [dtmMatchDate] DATETIME NOT NULL, 
+	[intCompanyLocationId] INT NOT NULL, 
+	[intCommodityId] INT NOT NULL, 
+	[intFutureMarketId] INT NOT NULL, 
+	[intFutureMonthId] int NOT NULL, 
+	[intBrokerId] INT NOT NULL, 
+	[intBrokerageAccountId] INT NOT NULL, 
+	[intBookId] INT NULL, 
+    [intSubBookId] INT NULL
+    CONSTRAINT [PK_tblRKMatchFuturesPSHeader_intMatchFuturesPSHeaderId] PRIMARY KEY (intMatchFuturesPSHeaderId), 
+	CONSTRAINT [FK_tblRKMatchFuturesPSHeader_tblRKFuturesMonth_intFutureMonthId] FOREIGN KEY ([intFutureMonthId]) REFERENCES [tblRKFuturesMonth]([intFutureMonthId]),
+	CONSTRAINT [FK_tblRKMatchFuturesPSHeader_tblCTBook_intBookId] FOREIGN KEY ([intBookId]) REFERENCES [tblCTBook]([intBookId]),
+	CONSTRAINT [FK_tblRKMatchFuturesPSHeader_tblCTSubBook_intSubBookId] FOREIGN KEY ([intSubBookId]) REFERENCES [tblCTSubBook]([intSubBookId]),
+	CONSTRAINT [FK_tblRKMatchFuturesPSHeader_tblICCommodity_intCommodityId] FOREIGN KEY ([intCommodityId]) REFERENCES [tblICCommodity]([intCommodityId]),
+	CONSTRAINT [FK_tblRKMatchFuturesPSHeader_tblRKFutureMarket_intFutureMarketId] FOREIGN KEY ([intFutureMarketId]) REFERENCES [tblRKFutureMarket]([intFutureMarketId]),
+	CONSTRAINT [FK_tblRKMatchFuturesPSHeader_tblRKBrokerageAccount_intBrokerageAccountId] FOREIGN KEY ([intBrokerageAccountId]) REFERENCES [tblRKBrokerageAccount]([intBrokerageAccountId]),
+	CONSTRAINT [FK_tblRKMatchFuturesPSHeader_tblSMCompanyLocation_intCompanyLocationId] FOREIGN KEY ([intCompanyLocationId]) REFERENCES [tblSMCompanyLocation]([intCompanyLocationId]),
+
+    CONSTRAINT [UK_tblRKMatchFuturesPSHeader_intMatchNo] UNIQUE (intMatchNo)
+)
+
