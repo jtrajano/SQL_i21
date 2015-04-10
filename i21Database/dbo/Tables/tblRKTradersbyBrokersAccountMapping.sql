ef914ae0ad7d3dc08039953c978ff511b90d6cb9
Ramachandran <rshanmuganathan@BLRRAMW8PRON.irely.net>
2015-04-11 02:37:48 +0530

    RM-16 Fut Opt Transaction changes

--- i21Database/dbo/Tables/tblRKTradersbyBrokersAccountMapping.sql
+++ i21Database/dbo/Tables/tblRKTradersbyBrokersAccountMapping.sql
@@ -3,8 +3,8 @@
 	[intTradersbyBrokersAccountId] INT IDENTITY(1,1) NOT NULL,   
     [intConcurrencyId] INT NOT NULL, 
     [intBrokerageAccountId] INT NOT NULL, 
-    [intSalespersonId] INT NOT NULL, 
+    [intEntitySalespersonId] INT NOT NULL, 
     CONSTRAINT [PK_tblRKTradersbyBrokersAccountMapping_intTradersbyBrokersAccountId] PRIMARY KEY ([intTradersbyBrokersAccountId]), 
     CONSTRAINT [FK_tblRKTradersbyBrokersAccountMapping_tblRKBrokerageAccount_intBrokerageAccountId] FOREIGN KEY ([intBrokerageAccountId]) REFERENCES [tblRKBrokerageAccount]([intBrokerageAccountId]) ON DELETE CASCADE, 
-    CONSTRAINT [FK_tblRKTradersbyBrokersAccountMapping_tblARSalesperson_intSalesersonId] FOREIGN KEY ([intSalespersonId]) REFERENCES [tblARSalesperson]([intEntitySalespersonId])
+    CONSTRAINT [FK_tblRKTradersbyBrokersAccountMapping_tblARSalesperson_intSalesersonId] FOREIGN KEY ([intEntitySalespersonId]) REFERENCES [tblARSalesperson]([intEntitySalespersonId])
 )
