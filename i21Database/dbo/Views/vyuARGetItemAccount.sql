86c943914aad22fc7016bb26d58f962cc7033c44
Ruben  Escarcha <ruben.escarcha@irely.com>
2015-04-16 17:57:26 +0800

    AR-997 HDTN-163297 - AR Invoice - i21 - unable to post - accounts not picked from category maintenance

--- i21Database/dbo/Views/vyuARGetItemAccount.sql
+++ i21Database/dbo/Views/vyuARGetItemAccount.sql
@@ -4,10 +4,10 @@ SELECT
 Item.intItemId, 
 Item.strItemNo,
 Item.strType,
-dbo.fnGetItemGLAccount(Item.intItemId, ItemLocation.intLocationId, N'Service Charges') AS intAccountId, 
-dbo.fnGetItemGLAccount(Item.intItemId, ItemLocation.intLocationId, N'COGS') AS intCOGSAccountId, 
-dbo.fnGetItemGLAccount(Item.intItemId, ItemLocation.intLocationId, N'Sales') AS intSalesAccountId, 
-dbo.fnGetItemGLAccount(Item.intItemId, ItemLocation.intLocationId, N'Inventory') AS intInventoryAccountId
+dbo.fnGetItemBaseGLAccount(Item.intItemId, ItemLocation.intLocationId, N'Service Charges') AS intAccountId, 
+dbo.fnGetItemBaseGLAccount(Item.intItemId, ItemLocation.intLocationId, N'Cost of Goods') AS intCOGSAccountId, 
+dbo.fnGetItemBaseGLAccount(Item.intItemId, ItemLocation.intLocationId, N'Sales Account') AS intSalesAccountId, 
+dbo.fnGetItemBaseGLAccount(Item.intItemId, ItemLocation.intLocationId, N'Inventory') AS intInventoryAccountId
 FROM         
 dbo.tblICItem AS Item LEFT OUTER JOIN
 dbo.tblICItemLocation AS ItemLocation ON ItemLocation.intItemId = Item.intItemId
