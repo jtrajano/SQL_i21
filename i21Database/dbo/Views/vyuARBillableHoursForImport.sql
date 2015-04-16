05925cf7b55467501d9626bca7be2774838ca5b6
Ruben  Escarcha <ruben.escarcha@irely.com>
2015-04-16 16:12:18 +0800

    AR-706 Import Billable Hours from Help Desk

--- i21Database/dbo/Views/vyuARBillableHoursForImport.sql
+++ i21Database/dbo/Views/vyuARBillableHoursForImport.sql
@@ -1,43 +1,45 @@
 ﻿CREATE VIEW [dbo].[vyuARBillableHoursForImport]
 	AS 
 SELECT
-	C.intEntityCustomerId
-	,C.strCustomerNumber
-	,E.strName
-	,T.intTicketId
-	,T.strTicketNumber
-	,HW.intTicketHoursWorkedId
-	,HW.intAgentEntityId
-	,U.strName					AS "strAgentName"
-	,GETDATE()					AS	"dtmBilled"
-	,HW.dtmDate
-	,JC.intJobCodeId
-	,JC.strJobCode
-	,JC.intCompanyLocationId
-	,JC.intItemId
-	,IC.strItemNo
-	,HW.intHours
-	,HW.dblRate					AS "dblPrice"
-	,HW.intHours * HW.dblRate	AS "dblTotal"
+	C.[intEntityCustomerId]
+	,C.[strCustomerNumber]
+	,E.[strName]
+	,T.[intTicketId]
+	,T.[strTicketNumber]
+	,HW.[intTicketHoursWorkedId]
+	,HW.[intAgentEntityId]
+	,U.[strName]					AS "strAgentName"
+	,GETDATE()						AS	"dtmBilled"
+	,HW.[dtmDate]
+	,HW.[intInvoiceId] 
+	,JC.[intJobCodeId]
+	,JC.[strJobCode]
+	,JC.[intCompanyLocationId]
+	,JC.[intItemId]
+	,IC.[strItemNo]
+	,HW.[intHours]
+	,HW.[dblRate]					AS "dblPrice"
+	,HW.[intHours] * HW.[dblRate]	AS "dblTotal"
 FROM
 	tblHDJobCode JC
 INNER JOIN 
 	tblICItem IC
-		ON JC.intItemId = IC.intItemId	
+		ON JC.[intItemId] = IC.[intItemId]	
 INNER JOIN
 	tblHDTicketHoursWorked HW
-		ON JC.intJobCodeId = HW.intJobCodeId
-		AND HW.ysnBillable = 1
-		AND HW.ysnBilled = 0
+		ON JC.[intJobCodeId] = HW.[intJobCodeId]
+		AND HW.[ysnBillable] = 1
+		AND HW.[ysnBilled] = 0
+		AND (HW.[intInvoiceId] IS NULL OR HW.[intInvoiceId] = 0)
 INNER JOIN
 	tblHDTicket T
-		ON HW.intTicketId = T.intTicketId
+		ON HW.[intTicketId] = T.[intTicketId]
 INNER JOIN
 	tblARCustomer C
-		ON T.intCustomerId = C.intEntityCustomerId
+		ON T.[intCustomerId] = C.[intEntityCustomerId]
 INNER JOIN
 	tblEntity E
-		ON C.intEntityCustomerId = E.intEntityId	
+		ON C.[intEntityCustomerId] = E.[intEntityId]	
 INNER JOIN
 	tblEntity U
-		ON HW.intAgentEntityId = U.intEntityId
+		ON HW.[intAgentEntityId] = U.[intEntityId]
