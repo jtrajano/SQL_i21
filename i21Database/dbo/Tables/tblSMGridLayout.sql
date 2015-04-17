f40537bdfd02686fd6024d804af80ce4f00425fb
Kelvin Quinto <kelvin.quinto@irely.com>
2015-04-17 16:08:15 +0800

    FRM-2022 Grid Layout

---
+++ i21Database/dbo/Tables/tblSMGridLayout.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[tblSMGridLayout]
+(
+	[intGridLayoutId] INT IDENTITY (1, 1) NOT NULL, 
+    [strGridLayoutName] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
+	[strGridLayoutFields] NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NULL, 
+    [strScreen] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
+    [strGrid] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
+    [intUserId] INT NULL, 
+    [ysnActive] BIT NULL, 
+    [intConcurrencyId] INT NOT NULL, 
+    CONSTRAINT [PK_tblSMGridLayout] PRIMARY KEY CLUSTERED ([intGridLayoutId] ASC) 
+)
