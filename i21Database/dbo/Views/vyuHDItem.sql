58c96e6bfa2d3f7f2018c734f969b580106f1293
Jayson Ramos <jayson.ramos@iRely.com>
2015-04-20 16:27:15 +0800

    Modify vyuHDItem - casting Id to int.

--- i21Database/dbo/Views/vyuHDItem.sql
+++ i21Database/dbo/Views/vyuHDItem.sql
@@ -1,7 +1,7 @@
 ﻿CREATE VIEW [dbo].[vyuHDItem]
 	AS
 		select
-			intId = ROW_NUMBER() over (order by item.intItemId),
+			intId = cast(ROW_NUMBER() over (order by item.intItemId) as int),
 			item.intItemId, item.strItemNo, item.strDescription, item.strType, item.strStatus,
 			comLoc.intCompanyLocationId, comLoc.strLocationName, comLoc.strLocationNumber, comLoc.strLocationType,
 			uom.intUnitMeasureId, uom.strUnitMeasure, uom.strSymbol, uom.strUnitType
