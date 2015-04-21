a7569142ec09ff1c8206c52f70215fce8d3d402a
von.pasion <von.pasion@irely.com>
2015-04-21 17:19:14 +0800

    FRM-2057

---
+++ i21Database/dbo/Tables/cstGLAccount.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[cstGLAccount] (
+    [intId] INT NOT NULL,
+    CONSTRAINT [PK_cstGLAccount] PRIMARY KEY CLUSTERED ([intId] ASC),
+    CONSTRAINT [FK_cstGLAccount.tblGLAccount_intAccountId] FOREIGN KEY ([intId]) REFERENCES [dbo].[tblGLAccount] ([intAccountId]) ON DELETE CASCADE
+);
