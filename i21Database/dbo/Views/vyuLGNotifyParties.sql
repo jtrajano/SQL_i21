6c19a429102b189c658954bf0520a689d8355b33
James Balachander <balachander.j@irely.com>
2015-04-21 10:08:17 +0530

    LG-5 View changes for Shipping Instruction

--- i21Database/dbo/Views/vyuLGNotifyParties.sql
+++ i21Database/dbo/Views/vyuLGNotifyParties.sql
@@ -9,17 +9,39 @@ ON		C.intEntityCustomerId = E.intEntityId
 
 UNION ALL
 
-SELECT 	S.intShippingLineId 	AS intEntityId,
+SELECT 	S.intEntityId 			AS intEntityId,
 		S.strName,
 		'Shipping Line' 		AS strEntity
-FROM 	tblLGShippingLine 			S
+FROM 	tblEntity 				S
+JOIN	tblEntityType			SE
+ON		S.intEntityId			= SE.intEntityId and SE.strType = 'Shipping Line'
 
 UNION ALL
 
-SELECT	F.intForwardingAgentId 	AS intEntityId,
+SELECT 	F.intEntityId 			AS intEntityId,
 		F.strName,
 		'Forwarding Agent' 		AS strEntity
-FROM 	tblLGForwardingAgent 	F
+FROM 	tblEntity 				F
+JOIN	tblEntityType			FE
+ON		F.intEntityId			= FE.intEntityId and FE.strType = 'Forwarding Agent'
+
+UNION ALL
+
+SELECT 	T.intEntityId 			AS intEntityId,
+		T.strName,
+		'Trucker' 				AS strEntity
+FROM 	tblEntity 				T
+JOIN	tblEntityType			TE
+ON		T.intEntityId			= TE.intEntityId and TE.strType = 'Trucker'
+
+UNION ALL
+
+SELECT 	TM.intEntityId 			AS intEntityId,
+		TM.strName,
+		'Terminal' 				AS strEntity
+FROM 	tblEntity 				TM
+JOIN	tblEntityType			TME
+ON		TM.intEntityId			= TME.intEntityId and TME.strType = 'Terminal'
 
 UNION ALL
 
