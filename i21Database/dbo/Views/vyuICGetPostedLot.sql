fa75e70c691e78db1aed0661ce26424fd024d7be
Feb Montefrio <feb.montefrio@irely.com>
2015-04-22 14:15:34 +0800

    Add a new view for posted lot numbers IC-699

---
+++ i21Database/dbo/Views/vyuICGetPostedLot.sql
@@ -0,0 +1,49 @@
+﻿CREATE VIEW [dbo].[vyuICGetPostedLot]
+AS 
+
+SELECT	intLotId				= Lot.intLotId
+		,strLotNumber			= Lot.strLotNumber
+		,strLotAlias			= Lot.strLotAlias
+		,intItemId				= Lot.intItemId
+		,strItemNo				= Item.strItemNo 
+		,strDescription			= Item.strDescription
+		,intLocationId			= Lot.intLocationId
+		,strLocationName		= Location.strLocationName
+		,intItemLocationId		= Lot.intItemLocationId
+		,intSubLocationId		= Lot.intSubLocationId
+		,strSubLocationName		= SubLocation.strSubLocationName
+		,intStorageLocationId	= Lot.intStorageLocationId
+		,strStorageLocationName = StorageLocation.strName
+		,intItemUOMId			= Lot.intItemUOMId
+		,strItemUOM				= UOM.strUnitMeasure
+		,intWeightUOMId			= Lot.intWeightUOMId
+		,strWeightUOM			= WeightUOM.strUnitMeasure
+		,dblQty					= Lot.dblQty
+		,dblWeight				= Lot.dblWeight
+		,dblWeightPerQty		= Lot.dblWeightPerQty
+		,dblCost				= PostedLot.dblCost  
+		,dtmExpiryDate			= Lot.dtmExpiryDate
+		,intLotStatusId			= Lot.intLotStatusId
+		,strLotStatus			= LotStatus.strSecondaryStatus
+		,strLotPrimaryStatus	= LotStatus.strPrimaryStatus
+FROM	dbo.tblICLot Lot INNER JOIN dbo.tblICInventoryLot PostedLot
+			ON Lot.intLotId = PostedLot.intLotId
+			AND ISNULL(PostedLot.ysnIsUnposted, 0) = 0 
+		LEFT JOIN tblICItem Item 
+			ON Item.intItemId = Lot.intItemId
+		LEFT JOIN tblSMCompanyLocation Location 
+			ON Location.intCompanyLocationId = Lot.intLocationId
+		LEFT JOIN tblICItemUOM ItemUOM 
+			ON ItemUOM.intItemUOMId = Lot.intItemUOMId
+		LEFT JOIN tblICUnitMeasure UOM 
+			ON UOM.intUnitMeasureId = ItemUOM.intUnitMeasureId
+		LEFT JOIN tblSMCompanyLocationSubLocation SubLocation 
+			ON SubLocation.intCompanyLocationSubLocationId = Lot.intSubLocationId
+		LEFT JOIN tblICStorageLocation StorageLocation 
+			ON StorageLocation.intStorageLocationId = Lot.intStorageLocationId
+		LEFT JOIN tblICLotStatus LotStatus 
+			ON LotStatus.intLotStatusId = Lot.intLotStatusId
+		LEFT JOIN tblICItemUOM ItemWeightUOM 
+			ON ItemWeightUOM.intItemUOMId = Lot.intWeightUOMId
+		LEFT JOIN tblICUnitMeasure WeightUOM 
+			ON WeightUOM.intUnitMeasureId = ItemWeightUOM.intUnitMeasureId
