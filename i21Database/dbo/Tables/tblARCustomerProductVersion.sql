88b2a7b9a28c6022fb05ed59eb7b409848e7937d
Mon Gonzales <mon.gonzales@irely.com>
2015-04-22 17:05:12 +0800

    EM-85

--- i21Database/dbo/Tables/tblARCustomerProductVersion.sql
+++ i21Database/dbo/Tables/tblARCustomerProductVersion.sql
@@ -3,6 +3,15 @@
     [intCustomerId]               INT NOT NULL,
     [intProductId]                INT NOT NULL,
     [intVersionId]                INT NOT NULL,
+	[intModuleId]                 INT NULL,
+	[strCompany]				  NVARCHAR(50)  COLLATE Latin1_General_CI_AS,
+	[strName]					NVARCHAR(50)  COLLATE Latin1_General_CI_AS,
+	[strOperatingSystem]		NVARCHAR(50)  COLLATE Latin1_General_CI_AS,
+	[strAcuVersion]				NVARCHAR(50)  COLLATE Latin1_General_CI_AS,
+	[strDatabase]				NVARCHAR(50)  COLLATE Latin1_General_CI_AS,
+	[strServPak]				NVARCHAR(50)  COLLATE Latin1_General_CI_AS,
+	[strApplyDt]				NVARCHAR(50)  COLLATE Latin1_General_CI_AS,
+	[strInfoPulled]				NVARCHAR(50)  COLLATE Latin1_General_CI_AS,
     [intConcurrencyId]            INT CONSTRAINT [DF_tblARCustomerProductVersion_intConcurrencyId] DEFAULT ((0)) NOT NULL,
     CONSTRAINT [PK_tblARCustomerProductVersion] PRIMARY KEY CLUSTERED ([intCustomerProductVersionId] ASC)
 );
