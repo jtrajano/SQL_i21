6c1f2607fecd3879b63f9de6353d3a80781fadec
Mon Gonzales <mon.gonzales@irely.com>
2015-04-22 14:11:50 +0800

    EM-91

---
+++ i21Database/dbo/Tables/tblARSpecialTax.sql
@@ -0,0 +1,30 @@
+﻿CREATE TABLE [dbo].[tblARSpecialTax]
+(
+	[intARSpecialTaxId]		INT NOT NULL identity,
+	[intEntityCustomerId]	INT NOT NULL,
+	[intEntityVendorId]		INT NULL,
+	[intItemId]				INT NULL,
+	[intCategoryId]			INT NULL,
+	[intTaxGroupMasterId]	INT	NULL,
+	[intConcurrencyId]	INT	DEFAULT(0) NOT NULL,
+	CONSTRAINT [PK_dbo_tblARSpecialTax] PRIMARY KEY CLUSTERED ([intARSpecialTaxId] ASC),
+	CONSTRAINT FK_tblARSpecialTax_tblARCustomer FOREIGN KEY ([intEntityCustomerId]) REFERENCES [tblARCustomer]([intEntityCustomerId]),
+	CONSTRAINT FK_tblARSpecialTax_tblAPVendor FOREIGN KEY ([intEntityVendorId]) REFERENCES [tblAPVendor]([intEntityVendorId]),
+	CONSTRAINT FK_tblARSpecialTax_tbltblICItem FOREIGN KEY ([intItemId]) REFERENCES [tblICItem]([intItemId]),
+	CONSTRAINT FK_tblARSpecialTax_tblICCategory FOREIGN KEY ([intCategoryId]) REFERENCES [tblICCategory]([intCategoryId]),
+	CONSTRAINT FK_tblARSpecialTax_tblSMTaxGroupMaster FOREIGN KEY ([intTaxGroupMasterId]) REFERENCES [tblSMTaxGroupMaster]([intTaxGroupMasterId])
+
+)
+
+
+--CREATE TABLE tblARSpecialTax
+--(
+--	intARSpecialTaxId	int not null identity,
+--	intEntityCustomerId int not null,
+--	strColumn1	nvarchar(100) COLLATE Latin1_General_CI_AS,
+--	strColumn2	nvarchar(100) COLLATE Latin1_General_CI_AS,
+--	strColumn3	nvarchar(100) COLLATE Latin1_General_CI_AS,
+--	strColumn4	nvarchar(100) COLLATE Latin1_General_CI_AS,
+--	CONSTRAINT [PK_dbo_tblARSpecialTax] PRIMARY KEY CLUSTERED ([intARSpecialTaxId] ASC),
+--	CONSTRAINT FK_tblARSpecialTax_tblARCustomer FOREIGN KEY ([intEntityCustomerId]) REFERENCES [tblARCustomer]([intEntityCustomerId]),
+--)
