7785326b9eb0cedaf282e6fdc7b982ddc70e46e0
unknown <MParagas@Huebert.COMTECH.COM>
2015-04-23 17:57:57 +0800

    FCOM-313

---
+++ i21Database/Scripts/Post-Deployment/SM/DefaultData/13_ShortcutKey.sql
@@ -0,0 +1,26 @@
+﻿GO
+	PRINT N'BEGIN INSERT DEFAULT SHORCUTKEY'
+GO
+SET IDENTITY_INSERT [dbo].[tblSMShortcutKeys] ON 
+
+INSERT [dbo].[tblSMShortcutKeys] ([intShortcutKeyId], [strModule], [strItemId], [strShortcutKey], [ctrl], [shift], [alt], [isEnabled], [intConcurrencyId]) VALUES (1, NULL, N'btnRefresh', N'r', 1, 0, 0, 1, 1)
+INSERT [dbo].[tblSMShortcutKeys] ([intShortcutKeyId], [strModule], [strItemId], [strShortcutKey], [ctrl], [shift], [alt], [isEnabled], [intConcurrencyId]) VALUES (2, NULL, N'btnClose', N'x', 0, 0, 1, 1, 2)
+INSERT [dbo].[tblSMShortcutKeys] ([intShortcutKeyId], [strModule], [strItemId], [strShortcutKey], [ctrl], [shift], [alt], [isEnabled], [intConcurrencyId]) VALUES (3, NULL, N'btnNew', N'n', 0, 1, 0, 1, 1)
+INSERT [dbo].[tblSMShortcutKeys] ([intShortcutKeyId], [strModule], [strItemId], [strShortcutKey], [ctrl], [shift], [alt], [isEnabled], [intConcurrencyId]) VALUES (4, NULL, N'btnSave', N's', 1, 0, 0, 1, 1)
+INSERT [dbo].[tblSMShortcutKeys] ([intShortcutKeyId], [strModule], [strItemId], [strShortcutKey], [ctrl], [shift], [alt], [isEnabled], [intConcurrencyId]) VALUES (6, NULL, N'btnDelete', N'd', 1, 0, 0, 1, 1)
+INSERT [dbo].[tblSMShortcutKeys] ([intShortcutKeyId], [strModule], [strItemId], [strShortcutKey], [ctrl], [shift], [alt], [isEnabled], [intConcurrencyId]) VALUES (7, NULL, N'btnDeleteItems', N'd', 1, 1, 0, 1, 1)
+INSERT [dbo].[tblSMShortcutKeys] ([intShortcutKeyId], [strModule], [strItemId], [strShortcutKey], [ctrl], [shift], [alt], [isEnabled], [intConcurrencyId]) VALUES (9, NULL, N'btnUndo', N'z', 1, 0, 0, 1, 1)
+INSERT [dbo].[tblSMShortcutKeys] ([intShortcutKeyId], [strModule], [strItemId], [strShortcutKey], [ctrl], [shift], [alt], [isEnabled], [intConcurrencyId]) VALUES (10, NULL, N'btnOpenSelected', N'q', 1, 0, 0, 1, 1)
+INSERT [dbo].[tblSMShortcutKeys] ([intShortcutKeyId], [strModule], [strItemId], [strShortcutKey], [ctrl], [shift], [alt], [isEnabled], [intConcurrencyId]) VALUES (11, NULL, N'btnEdit', N'e', 1, 0, 0, 1, 1)
+INSERT [dbo].[tblSMShortcutKeys] ([intShortcutKeyId], [strModule], [strItemId], [strShortcutKey], [ctrl], [shift], [alt], [isEnabled], [intConcurrencyId]) VALUES (12, NULL, N'btnRefresh', N'r', 1, 0, 0, 1, 1)
+INSERT [dbo].[tblSMShortcutKeys] ([intShortcutKeyId], [strModule], [strItemId], [strShortcutKey], [ctrl], [shift], [alt], [isEnabled], [intConcurrencyId]) VALUES (13, NULL, N'btnFind', N's', 0, 0, 1, 1, 1)
+INSERT [dbo].[tblSMShortcutKeys] ([intShortcutKeyId], [strModule], [strItemId], [strShortcutKey], [ctrl], [shift], [alt], [isEnabled], [intConcurrencyId]) VALUES (14, NULL, N'btnOk', N'13', 0, 0, 0, 1, 1)
+INSERT [dbo].[tblSMShortcutKeys] ([intShortcutKeyId], [strModule], [strItemId], [strShortcutKey], [ctrl], [shift], [alt], [isEnabled], [intConcurrencyId]) VALUES (16, NULL, N'btnCancel', N'c', 0, 0, 1, 1, 1)
+INSERT [dbo].[tblSMShortcutKeys] ([intShortcutKeyId], [strModule], [strItemId], [strShortcutKey], [ctrl], [shift], [alt], [isEnabled], [intConcurrencyId]) VALUES (18, NULL, N'btnClose', N'x', 0, 0, 1, 1, 1)
+INSERT [dbo].[tblSMShortcutKeys] ([intShortcutKeyId], [strModule], [strItemId], [strShortcutKey], [ctrl], [shift], [alt], [isEnabled], [intConcurrencyId]) VALUES (19, NULL, N'btnPrint', N'p', 1, 0, 0, 1, 1)
+INSERT [dbo].[tblSMShortcutKeys] ([intShortcutKeyId], [strModule], [strItemId], [strShortcutKey], [ctrl], [shift], [alt], [isEnabled], [intConcurrencyId]) VALUES (20, NULL, N'btnAbout', N'i', 1, 0, 0, 1, 1)
+INSERT [dbo].[tblSMShortcutKeys] ([intShortcutKeyId], [strModule], [strItemId], [strShortcutKey], [ctrl], [shift], [alt], [isEnabled], [intConcurrencyId]) VALUES (22, NULL, N'btnLogOut', N'l', 1, 0, 0, 1, 1)
+SET IDENTITY_INSERT [dbo].[tblSMShortcutKeys] OFF
+GO
+	PRINT N'END INSERT DEFAULT SHORCUTKEY'
+GO
---
+++ i21Database/dbo/Tables/tblSMShortcutKeys.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[tblSMShortcutKeys](
+	[intShortcutKeyId] [int] IDENTITY(1,1) NOT NULL,
+	[strModule] [nvarchar](50) NULL,
+	[strItemId] [nvarchar](30) NOT NULL,
+	[strShortcutKey] [nvarchar](5) NOT NULL,
+	[ctrl] [bit] NULL,
+	[shift] [bit] NULL,
+	[alt] [bit] NULL,
+	[isEnabled] [bit] NULL,
+	[intConcurrencyId] [int] NULL
+) ON [PRIMARY]
+
