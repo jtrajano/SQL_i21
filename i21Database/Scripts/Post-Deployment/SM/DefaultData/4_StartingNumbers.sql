dcc858806b6ba86b7b872be6905f9d4db7046fe1
Chakra <chakradhar.behera@irely.com>
2015-04-23 20:45:20 +0530

    Starting No for Blend Demand

--- i21Database/Scripts/Post-Deployment/SM/DefaultData/4_StartingNumbers.sql
+++ i21Database/Scripts/Post-Deployment/SM/DefaultData/4_StartingNumbers.sql
@@ -425,6 +425,15 @@ GO
 			,[ysnEnable]			= 1
 			,[intConcurrencyId]		= 1
 	WHERE NOT EXISTS (SELECT TOP 1 1 FROM tblSMStartingNumber WHERE strTransactionType = N'FutOpt Transaction')
+	UNION ALL
+	SELECT	[intStartingNumberId]	= 46
+			,[strTransactionType]	= N'Demand Number'
+			,[strPrefix]			= N'DN-'
+			,[intNumber]			= 1
+			,[strModule]			= 'Manufacturing'
+			,[ysnEnable]			= 1
+			,[intConcurrencyId]		= 1
+	WHERE NOT EXISTS (SELECT TOP 1 1 FROM tblSMStartingNumber WHERE strTransactionType = N'Demand Number')
 
 	SET IDENTITY_INSERT [dbo].[tblSMStartingNumber] OFF
 GO
