9ae21cc45459a7595063c890433dc248702e9cdb
Lawrence Zabala <lawrence.zabala@irely.com>
2015-04-24 10:36:59 +0800

    IC-567

---
+++ i21Database/dbo/Stored Procedures/uspICUpdatePOStatusOnReceiptSave.sql
@@ -0,0 +1,31 @@
+﻿CREATE PROCEDURE [dbo].[uspICUpdatePOStatusOnReceiptSave]
+	@intReceiptNo INT,
+	@ysnOpenStatus BIT = 0
+AS
+
+DECLARE @POId INT
+
+BEGIN
+
+	IF EXISTS(SELECT * FROM tblICInventoryReceipt WHERE strReceiptType = 'Purchase Order')
+	BEGIN
+		SELECT DISTINCT intSourceId INTO #tmpPOList FROM tblICInventoryReceiptItem
+		WHERE intInventoryReceiptId = @intReceiptNo
+
+		WHILE EXISTS(SELECT TOP 1 1 FROM #tmpPOList)
+		BEGIN
+			SELECT TOP 1 @POId = intSourceId FROM #tmpPOList
+
+			IF (@ysnOpenStatus = 1)
+				EXEC uspPOUpdateStatus @POId, 1
+			ELSE
+				EXEC uspPOUpdateStatus @POId
+
+			DELETE FROM #tmpPOList WHERE intSourceId = @POId
+		END
+		
+		DROP TABLE #tmpPOList
+	END
+
+	RETURN
+END
