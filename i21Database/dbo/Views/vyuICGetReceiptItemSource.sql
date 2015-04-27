200d4b181cb7373c4c70b9b88ac3da63fbd3b134
Lawrence Zabala <lawrence.zabala@irely.com>
2015-04-27 15:10:50 +0800

    IC-649

--- i21Database/dbo/Views/vyuICGetReceiptItemSource.sql
+++ i21Database/dbo/Views/vyuICGetReceiptItemSource.sql
@@ -4,6 +4,7 @@
 SELECT 
 	ReceiptItem.intInventoryReceiptItemId,
 	ReceiptItem.intSourceId,
+	Receipt.strReceiptType,
 	strSourceId = 
 		(
 			CASE WHEN Receipt.strReceiptType = 'Contract'
@@ -32,6 +33,54 @@ SELECT
 			WHEN Receipt.strReceiptType = 'Direct'
 				THEN NULL
 			ELSE NULL
+			END
+		),
+	strUnitMeasure = 
+		(
+			CASE WHEN Receipt.strReceiptType = 'Contract'
+				THEN NULL
+			WHEN Receipt.strReceiptType = 'Purchase Order'
+				THEN (SELECT strUnitMeasure FROM tblPOPurchaseDetail
+						LEFT JOIN tblICItemUOM ON tblPOPurchaseDetail.intUnitOfMeasureId = tblICItemUOM.intItemUOMId
+						LEFT JOIN tblICUnitMeasure ON tblICUnitMeasure.intUnitMeasureId = tblICItemUOM.intUnitMeasureId
+						WHERE intPurchaseId = ReceiptItem.intSourceId AND intPurchaseDetailId = ReceiptItem.intLineNo)
+			WHEN Receipt.strReceiptType = 'Transfer Receipt'
+				THEN NULL
+			WHEN Receipt.strReceiptType = 'Direct Transfer'
+				THEN NULL
+			WHEN Receipt.strReceiptType = 'Direct'
+				THEN NULL
+			ELSE NULL
+			END
+		),
+	dblOrdered = 
+		(
+			CASE WHEN Receipt.strReceiptType = 'Contract'
+				THEN 0.00
+			WHEN Receipt.strReceiptType = 'Purchase Order'
+				THEN ISNULL((SELECT ISNULL(dblOrderQty, 0.00) FROM tblPOPurchaseDetail WHERE intPurchaseId = ReceiptItem.intSourceId AND intPurchaseDetailId = ReceiptItem.intLineNo), 0.00)
+			WHEN Receipt.strReceiptType = 'Transfer Receipt'
+				THEN 0.00
+			WHEN Receipt.strReceiptType = 'Direct Transfer'
+				THEN 0.00
+			WHEN Receipt.strReceiptType = 'Direct'
+				THEN 0.00
+			ELSE 0.00
+			END
+		),
+	dblReceived = 
+		(
+			CASE WHEN Receipt.strReceiptType = 'Contract'
+				THEN 0.00
+			WHEN Receipt.strReceiptType = 'Purchase Order'
+				THEN ISNULL((SELECT ISNULL(dblReceived, 0.00) FROM tblPOPurchaseDetail WHERE intPurchaseId = ReceiptItem.intSourceId AND intPurchaseDetailId = ReceiptItem.intLineNo), 0.00)
+			WHEN Receipt.strReceiptType = 'Transfer Receipt'
+				THEN 0.00
+			WHEN Receipt.strReceiptType = 'Direct Transfer'
+				THEN 0.00
+			WHEN Receipt.strReceiptType = 'Direct'
+				THEN 0.00
+			ELSE 0.00
 			END
 		)
 FROM tblICInventoryReceiptItem ReceiptItem
