ddc8f3003f1300c0238e492c1f14e475ca51c19f
Ravikumar Kumashi <ravikumar.k@irely.com>
2015-04-28 12:53:06 +0530

    ST-152

---
+++ Integration/dbo/Views/vyustpbkmst.sql
@@ -0,0 +1,32 @@
+﻿GO
+
+
+IF EXISTS(select top 1 1 from INFORMATION_SCHEMA.VIEWS where TABLE_NAME = 'vyustpbkmst')
+   BEGIN
+	DROP VIEW vyustpbkmst
+	END
+
+IF EXISTS(select top 1 1 from INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'stpbkmst')
+BEGIN
+	EXEC ('
+		CREATE VIEW [dbo].vyustpbkmst
+		AS 
+       select stpbk_store_name,
+       stpbk_upcno,
+       stpbk_upc_modno,
+       stpbk_item_desc,
+       stpbk_pos_desc,
+       stpbk_price,
+       stpbk_vnd_id,
+       stpbk_vnd_itm_id,
+       stpbk_casesize, 
+       stpbk_deptno,
+       stpbk_family,
+       stpbk_class,
+       A4GLIdentity
+       from stpbkmst
+	')
+END
+
+GO
+
