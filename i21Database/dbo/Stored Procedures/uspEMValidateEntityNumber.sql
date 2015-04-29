b9c4fcad71cba8bff2a4b50fbe03983eb8294273
Mon Gonzales <mon.gonzales@irely.com>
2015-04-29 14:12:12 +0800

    EM-116

---
+++ i21Database/dbo/Stored Procedures/uspEMValidateEntityNumber.sql
@@ -0,0 +1,40 @@
+﻿CREATE PROCEDURE uspEMValidateEntityNumber
+	@Identification		NVARCHAR(100),
+	@EntityId			int,
+	@Message			NVARCHAR(200)	OUTPUT
+as
+BEGIN
+	SET QUOTED_IDENTIFIER OFF
+	SET ANSI_NULLS ON
+	SET NOCOUNT ON
+	SET XACT_ABORT ON
+	SET ANSI_WARNINGS OFF
+
+	IF EXISTS(SELECT TOP 1 1 FROM tblEntity where strEntityNo = @Identification AND intEntityId != @EntityId)
+	BEGIN
+		Set @Message = 'Entity No already exists'
+		GOTO ExitHere
+	END
+
+	IF EXISTS(SELECT TOP 1 1 FROM tblAPVendor where strVendorId = @Identification  AND intEntityVendorId != @EntityId)
+	BEGIN
+		Set @Message = 'Entity No already exists as Vendor'
+		GOTO ExitHere
+	END
+
+	IF EXISTS(SELECT TOP 1 1 FROM tblARCustomer where strCustomerNumber = @Identification AND intEntityCustomerId != @EntityId)
+	BEGIN
+		Set @Message = 'Entity No already exists as Customer'
+		GOTO ExitHere
+	END
+
+	IF EXISTS(SELECT TOP 1 1 FROM tblARSalesperson where strSalespersonId = @Identification AND intEntitySalespersonId != @EntityId)
+	BEGIN
+		Set @Message = 'Entity No already exists as Salesperson'
+		GOTO ExitHere
+	END
+
+
+ExitHere: 
+END
+GO
