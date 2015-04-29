d0207ba4689309a8fcff306f994b2f2f967afec9
Arman Arenas <arman.arenas@irely.com>
2015-04-30 04:39:59 +0800

    CM-621 bank transaction reversal option for ap payments

--- i21Database/dbo/Stored Procedures/uspCMBankTransactionReversal.sql
+++ i21Database/dbo/Stored Procedures/uspCMBankTransactionReversal.sql
@@ -168,7 +168,9 @@ ELSE
 				IF (@intVoidBankTransactionTypeId = @AP_PAYMENT)
 					BEGIN
 						/* If Void Check entry for AP Payment, do not post the reversal*/
-						UPDATE tblCMBankTransaction SET ysnPosted = 1, ysnCheckVoid = 1, @isPostingSuccessful = 1 
+						UPDATE tblCMBankTransaction 
+							SET ysnPosted = 1, ysnCheckVoid = 1, ysnClr = 1, 
+								dtmReconciliationDate = dtmDate, dtmCheckPrinted = dtmDate, @isPostingSuccessful = 1 
 						WHERE strTransactionId = @strVoidTransactionId AND intBankTransactionTypeId = @VOID_CHECK
 					END
 				ELSE
