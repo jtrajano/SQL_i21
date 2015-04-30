7403524a63feee9540d7c372ce3f05756f6f55d5
Bonn Paolo Ponce <bonn.ponce@irely.com>
2015-04-30 10:46:05 +0800

    CF-260 Add card fueling tables on SSDT project

--- i21Database/dbo/Views/vyuCFAccountSearchCustomer.sql
+++ i21Database/dbo/Views/vyuCFAccountSearchCustomer.sql
@@ -1,15 +1,9 @@
 ﻿CREATE VIEW dbo.vyuCFAccountSearchCustomer
 AS
-SELECT     intEntityCustomerId, strCustomerNumber, strType, dblCreditLimit, dblARBalance, strAccountNumber, strTaxNumber, strCurrency, intCurrencyId, intAccountStatusId, 
-                      intSalespersonId, strPricing, strLevel, dblPercent, strTimeZone, ysnActive, intDefaultContactId, intDefaultLocationId, intBillToId, intShipToId, strTaxState, 
-                      ysnPORequired, ysnCreditHold, ysnStatementDetail, strStatementFormat, intCreditStopDays, strTaxAuthority1, strTaxAuthority2, ysnPrintPriceOnPrintTicket, 
-                      intServiceChargeId, ysnApplySalesTax, ysnApplyPrepaidTax, dblBudgetAmountForBudgetBilling, strBudgetBillingBeginMonth, strBudgetBillingEndMonth, 
-                      ysnCalcAutoFreight, strUpdateQuote, strCreditCode, strDiscSchedule, strPrintInvoice, ysnSpecialPriceGroup, ysnExcludeDunningLetter, strLinkCustomerNumber, 
-                      intReferredByCustomer, ysnReceivedSignedLiscense, strDPAContract, dtmDPADate, strGBReceiptNumber, ysnCheckoffExempt, ysnVoluntaryCheckoff, 
-                      strCheckoffState, ysnMarketAgreementSigned, intMarketZoneId, ysnHoldBatchGrainPayment, ysnFederalWithholding, strAEBNumber, strAgrimineId, 
-                      strHarvestPartnerCustomerId, strComments, ysnTransmittedCustomer, dtmMembershipDate, dtmBirthDate, strStockStatus, strPatronClass, dtmDeceasedDate, 
-                      ysnSubjectToFWT, ysnHDBillableSupport, intTaxCodeId, intConcurrencyId
-FROM         dbo.tblARCustomer
+SELECT     intEntityId, intEntityCustomerId, strName, strCustomerNumber, strType, strPhone, strAddress, strCity, strState, strZipCode, ysnActive, intSalespersonId, intCurrencyId, 
+                      intTermsId, intShipViaId, strShipToLocationName, strShipToAddress, strShipToCity, strShipToState, strShipToZipCode, strShipToCountry, strBillToLocationName, 
+                      strBillToAddress, strBillToCity, strBillToState, strBillToZipCode, strBillToCountry
+FROM         dbo.vyuARCustomer
 WHERE     (intEntityCustomerId NOT IN
                           (SELECT     intCustomerId
                             FROM          dbo.tblCFAccount))
