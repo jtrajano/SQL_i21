2930a45061f19c757077f40cdc6beeb0a8cb67e6
Jayson Ramos <jayson.ramos@iRely.com>
2015-05-04 11:14:01 +0800

    Add vyuHDMention.

---
+++ i21Database/dbo/Views/vyuHDMention.sql
@@ -0,0 +1,26 @@
+﻿CREATE VIEW [dbo].[vyuHDMention]
+	AS
+	select
+		strId = NEWID()
+		,cu.intEntityCustomerId
+		,cu.strCustomerNumber
+		,intEntityContactId = con.intEntityId
+		,con.strName
+		,con.strEmail
+	from
+		tblARCustomer cu
+		left outer join tblARCustomerToContact cc on cc.intEntityCustomerId = cu.intEntityCustomerId
+		left outer join tblEntity con on con.intEntityId = cc.intEntityContactId and con.ysnActive = 1
+	where cu.ysnActive = 1
+	union all
+	select
+		strId = NEWID()
+		,intEntityCustomerId = us.intEntityId
+		,strCustomerNumber = 'INTERNALUSER'
+		,intEntityContactId = con.intEntityId
+		,con.strName
+		,con.strEmail
+	from
+		tblSMUserSecurity us
+		left outer join tblEntity con on con.intEntityId = us.intEntityId
+	where us.ysnDisabled <> 1
