c4a689e86be1541254cc18b48a67f60166ee7b6c
Bonn Paolo Ponce <bonn.ponce@irely.com>
2015-05-04 21:17:17 +0800

    CF-260 Add card fueling tables on SSDT project

--- i21Database/dbo/Tables/tblCFStateCode.sql
+++ i21Database/dbo/Tables/tblCFStateCode.sql
@@ -1,11 +1,15 @@
 ﻿CREATE TABLE [dbo].[tblCFStateCode] (
     [intStateCodeId]   INT            IDENTITY (1, 1) NOT NULL,
+    [intNetworkId]     INT            NULL,
     [intStateCode]     INT            NULL,
     [strStateName]     NVARCHAR (250) COLLATE Latin1_General_CI_AS NULL,
     [strPostalCode]    NVARCHAR (250) COLLATE Latin1_General_CI_AS NULL,
     [intConcurrencyId] INT            CONSTRAINT [DF_tblCFStateCode_intConcurrencyId] DEFAULT ((1)) NULL,
-    CONSTRAINT [PK_tblCFStateCode] PRIMARY KEY CLUSTERED ([intStateCodeId] ASC)
+    CONSTRAINT [PK_tblCFStateCode] PRIMARY KEY CLUSTERED ([intStateCodeId] ASC),
+    CONSTRAINT [FK_tblCFStateCode_tblCFNetwork] FOREIGN KEY ([intNetworkId]) REFERENCES [dbo].[tblCFNetwork] ([intNetworkId]) ON DELETE CASCADE
 );
+
+
 
 
 
