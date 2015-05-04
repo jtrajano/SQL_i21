53ba8cbb7cfe49a1dd34c3fb860d9ae421722067
Lawrence Zabala <lawrence.zabala@irely.com>
2015-05-04 18:07:42 +0800

    IC-238

---
+++ i21Database/dbo/Stored Procedures/uspICCreateLotNumberOnInventoryTransfer.sql
@@ -0,0 +1,216 @@
+﻿CREATE PROCEDURE [dbo].[uspICCreateLotNumberOnInventoryTransfer]
+	@strTransactionId NVARCHAR(40) = NULL   
+	,@intUserId INT
+	,@ysnPost BIT
+AS
+
+SET QUOTED_IDENTIFIER OFF
+SET ANSI_NULLS ON
+SET NOCOUNT ON
+SET XACT_ABORT ON
+SET ANSI_WARNINGS OFF
+
+DECLARE @ItemsThatNeedLotId AS dbo.ItemLotTableType
+
+DECLARE @LotType_Manual AS INT = 1
+		,@LotType_Serial AS INT = 2
+
+-- Create the temp table 
+CREATE TABLE #GeneratedLotItems (
+	intLotId INT
+	,strLotNumber NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL
+	,intDetailId INT 
+)
+
+------------------------------------------------------------------------------
+-- Validation 
+------------------------------------------------------------------------------
+BEGIN 
+	DECLARE @strItemNo AS NVARCHAR(50)
+	DECLARE @strUnitMeasure AS NVARCHAR(50)
+	DECLARE @intItemId AS INT
+	DECLARE @OpenReceiveQty AS NUMERIC(18,6)
+	DECLARE @LotQty AS NUMERIC(18,6)
+	DECLARE @OpenReceiveQtyInItemUOM AS NUMERIC(18,6)
+	DECLARE @LotQtyInItemUOM AS NUMERIC(18,6)
+
+	DECLARE @FormattedReceivedQty AS NVARCHAR(50)
+	DECLARE @FormattedLotQty AS NVARCHAR(50)
+	DECLARE @FormattedDifference AS NVARCHAR(50)
+
+	-- Check if the unit quantities on the UOM table are valid. 
+	BEGIN 
+		SELECT	TOP 1 
+				@strItemNo = Item.strItemNo
+				,@intItemId = Item.intItemId
+				,@strUnitMeasure = UOM.strUnitMeasure
+		FROM	dbo.tblICInventoryTransfer Transfer
+				INNER JOIN dbo.tblICInventoryTransferDetail TransferItem
+					ON Transfer.intInventoryTransferId = TransferItem.intInventoryTransferId
+				INNER JOIN dbo.tblICItem Item
+					ON TransferItem.intItemId = Item.intItemId
+				INNER JOIN dbo.tblICItemUOM ItemUOM
+					ON ItemUOM.intItemId = TransferItem.intItemId
+				INNER JOIN dbo.tblICUnitMeasure UOM
+					ON ItemUOM.intUnitMeasureId = UOM.intUnitMeasureId
+		WHERE	ISNULL(ItemUOM.dblUnitQty, 0) <= 0
+
+		IF @intItemId IS NOT NULL 
+		BEGIN 
+			IF ISNULL(@strItemNo, '') = '' 
+				SET @strItemNo = 'an item with id ' + CAST(@intItemId AS NVARCHAR(50)) 
+
+			-- 'Please correct the unit qty in UOM {UOM} on {Item}.'
+			RAISERROR(51050, 11, 1, @strUnitMeasure, @strItemNo) 
+			RETURN -1; 			 
+		END 
+	END 
+		
+	-- Check if the Item Transfer quantity matches the total Quantity in the Lot
+	SET @strItemNo = NULL 
+	SET @intItemId = NULL 
+
+	SELECT	TOP 1 
+			@strItemNo					= Item.strItemNo
+			,@intItemId					= Item.intItemId
+			,@OpenReceiveQty			= TransferItem.dblQuantity
+			,@LotQty					= ItemLot.TotalLotQty
+			,@LotQtyInItemUOM			= ItemLot.TotalLotQtyInItemUOM
+			,@OpenReceiveQtyInItemUOM	= dbo.fnCalculateQtyBetweenUOM (
+											TransferItem.intItemUOMId
+											,TransferItem.intItemUOMId
+											,TransferItem.dblQuantity
+										)
+	FROM	dbo.tblICInventoryTransfer Transfer INNER JOIN dbo.tblICInventoryTransferDetail TransferItem
+				ON Transfer.intInventoryTransferId = TransferItem.intInventoryTransferId
+			INNER JOIN dbo.tblICItem Item
+				ON Item.intItemId = TransferItem.intItemId
+			LEFT JOIN (
+				SELECT  tblICInventoryTransferDetail.intInventoryTransferDetailId
+						,TotalLotQtyInItemUOM = SUM(
+							dbo.fnCalculateQtyBetweenUOM(
+								ISNULL(tblICInventoryTransferDetail.intItemUOMId, tblICInventoryTransferDetail.intItemUOMId)
+								,tblICInventoryTransferDetail.intItemUOMId
+								,tblICInventoryTransferDetail.dblQuantity
+							)
+						)
+						,TotalLotQty = SUM(ISNULL(tblICInventoryTransferDetail.dblQuantity, 0))
+				FROM	dbo.tblICInventoryTransfer INNER JOIN dbo.tblICInventoryTransferDetail
+							ON tblICInventoryTransfer.intInventoryTransferId = tblICInventoryTransferDetail.intInventoryTransferId
+				WHERE	tblICInventoryTransfer.strTransferNo = @strTransactionId				
+				GROUP BY tblICInventoryTransferDetail.intInventoryTransferDetailId
+			) ItemLot
+				ON ItemLot.intInventoryTransferDetailId = TransferItem.intInventoryTransferDetailId											
+	WHERE	dbo.fnGetItemLotType(TransferItem.intItemId) IN (@LotType_Manual, @LotType_Serial)	
+			AND Transfer.strTransferNo = @strTransactionId
+			AND ROUND(ItemLot.TotalLotQtyInItemUOM, 2) <>
+				ROUND(dbo.fnCalculateQtyBetweenUOM (
+					TransferItem.intItemUOMId
+					,TransferItem.intItemUOMId
+					,TransferItem.dblQuantity
+				), 2)
+
+	IF @intItemId IS NOT NULL 
+	BEGIN 
+		IF ISNULL(@strItemNo, '') = '' 
+			SET @strItemNo = 'Item with id ' + CAST(@intItemId AS NVARCHAR(50)) 
+
+		-- The expected qty to receive for {Item} is {Open Receive Qty}. Lot Quantity is {Total Lot Qty}. The difference is {Calculated difference}.'
+		SET @FormattedReceivedQty =  CONVERT(NVARCHAR, CAST(@OpenReceiveQty AS MONEY), 1)
+		SET @FormattedLotQty =  CONVERT(NVARCHAR, CAST(@LotQtyInItemUOM AS MONEY), 1)
+		SET @FormattedDifference =  CAST(ABS(@OpenReceiveQty - @LotQtyInItemUOM) AS NVARCHAR(50))
+
+		RAISERROR(51038, 11, 1, @strItemNo, @FormattedReceivedQty, @FormattedLotQty, @FormattedDifference)  
+		RETURN -1; 
+	END 
+END
+
+-- Get the list of item that needs lot numbers
+BEGIN 
+	INSERT INTO @ItemsThatNeedLotId (
+			intLotId
+			,strLotNumber
+			,strLotAlias
+			,intItemId
+			,intItemLocationId
+			,intSubLocationId
+			,intStorageLocationId
+			,dblQty
+			,intItemUOMId
+			,dblWeight
+			,intWeightUOMId
+			,dtmExpiryDate
+			,dtmManufacturedDate
+			,intOriginId
+			,strBOLNo
+			,strVessel
+			,strReceiptNumber
+			,strMarkings
+			,strNotes
+			,intEntityVendorId
+			,strVendorLotNo
+			,intVendorLocationId
+			,strVendorLocation
+			,intDetailId		
+	)
+	SELECT	intLotId				= TransferItem.intNewLotId
+			,strLotNumber			= TransferItem.strNewLotId
+			,strLotAlias			= TransferItem.strNewLotId
+			,intItemId				= TransferItem.intItemId
+			,intItemLocationId		= ItemLocation.intItemLocationId
+			,intSubLocationId		= TransferItem.intToSubLocationId
+			,intStorageLocationId	= TransferItem.intToStorageLocationId
+			,dblQty					= TransferItem.dblQuantity * CASE WHEN @ysnPost = 0 THEN -1 ELSE 1 END 
+			,intItemUOMId			= ISNULL(TransferItem.intItemUOMId, TransferItem.intItemUOMId) 
+			,dblWeight				= ISNULL(TransferItem.dblGrossWeight, 0) - ISNULL(TransferItem.dblTareWeight, 0) * CASE WHEN @ysnPost = 0 THEN -1 ELSE 1 END
+			,intWeightUOMId			= TransferItem.intItemWeightUOMId
+			,dtmExpiryDate			= Lot.dtmExpiryDate
+			,dtmManufacturedDate	= Lot.dtmManufacturedDate
+			,intOriginId			= Lot.intOriginId
+			,strBOLNo				= Lot.strBOLNo
+			,strVessel				= Lot.strVessel
+			,strTransferNumber		= Transfer.strTransferNo
+			,strMarkings			= Lot.strMarkings
+			,strNotes				= Lot.strNotes
+			,intEntityVendorId		= Lot.intEntityVendorId
+			,strVendorLotNo			= Lot.strVendorLotNo
+			,intVendorLocationId	= Lot.intVendorLocationId
+			,strVendorLocation		= Lot.strVendorLocation
+			,intDetailId			= TransferItem.intInventoryTransferDetailId
+	FROM	dbo.tblICInventoryTransfer Transfer INNER JOIN dbo.tblICInventoryTransferDetail TransferItem
+				ON Transfer.intInventoryTransferId = TransferItem.intInventoryTransferId
+			INNER JOIN dbo.tblICItem Item
+				ON TransferItem.intItemId = Item.intItemId		
+			INNER JOIN dbo.tblICItemLocation ItemLocation
+				ON TransferItem.intItemId = ItemLocation.intItemId
+				AND Transfer.intToLocationId = ItemLocation.intLocationId	
+			INNER JOIN tblICLot Lot ON Lot.intLotId = TransferItem.intLotId
+	WHERE	Transfer.strTransferNo = @strTransactionId
+
+END 
+
+-- Call the common stored procedure that will create or updat the lot master table
+BEGIN 
+	DECLARE @intErrorFoundOnCreateUpdateLotNumber AS INT
+
+	EXEC @intErrorFoundOnCreateUpdateLotNumber = dbo.uspICCreateUpdateLotNumber 
+		@ItemsThatNeedLotId
+		,@intUserId
+
+	IF @intErrorFoundOnCreateUpdateLotNumber <> 0
+		RETURN @intErrorFoundOnCreateUpdateLotNumber;
+END
+
+-- Assign the generated lot id's back to the inventory Transfer item-lot table. 
+BEGIN 
+	UPDATE	dbo.tblICInventoryTransferDetail
+	SET		intNewLotId = LotNumbers.intLotId
+			,strNewLotId = LotNumbers.strLotNumber		
+	FROM	dbo.tblICInventoryTransferDetail Detail INNER JOIN #GeneratedLotItems LotNumbers
+				ON Detail.intInventoryTransferDetailId = LotNumbers.intDetailId
+END 
+
+IF EXISTS (SELECT 1 FROM tempdb..sysobjects WHERE id = OBJECT_ID('tempdb..#GeneratedLotItems')) 
+	DROP TABLE #GeneratedLotItems
+
+RETURN 0; 
---
+++ i21Database/dbo/Stored Procedures/uspICPostInventoryTransfer.sql
@@ -0,0 +1,394 @@
+﻿CREATE PROCEDURE [dbo].[uspICPostInventoryTransfer]
+	@ysnPost BIT  = 0  
+	,@ysnRecap BIT  = 0  
+	,@strTransactionId NVARCHAR(40) = NULL   
+	,@intUserId  INT  = NULL   
+	,@intEntityId INT  = NULL    
+AS  
+  
+SET QUOTED_IDENTIFIER OFF  
+SET ANSI_NULLS ON  
+SET NOCOUNT ON  
+SET XACT_ABORT ON  
+SET ANSI_WARNINGS OFF  
+  
+--------------------------------------------------------------------------------------------  
+-- Initialize   
+--------------------------------------------------------------------------------------------    
+-- Create a unique transaction name. 
+DECLARE @TransactionName AS VARCHAR(500) = 'Inventory Transfer Transaction' + CAST(NEWID() AS NVARCHAR(100));
+
+-- Constants  
+DECLARE @INVENTORY_TRANSFER_TYPE AS INT = 12
+DECLARE @STARTING_NUMBER_BATCH AS INT = 3 
+
+-- Get the Inventory Receipt batch number
+DECLARE @strBatchId AS NVARCHAR(40) 
+DECLARE @strItemNo AS NVARCHAR(50)
+
+-- Create the gl entries variable 
+DECLARE @GLEntries AS RecapTableType 
+
+-- Ensure ysnPost is not NULL  
+SET @ysnPost = ISNULL(@ysnPost, 0)  
+ 
+-- Read the transaction info   
+BEGIN   
+	DECLARE @dtmDate AS DATETIME   
+	DECLARE @intTransactionId AS INT  
+	DECLARE @intCreatedEntityId AS INT  
+	DECLARE @ysnAllowUserSelfPost AS BIT   
+	DECLARE @ysnTransactionPostedFlag AS BIT  
+  
+	SELECT TOP 1   
+			@intTransactionId = intInventoryTransferId
+			,@ysnTransactionPostedFlag = ysnPosted
+			,@dtmDate = dtmTransferDate
+			,@intCreatedEntityId = intEntityId
+	FROM	dbo.tblICInventoryTransfer
+	WHERE	strTransferNo = @strTransactionId
+END  
+
+-- Read the user preference  
+BEGIN  
+	SELECT	@ysnAllowUserSelfPost = 1  
+	FROM	dbo.tblSMPreferences   
+	WHERE	strPreference = 'AllowUserSelfPost'   
+			AND LOWER(RTRIM(LTRIM(strValue))) = 'true'    
+			AND intUserID = @intUserId  
+END   
+--------------------------------------------------------------------------------------------  
+-- Validate  
+--------------------------------------------------------------------------------------------  
+-- Validate if the Inventory Receipt exists   
+IF @intTransactionId IS NULL  
+BEGIN   
+	-- Cannot find the transaction.  
+	RAISERROR(50004, 11, 1)  
+	GOTO Post_Exit  
+END   
+  
+-- Validate the date against the FY Periods  
+IF @ysnRecap = 0 AND EXISTS (SELECT 1 WHERE dbo.isOpenAccountingDate(@dtmDate) = 0) 
+BEGIN   
+	-- Unable to find an open fiscal year period to match the transaction date.  
+	RAISERROR(50005, 11, 1)  
+	GOTO Post_Exit  
+END  
+  
+-- Check if the transaction is already posted  
+IF @ysnPost = 1 AND @ysnTransactionPostedFlag = 1  
+BEGIN   
+	-- The transaction is already posted.  
+	RAISERROR(50007, 11, 1)  
+	GOTO Post_Exit  
+END   
+  
+-- Check if the transaction is already posted  
+IF @ysnPost = 0 AND @ysnTransactionPostedFlag = 0  
+BEGIN   
+	-- The transaction is already unposted.  
+	RAISERROR(50008, 11, 1)  
+	GOTO Post_Exit  
+END
+
+-- Check if all Items are avaiable under the To Location
+SELECT TOP 1 Detail.intItemId, Header.intToLocationId, Item.strItemNo, Location.strLocationName
+INTO #tempValidateItemLocation
+FROM tblICInventoryTransferDetail Detail
+INNER JOIN tblICInventoryTransfer Header ON Header.intInventoryTransferId = Detail.intInventoryTransferId
+INNER JOIN tblICItem Item ON Item.intItemId = Detail.intItemId
+INNER JOIN tblSMCompanyLocation Location ON Location.intCompanyLocationId = Header.intToLocationId
+WHERE Detail.intInventoryTransferId = @intTransactionId AND ISNULL(dbo.fnICGetItemLocation(Detail.intItemId, Header.intToLocationId), -1) = -1
+IF EXISTS(SELECT TOP 1 1 FROM #tempValidateItemLocation)
+BEGIN
+	DECLARE @ItemId NVARCHAR(100),
+		@LocationId NVARCHAR(100)
+	SELECT TOP 1 @ItemId = strItemNo, @LocationId = strLocationName FROM #tempValidateItemLocation
+	IF EXISTS(SELECT TOP 1 1 FROM sys.tables WHERE object_id = object_id('tempValidateItemLocation')) DROP TABLE #tempValidateItemLocation
+	RAISERROR(51099, 11, 1, @ItemId, @LocationId)  
+	GOTO Post_Exit  
+END
+IF EXISTS(SELECT TOP 1 1 FROM sys.tables WHERE object_id = object_id('tempValidateItemLocation')) DROP TABLE #tempValidateItemLocation
+
+-- Check Company preference: Allow User Self Post  
+IF @ysnAllowUserSelfPost = 1 AND @intEntityId <> @intCreatedEntityId AND @ysnRecap = 0   
+BEGIN   
+	-- 'You cannot %s transactions you did not create. Please contact your local administrator.'  
+	IF @ysnPost = 1   
+	BEGIN   
+		RAISERROR(50013, 11, 1, 'Post')  
+		GOTO Post_Exit  
+	END   
+
+	IF @ysnPost = 0  
+	BEGIN  
+		RAISERROR(50013, 11, 1, 'Unpost')  
+		GOTO Post_Exit    
+	END  
+END   
+
+--------------------------------------------------------------------------------------------  
+-- Begin a transaction and immediately create a save point 
+--------------------------------------------------------------------------------------------  
+BEGIN TRAN @TransactionName
+SAVE TRAN @TransactionName
+
+-- Create and validate the lot numbers
+IF @ysnPost = 1
+BEGIN 	
+	DECLARE @intCreateUpdateLotError AS INT 
+
+	EXEC @intCreateUpdateLotError = dbo.uspICCreateLotNumberOnInventoryTransfer 
+			@strTransactionId
+			,@intUserId
+			,@ysnPost
+
+	IF @intCreateUpdateLotError <> 0
+	BEGIN 
+		ROLLBACK TRAN @TransactionName
+		COMMIT TRAN @TransactionName
+		GOTO Post_Exit;
+	END
+END
+
+-- Get the next batch number
+EXEC dbo.uspSMGetStartingNumber @STARTING_NUMBER_BATCH, @strBatchId OUTPUT   
+
+--------------------------------------------------------------------------------------------  
+-- If POST, call the post routines  
+--------------------------------------------------------------------------------------------  
+IF @ysnPost = 1  
+BEGIN  
+	-- Get the items to post  
+	DECLARE @ItemsForRemovalPost AS ItemCostingTableType  
+	INSERT INTO @ItemsForRemovalPost (  
+			intItemId  
+			,intItemLocationId 
+			,intItemUOMId  
+			,dtmDate  
+			,dblQty  
+			,dblUOMQty  
+			,dblCost  
+			,dblSalesPrice  
+			,intCurrencyId  
+			,dblExchangeRate  
+			,intTransactionId  
+			,strTransactionId  
+			,intTransactionTypeId  
+			,intLotId 
+			,intSubLocationId
+			,intStorageLocationId
+	) 
+	SELECT Detail.intItemId  
+			,dbo.fnICGetItemLocation(Detail.intItemId, Header.intFromLocationId)
+			,Detail.intItemUOMId  
+			,Header.dtmTransferDate
+			,Detail.dblQuantity * -1
+			,Detail.dblQuantity * -1
+			,Detail.dblCost  
+			,0
+			,NULL
+			,1
+			,@intTransactionId 
+			,@strTransactionId
+			,@INVENTORY_TRANSFER_TYPE
+			,Detail.intLotId 
+			,Detail.intFromSubLocationId
+			,Detail.intFromStorageLocationId
+	FROM tblICInventoryTransferDetail Detail
+	LEFT JOIN tblICInventoryTransfer Header ON Header.intInventoryTransferId = Detail.intInventoryTransferId
+
+	-- Call the post routine 
+	BEGIN 
+		-- Call the post routine 
+		INSERT INTO @GLEntries (
+				[dtmDate] 
+				,[strBatchId]
+				,[intAccountId]
+				,[dblDebit]
+				,[dblCredit]
+				,[dblDebitUnit]
+				,[dblCreditUnit]
+				,[strDescription]
+				,[strCode]
+				,[strReference]
+				,[intCurrencyId]
+				,[dblExchangeRate]
+				,[dtmDateEntered]
+				,[dtmTransactionDate]
+				,[strJournalLineDescription]
+				,[intJournalLineNo]
+				,[ysnIsUnposted]
+				,[intUserId]
+				,[intEntityId]
+				,[strTransactionId]
+				,[intTransactionId]
+				,[strTransactionType]
+				,[strTransactionForm]
+				,[strModuleName]
+				,[intConcurrencyId]
+		)
+		EXEC	dbo.uspICPostCosting  
+				@ItemsForRemovalPost  
+				,@strBatchId  
+				,NULL
+				,@intUserId
+	END
+
+	-- Get the assembly item to post  
+	DECLARE @ItemsForTransferPost AS ItemCostingTableType  
+	INSERT INTO @ItemsForTransferPost (  
+			intItemId  
+			,intItemLocationId 
+			,intItemUOMId  
+			,dtmDate  
+			,dblQty  
+			,dblUOMQty  
+			,dblCost  
+			,dblSalesPrice  
+			,intCurrencyId  
+			,dblExchangeRate  
+			,intTransactionId  
+			,strTransactionId  
+			,intTransactionTypeId  
+			,intLotId 
+			,intSubLocationId
+			,intStorageLocationId
+	) 
+	SELECT Detail.intItemId  
+			,dbo.fnICGetItemLocation(Detail.intItemId, Header.intToLocationId)
+			,Detail.intItemUOMId  
+			,Header.dtmTransferDate
+			,Detail.dblQuantity
+			,Detail.dblQuantity
+			,Detail.dblCost  
+			,0
+			,NULL
+			,1
+			,@intTransactionId 
+			,@strTransactionId
+			,@INVENTORY_TRANSFER_TYPE
+			,Detail.intLotId
+			,Detail.intToSubLocationId
+			,Detail.intToStorageLocationId
+	FROM tblICInventoryTransferDetail Detail
+	LEFT JOIN tblICInventoryTransfer Header ON Header.intInventoryTransferId = Detail.intInventoryTransferId
+
+	-- Call the post routine 
+	BEGIN 
+		-- Call the post routine 
+		INSERT INTO @GLEntries (
+				[dtmDate] 
+				,[strBatchId]
+				,[intAccountId]
+				,[dblDebit]
+				,[dblCredit]
+				,[dblDebitUnit]
+				,[dblCreditUnit]
+				,[strDescription]
+				,[strCode]
+				,[strReference]
+				,[intCurrencyId]
+				,[dblExchangeRate]
+				,[dtmDateEntered]
+				,[dtmTransactionDate]
+				,[strJournalLineDescription]
+				,[intJournalLineNo]
+				,[ysnIsUnposted]
+				,[intUserId]
+				,[intEntityId]
+				,[strTransactionId]
+				,[intTransactionId]
+				,[strTransactionType]
+				,[strTransactionForm]
+				,[strModuleName]
+				,[intConcurrencyId]
+		)
+		EXEC	dbo.uspICPostCosting  
+				@ItemsForTransferPost  
+				,@strBatchId  
+				,NULL
+				,@intUserId
+	END
+
+END   
+
+--------------------------------------------------------------------------------------------  
+-- If UNPOST, call the Unpost routines  
+--------------------------------------------------------------------------------------------  
+IF @ysnPost = 0   
+BEGIN   
+	-- Call the unpost routine 
+	BEGIN 
+		-- Call the post routine 
+		INSERT INTO @GLEntries (
+				[dtmDate] 
+				,[strBatchId]
+				,[intAccountId]
+				,[dblDebit]
+				,[dblCredit]
+				,[dblDebitUnit]
+				,[dblCreditUnit]
+				,[strDescription]
+				,[strCode]
+				,[strReference]
+				,[intCurrencyId]
+				,[dblExchangeRate]
+				,[dtmDateEntered]
+				,[dtmTransactionDate]
+				,[strJournalLineDescription]
+				,[intJournalLineNo]
+				,[ysnIsUnposted]
+				,[intUserId]
+				,[intEntityId]
+				,[strTransactionId]
+				,[intTransactionId]
+				,[strTransactionType]
+				,[strTransactionForm]
+				,[strModuleName]
+				,[intConcurrencyId]
+		)
+		EXEC	dbo.uspICUnpostCosting
+				@intTransactionId
+				,@strTransactionId
+				,@strBatchId
+				,@intUserId						
+	END 
+END   
+
+--------------------------------------------------------------------------------------------  
+-- If RECAP is TRUE, 
+-- 1.	Store all the GL entries in a holding table. It will be used later as data  
+--		for the recap screen.
+--
+-- 2.	Rollback the save point 
+--------------------------------------------------------------------------------------------  
+IF @ysnRecap = 1
+BEGIN 
+	ROLLBACK TRAN @TransactionName
+	EXEC dbo.uspCMPostRecap @GLEntries
+	COMMIT TRAN @TransactionName
+END 
+
+--------------------------------------------------------------------------------------------  
+-- If RECAP is FALSE,
+-- 1. Book the G/L entries
+-- 2. Update the ysnPosted flag in the transaction. Increase the concurrency. 
+-- 3. Update the PO (if it exists)
+-- 4. Commit the save point 
+--------------------------------------------------------------------------------------------  
+IF @ysnRecap = 0
+BEGIN 
+	EXEC dbo.uspGLBookEntries @GLEntries, @ysnPost 
+
+	UPDATE	dbo.tblICInventoryTransfer  
+	SET		ysnPosted = @ysnPost
+			,intConcurrencyId = ISNULL(intConcurrencyId, 0) + 1
+	WHERE	strTransferNo = @strTransactionId  
+
+	COMMIT TRAN @TransactionName
+END 
+    
+-- This is our immediate exit in case of exceptions controlled by this stored procedure
+Post_Exit:
