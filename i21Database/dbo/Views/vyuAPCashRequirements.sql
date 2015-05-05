e8bbf3d1f7bc25455ecc3a0d09320574b123b0b2
Erick F. Dela Cruz <erick.delacruz@irely.com>
2015-05-05 17:38:49 +0800

    AP-306

---
+++ i21Database/dbo/Views/vyuAPCashRequirements.sql
@@ -0,0 +1,24 @@
+﻿CREATE VIEW [dbo].[vyuAPCashRequirements]
+WITH SCHEMABINDING
+AS
+SELECT
+	dbo.tblAPVendor.strVendorId,
+	--dbo.tblAPPayment.dtmDatePaid,
+	dbo.tblAPBill.strBillId,
+	dbo.tblAPBill.dtmDate,
+	dbo.tblAPBill.dtmDueDate,
+	dbo.tblAPBill.dtmDiscountDate,
+	dbo.tblAPPaymentDetail.dblPayment,
+	dbo.tblAPPaymentDetail.dblDiscount,
+	dbo.tblAPPayment.dblUnapplied,
+	dbo.tblAPPayment.dblWithheld,
+	dbo.tblAPPayment.dblAmountPaid
+FROM dbo.tblAPVendor
+INNER JOIN dbo.tblAPPayment
+ON tblAPPayment.intEntityVendorId = tblAPVendor.intEntityVendorId
+INNER JOIN dbo.tblAPPaymentDetail
+ON dbo.tblAPPayment.intPaymentId = dbo.tblAPPaymentDetail.intPaymentId
+INNER JOIN dbo.tblAPBill
+ON dbo.tblAPBill.intBillId = dbo.tblAPPaymentDetail.intBillId
+INNER JOIN dbo.tblAPBillBatch
+ON dbo.tblAPBill.intBillBatchId = dbo.tblAPBillBatch.intBillBatchId
