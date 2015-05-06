fe66dfab32711e92625cc753cc72c5b893ee866a
Lawrence Zabala <lawrence.zabala@irely.com>
2015-05-06 14:03:19 +0800

    IC-621

---
+++ i21Database/dbo/Stored Procedures/uspICProcessToBill.sql
@@ -0,0 +1,46 @@
+﻿CREATE PROCEDURE [dbo].[uspICProcessToBill]
+	@intReceiptId int,
+	@intUserId int,
+	@intBillId int OUTPUT
+AS
+SET QUOTED_IDENTIFIER OFF
+SET ANSI_NULLS ON
+SET NOCOUNT ON
+SET XACT_ABORT ON
+SET ANSI_WARNINGS OFF
+
+DECLARE @ErrorMessage NVARCHAR(4000);
+DECLARE @ErrorSeverity INT;
+DECLARE @ErrorState INT;
+
+BEGIN TRY
+	
+	CREATE TABLE #tmpBillIds (
+		[intBillId] [INT] PRIMARY KEY,
+		[intInventoryReceiptId] [INT]
+	)
+
+	INSERT INTO #tmpBillIds
+	EXEC dbo.[uspAPCreateBillFromIR] 
+		@intReceiptId,
+		@intUserId
+
+	SELECT TOP 1 @intBillId = intBillId FROM #tmpBillIds
+	DROP TABLE #tmpBillIds
+
+END TRY
+BEGIN CATCH
+	SELECT 
+		@ErrorMessage = ERROR_MESSAGE(),
+		@ErrorSeverity = ERROR_SEVERITY(),
+		@ErrorState = ERROR_STATE();
+
+	-- Use RAISERROR inside the CATCH block to return error
+	-- information about the original error that caused
+	-- execution to jump to the CATCH block.
+	RAISERROR (
+		@ErrorMessage, -- Message text.
+		@ErrorSeverity, -- Severity.
+		@ErrorState -- State.
+	);
+END CATCH
