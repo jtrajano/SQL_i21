dc6aa67641e5476fb4ffad3f10811699eb82ea3c
Dan Jalimao <Dan.Jalimao@irely.com>
2015-05-06 20:48:53 +0800

    FCOM-419

--- i21Database/dbo/Stored Procedures/uspSMDuplicateTaxCode.sql
+++ i21Database/dbo/Stored Procedures/uspSMDuplicateTaxCode.sql
@@ -4,12 +4,20 @@
 AS
 BEGIN
 
+	DECLARE @intCount NVARCHAR
+
+	SELECT @intCount = COUNT(*) FROM [tblSMTaxCode] WHERE [strTaxCode] LIKE 'DUP: ' + (SELECT [strTaxCode] FROM [dbo].[tblSMTaxCode] WHERE intTaxCodeId = @intTaxCodeId) + '%' 
+		--AND [strTaxCode] NOT LIKE '% DUP: ' + (SELECT [strTaxCode] FROM [dbo].[tblSMTaxCode] WHERE intTaxCodeId = @intTaxCodeId)
+
 	INSERT dbo.tblSMTaxCode([strTaxCode], [intTaxClassId], [strDescription], [strCalculationMethod], [numRate], 
 							[strTaxAgency], [strAddress], [strZipCode], [strState], [strCity], [strCountry], [strCounty], 
 							[intSalesTaxAccountId], [intPurchaseTaxAccountId], [strTaxableByOtherTaxes], [ysnCheckoffTax])
-	SELECT 'DUP: ' + [strTaxCode], [intTaxClassId], [strDescription], [strCalculationMethod], [numRate], 
-		   [strTaxAgency], [strAddress], [strZipCode], [strState], [strCity],[strCountry], [strCounty], 
-		   [intSalesTaxAccountId], [intPurchaseTaxAccountId], [strTaxableByOtherTaxes], [ysnCheckoffTax]
+	SELECT CASE @intCount WHEN 0 
+		   THEN 'DUP: ' + [strTaxCode] 
+		   ELSE 'DUP: ' + [strTaxCode] + ' (' + @intCount + ')' END,
+	[intTaxClassId], [strDescription], [strCalculationMethod], [numRate], 
+	[strTaxAgency], [strAddress], [strZipCode], [strState], [strCity],[strCountry], [strCounty], 
+	[intSalesTaxAccountId], [intPurchaseTaxAccountId], [strTaxableByOtherTaxes], [ysnCheckoffTax]
 	FROM dbo.tblSMTaxCode 
 	WHERE [intTaxCodeId] = @intTaxCodeId;
 	
