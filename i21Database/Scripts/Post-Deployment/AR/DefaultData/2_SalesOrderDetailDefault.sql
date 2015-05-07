3b2d59b6d0646f4739265c6055e6858175906016
Ruben  Escarcha <ruben.escarcha@irely.com>
2015-05-07 14:48:51 +0800

    AR-1037 Shipped Qty field on Sales Order

---
+++ i21Database/Scripts/Post-Deployment/AR/DefaultData/2_SalesOrderDetailDefault.sql
@@ -0,0 +1,13 @@
+﻿---------------------------------------------------------
+-- IMPORTANT NOTE: REMOVE THIS SCRIPT ON 1530
+-- Purpose: To default dblQtyShipped to Zero 
+---------------------------------------------------------
+print('/*******************  BEGIN Update NULL dblQtyShipped in tblSOSalesOrderDetail with zero  *******************/')
+GO
+
+UPDATE	dbo.tblSOSalesOrderDetail 
+SET		dblQtyShipped = 0.00
+WHERE	dblQtyShipped IS NULL 
+
+GO
+print('/*******************  END Update NULL dblQtyShipped in tblSOSalesOrderDetail with zero  *******************/')
