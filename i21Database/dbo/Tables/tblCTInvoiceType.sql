46b671696d76bd00e84cf999f00f632a72d11b68
Satya <ssahoo@BLRSATYAWIN7.irely.net>
2015-05-07 12:41:38 +0530

    CT - Data type change

--- i21Database/dbo/Tables/tblCTInvoiceType.sql
+++ i21Database/dbo/Tables/tblCTInvoiceType.sql
@@ -2,7 +2,7 @@
 	[intInvoiceTypeId] [int] NOT NULL,
 	[strInvoiceType] [nvarchar](30) COLLATE Latin1_General_CI_AS NOT NULL,
 	[strDescription] [nvarchar](60) COLLATE Latin1_General_CI_AS NOT NULL,
-	[ysnDefault] [int] NULL,
+	[ysnDefault] BIT NULL,
 	[intConcurrencyId] INT NOT NULL, 
 	CONSTRAINT [PK_tblCTInvoiceType_intInvoiceTypeId] PRIMARY KEY CLUSTERED ([intInvoiceTypeId] ASC)
 )
