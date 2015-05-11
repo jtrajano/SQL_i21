fcf6ddbf10181ab7b5bd3980ddabb095871a299c
Feb Montefrio <feb.montefrio@irely.com>
2015-05-11 17:56:57 +0800

    Fix issue in Split Lot Qty. The cost and qty are not calculated correctly.

--- i21Database/dbo/Stored Procedures/uspICPostInventoryAdjustmentSplitLotChange.sql
+++ i21Database/dbo/Stored Procedures/uspICPostInventoryAdjustmentSplitLotChange.sql
@@ -42,11 +42,36 @@ BEGIN
 	)
 	SELECT 	intItemId				= Detail.intItemId
 			,intItemLocationId		= ItemLocation.intItemLocationId
-			,intItemUOMId			= ItemUOM.intItemUOMId 
+			,intItemUOMId			=	-- Use weight UOM id if it is present. Otherwise, use the qty UOM. 
+										CASE	WHEN ISNULL(Detail.intWeightUOMId, 0) <> 0 THEN Detail.intWeightUOMId 
+												ELSE Detail.intItemUOMId 
+										END
 			,dtmDate				= Header.dtmAdjustmentDate
-			,dblQty					= ISNULL(Detail.dblNewQuantity, 0) - ISNULL(Detail.dblQuantity, 0)
-			,dblUOMQty				= ItemUOM.dblUnitQty
-			,dblCost				= Detail.dblCost
+			,dblQty					=	-- If using Weight UOM, convert the qty from Item UOM to Weight UOM. 
+										-- Otherwise, use the same value (New Quantity - Original Quantity). 
+										CASE	WHEN ISNULL(Detail.intWeightUOMId, 0) <> 0  THEN
+													dbo.fnCalculateQtyBetweenUOM(
+														Detail.intItemUOMId,
+														Detail.intWeightUOMId, 
+														ISNULL(Detail.dblNewQuantity, 0) - ISNULL(Detail.dblQuantity, 0)
+													)
+												ELSE 
+													ISNULL(Detail.dblNewQuantity, 0) - ISNULL(Detail.dblQuantity, 0)
+										END 
+			,dblUOMQty				=	-- If using Weight UOM, use the Unit Qty from the Weight UOM. 
+										-- Otherwise, use the unit qty from the Item UOM. 
+										CASE	WHEN ISNULL(Detail.intWeightUOMId, 0) <> 0  THEN
+													WeightUOM.dblUnitQty
+												ELSE 
+													ItemUOM.dblUnitQty
+										END 				
+			,dblCost				=	-- If using Weight UOM, use the same cost. This is the cost from the Lot costing bucket. 
+										-- Otherwise, adjustment needs to calculate the cost by the Item UOM > Unit Qty. 
+										CASE	WHEN ISNULL(Detail.intWeightUOMId, 0) <> 0  THEN
+													Detail.dblCost
+												ELSE 
+													Detail.dblCost * ItemUOM.dblUnitQty
+										END
 			,dblSalesPrice			= 0
 			,intCurrencyId			= NULL 
 			,dblExchangeRate		= 1
@@ -64,6 +89,9 @@ BEGIN
 			LEFT JOIN dbo.tblICItemUOM ItemUOM
 				ON ItemUOM.intItemUOMId = Detail.intItemUOMId
 				AND ItemUOM.intItemId = Detail.intItemId
+			LEFT JOIN dbo.tblICItemUOM WeightUOM
+				ON WeightUOM.intItemUOMId = Detail.intWeightUOMId
+				AND WeightUOM.intItemId = Detail.intItemId				
 	WHERE	Header.intInventoryAdjustmentId = @intTransactionId
 			AND Detail.dblNewQuantity IS NOT NULL 
 			AND ISNULL(Detail.dblNewQuantity, 0) - ISNULL(Detail.dblQuantity, 0) <> 0 
@@ -110,27 +138,87 @@ BEGIN
 	)
 	SELECT 	intItemId				= Detail.intItemId
 			,intItemLocationId		= ItemLocation.intItemLocationId
-			,intItemUOMId			= ItemUOM.intItemUOMId 
-			,dtmDate				= Header.dtmAdjustmentDate
-			,dblQty					= 
-										CASE	WHEN ISNULL(Detail.dblNewSplitLotQuantity, 0) = 0 THEN Detail.dblAdjustByQuantity * -1
-												ELSE Detail.dblNewSplitLotQuantity
-										END			
-			,dblUOMQty				= ItemUOM.dblUnitQty
-			,dblCost				= 
-										CASE	WHEN ISNULL(Detail.dblNewSplitLotQuantity, 0) = 0 THEN 
-													CASE	WHEN ISNULL(Detail.dblNewCost, 0) = 0 THEN Detail.dblCost
-															ELSE Detail.dblNewCost
-													END 
+			,intItemUOMId			=	-- If there is a weight UOM, try to use the new weight UOM.
+										-- Otherwise, try to use the new item UOM. 		
+										CASE	WHEN Detail.intWeightUOMId IS NOT NULL THEN 
+													ISNULL(Detail.intNewWeightUOMId, Detail.intWeightUOMId)
 												ELSE 
-													CASE	WHEN ISNULL(Detail.dblNewCost, 0) = 0 THEN	
-																-- Redistribute the cost from the qty to adjust to the Qty of the Split lot. 
-																ABS(Detail.dblCost * Detail.dblAdjustByQuantity / Detail.dblNewSplitLotQuantity)		
-															ELSE
-																-- The user manually computed the new cost for the split lot.  
-																Detail.dblNewCost
+													ISNULL(Detail.intNewItemUOMId, Detail.intItemUOMId) 
+										END
+
+			,dtmDate				= Header.dtmAdjustmentDate
+			,dblQty					= CASE	WHEN Detail.intWeightUOMId IS NOT NULL THEN 
+													CASE	
+															WHEN Detail.dblNewWeight IS NOT NULL THEN 
+																-- Use the new net weight. 
+																ISNULL(Detail.dblNewWeight, 0) 
+															ELSE 
+																-- If there is no new weight, calculate as: {Weight per Qty} x {Adjust by Qty}. 
+																-- And then the result is converted into the new weight UOM. 
+																dbo.fnCalculateQtyBetweenUOM(
+																	Detail.intWeightUOMId,
+																	ISNULL(Detail.intNewWeightUOMId, Detail.intWeightUOMId), 
+																	ABS(ISNULL(Detail.dblWeightPerQty, 0) * (ISNULL(Detail.dblNewQuantity, 0) - ISNULL(Detail.dblQuantity, 0)))
+																)
 													END 													
+												WHEN Detail.dblNewSplitLotQuantity IS NOT NULL THEN 
+													Detail.dblNewSplitLotQuantity
+												ELSE 
+													-1 * (ISNULL(Detail.dblNewQuantity, 0) - ISNULL(Detail.dblQuantity, 0))
 										END 
+
+			,dblUOMQty				=	-- If there is a weight UOM, try to use the new weight UOM > unit qty. 
+										-- Otherwise, try to use the new item UOM > unit qty. 
+										CASE	WHEN Detail.intWeightUOMId IS NOT NULL THEN 
+													ISNULL(NewWeightUOM.dblUnitQty, WeightUOM.dblUnitQty)
+												ELSE 
+													ISNULL(NewItemUOM.dblUnitQty, ItemUOM.dblUnitQty)
+										END
+
+			,dblCost				=	
+										CASE	
+												WHEN Detail.intWeightUOMId IS NOT NULL THEN 
+													-- If Weight is used, use the Cost per Weight. Otherwise, use the cost per qty. 
+													dbo.fnCalculateCostPerWeight (
+														-- 1 of 2. Calculate the overall item value according to the (new or original) cost and original net weight. 
+														ISNULL(Detail.dblNewCost, Detail.dblCost) 
+														* ABS(ISNULL(Detail.dblWeightPerQty, 0) * (ISNULL(Detail.dblNewQuantity, 0) - ISNULL(Detail.dblQuantity, 0)))
+
+														-- 2 of 2. Determine the weight to use (either new or original). 
+														,ISNULL(
+															Detail.dblNewWeight, 
+															dbo.fnCalculateQtyBetweenUOM(
+																Detail.intWeightUOMId,
+																ISNULL(Detail.intNewWeightUOMId, Detail.intWeightUOMId), 
+																ABS(ISNULL(Detail.dblWeightPerQty, 0) * (ISNULL(Detail.dblNewQuantity, 0) - ISNULL(Detail.dblQuantity, 0)))
+															)													
+														) 
+													) 
+										
+												WHEN Detail.dblNewSplitLotQuantity IS NOT NULL THEN 
+													-- Distribute the value over the new split lot qty. 
+													CASE	WHEN Detail.dblNewSplitLotQuantity = 0 THEN 
+																ISNULL(Detail.dblNewCost, Detail.dblCost)
+															WHEN Detail.intWeightUOMId IS NOT NULL THEN 
+																-- If there is weight uom, 
+																ABS (
+																	ISNULL(Detail.dblNewCost, Detail.dblCost) 
+																	* ABS(ISNULL(Detail.dblWeightPerQty, 0) 
+																	* (ISNULL(Detail.dblNewQuantity, 0) - ISNULL(Detail.dblQuantity, 0)))
+																	/ Detail.dblNewSplitLotQuantity																
+																)
+																
+															ELSE 
+																ISNULL(Detail.dblNewCost, Detail.dblCost)
+																* (ISNULL(Detail.dblNewQuantity, 0) - ISNULL(Detail.dblQuantity, 0))
+																/ Detail.dblNewSplitLotQuantity
+													END
+												ELSE 
+													-- Otherwise, recalculate the (new or original) cost to the (new or original) Item UOM unit qty. 
+													ISNULL(Detail.dblNewCost, Detail.dblCost)
+													* ISNULL(NewItemUOM.dblUnitQty, ItemUOM.dblUnitQty)
+										END 
+
 			,dblValue				= 0
 			,dblSalesPrice			= 0
 			,intCurrencyId			= NULL 
@@ -147,7 +235,17 @@ BEGIN
 				ON ItemLocation.intLocationId = Header.intLocationId 
 				AND ItemLocation.intItemId = Detail.intItemId
 			LEFT JOIN dbo.tblICItemUOM ItemUOM
-				ON ItemUOM.intItemUOMId = ISNULL(Detail.intNewItemUOMId, Detail.intItemUOMId)
+				ON ItemUOM.intItemUOMId = Detail.intItemUOMId
+				AND ItemUOM.intItemId = Detail.intItemId
+			LEFT JOIN dbo.tblICItemUOM NewItemUOM
+				ON NewItemUOM.intItemUOMId = Detail.intNewItemUOMId
+				AND NewItemUOM.intItemId = Detail.intItemId
+			LEFT JOIN dbo.tblICItemUOM WeightUOM
+				ON WeightUOM.intItemUOMId = Detail.intWeightUOMId
+				AND WeightUOM.intItemId = Detail.intItemId
+			LEFT JOIN dbo.tblICItemUOM NewWeightUOM
+				ON NewWeightUOM.intItemUOMId = Detail.intNewWeightUOMId
+				AND NewWeightUOM.intItemId = Detail.intItemId
 	WHERE	Header.intInventoryAdjustmentId = @intTransactionId
 			AND Detail.dblNewQuantity IS NOT NULL 
 			AND ISNULL(Detail.dblNewQuantity, 0) - ISNULL(Detail.dblQuantity, 0) <> 0 
