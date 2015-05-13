d2388686591a35123d30a792e0b1b3132cdd95a9
Erick F. Dela Cruz <erick.delacruz@irely.com>
2015-05-13 14:52:24 +0800

    AP-1164

--- i21Database/dbo/Stored Procedures/uspAPPostPayment.sql
+++ i21Database/dbo/Stored Procedures/uspAPPostPayment.sql
@@ -197,7 +197,7 @@ BEGIN
 		
 		--Unposting Process
 		UPDATE tblAPPaymentDetail
-		SET tblAPPaymentDetail.dblAmountDue = (CASE WHEN B.dblAmountDue = 0 THEN B.dblDiscount + C.dblAmountDue + B.dblPayment ELSE (B.dblAmountDue + B.dblPayment) END)
+		SET tblAPPaymentDetail.dblAmountDue = (CASE WHEN B.dblAmountDue = 0 THEN B.dblDiscount + B.dblPayment - B.dblInterest ELSE (B.dblAmountDue + B.dblPayment) END)
 		FROM tblAPPayment A
 			LEFT JOIN tblAPPaymentDetail B
 				ON A.intPaymentId = B.intPaymentId
