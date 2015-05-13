ed4bf7001024f1f3314e7bdefb6ebc51cfed2fce
Satya <ssahoo@BLRSATYAWIN7.irely.net>
2015-05-13 21:08:32 +0530

    tblCTPosition change

--- i21Database/dbo/Tables/tblCTPosition.sql
+++ i21Database/dbo/Tables/tblCTPosition.sql
@@ -1,9 +1,8 @@
 ﻿CREATE TABLE [dbo].[tblCTPosition](
 	[intPositionId] [int] IDENTITY(1,1) NOT NULL,
 	[strPosition] [nvarchar](30) COLLATE Latin1_General_CI_AS NOT NULL,
-	[strDescription] [nvarchar](100) COLLATE Latin1_General_CI_AS NOT NULL,
+	[strPositionType] [nvarchar](100) COLLATE Latin1_General_CI_AS NOT NULL,
 	[ysnDefault] [bit] NULL,
-	[intSequence] [int] NULL,
 	[intConcurrencyId] INT NOT NULL, 
 	CONSTRAINT [PK_tblCTPosition_intPositionId] PRIMARY KEY CLUSTERED ([intPositionId] ASC)
 )
