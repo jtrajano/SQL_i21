21cffd3c6efab6eacbfe4d7f7056601f8f85c0aa
Bonn Paolo Ponce <bonn.ponce@irely.com>
2015-05-13 11:41:00 +0800

    CF-260 Add card fueling tables on SSDT project Fixed CONSTRAINTS issue

--- i21Database/dbo/Tables/tblCFindexPricingBySiteGroup.sql
+++ i21Database/dbo/Tables/tblCFindexPricingBySiteGroup.sql
@@ -4,7 +4,7 @@
     [intARItemID]                        INT             NULL,
     [intTime]                            INT             NULL,
     [dblIndexPrice]                      NUMERIC (18, 6) NULL,
-    [intConcurrencyId]                   INT             CONSTRAINT [DF_tblCFindexPricingBySiteGroup_intConcurrencyId] DEFAULT ((1)) NULL,
+    [intConcurrencyId]                   INT             CONSTRAINT [DF_tblCFIndexPricingBySiteGroup_intConcurrencyId] DEFAULT ((1)) NULL,
     CONSTRAINT [PK_tblCFIndexPricingBySiteGroup] PRIMARY KEY CLUSTERED ([intIndexPricingBySiteGroupId] ASC),
     CONSTRAINT [FK_tblCFIndexPricingBySiteGroup_tblCFIndexPricingBySiteGroupHeader] FOREIGN KEY ([intIndexPricingBySiteGroupHeaderId]) REFERENCES [dbo].[tblCFIndexPricingBySiteGroupHeader] ([intIndexPricingBySiteGroupHeaderId]) ON DELETE CASCADE
 );
