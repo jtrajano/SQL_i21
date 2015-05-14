f0f880b39833aea148deb9a4e5d2e67d642e239b
von.pasion <von.pasion@irely.com>
2015-05-14 14:28:36 +0800

    FRM-2156

--- i21Database/dbo/Tables/cstEntity.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[cstEntity]
-(
-	[intId] INT NOT NULL,
-    CONSTRAINT [PK_cstEntity] PRIMARY KEY CLUSTERED ([intId] ASC),
-    CONSTRAINT [FK_cstEntity_tblEntity] FOREIGN KEY ([intId]) REFERENCES [dbo].[tblEntity] ([intEntityId]) ON DELETE CASCADE
-)
--- i21Database/dbo/Tables/cstGLAccount.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE TABLE [dbo].[cstGLAccount] (
-    [intId] INT NOT NULL,
-    CONSTRAINT [PK_cstGLAccount] PRIMARY KEY CLUSTERED ([intId] ASC),
-    CONSTRAINT [FK_cstGLAccount.tblGLAccount_intAccountId] FOREIGN KEY ([intId]) REFERENCES [dbo].[tblGLAccount] ([intAccountId]) ON DELETE CASCADE
-);
