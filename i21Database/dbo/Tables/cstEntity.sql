c87fcbfe3563b379de06b4489919372101c7c4e4
Mon Gonzales <mon.gonzales@irely.com>
2015-05-14 12:56:43 +0800

    EM-11

---
+++ i21Database/dbo/Tables/cstEntity.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[cstEntity]
+(
+	[intId] INT NOT NULL,
+    CONSTRAINT [PK_cstEntity] PRIMARY KEY CLUSTERED ([intId] ASC),
+    CONSTRAINT [FK_cstEntity_tblEntity] FOREIGN KEY ([intId]) REFERENCES [dbo].[tblEntity] ([intEntityId]) ON DELETE CASCADE
+)
