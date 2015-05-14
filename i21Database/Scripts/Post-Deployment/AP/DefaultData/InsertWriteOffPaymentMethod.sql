96581807660522855c2d0ae02e79d013ef6c3fc1
Erick F. Dela Cruz <erick.delacruz@irely.com>
2015-05-14 16:43:51 +0800

    AP-1158

--- i21Database/Scripts/Post-Deployment/AP/DefaultData/InsertWriteOffPaymentMethod.sql
+++ i21Database/Scripts/Post-Deployment/AP/DefaultData/InsertWriteOffPaymentMethod.sql
@@ -2,12 +2,14 @@
 MERGE INTO tblSMPaymentMethod AS Target
 USING 
 (
-VALUES (N'Write Off', 1)
+VALUES 
+	(N'Write Off', 1),
+	(N'Debit memos and Payments', 1)
 )
 AS Source (strPaymentMethod, ysnActive)
 ON Target.strPaymentMethod = Source.strPaymentMethod 
 -- insert new rows
-WHEN NOT MATCHED BY TARGET THEN
-INSERT (strPaymentMethod, ysnActive)
-VALUES (strPaymentMethod, ysnActive);
+WHEN NOT MATCHED BY TARGET 
+	THEN INSERT (strPaymentMethod, ysnActive) VALUES (strPaymentMethod, ysnActive)
+WHEN MATCHED AND Target.strPaymentMethod = 'Write Off' THEN DELETE;
 GO
