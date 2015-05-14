251aac24cd4558d9b3c9c49161b41da899b9bdbf
Ruben  Escarcha <ruben.escarcha@irely.com>
2015-05-14 14:59:58 +0800

    AR-1039 Customer Statement for 15.2

---
+++ i21Database/dbo/Views/vyuARReceivables.sql
@@ -0,0 +1,60 @@
+﻿CREATE VIEW vyuARReceivables
+AS 
+SELECT 
+	 I.[dtmDate]									AS [dtmDate] 
+	,I.[intInvoiceId]								AS [intInvoiceId]
+	,I.[strInvoiceNumber]							AS [strInvoiceNumber]
+	,0.00											AS [dblAmountPaid] 
+	,ISNULL(I.[dblInvoiceTotal],0)					AS [dblTotal] 
+	,I.[dblAmountDue]								AS [dblAmountDue] 
+	,0.00											AS [dblDiscount] 
+	,C.[strCustomerNumber]							AS [strCustomerNumber]
+	,ISNULL(RTRIM(LTRIM(C.[strCustomerNumber])),'') 
+		+ ' - ' + ISNULL(E.[strName],'')			AS [strCustomerIdName] 
+	,I.[dtmDueDate]									AS [dtmDueDate]
+	,I.[ysnPosted]									AS [ysnPosted]								
+	,I.[ysnPaid]									AS [ysnPaid]
+FROM
+	tblARInvoice I
+INNER JOIN
+	tblARCustomer C
+		ON C.[intEntityCustomerId] = I.[intEntityCustomerId]
+INNER JOIN 
+	tblEntity E 
+		ON C.[intEntityCustomerId] = E.[intEntityId]
+WHERE
+	I.[ysnPosted] = 1
+
+UNION ALL
+   
+SELECT 
+	 P.[dtmDatePaid]								AS [dtmDate] 
+	,PD.[intInvoiceId]								AS [intInvoiceId]
+	,I.[strInvoiceNumber]							AS [strInvoiceNumber]
+	,PD.[dblPayment]								AS [dblAmountPaid] 
+	,0.00											AS [dblTotal] 
+	,0.00											AS [dblAmountDue] 
+	,PD.[dblDiscount]								AS [dblDiscount] 
+	,C.[strCustomerNumber]							AS [strCustomerNumber]
+	,ISNULL(RTRIM(LTRIM(C.[strCustomerNumber])),'') 
+		+ ' - ' + ISNULL(E.[strName],'')			AS [strCustomerIdName] 
+	,I.[dtmDueDate]									AS [dtmDueDate]
+	,I.[ysnPosted]									AS [ysnPosted]				
+	,I.[ysnPaid]									AS [ysnPaid]
+FROM
+	tblARPayment  P
+INNER JOIN
+	tblARCustomer C
+		ON P.[intEntityCustomerId] = C.[intEntityCustomerId] 
+INNER JOIN
+	tblEntity E
+		ON C.[intEntityCustomerId] = E.[intEntityId] 			
+LEFT JOIN 
+	tblARPaymentDetail PD
+		ON P.[intPaymentId] = PD.[intPaymentId]
+LEFT JOIN
+	tblARInvoice I
+	ON PD.[intInvoiceId] = I.[intInvoiceId]
+WHERE
+	P.[ysnPosted] = 1  
+	AND I.[ysnPosted] = 1
