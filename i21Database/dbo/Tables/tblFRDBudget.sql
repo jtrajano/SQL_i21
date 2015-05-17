45fff56873eda19f269ca15c1b8da8f23896fca3
axel_casauran <axel.casauran@irely.com>
2015-05-18 07:23:52 +0800

    FRD-524

---
+++ i21Database/dbo/Tables/tblFRDBudget.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE [dbo].[tblFRDBudget] (
+    [intBudgetId]       INT             IDENTITY (1, 1) NOT NULL,
+    [intBudgetCode]     INT             NOT NULL,
+    [strPeriod]         NVARCHAR (50)   COLLATE Latin1_General_CI_AS NOT NULL,
+    [dtmStartDate]      DATETIME        NULL,
+    [intFiscalYearId]   INT             NOT NULL,
+    [intAccountId]      INT             NOT NULL,
+    [intAccountGroupId] INT             NULL,
+    [curActual]         NUMERIC (18, 6) NULL,
+    [intSort]           INT             NULL,
+    [dtmEndDate]        DATETIME        NULL,
+    [curThisYear]       NUMERIC (18, 6) NULL,
+    [dtmDate]           DATETIME        NOT NULL,
+    [curLastYear]       NUMERIC (18, 6) NULL,
+    [curBudget]         NUMERIC (18, 6) NULL,
+    [curOperPlan]       NUMERIC (18, 6) NULL,
+	[ysnSelect]         BIT             NULL,
+    [intConcurrencyId]  INT             DEFAULT 1 NOT NULL,    
+    CONSTRAINT [PK_tblFRDBudget] PRIMARY KEY CLUSTERED ([intBudgetId] ASC),
+    CONSTRAINT [FK_tblFRDBudget_tblGLAccount] FOREIGN KEY ([intAccountId]) REFERENCES [dbo].[tblGLAccount] ([intAccountId]),
+    CONSTRAINT [FK_tblFRDBudget_tblFRDBudgetCode] FOREIGN KEY ([intBudgetCode]) REFERENCES [dbo].[tblFRDBudgetCode] ([intBudgetCode]),
+    CONSTRAINT [FK_tblFRDBudget_tblGLFiscalYear] FOREIGN KEY ([intFiscalYearId]) REFERENCES [dbo].[tblGLFiscalYear] ([intFiscalYearId])
+);
+
---
+++ i21Database/dbo/Tables/tblFRDBudgetCode.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[tblFRDBudgetCode] (
+    [intBudgetCode]               INT            IDENTITY (1, 1) NOT NULL,
+    [ysnDefault]                  BIT            DEFAULT ((0)) NULL,
+    [strBudgetCode]               NVARCHAR (40)  COLLATE Latin1_General_CI_AS NULL,
+    [strBudgetEnglishDescription] NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NULL,
+    [intConcurrencyId]            INT            DEFAULT 1 NOT NULL,
+    CONSTRAINT [PK_tblFRDBudgetCode] PRIMARY KEY CLUSTERED ([intBudgetCode] ASC)
+);
+
