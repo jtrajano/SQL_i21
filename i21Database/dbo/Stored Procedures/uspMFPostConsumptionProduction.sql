42b65c1b021c3204c32d87a1a97ea784752bcfba
Premkumar S <prem.selladurai@irely.com>
2015-05-18 19:48:07 +0530

    Bag Off GL Issue corrected.

---
+++ i21Database/dbo/Stored Procedures/uspMFPostConsumptionProduction.sql
@@ -0,0 +1,281 @@
+﻿CREATE PROCEDURE [dbo].[uspMFPostConsumptionProduction] @intWorkOrderId INT
+	,@strLotNumber NVARCHAR(50)
+	,@dblWeight NUMERIC(18, 6)
+	,@intWeightUOMId INT
+	,@dblUnitQty NUMERIC(18, 6) = NULL
+	,@dblQty NUMERIC(18, 6)
+	,@intItemUOMId INT
+	,@intUserId INT = NULL
+	,@intLotId INT OUTPUT
+AS
+BEGIN
+	SET QUOTED_IDENTIFIER OFF
+	SET ANSI_NULLS ON
+	SET NOCOUNT ON
+	SET XACT_ABORT ON
+	SET ANSI_WARNINGS OFF
+
+	DECLARE @STARTING_NUMBER_BATCH AS INT = 3
+		,@INVENTORY_CONSUME AS INT = 8
+		,@INVENTORY_PRODUCE AS INT = 9
+		,@ItemsThatNeedLotId AS dbo.ItemLotTableType
+		,@ItemsForPost AS ItemCostingTableType
+		,@GLEntries AS RecapTableType
+		,@intTransactionId AS INT
+		,@strTransactionId NVARCHAR(50)
+		,@strBatchId NVARCHAR(40)
+		,@intItemLocationId INT
+		,@intItemId INT
+		,@strItemNo AS NVARCHAR(50)
+		,@intLocationId INT
+		,@intSubLocationId INT
+		,@intStorageLocationId INT
+		,@dblNewCost NUMERIC(18, 6)
+		,@dblNewUnitCost NUMERIC(18, 6)
+		,@strLifeTimeType NVARCHAR(50)
+		,@intLifeTime INT
+		,@dtmExpiryDate DATETIME
+
+	SELECT TOP 1 @intTransactionId = intWorkOrderId
+		,@strTransactionId = strWorkOrderNo
+		,@intItemId = intItemId
+		,@intLocationId = intLocationId
+		,@intSubLocationId = intSubLocationId
+		,@intStorageLocationId = intStorageLocationId
+	FROM dbo.tblMFWorkOrder
+	WHERE intWorkOrderId = @intWorkOrderId
+
+	EXEC dbo.uspSMGetStartingNumber @STARTING_NUMBER_BATCH
+		,@strBatchId OUTPUT
+
+	INSERT INTO @ItemsForPost (
+		intItemId
+		,intItemLocationId
+		,intItemUOMId
+		,dtmDate
+		,dblQty
+		,dblUOMQty
+		,dblCost
+		,dblSalesPrice
+		,intCurrencyId
+		,dblExchangeRate
+		,intTransactionId
+		,strTransactionId
+		,intTransactionTypeId
+		,intLotId
+		,intSubLocationId
+		,intStorageLocationId
+		)
+	SELECT intItemId = l.intItemId
+		,intItemLocationId = l.intItemLocationId
+		,intItemUOMId = ISNULL(l.intWeightUOMId, l.intItemUOMId)
+		,dtmDate = GetDate()
+		,dblQty = (- cl.dblQuantity)
+		,dblUOMQty = ItemUOM.dblUnitQty
+		,dblCost = l.dblLastCost
+		,dblSalesPrice = 0
+		,intCurrencyId = NULL
+		,dblExchangeRate = 1
+		,intTransactionId = @intTransactionId
+		,strTransactionId = @strTransactionId
+		,intTransactionTypeId = @INVENTORY_CONSUME
+		,intLotId = l.intLotId
+		,intSubLocationId = l.intSubLocationId
+		,intStorageLocationId = l.intStorageLocationId
+	FROM tblMFWorkOrderConsumedLot cl
+	INNER JOIN tblICLot l ON cl.intLotId = l.intLotId
+	INNER JOIN dbo.tblICItemUOM ItemUOM ON cl.intItemUOMId = ItemUOM.intItemUOMId
+	WHERE cl.intWorkOrderId = @intTransactionId
+
+	EXEC dbo.uspICPostCosting @ItemsForPost
+		,@strBatchId
+		,NULL
+		,@intUserId
+
+	SELECT @intItemLocationId = intItemLocationId
+	FROM tblICItemLocation
+	WHERE intLocationId = @intLocationId
+		AND intItemId = @intItemId
+
+	SELECT @dblNewCost = [dbo].[fnGetTotalStockValueFromTransactionBatch](@intWorkOrderId, @strBatchId)
+
+	SET @dblNewCost = ABS(@dblNewCost)
+	SET @dblNewUnitCost = ABS(@dblNewCost) / @dblQty
+
+	CREATE TABLE #GeneratedLotItems (
+		intLotId INT
+		,strLotNumber NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL
+		,intDetailId INT
+		)
+
+	SELECT @strLifeTimeType = strLifeTimeType
+		,@intLifeTime = intLifeTime
+	FROM dbo.tblICItem
+	WHERE intItemId = @intItemId
+
+	IF @strLifeTimeType = 'Years'
+		SET @dtmExpiryDate = DateAdd(yy, @intLifeTime, GetDate())
+	ELSE IF @strLifeTimeType = 'Months'
+		SET @dtmExpiryDate = DateAdd(mm, @intLifeTime, GetDate())
+	ELSE IF @strLifeTimeType = 'Days'
+		SET @dtmExpiryDate = DateAdd(dd, @intLifeTime, GetDate())
+	ELSE IF @strLifeTimeType = 'Hours'
+		SET @dtmExpiryDate = DateAdd(hh, @intLifeTime, GetDate())
+	ELSE IF @strLifeTimeType = 'Minutes'
+		SET @dtmExpiryDate = DateAdd(mi, @intLifeTime, GetDate())
+	ELSE
+		SET @dtmExpiryDate = DateAdd(yy, 1, GetDate())
+
+	INSERT INTO @ItemsThatNeedLotId (
+		intLotId
+		,strLotNumber
+		,strLotAlias
+		,intItemId
+		,intItemLocationId
+		,intSubLocationId
+		,intStorageLocationId
+		,dblQty
+		,intItemUOMId
+		,dblWeight
+		,intWeightUOMId
+		,dtmExpiryDate
+		,dtmManufacturedDate
+		,intOriginId
+		,strBOLNo
+		,strVessel
+		,strReceiptNumber
+		,strMarkings
+		,strNotes
+		,intEntityVendorId
+		,strVendorLotNo
+		,intVendorLocationId
+		,strVendorLocation
+		,intDetailId
+		,ysnProduced
+		)
+	SELECT intLotId = NULL
+		,strLotNumber = @strLotNumber
+		,strLotAlias = NULL
+		,intItemId = @intItemId
+		,intItemLocationId = @intItemLocationId
+		,intSubLocationId = @intSubLocationId
+		,intStorageLocationId = @intStorageLocationId
+		,dblQty = @dblQty
+		,intItemUOMId = @intItemUOMId
+		,dblWeight = @dblWeight
+		,intWeightUOMId = @intWeightUOMId
+		,dtmExpiryDate = @dtmExpiryDate
+		,dtmManufacturedDate = GetDate()
+		,intOriginId = NULL
+		,strBOLNo = NULL
+		,strVessel = NULL
+		,strReceiptNumber = NULL
+		,strMarkings = NULL
+		,strNotes = NULL
+		,intEntityVendorId = NULL
+		,strVendorLotNo = NULL
+		,intVendorLocationId = NULL
+		,strVendorLocation = NULL
+		,intDetailId = @intWorkOrderId
+		,ysnProduced = 1
+
+	EXEC dbo.uspICCreateUpdateLotNumber @ItemsThatNeedLotId
+		,@intUserId
+
+	SELECT TOP 1 @intLotId = intLotId
+	FROM #GeneratedLotItems
+	WHERE intDetailId = @intWorkOrderId
+
+	DELETE
+	FROM @ItemsForPost
+
+	INSERT INTO @ItemsForPost (
+		intItemId
+		,intItemLocationId
+		,intItemUOMId
+		,dtmDate
+		,dblQty
+		,dblUOMQty
+		,dblCost
+		,dblSalesPrice
+		,intCurrencyId
+		,dblExchangeRate
+		,intTransactionId
+		,strTransactionId
+		,intTransactionTypeId
+		,intLotId
+		,intSubLocationId
+		,intStorageLocationId
+		)
+	SELECT intItemId = @intItemId
+		,intItemLocationId = @intItemLocationId
+		,intItemUOMId = @intItemUOMId
+		,dtmDate = GetDate()
+		,dblQty = @dblQty
+		,dblUOMQty = CASE 
+			WHEN (@intWeightUOMId = @intItemUOMId)
+				THEN (
+						SELECT 1
+						)
+			ELSE (
+					CASE 
+						WHEN @dblUnitQty IS NOT NULL
+							THEN @dblUnitQty
+						ELSE (
+								SELECT TOP 1 dblUnitQty
+								FROM dbo.tblICItemUOM
+								WHERE intItemUOMId = @intItemUOMId
+								)
+						END
+					)
+			END
+		,dblCost = @dblNewUnitCost
+		,dblSalesPrice = 0
+		,intCurrencyId = NULL
+		,dblExchangeRate = 1
+		,intTransactionId = @intTransactionId
+		,strTransactionId = @strTransactionId
+		,intTransactionTypeId = @INVENTORY_PRODUCE
+		,intLotId = @intLotId
+		,intSubLocationId = @intSubLocationId
+		,intStorageLocationId = @intStorageLocationId
+
+	EXEC dbo.uspICPostCosting @ItemsForPost
+		,@strBatchId
+		,NULL
+		,@intUserId
+
+	INSERT INTO @GLEntries (
+		[dtmDate]
+		,[strBatchId]
+		,[intAccountId]
+		,[dblDebit]
+		,[dblCredit]
+		,[dblDebitUnit]
+		,[dblCreditUnit]
+		,[strDescription]
+		,[strCode]
+		,[strReference]
+		,[intCurrencyId]
+		,[dblExchangeRate]
+		,[dtmDateEntered]
+		,[dtmTransactionDate]
+		,[strJournalLineDescription]
+		,[intJournalLineNo]
+		,[ysnIsUnposted]
+		,[intUserId]
+		,[intEntityId]
+		,[strTransactionId]
+		,[intTransactionId]
+		,[strTransactionType]
+		,[strTransactionForm]
+		,[strModuleName]
+		,[intConcurrencyId]
+		)
+	EXEC dbo.uspICCreateGLEntries @strBatchId
+		,NULL
+		,@intUserId
+
+	EXEC dbo.uspGLBookEntries @GLEntries
+		,1
+END
