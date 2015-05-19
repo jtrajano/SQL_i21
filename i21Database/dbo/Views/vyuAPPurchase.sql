4d2328f6b8b036e1ed4405fb9d6f9aa00fa9cd58
Erick F. Dela Cruz <erick.delacruz@irely.com>
2015-05-19 11:50:26 +0800

    AP-1134

--- i21Database/dbo/Views/vyuAPPurchase.sql
+++ i21Database/dbo/Views/vyuAPPurchase.sql
@@ -9,9 +9,11 @@ A.intPurchaseId
 ,A.strVendorOrderNumber
 ,A.strPurchaseOrderNumber
 ,B.strVendorId 
+,B1.strName
 ,C.strStatus
 ,D.strLocationName
 FROM dbo.tblPOPurchase A
 	INNER JOIN dbo.tblAPVendor B ON A.[intEntityVendorId] = B.[intEntityVendorId]
+	INNER JOIN dbo.tblEntity B1 ON B.intEntityVendorId = B1.intEntityId
 	INNER JOIN dbo.tblPOOrderStatus C ON A.intOrderStatusId = C.intOrderStatusId
 	INNER JOIN dbo.tblSMCompanyLocation D ON A.intShipToId = D.intCompanyLocationId
