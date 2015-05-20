165cf19ea0c06d2cb31596da64ba3cc91be4883e
Ruben  Escarcha <ruben.escarcha@irely.com>
2015-05-20 14:36:36 +0800

    AR-1115 No Service Charge Account error even if this account is already setup

--- i21Database/dbo/Views/vyuARGetItemAccount.sql
+++ i21Database/dbo/Views/vyuARGetItemAccount.sql
@@ -1,14 +1,39 @@
 ﻿CREATE VIEW [dbo].[vyuARGetItemAccount]
 AS
 SELECT     
-Item.intItemId, 
-Item.strItemNo,
-Item.strType,
-ItemLocation.intLocationId,
-dbo.fnGetItemBaseGLAccount(Item.intItemId, ItemLocation.intLocationId, N'Service Charges') AS intAccountId, 
-dbo.fnGetItemBaseGLAccount(Item.intItemId, ItemLocation.intLocationId, N'Cost of Goods') AS intCOGSAccountId, 
-dbo.fnGetItemBaseGLAccount(Item.intItemId, ItemLocation.intLocationId, N'Sales Account') AS intSalesAccountId, 
-dbo.fnGetItemBaseGLAccount(Item.intItemId, ItemLocation.intLocationId, N'Inventory') AS intInventoryAccountId
+	I.intItemId, 
+	I.strItemNo,
+	I.strType,
+	IL.intLocationId,
+	(CASE WHEN ISNULL(dbo.fnGetItemBaseGLAccount(I.intItemId, IL.intLocationId, N'Service Charges'),0) = 0
+		THEN 
+			(CASE WHEN CL.intServiceCharges = 0 THEN NULL ELSE CL.intServiceCharges END)
+		ELSE
+			dbo.fnGetItemBaseGLAccount(I.intItemId, IL.intLocationId, N'Service Charges')				
+	END) AS intAccountId, 
+	(CASE WHEN ISNULL(dbo.fnGetItemBaseGLAccount(I.intItemId, IL.intLocationId, N'Cost of Goods'),0) = 0
+		THEN 
+			(CASE WHEN CL.intCostofGoodsSold = 0 THEN NULL ELSE CL.intCostofGoodsSold END)
+		ELSE
+			dbo.fnGetItemBaseGLAccount(I.intItemId, IL.intLocationId, N'Cost of Goods')				
+	END) AS intCOGSAccountId, 	
+	(CASE WHEN ISNULL(dbo.fnGetItemBaseGLAccount(I.intItemId, IL.intLocationId, N'Sales Account'),0) = 0
+		THEN 
+			(CASE WHEN CL.intSalesAccount = 0 THEN NULL ELSE CL.intSalesAccount END)
+		ELSE
+			dbo.fnGetItemBaseGLAccount(I.intItemId, IL.intLocationId, N'Sales Account')				
+	END) AS intSalesAccountId, 
+	(CASE WHEN ISNULL(dbo.fnGetItemBaseGLAccount(I.intItemId, IL.intLocationId, N'Inventory'),0) = 0
+		THEN 
+			(CASE WHEN CL.intInventory = 0 THEN NULL ELSE CL.intInventory END)
+		ELSE
+			dbo.fnGetItemBaseGLAccount(I.intItemId, IL.intLocationId, N'Inventory')				
+	END) AS intInventoryAccountId
 FROM         
-dbo.tblICItem AS Item LEFT OUTER JOIN
-dbo.tblICItemLocation AS ItemLocation ON ItemLocation.intItemId = Item.intItemId
+	dbo.tblICItem AS I 
+LEFT OUTER JOIN
+	dbo.tblICItemLocation AS IL 
+		ON IL.intItemId = I.intItemId
+LEFT OUTER JOIN
+	dbo.tblSMCompanyLocation CL
+		ON IL.intLocationId = CL.intCompanyLocationId 
