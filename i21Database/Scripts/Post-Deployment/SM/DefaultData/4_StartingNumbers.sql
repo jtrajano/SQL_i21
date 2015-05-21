418bf298aa8ca6a45848b0e50d82be62eec33fa3
Dan Jalimao <Dan.Jalimao@irely.com>
2015-05-21 09:18:39 +0800

    LG-66

--- i21Database/Scripts/Post-Deployment/SM/DefaultData/4_StartingNumbers.sql
+++ i21Database/Scripts/Post-Deployment/SM/DefaultData/4_StartingNumbers.sql
@@ -452,6 +452,24 @@ GO
 			,[ysnEnable]			= 1
 			,[intConcurrencyId]		= 1
 	WHERE NOT EXISTS (SELECT TOP 1 1 FROM tblSMStartingNumber WHERE strTransactionType = N'Site Number')
+	UNION ALL
+	SELECT	[intStartingNumberId]	= 49
+			,[strTransactionType]	= N'Pick Lots'
+			,[strPrefix]			= N'PL'
+			,[intNumber]			= 1
+			,[strModule]			= 'Logistics'
+			,[ysnEnable]			= 1
+			,[intConcurrencyId]		= 1
+	WHERE NOT EXISTS (SELECT TOP 1 1 FROM tblSMStartingNumber WHERE strTransactionType = N'Pick Lots')
+	UNION ALL
+	SELECT	[intStartingNumberId]	= 50
+			,[strTransactionType]	= N'Delivery Orders'
+			,[strPrefix]			= N'DO'
+			,[intNumber]			= 1
+			,[strModule]			= 'Logistics'
+			,[ysnEnable]			= 1
+			,[intConcurrencyId]		= 1
+	WHERE NOT EXISTS (SELECT TOP 1 1 FROM tblSMStartingNumber WHERE strTransactionType = N'Delivery Orders')
 	SET IDENTITY_INSERT [dbo].[tblSMStartingNumber] OFF
 GO
 	PRINT N'END INSERT DEFAULT STARTING NUMBERS'
