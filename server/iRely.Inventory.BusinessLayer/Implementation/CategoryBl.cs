1dbef94b9e24213bf6a6abcc8c50493121cec5b8
Lawrence Zabala <lawrence.zabala@irely.com>
2015-05-21 08:52:04 +0800

    Fixed Error messages on Category

--- server/iRely.Inventory.BusinessLayer/Implementation/CategoryBl.cs
+++ server/iRely.Inventory.BusinessLayer/Implementation/CategoryBl.cs
@@ -2,6 +2,9 @@
 using iRely.GlobalComponentEngine.BusinessLayer;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,5 +21,27 @@ namespace iRely.Inventory.BusinessLayer
             _db = db;
         }
         #endregion
+
+        public override async Task<BusinessResult<tblICCategory>> SaveAsync(bool continueOnConflict)
+        {
+            var result = await _db.SaveAsync(continueOnConflict).ConfigureAwait(false);
+            var msg = result.Exception.Message;
+
+            if (result.BaseException.Message.Contains("Cannot insert the value NULL into column 'intCostingMethod'"))
+            {
+                msg = "Please specify a valid Costing Method.";
+            }
+
+            return new BusinessResult<tblICCategory>()
+            {
+                success = !result.HasError,
+                message = new MessageResult()
+                {
+                    statusText = msg,
+                    status = result.Exception.Error,
+                    button = result.Exception.Button.ToString()
+                }
+            };
+        }
     }
 }
