096d6c87cba0ec47c6d94474656a88967243126d
Mon Gonzales <mon.gonzales@irely.com>
2015-05-25 18:52:41 +0800

    EM-99

--- i21Database/dbo/Tables/tblARCustomerRackQuoteCategory.sql
+++ i21Database/dbo/Tables/tblARCustomerRackQuoteCategory.sql
@@ -5,6 +5,7 @@
 	[intCategoryId]							INT         NULL,	
 	[intConcurrencyId]						INT         CONSTRAINT [DF_tblARCustomerRackQuoteCategory_intConcurrencyId] DEFAULT ((0)) NOT NULL,
 	CONSTRAINT [PK_tblARCustomerRackQuoteCategory] PRIMARY KEY CLUSTERED ([intCustomerRackQuoteCategoryId] ASC),
-	CONSTRAINT [FK_tblARCustomerRackQuoteCategory_tblARCustomerRackQuoteHeader] FOREIGN KEY ([intCustomerRackQuoteHeaderId]) REFERENCES [dbo].[tblARCustomerRackQuoteHeader] ([intCustomerRackQuoteHeaderId]) ON DELETE CASCADE
+	CONSTRAINT [FK_tblARCustomerRackQuoteCategory_tblARCustomerRackQuoteHeader] FOREIGN KEY ([intCustomerRackQuoteHeaderId]) REFERENCES [dbo].[tblARCustomerRackQuoteHeader] ([intCustomerRackQuoteHeaderId]) ON DELETE CASCADE,
+	CONSTRAINT [FK_tblARCustomerRackQuoteCategory_tblICCategory] FOREIGN KEY ([intCategoryId]) REFERENCES [dbo].[tblICCategory] ([intCategoryId]) 
 	
 )
--- i21Database/dbo/Tables/tblARCustomerRackQuoteItem.sql
+++ i21Database/dbo/Tables/tblARCustomerRackQuoteItem.sql
@@ -1,6 +1,11 @@
 ﻿CREATE TABLE [dbo].[tblARCustomerRackQuoteItem]
 (
-	[Id] INT NOT NULL PRIMARY KEY
-
+	[intCustomerRackQuoteItemId]		INT			NOT NULL,
+	[intCustomerRackQuoteHeaderId]		INT			NOT NULL,
+	[intItemId]							INT         NULL,	
+	[intConcurrencyId]					INT         CONSTRAINT [DF_tblARCustomerRackQuoteItem_intConcurrencyId] DEFAULT ((0)) NOT NULL,
+	CONSTRAINT [PK_tblARCustomerRackQuoteItem] PRIMARY KEY CLUSTERED ([intCustomerRackQuoteItemId] ASC),
+	CONSTRAINT [FK_tblARCustomerRackQuoteItem_tblARCustomerRackQuoteHeader] FOREIGN KEY ([intCustomerRackQuoteHeaderId]) REFERENCES [dbo].[tblARCustomerRackQuoteHeader] ([intCustomerRackQuoteHeaderId]) ON DELETE CASCADE,
+	CONSTRAINT [FK_tblARCustomerRackQuoteItem_tblICItem] FOREIGN KEY ([intItemId]) REFERENCES [dbo].[tblICItem] ([intItemId])
 
 )
