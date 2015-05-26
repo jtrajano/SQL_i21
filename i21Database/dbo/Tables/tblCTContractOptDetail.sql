d23c4bbebb8f2b9979dc7dfb568da6eace269586
Satya <ssahoo@BLRSATYAWIN7.irely.net>
2015-05-26 21:46:11 +0530

    Column renamed

--- i21Database/dbo/Tables/tblCTContractOptDetail.sql
+++ i21Database/dbo/Tables/tblCTContractOptDetail.sql
@@ -2,20 +2,11 @@ CREATE TABLE [dbo].[tblCTContractOptDetail](
 	[intContractOptDetailId] [int] IDENTITY(1,1) NOT NULL,
 	[intConcurrencyId] [int] NOT NULL,
 	[intContractOptHeaderId] [int] NOT NULL,
-	[intBuySell] [int] NOT NULL,
-	[intPutCall] [int] NOT NULL,
- CONSTRAINT [PK_tblCTContractOptDetail_intContractOptDetailId] PRIMARY KEY CLUSTERED 
-(
-	[intContractOptDetailId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-
-GO
-
-ALTER TABLE [dbo].[tblCTContractOptDetail]  WITH CHECK ADD  CONSTRAINT [FK_tblCTContractOptDetail_tblCTContractOptHeader_intContractOptHeaderId] FOREIGN KEY([intContractOptHeaderId])
-REFERENCES [dbo].[tblCTContractOptHeader] ([intContractOptHeaderId]) ON DELETE CASCADE
-GO
-
-ALTER TABLE [dbo].[tblCTContractOptDetail] CHECK CONSTRAINT [FK_tblCTContractOptDetail_tblCTContractOptHeader_intContractOptHeaderId]
-GO
+	[intBuySellId] [int] NOT NULL,
+	[intPutCallId] [int] NOT NULL,
+	CONSTRAINT [PK_tblCTContractOptDetail_intContractOptDetailId] PRIMARY KEY CLUSTERED ([intContractOptDetailId] ASC),
+	CONSTRAINT [FK_tblCTContractOptDetail_tblCTContractOptHeader_intContractOptHeaderId] FOREIGN KEY([intContractOptHeaderId]) REFERENCES [dbo].[tblCTContractOptHeader] ([intContractOptHeaderId]) ON DELETE CASCADE,
+	CONSTRAINT [FK_tblCTContractOptDetail_tblCTBuySell_intBuySellId] FOREIGN KEY([intBuySellId]) REFERENCES [dbo].[tblCTBuySell] ([intBuySellId]),
+	CONSTRAINT [FK_tblCTContractOptDetail_tblCTPutCall_intPutCallId] FOREIGN KEY([intBuySellId]) REFERENCES [dbo].[tblCTPutCall] ([intPutCallId])
+)
 
