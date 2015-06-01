b91bb2a8a827e306a7fad6c7e6a9411aa2521da9
Ramachandran <rshanmuganathan@BLRRAMW8PRON.irely.net>
2015-06-01 12:32:57 +0530

    RK - Restrict the fut opt transaction screen. loading a combo.

--- i21Database/dbo/Views/vyuRKGetBrokerInstrumentType.sql
+++ i21Database/dbo/Views/vyuRKGetBrokerInstrumentType.sql
@@ -1,7 +1,32 @@
 ﻿CREATE VIEW [dbo].[vyuRKGetBrokerInstrumentType]	
 AS  
-SELECT intBrokerageAccountId,intEntityId,intInstrumentTypeId, 
-CASE WHEN intInstrumentTypeId = 1 Then 'Futures' 
-	 WHEN intInstrumentTypeId = 2 THEN 'Options'
-ELSE 'Futures & Options' end as strInstrumentType
-FROM tblRKBrokerageAccount 
+SELECT intBrokerageAccountId
+ ,intEntityId
+ ,intInstrumentTypeId
+ ,CASE 
+  WHEN intInstrumentTypeId = 1
+   THEN 'Futures'
+  WHEN intInstrumentTypeId = 2
+   THEN 'Options'
+  ELSE 'Futures & Options'
+  END AS strInstrumentType
+FROM tblRKBrokerageAccount
+WHERE intInstrumentTypeId <> 3
+
+UNION
+
+SELECT intBrokerageAccountId
+ ,intEntityId
+ ,2
+ ,'Options' AS strInstrumentType
+FROM tblRKBrokerageAccount
+WHERE intInstrumentTypeId = 3
+
+UNION
+
+SELECT intBrokerageAccountId
+ ,intEntityId
+ ,1
+ ,'Futures' AS strInstrumentType
+FROM tblRKBrokerageAccount
+WHERE intInstrumentTypeId = 3
