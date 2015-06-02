53aec44999a5c5ceac30f23e43606a55bebacb53
Satya <ssahoo@BLRSATYAWIN7.irely.net>
2015-06-02 19:24:25 +0530

    New table added

---
+++ i21Database/dbo/Tables/tblCTContractCertification.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[tblCTContractCertification]
+(
+	[intContractCertificationId] INT IDENTITY(1,1) NOT NULL, 
+    [intContractDetailId] INT NOT NULL, 
+    [intCertificationId] INT NOT NULL, 
+    [intConcurrencyId] INT NOT NULL,
+	CONSTRAINT [PK_tblCTContractCertification_intContractCertificationId] PRIMARY KEY CLUSTERED ([intContractCertificationId] ASC),
+	CONSTRAINT [FK_tblCTContractCertification_tblCTContractDetail_intContractDetailId] FOREIGN KEY ([intContractDetailId]) REFERENCES [tblCTContractDetail]([intContractDetailId]) ON DELETE CASCADE,
+	CONSTRAINT [FK_tblCTContractCertification_tblICCertification_intCertificationId] FOREIGN KEY ([intCertificationId]) REFERENCES [tblICCertification]([intCertificationId]) 
+)
