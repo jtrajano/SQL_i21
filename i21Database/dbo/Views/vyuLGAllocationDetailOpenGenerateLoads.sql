f95063df241518bf8162c3ced51bcf34d49a119a
James Balachander <balachander.j@irely.com>
2015-06-03 08:32:55 +0530

    LG-99

--- i21Database/dbo/Views/vyuLGAllocationDetailOpenGenerateLoads.sql
+++ i21Database/dbo/Views/vyuLGAllocationDetailOpenGenerateLoads.sql
@@ -16,6 +16,7 @@ AS
 			AD.intPUnitMeasureId,
 			ENP.strName AS strVendor,
 			IsNull(CDP.dblBalance, 0) - IsNull(CDP.dblScheduleQty, 0)		AS dblPUnLoadedQuantity,
+			ENP.intDefaultLocationId as intPDefaultLocationId,
 
 			AD.intSContractDetailId,
 			CHS.intContractNumber as intSContractNumber,
@@ -28,7 +29,8 @@ AS
 			AD.intSUnitMeasureId,
 			ENS.strName AS strCustomer,
 			IsNull(CDS.dblBalance, 0) - IsNull(CDS.dblScheduleQty, 0)		AS dblSUnLoadedQuantity,
-			IsNull(AD.dblPAllocatedQty, 0) - IsNull(GL.dblQuantity, 0)		AS dblGenerateLoadOpenQuantity
+			IsNull(AD.dblPAllocatedQty, 0) - IsNull(GL.dblQuantity, 0)		AS dblGenerateLoadOpenQuantity,
+			ENS.intDefaultLocationId as intSDefaultLocationId
 
 	FROM 	tblLGAllocationDetail AD
 	LEFT JOIN	tblLGGenerateLoad		GL	ON GL.intAllocationDetailId = AD.intAllocationDetailId
@@ -37,6 +39,6 @@ AS
 	JOIN	tblCTContractHeader		CHP	ON	CHP.intContractHeaderId		=	CDP.intContractHeaderId
 	JOIN	tblCTContractDetail 	CDS	ON CDS.intContractDetailId = AD.intSContractDetailId
 	JOIN	tblCTContractHeader		CHS	ON	CHS.intContractHeaderId		=	CDS.intContractHeaderId
-	JOIN	tblEntity				ENP	ON	ENP.intEntityId				=	CHP.intEntityId
-	JOIN	tblEntity				ENS	ON	ENS.intEntityId				=	CHS.intEntityId
+	LEFT JOIN	tblEntity				ENP	ON	ENP.intEntityId				=	CHP.intEntityId
+	LEFT JOIN	tblEntity				ENS	ON	ENS.intEntityId				=	CHS.intEntityId
 	WHERE	AD.intPUnitMeasureId = AD.intSUnitMeasureId
