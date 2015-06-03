b3ff2de61e541f5b9463526f3423f7a5d538bd86
Ramachandran <rshanmuganathan@BLRRAMW8PRON.irely.net>
2015-06-03 20:22:04 +0530

    RK- Merging form 15.3 master

--- i21Database/dbo/Tables/tblRKMatchFuturesPSDetail.sql
+++ i21Database/dbo/Tables/tblRKMatchFuturesPSDetail.sql
@@ -5,8 +5,9 @@
 	[intMatchFuturesPSHeaderId] INT NOT NULL, 
 	[dblMatchQty] NUMERIC(18, 6) NOT NULL,
 	[intLFutOptTransactionId] INT NOT NULL,
-	[intSFutOptTransactionId] INT NOT NULL		 
-	 CONSTRAINT [PK_tblRKMatchFuturesPSDetail_intMatchFuturesPSDetailId] PRIMARY KEY (intMatchFuturesPSDetailId), 
+	[intSFutOptTransactionId] INT NOT NULL,			 
+	 [dtmMatchedDate] DATETIME NULL, 
+    CONSTRAINT [PK_tblRKMatchFuturesPSDetail_intMatchFuturesPSDetailId] PRIMARY KEY (intMatchFuturesPSDetailId), 
 	 CONSTRAINT [FK_tblRKMatchFuturesPSDetail_tblRKMatchFuturesPSHeader_intMatchFuturesPSHeaderId] FOREIGN KEY ([intMatchFuturesPSHeaderId]) REFERENCES [tblRKMatchFuturesPSHeader]([intMatchFuturesPSHeaderId]) ON DELETE CASCADE,
 	 CONSTRAINT [FK_tblRKMatchFuturesPSDetail_tblRKFutOptTransaction_intLFutOptTransactionId] FOREIGN KEY ([intLFutOptTransactionId]) REFERENCES [tblRKFutOptTransaction]([intFutOptTransactionId]),
 	 CONSTRAINT [FK_tblRKMatchFuturesPSDetail_tblRKFutOptTransaction_intSFutOptTransactionId] FOREIGN KEY ([intSFutOptTransactionId]) REFERENCES [tblRKFutOptTransaction]([intFutOptTransactionId]),
