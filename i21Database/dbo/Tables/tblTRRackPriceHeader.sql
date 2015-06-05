68e67ffee34952147b553fbf62262d036374efda
Pruthvi <pruthvi.c@irely.com>
2015-06-05 12:10:23 +0530

    Transports

---
+++ i21Database/dbo/Tables/tblTRRackPriceHeader.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[tblTRRackPriceHeader]
+(
+	[intRackPriceHeaderId] INT NOT NULL IDENTITY,
+	[intSupplyPointId] INT NOT NULL,
+	[dtmEffectiveDateTime]  DATETIME        NOT NULL,
+	[strComments] nvarchar(max) COLLATE Latin1_General_CI_AS NULL,
+	[intConcurrencyId] [int] NOT NULL,
+	CONSTRAINT [PK_tblTRRackPriceHeader] PRIMARY KEY ([intRackPriceHeaderId]),
+	CONSTRAINT [FK_tblTRRackPriceHeader_tblTRSupplyPoint_intSupplyPointId] FOREIGN KEY ([intSupplyPointId]) REFERENCES [dbo].[tblTRSupplyPoint] ([intSupplyPointId])
+)
---
+++ i21Database/dbo/Tables/tblTRSupplyPoint.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[tblTRSupplyPoint]
+(
+	[intSupplyPointId] INT NOT NULL IDENTITY,
+	[intEntityVendorId] INT NOT NULL,		
+	[strSupplyPoint] nvarchar(50) COLLATE Latin1_General_CI_AS NOT NULL,
+	[strTerminalNumber] nvarchar(50) COLLATE Latin1_General_CI_AS NOT NULL,
+	[strGrossOrNet] nvarchar(50) COLLATE Latin1_General_CI_AS NOT NULL,
+	[strFuelDealerId1] nvarchar(50) COLLATE Latin1_General_CI_AS NOT NULL,
+	[strFuelDealerId2] nvarchar(50) COLLATE Latin1_General_CI_AS NOT NULL,
+	[strDefaultOrigin] nvarchar(50) COLLATE Latin1_General_CI_AS NOT NULL,
+	[intTaxGroupId] INT NOT NULL,
+	[ysnMultipleDueDates]  BIT  DEFAULT ((0)) NOT NULL,
+    [ysnMultipleBolInvoiced]  BIT  DEFAULT ((0)) NOT NULL,
+	[intConcurrencyId] [int] NOT NULL,
+	CONSTRAINT [PK_tblTRSupplyPoint] PRIMARY KEY ([intSupplyPointId]),
+	CONSTRAINT [AK_tblTRSupplyPoint] UNIQUE ([strTerminalNumber]),
+	CONSTRAINT [FK_tblTRSupplyPoint_tblAPVendor_intEntityVendorId] FOREIGN KEY (intEntityVendorId) REFERENCES [dbo].[tblAPVendor] (intEntityVendorId),
+	CONSTRAINT [FK_tblTRSupplyPoint_tblSMTaxGroup_intTaxGroupId] FOREIGN KEY ([intTaxGroupId]) REFERENCES [dbo].[tblSMTaxGroup] ([intTaxGroupId])
+
+)
