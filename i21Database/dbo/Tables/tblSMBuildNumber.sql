53e1814feb289488d7023ec1dc47c2739fdbea78
Edgardo Amurao <ed.amurao@irely.com>
2015-06-05 11:03:15 +0800

    fcom-506

--- i21Database/dbo/Tables/tblSMBuildNumber.sql
+++ i21Database/dbo/Tables/tblSMBuildNumber.sql
@@ -2,6 +2,6 @@
     [intVersionID]  INT           IDENTITY (1, 1) NOT NULL,
     [strVersionNo]  NVARCHAR (30) COLLATE Latin1_General_CI_AS NOT NULL,
     [dtmLastUpdate] DATETIME      NOT NULL, 
-    [strStashCommitId] VARCHAR(11) NOT NULL
+    [strStashCommitId] VARCHAR(11) NULL
 );
 
