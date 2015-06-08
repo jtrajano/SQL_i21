2a791cf1ea2dda92e820acb026ffdc50fda5eeb4
Edgardo Amurao <ed.amurao@irely.com>
2015-06-08 14:32:19 +0800

    CF-267

--- i21Database/Scripts/Post-Deployment/SM/DefaultData/4_StartingNumbers.sql
+++ i21Database/Scripts/Post-Deployment/SM/DefaultData/4_StartingNumbers.sql
@@ -488,6 +488,15 @@ GO
 			,[ysnEnable]			= 1
 			,[intConcurrencyId]		= 1
 	WHERE NOT EXISTS (SELECT TOP 1 1 FROM tblSMStartingNumber WHERE strTransactionType = N'Detailed Transaction')
+	UNION ALL
+	SELECT	[intStartingNumberId]	= 53
+			,[strTransactionType]	= N'Summarized Invoice'
+			,[strPrefix]			= N'CFSI-'
+			,[intNumber]			= 1
+			,[strModule]			= 'Card Fueling'
+			,[ysnEnable]			= 1
+			,[intConcurrencyId]		= 1
+	WHERE NOT EXISTS (SELECT TOP 1 1 FROM tblSMStartingNumber WHERE strTransactionType = N'Summarized Invoice')
 	SET IDENTITY_INSERT [dbo].[tblSMStartingNumber] OFF
 GO
 	PRINT N'END INSERT DEFAULT STARTING NUMBERS'
