d56d574bef4011a574b6b8db8294fe5edacfb9e3
jeffrey trajano <jeffrey.trajano@irely.com>
2015-06-09 20:07:24 +0800

    GL-1850 Update the Segment's Account Category based on Chart Of Account's Account Category

--- i21Database/dbo/Stored Procedures/uspGLUpdateSegmentCategory.sql
+++ i21Database/dbo/Stored Procedures/uspGLUpdateSegmentCategory.sql
@@ -1,32 +1,23 @@
-﻿CREATE PROCEDURE [dbo].[uspGLUpdateSegmentCategory]
+﻿--Author		: Trajano, Jeffrey	
+--Description	: UPDATES THE tblGLAccountSegment based on the Chart Of Accounts
+--Date			: June 9, 2015 
+--Version		: 15.3
+CREATE PROCEDURE [dbo].[uspGLUpdateSegmentCategory]
 AS
 PRINT 'START UPDATING ACCOUNT SEGMENT CATEGORY ID'
-DECLARE @segmentId INT
-DECLARE segment_cursor CURSOR FOR 
-SELECT s.intAccountSegmentId
-FROM tblGLAccountSegment s join tblGLAccountSegmentMapping m on s.intAccountSegmentId = m.intAccountSegmentId
-JOIN tblGLAccountStructure u on s.intAccountStructureId = u.intAccountStructureId where u.strType= 'Primary'
-GROUP BY s.intAccountSegmentId
-ORDER BY s.intAccountSegmentId
-OPEN segment_cursor
-FETCH NEXT FROM segment_cursor INTO @segmentId
-WHILE @@FETCH_STATUS = 0
-BEGIN
-;WITH accountCat(intAccountCategoryId, intAccountSegmentId)
-	AS
-	(
-		SELECT a.intAccountCategoryId,s.intAccountSegmentId from tblGLAccount a join 
-		tblGLAccountSegmentMapping m on a.intAccountId = m.intAccountId join
-		tblGLAccountSegment s on m.intAccountSegmentId = s.intAccountSegmentId join
-		tblGLAccountStructure u on s.intAccountStructureId = u.intAccountStructureId
-		WHERE u.strType = 'Primary' 
-		GROUP BY  a.intAccountCategoryId,s.intAccountSegmentId
-	)
-	UPDATE tblGLAccountSegment SET intAccountCategoryId = 
-	(SELECT TOP 1 intAccountCategoryId FROM accountCat	WHERE intAccountSegmentId = @segmentId)
-	WHERE intAccountSegmentId = @segmentId
-FETCH NEXT FROM segment_cursor INTO @segmentId
-END
-CLOSE segment_cursor;
-DEALLOCATE segment_cursor;
+DECLARE @generalCategoryId INT
+SELECT @generalCategoryId = intAccountCategoryId  FROM tblGLAccountCategory WHERE strAccountCategory = 'General'
+;WITH categories(segmentId,categoryId)as
+(
+	SELECT s.intAccountSegmentId, a.intAccountCategoryId from tblGLAccount a join 
+	tblGLAccountSegmentMapping m on a.intAccountId = m.intAccountId join
+	tblGLAccountSegment s on m.intAccountSegmentId = s.intAccountSegmentId join
+	tblGLAccountStructure u on s.intAccountStructureId = u.intAccountStructureId
+	WHERE u.strType = 'Primary' and a.intAccountCategoryId <> @generalCategoryId
+	GROUP BY  a.intAccountCategoryId,s.intAccountSegmentId
+)
+UPDATE s SET intAccountCategoryId = c.categoryId
+FROM tblGLAccountSegment s
+JOIN categories c
+on s.intAccountSegmentId = c.segmentId
 PRINT 'COMPLETED UPDATING ACCOUNT SEGMENT CATEGORY IDS'
