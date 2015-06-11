8a4cdf451f8d45d8430c7d71a13e4677271a8e0a
Feb Montefrio <feb.montefrio@irely.com>
2015-06-11 09:46:13 +0800

    IC-911

---
+++ Test/testi21Database/Fake Data/Fake data for item uom table, bushel scenario.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [testi21Database].[Fake data for item uom table, bushel scenario]
+AS
+BEGIN
+	EXEC tSQLt.FakeTable 'dbo.tblICItemUOM';	
+		
+	DECLARE @BUSHEL AS INT = 1 
+			,@LB AS INT = 2
+		
+	INSERT INTO dbo.tblICItemUOM(intItemUOMId, dblUnitQty)
+	
+	SELECT @BUSHEL, 1								-- Bushel is the stock unit. 
+	UNION ALL SELECT @LB,  0.017857142857142		-- one bushel is 56 pounds. 
+END
---
+++ Test/testi21Database/Stored Procedures/test fnCalculateQtyBetweenUOM for converting Bushel to LB.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROCEDURE testi21Database.[test fnCalculateQtyBetweenUOM for converting Bushel to LB]
+AS 
+BEGIN
+	-- Call the fake data
+	EXEC [testi21Database].[Fake data for item uom table, bushel scenario]
+
+	DECLARE @BUSHEL AS INT = 1 
+			,@LB AS INT = 2
+
+	-- Arrange
+	DECLARE @dblQty AS NUMERIC(18,6) = 1
+
+	DECLARE @result AS NUMERIC(38,20) 
+	DECLARE @expected AS NUMERIC(38,20) = 56.00 -- One bushel is 56 pounds. 
+
+	-- Act
+	SELECT @result = dbo.fnCalculateQtyBetweenUOM(@BUSHEL, @LB, @dblQty)
+
+	-- Assert 
+	EXEC tSQLt.AssertEquals @expected, @result;
+END
