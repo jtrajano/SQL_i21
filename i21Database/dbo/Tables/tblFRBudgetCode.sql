6f55322326d72ebdc4445b6c24f8bd78660bffba
axel_casauran <axel.casauran@irely.com>
2015-06-15 09:50:07 +0800

    Revert "Merge branch '1520_Dev' into 1520_Prod"
    This reverts commit e541e9c765421182cc7f971e0c99acd19aa63758.

--- i21Database/dbo/Tables/tblFRBudgetCode.sql
+++ i21Database/dbo/Tables/tblFRBudgetCode.sql
@@ -1,10 +1,9 @@
 ﻿CREATE TABLE [dbo].[tblFRBudgetCode] (
     [intBudgetCode]               INT            IDENTITY (1, 1) NOT NULL,
-	[strBudgetCode]               NVARCHAR (150) COLLATE Latin1_General_CI_AS NULL,
-	[intFiscalYearId]             INT            NULL,    
+    [ysnDefault]                  BIT            DEFAULT ((0)) NULL,
+    [strBudgetCode]               NVARCHAR (40)  COLLATE Latin1_General_CI_AS NULL,
     [strBudgetEnglishDescription] NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NULL,
-	[ysnDefault]                  BIT            DEFAULT ((0)) NULL,    
     [intConcurrencyId]            INT            DEFAULT 1 NOT NULL,
-    CONSTRAINT [PK_tblFRBudgetCode] PRIMARY KEY CLUSTERED ([intBudgetCode] ASC),
-	CONSTRAINT [FK_tblFRBudgetCode_tblGLFiscalYear] FOREIGN KEY ([intFiscalYearId]) REFERENCES [dbo].[tblGLFiscalYear] ([intFiscalYearId])
+    CONSTRAINT [PK_tblFRBudgetCode] PRIMARY KEY CLUSTERED ([intBudgetCode] ASC)
 );
+
