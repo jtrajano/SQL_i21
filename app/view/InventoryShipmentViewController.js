a23c71b98739456c2d6a5b4dc8b4bd565e706a29
Lawrence Zabala <lawrence.zabala@irely.com>
2015-06-16 11:22:12 +0800

    IC-888

--- app/view/InventoryShipmentViewController.js
+++ app/view/InventoryShipmentViewController.js
@@ -146,6 +146,12 @@ Ext.define('Inventory.view.InventoryShipmentViewController', {
             },
             txtReceivedBy: '{current.strReceivedBy}',
 
+            btnInsertItem: {
+                hidden: '{current.ysnPosted}'
+            },
+            btnRemoveItem: {
+                hidden: '{current.ysnPosted}'
+            },
             grdInventoryShipment: {
                 readOnly: '{current.ysnPosted}',
                 colOrderNumber: {
@@ -610,6 +616,27 @@ Ext.define('Inventory.view.InventoryShipmentViewController', {
 
     },
 
+    onViewItemClick: function(button, e, eOpts) {
+        var win = button.up('window');
+        var grd = win.down('#grdInventoryShipment');
+
+        var selected = grd.getSelectionModel().getSelection();
+
+        if (selected) {
+            if (selected.length > 0){
+                var current = selected[0];
+                if (!current.dummy)
+                    iRely.Functions.openScreen('Inventory.view.Item', current.get('intItemId'));
+            }
+            else {
+                iRely.Functions.showErrorDialog('Please select an Item to view.');
+            }
+        }
+        else {
+            iRely.Functions.showErrorDialog('Please select an Item to view.');
+        }
+    },
+
     init: function(application) {
         this.control({
             "#cboShipFromAddress": {
@@ -653,6 +680,9 @@ Ext.define('Inventory.view.InventoryShipmentViewController', {
             },
             "#btnInvoice": {
                 click: this.onInvoiceClick
+            },
+            "#btnViewItem": {
+                click: this.onViewItemClick
             }
         })
     }
