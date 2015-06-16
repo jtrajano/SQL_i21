bce20906913ad77fff12bfce9797f57989168041
Lawrence Zabala <lawrence.zabala@irely.com>
2015-06-16 10:43:28 +0800

    IC-906

--- server/iRely.Inventory.BusinessLayer/Implementation/StorageLocationBl.cs
+++ server/iRely.Inventory.BusinessLayer/Implementation/StorageLocationBl.cs
@@ -21,6 +21,31 @@ namespace iRely.Inventory.BusinessLayer
         }
         #endregion
 
+        public override async Task<BusinessResult<tblICStorageLocation>> SaveAsync(bool continueOnConflict)
+        {
+            var result = await _db.SaveAsync(continueOnConflict).ConfigureAwait(false);
+            var msg = result.Exception.Message;
+
+            if (result.HasError)
+            {
+                if (result.BaseException.Message.Contains("Violation of UNIQUE KEY constraint 'AK_tblICStorageLocation_strName'"))
+                {
+                    msg = "Storage Location must be unique per Location and Sub Location.";
+                }
+            }
+
+            return new BusinessResult<tblICStorageLocation>()
+            {
+                success = !result.HasError,
+                message = new MessageResult()
+                {
+                    statusText = msg,
+                    status = result.Exception.Error,
+                    button = result.Exception.Button.ToString()
+                }
+            };
+        }
+
         public override async Task<SearchResult> Search(GetParameter param)
         {
             var query = _db.GetQuery<vyuICGetStorageLocation>()
