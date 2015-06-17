4a043b0a56d89ae38a21a6dc4cd566bccf53d77f
Mon Gonzales <mon.gonzales@irely.com>
2015-06-17 12:46:35 +0800

    EM-34

---
+++ i21Database/dbo/Views/vyuEMCustomerApplicator.sql
@@ -0,0 +1,20 @@
+﻿CREATE VIEW [dbo].[vyuEMCustomerApplicator]
+	AS 
+
+
+	select 
+	intEntityId = A.intEntityCustomerId,
+	A.strLicenseNo,
+	A.dtmExpirationDate,
+	A.ysnCustomerApplicator,
+	A.strComment,
+	B.strCustomerNumber,
+	E.strName
+
+	from tblARCustomerApplicatorLicense A
+		join tblARCustomer B
+			on A.intEntityCustomerId = B.intEntityCustomerId
+		join tblEntity E
+			on A.intEntityCustomerId = E.intEntityId
+
+
