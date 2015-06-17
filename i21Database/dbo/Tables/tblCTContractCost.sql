adc15aacee4b07abfdb6ead03a0aef51e9e98101
Satya <ssahoo@BLRSATYAWIN7.irely.net>
2015-06-17 16:37:26 +0530

    CT-152

--- i21Database/dbo/Tables/tblCTContractCost.sql
+++ i21Database/dbo/Tables/tblCTContractCost.sql
@@ -7,7 +7,6 @@ CREATE TABLE [dbo].[tblCTContractCost](
 	[intCostMethodId] [int] NOT NULL,
 	[dblRate] [numeric](10, 4) NOT NULL,
 	[intUnitMeasureId] [int] NULL,
-	[intCurrencyId] [int] NOT NULL,
 	[ysnAccrue] [bit] NOT NULL CONSTRAINT [DF_tblCTContractCost_ysnAccrue]  DEFAULT ((1)),
 	[ysnMTM] [bit] NULL,
 	[ysnPrice] [bit] NULL,
@@ -15,7 +14,6 @@ CREATE TABLE [dbo].[tblCTContractCost](
 	CONSTRAINT [PK_tblCTContractCost_intContractCostId] PRIMARY KEY CLUSTERED ([intContractCostId] ASC),
 	CONSTRAINT [FK_tblCTContractCost_tblCTContractDetail_intContractDetailId] FOREIGN KEY ([intContractDetailId]) REFERENCES [tblCTContractDetail]([intContractDetailId]),
 	CONSTRAINT [FK_tblCTContractCost_tblCTCostType_intCostTypeId] FOREIGN KEY ([intCostTypeId]) REFERENCES [tblCTCostType]([intCostTypeId]),
-	CONSTRAINT [FK_tblCTContractCost_tblSMCurrency_intCurrencyId] FOREIGN KEY ([intCurrencyId]) REFERENCES [tblSMCurrency]([intCurrencyID]),
 	CONSTRAINT [FK_tblCTContractCost_tblAPVendor_intVendorId] FOREIGN KEY ([intVendorId]) REFERENCES [tblAPVendor]([intEntityVendorId]),
 	CONSTRAINT [FK_tblCTContractCost_tblICUnitMeasure_intUnitMeasureId] FOREIGN KEY ([intUnitMeasureId]) REFERENCES [tblICUnitMeasure]([intUnitMeasureId]),
 	CONSTRAINT [FK_tblCTContractCost_tblCTCostMethod_intCostMethodId] FOREIGN KEY ([intCostMethodId]) REFERENCES [tblCTCostMethod]([intCostMethodId])
