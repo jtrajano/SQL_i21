8c3eb0a4f2313bb5ff316a4978d36e19a2b1d425
Mon Gonzales <mon.gonzales@irely.com>
2015-06-18 11:11:53 +0800

    EM-218

---
+++ i21Database/dbo/Tables/tblARCustomerFailedImport.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[tblARCustomerFailedImport]
+(
+	[intCustomerFailedImportId]			INT IDENTITY(1,1) NOT NULL,
+	[strCustomerNumber]					NVARCHAR(100) NOT NULL,
+	[strReason]							NVARCHAR(MAX) NULL
+	CONSTRAINT [PK_tblARCustomerFailedImport] PRIMARY KEY  CLUSTERED ([intCustomerFailedImportId] ASC)
+)
