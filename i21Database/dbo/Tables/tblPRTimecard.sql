13c3b65702dcd334999684bd4c2a69c00bc85171
Arman Arenas <arman.arenas@irely.com>
2015-06-19 04:51:18 +0800

    PR-78 Added tblPRTimecard.intEmployeeDepartmentId

--- i21Database/dbo/Tables/tblPRTimecard.sql
+++ i21Database/dbo/Tables/tblPRTimecard.sql
@@ -6,6 +6,7 @@
     [dtmTimeIn] DATETIME NULL, 
     [dtmTimeOut] DATETIME NULL, 
     [intEmployeeEarningId] INT NULL, 
+	[intEmployeeDepartmentId] INT NULL, 
     [intTimeEntryId] INT NULL, 
     [strNotes] NVARCHAR(200) COLLATE Latin1_General_CI_AS NULL, 
     [ysnApproved] BIT NULL DEFAULT ((0)), 
@@ -102,3 +103,12 @@ EXEC sp_addextendedproperty @name = N'MS_Description',
     @level1name = N'tblPRTimecard',
     @level2type = N'COLUMN',
     @level2name = N'intConcurrencyId'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Employee Department Id',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'tblPRTimecard',
+    @level2type = N'COLUMN',
+    @level2name = N'intEmployeeDepartmentId'
