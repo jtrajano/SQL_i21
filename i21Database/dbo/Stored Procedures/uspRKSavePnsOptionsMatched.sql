52fcd92fd8cfbe1a1a5410e5251c194747b0f84d
Ramachandran <rshanmuganathan@BLRRAMW8PRON.irely.net>
2015-06-19 18:45:46 +0530

    RM-65

---
+++ i21Database/dbo/Stored Procedures/uspRKSavePnsOptionsMatched.sql
@@ -0,0 +1,80 @@
+﻿  
+CREATE PROCEDURE [dbo].[uspRKSavePnsOptionsMatched]  
+	 @strXml nVarchar(Max)
+AS  
+BEGIN TRY  
+  
+SET QUOTED_IDENTIFIER OFF  
+SET ANSI_NULLS ON  
+SET NOCOUNT ON  
+SET XACT_ABORT ON  
+SET ANSI_WARNINGS OFF  
+  
+DECLARE @idoc int   
+Declare @intOptionsMatchPnSHeaderId int  
+Declare @strTranNo nVarchar(50)  
+Declare @dtmMatchDate  datetime  
+Declare @intMatchQty int  
+Declare @intLFutOptTransactionId int  
+Declare @intSFutOptTransactionId int  
+Declare @ErrMsg nvarchar(Max)
+
+EXEC sp_xml_preparedocument @idoc OUTPUT, @strXml    
+  
+ BEGIN TRANSACTION		
+  
+ INSERT INTO tblRKOptionsMatchPnSHeader 
+		(
+			intConcurrencyId
+		)
+ VALUES
+		(
+		   1		
+		 )
+
+SELECT @intOptionsMatchPnSHeaderId = SCOPE_IDENTITY();  
+
+SELECT @strTranNo=isnull(max(strTranNo),0) from tblRKOptionsMatchPnS   
+ 
+   INSERT INTO tblRKOptionsMatchPnS
+		(	
+		intOptionsMatchPnSHeaderId,
+		strTranNo,	
+		dtmMatchDate,
+		intMatchQty,
+		intLFutOptTransactionId,
+		intSFutOptTransactionId,
+		intConcurrencyId		
+		)  
+
+ SELECT  
+	@intOptionsMatchPnSHeaderId as intOptionsMatchPnSHeaderId,
+	@strTranNo + ROW_NUMBER()over(order by intLFutOptTransactionId)strTranNo,
+	dtmMatchDate,
+	intMatchQty,
+	intLFutOptTransactionId,
+	intSFutOptTransactionId,
+	1 as intConcurrencyId		
+  FROM OPENXML(@idoc,'root/Transaction', 2)      
+ WITH    
+ ( 
+	[intOptionsMatchPnSHeaderId] int ,
+	[dtmMatchDate]  DATETIME  , 
+    [intMatchQty] int , 
+	[intLFutOptTransactionId] INT,
+	[intSFutOptTransactionId] INT
+ )   
+   
+Commit Tran  
+  
+EXEC sp_xml_removedocument @idoc   
+  
+END TRY    
+    
+BEGIN CATCH    
+ IF XACT_STATE() != 0 AND @@TRANCOUNT > 0 ROLLBACK TRANSACTION        
+ SET @ErrMsg = ERROR_MESSAGE()    
+ IF @idoc <> 0 EXEC sp_xml_removedocument @idoc    
+ RAISERROR(@ErrMsg, 16, 1, 'WITH NOWAIT')    
+    
+END CATCH
---
+++ i21Database/dbo/Views/vyuRKSOptionMatchedTransaction.sql
@@ -0,0 +1,26 @@
+﻿CREATE VIEW vyuRKSOptionMatchedTransaction
+AS
+SELECT m.intMatchOptionsPnSId,strTranNo,dtmMatchDate,intMatchQty,e.strName,b.strAccountNumber,t.strInternalTradeNo,scl.strLocationName,
+	   fm.strFutMarketName,om.strOptionMonth,t.dblStrike,t.strOptionType
+	   ,strCommodityCode,t.dtmTransactionDate as dtmMLTransactionDate,t.strInternalTradeNo as strMLInternalTradeNo,cb.strBook as strMLBook,csb.strSubBook as strMLSubBook,
+	   (SELECT TOP 1 dtmTransactionDate FROM tblRKOptionsMatchPnS om
+	    JOIN tblRKFutOptTransaction t1 on m.intSFutOptTransactionId= t1.intFutOptTransactionId) as  dtmMSTransactionDate,
+	    (SELECT TOP 1 strInternalTradeNo FROM tblRKOptionsMatchPnS om
+	    JOIN tblRKFutOptTransaction t1 on m.intSFutOptTransactionId= t1.intFutOptTransactionId) as  strMSInternalTradeNo,
+	    (SELECT TOP 1 strBook FROM tblRKOptionsMatchPnS om
+	    JOIN tblRKFutOptTransaction t1 on m.intSFutOptTransactionId= t1.intFutOptTransactionId
+	    JOIN tblCTBook cb on cb.intBookId= t1.intBookId ) as  strMSBook,
+	    (SELECT TOP 1 strSubBook FROM tblRKOptionsMatchPnS om
+	    JOIN tblRKFutOptTransaction t1 on m.intSFutOptTransactionId= t1.intFutOptTransactionId
+	    JOIN tblCTSubBook scb on scb.intBookId= t1.intBookId ) as  strMSSubBook
+FROM tblRKOptionsMatchPnS m
+join tblRKFutOptTransaction t on m.intLFutOptTransactionId= t.intFutOptTransactionId
+Join tblEntity e on e.intEntityId=t.intEntityId
+Join tblRKBrokerageAccount b on b.intBrokerageAccountId=t.intBrokerageAccountId
+Join tblRKFutureMarket fm on fm.intFutureMarketId = t.intFutureMarketId
+JOIN tblRKOptionsMonth om on om.intOptionMonthId=t.intOptionMonthId
+join tblICCommodity ic on ic.intCommodityId=t.intCommodityId
+JOIN tblCTBook cb on cb.intBookId= t.intBookId
+join tblCTSubBook csb on csb.intSubBookId=t.intSubBookId
+JOIN tblSMCompanyLocation scl on scl.intCompanyLocationId=t.intLocationId
+
