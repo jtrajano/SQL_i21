418b80b64078deaf1d419430e0eb1ab584c1a580
James Balachander <balachander.j@irely.com>
2015-06-20 13:00:49 +0530

    LG-117

--- i21Database/dbo/Views/vyuLGShipmentContainerPurchaseContracts.sql
+++ i21Database/dbo/Views/vyuLGShipmentContainerPurchaseContracts.sql
@@ -1,8 +1,9 @@
-CREATE VIEW vyuLGShipmentPurchaseContracts
+CREATE VIEW vyuLGShipmentContainerPurchaseContracts
 AS   
 SELECT 
-	   SCQ.intShipmentContractQtyId,
-       SCQ.intShipmentId,
+	   SC.intShipmentBLContainerContractId,
+	   SC.intShipmentContractQtyId,
+       SC.intShipmentId,
        S.intTrackingNumber,
 	   S.ysnDirectShipment,
        SCQ.intContractDetailId,
@@ -13,11 +14,11 @@ SELECT
        CT.intItemUOMId,
        S.intSubLocationId,
        intLocationId = S.intCompanyLocationId,
-       SCQ.dblQuantity,
-	   SCQ.dblReceivedQty,
-       SCQ.dblGrossWt,
-       SCQ.dblTareWt,
-       SCQ.dblNetWt,
+       SC.dblQuantity,
+	   SC.dblReceivedQty,
+       dblGrossWt = (Container.dblGrossWt / Container.dblQuantity) * SC.dblQuantity,
+       dblTareWt = (Container.dblTareWt / Container.dblQuantity) * SC.dblQuantity,
+       dblNetWt = (Container.dblNetWt / Container.dblQuantity) * SC.dblQuantity,
        dblCost = CT.dblCashPrice, 
        intWeightUOMId = S.intWeightUnitMeasureId,
        WTUOM.strUnitMeasure as strWeightUOM,
@@ -34,15 +35,25 @@ SELECT
        ,strStockUOMType = (SELECT TOP 1 strUnitType FROM tblICItemUOM ItemUOM LEFT JOIN tblICUnitMeasure UOM ON UOM.intUnitMeasureId = ItemUOM.intUnitMeasureId WHERE ysnStockUnit = 1 AND ItemUOM.intItemUOMId = CT.intItemUOMId)
        ,dblStockUOMCF = ISNULL((SELECT TOP 1 dblUnitQty FROM tblICItemUOM ItemUOM LEFT JOIN tblICUnitMeasure UOM ON UOM.intUnitMeasureId = ItemUOM.intUnitMeasureId WHERE ysnStockUnit = 1 AND ItemUOM.intItemUOMId = CT.intItemUOMId),0)
        ,SubLocation.strSubLocationName
+	   , BL.strBLNumber
+	   , Container.strContainerNumber
+	   , Container.strLotNumber
+	   , Container.strMarks
+	   , Container.strOtherMarks
+	   , Container.strSealNumber
+	   , ContType.strContainerType
 
-FROM tblLGShipmentContractQty SCQ
-JOIN tblLGShipment S ON S.intShipmentId = SCQ.intShipmentId
+FROM tblLGShipmentBLContainerContract SC
+LEFT JOIN tblLGShipmentContractQty SCQ ON SCQ.intShipmentContractQtyId = SC.intShipmentContractQtyId
+JOIN tblLGShipment S ON S.intShipmentId = SC.intShipmentId
+JOIN tblLGShipmentBL BL ON BL.intShipmentBLId = SC.intShipmentBLId
+LEFT JOIN tblLGShipmentBLContainer Container ON Container.intShipmentBLContainerId = SC.intShipmentBLContainerId
+LEFT JOIN tblLGContainerType ContType ON ContType.intContainerTypeId = Container.intContainerTypeId
 JOIN tblCTContractDetail CT ON CT.intContractDetailId = SCQ.intContractDetailId
 JOIN tblICItemUOM ItemUOM ON ItemUOM.intItemUOMId = CT.intItemUOMId
 JOIN tblCTContractHeader CH ON CH.intContractHeaderId = CT.intContractHeaderId
 JOIN tblEntity EN ON EN.intEntityId = S.intVendorEntityId
-JOIN tblICUnitMeasure UOM ON UOM.intUnitMeasureId = SCQ.intUnitMeasureId
+JOIN tblICUnitMeasure UOM ON UOM.intUnitMeasureId = SC.intUnitMeasureId
 JOIN tblICUnitMeasure WTUOM ON WTUOM.intUnitMeasureId = S.intWeightUnitMeasureId
 LEFT JOIN tblICItem Item ON Item.intItemId = SCQ.intItemId
 LEFT JOIN tblSMCompanyLocationSubLocation SubLocation ON SubLocation.intCompanyLocationSubLocationId = S.intSubLocationId
-
