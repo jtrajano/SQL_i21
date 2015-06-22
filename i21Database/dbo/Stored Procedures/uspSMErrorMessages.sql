0f080ab9e48cabd24c0c93ebab244d3008416ed2
Premkumar S <prem.selladurai@irely.com>
2015-06-22 18:06:29 +0530

    MFG-264

--- i21Database/dbo/Stored Procedures/uspSMErrorMessages.sql
+++ i21Database/dbo/Stored Procedures/uspSMErrorMessages.sql
@@ -528,7 +528,7 @@ SET @strmessage = 'Cycle count entries for the run not available, cannot proceed
 EXEC sp_addmessage 51131,11,@strmessage,'us_english','False'
 
 IF EXISTS(SELECT 1 FROM sys.messages WHERE message_id = 51132) EXEC sp_dropmessage 51132, 'us_english'	
-SET @strmessage = 'Cycle count qty is not saved. Please save cycle count before true up.'
+SET @strmessage = 'Please complete and save Cycle count entries for all the items before posting adjustment.'
 EXEC sp_addmessage 51132,11,@strmessage,'us_english','False'
 
 IF EXISTS(SELECT 1 FROM sys.messages WHERE message_id = 51133) EXEC sp_dropmessage 51133, 'us_english'	
