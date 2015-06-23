83fce7cab6cbeed4bf2f034c0f27a12be2f6c680
Mon Gonzales <mon.gonzales@irely.com>
2015-06-23 11:44:52 +0800

    EM-34

--- i21Database/dbo/Tables/tblARCustomerApplicatorLicense.sql
+++ i21Database/dbo/Tables/tblARCustomerApplicatorLicense.sql
@@ -2,8 +2,8 @@
 (
 	[intCustomerApplicatorLicenseId]		INT IDENTITY(1,1) NOT NULL, 
     [intEntityCustomerId]					INT NOT NULL,
-	[strLicenseNo]							NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL, 
-	[dtmExpirationDate]						DATETIME NOT NULL, 
+	[strLicenseNo]							NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
+	[dtmExpirationDate]						DATETIME NULL, 
 	[ysnCustomerApplicator]					BIT, 
 	[strComment]							NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NULL,
 	[intConcurrencyId]						INT             CONSTRAINT [DF_tblARCustomerApplicatorLicense_intConcurrencyId] DEFAULT ((0)) NOT NULL,
