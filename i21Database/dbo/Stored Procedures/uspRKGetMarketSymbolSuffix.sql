18f28c31432ec1fe27e934ac8d4896e12bd7ec05
Gopinath Nayak <gopinath.n@irely.com>
2015-06-24 20:02:13 +0530

    GRN-121

--- i21Database/dbo/Stored Procedures/uspRKGetMarketSymbolSuffix.sql
+++ i21Database/dbo/Stored Procedures/uspRKGetMarketSymbolSuffix.sql
@@ -1,28 +1,15 @@
-﻿CREATE PROCEDURE [dbo].[uspRKGetElectronicPricingURL] 
-	 @FutureMarketId INT
-	,@intUserId INT
+﻿CREATE PROCEDURE [dbo].[uspRKGetMarketSymbolSuffix]
+
+	@FutureMarketId INT	
+
 AS
 BEGIN TRY
 	SET NOCOUNT ON
 
 	DECLARE @ErrMsg NVARCHAR(MAX)
-	DECLARE @strUserName NVARCHAR(100)
-	DECLARE @strPassword NVARCHAR(100)
-	DECLARE @IntinterfaceSystem INT
-	DECLARE @StrQuoteProvider NVARCHAR(100)
-	DECLARE @StrTradedMonthSymbol NVARCHAR(1000)
-	DECLARE @MarketExchangeCode NVARCHAR(10)
-	DECLARE @Commoditycode NVARCHAR(10)
-	DECLARE @URL NVARCHAR(1000)
-
-	SELECT @Commoditycode = strFutSymbol
-	FROM tblRKFutureMarket
-	WHERE intFutureMarketId = @FutureMarketId
-
-	SELECT @MarketExchangeCode = E.strExchangeInterfaceCode
-	FROM tblRKMarketExchange E
-	JOIN tblRKFutureMarket M ON M.intMarketExchangeId = E.intMarketExchangeId AND M.intFutureMarketId = @FutureMarketId
-
+	
+	DECLARE @StrTradedMonthSymbol NVARCHAR(2)	
+	
 	DECLARE @FutureTradedMonths AS TABLE 
 	(
 		 IntMonthNumber INT
@@ -221,99 +208,36 @@ BEGIN TRY
 		,'Z'
 	FROM tblRKFutureMarket
 	WHERE intFutureMarketId = @FutureMarketId
+		
 
 	SELECT TOP 1 @StrTradedMonthSymbol = StrTradedMonthSymbol
 	FROM @FutureTradedMonths
 	WHERE IntMonthNumber >= MONTH(Getdate()) AND IsTraded = 1
 	ORDER BY IntMonthNumber ASC
-
-	SELECT @URL = strValue
-	FROM tblSMPreferences
-	WHERE strPreference = 'InterfaceWebServicesURL'
-
-	SELECT @strUserName = strValue
-	FROM tblSMPreferences a
-	JOIN tblSMUserSecurity b ON b.intUserSecurityID = a.intUserID
-	WHERE a.strPreference = 'ProviderUserId' AND b.intUserSecurityID = @intUserId
-
-	IF NOT EXISTS (
-			SELECT 1
-			FROM tblSMPreferences a
-			JOIN tblSMUserSecurity b ON b.intUserSecurityID = a.intUserID
-			WHERE a.strPreference = 'QuoteProvider'
-				AND b.intUserSecurityID = @intUserId
-				AND strValue = 'DTN/Agricharts'
-			)
-		OR (@strUserName = '')
-	BEGIN
-	
-		RAISERROR ('The User cannot access Electronic Pricing',16,1)
 		
-	END
-
-	SELECT @strPassword = strValue
-	FROM tblSMPreferences a
-	JOIN tblSMUserSecurity b ON b.intUserSecurityID = a.intUserID
-	WHERE a.strPreference = 'ProviderPassword' AND b.intUserSecurityID = @intUserId
-
-	SELECT @IntinterfaceSystem = strValue
-	FROM tblSMPreferences
-	WHERE strPreference = 'InterfaceSystem'
-
-	
-	IF ISNULL(@StrTradedMonthSymbol, '') = ''
+	IF ISNULL(@StrTradedMonthSymbol,'')=''
 	BEGIN
-			SET @StrTradedMonthSymbol = (
-											SELECT TOP 1 StrTradedMonthSymbol
-											FROM @FutureTradedMonths
-											WHERE IsTraded = 1
-											ORDER BY IntMonthNumber ASC
-										)
-			SET @StrTradedMonthSymbol = @StrTradedMonthSymbol + Convert(NVARCHAR, RIGHT(YEAR(GetDate()), 1) + 1)
+	
+		SET @StrTradedMonthSymbol=
+		(
+			SELECT TOP 1 StrTradedMonthSymbol
+			FROM @FutureTradedMonths
+			WHERE IsTraded = 1
+			ORDER BY IntMonthNumber ASC
+	   )
+	   SELECT @StrTradedMonthSymbol+Convert(nvarchar,RIGHT(YEAR(GetDate()), 1)+1)
 	END
 	ELSE
 	BEGIN
-		SET @StrTradedMonthSymbol = @StrTradedMonthSymbol + RIGHT(YEAR(GetDate()), 1)
-	END
-		
-	
-
-	IF @IntinterfaceSystem = 1
-	BEGIN
-		IF @strPassword = ''
-			SET @strPassword = '?&Type=F'
-			
-		IF ISNULL(@MarketExchangeCode,'')=''
-		BEGIN
-			SELECT @URL = @URL + 'UserID=' + @strUserName + '&Password=' + @strPassword + '&Symbol=@' + @Commoditycode + @StrTradedMonthSymbol
-		END
-		ELSE
-		BEGIN
-		SELECT @URL = @URL + 'UserID=' + @strUserName + '&Password=' + @strPassword +'&Market='+@MarketExchangeCode+'&Symbol=@' + @Commoditycode + @StrTradedMonthSymbol			
-		END
-		
-	END
-	ELSE IF @IntinterfaceSystem = 2
-	BEGIN
-		IF ISNULL(@MarketExchangeCode,'')=''
-		BEGIN
-			SELECT @URL = @URL + 'username=' + @strUserName + '&password=' + @strPassword + '&symbols=Z' + @Commoditycode + @StrTradedMonthSymbol
-		END
-		ELSE
-		BEGIN
-			SELECT @URL = @URL + 'username=' + @strUserName + '&password=' + @strPassword +'&Market='+@MarketExchangeCode+ '&symbols=Z' + @Commoditycode + @StrTradedMonthSymbol
-		END
-		
-	END
-
-	SELECT @URL AS URL
+		SELECT @StrTradedMonthSymbol+RIGHT(YEAR(GetDate()), 1)
+	END	
 	
 END TRY
 
 BEGIN CATCH
 
 	SET @ErrMsg = ERROR_MESSAGE()
-	SET @ErrMsg = 'uspRKGetElectronicPricingURL: ' + @ErrMsg
+	SET @ErrMsg = 'uspRKGetMarketSymbolSuffix: ' + @ErrMsg
 	RAISERROR (@ErrMsg,16,1,'WITH NOWAIT')
-	
+		
 END CATCH
