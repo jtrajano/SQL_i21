0979733f6bfa9ceea51d4d56ec066824b985a882
Melanie Ferrer <Mel.Ferrer@irely.com>
2015-06-24 13:51:04 +0800

    PR - 188 - Add necessary fields for employee search screen

--- i21Database/dbo/Views/vyuPREmployee.sql
+++ i21Database/dbo/Views/vyuPREmployee.sql
@@ -8,16 +8,17 @@ Emp.strLastName,
 Emp.strFirstName,
 Emp.strNameSuffix,
 Emp.strMiddleName,
-Emp.strWorkPhone,
 Con.strPhone,
 Con.strTitle,
-Con.strDepartment,
 Loc.strAddress,
 Loc.strCity,
 Loc.strState,
 Loc.strZipCode,
-Emp.ysnActive
+Loc.strCountry,
+Emp.ysnActive,
+Pay.strPayGroup
 FROM dbo.tblPREmployee AS Emp
 INNER JOIN dbo.tblEntity AS Ent ON Ent.intEntityId = Emp.intEntityId
 LEFT JOIN dbo.tblEntity AS Con ON Con.[intEntityId] = Emp.intEntityId
 LEFT JOIN dbo.tblEntityLocation AS Loc ON Loc.intEntityId = Emp.intEntityId
+LEFT JOIN dbo.tblPRPayGroup AS Pay ON Pay.intPayGroupId = Emp.intPayGroupId
