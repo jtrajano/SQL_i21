5fbf6c01e54ff4b4086bd5c8e121eeff50b4614f
bonn.ponce <bonn.ponce@irely.com>
2015-06-24 13:27:32 +0800

    CF-135

---
+++ i21Database/dbo/Tables/tblCFTransactionPrice.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tblCFTransactionPrice] (
+    [intTransactionPriceId] INT             IDENTITY (1, 1) NOT NULL,
+    [intTransactionId]      INT             NOT NULL,
+    [strTransactionPriceId] NVARCHAR (MAX)  COLLATE Latin1_General_CI_AS NULL,
+    [dblOriginalAmount]     NUMERIC (18, 6) NULL,
+    [dblCalculatedAmount]   NUMERIC (18, 6) NULL,
+    [intConcurrencyId]      INT             CONSTRAINT [DF_tblTransactionPrice_intConcurrencyId] DEFAULT ((1)) NOT NULL,
+    CONSTRAINT [PK_tblTransactionPrice] PRIMARY KEY CLUSTERED ([intTransactionPriceId] ASC),
+    CONSTRAINT [FK_tblCFTransactionPrice_tblCFTransaction] FOREIGN KEY ([intTransactionId]) REFERENCES [dbo].[tblCFTransaction] ([intTransactionId]) ON DELETE CASCADE
+);
+
