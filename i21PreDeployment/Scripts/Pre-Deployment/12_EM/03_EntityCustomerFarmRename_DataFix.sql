5fb65cdb27ce1786af2295e9b43ba4f8da272dec
Mon Gonzales <mon.gonzales@irely.com>
2015-06-24 17:46:50 +0800

    EM-284

---
+++ i21PreDeployment/Scripts/Pre-Deployment/12_EM/03_EntityCustomerFarmRename_DataFix.sql
@@ -0,0 +1,11 @@
+﻿PRINT '*** Update Customer Farm Id ***'
+
+IF EXISTS(SELECT TOP 1 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE [TABLE_NAME] = 'tblARCustomerFarm' AND [COLUMN_NAME] = 'intCustomerId') 
+BEGIN
+	PRINT '*** Begin updating Customer Farm Id ***'
+	EXEC('ALTER TABLE tblARCustomerFarm  add [intEntityCustomerId] INT NULL')
+	EXEC('update tblARCustomerFarm  set intEntityCustomerId = intCustomerId')
+END
+
+PRINT '*** End Update Customer Farm Id ***'
+
