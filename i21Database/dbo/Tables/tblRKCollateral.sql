0d1fc70492b550b4a2b5ed6099c7cb7bffecfa7a
Ramachandran <rshanmuganathan@BLRRAMW8PRON.irely.net>
2015-06-25 17:42:29 +0530

    RK-82

--- i21Database/dbo/Tables/tblRKCollateral.sql
+++ i21Database/dbo/Tables/tblRKCollateral.sql
@@ -1,4 +1,20 @@
 ﻿CREATE TABLE [dbo].[tblRKCollateral]
 (
-	[Id] INT NOT NULL PRIMARY KEY
+	[intCollateralId] INT IDENTITY(1,1) NOT NULL, 
+	[intConcurrencyId] INT NOT NULL, 
+    [intReceiptNo]  NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL, 
+    [dtmOpenDate] DATETIME  NOT NULL, 
+    [strType] NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL, 
+    [intCommodityId] INT NOT NULL, 
+	[intLocationId] INT NOT NULL, 
+	[strCustomer] NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL, 
+	[dblOriginalQuantity] numeric(18,6) NOT NULL,
+	[dblRemainingQuantity] numeric(18,6) NOT NULL,
+	[intUOMId] INT NOT NULL,
+	[intContractHeaderId] INT NULL, 
+    [intTransNo] INT NOT NULL,
+	[strComments] NVARCHAR(500) COLLATE Latin1_General_CI_AS NULL 
+
+    CONSTRAINT [PK_tblRKCollateral_intCollateralId] PRIMARY KEY ([intCollateralId])
+    CONSTRAINT [FK_tblRKCollateral_tblCTContractHeader_intContractHeaderId] FOREIGN KEY([intContractHeaderId])REFERENCES [dbo].[tblCTContractHeader] ([intContractHeaderId])
 )
---
+++ i21Database/dbo/Tables/tblRKCollateralAdjustment.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[tblRKCollateralAdjustment]
+(
+	[intCollateralAdjustmentId] INT IDENTITY(1,1) NOT NULL, 
+	[intCollateralId] INT NOT NULL,
+	[intConcurrencyId] INT NOT NULL, 
+	[dtmAdjustmentDate] INT NOT NULL, 
+	[dblRemainingQuantity] numeric(18,6)  NOT NULL, 
+	[dblAdjustmentAmount] numeric(18,6)  NOT NULL, 
+	[strComments] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL,
+	[intAdjustmentNo] INT NOT NULL
+	
+	CONSTRAINT [PK_tblRKCollateralAdjustment_intCollateralAdjustmentId] PRIMARY KEY ([intCollateralAdjustmentId])
+	CONSTRAINT [FK_tblRKCollateralAdjustment_tblRKCollateral_intCollateralId] FOREIGN KEY([intCollateralId])REFERENCES [dbo].[tblRKCollateral] ([intCollateralId])
+)
