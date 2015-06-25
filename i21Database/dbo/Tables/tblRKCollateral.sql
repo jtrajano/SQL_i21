29a4d4a23872e746c75a76305f1f1f32ead7694c
Ramachandran <rshanmuganathan@BLRRAMW8PRON.irely.net>
2015-06-25 21:18:26 +0530

    RK-82

--- i21Database/dbo/Tables/tblRKCollateral.sql
+++ i21Database/dbo/Tables/tblRKCollateral.sql
@@ -10,11 +10,14 @@
 	[strCustomer] NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL, 
 	[dblOriginalQuantity] numeric(18,6) NOT NULL,
 	[dblRemainingQuantity] numeric(18,6) NOT NULL,
-	[intUOMId] INT NOT NULL,
+	[intUnitMeasureId] INT NOT NULL,
 	[intContractHeaderId] INT NULL, 
     [intTransNo] INT NOT NULL,
 	[strComments] NVARCHAR(500) COLLATE Latin1_General_CI_AS NULL 
 
-    CONSTRAINT [PK_tblRKCollateral_intCollateralId] PRIMARY KEY ([intCollateralId])
-    CONSTRAINT [FK_tblRKCollateral_tblCTContractHeader_intContractHeaderId] FOREIGN KEY([intContractHeaderId])REFERENCES [dbo].[tblCTContractHeader] ([intContractHeaderId])
+    CONSTRAINT [PK_tblRKCollateral_intCollateralId] PRIMARY KEY ([intCollateralId]),
+    CONSTRAINT [FK_tblRKCollateral_tblCTContractHeader_intContractHeaderId] FOREIGN KEY([intContractHeaderId])REFERENCES [dbo].[tblCTContractHeader] ([intContractHeaderId]),
+	CONSTRAINT [FK_tblRKCollateral_tblICCommodity_intCommodityId] FOREIGN KEY ([intCommodityId]) REFERENCES [tblICCommodity]([intCommodityId]),
+	CONSTRAINT [FK_tblRKCollateral_tblSMCompanyLocation_intCompanyLocationId] FOREIGN KEY ([intLocationId]) REFERENCES [tblSMCompanyLocation]([intCompanyLocationId]),
+	CONSTRAINT [FK_tblRKCollateral_tblICUnitMeasure_intUnitMeasureId] FOREIGN KEY([intUnitMeasureId]) REFERENCES [dbo].[tblICUnitMeasure] ([intUnitMeasureId]),
 )
