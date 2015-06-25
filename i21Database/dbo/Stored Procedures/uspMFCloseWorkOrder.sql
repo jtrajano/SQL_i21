97ce1c7316b4a90f7b7955bf029624b475bf87b2
Premkumar S <prem.selladurai@irely.com>
2015-06-25 21:57:25 +0530

    MFG-80

--- i21Database/dbo/Stored Procedures/uspMFCloseWorkOrder.sql
+++ i21Database/dbo/Stored Procedures/uspMFCloseWorkOrder.sql
@@ -1,58 +1,165 @@
-﻿CREATE PROCEDURE [dbo].[uspMFCloseWorkOrder] (@strXML NVARCHAR(MAX))
+﻿CREATE PROCEDURE uspMFCloseWorkOrder (@strXML NVARCHAR(MAX))
 AS
 BEGIN TRY
-	SET QUOTED_IDENTIFIER OFF
-	SET ANSI_NULLS ON
-	SET NOCOUNT ON
-	SET XACT_ABORT ON
-	SET ANSI_WARNINGS OFF
-
 	DECLARE @idoc INT
 		,@ErrMsg NVARCHAR(MAX)
 		,@intWorkOrderId INT
-		,@dtmCurrentDateTime DATETIME
+		,@intLotId INT
 		,@intUserId INT
-		,@intExecutionOrder INT
-		,@intManufacturingCellId INT
-		,@dtmPlannedDate DATETIME
+		,@strBatchId NVARCHAR(40)
+		,@intTransactionId INT
+		,@strTransactionId NVARCHAR(50)
+		,@dblQuantity NUMERIC(18, 6)
+		,@RecordKey INT
+		,@dtmCurrentDate DATETIME
+		,@strLotNumber nvarchar(50)
 
-	SELECT @dtmCurrentDateTime = GETDATE()
+	SELECT @dtmCurrentDate = GetDate()
 
 	EXEC sp_xml_preparedocument @idoc OUTPUT
 		,@strXML
 
-	SELECT @intUserId = intUserId
-		,@intWorkOrderId = intWorkOrderId
+	SELECT @intWorkOrderId = intWorkOrderId
+		,@intUserId = intUserId
 	FROM OPENXML(@idoc, 'root', 2) WITH (
-			intUserId INT
-			,intWorkOrderId INT
+			intWorkOrderId INT
+			,intUserId INT
 			)
 
-	SELECT @intExecutionOrder = intExecutionOrder
-		,@intManufacturingCellId = intManufacturingCellId
-		,@dtmPlannedDate = dtmPlannedDate
-	FROM dbo.tblMFWorkOrder
-	WHERE intWorkOrderId = @intWorkOrderId
+	IF NOT EXISTS (
+			SELECT *
+			FROM tblMFWorkOrder
+			WHERE intWorkOrderId = @intWorkOrderId
+			)
+	BEGIN
+		RAISERROR (51140
+				,11
+				,1
+				)
+	END
 
-	UPDATE tblMFWorkOrder
-	SET intExecutionOrder = 0
-		,intStatusId = 13
-		,dtmCompletedDate = @dtmCurrentDateTime
-		,dtmLastModified = @dtmCurrentDateTime
+	IF EXISTS (
+			SELECT *
+			FROM dbo.tblMFWorkOrderProducedLot
+			WHERE intWorkOrderId = @intWorkOrderId
+				AND ysnReleased = 0
+				AND ysnProductionReversed = 0
+			)
+	BEGIN
+		RAISERROR (
+				51141
+				,11
+				,1
+				)
+
+		RETURN
+	END
+
+	DECLARE @Lot TABLE (
+		RecordKey INT identity(1, 1)
+		,intLotId INT
+		,strLotNumber nvarchar(50)
+		)
+
+	INSERT INTO @Lot (intLotId,strLotNumber)
+	SELECT PL.intLotId,L.strLotNumber
+	FROM dbo.tblMFWorkOrderProducedLot PL
+	JOIN dbo.tblICLot L ON L.intLotId = PL.intLotId
+	WHERE intWorkOrderId = @intWorkOrderId
+		AND L.intLotStatusId = 3
+		AND ysnProductionReversed = 0
+
+	SELECT @RecordKey = MIN(RecordKey)
+	FROM @Lot
+
+	WHILE @RecordKey IS NOT NULL
+	BEGIN
+		SELECT @intLotId = intLotId,@strLotNumber=strLotNumber
+		FROM @Lot
+		WHERE RecordKey = @RecordKey
+
+		BEGIN TRANSACTION
+
+		DECLARE @STARTING_NUMBER_BATCH AS INT = 3
+
+		EXEC dbo.uspSMGetStartingNumber @STARTING_NUMBER_BATCH
+			,@strBatchId OUTPUT
+
+		DECLARE @GLEntries AS RecapTableType
+
+		INSERT INTO @GLEntries (
+			[dtmDate]
+			,[strBatchId]
+			,[intAccountId]
+			,[dblDebit]
+			,[dblCredit]
+			,[dblDebitUnit]
+			,[dblCreditUnit]
+			,[strDescription]
+			,[strCode]
+			,[strReference]
+			,[intCurrencyId]
+			,[dblExchangeRate]
+			,[dtmDateEntered]
+			,[dtmTransactionDate]
+			,[strJournalLineDescription]
+			,[intJournalLineNo]
+			,[ysnIsUnposted]
+			,[intUserId]
+			,[intEntityId]
+			,[strTransactionId]
+			,[intTransactionId]
+			,[strTransactionType]
+			,[strTransactionForm]
+			,[strModuleName]
+			,[intConcurrencyId]
+			)
+		EXEC dbo.uspICUnpostCosting @intLotId
+			,@strLotNumber
+			,@strBatchId
+			,@intUserId
+
+		EXEC dbo.uspGLBookEntries @GLEntries
+			,0
+
+		UPDATE dbo.tblMFWorkOrderProducedLot
+		SET ysnProductionReversed = 1
+			,dtmLastModified = @dtmCurrentDate
+			,intLastModifiedUserId = @intUserId
+		WHERE intLotId = @intLotId
+			AND intWorkOrderId = @intWorkOrderId
+
+		SELECT @dblQuantity = dblQuantity
+		FROM tblMFWorkOrderProducedLot
+		WHERE intLotId = @intLotId
+			AND intWorkOrderId = @intWorkOrderId
+
+		UPDATE tblMFWorkOrder
+		SET dblProducedQuantity = dblProducedQuantity - @dblQuantity
+		WHERE intWorkOrderId = @intWorkOrderId
+
+		SELECT @RecordKey = MIN(RecordKey)
+		FROM @Lot
+		WHERE RecordKey > @RecordKey
+	END
+
+	UPDATE dbo.tblMFWorkOrder
+	SET intStatusId = 13
+		,dtmCompletedDate = @dtmCurrentDate
+		,dtmLastModified = @dtmCurrentDate
 		,intLastModifiedUserId = @intUserId
 	WHERE intWorkOrderId = @intWorkOrderId
 
-	UPDATE dbo.tblMFWorkOrder
-	SET intExecutionOrder = intExecutionOrder - 1
-	WHERE intManufacturingCellId = @intManufacturingCellId
-		AND dtmPlannedDate = @dtmPlannedDate
-		AND intExecutionOrder > @intExecutionOrder
+	COMMIT TRANSACTION
 
 	EXEC sp_xml_removedocument @idoc
 END TRY
 
 BEGIN CATCH
 	SET @ErrMsg = ERROR_MESSAGE()
+
+	IF XACT_STATE() != 0
+		ROLLBACK TRANSACTION
 
 	IF @idoc <> 0
 		EXEC sp_xml_removedocument @idoc
