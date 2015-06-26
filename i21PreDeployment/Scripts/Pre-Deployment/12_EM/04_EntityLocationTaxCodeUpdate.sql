d98f165e8ad45f72784ee0311c64cc3c085bef96
Mon Gonzales <mon.gonzales@irely.com>
2015-06-26 19:41:52 +0800

    EM-273

---
+++ i21PreDeployment/Scripts/Pre-Deployment/12_EM/04_EntityLocationTaxCodeUpdate.sql
@@ -0,0 +1,10 @@
+﻿PRINT '*** Update Entity Location Tax Code Id ***'
+
+IF EXISTS(SELECT TOP 1 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE [TABLE_NAME] = 'tblEntityLocation' AND [COLUMN_NAME] = 'intTaxCodeId') 
+BEGIN
+	PRINT '*** Begin Updatting Entity Location Tax Code Id***'	
+	EXEC('update tblEntityLocation set intTaxCodeId = null where intTaxCodeId not in( select intTaxCodeId from tblSMTaxCode )')
+END
+
+PRINT '*** EndUpdate Entity Location Tax Code Id ***'
+
