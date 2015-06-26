f6dda220302c47b1c24ca19d4da015759b572fa2
Kelvin Quinto <kelvin.quinto@irely.com>
2015-06-26 08:21:31 +0800

    AR-1301

--- i21Database/dbo/Tables/tblARCompanyPreference.sql
+++ i21Database/dbo/Tables/tblARCompanyPreference.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[tblARCompanyPreference]
 (
-	[intCompanyPreferenceId] INT NOT NULL PRIMARY KEY, 
+	[intCompanyPreferenceId] INT NOT NULL PRIMARY KEY IDENTITY, 
     [intARAccountId] INT NULL, 
     [intDiscountAccountId] INT NULL,
 	[intConcurrencyId] INT NOT NULL DEFAULT 1
