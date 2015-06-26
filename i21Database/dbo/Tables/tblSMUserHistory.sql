1cfaf5dda8472f994763808192adfcd789a39eb0
von.pasion <von.pasion@irely.com>
2015-06-26 16:37:41 +0800

    FRM-2201

--- i21Database/dbo/Tables/tblSMUserHistory.sql
+++ i21Database/dbo/Tables/tblSMUserHistory.sql
@@ -1,11 +1,13 @@
 ﻿CREATE TABLE [dbo].[tblSMUserHistory] (
     [intUserHistoryId]   INT              IDENTITY (1, 1) NOT NULL,
-    [strType]			 NVARCHAR (100)   COLLATE Latin1_General_CI_AS NULL,
+    [strActionType]		 NVARCHAR(100)    COLLATE Latin1_General_CI_AS NULL,
+	[strTransactionType] NVARCHAR(100)	  COLLATE Latin1_General_CI_AS NULL, 
+	[strRecordNo]		 NVARCHAR(50)	  COLLATE Latin1_General_CI_AS NULL, 
 	[strDescription]	 NVARCHAR (MAX)   COLLATE Latin1_General_CI_AS NULL,
     [dtmDate]			 DATETIME         NOT NULL,
     [intEntityId]        INT              NULL,
     [intConcurrencyId]   INT              NOT NULL,
-	CONSTRAINT [FK_dbo.tblSMUserHistory_dbo.tblEntity_intEntityId] FOREIGN KEY ([intEntityId]) REFERENCES [dbo].[tblEntity] ([intEntityId]),
+    CONSTRAINT [FK_dbo.tblSMUserHistory_dbo.tblEntity_intEntityId] FOREIGN KEY ([intEntityId]) REFERENCES [dbo].[tblEntity] ([intEntityId]),
     CONSTRAINT [PK_dbo.tblSMUserHistory] PRIMARY KEY CLUSTERED ([intUserHistoryId] ASC)
 );
 
