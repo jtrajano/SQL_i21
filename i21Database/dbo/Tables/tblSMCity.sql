5dcc7c69747275e4e4aa9cc2e124bccd46d636a6
Dan Jalimao <Dan.Jalimao@irely.com>
2015-06-26 11:26:22 +0800

    FCOM-554

--- i21Database/dbo/Tables/tblSMCity.sql
+++ i21Database/dbo/Tables/tblSMCity.sql
@@ -4,7 +4,9 @@
     [strCity] NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL, 
     [intCountryId] INT NOT NULL, 
     [strState] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
-    [ysnPort] BIT NOT NULL DEFAULT 0, 
+	[ysnRegion] BIT NOT NULL DEFAULT 0,
+    [ysnPort] BIT NOT NULL DEFAULT 0,
+	[ysnArbitration] BIT NOT NULL DEFAULT 0,
     [intConcurrencyId] INT NOT NULL DEFAULT 1, 
     CONSTRAINT [FK_tblSMCity_tblSMCountry] FOREIGN KEY (intCountryId) REFERENCES tblSMCountry(intCountryID), 
     CONSTRAINT [AK_tblSMCity_City_Country_State] UNIQUE (strCity, intCountryId, strState)
