1b701ad5a8c3cfcfe89005f027f777fb7764db86
Satya <ssahoo@BLRSATYAWIN7.irely.net>
2015-06-29 16:33:40 +0530

    CT-189

---
+++ i21Database/dbo/Tables/tblCTCompanyPreference.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[tblCTCompanyPreference]
+(
+	[intCompanyPreferenceId] INT NOT NULL IDENTITY, 
+    [ysnAssignSaleContract] BIT NULL, 
+    [ysnAssignPurchaseContract] BIT NULL,
+	[ysnRequireDPContract] BIT NULL,
+	[ysnApplyScaleToBasis] BIT NULL,
+	[intPriceCalculationTypeId] INT NULL,
+	CONSTRAINT [PK_tblCTCompanyPreference_intCompanyPreferenceId] PRIMARY KEY CLUSTERED ([intCompanyPreferenceId] ASC), 
+)
