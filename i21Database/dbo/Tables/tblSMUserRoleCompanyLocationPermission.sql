7121c4e14026dc953d11b1848d1ddb3aa69445a0
Dan Jalimao <Dan.Jalimao@irely.com>
2015-06-29 17:58:01 +0800

    FCOM-535

--- i21Database/dbo/Tables/tblSMUserRoleCompanyLocationPermission.sql
+++ i21Database/dbo/Tables/tblSMUserRoleCompanyLocationPermission.sql
@@ -3,7 +3,6 @@
 	[intUserRoleCompanyLocationPermissionId] INT NOT NULL PRIMARY KEY IDENTITY, 
     [intUserRoleId] INT NOT NULL, 
     [intCompanyLocationId] INT NOT NULL, 
-    [strPermission] NVARCHAR(20) NOT NULL, 
     [intConcurrencyId] INT NOT NULL DEFAULT 1, 
     CONSTRAINT [FK_tblSMUserRoleCompanyLocationPermission_tblSMUserRole] FOREIGN KEY ([intUserRoleId]) REFERENCES [tblSMUserRole]([intUserRoleID]) ON DELETE CASCADE, 
     CONSTRAINT [FK_tblSMUserRoleCompanyLocationPermission_tblSMCompanyLocation] FOREIGN KEY ([intCompanyLocationId]) REFERENCES [tblSMCompanyLocation]([intCompanyLocationId]) ON DELETE CASCADE
