aa676c8f12346b0254f5e60f56f254d30e53c6d1
Ramachandran <rshanmuganathan@BLRRAMW8PRON.irely.net>
2015-06-30 17:01:25 +0530

    RK-66

--- i21Database/dbo/Tables/tblRKCollateralAdjustment.sql
+++ i21Database/dbo/Tables/tblRKCollateralAdjustment.sql
@@ -3,12 +3,11 @@
 	[intCollateralAdjustmentId] INT IDENTITY(1,1) NOT NULL, 
 	[intCollateralId] INT NOT NULL,
 	[intConcurrencyId] INT NOT NULL, 
-	[dtmAdjustmentDate] INT NOT NULL, 
-	[dblRemainingQuantity] numeric(18,6)  NOT NULL, 
+	[dtmAdjustmentDate] DATETIME NOT NULL, 
 	[dblAdjustmentAmount] numeric(18,6)  NOT NULL, 
 	[strComments] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL,
-	[intAdjustmentNo] INT NOT NULL
+	[strAdjustmentNo] NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL
 	
 	CONSTRAINT [PK_tblRKCollateralAdjustment_intCollateralAdjustmentId] PRIMARY KEY ([intCollateralAdjustmentId])
-	CONSTRAINT [FK_tblRKCollateralAdjustment_tblRKCollateral_intCollateralId] FOREIGN KEY([intCollateralId])REFERENCES [dbo].[tblRKCollateral] ([intCollateralId])
+	CONSTRAINT [FK_tblRKCollateralAdjustment_tblRKCollateral_intCollateralId] FOREIGN KEY([intCollateralId])REFERENCES [dbo].[tblRKCollateral] ([intCollateralId]) ON DELETE CASCADE
 )
