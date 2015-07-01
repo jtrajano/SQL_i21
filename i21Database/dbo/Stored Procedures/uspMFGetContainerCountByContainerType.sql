7d1f7059adf55b43054946afa458878b8f9e5baa
Premkumar S <prem.selladurai@irely.com>
2015-07-01 18:46:04 +0530

    MFG-80

---
+++ i21Database/dbo/Stored Procedures/uspMFGetContainerCountByContainerType.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE uspMFGetContainerCountByContainerType (
+	@intLocationId INT
+	,@strContainerType NVARCHAR(MAX)
+	)
+AS
+BEGIN
+	SELECT Count(*) As ContainerCount
+	FROM dbo.tblICContainer C
+	JOIN dbo.tblICStorageLocation L ON L.intStorageLocationId = C.intStorageLocationId
+	JOIN dbo.tblICContainerType CT ON CT.intContainerTypeId = C.intContainerTypeId
+	WHERE L.intLocationId = @intLocationId
+		AND CT.strDisplayMember IN (
+			SELECT Item Collate Latin1_General_CI_AS
+			FROM [dbo].[fnSplitString](@strContainerType, ',')
+			)
+END
---
+++ i21Database/dbo/Stored Procedures/uspMFGetProductCountBySubLocation.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE uspMFGetProductCountBySubLocation (@intSubLocationId INT)
+AS
+BEGIN
+	SELECT Count(*) AS ProductCount
+	FROM dbo.tblMFRecipe R
+	JOIN dbo.tblICItem I ON I.intItemId = R.intItemId
+	JOIN dbo.tblICItemUOM IU ON IU.intItemId = I.intItemId
+	JOIN dbo.tblICUnitMeasure U ON U.intUnitMeasureId = IU.intUnitMeasureId
+	JOIN dbo.tblSMCompanyLocationSubLocationCategory C ON C.intCategoryId = I.intCategoryId
+		AND C.intCompanyLocationSubLocationId = @intSubLocationId
+		AND R.ysnActive = 1
+		AND IU.ysnStockUnit = 1
+		AND I.strStatus='Active'
+END
