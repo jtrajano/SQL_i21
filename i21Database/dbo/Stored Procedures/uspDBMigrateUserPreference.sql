1cd68ca659a804f84dc65173982b777eb9a746e1
Randy Honorato <randy.honorato@irely.com>
2015-07-01 16:30:13 +0800

    IG-1322 - Breaking changes on Company Pref.

--- i21Database/dbo/Stored Procedures/uspDBMigrateUserPreference.sql
+++ i21Database/dbo/Stored Procedures/uspDBMigrateUserPreference.sql
@@ -5,7 +5,7 @@ BEGIN
 	PRINT N'MIGRATING tblDBUserPreference from tblSMPreference'
 
 	DECLARE @intPreferenceID int
-	DECLARE @intUserId int
+	DECLARE @intUserSecurityId int
 
 	DECLARE @AutoPanelWidth bit
 	DECLARE @AutoRefresh bit
@@ -31,41 +31,41 @@ BEGIN
  
 
 	OPEN db_cursor   
-	FETCH NEXT FROM db_cursor INTO @intUserId
+	FETCH NEXT FROM db_cursor INTO @intUserSecurityId
 
 	WHILE @@FETCH_STATUS = 0   
 	BEGIN
-		set @AutoPanelWidth = (SELECT strValue FROM tblSMPreferences where intUserID = @intUserId AND strPreference = 'AutoPanelWidth')
-		set @AutoRefresh = (SELECT strValue FROM tblSMPreferences where intUserID = @intUserId AND strPreference = 'AutoRefresh')
-		set @AutoRefreshMinute = (SELECT strValue FROM tblSMPreferences where intUserID = @intUserId AND strPreference = 'AutoRefreshMinute')
-		set @ColumnFiltering = (SELECT strValue FROM tblSMPreferences where intUserID = @intUserId AND strPreference = 'ColumnFiltering')
-		set @ColumnMoving = (SELECT strValue FROM tblSMPreferences where intUserID = @intUserId AND strPreference = 'ColumnMoving')
-		set @ColumnResizing = (SELECT strValue FROM tblSMPreferences where intUserID = @intUserId AND strPreference = 'ColumnResizing')
-		set @ColumnSorting = (SELECT strValue FROM tblSMPreferences where intUserID = @intUserId AND strPreference = 'ColumnSorting')
-		set @Columns = (SELECT strValue FROM tblSMPreferences where intUserID = @intUserId AND strPreference = 'DashboardColumns')
-		set @ColumnWidth1 = (SELECT strValue FROM tblSMPreferences where intUserID = @intUserId AND strPreference = 'DashboardColumnWidth1')
-		set @ColumnWidth2 = (SELECT strValue FROM tblSMPreferences where intUserID = @intUserId AND strPreference = 'DashboardColumnWidth2')
-		set @ColumnWidth3 = (SELECT strValue FROM tblSMPreferences where intUserID = @intUserId AND strPreference = 'DashboardColumnWidth3')
-		set @ColumnWidth4 = (SELECT strValue FROM tblSMPreferences where intUserID = @intUserId AND strPreference = 'DashboardColumnWidth4')
-		set @DefaultTabId = (SELECT strValue FROM tblSMPreferences where intUserID = @intUserId AND strPreference = 'DefaultTabID')
-		set @ExportAll = (SELECT strValue FROM tblSMPreferences where intUserID = @intUserId AND strPreference = 'ExportAll')
-		set @PrintAll = (SELECT strValue FROM tblSMPreferences where intUserID = @intUserId AND strPreference = 'PrintAll')
-		set @RefreshTab = (SELECT strValue FROM tblSMPreferences where intUserID = @intUserId AND strPreference = 'RefreshTab')
-		set @SaveGridLayout = (SELECT strValue FROM tblSMPreferences where intUserID = @intUserId AND strPreference = 'SaveGridLayout')
+		set @AutoPanelWidth = (SELECT strValue FROM tblSMPreferences where intUserID = @intUserSecurityId AND strPreference = 'AutoPanelWidth')
+		set @AutoRefresh = (SELECT strValue FROM tblSMPreferences where intUserID = @intUserSecurityId AND strPreference = 'AutoRefresh')
+		set @AutoRefreshMinute = (SELECT strValue FROM tblSMPreferences where intUserID = @intUserSecurityId AND strPreference = 'AutoRefreshMinute')
+		set @ColumnFiltering = (SELECT strValue FROM tblSMPreferences where intUserID = @intUserSecurityId AND strPreference = 'ColumnFiltering')
+		set @ColumnMoving = (SELECT strValue FROM tblSMPreferences where intUserID = @intUserSecurityId AND strPreference = 'ColumnMoving')
+		set @ColumnResizing = (SELECT strValue FROM tblSMPreferences where intUserID = @intUserSecurityId AND strPreference = 'ColumnResizing')
+		set @ColumnSorting = (SELECT strValue FROM tblSMPreferences where intUserID = @intUserSecurityId AND strPreference = 'ColumnSorting')
+		set @Columns = (SELECT strValue FROM tblSMPreferences where intUserID = @intUserSecurityId AND strPreference = 'DashboardColumns')
+		set @ColumnWidth1 = (SELECT strValue FROM tblSMPreferences where intUserID = @intUserSecurityId AND strPreference = 'DashboardColumnWidth1')
+		set @ColumnWidth2 = (SELECT strValue FROM tblSMPreferences where intUserID = @intUserSecurityId AND strPreference = 'DashboardColumnWidth2')
+		set @ColumnWidth3 = (SELECT strValue FROM tblSMPreferences where intUserID = @intUserSecurityId AND strPreference = 'DashboardColumnWidth3')
+		set @ColumnWidth4 = (SELECT strValue FROM tblSMPreferences where intUserID = @intUserSecurityId AND strPreference = 'DashboardColumnWidth4')
+		set @DefaultTabId = (SELECT strValue FROM tblSMPreferences where intUserID = @intUserSecurityId AND strPreference = 'DefaultTabID')
+		set @ExportAll = (SELECT strValue FROM tblSMPreferences where intUserID = @intUserSecurityId AND strPreference = 'ExportAll')
+		set @PrintAll = (SELECT strValue FROM tblSMPreferences where intUserID = @intUserSecurityId AND strPreference = 'PrintAll')
+		set @RefreshTab = (SELECT strValue FROM tblSMPreferences where intUserID = @intUserSecurityId AND strPreference = 'RefreshTab')
+		set @SaveGridLayout = (SELECT strValue FROM tblSMPreferences where intUserID = @intUserSecurityId AND strPreference = 'SaveGridLayout')
 		
 
 			
 		--Check if User already exist
-		IF NOT EXISTS (SELECT TOP 1 1 FROM [dbo].[tblDBUserPreference] WHERE intUserId = @intUserId)			
+		IF NOT EXISTS (SELECT TOP 1 1 FROM [dbo].[tblDBUserPreference] WHERE intUserSecurityId = @intUserSecurityId)			
 			BEGIN
 
 				INSERT INTO [dbo].[tblDBUserPreference] 
-					   ([intAutoRefreshMinute], [intColumns], [intColumnWidth1], [intColumnWidth2], [intColumnWidth3], [intColumnWidth4], [intDafaultTabId], [intUserId], [ysnAutoPanelWidth], [ysnAutoRefresh], [ysnColumnFiltering], [ysnColumnMoving], [ysnColumnResizing], [ysnColumnSorting], [ysnExportAll], [ysnPrintAll], [ysnRefreshTab], [ysnSaveGridLayout])
-				 VALUES (@AutoRefreshMinute, @Columns, @ColumnWidth1, @ColumnWidth2, @ColumnWidth3, @ColumnWidth4, @DefaultTabId, @intUserId, @AutoPanelWidth, @AutoRefresh, @ColumnFiltering, @ColumnMoving, @ColumnResizing, @ColumnSorting, @ExportAll, @PrintAll, @RefreshTab, @SaveGridLayout);
+					   ([intAutoRefreshMinute], [intColumns], [intColumnWidth1], [intColumnWidth2], [intColumnWidth3], [intColumnWidth4], [intDafaultTabId], [intUserSecurityId], [ysnAutoPanelWidth], [ysnAutoRefresh], [ysnColumnFiltering], [ysnColumnMoving], [ysnColumnResizing], [ysnColumnSorting], [ysnExportAll], [ysnPrintAll], [ysnRefreshTab], [ysnSaveGridLayout])
+				 VALUES (@AutoRefreshMinute, @Columns, @ColumnWidth1, @ColumnWidth2, @ColumnWidth3, @ColumnWidth4, @DefaultTabId, @intUserSecurityId, @AutoPanelWidth, @AutoRefresh, @ColumnFiltering, @ColumnMoving, @ColumnResizing, @ColumnSorting, @ExportAll, @PrintAll, @RefreshTab, @SaveGridLayout);
 			END 
 
 	
-	FETCH NEXT FROM db_cursor INTO @intUserId
+	FETCH NEXT FROM db_cursor INTO @intUserSecurityId
 	END   
 
 	CLOSE db_cursor   
