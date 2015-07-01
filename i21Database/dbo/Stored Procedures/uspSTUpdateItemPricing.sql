504f8577a0bed514a45b53b9eb8c4f5ae11ee259
Ravikumar Kumashi <ravikumar.k@irely.com>
2015-07-01 19:34:57 +0530

    ST-135

--- i21Database/dbo/Stored Procedures/uspSTUpdateItemPricing.sql
+++ i21Database/dbo/Stored Procedures/uspSTUpdateItemPricing.sql
@@ -1,59 +1,45 @@
 ﻿CREATE PROCEDURE [dbo].[uspSTUpdateItemPricing]
-	-- Add the parameters for the stored procedure here
 	@XML varchar(max)
 	
 AS
 BEGIN TRY
-
-    SET QUOTED_IDENTIFIER OFF
-    SET ANSI_NULLS ON
-    SET NOCOUNT ON
-    SET XACT_ABORT ON
-    SET ANSI_WARNINGS OFF
-
-	DECLARE @ErrMsg					NVARCHAR(MAX),
-	        @idoc					INT,
-			@ItemLocation 			NVARCHAR(MAX),
-			@ItemVendor             NVARCHAR(MAX),
-			@ItemCategory           NVARCHAR(MAX),
-			@ItemFamily             NVARCHAR(MAX),
-			@ItemClass              NVARCHAR(MAX),
-			@ItemManufacturer       NVARCHAR(MAX),
-			@ItemDescription        NVARCHAR(250),
-			@ItemRegion             NVARCHAR(6),
-			@ItemDistrict           NVARCHAR(6),
-			@ItemState              NVARCHAR(2),
-			@ItemUpcCode            NVARCHAR(MAX),
-			@ItemStandardCost       DECIMAL (7,6),
-			@ItemRetailPrice        DECIMAL (7,2),
-			@ItemSalesPrice         DECIMAL (5,2),
-			@ItemQuantity           INT,
-			@ItemUom                NVARCHAR(10),
-			@SalesStartDate		    DATETIME,
-			@SalesEndDate   		DATETIME
+	DECLARE @ErrMsg			    NVARCHAR(MAX),
+	        @idoc			    INT,
+			@Location 			NVARCHAR(MAX),
+			@Vendor             NVARCHAR(MAX),
+			@Category           NVARCHAR(MAX),
+			@Family             NVARCHAR(MAX),
+			@Class              NVARCHAR(MAX),
+			@Description        NVARCHAR(250),
+			@Region             NVARCHAR(6),
+			@District           NVARCHAR(6),
+			@State              NVARCHAR(2),
+			@UpcCode            NVARCHAR(MAX),
+			@StandardCost       DECIMAL (18,6),
+			@RetailPrice        DECIMAL (18,6),
+			@SalesPrice         DECIMAL (18,6),
+		    @SalesStartDate		DATETIME,
+			@SalesEndDate   	DATETIME
 		
 	                  
 	EXEC sp_xml_preparedocument @idoc OUTPUT, @XML 
 	
 	SELECT	
-			@ItemLocation		 =	Location,
-            @ItemVendor          =   Vendor,
-			@ItemCategory        =   Category,
-			@ItemFamily          =   Family,
-            @ItemClass           =   Class,
-			@ItemManufacturer    =   Manufacturer,
-            @ItemDescription     =   ItmDescription,
-			@ItemRegion          =   Region,
-			@ItemDistrict        =   District,
-			@ItemState           =   States,
-			@ItemUpcCode         =   UPCCode,
-			@ItemStandardCost 	 = 	 Cost,
-			@ItemRetailPrice   	 =	 Retail,
-			@ItemSalesPrice		 =	 SalesPrice,
-			@ItemQuantity		 =	 QuantityCase,
-			@ItemUom		     =	 UOM,
-			@SalesStartDate		 =	 SalesStartingDate,
-			@SalesEndDate		 =	 SalesEndingDate
+			@Location		 =	 Location,
+            @Vendor          =   Vendor,
+			@Category        =   Category,
+			@Family          =   Family,
+            @Class           =   Class,
+            @Description     =   ItmDescription,
+			@Region          =   Region,
+			@District        =   District,
+			@State           =   States,
+			@UpcCode         =   UPCCode,
+			@StandardCost 	 = 	 Cost,
+			@RetailPrice   	 =	 Retail,
+			@SalesPrice		 =	 SalesPrice,
+			@SalesStartDate	 =	 SalesStartingDate,
+			@SalesEndDate	 =	 SalesEndingDate
 		
 	FROM	OPENXML(@idoc, 'root',2)
 	WITH
@@ -63,17 +49,14 @@ BEGIN TRY
 			Category		        NVARCHAR(MAX),
 			Family	     	        NVARCHAR(MAX),
 			Class	     	        NVARCHAR(MAX),
-			Manufacturer	     	NVARCHAR(MAX),
 			ItmDescription		    NVARCHAR(250),
 			Region                  NVARCHAR(6),
 			District                NVARCHAR(6),
 			States                  NVARCHAR(2),
 			UPCCode		            NVARCHAR(MAX),
-			Cost		            DECIMAL (7,6),
-			Retail		            DECIMAL (7,2),
-			SalesPrice       		DECIMAL (5,2),
-			QuantityCase			INT,
-			UOM                     NVARCHAR(10),
+			Cost		            DECIMAL (18,6),
+			Retail		            DECIMAL (18,6),
+			SalesPrice       		DECIMAL (18,6),
 			SalesStartingDate		DATETIME,
 			SalesEndingDate			DATETIME
 	)  
@@ -81,158 +64,238 @@ BEGIN TRY
 
       DECLARE @SQL1 NVARCHAR(MAX)
 
-	  set @SQL1 = 'update stpbkmst set ' 
-	  if (@ItemStandardCost IS NOT NULL)
+	  DECLARE @UpdateCount INT
+
+	  set @UpdateCount = 0
+	  
+	  IF ((@StandardCost IS NOT NULL)
+      OR (@RetailPrice IS NOT NULL))
 	      BEGIN
-          set @SQL1 = @SQL1 + 'stpbk_casecost = ''' + LTRIM(@ItemStandardCost) + ''''
-		  END
 
-      if (@ItemRetailPrice IS NOT NULL)  
-	      BEGIN
-	      if (@ItemStandardCost IS NOT NULL)
- 	         set @SQL1 = @SQL1 + ' , stpbk_price = ''' + LTRIM(@ItemRetailPrice) + ''''
-          else
-	         set @SQL1 = @SQL1 + ' stpbk_price = ''' + LTRIM(@ItemRetailPrice) + '''' 
-		  END
+		     set @SQL1 = ' update tblICItemPricing set '
 
-      if (@ItemRetailPrice IS NOT NULL)  
-	      BEGIN
- 	         set @SQL1 = @SQL1 + ' , stpbk_pm = 1 '
-          END
+			 if (@StandardCost IS NOT NULL)
+	         BEGIN
+                  set @SQL1 = @SQL1 + 'dblStandardCost = ''' + LTRIM(@StandardCost) + ''''
+		     END
 
+			 if (@RetailPrice IS NOT NULL)  
+	         BEGIN
+	             if (@RetailPrice IS NOT NULL)
+ 	                 set @SQL1 = @SQL1 + ' , dblSalePrice = ''' + LTRIM(@RetailPrice) + ''''
+                 else
+	                set @SQL1 = @SQL1 + ' dblSalePrice = ''' + LTRIM(@RetailPrice) + '''' 
+		     END
 
-      if (@ItemSalesPrice IS NOT NULL)
-	     BEGIN
-	     if ((@ItemStandardCost IS NOT NULL) OR (@ItemRetailPrice IS NOT NULL))
-             set @SQL1 = @SQL1 + ' , stpbk_sale_price = ''' + LTRIM(@ItemSalesPrice) + ''''
-         else
-             set @SQL1 = @SQL1 + ' stpbk_sale_price = ''' + LTRIM(@ItemSalesPrice) + ''''		        
-	     END
+    		
+             set @SQL1 = @SQL1 + ' where 1=1 ' 
 
-      if (@ItemSalesPrice IS NOT NULL)
-	     BEGIN
-	        set @SQL1 = @SQL1 + ' , stpbk_sale_pm = 1 '
-	     END
-        
-      if (@SalesStartDate IS NOT NULL)
-	      BEGIN
-	      if ((@ItemStandardCost IS NOT NULL) OR (@ItemRetailPrice IS NOT NULL) 
-		  OR (@ItemSalesPrice IS NOT NULL))
-               set @SQL1 = @SQL1 + ', stpbk_sale_startdate = ''' 
-			   + (SELECT CONVERT(NVARCHAR(12), CONVERT(DATETIME,@SalesStartDate),112)) + ''''
-		  else
-		       set @SQL1 = @SQL1 + ' stpbk_sale_startdate = ''' 
-			   + (SELECT CONVERT(NVARCHAR(12), CONVERT(DATETIME,@SalesStartDate),112)) + ''''
-		  END
+			 if (@Location IS NOT NULL)
+		         BEGIN 
+		               set @SQL1 = @SQL1 +  ' and  tblICItemPricing.intItemLocationId
+		                IN (select intItemLocationId from tblICItemLocation where intLocationId
+		                IN (select intLocationId from tblICItemLocation where intLocationId 
+						IN (' + CAST(@Location as NVARCHAR) + ')' + '))'
+		         END
 
-      if (@SalesEndDate IS NOT NULL)
-	      BEGIN
-	      if ((@ItemStandardCost IS NOT NULL) OR (@ItemRetailPrice IS NOT NULL)
-		   OR (@ItemSalesPrice IS NOT NULL) OR (@SalesStartDate IS NOT NULL))
-	           set @SQL1 = @SQL1 + ' , stpbk_sale_enddate = ''' 
-			   + (SELECT CONVERT(NVARCHAR(12), CONVERT(DATETIME,@SalesEndDate),112)) + ''''
-          else
-		       set @SQL1 = @SQL1 + ' stpbk_sale_enddate = ''' 
-			   + (SELECT CONVERT(NVARCHAR(12), CONVERT(DATETIME,@SalesEndDate),112)) + ''''
-		  END
+				 if (@Vendor IS NOT NULL)
+		             BEGIN 
+		                  set @SQL1 = @SQL1 +  ' and  tblICItemPricing.intItemLocationId
+		                  IN (select intItemLocationId from tblICItemLocation where intVendorId
+		                  IN (select intEntityId from tblEntity where intEntityId 
+						  IN (' + CAST(@Vendor as NVARCHAR) + ')' + '))'
+		             END
 
-      if (@ItemQuantity IS NOT NULL)
-	     BEGIN
-         if ((@ItemStandardCost IS NOT NULL) OR (@ItemRetailPrice IS NOT NULL)
-		 OR (@ItemSalesPrice IS NOT NULL) OR (@SalesStartDate IS NOT NULL)
-		 OR (@SalesEndDate IS NOT NULL))
-        	    set @SQL1 = @SQL1 + ' , stpbk_casesize = ''' + LTRIM(@ItemQuantity) + ''''
-		 else
-		        set @SQL1 = @SQL1 + '  stpbk_casesize = ''' + LTRIM(@ItemQuantity) + ''''
-		 END
+				 if (@Category IS NOT NULL)
+		             BEGIN
+     	                 set @SQL1 = @SQL1 +  ' and tblICItemPricing.intItemId  
+		                 IN (select intItemId from tblICItem where intCategoryId IN
+			             (select intCategoryId from tblICCategory where intCategoryId 
+						 IN (' + CAST(@Category as NVARCHAR) + ')' + '))'
+		             END
 
-      if (@ItemUom IS NOT NULL)
-	     BEGIN
-         if ((@ItemStandardCost IS NOT NULL) OR (@ItemRetailPrice IS NOT NULL)
-		 OR (@ItemSalesPrice IS NOT NULL) OR (@SalesStartDate IS NOT NULL)
-		 OR (@SalesEndDate IS NOT NULL) OR (@ItemQuantity IS NOT NULL) )
-        	    set @SQL1 = @SQL1 + ' , stpbk_itemuom = ''' + LTRIM(@ItemUom) + ''''
-		 else
-		        set @SQL1 = @SQL1 + '  stpbk_itemuom = ''' + LTRIM(@ItemUom) + ''''
-		 END
-     
-	 set @SQL1 = @SQL1 + ' where 1=1 ' 
+			      if (@Family IS NOT NULL)
+		              BEGIN
+  			              set @SQL1 = @SQL1 +  ' and tblICItemPricing.intItemLocationId IN 
+			              (select intItemLocationId from tblICItemLocation where intFamilyId IN
+			              (select intFamilyId from tblICItemLocation where intFamilyId 
+						  IN (' + CAST(@Family as NVARCHAR) + ')' + '))'
+		              END
 
-	 if (@ItemLocation IS NOT NULL)
-		 BEGIN 
-		   set @SQL1 = @SQL1 +  ' and  stpbk_store_name IN 
-			 (''' + replace((SELECT (CAST(@ItemLocation AS NVARCHAR(MAX)))),',',''',''') +''')'
-		 END
-       
-	 if (@ItemVendor IS NOT NULL)
-	     BEGIN
-		   set @SQL1 = @SQL1 +  ' and  stpbk_vnd_id IN 
-		   (''' + replace((SELECT (CAST(@ItemVendor AS NVARCHAR(MAX)))),', ',''',''') +''')'
-		 END
-         
-	  if (@ItemCategory IS NOT NULL)
+				  if (@Class  IS NOT NULL)
+		              BEGIN
+		                  set @SQL1 = @SQL1 +  ' and tblICItemPricing.intItemLocationId IN 
+			              (select intItemLocationId from tblICItemLocation where intClassId IN
+			              (select intClassId from tblICItemLocation where intClassId 
+						  IN (' + CAST(@Class as NVARCHAR) + ')' + '))'
+		              END
+
+				  if (@UpcCode IS NOT NULL)
+			          BEGIN
+				          set @SQL1 = @SQL1 +  ' and tblICItemPricing.intItemId  
+                          IN (select intItemId from tblICItemUOM where  intItemUOMId IN
+		                  (select intItemUOMId from tblICItemUOM  where intItemUOMId 
+						  IN (' + CAST(@UpcCode as NVARCHAR) + ')' + '))'
+			          END
+
+			      if ((@Region IS NOT NULL)
+				  and(@Region != ''))
+				 	   BEGIN
+						 set @SQL1 = @SQL1 +  ' and tblICItemPricing.intItemLocationId IN 
+						 (select intItemLocationId from tblICItemLocation where intLocationId IN 
+						 (select intCompanyLocationId from tblSTStore where strRegion 
+						 IN ( ''' + (@Region) + ''')' + '))'
+				       END
+
+				  if ((@District IS NOT NULL)
+				  and(@District != ''))
+					  BEGIN
+						 set @SQL1 = @SQL1 +  ' and tblICItemPricing.intItemLocationId IN 
+						 (select intItemLocationId from tblICItemLocation where intLocationId IN 
+						 (select intCompanyLocationId from tblSTStore where strDestrict 
+						 IN ( ''' + (@District) + ''')' + '))'
+					  END
+
+                  if ((@State IS NOT NULL)
+				  and(@State != ''))
+					  BEGIN
+						 set @SQL1 = @SQL1 +  ' and tblICItemPricing.intItemLocationId IN 
+						 (select intItemLocationId from tblICItemLocation where intLocationId IN 
+						 (select intCompanyLocationId from tblSTStore where strState 
+						 IN ( ''' + (@State) + ''')' + '))'
+					  END
+
+                   if ((@Description IS NOT NULL)
+				   and (@Description != ''))
+					BEGIN
+					   set @SQL1 = @SQL1 +  ' and tblICItemPricing.intItemId IN 
+					   (select intItemId from tblICItem where strDescription 
+					    like ''%' + LTRIM(@Description) + '%'' )'
+					END
+ 		  END
+
+		  exec (@SQL1)
+	      select  @UpdateCount =   @UpdateCount + (@@ROWCOUNT) 
+
+		  if ((@SalesPrice IS NOT NULL)
+		  OR (@SalesStartDate IS NOT NULL)
+		  OR (@SalesEndDate IS NOT NULL))
 		  BEGIN
-     	   set @SQL1 = @SQL1 +  ' and  stpbk_deptno IN 
-			 (''' + replace((SELECT (CAST(@ItemCategory AS NVARCHAR(MAX)))),',',''',''') +''')'
+		       set @SQL1 = ' update tblICItemSpecialPricing set '
+
+			   if (@SalesPrice IS NOT NULL)
+	           BEGIN
+                  set @SQL1 = @SQL1 + 'dblUnitAfterDiscount = ''' + LTRIM(@SalesPrice) + ''''
+		       END
+
+			   if (@SalesStartDate IS NOT NULL)  
+	           BEGIN
+	               if (@SalesPrice IS NOT NULL)
+ 	                   set @SQL1 = @SQL1 + ' , dtmBeginDate = ''' + LTRIM(@SalesStartDate) + ''''
+                  else
+	                 set @SQL1 = @SQL1 + ' dtmBeginDate = ''' + LTRIM(@SalesStartDate) + '''' 
+		       END
+
+			   if (@SalesEndDate IS NOT NULL)  
+	           BEGIN
+	               if ((@SalesPrice IS NOT NULL)
+				   OR (@SalesStartDate IS NOT NULL))
+ 	                   set @SQL1 = @SQL1 + ' , dtmEndDate = ''' + LTRIM(@SalesEndDate) + ''''
+                  else
+	                   set @SQL1 = @SQL1 + ' dtmEndDate = ''' + LTRIM(@SalesEndDate) + '''' 
+		       END
+
+			   set @SQL1 = @SQL1 + ' where 1=1 ' 
+
+			   if (@Location IS NOT NULL)
+		          BEGIN 
+		                set @SQL1 = @SQL1 +  ' and  tblICItemSpecialPricing.intItemLocationId
+		                IN (select intItemLocationId from tblICItemLocation where intLocationId
+		                IN (select intLocationId from tblICItemLocation where intLocationId
+						IN (' + CAST(@Location as NVARCHAR) + ')' + '))'
+		          END
+
+			   if (@Vendor IS NOT NULL)
+		           BEGIN 
+		                 set @SQL1 = @SQL1 +  ' and  tblICItemSpecialPricing.intItemLocationId
+		                 IN (select intItemLocationId from tblICItemLocation where intVendorId
+		                 IN (select intEntityId from tblEntity where intEntityId 
+						 IN (' + CAST(@Vendor as NVARCHAR) + ')' + '))'
+		           END
+
+				  if (@Category IS NOT NULL)
+		             BEGIN
+     	                  set @SQL1 = @SQL1 +  ' and tblICItemSpecialPricing.intItemId  
+		                  IN (select intItemId from tblICItem where intCategoryId IN
+			              (select intCategoryId from tblICCategory where intCategoryId
+						   IN (' + CAST(@Category as NVARCHAR) + ')' + '))'
+		             END
+
+				  if (@Family IS NOT NULL)
+		             BEGIN
+  			              set @SQL1 = @SQL1 +  ' and tblICItemSpecialPricing.intItemLocationId IN 
+			              (select intItemLocationId from tblICItemLocation where intFamilyId IN
+			              (select intFamilyId from tblICItemLocation where intFamilyId 
+						  IN (' + CAST(@Family as NVARCHAR) + ')' + '))'
+		             END
+
+				  if (@Class IS NOT NULL)
+		              BEGIN
+		                  set @SQL1 = @SQL1 +  ' and tblICItemSpecialPricing.intItemLocationId IN 
+			              (select intItemLocationId from tblICItemLocation where intClassId IN
+			              (select intClassId from tblICItemLocation where intClassId 
+						  IN (' + CAST(@Class as NVARCHAR) + ')' + '))'
+		               END
+
+				   if (@UpcCode IS NOT NULL)
+			           BEGIN
+				           set @SQL1 = @SQL1 +  ' and tblICItemSpecialPricing.intItemId  
+                           IN (select intItemId from tblICItemUOM where  intItemUOMId IN
+		                   (select intItemUOMId from tblICItemUOM  where intItemUOMId 
+						   IN (' + CAST(@UpcCode as NVARCHAR) + ')' + '))'
+			           END
+
+				    if ((@Region IS NOT NULL)
+					and(@Region != ''))
+						BEGIN
+						 set @SQL1 = @SQL1 +  ' and tblICItemSpecialPricing.intItemLocationId IN 
+						 (select intItemLocationId from tblICItemLocation where intLocationId IN 
+						 (select intCompanyLocationId from tblSTStore where strRegion 
+						 IN ( ''' + (@Region) + ''')' + '))'
+						END
+
+				    if ((@District IS NOT NULL)
+					and(@District != ''))
+						BEGIN
+						 set @SQL1 = @SQL1 +  ' and tblICItemSpecialPricing.intItemLocationId IN 
+						 (select intItemLocationId from tblICItemLocation where intLocationId IN 
+						 (select intCompanyLocationId from tblSTStore where strDestrict 
+						 IN ( ''' + (@District) + ''')' + '))'
+						END
+
+                    if ((@State IS NOT NULL)
+					and(@State != ''))
+						BEGIN
+						 set @SQL1 = @SQL1 +  ' and tblICItemSpecialPricing.intItemLocationId IN 
+						 (select intItemLocationId from tblICItemLocation where intLocationId IN 
+						 (select intCompanyLocationId from tblSTStore where strState 
+						 IN ( ''' + (@State) + ''')' + '))'
+						END
+
+                    if ((@Description IS NOT NULL)
+					and (@Description != ''))
+					BEGIN
+					   set @SQL1 = @SQL1 +  ' and tblICItemSpecialPricing.intItemId IN 
+					   (select intItemId from tblICItem where strDescription 
+					    like ''%' + LTRIM(@Description) + '%'' )'
+					END
+
+					set @SQL1 = @SQL1 + ' and  strPromotionType = ''Discount''' 
+					exec (@SQL1)
+	                select  @UpdateCount =   @UpdateCount + (@@ROWCOUNT)   
 		  END
-           
-  	   if (@ItemFamily IS NOT NULL)
-		   BEGIN
-  			  set @SQL1 = @SQL1 +  ' and  stpbk_family IN 
-			 (''' + replace((SELECT (CAST(@ItemFamily AS NVARCHAR(MAX)))),',',''',''') +''')'
-		   END
-    
-		if (@ItemClass  IS NOT NULL)
-		   BEGIN
-		    set @SQL1 = @SQL1 +  ' and  stpbk_class IN 
-		 	 (''' + replace((SELECT (CAST(@ItemClass AS NVARCHAR(MAX)))),',',''',''') +''')'
-		   END
-    
-		 if (@ItemUpcCode IS NOT NULL)
-			 BEGIN
-				set @SQL1 = @SQL1 +  ' and  stpbk_upcno IN 
-				 (''' + replace((SELECT (CAST(@ItemUpcCode AS NVARCHAR(MAX)))),',',''',''') +''')'
-			  END
-
-	    
-		 if ((@ItemDescription IS NOT NULL) and (RTRIM (LTRIM(@ItemDescription)) != ''))
-			   BEGIN
-				 set @SQL1 = @SQL1 +  ' and  stpbk_item_desc like ''%' + LTRIM(@ItemDescription) + '%'''
-			   END
-
-         if ((@ItemLocation IS NULL) OR (@ItemLocation = ''))
-			  BEGIN
-			  if ((@ItemRegion IS NOT NULL)
-			  and(@ItemRegion != ''))
-			      BEGIN
-				       set @SQL1 = @SQL1 + ' and stpbk_store_name IN
-				       (select ststo_store_name from ststomst where ststo_region_id = '''+ LTRIM(@ItemRegion) +''')'  
-				  END
-              END 
-
-          if ((@ItemLocation IS NULL) OR (@ItemLocation = ''))
-			  BEGIN
-			  if ((@ItemDistrict IS NOT NULL)
-			  and(@ItemDistrict != ''))
-			      BEGIN
-				       set @SQL1 = @SQL1 + ' and stpbk_store_name IN
-				       (select ststo_store_name from ststomst where ststo_district_id = '''+ LTRIM(@ItemDistrict) +''')'  
-				  END
-              END 
-
-          if ((@ItemLocation IS NULL) OR (@ItemLocation = ''))
-			  BEGIN
-			  if ((@ItemState IS NOT NULL)
-			  and(@ItemState != ''))
-			      BEGIN
-				       set @SQL1 = @SQL1 + ' and stpbk_store_name IN
-				       (select ststo_store_name from ststomst where ststo_state_id = '''+ LTRIM(@ItemState) +''')'  
-				  END
-              END 
-		    
-	  exec (@SQL1)
-	  select (@@ROWCOUNT)
-      
+          
+		  select @UpdateCount as UpdateItemPrcicingCount		    
 
 END TRY
 
