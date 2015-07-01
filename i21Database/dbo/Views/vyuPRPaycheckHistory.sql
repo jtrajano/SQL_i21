d05ba46c82891736e0050f713727306da3699abd
Melanie Ferrer <Mel.Ferrer@irely.com>
2015-07-01 09:41:18 +0800

    PR - 185 - Posted Employee paychecks are not displaying under Paycheck > History Tab

---
+++ i21Database/dbo/Views/vyuPRPaycheckHistory.sql
@@ -0,0 +1,15 @@
+﻿CREATE VIEW [dbo].[vyuPRPaycheckHistory]
+AS
+SELECT      PC.intPaycheckId, 
+			PC.strPaycheckId, 
+			PC.intEmployeeId, 
+			PC.dtmPayDate, 
+			PC.intBankAccountId,  
+			CM.strBankAccountNo, 
+			PC.dblGross,
+			PC.dblNetPayTotal, 
+			PC.dblTaxTotal,
+			PC.dblDeductionTotal,
+			PC.ysnPosted
+FROM        dbo.tblPRPaycheck PC
+INNER JOIN	dbo.tblCMBankAccount CM  ON PC.intBankAccountId = CM.intBankAccountId
