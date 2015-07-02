039f8e97f0567aa9f56e5bfe250087dc08f738a4
Pruthvi <pruthvi.c@irely.com>
2015-07-02 13:35:15 +0530

    TR-88

--- i21Database/dbo/Functions/fnGetStartingNumber.sql
+++ i21Database/dbo/Functions/fnGetStartingNumber.sql
@@ -7,12 +7,5 @@ BEGIN
     DECLARE @StartingNumber AS NVARCHAR(20)
     EXEC dbo.uspSMGetStartingNumber @startingNumberId, @StartingNumber OUTPUT
 
-    IF @StartingNumber IS NULL 
-    BEGIN 
-	   -- Raise the error:
-	   -- Unable to generate the transaction id. Please ask your local administrator to check the starting numbers setup.
-	   RAISERROR(50030, 11, 1);
-	   RETURN @StartingNumber;
-    END 
 	RETURN @StartingNumber
 END
