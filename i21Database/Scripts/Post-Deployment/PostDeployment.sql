0474f12fe2233cb4282d034de87fa10de4610fc9
Mon Gonzales <mon.gonzales@irely.com>
2015-07-06 10:57:34 +0800

    EM-293

--- i21Database/Scripts/Post-Deployment/PostDeployment.sql
+++ i21Database/Scripts/Post-Deployment/PostDeployment.sql
@@ -180,6 +180,7 @@ print 'BEGIN POST DEPLOYMENT'
 :r .\EM\007_MoveShippingLineData.sql
 :r .\EM\008_MoveTruckerData.sql
 :r .\EM\009_UpdateEntityContactTypeData.sql
+:r .\EM\010_UpdateVendorAccountNumber.sql
 -- Quality Module
 :r .\QM\1_MasterTables.sql
 
