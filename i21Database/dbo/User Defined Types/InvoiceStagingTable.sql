174d87ca0d3c151a94900d1dc79f487662d749c4
Pruthvi <pruthvi.c@irely.com>
2015-07-06 20:44:48 +0530

    TR-88

---
+++ i21Database/dbo/User Defined Types/InvoiceStagingTable.sql
@@ -0,0 +1,23 @@
+﻿/*
+	This is a user-defined table type used in the Invoice Creation stored procedures. 
+*/
+CREATE TYPE [dbo].[InvoiceStagingTable] AS TABLE
+(
+	 [intId] INT IDENTITY PRIMARY KEY CLUSTERED	
+	,[intEntityCustomerId] INT NOT NULL						-- The Customer. 	
+	,[intLocationId] INT NOT NULL                           -- Company Location
+	,[intItemId] INT NOT NULL								-- The item. 
+	,[intItemUOMId] INT NOT NULL							-- The UOM used for the item.
+	,[dtmDate] DATETIME NOT NULL							-- The date of the transaction
+	,[intContractDetailId] INT NULL                         -- Contract
+	,[intShipViaId] INT NULL                                -- ShipVia
+	,[intSalesPersonId] INT NULL                            -- SalesPerson
+    ,[dblQty] NUMERIC(18, 6) NOT NULL DEFAULT 0				-- The quantity of an item in relation to its UOM. For example a box can have 12 pieces of an item. If you have 10 boxes, this parameter must be 10 and not 120 (10 boxes x 12 pieces per box). Positive unit qty means additional stock. Negative unit qty means reduction (selling) of the stock. 
+    ,[dblPrice] NUMERIC(18, 6) NOT NULL DEFAULT 0			-- The Price of purchasing a item per UOM. For example, $12 is the cost for a 12-piece box. This parameter should hold a $12 value and not $1 per pieces found in a 12-piece box. The cost is stored in base currency. 
+	,[intCurrencyId] INT NULL								-- The currency id used in a tranaction. 
+	,[dblExchangeRate] DECIMAL (38, 20) DEFAULT 1 NOT NULL	-- The exchange rate used in the transaction. It is used to convert the cost or sales price (both in base currency) to the foreign currency value.
+    ,[dblFreightRate] DECIMAL(18, 6) NULL DEFAULT 0         -- Freight Rate 
+	,[strComments] NVARCHAR (250)  COLLATE Latin1_General_CI_AS NULL --comments
+	,[strSourceId] NVARCHAR (250)  COLLATE Latin1_General_CI_AS NOT NULL -- Source Id of the Originated Transaction
+    ,[strPurchaseOrder] NVARCHAR (25)  COLLATE Latin1_General_CI_AS  NULL -- Purchase Order Number
+)
