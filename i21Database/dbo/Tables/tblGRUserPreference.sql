730e5c76023745571284920ae16a08b6537b9d81
Gopinath Nayak <gopinath.n@irely.com>
2015-07-06 12:26:26 +0530

    GRN-122 and GRN-150 schema changes.

--- i21Database/dbo/Tables/tblGRUserPreference.sql
+++ i21Database/dbo/Tables/tblGRUserPreference.sql
@@ -2,10 +2,10 @@
 (
 	[intUserPreferenceId] INT NOT NULL PRIMARY KEY IDENTITY, 
     [intUserSecurityId] INT NOT NULL, 
-    [strQuoteProvider] NVARCHAR(100) NULL DEFAULT (''), 
-    [strProviderUserId] NVARCHAR(100) NULL DEFAULT (''), 
-    [strProviderPassword] NVARCHAR(100) NULL DEFAULT (''), 
-    [strProviderAccessType] NVARCHAR(100) NULL DEFAULT (''), 
+    [strQuoteProvider] NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL DEFAULT (''), 
+    [strProviderUserId] NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL DEFAULT (''), 
+    [strProviderPassword] NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL DEFAULT (''), 
+    [strProviderAccessType] NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL DEFAULT (''), 
     [intConcurrencyId] INT NOT NULL DEFAULT 1--, 
     --CONSTRAINT [FK_tblGRUserPreference_tblSMUserSecurity] FOREIGN KEY (intUserSecurityId) REFERENCES tblSMUserSecurity(intUserSecurityID) ON DELETE CASCADE
 )
