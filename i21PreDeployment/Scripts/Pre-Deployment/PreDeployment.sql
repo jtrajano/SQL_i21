75fdd6728a4aaa94962dc941f22921145e469fa0
Mon Gonzales <mon.gonzales@irely.com>
2015-07-06 18:11:58 +0800

    EM-294

--- i21PreDeployment/Scripts/Pre-Deployment/PreDeployment.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/PreDeployment.sql
@@ -86,7 +86,6 @@
 :r .\12_EM\03_EntityCustomerFarmRename_DataFix.sql
 :r .\12_EM\04_EntityLocationTaxCodeUpdate.sql
 :r .\12_EM\05_EntitySplitSchemaUpdate.sql
-:r .\12_EM\06_DeleteOldTables.sql
 :r .\12_EM\07_EntityFarmSchemaUpdate.sql
 --RK
 :r .\13_RK\01_DropTableScript.sql
