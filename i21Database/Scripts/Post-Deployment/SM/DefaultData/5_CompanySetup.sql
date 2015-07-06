6657528a96ace99a4b3023cbbc5008d39d3c94b6
Dan Jalimao <Dan.Jalimao@irely.com>
2015-07-06 17:56:34 +0800

    FCOM-571

--- i21Database/Scripts/Post-Deployment/SM/DefaultData/5_CompanySetup.sql
+++ i21Database/Scripts/Post-Deployment/SM/DefaultData/5_CompanySetup.sql
@@ -4,8 +4,8 @@ GO
 
 	IF EXISTS (SELECT TOP 1 1 FROM tblSMCompanySetup)
 	BEGIN
-		IF EXISTS(SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[tblSMPreferences]'))
-			IF EXISTS(SELECT TOP 1 1 FROM tblSMPreferences WHERE strPreference = 'isLegacyIntegration' AND strValue = 'false')
+		IF EXISTS(SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[tblSMCompanyPreference]'))
+			IF EXISTS(SELECT TOP 1 1 FROM tblSMCompanyPreference WHERE ysnLegacyIntegration = 0)
 				GOTO Check_Exit;
 
 		PRINT N'BEGIN UPDATING EXISTING COMPANY SETUP'
@@ -17,19 +17,25 @@ GO
 	BEGIN
 		PRINT N'BEGIN INSERTING COMPANY SETUP FROM ORIGIN'
 			INSERT INTO tblSMCompanySetup (strCompanyName)
-				SELECT TOP 1 coctl_co_name FROM [coctlmst]	
+			SELECT TOP 1 coctl_co_name FROM [coctlmst]	
 		PRINT N'END INSERTING COMPANY SETUP FROM ORIGIN'
 	END
 	
 	Check_Exit:
 
-PRINT N'BEGIN UPDATING SCREEN AND CONTROL LISTING FLAG'
-	UPDATE tblSMCompanySetup 
-	SET ysnScreenControlListingUpdated = 0
-PRINT N'END UPDATING SCREEN AND CONTROL LISTING FLAG'
+	IF NOT EXISTS (SELECT TOP 1 1 FROM tblSMCompanySetup)
+	BEGIN
+		PRINT N'INSERTING A DEFAULT EMPTY ROW'
+		INSERT INTO tblSMCompanySetup(strCompanyName, ysnScreenControlListingUpdated) VALUES('', 0)
+	END
 
+	PRINT N'BEGIN UPDATING SCREEN AND CONTROL LISTING FLAG'
+		UPDATE tblSMCompanySetup 
+		SET ysnScreenControlListingUpdated = 0
+	PRINT N'END UPDATING SCREEN AND CONTROL LISTING FLAG'
 
 GO
-PRINT N'END COMPANY SETUP '
+
+	PRINT N'END COMPANY SETUP '
 
 GO
