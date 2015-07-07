91c37a29702aff599e0ae54c6b903f26ec2b789c
Arman Arenas <arman.arenas@irely.com>
2015-07-08 05:00:11 +0800

    PR-234 Revised tblPRCompanyPreference table

---
+++ i21Database/dbo/Tables/tblPRCompanyPreference.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[tblPRCompanyPreference](
+	[intCompanyPreferenceId] [int] IDENTITY(1,1) NOT NULL,
+	[intBankAccountId] [int] NULL,
+	[intLiabilityAccount] [int] NULL,
+	[intExpenseAccount] [int] NULL,
+	[intTypeEarningId] [int] NULL,
+	[intAccountId] [int] NULL,
+	[intTimeOff] [int] NULL,
+	[intConcurrencyId] [int] NULL,
+ CONSTRAINT [PK_tblPRCompanyPreference] PRIMARY KEY CLUSTERED 
+(
+	[intCompanyPreferenceId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[tblPRCompanyPreference] ADD  CONSTRAINT [DF_tblPRCompanyPreference_intConcurrencyId]  DEFAULT ((1)) FOR [intConcurrencyId]
+GO
