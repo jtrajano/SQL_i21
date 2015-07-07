532c6e1e02add9669e8e916db1ec0ea632668d7e
Satya <ssahoo@BLRSATYAWIN7.irely.net>
2015-07-07 23:11:57 +0530

    CT-166

--- i21Database/dbo/Views/vyuCTPriceFixation.sql
+++ i21Database/dbo/Views/vyuCTPriceFixation.sql
@@ -2,39 +2,27 @@
 
 AS
 
-	SELECT	D.intContractDetailId, 
-			D.dblQuantity,
-			D.intUnitMeasureId,
-			D.dtmStartDate,
-			D.dtmEndDate,
-			D.dblFutures,
-			D.dblBasis,
-			D.dblCashPrice,
-			D.intFutureMarketId,
-			D.intFutureMonthId,
-			D.intContractOptHeaderId,
-			D.intContractSeq,
-			D.intConcurrencyId,
-			T.strContractType,
-			C.strCommodityCode,
-			E.strName strEntityName,
-			H.intContractNumber,
-			L.strLocationName,
-			CASE D.intPricingTypeId 
-				WHEN 1 THEN 'Priced'
-				WHEN 2 THEN 'Basis'
-				WHEN 3 THEN 'HTA'
-				WHEN 4 THEN 'TBD'
-				ELSE 'CanadianBasis'
-			END	AS strPricingType,
-			M.strFutureMonth,
-			D.dblBalance
-
-	FROM	tblCTContractDetail		D
-	JOIN	tblSMCompanyLocation	L	ON	L.intCompanyLocationId	=	D.intCompanyLocationId
-	JOIN	tblCTContractHeader		H	ON	H.intContractHeaderId	=	D.intContractHeaderId
-	JOIN	tblEntity				E	ON	E.intEntityId			=	H.intEntityId
-	JOIN	tblICCommodity			C	ON	C.intCommodityId		=	H.intCommodityId
-	JOIN	tblCTContractType		T	ON	T.intContractTypeId		=	H.intContractTypeId		LEFT
-	JOIN	tblRKFuturesMonth		M	ON	M.intFutureMonthId		=	D.intFutureMonthId
-	WHERE	D.intPricingTypeId	<>	1
+		SELECT	P.intPriceFixationId,
+				D.intContractDetailId, 
+				D.dblDetailQuantity,
+				D.intItemUOMId,
+				D.dtmStartDate,
+				D.dtmEndDate,
+				D.dblFutures,
+				D.dblBasis,
+				D.dblCashPrice,
+				D.intFutureMarketId,
+				D.intFutureMonthId,
+				D.intContractOptHeaderId,
+				D.intContractSeq,
+				D.strContractType,
+				D.strCommodityCode,
+				D.strEntityName,
+				D.intContractNumber,
+				D.strLocationName,
+				D.strPricingType,
+				D.strFutureMonth,
+				D.dblBalance
+				
+	FROM		tblCTPriceFixation P
+	JOIN		vyuCTContractDetailView D ON D.intContractDetailId = P.intContractDetailId			
