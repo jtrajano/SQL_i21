7b0baf0b973c36d572e8ddadac656b5c2e4c3b84
Mon Gonzales <mon.gonzales@irely.com>
2015-07-08 17:13:48 +0800

    EM-286

---
+++ i21Database/dbo/Views/vyuEMSearchMerge.sql
@@ -0,0 +1,22 @@
+﻿CREATE VIEW [dbo].[vyuEMSearchMerge]
+AS 	
+select 
+	A.intEntityId,
+	B.intEntityContactId,
+	strEntityNo = A.strEntityNo,
+	strEntityName = A.strName,
+	strEmail = B.strEmail,
+	strPhone =	B.strPhone,
+	strContactName = B.strName,
+	strAddress = C.strAddress,
+	strZipCode = C.strZipCode,
+	strEntityType = 
+					STUFF((SELECT '; ' + t.strType
+						FROM tblEntityType t
+							WHERE t.intEntityId = A.intEntityId
+								FOR XML PATH('')), 1, 1, '') 
+	from tblEntity A
+		JOIN vyuEMEntityContact B
+			ON A.intEntityId = B.intEntityId
+		JOIN tblEntityLocation C
+			ON A.intEntityId = C.intEntityId
