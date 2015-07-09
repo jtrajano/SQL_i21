74852384ac98b648530c6837d61c2d7dd051067e
Edgardo Amurao <ed.amurao@irely.com>
2015-07-09 11:36:57 +0800

    FCOM-537: update sql script to retrieve not only due recurring accounts but also upcoming due account with updated warning days

--- i21Database/Scripts/Post-Deployment/SM/DefaultData/12_ReminderList.sql
+++ i21Database/Scripts/Post-Deployment/SM/DefaultData/12_ReminderList.sql
@@ -5,12 +5,17 @@ GO
     SELECT [strReminder]        =        N'Process',
 		   [strType]        	=        N'Invoice',
            [strDescription]     =        N'{0} {1} {2} {3}',
-           [strQuery]  			=        N'SELECT * FROM tblSMRecurringTransaction WHERE strTransactionType = ''Invoice'' ' + 
-										  'AND GETDATE() >= dtmNextProcess ' + 
-										  'AND dtmNextProcess >= dtmStartDate ' + 
-										  'AND dtmNextProcess <= dtmEndDate ' + 
-										  'AND ysnActive = 1 ' + 
-										  'AND strResponsibleUser = ''{0}''',
+           [strQuery]  			=        N'SELECT intRecurringId ' +
+										  'FROM tblSMRecurringTransaction WHERE strTransactionType = ''Invoice'' ' +
+										  'AND GETDATE() >= dtmNextProcess AND dtmNextProcess >= dtmStartDate AND dtmNextProcess <= dtmEndDate AND ysnActive = 1 ' +
+										  'AND strResponsibleUser = ''{0}'' ' +
+										  'UNION ' +
+										  'SELECT intRecurringId ' +
+										  'FROM tblSMRecurringTransaction WHERE strTransactionType = ''Invoice'' ' +
+										  'AND ysnActive = 1 ' +
+										  'AND strResponsibleUser = ''{0}'' ' +
+										  'AND DATEDIFF(DAY, GETDATE(), dtmNextProcess) > 0 ' +
+										  'AND DATEDIFF(DAY, GETDATE(), DATEADD(DAY, intWarningDays * -1 , dtmNextProcess)) <= 0 ',
            [strNamespace]       =        N'i21.view.RecurringTransaction', 
 		   [strParameter]		=		 N'strResponsibleUser',
            [intSort]            =        1
@@ -18,12 +23,17 @@ GO
 	SELECT [strReminder]        =        N'Process',
 		   [strType]        	=        N'General Journal',
            [strDescription]     =        N'{0} {1} {2} {3}',
-           [strQuery]  			=        N'SELECT * FROM tblSMRecurringTransaction WHERE strTransactionType = ''General Journal'' ' + 
-										  'AND GETDATE() >= dtmNextProcess ' + 
-										  'AND dtmNextProcess >= dtmStartDate ' + 
-										  'AND dtmNextProcess <= dtmEndDate ' +
-										  'AND ysnActive = 1 ' + 
-										  'AND strResponsibleUser = ''{0}''',
+           [strQuery]  			=        N'SELECT intRecurringId ' +
+                                          'FROM tblSMRecurringTransaction WHERE strTransactionType = ''General Journal'' ' +
+                                          'AND GETDATE() >= dtmNextProcess AND dtmNextProcess >= dtmStartDate AND dtmNextProcess <= dtmEndDate AND ysnActive = 1 ' +
+                                          'AND strResponsibleUser = ''{0}'' ' +
+                                          'UNION ' +
+                                          'SELECT intRecurringId ' +
+                                          'FROM tblSMRecurringTransaction WHERE strTransactionType = ''General Journal'' ' +
+                                          'AND ysnActive = 1 ' +
+                                          'AND strResponsibleUser = ''{0}'' ' +
+                                          'AND DATEDIFF(DAY, GETDATE(), dtmNextProcess) > 0 ' +
+                                          'AND DATEDIFF(DAY, GETDATE(), DATEADD(DAY, intWarningDays * -1 , dtmNextProcess)) <= 0 ',
            [strNamespace]       =        N'i21.view.RecurringTransaction', 
 		   [strParameter]		=		 N'strResponsibleUser',
            [intSort]            =        2
@@ -31,12 +41,17 @@ GO
 	SELECT [strReminder]        =        N'Process',
 		   [strType]        	=        N'Bill',
            [strDescription]     =        N'{0} {1} {2} {3}',
-           [strQuery]  			=        N'SELECT * FROM tblSMRecurringTransaction WHERE strTransactionType = ''Bill'' ' + 
-										  'AND GETDATE() >= dtmNextProcess ' + 
-										  'AND dtmNextProcess >= dtmStartDate ' + 
-										  'AND dtmNextProcess <= dtmEndDate ' +
-										  'AND ysnActive = 1 ' + 
-										  'AND strResponsibleUser = ''{0}''',
+           [strQuery]  			=        N'SELECT intRecurringId ' +
+										  'FROM tblSMRecurringTransaction WHERE strTransactionType = ''Bill'' ' +
+										  'AND GETDATE() >= dtmNextProcess AND dtmNextProcess >= dtmStartDate AND dtmNextProcess <= dtmEndDate AND ysnActive = 1 ' +
+										  'AND strResponsibleUser = ''{0}'' ' +
+										  'UNION ' +
+										  'SELECT intRecurringId ' +
+										  'FROM tblSMRecurringTransaction WHERE strTransactionType = ''Bill'' ' +
+										  'AND ysnActive = 1 ' +
+										  'AND strResponsibleUser = ''{0}'' ' +
+										  'AND DATEDIFF(DAY, GETDATE(), dtmNextProcess) > 0 ' +
+										  'AND DATEDIFF(DAY, GETDATE(), DATEADD(DAY, intWarningDays * -1 , dtmNextProcess)) <= 0 ',
            [strNamespace]       =        N'i21.view.RecurringTransaction', 
 		   [strParameter]		=		 N'strResponsibleUser',
            [intSort]            =        3
@@ -44,12 +59,17 @@ GO
 	SELECT [strReminder]        =        N'Process',
 		   [strType]        	=        N'Purchase Order',
            [strDescription]     =        N'{0} {1} {2} {3}',
-           [strQuery]  			=        N'SELECT * FROM tblSMRecurringTransaction WHERE strTransactionType = ''Purchase Order'' ' + 
-										  'AND GETDATE() >= dtmNextProcess ' + 
-										  'AND dtmNextProcess >= dtmStartDate ' + 
-										  'AND dtmNextProcess <= dtmEndDate ' +
-										  'AND ysnActive = 1 ' + 
-										  'AND strResponsibleUser = ''{0}''',
+           [strQuery]  			=        N'SELECT intRecurringId ' +
+										  'FROM tblSMRecurringTransaction WHERE strTransactionType = ''Purchase Order'' ' +
+										  'AND GETDATE() >= dtmNextProcess AND dtmNextProcess >= dtmStartDate AND dtmNextProcess <= dtmEndDate AND ysnActive = 1 ' +
+										  'AND strResponsibleUser = ''{0}'' ' +
+										  'UNION ' +
+										  'SELECT intRecurringId ' +
+										  'FROM tblSMRecurringTransaction WHERE strTransactionType = ''Purchase Order'' ' +
+										  'AND ysnActive = 1 ' +
+										  'AND strResponsibleUser = ''{0}'' ' +
+										  'AND DATEDIFF(DAY, GETDATE(), dtmNextProcess) > 0 ' +
+										  'AND DATEDIFF(DAY, GETDATE(), DATEADD(DAY, intWarningDays * -1 , dtmNextProcess)) <= 0 ',
            [strNamespace]       =        N'i21.view.RecurringTransaction', 
 		   [strParameter]		=		 N'strResponsibleUser',
            [intSort]            =        4
