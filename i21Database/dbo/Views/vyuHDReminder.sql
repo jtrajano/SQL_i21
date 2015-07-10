67b200539f001f1ee2b51948c8fc5701fca32ee9
Jayson Ramos <jayson.ramos@iRely.com>
2015-07-10 09:39:05 +0800

    HD-757;

---
+++ i21Database/dbo/Views/vyuHDReminder.sql
@@ -0,0 +1,20 @@
+﻿CREATE VIEW [dbo].[vyuHDReminder]
+	AS
+	select
+		t.strTicketNumber
+		,strTicketSubject = t.strSubject
+		,e.strName
+		,r.dtmDate
+		,r.intRemindAdvance
+		,dtmDateTo = DATEADD(MINUTE, r.intRemindAdvance, r.dtmDate)
+		,r.strSubject
+		,r.intReminderId
+		,r.intEntityId
+	from
+		tblHDReminder r
+		,tblHDTicket t
+		,tblEntity e
+	where
+		t.intTicketId = r.intTicketId
+		and e.intEntityId = t.intCustomerId
+		and r.ysnActive = 1
