f708cbfed89f6a98164260cc1432eed588ca6eba
Kelvin Quinto <kelvin.quinto@irely.com>
2015-07-13 10:54:38 +0800

    AR-89

--- i21Database/dbo/Tables/tblARCompanyPreference.sql
+++ i21Database/dbo/Tables/tblARCompanyPreference.sql
@@ -1,7 +1,10 @@
 ﻿CREATE TABLE [dbo].[tblARCompanyPreference]
 (
-	[intCompanyPreferenceId] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [intARAccountId] INT NULL, 
-    [intDiscountAccountId] INT NULL,
-	[intConcurrencyId] INT NOT NULL DEFAULT 1
+	[intCompanyPreferenceId]		INT NOT NULL PRIMARY KEY IDENTITY, 
+    [intARAccountId]				INT NULL, 
+    [intDiscountAccountId]			INT NULL,
+	[strServiceChargeAccount]		NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL,
+	[strServiceChargeCalculation]	NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL,
+	[strServiceChargeFrequency]		NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL,
+	[intConcurrencyId]				INT NOT NULL DEFAULT 1
 )
