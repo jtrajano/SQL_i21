65e30f75e48b9c86a493c489386654304257abc0
Maheshwari <maheshwari.gawade@irely.com>
2015-07-13 18:56:53 +0530

    ST - 153 - Register File Configuration

---
+++ i21Database/dbo/Tables/tblSTRegisterFileConfiguration.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[tblSTRegisterFileConfiguration]
+(
+	[intRegisterFileConfigId] INT NOT NULL PRIMARY KEY, 
+    [intRegisterId] INT NULL, 
+    [intLayoutId] INT NULL, 
+    [strFileType] NVARCHAR(50) NULL, 
+    [strFilePrefix] NVARCHAR(50) NULL, 
+    [strFolderPath] NVARCHAR(MAX) NULL, 
+    [strURICommand] NVARCHAR(MAX) NULL, 
+    [intCurrencyId] INT NULL, 
+    CONSTRAINT [PK_tblSTRegisterFileConfiguration_intRegisterFileConfigId] PRIMARY KEY CLUSTERED ([intRegisterFileConfigId] ASC)
+
+)
