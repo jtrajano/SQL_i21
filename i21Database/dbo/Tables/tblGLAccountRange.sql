f8024f4425db2521229b1b27dd9d05ab45cee037
jeffrey trajano <jeffrey.trajano@irely.com>
2015-07-14 19:01:18 +0800

    GL-1916 Error loading Chart of accounts and Account Group

--- i21Database/dbo/Tables/tblGLAccountRange.sql
+++ i21Database/dbo/Tables/tblGLAccountRange.sql
@@ -3,7 +3,8 @@
 	[strAccountType]  NVARCHAR(20) COLLATE Latin1_General_CI_AS NULL,
 	[intMinRange] [int] NULL,
 	[intMaxRange] [int] NULL,
- CONSTRAINT [PK_tblGLAccountRange] PRIMARY KEY CLUSTERED 
+ [intConcurrencyId] INT NOT NULL DEFAULT 1, 
+    CONSTRAINT [PK_tblGLAccountRange] PRIMARY KEY CLUSTERED 
 (
 	[intAccountRangeId] ASC
 )WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
