163b346b9f926c909bf0f6cd3585bb78366bead4
Maheshwari <maheshwari.gawade@irely.com>
2015-07-14 11:11:55 +0530

    ST - 153 - Foreign Key Ref for Register Id and Layout Id

--- i21Database/dbo/Tables/tblSTRegisterFileConfiguration.sql
+++ i21Database/dbo/Tables/tblSTRegisterFileConfiguration.sql
@@ -8,6 +8,8 @@
     [strFolderPath] NVARCHAR(MAX) NULL, 
     [strURICommand] NVARCHAR(MAX) NULL, 
     [intConcurrencyId] INT NULL, 
-    CONSTRAINT [PK_tblSTRegisterFileConfiguration_intRegisterFileConfigId] PRIMARY KEY CLUSTERED ([intRegisterFileConfigId] ASC)
+    CONSTRAINT [PK_tblSTRegisterFileConfiguration_intRegisterFileConfigId] PRIMARY KEY CLUSTERED ([intRegisterFileConfigId] ASC), 
+    CONSTRAINT [FK_tblSTRegisterFileConfiguration_tblSTRegister_intRegisterId] FOREIGN KEY ([intRegisterId]) REFERENCES [tblSTRegister]([intRegisterId]), 
+    CONSTRAINT [FK_tblSTRegisterFileConfiguration_tblSMGridLayout_intGridLayoutId] FOREIGN KEY ([intLayoutId]) REFERENCES [tblSMGridLayout]([intGridLayoutId])
 
 )
