951f4786397f12712cb468d8bc40008ded932849
Maheshwari <maheshwari.gawade@irely.com>
2015-07-14 11:35:48 +0530

    ST - 153 - Column Rename

--- i21Database/dbo/Tables/tblSTRegisterFileConfiguration.sql
+++ i21Database/dbo/Tables/tblSTRegisterFileConfiguration.sql
@@ -2,7 +2,7 @@
 (
 	[intRegisterFileConfigId] INT NOT NULL, 
     [intRegisterId] INT NULL, 
-    [intLayoutId] INT NULL, 
+    [intImportFileHeaderId] INT NULL, 
     [strFileType] NVARCHAR(50) NULL, 
     [strFilePrefix] NVARCHAR(50) NULL, 
     [strFolderPath] NVARCHAR(MAX) NULL, 
@@ -10,6 +10,6 @@
     [intConcurrencyId] INT NULL, 
     CONSTRAINT [PK_tblSTRegisterFileConfiguration_intRegisterFileConfigId] PRIMARY KEY CLUSTERED ([intRegisterFileConfigId] ASC), 
     CONSTRAINT [FK_tblSTRegisterFileConfiguration_tblSTRegister_intRegisterId] FOREIGN KEY ([intRegisterId]) REFERENCES [tblSTRegister]([intRegisterId]), 
-    CONSTRAINT [FK_tblSTRegisterFileConfiguration_tblSMImportFileHeader_intGridLayoutId] FOREIGN KEY ([intLayoutId]) REFERENCES [tblSMImportFileHeader]([intImportFileHeaderId])
+    CONSTRAINT [FK_tblSTRegisterFileConfiguration_tblSMImportFileHeader_intImportFileHeaderId] FOREIGN KEY ([intImportFileHeaderId]) REFERENCES [tblSMImportFileHeader]([intImportFileHeaderId])
 
 )
