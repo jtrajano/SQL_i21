e07c82a2a687288496e80bac7128e581218aaa92
Erick F. Dela Cruz <erick.delacruz@irely.com>
2015-07-14 11:27:00 +0800

    AP-1371

--- i21Database/dbo/Stored Procedures/uspICGetItemsForItemReceipt.sql
+++ i21Database/dbo/Stored Procedures/uspICGetItemsForItemReceipt.sql
@@ -49,6 +49,7 @@ BEGIN
 				AND PO.intShipToId = ItemLocation.intLocationId
 	WHERE	PODetail.intPurchaseId = @intSourceTransactionId
 			AND dbo.fnIsStockTrackingItem(PODetail.intItemId) = 1
+			AND PODetail.dblQtyOrdered != PODetail.dblQtyReceived
 			
 END
 
