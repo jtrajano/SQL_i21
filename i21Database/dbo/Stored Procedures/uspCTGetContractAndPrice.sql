5d3b88c4bf040e2ec355740494e3a87ca790ccd1
Satya <ssahoo@BLRSATYAWIN7.irely.net>
2015-07-15 20:46:54 +0530

    CT-215

---
+++ i21Database/dbo/Stored Procedures/uspCTGetContractAndPrice.sql
@@ -0,0 +1,25 @@
+﻿CREATE PROCEDURE [dbo].[uspCTGetContractAndPrice]
+	@intLocationId		INT,
+	@intItemId			INT,
+	@dblTransactionDate	DATETIME,
+	@dblQuantity		NUMERIC(18,4)
+AS
+BEGIN
+
+SET QUOTED_IDENTIFIER OFF
+SET ANSI_NULLS ON
+SET NOCOUNT ON
+SET XACT_ABORT ON
+SET ANSI_WARNINGS OFF
+
+SELECT	TOP 1 
+		intContractHeaderId,
+		dblCashPrice
+FROM	tblCTContractDetail
+WHERE	intCompanyLocationId	=		@intLocationId	AND	
+		intItemId				=		@intItemId		AND	
+		dblQuantity				=		@dblQuantity	AND
+		ISNULL(dblCashPrice,0)	>		0				AND
+		@dblTransactionDate	 BETWEEN	dtmStartDate	AND 
+										dtmEndDate		
+END 
