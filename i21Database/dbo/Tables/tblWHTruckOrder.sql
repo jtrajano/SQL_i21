485fe94072fa80e5018c6d272817ce0049948aef
unknown <swghosh@BLRSWETOWIN7.irely.net>
2015-07-15 15:45:48 +0530

    WH-33

---
+++ i21Database/dbo/Tables/tblWHTruckOrder.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[tblWHTruckOrder]
+(
+		[intTruckId] INT NOT NULL,
+		[intOrderHeaderId] INT NOT NULL,
+		[intSequenceNo] INT NULL DEFAULT 0,
+		[intLastModifiedUserId] INT NULL,
+		[dtmLastModified] DATETIME NULL DEFAULT GetDate(),
+)
