dd25d6a0d49bd1444e4451ea705a4b584850b535
Feb Montefrio <feb.montefrio@irely.com>
2015-07-16 16:32:59 +0800

    IC-665

--- i21Database/dbo/Tables/tblICInventoryReceiptItem.sql
+++ i21Database/dbo/Tables/tblICInventoryReceiptItem.sql
@@ -31,6 +31,7 @@ Type the overview for the table here.
 		[dblUnitRetail] NUMERIC(18,6) NULL DEFAULT ((0)),
 		[dblLineTotal] NUMERIC(18, 6) NULL DEFAULT ((0)), 
 		[intGradeId] INT NULL,
+		[dblOtherCharges] NUMERIC(38, 20) NULL, 
 		[intSort] INT NULL, 
 		[intConcurrencyId] INT NULL DEFAULT ((0)), 
 		CONSTRAINT [PK_tblICInventoryReceiptItem] PRIMARY KEY ([intInventoryReceiptItemId]), 
