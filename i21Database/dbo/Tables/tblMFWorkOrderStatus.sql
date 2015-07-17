2df2c77f1ce961570752d661311a115a32e7aae5
Premkumar S <prem.selladurai@irely.com>
2015-07-17 21:15:42 +0530

    MFG-80

--- i21Database/dbo/Tables/tblMFWorkOrderStatus.sql
+++ i21Database/dbo/Tables/tblMFWorkOrderStatus.sql
@@ -2,6 +2,7 @@
 (
 	[intStatusId] INT NOT NULL , 
     [strName] NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL,
+	[strColorName] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL,
 	CONSTRAINT [PK_tblMFWorkOrderStatus_intStatusId] PRIMARY KEY ([intStatusId]), 
     CONSTRAINT [UQ_tblMFWorkOrderStatus_strName] UNIQUE ([strName]) 
 )
