eb64c446bbc1bc3b74a08ca484998b0edcd65220
Ramachandran <rshanmuganathan@BLRRAMW8PRON.irely.net>
2015-07-17 13:13:41 +0530

    RM-75

--- i21Database/dbo/Stored Procedures/uspRKDPRInvDailyPositionDetail.sql
+++ i21Database/dbo/Stored Procedures/uspRKDPRInvDailyPositionDetail.sql
@@ -1,5 +1,5 @@
 ﻿CREATE Proc [dbo].[uspRKDPRInvDailyPositionDetail]	 
-		 @intCommodityId int,
+	 @intCommodityId int,
 		 @intLocationId int= null
 	
 	AS
@@ -78,95 +78,18 @@ SELECT
 		WHERE i.intCommodityId= @intCommodityId and il.intLocationId=@intLocationId) as ReserveQty     
  ) t    
 
-UNION ALL           
-SELECT @intCommodityId as intCommodityId,'Open Storage' as [strType],         
-	    ISNULL(invQty,0)-ISNULL(ReserveQty,0) AS dblTotal
-FROM(            
-SELECT 
-		(SELECT sum(isnull(it1.dblUnitOnHand,0)) 		   
-		FROM tblICItem i 		
-		JOIN tblICItemStock it1 ON it1.intItemId = i.intItemId   
-		JOIN tblICItemLocation il on il.intItemLocationId=it1.intItemLocationId     
-		WHERE i.intCommodityId= @intCommodityId and il.intLocationId=@intLocationId) as invQty
-		,(SELECT SUM(isnull(sr1.dblQty,0))  	   
-		FROM tblICItem i 
-		JOIN tblICItemStock it1 ON it1.intItemId = i.intItemId   
-		JOIN tblICStockReservation sr1 ON it1.intItemId = sr1.intItemId   
-		JOIN tblICItemLocation il on il.intItemLocationId=it1.intItemLocationId    
-		WHERE i.intCommodityId= @intCommodityId and il.intLocationId=@intLocationId) as ReserveQty     
- ) t      
+UNION ALL  
+			SELECT @intCommodityId as intCommodityId,* FROM(         
+			SELECT  [Storage Type] strType,SUM(Balance) dblTotal FROM vyuGRGetStorageDetail
+			WHERE intCommodityId= @intCommodityId and intCompanyLocationId=@intLocationId
+			GROUP BY [Storage Type])t   
 
 UNION ALL           
-SELECT @intCommodityId as intCommodityId,'Grain Bank' as [strType],         
-	    ISNULL(invQty,0)-ISNULL(ReserveQty,0) AS dblTotal
-FROM(            
-SELECT 
-		(SELECT sum(isnull(it1.dblUnitOnHand,0)) 		   
-		FROM tblICItem i 		
-		JOIN tblICItemStock it1 ON it1.intItemId = i.intItemId   
-		JOIN tblICItemLocation il on il.intItemLocationId=it1.intItemLocationId     
-		WHERE i.intCommodityId= @intCommodityId and il.intLocationId=@intLocationId) as invQty
-		,(SELECT SUM(isnull(sr1.dblQty,0))  	   
-		FROM tblICItem i 
-		JOIN tblICItemStock it1 ON it1.intItemId = i.intItemId   
-		JOIN tblICStockReservation sr1 ON it1.intItemId = sr1.intItemId   
-		JOIN tblICItemLocation il on il.intItemLocationId=it1.intItemLocationId    
-		WHERE i.intCommodityId= @intCommodityId and il.intLocationId=@intLocationId) as ReserveQty     
- ) t 
-
-UNION ALL           
-SELECT @intCommodityId as intCommodityId,'Condo Storage' as [strType],         
-	    ISNULL(invQty,0)-ISNULL(ReserveQty,0) AS dblTotal
-FROM(            
-SELECT 
-		(SELECT sum(isnull(it1.dblUnitOnHand,0)) 		   
-		FROM tblICItem i 		
-		JOIN tblICItemStock it1 ON it1.intItemId = i.intItemId   
-		JOIN tblICItemLocation il on il.intItemLocationId=it1.intItemLocationId     
-		WHERE i.intCommodityId= @intCommodityId and il.intLocationId=@intLocationId) as invQty
-		,(SELECT SUM(isnull(sr1.dblQty,0))  	   
-		FROM tblICItem i 
-		JOIN tblICItemStock it1 ON it1.intItemId = i.intItemId   
-		JOIN tblICStockReservation sr1 ON it1.intItemId = sr1.intItemId   
-		JOIN tblICItemLocation il on il.intItemLocationId=it1.intItemLocationId    
-		WHERE i.intCommodityId= @intCommodityId and il.intLocationId=@intLocationId) as ReserveQty     
- ) t   
-
-UNION ALL           
-SELECT @intCommodityId as intCommodityId,'Other Third Party Storage' as [strType],         
-	    ISNULL(invQty,0)-ISNULL(ReserveQty,0) AS dblTotal
-FROM(            
-SELECT 
-		(SELECT sum(isnull(it1.dblUnitOnHand,0)) 		   
-		FROM tblICItem i 		
-		JOIN tblICItemStock it1 ON it1.intItemId = i.intItemId   
-		JOIN tblICItemLocation il on il.intItemLocationId=it1.intItemLocationId     
-		WHERE i.intCommodityId= @intCommodityId and il.intLocationId=@intLocationId) as invQty
-		,(SELECT SUM(isnull(sr1.dblQty,0))  	   
-		FROM tblICItem i 
-		JOIN tblICItemStock it1 ON it1.intItemId = i.intItemId   
-		JOIN tblICStockReservation sr1 ON it1.intItemId = sr1.intItemId   
-		JOIN tblICItemLocation il on il.intItemLocationId=it1.intItemLocationId    
-		WHERE i.intCommodityId= @intCommodityId and il.intLocationId=@intLocationId) as ReserveQty     
- ) t             
-
-UNION ALL           
-SELECT @intCommodityId as intCommodityId,'dblTotal Non-Receipted' as [strType],         
-	    ISNULL(invQty,0)-ISNULL(ReserveQty,0) AS dblTotal
-FROM(            
-SELECT 
-		(SELECT sum(isnull(it1.dblUnitOnHand,0)) 		   
-		FROM tblICItem i 		
-		JOIN tblICItemStock it1 ON it1.intItemId = i.intItemId   
-		JOIN tblICItemLocation il on il.intItemLocationId=it1.intItemLocationId     
-		WHERE i.intCommodityId= @intCommodityId and il.intLocationId=@intLocationId) as invQty
-		,(SELECT SUM(isnull(sr1.dblQty,0))  	   
-		FROM tblICItem i 
-		JOIN tblICItemStock it1 ON it1.intItemId = i.intItemId   
-		JOIN tblICStockReservation sr1 ON it1.intItemId = sr1.intItemId   
-		JOIN tblICItemLocation il on il.intItemLocationId=it1.intItemLocationId    
-		WHERE i.intCommodityId= @intCommodityId and il.intLocationId=@intLocationId) as ReserveQty     
- ) t   
+					
+			SELECT @intCommodityId as intCommodityId, 'Total Non-Receipted' as [strType], dblTotal FROM(         
+			SELECT  SUM(Balance) dblTotal FROM vyuGRGetStorageDetail
+			WHERE ysnReceiptedStorage = 1 AND strOwnedPhysicalStock='Customer'	AND intCommodityId = @intCommodityId and intCompanyLocationId=@intLocationId
+			GROUP BY [Storage Type])t
 
 UNION ALL           
 
@@ -186,42 +109,6 @@ AND c.intCommodityId=@intCommodityId AND c.intLocationId=@intLocationId
 	GROUP BY intContractHeaderId
 	) t
 	 WHERE dblAdjustmentAmount <> dblOriginalQuantity
-
-UNION ALL           
-SELECT @intCommodityId as intCommodityId,'Warehouse Receipts' as [strType],         
-	    ISNULL(invQty,0)-ISNULL(ReserveQty,0) AS dblTotal
-FROM(            
-SELECT 
-		(SELECT sum(isnull(it1.dblUnitOnHand,0)) 		   
-		FROM tblICItem i 		
-		JOIN tblICItemStock it1 ON it1.intItemId = i.intItemId   
-		JOIN tblICItemLocation il on il.intItemLocationId=it1.intItemLocationId     
-		WHERE i.intCommodityId= @intCommodityId and il.intLocationId=@intLocationId) as invQty
-		,(SELECT SUM(isnull(sr1.dblQty,0))  	   
-		FROM tblICItem i 
-		JOIN tblICItemStock it1 ON it1.intItemId = i.intItemId   
-		JOIN tblICStockReservation sr1 ON it1.intItemId = sr1.intItemId   
-		JOIN tblICItemLocation il on il.intItemLocationId=it1.intItemLocationId    
-		WHERE i.intCommodityId= @intCommodityId and il.intLocationId=@intLocationId) as ReserveQty     
- ) t       
-
-UNION ALL           
-SELECT @intCommodityId as intCommodityId,'DP' as [strType],         
-	    ISNULL(invQty,0)-ISNULL(ReserveQty,0) AS dblTotal
-FROM(            
-SELECT 
-		(SELECT sum(isnull(it1.dblUnitOnHand,0)) 		   
-		FROM tblICItem i 		
-		JOIN tblICItemStock it1 ON it1.intItemId = i.intItemId   
-		JOIN tblICItemLocation il on il.intItemLocationId=it1.intItemLocationId     
-		WHERE i.intCommodityId= @intCommodityId and il.intLocationId=@intLocationId) as invQty
-		,(SELECT SUM(isnull(sr1.dblQty,0))  	   
-		FROM tblICItem i 
-		JOIN tblICItemStock it1 ON it1.intItemId = i.intItemId   
-		JOIN tblICStockReservation sr1 ON it1.intItemId = sr1.intItemId   
-		JOIN tblICItemLocation il on il.intItemLocationId=it1.intItemLocationId    
-		WHERE i.intCommodityId= @intCommodityId and il.intLocationId=@intLocationId) as ReserveQty     
- ) t     
 
 UNION ALL           
 SELECT @intCommodityId as intCommodityId,'Purchase Basis Deliveries' as [strType],         
@@ -284,11 +171,7 @@ BEGIN
 					ISNULL(ReserveQty,0) AS dblTotal
 			FROM(            
 			SELECT 
-					(SELECT sum(isnull(it1.dblUnitOnHand,0)) 		   
-					FROM tblICItem i 		
-					JOIN tblICItemStock it1 ON it1.intItemId = i.intItemId 
-					WHERE i.intCommodityId= @intCommodityId ) as invQty
-					,(SELECT SUM(isnull(sr1.dblQty,0))  	   
+					(SELECT SUM(isnull(sr1.dblQty,0))  	   
 					FROM tblICItem i 
 					JOIN tblICItemStock it1 ON it1.intItemId = i.intItemId   
 					JOIN tblICStockReservation sr1 ON it1.intItemId = sr1.intItemId 
@@ -300,96 +183,25 @@ BEGIN
 					ISNULL(ReserveQty,0) AS dblTotal
 			FROM(            
 			SELECT 
-					(SELECT sum(isnull(it1.dblUnitOnHand,0)) 		   
-					FROM tblICItem i 		
-					JOIN tblICItemStock it1 ON it1.intItemId = i.intItemId   
-					WHERE i.intCommodityId= @intCommodityId ) as invQty
-					,(SELECT SUM(isnull(sr1.dblQty,0))  	   
+				(SELECT SUM(isnull(sr1.dblQty,0))  	   
 					FROM tblICItem i 
 					JOIN tblICItemStock it1 ON it1.intItemId = i.intItemId   
 					JOIN tblICStockReservation sr1 ON it1.intItemId = sr1.intItemId  
 					WHERE i.intCommodityId= @intCommodityId ) as ReserveQty     
 			 ) t    
 
-			UNION ALL           
-			SELECT @intCommodityId as intCommodityId,'Open Storage' as [strType],         
-					ISNULL(invQty,0)-ISNULL(ReserveQty,0) AS dblTotal
-			FROM(            
-			SELECT 
-					(SELECT sum(isnull(it1.dblUnitOnHand,0)) 		   
-					FROM tblICItem i 		
-					JOIN tblICItemStock it1 ON it1.intItemId = i.intItemId 
-					WHERE i.intCommodityId= @intCommodityId ) as invQty
-					,(SELECT SUM(isnull(sr1.dblQty,0))  	   
-					FROM tblICItem i 
-					JOIN tblICItemStock it1 ON it1.intItemId = i.intItemId   
-					JOIN tblICStockReservation sr1 ON it1.intItemId = sr1.intItemId  
-					WHERE i.intCommodityId= @intCommodityId ) as ReserveQty     
-			 ) t      
-
-			UNION ALL           
-			SELECT @intCommodityId as intCommodityId,'Grain Bank' as [strType],         
-					ISNULL(invQty,0)-ISNULL(ReserveQty,0) AS dblTotal
-			FROM(            
-			SELECT 
-					(SELECT sum(isnull(it1.dblUnitOnHand,0)) 		   
-					FROM tblICItem i 		
-					JOIN tblICItemStock it1 ON it1.intItemId = i.intItemId   
-					WHERE i.intCommodityId= @intCommodityId ) as invQty
-					,(SELECT SUM(isnull(sr1.dblQty,0))  	   
-					FROM tblICItem i 
-					JOIN tblICItemStock it1 ON it1.intItemId = i.intItemId   
-					JOIN tblICStockReservation sr1 ON it1.intItemId = sr1.intItemId   
-					WHERE i.intCommodityId= @intCommodityId ) as ReserveQty     
-			 ) t 
-
-			UNION ALL           
-			SELECT @intCommodityId as intCommodityId,'Condo Storage' as [strType],         
-					ISNULL(invQty,0)-ISNULL(ReserveQty,0) AS dblTotal
-			FROM(            
-			SELECT 
-					(SELECT sum(isnull(it1.dblUnitOnHand,0)) 		   
-					FROM tblICItem i 		
-					JOIN tblICItemStock it1 ON it1.intItemId = i.intItemId 
-					WHERE i.intCommodityId= @intCommodityId ) as invQty
-					,(SELECT SUM(isnull(sr1.dblQty,0))  	   
-					FROM tblICItem i 
-					JOIN tblICItemStock it1 ON it1.intItemId = i.intItemId   
-					JOIN tblICStockReservation sr1 ON it1.intItemId = sr1.intItemId   
-					WHERE i.intCommodityId= @intCommodityId ) as ReserveQty     
-			 ) t   
-
-			UNION ALL           
-			SELECT @intCommodityId as intCommodityId,'Other Third Party Storage' as [strType],         
-					ISNULL(invQty,0)-ISNULL(ReserveQty,0) AS dblTotal
-			FROM(            
-			SELECT 
-					(SELECT sum(isnull(it1.dblUnitOnHand,0)) 		   
-					FROM tblICItem i 		
-					JOIN tblICItemStock it1 ON it1.intItemId = i.intItemId   
-					WHERE i.intCommodityId= @intCommodityId ) as invQty
-					,(SELECT SUM(isnull(sr1.dblQty,0))  	   
-					FROM tblICItem i 
-					JOIN tblICItemStock it1 ON it1.intItemId = i.intItemId   
-					JOIN tblICStockReservation sr1 ON it1.intItemId = sr1.intItemId   
-					WHERE i.intCommodityId= @intCommodityId ) as ReserveQty     
-			 ) t             
-
-			UNION ALL           
-			SELECT @intCommodityId as intCommodityId,'dblTotal Non-Receipted' as [strType],         
-					ISNULL(invQty,0)-ISNULL(ReserveQty,0) AS dblTotal
-			FROM(            
-			SELECT 
-					(SELECT sum(isnull(it1.dblUnitOnHand,0)) 		   
-					FROM tblICItem i 		
-					JOIN tblICItemStock it1 ON it1.intItemId = i.intItemId  
-					WHERE i.intCommodityId= @intCommodityId ) as invQty
-					,(SELECT SUM(isnull(sr1.dblQty,0))  	   
-					FROM tblICItem i 
-					JOIN tblICItemStock it1 ON it1.intItemId = i.intItemId   
-					JOIN tblICStockReservation sr1 ON it1.intItemId = sr1.intItemId 
-					WHERE i.intCommodityId= @intCommodityId ) as ReserveQty     
-			 ) t   
+			UNION ALL  
+			SELECT @intCommodityId as intCommodityId,* FROM(         
+			SELECT  [Storage Type] strType,SUM(Balance) dblTotal FROM vyuGRGetStorageDetail
+			WHERE intCommodityId= @intCommodityId
+			GROUP BY [Storage Type])t
+			
+			UNION ALL          
+						
+			SELECT @intCommodityId as intCommodityId, 'Total Non-Receipted' as [strType], dblTotal FROM(         
+			SELECT  SUM(Balance) dblTotal FROM vyuGRGetStorageDetail
+			WHERE ysnReceiptedStorage = 1 AND strOwnedPhysicalStock='Customer'	AND intCommodityId = @intCommodityId
+			GROUP BY [Storage Type])t
 
 			UNION ALL           
 
@@ -410,39 +222,6 @@ BEGIN
 				 WHERE dblAdjustmentAmount <> dblOriginalQuantity
 
 			UNION ALL           
-			SELECT @intCommodityId as intCommodityId,'Warehouse Receipts' as [strType],         
-					ISNULL(invQty,0)-ISNULL(ReserveQty,0) AS dblTotal
-			FROM(            
-			SELECT 
-					(SELECT sum(isnull(it1.dblUnitOnHand,0)) 		   
-					FROM tblICItem i 		
-					JOIN tblICItemStock it1 ON it1.intItemId = i.intItemId   
-					 
-					WHERE i.intCommodityId= @intCommodityId ) as invQty
-					,(SELECT SUM(isnull(sr1.dblQty,0))  	   
-					FROM tblICItem i 
-					JOIN tblICItemStock it1 ON it1.intItemId = i.intItemId   
-					JOIN tblICStockReservation sr1 ON it1.intItemId = sr1.intItemId  
-					WHERE i.intCommodityId= @intCommodityId ) as ReserveQty     
-			 ) t       
-
-			UNION ALL           
-			SELECT @intCommodityId as intCommodityId,'DP' as [strType],         
-					ISNULL(invQty,0)-ISNULL(ReserveQty,0) AS dblTotal
-			FROM(            
-			SELECT 
-					(SELECT sum(isnull(it1.dblUnitOnHand,0)) 		   
-					FROM tblICItem i 		
-					JOIN tblICItemStock it1 ON it1.intItemId = i.intItemId   
-					WHERE i.intCommodityId= @intCommodityId ) as invQty
-					,(SELECT SUM(isnull(sr1.dblQty,0))  	   
-					FROM tblICItem i 
-					JOIN tblICItemStock it1 ON it1.intItemId = i.intItemId   
-					JOIN tblICStockReservation sr1 ON it1.intItemId = sr1.intItemId 
-					WHERE i.intCommodityId= @intCommodityId ) as ReserveQty     
-			 ) t     
-
-			UNION ALL           
 			SELECT @intCommodityId as intCommodityId,'Purchase Basis Deliveries' as [strType],         
 					ISNULL(invQty,0)-ISNULL(ReserveQty,0) AS dblTotal
 			FROM(            
@@ -456,6 +235,6 @@ BEGIN
 					JOIN tblICItemStock it1 ON it1.intItemId = i.intItemId   
 					JOIN tblICStockReservation sr1 ON it1.intItemId = sr1.intItemId 
 					WHERE i.intCommodityId= @intCommodityId ) as ReserveQty     
-			 ) t      
+			 ) t     
 
 END
