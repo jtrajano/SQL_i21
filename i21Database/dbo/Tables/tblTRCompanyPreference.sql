c5b78d7c389a24b38e1b2fbdecedbb480081e5cb
Pruthvi <pruthvi.c@irely.com>
2015-07-18 18:45:29 +0530

    TR-104

---
+++ i21Database/dbo/Tables/tblTRCompanyPreference.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[tblTRCompanyPreference]
+(
+	[intCompanyPreferenceId] INT NOT NULL IDENTITY,
+	[intItemForFreightId]   int NULL,
+	[intShipViaId]   int NULL,
+	[intSellerId]   int NULL,
+	[intTerminalId]   int NULL,
+	[intBulkPlantTypeId] int NULL,
+	[intXferAcctStatusId] int NULL,
+	[intCongsinmentId] int NULL,
+	[strRackPriceToUse] int NULL,
+    [ysnTaxesInXfer] BIT NULL, 
+    [ysnAverageCostForNonBulkPlant] BIT NULL,
+	[ysnAutoAssignInvoiceNumber] BIT NULL,
+	[ysnSellerAsBillTo] BIT NULL,	
+	[intConcurrencyId] INT NOT NULL DEFAULT 1,
+	CONSTRAINT [PK_tblTRCompanyPreference_intCompanyPreferenceId] PRIMARY KEY CLUSTERED ([intCompanyPreferenceId] ASC)
+)
