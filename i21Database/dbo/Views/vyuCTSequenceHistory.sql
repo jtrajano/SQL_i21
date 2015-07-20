a7c62e39d602ae2aa110334b416cbb5197a707b1
Satya <ssahoo@BLRSATYAWIN7.irely.net>
2015-07-20 11:19:43 +0530

    CT-131

---
+++ i21Database/dbo/Views/vyuCTSequenceHistory.sql
@@ -0,0 +1,15 @@
+﻿CREATE VIEW [dbo].[vyuCTSequenceHistory]
+AS 
+
+	SELECT	CA.dtmAdjustmentDate,
+			'Inventory Receipt' AS	strAdjustmentType,
+			IR.strReceiptNumber	AS	strNumber,
+			'Balance'			AS	strFieldName,
+			CA.dblOldBalance	AS	dblOldValue,
+			CA.dblAdjAmount	AS	dblAdjAmount,
+			CA.dblNewBalance	AS	dblNewValue,
+			US.strUserName
+	FROM	tblCTContractAdjustment		CA
+	JOIN	tblICInventoryReceiptItem	RI	ON	RI.intInventoryReceiptItemId	=	CA.intInventoryReceiptItemId AND CA.intInventoryReceiptItemId IS NOT NULL
+	JOIN	tblICInventoryReceipt		IR	ON	IR.intInventoryReceiptId		=	RI.intInventoryReceiptId
+	JOIN	tblSMUserSecurity			US	ON	US.intUserSecurityID			=	CA.intUserId
