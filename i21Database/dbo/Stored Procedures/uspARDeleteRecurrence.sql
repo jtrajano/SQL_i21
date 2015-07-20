c0e763eff3f2250337c04d71c0817a4a1c1c05cb
Ruben  Escarcha <ruben.escarcha@irely.com>
2015-07-21 01:38:12 +0800

    AR-1376 Code for Invoice Template

---
+++ i21Database/dbo/Stored Procedures/uspARDeleteRecurrence.sql
@@ -0,0 +1,35 @@
+﻿CREATE PROCEDURE [dbo].[uspARDeleteRecurrence]
+	 @TransactionId		AS INT
+	,@TransactionNumber	AS NVARCHAR(100)
+	,@TransactionType	AS NVARCHAR(100)
+	,@UserId			AS INT
+	,@IsSucessfull		AS BIT		= NULL OUTPUT
+AS
+
+BEGIN
+
+
+SET QUOTED_IDENTIFIER OFF
+SET ANSI_NULLS ON
+SET NOCOUNT ON
+SET XACT_ABORT ON
+SET ANSI_WARNINGS OFF
+
+
+SET @IsSucessfull = 0;
+
+
+DELETE 
+FROM
+	[tblSMRecurringTransaction]
+WHERE
+		[intTransactionId] = @TransactionId
+	AND [strTransactionType] = @TransactionType
+	AND [strTransactionNumber] = @TransactionNumber
+	
+	
+SET @IsSucessfull = 1;
+
+RETURN 1
+
+END
