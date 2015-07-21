a88f9d0952a5a9fb2c4c0082ebf3634d62bd0c71
bonn ponce <bonn.ponce@irely.com>
2015-07-21 13:23:25 +0800

    RPT-786 Motor Fuel Tax Cycle screen does not open in i21 Demo Server

--- i21PreDeployment/Scripts/Pre-Deployment/10_RPT/1_1430_to_1430.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/10_RPT/1_1430_to_1430.sql
@@ -29,3 +29,27 @@ BEGIN
     END
 END
 GO
+
+--=====================MOTOR FUEL TAX CYCLE========================--
+--This script will alter the data type for date fields from char(8) to int. This will also delete all records from pxcyctag table
+IF EXISTS (SELECT TOP 1 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[pxcyctag]') AND type in (N'U')) 
+BEGIN
+	DELETE FROM pxcyctag
+	IF EXISTS (SELECT TOP 1 1 FROM sys.columns WHERE NAME  = N'pxcyctag_end_rev_dt' AND OBJECT_ID = OBJECT_ID(N'pxcyctag'))
+    BEGIN
+        ALTER TABLE pxcyctag 
+		ALTER COLUMN pxcyctag_end_rev_dt int NULL 
+    END
+	IF EXISTS (SELECT TOP 1 1 FROM sys.columns WHERE NAME  = N'pxcyctag_beg_rev_dt' AND OBJECT_ID = OBJECT_ID(N'pxcyctag'))
+    BEGIN
+        ALTER TABLE pxcyctag 
+		ALTER COLUMN pxcyctag_beg_rev_dt int NULL 
+    END
+	IF NOT EXISTS (SELECT TOP 1 1 FROM sys.columns WHERE NAME  = N'A4GLIdentity' AND OBJECT_ID = OBJECT_ID(N'pxcyctag'))
+    BEGIN
+        ALTER TABLE pxcyctag 
+		ADD A4GLIdentity NUMERIC(9, 0) NOT NULL IDENTITY
+    END
+END
+GO
+
