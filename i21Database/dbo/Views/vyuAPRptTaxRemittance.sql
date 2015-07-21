9f4ed5c5f01eb57b166a7935628d58fad5b310c6
Lex Eugene Peregrino <lex.peregrino@irely.com>
2015-07-21 14:13:56 +0800

    AP-1314

---
+++ i21Database/dbo/Views/vyuAPRptTaxRemittance.sql
@@ -0,0 +1,18 @@
+﻿CREATE VIEW [dbo].[vyuAPRptTaxRemittance]
+	AS 
+	 SELECT 
+		APV.strVendorId,
+		APV.intEntityVendorId,
+		'' as strTaxCode, -- no tax code field
+		dblComputedAmount = SUM(APP.dblWithheld),
+		Cast(APP.dtmDatePaid as Date )as dtmDate
+	FROM dbo.tblAPVendor APV
+	INNER JOIN 
+		tblAPPayment APP
+	ON APP.intEntityVendorId = APV.intEntityVendorId
+	WHERE 
+		APV.ysnWithholding = 1
+	GROUP BY 
+		APV.strVendorId,
+		APV.intEntityVendorId,
+		APP.dtmDatePaid
