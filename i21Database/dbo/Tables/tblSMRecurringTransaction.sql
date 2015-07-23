9ff01bf373e29cfc173ebc3e6a65151fd851d8d1
Edgardo Amurao <ed.amurao@irely.com>
2015-07-23 14:29:47 +0800

    .

--- i21Database/dbo/Tables/tblSMRecurringTransaction.sql
+++ i21Database/dbo/Tables/tblSMRecurringTransaction.sql
@@ -6,6 +6,7 @@
     [strTransactionType] NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL, 
     [strReference] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
 	[strResponsibleUser] NVARCHAR(100) COLLATE Latin1_General_CI_AS NOT NULL DEFAULT '', 
+	[intUserSecurityID] INT NOT NULL DEFAULT 0, 
 	[intWarningDays] INT NULL  , 
     [strFrequency] NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL, 
     [dtmLastProcess] DATETIME NOT NULL, 
