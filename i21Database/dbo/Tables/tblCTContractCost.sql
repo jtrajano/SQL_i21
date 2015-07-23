9a9458bd55e9ddf6be7aa37d489a6e68ed4daf52
Darwin Estrada <darwin.estrada@irely.com>
2015-07-23 17:15:50 -0400

    remove tblCTContractCost link to tblICItem - causing build problem.

--- i21Database/dbo/Tables/tblCTContractCost.sql
+++ i21Database/dbo/Tables/tblCTContractCost.sql
@@ -2,7 +2,7 @@ CREATE TABLE [dbo].[tblCTContractCost](
 	[intContractCostId] [int] IDENTITY(1,1) NOT NULL,
 	[intConcurrencyId] [int] NOT NULL,
 	[intContractDetailId] [int] NOT NULL,
-	[intItemId] [int] NOT NULL,
+	[intItemId] [int] NOT NULL DEFAULT 0,
 	[intVendorId] [int] NULL,
 	[strCostMethod] [nvarchar](30) COLLATE Latin1_General_CI_AS NOT NULL,
 	[dblRate] [numeric](10, 4) NOT NULL,
@@ -12,7 +12,6 @@ CREATE TABLE [dbo].[tblCTContractCost](
 	[ysnPrice] [bit] NULL,
 	CONSTRAINT [PK_tblCTContractCost_intContractCostId] PRIMARY KEY CLUSTERED ([intContractCostId] ASC),
 	CONSTRAINT [FK_tblCTContractCost_tblCTContractDetail_intContractDetailId] FOREIGN KEY ([intContractDetailId]) REFERENCES [tblCTContractDetail]([intContractDetailId]) ON DELETE CASCADE,
-	CONSTRAINT [FK_tblCTContractCost_tblICItem_intCostTypeId] FOREIGN KEY ([intItemId]) REFERENCES [tblICItem]([intItemId]),
 	CONSTRAINT [FK_tblCTContractCost_tblAPVendor_intVendorId] FOREIGN KEY ([intVendorId]) REFERENCES [tblAPVendor]([intEntityVendorId]),
 	CONSTRAINT [FK_tblCTContractCost_tblICItemUOM_intItemUOMId] FOREIGN KEY ([intItemUOMId]) REFERENCES [tblICItemUOM]([intItemUOMId])
 )
