9abf247f5daa2429af7308fc3b123b7533c6edd5
Kelvin Quinto <kelvin.quinto@irely.com>
2015-07-24 10:19:11 +0800

    AR-1415 - Miscellaneous item not displayed on Add Inventory Shipment screen

--- i21Database/dbo/Views/vyuARShippedItems.sql
+++ i21Database/dbo/Views/vyuARShippedItems.sql
@@ -198,3 +198,83 @@ LEFT OUTER JOIN
 WHERE
 	SOD.[intSalesOrderDetailId] NOT IN (SELECT ISNULL(tblARInvoiceDetail.[intSalesOrderDetailId],0) FROM tblARInvoiceDetail INNER JOIN tblARInvoice ON tblARInvoiceDetail.intInvoiceId = tblARInvoice.intInvoiceId WHERE tblARInvoice.[ysnPosted] = 1)
 	AND SO.[strTransactionType] = 'Order'
+
+	UNION ALL
+
+SELECT
+	 SO.[intEntityCustomerId]
+	,E.[strName]						AS [strCustomerName]
+	,SO.[intSalesOrderId]
+	,SO.[strSalesOrderNumber]
+	,SO.[dtmProcessDate]
+	,NULL								AS [intInventoryShipmentItemId]
+	,''									AS [strShipmentNumber]
+	,SOD.[intSalesOrderDetailId]
+	,SO.[intCompanyLocationId]
+	,CL.[strLocationName] 
+	,SO.[intShipToLocationId]
+	,SO.[intFreightTermId]
+	,SOD.[intItemId]	
+	,NULL								AS [strItemNo] 
+	,SOD.[strItemDescription]
+	,SOD.[intItemUOMId]
+	,SOD.[intItemUOMId]					AS [intShipmentItemUOMId]
+	,U.[strUnitMeasure]					AS [strShipmentUnitMeasure]
+	,SOD.[dblQtyOrdered] 
+	,SOD.[dblQtyOrdered]				AS [dblShipmentQuantity] 
+	,SOD.[dblQtyShipped]	
+	,SOD.[dblQtyShipped]				AS [dblShipmentQtyShipped] 
+	,SOD.[dblQtyShipped]				AS [dblShipmentQtyShippedTotal]
+	,SOD.[dblQtyOrdered] 
+		- SOD.[dblQtyShipped]			AS [dblQtyRemaining]
+	,SOD.[dblDiscount] 
+	,SOD.[dblPrice]
+	,SOD.[dblPrice]						AS [dblShipmentUnitPrice] 			
+	,SOD.[dblTotalTax]
+	,SOD.[dblTotal]
+	,SOD.[intAccountId]
+	,SOD.[intCOGSAccountId]
+	,SOD.[intSalesAccountId]
+	,SOD.[intInventoryAccountId]
+	,SOD.[intStorageLocationId]
+	,SL.[strName]						AS [strStorageLocationName]
+	,T.[intTermID]
+	,T.[strTerm]
+	,S.[intEntityShipViaId] 
+	,S.[strName]						AS [strShipVia]
+	,NULL								AS [intTicketNumber]
+	,NULL								AS [intTicketId]
+FROM
+	tblSOSalesOrder SO
+INNER JOIN
+	tblSOSalesOrderDetail SOD
+		ON SO.[intSalesOrderId] = SOD.[intSalesOrderId]
+		AND SOD.intItemId IS NULL
+		AND SOD.strItemDescription <> ''
+INNER JOIN
+	tblARCustomer C
+		ON SO.[intEntityCustomerId] = C.[intEntityCustomerId] 
+INNER JOIN
+	tblEntity E
+		ON C.[intEntityCustomerId] = E.[intEntityId] 
+LEFT OUTER JOIN
+	tblSMTerm T
+		ON SO.[intTermId] = T.[intTermID] 
+LEFT OUTER JOIN
+	tblSMShipVia S
+		ON SO.[intShipViaId] = S.[intEntityShipViaId] 
+LEFT OUTER JOIN
+	tblICStorageLocation SL
+		ON SOD.[intStorageLocationId] = SL.[intStorageLocationId]
+LEFT JOIN
+	tblICItemUOM IU
+		ON SOD.[intItemUOMId] = IU.[intItemUOMId]
+LEFT JOIN
+	tblICUnitMeasure U
+		ON IU.[intUnitMeasureId] = U.[intUnitMeasureId]
+LEFT OUTER JOIN
+	tblSMCompanyLocation CL
+		ON SO.[intCompanyLocationId] = CL.[intCompanyLocationId]		
+WHERE
+	SOD.[intSalesOrderDetailId] NOT IN (SELECT ISNULL(tblARInvoiceDetail.[intSalesOrderDetailId],0) FROM tblARInvoiceDetail INNER JOIN tblARInvoice ON tblARInvoiceDetail.intInvoiceId = tblARInvoice.intInvoiceId WHERE tblARInvoice.[ysnPosted] = 1)
+	AND SO.[strTransactionType] = 'Order'
