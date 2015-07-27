ae66f5f42854fb8393547063aa73e4e61c17dda7
Pruthvi <pruthvi.c@irely.com>
2015-07-27 12:48:34 +0530

    TR-126

---
+++ i21Database/dbo/Views/vyuTRTansportReceipt.sql
@@ -0,0 +1,32 @@
+﻿
+CREATE VIEW [dbo].[vyuTRTransportReceipt]
+	AS 
+SELECT    
+	TL.intTransportLoadId,
+	TR.intTransportReceiptId,
+    CT.intContractHeaderId,
+	TR.intContractDetailId,
+	CT.intContractNumber,
+	TL.strTransaction,
+	LG.dblQuantity as dblOrderedQuantity,	
+	dblReceivedQuantity     = CASE
+								  WHEN SP.strGrossOrNet = 'Gross'
+								  THEN TR.dblGross
+								  WHEN SP.strGrossOrNet = 'Net'
+								  THEN TR.dblNet
+								  END
+FROM
+    
+	 dbo.tblTRTransportLoad TL
+	JOIN dbo.tblTRTransportReceipt TR
+		ON TL.intTransportLoadId = TR.intTransportLoadId 
+    JOIN dbo.tblTRSupplyPoint SP
+	     ON SP.intSupplyPointId = TR.intSupplyPointId
+	LEFT JOIN dbo.vyuCTContractDetailView CT 
+	    on TR.intContractDetailId = CT.intContractDetailId
+    LEFT JOIN dbo.vyuLGLoadView LG
+	    on LG.intLoadId = TL.intLoadId
+		
+	
+	
+
