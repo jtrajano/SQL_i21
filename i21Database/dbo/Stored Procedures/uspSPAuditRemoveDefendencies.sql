aa93530789ba0d22f1f52f16970eb15c712bb77a
Crisman Lazaga <cris.lazaga@irely.com>
2015-07-29 10:10:30 +0800

    INS-680

---
+++ i21Database/dbo/Stored Procedures/uspSPAuditRemoveDefendencies.sql
@@ -0,0 +1,84 @@
+﻿CREATE PROCEDURE [dbo].[uspSPAuditRemoveDefendencies]
+	@Table NVARCHAR(150) = null,
+	@Column NVARCHAR(150) = null
+AS
+BEGIN	
+	SET NOCOUNT ON;
+
+	-- REMOVE SCHEMA BINDING
+	DECLARE @ViewName NVARCHAR(MAX), 
+		@ViewScript NVARCHAR(MAX)
+
+	DECLARE Cursor_View CURSOR FOR 
+		SELECT TABLE_NAME, VIEW_DEFINITION FROM  INFORMATION_SCHEMA.VIEWS 
+			where VIEW_DEFINITION like '%'+ @Table +'%'
+			and VIEW_DEFINITION like '%WITH SCHEMABINDING%'
+			and VIEW_DEFINITION like '%' + @Column +'%'
+
+	OPEN Cursor_View FETCH NEXT FROM Cursor_View into @ViewName, @ViewScript
+	WHILE (@@FETCH_STATUS <> -1)
+	BEGIN
+		SET @ViewScript = REPLACE(@ViewScript,'CREATE', 'ALTER')
+		SET @ViewScript = REPLACE(@ViewScript,'WITH SCHEMABINDING', '')
+		EXEC sp_executesql @ViewScript
+		FETCH NEXT FROM Cursor_View into @ViewName, @ViewScript
+	END
+
+	CLOSE Cursor_View
+	DEALLOCATE Cursor_View
+
+	--REMOVE CONTRAINTS
+	DECLARE @TableConstrainName NVARCHAR(MAX)
+
+	DECLARE Cursor_OwnConstraint CURSOR FOR
+		SELECT CONSTRAINT_NAME FROM  INFORMATION_SCHEMA.TABLE_CONSTRAINTS 
+			where TABLE_NAME = REPLACE(@Table,'dbo.','') 
+			AND CONSTRAINT_TYPE <> 'PRIMARY KEY'
+
+	OPEN Cursor_OwnConstraint FETCH NEXT FROM Cursor_OwnConstraint into @TableConstrainName
+	WHILE (@@FETCH_STATUS <> -1)
+	BEGIN
+			
+		--REMOVE REFERENCIAL CONTRAINTS
+		DECLARE @RefConstrainName NVARCHAR(MAX),
+			@RefTable NVARCHAR(MAX)
+
+		DECLARE Cursor_RefConstraint CURSOR FOR
+			SELECT R.CONSTRAINT_NAME, R.TABLE_NAME
+				FROM INFORMATION_SCHEMA.CONSTRAINT_COLUMN_USAGE U
+					INNER JOIN INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS FK
+						ON U.CONSTRAINT_CATALOG = FK.UNIQUE_CONSTRAINT_CATALOG
+						AND U.CONSTRAINT_SCHEMA = FK.UNIQUE_CONSTRAINT_SCHEMA
+						AND U.CONSTRAINT_NAME = FK.UNIQUE_CONSTRAINT_NAME
+					INNER JOIN INFORMATION_SCHEMA.CONSTRAINT_COLUMN_USAGE R
+						ON R.CONSTRAINT_CATALOG = FK.CONSTRAINT_CATALOG
+						AND R.CONSTRAINT_SCHEMA = FK.CONSTRAINT_SCHEMA
+						AND R.CONSTRAINT_NAME = FK.CONSTRAINT_NAME
+					WHERE U.TABLE_NAME = REPLACE(@Table,'dbo.','') 
+						AND U.CONSTRAINT_NAME = @TableConstrainName
+
+		OPEN Cursor_RefConstraint FETCH NEXT FROM Cursor_RefConstraint into @RefConstrainName, @RefTable
+		WHILE (@@FETCH_STATUS <> -1)
+		BEGIN
+			--REMOVE REFERENCIAL CONTRAINTS
+			DECLARE @RefConstrainScript NVARCHAR(MAX)
+			SET @RefConstrainScript = 'ALTER TABLE ' + @RefTable + ' DROP CONSTRAINT [' +@RefConstrainName + ']'
+			EXEC sp_executesql @RefConstrainScript
+			FETCH NEXT FROM Cursor_RefConstraint into @RefConstrainName, @RefTable
+		END	
+
+		CLOSE Cursor_RefConstraint
+		DEALLOCATE Cursor_RefConstraint
+
+		--REMOVE TABLE CONTRAINTS
+		DECLARE @TableConstrainScript NVARCHAR(MAX)
+		SET @TableConstrainScript = 'ALTER TABLE ' + @Table + ' DROP CONSTRAINT [' + @TableConstrainName + ']'
+		EXEC sp_executesql @TableConstrainScript
+		FETCH NEXT FROM Cursor_OwnConstraint into @TableConstrainName
+	END	
+
+	CLOSE Cursor_OwnConstraint
+	DEALLOCATE Cursor_OwnConstraint
+
+END
+
