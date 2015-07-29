9af1d915bbbd342e0da2068e5133647844dc66d8
bonn ponce <bonn.ponce@irely.com>
2015-07-29 13:18:03 +0800

    CF-308

--- i21Database/dbo/Tables/tblCFAccountFailedImport.sql
+++ i21Database/dbo/Tables/tblCFAccountFailedImport.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[tblCFAccountFailedImport] (
     [intAccountFailedImportId] INT            IDENTITY (1, 1) NOT NULL,
-    [strAccountNumber]         NVARCHAR (MAX) NOT NULL,
-    [strReason]                NVARCHAR (MAX) NULL,
+    [strAccountNumber]         NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NOT NULL,
+    [strReason]                NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NULL,
     CONSTRAINT [PK_tblCFAccountFailedImport] PRIMARY KEY CLUSTERED ([intAccountFailedImportId] ASC)
 );
 
--- i21Database/dbo/Tables/tblCFAccountSuccessImport.sql
+++ i21Database/dbo/Tables/tblCFAccountSuccessImport.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[tblCFAccountSuccessImport] (
     [intAccountSuccessImportId] INT            IDENTITY (1, 1) NOT NULL,
-    [strAccountNumber]          NVARCHAR (MAX) NULL,
+    [strAccountNumber]          NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NULL,
     CONSTRAINT [PK_tblCFAccountSuccessImport] PRIMARY KEY CLUSTERED ([intAccountSuccessImportId] ASC)
 );
 
