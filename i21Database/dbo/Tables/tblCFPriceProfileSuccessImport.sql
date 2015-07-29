c0e1e05d5860417c6e6b5a8a5188387552bd6297
bonn ponce <bonn.ponce@irely.com>
2015-07-29 18:03:17 +0800

    CF-306 -CF Price Profile

---
+++ i21Database/dbo/Tables/tblCFPriceProfileSuccessImport.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[tblCFPriceProfileSuccessImport] (
+    [intPriceProfileSuccessImportId] INT            IDENTITY (1, 1) NOT NULL,
+    [strPriceProfileId]              NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NULL,
+    CONSTRAINT [PK_tblCFPriceProfileSuccessImport] PRIMARY KEY CLUSTERED ([intPriceProfileSuccessImportId] ASC)
+);
+
