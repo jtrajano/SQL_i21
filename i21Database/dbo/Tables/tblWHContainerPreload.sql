dde69b82fddfb32a3075752c4ec576fe8ea466c9
Swetosuvro <swetosuvro.g@irely.com>
2015-07-30 16:23:40 +0530

    WH-42

--- i21Database/dbo/Tables/tblWHContainerPreload.sql
+++ i21Database/dbo/Tables/tblWHContainerPreload.sql
@@ -2,16 +2,16 @@
 (
 	[intPreloadContainerId] INT NOT NULL IDENTITY,
 	[intConcurrencyId] INT NOT NULL,
-	[strContainerNo] NVARCHAR(50) NOT NULL,
+	[strContainerNo] NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL,
 	[intUnitId] INT NOT NULL,
 	[intLotId] INT NOT NULL,
-	[strLotCode] NVARCHAR(50),
+	[strLotCode] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
 	[dblQty] NUMERIC(18,6) NOT NULL,
 	[intQtyUOMId] INT NOT NULL,
 	[ysnWHContainerCreated] BIT DEFAULT 0,
 	[intAddressID] INT,
 	[dtmProductionDate] DATETIME,
-	[strLastUpdateBy] NVARCHAR(50),
+	[strLastUpdateBy] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
 	[dtmLastUpdateOn] DATETIME,
 
 	CONSTRAINT [PK_tblWHContainerPreload_intPreloadContainerId] PRIMARY KEY ([intPreloadContainerId])
--- i21Database/dbo/Tables/tblWHCycleCountType.sql
+++ i21Database/dbo/Tables/tblWHCycleCountType.sql
@@ -2,7 +2,7 @@
 (
 	[intCycleCountTypeID] INT NOT NULL IDENTITY,
 	[intConcurrencyId] INT NOT NULL,
-	[strCycleCountTypeCode] NVARCHAR(32),
+	[strCycleCountTypeCode] NVARCHAR(32) COLLATE Latin1_General_CI_AS NULL, 
 
 	CONSTRAINT [PK_tblWHCycleCountType_intCycleCountTypeID] PRIMARY KEY ([intCycleCountTypeID])
 
