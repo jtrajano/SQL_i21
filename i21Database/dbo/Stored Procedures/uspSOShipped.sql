4fa24965a7e4ce267b859e5dc84d7794b7492878
Kelvin Quinto <kelvin.quinto@irely.com>
2015-07-30 15:35:04 +0800

    AR-1446

--- i21Database/dbo/Stored Procedures/uspSOShipped.sql
+++ i21Database/dbo/Stored Procedures/uspSOShipped.sql
@@ -7,3 +7,24 @@ SET ANSI_NULLS ON
 SET NOCOUNT ON
 SET XACT_ABORT ON
 SET ANSI_WARNINGS OFF
+--	@intTransactionId INT
+--AS
+--BEGIN	
+--	DECLARE @OrderToUpdate TABLE (intSalesOrderId INT);
+
+--	INSERT INTO @OrderToUpdate(intSalesOrderId)
+--	SELECT DISTINCT intSourceId 
+--		FROM tblICInventoryShipmentItem 
+--	WHERE intInventoryShipmentId = @intTransactionId
+	
+--	WHILE EXISTS(SELECT TOP 1 NULL FROM @OrderToUpdate ORDER BY intSalesOrderId)
+--	BEGIN				
+--		DECLARE @intSalesOrderId INT;
+					
+--		SELECT TOP 1 @intSalesOrderId = intSalesOrderId FROM @OrderToUpdate ORDER BY intSalesOrderId
+
+--		EXEC dbo.uspSOUpdateOrderShipmentStatus @intSalesOrderId
+			
+--		DELETE FROM @OrderToUpdate WHERE intSalesOrderId = @intSalesOrderId AND intSalesOrderId = @intSalesOrderId
+--	END 
+--END
