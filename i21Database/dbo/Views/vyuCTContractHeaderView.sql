eb9d2c7a7356227757ab9e7614902ef2b224883d
Satya <ssahoo@BLRSATYAWIN7.irely.net>
2015-07-30 23:24:35 +0530

    CT-209

--- i21Database/dbo/Views/vyuCTContractHeaderView.sql
+++ i21Database/dbo/Views/vyuCTContractHeaderView.sql
@@ -2,9 +2,9 @@
 
 AS
 
-	SELECT	CH.intContractHeaderId,		CH.intContractTypeId,			CH.intConcurrencyId		AS	intHeaderConcurrencyId,					
-			CH.intCommodityId,			CH.strCustomerContract,			CH.intCommodityUOMId	AS	intCommodityUnitMeasureId,					
-			CH.intContractNumber,		CH.dtmContractDate,				CH.dblQuantity			AS	dblHeaderQuantity,
+	SELECT	CH.intContractHeaderId,		CH.intContractTypeId,			CH.intConcurrencyId			AS	intHeaderConcurrencyId,					
+			CH.intCommodityId,			CH.strCustomerContract,			CH.intCommodityUOMId		AS	intCommodityUnitMeasureId,					
+			CH.intContractNumber,		CH.dtmContractDate,				CH.dblQuantity				AS	dblHeaderQuantity,
 			CH.dtmDeferPayDate,			CH.dblDeferPayRate,				CH.intContractTextId,			
 			CH.strInternalComments,		CH.ysnSigned,					CH.ysnPrinted,
 			CH.intSalespersonId,		CH.intGradeId,					CH.intWeightId,									
@@ -13,36 +13,53 @@ AS
 			CH.intPositionId,			CH.intInsuranceById,			CH.intInvoiceTypeId,
 			CH.dblTolerancePct,			CH.dblProvisionalInvoicePct,	CH.ysnPrepaid,
 			CH.ysnSubstituteItem,		CH.ysnUnlimitedQuantity,		CH.ysnMaxPrice,
-			CH.intINCOLocationTypeId,	CH.intCountryId,
-			
-			EY.intEntityId,				EY.strEntityName,				CY.strDescription		AS	strCommodityDescription,
-			EY.strEntityNumber,			EY.strEntityAddress,			U2.strUnitMeasure		AS	strHeaderUnitMeasure,
-			EY.strEntityState,			EY.strEntityZipCode,			W1.strWeightGradeDesc	AS	strGrade,
-			EY.strEntityPhone,			EY.intDefaultLocationId,		W2.strWeightGradeDesc	AS	strWeight,	
-			EY.strEntityType,			TX.strTextCode,					AN.strName				AS	strAssociationName,
-			EY.strEntityCity,			TM.strTerm,						CB.strDescription		AS	strContractBasisDescription,
-			EY.strEntityCountry,		PO.strPosition,					IB.strDescription		AS	strInsuranceByDescription,
-			TP.strContractType,			IB.strInsuranceBy,				IT.strDescription		AS	strInvoiceTypeDescription,
-			IT.strInvoiceType,			CO.strCountry,					AB.strDescription		AS	strApprovalBasisDescription,
+			CH.intINCOLocationTypeId,	CH.intCountryId,				CH.intPricingTypeId,
+			CH.ysnProvisional,			CH.ysnLoad,						CH.intCompanyLocationPricingLevelId,
+			CH.intNoOfLoad,				CH.dblQuantityPerLoad,			CH.intLoadUOMId,
+			CH.ysnCategory,				CH.ysnMultiplePriceFixation,	CH.intCategoryUnitMeasureId,
+			CH.intLoadCategoryUnitMeasureId,
+
+			EY.intEntityId,				EY.strEntityName,				CY.strDescription			AS	strCommodityDescription,
+			EY.strEntityNumber,			EY.strEntityAddress,			U2.strUnitMeasure			AS	strHeaderUnitMeasure,
+			EY.strEntityState,			EY.strEntityZipCode,			W1.strWeightGradeDesc		AS	strGrade,
+			EY.strEntityPhone,			EY.intDefaultLocationId,		W2.strWeightGradeDesc		AS	strWeight,	
+			EY.strEntityType,			TX.strTextCode,					AN.strName					AS	strAssociationName,
+			EY.strEntityCity,			TM.strTerm,						CB.strDescription			AS	strContractBasisDescription,
+			EY.strEntityCountry,		PO.strPosition,					IB.strDescription			AS	strInsuranceByDescription,
+			TP.strContractType,			IB.strInsuranceBy,				IT.strDescription			AS	strInvoiceTypeDescription,
+			IT.strInvoiceType,			CO.strCountry,					AB.strDescription			AS	strApprovalBasisDescription,
 			CY.strCommodityCode,		SP.strSalespersonId,			CB.strINCOLocationType,
-			AB.strApprovalBasis,		CB.strContractBasis				
+			AB.strApprovalBasis,		CB.strContractBasis,			PT.strPricingType,
+			PL.strPricingLevelName,										U3.strUnitMeasure			AS	strLoadUnitMeasure,
+																		U4.strUnitMeasure			AS	strCategoryUnitMeasure,
+																		U5.strUnitMeasure			AS	strLoadCategoryUnitMeasure,
+			CASE WHEN strINCOLocationType = 'City' THEN CT.strCity ELSE SL.strSubLocationName	END	AS	strINCOLocation,
+			dbo.fnCTGetContractStatuses(CH.intContractHeaderId)	AS	strStatuses
 			
-	FROM	tblCTContractHeader			CH	
-	JOIN	vyuCTEntity					EY	ON	EY.intEntityId					=		CH.intEntityId			AND
-												EY.strEntityType				=		(CASE WHEN CH.intContractTypeId = 1 THEN 'Vendor' ELSE 'Customer' END)
-	JOIN	tblCTContractType			TP	ON	TP.intContractTypeId			=		CH.intContractTypeId
-	JOIN	tblARSalesperson			SP	ON	SP.intEntitySalespersonId		=		CH.intSalespersonId
-	JOIN	tblSMTerm					TM	ON	TM.intTermID					=		CH.intTermId			LEFT
-	JOIN	tblICCommodity				CY	ON	CY.intCommodityId				=		CH.intCommodityId		LEFT
-	JOIN	tblCTAssociation			AN	ON	AN.intAssociationId				=		CH.intAssociationId		LEFT
-	JOIN	tblCTContractText			TX	ON	TX.intContractTextId			=		CH.intContractTextId	LEFT
-	JOIN	tblCTApprovalBasis			AB	ON	AB.intApprovalBasisId			=		CH.intApprovalBasisId	LEFT
-	JOIN	tblCTContractBasis			CB	ON	CB.intContractBasisId			=		CH.intContractBasisId	LEFT
-	JOIN	tblCTPosition				PO	ON	PO.intPositionId				=		CH.intPositionId		LEFT
-	JOIN	tblCTInsuranceBy			IB	ON	IB.intInsuranceById				=		CH.intInsuranceById		LEFT
-	JOIN	tblCTInvoiceType			IT	ON	IT.intInvoiceTypeId				=		CH.intInvoiceTypeId		LEFT
-	JOIN	tblSMCountry				CO	ON	CO.intCountryID					=		CH.intCountryId			LEFT
-	JOIN	tblICCommodityUnitMeasure	CM	ON	CM.intCommodityUnitMeasureId	=		CH.intCommodityUOMId	LEFT
-	JOIN	tblICUnitMeasure			U2	ON	U2.intUnitMeasureId				=		CM.intUnitMeasureId		LEFT
-	JOIN	tblCTWeightGrade			W1	ON	W1.intWeightGradeId				=		CH.intGradeId			LEFT
-	JOIN	tblCTWeightGrade			W2	ON	W2.intWeightGradeId				=		CH.intWeightId
+	FROM	tblCTContractHeader					CH	
+	JOIN	vyuCTEntity							EY	ON	EY.intEntityId						=		CH.intEntityId			AND
+														EY.strEntityType					=		(CASE WHEN CH.intContractTypeId = 1 THEN 'Vendor' ELSE 'Customer' END)
+	JOIN	tblCTContractType					TP	ON	TP.intContractTypeId				=		CH.intContractTypeId
+	JOIN	tblARSalesperson					SP	ON	SP.intEntitySalespersonId			=		CH.intSalespersonId
+	JOIN	tblSMTerm							TM	ON	TM.intTermID						=		CH.intTermId						LEFT
+	JOIN	tblICCommodity						CY	ON	CY.intCommodityId					=		CH.intCommodityId					LEFT
+	JOIN	tblCTAssociation					AN	ON	AN.intAssociationId					=		CH.intAssociationId					LEFT
+	JOIN	tblCTContractText					TX	ON	TX.intContractTextId				=		CH.intContractTextId				LEFT
+	JOIN	tblCTApprovalBasis					AB	ON	AB.intApprovalBasisId				=		CH.intApprovalBasisId				LEFT
+	JOIN	tblCTContractBasis					CB	ON	CB.intContractBasisId				=		CH.intContractBasisId				LEFT
+	JOIN	tblCTPosition						PO	ON	PO.intPositionId					=		CH.intPositionId					LEFT
+	JOIN	tblCTInsuranceBy					IB	ON	IB.intInsuranceById					=		CH.intInsuranceById					LEFT
+	JOIN	tblCTInvoiceType					IT	ON	IT.intInvoiceTypeId					=		CH.intInvoiceTypeId					LEFT
+	JOIN	tblSMCountry						CO	ON	CO.intCountryID						=		CH.intCountryId						LEFT
+	JOIN	tblICCommodityUnitMeasure			CM	ON	CM.intCommodityUnitMeasureId		=		CH.intCommodityUOMId				LEFT
+	JOIN	tblICUnitMeasure					U2	ON	U2.intUnitMeasureId					=		CM.intUnitMeasureId					LEFT
+	JOIN	tblCTWeightGrade					W1	ON	W1.intWeightGradeId					=		CH.intGradeId						LEFT
+	JOIN	tblCTWeightGrade					W2	ON	W2.intWeightGradeId					=		CH.intWeightId						LEFT
+	JOIN	tblSMCity							CT	ON	CT.intCityId						=		CH.intINCOLocationTypeId			LEFT
+	JOIN	tblCTPricingType					PT	ON	PT.intPricingTypeId					=		CH.intPricingTypeId					LEFT
+	JOIN	tblICCommodityUnitMeasure			CL	ON	CL.intCommodityUnitMeasureId		=		CH.intLoadUOMId						LEFT
+	JOIN	tblICUnitMeasure					U3	ON	U3.intUnitMeasureId					=		CL.intUnitMeasureId					LEFT
+	JOIN	tblICUnitMeasure					U4	ON	U4.intUnitMeasureId					=		CH.intCategoryUnitMeasureId			LEFT
+	JOIN	tblICUnitMeasure					U5	ON	U5.intUnitMeasureId					=		CH.intLoadCategoryUnitMeasureId		LEFT
+	JOIN	tblSMCompanyLocationPricingLevel	PL	ON	PL.intCompanyLocationPricingLevelId	=		CH.intCompanyLocationPricingLevelId LEFT
+	JOIN	tblSMCompanyLocationSubLocation		SL	ON	SL.intCompanyLocationSubLocationId	=		CH.intINCOLocationTypeId
--- i21Database/dbo/Views/vyuCTContractSearchView.sql
+++ i21Database/dbo/Views/vyuCTContractSearchView.sql
@@ -2,20 +2,29 @@
 
 AS
 
-	SELECT		CH.intContractHeaderId,
-				CH.dtmContractDate,
-				CH.strEntityName AS strCustomerVendor,
-				CH.strContractType,
-				CH.dblHeaderQuantity,
-				CH.intContractNumber,
-				CH.strCustomerContract,
-				CH.ysnSigned,
-				CH.ysnPrinted,
-				BL.dblBalance,
-				CH.strHeaderUnitMeasure
-			
+	SELECT		CH.intContractHeaderId,				CH.dtmContractDate,				CH.strEntityName		AS strCustomerVendor,
+				CH.strContractType,					CH.dblHeaderQuantity,			CH.intContractNumber,
+				CH.strCustomerContract,				CH.ysnSigned,					CH.ysnPrinted,
+				BL.dblBalance,						CH.strHeaderUnitMeasure,		CH.dtmDeferPayDate,
+				CH.dblDeferPayRate,					CH.strInternalComments,			CH.strContractComments,									
+				CH.dblTolerancePct,
+				CH.dblProvisionalInvoicePct,		CH.ysnPrepaid,					CH.ysnSubstituteItem,
+				CH.ysnUnlimitedQuantity,			CH.ysnMaxPrice,					CH.ysnProvisional,
+				CH.ysnLoad,							CH.intNoOfLoad,					CH.dblQuantityPerLoad,
+				CH.ysnCategory,						CH.ysnMultiplePriceFixation,	CH.strEntityName,
+				CH.strCommodityDescription,			CH.strEntityNumber,				CH.strEntityAddress,
+				CH.strEntityState,					CH.strEntityZipCode,			CH.strGrade,
+				CH.strEntityPhone,					CH.strWeight,					CH.strEntityType,
+				CH.strTextCode,						CH.strAssociationName,			CH.strEntityCity,
+				CH.strTerm,							CH.strContractBasisDescription,	CH.strEntityCountry,
+				CH.strPosition,						CH.strInsuranceByDescription,	CH.strInsuranceBy,
+				CH.strInvoiceTypeDescription,		CH.strInvoiceType,				CH.strCountry,
+				CH.strApprovalBasisDescription,		CH.strCommodityCode,			CH.strINCOLocationType,
+				CH.strApprovalBasis,				CH.strContractBasis,			CH.strPricingType,
+				CH.strPricingLevelName,				CH.strLoadUnitMeasure,			CH.strCategoryUnitMeasure,
+				CH.strLoadCategoryUnitMeasure,		CH.strINCOLocation,				CH.strStatuses
+
 	FROM		vyuCTContractHeaderView		CH	LEFT
-	JOIN		tblICCommodityUnitMeasure	CM	ON	CM.intCommodityUnitMeasureId	= CH.intCommodityUnitMeasureId
 	JOIN
 	(
 		SELECT	HV.intContractHeaderId,SUM([dbo].[fnCTConvertQuantityToTargetItemUOM](CD.intItemId,CD.intUnitMeasureId,UM.intUnitMeasureId,CD.dblBalance)) AS dblBalance
