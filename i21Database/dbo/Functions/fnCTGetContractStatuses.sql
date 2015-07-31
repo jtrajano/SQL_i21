915b6e74a03470203c76b763ff5ec4b87e805787
Satya <ssahoo@BLRSATYAWIN7.irely.net>
2015-07-31 10:24:33 +0530

    CT-264

--- i21Database/dbo/Functions/fnCTGetContractStatuses.sql
+++ i21Database/dbo/Functions/fnCTGetContractStatuses.sql
@@ -7,18 +7,18 @@ AS
 BEGIN 
 	DECLARE	@strStatus AS NVARCHAR(MAX)
 
-	--SELECT	@strStatus	=	STUFF(															
-	--							   (
-	--									SELECT	DISTINCT												
-	--									', ' + CD.strContractStatus											
-	--									FROM vyuCTContractDetailView CD																								
-	--									WHERE CD.intContractHeaderId=CH.intContractHeaderId																			
-	--									FOR XML PATH('')
-	--							   )											
-	--								,1,2, ''													
-	--							)														
-	--FROM tblCTContractHeader CH																
-	--WHERE intContractHeaderId = @intContractHeaderId	
+	SELECT	@strStatus	=	STUFF(															
+								   (
+										SELECT	DISTINCT												
+										', ' + CD.strContractStatus											
+										FROM vyuCTContractDetailView CD																								
+										WHERE CD.intContractHeaderId=CH.intContractHeaderId																			
+										FOR XML PATH('')
+								   )											
+									,1,2, ''													
+								)														
+	FROM tblCTContractHeader CH																
+	WHERE intContractHeaderId = @intContractHeaderId	
 
 	RETURN @strStatus;	
 END
