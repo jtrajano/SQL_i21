b4b189d0a1d62848fd604e8a1474e3acdff4057f
Vinoth Kumar T <vinoth.t@irely.com>
2015-08-04 18:44:18 +0530

    QC-28

---
+++ i21Database/dbo/Views/vyuQMPropertyList.sql
@@ -0,0 +1,17 @@
+﻿CREATE VIEW vyuQMPropertyList
+AS
+SELECT P.intPropertyId
+	,P.strPropertyName
+	,AT.strAnalysisTypeName
+	,P.strDescription
+	,DT.strDataTypeName
+	,L.strListName
+	,P.intDecimalPlaces
+	,P.ysnActive
+	,P.strIsMandatory
+	,dbo.fnQMGetTestNames(P.intPropertyId) AS strTestNames
+FROM tblQMProperty AS P
+JOIN tblQMAnalysisType AS AT ON AT.intAnalysisTypeId = P.intAnalysisTypeId
+JOIN tblQMDataType AS DT ON DT.intDataTypeId = P.intDataTypeId
+LEFT JOIN tblQMList AS L ON L.intListId = P.intListId
+WHERE ISNULL(strFormula, '') = ''
---
+++ i21Database/dbo/Views/vyuQMTestList.sql
@@ -0,0 +1,14 @@
+﻿CREATE VIEW vyuQMTestList
+AS
+SELECT T.intTestId
+	,T.strTestName
+	,T.strDescription
+	,AT.strAnalysisTypeName
+	,T.strTestMethod
+	,T.strIndustryStandards
+	,T.strSensComments
+	,T.intReplications
+	,T.ysnActive
+	,dbo.fnQMGetTemplateNames(T.intTestId) AS strTemplateNames
+FROM tblQMTest AS T
+JOIN tblQMAnalysisType AS AT ON AT.intAnalysisTypeId = T.intAnalysisTypeId
