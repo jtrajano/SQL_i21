4c7cd51f574481ac7762fe2661ea28b6036cdcad
Erick Dela Cruz <erick.delacruz@irely.com>
2015-08-05 16:42:05 +0800

    AP-1454

--- i21Database/Scripts/Post-Deployment/AP/FixPaymentRecordStatus.sql
+++ i21Database/Scripts/Post-Deployment/AP/FixPaymentRecordStatus.sql
@@ -1,5 +1,11 @@
-﻿--Update Payment Info of Payment
-UPDATE A
-	SET A.strPaymentInfo = B.strReferenceNo
-FROM tblAPPayment A
-	INNER JOIN tblCMBankTransaction B ON A.strPaymentRecordNum = B.strTransactionId
+﻿--THIS WILL FIXED THE PAYMENT WITHOUT PAYMENT INFO/CHECK NO
+IF(EXISTS(SELECT 1 FROM tblAPPayment A
+					INNER JOIN tblCMBankTransaction B ON A.strPaymentRecordNum = B.strTransactionId
+					WHERE B.dtmCheckPrinted IS NOT NULL AND ISNULL(A.strPaymentInfo,'') = ''))
+BEGIN
+	UPDATE A
+		SET A.strPaymentInfo = B.strReferenceNo
+	FROM tblAPPayment A
+					INNER JOIN tblCMBankTransaction B ON A.strPaymentRecordNum = B.strTransactionId
+					WHERE B.dtmCheckPrinted IS NOT NULL AND ISNULL(A.strPaymentInfo,'') = ''
+END
