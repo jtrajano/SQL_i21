33d054580f62a814244d3f38bf02e02164fa2ca3
Feb Montefrio <feb.montefrio@irely.com>
2015-08-05 13:56:21 +0800

    IC-1118

--- i21Database/dbo/Tables/tblICItemStockUOM.sql
+++ i21Database/dbo/Tables/tblICItemStockUOM.sql
@@ -41,8 +41,9 @@ It only tracks the qty of those non-'Stock UOM's.
 		[intItemUOMId] INT NOT NULL,
 		[intSubLocationId] INT NULL,
 		[intStorageLocationId] INT NULL,
-		[dblOnHand] NUMERIC(18, 6) NULL DEFAULT ((0)),
+		[dblOnHand] NUMERIC(18, 6) NULL DEFAULT ((0)),		
 		[dblOnOrder] NUMERIC(18, 6) NULL DEFAULT ((0)),
+		[dblOrderCommitted] NUMERIC(18, 6) NULL DEFAULT ((0)),
 		[intConcurrencyId] INT NULL DEFAULT ((1)), 
 		CONSTRAINT [PK_tblICItemStockUOM] PRIMARY KEY ([intItemStockUOMId]), 
 		CONSTRAINT [FK_tblICItemStockUOM_tblICItemUOM] FOREIGN KEY ([intItemUOMId]) REFERENCES [tblICItemUOM]([intItemUOMId])
