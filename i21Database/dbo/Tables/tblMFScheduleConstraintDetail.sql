bc914b4b246808cfc7ddd7a82cd902dca9b41e8a
Premkumar S <prem.selladurai@irely.com>
2015-08-05 21:48:19 +0530

    MFG-462

--- i21Database/dbo/Tables/tblMFScheduleConstraintDetail.sql
+++ i21Database/dbo/Tables/tblMFScheduleConstraintDetail.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE tblMFScheduleConstraintDetail (
 	intScheduleConstraintDetailId INT NOT NULL identity(1, 1)
 	,intScheduleWorkOrderId INT NOT NULL
+	,intWorkOrderId int
 	,intScheduleRuleId INT NOT NULL
 	,dtmChangeoverStartDate DATETIME NOT NULL
 	,dtmChangeoverEndDate DATETIME NOT NULL
@@ -8,4 +9,5 @@
 	,CONSTRAINT PK_tblMFScheduleConstraintDetail_intScheduleConstraintDetailId PRIMARY KEY (intScheduleConstraintDetailId)
 	,CONSTRAINT [FK_tblMFScheduleConstraintDetail_tblMFScheduleLine_intScheduleLineId] FOREIGN KEY (intScheduleWorkOrderId) REFERENCES tblMFScheduleWorkOrder(intScheduleWorkOrderId)ON DELETE CASCADE
 	,CONSTRAINT [FK_tblMFScheduleConstraintDetail_tblMFScheduleRule_intScheduleRuleId] FOREIGN KEY (intScheduleRuleId) REFERENCES tblMFScheduleRule(intScheduleRuleId)
+	,CONSTRAINT [FK_tblMFScheduleConstraintDetail_tblMFWorkOrder_intWorkOrderId] FOREIGN KEY (intWorkOrderId) REFERENCES [dbo].[tblMFWorkOrder](intWorkOrderId)
 	)
--- i21Database/dbo/Tables/tblMFScheduleMachineDetail.sql
+++ i21Database/dbo/Tables/tblMFScheduleMachineDetail.sql
@@ -1,8 +1,13 @@
 ﻿CREATE TABLE tblMFScheduleMachineDetail (
 	intScheduleMachineDetailId INT NOT NULL identity(1, 1)
 	,intScheduleWorkOrderDetailId INT NOT NULL
+	,intWorkOrderId INT NOT NULL
 	,intCalendarMachineId INT NOT NULL
+	,intCalendarDetailId INT NOT NULL
 	,CONSTRAINT PK_tblMFScheduleMachineDetail_intScheduleMachineDetailId PRIMARY KEY (intScheduleMachineDetailId)
 	,CONSTRAINT [FK_tblMFScheduleMachineDetail_tblMFScheduleWorkOrderDetail_intScheduleWorkOrderDetailId] FOREIGN KEY (intScheduleWorkOrderDetailId) REFERENCES tblMFScheduleWorkOrderDetail(intScheduleWorkOrderDetailId) ON DELETE CASCADE
 	,CONSTRAINT [FK_tblMFScheduleMachineDetail_tblMFScheduleCalendarMachineDetail_intCalendarMachineId] FOREIGN KEY (intCalendarMachineId) REFERENCES tblMFScheduleCalendarMachineDetail(intCalendarMachineId)
+	,CONSTRAINT [FK_tblMFScheduleMachinerDetail_tblMFWorkOrder_intWorkOrderId] FOREIGN KEY (intWorkOrderId) REFERENCES [dbo].[tblMFWorkOrder](intWorkOrderId)
+	,CONSTRAINT [FK_tblMFScheduleMachineDetail_tblMFScheduleCalendar_intCalendarDetailId] FOREIGN KEY (intCalendarDetailId) REFERENCES [dbo].[tblMFScheduleCalendarDetail](intCalendarDetailId)
+	
 	)
