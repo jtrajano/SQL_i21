084e7c243320d635cd1b3ac516e97c5f80b5b506
Dan Michael Jalimao <dan.jalimao@irely.com>
2015-08-05 13:02:14 +0800

    FCOM-710

--- i21Database/Scripts/Post-Deployment/SM/SMDataFixes.sql
+++ i21Database/Scripts/Post-Deployment/SM/SMDataFixes.sql
@@ -1,9 +1,22 @@
-﻿IF EXISTS(SELECT * FROM sys.columns WHERE object_id = object_id('tblSMRecurringTransaction') AND name = 'intRecurringId')
-BEGIN
-	EXEC ('
-		UPDATE dbo.tblSMRecurringTransaction
-		SET strResponsibleUser = CASE WHEN LEN(LTRIM(RTRIM(strResponsibleUser))) = 0 THEN strFullName ELSE strResponsibleUser END
-		FROM dbo.tblSMRecurringTransaction
-		INNER JOIN dbo.tblSMUserSecurity ON  dbo.tblSMRecurringTransaction.intUserId = dbo.tblSMUserSecurity.intUserSecurityID
-	')
-END
+﻿GO
+	IF EXISTS(SELECT * FROM sys.columns WHERE object_id = object_id('tblSMRecurringTransaction') AND name = 'intRecurringId')
+	BEGIN
+		EXEC ('
+			UPDATE dbo.tblSMRecurringTransaction
+			SET strResponsibleUser = CASE WHEN LEN(LTRIM(RTRIM(strResponsibleUser))) = 0 THEN strFullName ELSE strResponsibleUser END
+			FROM dbo.tblSMRecurringTransaction
+			INNER JOIN dbo.tblSMUserSecurity ON  dbo.tblSMRecurringTransaction.intUserId = dbo.tblSMUserSecurity.intUserSecurityID
+		')
+	END
+GO
+	/* DELETE i21 Updates MENU'S DUPLICATE */
+	DECLARE @UtilitiesParentMenuId INT
+	SELECT @UtilitiesParentMenuId = intMenuID FROM tblSMMasterMenu WHERE strMenuName = 'Utilities' AND strModuleName = 'System Manager' AND intParentMenuID = 1
+	IF EXISTS(SELECT strMenuName FROM tblSMMasterMenu WHERE strMenuName =  'i21 Updates' AND (SELECT COUNT(strMenuName) FROM tblSMMasterMenu WHERE strMenuName =  'i21 Updates') > 1)
+	BEGIN
+		DELETE FROM tblSMMasterMenu WHERE strMenuName = 'i21 Updates' AND strModuleName = 'Service Pack' AND intParentMenuID = @UtilitiesParentMenuId AND intMenuID NOT IN
+		(
+			SELECT TOP 1 intMenuID FROM tblSMMasterMenu WHERE strMenuName = 'i21 Updates' AND strModuleName = 'Service Pack' AND intParentMenuID = @UtilitiesParentMenuId
+		)
+	END
+GO
