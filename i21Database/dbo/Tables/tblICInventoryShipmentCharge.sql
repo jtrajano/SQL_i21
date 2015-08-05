e95ff659d3b0766b4bb3b457b98b6ebdb880236e
Lawrence Zabala <lawrence.zabala@irely.com>
2015-08-05 09:31:12 +0800

    IC-1108

---
+++ i21Database/dbo/Tables/tblICInventoryShipmentCharge.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[tblICInventoryShipmentCharge]
+(
+	[intInventoryShipmentChargeId] INT NOT NULL IDENTITY, 
+    [intInventoryShipmentId] INT NOT NULL, 
+    [intContractId] INT NULL,
+    [intChargeId] INT NOT NULL, 
+    [strCostMethod] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL DEFAULT ('Per Unit'), 
+    [dblRate] NUMERIC(18, 6) NULL DEFAULT ((0)), 
+    [intCostUOMId] INT NULL, 
+    [dblAmount] NUMERIC(18, 6) NULL DEFAULT ((0)), 
+    [strCostBilledBy] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL DEFAULT ('Third Party'), 
+	[intEntityVendorId] INT NULL, 
+    [intSort] INT NULL, 
+    [intConcurrencyId] INT NULL DEFAULT ((0)), 
+    CONSTRAINT [PK_tblICInventoryShipmentCharge] PRIMARY KEY ([intInventoryShipmentChargeId]), 
+    CONSTRAINT [FK_tblICInventoryShipmentCharge_tblICItem] FOREIGN KEY ([intChargeId]) REFERENCES [tblICItem]([intItemId]), 
+    CONSTRAINT [FK_tblICInventoryShipmentCharge_tblICItemLocation] FOREIGN KEY ([intCostUOMId]) REFERENCES [tblICItemUOM]([intItemUOMId]), 
+    CONSTRAINT [FK_tblICInventoryShipmentCharge_tblAPVendor] FOREIGN KEY ([intEntityVendorId]) REFERENCES [tblAPVendor]([intEntityVendorId]) 
+)
---
+++ i21Database/dbo/Views/vyuICGetInventoryShipmentCharge.sql
@@ -0,0 +1,23 @@
+﻿CREATE VIEW [dbo].[vyuICGetInventoryShipmentCharge]
+	AS 
+
+SELECT ShipmentCharge.intInventoryShipmentChargeId
+	, ShipmentCharge.intInventoryShipmentId
+	, ShipmentCharge.intContractId
+	, Contract.intContractNumber
+	, Charge.strItemNo
+	, strItemDescription = Charge.strDescription
+	, ShipmentCharge.strCostMethod
+	, ShipmentCharge.dblRate
+	, Charge.strCostUOM
+	, Charge.strUnitType
+	, Charge.intOnCostTypeId
+	, strOnCostType = Charge.strOnCostType
+	, ShipmentCharge.intEntityVendorId
+	, Vendor.strVendorId
+	, ShipmentCharge.dblAmount
+	, ShipmentCharge.strCostBilledBy
+FROM tblICInventoryShipmentCharge ShipmentCharge
+	LEFT JOIN vyuICGetOtherCharges Charge ON Charge.intItemId = ShipmentCharge.intChargeId
+	LEFT JOIN tblAPVendor Vendor ON Vendor.intEntityVendorId = ShipmentCharge.intEntityVendorId
+	LEFT JOIN tblCTContractHeader Contract ON Contract.intContractHeaderId = ShipmentCharge.intContractId
