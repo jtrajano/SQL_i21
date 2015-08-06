6a1aebe912fd91db270aa1c988862395b3950e55
Ruben  Escarcha <ruben.escarcha@irely.com>
2015-08-06 14:26:05 +0800

    AR-1498 Sales Order - is not updating Committed stock

---
+++ i21Database/dbo/Stored Procedures/uspSOUpdateOrderIntegrations.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[uspSOUpdateOrderIntegrations] 
+	 @SalesOrderId	INT = NULL
+	,@ForDelete		BIT = 0    
+	,@UserId		INT = NULL     
+AS  
+  
+SET QUOTED_IDENTIFIER OFF  
+SET ANSI_NULLS ON  
+SET NOCOUNT ON  
+SET XACT_ABORT ON  
+SET ANSI_WARNINGS OFF  
+
+
+EXEC dbo.[uspSOUpdateCommitted] @SalesOrderId, @ForDelete
+
+GO
