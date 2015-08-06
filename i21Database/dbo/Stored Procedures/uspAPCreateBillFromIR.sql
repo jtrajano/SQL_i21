0d8665400937d12ac82b938d6a2ce9cf775fee21
Erick Dela Cruz <erick.delacruz@irely.com>
2015-08-06 11:51:23 +0800

    AP-1473

--- i21Database/dbo/Stored Procedures/uspAPCreateBillFromIR.sql
+++ i21Database/dbo/Stored Procedures/uspAPCreateBillFromIR.sql
@@ -167,7 +167,7 @@ BEGIN
 											WHEN A.strReceiptType = 'Purchase Order' THEN POContractItems.intContractDetailId
 											ELSE NULL END,
 		[intContractHeaderId]		=	CASE WHEN A.strReceiptType = 'Purchase Contract' THEN E.intContractHeaderId 
-											WHEN A.strReceiptType = 'Purchase Order' THEN POContractItems.intContractDetailId
+											WHEN A.strReceiptType = 'Purchase Order' THEN POContractItems.intContractHeaderId
 											ELSE NULL END,
 		[intLineNo]					=	ISNULL(B.intSort,0)
 	FROM tblICInventoryReceipt A
