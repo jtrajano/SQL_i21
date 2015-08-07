a96406a4f03423d7258b2a276f4eefca4fea0308
Kelvin Quinto <kelvin.quinto@irely.com>
2015-08-07 12:01:07 +0800

    AR-1527

---
+++ i21Database/dbo/Stored Procedures/uspARValidatePendingSOItems.sql
@@ -0,0 +1,27 @@
+﻿CREATE PROCEDURE [dbo].[uspARValidatePendingSOItems]
+	@salesOrderId INT,
+	@isValid BIT OUTPUT
+AS
+
+BEGIN	
+	DECLARE @salesOrderNo NVARCHAR(25)
+	SET @isValid = 1
+	SELECT @salesOrderNo = strSalesOrderNumber FROM tblSOSalesOrder WHERE intSalesOrderId = @salesOrderId
+	
+	IF EXISTS (SELECT NULL FROM tblICInventoryShipment WHERE strReferenceNumber = @salesOrderNo)
+	BEGIN
+		SET @isValid = 0
+	END
+
+	IF EXISTS (SELECT NULL FROM tblICInventoryShipmentItem WHERE intOrderId = @salesOrderId)
+	BEGIN
+		SET @isValid = 0
+	END	
+
+	IF EXISTS (SELECT NULL FROM tblSOSalesOrderDetail SOD 
+				INNER JOIN tblARInvoiceDetail ID ON SOD.intSalesOrderDetailId = ID.intSalesOrderDetailId
+				WHERE ID.intSalesOrderDetailId IS NOT NULL AND SOD.intSalesOrderId = @salesOrderId)
+	BEGIN
+		SET @isValid = 0
+	END	
+END
