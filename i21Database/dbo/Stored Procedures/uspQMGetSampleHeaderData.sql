68724ff2bdfbcc36db61925d5de9195bd4abe6ef
Vinoth Kumar T <vinoth.t@irely.com>
2015-08-10 19:42:14 +0530

    QC-28

---
+++ i21Database/dbo/Stored Procedures/uspQMGetSampleHeaderData.sql
@@ -0,0 +1,101 @@
+﻿CREATE PROCEDURE [dbo].[uspQMGetSampleHeaderData]
+	@intProductTypeId INT
+	,@intProductValueId INT
+AS
+SET QUOTED_IDENTIFIER OFF
+SET ANSI_NULLS ON
+SET NOCOUNT ON
+SET XACT_ABORT ON
+SET ANSI_WARNINGS OFF
+
+IF @intProductTypeId = 2 -- Item
+BEGIN
+	SELECT @intProductTypeId AS intProductTypeId
+		,@intProductValueId AS intProductValueId
+		,I.intItemId
+		,I.strDescription
+	FROM tblICItem I
+	WHERE I.strStatus = 'Active'
+		AND I.intItemId = @intProductValueId
+END
+ELSE IF @intProductTypeId = 8 -- Contract Line Item
+BEGIN
+	SELECT @intProductTypeId AS intProductTypeId
+		,@intProductValueId AS intProductValueId
+		,C.intContractHeaderId
+		,C.intContractDetailId
+		,C.intItemId
+		,C.strItemDescription AS strDescription
+		,C.dblDetailQuantity AS dblRepresentingQty
+		,C.intUnitMeasureId AS intRepresentingUOMId
+		,C.intCountryId
+		,C.intEntityId
+	FROM vyuCTContractDetailView C
+	WHERE C.intContractDetailId = @intProductValueId
+END
+ELSE IF @intProductTypeId = 9 -- Container Line Item
+BEGIN
+	SELECT @intProductTypeId AS intProductTypeId
+		,@intProductValueId AS intProductValueId
+		,S.intShipmentId
+		,S.intShipmentContractQtyId
+		,S.intShipmentBLContainerId
+		,S.intShipmentBLContainerContractId
+		,C.intContractHeaderId
+		,C.intContractDetailId
+		,C.intItemId
+		,C.strItemDescription AS strDescription
+		,C.dblDetailQuantity AS dblRepresentingQty
+		,C.intUnitMeasureId AS intRepresentingUOMId
+		,C.intCountryId
+		,C.intEntityId
+	FROM vyuLGShipmentContainerReceiptContracts S
+	JOIN vyuCTContractDetailView C ON C.intContractDetailId = S.intContractDetailId
+	WHERE S.intShipmentBLContainerContractId = @intProductValueId
+END
+ELSE IF @intProductTypeId = 10 -- Shipment Line Item
+BEGIN
+	SELECT @intProductTypeId AS intProductTypeId
+		,@intProductValueId AS intProductValueId
+		,S.intShipmentId
+		,S.intShipmentContractQtyId
+		,C.intContractHeaderId
+		,C.intContractDetailId
+		,C.intItemId
+		,C.strItemDescription AS strDescription
+		,C.dblDetailQuantity AS dblRepresentingQty
+		,C.intUnitMeasureId AS intRepresentingUOMId
+		,C.intCountryId
+		,C.intEntityId
+	FROM vyuLGShipmentContainerReceiptContracts S
+	JOIN vyuCTContractDetailView C ON C.intContractDetailId = S.intContractDetailId
+	WHERE S.intShipmentContractQtyId = @intProductValueId
+END
+ELSE IF @intProductTypeId = 6 -- Lot
+BEGIN
+	SELECT @intProductTypeId AS intProductTypeId
+		,@intProductValueId AS intProductValueId
+		,L.intLotStatusId
+		,L.strLotNumber
+		,L.intItemId
+		,I.strDescription
+		,L.dblQty
+		,L.intItemUOMId AS intRepresentingUOMId
+	FROM tblICLot L
+	JOIN tblICItem I ON I.intItemId = L.intItemId
+	WHERE L.intLotId = @intProductValueId
+END
+ELSE IF @intProductTypeId = 11 -- Parent Lot
+BEGIN
+	SELECT @intProductTypeId AS intProductTypeId
+		,@intProductValueId AS intProductValueId
+		,PL.intLotStatusId
+		,PL.strParentLotNumber
+		,PL.intItemId
+		,I.strDescription
+		,PL.dblQty
+		,PL.intItemUOMId AS intRepresentingUOMId
+	FROM tblICParentLot PL
+	JOIN tblICItem I ON I.intItemId = PL.intItemId
+	WHERE PL.intParentLotId = @intProductValueId
+END
