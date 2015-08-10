24c95b7590bf70b517185597376f686f582314a2
Erick Dela Cruz <erick.delacruz@irely.com>
2015-08-10 13:45:59 +0800

    AP-1490

--- i21PreDeployment/Scripts/Pre-Deployment/8_AP/UpdateShipFrom.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/8_AP/UpdateShipFrom.sql
@@ -1,24 +1,27 @@
 ﻿--THIS WILL UPDATE ALL tblAPBill.intShipFrom THAT HAS VALUE "0" to NULL
-IF(EXISTS(SELECT 1 FROM tblAPBill WHERE intShipFromId <= 0))
+IF(EXISTS(SELECT 1 FROM sys.objects WHERE name = 'tblAPBill'))
 BEGIN
-	UPDATE A
-		SET A.intShipFromId = NULL
-	FROM tblAPBill A
-	WHERE intShipFromId <= 0
-
-	--LOCATION HAS BEEN DELETED
-	IF(EXISTS(SELECT 1 FROM tblAPBill WHERE intShipFromId NOT IN (SELECT intEntityLocationId FROM tblEntityLocation)))
+	IF(EXISTS(SELECT 1 FROM tblAPBill WHERE intShipFromId <= 0))
 	BEGIN
 		UPDATE A
-			SET A.intShipFromId = ISNULL(Location.intEntityLocationId, NULL) --SET TO NULL IF NO LOCATION SPECIFIED
+			SET A.intShipFromId = NULL
 		FROM tblAPBill A
-		INNER JOIN tblEntity B ON A.intEntityVendorId = B.intEntityId
-		OUTER APPLY(
-			SELECT 
-				TOP 1 intEntityLocationId
-			FROM tblEntityLocation C 
-			WHERE C.intEntityId = B.intEntityId AND C.ysnDefaultLocation = 1
-		) Location
-		WHERE intShipFromId NOT IN (SELECT intEntityLocationId FROM tblEntityLocation)
+		WHERE intShipFromId <= 0
+
+		--LOCATION HAS BEEN DELETED
+		IF(EXISTS(SELECT 1 FROM tblAPBill WHERE intShipFromId NOT IN (SELECT intEntityLocationId FROM tblEntityLocation)))
+		BEGIN
+			UPDATE A
+				SET A.intShipFromId = ISNULL(Location.intEntityLocationId, NULL) --SET TO NULL IF NO LOCATION SPECIFIED
+			FROM tblAPBill A
+			INNER JOIN tblEntity B ON A.intEntityVendorId = B.intEntityId
+			OUTER APPLY(
+				SELECT 
+					TOP 1 intEntityLocationId
+				FROM tblEntityLocation C 
+				WHERE C.intEntityId = B.intEntityId AND C.ysnDefaultLocation = 1
+			) Location
+			WHERE intShipFromId NOT IN (SELECT intEntityLocationId FROM tblEntityLocation)
+		END
 	END
 END
