cccf22f862c7ba6305211eb1fe542de8213e6661
Lawrence Zabala <lawrence.zabala@irely.com>
2015-08-10 18:54:21 +0800

    IC-1129

---
+++ i21Database/dbo/Views/vyuICGetUnpostedTransactions.sql
@@ -0,0 +1,37 @@
+﻿CREATE VIEW [dbo].[vyuICGetUnpostedTransactions]
+	AS 
+
+SELECT 
+	intTransactionId = intInventoryAdjustmentId
+	, strTransactionId = strAdjustmentNo
+	, strTransactionType = 'Inventory Adjustment'
+	, dtmDate = dtmAdjustmentDate
+FROM tblICInventoryAdjustment
+WHERE ISNULL(ysnPosted, 0) = 0
+
+UNION ALL
+SELECT 
+	intTransactionId = intInventoryReceiptId
+	, strTransactionId = strReceiptNumber
+	, strTransactionType = 'Inventory Receipt'
+	, dtmDate = dtmReceiptDate
+FROM tblICInventoryReceipt
+WHERE ISNULL(ysnPosted, 0) = 0
+
+UNION ALL
+SELECT
+	intTransactionId = intInventoryShipmentId
+	, strTransactionId = strShipmentNumber
+	, strTransactionType = 'Inventory Shipment'
+	, dtmDate = dtmShipDate
+FROM tblICInventoryShipment
+WHERE ISNULL(ysnPosted, 0) = 0
+
+UNION ALL
+SELECT
+	intTransactionId = intInventoryTransferId
+	, strTransactionId = strTransferNo
+	, strTransactionType = 'Inventory Transfer'
+	, dtmDate = dtmTransferDate
+FROM tblICInventoryTransfer
+WHERE ISNULL(ysnPosted, 0) = 0
