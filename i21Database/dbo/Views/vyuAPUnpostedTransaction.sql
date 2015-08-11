9969c19832694744071c062bc593b928b62285a9
Lex Eugene Peregrino <lex.peregrino@irely.com>
2015-08-11 10:40:54 +0800

    AP-1491

---
+++ i21Database/dbo/Views/vyuAPUnpostedTransaction.sql
@@ -0,0 +1,15 @@
+﻿CREATE VIEW [dbo].[vyuAPUnpostedTransaction]
+WITH SCHEMABINDING
+	AS 
+
+SELECT	DISTINCT
+		APB.strBillId AS strTransactionId, 
+		CASE  WHEN APB.intTransactionType = 1	THEN 'Bill' 
+			  WHEN APB.intTransactionType = 2	THEN 'Vendor Prepayment' 
+			  WHEN APB.intTransactionType = 3	THEN 'Debit Memo' 
+			  WHEN APB.intTransactionType = 6	THEN 'Bill Template' 
+			  ELSE 'Not Bill Type'
+		END AS strTransactionType,
+		APB.dtmDate 
+FROM dbo.tblAPBill APB
+WHERE ISNULL(ysnPosted, 0) = 0
