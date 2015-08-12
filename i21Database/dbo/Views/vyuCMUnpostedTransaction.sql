aabfbecc9107f57d4725703cf1bccee5d0bb200e
Smith Wilson de Jesus <smith.dejesus@irely.com>
2015-08-13 00:34:25 +0800

    CM-775 CM: Create sql view of unposted transactions

---
+++ i21Database/dbo/Views/vyuCMUnpostedTransaction.sql
@@ -0,0 +1,10 @@
+﻿CREATE VIEW [dbo].[vyuCMUnpostedTransaction]
+AS 
+
+SELECT 
+intTransactionId
+,strTransactionId
+,strTransactionType = (SELECT strBankTransactionTypeName FROM tblCMBankTransactionType WHERE intBankTransactionTypeId = BT.intBankTransactionTypeId)
+,dtmDate
+FROM tblCMBankTransaction BT
+WHERE ISNULL(ysnPosted,0) = 0
