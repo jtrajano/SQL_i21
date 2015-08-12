8251ecbd4fc52c118ed96475f0001ecb83b55264
Smith Wilson de Jesus <smith.dejesus@irely.com>
2015-08-12 13:55:43 +0800

    CM-704 The new name 'apcbkmst_origin' is already in use as a object name and would cause a duplicate that is not permitted.

--- i21PreDeployment/Scripts/Pre-Deployment/2_CM/1_CM.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/2_CM/1_CM.sql
@@ -1,5 +1,11 @@
 ﻿IF EXISTS(select 1  from INFORMATION_SCHEMA.TABLES where TABLE_NAME = N'apcbkmst' and TABLE_TYPE = N'BASE TABLE')
 BEGIN
+	--Drop apcbkmst_origin if exist in preparation for sp_rename
+	IF EXISTS(select 1  from INFORMATION_SCHEMA.TABLES where TABLE_NAME = N'apcbkmst_origin' and TABLE_TYPE = N'BASE TABLE')
+	BEGIN
+		DROP TABLE apcbkmst_origin
+	END
+
 	EXEC sp_rename 'apcbkmst', 'apcbkmst_origin'
 END
 
@@ -7,6 +13,12 @@ GO
 
 IF EXISTS(select 1  from INFORMATION_SCHEMA.TABLES where TABLE_NAME = N'apchkmst' and TABLE_TYPE = N'BASE TABLE')
 BEGIN
+	--Drop apchkmst_origin if exist in preparation for sp_rename
+	IF EXISTS(select 1  from INFORMATION_SCHEMA.TABLES where TABLE_NAME = N'apchkmst_origin' and TABLE_TYPE = N'BASE TABLE')
+	BEGIN
+		DROP TABLE apchkmst_origin
+	END
+
 	EXEC sp_rename 'apchkmst', 'apchkmst_origin'
 END
 GO
