c095d1398d1e946665c452d4cb6b6b1678ef4791
jeffrey trajano <jeffrey.trajano@irely.com>
2015-08-14 09:41:53 +0800

    GL-72 Chart of accounts Range (Limit account types vs account numbers)

--- i21Database/dbo/Stored Procedures/uspGLGenerateAccountRange.sql
+++ i21Database/dbo/Stored Procedures/uspGLGenerateAccountRange.sql
@@ -14,8 +14,8 @@ AS
 	SELECT 3,'Equity', CAST( '3' + REPLICATE('0', @intLength) AS int),CAST( '3' + REPLICATE('9', @intLength) AS INT) UNION
 	SELECT 4,'Revenue' , CAST( '4' + REPLICATE('0', @intLength) AS int),CAST( '4' + REPLICATE('9', @intLength) AS INT) UNION
 	SELECT 5,'Expense', CAST( '5' + REPLICATE('0', @intLength) AS int),CAST( '5' + REPLICATE('9', @intLength) AS INT) UNION
-	SELECT 6,'Sales', CAST( '6' + REPLICATE('0', @intLength) AS int),CAST( '5' + REPLICATE('9', @intLength) AS INT) UNION
-	SELECT 7,'Cost of Goods Sold', CAST( '7' + REPLICATE('0', @intLength) AS int),CAST( '5' + REPLICATE('9', @intLength) AS INT)
+	SELECT 6,'Sales', CAST( '6' + REPLICATE('0', @intLength) AS int),CAST( '6' + REPLICATE('9', @intLength) AS INT) UNION
+	SELECT 7,'Cost of Goods Sold', CAST( '7' + REPLICATE('0', @intLength) AS int),CAST( '7' + REPLICATE('9', @intLength) AS INT)
 	SET IDENTITY_INSERT tblGLAccountRange OFF
 	UPDATE A SET intAccountRangeId = B.intAccountRangeId
 	FROM  tblGLAccountGroup A INNER JOIN tblGLAccountRange B ON A.strAccountType = B.strAccountType
