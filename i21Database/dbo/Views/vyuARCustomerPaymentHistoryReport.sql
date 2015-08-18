6d812809e1c5303ab8c4eeae04f7e1296aed2492
Kelvin Quinto <kelvin.quinto@irely.com>
2015-08-19 01:01:46 +0800

    AR-1555

--- i21Database/dbo/Views/vyuARCustomerPaymentHistoryReport.sql
+++ i21Database/dbo/Views/vyuARCustomerPaymentHistoryReport.sql
@@ -10,7 +10,7 @@ SELECT DISTINCT
 	 , dblAmountPaid	= ISNULL(P.dblAmountPaid, 0)
 	 , dblAmountApplied = ISNULL(PD.dblPayment, 0)
 	 , dblInvoiceTotal	= ISNULL(PD.dblInvoiceTotal, 0)
-	 , dblAmountDue		= ISNULL(PD.dblAmountDue, 0)
+	 , dblAmountDue		= CASE WHEN I.strTransactionType <> 'Invoice' THEN ISNULL(PD.dblAmountDue, 0) * -1 ELSE ISNULL(PD.dblAmountDue, 0) END
 	 , ysnPaid			= CASE WHEN I.ysnPaid = 1 THEN 'Yes' ELSE 'No' END
 	 , P.intPaymentId	 
 	 , I.intEntityCustomerId
