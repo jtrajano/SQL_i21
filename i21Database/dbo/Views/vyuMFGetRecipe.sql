2d5d1ed34b986c35c81dfb98376a578ca66cc47c
Chakra <chakradhar.behera@irely.com>
2015-08-24 21:58:07 +0530

    MFG-394 & Filter issue in Blend Management Search

---
+++ i21Database/dbo/Views/vyuMFGetRecipe.sql
@@ -0,0 +1,12 @@
+﻿CREATE VIEW [dbo].[vyuMFGetRecipe]
+AS
+Select r.intRecipeId,i.strItemNo,i.strDescription,
+r.dblQuantity,um.strUnitMeasure AS strUOM,r.intVersionNo,r.ysnActive,
+cl.strLocationName,mc.strCellName,mp.strProcessName,cs.strCustomerNumber AS strCustomer
+from tblMFRecipe r
+Join tblICItem i on r.intItemId=i.intItemId 
+Join tblICUnitMeasure um on r.intUOMId=um.intUnitMeasureId
+Join tblSMCompanyLocation cl on r.intLocationId=cl.intCompanyLocationId
+Join tblMFManufacturingCell mc on r.intManufacturingCellId=mc.intManufacturingCellId
+Join tblMFManufacturingProcess mp on r.intManufacturingProcessId=mp.intManufacturingProcessId 
+Left Join tblARCustomer cs on r.intCustomerId=cs.intEntityCustomerId
