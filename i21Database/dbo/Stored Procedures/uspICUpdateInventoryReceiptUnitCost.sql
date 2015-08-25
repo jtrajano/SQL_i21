772a38f993e3bb8f8f4cb927832671521dbeb679
Lawrence Zabala <lawrence.zabala@irely.com>
2015-08-26 05:11:03 +0800

    IC-1223

---
+++ i21Database/dbo/Stored Procedures/uspICUpdateInventoryReceiptUnitCost.sql
@@ -0,0 +1,38 @@
+﻿CREATE PROCEDURE [dbo].[uspICUpdateInventoryReceiptUnitCost]
+	@intContractId AS INT,
+	@newUnitCost AS NUMERIC(18,6)
+AS
+BEGIN
+
+	SELECT Receipt.intInventoryReceiptId, ReceiptItem.intInventoryReceiptItemId, dblUnitCost = @newUnitCost, dblUnitRetail = @newUnitCost, dblLineTotal = (@newUnitCost * dblOpenReceive) + dblTax
+	INTO #tmpContractCost
+	FROM tblICInventoryReceiptItem ReceiptItem
+	LEFT JOIN tblICInventoryReceipt Receipt ON Receipt.intInventoryReceiptId = ReceiptItem.intInventoryReceiptId
+	WHERE ReceiptItem.intOrderId = @intContractId
+		AND Receipt.ysnPosted = 0
+		AND Receipt.strReceiptType = 'Purchase Contract'
+
+	UPDATE tblICInventoryReceiptItem
+	SET tblICInventoryReceiptItem.dblUnitCost = ContractCost.dblUnitCost,
+		tblICInventoryReceiptItem.dblUnitRetail = ContractCost.dblUnitRetail,
+		tblICInventoryReceiptItem.dblLineTotal = ContractCost.dblLineTotal
+	FROM #tmpContractCost ContractCost 
+	WHERE tblICInventoryReceiptItem.intInventoryReceiptItemId = ContractCost.intInventoryReceiptItemId
+
+	SELECT DISTINCT intInventoryReceiptId
+	INTO #ChangedReceipts
+	FROM #tmpContractCost
+
+	DECLARE @intInventoryReceiptId INT
+	
+	WHILE EXISTS(SELECT TOP 1 1 FROM #ChangedReceipts)
+	BEGIN
+		SELECT TOP 1 @intInventoryReceiptId = intInventoryReceiptId FROM #ChangedReceipts
+
+		EXEC uspICCalculateOtherCharges @intInventoryReceiptId
+
+		DELETE FROM #ChangedReceipts WHERE intInventoryReceiptId = @intInventoryReceiptId
+	END
+	DROP TABLE #ChangedReceipts
+	DROP TABLE #tmpContractCost
+END
