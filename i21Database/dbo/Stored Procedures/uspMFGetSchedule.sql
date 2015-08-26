8643b853f3d1a76a1f4dda0067d2095fec6f3d14
Premkumar S <prem.selladurai@irely.com>
2015-08-26 13:16:30 +0530

    MFG-581

--- i21Database/dbo/Stored Procedures/uspMFGetSchedule.sql
+++ i21Database/dbo/Stored Procedures/uspMFGetSchedule.sql
@@ -97,6 +97,7 @@ SELECT C.intManufacturingCellId
 	,SL.intCreatedUserId
 	,SL.dtmLastModified
 	,SL.intLastModifiedUserId
+	,WS.intSequenceNo 
 FROM dbo.tblMFWorkOrder W
 JOIN dbo.tblMFWorkOrderStatus WS ON WS.intStatusId = W.intStatusId
 	AND W.intStatusId <> 13
