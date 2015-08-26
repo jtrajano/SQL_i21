80d55c4c106334e915eb597b51af9154cef26838
Satya <ssahoo@BLRSATYAWIN7.irely.net>
2015-08-26 17:26:08 +0530

    CT-166

---
+++ i21Database/dbo/Tables/tblCTSpreadArbitrage.sql
@@ -0,0 +1,25 @@
+﻿CREATE TABLE [dbo].[tblCTSpreadArbitrage]
+(
+	[intSpreadArbitrageId] INT IDENTITY NOT NULL,
+	[intConcurrencyId] INT NOT NULL,
+	[intPriceFixationId] INT NOT NULL,
+	[dtmSpreadArbitrageDate] DATETIME NOT NULL,
+	[intTypeRef] INT NOT NULL,
+	[strTradeType] NVARCHAR(20) COLLATE Latin1_General_CI_AS NOT NULL ,
+	[strOrder] NVARCHAR(20) COLLATE Latin1_General_CI_AS NOT NULL ,
+	[intNewFutureMarketId] INT,
+	[intNewFutureMonthId] INT,
+	[strBuySell] NVARCHAR(20) COLLATE Latin1_General_CI_AS NOT NULL ,
+	[dblAmount] NUMERIC(18,6) NOT NULL,
+	[intNoOfLots] INT NOT NULL,
+	[dblCommission] NUMERIC(18,6) NOT NULL,
+	[strRemarks] NVARCHAR(20) COLLATE Latin1_General_CI_AS NOT NULL ,
+	[dblCurrentBasis] NUMERIC(18,6) NOT NULL,
+	[dblExchangeBasedSpreadArbitrage] NUMERIC(18,6) NOT NULL,
+	
+	CONSTRAINT [PK_tblCTSpreadArbitrage_intSpreadArbitrageId] PRIMARY KEY CLUSTERED ([intSpreadArbitrageId] ASC),
+	CONSTRAINT [FK_tblCTSpreadArbitrage_tblCTPriceFixation_intPriceFixationId] FOREIGN KEY ([intPriceFixationId]) REFERENCES [tblCTPriceFixation]([intPriceFixationId]) ON DELETE CASCADE,
+		
+	CONSTRAINT [FK_tblCTSpreadArbitrage_tblRKFutureMarket_intNewFutureMarketId_intFutureMarketId] FOREIGN KEY ([intNewFutureMarketId]) REFERENCES [tblRKFutureMarket]([intFutureMarketId]),
+	CONSTRAINT [FK_tblCTSpreadArbitrage_tblRKFuturesMonth_intNewFutureMonthId_intFutureMonthId] FOREIGN KEY ([intNewFutureMonthId]) REFERENCES [tblRKFuturesMonth]([intFutureMonthId])
+)
