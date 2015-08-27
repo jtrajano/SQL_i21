dfa01851dd80aa294c20c7b5407e63a448bf944d
Satya <ssahoo@BLRSATYAWIN7.irely.net>
2015-08-27 20:49:53 +0530

    CT-166

--- i21Database/dbo/Tables/tblCTSpreadArbitrage.sql
+++ i21Database/dbo/Tables/tblCTSpreadArbitrage.sql
@@ -8,18 +8,22 @@
 	[strTradeType] NVARCHAR(20) COLLATE Latin1_General_CI_AS NOT NULL ,
 	[strOrder] NVARCHAR(20) COLLATE Latin1_General_CI_AS NOT NULL ,
 	[intNewFutureMarketId] INT,
-	[intNewFutureMonthId] INT,
+	[intNewFutureMonthId] INT NOT NULL,
+	[intOldFutureMarketId] INT,
+	[intOldFutureMonthId] INT,
 	[strBuySell] NVARCHAR(20) COLLATE Latin1_General_CI_AS NOT NULL ,
-	[dblAmount] NUMERIC(18,6) NOT NULL,
+	[dblSpreadAmount] NUMERIC(18,6) NOT NULL,
 	[intNoOfLots] INT NOT NULL,
-	[dblCommission] NUMERIC(18,6) NOT NULL,
-	[strRemarks] NVARCHAR(20) COLLATE Latin1_General_CI_AS NOT NULL ,
+	[dblCommission] NUMERIC(18,6) NULL,
+	[strRemarks] NVARCHAR(20) COLLATE Latin1_General_CI_AS NULL ,
 	[dblCurrentBasis] NUMERIC(18,6) NOT NULL,
-	[dblExchangeBasedSpreadArbitrage] NUMERIC(18,6) NOT NULL,
+	[dblExchangeBasedSpreadArbitrage] NUMERIC(18,6) NULL,
 	
 	CONSTRAINT [PK_tblCTSpreadArbitrage_intSpreadArbitrageId] PRIMARY KEY CLUSTERED ([intSpreadArbitrageId] ASC),
 	CONSTRAINT [FK_tblCTSpreadArbitrage_tblCTPriceFixation_intPriceFixationId] FOREIGN KEY ([intPriceFixationId]) REFERENCES [tblCTPriceFixation]([intPriceFixationId]) ON DELETE CASCADE,
 		
 	CONSTRAINT [FK_tblCTSpreadArbitrage_tblRKFutureMarket_intNewFutureMarketId_intFutureMarketId] FOREIGN KEY ([intNewFutureMarketId]) REFERENCES [tblRKFutureMarket]([intFutureMarketId]),
-	CONSTRAINT [FK_tblCTSpreadArbitrage_tblRKFuturesMonth_intNewFutureMonthId_intFutureMonthId] FOREIGN KEY ([intNewFutureMonthId]) REFERENCES [tblRKFuturesMonth]([intFutureMonthId])
+	CONSTRAINT [FK_tblCTSpreadArbitrage_tblRKFuturesMonth_intNewFutureMonthId_intFutureMonthId] FOREIGN KEY ([intNewFutureMonthId]) REFERENCES [tblRKFuturesMonth]([intFutureMonthId]),
+	CONSTRAINT [FK_tblCTSpreadArbitrage_tblRKFutureMarket_intOldFutureMarketId_intFutureMarketId] FOREIGN KEY ([intOldFutureMarketId]) REFERENCES [tblRKFutureMarket]([intFutureMarketId]),
+	CONSTRAINT [FK_tblCTSpreadArbitrage_tblRKFuturesMonth_intOldFutureMonthId_intFutureMonthId] FOREIGN KEY ([intOldFutureMonthId]) REFERENCES [tblRKFuturesMonth]([intFutureMonthId])
 )
