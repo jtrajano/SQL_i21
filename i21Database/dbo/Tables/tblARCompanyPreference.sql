c21829d4520145fcae9a1b6a99d65a1652dc142f
Ruben  Escarcha <ruben.escarcha@irely.com>
2015-08-28 23:13:29 +0800

    AR-1571 Receive Payment Activate Write Off Account

--- i21Database/dbo/Tables/tblARCompanyPreference.sql
+++ i21Database/dbo/Tables/tblARCompanyPreference.sql
@@ -3,6 +3,7 @@
 	[intCompanyPreferenceId]		INT NOT NULL PRIMARY KEY IDENTITY, 
     [intARAccountId]				INT NULL, 
     [intDiscountAccountId]			INT NULL,
+	[intWriteOffAccountId]			INT NULL,
 	[intServiceChargeAccountId]		INT NULL,
 	[strServiceChargeCalculation]	NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL,
 	[strServiceChargeFrequency]		NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL,
