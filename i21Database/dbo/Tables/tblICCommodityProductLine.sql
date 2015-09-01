d2ee8c019af55c9329f8887a13a0238f6fa6cd2f
Lawrence Zabala <lawrence.zabala@irely.com>
2015-09-02 03:57:34 +0800

    IC-1232

---
+++ i21Database/dbo/Tables/tblICCommodityProductLine.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[tblICCommodityProductLine]
+(
+	[intCommodityProductLineId] INT NOT NULL IDENTITY, 
+    [intCommodityId] INT NOT NULL, 
+    [strDescription] NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL, 
+    [ysnDeltaHedge] BIT NULL DEFAULT ((0)), 
+    [dblDeltaPercent] NUMERIC(18, 6) NULL DEFAULT ((0)), 
+    [intSort] INT NULL, 
+    [intConcurrencyId] INT NULL DEFAULT ((0)), 
+    CONSTRAINT [PK_tblICCommodityProductLine] PRIMARY KEY ([intCommodityProductLineId]), 
+    CONSTRAINT [FK_tblICCommodityProductLine_tblICCommodity] FOREIGN KEY ([intCommodityId]) REFERENCES [tblICCommodity]([intCommodityId])
+)
