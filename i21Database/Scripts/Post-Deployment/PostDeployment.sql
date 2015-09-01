93c0fc92d341596835df4f66a328a1180085f159
Lawrence Zabala <lawrence.zabala@irely.com>
2015-09-02 03:57:34 +0800

    IC-1232

--- i21Database/Scripts/Post-Deployment/PostDeployment.sql
+++ i21Database/Scripts/Post-Deployment/PostDeployment.sql
@@ -181,6 +181,7 @@ print 'BEGIN POST DEPLOYMENT'
 :r .\IC\10_RemoveCommodityItems.sql
 :r .\IC\11_RemoveOtherChargesAsset.sql
 :r .\IC\12_UpdateExistingInventoryTransactionForm.sql
+:r .\IC\13_MoveCommodityAttributes.sql
 :r .\IC\PatchFor_1510_to_1520.sql
 :r .\GL\InventoryCategoryFix.sql
 
