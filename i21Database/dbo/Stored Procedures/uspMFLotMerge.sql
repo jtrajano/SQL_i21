e377d568cca8ca007db7b010a100e562fc0b011a
Swetosuvro <swetosuvro.g@irely.com>
2015-09-01 18:35:58 +0530

    MF-543, MF-542

---
+++ i21Database/dbo/Stored Procedures/uspMFLotMerge.sql
@@ -0,0 +1,89 @@
+﻿CREATE PROCEDURE [uspMFLotMerge] 
+ @intLotId INT,     
+ @intNewLotId INT,  
+ @dblMergeQty NUMERIC(16,8),
+ @intUserId INT
+
+AS
+
+BEGIN TRY
+
+	DECLARE @intItemId INT
+	DECLARE @dtmDate DATETIME
+	DECLARE @intLocationId INT
+	DECLARE @intSubLocationId INT 
+	DECLARE @intStorageLocationId INT
+	DECLARE @strLotNumber NVARCHAR(50)
+	DECLARE @intSourceId INT
+	DECLARE @intSourceTransactionTypeId INT
+	DECLARE @intLotStatusId INT
+		
+	DECLARE @intInventoryAdjustmentId INT
+	DECLARE @TransactionCount INT
+	DECLARE @ErrMsg NVARCHAR(MAX)
+	
+	DECLARE @intNewLocationId INT
+	DECLARE @intNewSubLocationId INT
+	DECLARE @intNewStorageLocationId INT	
+	DECLARE @intNewItemUOMId INT
+	DECLARE @strNewLotNumber NVARCHAR(100)
+	DECLARE @dblAdjustByQuantity NUMERIC(16,8)
+	
+	SELECT @intItemId = intItemId, 
+		   @intLocationId = intLocationId,
+		   @intSubLocationId = intSubLocationId,
+		   @intStorageLocationId = intStorageLocationId, 
+		   @strLotNumber = strLotNumber,
+		   @intLotStatusId = intLotStatusId,
+		   @intNewLocationId = intLocationId
+	FROM tblICLot WHERE intLotId = @intLotId
+	
+	SELECT @dblAdjustByQuantity = - @dblMergeQty
+	
+	SELECT @intNewLocationId = intLocationId ,
+		   @intNewSubLocationId = intSubLocationId ,	
+		   @intNewStorageLocationId = intStorageLocationId,
+		   @intNewItemUOMId = intItemUOMId,
+		   @strNewLotNumber = strLotNumber
+	FROM tblICLot WHERE intLotId = @intNewLotId
+		   
+	SELECT @dtmDate = GETDATE()
+	
+	SELECT @intSourceId = 1,@intSourceTransactionTypeId= 8
+	
+	IF ISNULL(@strLotNumber,'') = ''
+	BEGIN
+		RAISERROR(51178,11,1)
+	END
+													 
+	EXEC uspICInventoryAdjustment_CreatePostLotMerge @intItemId	= @intItemId,
+													 @dtmDate =	@dtmDate,
+													 @intLocationId	= @intLocationId,
+													 @intSubLocationId = @intSubLocationId,
+													 @intStorageLocationId = @intStorageLocationId,
+													 @strLotNumber = @strLotNumber,
+													 @intNewLocationId = @intNewLocationId,
+													 @intNewSubLocationId = @intNewSubLocationId,
+													 @intNewStorageLocationId = @intNewStorageLocationId,
+													 @strNewLotNumber = @strNewLotNumber,
+													 @dblAdjustByQuantity = @dblAdjustByQuantity,
+													 @dblNewSplitLotQuantity = NULL,
+													 @dblNewWeight = NULL,
+													 @intNewItemUOMId = @intNewItemUOMId,
+													 @intNewWeightUOMId = NULL,
+													 @dblNewUnitCost = NULL,
+													 @intSourceId = @intSourceId,
+													 @intSourceTransactionTypeId = @intSourceTransactionTypeId,
+													 @intUserId = @intUserId,
+													 @intInventoryAdjustmentId = @intInventoryAdjustmentId OUTPUT
+
+
+END TRY  
+  
+BEGIN CATCH  
+  
+ IF XACT_STATE() != 0 AND @TransactionCount = 0 AND @@TRANCOUNT > 0 ROLLBACK TRANSACTION  
+ SET @ErrMsg = ERROR_MESSAGE()      
+ RAISERROR(@ErrMsg, 16, 1, 'WITH NOWAIT')     
+  
+END CATCH 
---
+++ i21Database/dbo/Stored Procedures/uspMFLotMove.sql
@@ -0,0 +1,77 @@
+﻿CREATE PROCEDURE [uspMFLotMove]
+				 @intLotId INT,       
+				 @intNewSubLocationId INT,  
+				 @intNewStorageLocationId INT,  
+				 @dblMoveQty NUMERIC(16,8),
+				 @intUserId INT
+
+AS
+
+BEGIN TRY
+
+	DECLARE @intItemId INT
+	DECLARE @dtmDate DATETIME
+	DECLARE @intLocationId INT
+	DECLARE @intSubLocationId INT 
+	DECLARE @intStorageLocationId INT
+	DECLARE @strLotNumber NVARCHAR(50)
+	DECLARE @intSourceId INT
+	DECLARE @intSourceTransactionTypeId INT
+	DECLARE @intLotStatusId INT
+	DECLARE @intNewLocationId INT
+	DECLARE @strNewLotNumber NVARCHAR(50)
+	
+	DECLARE @intInventoryAdjustmentId INT
+	DECLARE @TransactionCount INT
+	DECLARE @ErrMsg NVARCHAR(MAX)
+	
+	
+	SELECT @intItemId = intItemId, 
+		   @intLocationId = intLocationId,
+		   @intSubLocationId = intSubLocationId,
+		   @intStorageLocationId = intStorageLocationId, 
+		   @strLotNumber = strLotNumber,
+		   @intLotStatusId = intLotStatusId,
+		   @intNewLocationId = intLocationId
+	FROM tblICLot WHERE intLotId = @intLotId
+	
+	SELECT @strNewLotNumber=@strLotNumber
+	SELECT @dtmDate = GETDATE()
+	
+	SELECT @intSourceId = 1,@intSourceTransactionTypeId= 8
+	
+	IF ISNULL(@strLotNumber,'') = ''
+	BEGIN
+		RAISERROR(51178,11,1)
+	END
+	
+	IF @intNewStorageLocationId = @intStorageLocationId
+	BEGIN
+		RAISERROR(51182,11,1)
+	END
+
+	EXEC uspICInventoryAdjustment_CreatePostLotMove @intItemId,
+													@dtmDate,
+													@intLocationId,
+													@intSubLocationId,
+													@intStorageLocationId,
+													@strLotNumber,
+													@intNewLocationId,
+													@intNewSubLocationId,
+													@intNewStorageLocationId,
+													@strNewLotNumber,
+													@dblMoveQty,
+													@intSourceId,
+													@intSourceTransactionTypeId,
+													@intUserId,
+													@intInventoryAdjustmentId
+
+END TRY  
+  
+BEGIN CATCH  
+  
+ IF XACT_STATE() != 0 AND @TransactionCount = 0 AND @@TRANCOUNT > 0 ROLLBACK TRANSACTION  
+ SET @ErrMsg = ERROR_MESSAGE()      
+ RAISERROR(@ErrMsg, 16, 1, 'WITH NOWAIT')     
+  
+END CATCH 
