2d31ae888ce024e899d00bc316cdbe644016abf3
unknown <rnkumashi@BLRRAVIWIN7.irely.net>
2015-09-01 17:08:00 +0530

    ST-175

---
+++ i21Database/dbo/Tables/tblSTRadiantItemTypeCode.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[tblSTRadiantItemTypeCode]
+(
+	[intRadiantItemTypeCodeId] INT NOT NULL IDENTITY, 
+    [intRadiantItemTypeCode] INT NOT NULL, 
+    [strDescription] NCHAR(250) COLLATE Latin1_General_CI_AS NULL,
+	CONSTRAINT [PK_tblSTRadiantItemTypeCode] PRIMARY KEY CLUSTERED ([intRadiantItemTypeCodeId] ASC),
+	CONSTRAINT [AK_tblSTRadiantItemTypeCode_intRadiantItemTypeCode] UNIQUE NONCLUSTERED ([intRadiantItemTypeCode] ASC)
+)
