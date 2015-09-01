195c116e7bfa40d645e9e10e42b614c31f0bfcbf
bonn ponce <bonn.ponce@irely.com>
2015-09-01 19:11:47 +0800

    CF-318 Transaction screen - Incorrect format for Price

---
+++ i21Database/dbo/Stored Procedures/uspCFGetItemTaxes.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[uspCFGetItemTaxes]    
+@CFTaxGroupMasterId    INT    
+AS    
+  
+SELECT TC.*,TCR.*  ,TGM.[intTaxGroupMasterId],TGTM.[intTaxGroupId]  ,TGC.[intTaxCodeId]  
+   FROM tblSMTaxCode TC   
+    INNER JOIN tblSMTaxGroupCode TGC ON TC.[intTaxCodeId] = TGC.[intTaxCodeId]   
+    INNER JOIN tblSMTaxGroup TG ON TGC.[intTaxGroupId] = TG.[intTaxGroupId]  
+    INNER JOIN tblSMTaxGroupMasterGroup TGTM ON TG.[intTaxGroupId] = TGTM.[intTaxGroupId]  
+    INNER JOIN tblSMTaxGroupMaster TGM ON TGTM.[intTaxGroupMasterId] = TGM.[intTaxGroupMasterId]  
+    INNER JOIN tblSMTaxCodeRate TCR ON TC.[intTaxCodeId] = TCR.[intTaxCodeId]
+   WHERE   
+    TGM.[intTaxGroupMasterId] = @CFTaxGroupMasterId
