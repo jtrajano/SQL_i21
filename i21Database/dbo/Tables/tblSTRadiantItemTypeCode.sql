e59b2fe12a98b0b58b149bf806700170e2269aa4
unknown <rnkumashi@BLRRAVIWIN7.irely.net>
2015-09-01 19:19:58 +0530

    ST-175

--- i21Database/dbo/Tables/tblSTRadiantItemTypeCode.sql
+++ i21Database/dbo/Tables/tblSTRadiantItemTypeCode.sql
@@ -3,6 +3,7 @@
 	[intRadiantItemTypeCodeId] INT NOT NULL IDENTITY, 
     [intRadiantItemTypeCode] INT NOT NULL, 
     [strDescription] NCHAR(250) COLLATE Latin1_General_CI_AS NULL,
-	CONSTRAINT [PK_tblSTRadiantItemTypeCode] PRIMARY KEY CLUSTERED ([intRadiantItemTypeCodeId] ASC),
+	[intConcurrencyId] INT NOT NULL, 
+    CONSTRAINT [PK_tblSTRadiantItemTypeCode] PRIMARY KEY CLUSTERED ([intRadiantItemTypeCodeId] ASC),
 	CONSTRAINT [AK_tblSTRadiantItemTypeCode_intRadiantItemTypeCode] UNIQUE NONCLUSTERED ([intRadiantItemTypeCode] ASC)
 )
