e71109c9e41c6298817bda26a1753b87ff35abf8
Premkumar S <prem.selladurai@irely.com>
2015-09-01 12:19:41 +0530

    MFG-462

---
+++ i21Database/dbo/Views/vyuMFGetSchedule.sql
@@ -0,0 +1,19 @@
+﻿CREATE VIEW vyuMFGetSchedule
+AS
+SELECT S.intScheduleId
+	,S.strScheduleNo
+	,S.dtmScheduleDate
+	,S.intCalendarId
+	,SC.strName
+	,S.intManufacturingCellId
+	,MC.strCellName
+	,S.ysnStandard
+	,S.intLocationId
+	,S.intConcurrencyId
+	,S.dtmCreated
+	,S.intCreatedUserId
+	,S.dtmLastModified
+	,S.intLastModifiedUserId
+FROM tblMFSchedule S
+JOIN tblMFManufacturingCell MC ON MC.intManufacturingCellId = S.intManufacturingCellId
+JOIN tblMFScheduleCalendar SC ON SC.intCalendarId = S.intCalendarId
