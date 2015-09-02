8b86e64e7729bb9dbba5086b9a5bd3231479781a
Satya <ssahoo@BLRSATYAWIN7.irely.net>
2015-09-02 20:09:34 +0530

    CT-316

---
+++ i21Database/dbo/Stored Procedures/uspCTContractSave.sql
@@ -0,0 +1,44 @@
+﻿CREATE PROCEDURE [dbo].[uspCTContractSave]
+	
+	@intContractHeaderId int
+	
+AS
+
+BEGIN TRY
+	
+	DECLARE @ErrMsg					NVARCHAR(MAX),
+			@intContractDetailId	INT,
+			@dblCashPrice			NUMERIC(6,4),
+			@intPricingTypeId		INT
+	
+	SELECT @intContractDetailId		=	MIN(intContractDetailId) FROM tblCTContractDetail WHERE intContractHeaderId = @intContractHeaderId
+	
+	WHILE ISNULL(@intContractDetailId,0) > 0
+	BEGIN
+		SELECT	@intPricingTypeId	=	NULL,
+				@dblCashPrice		=	NULL
+				
+		SELECT	@intPricingTypeId	=	intPricingTypeId,
+				@dblCashPrice		=	dblCashPrice 
+		FROM	tblCTContractDetail 
+		WHERE	intContractDetailId =	@intContractDetailId 
+		
+		IF 		@intPricingTypeId	IN	(1,6)
+		BEGIN	
+			EXEC uspICUpdateInventoryReceiptUnitCost @intContractDetailId,@dblCashPrice
+		END
+		
+		SELECT @intContractDetailId = MIN(intContractDetailId) FROM tblCTContractDetail WHERE intContractHeaderId = @intContractHeaderId AND intContractDetailId > @intContractDetailId
+	END
+
+	
+
+END TRY
+
+BEGIN CATCH
+
+	SET @ErrMsg = ERROR_MESSAGE()  
+	RAISERROR (@ErrMsg,18,1,'WITH NOWAIT')  
+	
+END CATCH
+GO
