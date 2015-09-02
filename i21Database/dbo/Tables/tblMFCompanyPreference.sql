a850bbe1f90506d8ae318868ae44e7fb40519898
Chakra <chakradhar.behera@irely.com>
2015-09-02 21:23:32 +0530

    Manufacturing Company Preference table added

---
+++ i21Database/dbo/Tables/tblMFCompanyPreference.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[tblMFCompanyPreference]
+(
+	[intCompanyPreferenceId] INT NOT NULL IDENTITY,
+	[ysnEnableParentLot] BIT NOT NULL CONSTRAINT [DF_tblMFCompanyPreference_ysnEnableParentLot] DEFAULT 0,
+	[intConcurrencyId] INT NULL CONSTRAINT [DF_tblMFCompanyPreference_intConcurrencyId] DEFAULT 0,
+	CONSTRAINT [PK_tblMFCompanyPreference_intCompanyPreferenceId] PRIMARY KEY ([intCompanyPreferenceId]),  
+)
