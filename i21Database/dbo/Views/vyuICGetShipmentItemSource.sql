14fae752e69079f6d4bb0c6812951f81d1a9f0ab
Lawrence Zabala <lawrence.zabala@irely.com>
2015-09-04 04:28:18 +0800

    IC-1262

--- i21Database/dbo/Views/vyuICGetShipmentItemSource.sql
+++ i21Database/dbo/Views/vyuICGetShipmentItemSource.sql
@@ -7,7 +7,7 @@ SELECT
 	strOrderNumber = 
 		(
 			CASE WHEN Shipment.intOrderType = 1
-				THEN NULL
+				THEN (SELECT CAST(ISNULL(strContractNumber, 'PO Number not found!') AS NVARCHAR) FROM tblCTContractHeader WHERE intContractHeaderId = ShipmentItem.intOrderId)
 			WHEN Shipment.intOrderType = 2
 				THEN (SELECT ISNULL(strSalesOrderNumber, 'SO Number not found!') FROM tblSOSalesOrder WHERE intSalesOrderId = ShipmentItem.intOrderId)
 			WHEN Shipment.intOrderType = 3
@@ -27,7 +27,19 @@ SELECT
 	strOrderUOM = 
 		(
 			CASE WHEN Shipment.intOrderType = 1
-				THEN NULL
+				THEN (
+					CASE WHEN Shipment.intSourceType = 0 -- None
+						THEN (SELECT ISNULL(strItemUOM, 'Ticket Number not found!') FROM vyuCTContractDetailView
+							WHERE intContractDetailId = ShipmentItem.intLineNo)
+					WHEN Shipment.intSourceType = 1 -- Scale
+						THEN NULL
+					WHEN Shipment.intSourceType = 2 -- Inbound Shipment
+						THEN NULL
+					WHEN Shipment.intSourceType = 3 -- Transport
+						THEN (SELECT ISNULL(strUnitMeasure, 'Transport not found!')  FROM tblICItemUOM LEFT JOIN tblICUnitMeasure ON tblICUnitMeasure.intUnitMeasureId = tblICItemUOM.intUnitMeasureId WHERE intItemUOMId = ShipmentItem.intItemUOMId)
+					ELSE NULL
+					END
+				)
 			WHEN Shipment.intOrderType = 2
 				THEN (SELECT strUnitMeasure FROM tblSOSalesOrderDetail 
 						LEFT JOIN tblICItemUOM ON tblICItemUOM.intItemUOMId = tblSOSalesOrderDetail.intItemUOMId
@@ -41,7 +53,20 @@ SELECT
 	dblQtyOrdered = 
 		(
 			CASE WHEN Shipment.intOrderType = 1
-				THEN 0.00
+				THEN (
+					CASE WHEN Shipment.intSourceType = 0 -- None
+						THEN (SELECT ISNULL(dblDetailQuantity, 0) FROM vyuCTContractDetailView
+							WHERE intContractDetailId = ShipmentItem.intLineNo)
+					WHEN Shipment.intSourceType = 1 -- Scale
+						THEN 0
+					WHEN Shipment.intSourceType = 2 -- Inbound Shipment
+						THEN NULL
+					WHEN Shipment.intSourceType = 3 -- Transport
+						THEN (SELECT ISNULL(dblOrderedQuantity, 0) FROM vyuTRTransportReceipt
+						WHERE intTransportReceiptId = ShipmentItem.intSourceId)
+					ELSE NULL
+					END
+				)
 			WHEN Shipment.intOrderType = 2
 				THEN ISNULL((SELECT ISNULL(dblQtyOrdered, 0.00) FROM tblSOSalesOrderDetail WHERE intSalesOrderId = ShipmentItem.intOrderId AND intSalesOrderDetailId = ShipmentItem.intLineNo), 0.00)
 			WHEN Shipment.intOrderType = 3
