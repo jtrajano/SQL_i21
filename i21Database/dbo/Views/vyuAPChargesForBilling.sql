223a3d8a15b927f3cede656170f8a832a3e94aa8
Feb Montefrio <feb.montefrio@irely.com>
2015-09-03 23:52:30 +0800

    IC-1249

--- i21Database/dbo/Views/vyuAPChargesForBilling.sql
+++ i21Database/dbo/Views/vyuAPChargesForBilling.sql
@@ -15,21 +15,33 @@ SELECT
 	,[dblQuantityToBill]						=	1
 	,[dblQuantityBilled]						=	0
 	,[intLineNo]								=	1
-	,[intInventoryReceiptItemId]				=	A.intInventoryReceiptItemId
-	,[intInventoryReceiptChargeId]				=	A.intInventoryReceiptChargeId
-	,[dblUnitCost]								=	A.dblCalculatedAmount
+	,[intInventoryReceiptItemId]				=	B.intInventoryReceiptItemId
+	,[intInventoryReceiptChargeId]				=	B.intInventoryReceiptChargeId
+	,[dblUnitCost]								=	B.dblCalculatedAmount
 	,[dblTax]									=	0
-	,[intAccountId]								=	F.intAccountId
-	,[strAccountId]								=	F.strAccountId
+	,[intAccountId]								=	
+													CASE	WHEN ISNULL(A.ysnInventoryCost, 0) = 0 THEN 
+																H.intAccountId
+															ELSE 
+																F.intAccountId
+													END 
+	,[strAccountId]								=	
+													CASE	WHEN ISNULL(A.ysnInventoryCost, 0) = 0 THEN 
+																H.strAccountId
+															ELSE 
+																F.strAccountId
+													END 
+
 	,[strName]									=	E2.strName
 	,[strVendorId]								=	E.strVendorId
-FROM tblICInventoryReceiptChargePerItem A
+FROM tblICInventoryReceiptCharge A
 INNER JOIN tblICInventoryReceiptChargePerItem B ON A.intInventoryReceiptChargeId = B.intInventoryReceiptChargeId
 INNER JOIN tblICItem C ON B.intChargeId = C.intItemId
-INNER JOIN tblICInventoryReceiptItem G ON A.intInventoryReceiptItemId = G.intInventoryReceiptItemId
+INNER JOIN tblICInventoryReceiptItem G ON B.intInventoryReceiptItemId = G.intInventoryReceiptItemId
 INNER JOIN tblICItem C1 ON G.intItemId = C1.intItemId
 INNER JOIN tblICInventoryReceipt D ON A.intInventoryReceiptId = D.intInventoryReceiptId
 INNER JOIN (tblAPVendor E INNER JOIN tblEntity E2 ON E.intEntityVendorId = E2.intEntityId) ON (CASE WHEN A.ysnAccrue = 1 THEN ISNULL(A.intEntityVendorId, D.intEntityVendorId) ELSE NULL END) = E.intEntityVendorId
 LEFT JOIN tblGLAccount F ON [dbo].[fnGetItemGLAccount](C1.intItemId, D.intLocationId, 'AP Clearing') = F.intAccountId
+LEFT JOIN tblGLAccount H ON [dbo].[fnGetItemGLAccount](C.intItemId, D.intLocationId, 'Other Charge Expense') = H.intAccountId
 WHERE	A.ysnAccrue = 1 
-		AND A.dblAmountBilled != A.dblCalculatedAmount
+		AND B.dblAmountBilled != B.dblCalculatedAmount
