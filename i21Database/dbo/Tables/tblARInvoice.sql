3091b4f9bab5c479095cd20820a25abc655ea8e4
Ruben  Escarcha <ruben.escarcha@irely.com>
2015-09-05 04:50:02 +0800

    AR-1706 Sales Invoice numbers skipping. Getting used by Prepayment

--- i21Database/dbo/Tables/tblARInvoice.sql
+++ i21Database/dbo/Tables/tblARInvoice.sql
@@ -86,22 +86,31 @@ ON dbo.tblARInvoice
 AFTER INSERT
 AS
 
-DECLARE @inserted TABLE(intInvoiceId INT)
+DECLARE @inserted TABLE(intInvoiceId INT, strTransactionType NVARCHAR(25))
 DECLARE @count INT = 0
 DECLARE @intInvoiceId INT
 DECLARE @InvoiceNumber NVARCHAR(50)
+DECLARE @TransactionType NVARCHAR(25)
 
 INSERT INTO @inserted
-SELECT intInvoiceId FROM INSERTED ORDER BY intInvoiceId
+SELECT intInvoiceId, strTransactionType FROM INSERTED ORDER BY intInvoiceId
 
 WHILE((SELECT TOP 1 1 FROM @inserted) IS NOT NULL)
 BEGIN
+	DECLARE @parameterINT AS INT
+	SET @parameterINT = 19
+	
+	SELECT TOP 1 @intInvoiceId = intInvoiceId, @TransactionType = strTransactionType FROM @inserted
+	
+	IF @TransactionType = 'Prepayment'
+		SET @parameterINT = 64
+		
+	IF @TransactionType = 'Overpayment'
+		SET @parameterINT = 65
 	
 	--EXEC uspARFixStartingNumbers 17
 	--IF(OBJECT_ID('tempdb..#tblTempAPByPassFixStartingNumber') IS NOT NULL) RETURN;
-	EXEC uspSMGetStartingNumber 19, @InvoiceNumber OUT
-
-	SELECT TOP 1 @intInvoiceId = intInvoiceId FROM @inserted
+	EXEC uspSMGetStartingNumber @parameterINT, @InvoiceNumber OUT	
 	
 	IF(@InvoiceNumber IS NOT NULL)
 	BEGIN
