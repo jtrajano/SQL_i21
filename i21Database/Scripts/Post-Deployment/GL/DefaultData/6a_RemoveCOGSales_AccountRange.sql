75acef15d541f34089b12cfe2ef1f8f27832b8f0
jeffrey trajano <jeffrey.trajano@irely.com>
2015-09-07 00:06:30 +0800

    GL-2084 Remove the Sales and Cost of Goods sold account types

--- i21Database/Scripts/Post-Deployment/GL/DefaultData/6a_RemoveCOGSales_AccountRange.sql
+++ i21Database/Scripts/Post-Deployment/GL/DefaultData/6a_RemoveCOGSales_AccountRange.sql
@@ -1,13 +1,16 @@
 ﻿GO
-	PRINT N'Begin removing account range for COGS and Sales'
-	DECLARE @intRangeIdExpense INT
-	DECLARE @intRangeIdRevenue INT
-	SELECT @intRangeIdExpense = intAccountRangeId FROM tblGLAccountRange WHERE strAccountType = 'Expense'
-	SELECT @intRangeIdRevenue = intAccountRangeId FROM tblGLAccountRange WHERE strAccountType = 'Revenue'
-	UPDATE tblGLAccountGroup SET intAccountRangeId = @intRangeIdExpense,strAccountType ='Expense'
-	WHERE strAccountType = 'Cost of Goods Sold'
-	UPDATE tblGLAccountGroup SET intAccountRangeId = @intRangeIdRevenue,strAccountType ='Revenue'
-	WHERE strAccountType = 'Sales'
-	DELETE FROM tblGLAccountRange WHERE strAccountType IN ('Sales','Cost of Goods Sold')
-	PRINT N'Finished removing account range for COGS and Sales'
+	IF EXISTS(SELECT TOP 1 1 FROM tblGLAccountRange WHERE strAccountType IN ('Sales','Cost of Goods Sold'))
+	BEGIN
+		PRINT N'Begin removing account range for COGS and Sales'
+		DECLARE @intRangeIdExpense INT
+		DECLARE @intRangeIdRevenue INT
+		SELECT @intRangeIdExpense = intAccountRangeId FROM tblGLAccountRange WHERE strAccountType = 'Expense'
+		SELECT @intRangeIdRevenue = intAccountRangeId FROM tblGLAccountRange WHERE strAccountType = 'Revenue'
+		UPDATE tblGLAccountGroup SET intAccountRangeId = @intRangeIdExpense,strAccountType ='Expense'
+		WHERE strAccountType = 'Cost of Goods Sold'
+		UPDATE tblGLAccountGroup SET intAccountRangeId = @intRangeIdRevenue,strAccountType ='Revenue'
+		WHERE strAccountType = 'Sales'
+		DELETE FROM tblGLAccountRange WHERE strAccountType IN ('Sales','Cost of Goods Sold')
+		PRINT N'Finished removing account range for COGS and Sales'
+	END
 GO
