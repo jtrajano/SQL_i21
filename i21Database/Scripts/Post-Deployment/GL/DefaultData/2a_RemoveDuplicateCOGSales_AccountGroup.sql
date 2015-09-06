cc3da5ccf33b038b75c0c9cc0be034705b458293
jeffrey trajano <jeffrey.trajano@irely.com>
2015-09-07 00:06:30 +0800

    GL-2084 Remove the Sales and Cost of Goods sold account types

--- i21Database/Scripts/Post-Deployment/GL/DefaultData/2a_RemoveDuplicateCOGSales_AccountGroup.sql
+++ i21Database/Scripts/Post-Deployment/GL/DefaultData/2a_RemoveDuplicateCOGSales_AccountGroup.sql
@@ -1,9 +1,9 @@
 ﻿GO
-	PRINT N'REMOVE DUPLICATE COGS AND SALES ACCOUNT GROUP'
 	DECLARE @intAccountGroupId int
 	DECLARE @intAccountGroupId1 int
 	IF (SELECT COUNT( 1) FROM tblGLAccountGroup WHERE strAccountGroup = 'Cost of Goods Sold') = 2
 	BEGIN
+		PRINT N'BEGIN REMOVING DUPLICATE COGS ACCOUNT GROUP'
 		SELECT @intAccountGroupId = intAccountGroupId FROM tblGLAccountGroup WHERE intParentGroupId = 0 and  strAccountGroup = 'Cost of Goods Sold'
 		SELECT @intAccountGroupId1 = intAccountGroupId FROM tblGLAccountGroup WHERE intParentGroupId > 0 and strAccountGroup = 'Cost of Goods Sold'
 		IF @intAccountGroupId1 IS NOT NULL AND @intAccountGroupId IS NOT NULL
@@ -15,9 +15,11 @@
 			WHERE intAccountGroupId = @intAccountGroupId1 
 			DELETE from tblGLAccountGroup where intAccountGroupId = @intAccountGroupId
 		END
+		PRINT N'FINISHED REMOVING DUPLICATE COGS ACCOUNT GROUP'
 	END
 	IF (SELECT COUNT( 1) FROM tblGLAccountGroup WHERE strAccountGroup = 'Sales') = 2
 	BEGIN
+		PRINT N'BEGIN REMOVING DUPLICATE SALES ACCOUNT GROUP'
 		SELECT @intAccountGroupId = intAccountGroupId FROM tblGLAccountGroup WHERE intParentGroupId = 0 and strAccountGroup = 'Sales'
 		SELECT @intAccountGroupId1 = intAccountGroupId FROM tblGLAccountGroup WHERE intParentGroupId > 0 and strAccountGroup = 'Sales'
 		IF @intAccountGroupId1 IS NOT NULL AND @intAccountGroupId IS NOT NULL
@@ -29,5 +31,6 @@
 			WHERE intAccountGroupId = @intAccountGroupId1 
 			DELETE from tblGLAccountGroup WHERE intAccountGroupId = @intAccountGroupId
 		END
+		PRINT N'FINISHED RMOVING DUPLICATE  SALES ACCOUNT GROUP'
 	END
 GO
