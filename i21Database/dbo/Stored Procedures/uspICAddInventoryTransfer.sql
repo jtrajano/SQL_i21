5a5eec00702293fafe05d063ea102817b57a0c6f
Feb Montefrio <feb.montefrio@irely.com>
2015-09-08 04:06:11 +0800

    IC-1254 Cherry pick to 15.3 Prod

---
+++ i21Database/dbo/Stored Procedures/uspICAddInventoryTransfer.sql
@@ -0,0 +1,283 @@
+﻿CREATE PROCEDURE [dbo].[uspICAddInventoryTransfer]
+	@TransferEntries InventoryTransferStagingTable READONLY
+	,@intUserId AS INT	
+AS
+
+SET QUOTED_IDENTIFIER OFF
+SET ANSI_NULLS ON
+SET NOCOUNT ON
+SET XACT_ABORT ON
+SET ANSI_WARNINGS OFF
+
+DECLARE @StartingNumberId_InventoryTransfer AS INT = 41;
+DECLARE @InventoryTransferNumber AS NVARCHAR(50);
+DECLARE @InventoryTransferId AS INT;
+
+-- Create the temp table if it does not exists. 
+IF NOT EXISTS (SELECT 1 FROM tempdb..sysobjects WHERE id = OBJECT_ID('tempdb..#tmpAddInventoryTransferResult')) 
+BEGIN 
+	CREATE TABLE #tmpAddInventoryTransferResult (
+		intSourceId INT
+		,intInventoryTransferId INT
+	)
+END 
+
+DECLARE @DataForInventoryTransferHeader TABLE(
+	intId INT IDENTITY PRIMARY KEY CLUSTERED
+	,TransferType NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL
+	,SourceType INT
+	,FromLocationId INT
+	,ToLocationId INT
+	,StatusId INT
+	,ShipViaId INT 
+)
+
+-- Sort the data from @TransferEntries and determine which ones are the header records. 
+INSERT INTO @DataForInventoryTransferHeader(
+		TransferType
+		,SourceType
+		,FromLocationId
+		,ToLocationId
+		,StatusId 
+		,ShipViaId
+)
+SELECT	RawData.strTransferType
+		,RawData.intSourceType
+		,RawData.intFromLocationId
+		,RawData.intToLocationId
+		,RawData.intStatusId
+		,RawData.intShipViaId
+FROM	@TransferEntries RawData
+GROUP BY RawData.strTransferType
+		,RawData.intSourceType
+		,RawData.intFromLocationId
+		,RawData.intToLocationId
+		,RawData.intStatusId
+		,RawData.intShipViaId
+;
+
+-- Validate if there is data to process. If there is no data, then raise an error. 
+IF NOT EXISTS (SELECT TOP 1 1 FROM @DataForInventoryTransferHeader)
+BEGIN 
+	-- 'Data not found. Unable to create the Inventory Transfer.'
+	RAISERROR(51180, 11, 1);	
+	GOTO _Exit;
+END 
+
+-- Do a loop using a cursor. 
+BEGIN 
+	DECLARE @intId INT
+	DECLARE loopDataForTransferHeader CURSOR LOCAL FAST_FORWARD 
+	FOR 
+	SELECT intId FROM @DataForInventoryTransferHeader
+
+	-- Open the cursor 
+	OPEN loopDataForTransferHeader;
+
+	-- First data row fetch from the cursor 
+	FETCH NEXT FROM loopDataForTransferHeader INTO @intId;
+
+	-- Begin Loop
+	WHILE @@FETCH_STATUS = 0
+	BEGIN 
+		SET @InventoryTransferNumber = NULL 
+		SET @InventoryTransferId = NULL 
+
+		-- Check if there is an existing Inventory Transfer 
+		SELECT	@InventoryTransferId = RawData.intInventoryTransferId
+		FROM	@TransferEntries RawData INNER JOIN @DataForInventoryTransferHeader RawHeaderData
+					ON RawHeaderData.TransferType = RawData.strTransferType
+					AND RawHeaderData.SourceType = RawData.intSourceType
+					AND RawHeaderData.FromLocationId = RawData.intFromLocationId
+					AND RawHeaderData.ToLocationId = RawData.intToLocationId
+					AND RawHeaderData.StatusId = RawData.intStatusId
+					AND ISNULL(RawHeaderData.ShipViaId, 0) = ISNULL(RawData.intShipViaId, 0)
+		WHERE	RawHeaderData.intId = @intId
+				
+		IF @InventoryTransferId IS NULL 
+		BEGIN 
+			-- Generate the transfer starting number
+			-- If @InventoryTransferNumber IS NULL, uspSMGetStartingNumber will throw an error. 
+			-- Error is 'Unable to generate the transaction id. Please ask your local administrator to check the starting numbers setup.'
+			EXEC dbo.uspSMGetStartingNumber @StartingNumberId_InventoryTransfer, @InventoryTransferNumber OUTPUT 
+			IF @@ERROR <> 0 OR @InventoryTransferNumber IS NULL GOTO _BreakLoop;
+		END 
+
+		MERGE	
+		INTO	dbo.tblICInventoryTransfer 
+		WITH	(HOLDLOCK) 
+		AS		InventoryTransfer 
+		USING (
+			SELECT	RawData.*
+			FROM	@TransferEntries RawData INNER JOIN @DataForInventoryTransferHeader RawHeaderData
+						ON RawHeaderData.TransferType = RawData.strTransferType
+						AND RawHeaderData.SourceType = RawData.intSourceType
+						AND RawHeaderData.FromLocationId = RawData.intFromLocationId
+						AND RawHeaderData.ToLocationId = RawData.intToLocationId
+						AND RawHeaderData.StatusId = RawData.intStatusId
+						AND ISNULL(RawHeaderData.ShipViaId, 0) = ISNULL(RawData.intShipViaId, 0)
+			WHERE	RawHeaderData.intId = @intId
+		) AS IntegrationData
+			ON InventoryTransfer.intInventoryTransferId = IntegrationData.intInventoryTransferId
+
+		WHEN MATCHED THEN 
+			UPDATE
+			SET 
+				dtmTransferDate			= dbo.fnRemoveTimeOnDate(ISNULL(IntegrationData.dtmTransferDate, GETDATE()))
+				,strTransferType		= IntegrationData.strTransferType
+				,intSourceType			= IntegrationData.intSourceType
+				,intTransferredById		= @intUserId
+				,strDescription			= IntegrationData.strDescription
+				,intFromLocationId		= IntegrationData.intFromLocationId
+				,intToLocationId		= IntegrationData.intToLocationId
+				,ysnShipmentRequired	= IntegrationData.ysnShipmentRequired
+				,intStatusId			= IntegrationData.intStatusId
+				,intShipViaId			= IntegrationData.intShipViaId
+				,intFreightUOMId		= IntegrationData.intFreightUOMId
+				,ysnPosted				= 0
+				,intCreatedUserId		= @intUserId
+				,intEntityId			= @intUserId
+
+		WHEN NOT MATCHED THEN 
+			INSERT (
+				strTransferNo
+				,dtmTransferDate		
+				,strTransferType	
+				,intSourceType		
+				,intTransferredById	
+				,strDescription		
+				,intFromLocationId	
+				,intToLocationId	
+				,ysnShipmentRequired
+				,intStatusId		
+				,intShipViaId		
+				,intFreightUOMId	
+				,ysnPosted			
+				,intCreatedUserId	
+				,intEntityId
+			)
+			VALUES (
+				/*strTransferNo*/			@InventoryTransferNumber		
+				/*dtmTransferDate*/			,dbo.fnRemoveTimeOnDate(ISNULL(IntegrationData.dtmTransferDate, GETDATE())) 
+				/*strTransferType*/			,IntegrationData.strTransferType
+				/*intSourceType*/			,IntegrationData.intSourceType
+				/*intTransferredById*/		,@intUserId
+				/*strDescription*/			,IntegrationData.strDescription
+				/*intFromLocationId*/		,IntegrationData.intFromLocationId
+				/*intToLocationId*/			,IntegrationData.intToLocationId
+				/*ysnShipmentRequired*/		,IntegrationData.ysnShipmentRequired
+				/*intStatusId*/				,IntegrationData.intStatusId
+				/*intShipViaId*/			,IntegrationData.intShipViaId
+				/*intFreightUOMId*/			,IntegrationData.intFreightUOMId
+				/*ysnPosted*/				,0
+				/*intCreatedUserId*/		,@intUserId
+				/*intEntityId*/				,@intUserId
+			)			
+		;
+				
+		-- Get the identity value from tblICInventoryReceipt to check if the insert was successful
+		IF @InventoryTransferId IS NULL 
+		BEGIN 
+			SELECT @InventoryTransferId = SCOPE_IDENTITY()
+		END 
+
+		-- Validate the Inventory Transfer id
+		IF @InventoryTransferId IS NULL 
+		BEGIN 
+			-- 'Unable to generate the Inventory Transfer. An error stopped the creation of the inventory transfer.'
+			RAISERROR(51181, 11, 1);
+			RETURN;
+		END
+
+		--  Flush out existing detail detail data for re-insertion
+		BEGIN 
+			DELETE FROM dbo.tblICInventoryTransferDetail
+			WHERE intInventoryTransferId = @InventoryTransferId
+		END 
+
+		-- Insert the Inventory Transfer Detail. 
+		INSERT INTO dbo.tblICInventoryTransferDetail (
+				[intInventoryTransferId]
+				,[intSourceId]
+				,[intItemId]
+				,[intLotId]
+				,[intFromSubLocationId]
+				,[intToSubLocationId]
+				,[intFromStorageLocationId]
+				,[intToStorageLocationId]
+				,[dblQuantity]
+				,[intItemUOMId]
+				,[intItemWeightUOMId]
+				,[dblGrossWeight]
+				,[dblTareWeight]
+				,[intNewLotId]
+				,[strNewLotId]
+				,[dblCost]
+				,[intTaxCodeId]
+				,[dblFreightRate]
+				,[dblFreightAmount]
+				,[intSort]
+				,[intConcurrencyId]		
+		)
+		SELECT
+				[intInventoryTransferId]	= @InventoryTransferId
+				,[intSourceId]				= RawData.intSourceId
+				,[intItemId]				= RawData.intItemId
+				,[intLotId]					= RawData.intLotId
+				,[intFromSubLocationId]		= RawData.intFromSubLocationId
+				,[intToSubLocationId]		= RawData.intToSubLocationId
+				,[intFromStorageLocationId]	= RawData.intFromStorageLocationId
+				,[intToStorageLocationId]	= RawData.intToStorageLocationId
+				,[dblQuantity]				= RawData.dblQuantityToTransfer
+				,[intItemUOMId]				= RawData.intItemUOMId
+				,[intItemWeightUOMId]		= Lot.intWeightUOMId
+				,[dblGrossWeight]			= 0
+				,[dblTareWeight]			= 0 
+				,[intNewLotId]				= NULL 
+				,[strNewLotId]				= RawData.strNewLotId
+				,[dblCost]					= ISNULL(Lot.dblLastCost, ItemPricing.dblLastCost)
+				,[intTaxCodeId]				= NULL 
+				,[dblFreightRate]			= NULL 
+				,[dblFreightAmount]			= NULL 
+				,[intSort]					= NULL 
+				,[intConcurrencyId]			= 1
+		FROM	@TransferEntries RawData INNER JOIN @DataForInventoryTransferHeader RawHeaderData
+					ON RawHeaderData.TransferType = RawData.strTransferType
+					AND RawHeaderData.SourceType = RawData.intSourceType
+					AND RawHeaderData.FromLocationId = RawData.intFromLocationId
+					AND RawHeaderData.ToLocationId = RawData.intToLocationId
+					AND RawHeaderData.StatusId = RawData.intStatusId
+					AND ISNULL(RawHeaderData.ShipViaId, 0) = ISNULL(RawData.intShipViaId, 0)
+				INNER JOIN dbo.tblICItemLocation FromItemLocation
+					ON FromItemLocation.intItemId = RawData.intItemId
+					AND FromItemLocation.intLocationId = RawData.intFromLocationId
+				LEFT JOIN dbo.tblICLot Lot
+					ON Lot.intLotId = RawData.intLotId 
+				LEFT JOIN dbo.tblICItemPricing ItemPricing
+					ON ItemPricing.intItemId = RawData.intItemId
+					AND ItemPricing.intItemLocationId = FromItemLocation.intItemLocationId
+		WHERE	RawHeaderData.intId = @intId
+
+		-- Log successful inserts. 
+		INSERT INTO #tmpAddInventoryTransferResult (
+			intSourceId
+			,intInventoryTransferId
+		)
+		SELECT	InvDetail.intSourceId
+				,InvTransfer.intInventoryTransferId
+		FROM	dbo.tblICInventoryTransfer InvTransfer INNER JOIN dbo.tblICInventoryTransferDetail InvDetail
+					ON InvTransfer.intInventoryTransferId = InvDetail.intInventoryTransferId
+		WHERE	InvTransfer.intInventoryTransferId = @InventoryTransferId
+
+		-- Fetch the next row from cursor. 
+		FETCH NEXT FROM loopDataForTransferHeader INTO @intId;
+	END
+	-- End of the loop
+
+	_BreakLoop:
+
+	CLOSE loopDataForTransferHeader;
+	DEALLOCATE loopDataForTransferHeader;
+END 
+
+_Exit:
---
+++ i21Database/dbo/User Defined Types/InventoryTransferStagingTable.sql
@@ -0,0 +1,34 @@
+﻿/*
+	This is a user-defined table type used in the inventory costing stored procedures. 
+*/
+CREATE TYPE [dbo].[InventoryTransferStagingTable] AS TABLE
+(
+	 [intId] INT IDENTITY PRIMARY KEY CLUSTERED
+
+	 -- Header
+    ,[dtmTransferDate]		DATETIME NOT NULL									-- Date of the transfer. 
+	,[strTransferType]		NVARCHAR(100) COLLATE Latin1_General_CI_AS NOT NULL	-- Type of Inventory Transfer. Values can be: 'Location to Location' and 'Storage to Storage'
+	,[intSourceType]		INT NOT NULL										-- Values can be: 0 (None), 1 (Scale), 2 (Inbound Shipment), 3 (Transports)
+    ,[strDescription]		NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL		-- Description pertaining to an inventory transfer. 
+    ,[intFromLocationId]	INT NOT NULL										-- Company location id where the stock is coming from. 
+    ,[intToLocationId]		INT NOT NULL										-- Company location id where the stock is going to. 
+    ,[ysnShipmentRequired]	BIT NULL											-- Values can be: 0 - It does not need a separate shipment and it is not tracked by logistics. 1 - It indicates that the transfer requires shipment and tracked by logistics. 
+	,[intStatusId]			INT NOT NULL										-- Values can be: 1 (Open), 2 (Partial), 3 (Closed), 4 (Short Closed). 
+    ,[intShipViaId]			INT NULL											-- Ship Via from Entity. 
+    ,[intFreightUOMId]		INT NULL											-- Unit of Measure. 
+
+	-- Detail 
+	,[intItemId]				INT NOT NULL									-- Item id to transfer
+	,[intLotId]					INT NULL										-- Existing Lot number to transfer
+    ,[intItemUOMId]				INT NULL										-- UOM used for the transfer. Ex. Gallon
+    ,[dblQuantityToTransfer]	NUMERIC(18, 6) NULL								-- Qty to transfer related to the UOM. Ex. "10" gallons. 
+    ,[strNewLotId]				NVARCHAR(50) COLLATE Latin1_General_CI_AS		-- New lot number to use when stock is transferred to the new location.
+	,[intFromSubLocationId]		INT NULL										-- Source sub location of the item. 
+    ,[intToSubLocationId]		INT NULL										-- Target sub location of the item. 
+    ,[intFromStorageLocationId]	INT NULL										-- Source storage location id of the item.
+    ,[intToStorageLocationId]	INT NULL										-- Target storage location id of the item.
+
+	-- Integration Field
+	,[intInventoryTransferId] INT NULL											-- Existing id of an Inventory Transfer
+	,[intSourceId] INT NOT NULL													-- PK id of the source transaction. Ex. Transport Load id. 
+)
