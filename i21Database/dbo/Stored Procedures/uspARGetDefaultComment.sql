505d2261c4f12f3d4eef29bb6fb2e534b46053d6
Kelvin Quinto <kelvin.quinto@irely.com>
2015-09-09 01:55:54 +0800

    AR-1644

---
+++ i21Database/dbo/Stored Procedures/uspARGetDefaultComment.sql
@@ -0,0 +1,52 @@
+﻿CREATE PROCEDURE [dbo].[uspARGetDefaultComment]
+	@intCompanyLocationId	INT = NULL,
+	@intEntityCustomerId	INT = NULL,
+	@strTransactionType     NVARCHAR(50) = NULL,
+	@strType                NVARCHAR(50) = NULL,
+	@strDefaultComment		NVARCHAR(250) = NULL OUTPUT
+AS
+
+--FILTER ALL
+SELECT TOP 1 @strDefaultComment = strCommentDesc
+	FROM tblARCommentMaintenance
+	WHERE ((@intCompanyLocationId IS NULL OR @intCompanyLocationId = 0) OR intCompanyLocationId = @intCompanyLocationId)
+	AND ((@intEntityCustomerId IS NULL OR @intEntityCustomerId = 0) OR intEntityCustomerId = @intEntityCustomerId)
+	AND ((@strTransactionType IS NULL OR @strTransactionType = '') OR strTransactionType = @strTransactionType)
+	AND ((@strType IS NULL OR @strType = '' OR @strTransactionType <> 'Invoice') OR strType = @strType)
+ORDER BY intCommentId DESC
+
+--FILTER BY COMPANY LOC, CUSTOMER, TRANSACTION TYPE
+IF @strDefaultComment IS NULL OR @strDefaultComment = ''
+	BEGIN
+		SELECT TOP 1 @strDefaultComment = strCommentDesc
+			FROM tblARCommentMaintenance
+			WHERE ((@intCompanyLocationId IS NULL OR @intCompanyLocationId = 0) OR intCompanyLocationId = @intCompanyLocationId)
+			AND ((@intEntityCustomerId IS NULL OR @intEntityCustomerId = 0) OR intEntityCustomerId = @intEntityCustomerId)
+			AND ((@strTransactionType IS NULL OR @strTransactionType = '') OR strTransactionType = @strTransactionType)			
+		ORDER BY intCommentId DESC
+	END
+
+--FILTER BY COMPANY LOC, TRANSACTION TYPE
+IF @strDefaultComment IS NULL OR @strDefaultComment = ''
+	BEGIN
+		SELECT TOP 1 @strDefaultComment = strCommentDesc
+			FROM tblARCommentMaintenance
+			WHERE ((@intCompanyLocationId IS NULL OR @intCompanyLocationId = 0) OR intCompanyLocationId = @intCompanyLocationId)
+			AND ((@strTransactionType IS NULL OR @strTransactionType = '') OR strTransactionType = @strTransactionType)
+		ORDER BY intCommentId DESC
+	END
+
+--FILTER BY TRANSACTION TYPE
+IF @strDefaultComment IS NULL OR @strDefaultComment = ''
+	BEGIN
+		SELECT TOP 1 @strDefaultComment = strCommentDesc
+			FROM tblARCommentMaintenance
+			WHERE ((@strTransactionType IS NULL OR @strTransactionType = '') OR strTransactionType = @strTransactionType)			
+		ORDER BY intCommentId DESC
+	END
+
+IF @strDefaultComment IS NULL OR @strDefaultComment = ''
+	BEGIN
+		SELECT TOP 1 @strDefaultComment = strCommentDesc
+			FROM tblARCommentMaintenance ORDER BY intCommentId DESC
+	END
