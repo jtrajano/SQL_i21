ca079c4b09a99797fbc5c4b4c0d852826cb8eb2c
kenneth lagunero <kenneth.lagunero@irely.com>
2015-09-09 15:00:40 +0800

    TM-1396

---
+++ i21Database/dbo/Views/vyuTMSiteDeliveryHistory.sql
@@ -0,0 +1,151 @@
+﻿CREATE VIEW [dbo].[vyuTMSiteDeliveryHistory]
+AS  
+	SELECT 
+		strCustomerNumber = C.strEntityNo
+		,strCustomerName = C.strName
+		,intSiteNumber = A.intSiteNumber
+		,strSiteBillingBy = A.strBillingBy
+		,ysnSiteActive = A.ysnActive
+		,strSiteDescription = A.strDescription
+		,strSiteAddress = A.strSiteAddress
+		,strSiteZipCode = A.strZipCode
+		,strSiteCity = A.strCity
+		,strSiteState = A.strState
+		,strSiteCountry = A.strCountry
+		,dblSiteLatitude = A.dblLatitude
+		,dblSiteLongitude = A.dblLongitude
+		,strSiteDriverName = L.strName
+		,strSiteRouteId = N.strRouteId
+		,strSiteSequenceID = A.strSequenceID
+		,ysnSiteOnHold = A.ysnOnHold
+		,ysnSiteHoldDDCalculations = A.ysnHoldDDCalculations
+		,strSiteHoldReason = O.strHoldReason	
+		,dtmSiteOnHoldStartDate = A.dtmOnHoldStartDate
+		,dtmSiteOnHoldEndDate = A.dtmOnHoldEndDate
+		,strSiteLocation = P.strLocationName
+		,dblSiteTotalCapacity = A.dblTotalCapacity
+		,strSiteClockNumber = Q.strClockNumber
+		,dblSiteTotalReserve = A.dblTotalReserve
+		,strSiteAcctStatus = A.strAcctStatus
+		,strSiteDeliveryTermID = R.strTerm
+		,ysnSiteTaxable = A.ysnTaxable
+		,dblSitePriceAdjustment = A.dblPriceAdjustment
+		,strSiteTax = S.strTaxGroupMaster
+		,strSiteRecurringPONumber = A.strRecurringPONumber
+		,strSiteClassFillOption = A.strClassFillOption
+		,ysnSitePrintARBalance = A.ysnPrintARBalance
+		,strSiteItemNumber = T.strItemNo
+		,strSiteItemDescription = T.strDescription
+		,strSiteFillMethod = U.strFillMethod
+		,strSiteFillGroupCode = V.strFillGroupCode
+		,dblSiteDegreeDayBetweenDelivery = A.dblDegreeDayBetweenDelivery
+		,dtmSiteNextDeliveryDate = A.dtmNextDeliveryDate
+		,dblSiteSummerDailyUse = A.dblSummerDailyUse
+		,dblSiteWinterDailyUse = A.dblWinterDailyUse
+		,dblSiteBurnRate = A.dblBurnRate
+		,dblSitePreviousBurnRate = A.dblPreviousBurnRate
+		,ysnSitePromptForPercentFull = A.ysnPromptForPercentFull 
+		,ysnSiteAdjustBurnRate = A.ysnAdjustBurnRate
+		,dtmSiteLastDeliveryDate = A.dtmLastDeliveryDate
+		,intSiteLastDeliveryDegreeDay = A.intLastDeliveryDegreeDay
+		,dblSiteLastDeliveredGal = A.dblLastDeliveredGal
+		,intSiteNextDeliveryDegreeDay = A.intNextDeliveryDegreeDay
+		,dblSiteEstimatedGallonsLeft = A.dblEstimatedGallonsLeft
+		,dblSiteEstimatedPercentLeft = A.dblEstimatedPercentLeft
+		,dblSiteLastGalsInTank = A.dblLastGalsInTank
+		,dblSiteYTDGalsThisSeason = A.dblYTDGalsThisSeason  
+		,dblSiteYTDGalsLastSeason = A.dblYTDGalsLastSeason
+		,dblSiteYTDGals2SeasonsAgo = A.dblYTDGals2SeasonsAgo 
+		,dblSiteYTDSales = A.dblYTDSales
+		,dblSiteYTDSalesLastSeason = A.dblYTDSalesLastSeason
+		,dblSiteYTDSales2SeasonsAgo = A.dblYTDSales2SeasonsAgo
+		,dtmSiteRunOutDate = A.dtmRunOutDate
+		,dtmSiteForecastedDelivery = A.dtmForecastedDelivery
+		,strSiteTankTownship = W.strTankTownship
+		,ysnSitePrintDeliveryTicket = A.ysnPrintDeliveryTicket
+		,strSiteInstruction = A.strInstruction
+		,strSiteComment = A.strComment
+		,strDHInvoiceNumber = D.strInvoiceNumber
+		,strDHBulkPlantNumber = D.strBulkPlantNumber
+		,dtmDHInvoiceDate = D.dtmInvoiceDate
+		,strDHProductDelivered = D.strProductDelivered
+		,dblDHQuantityDelivered = D.dblQuantityDelivered
+		,intDHDegreeDayOnDeliveryDate = D.intDegreeDayOnDeliveryDate
+		,intDHDegreeDayOnLastDeliveryDate = D.intDegreeDayOnLastDeliveryDate
+		,dblDHBurnRateAfterDelivery = D.dblBurnRateAfterDelivery
+		,dblDHCalculatedBurnRate = D.dblCalculatedBurnRate
+		,ysnDHAdjustBurnRate = D.ysnAdjustBurnRate
+		,intDHElapsedDegreeDaysBetweenDeliveries = D.intElapsedDegreeDaysBetweenDeliveries
+		,intDHElapsedDaysBetweenDeliveries = D.intElapsedDaysBetweenDeliveries
+		,strDHSeason = D.strSeason
+		,dblDHWinterDailyUsageBetweenDeliveries = D.dblWinterDailyUsageBetweenDeliveries
+		,dblDHSummerDailyUsageBetweenDeliveries = D.dblSummerDailyUsageBetweenDeliveries
+		,dblDHGallonsInTankbeforeDelivery = D.dblGallonsInTankbeforeDelivery 
+		,dblDHGallonsInTankAfterDelivery = D.dblGallonsInTankAfterDelivery
+		,dblDHEstimatedPercentBeforeDelivery = D.dblEstimatedPercentBeforeDelivery
+		,dblDHActualPercentAfterDelivery = D.dblActualPercentAfterDelivery
+		,strDHSalesPersonID = D.strSalesPersonID 
+		,dtmDHLastUpdated = D.dtmLastUpdated
+		,dblDHWillCallPercentLeft = D.dblWillCallPercentLeft
+		,dblDHWillCallCalculatedQuantity = D.dblWillCallCalculatedQuantity
+		,dblDHWillCallDesiredQuantity = D.dblWillCallDesiredQuantity
+		,strDHWillCallDriverName = E.strName
+		,strDHWillCallItemNo =  G.strItemNo
+		,strDHWillCallSubstituteItemNo =  H.strItemNo
+		,dblDHWillCallDeliveryPrice = D.dblWillCallDeliveryPrice
+		,strDHWillCallTerm = I.strTerm
+		,dtmDHWillCallRequestedDate = D.dtmWillCallRequestedDate
+		,intDHWillCallPriority = D.intWillCallPriority
+		,dblDHWillCallDeliveryTotal = D.dblWillCallDeliveryTotal 
+		,ysnDHWillCallPrinted = D.ysnWillCallPrinted
+		,strDHWillCallComments = D.strWillCallComments
+		,strDHWillCallEnteredBy = J.strUserName
+		,dtmDHWillCallCallInDate = D.dtmWillCallCallInDate
+		,dtmDHWillCallDispatch = D.dtmWillCallDispatch
+		,strDHWillCallOrderNumber = D.strWillCallOrderNumber
+	FROM tblTMSite A
+	INNER JOIN tblTMCustomer B	
+		ON A.intCustomerID = B.intCustomerID
+	INNER JOIN tblEntity C
+		ON B.intCustomerNumber = C.intEntityId
+	INNER JOIN tblTMDeliveryHistory D
+		ON A.intSiteID = D.intSiteID
+	----Start Getting Will call Driver	
+	LEFT JOIN tblEntity E
+		ON D.intWillCallDriverId = E.intEntityId	
+	----End Getting Driver
+	LEFT JOIN tblICItem G
+		ON D.intWillCallProductId = G.intItemId
+	LEFT JOIN tblICItem H
+		ON D.intWillCallSubstituteProductId = H.intItemId
+	LEFT JOIN tblSMTerm I
+		ON D.intWillCallDeliveryTermId = I.strTerm
+	LEFT JOIN tblSMUserSecurity J
+		ON D.intUserID = J.intUserSecurityID
+	----Start Getting will call Driver	
+	LEFT JOIN tblEntity L
+		ON A.intDriverID = L.intEntityId	
+	----End Getting Driver
+	LEFT JOIN tblTMRoute N
+		ON A.intRouteId = N.intRouteId
+	LEFT JOIN tblTMHoldReason O
+		ON A.intHoldReasonID = O.intHoldReasonID
+	---Start Getting Site Location	
+	LEFT JOIN tblSMCompanyLocation P
+		ON A.intLocationId = P.intCompanyLocationId
+	---END Getting Site Location
+	LEFT JOIN tblTMClock Q
+		ON A.intClockID = Q.intClockID	
+	LEFT JOIN tblSMTerm R
+		ON A.intDeliveryTermID = R.intTermID
+	LEFT JOIN tblSMTaxGroupMaster S
+		ON A.intTaxStateID = S.intTaxGroupMasterId
+	LEFT JOIN tblICItem T
+		ON A.intProduct = T.intItemId 
+	LEFT JOIN tblTMFillMethod U
+		ON A.intFillMethodId = U.intFillMethodId
+	LEFT JOIN tblTMFillGroup V
+		ON A.intFillGroupId = V.intFillGroupId
+	LEFT JOIN tblTMTankTownship W
+		ON A.intTankTownshipId = W.intTankTownshipId
+GO
---
+++ i21Database/dbo/Views/vyuTMSiteEventHistory.sql
@@ -0,0 +1,161 @@
+﻿CREATE VIEW [dbo].[vyuTMSiteEventHistory]
+AS  
+		SELECT 
+		strCustomerNumber = C.strEntityNo
+		,strCustomerName = C.strName
+		,intSiteNumber = A.intSiteNumber
+		,strSiteBillingBy = A.strBillingBy
+		,ysnSiteActive = A.ysnActive
+		,strSiteDescription = A.strDescription
+		,strSiteAddress = A.strSiteAddress
+		,strSiteZipCode = A.strZipCode
+		,strSiteCity = A.strCity
+		,strSiteState = A.strState
+		,strSiteCountry = A.strCountry
+		,dblSiteLatitude = A.dblLatitude
+		,dblSiteLongitude = A.dblLongitude
+		,strSiteDriverName = L.strName
+		,strSiteRouteId = N.strRouteId
+		,strSiteSequenceID = A.strSequenceID
+		,ysnSiteOnHold = A.ysnOnHold
+		,ysnSiteHoldDDCalculations = A.ysnHoldDDCalculations
+		,strSiteHoldReason = O.strHoldReason	
+		,dtmSiteOnHoldStartDate = A.dtmOnHoldStartDate
+		,dtmSiteOnHoldEndDate = A.dtmOnHoldEndDate
+		,strSiteLocation = P.strLocationName
+		,dblSiteTotalCapacity = A.dblTotalCapacity
+		,strSiteClockNumber = Q.strClockNumber
+		,dblSiteTotalReserve = A.dblTotalReserve
+		,strSiteAcctStatus = A.strAcctStatus
+		,strSiteDeliveryTermID = R.strTerm
+		,ysnSiteTaxable = A.ysnTaxable
+		,dblSitePriceAdjustment = A.dblPriceAdjustment
+		,strSiteTax = S.strTaxGroupMaster
+		,strSiteRecurringPONumber = A.strRecurringPONumber
+		,strSiteClassFillOption = A.strClassFillOption
+		,ysnSitePrintARBalance = A.ysnPrintARBalance
+		,strSiteItemNumber = T.strItemNo
+		,strSiteItemDescription = T.strDescription
+		,strSiteFillMethod = U.strFillMethod
+		,strSiteFillGroupCode = V.strFillGroupCode
+		,dblSiteDegreeDayBetweenDelivery = A.dblDegreeDayBetweenDelivery
+		,dtmSiteNextDeliveryDate = A.dtmNextDeliveryDate
+		,dblSiteSummerDailyUse = A.dblSummerDailyUse
+		,dblSiteWinterDailyUse = A.dblWinterDailyUse
+		,dblSiteBurnRate = A.dblBurnRate
+		,dblSitePreviousBurnRate = A.dblPreviousBurnRate
+		,ysnSitePromptForPercentFull = A.ysnPromptForPercentFull 
+		,ysnSiteAdjustBurnRate = A.ysnAdjustBurnRate
+		,dtmSiteLastDeliveryDate = A.dtmLastDeliveryDate
+		,intSiteLastDeliveryDegreeDay = A.intLastDeliveryDegreeDay
+		,dblSiteLastDeliveredGal = A.dblLastDeliveredGal
+		,intSiteNextDeliveryDegreeDay = A.intNextDeliveryDegreeDay
+		,dblSiteEstimatedGallonsLeft = A.dblEstimatedGallonsLeft
+		,dblSiteEstimatedPercentLeft = A.dblEstimatedPercentLeft
+		,dblSiteLastGalsInTank = A.dblLastGalsInTank
+		,dblSiteYTDGalsThisSeason = A.dblYTDGalsThisSeason  
+		,dblSiteYTDGalsLastSeason = A.dblYTDGalsLastSeason
+		,dblSiteYTDGals2SeasonsAgo = A.dblYTDGals2SeasonsAgo 
+		,dblSiteYTDSales = A.dblYTDSales
+		,dblSiteYTDSalesLastSeason = A.dblYTDSalesLastSeason
+		,dblSiteYTDSales2SeasonsAgo = A.dblYTDSales2SeasonsAgo
+		,dtmSiteRunOutDate = A.dtmRunOutDate
+		,dtmSiteForecastedDelivery = A.dtmForecastedDelivery
+		,strSiteTankTownship = W.strTankTownship
+		,ysnSitePrintDeliveryTicket = A.ysnPrintDeliveryTicket
+		,strSiteInstruction = A.strInstruction
+		,strSiteComment = A.strComment
+		,strDeviceType = F.strDeviceType
+		,strDeviceDescription = E.strDescription
+		,strDeviceOwnership = E.strOwnership
+		,strDeviceBulkPlant = E.strBulkPlant
+		,strDeviceInventoryStatus = G.strInventoryStatusType
+		,strDeviceComment = E.strComment
+		,strDeviceParent = H.strSerialNumber
+		,strDeviceRegulatorType = I.strRegulatorType
+		,strDeviceLease = J.strLeaseNumber
+		,strDeviceSerialNumber = E.strSerialNumber
+		,strDeviceManufacturerID = E.strManufacturerID
+		,strDeviceManufacturerName = E.strManufacturerName
+		,dtmDeviceManufacturedDate = E.dtmManufacturedDate
+		,strDeviceModelNumber = E.strModelNumber
+		,strDeviceAssetNumber = E.strAssetNumber
+		,dblDevicePurchasePrice = E.dblPurchasePrice
+		,dtmDevicePurchaseDate = E.dtmPurchaseDate
+		,dblDeviceTankCapacity = E.dblTankCapacity
+		,dblDeviceTankReserve = E.dblTankReserve
+		,strDeviceTankType = K.strTankType
+		,dblDeviceEstimatedGalTank = E.dblEstimatedGalTank
+		,ysnDeviceUnderground = E.ysnUnderground
+		,strDeviceMeterType = X.strMeterType
+		,intDeviceMeterCycle = E.intMeterCycle
+		,strDeviceMeterStatus = E.strMeterStatus
+		,dblDeviceMeterReading = E.dblMeterReading
+		,ysnDeviceAppliance = E.ysnAppliance
+		,dtmEventDate = D.dtmDate
+		,dtmEventLastUpdated = D.dtmLastUpdated
+		,dtmEventTankMonitorReading = D.dtmTankMonitorReading
+		,strEventType = Y.strEventType
+		,strEventPerformerName = Z.strName
+		,strEventUser = AB.strUserName
+		,strEventDescription = D.strDescription
+		,strEventLevel = D.strLevel
+	FROM tblTMSite A
+	INNER JOIN tblTMCustomer B	
+		ON A.intCustomerID = B.intCustomerID
+	INNER JOIN tblEntity C
+		ON B.intCustomerNumber = C.intEntityId
+	INNER JOIN tblTMEvent D
+		ON A.intSiteID = D.intSiteID
+	LEFT JOIN tblTMDevice E
+		ON D.intDeviceId = E.intDeviceId
+	LEFT JOIN tblTMDeviceType F
+		ON E.intDeviceTypeId = F.intDeviceTypeId
+	LEFT JOIN tblTMInventoryStatusType G
+		ON E.intInventoryStatusTypeId = G.intInventoryStatusTypeId
+	LEFT JOIN tblTMDevice H
+		ON E.intParentDeviceID = H.intDeviceId
+	LEFT JOIN tblTMRegulatorType I
+		ON E.intRegulatorTypeId = I.intRegulatorTypeId
+	LEFT JOIN tblTMLease J
+		ON E.intLeaseId = J.intLeaseId
+	LEFT JOIN tblTMTankType K
+		ON E.intTankTypeId = K.intTankTypeId
+	----Start Getting Site Driver	
+	LEFT JOIN tblEntity L
+		ON A.intDriverID = L.intEntityId	
+	----End Getting Driver
+	LEFT JOIN tblTMRoute N
+		ON A.intRouteId = N.intRouteId
+	LEFT JOIN tblTMHoldReason O
+		ON A.intHoldReasonID = O.intHoldReasonID
+	---Start Getting Site Location	
+	LEFT JOIN tblSMCompanyLocation P
+		ON A.intLocationId = P.intCompanyLocationId
+	---END Getting Site Location
+	LEFT JOIN tblTMClock Q
+		ON A.intClockID = Q.intClockID	
+	LEFT JOIN tblSMTerm R
+		ON A.intDeliveryTermID = R.intTermID
+	LEFT JOIN tblSMTaxGroupMaster S
+		ON A.intTaxStateID = S.intTaxGroupMasterId
+	LEFT JOIN tblICItem T
+		ON A.intProduct = T.intItemId 
+	LEFT JOIN tblTMFillMethod U
+		ON A.intFillMethodId = U.intFillMethodId
+	LEFT JOIN tblTMFillGroup V
+		ON A.intFillGroupId = V.intFillGroupId
+	LEFT JOIN tblTMTankTownship W
+		ON A.intTankTownshipId = W.intTankTownshipId
+	LEFT JOIN tblTMMeterType X
+		ON E.intMeterTypeId = X.intMeterTypeId
+	LEFT JOIN tblTMEventType Y
+		ON D.intEventTypeID = Y.intEventTypeID
+	----Start Getting Event Performer	
+	LEFT JOIN tblEntity Z
+		ON D.intPerformerID = Z.intEntityId	
+	----End Getting Event Performer
+	LEFT JOIN tblSMUserSecurity AB
+		ON D.intUserID = AB.intUserSecurityID
+		
+GO
