70c28f9e21b3128193caed4ad35910c9a7bf500b
Premkumar S <prem.selladurai@irely.com>
2015-09-09 18:46:35 +0530

    MFG-620

--- i21Database/dbo/Tables/tblMFCompanyPreference.sql
+++ i21Database/dbo/Tables/tblMFCompanyPreference.sql
@@ -2,6 +2,7 @@
 (
 	[intCompanyPreferenceId] INT NOT NULL IDENTITY,
 	[ysnEnableParentLot] BIT NOT NULL CONSTRAINT [DF_tblMFCompanyPreference_ysnEnableParentLot] DEFAULT 0,
+	ysnDisplayWorkOrderComments Bit Constraint DF_tblMFCompanyPreference_ysnDisplayWorkOrderComments Default 1, 
 	[intConcurrencyId] INT NULL CONSTRAINT [DF_tblMFCompanyPreference_intConcurrencyId] DEFAULT 0,
 	CONSTRAINT [PK_tblMFCompanyPreference_intCompanyPreferenceId] PRIMARY KEY ([intCompanyPreferenceId]),  
 )
