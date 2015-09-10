35d6a4fd8e5907ed2b2569401e99a67e41e81a72
Mon Gonzales <mon.gonzales@irely.com>
2015-09-10 14:16:56 +0800

    EM-477

--- i21Database/dbo/Tables/tblEntity.sql
+++ i21Database/dbo/Tables/tblEntity.sql
@@ -26,7 +26,8 @@
 	[strContactType]   NVARCHAR (50) COLLATE Latin1_General_CI_AS NULL,
 	[intDefaultLocationId]       INT            NULL,
 	[ysnActive]        BIT             CONSTRAINT [DF_tblEntity_ysnActive] DEFAULT ((1)) NOT NULL,
-	[ysnReceiveEmail]  BIT             CONSTRAINT [DF_tblEntity_ysnReceiveEmail] DEFAULT ((0)) NOT NULL,    	
+	[ysnReceiveEmail]  BIT             CONSTRAINT [DF_tblEntity_ysnReceiveEmail] DEFAULT ((0)) NOT NULL,
+	[strEmailDistributionOption]	NVARCHAR(MAX)	 COLLATE Latin1_General_CI_AS NULL,    	
     [intConcurrencyId] INT             CONSTRAINT [DF__tmp_ms_xx__intCo__5132705A] DEFAULT ((0)) NOT NULL,
 	--CONSTRAINT [FK_tblEntity_tblEntityLocation] FOREIGN KEY ([intDefaultLocationId]) REFERENCES [tblEntityLocation]([intEntityLocationId]),
     CONSTRAINT [PK_dbo.tblEntity] PRIMARY KEY CLUSTERED ([intEntityId] ASC)
