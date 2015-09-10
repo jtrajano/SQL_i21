c95a4c4c6c4248fe5f837ba7e23b83d885c15c79
Mon Gonzales <mon.gonzales@irely.com>
2015-09-10 20:20:33 +0800

    EM-383

--- i21Database/dbo/Tables/tblAPVendor.sql
+++ i21Database/dbo/Tables/tblAPVendor.sql
@@ -33,7 +33,7 @@
     CONSTRAINT [PK_dbo.tblAPVendor] PRIMARY KEY CLUSTERED ([intEntityVendorId] ASC),
     CONSTRAINT [FK_dbo.tblAPVendor_dbo.tblEntities_intEntityId] FOREIGN KEY ([intEntityVendorId]) REFERENCES [dbo].[tblEntity] ([intEntityId]) ON DELETE CASCADE,
     --CONSTRAINT [UK_strVendorId] UNIQUE NONCLUSTERED ([strVendorId] ASC),
-	CONSTRAINT [UK_intVendorId] UNIQUE NONCLUSTERED ([intEntityVendorId] ASC),
+	--CONSTRAINT [UK_intVendorId] UNIQUE NONCLUSTERED ([intEntityVendorId] ASC),
 	--CONSTRAINT [FK_tblAPVendor_tblEntity] FOREIGN KEY ([intDefaultContactId]) REFERENCES [tblEntity]([intEntityId]),
 	CONSTRAINT [FK_tblAPVendor_tblEntityLocation] FOREIGN KEY ([intDefaultLocationId]) REFERENCES [tblEntityLocation]([intEntityLocationId]),
 	CONSTRAINT [FK_tblAPVendor_tblGLAccount] FOREIGN KEY ([intGLAccountExpenseId]) REFERENCES [tblGLAccount]([intAccountId]),
