c6f0b4af76511808577993ad70c6cbfc069bead1
Jayson Ramos <jayson.ramos@iRely.com>
2015-09-10 11:42:35 +0800

    HD-253;

--- i21Database/Scripts/Post-Deployment/HD/DefaultData/1_StatusData.sql
+++ i21Database/Scripts/Post-Deployment/HD/DefaultData/1_StatusData.sql
@@ -20,4 +20,87 @@ IF NOT EXISTS (SELECT TOP 1 1 FROM [tblHDTicketStatus] WHERE strStatus = 'Reopen
 
 GO
 	PRINT N'END INSERT DEFAULT HELP DESK STATUS'
+	PRINT N'BEGIN INSERT DEFAULT HELP DESK TICKET LINK TYPE'
+GO
+
+SET IDENTITY_INSERT [dbo].[tblHDTicketLinkType] ON
+IF NOT EXISTS (SELECT TOP 1 1 FROM [tblHDTicketLinkType] WHERE strLinkType = 'relates to') INSERT [dbo].[tblHDTicketLinkType]
+																							(
+																								[intTicketLinkTypeId]
+																								,[strLinkType]
+																								,[intTicketLinkTypeCounterId]
+																								,[intSort]
+																								,[intConcurrencyId]
+																							) VALUES (
+																								1
+																								,N'relates to'
+																								,1
+																								,1
+																								,1
+																							)
+
+IF NOT EXISTS (SELECT TOP 1 1 FROM [tblHDTicketLinkType] WHERE strLinkType = 'is blocked by') INSERT [dbo].[tblHDTicketLinkType]
+																							(
+																								[intTicketLinkTypeId]
+																								,[strLinkType]
+																								,[intTicketLinkTypeCounterId]
+																								,[intSort]
+																								,[intConcurrencyId]
+																							) VALUES (
+																								2
+																								,N'is blocked by'
+																								,3
+																								,2
+																								,1
+																							)
+
+IF NOT EXISTS (SELECT TOP 1 1 FROM [tblHDTicketLinkType] WHERE strLinkType = 'blocks') INSERT [dbo].[tblHDTicketLinkType]
+																							(
+																								[intTicketLinkTypeId]
+																								,[strLinkType]
+																								,[intTicketLinkTypeCounterId]
+																								,[intSort]
+																								,[intConcurrencyId]
+																							) VALUES (
+																								3
+																								,N'blocks'
+																								,2
+																								,3
+																								,1
+																							)
+
+IF NOT EXISTS (SELECT TOP 1 1 FROM [tblHDTicketLinkType] WHERE strLinkType = 'is duplicated by') INSERT [dbo].[tblHDTicketLinkType]
+																							(
+																								[intTicketLinkTypeId]
+																								,[strLinkType]
+																								,[intTicketLinkTypeCounterId]
+																								,[intSort]
+																								,[intConcurrencyId]
+																							) VALUES (
+																								4
+																								,N'is duplicated by'
+																								,5
+																								,4
+																								,1
+																							)
+
+IF NOT EXISTS (SELECT TOP 1 1 FROM [tblHDTicketLinkType] WHERE strLinkType = 'duplicates') INSERT [dbo].[tblHDTicketLinkType]
+																							(
+																								[intTicketLinkTypeId]
+																								,[strLinkType]
+																								,[intTicketLinkTypeCounterId]
+																								,[intSort]
+																								,[intConcurrencyId]
+																							) VALUES (
+																								5
+																								,N'duplicates'
+																								,4
+																								,5
+																								,1
+																							)
+
+SET IDENTITY_INSERT [dbo].[tblHDTicketLinkType] OFF
+
+GO
+	PRINT N'END INSERT DEFAULT HELP DESK TICKET LINK TYPE'
 GO
