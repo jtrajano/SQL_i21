929554c221406be2e042e19b032e47ffc7f42ebf
kenneth lagunero <kenneth.lagunero@irely.com>
2015-09-11 08:58:16 +0800

    TM-1508

---
+++ i21Database/Scripts/Post-Deployment/TM/2_DataTransferAndCorrection.sql
@@ -0,0 +1,16 @@
+﻿print N'BEGIN CONVERSION - i21 TANK MANAGEMENT..'
+
+print N'BEGIN Migration of Data from preference company to lease code'
+IF EXISTS(SELECT TOP 1 1 FROM sys.columns WHERE name = N'intLeaseItemId' AND Object_ID = Object_ID(N'tblTMPreferenceCompany'))
+BEGIN
+	EXEC ('
+		UPDATE tblTMLeaseCode
+		SET intItemId = (SELECT TOP 1 intLeaseItemId FROM tblTMPreferenceCompany)
+		WHERE intItemId IS NOT NULL
+	')
+
+	EXEC('
+		ALTER TABLE tblTMPreferenceCompany DROP COLUMN intLeaseItemId 
+	')
+END
+print N'END Migration of Data from preference company to lease code'
