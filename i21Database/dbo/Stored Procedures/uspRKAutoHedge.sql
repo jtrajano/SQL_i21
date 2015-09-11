d5c688c725daf440d12d86b7f44aa7a392d117b2
Ramachandran <ramachandran.shanmuganathan@irely.com>
2015-09-11 15:22:16 +0530

    RM-117

---
+++ i21Database/dbo/Stored Procedures/uspRKAutoHedge.sql
@@ -0,0 +1,168 @@
+﻿CREATE PROC uspRKAutoHedge
+   @XML nvarchar(MAX),
+   @intFutOptTransactionId INT OUTPUT               
+AS          
+BEGIN   Try       
+ DECLARE @dtmTransactionDate datetime,
+            @intEntityId int,
+            @intBrokerageAccountId int,
+            @intFutureMarketId int,
+            @intFutureMonthId int,
+            @intInstrumentTypeId int,
+            @intCommodityId int,
+            @intLocationId int,
+            @intTraderId int,
+            @strInternalTradeNo nvarchar(10),
+            @strBrokerTradeNo nvarchar(50),
+            @strBuySell nvarchar(10),
+            @intNoOfContract int,
+            @dblPrice int,
+            @strStatus nvarchar(50),
+            @dtmFilledDate datetime,
+            @strReserveForFix nvarchar(50),
+            @intBookId int,
+            @intSubBookId int,
+            @ysnOffset bit,
+            @intFutOptTransactionHeaderId int,
+            @ErrMsg nvarchar(max),
+            @intCurrencyId INT
+    
+
+
+INSERT INTO tblRKFutOptTransactionHeader  Values(1)
+SELECT @intFutOptTransactionHeaderId = SCOPE_IDENTITY() 
+DECLARE @idoc int
+EXEC sp_xml_preparedocument @idoc OUTPUT, @XML          
+      
+SELECT  
+        @intFutOptTransactionId = intFutOptTransactionId,
+      @dtmTransactionDate =    dtmTransactionDate ,
+      @intEntityId =     intEntityId ,
+      @intBrokerageAccountId = intBrokerageAccountId ,
+      @intFutureMarketId =     intFutureMarketId ,
+      @intFutureMonthId =      intFutureMonthId ,
+      @intInstrumentTypeId =   intInstrumentTypeId ,
+      @intCommodityId =  intCommodityId ,
+      @intLocationId =   intLocationId ,
+      @intTraderId =     intTraderId ,
+      @strInternalTradeNo = strInternalTradeNo ,
+      @strBrokerTradeNo =      strBrokerTradeNo ,
+      @strBuySell =      strBuySell ,
+      @intNoOfContract = intNoOfContract, 
+      @dblPrice =  dblPrice ,
+      @strStatus = strStatus ,
+      @dtmFilledDate =   dtmFilledDate ,
+      @strReserveForFix =      strReserveForFix ,
+      @intBookId = intBookId ,
+      @intSubBookId =    intSubBookId ,
+      @ysnOffset = ysnOffset ,
+      @intCurrencyId = intCurrencyId
+
+FROM OPENXML(@idoc,'root',2)          
+WITH(
+intFutOptTransactionId INT,
+dtmTransactionDate datetime,
+intEntityId int,
+intBrokerageAccountId int,
+intFutureMarketId int,
+intFutureMonthId int,
+intInstrumentTypeId int,
+intCommodityId int,
+intLocationId int,
+intTraderId int,
+strInternalTradeNo nvarchar(10),
+strBrokerTradeNo nvarchar(50),
+strBuySell nvarchar(10),
+intNoOfContract int,
+dblPrice int,
+strStatus nvarchar(50),
+dtmFilledDate datetime,
+strReserveForFix nvarchar(50),
+intBookId int,
+intSubBookId int,
+ysnOffset bit,
+intCurrencyId INT,
+CurrentDate datetime
+)      
+IF ISNULL(@intFutOptTransactionId,0) > 0
+BEGIN
+      UPDATE tblRKFutOptTransaction
+      SET 
+      
+      intEntityId =     @intEntityId ,
+      intBrokerageAccountId = @intBrokerageAccountId ,
+      intFutureMarketId =     @intFutureMarketId ,
+      intFutureMonthId =      @intFutureMonthId ,
+      intNoOfContract = @intNoOfContract, 
+      dblPrice =  @dblPrice 
+      WHERE intFutOptTransactionId = @intFutOptTransactionId
+END
+ELSE
+BEGIN
+      IF ISNULL(@strInternalTradeNo,'') = ''
+      BEGIN
+            SELECT @strInternalTradeNo = strPrefix + LTRIM(intNumber) FROM tblSMStartingNumber WHERE strTransactionType = 'FutOpt Transaction'
+            UPDATE tblSMStartingNumber SET intNumber = intNumber + 1 WHERE strTransactionType = 'FutOpt Transaction'
+      END
+
+      INSERT INTO tblRKFutOptTransaction(dtmTransactionDate ,
+              intFutOptTransactionHeaderId,
+              intEntityId ,
+              intBrokerageAccountId ,
+              intFutureMarketId ,
+              intFutureMonthId ,
+              intInstrumentTypeId ,
+              intCommodityId ,
+              intLocationId ,
+              intTraderId ,
+              strInternalTradeNo ,
+              strBrokerTradeNo ,
+              strBuySell ,
+              intNoOfContract ,
+              dblPrice ,
+              strStatus ,
+              dtmFilledDate ,
+              strReserveForFix ,
+              intBookId ,
+              intSubBookId ,
+              ysnOffset,
+              intCurrencyId,
+              intConcurrencyId 
+               )    
+            VALUES(@dtmTransactionDate ,
+            @intFutOptTransactionHeaderId,
+              @intEntityId ,
+              @intBrokerageAccountId ,
+              @intFutureMarketId ,
+              @intFutureMonthId ,
+              @intInstrumentTypeId ,
+              @intCommodityId ,
+              @intLocationId ,
+              @intTraderId ,
+              @strinternalTradeNo ,
+              @strBrokerTradeNo ,
+              @strBuySell ,
+              @intNoOfContract ,
+              @dblPrice ,
+              @strStatus ,
+              @dtmFilledDate ,
+              @strReserveForFix ,
+              @intBookId ,
+              @intSubBookId ,
+              @ysnOffset  ,
+              @intCurrencyId,
+              1
+      )          
+
+      SET @intFutOptTransactionId = SCOPE_IDENTITY()
+END
+  
+END TRY    
+    
+BEGIN CATCH    
+ SET @ErrMsg = ERROR_MESSAGE()    
+ RAISERROR(@ErrMsg, 16, 1, 'WITH NOWAIT')    
+END CATCH    
+
+
+
---
+++ i21Database/dbo/Stored Procedures/uspRKDeleteAutoHedge.sql
@@ -0,0 +1,34 @@
+﻿CREATE PROCEDURE uspRKDeleteAutoHedge 
+      @intFutOptTransactionId int
+AS
+DECLARE @TransId int = 0
+DECLARE @ErrMsg nvarchar(max)
+BEGIN TRY
+IF EXISTS(SELECT * FROM tblRKMatchFuturesPSDetail WHERE intLFutOptTransactionId=@intFutOptTransactionId)
+BEGIN
+SET @TransId = 1
+END
+
+IF EXISTS(SELECT * FROM tblRKMatchFuturesPSDetail WHERE intSFutOptTransactionId=@intFutOptTransactionId)
+BEGIN
+      SET @TransId = 1
+END
+
+IF (@TransId = 1)
+BEGIN
+      RAISERROR('The selected transaction is used for match PnS.',16,1)
+END
+ELSE
+BEGIN
+      DELETE FROM tblRKFutOptTransaction WHERE intFutOptTransactionId=@intFutOptTransactionId 
+END
+
+END TRY
+BEGIN CATCH
+      SET @ErrMsg = ERROR_MESSAGE()    
+       IF XACT_STATE() != 0 ROLLBACK TRANSACTION    
+       IF @ErrMsg != ''     
+       BEGIN    
+            RAISERROR(@ErrMsg, 16, 1, 'WITH NOWAIT')    
+       END    
+END CATCH
