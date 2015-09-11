3261a7679f1ffb165b3e6063fbec4040b60d0cb5
Lex Eugene Peregrino <lex.peregrino@irely.com>
2015-09-12 04:54:11 +0800

    AP-1556

--- i21Database/dbo/Tables/tblAPBill.sql
+++ i21Database/dbo/Tables/tblAPBill.sql
@@ -57,6 +57,7 @@
 	[ysnForApproval] BIT NOT NULL DEFAULT 0,
     [ysnOrigin] BIT NOT NULL DEFAULT 0,
 	[ysnDeleted] BIT NULL DEFAULT 0 ,
+	[ysnReadyForPayment] BIT NULL DEFAULT 0 ,
 	[dtmDateDeleted] DATETIME NULL,
     [dtmDateCreated] DATETIME NULL DEFAULT GETDATE(), 
     CONSTRAINT [PK_dbo.tblAPBill] PRIMARY KEY CLUSTERED ([intBillId] ASC),
