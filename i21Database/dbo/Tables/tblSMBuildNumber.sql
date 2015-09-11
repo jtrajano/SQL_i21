e1e2a8ca70d084f7f8f0825787ffa09373f6fc46
Dan Michael Jalimao <dan.jalimao@irely.com>
2015-09-11 16:29:36 +0800

    FCOM-837

--- i21Database/dbo/Tables/tblSMBuildNumber.sql
+++ i21Database/dbo/Tables/tblSMBuildNumber.sql
@@ -2,6 +2,7 @@
     [intVersionID]  INT           IDENTITY (1, 1) NOT NULL,
     [strVersionNo]  NVARCHAR (30) COLLATE Latin1_General_CI_AS NOT NULL,
     [dtmLastUpdate] DATETIME      NOT NULL, 
-    [strStashCommitId] NVARCHAR (11) COLLATE Latin1_General_CI_AS NULL
+    [strStashCommitId] NVARCHAR (11) COLLATE Latin1_General_CI_AS NULL, 
+    CONSTRAINT [PK_tblSMBuildNumber] PRIMARY KEY ([intVersionID])
 );
 
