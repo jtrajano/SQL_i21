3d04d2768d353618d22ca207095a2f4179bacdd3
Dan Michael Jalimao <dan.jalimao@irely.com>
2015-09-14 11:08:26 +0800

    FCOM-839

--- i21Database/Scripts/Post-Deployment/SM/DefaultData/4_StartingNumbers.sql
+++ i21Database/Scripts/Post-Deployment/SM/DefaultData/4_StartingNumbers.sql
@@ -587,6 +587,16 @@ GO
 			,[ysnEnable]			= 1
 			,[intConcurrencyId]		= 1
 	WHERE NOT EXISTS (SELECT TOP 1 1 FROM tblSMStartingNumber WHERE strTransactionType = N'Schedule Number')
+	UNION ALL
+	SELECT	[intStartingNumberId]	= 64
+			,[strTransactionType]	= N'Comment Maintenance'
+			,[strPrefix]			= N'COM-'
+			,[intNumber]			= 1
+			,[strModule]			= 'Accounts Receivable'
+			,[ysnEnable]			= 1
+			,[intConcurrencyId]		= 1
+	WHERE NOT EXISTS (SELECT TOP 1 1 FROM tblSMStartingNumber WHERE strTransactionType = N'Comment Maintenance')
+	SET IDENTITY_INSERT [dbo].[tblSMStartingNumber] OFF
 	SET IDENTITY_INSERT [dbo].[tblSMStartingNumber] OFF
 GO
 	PRINT N'END INSERT DEFAULT STARTING NUMBERS'
