36e3751be4d3bd56ce30904c00d4fc598d29edfb
Crisman Lazaga <cris.lazaga@irely.com>
2015-09-15 10:40:20 +0800

    IU-51

---
+++ i21Database/dbo/Tables/tblIUPatchHistory.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[tblIUPatchHistory]
+(
+	[intPatchHistoryId] INT NOT NULL PRIMARY KEY, 
+    [intVersionId] INT NOT NULL, 
+    [strCommitId] NVARCHAR(11) NOT NULL, 
+	[strVersion] NVARCHAR(15) NOT NULL, 
+	[strUpdateType] NVARCHAR(15) NOT NULL,
+	[strChangeType] NVARCHAR(15) NULL,
+    [strFilePath] NVARCHAR(2000) NULL, 
+    [strFileName] NVARCHAR(500) NULL, 
+    CONSTRAINT [FK_tblIUPatchHistory_tblSMBuildNumber] FOREIGN KEY ([intVersionId]) REFERENCES [tblSMBuildNumber]([intVersionID])
+)
---
+++ i21Database/dbo/Views/vyuIUPatchHistory.sql
@@ -0,0 +1,16 @@
+﻿CREATE VIEW [dbo].[vyuIUPatchHistory]
+WITH SCHEMABINDING
+AS 
+SELECT	A.intPatchHistoryId, 
+	A.intVersionId, 
+	B.strVersionNo, 
+	B.dtmLastUpdate, 
+	B.strStashCommitId, 
+    A.strCommitId, 
+	A.strVersion, 
+	A.strUpdateType, 
+	A.strChangeType, 
+	A.strFilePath, 
+	A.strFileName
+FROM dbo.tblIUPatchHistory A INNER JOIN dbo.tblSMBuildNumber B 
+	ON A.intVersionId = B.intVersionID
