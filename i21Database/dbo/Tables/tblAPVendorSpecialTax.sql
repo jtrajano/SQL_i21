92bd77ec8ac6b6dd12b210465681da6ff476cf77
Mon Gonzales <mon.gonzales@irely.com>
2015-09-15 15:07:50 +0800

    EM-412

---
+++ i21Database/dbo/Tables/tblAPVendorSpecialTax.sql
@@ -0,0 +1,33 @@
+﻿CREATE TABLE [dbo].[tblAPVendorSpecialTax]
+(
+	[intAPVendorSpecialTaxId]				INT NOT NULL identity,
+
+	[intEntityVendorId]						INT NOT NULL,
+	
+	[intTaxEntityVendorId]					INT NULL,
+	
+	[intItemId]								INT NULL,
+	
+	[intCategoryId]							INT NULL,
+	
+	[intTaxGroupMasterId]					INT	NULL,
+	
+	[intEntityVendorLocationId]				INT	NULL,
+	
+	[intConcurrencyId]	INT	DEFAULT(0) NOT NULL,
+	
+	CONSTRAINT [PK_dbo_tblAPVendorSpecialTax]					PRIMARY KEY CLUSTERED ([intAPVendorSpecialTaxId] ASC),
+	
+	CONSTRAINT FK_tblAPVendorSpecialTax_tblAPVendor_Parent		FOREIGN KEY ([intEntityVendorId]) REFERENCES [tblAPVendor]([intEntityVendorId]),
+	
+	CONSTRAINT FK_tblAPVendorSpecialTax_tblAPVendor_Tax			FOREIGN KEY ([intTaxEntityVendorId]) REFERENCES [tblAPVendor]([intEntityVendorId]),
+	
+	CONSTRAINT FK_tblAPVendorSpecialTax_tbltblICItem			FOREIGN KEY ([intItemId]) REFERENCES [tblICItem]([intItemId]),
+	
+	CONSTRAINT FK_tblAPVendorSpecialTax_tblICCategory			FOREIGN KEY ([intCategoryId]) REFERENCES [tblICCategory]([intCategoryId]),
+	
+	CONSTRAINT FK_tblAPVendorSpecialTax_tblSMTaxGroupMaster		FOREIGN KEY ([intTaxGroupMasterId]) REFERENCES [tblSMTaxGroupMaster]([intTaxGroupMasterId]),
+	
+	CONSTRAINT FK_tblAPVendorSpecialTax_tblEntityLocation		FOREIGN KEY ([intEntityVendorLocationId]) REFERENCES [tblEntityLocation]([intEntityLocationId])
+
+)
---
+++ i21Database/dbo/Tables/tblAPVendorTaxException.sql
@@ -0,0 +1,32 @@
+﻿CREATE TABLE [dbo].[tblAPVendorTaxException]
+(
+	[intAPVendorTaxExceptionId]			INT             IDENTITY (1, 1) NOT NULL,    
+	
+	[intEntityVendorId]					INT             NOT NULL,
+    
+	[intItemId]							INT             NULL,
+	
+	[intCategoryId]						INT             NULL,
+	
+	[intTaxCodeId]						INT             NULL,
+	
+	[intTaxClassId]						INT             NULL,
+    
+	[strState]							NVARCHAR (50)   COLLATE Latin1_General_CI_AS NULL,
+	
+	[strException]						NVARCHAR (50)   COLLATE Latin1_General_CI_AS NULL,
+	
+	[dtmStartDate]						DATETIME		NULL,
+	
+	[dtmEndDate]						DATETIME		NULL,	
+	
+	[intEntityVendorLocationId]			INT				NULL,
+    
+	[intConcurrencyId]					INT			CONSTRAINT [DF_tblAPVendorTaxException_intConcurrencyId] DEFAULT ((0)) NOT NULL,
+
+    CONSTRAINT [PK_tblAPVendorTaxException]					PRIMARY KEY CLUSTERED ([intAPVendorTaxExceptionId] ASC),
+
+	CONSTRAINT [FK_tblAPVendorTaxException_tblAPVendor]		FOREIGN KEY ([intEntityVendorId]) REFERENCES [dbo].[tblAPVendor] ([intEntityVendorId]),
+
+	CONSTRAINT FK_ttblAPVendorTaxExceptiontblEntityLocation FOREIGN KEY (intEntityVendorLocationId) REFERENCES [tblEntityLocation]([intEntityLocationId])
+)
