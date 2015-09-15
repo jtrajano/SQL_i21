c2fd62622e812deeaaae065b79e5440959c35e10
Kelvin Quinto <kelvin.quinto@irely.com>
2015-09-15 22:08:05 +0800

    AR-1757

--- i21Database/dbo/Functions/fnARGetInvoiceFooterComment.sql
+++ i21Database/dbo/Functions/fnARGetInvoiceFooterComment.sql
@@ -15,8 +15,29 @@ BEGIN
 			FROM tblARCommentMaintenance
 			WHERE strTransactionType = @strTransactionType
 			  AND ((@intCompanyLocationId IS NULL OR @intCompanyLocationId = 0) OR intCompanyLocationId = @intCompanyLocationId)
-			  AND ((@intEntityCustomerId IS NULL OR @intEntityCustomerId = 0) OR intEntityCustomerId = @intEntityCustomerId)				  
+			  AND ((@intEntityCustomerId IS NULL OR @intEntityCustomerId = 0) OR intEntityCustomerId = @intEntityCustomerId)
 			ORDER BY intCommentId DESC
+
+			IF (@footerComment IS NULL)
+				BEGIN
+					SELECT TOP 1 @footerComment = strCommentDesc
+					FROM tblARCommentMaintenance
+					WHERE strTransactionType = @strTransactionType
+					  AND ((@intCompanyLocationId IS NULL OR @intCompanyLocationId = 0) OR intCompanyLocationId = @intCompanyLocationId)					  
+					ORDER BY intCommentId DESC
+				END
+			ELSE
+				RETURN @footerComment
+
+			IF (@footerComment IS NULL)
+				BEGIN
+					SELECT TOP 1 @footerComment = strCommentDesc
+					FROM tblARCommentMaintenance
+					WHERE strTransactionType = @strTransactionType					  
+					ORDER BY intCommentId DESC
+				END
+			ELSE
+				RETURN @footerComment
 		END
 
 	RETURN @footerComment	
