88ec0fadb8a573522d94fd54e9bee37ebf7065b5
Smith Wilson de Jesus <smith.dejesus@irely.com>
2015-09-16 14:46:59 +0800

    CM-857 Add columns to existing view of unposted transaction for CM

--- i21Database/dbo/Views/vyuCMUnpostedTransaction.sql
+++ i21Database/dbo/Views/vyuCMUnpostedTransaction.sql
@@ -6,12 +6,18 @@ intTransactionId
 ,strTransactionId
 ,strTransactionType = (SELECT strBankTransactionTypeName FROM tblCMBankTransactionType WHERE intBankTransactionTypeId = BT.intBankTransactionTypeId)
 ,dtmDate
+,strMemo as strDescription
+,strUserName = (SELECT strName from tblEntity where intEntityId = BT.intEntityId)
+,intEntityId
 FROM tblCMBankTransaction BT
 WHERE ISNULL(ysnPosted,0) = 0
 UNION SELECT 
 intTransactionId,
 strTransactionId,
 strTransactionType = (SELECT strBankTransactionTypeName FROM tblCMBankTransactionType WHERE intBankTransactionTypeId = BTransfer.intBankTransactionTypeId),
-dtmDate
+dtmDate,
+strDescription,
+strUserName = (SELECT strName from tblEntity where intEntityId = BTransfer.intEntityId),
+intEntityId
 FROM tblCMBankTransfer BTransfer
 WHERE        (ISNULL(ysnPosted, 0) = 0)
