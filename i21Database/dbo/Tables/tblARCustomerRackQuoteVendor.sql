2bc54be71919162ce2c3d4233663729ef9f53fe5
Mon Gonzales <mon.gonzales@irely.com>
2015-09-17 13:58:40 +0800

    EM-498

--- i21Database/dbo/Tables/tblARCustomerRackQuoteVendor.sql
+++ i21Database/dbo/Tables/tblARCustomerRackQuoteVendor.sql
@@ -2,9 +2,11 @@
 (
 	[intCustomerRackQuoteVendorId]			INT			IDENTITY (1, 1) NOT NULL,
 	[intCustomerRackQuoteHeaderId]			INT			NOT NULL,
-	[intSupplyPointId]						INT         NOT NULL,
+	[intSupplyPointId]						INT         NULL,
 	[ysnQuote]								BIT         NULL,	
 	[intConcurrencyId]						INT         CONSTRAINT [DF_tblARCustomerRackQuoteVendor_intConcurrencyId] DEFAULT ((0)) NOT NULL,
 	CONSTRAINT [PK_tblARCustomerRackQuoteVendor] PRIMARY KEY CLUSTERED ([intCustomerRackQuoteVendorId] ASC),
-	CONSTRAINT [FK_tblARCustomerRackQuoteHeader_tblARCustomerRackQuoteHeader] FOREIGN KEY ([intCustomerRackQuoteHeaderId]) REFERENCES [dbo].[tblARCustomerRackQuoteHeader] ([intCustomerRackQuoteHeaderId]) ON DELETE CASCADE
+	CONSTRAINT [FK_tblARCustomerRackQuoteHeader_tblARCustomerRackQuoteHeader] FOREIGN KEY ([intCustomerRackQuoteHeaderId]) REFERENCES [dbo].[tblARCustomerRackQuoteHeader] ([intCustomerRackQuoteHeaderId]) ON DELETE CASCADE,
+	CONSTRAINT [FK_tblARCustomerRackQuoteHeader_tblTRSupplyPoint] FOREIGN KEY ([intSupplyPointId]) REFERENCES [dbo].[tblTRSupplyPoint] ([intSupplyPointId]),
+
 )
