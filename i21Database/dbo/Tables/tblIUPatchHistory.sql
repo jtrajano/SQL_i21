ce5da9249c72473a93a3d63af3bf9fb821bea9ad
Crisman Lazaga <cris.lazaga@irely.com>
2015-09-17 08:31:40 +0800

    IU-51

--- i21Database/dbo/Tables/tblIUPatchHistory.sql
+++ i21Database/dbo/Tables/tblIUPatchHistory.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[tblIUPatchHistory]
 (
-	[intPatchHistoryId] INT NOT NULL PRIMARY KEY IDENTITY, 
+	[intPatchHistoryId] INT NOT NULL  IDENTITY, 
     [intVersionId] INT NOT NULL, 
     [strCommitId] NVARCHAR(11) COLLATE Latin1_General_CI_AS NOT NULL, 
 	[strVersion] NVARCHAR(15) COLLATE Latin1_General_CI_AS NOT NULL, 
@@ -8,5 +8,6 @@
     [strFilePath] NVARCHAR(2000) COLLATE Latin1_General_CI_AS NULL, 
     [strFileName] NVARCHAR(500) COLLATE Latin1_General_CI_AS NULL, 
     [intConcurrencyId] INT NULL, 
-    CONSTRAINT [FK_tblIUPatchHistory_tblSMBuildNumber] FOREIGN KEY ([intVersionId]) REFERENCES [tblSMBuildNumber]([intVersionID])
+    CONSTRAINT [FK_tblIUPatchHistory_tblSMBuildNumber] FOREIGN KEY ([intVersionId]) REFERENCES [tblSMBuildNumber]([intVersionID]), 
+    CONSTRAINT [PK_tblIUPatchHistory] PRIMARY KEY ([intPatchHistoryId])
 )
