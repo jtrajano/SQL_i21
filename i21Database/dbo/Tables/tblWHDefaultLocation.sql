cd56ae11c677b79afcbb17641328732747944d78
Swetosuvro <swetosuvro.g@irely.com>
2015-09-18 17:05:03 +0530

    WH-35

---
+++ i21Database/dbo/Tables/tblWHDefaultLocation.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[tblWHDefaultLocation]
+(
+	intEntityId INT,
+	intDefaultInboundStagingLocationId INT,
+	intDefaultOutboundStagingLocationId INT,
+	intDefaultInboundDockDoorLocationId INT,
+	intDefaultOutboundDockDoorLocationId INT
+)
