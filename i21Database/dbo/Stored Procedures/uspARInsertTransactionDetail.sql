ae256250129d03bdaa9716bed991bc32962ccf32
Ruben  Escarcha <ruben.escarcha@irely.com>
2015-09-19 05:15:21 +0800

    AR-1649 Calling of uspCTUpdateScheduleQuantity sp in invoice save(edit mode) only when there is a change in quantity.

---
+++ i21Database/dbo/Stored Procedures/uspARInsertTransactionDetail.sql
@@ -0,0 +1,42 @@
+﻿CREATE PROCEDURE [dbo].[uspARInsertTransactionDetail]
+	@InvoiceId	INT
+AS
+BEGIN
+
+	DELETE FROM [tblARTransactionDetail] WHERE [intTransactionId] = @InvoiceId AND [strTransactionType] = 'Invoice'
+
+	INSERT INTO [tblARTransactionDetail]
+	(
+		 [intTransactionDetailId]
+		,[intTransactionId]
+		,[strTransactionType]
+		,[intItemId]
+		,[intItemUOMId]
+		,[dblQtyOrdered]
+		,[dblQtyShipped]
+		,[dblPrice]
+		,[intInventoryShipmentItemId]
+		,[intSalesOrderDetailId]
+		,[intContractHeaderId]
+		,[intContractDetailId]
+		,[intConcurrencyId]
+	)
+	SELECT
+		 [intTransactionDetailId]		= [intInvoiceDetailId] 
+		,[intTransactionId]				= [intInvoiceId] 
+		,[strTransactionType]			= 'Invoice'
+		,[intItemId]					= [intItemId] 
+		,[intItemUOMId]					= [intItemUOMId] 
+		,[dblQtyOrdered]				= [dblQtyOrdered] 
+		,[dblQtyShipped]				= [dblQtyShipped] 
+		,[dblPrice]						= [dblPrice]
+		,[intInventoryShipmentItemId]	= [intInventoryShipmentItemId]
+		,[intSalesOrderDetailId]		= [intSalesOrderDetailId]
+		,[intContractHeaderId]			= [intContractHeaderId]
+		,[intContractDetailId]			= [intContractDetailId]
+		,[intConcurrencyId]				= 1
+	FROM
+		[tblARInvoiceDetail]
+	WHERE
+		[intInvoiceId] = @InvoiceId
+END
