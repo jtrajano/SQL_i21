f27cb310a2f48c9f5a9ca66f510407f91246d611
Renz Oliver Medina <renz.medina@irely.com>
2015-09-18 15:58:00 +0800

    FRM-2526

---
+++ i21Database/dbo/Tables/tblSMCompanyGridLayout.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[tblSMCompanyGridLayout]
+(
+	[intCompanyGridLayoutId] INT IDENTITY (1, 1) NOT NULL, 
+    [strGridLayoutName] NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL, 
+	[strGridLayoutFields] NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NULL, 
+	[strGridLayoutFilters] NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NULL, 
+    [strScreen] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
+    [strGrid] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
+    [ysnActive] BIT NULL, 
+    [intConcurrencyId] INT NOT NULL, 
+    CONSTRAINT [PK_tblSMCompanyGridLayout] PRIMARY KEY CLUSTERED ([intCompanyGridLayoutId] ASC) 
+)
