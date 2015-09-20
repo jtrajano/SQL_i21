bafa796e09fb2e151d7e0fa5f9d5b94b3e6c4515
Lawrence Zabala <lawrence.zabala@irely.com>
2015-09-20 21:26:37 +0800

    no message

---
+++ i21Database/dbo/Views/vyuICGetInventoryValuation.sql
@@ -0,0 +1,31 @@
+﻿CREATE VIEW [dbo].[vyuICGetInventoryValuation]
+	AS
+
+SELECT TOP 100 PERCENT
+	intInventoryValuationKeyId = CAST(ROW_NUMBER() OVER(ORDER BY [Transaction].intItemId, [Transaction].dtmDate DESC, [Transaction].intTransactionId DESC) AS INT),
+	[Transaction].intItemId,
+	strItemNo = Item.strItemNo,
+	strItemDescription = Item.strDescription,
+	Item.intCategoryId,
+	strCategroy = Category.strCategoryCode,
+	[Transaction].intItemLocationId,
+	Location.strLocationName,
+	[Transaction].intSubLocationId,
+	SubLocation.strSubLocationName,
+	[Transaction].intStorageLocationId,
+	strStorageLocationName = StorageLocation.strName,
+	[Transaction].dtmDate,
+	[Transaction].strTransactionForm,
+	[Transaction].strTransactionId,
+	dblQuantity = [Transaction].dblQty * [Transaction].dblUOMQty,
+	dblCost = [Transaction].dblCost,
+	dblValue = [Transaction].dblQty * [Transaction].dblUOMQty * [Transaction].dblCost,
+	strBatchId
+FROM tblICInventoryTransaction [Transaction]
+LEFT JOIN tblICItem Item ON Item.intItemId = [Transaction].intItemId
+LEFT JOIN tblICCategory Category ON Category.intCategoryId = Item.intCategoryId
+LEFT JOIN tblICItemLocation ItemLocation ON ItemLocation.intItemLocationId = [Transaction].intItemLocationId
+LEFT JOIN tblSMCompanyLocation Location ON Location.intCompanyLocationId = ItemLocation.intLocationId
+LEFT JOIN tblSMCompanyLocationSubLocation SubLocation ON SubLocation.intCompanyLocationSubLocationId = [Transaction].intSubLocationId
+LEFT JOIN tblICStorageLocation StorageLocation ON StorageLocation.intStorageLocationId = [Transaction].intStorageLocationId
+ORDER BY [Transaction].intItemId, [Transaction].dtmDate DESC, [Transaction].intTransactionId DESC
