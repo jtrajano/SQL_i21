5ff29399966f63ce4ffa14809b27320db7616f3f
Melanie Ferrer <Mel.Ferrer@irely.com>
2015-09-21 17:10:47 +0800

    PAT-3 - Stock Classification changes

--- i21Database/dbo/Tables/tblPATStockClassification.sql
+++ i21Database/dbo/Tables/tblPATStockClassification.sql
@@ -1,12 +1,11 @@
 ﻿CREATE TABLE [dbo].[tblPATStockClassification]
 (
 	[intStockId] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [strStockName] NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL, 
-    [strStockDescription] NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NULL, 
+    [strStockName] NVARCHAR(100) COLLATE Latin1_General_CI_AS NOT NULL, 
+    [strStockDescription] NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NOT NULL, 
     [dblParValue] NUMERIC(18, 6) NOT NULL DEFAULT 0, 
-    [dblNextCertificate] NUMERIC(18, 6) NOT NULL DEFAULT 0, 
-    [intDividendsGLAccount] INT NULL DEFAULT 0, 
-    [intTreasuryGLAccount] INT NULL DEFAULT 0, 
+    [intDividendsGLAccount] INT NOT NULL DEFAULT 0, 
+    [intTreasuryGLAccount] INT NOT NULL DEFAULT 0, 
     [intDividendsPerShare] INT NULL DEFAULT 0, 
     [intSort] INT NULL, 
     [intConcurrencyId] INT NULL DEFAULT 1
