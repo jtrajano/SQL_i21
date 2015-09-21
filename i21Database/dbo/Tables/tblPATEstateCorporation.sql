9566fc281d24e28615c4a0f9316aa68093b4b15f
Melanie Ferrer <Mel.Ferrer@irely.com>
2015-09-21 09:53:16 +0800

    PAT-11 - Collation issue

--- i21Database/dbo/Tables/tblPATEstateCorporation.sql
+++ i21Database/dbo/Tables/tblPATEstateCorporation.sql
@@ -2,11 +2,11 @@
 (
 	[intCorporateCustomerId] INT NOT NULL , 
     [intRefundType] INT NOT NULL, 
-    [strCorporateCustomerId] NVARCHAR(50) NULL, 
+    [strCorporateCustomerId] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
     [dblOwnerPercentage] NUMERIC(18, 6) NOT NULL DEFAULT 0, 
     [dtmPaidDate] DATETIME NULL, 
     [dblPaidAmount] NUMERIC(18, 6) NOT NULL DEFAULT 0, 
-    [strPaidCheckNo] CHAR(8) NULL, 
+    [strPaidCheckNo] CHAR(8) COLLATE Latin1_General_CI_AS  NULL, 
     [dtmBirthDate] DATETIME NULL, 
     [intConcurrencyId] INT NOT NULL DEFAULT 1, 
     PRIMARY KEY ([intCorporateCustomerId], [intRefundType])
--- i21Database/dbo/Tables/tblPATStockClassification.sql
+++ i21Database/dbo/Tables/tblPATStockClassification.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [dbo].[tblPATStockClassification]
 (
 	[intStockId] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [strStockName] NVARCHAR(100) NULL, 
-    [strStockDescription] NVARCHAR(MAX) NULL, 
+    [strStockName] NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL, 
+    [strStockDescription] NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NULL, 
     [dblParValue] NUMERIC(18, 6) NOT NULL DEFAULT 0, 
     [dblNextCertificate] NUMERIC(18, 6) NOT NULL DEFAULT 0, 
     [intDividendsGLAccount] INT NULL DEFAULT 0, 
