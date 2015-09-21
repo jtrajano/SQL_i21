0742324765b255a6e13b9e0e598b7d799240d3de
Ian Ocampo <ian.ocampo@irely.com>
2015-09-21 18:47:33 +0800

    FRM-2496

---
+++ i21Database/dbo/Tables/tblSMCommentWatcher.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[tblSMCommentWatcher](
+	[intCommentWatcherId] [int] IDENTITY(1,1) NOT NULL,
+	[intEntityId] [int] NULL,
+	[strScreen] [nvarchar](50) COLLATE Latin1_General_CI_AS NULL,
+	[strRecordNo] [nvarchar](50) COLLATE Latin1_General_CI_AS NULL,
+	[ysnWatched] [bit] NULL,
+	[intConcurrencyId] [int] NOT NULL, 
+    CONSTRAINT [PK_tblSMCommentWatcher] PRIMARY KEY ([intCommentWatcherId])
+)
