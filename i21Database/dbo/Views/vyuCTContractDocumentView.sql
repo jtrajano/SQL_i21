5373c710a668b049019271da092e6831e1875207
James Balachander <balachander.j@irely.com>
2015-09-21 19:46:24 +0530

    LG-161

---
+++ i21Database/dbo/Views/vyuCTContractDocumentView.sql
@@ -0,0 +1,14 @@
+﻿CREATE VIEW [dbo].[vyuCTContractDocumentView]
+	
+AS 
+	SELECT	CD.intContractDocumentId,
+			CD.intContractHeaderId,
+			CD.intDocumentId,
+			ID.strDocumentName,
+			ID.strDescription,
+			ID.ysnStandard,
+			ID.intCommodityId
+
+	FROM	tblCTContractDocument	CD
+	JOIN	tblCTContractHeader CH	ON	CH.intContractHeaderId	=	CD.intContractHeaderId
+	JOIN	tblICDocument		ID	ON	ID.intDocumentId		=	CD.intDocumentId
