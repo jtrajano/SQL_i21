3b329fc730345ea5c0bce517719e57a725967924
Lawrence Zabala <lawrence.zabala@irely.com>
2015-09-22 13:31:26 +0800

    IC-1309

---
+++ i21Database/dbo/Views/vyuICGetInventoryReceiptItemTax.sql
@@ -0,0 +1,26 @@
+﻿CREATE VIEW [dbo].[vyuICGetInventoryReceiptItemTax]
+	AS
+
+SELECT ReceiptItemTax.intInventoryReceiptItemTaxId,
+	ReceiptItemTax.intInventoryReceiptItemId,
+	ReceiptItem.intItemId,
+	Item.strItemNo,
+	strItemDescription = Item.strDescription,
+	ReceiptItemTax.intTaxGroupMasterId,
+	ReceiptItemTax.intTaxGroupId,
+	ReceiptItemTax.intTaxCodeId,
+	ReceiptItemTax.intTaxClassId,
+	ReceiptItemTax.strTaxableByOtherTaxes,
+	ReceiptItemTax.strCalculationMethod,
+	ReceiptItemTax.dblRate,
+	ReceiptItemTax.dblTax,
+	ReceiptItemTax.dblAdjustedTax,
+	ReceiptItemTax.intTaxAccountId,
+	ReceiptItemTax.ysnTaxAdjusted,
+	ReceiptItemTax.ysnSeparateOnInvoice,
+	ReceiptItemTax.ysnCheckoffTax,
+	ReceiptItemTax.strTaxCode,
+	ReceiptItemTax.intSort
+FROM tblICInventoryReceiptItemTax ReceiptItemTax
+	LEFT JOIN tblICInventoryReceiptItem ReceiptItem ON ReceiptItem.intInventoryReceiptItemId = ReceiptItemTax.intInventoryReceiptItemId
+	LEFT JOIN tblICItem Item ON Item.intItemId = ReceiptItem.intItemId
