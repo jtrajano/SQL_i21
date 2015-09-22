2ab383e6bb3ada7fe57778da4a93a5abea1d2cf2
Crisman Lazaga <cris.lazaga@irely.com>
2015-09-22 17:25:23 +0800

    IU-51

---
+++ i21Database/dbo/Tables/tblIUPatchDetail.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[tblIUPatchDetail]
+(
+	[intPatchDetailId] INT NOT NULL IDENTITY, 
+    [intVersionId] INT NOT NULL, 
+    [strCommitId] NVARCHAR(11) NOT NULL, 
+    [strVersion] NVARCHAR(15) NOT NULL, 
+    [strChangeType] NVARCHAR(15) NULL, 
+    [strFilePath] NVARCHAR(2000) NULL, 
+    [strFileName] NVARCHAR(500) NULL, 
+    [intConcurrencyId] INT NULL, 
+    CONSTRAINT [PK_tblIUPatchDetail] PRIMARY KEY ([intPatchDetailId]), 
+    CONSTRAINT [FK_tblIUPatchDetail_tblSMBuildNumber] FOREIGN KEY ([intVersionId]) REFERENCES [tblSMBuildNumber]([intVersionID]) 
+)
