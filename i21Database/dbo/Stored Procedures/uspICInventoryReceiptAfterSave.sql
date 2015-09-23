e3a7263fac4525c56b9d4ab62b8a38fff7989eac
Lawrence Zabala <lawrence.zabala@irely.com>
2015-09-23 08:18:30 +0800

    IC-1344 IC-1127

---
+++ i21Database/dbo/Stored Procedures/uspICInventoryReceiptAfterSave.sql
@@ -0,0 +1,102 @@
+﻿CREATE PROCEDURE [dbo].[uspICInventoryReceiptAfterSave]
+	@ReceiptId INT,
+	@ForDelete BIT = 0,
+	@UserId INT = NULL
+
+AS
+
+	SET QUOTED_IDENTIFIER OFF  
+	SET ANSI_NULLS ON  
+	SET NOCOUNT ON  
+	SET XACT_ABORT ON  
+	SET ANSI_WARNINGS OFF  
+
+BEGIN
+	
+	DECLARE @ReceiptType AS INT
+
+	DECLARE @ReceiptType_PurchaseContract AS INT = 1
+	DECLARE @ReceiptType_PurchaseOrder AS INT = 2
+	DECLARE @ReceiptType_TransferOrder AS INT = 3
+	DECLARE @ReceiptType_Direct AS INT = 4
+
+	DECLARE @SourceType_None AS INT = 0
+	DECLARE @SourceType_Scale AS INT = 1
+	DECLARE @SourceType_InboundShipment AS INT = 2
+	DECLARE @SourceType_Transport AS INT = 3
+
+	SELECT @ReceiptType = (
+		CASE WHEN strReceiptType = 'Purchase Contract' THEN @ReceiptType_PurchaseContract
+			WHEN strReceiptType = 'Purchase Order' THEN @ReceiptType_PurchaseOrder
+			WHEN strReceiptType = 'Transfer Order' THEN @ReceiptType_TransferOrder
+			WHEN strReceiptType = 'Direct' THEN @ReceiptType_Direct
+		END) FROM tblICInventoryReceipt
+	WHERE intInventoryReceiptId = @ReceiptId
+	
+	IF (@ReceiptType = @ReceiptType_PurchaseContract)
+	BEGIN
+		-- Create current snapshot of Receipt Items after Save
+		SELECT
+			ReceiptItem.intInventoryReceiptId,
+			ReceiptItem.intInventoryReceiptItemId,
+			intOrderType = (
+				CASE WHEN strReceiptType = 'Purchase Contract' THEN @ReceiptType_PurchaseContract
+					WHEN strReceiptType = 'Purchase Order' THEN @ReceiptType_PurchaseOrder
+					WHEN strReceiptType = 'Transfer Order' THEN @ReceiptType_TransferOrder
+					WHEN strReceiptType = 'Direct' THEN @ReceiptType_Direct
+				END),
+			ReceiptItem.intOrderId,
+			Receipt.intSourceType,
+			ReceiptItem.intSourceId,
+			ReceiptItem.intLineNo,
+			ReceiptItem.intItemId,
+			intItemUOMId = ReceiptItem.intUnitMeasureId,
+			ReceiptItem.dblOpenReceive
+		INTO #tmpReceiptItems
+		FROM tblICInventoryReceiptItem ReceiptItem
+			LEFT JOIN tblICInventoryReceipt Receipt ON Receipt.intInventoryReceiptId = ReceiptItem.intInventoryReceiptId
+		WHERE ReceiptItem.intInventoryReceiptId = @ReceiptId
+		-- Create snapshot of Receipt Items before Save
+		SELECT 
+			intInventoryReceiptId = intTransactionId,
+			intInventoryReceiptItemId = intTransactionDetailId,
+			intOrderType,
+			intOrderId = intOrderNumberId,
+			intSourceType,
+			intSourceId = intSourceNumberId,
+			intLineNo,
+			intItemId,
+			intItemUOMId,
+			dblOpenReceive = dblQuantity
+		INTO #tmpLogReceiptItems
+		FROM tblICTransactionDetailLog
+		WHERE intTransactionId = @ReceiptId
+			AND strTransactionType = 'Inventory Receipt'
+
+		-- Changed Quantity/UOM
+		SELECT 
+			 currentSnapshot.intInventoryReceiptItemId,
+			 currentSnapshot.intLineNo,
+			 currentSnapshot.intItemUOMId,
+			 dbo.fnCalculateQtyBetweenUOM(currentSnapshot.intItemUOMId, ContractDetail.intItemUOMId, (CASE WHEN @ForDelete = 1 THEN currentSnapshot.dblOpenReceive ELSE (currentSnapshot.dblOpenReceive - previousSnapshot.dblOpenReceive) END))
+		FROM #tmpReceiptItems currentSnapshot
+		INNER JOIN #tmpLogReceiptItems previousSnapshot
+			ON previousSnapshot.intInventoryReceiptId = currentSnapshot.intInventoryReceiptId
+			AND previousSnapshot.intInventoryReceiptItemId = currentSnapshot.intInventoryReceiptItemId
+		INNER JOIN tblCTContractDetail ContractDetail
+			ON ContractDetail.intContractDetailId = currentSnapshot.intLineNo
+		WHERE
+			currentSnapshot.intLineNo IS NOT NULL
+			AND currentSnapshot.intLineNo = previousSnapshot.intLineNo
+			AND currentSnapshot.intItemId = previousSnapshot.intItemId		
+			AND (currentSnapshot.intItemUOMId <> previousSnapshot.intItemUOMId OR currentSnapshot.dblOpenReceive <> previousSnapshot.dblOpenReceive)
+
+
+
+		DROP TABLE #tmpLogReceiptItems
+		DROP TABLE #tmpReceiptItems
+	END
+
+	DELETE FROM tblICTransactionDetailLog WHERE intTransactionId = @ReceiptId AND strTransactionType = 'Inventory Receipt'
+
+END
--- i21Database/dbo/Stored Procedures/uspICLogTransactionDetail.sql
+++ i21Database/dbo/Stored Procedures/uspICLogTransactionDetail.sql
@@ -2,36 +2,67 @@
 	@TransactionType int,
 	@TransactionId int
 AS
+	SET QUOTED_IDENTIFIER OFF  
+	SET ANSI_NULLS ON  
+	SET NOCOUNT ON  
+	SET XACT_ABORT ON  
+	SET ANSI_WARNINGS OFF  
+
 BEGIN
+
 	DECLARE @TransactionType_Receipt AS INT = 1
 	DECLARE @TransactionType_Shipment AS INT = 2
 	DECLARE @TransactionType_Transfer AS INT = 3
+
+	DECLARE @OrderType_PurchaseContract AS INT = 1
+	DECLARE @OrderType_SalesContract AS INT = 1
+	DECLARE @OrderType_PurchaseOrder AS INT = 2
+	DECLARE @OrderType_SalesOrder AS INT = 2
+	DECLARE @OrderType_TransferOrder AS INT = 3
+	DECLARE @OrderType_Direct AS INT = 4
+
+	DECLARE @SourceType_None AS INT = 0
+	DECLARE @SourceType_Scale AS INT = 1
+	DECLARE @SourceType_InboundShipment AS INT = 2
+	DECLARE @SourceType_Transport AS INT = 3
 
 	IF (@TransactionType = @TransactionType_Receipt)
 	BEGIN
 		IF EXISTS(SELECT TOP 1 1 FROM tblICInventoryReceipt WHERE intInventoryReceiptId = @TransactionId)
 		BEGIN
+			DELETE FROM tblICTransactionDetailLog WHERE strTransactionType = @TransactionType AND intTransactionId = @TransactionId
+
 			INSERT INTO tblICTransactionDetailLog(
-				strTransactionType.
+				strTransactionType,
 				intTransactionId, 
 				intTransactionDetailId,
 				intOrderNumberId,
+				intOrderType,
 				intSourceNumberId,
+				intSourceType,
 				intLineNo,
 				intItemId,
 				intItemUOMId,
 				dblQuantity)
-			SELECT 'Inventory Receipt'.
-				intInventoryReceiptId, 
-				intInventoryReceiptItemId,
-				intOrderId,
-				intSourceId,
-				intLineNo,
-				intItemId,
-				intUnitMeasureId,
-				dblOpenReceive
-			FROM tblICInventoryReceiptItem
-			WHERE intInventoryReceiptId = @TransactionId
+			SELECT 'Inventory Receipt',
+				ReceiptItem.intInventoryReceiptId, 
+				ReceiptItem.intInventoryReceiptItemId,
+				ReceiptItem.intOrderId,
+				intOrderType = (
+					CASE WHEN Receipt.strReceiptType = 'Purchase Contract' THEN @OrderType_PurchaseContract
+						WHEN Receipt.strReceiptType = 'Purchase Order' THEN @OrderType_PurchaseOrder
+						WHEN Receipt.strReceiptType = 'Transfer Order' THEN @OrderType_TransferOrder
+						WHEN Receipt.strReceiptType = 'Direct' THEN @OrderType_Direct
+					END), 
+				ReceiptItem.intSourceId,
+				intSourceType = Receipt.intSourceType,
+				ReceiptItem.intLineNo,
+				ReceiptItem.intItemId,
+				ReceiptItem.intUnitMeasureId,
+				ReceiptItem.dblOpenReceive
+			FROM tblICInventoryReceiptItem ReceiptItem
+				LEFT JOIN tblICInventoryReceipt Receipt ON Receipt.intInventoryReceiptId = ReceiptItem.intInventoryReceiptId
+			WHERE ReceiptItem.intInventoryReceiptId = @TransactionId
 		END
 		ELSE
 		BEGIN
@@ -41,29 +72,34 @@ BEGIN
 	END
 	ELSE IF (@TransactionType = @TransactionType_Shipment)
 	BEGIN
-		IF EXISTS(SELECT TOP 1 1 FROM tblICInventoryReceipt WHERE intInventoryReceiptId = @TransactionId)
+		IF EXISTS(SELECT TOP 1 1 FROM tblICInventoryShipment WHERE intInventoryShipmentId = @TransactionId)
 		BEGIN
 			INSERT INTO tblICTransactionDetailLog(
-				strTransactionType.
+				strTransactionType,
 				intTransactionId, 
 				intTransactionDetailId,
 				intOrderNumberId,
+				intOrderType,
 				intSourceNumberId,
+				intSourceType,
 				intLineNo,
 				intItemId,
 				intItemUOMId,
 				dblQuantity)
-			SELECT 'Inventory Shipment'.
-				intInventoryShipmentId, 
-				intInventoryShipmentItemId,
-				intOrderId,
-				intSourceId,
-				intLineNo,
-				intItemId,
-				intItemUOMId,
-				dblQuantity
-			FROM tblICInventoryShipmentItem
-			WHERE intInventoryShipmentId = @TransactionId
+			SELECT 'Inventory Shipment',
+				ShipmentItem.intInventoryShipmentId, 
+				ShipmentItem.intInventoryShipmentItemId,
+				ShipmentItem.intOrderId,
+				Shipment.intOrderType,
+				ShipmentItem.intSourceId,
+				Shipment.intSourceType,
+				ShipmentItem.intLineNo,
+				ShipmentItem.intItemId,
+				ShipmentItem.intItemUOMId,
+				ShipmentItem.dblQuantity
+			FROM tblICInventoryShipmentItem ShipmentItem
+				LEFT JOIN tblICInventoryShipment Shipment ON Shipment.intInventoryShipmentId = ShipmentItem.intInventoryShipmentId
+			WHERE ShipmentItem.intInventoryShipmentId = @TransactionId
 		END
 		ELSE
 		BEGIN
