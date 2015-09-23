aae7c120a56d45db6b12595dadce6da7e8f954b6
jeffrey trajano <jeffrey.trajano@irely.com>
2015-09-23 15:06:12 +0800

    GL-1668 Restrict journal entries to the currency noted in the transaction

--- i21Database/Scripts/Post-Deployment/PostDeployment.sql
+++ i21Database/Scripts/Post-Deployment/PostDeployment.sql
@@ -126,6 +126,7 @@ print 'BEGIN POST DEPLOYMENT'
 :r .\GL\DefaultData\6_AccountRange.sql
 :r .\GL\DefaultData\6a_RemoveCOGSales_AccountRange.sql
 :r .\GL\DefaultData\7_FiscalYearPeriod.sql
+:r .\GL\DefaultData\8_AccountCurrencyID.sql
 :r .\GL\GLEntryDataFix.sql
 :r .\GL\ReportData\GeneralLedgerByAccountDetail.sql
 :r .\GL\ReportData\IncomeStatementStandard.sql
