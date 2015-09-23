2f232e879ccad57645423fd434e21cb181f5bd4d
Kelvin Quinto <kelvin.quinto@irely.com>
2015-09-23 20:32:45 +0800

    AR-1798

---
+++ i21Database/dbo/Tables/tblARImportLog.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[tblARImportLog]
+(
+	[intImportLogId]	INT NOT NULL  IDENTITY, 
+    [strEventMessage]	NVARCHAR(255) COLLATE Latin1_General_CI_AS NULL,
+    [strVersion]		NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL,
+	[dtmDate]			DATETIME NULL,
+    [intEntityId]		INT NULL,
+	[intSuccessCount]   INT NULL,
+	[intFailedCount]    INT NULL,
+    [intConcurrencyId]	INT NOT NULL DEFAULT ((0)), 
+    CONSTRAINT [PK_tblARImportLog_intImportLogId] PRIMARY KEY CLUSTERED ([intImportLogId] ASC)
+)
