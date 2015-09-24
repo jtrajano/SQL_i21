bdb43e15e70d493985215e15d5efa3ff8f9ae91b
jeffrey trajano <jeffrey.trajano@irely.com>
2015-09-24 12:46:43 +0800

    GL-2150 Cannot Clone Account

--- i21Database/Scripts/Post-Deployment/PostDeployment.sql
+++ i21Database/Scripts/Post-Deployment/PostDeployment.sql
@@ -127,6 +127,7 @@ print 'BEGIN POST DEPLOYMENT'
 :r .\GL\DefaultData\6a_RemoveCOGSales_AccountRange.sql
 :r .\GL\DefaultData\7_FiscalYearPeriod.sql
 :r .\GL\DefaultData\8_AccountCurrency.sql
+:r .\GL\DefaultData\9_UpdateAccountSegmentWithNoMatchingCategory.sql
 :r .\GL\GLEntryDataFix.sql
 :r .\GL\ReportData\GeneralLedgerByAccountDetail.sql
 :r .\GL\ReportData\IncomeStatementStandard.sql
