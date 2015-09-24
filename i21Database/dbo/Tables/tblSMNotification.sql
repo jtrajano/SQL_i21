2aa07204225960bfcfb8c172861e756fa996a6e9
von.pasion <von.pasion@irely.com>
2015-09-24 10:33:27 +0800

    FRM-2495

---
+++ i21Database/dbo/Tables/tblSMNotification.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[tblSMNotification] (
+    [intNotificationId] INT              IDENTITY (1, 1) NOT NULL,
+    [strMessage]        NVARCHAR (MAX)   COLLATE Latin1_General_CI_AS NULL,
+    [strRoute]			NVARCHAR (MAX)   COLLATE Latin1_General_CI_AS NULL,
+	[strAlertType]		NVARCHAR (50)    COLLATE Latin1_General_CI_AS NULL,
+    [dtmDateCreated]    DATETIME         NULL,
+	[ysnSeen]			BIT				 NULL,
+	[dtmDateSeen]		DATETIME         NULL,
+    [intFromEntityId]	INT				 NULL, 
+	[intToEntityId]     INT              NULL,
+    [intConcurrencyId]  INT              NOT NULL,
+    CONSTRAINT [PK_dbo.tblSMNotification] PRIMARY KEY CLUSTERED ([intNotificationId] ASC)
+);
