e798249031b2e34b013969d2ca0b3f92dcb5368d
James Balachander <balachander.j@irely.com>
2015-09-25 09:07:59 +0530

    LG-91

--- i21Database/dbo/Stored Procedures/uspLGGetShippingInstructionCertificateReport.sql
+++ i21Database/dbo/Stored Procedures/uspLGGetShippingInstructionCertificateReport.sql
@@ -2,43 +2,43 @@
 		@xmlParam NVARCHAR(MAX) = NULL  
 AS
 BEGIN
-	DECLARE @intReferenceNumber			INT,
-			@xmlDocumentId				INT 
+	--DECLARE @intReferenceNumber			INT,
+	--		@xmlDocumentId				INT 
 			
-	IF	LTRIM(RTRIM(@xmlParam)) = ''   
-		SET @xmlParam = NULL   
+	--IF	LTRIM(RTRIM(@xmlParam)) = ''   
+	--	SET @xmlParam = NULL   
       
-	DECLARE @temp_xml_table TABLE 
-	(  
-			[fieldname]		NVARCHAR(50),  
-			condition		NVARCHAR(20),        
-			[from]			NVARCHAR(50), 
-			[to]			NVARCHAR(50),  
-			[join]			NVARCHAR(10),  
-			[begingroup]	NVARCHAR(50),  
-			[endgroup]		NVARCHAR(50),  
-			[datatype]		NVARCHAR(50) 
-	)  
+	--DECLARE @temp_xml_table TABLE 
+	--(  
+	--		[fieldname]		NVARCHAR(50),  
+	--		condition		NVARCHAR(20),        
+	--		[from]			NVARCHAR(50), 
+	--		[to]			NVARCHAR(50),  
+	--		[join]			NVARCHAR(10),  
+	--		[begingroup]	NVARCHAR(50),  
+	--		[endgroup]		NVARCHAR(50),  
+	--		[datatype]		NVARCHAR(50) 
+	--)  
   
-	EXEC sp_xml_preparedocument @xmlDocumentId output, @xmlParam  
+	--EXEC sp_xml_preparedocument @xmlDocumentId output, @xmlParam  
   
-	INSERT INTO @temp_xml_table  
-	SELECT	*  
-	FROM	OPENXML(@xmlDocumentId, 'xmlparam/filters/filter', 2)  
-	WITH (  
-				[fieldname]		NVARCHAR(50),  
-				condition		NVARCHAR(20),        
-				[from]			NVARCHAR(50), 
-				[to]			NVARCHAR(50),  
-				[join]			NVARCHAR(10),  
-				[begingroup]	NVARCHAR(50),  
-				[endgroup]		NVARCHAR(50),  
-				[datatype]		NVARCHAR(50)  
-	)  
+	--INSERT INTO @temp_xml_table  
+	--SELECT	*  
+	--FROM	OPENXML(@xmlDocumentId, 'xmlparam/filters/filter', 2)  
+	--WITH (  
+	--			[fieldname]		NVARCHAR(50),  
+	--			condition		NVARCHAR(20),        
+	--			[from]			NVARCHAR(50), 
+	--			[to]			NVARCHAR(50),  
+	--			[join]			NVARCHAR(10),  
+	--			[begingroup]	NVARCHAR(50),  
+	--			[endgroup]		NVARCHAR(50),  
+	--			[datatype]		NVARCHAR(50)  
+	--)  
     
-	SELECT	@intReferenceNumber = [from]
-	FROM	@temp_xml_table   
-	WHERE	[fieldname] = 'intReferenceNumber' 
+	--SELECT	@intReferenceNumber = [from]
+	--FROM	@temp_xml_table   
+	--WHERE	[fieldname] = 'intReferenceNumber' 
 
 SELECT 
 	SIC.intShippingInstructionId,
@@ -50,5 +50,5 @@ SELECT
 FROM	tblLGShippingInstructionCertificates SIC
 JOIN	tblLGShippingInstruction SI ON SI.intShippingInstructionId = SIC.intShippingInstructionId
 JOIN	tblICDocument DOC ON DOC.intDocumentId = SIC.intDocumentId
-WHERE 	SI.intReferenceNumber = @intReferenceNumber	
+WHERE 	SI.intReferenceNumber = @xmlParam	
 END
