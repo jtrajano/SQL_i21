43dbd87113f9bb4fed57be3a0e1f84048ef6051d
Melanie Ferrer <Mel.Ferrer@irely.com>
2015-09-29 09:47:22 +0800

    PAT-13 Refund Rate Maintenance

--- i21Database/dbo/Tables/tblPATRefundRateDetail.sql
+++ i21Database/dbo/Tables/tblPATRefundRateDetail.sql
@@ -5,6 +5,7 @@
     [intPatronageCategoryId] INT NOT NULL, 
     [strPurchaseSale] NVARCHAR(50) NOT NULL, 
     [dblRate] NUMERIC(18, 6) NOT NULL, 
+    [intConcurrencyId] INT NULL DEFAULT 0, 
     CONSTRAINT [PK_tblPATRefundRateDetail] PRIMARY KEY ([intRefundTypeDetailId]), 
     CONSTRAINT [FK_tblPATRefundRateDetail_tblPATRefundRate] FOREIGN KEY ([intRefundTypeId]) REFERENCES [tblPATRefundRate]([intRefundTypeId]), 
     CONSTRAINT [FK_tblPATRefundRateDetail_tblPATPatronageCategory] FOREIGN KEY ([intPatronageCategoryId]) REFERENCES [tblPATPatronageCategory]([intPatronageCategoryId]) 
