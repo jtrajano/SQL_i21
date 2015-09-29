2761ae7556a201d75ac9834996cf8916e0339548
Melanie Ferrer <Mel.Ferrer@irely.com>
2015-09-29 08:36:34 +0800

    PAT-13 Refund Rate Maintenance

--- i21Database/dbo/Tables/tblPATRefundRate.sql
+++ i21Database/dbo/Tables/tblPATRefundRate.sql
@@ -7,9 +7,6 @@
 	[intAllocatedReserveId] [int] NOT NULL,
 	[intUndistributedEquityId] [int] NOT NULL,
 	[dblCashPayout] [numeric](18, 6) NOT NULL,
-	[intPatronageCategoryId] [nchar](10) NULL,
-	[strPurchaseSale] [nvarchar](50) NULL,
-	[dblRate] [numeric](18, 6) NULL, 
     [intConcurrencyId] INT NULL DEFAULT 0, 
     CONSTRAINT [PK_tblPATRefundRate] PRIMARY KEY ([intRefundTypeId])
 )
---
+++ i21Database/dbo/Tables/tblPATRefundRateDetail.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tblPATRefundRateDetail]
+(
+	[intRefundTypeDetailId] INT NOT NULL IDENTITY, 
+    [intRefundTypeId] INT NULL, 
+    [intPatronageCategoryId] INT NOT NULL, 
+    [strPurchaseSale] NVARCHAR(50) NOT NULL, 
+    [dblRate] NUMERIC(18, 6) NOT NULL, 
+    CONSTRAINT [PK_tblPATRefundRateDetail] PRIMARY KEY ([intRefundTypeDetailId]), 
+    CONSTRAINT [FK_tblPATRefundRateDetail_tblPATRefundRate] FOREIGN KEY ([intRefundTypeId]) REFERENCES [tblPATRefundRate]([intRefundTypeId]), 
+    CONSTRAINT [FK_tblPATRefundRateDetail_tblPATPatronageCategory] FOREIGN KEY ([intPatronageCategoryId]) REFERENCES [tblPATPatronageCategory]([intPatronageCategoryId]) 
+)
