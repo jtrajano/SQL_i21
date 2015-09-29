73bcad8c91fd76ab9c3fdc8efe90921b96b5cc86
Ramachandran <ramachandran.shanmuganathan@irely.com>
2015-09-29 16:06:45 +0530

    RM-80

--- i21Database/dbo/Stored Procedures/uspRKAssignFuturesToContractSummarySave.sql
+++ i21Database/dbo/Stored Procedures/uspRKAssignFuturesToContractSummarySave.sql
@@ -42,22 +42,17 @@ SELECT
  intAssignFuturesToContractSummaryId INT,    
  [ysnDeleted] Bit    
  )    
-
-IF EXISTS(select * from @tblMatchedDelete)    
-BEGIN 
+ 
 	UPDATE tblRKFutOptTransaction set intContractDetailId = null WHERE intFutOptTransactionId in(SELECT intFutOptTransactionId FROM tblRKAssignFuturesToContractSummary    
 	WHERE intAssignFuturesToContractSummaryId in(SELECT intAssignFuturesToContractSummaryId from @tblMatchedDelete) )
-END    
-    
+       
 IF EXISTS(SELECT * FROM @tblMatchedDelete)    
 BEGIN    
 DELETE FROM tblRKAssignFuturesToContractSummary    
   WHERE intAssignFuturesToContractSummaryId in( SELECT intAssignFuturesToContractSummaryId from @tblMatchedDelete)    
 END    
   ----------------------- END Delete Matched ---------------------    
-     
- 
-     
+          
  ---------------Header Record Insert ----------------    
  INSERT INTO tblRKAssignFuturesToContractSummaryHeader     
   (    
