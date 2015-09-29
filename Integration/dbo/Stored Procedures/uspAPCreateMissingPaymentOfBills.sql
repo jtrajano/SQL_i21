d16ccc43fb09465bb83144b9dd8bf789877b62ff
Erick Dela Cruz <erick.delacruz@irely.com>
2015-09-29 15:05:01 +0800

    AP-1645

--- Integration/dbo/Stored Procedures/uspAPCreateMissingPaymentOfBills.sql
+++ Integration/dbo/Stored Procedures/uspAPCreateMissingPaymentOfBills.sql
@@ -28,7 +28,12 @@ BEGIN
 		IF OBJECT_ID(''tempdb..##OriginChecks'') IS NOT NULL DROP TABLE ##OriginChecks
 
 		--Join Origin Tables with the Bills
-		SELECT D.intBillId, D.strBillId, D.strVendorOrderNumber, A.apivc_ivc_no, E.intBankAccountId, E.intGLAccountId, A.apivc_trans_type, D.intTransactionType, D.dtmDate, D.dtmDueDate, A.apivc_due_rev_dt, A.apivc_chk_rev_dt 
+		SELECT D.intBillId, D.strBillId, D.strVendorOrderNumber
+		, A.apivc_ivc_no, E.intBankAccountId
+		, E.intGLAccountId, A.apivc_trans_type
+		, C.intEntityVendorId
+		, A.apivc_chk_no
+		, D.intTransactionType, D.dtmDate, D.dtmDueDate, A.apivc_due_rev_dt, A.apivc_chk_rev_dt 
 		INTO ##OriginChecks
 		FROM apivcmst A
 			LEFT JOIN apcbkmst B
@@ -54,9 +59,9 @@ BEGIN
 		SELECT 
 		intAccountId = B.intGLAccountId
 		,intBankAccountId = B.intBankAccountId
-		,intPaymentMethodId = 3
-		,intCurrencyId =3
-		,strPaymentInfo = ''not found''
+		,intPaymentMethodId = (SELECT TOP 1 intPaymentMethodID FROM tblSMPaymentMethod WHERE LOWER(strPaymentMethod) = ''check'')
+		,intCurrencyId = (SELECT TOP 1 intCurrencyID FROM tblSMCurrency WHERE LOWER(strCurrency) = ''usd'')
+		,strPaymentInfo = dbo.fnTrim(B.apivc_chk_no)
 		,strNotes  = A.strBillId
 		,dtmDatePaid = CASE WHEN ISDATE(apivc_chk_rev_dt) = 1 THEN CONVERT(DATE, CAST(apivc_chk_rev_dt AS CHAR(12)), 112) ELSE GETDATE() END
 						--((substring(convert(varchar(10),B.apivc_chk_rev_dt),5,2)  
@@ -65,7 +70,6 @@ BEGIN
 		,dblAmountPaid = A.dblTotal
 		,dblUnapplied = 0
 		,ysnPosted = 1
-		,strPaymentRecordNum = ''not found''
 		,dblWithheld = 0
 		,intUserId =  A.intEntityId
 		,intEntityId = A.intEntityId
@@ -97,7 +101,6 @@ BEGIN
 			,dblAmountPaid
 			,dblUnapplied
 			,ysnPosted
-			,strPaymentRecordNum
 			,dblWithheld
 			,intUserId
 			,intEntityId
@@ -121,7 +124,6 @@ BEGIN
 			,dblAmountPaid
 			,dblUnapplied
 			,ysnPosted
-			,strPaymentRecordNum
 			,dblWithheld
 			,intUserId
 			,intEntityId
@@ -133,27 +135,6 @@ BEGIN
 			,dtmDateDeleted
 			,dtmDateCreated
 		FROM ##TempAPCreatedPaymentFROMBills 
-
-
-		IF OBJECT_ID(''tempdb..##Tempi21APPaymentFROMBillOrigin'') IS NOT NULL DROP TABLE ##Tempi21APPaymentFROMBillOrigin
-		--Find Payment Header without Payment detail.
-		SELECT * INTO  ##Tempi21APPaymentFROMBillOrigin 
-		FROM tblAPPayment WHERE intPaymentId  in 
-			(SELECT intPaymentId FROM  tblAPPayment 
-			WHERE intPaymentId not in (SELECT intPaymentId FROM tblAPPaymentDetail) AND strPaymentInfo = ''not found'')
-
-		--Update Record no.
-		UPDATE A set strPaymentRecordNum  =  ''PAY-'' + convert (NVARCHAR,B.intPaymentId)
-		FROM tblAPPayment A
-		INNER JOIN ##Tempi21APPaymentFROMBillOrigin B
-		on A.intPaymentId  = B.intPaymentId 
-
-		--Update the Starting Number + 1 to the last prefix id inserted.
-		UPDATE tblSMStartingNumber 
-		SET intNumber = (select top 1 intPaymentId from ##Tempi21APPaymentFROMBillOrigin order by intPaymentId DESC) + 1
-		where strPrefix =''PAY-''
-
-
 
 		--Insert PaymentDetail
 		INSERT INTO tblAPPaymentDetail 
@@ -175,18 +156,19 @@ BEGIN
 		A.intPaymentId 
 		,C.intBillId
 		,C.intAccountId
-		,0
-		,0
-		,A.dblAmountPaid
+		,C.dblDiscount
 		,0
 		,A.dblAmountPaid
 		,0
+		,A.dblAmountPaid
+		,C.dblWithheld
 		 FROM  ##Tempi21APPaymentFROMBillOrigin A
 		 INNER JOIN tblAPBill C
 		 on C.strBillId = A.strNotes
- 
-		 INNER JOIN ##OriginChecks D
-		 ON D.apivc_ivc_no = C.strVendorOrderNumber   COLLATE Latin1_General_CS_AS
+		
+		 --INNER JOIN ##OriginChecks D
+		 --ON D.apivc_ivc_no = C.strVendorOrderNumber   COLLATE Latin1_General_CS_AS
+		 --AND C.intEntityVendorId = D.intEntityVendorId
 
 		END
 	')
---
+++ i21PreDeployment/Scripts/Pre-Deployment/8_AP/UpdatePaymentMethod.sql
@@ -0,0 +1,12 @@
+﻿--THIS SCRIPT WILL UPDATE INVALID PAYMENT METHOD USED IN PAYMENT
+IF(EXISTS(SELECT 1 FROM sys.columns WHERE name = N'intPaymentMethodId' and object_id = OBJECT_ID(N'tblAPPayment')))
+BEGIN
+	IF (EXISTS(SELECT 1 FROM tblAPPayment WHERE intPaymentMethodId NOT IN (SELECT intPaymentMethodID FROM tblSMPaymentMethod)))
+	BEGIN
+		UPDATE A
+			SET A.intPaymentMethodId = ISNULL((SELECT TOP 1 intPaymentMethodID FROM tblSMPaymentMethod WHERE LOWER(strPaymentMethod) = 'check'),
+												(SELECT TOP 1 intPaymentMethodID FROM tblSMPaymentMethod))
+		FROM tblAPPayment A
+		WHERE intPaymentMethodId NOT IN (SELECT intPaymentMethodID FROM tblSMPaymentMethod)
+	END
+END
