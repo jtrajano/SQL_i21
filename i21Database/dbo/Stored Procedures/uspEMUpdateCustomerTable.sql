c219e2ef631186d6ef5be479fdd795b1d462ce25
Mon Gonzales <mon.gonzales@irely.com>
2015-09-29 20:03:02 +0800

    EM-522

---
+++ i21Database/dbo/Stored Procedures/uspEMUpdateCustomerTable.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [dbo].[uspEMUpdateCustomerTable]
+	@Field NVARCHAR(200),
+	@Value NVARCHAR(200),
+	@Id		INT
+AS
+BEGIN
+	DECLARE @Com   AS NVARCHAR(MAX)
+	DECLARE @Param AS NVARCHAR(MAX)
+
+	IF CHARINDEX('--', @Field, 0) <=0 AND CHARINDEX('--', @Value, 0) <=0 AND @Field not in ('strCustomerNumber', 'intEntityCustomerId') AND (@Id is not null and @Id > 0)
+	BEGIN
+	
+		SET @Param = '@Value AS NVARCHAR(200), @Id	AS INT'
+		SET @Com = 'UPDATE tblARCustomer SET ' +@Field + ' = @Value WHERE intEntityCustomerId = @Id' 
+	
+		EXECUTE sp_executesql @Com, @Param, @Value, @Id
+	END
+
+END
