db3d120241792e4133fa3593d736d541a9a12bfe
von.pasion <von.pasion@irely.com>
2015-09-29 21:44:21 +0800

    FRM-2495

--- i21Database/dbo/Tables/tblSMNotification.sql
+++ i21Database/dbo/Tables/tblSMNotification.sql
@@ -1,10 +1,12 @@
 ﻿CREATE TABLE [dbo].[tblSMNotification] (
-    [intNotificationId] INT              IDENTITY (1, 1) NOT NULL,
+    [intNotificationId] INT              IDENTITY (1, 1) NOT NULL,	
+    [strTitle]			NVARCHAR(100)	 COLLATE Latin1_General_CI_AS NULL, 
     [strMessage]        NVARCHAR (MAX)   COLLATE Latin1_General_CI_AS NULL,
     [strRoute]			NVARCHAR (MAX)   COLLATE Latin1_General_CI_AS NULL,
 	[strAlertType]		NVARCHAR (50)    COLLATE Latin1_General_CI_AS NULL,
     [dtmDateCreated]    DATETIME         NULL,
 	[ysnSeen]			BIT				 NULL,
+	[ysnClosable]		BIT				 NULL,
 	[dtmDateSeen]		DATETIME         NULL,
     [intFromEntityId]	INT				 NULL, 
 	[intToEntityId]     INT              NULL,
