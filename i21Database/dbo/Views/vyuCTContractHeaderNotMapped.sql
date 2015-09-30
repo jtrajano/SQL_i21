0181f1eb78e289b04552e3ba6d5e3bda79b14e1f
Satya <ssahoo@BLRSATYAWIN7.irely.net>
2015-09-30 19:12:12 +0530

    CT-166

---
+++ i21Database/dbo/Views/vyuCTContractHeaderNotMapped.sql
@@ -0,0 +1,18 @@
+﻿CREATE VIEW [dbo].[vyuCTContractHeaderNotMapped]
+	
+AS 
+	
+SELECT	CH.intContractHeaderId,
+		PF.intPriceFixationId, 
+		CASE WHEN	(	
+						SELECT	COUNT(SA.intSpreadArbitrageId) 
+						FROM	tblCTSpreadArbitrage SA  
+						WHERE	SA.intPriceFixationId = PF.intPriceFixationId
+					) > 0
+		THEN	CAST(1 AS BIT) 
+		ELSE	CAST(0 AS BIT)
+		END		AS ysnSpreadAvailable
+		
+FROM	tblCTContractHeader CH	LEFT
+JOIN	tblCTPriceFixation	PF	ON	CH.intContractHeaderId = PF.intContractHeaderId
+WHERE	PF.intContractDetailId	IS	NULL
