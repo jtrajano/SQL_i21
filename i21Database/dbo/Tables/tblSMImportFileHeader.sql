63cffdaa75bc4384a67a7203176cef0f9cbd447c
Maheshwari <maheshwari.gawade@irely.com>
2015-10-01 13:40:32 +0530

    CCR-69 - added new column "Active"

--- i21Database/dbo/Tables/tblSMImportFileHeader.sql
+++ i21Database/dbo/Tables/tblSMImportFileHeader.sql
@@ -6,6 +6,7 @@
 	[strFieldDelimiter] nvarchar(50) COLLATE Latin1_General_CI_AS NULL,
 	[strXMLType] nvarchar(50) COLLATE Latin1_General_CI_AS NULL, 
 	[strXMLInitiater] nvarchar(max) COLLATE Latin1_General_CI_AS NULL,
+	[ysnActive] bit DEFAULT 1,
     [intConcurrencyId] INT NULL DEFAULT ((0)), 
 	CONSTRAINT [PK_tblSMImportFileHeader] PRIMARY KEY ([intImportFileHeaderId]),
 	CONSTRAINT [AK_tblSMImportFileHeader] UNIQUE ([strLayoutTitle])	 
