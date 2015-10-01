bd0feb87d47df2d7d3595d468358e7fb11088606
Feb Montefrio <feb.montefrio@irely.com>
2015-10-01 14:51:49 +0800

    Fix predeployment. Convert it to string.

--- Integration/Scripts/PreDeployment.sql
+++ Integration/Scripts/PreDeployment.sql
@@ -3,7 +3,7 @@
 --------------------------------------------------------------------------------------
  This file contains SQL statements that will be executed before the build script.	
  Use SQLCMD syntax to include a file in the pre-deployment script.			
- Example:      :r .\myfile.sql								
+ Example:      :r .\myfile.sql"								
  Use SQLCMD syntax to reference a variable in the pre-deployment script.		
  Example:      :setvar TableName MyTable							
                SELECT * FROM [$(TableName)]					
@@ -36,13 +36,13 @@
 :r "..\dbo\Stored Procedures\uspFRDProcessOriginImport.sql"
 
 -- CASH MANAGEMENT
-:r ..\dbo\Views\apcbkmst.sql
-:r ..\dbo\Views\apchkmst.sql
-:r ..\dbo\Functions\fnIsDepositEntry.sql 
-:r ..\dbo\Functions\fnGetCurrencyIdFromi21ToOrigin.sql 
-:r ..\dbo\Views\vyuCMBankAccount.sql
-:r ..\dbo\Views\vyuCMOriginDepositEntry.sql
-:r ..\dbo\Views\vyuCMOriginUndepositedFund.sql
+:r "..\dbo\Views\apcbkmst.sql"
+:r "..\dbo\Views\apchkmst.sql"
+:r "..\dbo\Functions\fnIsDepositEntry.sql"
+:r "..\dbo\Functions\fnGetCurrencyIdFromi21ToOrigin.sql"
+:r "..\dbo\Views\vyuCMBankAccount.sql"
+:r "..\dbo\Views\vyuCMOriginDepositEntry.sql"
+:r "..\dbo\Views\vyuCMOriginUndepositedFund.sql"
 :r "..\dbo\Stored Procedures\uspCMProcessUndepositedFunds.sql"
 :r "..\dbo\Stored Procedures\uspCMBankTransactionReversalOrigin.sql"
 :r "..\dbo\Stored Procedures\uspCMImportBankAccountsFromOrigin.sql"
@@ -51,80 +51,78 @@
 :r "..\dbo\Stored Procedures\uspCMImportValidations.sql"
 
 -- ACCOUNTS PAYABLE
-:r ..\dbo\Views\vwapivcmst.sql
+:r "..\dbo\Views\vwapivcmst.sql"
 :r "..\dbo\Stored Procedures\uspAPCreatePaymentFromOriginBill.sql"
 
 
 ---- CUSTOMER PORTAL
---:r ..\dbo\Views\vyuCPAgcusMst.sql
---:r ..\dbo\Views\vyuCPBABusinessSummary.sql
---:r ..\dbo\Views\vyuCPPaymentsDetails.sql
+--:r "..\dbo\Views\vyuCPAgcusMst.sql"
+--:r "..\dbo\Views\vyuCPBABusinessSummary.sql"
+--:r "..\dbo\Views\vyuCPPaymentsDetails.sql"
 
---:r ..\dbo\Views\vyuCPBillingAccountPayments.sql
---:r ..\dbo\Views\vyuCPContracts.sql
---:r ..\dbo\Views\vyuCPCurrentCashBids.sql
---:r ..\dbo\Views\vyuCPDatabaseDate.sql
---:r ..\dbo\Views\vyuCPGABusinessSummary.sql
---:r ..\dbo\Views\vyuCPGAContractDetail.sql
---:r ..\dbo\Views\vyuCPGAContracts.sql
---:r ..\dbo\Views\vyuCPGASettlementsReports.sql
---:r ..\dbo\Views\vyuCPInvoicesCredits.sql
---:r ..\dbo\Views\vyuCPInvoicesCreditsReports.sql
---:r ..\dbo\Views\vyuCPOptions.sql
---:r ..\dbo\Views\vyuCPOrders.sql
---:r ..\dbo\Views\vyuCPPayments.sql
+--:r "..\dbo\Views\vyuCPBillingAccountPayments.sql"
+--:r "..\dbo\Views\vyuCPContracts.sql"
+--:r "..\dbo\Views\vyuCPCurrentCashBids.sql"
+--:r "..\dbo\Views\vyuCPDatabaseDate.sql"
+--:r "..\dbo\Views\vyuCPGABusinessSummary.sql"
+--:r "..\dbo\Views\vyuCPGAContractDetail.sql"
+--:r "..\dbo\Views\vyuCPGAContracts.sql"
+--:r "..\dbo\Views\vyuCPGASettlementsReports.sql"
+--:r "..\dbo\Views\vyuCPInvoicesCredits.sql"
+--:r "..\dbo\Views\vyuCPInvoicesCreditsReports.sql"
+--:r "..\dbo\Views\vyuCPOptions.sql"
+--:r "..\dbo\Views\vyuCPOrders.sql"
+--:r "..\dbo\Views\vyuCPPayments.sql"
 
---:r ..\dbo\Views\vyuCPPendingInvoices.sql
---:r ..\dbo\Views\vyuCPPendingPayments.sql
---:r ..\dbo\Views\vyuCPPrepaidCredits.sql
---:r ..\dbo\Views\vyuCPProductionHistory.sql
---:r ..\dbo\Views\vyuCPPurchaseDetail.sql
---:r ..\dbo\Views\vyuCPPurchaseMain.sql
---:r ..\dbo\Views\vyuCPPurchases.sql
---:r ..\dbo\Views\vyuCPPurchasesDetail.sql
---:r ..\dbo\Views\vyuCPSettlements.sql
---:r ..\dbo\Views\vyuCPStorage.sql
---:r ..\dbo\Views\vyuCPCustomer.sql
---:r ..\dbo\Views\vyuCPAgcrdMst.sql
---:r ..\dbo\Views\vyuCPGAContractHistory.sql
---:r ..\dbo\Views\vyuCPGAContractOption.sql
---:r ..\dbo\Views\vyuECCFCardTransaction.sql
+--:r "..\dbo\Views\vyuCPPendingInvoices.sql"
+--:r "..\dbo\Views\vyuCPPendingPayments.sql"
+--:r "..\dbo\Views\vyuCPPrepaidCredits.sql"
+--:r "..\dbo\Views\vyuCPProductionHistory.sql"
+--:r "..\dbo\Views\vyuCPPurchaseDetail.sql"
+--:r "..\dbo\Views\vyuCPPurchaseMain.sql"
+--:r "..\dbo\Views\vyuCPPurchases.sql"
+--:r "..\dbo\Views\vyuCPPurchasesDetail.sql"
+--:r "..\dbo\Views\vyuCPSettlements.sql"
+--:r "..\dbo\Views\vyuCPStorage.sql"
+--:r "..\dbo\Views\vyuCPCustomer.sql"
+--:r "..\dbo\Views\vyuCPAgcrdMst.sql"
+--:r "..\dbo\Views\vyuCPGAContractHistory.sql"
+--:r "..\dbo\Views\vyuCPGAContractOption.sql"
+--:r "..\dbo\Views\vyuECCFCardTransaction.sql"
 
 -- TANK MANAGEMENT
-:r ..\dbo\Views\vwclsmst.sql
-:r ..\dbo\Views\vwpyemst.sql
-:r ..\dbo\Views\vwticmst.sql
---:r ..\dbo\Views\vyuTMOriginAccountStatus.sql
+:r "..\dbo\Views\vwclsmst.sql"
+:r "..\dbo\Views\vwpyemst.sql"
+:r "..\dbo\Views\vwticmst.sql"
+--:r "..\dbo\Views\vyuTMOriginAccountStatus.sql"
 :r "..\dbo\Stored Procedures\uspTMRecreateAccountStatusView.sql"
---:r ..\dbo\Views\vwcmtmst.sql
+--:r "..\dbo\Views\vwcmtmst.sql"
 :r "..\dbo\Stored Procedures\uspTMRecreateCommentsView.sql"
---:r ..\dbo\Views\vwcntmst.sql
+--:r "..\dbo\Views\vwcntmst.sql"
 :r "..\dbo\Stored Procedures\uspTMRecreateContractView.sql"
---:r ..\dbo\Views\vyuTMOriginOption.sql
+--:r "..\dbo\Views\vyuTMOriginOption.sql"
 :r "..\dbo\Stored Procedures\uspTMRecreateOriginOptionView.sql"
---:r ..\dbo\Views\vwctlmst.sql
+--:r "..\dbo\Views\vwctlmst.sql"
 :r "..\dbo\Stored Procedures\uspTMRecreateCTLMSTView.sql"
---:r ..\dbo\Views\vwitmmst.sql
+--:r "..\dbo\Views\vwitmmst.sql"
 :r "..\dbo\Stored Procedures\uspTMRecreateItemView.sql"
---:r ..\dbo\Views\vwivcmst.sql
+--:r "..\dbo\Views\vwivcmst.sql"
 :r "..\dbo\Stored Procedures\uspTMRecreateInvoiceView.sql"
---:r ..\dbo\Views\vwlclmst.sql
+--:r "..\dbo\Views\vwlclmst.sql"
 :r "..\dbo\Stored Procedures\uspTMRecreateLocaleTaxView.sql"
---:r ..\dbo\Views\vwlocmst.sql
+--:r "..\dbo\Views\vwlocmst.sql"
 :r "..\dbo\Stored Procedures\uspTMRecreateLocationView.sql"
-:r ..\dbo\Views\vwprcmst.sql
+:r "..\dbo\Views\vwprcmst.sql"
 
---:r ..\dbo\Views\vwcusmst.sql
+--:r "..\dbo\Views\vwcusmst.sql"
 :r "..\dbo\Stored Procedures\uspTMRecreateCustomerView.sql"
---:r ..\dbo\Views\vwslsmst.sql
+--:r "..\dbo\Views\vwslsmst.sql"
 :r "..\dbo\Stored Procedures\uspTMRecreateSalesPersonView.sql"
-:r ..\dbo\Views\vwtaxmst.sql
---:r ..\dbo\Views\vwtrmmst.sql
+:r "..\dbo\Views\vwtaxmst.sql"
+--:r "..\dbo\Views\vwtrmmst.sql"
 :r "..\dbo\Stored Procedures\uspTMRecreateTermsView.sql"
-:r ..\dbo\Views\vwDispatch.sql
-:r ..\dbo\Views\vyuTMtrprcmst.sql
-
-
+:r "..\dbo\Views\vwDispatch.sql"
+:r "..\dbo\Views\vyuTMtrprcmst.sql"
 :r "..\dbo\Stored Procedures\TwoPartDeliveryFillReport.sql"
 :r "..\dbo\Stored Procedures\uspTMGetConsumptionWithGasCheck.sql"
 :r "..\dbo\Stored Procedures\uspTMGetConsumptionWithLeakCheck.sql"
@@ -162,7 +160,7 @@
 
 
 -- TAX FORMS
-:r ..\dbo\Views\vyuTFTaxCycle.sql
+:r "..\dbo\Views\vyuTFTaxCycle.sql"
 
 --ACCOUNTS RECEIVABLE
 :r "..\dbo\Stored Procedures\uspARImportAccount.sql"
@@ -179,7 +177,7 @@
 
 
 -- C-Store
-:r ..\dbo\Views\vyustpbkmst.sql
+:r "..\dbo\Views\vyustpbkmst.sql"
 
 
 
