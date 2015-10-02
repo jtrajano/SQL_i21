d2e316e5df528ee7de1187cc1ce7b7506a59d2f8
Crisman Lazaga <cris.lazaga@irely.com>
2015-10-02 09:08:09 +0800

    IU-58

--- i21Database/dbo/Tables/tblIUPatchDetail.sql
+++ i21Database/dbo/Tables/tblIUPatchDetail.sql
@@ -2,11 +2,11 @@
 (
 	[intPatchDetailId] INT NOT NULL IDENTITY, 
     [intVersionId] INT NOT NULL, 
-    [strCommitId] NVARCHAR(11) NOT NULL, 
-    [strVersion] NVARCHAR(15) NOT NULL, 
-    [strChangeType] NVARCHAR(15) NULL, 
-    [strFilePath] NVARCHAR(2000) NULL, 
-    [strFileName] NVARCHAR(500) NULL, 
+    [strCommitId] NVARCHAR(11)  COLLATE Latin1_General_CI_AS NOT NULL, 
+    [strVersion] NVARCHAR(15)  COLLATE Latin1_General_CI_AS NOT NULL, 
+    [strChangeType] NVARCHAR(15)  COLLATE Latin1_General_CI_AS NULL, 
+    [strFilePath] NVARCHAR(2000)  COLLATE Latin1_General_CI_AS NULL, 
+    [strFileName] NVARCHAR(500)  COLLATE Latin1_General_CI_AS NULL, 
     [intConcurrencyId] INT NULL, 
     CONSTRAINT [PK_tblIUPatchDetail] PRIMARY KEY ([intPatchDetailId]), 
     CONSTRAINT [FK_tblIUPatchDetail_tblSMBuildNumber] FOREIGN KEY ([intVersionId]) REFERENCES [tblSMBuildNumber]([intVersionID]) 
