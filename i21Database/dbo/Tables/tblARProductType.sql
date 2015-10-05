07ef91cf6007917760024813b180c89f46713137
Kelvin Quinto <kelvin.quinto@irely.com>
2015-10-05 23:47:38 +0800

    AR-1871

--- i21Database/dbo/Tables/tblARProductType.sql
+++ i21Database/dbo/Tables/tblARProductType.sql
@@ -4,5 +4,6 @@
     [strProductTypeName] NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL, 
     [strProductTypeDescription] NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL, 
     [intConcurrencyId] INT NOT NULL,
-	CONSTRAINT [PK_tblARProductType_intProductTypeId] PRIMARY KEY CLUSTERED ([intProductTypeId] ASC)
+	CONSTRAINT [PK_tblARProductType_intProductTypeId] PRIMARY KEY CLUSTERED ([intProductTypeId] ASC),
+	CONSTRAINT [UK_tblARProductType_strProductTypeName] UNIQUE (strProductTypeName)
 )
