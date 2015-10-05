c0a56bc7d1cebe92b7474aa78a6766e03aa41da7
Lawrence Zabala <lawrence.zabala@irely.com>
2015-10-05 14:15:36 +0800

    IC-1122

---
+++ i21Database/dbo/Tables/tblICItemMotorFuelTax.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[tblICItemMotorFuelTax]
+(
+	[intItemMotorFuelTaxId] INT NOT NULL IDENTITY, 
+    [intItemId] INT NOT NULL, 
+    [intTaxAuthorityId] INT NOT NULL, 
+    [intProductCodeId] INT NOT NULL, 
+    [intSort] INT NULL, 
+    [intConcurrencyId] INT NULL DEFAULT ((0)), 
+    CONSTRAINT [PK_tblICItemMotorFuelTax] PRIMARY KEY ([intItemMotorFuelTaxId]), 
+    CONSTRAINT [FK_tblICItemMotorFuelTax_tblICItem] FOREIGN KEY ([intItemId]) REFERENCES [tblICItem]([intItemId]), 
+    CONSTRAINT [FK_tblICItemMotorFuelTax_tblTFTaxAuthority] FOREIGN KEY ([intTaxAuthorityId]) REFERENCES [tblTFTaxAuthority]([intTaxAuthorityId]), 
+    CONSTRAINT [FK_tblICItemMotorFuelTax_tblTFProductCode] FOREIGN KEY ([intProductCodeId]) REFERENCES [tblTFProductCode]([intProductCodeId])
+)
