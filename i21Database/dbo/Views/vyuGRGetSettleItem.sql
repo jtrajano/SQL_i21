ef0ed51fcebd0dcef25e8c9f79b1048c83787d59
Gopinath Nayak <gopinath.n@irely.com>
2015-10-05 13:16:17 +0530

    GRN-165

---
+++ i21Database/dbo/Views/vyuGRGetSettleItem.sql
@@ -0,0 +1,9 @@
+﻿CREATE VIEW [dbo].[vyuGRGetSettleItem]
+AS 
+SELECT Distinct
+   Cs.intEntityId    
+  ,Cs.intItemId  
+ ,Item.strItemNo
+FROM tblGRCustomerStorage Cs
+JOIN tblICItem Item ON Item.intItemId = Cs.intItemId
+Where Cs.dblOpenBalance >0 
---
+++ i21Database/dbo/Views/vyuGRGetVendors.sql
@@ -0,0 +1,10 @@
+﻿CREATE VIEW [dbo].[vyuGRGetVendors]
+AS   
+SELECT Distinct     
+  CS.intEntityId    
+ ,E.strName
+,ET.strType  
+FROM tblGRCustomerStorage CS  
+JOIN tblEntity E ON E.intEntityId = CS.intEntityId
+JOIN tblEntityType ET ON ET.intEntityId=E.intEntityId  
+Where CS.dblOpenBalance >0 AND ET.strType='Vendor'
