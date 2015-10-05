486fea6553b46571d11bbef3f320231384af6c0d
Ramachandran <ramachandran.shanmuganathan@irely.com>
2015-10-05 18:11:13 +0530

    RM-147

--- i21Database/dbo/Stored Procedures/uspRKGetBasisDeliveries.sql
+++ i21Database/dbo/Stored Procedures/uspRKGetBasisDeliveries.sql
@@ -3,12 +3,22 @@
 	@intLocationId int = NULL,
 	@intSeqId int
 AS
+DECLARE @tblTemp TABLE (intInventoryShipmentItemId int,
+						strLocationName NVARCHAR(50),
+						strTicketNumber NVARCHAR(50),
+						dtmTicketDateTime datetime,
+						strCustomerReference NVARCHAR(50),
+					    strDistributionOption NVARCHAR(500),
+					    dblTotal NUMERIC(24,10),
+					    intCommodityId int)
+						
 IF @intSeqId = 13
 	BEGIN
 		IF ISNULL(@intLocationId, 0) <> 0
 		BEGIN
+			INSERT INTO @tblTemp (intInventoryShipmentItemId,strLocationName,strTicketNumber,dtmTicketDateTime,strCustomerReference, strDistributionOption,dblTotal,intCommodityId)
 			SELECT PLDetail.intPickLotDetailId as intInventoryShipmentItemId,cl.strLocationName,convert(nvarchar,CT.strContractNumber)+'/'+convert(nvarchar,CT.intContractSeq) strTicketNumber,CT.dtmContractDate as dtmTicketDateTime ,
-			CT.strCustomerContract as strCustomerReference, 'CNT' as strDistributionOption,	PLDetail.dblLotPickedQty AS dblTotal
+			CT.strCustomerContract as strCustomerReference, 'CNT' as strDistributionOption,	PLDetail.dblLotPickedQty AS dblTotal,intCommodityId
 			FROM tblLGDeliveryPickDetail Del
 			INNER JOIN tblLGPickLotDetail PLDetail ON PLDetail.intPickLotDetailId = Del.intPickLotDetailId
 			INNER JOIN vyuLGPickOpenInventoryLots Lots ON Lots.intLotId = PLDetail.intLotId
@@ -19,7 +29,7 @@ IF @intSeqId = 13
 			UNION ALL
 			
 			SELECT ri.intInventoryReceiptItemId as intInventoryShipmentItemId,cl.strLocationName,convert(nvarchar,st.intTicketNumber) strTicketNumber,st.dtmTicketDateTime,strCustomerReference,
-					strDistributionOption,isnull(ri.dblReceived, 0) AS dblTotal
+					strDistributionOption,isnull(ri.dblReceived, 0) AS dblTotal,st.intCommodityId
 			FROM tblICInventoryReceipt r
 			INNER JOIN tblICInventoryReceiptItem ri ON r.intInventoryReceiptId = ri.intInventoryReceiptId AND r.strReceiptType = 'Purchase Contract'
 			INNER JOIN tblSCTicket st ON st.intTicketId = ri.intSourceId  AND strDistributionOption IN ('CNT')
@@ -30,8 +40,9 @@ IF @intSeqId = 13
 		END
 		ELSE
 		BEGIN
+		Insert into @tblTemp (intInventoryShipmentItemId,strLocationName,strTicketNumber,dtmTicketDateTime,strCustomerReference, strDistributionOption,dblTotal,intCommodityId)
 			SELECT PLDetail.intPickLotDetailId as intInventoryShipmentItemId,CT.strLocationName,convert(nvarchar,CT.strContractNumber)+'/'+convert(nvarchar,CT.intContractSeq) strTicketNumber,CT.dtmContractDate as dtmTicketDateTime ,
-			CT.strCustomerContract as strCustomerReference, 'CNT' as strDistributionOption,	PLDetail.dblLotPickedQty AS dblTotal
+			CT.strCustomerContract as strCustomerReference, 'CNT' as strDistributionOption,	PLDetail.dblLotPickedQty AS dblTotal,intCommodityId
 			FROM tblLGDeliveryPickDetail Del
 			INNER JOIN tblLGPickLotDetail PLDetail ON PLDetail.intPickLotDetailId = Del.intPickLotDetailId
 			INNER JOIN vyuLGPickOpenInventoryLots Lots ON Lots.intLotId = PLDetail.intLotId
@@ -42,7 +53,7 @@ IF @intSeqId = 13
 			UNION ALL
 			
 			SELECT ri.intInventoryReceiptItemId as intInventoryShipmentItemId,cl.strLocationName,convert(nvarchar,st.intTicketNumber) strTicketNumber,st.dtmTicketDateTime,strCustomerReference,
-					strDistributionOption,isnull(ri.dblReceived, 0) AS dblTotal
+					strDistributionOption,isnull(ri.dblReceived, 0) AS dblTotal,st.intCommodityId
 			FROM tblICInventoryReceipt r
 			INNER JOIN tblICInventoryReceiptItem ri ON r.intInventoryReceiptId = ri.intInventoryReceiptId AND r.strReceiptType = 'Purchase Contract'
 			INNER JOIN tblSCTicket st ON st.intTicketId = ri.intSourceId  AND strDistributionOption IN ('CNT')
@@ -56,9 +67,10 @@ ELSE IF @intSeqId = 14
 	BEGIN
 		IF ISNULL(@intLocationId, 0) <> 0
 		BEGIN
+		Insert into @tblTemp (intInventoryShipmentItemId,strLocationName,strTicketNumber,dtmTicketDateTime,strCustomerReference, strDistributionOption,dblTotal,intCommodityId)
 			SELECT ri.intInventoryShipmentItemId,cl.strLocationName,convert(nvarchar,ch.strContractNumber)+'/'+convert(nvarchar,cd.intContractSeq) strTicketNumber,
 			ch.dtmContractDate as dtmTicketDateTime ,
-			ch.strCustomerContract as strCustomerReference, 'CNT' as strDistributionOption,	ri.dblQuantity AS dblTotal
+			ch.strCustomerContract as strCustomerReference, 'CNT' as strDistributionOption,	ri.dblQuantity AS dblTotal,intCommodityId
 			FROM tblICInventoryShipment r
 			INNER JOIN tblICInventoryShipmentItem ri ON r.intInventoryShipmentId = ri.intInventoryShipmentId
 			INNER JOIN tblCTContractDetail cd ON cd.intContractDetailId = ri.intLineNo AND cd.intPricingTypeId = 2	
@@ -69,9 +81,10 @@ ELSE IF @intSeqId = 14
 		END
 		ELSE
 		BEGIN
+		Insert into @tblTemp (intInventoryShipmentItemId,strLocationName,strTicketNumber,dtmTicketDateTime,strCustomerReference, strDistributionOption,dblTotal,intCommodityId)
 			SELECT ri.intInventoryShipmentItemId,cl.strLocationName,convert(nvarchar,ch.strContractNumber)+'/'+convert(nvarchar,cd.intContractSeq) strTicketNumber,
 			ch.dtmContractDate as dtmTicketDateTime ,
-			ch.strCustomerContract as strCustomerReference, 'CNT' as strDistributionOption,	ri.dblQuantity AS dblTotal
+			ch.strCustomerContract as strCustomerReference, 'CNT' as strDistributionOption,	ri.dblQuantity AS dblTotal,intCommodityId
 			FROM tblICInventoryShipment r
 			INNER JOIN tblICInventoryShipmentItem ri ON r.intInventoryShipmentId = ri.intInventoryShipmentId
 			INNER JOIN tblCTContractDetail cd ON cd.intContractDetailId = ri.intLineNo AND cd.intPricingTypeId = 2	
@@ -80,3 +93,19 @@ ELSE IF @intSeqId = 14
 			WHERE ch.intCommodityId = @intCommodityId 
 		END
 	END
+
+DECLARE @intUnitMeasureId int
+SELECT TOP 1 @intUnitMeasureId = intUnitMeasureId FROM tblRKCompanyPreference
+if isnull(@intUnitMeasureId,'')<> ''
+BEGIN
+
+SELECT intInventoryShipmentItemId,strLocationName,strTicketNumber,dtmTicketDateTime,strCustomerReference, strDistributionOption,
+			isnull(dbo.fnCTConvertQuantityToTargetCommodityUOM(cuc.intCommodityUnitMeasureId,case when isnull(cuc1.intCommodityUnitMeasureId,0) = 0 then cuc.intCommodityUnitMeasureId else cuc1.intCommodityUnitMeasureId end,round(dblTotal,4)),0) dblTotal
+	FROM @tblTemp t
+JOIN tblICCommodityUnitMeasure cuc on t.intCommodityId=cuc.intCommodityId and cuc.ysnDefault=1 
+JOIN tblICCommodityUnitMeasure cuc1 on t.intCommodityId=cuc1.intCommodityId and @intUnitMeasureId=cuc1.intUnitMeasureId
+END
+ELSE
+BEGIN
+SELECT intInventoryShipmentItemId,strLocationName,strTicketNumber,dtmTicketDateTime,strCustomerReference, strDistributionOption,dblTotal FROM @tblTemp
+END
