4fbb56eae3aecb8bfec304365183827e69734474
Premkumar S <prem.selladurai@irely.com>
2015-10-06 16:42:43 +0530

    MFG-385

--- i21Database/dbo/Stored Procedures/uspMFGetScheduleGroupDetail.sql
+++ i21Database/dbo/Stored Procedures/uspMFGetScheduleGroupDetail.sql
@@ -1,5 +1,5 @@
 ﻿CREATE PROCEDURE [uspMFGetScheduleGroupDetail] @intScheduleRuleId INT
-	,@intStart INT = 1
+	,@intStart INT = 0
 	,@intLimit INT = 1
 	,@strFilterCriteria NVARCHAR(MAX) = ''
 AS
@@ -15,7 +15,7 @@ FROM dbo.tblMFScheduleRule R
 JOIN dbo.tblMFScheduleAttribute A ON A.intScheduleAttributeId = R.intScheduleAttributeId
 WHERE R.intScheduleRuleId = @intScheduleRuleId
 
-SET @sqlCommand = 'SELECT TOP '+LTRIM(@intLimit)+' * FROM (SELECT ' + @strColumnName + ' AS strName, ROW_NUMBER() OVER (ORDER BY ' + @strColumnName + ') AS intRowNumber
+SET @sqlCommand = 'SELECT TOP '+LTRIM(@intLimit)+' *,0 as intConcurrencyId FROM (SELECT ' + @strColumnName + ' AS strName, strDescription, ROW_NUMBER() OVER (ORDER BY ' + @strColumnName + ') AS intRowNumber
 						FROM ' + @strTableName + '
 						WHERE ' + @strColumnName + ' NOT IN (SELECT strGroupValue FROM dbo.tblMFScheduleGroupDetail) AND ' + @strColumnName + ' NOT IN (''' + @strFilterCriteria + ''')) AS DT WHERE intRowNumber > ' + LTRIM(@intStart) 
 
--- i21Database/dbo/Views/vyuMFGetPackType.sql
+++ i21Database/dbo/Views/vyuMFGetPackType.sql
@@ -3,4 +3,3 @@ AS
 SELECT P.strPackName
 	,P.strDescription
 FROM tblMFPackType P
-JOIN dbo.tblICItem I ON P.intPackTypeId = I.intPackTypeId
