9cbd48fd30bf3e7eab7e121713335cfbb1f7b3d5
Gopinath Nayak <gopinath.n@irely.com>
2015-10-07 16:59:06 +0530

    GRN-204

--- i21Database/dbo/Views/vyuGRGetTransferStorageType.sql
+++ i21Database/dbo/Views/vyuGRGetTransferStorageType.sql
@@ -5,5 +5,5 @@ SELECT DISTINCT a.intStorageScheduleTypeId
  ,b.intCurrencyID  
 FROM tblGRStorageType a  
 JOIN tblGRStorageScheduleRule b ON b.intStorageType = a.intStorageScheduleTypeId  
-WHERE a.ysnCustomerStorage=0 AND CONVERT(NVARCHAR, GETDATE(), 106) BETWEEN ISNULL(CONVERT(NVARCHAR, b.dtmEffectiveDate, 106), CONVERT(NVARCHAR, GETDATE(), 106))  
+WHERE a.ysnActive=1 AND CONVERT(NVARCHAR, GETDATE(), 106) BETWEEN ISNULL(CONVERT(NVARCHAR, b.dtmEffectiveDate, 106), CONVERT(NVARCHAR, GETDATE(), 106))  
   AND ISNULL(CONVERT(NVARCHAR, b.dtmTerminationDate, 106), CONVERT(NVARCHAR, GETDATE(), 106))
