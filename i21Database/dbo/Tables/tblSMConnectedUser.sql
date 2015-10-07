932405e782eb72fe4f9645a5c4579e309f7d1861
Crisman Lazaga <cris.lazaga@irely.com>
2015-10-07 14:50:26 +0800

    FRM-2064

---
+++ i21Database/dbo/Tables/tblSMConnectedUser.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tblSMConnectedUser]
+(
+	[intConnectedUserId] INT NOT NULL IDENTITY, 
+    [strContextId] NVARCHAR(40) COLLATE Latin1_General_CI_AS NOT NULL, 
+    [intEntityId] INT NOT NULL, 
+    [dtmConnectDate] DATETIME NOT NULL, 
+    [strMachine] NVARCHAR(200) COLLATE Latin1_General_CI_AS NULL, 
+    [strBrowser] NVARCHAR(200) COLLATE Latin1_General_CI_AS NULL, 
+    CONSTRAINT [PK_tblSMConnectedUser] PRIMARY KEY ([intConnectedUserId]), 
+    CONSTRAINT [FK_tblSMConnectedUser_tblEntity] FOREIGN KEY (intEntityId) REFERENCES [tblEntity]([intEntityId]) 
+)
