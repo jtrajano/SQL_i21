cf451343377dc5126a14699cc49bbf1dc2f3d924
Melanie Ferrer <Mel.Ferrer@irely.com>
2015-10-08 14:17:46 +0800

    PAT-46 - Category volume - integrate with other modules and store transaction volumes

---
+++ i21Database/dbo/Tables/tblPATCategoryVolume.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[tblPATCategoryVolume]
+(
+	[intCategoryVolumeId] INT NOT NULL IDENTITY, 
+    [intCustomerPatronId] INT NULL, 
+    [intPatronageCategoryId] INT NULL, 
+    [intFiscalYear] INT NULL, 
+    [dblVolume] NUMERIC(18, 6) NULL, 
+    [intConcurrencyId] INT NULL DEFAULT 0, 
+    CONSTRAINT [PK_tblPATCategoryVolume] PRIMARY KEY ([intCategoryVolumeId]) 
+)
