5323034bea77c4d3f5fdda09fecba89103f3011c
jeffrey trajano <jeffrey.trajano@irely.com>
2015-10-08 12:16:27 +0800

    GL-1939 Change glactmst conversion from origin to i21

---
+++ i21Database/dbo/Tables/tblGLOriginAccounts.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE [dbo].[tblGLOriginAccounts](
+	[glact_acct1_8] [int] NOT NULL,
+	[glact_acct9_16] [int] NOT NULL,
+	[glact_desc] [char](30)   NOT NULL,
+	[glact_type] [char](1)   NULL,
+	[glact_normal_value] [char](1)   NULL,
+	[glact_saf_cat] [char](1)   NULL,
+	[glact_flow_cat] [char](1)   NULL,
+	[glact_uom] [char](6)   NULL,
+	[glact_verify_flag] [char](1)   NULL,
+	[glact_active_yn] [char](1)   NULL,
+	[glact_sys_acct_yn] [char](1)   NULL,
+	[glact_desc_lookup] [char](8) NOT NULL,
+	[glact_user_fld_1] [char](10)  NULL,
+	[glact_user_fld_2] [char](10) NULL,
+	[glact_user_id] [char](16) NULL,
+	[glact_user_rev_dt] [int] NULL,
+	[A4GLIdentity] [numeric](9, 0) NOT NULL,
+	[glact_acct1_8_new] [int] NOT NULL
+) ON [PRIMARY]
+
+GO
+
