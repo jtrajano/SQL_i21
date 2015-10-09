aefb9455f1184015ee4126d50855e2fcc2038b7a
James Balachander <balachander.j@irely.com>
2015-10-09 12:37:33 +0530

    LG-56

--- i21Database/dbo/Views/vyuLGShipmentOpenContracts.sql
+++ i21Database/dbo/Views/vyuLGShipmentOpenContracts.sql
@@ -1,18 +1,26 @@
 CREATE VIEW vyuLGShipmentOpenContracts
 AS
-	SELECT 	CD.intContractDetailId, 
+	SELECT * FROM (
+		SELECT 	CD.intContractDetailId, 
 			CD.intContractHeaderId, 
+			CD.strContractNumber,
 			CD.intContractSeq, 
 			CD.intItemId, 					
 			CD.strItemDescription,
 			CD.dblDetailQuantity,
 			CD.intUnitMeasureId,
 			CD.strItemUOM as strUnitMeasure,
-			CD.dblDetailQuantity - IsNull((SELECT SUM (S.dblQuantity) from tblLGShipmentContractQty S Group By S.intContractDetailId Having CD.intContractDetailId = S.intContractDetailId), 0) AS dblUnShippedQuantity,
-			
+			CD.dblDetailQuantity - 
+				IsNull((SELECT SUM (S.dblQuantity) 
+						from tblLGShipmentContractQty S Group By S.intContractDetailId Having CD.intContractDetailId = S.intContractDetailId), 0) +
+				IsNull((SELECT SUM(S.dblQuantity)
+						from tblLGShipmentBLContainerContract S 
+						LEFT JOIN tblLGShipmentContractQty SC ON SC.intShipmentContractQtyId = S.intShipmentContractQtyId
+						LEFT JOIN tblLGShipmentBLContainer C ON C.intShipmentBLContainerId = S.intShipmentBLContainerId
+						Group By C.ysnRejected, SC.intContractDetailId Having SC.intContractDetailId = CD.intContractDetailId AND C.ysnRejected = 1), 0) 
+				AS dblUnShippedQuantity,
 			CD.intContractTypeId as intPurchaseSale,
 			CD.intEntityId,
-			CD.strContractNumber,
 			CD.dtmContractDate,
 			CD.intCompanyLocationId,
 			CD.intCommodityId,
@@ -29,11 +37,11 @@ AS
 			CD.intShippingLineId as intShippingLineEntityId,
 			CD.intNumberOfContainers,
 			CD.intContainerTypeId,
-			CD.strVessel
+			CD.strVessel,
+			CD.intContractTypeId
 
 	FROM 	vyuCTContractDetailView 		CD
 	LEFT JOIN tblSMCity LoadingPort ON LoadingPort.intCityId = CD.intLoadingPortId
 	LEFT JOIN tblSMCity DestPort ON DestPort.intCityId = CD.intDestinationPortId
-	LEFT JOIN tblSMCity DestCity ON DestCity.intCityId = CD.intDestinationCityId
-	WHERE (CD.dblDetailQuantity - IsNull((SELECT SUM (S.dblQuantity) from tblLGShipmentContractQty S Group By S.intContractDetailId Having CD.intContractDetailId = S.intContractDetailId), 0)) > 0 AND 
-			CD.intContractTypeId = 1
+	LEFT JOIN tblSMCity DestCity ON DestCity.intCityId = CD.intDestinationCityId) t1
+	WHERE t1.dblUnShippedQuantity > 0 AND t1.intContractTypeId = 1
