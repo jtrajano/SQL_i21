00200b48a9ae66f2590d038ed6c5f084a8d1a94f
Chakra <chakradhar.behera@irely.com>
2015-10-14 04:12:41 +0530

    MFG-260

---
+++ i21Database/dbo/Stored Procedures/uspMFCreateUpdateParentLotNumber.sql
@@ -0,0 +1,46 @@
+﻿CREATE PROCEDURE [dbo].[uspMFCreateUpdateParentLotNumber]
+	@strParentLotNumber NVARCHAR(50),
+	@strParentLotAlias  NVARCHAR(50),
+	@intItemId			INT,
+	@dtmExpiryDate		DATETIME,
+	@intLotStatusId		INT,
+	@intUserId			INT,
+	@dtmDate			DATETIME,
+	@intLotId int
+AS
+Begin Try
+
+Declare @ErrMsg nvarchar(Max)
+
+Declare @intParentLotId int
+
+Select @intParentLotId=intParentLotId From tblICParentLot Where strParentLotNumber=@strParentLotNumber
+
+If @dtmDate is null Set @dtmDate=GETDATE()
+
+If Not Exists (Select 1 From tblICLot Where intLotId=@intLotId)
+	RaisError('Lot does not exist for parent lot creation.',16,1)
+
+If ISNULL(@intParentLotId,0)=0
+Begin
+	INSERT INTO tblICParentLot(strParentLotNumber,strParentLotAlias,intItemId,dtmExpiryDate,intLotStatusId,intCreatedUserId,dtmDateCreated)
+	Values(@strParentLotNumber,@strParentLotAlias,@intItemId,@dtmExpiryDate,@intLotStatusId,@intUserId,@dtmDate)
+
+	Select @intParentLotId=SCOPE_IDENTITY()
+	
+	Update tblICLot Set intParentLotId=@intParentLotId Where intLotId=@intLotId
+End
+Else
+Begin
+	If (Select intItemId From tblICParentLot Where intParentLotId =@intParentLotId ) <> @intItemId
+		RaisError('Lot and Parent Lot cannot have different item.',16,1)
+
+	Update tblICLot Set intParentLotId=@intParentLotId Where intLotId=@intLotId	
+End
+
+End Try
+
+Begin Catch
+ SET @ErrMsg = ERROR_MESSAGE()  
+ RAISERROR(@ErrMsg, 16, 1, 'WITH NOWAIT')  
+End Catch
--- i21Database/dbo/Tables/tblICParentLot.sql
+++ i21Database/dbo/Tables/tblICParentLot.sql
@@ -4,21 +4,13 @@
     [strParentLotNumber] NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL, 
     [strParentLotAlias] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
     [intItemId] INT NOT NULL, 
-    [intLocationId] INT NOT NULL, 
-	[dblQty] NUMERIC(18, 6) NOT NULL, 
-    [intItemUOMId] INT NOT NULL, 
-    [dblWeight] NUMERIC(18, 6) NULL DEFAULT 0, 
-    [intWeightUOMId] INT NULL,
-	[dblWeightPerQty] NUMERIC(38,20) DEFAULT 1,
     [dtmExpiryDate] DATETIME NULL, 
 	[intLotStatusId] INT NOT NULL DEFAULT 1,
     [dtmDateCreated] DATETIME NULL, 
     [intCreatedUserId] INT NULL, 
 	[intConcurrencyId] INT NULL CONSTRAINT [DF_tblICParentLot_intConcurrencyId] DEFAULT 0,
 	CONSTRAINT [PK_tblICParentLot_intParentLotId] PRIMARY KEY ([intParentLotId]),
-	CONSTRAINT [UQ_tblICParentLot_strParentLotId] UNIQUE ([strParentLotNumber],[intLocationId]),
+	CONSTRAINT [UQ_tblICParentLot_strParentLotId] UNIQUE ([strParentLotNumber]),
 	CONSTRAINT [FK_tblICParentLot_tblICItem_intItemId] FOREIGN KEY ([intItemId]) REFERENCES [tblICItem]([intItemId]),
-	CONSTRAINT [FK_tblICParentLot_tblICItemUOM_intItemUOMId] FOREIGN KEY ([intItemUOMId]) REFERENCES [tblICItemUOM]([intItemUOMId]), 
-	CONSTRAINT [FK_tblICParentLot_tblICItemUOM_intWeightUOMId] FOREIGN KEY ([intWeightUOMId]) REFERENCES [tblICItemUOM]([intItemUOMId]), 
-	CONSTRAINT [FK_tblICParentLot_tblSMCompanyLocation_intLocationId] FOREIGN KEY ([intLocationId]) REFERENCES [tblSMCompanyLocation]([intCompanyLocationId])
+	CONSTRAINT [FK_tblICParentLot_tblICLotStatus_intLotStatusId] FOREIGN KEY ([intLotStatusId]) REFERENCES [tblICLotStatus]([intLotStatusId])
 )
