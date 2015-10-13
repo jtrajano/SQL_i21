d7f5be29ada58f31c6c1d79c54034e082906dea1
Feb Montefrio <feb.montefrio@irely.com>
2015-10-13 11:24:55 +0800

    IC-1403

--- Test/testi21Database/Stored Procedures/test uspICInventoryAdjustment_CreatePostQtyChange for the basics.sql
+++ Test/testi21Database/Stored Procedures/test uspICInventoryAdjustment_CreatePostQtyChange for the basics.sql
@@ -131,7 +131,7 @@ BEGIN
 	BEGIN 
 		EXEC tSQLt.ExpectException 
 			@ExpectedMessage = 'Internal Error. The source transaction type provided is invalid or not supported.'
-			,@ExpectedErrorNumber = 51124
+			,@ExpectedErrorNumber = 80032
 	END 
 
 	-- Act
