913c61f130536d2d85cffe5a1d7c3f179e576c29
Pruthvi <pruthvi.c@irely.com>
2015-10-14 16:35:27 +0530

    TR-262

--- i21Database/dbo/Stored Procedures/uspTRPosting.sql
+++ i21Database/dbo/Stored Procedures/uspTRPosting.sql
@@ -14,16 +14,40 @@ SET ANSI_WARNINGS OFF
 DECLARE @ErrorMessage NVARCHAR(4000);
 DECLARE @ErrorSeverity INT;
 DECLARE @ErrorState INT;
-
+DECLARE @intEntityId int;
 BEGIN TRY
+
+SELECT	@intEntityId = intEntityId
+FROM	dbo.tblSMUserSecurity 
+WHERE	intUserSecurityID = @intUserId
+
 if @ysnPostOrUnPost = 0 and @ysnRecap = 0
     BEGIN
+	    EXEC uspSMAuditLog 
+              @keyValue = @intTransportLoadId,                         -- Primary Key Value
+              @screenName = 'Transports.view.TransportLoad',           -- Screen Namespace
+              @entityId = @intEntityId,                                -- Entity Id.
+              @actionType = 'Processed',                               -- Action Type
+              @changeDescription = 'UnPosted',                        -- Description
+              @fromValue = '',                                        -- Previous Value
+              @toValue = ''                                           -- New Value
         EXEC uspTRProcessToInvoice @intTransportLoadId,@intUserId,@ysnRecap,@ysnPostOrUnPost
 	    EXEC uspTRProcessToInventoryTransfer @intTransportLoadId,@intUserId,@ysnRecap,@ysnPostOrUnPost
 	    EXEC uspTRProcessToInventoryReceipt @intTransportLoadId,@intUserId,@ysnRecap,@ysnPostOrUnPost
     END
 ELSE
     BEGIN
+	    if @ysnPostOrUnPost = 1
+		BEGIN
+	         EXEC uspSMAuditLog 
+                  @keyValue = @intTransportLoadId,                          -- Primary Key Value
+                  @screenName = 'Transports.view.TransportLoad',            -- Screen Namespace
+                  @entityId = @intEntityId,                                   -- Entity Id.
+                  @actionType = 'Processed',                                -- Action Type
+                  @changeDescription = 'Posted',             -- Description
+                  @fromValue = '',                                         -- Previous Value
+                  @toValue = ''                                            -- New Value
+         END
          EXEC uspTRPostingValidation @intTransportLoadId,@ysnPostOrUnPost
          EXEC uspTRProcessToInventoryReceipt @intTransportLoadId,@intUserId,@ysnRecap,@ysnPostOrUnPost
          EXEC uspTRProcessToInventoryTransfer @intTransportLoadId,@intUserId,@ysnRecap,@ysnPostOrUnPost
