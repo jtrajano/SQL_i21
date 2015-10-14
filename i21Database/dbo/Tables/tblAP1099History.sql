e0689337c55bc17fa3324768ccf7f07853cfe22a
Erick F. Dela Cruz <erick.delacruz@irely.com>
2015-10-15 04:46:16 +0800

    AP-1760

--- i21Database/dbo/Tables/tblAP1099History.sql
+++ i21Database/dbo/Tables/tblAP1099History.sql
@@ -1,4 +1,14 @@
 ﻿CREATE TABLE [dbo].[tblAP1099History]
 (
-	[int1099HistoryId] INT NOT NULL PRIMARY KEY IDENTITY
+	[int1099HistoryId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [intEntityVendorId] INT NOT NULL, 
+    [intYear] INT NOT NULL DEFAULT 0, 
+    [ysnPrinted] BIT NOT NULL DEFAULT 0, 
+    [ysnFiled] BIT NOT NULL DEFAULT 0, 
+    [strComment] NVARCHAR(500) NULL, 
+    [dblAmount] DECIMAL(18, 6) NULL, 
+    [strVendorName] NVARCHAR(200) NULL, 
+    [intConcurrencyId] INT NOT NULL DEFAULT 0, 
+    [dtmDatePrinted] DATETIME NULL, 
+    [dtmDateFiled] DATETIME NULL
 )
