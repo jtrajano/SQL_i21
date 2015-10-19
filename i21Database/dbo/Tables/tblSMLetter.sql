33d8c0f58068a004c38b4915f28bbecadc96a513
Crisman Lazaga <cris.lazaga@irely.com>
2015-10-19 16:37:55 +0800

    FCOM-804

---
+++ i21Database/dbo/Tables/tblSMLetter.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[tblSMLetter]
+(
+	[intLetterId] INT NOT NULL IDENTITY, 
+    [strName] NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL, 
+    [strDescription] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
+    [blbMessage] VARBINARY(MAX) NULL, 
+    [intConcurrencyId] INT NOT NULL, 
+    CONSTRAINT [PK_tblSMLetter] PRIMARY KEY ([intLetterId]) 
+)
