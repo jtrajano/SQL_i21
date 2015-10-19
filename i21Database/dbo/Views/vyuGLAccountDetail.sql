7b5fbed0b7be9a3d186a098879f26fce0145983e
jeffrey trajano <jeffrey.trajano@irely.com>
2015-10-19 17:18:12 +0800

    GL-2258 Modify Chart of Accounts web api

--- i21Database/dbo/Views/vyuGLAccountDetail.sql
+++ i21Database/dbo/Views/vyuGLAccountDetail.sql
@@ -1,15 +1,16 @@
 ﻿CREATE VIEW [dbo].[vyuGLAccountDetail]
 AS
-	SELECT        dbo.tblGLAccount.intAccountId, dbo.tblGLAccount.strAccountId, dbo.tblGLAccountCategory.strAccountCategory, dbo.tblGLAccount.strDescription, dbo.tblGLAccountGroup.strAccountGroup, 
-                         dbo.tblGLAccount.ysnActive, dbo.tblGLAccount.strCashFlow, dbo.tblGLAccount.intCurrencyID, dbo.tblGLAccount.intCurrencyExchangeRateTypeId, dbo.tblGLAccount.strNote, dbo.tblGLAccount.strComments, 
-                         dbo.tblSMCurrency.strCurrency, dbo.tblGLAccount.intAccountGroupId, dbo.tblGLAccountGroup.strAccountType, dbo.tblGLAccountSegment.intAccountCategoryId
+	SELECT        dbo.tblGLAccount.strAccountId, dbo.tblGLAccount.strDescription, dbo.tblGLAccountGroup.strAccountGroup, dbo.tblGLAccountGroup.strAccountType, dbo.tblGLAccountCategory.strAccountCategory, 
+                         dbo.tblGLAccount.strComments, dbo.tblGLAccount.strCashFlow, dbo.tblGLAccount.ysnActive, dbo.tblGLAccount.ysnSystem, dbo.tblGLAccount.ysnRevalue, dbo.tblGLAccountUnit.intAccountUnitId, 
+                         dbo.tblGLAccountUnit.strUOMCode, dbo.tblGLAccount.intAccountId, dbo.tblGLAccount.intCurrencyID, dbo.tblGLAccount.intCurrencyExchangeRateTypeId, dbo.tblGLAccount.strNote, dbo.tblSMCurrency.strCurrency, 
+                         dbo.tblSMCurrencyExchangeRateType.strCurrencyExchangeRateType, dbo.tblGLAccount.intAccountGroupId, dbo.tblGLAccountSegment.intAccountCategoryId
 FROM            dbo.tblGLAccount INNER JOIN
                          dbo.tblGLAccountSegmentMapping ON dbo.tblGLAccount.intAccountId = dbo.tblGLAccountSegmentMapping.intAccountId INNER JOIN
                          dbo.tblGLAccountSegment ON dbo.tblGLAccountSegmentMapping.intAccountSegmentId = dbo.tblGLAccountSegment.intAccountSegmentId INNER JOIN
                          dbo.tblGLAccountStructure ON dbo.tblGLAccountSegment.intAccountStructureId = dbo.tblGLAccountStructure.intAccountStructureId INNER JOIN
                          dbo.tblGLAccountCategory ON dbo.tblGLAccountSegment.intAccountCategoryId = dbo.tblGLAccountCategory.intAccountCategoryId LEFT OUTER JOIN
+                         dbo.tblSMCurrencyExchangeRateType ON dbo.tblGLAccount.intCurrencyExchangeRateTypeId = dbo.tblSMCurrencyExchangeRateType.intCurrencyExchangeRateTypeId LEFT OUTER JOIN
                          dbo.tblSMCurrency ON dbo.tblGLAccount.intCurrencyID = dbo.tblSMCurrency.intCurrencyID LEFT OUTER JOIN
                          dbo.tblGLAccountUnit ON dbo.tblGLAccount.intAccountUnitId = dbo.tblGLAccountUnit.intAccountUnitId LEFT OUTER JOIN
                          dbo.tblGLAccountGroup ON dbo.tblGLAccount.intAccountGroupId = dbo.tblGLAccountGroup.intAccountGroupId
 WHERE        (dbo.tblGLAccountStructure.strType = 'Primary')
-
