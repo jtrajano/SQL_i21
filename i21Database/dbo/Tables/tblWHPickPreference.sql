6e0cd544a22dc5d45aac77dd67f17d6985bb7701
Swetosuvro <swetosuvro.g@irely.com>
2015-10-19 21:32:42 +0530

    WH-44

---
+++ i21Database/dbo/Tables/tblWHPickPreference.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[tblWHPickPreference]
+(
+	intPickPreferenceId INT NOT NULL PRIMARY KEY,
+	intPreferenceType INT NULL,
+	strDescription NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL, 
+	ysnDefault BIT,
+	strInternalCode NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL
+)
