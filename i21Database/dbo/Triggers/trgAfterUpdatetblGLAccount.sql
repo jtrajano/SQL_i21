d91663fffc4af7d77e6b0941d819f1103a459832
jeffrey trajano <jeffrey.trajano@irely.com>
2015-10-19 16:10:41 +0800

    GL-2257 Modify trgAfterUpdatetblGLAccount trigger

--- i21Database/dbo/Triggers/trgAfterUpdatetblGLAccount.sql
+++ i21Database/dbo/Triggers/trgAfterUpdatetblGLAccount.sql
@@ -6,19 +6,6 @@ BEGIN
 	INSERT INTO tblGLAccountAdjustmentLog(intPrimaryKey,strColumn,strAction,dtmAction,strOriginalValue,strNewValue,intEntityId,strTable,strName)
 	SELECT 
 		d.intAccountId
-		,'intAccountCategoryId'
-		,'Move'
-		,getdate()
-		,CAST(d.intAccountCategoryId as nvarchar(10)) 
-		,CAST(i.intAccountCategoryId AS nvarchar(10))
-		,i.[intEntityIdLastModified],'tblGLAccount',d.strAccountId
-	FROM deleted d 
-	JOIN inserted i ON i.intAccountId = d.intAccountId
-	WHERE i.intAccountCategoryId != d.intAccountCategoryId
-	
-	INSERT INTO tblGLAccountAdjustmentLog(intPrimaryKey,strColumn,strAction,dtmAction,strOriginalValue,strNewValue,intEntityId,strTable,strName)
-	SELECT 
-		d.intAccountId
 		,'intAccountGroupId'
 		,'Move'
 		,getdate()
