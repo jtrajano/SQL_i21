fb9f3bfae197078c5b7d2431b478fef5de5729e8
Emerson Versoza <emerson.versoza@irely.com>
2015-10-23 10:59:25 +0800

    GRN-162

---
+++ i21Database/dbo/Tables/tblSCLastScaleSetup.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[tblSCLastScaleSetup]
+(
+	[intLastScaleSetupId] [int] IDENTITY(1,1) NOT NULL,
+	[intScaleSetupId] [int] NULL,
+	[dtmScaleDate] [date] NULL,
+	[strScaleOperator] [varchar](40) NULL,
+	[intConcurrencyId] [int] NULL, 
+    CONSTRAINT [PK_tblSCLastScaleSetup] PRIMARY KEY ([intLastScaleSetupId]), 
+)
