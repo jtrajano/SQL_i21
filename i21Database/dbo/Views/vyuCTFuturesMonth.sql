e66de78e0b1f16abc46b18ff417dcd8e4df66ca7
Satya <ssahoo@BLRSATYAWIN7.irely.net>
2015-10-24 23:37:15 +0530

    CT-425

---
+++ i21Database/dbo/Views/vyuCTFuturesMonth.sql
@@ -0,0 +1,8 @@
+﻿CREATE VIEW [dbo].[vyuCTFuturesMonth]
+
+AS 
+
+	SELECT	intFutureMonthId,
+			REPLACE(strFutureMonth,' ','('+strSymbol+') ') strFutureMonthYear,
+			intFutureMarketId 
+	FROM	tblRKFuturesMonth
