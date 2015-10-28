8b94de70868a880f3dcfb1ae94974a1a4a48c10a
Dan Michael Jalimao <dan.jalimao@irely.com>
2015-10-28 20:23:45 +0800

    FCOM-912

--- i21Database/dbo/Tables/tblSMApprovalListUserSecurity.sql
+++ i21Database/dbo/Tables/tblSMApprovalListUserSecurity.sql
@@ -3,7 +3,12 @@
 	[intApprovalListUserSecurityId] INT NOT NULL PRIMARY KEY IDENTITY, 
     [intApprovalListId] INT NOT NULL, 
     [intEntityUserSecurityId] INT NOT NULL, 
-	[intSort] INT NOT NULL,
+	[intApproverLevel] INT NOT NULL DEFAULT 1,
+	[intAlternateEntityUserSecurityId] INT NULL, 
+	[dblAmountOver] NUMERIC(18, 6) NOT NULL DEFAULT 0,
+	[dblAmountLessThanEqual] NUMERIC(18, 6) NOT NULL DEFAULT 0,
+	[intSort] INT NOT NULL DEFAULT 0,
     [intConcurrencyId] INT NOT NULL DEFAULT 1, 
-    CONSTRAINT [FK_tblSMApprovalListUserSecurity_tblSMUserSecurity] FOREIGN KEY ([intEntityUserSecurityId]) REFERENCES tblSMUserSecurity([intEntityUserSecurityId])
+    CONSTRAINT [FK_tblSMApprovalListUserSecurity_tblSMUserSecurity] FOREIGN KEY ([intEntityUserSecurityId]) REFERENCES tblSMUserSecurity([intEntityUserSecurityId]),
+	CONSTRAINT [FK_tblSMApprovalListUserSecurity_tblSMUserSecurity_Alternate] FOREIGN KEY ([intAlternateEntityUserSecurityId]) REFERENCES tblSMUserSecurity([intEntityUserSecurityId])
 )
