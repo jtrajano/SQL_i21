c09f9cacdd3b8002b6bd1cc3c8f9009dd8cd7a1b
Satya <ssahoo@BLRSATYAWIN7.irely.net>
2015-10-28 15:51:12 +0530

    CT-183

--- i21Database/dbo/Tables/tblCTCompanyPreference.sql
+++ i21Database/dbo/Tables/tblCTCompanyPreference.sql
@@ -13,7 +13,9 @@
 	[strPricingQuantity] NVARCHAR(50) COLLATE Latin1_General_CI_AS,
 	[intCleanCostCurrencyId] INT NULL,
 	[intCleanCostUOMId] INT NULL,
-	CONSTRAINT [PK_tblCTCompanyPreference_intCompanyPreferenceId] PRIMARY KEY CLUSTERED ([intCompanyPreferenceId] ASC),
+	[strDefaultContractReport] NVARCHAR(50) COLLATE Latin1_General_CI_AS, 
+
+    CONSTRAINT [PK_tblCTCompanyPreference_intCompanyPreferenceId] PRIMARY KEY CLUSTERED ([intCompanyPreferenceId] ASC),
 	CONSTRAINT [FK_tblCTCompanyPreference_tblSMCurrency_intCleanCostCurrencyId_intCurrencyId] FOREIGN KEY ([intCleanCostCurrencyId]) REFERENCES [tblSMCurrency]([intCurrencyID]),
 	CONSTRAINT [FK_tblCTCompanyPreference_tblICUnitMeasure_intCleanCostUOMId_intUnitMeasureId] FOREIGN KEY ([intCleanCostUOMId]) REFERENCES [tblICUnitMeasure]([intUnitMeasureId])
 
