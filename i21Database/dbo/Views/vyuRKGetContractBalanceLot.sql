2cd37c926abb79b5a9147a41ec5bb093a320da8d
Ramachandran <ramachandran.shanmuganathan@irely.com>
2015-10-28 13:49:47 +0530

    RM-151

--- i21Database/dbo/Views/vyuRKGetContractBalanceLot.sql
+++ i21Database/dbo/Views/vyuRKGetContractBalanceLot.sql
@@ -5,10 +5,11 @@ case when isnull(dblAvailableLot,0)-dblSelectedLot <=0 then 0 else isnull(dblAva
  FROM
 (SELECT cd.intContractDetailId, strContractNumber, intContractSeq as intContractSeq,
 		strContractNumber + ' - ' + CONVERT(varchar,intContractSeq) as strContractSeq,
+		cd.strEntityName, 
 		cd.intFutureMarketId,cd.intFutureMonthId, cd.strContractType,
 		isnull(SUM(cd.dblNoOfLots),0) as dblAvailableLot,
 		(SELECT convert(decimal, isnull(SUM(fot.dblAssignedLots),0.0)) 
 		 FROM tblRKAssignFuturesToContractSummary fot WHERE fot.intContractDetailId= cd.intContractDetailId) as dblSelectedLot		 
 FROM vyuCTContractDetailView cd
-WHERE ysnAllowedToShow=1 AND cd.intFutureMarketId IS NOT NULL AND cd.intFutureMonthId IS NOT NULL
-GROUP BY strContractNumber,cd.intContractDetailId,intContractSeq,cd.intFutureMarketId,cd.intFutureMonthId,cd.strContractType)t  
+WHERE cd.intFutureMarketId IS NOT NULL AND cd.intFutureMonthId IS NOT NULL
+GROUP BY strContractNumber,cd.intContractDetailId,intContractSeq,cd.intFutureMarketId,cd.intFutureMonthId,cd.strContractType,cd.strEntityName)t  
