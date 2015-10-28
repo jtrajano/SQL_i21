ee287af977626dc14e09292a4c2d90f39bf94781
Premkumar S <prem.selladurai@irely.com>
2015-10-28 12:36:46 +0530

    MFG-842

---
+++ i21Database/dbo/Tables/tblMFReportProperty.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE dbo.tblMFReportProperty (
+	intReportPropertyId INT NOT NULL identity(1, 1) CONSTRAINT PK_tblMFReportProperty_intReportPropertyId PRIMARY KEY
+	,strReportName NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL
+	,intPropertyId INT NOT NULL
+	,intSequenceNo INT NOT NULL
+	,CONSTRAINT PK_tblMFReportProperty_intPropertyId FOREIGN KEY (intPropertyId) REFERENCES tblQMProperty(intPropertyId)
+	,CONSTRAINT UQ_tblMFReportProperty_strReportName_intPropertyId UNIQUE (
+		strReportName
+		,intPropertyId
+		)
+	)
