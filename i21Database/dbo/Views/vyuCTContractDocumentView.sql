fede5add9e87f7b5388ab87e90e6e27292cbf81e
Satya <ssahoo@BLRSATYAWIN7.irely.net>
2015-10-28 15:48:22 +0530

    strDocumentType added in the view

--- i21Database/dbo/Views/vyuCTContractDocumentView.sql
+++ i21Database/dbo/Views/vyuCTContractDocumentView.sql
@@ -7,8 +7,12 @@ AS
 			ID.strDocumentName,
 			ID.strDescription,
 			ID.ysnStandard,
-			ID.intCommodityId
-
+			ID.intCommodityId,
+			CASE	WHEN ID.intDocumentType = 1	THEN 'Contract'
+					WHEN ID.intDocumentType = 2	THEN 'Bill Of Lading'
+					WHEN ID.intDocumentType = 3	THEN 'Container'
+					ELSE ''
+			END AS strDocumentType
 	FROM	tblCTContractDocument	CD
 	JOIN	tblCTContractHeader CH	ON	CH.intContractHeaderId	=	CD.intContractHeaderId
 	JOIN	tblICDocument		ID	ON	ID.intDocumentId		=	CD.intDocumentId
