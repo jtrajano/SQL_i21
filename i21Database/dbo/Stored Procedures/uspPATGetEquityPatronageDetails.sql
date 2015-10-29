e52763c26328c82561ee76c1d0de0bee0c5551f4
Melanie Ferrer <Mel.Ferrer@irely.com>
2015-10-29 16:23:23 +0800

    PAT-98 - Equity Details

---
+++ i21Database/dbo/Stored Procedures/uspPATGetEquityPatronageDetails.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROCEDURE uspPATGetEquityPatronageDetails 
+	@intRefundTypeId INT = NULL
+AS
+BEGIN
+	SELECT RR.intRefundTypeId, 
+		   PC.strCategoryCode,
+		   PC.strPurchaseSale,
+		   PC.strUnitAmount,
+		   CE.dblEquity,
+		   RR.intConcurrencyId
+	  FROM tblPATRefundRate RR
+INNER JOIN tblPATRefundRateDetail RRD
+		ON RRD.intRefundTypeId = RR.intRefundTypeId
+INNER JOIN tblPATPatronageCategory PC
+		ON PC.intPatronageCategoryId = RRD.intPatronageCategoryId
+INNER JOIN tblPATCustomerEquity CE
+		ON CE.intRefundTypeId = RR.intRefundTypeId
+	 WHERE RR.intRefundTypeId = @intRefundTypeId
+
+
+END
+GO
---
+++ i21Database/dbo/Views/vyuPATEquityDetails.sql
@@ -0,0 +1,24 @@
+﻿CREATE VIEW [dbo].[vyuPATEquityDetails]
+	AS 
+	 SELECT CE.intCustomerEquityId,
+			CE.intRefundTypeId,
+			CE.intCustomerId,
+			ENT.strName,
+			CE.intFiscalYearId,
+			FY.strFiscalYear,
+			AR.strStockStatus,
+			TC.strTaxCode,
+			CE.dtmLastActivityDate,
+			CE.dblEquity,
+			CE.intConcurrencyId 
+	   FROM tblPATCustomerEquity CE
+ INNER JOIN tblEntity ENT
+		 ON ENT.intEntityId = CE.intCustomerId
+ INNER JOIN tblPATRefundRate RR
+		 ON RR.intRefundTypeId = CE.intRefundTypeId
+ INNER JOIN tblGLFiscalYear FY
+		 ON FY.intFiscalYearId = CE.intFiscalYearId
+ INNER JOIN tblARCustomer AR
+		 ON AR.intEntityCustomerId = CE.intCustomerId
+  LEFT JOIN tblSMTaxCode TC
+		 ON TC.intTaxCodeId = AR.intTaxCodeId
