9bb455723bcaa60b80d425560fa743974707c1c9
Melanie Ferrer <Mel.Ferrer@irely.com>
2015-10-29 13:46:03 +0800

    PAT-97 - Volume Details

---
+++ i21Database/dbo/Stored Procedures/uspPATGetPatronageDetails.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE uspPATGetPatronageDetails 
+	@intCustomerId INT = NULL
+AS
+BEGIN
+	SELECT CV.intPatronageCategoryId,
+		   PC.strCategoryCode,
+		   PC.strPurchaseSale,
+		   PC.strUnitAmount,
+		   CV.dblVolume,
+		   CV.intConcurrencyId
+	  FROM tblPATCustomerVolume CV
+INNER JOIN tblPATPatronageCategory PC
+		ON PC.intPatronageCategoryId = CV.intPatronageCategoryId
+	 WHERE intCustomerPatronId = @intCustomerId
+
+END
+GO
---
+++ i21Database/dbo/Views/vyuPATVolumeDetails.sql
@@ -0,0 +1,27 @@
+﻿CREATE VIEW [dbo].[vyuPATVolumeDetails]
+		 AS 
+	 SELECT CV.intCategoryVolumeId,
+			CV.intCustomerPatronId,
+			ENT.strName,
+			CV.intPatronageCategoryId,
+			PC.strPurchaseSale,
+			CV.intFiscalYear,
+			FY.strFiscalYear,
+			AR.strStockStatus,
+			TC.strTaxCode,
+			dblPurchase = CASE WHEN PC.strPurchaseSale = 'Purchase' THEN CV.dblVolume ELSE 0 END,
+			dblSale = CASE WHEN PC.strPurchaseSale = 'Sale' THEN CV.dblVolume ELSE 0 END,
+			CV.dtmLastActivityDate,
+			CV.dblVolume,
+			CV.intConcurrencyId 
+	   FROM tblPATCustomerVolume CV
+ INNER JOIN tblEntity ENT
+		 ON ENT.intEntityId = intCustomerPatronId
+ INNER JOIN tblPATPatronageCategory PC
+		 ON PC.intPatronageCategoryId = CV.intPatronageCategoryId
+ INNER JOIN tblGLFiscalYear FY
+		 ON FY.intFiscalYearId = CV.intFiscalYear
+ INNER JOIN tblARCustomer AR
+		 ON AR.intEntityCustomerId = CV.intCustomerPatronId
+  LEFT JOIN tblSMTaxCode TC
+		 ON TC.intTaxCodeId = AR.intTaxCodeId
