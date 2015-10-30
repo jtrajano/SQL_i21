957808b62586e17795454e1dbf510c02002932d6
Erick F. Dela Cruz <erick.delacruz@irely.com>
2015-10-30 18:10:34 +0800

    AP-1710

--- i21Database/Scripts/Post-Deployment/SM/DefaultData/16_PaymentMethod.sql
+++ i21Database/Scripts/Post-Deployment/SM/DefaultData/16_PaymentMethod.sql
@@ -4,10 +4,4 @@
 		INSERT INTO tblSMPaymentMethod([strPaymentMethod], [strPaymentMethodCode], [intAccountId], [strPrintOption], [ysnActive], [intSort])
 		VALUES('ACH', NULL, 0, NULL, 1, 0)
 	END
-
-	IF NOT EXISTS(SELECT TOP 1 1 FROM tblSMPaymentMethod WHERE strPaymentMethod LIKE '%Write Off%')
-	BEGIN
-		INSERT INTO tblSMPaymentMethod([strPaymentMethod], [strPaymentMethodCode], [intAccountId], [strPrintOption], [ysnActive], [intSort])
-		VALUES('Write Off', NULL, 0, NULL, 1, 0)
-	END
 GO
