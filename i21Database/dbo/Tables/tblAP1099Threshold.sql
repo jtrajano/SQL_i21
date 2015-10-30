13f582383a5510fa8225fce4b05bdb734798e7fb
Erick F. Dela Cruz <erick.delacruz@irely.com>
2015-10-30 13:13:39 +0800

    AP-1771

--- i21Database/dbo/Tables/tblAP1099Threshold.sql
+++ i21Database/dbo/Tables/tblAP1099Threshold.sql
@@ -15,5 +15,8 @@
     [dbl1099MISCCrop] DECIMAL(18, 6) NOT NULL DEFAULT 0, 
     [dbl1099MISCExcessGolden] DECIMAL(18, 6) NOT NULL DEFAULT 0, 
     [dbl1099MISCGrossProceeds] DECIMAL(18, 6) NOT NULL DEFAULT 0, 
+	[strContactName] NVARCHAR(100) NULL,
+	[strContactPhone] NVARCHAR(100) NULL,
+	[strContactEmail] NVARCHAR(100) NULL,
     [intConcurrencyId] INT NOT NULL DEFAULT 0
 )
