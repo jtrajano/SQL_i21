1ff38d4dbda04b824d8b915df837602386b7140c
Satya sankalpa sahoo <satya.sahoo@irely.com>
2015-11-01 20:11:58 +0530

    CT-286

---
+++ i21Database/dbo/Views/vyuCTItemContractView.sql
@@ -0,0 +1,29 @@
+﻿CREATE VIEW [dbo].[vyuCTItemContractView]
+	
+AS 
+	
+	SELECT	IC.intItemContractId,
+			IM.intCommodityId,
+			IC.intItemId,
+			IC.intItemLocationId,
+			IC.strContractItemName,
+			IC.intCountryId,
+			IC.strGrade,
+			IC.strGradeType,
+			IC.strGarden,
+			IC.dblYieldPercent,
+			IC.dblTolerancePercent,
+			IC.dblFranchisePercent,
+			IM.strItemNo,
+			IM.strDescription strItemDescription,
+			RY.strCountry strOrigin,
+			CL.strLocationName,
+			CL.intCompanyLocationId AS	intLocationId
+
+	FROM	tblICItemContract		IC
+	JOIN	tblICItem				IM	ON	IM.intItemId			=	IC.intItemId
+	JOIN	tblICItemLocation		IL	ON	IL.intItemLocationId	=	IC.intItemLocationId
+	JOIN	tblSMCompanyLocation	CL	ON	CL.intCompanyLocationId =	IL.intLocationId		LEFT
+	JOIN	tblSMCountry			RY	ON	RY.intCountryID			=	IC.intCountryId 
+	
+
