bfb7e206ec0a0da6509ff93db8b99267ef9bb5a8
Melanie Ferrer <Mel.Ferrer@irely.com>
2015-11-02 17:01:40 +0800

    PAT-56

---
+++ i21Database/dbo/Tables/tblPATDividends.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[tblPATDividends]
+(
+	[intDividendId] INT NOT NULL IDENTITY, 
+    [intFiscalYearId] INT NULL, 
+    [dtmProcessDate] DATETIME NULL, 
+    [dtmProcessingFrom] DATETIME NULL, 
+    [dtmProcessingTo] DATETIME NULL, 
+    [dblProcessedDays] NUMERIC(18, 6) NULL, 
+    [dblDividendNo] NUMERIC(18, 6) NULL, 
+    [dblMinimumDividend] NUMERIC(18, 6) NULL, 
+    [dblProrateDividend] NUMERIC(18, 6) NULL, 
+    [dtmCutoffDate] DATETIME NULL, 
+    [dblFederalTaxWithholding] NUMERIC(18, 6) NULL, 
+    [intConcurrencyId] INT NULL DEFAULT 0, 
+    CONSTRAINT [PK_tblPATDividends] PRIMARY KEY ([intDividendId]) 
+)
---
+++ i21Database/dbo/Tables/tblPATDividendsCustomer.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[tblPATDividendsCustomer]
+(
+	[intDividendCustomerId] INT NOT NULL IDENTITY, 
+    [intDividendId] INT NOT NULL, 
+    [intCustomerId] INT NULL, 
+    [strStockStatus] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
+    [dtmLastActivityDate] DATETIME NULL, 
+    [dblLessFWT] NUMERIC(18, 6) NULL, 
+    [dblCheckAmount] NUMERIC(18, 6) NULL, 
+    [intConcurrencyId] INT NULL DEFAULT 0, 
+    CONSTRAINT [PK_tblPATDividendsCustomer] PRIMARY KEY ([intDividendCustomerId]), 
+    CONSTRAINT [FK_tblPATDividendsCustomer_tblPATDividends] FOREIGN KEY ([intDividendId]) REFERENCES [tblPATDividends]([intDividendId]) 
+)
