2db2970ec4ede41201027feca16a153754fc2f80
Premkumar S <prem.selladurai@irely.com>
2015-11-02 12:35:29 +0530

    MFG-842

--- i21Database/dbo/Stored Procedures/uspMFReportDailyProduction.sql
+++ i21Database/dbo/Stored Procedures/uspMFReportDailyProduction.sql
@@ -6,7 +6,6 @@ BEGIN TRY
 		,@dtmEndDate DATETIME
 		,@dtmStartDate1 DATETIME
 		,@dtmEndDate1 DATETIME
-		
 		,@intLocationId INT
 		,@xmlDocumentId INT
 
@@ -48,8 +47,8 @@ BEGIN TRY
 	FROM @temp_xml_table
 	WHERE [fieldname] = 'dtmEndDate'
 
-	SELECT @dtmStartDate1=@dtmStartDate,@dtmEndDate1=@dtmEndDate
-
+	SELECT @dtmStartDate1 = @dtmStartDate
+		,@dtmEndDate1 = @dtmEndDate
 
 	SELECT @intLocationId = [from]
 	FROM @temp_xml_table
@@ -57,9 +56,10 @@ BEGIN TRY
 
 	DECLARE @dtmShiftStartTime DATETIME
 		,@dtmShiftEndTime DATETIME
-		,@strCompanyName nvarchar(50)
+		,@strCompanyName NVARCHAR(50)
 
-	SELECT @strCompanyName=strCompanyName FROM dbo.tblSMCompanySetup
+	SELECT @strCompanyName = strCompanyName
+	FROM dbo.tblSMCompanySetup
 
 	SELECT TOP 1 @dtmShiftStartTime = dtmShiftStartTime + intStartOffset
 	FROM dbo.tblMFShift
@@ -75,119 +75,132 @@ BEGIN TRY
 
 	SELECT @dtmEndDate = @dtmEndDate + @dtmShiftEndTime
 
-	SELECT *
-	FROM (
-		SELECT L.strLotNumber
-			,LT.dtmDate
-			,LT.dtmCreated
-			,LTT.strName
-			,I.strItemNo
-			,I.strDescription
-			,LT.dblQty
-			,UM.strUnitMeasure
-			,SL.strName AS strStorageLocationName
-			,@strCompanyName AS strCompanyName
-			,@dtmStartDate1 AS dtmStartDate
-			,@dtmEndDate1 AS dtmEndDate
-			,'' as strShiftName
-			,L.dtmExpiryDate 
-		FROM dbo.tblICLot L
-		JOIN dbo.tblICInventoryLotTransaction LT ON L.intLotId = LT.intLotId
-		JOIN dbo.tblICInventoryTransactionType LTT ON LT.intTransactionTypeId = LTT.intTransactionTypeId
-		JOIN dbo.tblICItem I ON L.intItemId = I.intItemId
-		JOIN dbo.tblICItemUOM IU ON L.intItemUOMId = IU.intItemUOMId
-		JOIN dbo.tblICStorageLocation SL ON L.intStorageLocationId = SL.intStorageLocationId
-		JOIN dbo.tblICUnitMeasure UM ON IU.intUnitMeasureId = UM.intUnitMeasureId
-		WHERE LT.intTransactionTypeId = 9
-			AND dtmDate >= @dtmStartDate
-			AND dtmDate <= @dtmEndDate
-			AND LT.dblQty > 0
-		
-		UNION
-		
-		SELECT L.strLotNumber
-			,LT.dtmDate
-			,LT.dtmCreated
-			,LTT.strName
-			,I.strItemNo
-			,I.strDescription
-			,LT.dblQty
-			,UM.strUnitMeasure
-			,SL.strName
-			,@strCompanyName as strCompanyName
-			,@dtmStartDate1 AS dtmStartDate
-			,@dtmEndDate1 AS dtmEndDate
-			,'' as strShiftName
-			,L.dtmExpiryDate 
-		FROM dbo.tblICLot L
-		JOIN dbo.tblICInventoryLotTransaction LT ON L.intLotId = LT.intLotId
-		JOIN dbo.tblICInventoryTransactionType LTT ON LT.intTransactionTypeId = LTT.intTransactionTypeId
-		JOIN dbo.tblICItem I ON L.intItemId = I.intItemId
-		JOIN dbo.tblICItemUOM IU ON L.intItemUOMId = IU.intItemUOMId
-		JOIN dbo.tblICStorageLocation SL ON L.intStorageLocationId = SL.intStorageLocationId
-		JOIN dbo.tblICUnitMeasure UM ON IU.intUnitMeasureId = UM.intUnitMeasureId
-		WHERE LT.intTransactionTypeId = 9
-			AND dtmDate >= @dtmStartDate
-			AND dtmDate <= @dtmEndDate
-			AND LT.dblQty < 0
-		
-		UNION
-		
-		SELECT L.strLotNumber
-			,A.dtmAdjustmentDate
-			,A.dtmAdjustmentDate
-			,LTT.strName
-			,I.strItemNo
-			,I.strDescription
-			,- AD.dblQuantity
-			,UM.strUnitMeasure
-			,SL.strName
-			,@strCompanyName as strCompanyName
-			,@dtmStartDate1 AS dtmStartDate
-			,@dtmEndDate1 AS dtmEndDate
-			,'' as strShiftName
-			,L.dtmExpiryDate 
-		FROM tblICInventoryAdjustment A
-		JOIN tblICInventoryAdjustmentDetail AD ON A.intInventoryAdjustmentId = AD.intInventoryAdjustmentId
-		JOIN tblICLot L ON AD.intLotId = L.intLotId
-		JOIN dbo.tblICInventoryTransactionType LTT ON A.intAdjustmentType = LTT.intTransactionTypeId
-		JOIN dbo.tblICItem I ON L.intItemId = I.intItemId
-		JOIN dbo.tblICItemUOM IU ON L.intItemUOMId = IU.intItemUOMId
-		JOIN dbo.tblICStorageLocation SL ON L.intStorageLocationId = SL.intStorageLocationId
-		JOIN dbo.tblICUnitMeasure UM ON IU.intUnitMeasureId = UM.intUnitMeasureId
-		WHERE A.intAdjustmentType = 15
-			AND A.dtmAdjustmentDate >= @dtmStartDate
-			AND A.dtmAdjustmentDate <= @dtmEndDate
-		
-		UNION
-		
-		SELECT L.strLotNumber
-			,A.dtmAdjustmentDate
-			,A.dtmAdjustmentDate
-			,LTT.strName
-			,I.strItemNo
-			,I.strDescription
-			,AD.dblQuantity
-			,UM.strUnitMeasure
-			,SL.strName
-			,@strCompanyName as strCompanyName
-			,@dtmStartDate1 AS dtmStartDate
-			,@dtmEndDate1 AS dtmEndDate
-			,'' as strShiftName
-			,L.dtmExpiryDate 
-		FROM tblICInventoryAdjustment A
-		JOIN tblICInventoryAdjustmentDetail AD ON A.intInventoryAdjustmentId = AD.intInventoryAdjustmentId
-		JOIN tblICLot L ON AD.intLotId = L.intLotId
-		JOIN dbo.tblICInventoryTransactionType LTT ON A.intAdjustmentType = LTT.intTransactionTypeId
-		JOIN dbo.tblICItem I ON AD.intNewItemId = I.intItemId
-		JOIN dbo.tblICItemUOM IU ON L.intItemUOMId = IU.intItemUOMId
-		JOIN dbo.tblICStorageLocation SL ON L.intStorageLocationId = SL.intStorageLocationId
-		JOIN dbo.tblICUnitMeasure UM ON IU.intUnitMeasureId = UM.intUnitMeasureId
-		WHERE A.intAdjustmentType = 15
-			AND A.dtmAdjustmentDate >= @dtmStartDate
-			AND A.dtmAdjustmentDate <= @dtmEndDate
-		) DT
-	ORDER BY dtmCreated 
+	SELECT L.strLotNumber
+		,LT.dtmDate
+		,LT.dtmCreated
+		,LTT.strName
+		,I.strItemNo
+		,I.strDescription
+		,LT.dblQty
+		,UM.strUnitMeasure
+		,SL.strName AS strStorageLocationName
+		,@strCompanyName AS strCompanyName
+		,@dtmStartDate1 AS dtmStartDate
+		,@dtmEndDate1 AS dtmEndDate
+		,'' AS strShiftName
+		,L.dtmExpiryDate
+		,LS.strSecondaryStatus 
+	INTO #tblMFDailyProduction
+	FROM dbo.tblICLot L
+	JOIN dbo.tblICInventoryLotTransaction LT ON L.intLotId = LT.intLotId
+	JOIN dbo.tblICInventoryTransactionType LTT ON LT.intTransactionTypeId = LTT.intTransactionTypeId
+	JOIN dbo.tblICLotStatus  LS on L.intLotStatusId =LS.intLotStatusId 
+	JOIN dbo.tblICItem I ON L.intItemId = I.intItemId
+	JOIN dbo.tblICItemUOM IU ON L.intItemUOMId = IU.intItemUOMId
+	JOIN dbo.tblICStorageLocation SL ON L.intStorageLocationId = SL.intStorageLocationId
+	JOIN dbo.tblICUnitMeasure UM ON IU.intUnitMeasureId = UM.intUnitMeasureId
+	WHERE LT.intTransactionTypeId = 9
+		AND dtmDate >= @dtmStartDate
+		AND dtmDate <= @dtmEndDate
+		AND LT.dblQty > 0
+	
+	UNION
+	
+	SELECT L.strLotNumber
+		,LT.dtmDate
+		,LT.dtmCreated
+		,LTT.strName
+		,I.strItemNo
+		,I.strDescription
+		,LT.dblQty
+		,UM.strUnitMeasure
+		,SL.strName
+		,@strCompanyName AS strCompanyName
+		,@dtmStartDate1 AS dtmStartDate
+		,@dtmEndDate1 AS dtmEndDate
+		,'' AS strShiftName
+		,L.dtmExpiryDate
+		,LS.strSecondaryStatus
+	FROM dbo.tblICLot L
+	JOIN dbo.tblICInventoryLotTransaction LT ON L.intLotId = LT.intLotId
+	JOIN dbo.tblICInventoryTransactionType LTT ON LT.intTransactionTypeId = LTT.intTransactionTypeId
+	JOIN dbo.tblICLotStatus  LS on L.intLotStatusId =LS.intLotStatusId 
+	JOIN dbo.tblICItem I ON L.intItemId = I.intItemId
+	JOIN dbo.tblICItemUOM IU ON L.intItemUOMId = IU.intItemUOMId
+	JOIN dbo.tblICStorageLocation SL ON L.intStorageLocationId = SL.intStorageLocationId
+	JOIN dbo.tblICUnitMeasure UM ON IU.intUnitMeasureId = UM.intUnitMeasureId
+	WHERE LT.intTransactionTypeId = 9
+		AND dtmDate >= @dtmStartDate
+		AND dtmDate <= @dtmEndDate
+		AND LT.dblQty < 0
+	
+	UNION
+	
+	SELECT L.strLotNumber
+		,A.dtmAdjustmentDate
+		,A.dtmAdjustmentDate
+		,LTT.strName
+		,I.strItemNo
+		,I.strDescription
+		,- AD.dblQuantity
+		,UM.strUnitMeasure
+		,SL.strName
+		,@strCompanyName AS strCompanyName
+		,@dtmStartDate1 AS dtmStartDate
+		,@dtmEndDate1 AS dtmEndDate
+		,'' AS strShiftName
+		,L.dtmExpiryDate
+		,LS.strSecondaryStatus
+	FROM tblICInventoryAdjustment A
+	JOIN tblICInventoryAdjustmentDetail AD ON A.intInventoryAdjustmentId = AD.intInventoryAdjustmentId
+	JOIN tblICLot L ON AD.intLotId = L.intLotId
+	JOIN dbo.tblICInventoryTransactionType LTT ON A.intAdjustmentType = LTT.intTransactionTypeId
+	JOIN dbo.tblICLotStatus  LS on L.intLotStatusId =LS.intLotStatusId 
+	JOIN dbo.tblICItem I ON L.intItemId = I.intItemId
+	JOIN dbo.tblICItemUOM IU ON L.intItemUOMId = IU.intItemUOMId
+	JOIN dbo.tblICStorageLocation SL ON L.intStorageLocationId = SL.intStorageLocationId
+	JOIN dbo.tblICUnitMeasure UM ON IU.intUnitMeasureId = UM.intUnitMeasureId
+	WHERE A.intAdjustmentType = 15
+		AND A.dtmAdjustmentDate >= @dtmStartDate
+		AND A.dtmAdjustmentDate <= @dtmEndDate
+	
+	UNION
+	
+	SELECT L.strLotNumber
+		,A.dtmAdjustmentDate
+		,A.dtmAdjustmentDate
+		,LTT.strName
+		,I.strItemNo
+		,I.strDescription
+		,AD.dblQuantity
+		,UM.strUnitMeasure
+		,SL.strName
+		,@strCompanyName AS strCompanyName
+		,@dtmStartDate1 AS dtmStartDate
+		,@dtmEndDate1 AS dtmEndDate
+		,'' AS strShiftName
+		,L.dtmExpiryDate
+		,LS.strSecondaryStatus
+	FROM tblICInventoryAdjustment A
+	JOIN tblICInventoryAdjustmentDetail AD ON A.intInventoryAdjustmentId = AD.intInventoryAdjustmentId
+	JOIN tblICLot L ON AD.intLotId = L.intLotId
+	JOIN dbo.tblICInventoryTransactionType LTT ON A.intAdjustmentType = LTT.intTransactionTypeId
+	JOIN dbo.tblICLotStatus  LS on L.intLotStatusId =LS.intLotStatusId 
+	JOIN dbo.tblICItem I ON AD.intNewItemId = I.intItemId
+	JOIN dbo.tblICItemUOM IU ON L.intItemUOMId = IU.intItemUOMId
+	JOIN dbo.tblICStorageLocation SL ON L.intStorageLocationId = SL.intStorageLocationId
+	JOIN dbo.tblICUnitMeasure UM ON IU.intUnitMeasureId = UM.intUnitMeasureId
+	WHERE A.intAdjustmentType = 15
+		AND A.dtmAdjustmentDate >= @dtmStartDate
+		AND A.dtmAdjustmentDate <= @dtmEndDate
+
+	SELECT DP.*
+		,(
+			SELECT Count(DISTINCT DP1.strLotNumber)
+			FROM #tblMFDailyProduction DP1
+			) AS intNoOfPallets
+	FROM #tblMFDailyProduction DP
+	ORDER BY DP.dtmCreated
 
 	EXEC sp_xml_removedocument @xmlDocumentId
 END TRY
