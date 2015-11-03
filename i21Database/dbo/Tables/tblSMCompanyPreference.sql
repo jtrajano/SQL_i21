48533722c2d910dece86e1677a2e53b9a0d44f9c
von.pasion <von.pasion@irely.com>
2015-11-03 21:02:56 +0800

    FRM-2495

--- i21Database/dbo/Tables/tblSMCompanyPreference.sql
+++ i21Database/dbo/Tables/tblSMCompanyPreference.sql
@@ -31,6 +31,7 @@
     [strIntervalUpdatesMinutes] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
     [strQuotesDecimalsShown] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
 	[strHelperUrlDomain] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
+	[strNotificationEmailUrl] NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NULL, 
 	[intCompanySetupId] INT NOT NULL,
     [intConcurrencyId] INT NOT NULL DEFAULT 1
 )
