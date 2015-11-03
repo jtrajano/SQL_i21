1f954eae4b29faf488bc335745aea3e2e7239cf1
Ian Ocampo <ian.ocampo@irely.com>
2015-11-03 17:22:52 +0800

    FRM-2495

--- i21Database/dbo/Tables/tblSMNotification.sql
+++ i21Database/dbo/Tables/tblSMNotification.sql
@@ -3,7 +3,6 @@
     [strTitle]			NVARCHAR(255)	 COLLATE Latin1_General_CI_AS NULL, 
     [strMessage]        NVARCHAR (MAX)   COLLATE Latin1_General_CI_AS NULL,
     [strRoute]			NVARCHAR (MAX)   COLLATE Latin1_General_CI_AS NULL,
-	[strDomain]			NVARCHAR (MAX)   COLLATE Latin1_General_CI_AS NULL,
     [dtmLastUpdated]    DATETIME         NULL,
 	[ysnClosable]		BIT				 NULL,
 	[ysnSent]			BIT				 NULL,
