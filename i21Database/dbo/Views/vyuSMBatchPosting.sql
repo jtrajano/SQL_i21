5ade54ba9794cf5f5fbd65ca6c3a53433d9fa04b
Dan Michael Jalimao <dan.jalimao@irely.com>
2015-11-03 12:39:35 +0800

    AP-1765

--- i21Database/dbo/Views/vyuSMBatchPosting.sql
+++ i21Database/dbo/Views/vyuSMBatchPosting.sql
@@ -16,11 +16,11 @@ FROM
 	AND strTransactionType <> 'Recurring'
 	AND ysnPosted = 0
 	UNION ALL
-	SELECT 'Bill', intBillId, strBillId, intEntityId, dtmDate, strComment as strReference FROM tblAPBill WHERE intTransactionType = 1 AND ISNULL(ysnPosted, 0) = 0 AND intTransactionType != 6 AND ysnForApproval != 1 AND (ysnApproved = 0 AND dtmApprovalDate IS NOT NULL) --WHERE ysnPosted = 0 AND ysnForApproval = 0 AND intTransactionType = 1
+	SELECT 'Bill', intBillId, strBillId, intEntityId, dtmDate, strComment as strReference FROM tblAPBill WHERE intTransactionType = 1 AND intTransactionType != 6 AND ysnForApproval != 1 AND ISNULL(ysnPosted, 0) = 0 OR (dtmApprovalDate IS NOT NULL AND ysnApproved = 1)
 	UNION ALL
 	SELECT 'Payable', intPaymentId, strPaymentRecordNum, intEntityId, dtmDatePaid, strNotes as strReference FROM tblAPPayment WHERE ysnPosted = 0
 	UNION ALL
-	SELECT 'Debit Memo', intBillId, strBillId, intEntityId, dtmDate, strComment as strReference FROM tblAPBill WHERE intTransactionType = 3 AND ISNULL(ysnPosted, 0) = 0 AND intTransactionType != 6 AND ysnForApproval != 1 AND (ysnApproved = 0 AND dtmApprovalDate IS NOT NULL) --ysnPosted = 0 AND ysnForApproval = 0 AND intTransactionType = 3
+	SELECT 'Debit Memo', intBillId, strBillId, intEntityId, dtmDate, strComment as strReference FROM tblAPBill WHERE intTransactionType = 3 AND ISNULL(ysnPosted, 0) = 0 AND intTransactionType != 6 
 	UNION ALL
 	SELECT strTransactionType, intInvoiceId, strInvoiceNumber, intEntityId, dtmDate, strComments FROM tblARInvoice WHERE strTransactionType IN ('Invoice', 'Credit Memo')
 	AND ysnPosted = 0  AND ISNULL(intDistributionHeaderId, 0) = 0 AND ISNULL(ysnTemplate,0) = 0
