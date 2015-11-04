6c2e2b3a4fb64f84f76591c83c5e66c1814bd467
Dan Michael Jalimao <dan.jalimao@irely.com>
2015-11-04 18:16:11 +0800

    FCOM-1043

--- i21Database/dbo/Views/vyuSMBatchPosting.sql
+++ i21Database/dbo/Views/vyuSMBatchPosting.sql
@@ -16,11 +16,11 @@ FROM
 	FROM tblGLJournal Journal CROSS APPLY(SELECT SUM(dblDebit) dblDebit FROM tblGLJournalDetail Detail WHERE Detail.intJournalId = Journal.intJournalId) Total 
 	WHERE Journal.strJournalType IN ('Adjusted Origin Journal', 'General Journal', 'Audit Adjustment', 'Imported Journal', 'Origin Journal', 'Recurring Journal') AND Journal.strTransactionType <> 'Recurring' AND Journal.ysnPosted = 0
 	UNION ALL
-	SELECT 'Bill', intBillId, strBillId, dblTotal, intEntityId, dtmDate, strComment as strReference FROM tblAPBill WHERE intTransactionType = 1 AND ISNULL(ysnPosted, 0) = 0 AND intTransactionType != 6 AND ysnForApproval != 1 AND (ysnApproved = 0 AND dtmApprovalDate IS NOT NULL) --WHERE ysnPosted = 0 AND ysnForApproval = 0 AND intTransactionType = 1
+	SELECT 'Voucher', intBillId, strBillId, dblTotal, intEntityId, dtmDate, strComment as strReference FROM tblAPBill WHERE intTransactionType = 1 AND intTransactionType != 6 AND ysnForApproval != 1 AND ISNULL(ysnPosted, 0) = 0 OR (dtmApprovalDate IS NOT NULL AND ysnApproved = 1)
 	UNION ALL
 	SELECT 'Payable', intPaymentId, strPaymentRecordNum, dblAmountPaid, intEntityId, dtmDatePaid, strNotes as strReference FROM tblAPPayment WHERE ysnPosted = 0
 	UNION ALL
-	SELECT 'Debit Memo', intBillId, strBillId, dblTotal, intEntityId, dtmDate, strComment as strReference FROM tblAPBill WHERE intTransactionType = 3 AND ISNULL(ysnPosted, 0) = 0 AND intTransactionType != 6 AND ysnForApproval != 1 AND (ysnApproved = 0 AND dtmApprovalDate IS NOT NULL) --ysnPosted = 0 AND ysnForApproval = 0 AND intTransactionType = 3
+	SELECT 'Debit Memo', intBillId, strBillId, dblTotal, intEntityId, dtmDate, strComment as strReference FROM tblAPBill WHERE intTransactionType = 3 AND ISNULL(ysnPosted, 0) = 0 AND intTransactionType != 6 
 	UNION ALL
 	SELECT strTransactionType, intInvoiceId, strInvoiceNumber, dblInvoiceTotal, intEntityId, dtmDate, strComments FROM tblARInvoice WHERE strTransactionType IN ('Invoice', 'Credit Memo') AND ysnPosted = 0  AND ISNULL(intDistributionHeaderId, 0) = 0 AND ISNULL(ysnTemplate,0) = 0
 	UNION ALL
