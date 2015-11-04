f91c026d2f307fd76946b053514b2e7c1819f476
Emerson Versoza <emerson.versoza@irely.com>
2015-11-04 19:08:06 +0800

    GRN-20

---
+++ i21Database/dbo/Stored Procedures/uspSCUpdateScaleDeviceInterface.sql
@@ -0,0 +1,33 @@
+﻿CREATE PROCEDURE [dbo].[uspSCUpdateScaleDeviceInterface]
+       @intScaleDeviceId INT,
+	   @strDeviceData NVARCHAR(256),
+	   @ReturnValue INT OUTPUT
+AS
+SET QUOTED_IDENTIFIER OFF
+SET ANSI_NULLS ON
+SET NOCOUNT ON
+SET XACT_ABORT ON
+SET ANSI_WARNINGS OFF
+
+DECLARE @intDeviceInterfaceFileId AS INT
+
+BEGIN
+       SELECT @intDeviceInterfaceFileId = SD.intDeviceInterfaceFileId from tblSCDeviceInterfaceFile SD
+       WHERE intScaleDeviceId = @intScaleDeviceId
+       IF @intDeviceInterfaceFileId IS NULL
+       BEGIN
+              INSERT INTO tblSCDeviceInterfaceFile (intScaleDeviceId, strDeviceData, intConcurrencyId) 
+              VALUES (@intScaleDeviceId, @strDeviceData, 1)
+
+			  SET @ReturnValue = SCOPE_IDENTITY()
+       END
+       ELSE
+       BEGIN
+              UPDATE tblSCDeviceInterfaceFile SET strDeviceData = @strDeviceData 
+              WHERE intDeviceInterfaceFileId = @intDeviceInterfaceFileId
+
+			  SET @ReturnValue = @intDeviceInterfaceFileId
+       END
+END
+
+GO
