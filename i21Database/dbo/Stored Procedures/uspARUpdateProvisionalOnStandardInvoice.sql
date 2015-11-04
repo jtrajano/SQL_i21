fe3514558d4a018e3b7e2d61d12352b32b1ec5ec
Ruben  Escarcha <ruben.escarcha@irely.com>
2015-11-04 21:20:06 +0800

    AR-1187 Provisional Invoice Type

---
+++ i21Database/dbo/Stored Procedures/uspARUpdateProvisionalOnStandardInvoice.sql
@@ -0,0 +1,46 @@
+﻿CREATE PROCEDURE [dbo].[uspARUpdateProvisionalOnStandardInvoice]  
+	 @InvoiceId		INT   
+	,@ForDelete		BIT = 0
+	,@UserId		INT = NULL     
+AS  
+  
+SET QUOTED_IDENTIFIER OFF  
+SET ANSI_NULLS ON  
+SET NOCOUNT ON  
+SET XACT_ABORT ON  
+SET ANSI_WARNINGS OFF  
+
+
+DECLARE @InvoiceIds AS TABLE(intInvoiceId INT)
+
+
+INSERT INTO @InvoiceIds(intInvoiceId)
+SELECT DISTINCT
+	[intInvoiceId]
+FROM
+	tblARInvoice
+WHERE
+	[intInvoiceId] IN (SELECT [intInvoiceId] FROM tblARInvoiceDetail WHERE [intInvoiceDetailId] IN (SELECT intOriginalInvoiceDetailId FROM tblARInvoiceDetail WHERE [intInvoiceId] = @InvoiceId))
+	
+UNION ALL
+
+SELECT
+	[intInvoiceId] 
+FROM
+	tblARInvoice 
+WHERE
+	[intInvoiceId] IN (SELECT [intInvoiceId] FROM tblARInvoiceDetail WHERE [intInvoiceDetailId] IN (SELECT intOriginalInvoiceDetailId FROM tblARTransactionDetail WHERE [intTransactionId] = @InvoiceId))
+	AND @ForDelete = 1
+	
+	
+
+UPDATE
+	tblARInvoice
+SET
+	[ysnProcessed] = 0
+WHERE
+	[intInvoiceId] IN (SELECT [intInvoiceId] FROM @InvoiceIds)
+	AND NOT EXISTS(SELECT NULL FROM tblARInvoiceDetail WHERE intOriginalInvoiceDetailId IN (SELECT intInvoiceDetailId FROM tblARInvoiceDetail WHERE intInvoiceId <> tblARInvoice.intInvoiceId))
+	AND strType = 'Provisional Invoice'
+		
+GO
