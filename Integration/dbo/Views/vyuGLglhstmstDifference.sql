c784944a9e812e7737fb5c017eb3a4bc2f1ad06c
Liz Dorado <liz.dorado@irely.com>
2015-11-04 11:34:32 +0800

    GL-1932

---
+++ Integration/dbo/Views/vyuGLglhstmstDifference.sql
@@ -0,0 +1,102 @@
+GO
+
+IF EXISTS(select top 1 1 from INFORMATION_SCHEMA.VIEWS where TABLE_NAME = 'vyuGLglhstmstDifference')
+	DROP VIEW vyuGLglhstmstDifference
+GO
+
+IF  (SELECT TOP 1 ysnLegacyIntegration FROM tblSMCompanyPreference WHERE ysnLegacyIntegration = 1) = 1
+
+BEGIN
+
+EXEC (
+    '
+      CREATE VIEW [dbo].[vyuGLglhstmstDifference] AS
+
+     select A.strJournalId, A.strAccountId, A.dblDebit, A.dblCredit, A.dblDebitUnit, A.dblCreditUnit, A.strSourceId, A.strSourceType, A.strSourceKey, B.glhst_acct1_8, B.glhst_acct9_16, B.glhst_amt, B.glhst_units, B.glhst_dr_cr_ind, B.Debit, B.Credit, B.DebitUnit, B.CreditUnit, B.glhst_date, B.glhst_period, B.glhst_src_id, B.glhst_src_seq, B.A4GLIdentity from ( SELECT	   B.strJournalId,
+                  A.intLineNo,
+                  C.intAccountId,
+                  C.strAccountId,
+                  A.dblDebitUnit,
+                  A.dblCreditUnit,
+                  A.dblDebit,
+                  A.dblCredit,
+                  B.dtmDate,
+                  B.strSourceId,
+                  B.strSourceType,
+                  A.strSourceKey
+           FROM tblGLJournalDetail AS A
+                INNER JOIN tblGLJournal AS B ON A.intJournalId = B.intJournalId
+                INNER JOIN tblGLAccount AS C ON C.intAccountId = A.intAccountId
+           WHERE B.strJournalType IN( ''Origin Journal'', ''Adjusted Origin Journal'' )
+           GROUP BY B.strJournalId,
+                    A.intLineNo,
+                    C.intAccountId,
+                    C.strAccountId,
+                    A.dblDebitUnit,
+                    A.dblCreditUnit,
+                    A.dblDebit,
+                    A.dblCredit,
+                    B.dtmDate,
+                    B.strSourceId,
+                    B.strSourceType,
+                    A.strSourceKey ) AS A
+         INNER JOIN( 
+                                       SELECT SUM(CASE
+                                    WHEN glhst_dr_cr_ind = ''D''
+                                    THEN glhst_units
+                                    ELSE 0
+                                END) AS DebitUnit,
+                            SUM(CASE
+                                    WHEN glhst_dr_cr_ind = ''C''
+                                    THEN glhst_units
+                                    ELSE 0
+                                END) AS CreditUnit,
+                            SUM(CASE
+                                    WHEN glhst_dr_cr_ind = ''D''  and glhst_amt > 0 OR glhst_dr_cr_ind = ''C'' and glhst_amt <  0 
+							 THEN ABS (glhst_amt)
+                                    ELSE 0
+                                END) AS Debit,
+                            SUM(CASE
+                                    WHEN glhst_dr_cr_ind = ''C'' and glhst_amt > 0 OR glhst_dr_cr_ind = ''D'' and glhst_amt <  0 or  glhst_dr_cr_ind is NULL
+                                    THEN ABS (glhst_amt)
+                                    ELSE 0
+                                END) AS Credit,
+                            CONVERT( DATETIME, SUBSTRING(CONVERT(VARCHAR(10), A.glhst_date), 1, 4) + ''/'' + SUBSTRING(CONVERT(VARCHAR(10), A.glhst_date), 5, 2) + ''/'' + SUBSTRING(CONVERT(VARCHAR(10), A.glhst_date), 7, 2)) AS glhst_date,
+                            A.glhst_amt,
+					   A.glhst_units,
+					   A.glhst_dr_cr_ind,
+					   A.glhst_period,
+                            A.glhst_src_id,
+                            A.glhst_src_seq,
+                            A.A4GLIdentity,
+                            C.stri21Id,
+                            C.inti21Id,
+                            A.glhst_line_no,
+                            glhst_acct1_8,
+					   glhst_acct9_16
+                     FROM glhstmst AS A
+					 INNER JOIN glactmst AS B ON A.glhst_acct1_8 = B.glact_acct1_8
+				      INNER JOIN tblGLCOACrossReference AS C ON C.intLegacyReferenceId = B.A4GLIdentity
+                          INNER JOIN tblGLAccount AS D ON D.intAccountId = C.inti21Id
+                     GROUP BY A.glhst_dr_cr_ind,
+					   A.glhst_amt,
+					   A.glhst_units,
+					   A.glhst_period,
+                            A.glhst_src_id,
+                            A.glhst_src_seq,
+                            A.A4GLIdentity,
+                            C.stri21Id,
+                            C.inti21Id,
+                            A.glhst_line_no,
+                            A.glhst_acct1_8,
+					   A.glhst_acct9_16,
+					   A.glhst_date) AS B 
+						
+						ON  A.strSourceKey = B.A4GLIdentity
+     WHERE A.strSourceType = B.glhst_src_id COLLATE Latin1_General_CI_AS
+     --AND ( A.dblDebit <> B.Debit OR A.dblCredit <> B.Credit)    	 
+	'
+				
+      )
+	 
+END
