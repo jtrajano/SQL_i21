478b58ff798d4fd920f24c308c5ce7c7c407a04e
Mon Gonzales <mon.gonzales@irely.com>
2015-11-04 19:25:43 +0800

    TEMP FIX PLEASE REVIEW THEN REVERT THANKS - MON GONZALES

--- i21Database/dbo/Tables/tblPATDividends.sql
+++ i21Database/dbo/Tables/tblPATDividends.sql
@@ -15,42 +15,42 @@
     CONSTRAINT [PK_tblPATDividends] PRIMARY KEY ([intDividendId]) 
 )
 
-GO
+--GO
 
-CREATE TRIGGER [dbo].[trgDividendNo]
-  ON [dbo].[tblPATDividends]
-AFTER INSERT
-AS
+--CREATE TRIGGER [dbo].[trgDividendNo]
+--  ON [dbo].[tblPATDividends]
+--AFTER INSERT
+--AS
 
-DECLARE @inserted TABLE(intTransferId INT)
-DECLARE @intDividendId INT
-DECLARE @dblDividendNo NUMERIC(18,6)
-DECLARE @intMaxCount INT = 0
+--DECLARE @inserted TABLE(intTransferId INT)
+--DECLARE @intDividendId INT
+--DECLARE @dblDividendNo NUMERIC(18,6)
+--DECLARE @intMaxCount INT = 0
 
-INSERT INTO @inserted
-SELECT intDividendId FROM INSERTED ORDER BY intDividendId
-WHILE((SELECT TOP 1 1 FROM @inserted) IS NOT NULL)
-BEGIN
-	SELECT TOP 1 @intDividendId = intDividendId FROM @inserted
+--INSERT INTO @inserted
+--SELECT intDividendId FROM INSERTED ORDER BY intDividendId
+--WHILE((SELECT TOP 1 1 FROM @inserted) IS NOT NULL)
+--BEGIN
+--	SELECT TOP 1 @intDividendId = intDividendId FROM @inserted
 
-	EXEC uspSMGetStartingNumber 82, @dblDividendNo OUT
+--	EXEC uspSMGetStartingNumber 82, @dblDividendNo OUT
 
-	IF(@dblDividendNo IS NOT NULL)
-	BEGIN
-		IF EXISTS (SELECT NULL FROM tblPATDividends WHERE dblDividendNo = @dblDividendNo)
-			BEGIN
-				SET @dblDividendNo = NULL
-				SELECT @intMaxCount = MAX(CONVERT(INT, SUBSTRING(dblDividendNo, 5, 10))) FROM tblPATDividends
-				UPDATE tblSMStartingNumber SET intNumber = @intMaxCount + 1 WHERE intStartingNumberId = 80
-				EXEC uspSMGetStartingNumber 80, @dblDividendNo OUT
-			END
+--	IF(@dblDividendNo IS NOT NULL)
+--	BEGIN
+--		IF EXISTS (SELECT NULL FROM tblPATDividends WHERE dblDividendNo = @dblDividendNo)
+--			BEGIN
+--				SET @dblDividendNo = NULL
+--				SELECT @intMaxCount = MAX(CONVERT(INT, SUBSTRING(dblDividendNo, 5, 10))) FROM tblPATDividends
+--				UPDATE tblSMStartingNumber SET intNumber = @intMaxCount + 1 WHERE intStartingNumberId = 80
+--				EXEC uspSMGetStartingNumber 80, @dblDividendNo OUT
+--			END
 		
-		UPDATE tblPATDividends
-			SET tblPATDividends.dblDividendNo = @dblDividendNo
-		FROM tblPATDividends A
-		WHERE A.intDividendId = @intDividendId
-	END
+--		UPDATE tblPATDividends
+--			SET tblPATDividends.dblDividendNo = @dblDividendNo
+--		FROM tblPATDividends A
+--		WHERE A.intDividendId = @intDividendId
+--	END
 
-	DELETE FROM @inserted
-	WHERE intTransferId = @intDividendId
-END
+--	DELETE FROM @inserted
+--	WHERE intTransferId = @intDividendId
+--END
