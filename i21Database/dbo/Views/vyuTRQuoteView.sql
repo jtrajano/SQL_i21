6f99e48193c1cd6bfe9befd539b352bd517c4c8d
Pruthvi <pruthvi.c@irely.com>
2015-11-05 15:52:05 +0530

    TR-288

--- i21Database/dbo/Views/vyuTRQuoteView.sql
+++ i21Database/dbo/Views/vyuTRQuoteView.sql
@@ -8,7 +8,8 @@ SELECT
    QH.strCustomerComments,
    QH.dtmQuoteDate,
    AR.strName,
-   AR.intEntityCustomerId
+   AR.intEntityCustomerId,
+   0 "dblQuoteTotal" 
     	
 FROM
     tblTRQuoteHeader QH
