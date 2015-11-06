0b3a2925bcf447d3cb8d17f7956a64f8d805c865
Dan Michael Jalimao <dan.jalimao@irely.com>
2015-11-06 17:11:06 +0800

    FCOM-1079

--- i21Database/Scripts/Post-Deployment/SM/DefaultData/4_StartingNumbers.sql
+++ i21Database/Scripts/Post-Deployment/SM/DefaultData/4_StartingNumbers.sql
@@ -782,6 +782,16 @@ GO
 			,[intConcurrencyId]		= 1
 	WHERE NOT EXISTS (SELECT TOP 1 1 FROM tblSMStartingNumber WHERE strTransactionType = N'Certificate Number')
 
+	UNION ALL
+	SELECT	[intStartingNumberId]	= 84
+			,[strTransactionType]	= N'Service Charge'
+			,[strPrefix]			= N'SC-'
+			,[intNumber]			= 1
+			,[strModule]			= 'Accounts Receivable'
+			,[ysnEnable]			= 1
+			,[intConcurrencyId]		= 1
+	WHERE NOT EXISTS (SELECT TOP 1 1 FROM tblSMStartingNumber WHERE strTransactionType = N'Service Charge')
+
 	SET IDENTITY_INSERT [dbo].[tblSMStartingNumber] OFF
 GO
 	PRINT N'END INSERT DEFAULT STARTING NUMBERS'
