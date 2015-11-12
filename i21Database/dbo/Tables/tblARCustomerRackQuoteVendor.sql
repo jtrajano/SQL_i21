a66abbafe188883634639682542d58643f574cb5
Mon Gonzales <mon.gonzales@irely.com>
2015-11-12 19:07:23 +0800

    EM-668

--- i21Database/dbo/Tables/tblARCustomerRackQuoteVendor.sql
+++ i21Database/dbo/Tables/tblARCustomerRackQuoteVendor.sql
@@ -3,10 +3,13 @@
 	[intCustomerRackQuoteVendorId]			INT			IDENTITY (1, 1) NOT NULL,
 	[intCustomerRackQuoteHeaderId]			INT			NOT NULL,
 	[intSupplyPointId]						INT         NULL,
-	[ysnQuote]								BIT         NULL,	
+	[ysnQuote]								BIT         NULL,
+	[intEntityCustomerLocationId]			INT			NULL,	
 	[intConcurrencyId]						INT         CONSTRAINT [DF_tblARCustomerRackQuoteVendor_intConcurrencyId] DEFAULT ((0)) NOT NULL,
 	CONSTRAINT [PK_tblARCustomerRackQuoteVendor] PRIMARY KEY CLUSTERED ([intCustomerRackQuoteVendorId] ASC),
 	CONSTRAINT [FK_tblARCustomerRackQuoteHeader_tblARCustomerRackQuoteHeader] FOREIGN KEY ([intCustomerRackQuoteHeaderId]) REFERENCES [dbo].[tblARCustomerRackQuoteHeader] ([intCustomerRackQuoteHeaderId]) ON DELETE CASCADE,
 	CONSTRAINT [FK_tblARCustomerRackQuoteHeader_tblTRSupplyPoint] FOREIGN KEY ([intSupplyPointId]) REFERENCES [dbo].[tblTRSupplyPoint] ([intSupplyPointId]),
+	CONSTRAINT [FK_tblARCustomerRackQuoteHeader_tblEntityLocation] FOREIGN KEY ([intEntityCustomerLocationId]) REFERENCES [dbo].[tblEntityLocation]([intEntityLocationId]),
+	CONSTRAINT [UK_tblARCustomerRackQuoteHeader_intSupplyPointId_intEntityCustomerLocationId] UNIQUE NONCLUSTERED ([intEntityCustomerLocationId] ASC, [intSupplyPointId] ASC)	
 
 )
