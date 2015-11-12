70179766e79ae81de3e3c0c0e0c65bdbf93e48b1
Melanie Ferrer <Mel.Ferrer@irely.com>
2015-11-12 16:06:19 +0800

    PAT-61

---
+++ i21Database/dbo/Tables/tblPATChangeStatusDetail.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[tblPATChangeStatusDetail]
+(
+	[intUpdateDetailId] INT NOT NULL IDENTITY, 
+    [intUpdateId] INT NOT NULL, 
+    [intCustomerId] INT NULL, 
+    [strCurrentStatus] NVARCHAR(50) NULL, 
+    [intConcurrencyId] INT NULL DEFAULT 0, 
+    CONSTRAINT [PK_tblPATChangeStatusDetail] PRIMARY KEY ([intUpdateDetailId]), 
+    CONSTRAINT [FK_tblPATChangeStatusDetail_tblPATChangeStatus] FOREIGN KEY ([intUpdateId]) REFERENCES [tblPATChangeStatus]([intUpdateId]) 
+)
