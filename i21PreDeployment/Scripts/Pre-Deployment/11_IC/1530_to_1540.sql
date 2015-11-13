a3e5ca69980a283fd76aa39fa605e0a7679c5085
Lawrence Zabala <lawrence.zabala@irely.com>
2015-11-13 15:13:20 +0800

    IC-1557

--- i21PreDeployment/Scripts/Pre-Deployment/11_IC/1530_to_1540.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/11_IC/1530_to_1540.sql
@@ -13,3 +13,19 @@ BEGIN
 	')
 
 END
+
+-- Pre deployment fix for non-unique UOMs prior to 15.4 builds
+IF EXISTS(SELECT TOP 1 1 FROM sys.tables WHERE name = 'tblICUnitMeasure')
+BEGIN
+
+	EXEC ('
+		UPDATE tblICUnitMeasure
+		SET strUnitMeasure = strUnitMeasure + CAST(intUnitMeasureId AS NVARCHAR)
+		WHERE strUnitMeasure IN (
+			SELECT strUnitMeasure FROM tblICUnitMeasure
+			GROUP BY strUnitMeasure
+			HAVING COUNT(*) > 1
+		)
+	')
+
+END
