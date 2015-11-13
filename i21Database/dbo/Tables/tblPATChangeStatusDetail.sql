6493b9112e5e489aa9f04976ef6385213be6704f
Melanie Ferrer <Mel.Ferrer@irely.com>
2015-11-13 14:43:46 +0800

    PAT-61

--- i21Database/dbo/Tables/tblPATChangeStatusDetail.sql
+++ i21Database/dbo/Tables/tblPATChangeStatusDetail.sql
@@ -3,7 +3,7 @@
 	[intUpdateDetailId] INT NOT NULL IDENTITY, 
     [intUpdateId] INT NOT NULL, 
     [intCustomerId] INT NULL, 
-    [strCurrentStatus] NVARCHAR(50) NULL, 
+    [strCurrentStatus] NVARCHAR(50)  COLLATE Latin1_General_CI_AS  NULL, 
     [intConcurrencyId] INT NULL DEFAULT 0, 
     CONSTRAINT [PK_tblPATChangeStatusDetail] PRIMARY KEY ([intUpdateDetailId]), 
     CONSTRAINT [FK_tblPATChangeStatusDetail_tblPATChangeStatus] FOREIGN KEY ([intUpdateId]) REFERENCES [tblPATChangeStatus]([intUpdateId]) 
