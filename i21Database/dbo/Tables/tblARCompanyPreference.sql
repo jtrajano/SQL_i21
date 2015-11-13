6758524a72eac7faa3cca320e1b7132df0108634
Ruben  Escarcha <ruben.escarcha@irely.com>
2015-11-13 20:54:47 +0800

    AR-1921 Add interest column to receive payments detail

--- i21Database/dbo/Tables/tblARCompanyPreference.sql
+++ i21Database/dbo/Tables/tblARCompanyPreference.sql
@@ -4,6 +4,7 @@
     [intARAccountId]				INT NULL, 
     [intDiscountAccountId]			INT NULL,
 	[intWriteOffAccountId]			INT NULL,
+	[intInterestIncomeAccountId]	INT NULL,
 	[intServiceChargeAccountId]		INT NULL,
 	[strServiceChargeCalculation]	NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL,
 	[strServiceChargeFrequency]		NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL,
