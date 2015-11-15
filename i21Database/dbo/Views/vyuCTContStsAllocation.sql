edc4fec0225d5c902ba24ce0839ff4b665b2cc35
Satya sankalpa sahoo <satya.sahoo@irely.com>
2015-11-15 14:47:14 +0530

    CT-340

---
+++ i21Database/dbo/Views/vyuCTContStsAllocation.sql
@@ -0,0 +1,27 @@
+﻿CREATE VIEW [dbo].[vyuCTContStsAllocation]
+
+AS 
+
+		SELECT	AD.intAllocationDetailId,
+				CH.strContractNumber + '-' + LTRIM(CD.intContractSeq) strSequenceNumber,
+				EY.strName strEntityName,
+				dblSAllocatedQty dblAllocatedQty,
+				AD.intPContractDetailId intContractDetailId,
+				'Sale' strContractType
+		FROM	tblLGAllocationDetail	AD
+		JOIN	tblCTContractDetail		CD	ON	CD.intContractDetailId		=	AD.intSContractDetailId
+		JOIN	tblCTContractHeader		CH	ON	CH.intContractHeaderId		=	CD.intContractHeaderId
+		JOIN	tblEntity				EY	ON	EY.intEntityId				=	CH.intEntityId
+
+		UNION ALL 
+
+		SELECT	AD.intAllocationDetailId,
+				CH.strContractNumber + '-' + LTRIM(CD.intContractSeq) strSequenceNumber,
+				EY.strName strEntityName,
+				dblPAllocatedQty,
+				AD.intSContractDetailId,
+				'Purchase' strContractType
+		FROM	tblLGAllocationDetail	AD
+		JOIN	tblCTContractDetail		CD	ON	CD.intContractDetailId		=	AD.intPContractDetailId
+		JOIN	tblCTContractHeader		CH	ON	CH.intContractHeaderId		=	CD.intContractHeaderId
+		JOIN	tblEntity				EY	ON	EY.intEntityId				=	CH.intEntityId
