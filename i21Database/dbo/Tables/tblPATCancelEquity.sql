370b511f06f2665456d8e6273c90cb42b1d71468
Melanie Ferrer <Mel.Ferrer@irely.com>
2015-11-16 12:49:38 +0800

    PAT-62

---
+++ i21Database/dbo/Tables/tblPATCancelEquity.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[tblPATCancelEquity]
+(
+	[intCancelId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [dtmCancelDate] DATETIME NULL, 
+    [strCancelNo] NVARCHAR(50)  COLLATE Latin1_General_CI_AS NULL, 
+    [strDescription] NVARCHAR(50)  COLLATE Latin1_General_CI_AS NULL, 
+    [intFromCustomerId] INT NULL, 
+    [intToCustomerId] INT NULL, 
+    [intFiscalYearId] INT NULL, 
+    [strCancelBy] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
+    [dblCancelByAmount] NUMERIC(18, 6) NULL, 
+    [dblCancelLessAmount] NUMERIC(18, 6) NULL, 
+    [intIncludeEquityReserve] INT NULL, 
+    [intConcurrencyId] INT NULL DEFAULT 0
+)
---
+++ i21Database/dbo/Tables/tblPATCancelEquityDetail.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[tblPATCancelEquityDetail]
+(
+	[intCancelDetailId] INT NOT NULL IDENTITY, 
+    [intCancelId] INT NULL, 
+    [intFiscalYearId] INT NULL, 
+    [intCustomerId] INT NULL, 
+    [intRefundTypeId] INT NULL, 
+    [dblQuantityAvailable] NUMERIC(18, 6) NULL, 
+    [strCancelBy] NVARCHAR(50)  COLLATE Latin1_General_CI_AS NULL, 
+    [dblCancelByPercentage] NUMERIC(18, 6) NULL, 
+    [dblQuantityCancelled] NUMERIC(18, 6) NULL, 
+    [intConcurrencyId] INT NULL DEFAULT 0, 
+    CONSTRAINT [PK_tblPATCancelEquityDetail] PRIMARY KEY ([intCancelDetailId]), 
+    CONSTRAINT [FK_tblPATCancelEquityDetail_tblPATCancelEquity] FOREIGN KEY ([intCancelId]) REFERENCES [tblPATCancelEquity]([intCancelId]) 
+)
