acfd6209ca23f064bb1a1af159866be5d9b6a52d
Swetosuvro <swetosuvro.g@irely.com>
2015-11-16 17:12:43 +0530

    WH-44

---
+++ i21Database/dbo/Tables/tblWHCompanyPreference.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE [dbo].[tblWHCompanyPreference]
+(
+	intCompanyPreferenceId INT NOT NULL IDENTITY,
+	intAllowablePickDayRange INT,
+	ysnAllowMoveAssignedTask BIT,
+	strHandheldType NVARCHAR(50),
+	strWarehouseType NVARCHAR(50),
+	intContainerMinimumLength INT,
+	intLocationMinLength INT,
+	ysnNegativeQtyAllowed BIT,
+	ysnPartialMoveAllowed BIT,
+	ysnGTINCaseCodeMandatory BIT,
+	ysnEnableMoveAndMergeSplit BIT,
+	ysnTicketLabelToPrinter BIT,
+	intNoOfCopiesToPrintforPalletSlip INT,
+	strWebServiceServerURL NVARCHAR(256),
+	strWMSStatus NVARCHAR(100),
+	dblPalletWeight NUMERIC(18,6),
+	intNumberOfDecimalPlaces INT,
+	ysnCreateLoadTasks BIT,
+	intMaximumPalletsOnForklift INT
+)
