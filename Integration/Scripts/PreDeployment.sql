ba154237e4e0a004545440578bec460837092ab4
kenneth lagunero <kenneth.lagunero@irely.com>
2015-11-17 03:20:43 +0800

    TM-1505

--- Integration/Scripts/PreDeployment.sql
+++ Integration/Scripts/PreDeployment.sql
@@ -153,6 +153,8 @@
 :r "..\dbo\Stored Procedures\uspTMRecreateOpenWorkOrderView.sql"
 :r "..\dbo\Stored Procedures\uspTMRecreateConsumptionSiteSearchView.sql"
 :r "..\dbo\Stored Procedures\uspTMRecreateSiteCustomerView.sql"
+:r "..\dbo\Views\vyuTMDeliveryFillReport.sql"
+:r "..\dbo\Stored Procedures\uspTMDeliveryFillReport.sql"
 
 
 
