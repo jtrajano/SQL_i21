acb4a39bba685738435f9e76b7c1bf311f1d574a
Melanie Ferrer <Mel.Ferrer@irely.com>
2015-11-17 11:55:04 +0800

    PAT-62

--- i21Database/dbo/Tables/tblPATCancelEquity.sql
+++ i21Database/dbo/Tables/tblPATCancelEquity.sql
@@ -14,3 +14,43 @@
 	[ysnPosted] BIT NULL DEFAULT 0,
     [intConcurrencyId] INT NULL DEFAULT 0
 )
+
+GO
+
+CREATE TRIGGER [dbo].[trgCancelNo]
+ON [dbo].[tblPATCancelEquity]
+AFTER INSERT
+AS
+
+DECLARE @inserted TABLE(intCancelId INT)
+DECLARE @intCancelId INT
+DECLARE @strCancelNo NVARCHAR(10)
+DECLARE @intMaxCount INT = 0
+
+INSERT INTO @inserted
+SELECT intCancelId FROM INSERTED ORDER BY intCancelId
+WHILE((SELECT TOP 1 1 FROM @inserted) IS NOT NULL)
+BEGIN
+	SELECT TOP 1 @intCancelId = intCancelId FROM @inserted
+
+	EXEC uspSMGetStartingNumber 89 , @strCancelNo OUT
+
+	IF(@strCancelNo IS NOT NULL)
+	BEGIN
+		IF EXISTS (SELECT NULL FROM tblPATCancelEquity WHERE strCancelNo = @strCancelNo)
+			BEGIN
+				SET @strCancelNo = NULL
+				SELECT @intMaxCount = MAX(CONVERT(INT, SUBSTRING(strCancelNo, 5, 10))) FROM tblPATCancelEquity
+				UPDATE tblSMStartingNumber SET intNumber = @intMaxCount + 1 WHERE intStartingNumberId = 89
+				EXEC uspSMGetStartingNumber 89, @strCancelNo OUT
+			END
+		
+		UPDATE tblPATCancelEquity
+			SET tblPATCancelEquity.strCancelNo = @strCancelNo
+		FROM tblPATCancelEquity A
+		WHERE A.intCancelId = @intCancelId
+	END
+
+	DELETE FROM @inserted
+	WHERE intCancelId = @intCancelId
+END
