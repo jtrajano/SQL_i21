1e2265c7e7af66f5718b19ae9088fd0b348675e5
Swetosuvro <swetosuvro.g@irely.com>
2015-11-17 14:11:19 +0530

    WH-44

---
+++ i21Database/dbo/Stored Procedures/uspWHGetCompanyPreferenceValueList.sql
@@ -0,0 +1,120 @@
+﻿CREATE PROCEDURE uspWHGetCompanyPreferenceValueList 
+					@strCompanyLocationName NVARCHAR(50) = NULL
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	DECLARE @intCompanyLocationId INT
+
+	SET @strCompanyLocationName = LTRIM(RTRIM(@strCompanyLocationName))
+
+	IF (@strCompanyLocationName IS NOT NULL)
+	BEGIN
+		SELECT @intCompanyLocationId = intCompanyLocationId
+		FROM tblSMCompanyLocation
+		WHERE strLocationName = @strCompanyLocationName
+
+		SELECT SettingName,SettingValue
+		FROM (
+			SELECT intCompanyPreferenceId, intCompanyLocationId, SettingName, CAST(SettingValue AS NVARCHAR(255)) SettingValue
+			FROM (
+				SELECT CAST(intCompanyPreferenceId AS NVARCHAR(255)) intCompanyPreferenceId, 
+					   CAST(intCompanyLocationId AS NVARCHAR(255)) intCompanyLocationId, 
+					   CAST(intAllowablePickDayRange AS NVARCHAR(255)) intAllowablePickDayRange, 
+					   CAST(ysnAllowMoveAssignedTask AS NVARCHAR(255)) ysnAllowMoveAssignedTask, 
+					   CAST(ysnScanForkliftOnLogin AS NVARCHAR(255)) ysnScanForkliftOnLogin, 
+					   CAST(strHandheldType AS NVARCHAR(255)) strHandheldType, 
+					   CAST(strWarehouseType AS NVARCHAR(255)) strWarehouseType, 
+					   CAST(intContainerMinimumLength AS NVARCHAR(255)) intContainerMinimumLength, 
+					   CAST(intLocationMinLength AS NVARCHAR(255)) intLocationMinLength, 
+					   CAST(ysnNegativeQtyAllowed AS NVARCHAR(255)) ysnNegativeQtyAllowed, 
+					   CAST(ysnPartialMoveAllowed AS NVARCHAR(255)) ysnPartialMoveAllowed, 
+					   CAST(ysnGTINCaseCodeMandatory AS NVARCHAR(255)) ysnGTINCaseCodeMandatory, 
+					   CAST(ysnEnableMoveAndMergeSplit AS NVARCHAR(255)) ysnEnableMoveAndMergeSplit, 
+					   CAST(ysnTicketLabelToPrinter AS NVARCHAR(255)) ysnTicketLabelToPrinter, 
+					   CAST(intNoOfCopiesToPrintforPalletSlip AS NVARCHAR(255)) intNoOfCopiesToPrintforPalletSlip, 
+					   CAST(strWebServiceServerURL AS NVARCHAR(255)) strWebServiceServerURL, 
+					   CAST(strWMSStatus AS NVARCHAR(255)) strWMSStatus, 
+					   CAST(dblPalletWeight AS NVARCHAR(255)) dblPalletWeight, 
+					   CAST(intNumberOfDecimalPlaces AS NVARCHAR(255)) intNumberOfDecimalPlaces, 
+					   CAST(ysnCreateLoadTasks AS NVARCHAR(255)) ysnCreateLoadTasks, 
+					   CAST(intMaximumPalletsOnForklift AS NVARCHAR(255)) intMaximumPalletsOnForklift
+				FROM tblWHCompanyPreference
+				) p
+			UNPIVOT(SettingValue FOR SettingName IN (
+						intAllowablePickDayRange
+						,ysnAllowMoveAssignedTask
+						,ysnScanForkliftOnLogin
+						,strHandheldType
+						,strWarehouseType
+						,intContainerMinimumLength
+						,intLocationMinLength
+						,ysnNegativeQtyAllowed
+						,ysnPartialMoveAllowed
+						,ysnGTINCaseCodeMandatory
+						,ysnEnableMoveAndMergeSplit
+						,ysnTicketLabelToPrinter
+						,intNoOfCopiesToPrintforPalletSlip
+						,strWebServiceServerURL
+						,strWMSStatus
+						,dblPalletWeight
+						,intNumberOfDecimalPlaces
+						,ysnCreateLoadTasks
+						,intMaximumPalletsOnForklift
+						)) AS unpvt
+			) tblCompanyPreference
+		WHERE intCompanyLocationId = @intCompanyLocationId
+	END
+	ELSE
+	BEGIN
+		SELECT SettingName,SettingValue
+		FROM (
+			SELECT intCompanyPreferenceId, intCompanyLocationId, SettingName, CAST(SettingValue AS NVARCHAR(255)) SettingValue
+			FROM (
+					SELECT CAST(intCompanyPreferenceId AS NVARCHAR(255)) intCompanyPreferenceId, 
+					   CAST(intCompanyLocationId AS NVARCHAR(255)) intCompanyLocationId, 
+					   CAST(intAllowablePickDayRange AS NVARCHAR(255)) intAllowablePickDayRange, 
+					   CAST(ysnAllowMoveAssignedTask AS NVARCHAR(255)) ysnAllowMoveAssignedTask, 
+					   CAST(ysnScanForkliftOnLogin AS NVARCHAR(255)) ysnScanForkliftOnLogin, 
+					   CAST(strHandheldType AS NVARCHAR(255)) strHandheldType, 
+					   CAST(strWarehouseType AS NVARCHAR(255)) strWarehouseType, 
+					   CAST(intContainerMinimumLength AS NVARCHAR(255)) intContainerMinimumLength, 
+					   CAST(intLocationMinLength AS NVARCHAR(255)) intLocationMinLength, 
+					   CAST(ysnNegativeQtyAllowed AS NVARCHAR(255)) ysnNegativeQtyAllowed, 
+					   CAST(ysnPartialMoveAllowed AS NVARCHAR(255)) ysnPartialMoveAllowed, 
+					   CAST(ysnGTINCaseCodeMandatory AS NVARCHAR(255)) ysnGTINCaseCodeMandatory, 
+					   CAST(ysnEnableMoveAndMergeSplit AS NVARCHAR(255)) ysnEnableMoveAndMergeSplit, 
+					   CAST(ysnTicketLabelToPrinter AS NVARCHAR(255)) ysnTicketLabelToPrinter, 
+					   CAST(intNoOfCopiesToPrintforPalletSlip AS NVARCHAR(255)) intNoOfCopiesToPrintforPalletSlip, 
+					   CAST(strWebServiceServerURL AS NVARCHAR(255)) strWebServiceServerURL, 
+					   CAST(strWMSStatus AS NVARCHAR(255)) strWMSStatus, 
+					   CAST(dblPalletWeight AS NVARCHAR(255)) dblPalletWeight, 
+					   CAST(intNumberOfDecimalPlaces AS NVARCHAR(255)) intNumberOfDecimalPlaces, 
+					   CAST(ysnCreateLoadTasks AS NVARCHAR(255)) ysnCreateLoadTasks, 
+					   CAST(intMaximumPalletsOnForklift AS NVARCHAR(255)) intMaximumPalletsOnForklift
+				FROM tblWHCompanyPreference
+			) p
+		UNPIVOT(SettingValue FOR SettingName IN (
+					intAllowablePickDayRange
+					,ysnAllowMoveAssignedTask
+					,ysnScanForkliftOnLogin
+					,strHandheldType
+					,strWarehouseType
+					,intContainerMinimumLength
+					,intLocationMinLength
+					,ysnNegativeQtyAllowed
+					,ysnPartialMoveAllowed
+					,ysnGTINCaseCodeMandatory
+					,ysnEnableMoveAndMergeSplit
+					,ysnTicketLabelToPrinter
+					,intNoOfCopiesToPrintforPalletSlip
+					,strWebServiceServerURL
+					,strWMSStatus
+					,dblPalletWeight
+					,intNumberOfDecimalPlaces
+					,ysnCreateLoadTasks
+					,intMaximumPalletsOnForklift
+					)) AS unpvt
+				) tblCompanyPreference
+	END
+END
---
+++ i21Database/dbo/Stored Procedures/uspWMGetUserLocationListWMS.sql
@@ -0,0 +1,64 @@
+﻿CREATE PROCEDURE uspWMGetUserLocationListWMS
+ @UserName nvarchar(50)  = null
+AS   
+BEGIN  
+
+ IF @UserName = '' OR @UserName IS NULL   
+ BEGIN 
+	 SELECT cl.strLocationName FactoryLocation,  -- + ' - ' + ISNULL(clsl.strSubLocationName,'') FactoryLocation,  
+	  smus.strUserName UserName, 
+	  smus.intEntityUserSecurityId UserKey, 
+	  'Admin' UserGroupName,  
+	  cl.intCompanyLocationId FactoryKey,  
+	  cl.strLocationName FactoryName,  
+	  --clsl.intCompanyLocationSubLocationId LocationKey,  
+	  --ISNULL(clsl.strSubLocationName,'') LocationName,  
+	  0 NewLotUnitKey,  
+	  '' NewLotUnitName,  
+	  0 AuditUnitKey,  
+	  '' AuditUnitName,  
+	  '' DefaultShipFromAddressTitle,  
+	  0 DefaultShipFromAddressID,  
+	  '' DefaultShipToAddressTitle,  
+	  0 DefaultShipToAddressID,  
+	  CONVERT(bit,0) IsDefault,  
+	  0 WIPLocationKey,  
+	  '' [language],  
+	  '' ERPCompanyID,  
+	  '' ERPWareHouseID,  
+	  0 WarehouseID, '' AS WarehouseName, '' AS AddressType  
+	 FROM tblSMUserSecurity smus,tblSMCompanyLocation cl --,tblSMCompanyLocationSubLocation clsl
+	 --LEFT JOIN tblSMCompanyLocation cl ON smus.intCompanyLocationId = cl.intCompanyLocationId
+	 --LEFT JOIN tblSMCompanyLocationSubLocation clsl ON clsl.intCompanyLocationId = cl.intCompanyLocationId
+
+ END
+ ELSE    
+ BEGIN
+ 	  SELECT cl.strLocationName FactoryLocation, -- + ' - ' + ISNULL(clsl.strSubLocationName,'') FactoryLocation,  
+	  smus.strUserName UserName,  
+  	  smus.intEntityUserSecurityId UserKey, 
+	  'Admin' UserGroupName,  
+	  cl.intCompanyLocationId FactoryKey,  
+	  cl.strLocationName FactoryName,  
+	  --clsl.intCompanyLocationSubLocationId LocationKey,  
+	  --ISNULL(clsl.strSubLocationName,'') LocationName,  
+	  0 NewLotUnitKey,  
+	  '' NewLotUnitName,  
+	  0 AuditUnitKey,  
+	  '' AuditUnitName,  
+	  '' DefaultShipFromAddressTitle,  
+	  0 DefaultShipFromAddressID,  
+	  '' DefaultShipToAddressTitle,  
+	  0 DefaultShipToAddressID,  
+	  CONVERT(bit,0) IsDefault,  
+	  0 WIPLocationKey,  
+	  '' [language],  
+	  '' ERPCompanyID,  
+	  '' ERPWareHouseID,  
+	  0 WarehouseID, '' AS WarehouseName, '' AS AddressType  
+	 FROM tblSMUserSecurity smus
+	 JOIN tblSMCompanyLocation cl ON smus.intCompanyLocationId = cl.intCompanyLocationId 
+	 WHERE smus.strUserName = @UserName
+ END
+  
+END  
