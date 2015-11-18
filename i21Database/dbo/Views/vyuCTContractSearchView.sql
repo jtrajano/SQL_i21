d8948460cd7ef1132ae4a95c693a66460b1f9916
Satya <ssahoo@BLRSATYAWIN7.irely.net>
2015-11-18 20:46:34 +0530

    CT-304

--- i21Database/dbo/Views/vyuCTContractSearchView.sql
+++ i21Database/dbo/Views/vyuCTContractSearchView.sql
@@ -5,9 +5,8 @@ AS
 	SELECT		CH.intContractHeaderId,				CH.dtmContractDate,				CH.strEntityName		AS strCustomerVendor,
 				CH.strContractType,					CH.dblHeaderQuantity,			CH.strContractNumber,
 				CH.strCustomerContract,				CH.ysnSigned,					CH.ysnPrinted,
-				BL.dblBalance,						CH.strHeaderUnitMeasure,		CH.dtmDeferPayDate,
+				BL.dblBalance,						CH.dblTolerancePct,				CH.dtmDeferPayDate,
 				CH.dblDeferPayRate,					CH.strInternalComment,			CH.strPrintableRemarks,									
-				CH.dblTolerancePct,
 				CH.dblProvisionalInvoicePct,		CH.ysnPrepaid,					CH.ysnSubstituteItem,
 				CH.ysnUnlimitedQuantity,			CH.ysnMaxPrice,					CH.ysnProvisional,
 				CH.ysnLoad,							CH.intNoOfLoad,					CH.dblQuantityPerLoad,
@@ -23,7 +22,8 @@ AS
 				CH.strApprovalBasis,				CH.strContractBasis,			CH.strPricingType,
 				CH.strPricingLevelName,				CH.strLoadUnitMeasure,			CH.strCategoryUnitMeasure,
 				CH.strLoadCategoryUnitMeasure,		CH.strINCOLocation,				CH.strStatuses,
-				CH.dtmCreated
+				CH.dtmCreated,
+				CASE WHEN CH.ysnLoad = 1 THEN CH.strHeaderUnitMeasure + '/Load' ELSE CH.strHeaderUnitMeasure END strHeaderUnitMeasure
 
 	FROM		vyuCTContractHeaderView		CH	LEFT
 	JOIN
