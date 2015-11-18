de1c1b900d8bc83279275d3f0d17022a8d3e1525
Lawrence Zabala <lawrence.zabala@irely.com>
2015-11-18 13:34:38 +0800

    IC-1533

---
+++ server/iRely.Inventory.BusinessLayer/Implementation/RestrictionBl.cs
@@ -0,0 +1,23 @@
+﻿using iRely.Common;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using iRely.Inventory.Model;
+
+namespace iRely.Inventory.BusinessLayer
+{
+    public class RestrictionBl : BusinessLayer<tblICRestriction>, IRestrictionBl 
+    {
+        #region Constructor
+        public RestrictionBl(IRepository db)
+            : base(db)
+        {
+            _db = db;
+        }
+        #endregion
+    }
+}
---
+++ server/iRely.Inventory.WebAPI/Controllers/RestrictionController.cs
@@ -0,0 +1,25 @@
+﻿using iRely.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using iRely.Inventory.Model;
+using iRely.Inventory.BusinessLayer;
+
+namespace iRely.Inventory.WebApi
+{
+    public class RestrictionController : BaseApiController<tblICRestriction>
+    {
+        private IRestrictionBl _bl;
+
+        public RestrictionController(IRestrictionBl bl)
+            : base(bl)
+        {
+            _bl = bl;
+        }
+
+    }
+}
