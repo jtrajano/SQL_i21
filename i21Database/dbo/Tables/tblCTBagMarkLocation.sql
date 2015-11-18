9a5da88b163778ea53368dc72c206c8e59a93e0b
Satya <ssahoo@BLRSATYAWIN7.irely.net>
2015-11-18 19:50:44 +0530

    CT-287

---
+++ i21Database/dbo/Tables/tblCTBagMarkLocation.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[tblCTBagMarkLocation]
+(
+	[intBagMarkLocationId] [int] IDENTITY(1,1) NOT NULL,
+	[strBagMarkLocationName] [nvarchar](100) COLLATE Latin1_General_CI_AS NOT NULL,
+	[strStartingNumber] [nvarchar](100) COLLATE Latin1_General_CI_AS NULL,
+	[intConcurrencyId] INT NOT NULL, 
+	CONSTRAINT [PK_tblCTBagMarkLocation_intBagMarkLocationId] PRIMARY KEY CLUSTERED ([intBagMarkLocationId] ASC),
+	CONSTRAINT [UK_tblCTBagMarkLocation_strBagMarkLocationName] UNIQUE ([strBagMarkLocationName])
+)
