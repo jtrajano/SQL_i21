e3b461ca62713f4c31cb7d4f116be06107dc7943
Satya <ssahoo@BLRSATYAWIN7.irely.net>
2015-11-19 20:26:36 +0530

    CT-94

--- i21Database/dbo/Tables/tblCTEventMatrix.sql
+++ i21Database/dbo/Tables/tblCTEventMatrix.sql
@@ -2,12 +2,12 @@
 (
 	intEventMatrixId [int] IDENTITY(1,1) NOT NULL,
 	strLoadingPointType [nvarchar](100) COLLATE Latin1_General_CI_AS NULL,
-	[intLoadingPointtId] INT NULL,	
+	[intLoadingPointId] INT NULL,	
 	strDestinationPointType	 [nvarchar](100) COLLATE Latin1_General_CI_AS NULL,
 	[intDestinationPointId] INT NULL,
 	intConcurrencyId INT NOT NULL, 
 	CONSTRAINT [PK_tblCTEventMatrix_intEventMatrixId] PRIMARY KEY CLUSTERED ([intEventMatrixId] ASC),
-	CONSTRAINT [UK_tblCTEventMatrix_LoadingDestinationPointTypeAndId] UNIQUE (strLoadingPointType,[intLoadingPointtId],strDestinationPointType,[intDestinationPointId]),
-	CONSTRAINT [FK_tblCTEventMatrix_tblSMCity_intLoadingPortId_intCityId] FOREIGN KEY ([intLoadingPointtId]) REFERENCES [tblSMCity]([intCityId]),
-	CONSTRAINT [FK_tblCTEventMatrix_tblSMCity_intDestinationPortId_intCityId] FOREIGN KEY ([intDestinationPointId]) REFERENCES [tblSMCity]([intCityId])
+	CONSTRAINT [UK_tblCTEventMatrix_LoadingDestinationPointTypeAndId] UNIQUE (strLoadingPointType,[intLoadingPointId],strDestinationPointType,[intDestinationPointId]),
+	CONSTRAINT [FK_tblCTEventMatrix_tblSMCity_intLoadingPointId_intCityId] FOREIGN KEY ([intLoadingPointId]) REFERENCES [tblSMCity]([intCityId]),
+	CONSTRAINT [FK_tblCTEventMatrix_tblSMCity_intDestinationPointId_intCityId] FOREIGN KEY ([intDestinationPointId]) REFERENCES [tblSMCity]([intCityId])
 )
