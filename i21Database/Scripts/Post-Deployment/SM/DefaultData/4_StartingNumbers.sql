ede46b32d60ef0a53e95d77bc2c7ea283efdd4c8
csDJalimao <DJalimao@COMTECH.COM>
2015-11-19 15:09:26 +0800

    FCOM-1124

--- i21Database/Scripts/Post-Deployment/SM/DefaultData/4_StartingNumbers.sql
+++ i21Database/Scripts/Post-Deployment/SM/DefaultData/4_StartingNumbers.sql
@@ -833,6 +833,16 @@ GO
 			,[intConcurrencyId]		= 1
 	WHERE NOT EXISTS (SELECT TOP 1 1 FROM tblSMStartingNumber WHERE strTransactionType = N'Cancel Equity')
 
+	UNION ALL
+	SELECT	[intStartingNumberId]	= 90
+			,[strTransactionType]	= N'Bag Mark'
+			,[strPrefix]			= N''
+			,[intNumber]			= 1
+			,[strModule]			= 'Contract Management'
+			,[ysnEnable]			= 1
+			,[intConcurrencyId]		= 1
+	WHERE NOT EXISTS (SELECT TOP 1 1 FROM tblSMStartingNumber WHERE strTransactionType = N'Bag Mark')
+
 	SET IDENTITY_INSERT [dbo].[tblSMStartingNumber] OFF
 GO
 	PRINT N'END INSERT DEFAULT STARTING NUMBERS'
