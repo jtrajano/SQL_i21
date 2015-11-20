cd4193cf3627035b964cf4943d08307404b00ed0
Kelvin Quinto <kelvin.quinto@irely.com>
2015-11-20 15:40:53 +0800

    AR-2057

---
+++ i21Database/dbo/Stored Procedures/uspARCalculateCommission.sql
@@ -0,0 +1,118 @@
+﻿CREATE PROCEDURE [dbo].[uspARCalculateCommission]
+	@intCommissionPlanId	INT,
+	@intEntityId			INT,
+	@ysnEmployee			BIT = 0,
+	@ysnAutoCreatePayables	BIT = 0,
+	@ysnAutoCreatePayroll	BIT = 0
+AS
+DECLARE  @intCommissionAccountId	INT
+	   , @strBasis					NVARCHAR(50)
+	   , @strCalculationType		NVARCHAR(50)
+	   , @strHurdleFrequency		NVARCHAR(25)
+	   , @strHurdleType				NVARCHAR(25)
+	   , @dblHurdle					NUMERIC(18,6)
+	   , @dblCalculationAmount		NUMERIC(18,6)
+	   , @strHourType				NVARCHAR(25)
+	   , @strUnitType				NVARCHAR(25)
+	   , @strAccounts				NVARCHAR(MAX)
+	   , @strSalespersons			NVARCHAR(MAX)
+	   , @strAgents					NVARCHAR(MAX)
+	   , @strDrivers				NVARCHAR(MAX)
+	   , @strItemCategories			NVARCHAR(MAX)
+	   , @strItems					NVARCHAR(MAX)
+	   , @strCondition				NVARCHAR(MAX)
+	   , @ysnMarginalSales			BIT
+	   , @ysnPaymentRequired		BIT
+
+SELECT TOP 1 
+	@intCommissionAccountId	= intCommissionAccountId
+  , @strBasis				= strBasis
+  , @strCalculationType		= strCalculationType
+  , @strHurdleFrequency		= strHurdleFrequency
+  , @strHurdleType			= strHurdleType
+  , @strHourType			= CASE WHEN strBasis = 'Hours' THEN strHourType ELSE NULL END
+  , @strUnitType			= CASE WHEN strBasis = 'Units' THEN strUnitType ELSE NULL END
+  , @strAccounts			= CASE WHEN strBasis = 'Revenue' THEN strAccounts ELSE NULL END
+  , @strSalespersons		= CASE WHEN strBasis = 'Revenue' THEN strSalespersons ELSE NULL END
+  , @strAgents				= CASE WHEN strBasis = 'Revenue' THEN strAgents ELSE NULL END
+  , @strDrivers				= CASE WHEN strBasis = 'Revenue' THEN strDrivers ELSE NULL END
+  , @strItemCategories		= CASE WHEN strBasis = 'Revenue' THEN strItemCategories ELSE NULL END
+  , @strItems				= CASE WHEN strBasis = 'Revenue' THEN strItems ELSE NULL END
+  , @strCondition			= CASE WHEN strBasis = 'Conditional' THEN strCondition ELSE NULL END
+  , @dblHurdle				= ISNULL(dblHurdle, 0.000000)
+  , @dblCalculationAmount	= ISNULL(dblCalculationAmount, 0.000000)
+  , @ysnPaymentRequired		= ysnPaymentRequired
+  , @ysnMarginalSales		= CASE WHEN strBasis = 'Revenue' THEN ysnMarginalSales ELSE 0 END
+FROM tblARCommissionPlan 
+WHERE intCommissionId = @intCommissionPlanId
+AND GETDATE() BETWEEN dtmStartDate AND dtmEndDate
+
+IF @strBasis = 'Hours'
+	BEGIN
+		SELECT @strHourType
+	END
+ELSE IF @strBasis = 'Revenue'
+	BEGIN
+		DECLARE @tmpAccountsTable		TABLE (intAccountId INT)
+		DECLARE @tmpSalespersonsTable	TABLE (intSalespersonId INT)
+		DECLARE @tmpAgentsTable			TABLE (intAgentId INT)
+		DECLARE @tmpDriversTable		TABLE (intDriversId INT)
+		DECLARE @tmpItemCategoriesTable TABLE (intItemCategoryId INT)
+		DECLARE @tmpItemsTable			TABLE (intItemId INT)
+	
+		IF ISNULL(@strAccounts, '') <> ''
+			BEGIN
+				INSERT INTO @tmpAccountsTable
+				SELECT intID FROM dbo.fnGetRowsFromDelimitedValues(@strAccounts)
+			END
+
+		IF ISNULL(@strSalespersons, '') <> ''
+			BEGIN
+				INSERT INTO @tmpSalespersonsTable
+				SELECT intID FROM dbo.fnGetRowsFromDelimitedValues(@strSalespersons)
+			END
+
+		IF ISNULL(@strAgents, '') <> ''
+			BEGIN
+				INSERT INTO @tmpAgentsTable
+				SELECT intID FROM dbo.fnGetRowsFromDelimitedValues(@strAgents)
+			END
+
+		IF ISNULL(@strDrivers, '') <> ''
+			BEGIN
+				INSERT INTO @tmpDriversTable
+				SELECT intID FROM dbo.fnGetRowsFromDelimitedValues(@strDrivers)
+			END
+
+		IF ISNULL(@strItemCategories, '') <> ''
+			BEGIN
+				INSERT INTO @tmpItemCategoriesTable
+				SELECT intID FROM dbo.fnGetRowsFromDelimitedValues(@strItemCategories)
+			END
+		
+		IF ISNULL(@strItems, '') <> ''
+			BEGIN
+				INSERT INTO @tmpItemsTable
+				SELECT intID FROM dbo.fnGetRowsFromDelimitedValues(@strItems)
+			END
+	END
+ELSE IF @strBasis = 'Units'
+	BEGIN
+		SELECT @strUnitType
+	END
+ELSE IF @strBasis = 'Conditional'
+	BEGIN
+		SELECT @strCondition
+	END
+
+--CREATE AUTO PAYABLES
+IF @ysnAutoCreatePayables = 1
+BEGIN
+	SELECT * FROM tblAPBill											
+END
+
+--CREATE AUTO PAYROLL
+IF @ysnAutoCreatePayroll = 1 AND @ysnEmployee = 1
+BEGIN
+	SELECT * FROM tblPRPaycheck
+END
--- i21Database/dbo/Stored Procedures/uspARProcessCommissionSchedule.sql
+++ i21Database/dbo/Stored Procedures/uspARProcessCommissionSchedule.sql
@@ -1,34 +1,91 @@
 ﻿CREATE PROCEDURE [dbo].[uspARProcessCommissionSchedule]
 	@commissionIds	NVARCHAR(MAX) = ''
 AS
-	CREATE TABLE #tmpCommissionSchedule (intCommissionScheduleId INT)
-
-	IF (@commissionIds <> '' AND @commissionIds IS NOT NULL)
-		BEGIN
-			DECLARE @commSchedId INT,
-					@pos INT
+	--INSERT PARSE SELECTED COMM. SCHED. FROM STRING INTO TEMP TABLE
+	DECLARE @tmpCommissionSchedule TABLE (intCommissionScheduleId INT)
+	
+	INSERT INTO @tmpCommissionSchedule
+	SELECT intID FROM fnGetRowsFromDelimitedValues(@commissionIds)
 			
-			WHILE CHARINDEX(',', @commissionIds) > 0
-			BEGIN
-				SELECT @pos  = CHARINDEX(',', @commissionIds)  				
-				SELECT @commSchedId = CONVERT(INT, SUBSTRING(@commissionIds, 1, @pos-1))
-
-				INSERT INTO #tmpCommissionSchedule VALUES (@commSchedId)				
-
-				SELECT @commissionIds = SUBSTRING(@commissionIds, @pos + 1, LEN(@commissionIds) - @pos)
-			END
-		END
-
-	IF EXISTS(SELECT NULL FROM #tmpCommissionSchedule)
+	IF EXISTS(SELECT NULL FROM @tmpCommissionSchedule)
 		BEGIN
-			WHILE EXISTS(SELECT TOP 1 1 FROM #tmpCommissionSchedule)
-			BEGIN
-				DECLARE @commissionScheduleId INT
+			--GET COMM. SCHED. INFO WHERE ysnActive = 1
+			DECLARE @tblCommissionSchedules TABLE (intCommissionScheduleId INT, ysnAutoCreatePayables BIT, ysnAutoCreatePayroll BIT)
 
-				SELECT TOP 1 @commissionScheduleId = intCommissionScheduleId FROM #tmpCommissionSchedule
+			WHILE EXISTS(SELECT TOP 1 1 FROM @tmpCommissionSchedule)
+				BEGIN
+					DECLARE @intCommissionScheduleId	INT
 
-				DELETE FROM #tmpCommissionSchedule WHERE intCommissionScheduleId = @commissionScheduleId
-			END
+					SELECT TOP 1 @intCommissionScheduleId = intCommissionScheduleId FROM @tmpCommissionSchedule ORDER BY intCommissionScheduleId
+
+					INSERT INTO @tblCommissionSchedules 
+						  (intCommissionScheduleId
+						 , ysnAutoCreatePayables
+						 , ysnAutoCreatePayroll)
+					SELECT intCommissionScheduleId
+						 , ysnAutoPayables
+						 , ysnAutoPayroll
+					FROM tblARCommissionSchedule
+					WHERE intCommissionScheduleId = @intCommissionScheduleId
+					  AND ysnActive = 1
+
+					DELETE FROM @tmpCommissionSchedule WHERE intCommissionScheduleId = @intCommissionScheduleId
+				END
+
+			--GET COMM. SCHED. DETAILS WHERE intCommissionScheduleId
+			IF EXISTS(SELECT NULL FROM @tblCommissionSchedules)
+				BEGIN
+					DECLARE @tblCommissionScheduleDetails TABLE (intEntityId INT, intCommissionPlanId INT, ysnAdjustForPrevious BIT, ysnEmployee BIT)
+
+					WHILE EXISTS(SELECT TOP 1 1 FROM @tblCommissionSchedules)
+						BEGIN
+							DECLARE @intActiveCommSchedId	INT,
+								    @ysnAutoCreatePayables	BIT,
+								    @ysnAutoCreatePayroll	BIT
+
+							SELECT TOP 1 
+							       @intActiveCommSchedId  = intCommissionScheduleId
+							     , @ysnAutoCreatePayables = ysnAutoCreatePayables
+								 , @ysnAutoCreatePayroll  = ysnAutoCreatePayroll
+							FROM @tblCommissionSchedules ORDER BY intCommissionScheduleId
+
+							INSERT INTO @tblCommissionScheduleDetails
+								  (intEntityId
+								 , intCommissionPlanId
+								 , ysnAdjustForPrevious
+								 , ysnEmployee)
+							SELECT CSD.intEntityId
+							     , CSD.intCommissionId
+								 , CSD.ysnAdjustPrevious
+								 , Employee
+							FROM tblARCommissionScheduleDetail CSD 
+								INNER JOIN tblARCommissionPlan CP ON CSD.intCommissionId = CP.intCommissionId
+								LEFT JOIN vyuEMSearch E ON CSD.intEntityId = E.intEntityId
+							WHERE CSD.intCommissionScheduleId = @intActiveCommSchedId
+							  AND CP.ysnActive = 1
+							
+							--CALCULATE COMMISSION PLANS
+							IF EXISTS(SELECT NULL FROM @tblCommissionScheduleDetails)
+								BEGIN
+									WHILE EXISTS(SELECT TOP 1 1 FROM @tblCommissionScheduleDetails)
+										BEGIN
+											DECLARE @intCommissionPlanId	INT
+									              , @intEntityId			INT
+												  , @ysnEmployee			BIT
+											SELECT TOP 1
+												   @intCommissionPlanId = intCommissionPlanId
+											     , @intEntityId			= intEntityId
+												 , @ysnEmployee			= ysnEmployee
+											FROM @tblCommissionScheduleDetails
+											
+											EXEC dbo.uspARCalculateCommission @intCommissionPlanId, @intEntityId, @ysnEmployee, @ysnAutoCreatePayables, @ysnAutoCreatePayroll
+
+											DELETE FROM @tblCommissionScheduleDetails WHERE intCommissionPlanId = @intCommissionPlanId
+										END									
+								END
+
+							DELETE FROM @tblCommissionSchedules WHERE intCommissionScheduleId = @intActiveCommSchedId
+						END
+				END
+			
 		END
-
-	DROP TABLE #tmpCommissionSchedule
