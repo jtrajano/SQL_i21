b33dddd3f97d07d7d4209701c1e6a12e9e97f20b
csDJalimao <DJalimao@COMTECH.COM>
2015-11-23 15:41:42 +0800

    FCOM-1140

--- i21Database/dbo/Tables/tblSMUserSecurityCompanyLocationRolePermission.sql
+++ i21Database/dbo/Tables/tblSMUserSecurityCompanyLocationRolePermission.sql
@@ -9,5 +9,5 @@
     CONSTRAINT [FK_tblSMUserSecurityCompanyLocationRolePermission_tblSMUserSecurity] FOREIGN KEY ([intEntityUserSecurityId]) REFERENCES [tblSMUserSecurity]([intEntityUserSecurityId]) ON DELETE CASCADE,
 	CONSTRAINT [FK_tblSMUserSecurityCompanyLocationRolePermission_tblSMUserRole] FOREIGN KEY ([intUserRoleId]) REFERENCES [tblSMUserRole]([intUserRoleID]) ON DELETE CASCADE,
 	CONSTRAINT [FK_tblSMUserSecurityCompanyLocationRolePermission_tblSMCompanyLocation] FOREIGN KEY ([intCompanyLocationId]) REFERENCES [tblSMCompanyLocation]([intCompanyLocationId]) ON DELETE CASCADE, 
-    CONSTRAINT [AK_tblSMUserSecurityCompanyLocationRolePermission_Column] UNIQUE ([intEntityId], [intUserRoleId], [intCompanyLocationId])
+    CONSTRAINT [AK_tblSMUserSecurityCompanyLocationRolePermission_Column] UNIQUE ([intEntityUserSecurityId], [intUserRoleId], [intCompanyLocationId])
 )
