26268e83e211575c4c42be0a8cab0875a4a234b9
Mon Gonzales <mon.gonzales@irely.com>
2015-11-23 17:05:17 +0800

    EM-572

---
+++ i21Database/dbo/Tables/tblSMUserSecurityRequireApprovalFor.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[tblSMUserSecurityRequireApprovalFor]
+(
+	[intUserSecurityReqApprovalForId]		INT IDENTITY (1, 1) NOT NULL,
+    [intEntityUserSecurityId]				INT NOT NULL,
+    [strRequireApprovalFor]					NVARCHAR (100) COLLATE Latin1_General_CI_AS NOT NULL,
+    [intConcurrencyId]						INT            NOT NULL,
+    CONSTRAINT [PK_dbo.tblSMUserSecurityRequireApprovalFor] PRIMARY KEY CLUSTERED ([intUserSecurityReqApprovalForId] ASC),
+    CONSTRAINT [FK_dbo.tblSMUserSecurityRequireApprovalFor_tblSMUserSecurity] FOREIGN KEY ([intEntityUserSecurityId]) REFERENCES [dbo].[tblSMUserSecurity] ([intEntityUserSecurityId]) ON DELETE CASCADE
+)
