815e3ce5f17acc88f535626b4a1b5e7c70825a56
Maheshwari <maheshwari.gawade@irely.com>
2015-11-23 12:30:17 +0530

    ST - 170 - Mark Up/Down

---
+++ i21Database/dbo/Tables/tblSTMarkUpDown.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[tblSTMarkUpDown]
+(
+	[intMarkUpDownId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [intStoreId] INT NOT NULL, 
+    [dtmMarkUpDownDate] DATETIME NULL, 
+    [intShiftNo] INT NULL, 
+    [strType] NVARCHAR(50) NULL, 
+    [strAdjustmentType] NVARCHAR(50) NULL, 
+    [intConcurrencyId] INT NULL,
+	CONSTRAINT [AK_tblSTMarkUpDown_intStoreId_dtmMarkUpDownDate_intShiftNo] UNIQUE NONCLUSTERED ([intStoreId],[dtmMarkUpDownDate],[intShiftNo] ASC), 
+    CONSTRAINT [FK_tblSTMarkUpDown_tblSTStore] FOREIGN KEY ([intStoreId]) REFERENCES [tblSTStore]([intStoreId]), 
+	)
---
+++ i21Database/dbo/Tables/tblSTMarkUpDownDetail.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[tblSTMarkUpDownDetail]
+(
+	[intMarkUpDownDetailId] INT NOT NULL PRIMARY KEY,
+	[intMarkUpDownId] INT NOT NULL,
+	[intItemUOMId] INT,
+	[intCategoryId] INT,
+	[strMarkUpOrDown] nvarchar(50),
+	[strRetailShrinkRS] nvarchar(50),
+	[intQty] int, 
+    [dblRetailPerUnit] NUMERIC(18, 6) NULL, 
+    [dblTotalRetailAmount] NUMERIC(18, 6) NULL, 
+    [dblTotalCostAmount] NUMERIC(18, 6) NULL, 
+    [strNote] NVARCHAR(250) NULL, 
+    [dblActulaGrossProfit] NUMERIC(18, 6) NULL, 
+    [ysnSentToHost] BIT NULL, 
+    [strReason] NVARCHAR(250) NULL, 
+    [intConcurrencyId] INT NULL,
+	CONSTRAINT [FK_tblSTMarkUpDownDetail_tblSTMarkUpDown_intMarkUpDownId] FOREIGN KEY ([intMarkUpDownId]) REFERENCES [dbo].[tblSTMarkUpDown] ([intMarkUpDownId]) ON DELETE CASCADE,  
+	
+)
