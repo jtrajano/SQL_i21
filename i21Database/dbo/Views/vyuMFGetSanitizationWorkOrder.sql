1985ec61a28de4320ba781c646bcb34ec71053f2
Premkumar S <prem.selladurai@irely.com>
2015-11-24 20:17:16 +0530

    MFG-1116

--- i21Database/dbo/Views/vyuMFGetSanitizationWorkOrder.sql
+++ i21Database/dbo/Views/vyuMFGetSanitizationWorkOrder.sql
@@ -20,9 +20,9 @@ SELECT W.intWorkOrderId
 	,W.dtmPlannedDate
 FROM dbo.tblMFWorkOrder W
 JOIN dbo.tblMFWorkOrderStatus WS ON W.intStatusId = WS.intStatusId
-	AND W.intOrderHeaderId IS NOT NULL
+	AND W.intOrderHeaderId IS NOT NULL and W.intStatusId <> 13 and W.intManufacturingProcessId IS NULL
 JOIN dbo.tblSMUserSecurity US ON US.intEntityUserSecurityId = W.intCreatedUserId
 JOIN dbo.tblSMUserSecurity US1 ON US1.intEntityUserSecurityId = W.intLastModifiedUserId
 LEFT JOIN dbo.tblWHOrderHeader OH ON OH.intOrderHeaderId = W.intOrderHeaderId
 LEFT JOIN dbo.tblWHOrderStatus OS ON OS.intOrderStatusId = OH.intOrderStatusId
-WHERE W.intStatusId <> 13
+
