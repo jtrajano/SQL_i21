00fb925a767eefb0cf96c0a27ff473d2b6d66336
Erick F. Dela Cruz <erick.delacruz@irely.com>
2015-11-24 13:16:41 +0800

    AP-1880

--- i21Database/dbo/Views/vyuPOStatus.sql
+++ i21Database/dbo/Views/vyuPOStatus.sql
@@ -4,18 +4,23 @@ SELECT
 	A.intPurchaseId
 	,A.strPurchaseOrderNumber
 	,A.intOrderStatusId
+	,A.intShipToId
+	,B.intUnitOfMeasureId
 	,B.intPurchaseDetailId
+	,B.intSubLocationId
+	,B.intStorageLocationId
 	,B.intItemId
 	,B.dblQtyOrdered
 	,B.dblQtyReceived
 	,C.strStatus
 	,D.strType
+	,E.intItemLocationId
 	,ISNULL(BillItems.dblItemQtyBilled,0) AS dblItemQtyBilled
 	,ISNULL(BillItems.dblItemQtyForBill,0) AS dblItemQtyForBill
 	,ISNULL(ReceivedItems.dblIRItemQtyReceive,0) AS dblIRItemQtyReceive
 	,ISNULL(ReceivedItems.dblPOItemQtyReceive,0) AS dblPOItemQtyReceive
-	,ysnItemReceived = CASE WHEN (B.dblQtyOrdered = ISNULL(ReceivedItems.dblPOItemQtyReceive,0)) AND A.intOrderStatusId <> 1 THEN 1 ELSE 0 END
-	,ysnItemBilled = CASE WHEN (B.dblQtyOrdered = ISNULL(BillItems.dblItemQtyBilled,0)) AND A.intOrderStatusId <> 1 THEN 1 ELSE 0 END
+	,ysnItemReceived = CASE WHEN (ISNULL(ReceivedItems.dblPOItemQtyReceive,0) >= B.dblQtyOrdered) AND A.intOrderStatusId <> 1 THEN 1 ELSE 0 END
+	,ysnItemBilled = CASE WHEN (ISNULL(BillItems.dblItemQtyBilled,0) >= B.dblQtyOrdered) AND A.intOrderStatusId <> 1 THEN 1 ELSE 0 END
 FROM tblPOPurchase A
 	INNER JOIN tblPOPurchaseDetail B
 		ON A.intPurchaseId = B.intPurchaseId
@@ -23,6 +28,8 @@ FROM tblPOPurchase A
 		ON A.intOrderStatusId = C.intOrderStatusId
 	INNER JOIN tblICItem D
 		ON B.intItemId = D.intItemId
+	INNER JOIN tblICItemLocation E
+		ON A.intShipToId = E.intLocationId AND B.intItemId = E.intItemId
 	OUTER APPLY (
 		SELECT 
 			SUM(dblIRItemQtyReceive) dblIRItemQtyReceive
@@ -48,13 +55,13 @@ FROM tblPOPurchase A
 	OUTER APPLY (
 		SELECT 
 			E2.intItemId
-			,E2.[intInventoryReceiptItemId]
+			,E2.intPurchaseDetailId
 			,SUM(CASE WHEN E1.ysnPosted = 1 THEN E2.dblQtyReceived ELSE 0 END) AS dblItemQtyBilled
 			,SUM(CASE WHEN E1.ysnPosted = 0 THEN E2.dblQtyReceived ELSE 0 END) AS dblItemQtyForBill
 		FROM tblAPBill E1 INNER JOIN tblAPBillDetail E2
 						ON E1.intBillId = E2.intBillId
-		WHERE E1.ysnPosted = 1 AND E2.[intInventoryReceiptItemId] = B.intPurchaseDetailId
-		GROUP BY E2.intItemId, E2.[intInventoryReceiptItemId]
+		WHERE E1.ysnPosted = 1 AND E2.intPurchaseDetailId = B.intPurchaseDetailId
+		GROUP BY E2.intItemId, E2.intPurchaseDetailId
 	) BillItems
 
 	
