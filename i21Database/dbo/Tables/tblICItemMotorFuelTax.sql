4288a53dfb6ff8d4fb4d1abea96b73a7aa7c3149
Lawrence Zabala <lawrence.zabala@irely.com>
2015-11-25 11:03:51 +0800

    IC-1570

--- i21Database/dbo/Tables/tblICItemMotorFuelTax.sql
+++ i21Database/dbo/Tables/tblICItemMotorFuelTax.sql
@@ -9,5 +9,6 @@
     CONSTRAINT [PK_tblICItemMotorFuelTax] PRIMARY KEY ([intItemMotorFuelTaxId]), 
     CONSTRAINT [FK_tblICItemMotorFuelTax_tblICItem] FOREIGN KEY ([intItemId]) REFERENCES [tblICItem]([intItemId]), 
     CONSTRAINT [FK_tblICItemMotorFuelTax_tblTFTaxAuthority] FOREIGN KEY ([intTaxAuthorityId]) REFERENCES [tblTFTaxAuthority]([intTaxAuthorityId]), 
-    CONSTRAINT [FK_tblICItemMotorFuelTax_tblTFProductCode] FOREIGN KEY ([intProductCodeId]) REFERENCES [tblTFProductCode]([intProductCodeId])
+    CONSTRAINT [FK_tblICItemMotorFuelTax_tblTFProductCode] FOREIGN KEY ([intProductCodeId]) REFERENCES [tblTFProductCode]([intProductCodeId]), 
+    CONSTRAINT [AK_tblICItemMotorFuelTax] UNIQUE ([intItemId], [intTaxAuthorityId], [intProductCodeId])
 )
