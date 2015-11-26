7b2243a83c5623e8a683bfe276f68717ac043fe8
Ian Ocampo <ian.ocampo@irely.com>
2015-11-26 22:18:03 +0800

    FRM-2760

--- i21Database/dbo/Tables/tblSMNotification.sql
+++ i21Database/dbo/Tables/tblSMNotification.sql
@@ -1,18 +1,14 @@
 ﻿CREATE TABLE [dbo].[tblSMNotification] (
     [intNotificationId] INT              IDENTITY (1, 1) NOT NULL,	
 	[intCommentId]      INT              NULL,
-    [strTitle]			NVARCHAR(255)	 COLLATE Latin1_General_CI_AS NULL, 
-    [strMessage]        NVARCHAR (MAX)   COLLATE Latin1_General_CI_AS NULL,
-    [strRoute]			NVARCHAR (MAX)   COLLATE Latin1_General_CI_AS NULL,
+    [strTitle]			NVARCHAR(255)	 COLLATE Latin1_General_CI_AS NULL,
 	[strAction]         NVARCHAR(255)    COLLATE Latin1_General_CI_AS NULL,
 	[strType]           NVARCHAR(255)    COLLATE Latin1_General_CI_AS NULL,
-    [dtmLastUpdated]    DATETIME         NULL,
-	[ysnClosable]		BIT				 NULL,
 	[ysnSent]			BIT				 DEFAULT ((0)) NULL,	
     [ysnSeen]			BIT				 NULL, 
-	[intCounter]		INT				 NULL, 
     [intFromEntityId]	INT				 NULL, 
 	[intToEntityId]     INT              NULL,
     [intConcurrencyId]  INT              NOT NULL,
-    CONSTRAINT [PK_dbo.tblSMNotification] PRIMARY KEY CLUSTERED ([intNotificationId] ASC)
+    CONSTRAINT [PK_dbo.tblSMNotification] PRIMARY KEY CLUSTERED ([intNotificationId] ASC),
+	CONSTRAINT [FK_tblSMNotification_tblSMComment] FOREIGN KEY ([intCommentId]) REFERENCES [dbo].[tblSMComment] ([intCommentId]) ON DELETE CASCADE
 );
