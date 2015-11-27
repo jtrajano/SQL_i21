1f7ef95aeb476ff561df24e57eb80fe46765565b
Swetosuvro <swetosuvro.g@irely.com>
2015-11-27 12:50:47 +0530

    WH-104

---
+++ i21Database/dbo/Stored Procedures/uspWHGetLotIdByLotNo.sql
@@ -0,0 +1,29 @@
+﻿CREATE PROCEDURE uspWHGetLotIdByLotNo 
+		@strLotNumber NVARCHAR(30), 
+		@intStorageLocationId INT = 0
+AS
+BEGIN
+	DECLARE @intLotId INT
+
+	IF @intStorageLocationId = 0
+	BEGIN
+		SET @intLotId = (
+				SELECT TOP 1 intLotId
+				FROM dbo.tblICLot
+				WHERE strLotNumber = @strLotNumber
+				)
+
+		SELECT ISNULL(@intLotId, 0) intLotId, @strLotNumber strLotNo
+	END
+	ELSE
+	BEGIN
+		SET @intLotId = (
+				SELECT TOP 1 intLotId
+				FROM dbo.tblICLot
+				WHERE strLotNumber = @strLotNumber
+					AND intStorageLocationId= @intStorageLocationId
+				)
+
+		SELECT ISNULL(@intLotId, 0) intLotId, @strLotNumber strLotNo
+	END
+END
