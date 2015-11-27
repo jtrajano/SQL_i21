0518720760adc728ddef1f3cc05bbf22cac5aa73
Maheshwari <maheshwari.gawade@irely.com>
2015-11-27 13:16:03 +0530

    ST - 186

---
+++ i21Database/dbo/Tables/tblSTCheckoutCustomerPayments.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[tblSTCheckoutCustomerPayments]
+(
+	[intCustPaymentsId] INT NOT NULL IDENTITY, 
+	[intCheckoutId] INT,
+    [intCustomerId] INT NULL, 
+    [strName] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
+    [intInvoice] INT NULL, 
+    [dblAmount] DECIMAL(18, 6) NULL, 
+    [strType] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
+    [strCheckNo] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
+    [strComment] NVARCHAR(200) COLLATE Latin1_General_CI_AS NULL, 
+    CONSTRAINT [PK_tblSTCheckoutCustomerPayments_intCustPaymentsId] PRIMARY KEY ([intCustPaymentsId]), 
+	CONSTRAINT [FK_tblSTCheckoutCustomerPayments_tblSTCheckoutHeader] FOREIGN KEY ([intCheckoutId]) REFERENCES [tblSTCheckoutHeader]([intCheckoutId]) ,
+    CONSTRAINT [FK_tblSTCheckoutCustomerPayments_tblEntity] FOREIGN KEY ([intCustomerId]) REFERENCES [tblEntity]([intEntityId]) 
+)
--- i21Database/dbo/Tables/tblSTMarkUpDown.sql
+++ i21Database/dbo/Tables/tblSTMarkUpDown.sql
@@ -1,12 +1,13 @@
 ﻿CREATE TABLE [dbo].[tblSTMarkUpDown]
 (
-	[intMarkUpDownId] INT NOT NULL PRIMARY KEY IDENTITY, 
+	[intMarkUpDownId] INT NOT NULL IDENTITY, 
     [intStoreId] INT NOT NULL, 
     [dtmMarkUpDownDate] DATETIME NULL, 
     [intShiftNo] INT NULL, 
-    [strType] NVARCHAR(50) NULL, 
-    [strAdjustmentType] NVARCHAR(50) NULL, 
+    [strType] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
+    [strAdjustmentType] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
     [intConcurrencyId] INT NULL,
+    CONSTRAINT [PK_tblSTMarkUpDown] PRIMARY KEY CLUSTERED ([intMarkUpDownId] ASC), 
 	CONSTRAINT [AK_tblSTMarkUpDown_intStoreId_dtmMarkUpDownDate_intShiftNo] UNIQUE NONCLUSTERED ([intStoreId],[dtmMarkUpDownDate],[intShiftNo] ASC), 
     CONSTRAINT [FK_tblSTMarkUpDown_tblSTStore] FOREIGN KEY ([intStoreId]) REFERENCES [tblSTStore]([intStoreId]), 
 	)
