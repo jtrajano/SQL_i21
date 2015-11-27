26f523c7889e5d1a9a2bcb79fd35225c9730dcde
Ian Ocampo <ian.ocampo@irely.com>
2015-11-27 23:05:12 +0800

    FRM-2760

---
+++ i21Database/dbo/Tables/tblSMMessage.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[tblSMMessage](
+	[intMessageId] [int] IDENTITY(1,1) NOT NULL,
+	[intEntityId] [int] NULL,
+	[strSubject] [nvarchar](max) NULL,
+	[dtmAdded] [datetime] NULL DEFAULT (GETDATE()),
+	[dtmModified] [datetime] NULL DEFAULT (GETDATE()),
+	[intConcurrencyId] [int] NOT NULL
+	CONSTRAINT [PK_tblSMMessage] PRIMARY KEY ([intMessageId])
+ )
