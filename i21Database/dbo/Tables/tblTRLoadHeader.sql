f2b00ae002160c75f01439d9537acc77faaaea80
Pruthvi <pruthvi.c@irely.com>
2015-11-27 12:59:54 +0530

    TR-239

---
+++ i21Database/dbo/Tables/tblTRLoadHeader.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[tblTRLoadHeader]
+(
+	[intLoadHeaderId] INT NOT NULL IDENTITY,
+	[intLoadId] INT NULL,
+	[strTransaction] nvarchar(50) COLLATE Latin1_General_CI_AS NOT NULL,
+	[dtmLoadDateTime]  DATETIME        NOT NULL,
+	[intShipViaId] INT NOT NULL,	
+	[intSellerId] INT NOT NULL,	
+	[intDriverId] INT NOT NULL,	
+    [strTractor] nvarchar(50) COLLATE Latin1_General_CI_AS NULL,
+	[strTrailer] nvarchar(50) COLLATE Latin1_General_CI_AS NULL,
+	[ysnPosted]  BIT  DEFAULT ((0)) NOT NULL,
+	[intConcurrencyId] [int] NOT NULL,
+	CONSTRAINT [PK_tblTRLoadHeader] PRIMARY KEY ([intLoadHeaderId]),
+	CONSTRAINT [FK_tblTRLoadHeader_tblSMShipVia_intShipViaId] FOREIGN KEY ([intShipViaId]) REFERENCES [dbo].[tblSMShipVia] ([intEntityShipViaId]),
+	CONSTRAINT [FK_tblTRLoadHeader_tblSMShipVia_intSellerId] FOREIGN KEY ([intSellerId]) REFERENCES [dbo].[tblSMShipVia] ([intEntityShipViaId]),
+	CONSTRAINT [FK_tblTRLoadHeader_tblARSalesperson_intDriverId] FOREIGN KEY ([intDriverId]) REFERENCES [dbo].[tblARSalesperson] ([intEntitySalespersonId]),
+	CONSTRAINT [FK_tblTRLoadHeader_tblLGLoad_intLoadId] FOREIGN KEY ([intLoadId]) REFERENCES [dbo].[tblLGLoad] ([intLoadId])
+
+)
