fc01fe17f96f74f53600140bc9351fcbef2beba8
Satya <ssahoo@BLRSATYAWIN7.irely.net>
2015-11-29 10:36:13 +0530

    CT-348

--- i21Database/dbo/Views/vyuCTContStsContractSummary.sql
+++ i21Database/dbo/Views/vyuCTContStsContractSummary.sql
@@ -2,26 +2,27 @@
 
 AS
 
+	
 	SELECT	CAST(ROW_NUMBER() OVER (ORDER BY UP.intContractDetailId ASC) AS INT) intUniqueId,
 			UP.intContractDetailId,
 			UP.strName,
 			UP.strValue
 	FROM	(
 				SELECT	CD.intContractDetailId,
-						CAST(CAST(CD.dblQuantity AS NUMERIC(18,2)) AS NVARCHAR(100)) collate Latin1_General_CI_AS Quantity,
-						CAST(LTRIM(CAST(CD.dblBasis AS NUMERIC(18,2)))  + ' ' + CY.strCurrency + ' Per ' + PM.strUnitMeasure AS NVARCHAR(100) ) collate Latin1_General_CI_AS [Differential],
-						CAST(LTRIM(PF.intLotsFixed) + '/' + LTRIM(PF.intTotalLots - PF.intLotsFixed) AS NVARCHAR(100)) collate Latin1_General_CI_AS AS [Fixed/Unfixed],
-						CAST(LTRIM(PF.intLotsHedged) + '/' + LTRIM(PF.intTotalLots - PF.intLotsHedged) AS NVARCHAR(100)) collate Latin1_General_CI_AS AS [Hedge/Not Hedge],
-						CAST(LTRIM(CAST(PF.dblFinalPrice AS NUMERIC(18,2)))  + ' ' + CY.strCurrency + ' Per ' + FM.strUnitMeasure AS NVARCHAR(100)) collate Latin1_General_CI_AS [Final Price]
+						ISNULL(CAST(CAST(CD.dblQuantity AS NUMERIC(18,2)) AS NVARCHAR(100)) collate Latin1_General_CI_AS,'') Quantity,
+						ISNULL(CAST(LTRIM(CAST(CD.dblBasis AS NUMERIC(18,2)))  + ' ' + CY.strCurrency + ' Per ' + PM.strUnitMeasure AS NVARCHAR(100) ) collate Latin1_General_CI_AS,'') [Differential],
+						ISNULL(CAST(LTRIM(PF.intLotsFixed) + '/' + LTRIM(PF.intTotalLots - PF.intLotsFixed) AS NVARCHAR(100)) collate Latin1_General_CI_AS,'') AS [Fixed/Unfixed],
+						ISNULL(CAST(LTRIM(PF.intLotsHedged) + '/' + LTRIM(PF.intTotalLots - PF.intLotsHedged) AS NVARCHAR(100)) collate Latin1_General_CI_AS,'') AS [Hedge/Not Hedge],
+						ISNULL(CAST(LTRIM(CAST(ISNULL(PF.dblFinalPrice,CD.dblCashPrice) AS NUMERIC(18,2)))  + ' ' + CY.strCurrency + ' Per ' + ISNULL(FM.strUnitMeasure,PM.strUnitMeasure) AS NVARCHAR(100)) collate Latin1_General_CI_AS,'') [Final Price]
 				FROM	tblCTContractDetail			CD LEFT
 				JOIN	tblCTPriceFixation			PF	ON	PF.intContractDetailId			=		CD.intContractDetailId	LEFT
 				JOIN	tblSMCurrency				CY	ON	CY.intCurrencyID				=		CD.intCurrencyId		LEFT
 				JOIN	tblICItemUOM				PU	ON	PU.intItemUOMId					=		CD.intPriceItemUOMId	LEFT
 				JOIN	tblICUnitMeasure			PM	ON	PM.intUnitMeasureId				=		PU.intUnitMeasureId		LEFT
 				JOIN	tblICCommodityUnitMeasure	FU	ON	FU.intCommodityUnitMeasureId	=		PF.intFinalPriceUOMId	LEFT
-				JOIN	tblICUnitMeasure			FM	ON	FM.intUnitMeasureId				=		FU.intUnitMeasureId
+				JOIN	tblICUnitMeasure			FM	ON	FM.intUnitMeasureId				=		FU.intUnitMeasureId		
 			) s
-	UNPIVOT	(strValue FOR strName IN 
+	UNPIVOT	(	strValue FOR strName IN 
 				(
 					[Quantity],
 					[Differential],
