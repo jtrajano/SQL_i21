e1bbee4c3037ee8059323c21221ec468c133e21e
Dan Michael Jalimao <dan.jalimao@irely.com>
2015-11-30 12:25:34 +0800

    FCOM-1145

--- i21Database/dbo/Tables/tblEntityCredential.sql
+++ i21Database/dbo/Tables/tblEntityCredential.sql
@@ -9,10 +9,12 @@
 	[strTFASecretKey]		NVARCHAR(MAX)  COLLATE Latin1_General_CI_AS NULL, 
 	[strTFACurrentCode]     NVARCHAR(MAX)  COLLATE Latin1_General_CI_AS NULL, 
 	[ysnTFAEnabled]			BIT            DEFAULT ((0)) NULL, 
+	[intEntityRoleId]		INT				NULL,
     [intConcurrencyId]      INT            DEFAULT ((1)) NOT NULL
     CONSTRAINT [PK_tblEntityCredential] PRIMARY KEY CLUSTERED ([intEntityCredentialId] ASC),
     CONSTRAINT [FK_tblEntityCredential_tblEntity] FOREIGN KEY ([intEntityId]) REFERENCES [dbo].[tblEntity] ([intEntityId]) ON DELETE CASCADE,
-    CONSTRAINT [AK_tblEntityCredential_strUserName] UNIQUE NONCLUSTERED ([strUserName] ASC)
+    CONSTRAINT [AK_tblEntityCredential_strUserName] UNIQUE NONCLUSTERED ([strUserName] ASC),
+	CONSTRAINT [FK_tblEntityCredential_tblSMUserRole] FOREIGN KEY ([intEntityRoleId]) REFERENCES [tblSMUserRole]([intUserRoleID])
 );
 
 
