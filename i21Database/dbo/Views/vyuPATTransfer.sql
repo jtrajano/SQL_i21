f04a1430101283123982a792fad67c31cc0201a4
Melanie Ferrer <Mel.Ferrer@irely.com>
2015-11-30 14:55:02 +0800

    PAT-150

---
+++ i21Database/dbo/Views/vyuPATTransfer.sql
@@ -0,0 +1,22 @@
+﻿CREATE VIEW [dbo].[vyuPATTransfer]
+	AS 
+	SELECT  PT.intTransferId,
+		    PT.strTransferNo,
+			PT.dtmTransferDate,
+			PT.strTransferType,
+			PTD.intTransferorId,
+			strTransferorName = (SELECT DISTINCT strName FROM tblEntity WHERE intEntityId = PTD.intTransferorId),
+			PTD.strCertificateNo,
+			PTD.strStockName,
+			PTD.dblQuantityAvailable,
+			PTD.intTransfereeId,
+			strTransfereeName = (SELECT DISTINCT strName FROM tblEntity WHERE intEntityId = PTD.intTransfereeId),
+			PTD.strToCertificateNo,
+			PTD.strToStockName,
+			PTD.dblQuantityTransferred,
+			PT.intConcurrencyId
+		FROM tblPATTransfer PT
+INNER JOIN tblPATTransferDetail PTD
+		ON PT.intTransferId = PTD.intTransferId
+
+
