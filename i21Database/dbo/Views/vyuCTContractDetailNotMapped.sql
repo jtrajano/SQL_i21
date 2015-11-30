3ffac71767b928d23126496445e7cfcfcf587ae5
Satya <ssahoo@BLRSATYAWIN7.irely.net>
2015-11-30 23:57:50 +0530

    CT-127

--- i21Database/dbo/Views/vyuCTContractDetailNotMapped.sql
+++ i21Database/dbo/Views/vyuCTContractDetailNotMapped.sql
@@ -14,14 +14,29 @@ SELECT	CD.intContractDetailId,
 		PF.intLotsFixed,
 		IC.strContractItemName,
 		WU.strUnitMeasure strNetWeightUOM,
-		RY.strCountry AS strOrigin
-		
-FROM	tblCTContractDetail CD	LEFT
-JOIN	tblICItemContract	IC	ON	IC.intItemContractId	=	CD.intItemContractId	LEFT
-JOIN	tblSMCountry		RY	ON	RY.intCountryID			=	IC.intCountryId			LEFT
-JOIN	tblICItemUOM		WM	ON	WM.intItemUOMId			=	CD.intNetWeightUOMId	LEFT
-JOIN	tblICUnitMeasure	WU	ON	WU.intUnitMeasureId		=	WM.intUnitMeasureId		LEFT		
-JOIN	tblCTPriceFixation	PF	ON	CD.intContractDetailId	=	PF.intContractDetailId	LEFT
+		RY.strCountry AS strOrigin,
+		CASE	WHEN	CH.ysnCategory = 1
+				THEN	dbo.fnCTConvertQtyToTargetCategoryUOM(CD.intCategoryUOMId,GU.intCategoryUOMId,1)
+				ELSE	dbo.fnCTConvertQtyToTargetItemUOM(CD.intItemUOMId,CM.intItemUOMId,1) 
+		END		AS		dblConversionFactor,
+		ISNULL(QM.strUnitMeasure,YM.strUnitMeasure)	AS	strUOM
+
+FROM	tblCTContractDetail			CD	
+JOIN	tblCTContractHeader			CH	ON	CH.intContractHeaderId			=	CD.intContractHeaderId		LEFT
+JOIN	tblICItemContract			IC	ON	IC.intItemContractId			=	CD.intItemContractId		LEFT
+JOIN	tblSMCountry				RY	ON	RY.intCountryID					=	IC.intCountryId				LEFT
+JOIN	tblICItemUOM				QU	ON	QU.intItemUOMId					=	CD.intItemUOMId				LEFT
+JOIN	tblICUnitMeasure			QM	ON	QM.intUnitMeasureId				=	QU.intUnitMeasureId			LEFT
+JOIN	tblICItemUOM				WM	ON	WM.intItemUOMId					=	CD.intNetWeightUOMId		LEFT
+JOIN	tblICUnitMeasure			WU	ON	WU.intUnitMeasureId				=	WM.intUnitMeasureId			LEFT
+JOIN	tblICCommodityUnitMeasure	CO	ON	CO.intCommodityUnitMeasureId	=	CH.intCommodityUOMId		LEFT
+JOIN	tblICItemUOM				CM	ON	CM.intItemId					=	CD.intItemId				AND
+											CM.intUnitMeasureId				=	CO.intUnitMeasureId			LEFT
+JOIN	tblICCategoryUOM			YU	ON	YU.intCategoryUOMId				=	CD.intCategoryUOMId			LEFT
+JOIN	tblICUnitMeasure			YM	ON	YM.intUnitMeasureId				=	YU.intUnitMeasureId			LEFT
+JOIN	tblICCategoryUOM			GU	ON	GU.intCategoryId				=	CD.intCategoryId			AND
+											GU.intUnitMeasureId				=	CH.intCategoryUnitMeasureId	LEFT	
+JOIN	tblCTPriceFixation			PF	ON	CD.intContractDetailId			=	PF.intContractDetailId		LEFT
 JOIN	(
 			SELECT	 intPriceFixationId,
 					 COUNT(intPriceFixationDetailId) intPFDCount,
@@ -29,4 +44,4 @@ JOIN	(
 					 MAX(intQtyItemUOMId) dblPFQuantityUOMId  
 			FROM	 tblCTPriceFixationDetail
 			GROUP BY intPriceFixationId
-		)					PD	ON	PD.intPriceFixationId	=	PF.intPriceFixationId
+		)							PD	ON	PD.intPriceFixationId			=	PF.intPriceFixationId
--- i21Database/dbo/Views/vyuCTContractHeaderNotMapped.sql
+++ i21Database/dbo/Views/vyuCTContractHeaderNotMapped.sql
@@ -7,14 +7,17 @@ AS
 	FROM	(
 				SELECT	CH.intContractHeaderId,
 						PF.intPriceFixationId, 
-						CASE WHEN	(	
-										SELECT	COUNT(SA.intSpreadArbitrageId) 
-										FROM	tblCTSpreadArbitrage SA  
-										WHERE	SA.intPriceFixationId = PF.intPriceFixationId
-									) > 0
-						THEN	CAST(1 AS BIT) 
-						ELSE	CAST(0 AS BIT)
-						END		AS ysnSpreadAvailable,
+						CASE	WHEN	(	
+											SELECT	COUNT(SA.intSpreadArbitrageId) 
+											FROM	tblCTSpreadArbitrage SA  
+											WHERE	SA.intPriceFixationId = PF.intPriceFixationId
+										) > 0
+								THEN	CAST(1 AS BIT) 
+								ELSE	CAST(0 AS BIT)
+						END		AS		ysnSpreadAvailable,
+
+						dbo.fnCTConvertQuantityToTargetCommodityUOM(CH.intLoadUOMId,CH.intCommodityUOMId,1)	AS	dblCommodityUOMConversionFactor,
+
 						dbo.fnCTGetBillIds(CH.intContractHeaderId) strBillIds
 				FROM	tblCTContractHeader CH	LEFT
 				JOIN	tblCTPriceFixation	PF	ON	CH.intContractHeaderId = PF.intContractHeaderId
