419aca3e09f0844897dd06d01f32ff0ab93316c1
Gopinath Nayak <gopinath.n@irely.com>
2015-12-01 22:49:54 +0530

    GRN-278

---
+++ i21Database/dbo/Views/vyuGRGetActionEntity.sql
@@ -0,0 +1,24 @@
+﻿CREATE VIEW vyuGRGetActionEntity
+AS
+	SELECT	E.intEntityId,
+			E.strName			AS strEntityName,
+			Y.strType			AS strEntityType,
+			E.strEntityNo		AS strEntityNumber,
+			L.strAddress		AS strEntityAddress,
+			L.strCity			AS strEntityCity,
+			L.strState			AS strEntityState,
+			L.strZipCode		AS strEntityZipCode,
+			L.strCountry		AS strEntityCountry,
+			T.strPhone			AS strEntityPhone,
+			E.intDefaultLocationId,
+			V.ysnPymtCtrlActive ysnActive,
+			CASE 
+				WHEN ISNULL(V.intCurrencyId,0)=0 THEN (SELECT intDefaultCurrencyId FROm tblSMCompanyPreference) ELSE V.intCurrencyId 
+			END	intCurrencyId		
+	FROM	tblEntity			E
+	JOIN	tblEntityLocation	L	ON	E.intEntityId			=	L.intEntityId AND L.ysnDefaultLocation = 1
+	JOIN	tblEntityType		Y	ON	Y.intEntityId			=	E.intEntityId
+	JOIN	tblEntityToContact	C	ON	C.intEntityId			=	E.intEntityId AND C.ysnDefaultContact = 1
+	JOIN	tblEntity			T	ON	T.intEntityId			=	C.intEntityContactId	LEFT 
+	JOIN	tblAPVendor			V	ON	V.intEntityVendorId		=	E.intEntityId
+	WHERE Y.strType = 'Vendor'
