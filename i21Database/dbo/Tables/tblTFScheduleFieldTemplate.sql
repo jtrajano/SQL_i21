81b4a4554683cbda6cee50b7bee4e4462bbee031
Mark Paragas <Mark.Paragas@irely.com>
2015-12-01 14:23:04 +0800

    MFT

---
+++ i21Database/dbo/Tables/tblTFScheduleFieldTemplate.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[tblTFScheduleFieldTemplate](
+	[intScheduleFieldTemplateId] [int] IDENTITY(1,1) NOT NULL,
+	[intReportingComponentDetailId] [int] NOT NULL,
+	[strColumn] [nvarchar](max) NOT NULL,
+	[intConcurrencyId] [int] CONSTRAINT [DF_tblTFScheduleFieldTemplate_intConcurrencyId] DEFAULT ((1)) NULL,
+ CONSTRAINT [PK_tblTFScheduleFieldsTemplate] PRIMARY KEY CLUSTERED 
+(
+	[intScheduleFieldTemplateId] ASC
+))
