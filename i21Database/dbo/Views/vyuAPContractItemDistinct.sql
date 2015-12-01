94f1638fc64f0b87fe97ea12ec72f96ba443dc97
Lex Eugene Peregrino <lex.peregrino@irely.com>
2015-12-01 19:30:08 +0800

    AP-1731 - Additional Column Contract TotalBalance

--- i21Database/dbo/Views/vyuAPContractItemDistinct.sql
+++ i21Database/dbo/Views/vyuAPContractItemDistinct.sql
@@ -2,6 +2,7 @@
 AS
 
 SELECT
+	DISTINCT
 	A.intContractHeaderId,
 	A.intContractDetailId,
 	A.intEntityId,
@@ -14,12 +15,34 @@ SELECT
 	A.strPricingType,
 	A.dblAdjustment,
 	A.dblCashPrice,
-	A.dblBalance
+	A.dblBalance,
+	dblTotalBalance = Total.dblTotalBalance
 FROM vyuCTContractDetailView A
 INNER JOIN
 (
-	SELECT intContractHeaderId, intItemId, MIN(dblCashPrice) dblCashPriceMin FROM vyuCTContractDetailView GROUP BY intContractHeaderId, intItemId
+	SELECT intContractHeaderId, 
+		   intItemId, 
+	MIN(dblCashPrice) dblCashPriceMin 
+	FROM vyuCTContractDetailView 
+	GROUP BY 
+			intContractHeaderId, 
+			intItemId
 ) MinimumCashPrice 
 ON A.intContractHeaderId = MinimumCashPrice.intContractHeaderId 
 AND A.intItemId = MinimumCashPrice.intItemId
 AND A.dblCashPrice = MinimumCashPrice.dblCashPriceMin
+CROSS APPLY
+(
+	SELECT intContractHeaderId, 
+		   SUM(dblBalance) AS dblTotalBalance, 
+		   intItemId 
+	FROM vyuCTContractDetailView B
+	WHERE B.intContractHeaderId = A.intContractHeaderId  
+		  AND B.intItemId = A.intItemId
+	GROUP BY 
+			intContractHeaderId, 
+			dblBalance, 
+			intItemId	
+) Total 
+
+GO
