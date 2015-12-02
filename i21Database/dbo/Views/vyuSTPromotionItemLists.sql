aa9383e07abba0e46d2cbb5c1037a9423c3e4017
unknown <rnkumashi@BLRRAVIWIN7.irely.net>
2015-12-02 15:22:12 +0530

    ST-185

---
+++ i21Database/dbo/Views/vyuSTPromotionItemLists.sql
@@ -0,0 +1,13 @@
+﻿CREATE VIEW vyuSTPromotionItemLists
+AS
+SELECT 
+adj3.strLocationName, 
+adj1.strUpcCode,
+adj5.strDescription,
+adj4.dblSalePrice,
+adj2.intFamilyId,
+adj2.intClassId
+from tblICItemUOM adj1 JOIN tblICItemLocation adj2 ON adj1.intItemId = adj2.intItemId
+JOIN tblSMCompanyLocation adj3 ON adj3.intCompanyLocationId = adj2.intLocationId
+JOIN tblICItemPricing adj4 on adj4.intItemLocationId = adj2.intItemLocationId
+JOIN tblICItem adj5 ON adj5.intItemId = adj1.intItemId and adj1.strUpcCode IS NOT NULL
