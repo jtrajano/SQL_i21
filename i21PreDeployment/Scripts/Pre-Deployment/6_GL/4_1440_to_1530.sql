7be95101bde0d4da7f06f9a9da764cadeed42088
jeffrey trajano <jeffrey.trajano@irely.com>
2015-12-03 16:16:11 +0800

    GL-2391 Error on predeployment script (new company)

--- i21PreDeployment/Scripts/Pre-Deployment/6_GL/4_1440_to_1530.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/6_GL/4_1440_to_1530.sql
@@ -16,30 +16,38 @@ END
 PRINT 'Finished Renaming tblGLTempCOASegment column Location to Location'
 
 PRINT 'Begin Fixing Segment Categories'
-IF EXISTS(SELECT TOP 1 1 FROM tblGLAccountSegment where intAccountCategoryId NOT IN (SELECT intAccountCategoryId FROM tblGLAccountCategory))
-BEGIN
-	--not used in category but used in account table
-	IF EXISTS(SELECT 1 FROM sys.columns 
-	WHERE [name] = N'intAccountCategoryId' AND [object_id] = OBJECT_ID(N'tblGLAccount'))
-	BEGIN
-		declare @sqlStmt NVARCHAR(MAX) =
-		'UPDATE t SET intAccountCategoryId =(
-		select TOP 1 intAccountCategoryId FROM tblGLAccount where intAccountId =
-		 (SELECT TOP 1  intAccountId from tblGLAccountSegmentMapping WHERE intAccountSegmentId = t.intAccountSegmentId ))
-		FROM tblGLAccountSegment t
-		WHERE intAccountCategoryId NOT IN (SELECT intAccountCategoryId FROM tblGLAccountCategory)
-		AND intAccountSegmentId IN (SELECT intAccountSegmentId FROM tblGLAccountSegmentMapping)'
-		EXEC sp_executesql @sqlStmt
-	END
 
-	--not used in category and account table
-	DECLARE @GeneralCategoryId INT
-	SELECT @GeneralCategoryId = intAccountCategoryId FROM tblGLAccountCategory where strAccountCategory = 'General'
-	UPDATE tblGLAccountSegment SET intAccountCategoryId = @GeneralCategoryId
-	WHERE intAccountCategoryId NOT IN (SELECT intAccountCategoryId FROM tblGLAccountCategory)
-	AND intAccountSegmentId NOT IN (SELECT intAccountSegmentId FROM tblGLAccountSegmentMapping)
+IF EXISTS (SELECT 1 FROM sys.objects WHERE name = 'tblGLAccount' and type = 'U')
+	AND EXISTS (SELECT 1 FROM sys.objects WHERE name = 'tblGLAccountSegmentMapping' and type = 'U')
+	AND EXISTS (SELECT 1 FROM sys.objects WHERE name = 'tblGLAccountSegment' and type = 'U')
+	AND EXISTS (SELECT 1 FROM sys.objects WHERE name = 'tblGLAccountCategory' and type = 'U')
+	BEGIN
+	IF EXISTS(SELECT TOP 1 1 FROM tblGLAccountSegment where intAccountCategoryId NOT IN (SELECT intAccountCategoryId FROM tblGLAccountCategory))
+	BEGIN
+		--not used in category but used in account table
+	
+			IF EXISTS(SELECT 1 FROM sys.columns 
+			WHERE [name] = N'intAccountCategoryId' AND [object_id] = OBJECT_ID(N'tblGLAccount'))
+			BEGIN
+				declare @sqlStmt NVARCHAR(MAX) =
+				'UPDATE t SET intAccountCategoryId =(
+				select TOP 1 intAccountCategoryId FROM tblGLAccount where intAccountId =
+				 (SELECT TOP 1  intAccountId from tblGLAccountSegmentMapping WHERE intAccountSegmentId = t.intAccountSegmentId ))
+				FROM tblGLAccountSegment t
+				WHERE intAccountCategoryId NOT IN (SELECT intAccountCategoryId FROM tblGLAccountCategory)
+				AND intAccountSegmentId IN (SELECT intAccountSegmentId FROM tblGLAccountSegmentMapping)'
+				EXEC sp_executesql @sqlStmt
+			END
+
+			--not used in category and account table
+			DECLARE @GeneralCategoryId INT
+			SELECT @GeneralCategoryId = intAccountCategoryId FROM tblGLAccountCategory where strAccountCategory = 'General'
+			UPDATE tblGLAccountSegment SET intAccountCategoryId = @GeneralCategoryId
+			WHERE intAccountCategoryId NOT IN (SELECT intAccountCategoryId FROM tblGLAccountCategory)
+			AND intAccountSegmentId NOT IN (SELECT intAccountSegmentId FROM tblGLAccountSegmentMapping)
 	
 	
+	END
 END
 PRINT 'Finish Fixing Segment Categories'
 GO
