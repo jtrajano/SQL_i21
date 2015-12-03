e855ed5881bdaf28554244c9ef86e93358e56f02
Mark Paragas <Mark.Paragas@irely.com>
2015-12-03 16:57:39 +0800

    MFT fix contraint issue

---
+++ i21Database/Scripts/Post-Deployment/TF/DefaultData/_Frequency.sql
@@ -0,0 +1,8 @@
+﻿GO
+SET IDENTITY_INSERT [dbo].[tblTFFrequency] ON 
+
+INSERT [dbo].[tblTFFrequency] ([intFrequencyId], [strFrequency], [intConcurrencyId]) VALUES (1, N'Weekly', 1)
+INSERT [dbo].[tblTFFrequency] ([intFrequencyId], [strFrequency], [intConcurrencyId]) VALUES (2, N'Monthly', 1)
+INSERT [dbo].[tblTFFrequency] ([intFrequencyId], [strFrequency], [intConcurrencyId]) VALUES (3, N'Daily', 1)
+INSERT [dbo].[tblTFFrequency] ([intFrequencyId], [strFrequency], [intConcurrencyId]) VALUES (4, N'Yearly', 1)
+SET IDENTITY_INSERT [dbo].[tblTFFrequency] OFF
