4d15549d6f11a3a021167eed9b9af583a5bd1ff5
Mon Gonzales <mon.gonzales@irely.com>
2015-12-03 18:04:36 +0800

    EM-756

---
+++ i21Database/dbo/Views/vyuEMCustomerGroupWithDetail.sql
@@ -0,0 +1,8 @@
+﻿CREATE VIEW [dbo].[vyuEMCustomerGroupWithDetail]
+	AS 
+	SELECT
+		intCustomerGroupId,
+		strGroupName,
+		strDescription,
+		strCustomerGroupMember = dbo.fnEMGetCustomerGroupMember(a.intCustomerGroupId)  
+			from tblARCustomerGroup a
