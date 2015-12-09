08efaf1c5d02648fb853bfa4ff00eda945115bc1
Mark Paragas <Mark.Paragas@irely.com>
2015-12-09 13:40:26 +0800

    MFT - Added Transaction data

--- i21Database/Scripts/Post-Deployment/PostDeployment.sql
+++ i21Database/Scripts/Post-Deployment/PostDeployment.sql
@@ -276,6 +276,7 @@ print 'BEGIN POST DEPLOYMENT'
 :r .\TF\DefaultData\_TerminalControlNumber.sql
 :r .\TF\DefaultData\_ConfigurationType.sql
 :r .\TF\DefaultData\_ConfigurationTemplate.sql
+:r .\TF\DefaultData\_Transaction.sql
 
 -- Common
 :r .\Common\ErrorMessages.sql 
