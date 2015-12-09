c52a47170d4f7dbd2bbf85ec41f4b2c28e36472a
kenneth lagunero <kenneth.lagunero@irely.com>
2015-12-09 09:51:59 +0800

    TM-1665

--- Integration/dbo/Views/vwDispatch.sql
+++ Integration/dbo/Views/vwDispatch.sql
@@ -9,9 +9,12 @@ IF  (((SELECT TOP 1 ysnUsed FROM ##tblOriginMod WHERE strPrefix = 'AG' and strDB
 	(SELECT TOP 1 1 TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'vwitmmst') = 1 and
 	(SELECT TOP 1 1 TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'vwslsmst') = 1 and
 	(SELECT TOP 1 1 TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'vwlocmst') = 1 and
-	(SELECT TOP 1 1 TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'vwcusmst') = 1
+	(SELECT TOP 1 1 TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'vwcusmst') = 1 
+	
 BEGIN
-	EXEC('
+	IF((SELECT TOP 1 ysnUseOriginIntegration FROM tblTMPreferenceCompany) = 1)
+	BEGIN
+		EXEC('
 		CREATE VIEW [dbo].[vwDispatch]
 			AS
 			SELECT
@@ -53,16 +56,71 @@ BEGIN
 			INNER JOIN tblTMCustomer C ON B.intCustomerID = C.intCustomerID
 			INNER JOIN tblTMClock D ON D.intClockID = B.intClockID
 			INNER JOIN tblTMRoute E ON B.intRouteId = E.intRouteId
-			INNER JOIN vwlocmst K ON B.intLocationId = K.A4GLIdentity
 			INNER JOIN vwitmmst F ON CAST(F.A4GLIdentity AS INT) = B.intProduct 
-				AND K.vwloc_loc_no = F.vwitm_loc_no
 			INNER JOIN vwslsmst G ON CAST(G.A4GLIdentity AS INT) = A.intDriverID
 			INNER JOIN vwcusmst H ON H.A4GLIdentity = C.intCustomerNumber
 			WHERE B.ysnActive  = 1
 				AND H.vwcus_active_yn = ''Y''
 		
 	')
+	END
+	ELSE
+	BEGIN
+		EXEC('
+			CREATE VIEW [dbo].[vwDispatch]
+				AS
+				SELECT
+				DispatchID = A.intDispatchID,
+				CustomerNumber = H.vwcus_key + '' '' + CASE H.vwcus_co_per_ind_cp
+						WHEN ''C'' THEN RTRIM(H.vwcus_last_name) + H.vwcus_first_name
+						ELSE RTRIM(H.vwcus_last_name) + '','' + H.vwcus_first_name
+						END,
+				CustomerName = 
+						CASE H.vwcus_co_per_ind_cp
+						WHEN ''C'' THEN RTRIM(H.vwcus_last_name) + H.vwcus_first_name
+						ELSE RTRIM(H.vwcus_last_name) + '','' + H.vwcus_first_name
+						END,
+				SiteNumber = RIGHT(CAST(''0000'' + CAST(B.intSiteNumber as varchar) as varchar), 4)+'' ''+LTRIM(B.strSiteAddress),--B.intSiteNumber,
+				SiteAddress = B.strSiteAddress,
+				CustomerID = B.intCustomerID,
+				SiteDescription = B.strDescription,
+				SiteLocation = B.strLocation,
+				SiteID = B.intSiteID,
+				RunOutDate = B.dtmRunOutDate,
+				ProductNo = F.vwitm_no,
+				ProductDesc = RTRIM(F.vwitm_desc),
+				ProductID = F.A4GLIdentity,
+				ClockNo = D.strClockNumber,
+				DispatchRequestedDate = A.dtmRequestedDate,
+				DispatchDate = A.dtmDispatchingDate,
+				MinimumQuantity = A.dblMinimumQuantity,
+				Price = A.dblPrice,
+				Total = A.dblTotal,
+				DriverName = RTRIM(G.vwsls_name),
+				DriverID = G.vwsls_slsmn_id,
+				NextDeliveryDegreeDay = B.intNextDeliveryDegreeDay,
+				isDispatched = A.ysnDispatched,
+				RouteName = E.strRouteId,
+				RouteID = E.intRouteId,
+				intConcurrencyId = 0
+				FROM tblTMDispatch A
+				INNER JOIN tblTMSite B ON A.intSiteID = B.intSiteID
+				INNER JOIN tblTMCustomer C ON B.intCustomerID = C.intCustomerID
+				INNER JOIN tblTMClock D ON D.intClockID = B.intClockID
+				INNER JOIN tblTMRoute E ON B.intRouteId = E.intRouteId
+				INNER JOIN vwlocmst K ON B.intLocationId = K.A4GLIdentity
+				INNER JOIN vwitmmst F ON CAST(F.A4GLIdentity AS INT) = B.intProduct 
+					AND K.vwloc_loc_no COLLATE Latin1_General_CI_AS = F.vwitm_loc_no COLLATE Latin1_General_CI_AS
+				INNER JOIN vwslsmst G ON CAST(G.A4GLIdentity AS INT) = A.intDriverID
+				INNER JOIN vwcusmst H ON H.A4GLIdentity = C.intCustomerNumber
+				WHERE B.ysnActive  = 1
+					AND H.vwcus_active_yn = ''Y''
+		
+		')
+	END
 END
+
+
 GO
 PRINT 'END OF CREATING vwDispatch VIEW'
 GO
