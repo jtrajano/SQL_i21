4dbffef74234b1d7540040b1460e404e0e7e518e
Melanie Ferrer <Mel.Ferrer@irely.com>
2015-12-09 15:51:34 +0800

    PAT-227

---
+++ i21Database/dbo/Views/vyuPATCancel.sql
@@ -0,0 +1,34 @@
+﻿CREATE VIEW [dbo].[vyuPATCancel]
+	AS 
+	SELECT CE.intCancelId,
+		   CE.dtmCancelDate,
+		   CE.strCancelNo,
+		   CE.strDescription,
+		   CE.intFromCustomerId,
+		   CE.intToCustomerId,
+		   CE.intFiscalYearId AS intFiscalYearSearch,
+		   CE.strCancelBy AS strCancelBySearch,
+		   CE.dblCancelByAmount,
+		   CE.dblCancelLessAmount,
+		   CE.intIncludeEquityReserve,
+		   CE.ysnPosted,
+		   CE.intConcurrencyId,
+		   CED.intCancelDetailId,
+		   CED.intFiscalYearId,
+		   FY.strFiscalYear,
+		   CED.intCustomerId,
+		   ENT.strName,
+		   RR.strRefundDescription,
+		   CED.dblQuantityAvailable,
+		   CED.strCancelBy,
+		   CED.dblCancelByPercentage,
+		   CED.dblQuantityCancelled
+	  FROM tblPATCancelEquity CE
+INNER JOIN tblPATCancelEquityDetail CED
+		ON CED.intCancelId = CE.intCancelId
+ LEFT JOIN tblGLFiscalYear FY
+		ON FY.intFiscalYearId = CED.intFiscalYearId
+INNER JOIN tblEntity ENT
+		ON ENT.intEntityId = CED.intCustomerId
+ LEFT JOIN tblPATRefundRate RR
+		ON RR.intRefundTypeId = CED.intRefundTypeId
