8a046753777299b5e419ddbf4160ddf316e71244
Arman Arenas <arman.arenas@irely.com>
2015-12-14 16:04:56 +0800

    PR-421 Added vyuPRUnpostedTransactions

---
+++ i21Database/dbo/Views/vyuPRUnpostedTransactions.sql
@@ -0,0 +1,9 @@
+﻿CREATE VIEW [dbo].[vyuPRUnpostedTransactions]
+AS 
+	SELECT
+		strPaycheckId	AS [strTransactionId]
+		,'Paycheck'		AS [strTransactionType]
+		,dtmPayDate		AS [dtmDate]
+	FROM tblPRPaycheck
+	WHERE ysnPosted = 0
+	  AND ysnVoid = 0
