d7956a4c6ea315ea6db1bbf752ac6432397673a1
kenneth lagunero <kenneth.lagunero@irely.com>
2015-12-16 12:04:14 +0800

    TM-1505

--- Integration/dbo/Stored Procedures/uspTMDeliveryFillReport.sql
+++ Integration/dbo/Stored Procedures/uspTMDeliveryFillReport.sql
@@ -151,7 +151,7 @@ BEGIN
 		FROM @temp_params where [fieldname] = 'strDriverId'
 		IF (ISNULL(@FromDriverID,'') <> '')
 		BEGIN
-			SET @whereClause = @whereClause + CASE WHEN RTRIM(@whereClause) = '' THEN ' WHERE ' ELSE ' AND ' END + ' (strDriverId ''' + @FromDriverID + ''' AND ''' + @ToDriverID + ''')'
+			SET @whereClause = @whereClause + CASE WHEN RTRIM(@whereClause) = '' THEN ' WHERE ' ELSE ' AND ' END + ' (strDriverId BETWEEN ''' + @FromDriverID + ''' AND ''' + @ToDriverID + ''')'
 		END
 		
 		
@@ -331,6 +331,7 @@ BEGIN
 									,(CASE WHEN dblCallEntryMinimumQuantity IS NULL THEN COALESCE(dblQuantity,1.00) ELSE dblCallEntryMinimumQuantity END)
 									,NULL,intSiteID))) AS dblProductCost
 		FROM #tmpDeliveryFill
+		OPTION (RECOMPILE)
 		')
 		
 	END
