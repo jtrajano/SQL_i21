52750494e2a140747721268304162a7b49fd00bf
Kelvin Quinto <kelvin.quinto@irely.com>
2015-12-21 16:53:40 +0800

    AR-2212

--- i21Database/dbo/Functions/fnARGetCustomerBudget.sql
+++ i21Database/dbo/Functions/fnARGetCustomerBudget.sql
@@ -7,7 +7,7 @@ RETURNS NUMERIC(18,6) AS
 BEGIN
 	DECLARE @customerBudget NUMERIC(18,6)
 
-	SELECT @customerBudget = dblMonthlyBudget 
+	SELECT @customerBudget = dblBudgetAmount 
 	FROM tblARCustomer C INNER JOIN tblARCustomerBudget CB 
 		ON C.intEntityCustomerId = CB.intEntityCustomerId
 	WHERE C.intEntityCustomerId = @entityCustomerId
