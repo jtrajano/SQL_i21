c35bddee07d291ce774e4fd10a0dfa9c52966386
Gopinath Nayak <gopinath.n@irely.com>
2015-12-22 19:30:30 +0530

    GRN-294

--- i21Database/dbo/Views/vyuGRGetStorageLocation.sql
+++ i21Database/dbo/Views/vyuGRGetStorageLocation.sql
@@ -5,6 +5,7 @@ SELECT Distinct
   ,Cs.intCompanyLocationId  
   ,Loc.strLocationName  
 FROM tblGRCustomerStorage Cs
-JOIN tblSMCompanyLocation Loc ON Loc.intCompanyLocationId =Cs.intCompanyLocationId 
-Where Cs.dblOpenBalance >0 AND ISNULL(Cs.strStorageType,'') <> 'ITR'
+JOIN tblSMCompanyLocation Loc ON Loc.intCompanyLocationId =Cs.intCompanyLocationId
+JOIN tblGRStorageType ST ON ST.intStorageScheduleTypeId=Cs.intStorageTypeId  
+Where Cs.dblOpenBalance >0 AND ISNULL(Cs.strStorageType,'') <> 'ITR' AND ST.ysnCustomerStorage=0
 
