03d782109b90bbd0aca895d59eb2c2e7af353d9c
Ruben  Escarcha <ruben.escarcha@irely.com>
2015-12-22 16:32:59 +0800

    AR-2220 Batch Posting Needs

---
+++ i21Database/dbo/Views/vyuARBatchPostingTransaction.sql
@@ -0,0 +1,48 @@
+﻿CREATE VIEW [dbo].[vyuARBatchPostingTransaction]
+AS
+SELECT
+	 GL.strBatchId
+	,GL.dtmDate
+	,GL.strTransactionType
+	,COUNT(AR.intTransactionId) dblEntriesCount
+	,SUM(AR.dblTotal) dblTotalAmount
+FROM
+	tblGLDetail GL
+INNER JOIN
+	(
+		SELECT
+			 strPostingType		= 'Invoice' 
+			,dblTotal			= dblInvoiceTotal
+			,intTransactionId	= intInvoiceId
+			,strTransactionId	= strInvoiceNumber
+			,intAccountId		= intAccountId
+		FROM
+			tblARInvoice
+		WHERE
+			ysnPosted = 1
+			
+		UNION
+		
+		SELECT
+			 strPostingType		= 'Receive Payments' 
+			,dblTotal			= dblAmountPaid
+			,intTransactionId	= intPaymentId
+			,strTransactionId	= strRecordNumber
+			,intAccountId		= intAccountId
+		FROM
+			tblARPayment
+		WHERE
+			ysnPosted = 1
+			
+	) AR
+		ON GL.intTransactionId = AR.intTransactionId
+		AND GL.strTransactionType IN ('Invoice','Receive Payments')
+		AND GL.strCode = 'AR'		
+		AND GL.strTransactionId = AR.strTransactionId
+		AND GL.intAccountId = AR.intAccountId
+		AND GL.strTransactionType = AR.strPostingType
+		AND GL.ysnIsUnposted = 0			
+GROUP BY
+	 GL.strBatchId
+	,GL.dtmDate
+	,GL.strTransactionType
