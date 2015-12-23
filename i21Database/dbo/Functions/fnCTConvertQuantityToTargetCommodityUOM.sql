4228afeaf154743d90353607db0d4c935dcf4f19
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2015-12-23 15:26:45 +0530

    RM-219

--- i21Database/dbo/Functions/fnCTConvertQuantityToTargetCommodityUOM.sql
+++ i21Database/dbo/Functions/fnCTConvertQuantityToTargetCommodityUOM.sql
@@ -8,8 +8,8 @@ RETURNS NUMERIC(38,20)
 AS 
 BEGIN 
 	DECLARE	@result AS NUMERIC(38,20),
-			@dblUnitQtyFrom AS NUMERIC(38,20),
-			@dblUnitQtyTo AS NUMERIC(38,20)
+			@dblUnitQtyFrom AS NUMERIC(18,6),
+			@dblUnitQtyTo AS NUMERIC(18,6)
 
 
 	SELECT	@dblUnitQtyFrom = CommodityUOM.dblUnitQty
@@ -33,7 +33,7 @@ BEGIN
 		CASE	WHEN @dblUnitQtyFrom = @dblUnitQtyTo THEN 
 					@dblQty
 				ELSE 
-					CASE	WHEN @dblUnitQtyTo <> 0 THEN (@dblQty * @dblUnitQtyFrom) / @dblUnitQtyTo							
+					CASE	WHEN @dblUnitQtyTo <> 0 THEN CAST((@dblQty * @dblUnitQtyFrom)AS NUMERIC(18,6)) / @dblUnitQtyTo							
 							ELSE NULL 
 					END
 		END 
