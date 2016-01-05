4252ce3608f7b2f1873b9031f2eb4ccf9d00cbe4
Kelvin Quinto <kelvin.quinto@irely.com>
2016-01-05 11:13:23 +0800

    AR-2175, AR-2111, AR-2256, AR-2255

--- i21Database/dbo/Views/vyuARGetRecipe.sql
+++ i21Database/dbo/Views/vyuARGetRecipe.sql
@@ -8,10 +8,9 @@ SELECT R.intRecipeId
 	 , UOM.strUnitMeasure
 	 , R.intLocationId	AS intCompanyLocationId
 	 , L.strLocationName
+	 , R.dblQuantity	AS dblOrigQuantity
 	 , R.dblQuantity
 	 , R.intVersionNo
-	 , R.intManufacturingCellId
-	 , MC.strCellName
 	 , R.intManufacturingProcessId
 	 , MP.strProcessName
 	 , R.intCustomerId	AS intEntityCustomerId
@@ -20,7 +19,6 @@ FROM tblMFRecipe R
 	LEFT JOIN tblICItem I ON R.intItemId = I.intItemId
 	LEFT JOIN vyuARItemUOM UOM ON R.[intItemUOMId] = UOM.intItemUOMId
 	LEFT JOIN tblSMCompanyLocation L ON R.intLocationId = L.intCompanyLocationId
-	LEFT JOIN tblMFManufacturingCell MC ON R.intManufacturingCellId = MC.intManufacturingCellId
 	LEFT JOIN tblMFManufacturingProcess MP ON R.intManufacturingProcessId = MP.intManufacturingProcessId
 	LEFT JOIN (vyuARCustomer C INNER JOIN tblEntity E ON C.intEntityCustomerId = E.intEntityId) ON R.intCustomerId = C.intEntityCustomerId
 WHERE R.ysnActive = 1
