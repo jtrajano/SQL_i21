73bdaeae400341c41a8895dfd01f1925c9f9c7c1
Premkumar S <prem.selladurai@irely.com>
2016-01-05 16:03:24 +0530

    MFG-1274

---
+++ i21Database/dbo/Tables/tblMFScheduledMaintenance.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE dbo.tblMFScheduledMaintenance (
+	intScheduledMaintenanceId INT IDENTITY(1, 1) NOT NULL
+	,intManufacturingCellId INT NULL
+	,dtmStartDate DATETIME NOT NULL
+	,dtmStartTime DATETIME NOT NULL
+	,dtmEndDate DATETIME NOT NULL
+	,dtmEndTime DATETIME NOT NULL
+	,strReason NVARCHAR(MAX) NULL
+	,intLocationId int Not NULL
+	,dtmCreated DATETIME NOT NULL
+	,intCreatedUserId INT NOT NULL
+	,dtmLastModified DATETIME NOT NULL
+	,intLastModifiedUserId INT NOT NULL
+	,intConcurrencyId INT
+	,CONSTRAINT PK_tblMFScheduledMaintenance_intScheduledMaintenanceId PRIMARY KEY (intScheduledMaintenanceId)
+	,CONSTRAINT [FK_tblMFScheduledMaintenance_tblMFManufacturingCellId_intManufacturingCellId] FOREIGN KEY (intManufacturingCellId) REFERENCES tblMFManufacturingCell(intManufacturingCellId)
+	)
