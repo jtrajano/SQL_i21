4b8ebdd2ab50f59a6b25b18f9442d00dd8f56d2b
Premkumar S <prem.selladurai@irely.com>
2016-01-05 19:06:53 +0530

    MFG-1279

--- i21Database/dbo/Tables/tblMFWorkOrderProducedLot.sql
+++ i21Database/dbo/Tables/tblMFWorkOrderProducedLot.sql
@@ -41,6 +41,7 @@
     [strParentLotNumber] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL,
 	intInputLotId int,
 	intInputStorageLocationId int,
+	ysnFeedProcessed bit,
     CONSTRAINT [PK_tblMFWorkOrderProducedLot_intWorkOrderProducedLotId] PRIMARY KEY ([intWorkOrderProducedLotId]),
 	CONSTRAINT [FK_tblMFWorkOrderProducedLot_tblMFWorkOrder_intWorkOrderId] FOREIGN KEY ([intWorkOrderId]) REFERENCES [tblMFWorkOrder]([intWorkOrderId]) ON DELETE CASCADE,
 	CONSTRAINT [FK_tblMFWorkOrderProducedLot_tblICItem_intItemId] FOREIGN KEY ([intItemId]) REFERENCES [tblICItem]([intItemId]),
