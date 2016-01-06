d8d798b6e6b5edc3f9a83ed713f7b083de517b83
Kelvin Quinto <kelvin.quinto@irely.com>
2016-01-06 14:04:08 +0800

    AR-2254

--- i21Database/dbo/Views/vyuARCustomerHistory.sql
+++ i21Database/dbo/Views/vyuARCustomerHistory.sql
@@ -1,78 +1,48 @@
 ﻿CREATE VIEW vyuARCustomerHistory
 AS 
-SELECT
-	ISNULL(SO.[dtmProcessDate], SO.[dtmDate])	AS [dtmDate]
-	,SO.[strSalesOrderNumber]					AS [strTransactionNumber]
-	,SO.[strTransactionType]					AS [strTransactionType]
-	,ISNULL(SO.[dblSalesOrderTotal], 0.00)		AS [dblTransactionTotal]
-	,ISNULL(SO.[dblPayment], 0.00)				AS [dblAmountPaid]
-	,ISNULL(SO.[dblAmountDue], 0.00)			AS [dblAmountDue]
-	,C.[intEntityCustomerId]					AS [intEntityCustomerId]
-	,C.[strCustomerNumber]						AS [strCustomerNumber]
-	,E.[strName]								AS [strCustomerName] 
-	,0                                          AS [ysnPaid]
-FROM
-	tblSOSalesOrder SO
-INNER JOIN
-	tblARCustomer C
-		ON SO.[intEntityCustomerId] = C.[intEntityCustomerId]
-INNER JOIN
-	tblEntity E
-		ON C.[intEntityCustomerId] = E.[intEntityId] 
+SELECT dtmDate				= ISNULL(SO.dtmProcessDate, SO.dtmDate)
+	 , strTransactionNumber	= SO.strSalesOrderNumber
+	 , strTransactionType	= SO.strTransactionType
+	 , dblTransactionTotal	= ISNULL(SO.dblSalesOrderTotal, 0.00)
+	 , dblAmountPaid		= ISNULL(SO.dblPayment, 0.00)
+	 , dblAmountDue			= ISNULL(SO.dblAmountDue, 0.00)
+	 , intEntityCustomerId	= C.intEntityCustomerId
+	 , strCustomerNumber	= C.strCustomerNumber
+	 , strCustomerName		= E.strName
+	 , ysnPaid				= 0
+FROM tblSOSalesOrder SO
+INNER JOIN tblARCustomer C ON SO.intEntityCustomerId = C.intEntityCustomerId
+INNER JOIN tblEntity E ON C.intEntityCustomerId = E.intEntityId
 	
 UNION	
 	
-SELECT
-	ISNULL(I.[dtmPostDate], I.[dtmDate])		AS [dtmDate]
-	,I.[strInvoiceNumber]						AS [strTransactionNumber]
-	,I.[strTransactionType]						AS [strTransactionType]
-	,ISNULL(I.[dblInvoiceTotal], 0.00)			AS [dblTransactionTotal]
-	,ISNULL(I.[dblPayment], 0.00)				AS [dblAmountPaid]
-	,ISNULL(I.[dblAmountDue], 0.00)				AS [dblAmountDue]
-	,C.[intEntityCustomerId]					AS [intEntityCustomerId]
-	,C.[strCustomerNumber]						AS [strCustomerNumber]  
-	,E.[strName]								AS [strCustomerName]
-	,I.[ysnPaid]                                AS [ysnPaid]	
-FROM
-	tblARInvoice I
-INNER JOIN
-	tblARCustomer C
-		ON I.[intEntityCustomerId] = C.[intEntityCustomerId] 
-INNER JOIN
-	tblEntity E
-		ON C.[intEntityCustomerId] = E.[intEntityId] 				
-	
+SELECT dtmDate				= ISNULL(I.dtmPostDate, I.dtmDate)
+	 , strTransactionNumber	= I.strInvoiceNumber
+	 , strTransactionType	= I.strTransactionType
+	 , dblTransactionTotal	= ISNULL(I.dblInvoiceTotal, 0.00)
+	 , dblAmountPaid		= ISNULL(I.dblPayment, 0.00)
+	 , dblAmountDue			= CASE WHEN I.strTransactionType <> 'Invoice' THEN ISNULL(I.dblAmountDue, 0.00) * -1 ELSE ISNULL(I.dblAmountDue, 0.00) END
+	 , intEntityCustomerId	= C.intEntityCustomerId
+	 , strCustomerNumber	= C.strCustomerNumber
+	 , strCustomerName		= E.strName
+	 , ysnPaid				= I.ysnPaid
+FROM tblARInvoice I
+INNER JOIN tblARCustomer C ON I.intEntityCustomerId = C.intEntityCustomerId 
+INNER JOIN tblEntity E ON C.intEntityCustomerId = E.intEntityId
 	
 UNION	
 	
-SELECT
-	 P.[dtmDatePaid]							AS [dtmDate]
-	,P.[strRecordNumber]						AS [strTransactionNumber]
-	,'Receive Payment'							AS [strTransactionType]
-	,ISNULL(PD.[dblTotal], 0.00)				AS [dblTransactionTotal] 
-	,ISNULL(P.[dblAmountPaid], 0.00)			AS [dblAmountPaid]
-	,ISNULL(P.[dblUnappliedAmount], 0.00)		AS [dblAmountDue]
-	,C.[intEntityCustomerId]					AS [intEntityCustomerId]
-	,C.[strCustomerNumber]						AS [strCustomerNumber]
-	,E.[strName]								AS [strCustomerName]
-	,0                                          AS [ysnPaid]
-FROM
-	tblARPayment P
-LEFT OUTER JOIN
-	(
-		SELECT
-			 [intPaymentId]
-			,SUM(dblInvoiceTotal)	AS [dblTotal]
-		FROM
-			tblARPaymentDetail
-		GROUP BY
-			[intPaymentId] 
-	) AS PD
-		ON P.[intPaymentId] = PD.[intPaymentId]
-INNER JOIN
-	tblARCustomer C
-		ON P.[intEntityCustomerId] = C.[intEntityCustomerId] 
-INNER JOIN
-	tblEntity E
-		ON C.[intEntityCustomerId] = E.[intEntityId] 		
-
+SELECT dtmDate				= P.dtmDatePaid
+	 , strTransactionNumber	= P.strRecordNumber
+	 , strTransactionType	= 'Receive Payment'
+	 , dblTransactionTotal	= ISNULL(PD.dblTotal, 0.00)
+	 , dblAmountPaid		= ISNULL(P.dblAmountPaid, 0.00)
+	 , dblAmountDue			= 0.00
+	 , intEntityCustomerId	= C.intEntityCustomerId
+	 , strCustomerNumber	= C.strCustomerNumber
+	 , strCustomerName		= E.strName
+	 , ysnPaid				= 0
+FROM tblARPayment P
+LEFT OUTER JOIN (SELECT intPaymentId ,SUM(dblInvoiceTotal)	AS dblTotal FROM tblARPaymentDetail GROUP BY intPaymentId) AS PD ON P.intPaymentId = PD.intPaymentId
+INNER JOIN tblARCustomer C ON P.intEntityCustomerId = C.intEntityCustomerId 
+INNER JOIN tblEntity E ON C.intEntityCustomerId = E.intEntityId
