823022e6445704a11bab1deac8dee84b50b45e91
jeffrey trajano <jeffrey.trajano@irely.com>
2016-01-06 15:21:11 +0800

    GL-2447

--- i21Database/dbo/Index/IX_tblGLDetail_strTransactionId.sql
+++ i21Database/dbo/Index/IX_tblGLDetail_strTransactionId.sql
@@ -1,4 +1,12 @@
-﻿CREATE NONCLUSTERED INDEX IX_tblGLDetail_strTransactionId
-ON [dbo].[tblGLDetail] ([strTransactionId])
-INCLUDE ([dtmDate],[strBatchId],[intAccountId],[dblDebit],[dblCredit],[intJournalLineNo],[ysnIsUnposted])
+﻿﻿CREATE NONCLUSTERED INDEX [IX_tblGLDetail_strTransactionId] ON [dbo].[tblGLDetail]
+(
+	[strTransactionId] ASC
+)
+INCLUDE ( 	[dtmDate],
+	[strBatchId],
+	[intAccountId],
+	[dblDebit],
+	[dblCredit],
+	[intJournalLineNo],
+	[ysnIsUnposted]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
 GO
