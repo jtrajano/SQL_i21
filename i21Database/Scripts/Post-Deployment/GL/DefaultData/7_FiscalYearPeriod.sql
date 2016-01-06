93d0e6b02b9f31acdd688a1ebe31107d8a4b7232
jeffrey trajano <jeffrey.trajano@irely.com>
2016-01-06 15:11:12 +0800

    GL-2446 Invalid Column Name ysnAPOpen

--- i21Database/Scripts/Post-Deployment/GL/DefaultData/7_FiscalYearPeriod.sql
+++ i21Database/Scripts/Post-Deployment/GL/DefaultData/7_FiscalYearPeriod.sql
@@ -1,4 +1,8 @@
 ﻿--UPDATES THE STATUS OF AR/AP/INVENTORY MODULE IF IT WILL BE OPEN FOR POSTING IN THE FISCAL YEAR SCREEN
 GO
-	 UPDATE tblGLFiscalYearPeriod SET  ysnAPOpen = 0,	ysnAROpen = 0 ,ysnINVOpen = 0, ysnPROpen = 0 WHERE ysnOpen = 0
+	 UPDATE tblGLFiscalYearPeriod SET  ysnAPOpen = 1 WHERE ysnAPOpen IS NULL
+	 UPDATE tblGLFiscalYearPeriod SET  ysnAROpen = 1 WHERE ysnAROpen IS NULL
+	 UPDATE tblGLFiscalYearPeriod SET  ysnINVOpen = 1 WHERE ysnINVOpen IS NULL
+	 UPDATE tblGLFiscalYearPeriod SET  ysnPROpen = 1 WHERE ysnPROpen IS NULL
+	 UPDATE tblGLFiscalYearPeriod SET  ysnCMOpen = 1 WHERE ysnCMOpen IS NULL
 GO
