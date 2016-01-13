04c76dd3c62af61e65ce008a766ebf2d548e1bb1
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2016-01-13 16:49:15 +0530

    RM-239

--- i21Database/dbo/Stored Procedures/uspRKM2MGetClosingPrice.sql
+++ i21Database/dbo/Stored Procedures/uspRKM2MGetClosingPrice.sql
@@ -1,4 +1,4 @@
-﻿CREATE PROC uspRKM2MGetClosingPrice   
+﻿create PROC uspRKM2MGetClosingPrice   
    @dtmPriceDate DateTime ,
   @intCommodityId int
 AS  
@@ -8,6 +8,7 @@ SELECT ROW_NUMBER() OVER(ORDER BY f.intFutureMarketId DESC) AS intRowNum,f.intFu
 dbo.fnRKGetLatestClosingPrice(f.intFutureMarketId,fm.intFutureMonthId,@dtmPriceDate) as dblClosingPrice,0 as intConcurrencyId  
 FROM tblRKFutureMarket f  
 JOIN tblRKFuturesMonth fm on f.intFutureMarketId = fm.intFutureMarketId 
-join tblRKCommodityMarketMapping mm on fm.intFutureMarketId=mm.intFutureMarketId where mm.intCommodityId=@intCommodityId
+join tblRKCommodityMarketMapping mm on fm.intFutureMarketId=mm.intFutureMarketId where mm.intCommodityId  =
+case when @intCommodityId = 0 then mm.intCommodityId else @intCommodityId end 
 )t where dblClosingPrice > 0
 order by strFutMarketName,convert(datetime,'01 '+strFutureMonth) 
