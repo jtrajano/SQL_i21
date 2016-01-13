7a45682deb41628c811991ae8e07128fc37a2f3b
Mon Gonzales <mon.gonzales@irely.com>
2016-01-13 19:16:55 +0800

    EM-842

--- i21Database/dbo/Tables/tblARCustomerBudget.sql
+++ i21Database/dbo/Tables/tblARCustomerBudget.sql
@@ -4,6 +4,9 @@
     [intEntityCustomerId]		INT				NOT NULL,
 	[dblBudgetAmount]			NUMERIC (18, 6) DEFAULT ((0)) NOT NULL,
 	[dtmBudgetDate]				DATETIME		NULL,    
+	[ysnForgiven]				BIT NULL DEFAULT(0),
+	[ysnCalculated]				BIT NULL DEFAULT(0),
+
     [intConcurrencyId]			INT				NOT NULL,
     CONSTRAINT [PK_tblARCustomerBudget] PRIMARY KEY CLUSTERED ([intCustomerBudgetId] ASC),
 	CONSTRAINT [FK_tblARCustomerBudget_tblARCustomer] FOREIGN KEY ([intEntityCustomerId]) REFERENCES [dbo].[tblARCustomer] ([intEntityCustomerId]) ON DELETE CASCADE
