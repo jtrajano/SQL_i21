6acb9a066c344ea66fc034ef1fe80b7b6960a2b5
Mon Gonzales <mon.gonzales@irely.com>
2016-01-14 16:30:20 +0800

    EM-806

--- i21Database/Scripts/Post-Deployment/EM/020_DefaultDataForEntityImportSchemaCSV.sql
+++ i21Database/Scripts/Post-Deployment/EM/020_DefaultDataForEntityImportSchemaCSV.sql
@@ -11,15 +11,18 @@ UNION
 SELECT 1003, 'tblEntity', 'strMobile', 'mobile'
 
 UNION
-SELECT 1101, 'tblEntity.tblEntityToContact.tblEntity', 'strName', 'con_name'
+SELECT 1101, 'tblEntity.tblEntityToContact.tblEntityContact', 'strName', 'con_name'
 UNION
-SELECT 1102, 'tblEntity.tblEntityToContact.tblEntity', 'strName', 'con_email'
+SELECT 1102, 'tblEntity.tblEntityToContact.tblEntityContact', 'strEmail', 'con_email'
 UNION
-SELECT 1103, 'tblEntity.tblEntityToContact.tblEntity', 'strName', 'con_mobile'
+SELECT 1103, 'tblEntity.tblEntityToContact.tblEntityContact', 'strMobile', 'con_mobile'
+
 
 UNION
 SELECT 1201, 'tblEntity.tblEntityLocations', 'strLocationName', 'loc_name'
 UNION
 SELECT 1202, 'tblEntity.tblEntityLocations', 'strAddress', 'loc_address'
+UNION
+SELECT 1203, 'tblEntity.tblEntityLocations', 'strTermCodeId', 'loc_termsId'
 
 SET IDENTITY_INSERT tblEntityImportSchemaCSV OFF
