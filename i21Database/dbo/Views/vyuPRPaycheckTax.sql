093fb908c570ebf0734737aa34d90c6739cdd761
Arman Arenas <arman.arenas@irely.com>
2016-01-15 06:26:26 +0800

    PR-487 complete revision of Paycheck Tax YTD calculation

--- i21Database/dbo/Views/vyuPRPaycheckTax.sql
+++ i21Database/dbo/Views/vyuPRPaycheckTax.sql
@@ -1,32 +1,39 @@
-﻿CREATE VIEW [dbo].[vyuPRPaycheckTax]   
+﻿CREATE VIEW [dbo].[vyuPRPaycheckTax] 
 AS
-
-SELECT DISTINCT 
-tblPRPaycheck.intPaycheckId 
-, ISNULL(tblPRPaycheckTax.strTax,'') AS strTaxId
-, ISNULL(tblPRPaycheckTax.dblTotal,0) AS dblTotal 
-, SUM(ISNULL(tblTaxSum.dblTotal,0)) AS dblTotalYTD 
-FROM (SELECT tblPRPaycheckTax.intPaycheckId, 
-			 tblPRPaycheckTax.intTypeTaxId, 
-			 tblPRTypeTax.strTax,
-			 tblPRPaycheckTax.dblAmount,
-			 tblPRPaycheckTax.dblTotal, 
-			 tblPRPaycheckTax.strPaidBy FROM tblPRPaycheckTax 
-		LEFT JOIN tblPRTypeTax ON tblPRPaycheckTax.intTypeTaxId = tblPRTypeTax.intTypeTaxId) tblPRPaycheckTax
-LEFT JOIN (SELECT tblPRPaycheck.[intEntityEmployeeId]
-		, tblPRPaycheckTax.intPaycheckId
-		, tblPRPaycheckTax.intTypeTaxId 
-		, dblTotal 
-		, dtmPayDate 
-		FROM tblPRPaycheckTax 
-		LEFT JOIN tblPRPaycheck ON tblPRPaycheckTax.intPaycheckId = tblPRPaycheck.intPaycheckId 
-		WHERE ysnPosted = 1 AND ysnVoid = 0
-) tblTaxSum ON tblPRPaycheckTax.intTypeTaxId = tblTaxSum.intTypeTaxId 
-LEFT JOIN tblPRPaycheck ON tblPRPaycheckTax.intPaycheckId = tblPRPaycheck.intPaycheckId 
-WHERE CAST(FLOOR(CAST(tblTaxSum.dtmPayDate AS FLOAT)) AS DATETIME) 
-		BETWEEN CAST(('1/1/' + CAST(YEAR(tblPRPaycheck.dtmPayDate) AS NVARCHAR(4))) AS DATETIME) 
-			AND CAST(FLOOR(CAST(tblPRPaycheck.dtmPayDate AS FLOAT)) AS DATETIME) 
-		AND strPaidBy = 'Employee'   
-GROUP BY tblPRPaycheck.intPaycheckId 
-, tblPRPaycheckTax.strTax
-, tblPRPaycheckTax.dblTotal 
+SELECT 
+	PCX.intPaycheckId, 
+	PCX.intEntityEmployeeId,
+	PCX.dtmPayDate,
+	strTaxId = (SELECT TOP 1 strTax FROM tblPRTypeTax WHERE intTypeTaxId = PCX.intTypeTaxId),
+    PCX.intTypeTaxId,
+	PCX.dblTotal,
+    dblTotalYTD = (SELECT SUM (dblTotal) FROM 
+						(SELECT PC2.intPaycheckId, 
+								PC2.intEntityEmployeeId, 
+								PC2.dtmPayDate, 
+								PCT2.intTypeTaxId, 
+								PCT2.dblTotal 
+								FROM tblPRPaycheckTax PCT2 
+									RIGHT JOIN tblPRPaycheck PC2 
+									ON PC2.intPaycheckId = PCT2.intPaycheckId
+									AND PCT2.strPaidBy = 'Employee') PCX2
+							WHERE PCX2.dtmPayDate <= PCX.dtmPayDate 
+								AND YEAR(PCX2.dtmPayDate) = YEAR(PCX.dtmPayDate)
+								AND PCX2.intEntityEmployeeId = PCX.intEntityEmployeeId
+								AND PCX2.intTypeTaxId = PCX.intTypeTaxId)
+ FROM 
+	(SELECT PC1.intPaycheckId, 
+			PC1.intEntityEmployeeId, 
+			PC1.dtmPayDate, 
+			PCT1.intTypeTaxId, 
+			PCT1.dblTotal 
+		FROM tblPRPaycheckTax PCT1
+		RIGHT JOIN tblPRPaycheck PC1 
+		ON PC1.intPaycheckId = PCT1.intPaycheckId
+		AND PCT1.strPaidBy = 'Employee') PCX
+ GROUP BY 
+	 PCX.intPaycheckId, 
+	 PCX.intEntityEmployeeId, 
+	 PCX.dtmPayDate, 
+	 PCX.intTypeTaxId, 
+	 PCX.dblTotal
