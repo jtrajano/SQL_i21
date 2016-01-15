ef123f9908d0b13c3c189f1f274b2cc7d12039ed
von.pasion <von.pasion@irely.com>
2016-01-15 15:11:11 +0800

    FRM-2941

--- i21Database/Scripts/Post-Deployment/SM/DefaultData/12_ReminderList.sql
+++ i21Database/Scripts/Post-Deployment/SM/DefaultData/12_ReminderList.sql
@@ -1,122 +1,158 @@
 ﻿GO
 
 	IF NOT EXISTS (SELECT 1 FROM [dbo].[tblSMReminderList] WHERE [strReminder] = N'Process' AND [strType] = N'Invoice')
-	BEGIN
-		INSERT INTO [dbo].[tblSMReminderList] ([strReminder], [strType], [strMessage], [strQuery], [strNamespace], [intSort])
-		SELECT [strReminder]        =        N'Process',
-			   [strType]        	=        N'Invoice',
-			   [strMessage]			=        N'{0} {1} {2} unprocessed.',
-			   [strQuery]  			=        N'SELECT intRecurringId ' +
-											  'FROM tblSMRecurringTransaction WHERE strTransactionType = ''Invoice'' ' +
-											  'AND GETDATE() >= dtmNextProcess AND dtmNextProcess >= dtmStartDate AND dtmNextProcess <= dtmEndDate AND ysnActive = 1 ' +
-											  'AND intEntityId = {0} ' +
-											  'UNION ' +
-											  'SELECT intRecurringId ' +
-											  'FROM tblSMRecurringTransaction WHERE strTransactionType = ''Invoice'' ' +
-											  'AND ysnActive = 1 ' +
-											  'AND intEntityId = {0} ' +
-											  'AND DATEDIFF(DAY, GETDATE(), dtmNextProcess) > 0 ' +
-											  'AND DATEDIFF(DAY, GETDATE(), DATEADD(DAY, intWarningDays * -1 , dtmNextProcess)) <= 0 ',
-			   [strNamespace]       =        N'i21.view.RecurringTransaction', 
-			   [intSort]            =        1
+		BEGIN
+			INSERT INTO [dbo].[tblSMReminderList] ([strReminder], [strType], [strMessage], [strQuery], [strNamespace], [intSort])
+			SELECT [strReminder]        =        N'Process',
+				   [strType]        	=        N'Invoice',
+				   [strMessage]			=        N'{0} {1} {2} unprocessed.',
+				   [strQuery]  			=        N'SELECT intRecurringId ' +
+												  'FROM tblSMRecurringTransaction WHERE strTransactionType = ''Invoice'' ' +
+												  'AND GETDATE() >= dtmNextProcess AND dtmNextProcess >= dtmStartDate AND dtmNextProcess <= dtmEndDate AND ysnActive = 1 ' +
+												  'AND intEntityId = {0} ' +
+												  'UNION ' +
+												  'SELECT intRecurringId ' +
+												  'FROM tblSMRecurringTransaction WHERE strTransactionType = ''Invoice'' ' +
+												  'AND ysnActive = 1 ' +
+												  'AND intEntityId = {0} ' +
+												  'AND DATEDIFF(DAY, GETDATE(), dtmNextProcess) > 0 ' +
+												  'AND DATEDIFF(DAY, GETDATE(), DATEADD(DAY, intWarningDays * -1 , dtmNextProcess)) <= 0 ',
+				   [strNamespace]       =        N'i21.view.RecurringTransaction', 
+				   [intSort]            =        1
 
-	END
+		END
+	ELSE
+		BEGIN
+			UPDATE [dbo].[tblSMReminderList]
+			SET	[strMessage] = N'{0} {1} {2} unprocessed.'
+			WHERE [strReminder] = N'Process' AND [strType] = N'Invoice' 
+		END
 
 	IF NOT EXISTS (SELECT 1 FROM [dbo].[tblSMReminderList] WHERE [strReminder] = N'Process' AND [strType] = N'General Journal')
-	BEGIN
-		INSERT INTO [dbo].[tblSMReminderList] ([strReminder], [strType], [strMessage], [strQuery], [strNamespace], [intSort])
-		SELECT [strReminder]        =        N'Process',
-			   [strType]        	=        N'General Journal',
-			   [strMessage]			=        N'{0} {1} {2} unprocessed.',
-			   [strQuery]  			=        N'SELECT intRecurringId ' +
-											  'FROM tblSMRecurringTransaction WHERE strTransactionType = ''General Journal'' ' +
-											  'AND GETDATE() >= dtmNextProcess AND dtmNextProcess >= dtmStartDate AND dtmNextProcess <= dtmEndDate AND ysnActive = 1 ' +
-											  'AND intEntityId = {0} ' +
-											  'UNION ' +
-											  'SELECT intRecurringId ' +
-											  'FROM tblSMRecurringTransaction WHERE strTransactionType = ''General Journal'' ' +
-											  'AND ysnActive = 1 ' +
-											  'AND intEntityId = {0} ' +
-											  'AND DATEDIFF(DAY, GETDATE(), dtmNextProcess) > 0 ' +
-											  'AND DATEDIFF(DAY, GETDATE(), DATEADD(DAY, intWarningDays * -1 , dtmNextProcess)) <= 0 ',
-			   [strNamespace]       =        N'i21.view.RecurringTransaction', 
-			   [intSort]            =        2
+		BEGIN
+			INSERT INTO [dbo].[tblSMReminderList] ([strReminder], [strType], [strMessage], [strQuery], [strNamespace], [intSort])
+			SELECT [strReminder]        =        N'Process',
+				   [strType]        	=        N'General Journal',
+				   [strMessage]			=        N'{0} {1} {2} unprocessed.',
+				   [strQuery]  			=        N'SELECT intRecurringId ' +
+												  'FROM tblSMRecurringTransaction WHERE strTransactionType = ''General Journal'' ' +
+												  'AND GETDATE() >= dtmNextProcess AND dtmNextProcess >= dtmStartDate AND dtmNextProcess <= dtmEndDate AND ysnActive = 1 ' +
+												  'AND intEntityId = {0} ' +
+												  'UNION ' +
+												  'SELECT intRecurringId ' +
+												  'FROM tblSMRecurringTransaction WHERE strTransactionType = ''General Journal'' ' +
+												  'AND ysnActive = 1 ' +
+												  'AND intEntityId = {0} ' +
+												  'AND DATEDIFF(DAY, GETDATE(), dtmNextProcess) > 0 ' +
+												  'AND DATEDIFF(DAY, GETDATE(), DATEADD(DAY, intWarningDays * -1 , dtmNextProcess)) <= 0 ',
+				   [strNamespace]       =        N'i21.view.RecurringTransaction', 
+				   [intSort]            =        2
 
-	END
+		END
+	ELSE
+		BEGIN
+			UPDATE [dbo].[tblSMReminderList]
+			SET	[strMessage] = N'{0} {1} {2} unprocessed.'
+			WHERE [strReminder] = N'Process' AND [strType] = N'General Journal' 
+		END
 
 	IF NOT EXISTS (SELECT 1 FROM [dbo].[tblSMReminderList] WHERE [strReminder] = N'Process' AND [strType] = N'Voucher')
-	BEGIN
-		INSERT INTO [dbo].[tblSMReminderList] ([strReminder], [strType], [strMessage], [strQuery], [strNamespace], [intSort])
-		SELECT [strReminder]        =        N'Process',
-			   [strType]        	=        N'Voucher',
-			   [strMessage]			=        N'{0} {1} {2} unprocessed.',
-			   [strQuery]  			=        N'SELECT intRecurringId ' +
-											  'FROM tblSMRecurringTransaction WHERE strTransactionType = ''Voucher'' ' +
-											  'AND GETDATE() >= dtmNextProcess AND dtmNextProcess >= dtmStartDate AND dtmNextProcess <= dtmEndDate AND ysnActive = 1 ' +
-											  'AND intEntityId = {0} ' +
-											  'UNION ' +
-											  'SELECT intRecurringId ' +
-											  'FROM tblSMRecurringTransaction WHERE strTransactionType = ''Voucher'' ' +
-											  'AND ysnActive = 1 ' +
-											  'AND intEntityId = {0} ' +
-											  'AND DATEDIFF(DAY, GETDATE(), dtmNextProcess) > 0 ' +
-											  'AND DATEDIFF(DAY, GETDATE(), DATEADD(DAY, intWarningDays * -1 , dtmNextProcess)) <= 0 ',
-			   [strNamespace]       =        N'i21.view.RecurringTransaction', 
-			   [intSort]            =        3
-	END
+		BEGIN
+			INSERT INTO [dbo].[tblSMReminderList] ([strReminder], [strType], [strMessage], [strQuery], [strNamespace], [intSort])
+			SELECT [strReminder]        =        N'Process',
+				   [strType]        	=        N'Voucher',
+				   [strMessage]			=        N'{0} {1} {2} unprocessed.',
+				   [strQuery]  			=        N'SELECT intRecurringId ' +
+												  'FROM tblSMRecurringTransaction WHERE strTransactionType = ''Voucher'' ' +
+												  'AND GETDATE() >= dtmNextProcess AND dtmNextProcess >= dtmStartDate AND dtmNextProcess <= dtmEndDate AND ysnActive = 1 ' +
+												  'AND intEntityId = {0} ' +
+												  'UNION ' +
+												  'SELECT intRecurringId ' +
+												  'FROM tblSMRecurringTransaction WHERE strTransactionType = ''Voucher'' ' +
+												  'AND ysnActive = 1 ' +
+												  'AND intEntityId = {0} ' +
+												  'AND DATEDIFF(DAY, GETDATE(), dtmNextProcess) > 0 ' +
+												  'AND DATEDIFF(DAY, GETDATE(), DATEADD(DAY, intWarningDays * -1 , dtmNextProcess)) <= 0 ',
+				   [strNamespace]       =        N'i21.view.RecurringTransaction', 
+				   [intSort]            =        3
+		END
+	ELSE
+		BEGIN
+			UPDATE [dbo].[tblSMReminderList]
+			SET	[strMessage] = N'{0} {1} {2} unprocessed.'
+			WHERE [strReminder] = N'Process' AND [strType] = N'Voucher' 
+		END
 
 	IF NOT EXISTS (SELECT 1 FROM [dbo].[tblSMReminderList] WHERE [strReminder] = N'Process' AND [strType] = N'Purchase Order')
-	BEGIN
-		INSERT INTO [dbo].[tblSMReminderList] ([strReminder], [strType], [strMessage], [strQuery], [strNamespace], [intSort])
-		SELECT [strReminder]        =        N'Process',
-			   [strType]        	=        N'Purchase Order',
-			   [strMessage]			=        N'{0} {1} {2} unprocessed.',
-			   [strQuery]  			=        N'SELECT intRecurringId ' +
-											  'FROM tblSMRecurringTransaction WHERE strTransactionType = ''Purchase Order'' ' +
-											  'AND GETDATE() >= dtmNextProcess AND dtmNextProcess >= dtmStartDate AND dtmNextProcess <= dtmEndDate AND ysnActive = 1 ' +
-											  'AND intEntityId = {0} ' +
-											  'UNION ' +
-											  'SELECT intRecurringId ' +
-											  'FROM tblSMRecurringTransaction WHERE strTransactionType = ''Purchase Order'' ' +
-											  'AND ysnActive = 1 ' +
-											  'AND intEntityId = {0} ' +
-											  'AND DATEDIFF(DAY, GETDATE(), dtmNextProcess) > 0 ' +
-											  'AND DATEDIFF(DAY, GETDATE(), DATEADD(DAY, intWarningDays * -1 , dtmNextProcess)) <= 0 ',
-			   [strNamespace]       =        N'i21.view.RecurringTransaction', 
-			   [intSort]            =        4
-	END
+		BEGIN
+			INSERT INTO [dbo].[tblSMReminderList] ([strReminder], [strType], [strMessage], [strQuery], [strNamespace], [intSort])
+			SELECT [strReminder]        =        N'Process',
+				   [strType]        	=        N'Purchase Order',
+				   [strMessage]			=        N'{0} {1} {2} unprocessed.',
+				   [strQuery]  			=        N'SELECT intRecurringId ' +
+												  'FROM tblSMRecurringTransaction WHERE strTransactionType = ''Purchase Order'' ' +
+												  'AND GETDATE() >= dtmNextProcess AND dtmNextProcess >= dtmStartDate AND dtmNextProcess <= dtmEndDate AND ysnActive = 1 ' +
+												  'AND intEntityId = {0} ' +
+												  'UNION ' +
+												  'SELECT intRecurringId ' +
+												  'FROM tblSMRecurringTransaction WHERE strTransactionType = ''Purchase Order'' ' +
+												  'AND ysnActive = 1 ' +
+												  'AND intEntityId = {0} ' +
+												  'AND DATEDIFF(DAY, GETDATE(), dtmNextProcess) > 0 ' +
+												  'AND DATEDIFF(DAY, GETDATE(), DATEADD(DAY, intWarningDays * -1 , dtmNextProcess)) <= 0 ',
+				   [strNamespace]       =        N'i21.view.RecurringTransaction', 
+				   [intSort]            =        4
+		END
+	ELSE
+		BEGIN
+			UPDATE [dbo].[tblSMReminderList]
+			SET	[strMessage] = N'{0} {1} {2} unprocessed.'
+			WHERE [strReminder] = N'Process' AND [strType] = N'Purchase Order' 
+		END
 	
 	IF NOT EXISTS (SELECT 1 FROM [dbo].[tblSMReminderList] WHERE [strReminder] = N'Process' AND [strType] = N'Bill Template')
-	BEGIN
-		INSERT INTO [dbo].[tblSMReminderList] ([strReminder], [strType], [strMessage], [strQuery], [strNamespace], [intSort])
-		SELECT [strReminder]        =        N'Process',
-			   [strType]        	=        N'Bill Template',
-			   [strMessage]			=        N'{0} {1} {2} unprocessed.',
-			   [strQuery]  			=        N'SELECT intRecurringId ' +
-											  'FROM tblSMRecurringTransaction WHERE strTransactionType = ''Bill Template'' ' +
-											  'AND GETDATE() >= dtmNextProcess AND dtmNextProcess >= dtmStartDate AND dtmNextProcess <= dtmEndDate AND ysnActive = 1 ' +
-											  'AND intEntityId = {0} ' +
-											  'UNION ' +
-											  'SELECT intRecurringId ' +
-											  'FROM tblSMRecurringTransaction WHERE strTransactionType = ''Bill Template'' ' +
-											  'AND ysnActive = 1 ' +
-											  'AND intEntityId = {0} ' +
-											  'AND DATEDIFF(DAY, GETDATE(), dtmNextProcess) > 0 ' +
-											  'AND DATEDIFF(DAY, GETDATE(), DATEADD(DAY, intWarningDays * -1 , dtmNextProcess)) <= 0 ',
-			   [strNamespace]       =        N'i21.view.RecurringTransaction', 
-			   [intSort]            =        5
-	END
+		BEGIN
+			INSERT INTO [dbo].[tblSMReminderList] ([strReminder], [strType], [strMessage], [strQuery], [strNamespace], [intSort])
+			SELECT [strReminder]        =        N'Process',
+				   [strType]        	=        N'Bill Template',
+				   [strMessage]			=        N'{0} {1} {2} unprocessed.',
+				   [strQuery]  			=        N'SELECT intRecurringId ' +
+												  'FROM tblSMRecurringTransaction WHERE strTransactionType = ''Bill Template'' ' +
+												  'AND GETDATE() >= dtmNextProcess AND dtmNextProcess >= dtmStartDate AND dtmNextProcess <= dtmEndDate AND ysnActive = 1 ' +
+												  'AND intEntityId = {0} ' +
+												  'UNION ' +
+												  'SELECT intRecurringId ' +
+												  'FROM tblSMRecurringTransaction WHERE strTransactionType = ''Bill Template'' ' +
+												  'AND ysnActive = 1 ' +
+												  'AND intEntityId = {0} ' +
+												  'AND DATEDIFF(DAY, GETDATE(), dtmNextProcess) > 0 ' +
+												  'AND DATEDIFF(DAY, GETDATE(), DATEADD(DAY, intWarningDays * -1 , dtmNextProcess)) <= 0 ',
+				   [strNamespace]       =        N'i21.view.RecurringTransaction', 
+				   [intSort]            =        5
+		END
+	ELSE
+		BEGIN
+			UPDATE [dbo].[tblSMReminderList]
+			SET	[strMessage] = N'{0} {1} {2} unprocessed.'
+			WHERE [strReminder] = N'Process' AND [strType] = N'Bill Template' 
+		END
 
 	IF NOT EXISTS (SELECT 1 FROM [dbo].[tblSMReminderList] WHERE [strReminder] = N'Approve' AND [strType] = N'Voucher')
-	BEGIN
-		INSERT INTO [dbo].[tblSMReminderList] ([strReminder], [strType], [strMessage], [strQuery], [strNamespace], [intSort])
-		SELECT [strReminder]        =        N'Approve',
-			   [strType]        	=        N'Voucher',
-			   [strMessage]			=        N'{0} {1} {2} unapproved.',
-			   [strQuery]  			=        N'SELECT * FROM vyuAPBillForApproval WHERE intEntityApproverId = {0} AND ysnApproved = 0',
-			   [strNamespace]       =        N'AccountsPayable.view.VendorExpenseApproval', 
-			   [intSort]            =        6
-	END
+		BEGIN
+			INSERT INTO [dbo].[tblSMReminderList] ([strReminder], [strType], [strMessage], [strQuery], [strNamespace], [intSort])
+			SELECT [strReminder]        =        N'Approve',
+				   [strType]        	=        N'Voucher',
+				   [strMessage]			=        N'{0} {1} {2} unapproved.',
+				   [strQuery]  			=        N'SELECT * FROM vyuAPBillForApproval WHERE intEntityApproverId = {0} AND ysnApproved = 0',
+				   [strNamespace]       =        N'AccountsPayable.view.VendorExpenseApproval', 
+				   [intSort]            =        6
+		END
+	ELSE
+		BEGIN
+			UPDATE [dbo].[tblSMReminderList]
+			SET	[strMessage] = N'{0} {1} {2} unapproved.'
+			WHERE [strReminder] = N'Approve' AND [strType] = N'Voucher' 
+		END
 
 	IF NOT EXISTS (SELECT 1 FROM [dbo].[tblSMReminderList] WHERE [strReminder] = N'Update' AND [strType] = N'Invoice')
 	BEGIN
@@ -127,8 +163,6 @@
 				[strQuery]  		=        N'SELECT intEntityCustomerId, DATEADD(MONTH, 1, MAX(dtmBudgetDate)) AS dtmBudgetEndDate FROM tblARCustomerBudget GROUP BY intEntityCustomerId HAVING GETDATE() > DATEADD(MONTH, 1, MAX(dtmBudgetDate))',
 				[strNamespace]      =        N'AccountsReceivable.view.Invoice', 
 				[intSort]           =        7
-	END
-	
-	
+	END	
 
 GO
