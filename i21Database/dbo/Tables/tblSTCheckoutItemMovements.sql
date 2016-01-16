cb86e889e54b559c0095b14d988677e5e8f7addc
Maheshwari <mgawade@BLRMAHEWIN10.irely.net>
2016-01-16 23:22:38 +0530

    ST - 190 - Added Item Movements table for Checkouts

---
+++ i21Database/dbo/Tables/tblSTCheckoutItemMovements.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[tblSTCheckoutItemMovements]
+(
+	[intItemMovementId] INT NOT NULL IDENTITY,
+	[intCheckoutId] INT,
+    [intItemUPCId] INT NULL, 
+    [strDescription] NVARCHAR(MAX) NULL, 
+    [intVendorId] INT NULL, 
+    [intQtySold] INT NULL, 
+    [dblCurrentPrice] DECIMAL(18, 6) NULL, 
+    [dblTotalSales] DECIMAL(18, 6) NULL, 
+    [intConcurrencyId] INT NULL, 
+	CONSTRAINT [PK_tblSTCheckoutItemMovements_intItemMovementId] PRIMARY KEY ([intItemMovementId]), 
+	CONSTRAINT [FK_tblSTCheckoutItemMovements_tblSTCheckoutHeader] FOREIGN KEY ([intCheckoutId]) REFERENCES [tblSTCheckoutHeader]([intCheckoutId]), 
+    CONSTRAINT [FK_tblSTCheckoutItemMovements_tblICItemUOM] FOREIGN KEY ([intItemUPCId]) REFERENCES [tblICItemUOM]([intItemUOMId]) 
+
+)
