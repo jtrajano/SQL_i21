a6dc4322147ce7be7f4542d3a998064456e83fef
Jayson Ramos <jayson.ramos@iRely.com>
2016-01-18 16:34:26 +0800

    HD-1025

---
+++ i21Database/dbo/Views/vyuHDModule.sql
@@ -0,0 +1,17 @@
+﻿CREATE VIEW [dbo].[vyuHDModule]
+	AS
+		select
+			intModuleId = intModuleId
+			,intSMModuleId = intSMModuleId
+			,intSort = intSort
+			,intTicketGroupId = intTicketGroupId
+			,intTicketProductId = intTicketProductId
+			,strSMModuleName = (select top 1 strModule from tblSMModule where intModuleId = intSMModuleId)
+			,strSMModuleApplicationName = (select top 1 strApplicationName from tblSMModule where intModuleId = intSMModuleId)
+			,strSMModuleAppCode = (select top 1 strAppCode from tblSMModule where intModuleId = intSMModuleId)
+			,strDescription = strDescription
+			,strGroup = (select top 1 strGroup from tblHDTicketGroup where intTicketGroupId = tblHDModule.intTicketGroupId)
+			,strModule = strModule
+			,ysnSupported = ysnSupported
+		from
+			tblHDModule
