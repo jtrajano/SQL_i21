426541c4ca6ff8ee4a58fbbfbeb00db80de0036e
Jayson Ramos <jayson.ramos@iRely.com>
2016-01-19 17:01:56 +0800

    HD-1044

---
+++ i21Database/dbo/Views/vyuHDEmailSetup.sql
@@ -0,0 +1,17 @@
+﻿CREATE VIEW [dbo].[vyuHDEmailSetup]
+	AS
+	select top 1
+		intEmailSetupId = tblSMCompanyPreference.intCompanyPreferenceId
+		,strFromEmail = tblSMCompanyPreference.strSMTPFromEmail
+		,strFromName = tblHDSetting.strFromName
+		,strSubjectPrefix = tblHDSetting.strSubjectPrefix
+		,strSMTPServer = tblSMCompanyPreference.strSMTPHost
+		,intSMTPPort = tblSMCompanyPreference.intSMTPPort
+		,strEncryptedConnection = tblSMCompanyPreference.strSMTPSsl
+		,ysnRequiresAuthentication = convert(bit, 1)
+		,strUserName = tblSMCompanyPreference.strSMTPUserName
+		,strPassword = tblSMCompanyPreference.strSMTPPassword
+		,intConcurrencyId = tblSMCompanyPreference.intConcurrencyId
+	from
+		tblHDSetting
+		,tblSMCompanyPreference
