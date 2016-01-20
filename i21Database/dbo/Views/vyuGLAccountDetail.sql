67b5ba8a58d81f6748bc8ceef6e8bc9a4e39f4f8
Liz Dorado <liz.dorado@irely.com>
2016-01-20 15:01:55 +0800

    GL-1427

--- i21Database/dbo/Views/vyuGLAccountDetail.sql
+++ i21Database/dbo/Views/vyuGLAccountDetail.sql
@@ -3,7 +3,8 @@ AS
 	SELECT        dbo.tblGLAccount.strAccountId, dbo.tblGLAccount.strDescription, dbo.tblGLAccountGroup.strAccountGroup, dbo.tblGLAccountGroup.strAccountType, dbo.tblGLAccountCategory.strAccountCategory, 
                          dbo.tblGLAccount.strComments, dbo.tblGLAccount.strCashFlow, dbo.tblGLAccount.ysnActive, dbo.tblGLAccount.ysnSystem, dbo.tblGLAccount.ysnRevalue, dbo.tblGLAccountUnit.intAccountUnitId, 
                          dbo.tblGLAccountUnit.strUOMCode, dbo.tblGLAccount.intAccountId, dbo.tblGLAccount.intCurrencyID, dbo.tblGLAccount.intCurrencyExchangeRateTypeId, dbo.tblGLAccount.strNote, dbo.tblSMCurrency.strCurrency, 
-                         dbo.tblSMCurrencyExchangeRateType.strCurrencyExchangeRateType, dbo.tblGLAccount.intAccountGroupId, dbo.tblGLAccountSegment.intAccountCategoryId
+                         dbo.tblSMCurrencyExchangeRateType.strCurrencyExchangeRateType, dbo.tblGLAccount.intAccountGroupId, dbo.tblGLAccountSegment.intAccountCategoryId,
+						 dbo.tblGLCOACrossReference.strExternalId,dbo.tblGLCOACrossReference.strCurrentExternalId
 FROM            dbo.tblGLAccount INNER JOIN
                          dbo.tblGLAccountSegmentMapping ON dbo.tblGLAccount.intAccountId = dbo.tblGLAccountSegmentMapping.intAccountId INNER JOIN
                          dbo.tblGLAccountSegment ON dbo.tblGLAccountSegmentMapping.intAccountSegmentId = dbo.tblGLAccountSegment.intAccountSegmentId INNER JOIN
@@ -12,5 +13,6 @@ FROM            dbo.tblGLAccount INNER JOIN
                          dbo.tblSMCurrencyExchangeRateType ON dbo.tblGLAccount.intCurrencyExchangeRateTypeId = dbo.tblSMCurrencyExchangeRateType.intCurrencyExchangeRateTypeId LEFT OUTER JOIN
                          dbo.tblSMCurrency ON dbo.tblGLAccount.intCurrencyID = dbo.tblSMCurrency.intCurrencyID LEFT OUTER JOIN
                          dbo.tblGLAccountUnit ON dbo.tblGLAccount.intAccountUnitId = dbo.tblGLAccountUnit.intAccountUnitId LEFT OUTER JOIN
+						 dbo.tblGLCOACrossReference ON dbo.tblGLAccount.intAccountId = dbo.tblGLCOACrossReference.inti21Id LEFT OUTER JOIN
                          dbo.tblGLAccountGroup ON dbo.tblGLAccount.intAccountGroupId = dbo.tblGLAccountGroup.intAccountGroupId
 WHERE        (dbo.tblGLAccountStructure.strType = 'Primary')
