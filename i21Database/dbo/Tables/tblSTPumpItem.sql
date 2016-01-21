c16ee6db34e0609a712744f95116b709cf17b78c
Ravikumar Kumashi <ravikumar.k@irely.com>
2016-01-21 12:13:05 +0530

    ST-199

--- i21Database/dbo/Tables/tblSTPumpItem.sql
+++ i21Database/dbo/Tables/tblSTPumpItem.sql
@@ -5,11 +5,13 @@
     [intItemUOMId] INT NOT NULL, 
     [dblPrice] NUMERIC(18, 6) NULL  DEFAULT 0, 
     [intTaxGroupId] int NULL,
-    [intConcurrencyId ] INT NOT NULL,
+	[intCategoryId] int NULL,
+    [intConcurrencyId] INT NOT NULL,
 	CONSTRAINT [PK_tblSTPumpItem] PRIMARY KEY ([intStorePumpItemId]),
 	CONSTRAINT [FK_tblSTPumpItem_tblSTStore] FOREIGN KEY ([intStoreId]) REFERENCES [tblSTStore]([intStoreId]),
 	CONSTRAINT [FK_tblSTPumpItem_tblICItemUOM_intItemUOMId] FOREIGN KEY ([intItemUOMId]) REFERENCES [dbo].[tblICItemUOM] ([intItemUOMId]),
-	CONSTRAINT [FK_tblSTPumpItem_tblSMTaxGroup_intTaxGroupId] FOREIGN KEY ([intTaxGroupId]) REFERENCES [dbo].[tblSMTaxGroup] ([intTaxGroupId])
+	CONSTRAINT [FK_tblSTPumpItem_tblSMTaxGroup_intTaxGroupId] FOREIGN KEY ([intTaxGroupId]) REFERENCES [dbo].[tblSMTaxGroup] ([intTaxGroupId]),
+	CONSTRAINT [FK_tblSTPumpItem_tblICCategory_intCategoryId] FOREIGN KEY ([intCategoryId]) REFERENCES [dbo].[tblICCategory] ([intCategoryId])
 )
 
 
