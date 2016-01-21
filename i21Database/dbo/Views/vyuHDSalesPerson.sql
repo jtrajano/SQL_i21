929f8b4c3620444963e110cc39894fbb0a30bae4
Jayson Ramos <jayson.ramos@iRely.com>
2016-01-21 10:47:03 +0800

    HD-1054

---
+++ i21Database/dbo/Views/vyuHDSalesPerson.sql
@@ -0,0 +1,15 @@
+﻿CREATE VIEW [dbo].[vyuHDSalesPerson]
+	AS
+	select
+			tblEntity.intEntityId
+			,tblEntity.strName
+			,tblARSalesperson.strSalespersonId
+			,tblARSalesperson.strTitle
+			,tblARSalesperson.strType
+			,tblEntity.strEmail
+			,tblEntity.strPhone
+			,ysnActiveEntity = tblEntity.ysnActive
+			,ysnActiveSalesPerson = tblARSalesperson.ysnActive
+	from tblARSalesperson, tblEntity
+	where
+		tblEntity.intEntityId = tblARSalesperson.intEntitySalespersonId
