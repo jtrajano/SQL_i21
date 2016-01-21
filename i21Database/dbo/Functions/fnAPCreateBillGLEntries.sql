a32220fd0a62586e30e538e9a66db5e5f9eaecf4
Erick F. Dela Cruz <erick.delacruz@irely.com>
2016-01-21 14:51:57 +0800

    AP-1967

--- i21Database/dbo/Functions/fnAPCreateBillGLEntries.sql
+++ i21Database/dbo/Functions/fnAPCreateBillGLEntries.sql
@@ -208,7 +208,7 @@ BEGIN
 		[dtmDate]						=	DATEADD(dd, DATEDIFF(dd, 0, A.dtmDate), 0),
 		[strBatchID]					=	@batchId,
 		[intAccountId]					=	[dbo].[fnGetItemGLAccount](B.intItemId, ItemLoc.intItemLocationId, 'Cost Adjustment'),
-		[dblDebit]						=	(CASE WHEN A.intTransactionType IN (1) THEN CAST(((B.dblCost - B.dblOldCost)  * B.dblQtyReceived) AS DECIMAL(18,2)) ELSE 0 END), 
+		[dblDebit]						=	(CASE WHEN A.intTransactionType IN (1) THEN B.dblTotal - CAST(B.dblOldCost  * B.dblQtyReceived AS DECIMAL(18,2)) ELSE 0 END), 
 		[dblCredit]						=	0, -- Bill
 		[dblDebitUnit]					=	0,
 		[dblCreditUnit]					=	0,
