c72e6b36fe39932e0b2848e73828ce97b1b39e13
Liz Dorado <liz.dorado@irely.com>
2016-01-22 17:05:58 +0800

    GL-2472

--- i21Database/dbo/Views/vyuGLReallocation.sql
+++ i21Database/dbo/Views/vyuGLReallocation.sql
@@ -1,7 +1,7 @@
 CREATE VIEW [dbo].[vyuGLReallocation]
 AS
      SELECT
-             RA.strName, RA.strDescription AS strDescription, A.strAccountId, A.strDescription as strAccountIdDescription, ISNULL(dblPercentage, 0) AS dblPercentage
+             RA.intAccountReallocationId, RA.strName, RA.strDescription AS strDescription, A.strAccountId, A.strDescription as strAccountIdDescription, ISNULL(dblPercentage, 0) AS dblPercentage
      FROM tblGLAccountReallocationDetail AAD
           INNER JOIN tblGLAccount A ON AAD.intAccountId = A.intAccountId
           INNER JOIN tblGLAccountReallocation RA ON AAD.intAccountReallocationId = RA.intAccountReallocationId
