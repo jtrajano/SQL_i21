ca18c64915eca0292c07dc9a892479da0e45b808
jeffrey trajano <jeffrey.trajano@irely.com>
2016-01-22 18:31:01 +0800

    GL-2169 Add a reconciliation table

---
+++ i21Database/dbo/Tables/tblGLReconcileAccount.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[tblGLReconcileAccount]
+(
+	[intId] INT NOT NULL PRIMARY KEY, 
+    [intConcurrencyId] INT NOT NULL, 
+    [intAccountId] INT NOT NULL, 
+    [dtmReconcileDate] DATETIME NOT NULL, 
+    [strReconciledId] NVARCHAR(50) NOT NULL
+)
