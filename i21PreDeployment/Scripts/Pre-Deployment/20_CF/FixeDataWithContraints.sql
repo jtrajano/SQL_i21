dca59318ceee343b833cc9c9692fcabec857ac10
bonn ponce <bonn.ponce@irely.com>
2016-01-23 00:26:48 +0800

    CF-452 Error upgrading db from 15.3 into 16.1

--- i21PreDeployment/Scripts/Pre-Deployment/20_CF/FixeDataWithContraints.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/20_CF/FixeDataWithContraints.sql
@@ -1,87 +1,176 @@
 ﻿IF EXISTS(select 1  from INFORMATION_SCHEMA.TABLES where TABLE_NAME = N'tblCFCard')
 BEGIN
 	print 'begin updating card data'
-	update tblCFCard set intExpenseItemId = NULL where intExpenseItemId = 0
-	update tblCFCard set intDepartmentId = NULL where intDepartmentId = 0
-	update tblCFCard set intCardTypeId = NULL where intCardTypeId = 0
-	update tblCFCard set intDefaultFixVehicleNumber = NULL where intDefaultFixVehicleNumber = 0
-	update tblCFCard set intNetworkId = NULL where intNetworkId = 0
+	
+	IF EXISTS(SELECT 1 FROM   INFORMATION_SCHEMA.COLUMNS WHERE  TABLE_NAME = 'tblCFCard'AND COLUMN_NAME = 'intExpenseItemId')
+	BEGIN
+		update tblCFCard set intExpenseItemId = NULL where intExpenseItemId = 0
+	END
+
+	IF EXISTS(SELECT 1 FROM   INFORMATION_SCHEMA.COLUMNS WHERE  TABLE_NAME = 'tblCFCard'AND COLUMN_NAME = 'intDepartmentId')
+	BEGIN
+		update tblCFCard set intDepartmentId = NULL where intDepartmentId = 0
+	END
+
+	IF EXISTS(SELECT 1 FROM   INFORMATION_SCHEMA.COLUMNS WHERE  TABLE_NAME = 'tblCFCard'AND COLUMN_NAME = 'intCardTypeId')
+	BEGIN
+		update tblCFCard set intCardTypeId = NULL where intCardTypeId = 0
+	END
+
+	IF EXISTS(SELECT 1 FROM   INFORMATION_SCHEMA.COLUMNS WHERE  TABLE_NAME = 'tblCFCard'AND COLUMN_NAME = 'intDefaultFixVehicleNumber')
+	BEGIN
+		update tblCFCard set intDefaultFixVehicleNumber = NULL where intDefaultFixVehicleNumber = 0
+	END
+
+	IF EXISTS(SELECT 1 FROM   INFORMATION_SCHEMA.COLUMNS WHERE  TABLE_NAME = 'tblCFCard'AND COLUMN_NAME = 'intNetworkId')
+	BEGIN
+		update tblCFCard set intNetworkId = NULL where intNetworkId = 0
+	END
 	print 'end updating card data'
 END
 
 IF EXISTS(select 1  from INFORMATION_SCHEMA.TABLES where TABLE_NAME = N'tblCFCreditCard')
 BEGIN
 	print 'begin updating credit card data'
-	update tblCFCreditCard set intCustomerId = NULL where intCustomerId = 0
+
+	IF EXISTS(SELECT 1 FROM   INFORMATION_SCHEMA.COLUMNS WHERE  TABLE_NAME = 'tblCFCreditCard'AND COLUMN_NAME = 'intCustomerId')
+	BEGIN
+		update tblCFCreditCard set intCustomerId = NULL where intCustomerId = 0
+	END
 	print 'end updating credit card data'
 END
 
 IF EXISTS(select 1  from INFORMATION_SCHEMA.TABLES where TABLE_NAME = N'tblCFIndexPricingBySiteGroup')
 BEGIN
 	print 'begin updating index pricing by site group data'
-	update tblCFIndexPricingBySiteGroup set intIndexPricingBySiteGroupHeaderId = NULL where intIndexPricingBySiteGroupHeaderId = 0
-	update tblCFIndexPricingBySiteGroup set intARItemID = NULL where intARItemID = 0
+	IF EXISTS(SELECT 1 FROM   INFORMATION_SCHEMA.COLUMNS WHERE  TABLE_NAME = 'tblCFIndexPricingBySiteGroup'AND COLUMN_NAME = 'intIndexPricingBySiteGroupHeaderId')
+	BEGIN
+		update tblCFIndexPricingBySiteGroup set intIndexPricingBySiteGroupHeaderId = NULL where intIndexPricingBySiteGroupHeaderId = 0
+	END
+	
+	IF EXISTS(SELECT 1 FROM   INFORMATION_SCHEMA.COLUMNS WHERE  TABLE_NAME = 'tblCFIndexPricingBySiteGroup'AND COLUMN_NAME = 'intARItemID')
+	BEGIN
+		update tblCFIndexPricingBySiteGroup set intARItemID = NULL where intARItemID = 0
+	END
 	print 'end updating index pricing By site group data'
 END
 
 IF EXISTS(select 1  from INFORMATION_SCHEMA.TABLES where TABLE_NAME = N'tblCFItem')
 BEGIN
 	print 'begin updating item data'
-	update tblCFItem set intARItemId = NULL where intARItemId = 0
-	update tblCFItem set intTaxGroupMaster = NULL where intTaxGroupMaster = 0
+	IF EXISTS(SELECT 1 FROM   INFORMATION_SCHEMA.COLUMNS WHERE  TABLE_NAME = 'tblCFItem'AND COLUMN_NAME = 'intARItemId')
+	BEGIN
+		update tblCFItem set intARItemId = NULL where intARItemId = 0
+	END
+	IF EXISTS(SELECT 1 FROM   INFORMATION_SCHEMA.COLUMNS WHERE  TABLE_NAME = 'tblCFItem'AND COLUMN_NAME = 'intTaxGroupMaster')
+	BEGIN
+		update tblCFItem set intTaxGroupMaster = NULL where intTaxGroupMaster = 0
+	END
 	print 'end updating item data'
 END
 
 IF EXISTS(select 1  from INFORMATION_SCHEMA.TABLES where TABLE_NAME = N'tblCFNetwork')
 BEGIN
 	print 'begin updating network data'
-	update tblCFNetwork set intCustomerId = NULL where intCustomerId = 0
-	update tblCFNetwork set intLocationId = NULL where intLocationId = 0
-	update tblCFNetwork set intImportMapperId = NULL where intImportMapperId = 0
+	IF EXISTS(SELECT 1 FROM   INFORMATION_SCHEMA.COLUMNS WHERE  TABLE_NAME = 'tblCFNetwork'AND COLUMN_NAME = 'intCustomerId')
+	BEGIN
+		update tblCFNetwork set intCustomerId = NULL where intCustomerId = 0
+	END
+	IF EXISTS(SELECT 1 FROM   INFORMATION_SCHEMA.COLUMNS WHERE  TABLE_NAME = 'tblCFNetwork'AND COLUMN_NAME = 'intLocationId')
+	BEGIN
+		update tblCFNetwork set intLocationId = NULL where intLocationId = 0
+	END
+	IF EXISTS(SELECT 1 FROM   INFORMATION_SCHEMA.COLUMNS WHERE  TABLE_NAME = 'tblCFNetwork'AND COLUMN_NAME = 'intImportMapperId')
+	BEGIN
+		update tblCFNetwork set intImportMapperId = NULL where intImportMapperId = 0
+	END
 	print 'end updating network data'
 END
 
 IF EXISTS(select 1  from INFORMATION_SCHEMA.TABLES where TABLE_NAME = N'tblCFPriceProfileDetail')
 BEGIN
 	print 'begin updating price profile detail data'
-	update tblCFPriceProfileDetail set intItemId = NULL where intItemId = 0
+	IF EXISTS(SELECT 1 FROM   INFORMATION_SCHEMA.COLUMNS WHERE  TABLE_NAME = 'tblCFPriceProfileDetail'AND COLUMN_NAME = 'intItemId')
+	BEGIN
+		update tblCFPriceProfileDetail set intItemId = NULL where intItemId = 0
+	END
+	
 	print 'end updating price profile detail data'
 END
 
 IF EXISTS(select 1  from INFORMATION_SCHEMA.TABLES where TABLE_NAME = N'tblCFSiteGroupPriceAdjustment')
 BEGIN
 	print 'begin updating site group price adjustment data'
-	update tblCFPriceProfileDetail set intSiteGroupId = NULL where intSiteGroupId = 0
-	update tblCFPriceProfileDetail set intItemId = NULL where intItemId = 0
-	
+	IF EXISTS(SELECT 1 FROM   INFORMATION_SCHEMA.COLUMNS WHERE  TABLE_NAME = 'tblCFSiteGroupPriceAdjustment'AND COLUMN_NAME = 'intSiteGroupId')
+	BEGIN
+		update tblCFSiteGroupPriceAdjustment set intSiteGroupId = NULL where intSiteGroupId = 0
+	END
+	IF EXISTS(SELECT 1 FROM   INFORMATION_SCHEMA.COLUMNS WHERE  TABLE_NAME = 'tblCFSiteGroupPriceAdjustment'AND COLUMN_NAME = 'intItemId')
+	BEGIN
+		update tblCFSiteGroupPriceAdjustment set intItemId = NULL where intItemId = 0
+	END
 	print 'end updating site group price adjustment data'
 END
 
 IF EXISTS(select 1  from INFORMATION_SCHEMA.TABLES where TABLE_NAME = N'tblCFSite')
 BEGIN
 	print 'begin updating site data'
-	update tblCFSite set intNetworkId = NULL where intNetworkId = 0
-	update tblCFSite set intARLocationId = NULL where intARLocationId = 0
-	update tblCFSite set intImportMapperId = NULL where intImportMapperId = 0
-	update tblCFSite set intCashCustomerID = NULL where intCashCustomerID = 0
-	update tblCFSite set intAdjustmentSiteGroupId = NULL where intAdjustmentSiteGroupId = 0
+	IF EXISTS(SELECT 1 FROM   INFORMATION_SCHEMA.COLUMNS WHERE  TABLE_NAME = 'tblCFSite'AND COLUMN_NAME = 'intNetworkId')
+	BEGIN
+		update tblCFSite set intNetworkId = NULL where intNetworkId = 0
+	END
+	IF EXISTS(SELECT 1 FROM   INFORMATION_SCHEMA.COLUMNS WHERE  TABLE_NAME = 'tblCFSite'AND COLUMN_NAME = 'intARLocationId')
+	BEGIN
+		update tblCFSite set intARLocationId = NULL where intARLocationId = 0
+	END
+	IF EXISTS(SELECT 1 FROM   INFORMATION_SCHEMA.COLUMNS WHERE  TABLE_NAME = 'tblCFSite'AND COLUMN_NAME = 'intImportMapperId')
+	BEGIN
+		update tblCFSite set intImportMapperId = NULL where intImportMapperId = 0
+	END
+	IF EXISTS(SELECT 1 FROM   INFORMATION_SCHEMA.COLUMNS WHERE  TABLE_NAME = 'tblCFSite'AND COLUMN_NAME = 'intCashCustomerID')
+	BEGIN
+		update tblCFSite set intCashCustomerID = NULL where intCashCustomerID = 0
+	END
+	IF EXISTS(SELECT 1 FROM   INFORMATION_SCHEMA.COLUMNS WHERE  TABLE_NAME = 'tblCFSite'AND COLUMN_NAME = 'intAdjustmentSiteGroupId')
+	BEGIN
+		update tblCFSite set intAdjustmentSiteGroupId = NULL where intAdjustmentSiteGroupId = 0
+	END
 	print 'end updating site data'
 END
 
 IF EXISTS(select 1  from INFORMATION_SCHEMA.TABLES where TABLE_NAME = N'tblCFTransaction')
 BEGIN
 	print 'begin updating transaction data'
-	update tblCFTransaction set intNetworkId = NULL where intNetworkId = 0
-	update tblCFTransaction set intSiteId = NULL where intSiteId = 0
-	update tblCFTransaction set intCardId = NULL where intCardId = 0
-	update tblCFTransaction set intVehicleId = NULL where intVehicleId = 0
-	update tblCFTransaction set intSalesPersonId = NULL where intSalesPersonId = 0
+	IF EXISTS(SELECT 1 FROM   INFORMATION_SCHEMA.COLUMNS WHERE  TABLE_NAME = 'tblCFTransaction'AND COLUMN_NAME = 'intNetworkId')
+	BEGIN
+		update tblCFTransaction set intNetworkId = NULL where intNetworkId = 0
+	END
+	IF EXISTS(SELECT 1 FROM   INFORMATION_SCHEMA.COLUMNS WHERE  TABLE_NAME = 'tblCFTransaction'AND COLUMN_NAME = 'intSiteId')
+	BEGIN
+		update tblCFTransaction set intSiteId = NULL where intSiteId = 0
+	END
+	IF EXISTS(SELECT 1 FROM   INFORMATION_SCHEMA.COLUMNS WHERE  TABLE_NAME = 'tblCFTransaction'AND COLUMN_NAME = 'intCardId')
+	BEGIN
+		update tblCFTransaction set intCardId = NULL where intCardId = 0
+	END
+	IF EXISTS(SELECT 1 FROM   INFORMATION_SCHEMA.COLUMNS WHERE  TABLE_NAME = 'tblCFTransaction'AND COLUMN_NAME = 'intVehicleId')
+	BEGIN
+		update tblCFTransaction set intVehicleId = NULL where intVehicleId = 0
+	END
+	IF EXISTS(SELECT 1 FROM   INFORMATION_SCHEMA.COLUMNS WHERE  TABLE_NAME = 'tblCFTransaction'AND COLUMN_NAME = 'intSalesPersonId')
+	BEGIN
+		update tblCFTransaction set intSalesPersonId = NULL where intSalesPersonId = 0
+	END
 	print 'end updating transaction data'
 END
 
 IF EXISTS(select 1  from INFORMATION_SCHEMA.TABLES where TABLE_NAME = N'tblCFVehicle')
 BEGIN
 	print 'begin updating vehicle data'
-	update tblCFVehicle set intExpenseItemId = NULL where intExpenseItemId = 0
+	IF EXISTS(SELECT 1 FROM   INFORMATION_SCHEMA.COLUMNS WHERE  TABLE_NAME = 'tblCFVehicle'AND COLUMN_NAME = 'intExpenseItemId')
+	BEGIN
+		update tblCFVehicle set intExpenseItemId = NULL where intExpenseItemId = 0
+	END
+	
 	print 'end updating vehicle data'
 END
