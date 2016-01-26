1d75758ad092d2c7a255cea5d1c1ab9018d497fd
Erick Dela Cruz <erick.delacruz@irely.com>
2016-01-26 21:03:37 +0800

    AP-2063

--- i21Database/dbo/Views/vyuAP1099MISC.sql
+++ i21Database/dbo/Views/vyuAP1099MISC.sql
@@ -42,7 +42,7 @@ AS
 										, B.strCountry
 										, B.strPhone)
 		, B.strCompanyName
-		, strEIN = B.strFederalTaxID
+		, strEIN = B.strEin--B.strFederalTaxID
 		, A.strFederalTaxId
 		, A.strAddress
 		, A.strVendorCompanyName
@@ -71,7 +71,7 @@ AS
 	CROSS JOIN tblAP1099Threshold C
 	WHERE A.int1099Form = 1
 	GROUP BY intYear, intEntityVendorId
-	,B.strCompanyName, B.strAddress, B.strCity, B.strState, B.strZip, B.strCountry, B.strPhone, B.strFederalTaxID
+	,B.strCompanyName, B.strAddress, B.strCity, B.strState, B.strZip, B.strCountry, B.strPhone, B.strEin--B.strFederalTaxID
 	, A.strAddress
 	, A.strVendorCompanyName
 	, A.strVendorId
