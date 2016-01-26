64fec9a84f7eb9778667cd79debceb4679f376e5
Smith Wilson de Jesus <smith.dejesus@irely.com>
2016-01-26 10:49:12 +0800

    CM-1039 Error when processing an ACH

--- i21Database/dbo/Views/vyuCMBankTransaction.sql
+++ i21Database/dbo/Views/vyuCMBankTransaction.sql
@@ -3,28 +3,28 @@ AS
 
 SELECT 
 *,
-strPayeeBankName = (
+strPayeeBankName = ISNULL((
 		SELECT TOP 1 strBankName FROM tblEntityEFTInformation EFTInfo 
 		WHERE EFTInfo.ysnActive = 1 AND dtmEffectiveDate >= DATEADD(dd, DATEDIFF(dd, 0, GETDATE()), 0) AND intEntityId = intPayeeId ORDER BY dtmEffectiveDate desc
-),
-strPayeeBankAccountNumber  = (
+),''),
+strPayeeBankAccountNumber  = ISNULL((
 		SELECT TOP 1 strAccountNumber FROM tblEntityEFTInformation EFTInfo 
 		WHERE EFTInfo.ysnActive = 1 AND dtmEffectiveDate >= DATEADD(dd, DATEDIFF(dd, 0, GETDATE()), 0) AND intEntityId = intPayeeId ORDER BY dtmEffectiveDate desc
-),
-strPayeeBankRoutingNumber = (
+),''),
+strPayeeBankRoutingNumber = ISNULL((
 		SELECT TOP 1 strRTN FROM tblEntityEFTInformation EFTInfo 
 		INNER JOIN tblCMBank BANK ON EFTInfo.intBankId = BANK.intBankId
 		WHERE EFTInfo.ysnActive = 1 AND dtmEffectiveDate >= DATEADD(dd, DATEDIFF(dd, 0, GETDATE()), 0) AND intEntityId = intPayeeId ORDER BY dtmEffectiveDate desc
-),
-strEntityNo = (
+),''),
+strEntityNo = ISNULL((
 		SELECT strEntityNo FROM tblEntity
 		WHERE intEntityId = intPayeeId
-),
-strSocialSecurity = (
+),''),
+strSocialSecurity = ISNULL((
 		SELECT Emp.strSocialSecurity FROM 
 		tblPRPaycheck PayCheck  INNER JOIN
 		tblPREmployee Emp ON PayCheck.intEntityEmployeeId = Emp.intEntityEmployeeId
 		WHERE PayCheck.strPaycheckId = tblCMBankTransaction.strTransactionId 
-)
+),'')
 FROM tblCMBankTransaction
 WHERE dbo.fnIsDepositEntry(strLink) = 0
