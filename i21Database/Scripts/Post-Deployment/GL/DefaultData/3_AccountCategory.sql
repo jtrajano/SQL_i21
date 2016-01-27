522b7513704cfa3801e2c2cfca7b2e10321ad27f
Liz Dorado <liz.dorado@irely.com>
2016-01-27 08:07:20 +0800

    GL-2477

--- i21Database/Scripts/Post-Deployment/GL/DefaultData/3_AccountCategory.sql
+++ i21Database/Scripts/Post-Deployment/GL/DefaultData/3_AccountCategory.sql
@@ -5,6 +5,7 @@ GO
 --DO NOT CHANGE THE ID NAME COMBINATION AS OTHER MODULES ARE USING ID AS REFERENCE
 --TOTAL COUNT IS 56 AS OF 10-22-2015
 --TOTAL COUNT IS 59 AS OF 01-26-2015
+--TOTAL COUNT IS 60 AS OF 01-27-2015
 BEGIN TRY --ACCOUNT CATEGORY DEFAULTS
 	BEGIN TRANSACTION
 	DECLARE @tblSegment TABLE(intAccountSegmentId INT, strAccountCategory VARCHAR(100))
@@ -104,8 +105,9 @@ BEGIN TRY --ACCOUNT CATEGORY DEFAULTS
 			SELECT id = 55,name = 'Other Charge Expense'UNION ALL 
 			SELECT id = 56,name = 'Other Charge Income' UNION ALL 
 			SELECT id = 57,name = 'Maintenance Sales' UNION ALL
-			SELECT id = 58,name = 'Realized Gain or Loss Foreign Currency' UNION ALL
-			SELECT id = 59,name = 'Unrealized Gain or Loss Foreign Currency'
+			SELECT id = 58,name = 'Deferred Revenue' UNION ALL
+			SELECT id = 59,name = 'Realized Gain or Loss Foreign Currency' UNION ALL
+			SELECT id = 60,name = 'Unrealized Gain or Loss Foreign Currency'
 	) AS CategoryHardCodedValues
 		ON  CategoryTable.intAccountCategoryId = CategoryHardCodedValues.id
 
@@ -167,7 +169,7 @@ BEGIN TRY --ACCOUNT CATEGORY DEFAULTS
 		JOIN tblGLAccountCategory C ON C.strAccountCategory COLLATE Latin1_General_CI_AS = t.strAccountCategory COLLATE Latin1_General_CI_AS
 
 		--REMOVE EXCESS
-		DELETE FROM tblGLAccountCategory WHERE intAccountCategoryId > 57
+		DELETE FROM tblGLAccountCategory WHERE intAccountCategoryId > 60
 	END
 	COMMIT TRANSACTION
 END TRY
