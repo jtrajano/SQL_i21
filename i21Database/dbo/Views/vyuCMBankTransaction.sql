d8c112ac76efdcc6ab35c679db7afbda4dcb09f9
Smith Wilson de Jesus <smith.dejesus@irely.com>
2016-01-28 00:08:03 +0800

    CM-839 Modify generic error message when generating ACH file with incomplete EFT Information

--- i21Database/dbo/Views/vyuCMBankTransaction.sql
+++ i21Database/dbo/Views/vyuCMBankTransaction.sql
@@ -3,6 +3,14 @@ AS
 
 SELECT 
 *,
+ysnPayeeEFTInfoActive = ISNULL((
+		SELECT TOP 1 ysnActive FROM tblEntityEFTInformation EFTInfo 
+		WHERE intEntityId = intPayeeId ORDER BY dtmEffectiveDate desc
+),0),
+ysnPayeeEFTInfoExpired = ISNULL((
+		SELECT TOP 1 (CASE WHEN dtmEffectiveDate >= DATEADD(dd, DATEDIFF(dd, 0, GETDATE()), 0) THEN CAST(0 AS BIT) ELSE CAST(1 AS BIT) END)  FROM tblEntityEFTInformation EFTInfo 
+		WHERE intEntityId = intPayeeId ORDER BY dtmEffectiveDate desc
+),0),
 strPayeeBankName = ISNULL((
 		SELECT TOP 1 strBankName FROM tblEntityEFTInformation EFTInfo 
 		WHERE EFTInfo.ysnActive = 1 AND dtmEffectiveDate >= DATEADD(dd, DATEDIFF(dd, 0, GETDATE()), 0) AND intEntityId = intPayeeId ORDER BY dtmEffectiveDate desc
