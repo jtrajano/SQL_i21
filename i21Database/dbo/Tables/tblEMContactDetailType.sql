e18a6f41b67b5dc4930bcaa4e43e755a1ebca50f
Mon Gonzales <mon.gonzales@irely.com>
2016-01-28 18:05:44 +0800

    EM-803

---
+++ i21Database/dbo/Tables/tblEMContactDetailType.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tblEMContactDetailType]
+(
+	[intContactDetailTypeId]		INT NOT NULL IDENTITY(1,1),
+	[strField]						NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL, 
+	[strType]						NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL, 
+	[strMasking]					NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL, 
+	[ysnDefault]					BIT NOT NULL DEFAULT(0),
+	[intConcurrencyId]				INT CONSTRAINT [DF_tblEMContactDetailType_intConcurrencyId] DEFAULT ((0)) NOT NULL,
+	CONSTRAINT [PK_tblEMContactDetailType] PRIMARY KEY CLUSTERED ([intContactDetailTypeId] ASC),	
+	
+)
