baca12d7e50c026bb3dc63b92e608778410130e0
axel_casauran <axel.casauran@irely.com>
2016-01-29 09:04:17 +0800

    FRD-775

---
+++ i21Database/dbo/Views/vyuFRDRowDesign.sql
@@ -0,0 +1,39 @@
+CREATE VIEW [dbo].[vyuFRDRowDesign]
+AS
+
+select 
+
+intRowDetailId
+,B.intRowId
+,B.strRowName
+,B.strDescription as strHeaderDescription
+,intRefNo
+,'R' + CAST(intRefNo as NVARCHAR(10)) as strRefNo
+,B.strDescription
+,strRowType
+,strBalanceSide
+,strSource
+,strRelatedRows
+,strAccountsUsed
+,strAccountsType
+,ysnShowCredit
+,ysnShowDebit
+,ysnShowOthers
+,ysnLinktoGL
+,ysnPrintEach
+,ysnHidden
+,dblHeight
+,strFontName
+,strFontStyle
+,strFontColor
+,intFontSize
+,strOverrideFormatMask
+,ysnForceReversedExpense
+,ysnOverrideFormula
+,ysnOverrideColumnFormula
+,intSort
+
+from tblFRRowDesign A
+inner join tblFRRow B
+on A.intRowId = B.intRowId
+
