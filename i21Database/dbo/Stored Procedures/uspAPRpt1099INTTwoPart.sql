59da6ec0fe80c39246a7ab68be6caf249352caae
Erick Dela Cruz <erick.delacruz@irely.com>
2016-01-29 18:12:58 +0800

    AP-2081

--- i21Database/dbo/Stored Procedures/uspAPRpt1099INTTwoPart.sql
+++ i21Database/dbo/Stored Procedures/uspAPRpt1099INTTwoPart.sql
@@ -57,6 +57,7 @@ DECLARE @vendorFromParam NVARCHAR(100) = NULL;
 DECLARE @vendorToParam NVARCHAR(100) = NULL;
 DECLARE @yearParam INT = YEAR(GETDATE());
 DECLARE @correctedParam BIT = 0;
+DECLARE @reprintParam BIT = 0;
 DECLARE @query NVARCHAR(MAX);
 DECLARE @xmlDocumentId AS INT;
 
@@ -123,6 +124,10 @@ BEGIN
 	FROM @temp_xml_table WHERE [fieldname] = 'year'
 
 	SELECT 
+		@reprintParam = [from]
+	FROM @temp_xml_table WHERE [fieldname] = 'reprint'
+
+	SELECT 
 		@correctedParam = [from]
 	FROM @temp_xml_table WHERE [fieldname] = 'corrected'
 END;
@@ -140,6 +145,7 @@ WITH INT1099 (
 	,strAddress
 	,strVendorCompanyName
 	,strVendorId
+	,strCity
 	,strZip
 	,strZipState
 	,strFederalTaxId
@@ -155,10 +161,25 @@ AS
 	,A.* 
 	,(CASE WHEN @correctedParam = 0 THEN NULL ELSE 'X' END) AS strCorrected
 	FROM vyuAP1099INT A
+	OUTER APPLY 
+	(
+		SELECT TOP 1 * FROM tblAP1099History B
+		WHERE A.intYear = B.intYear AND B.int1099Form = 1
+		AND B.intEntityVendorId = A.intEntityVendorId
+		ORDER BY B.dtmDatePrinted DESC
+	) History
 	WHERE 1 = (CASE WHEN @vendorFromParam IS NOT NULL THEN
-					(CASE WHEN strVendorId BETWEEN @vendorFromParam AND @vendorToParam THEN 1 ELSE 0 END)
+					(CASE WHEN A.strVendorId BETWEEN @vendorFromParam AND @vendorToParam THEN 1 ELSE 0 END)
 				ELSE 1 END)
-	AND intYear = @yearParam
+	AND A.intYear = @yearParam
+	AND 1 = (
+		CASE WHEN  ISNULL(@correctedParam,0) = 1 THEN 1 
+				ELSE 
+					(CASE WHEN History.ysnPrinted IS NOT NULL AND History.ysnPrinted = 1 AND @reprintParam = 1 THEN 1 
+						WHEN History.ysnPrinted IS NULL THEN 1
+						WHEN History.ysnPrinted IS NOT NULL AND History.ysnPrinted = 0 THEN 1
+					ELSE 0 END)
+		END)
 )
 
 SELECT
