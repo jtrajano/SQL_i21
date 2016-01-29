cedad3744ea670cd0309f9108b2d7d783d7b53ad
jeffrey trajano <jeffrey.trajano@irely.com>
2016-01-29 09:48:22 +0800

    GL-2483 Collation issue

--- i21Database/dbo/Tables/tblGLReconcileAccount.sql
+++ i21Database/dbo/Tables/tblGLReconcileAccount.sql
@@ -4,5 +4,5 @@
     [intConcurrencyId] INT NOT NULL, 
     [intAccountId] INT NOT NULL, 
     [dtmReconcileDate] DATETIME NOT NULL, 
-    [strReconciledId] NVARCHAR(50) NOT NULL
+    [strReconciledId] NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL,
 )
