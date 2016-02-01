47b8a5c5f2fb5af7c6c80708a620a3a0723caccf
Feb Montefrio <feb.montefrio@irely.com>
2016-02-01 12:22:48 +0800

    IC-1798

---
+++ i21Database/dbo/Stored Procedures/uspICPostReturnAverageCosting.sql
@@ -0,0 +1,163 @@
+﻿/*
+	This is the stored procedure that handles the moving average costing method. 
+	
+	Parameters: 
+	@intItemId - The item to process
+
+	@intLocationId - The location where the item is being process. 
+
+	@intItemUOMId - The UOM used for the item in a transaction. Each transaction can use different kinds of UOM on its items. 
+	
+	@dtmDate - The date used in the transaction and posting. 
+
+	@dblQty - A positive qty indicates an increase of stock. A negative qty indicates a decrease in stock. 
+
+	@dblUOMQty - The stock unit qty associated with the UOM. For example, a box may have 10 pieces of an item. In this case, UOM qty will be 10. 
+
+	@dblCost - The cost per base qty of the item. 
+
+	@dblSalesPrice - The sales price of an item sold to the customer. 
+
+	@intCurrencyId - The foreign currency associated with the transaction. 
+
+	@dblExchangeRate - The conversion factor between the base currency and the foreign currency. 
+
+	@intTransactionId - The primary key id used in a transaction. 
+
+	@strTransactionId - The string value of a transaction id. 
+
+	@strBatchId - The batch id to use in generating the g/l entries. 
+
+	@intEntityUserSecurityId - The user who initiated or called this stored procedure. 
+*/
+
+CREATE PROCEDURE [dbo].[uspICPostReturnAverageCosting]
+	@intItemId AS INT
+	,@intItemLocationId AS INT
+	,@intItemUOMId AS INT
+	,@intSubLocationId AS INT
+	,@intStorageLocationId AS INT 
+	,@dtmDate AS DATETIME
+	,@dblQty AS NUMERIC(18,6)
+	,@dblUOMQty AS NUMERIC(18,6)
+	,@dblCost AS NUMERIC(38, 20)	
+	,@dblSalesPrice AS NUMERIC(18,6)
+	,@intCurrencyId AS INT
+	,@dblExchangeRate AS NUMERIC(18,6)
+	,@intTransactionId AS INT
+	,@intTransactionDetailId AS INT
+	,@strTransactionId AS NVARCHAR(20)
+	,@strBatchId AS NVARCHAR(20)
+	,@intTransactionTypeId AS INT
+	,@strTransactionForm AS NVARCHAR(255)
+	,@intEntityUserSecurityId AS INT
+AS
+
+SET QUOTED_IDENTIFIER OFF
+SET ANSI_NULLS ON
+SET NOCOUNT ON
+SET XACT_ABORT ON
+SET ANSI_WARNINGS OFF
+
+DECLARE @AVERAGECOST AS INT = 1
+		,@FIFO AS INT = 2
+		,@LIFO AS INT = 3
+		,@LOTCOST AS INT = 4
+		,@ACTUALCOST AS INT = 5
+
+-- Create the variables for the internal transaction types used by costing. 
+DECLARE @Inventory_Auto_Negative AS INT = 1;
+DECLARE @Inventory_Write_Off_Sold AS INT = 2;
+DECLARE @Inventory_Revalue_Sold AS INT = 3;
+
+-- Create the variables 
+DECLARE @RemainingQty AS NUMERIC(18,6);
+DECLARE @dblReduceQty AS NUMERIC(18,6);
+DECLARE @dblAddQty AS NUMERIC(18,6);
+DECLARE @CostUsed AS NUMERIC(18,6);
+DECLARE @FullQty AS NUMERIC(18,6);
+DECLARE @QtyOffset AS NUMERIC(18,6);
+DECLARE @TotalQtyOffset AS NUMERIC(18,6);
+
+DECLARE @InventoryTransactionIdentityId AS INT
+
+DECLARE @NewFifoId AS INT
+DECLARE @UpdatedFifoId AS INT 
+DECLARE @strRelatedTransactionId AS NVARCHAR(40)
+DECLARE @intRelatedTransactionId AS INT 
+DECLARE @dblValue AS NUMERIC(18,6)
+
+-------------------------------------------------
+-- 1. Process the Fifo Cost buckets
+-------------------------------------------------
+BEGIN 
+	-- Reduce stock 
+	IF (ISNULL(@dblQty, 0) < 0)
+	BEGIN 
+		SET @dblReduceQty = ISNULL(@dblQty, 0)
+
+		EXEC [dbo].[uspICPostInventoryTransaction]
+				@intItemId = @intItemId
+				,@intItemLocationId = @intItemLocationId
+				,@intItemUOMId = @intItemUOMId
+				,@intSubLocationId = @intSubLocationId
+				,@intStorageLocationId = @intStorageLocationId
+				,@dtmDate = @dtmDate
+				,@dblQty  = @dblQty
+				,@dblUOMQty = @dblUOMQty
+				,@dblCost = @dblCost
+				,@dblValue = NULL
+				,@dblSalesPrice = @dblSalesPrice
+				,@intCurrencyId = @intCurrencyId
+				,@dblExchangeRate = @dblExchangeRate
+				,@intTransactionId = @intTransactionId
+				,@intTransactionDetailId = @intTransactionDetailId
+				,@strTransactionId = @strTransactionId
+				,@strBatchId = @strBatchId
+				,@intTransactionTypeId = @intTransactionTypeId
+				,@intLotId = NULL 
+				,@intRelatedInventoryTransactionId = NULL 
+				,@intRelatedTransactionId = NULL 
+				,@strRelatedTransactionId = NULL 
+				,@strTransactionForm = @strTransactionForm
+				,@intEntityUserSecurityId = @intEntityUserSecurityId
+				,@intCostingMethod = @AVERAGECOST
+				,@InventoryTransactionIdentityId = @InventoryTransactionIdentityId OUTPUT
+
+		-- Repeat call on uspICReduceStockInFIFO until @dblReduceQty is completely distributed to all available fifo buckets 
+		-- If there is no avaiable fifo buckets, it will add a new negative bucket. 
+		WHILE (ISNULL(@dblReduceQty, 0) < 0)
+		BEGIN 
+			EXEC dbo.uspICReturnStockInFIFO
+				@intItemId
+				,@intItemLocationId
+				,@intItemUOMId
+				,@dtmDate
+				,@dblReduceQty
+				,@dblCost
+				,@strTransactionId
+				,@intTransactionId
+				,@intEntityUserSecurityId
+				,@RemainingQty OUTPUT
+				,@CostUsed OUTPUT 
+				,@QtyOffset OUTPUT 
+				,@UpdatedFifoId OUTPUT 
+			
+			-- Insert the record to the fifo-out table
+			INSERT INTO dbo.tblICInventoryFIFOOut (
+					intInventoryTransactionId
+					,intInventoryFIFOId
+					,dblQty
+			)
+			SELECT	intInventoryTransactionId = @InventoryTransactionIdentityId
+					,intInventoryFIFOId = @UpdatedFifoId
+					,dblQty = @QtyOffset
+			WHERE	@InventoryTransactionIdentityId IS NOT NULL
+					AND @UpdatedFifoId IS NOT NULL 
+					AND @QtyOffset IS NOT NULL 
+
+			-- Reduce the remaining qty
+			SET @dblReduceQty = @RemainingQty;
+		END 
+	END
+END 
