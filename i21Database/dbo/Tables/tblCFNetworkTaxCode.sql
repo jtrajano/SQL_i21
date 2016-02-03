7191cb4501d48545ea99e66fce0f9bf37f3411c7
bonn ponce <bonn.ponce@irely.com>
2016-02-03 08:56:35 +0800

    CF-301 Calc Transaction Rules: Remote Taxes

--- i21Database/dbo/Tables/tblCFNetworkTaxCode.sql
+++ i21Database/dbo/Tables/tblCFNetworkTaxCode.sql
@@ -1,9 +1,9 @@
 ﻿CREATE TABLE [dbo].[tblCFNetworkTaxCode] (
     [intNetworkTaxCodeId] INT            IDENTITY (1, 1) NOT NULL,
     [intNetworkId]        INT            NULL,
-    [strNetworkTaxCode]   NVARCHAR (MAX) NULL,
+    [strNetworkTaxCode]   NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NULL,
     [strDescription]      NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NULL,
-    [strState]            NVARCHAR (MAX) NULL,
+    [strState]            NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NULL,
     [intTaxCodeId]        INT            NULL,
     [intConcurrencyId]    INT            CONSTRAINT [DF_tblCFNetworkTaxCode_intConcurrencyId] DEFAULT ((1)) NULL,
     CONSTRAINT [PK_tblCFNetworkTaxCode] PRIMARY KEY CLUSTERED ([intNetworkTaxCodeId] ASC),
