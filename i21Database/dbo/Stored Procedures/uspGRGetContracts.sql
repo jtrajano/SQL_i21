ccd015741295b8623150b716ddf583b6a64308f6
Gopinath Nayak <gopinath.n@irely.com>
2016-02-04 13:25:08 +0530

    GRN-280

---
+++ i21Database/dbo/Stored Procedures/uspGRGetContracts.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROCEDURE [dbo].[uspGRGetContracts]
+	@strSearchCriteria NVARCHAR(MAX)
+AS
+BEGIN TRY
+	DECLARE @ErrMsg NVARCHAR(MAX)
+	DECLARE @sql NVARCHAR(MAX)	
+	
+	IF @strSearchCriteria <> ''
+		SET @sql='SELECT * FROM vyuCTContractDetailView WHERE '+@strSearchCriteria+' Order By intContractDetailId'
+	ELSE
+		SET @sql='SELECT * FROM vyuCTContractDetailView Order By intContractDetailId'
+
+	EXEC sp_executesql @sql
+			
+END TRY
+
+BEGIN CATCH
+	IF XACT_STATE() != 0 AND @@TRANCOUNT > 0 ROLLBACK TRANSACTION
+	SET @ErrMsg = ERROR_MESSAGE()
+	RAISERROR (@ErrMsg,16,1,'WITH NOWAIT')		
+END CATCH
