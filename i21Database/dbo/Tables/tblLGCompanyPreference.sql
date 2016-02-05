744a770ba17cf05eb34c0d102793b9e7320edfb8
James Balachander <balachander.j@irely.com>
2016-02-05 16:20:06 +0530

    LG-240

---
+++ i21Database/dbo/Tables/tblLGCompanyPreference.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[tblLGCompanyPreference]
+(
+[intCompanyPreferenceId] INT NOT NULL IDENTITY (1, 1),
+[intConcurrencyId] INT NOT NULL, 
+[intCommodityId] INT NULL,
+[intWeightUOMId] INT NULL,
+[ysnDropShip] [bit] NULL,
+[ysnAutoGenerateReleaseNo] [bit] NULL,
+
+CONSTRAINT [PK_tblLGCompanyPreference] PRIMARY KEY ([intCompanyPreferenceId]), 
+CONSTRAINT [FK_tblLGCompanyPreference_tblICCommodity_intCommodityId] FOREIGN KEY ([intCommodityId]) REFERENCES [tblICCommodity]([intCommodityId]),
+CONSTRAINT [FK_tblLGCompanyPreference_tblICUnitMeasure_intWeightUOMId] FOREIGN KEY ([intWeightUOMId]) REFERENCES [tblICUnitMeasure]([intUnitMeasureId])
+)
