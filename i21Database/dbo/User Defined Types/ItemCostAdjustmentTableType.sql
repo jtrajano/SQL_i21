2270e2bda895ed3fa2802f7bca773f10aadd2e26
Feb Montefrio <feb.montefrio@irely.com>
2016-02-09 11:32:29 +0800

    IC-1801

--- i21Database/dbo/User Defined Types/ItemCostAdjustmentTableType.sql
+++ i21Database/dbo/User Defined Types/ItemCostAdjustmentTableType.sql
@@ -12,7 +12,7 @@ CREATE TYPE [dbo].[ItemCostAdjustmentTableType] AS TABLE
 	,[dblUOMQty] NUMERIC(38, 20) NOT NULL DEFAULT 1			-- The quantity of an item per UOM. For example, a box can contain 12 individual pieces of an item. 
 	,[dblNewCost] NUMERIC(38, 20) NOT NULL DEFAULT 0		-- The cost of purchasing a item per UOM. For example, $12 is the cost for a 12-piece box. This parameter should hold a $12 value and not $1 per pieces found in a 12-piece box. The cost is stored in base currency. 
 	,[intCurrencyId] INT NULL								-- The currency id used in a transaction. 
-	,[dblExchangeRate] DECIMAL (38, 20) DEFAULT 1 NOT NULL	-- The exchange rate used in the transaction. It is used to convert the cost or sales price (both in base currency) to the foreign currency value.
+	,[dblExchangeRate] NUMERIC (38, 20) DEFAULT 1 NOT NULL	-- The exchange rate used in the transaction. It is used to convert the cost or sales price (both in base currency) to the foreign currency value.
 	,[intTransactionId] INT NOT NULL						-- The integer id of the source transaction (e.g. Sales Invoice, Inventory Adjustment id, etc. ). 
 	,[intTransactionDetailId] INT NULL						-- Link id to the transaction detail. 
 	,[strTransactionId] NVARCHAR(40) COLLATE Latin1_General_CI_AS NOT NULL -- The string id of the source transaction. 
