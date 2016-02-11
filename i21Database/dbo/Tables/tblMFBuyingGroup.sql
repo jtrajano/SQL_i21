5330d4dd7d2f8559e3e1e20e77d4bcab3f162702
Premkumar S <prem.selladurai@irely.com>
2016-02-11 13:51:06 +0530

    MFG-1323

---
+++ i21Database/dbo/Tables/tblMFBuyingGroup.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE dbo.tblMFBuyingGroup (
+	intBuyingGroupId INT IDENTITY(1, 1) NOT NULL
+	,strBuyingGroup NVARCHAR(50) NULL
+	,dtmCreated DATETIME NOT NULL
+	,intCreatedUserId INT NOT NULL
+	,dtmLastModified DATETIME NOT NULL
+	,intLastModifiedUserId INT NOT NULL
+	,intConcurrencyId INT
+	,CONSTRAINT PK_tblMFBuyingGroup_intBuyingGroupId PRIMARY KEY CLUSTERED (intBuyingGroupId ASC)
+	,CONSTRAINT UQ_tblMFBuyingGroup_strBuyingGroup UNIQUE NONCLUSTERED (strBuyingGroup ASC)
+	)
