be6c01a852c96a4a3766d94732ef76297e6fa643
Satya <ssahoo@BLRSATYAWIN7.irely.net>
2016-02-11 17:20:24 +0530

    CT-515

---
+++ i21Database/dbo/Functions/fnCTGetCleanUncleanWeight.sql
@@ -0,0 +1,31 @@
+﻿CREATE FUNCTION [dbo].[fnCTGetCleanUncleanWeight]
+(
+	@intInventoryReceiptId	INT
+)
+RETURNS @returntable	TABLE
+(
+	dblUncleanWeight	INT,
+	dblGrossWeight		INT
+)
+AS
+BEGIN
+
+	DECLARE @intCleanCostUOMId	INT
+
+	SELECT	@intCleanCostUOMId	= intCleanCostUOMId 
+	FROM	tblCTCompanyPreference
+	
+	INSERT	@returntable
+	(
+			dblUncleanWeight, 
+			dblGrossWeight
+	)
+	SELECT	SUM(dbo.fnCTConvertQuantityToTargetItemUOM(RI.intItemId,UM.intUnitMeasureId, @intCleanCostUOMId, RI.dblNet)) dblUncleanWeight,  
+			SUM(dbo.fnCTConvertQuantityToTargetItemUOM(RI.intItemId,UM.intUnitMeasureId, @intCleanCostUOMId, ISNULL(IL.dblGrossWeight,0) - ISNULL(dblTareWeight,0))) dblCleanWeight  
+	FROM	tblICInventoryReceiptItem		RI  
+	JOIN	tblICInventoryReceiptItemLot	IL ON IL.intInventoryReceiptItemId	=	RI.intInventoryReceiptItemId
+	JOIN	tblICItemUOM					UM ON UM.intItemUOMId				=	RI.intWeightUOMId  
+	WHERE	RI.intInventoryReceiptId = @intInventoryReceiptId ;
+		
+	RETURN;
+END
