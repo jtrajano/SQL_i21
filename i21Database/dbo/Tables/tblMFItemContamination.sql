3a20ae8dfbc76dd33959fb7ae1ef02aff9cfe276
Premkumar S <prem.selladurai@irely.com>
2016-02-16 13:46:39 +0530

    MFG-1279

---
+++ i21Database/dbo/Tables/tblMFItemContamination.sql
@@ -0,0 +1,18 @@
+﻿
+CREATE TABLE dbo.tblMFItemContamination (
+	intItemContaminationId INT IDENTITY(1, 1) NOT NULL
+	,intItemId INT NOT NULL
+	,intItemGroupId INT NOT NULL
+	,dtmCreated DATETIME NOT NULL
+	,intCreatedUserId INT NOT NULL
+	,dtmLastModified DATETIME NOT NULL
+	,intLastModifiedUserId INT NOT NULL
+	,intConcurrencyId INT
+	,CONSTRAINT PK_tblMFItemContamination_intItemContaminationId PRIMARY KEY (intItemContaminationId)
+	,CONSTRAINT UQ_tblMFItemContamination_intItemId_intItemGroupId UNIQUE (
+		intItemId
+		,intItemGroupId
+		)
+	,CONSTRAINT FK_tblMFItemContamination_tblICItem_intItemId FOREIGN KEY (intItemId) REFERENCES tblICItem(intItemId)
+	,CONSTRAINT FK_tblMFItemContamination_tblMFItemGroup_intItemGroupId FOREIGN KEY (intItemGroupId) REFERENCES tblMFItemGroup(intItemGroupId)
+	)
