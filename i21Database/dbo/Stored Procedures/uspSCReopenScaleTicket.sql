533c8664d6a8add1f553f5386cf916aa13d26d7c
Emerson Versoza <emerson.versoza@irely.com>
2016-02-17 16:42:52 +0800

    SC-1

---
+++ i21Database/dbo/Stored Procedures/uspSCReopenScaleTicket.sql
@@ -0,0 +1,39 @@
+﻿CREATE PROCEDURE [dbo].[uspSCReopenScaleTicket]
+	@intSourceId AS INT
+AS
+SET QUOTED_IDENTIFIER OFF
+SET ANSI_NULLS ON
+SET NOCOUNT ON
+SET XACT_ABORT ON
+SET ANSI_WARNINGS OFF
+
+DECLARE @ErrorMessage NVARCHAR(4000);
+DECLARE @ErrorSeverity INT;
+DECLARE @ErrorState INT;
+
+DECLARE @ItemsForItemReceipt AS ItemCostingTableType
+DECLARE @total AS INT
+DECLARE @ErrMsg NVARCHAR(MAX)
+DECLARE @strTransactionId NVARCHAR(40) = NULL
+
+BEGIN TRY
+	
+	UPDATE dbo.tblSCTicket SET strTicketStatus = 'R' WHERE intTicketId = @intSourceId;
+
+	_Exit:
+END TRY
+BEGIN CATCH
+	SELECT 
+		@ErrorMessage = ERROR_MESSAGE(),
+		@ErrorSeverity = ERROR_SEVERITY(),
+		@ErrorState = ERROR_STATE();
+
+	-- Use RAISERROR inside the CATCH block to return error
+	-- information about the original error that caused
+	-- execution to jump to the CATCH block.
+	RAISERROR (
+		@ErrorMessage, -- Message text.
+		@ErrorSeverity, -- Severity.
+		@ErrorState -- State.
+	);
+END CATCH
