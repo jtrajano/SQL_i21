a61f9b572326762f1127b409df4f6bba7cfd1615
Chakra <CBehera@BLRCHAKRAWIN10.irely.net>
2016-02-18 15:36:38 +0530

    MFG-1430

--- i21Database/dbo/Stored Procedures/uspMFPickListReport.sql
+++ i21Database/dbo/Stored Procedures/uspMFPickListReport.sql
@@ -52,6 +52,8 @@ Declare @strWorkOrderNo nvarchar(max)
 Declare @dblTotalPickQty numeric(18,6)
 Declare @intWorkOrderId int
 Declare @intMinRemainingItem int
+Declare @intBlendRequirementId int
+Declare @intKitStatusId int
 
 DECLARE @tblInputItem TABLE (
 	intRowNo INT IDENTITY,
@@ -62,8 +64,9 @@ DECLARE @tblInputItem TABLE (
 	,intParentItemId INT
 	)
 
-Declare @tblRemainingPickedLots AS table
+Declare @tblRemainingPickedItems AS table
 ( 
+	intRowNo INT IDENTITY,
 	intItemId int,
 	dblRemainingQuantity numeric(18,6),
 	intConsumptionMethodId int,
@@ -71,10 +74,41 @@ Declare @tblRemainingPickedLots AS table
 	intParentItemId int
 )
 
+Declare @tblRemainingPickedLots AS table
+( 
+	intWorkOrderInputLotId int,
+	intLotId int,
+	strLotNumber nvarchar(50) COLLATE Latin1_General_CI_AS,
+	strItemNo nvarchar(50) COLLATE Latin1_General_CI_AS,
+	strDescription nvarchar(200) COLLATE Latin1_General_CI_AS,
+	dblQuantity numeric(18,6),
+	intItemUOMId int,
+	strUOM nvarchar(50) COLLATE Latin1_General_CI_AS,
+	dblIssuedQuantity numeric(18,6),
+	intItemIssuedUOMId int,
+	strIssuedUOM nvarchar(50) COLLATE Latin1_General_CI_AS,
+	intItemId int,
+	intRecipeItemId int,
+	dblUnitCost numeric(18,6),
+	dblDensity numeric(18,6),
+	dblRequiredQtyPerSheet numeric(18,6),
+	dblWeightPerUnit numeric(18,6),
+	dblRiskScore numeric(18,6),
+	intStorageLocationId int,
+	strStorageLocationName nvarchar(50) COLLATE Latin1_General_CI_AS,
+	strLocationName nvarchar(50) COLLATE Latin1_General_CI_AS,
+	intLocationId int,
+	strSubLocationName nvarchar(50) COLLATE Latin1_General_CI_AS,
+	strLotAlias nvarchar(50) COLLATE Latin1_General_CI_AS,
+	ysnParentLot bit,
+	strRowState nvarchar(50) COLLATE Latin1_General_CI_AS
+)
+
 Select @intLocationId=intLocationId,@strPickListNo=strPickListNo,@strWorkOrderNo=strWorkOrderNo from tblMFPickList Where intPickListId=@intPickListId
-Select TOP 1 @intBlendItemId=w.intItemId,@strBlendItemNoDesc=(i.strItemNo + ' - '  + ISNULL(i.strDescription,'')),@intWorkOrderId=intWorkOrderId  
+Select TOP 1 @intBlendItemId=w.intItemId,@strBlendItemNoDesc=(i.strItemNo + ' - '  + ISNULL(i.strDescription,'')),@intWorkOrderId=intWorkOrderId,@intBlendRequirementId=intBlendRequirementId,@intKitStatusId=intKitStatusId 
 From tblMFWorkOrder w Join tblICItem i on w.intItemId=i.intItemId Where intPickListId=@intPickListId
 Select @dblQtyToProduce=SUM(dblQuantity) From tblMFWorkOrder Where intPickListId=@intPickListId
+Select @dblTotalPickQty=SUM(dblQuantity) From tblMFPickListDetail Where intPickListId=@intPickListId
 
 	SELECT @intRecipeId = intRecipeId
 	FROM tblMFWorkOrderRecipe
@@ -117,11 +151,15 @@ Select @dblQtyToProduce=SUM(dblQuantity) From tblMFWorkOrder Where intPickListId
 		AND rs.intRecipeItemTypeId = 1
 		AND r.intWorkOrderId = @intWorkOrderId
 
-	Insert Into @tblRemainingPickedLots(intItemId,dblRemainingQuantity,intConsumptionMethodId,ysnIsSubstitute,intParentItemId)
+	Insert Into @tblRemainingPickedItems(intItemId,dblRemainingQuantity,intConsumptionMethodId,ysnIsSubstitute,intParentItemId)
 	Select ti.intItemId,(ti.dblRequiredQty - ISNULL(tpl.dblQuantity,0)) AS dblRemainingQuantity,ti.intConsumptionMethodId,ti.ysnIsSubstitute,ti.intParentItemId 
 	From @tblInputItem ti Left Join 
 	(Select intItemId,SUM(dblQuantity) AS dblQuantity From tblMFPickListDetail Where intPickListId=@intPickListId Group by intItemId) tpl on  ti.intItemId=tpl.intItemId
 	WHERE ROUND((ti.dblRequiredQty - ISNULL(tpl.dblQuantity,0)),0) > 0
+
+	--intSalesOrderLineItemId = 0 implies WOs are created from Blend Managemnet Screen And Lots are already attached, keep only bulk items
+	If (Select TOP 1 ISNULL(intSalesOrderLineItemId,0) From tblMFWorkOrder Where intPickListId=@intPickListId)=0 OR @intKitStatusId=12
+		Delete From @tblRemainingPickedItems Where intConsumptionMethodId not in (2,3)
 
 	--Remove main item if substitute is selected
 	Select @intMinRemainingItem=Min(intRowNo) From @tblInputItem Where ysnIsSubstitute=1
@@ -132,15 +170,64 @@ Select @dblQtyToProduce=SUM(dblQuantity) From tblMFWorkOrder Where intPickListId
 		Select @intRemainingSubItemId=intItemId,@intRemainingParentItemId=intParentItemId From @tblInputItem Where intRowNo=@intMinRemainingItem
 		
 		If Exists (Select 1 From tblMFPickListDetail Where intPickListId=@intPickListId And intItemId=@intRemainingSubItemId)
-			Delete From @tblRemainingPickedLots Where intItemId=@intRemainingParentItemId
+			Delete From @tblRemainingPickedItems Where intItemId=@intRemainingParentItemId
 
 		Select @intMinRemainingItem=Min(intRowNo) From @tblInputItem Where intRowNo>@intMinRemainingItem And ysnIsSubstitute=1
 	End
 
 	--Remove sub item if main is selected
-	Delete a From @tblRemainingPickedLots a join tblMFPickListDetail b on a.intParentItemId=b.intItemId Where b.intPickListId=@intPickListId And a.ysnIsSubstitute=1
+	Delete a From @tblRemainingPickedItems a join tblMFPickListDetail b on a.intParentItemId=b.intItemId Where b.intPickListId=@intPickListId And a.ysnIsSubstitute=1
 
-	Select @dblTotalPickQty=SUM(dblQuantity) From tblMFPickListDetail Where intPickListId=@intPickListId
+	Declare @intMinItemCount int
+	Declare @strXml nvarchar(max)
+	Declare @intRawItemId int
+	Declare @dblRequiredQty numeric(18,6)
+	Declare @ysnIsSubstitute bit
+	Declare @intParentItemId int
+	Declare @intRecipeItemId int
+	Declare @intConsumptionMethodId int
+	Declare @intConsumptionStoragelocationId int
+
+	If (Select COUNT(1) From @tblRemainingPickedItems) > 0
+	Begin
+		Select @intMinItemCount=Min(intRowNo) from @tblRemainingPickedItems
+
+		Set @strXml = '<root>'
+		While(@intMinItemCount is not null)
+		Begin
+			Select @intRawItemId=intItemId,@dblRequiredQty=dblRemainingQuantity,@ysnIsSubstitute=ysnIsSubstitute,@intParentItemId=ISNULL(intParentItemId,0)
+			From @tblRemainingPickedItems Where intRowNo=@intMinItemCount
+
+			--WO created from Blend Management Screen if Lots are there input lot table when kitting enabled
+			If (Select TOP 1 ISNULL(intSalesOrderLineItemId,0) From tblMFWorkOrder Where intPickListId=@intPickListId)=0
+				Select @intRecipeItemId=ri.intRecipeItemId,@intConsumptionMethodId=ri.intConsumptionMethodId,@intConsumptionStoragelocationId=ri.intStorageLocationId 
+				From tblMFRecipe r Join tblMFRecipeItem ri on r.intRecipeId=ri.intRecipeId 
+				Where r.intRecipeId=@intRecipeId And ri.intItemId=@intRawItemId And r.intLocationId=@intLocationId And r.ysnActive=1
+			Else
+				Select @intRecipeItemId=ri.intRecipeItemId,@intConsumptionMethodId=ri.intConsumptionMethodId,@intConsumptionStoragelocationId=ri.intStorageLocationId 
+				From tblMFWorkOrderRecipe r Join tblMFWorkOrderRecipeItem ri on r.intWorkOrderId=ri.intWorkOrderId 
+				Where r.intRecipeId=@intRecipeId And ri.intItemId=@intRawItemId And r.intLocationId=@intLocationId And r.ysnActive=1 AND r.intWorkOrderId=@intWorkOrderId
+
+			Set @strXml = @strXml + '<item>'
+			Set @strXml = @strXml + '<intRecipeId>' + CONVERT(varchar,@intRecipeId) + '</intRecipeId>'
+			Set @strXml = @strXml + '<intRecipeItemId>' + CONVERT(varchar,@intRecipeItemId) + '</intRecipeItemId>'
+			Set @strXml = @strXml + '<intItemId>' + CONVERT(varchar,@intRawItemId) + '</intItemId>'
+			Set @strXml = @strXml + '<dblRequiredQty>' + CONVERT(varchar,@dblRequiredQty) + '</dblRequiredQty>'
+			Set @strXml = @strXml + '<ysnIsSubstitute>' + CONVERT(varchar,@ysnIsSubstitute) + '</ysnIsSubstitute>'
+			Set @strXml = @strXml + '<ysnMinorIngredient>' + CONVERT(varchar,0) + '</ysnMinorIngredient>'
+			Set @strXml = @strXml + '<intConsumptionMethodId>' + CONVERT(varchar,@intConsumptionMethodId) + '</intConsumptionMethodId>'
+			Set @strXml = @strXml + '<intConsumptionStoragelocationId>' + CONVERT(varchar,ISNULL(@intConsumptionStoragelocationId,0)) + '</intConsumptionStoragelocationId>'
+			Set @strXml = @strXml + '<intParentItemId>' + CONVERT(varchar,@intParentItemId) + '</intParentItemId>'
+			Set @strXml = @strXml + '</item>'
+
+			Select @intMinItemCount=Min(intRowNo) from @tblRemainingPickedItems Where intRowNo > @intMinItemCount
+		End
+		Set @strXml = @strXml + '</root>'
+
+		Insert Into @tblRemainingPickedLots
+		Exec uspMFAutoBlendSheetFIFO @intLocationId,@intBlendRequirementId,0,@strXml
+
+	End
 
 	SELECT distinct pl.strPickListNo,  
 			bi.strItemNo + ' - '  + ISNULL(bi.strDescription,'')  AS strBlendItemNoDesc,  
@@ -163,14 +250,16 @@ Select @dblQtyToProduce=SUM(dblQuantity) From tblMFWorkOrder Where intPickListId
 	JOIN tblMFWorkOrder w on w.intPickListId=pl.intPickListId
 	JOIN tblICItem bi on bi.intItemId = w.intItemId
 	JOIN tblICItem i on pld.intItemId=i.intItemId
-	JOIN tblICLot l on l.intLotId=pld.intLotId
-	JOIN tblICStorageLocation sl on sl.intStorageLocationId=pld.intStorageLocationId
+	JOIN tblICLot l on l.intLotId=pld.intStageLotId
+	JOIN tblICStorageLocation sl on sl.intStorageLocationId=l.intStorageLocationId
 	Join tblICItemUOM iu on pld.intPickUOMId=iu.intItemUOMId
 	Join tblICUnitMeasure um on iu.intUnitMeasureId=um.intUnitMeasureId
 	Join tblICParentLot p on l.intParentLotId=p.intParentLotId
 	WHERE pl.intPickListId=@intPickListId
 	UNION
-	Select @strPickListNo,@strBlendItemNoDesc,@strWorkOrderNo,'','','',i.strItemNo,i.strDescription,dbo.fnRemoveTrailingZeroes(tpl.dblRemainingQuantity),'','',
-	@intWorkOrderCount,'','',dbo.fnRemoveTrailingZeroes(@dblQtyToProduce) AS dblReqQty,dbo.fnRemoveTrailingZeroes(@dblTotalPickQty) AS dblTotalPickQty
+	Select @strPickListNo,@strBlendItemNoDesc,@strWorkOrderNo,tpl.strLotNumber,tpl.strLotAlias,tpl.strStorageLocationName,i.strItemNo,i.strDescription,dbo.fnRemoveTrailingZeroes(tpl.dblQuantity),tpl.strUOM,'',
+	@intWorkOrderCount,pl.strParentLotNumber,'',dbo.fnRemoveTrailingZeroes(@dblQtyToProduce) AS dblReqQty,dbo.fnRemoveTrailingZeroes(@dblTotalPickQty) AS dblTotalPickQty
 	From @tblRemainingPickedLots tpl Join tblICItem i on tpl.intItemId=i.intItemId
+	Join tblICLot l on tpl.intLotId=l.intLotId
+	Join tblICParentLot pl on l.intParentLotId=pl.intParentLotId
 	ORDER BY strPickUOM
