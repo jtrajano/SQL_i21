63e7d815ae2a4c93d79fa60d60c1cdacfbfcab10
Premkumar S <prem.selladurai@irely.com>
2016-02-18 17:42:29 +0530

    MFG-1434

--- i21Database/dbo/Stored Procedures/uspMFLotMove.sql
+++ i21Database/dbo/Stored Procedures/uspMFLotMove.sql
@@ -1,18 +1,14 @@
-﻿CREATE PROCEDURE [uspMFLotMove]
-				 @intLotId INT,       
-				 @intNewSubLocationId INT,  
-				 @intNewStorageLocationId INT,  
-				 @dblMoveQty NUMERIC(16,8),
-				 @intUserId INT
-
+﻿CREATE PROCEDURE [uspMFLotMove] @intLotId INT
+	,@intNewSubLocationId INT
+	,@intNewStorageLocationId INT
+	,@dblMoveQty NUMERIC(16, 8)
+	,@intUserId INT
 AS
-
 BEGIN TRY
-
 	DECLARE @intItemId INT
 	DECLARE @dtmDate DATETIME
 	DECLARE @intLocationId INT
-	DECLARE @intSubLocationId INT 
+	DECLARE @intSubLocationId INT
 	DECLARE @intStorageLocationId INT
 	DECLARE @strLotNumber NVARCHAR(50)
 	DECLARE @intSourceId INT
@@ -20,58 +16,93 @@ BEGIN TRY
 	DECLARE @intLotStatusId INT
 	DECLARE @intNewLocationId INT
 	DECLARE @strNewLotNumber NVARCHAR(50)
-	
 	DECLARE @intInventoryAdjustmentId INT
 	DECLARE @TransactionCount INT
 	DECLARE @ErrMsg NVARCHAR(MAX)
-	
-	
-	SELECT @intItemId = intItemId, 
-		   @intLocationId = intLocationId,
-		   @intSubLocationId = intSubLocationId,
-		   @intStorageLocationId = intStorageLocationId, 
-		   @strLotNumber = strLotNumber,
-		   @intLotStatusId = intLotStatusId,
-		   @intNewLocationId = intLocationId
-	FROM tblICLot WHERE intLotId = @intLotId
-	
-	SELECT @strNewLotNumber=@strLotNumber
+
+	SELECT @intItemId = intItemId
+		,@intLocationId = intLocationId
+		,@intSubLocationId = intSubLocationId
+		,@intStorageLocationId = intStorageLocationId
+		,@strLotNumber = strLotNumber
+		,@intLotStatusId = intLotStatusId
+		,@intNewLocationId = intLocationId
+	FROM tblICLot
+	WHERE intLotId = @intLotId
+
+	SELECT @strNewLotNumber = @strLotNumber
+
 	SELECT @dtmDate = GETDATE()
-	
-	SELECT @intSourceId = 1,@intSourceTransactionTypeId= 8
-	
-	IF ISNULL(@strLotNumber,'') = ''
+
+	SELECT @intSourceId = 1
+		,@intSourceTransactionTypeId = 8
+
+	IF ISNULL(@strLotNumber, '') = ''
 	BEGIN
-		RAISERROR(51192,11,1)
+		RAISERROR (
+				51192
+				,11
+				,1
+				)
 	END
-	
+
 	IF @intNewStorageLocationId = @intStorageLocationId
 	BEGIN
-		RAISERROR(51182,11,1)
+		RAISERROR (
+				51182
+				,11
+				,1
+				)
 	END
 
-	EXEC uspICInventoryAdjustment_CreatePostLotMove @intItemId,
-													@dtmDate,
-													@intLocationId,
-													@intSubLocationId,
-													@intStorageLocationId,
-													@strLotNumber,
-													@intNewLocationId,
-													@intNewSubLocationId,
-													@intNewStorageLocationId,
-													@strNewLotNumber,
-													@dblMoveQty,
-													@intSourceId,
-													@intSourceTransactionTypeId,
-													@intUserId,
-													@intInventoryAdjustmentId
+	EXEC uspICInventoryAdjustment_CreatePostLotMove @intItemId
+		,@dtmDate
+		,@intLocationId
+		,@intSubLocationId
+		,@intStorageLocationId
+		,@strLotNumber
+		,@intNewLocationId
+		,@intNewSubLocationId
+		,@intNewStorageLocationId
+		,@strNewLotNumber
+		,@dblMoveQty
+		,@intSourceId
+		,@intSourceTransactionTypeId
+		,@intUserId
+		,@intInventoryAdjustmentId
 
-END TRY  
-  
-BEGIN CATCH  
-  
- IF XACT_STATE() != 0 AND @TransactionCount = 0 AND @@TRANCOUNT > 0 ROLLBACK TRANSACTION  
- SET @ErrMsg = ERROR_MESSAGE()      
- RAISERROR(@ErrMsg, 16, 1, 'WITH NOWAIT')     
-  
-END CATCH 
+	IF EXISTS (
+			SELECT *
+			FROM dbo.tblMFWorkOrderProducedLot
+			WHERE intLotId = @intLotId
+				AND dblPhysicalCount = @dblMoveQty
+			)
+	BEGIN
+		DECLARE @intNewLotId INT
+
+		SELECT @intNewLotId = intLotId
+		FROM dbo.tblICLot
+		WHERE strLotNumber = @strNewLotNumber
+			AND intStorageLocationId = @intNewStorageLocationId
+
+		UPDATE dbo.tblMFWorkOrderProducedLot
+		SET intLotId = @intNewLotId
+		WHERE intLotId = @intLotId
+	END
+END TRY
+
+BEGIN CATCH
+	IF XACT_STATE() != 0
+		AND @TransactionCount = 0
+		AND @@TRANCOUNT > 0
+		ROLLBACK TRANSACTION
+
+	SET @ErrMsg = ERROR_MESSAGE()
+
+	RAISERROR (
+			@ErrMsg
+			,16
+			,1
+			,'WITH NOWAIT'
+			)
+END CATCH
