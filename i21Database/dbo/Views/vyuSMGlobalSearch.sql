4c2144f5d619a901534ce48f539b31d5d6f9bc0f
jesmar mendones <jesmar.mendones@irely.com>
2016-02-19 19:19:04 +0800

    FRM-2856

---
+++ i21Database/dbo/Views/vyuSMGlobalSearch.sql
@@ -0,0 +1,26 @@
+﻿CREATE VIEW [dbo].[vyuSMGlobalSearch] WITH SCHEMABINDING
+AS
+SELECT ROW_NUMBER() over(order by Id) as intGSIndexId, strNamespace, strDisplayTitle, strValueField, strValueData, strDisplayData, strTag
+FROM
+(
+	select 
+		'IC' + CONVERT(VARCHAR,[intItemId]) as Id,
+		'Inventory.view.Item' as strNamespace,
+		'Item' as strDisplayTitle,			
+		'intItemId' as strValueField,
+		item.intItemId as strValueData,
+		item.strItemNo as strDisplayData,			
+		item.strItemNo + ', ' + item.strType + ', ' + item.strDescription   as strTag
+	from [dbo].[tblICItem] as item		 		 
+	union
+	select 
+		'EM' + CONVERT(VARCHAR,[intEntityId]) as Id,
+		'EntityManagement.view.Entity' as strNamespace,
+		'Sample User' as strDisplayTitle,			
+		'intEntityId' as strValueField,
+		entity.intEntityId as strValueData,
+		entity.strName as strDisplayData,
+		entity.strName + ', ' + entity.strEntityNo as strTag
+	from [dbo].[tblEntity] as entity
+)
+as viewResult
