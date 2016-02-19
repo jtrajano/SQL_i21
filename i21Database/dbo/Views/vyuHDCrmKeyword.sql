942f88431431e8edcc683b5e49ce0cf558192a69
Jayson Ramos <jayson.ramos@iRely.com>
2016-02-19 13:33:14 +0800

    HD-1168

--- i21Database/dbo/Views/vyuHDCrmKeyword.sql
+++ i21Database/dbo/Views/vyuHDCrmKeyword.sql
@@ -7,75 +7,74 @@
 		,strDescription
 		,strCurrentValue
 		,ysnActive
-	from 
-		(
-			select
-				intEntityId = tblEntity.intEntityId
-				,strKeyword = '{Sales Person}'
-				,strDescription = 'Sales Person Name'
-				,strCurrentValue = tblEntity.strName
-				,ysnActive = (case when tblARSalesperson.ysnActive is null then convert(bit, 0) else tblARSalesperson.ysnActive end)
-			from
-				tblEntity
-				left outer join tblARSalesperson on tblARSalesperson.intEntitySalespersonId = tblEntity.intEntityId
+	from (
+		select
+			intEntityId = tblEntity.intEntityId
+			,strKeyword = '{Sales Person}'
+			,strDescription = 'Sales Person Name'
+			,strCurrentValue = tblEntity.strName
+			,ysnActive = (case when tblARSalesperson.ysnActive is null then convert(bit, 0) else tblARSalesperson.ysnActive end)
+		from
+			tblEntity
+			left outer join tblARSalesperson on tblARSalesperson.intEntitySalespersonId = tblEntity.intEntityId
 
-			union all
+		union all
 
-			select
-				intEntityId = tblEntity.intEntityId
-				,strKeyword = '{Company}'
-				,strDescription = 'Sales Person Company'
-				,strCurrentValue = (select top 1 tblSMCompanySetup.strCompanyName from tblSMCompanySetup)
-				,ysnActive = (case when tblARSalesperson.ysnActive is null then convert(bit, 0) else tblARSalesperson.ysnActive end)
-			from
-				tblEntity
-				left outer join tblARSalesperson on tblARSalesperson.intEntitySalespersonId = tblEntity.intEntityId
+		select
+			intEntityId = tblEntity.intEntityId
+			,strKeyword = '{Company}'
+			,strDescription = 'Sales Person Company'
+			,strCurrentValue = (select top 1 tblSMCompanySetup.strCompanyName from tblSMCompanySetup)
+			,ysnActive = (case when tblARSalesperson.ysnActive is null then convert(bit, 0) else tblARSalesperson.ysnActive end)
+		from
+			tblEntity
+			left outer join tblARSalesperson on tblARSalesperson.intEntitySalespersonId = tblEntity.intEntityId
 
-			union all
+		union all
 
-			select
-				intEntityId = tblEntity.intEntityId
-				,strKeyword = '{Enterprise Software Simplified}'
-				,strDescription = 'Enterprise Software Simplified'
-				,strCurrentValue = 'Enterprise Software Simplified'
-				,ysnActive = (case when tblARSalesperson.ysnActive is null then convert(bit, 0) else tblARSalesperson.ysnActive end)
-			from
-				tblEntity
-				left outer join tblARSalesperson on tblARSalesperson.intEntitySalespersonId = tblEntity.intEntityId
+		select
+			intEntityId = tblEntity.intEntityId
+			,strKeyword = '{Enterprise Software Simplified}'
+			,strDescription = 'Enterprise Software Simplified'
+			,strCurrentValue = 'Enterprise Software Simplified'
+			,ysnActive = (case when tblARSalesperson.ysnActive is null then convert(bit, 0) else tblARSalesperson.ysnActive end)
+		from
+			tblEntity
+			left outer join tblARSalesperson on tblARSalesperson.intEntitySalespersonId = tblEntity.intEntityId
 
-			union all
+		union all
 
-			select
-				intEntityId = tblEntity.intEntityId
-				,strKeyword = '{Phone}'
-				,strDescription = 'Sales Person Phone Number'
-				,strCurrentValue = tblEntity.strPhone
-				,ysnActive = (case when tblARSalesperson.ysnActive is null then convert(bit, 0) else tblARSalesperson.ysnActive end)
-			from
-				tblEntity
-				left outer join tblARSalesperson on tblARSalesperson.intEntitySalespersonId = tblEntity.intEntityId
+		select
+			intEntityId = tblEntity.intEntityId
+			,strKeyword = '{Phone}'
+			,strDescription = 'Sales Person Phone Number'
+			,strCurrentValue = (select top 1 e.strPhone from tblEntity e where e.intEntityId in (select ec.intEntityContactId from tblEntityToContact ec where ec.intEntityId = tblEntity.intEntityId))
+			,ysnActive = (case when tblARSalesperson.ysnActive is null then convert(bit, 0) else tblARSalesperson.ysnActive end)
+		from
+			tblEntity
+			left outer join tblARSalesperson on tblARSalesperson.intEntitySalespersonId = tblEntity.intEntityId
 
-			union all
+		union all
 
-			select
-				intEntityId = tblEntity.intEntityId
-				,strKeyword = '{Mobile}'
-				,strDescription = 'Sales Person Mobile Number'
-				,strCurrentValue = tblEntity.strMobile
-				,ysnActive = (case when tblARSalesperson.ysnActive is null then convert(bit, 0) else tblARSalesperson.ysnActive end)
-			from
-				tblEntity
-				left outer join tblARSalesperson on tblARSalesperson.intEntitySalespersonId = tblEntity.intEntityId
+		select
+			intEntityId = tblEntity.intEntityId
+			,strKeyword = '{Mobile}'
+			,strDescription = 'Sales Person Mobile Number'
+			,strCurrentValue = (select top 1 e.strMobile from tblEntity e where e.intEntityId in (select ec.intEntityContactId from tblEntityToContact ec where ec.intEntityId = tblEntity.intEntityId))
+			,ysnActive = (case when tblARSalesperson.ysnActive is null then convert(bit, 0) else tblARSalesperson.ysnActive end)
+		from
+			tblEntity
+			left outer join tblARSalesperson on tblARSalesperson.intEntitySalespersonId = tblEntity.intEntityId
 
-			union all
+		union all
 
-			select
-				intEntityId = tblEntity.intEntityId
-				,strKeyword = '{Email}'
-				,strDescription = 'Sales Person Email Address'
-				,strCurrentValue = tblEntity.strEmail
-				,ysnActive = (case when tblARSalesperson.ysnActive is null then convert(bit, 0) else tblARSalesperson.ysnActive end)
-			from
-				tblEntity
-				left outer join tblARSalesperson on tblARSalesperson.intEntitySalespersonId = tblEntity.intEntityId
-		) as keyword
+		select
+			intEntityId = tblEntity.intEntityId
+			,strKeyword = '{Email}'
+			,strDescription = 'Sales Person Email Address'
+			,strCurrentValue = (select top 1 e.strEmail from tblEntity e where e.intEntityId in (select ec.intEntityContactId from tblEntityToContact ec where ec.intEntityId = tblEntity.intEntityId))
+			,ysnActive = (case when tblARSalesperson.ysnActive is null then convert(bit, 0) else tblARSalesperson.ysnActive end)
+		from
+			tblEntity
+			left outer join tblARSalesperson on tblARSalesperson.intEntitySalespersonId = tblEntity.intEntityId
+			) as keyword
