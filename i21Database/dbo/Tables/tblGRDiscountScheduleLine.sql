404ac264d0e65615f88d5276457ea2bfab0d6995
Gopinath Nayak <gopinath.n@irely.com>
2016-02-21 17:37:57 +0530

    GRN-371

--- i21Database/dbo/Tables/tblGRDiscountScheduleLine.sql
+++ i21Database/dbo/Tables/tblGRDiscountScheduleLine.sql
@@ -2,11 +2,11 @@
 (
 	[intDiscountScheduleLineId] INT NOT NULL  IDENTITY, 
     [intDiscountScheduleCodeId] INT NOT NULL, 
-    [dblRangeStartingValue] NUMERIC(5, 3) NOT NULL, 
-    [dblRangeEndingValue] NUMERIC(5, 3) NOT NULL, 
-    [dblIncrementValue] NUMERIC(5, 3) NOT NULL, 
-    [dblDiscountValue] NUMERIC(8, 6) NOT NULL, 
-    [dblShrinkValue] NUMERIC(6, 4) NOT NULL, 
+    [dblRangeStartingValue] NUMERIC(24, 10) NOT NULL, 
+    [dblRangeEndingValue] NUMERIC(24, 10) NOT NULL, 
+    [dblIncrementValue] NUMERIC(24, 10) NOT NULL, 
+    [dblDiscountValue] NUMERIC(24, 10) NOT NULL, 
+    [dblShrinkValue] NUMERIC(24, 10) NOT NULL, 
     [intConcurrencyId] INT NOT NULL, 
     CONSTRAINT [PK_tblGRDiscountScheduleLine] PRIMARY KEY ([intDiscountScheduleLineId]), 
     CONSTRAINT [FK_tblGRDiscountScheduleLine_tblGRDiscountScheduleCode] FOREIGN KEY ([intDiscountScheduleCodeId]) REFERENCES [tblGRDiscountScheduleCode]([intDiscountScheduleCodeId]) 
