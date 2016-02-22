5ecf0987d2834b742ec245bab036ddaa75b7fa2c
jeffrey trajano <jeffrey.trajano@irely.com>
2016-02-22 11:31:33 +0800

    GL-2550 Retain Earning Account is displaying

--- i21Database/dbo/Functions/fnGLGetBeginningBalanceAndUnitRE.sql
+++ i21Database/dbo/Functions/fnGLGetBeginningBalanceAndUnitRE.sql
@@ -2,13 +2,14 @@
 (	
 	-- Add the parameters for the function here
 	@strAccountId NVARCHAR(100),
-	@dtmDate DATETIME
+	@dtmDate NVARCHAR(20) = ''
 )
 RETURNS TABLE 
 AS
 RETURN 
 (
 	-- Add the SELECT statement with parameter references here
+	
 	WITH cte as(
 	SELECT  
 			 @strAccountId AS strAccountId,
@@ -18,7 +19,7 @@ RETURN
 	FROM tblGLAccount A
 		LEFT JOIN tblGLAccountGroup B ON A.intAccountGroupId = B.intAccountGroupId
 		LEFT JOIN tblGLSummary C ON A.intAccountId = C.intAccountId
-	WHERE B.strAccountType in ('Expense','Revenue') and C.dtmDate < @dtmDate and strCode <> ''
+	WHERE B.strAccountType in ('Expense','Revenue') and C.dtmDate < (CASE WHEN @dtmDate= '' THEN '2100-01-01' ELSE @dtmDate END) and strCode <> ''
 	)
 	select sum(beginbalance) beginBalance ,sum(beginbalanceunit) beginBalanceUnit from cte group by strAccountId
 )
--- i21Database/dbo/Stored Procedures/uspGLGetAccountDetailReport.sql
+++ i21Database/dbo/Stored Procedures/uspGLGetAccountDetailReport.sql
@@ -226,10 +226,19 @@ GLAccountDetails A
 OUTER APPLY(SELECT * from GLAccountBalance  bal WHERE bal.intAccountId = A.intAccountId and bal.intGLDetailId = A.intGLDetailId) B
 OUTER APPLY(SELECT dblLbsPerUnit from Units u WHERE u.intAccountId = A.intAccountId) U
 OUTER APPLY(SELECT TOP 1 strCompanyName from tblSMCompanySetup) C
-WHERE ISNULL(A.ysnIsUnposted ,0) = 0 AND NOT EXISTS (SELECT TOP 1 1 FROM tblGLFiscalYear WHERE intRetainAccount = A.intAccountId))'
+WHERE ISNULL(A.ysnIsUnposted ,0) = 0 
+AND A.intAccountId NOT IN (SELECT DISTINCT(intRetainAccount) FROM tblGLFiscalYear)
+) '
 
-SELECT @sqlCte += ',cteBase as(
-	select * from RAWREPORT ' + CASE WHEN @Where <> 'Where' THEN  @Where END + ')'
+DELETE FROM @filterTable WHERE fieldname = 'dtmDate'
+DECLARE @Where1 NVARCHAR(MAX) = dbo.fnConvertFilterTableToWhereExpression (@filterTable)
+
+SELECT @sqlCte += dbo.fnGLGetRetainedEarningSQLString(@dtmDateFrom,'cteRetain2',@Where1)
+SELECT @sqlCte += ',cteBase1 as(SELECT * from RAWREPORT ' +  CASE WHEN @Where <> 'Where' THEN  @Where END +' )'-- UNION ALL SELECT ' + @cols + ' from cteRetain2 )'
+SELECT @sqlCte+= ',cteBase as (SELECT '+ @cols +' from cteBase1 UNION ALL SELECT ' + @cols + ' FROM  cteRetain2)'
+
+
+
 
 
 IF @dtmDateFrom <> ''
@@ -302,45 +311,7 @@ BEGIN
 		)'
 		
 	END
-	DECLARE @dte DATETIME 
-	DECLARE @dtmDateFrom1 NVARCHAR(10) = @dtmDateFrom
-	
-	SELECT TOP 1 @dte= dtmDateFrom from tblGLFiscalYear WHERE  CAST(@dtmDateFrom AS DATETIME) >= dtmDateFrom  and CAST(@dtmDateFrom AS DATETIME) <= dtmDateTo ORDER BY dtmDateFrom DESC
-	IF @dte IS NOT NULL
-	BEGIN
-		SELECT TOP 1 @dtmDateFrom =  convert(varchar(10), dtmDateFrom,101) ,@dtmDateTo = convert(varchar(20), dtmDateTo,101)  from tblGLFiscalYear WHERE  dtmDateTo = DATEADD(DAY,-1, @dte)
-	END
-	DECLARE @cols2 NVARCHAR (MAX) = 'strCompanyName,AccountHeader, strAccountDescription,strAccountType = '''',strAccountGroup = '''',dtmDate,strBatchId = '''',dblDebit,dblCredit,dblDebitUnit,dblCreditUnit,strDetailDescription='''',strTransactionId, intTransactionId = 0,strTransactionType = '''',strTransactionForm= '''',strModuleName='''',strReference ,strReferenceDetail='''',strDocument='''',dblTotal=dblCredit-dblDebit,intAccountUnitId = 0,strCode = '''',intGLDetailId= 0,ysnIsUnposted=0,strAccountId,[Primary Account]='''',Location='''',strUOMCode='''',dblBeginBalance,dblBeginBalanceUnit'
-	SET @cols2 = REPLACE(@cols2, 'AccountHeader', 'strAccountId + '' - '' + strAccountDescription + ''(Retained Earnings)'' as AccountHeader' )
-	SET @sqlCte +=
-	',cteRetain as(
-	select sum(dblDebit) dblDebit,sum(dblCredit) dblCredit,
-	sum(dblDebitUnit) dblDebitUnit, sum(dblCreditUnit) dblCreditUnit,
-	intAccountId, ysnIsUnposted, dtmDate from tblGLDetail group by intAccountId , ysnIsUnposted, dtmDate
-	having ysnIsUnposted = 0
-	and dtmDate between '''+ @dtmDateFrom + ''' and '''+ @dtmDateTo + '''),
-	cteRetain1 as
-	(select 
-	CAST(CONVERT(VARCHAR(4),YEAR(dtmDate)) + ''-'' +  CONVERT(VARCHAR(2), MONTH (dtmDate)) + ''-'' + ''1'' AS DATE) as dtmDate,
-	(select top 1 strAccountId from tblGLFiscalYear F join tblGLAccount A on F.intRetainAccount = A.intAccountId) as strAccountId,
-	(select top 1 strDescription from tblGLFiscalYear F join tblGLAccount A on F.intRetainAccount = A.intAccountId) as strAccountDescription,
-	DATENAME(YEAR,dtmDate) + ''-'' + CAST(MONTH(dtmDate) AS VARCHAR(4))  as strTransactionId,
-	DATENAME(MONTH,dtmDate) + '' '' +  DATENAME(YEAR,dtmDate) + '' '' +c.strAccountType as strReference,
-	D.beginBalance dblBeginBalance,
-	D.beginBalanceUnit dblBeginBalanceUnit, 
-	sum(dblDebit) dblDebit,
-	sum(dblCredit) dblCredit, 
-	sum(dblDebitUnit) dblDebitUnit,
-	sum(dblCreditUnit) dblCreditUnit,  
-	E.strCompanyName as strCompanyName,
-	c.strAccountType from cteRetain a
-	join tblGLAccount b on a.intAccountId = b.intAccountId
-	join tblGLAccountGroup c on b.intAccountGroupId = c.intAccountGroupId
-	OUTER APPLY dbo.fnGLGetBeginningBalanceAndUnitRE(b.strAccountId,''' + @dtmDateFrom1 + ''') D
-	OUTER APPLY (SELECT TOP 1 strCompanyName FROM tblSMCompanySetup) E
-	where c.strAccountType in (''Revenue'', ''Expense'') 
-	group by  DATENAME(MONTH,dtmDate) ,DATENAME(YEAR,dtmDate),MONTH(dtmDate) ,strAccountType, year(dtmDate),D.beginBalance,D.beginBalanceUnit,strCompanyName)
-	select ' + @cols2 + ' FROM cteRetain1 union all select ' + @cols1  + ' FROM cte1 union all select ' + @cols + ' from cteBase '
+	select @sqlCte +=	' select ' + @cols1  + ' FROM cte1 union all select ' + @cols + ' from cteBase '
 
 END
 ELSE
