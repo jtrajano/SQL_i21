a348243e04a29d3ad127018b3d1868d420dd517f
Jayson Ramos <jayson.ramos@iRely.com>
2016-02-22 15:42:32 +0800

    HD-1140

---
+++ i21Database/dbo/Views/vyuHDProjectSearchSummary.sql
@@ -0,0 +1,12 @@
+﻿CREATE VIEW [dbo].[vyuHDProjectSearchSummary]
+	AS
+		select
+			intId = ROW_NUMBER() over (order by strProjectType)
+			,strProjectType
+			,strCustomerName
+			,dblTotalNetOpportunityAmount = sum(dblNetOpportunityAmmount)
+		from
+			vyuHDProjectSearch
+		group by
+			strProjectType
+			,strCustomerName
