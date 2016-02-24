10b99fd02149061ceee4bfce55402adbf8cbd729
Satya <ssahoo@BLRSATYAWIN7.irely.net>
2016-02-24 22:10:01 +0530

    CT-279

--- i21Database/dbo/Stored Procedures/uspCTLoadReassign.sql
+++ i21Database/dbo/Stored Procedures/uspCTLoadReassign.sql
@@ -3,7 +3,7 @@
 		@intRecipientId	INT
 AS
 BEGIN
-
+	--Detail
 	SELECT	CH.intEntityId,			
 			CD.intContractDetailId,	
 			CD.intFutureMarketId,	
@@ -31,8 +31,8 @@ BEGIN
 	FROM	tblCTContractDetail	CD
 	JOIN	tblCTContractHeader	CH	ON	CH.intContractHeaderId	=	CD.intContractHeaderId	
 	JOIn	tblEntity			EY	ON	EY.intEntityId			=	CH.intEntityId				LEFT
-	JOIn	tblRKFutureMarket	MA	ON	MA.intFutureMarketId	=	CD.intFutureMarketId		LEFT
-	JOIn	tblRKFuturesMonth	MO	ON	MO.intFutureMonthId		=	CD.intFutureMonthId			LEFT
+	JOIN	tblRKFutureMarket	MA	ON	MA.intFutureMarketId	=	CD.intFutureMarketId		LEFT
+	JOIN	tblRKFuturesMonth	MO	ON	MO.intFutureMonthId		=	CD.intFutureMonthId			LEFT
 	JOIN	tblICItemUOM		PU	ON	PU.intItemUOMId			=	CD.intPriceItemUOMId		LEFT
 	JOIN	tblICUnitMeasure	PM	ON	PM.intUnitMeasureId		=	PU.intUnitMeasureId			LEFT
 	JOIN	tblCTPriceFixation	PF	ON	PF.intContractDetailId	=	CD.intContractDetailId		LEFT
@@ -58,4 +58,60 @@ BEGIN
 			)					SA	ON	SA.intSContractDetailId		=	CD.intContractDetailId
 	WHERE	CD.intContractDetailId	IN(@intDonorId,@intRecipientId)
 	
+	--Pricing
+
+	SELECT	PD.intPriceFixationDetailId,
+			PD.intFutureMarketId,
+			PD.intFutureMonthId,
+			PF.intFinalPriceUOMId intPriceUOMId,
+			PD.dblFutures AS dblPrice,
+			CAST(PD.intNoOfLots AS NUMERIC(18,6)) dblLot,
+			NULL AS dblReassign,
+			MA.strFutMarketName strMarketName,	
+			MO.strFutureMonth AS strMonth,	
+			PD.strTradeNo,
+			PM.strUnitMeasure strPriceUOM,
+			PD.intFutOptTransactionId,
+
+			--Dummy--
+			CAST(ROW_NUMBER() OVER (ORDER BY PF.intContractDetailId ASC) AS INT) * -1 AS intReassignPricingId,
+			0 AS intReassignId,
+			0 AS intConcurrencyId
+			---------
+
+	FROM	tblCTPriceFixationDetail	PD
+	JOIN	tblCTPriceFixation			PF	ON	PF.intPriceFixationId			=		PD.intPriceFixationId
+	JOIN	tblRKFutureMarket			MA	ON	MA.intFutureMarketId			=		PD.intFutureMarketId	LEFT
+	JOIN	tblRKFuturesMonth			MO	ON	MO.intFutureMonthId				=		PD.intFutureMonthId		LEFT
+	JOIN	tblICCommodityUnitMeasure	PU	ON	PU.intCommodityUnitMeasureId	=		PF.intFinalPriceUOMId	LEFT
+	JOIN	tblICUnitMeasure			PM	ON	PM.intUnitMeasureId				=		PU.intUnitMeasureId	
+	WHERE	PF.intContractDetailId	=	@intDonorId
+
+	--Futures
+
+	SELECT	SY.intFutOptTransactionId,
+			FO.intFutureMarketId,
+			FO.intFutureMonthId,
+			MA.intUnitMeasureId AS intPriceUOMId,
+			FO.dblPrice,
+			SY.dblAssignedLots + SY.intHedgedLots dblLot,
+			NULL AS dblReassign,
+			MA.strFutMarketName strMarketName,	
+			MO.strFutureMonth AS strMonth,	
+			strInternalTradeNo,
+			FO.strBuySell strTradeType,
+			UM.strUnitMeasure strPriceUOM,
+
+			--Dummy--
+			CAST(ROW_NUMBER() OVER (ORDER BY SY.intContractDetailId ASC) AS INT) * -1 AS intReassignFutureId,
+			0 AS intReassignId,
+			0 AS intConcurrencyId
+			---------
+
+	FROM	tblRKAssignFuturesToContractSummary	SY
+	JOIN	tblRKFutOptTransaction				FO	ON	FO.intFutOptTransactionId	=	SY.intFutOptTransactionId
+	JOIN	tblRKFutureMarket					MA	ON	MA.intFutureMarketId		=	FO.intFutureMarketId	
+	JOIN	tblRKFuturesMonth					MO	ON	MO.intFutureMonthId			=	FO.intFutureMonthId			
+	JOIN	tblICUnitMeasure					UM	ON	UM.intUnitMeasureId			=	MA.intUnitMeasureId
+	WHERE	SY.intContractDetailId	=	@intDonorId
 END
