2bd7ab24e5ef3211122aeb30b5436ae0d6e7a8c1
Maheshwari <mgawade@BLRMAHEWIN10.irely.net>
2016-02-25 23:48:28 +0530

    ST - 186 - Radiant Checkout Pull

--- i21Database/dbo/Stored Procedures/uspSTCheckoutRadiantFGM.sql
+++ i21Database/dbo/Stored Procedures/uspSTCheckoutRadiantFGM.sql
@@ -3,27 +3,47 @@
 AS
 BEGIN
 
-DECLARE @intStoreId Int
-Select @intStoreId = intStoreId from dbo.tblSTCheckoutHeader Where intCheckoutId = @intCheckoutId
+	DECLARE @intStoreId Int
+	Select @intStoreId = intStoreId from dbo.tblSTCheckoutHeader Where intCheckoutId = @intCheckoutId
 
-INSERT INTO dbo.tblSTCheckoutPumpTotals
-SELECT @intCheckoutId 
-, UOM.intItemUOMId [intPumpCardCouponId]
-, I.strDescription
-, (CAST(ISNULL(Chk.FuelGradeSalesAmount,0) as numeric(18,6))/ CAST(ISNULL(Chk.FuelGradeSalesVolume,1) as numeric(18,6))) [dblPrice]
-, ISNULL(Chk.FuelGradeSalesVolume, 0) [dblQuantity]
-, ISNULL(Chk.FuelGradeSalesAmount,0) [dblAmount]
-, 0
-from #tempCheckoutInsert Chk
-JOIN dbo.tblICItemLocation IL ON Chk.FuelGradeID COLLATE Latin1_General_CI_AS = CASE WHEN ISNULL(IL.strPassportFuelId1, '') <> '' THEN IL.strPassportFuelId1
-																					WHEN ISNULL(IL.strPassportFuelId2, '') <> '' THEN IL.strPassportFuelId2
-																					WHEN ISNULL(IL.strPassportFuelId3, '') <> '' THEN IL.strPassportFuelId3
-																				END
-JOIN dbo.tblICItem I ON I.intItemId = IL.intItemId
-JOIN dbo.tblICItemUOM UOM ON UOM.intItemId = I.intItemId
-JOIN dbo.tblSMCompanyLocation CL ON CL.intCompanyLocationId = IL.intLocationId
-JOIN dbo.tblSTStore S ON S.intCompanyLocationId = CL.intCompanyLocationId
-WHERE S.intStoreId = @intStoreId
+	IF NOT EXISTS (SELECT 1 FROM dbo.tblSTCheckoutPumpTotals Where intCheckoutId = @intCheckoutId)
+	BEGIN
+		INSERT INTO dbo.tblSTCheckoutPumpTotals
+		SELECT @intCheckoutId 
+		, UOM.intItemUOMId [intPumpCardCouponId]
+		, I.strDescription
+		, (CAST(ISNULL(Chk.FuelGradeSalesAmount,0) as numeric(18,6))/ CAST(ISNULL(Chk.FuelGradeSalesVolume,1) as numeric(18,6))) [dblPrice]
+		, ISNULL(Chk.FuelGradeSalesVolume, 0) [dblQuantity]
+		, ISNULL(Chk.FuelGradeSalesAmount,0) [dblAmount]
+		, 0
+		from #tempCheckoutInsert Chk
+		JOIN dbo.tblICItemLocation IL ON Chk.FuelGradeID COLLATE Latin1_General_CI_AS = CASE WHEN ISNULL(IL.strPassportFuelId1, '') <> '' THEN IL.strPassportFuelId1
+																							WHEN ISNULL(IL.strPassportFuelId2, '') <> '' THEN IL.strPassportFuelId2
+																							WHEN ISNULL(IL.strPassportFuelId3, '') <> '' THEN IL.strPassportFuelId3
+																						END
+		JOIN dbo.tblICItem I ON I.intItemId = IL.intItemId
+		JOIN dbo.tblICItemUOM UOM ON UOM.intItemId = I.intItemId
+		JOIN dbo.tblSMCompanyLocation CL ON CL.intCompanyLocationId = IL.intLocationId
+		JOIN dbo.tblSTStore S ON S.intCompanyLocationId = CL.intCompanyLocationId
+		WHERE S.intStoreId = @intStoreId
+	END
+	ELSE
+	BEGIN
+		UPDATE dbo.tblSTCheckoutPumpTotals
+		SET [dblPrice] = (CAST(ISNULL(Chk.FuelGradeSalesAmount,0) as numeric(18,6))/ CAST(ISNULL(Chk.FuelGradeSalesVolume,1) as numeric(18,6)))
+		, [dblQuantity] = ISNULL(Chk.FuelGradeSalesVolume, 0)
+		, [dblAmount] = ISNULL(Chk.FuelGradeSalesAmount,0)
+		from #tempCheckoutInsert Chk
+		JOIN dbo.tblICItemLocation IL ON Chk.FuelGradeID COLLATE Latin1_General_CI_AS = CASE WHEN ISNULL(IL.strPassportFuelId1, '') <> '' THEN IL.strPassportFuelId1
+																							WHEN ISNULL(IL.strPassportFuelId2, '') <> '' THEN IL.strPassportFuelId2
+																							WHEN ISNULL(IL.strPassportFuelId3, '') <> '' THEN IL.strPassportFuelId3
+																						END
+		JOIN dbo.tblICItem I ON I.intItemId = IL.intItemId
+		JOIN dbo.tblICItemUOM UOM ON UOM.intItemId = I.intItemId
+		JOIN dbo.tblSMCompanyLocation CL ON CL.intCompanyLocationId = IL.intLocationId
+		JOIN dbo.tblSTStore S ON S.intCompanyLocationId = CL.intCompanyLocationId
+		WHERE intCheckoutId = @intCheckoutId
+	END
 
 END
 
--- i21Database/dbo/Stored Procedures/uspSTCheckoutRadiantMCM.sql
+++ i21Database/dbo/Stored Procedures/uspSTCheckoutRadiantMCM.sql
@@ -6,36 +6,56 @@ BEGIN
 DECLARE @intStoreId Int
 Select @intStoreId = intStoreId from dbo.tblSTCheckoutHeader Where intCheckoutId = @intCheckoutId
 
-INSERT INTO dbo.tblSTCheckoutDepartmetTotals
-SELECT @intCheckoutId 
-, Cat.intCategoryId
-, ISNULL(Chk.SalesQuantity,0) [intTotalSalesCount]
-, (CASE WHEN (S.strReportDepartmentAtGrossOrNet) = 'G' 
-	THEN ISNULL(Chk.SalesAmount,0) + ISNULL(Chk.DiscountAmount,0) + ISNULL(Chk.PromotionAmount,0) + ISNULL(Chk.RefundAmount,0)   
-	WHEN (S.strReportDepartmentAtGrossOrNet) = 'N' 
-	THEN ISNULL(Chk.SalesAmount,0)
-   END) [dblTotalSalesAmount]
-, 0 [dblRegisterSalesAmount]
-, '' [strDepartmentTotalsComment]
-, Chk.PromotionCount [intPromotionalDiscountsCount]
-, Chk.PromotionAmount [dblPromotionalDiscountAmount]
-, Chk.DiscountCount [intManagerDiscountCount]
-, Chk.DiscountAmount [dblManagerDiscountAmount]
-, Chk.RefundCount [intRefundCount]
-, Chk.RefundAmount [dblRefundAmount]
-, Chk.TransactionCount [intItemsSold]
-, 0 [dblTaxAmount1]
-, 0 [dblTaxAmount2]
-, 0 [dblTaxAmount3]
-, 0 [dblTaxAmount4]
-, 1 [intConcurrencyId]
-from #tempCheckoutInsert Chk
-JOIN dbo.tblICCategory Cat ON Cat.strCategoryCode = CASE WHEN LEN(Chk.MerchandiseCode COLLATE Latin1_General_CI_AS)<=4 THEN Chk.MerchandiseCode COLLATE Latin1_General_CI_AS ELSE SUBSTRING(Chk.MerchandiseCode COLLATE Latin1_General_CI_AS, 0, 4) END
-JOIN dbo.tblICItem I ON I.intCategoryId = Cat.intCategoryId
-JOIN dbo.tblICItemLocation IL ON IL.intItemId = I.intItemId
-JOIN dbo.tblSMCompanyLocation CL ON CL.intCompanyLocationId = IL.intLocationId
-JOIN dbo.tblSTStore S ON S.intCompanyLocationId = CL.intCompanyLocationId
-WHERE S.intStoreId = @intStoreId
+IF NOT EXISTS(SELECT 1 FROM dbo.tblSTCheckoutDepartmetTotals WHERE intCheckoutId = @intCheckoutId)
+BEGIN
+	INSERT INTO dbo.tblSTCheckoutDepartmetTotals
+	SELECT @intCheckoutId 
+	, Cat.intCategoryId
+	, ISNULL(Chk.SalesQuantity,0) [intTotalSalesCount]
+	, (CASE WHEN (S.strReportDepartmentAtGrossOrNet) = 'G' 
+		THEN ISNULL(Chk.SalesAmount,0) + ISNULL(Chk.DiscountAmount,0) + ISNULL(Chk.PromotionAmount,0) + ISNULL(Chk.RefundAmount,0)   
+		WHEN (S.strReportDepartmentAtGrossOrNet) = 'N' 
+		THEN ISNULL(Chk.SalesAmount,0)
+	   END) [dblTotalSalesAmount]
+	, 0 [dblRegisterSalesAmount]
+	, '' [strDepartmentTotalsComment]
+	, Chk.PromotionCount [intPromotionalDiscountsCount]
+	, Chk.PromotionAmount [dblPromotionalDiscountAmount]
+	, Chk.DiscountCount [intManagerDiscountCount]
+	, Chk.DiscountAmount [dblManagerDiscountAmount]
+	, Chk.RefundCount [intRefundCount]
+	, Chk.RefundAmount [dblRefundAmount]
+	, Chk.TransactionCount [intItemsSold]
+	, 0 [dblTaxAmount1]
+	, 0 [dblTaxAmount2]
+	, 0 [dblTaxAmount3]
+	, 0 [dblTaxAmount4]
+	, 1 [intConcurrencyId]
+	from #tempCheckoutInsert Chk
+	JOIN dbo.tblICCategory Cat ON Cat.strCategoryCode = CASE WHEN LEN(Chk.MerchandiseCode COLLATE Latin1_General_CI_AS)<=4 THEN Chk.MerchandiseCode COLLATE Latin1_General_CI_AS ELSE SUBSTRING(Chk.MerchandiseCode COLLATE Latin1_General_CI_AS, 0, 4) END
+	JOIN dbo.tblICItem I ON I.intCategoryId = Cat.intCategoryId
+	JOIN dbo.tblICItemLocation IL ON IL.intItemId = I.intItemId
+	JOIN dbo.tblSMCompanyLocation CL ON CL.intCompanyLocationId = IL.intLocationId
+	JOIN dbo.tblSTStore S ON S.intCompanyLocationId = CL.intCompanyLocationId
+	WHERE S.intStoreId = @intStoreId
+END
+ELSE
+BEGIN
+	UPDATE dbo.tblSTCheckoutDepartmetTotals
+	SET intTotalSalesCount = ISNULL(Chk.SalesQuantity,0),
+	dblTotalSalesAmount = (CASE WHEN (S.strReportDepartmentAtGrossOrNet) = 'G' 
+		THEN ISNULL(Chk.SalesAmount,0) + ISNULL(Chk.DiscountAmount,0) + ISNULL(Chk.PromotionAmount,0) + ISNULL(Chk.RefundAmount,0)   
+		WHEN (S.strReportDepartmentAtGrossOrNet) = 'N' 
+		THEN ISNULL(Chk.SalesAmount,0)
+	   END)
+	FROM #tempCheckoutInsert Chk
+	JOIN dbo.tblICCategory Cat ON Cat.strCategoryCode = CASE WHEN LEN(Chk.MerchandiseCode COLLATE Latin1_General_CI_AS)<=4 THEN Chk.MerchandiseCode COLLATE Latin1_General_CI_AS ELSE SUBSTRING(Chk.MerchandiseCode COLLATE Latin1_General_CI_AS, 0, 4) END
+	JOIN dbo.tblICItem I ON I.intCategoryId = Cat.intCategoryId
+	JOIN dbo.tblICItemLocation IL ON IL.intItemId = I.intItemId
+	JOIN dbo.tblSMCompanyLocation CL ON CL.intCompanyLocationId = IL.intLocationId
+	JOIN dbo.tblSTStore S ON S.intCompanyLocationId = CL.intCompanyLocationId
+	WHERE intCheckoutId = @intCheckoutId
+END
 
 UPDATE dbo.tblSTCheckoutDepartmetTotals SET dblRegisterSalesAmount = dblTotalSalesAmount Where intCheckoutId = @intCheckoutId
 
