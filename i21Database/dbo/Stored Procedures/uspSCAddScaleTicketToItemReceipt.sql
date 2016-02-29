299b33c74d4602ae02ef207446410721bef09136
Emerson Versoza <emerson.versoza@irely.com>
2016-02-29 13:01:29 +0800

    SC-65

--- i21Database/dbo/Stored Procedures/uspSCAddScaleTicketToItemReceipt.sql
+++ i21Database/dbo/Stored Procedures/uspSCAddScaleTicketToItemReceipt.sql
@@ -32,7 +32,6 @@ DECLARE @dblTicketFees AS DECIMAL(7, 2)
 DECLARE @intFeeItemId AS INT
 DECLARE @checkContract AS INT
 
-
 BEGIN 
 	SELECT	@intTicketUOM = UOM.intUnitMeasureId
 	FROM	dbo.tblSCTicket SC	        
@@ -83,40 +82,6 @@ END
 
 -- Insert Entries to Stagging table that needs to processed to Transport Load
 INSERT into @ReceiptStagingTable(
-		--strReceiptType
-		--,intEntityVendorId
-		--,intShipFromId
-		--,intLocationId
-		--,intItemId
-		--,intItemLocationId
-		--,intItemUOMId
-		--,strBillOfLadding
-		--,intContractHeaderId
-		--,intContractDetailId
-		--,dtmDate
-		--,intShipViaId
-		--,dblQty
-		--,dblCost
-		--,intCurrencyId
-		--,dblExchangeRate
-		--,intLotId
-		--,intSubLocationId
-		--,intStorageLocationId
-		--,ysnIsStorage
-		--,dblFreightRate
-		--,intSourceId	
-		--,intSourceType		 	
-		--,dblGross
-		--,dblNet
-		--,intInventoryReceiptId
-		--,dblSurcharge
-		--,ysnFreightInPrice
-		--,strActualCostId
-		----,intTaxGroupId
-		--,strVendorRefNo
-		--,strSourceId
-		--,strSourceScreenName
-
 		-- Header
 		strReceiptType
 		,intEntityVendorId
@@ -328,6 +293,68 @@ EXEC dbo.uspICAddItemReceipt
 		,@OtherCharges
 		,@intEntityId;
 
+		IF EXISTS(SELECT * FROM #tmpAddItemReceiptResult)
+		BEGIN
+			SELECT TOP 1 @InventoryReceiptId = intInventoryReceiptId FROM  #tmpAddItemReceiptResult	
+
+			DECLARE @intInventoryReceiptItemId	INT = NULL,
+					@dblQty						NUMERIC(18,6) = 0
+
+			SELECT	@intInventoryReceiptItemId = MIN(intInventoryReceiptItemId) 
+			FROM	tblICInventoryReceiptItem
+			WHERE	intInventoryReceiptId = @InventoryReceiptId
+
+			WHILE ISNULL(@intInventoryReceiptItemId,0) > 0
+			BEGIN
+				SELECT	@dblQty						=	dblOpenReceive
+				FROM	tblICInventoryReceiptItem 
+				WHERE	intInventoryReceiptItemId	=	 @intInventoryReceiptItemId
+				
+				DECLARE @intLoopContractDetailId INT;
+				DECLARE @dblLoopContractUnits NUMERIC(12,4);
+				DECLARE intListCursor CURSOR LOCAL FAST_FORWARD
+				FOR
+				SELECT intTransactionDetailId, dblQty
+				FROM @Items;
+
+				OPEN intListCursor;
+
+				-- Initial fetch attempt
+				FETCH NEXT FROM intListCursor INTO @intLoopContractDetailId, @dblLoopContractUnits;
+
+				WHILE @@FETCH_STATUS = 0
+				BEGIN
+				   -- Here we do some kind of action that requires us to 
+				   -- process the table variable row-by-row. This example simply
+				   -- uses a PRINT statement as that action (not a very good
+				   -- example).
+					IF	ISNULL(@intLoopContractDetailId,0) != 0
+				   	--DEALLOCATE intListCursor;
+					EXEC	uspCTUpdateScheduleQuantity
+							@intContractDetailId	=	@intLoopContractDetailId,
+							@dblQuantityToUpdate	=	@dblQty,
+							@intUserId				=	@intUserId,
+							@intExternalId			=	@intInventoryReceiptItemId,
+							@strScreenName			=	'Inventory Receipt'
+				   
+				   -- Attempt to fetch next row from cursor
+				   FETCH NEXT FROM intListCursor INTO @intLoopContractDetailId, @dblLoopContractUnits;
+				END;
+				CLOSE intListCursor;
+				DEALLOCATE intListCursor;
+
+				SELECT	@intInventoryReceiptItemId = MIN(intInventoryReceiptItemId) 
+				FROM	tblICInventoryReceiptItem
+				WHERE	intInventoryReceiptId = @InventoryReceiptId	AND
+						intInventoryReceiptItemId > @intInventoryReceiptItemId
+			END
+
+			SELECT	@InventoryReceiptId
+		END
+		ELSE
+		BEGIN
+			SELECT 0
+		END
 -- Update the Inventory Receipt Key to the Transaction Table
 UPDATE	SC
 SET		SC.intInventoryReceiptId = addResult.intInventoryReceiptId
@@ -363,282 +390,6 @@ BEGIN
 	DELETE	FROM #tmpAddItemReceiptResult 
 	WHERE	intInventoryReceiptId = @ReceiptId
 END;
-
---OLD INSERTING
-/*
--- Insert the Inventory Receipt header 
-INSERT INTO dbo.tblICInventoryReceipt (
-		strReceiptNumber
-		,dtmReceiptDate
-		,intEntityVendorId
-		,strReceiptType
-		,intBlanketRelease
-		,intLocationId
-		,strVendorRefNo
-		,strBillOfLading
-		,intShipViaId
-		,intShipFromId
-		,intReceiverId
-		,intCurrencyId
-		,strVessel
-		,intFreightTermId
-		,intShiftNumber
-		,dblInvoiceAmount
-		,ysnInvoicePaid
-		,intCheckNo
-		,dtmCheckDate
-		,intTrailerTypeId
-		,dtmTrailerArrivalDate
-		,dtmTrailerArrivalTime
-		,strSealNo
-		,strSealStatus
-		,dtmReceiveTime
-		,dblActualTempReading
-		,intConcurrencyId
-		,intEntityId
-		,intCreatedUserId
-		,ysnPosted
-		,intSourceType
-)
-SELECT 	strReceiptNumber		= @ReceiptNumber
-		,dtmReceiptDate			= dbo.fnRemoveTimeOnDate(GETDATE())
-		,intEntityVendorId		= @intEntityId
-		,strReceiptType			= @strReceiptType
-		,intBlanketRelease		= NULL
-		,intLocationId			= SC.intProcessingLocationId
-		,strVendorRefNo			= SC.strCustomerReference
-		,strBillOfLading		= NULL
-		,intShipViaId			= SC.intFreightCarrierId
-		,intShipFromId			= NULL 
-		,intReceiverId			= @intUserId 
-		,intCurrencyId			= SC.intCurrencyId
-		,strVessel				= SC.strTruckName
-		,intFreightTermId		= NULL
-		,intShiftNumber			= NULL 
-		,dblInvoiceAmount		= 0
-		,ysnInvoicePaid			= 0 
-		,intCheckNo				= NULL 
-		,dteCheckDate			= NULL 
-		,intTrailerTypeId		= NULL 
-		,dteTrailerArrivalDate	= NULL 
-		,dteTrailerArrivalTime	= NULL 
-		,strSealNo				= NULL 
-		,strSealStatus			= NULL 
-		,dteReceiveTime			= NULL 
-		,dblActualTempReading	= NULL 
-		,intConcurrencyId		= 1
-		,intEntityId			= (SELECT TOP 1 [intEntityUserSecurityId] FROM dbo.tblSMUserSecurity WHERE [intEntityUserSecurityId] = @intUserId)
-		,intCreatedUserId		= @intUserId
-		,ysnPosted				= 0
-		,intSourceType          = 1
-FROM	dbo.tblSCTicket SC
-WHERE	SC.intTicketId = @intTicketId
-
--- Get the identity value from tblICInventoryReceipt
-SELECT @InventoryReceiptId = SCOPE_IDENTITY()
-
-IF @InventoryReceiptId IS NULL 
-BEGIN 
-	-- Raise the error:
-	-- Unable to generate the Inventory Receipt. An error stopped the process from Purchase Order to Inventory Receipt.
-	RAISERROR(80004, 11, 1);
-	RETURN;
-END
-
-INSERT INTO dbo.tblICInventoryReceiptItem (
-	intInventoryReceiptId
-    ,intLineNo
-	,intOrderId
-	,intSourceId
-    ,intItemId
-	,intSubLocationId
-	,dblOrderQty
-	,dblOpenReceive
-	,dblReceived
-    ,intUnitMeasureId
-	,intWeightUOMId
-    ,dblUnitCost
-	,dblLineTotal
-    ,intSort
-    ,intConcurrencyId
-	,intOwnershipType
-	,dblGross
-	,dblNet
-)
-SELECT	intInventoryReceiptId	= @InventoryReceiptId
-		,intLineNo				= ISNULL (LI.intTransactionDetailId, 1)
-		,intOrderId				= CNT.intContractHeaderId
-		,intSourceId			= @intTicketId
-		,intItemId				= SC.intItemId
-		,intSubLocationId		= SC.intSubLocationId
-		,dblOrderQty			= LI.dblQty
-		,dblOpenReceive			= LI.dblQty
-		,dblReceived			= LI.dblQty
-		,intUnitMeasureId		= ItemUOM.intItemUOMId
-		,intWeightUOMId			=	(
-										SELECT	TOP 1 
-												tblICItemUOM.intItemUOMId 
-										FROM	dbo.tblICItemUOM INNER JOIN dbo.tblICUnitMeasure
-													ON tblICItemUOM.intUnitMeasureId = tblICUnitMeasure.intUnitMeasureId
-										WHERE	tblICItemUOM.intItemId = SC.intItemId 
-												AND tblICItemUOM.ysnStockUnit = 1 
-												AND tblICUnitMeasure.strUnitType = 'Weight'
-												AND dbo.fnGetItemLotType(SC.intItemId) IN (1,2)
-									)
-		--,dblUnitCost			= LI.dblCost
-		,dblUnitCost			= SC.dblUnitPrice + SC.dblUnitBasis
-		,dblLineTotal			= LI.dblQty * LI.dblCost
-		,intSort				= 1
-		,intConcurrencyId		= 1
-		,intOwnershipType       = CASE
-								  WHEN LI.ysnIsStorage = 0
-								  THEN 1
-								  WHEN LI.ysnIsStorage = 1
-								  THEN 2
-								  END
-		,dblGros				= SC.dblGross
-		,dblNet					= SC.dblGross - dblShrink
-FROM	@Items LI INNER JOIN dbo.tblSCTicket SC ON SC.intTicketId = LI.intTransactionId INNER JOIN dbo.tblICItemUOM ItemUOM			
-			ON ItemUOM.intItemId = SC.intItemId
-			AND ItemUOM.intItemUOMId = @intTicketItemUOMId
-		INNER JOIN dbo.tblICUnitMeasure UOM
-			ON ItemUOM.intUnitMeasureId = UOM.intUnitMeasureId
-			LEFT JOIN dbo.tblCTContractDetail CNT
-			ON CNT.intContractDetailId = LI.intTransactionDetailId
-WHERE	SC.intTicketId = @intTicketId
-
-INSERT INTO tblICInventoryReceiptCharge
-(
-		intInventoryReceiptId,
-		intChargeId,
-		ysnInventoryCost,
-		strCostMethod,
-		dblRate,
-		intCostUOMId,
-		intEntityVendorId,
-		dblAmount,
-		strAllocateCostBy,
-		ysnAccrue,
-		intSort,
-		intConcurrencyId
-)
-SELECT	@InventoryReceiptId, 
-		SC.intItemId,
-		0,
-		SC.strCostMethod,
-		SUM(SC.dblRate),
-		SC.intItemUOMId,
-		SC.intEntityVendorId,
-		NULL,
-		NULL,
-		1,
-		NULL,
-		1
-FROM	tblSCTicketCost SC
-WHERE SC.intTicketId = @intTicketId
-GROUP 
-BY		SC.intItemId,
-		SC.intEntityVendorId,
-		SC.strCostMethod,
-		SC.dblRate,
-		SC.intItemUOMId
-
-IF (@dblTicketFreightRate > 0 AND (@intFreightVendorId != null OR @ysnDeductFreightFarmer = 1))
-BEGIN
-SELECT	@intFreightItemId = ST.intFreightItemId
-FROM	dbo.tblSCScaleSetup ST	        
-WHERE	ST.intScaleSetupId = @intScaleStationId
-IF @intFreightItemId IS NULL 
-BEGIN 
-	-- Raise the error:
-	RAISERROR('Invalid Default Freight Item in Scale Setup - uspSCProcessToItemReceipt', 16, 1);
-	RETURN;
-END
-INSERT INTO tblICInventoryReceiptCharge
-(
-		intInventoryReceiptId,
-		intChargeId,
-		ysnInventoryCost,
-		strCostMethod,
-		dblRate,
-		intCostUOMId,
-		intEntityVendorId,
-		dblAmount,
-		strAllocateCostBy,
-		ysnAccrue,
-		intSort,
-		intConcurrencyId
-)
-SELECT	
-		intInventoryReceiptId	= @InventoryReceiptId
-		,intChargeId			= @intFreightItemId
-		,ysnInventoryCost		= 0
-		,strCostMethod			= 'Per Unit'
-		,dblRate				= @dblTicketFreightRate
-		,intCostUOMId			= @intTicketItemUOMId
-		,intEntityVendorId		= CASE	WHEN SS.ysnFarmerPaysFreight = 1 THEN NULL
-										WHEN SS.ysnFarmerPaysFreight = 0 THEN SS.intFreightCarrierId
-								END
-		,dblAmount				= NULL
-		,strAllocateCostBy		= NULL
-		,ysnAccrue				= 1
-		,intSort				= NULL 
-		,intConcurrencyId		= 1
-FROM	tblSCTicket SS WHERE SS.intTicketId = @intTicketId
-END
-
-IF @dblTicketFees > 0
-BEGIN
-SELECT	@intFeeItemId = ST.intDefaultFeeItemId
-FROM	dbo.tblSCScaleSetup ST	        
-WHERE	ST.intScaleSetupId = @intScaleStationId
-IF @intFeeItemId IS NULL 
-BEGIN 
-	-- Raise the error:
-	RAISERROR('Invalid Default Fee Item in Scale Setup - uspSCProcessToItemReceipt', 16, 1);
-	RETURN;
-END
-INSERT INTO tblICInventoryReceiptCharge
-(
-		intInventoryReceiptId,
-		intChargeId,
-		ysnInventoryCost,
-		strCostMethod,
-		dblRate,
-		intCostUOMId,
-		intEntityVendorId,
-		dblAmount,
-		strAllocateCostBy,
-		ysnAccrue,
-		intSort,
-		intConcurrencyId
-)
-SELECT	intInventoryReceiptId	= @InventoryReceiptId
-		,intChargeId			= @intFeeItemId
-		,ysnInventoryCost		= 0
-		,strCostMethod			= 'Amount'
-		,dblRate				= 0
-		,intCostUOMId			= NULL
-		,intEntityVendorId		= NULL
-		,dblAmount				= @dblTicketFees * -1
-		,strAllocateCostBy		= NULL
-		,ysnAccrue				= 0
-		,intSort				= NULL
-		,intConcurrencyId		= 1		
-FROM	tblSCTicket SS WHERE SS.intTicketId = @intTicketId
-END
-*/
-
--- Re-update the total cost 
-
---UPDATE	Receipt
---SET		dblInvoiceAmount = (
---			SELECT	ISNULL(SUM(ISNULL(ReceiptItem.dblOpenReceive, 0) * ISNULL(ReceiptItem.dblUnitCost, 0)) , 0)
---			FROM	dbo.tblICInventoryReceiptItem ReceiptItem
---			WHERE	ReceiptItem.intInventoryReceiptId = Receipt.intInventoryReceiptId
---		)
---FROM	dbo.tblICInventoryReceipt Receipt 
---WHERE	Receipt.intInventoryReceiptId = @InventoryReceiptId
 
 BEGIN
 	INSERT INTO [dbo].[tblQMTicketDiscount]
