183e34b5ee95f93ef44d053a83f1b7f511d7592e
Ruben  Escarcha <ruben.escarcha@irely.com>
2016-02-29 21:54:50 +0800

    AR-2205 Calculate Item Discounts on Sales Invoice from Inventory Item setup

--- i21Database/dbo/Functions/fnARGetInvoiceItemsTermTotalDiscount.sql
+++ i21Database/dbo/Functions/fnARGetInvoiceItemsTermTotalDiscount.sql
@@ -2,7 +2,6 @@
 (
 	 @PaymentDate			DATETIME 
 	,@InvoiceId				INT
-	,@TermId				INT
 )
 RETURNS NUMERIC(18,6)
 AS
@@ -13,7 +12,19 @@ DECLARE @DiscountDay INT, @DayMonthDue INT, @DueNextMonth INT
 DECLARE @DiscountEP NUMERIC(18,6)
 DECLARE @DiscountDate DATETIME
 DECLARE @TransactionDate DATETIME
-DECLARE @InvoiceTotal NUMERIC(18,6)
+DECLARE @InvoiceDiscountTotal NUMERIC(18,6)
+DECLARE @TermId INT
+DECLARE @LocationId INT
+
+
+SELECT
+	 @TermId			= intTermId
+	,@TransactionDate	= dtmDate
+	,@LocationId		= intCompanyLocationId 
+FROM
+	tblARInvoice
+WHERE
+	intInvoiceId = @InvoiceId
 
 SELECT 
 	 @Type = strType 
@@ -26,48 +37,113 @@ WHERE
 	intTermID = @TermId
 
 
-IF (@Type = 'Standard')
-	BEGIN
-		IF (DATEADD(DAY,@DiscountDay,@TransactionDate) >= @PaymentDate)
-			BEGIN
-				RETURN @InvoiceTotal * (@DiscountEP / 100)
-			END
-		ELSE
-			BEGIN
-				RETURN 0
-			END
-	END	
-ELSE IF (@Type = 'Date Driven')
-	BEGIN
-		DECLARE @TransactionMonth int, @TransactionDay int, @TransactionYear int
-		SELECT @TransactionMonth = DATEPART(MONTH,@TransactionDate), @TransactionDay = DATEPART(DAY,@TransactionDate) ,@TransactionYear = DATEPART(YEAR,@TransactionDate)
-		
-		DECLARE @TempDiscountDate datetime
-		Set @TempDiscountDate = CONVERT(datetime, (CAST(@TransactionMonth AS nvarchar(10)) + '/' + CAST(@DiscountDay AS nvarchar(10)) + '/' + CAST(@TransactionYear AS nvarchar(10))), 101)
-			
-		IF (@TempDiscountDate >= @PaymentDate)
-			BEGIN
-				RETURN @InvoiceTotal * (@DiscountEP / 100)
-			END
-		ELSE
-			BEGIN
-				RETURN 0
-			END
-		
-	END	
-ELSE
-	BEGIN
-		IF (@DiscountDate >= @PaymentDate)
-			BEGIN
-				RETURN @InvoiceTotal * (@DiscountEP / 100)
-			END
-		ELSE
-			BEGIN
-				RETURN 0
-			END
-	END	
+DECLARE @DetailTable AS TABLE (intInvoiceDetailId INT, intItemId INT, intItemUOMId INT, dblPrice NUMERIC(18,6), dblQuantity NUMERIC(18,6))
+INSERT INTO @DetailTable
+	(
+	 intInvoiceDetailId
+	,intItemId
+	,intItemUOMId
+	,dblPrice
+	,dblQuantity
+	)
+SELECT
+	 intInvoiceDetailId
+	,intItemId
+	,intItemUOMId
+	,dblPrice
+	,dblQtyShipped 
+FROM
+	tblARInvoiceDetail
+WHERE
+	intInvoiceId = @InvoiceId
+	AND CHARINDEX('Inventory Promotional Pricing(Terms Discount)', strPricing) > 0
 
-RETURN 0;
+
+
+WHILE EXISTS(SELECT NULL FROM @DetailTable)
+BEGIN
+	 DECLARE @InvoiceDetailId	INT
+			,@ItemId			INT		
+			,@ItemUOMId			INT
+			,@Price				NUMERIC(18,6)	
+			,@QtyShipped		NUMERIC(18,6)
+			,@ItemDiscount		NUMERIC(18,6)
+
+	SELECT TOP 1
+		 @InvoiceDetailId	= intInvoiceDetailId
+		,@ItemId			= intItemId
+		,@ItemUOMId			= intItemUOMId
+		,@Price				= dblPrice
+		,@QtyShipped		= dblQuantity
+	FROM
+		@DetailTable
+
+	DECLARE  @DiscountBy	NVARCHAR(50)
+			,@Discount		NUMERIC(18, 6)
+
+	SELECT TOP 1 
+		@ItemDiscount		= @QtyShipped *
+								(CASE
+									WHEN strDiscountBy = 'Amount'
+										THEN ISNULL(dblDiscount, 0.00)
+									ELSE	
+										(@Price * (ISNULL(dblDiscount, 0.00)/100.00) )
+								END)						
+		,@DiscountBy		= strDiscountBy
+	FROM
+		tblICItemSpecialPricing 
+	WHERE
+		intItemId = @ItemId 
+		AND intItemLocationId = @LocationId 
+		AND intItemUnitMeasureId = @ItemUOMId
+		AND CAST(@TransactionDate AS DATE) BETWEEN CAST(dtmBeginDate AS DATE) AND CAST(ISNULL(dtmEndDate,@TransactionDate) AS DATE)
+	ORDER BY
+		dtmBeginDate DESC
+
+	IF @DiscountBy = 'Terms Rate'
+		BEGIN
+			IF (@Type = 'Standard')
+				BEGIN
+					IF (DATEADD(DAY,@DiscountDay,@TransactionDate) >= @PaymentDate)
+						BEGIN
+							SET @InvoiceDiscountTotal = @InvoiceDiscountTotal + (@QtyShipped * (@Price * (@DiscountEP / 100)))
+						END
+				END	
+			ELSE IF (@Type = 'Date Driven')
+				BEGIN
+					DECLARE @TransactionMonth int, @TransactionDay int, @TransactionYear int
+					SELECT @TransactionMonth = DATEPART(MONTH,@TransactionDate), @TransactionDay = DATEPART(DAY,@TransactionDate) ,@TransactionYear = DATEPART(YEAR,@TransactionDate)
+		
+					DECLARE @TempDiscountDate datetime
+					Set @TempDiscountDate = CONVERT(datetime, (CAST(@TransactionMonth AS nvarchar(10)) + '/' + CAST(@DiscountDay AS nvarchar(10)) + '/' + CAST(@TransactionYear AS nvarchar(10))), 101)
+			
+					IF (@TempDiscountDate >= @PaymentDate)
+						BEGIN
+							SET @InvoiceDiscountTotal = @InvoiceDiscountTotal + (@QtyShipped * (@Price * (@DiscountEP / 100)))
+						END		
+				END	
+			ELSE
+				BEGIN
+					IF (@DiscountDate >= @PaymentDate)
+						BEGIN
+							SET @InvoiceDiscountTotal = @InvoiceDiscountTotal + (@QtyShipped * (@Price * (@DiscountEP / 100)))
+						END
+				END
+		END
+	ELSE
+		BEGIN
+			SET @InvoiceDiscountTotal = @InvoiceDiscountTotal + @ItemDiscount
+		END
+
+	
+
+
+	DELETE FROM @DetailTable WHERE intInvoiceDetailId = @InvoiceDetailId	
+END
+
+
+
+RETURN @InvoiceDiscountTotal;
 
 END
 
