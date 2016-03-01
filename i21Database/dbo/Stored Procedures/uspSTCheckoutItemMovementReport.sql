d1eedcf14697c9dd19cfa33025e705804dbc5a42
Maheshwari <mgawade@BLRMAHEWIN10.irely.net>
2016-03-01 11:43:03 +0530

    ST - 186 - Added new columns and made SP changes to accommodate new requirement.

--- i21Database/dbo/Stored Procedures/uspSTCheckoutItemMovementReport.sql
+++ i21Database/dbo/Stored Procedures/uspSTCheckoutItemMovementReport.sql
@@ -4,14 +4,43 @@
 AS
 BEGIN
 
-	SELECT CIM.intItemUPCId, UOM.strUpcCode, UOM.strLongUPCCode
-	, SUM(CIM.intQtySold) [intQtySold]
-	, SUM(CIM.dblCurrentPrice) [dblCurrentPrice]
-	, (SUM(CIM.dblCurrentPrice) * SUM(CIM.intQtySold)) [dblTotalSales]
-	FROM dbo.tblSTCheckoutItemMovements CIM
-	JOIN dbo.tblSTCheckoutHeader CH ON CH.intCheckoutId = CIM.intCheckoutId
-	JOIN dbo.tblICItemUOM UOM ON UOM.intItemUOMId = CIM.intItemUPCId
-	Where CH.dtmCheckoutDate BETWEEN @BeginDate AND @EndDate
-	GROUP BY CIM.intItemUPCId, UOM.strUpcCode, UOM.strLongUPCCode
+DECLARE @tblItemMovement TABLE (intItemUOMId int, intVendorId int, dblQty decimal(18,6))
+
+INSERT INTO @tblItemMovement
+SELECT CIM.intItemUPCId, CIM.intVendorId, SUM(CIM.intQtySold)
+FROM dbo.tblSTCheckoutItemMovements CIM
+JOIN dbo.tblSTCheckoutHeader CH ON CH.intCheckoutId = CIM.intCheckoutId
+WHERE CH.dtmCheckoutDate BETWEEN @BeginDate AND @EndDate
+GROUP BY CIM.intItemUPCId, CIM.intVendorId
+
+SELECT * FROM @tblItemMovement
+
+SELECT t1.*, ((t1.[Gross Margin $]/t1.[Total Sales])*100) [Gross Margin %] FROM
+(
+SELECT t.*
+, (t.[Current Price]*t.[Qty Sold]) [Total Sales] 
+, ((t.[Current Price]*t.[Qty Sold]) - (t.[Item Cost]*t.[Qty Sold])) [Gross Margin $]
+FROM
+(
+SELECT DISTINCT CASE WHEN UOM.strUpcCode is not null then UOM.strUpcCode else UOM.strLongUPCCode end [UPC Number]
+, I.strDescription [Description]
+, V.strVendorId [Vendor]
+, ISNULL(CIM.dblItemStandardCost, 0) [Item Cost]
+, CASE WHEN (SP.dtmBeginDate < CH.dtmCheckoutDate AND SP.dtmEndDate > CH.dtmCheckoutDate) 
+		THEN SP.dblUnit 
+		ELSE Pr.dblSalePrice 
+	END [Current Price]
+, IM.dblQty [Qty Sold]
+FROM @tblItemMovement IM
+JOIN dbo.tblSTCheckoutItemMovements CIM ON CIM.intItemUPCId = IM.intItemUOMId AND CIM.intVendorId = IM.intVendorId
+JOIN dbo.tblSTCheckoutHeader CH ON CH.intCheckoutId = CIM.intCheckoutId
+JOIN dbo.tblICItemUOM UOM ON UOM.intItemUOMId = CIM.intItemUPCId
+JOIN dbo.tblICItem I ON I.intItemId = UOM.intItemId
+JOIN dbo.tblAPVendor V ON V.intEntityVendorId = CIM.intVendorId
+JOIN dbo.tblICItemSpecialPricing SP ON I.intItemId = SP.intItemId 
+JOIN dbo.tblICItemPricing Pr ON Pr.intItemId = I.intItemId 
+) t
+)t1
+
 
 END
