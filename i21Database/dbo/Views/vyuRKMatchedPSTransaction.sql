c1e2c33c29fc55457fbfaa0ed630fabf75d88392
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2016-03-02 17:20:20 +0530

    RM-292

--- i21Database/dbo/Views/vyuRKMatchedPSTransaction.sql
+++ i21Database/dbo/Views/vyuRKMatchedPSTransaction.sql
@@ -1,9 +1,9 @@
 ﻿CREATE VIEW [dbo].[vyuRKMatchedPSTransaction]
 
 AS
-SELECT *,(dblGrossPL-dblFutCommission) AS dblNetPL FROM(
+SELECT *,(dblGrossPL-dblFutCommission)  AS dblNetPL FROM(
 SELECT 
-((dblSPrice - dblLPrice)*dblMatchQty*dblContractSize) as dblGrossPL,* FROM
+((dblSPrice - dblLPrice)*dblMatchQty*dblContractSize)/ case when ysnSubCurrency = 'true' then intCent else 1 end as dblGrossPL,* FROM
 (
 SELECT psh.intMatchFuturesPSHeaderId,
           psd.intMatchFuturesPSDetailId,
@@ -18,11 +18,13 @@ SELECT psh.intMatchFuturesPSHeaderId,
           ot.strInternalTradeNo strLBrokerTradeNo,
           ot1.strInternalTradeNo strSBrokerTradeNo,
           fm.dblContractSize dblContractSize,0 as intConcurrencyId,
-          CASE WHEN bc.intFuturesRateType= 2 then 0 else  isnull(bc.dblFutCommission,0)* isnull(psd.dblMatchQty,0) end as dblFutCommission         
+          (CASE WHEN bc.intFuturesRateType= 2 then 0 else  isnull(bc.dblFutCommission,0)* isnull(psd.dblMatchQty,0) end) / case when ysnSubCurrency = 'true' then intCent else 1 end as dblFutCommission,
+		  c.intCurrencyID as intCurrencyId,c.intCent,ysnSubCurrency           
 FROM tblRKMatchFuturesPSHeader psh
 JOIN tblRKMatchFuturesPSDetail psd on psd.intMatchFuturesPSHeaderId=psh.intMatchFuturesPSHeaderId 
 JOIN tblRKFutOptTransaction ot on psd.intLFutOptTransactionId= ot.intFutOptTransactionId
 JOIN tblRKFutureMarket fm on ot.intFutureMarketId=fm.intFutureMarketId
+JOIN tblSMCurrency c on c.intCurrencyID=fm.intCurrencyId
 JOIN tblRKBrokerageAccount ba on ot.intBrokerageAccountId=ba.intBrokerageAccountId and ot.intInstrumentTypeId in(1) 
 LEFT JOIN tblRKBrokerageCommission bc on bc.intFutureMarketId=ot.intFutureMarketId and ot.intBrokerageAccountId=bc.intBrokerageAccountId
 JOIN tblRKFutOptTransaction ot1 on psd.intSFutOptTransactionId= ot1.intFutOptTransactionId
