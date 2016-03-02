591c7b5da525ecf271d285b827a47629be6d1288
Erick Dela Cruz <erick.delacruz@irely.com>
2016-03-02 09:49:07 +0800

    AP-1852

--- i21Database/dbo/Stored Procedures/uspAPUpdatePaymentAmountDue.sql
+++ i21Database/dbo/Stored Procedures/uspAPUpdatePaymentAmountDue.sql
@@ -6,6 +6,19 @@ AS
 DECLARE @isPosted BIT = @post;
 
 UPDATE tblAPPaymentDetail
-		SET tblAPPaymentDetail.dblAmountDue = (CASE WHEN B.dblAmountDue = 0 
-													THEN (B.dblDiscount + B.dblPayment - B.dblInterest) 
-												ELSE (B.dblAmountDue + B.dblPayment) END)
+SET tblAPPaymentDetail.dblAmountDue = (CASE WHEN B.dblAmountDue = 0 
+											THEN (B.dblDiscount + B.dblPayment - B.dblInterest) 
+										ELSE (B.dblAmountDue + B.dblPayment) END)
+
+
+UPDATE B
+	SET B.dblAmountDue = CASE WHEN (B.dblPayment + B.dblDiscount - B.dblInterest) = B.dblAmountDue 
+							THEN 0 ELSE (B.dblAmountDue) - (B.dblPayment) END,
+	B.dblDiscount = CASE WHEN (B.dblPayment + B.dblDiscount - B.dblInterest) = B.dblAmountDue 
+						THEN B.dblDiscount ELSE 0 END,
+	B.dblInterest = CASE WHEN (B.dblPayment + B.dblDiscount - B.dblInterest) = B.dblAmountDue 
+						THEN B.dblInterest ELSE 0 END
+FROM tblAPPayment A
+	LEFT JOIN tblAPPaymentDetail B
+		ON A.intPaymentId = B.intPaymentId
+WHERE A.intPaymentId IN (SELECT intPaymentId FROM #tmpPayablePostData)
