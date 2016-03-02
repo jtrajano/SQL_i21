25edf76793ddcd7599943f25dfa47bc0aec3a234
Maheshwari <mgawade@BLRMAHEWIN10.irely.net>
2016-03-02 23:52:55 +0530

    ST - 186 - Small Changes in Pull SPs

--- i21Database/dbo/Stored Procedures/uspSTCheckoutRadiantFGM.sql
+++ i21Database/dbo/Stored Procedures/uspSTCheckoutRadiantFGM.sql
@@ -34,7 +34,7 @@ BEGIN
 		, [dblQuantity] = ISNULL(Chk.FuelGradeSalesVolume, 0)
 		, [dblAmount] = ISNULL(Chk.FuelGradeSalesAmount,0)
 		from #tempCheckoutInsert Chk
-		JOIN dbo.tblICItemLocation IL ON Chk.FuelGradeID COLLATE Latin1_General_CI_AS = CASE WHEN ISNULL(IL.strPassportFuelId1, '') <> '' THEN IL.strPassportFuelId1
+		JOIN dbo.tblICItemLocation IL ON RIGHT(Chk.FuelGradeID, 3) COLLATE Latin1_General_CI_AS = CASE WHEN ISNULL(IL.strPassportFuelId1, '') <> '' THEN IL.strPassportFuelId1
 																							WHEN ISNULL(IL.strPassportFuelId2, '') <> '' THEN IL.strPassportFuelId2
 																							WHEN ISNULL(IL.strPassportFuelId3, '') <> '' THEN IL.strPassportFuelId3
 																						END
@@ -42,7 +42,8 @@ BEGIN
 		JOIN dbo.tblICItemUOM UOM ON UOM.intItemId = I.intItemId
 		JOIN dbo.tblSMCompanyLocation CL ON CL.intCompanyLocationId = IL.intLocationId
 		JOIN dbo.tblSTStore S ON S.intCompanyLocationId = CL.intCompanyLocationId
-		WHERE intCheckoutId = @intCheckoutId
+		WHERE intCheckoutId = @intCheckoutId AND intPumpCardCouponId = UOM.intItemUOMId
+
 	END
 
 END
