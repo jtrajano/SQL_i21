7da0b8a9a7a03eec94aa76db540f2f1471b450fc
Maheshwari <mgawade@BLRMAHEWIN10.irely.net>
2016-03-04 00:47:23 +0530

    ST - 186 - Resolved few issues found by Pawan from XML data

--- i21Database/dbo/Stored Procedures/uspSTCheckoutRadiantMCM.sql
+++ i21Database/dbo/Stored Procedures/uspSTCheckoutRadiantMCM.sql
@@ -11,11 +11,11 @@ BEGIN
 	INSERT INTO dbo.tblSTCheckoutDepartmetTotals
 	SELECT @intCheckoutId 
 	, Cat.intCategoryId
-	, ISNULL(Chk.SalesQuantity,0) [intTotalSalesCount]
+	, ISNULL(CAST(Chk.SalesQuantity as int),0) [intTotalSalesCount]
 	, (CASE WHEN (S.strReportDepartmentAtGrossOrNet) = 'G' 
-		THEN ISNULL(Chk.SalesAmount,0) + ISNULL(Chk.DiscountAmount,0) + ISNULL(Chk.PromotionAmount,0) + ISNULL(Chk.RefundAmount,0)   
+		THEN ISNULL(CAST(Chk.SalesAmount as decimal(18,6)),0) + ISNULL(CAST(Chk.DiscountAmount as decimal(18,6)),0) + ISNULL(CAST(Chk.PromotionAmount as decimal(18,6)),0) + ISNULL(CAST(Chk.RefundAmount as decimal(18,6)),0)   
 		WHEN (S.strReportDepartmentAtGrossOrNet) = 'N' 
-		THEN ISNULL(Chk.SalesAmount,0)
+		THEN ISNULL(CAST(Chk.SalesAmount as decimal(18,6)),0)
 	   END) [dblTotalSalesAmount]
 	, 0 [dblRegisterSalesAmount]
 	, '' [strDepartmentTotalsComment]
@@ -42,19 +42,20 @@ END
 ELSE
 BEGIN
 	UPDATE dbo.tblSTCheckoutDepartmetTotals  
-	SET intTotalSalesCount = ISNULL(Chk.SalesQuantity,0),
-	dblTotalSalesAmount = (CASE WHEN (S.strReportDepartmentAtGrossOrNet) = 'G' 
-		THEN ISNULL(Chk.SalesAmount,0) + ISNULL(Chk.DiscountAmount,0) + ISNULL(Chk.PromotionAmount,0) + ISNULL(Chk.RefundAmount,0)   
-		WHEN (S.strReportDepartmentAtGrossOrNet) = 'N' 
-		THEN ISNULL(Chk.SalesAmount,0)
-	   END)
-	FROM #tempCheckoutInsert Chk
-	JOIN dbo.tblICCategory Cat ON Cat.strCategoryCode = CASE WHEN LEN(Chk.MerchandiseCode COLLATE Latin1_General_CI_AS)<=4 THEN Chk.MerchandiseCode COLLATE Latin1_General_CI_AS ELSE SUBSTRING(Chk.MerchandiseCode COLLATE Latin1_General_CI_AS, 0, 4) END
-	JOIN dbo.tblICItem I ON I.intCategoryId = Cat.intCategoryId
-	JOIN dbo.tblICItemLocation IL ON IL.intItemId = I.intItemId
-	JOIN dbo.tblSMCompanyLocation CL ON CL.intCompanyLocationId = IL.intLocationId
-	JOIN dbo.tblSTStore S ON S.intCompanyLocationId = CL.intCompanyLocationId
-	WHERE intCheckoutId = @intCheckoutId AND tblSTCheckoutDepartmetTotals.intCategoryId = Cat.intCategoryId
+       SET intTotalSalesCount = ISNULL(CAST(Chk.SalesQuantity as int),0),
+       dblTotalSalesAmount = (CASE WHEN (S.strReportDepartmentAtGrossOrNet) = 'G' 
+              THEN ISNULL(CAST(Chk.SalesAmount as decimal(18,6)),0) + ISNULL(CAST(Chk.DiscountAmount as decimal(18,6)),0) 
+              + ISNULL(CAST(Chk.PromotionAmount as decimal(18,6)),0) + ISNULL(CAST(Chk.RefundAmount as decimal(18,6)),0)   
+              WHEN (S.strReportDepartmentAtGrossOrNet) = 'N' 
+              THEN ISNULL(CAST(Chk.SalesAmount as decimal(18,6)),0)
+          END)
+       FROM #tempCheckoutInsert Chk
+       JOIN dbo.tblICCategory Cat ON Cat.strCategoryCode = CASE WHEN LEN(Chk.MerchandiseCode COLLATE Latin1_General_CI_AS)<=4 THEN Chk.MerchandiseCode COLLATE Latin1_General_CI_AS ELSE SUBSTRING(Chk.MerchandiseCode COLLATE Latin1_General_CI_AS, 0, 4) END
+       JOIN dbo.tblICItem I ON I.intCategoryId = Cat.intCategoryId
+       JOIN dbo.tblICItemLocation IL ON IL.intItemId = I.intItemId
+       JOIN dbo.tblSMCompanyLocation CL ON CL.intCompanyLocationId = IL.intLocationId
+       JOIN dbo.tblSTStore S ON S.intCompanyLocationId = CL.intCompanyLocationId
+       WHERE intCheckoutId = @intCheckoutId AND tblSTCheckoutDepartmetTotals.intCategoryId = Cat.intCategoryId
 END
 
 UPDATE dbo.tblSTCheckoutDepartmetTotals SET dblRegisterSalesAmount = dblTotalSalesAmount Where intCheckoutId = @intCheckoutId
