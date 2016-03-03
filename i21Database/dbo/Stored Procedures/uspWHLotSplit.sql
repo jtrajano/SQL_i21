9d9162a999cc2de72ac2f722f830747eab87f73c
Swetosuvro Ghosh <swetosuvro.g@irely.com>
2016-03-03 21:45:03 +0530

    MFG-1499

---
+++ i21Database/dbo/Stored Procedures/uspWHLotSplit.sql
@@ -0,0 +1,156 @@
+﻿CREATE PROCEDURE uspWHLotSplit
+ @intLotId INT,       
+ @intSplitSubLocationId INT,  
+ @intSplitStorageLocationId INT,  
+ @dblSplitQty NUMERIC(38,20),  
+ @intUserId INT,
+ @strSplitLotNumber NVARCHAR(100)=NULL OUTPUT,
+ @strNewLotNumber NVARCHAR(100) = NULL,    
+ @strNote NVARCHAR(1024) = NULL,
+ @intInventoryAdjustmentId int=NULL OUTPUT
+
+AS
+
+BEGIN TRY
+
+	DECLARE @intItemId INT
+	DECLARE @dtmDate DATETIME
+	DECLARE @intLocationId INT
+	DECLARE @intSubLocationId INT 
+	DECLARE @intStorageLocationId INT
+	DECLARE @strLotNumber NVARCHAR(50)
+	DECLARE @intSourceId INT
+	DECLARE @intSourceTransactionTypeId INT
+	DECLARE @intLotStatusId INT
+	DECLARE @intItemUOMId INT
+		
+	DECLARE @TransactionCount INT
+	DECLARE @ErrMsg NVARCHAR(MAX)
+	
+	DECLARE @intNewLocationId INT
+	DECLARE @intNewSubLocationId INT
+	DECLARE @intNewStorageLocationId INT	
+	DECLARE @intNewItemUOMId INT
+	DECLARE @dblAdjustByQuantity NUMERIC(38,20)
+	DECLARE @strLotTracking NVARCHAR(50)
+	DECLARE @dblWeightPerQty NUMERIC(38, 20)
+	DECLARE @intWeightUOMId INT
+	DECLARE @intItemStockUOMId INT
+	DECLARE @dblLotReservedQty NUMERIC(38, 20)
+	DECLARE @dblWeight NUMERIC(38,20)
+	DECLARE @dblLotQty NUMERIC(38,20)
+	DECLARE @dblLotAvailableQty NUMERIC(38,20)
+
+	SELECT @intNewLocationId = intCompanyLocationId FROM tblSMCompanyLocationSubLocation WHERE intCompanyLocationSubLocationId = @intSplitSubLocationId
+	
+	SELECT @intItemId = intItemId, 
+		   @intLocationId = intLocationId,
+		   @intSubLocationId = intSubLocationId,
+		   @intStorageLocationId = intStorageLocationId, 
+		   @strLotNumber = strLotNumber,
+		   @dblLotQty = dblQty,
+		   @intLotStatusId = intLotStatusId,
+		   @intItemUOMId = intItemUOMId,	 
+		   @dblWeightPerQty = dblWeightPerQty,
+		   @intWeightUOMId = intWeightUOMId,
+		   @dblWeight = dblWeight
+	FROM tblICLot WHERE intLotId = @intLotId
+
+	SELECT @intItemStockUOMId = intItemUOMId
+	FROM dbo.tblICItemUOM
+	WHERE intItemId = @intItemId
+		AND ysnStockUnit = 1
+		
+	SELECT @dblLotAvailableQty = (CASE 
+		WHEN ISNULL(@dblWeight, 0) = 0
+			THEN ISNULL(@dblLotQty, 0)
+		ELSE ISNULL(@dblWeight, 0)
+		END)
+
+	SELECT @dblAdjustByQuantity = - @dblSplitQty, 
+		   @intNewItemUOMId = @intItemUOMId, 
+		   @dtmDate = GETDATE(), 
+		   @intSourceId = 1,
+		   @intSourceTransactionTypeId= 8
+	
+	SELECT @dblLotReservedQty = ISNULL(SUM(dblQty),0) FROM tblICStockReservation WHERE intLotId = @intLotId 
+	
+	IF (@dblLotAvailableQty + @dblAdjustByQuantity) < @dblLotReservedQty
+	BEGIN
+		RAISERROR('There is reservation against this lot. Cannot proceed.',16,1)
+	END
+	
+	SELECT @strLotTracking = strLotTracking
+	FROM dbo.tblICItem
+	WHERE intItemId = @intItemId
+
+	IF(ISNULL(@strNewLotNumber,'') <> '')
+	BEGIN
+		IF EXISTS(SELECT 1 FROM tblICLot WHERE strLotNumber = @strNewLotNumber AND intItemId <> @intItemId)
+		BEGIN
+			RAISERROR('Supplied lot number already exists for a lot with a different item. Please provide a different lot number to continue.',11,1)
+		END
+	END
+
+	IF (@strNewLotNumber = '' OR @strNewLotNumber IS NULL) 
+	BEGIN 
+		IF (@strLotTracking = 'Yes - Serial Number')
+		BEGIN
+			EXEC dbo.uspSMGetStartingNumber 24, @strNewLotNumber OUTPUT
+		END
+		ELSE 
+		BEGIN
+			RAISERROR('Lot tracking for the item is set as manual. Please supply the split lot number.',11,1)
+		END
+	END
+							 
+	EXEC uspICInventoryAdjustment_CreatePostSplitLot @intItemId	= @intItemId,
+													 @dtmDate =	@dtmDate,
+													 @intLocationId	= @intLocationId,
+													 @intSubLocationId = @intSubLocationId,
+													 @intStorageLocationId = @intStorageLocationId,
+													 @strLotNumber = @strLotNumber,
+													 @intNewLocationId = @intNewLocationId,
+													 @intNewSubLocationId = @intSplitSubLocationId,
+													 @intNewStorageLocationId = @intSplitStorageLocationId,
+													 @strNewLotNumber = @strNewLotNumber,
+													 @dblAdjustByQuantity = @dblAdjustByQuantity,
+													 @dblNewSplitLotQuantity = NULL,
+													 @dblNewWeight = NULL,
+													 @intNewItemUOMId = @intNewItemUOMId,
+													 @intNewWeightUOMId = NULL,
+													 @dblNewUnitCost = NULL,
+													 @intSourceId = @intSourceId,
+													 @intSourceTransactionTypeId = @intSourceTransactionTypeId,
+													 @intEntityUserSecurityId = @intUserId,
+													 @intInventoryAdjustmentId = @intInventoryAdjustmentId OUTPUT
+	UPDATE dbo.tblICLot
+	SET dblWeightPerQty = @dblWeightPerQty
+	WHERE intSubLocationId =@intSplitSubLocationId AND intStorageLocationId=@intSplitStorageLocationId AND strLotNumber=@strNewLotNumber
+	
+	SELECT @strSplitLotNumber = strLotNumber FROM tblICLot WHERE intSplitFromLotId = @intLotId
+	SELECT @strSplitLotNumber AS strSplitLotNumber
+
+	IF ((SELECT dblWeight FROM dbo.tblICLot WHERE intLotId = @intLotId) < 0.01) AND ((SELECT dblQty FROM dbo.tblICLot WHERE intLotId = @intLotId) < 0.01)
+	BEGIN
+		--EXEC dbo.uspMFLotAdjustQty
+		-- @intLotId =@intLotId,       
+		-- @dblNewLotQty =0,
+		-- @intUserId=@intUserId ,
+		-- @strReasonCode ='Residue qty clean up',
+		-- @strNotes ='Residue qty clean up'
+		UPDATE tblICLot
+		SET dblWeight = 0
+			,dblQty = 0
+		WHERE intLotId = @intLotId
+	END
+													 
+END TRY  
+  
+BEGIN CATCH  
+  
+ IF XACT_STATE() != 0 AND @TransactionCount = 0 AND @@TRANCOUNT > 0 ROLLBACK TRANSACTION  
+ SET @ErrMsg = ERROR_MESSAGE()      
+ RAISERROR(@ErrMsg, 16, 1, 'WITH NOWAIT')     
+  
+END CATCH 
