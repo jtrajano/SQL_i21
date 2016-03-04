270eb89d75fa9a63e22ab6ff208d38d7ac1ca221
kenneth lagunero <kenneth.lagunero@irely.com>
2016-03-04 17:39:00 +0800

    TM-1680

--- Integration/Scripts/TM/PopulateLocationIdInDeviceTable.sql
+++ Integration/Scripts/TM/PopulateLocationIdInDeviceTable.sql
@@ -1,18 +1,17 @@
-﻿PRINT N'BEGIN Update of data in tblTMCustomer Populate strOriginCustomerKey'
+﻿PRINT N'BEGIN Update of data in tblTMDevice Populate intLocationId'
 GO
 
-IF EXISTS(SELECT TOP 1 1 FROM INFORMATION_SCHEMA.VIEWS WHERE TABLE_NAME = 'vwcusmst') 
-	AND EXISTS(SELECT TOP 1 1 FROM sys.columns WHERE NAME  = N'strOriginCustomerKey' AND OBJECT_ID = OBJECT_ID(N'tblTMCustomer'))
+IF EXISTS(SELECT TOP 1 1 FROM INFORMATION_SCHEMA.VIEWS WHERE TABLE_NAME = 'vwlocmst') 
+	AND EXISTS(SELECT TOP 1 1 FROM sys.columns WHERE NAME  = N'intLocationId' AND OBJECT_ID = OBJECT_ID(N'tblTMDevice'))
 BEGIN
 		EXEC ('
-				UPDATE tblTMCustomer
-				SET strOriginCustomerKey = ISNULL(A.vwcus_key,'''')
-				FROM vwcusmst A
-				WHERE tblTMCustomer.intCustomerNumber = A.A4GLIdentity
-				AND (tblTMCustomer.strOriginCustomerKey IS NULL OR tblTMCustomer.strOriginCustomerKey = '''')
+				UPDATE tblTMDevice
+				SET intLocationId = A.A4GLIdentity
+				FROM vwlocmst A
+				WHERE tblTMDevice.strBulkPlant = A.vwloc_loc_no COLLATE Latin1_General_CI_AS
+				AND tblTMDevice.intLocationId IS NULL 
 			  ')
 END
 GO
 
-PRINT N'END Update of data in tblTMCustomer Populate strOriginCustomerKey'
-GO
+PRINT N'END Update of data in tblTMDevice Populate intLocationId'
