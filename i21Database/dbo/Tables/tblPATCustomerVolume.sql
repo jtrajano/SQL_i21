4ad1e2064796852be6d803a458c8d2b92831b837
jc_flores <jc.flores@irely.com>
2016-03-07 10:36:15 +0800

    PAT-275 * Additional changes

--- i21Database/dbo/Tables/tblPATCustomerVolume.sql
+++ i21Database/dbo/Tables/tblPATCustomerVolume.sql
@@ -3,9 +3,12 @@
 	[intCategoryVolumeId] INT NOT NULL IDENTITY, 
     [intCustomerPatronId] INT NULL, 
     [intPatronageCategoryId] INT NULL, 
+	[intRefundTypeDetailId] [int] NOT NULL,
     [intFiscalYear] INT NULL, 
 	[dtmLastActivityDate] DATETIME NULL,
     [dblVolume] NUMERIC(18, 6) NULL, 
     [intConcurrencyId] INT NULL DEFAULT 0, 
-    CONSTRAINT [PK_tblPATCustomerVolume] PRIMARY KEY ([intCategoryVolumeId]) 
+    CONSTRAINT [PK_tblPATCustomerVolume] PRIMARY KEY ([intCategoryVolumeId]), 
+    CONSTRAINT [FK_tblPATCustomerVolume_tblPATRefundRateDetail] FOREIGN KEY ([intRefundTypeDetailId]) REFERENCES [tblPATRefundRateDetail]([intRefundTypeDetailId]), 
+    CONSTRAINT [CK_tblPATCustomerVolume_tblPATRefundRateDetail] CHECK (1 = 1), 
 )
