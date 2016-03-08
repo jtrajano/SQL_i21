c394c9c9f3fdaf9be331e14c8871e8f762348abd
Satya <ssahoo@BLRSATYAWIN7.irely.net>
2016-03-08 21:49:47 +0530

    CT-279

--- i21Database/dbo/Tables/tblCTReassign.sql
+++ i21Database/dbo/Tables/tblCTReassign.sql
@@ -7,6 +7,11 @@
     intRecipientId		INT NOT NULL,
     intCreatedById		INT	NOT NULL,
 	dtmCreated			DATETIME NOT NULL,
+	dblUnallocatedQty	NUMERIC(18,6),
+	intAllocationUOMId	INT,
+	strAllocationUOM	NVARCHAR(50)  COLLATE Latin1_General_CI_AS,
+	dblUnpricedLots		NUMERIC(18,6),
+	dblUnhedgedLots		NUMERIC(18,6),
 	intConcurrencyId	INT NOT NULL,
 	
     PRIMARY KEY CLUSTERED ([intReassignId] ASC),
@@ -14,5 +19,6 @@
     CONSTRAINT [FK_tblCTReassign_tblCTContractType_intContractTypeId] FOREIGN KEY (intContractTypeId) REFERENCES tblCTContractType(intContractTypeId),
     CONSTRAINT [FK_tblCTReassign_tblCTContractDetail_intDonorId] FOREIGN KEY (intDonorId) REFERENCES tblCTContractDetail(intContractDetailId),
     CONSTRAINT [FK_tblCTReassign_tblCTContractDetail_intRecipientId] FOREIGN KEY (intRecipientId) REFERENCES tblCTContractDetail(intContractDetailId),
-    CONSTRAINT [FK_tblCTReassign_tblEntity_intCreatedById] FOREIGN KEY (intCreatedById) REFERENCES tblEntity(intEntityId)
+    CONSTRAINT [FK_tblCTReassign_tblEntity_intCreatedById] FOREIGN KEY (intCreatedById) REFERENCES tblEntity(intEntityId),
+	CONSTRAINT [FK_tblCTReassign_tblICUnitMeasure_intAllocationUOMId_intUnitMeasureId] FOREIGN KEY (intAllocationUOMId) REFERENCES tblICUnitMeasure(intUnitMeasureId)
 )
