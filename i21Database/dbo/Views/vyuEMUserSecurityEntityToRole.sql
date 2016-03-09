a7bdc0dfd1c622294441b712aa44781248da7d45
Mon Gonzales <mon.gonzales@irely.com>
2016-03-09 18:22:11 +0800

    EM-1036

---
+++ i21Database/dbo/Views/vyuEMUserSecurityEntityToRole.sql
@@ -0,0 +1,16 @@
+﻿CREATE VIEW [dbo].[vyuEMUserSecurityEntityToRole]
+	AS 
+	select 	
+	a.intUserRoleID
+	,a.ysnAdmin
+	,a.strRoleType
+	,strRoleName			= a.strName
+	,strEntityName			= c.strName
+	,strEntityNo			= c.strEntityNo
+	,intEntityId			= c.intEntityId
+
+from tblSMUserRole a
+	join tblEMEntityToRole b
+		on a.intUserRoleID = b.intEntityRoleId
+	join tblEntity c
+		on c.intEntityId = b.intEntityId
