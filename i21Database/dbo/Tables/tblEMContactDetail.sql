648dc0d22d4acf48f2690582c8725444fb06fea2
Mon Gonzales <mon.gonzales@irely.com>
2016-03-09 11:49:08 +0800

    EM-1031

--- i21Database/dbo/Tables/tblEMContactDetail.sql
+++ i21Database/dbo/Tables/tblEMContactDetail.sql
@@ -3,7 +3,7 @@
 	[intContactDetailId]			INT NOT NULL IDENTITY(1,1),
 	[intEntityId]					INT NOT NULL,
 	[intContactDetailTypeId]		INT NOT NULL,	
-	[strValue]						NVARCHAR(100) COLLATE Latin1_General_CI_AS NOT NULL, 		
+	[strValue]						NVARCHAR(500) COLLATE Latin1_General_CI_AS NOT NULL, 		
 	[intConcurrencyId]				INT CONSTRAINT [DF_tblEMContactDetail_intConcurrencyId] DEFAULT ((0)) NOT NULL,
 	CONSTRAINT [PK_tblEMContactDetail] PRIMARY KEY CLUSTERED ([intContactDetailId] ASC),	
 	CONSTRAINT [FK_tblEMContactDetail_tblEntity] FOREIGN KEY ([intEntityId]) REFERENCES [dbo].[tblEntity] ([intEntityId]) ON DELETE CASCADE,
