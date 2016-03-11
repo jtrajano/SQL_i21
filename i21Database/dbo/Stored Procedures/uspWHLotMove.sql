111735083bb125b79a68a27fc905d7a842049763
Swetosuvro Ghosh <swetosuvro.g@irely.com>
2016-03-11 13:00:48 +0530

    MFG-1499 CherryPick From 15.4Dev

---
+++ i21Database/dbo/Stored Procedures/uspWHLotMove.sql
@@ -0,0 +1,131 @@
+﻿CREATE PROCEDURE [uspWHLotMove] @intLotId INT
+	,@intNewSubLocationId INT
+	,@intNewStorageLocationId INT
+	,@dblMoveQty NUMERIC(38, 20)
+	,@intUserId INT
+AS
+BEGIN TRY
+	DECLARE @intItemId INT
+	DECLARE @dtmDate DATETIME
+	DECLARE @intLocationId INT
+	DECLARE @intSubLocationId INT
+	DECLARE @intStorageLocationId INT
+	DECLARE @strLotNumber NVARCHAR(50)
+	DECLARE @intSourceId INT
+	DECLARE @intSourceTransactionTypeId INT
+	DECLARE @intLotStatusId INT
+	DECLARE @intNewLocationId INT
+	DECLARE @strNewLotNumber NVARCHAR(50)
+	DECLARE @intInventoryAdjustmentId INT
+	DECLARE @TransactionCount INT
+	DECLARE @ErrMsg NVARCHAR(MAX)
+		,@dblWeightPerQty NUMERIC(38, 20)
+		,@intWeightUOMId INT
+		,@intItemStockUOMId INT
+
+	SELECT @intItemId = intItemId
+		,@intLocationId = intLocationId
+		,@intSubLocationId = intSubLocationId
+		,@intStorageLocationId = intStorageLocationId
+		,@strLotNumber = strLotNumber
+		,@intLotStatusId = intLotStatusId
+		,@intNewLocationId = intLocationId
+		,@dblWeightPerQty = dblWeightPerQty
+		,@intWeightUOMId = intWeightUOMId
+	FROM tblICLot
+	WHERE intLotId = @intLotId
+
+	SELECT @intItemStockUOMId = intItemUOMId
+	FROM dbo.tblICItemUOM
+	WHERE intItemId = @intItemId
+		AND ysnStockUnit = 1
+
+	SELECT @strNewLotNumber = @strLotNumber
+
+	SELECT @dtmDate = GETDATE()
+
+	SELECT @intSourceId = 1
+		,@intSourceTransactionTypeId = 8
+
+	IF ISNULL(@strLotNumber, '') = ''
+	BEGIN
+		RAISERROR (
+				51192
+				,11
+				,1
+				)
+	END
+
+	IF @intNewStorageLocationId = @intStorageLocationId
+	BEGIN
+		RAISERROR (
+				51182
+				,11
+				,1
+				)
+	END
+
+	EXEC uspICInventoryAdjustment_CreatePostLotMove @intItemId
+		,@dtmDate
+		,@intLocationId
+		,@intSubLocationId
+		,@intStorageLocationId
+		,@strLotNumber
+		,@intNewLocationId
+		,@intNewSubLocationId
+		,@intNewStorageLocationId
+		,@strNewLotNumber
+		,@dblMoveQty
+		,@intSourceId
+		,@intSourceTransactionTypeId
+		,@intUserId
+		,@intInventoryAdjustmentId
+
+	IF EXISTS (
+			SELECT *
+			FROM dbo.tblMFWorkOrderProducedLot
+			WHERE intLotId = @intLotId
+				AND dblPhysicalCount = @dblMoveQty
+			)
+	BEGIN
+		DECLARE @intNewLotId INT
+
+		SELECT @intNewLotId = intLotId
+		FROM dbo.tblICLot
+		WHERE strLotNumber = @strNewLotNumber
+			AND intStorageLocationId = @intNewStorageLocationId
+
+		UPDATE dbo.tblMFWorkOrderProducedLot
+		SET intLotId = @intNewLotId
+		WHERE intLotId = @intLotId
+	END
+	
+	UPDATE tblICLot
+	SET dblWeight = dblQty
+	WHERE dblQty <> dblWeight
+		AND intItemUOMId = intWeightUOMId
+	AND intLotId=@intLotId
+
+	IF (
+			SELECT dblWeight
+			FROM dbo.tblICLot
+			WHERE intLotId = @intLotId
+			) < 0.01
+	BEGIN
+		UPDATE tblICLot
+		SET dblWeight = 0
+			,dblQty = 0
+		WHERE intLotId = @intLotId
+	END
+END TRY
+
+BEGIN CATCH
+	IF XACT_STATE() != 0
+		AND @TransactionCount = 0
+		AND @@TRANCOUNT > 0
+		ROLLBACK TRANSACTION
+
+	SET @ErrMsg = ERROR_MESSAGE()
+
+	RAISERROR (@ErrMsg,16,1,'WITH NOWAIT')
+END CATCH
