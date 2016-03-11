0fe3697b4c663095a909ed766d0f9b223bd3d082
Arman Arenas <arman.arenas@irely.com>
2016-03-11 10:47:54 +0800

    PR-544 insert accrue and deduct on a time off as single entry

--- i21Database/dbo/Stored Procedures/uspPRInsertPaycheckTimeOff.sql
+++ i21Database/dbo/Stored Procedures/uspPRInsertPaycheckTimeOff.sql
@@ -20,24 +20,45 @@ INSERT INTO tblPRPaycheckTimeOff (
 )
 SELECT
 	@intPaycheckId
-	,D.intEmployeeTimeOffId 
-	,D.intTypeTimeOffId
-	,D.dblRate
-	,D.dblPerPeriod
-	,D.strPeriod
-	,D.dblRateFactor
-	,D.dblMaxEarned 
-	,D.dblMaxCarryover
-	,dblHoursAccrued	= CASE WHEN (C.intEmployeeAccrueTimeOffId = D.intTypeTimeOffId) THEN  B.dblHours * D.dblRate * D.dblPerPeriod * D.dblRateFactor ELSE 0 END
-	,dblHoursUsed		= CASE WHEN (C.intEmployeeTimeOffId = D.intTypeTimeOffId) THEN B.dblHours ELSE 0 END
-	,dblHoursYTD		= D.dblHoursEarned
+	,intEmployeeTimeOffId 
+	,intTypeTimeOffId
+	,dblRate
+	,dblPerPeriod
+	,strPeriod
+	,dblRateFactor
+	,dblMaxEarned 
+	,dblMaxCarryover
+	,SUM(dblHoursAccrued) dblHoursAccrued
+	,SUM(dblHoursUsed) dblHoursUsed
+	,MAX(dblHoursYTD) dblHoursYTD
 FROM 
-	tblPRPaycheckEarning B 
-	INNER JOIN tblPREmployeeEarning C ON B.intEmployeeEarningId = C.intEmployeeEarningId
-	LEFT JOIN tblPRPaycheck A ON A.intPaycheckId = B.intPaycheckId
-	LEFT JOIN tblPREmployeeTimeOff D 
-		 ON C.intEntityEmployeeId = D.intEntityEmployeeId AND
-		 (D.intTypeTimeOffId = C.intEmployeeTimeOffId OR D.intTypeTimeOffId = C.intEmployeeAccrueTimeOffId)
-WHERE D.intEmployeeTimeOffId IS NOT NULL AND B.intPaycheckId = @intPaycheckId AND A.ysnPosted = 1
-
+	(SELECT
+		D.intEmployeeTimeOffId 
+		,D.intTypeTimeOffId
+		,D.dblRate
+		,D.dblPerPeriod
+		,D.strPeriod
+		,D.dblRateFactor
+		,D.dblMaxEarned 
+		,D.dblMaxCarryover
+		,dblHoursAccrued	= CASE WHEN (C.intEmployeeAccrueTimeOffId = D.intTypeTimeOffId) THEN B.dblHours * D.dblRate * D.dblPerPeriod * D.dblRateFactor ELSE 0 END
+		,dblHoursUsed		= CASE WHEN (C.intEmployeeTimeOffId = D.intTypeTimeOffId) THEN B.dblHours ELSE 0 END
+		,dblHoursYTD		= D.dblHoursEarned - D.dblHoursUsed
+	FROM
+		tblPRPaycheckEarning B 
+		INNER JOIN tblPREmployeeEarning C ON B.intEmployeeEarningId = C.intEmployeeEarningId
+		INNER JOIN tblPRPaycheck A ON A.intPaycheckId = B.intPaycheckId
+		LEFT JOIN tblPREmployeeTimeOff D 
+			 ON C.intEntityEmployeeId = D.intEntityEmployeeId AND
+			 (D.intTypeTimeOffId = C.intEmployeeTimeOffId OR D.intTypeTimeOffId = C.intEmployeeAccrueTimeOffId)
+	WHERE D.intEmployeeTimeOffId IS NOT NULL AND B.intPaycheckId = @intPaycheckId AND A.ysnPosted = 1) TimeOff
+GROUP BY
+	intEmployeeTimeOffId 
+	,intTypeTimeOffId
+	,dblRate
+	,dblPerPeriod
+	,strPeriod
+	,dblRateFactor
+	,dblMaxEarned 
+	,dblMaxCarryover
 GO
