89c6e03767ccf20a21ed9dc444ff163c27afe643
jeffrey trajano <jeffrey.trajano@irely.com>
2016-03-22 08:14:27 +0800

    GL-2587 RE is not correct if there is actual transactions for RE account

--- i21Database/dbo/Functions/fnGLGetBeginningBalanceAndUnit.sql
+++ i21Database/dbo/Functions/fnGLGetBeginningBalanceAndUnit.sql
@@ -1,6 +1,5 @@
 ﻿CREATE FUNCTION [dbo].[fnGLGetBeginningBalanceAndUnit] 
 (	
-	-- Add the parameters for the function here
 	@strAccountId NVARCHAR(100),
 	@dtmDate DATETIME
 )
@@ -15,6 +14,29 @@ BEGIN
 	-- *BOY = BEGINNING OF FISCAL YEAR
 	-- *BOT = BEGINNING OF TIME
 	--  NOTE : EXPENSE AND REVENUE BEGINNING BALANCE IS COMPUTED VIA *BOY WHILE OTHER ARE COMPUTE VIA *BOT
+	IF EXISTS(SELECT TOP 1 1 FROM tblGLAccount A JOIN tblGLFiscalYear B ON A.intAccountId = B.intRetainAccount WHERE A.strAccountId = @strAccountId)
+	BEGIN
+		DECLARE @dte DATETIME
+		SELECT TOP 1 @dte= dtmDateFrom from tblGLFiscalYear WHERE @dtmDate >= dtmDateFrom  and @dtmDate <= dtmDateTo ORDER BY dtmDateFrom DESC
+
+		;WITH cte as(
+		SELECT  
+				 @strAccountId AS strAccountId,
+				(dblCredit - dblDebit) as beginbalance,
+				(dblCreditUnit - dblDebitUnit) as beginbalanceunit
+		  
+		FROM tblGLAccount A
+			LEFT JOIN tblGLAccountGroup B ON A.intAccountGroupId = B.intAccountGroupId
+			LEFT JOIN tblGLSummary C ON A.intAccountId = C.intAccountId
+		WHERE
+		(B.strAccountType in ('Expense','Revenue')  and C.dtmDate < ISNULL(@dte,@dtmDate) )
+		OR (strAccountId =@strAccountId AND C.dtmDate < @dtmDate) and strCode <> '')
+		insert into @tbl
+		select strAccountId, sum(beginbalance) beginBalance ,sum(beginbalanceunit) beginBalanceUnit from cte group by strAccountId
+		
+		RETURN 
+	END
+
 	DECLARE @accountType NVARCHAR(30)
 	SELECT @accountType= B.strAccountType  FROM tblGLAccount A JOIN tblGLAccountGroup B on A.intAccountGroupId = B.intAccountGroupId WHERE
 	A.strAccountId = @strAccountId and B.strAccountType IN ('Expense','Revenue','Cost of Goods Sold')
@@ -59,5 +81,3 @@ BEGIN
 		GROUP BY strAccountId
 		RETURN
 END
-	
-	
