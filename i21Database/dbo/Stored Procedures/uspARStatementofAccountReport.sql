73f9986adc4f270969ac94a89687eddf950467cb
Kelvin Quinto <kelvin.quinto@irely.com>
2016-03-22 10:17:17 +0800

    AR-2562

---
+++ i21Database/dbo/Stored Procedures/uspARStatementofAccountReport.sql
@@ -0,0 +1,109 @@
+﻿CREATE PROCEDURE [dbo].[uspARStatementofAccountReport]
+	@xmlParam NVARCHAR(MAX) = NULL
+AS
+
+SET QUOTED_IDENTIFIER OFF
+SET ANSI_NULLS ON
+SET NOCOUNT ON
+SET XACT_ABORT ON
+SET ANSI_WARNINGS OFF
+
+-- Sanitize the @xmlParam 
+IF LTRIM(RTRIM(@xmlParam)) = '' 
+	SET @xmlParam = NULL 
+
+-- Declare the variables.
+DECLARE  @dtmDateTo					AS DATETIME
+		,@dtmDateFrom				AS DATETIME
+		,@strDateTo					AS NVARCHAR(50)
+		,@strDateFrom				AS NVARCHAR(50)
+		,@xmlDocumentId				AS INT
+		,@query						AS NVARCHAR(MAX)
+		,@innerQuery				AS NVARCHAR(MAX) = ''
+		,@joinQuery                 AS NVARCHAR(MAX) = ''
+		,@filter					AS NVARCHAR(MAX) = ''
+		,@fieldname					AS NVARCHAR(50)
+		,@condition					AS NVARCHAR(20)
+		,@id						AS INT 
+		,@from						AS NVARCHAR(50)
+		,@to						AS NVARCHAR(50)
+		,@join						AS NVARCHAR(10)
+		,@begingroup				AS NVARCHAR(50)
+		,@endgroup					AS NVARCHAR(50)
+		,@datatype					AS NVARCHAR(50)
+		
+-- Create a table variable to hold the XML data. 		
+DECLARE @temp_xml_table TABLE (
+	 [id]			INT IDENTITY(1,1)
+	,[fieldname]	NVARCHAR(50)
+	,[condition]	NVARCHAR(20)
+	,[from]			NVARCHAR(50)
+	,[to]			NVARCHAR(50)
+	,[join]			NVARCHAR(10)
+	,[begingroup]	NVARCHAR(50)
+	,[endgroup]		NVARCHAR(50)
+	,[datatype]		NVARCHAR(50)
+)
+
+DECLARE @temp_SOA_table TABLE(
+	 [intEntityCustomerId]	 INT
+	,[strCustomerNumber]	 NVARCHAR(100)
+	,[strCustomerName]		 NVARCHAR(100)
+	,[strStatementFormat]	 NVARCHAR(100)
+	,[strXmlParam]			 NVARCHAR(MAX)
+	,[dtmAsOfDate]			 DATETIME
+)
+
+-- Prepare the XML 
+EXEC sp_xml_preparedocument @xmlDocumentId OUTPUT, @xmlParam
+
+-- Insert the XML to the xml table. 		
+INSERT INTO @temp_xml_table
+SELECT *
+FROM OPENXML(@xmlDocumentId, 'xmlparam/filters/filter', 2)
+WITH (
+	  [fieldname]  NVARCHAR(50)
+	, [condition]  NVARCHAR(20)
+	, [from]	   NVARCHAR(50)
+	, [to]		   NVARCHAR(50)
+	, [join]	   NVARCHAR(10)
+	, [begingroup] NVARCHAR(50)
+	, [endgroup]   NVARCHAR(50)
+	, [datatype]   NVARCHAR(50)
+)
+
+DELETE FROM @temp_xml_table WHERE [fieldname] = 'dtmAsOfDate'
+
+WHILE EXISTS(SELECT 1 FROM @temp_xml_table)
+BEGIN
+	SELECT @id = id, @fieldname = [fieldname], @condition = [condition], @from = [from], @to = [to], @join = [join], @datatype = [datatype] FROM @temp_xml_table
+	SET @filter = @filter + ' ' + dbo.fnAPCreateFilter(@fieldname, @condition, @from, @to, @join, null, null, @datatype)
+	
+	DELETE FROM @temp_xml_table WHERE id = @id
+
+	IF EXISTS(SELECT 1 FROM @temp_xml_table)
+	BEGIN
+		SET @filter = @filter + ' AND '
+	END
+END
+
+SET @query = 'SELECT * FROM
+(SELECT C.intEntityCustomerId
+      , C.strCustomerNumber
+      , strCustomerName		= E.strName
+	  , C.strStatementFormat
+	  , strXmlParam			= '''+@xmlParam+'''
+      , dtmAsOfDate			= CONVERT(DATE, GETDATE())
+FROM tblARCustomer C INNER JOIN
+       tblEntity E ON C.intEntityCustomerId = E.intEntityId
+) MainQuery'
+
+IF ISNULL(@filter,'') != ''
+BEGIN
+	SET @query = @query + ' WHERE ' + @filter
+END
+
+INSERT INTO @temp_SOA_table
+EXEC sp_executesql @query
+
+SELECT * FROM @temp_SOA_table
