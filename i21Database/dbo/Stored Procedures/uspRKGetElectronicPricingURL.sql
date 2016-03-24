7aa49498a0eba9823a0883b0c9cfc786d9eae922
Gopinath Nayak <gopinath.n@irely.com>
2016-03-24 22:55:26 +0530

    GRN-388

--- i21Database/dbo/Stored Procedures/uspRKGetElectronicPricingURL.sql
+++ i21Database/dbo/Stored Procedures/uspRKGetElectronicPricingURL.sql
@@ -24,80 +24,26 @@ BEGIN TRY
 	SELECT @MarketExchangeCode = E.strExchangeInterfaceCode
 	FROM tblRKMarketExchange E
 	JOIN tblRKFutureMarket M ON M.intMarketExchangeId = E.intMarketExchangeId AND M.intFutureMarketId = @FutureMarketId
+	
+	IF ISNULL(@FutureMonthId,0)=0
+	BEGIN
+		SELECT TOP 1 @FutureMonthId =ISNULL(intFutureMonthId,0)	
+		FROM tblRKFuturesMonth
+		WHERE intFutureMarketId = @FutureMarketId AND ysnExpired=0
+		AND ISNULL(dtmLastTradingDate, CONVERT(DATETIME, SUBSTRING(LTRIM(year(GETDATE())), 1, 2) + LTRIM(intYear) + '-' + SUBSTRING(strFutureMonth, 1, 3) + '-01')) >= DATEADD(d, 0, DATEDIFF(d, 0, GETDATE()))
+		ORDER BY ISNULL(dtmLastTradingDate, CONVERT(DATETIME, SUBSTRING(LTRIM(year(GETDATE())), 1, 2) + LTRIM(intYear) + '-' + SUBSTRING(strFutureMonth, 1, 3) + '-01')) ASC
+		
+	END
 
 	IF @FutureMonthId >0
 	BEGIN
 		SELECT @StrTradedMonthSymbol=strSymbol+RIGHT(intYear,1) from tblRKFuturesMonth Where  intFutureMonthId=@FutureMonthId
 	END
-	ELSE
-	BEGIN
-
-		DECLARE @FutureTradedMonths AS TABLE 
-		(
-			 IntMonthNumber INT
-			,StrMonthName NVARCHAR(20)
-			,IsTraded BIT
-			,StrTradedMonthSymbol NVARCHAR(2)
-		 )
-	 	 
-		INSERT INTO @FutureTradedMonths (IntMonthNumber,StrMonthName,IsTraded,StrTradedMonthSymbol)
-
-		SELECT 1,'JAN',ysnFutJan,'F' FROM tblRKFutureMarket WHERE intFutureMarketId = @FutureMarketId
-		UNION
-		SELECT 2,'FEB',ysnFutFeb,'G' FROM tblRKFutureMarket WHERE intFutureMarketId = @FutureMarketId	
-		UNION
-		SELECT 3,'MAR',ysnFutMar,'H' FROM tblRKFutureMarket WHERE intFutureMarketId = @FutureMarketId
-		UNION
-		SELECT 4,'APR',ysnFutApr,'J' FROM tblRKFutureMarket	WHERE intFutureMarketId = @FutureMarketId
-		UNION
-		SELECT 5,'MAY',ysnFutMay,'K' FROM tblRKFutureMarket WHERE intFutureMarketId = @FutureMarketId
-		UNION 
-		SELECT 6,'JUN',ysnFutJun,'M' FROM tblRKFutureMarket	WHERE intFutureMarketId = @FutureMarketId
-		UNION
-		SELECT 7,'JUL',ysnFutJul,'N' FROM tblRKFutureMarket WHERE intFutureMarketId = @FutureMarketId
-		UNION
-		SELECT 8,'AUG',ysnFutAug,'Q' FROM tblRKFutureMarket WHERE intFutureMarketId = @FutureMarketId
-		UNION
-		SELECT 9,'SEP',ysnFutSep,'U' FROM tblRKFutureMarket	WHERE intFutureMarketId = @FutureMarketId
-		UNION
-		SELECT 10,'OCT',ysnFutOct,'V' FROM tblRKFutureMarket WHERE intFutureMarketId = @FutureMarketId
-		UNION
-		SELECT 11,'NOV',ysnFutNov,'X' FROM tblRKFutureMarket WHERE intFutureMarketId = @FutureMarketId
-		UNION
-		SELECT 12,'DEC',ysnFutDec,'Z' FROM tblRKFutureMarket WHERE intFutureMarketId = @FutureMarketId
-
-		SELECT TOP 1 @StrTradedMonthSymbol = StrTradedMonthSymbol
-		FROM @FutureTradedMonths
-		WHERE IntMonthNumber >= MONTH(Getdate()) AND IsTraded = 1
-		ORDER BY IntMonthNumber ASC
-
-		IF ISNULL(@StrTradedMonthSymbol, '') = ''
-		BEGIN
-				SET @StrTradedMonthSymbol = (
-												SELECT TOP 1 StrTradedMonthSymbol
-												FROM @FutureTradedMonths
-												WHERE IsTraded = 1
-												ORDER BY IntMonthNumber ASC
-											)
-				SET @StrTradedMonthSymbol = @StrTradedMonthSymbol + Convert(NVARCHAR, RIGHT(YEAR(GetDate()), 1) + 1)
-		END
-		ELSE
-		BEGIN
-			SET @StrTradedMonthSymbol = @StrTradedMonthSymbol + RIGHT(YEAR(GetDate()), 1)
-		END
-		
-	END
-
+	
 	SELECT @URL = strInterfaceWebServicesURL FROM tblSMCompanyPreference
 
-	SELECT @strUserName = strProviderUserId FROM tblGRUserPreference Where [intEntityUserSecurityId]= @intUserId  
-
-	 --IF NOT EXISTS (SELECT 1 FROM tblGRUserPreference Where strQuoteProvider='DTN/Agricharts' AND [intEntityUserSecurityId]=@intUserId) OR (@strUserName = '')  
-	 --BEGIN
-	 -- RAISERROR ('The User cannot access Electronic Pricing',16,1)  
-	 --END
-
-
+	SELECT @strUserName = strProviderUserId FROM tblGRUserPreference Where [intEntityUserSecurityId]= @intUserId 
+	
 	SELECT @strPassword = strProviderPassword FROM tblGRUserPreference Where [intEntityUserSecurityId]=@intUserId  
 
 	SELECT @IntinterfaceSystem = intInterfaceSystemId FROM   tblSMCompanyPreference
