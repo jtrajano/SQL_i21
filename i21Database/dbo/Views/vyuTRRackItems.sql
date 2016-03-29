3b7654ade7f988e8270e5e0f9cc1c14afb4f754c
Lawrence Zabala <lawrence.zabala@irely.com>
2016-03-30 02:48:19 +0800

    TR-404

--- i21Database/dbo/Views/vyuTRRackItems.sql
+++ i21Database/dbo/Views/vyuTRRackItems.sql
@@ -1,22 +1,16 @@
 ﻿CREATE VIEW [dbo].[vyuTRRackItems]
 	AS 
-SELECT Distinct
-    A.intItemId,
-	B.intSupplyPointId,
-	A.strItemNo,
-	D.intLocationId,
-	E.strLocationName,
-	A.strType,
-	A.strDescription,
-	C.strEquation
-	
-FROM
-     dbo.tblICItem  A
-	LEFT JOIN dbo.tblTRSupplyPointRackPriceEquation B
-		ON B.intItemId = A.intItemId
-	LEFT JOIN dbo.vyuTRRackPriceEquation C
-		ON C.intItemId = B.intItemId and C.intSupplyPointId = B.intSupplyPointId
-    LEFT JOIN tblICItemLocation D
-	    ON A.intItemId = D.intItemId
-    LEFT JOIN tblSMCompanyLocation E
-	    ON D.intLocationId = E.intCompanyLocationId
+
+SELECT DISTINCT A.intItemId
+	, B.intSupplyPointId
+	, A.strItemNo
+	, D.intLocationId
+	, D.strLocationName
+	, A.strType
+	, A.strDescription
+	, intRackPriceDetailId = RackPriceDetail.intRackPriceDetailId
+	, strEquation = ISNULL(RackPriceDetail.strEquation, '')
+FROM dbo.tblICItem A
+LEFT JOIN dbo.tblTRSupplyPointRackPriceEquation B ON B.intItemId = A.intItemId
+LEFT JOIN vyuTRRackPrice RackPriceDetail ON RackPriceDetail.intSupplyPointId = B.intSupplyPointId AND RackPriceDetail.intItemId = A.intItemId
+LEFT JOIN vyuICGetItemLocation D ON A.intItemId = D.intItemId
