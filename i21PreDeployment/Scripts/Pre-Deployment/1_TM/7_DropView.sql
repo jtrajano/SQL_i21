060e89def46810e30d0ae4ff4d0f4c27384f9288
kenneth lagunero <kenneth.lagunero@irely.com>
2016-03-30 14:02:10 +0800

    TM-1798

--- i21PreDeployment/Scripts/Pre-Deployment/1_TM/7_DropView.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/1_TM/7_DropView.sql
@@ -5,5 +5,10 @@ IF EXISTS(select top 1 1 from INFORMATION_SCHEMA.VIEWS where TABLE_NAME = 'vwprc
 	DROP VIEW vwprcmst
 GO
 
+IF EXISTS(select top 1 1 from INFORMATION_SCHEMA.VIEWS where TABLE_NAME = 'vwcntmst')
+	DROP VIEW vwcntmst
+
+GO
+
 PRINT N'END Drop some view for TM'
 GO
