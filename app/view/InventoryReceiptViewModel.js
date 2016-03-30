bc3d9759da0d30f4e0e65824c3c2f3c7bd4d900f
Feb Montefrio <feb.montefrio@irely.com>
2016-03-30 14:35:12 +0800

    Comment out GridExtension in 16.1 Dev

--- app/view/InventoryReceiptViewModel.js
+++ app/view/InventoryReceiptViewModel.js
@@ -31,7 +31,7 @@ Ext.define('Inventory.view.InventoryReceiptViewModel', {
         'ContractManagement.store.ContractDetailView',
         'ContractManagement.store.ContractHeaderViewBuffered',
         'Logistics.store.BufferedShipmentReceiptContracts',
-        'AccountsPayable.common.extensions.GridExtension'
+        //'AccountsPayable.common.extensions.GridExtension'
     ],
 
     data: {
