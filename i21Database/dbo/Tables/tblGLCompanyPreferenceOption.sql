8e96bc6f1ec197f0bced3ae26b16382e86051afb
jeffrey trajano <jeffrey.trajano@irely.com>
2016-03-31 18:45:52 +0800

    GL-2392 Add a section to the Company Configurations to remind a user that there are unposted balances in the general ledger

---
+++ i21Database/dbo/Tables/tblGLCompanyPreferenceOption.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[tblGLCompanyPreferenceOption]
+(
+	[intCompanyPreferenceOptionId] [int] NOT NULL IDENTITY,
+	[strNotificationMessage] [nvarchar](100) NULL,
+	[intDaysBeforeEvent] [int] NULL,
+	[intDaysAfterEvent] [int] NULL,
+	[intConcurrencyId] [int] NULL,
+	[strRemindUsers] [nvarchar](500) NULL,
+	[strEventDescription] [nvarchar](50) NULL, 
+    CONSTRAINT [PK_tblGLCompanyPreferenceOption] PRIMARY KEY ([intCompanyPreferenceOptionId])
+
+)
