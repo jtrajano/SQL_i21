9764033c3a7999bf63c3ac8d45ece7b282d352a4
Lawrence Zabala <lawrence.zabala@irely.com>
2016-04-02 02:12:15 +0800

    TR-424

--- i21Database/dbo/Tables/tblTRTransactionDetailLog.sql
+++ i21Database/dbo/Tables/tblTRTransactionDetailLog.sql
@@ -1,10 +1,10 @@
 ﻿CREATE TABLE [dbo].[tblTRTransactionDetailLog]
 (
 	[intTransactionDetailLogId] INT NOT NULL IDENTITY, 
-    [strTransactionType] NVARCHAR(50) NOT NULL, 
+    [strTransactionType] NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL, 
     [intTransactionId] INT NOT NULL, 
     [intTransactionDetailId] INT NOT NULL, 
-    [strSourceType] NVARCHAR(50) NULL, 
+    [strSourceType] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
     [intSourceId] INT NULL, 
     [dblQuantity] NUMERIC(18, 6) NULL DEFAULT ((0)), 
 	[intItemId] INT NULL,
