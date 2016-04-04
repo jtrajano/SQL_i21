d18b9f33ca046582a44b24914cba5bd8ef7a5d70
Chakra <CBehera@BLRCHAKRAWIN10.irely.net>
2016-04-05 03:09:45 +0530

    IN-1

---
+++ i21Database/dbo/Tables/tblIPFileType.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[tblIPFileType]
+(
+	[intFileTypeId] INT NOT NULL , 
+    [strName] NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL,
+	CONSTRAINT [PK_tblIPFileType_intFileTypeId] PRIMARY KEY ([intFileTypeId]), 
+    CONSTRAINT [UQ_tblIPFileType_strName] UNIQUE ([strName])
+)
---
+++ i21Database/dbo/Tables/tblIPProcess.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[tblIPProcess]
+(
+	[intProcessId] INT NOT NULL IDENTITY, 
+    [strProcessName] NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL, 
+    [strDescription] NVARCHAR(250) COLLATE Latin1_General_CI_AS NULL, 
+    [intScheduleId] INT NULL,
+	[ysnAutoExecution] bit DEFAULT 0,
+	[dtmLastExecution] [datetime],
+	[ysnBusy] BIT DEFAULT 0,
+	[intCreatedUserId] [int] NULL,
+	[dtmCreated] [datetime] DEFAULT GetDate(),
+	[intLastModifiedUserId] [int] NULL,
+	[dtmLastModified] [datetime] DEFAULT GetDate(),	 
+    [intConcurrencyId] INT NULL DEFAULT 0, 	 
+	CONSTRAINT [PK_tblIPProcess_intProcessId] PRIMARY KEY ([intProcessId]),
+	CONSTRAINT [UQ_tblIPProcess_strProcessName] UNIQUE ([strProcessName]),
+	CONSTRAINT [FK_tblIPProcess_tblIPSchedule_intScheduleId] FOREIGN KEY ([intScheduleId]) REFERENCES [tblIPSchedule]([intScheduleId]), 
+)
