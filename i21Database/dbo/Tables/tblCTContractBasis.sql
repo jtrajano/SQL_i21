169f2c2c33a851b8c1884b0ba2ec4540bd72b2bd
Satya <ssahoo@BLRSATYAWIN7.irely.net>
2016-04-05 21:18:40 +0530

    CT-460

--- i21Database/dbo/Tables/tblCTContractBasis.sql
+++ i21Database/dbo/Tables/tblCTContractBasis.sql
@@ -8,6 +8,7 @@
 	[intPositionId] [int] NULL,
 	[intConcurrencyId] INT NOT NULL, 
 	[strINCOLocationType] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
+	[ysnActive] [bit] NULL,
     CONSTRAINT [PK_tblCTContractBasis_intContractBasisId] PRIMARY KEY CLUSTERED ([intContractBasisId] ASC),
 	CONSTRAINT [UK_tblCTContractBasis_strContractBasis] UNIQUE ([strContractBasis]),
 	CONSTRAINT [FK_tblCTContractBasis_tblCTInsuranceBy_intInsuranceById] FOREIGN KEY ([intInsuranceById]) REFERENCES [tblCTInsuranceBy]([intInsuranceById]),
