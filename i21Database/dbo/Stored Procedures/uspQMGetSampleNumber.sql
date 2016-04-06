32e4943f4b06ef9a92a174064ab196593098d9b8
Vinoth Kumar T <vinoth.t@irely.com>
2016-04-06 21:48:07 +0530

    QC-129

--- i21Database/dbo/Stored Procedures/uspQMGetSampleNumber.sql
+++ i21Database/dbo/Stored Procedures/uspQMGetSampleNumber.sql
@@ -10,11 +10,28 @@ SET ANSI_WARNINGS OFF
 
 DECLARE @strSampleId NVARCHAR(MAX)
 
-SELECT @strSampleId = COALESCE(@strSampleId + '|^|', '') + CONVERT(NVARCHAR, intSampleId)
-FROM dbo.tblQMSample
-WHERE intProductTypeId = @intProductTypeId
-	AND intProductValueId = @intProductValueId
-ORDER BY intSampleId DESC
+IF @intProductTypeId = 6 -- Take all samples from the multiple Lot ID
+BEGIN
+	DECLARE @strLotNumber NVARCHAR(50)
+
+	SELECT @strLotNumber = strLotNumber
+	FROM dbo.tblICLot
+	WHERE intLotId = @intProductValueId
+
+	SELECT @strSampleId = COALESCE(@strSampleId + '|^|', '') + CONVERT(NVARCHAR, intSampleId)
+	FROM dbo.tblQMSample
+	WHERE intProductTypeId = @intProductTypeId
+		AND strLotNumber = @strLotNumber
+	ORDER BY intSampleId DESC
+END
+ELSE
+BEGIN
+	SELECT @strSampleId = COALESCE(@strSampleId + '|^|', '') + CONVERT(NVARCHAR, intSampleId)
+	FROM dbo.tblQMSample
+	WHERE intProductTypeId = @intProductTypeId
+		AND intProductValueId = @intProductValueId
+	ORDER BY intSampleId DESC
+END
 
 IF @strSampleId IS NULL
 	SELECT '0' AS strSampleId
