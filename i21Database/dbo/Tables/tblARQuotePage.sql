0b5dff268c14b5058c390f60983537c4fd41f69e
Kelvin Quinto <kelvin.quinto@irely.com>
2016-04-07 13:56:55 +0800

    AR-2624

--- i21Database/dbo/Tables/tblARQuotePage.sql
+++ i21Database/dbo/Tables/tblARQuotePage.sql
@@ -5,5 +5,6 @@
 	[strPageDescription]		NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL,
 	[strPageBody]				NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NULL,
 	[intConcurrencyId]			INT NOT NULL,	
-	CONSTRAINT [PK_tblARQuotePage_intQuotePageId] PRIMARY KEY CLUSTERED ([intQuotePageId] ASC)
+	CONSTRAINT [PK_tblARQuotePage_intQuotePageId] PRIMARY KEY CLUSTERED ([intQuotePageId] ASC),
+	CONSTRAINT [UK_tblARQuotePage_strPageTitle] UNIQUE (strPageTitle)
 )
