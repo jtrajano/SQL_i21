f8418d5b48eaf3dd67a84ab5ced225537fb2094c
Feb Montefrio <feb.montefrio@irely.com>
2016-04-09 00:02:31 +0800

    IC-2028

--- i21Database/dbo/Stored Procedures/uspICRebuildInventoryValuation.sql
+++ i21Database/dbo/Stored Procedures/uspICRebuildInventoryValuation.sql
@@ -829,6 +829,26 @@ BEGIN
 	END 
 END 
 
+-- Rebuild the G/L Summary 
+BEGIN 
+	DELETE [dbo].[tblGLSummary]
+
+	INSERT INTO tblGLSummary
+	SELECT
+			intAccountId
+			,dtmDate
+			,SUM(ISNULL(dblDebit,0)) as dblDebit
+			,SUM(ISNULL(dblCredit,0)) as dblCredit
+			,SUM(ISNULL(dblDebitUnit,0)) as dblDebitUnit
+			,SUM(ISNULL(dblCreditUnit,0)) as dblCreditUnit
+			,strCode
+			,0 as intConcurrencyId
+	FROM
+		tblGLDetail
+	WHERE ysnIsUnposted = 0	
+	GROUP BY intAccountId, dtmDate, strCode
+END
+
 COMMIT TRANSACTION 
 GOTO _EXIT
 
