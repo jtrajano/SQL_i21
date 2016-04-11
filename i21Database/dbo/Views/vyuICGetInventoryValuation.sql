036473e1b419b96c8856a59f3a94b92d347a882f
Joy Balangatan <joy.balangatan@irely.com>
2016-04-11 19:36:54 +0800

    IC-1727 - Updated vyuICGetInventoryValuation to include items without transaction in the report

--- i21Database/dbo/Views/vyuICGetInventoryValuation.sql
+++ i21Database/dbo/Views/vyuICGetInventoryValuation.sql
@@ -1,42 +1,49 @@
 ﻿CREATE VIEW [dbo].[vyuICGetInventoryValuation]
 AS
 
-SELECT	intInventoryValuationKeyId  = [Transaction].intInventoryTransactionId
-		,[Transaction].intInventoryTransactionId
-		,[Transaction].intItemId
-		,strItemNo = Item.strItemNo
-		,strItemDescription = Item.strDescription
-		,Item.intCategoryId
-		,strCategory = Category.strCategoryCode
-		,[Transaction].intItemLocationId
-		,Location.strLocationName
-		,[Transaction].intSubLocationId
-		,SubLocation.strSubLocationName
-		,[Transaction].intStorageLocationId
-		,strStorageLocationName = StorageLocation.strName
-		,dtmDate = dbo.fnRemoveTimeOnDate([Transaction].dtmDate)
-		,strTransactionType = TransactionType.strName
-		,[Transaction].strTransactionForm
-		,[Transaction].strTransactionId
-		,dblBeginningQtyBalance = CAST(0 AS NUMERIC(38, 20)) 
-		,dblQuantity = [Transaction].dblQty 
-		,dblRunningQtyBalance = CAST(0 AS NUMERIC(38, 20))
-		,dblCost = [Transaction].dblCost
-		,dblBeginningBalance = CAST(0 AS NUMERIC(38, 20))
-		,dblValue = ISNULL([Transaction].dblQty, 0) * ISNULL([Transaction].dblCost, 0) + ISNULL([Transaction].dblValue, 0)
-		,dblRunningBalance = CAST(0 AS NUMERIC(38, 20))
-		,strBatchId
-FROM tblICInventoryTransaction [Transaction] LEFT JOIN tblICItem Item 
-		ON Item.intItemId = [Transaction].intItemId
-	LEFT JOIN tblICCategory Category 
-		ON Category.intCategoryId = Item.intCategoryId
-	LEFT JOIN tblICItemLocation ItemLocation 
-		ON ItemLocation.intItemLocationId = [Transaction].intItemLocationId
-	LEFT JOIN tblSMCompanyLocation Location 
-		ON Location.intCompanyLocationId = ItemLocation.intLocationId
-	LEFT JOIN tblSMCompanyLocationSubLocation SubLocation 
-		ON SubLocation.intCompanyLocationSubLocationId = [Transaction].intSubLocationId
-	LEFT JOIN tblICStorageLocation StorageLocation 
-		ON StorageLocation.intStorageLocationId = [Transaction].intStorageLocationId
-	LEFT JOIN tblICInventoryTransactionType TransactionType 
-		ON TransactionType.intTransactionTypeId = [Transaction].intTransactionTypeId
+SELECT       
+			  CASE 
+				WHEN [Transaction].intInventoryTransactionId IS NULL
+				THEN CAST(ROW_NUMBER() OVER (ORDER BY [Transaction].intInventoryTransactionId) AS INT)
+				ELSE [Transaction].intInventoryTransactionId
+			  END 
+			  AS intInventoryValuationKeyId,
+			  CASE 
+				WHEN [Transaction].intInventoryTransactionId IS NULL
+				THEN CAST(ROW_NUMBER() OVER (ORDER BY [Transaction].intInventoryTransactionId) AS INT)
+				ELSE [Transaction].intInventoryTransactionId
+			  END 
+			  AS intInventoryTransactionId,
+			  ISNULL([Transaction].intItemId, 0) AS intItemId,
+			  Item.strItemNo, 
+			  Item.strDescription AS strItemDescription, 
+              Item.intCategoryId, 
+			  Category.strCategoryCode AS strCategory, 
+			  ISNULL([Transaction].intItemLocationId, 0) AS intItemLocationId, 
+			  ISNULL(Location.strLocationName, ' ') AS strLocationName, 
+			  ISNULL([Transaction].intSubLocationId, 0) AS intSubLocationId, 
+			  ISNULL(SubLocation.strSubLocationName, ' ') AS strSubLocationName, 
+              ISNULL([Transaction].intStorageLocationId, 0) AS intStorageLocationId, 
+			  ISNULL(StorageLocation.strName, ' ') AS strStorageLocationName, 
+			  dbo.fnRemoveTimeOnDate([Transaction].dtmDate) AS dtmDate, 
+			  ISNULL(TransactionType.strName, ' ') AS strTransactionType, 
+              ISNULL([Transaction].strTransactionForm, ' ') AS strTransactionForm, 
+			  ISNULL([Transaction].strTransactionId, ' ') AS strTransactionId, 
+			  CAST(0 AS NUMERIC(38, 20)) AS dblBeginningQtyBalance, 
+			  ISNULL([Transaction].dblQty, 0) AS dblQuantity, 
+			  CAST(0 AS NUMERIC(38, 20)) AS dblRunningQtyBalance, 
+			  ISNULL([Transaction].dblCost, 0) AS dblCost, 
+			  CAST(0 AS NUMERIC(38, 20)) AS dblBeginningBalance, 
+			  ISNULL([Transaction].dblQty, 0) * ISNULL([Transaction].dblCost, 0) + ISNULL([Transaction].dblValue, 0) AS dblValue, 
+			  CAST(0 AS NUMERIC(38, 20)) AS dblRunningBalance, 
+			  ISNULL([Transaction].strBatchId, ' ') AS strBatchId
+
+FROM					 
+			   dbo.tblICItem AS Item  LEFT OUTER JOIN
+			   dbo.tblICInventoryTransaction AS [Transaction] ON [Transaction].intItemId = Item.intItemId LEFT OUTER JOIN
+			   dbo.tblICCategory AS Category ON Category.intCategoryId = Item.intCategoryId LEFT OUTER JOIN
+               dbo.tblICItemLocation AS ItemLocation ON ItemLocation.intItemLocationId = [Transaction].intItemLocationId LEFT OUTER JOIN
+               dbo.tblSMCompanyLocation AS Location ON Location.intCompanyLocationId = ItemLocation.intLocationId LEFT OUTER JOIN
+               dbo.tblSMCompanyLocationSubLocation AS SubLocation ON SubLocation.intCompanyLocationSubLocationId = [Transaction].intSubLocationId LEFT OUTER JOIN
+               dbo.tblICStorageLocation AS StorageLocation ON StorageLocation.intStorageLocationId = [Transaction].intStorageLocationId LEFT OUTER JOIN
+               dbo.tblICInventoryTransactionType AS TransactionType ON TransactionType.intTransactionTypeId = [Transaction].intTransactionTypeId
