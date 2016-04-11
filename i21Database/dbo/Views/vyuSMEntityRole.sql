236f0c0385e400795bc13675459833f26806a5ee
Dan Michael Jalimao <dan.jalimao@irely.com>
2016-04-11 16:30:36 +0800

    FCOM-1611

---
+++ i21Database/dbo/Views/vyuSMEntityRole.sql
@@ -0,0 +1,13 @@
+﻿CREATE VIEW [dbo].[vyuSMEntityRole]
+AS
+SELECT CAST (ROW_NUMBER() OVER (ORDER BY intEntityId DESC) AS INT)	AS	intEntityRoleId, intEntityId, strName, intUserRoleId FROM 
+( 
+	SELECT Entity.intEntityId AS intEntityId, Entity.strName AS strName,  UserRole.intUserRoleID AS intUserRoleId FROM tblSMUserSecurityCompanyLocationRolePermission UserSecurityCompanyLocationRolePermission
+	INNER JOIN tblEMEntity Entity ON UserSecurityCompanyLocationRolePermission.intEntityUserSecurityId = Entity.intEntityId
+	INNER JOIN tblSMUserRole UserRole ON UserSecurityCompanyLocationRolePermission.intUserRoleId = UserRole.intUserRoleID
+	UNION ALL
+	SELECT Entity.intEntityId AS intEntityId, Entity.strName AS strName,  UserRole.intUserRoleID AS intUserRoleId FROM tblSMUserSecurity UserSecurity
+	INNER JOIN tblEMEntity Entity ON UserSecurity.intEntityUserSecurityId = Entity.intEntityId
+	INNER JOIN tblSMUserRole UserRole ON UserSecurity.intUserRoleID = UserRole.intUserRoleID
+) EntityRole
+GO
