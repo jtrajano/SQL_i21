92ffaa27a973401e54eec8184346bc1dc2aa34d3
Joy Balangatan <joy.balangatan@irely.com>
2016-04-12 10:17:22 +0800

    IC-1727 Added items without transaction to vyuICGetInventorySummary

--- i21Database/dbo/Views/vyuICGetInventoryValuationSummary.sql
+++ i21Database/dbo/Views/vyuICGetInventoryValuationSummary.sql
@@ -1,37 +1,34 @@
 ﻿CREATE VIEW [dbo].[vyuICGetInventoryValuationSummary]
 	AS 
 
-SELECT 
-	intInventoryValuationKeyId = CAST(ROW_NUMBER() OVER(ORDER BY [Transaction].intItemId) AS INT),
-	[Transaction].intItemId,
-	strItemNo = Item.strItemNo,
-	strItemDescription = Item.strDescription,
-	[Transaction].intItemLocationId,
-	Location.strLocationName,
-	[Transaction].intSubLocationId,
-	SubLocation.strSubLocationName,
-	dblQuantity,
-	dblValue,
-	dblLastCost = dblQuantity * ItemPricing.dblLastCost,
-	dblStandardCost = dblQuantity * ItemPricing.dblStandardCost,
-	dblAverageCost = dblQuantity * ItemPricing.dblAverageCost
+SELECT        intInventoryValuationKeyId = CAST(ROW_NUMBER() OVER (ORDER BY Item.intItemId) AS INT), 
+			  Item.intItemId, 
+			  strItemNo = Item.strItemNo, 
+			  strItemDescription = Item.strDescription, 
+			  ISNULL([Transaction].intItemLocationId, 0) AS intItemLocationId, 
+			  ISNULL(Location.strLocationName, ' ') AS strLocationName, 
+			  ISNULL([Transaction].intSubLocationId, 0) AS intSubLocationId, 
+			  ISNULL (SubLocation.strSubLocationName, ' ') AS strSubLocationName, 
+			  ISNULL(dblQuantity, 0) AS dblQuantity, 
+			  ISNULL(dblValue, 0) AS dblValue, 
+			  ISNULL((dblQuantity * ItemPricing.dblLastCost), 0) AS dblLastCost, 
+			  ISNULL((dblQuantity * ItemPricing.dblStandardCost),0) AS dblStandardCost, 
+			  ISNULL((dblQuantity * ItemPricing.dblAverageCost),0) AS dblAverageCost
 FROM (
-	SELECT [Transaction].intItemId,
-		[Transaction].intItemLocationId,
-		[Transaction].intSubLocationId,
-		dblQuantity = SUM([Transaction].dblQty * [Transaction].dblUOMQty),
-		dblValue = SUM (
-			ISNULL([Transaction].dblQty, 0)
-			* ISNULL([Transaction].dblCost, 0) 
-			+ ISNULL([Transaction].dblValue, 0)
-		)
-	FROM tblICInventoryTransaction [Transaction]
-	GROUP BY [Transaction].intItemId,
-		[Transaction].intItemLocationId,
-		[Transaction].intSubLocationId
-	) [Transaction]
-LEFT JOIN tblICItem Item ON Item.intItemId = [Transaction].intItemId
-LEFT JOIN tblICItemLocation ItemLocation ON ItemLocation.intItemLocationId = [Transaction].intItemLocationId
-LEFT JOIN tblICItemPricing ItemPricing ON ItemPricing.intItemLocationId = ItemLocation.intItemLocationId
-LEFT JOIN tblSMCompanyLocation Location ON Location.intCompanyLocationId = ItemLocation.intLocationId
-LEFT JOIN tblSMCompanyLocationSubLocation SubLocation ON SubLocation.intCompanyLocationSubLocationId = [Transaction].intSubLocationId
+		SELECT        Item.intItemId, [Transaction].intItemLocationId, 
+					  [Transaction].intSubLocationId, 
+					  dblQuantity = SUM([Transaction].dblQty * [Transaction].dblUOMQty), 
+                      dblValue = SUM(ISNULL([Transaction].dblQty, 0) * ISNULL([Transaction].dblCost, 0) + ISNULL([Transaction].dblValue, 0))
+                  
+		FROM          tblICItem Item LEFT JOIN tblICInventoryTransaction[Transaction] ON [Transaction].intItemId = Item.intItemId
+        GROUP BY	  Item.intItemId, 
+					  [Transaction].intItemLocationId, 
+					  [Transaction].intSubLocationId
+	) 
+	
+	[Transaction] LEFT JOIN
+	tblICItem Item ON Item.intItemId = [Transaction].intItemId LEFT JOIN
+	tblICItemLocation ItemLocation ON ItemLocation.intItemLocationId = [Transaction].intItemLocationId LEFT JOIN
+	tblICItemPricing ItemPricing ON ItemPricing.intItemLocationId = ItemLocation.intItemLocationId LEFT JOIN
+	tblSMCompanyLocation Location ON Location.intCompanyLocationId = ItemLocation.intLocationId LEFT JOIN
+	tblSMCompanyLocationSubLocation SubLocation ON SubLocation.intCompanyLocationSubLocationId = [Transaction].intSubLocationId
