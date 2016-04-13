715c593deabc44f11ab7c341c0f42be694d04787
Emerson Versoza <emerson.versoza@irely.com>
2016-04-13 18:42:22 +0800

    SC-131

--- i21Database/dbo/Views/vyuSCTicketInventoryShipmentView.sql
+++ i21Database/dbo/Views/vyuSCTicketInventoryShipmentView.sql
@@ -17,14 +17,7 @@
 	ICSI.dblPrice,
 	ICSI.dblLineTotal,
 	SC.strTicketNumber,
-	ISNULL(GRST.strStorageTypeDescription, 
-	   CASE 
-			WHEN SC.strDistributionOption = 'CNT' THEN 'Contract'
-			WHEN SC.strDistributionOption = 'LOD' THEN 'Load'
-			WHEN SC.strDistributionOption = 'SPT' THEN 'Spot Sale'
-			WHEN SC.strDistributionOption = 'SPL' THEN 'Split'
-			WHEN SC.strDistributionOption = 'HLD' THEN 'Hold'
-		END ) AS strStorageTypeDescription
+	GRST.strStorageTypeDescription
 	FROM tblSCTicket SC
 	INNER JOIN vyuICGetInventoryShipmentItem ICSI ON SC.intTicketId = ICSI.intSourceId
 	LEFT JOIN tblGRStorageType GRST ON GRST.strStorageTypeCode = SC.strDistributionOption
