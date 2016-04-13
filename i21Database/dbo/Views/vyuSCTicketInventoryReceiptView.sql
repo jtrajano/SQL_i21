36721876dd4b46e184e771888d64df5dd6457e03
Emerson Versoza <emerson.versoza@irely.com>
2016-04-13 18:36:42 +0800

    SC-131

---
+++ i21Database/dbo/Views/vyuSCTicketInventoryReceiptView.sql
@@ -0,0 +1,31 @@
+﻿CREATE VIEW [dbo].[vyuSCTicketInventoryReceiptView]
+	AS SELECT 
+	ICRI.intInventoryReceiptId,
+	ICRI.strReceiptNumber,
+	ICRI.strReceiptType,
+	ICRI.strItemNo,
+	ICRI.strItemDescription,
+	ICRI.strOrderNumber,
+	ICRI.strSourceNumber,
+	ICRI.strUnitMeasure,
+	ICRI.dblQtyToReceive,
+	ICRI.dblUnitCost,
+	ICRI.dblTax,
+	ICRI.dblLineTotal,
+	ICRI.strCostUOM,
+	ICRI.dtmReceiptDate,
+	ICRI.strVendorName,
+	ICRI.strLocationName,
+	ICRI.ysnPosted,
+	SC.strTicketNumber,
+	ISNULL(GRST.strStorageTypeDescription, 
+	   CASE 
+			WHEN SC.strDistributionOption = 'CNT' THEN 'Contract'
+			WHEN SC.strDistributionOption = 'LOD' THEN 'Load'
+			WHEN SC.strDistributionOption = 'SPT' THEN 'Spot Sale'
+			WHEN SC.strDistributionOption = 'SPL' THEN 'Split'
+			WHEN SC.strDistributionOption = 'HLD' THEN 'Hold'
+		END) AS strStorageTypeDescription
+	FROM tblSCTicket SC
+	INNER JOIN vyuICGetInventoryReceiptItem ICRI ON SC.intInventoryReceiptId = ICRI.intInventoryReceiptId
+	LEFT JOIN tblGRStorageType GRST ON GRST.strStorageTypeCode = SC.strDistributionOption
---
+++ i21Database/dbo/Views/vyuSCTicketInventoryShipmentView.sql
@@ -0,0 +1,31 @@
+﻿ALTER VIEW [dbo].[vyuSCTicketInventoryShipmentView]
+	AS SELECT 
+	ICSI.intInventoryShipmentId,
+	ICSI.strShipmentNumber,
+	ICSI.dtmShipDate,
+	ICSI.strOrderType,
+	ICSI.strSourceType,
+	ICSI.strCustomerNumber,
+	ICSI.strCustomerName,
+	ICSI.ysnPosted,
+	ICSI.strItemNo,
+	ICSI.strItemDescription,
+	ICSI.strOrderNumber,
+	ICSI.strSourceNumber,
+	ICSI.strUnitMeasure,
+	ICSI.dblQtyToShip,
+	ICSI.dblPrice,
+	ICSI.dblLineTotal,
+	SC.strTicketNumber,
+	ISNULL(GRST.strStorageTypeDescription, 
+	   CASE 
+			WHEN SC.strDistributionOption = 'CNT' THEN 'Contract'
+			WHEN SC.strDistributionOption = 'LOD' THEN 'Load'
+			WHEN SC.strDistributionOption = 'SPT' THEN 'Spot Sale'
+			WHEN SC.strDistributionOption = 'SPL' THEN 'Split'
+			WHEN SC.strDistributionOption = 'HLD' THEN 'Hold'
+		END ) AS strStorageTypeDescription
+	FROM tblSCTicket SC
+	INNER JOIN vyuICGetInventoryShipmentItem ICSI ON SC.intTicketId = ICSI.intSourceId
+	LEFT JOIN tblGRStorageType GRST ON GRST.strStorageTypeCode = SC.strDistributionOption
+	
