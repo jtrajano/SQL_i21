a97121666466e7dcf318e0634e501562923fa3da
Darwin Estrada <darwin.estrada@irely.com>
2016-04-14 15:29:57 +0800

    AR-2248

--- i21Database/dbo/Tables/tblARCustomer.sql
+++ i21Database/dbo/Tables/tblARCustomer.sql
@@ -84,6 +84,7 @@
 	[intEntityTariffTypeId]			  INT			  NULL,
 	[dblRevenue]						NUMERIC (18, 6) NULL DEFAULT(0),
 	[intEmployeeCount]					INT			  NULL  DEFAULT(0),
+	[ysnIncludeEntityName]				  BIT             DEFAULT ((0)) NOT NULL,
     [intConcurrencyId]                INT             CONSTRAINT [DF_tblARCustomer_intConcurrencyId] DEFAULT ((0)) NOT NULL,
     CONSTRAINT [PK_tblARCustomer] PRIMARY KEY CLUSTERED ([intEntityCustomerId] ASC),	
     CONSTRAINT [FK_tblARCustomer_tblARAccountStatus] FOREIGN KEY ([intAccountStatusId]) REFERENCES [dbo].[tblARAccountStatus] ([intAccountStatusId]),
