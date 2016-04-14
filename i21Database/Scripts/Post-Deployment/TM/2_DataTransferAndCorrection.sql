feaf6540cd8b1945f86ab7465f2a07a3c019656a
kenneth lagunero <kenneth.lagunero@irely.com>
2016-04-14 17:46:02 +0800

    TM-1792

--- i21Database/Scripts/Post-Deployment/TM/2_DataTransferAndCorrection.sql
+++ i21Database/Scripts/Post-Deployment/TM/2_DataTransferAndCorrection.sql
@@ -1,5 +1,5 @@
 ﻿print N'BEGIN CONVERSION - i21 TANK MANAGEMENT..'
-
+GO
 print N'BEGIN Migration of Data from preference company to lease code'
 IF EXISTS(SELECT TOP 1 1 FROM sys.columns WHERE name = N'intLeaseItemId' AND Object_ID = Object_ID(N'tblTMPreferenceCompany'))
 BEGIN
@@ -13,4 +13,30 @@ BEGIN
 		ALTER TABLE tblTMPreferenceCompany DROP COLUMN intLeaseItemId 
 	')
 END
+GO
 print N'END Migration of Data from preference company to lease code'
+GO
+
+print N'BEGIN Populate global julian calendar for site'
+GO
+IF EXISTS(SELECT TOP 1 1 FROM sys.columns WHERE name = N'intGlobalJulianCalendarId' AND Object_ID = Object_ID(N'tblTMSite'))
+BEGIN
+	EXEC ('
+		UPDATE tblTMSite
+		SET intGlobalJulianCalendarId = (SELECT TOP 1 intGlobalJulianCalendarId 
+											FROM tblTMGlobalJulianCalendar
+											WHERE ysnDefault = 1)
+		WHERE intGlobalJulianCalendarId IS NULL 
+			AND intFillMethod = (SELECT TOP 1 
+								intFillMethodId 
+							FROM tblTMFillMethod 
+							WHERE strFillMethod = ''Julian Calendar'')
+	')
+
+	EXEC('
+		ALTER TABLE tblTMPreferenceCompany DROP COLUMN intLeaseItemId 
+	')
+END
+GO
+print N'END Populate global julian calendar for site'
+GO
