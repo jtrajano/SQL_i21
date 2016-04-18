4d25cb0b6c4290ff4d7ea17e9106196c41e1b91a
Emerson Versoza <emerson.versoza@irely.com>
2016-04-18 19:33:53 +0800

    GRN-396

--- i21Database/dbo/Views/vyuGRGetStorageTransferTicket.sql
+++ i21Database/dbo/Views/vyuGRGetStorageTransferTicket.sql
@@ -7,24 +7,25 @@ SELECT TOP 100 PERCENT
  ,E.strName  
  ,a.intStorageTypeId  
  ,b.strStorageTypeDescription
- ,b.ysnCustomerStorage
- ,a.intStorageScheduleId
- ,SR.strScheduleId  
+,b.ysnCustomerStorage
+,a.intStorageScheduleId
+,SR.strScheduleId  
  ,a.intCommodityId  
  ,CM.strCommodityCode  
  ,CM.strDescription
- ,a.intItemId
- ,Item.strItemNo   
+,a.intItemId
+,Item.strItemNo   
  ,a.intCompanyLocationId  
  ,c.strLocationName
- ,ISNULL(a.intCompanyLocationSubLocationId,0) intCompanyLocationSubLocationId
- ,ISNULL(Sub.strSubLocationName,'') strSubLocationName 
+,ISNULL(a.intCompanyLocationSubLocationId,0) intCompanyLocationSubLocationId
+,ISNULL(Sub.strSubLocationName,'') strSubLocationName 
  ,a.dtmDeliveryDate  
  ,ISNULL(a.strDPARecieptNumber,'')strDPARecieptNumber  
  ,a.dblOpenBalance
- ,b.ysnDPOwnedType
- ,SH.intContractHeaderId
- ,CD.strContractNumber   
+,b.ysnDPOwnedType
+,SH.intContractHeaderId
+,CD.strContractNumber   
+ ,ISNULL(SH1.intTicketId,0) AS intTicketId
 FROM tblGRCustomerStorage a  
 JOIN tblGRStorageType b ON b.intStorageScheduleTypeId = a.intStorageTypeId  
 JOIN tblSMCompanyLocation c ON c.intCompanyLocationId = a.intCompanyLocationId  
@@ -34,6 +35,7 @@ JOIN tblGRStorageScheduleRule SR ON SR.intStorageScheduleRuleId=a.intStorageSche
 JOIN tblICItem Item ON Item.intItemId = a.intItemId
 LEFT JOIN tblSMCompanyLocationSubLocation Sub ON Sub.intCompanyLocationSubLocationId=a.intCompanyLocationSubLocationId
 LEFT JOIN tblGRStorageHistory SH ON SH.intCustomerStorageId=a.intCustomerStorageId
+LEFT JOIN tblGRStorageHistory SH1 ON SH1.intCustomerStorageId=a.intCustomerStorageId AND SH.strType='From Scale'
 LEFT JOIN vyuCTContractDetailView CD ON CD.intContractHeaderId=SH.intContractHeaderId  
 Where a.dblOpenBalance >0 AND ISNULL(a.strStorageType,'') <> 'ITR'AND SH.strType IN('From Scale','From Transfer')  
 ORDER BY a.dtmDeliveryDate
