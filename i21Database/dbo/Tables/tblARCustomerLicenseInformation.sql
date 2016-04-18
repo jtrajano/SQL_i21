34b19b9d7e63346c09ccfb7f13c6e4261b3a3d45
Mon Gonzales <mon.gonzales@irely.com>
2016-04-18 19:44:52 +0800

    EM-310

--- i21Database/dbo/Tables/tblARCustomerLicenseInformation.sql
+++ i21Database/dbo/Tables/tblARCustomerLicenseInformation.sql
@@ -1,10 +1,14 @@
 ﻿CREATE TABLE [dbo].[tblARCustomerLicenseInformation]
 (
-	[intCustomerLicenseInformationId]	INT NOT NULL,
+	[intCustomerLicenseInformationId]	INT NOT NULL IDENTITY(1,1),
 	[intEntityCustomerId]				INT NOT NULL,
 	[strCompanyId]						NVARCHAR(100) COLLATE Latin1_General_CI_AS NOT NULL ,
 	[intNumberOfUser]					INT NOT NULL,
 	[intNumberOfSite]					INT NOT NULL,
+	[dtmDateIssued]						DATETIME NOT NULL DEFAULT(GETDATE()),
+	[dtmDateExpiration]						DATETIME NOT NULL DEFAULT(GETDATE()),
+	[strLicenseKey]						NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NOT NULL ,
+	
     [intConcurrencyId]					INT CONSTRAINT [DF_tblARCustomerLicenseInformation_intConcurrencyId] DEFAULT ((0)) NOT NULL,
 	
 	CONSTRAINT [PK_tblARCustomerLicenseInformation] PRIMARY KEY CLUSTERED ([intCustomerLicenseInformationId] ASC),
--- i21Database/dbo/Tables/tblARCustomerLicenseModule.sql
+++ i21Database/dbo/Tables/tblARCustomerLicenseModule.sql
@@ -1,11 +1,11 @@
 ﻿CREATE TABLE [dbo].[tblARCustomerLicenseModule]
 (
-	[intCustomerLicenseModuleId]			INT NOT NULL,
+	[intCustomerLicenseModuleId]			INT NOT NULL IDENTITY(1,1),
 	[intCustomerLicenseInformationId]		INT NOT NULL,
 	[strModuleName]							NVARCHAR(100) COLLATE Latin1_General_CI_AS NOT NULL ,
-
     [intConcurrencyId]						INT CONSTRAINT [DF_tblARCustomerLicenseModule_intConcurrencyId] DEFAULT ((0)) NOT NULL,
 
 	CONSTRAINT [PK_tblARCustomerLicenseModule] PRIMARY KEY CLUSTERED ([intCustomerLicenseModuleId] ASC),
-	CONSTRAINT [FK_tblARCustomerLicenseModule_tblARCustomerLicenseInformation] FOREIGN KEY ([intCustomerLicenseInformationId]) REFERENCES [dbo].[tblARCustomerLicenseInformation] ([intCustomerLicenseInformationId]) ON DELETE CASCADE,
+	CONSTRAINT [FK_tblARCustomerLicenseModule_tblARCustomerLicenseInformation] FOREIGN KEY ([intCustomerLicenseInformationId]) REFERENCES [dbo].[tblARCustomerLicenseInformation] ([intCustomerLicenseInformationId]) ON DELETE CASCADE,	
+	CONSTRAINT [UK_tblARCustomerLicenseModule_strModuleName_intCustomerLicenseInformationId] UNIQUE NONCLUSTERED ([strModuleName] ASC, [intCustomerLicenseInformationId] ASC)	
 )
