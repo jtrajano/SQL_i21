9c4c4810a3818867e491fb5f48945b23e1556fe6
Mark Paragas <mark.paragas@irely.com>
2016-04-19 19:12:49 +0800

    MFT - Add tables for States

---
+++ i21Database/dbo/Tables/tblTFOriginDestinationState.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[tblTFOriginDestinationState](
+	[intOriginDestinationStateId] [int] IDENTITY(1,1) NOT NULL,
+	[strOriginDestinationState] [nvarchar](10) NOT NULL,
+	[intConcurrencyId] [int] NULL,
+ CONSTRAINT [PK_tblTFOriginDestination] PRIMARY KEY CLUSTERED 
+(
+	[intOriginDestinationStateId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
---
+++ i21Database/dbo/Tables/tblTFValidDestinationState.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE [dbo].[tblTFValidDestinationState](
+	[intValidDestinationStateId] [int] IDENTITY(1,1) NOT NULL,
+	[intReportingComponentDetailId] [int] NOT NULL,
+	[intDestinationStateId] [int] NOT NULL,
+	[strDestinationState] [nvarchar](10) COLLATE Latin1_General_CI_AS NOT NULL,
+	[intConcurrencyId] [int] NULL,
+ CONSTRAINT [PK_tblTFValidDestinationState] PRIMARY KEY CLUSTERED 
+(
+	[intValidDestinationStateId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[tblTFValidDestinationState]  WITH CHECK ADD  CONSTRAINT [FK_tblTFOriginDestinationState_tblTFReportingComponentDetail] FOREIGN KEY([intReportingComponentDetailId])
+REFERENCES [dbo].[tblTFReportingComponentDetail] ([intReportingComponentDetailId])
+GO
+
+ALTER TABLE [dbo].[tblTFValidDestinationState] CHECK CONSTRAINT [FK_tblTFOriginDestinationState_tblTFReportingComponentDetail]
+GO
+
