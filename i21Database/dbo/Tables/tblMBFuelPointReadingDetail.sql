50f57ea37907ba4c2af97481c9f4247fac459f36
Lawrence Zabala <lawrence.zabala@irely.com>
2016-04-19 13:50:38 +0800

    MB-38

--- i21Database/dbo/Tables/tblMBFuelPointReadingDetail.sql
+++ i21Database/dbo/Tables/tblMBFuelPointReadingDetail.sql
@@ -3,13 +3,12 @@
 	[intFuelPointReadingDetailId] INT NOT NULL IDENTITY, 
     [intFuelPointReadingId] INT NOT NULL, 
     [dtmDate] DATETIME NOT NULL DEFAULT (GETDATE()), 
-    [strFuelingPoint] NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL, 
-    [intItemId] INT NOT NULL, 
+    [strFuelingPoint] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
+	[strProductNo] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL,
 	[dblVolume] NUMERIC(18, 6) NULL DEFAULT ((0)), 
     [dblPrice] NUMERIC(18, 6) NULL DEFAULT ((0)), 
     [intSort] INT NULL, 
     [intConcurrencyId] INT NULL DEFAULT ((0)), 
     CONSTRAINT [PK_tblMBFuelPointReadingDetail] PRIMARY KEY ([intFuelPointReadingDetailId]), 
-    CONSTRAINT [FK_tblMBFuelPointReadingDetail_tblMBFuelPointReading] FOREIGN KEY ([intFuelPointReadingId]) REFERENCES [tblMBFuelPointReading]([intFuelPointReadingId]) ON DELETE CASCADE, 
-    CONSTRAINT [FK_tblMBFuelPointReadingDetail_tblICItem] FOREIGN KEY ([intItemId]) REFERENCES [tblICItem]([intItemId])
+    CONSTRAINT [FK_tblMBFuelPointReadingDetail_tblMBFuelPointReading] FOREIGN KEY ([intFuelPointReadingId]) REFERENCES [tblMBFuelPointReading]([intFuelPointReadingId]) ON DELETE CASCADE
 )
--- i21Database/dbo/Views/vyuMBGetFuelPointReadingDetail.sql
+++ i21Database/dbo/Views/vyuMBGetFuelPointReadingDetail.sql
@@ -5,11 +5,8 @@ SELECT FPReadingDetail.intFuelPointReadingDetailId
 	, FPReadingDetail.intFuelPointReadingId
 	, FPReadingDetail.dtmDate
 	, FPReadingDetail.strFuelingPoint
-	, FPReadingDetail.intItemId
-	, Item.strItemNo
-	, strItemDescription = Item.strDescription
+	, FPReadingDetail.strProductNo
 	, FPReadingDetail.dblVolume
 	, FPReadingDetail.dblPrice
 	, FPReadingDetail.intSort
 FROM tblMBFuelPointReadingDetail FPReadingDetail
-LEFT JOIN tblICItem Item ON Item.intItemId = FPReadingDetail.intItemId
