e1e4f6e8dec49d99831bdbfb1d507a7d07cbab11
Kelvin Quinto <kelvin.quinto@irely.com>
2016-04-19 15:20:19 +0800

    AR-2640

--- i21Database/dbo/Tables/tblARQuoteTemplate.sql
+++ i21Database/dbo/Tables/tblARQuoteTemplate.sql
@@ -5,5 +5,6 @@
 	[strOrganization] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
     [ysnDisplayTitle] BIT NULL,     
     [intConcurrencyId] INT NOT NULL,
-	CONSTRAINT [PK_tblARQuoteTemplate_intQuoteTemplateId] PRIMARY KEY CLUSTERED ([intQuoteTemplateId] ASC)
+	CONSTRAINT [PK_tblARQuoteTemplate_intQuoteTemplateId] PRIMARY KEY CLUSTERED ([intQuoteTemplateId] ASC),
+	CONSTRAINT [UK_tblARQuoteTemplate_strTemplateName] UNIQUE (strTemplateName)
 )
