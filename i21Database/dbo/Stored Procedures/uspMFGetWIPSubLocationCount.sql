83f6f4e1e228feee2c8a5dac729998bea838c3a8
Premkumar S <prem.selladurai@irely.com>
2016-04-22 19:41:19 +0530

    MFG-1014

--- i21Database/dbo/Stored Procedures/uspMFGetWIPSubLocationCount.sql
+++ i21Database/dbo/Stored Procedures/uspMFGetWIPSubLocationCount.sql
@@ -6,25 +6,33 @@
 	)
 AS
 BEGIN
-	SELECT Distinct Count(*) As SubLocationCount
-	FROM dbo.tblSMCompanyLocationSubLocation SL
-	JOIN tblMFMachine M ON M.intSubLocationId = SL.intCompanyLocationSubLocationId
-	JOIN tblMFManufacturingProcessMachine PM ON PM.intMachineId = M.intMachineId
-	WHERE SL.intCompanyLocationId = @intLocationId
-		AND SL.strSubLocationName LIKE @strSubLocationName + '%'
-		AND SL.intCompanyLocationSubLocationId = (
-			CASE 
-				WHEN @intSubLocationId > 0
-					THEN @intSubLocationId
-				ELSE SL.intCompanyLocationSubLocationId
-				END
-			)
-		AND PM.intManufacturingProcessId = (
-			CASE 
-				WHEN @intManufacturingProcessId > 0
-					THEN @intManufacturingProcessId
-				ELSE PM.intManufacturingProcessId
-				END
-			) and SL.strClassification='WIP'
-
+	SELECT Count(*) AS SubLocationCount
+	FROM (
+		SELECT DISTINCT SL.intCompanyLocationSubLocationId
+			,SL.intCompanyLocationId
+			,SL.strSubLocationName
+			,SL.strSubLocationDescription
+			,SL.strClassification
+			,SL.intConcurrencyId
+		FROM dbo.tblSMCompanyLocationSubLocation SL
+		JOIN tblMFMachine M ON M.intSubLocationId = SL.intCompanyLocationSubLocationId
+		JOIN tblMFManufacturingProcessMachine PM ON PM.intMachineId = M.intMachineId
+		WHERE SL.intCompanyLocationId = @intLocationId
+			AND SL.strSubLocationName LIKE @strSubLocationName + '%'
+			AND SL.intCompanyLocationSubLocationId = (
+				CASE 
+					WHEN @intSubLocationId > 0
+						THEN @intSubLocationId
+					ELSE SL.intCompanyLocationSubLocationId
+					END
+				)
+			AND PM.intManufacturingProcessId = (
+				CASE 
+					WHEN @intManufacturingProcessId > 0
+						THEN @intManufacturingProcessId
+					ELSE PM.intManufacturingProcessId
+					END
+				)
+			AND SL.strClassification = 'WIP'
+		) AS DT
 END
