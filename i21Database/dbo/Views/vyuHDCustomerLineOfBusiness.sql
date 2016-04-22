101563920de5d2074fc099077c3329877cb144a9
Jayson Ramos <jayson.ramos@iRely.com>
2016-04-22 11:09:23 +0800

    HD-1136

---
+++ i21Database/dbo/Views/vyuHDCustomerLineOfBusiness.sql
@@ -0,0 +1,11 @@
+﻿CREATE VIEW [dbo].[vyuHDCustomerLineOfBusiness]
+	AS
+		select
+			tblARCustomerLineOfBusiness.intCustomerLineOfBusinessId
+			,tblARCustomerLineOfBusiness.intEntityCustomerId
+			,tblHDLineOfBusiness.*
+		from
+			tblARCustomerLineOfBusiness
+			,tblHDLineOfBusiness
+		where
+			tblHDLineOfBusiness.intLineOfBusinessId = tblARCustomerLineOfBusiness.intLineOfBusinessId
