ecb78f627ea3ed9c9b1c8fb570b0d2ab4f8e549b
Jayson Ramos <jayson.ramos@iRely.com>
2016-04-22 11:16:58 +0800

    HD-1136

--- i21Database/dbo/Views/vyuHDCustomerLineOfBusiness.sql
+++ i21Database/dbo/Views/vyuHDCustomerLineOfBusiness.sql
@@ -1,11 +1,14 @@
 ﻿CREATE VIEW [dbo].[vyuHDCustomerLineOfBusiness]
 	AS
-		select
+		select distinct
 			tblARCustomerLineOfBusiness.intCustomerLineOfBusinessId
 			,tblARCustomerLineOfBusiness.intEntityCustomerId
 			,tblHDLineOfBusiness.*
+			,strSalesPerson = tblEMEntity.strName
 		from
 			tblARCustomerLineOfBusiness
 			,tblHDLineOfBusiness
+			,tblEMEntity
 		where
 			tblHDLineOfBusiness.intLineOfBusinessId = tblARCustomerLineOfBusiness.intLineOfBusinessId
+			and tblEMEntity.intEntityId = tblHDLineOfBusiness.intEntityId
