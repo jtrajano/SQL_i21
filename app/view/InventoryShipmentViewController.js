3506198dc632cbc1cf2d9d43bcd38f787db9f178
Chakra <CBehera@irely.net>
2016-04-26 20:43:26 +0530

    MFG-1633

--- app/view/InventoryShipmentViewController.js
+++ app/view/InventoryShipmentViewController.js
@@ -1822,23 +1822,31 @@ Ext.define('Inventory.view.InventoryShipmentViewController', {
 
     onPrintBOLClick: function(button, e, eOpts) {
         var win = button.up('window');
-        var vm = win.viewModel;
-        var current = vm.data.current;
-
-        var filters = [{
-            Name: 'strShipmentNumber',
-            Type: 'string',
-            Condition: 'EQUAL TO',
-            From: current.get('strShipmentNumber'),
-            Operator: 'AND'
-        }];
 
         // Save has data changes first before doing the post.
         win.context.data.saveRecord({
             callbackFn: function() {
+                var vm = win.viewModel;
+                var current = vm.data.current;
+
+                var filters = [{
+                    Name: 'strShipmentNumber',
+                    Type: 'string',
+                    Condition: 'EQUAL TO',
+                    From: current.get('strShipmentNumber'),
+                    Operator: 'AND'
+                },
+                    {
+                        Name: 'strOrderType',
+                        Type: 'string',
+                        Condition: 'EQUAL TO',
+                        From: current.get('strOrderType'),
+                        Operator: 'AND'
+                    }];
+
                 iRely.Functions.openScreen('Reporting.view.ReportViewer', {
-                    selectedReport: 'BillOfLading',
-                    selectedGroup: 'Inventory',
+                    selectedReport: 'BillOfLadingReport',
+                    selectedGroup: 'Manufacturing',
                     selectedParameters: filters
                 });
             }
