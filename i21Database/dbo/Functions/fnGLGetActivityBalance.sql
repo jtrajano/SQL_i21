2cdfad1bc06477d47b660fc855a192dcb66b5edc
jeffrey trajano <jeffrey.trajano@irely.com>
2016-04-26 12:56:46 +0800

    GL-2420 Be able to see balance on the "GL Account Detail" Grid

---
+++ i21Database/dbo/Functions/fnGLGetActivityBalance.sql
@@ -0,0 +1,84 @@
+﻿CREATE FUNCTION [dbo].[fnGLGetActivityBalance] 
+(	
+	@strAccountId NVARCHAR(100),
+	@dtmDate DATETIME,
+	@dtmStartDate DATETIME, -- DATE GIVEN BY THE USER IN GL DETAIL SEARCH SCREEN
+	@dtmEndDate DATETIME, -- DATE GIVEN BY THE USER IN GL DETAIL SEARCH SCREEN
+	@strBalanceType NVARCHAR(30)
+)
+RETURNS @tbl TABLE (
+strAccountId NVARCHAR(100),
+beginBalance NUMERIC (18,6),
+beginBalanceUnit NUMERIC(18,6)
+)
+
+AS
+BEGIN
+	-- *BOY = BEGINNING OF FISCAL YEAR
+	-- *BOT = BEGINNING OF TIME
+	--  NOTE : EXPENSE AND REVENUE BEGINNING BALANCE IS COMPUTED VIA *BOY WHILE OTHER ARE COMPUTE VIA *BOT
+	IF EXISTS(SELECT TOP 1 1 FROM tblGLAccount A JOIN tblGLFiscalYear B ON A.intAccountId = B.intRetainAccount WHERE A.strAccountId = @strAccountId)
+	BEGIN
+		;WITH cte as(
+		SELECT  
+				 @strAccountId AS strAccountId,
+				(dblCredit - dblDebit) as beginbalance,
+				(dblCreditUnit - dblDebitUnit) as beginbalanceunit
+		  
+		FROM tblGLAccount A
+			LEFT JOIN tblGLAccountGroup B ON A.intAccountGroupId = B.intAccountGroupId
+			LEFT JOIN tblGLSummary C ON A.intAccountId = C.intAccountId
+		WHERE
+		(B.strAccountType in ('Expense','Revenue')  and C.dtmDate < @dtmStartDate  AND @strBalanceType = 'YTD' ) OR
+		(strAccountId =@strAccountId AND C.dtmDate BETWEEN @dtmStartDate AND @dtmDate)   and strCode <> ''
+		
+		)
+		insert into @tbl
+		select strAccountId, sum(beginbalance) beginBalance ,sum(beginbalanceunit) beginBalanceUnit from cte group by strAccountId
+		
+		RETURN 
+	END
+
+	DECLARE @accountType NVARCHAR(30)
+	SELECT @accountType= B.strAccountType  FROM tblGLAccount A JOIN tblGLAccountGroup B on A.intAccountGroupId = B.intAccountGroupId WHERE
+	A.strAccountId = @strAccountId and B.strAccountType IN ('Expense','Revenue','Cost of Goods Sold')
+	IF @accountType IS NOT NULL
+			INSERT  @tbl
+			SELECT  
+					strAccountId,
+					SUM ( CASE 
+						  WHEN @dtmStartDate IS NULL THEN 0 
+						  WHEN @accountType = 'Revenue' THEN (dblCredit - dblDebit) * -1
+						  ELSE dblDebit - dblCredit
+					END)  beginBalance,
+					SUM( 
+					CASE 
+						WHEN @dtmStartDate IS NULL THEN 0 
+						WHEN @accountType = 'Revenue' THEN (dblCreditUnit - dblDebitUnit) * -1
+							ELSE dblDebitUnit - dblCreditUnit
+					END)  beginBalanceUnit
+			FROM tblGLAccount A
+				LEFT JOIN tblGLAccountGroup B ON A.intAccountGroupId = B.intAccountGroupId
+				LEFT JOIN tblGLSummary C ON A.intAccountId = C.intAccountId
+			WHERE strAccountId = @strAccountId and ( C.dtmDate BETWEEN @dtmStartDate AND @dtmEndDate) and strCode <> ''
+			GROUP BY strAccountId
+	ELSE
+		INSERT  @tbl
+		SELECT  
+				strAccountId,
+				SUM( 
+				CASE WHEN B.strAccountType = 'Asset' THEN dblDebit - dblCredit
+						ELSE (dblCredit - dblDebit) * -1
+				END)  beginBalance,
+				SUM( 
+				CASE WHEN B.strAccountType = 'Asset' THEN dblDebitUnit - dblCreditUnit
+						ELSE dblCreditUnit - dblDebitUnit * -1
+				END)  beginBalanceUnit
+		
+		FROM tblGLAccount A
+			LEFT JOIN tblGLAccountGroup B ON A.intAccountGroupId = B.intAccountGroupId
+			LEFT JOIN tblGLSummary C ON A.intAccountId = C.intAccountId
+		WHERE strAccountId = @strAccountId and  C.dtmDate BETWEEN @dtmStartDate AND @dtmEndDate and strCode <> ''
+		GROUP BY strAccountId
+		RETURN
+END
---
+++ i21Database/dbo/Functions/fnGLGetBalancesDateCriteria.sql
@@ -0,0 +1,57 @@
+﻿CREATE FUNCTION [dbo].[fnGLGetBalancesDateCriteria]
+(
+@dtmDate DATETIME,
+@strAccountId NVARCHAR(30),
+@strBalanceType NVARCHAR(30)
+)
+RETURNS @tbl TABLE 
+(
+dtmDateFrom DATETIME,
+dtmDateTo DATETIME
+)
+
+AS 
+
+BEGIN
+
+DECLARE @dtmDate1 DATETIME
+DECLARE @dtmDate2 DATETIME
+DECLARE @strAccountType NVARCHAR(50)
+DECLARE @reAccount BIT = 0
+SELECT @strAccountType=G.strAccountType from tblGLAccount A JOIN tblGLAccountGroup G ON A.intAccountGroupId = G.intAccountGroupId WHERE strAccountId = @strAccountId 
+SELECT @reAccount = 1  from tblGLFiscalYear F JOIN tblGLAccount A ON F.intRetainAccount = A.intAccountId WHERE A.strAccountId = @strAccountId
+
+	;WITH Cte AS(
+
+	
+	SELECT 
+			TOP 1
+			CASE WHEN @strBalanceType= 'YTD' OR @strBalanceType= 'OPENING' THEN 
+					CASE WHEN @reAccount = 1 OR @strAccountType IN ('Expense','Revenue','Cost of Goods Sold')
+						 THEN FiscalYear.dtmDateFrom
+						 ELSE CAST('1900/01/01' AS datetime)--ALE
+					 	 
+					END
+				WHEN @strBalanceType ='MTD' THEN FiscalPeriod.dtmStartDate
+			END
+			dtmDate1,
+			CASE WHEN @strBalanceType= 'YTD' THEN 
+					CASE  WHEN @reAccount = 1 THEN @dtmDate
+					 	 ELSE FiscalPeriod.dtmEndDate
+					END
+				WHEN @strBalanceType ='MTD' THEN FiscalPeriod.dtmStartDate
+				WHEN @strBalanceType= 'OPENING' THEN @dtmDate
+			END
+			dtmDate2
+		FROM
+
+		tblGLFiscalYear FiscalYear,
+		tblGLFiscalYearPeriod FiscalPeriod
+		WHERE @dtmDate BETWEEN FiscalYear.dtmDateFrom AND FiscalYear.dtmDateTo
+		AND @dtmDate BETWEEN FiscalPeriod.dtmStartDate AND FiscalPeriod.dtmEndDate
+
+	)
+	INSERT INTO @tbl(dtmDateFrom,dtmDateTo)
+	SELECT dtmDate1,dtmDate2 from Cte
+RETURN
+END
