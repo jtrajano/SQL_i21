3ca14c1ff4688ee87752965605695060c59ec97d
James Balachander <balachander.j@irely.com>
2016-04-27 10:03:59 +0530

    LG-220

--- i21Database/dbo/Tables/tblLGCompanyPreference.sql
+++ i21Database/dbo/Tables/tblLGCompanyPreference.sql
@@ -10,6 +10,8 @@
 [intDefaultShipmentSourceType] INT NULL,
 [intDefaultTransportationMode] INT NULL,
 [intDefaultPositionId] INT NULL,
+[intDefaultLeastCostSourceType] INT NULL,
+[strALKMapKey] [nvarchar](200) COLLATE Latin1_General_CI_AS NULL,
 
 CONSTRAINT [PK_tblLGCompanyPreference] PRIMARY KEY ([intCompanyPreferenceId]), 
 CONSTRAINT [FK_tblLGCompanyPreference_tblICCommodity_intCommodityId] FOREIGN KEY ([intCommodityId]) REFERENCES [tblICCommodity]([intCommodityId]),
