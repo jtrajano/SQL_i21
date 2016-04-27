17cd68f3b41f5885f546f5dafec67a7bc8dbcfe9
jeffrey trajano <jeffrey.trajano@irely.com>
2016-04-27 18:25:08 +0800

    GL-2658 Cannot filter GL by Account ID detail by strTransactionId

--- i21Database/dbo/Tables/tblGLTempCOASegment.sql
+++ i21Database/dbo/Tables/tblGLTempCOASegment.sql
@@ -1,8 +1,11 @@
-﻿CREATE TABLE [dbo].[tblGLTempCOASegment]
-(
+﻿CREATE TABLE [dbo].[tblGLTempCOASegment](
 	[intAccountId] [int] NOT NULL,
 	[strAccountId] [nvarchar](40) NOT NULL,
 	[Primary Account] [nvarchar](20) NULL,
 	[Location] [nvarchar](20) NULL,
-	[LOB] [nvarchar](20) NULL
-)ON [PRIMARY]
+	[LOB] [nvarchar](20) NULL,
+ CONSTRAINT [PK_tblGLTempCOASegment] PRIMARY KEY CLUSTERED 
+(
+	[intAccountId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
