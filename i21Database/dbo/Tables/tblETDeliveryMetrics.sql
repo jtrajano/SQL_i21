8c3b81fdbe348de2d560985bf7b3bc30f3c4d34a
Manuel Armas <manny.armas@irely.com>
2016-04-27 17:00:48 +0800

    IET-7 - Create i21 table for ET Metrics.

--- i21Database/dbo/Tables/tblETDeliveryMetrics.sql
+++ i21Database/dbo/Tables/tblETDeliveryMetrics.sql
@@ -1,11 +1,14 @@
 ﻿CREATE TABLE [dbo].[tblETDeliveryMetrics]
 (
-	[intDeliveryMetricsId] INT NOT NULL PRIMARY KEY, 
+    [intDeliveryMetricsId] INT NOT NULL , 
     [intBeginningOdometerReading ] INT NULL, 
     [intEndingOdometerReading] INT NULL, 
     [dblGallonsDelivered] NUMERIC(18, 6) NULL DEFAULT 0, 
     [dblTotalFuelSales] NUMERIC(18, 6) NULL DEFAULT 0, 
     [intTotalInvoice] INT NULL, 
     [strDriverNumber] NVARCHAR(50) NULL, 
-    [strTruckNumber] NVARCHAR(50) NULL
+    [strTruckNumber] NVARCHAR(50) NULL, 
+    [strShiftNumber] NVARCHAR(10) NULL, 
+    [dtmShiftDate] DATETIME NULL, 
+    CONSTRAINT [PK_tblETDeliveryMetrics] PRIMARY KEY ([intDeliveryMetricsId])
 )
