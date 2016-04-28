9514f429885b61b1efdc0717b89b88ee1eff8390
Dan Michael Jalimao <dan.jalimao@irely.com>
2016-04-28 15:53:53 +0800

    FCOM-1689

---
+++ i21Database/dbo/Tables/tblSMApproval.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[tblSMApproval]
+(
+	[intApprovalId] INT NOT NULL PRIMARY KEY IDENTITY,
+	[intTransactionId] [int] NOT NULL,
+	[intApproverId] [int] NULL,
+	[strTransactionNumber] [nvarchar](50) NOT NULL,
+	[intSubmittedById] [int] NOT NULL,
+	[dblAmount] [numeric](18, 6) NOT NULL,
+	[dtmDueDate] [datetime] NOT NULL,
+	[strStatus] [nvarchar](15) NOT NULL,
+	[strComment] [nvarchar](max) NOT NULL,
+	[dtmDate] [datetime] NOT NULL,
+	[intConcurrencyId] [int] NOT NULL DEFAULT ((1)), 
+    CONSTRAINT [FK_tblSMApproval_tblSMTransaction] FOREIGN KEY ([intTransactionId]) REFERENCES [tblSMTransaction]([intTransactionId]), 
+    CONSTRAINT [FK_tblSMApproval_tblEMEntity_Approver] FOREIGN KEY ([intApproverId]) REFERENCES [tblEMEntity]([intEntityId]), 
+    CONSTRAINT [FK_tblSMApproval_tblEMEntity_SubmitterBy] FOREIGN KEY ([intSubmittedById]) REFERENCES [tblEMEntity]([intEntityId])
+)
---
+++ i21Database/dbo/Tables/tblSMTransaction.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[tblSMTransaction]
+(
+	[intTransactionId] INT NOT NULL PRIMARY KEY IDENTITY,
+	[intScreenId] [int] NOT NULL DEFAULT ((1)),
+	[strRecordNo] [nvarchar](50) NOT NULL,
+	[intConcurrencyId] [int] NOT NULL DEFAULT ((1)), 
+    CONSTRAINT [FK_tblSMTransaction_tblSMScreen] FOREIGN KEY ([intScreenId]) REFERENCES [tblSMScreen]([intScreenId])
+)
