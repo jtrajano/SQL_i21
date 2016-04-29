80ceb6369a9a1b2f12004e5432cfa6e8c30b4d80
Jayson Ramos <jayson.ramos@iRely.com>
2016-04-29 11:37:37 +0800

    HD-1303

---
+++ i21Database/dbo/Views/vyuHDOpportunityCampaignSearch.sql
@@ -0,0 +1,13 @@
+﻿CREATE VIEW [dbo].[vyuHDOpportunityCampaignSearch]
+	AS
+	select
+		tblHDOpportunityCampaign.*
+		,tblEMEntity.strName
+		,tblHDLineOfBusiness.strLineOfBusiness
+	from
+		tblHDOpportunityCampaign
+		,tblEMEntity
+		,tblHDLineOfBusiness
+	where
+		tblEMEntity.intEntityId = tblHDOpportunityCampaign.intEntityId
+		and tblHDLineOfBusiness.intLineOfBusinessId = tblHDOpportunityCampaign.intLineOfBusinessId
