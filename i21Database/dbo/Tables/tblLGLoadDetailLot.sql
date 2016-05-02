442381365360a45180e0558b2d2829663065768c
Swetosuvro Ghosh <swetosuvro.g@irely.com>
2016-05-02 22:26:39 +0530

    LG-370

--- i21Database/dbo/Tables/tblLGLoadDetailLot.sql
+++ i21Database/dbo/Tables/tblLGLoadDetailLot.sql
@@ -15,6 +15,7 @@
 	[intConcurrencyId] INT NULL DEFAULT ((0)),
 
 	CONSTRAINT [PK_tblLGLoadDetailLot_intLoadDetailLotId] PRIMARY KEY CLUSTERED ([intLoadDetailLotId] ASC),
+	CONSTRAINT [FK_tblLGLoadDetailLot_tblICLoadDetail] FOREIGN KEY([intLoadDetailId]) REFERENCES [dbo].[tblLGLoadDetail] ([intLoadDetailId]) ON DELETE CASCADE, 
 	CONSTRAINT [FK_tblLGLoadDetailLot_tblICLot] FOREIGN KEY([intLotId]) REFERENCES [dbo].[tblICLot] ([intLotId]),
 	CONSTRAINT [FK_tblLGLoadDetailLot_intItemUOMId] FOREIGN KEY([intItemUOMId]) REFERENCES [dbo].[tblICItemUOM] ([intItemUOMId]),
 	CONSTRAINT [FK_tblLGLoadDetailLot_intWeightUOMId] FOREIGN KEY([intWeightUOMId]) REFERENCES [dbo].[tblICItemUOM] ([intItemUOMId]),
