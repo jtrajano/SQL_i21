473ea3d5a66ceae769a3fe8303ed7b66db3d5e0d
Premkumar S <prem.selladurai@irely.com>
2016-05-02 16:41:52 +0530

    MFG-1684

--- i21Database/dbo/Stored Procedures/uspMFGetStageDetail.sql
+++ i21Database/dbo/Stored Procedures/uspMFGetStageDetail.sql
@@ -1,99 +1,82 @@
 ﻿CREATE PROCEDURE uspMFGetStageDetail (@intWorkOrderId INT)
 AS
 BEGIN
-	Declare @intAttributeId int
-			,@strAttributeValue nvarchar(50)
-			,@intManufacturingProcessId int
-			,@intLocationId int
-			,@intStatusId int
-
-	Select @intManufacturingProcessId=intManufacturingProcessId, @intLocationId=intLocationId,@intStatusId=intStatusId  From dbo.tblMFWorkOrder Where intWorkOrderId =@intWorkOrderId 
-
-	Select @intAttributeId=intAttributeId from tblMFAttribute Where strAttributeName='Display actual consumption in WM'
-
-	Select @strAttributeValue=strAttributeValue
-	From tblMFManufacturingProcessAttribute
-	Where intManufacturingProcessId=@intManufacturingProcessId and intLocationId=@intLocationId and intAttributeId=@intAttributeId
-
-	IF @strAttributeValue='True' OR @intStatusId=13
-	BEGIN
-		SELECT W.intWorkOrderConsumedLotId As intWorkOrderInputLotId,
-			L.intLotId 
-			,L.strLotNumber
-			,I.strItemNo
-			,I.strDescription
-			,W.dblQuantity
-			,IU.intItemUOMId
-			,U.intUnitMeasureId
-			,U.strUnitMeasure
-			,W.dtmCreated
-			,W.intCreatedUserId
-			,US.strUserName
-			,W.intWorkOrderId
-			,SL.intStorageLocationId
-			,SL.strName AS strStorageLocationName
-			,ISNULL(W.intMachineId,0)intMachineId
-			,ISNULL(M.strName,'') AS strMachineName
-			,W.ysnConsumptionReversed
-			,W.strReferenceNo
-			,W.dtmActualInputDateTime
-			,C.intContainerId
-			,C.strContainerId
-			,S.intShiftId
-			,S.strShiftName
-			,L.intParentLotId 
-		FROM dbo.tblMFWorkOrderConsumedLot W
-		JOIN dbo.tblICLot L ON L.intLotId = W.intLotId
-		JOIN dbo.tblICItem I ON I.intItemId = L.intItemId
-		JOIN dbo.tblICItemUOM IU ON IU.intItemUOMId = W.intItemUOMId
-		JOIN dbo.tblICUnitMeasure U ON U.intUnitMeasureId = IU.intUnitMeasureId
-		JOIN dbo.tblSMUserSecurity US ON US.[intEntityUserSecurityId] = W.intCreatedUserId
-		LEFT JOIN dbo.tblICStorageLocation SL ON SL.intStorageLocationId = W.intStorageLocationId
-		LEFT JOIN dbo.tblMFMachine M ON M.intMachineId = W.intMachineId
-		LEFT JOIN dbo.tblICContainer C ON C.intContainerId = W.intContainerId
-		LEFT JOIN dbo.tblMFShift S ON S.intShiftId = W.intShiftId
-		WHERE intWorkOrderId = @intWorkOrderId
-		Order by W.intWorkOrderConsumedLotId
-	END
-	ELSE
-	BEGIN
-		SELECT W.intWorkOrderInputLotId,
-			L.intLotId 
-			,L.strLotNumber
-			,I.strItemNo
-			,I.strDescription
-			,W.dblQuantity
-			,IU.intItemUOMId
-			,U.intUnitMeasureId
-			,U.strUnitMeasure
-			,W.dtmCreated
-			,W.intCreatedUserId
-			,US.strUserName
-			,W.intWorkOrderId
-			,SL.intStorageLocationId
-			,SL.strName AS strStorageLocationName
-			,W.intMachineId
-			,M.strName AS strMachineName
-			,W.ysnConsumptionReversed
-			,W.strReferenceNo
-			,W.dtmActualInputDateTime
-			,C.intContainerId
-			,C.strContainerId
-			,S.intShiftId
-			,S.strShiftName
-			,L.intParentLotId 
-		FROM dbo.tblMFWorkOrderInputLot W
-		JOIN dbo.tblICLot L ON L.intLotId = W.intLotId
-		JOIN dbo.tblICItem I ON I.intItemId = L.intItemId
-		JOIN dbo.tblICItemUOM IU ON IU.intItemUOMId = W.intItemUOMId
-		JOIN dbo.tblICUnitMeasure U ON U.intUnitMeasureId = IU.intUnitMeasureId
-		JOIN dbo.tblSMUserSecurity US ON US.[intEntityUserSecurityId] = W.intCreatedUserId
-		LEFT JOIN dbo.tblICStorageLocation SL ON SL.intStorageLocationId = W.intStorageLocationId
-		LEFT JOIN dbo.tblMFMachine M ON M.intMachineId = W.intMachineId
-		LEFT JOIN dbo.tblICContainer C ON C.intContainerId = W.intContainerId
-		LEFT JOIN dbo.tblMFShift S ON S.intShiftId = W.intShiftId
-		WHERE intWorkOrderId = @intWorkOrderId
-		Order by W.intWorkOrderInputLotId
-	END
+	SELECT W.intWorkOrderConsumedLotId AS intWorkOrderInputLotId
+		,L.intLotId
+		,L.strLotNumber
+		,I.strItemNo
+		,I.strDescription
+		,W.dblQuantity
+		,IU.intItemUOMId
+		,U.intUnitMeasureId
+		,U.strUnitMeasure
+		,W.dtmCreated
+		,W.intCreatedUserId
+		,US.strUserName
+		,W.intWorkOrderId
+		,SL.intStorageLocationId
+		,SL.strName AS strStorageLocationName
+		,ISNULL(W.intMachineId, 0) intMachineId
+		,ISNULL(M.strName, '') AS strMachineName
+		,W.ysnConsumptionReversed
+		,W.strReferenceNo
+		,W.dtmActualInputDateTime
+		,C.intContainerId
+		,C.strContainerId
+		,S.intShiftId
+		,S.strShiftName
+		,L.intParentLotId
+		,'STAGE' AS strTransactionName
+	FROM dbo.tblMFWorkOrderConsumedLot W
+	JOIN dbo.tblICLot L ON L.intLotId = W.intLotId
+	JOIN dbo.tblICItem I ON I.intItemId = L.intItemId
+	JOIN dbo.tblICItemUOM IU ON IU.intItemUOMId = W.intItemUOMId
+	JOIN dbo.tblICUnitMeasure U ON U.intUnitMeasureId = IU.intUnitMeasureId
+	JOIN dbo.tblSMUserSecurity US ON US.[intEntityUserSecurityId] = W.intCreatedUserId
+	LEFT JOIN dbo.tblICStorageLocation SL ON SL.intStorageLocationId = W.intStorageLocationId
+	LEFT JOIN dbo.tblMFMachine M ON M.intMachineId = W.intMachineId
+	LEFT JOIN dbo.tblICContainer C ON C.intContainerId = W.intContainerId
+	LEFT JOIN dbo.tblMFShift S ON S.intShiftId = W.intShiftId
+	WHERE intWorkOrderId = @intWorkOrderId
+	
+	UNION
+	
+	SELECT W.intWorkOrderInputLotId
+		,L.intLotId
+		,L.strLotNumber
+		,I.strItemNo
+		,I.strDescription
+		,W.dblQuantity
+		,IU.intItemUOMId
+		,U.intUnitMeasureId
+		,U.strUnitMeasure
+		,W.dtmCreated
+		,W.intCreatedUserId
+		,US.strUserName
+		,W.intWorkOrderId
+		,SL.intStorageLocationId
+		,SL.strName AS strStorageLocationName
+		,W.intMachineId
+		,M.strName AS strMachineName
+		,W.ysnConsumptionReversed
+		,W.strReferenceNo
+		,W.dtmActualInputDateTime
+		,C.intContainerId
+		,C.strContainerId
+		,S.intShiftId
+		,S.strShiftName
+		,L.intParentLotId
+		,'CONSUME' AS strTransactionName
+	FROM dbo.tblMFWorkOrderInputLot W
+	JOIN dbo.tblICLot L ON L.intLotId = W.intLotId
+	JOIN dbo.tblICItem I ON I.intItemId = L.intItemId
+	JOIN dbo.tblICItemUOM IU ON IU.intItemUOMId = W.intItemUOMId
+	JOIN dbo.tblICUnitMeasure U ON U.intUnitMeasureId = IU.intUnitMeasureId
+	JOIN dbo.tblSMUserSecurity US ON US.[intEntityUserSecurityId] = W.intCreatedUserId
+	LEFT JOIN dbo.tblICStorageLocation SL ON SL.intStorageLocationId = W.intStorageLocationId
+	LEFT JOIN dbo.tblMFMachine M ON M.intMachineId = W.intMachineId
+	LEFT JOIN dbo.tblICContainer C ON C.intContainerId = W.intContainerId
+	LEFT JOIN dbo.tblMFShift S ON S.intShiftId = W.intShiftId
+	WHERE intWorkOrderId = @intWorkOrderId
+	ORDER BY W.intWorkOrderConsumedLotId
 END
-
