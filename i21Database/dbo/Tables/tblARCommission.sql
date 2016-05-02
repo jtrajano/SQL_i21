fd83829a5730a5c28175ef447b4b8d4cb678ed4c
Kelvin Quinto <kelvin.quinto@irely.com>
2016-05-03 05:40:54 +0800

    AR-2677

---
+++ i21Database/dbo/Tables/tblARCommission.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[tblARCommission]
+(
+	[intCommissionId]			INT NOT NULL IDENTITY,
+	[intCommissionScheduleId]	INT NOT NULL,
+	[intCommissionPlanId]		INT NULL,
+	[intEntityId]				INT NULL,
+	[dtmStartDate]				DATETIME NULL,
+	[dtmEndDate]				DATETIME NULL,
+	[ysnConditional]			BIT NULL,
+	[ysnApproved]				BIT NULL DEFAULT ((0)),
+	[ysnRejected]				BIT NULL DEFAULT ((0)),
+	[ysnPayroll]				BIT NULL DEFAULT ((0)),
+	[ysnPayables]				BIT NULL DEFAULT ((0)),
+	[dblTotalAmount]			NUMERIC(18,6) NULL DEFAULT(0),
+	[intConcurrencyId]			INT NOT NULL DEFAULT ((0)),
+    CONSTRAINT [PK_tblARCommission_intCommissionId] PRIMARY KEY CLUSTERED ([intCommissionId] ASC)
+)
