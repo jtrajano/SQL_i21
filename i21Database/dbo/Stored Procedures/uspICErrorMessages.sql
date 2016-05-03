480048abcdbb8522a6ddf1b1283b533558643ba6
Feb Montefrio <feb.montefrio@irely.com>
2016-05-03 09:41:02 +0800

    IC-2115

--- i21Database/dbo/Stored Procedures/uspICErrorMessages.sql
+++ i21Database/dbo/Stored Procedures/uspICErrorMessages.sql
@@ -398,3 +398,7 @@ EXEC sp_addmessage 80079,11,@strmessage,'us_english','False'
 IF EXISTS(SELECT 1 FROM sys.messages WHERE message_id = 80080) EXEC sp_dropmessage 80080, 'us_english'	
 SET @strmessage = 'Item UOM %s for %s is invalid or missing.'
 EXEC sp_addmessage 80080,11,@strmessage,'us_english','False'
+
+IF EXISTS(SELECT 1 FROM sys.messages WHERE message_id = 80081) EXEC sp_dropmessage 80081, 'us_english'	
+SET @strmessage = 'Net quantity mistmatch. It is %s on item %s but the total net from the lot(s) is %s.'
+EXEC sp_addmessage 80081,11,@strmessage,'us_english','False'
