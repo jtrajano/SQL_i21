a43bab193e08269e655a4feae1fa791ac03f832e
Premkumar S <prem.selladurai@irely.com>
2016-05-03 20:56:00 +0530

    MFG-1633

---
+++ i21Database/dbo/Stored Procedures/uspICBillOfLadingReport.sql
@@ -0,0 +1,123 @@
+﻿CREATE PROCEDURE uspICBillOfLadingReport @xmlParam NVARCHAR(MAX) = NULL
+AS
+SET NOCOUNT ON
+SET XACT_ABORT ON
+SET ANSI_WARNINGS OFF
+
+BEGIN TRY
+	IF ISNULL(@xmlParam, '') = ''
+	BEGIN
+		SELECT '' AS 'intInventoryShipmentId'
+		     ,'' AS 'strShipToAddress'
+		 	,'' AS 'strShipmentNumber'
+			,'' AS 'strShipFromAddress'
+			,'' AS 'strBOLNumber'
+			,'' AS 'strOrderNumber'
+			,'' AS 'strPONumber'
+			,'' AS 'dtmShipDate'
+			,'' AS 'strShipVia'
+			,'' AS 'strDeliveryInstruction'
+			,'' AS 'strFreightTerm'
+			,'' AS 'strItemNo'
+			,'' AS 'strItemDescription'
+			,'' AS 'strLotNumber'
+			,'' AS 'strLotAlias'
+			,'' AS 'dblLotQty'
+			,'' AS 'strLotUOM'
+			,'' AS 'intWarehouseInstructionHeaderId'
+			, '' AS 'dblNetWeight'
+			,'' AS 'dblTotalWeight'
+		RETURN
+	END
+
+	DECLARE @strErrMsg NVARCHAR(MAX)
+	DECLARE @strOrderType NVARCHAR(50)
+	DECLARE @xmlDocumentId INT
+	DECLARE @strShipmentNo NVARCHAR(100)
+
+	IF LTRIM(RTRIM(@xmlParam)) = ''
+		SET @xmlParam = NULL
+
+	DECLARE @temp_xml_table TABLE (
+		[fieldname] NVARCHAR(50)
+		,condition NVARCHAR(20)
+		,[from] NVARCHAR(50)
+		,[to] NVARCHAR(50)
+		,[join] NVARCHAR(10)
+		,[begingroup] NVARCHAR(50)
+		,[endgroup] NVARCHAR(50)
+		,[datatype] NVARCHAR(50)
+		)
+
+	EXEC sp_xml_preparedocument @xmlDocumentId OUTPUT
+		,@xmlParam
+
+	INSERT INTO @temp_xml_table
+	SELECT *
+	FROM OPENXML(@xmlDocumentId, 'xmlparam/filters/filter', 2) WITH (
+			[fieldname] NVARCHAR(50)
+			,condition NVARCHAR(20)
+			,[from] NVARCHAR(50)
+			,[to] NVARCHAR(50)
+			,[join] NVARCHAR(10)
+			,[begingroup] NVARCHAR(50)
+			,[endgroup] NVARCHAR(50)
+			,[datatype] NVARCHAR(50)
+			)
+
+	SELECT @strShipmentNo = [from]
+	FROM @temp_xml_table
+	WHERE [fieldname] = 'strShipmentNumber'
+	
+	SELECT @strOrderType = [from]
+	FROM @temp_xml_table
+	WHERE [fieldname] = 'strOrderType'
+
+	IF @strShipmentNo IS NOT NULL
+	BEGIN
+		SELECT *
+		FROM (
+			SELECT Shipment.intInventoryShipmentId
+				,Shipment.strShipmentNumber
+				,strShipFromAddress = Shipment.strShipFromLocation + ' ' + Shipment.strShipFromAddress 
+				,strShipToAddress = Shipment.strShipToLocation + ' ' + Shipment.strShipToAddress
+				,Shipment.strBOLNumber
+				,ShipmentItem.strOrderNumber
+				,strCustomerPO = SO.strPONumber
+				,Shipment.dtmShipDate
+				,ShipVia.strShipVia
+				,Shipment.strDeliveryInstruction
+				,FreightTerm.strFreightTerm
+				,ShipmentItem.strItemNo
+				,ShipmentItem.strItemDescription
+				,Lot.strLotNumber
+				,Lot.strLotAlias
+				,ISNULL(ShipmentItemLot.dblLotQty, ShipmentItem.dblQtyToShip) AS dblQty
+				,ISNULL(ShipmentItemLot.strLotUOM, ShipmentItem.strUnitMeasure) AS strUOM
+				,ShipmentItemLot.dblNetWeight
+				,SUM(ShipmentItemLot.dblNetWeight) OVER() AS dblTotalWeight
+				,intWarehouseInstructionHeaderId = ISNULL(WarehouseInstruction.intWarehouseInstructionHeaderId, 0)
+			FROM vyuICGetInventoryShipment Shipment
+			LEFT JOIN vyuICGetInventoryShipmentItem ShipmentItem ON Shipment.intInventoryShipmentId = ShipmentItem.intInventoryShipmentId
+			LEFT JOIN vyuICGetInventoryShipmentItemLot ShipmentItemLot ON ShipmentItemLot.intInventoryShipmentItemId = ShipmentItem.intInventoryShipmentItemId
+			LEFT JOIN vyuICGetLot Lot ON Lot.intLotId = ShipmentItemLot.intLotId
+			LEFT JOIN tblSMShipVia ShipVia ON ShipVia.intEntityShipViaId = Shipment.intShipViaId
+			LEFT JOIN tblSMFreightTerms FreightTerm ON FreightTerm.intFreightTermId = Shipment.intFreightTermId
+			LEFT JOIN tblLGWarehouseInstructionHeader WarehouseInstruction ON WarehouseInstruction.intInventoryShipmentId = Shipment.intInventoryShipmentId
+		    LEFT JOIN tblSOSalesOrder SO ON SO.intSalesOrderId = ShipmentItem.intOrderId AND ShipmentItem.strOrderType = @strOrderType
+				
+			) AS a
+		WHERE strShipmentNumber = @strShipmentNo 
+	END
+END TRY
+
+BEGIN CATCH
+	SET @strErrMsg = 'uspICBillOfLadingReport - ' + ERROR_MESSAGE()
+
+	RAISERROR (
+			@strErrMsg
+			,18
+			,1
+			,'WITH NOWAIT'
+			)
+END CATCH
