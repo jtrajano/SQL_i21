88af427b705136ea9484b6193684b49d6f6c4e46
Erick Dela Cruz <erick.delacruz@irely.com>
2016-05-04 19:03:43 +0800

    AP-2078, AP-2068

---
+++ Integration/dbo/Stored Procedures/uspAPValidateVoucherImport.sql
@@ -0,0 +1,33 @@
+﻿CREATE PROCEDURE [dbo].[uspAPValidateVoucherImport]
+	@UserId INT,
+	@DateFrom DATETIME = NULL,
+	@DateTo DATETIME = NULL,
+	@totalImported INT OUTPUT
+AS
+
+
+DECLARE @log TABLE
+(
+	[strDescription] NVARCHAR(200) COLLATE Latin1_General_CI_AS NULL, 
+    [intEntityId] INT NOT NULL, 
+    [dtmDate] DATETIME NOT NULL,
+	[intLogType] INT NOT NULL
+)
+
+IF(@UserId <= 0)
+BEGIN
+	INSERT INTO @log
+	SELECT 'You cannot import without user.', @UserId, GETDATE(), 1
+END
+
+--MAKE SURE USER HAS DEFAULT LOCATION
+DECLARE @userLocation INT;
+SELECT @userLocation = A.intCompanyLocationId FROM tblSMCompanyLocation A
+		INNER JOIN tblSMUserSecurity B ON A.intCompanyLocationId = B.intCompanyLocationId
+WHERE intEntityUserSecurityId = @UserId
+
+IF(@userLocation IS NULL OR @userLocation <= 0)
+BEGIN
+	INSERT INTO @log
+	SELECT 'Please setup default location on user screen.', @UserId, GETDATE(), 2
+END
---
+++ i21Database/dbo/Tables/tblAPImportVoucherLog.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[tblAPImportVoucherLog]
+(
+	[intImportLogId] INT NOT NULL IDENTITY, 
+    [strDescription] NVARCHAR(200) COLLATE Latin1_General_CI_AS NULL, 
+    [intEntityId] INT NOT NULL, 
+    [dtmDate] DATETIME NOT NULL, 
+    [intLogType] INT NOT NULL, 
+    CONSTRAINT [PK_tblAPImportVoucherLog] PRIMARY KEY ([intImportLogId])
+)
