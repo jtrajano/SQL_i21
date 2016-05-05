fd3b5ef0234ab7dfe73d4a4d52fee584460b9f82
Jayson Ramos <jayson.ramos@iRely.com>
2016-05-05 13:41:57 +0800

    HD-1348

--- i21Database/dbo/Tables/tblHDCampaignEntity.sql
+++ i21Database/dbo/Tables/tblHDCampaignEntity.sql
@@ -2,12 +2,12 @@
 (
 	[intCampaignEntityId] [int] IDENTITY(1,1) NOT NULL,
 	[intOpportunityCampaignId] [int] NOT NULL,
-	[intEntityCustomerId] [int] NOT NULL,
+	[intEntityId] [int] NOT NULL,
 	[intConcurrencyId] [int] NOT NULL DEFAULT 1,
 	CONSTRAINT [PK_tblCampaignEntity] PRIMARY KEY CLUSTERED ([intCampaignEntityId] ASC),
-	CONSTRAINT [UQ_tblHDCampaignEntity_intOpportunityCampaignId_intEntityCustomerId] UNIQUE ([intOpportunityCampaignId],[intEntityCustomerId]),
+	CONSTRAINT [UQ_tblHDCampaignEntity_intOpportunityCampaignId_intEntityCustomerId] UNIQUE ([intOpportunityCampaignId],[intEntityId]),
     CONSTRAINT [FK_tblHDCampaignEntity_tblHDProject] FOREIGN KEY ([intOpportunityCampaignId]) REFERENCES [dbo].[tblHDOpportunityCampaign] ([intOpportunityCampaignId]),
-    CONSTRAINT [FK_tblHDCampaignEntity_tblCTContractHeader] FOREIGN KEY ([intEntityCustomerId]) REFERENCES [dbo].[tblARCustomer] ([intEntityCustomerId])
+    CONSTRAINT [FK_tblHDCampaignEntity_tblCTContractHeader] FOREIGN KEY ([intEntityId]) REFERENCES [dbo].[tblEMEntity] ([intEntityId])
 )
 
 GO
@@ -28,15 +28,6 @@ EXEC sp_addextendedproperty @name = N'MS_Description',
     @level1name = N'tblHDCampaignEntity',
     @level2type = N'COLUMN',
     @level2name = N'intOpportunityCampaignId'
-GO
-EXEC sp_addextendedproperty @name = N'MS_Description',
-    @value = N'Reference Id for AR Customer',
-    @level0type = N'SCHEMA',
-    @level0name = N'dbo',
-    @level1type = N'TABLE',
-    @level1name = N'tblHDCampaignEntity',
-    @level2type = N'COLUMN',
-    @level2name = N'intEntityCustomerId'
 GO
 EXEC sp_addextendedproperty @name = N'MS_Description',
     @value = N'Concurrency Check',
