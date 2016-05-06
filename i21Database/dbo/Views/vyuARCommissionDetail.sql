6d483c4842fea620ed047bee73db9bb86420605b
Kelvin Quinto <kelvin.quinto@irely.com>
2016-05-07 01:51:51 +0800

    AR-2677

---
+++ i21Database/dbo/Views/vyuARCommissionDetail.sql
@@ -0,0 +1,10 @@
+﻿CREATE VIEW [dbo].[vyuARCommissionDetail]
+AS
+SELECT strComDetailEntityName	= E.strName
+	 , intComDetailEntityId		= CD.intEntityId
+	 , dblLineItemAmount		= CD.dblAmount
+	 , C.*
+FROM vyuARCommission C
+	INNER JOIN tblARCommissionDetail CD ON C.intCommissionId = CD.intCommissionId
+	LEFT JOIN tblEMEntity E ON CD.intEntityId = E.intEntityId
+WHERE C.ysnConditional = 0 OR (C.ysnConditional = 1 AND C.ysnApproved = 1)
