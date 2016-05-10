3dcad676f9f6e9a2665053d9ba78c5bcb8a86bbd
Emerson Versoza <emerson.versoza@irely.com>
2016-05-10 21:00:15 +0800

    SC-95

--- i21Database/dbo/tblSCUncompletedTicketAlert.sql
+++ i21Database/dbo/tblSCUncompletedTicketAlert.sql
@@ -5,6 +5,7 @@
     [intCompanyLocationId] INT NULL, 
     [intTicketUncompletedDaysAlert] INT NULL, 
     [intCompanyPreferenceId] INT NOT NULL, 
+    [intConcurrencyId] INT NOT NULL 
     CONSTRAINT [PK_tblSCUncompletedTicketAlert] PRIMARY KEY ([intUncompletedTicketAlertId]),
     CONSTRAINT [FK_tblSCUncompletedTicketAlert_tblGRCompanyPreference_intCompanyPreferenceId] FOREIGN KEY ([intCompanyPreferenceId]) REFERENCES [tblGRCompanyPreference]([intCompanyPreferenceId]) 
 )
