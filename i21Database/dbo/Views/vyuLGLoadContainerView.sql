fed48785b19bdf2599fd063d613f84a01a315578
Swetosuvro Ghosh <swetosuvro.g@irely.com>
2016-05-11 19:21:55 +0530

    LG-370

---
+++ i21Database/dbo/Views/vyuLGLoadContainerView.sql
@@ -0,0 +1,57 @@
+﻿CREATE VIEW [dbo].[vyuLGLoadContainerView]
+AS
+SELECT  L.intLoadId
+		,LDV.intLoadDetailId
+		,LC.intLoadContainerId
+		,L.strLoadNumber
+		,L.strBLNumber
+		,L.dtmBLDate
+		,L.dtmScheduledDate
+		,LDV.strCustomerFax
+		,LDV.strCustomerMobile
+		,LDV.strCustomerNo
+		,LDV.strCustomerPhone
+		,LDV.strCustomerReference
+		,LDV.strDispatcher
+		,LDV.strDriver
+		,LDV.strEquipmentType
+		,LDV.strExternalLoadNumber
+		,LDV.strHauler
+		,LDV.strItemDescription
+		,LDV.strItemNo
+		,LDV.strItemUOM
+		,LDV.strLoadDirectionMsg
+		,LDV.strLotTracking
+		,LC.strComments
+		,LC.strContainerNumber
+		,LC.strCustomsComments
+		,LC.strFDAComments
+		,LC.strFreightComments
+		,LC.strLotNumber
+		,LC.strMarks
+		,LC.strOtherMarks
+		,LC.strSealNumber
+		,LC.strUSDAComments
+		,LC.dblGrossWt
+		,LC.dblNetWt
+		,LC.dblQuantity
+		,LC.dblTareWt
+		,LC.dblTotalCost
+		,LC.dblUnitCost
+		,LC.dtmCustoms
+		,LC.dtmFDA
+		,LC.dtmFreight
+		,LC.dtmUSDA
+		,LC.ysnCustomsHold
+		,LC.ysnDutyPaid
+		,LC.ysnFDAHold
+		,LC.ysnRejected
+		,LC.ysnUSDAHold
+		,PCDV.strContractNumber AS strPContractNumber
+		,SCDV.strContractNumber AS strSContractNumber
+FROM vyuLGLoadView L
+JOIN vyuLGLoadDetailView LDV ON L.intLoadId = LDV.intLoadId
+JOIN tblLGLoadDetailContainerLink LDCL ON LDCL.intLoadDetailId = LDV.intLoadDetailId
+JOIN tblLGLoadContainer LC ON LDCL.intLoadContainerId = LC.intLoadContainerId
+LEFT JOIN vyuCTContractDetailView PCDV ON PCDV.intContractDetailId = LDV.intPContractDetailId
+LEFT JOIN vyuCTContractDetailView SCDV ON SCDV.intContractDetailId = LDV.intSContractDetailId
