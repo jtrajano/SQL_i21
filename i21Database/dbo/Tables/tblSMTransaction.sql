9dea910cc46df3b07e35311a1cc3ba505bb8a258
Von Carlo Pasion <von.pasion@irely.com>
2016-05-12 19:01:06 +0800

    FCOM-1689

--- i21Database/dbo/Tables/tblSMTransaction.sql
+++ i21Database/dbo/Tables/tblSMTransaction.sql
@@ -2,7 +2,8 @@
 (
 	[intTransactionId] INT NOT NULL PRIMARY KEY IDENTITY,
 	[intScreenId] [int] NOT NULL DEFAULT ((1)),
-	[strRecordNo] [nvarchar](50) NOT NULL,
+	[strRecordNo] [nvarchar](50) COLLATE Latin1_General_CI_AS NOT NULL,
+	[strApprovalStatus] [nvarchar](150) COLLATE Latin1_General_CI_AS NULL,
 	[intConcurrencyId] [int] NOT NULL DEFAULT ((1)), 
     CONSTRAINT [FK_tblSMTransaction_tblSMScreen] FOREIGN KEY ([intScreenId]) REFERENCES [tblSMScreen]([intScreenId])
 )
