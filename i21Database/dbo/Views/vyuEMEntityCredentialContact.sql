d526738b94a6667463c04c579ce95cb33942817e
Ian Ocampo <ian.ocampo@irely.com>
2016-05-12 19:54:57 +0800

    FRM-3226

---
+++ i21Database/dbo/Views/vyuEMEntityCredentialContact.sql
@@ -0,0 +1,40 @@
+﻿CREATE VIEW [dbo].[vyuEMEntityCredentialContact]
+	AS 
+
+SELECT
+	EntityCredential.intEntityId,
+	vEntityContact.intEntityContactId,
+	vEntityContact.strEntityName,
+	vEntityContact.strName,
+	vEntityContact.strEmail,
+	vEntityContact.strLocationName,
+	vEntityContact.strPhone,
+	vEntityContact.strMobile,
+	vEntityContact.strTimezone,
+	vEntityContact.strTitle,
+	vEntityContact.ysnPortalAccess,
+	vEntityContact.ysnActive,
+	vEntityContact.ysnDefaultContact,
+	vEntityContact.Customer,
+	vEntityContact.Vendor,
+	vEntityContact.Employee,
+	vEntityContact.Salesperson,
+	vEntityContact.[User],
+	vEntityContact.FuturesBroker,
+	vEntityContact.ForwardingAgent,
+	vEntityContact.Terminal,
+	vEntityContact.ShippingLine,
+	vEntityContact.Trucker,
+	vEntityContact.strContactType,
+	vEntityContact.strEmailDistributionOption,
+	vEntityContact.imgPhoto,
+	vEntityContact.papit
+FROM tblEMEntityCredential AS EntityCredential
+INNER JOIN vyuEMEntityContact AS vEntityContact
+ON EntityCredential.intEntityId = (
+	CASE WHEN vEntityContact.ysnPortalAccess = 1
+		THEN vEntityContact.intEntityContactId
+		ELSE vEntityContact.intEntityId
+	END
+)
+
