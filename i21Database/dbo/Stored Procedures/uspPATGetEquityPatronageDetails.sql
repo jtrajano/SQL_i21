4e6adbd98de8c7a3ea0a3de2932414c5a28e9e6e
jc_flores <jc.flores@irely.com>
2016-05-13 20:20:25 +0800

    PAT-342

--- i21Database/dbo/Stored Procedures/uspPATGetEquityPatronageDetails.sql
+++ i21Database/dbo/Stored Procedures/uspPATGetEquityPatronageDetails.sql
@@ -2,19 +2,19 @@
 	@intCustomerId INT = NULL
 AS
 BEGIN
-	SELECT RR.intRefundTypeId, 
-		   PC.strCategoryCode,
-		   PC.strPurchaseSale,
-		   PC.strUnitAmount,
-		   CE.dblEquity,
-		   RR.intConcurrencyId
-	  FROM tblPATRefundRate RR
-INNER JOIN tblPATRefundRateDetail RRD
-		ON RRD.intRefundTypeId = RR.intRefundTypeId
-INNER JOIN tblPATPatronageCategory PC
-		ON PC.intPatronageCategoryId = RRD.intPatronageCategoryId
-INNER JOIN tblPATCustomerEquity CE
-		ON CE.intRefundTypeId = RR.intRefundTypeId
-	 WHERE CE.intCustomerId = @intCustomerId
+	SELECT	RRD.intRefundTypeId,
+			PC.strCategoryCode,
+			PC.strPurchaseSale,
+			PC.strUnitAmount,
+			CE.dblEquity,
+			RRD.intConcurrencyId
+	FROM tblPATCustomerVolume CV
+	INNER JOIN tblPATPatronageCategory PC
+		ON CV.intPatronageCategoryId = PC.intPatronageCategoryId
+	INNER JOIN tblPATRefundRateDetail RRD
+		ON RRD.intPatronageCategoryId = PC.intPatronageCategoryId
+	INNER JOIN tblPATCustomerEquity CE
+		ON CE.intRefundTypeId = RRD.intRefundTypeId AND CE.intCustomerId = CV.intCustomerPatronId
+	WHERE CV.intCustomerPatronId = @intCustomerId
 END
 GO
