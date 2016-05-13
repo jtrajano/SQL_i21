4776c2783d87db29f86147f2015fac22442692e2
Mon Gonzales <mon.gonzales@irely.com>
2016-05-13 14:40:59 +0800

    EM-1133

---
+++ i21Database/dbo/Tables/tblEMEntityLineOfBusiness.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[tblEMEntityLineOfBusiness]
+(
+	[intEntityLineOfBusinessId]		INT IDENTITY(1,1) NOT NULL,
+	[intEntityId]					INT NOT NULL,
+    [intLineOfBusinessId]			INT NOT NULL,	
+	[intConcurrencyId]				INT DEFAULT ((0)) NOT NULL,
+	CONSTRAINT [FK_tblEMEntityLineOfBusiness_tblEMEntity] FOREIGN KEY ([intEntityId]) REFERENCES [dbo].[tblEMEntity] ([intEntityId]) ON DELETE CASCADE,
+	CONSTRAINT [FK_tblEMEntityLineOfBusiness_tblHDLineOfBusiness] FOREIGN KEY ([intLineOfBusinessId]) REFERENCES [dbo].[tblHDLineOfBusiness] ([intLineOfBusinessId]),
+)
