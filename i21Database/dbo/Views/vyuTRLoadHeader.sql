4edea37095d8fe5815ea91aa2483f7303d26635e
Lawrence Zabala <lawrence.zabala@irely.com>
2016-05-17 14:44:08 +0800

    TR-456

--- i21Database/dbo/Views/vyuTRLoadHeader.sql
+++ i21Database/dbo/Views/vyuTRLoadHeader.sql
@@ -6,57 +6,69 @@ SELECT TL.intLoadHeaderId
 	, strReceiptLink = TR.strReceiptLine
 	, strRecordType = (CASE WHEN TR.intLoadReceiptId IS NULL THEN 'Header'
 							ELSE 'Receipt' END)
-	, NULL AS strCustomer
-	, NULL AS strCustomerLocation
-	, NULL AS strCustomerCompanyLocation
+	, strCustomer = NULL
+	, strCustomerLocation = NULL
+	, strCustomerCompanyLocation = NULL
 	, SP.strFuelSupplier
 	, SP.strSupplyPoint
-	, TR.strBillOfLading AS strBOL
-	, (select top 1 SM.strLocationName from dbo.tblSMCompanyLocation SM where SM.intCompanyLocationId = TR.intCompanyLocationId) AS strReceiptCompanyLocation
-	, (select top 1 IC.strItemNo from dbo.vyuICGetItemStock IC where IC.intItemId = TR.intItemId) AS strItem
+	, strBOL = TR.strBillOfLading
+	, strReceiptCompanyLocation = Location.strLocationName
+	, strItem = Item.strItemNo
 	, dblQuantity = CASE WHEN SP.strGrossOrNet = 'Gross' THEN TR.dblGross
 						WHEN SP.strGrossOrNet = 'Net' THEN TR.dblNet END
-	, NULL AS dblPrice
-	, TR.dblUnitCost AS dblCost
-	, NULL AS dblMargin
-	, dblTotalFreight = (dblFreightRate * dblPurSurcharge * dblNet)
-	, (select  top 1 AR.strName from dbo.vyuEMEntity AR where AR.intEntityId = TL.intDriverId) AS strDriver
-	, TL.dtmLoadDateTime AS dtmDateTime
+	, dblPrice = NULL
+	, dblCost = TR.dblUnitCost
+	, dblMargin = NULL
+	, dblTotalFreight = CASE WHEN SP.strGrossOrNet = 'Gross' THEN (dblGross * dblFreightRate * (1 + dblPurSurcharge / 100))
+						WHEN SP.strGrossOrNet = 'Net' THEN (dblNet * dblFreightRate * (1 + dblPurSurcharge / 100)) END
+	, strDriver = Driver.strName
+	, dtmDateTime = TL.dtmLoadDateTime
 	, TL.ysnPosted
-	, (select top 1 IR.strReceiptNumber from dbo.tblICInventoryReceipt IR where IR.intInventoryReceiptId = TR.intInventoryReceiptId) AS strInventoryReceiptNo
-	, (select top 1 IT.strTransferNo from dbo.tblICInventoryTransfer IT where IT.intInventoryTransferId = TR.intInventoryTransferId) AS strInventoryTransferNo
-	, NULL AS strInvoiceNo
-FROM dbo.tblTRLoadHeader TL
-LEFT JOIN dbo.tblTRLoadReceipt TR ON TL.intLoadHeaderId = TR.intLoadHeaderId
-LEFT JOIN dbo.vyuTRSupplyPointView SP ON SP.intSupplyPointId = TR.intSupplyPointId
+	, strInventoryReceiptNo = Receipt.strReceiptNumber
+	, strInventoryTransferNo = Transfer.strTransferNo
+	, strInvoiceNo = NULL
+FROM tblTRLoadHeader TL
+LEFT JOIN tblTRLoadReceipt TR ON TL.intLoadHeaderId = TR.intLoadHeaderId
+LEFT JOIN vyuTRSupplyPointView SP ON SP.intSupplyPointId = TR.intSupplyPointId
+LEFT JOIN tblSMCompanyLocation Location ON Location.intCompanyLocationId = TR.intCompanyLocationId
+LEFT JOIN tblICItem Item ON Item.intItemId = TR.intItemId
+LEFT JOIN vyuEMSalesperson Driver ON Driver.strType = 'Driver' AND Driver.intEntitySalespersonId = TL.intDriverId
+LEFT JOIN tblICInventoryReceipt Receipt ON Receipt.intInventoryReceiptId = TR.intInventoryReceiptId
+LEFT JOIN tblICInventoryTransfer Transfer ON Transfer.intInventoryTransferId = TR.intInventoryTransferId
 
 UNION ALL
 SELECT TL.intLoadHeaderId
 	, TL.strTransaction
 	, DD.strReceiptLink
-	, 'Distribution' AS strRecordType
-	, (select  top 1 CS.strName from dbo.vyuEMEntity CS where CS.intEntityId = DH.intEntityCustomerId) AS strCustomer
-	, (select  top 1 EL.strLocationName from dbo.[tblEMEntityLocation] EL where EL.intEntityLocationId = DH.intShipToLocationId) AS strCustomerLocation
-	, (select top 1 SM.strLocationName from dbo.tblSMCompanyLocation SM where SM.intCompanyLocationId = DH.intCompanyLocationId) AS strCustomerCompanyLocation
-	, ee.strFuelSupplier
-	, ee.strSupplyPoint
-	, ee.strBillOfLading AS strBOL
-	, ee.strReceiptCompanyLocation
-	, (select top 1 IC.strItemNo from dbo.vyuICGetItemStock IC where IC.intItemId = DD.intItemId) AS strItem
-	, DD.dblUnits AS dblQuantity
-	, DD.dblPrice AS dblPrice
-	, ee.dblUnitCost AS dblCost
-	, DD.dblPrice - ee.dblUnitCost AS dblMargin
-	, dblTotalFreight = (dblFreightRate * dblDistSurcharge * dblUnits)
-	, (select  top 1 AR.strName from dbo.vyuEMEntity AR where AR.intEntityId = TL.intDriverId) AS strDriver
-	, DH.dtmInvoiceDateTime AS dtmDateTime
+	, strRecordType = 'Distribution'
+	, strCustomer = CS.strName
+	, strCustomerLocation = EL.strLocationName
+	, strCustomerCompanyLocation = SM.strLocationName
+	, Receipts.strFuelSupplier
+	, Receipts.strSupplyPoint
+	, strBOL = Receipts.strBillOfLading
+	, Receipts.strReceiptCompanyLocation
+	, strItem = Item.strItemNo
+	, dblQuantity = DD.dblUnits
+	, dblPrice = DD.dblPrice
+	, dblCost = Receipts.dblUnitCost
+	, dblMargin = DD.dblPrice - Receipts.dblUnitCost
+	, dblTotalFreight = (dblUnits * dblFreightRate * (1 + dblDistSurcharge / 100))
+	, strDriver = Driver.strName
+	, dtmDateTime = DH.dtmInvoiceDateTime
 	, TL.ysnPosted
-	, ee.strReceiptNumber AS strInventoryReceiptNo
-	, ee.strTransferNo AS strInventoryTransferNo
-	, (select strInvoiceNumber from tblARInvoice ARI where ARI.intInvoiceId = DH.intInvoiceId) AS strInvoiceNo
-FROM dbo.tblTRLoadHeader TL
-JOIN dbo.tblTRLoadDistributionHeader DH ON DH.intLoadHeaderId = TL.intLoadHeaderId
-LEFT JOIN dbo.tblTRLoadDistributionDetail DD ON DD.intLoadDistributionHeaderId = DH.intLoadDistributionHeaderId
+	, strInventoryReceiptNo = Receipts.strReceiptNumber
+	, strInventoryTransferNo = Receipts.strTransferNo
+	, strInvoiceNo = Invoice.strInvoiceNumber
+FROM tblTRLoadHeader TL
+JOIN tblTRLoadDistributionHeader DH ON DH.intLoadHeaderId = TL.intLoadHeaderId
+LEFT JOIN tblTRLoadDistributionDetail DD ON DD.intLoadDistributionHeaderId = DH.intLoadDistributionHeaderId
+LEFT JOIN vyuEMSalesperson Driver ON Driver.strType = 'Driver' AND Driver.intEntitySalespersonId = TL.intDriverId
+LEFT JOIN tblICItem Item ON Item.intItemId = DD.intItemId
+LEFT JOIN tblARInvoice Invoice ON Invoice.intInvoiceId = DH.intInvoiceId
+LEFT JOIN vyuEMEntity CS ON CS.intEntityId = DH.intEntityCustomerId
+LEFT JOIN tblEMEntityLocation EL ON EL.intEntityLocationId = DH.intShipToLocationId
+LEFT JOIN tblSMCompanyLocation SM ON SM.intCompanyLocationId = DH.intCompanyLocationId
 LEFT JOIN(
 	SELECT DISTINCT intLoadDistributionDetailId
 		, STUFF(
@@ -104,4 +116,4 @@ LEFT JOIN(
 		, (SELECT AVG(CD.dblUnitCost)
 			FROM vyuTRLinkedReceipts CD
 			WHERE CD.intLoadHeaderId = CH.intLoadHeaderId AND CD.intLoadDistributionDetailId = CH.intLoadDistributionDetailId) dblUnitCost
-	FROM vyuTRLinkedReceipts CH)ee ON ee.intLoadDistributionDetailId = DD.intLoadDistributionDetailId
+	FROM vyuTRLinkedReceipts CH) Receipts ON Receipts.intLoadDistributionDetailId = DD.intLoadDistributionDetailId
