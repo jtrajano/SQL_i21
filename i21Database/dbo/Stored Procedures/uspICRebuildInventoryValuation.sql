aa56a9b01903f538717af50b16621f4968ac6efa
Feb Montefrio <feb.montefrio@irely.com>
2016-05-17 14:16:40 +0800

    IC-2050

--- i21Database/dbo/Stored Procedures/uspICRebuildInventoryValuation.sql
+++ i21Database/dbo/Stored Procedures/uspICRebuildInventoryValuation.sql
@@ -490,13 +490,16 @@ BEGIN
 						,ICTrans.dtmDate  
 						,ICTrans.dblQty  
 						,ISNULL(ItemUOM.dblUnitQty, ICTrans.dblUOMQty)
-						,dblCost = 							
-							ISNULL (
+						,dblCost = ISNULL (
 								dbo.fnDivide(
-									(	SELECT SUM (
-													-1 
-													-- Round the values of each of the items. 
-													* ROUND(CAST(dbo.fnMultiply(dblQty, dblCost) + dblValue AS NUMERIC(18, 6)) ,2)
+									(	SELECT SUM( 
+													dbo.fnMultiply(
+														-1
+														,ROUND(
+															dbo.fnMultiply(dblQty, dblCost) + dblValue
+															, 2
+														)
+													)
 												)												
 										FROM	dbo.tblICInventoryTransaction 
 										WHERE	strTransactionId = @strTransactionId 
