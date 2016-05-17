e2dafd4b636b4f897ddacfa33aa3d2712d2420b8
Dan Michael Jalimao <dan.jalimao@irely.com>
2016-05-17 17:13:18 +0800

    FCOM-1571

--- i21Database/Scripts/Post-Deployment/SM/DefaultData/16_PaymentMethod.sql
+++ i21Database/Scripts/Post-Deployment/SM/DefaultData/16_PaymentMethod.sql
@@ -1,7 +1,43 @@
 ﻿GO
+	IF NOT EXISTS(SELECT TOP 1 1 FROM tblSMPaymentMethod WHERE strPaymentMethod = 'Write Off')
+	BEGIN
+		INSERT INTO tblSMPaymentMethod([strPaymentMethod], [strPaymentMethodCode], [intAccountId], [strPrintOption], [ysnActive], [intSort])
+		VALUES('Write Off', NULL, 0, NULL, 1, 0)
+	END
+
 	IF NOT EXISTS(SELECT TOP 1 1 FROM tblSMPaymentMethod WHERE strPaymentMethod = 'ACH')
 	BEGIN
 		INSERT INTO tblSMPaymentMethod([strPaymentMethod], [strPaymentMethodCode], [intAccountId], [strPrintOption], [ysnActive], [intSort])
 		VALUES('ACH', NULL, 0, NULL, 1, 0)
+	END
+
+	IF NOT EXISTS(SELECT TOP 1 1 FROM tblSMPaymentMethod WHERE strPaymentMethod = 'Debit memos and Payments')
+	BEGIN
+		INSERT INTO tblSMPaymentMethod([strPaymentMethod], [strPaymentMethodCode], [intAccountId], [strPrintOption], [ysnActive], [intSort])
+		VALUES('Debit memos and Payments', NULL, 0, NULL, 1, 0)
+	END
+
+	IF NOT EXISTS(SELECT TOP 1 1 FROM tblSMPaymentMethod WHERE strPaymentMethod = 'Credit')
+	BEGIN
+		INSERT INTO tblSMPaymentMethod([strPaymentMethod], [strPaymentMethodCode], [intAccountId], [strPrintOption], [ysnActive], [intSort])
+		VALUES('Credit', NULL, 0, NULL, 1, 0)
+	END
+
+	IF NOT EXISTS(SELECT TOP 1 1 FROM tblSMPaymentMethod WHERE strPaymentMethod = 'Refund')
+	BEGIN
+		INSERT INTO tblSMPaymentMethod([strPaymentMethod], [strPaymentMethodCode], [intAccountId], [strPrintOption], [ysnActive], [intSort])
+		VALUES('Refund', NULL, 0, NULL, 1, 0)
+	END
+
+	IF NOT EXISTS(SELECT TOP 1 1 FROM tblSMPaymentMethod WHERE strPaymentMethod = 'eCheck')
+	BEGIN
+		INSERT INTO tblSMPaymentMethod([strPaymentMethod], [strPaymentMethodCode], [intAccountId], [strPrintOption], [ysnActive], [intSort])
+		VALUES('eCheck', NULL, 0, NULL, 1, 0)
+	END
+
+	IF NOT EXISTS(SELECT TOP 1 1 FROM tblSMPaymentMethod WHERE strPaymentMethod = 'Check')
+	BEGIN
+		INSERT INTO tblSMPaymentMethod([strPaymentMethod], [strPaymentMethodCode], [intAccountId], [strPrintOption], [ysnActive], [intSort])
+		VALUES('Check', NULL, 0, NULL, 1, 0)
 	END
 GO
