c685711f2bdc31ad2ac426f0b2f2777639335117
Swetosuvro Ghosh <swetosuvro.g@irely.com>
2016-05-18 12:59:29 +0530

    LG-408

--- i21Database/dbo/Stored Procedures/uspLGGetInboundShipmentContainerReport.sql
+++ i21Database/dbo/Stored Procedures/uspLGGetInboundShipmentContainerReport.sql
@@ -40,7 +40,7 @@ BEGIN
 	--FROM	@temp_xml_table   
 	--WHERE	[fieldname] = 'intReferenceNumber' 
 
-SELECT  LC.strContainerNumber,
+SELECT DISTINCT LC.strContainerNumber,
 		LV.strBLNumber,
 		LV.strMarks,
 		LDV.*,
@@ -52,9 +52,32 @@ SELECT  LC.strContainerNumber,
 		CD.strApprovalBasis
 FROM vyuLGLoadDetailView LDV
 JOIN vyuLGLoadView LV ON LV.intLoadId = LDV.intLoadId
-LEFT JOIN tblLGLoadContainer LC ON LC.intLoadId = LDV.intLoadId
+JOIN tblLGLoadDetailContainerLink LDCL ON LDCL.intLoadDetailId = LDV.intLoadDetailId
+JOIN tblLGLoadContainer LC ON LC.intLoadContainerId = LDCL.intLoadContainerId
 LEFT JOIN tblLGLoadWarehouseContainer LWC ON LWC.intLoadContainerId = LC.intLoadContainerId
 LEFT JOIN tblLGLoadWarehouse LW ON LW.intLoadWarehouseId = LWC.intLoadWarehouseId
 LEFT JOIN vyuCTContractDetailView CD ON CD.intContractDetailId = LDV.intPContractDetailId
 WHERE LDV.strLoadNumber = @xmlParam	
+
+UNION
+
+SELECT DISTINCT LC.strContainerNumber,
+		LV.strBLNumber,
+		LV.strMarks,
+		LDV.*,
+		LC.dblQuantity AS dblContainerContractQty,
+		CD.strCustomerContract,
+		CD.dtmContractDate,
+		CD.strContractBasis,
+		CD.strContractBasisDescription,
+		CD.strApprovalBasis
+FROM vyuLGLoadDetailView LDV
+JOIN vyuLGLoadView LV ON LV.intLoadId = LDV.intLoadId
+JOIN tblLGLoadDetailContainerLink LDCL ON LDCL.intLoadDetailId = LDV.intLoadDetailId
+JOIN tblLGLoadContainer LC ON LC.intLoadContainerId = LDCL.intLoadContainerId
+LEFT JOIN tblLGLoadWarehouseContainer LWC ON LWC.intLoadContainerId = LC.intLoadContainerId
+LEFT JOIN tblLGLoadWarehouse LW ON LW.intLoadWarehouseId = LWC.intLoadWarehouseId
+LEFT JOIN vyuCTContractDetailView CD ON CD.intContractDetailId = LDV.intPContractDetailId
+WHERE LW.strDeliveryNoticeNumber = @xmlParam	
+
 END
