85398232789f036f11c18c1e2d11442a840bc1a4
Swetosuvro Ghosh <swetosuvro.g@irely.com>
2016-05-18 16:06:48 +0530

    MFG-1575

--- i21Database/dbo/Stored Procedures/uspMFLotItemChange.sql
+++ i21Database/dbo/Stored Procedures/uspMFLotItemChange.sql
@@ -28,6 +28,7 @@ BEGIN TRY
 			,@strItemNo nvarchar(50)
 
 	DECLARE @dblAdjustByQuantity NUMERIC(16,8)
+	DECLARE @dblLotReservedQty NUMERIC(16,8)
 		
 	SELECT @intItemId = intItemId, 
 		   @intLocationId = intLocationId,
@@ -39,6 +40,12 @@ BEGIN TRY
 		   @intAdjustItemUOMId= intItemUOMId  
 	FROM tblICLot WHERE intLotId = @intLotId
 
+	SELECT @dblLotReservedQty = dblQty  FROM tblICStockReservation WHERE intLotId = @intLotId AND ISNULL(ysnPosted,0)=0
+	IF (ISNULL(@dblLotReservedQty,0) > 0)
+	BEGIN
+		RAISERROR('There is reservation against this lot. Cannot proceed.',16,1)
+	END
+	
 	SELECT @intUnitMeasureId=intUnitMeasureId FROM tblICItemUOM WHERE intItemUOMId=@intItemUOMId
 
 	IF NOT EXISTS(SELECT *FROM dbo.tblICItemUOM WHERE intItemId=@intNewItemId AND intUnitMeasureId=@intUnitMeasureId)
