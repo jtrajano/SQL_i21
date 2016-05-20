9b7502d222afff74103eac16cd780f7509acdc8b
Emerson Versoza <emerson.versoza@irely.com>
2016-05-20 17:57:24 +0800

    Revert "GRN-412"
    This reverts commit 1c96babe63569547bb5a75ff6a3db32595acff40.

--- i21Database/dbo/Stored Procedures/uspSCStorageUpdate.sql
+++ i21Database/dbo/Stored Procedures/uspSCStorageUpdate.sql
@@ -587,7 +587,7 @@ BEGIN TRY
 			,SC.[intItemId]
 			,SC.[intSubLocationId]
 			,SC.[intStorageLocationId]
-			,(SELECT intUnitMeasureId FROM tblSCScaleSetup WHERE intScaleSetupId = SC.intScaleSetupId)
+			,(SELECT intUnitMeasureId FROM tblICItemUOM WHERE intItemUOMId = @intTicketItemUOMId)
 	FROM	dbo.tblSCTicket SC
 	WHERE	SC.intTicketId = @intTicketId
 
