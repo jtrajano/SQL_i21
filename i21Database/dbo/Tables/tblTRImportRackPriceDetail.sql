1c7d9ef6b9560bd031877aff1f46ff7aea266de4
Lawrence Zabala <lawrence.zabala@irely.com>
2016-05-20 14:06:16 +0800

    TR-79

---
+++ i21Database/dbo/Tables/tblTRImportRackPriceDetail.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[tblTRImportRackPriceDetail]
+(
+	[intImportRackPriceDetailId] INT NOT NULL IDENTITY, 
+    [intImportRackPriceId] INT NOT NULL, 
+    [intItemId] INT NOT NULL, 
+    [dblVendorPrice] INT NOT NULL, 
+    [dtmEffectiveDate] DATETIME NOT NULL, 
+    [ysnSuccess] BIT NOT NULL, 
+    [intConcurrencyId] INT NULL DEFAULT ((0)), 
+    CONSTRAINT [PK_tblTRImportRackPriceDetail] PRIMARY KEY ([intImportRackPriceDetailId]), 
+    CONSTRAINT [FK_tblTRImportRackPriceDetail_tblTRImportRackPrice] FOREIGN KEY ([intImportRackPriceId]) REFERENCES [tblTRImportRackPrice]([intImportRackPriceId]) ON DELETE CASCADE
+)
