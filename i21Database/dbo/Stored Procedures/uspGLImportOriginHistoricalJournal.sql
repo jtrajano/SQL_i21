22bc4c17dbcfc14a975462a116e1abed35d9f364
jeffrey trajano <jeffrey.trajano@irely.com>
2016-05-23 08:42:03 +0800

    GL-2700 uspGLImportOriginHistoricalJournalCLOSED - string /binary would be truncated

--- i21Database/dbo/Stored Procedures/uspGLImportOriginHistoricalJournal.sql
+++ i21Database/dbo/Stored Procedures/uspGLImportOriginHistoricalJournal.sql
@@ -176,7 +176,8 @@ SELECT @result = REPLACE(@result , 'SUCCESS ','')
 	#iRelyImptblGLJournalDetail A
 	JOIN glhstmst B ON A.A4GLIdentity = B.A4GLIdentity
 	WHERE A.Credit > 0
-
+	
+	ALTER TABLE #iRelyImptblGLJournalDetail ALTER COLUMN strDescription CHAR(50) NULL
 	--GL-2040 For Journal entries - if there is a negative credit break into two entries on import
 	IF EXISTS(SELECT TOP 1 1 FROM #iRelyImptblGLJournalDetail WHERE NegativeCreditUnits = 1)
 	BEGIN
