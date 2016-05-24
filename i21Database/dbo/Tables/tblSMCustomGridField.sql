ccb691a0b6da204b52146a66bff138aeae420e3b
Von Carlo Pasion <von.pasion@irely.com>
2016-05-24 18:30:04 +0800

    FRM-3257

--- i21Database/dbo/Tables/tblSMCustomGridField.sql
+++ i21Database/dbo/Tables/tblSMCustomGridField.sql
@@ -4,6 +4,7 @@
 	[intCustomGridTabId]		INT             NOT NULL,
     [strFieldName]				NVARCHAR (MAX)  COLLATE Latin1_General_CI_AS NULL,
 	[strDataType]				NVARCHAR (MAX)  COLLATE Latin1_General_CI_AS NULL,
+	[ysnBuild]					BIT             NOT NULL DEFAULT 0,	
     [intSort]					INT             NOT NULL DEFAULT (1),
     [intConcurrencyId]			INT				NOT NULL DEFAULT (1),
 	CONSTRAINT [FK_tblSMCustomGridField_tblSMCustomGridTab] FOREIGN KEY ([intCustomGridTabId]) REFERENCES [tblSMCustomGridTab]([intCustomGridTabId])
