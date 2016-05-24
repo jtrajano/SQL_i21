46b62912dca140034442cd4d3776841507d9718c
Kelvin Quinto <kelvin.quinto@irely.com>
2016-05-24 09:34:49 +0800

    AR-2721

--- i21Database/dbo/Stored Procedures/uspARCreateCustomerInvoice.sql
+++ i21Database/dbo/Stored Procedures/uspARCreateCustomerInvoice.sql
@@ -198,7 +198,7 @@ BEGIN TRY
 		,[strBOLNumber]
 		,[strDeliverPickup]
 		,[strComments]
-		--,[strFooterComments]
+		,[strFooterComments]
 		,[intShipToLocationId]
 		,[strShipToLocationName]
 		,[strShipToAddress]
@@ -258,7 +258,7 @@ BEGIN TRY
 		,[strBOLNumber]					= @BOLNumber
 		,[strDeliverPickup]				= @DeliverPickUp
 		,[strComments]					= @Comment
-		--,[strFooterComments]			= dbo.fnARGetFooterComment(@CompanyLocationId, C.intEntityCustomerId, 'Invoice Footer')
+		,[strFooterComments]			= dbo.fnARGetFooterComment(@CompanyLocationId, C.intEntityCustomerId, 'Invoice Footer')
 		,[intShipToLocationId]			= ISNULL(@ShipToLocationId, ISNULL(SL1.[intEntityLocationId], EL.[intEntityLocationId]))
 		,[strShipToLocationName]		= ISNULL(SL.[strLocationName], ISNULL(SL1.[strLocationName], EL.[strLocationName]))
 		,[strShipToAddress]				= ISNULL(SL.[strAddress], ISNULL(SL1.[strAddress], EL.[strAddress]))
