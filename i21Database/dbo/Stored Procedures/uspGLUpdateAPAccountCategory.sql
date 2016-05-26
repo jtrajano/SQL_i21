da3c3b75014bdf857102f25058305867092946f5
jeffrey trajano <jeffrey.trajano@irely.com>
2016-05-26 08:48:29 +0800

    GL-2718 tblGLAccountGroup did not update the intAccountCategoryId

--- i21Database/dbo/Stored Procedures/uspGLUpdateAPAccountCategory.sql
+++ i21Database/dbo/Stored Procedures/uspGLUpdateAPAccountCategory.sql
@@ -8,14 +8,17 @@ DECLARE @strMask NVARCHAR(3)
 
 SELECT @intPrimary =intAccountStructureId,@strMask = strMask, @intLength = intLength FROM tblGLAccountStructure WHERE strType = 'Primary'
 
-DECLARE @tblAP TABLE(strCOde NVARCHAR(10) COLLATE Latin1_General_CI_AS)
+DECLARE @tblAP TABLE(strCode NVARCHAR(50) COLLATE Latin1_General_CI_AS)
 
-INSERT INTO @tblAP	SELECT CONVERT(NVARCHAR(10), CONVERT(NUMERIC, apcbk_gl_ap)) from apcbkmst
-SELECT TOP 1 @apLength = LEN(strCOde) from @tblAP
+INSERT INTO @tblAP	
+SELECT SUBSTRING(CAST(apcbk_gl_ap as NVARCHAR(50)),0, CHARINDEX('.', apcbk_gl_ap))
+FROM apcbkmst
+
+SELECT TOP 1 @apLength = LEN(strCode) from @tblAP
 
 IF @intLength > @apLength
 BEGIN
-	UPDATE @tblAP SET strCOde = strCOde +  REPLICATE(@strMask, @intLength - @apLength)
+	UPDATE @tblAP SET strCode = strCode +  REPLICATE(@strMask, @intLength - @apLength)
 END
 DECLARE @intPayablesGroup  INT
 DECLARE @intPayablesCategory INT
@@ -25,12 +28,25 @@ SELECT @intLiabilityGroup = intAccountGroupId FROM tblGLAccountGroup WHERE strAc
 SELECT @intPayablesGroup = intAccountGroupId FROM tblGLAccountGroup WHERE strAccountGroup = 'Payables'
 SELECT @intPayablesCategory = intAccountCategoryId FROM tblGLAccountCategory WHERE strAccountCategory = 'AP Account'
 
-IF EXISTS(SELECT TOP 1 1 FROM tblGLAccountSegment A INNER JOIN @tblAP B ON A.strCode = B.strCOde WHERE A.intAccountGroupId = @intLiabilityGroup)
-BEGIN
-	UPDATE A SET intAccountGroupId = @intPayablesGroup,	intAccountCategoryId = @intPayablesCategory
-	FROM tblGLAccountSegment A INNER JOIN @tblAP B ON A.strCode = B.strCOde 
-	INNER JOIN tblGLAccountStructure C ON A.intAccountStructureId = C.intAccountStructureId AND C.intAccountStructureId = @intPrimary
-	WHERE A.intAccountGroupId = @intLiabilityGroup
-END
+UPDATE 
+	Segment 
+SET 
+	intAccountGroupId = @intPayablesGroup,	intAccountCategoryId = @intPayablesCategory
+FROM 
+	tblGLAccountSegment Segment 
+	JOIN @tblAP B ON Segment.strCode = B.strCode 
+	JOIN tblGLAccountStructure C ON Segment.intAccountStructureId = C.intAccountStructureId 
+WHERE 
+	Segment.intAccountGroupId = @intLiabilityGroup AND C.intAccountStructureId = @intPrimary
+
+UPDATE 
+	Account
+SET 
+	intAccountGroupId = @intPayablesGroup
+FROM 
+	tblGLAccount Account
+	JOIN tblGLCOACrossReference COA ON Account.intAccountId  = inti21Id
+	JOIN apcbkmst E ON E.apcbk_gl_ap = COA.strExternalId
+
 
 
