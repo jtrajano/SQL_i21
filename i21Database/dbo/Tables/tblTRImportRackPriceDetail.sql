538eb7912063ad51a08ae86644912358d448e887
Lawrence Zabala <lawrence.zabala@irely.com>
2016-05-27 11:51:29 +0800

    TR-79

--- i21Database/dbo/Tables/tblTRImportRackPriceDetail.sql
+++ i21Database/dbo/Tables/tblTRImportRackPriceDetail.sql
@@ -2,10 +2,12 @@
 (
 	[intImportRackPriceDetailId] INT NOT NULL IDENTITY, 
     [intImportRackPriceId] INT NOT NULL, 
-    [intItemId] INT NOT NULL, 
-    [dblVendorPrice] INT NOT NULL, 
+	[strSupplyPoint] NVARCHAR(100) COLLATE Latin1_General_CI_AS NOT NULL,
+	[intSupplyPointId] INT NOT NULL,
     [dtmEffectiveDate] DATETIME NOT NULL, 
-    [ysnSuccess] BIT NOT NULL, 
+	[strComments] NVARCHAR(250) COLLATE Latin1_General_CI_AS NULL,
+	[ysnSelected] BIT NOT NULL DEFAULT ((1)),
+	[ysnValid] BIT NOT NULL DEFAULT((1)),
     [intConcurrencyId] INT NULL DEFAULT ((0)), 
     CONSTRAINT [PK_tblTRImportRackPriceDetail] PRIMARY KEY ([intImportRackPriceDetailId]), 
     CONSTRAINT [FK_tblTRImportRackPriceDetail_tblTRImportRackPrice] FOREIGN KEY ([intImportRackPriceId]) REFERENCES [tblTRImportRackPrice]([intImportRackPriceId]) ON DELETE CASCADE
