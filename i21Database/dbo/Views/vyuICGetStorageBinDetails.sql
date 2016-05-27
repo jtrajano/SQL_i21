896542e1f02ec695b83dcdc2f7a5eb18cb791450
csWEstrada <WEstrada@COMTECH.COM>
2016-05-27 13:24:09 +0800

    IC-1920 - Report -> Bin Vizualization

--- i21Database/dbo/Views/vyuICGetStorageBinDetails.sql
+++ i21Database/dbo/Views/vyuICGetStorageBinDetails.sql
@@ -6,7 +6,8 @@ SELECT i.intItemId, i.strItemNo, i.strDescription strItemDescription, su.intItem
 	ABS(CAST(su.dblOnHand + su.dblUnitStorage AS NUMERIC(18,6))) dblStock, 
 	ABS(CAST(CASE WHEN ISNULL(stl.dblEffectiveDepth, 0) = 0 THEN 0 
 	ELSE (ISNULL(stl.dblPackFactor, 1) * ISNULL(stl.dblEffectiveDepth, 1)
-		* ISNULL(stl.dblUnitPerFoot, 1)) - (su.dblOnHand + su.dblUnitStorage) End AS NUMERIC(18,6))) dblAvailable
+		* ISNULL(stl.dblUnitPerFoot, 1)) - (su.dblOnHand + su.dblUnitStorage) End AS NUMERIC(18,6))) dblAvailable,
+	co.strCommodityCode, i.intCommodityId, stl.dblEffectiveDepth
 FROM tblICItemStockUOM su
 	INNER JOIN tblICItem i ON i.intItemId = su.intItemId
 	INNER JOIN tblICItemLocation il ON il.intItemLocationId = su.intItemLocationId
@@ -15,3 +16,4 @@ FROM tblICItemStockUOM su
 	INNER JOIN tblICStorageLocation stl ON stl.intStorageLocationId = su.intStorageLocationId
 	INNER JOIN tblICItemUOM iu ON iu.intItemUOMId = su.intItemUOMId
 	INNER JOIN tblICUnitMeasure u ON u.intUnitMeasureId = iu.intUnitMeasureId
+	LEFT OUTER JOIN tblICCommodity co ON co.intCommodityId = i.intCommodityId
