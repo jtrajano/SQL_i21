355982b77eb25d6f705eeaa8545e23a515a653d3
Chakra <CBehera@irely.net>
2016-05-30 21:04:28 +0530

    MFG-1674, MFG-1675

--- i21Database/dbo/Stored Procedures/uspMFCreateShipmentFromSalesOrderPickList.sql
+++ i21Database/dbo/Stored Procedures/uspMFCreateShipmentFromSalesOrderPickList.sql
@@ -19,6 +19,20 @@ Declare @intLotId INT
 Declare @dblShipQty NUMERIC(38,20)
 Declare @intInventoryShipmentItemId INT
 Declare @intItemId INT
+Declare @intMinSalesOrderItem INT
+Declare @dblReqQty NUMERIC(38,20)
+Declare @strItemNo nvarchar(50)
+Declare @dblSelQty NUMERIC(38,20)
+Declare @strUOM nvarchar(50)
+Declare @intItemUOMId int
+
+DECLARE @tblInputItem TABLE (
+	intRowNo INT IDENTITY(1, 1)
+	,intItemId INT
+	,dblQty NUMERIC(38,20)
+	,intItemUOMId int
+	,strLotTracking nvarchar(50)
+	)
 
 	Select TOP 1 @intPickListId=intPickListId From tblMFPickList Where intSalesOrderId=@intSalesOrderId
 	
@@ -28,6 +42,36 @@ Declare @intItemId INT
 	If Exists (Select 1 From tblICInventoryShipment sh Join tblICInventoryShipmentItem sd on sh.intInventoryShipmentId=sd.intInventoryShipmentId 
 		Where sh.intOrderType=2 AND sd.intOrderId=@intSalesOrderId)
 		RaisError('Shipmnet is alredy created for the sales order.',16,1)
+
+Insert Into @tblInputItem(intItemId,dblQty,intItemUOMId)
+Select sd.intItemId,SUM(sd.dblQtyOrdered),sd.intItemUOMId From tblSOSalesOrderDetail sd Join tblICItem i on sd.intItemId=i.intItemId 
+Where intSalesOrderId=@intSalesOrderId Group By sd.intItemId,sd.intItemUOMId
+
+Select @intMinSalesOrderItem=MIN(intRowNo) From @tblInputItem
+
+While @intMinSalesOrderItem is not null
+Begin
+	Select @intItemId=intItemId,@dblReqQty=dblQty,@intItemUOMId=intItemUOMId From @tblInputItem Where intRowNo=@intMinSalesOrderItem
+	Select @strItemNo=strItemNo From tblICItem Where intItemId=@intItemId
+
+	If NOT Exists(Select 1 From tblMFPickListDetail Where intPickListId=@intPickListId AND intItemId=@intItemId)
+	Begin
+		Set @ErrMsg='Item ' + @strItemNo + ' is not selected in the pick list.'
+		RaisError(@ErrMsg,16,1)
+	End
+
+	Select @dblSelQty=SUM(dblQuantity) From tblMFPickListDetail Where intPickListId=@intPickListId AND intItemId=@intItemId
+	Select @strUOM=um.strUnitMeasure From tblICUnitMeasure um Join tblICItemUOM iu on um.intUnitMeasureId=iu.intUnitMeasureId 
+	Where iu.intItemUOMId = @intItemUOMId
+
+	If @dblSelQty < @dblReqQty
+	Begin
+		Set @ErrMsg='Item ' + @strItemNo + ' is required ' + dbo.fnRemoveTrailingZeroes(@dblReqQty) + ' ' + @strUOM + ' but selected ' + dbo.fnRemoveTrailingZeroes(@dblSelQty) + ' ' + @strUOM + '.'
+		RaisError(@ErrMsg,16,1)
+	End
+
+	Select @intMinSalesOrderItem=MIN(intRowNo) From @tblInputItem Where intRowNo>@intMinSalesOrderItem
+End
 
 Begin Tran
 	--Create Shipment Header and Line	
