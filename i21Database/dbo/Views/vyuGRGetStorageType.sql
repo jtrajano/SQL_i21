ddec093698569c8ab570f512af5727cab986095f
Gopinath Nayak <gopinath.n@irely.com>
2016-05-30 16:48:30 +0530

    GRN-262

---
+++ i21Database/dbo/Views/vyuGRGetStorageType.sql
@@ -0,0 +1,12 @@
+﻿CREATE VIEW [dbo].[vyuGRGetStorageType]
+AS  
+SELECT Distinct
+   Cs.intEntityId
+  ,Cs.intCompanyLocationId     
+  ,Cs.intItemId  
+ ,ST.intStorageScheduleTypeId
+ ,ST.strStorageTypeCode
+ ,ST.strStorageTypeDescription
+FROM tblGRCustomerStorage Cs
+JOIN tblGRStorageType ST ON ST.intStorageScheduleTypeId=Cs.intStorageTypeId 
+Where Cs.dblOpenBalance >0 AND ISNULL(Cs.strStorageType,'') <> 'ITR' AND ST.ysnCustomerStorage=0
