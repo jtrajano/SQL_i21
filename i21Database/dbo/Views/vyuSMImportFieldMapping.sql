0952823b9514f7dc624d64bfd455c9716fb548cf
Lawrence Zabala <lawrence.zabala@irely.com>
2016-06-01 10:04:55 +0800

    TR-79

---
+++ i21Database/dbo/Views/vyuSMImportFieldMapping.sql
@@ -0,0 +1,19 @@
+﻿CREATE VIEW [dbo].[vyuSMImportFieldMapping]
+	AS
+
+SELECT intFieldMapId = FieldMap.intImportFileHeaderId
+	, FieldMap.strLayoutTitle
+	, FieldMap.strFileType
+	, FieldMap.strFieldDelimiter
+	, FieldMap.strXMLType
+	, FieldMap.strXMLInitiater
+	, RecordMarker.intImportFileRecordMarkerId
+	, RecordMarker.strRecordMarker
+	, RecordMarker.intRowsToSkip
+	, RecordMarker.intPosition
+	, RecordMarker.strCondition
+	, RecordMarker.intSequence
+	, RecordMarker.strFormat
+FROM tblSMImportFileHeader FieldMap
+LEFT JOIN tblSMImportFileRecordMarker RecordMarker ON RecordMarker.intImportFileHeaderId = FieldMap.intImportFileHeaderId
+WHERE FieldMap.ysnActive = 1
