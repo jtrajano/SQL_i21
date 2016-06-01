2f48f204c0a38f96f67febbd18d7750061db4fcf
Chakra <CBehera@irely.net>
2016-06-01 22:53:53 +0530

    MFG-1675

---
+++ i21Database/dbo/Stored Procedures/uspMFUnReservePickListBySalesOrder.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[uspMFUnReservePickListBySalesOrder]
+	@intSalesOrderId int
+AS
+
+Update tblICStockReservation Set ysnPosted=0 
+Where intTransactionId=
+(
+	Select pl.intPickListId From tblMFPickList pl Where pl.intSalesOrderId=@intSalesOrderId
+)
+AND intInventoryTransactionType=34
