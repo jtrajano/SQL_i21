e81d3234eb117e5d7b4a29e56ef4b59c2852a353
bonn ponce <bonn.ponce@irely.com>
2016-06-03 10:42:55 +0800

    CF-307

--- i21Database/dbo/Stored Procedures/uspCFImportTransaction.sql
+++ i21Database/dbo/Stored Procedures/uspCFImportTransaction.sql
@@ -53,6 +53,7 @@
 		DECLARE @dblOriginalPumpPrice			NUMERIC(18,6)	= 0.000000
 		DECLARE @guid							NVARCHAR(MAX)
 		DECLARE @processDate					NVARCHAR(MAX)
+		DECLARE @pk								INT
 
 
 		SET @guid = NEWID()
@@ -159,6 +160,31 @@
 				,@strGUID					= @guid
 				,@strProcessDate			= @processDate
 
+				SET @pk = SCOPE_IDENTITY();
+
+				INSERT INTO tblCFImportResult(
+					 dtmImportDate
+					,strSetupName
+					,ysnSuccessful
+					,strFailedReason
+					,strOriginTable
+					,strOriginIdentityId
+					,strI21Table
+					,intI21IdentityId
+					,strUserId
+				)
+				VALUES(
+					GETDATE()
+				   ,'Transaction'
+				   ,1
+				   ,''
+				   ,'cftrxmst'
+				   ,@originTransaction
+				   ,'tblCFTransaction'
+				   ,@pk
+				   ,''
+				)
+
 
 				COMMIT TRANSACTION
 				SET @TotalSuccess += 1;
@@ -168,10 +194,30 @@
 			BEGIN CATCH
 				ROLLBACK TRANSACTION
 				SET @TotalFailed += 1;
-				PRINT 'IMPORTING TRANSACTION' + ERROR_MESSAGE()
-				--INSERT INTO tblCFDiscountScheduleFailedImport(strDiscountScheduleId,strReason)					
-				--VALUES(@originCard,ERROR_MESSAGE())					
-				--PRINT 'Failed to imports' + CONVERT(@originTransaction,NVARCHAR); --@@ERROR;
+
+				INSERT INTO tblCFImportResult(
+					 dtmImportDate
+					,strSetupName
+					,ysnSuccessful
+					,strFailedReason
+					,strOriginTable
+					,strOriginIdentityId
+					,strI21Table
+					,intI21IdentityId
+					,strUserId
+				)
+				VALUES(
+					GETDATE()
+				   ,'Transaction'
+				   ,0
+				   ,ERROR_MESSAGE()
+				   ,'cftrxmst'
+				   ,@originTransaction
+				   ,'tblCFTransaction'
+				   ,null
+				   ,''
+				)
+
 				GOTO CONTINUELOOP;
 			END CATCH
 			IF(@@ERROR <> 0) 
