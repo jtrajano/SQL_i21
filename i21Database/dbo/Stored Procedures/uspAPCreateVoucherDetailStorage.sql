a967e8f0c374288f984e286c42dbe3913afd6f03
Erick F. Dela Cruz <erick.delacruz@irely.com>
2016-06-03 08:28:08 +0800

    AP-2438

---
+++ i21Database/dbo/Stored Procedures/uspAPCreateVoucherDetailStorage.sql
@@ -0,0 +1,77 @@
+﻿CREATE PROCEDURE [dbo].[uspAPCreateVoucherDetailStorage]
+	@voucherId INT,
+	@voucherDetailStorage AS [VoucherDetailStorage] READONLY
+AS
+
+
+SET QUOTED_IDENTIFIER OFF
+SET ANSI_NULLS ON
+SET NOCOUNT ON
+SET XACT_ABORT ON
+SET ANSI_WARNINGS OFF
+
+BEGIN TRY
+
+DECLARE @transCount INT = @@TRANCOUNT;
+DECLARE @voucherIds AS Id;
+DECLARE @error NVARCHAR(200);
+IF @transCount = 0 BEGIN TRANSACTION
+
+	INSERT INTO tblAPBillDetail(
+		[intBillId]						,
+		[intAccountId]					,
+		[intCustomerStorageId]			,
+		[strMiscDescription]				,
+		[dblTotal]						,
+		[dblQtyOrdered]					,
+		[dblQtyReceived]					,
+		[dblCost]						,
+		[int1099Form]					,
+		[int1099Category]				,
+		[intLineNo]						
+	)
+	SELECT
+		[intBillId]						=	@voucherId,
+		[intAccountId]					=	A.intAccountId,
+		[intCustomerStorageId]			=	A.intCustomerStorageId,
+		[strMiscDescription]				=	ISNULL(A.strMiscDescription, A2.strDescription),
+		[dblTotal]						=	A.dblCost * A.dblQtyReceived,
+		[dblQtyOrdered]					=	A.dblQtyReceived,
+		[dblQtyReceived]					=	A.dblQtyReceived,
+		[dblCost]						=	A.dblCost,
+		[int1099Form]					=	(CASE WHEN E.str1099Form = '1099-MISC' THEN 1
+													WHEN E.str1099Form = '1099-INT' THEN 2
+													WHEN E.str1099Form = '1099-B' THEN 3
+												ELSE 0 END),
+		[int1099Category]				=	ISNULL(F.int1099CategoryId, 0),
+		[intLineNo]						=	ROW_NUMBER() OVER(ORDER BY (SELECT 1))			
+	FROM @voucherDetailStorage A
+	INNER JOIN tblICItem A2 ON A.intItemId = A2.intItemId
+	CROSS APPLY tblAPBill B
+	INNER JOIN tblAPVendor D ON B.intEntityVendorId = D.intEntityVendorId
+	INNER JOIN tblEMEntity E ON D.intEntityVendorId = E.intEntityId
+	LEFT JOIN tblAP1099Category F ON E.str1099Type = F.strCategory
+	WHERE B.intBillId = @voucherId
+
+	INSERT INTO @voucherIds
+	SELECT @voucherId
+	EXEC uspAPUpdateVoucherTotal @voucherIds
+
+IF @transCount = 0 COMMIT TRANSACTION
+END TRY
+BEGIN CATCH
+	DECLARE @ErrorSeverity INT,
+			@ErrorNumber   INT,
+			@ErrorMessage nvarchar(4000),
+			@ErrorState INT,
+			@ErrorLine  INT,
+			@ErrorProc nvarchar(200);
+	-- Grab error information from SQL functions
+	SET @ErrorSeverity = ERROR_SEVERITY()
+	SET @ErrorNumber   = ERROR_NUMBER()
+	SET @ErrorMessage  = ERROR_MESSAGE()
+	SET @ErrorState    = ERROR_STATE()
+	SET @ErrorLine     = ERROR_LINE()
+	IF @transCount = 0 AND XACT_STATE() <> 0 ROLLBACK TRANSACTION
+	RAISERROR (@ErrorMessage , @ErrorSeverity, @ErrorState, @ErrorNumber)
+END CATCH
---
+++ i21Database/dbo/User Defined Types/VoucherDetailStorage.sql
@@ -0,0 +1,9 @@
+﻿CREATE TYPE [dbo].[VoucherDetailStorage] AS TABLE
+(
+	[intCustomerStorageId]		INT NOT NULL,
+	[intItemId]					INT	NOT NULL,
+	[dblQtyReceived]				DECIMAL(18, 6)	NOT NULL, 
+	[strMiscDescription]			NVARCHAR(500)	NULL, 
+    [dblCost]					DECIMAL(18, 6)	NOT NULL,
+	PRIMARY KEY CLUSTERED ([intCustomerStorageId] ASC) 
+)
