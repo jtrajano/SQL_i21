ef8541270c29ac2a0a6f9f34fa6b0af4753a6ddf
Crisman Lazaga <cris.lazaga@irely.com>
2016-06-06 15:16:02 +0800

    CCR-106

---
+++ i21Database/dbo/Views/vyuCCSiteHeader.sql
@@ -0,0 +1,17 @@
+﻿CREATE VIEW [dbo].[vyuCCSiteHeader]
+WITH SCHEMABINDING
+AS 
+SELECT
+	A.intSiteHeaderId, 
+	B.strName AS strVendorName,
+	A.strCcdReference,
+	A.strReference,
+	A.strApType,
+	A.dtmDate,
+	A.ysnPosted,
+	A.dblGross,
+	A.dblFees,
+	A.dblNet
+FROM dbo.tblCCSiteHeader AS A	
+	INNER JOIN dbo.vyuCCVendor AS B
+		ON A.intVendorDefaultId = B.intVendorDefaultId
