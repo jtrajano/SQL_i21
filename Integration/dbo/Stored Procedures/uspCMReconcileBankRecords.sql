c8b1d4a1e176b62df5792365534e5d3e95340393
Smith Wilson de Jesus <smith.dejesus@irely.com>
2016-06-06 21:17:30 +0800

    CM-1235 HDTN-181610 - Cash Management Bank Reconciliation error pop up

--- Integration/dbo/Stored Procedures/uspCMReconcileBankRecords.sql
+++ Integration/dbo/Stored Procedures/uspCMReconcileBankRecords.sql
@@ -57,18 +57,7 @@ BEGIN
 	CREATE TABLE #tmpOriginDepositTransaction(strLink NVARCHAR(50))
 
 	INSERT INTO #tmpOriginDepositTransaction
-	SELECT	strLink = ( CAST(a.apchk_cbk_no AS NVARCHAR(2)) 
-									+ CAST(a.apchk_rev_dt AS NVARCHAR(10)) 
-									+ CAST(a.apchk_trx_ind AS NVARCHAR(1)) 
-									+ CAST(a.apchk_chk_no AS NVARCHAR(8))
-						) COLLATE Latin1_General_CI_AS
-					FROM	dbo.apchkmst a INNER JOIN dbo.aptrxmst b
-								ON a.apchk_cbk_no = b.aptrx_cbk_no
-								AND a.apchk_chk_no = b.aptrx_chk_no
-								AND a.apchk_trx_ind = b.aptrx_trans_type			
-								AND a.apchk_rev_dt = b.aptrx_chk_rev_dt
-								AND a.apchk_vnd_no = b.aptrx_vnd_no
-					WHERE	 b.aptrx_trans_type = ''O'' -- Other CW transactions
+	SELECT strLink FROM [dbo].[fnGetDepositEntry]()
 
 	-- Log the status of the transactions to clear prior to the reconciliation. 
 	INSERT INTO [dbo].[tblCMBankReconciliationAudit]
---
+++ i21Database/dbo/Functions/fnGetDepositEntry.sql
@@ -0,0 +1,18 @@
+﻿
+-- Create a default function. 
+-- 
+-- It always return none. It means all bank records are assumed to be created in i21 and not in the Deposit Entry (origin). 
+
+-- This default function solves the following: 
+-- 1. If Integration is not established, it is assumed all bank records are not from the Deposit Entry. 
+-- 2. It avoids failure on all stored procedures in i21Database that is using this function. 
+-- 3. When integration is established, the function is overwritten by the integration script deployment. 
+CREATE FUNCTION [dbo].[fnGetDepositEntry]()
+	RETURNS @OriginDepositEntrty TABLE 
+	(
+		strLink NVARCHAR(100) COLLATE Latin1_General_CI_AS
+	)
+AS
+BEGIN
+	RETURN
+END
