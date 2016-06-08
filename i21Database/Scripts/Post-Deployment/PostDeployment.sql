c4107b3cbde8900bd98cca81b25be232e0ae2fba
Mark Paragas <mark.paragas@irely.com>
2016-06-09 00:53:55 +0800

    MFT-142

--- i21Database/Scripts/Post-Deployment/PostDeployment.sql
+++ i21Database/Scripts/Post-Deployment/PostDeployment.sql
@@ -343,7 +343,7 @@ print 'BEGIN POST DEPLOYMENT'
 :r .\TF\DefaultData\_ConfigurationType.sql
 :r .\TF\DefaultData\_ConfigurationTemplate.sql
 :r .\TF\DefaultData\_OriginDestinationState.sql
-
+:r .\TF\DefaultData\_ReportingComponent.sql
 
 --Transports
 :R .\TR\01_OldTransportLoadConversion.sql
