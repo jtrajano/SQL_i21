5516ae97c242ae05573ee044b192df783133af6e
Darwin Estrada <Darwin Estrada>
2016-06-08 14:24:04 +0800

    IET-58 IET-48 IET-49 IET-50

---
+++ i21Database/dbo/Tables/tblETExportFilterTerm.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE tblETExportFilterTerm(
+	[intExportFilterTermId] int IDENTITY(1,1) NOT NULL,
+	[intTermId] int NOT NULL,
+	[intConcurrencyId] [int] NOT NULL,
+CONSTRAINT [PK_tblETExportFilterTerm] PRIMARY KEY CLUSTERED
+(
+	[intExportFilterTermId] ASC
+))
