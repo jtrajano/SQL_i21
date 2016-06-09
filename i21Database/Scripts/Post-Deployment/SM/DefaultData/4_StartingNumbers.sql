e72411b1cc34fece501bf8f482b6e127d632fe24
Satya <ssahoo@BLRSATYAWIN7.irely.net>
2016-06-09 21:25:35 +0530

    CT-693

--- i21Database/Scripts/Post-Deployment/SM/DefaultData/4_StartingNumbers.sql
+++ i21Database/Scripts/Post-Deployment/SM/DefaultData/4_StartingNumbers.sql
@@ -894,6 +894,16 @@ GO
 			,[intConcurrencyId]		= 1
 	WHERE NOT EXISTS (SELECT TOP 1 1 FROM tblSMStartingNumber WHERE strTransactionType = N'Least Cost Routing')
 
+	UNION ALL
+	SELECT	[intStartingNumberId]	= 97
+			,[strTransactionType]	= N'Clean Cost'
+			,[strPrefix]			= N'CC-'
+			,[intNumber]			= 1
+			,[strModule]			= 'Contract Management'
+			,[ysnEnable]			= 1
+			,[intConcurrencyId]		= 1
+	WHERE NOT EXISTS (SELECT TOP 1 1 FROM tblSMStartingNumber WHERE strTransactionType = N'Clean Cost')
+
 	SET IDENTITY_INSERT [dbo].[tblSMStartingNumber] OFF
 GO
 	PRINT N'END INSERT DEFAULT STARTING NUMBERS'
