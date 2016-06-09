4a5a63afdbe9a49715996d052965503fbeb3a681
Wendell Wayne Estrada <wendell.estrada@irely.com>
2016-06-09 16:46:01 +0800

    IC-1331 Energy Trac View for Inventory Items

---
+++ i21Database/dbo/Views/vyuICEnergyTracExportItem.sql
@@ -0,0 +1,19 @@
+﻿CREATE VIEW [dbo].[vyuICEnergyTracExportItem]
+AS
+SELECT Item.intItemId, ItemLocation.intItemLocationId, 
+	Item.strItemNo, Item.strDescription, Item.strType strItemType, Item.strLotTracking,
+	CASE WHEN Item.strType IN ('Non-Inventory', 'Other Charge', 'Service', 'Software') THEN 'Y' ELSE 'N' END strCounted,
+	Measure.strUnitMeasure, ItemPricing.dblSalePrice, ItemAccount.intAccountId, Item.strStatus, Account.intAccountGroupId
+FROM tblICItem Item
+	LEFT OUTER JOIN tblICItemLocation ItemLocation ON ItemLocation.intItemId = Item.intItemId
+	LEFT OUTER JOIN tblSMCompanyLocation Location ON ItemLocation.intLocationId = Location.intCompanyLocationId
+	LEFT OUTER JOIN tblSMCompanyLocationSubLocation sub ON sub.intCompanyLocationSubLocationId = ItemLocation.intSubLocationId
+	LEFT OUTER JOIN tblICStorageLocation sloc ON sloc.intStorageLocationId = ItemLocation.intStorageLocationId
+	LEFT OUTER JOIN tblICItemUOM UOM ON UOM.intItemId = Item.intItemId AND UOM.intItemUOMId = ItemLocation.intIssueUOMId
+		AND UOM.ysnStockUnit = 1
+	LEFT OUTER JOIN tblICUnitMeasure Measure ON Measure.intUnitMeasureId = UOM.intUnitMeasureId
+	LEFT OUTER JOIN tblICItemPricing ItemPricing ON ItemPricing.intItemId = Item.intItemId
+		AND ItemPricing.intItemLocationId = ItemLocation.intItemLocationId
+	LEFT OUTER JOIN tblICItemAccount ItemAccount ON ItemAccount.intItemId = Item.intItemId
+	LEFT OUTER JOIN tblGLAccount Account ON Account.intAccountId = ItemAccount.intAccountId
+WHERE Item.ysnUsedForEnergyTracExport = 1
