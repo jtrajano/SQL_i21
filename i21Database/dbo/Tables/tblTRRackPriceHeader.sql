4fa208f99ecf48672896e776ee909176fe04612d
Lawrence Zabala <lawrence.zabala@irely.com>
2016-06-09 08:47:09 +0800

    TR-79

--- i21Database/dbo/Tables/tblTRRackPriceHeader.sql
+++ i21Database/dbo/Tables/tblTRRackPriceHeader.sql
@@ -2,9 +2,9 @@
 (
 	[intRackPriceHeaderId] INT NOT NULL IDENTITY,
 	[intSupplyPointId] INT NOT NULL,
-	[dtmEffectiveDateTime]  DATETIME        NOT NULL,
-	[strComments] nvarchar(max) COLLATE Latin1_General_CI_AS NULL,
-	[intConcurrencyId] [int] NOT NULL,
+	[dtmEffectiveDateTime]  DATETIME NOT NULL,
+	[strComments] NVARCHAR(max) COLLATE Latin1_General_CI_AS NULL,
+	[intConcurrencyId] INT NULL DEFAULT((0)),
 	CONSTRAINT [PK_tblTRRackPriceHeader] PRIMARY KEY ([intRackPriceHeaderId]),
 	CONSTRAINT [FK_tblTRRackPriceHeader_tblTRSupplyPoint_intSupplyPointId] FOREIGN KEY ([intSupplyPointId]) REFERENCES [dbo].[tblTRSupplyPoint] ([intSupplyPointId])
 )
