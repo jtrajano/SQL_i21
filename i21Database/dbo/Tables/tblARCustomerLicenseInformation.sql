18b35d622dfc7a6da5f36afb283cff4b171e5a38
Dan Michael Jalimao <dan.jalimao@irely.com>
2016-06-09 20:28:51 +0800

    FCOM-1357

--- i21Database/dbo/Tables/tblARCustomerLicenseInformation.sql
+++ i21Database/dbo/Tables/tblARCustomerLicenseInformation.sql
@@ -1,17 +1,19 @@
 ﻿CREATE TABLE [dbo].[tblARCustomerLicenseInformation]
 (
 	[intCustomerLicenseInformationId]	INT NOT NULL IDENTITY(1,1),
-	[strUniqueId]						NVARCHAR(40) COLLATE Latin1_General_CI_AS NOT NULL ,
+	[strUniqueId]						NVARCHAR(40) COLLATE Latin1_General_CI_AS NOT NULL,
 	[intEntityCustomerId]				INT NOT NULL,
-	[strCompanyId]						NVARCHAR(100) COLLATE Latin1_General_CI_AS NOT NULL ,
+	[strCompanyId]						NVARCHAR(100) COLLATE Latin1_General_CI_AS NOT NULL,
 	[intNumberOfUser]					INT NOT NULL,
+	[strDescription]					NVARCHAR(150) COLLATE Latin1_General_CI_AS NOT NULL,
 	[intNumberOfSite]					INT NOT NULL,
 	[dtmDateIssued]						DATETIME NOT NULL DEFAULT(GETDATE()),
-	[dtmDateExpiration]						DATETIME NOT NULL DEFAULT(GETDATE()),
-	[strLicenseKey]						NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NOT NULL ,
-	
+	[dtmDateExpiration]					DATETIME NOT NULL DEFAULT(GETDATE()),
+	[dtmSupportExpiration]				DATETIME NOT NULL DEFAULT(GETDATE()),
+	[strLicenseKey]						NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NOT NULL,
+	[ysnNew]							BIT NOT NULL DEFAULT 0, 	
     [intConcurrencyId]					INT CONSTRAINT [DF_tblARCustomerLicenseInformation_intConcurrencyId] DEFAULT ((0)) NOT NULL,
-	
-	CONSTRAINT [PK_tblARCustomerLicenseInformation] PRIMARY KEY CLUSTERED ([intCustomerLicenseInformationId] ASC),
+
+    CONSTRAINT [PK_tblARCustomerLicenseInformation] PRIMARY KEY CLUSTERED ([intCustomerLicenseInformationId] ASC),
 	CONSTRAINT [FK_tblARCustomerLicenseInformation_tblARCustomer] FOREIGN KEY ([intEntityCustomerId]) REFERENCES [dbo].[tblARCustomer] ([intEntityCustomerId]) ON DELETE CASCADE,
 )
