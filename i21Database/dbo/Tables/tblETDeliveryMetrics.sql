07323b1e89eeaec28c1b23f4740216b8779486e9
Manuel Armas <manny.armas@irely.com>
2016-06-09 16:35:49 +0800

    IET-7 add [intConcurrencyId] column

--- i21Database/dbo/Tables/tblETDeliveryMetrics.sql
+++ i21Database/dbo/Tables/tblETDeliveryMetrics.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[tblETDeliveryMetrics]
 (
     [intDeliveryMetricsId] INT NOT NULL IDENTITY , 
-    [intBeginningOdometerReading ] INT NULL, 
+    [intBeginningOdometerReading] INT NULL, 
     [intEndingOdometerReading] INT NULL, 
     [dblGallonsDelivered] NUMERIC(18, 6) NULL DEFAULT 0, 
     [dblTotalFuelSales] NUMERIC(18, 6) NULL DEFAULT 0, 
@@ -11,5 +11,6 @@
     [strShiftNumber] NVARCHAR(10) NULL, 
     [dtmShiftBeginDate] DATETIME NULL, 
 	[dtmShiftEndDate] DATETIME NULL, 
+	[intConcurrencyId] INT NOT NULL DEFAULT 1,
     CONSTRAINT [PK_tblETDeliveryMetrics] PRIMARY KEY ([intDeliveryMetricsId])
 )
