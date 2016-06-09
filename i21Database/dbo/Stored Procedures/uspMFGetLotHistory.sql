2b1e7f6c288a18c5386f035a48e7ad82bd45fa4d
Premkumar S <prem.selladurai@irely.com>
2016-06-09 18:16:15 +0530

    MFG-1814

--- i21Database/dbo/Stored Procedures/uspMFGetLotHistory.sql
+++ i21Database/dbo/Stored Procedures/uspMFGetLotHistory.sql
@@ -10,7 +10,11 @@ BEGIN
 	SELECT @dblPrimaryQty = 0
 		,@dblPrimaryWeight = 0
 
-	SELECT ilt.dtmDate AS dtmDateTime
+	SELECT CASE 
+			WHEN Convert(DATETIME, Convert(CHAR, dtmDate, 101)) = Convert(DATETIME, Convert(CHAR, dtmCreated, 101))
+				THEN dtmCreated
+			ELSE dtmDate
+			END AS dtmDateTime
 		,l.strLotNumber AS strLotNo
 		,CASE 
 			WHEN iad.intNewItemId IS NULL
@@ -25,7 +29,15 @@ BEGIN
 		,c.strCategoryCode
 		,clsl.strSubLocationName AS strSubLocation
 		,sl.strName AS strStorageLocation
-		,itt.strName AS strTransaction
+		,CASE 
+			WHEN itt.strName = 'Produce'
+				AND ilt.dblQty < 0
+				THEN 'Produce Reversal'
+			WHEN itt.strName = 'Consume'
+				AND ilt.dblQty > 0
+				THEN 'Consume Reversal'
+			ELSE itt.strName
+			END AS strTransaction
 		,CONVERT(NUMERIC(38, 20), 0.0) AS dblWeight
 		,CONVERT(NUMERIC(38, 20), ilt.dblQty) AS dblTransactionWeight
 		,uwm.strUnitMeasure AS strTransactionWeightUOM
@@ -73,7 +85,13 @@ BEGIN
 	LEFT JOIN tblICInventoryTransaction ilt ON ilt.intLotId = l.intLotId
 	LEFT JOIN tblICInventoryTransactionType itt ON itt.intTransactionTypeId = ilt.intTransactionTypeId
 	LEFT JOIN tblICInventoryAdjustmentDetail iad ON ilt.intTransactionDetailId = iad.intInventoryAdjustmentDetailId
-	LEFT JOIN tblICItem i ON i.intItemId = ISNULL((Case When ilt.intTransactionTypeId  =15 Then iad.intItemId Else ilt.intItemId End),ilt.intItemId) 
+	LEFT JOIN tblICItem i ON i.intItemId = ISNULL((
+				CASE 
+					WHEN ilt.intTransactionTypeId = 15
+						THEN iad.intItemId
+					ELSE ilt.intItemId
+					END
+				), ilt.intItemId)
 	JOIN tblICItemUOM iu ON iu.intItemUOMId = l.intItemUOMId
 	JOIN tblICUnitMeasure um ON um.intUnitMeasureId = iu.intUnitMeasureId
 	LEFT JOIN tblICItemUOM iwu ON iwu.intItemUOMId = IsNULL(l.intWeightUOMId, l.intItemUOMId)
