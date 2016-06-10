54ce51307a196dee529b440693c17d58d54bc316
Swetosuvro Ghosh <swetosuvro.g@irely.com>
2016-06-10 16:17:36 +0530

    Cherry Picked From 16.2Dev LG-453

--- i21Database/dbo/Views/vyuLGLoadWarehouseContainerView.sql
+++ i21Database/dbo/Views/vyuLGLoadWarehouseContainerView.sql
@@ -1,28 +1,29 @@
 ﻿CREATE VIEW vyuLGLoadWarehouseContainerView
 AS
-SELECT  L.intLoadId
-		,LDV.intLoadDetailId
-		,LC.intLoadContainerId
-		,LWC.intLoadWarehouseContainerId
+SELECT   L.intLoadId
 		,L.strLoadNumber
 		,L.strBLNumber
 		,L.dtmBLDate
 		,L.dtmScheduledDate
-		,LDV.strCustomerFax
-		,LDV.strCustomerMobile
-		,LDV.strCustomerNo
-		,LDV.strCustomerPhone
-		,LDV.strCustomerReference
-		,LDV.strDispatcher
-		,LDV.strDriver
-		,LDV.strEquipmentType
-		,LDV.strExternalLoadNumber
-		,LDV.strHauler
-		,LDV.strItemDescription
-		,LDV.strItemNo
-		,LDV.strItemUOM
-		,LDV.strLoadDirectionMsg
-		,LDV.strLotTracking
+		,L.strExternalLoadNumber
+
+		,LD.intLoadDetailId
+		,strCustomerFax = CEN.strFax
+		,strCustomerMobile = CEN.strMobile
+		,strCustomerNo = CEN.strEntityNo
+		,strCustomerPhone = CEN.strPhone
+		,LD.strCustomerReference
+		,strDispatcher = US.strUserName 
+		,strDriver = Driver.strName
+		,strEquipmentType = EQ.strEquipmentType
+		,strHauler = Hauler.strName
+		,Item.strDescription AS strItemDescription
+		,Item.strItemNo
+		,Item.strLotTracking
+		,strItemUOM = UOM.strUnitMeasure
+		,LD.strLoadDirectionMsg
+
+		,LC.intLoadContainerId
 		,LC.strComments
 		,LC.strContainerNumber
 		,LC.strCustomsComments
@@ -32,6 +33,7 @@ SELECT  L.intLoadId
 		,LC.strOtherMarks
 		,LC.strSealNumber
 		,LC.strUSDAComments
+
 		,LW.intLoadWarehouseId
 		,LW.strDeliveryNoticeNumber
 		,LW.dtmDeliveryDate
@@ -40,11 +42,22 @@ SELECT  L.intLoadId
 		,LW.strFreightComments
 		,LW.strOtherComments
 		,LW.strSampleComments
+
+		,LWC.intLoadWarehouseContainerId
 		,CLSL.strSubLocationName
-FROM vyuLGLoadView L
-JOIN vyuLGLoadDetailView LDV ON L.intLoadId = LDV.intLoadId
-JOIN tblLGLoadDetailContainerLink LDCL ON LDCL.intLoadDetailId = LDV.intLoadDetailId
+FROM tblLGLoad L
+JOIN tblLGLoadDetail LD ON L.intLoadId = LD.intLoadId
+JOIN tblLGLoadDetailContainerLink LDCL ON LDCL.intLoadDetailId = LD.intLoadDetailId
 JOIN tblLGLoadContainer LC ON LC.intLoadContainerId =  LDCL.intLoadContainerId
 JOIN tblLGLoadWarehouseContainer LWC ON LWC.intLoadContainerId = LC.intLoadContainerId
 JOIN tblLGLoadWarehouse LW ON LW.intLoadWarehouseId = LWC.intLoadWarehouseId
 JOIN tblSMCompanyLocationSubLocation CLSL ON CLSL.intCompanyLocationSubLocationId = LW.intSubLocationId
+LEFT JOIN tblEMEntity CEN ON CEN.intEntityId = LD.intCustomerEntityId
+LEFT JOIN tblEMEntityLocation CEL ON CEL.intEntityLocationId = LD.intCustomerEntityLocationId
+LEFT JOIN tblICItem Item On Item.intItemId = LD.intItemId
+LEFT JOIN tblICItemUOM ItemUOM ON ItemUOM.intItemUOMId = LD.intItemUOMId
+LEFT JOIN tblICUnitMeasure UOM ON UOM.intUnitMeasureId = ItemUOM.intUnitMeasureId
+LEFT JOIN tblEMEntity Hauler ON Hauler.intEntityId = L.intHaulerEntityId
+LEFT JOIN tblEMEntity Driver ON Driver.intEntityId = L.intDriverEntityId
+LEFT JOIN tblLGEquipmentType EQ ON EQ.intEquipmentTypeId = L.intEquipmentTypeId
+LEFT JOIN tblSMUserSecurity US ON US.intEntityUserSecurityId = L.intDispatcherId
