dc86029d8dce331aa88468ea2c11c2ae932dbb36
Vinoth Kumar T <vinoth.t@irely.com>
2016-06-10 22:02:24 +0530

    QC-132, QC-133

--- i21Database/dbo/Stored Procedures/uspQMReportCOAHeader.sql
+++ i21Database/dbo/Stored Procedures/uspQMReportCOAHeader.sql
@@ -1,6 +1,6 @@
-﻿--EXEC uspQMReportCOAHeader @intInventoryShipmentItemLotId = 1231;
+﻿--EXEC uspQMReportCOAHeader '<?xml version="1.0" encoding="utf-16"?><xmlparam><filters><filter><fieldname>intInventoryShipmentItemLotId</fieldname><condition>EQUAL TO</condition><from>1231</from><join /><begingroup /><endgroup /><datatype>Int32</datatype></filter></filters></xmlparam>'
 CREATE PROCEDURE uspQMReportCOAHeader
-     @intInventoryShipmentItemLotId INT
+     @xmlParam NVARCHAR(MAX) = NULL
 AS
 SET QUOTED_IDENTIFIER OFF
 SET ANSI_NULLS ON
@@ -10,6 +10,52 @@ SET ANSI_WARNINGS OFF
 
 BEGIN TRY
 	DECLARE @ErrMsg NVARCHAR(MAX)
+	DECLARE @intInventoryShipmentItemLotId INT
+		,@xmlDocumentId INT
+
+	IF LTRIM(RTRIM(@xmlParam)) = ''
+		SET @xmlParam = NULL
+
+	IF (@xmlParam IS NULL)
+	BEGIN
+		SELECT NULL intLotId
+			,NULL intInventoryShipmentId
+			,NULL intInventoryShipmentItemLotId
+			,NULL intItemId
+
+		RETURN
+	END
+
+	DECLARE @temp_xml_table TABLE (
+		[fieldname] NVARCHAR(50)
+		,condition NVARCHAR(20)
+		,[from] NVARCHAR(50)
+		,[to] NVARCHAR(50)
+		,[join] NVARCHAR(10)
+		,[begingroup] NVARCHAR(50)
+		,[endgroup] NVARCHAR(50)
+		,[datatype] NVARCHAR(50)
+		)
+
+	EXEC sp_xml_preparedocument @xmlDocumentId OUTPUT
+		,@xmlParam
+
+	INSERT INTO @temp_xml_table
+	SELECT *
+	FROM OPENXML(@xmlDocumentId, 'xmlparam/filters/filter', 2) WITH (
+			[fieldname] NVARCHAR(50)
+			,condition NVARCHAR(20)
+			,[from] NVARCHAR(50)
+			,[to] NVARCHAR(50)
+			,[join] NVARCHAR(10)
+			,[begingroup] NVARCHAR(50)
+			,[endgroup] NVARCHAR(50)
+			,[datatype] NVARCHAR(50)
+			)
+
+	SELECT @intInventoryShipmentItemLotId = [from]
+	FROM @temp_xml_table
+	WHERE [fieldname] = 'intInventoryShipmentItemLotId'
 
 	SELECT L.intLotId
 		,S.intInventoryShipmentId
