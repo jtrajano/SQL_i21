9228700724e468ff7b23127e2d8d6fd6ff1bacec
Swetosuvro Ghosh <swetosuvro.g@irely.com>
2016-06-13 18:43:23 +0530

    LG-453

---
+++ i21Database/dbo/Views/vyuLGLoadCostView.sql
@@ -0,0 +1,22 @@
+﻿CREATE VIEW vyuLGLoadCostView
+AS
+SELECT LC.intLoadCostId
+	  ,LC.intLoadId
+	  ,LC.intItemId
+	  ,LC.intVendorId
+	  ,LC.strCostMethod
+	  ,LC.dblRate
+	  ,LC.intItemUOMId
+	  ,LC.ysnAccrue
+	  ,LC.ysnMTM
+	  ,LC.ysnPrice
+	  ,E.strName AS strEntityName
+	  ,L.strLoadNumber
+	  ,UM.strUnitMeasure
+	  ,I.strItemNo
+FROM tblLGLoadCost LC
+JOIN tblEMEntity E ON E.intEntityId = LC.intVendorId
+JOIN tblLGLoad L ON L.intLoadId = LC.intLoadId
+LEFT JOIN tblICItemUOM IU ON IU.intItemUOMId = LC.intItemUOMId
+LEFT JOIN tblICUnitMeasure UM ON UM.intUnitMeasureId = IU.intUnitMeasureId
+LEFT JOIN tblICItem I ON I.intItemId = LC.intItemId
---
+++ i21Database/dbo/Views/vyuLGLoadWarehouseView.sql
@@ -0,0 +1,29 @@
+﻿CREATE VIEW vyuLGLoadWarehouseView
+AS
+SELECT LW.intLoadWarehouseId
+	  ,LW.intConcurrencyId
+	  ,LW.strDeliveryNoticeNumber
+	  ,LW.dtmDeliveryNoticeDate
+	  ,LW.intSubLocationId
+	  ,LW.intHaulerEntityId
+	  ,LW.dtmPickupDate
+	  ,LW.dtmDeliveryDate
+	  ,LW.dtmLastFreeDate
+	  ,LW.dtmStrippingReportReceivedDate
+	  ,LW.dtmSampleAuthorizedDate
+	  ,LW.strStrippingReportComments
+	  ,LW.strFreightComments
+	  ,LW.strSampleComments
+	  ,LW.strOtherComments
+	  ,LW.intWarehouseRateMatrixHeaderId
+	  ,L.intLoadId
+	  ,L.strLoadNumber
+	  ,WRMH.strServiceContractNo AS strServiceContract
+	  ,Hauler.strName AS strShipVia
+	  ,CLSL.strSubLocationName AS strWarehouse
+FROM tblLGLoadWarehouse LW
+JOIN tblLGLoad L ON L.intLoadId = LW.intLoadId
+JOIN tblSMCompanyLocationSubLocation CLSL ON CLSL.intCompanyLocationSubLocationId = LW.intSubLocationId
+LEFT JOIN tblEMEntity Hauler ON Hauler.intEntityId = LW.intHaulerEntityId
+LEFT JOIN tblEMEntity Driver ON Driver.intEntityId = L.intDriverEntityId
+LEFT JOIN tblLGWarehouseRateMatrixHeader WRMH ON WRMH.intWarehouseRateMatrixHeaderId = LW.intWarehouseRateMatrixHeaderId
