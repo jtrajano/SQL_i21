d091c6291b53670333930a3fc02e0d5b6f3fc85c
Jb Mago <benedict.mago@irely.com>
2016-06-14 16:47:53 +0800

    IET-66 IET-64

---
+++ i21Database/dbo/Views/vyuSMETExportTerm.sql
@@ -0,0 +1,17 @@
+﻿CREATE VIEW [dbo].[vyuSMETExportTerm]
+AS 
+SELECT [Location].[strLocationNumber] AS [trloc], 
+[Term].[strTerm] AS [trdesc], 
+[Term].[strTermCode] AS [trtrm], 
+[Term].[intDiscountDay] AS [trdays],
+'D' AS [trtype],
+'0.000000' AS [trpct],
+'N' AS [ovr_price]
+FROM tblSMCompanyLocation Location
+CROSS JOIN tblSMTerm Term
+LEFT JOIN tblETExportFilterTerm ExportTerm
+	on Term.intTermID = ExportTerm.intTermId
+LEFT JOIN tblETExportFilterLocation ExportLocation
+	on Location.intCompanyLocationId = ExportLocation.intCompanyLocationId
+WHERE Term.intTermID = ExportTerm.intTermId OR Location.intCompanyLocationId = ExportLocation.intCompanyLocationId
+--WHERE [Term].[ysnEnergyTrac] = 1
