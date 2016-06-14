21740113e96bd5a2df63594ff7d76fb67f6d1110
Swetosuvro Ghosh <swetosuvro.g@irely.com>
2016-06-14 19:40:18 +0530

    LG-453

---
+++ i21Database/dbo/Views/vyuLGLoadDetailLotsView.sql
@@ -0,0 +1,25 @@
+﻿CREATE View vyuLGLoadDetailLotsView
+AS
+SELECT L.strLoadNumber
+	  ,L.intLoadId
+	  ,LD.intLoadDetailId
+	  ,LDL.intLoadDetailLotId
+	  ,LDL.intLotId
+	  ,LDL.dblLotQuantity
+	  ,LDL.intItemUOMId
+	  ,LDL.dblGross
+	  ,LDL.dblTare
+	  ,LDL.dblNet
+	  ,LDL.intWeightUOMId
+	  ,LDL.strWarehouseCargoNumber
+	  ,UM.strUnitMeasure AS strItemUnitMeasure
+	  ,WUM.strUnitMeasure AS strWeightUnitMeasure
+	  ,LOT.strLotNumber 
+FROM tblLGLoad L
+JOIN tblLGLoadDetail LD ON L.intLoadId = LD.intLoadId
+JOIN tblLGLoadDetailLot LDL ON LDL.intLoadDetailId = LD.intLoadDetailId
+JOIN tblICLot LOT ON LOT.intLotId = LDL.intLotId
+JOIN tblICItemUOM IU ON IU.intItemUOMId = LOT.intItemUOMId
+JOIN tblICUnitMeasure UM ON UM.intUnitMeasureId = IU.intUnitMeasureId
+JOIN tblICItemUOM WU ON WU.intItemUOMId = LOT.intWeightUOMId
+JOIN tblICUnitMeasure WUM ON WUM.intUnitMeasureId = WU.intUnitMeasureId
