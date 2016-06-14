7bdc080304e4642de54f21bff39ceaaffa450861
Premkumar S <prem.selladurai@irely.com>
2016-06-14 12:38:49 +0530

    MFG-1820

--- i21Database/dbo/Stored Procedures/uspMFPostConsumptionProduction.sql
+++ i21Database/dbo/Stored Procedures/uspMFPostConsumptionProduction.sql
@@ -50,7 +50,7 @@ BEGIN
 		,@OtherChargesGLAccounts AS dbo.ItemOtherChargesGLAccount
 		,@intItemId1 INT
 		,@strItemNo1 AS NVARCHAR(50)
-
+		,@intRecipeItemUOMId INT
 	DECLARE @tblMFLot TABLE (
 		intRecordId INT Identity(1, 1)
 		,intLotId INT
@@ -515,6 +515,10 @@ BEGIN
 			,NULL
 			,@intUserId
 
+		SELECT @intRecipeItemUOMId = intItemUOMId
+		FROM tblMFWorkOrderRecipe
+		WHERE intWorkOrderId = @intWorkOrderId
+
 		INSERT INTO @GLEntriesForOtherCost
 		SELECT dtmDate = @dtmDate
 			,intItemId = @intItemId
@@ -523,7 +527,13 @@ BEGIN
 			,intChargeItemLocation = @intItemLocationId
 			,intTransactionId = @intBatchId
 			,strTransactionId = @strWorkOrderNo
-			,dblCost = @dblOtherCharges
+			,dblCost = (
+				CASE 
+					WHEN @intRecipeItemUOMId = @intItemUOMId
+						THEN @dblOtherCharges * @dblQty
+					ELSE @dblOtherCharges * @dblWeight
+					END
+				)
 			,intTransactionTypeId = @INVENTORY_PRODUCE
 			,intCurrencyId = (
 				SELECT TOP 1 intCurrencyId
