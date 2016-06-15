cf6c60958ddfcaa6535dbab2218ee4de377dfb3b
jeffrey trajano <jeffrey.trajano@irely.com>
2016-06-15 10:31:50 +0800

    GL-2723 Canned report: Sign should be negative on credit

--- i21Database/dbo/Functions/fnGLGetBeginningBalanceAndUnit.sql
+++ i21Database/dbo/Functions/fnGLGetBeginningBalanceAndUnit.sql
@@ -20,7 +20,7 @@ BEGIN
 					strAccountId,
 					SUM ( CASE 
 						  WHEN D.dtmDateFrom IS NULL THEN 0 
-						  WHEN @accountType = 'Revenue' THEN dblCredit - dblDebit
+						  WHEN @accountType = 'Revenue' THEN (dblCredit - dblDebit)*-1
 						  ELSE dblDebit - dblCredit
 					END)  beginBalance,
 					SUM( 
@@ -41,7 +41,7 @@ BEGIN
 				strAccountId,
 				SUM( 
 				CASE WHEN B.strAccountType = 'Asset' THEN dblDebit - dblCredit
-						ELSE dblCredit - dblDebit
+						ELSE (dblCredit - dblDebit)*-1
 				END)  beginBalance,
 				SUM( 
 				CASE WHEN B.strAccountType = 'Asset' THEN dblDebitUnit - dblCreditUnit
