5034a18725a313f3177a165e5ada4ed5df94531c
Darwin Estrada <Darwin Estrada>
2016-06-16 11:23:54 +0800

    fixed ET Preference table

--- i21Database/dbo/Tables/tblETCompanyPreference.sql
+++ i21Database/dbo/Tables/tblETCompanyPreference.sql
@@ -2,5 +2,10 @@
 (
 	[intCompanyPreferenceId] INT NOT NULL PRIMARY KEY IDENTITY, 
     [strXMLPath] NVARCHAR(500) COLLATE Latin1_General_CI_AS NULL DEFAULT '', 
-	[intConcurrencyId] INT NOT NULL DEFAULT 1
+	[intConcurrencyId] INT NOT NULL DEFAULT 1,
+	[strBasePath] [nvarchar](max) COLLATE Latin1_General_CI_AS NULL,
+	[strExportPath] [nvarchar](max) COLLATE Latin1_General_CI_AS NULL,
+	[strUploadPath] [nvarchar](max) COLLATE Latin1_General_CI_AS NULL,
+	[strArchivePath] [nvarchar](150) COLLATE Latin1_General_CI_AS NULL,
+
 )
