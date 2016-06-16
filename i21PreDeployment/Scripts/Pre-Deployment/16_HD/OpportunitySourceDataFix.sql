55bd32f21c90ea9270a6442fa93fd1c255e40d45
Jayson Ramos <jayson.ramos@iRely.com>
2016-06-16 16:02:52 +0800

    HD-1469

--- i21PreDeployment/Scripts/Pre-Deployment/16_HD/OpportunitySourceDataFix.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/16_HD/OpportunitySourceDataFix.sql
@@ -3,11 +3,14 @@ GO
 	PRINT N'Start fixing HD Opportunity Source existing data.'
 GO
 
-	update tblHDOpportunitySource set tblHDOpportunitySource.strSource = tblHDOpportunitySource.strSource+'_'+convert(nvarchar(20),tblHDOpportunitySource.intOpportunitySourceId) where tblHDOpportunitySource.strSource in (
-		select strSource from (
-			select s1.strSource, cnt = count(s1.strSource) from tblHDOpportunitySource s1 group by s1.strSource
-		) as f where cnt > 1
-	)
+	IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE' AND TABLE_NAME='tblHDOpportunitySource')
+	begin
+		update tblHDOpportunitySource set tblHDOpportunitySource.strSource = tblHDOpportunitySource.strSource+'_'+convert(nvarchar(20),tblHDOpportunitySource.intOpportunitySourceId) where tblHDOpportunitySource.strSource in (
+			select strSource from (
+				select s1.strSource, cnt = count(s1.strSource) from tblHDOpportunitySource s1 group by s1.strSource
+			) as f where cnt > 1
+		)
+	end
 
 GO
 	PRINT N'End fixing HD Opportunity Source existing data.'
