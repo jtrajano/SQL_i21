616929bf7bb227002a614fefc1b9f164181b954e
Joy Balangatan <joy.balangatan@irely.com>
2016-06-21 13:08:32 +0800

    IC-2338 Added blank prints for No of Pallet, Qty Per Pallet, and Physical Count in Print Count Sheets

--- i21Database/dbo/Views/vyuICGetCountSheet.sql
+++ i21Database/dbo/Views/vyuICGetCountSheet.sql
@@ -11,6 +11,8 @@ SELECT Header.intLocationId
 	, Header.ysnCountByPallets
 	, Header.ysnIncludeOnHand
 	, Header.ysnIncludeZeroOnHand
-	
+	, dblPalletsBlank = null
+	, dblQtyPerPalletBlank = null
+	, dblPhysicalCountBlank = null
 FROM vyuICGetInventoryCountDetail Detail
 	LEFT JOIN vyuICGetInventoryCount Header ON Header.intInventoryCountId = Detail.intInventoryCountId
