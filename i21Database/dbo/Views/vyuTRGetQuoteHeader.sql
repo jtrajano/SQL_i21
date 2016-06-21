afa73fa10c14da6a2d0d23c52c697c99f072c62a
Lawrence Zabala <lawrence.zabala@irely.com>
2016-06-21 16:15:07 +0800

    TR-521 TR-519

--- i21Database/dbo/Views/vyuTRGetQuoteHeader.sql
+++ i21Database/dbo/Views/vyuTRGetQuoteHeader.sql
@@ -20,6 +20,7 @@ SELECT QuoteHeader.intQuoteHeaderId
 	, QuoteHeader.strQuoteComments
 	, QuoteHeader.strCustomerComments
 	, ysnHasEmailSetup = CASE WHEN (SELECT COUNT(*) FROM vyuARCustomerContacts CC WHERE CC.intCustomerEntityId = Customer.intEntityCustomerId AND ISNULL(CC.strEmail, '') <> '' AND CC.strEmailDistributionOption LIKE '%' + 'Transport Quote' + '%') > 0 THEN CONVERT(BIT, 1) ELSE CONVERT(BIT, 0) END
+	, dblQuoteTotal = CONVERT(DECIMAL, 0.000000) 
 FROM tblTRQuoteHeader QuoteHeader
 LEFT JOIN vyuARCustomer Customer ON Customer.intEntityCustomerId = QuoteHeader.intEntityCustomerId
 LEFT JOIN vyuEMSalesperson SalesPerson ON SalesPerson.intEntitySalespersonId = Customer.intSalespersonId
