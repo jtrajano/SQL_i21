1e28775c67fee82c8c8f8b5a5c529079f741f11d
Dan Michael Jalimao <dan.jalimao@irely.com>
2016-06-22 15:11:29 +0800

    FCOM-1489

---
+++ i21Database/dbo/Tables/tblSMTaxGroupCodeCategoryExemption.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[tblSMTaxGroupCodeCategoryExemption]
+(
+	[intTaxGroupCodeCategoryExemptionId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [intTaxGroupCodeId] INT NOT NULL,  
+	[intCategoryId] INT NOT NULL, 
+    [intConcurrencyId] INT NOT NULL DEFAULT 1, 
+    CONSTRAINT [FK_tblSMTaxGroupCodeCategoryExemption_tblSMTaxGroup] FOREIGN KEY ([intTaxGroupCodeId]) REFERENCES [tblSMTaxGroupCode]([intTaxGroupCodeId]) ON DELETE CASCADE, 
+    CONSTRAINT [FK_tblSMTaxGroupCodeCategoryExemption_tblICCategory] FOREIGN KEY ([intCategoryId]) REFERENCES [tblICCategory]([intCategoryId])
+)
