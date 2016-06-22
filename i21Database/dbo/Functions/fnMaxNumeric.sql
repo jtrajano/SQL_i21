e3d5d9d9b4e5bbb02a2a2f565adbe9922cc8a0d7
Wendell Wayne Estrada <wendell.estrada@irely.com>
2016-06-22 10:33:06 +0800

    IC-1920 Report -> Bin Vizualization

---
+++ i21Database/dbo/Functions/fnMaxNumeric.sql
@@ -0,0 +1,12 @@
+﻿CREATE FUNCTION [dbo].[fnMaxNumeric]
+(
+	@dblValue1 NUMERIC,
+	@dblValue2 NUMERIC
+)
+RETURNS NUMERIC
+AS
+BEGIN
+	IF @dblValue1 > @dblValue2
+		RETURN @dblValue1
+	RETURN @dblValue2
+END
--- i21Database/dbo/Views/vyuICGetStorageBins.sql
+++ i21Database/dbo/Views/vyuICGetStorageBins.sql
@@ -1,18 +1,17 @@
 ﻿CREATE VIEW [dbo].[vyuICGetStorageBins]
-AS 
-SELECT su.intStorageLocationId, sl.intCompanyLocationId,
-	cl.strLocationName strLocation, stl.strName strStorageLocation,
-	SUM(CAST((ISNULL(stl.dblPackFactor, 0) * ISNULL(stl.dblEffectiveDepth, 0) * ISNULL(stl.dblUnitPerFoot, 0)) AS NUMERIC(18,6))) dblCapacity,
-	ABS(SUM(CAST(su.dblOnHand + su.dblUnitStorage AS NUMERIC(18,6)))) dblStock, 
-	ABS(SUM(CAST(CASE WHEN ISNULL(stl.dblEffectiveDepth, 0) = 0 THEN 0 
-	ELSE (ISNULL(stl.dblPackFactor, 1) * ISNULL(stl.dblEffectiveDepth, 1)
-		* ISNULL(stl.dblUnitPerFoot, 1)) - (su.dblOnHand + su.dblUnitStorage) End AS NUMERIC(18,6)))) dblAvailable
-FROM tblICItemStockUOM su
-	INNER JOIN tblICItem i ON i.intItemId = su.intItemId
-	INNER JOIN tblICItemLocation il ON il.intItemLocationId = su.intItemLocationId
-	INNER JOIN tblSMCompanyLocation cl ON cl.intCompanyLocationId = il.intLocationId
-	INNER JOIN tblSMCompanyLocationSubLocation sl ON sl.intCompanyLocationSubLocationId = su.intSubLocationId
-	INNER JOIN tblICStorageLocation stl ON stl.intStorageLocationId = su.intStorageLocationId
-	INNER JOIN tblICItemUOM iu ON iu.intItemUOMId = su.intItemUOMId
-	INNER JOIN tblICUnitMeasure u ON u.intUnitMeasureId = iu.intUnitMeasureId
-GROUP BY su.intStorageLocationId, sl.intCompanyLocationId, cl.strLocationName, stl.strName
+AS
+SELECT companyLocation.intCompanyLocationId, storageLocation.intStorageLocationId
+	, companyLocation.strLocationName strLocation, storageLocation.strName strStorageLocation
+	, storageLocation.dblEffectiveDepth, storageLocation.dblPackFactor, storageLocation.dblUnitPerFoot
+	, CAST(dbo.fnMaxNumeric(summary.dblStock, 0) AS NUMERIC(16, 8)) dblStock
+	, CAST(storageLocation.dblEffectiveDepth * storageLocation.dblPackFactor * storageLocation.dblUnitPerFoot AS NUMERIC(16, 8)) dblCapacity
+	, CAST(dbo.fnMaxNumeric((storageLocation.dblEffectiveDepth * storageLocation.dblPackFactor * storageLocation.dblUnitPerFoot) - dbo.fnMaxNumeric(summary.dblStock, 0), 0) AS NUMERIC(16, 8)) dblAvailable
+FROM tblICStorageLocation storageLocation
+	INNER JOIN (
+		SELECT storageLocation.intStorageLocationId,
+			SUM(stockUOM.dblOnHand + stockUOM.dblUnitStorage) dblStock
+		FROM tblICItemStockUOM stockUOM
+			INNER JOIN tblICStorageLocation storageLocation ON storageLocation.intStorageLocationId = stockUOM.intStorageLocationId
+		GROUP BY storageLocation.intStorageLocationId
+	) summary ON summary.intStorageLocationId = storageLocation.intStorageLocationId
+	INNER JOIN tblSMCompanyLocation companyLocation ON companyLocation.intCompanyLocationId = storageLocation.intLocationId
