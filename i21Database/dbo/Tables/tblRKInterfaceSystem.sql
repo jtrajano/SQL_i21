702d69fc94f18b01c8db164759792d84d0252c81
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2016-06-22 12:34:49 +0530

    RM-191

---
+++ i21Database/dbo/Tables/tblRKInterfaceSystem.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[tblRKInterfaceSystem]
+(
+	[intInterfaceSystemId] INT IDENTITY(1,1) NOT NULL,
+	[intConcurrencyId] INT NOT NULL, 
+	[strInterfaceSystem] NVARCHAR(100) COLLATE Latin1_General_CI_AS NOT NULL,
+	[strInterfaceSystemURL] NVARCHAR(max) COLLATE Latin1_General_CI_AS NOT NULL	
+	CONSTRAINT [PK_tblRKInterfaceSystem_intInterfaceSystemId] PRIMARY KEY ([intInterfaceSystemId])
+)
