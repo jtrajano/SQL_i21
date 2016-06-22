4b2dd6667b8c028358240a3480717da350a15e7a
jeffrey trajano <jeffrey.trajano@irely.com>
2016-06-23 03:13:25 +0800

    GL-2729 Rate does not update to the latest exchange rate on Duplicate GJ

---
+++ i21Database/dbo/Functions/fnGLGetExchangeRate.sql
@@ -0,0 +1,18 @@
+﻿CREATE FUNCTION [dbo].[fnGLGetExchangeRate]
+(
+	@intFromCurrencyId INT,
+	@intToCurrencyId INT,
+	@intCurrencyExchangeRateTypeId INT,
+	@dtmDate DATETIME
+)
+RETURNS NUMERIC(18,6)
+AS
+BEGIN
+	DECLARE @dblRate NUMERIC(18,6)
+	SELECT TOP 1 @dblRate = dblRate FROM vyuGLExchangeRate
+	WHERE intFromCurrencyId = @intFromCurrencyId AND intToCurrencyId = @intToCurrencyId
+	AND intCurrencyExchangeRateTypeId = @intCurrencyExchangeRateTypeId
+	AND dtmValidFromDate<=@dtmDate
+	ORDER BY dtmValidFromDate DESC
+	RETURN ISNULL(@dblRate,0)
+END
