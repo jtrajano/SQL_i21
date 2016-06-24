09f36797dc8f201b9adfad85e78f734eda5b1997
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2016-06-24 18:33:35 +0530

    RM-119

---
+++ i21Database/dbo/Stored Procedures/uspRKGetAllElectronicPricingURL.sql
@@ -0,0 +1,46 @@
+﻿CREATE PROCEDURE [dbo].uspRKGetAllElectronicPricingURL 
+	 @FutureMarketId INT
+	,@intUserId INT
+AS
+BEGIN TRY
+	SET NOCOUNT ON
+
+	DECLARE @ErrMsg NVARCHAR(MAX)
+	DECLARE @strUserName NVARCHAR(100)
+	DECLARE @strPassword NVARCHAR(100)
+	DECLARE @IntinterfaceSystem INT
+	DECLARE @StrQuoteProvider NVARCHAR(100)
+	DECLARE @StrTradedMonthSymbol NVARCHAR(1000)
+	DECLARE @MarketExchangeCode NVARCHAR(10)
+	DECLARE @Commoditycode NVARCHAR(10)
+	DECLARE @URL NVARCHAR(1000)
+	DECLARE @SymbolPrefix NVARCHAR(5)
+	DECLARE @strOpen nvarchar(50)
+	DECLARE @strHigh nvarchar(50)
+	DECLARE @strLow nvarchar(50)
+	DECLARE @strLastSettle nvarchar(50)
+	DECLARE @strLastElement nvarchar(50)
+	
+	SELECT @Commoditycode = strFutSymbol
+	FROM tblRKFutureMarket
+	WHERE intFutureMarketId = @FutureMarketId		
+		
+	SELECT TOP 1 @URL= s.strInterfaceSystemURL,@SymbolPrefix=strSymbolPrefix, @strOpen=strOpen,@strHigh=strHigh,@strLow=strLow,@strLastSettle=strLastSettle,@strLastElement=strLastElement FROM tblRKCompanyPreference c
+	JOIN tblRKInterfaceSystem s on c.intInterfaceSystemId=s.intInterfaceSystemId 
+
+	if isnull(@URL,'') <> ''
+	BEGIN
+		SELECT  @URL+@SymbolPrefix+@Commoditycode + strSymbol+RIGHT(intYear,1) as URL,strFutureMonth strFutureMonthYearWOSymbol,intFutureMonthId as intFutureMonthId,
+		@strOpen as strOpen,@strHigh as strHigh,@strLow as strLow,@strLastSettle as strLastSettle,@strLastElement as strLastElement
+		FROM tblRKFuturesMonth where intFutureMarketId=@FutureMarketId and ysnExpired = 0 
+	END
+	
+	END TRY
+	
+BEGIN CATCH
+
+	SET @ErrMsg = ERROR_MESSAGE()
+	SET @ErrMsg = 'uspRKGetAllElectronicPricingURL: ' + @ErrMsg
+	RAISERROR (@ErrMsg,16,1,'WITH NOWAIT')
+	
+END CATCH
---
+++ i21Database/dbo/Tables/tblRKInterfaceSystem.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[tblRKInterfaceSystem]
+(
+	[intInterfaceSystemId] INT IDENTITY(1,1) NOT NULL,
+	[intConcurrencyId] INT NOT NULL, 
+	[strInterfaceSystem] NVARCHAR(100) COLLATE Latin1_General_CI_AS NOT NULL,
+	[strInterfaceSystemURL] NVARCHAR(max) COLLATE Latin1_General_CI_AS NOT NULL,	
+	[strSymbolPrefix] NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
+	[strOpen] NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
+	[strHigh] NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
+	[strLow] NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
+	[strLastSettle] NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
+	[strLastElement] NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
+	CONSTRAINT [PK_tblRKInterfaceSystem_intInterfaceSystemId] PRIMARY KEY ([intInterfaceSystemId])
+)
