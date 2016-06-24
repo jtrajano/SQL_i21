8f2ff67f84354b0ca4b41a646221f2b0f3f3cf33
Dan Michael Jalimao <dan.jalimao@irely.com>
2016-06-24 17:32:13 +0800

    FCOM-1824

--- i21Database/dbo/Views/vyuSMEntityRole.sql
+++ i21Database/dbo/Views/vyuSMEntityRole.sql
@@ -11,6 +11,11 @@ SELECT CAST (ROW_NUMBER() OVER (ORDER BY intEntityId DESC) AS INT)	AS	intEntityR
 		SELECT Entity.intEntityId AS intEntityId, Entity.strName AS strName,  UserRole.intUserRoleID AS intUserRoleId FROM tblSMUserSecurity UserSecurity
 		INNER JOIN tblEMEntity Entity ON UserSecurity.intEntityUserSecurityId = Entity.intEntityId
 		INNER JOIN tblSMUserRole UserRole ON UserSecurity.intUserRoleID = UserRole.intUserRoleID
+		UNION ALL
+		SELECT Entity.intEntityId AS intEntityId, Entity.strName AS strName, SubRole.intSubRoleId AS intUserRoleId FROM tblSMUserRoleSubRole SubRole
+		INNER JOIN tblSMUserSecurity UserSecurity ON SubRole.intUserRoleId = UserSecurity.intUserRoleID
+		INNER JOIN tblSMUserRole UserRole ON SubRole.intSubRoleId = UserRole.intUserRoleID
+		INNER JOIN tblEMEntity Entity ON Entity.intEntityId = UserSecurity.intEntityUserSecurityId
 	) EntityRoleSub
 ) EntityRoleMain
 GO
