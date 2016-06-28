62e4432922a87abd6109941fc621cbd87bee5811
Jayson Ramos <jayson.ramos@iRely.com>
2016-06-28 16:59:40 +0800

    HD-1456

--- i21Database/dbo/Tables/tblHDProject.sql
+++ i21Database/dbo/Tables/tblHDProject.sql
@@ -38,6 +38,7 @@
 	[ysnGenerateTicket] [bit] null,
 	[strType] [nvarchar](50) COLLATE Latin1_General_CI_AS NULL,
 	[strLinesOfBusinessId] [nvarchar](255) COLLATE Latin1_General_CI_AS NULL,
+	[strRFPRFILink] [nvarchar](255) COLLATE Latin1_General_CI_AS NULL,
 	[intConcurrencyId] [int] NOT NULL DEFAULT 1,
 
 	CONSTRAINT [PK_tblHDProject] PRIMARY KEY CLUSTERED ([intProjectId] ASC),
