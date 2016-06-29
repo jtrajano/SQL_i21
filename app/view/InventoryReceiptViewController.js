9f6fbf7c82d287917d4f29eef3b32047a055fd92
Lawrence Zabala <lawrence.zabala@irely.com>
2016-06-29 18:34:09 +0800

    IC-1938

--- app/view/InventoryReceiptViewController.js
+++ app/view/InventoryReceiptViewController.js
@@ -3949,14 +3949,6 @@ Ext.define('Inventory.view.InventoryReceiptViewController', {
                     From: strLotNo,
                     To: '',
                     Operator: ''
-                },
-                {
-                    Name: 'intLotId',
-                    Type: 'string',
-                    Condition: 'EQUAL TO',
-                    From: intLotId,
-                    To: '',
-                    Operator: ''
                 }],
             directPrint: true
         });
