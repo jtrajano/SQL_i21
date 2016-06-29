041bd642e978ba6487e0f592cc625fd2ac95b6e1
Wendell Wayne Estrada <wendell.estrada@irely.com>
2016-06-29 14:53:36 +0800

    IC-1474 Storage Locations -> Measurment Tab Combo Box Console Error

---
+++ server/iRely.Inventory.BusinessLayer/Interface/IMeasurementBl.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iRely.Inventory.Model;
+using iRely.Common;
+
+namespace iRely.Inventory.BusinessLayer
+{
+    public interface IMeasurementBl : IBusinessLayer<tblICMeasurement>
+    {
+    }
+}
---
+++ server/iRely.Inventory.WebAPI/Controllers/MeasurementController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using iRely.Inventory.Model;
+using iRely.Inventory.BusinessLayer;
+using iRely.Common;
+
+namespace iRely.Inventory.WebApi
+{
+    public class MeasurementController : BaseApiController<tblICMeasurement>
+    {
+        public MeasurementController(IMeasurementBl bl)
+            : base(bl)
+        {
+        }
+    }
+}
