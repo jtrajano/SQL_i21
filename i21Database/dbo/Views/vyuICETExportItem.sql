9a4cd9f9a9dee350279b6625838c765ee05dc526
Wendell Wayne Estrada <wendell.estrada@irely.com>
2016-06-29 11:17:36 +0800

    IC-1331 Energy Trac View for Inventory Items

--- i21Database/dbo/Views/vyuICETExportItem.sql
+++ i21Database/dbo/Views/vyuICETExportItem.sql
@@ -1,21 +1,27 @@
 ﻿CREATE VIEW [dbo].[vyuICETExportItem]
 AS
-SELECT Item.intItemId, ItemLocation.intItemLocationId,
-	Item.strItemNo, Item.strDescription, Item.strType strItemType, Item.strLotTracking,
-	CASE WHEN Item.strType IN ('Non-Inventory', 'Other Charge', 'Service', 'Software') THEN 'Y' ELSE 'N' END strCounted,
-	Measure.strUnitMeasure, ItemPricing.dblSalePrice, ItemAccount.intAccountId, Item.strStatus, Account.intAccountGroupId
-FROM tblICItem Item
-	LEFT OUTER JOIN tblICItemLocation ItemLocation ON ItemLocation.intItemId = Item.intItemId
-	LEFT OUTER JOIN tblSMCompanyLocation Location ON ItemLocation.intLocationId = Location.intCompanyLocationId
-	LEFT OUTER JOIN tblSMCompanyLocationSubLocation sub ON sub.intCompanyLocationSubLocationId = ItemLocation.intSubLocationId
-	LEFT OUTER JOIN tblICStorageLocation sloc ON sloc.intStorageLocationId = ItemLocation.intStorageLocationId
-	LEFT OUTER JOIN tblICItemUOM UOM ON UOM.intItemId = Item.intItemId AND UOM.intItemUOMId = ItemLocation.intIssueUOMId
-		AND UOM.ysnStockUnit = 1
-	LEFT OUTER JOIN tblICUnitMeasure Measure ON Measure.intUnitMeasureId = UOM.intUnitMeasureId
-	LEFT OUTER JOIN tblICItemPricing ItemPricing ON ItemPricing.intItemId = Item.intItemId
-		AND ItemPricing.intItemLocationId = ItemLocation.intItemLocationId
-	LEFT OUTER JOIN tblICItemAccount ItemAccount ON ItemAccount.intItemId = Item.intItemId
-	LEFT OUTER JOIN tblGLAccount Account ON Account.intAccountId = ItemAccount.intAccountId
-	LEFT OUTER JOIN tblETExportFilterItem ExportItem ON Item.intItemId = ExportItem.intItemId
-	LEFT OUTER JOIN tblETExportFilterCategory ExportCategory ON Item.intCategoryId = ExportCategory.intCategoryId
-WHERE Item.ysnUsedForEnergyTracExport = 1 AND Item.intItemId = ExportItem.intItemId OR Item.intCategoryId = ExportCategory.intCategoryId
+SELECT
+	CAST(location.strLocationNumber AS VARCHAR(3)) inloc,
+	CAST(item.strItemNo AS VARCHAR(15)) initm,
+	CAST(item.strDescription AS VARCHAR(30)) indesc,
+	NULL intax,
+	CAST(ISNULL(itemPricing.dblSalePrice, 0) AS NUMERIC(18, 6)) salepr,
+	CAST(REPLACE(salesAccount.strAccountId, '-', '') AS VARCHAR(13)) salact,
+	unitMeasure.strUnitMeasure sunmsr,
+	1 msgno, NULL interm,
+	CASE WHEN item.strType IN ('Non-Inventory', 'Other Charge', 'Service', 'Software') THEN 'N' ELSE 'Y' END Counted
+FROM tblICItem item
+	INNER JOIN tblICItemLocation itemLocation ON itemLocation.intItemId = item.intItemId
+	INNER JOIN tblSMCompanyLocation location ON location.intCompanyLocationId = itemLocation.intLocationId
+	LEFT JOIN tblICItemPricing itemPricing ON itemPricing.intItemId = item.intItemId
+		AND itemPricing.intItemLocationId = itemLocation.intItemLocationId
+	LEFT JOIN tblICItemUOM itemUOM ON itemUOM.intItemId = item.intItemId
+		AND itemUOM.ysnStockUnit = 1
+	LEFT JOIN tblICUnitMeasure unitMeasure ON unitMeasure.intUnitMeasureId = itemUOM.intUnitMeasureId
+	LEFT JOIN (
+		SELECT itemAccount.intItemId, account.strAccountId
+		FROM tblICItemAccount itemAccount
+			INNER JOIN tblGLAccount account ON account.intAccountId = itemAccount.intAccountId
+			INNER JOIN tblGLAccountCategory accountCategory ON accountCategory.intAccountCategoryId = itemAccount.intAccountCategoryId
+		WHERE accountCategory.strAccountCategory = 'Sales Account'
+	) salesAccount ON salesAccount.intItemId = item.intItemId
