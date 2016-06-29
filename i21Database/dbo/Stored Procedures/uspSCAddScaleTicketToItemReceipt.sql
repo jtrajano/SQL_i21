09e6416694dff34c6e7cd60dc92abfe83f872500
Emerson Versoza <emerson.versoza@irely.com>
2016-06-29 16:57:50 +0800

    SC-16

--- i21Database/dbo/Stored Procedures/uspSCAddScaleTicketToItemReceipt.sql
+++ i21Database/dbo/Stored Procedures/uspSCAddScaleTicketToItemReceipt.sql
@@ -1225,33 +1225,35 @@ BEGIN
   
 	SET @InventoryReceiptId = @ReceiptId
 
+	UPDATE tblGRStorageHistory 
+	SET [intInventoryReceiptId] = @InventoryReceiptId
+	WHERE [strType] = 'From Scale' AND intCustomerStorageId = (SELECT MAX(intCustomerStorageId) FROM tblGRCustomerStorage) 
+	AND ISNULL(intInventoryReceiptId,0) = 0
 
-	SELECT TOP 1 ReceiptId = intInventoryReceiptId FROM  #tmpAddItemReceiptResult	
+	--DECLARE @intInventoryReceiptItemId	INT = NULL,
+	--		@dblQty						NUMERIC(18,6) = 0
 
-	DECLARE @intInventoryReceiptItemId	INT = NULL,
-			@dblQty						NUMERIC(18,6) = 0
+	--SELECT	@intInventoryReceiptItemId = MIN(intInventoryReceiptItemId) 
+	--FROM	tblICInventoryReceiptItem
+	--WHERE	intInventoryReceiptId = @InventoryReceiptId
 
-	SELECT	@intInventoryReceiptItemId = MIN(intInventoryReceiptItemId) 
-	FROM	tblICInventoryReceiptItem
-	WHERE	intInventoryReceiptId = @InventoryReceiptId
+	--WHILE ISNULL(@intInventoryReceiptItemId,0) > 0
+	--BEGIN
+	--	SELECT	@dblQty						=	dblOpenReceive,
+	--			@intContractDetailId		=	intLineNo
+	--	FROM	tblICInventoryReceiptItem 
+	--	WHERE	intInventoryReceiptItemId	=	 @intInventoryReceiptItemId
 
-	WHILE ISNULL(@intInventoryReceiptItemId,0) > 0
-	BEGIN
-		SELECT	@dblQty						=	dblOpenReceive,
-				@intContractDetailId		=	intLineNo
-		FROM	tblICInventoryReceiptItem 
-		WHERE	intInventoryReceiptItemId	=	 @intInventoryReceiptItemId
+	--	IF @intContractDetailId > 0
+	--	BEGIN
+	--		EXEC uspCTUpdateScheduleQuantityUsingUOM @intContractDetailId, @dblQty, @intUserId, @intInventoryReceiptItemId, 'Scale', @intTicketItemUOMId
+	--	END
 
-		IF @intContractDetailId > 0
-		BEGIN
-			EXEC uspCTUpdateScheduleQuantityUsingUOM @intContractDetailId, @dblQty, @intUserId, @intInventoryReceiptItemId, 'Scale', @intTicketItemUOMId
-		END
-
-		SELECT	@intInventoryReceiptItemId = MIN(intInventoryReceiptItemId) 
-		FROM	tblICInventoryReceiptItem
-		WHERE	intInventoryReceiptId = @InventoryReceiptId	AND
-				intInventoryReceiptItemId > @intInventoryReceiptItemId
-	END
+	--	SELECT	@intInventoryReceiptItemId = MIN(intInventoryReceiptItemId) 
+	--	FROM	tblICInventoryReceiptItem
+	--	WHERE	intInventoryReceiptId = @InventoryReceiptId	AND
+	--			intInventoryReceiptItemId > @intInventoryReceiptItemId
+	--END
 
 	-- Post the Inventory Receipt that was created
 	--SELECT	@strTransactionId = strReceiptNumber 
