e714489807d9179cf8644ec2f12a9f90227b1151
Mark Paragas <mark.paragas@irely.com>
2016-07-04 12:05:51 +0800

    MFT-179

--- i21Database/Scripts/Post-Deployment/PostDeployment.sql
+++ i21Database/Scripts/Post-Deployment/PostDeployment.sql
@@ -339,6 +339,7 @@ print 'BEGIN POST DEPLOYMENT'
 :r .\TF\DefaultData\_TerminalControlNumber.sql
 :r .\TF\DefaultData\_ConfigurationType.sql
 :r .\TF\DefaultData\_ConfigurationTemplate.sql
+:r .\TF\DefaultData\_OriginDestinationState.sql
 :r .\TF\DefaultData\_TaxCategory.sql
 
 --Transports
