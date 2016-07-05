035ef4ddf4e390cb8721d629c862633aa7e9cfad
Satya <ssahoo@BLRSATYAWIN7.irely.net>
2016-07-05 19:35:19 +0530

    CT-723

--- i21Database/dbo/Functions/fnCTGetCleanUncleanWeight.sql
+++ i21Database/dbo/Functions/fnCTGetCleanUncleanWeight.sql
@@ -10,22 +10,32 @@ RETURNS @returntable	TABLE
 AS
 BEGIN
 
-	DECLARE @intCleanCostUOMId	INT
+	DECLARE @intCleanCostUOMId	INT,
+			@dblUncleanWeight	NUMERIC(38,20), 
+			@dblCleanWeight		NUMERIC(38,20)
 
 	SELECT	@intCleanCostUOMId	= intCleanCostUOMId 
 	FROM	tblCTCompanyPreference
 	
+	SELECT	@dblUncleanWeight = SUM(dbo.fnCTConvertQuantityToTargetItemUOM(RI.intItemId,UM.intUnitMeasureId, 3, RI.dblNet))
+			
+	FROM	tblICInventoryReceiptItem		RI  
+	JOIN	tblICItemUOM					UM ON UM.intItemUOMId				=	RI.intWeightUOMId  
+	WHERE	RI.intInventoryReceiptId = @intInventoryReceiptId
+
+	SELECT	@dblCleanWeight = SUM(dbo.fnCTConvertQuantityToTargetItemUOM(RI.intItemId,UM.intUnitMeasureId, 3, ISNULL(IL.dblGrossWeight,0) - ISNULL(dblTareWeight,0)))  
+	
+	FROM	tblICInventoryReceiptItem		RI  
+	JOIN	tblICInventoryReceiptItemLot	IL ON IL.intInventoryReceiptItemId	=	RI.intInventoryReceiptItemId
+	JOIN	tblICItemUOM					UM ON UM.intItemUOMId				=	RI.intWeightUOMId  
+	WHERE	RI.intInventoryReceiptId = @intInventoryReceiptId AND ISNULL(IL.strCondition,'') <> 'Damaged'
+
 	INSERT	@returntable
 	(
 			dblUncleanWeight, 
 			dblCleanWeight
 	)
-	SELECT	SUM(dbo.fnCTConvertQuantityToTargetItemUOM(RI.intItemId,UM.intUnitMeasureId, @intCleanCostUOMId, RI.dblNet)) dblUncleanWeight,  
-			SUM(dbo.fnCTConvertQuantityToTargetItemUOM(RI.intItemId,UM.intUnitMeasureId, @intCleanCostUOMId, ISNULL(IL.dblGrossWeight,0) - ISNULL(dblTareWeight,0))) dblCleanWeight  
-	FROM	tblICInventoryReceiptItem		RI  
-	JOIN	tblICInventoryReceiptItemLot	IL ON IL.intInventoryReceiptItemId	=	RI.intInventoryReceiptItemId
-	JOIN	tblICItemUOM					UM ON UM.intItemUOMId				=	RI.intWeightUOMId  
-	WHERE	RI.intInventoryReceiptId = @intInventoryReceiptId ;
-		
+	SELECT 	@dblUncleanWeight, @dblCleanWeight
+
 	RETURN;
 END
