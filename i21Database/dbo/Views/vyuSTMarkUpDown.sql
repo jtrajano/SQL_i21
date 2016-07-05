003d651d82ea994e2d8e93a4f49c49cebb58153a
Liz Dorado <liz.dorado@irely.com>
2016-07-05 15:56:09 +0800

    ST-310

---
+++ i21Database/dbo/Views/vyuSTMarkUpDown.sql
@@ -0,0 +1,12 @@
+﻿CREATE VIEW [dbo].[vyuSTMarkUpDown]
+AS
+
+select A.intMarkUpDownId
+, B.intStoreNo
+, A.dtmMarkUpDownDate
+, A.intShiftNo
+, A.strType
+, A.strAdjustmentType
+
+from tblSTMarkUpDown A inner join tblSTStore B
+on A.intStoreId = B.intStoreId
---
+++ i21Database/dbo/Views/vyuSTMarkUpDownDetail.sql
@@ -0,0 +1,26 @@
+﻿CREATE VIEW [dbo].[vyuSTMarkUpDownDetail]
+AS
+
+select C.intMarkUpDownDetailId
+, A.intMarkUpDownId
+, D.strUpcCode
+, E.strCategoryCode
+, C.strMarkUpOrDown
+, C.intQty
+, C.dblRetailPerUnit
+, C.dblTotalRetailAmount
+, C.dblTotalCostAmount
+, C.strReason
+
+from tblSTMarkUpDown A inner join tblSTStore B
+on A.intStoreId = B.intStoreId
+
+inner join tblSTMarkUpDownDetail C
+on A.intMarkUpDownId = C.intMarkUpDownId
+
+inner join tblICItemUOM D
+on D.intItemUOMId = C.intItemUOMId
+
+left join tblICCategory E
+on E.intCategoryId =  C.intCategoryId
+
