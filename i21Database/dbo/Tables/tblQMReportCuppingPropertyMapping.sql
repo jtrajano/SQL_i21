942ed41bdcf53dfb123d3fab476ccd0911e0ca4d
Vinoth Kumar T <vinoth.t@irely.com>
2016-07-07 21:36:45 +0530

    QM-

--- i21Database/dbo/Tables/tblQMReportCuppingPropertyMapping.sql
+++ i21Database/dbo/Tables/tblQMReportCuppingPropertyMapping.sql
@@ -1,4 +1,5 @@
 ﻿CREATE TABLE dbo.tblQMReportCuppingPropertyMapping (
+	intReportCuppingPropertyMappingId INT NOT NULL IDENTITY(1, 1) CONSTRAINT PK_tblQMReportCuppingPropertyMapping_intReportCuppingPropertyMapping PRIMARY KEY,
 	strPropertyName NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL
 	,strActualPropertyName NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL
 	)
