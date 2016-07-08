6ae2db485274c4dcb85012967be53a5ebd41d434
Liz Dorado <liz.dorado@irely.com>
2016-07-08 16:00:24 +0800

    ST-310

--- i21Database/dbo/Views/vyuSTMarkUpDownItems.sql
+++ i21Database/dbo/Views/vyuSTMarkUpDownItems.sql
@@ -1,11 +1,20 @@
 ﻿CREATE VIEW [dbo].[vyuSTMarkUpDownItems]
 AS
 
-select strItemNo
-, strDescription
-, strUpcCode
-, strLongUPCCode
-, intItemId
-, intLocationId
-, dblSalePrice 
-from vyuICGetItemPricing 
+select A.strItemNo
+,A.strDescription
+,A.strUpcCode
+,A.strLongUPCCode
+,A.intItemId
+,A.intLocationId
+,B.intStoreId
+,B.intStoreNo
+,A.dblSalePrice 
+from vyuICGetItemPricing A
+inner join tblSTStore  B
+on A.intLocationId  = B.intCompanyLocationId
+
+inner join vyuICGetItemLocation C
+on A.intItemUOMId = C.intIssueUOMId
+
+where C.intItemLocationId =  A.intItemLocationId
