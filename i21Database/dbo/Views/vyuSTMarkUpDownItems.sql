66307f05ede3f5c74c9e7ddc7e468c2c14c928e9
Liz Dorado <liz.dorado@irely.com>
2016-07-08 10:05:29 +0800

    ST-310

---
+++ i21Database/dbo/Views/vyuSTMarkUpDownItems.sql
@@ -0,0 +1,11 @@
+﻿CREATE VIEW [dbo].[vyuSTMarkUpDownItems]
+AS
+
+select strItemNo
+, strDescription
+, strUpcCode
+, strLongUPCCode
+, intItemId
+, intLocationId
+, dblSalePrice 
+from vyuICGetItemPricing 
