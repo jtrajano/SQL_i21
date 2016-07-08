f7fe449cb196a0c398a8239f61e883dc06569218
Lex Eugene Peregrino <lex.peregrino@irely.com>
2016-07-08 16:46:26 +0800

    AP-2545 - dblNewCost > dblVoucherCost

--- i21Database/dbo/Stored Procedures/uspAPPostBill.sql
+++ i21Database/dbo/Stored Procedures/uspAPPostBill.sql
@@ -189,8 +189,7 @@ SELECT
 		,[dblQty] 							=	B.dblQtyReceived
 		,[dblUOMQty] 						=	itemUOM.dblUnitQty
 		,[intCostUOMId]						=	voucherCostUOM.intItemUOMId 
-		,[dblNewCost] 						=	B.dblCost
-		,[dblCost]							=	ROUND(B.dblTotal / (B.dblQtyReceived),6) --Cost w/ line item discount consideration
+		,[dblVoucherCost] 					=	ROUND(B.dblTotal / (B.dblQtyReceived),6) --Cost with line item discount consideration
 		,[intCurrencyId] 					=	A.intCurrencyId
 		,[dblExchangeRate] 					=	0
 		,[intTransactionId]					=	A.intBillId
