77ac834e7a53c6e5111eb3f49b198f9538ce94b3
Jayson Ramos <jayson.ramos@iRely.com>
2016-07-11 16:21:43 +0800

    HD-1475

--- i21Database/dbo/Tables/tblHDOpportunityCampaign.sql
+++ i21Database/dbo/Tables/tblHDOpportunityCampaign.sql
@@ -16,6 +16,7 @@
 	[ysnHold] [bit] null,
 	[ysnActive] [bit] null,
 	[intEntityId] [int] NOT NULL,
+	[strRetrospective] [nvarchar](255) COLLATE Latin1_General_CI_AS NULL,
 	[intConcurrencyId] [int] NOT NULL DEFAULT 1,
 	CONSTRAINT [PK_tblHDOpportunityCampaign] PRIMARY KEY CLUSTERED ([intOpportunityCampaignId] ASC),
 	CONSTRAINT [UQ_tblHDOpportunityCampaign] UNIQUE ([strCampaignName]),
