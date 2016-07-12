c0d1d575a0ca63af25b2cbf34926bf79f371fca2
Liz Dorado <liz.dorado@irely.com>
2016-07-12 15:36:29 +0800

    ST-310

--- i21Database/dbo/Views/vyuSTMarkUpDownDetail.sql
+++ i21Database/dbo/Views/vyuSTMarkUpDownDetail.sql
@@ -3,26 +3,25 @@ AS
 
 select C.intMarkUpDownDetailId
 , A.intMarkUpDownId
-, D.strItemNo
-, D.intItemId
-, E.strCategoryCode
-, C.strMarkUpOrDown
+, strItemNo = (CASE WHEN C.intCategoryId <> 0 THEN '' ELSE D.strItemNo END )
+, intItemId = (CASE WHEN C.intCategoryId <>  0 THEN 0 ELSE D.intItemId END)
+, strCategoryCode = (CASE WHEN C.intCategoryId = 0 and D.strItemNo is NULL THEN '' ELSE (select strCategoryCode from tblICCategory where intCategoryId =  C.intCategoryId)END )
+,C.strMarkUpOrDown
 , C.intQty
 , C.dblRetailPerUnit
 , C.dblTotalRetailAmount
 , C.dblTotalCostAmount
 , C.strReason
 
-from tblSTMarkUpDown A inner join tblSTStore B
+
+from tblSTMarkUpDown A 
+inner join tblSTStore B
 on A.intStoreId = B.intStoreId
 
 inner join tblSTMarkUpDownDetail C
 on A.intMarkUpDownId = C.intMarkUpDownId
 
-left join vyuSTMarkUpDownItems D
+left join  vyuSTMarkUpDownItems D
 on C.intItemId = D.intItemId
 
-left join tblICCategory E
-on E.intCategoryId =  C.intCategoryId
-
-
+and A.intStoreId =  D.intStoreId
