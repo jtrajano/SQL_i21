236f3c8e9edd7c39cdd9a4f0608bdc6dfcf72a10
Satya <ssahoo@BLRSATYAWIN7.irely.net>
2016-07-13 14:29:21 +0530

    CT-712

--- i21Database/dbo/Stored Procedures/uspCTContractSave.sql
+++ i21Database/dbo/Stored Procedures/uspCTContractSave.sql
@@ -10,7 +10,9 @@ BEGIN TRY
 			@intContractDetailId		INT,
 			@dblCashPrice				NUMERIC(18,6),
 			@intPricingTypeId			INT,
-			@ysnMultiplePriceFixation	BIT
+			@ysnMultiplePriceFixation	BIT,
+			@dblBasis					NUMERIC(18,6),
+			@dblOriginalBasis			NUMERIC(18,6)
 	
 	SELECT @ysnMultiplePriceFixation = ysnMultiplePriceFixation FROM tblCTContractHeader WHERE intContractHeaderId = @intContractHeaderId
 	SELECT @intContractDetailId		=	MIN(intContractDetailId) FROM tblCTContractDetail WHERE intContractHeaderId = @intContractHeaderId
@@ -18,10 +20,14 @@ BEGIN TRY
 	WHILE ISNULL(@intContractDetailId,0) > 0
 	BEGIN
 		SELECT	@intPricingTypeId	=	NULL,
-				@dblCashPrice		=	NULL
-				
+				@dblCashPrice		=	NULL,
+				@dblBasis			=	NULL,
+				@dblOriginalBasis	=	NULL
+
 		SELECT	@intPricingTypeId	=	intPricingTypeId,
-				@dblCashPrice		=	dblCashPrice 
+				@dblCashPrice		=	dblCashPrice,
+				@dblBasis			=	dblBasis,
+				@dblOriginalBasis	=	dblOriginalBasis
 		FROM	tblCTContractDetail 
 		WHERE	intContractDetailId =	@intContractDetailId 
 		
@@ -30,6 +36,11 @@ BEGIN TRY
 			EXEC uspICUpdateInventoryReceiptUnitCost @intContractDetailId,@dblCashPrice
 		END
 		
+		IF @dblOriginalBasis IS NOT NULL AND  @dblBasis <> @dblOriginalBasis
+		BEGIN
+			EXEC uspCTUpdateSequenceBasis @intContractDetailId,@dblBasis
+		END
+
 		EXEC uspLGUpdateLoadItem @intContractDetailId
 
 		SELECT @intContractDetailId = MIN(intContractDetailId) FROM tblCTContractDetail WHERE intContractHeaderId = @intContractHeaderId AND intContractDetailId > @intContractDetailId
---
+++ i21Database/dbo/Stored Procedures/uspCTUpdateSequenceBasis.sql
@@ -0,0 +1,38 @@
+﻿CREATE PROCEDURE [dbo].[uspCTUpdateSequenceBasis]
+
+	@intContractDetailId	INT,
+	@dblNewBasis			NUMERIC(18,6)
+
+AS
+	DECLARE @intPriceFixationId INT
+
+	SELECT @intPriceFixationId = intPriceFixationId FROM tblCTPriceFixation WHERE intContractDetailId = @intContractDetailId
+
+	IF @intPriceFixationId IS NULL RETURN
+
+	UPDATE tblCTContractDetail SET dblOriginalBasis = @dblNewBasis WHERE intContractDetailId = @intContractDetailId
+
+	UPDATE	FD
+	SET		FD.dblBasis		 =	@dblNewBasis, 
+			FD.dblCashPrice	 =	FD.dblCashPrice - FD.dblBasis + @dblNewBasis,
+			FD.dblFinalPrice =	FD.dblFinalPrice - 
+								dbo.fnCTConvertQuantityToTargetCommodityUOM(intFinalPriceUOMId,intPricingUOMId,FD.dblBasis) + 
+								dbo.fnCTConvertQuantityToTargetCommodityUOM(intFinalPriceUOMId,intPricingUOMId,@dblNewBasis)
+	FROM	tblCTPriceFixationDetail	FD
+	JOIN	tblCTPriceFixation			PF	ON	PF.intPriceFixationId = FD.intPriceFixationId
+	WHERE	FD.intPriceFixationId = @intPriceFixationId
+
+	
+	UPDATE PF
+	SET		PF.dblPriceWORollArb =	PF.dblPriceWORollArb - 
+									dbo.fnCTConvertQuantityToTargetCommodityUOM(PF.intFinalPriceUOMId,CU.intCommodityUnitMeasureId,PF.dblOriginalBasis) +
+									dbo.fnCTConvertQuantityToTargetCommodityUOM(PF.intFinalPriceUOMId,CU.intCommodityUnitMeasureId,@dblNewBasis),
+			PF.dblOriginalBasis	 =	@dblNewBasis
+
+	FROM	tblCTPriceFixation			PF
+	JOIN	tblCTContractDetail			CD	ON	PF.intContractDetailId	=	CD.intContractDetailId
+	JOIN	tblCTContractHeader			CH	ON	CH.intContractHeaderId	=	CD.intContractHeaderId
+	JOIN	tblICItemUOM				IU	ON	IU.intItemUOMId			=	CD.intPriceItemUOMId
+	JOIN	tblICCommodityUnitMeasure	CU	ON	CU.intCommodityId		=	CH.intCommodityId
+											AND	CU.intUnitMeasureId		=	IU.intUnitMeasureId
+	WHERE	PF.intPriceFixationId = @intPriceFixationId
