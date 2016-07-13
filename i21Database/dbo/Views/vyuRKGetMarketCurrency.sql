85e905b9cbfc19ec3021f79679a05ff997d352a9
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2016-07-13 15:14:17 +0530

    RM-396

--- i21Database/dbo/Views/vyuRKGetMarketCurrency.sql
+++ i21Database/dbo/Views/vyuRKGetMarketCurrency.sql
@@ -1,4 +1,7 @@
 ﻿CREATE VIEW [dbo].[vyuRKGetMarketCurrency]
 AS  
 SELECT intFutureMarketId,mm.intCurrencyID,mm.strCurrency from tblRKFutureMarket f
-JOIN tblSMCurrency mm on f.intCurrencyId=mm.intCurrencyID
+JOIN tblSMCurrency mm on f.intCurrencyId=mm.intCurrencyID 
+UNION
+SELECT intFutureMarketId,intMainCurrencyId,(Select C1.strCurrency from tblSMCurrency C1 Where C1.intCurrencyID= mm.intMainCurrencyId) from tblRKFutureMarket f
+JOIN tblSMCurrency mm on f.intCurrencyId=mm.intCurrencyID and ysnSubCurrency =1
