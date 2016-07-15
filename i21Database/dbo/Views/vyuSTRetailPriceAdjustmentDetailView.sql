a8278f8ba605a613c0ecbeacf84ac82ea7448546
Liz Dorado <liz.dorado@irely.com>
2016-07-15 16:10:21 +0800

    ST-317

---
+++ i21Database/dbo/Views/vyuSTRetailPriceAdjustmentDetailView.sql
@@ -0,0 +1,44 @@
+CREATE VIEW [dbo].[vyuSTRetailPriceAdjustmentDetailView]
+AS 
+
+SELECT 
+RetailPriceAdjustmentDetail.intRetailPriceAdjustmentId,
+RetailPriceAdjustmentDetail.intRetailPriceAdjustmentDetailId,
+RetailPriceAdjustmentDetail.intCompanyLocationId,
+strLocationName = SMCompanyLocation.strLocationName,
+RetailPriceAdjustmentDetail.strRegion,
+RetailPriceAdjustmentDetail.strDestrict,
+RetailPriceAdjustmentDetail.strState,
+RetailPriceAdjustmentDetail.intVendorId,
+strVendorName =  EMEntity.strName,
+RetailPriceAdjustmentDetail.intCategoryId,
+strCategoryCode  = ICCategory.strCategoryCode,
+RetailPriceAdjustmentDetail.intManufacturerId,
+RetailPriceAdjustmentDetail.intFamilyId,
+strFamily = STSubcategoryFamily.strSubcategoryDesc,
+RetailPriceAdjustmentDetail.intClassId,
+strClass = STSubcategoryClass.strSubcategoryDesc,
+RetailPriceAdjustmentDetail.intItemUOMId,
+strCode  = ICItemUOM.strUpcCode,
+RetailPriceAdjustmentDetail.strUpcDescription,
+RetailPriceAdjustmentDetail.ysnPromo,
+RetailPriceAdjustmentDetail.strPriceMethod,
+RetailPriceAdjustmentDetail.dblFactor,
+RetailPriceAdjustmentDetail.dblPrice,
+RetailPriceAdjustmentDetail.dblLastCost,
+RetailPriceAdjustmentDetail.ysnActive,
+RetailPriceAdjustmentDetail.ysnOneTimeuse,
+RetailPriceAdjustmentDetail.ysnChangeCost,
+RetailPriceAdjustmentDetail.dblCost,
+RetailPriceAdjustmentDetail.dtmSalesStartDate,
+RetailPriceAdjustmentDetail.dtmSalesEndDate,
+RetailPriceAdjustmentDetail.ysnPosted,
+RetailPriceAdjustmentDetail.strPriceType
+
+FROM tblSTRetailPriceAdjustmentDetail RetailPriceAdjustmentDetail
+LEFT JOIN tblICItemUOM ICItemUOM ON RetailPriceAdjustmentDetail.intItemUOMId = ICItemUOM.intItemUOMId
+LEFT JOIN tblICCategory ICCategory ON RetailPriceAdjustmentDetail.intCategoryId = ICCategory.intCategoryId
+LEFT JOIN tblSTSubcategory STSubcategoryFamily ON RetailPriceAdjustmentDetail.intFamilyId = STSubcategoryFamily.intSubcategoryId
+LEFT JOIN tblSTSubcategory STSubcategoryClass ON RetailPriceAdjustmentDetail.intClassId = STSubcategoryClass.intSubcategoryId
+LEFT JOIN tblSMCompanyLocation SMCompanyLocation ON RetailPriceAdjustmentDetail.intCompanyLocationId = SMCompanyLocation.intCompanyLocationId
+LEFT JOIN tblEMEntity EMEntity ON RetailPriceAdjustmentDetail.intVendorId = EMEntity.intEntityId
