9c90635174073803eb40dc949d0b1f2dd2805437
Mon Gonzales <mon.gonzales@irely.com>
2016-07-15 19:54:16 +0800

    EM-1055

---
+++ i21Database/dbo/Views/vyuEMSearchVendorMin.sql
@@ -0,0 +1,11 @@
+﻿CREATE VIEW [dbo].[vyuEMSearchVendorMin]
+	AS 
+
+	select 
+		a.intEntityId,
+		b.ysnTransportTerminal
+		from vyuEMEntityType a
+			join tblAPVendor b
+				on a.intEntityId = b.intEntityVendorId	
+
+	where Vendor = 1
