f91a4e0b10c287e351dcfd3371f9a12a85e75aa2
Gopinath Nayak <gopinath.n@irely.com>
2016-07-18 22:40:49 +0530

    GRN-394

--- i21Database/dbo/Stored Procedures/uspGRReverseOnReceiptDelete.sql
+++ i21Database/dbo/Stored Procedures/uspGRReverseOnReceiptDelete.sql
@@ -19,51 +19,50 @@ BEGIN TRY
 	FROM dbo.tblICInventoryReceipt
 	WHERE intInventoryReceiptId = @InventoryReceiptId
 
-	----Receipt Created By Scale Distributuion
-	IF @intSourceType = 1
+	----Receipt Created By Scale Distributuion	
 	BEGIN
-	  SELECT @intSourceId= ReceiptItem.intSourceId 
-	  FROM tblICInventoryReceipt Receipt
-	  JOIN tblICInventoryReceiptItem ReceiptItem ON ReceiptItem.intInventoryReceiptId = Receipt.intInventoryReceiptId 
-	  WHERE Receipt.intInventoryReceiptId = @InventoryReceiptId
+		  SELECT @intSourceId= ReceiptItem.intSourceId 
+		  FROM tblICInventoryReceipt Receipt
+		  JOIN tblICInventoryReceiptItem ReceiptItem ON ReceiptItem.intInventoryReceiptId = Receipt.intInventoryReceiptId 
+		  WHERE Receipt.intInventoryReceiptId = @InventoryReceiptId
 
-		IF EXISTS (
+		  IF EXISTS (
 				SELECT 1
 				FROM tblGRCustomerStorage CS
 				JOIN tblGRStorageHistory SH ON SH.intCustomerStorageId = CS.intCustomerStorageId
 				WHERE SH.strType = 'From Scale' AND SH.intInventoryReceiptId = @InventoryReceiptId
 				)
-		BEGIN
-			SELECT @intCustomerStorageId = CS.intCustomerStorageId
-			FROM tblGRCustomerStorage CS
-			JOIN tblGRStorageHistory SH ON SH.intCustomerStorageId = CS.intCustomerStorageId
-			WHERE SH.strType = 'From Scale' AND SH.intInventoryReceiptId = @InventoryReceiptId
+			BEGIN
+				SELECT @intCustomerStorageId = CS.intCustomerStorageId
+				FROM tblGRCustomerStorage CS
+				JOIN tblGRStorageHistory SH ON SH.intCustomerStorageId = CS.intCustomerStorageId
+				WHERE SH.strType = 'From Scale' AND SH.intInventoryReceiptId = @InventoryReceiptId
 			
-			IF EXISTS(SELECT 1 FROM tblARInvoiceDetail WHERE intCustomerStorageId = @intCustomerStorageId)
-			BEGIN
-				RAISERROR('Invoice Exists for the Grain Ticket for this receipt.',16, 1);
-			END
-			ELSE IF EXISTS(SELECT 1 FROM [tblAPBillDetail] WHERE [intCustomerStorageId] = @intCustomerStorageId)
-			BEGIN
-				RAISERROR('Voucher Exists for the Grain Ticket for this receipt.',16, 1);
-			END
-			ELSE IF EXISTS(SELECT 1 FROM [tblGRStorageHistory] WHERE [intCustomerStorageId] = @intCustomerStorageId AND strType = 'Transfer')
-			BEGIN
-				RAISERROR('The Grain Ticket of this receipt has transferred.',16, 1);
-			END
-			ELSE IF EXISTS(SELECT 1 FROM [tblGRCustomerStorage] WHERE [intCustomerStorageId] = @intCustomerStorageId AND dblOriginalBalance < > dblOpenBalance)
-			BEGIN
-				RAISERROR('There is mismatch between the original balance and open balance of the grain ticket of this receipt.',16, 1);
-			END
+				IF EXISTS(SELECT 1 FROM tblARInvoiceDetail WHERE intCustomerStorageId = @intCustomerStorageId)
+				BEGIN
+					RAISERROR('Invoice Exists for the Grain Ticket for this receipt.',16, 1);
+				END
+				ELSE IF EXISTS(SELECT 1 FROM [tblAPBillDetail] WHERE [intCustomerStorageId] = @intCustomerStorageId)
+				BEGIN
+					RAISERROR('Voucher Exists for the Grain Ticket for this receipt.',16, 1);
+				END
+				ELSE IF EXISTS(SELECT 1 FROM [tblGRStorageHistory] WHERE [intCustomerStorageId] = @intCustomerStorageId AND strType = 'Transfer')
+				BEGIN
+					RAISERROR('The Grain Ticket of this receipt has transferred.',16, 1);
+				END
+				ELSE IF EXISTS(SELECT 1 FROM [tblGRCustomerStorage] WHERE [intCustomerStorageId] = @intCustomerStorageId AND dblOriginalBalance < > dblOpenBalance)
+				BEGIN
+					RAISERROR('There is mismatch between the original balance and open balance of the grain ticket of this receipt.',16, 1);
+				END
 
-			DELETE
-			FROM tblQMTicketDiscount
-			WHERE intTicketFileId = @intCustomerStorageId AND strSourceType = 'Storage'
+				DELETE
+				FROM tblQMTicketDiscount
+				WHERE intTicketFileId = @intCustomerStorageId AND strSourceType = 'Storage'
 
-			DELETE
-			FROM tblGRCustomerStorage
-			WHERE intCustomerStorageId = @intCustomerStorageId
-		END		
+				DELETE
+				FROM tblGRCustomerStorage
+				WHERE intCustomerStorageId = @intCustomerStorageId
+			END		
 	END
 END TRY
 
