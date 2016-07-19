dde45d587f056c4d998302f5d2bbd11f5364d10e
Swetosuvro Ghosh <swetosuvro.g@irely.com>
2016-07-19 21:30:21 +0530

    LG-405

--- i21Database/dbo/Tables/tblLGLoadCost.sql
+++ i21Database/dbo/Tables/tblLGLoadCost.sql
@@ -5,8 +5,11 @@ CREATE TABLE [dbo].[tblLGLoadCost]
 	[intLoadId] [int] NOT NULL,
 	[intItemId] [int] NOT NULL,
 	[intVendorId] [int] NULL,
+	[strEntityType] [nvarchar](100) COLLATE Latin1_General_CI_AS NULL,
 	[strCostMethod] [nvarchar](30) COLLATE Latin1_General_CI_AS NOT NULL,
+	[intCurrencyId] INT NULL,
 	[dblRate] [numeric](10, 4) NOT NULL,
+	[dblAmount] [numeric](10, 4) NULL,
 	[intItemUOMId] [int] NULL,
 	[ysnAccrue] [bit] NOT NULL CONSTRAINT [DF_tblLGLoadCost_ysnAccrue]  DEFAULT ((1)),
 	[ysnMTM] [bit] NULL,
@@ -15,6 +18,7 @@ CREATE TABLE [dbo].[tblLGLoadCost]
 	CONSTRAINT [PK_tblLGLoadCost] PRIMARY KEY ([intLoadCostId]), 
 	CONSTRAINT [FK_tblLGLoadCost_tblLGLoad_intLoadId] FOREIGN KEY ([intLoadId]) REFERENCES [tblLGLoad]([intLoadId]) ON DELETE CASCADE,
 	CONSTRAINT [FK_tblLGLoadCost_tblICItem_intItemId] FOREIGN KEY ([intItemId]) REFERENCES [tblICItem]([intItemId]),
-	CONSTRAINT [FK_tblLGLoadCost_tblAPVendor_intVendorId] FOREIGN KEY ([intVendorId]) REFERENCES [tblAPVendor]([intEntityVendorId]),
-	CONSTRAINT [FK_tblLGLoadCost_tblICItemUOM_intItemUOMId] FOREIGN KEY ([intItemUOMId]) REFERENCES [tblICItemUOM]([intItemUOMId])
+	CONSTRAINT [FK_tblLGLoadCost_tblAPVendor_intVendorId] FOREIGN KEY ([intVendorId]) REFERENCES [tblEMEntity]([intEntityId]),
+	CONSTRAINT [FK_tblLGLoadCost_tblICItemUOM_intItemUOMId] FOREIGN KEY ([intItemUOMId]) REFERENCES [tblICItemUOM]([intItemUOMId]),
+	CONSTRAINT [FK_tblLGLoadCost_tblSMCurrency_intCurrencyId] FOREIGN KEY ([intCurrencyId]) REFERENCES [tblSMCurrency]([intCurrencyID])
 )
--- i21Database/dbo/Views/vyuLGLoadCostView.sql
+++ i21Database/dbo/Views/vyuLGLoadCostView.sql
@@ -4,8 +4,12 @@ SELECT LC.intLoadCostId
 	  ,LC.intLoadId
 	  ,LC.intItemId
 	  ,LC.intVendorId AS intEntityId
+	  ,LC.strEntityType
 	  ,LC.strCostMethod
+	  ,LC.intCurrencyId
+	  ,C.strCurrency
 	  ,LC.dblRate
+	  ,LC.dblAmount
 	  ,LC.intItemUOMId
 	  ,LC.ysnAccrue
 	  ,LC.ysnMTM
@@ -20,3 +24,4 @@ JOIN tblLGLoad L ON L.intLoadId = LC.intLoadId
 LEFT JOIN tblICItemUOM IU ON IU.intItemUOMId = LC.intItemUOMId
 LEFT JOIN tblICUnitMeasure UM ON UM.intUnitMeasureId = IU.intUnitMeasureId
 LEFT JOIN tblICItem I ON I.intItemId = LC.intItemId
+LEFT JOIN tblSMCurrency C ON C.intCurrencyID = LC.intCurrencyId
