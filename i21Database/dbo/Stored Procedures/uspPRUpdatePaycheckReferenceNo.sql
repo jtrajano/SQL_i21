754180fc27647c6cf4bcb1714b4a131d14e194bd
Arman Arenas <arman.arenas@irely.com>
2016-07-20 13:33:05 +0800

    PR-719 fixed check number not updating when printing from Process Paychecks

--- i21Database/dbo/Stored Procedures/uspPRUpdatePaycheckReferenceNo.sql
+++ i21Database/dbo/Stored Procedures/uspPRUpdatePaycheckReferenceNo.sql
@@ -6,7 +6,6 @@ UPDATE tblPRPaycheck
 SET strReferenceNo = ISNULL((SELECT TOP 1 strReferenceNo FROM tblCMBankTransaction 
 						WHERE strTransactionId = tblPRPaycheck.strPaycheckId
 						  AND intBankTransactionTypeId IN (21, 23)
-						  AND strSourceSystem = 'PR'
 						  AND intBankAccountId = ISNULL(@intBankAccountId, intBankAccountId)
 						  AND dtmCheckPrinted IS NOT NULL), ''),
 	ysnPrinted = 1
