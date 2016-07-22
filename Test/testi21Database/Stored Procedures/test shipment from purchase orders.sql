3f995dfb62db5a50f7e6fd20048522d20388ac67
Wendell Wayne Estrada <wendell.estrada@irely.com>
2016-07-22 08:24:12 +0800

    IC-1866 Need a SP to create Inventory Shipment

---
+++ Test/testi21Database/Stored Procedures/test shipment from purchase orders.sql
@@ -0,0 +1,93 @@
+﻿CREATE PROCEDURE testi21Database.[test shipment from purchase orders]
+AS
+BEGIN
+	CREATE TABLE expected_tblICInventoryShipmentItem (intItemId INT, intOwnershipType INT, 
+		dblQuantity NUMERIC(38, 20), intItemUOMId INT)
+		
+	CREATE TABLE actual_tblICInventoryShipmentItem (intItemId INT, intOwnershipType INT, 
+		dblQuantity NUMERIC(38, 20), intItemUOMId INT)
+
+	DECLARE 
+		@ExpectedShipmentNumber VARCHAR(50) = 'IS-75',
+		@ActualShipmentNumber VARCHAR(50),
+		@intUserId INT = 1,
+		@ShipmentEntries ShipmentStagingTable,
+		@ShipmentCharges ShipmentChargeStagingTable,
+		@ShipmentItemLots ShipmentItemLotStagingTable,
+		
+		@OWNERSHIP_TYPE_Own INT = 1,
+		@OWNERSHIP_TYPE_Storage INT = 2,
+		@OWNERSHIP_TYPE_ConsignedPurchase INT = 3,
+
+		@ORDER_TYPE_SalesContract INT = 1,
+		@ORDER_TYPE_SalesOrder INT = 2,
+		@ORDER_TYPE_TransferOrder INT = 3,
+		@ORDER_TYPE_Direct INT = 4,
+
+		@SOURCE_TYPE_None INT = 0,
+		@SOURCE_TYPE_Scale INT = 1,
+		@SOURCE_TYPE_InboundShipment INT = 2,
+		@SOURCE_TYPE_PickLot INT = 3
+
+		-- Test Values
+		DECLARE @intItemId INT = 2,	@intOwnerShipType INT = @OWNERSHIP_TYPE_Storage, 
+		@dblQuantity INT = 1, @intItemUOMId INT = 7
+
+	INSERT INTO @ShipmentEntries(
+		intOrderType, intSourceType, intEntityCustomerId, dtmShipDate,
+		intShipFromLocationId, intShipToLocationId, intFreightTermId, 
+		strBOLNumber, strSourceScreenName,
+		
+		intItemId, intOwnershipType,
+		dblQuantity, intItemUOMId, intItemLotGroup,
+
+		intOrderId, intSourceId, intLineNo, dblUnitPrice, intWeightUOMId)
+	SELECT
+		intOrderType = @ORDER_TYPE_SalesOrder, -- Sales Order
+		intSourceType = @SOURCE_TYPE_None, -- No Source
+		intEntityCustomerId = 8, -- Apple Spice Sales
+		dtmShipDate = GETDATE(), -- Today
+		intShipFromLocationId = 2, -- Fort Wayne
+		intShipToLocationId = 638, -- Apple Spice
+		intFreightTermId = 3, -- Pickup
+		strBOLNumber = 'BOL-1',
+		strSourceScreenName = 'Inventory Shipment',
+		
+		intItemId = @intItemId, 
+		intOwnershipType = @intOwnerShipType,
+		dblQuantity = @dblQuantity, -- Quantity should be greater than zero.
+		intItemUOMId = @intItemUOMId,
+		intItemLotGroup = 1,
+
+		intOrderId = 89,
+		intSourceId = NULL,
+		intLineNo = 100,
+		dblUnitPrice = 10.0,
+		intWeightUOMId = 7
+
+	EXEC dbo.uspICAddItemShipment @ShipmentEntries, @ShipmentCharges, @ShipmentItemLots, @intUserId
+
+	SELECT TOP 1 @ActualShipmentNumber = strShipmentNumber
+	FROM tblICInventoryShipment
+	ORDER BY intInventoryShipmentId DESC
+
+	EXEC tSQLt.AssertEqualsString @ExpectedShipmentNumber, @ActualShipmentNumber, N'Error creating shipment.'
+	
+	-- Expected
+	INSERT INTO expected_tblICInventoryShipmentItem(intItemId, intOwnershipType, dblQuantity, intItemUOMId)
+	SELECT @intItemId, @intOwnerShipType, @dblQuantity, @intItemUOMId
+	-- Actual
+	INSERT INTO actual_tblICInventoryShipmentItem(intItemId, intOwnershipType, dblQuantity, intItemUOMId)
+	SELECT TOP 1 intItemId, intOwnershipType, dblQuantity, intItemUOMId
+	FROM tblICInventoryShipmentItem
+	ORDER BY intInventoryShipmentId DESC 
+
+	EXEC tSQLt.AssertEqualsTable @Expected = 'expected_tblICInventoryShipmentItem',
+		@Actual = 'actual_tblICInventoryShipmentItem'
+
+	IF OBJECT_ID('actual_tblICInventoryShipmentItem') IS NOT NULL 
+		DROP TABLE actual_tblICInventoryShipmentItem
+
+	IF OBJECT_ID('expected_tblICInventoryShipmentItem') IS NOT NULL 
+		DROP TABLE expected_tblICInventoryShipmentItem
+END
---
+++ i21Database/dbo/User Defined Types/ShipmentStagingTable.sql
@@ -0,0 +1,61 @@
+﻿CREATE TYPE [dbo].[ShipmentStagingTable] AS TABLE
+(
+	intId INT IDENTITY(1,1) PRIMARY KEY CLUSTERED,
+	
+	-- Header
+	-- Required Fields
+	intOrderType INT NOT NULL,
+	intSourceType INT NOT NULL,
+	intEntityCustomerId INT NULL,
+	dtmShipDate DATETIME NOT NULL,
+	intShipFromLocationId INT NOT NULL,
+	intShipToLocationId INT NULL,
+	intFreightTermId INT NOT NULL,
+	strSourceScreenName NVARCHAR(100) COLLATE Latin1_General_CI_AS NOT NULL,
+
+	-- Optional Fields
+	strReferenceNumber NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL,
+	dtmRequestedArrivalDate DATETIME NULL,
+	intShipToCompanyLocationId INT NULL,
+	strBOLNumber NVARCHAR(100) COLLATE Latin1_General_CI_AS NOT NULL,
+	intShipViaId INT NULL,
+	strVessel NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL,
+	strProNumber NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL,
+	strDriverId	NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL,
+	strSealNumber NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL,
+	strDeliveryInstruction NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL,
+	dtmAppointmentTime DATETIME NULL,
+	dtmDepartureTime DATETIME NULL,
+	dtmArrivalTime DATETIME NULL,
+	dtmDeliveredDate DATETIME NULL,
+	dtmFreeTime DATETIME NULL,
+	strReceivedBy NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL,
+	strComment NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL,
+
+	-- Details
+	-- Required Field for Details
+	intItemId INT NOT NULL,
+	intOwnershipType INT NOT NULL,
+	dblQuantity NUMERIC(38, 20) NOT NULL CHECK(dblQuantity > 0),
+	intItemUOMId INT NOT NULL,
+	-- This is used to group lots for lotted items
+	intItemLotGroup INT NULL,
+
+	intOrderId INT NULL,
+	intSourceId INT NULL,
+	intLineNo INT NULL,
+	intSubLocationId INT NULL,
+	intStorageLocationId INT NULL,
+	intCurrencyId INT NULL,
+	intWeightUOMId INT NULL,
+	dblUnitPrice NUMERIC(38, 20) NULL,
+	intDockDoorId INT NULL,
+	strNotes NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NULL,
+	intGradeId INT NULL,
+	intDiscountSchedule INT NULL,
+	
+	-- Fields for Internal Use Only
+	intHeaderId INT NULL,
+	intShipmentId INT NULL,
+	UNIQUE (intItemLotGroup, intOrderType, intSourceType, intEntityCustomerId, dtmShipDate, intShipFromLocationId, intShipToLocationId, intFreightTermId)
+)
