180a89fc4e69d392e09dafb42956e2e53cb3f4a7
Mark Paragas <mark.paragas@irely.com>
2016-07-22 11:51:22 +0800

    MFT-207

--- i21Database/Scripts/Post-Deployment/TF/DefaultData/_TaxCategory.sql
+++ i21Database/Scripts/Post-Deployment/TF/DefaultData/_TaxCategory.sql
@@ -4,91 +4,106 @@ GO
 
 IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = N'tblTFTaxCategory')
 BEGIN
+	DECLARE @intTaxAuthorityId INT
 
-DECLARE @intTaxAuthorityId INT
-
-SELECT TOP 1 @intTaxAuthorityId = intTaxAuthorityId FROM tblTFTaxAuthority WHERE strTaxAuthorityCode = 'IN'
-IF (@intTaxAuthorityId IS NOT NULL)
-
-BEGIN
-	IF NOT EXISTS(SELECT TOP 1 [intTaxAuthorityId] FROM [tblTFTaxCategory] WHERE [intTaxAuthorityId] = @intTaxAuthorityId)
+	SELECT TOP 1 @intTaxAuthorityId = intTaxAuthorityId FROM tblTFTaxAuthority WHERE strTaxAuthorityCode = 'IN'
+	IF (@intTaxAuthorityId IS NOT NULL)
 	BEGIN
-		INSERT INTO [tblTFTaxCategory]
-		(
-			[intTaxAuthorityId],[strState],[strTaxCategory],[intConcurrencyId]
-		)
-		VALUES
-		 (@intTaxAuthorityId,'IN','IN Excise Tax Gasoline', 0)
-		,(@intTaxAuthorityId,'IN','IN Excise Tax Diesel Clear', 0)
-		,(@intTaxAuthorityId,'IN','IN Inspection Fee', 0)
-		,(@intTaxAuthorityId,'IN','IN Gasoline Use Tax (GUT)', 0)
+
+	UPDATE tblSMTaxCode SET intTaxCategoryId = NULL
+
+	-- DROP CONSTRAINTS TO TRUNCATE tblTFReportingComponent
+	ALTER TABLE tblSMTaxCode
+	DROP CONSTRAINT FK_tblSMTaxCode_tblTFTaxCategory
+
+	TRUNCATE TABLE tblTFTaxCategory
+	TRUNCATE TABLE tblTFTaxCriteria
+
+	--ADD FOREIGN KEY BACK
+	ALTER TABLE tblSMTaxCode ADD CONSTRAINT
+	FK_tblSMTaxCode_tblTFTaxCategory FOREIGN KEY
+	( intTaxCategoryId )
+	REFERENCES tblTFTaxCategory
+	( intTaxCategoryId )
+
+		IF NOT EXISTS(SELECT TOP 1 [intTaxAuthorityId] FROM [tblTFTaxCategory] WHERE [intTaxAuthorityId] = @intTaxAuthorityId)
+		BEGIN
+			INSERT INTO [tblTFTaxCategory]
+			(
+				[intTaxAuthorityId],[strState],[strTaxCategory],[intConcurrencyId]
+			)
+			VALUES
+			 (@intTaxAuthorityId,'IN','IN Excise Tax Gasoline', 0)
+			,(@intTaxAuthorityId,'IN','IN Excise Tax Diesel Clear', 0)
+			,(@intTaxAuthorityId,'IN','IN Inspection Fee', 0)
+			,(@intTaxAuthorityId,'IN','IN Gasoline Use Tax (GUT)', 0)
+		END
 	END
-END
 
-SELECT TOP 1 @intTaxAuthorityId = intTaxAuthorityId FROM tblTFTaxAuthority WHERE strTaxAuthorityCode = 'OH'
-IF (@intTaxAuthorityId IS NOT NULL)
+	SELECT TOP 1 @intTaxAuthorityId = intTaxAuthorityId FROM tblTFTaxAuthority WHERE strTaxAuthorityCode = 'OH'
+	IF (@intTaxAuthorityId IS NOT NULL)
 
-BEGIN
-	IF NOT EXISTS(SELECT TOP 1 [intTaxAuthorityId] FROM [tblTFTaxCategory] WHERE [intTaxAuthorityId] = @intTaxAuthorityId)
 	BEGIN
-		INSERT INTO [tblTFTaxCategory]
-		(
-			[intTaxAuthorityId],[strState],[strTaxCategory],[intConcurrencyId]
-		)
-		VALUES
-		(@intTaxAuthorityId,'OH','OH Excise Tax', 0)
+		IF NOT EXISTS(SELECT TOP 1 [intTaxAuthorityId] FROM [tblTFTaxCategory] WHERE [intTaxAuthorityId] = @intTaxAuthorityId)
+		BEGIN
+			INSERT INTO [tblTFTaxCategory]
+			(
+				[intTaxAuthorityId],[strState],[strTaxCategory],[intConcurrencyId]
+			)
+			VALUES
+			(@intTaxAuthorityId,'OH','OH Excise Tax', 0)
 
+		END
 	END
-END
 
-SELECT TOP 1 @intTaxAuthorityId = intTaxAuthorityId FROM tblTFTaxAuthority WHERE strTaxAuthorityCode = 'IL'
-IF (@intTaxAuthorityId IS NOT NULL)
+	SELECT TOP 1 @intTaxAuthorityId = intTaxAuthorityId FROM tblTFTaxAuthority WHERE strTaxAuthorityCode = 'IL'
+	IF (@intTaxAuthorityId IS NOT NULL)
 
-BEGIN
-	IF NOT EXISTS(SELECT TOP 1 [intTaxAuthorityId] FROM [tblTFTaxCategory] WHERE [intTaxAuthorityId] = @intTaxAuthorityId)
 	BEGIN
-		INSERT INTO [tblTFTaxCategory]
-		(
-			[intTaxAuthorityId],[strState],[strTaxCategory],[intConcurrencyId]
-		)
-		VALUES
-		(@intTaxAuthorityId,'IL','IL Excise Tax', 0)
-		,(@intTaxAuthorityId,'IL','IL UST/EIF', 0)
+		IF NOT EXISTS(SELECT TOP 1 [intTaxAuthorityId] FROM [tblTFTaxCategory] WHERE [intTaxAuthorityId] = @intTaxAuthorityId)
+		BEGIN
+			INSERT INTO [tblTFTaxCategory]
+			(
+				[intTaxAuthorityId],[strState],[strTaxCategory],[intConcurrencyId]
+			)
+			VALUES
+			(@intTaxAuthorityId,'IL','IL Excise Tax', 0)
+			,(@intTaxAuthorityId,'IL','IL UST/EIF', 0)
 
+		END
 	END
-END
 
-SELECT TOP 1 @intTaxAuthorityId = intTaxAuthorityId FROM tblTFTaxAuthority WHERE strTaxAuthorityCode = 'MS'
-IF (@intTaxAuthorityId IS NOT NULL)
+	SELECT TOP 1 @intTaxAuthorityId = intTaxAuthorityId FROM tblTFTaxAuthority WHERE strTaxAuthorityCode = 'MS'
+	IF (@intTaxAuthorityId IS NOT NULL)
 
-BEGIN
-	IF NOT EXISTS(SELECT TOP 1 [intTaxAuthorityId] FROM [tblTFTaxCategory] WHERE [intTaxAuthorityId] = @intTaxAuthorityId)
 	BEGIN
-		INSERT INTO [tblTFTaxCategory]
-		(
-			[intTaxAuthorityId],[strState],[strTaxCategory],[intConcurrencyId]
-		)
-		VALUES
-		(@intTaxAuthorityId,'MS','MS Excise Tax', 0)
+		IF NOT EXISTS(SELECT TOP 1 [intTaxAuthorityId] FROM [tblTFTaxCategory] WHERE [intTaxAuthorityId] = @intTaxAuthorityId)
+		BEGIN
+			INSERT INTO [tblTFTaxCategory]
+			(
+				[intTaxAuthorityId],[strState],[strTaxCategory],[intConcurrencyId]
+			)
+			VALUES
+			(@intTaxAuthorityId,'MS','MS Excise Tax', 0)
 
+		END
 	END
-END
 
-SELECT TOP 1 @intTaxAuthorityId = intTaxAuthorityId FROM tblTFTaxAuthority WHERE strTaxAuthorityCode = 'US'
-IF (@intTaxAuthorityId IS NOT NULL)
+	SELECT TOP 1 @intTaxAuthorityId = intTaxAuthorityId FROM tblTFTaxAuthority WHERE strTaxAuthorityCode = 'US'
+	IF (@intTaxAuthorityId IS NOT NULL)
 
-BEGIN
-	IF NOT EXISTS(SELECT TOP 1 [intTaxAuthorityId] FROM [tblTFTaxCategory] WHERE [intTaxAuthorityId] = @intTaxAuthorityId)
 	BEGIN
-		INSERT INTO [tblTFTaxCategory]
-		(
-			[intTaxAuthorityId],[strState],[strTaxCategory],[intConcurrencyId]
-		)
-		VALUES
-		(@intTaxAuthorityId,'Federal','Federal Excise Tax', 0)
+		IF NOT EXISTS(SELECT TOP 1 [intTaxAuthorityId] FROM [tblTFTaxCategory] WHERE [intTaxAuthorityId] = @intTaxAuthorityId)
+		BEGIN
+			INSERT INTO [tblTFTaxCategory]
+			(
+				[intTaxAuthorityId],[strState],[strTaxCategory],[intConcurrencyId]
+			)
+			VALUES
+			(@intTaxAuthorityId,'Federal','Federal Excise Tax', 0)
 
+		END
 	END
-END
 
 END
 ELSE
