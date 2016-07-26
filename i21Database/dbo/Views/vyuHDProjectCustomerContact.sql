fb2e74f8e8b0a250ee5862c18f73c76bdd2b2818
Jayson Ramos <jayson.ramos@iRely.com>
2016-07-26 15:21:09 +0800

    HD-1505

--- i21Database/dbo/Views/vyuHDProjectCustomerContact.sql
+++ i21Database/dbo/Views/vyuHDProjectCustomerContact.sql
@@ -11,30 +11,12 @@
 			,strLocationName = (select top 1 [tblEMEntityLocation].strLocationName from [tblEMEntityLocation] where [tblEMEntityLocation].intEntityLocationId = [tblEMEntityToContact].intEntityLocationId)
 			,tblEMEntity.ysnActive
 			,strEntityType = (select top 1 et.strType from [tblEMEntityType] et where et.intEntityId = tblARCustomer.intEntityCustomerId and et.strType in ('Customer','Prospect'))
+			,tblEMEntityPhoneNumber.strPhone
 		from tblARCustomer
 			,[tblEMEntityToContact]
 			,tblEMEntity
+			,tblEMEntityPhoneNumber
 		where
 			[tblEMEntityToContact].intEntityId = tblARCustomer.intEntityCustomerId
 			and tblEMEntity.intEntityId = [tblEMEntityToContact].intEntityContactId
-		/*
-		select
-			tblARCustomer.intEntityCustomerId
-			,tblARCustomer.strCustomerNumber
-			,tblEMEntity.intEntityId
-			,tblEMEntity.strName
-			,tblEMEntity.strTitle
-			,tblEMEntity.strEmail
-			,[tblEMEntityLocation].intEntityLocationId
-			,[tblEMEntityLocation].strLocationName
-			,tblEMEntity.ysnActive
-			,strEntityType = (select top 1 et.strType from [tblEMEntityType] et where et.intEntityId = tblARCustomer.intEntityCustomerId and et.strType in ('Customer','Prospect'))
-		from tblARCustomer
-			,[tblEMEntityToContact]
-			,tblEMEntity
-			,[tblEMEntityLocation]
-		where
-			[tblEMEntityToContact].intEntityId = tblARCustomer.intEntityCustomerId
-			and tblEMEntity.intEntityId = [tblEMEntityToContact].intEntityContactId
-			and [tblEMEntityLocation].intEntityLocationId = [tblEMEntityToContact].intEntityLocationId
-			*/
+			and tblEMEntityPhoneNumber.intEntityId = tblEMEntity.intEntityId
