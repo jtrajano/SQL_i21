054ce741ae28aa41ee69ebf637bcbf20d7868636
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2016-07-26 16:11:21 +0530

    RM-191

--- i21Database/dbo/Tables/tblRKInterfaceSystem.sql
+++ i21Database/dbo/Tables/tblRKInterfaceSystem.sql
@@ -9,10 +9,10 @@
 	[strLow] NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
 	[strLastSettle] NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
 	[strLastElement] NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
-	[strOptInterfaceSystemURL] NVARCHAR(MAX) NULL, 
-    [strOptOpen] NVARCHAR(100) NULL, 
-    [strOptHigh] NVARCHAR(100) NULL, 
-    [strOptLow] NVARCHAR(100) NULL, 
-    [strOptLastSettle] NVARCHAR(100) NULL, 
+	[strOptInterfaceSystemURL] NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NULL, 
+    [strOptOpen] NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL, 
+    [strOptHigh] NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL, 
+    [strOptLow] NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL, 
+    [strOptLastSettle] NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL, 
     CONSTRAINT [PK_tblRKInterfaceSystem_intInterfaceSystemId] PRIMARY KEY ([intInterfaceSystemId])
 )
