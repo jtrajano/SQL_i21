9f0ca459f94e4d353d45998028c0a85556f8a599
jesmar mendones <jesmar.mendones@irely.com>
2016-07-28 16:05:07 +0800

    FRM-3508

--- i21Database/dbo/Tables/tblSMHomePanelDashboard.sql
+++ i21Database/dbo/Tables/tblSMHomePanelDashboard.sql
@@ -17,6 +17,6 @@
 	[intConcurrencyId]			int											NOT NULL, 
 
 	CONSTRAINT [PK_tblSMHomePanelDashboard] PRIMARY KEY CLUSTERED ([intHomePanelDashboardId] ASC),
-	CONSTRAINT [FK_tblSMHomePanelDashboard_tblSMGridLayout_intEntityId] FOREIGN KEY (intGridLayoutId) REFERENCES tblSMGridLayout([intGridLayoutId]),
-	CONSTRAINT [FK_tblSMHomePanelDashboard_tblEMEntity_intEntityId] FOREIGN KEY ([intEntityId]) REFERENCES tblEMEntity([intEntityId]) 
+	CONSTRAINT [FK_tblSMHomePanelDashboard_tblEMEntity_intEntityId] FOREIGN KEY ([intEntityId]) REFERENCES tblEMEntity([intEntityId]),
+	CONSTRAINT [FK_tblSMHomePanelDashboard_tblSMGridLayout_intGridLayoutId] FOREIGN KEY (intGridLayoutId) REFERENCES tblSMGridLayout([intGridLayoutId]) ON DELETE NO ACTION ON UPDATE NO ACTION
 )
