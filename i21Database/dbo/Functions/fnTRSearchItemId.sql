fae382fe575bf97582ff7fa9eb28d1d744aa3e23
Lawrence Zabala <lawrence.zabala@irely.com>
2016-07-28 17:02:29 +0800

    TR-79

---
+++ i21Database/dbo/Functions/fnTRSearchItemId.sql
@@ -0,0 +1,39 @@
+﻿CREATE FUNCTION [dbo].[fnTRSearchItemId]
+(
+	@SupplyPoint AS NVARCHAR(100),
+	@Item AS NVARCHAR(100)
+)
+RETURNS INT
+
+AS
+
+BEGIN
+	DECLARE @Location NVARCHAR(50)
+		, @Supplier NVARCHAR(50)
+		, @ItemNo NVARCHAR(50)
+		, @Id INT
+
+	SELECT TOP 1 @Location = SearchValue.strLocation
+		, @Supplier = SearchValue.strSupplier
+		, @ItemNo = SearchValue.strItemNo
+	FROM vyuTRGetSupplyPointSearchValue SearchValue
+	CROSS APPLY (
+		SELECT Total.strLocation, Total.strSupplier, Total.strItemNo, dblTotal = COUNT(*) FROM vyuTRGetSupplyPointSearchValue Total
+		WHERE SearchValue.strLocation = Total.strLocation
+			AND SearchValue.strSupplier = Total.strSupplier
+			AND SearchValue.strItemNo = Total.strItemNo
+		GROUP BY Total.strLocation, Total.strSupplier, Total.strItemNo
+	) Total
+	WHERE @SupplyPoint LIKE '%' + SearchValue.strSearchValue + '%'
+			OR @Item LIKE '%' + SearchValue.strSearchValue + '%'
+	GROUP BY SearchValue.strLocation, SearchValue.strSupplier, SearchValue.strItemNo, Total.dblTotal
+	HAVING COUNT(*) = Total.dblTotal
+	ORDER BY Total.dblTotal DESC
+
+	SELECT @Id = intKeyId FROM vyuTRGetSupplyPointSearchValue
+	WHERE @Location = strLocation
+		AND @Supplier = strSupplier
+		AND @ItemNo = strItemNo
+
+	RETURN @Id
+END
--- i21Database/dbo/Views/vyuTRGetImportRackPriceDetail.sql
+++ i21Database/dbo/Views/vyuTRGetImportRackPriceDetail.sql
@@ -3,15 +3,18 @@
 
 SELECT Detail.intImportRackPriceDetailId
 	, Detail.intImportRackPriceId
-	, Detail.strSupplyPoint
+	, strImportSupplyPoint = Detail.strSupplyPoint
 	, Detail.intSupplyPointId
+	, SupplyPoint.strFuelSupplier
+	, strSupplyPoint = SupplyPoint.strSupplyPoint
 	, Detail.dtmEffectiveDate
 	, Detail.strComments
 	, Detail.ysnSelected
 	, ysnValidDetail = Detail.ysnValid
 	, DetailItem.intImportRackPriceDetailItemId
-	, DetailItem.strItemNo
+	, strImportItemNo = DetailItem.strItemNo
 	, DetailItem.intItemId
+	, strItemNo = Item.strItemNo
 	, DetailItem.dblVendorPrice
 	, DetailItem.dblJobberPrice
 	, DetailItem.strEquation
@@ -21,4 +24,6 @@ SELECT Detail.intImportRackPriceDetailId
 						ELSE 'Success!' END					
 FROM tblTRImportRackPriceDetail Detail
 LEFT JOIN tblTRImportRackPriceDetailItem DetailItem ON DetailItem.intImportRackPriceDetailId = Detail.intImportRackPriceDetailId
+LEFT JOIN vyuTRSupplyPointView SupplyPoint ON SupplyPoint.intSupplyPointId = Detail.intSupplyPointId
+LEFT JOIN tblICItem Item ON Item.intItemId = DetailItem.intItemId
 WHERE Detail.ysnValid = 1 AND DetailItem.ysnValid = 1
