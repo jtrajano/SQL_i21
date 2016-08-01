26028304c33a769634269d0b4dae3d19209a085e
bonn ponce <bonn.ponce@irely.com>
2016-08-01 21:40:12 +0800

    CF-634

---
+++ i21Database/dbo/Tables/tblCFNetworkAccount.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tblCFNetworkAccount] (
+    [intNetworkAccountId] INT            IDENTITY (1, 1) NOT NULL,
+    [intAccountId]        INT            NOT NULL,
+    [intNetworkId]        INT            NOT NULL,
+    [strNetworkAccountId] NVARCHAR (250) COLLATE Latin1_General_CI_AS NOT NULL,
+    [intConcurrencyId]    INT            CONSTRAINT [DF_tblCFNetworkAccount_intConcurrencyId] DEFAULT ((1)) NULL,
+    CONSTRAINT [PK_tblCFNetworkAccount] PRIMARY KEY CLUSTERED ([intNetworkAccountId] ASC)
+);
+
+
+
