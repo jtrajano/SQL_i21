cca8ae4b532b14060d520d3c5766dedc66312e72
Von Carlo Pasion <von.pasion@irely.com>
2016-08-02 18:13:49 +0800

    FRM-3511

--- i21Database/dbo/Tables/tblSMGridColumn.sql
+++ i21Database/dbo/Tables/tblSMGridColumn.sql
@@ -5,6 +5,8 @@
     [strFieldName]				NVARCHAR (100)  COLLATE Latin1_General_CI_AS NOT NULL,
 	[strControlName]			NVARCHAR (100)  COLLATE Latin1_General_CI_AS NOT NULL,
 	[strControlType]			NVARCHAR (100)  COLLATE Latin1_General_CI_AS NOT NULL,
+	[intFlex]					INT				NULL,
+	[intWidth]					INT				NULL,
 	[ysnBuild]					BIT             NOT NULL DEFAULT 0,	
     [intSort]					INT             NOT NULL DEFAULT (1),
     [intConcurrencyId]			INT				NOT NULL DEFAULT (1),
