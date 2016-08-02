1ecb4a247ee1a9fd5eccffa72b50f13f3d456de9
Lawrence Zabala <lawrence.zabala@irely.com>
2016-08-02 14:36:50 +0800

    MB-74

--- i21Database/dbo/Tables/tblMBConsignmentRateDetail.sql
+++ i21Database/dbo/Tables/tblMBConsignmentRateDetail.sql
@@ -8,6 +8,7 @@
     [dblIntervalPumpPrice] NUMERIC(18, 6) NOT NULL DEFAULT ((0)), 
     [dblIntervalRate] NUMERIC(18, 6) NOT NULL DEFAULT ((0)), 
     [dblConsignmentFloor] NUMERIC(18, 6) NOT NULL DEFAULT ((0)), 
+	[strRateType] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL,
     [intSort] INT NULL, 
     [intConcurrencyId] INT NULL DEFAULT ((0)), 
     CONSTRAINT [PK_tblMBConsignmentRateDetail] PRIMARY KEY ([intConsignmentRateDetailId]), 
