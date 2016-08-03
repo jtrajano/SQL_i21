ff15e19ddac4bea46a56dd8c0c6eba1070d7e646
Premkumar S <prem.selladurai@irely.com>
2016-08-03 12:24:19 +0530

    MFG-1924

--- i21Database/dbo/Stored Procedures/uspMFGetProductBySubLocation.sql
+++ i21Database/dbo/Stored Procedures/uspMFGetProductBySubLocation.sql
@@ -1,6 +1,17 @@
-﻿CREATE PROCEDURE uspMFGetProductBySubLocation (@intSubLocationId INT,@strItemNo nvarchar(50)='%',@intItemId int=0,@intManufacturingProcessId int=0)
+﻿CREATE PROCEDURE uspMFGetProductBySubLocation (
+	@intSubLocationId INT
+	,@strItemNo NVARCHAR(50) = '%'
+	,@intItemId INT = 0
+	,@intManufacturingProcessId INT = 0
+	)
 AS
 BEGIN
+	DECLARE @intLocationId INT
+
+	SELECT @intLocationId = intCompanyLocationId
+	FROM tblSMCompanyLocationSubLocation
+	WHERE intCompanyLocationSubLocationId = @intSubLocationId
+
 	SELECT I.intItemId
 		,I.strItemNo
 		,I.strDescription
@@ -14,11 +25,24 @@ BEGIN
 	JOIN dbo.tblICUnitMeasure U ON U.intUnitMeasureId = IU.intUnitMeasureId
 	JOIN dbo.tblSMCompanyLocationSubLocationCategory C ON C.intCategoryId = I.intCategoryId
 		AND C.intCompanyLocationSubLocationId = @intSubLocationId
+		AND R.intLocationId = @intLocationId
 		AND R.ysnActive = 1
 		AND IU.ysnStockUnit = 1
-		AND I.strStatus='Active'
-		AND I.strItemNo LIKE @strItemNo+'%'
-		AND I.intItemId =(Case When @intItemId >0 then @intItemId else I.intItemId end)
-		AND R.intManufacturingProcessId =(Case When @intManufacturingProcessId >0 then @intManufacturingProcessId else R.intManufacturingProcessId end)
+		AND I.strStatus = 'Active'
+		AND I.strItemNo LIKE @strItemNo + '%'
+		AND I.intItemId = (
+			CASE 
+				WHEN @intItemId > 0
+					THEN @intItemId
+				ELSE I.intItemId
+				END
+			)
+		AND R.intManufacturingProcessId = (
+			CASE 
+				WHEN @intManufacturingProcessId > 0
+					THEN @intManufacturingProcessId
+				ELSE R.intManufacturingProcessId
+				END
+			)
 	ORDER BY I.strItemNo
 END
