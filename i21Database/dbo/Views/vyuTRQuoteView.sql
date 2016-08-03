2f1a4e5a41e8670dc281658d17a8c904511157ab
Lawrence Zabala <lawrence.zabala@irely.com>
2016-08-03 12:07:06 +0800

    TR-555

--- i21Database/dbo/Views/vyuTRQuoteView.sql
+++ i21Database/dbo/Views/vyuTRQuoteView.sql
@@ -1,20 +1,22 @@
 ﻿CREATE VIEW [dbo].[vyuTRQuoteView]
 	AS 
-SELECT 
-   QH.intQuoteHeaderId,
-   QH.strQuoteNumber,
-   QH.strQuoteStatus,
-   QH.strQuoteComments,
-   QH.strCustomerComments,
-   QH.dtmQuoteDate,
-   AR.strName,
-   AR.intEntityCustomerId,
-   convert(decimal,0.000000) "dblQuoteTotal",
-   DATEADD(dd, DATEDIFF(dd, 0, dtmQuoteDate), 0) "dtmQuote",
-   ysnHasEmailSetup = CASE WHEN (SELECT COUNT(*) FROM vyuARCustomerContacts CC WHERE CC.intCustomerEntityId = QH.intEntityCustomerId AND ISNULL(CC.strEmail, '') <> '' AND CC.strEmailDistributionOption LIKE '%' + 'Transport Quote' + '%') > 0 THEN CONVERT(BIT, 1) ELSE CONVERT(BIT, 0) END
-    	
-FROM
-    tblTRQuoteHeader QH
-	join vyuARCustomer AR on QH.intEntityCustomerId = AR.intEntityCustomerId
 
-	
+SELECT QH.intQuoteHeaderId
+   , QH.strQuoteNumber
+   , QH.strQuoteStatus
+   , QH.strQuoteComments
+   , QH.strCustomerComments
+   , QH.dtmQuoteDate
+   , QH.dtmQuoteEffectiveDate
+   , AR.strName
+   , AR.intEntityCustomerId
+   , dblQuoteTotal = CONVERT(DECIMAL, 0.000000)
+   , dtmQuote = DATEADD(dd, DATEDIFF(dd, 0, dtmQuoteDate), 0)
+   , ysnHasEmailSetup = CASE WHEN (SELECT COUNT(*) 
+									FROM vyuARCustomerContacts CC 
+									WHERE CC.intCustomerEntityId = QH.intEntityCustomerId 
+										AND ISNULL(CC.strEmail, '') <> '' 
+										AND CC.strEmailDistributionOption LIKE '%' + 'Transport Quote' + '%') > 0 THEN CONVERT(BIT, 1) 
+							ELSE CONVERT(BIT, 0) END
+FROM tblTRQuoteHeader QH
+LEFT JOIN vyuARCustomer AR ON QH.intEntityCustomerId = AR.intEntityCustomerId	
