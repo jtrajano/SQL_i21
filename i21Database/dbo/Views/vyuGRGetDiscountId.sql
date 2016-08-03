a92427c2bd1e90b27735ebcca910d43ac86b6657
Gopinath Nayak <gopinath.n@irely.com>
2016-08-03 16:06:24 +0530

    GRN-440

---
+++ i21Database/dbo/Views/vyuGRGetDiscountId.sql
@@ -0,0 +1,11 @@
+﻿CREATE VIEW [dbo].[vyuGRGetDiscountId]
+AS     
+ SELECT 
+ DId.intDiscountId
+,DId.intCurrencyId
+,Cur.strCurrency
+,DId.strDiscountId
+,DId.strDiscountDescription
+,DId.ysnDiscountIdActive
+FROM tblGRDiscountId DId
+JOIN tblSMCurrency Cur ON Cur.intCurrencyID = DId.intCurrencyId
