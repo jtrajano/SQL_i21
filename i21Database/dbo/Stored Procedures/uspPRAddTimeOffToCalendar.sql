ef2e9e95653c5cc3ea3017cdcaa2d8961cc3bbff
Arman Arenas <arman.arenas@irely.com>
2016-08-03 17:38:49 +0800

    PR-713 Added uspPRAddTimeOffToCalendar

---
+++ i21Database/dbo/Stored Procedures/uspPRAddTimeOffToCalendar.sql
@@ -0,0 +1,44 @@
+﻿CREATE PROCEDURE [dbo].[uspPRAddTimeOffToCalendar]
+	@intTransactionId INT
+	,@intUserId INT
+AS
+BEGIN
+
+	INSERT INTO tblSMEvents
+	(intEntityId
+	,intCalendarId
+	,strEventTitle
+	,strEventDetail
+	,strJsonData
+	,strScreen
+	,strRecordNo
+	,dtmStart
+	,dtmEnd
+	,dtmCreated
+	,dtmModified
+	,ysnActive
+	,intConcurrencyId)
+	SELECT
+	@intUserId
+	,NULL
+	,'Time Off - ' + ENT.strName
+	,'<p><b>Time Off Type:</b> ' + TTO.strTimeOff +'</p>' 
+		+ '<p><b>Reason:</b> </br>' + TOR.strReason +'</p>'
+		+ '<p><b>Address while on Time Off:</b></br>' + TOR.strAddress +'</p>'
+	,'{"allDay":true,"drillDown":{"enabled":true,"url":"#/PR/TimeOffRequest?routeId=1%7C%5E%7C&activeTab=Details","text":"View Time Off Request "},"title":"Time Off - ' + REPLACE(ENT.strName, '', '''') + '"}'
+	,'Payroll.view.TimeOffRequest'
+	,CAST(TOR.intTimeOffRequestId AS NVARCHAR(100))
+	,TOR.dtmDateFrom
+	,TOR.dtmDateTo
+	,GETDATE()
+	,GETDATE()
+	,1
+	,1
+	FROM tblPRTimeOffRequest TOR
+	INNER JOIN tblEMEntity ENT ON TOR.intEntityEmployeeId = ENT.intEntityId
+	INNER JOIN tblPRTypeTimeOff TTO ON TOR.intTypeTimeOffId = TTO.intTypeTimeOffId
+	WHERE intTimeOffRequestId = @intTransactionId
+	
+	IF (@@ERROR = 0) UPDATE tblPRTimeOffRequest SET ysnPostedToCalendar = 1 WHERE intTimeOffRequestId = @intTransactionId
+
+END
