29cf1d703d602d660aaa07078564b3e655b6e45a
Von Carlo Pasion <von.pasion@irely.com>
2016-08-03 15:29:25 +0800

    FRM-3511

--- i21Database/dbo/Tables/tblSMCustomTab.sql
+++ i21Database/dbo/Tables/tblSMCustomTab.sql
@@ -6,6 +6,7 @@
 	[strLayout]					NVARCHAR (50)	COLLATE Latin1_General_CI_AS NULL,
     [ysnBuild]					BIT             NOT NULL DEFAULT 0,	
     [intConcurrencyId]			INT				NOT NULL DEFAULT (1),
-	CONSTRAINT [FK_tblSMCustomTab_tblSMScreen] FOREIGN KEY ([intScreenId]) REFERENCES [tblSMScreen]([intScreenId])
+	CONSTRAINT [FK_tblSMCustomTab_tblSMScreen] FOREIGN KEY ([intScreenId]) REFERENCES [tblSMScreen]([intScreenId]),
+	CONSTRAINT [UC_Screen_Tab] UNIQUE (intScreenId, strTabName)
 );
 
