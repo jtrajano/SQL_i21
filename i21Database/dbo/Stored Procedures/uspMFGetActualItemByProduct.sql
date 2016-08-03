d3cff9a26229a743e75f2904716ffa7dcc416b06
Premkumar S <prem.selladurai@irely.com>
2016-08-03 12:22:40 +0530

    MFG-1922

--- i21Database/dbo/Stored Procedures/uspMFGetActualItemByProduct.sql
+++ i21Database/dbo/Stored Procedures/uspMFGetActualItemByProduct.sql
@@ -17,7 +17,7 @@ BEGIN
 	JOIN dbo.tblMFWorkOrderRecipeItem RI on RI.intRecipeId=R.intRecipeId and RI.intWorkOrderId=R.intWorkOrderId and RI.intRecipeItemTypeId=2 and R.intWorkOrderId=@intWorkOrderId
 	LEFT JOIN dbo.tblMFWorkOrderRecipeSubstituteItem SI ON SI.intRecipeItemId = RI.intRecipeItemId and SI.intWorkOrderId = RI.intWorkOrderId
 	JOIN dbo.tblICItem I ON (I.intItemId = RI.intItemId OR I.intItemId = SI.intSubstituteItemId)
-	JOIN dbo.tblICItemUOM IU ON IU.intItemId = I.intItemId and IU.ysnStockUnit=1
+	JOIN dbo.tblICItemUOM IU ON IU.intItemUOMId = I.intWeightUOMId 
 	JOIN dbo.tblICUnitMeasure U ON U.intUnitMeasureId = IU.intUnitMeasureId
 		AND R.intLocationId = @intLocationId
 		AND R.ysnActive = 1
