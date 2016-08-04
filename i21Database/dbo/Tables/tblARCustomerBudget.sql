469b4c9c2599a33714f0419549050f1c28bad9e6
Mon Gonzales <mon.gonzales@irely.com>
2016-08-04 18:41:37 +0800

    EM-1287

--- i21Database/dbo/Tables/tblARCustomerBudget.sql
+++ i21Database/dbo/Tables/tblARCustomerBudget.sql
@@ -4,6 +4,7 @@
     [intEntityCustomerId]		INT				NOT NULL,
 	[dblBudgetAmount]			NUMERIC (18, 6) DEFAULT ((0)) NOT NULL,
 	[dtmBudgetDate]				DATETIME		NULL,
+	[dblAmountPaid]				NUMERIC (18, 6) DEFAULT ((0)) NOT NULL,
 	[ysnUsedBudget]				BIT				NULL DEFAULT(0),
 	[ysnForgiven]				BIT				NULL DEFAULT(0),
 	[ysnCalculated]				BIT				NULL DEFAULT(0),
