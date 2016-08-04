c475689e7123e77872354f5514bd9d26670d6f03
Emerson Versoza <emerson.versoza@irely.com>
2016-08-04 14:37:48 +0800

    SC-302

--- i21Database/dbo/Tables/tblSCDistributionOption.sql
+++ i21Database/dbo/Tables/tblSCDistributionOption.sql
@@ -6,10 +6,12 @@
     [intTicketTypeId] INT NOT NULL, 
     [ysnDistributionAllowed] BIT NOT NULL,
 	[ysnDefaultDistribution] BIT NOT NULL, 
+	[intStorageScheduleTypeId] INT NULL, 
     [intConcurrencyId] INT NULL DEFAULT ((0)), 
     CONSTRAINT [PK_tblSCDistributionOption_intDistributionOptionId] PRIMARY KEY ([intDistributionOptionId]), 
     CONSTRAINT [FK_tblSCDistributionOption_tblSCTicketPool_intTicketPoolId] FOREIGN KEY (intTicketPoolId) REFERENCES tblSCTicketPool(intTicketPoolId), 
     CONSTRAINT [FK_tblSCDistributionOption_tblSCTicketType_intTicketTypeId] FOREIGN KEY (intTicketTypeId) REFERENCES tblSCTicketType(intTicketTypeId), 
+	CONSTRAINT [FK_tblSCDistributionOption_tblGRStorageType_intStorageScheduleTypeId] FOREIGN KEY (intStorageScheduleTypeId) REFERENCES tblGRStorageType(intStorageScheduleTypeId), 
     CONSTRAINT [UK_tblSCDistributionOption_intTicketPoolId] UNIQUE ([intTicketPoolId],[intTicketTypeId],[strDistributionOption]) 
 )
 
