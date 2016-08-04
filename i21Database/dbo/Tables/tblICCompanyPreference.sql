97d95851165be365dbdfe5ca01a7887f5a2746e5
Joy Balangatan <joy.balangatan@irely.com>
2016-08-04 11:31:32 +0800

    IC-2467 Added fields strLotCondition, strReceiptType, intReceiptSourceType, intShipmentOrderType, intShipmentSourceType to tblICCompanyPreference table

--- i21Database/dbo/Tables/tblICCompanyPreference.sql
+++ i21Database/dbo/Tables/tblICCompanyPreference.sql
@@ -4,5 +4,10 @@
     [intInheritSetup] INT NULL DEFAULT ((1)), 
     [intSort] INT NULL, 
     [intConcurrencyId] INT NULL DEFAULT ((0)), 
+    [strLotCondition] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
+    [strReceiptType] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
+    [intReceiptSourceType] INT NULL, 
+    [intShipmentOrderType] INT NULL, 
+    [intShipmentSourceType] INT NULL, 
     CONSTRAINT [PK_tblICCompanyPreference] PRIMARY KEY ([intCompanyPreferenceId]) 
 )
