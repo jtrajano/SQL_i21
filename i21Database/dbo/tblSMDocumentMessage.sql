3a7287a57e035834be74770a5df312e9d7acc0a1
Crisman Lazaga <cris.lazaga@irely.com>
2016-08-05 15:07:46 +0800

    FCOM-2010

---
+++ i21Database/dbo/tblSMDocumentMessage.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[tblSMDocumentMessage]
+(
+	[intDocumentMessageId] INT NOT NULL  IDENTITY, 
+    [intTransactionId] INT NOT NULL, 
+    [intDocumentMaintenanceId] INT NULL, 
+    [strHeaderMessage] NVARCHAR(MAX) NULL, 
+    [strFooterMessage] NVARCHAR(MAX) NULL, 
+    [dtmDateModified] DATETIME NULL, 
+    [intConcurrencyId] INT NOT NULL, 
+    CONSTRAINT [PK_tblSMDocumentMessage] PRIMARY KEY ([intDocumentMessageId]), 
+    CONSTRAINT [FK_tblSMDocumentMessage_tblSMTransaction] FOREIGN KEY ([intTransactionId]) REFERENCES [tblSMTransaction]([intTransactionId])
+)
