63d1ce667e8f4e06a18cd3b2f79dd3649c70ae59
Jayson Ramos <jayson.ramos@iRely.com>
2016-08-08 16:44:48 +0800

    HD-1432

---
+++ i21Database/dbo/Views/vyuHDCustomerProspectVendor.sql
@@ -0,0 +1,29 @@
+﻿CREATE VIEW [dbo].[vyuHDCustomerProspectVendor]
+	AS
+		select
+			intEntityCustomerId = b.intEntityId
+			,strCustomerNumber = a.strVendorId
+			,strName = b.strName
+			,strType = c.strType
+			,ysnActive = b.ysnActive
+		from
+			tblAPVendor a
+			,tblEMEntity b
+			,tblEMEntityType c
+		where b.intEntityId = a.intEntityVendorId
+			and c.intEntityId = b.intEntityId
+
+		union all
+
+		select
+			intEntityCustomerId = b.intEntityId
+			,strCustomerNumber = a.strCustomerNumber
+			,strName = b.strName
+			,strType = c.strType
+			,ysnActive = b.ysnActive
+		from
+			tblARCustomer a
+			,tblEMEntity b
+			,tblEMEntityType c
+		where b.intEntityId = a.intEntityCustomerId
+			and c.intEntityId = b.intEntityId
