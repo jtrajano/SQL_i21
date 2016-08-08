a4403849bb7dab74c8f1b823d1b81274487d7616
Arman Arenas <arman.arenas@irely.com>
2016-08-08 16:34:33 +0800

    PR-681 Added vyuPRReportWorkersCompensation

---
+++ i21Database/dbo/Views/vyuPRReportWorkersCompensation.sql
@@ -0,0 +1,38 @@
+﻿CREATE VIEW [dbo].[vyuPRReportWorkersCompensation]
+AS
+SELECT
+	PC.intEntityEmployeeId,
+	EMP.strSocialSecurity,
+	strEmployeeId = ENT.strEntityNo,
+	strName = ENT.strName,
+	PC.dtmPayDate,
+	TE.strEarning,
+	strDepartment = ISNULL(DEP.strDepartment, '(No Department)'),
+	PE.dblHours,
+	PE.dblTotal,
+	WC.strWCCode,
+	WC.strDescription,
+	WC.dblRate,
+	dblWCTotal = CASE WHEN (WC.strCalculationType = 'Per Hour') THEN 
+					CAST((PE.dblHours * WC.dblRate) AS NUMERIC(18, 6))
+				 ELSE
+					CAST(
+						(CASE WHEN (PE.strCalculationType = 'Overtime') THEN 
+							(PE.dblTotal / EE.dblAmount)
+						 ELSE 
+							PE.dblTotal
+						 END
+						* WC.dblRate)
+					AS NUMERIC(18, 6))
+				 END
+FROM 
+	tblPRPaycheckEarning PE
+	INNER JOIN tblPREmployeeEarning EE ON EE.intEmployeeEarningId = PE.intEmployeeEarningId
+	INNER JOIN tblPRTypeEarning TE ON TE.intTypeEarningId = PE.intTypeEarningId 
+	INNER JOIN tblPRPaycheck PC ON PC.intPaycheckId = PE.intPaycheckId
+	INNER JOIN tblPREmployee EMP ON PC.intEntityEmployeeId = EMP.intEntityEmployeeId
+	INNER JOIN tblEMEntity ENT ON EMP.intEntityEmployeeId = ENT.intEntityId
+	INNER JOIN tblPRWorkersCompensation WC ON PE.intWorkersCompensationId = WC.intWorkersCompensationId
+	LEFT JOIN tblPRDepartment DEP ON DEP.intDepartmentId = PE.intEmployeeDepartmentId 
+WHERE 
+	PE.intWorkersCompensationId IS NOT NULL
