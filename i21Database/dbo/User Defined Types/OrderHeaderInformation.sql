d33e3579c7b7cecb358d7ec2184bfc6f9a985b7e
Swetosuvro Ghosh <swetosuvro.g@irely.com>
2016-08-09 14:01:18 +0530

    MFG-1921

---
+++ i21Database/dbo/User Defined Types/OrderHeaderInformation.sql
@@ -0,0 +1,13 @@
+﻿CREATE TYPE [dbo].[OrderHeaderInformation] AS TABLE (
+	 [intId]							INT IDENTITY PRIMARY KEY CLUSTERED
+	,[intOrderStatusId]					INT
+	,[intOrderTypeId]					INT
+	,[intOrderDirectionId]				INT
+	,[strOrderNo]						INT
+	,[strReferenceNo]					INT
+	,[intStagingLocationId]				INT
+	,[intWorkOrderId]					NVARCHAR(MAX)
+	,[strComment]						INT NULL
+	,[dtmOrderDate]						INT NULL
+	,[strLastUpdateBy]					NVARCHAR(100)
+	)
