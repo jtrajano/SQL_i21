90baeeec61bcf8b4a4a3b5210e78c480f850373a
Gopinath Nayak <gopinath.n@irely.com>
2016-08-09 13:03:13 +0530

    GRN-440

---
+++ i21Database/dbo/Views/vyuGRGetAllStorageSchedule.sql
@@ -0,0 +1,17 @@
+﻿CREATE VIEW [dbo].[vyuGRGetAllStorageSchedule]
+AS
+SELECT 
+ S.intStorageScheduleRuleId
+,S.strScheduleId
+,S.strScheduleDescription
+,S.intStorageType
+,ST.strStorageTypeDescription
+,S.intCommodity
+,Com.strCommodityCode  
+,S.strFeeType
+,S.intCurrencyID
+,Cur.strCurrency  
+FROM tblGRStorageScheduleRule S
+JOIN tblGRStorageType ST ON ST.intStorageScheduleTypeId=S.intStorageType
+JOIN tblSMCurrency Cur ON Cur.intCurrencyID = S.intCurrencyID  
+JOIN tblICCommodity Com ON Com.intCommodityId=S.intCommodity
---
+++ i21Database/dbo/Views/vyuGRGetDiscountSchedule.sql
@@ -0,0 +1,12 @@
+﻿CREATE VIEW [dbo].[vyuGRGetDiscountSchedule]
+AS       
+SELECT   
+ DSch.intDiscountScheduleId  
+,DSch.intCurrencyId  
+,Cur.strCurrency  
+,DSch.intCommodityId  
+,Com.strCommodityCode  
+,DSch.strDiscountDescription  
+FROM tblGRDiscountSchedule DSch  
+JOIN tblSMCurrency Cur ON Cur.intCurrencyID = DSch.intCurrencyId  
+JOIN tblICCommodity Com ON Com.intCommodityId=DSch.intCommodityId  
