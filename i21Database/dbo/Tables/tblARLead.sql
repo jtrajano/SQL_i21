05c45d790be76ae002f5f499d36b52b69be5d747
Mon Gonzales <mon.gonzales@irely.com>
2016-08-11 18:19:19 +0800

    EM-1343

---
+++ i21Database/dbo/Tables/tblARLead.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tblARLead]
+(
+	[intEntityLeadId]	INT NOT NULL,
+	[dtmDateAdded]		DATETIME NULL,
+	[dtmLastModified]	DATETIME NULL,
+	[ysnActive]			BIT DEFAULT(0) NOT NULL,
+	[intConcurrencyId]	INT DEFAULT ((0)) NOT NULL,
+	
+	CONSTRAINT [PK_tblARLead] PRIMARY KEY CLUSTERED ([intEntityLeadId] ASC),	
+
+)
