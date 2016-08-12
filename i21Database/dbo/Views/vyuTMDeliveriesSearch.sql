fbd028ff2246da71ade380f950b2728661a02d28
kenneth lagunero <kenneth.lagunero@irely.com>
2016-08-12 14:13:23 +0800

    TM-1966

--- i21Database/dbo/Views/vyuTMDeliveriesSearch.sql
+++ i21Database/dbo/Views/vyuTMDeliveriesSearch.sql
@@ -11,9 +11,9 @@ AS
 		,C.dblTotalCapacity
 		,F.dblQuantityDelivered
 		,F.dblCalculatedBurnRate
-		,dblAverageBurnRate =CAST( CASE WHEN ISNULL(C.dblPreviousBurnRate, 0.0) = 0 THEN ((ISNULL(F.dblCalculatedBurnRate,0.0) + ISNULL(C.dblBurnRate,0.0))/2) ELSE (ISNULL(F.dblCalculatedBurnRate,0.0) + ISNULL(C.dblBurnRate,0.0) + C.dblPreviousBurnRate)/3 END AS NUMERIC(18,6))
+		,dblAverageBurnRate =CAST( CASE WHEN ISNULL(C.dblPreviousBurnRate, 0.0) = 0 THEN ((ISNULL(F.dblCalculatedBurnRate,0.0) + ISNULL(C.dblBurnRate,0.0))/2.0) ELSE (ISNULL(F.dblCalculatedBurnRate,0.0) + ISNULL(C.dblBurnRate,0.0) + C.dblPreviousBurnRate)/3.0 END AS NUMERIC(18,6))
 		,dblPercentFilled = CAST( CASE WHEN ISNULL(C.dblTotalCapacity,0) = 0 THEN 0 ELSE F.dblQuantityDelivered / C.dblTotalCapacity * 100 END AS NUMERIC(18,6))
-		,dblPercentIdeal = CAST( CASE WHEN ((ISNULL(C.dblTotalCapacity,0.0) * (I.dblDefaultFull/100)) - ISNULL(C.dblTotalReserve,0.0))  <= 0 THEN 0 ELSE F.dblQuantityDelivered / ((ISNULL(C.dblTotalCapacity,0.0) * (I.dblDefaultFull/100)) - ISNULL(C.dblTotalReserve,0.0)) END AS NUMERIC(18,6)) * 100
+		,dblPercentIdeal = CAST( CASE WHEN ((ISNULL(C.dblTotalCapacity,0.0) * (I.dblDefaultFull/100.00)) - ISNULL(C.dblTotalReserve,0.0))  <= 0 THEN 0 ELSE F.dblQuantityDelivered / ((ISNULL(C.dblTotalCapacity,0.0) * (I.dblDefaultFull/100.00)) - ISNULL(C.dblTotalReserve,0.0)) END AS NUMERIC(18,6)) * 100
 		,intConcurrencyId = 0
 		,intCustomerID = C.intCustomerID
 		,intSiteID = C.intSiteID
