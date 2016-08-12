96dfb53a794790a47382b2c35931d5e0815e91c3
Swetosuvro Ghosh <swetosuvro.g@irely.com>
2016-08-12 16:37:49 +0530

    MFG-1921

---
+++ i21Database/dbo/Stored Procedures/uspMFCreateStagingOrderDetail.sql
@@ -0,0 +1,102 @@
+﻿CREATE PROCEDURE [dbo].[uspMFCreateStagingOrderDetail] 
+					@OrderDetailInformation OrderDetailInformation READONLY
+AS
+BEGIN TRY
+	DECLARE @idoc INT
+	DECLARE @strErrMsg NVARCHAR(MAX)
+	DECLARE @intOrderDetailId INT
+	DECLARE @strOrderNo NVARCHAR(32)
+	DECLARE @strLastUpdateBy NVARCHAR(32)
+	DECLARE @intLastUpdateById INT
+	DECLARE @intOrderTypeId INT
+	DECLARE @strWorkOrderId NVARCHAR(MAX)
+	DECLARE @intLocalTran TINYINT
+
+	SET @strErrMsg = ''
+	SET NOCOUNT ON
+
+	IF @@TRANCOUNT = 0
+		SET @intLocalTran = 1
+
+	IF @intLocalTran = 1
+		BEGIN TRANSACTION
+
+	SELECT @strLastUpdateBy = strLastUpdateBy
+	FROM @OrderDetailInformation
+
+	SELECT @intLastUpdateById = intEntityUserSecurityId
+	FROM tblSMUserSecurity
+	WHERE strUserName = @strLastUpdateBy
+
+	INSERT INTO tblMFOrderDetail (
+		intConcurrencyId
+		,intOrderHeaderId
+		,intItemId
+		,dblQty
+		,intItemUOMId
+		,dblWeight
+		,intWeightUOMId
+		,dblWeightPerUnit
+		,intWeightPerUnitUOMId
+		,intLotId
+		,strLotAlias
+		,intUnitsPerLayer
+		,intLayersPerPallet
+		,intPreferenceId
+		,dtmProductionDate
+		,intLineNo
+		,intSanitizationOrderDetailsId
+		,strLineItemNote
+		,intCreatedById
+		,dtmCreatedOn
+		,intLastUpdateById
+		,dtmLastUpdateOn
+		)
+	SELECT 1
+		,intOrderHeaderId
+		,intItemId
+		,dblQty
+		,intItemUOMId
+		,dblWeight
+		,intWeightUOMId
+		,dblWeightPerUnit
+		,intWeightPerUnitUOMId
+		,intLotId
+		,strLotAlias
+		,intUnitsPerLayer
+		,intLayersPerPallet
+		,intPreferenceId
+		,dtmProductionDate
+		,intLineNo
+		,intSanitizationOrderDetailsId
+		,strLineItemNote
+		,@intLastUpdateById
+		,GETDATE()
+		,@intLastUpdateById
+		,GETDATE()
+	FROM @OrderDetailInformation
+
+	SELECT @intOrderDetailId = SCOPE_IDENTITY()
+
+	--Return the new Order Detail Id
+	SELECT @intOrderDetailId
+
+	IF @intLocalTran = 1
+		AND @@TRANCOUNT > 0
+		COMMIT TRANSACTION
+END TRY
+
+BEGIN CATCH
+	IF @intLocalTran = 1
+		AND @@TRANCOUNT > 0
+		ROLLBACK TRANSACTION
+
+	SET @strErrMsg = ERROR_MESSAGE()
+
+	IF @strErrMsg != ''
+	BEGIN
+		SET @strErrMsg = 'uspMFCreateStagingOrderDetail: ' + @strErrMsg
+
+		RAISERROR (@strErrMsg,16,1,'WITH NOWAIT')
+	END
+END CATCH
---
+++ i21Database/dbo/User Defined Types/OrderDetailInformation_1.sql
@@ -0,0 +1,19 @@
+﻿CREATE TYPE [dbo].[OrderDetailInformation] AS TABLE (
+	 [intId]							INT IDENTITY PRIMARY KEY CLUSTERED
+	,[intOrderHeaderId]					INT
+	,[intItemId]						INT
+	,[dblQty]							NUMERIC(18, 6)
+	,[intItemUOMId]						INT
+	,[dblWeight]						NUMERIC(18, 6)
+	,[intWeightUOMId]					INT
+	,[dblWeightPerUnit]					NUMERIC(18, 6)
+	,[intWeightPerUnitUOMId]			INT
+	,[intLotId]							INT
+	,[strLotAlias]						NVARCHAR(100)
+	,[intUnitsPerLayer]					INT
+	,[intLayersPerPallet]				INT
+	,[intPreferenceId]					INT
+	,[dtmProductionDate]				DATETIME
+	,[intLineNo]						INT
+	,[intSanitizationOrderDetailsId]	INT
+	,[strLineItemNote]					NVARCHAR(MAX))
