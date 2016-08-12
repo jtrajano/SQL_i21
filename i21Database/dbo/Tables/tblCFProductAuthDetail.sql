9650d7b3d9362c2243971b87482080972e72da93
bonn ponce <bonn.ponce@irely.com>
2016-08-12 10:00:58 +0800

    CF-647 Add Product Authorization Profile

--- i21Database/dbo/Tables/tblCFProductAuthDetail.sql
+++ i21Database/dbo/Tables/tblCFProductAuthDetail.sql
@@ -1,10 +1,12 @@
 ﻿CREATE TABLE [dbo].[tblCFProductAuthDetail] (
-    [intProductAuthDetailId] INT            IDENTITY (1, 1) NOT NULL,
-    [intProductAuthId]       INT            NULL,
-    [strNetworkProductCode]  NVARCHAR (50)  COLLATE Latin1_General_CI_AS NULL,
-    [strDescription]         NVARCHAR (200) COLLATE Latin1_General_CI_AS NULL,
-    [intConcurrencyId]       INT            CONSTRAINT [DF_tblCFProductAuth_intConcurrencyId] DEFAULT ((1)) NULL,
+    [intProductAuthDetailId] INT IDENTITY (1, 1) NOT NULL,
+    [intProductAuthId]       INT NULL,
+    [intItemId]              INT NULL,
+    [intConcurrencyId]       INT CONSTRAINT [DF_tblCFProductAuth_intConcurrencyId] DEFAULT ((1)) NULL,
     CONSTRAINT [PK_tblCFProductAuth] PRIMARY KEY CLUSTERED ([intProductAuthDetailId] ASC),
+    CONSTRAINT [FK_tblCFProductAuthDetail_tblCFItem] FOREIGN KEY ([intItemId]) REFERENCES [dbo].[tblCFItem] ([intItemId]),
     CONSTRAINT [FK_tblCFProductAuthDetail_tblCFProductAuth] FOREIGN KEY ([intProductAuthId]) REFERENCES [dbo].[tblCFProductAuth] ([intProductAuthId])
 );
+
+
 
