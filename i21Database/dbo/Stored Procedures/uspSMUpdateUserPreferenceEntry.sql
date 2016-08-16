8d92c66cd69d6fd31867f24c95010ed26707d91a
Dan Michael Jalimao <dan.jalimao@irely.com>
2016-08-16 13:37:37 +0800

    FCOM-2081

--- i21Database/dbo/Stored Procedures/uspSMUpdateUserPreferenceEntry.sql
+++ i21Database/dbo/Stored Procedures/uspSMUpdateUserPreferenceEntry.sql
@@ -1,35 +1,26 @@
 ﻿CREATE PROCEDURE [dbo].[uspSMUpdateUserPreferenceEntry]
-	@intEntityUserSecurityId int
+	@intEntityUserSecurityId INT
 AS
 BEGIN
 
-	DECLARE @currentRow INT
-	DECLARE @totalRows INT
 	DECLARE @sql NVARCHAR(MAX)
+	DECLARE @currentTable NVARCHAR(200)
 
-	SET @currentRow = 1
-	SELECT @totalRows = Count(DISTINCT(TABLE_NAME)) FROM INFORMATION_SCHEMA.COLUMNS WHERE [TABLE_NAME] LIKE '%UserPreference'
+	IF OBJECT_ID('tempdb..#TempUserPreference') IS NOT NULL DROP TABLE #TempUserPreference
+	SELECT TABLE_NAME INTO #TempUserPreference FROM INFORMATION_SCHEMA.COLUMNS WHERE [TABLE_NAME] LIKE '%UserPreference' 
 
-	WHILE (@currentRow <= @totalRows)
+	WHILE EXISTS(SELECT TOP 1 1 FROM #TempUserPreference)
 	BEGIN
+		SELECT TOP 1 @currentTable = TABLE_NAME FROM #TempUserPreference
 
-	Declare @tableName NVARCHAR(50)
-	SELECT @tableName = TABLE_NAME FROM (  
-		SELECT ROW_NUMBER() OVER(ORDER BY TABLE_NAME ASC) AS 'ROWID', *
-		FROM 
-		(
-			SELECT DISTINCT(TABLE_NAME) FROM INFORMATION_SCHEMA.COLUMNS WHERE [TABLE_NAME] LIKE '%UserPreference'
-		) a
-	) b
-	WHERE ROWID = @currentRow
+		SET @sql = 'IF NOT EXISTS(SELECT TOP 1 1 FROM ' + @currentTable + ' WHERE intEntityUserSecurityId = @intEntityUserSecurityId)
+					BEGIN
+						INSERT INTO ' + @currentTable + ' (intEntityUserSecurityId) VALUES (@intEntityUserSecurityId)
+					END'
+  
+		EXEC sp_executesql @sql, N'@intEntityUserSecurityId INT', @intEntityUserSecurityId = @intEntityUserSecurityId
 
-	SET @sql = 'IF NOT EXISTS(SELECT TOP 1 1 FROM ' + @tableName + ' WHERE intEntityUserSecurityId = @intEntityUserSecurityId)
-				BEGIN
-					INSERT INTO ' + @tableName + ' (intEntityUserSecurityId) VALUES (@intEntityUserSecurityId)
-				END'
-	EXEC sp_executesql @sql, N'@intEntityUserSecurityId INT', @intEntityUserSecurityId = @intEntityUserSecurityId
-
-	SET @currentRow = @currentRow + 1
+		DELETE FROM #TempUserPreference WHERE TABLE_NAME = @currentTable
 	END
 
 END
