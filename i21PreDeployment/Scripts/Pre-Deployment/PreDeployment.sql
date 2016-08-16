a150116a31daffbd5257a47a45df4bf398f765f6
Mon Gonzales <mon.gonzales@irely.com>
2016-08-16 18:44:15 +0800

    EM-1376

--- i21PreDeployment/Scripts/Pre-Deployment/PreDeployment.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/PreDeployment.sql
@@ -119,6 +119,7 @@
 :r .\12_EM\12_UpdateCustomerFreightNullLocation.sql
 :r .\12_EM\15_DropSMUserSecurityTrigger.sql
 :r .\12_EM\18_FixDuplicateLocationEntry.sql
+:r .\12_EM\19_CleanEntityPhoneNumber.sql
 
 --RK
 :r .\13_RK\01_DropTableScript.sql
