a8221a77ddaaa17f60b54a3841b784487baa7cfa
Von Carlo Pasion <von.pasion@irely.com>
2016-08-16 17:08:45 +0800

    FRM-3580

--- i21Database/Scripts/Post-Deployment/SM/DefaultData/10_Screen.sql
+++ i21Database/Scripts/Post-Deployment/SM/DefaultData/10_Screen.sql
@@ -75,14 +75,15 @@ GO
 			UPDATE tblSMScreen
 			SET strTableName = N'tblEMEntity', 
 				strScreenId = N'Entity Contact', 
-				strScreenName = 'Entity Contact'
+				strScreenName = 'Entity Contact',
+				ysnCustomTab = 1
 			WHERE strNamespace = 'EntityManagement.view.EntityContact'
 		END
 
 	IF NOT EXISTS (SELECT TOP 1 1 FROM tblSMScreen WHERE strNamespace = 'AccountsPayable.view.Voucher') 
 		BEGIN
 			INSERT [dbo].[tblSMScreen] ([strScreenId], [strScreenName], [strNamespace], [strModule], [strTableName], [strRecordNoField], [strEntityField], [ysnApproval], [ysnCustomTab], [intConcurrencyId]) 
-			VALUES (N'Voucher', N'Voucher', N'AccountsPayable.view.Voucher', N'Accounts Payable', N'tblAPBill', N'strBillId', N'intEntityVendorId',  1, 1,  0)
+			VALUES (N'Voucher', N'Voucher', N'AccountsPayable.view.Voucher', N'Accounts Payable', N'tblAPBill', N'strBillId', N'intEntityVendorId',  1, NULL,  0)
 		END
 	ELSE
 		BEGIN
@@ -90,15 +91,14 @@ GO
 			SET strTableName = 'tblAPBill',
 				strRecordNoField = 'strBillId',
 				strEntityField = 'intEntityVendorId', 
-				ysnApproval = 1,
-				ysnCustomTab = 1
+				ysnApproval = 1
 			WHERE strNamespace = 'AccountsPayable.view.Voucher'
 		END
 
 	IF NOT EXISTS (SELECT TOP 1 1 FROM tblSMScreen WHERE strNamespace = 'AccountsPayable.view.PurchaseOrder') 
 		BEGIN
 			INSERT [dbo].[tblSMScreen] ([strScreenId], [strScreenName], [strNamespace], [strModule], [strTableName], [strRecordNoField], [strEntityField], [ysnApproval], [ysnCustomTab], [intConcurrencyId]) 
-			VALUES (N'Purchase Order', N'Purchase Order', N'AccountsPayable.view.PurchaseOrder', N'Accounts Payable', N'tblPOPurchase', N'strPurchaseOrderNumber', N'intEntityVendorId',  1, 1,  0)
+			VALUES (N'Purchase Order', N'Purchase Order', N'AccountsPayable.view.PurchaseOrder', N'Accounts Payable', N'tblPOPurchase', N'strPurchaseOrderNumber', N'intEntityVendorId',  1, NULL,  0)
 		END
 	ELSE
 		BEGIN
@@ -106,15 +106,14 @@ GO
 			SET strTableName = 'tblPOPurchase',
 				strRecordNoField = 'strPurchaseOrderNumber',
 				strEntityField = 'intEntityVendorId', 
-				ysnApproval = 1,
-				ysnCustomTab = 1
+				ysnApproval = 1
 			WHERE strNamespace = 'AccountsPayable.view.PurchaseOrder'
 		END
 
 	IF NOT EXISTS (SELECT TOP 1 1 FROM tblSMScreen WHERE strNamespace = 'AccountsReceivable.view.Quote') 
 		BEGIN
 			INSERT [dbo].[tblSMScreen] ([strScreenId], [strScreenName], [strNamespace], [strModule], [strTableName], [strRecordNoField], [strEntityField], [ysnApproval], [ysnCustomTab], [intConcurrencyId]) 
-			VALUES (N'Quote', N'Quote', N'AccountsReceivable.view.Quote', N'Accounts Receivable', N'tblSOSalesOrder', N'strSalesOrderNumber', N'intEntityCustomerId',  1,  1,  0)
+			VALUES (N'Quote', N'Quote', N'AccountsReceivable.view.Quote', N'Accounts Receivable', N'tblSOSalesOrder', N'strSalesOrderNumber', N'intEntityCustomerId',  1,  NULL,  0)
 		END
 	ELSE
 		BEGIN
@@ -122,15 +121,14 @@ GO
 			SET strTableName = 'tblSOSalesOrder',
 				strRecordNoField = 'strSalesOrderNumber',
 				strEntityField = 'intEntityCustomerId', 
-				ysnApproval = 1,
-				ysnCustomTab = 1
+				ysnApproval = 1
 			WHERE strNamespace = 'AccountsReceivable.view.Quote'
 		END
 
 	IF NOT EXISTS (SELECT TOP 1 1 FROM tblSMScreen WHERE strNamespace = 'AccountsReceivable.view.SalesOrder') 
 		BEGIN
 			INSERT [dbo].[tblSMScreen] ([strScreenId], [strScreenName], [strNamespace], [strModule], [strTableName],[strRecordNoField], [strEntityField], [ysnApproval], [ysnCustomTab], [intConcurrencyId]) 
-			VALUES (N'Sales Order', N'Sales Order', N'AccountsReceivable.view.SalesOrder', N'Accounts Receivable', N'tblSOSalesOrder', N'strSalesOrderNumber', N'intEntityCustomerId',  1,  1,  0)
+			VALUES (N'Sales Order', N'Sales Order', N'AccountsReceivable.view.SalesOrder', N'Accounts Receivable', N'tblSOSalesOrder', N'strSalesOrderNumber', N'intEntityCustomerId',  1,  NULL,  0)
 		END
 	ELSE
 		BEGIN
@@ -138,15 +136,14 @@ GO
 			SET strTableName = 'tblSOSalesOrder',
 				strRecordNoField = 'strSalesOrderNumber',
 				strEntityField = 'intEntityCustomerId', 
-				ysnApproval = 1,
-				ysnCustomTab = 1
+				ysnApproval = 1
 			WHERE strNamespace = 'AccountsReceivable.view.SalesOrder'
 		END
 
 	IF NOT EXISTS (SELECT TOP 1 1 FROM tblSMScreen WHERE strNamespace = 'ContractManagement.view.Contract') 
 		BEGIN
 			INSERT [dbo].[tblSMScreen] ([strScreenId], [strScreenName], [strNamespace], [strModule], [strTableName], [strRecordNoField], [strEntityField], [ysnApproval], [ysnCustomTab], [intConcurrencyId]) 
-			VALUES (N'Contract', N'Contract', N'ContractManagement.view.Contract', N'Contract Management', N'tblCTContractHeader', N'strContractNumber', N'intEntityId',  1,  1,  0)
+			VALUES (N'Contract', N'Contract', N'ContractManagement.view.Contract', N'Contract Management', N'tblCTContractHeader', N'strContractNumber', N'intEntityId',  1,  NULL,  0)
 		END
 	ELSE
 		BEGIN
@@ -154,30 +151,28 @@ GO
 			SET strTableName = 'tblCTContractHeader',
 				strRecordNoField = 'strContractNumber',
 				strEntityField = 'intEntityId', 
-				ysnApproval = 1,
-				ysnCustomTab = 1
+				ysnApproval = 1
 			WHERE strNamespace = 'ContractManagement.view.Contract'
 		END
 
 	IF NOT EXISTS (SELECT TOP 1 1 FROM tblSMScreen WHERE strNamespace = 'RiskManagement.view.FuturesOptionsTransactions') 
 		BEGIN
 			INSERT [dbo].[tblSMScreen] ([strScreenId], [strScreenName], [strNamespace], [strModule], [strTableName], [strRecordNoField], [strEntityField], [ysnApproval], [ysnCustomTab], [intConcurrencyId]) 
-			VALUES (N'Futures Options Transactions', N'Futures Options Transactions', N'RiskManagement.view.FuturesOptionsTransactions', N'Risk Management', N'tblRKFutOptTransaction', NULL, N'intEntityId',  1,  1,  0)
+			VALUES (N'Futures Options Transactions', N'Futures Options Transactions', N'RiskManagement.view.FuturesOptionsTransactions', N'Risk Management', N'tblRKFutOptTransaction', NULL, N'intEntityId',  1,  NULL,  0)
 		END
 	ELSE
 		BEGIN
 			UPDATE tblSMScreen
 			SET strTableName = 'tblRKFutOptTransaction',
 				strEntityField = 'intEntityId', 
-				ysnApproval = 1,
-				ysnCustomTab = 1
+				ysnApproval = 1
 			WHERE strNamespace = 'RiskManagement.view.FuturesOptionsTransactions'
 		END
 
 	IF NOT EXISTS (SELECT TOP 1 1 FROM tblSMScreen WHERE strNamespace = 'Payroll.view.TimeOffRequest') 
 		BEGIN
 			INSERT [dbo].[tblSMScreen] ([strScreenId], [strScreenName], [strNamespace], [strModule], [strTableName], [strRecordNoField], [strEntityField], [ysnApproval], [ysnCustomTab], [intConcurrencyId]) 
-			VALUES (N'Time Off Requests', N'Time Off Requests', N'Payroll.view.TimeOffRequest', N'Payroll', N'tblPRTimeOffRequest', N'strRequestId', N'intEntityEmployeeId',  1,  1,  0)
+			VALUES (N'Time Off Requests', N'Time Off Requests', N'Payroll.view.TimeOffRequest', N'Payroll', N'tblPRTimeOffRequest', N'strRequestId', N'intEntityEmployeeId',  1,  NULL,  0)
 		END
 	ELSE
 		BEGIN
@@ -185,8 +180,7 @@ GO
 			SET strTableName = 'tblPRTimeOffRequest',
 				strRecordNoField = 'strRequestId',
 				strEntityField = 'intEntityEmployeeId', 
-				ysnApproval = 1,
-				ysnCustomTab = 1
+				ysnApproval = 1
 			WHERE strNamespace = 'Payroll.view.TimeOffRequest'
 		END
 GO
