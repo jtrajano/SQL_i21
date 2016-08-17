b49aceb8da213a3c2212c4f0cb219f59e316c000
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2016-08-17 12:16:15 +0530

    RM-406

---
+++ i21Database/dbo/Views/vyuRKGetSalesIntransitWOPickLot.sql
@@ -0,0 +1,27 @@
+﻿CREATE VIEW vyuRKGetSalesIntransitWOPickLot
+AS
+SELECT strContractNumber,
+		dblShipmentQty,
+		intCompanyLocationId,
+		strLocationName,
+		intContractDetailId,
+		dblInvoiceQty,
+		(isnull(dblShipmentQty,0)-isnull(dblInvoiceQty,0)) dblBalanceToInvoice,intCommodityId,strContractItemName as  strItemName, intCommodityUnitMeasureId as intUnitMeasureId
+		,intEntityId
+from(
+		SELECT d.strContractNumber +'-' +Convert(nvarchar,intContractSeq) strContractNumber,
+				c.dblQuantity dblShipmentQty,
+				d.intCompanyLocationId,
+				d.strLocationName,
+				d.intContractDetailId,
+				intCommodityId,
+				c.intItemUOMId,
+				d.strContractItemName,
+				d.intCommodityUnitMeasureId,
+				d.intEntityId,
+				isnull((SELECT isnull(id.dblQtyShipped,0) FROM tblARInvoice i
+					join tblARInvoiceDetail id on i.intInvoiceId=id.intInvoiceId and i.ysnPosted=1 and id.intContractDetailId=d.intContractDetailId),0) dblInvoiceQty
+		FROM tblICInventoryShipment b
+		JOIN tblICInventoryShipmentItem c on c.intInventoryShipmentId=b.intInventoryShipmentId and b.ysnPosted=1 
+		JOIN vyuCTContractDetailView d on d.intContractDetailId=c.intLineNo		
+		)t
---
+++ i21Database/dbo/Views/vyuRKOptionTransactionChart.sql
@@ -0,0 +1,16 @@
+﻿CREATE VIEW [dbo].[vyuRKOptionTransactionChart]
+
+AS
+
+SELECT TOP 100 PERCENT CONVERT(INT,ROW_NUMBER() OVER(ORDER BY CONVERT(DATETIME,'01 '+t.strOptionMonth))) as intRowNumber,strOptionMonth,sum(dblBuy) dblBuy,sum(dblSell) dblSell from(
+SELECT 
+	fm.strOptionMonth,
+	SUM(isnull(f.intNoOfContract,0)) dblBuy,
+	isnull((SELECT SUM(isnull(ft.intNoOfContract,0)) FROM tblRKFutOptTransaction ft 
+		WHERE ft.intOptionMonthId=fm.intOptionMonthId and ft.intInstrumentTypeId=2 and ft.strBuySell='Sell'
+	 ),0) dblSell
+FROM  tblRKFutOptTransaction f
+JOIN tblRKOptionsMonth fm on f.intOptionMonthId=fm.intOptionMonthId
+WHERE intInstrumentTypeId=2 and strBuySell='Buy'
+GROUP BY f.strBuySell, fm.intOptionMonthId,fm.strOptionMonth,f.intInstrumentTypeId)t group by strOptionMonth
+order by CONVERT(DATETIME,'01 '+t.strOptionMonth) 
