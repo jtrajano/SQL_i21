589c9d2d88b80ace0a67808ccc35f5d50ffbadd5
Swetosuvro Ghosh <swetosuvro.g@irely.com>
2016-08-17 16:13:11 +0530

    LG-261

--- i21Database/dbo/Stored Procedures/uspLGPostInventoryShipment.sql
+++ i21Database/dbo/Stored Procedures/uspLGPostInventoryShipment.sql
@@ -551,6 +551,89 @@ BEGIN
 			,@intTransactionId 
 			,@intEntityUserSecurityId
 
+			DECLARE @ItemsFromInventoryShipment AS dbo.ShipmentItemTableType
+
+			INSERT INTO @ItemsFromInventoryShipment (
+				-- Header
+				[intShipmentId]
+				,[strShipmentId]
+				,[intOrderType]
+				,[intSourceType]
+				,[dtmDate]
+				,[intCurrencyId]
+				,[dblExchangeRate]
+				-- Detail 
+				,[intInventoryShipmentItemId]
+				,[intItemId]
+				,[intLotId]
+				,[strLotNumber]
+				,[intLocationId]
+				,[intItemLocationId]
+				,[intSubLocationId]
+				,[intStorageLocationId]
+				,[intItemUOMId]
+				,[intWeightUOMId]
+				,[dblQty]
+				,[dblUOMQty]
+				,[dblNetWeight]
+				,[dblSalesPrice]
+				,[intDockDoorId]
+				,[intOwnershipType]
+				,[intOrderId]
+				,[intSourceId]
+				,[intLineNo]
+				)
+			SELECT L.intLoadId
+				,L.strLoadNumber
+				,1 AS intOrderType
+				,1 AS intSourceType
+				,L.dtmScheduledDate
+				,intCurrencyId = NULL
+				,[dblExchangeRate] = 1
+				,LD.intLoadDetailId
+				,LD.intItemId
+				,LDL.intLotId
+				,Lot.strLotNumber
+				,[intLocationId] = LD.intSCompanyLocationId
+				,[intItemLocationId] = IL.intItemLocationId
+				,[intSubLocationId] = LD.intSSubLocationId
+				,[intStorageLocationId] = NULL
+				,[intItemUOMId] = LD.intItemUOMId
+				,[intWeightUOMId] = LD.intWeightItemUOMId
+				,[dblQty] = CASE 
+							WHEN @ysnPost = 1
+								THEN - 1 *
+									CASE 
+										WHEN LDL.intLoadDetailLotId > 0
+											THEN LDL.dblLotQuantity
+										ELSE LD.dblQuantity
+										END
+							ELSE CASE 
+									WHEN LDL.intLoadDetailLotId > 0
+										THEN LDL.dblLotQuantity
+									ELSE LD.dblQuantity
+									END
+							END
+				,[dblUOMQty] = IU.dblUnitQty
+				,[dblNetWeight] = LDL.dblGross - LDL.dblTare
+				,[dblSalesPrice] = CD.dblCashPrice
+				,[intDockDoorId] = NULL
+				,[intOwnershipType] = Lot.intOwnershipType
+				,[intOrderId] = NULL
+				,[intSourceId] = NULL
+				,[intLineNo] = ISNULL(LD.intSContractDetailId, 0)
+			FROM tblLGLoad L
+			JOIN tblLGLoadDetail LD ON L.intLoadId = LD.intLoadId
+			JOIN tblCTContractDetail CD ON CD.intContractDetailId = LD.intSContractDetailId
+			JOIN tblICItemLocation IL ON IL.intItemId = LD.intItemId
+			JOIN tblICItemUOM IU ON IU.intItemUOMId = LD.intItemUOMId
+			LEFT JOIN tblICItemUOM WU ON WU.intItemUOMId = LD.intWeightItemUOMId
+			LEFT JOIN tblLGLoadDetailLot LDL ON LDL.intLoadDetailId = LD.intLoadDetailId
+			JOIN tblICLot Lot ON Lot.intLotId = LDL.intLotId
+			WHERE L.intLoadId = @intTransactionId
+
+	EXEC dbo.uspCTShipped @ItemsFromInventoryShipment ,@intEntityUserSecurityId  
+
 	-- Mark stock reservation as posted (or unposted)
 	EXEC dbo.uspICPostStockReservation
 		@intTransactionId
