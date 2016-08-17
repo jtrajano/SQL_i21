0e9b96007ec2f6c7f8aeff8dc8d3d0095e94f185
Erick Dela Cruz <erick.delacruz@irely.com>
2016-08-17 13:46:14 +0800

    AP-2111

--- i21Database/Scripts/Post-Deployment/SM/DefaultData/4_StartingNumbers.sql
+++ i21Database/Scripts/Post-Deployment/SM/DefaultData/4_StartingNumbers.sql
@@ -924,6 +924,16 @@ GO
 			,[intConcurrencyId]		= 1
 	WHERE NOT EXISTS (SELECT TOP 1 1 FROM tblSMStartingNumber WHERE strTransactionType = N'Sales Receipt')
 
+	UNION ALL
+	SELECT	[intStartingNumberId]	= 100
+			,[strTransactionType]	= N'Claim'
+			,[strPrefix]			= N'CL-'
+			,[intNumber]			= 1
+			,[strModule]			= 'Accounts Payable'
+			,[ysnEnable]			= 1
+			,[intConcurrencyId]		= 1
+	WHERE NOT EXISTS (SELECT TOP 1 1 FROM tblSMStartingNumber WHERE strTransactionType = N'Claim')
+
 	SET IDENTITY_INSERT [dbo].[tblSMStartingNumber] OFF
 GO
 	PRINT N'END INSERT DEFAULT STARTING NUMBERS'
---
+++ i21Database/dbo/User Defined Types/VoucherDetailClaim.sql
@@ -0,0 +1,11 @@
+﻿CREATE TYPE [dbo].[VoucherDetailClaim] AS TABLE
+(
+	dblNetShippedWeight		DECIMAL(18,6),
+	dblWeightLoss			DECIMAL(18,6),
+	dblFranchiseWeight		DECIMAL(18,6),
+	dblClaim				DECIMAL(18,6),
+	dblCost					DECIMAL(18,6),
+	intItemId				INT,
+	intContractHeaderId		INT,
+	intContractDetailId		INT
+)
