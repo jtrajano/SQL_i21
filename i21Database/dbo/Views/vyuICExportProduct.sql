368281fb264e9e8107c30a010b834c1a8016ed4b
Wendell Wayne Estrada <wendell.estrada@irely.com>
2016-08-17 13:48:17 +0800

    IC-2553 BASE View - Products

---
+++ i21Database/dbo/Views/vyuICExportProduct.sql
@@ -0,0 +1,16 @@
+﻿CREATE VIEW [dbo].[vyuICExportProduct]
+	AS SELECT
+	CAST(item.strItemNo AS NVARCHAR(16)) code
+	, CAST(item.strDescription AS NVARCHAR(35)) name
+	, CAST(item.intItemId AS VARCHAR(16)) itemID
+	, CAST(0 AS NVARCHAR(16)) priceID
+	, CAST(0 AS NVARCHAR(8)) taxCode
+	, 0 aux1
+	, 0 aux2
+	, CAST(0 AS NVARCHAR(8)) fuelTypeCode
+	, 0 preOp
+	, 0 postOp
+FROM tblICItem item
+	LEFT OUTER JOIN tblICRinFuelCategory ftc ON ftc.intRinFuelCategoryId = item.intRINFuelTypeId
+	LEFT OUTER JOIN tblICFuelType ft ON ft.intRinFuelCategoryId = ft.intRinFuelCategoryId
+	LEFT OUTER JOIN tblICRinFuel rf ON rf.intRinFuelId = ft.intRinFuelId
