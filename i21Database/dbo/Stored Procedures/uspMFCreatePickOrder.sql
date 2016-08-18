a11c9ae55cc5493f1cea529d44e22bc42d8b724d
Premkumar S <prem.selladurai@irely.com>
2016-08-18 21:45:51 +0530

    MFG-1954

--- i21Database/dbo/Stored Procedures/uspMFCreatePickOrder.sql
+++ i21Database/dbo/Stored Procedures/uspMFCreatePickOrder.sql
@@ -22,13 +22,11 @@ BEGIN TRY
 		,@intManufacturingProcessId INT
 		,@intDayOfYear INT
 
-	EXEC sp_xml_preparedocument @idoc OUTPUT
-		,@strXML
-
 	SELECT @dtmCurrentDate = GetDate()
 
 	SELECT @intDayOfYear = DATEPART(dy, @dtmCurrentDate)
-
+	EXEC sp_xml_preparedocument @idoc OUTPUT
+		,@strXML
 	SELECT @intManufacturingProcessId = intManufacturingProcessId
 		,@intLocationId = x.intLocationId
 		,@intUserId = intUserId
@@ -90,41 +88,9 @@ BEGIN TRY
 		AND intLocationId = @intLocationId
 		AND intAttributeId = @intPackagingCategoryId
 
-	SELECT @intOwnerId = IO.intOwnerId
-	FROM dbo.tblMFWorkOrderConsumedLot WC
-	JOIN dbo.tblICItemOwner IO ON WC.intItemId = IO.intItemId
-	WHERE WC.intWorkOrderId IN (
-			SELECT x.intWorkOrderId
-			FROM OPENXML(@idoc, 'root/WorkOrders/WorkOrder', 2) WITH (intWorkOrderId INT) x
-			)
-
-	IF @intOwnerId IS NULL
-	BEGIN
-		SELECT @intOwnerId = IO.intOwnerId
-		FROM dbo.tblMFWorkOrder W
-		JOIN dbo.tblMFRecipe I ON I.intItemId = W.intItemId
-			AND I.intLocationId = @intLocationId
-		JOIN dbo.tblMFRecipeItem RI ON RI.intRecipeId = I.intRecipeId
-		JOIN dbo.tblICItemOwner IO ON RI.intItemId = IO.intItemId
-		WHERE W.intWorkOrderId IN (
-				SELECT x.intWorkOrderId
-				FROM OPENXML(@idoc, 'root/WorkOrders/WorkOrder', 2) WITH (intWorkOrderId INT) x
-				)
-	END
-
 	SELECT @intBlendProductionStagingUnitId = intBlendProductionStagingUnitId
 	FROM tblSMCompanyLocation
 	WHERE intCompanyLocationId = @intLocationId
-
-	SELECT @intEntityId = E.intEntityId
-	FROM dbo.tblEMEntity E
-	JOIN dbo.[tblEMEntityType] ET ON E.intEntityId = ET.intEntityId
-	WHERE ET.strType = 'Customer'
-		AND E.strName = 'Production'
-
-	SELECT @intOrderTermsId = intOrderTermsId
-	FROM tblWHOrderTerms
-	WHERE ysnDefault = 1
 
 	SELECT @strUserName = strUserName
 	FROM tblSMUserSecurity
@@ -141,67 +107,40 @@ BEGIN TRY
 		,@ysnProposed = 0
 		,@strPatternString = @strBOLNo OUTPUT
 
-	DECLARE @tblWHOrderHeader TABLE (intOrderHeaderId INT)
-
-	IF @intOwnerId IS NULL
-	BEGIN
-		SELECT @strItemNo = I.strItemNo
-		FROM dbo.tblMFWorkOrderConsumedLot WC
-		JOIN dbo.tblICItem I ON I.intItemId = WC.intItemId
-		WHERE WC.intWorkOrderId IN (
-				SELECT x.intWorkOrderId
-				FROM OPENXML(@idoc, 'root/WorkOrders/WorkOrder', 2) WITH (intWorkOrderId INT) x
-				)
-
-		RAISERROR (
-				90005
-				,14
-				,1
-				,@strItemNo
-				)
-	END
-
-	SELECT @strXML = '<root>'
-
-	SELECT @strXML += '<intOrderStatusId>1</intOrderStatusId>'
-
-	SELECT @strXML += '<intOrderTypeId>6</intOrderTypeId>'
-
-	SELECT @strXML += '<intOrderDirectionId>2</intOrderDirectionId>'
-
-	SELECT @strXML += '<strBOLNo>' + @strBOLNo + '</strBOLNo>'
-
-	SELECT @strXML += '<dtmRAD>' + LTRIM(@dtmCurrentDate) + '</dtmRAD>'
-
-	SELECT @strXML += '<intOwnerAddressId>' + LTRIM(@intOwnerId) + '</intOwnerAddressId>'
-
-	SELECT @strXML += '<intStagingLocationId>' + LTRIM(@intBlendProductionStagingUnitId) + '</intStagingLocationId>'
-
-	SELECT @strXML += '<intFreightTermId>' + LTRIM(@intOrderTermsId) + '</intFreightTermId>'
-
-	SELECT @strXML += '<intShipFromAddressId>' + LTRIM(@intLocationId) + '</intShipFromAddressId>'
-
-	SELECT @strXML += '<intShipToAddressId>' + LTRIM(@intEntityId) + '</intShipToAddressId>'
-
-	SELECT @strXML += '<strLastUpdateBy>' + LTRIM(@strUserName) + ' </strLastUpdateBy>'
-
-	SELECT @strXML += '</root>'
+	DECLARE @tblMFOrderHeader TABLE (intOrderHeaderId INT)
 
 	BEGIN TRANSACTION
 
-	INSERT INTO @tblWHOrderHeader
-	EXEC dbo.uspWHCreateOutboundOrder @strXML = @strXML
+	DECLARE @OrderHeaderInformation AS OrderHeaderInformation
+
+	INSERT INTO @OrderHeaderInformation (
+		intOrderStatusId
+		,intOrderTypeId
+		,intOrderDirectionId
+		,strOrderNo
+		,strReferenceNo
+		,intStagingLocationId
+		,strComment
+		,dtmOrderDate
+		,strLastUpdateBy
+		)
+	SELECT 1
+		,1
+		,2
+		,@strBOLNo
+		,''
+		,@intBlendProductionStagingUnitId
+		,''
+		,@dtmCurrentDate
+		,@strUserName
+
+	INSERT INTO @tblMFOrderHeader
+	EXEC dbo.uspMFCreateStagingOrder @OrderHeaderInformation = @OrderHeaderInformation
 
 	SELECT @intOrderHeaderId = intOrderHeaderId
-	FROM @tblWHOrderHeader
+	FROM @tblMFOrderHeader
 
-	UPDATE dbo.tblMFWorkOrder
-	SET intOrderHeaderId = @intOrderHeaderId
-		,strBOLNo = @strBOLNo
-	WHERE intWorkOrderId IN (
-			SELECT x.intWorkOrderId
-			FROM OPENXML(@idoc, 'root/WorkOrders/WorkOrder', 2) WITH (intWorkOrderId INT) x
-			)
+	DECLARE @OrderDetailInformation AS OrderDetailInformation
 
 	IF EXISTS (
 			SELECT *
@@ -210,64 +149,45 @@ BEGIN TRY
 			WHERE intTransactionFrom = 1
 			)
 	BEGIN
-		INSERT INTO tblWHOrderLineItem (
+		INSERT INTO @OrderDetailInformation (
 			intOrderHeaderId
 			,intItemId
 			,dblQty
-			,intReceiptQtyUOMId
-			,intLastUpdateId
-			,dtmLastUpdateOn
-			--,intPreferenceId
-			,dblRequiredQty
+			,intItemUOMId
+			,dblWeight
+			,intWeightUOMId
+			,dblWeightPerUnit
+			,intLotId
+			,strLotAlias
 			,intUnitsPerLayer
 			,intLayersPerPallet
-			,intLineNo
-			,dblPhysicalCount
-			,intPhysicalCountUOMId
-			,dblWeightPerUnit
-			,intWeightPerUnitUOMId
+			,intPreferenceId
 			,dtmProductionDate
-			,strLotAlias
-			--,intSanitizationOrderDetailsId
-			,intLotId
-			,intConcurrencyId
-			,ysnIsWeightCertified
+			,intLineNo
+			,intSanitizationOrderDetailsId
+			,strLineItemNote
 			)
 		SELECT @intOrderHeaderId
 			,CL.intItemId
-			,SUM(CL.dblIssuedQuantity)
-			,IU1.intUnitMeasureId
-			,CL.intCreatedUserId
-			,@dtmCurrentDate
-			--,(
-			--	SELECT TOP 1 intPickPreferenceId
-			--	FROM dbo.tblWHPickPreference
-			--	WHERE ysnDefault = 1
-			--	)
-			,SUM(CL.dblIssuedQuantity)
-			,ISNULL((
-					--SELECT MAX(intUnitPerLayer)
-					--FROM tblWHSKU S
-					--WHERE S.intLotId = CL.intLotId
-					NULL
-					), I.intUnitPerLayer)
-			,ISNULL((
-					--SELECT MAX(intLayerPerPallet)
-					--FROM tblWHSKU S1
-					--WHERE S1.intLotId = CL.intLotId
-					NULL
-					), I.intLayerPerPallet)
-			,intSequenceNo
-			,SUM(CL.dblIssuedQuantity)
-			,IU1.intUnitMeasureId
+			,SUm(L.dblQty)
+			,L.intItemUOMId
+			,SUm(L.dblWeight)
+			,L.intWeightUOMId
 			,L.dblWeightPerQty
-			,IU.intUnitMeasureId
-			,@dtmCurrentDate
-			,L.strLotAlias
-			--,CL.intWorkOrderInputLotId
 			,L.intLotId
-			,1
-			,1
+			,L.strLotAlias
+			,I.intUnitPerLayer
+			,intLayerPerPallet
+			,(
+				SELECT TOP 1 intPickListPreferenceId
+				FROM tblMFPickListPreference
+				)
+			,L.dtmDateCreated
+			,Row_Number() OVER (
+				ORDER BY CL.intItemId
+				)
+			,NULL
+			,''
 		FROM dbo.tblMFWorkOrderConsumedLot CL
 		JOIN dbo.tblICLot L ON L.intLotId = CL.intLotId
 		JOIN dbo.tblICItem I ON I.intItemId = CL.intItemId
@@ -278,56 +198,31 @@ BEGIN TRY
 				FROM OPENXML(@idoc, 'root/WorkOrders/WorkOrder', 2) WITH (intWorkOrderId INT) x
 				)
 		GROUP BY CL.intItemId
-			,IU1.intUnitMeasureId
-			,CL.intCreatedUserId
-			--,CL.dtmCreated
-			--,(
-			--	SELECT TOP 1 intPickPreferenceId
-			--	FROM dbo.tblWHPickPreference
-			--	WHERE ysnDefault = 1
-			--	)
-			,ISNULL((
-					--SELECT MAX(intUnitPerLayer)
-					--FROM tblWHSKU S
-					--WHERE S.intLotId = CL.intLotId
-					NULL
-					), I.intUnitPerLayer)
-			,ISNULL((
-					--SELECT MAX(intLayerPerPallet)
-					--FROM tblWHSKU S1
-					--WHERE S1.intLotId = CL.intLotId
-					NULL
-					), I.intLayerPerPallet)
-			,intSequenceNo
-			,IU1.intUnitMeasureId
+			,L.intItemUOMId
+			,L.intWeightUOMId
 			,L.dblWeightPerQty
-			,IU.intUnitMeasureId
-			,L.strLotAlias
-			--,CL.intWorkOrderInputLotId
 			,L.intLotId
+			,L.strLotAlias
+			,I.intUnitPerLayer
+			,intLayerPerPallet
+			,L.dtmDateCreated
 	END
 	ELSE
 	BEGIN
-		INSERT INTO tblWHOrderLineItem (
+		INSERT INTO @OrderDetailInformation (
 			intOrderHeaderId
 			,intItemId
 			,dblQty
-			,intReceiptQtyUOMId
-			,intLastUpdateId
-			,dtmLastUpdateOn
-			,dblRequiredQty
+			,intItemUOMId
+			,dblWeight
+			,intWeightUOMId
+			,dblWeightPerUnit
 			,intUnitsPerLayer
 			,intLayersPerPallet
+			,intPreferenceId
 			,intLineNo
-			,dblPhysicalCount
-			,intPhysicalCountUOMId
-			,dblWeightPerUnit
-			,intWeightPerUnitUOMId
-			,dtmProductionDate
-			,strLotAlias
-			,intLotId
-			,intConcurrencyId
-			,ysnIsWeightCertified
+			,intSanitizationOrderDetailsId
+			,strLineItemNote
 			)
 		SELECT @intOrderHeaderId
 			,ri.intItemId
@@ -336,32 +231,25 @@ BEGIN TRY
 					THEN CAST(CEILING((ri.dblCalculatedQuantity * (W.dblPlannedQty / r.dblQuantity))) AS NUMERIC(38, 20))
 				ELSE (ri.dblCalculatedQuantity * (W.dblPlannedQty / r.dblQuantity))
 				END
-			,IU.intUnitMeasureId
-			,@intUserId
-			,@dtmCurrentDate
+			,ri.intItemUOMId
 			,CASE 
 				WHEN C.strCategoryCode = @strPackagingCategory
 					THEN CAST(CEILING((ri.dblCalculatedQuantity * (W.dblPlannedQty / r.dblQuantity))) AS NUMERIC(38, 20))
 				ELSE (ri.dblCalculatedQuantity * (W.dblPlannedQty / r.dblQuantity))
 				END
+			,ri.intItemUOMId
+			,IU.dblUnitQty
 			,ISNULL(NULL, I.intUnitPerLayer)
 			,ISNULL(NULL, I.intLayerPerPallet)
-			,ROW_NUMBER() OVER (
-				ORDER BY ri.intItemId
+			,(
+				SELECT TOP 1 intPickListPreferenceId
+				FROM tblMFPickListPreference
 				)
-			,CASE 
-				WHEN C.strCategoryCode = @strPackagingCategory
-					THEN CAST(CEILING((ri.dblCalculatedQuantity * (W.dblPlannedQty / r.dblQuantity))) AS NUMERIC(38, 20))
-				ELSE (ri.dblCalculatedQuantity * (W.dblPlannedQty / r.dblQuantity))
-				END
-			,IU.intUnitMeasureId
-			,1
-			,IU.intUnitMeasureId
-			,@dtmCurrentDate
+			,Row_Number() OVER (
+				ORDER BY ri.intRecipeItemId
+				)
 			,NULL
-			,NULL
-			,1
-			,1
+			,''
 		FROM dbo.tblMFRecipeItem ri
 		JOIN dbo.tblMFRecipe r ON r.intRecipeId = ri.intRecipeId
 		JOIN dbo.tblICItem I ON I.intItemId = ri.intItemId
@@ -383,6 +271,8 @@ BEGIN TRY
 					)
 				)
 	END
+
+	EXEC dbo.uspMFCreateStagingOrderDetail @OrderDetailInformation =@OrderDetailInformation
 
 	INSERT INTO tblMFStageWorkOrder (
 		intWorkOrderId
@@ -407,7 +297,6 @@ BEGIN TRY
 	FROM @tblMFWorkOrder
 
 	COMMIT TRANSACTION
-
 	EXEC sp_xml_removedocument @idoc
 END TRY
 
@@ -417,7 +306,7 @@ BEGIN CATCH
 	IF XACT_STATE() != 0
 		ROLLBACK TRANSACTION
 
-	IF @idoc <> 0
+		IF @idoc <> 0
 		EXEC sp_xml_removedocument @idoc
 
 	RAISERROR (
