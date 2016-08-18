583426c911133c4f9959e578764583bace5d35e5
Liz Dorado <liz.dorado@irely.com>
2016-08-18 15:40:51 +0800

    ST-282

--- i21Database/dbo/Views/vyuSTPromotionItemLists.sql
+++ i21Database/dbo/Views/vyuSTPromotionItemLists.sql
@@ -6,12 +6,13 @@ adj3.strLocationName,
 adj1.strUpcCode,
 adj1.strLongUPCCode,
 adj1.intItemUOMId,
-adj5.strDescription,
+adj5.strItemNo,
+adj5.strDescription as strPumpItemDescription,
 adj6.intCategoryId,
 adj6.strCategoryCode,
 adj6.strDescription as categoryDesc,
 adj5.ysnFuelItem,
-adj4.dblSalePrice,
+adj4.dblSalePrice as dblPrice,
 adj2.intFamilyId,
 adj2.intClassId
 from tblICItemUOM adj1 JOIN tblICItemLocation adj2 ON adj1.intItemId = adj2.intItemId
@@ -19,6 +20,5 @@ JOIN tblSMCompanyLocation adj3 ON adj3.intCompanyLocationId = adj2.intLocationId
 JOIN tblICItemPricing adj4 on adj4.intItemLocationId = adj2.intItemLocationId
 JOIN tblICItem adj5 ON adj5.intItemId = adj1.intItemId  
 JOIN tblICCategory adj6 ON adj5.intCategoryId = adj6.intCategoryId
-and (adj1.strUpcCode IS NOT NULL OR  adj1.strLongUPCCode  IS NOT NULL)
 and adj5.strType = 'Inventory' and adj5.strStatus = 'Active'
 
