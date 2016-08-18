81fc77a5050c3fa2d7877ce790cae9750fdbca79
Wendell Wayne Estrada <wendell.estrada@irely.com>
2016-08-18 17:31:42 +0800

    IC-2464 Bin Visualization - Show average discount schedule reading in the grid.

--- i21Database/dbo/Views/vyuICGetStorageBinDetails.sql
+++ i21Database/dbo/Views/vyuICGetStorageBinDetails.sql
@@ -7,6 +7,11 @@ SELECT storageLocation.intStorageLocationId, stockUOM.intItemId, companyLocation
 	, CAST(SUM(stockUOM.dblOnHand + stockUOM.dblUnitStorage) AS NUMERIC(16, 8)) dblStock
 	, CAST(storageLocation.dblEffectiveDepth * storageLocation.dblPackFactor * storageLocation.dblUnitPerFoot AS NUMERIC(16, 8)) dblCapacity
 	, CAST(dbo.fnMaxNumeric(storageLocation.dblEffectiveDepth * storageLocation.dblPackFactor * storageLocation.dblUnitPerFoot - dbo.fnMaxNumeric(summary.dblStock, 0), 0) AS NUMERIC(16, 8)) dblAvailable
+	, commodity.strCommodityCode
+	, CAST(ISNULL(mrc.dblAirSpaceReading, 0) AS NUMERIC(16, 8)) dblAirSpaceReading
+	, CAST(((storageLocation.dblEffectiveDepth - ISNULL(mrc.dblAirSpaceReading, 0)) * storageLocation.dblUnitPerFoot * storageLocation.dblPackFactor) + storageLocation.dblResidualUnit AS NUMERIC(16, 8)) dblPhysicalReading
+	, CAST(SUM(stockUOM.dblOnHand + stockUOM.dblUnitStorage) - ((storageLocation.dblEffectiveDepth - ISNULL(mrc.dblAirSpaceReading, 0)) * storageLocation.dblUnitPerFoot * storageLocation.dblPackFactor) + storageLocation.dblResidualUnit AS NUMERIC(16, 8)) dblStockVariance,
+	grd.strDiscountId strDiscountCode, grd.strDiscountDescription, smr.dtmDate [dtmReadingDate]
 FROM tblICItemStockUOM stockUOM
 	INNER JOIN tblICItem item ON item.intItemId = stockUOM.intItemId
 	INNER JOIN tblICItemUOM itemUOM ON itemUOM.intItemUOMId = stockUOM.intItemUOMId
@@ -20,7 +25,15 @@ FROM tblICItemStockUOM stockUOM
 			INNER JOIN tblICStorageLocation storageLocation ON storageLocation.intStorageLocationId = stockUOM.intStorageLocationId
 		GROUP BY storageLocation.intStorageLocationId
 	) summary ON summary.intStorageLocationId = storageLocation.intStorageLocationId
+	LEFT OUTER JOIN tblICCommodity commodity ON commodity.intCommodityId = item.intCommodityId
+	LEFT OUTER JOIN tblICStorageMeasurementReadingConversion mrc ON mrc.intCommodityId = commodity.intCommodityId
+		AND mrc.intStorageLocationId = storageLocation.intStorageLocationId
+		AND mrc.intItemId = item.intItemId
+	LEFT OUTER JOIN tblICStorageMeasurementReading smr ON smr.intLocationId = companyLocation.intCompanyLocationId
+	LEFT OUTER JOIN tblGRDiscountId grd ON grd.intDiscountId = mrc.intDiscountSchedule
 GROUP BY storageLocation.intStorageLocationId, stockUOM.intItemId,
 	companyLocation.intCompanyLocationId, storageLocation.strName, companyLocation.strLocationName,
 	item.strItemNo, item.strDescription, storageLocation.dblEffectiveDepth, stockUOM.intItemLocationId,
-	storageLocation.dblPackFactor, storageLocation.dblUnitPerFoot, summary.dblStock, unitMeasure.strUnitMeasure
+	storageLocation.dblPackFactor, storageLocation.dblUnitPerFoot, summary.dblStock, unitMeasure.strUnitMeasure,
+	commodity.strCommodityCode, mrc.dblAirSpaceReading, storageLocation.dblResidualUnit, grd.strDiscountId, grd.strDiscountDescription,
+	smr.dtmDate
