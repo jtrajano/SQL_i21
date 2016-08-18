0de5cad2bb3fd367a32d2a4ef0cb8c915b4d94d1
Mon Gonzales <mon.gonzales@irely.com>
2016-08-18 13:22:45 +0800

    EM-1366

--- i21PreDeployment/Scripts/Pre-Deployment/12_EM/16_CleanCustomerProductVersion.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/12_EM/16_CleanCustomerProductVersion.sql
@@ -7,7 +7,7 @@ BEGIN
 	 AND EXISTS(SELECT TOP 1 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE [TABLE_NAME] = 'tblARCustomer' and [COLUMN_NAME] = 'intEntityCustomerId')
 	BEGIN
 		PRINT 'CLEAN CUSTOMER PRODUCT VERSION'
-		EXEC('UPDATE tblARCustomerProductVersion set intCustomerId = null where intCustomerId not in (select intEntityCustomerId from tblARCustomer)')
+		EXEC('DELETE tblARCustomerProductVersion where intCustomerId not in (select intEntityCustomerId from tblARCustomer)')
 	END
 
 	
