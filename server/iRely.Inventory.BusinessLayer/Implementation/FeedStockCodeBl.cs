db3c5e6ec6bc99e7179d4dc4b027443d74af1e22
Wendell Wayne Estrada <wendell.estrada@irely.com>
2016-08-19 11:39:18 +0800

    IC-2007 Feed Stock - Duplicate Error Message

--- server/iRely.Inventory.BusinessLayer/Implementation/FeedStockCodeBl.cs
+++ server/iRely.Inventory.BusinessLayer/Implementation/FeedStockCodeBl.cs
@@ -19,5 +19,29 @@ namespace iRely.Inventory.BusinessLayer
             _db = db;
         }
         #endregion
+        public override async Task<BusinessResult<tblICRinFeedStock>> SaveAsync(bool continueOnConflict)
+        {
+            SaveResult result = new SaveResult();
+
+            result = await _db.SaveAsync(continueOnConflict).ConfigureAwait(false);
+            if (result.HasError)
+            {
+                if (result.Exception.Message.Contains("{0} already exists!"))
+                {
+                    result.HasError = true;
+                    result.Exception = new ServerException(new Exception("Feed Stock must be unique"));
+                }
+            }
+            return new BusinessResult<tblICRinFeedStock>()
+            {
+                success = !result.HasError,
+                message = new MessageResult()
+                {
+                    statusText = result.Exception.Message,
+                    status = result.Exception.Error,
+                    button = result.Exception.Button.ToString()
+                }
+            };
+        }
     }
 }
--- server/iRely.Inventory.BusinessLayer/Implementation/FuelCodeBl.cs
+++ server/iRely.Inventory.BusinessLayer/Implementation/FuelCodeBl.cs
@@ -19,5 +19,30 @@ namespace iRely.Inventory.BusinessLayer
             _db = db;
         }
         #endregion
+
+        public override async Task<BusinessResult<tblICRinFuel>> SaveAsync(bool continueOnConflict)
+        {
+            SaveResult result = new SaveResult();
+
+            result = await _db.SaveAsync(continueOnConflict).ConfigureAwait(false);
+            if (result.HasError)
+            {
+                if(result.Exception.Message.Contains("{0} already exists!"))
+                {
+                    result.HasError = true;
+                    result.Exception = new ServerException(new Exception("Fuel Code must be unique"));
+                }
+            }
+            return new BusinessResult<tblICRinFuel>()
+            {
+                success = !result.HasError,
+                message = new MessageResult()
+                {
+                    statusText = result.Exception.Message,
+                    status = result.Exception.Error,
+                    button = result.Exception.Button.ToString()
+                }
+            };
+        }
     }
 }
