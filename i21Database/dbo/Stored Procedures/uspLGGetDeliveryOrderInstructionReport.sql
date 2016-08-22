9cdb3bd7223ba543da0f42e75268be78caabdef3
Swetosuvro Ghosh <swetosuvro.g@irely.com>
2016-08-22 16:34:22 +0530

    LG-431

---
+++ i21Database/dbo/Stored Procedures/uspLGGetDeliveryOrderInstructionReport.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE uspLGGetDeliveryOrderInstructionReport 
+					@strLoadNumber NVARCHAR(100)
+AS
+SELECT L.strLoadNumber
+	,L.intLoadId
+	,LWS.strCategory
+	,LWS.strActivity
+FROM tblLGLoad L
+JOIN tblLGLoadWarehouse LW ON L.intLoadId = LW.intLoadId
+JOIN tblLGLoadWarehouseServices LWS ON LWS.intLoadWarehouseId = LW.intLoadWarehouseId
+WHERE L.strLoadNumber = @strLoadNumber
