286d11556130a279b7becc4bb7d454e2cc0d09de
James Balachander <balachander.j@irely.com>
2016-08-22 19:32:33 +0530

    LG-503

---
+++ i21Database/dbo/Tables/tblLGRouteDirection.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[tblLGRouteDirection]
+(
+	[intRouteDirectionId] INT NOT NULL IDENTITY(1, 1), 
+    [intConcurrencyId] INT NOT NULL, 
+	[intRouteId] INT NOT NULL, 	
+	[intSequence] INT NULL, 	
+	[strOrigin] [NVARCHAR](MAX) COLLATE Latin1_General_CI_AS NULL,
+	[strDestination] [NVARCHAR](MAX) COLLATE Latin1_General_CI_AS NULL,
+	[strDirection] [NVARCHAR](MAX) COLLATE Latin1_General_CI_AS NULL,
+	[intSort] INT NULL, 
+	
+    CONSTRAINT [PK_tblLGRouteDirection] PRIMARY KEY ([intRouteDirectionId]),
+    CONSTRAINT [FK_tblLGRouteDirection_tblLGRoute_intRouteId] FOREIGN KEY ([intRouteId]) REFERENCES [tblLGRoute]([intRouteId]) ON DELETE CASCADE
+)
