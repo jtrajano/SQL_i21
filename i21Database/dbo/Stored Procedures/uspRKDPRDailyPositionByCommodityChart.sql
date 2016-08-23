94f96195c3e2c222dce07b652554a4047ce2a572
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2016-08-23 21:03:18 +0530

    RM-454

---
+++ i21Database/dbo/Stored Procedures/uspRKDPRDailyPositionByCommodityChart.sql
@@ -0,0 +1,36 @@
+﻿CREATE PROC [dbo].[uspRKDPRDailyPositionByCommodityChart] 
+	 @intCommodityId int 
+	,@intVendorId int = null
+AS
+
+DECLARE @tblFinalDetail TABLE (
+	intRowNum int,
+	strLocationName NVARCHAR(500) COLLATE Latin1_General_CI_AS,
+	intLocationId int
+	,intCommodityId INT
+	,strCommodityCode NVARCHAR(50) COLLATE Latin1_General_CI_AS
+	,strUnitMeasure NVARCHAR(50) COLLATE Latin1_General_CI_AS	
+	,[Open Purchase] decimal(24,10)
+	,[Open Sale]  decimal(24,10)	
+	,[Company Titled] decimal(24,10)
+	,[Case Exposure] decimal(24,10)
+	,[Open Sales] decimal(24,10)
+	,[Avail For Sale] decimal(24,10)
+	,[In House] decimal(24,10)
+	,[Basis Exposure] decimal(24,10)
+	)
+
+
+INSERT INTO @tblFinalDetail
+EXEC uspRKDPRDailyPositionByCommodityLocation @intCommodityId =  @intCommodityId,@intVendorId=@intVendorId
+
+SELECT strTransactionType
+ ,sum(dblTransactionQuantity) dblTransactionQuantity
+FROM @tblFinalDetail 
+UNPIVOT(dblTransactionQuantity FOR strTransactionType IN (
+	 [Company Titled]
+	,[Case Exposure]
+	,[Basis Exposure] 
+	,[Avail For Sale] 	
+	,[In House] 	
+   )) AS UnPvt group by strTransactionType 
--- i21Database/dbo/Views/vyuRKGetInventoryAdjustQty.sql
+++ i21Database/dbo/Views/vyuRKGetInventoryAdjustQty.sql
@@ -2,10 +2,10 @@
 
 AS
 
-SELECT isnull(dblBalance,0)+(isnull(dblQuantity,0)-(-isnull(dblAdjustByQuantity,0))) as dblDetailQuantity,isnull(dblBalance,0) dblBalance,intContractDetailId FROM (
+SELECT DISTINCT ISNULL(dblBalance,0)+(isnull(dblQuantity,0)-(-isnull(dblAdjustByQuantity,0))) as dblDetailQuantity,isnull(dblBalance,0) dblBalance,intContractDetailId FROM (
 SELECT cd.dblBalance,il.dblQuantity,il.intLotId,intContractDetailId,
-		isnull((select sum(ad.dblAdjustByQuantity) dblAdjustByQuantity from tblICInventoryAdjustmentDetail ad where  ad.intLotId=il.intLotId),0) dblAdjustByQuantity
+		ISNULL((SELECT SUM(ad.dblAdjustByQuantity) dblAdjustByQuantity from tblICInventoryAdjustmentDetail ad where  ad.intLotId=il.intLotId),0) dblAdjustByQuantity
 FROM tblCTContractDetail cd
-JOIN tblICInventoryReceiptItem ri on cd.intContractDetailId=ri.intLineNo
-JOIN tblICInventoryReceiptItemLot il on ri.intInventoryReceiptItemId=il.intInventoryReceiptItemId 
+LEFT JOIN tblICInventoryReceiptItem ri on cd.intContractDetailId=ri.intLineNo
+LEFT JOIN tblICInventoryReceiptItemLot il on ri.intInventoryReceiptItemId=il.intInventoryReceiptItemId 
 )t
