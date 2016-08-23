f6e2895b3b8e04c4325051c88cbe43bf0fdcf7e4
Jb Mago <benedict.mago@irely.com>
2016-08-23 20:34:10 +0800

    FCOM-1589

---
+++ i21Database/dbo/Tables/tblSMFileDownloadDetail.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[tblSMFileDownloadDetail]
+(
+	[intFileDownloadDetailId] INT NOT NULL IDENTITY,
+	[intFileDownloadId] INT NOT NULL,
+	[strFile] NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL,
+	[strVersion] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL,
+	[intSize] INT NULL,
+	[dtmDateAdded] DATETIME NOT NULL,
+	[strDescription] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL,
+	[intConcurrencyId] INT NOT NULL DEFAULT 1,
+	CONSTRAINT [PK_tblSMFileDownloadDetail] PRIMARY KEY ([intFileDownloadDetailId]),
+	CONSTRAINT [FK_tblSMFileDownloadDetail_tblSMFileDownload] FOREIGN KEY ([intFileDownloadId]) REFERENCES [tblSMFileDownload]([intFileDownloadId]) ON DELETE CASCADE
+)
