b4d54b54b54b3c803cf76dafccd7755a7327fe2f
Arman Arenas <arman.arenas@irely.com>
2016-08-23 17:56:23 +0800

    PR-849 fixed update paycheck reference affecting other paychecks when committing

--- i21Database/dbo/Stored Procedures/uspPRUpdatePaycheckReferenceNo.sql
+++ i21Database/dbo/Stored Procedures/uspPRUpdatePaycheckReferenceNo.sql
@@ -2,13 +2,25 @@
 	@intBankAccountId INT = NULL
 AS
 
+/* Update Reference (Check) No*/
 UPDATE tblPRPaycheck 
 SET strReferenceNo = ISNULL((SELECT TOP 1 strReferenceNo FROM tblCMBankTransaction 
 						WHERE strTransactionId = tblPRPaycheck.strPaycheckId
-						  AND intBankTransactionTypeId IN (21, 23)
+						  AND intBankTransactionTypeId = 21
+						  AND strSourceSystem = 'PR'
 						  AND intBankAccountId = ISNULL(@intBankAccountId, intBankAccountId)
 						  AND dtmCheckPrinted IS NOT NULL), ''),
 	ysnPrinted = 1
-WHERE ISNULL(strReferenceNo, '') = ''
+WHERE ISNULL(strReferenceNo, '') = '' AND ysnPosted = 1 AND ysnVoid = 0
+
+/* Update Printed (Committed) Status*/
+UPDATE tblPRPaycheck 
+SET ysnPrinted = 1
+WHERE strPaycheckId IN (SELECT strTransactionId FROM tblCMBankTransaction
+						WHERE intBankAccountId = ISNULL(@intBankAccountId, intBankAccountId)
+							AND intBankTransactionTypeId = 23
+							AND strSourceSystem = 'PR'
+							AND dtmCheckPrinted IS NOT NULL)
+	  AND ysnPrinted = 0 AND ysnPosted = 1 AND ysnVoid = 0
 
 GO
