8f97c37ed221558aeab041f8d28b263c0cea0610
Ruben  Escarcha <ruben.escarcha@irely.com>
2016-08-23 21:28:40 +0800

    AR-3295 BASE View - Customer Accounting

---
+++ i21Database/dbo/Views/vyuARBEExportCustomerAccounting.sql
@@ -0,0 +1,13 @@
+﻿CREATE VIEW [dbo].[vyuARBEExportCustomerAccounting]
+AS 
+SELECT
+	 account			= ISNULL(ARCI.[strCustomerNumber], '')
+	,priceID			= ''
+	,balance			= ISNULL(ARC.[dblARBalance], 0.000000) 
+	,pastDue30			= ISNULL(ARCI.[dbl60Days], 0.000000)
+	,creditRating		= ISNULL(ARC.strCreditCode, '')
+FROM
+	vyuARCustomerInquiry ARCI
+INNER JOIN
+	tblARCustomer ARC
+		ON ARCI.[intEntityCustomerId] = ARC.[intEntityCustomerId]
