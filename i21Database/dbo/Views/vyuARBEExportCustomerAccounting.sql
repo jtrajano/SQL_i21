c98ac592f59466216f81b9617957c9eb2d2ec277
Ruben  Escarcha <ruben.escarcha@irely.com>
2016-08-24 11:34:28 +0800

    AR-3295 BASE View - Customer Accounting

--- i21Database/dbo/Views/vyuARBEExportCustomerAccounting.sql
+++ i21Database/dbo/Views/vyuARBEExportCustomerAccounting.sql
@@ -4,7 +4,7 @@ SELECT
 	 account			= ISNULL(ARCI.[strCustomerNumber], '')
 	,priceID			= ''
 	,balance			= ISNULL(ARC.[dblARBalance], 0.000000) 
-	,pastDue30			= ISNULL(ARCI.[dbl30Days], 0.000000) + ISNULL(ARCI.[dbl60Days], 0.000000) + ISNULL(ARCI.[dbl90Days], 0.000000) + ISNULL(ARCI.[dbl91Days], 0.000000)
+	,pastDue30			= ISNULL(ARCI.[dbl60Days], 0.000000) + ISNULL(ARCI.[dbl90Days], 0.000000) + ISNULL(ARCI.[dbl91Days], 0.000000)
 	,creditRating		= ISNULL(ARC.strCreditCode, '')
 FROM
 	vyuARCustomerInquiry ARCI
