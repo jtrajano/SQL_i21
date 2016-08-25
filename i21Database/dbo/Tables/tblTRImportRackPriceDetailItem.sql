f568e293c0772ac7f0020364d4019db8a7a513ee
Lawrence Zabala <lawrence.zabala@irely.com>
2016-08-25 18:37:13 +0800

    TR-583 TR-582

--- i21Database/dbo/Tables/tblTRImportRackPriceDetailItem.sql
+++ i21Database/dbo/Tables/tblTRImportRackPriceDetailItem.sql
@@ -7,7 +7,7 @@
     [dblVendorPrice] NUMERIC(18, 6) NULL DEFAULT ((0)), 
     [dblJobberPrice] NUMERIC(18, 6) NULL DEFAULT ((0)), 
     [strEquation] NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL, 
-	[ysnValid] BIT NULL DEFAULT ((1)),
+	[ysnValid] BIT NOT NULL DEFAULT ((0)),
     [intConcurrencyId] INT NULL DEFAULT ((0)), 
     CONSTRAINT [PK_tblTRImportRackPriceDetailItem] PRIMARY KEY ([intImportRackPriceDetailItemId]), 
     CONSTRAINT [FK_tblTRImportRackPriceDetailItem_tblTRImportRackPriceDetail] FOREIGN KEY ([intImportRackPriceDetailId]) REFERENCES [tblTRImportRackPriceDetail]([intImportRackPriceDetailId]) ON DELETE CASCADE
--- i21Database/dbo/Tables/tblTRRackPriceDetail.sql
+++ i21Database/dbo/Tables/tblTRRackPriceDetail.sql
@@ -8,5 +8,6 @@
 	[intConcurrencyId] INT NULL DEFAULT((0)),
 	CONSTRAINT [PK_tblTRRackPriceDetail] PRIMARY KEY ([intRackPriceDetailId]),
 	CONSTRAINT [FK_tblTRRackPriceDetail_tblTRRackPriceHeader_intRackPriceHeaderId] FOREIGN KEY ([intRackPriceHeaderId]) REFERENCES [dbo].[tblTRRackPriceHeader] ([intRackPriceHeaderId]) ON DELETE CASCADE,
-	CONSTRAINT [FK_tblTRRackPriceDetail_tblICItem_intItemId] FOREIGN KEY ([intItemId]) REFERENCES [dbo].[tblICItem] ([intItemId])	
+	CONSTRAINT [FK_tblTRRackPriceDetail_tblICItem_intItemId] FOREIGN KEY ([intItemId]) REFERENCES [dbo].[tblICItem] ([intItemId]), 
+    CONSTRAINT [AK_tblTRRackPriceDetail_intItemId] UNIQUE ([intRackPriceHeaderId], [intItemId])	
 )
