7ec0b67b549a2b2c849878261d296a5ce361f878
Mark Paragas <mark.paragas@irely.com>
2016-08-26 16:54:57 +0800

    MFT-257, MFT-244

--- i21Database/dbo/Tables/tblTFTaxReportTemplate.sql
+++ i21Database/dbo/Tables/tblTFTaxReportTemplate.sql
@@ -11,6 +11,8 @@
 	[strDescription] [nvarchar](MAX) COLLATE Latin1_General_CI_AS NOT NULL,
 	[strScheduleCode] [nvarchar](MAX) COLLATE Latin1_General_CI_AS NULL,
 	[strConfiguration] [nvarchar](50) COLLATE Latin1_General_CI_AS NULL,
+	[ysnDynamicConfiguration] BIT COLLATE Latin1_General_CI_AS NULL,
+	[strLastIndexOf] [nvarchar](10) COLLATE Latin1_General_CI_AS NULL,
 	[strSegment] [nvarchar](50) COLLATE Latin1_General_CI_AS NULL,
 	[intConfigurationSequence] [int] NULL,
 	[intConcurrencyId] INT NULL, 
