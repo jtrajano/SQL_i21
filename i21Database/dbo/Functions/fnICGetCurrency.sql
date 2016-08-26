d0541ce66689238bbb05c06e0f73ecef3289fb48
Joy Balangatan <joy.balangatan@irely.com>
2016-08-26 18:06:23 +0800

    IC-2632 Fixed the issue with SubCurrency

---
+++ i21Database/dbo/Functions/fnICGetCurrency.sql
@@ -0,0 +1,32 @@
+﻿CREATE FUNCTION [dbo].[fnICGetCurrency] (@contractDetailId INT, @ysnSubCurrency BIT)
+RETURNS INT
+AS
+BEGIN
+	DECLARE @defaultCurrency INT = NULL
+
+	IF @ysnSubCurrency=1
+		BEGIN
+			SELECT @defaultCurrency = 
+					CASE 
+						WHEN ContractDetail.ysnUseFXPrice = 1
+						THEN ContractDetail.intSeqCurrencyId			
+						ELSE ISNULL(ISNULL(ContractDetail.intCurrencyId, ContractDetail.intMainCurrencyId), dbo.fnSMGetDefaultCurrency('FUNCTIONAL'))
+					END
+			FROM vyuCTContractDetailView ContractDetail 
+			WHERE ContractDetail.intContractDetailId = @contractDetailId
+		END
+	ELSE
+		BEGIN
+			SELECT @defaultCurrency = 
+					CASE 
+						WHEN ContractDetail.ysnUseFXPrice = 1
+						THEN ContractDetail.intSeqCurrencyId			
+						ELSE ISNULL(ISNULL(ContractDetail.intMainCurrencyId, ContractDetail.intCurrencyId), dbo.fnSMGetDefaultCurrency('FUNCTIONAL'))
+					END
+			FROM vyuCTContractDetailView ContractDetail 
+			WHERE ContractDetail.intContractDetailId = @contractDetailId
+		END
+
+	
+	RETURN @defaultCurrency
+END
