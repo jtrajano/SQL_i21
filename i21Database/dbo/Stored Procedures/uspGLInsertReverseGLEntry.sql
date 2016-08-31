270fedacfaceb2601753e229f10f71b55d011878
jeffrey trajano <jeffrey.trajano@irely.com>
2016-08-31 16:25:27 +0800

    GL-2920 Ensure Reverse Entry Upon Unposting

---
+++ i21Database/dbo/Stored Procedures/uspGLInsertReverseGLEntry.sql
@@ -0,0 +1,73 @@
+﻿CREATE PROCEDURE uspGLInsertReverseGLEntry
+(	
+	@strTransactionId NVARCHAR(100),
+	@intEntityId INT,
+	@dtmDateReverse DATETIME = NULL
+)
+AS
+BEGIN
+	DECLARE @GLEntries RecapTableType
+		INSERT INTO @GLEntries (
+				 [strTransactionId]
+				,[intTransactionId]
+				,[dtmDate]
+				,[strBatchId]
+				,[intAccountId]
+				,[dblDebit]
+				,[dblCredit]
+				,[dblDebitUnit]
+				,[dblCreditUnit]
+				--,[dblDebitForeign]
+				--,[dblCreditForeign]
+				--,[dblForeignRate]
+				,[strDescription]
+				,[strCode]
+				,[strReference]
+				,[intCurrencyId]
+				,[dblExchangeRate]
+				,[dtmDateEntered]
+				,[dtmTransactionDate]
+				,[strJournalLineDescription]
+				,[intJournalLineNo]
+				,[ysnIsUnposted]
+				,[intConcurrencyId]
+				,[intUserId]
+				,[intEntityId]
+				,[strTransactionType]
+				,[strTransactionForm]
+				,[strModuleName]
+		)
+		SELECT	 [strTransactionId]
+				,[intTransactionId]
+				,dtmDate			= ISNULL(@dtmDateReverse, [dtmDate]) -- If date is provided, use date reverse as the date for unposting the transaction.
+				,[strBatchId]
+				,[intAccountId]
+				,dblDebit			= [dblCredit]		-- (Debit -> Credit)
+				,dblCredit			= [dblDebit]		-- (Debit <- Credit)
+				,dblDebitUnit		= [dblCreditUnit]	-- (Debit Unit -> Credit Unit)
+				,dblCreditUnit		= [dblDebitUnit]	-- (Debit Unit <- Credit Unit)
+				--,dblDebitForeign	= [dblCreditForeign]
+				--,dblCreditForeign	= [dblDebitForeign]
+				--,dblForeignRate		
+				,[strDescription]
+				,[strCode]
+				,[strReference]
+				,[intCurrencyId]
+				,[dblExchangeRate]
+				,dtmDateEntered		= GETDATE()
+				,[dtmTransactionDate]
+				,[strJournalLineDescription]
+				,[intJournalLineNo]
+				,ysnIsUnposted		= 1
+				,[intConcurrencyId]
+				,[intUserId]		= 0
+				,[intEntityId]		= @intEntityId
+				,[strTransactionType]
+				,[strTransactionForm]
+				,[strModuleName]
+		FROM	tblGLDetail 
+		WHERE	strTransactionId = @strTransactionId and ysnIsUnposted = 0
+		ORDER BY intGLDetailId		
+
+		EXEC uspGLBookEntries @GLEntries, 0
+END
--- i21Database/dbo/Stored Procedures/uspGLReverseGLEntries.sql
+++ i21Database/dbo/Stored Procedures/uspGLReverseGLEntries.sql
@@ -34,70 +34,7 @@ END
 IF ISNULL(@ysnRecap, 0) = 0
 	BEGIN			
 		DECLARE @GLEntries RecapTableType
-		INSERT INTO @GLEntries (
-				 [strTransactionId]
-				,[intTransactionId]
-				,[dtmDate]
-				,[strBatchId]
-				,[intAccountId]
-				,[dblDebit]
-				,[dblCredit]
-				,[dblDebitUnit]
-				,[dblCreditUnit]
-				--,[dblDebitForeign]
-				--,[dblCreditForeign]
-				--,[dblForeignRate]
-				,[strDescription]
-				,[strCode]
-				,[strReference]
-				,[intCurrencyId]
-				,[dblExchangeRate]
-				,[dtmDateEntered]
-				,[dtmTransactionDate]
-				,[strJournalLineDescription]
-				,[intJournalLineNo]
-				,[ysnIsUnposted]
-				,[intConcurrencyId]
-				,[intUserId]
-				,[intEntityId]
-				,[strTransactionType]
-				,[strTransactionForm]
-				,[strModuleName]
-		)
-		SELECT	 [strTransactionId]
-				,[intTransactionId]
-				,dtmDate			= ISNULL(@dtmDateReverse, [dtmDate]) -- If date is provided, use date reverse as the date for unposting the transaction.
-				,[strBatchId]
-				,[intAccountId]
-				,dblDebit			= [dblCredit]		-- (Debit -> Credit)
-				,dblCredit			= [dblDebit]		-- (Debit <- Credit)
-				,dblDebitUnit		= [dblCreditUnit]	-- (Debit Unit -> Credit Unit)
-				,dblCreditUnit		= [dblDebitUnit]	-- (Debit Unit <- Credit Unit)
-				--,dblDebitForeign	= [dblCreditForeign]
-				--,dblCreditForeign	= [dblDebitForeign]
-				--,dblForeignRate		
-				,[strDescription]
-				,[strCode]
-				,[strReference]
-				,[intCurrencyId]
-				,[dblExchangeRate]
-				,dtmDateEntered		= GETDATE()
-				,[dtmTransactionDate]
-				,[strJournalLineDescription]
-				,[intJournalLineNo]
-				,ysnIsUnposted		= 1
-				,[intConcurrencyId]
-				,[intUserId]		= 0
-				,[intEntityId]		= @intEntityId
-				,[strTransactionType]
-				,[strTransactionForm]
-				,[strModuleName]
-		FROM	tblGLDetail 
-		WHERE	strTransactionId = @strTransactionId and ysnIsUnposted = 0
-		ORDER BY intGLDetailId		
-
-		EXEC uspGLBookEntries @GLEntries, 0
-
+		EXEC uspGLInsertReverseGLEntry @strTransactionId,@intEntityId,@dtmReverse
 		IF @@ERROR <> 0	GOTO Post_Rollback;
 	END
 ELSE
