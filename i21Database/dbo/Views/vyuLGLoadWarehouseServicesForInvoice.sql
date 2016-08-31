c0e435ef6602f0ddcdca0f9f2ab82dffbcbad88f
Swetosuvro Ghosh <swetosuvro.g@irely.com>
2016-08-31 17:11:15 +0530

    LG-566

--- i21Database/dbo/Views/vyuLGLoadWarehouseServicesForInvoice.sql
+++ i21Database/dbo/Views/vyuLGLoadWarehouseServicesForInvoice.sql
@@ -1,51 +1,61 @@
 ﻿CREATE VIEW vyuLGLoadWarehouseServicesForInvoice
 AS 
-	  SELECT [strTransactionType] = 'Load Schedule'
-			,[strTransactionNumber] = L.[strLoadNumber]
-			,[strShippedItemId] = 'ld:' + CAST(LD.intLoadDetailId AS NVARCHAR(250))
-			,[intEntityCustomerId] = LD.intCustomerEntityId
-			,[strCustomerName] = EME.[strName]
-			,[intCurrencyId] = ISNULL(ISNULL(CD.[intCurrencyId], ARC.[intCurrencyId]), (
-					SELECT TOP 1 intDefaultCurrencyId
-					FROM tblSMCompanyPreference
-					WHERE intDefaultCurrencyId IS NOT NULL
-						AND intDefaultCurrencyId <> 0
-					))
-			,[dtmProcessDate] = L.dtmScheduledDate
-			,L.intLoadId
-			,LD.intLoadDetailId
-			,L.[strLoadNumber]
-			,[intContractHeaderId] = ISNULL(CH.[intContractHeaderId], CD.[intContractHeaderId])
-			,[strContractNumber] = CH.strContractNumber
-			,[intContractDetailId] = ISNULL(CD.[intContractDetailId], LD.[intPContractDetailId])
-			,[intContractSeq] = CD.[intContractSeq]
-			,[intCompanyLocationId] = LD.intSCompanyLocationId
-			,[strLocationName] = SMCL.[strLocationName]
-			,[intItemId] = ICI.[intItemId]
-			,[strItemNo] = ICI.[strItemNo]
-			,[strItemDescription] = CASE 
-				WHEN ISNULL(ICI.[strDescription], '') = ''
-					THEN ICI.[strItemNo]
-				ELSE ICI.[strDescription]
-				END
-			,[intShipmentItemUOMId] = LD.[intItemUOMId]
-			,[dblPrice] = (Sum(LWS.dblBillAmount) / (
-						SELECT SUM(dblNet)
-						FROM tblLGLoadDetail D
-						WHERE L.intLoadId = D.intLoadId) * SUM(LD.dblNet))
-			,[dblShipmentUnitPrice] = (Sum(LWS.dblBillAmount) / (
-						SELECT SUM(dblNet)
-						FROM tblLGLoadDetail D
-						WHERE L.intLoadId = D.intLoadId) * SUM(LD.dblNet))
-			,[dblTotal] = (Sum(LWS.dblBillAmount) / (
-						SELECT SUM(dblNet)
-						FROM tblLGLoadDetail D
-						WHERE L.intLoadId = D.intLoadId) * SUM(LD.dblNet))
-			,[intAccountId] = ARIA.[intAccountId]
-			,[intCOGSAccountId] = ARIA.[intCOGSAccountId]
-			,[intSalesAccountId] = ARIA.[intSalesAccountId]
-			,[intInventoryAccountId] = ARIA.[intInventoryAccountId]
-			,[ysnPosted] = L.ysnPosted
+SELECT * FROM (
+	SELECT [strTransactionType] = 'Load Schedule'
+		,[strTransactionNumber] = L.[strLoadNumber]
+		,[strShippedItemId] = 'ld:' + CAST(LD.intLoadDetailId AS NVARCHAR(250))
+		,[intEntityCustomerId] = LD.intCustomerEntityId
+		,[strCustomerName] = EME.[strName]
+		,[intCurrencyId] = ISNULL(ISNULL(CD.[intCurrencyId], ARC.[intCurrencyId]), (
+				SELECT TOP 1 intDefaultCurrencyId
+				FROM tblSMCompanyPreference
+				WHERE intDefaultCurrencyId IS NOT NULL
+					AND intDefaultCurrencyId <> 0
+				))
+		,[dtmProcessDate] = L.dtmScheduledDate
+		,L.intLoadId
+		,LD.intLoadDetailId
+		,L.[strLoadNumber]
+		,[intContractHeaderId] = ISNULL(CH.[intContractHeaderId], CD.[intContractHeaderId])
+		,[strContractNumber] = CH.strContractNumber
+		,[intContractDetailId] = ISNULL(CD.[intContractDetailId], LD.[intPContractDetailId])
+		,[intContractSeq] = CD.[intContractSeq]
+		,[intCompanyLocationId] = LD.intSCompanyLocationId
+		,[strLocationName] = SMCL.[strLocationName]
+		,[intItemId] = ICI.[intItemId]
+		,[strItemNo] = ICI.[strItemNo]
+		,[strItemDescription] = CASE 
+			WHEN ISNULL(ICI.[strDescription], '') = ''
+				THEN ICI.[strItemNo]
+			ELSE ICI.[strDescription]
+			END
+		,[intShipmentItemUOMId] = NULL
+		,[dblPrice] = (
+			Sum(LWS.dblBillAmount) / (
+				SELECT SUM(dblNet)
+				FROM tblLGLoadDetail D
+				WHERE L.intLoadId = D.intLoadId
+				) * SUM(LD.dblNet)
+			)
+		,[dblShipmentUnitPrice] = (
+			Sum(LWS.dblBillAmount) / (
+				SELECT SUM(dblNet)
+				FROM tblLGLoadDetail D
+				WHERE L.intLoadId = D.intLoadId
+				) * SUM(LD.dblNet)
+			)
+		,[dblTotal] = (
+			Sum(LWS.dblBillAmount) / (
+				SELECT SUM(dblNet)
+				FROM tblLGLoadDetail D
+				WHERE L.intLoadId = D.intLoadId
+				) * SUM(LD.dblNet)
+			)
+		,[intAccountId] = ARIA.[intAccountId]
+		,[intCOGSAccountId] = ARIA.[intCOGSAccountId]
+		,[intSalesAccountId] = ARIA.[intSalesAccountId]
+		,[intInventoryAccountId] = ARIA.[intInventoryAccountId]
+		,[ysnPosted] = L.ysnPosted
 	FROM tblLGLoad L
 	JOIN tblLGLoadDetail LD ON L.intLoadId = LD.intLoadId
 	JOIN tblARCustomer ARC ON LD.intCustomerEntityId = ARC.intEntityCustomerId
@@ -59,13 +69,41 @@ AS
 		AND LD.intSCompanyLocationId = ARIA.[intLocationId]
 	LEFT JOIN tblARInvoiceDetail ARID ON LD.intLoadDetailId = ARID.[intInventoryShipmentItemId]
 	LEFT JOIN [tblSMCompanyLocation] SMCL ON LD.intSCompanyLocationId = SMCL.[intCompanyLocationId]
-	GROUP BY LWS.intLoadWarehouseServicesId, L.[strLoadNumber],LD.intLoadDetailId,EME.[strName],CD.intCurrencyId,
-			 L.dtmScheduledDate,L.intLoadId,SMCL.[strLocationName],ICI.strItemNo,
-			 ICI.strDescription,CD.intContractHeaderId,CH.strContractNumber,
-			 LD.intPContractDetailId,CD.intContractSeq,LD.intItemUOMId,
-			 ARC.[intCurrencyId],ARC.[intSalespersonId],LD.intCustomerEntityId,
-			 LD.intSCompanyLocationId,CH.intTermId,CD.intFreightTermId,
-			 CD.intShipViaId,ICI.intItemId,CD.intItemUOMId,CD.dblQuantity,
-			 LD.dblQuantity,CH.intContractHeaderId,CD.intContractDetailId,
-			 CD.dblCashPrice,LD.[intWeightItemUOMId],ARIA.[intAccountId],
-			 ARIA.[intCOGSAccountId],ARIA.[intSalesAccountId],ARIA.[intInventoryAccountId],L.ysnPosted,LWS.dblUnitRate
+	GROUP BY LWS.intLoadWarehouseServicesId
+		,L.[strLoadNumber]
+		,LD.intLoadDetailId
+		,EME.[strName]
+		,CD.intCurrencyId
+		,L.dtmScheduledDate
+		,L.intLoadId
+		,SMCL.[strLocationName]
+		,ICI.strItemNo
+		,ICI.strDescription
+		,CD.intContractHeaderId
+		,CH.strContractNumber
+		,LD.intPContractDetailId
+		,CD.intContractSeq
+		,LD.intItemUOMId
+		,ARC.[intCurrencyId]
+		,ARC.[intSalespersonId]
+		,LD.intCustomerEntityId
+		,LD.intSCompanyLocationId
+		,CH.intTermId
+		,CD.intFreightTermId
+		,CD.intShipViaId
+		,ICI.intItemId
+		,CD.intItemUOMId
+		,CD.dblQuantity
+		,LD.dblQuantity
+		,CH.intContractHeaderId
+		,CD.intContractDetailId
+		,CD.dblCashPrice
+		,LD.[intWeightItemUOMId]
+		,ARIA.[intAccountId]
+		,ARIA.[intCOGSAccountId]
+		,ARIA.[intSalesAccountId]
+		,ARIA.[intInventoryAccountId]
+		,L.ysnPosted
+		,LWS.dblUnitRate
+	) tbl
+WHERE dblTotal > 0
