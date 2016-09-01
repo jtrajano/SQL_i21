220c05a06df489f2350b50552943b342478780d0
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2016-09-01 16:42:46 +0530

    RM-450

--- i21Database/dbo/Stored Procedures/uspRKDPRDailyPositionByCommodityChart.sql
+++ i21Database/dbo/Stored Procedures/uspRKDPRDailyPositionByCommodityChart.sql
@@ -9,28 +9,28 @@ DECLARE @tblFinalDetail TABLE (
 	intLocationId int
 	,intCommodityId INT
 	,strCommodityCode NVARCHAR(50) COLLATE Latin1_General_CI_AS
-	,strUnitMeasure NVARCHAR(50) COLLATE Latin1_General_CI_AS	
+	,UOM NVARCHAR(50) COLLATE Latin1_General_CI_AS	
 	,[Open Purchase] decimal(24,10)
 	,[Open Sale]  decimal(24,10)	
 	,[Company Titled] decimal(24,10)
-	,[Case Exposure] decimal(24,10)
+	,[Price Risk] decimal(24,10)
 	,[Open Sales] decimal(24,10)
 	,[Avail For Sale] decimal(24,10)
 	,[In House] decimal(24,10)
-	,[Basis Exposure] decimal(24,10)
+	,[Basis Risk] decimal(24,10)
 	)
 
 
 INSERT INTO @tblFinalDetail
 EXEC uspRKDPRDailyPositionByCommodityLocation @intCommodityId =  @intCommodityId,@intVendorId=@intVendorId
 
-SELECT strTransactionType
+SELECT strTransactionType,UOM
  ,sum(dblTransactionQuantity) dblTransactionQuantity
 FROM @tblFinalDetail 
 UNPIVOT(dblTransactionQuantity FOR strTransactionType IN (
 	 [Company Titled]
-	,[Case Exposure]
-	,[Basis Exposure] 
+	,[Price Risk]
+	,[Basis Risk] 
 	,[Avail For Sale] 	
-	,[In House] 	
-   )) AS UnPvt group by strTransactionType 
+	,[In House]
+   )) AS UnPvt group by strTransactionType,UOM 
