1c3030d098063e9c84c698e6559a4262a7d44e7d
kenneth lagunero <kenneth.lagunero@irely.com>
2016-09-01 17:27:46 +0800

    TM-1994 Base-E

---
+++ i21Database/dbo/Views/vyuTMBEExportAssetAct.sql
@@ -0,0 +1,37 @@
+﻿CREATE VIEW [dbo].[vyuTMBEExportAssetAct]  
+AS 
+
+SELECT 
+	 account = C.strEntityNo
+	 ,number = REPLACE(STR(intSiteNumber, 4), SPACE(1), '0')
+	 ,reference = ''
+	 ,priceID = dblStandardCost
+	 ,priceDiscount = 0.0000
+	 ,cashCode = ''
+	 ,miscTranCode = ''
+	 ,misPriceID = ''
+	 ,lastDate =  CONVERT(VARCHAR(10), dtmLastDeliveryDate, 112)
+	 ,lastAmount = dblLastDeliveredGal
+FROM tblTMSite A
+INNER JOIN tblTMCustomer B
+	ON A.intCustomerID =B.intCustomerID
+INNER JOIN (SELECT 
+				Ent.strEntityNo
+				,Ent.intEntityId
+				,Cus.ysnActive
+			FROM tblEMEntity Ent
+			INNER JOIN tblARCustomer Cus 
+				ON Ent.intEntityId = Cus.intEntityCustomerId) C
+	ON B.intCustomerNumber = C.intEntityId
+LEFT JOIN (
+	SELECT 
+		A.intItemId
+		,A.strItemNo 
+		,A.intLocationId
+		,A.dblStandardCost
+	FROM vyuICGetItemPricing A
+	) F
+	ON 	F.intItemId = A.intProduct
+		AND A.intLocationId = F.intLocationId
+WHERE C.ysnActive = 1 AND A.ysnActive = 1
+GO
