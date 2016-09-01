a8dcb62016b151c9069ede94cb0f11bd649fd848
Wendell Wayne Estrada <wendell.estrada@irely.com>
2016-09-01 16:58:30 +0800

    IC-2553 BASE View - Products

--- i21Database/dbo/Views/vyuICExportProduct.sql
+++ i21Database/dbo/Views/vyuICExportProduct.sql
@@ -2,15 +2,25 @@
 AS
 SELECT
 	CAST(item.strItemNo AS NVARCHAR(16)) code
-	, CAST(item.strDescription AS NVARCHAR(35)) name
-	, CAST(item.intItemId AS VARCHAR(16)) itemID
-	, CAST(0 AS NVARCHAR(16)) priceID
-	, CAST(0 AS NVARCHAR(8)) taxCode
+	, CAST(item.strShortName AS NVARCHAR(35)) name
+	, CAST(prices.dblPrice AS NVARCHAR(16)) priceID
+	, CAST('' AS NVARCHAR(8)) taxCode
 	, 0 aux1
 	, 0 aux2
 	, CAST(0 AS NVARCHAR(8)) fuelTypeCode
 	, 0 preOp
 	, 0 postOp
 FROM tblICItem item
+	LEFT OUTER JOIN (
+		SELECT
+			Item.intItemId,
+			dblPrice = CAST(ISNULL(ItemPricing.dblStandardCost * ItemUOM.dblUnitQty, 0) AS NUMERIC(18, 6))
+		FROM vyuICGetItemStock Item
+		LEFT JOIN tblEMEntity Vendor ON Vendor.intEntityId = Item.intVendorId
+		LEFT JOIN tblICItemPricing ItemPricing ON ItemPricing.intItemId = Item.intItemId and ItemPricing.intItemLocationId = Item.intItemLocationId
+		LEFT JOIN tblICItemUOM ItemUOM ON ItemUOM.intItemId = Item.intItemId
+		LEFT JOIN tblICUnitMeasure UOM ON UOM .intUnitMeasureId= ItemUOM.intUnitMeasureId
+	) prices ON prices.intItemId = item.intItemId
 WHERE item.ysnAvailableTM = 1
 	OR item.strType = 'Service'
+GROUP BY prices.dblPrice, item.strItemNo, item.strShortName
