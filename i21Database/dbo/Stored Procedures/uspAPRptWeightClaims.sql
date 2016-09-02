323a2920b96c3ddd57812d54316ccacc254a79d3
Lex Eugene Peregrino <lex.peregrino@irely.com>
2016-09-02 11:34:30 +0800

    AP-2209 - Create weight claims report from debit memo

---
+++ i21Database/dbo/Stored Procedures/uspAPRptWeightClaims.sql
@@ -0,0 +1,79 @@
+﻿CREATE PROCEDURE [dbo].[uspAPRptWeightClaims]
+	@xmlParam NVARCHAR(MAX) = NULL
+	--@intBillId INT
+AS
+
+SET QUOTED_IDENTIFIER OFF
+SET ANSI_NULLS ON
+SET NOCOUNT ON
+SET XACT_ABORT ON
+SET ANSI_WARNINGS OFF
+
+-- Sample XML string structure:
+--DECLARE @xmlParam NVARCHAR(MAX)
+--SET @xmlParam = '
+--<xmlparam>
+--	<filters>
+--		<filter>
+--			<fieldname>intPurchaseId</fieldname>
+--			<condition>Equal To</condition>
+--			<from></from>
+--			<to></to>
+--			<join>And</join>
+--			<datatype>Int</datatype>
+--		</filter>
+--	</filters>
+--	<options />
+--</xmlparam>'
+
+DECLARE @intBillId INT 
+DECLARE @query NVARCHAR(MAX);
+DECLARE @xmlDocumentId AS INT;
+
+-- Sanitize the @xmlParam 
+IF LTRIM(RTRIM(@xmlParam)) = '' 
+BEGIN
+--SET @xmlParam = NULL 
+	SELECT * FROM [vyuAPRptWeightClaims] WHERE intBillId = 0 --RETURN NOTHING TO RETURN SCHEMA
+END
+
+-- Create a table variable to hold the XML data. 		
+DECLARE @temp_xml_table TABLE (
+	id INT IDENTITY(1,1)
+	,[fieldname] NVARCHAR(50)
+	,condition NVARCHAR(20)      
+	,[from] NVARCHAR(50)
+	,[to] NVARCHAR(50)
+	,[join] NVARCHAR(10)
+	,[datatype] NVARCHAR(50)
+)
+
+-- Prepare the XML 
+EXEC sp_xml_preparedocument @xmlDocumentId output, @xmlParam
+
+-- Insert the XML to the xml table. 		
+INSERT INTO @temp_xml_table
+SELECT *
+FROM OPENXML(@xmlDocumentId, 'xmlparam/filters/filter', 2)
+WITH (
+	[fieldname] nvarchar(50)
+	, [condition] nvarchar(20)
+	, [from] nvarchar(50)
+	, [to] nvarchar(50)
+	, [join] nvarchar(10)
+	, [datatype] nvarchar(50)
+)
+
+IF EXISTS(SELECT 1 FROM @temp_xml_table)
+BEGIN
+	SELECT 
+		@intBillId = CASE WHEN ISNULL([from],'') = '' THEN NULL ELSE [from] END
+	FROM @temp_xml_table WHERE [fieldname] = 'intBillId'
+END
+
+--SET @intPurchaseId = @intBillId;
+
+SELECT 
+* 
+FROM [vyuAPRptWeightClaims]
+WHERE intBillId = (CASE WHEN @intBillId IS NOT NULL THEN @intBillId ELSE 0 END)
