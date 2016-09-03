86f1611da438c100396ac564fdca54feb1872a4b
Swetosuvro Ghosh <swetosuvro.g@irely.com>
2016-09-04 03:17:02 +0530

    MFG-1989

---
+++ i21Database/dbo/Views/vyuMFOrderManifest.sql
@@ -0,0 +1,20 @@
+﻿CREATE VIEW vyuMFOrderManifest
+AS 
+SELECT OH.intOrderHeaderId
+	,M.intOrderManifestId
+	,M.intConcurrencyId
+	,L.intLotId
+	,L.strLotNumber
+	,L.strLotAlias
+	,L.dblQty
+	,I.strItemNo
+	,I.strDescription
+	,UM.strUnitMeasure
+	,LS.strSecondaryStatus AS strLotStatus
+FROM tblMFOrderHeader OH 
+JOIN tblMFOrderManifest M ON OH.intOrderHeaderId = M.intOrderHeaderId
+JOIN tblICLot L ON L.intLotId = M.intLotId
+JOIN tblICLotStatus LS ON LS.intLotStatusId = L.intLotStatusId
+JOIN tblICItem I ON I.intItemId = L.intItemId
+JOIN tblICItemUOM IU ON IU.intItemUOMId = L.intItemUOMId
+JOIN tblICUnitMeasure UM ON UM.intUnitMeasureId = IU.intUnitMeasureId
