dad06e76890e635172f28d43c2efbde6fbcbc775
Jayson Ramos <jayson.ramos@iRely.com>
2016-09-05 10:40:26 +0800

    Help Desk and CRM data fix.

--- i21PreDeployment/Scripts/Pre-Deployment/16_HD/OpportunitySourceDataFix.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/16_HD/OpportunitySourceDataFix.sql
@@ -5,18 +5,18 @@ GO
 
 	IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE' AND TABLE_NAME='tblHDOpportunitySource')
 	begin
-		update tblHDOpportunitySource set tblHDOpportunitySource.strSource = tblHDOpportunitySource.strSource+'_'+convert(nvarchar(20),tblHDOpportunitySource.intOpportunitySourceId) where tblHDOpportunitySource.strSource in (
+		exec('update tblHDOpportunitySource set tblHDOpportunitySource.strSource = tblHDOpportunitySource.strSource+''_''+convert(nvarchar(20),tblHDOpportunitySource.intOpportunitySourceId) where tblHDOpportunitySource.strSource in (
 			select strSource from (
 				select s1.strSource, cnt = count(s1.strSource) from tblHDOpportunitySource s1 group by s1.strSource
 			) as f where cnt > 1
-		)
+		)')
 	end
 
 	IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE' AND TABLE_NAME='tblHDModule')
 	begin
 		IF EXISTS(SELECT * FROM   INFORMATION_SCHEMA.COLUMNS WHERE  TABLE_NAME = 'tblHDModule' AND COLUMN_NAME = 'intSMModuleId')
 		begin
-			update tblHDModule set tblHDModule.intSMModuleId = 15 where tblHDModule.intSMModuleId = 92
+			exec('update tblHDModule set tblHDModule.intSMModuleId = 15 where tblHDModule.intSMModuleId = 92')
 		end
 	end
 
