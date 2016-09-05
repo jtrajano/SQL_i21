de0f78613d09d353ce222aa499faa463de625b94
Jayson Ramos <jayson.ramos@iRely.com>
2016-09-05 11:53:41 +0800

    CRM-209

--- i21Database/Scripts/Post-Deployment/HD/DefaultData/2_Screen.sql
+++ i21Database/Scripts/Post-Deployment/HD/DefaultData/2_Screen.sql
@@ -1,11 +1,11 @@
 ﻿IF NOT EXISTS (SELECT TOP 1 1 FROM tblSMScreen WHERE strNamespace = 'HelpDesk.view.Campaign')
     BEGIN
-        INSERT [dbo].[tblSMScreen] ([strScreenId], [strScreenName], [strNamespace], [strModule], [strTableName], [intConcurrencyId])
-            VALUES (N'Campaign', N'Campaign', N'HelpDesk.view.Campaign', N'Help Desk', N'tblHDOpportunityCampaign', 0)
+        INSERT [dbo].[tblSMScreen] ([strScreenId], [strScreenName], [strNamespace], [strModule], [strTableName], [ysnCustomTab], [intConcurrencyId])
+            VALUES (N'Campaign', N'Campaign', N'HelpDesk.view.Campaign', N'Help Desk', N'tblHDOpportunityCampaign', convert(bit,1), 0)
         END
 ELSE
     BEGIN
         UPDATE tblSMScreen
-        SET strTableName = N'tblHDOpportunityCampaign'
+        SET strTableName = N'tblHDOpportunityCampaign', ysnCustomTab = convert(bit,1)
         WHERE strNamespace = 'HelpDesk.view.Campaign'
     END
