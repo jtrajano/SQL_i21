785caf7548b140bcef285dccafe2edc17b6c1ee1
Dan Michael Jalimao <dan.jalimao@irely.com>
2016-09-06 12:29:01 +0800

    FCOM-2135

--- i21Database/dbo/Views/vyuEMUserAdmin.sql
+++ i21Database/dbo/Views/vyuEMUserAdmin.sql
@@ -2,14 +2,20 @@
 	AS 
 
 	select intEntityUserSecurityId,ysnActive = ~ysnDisabled from tblSMUserSecurity a 
-		join tblSMUserRole b 
-				on a.intUserRoleID = b.intUserRoleID and b.ysnAdmin = 1
-			where intEntityUserSecurityId not in (select intEntityUserSecurityId from tblSMUserSecurityCompanyLocationRolePermission) 	
-	union	
+	  join tblSMUserRole b 
+		on a.intUserRoleID = b.intUserRoleID and b.ysnAdmin = 1
+	   where intEntityUserSecurityId not in (select
+	 intEntityUserSecurityId from vyuSMUserLocationSubRolePermission)  
+	 union 
 
-	select a.intEntityUserSecurityId, ysnActive = ~ysnDisabled from tblSMUserSecurityCompanyLocationRolePermission a 
-		join tblSMUserRole b 
-			on a.intUserRoleId = b.intUserRoleID and ysnAdmin = 1
-		join tblSMUserSecurity c
-			on a.intEntityUserSecurityId = c.intEntityUserSecurityId
-		group by a.intEntityUserSecurityId,c.ysnDisabled
+	  SELECT Permission.intEntityUserSecurityId, ysnActive = ~ysnDisabled
+	  FROM vyuSMUserLocationSubRolePermission Permission
+	   INNER JOIN tblSMUserRole UserRole ON Permission.intUserRoleId = UserRole.intUserRoleID
+	   join tblSMUserSecurity a
+		on Permission.intEntityUserSecurityId = a.intEntityUserSecurityId
+	   where UserRole.ysnAdmin = 1
+
+	  group by Permission.intEntityUserSecurityId,a.ysnDisabled
+
+
+
