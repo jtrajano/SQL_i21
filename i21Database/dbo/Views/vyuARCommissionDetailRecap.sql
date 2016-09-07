79291239dba84c22fe5057f355ada75cd1ee8725
Kelvin Quinto <kelvin.quinto@irely.com>
2016-09-07 12:57:10 +0800

    AR-3395

---
+++ i21Database/dbo/Views/vyuARCommissionDetailRecap.sql
@@ -0,0 +1,18 @@
+﻿CREATE VIEW [dbo].[vyuARCommissionDetailRecap]
+AS
+SELECT CRD.*
+     , strEntityName	= EM.strName
+	 , strDocumentNumber = CASE WHEN CRD.strSourceType = 'tblARInvoiceDetail'
+									THEN I.strInvoiceNumber
+								WHEN CRD.strSourceType = 'tblGLDetail'
+									THEN GL.strTransactionId
+								WHEN CRD.strSourceType = 'tblHDTicketHoursWorked'
+									THEN T.strTicketNumber
+							END
+FROM tblARCommissionRecapDetail CRD
+	LEFT JOIN tblEMEntity EM ON CRD.intEntityId = EM.intEntityId
+	LEFT JOIN tblARInvoiceDetail ID ON CRD.intSourceId = ID.intInvoiceDetailId
+	LEFT JOIN tblARInvoice I ON ID.intInvoiceId = I.intInvoiceId
+	LEFT JOIN tblHDTicketHoursWorked THW ON CRD.intSourceId = THW.intTicketHoursWorkedId
+	LEFT JOIN tblHDTicket T ON THW.intTicketId = THW.intTicketId
+	LEFT JOIN tblGLDetail GL ON CRD.intSourceId = GL.intGLDetailId
