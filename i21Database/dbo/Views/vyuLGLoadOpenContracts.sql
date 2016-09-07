604025fd57d025e5372e7fa0d756f1221fb93266
Swetosuvro Ghosh <swetosuvro.g@irely.com>
2016-09-07 21:58:29 +0530

    LG-575

--- i21Database/dbo/Views/vyuLGLoadOpenContracts.sql
+++ i21Database/dbo/Views/vyuLGLoadOpenContracts.sql
@@ -1,57 +1,67 @@
 CREATE VIEW vyuLGLoadOpenContracts
 AS
-	SELECT 	CD.intContractDetailId, 
-			CD.intContractHeaderId, 
-			CD.intContractSeq, 
-			CD.intItemId,
-			CD.strItemDescription,
-			CD.strItemNo,
-			CD.dblDetailQuantity,
-			CD.intUnitMeasureId,
-			CD.intItemUOMId,
-			CD.strItemUOM as strUnitMeasure,
-			CD.intCompanyLocationId,
-			CD.strLocationName,
-			IsNull(CD.dblBalance, 0) - IsNull(CD.dblScheduleQty, 0)		AS dblUnLoadedQuantity,
-			CD.intContractTypeId intPurchaseSale,
-			CD.intEntityId,
-			CD.strContractNumber,
-			CD.dtmContractDate,
-			CD.strEntityName,
-			convert(nvarchar(100), CD.dtmStartDate, 101) as strStartDate,
-			convert(nvarchar(100), CD.dtmEndDate, 101) as strEndDate,
-			CD.dtmStartDate,
-			CD.dtmEndDate,
-			CD.intDefaultLocationId,
-			IsNull(CD.dblScheduleQty, 0) as dblScheduleQty,
-			CD.strCustomerContract,
-			IsNull(CD.dblBalance, 0) as dblBalance,
-			CASE WHEN ((CD.ysnAllowedToShow = 1) AND ((IsNull(CD.dblBalance, 0) - IsNull(CD.dblScheduleQty, 0) > 0) Or (CD.ysnUnlimitedQuantity = 1)))
-				THEN CAST(1 as Bit)
-				ELSE CAST (0 as Bit)
-				END as ysnAllowedToShow,
-			CD.ysnUnlimitedQuantity,
-			CD.ysnLoad,
-			CD.dblQuantityPerLoad,
-			CD.intNoOfLoad,
-
-			Item.strType as strItemType,
-			CD.intPositionId,
-			CD.intLoadingPortId,
-			CD.intDestinationPortId,
-			CD.intDestinationCityId,
-			LoadingPort.strCity as strOriginPort,
-			DestPort.strCity as strDestinationPort,
-			DestCity.strCity as strDestinationCity,
-			CD.strPackingDescription,
-			CD.intShippingLineId as intShippingLineEntityId,
-			CD.intNumberOfContainers,
-			CD.intContainerTypeId,
-			CD.strVessel,
-			CD.intContractTypeId
-
-	FROM vyuCTContractDetailView 		CD
-	JOIN tblICItem Item ON Item.intItemId = CD.intItemId
-	LEFT JOIN tblSMCity LoadingPort ON LoadingPort.intCityId = CD.intLoadingPortId
-	LEFT JOIN tblSMCity DestPort ON DestPort.intCityId = CD.intDestinationPortId
-	LEFT JOIN tblSMCity DestCity ON DestCity.intCityId = CD.intDestinationCityId
+SELECT CD.intContractDetailId
+	,CD.intContractHeaderId
+	,CD.intContractSeq
+	,CD.intItemId
+	,Item.strDescription strItemDescription
+	,Item.strItemNo
+	,CD.dblQuantity AS dblDetailQuantity
+	,CD.intUnitMeasureId
+	,CD.intItemUOMId
+	,U1.strUnitMeasure AS strUnitMeasure
+	,CD.intCompanyLocationId
+	,CL.strLocationName AS strLocationName
+	,ISNULL(CD.dblBalance, 0) - ISNULL(CD.dblScheduleQty, 0) AS dblUnLoadedQuantity
+	,CH.intContractTypeId intPurchaseSale
+	,CH.intEntityId
+	,CH.strContractNumber
+	,CH.dtmContractDate
+	,EY.strEntityName
+	,CONVERT(NVARCHAR(100), CD.dtmStartDate, 101) AS strStartDate
+	,CONVERT(NVARCHAR(100), CD.dtmEndDate, 101) AS strEndDate
+	,CD.dtmStartDate
+	,CD.dtmEndDate
+	,EY.intDefaultLocationId
+	,ISNULL(CD.dblScheduleQty, 0) AS dblScheduleQty
+	,CH.strCustomerContract
+	,ISNULL(CD.dblBalance, 0) AS dblBalance
+	,CASE 
+		WHEN ((CAST(CASE 
+						WHEN CD.intContractStatusId IN (1,4)
+							THEN 1
+						ELSE 0
+						END AS BIT) = 1
+			  ) AND ((ISNULL(CD.dblBalance, 0) - ISNULL(CD.dblScheduleQty, 0) > 0) OR (CH.ysnUnlimitedQuantity = 1)))
+			THEN CAST(1 AS BIT)
+		ELSE CAST(0 AS BIT)
+		END AS ysnAllowedToShow
+	,CH.ysnUnlimitedQuantity
+	,CH.ysnLoad
+	,CD.dblQuantityPerLoad
+	,CD.intNoOfLoad
+	,Item.strType AS strItemType
+	,CH.intPositionId
+	,CD.intLoadingPortId
+	,CD.intDestinationPortId
+	,CD.intDestinationCityId
+	,LoadingPort.strCity AS strOriginPort
+	,DestPort.strCity AS strDestinationPort
+	,DestCity.strCity AS strDestinationCity
+	,CD.strPackingDescription
+	,CD.intShippingLineId AS intShippingLineEntityId
+	,CD.intNumberOfContainers
+	,CD.intContainerTypeId
+	,CD.strVessel
+	,CH.intContractTypeId
+FROM tblCTContractHeader CH
+JOIN tblCTContractDetail CD ON CD.intContractHeaderId = CH.intContractHeaderId
+JOIN tblICItem Item ON Item.intItemId = CD.intItemId
+JOIN tblSMCompanyLocation CL ON CL.intCompanyLocationId = CD.intCompanyLocationId
+JOIN vyuCTEntity EY ON EY.intEntityId = CH.intEntityId
+	AND EY.strEntityType = (CASE WHEN CH.intContractTypeId = 1 THEN 'Vendor' ELSE 'Customer'END)
+LEFT JOIN tblICItemUOM IU ON IU.intItemUOMId = CD.intItemUOMId
+LEFT JOIN tblICUnitMeasure U1 ON U1.intUnitMeasureId = IU.intUnitMeasureId
+LEFT JOIN tblSMCity LoadingPort ON LoadingPort.intCityId = CD.intLoadingPortId
+LEFT JOIN tblSMCity DestPort ON DestPort.intCityId = CD.intDestinationPortId
+LEFT JOIN tblSMCity DestCity ON DestCity.intCityId = CD.intDestinationCityId
