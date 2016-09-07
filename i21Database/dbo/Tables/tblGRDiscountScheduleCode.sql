fed93235274d9d9fca11433689ec6978c1b8a0ce
Gopinath Nayak <gopinath.n@irely.com>
2016-09-07 19:23:38 +0530

    GRN-451

--- i21Database/dbo/Tables/tblGRDiscountScheduleCode.sql
+++ i21Database/dbo/Tables/tblGRDiscountScheduleCode.sql
@@ -15,10 +15,15 @@
     [intConcurrencyId] INT NOT NULL DEFAULT 1, 
 	[intSort] INT NULL , 
     [strDiscountChargeType] NVARCHAR(30)  COLLATE Latin1_General_CI_AS NULL,
-	[intItemId] INT NULL,  
+	[intItemId] INT NULL,
+	[intStorageTypeId] INT NULL,
+	[intCompanyLocationId] INT NULL,  
     CONSTRAINT [PK_tblGRDiscountScheduleCode_intDiscountScheduleCodeId] PRIMARY KEY ([intDiscountScheduleCodeId]), 	
     CONSTRAINT [FK_tblGRDiscountScheduleCode_tblGRDiscountSchedule_intDiscountScheduleId] FOREIGN KEY ([intDiscountScheduleId]) REFERENCES [tblGRDiscountSchedule]([intDiscountScheduleId]), 
     CONSTRAINT [FK_tblGRDiscountScheduleCode_tblGRDiscountCalculationOption_intValueFieldId_intDiscountCalculationOptionId] FOREIGN KEY ([intDiscountCalculationOptionId]) REFERENCES [tblGRDiscountCalculationOption]([intValueFieldId]),
 	CONSTRAINT [FK_tblGRDiscountScheduleCode_tblGRShrinkCalculationOption_intValueFieldId_intShrinkCalculationOptionId] FOREIGN KEY ([intShrinkCalculationOptionId]) REFERENCES [tblGRShrinkCalculationOption]([intValueFieldId]),
-	CONSTRAINT [FK_tblGRDiscountScheduleCode_tblICItem_intItemId] FOREIGN KEY ([intItemId]) REFERENCES [tblICItem]([intItemId])
+	CONSTRAINT [FK_tblGRDiscountScheduleCode_tblICItem_intItemId] FOREIGN KEY ([intItemId]) REFERENCES [tblICItem]([intItemId]),
+	CONSTRAINT [FK_tblGRDiscountScheduleCode_tblGRStorageType_intStorageScheduleTypeId_intStorageTypeId] FOREIGN KEY ([intStorageTypeId]) REFERENCES [tblGRStorageType]([intStorageScheduleTypeId]),
+	CONSTRAINT [FK_tblGRDiscountScheduleCode_tblSMCompanyLocation_intCompanyLocationId] FOREIGN KEY ([intCompanyLocationId]) REFERENCES [tblSMCompanyLocation]([intCompanyLocationId]),
+	CONSTRAINT [UK_tblGRDiscountScheduleCode_intDiscountScheduleId_intItemId_intStorageTypeId_intCompanyLocationId] UNIQUE ([intDiscountScheduleId], [intItemId], [intStorageTypeId],[intCompanyLocationId])
 )
