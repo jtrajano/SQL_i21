cd82178edc917d404f72e62e7754720d962ab85d
Kelvin Quinto <kelvin.quinto@irely.com>
2016-09-07 18:00:10 +0800

    AR-3123

--- i21Database/dbo/Views/vyuARDocumentMaintenanceMessage.sql
+++ i21Database/dbo/Views/vyuARDocumentMaintenanceMessage.sql
@@ -1,9 +1,28 @@
 ﻿CREATE VIEW [dbo].[vyuARDocumentMaintenanceMessage]
 AS 
-SELECT A.intDocumentMaintenanceId, A.strCode, A.strTitle, A.intCompanyLocationId, A.intLineOfBusinessId, 
-A.intEntityCustomerId, A.strSource, A.strType, A.ysnCopyAll,
-B.intDocumentMaintenanceMessageId, B.strHeaderFooter, B.intCharacterLimit, B.strMessage, B.ysnRecipe, 
-B.ysnQuote, B.ysnSalesOrder, B.ysnPickList, B.ysnBOL, B.ysnInvoice, B.ysnScaleTicket 
+SELECT A.intDocumentMaintenanceId
+     , A.strCode
+	 , A.strTitle
+	 , A.intCompanyLocationId
+	 , A.intLineOfBusinessId
+	 , A.intEntityCustomerId
+	 , A.strSource
+	 , A.strType
+	 , A.ysnCopyAll
+	 , B.intDocumentMaintenanceMessageId
+	 , B.strHeaderFooter
+	 , B.intCharacterLimit
+	 , B.strMessage
+	 , B.ysnRecipe
+	 , B.ysnQuote
+	 , B.ysnSalesOrder
+	 , B.ysnPickList
+	 , B.ysnBOL
+	 , B.ysnInvoice
+	 , B.ysnScaleTicket 
+	 , C.strCustomerNumber
+	 , CL.strLocationName
 FROM tblSMDocumentMaintenance A 
-INNER JOIN (SELECT intDocumentMaintenanceId, intDocumentMaintenanceMessageId, strHeaderFooter, intCharacterLimit, strMessage, ysnRecipe, ysnQuote, ysnSalesOrder, ysnPickList, ysnBOL, ysnInvoice,ysnScaleTicket 
-			FROM tblSMDocumentMaintenanceMessage) B ON A.intDocumentMaintenanceId = B.intDocumentMaintenanceId
+INNER JOIN tblSMDocumentMaintenanceMessage B ON A.intDocumentMaintenanceId = B.intDocumentMaintenanceId
+LEFT JOIN vyuARCustomer C ON A.intEntityCustomerId = C.intEntityCustomerId
+LEFT JOIN tblSMCompanyLocation CL ON A.intCompanyLocationId = CL.intCompanyLocationId
---
+++ i21Database/dbo/Views/vyuARImportLog.sql
@@ -0,0 +1,10 @@
+﻿CREATE VIEW [dbo].[vyuARImportLog]
+AS
+SELECT IL.*
+	 , EM.strName
+	 , ICI.strItemNo
+	 , strCompanyLocation  = CL.strLocationName
+ FROM tblARImportLog IL
+	LEFT JOIN tblEMEntity EM ON IL.intEntityId = EM.intEntityId
+	LEFT JOIN tblICItem ICI ON IL.intItemId = ICI.intItemId
+	LEFT JOIN tblSMCompanyLocation CL ON IL.intCompanyLocationId = CL.intCompanyLocationId
