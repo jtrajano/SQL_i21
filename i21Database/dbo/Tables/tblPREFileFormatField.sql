7b7b4ce0989acae077cfbb5c5de8759b178f523b
Arman Arenas <arman.arenas@irely.com>
2016-09-07 11:10:57 +0800

    PR-873 Added Payroll E-File Format tables

---
+++ i21Database/dbo/Tables/tblPREFileFormatField.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[tblPREFileFormatField]
+(
+	[intEFileFormatFieldId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [intEFileFormatRecordId] INT NOT NULL, 
+    [intFieldNo] INT NULL, 
+    [intLength] INT NULL, 
+    [intType] INT NULL, 
+    [strDescription] NVARCHAR(200) COLLATE Latin1_General_CI_AS NULL, 
+    [strFieldValue] NVARCHAR(200) COLLATE Latin1_General_CI_AS NULL, 
+    [strFormat] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
+    [intFillerPosition] INT NULL, 
+    [intFillerType] INT NULL, 
+    [ysnNewLine] BIT NULL DEFAULT ((0)), 
+    [intConcurrencyId] INT NOT NULL DEFAULT ((1)), 
+    CONSTRAINT [FK_tblPREFileFormatField_tblPRFileFormatRecord] FOREIGN KEY ([intEFileFormatRecordId]) REFERENCES [tblPREFileFormatRecord]([intEFileFormatRecordId]) ON DELETE CASCADE
+)
---
+++ i21Database/dbo/Tables/tblPREFileFormatRecord.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tblPREFileFormatRecord]
+(
+	[intEFileFormatRecordId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [intEFileFormatId] INT NOT NULL, 
+    [intRecordNo] INT NULL, 
+    [strDescription] NVARCHAR(200) COLLATE Latin1_General_CI_AS NULL, 
+    [intRecordType] INT NULL, 
+    [strDatasource] NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL, 
+    [intConcurrencyId] INT NOT NULL DEFAULT ((1)), 
+    CONSTRAINT [FK_tblPREFileFormatRecord_tblPREFileFormat] FOREIGN KEY (intEFileFormatId) REFERENCES tblPREFileFormat(intEFileFormatId) ON DELETE CASCADE
+)
