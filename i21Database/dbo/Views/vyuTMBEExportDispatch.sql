92c20337b74b4abad64edc37c177e06a7c273952
kenneth lagunero <kenneth.lagunero@irely.com>
2016-09-14 13:19:40 +0800

    TM-1975

--- i21Database/dbo/Views/vyuTMBEExportDispatch.sql
+++ i21Database/dbo/Views/vyuTMBEExportDispatch.sql
@@ -5,7 +5,7 @@ SELECT
 	 routeNum = J.strRouteId
 	 ,seqNum = ''
 	 ,account = CAST(C.strEntityNo AS NVARCHAR(16))
-	 ,asset = REPLACE(STR(A.intSiteNumber, 4), SPACE(1), '0')
+	 ,asset = REPLACE(STR(A.intSiteNumber, 4), SPACE(1), '0') + '-' + ISNULL(K.strSerialNumber,'')
 	 ,orderQty = CAST(ROUND((CASE WHEN ISNULL(D.dblMinimumQuantity,0) = 0 THEN ISNULL(D.dblQuantity,0.0) ELSE D.dblMinimumQuantity END),0) AS INT)
 	 ,priceID = CAST(D.dblPrice AS NVARCHAR(16))
 	 ,invoice = ''
@@ -29,5 +29,17 @@ LEFT JOIN tblTMRoute J
 	ON A.intRouteId = J.intRouteId
 LEFT JOIN tblSMTaxGroup H
 	ON A.intTaxStateID = H.intTaxGroupId
+LEFT JOIN (
+	SELECT 
+		AA.intSiteID
+		,BB.strSerialNumber
+		,intCntId = ROW_NUMBER() OVER (PARTITION BY AA.intSiteID ORDER BY AA.intSiteDeviceID ASC)
+	FROM tblTMSiteDevice AA
+	INNER JOIN tblTMDevice BB
+		ON AA.intDeviceId = BB.intDeviceId
+	WHERE ISNULL(BB.ysnAppliance,0) = 0
+) K
+	ON A.intSiteID = K.intSiteID
+	AND K.intCntId = 1
 WHERE C.ysnActive = 1 AND A.ysnActive = 1
 GO
