60efb148d39da3ab00886d69fe68e9ae67721a46
Kelvin Quinto <kelvin.quinto@irely.com>
2016-09-14 20:09:12 +0800

    AR-3346

---
+++ i21Database/dbo/Stored Procedures/uspARImportPaymentCSV.sql
@@ -0,0 +1,130 @@
+﻿CREATE PROCEDURE [dbo].[uspARImportPaymentCSV]
+	 @ImportLogId			INT
+	,@IsRecap				BIT = 0
+	,@UserEntityId			INT	= NULL
+AS
+
+BEGIN
+
+SET QUOTED_IDENTIFIER OFF
+SET ANSI_NULLS ON
+SET NOCOUNT ON
+SET ANSI_WARNINGS OFF
+	
+DECLARE @DateNow			DATETIME
+	  , @DefaultCurrencyId	INT
+
+SET @DateNow = CAST(GETDATE() AS DATE)
+SET @DefaultCurrencyId = (SELECT TOP 1 intDefaultCurrencyId FROM tblSMCompanyPreference)
+
+DECLARE @PaymentsForImport AS TABLE(intImportLogDetailId INT UNIQUE)
+
+INSERT INTO @PaymentsForImport
+SELECT intImportLogDetailId FROM tblARImportLogDetail
+WHERE intImportLogId = @ImportLogId
+	AND ISNULL(ysnSuccess,0) = 1
+	AND ISNULL(ysnImported,0) = 0
+	--AND ISNULL(ysnRecap,0) = 0
+ORDER BY intImportLogDetailId
+
+WHILE EXISTS(SELECT NULL FROM @PaymentsForImport)
+	BEGIN
+		DECLARE @intImportLogDetailId		INT
+			  , @intEntityCustomerId		INT
+		      , @intCompanyLocationId		INT
+			  , @intUndepositedAccountId	INT
+			  , @intBankAccountId			INT
+			  , @intPaymentMethodId			INT
+			  , @intPaymentId				INT
+			  , @dtmDatePaid				DATETIME
+			  , @dblAmountPaid				DECIMAL(18, 6)
+			  , @strPaymentInfo				NVARCHAR(50)
+			  , @ErrorMessage				NVARCHAR(250)
+		
+		SELECT TOP 1 @intImportLogDetailId = intImportLogDetailId FROM @PaymentsForImport ORDER BY intImportLogDetailId
+		     
+		SELECT  @intEntityCustomerId		= (SELECT TOP 1 intEntityId FROM tblEMEntity WHERE strEntityNo = ILD.strCustomerNumber)
+			  , @intCompanyLocationId		= (SELECT TOP 1 intCompanyLocationId FROM tblSMCompanyLocation WHERE strLocationName = ILD.strLocationName)
+			  , @intUndepositedAccountId	= (SELECT TOP 1 intUndepositedFundsId FROM tblSMCompanyLocation WHERE strLocationName = ILD.strLocationName)
+			  , @intBankAccountId			= (SELECT TOP 1 intBankAccountId FROM vyuCMBankAccount WHERE RTRIM(LTRIM(strBankAccountNo)) = ILD.strBankAccountNo)
+			  , @intPaymentMethodId			= (SELECT TOP 1 intPaymentMethodID FROM tblSMPaymentMethod WHERE strPaymentMethod = (CASE WHEN ILD.strPaymentMethod = 'C' THEN 'Check' ELSE NULL END))
+			  , @strPaymentInfo				= ILD.strCheckNumber
+			  , @dtmDatePaid				= ILD.dtmDatePaid
+			  , @dblAmountPaid				= ISNULL(ILD.dblAmountPaid, 0)
+		FROM 
+			tblARImportLogDetail ILD
+		WHERE ILD.intImportLogDetailId = @intImportLogDetailId
+
+		--VALIDATIONS
+		IF ISNULL(@intEntityCustomerId, 0) = 0
+			SELECT @ErrorMessage = 'Failed: Customer ' + strCustomerNumber + 'does not exist.' FROM tblARImportLogDetail WHERE intImportLogDetailId = @intImportLogDetailId
+
+		IF ISNULL(@intCompanyLocationId, 0) = 0
+			SELECT @ErrorMessage = ISNULL(@ErrorMessage, '') + 'Failed: Location ' + strLocationName + 'does not exist.' FROM tblARImportLogDetail WHERE intImportLogDetailId = @intImportLogDetailId
+		
+		IF ISNULL(@intBankAccountId, 0) = 0
+			SELECT @ErrorMessage = ISNULL(@ErrorMessage, '') + 'Failed: Bank Account # ' + strBankAccountNo + 'does not exist.' FROM tblARImportLogDetail WHERE intImportLogDetailId = @intImportLogDetailId
+
+		IF ISNULL(@intPaymentMethodId, 0) = 0
+			SELECT @ErrorMessage = ISNULL(@ErrorMessage, '') + 'Failed: Payment Method ' + strPaymentMethod + 'does not exist.' FROM tblARImportLogDetail WHERE intImportLogDetailId = @intImportLogDetailId
+
+		IF LEN(RTRIM(LTRIM(ISNULL(@ErrorMessage,'')))) < 1 AND @IsRecap = 0
+			BEGIN TRY
+				EXEC dbo.uspARCreateCustomerPayment
+					 @EntityCustomerId		= @intEntityCustomerId
+					,@CompanyLocationId		= @intCompanyLocationId
+					,@CurrencyId			= @DefaultCurrencyId
+					,@DatePaid				= @dtmDatePaid
+					,@AccountId				= @intUndepositedAccountId
+					,@BankAccountId			= @intBankAccountId
+					,@AmountPaid			= @dblAmountPaid
+					,@PaymentMethodId		= @intPaymentMethodId
+					,@PaymentInfo			= @strPaymentInfo
+					,@EntityId				= @UserEntityId	
+					,@AllowPrepayment		= 1		
+					,@RaiseError			= 1
+					,@ErrorMessage			= @ErrorMessage OUT
+					,@NewPaymentId			= @intPaymentId OUT
+			END TRY	
+			BEGIN CATCH
+				SET @ErrorMessage = ERROR_MESSAGE();
+			END CATCH
+
+		IF LEN(RTRIM(LTRIM(ISNULL(@ErrorMessage,'')))) > 0
+			BEGIN
+				UPDATE tblARImportLogDetail
+				SET ysnImported		= 0
+				   ,ysnSuccess      = 0
+				   ,strEventResult	= @ErrorMessage
+				WHERE [intImportLogDetailId] = @intImportLogDetailId
+
+				UPDATE tblARImportLog 
+				SET intSuccessCount	= intSuccessCount - 1
+				  , intFailedCount	= intFailedCount + 1
+				WHERE intImportLogId  = @ImportLogId
+			END
+		ELSE IF(ISNULL(@intPaymentId,0) <> 0) AND @IsRecap = 0
+			BEGIN
+				UPDATE tblARImportLogDetail
+				SET ysnImported			= 1
+				   ,ysnSuccess			= 1
+				   ,strEventResult		= (SELECT 'Payment #:' + strRecordNumber FROM tblARPayment WHERE intPaymentId = @intPaymentId) + ' Imported.'
+				WHERE intImportLogDetailId = @intImportLogDetailId
+			END
+		ELSE IF (LEN(RTRIM(LTRIM(ISNULL(@ErrorMessage,'')))) < 1) AND @IsRecap = 1
+			BEGIN
+				UPDATE tblARImportLogDetail
+				SET ysnImported			= 0
+				   ,ysnSuccess			= 1
+				   ,ysnRecap			= 1
+				   ,strEventResult		= 'Success!'
+				WHERE intImportLogDetailId = @intImportLogDetailId
+			END
+
+		DELETE FROM @PaymentsForImport WHERE intImportLogDetailId = @intImportLogDetailId
+	END
+
+--UPDATE tblARImportLogDetail SET ysnRecap = 0
+--WHERE intImportLogId = @ImportLogId
+
+END
