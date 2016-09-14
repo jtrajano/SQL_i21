7719b589eb51e687a4c8f6c4d9af8fc96dce7754
Dan Michael Jalimao <dan.jalimao@irely.com>
2016-09-14 10:07:50 +0800

    FCOM-2181

--- i21Database/dbo/Views/vyuSMUserRoleMenuSubRole.sql
+++ i21Database/dbo/Views/vyuSMUserRoleMenuSubRole.sql
@@ -1,21 +1,21 @@
 ﻿CREATE VIEW [dbo].[vyuSMUserRoleMenuSubRole]
 AS 
 SELECT DISTINCT
-ISNULL(SubRole.intUserRoleId, RoleMenu.intUserRoleId) AS intUserRoleId,
-RoleMenu.intMenuId,
-Menu.intParentMenuID as intParentMenuId,
-RoleMenu.ysnVisible,
-Sort.intSort,--RoleMenu.intSort,
-strMenuName,
-strModuleName,
-Menu.strDescription,
-Menu.strCategory,
-strType,
-strCommand,
-strIcon,
-ysnExpanded,
-ysnIsLegacy,
-ysnLeaf
+ISNULL(SubRole.intUserRoleId, RoleMenu.intUserRoleId) AS intUserRoleId
+,RoleMenu.intMenuId
+,Menu.intParentMenuID as intParentMenuId
+,RoleMenu.ysnVisible
+,Sort.intSort--RoleMenu.intSort,
+,strMenuName
+,strModuleName
+,Menu.strDescription
+,Menu.strCategory
+,strType
+,strCommand
+,strIcon
+,ysnExpanded
+,ysnIsLegacy
+,ysnLeaf
 FROM vyuSMUserRoleSubRole SubRole
 RIGHT JOIN tblSMUserRoleMenu RoleMenu ON SubRole.intUserRoleID = RoleMenu.intUserRoleId
 LEFT JOIN tblSMMasterMenu Menu ON Menu.intMenuID = RoleMenu.intMenuId
