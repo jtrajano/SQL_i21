2a8cf83ff4bdfaf739098fd0058bf4d6dfbfcb1a
Dan Michael Jalimao <dan.jalimao@irely.com>
2016-09-14 10:07:50 +0800

    FCOM-2181

--- i21Database/dbo/Views/vyuSMUserRoleMenuLocation.sql
+++ i21Database/dbo/Views/vyuSMUserRoleMenuLocation.sql
@@ -1,22 +1,23 @@
 ﻿CREATE VIEW [dbo].[vyuSMUserRoleMenuLocation]
 AS 
 SELECT DISTINCT
-SubRolePermimssion.intCompanyLocationId,
-SubRolePermimssion.intEntityUserSecurityId as intEntityId,
-RoleMenu.intMenuId,
-Menu.intParentMenuID as intParentMenuId,
-RoleMenu.ysnVisible,
-RoleMenu.intSort,
-strMenuName,
-strModuleName,
-strDescription,
-Menu.strCategory,
-strType,
-strCommand,
-strIcon,
-ysnExpanded,
-ysnIsLegacy,
-ysnLeaf
+SubRolePermimssion.intCompanyLocationId
+,SubRolePermimssion.intEntityUserSecurityId as intEntityId
+,RoleMenu.intMenuId
+,Menu.intParentMenuID as intParentMenuId
+,RoleMenu.ysnVisible
+,MAX(RoleMenu.intSort) as intSort
+,strMenuName
+,strModuleName
+,strDescription
+,Menu.strCategory
+,strType
+,strCommand
+,strIcon
+,ysnExpanded
+,ysnIsLegacy
+,ysnLeaf
 FROM tblSMUserRoleMenu RoleMenu
 LEFT JOIN tblSMMasterMenu Menu ON Menu.intMenuID = RoleMenu.intMenuId
 INNER JOIN vyuSMUserLocationSubRolePermission SubRolePermimssion ON SubRolePermimssion.intUserRoleId = RoleMenu.intUserRoleId
+GROUP BY SubRolePermimssion.intCompanyLocationId, SubRolePermimssion.intEntityUserSecurityId, RoleMenu.intMenuId, Menu.intParentMenuID, RoleMenu.ysnVisible, strMenuName, strModuleName, strDescription, Menu.strCategory, strType, strCommand, strIcon, ysnExpanded, ysnIsLegacy, ysnLeaf
