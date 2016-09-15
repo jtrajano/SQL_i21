ccb3e4f304a5034f09be4cc04d8144be5910e404
Von Carlo Pasion <von.pasion@irely.com>
2016-09-15 19:27:54 +0800

    FRM-3722

--- i21Database/dbo/Views/vyuEMEntityCredentialContact.sql
+++ i21Database/dbo/Views/vyuEMEntityCredentialContact.sql
@@ -19,7 +19,7 @@ SELECT
 	vEntityContact.Vendor,
 	vEntityContact.Employee,
 	vEntityContact.Salesperson,
-	vEntityContact.[User],
+	CASE WHEN ISNULL(UserSecurity.ysnDisabled, 2) = 2 THEN 0 ELSE vEntityContact.[User] END [User],
 	vEntityContact.FuturesBroker,
 	vEntityContact.ForwardingAgent,
 	vEntityContact.Terminal,
@@ -31,17 +31,17 @@ SELECT
 	vEntityContact.papit,
 	ysnReqTwoFactorAuth = Cast(isnull(SecurityPolicy.ysnReqTwoFactorAuth, 0) as bit),
 	UserSecurity.ysnDisabled
-FROM tblEMEntityCredential AS EntityCredential
-INNER JOIN vyuEMEntityContact AS vEntityContact
-ON EntityCredential.intEntityId = (
-	CASE WHEN vEntityContact.ysnPortalAccess = 1
-		THEN vEntityContact.intEntityContactId
-		ELSE vEntityContact.intEntityId
-	END
-)
-LEFT JOIN tblSMUserSecurity UserSecurity 
-	on EntityCredential.intEntityId = UserSecurity.intEntityUserSecurityId
-LEFT JOIN tblSMSecurityPolicy SecurityPolicy
-	on UserSecurity.intSecurityPolicyId = SecurityPolicy.intSecurityPolicyId
+FROM tblEMEntityCredential AS EntityCredential 
+	INNER JOIN vyuEMEntityContact AS vEntityContact
+		ON EntityCredential.intEntityId = (
+			CASE WHEN vEntityContact.ysnPortalAccess = 1
+				THEN vEntityContact.intEntityContactId
+				ELSE vEntityContact.intEntityId
+			END
+		)
+	LEFT JOIN tblSMUserSecurity UserSecurity 
+		ON EntityCredential.intEntityId = UserSecurity.intEntityUserSecurityId
+	LEFT JOIN tblSMSecurityPolicy SecurityPolicy
+		ON UserSecurity.intSecurityPolicyId = SecurityPolicy.intSecurityPolicyId 
 
 
