a6366c821167951e328a8d8259dc6c36854465ae
jeffrey trajano <jeffrey.trajano@irely.com>
2016-09-16 11:31:55 +0800

    GL-2916 Units does not compute properly in GL reports

--- i21Database/dbo/Functions/fnGLGetRetainedEarningSQLString.sql
+++ i21Database/dbo/Functions/fnGLGetRetainedEarningSQLString.sql
@@ -21,8 +21,6 @@ DECLARE @return NVARCHAR(MAX)
 IF @dtmDateTo = ''  SET @dtmDateTo = '2100/01/01'
 IF @dtmDateFrom = '' SET @dtmDateFrom = '1900/01/01'
 
-
-
 SELECT @dtmDateFrom =CONVERT(VARCHAR(10), CONVERT(DATE,@dtmDateFrom) ,111)
 SELECT @dtmDateTo = CONVERT(VARCHAR(10), CONVERT(DATE,@dtmDateTo) ,111)
 
@@ -62,50 +60,53 @@ END
 DECLARE @cols2 NVARCHAR (MAX) = 'strCompanyName,AccountHeader, strAccountDescription,strAccountType = '''',strAccountGroup = '''',dtmDate,strBatchId = '''',dblDebit,dblCredit,dblDebitUnit,dblCreditUnit,strDetailDescription='''',strTransactionId, intTransactionId = 0,strTransactionType = '''',strTransactionForm= '''',strModuleName='''',strReference ,strReferenceDetail='''',strDocument='''',dblTotal=dblCredit-dblDebit,intAccountUnitId = 0,strCode = '''',intGLDetailId= 0,ysnIsUnposted=0,strAccountId,[Primary Account],Location='''',strUOMCode='''',dblBeginBalance,dblBeginBalanceUnit'
 SET @cols2 = REPLACE(@cols2, 'AccountHeader', 'strAccountId + '' - '' + strAccountDescription + ''(Retained Earnings)'' as AccountHeader')
 INSERT INTO @tbl
-SELECT ',cteRetain as(
-select sum(dblDebit) dblDebit,sum(dblCredit) dblCredit,
-SUM(dblDebitUnit) dblDebitUnit, sum(dblCreditUnit) dblCreditUnit,
-intAccountId, ysnIsUnposted, dtmDate from tblGLDetail
-
-GROUP by intAccountId, ysnIsUnposted, dtmDate
-HAVING ysnIsUnposted = 0 AND dtmDate >= ''' + @dtmDateFrom  + ''' AND dtmDate < ''' + @dtmDateTo + '''),
-						 cteRetain1 AS
-(select
-CAST(CONVERT(VARCHAR(4),YEAR(dtmDate)) + ''-'' +  CONVERT(VARCHAR(2), MONTH (dtmDate)) + ''-'' + ''1'' AS DATE) AS dtmDate,
-Account.strAccountId strAccountId,
-Segment.[Primary Account] [Primary Account],
-Account.strDescription AS strAccountDescription,
-DATENAME(YEAR,dtmDate) + ''-'' + CAST(MONTH(dtmDate) AS VARCHAR(4))  as strTransactionId,
-DATENAME(MONTH,dtmDate) + '' '' +  DATENAME(YEAR,dtmDate) + '' '' + c.strAccountType as strReference,
-0 AS dblBeginBalance,
-0 AS dblBeginBalanceUnit,
-SUM(dblDebit) dblDebit,
-SUM(dblCredit) dblCredit,
-SUM(dblDebitUnit) dblDebitUnit,
-SUM(dblCreditUnit) dblCreditUnit,
-Company.strCompanyName AS strCompanyName,
-'''' AS strCode,
-Grop.strAccountType
-FROM cteRetain a
-JOIN tblGLAccount b ON a.intAccountId = b.intAccountId
-JOIN tblGLAccountGroup c on b.intAccountGroupId = c.intAccountGroupId
-OUTER APPLY (SELECT TOP 1 g.strAccountType FROM tblGLFiscalYear f  join tblGLAccount a on f.intRetainAccount = a.intAccountId join tblGLAccountGroup g on g.intAccountGroupId = a.intAccountGroupId)Grop
-OUTER APPLY (SELECT TOP 1 strCompanyName FROM tblSMCompanySetup) Company
-
-OUTER APPLY (select top 1 s.[Primary Account] FROM tblGLFiscalYear F join tblGLTempCOASegment s on F.intRetainAccount = s.intAccountId) Segment
-OUTER APPLY (select top 1 strAccountId,strDescription FROM tblGLFiscalYear F join tblGLAccount A on F.intRetainAccount = A.intAccountId) Account
-
-where c.strAccountType in (''Revenue'', ''Expense'')
-group by
-c.strAccountType,
-DATENAME(YEAR,dtmDate) + ''-'' + CAST(MONTH(dtmDate) AS VARCHAR(4)),
-CAST(CONVERT(VARCHAR(4),YEAR(dtmDate)) + ''-'' +  CONVERT(VARCHAR(2), MONTH (dtmDate)) + ''-'' + ''1'' AS DATE),
-DATENAME(MONTH,dtmDate) + '' '' +  DATENAME(YEAR,dtmDate),
-strCompanyName,Account.strAccountId,Account.strDescription,Grop.strAccountType, [Primary Account]),' + @strCteName + ' AS( SELECT ' + @cols2 + ' FROM cteRetain1 ' + CASE
+SELECT 
+',cteRetain as(
+	select sum(dblDebit) dblDebit,sum(dblCredit) dblCredit,
+	SUM(dblDebitUnit) dblDebitUnit, sum(dblCreditUnit) dblCreditUnit,
+	intAccountId, ysnIsUnposted, dtmDate from tblGLDetail
+	GROUP by intAccountId, ysnIsUnposted, dtmDate
+	HAVING ysnIsUnposted = 0 AND dtmDate >= ''' + @dtmDateFrom  + ''' AND dtmDate < ''' + @dtmDateTo + ''')
+,cteRetain1 AS
+(
+	select
+	CAST(CONVERT(VARCHAR(4),YEAR(dtmDate)) + ''-'' +  CONVERT(VARCHAR(2), MONTH (dtmDate)) + ''-'' + ''1'' AS DATE) AS dtmDate,
+	Account.strAccountId strAccountId,
+	Segment.[Primary Account] [Primary Account],
+	Account.strDescription AS strAccountDescription,
+	DATENAME(YEAR,dtmDate) + ''-'' + CAST(MONTH(dtmDate) AS VARCHAR(4))  as strTransactionId,
+	DATENAME(MONTH,dtmDate) + '' '' +  DATENAME(YEAR,dtmDate) + '' '' + c.strAccountType as strReference,
+	0 AS dblBeginBalance,
+	0 AS dblBeginBalanceUnit,
+	SUM(dblDebit) dblDebit,
+	SUM(dblCredit) dblCredit,
+	SUM(Debit.Unit) dblDebitUnit,
+	SUM(Credit.Unit) dblCreditUnit,
+	Company.strCompanyName AS strCompanyName,
+	'''' AS strCode,
+	Grop.strAccountType
+	FROM cteRetain a
+	JOIN tblGLAccount b ON a.intAccountId = b.intAccountId
+	JOIN tblGLAccountGroup c on b.intAccountGroupId = c.intAccountGroupId
+	OUTER APPLY (SELECT TOP 1 g.strAccountType FROM tblGLFiscalYear f  join tblGLAccount a on f.intRetainAccount = a.intAccountId join tblGLAccountGroup g on g.intAccountGroupId = a.intAccountGroupId)Grop
+	OUTER APPLY (SELECT TOP 1 strCompanyName FROM tblSMCompanySetup) Company
+	OUTER APPLY (select top 1 s.[Primary Account] FROM tblGLFiscalYear F join tblGLTempCOASegment s on F.intRetainAccount = s.intAccountId) Segment
+	OUTER APPLY (select top 1 intAccountId, strAccountId,strDescription FROM tblGLFiscalYear F join tblGLAccount A on F.intRetainAccount = A.intAccountId) Account
+	OUTER APPLY (SELECT dblLbsPerUnit,[strUOMCode] FROM Units WHERE [intAccountId] = Account.intAccountId) U
+	OUTER APPLY dbo.fnGLGetAccountUnit(a.dblDebitUnit, U.dblLbsPerUnit) Debit
+	OUTER APPLY dbo.fnGLGetAccountUnit(a.dblCreditUnit, U.dblLbsPerUnit) Credit
+	WHERE c.strAccountType in (''Revenue'', ''Expense'')
+	GROUP BY
+	c.strAccountType,
+	DATENAME(YEAR,dtmDate) + ''-'' + CAST(MONTH(dtmDate) AS VARCHAR(4)),
+	CAST(CONVERT(VARCHAR(4),YEAR(dtmDate)) + ''-'' +  CONVERT(VARCHAR(2), MONTH (dtmDate)) + ''-'' + ''1'' AS DATE),
+	DATENAME(MONTH,dtmDate) + '' '' +  DATENAME(YEAR,dtmDate),
+	strCompanyName,Account.strAccountId,Account.strDescription,Grop.strAccountType, [Primary Account]),' + @strCteName + ' AS( SELECT ' + @cols2 + ' FROM cteRetain1 ' + CASE
 																																									 WHEN @Where = 'Where' THEN + ')'
 																																									 ELSE + @Where + ')'
 																																								 END , @dtmDateTo , @multiFiscal
-
 RETURN
 
 END
+GO
+
--- i21Database/dbo/Stored Procedures/uspGLGetAccountDetailReport.sql
+++ i21Database/dbo/Stored Procedures/uspGLGetAccountDetailReport.sql
@@ -5,39 +5,39 @@ AS
    SET NOCOUNT ON;
    IF (ISNULL(@xmlParam,'')  = '')
    BEGIN
-	SELECT DISTINCT
-	'' AS strCompanyName,
-	'' AS AccountHeader,
-	'' AS strAccountDescription,
-	'' AS strAccountType,
-	'' AS strAccountGroup,
-	getdate() AS dtmDate,
-	'' AS strBatchId,
-	0.0  AS dblDebit,
-	0.0 AS dblCredit,
-	0.0 AS dblDebitUnit,
-	0.0 AS dblCreditUnit,
-	'' AS strDetailDescription,
-	'' AS strTransactionId,
-	0 AS intTransactionId,
-	'' AS strTransactionType,
-	'' AS strTransactionForm ,
-	'' AS strModuleName,
-	'' AS strReference,
-	'' AS strReferenceDetail,
-	'' AS strDocument,
-	0.0 AS dblTotal,
-	0 AS intAccountUnitId,
-	'' AS strCode,
-	0 AS intGLDetailId,
-	0 AS ysnIsUnposted,
-	'' AS strAccountId,
-	'' AS [Primary Account],
-	'' AS Location,
-	'' AS strUOMCode,
-	0.0 AS dblBeginBalance,
-	0.0 AS dblBeginBalanceUnit
-	RETURN;
+		SELECT DISTINCT
+		'' AS strCompanyName,
+		'' AS AccountHeader,
+		'' AS strAccountDescription,
+		'' AS strAccountType,
+		'' AS strAccountGroup,
+		getdate() AS dtmDate,
+		'' AS strBatchId,
+		0.0  AS dblDebit,
+		0.0 AS dblCredit,
+		0.0 AS dblDebitUnit,
+		0.0 AS dblCreditUnit,
+		'' AS strDetailDescription,
+		'' AS strTransactionId,
+		0 AS intTransactionId,
+		'' AS strTransactionType,
+		'' AS strTransactionForm ,
+		'' AS strModuleName,
+		'' AS strReference,
+		'' AS strReferenceDetail,
+		'' AS strDocument,
+		0.0 AS dblTotal,
+		0 AS intAccountUnitId,
+		'' AS strCode,
+		0 AS intGLDetailId,
+		0 AS ysnIsUnposted,
+		'' AS strAccountId,
+		'' AS [Primary Account],
+		'' AS Location,
+		'' AS strUOMCode,
+		0.0 AS dblBeginBalance,
+		0.0 AS dblBeginBalanceUnit
+		RETURN;
    END
    --SET FMTONLY off;
    DECLARE @idoc INT
@@ -53,11 +53,11 @@ AS
 
    IF @xmlParam <> ''
    BEGIN
-	exec sp_xml_preparedocument @idoc output, @xmlParam
-	insert into @filterTable
-	SELECT  *
-	FROM    OPENXML(@idoc, '/xmlparam/filters/filter', 2)
-		 with ([fieldname] nvarchar(50)
+		EXEC sp_xml_preparedocument @idoc OUTPUT, @xmlParam
+		INSERT INTO @filterTable
+		SELECT  *
+		FROM    OPENXML(@idoc, '/xmlparam/filters/filter', 2)
+		WITH ([fieldname] nvarchar(50)
 						, [condition] nvarchar(20)
 						, [from] nvarchar(50)
 						, [to] nvarchar(50)
@@ -66,36 +66,45 @@ AS
 						, [endgroup] nvarchar(50)
 						, [datatype] nvarchar(50))
 
-	DELETE FROM @filterTable WHERE [from] IS NULL OR RTRIM([from]) = ''
-	update @filterTable SET [fieldname] = 'strCode',[from] = '' , [condition]= 'Not Equal To' WHERE fieldname = 'ysnIncludeAuditAdjustment' AND [from] = 'Yes'
-	update @filterTable SET [fieldname] = 'strCode',[from] = 'AA' , [condition]= 'Not Equal To' WHERE fieldname = 'ysnIncludeAuditAdjustment' AND [from] = 'No'
-	update @filterTable SET [fieldname] = '[Primary Account]' WHERE fieldname = 'PrimaryAccount'
-	update @filterTable SET [fieldname] = '[Primary Account]' WHERE fieldname = 'Primary Account'
-	delete FROM @filterTable WHERE [condition]= 'All Date'
+		DELETE FROM @filterTable WHERE [from] IS NULL OR RTRIM([from]) = ''
+		UPDATE @filterTable SET [fieldname] = 'strCode',[from] = '' , [condition]= 'Not Equal To' WHERE fieldname = 'ysnIncludeAuditAdjustment' AND [from] = 'Yes'
+		UPDATE @filterTable SET [fieldname] = 'strCode',[from] = 'AA' , [condition]= 'Not Equal To' WHERE fieldname = 'ysnIncludeAuditAdjustment' AND [from] = 'No'
+		UPDATE @filterTable SET [fieldname] = '[Primary Account]' WHERE fieldname = 'PrimaryAccount'
+		UPDATE @filterTable SET [fieldname] = '[Primary Account]' WHERE fieldname = 'Primary Account'
+		DELETE FROM @filterTable WHERE [condition]= 'All Date'
+		SELECT TOP 1 @strAccountIdFrom= ISNULL([from],'') , @strAccountIdTo = ISNULL([to],'') ,@strAccountIdCondition =ISNULL([condition],'') from  @filterTable WHERE [fieldname] = 'strAccountId'
+		SELECT TOP 1 @strPrimaryCodeFrom= ISNULL([from],'') , @strPrimaryCodeTo = ISNULL([to],'') ,@strPrimaryCodeCondition =ISNULL([condition],'') from  @filterTable WHERE [fieldname] = '[Primary Account]'
+		SELECT TOP 1 @dtmDateFrom= ISNULL([from],'') , @dtmDateTo = ISNULL([to],'') from  @filterTable WHERE [fieldname] = 'dtmDate'
 
+		IF EXISTS(
+				SELECT TOP 1 1 FROM @filterTable WHERE
+				(
+					condition LIKE  '%Date'  or
+					condition like '%Month' or
+					condition like '%Period' or
+					condition like '%Year' or
+					condition like '%Quarter' or
+					condition = 'As Of') AND ([from] ='' OR [to] =''))
+		BEGIN
+			RAISERROR (N'Between condition needs from and to parameter to be present.',10, 1);
+			RETURN
+		END
 
-	SELECT TOP 1 @strAccountIdFrom= ISNULL([from],'') , @strAccountIdTo = ISNULL([to],'') ,@strAccountIdCondition =ISNULL([condition],'') from  @filterTable WHERE [fieldname] = 'strAccountId'
-	SELECT TOP 1 @strPrimaryCodeFrom= ISNULL([from],'') , @strPrimaryCodeTo = ISNULL([to],'') ,@strPrimaryCodeCondition =ISNULL([condition],'') from  @filterTable WHERE [fieldname] = '[Primary Account]'
-	SELECT TOP 1 @dtmDateFrom= ISNULL([from],'') , @dtmDateTo = ISNULL([to],'') from  @filterTable WHERE [fieldname] = 'dtmDate'
-
-
-
-
-	IF EXISTS(
-	SELECT TOP 1 1 FROM @filterTable WHERE
-		(condition LIKE  '%Date'  or
-				 condition like '%Month' or
-				 condition like '%Period' or
-				 condition like '%Year' or
-				 condition like '%Quarter' or
-				 condition = 'As Of') AND ([from] ='' OR [to] =''))
-	BEGIN
-		RAISERROR (N'Between condition needs from and to parameter to be present.',10, 1);
-		RETURN
-	END
-
+		DECLARE @sqlCte NVARCHAR(MAX) = 
+		dbo.[fnGLGetAccountDetailSQL]
+		(
+			@strAccountIdFrom,
+			@dtmDateFrom,
+			@dtmDateTo,
+			@filterTable
+		)
+		SELECT @sqlCte += 'select * from result'
+		EXEC (@sqlCte)
    END
+END
+GO
 
+<<<<<<< HEAD
    DECLARE @Where NVARCHAR(MAX) = dbo.fnConvertFilterTableToWhereExpression (@filterTable)
    DECLARE @cols NVARCHAR (MAX) = 'strCompanyName,AccountHeader, strAccountDescription,strAccountType,strAccountGroup,dtmDate,strBatchId,dblDebit,dblCredit,dblDebitUnit,dblCreditUnit,strDetailDescription,strTransactionId, intTransactionId,strTransactionType,strTransactionForm,strModuleName,strReference,strReferenceDetail,strDocument,dblTotal,intAccountUnitId,strCode,intGLDetailId,ysnIsUnposted,strAccountId,[Primary Account],Location,strUOMCode,dblBeginBalance,dblBeginBalanceUnit'
    DECLARE @sqlCte NVARCHAR(MAX)
@@ -262,3 +271,5 @@ AS
 
 	EXEC (@sqlCte)
 END
+=======
+>>>>>>> ea16ff1... GL-2916 Units does not compute properly in GL reports
