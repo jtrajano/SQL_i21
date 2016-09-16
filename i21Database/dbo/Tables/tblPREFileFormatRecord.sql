a086f5f3388620df1578925d966bbe80cb506e35
Arman Arenas <arman.arenas@irely.com>
2016-09-16 15:44:57 +0800

    PR-873 - added tblPREFileFormatRecord.strFilterField and strFieldValue

--- i21Database/dbo/Tables/tblPREFileFormatRecord.sql
+++ i21Database/dbo/Tables/tblPREFileFormatRecord.sql
@@ -6,6 +6,8 @@
     [strDescription] NVARCHAR(200) COLLATE Latin1_General_CI_AS NULL, 
     [intRecordType] INT NULL, 
     [strDatasource] NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL, 
+	[strFilterField] NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL, 
+	[strFilterValue] NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL, 
     [intConcurrencyId] INT NOT NULL DEFAULT ((1)), 
     CONSTRAINT [FK_tblPREFileFormatRecord_tblPREFileFormat] FOREIGN KEY (intEFileFormatId) REFERENCES tblPREFileFormat(intEFileFormatId) ON DELETE CASCADE
 )
