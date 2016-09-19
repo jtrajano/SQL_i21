b8c480f1289f2d14023c205469257a797a67daf1
kenneth lagunero <kenneth.lagunero@irely.com>
2016-09-19 23:07:51 +0800

    TM-1922, TM-1997

---
+++ i21Database/dbo/Views/vyuTMForPrintDeliveryTicket.sql
@@ -0,0 +1,46 @@
+﻿CREATE VIEW [dbo].[vyuTMForPrintDeliveryTicket]  
+AS  
+SELECT 
+	strLocation = G.strLocationName
+	,strItemNo = CASE WHEN M.intItemId IS NULL THEN F.strItemNo ELSE M.strItemNo END
+	,strRoute = J.strRouteId
+	,strDriverId = L.strEntityNo
+	,dtmRequestedDate = E.dtmRequestedDate
+	,ysnCallEntryPrinted = E.ysnCallEntryPrinted
+	,strDeliveryTicketFormat = K.strDeliveryTicketFormat
+	,strDeliveryTicketPrinter = K.strDeliveryTicketPrinter
+	,intDispatchId = E.intDispatchID
+	,intCustomerEntityId = C.intEntityId
+	,intSiteId = A.intSiteID
+	,intLocationId = A.intLocationId
+	,intItemId = CASE WHEN M.intItemId IS NULL THEN F.intItemId ELSE M.intItemId END
+	,intRouteId = A.intRouteId
+	,intDriverID = E.intDriverID
+	,intConcurrencyId = E.intConcurrencyId
+FROM tblTMSite A
+INNER JOIN tblTMCustomer B
+	ON A.intCustomerID = B.intCustomerID
+INNER JOIN tblEMEntity C
+	ON B.intCustomerNumber = C.intEntityId
+INNER JOIN tblARCustomer D 
+	ON C.intEntityId = D.intEntityCustomerId
+INNER JOIN tblTMDispatch E
+	ON A.intSiteID = E.intSiteID
+INNER JOIN tblICItem F
+	ON E.intProductID = F.intItemId
+INNER JOIN tblSMCompanyLocation G
+	ON A.intLocationId = G.intCompanyLocationId
+LEFT JOIN tblICItem H
+	ON E.intProductID = H.intItemId
+LEFT JOIN tblTMFillMethod I
+	ON A.intFillMethodId = I.intFillMethodId
+LEFT JOIN tblTMRoute J
+	ON A.intRouteId = J.intRouteId
+LEFT JOIN tblTMClock K
+	ON A.intClockID = K.intClockID
+LEFT JOIN tblEMEntity L
+	ON E.intDriverID = L.intEntityId
+LEFT JOIN tblICItem M
+	ON E.intSubstituteProductID = M.intItemId
+
+GO
