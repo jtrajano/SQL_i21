41a015f4d7fda5221f295a91a900627b657e4361
Erick Dela Cruz <erick.delacruz@irely.com>
2016-09-21 01:50:57 +0800

    AP-2775

---
+++ i21Database/dbo/Views/vyuAPBatchPostTransaction.sql
@@ -0,0 +1,46 @@
+﻿CREATE VIEW [dbo].[vyuAPBatchPostTransaction] 
+AS
+SELECT 'Voucher' COLLATE Latin1_General_CI_AS AS strTransactionType,
+       A.intBillId,
+       A.strBillId,
+       A.dblTotal,
+       A.strVendorOrderNumber,
+       A.intEntityVendorId,
+       A.intEntityId,
+       A.dtmDate,
+       A.strComment AS strReference,
+       NULL AS intCompanyLocationId
+FROM tblAPBill A
+WHERE intTransactionType = 1
+ AND ISNULL(ysnPosted, 0) = 0
+ AND NOT EXISTS(
+	SELECT 1 FROM vyuAPForApprovalTransaction B WHERE A.intBillId = B.intTransactionId AND B.strScreenName = 'Voucher'
+ )
+UNION ALL
+SELECT 'Payable' AS strTransactionType,
+       intPaymentId,
+       strPaymentRecordNum,
+       dblAmountPaid,
+       '' AS strVendorInvoiceNumber,
+       intEntityVendorId AS intEntityVendorId,
+       intEntityId,
+       dtmDatePaid,
+       strNotes AS strReference,
+       NULL AS intCompanyLocationId
+FROM tblAPPayment
+WHERE ysnPosted = 0
+UNION ALL
+SELECT 'Debit Memo' AS strTransactionType,
+       intBillId,
+       strBillId,
+       dblTotal,
+       strVendorOrderNumber,
+       intEntityVendorId,
+       intEntityId,
+       dtmDate,
+       strComment AS strReference,
+       NULL AS intCompanyLocationId
+FROM tblAPBill
+WHERE intTransactionType = 3
+  AND ISNULL(ysnPosted, 0) = 0
+  AND intTransactionType != 6
---
+++ i21Database/dbo/Views/vyuAPForApprovalTransaction.sql
@@ -0,0 +1,18 @@
+﻿CREATE VIEW [dbo].[vyuAPForApprovalTransaction]
+AS
+
+SELECT
+	A.intScreenId
+	,A.strScreenName
+	,APTransaction.intTransactionId
+	,APTransaction.strTransactionNo
+FROM tblSMScreen A
+CROSS APPLY (
+	SELECT
+		B.strTransactionNo, CAST(B.strRecordNo AS INT) intTransactionId
+	FROM tblSMTransaction B
+	WHERE A.intScreenId = B.intScreenId
+	AND (B.strApprovalStatus IS NOT NULL AND B.strApprovalStatus NOT IN ('No Need for Approval','Approved'))
+) APTransaction
+WHERE A.strScreenName IN ('Purchase Order','Voucher')
+
