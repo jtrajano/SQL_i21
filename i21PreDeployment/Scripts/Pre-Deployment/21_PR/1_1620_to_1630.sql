15485662d667fc050e94e5cedeef5828b6306488
Arman Arenas <arman.arenas@irely.com>
2016-09-20 10:21:47 +0800

    PR-882 fixes on predeployment datafix

--- i21PreDeployment/Scripts/Pre-Deployment/21_PR/1_1620_to_1630.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/21_PR/1_1620_to_1630.sql
@@ -5,16 +5,16 @@ GO
 /* Clean-up tblPRPaycheckEarning.intEmployeeDepartmentId */
 IF EXISTS (SELECT TOP 1 1 FROM sys.columns WHERE NAME  = N'intEmployeeDepartmentId' AND OBJECT_ID = OBJECT_ID(N'tblPRPaycheckEarning')) 
 BEGIN
-    UPDATE tblPRPaycheckEarning SET intEmployeeDepartmentId = NULL 
-	WHERE intEmployeeDepartmentId NOT IN (SELECT intDepartmentId FROM tblPRDepartment) AND intEmployeeDepartmentId IS NOT NULL
+    EXEC('UPDATE tblPRPaycheckEarning SET intEmployeeDepartmentId = NULL 
+		WHERE intEmployeeDepartmentId NOT IN (SELECT intDepartmentId FROM tblPRDepartment) AND intEmployeeDepartmentId IS NOT NULL')
 END
 GO
 
 /* Clean-up tblPRPayGroupDetail.intDepartmentId */
 IF EXISTS (SELECT TOP 1 1 FROM sys.columns WHERE NAME  = N'intDepartmentId' AND OBJECT_ID = OBJECT_ID(N'tblPRPayGroupDetail')) 
 BEGIN
-    UPDATE tblPRPayGroupDetail SET intDepartmentId = NULL 
-	WHERE intDepartmentId NOT IN (SELECT intDepartmentId FROM tblPRDepartment) AND intEmployeeDepartmentId IS NOT NULL
+    EXEC('UPDATE tblPRPayGroupDetail SET intDepartmentId = NULL 
+		WHERE intDepartmentId NOT IN (SELECT intDepartmentId FROM tblPRDepartment) AND intDepartmentId IS NOT NULL')
 END
 GO
 
