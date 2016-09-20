9ddc4fccc7cf724981dc1fead9279098f1d66377
Wendell Wayne Estrada <wendell.estrada@irely.com>
2016-09-20 13:13:44 +0800

    IC-2029 HDTN-179225 - Need to See more Data on Inventory Valuation for auditing

--- i21Database/dbo/Views/vyuICGetInventoryValuation.sql
+++ i21Database/dbo/Views/vyuICGetInventoryValuation.sql
@@ -6,7 +6,7 @@ SELECT	intInventoryValuationKeyId  =
 						CAST(ROW_NUMBER() OVER (ORDER BY t.intInventoryTransactionId) AS INT)
 					ELSE 
 						t.intInventoryTransactionId
-			END 
+			END
 		,t.intInventoryTransactionId
 		,t.intItemId
 		,strItemNo				= i.strItemNo
@@ -35,7 +35,11 @@ SELECT	intInventoryValuationKeyId  =
 		,strUOM						= umTransUOM.strUnitMeasure
 		,strStockUOM				= umStock.strUnitMeasure
 		,dblQuantityInStockUOM		= dbo.fnCalculateQtyBetweenUOM(t.intItemUOMId, iuStock.intItemUOMId, t.dblQty) 
-		,dblCostInStockUOM			= dbo.fnCalculateCostBetweenUOM(t.intItemUOMId, iuStock.intItemUOMId, t.dblCost) 
+		,dblCostInStockUOM			= dbo.fnCalculateCostBetweenUOM(t.intItemUOMId, iuStock.intItemUOMId, t.dblCost)
+		,strBOLNumber				= CASE ty.intTransactionTypeId WHEN 4 THEN receipt.strBillOfLading WHEN 5 THEN shipment.strBOLNumber ELSE NULL END
+		,strEntity					= CASE ty.intTransactionTypeId WHEN 4 THEN vendor.strName WHEN 5 THEN customer.strName ELSE NULL END
+		,lot.strLotNumber
+		,strAdjustedTransaction		= t.strRelatedTransactionId
 FROM 	tblICItem i LEFT JOIN tblICItemUOM iuStock
 			ON iuStock.intItemId = i.intItemId
 			AND iuStock.ysnStockUnit = 1
@@ -61,4 +65,11 @@ FROM 	tblICItem i LEFT JOIN tblICItemUOM iuStock
 			ON strgLoc.intStorageLocationId = t.intStorageLocationId
 		LEFT JOIN tblICInventoryTransactionType ty 
 			ON ty.intTransactionTypeId = t.intTransactionTypeId
+		LEFT JOIN tblICInventoryReceipt receipt ON receipt.intInventoryReceiptId = t.intTransactionId
+			AND t.intTransactionTypeId = 4
+		LEFT JOIN tblEMEntity vendor ON vendor.intEntityId = receipt.intEntityVendorId
+		LEFT JOIN tblICInventoryShipment shipment ON shipment.intInventoryShipmentId = t.intTransactionId
+			AND t.intTransactionTypeId = 5
+		LEFT JOIN tblEMEntity customer ON customer.intEntityId = shipment.intEntityCustomerId
+		LEFT JOIN tblICInventoryReceiptItemLot lot ON lot.intLotId = t.intLotId
 WHERE	i.strType != 'Comment'
