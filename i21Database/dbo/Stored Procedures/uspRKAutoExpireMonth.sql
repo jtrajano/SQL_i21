720454faaa547a1aff324592d11280d1960f1c64
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2016-09-20 15:40:16 +0530

    RM-194

---
+++ i21Database/dbo/Stored Procedures/uspRKAutoExpireMonth.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE uspRKAutoExpireMonth
+AS
+
+DECLARE @ysnAutoExpire bit
+
+SELECT @ysnAutoExpire = isnull(ysnAutoExpire,0) FROM tblRKCompanyPreference
+if @ysnAutoExpire=1
+BEGIN
+update tblRKFuturesMonth set ysnExpired = 1 where dtmLastTradingDate < getdate() and ysnExpired <> 1
+update tblRKOptionsMonth set ysnMonthExpired = 1 where dtmExpirationDate < getdate() and ysnMonthExpired <> 1
+END
