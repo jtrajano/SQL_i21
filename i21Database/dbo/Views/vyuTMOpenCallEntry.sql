0320251725aa3545d303283a53ddf880253cc5d5
kenneth lagunero <kenneth.lagunero@irely.com>
2016-09-21 18:05:31 +0800

    TM-2021

--- i21Database/dbo/Views/vyuTMOpenCallEntry.sql
+++ i21Database/dbo/Views/vyuTMOpenCallEntry.sql
@@ -28,6 +28,7 @@ AS
 		,intLocationId = B.intLocationId
 		,strLocation = B.strLocation
 		,ysnLeakCheckRequired = A.ysnLeakCheckRequired
+		,ysnCallEntryPrinted = ISNULL(A.ysnCallEntryPrinted,0)
 	FROM tblTMDispatch A
 	INNER JOIN tblTMSite B
 		ON A.intSiteID = B.intSiteID
