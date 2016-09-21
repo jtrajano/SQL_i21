4d1c2858b8a737cd608f670572664c75e6b0ee34
Gopinath Nayak <gopinath.n@irely.com>
2016-09-21 19:19:25 +0530

    GRN-458

---
+++ i21Database/dbo/Views/vyuGRGetDiscountIdByLocation.sql
@@ -0,0 +1,16 @@
+﻿CREATE VIEW [dbo].[vyuGRGetDiscountIdByLocation]
+AS     
+SELECT 
+ DId.intDiscountId
+,DId.strDiscountId
+,DId.strDiscountDescription
+,DId.ysnDiscountIdActive
+,DId.intCurrencyId
+,Cur.strCurrency
+,LC.intCompanyLocationId
+,L.strLocationName
+,LC.ysnDiscountLocationActive
+FROM tblGRDiscountId DId
+JOIN tblSMCurrency Cur ON Cur.intCurrencyID = DId.intCurrencyId
+JOIN tblGRDiscountLocationUse LC ON LC.intDiscountId=DId.intDiscountId
+JOIN tblSMCompanyLocation L ON L.intCompanyLocationId = LC.intCompanyLocationId 
