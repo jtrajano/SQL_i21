4bb38e98116f7325ce937a586643e86346cd4c47
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2016-09-21 18:10:50 +0530

    RM-484

--- i21Database/dbo/Views/vyuRKGetOpenContract.sql
+++ i21Database/dbo/Views/vyuRKGetOpenContract.sql
@@ -5,13 +5,29 @@ AS
 SELECT distinct intFutOptTransactionId,(intNoOfContract-isnull(intOpenContract,0)) intOpenContract from (
 SELECT ot.intFutOptTransactionId,sum(ot.intNoOfContract) intNoOfContract,
 	   (SELECT SUM(CONVERT(int,mf.dblMatchQty)) FROM tblRKMatchFuturesPSDetail mf where ot.intFutOptTransactionId=mf.intLFutOptTransactionId) intOpenContract
-FROM tblRKFutOptTransaction ot where ot.strBuySell='Buy'
-Group by intFutOptTransactionId) t
+FROM tblRKFutOptTransaction ot where ot.strBuySell='Buy' and intInstrumentTypeId = 1
+GROUP BY intFutOptTransactionId) t
 
 UNION 
 
 SELECT distinct intFutOptTransactionId,-(intNoOfContract-isnull(intOpenContract,0)) intOpenContract from (
 SELECT ot.intFutOptTransactionId,sum(ot.intNoOfContract) intNoOfContract,
 	   (SELECT SUM(CONVERT(int,mf.dblMatchQty)) FROM tblRKMatchFuturesPSDetail mf where ot.intFutOptTransactionId=mf.intSFutOptTransactionId) intOpenContract
-FROM tblRKFutOptTransaction ot where ot.strBuySell='Sell'
+FROM tblRKFutOptTransaction ot where ot.strBuySell='Sell' and intInstrumentTypeId = 1
+Group by intFutOptTransactionId) t
+
+UNION
+
+SELECT distinct intFutOptTransactionId,(isnull(intNoOfContract,0)-isnull(intOpenContract,0)) intOpenContract from (
+SELECT ot.intFutOptTransactionId,sum(isnull(ot.intNoOfContract,0)) intNoOfContract,
+	   (SELECT SUM(CONVERT(int,mf.intMatchQty)) FROM tblRKOptionsMatchPnS mf where ot.intFutOptTransactionId=mf.intLFutOptTransactionId) intOpenContract
+FROM tblRKFutOptTransaction ot where ot.strBuySell='Buy' and intInstrumentTypeId = 2
+Group by intFutOptTransactionId) t
+
+UNION 
+
+SELECT distinct intFutOptTransactionId,-(intNoOfContract-isnull(intOpenContract,0)) intOpenContract from (
+SELECT ot.intFutOptTransactionId,sum(ot.intNoOfContract) intNoOfContract,
+	   (SELECT SUM(CONVERT(int,mf.intMatchQty)) FROM tblRKOptionsMatchPnS mf where ot.intFutOptTransactionId=mf.intSFutOptTransactionId) intOpenContract
+FROM tblRKFutOptTransaction ot where ot.strBuySell='Sell' and intInstrumentTypeId = 2
 Group by intFutOptTransactionId) t
