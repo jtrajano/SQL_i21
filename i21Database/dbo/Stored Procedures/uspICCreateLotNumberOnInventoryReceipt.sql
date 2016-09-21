69fb3084a14e6ec883226c5a89f5f24d39d0fcbc
Joy Balangatan <joy.balangatan@irely.com>
2016-09-21 17:14:52 +0800

    IC-2766 Removed condition in Received Qty and Lot Qty validation that it will only validate if Gross/Net is not present

--- i21Database/dbo/Stored Procedures/uspICCreateLotNumberOnInventoryReceipt.sql
+++ i21Database/dbo/Stored Procedures/uspICCreateLotNumberOnInventoryReceipt.sql
@@ -106,7 +106,7 @@ BEGIN
 	WHERE	dbo.fnGetItemLotType(ReceiptItem.intItemId) IN (@LotType_Manual, @LotType_Serial)	
 			AND Receipt.strReceiptNumber = @strTransactionId
 			AND ROUND(ISNULL(ItemLot.TotalLotQtyInItemUOM, 0), 2) <> ReceiptItem.dblOpenReceive
-			AND ReceiptItem.intWeightUOMId IS NULL 
+			--AND ReceiptItem.intWeightUOMId IS NULL 
 			
 	IF @intItemId IS NOT NULL 
 	BEGIN 
