46d00df2c188333098edb52322a4d36bc0f4b2a7
Ruben  Escarcha <ruben.escarcha@irely.com>
2016-09-21 12:31:49 +0800

    AR-2591 Invoice Issue regarding Price and Total

--- i21PreDeployment/Scripts/Pre-Deployment/7_AR/5_InvoiceCurrencyCleanUp.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/7_AR/5_InvoiceCurrencyCleanUp.sql
@@ -1,10 +1,16 @@
 ﻿IF(EXISTS(SELECT NULL FROM sys.tables WHERE name = N'tblARInvoice') AND EXISTS(SELECT NULL FROM sys.columns WHERE [name] = N'intCurrencyId' AND [object_id] = OBJECT_ID(N'tblARInvoice'))
-			AND EXISTS(SELECT NULL FROM sys.tables WHERE name = N'tblSMCurrency') AND EXISTS(SELECT NULL FROM sys.columns WHERE [name] = N'intCurrencyID' AND [object_id] = OBJECT_ID(N'tblSMCurrency')))
+			AND EXISTS(SELECT NULL FROM sys.tables WHERE name = N'tblSMCurrency') AND EXISTS(SELECT NULL FROM sys.columns WHERE [name] = N'intCurrencyID' AND [object_id] = OBJECT_ID(N'tblSMCurrency'))
+			AND EXISTS(SELECT NULL FROM sys.tables WHERE name = N'tblSMCompanyPreference') AND EXISTS(SELECT NULL FROM sys.columns WHERE [name] = N'intDefaultCurrencyId' AND [object_id] = OBJECT_ID(N'tblSMCompanyPreference')))
 BEGIN
+	DECLARE @DefaultCurrencyId INT
+	SET @DefaultCurrencyId = (SELECT intDefaultCurrencyId FROM tblSMCompanyPreference) 
+
 	UPDATE
 		tblARInvoice
 	SET
-		intCurrencyId = NULL
+		intCurrencyId = @DefaultCurrencyId
 	WHERE
-		intCurrencyId NOT IN (SELECT intCurrencyID FROM tblSMCurrency)
+		NOT EXISTS(SELECT SMC.intCurrencyID FROM tblSMCurrency SMC WHERE SMC.intCurrencyID = tblARInvoice.intCurrencyId)
+		AND EXISTS(SELECT intCurrencyID FROM tblSMCurrency WHERE intCurrencyID = @DefaultCurrencyId)
+
 END
