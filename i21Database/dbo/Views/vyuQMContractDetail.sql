fa9e11a16676ec5f187aefec6b62e2ea68507d90
Vinoth Kumar T <vinoth.t@irely.com>
2016-09-21 19:45:42 +0530

    QC-190

--- i21Database/dbo/Views/vyuQMContractDetail.sql
+++ i21Database/dbo/Views/vyuQMContractDetail.sql
@@ -24,6 +24,10 @@ SELECT CD.intContractDetailId
 	,IC.strContractItemName
 	,IC.intCountryId AS intItemContractOriginId
 	,CG.strCountry AS strItemContractOrigin
+	,S.strSampleStatus
+	,S.strSampleNumber
+	,S.strContainerNumber
+	,S.strSampleTypeName
 FROM tblCTContractDetail CD
 JOIN tblCTContractHeader CH ON CH.intContractHeaderId = CD.intContractHeaderId
 JOIN tblSMCompanyLocation CL ON CL.intCompanyLocationId = CD.intCompanyLocationId
@@ -33,3 +37,21 @@ LEFT JOIN tblICUnitMeasure U1 ON U1.intUnitMeasureId = IU.intUnitMeasureId
 LEFT JOIN tblICCommodityAttribute CA ON CA.intCommodityAttributeId = IM.intOriginId
 LEFT JOIN tblICItemContract IC ON IC.intItemContractId = CD.intItemContractId
 LEFT JOIN tblSMCountry CG ON CG.intCountryID = IC.intCountryId
+LEFT JOIN (
+	SELECT *
+	FROM (
+		SELECT ROW_NUMBER() OVER (
+				PARTITION BY S.intContractDetailId ORDER BY S.intSampleId DESC
+				) intRowNum
+			,S.intContractDetailId
+			,S.strSampleNumber
+			,S.strContainerNumber
+			,ST.strSampleTypeName
+			,SS.strStatus AS strSampleStatus
+		FROM tblQMSample S
+		JOIN tblQMSampleType AS ST ON ST.intSampleTypeId = S.intSampleTypeId
+		JOIN tblQMSampleStatus AS SS ON SS.intSampleStatusId = S.intSampleStatusId
+		WHERE S.intContractDetailId IS NOT NULL
+		) t
+	WHERE intRowNum = 1
+	) S ON S.intContractDetailId = CD.intContractDetailId
