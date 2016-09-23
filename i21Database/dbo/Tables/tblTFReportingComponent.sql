2aa11e1b964c288f2c8e600e3a7324ec8616be68
Mark Paragas <mark.paragas@irely.com>
2016-09-24 01:15:04 +0800

    MFT-292 MFT-274

--- i21Database/dbo/Tables/tblTFReportingComponent.sql
+++ i21Database/dbo/Tables/tblTFReportingComponent.sql
@@ -9,10 +9,19 @@
     [strNote]                 NVARCHAR (300) COLLATE Latin1_General_CI_AS NULL,
 	[strTransactionType]      NVARCHAR (50) COLLATE Latin1_General_CI_AS NULL,
     [intPositionId]           INT           NULL,
-    [strSPInventory]        NVARCHAR(50)           CONSTRAINT [DF_tblTFReportingComponent_intConcurrencyId] DEFAULT ((1)) NULL,
-    [strSPInvoice] NVARCHAR(50) NULL, 
-    [strSPRunReport] NVARCHAR(50) NULL, 
+    [strSPInventory]        NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL,
+    [strSPInvoice] NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL,
+    [strSPRunReport] NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL,
     [intConcurrencyId] INT NULL, 
-    CONSTRAINT [PK_tblTFReportingComponent] PRIMARY KEY CLUSTERED ([intReportingComponentId] ASC)
-);
+    CONSTRAINT [PK_tblTFReportingComponent] PRIMARY KEY CLUSTERED 
+(
+	[intReportingComponentId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+ALTER TABLE [dbo].[tblTFReportingComponent] ADD  CONSTRAINT [DF_tblTFReportingComponent_intConcurrencyId]  DEFAULT ((1)) FOR [intConcurrencyId]
+GO
+
+
 
