3fd619cd7223f0036a31b0ea5129b27f0c2db77f
Jayson Ramos <jayson.ramos@iRely.com>
2016-09-26 17:26:57 +0800

    rename [tblHDOpportunityWinLossReason] to [tblCRMWinLossReason]

--- i21Database/dbo/Tables/tblCRMWinLossReason.sql
+++ i21Database/dbo/Tables/tblCRMWinLossReason.sql
@@ -1,9 +1,9 @@
-﻿CREATE TABLE [dbo].[tblHDOpportunityWinLossReason]
+﻿CREATE TABLE [dbo].[tblCRMWinLossReason]
 (
-	[intOpportunityWinLossReasonId] INT IDENTITY (1, 1) NOT NULL,
+	[intWinLossReasonId] INT IDENTITY (1, 1) NOT NULL,
 	[strReason] NVARCHAR(100) COLLATE Latin1_General_CI_AS NOT NULL,
 	[strDescription] NVARCHAR(255) COLLATE Latin1_General_CI_AS NULL,
     [intConcurrencyId] INT NOT NULL DEFAULT 1,
-	CONSTRAINT [PK_tblHDOpportunityWinLossReason] PRIMARY KEY CLUSTERED ([intOpportunityWinLossReasonId] ASC),
+	CONSTRAINT [PK_tblCRMWinLossReason] PRIMARY KEY CLUSTERED ([intWinLossReasonId] ASC),
     CONSTRAINT [UNQ_tblHDOpportunityWinLossReason_strReason] UNIQUE ([strReason])
 )
