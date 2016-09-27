672b1cdf35d25adf8d13591e3d2ad9d150626698
Joy Balangatan <joy.balangatan@irely.com>
2016-09-27 21:09:03 +0800

    IC-2072 Added logic for buttons Select All and Clear All in Incoming Inspection grid

--- app/view/InventoryReceiptViewController.js
+++ app/view/InventoryReceiptViewController.js
@@ -5083,6 +5083,31 @@ Ext.define('Inventory.view.InventoryReceiptViewController', {
         }
     },
 
+    onSelectClearGridInspectionClick: function (button, e, eOpts) {
+        var win = button.up('window');
+        var current = win.viewModel.data.current;
+
+        if(button.itemId === 'btnSelectAll') {
+            if (current.tblICInventoryReceiptInspections()) {
+                Ext.Array.each(current.tblICInventoryReceiptInspections().data.items, function(row) {
+                    if (!row.dummy) {
+                            row.set('ysnSelected', true);
+                        }
+                    });
+            }
+        }
+
+        if(button.itemId === 'btnClearAll') {
+            if (current.tblICInventoryReceiptInspections()) {
+                Ext.Array.each(current.tblICInventoryReceiptInspections().data.items, function(row) {
+                    if (!row.dummy) {
+                            row.set('ysnSelected', false);
+                        }
+                    });
+            }
+        }
+    },
+
     init: function (application) {
         this.control({
             "#cboVendor": {
@@ -5239,6 +5264,12 @@ Ext.define('Inventory.view.InventoryReceiptViewController', {
             },
             "#tabInventoryReceipt": {
                 tabChange: this.onReceiptTabChange
+            },
+            "#btnSelectAll": {
+                click: this.onSelectClearGridInspectionClick
+            },
+            "#btnClearAll": {
+                click: this.onSelectClearGridInspectionClick
             }
         })
     }
