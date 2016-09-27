3b24cfc46bb1ebd4d459b20deaefb87d5043508f
Jayson Ramos <jayson.ramos@iRely.com>
2016-09-27 10:56:34 +0800

    Added tblCRMOpportunityOverviewProblem table for CRM.

---
+++ i21Database/dbo/Tables/tblCRMOpportunityOverviewProblem.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[tblCRMOpportunityOverviewProblem]
+(
+	[intOpportunityOverviewId] [int] IDENTITY(1,1) NOT NULL,
+	[intOpportunityId] [int] NOT NULL,
+	[strDescription] [nvarchar](MAX) COLLATE Latin1_General_CI_AS NOT NULL,
+	[intEntityId] [int] NULL,
+	[strOverviewType] [nvarchar](100) COLLATE Latin1_General_CI_AS NOT NULL,
+	[intConcurrencyId] [int] NOT NULL DEFAULT 1,
+	CONSTRAINT [PK_tblCRMOpportunityOverview] PRIMARY KEY CLUSTERED ([intOpportunityOverviewId] ASC),
+    CONSTRAINT [FK_tblCRMOpportunityOverview_tblCRMOpportunity] FOREIGN KEY ([intOpportunityId]) REFERENCES [dbo].[tblCRMOpportunity] ([intOpportunityId]),
+    CONSTRAINT [FK_tblCRMOpportunityOverview_tblEMEntity] FOREIGN KEY ([intEntityId]) REFERENCES [dbo].tblEMEntity ([intEntityId])
+)
