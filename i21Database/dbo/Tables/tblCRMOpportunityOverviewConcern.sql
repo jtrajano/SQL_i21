e2ab5d06d0d8b1ddf4a7ecfc29c87ce6d62e266e
Jayson Ramos <jayson.ramos@iRely.com>
2016-09-27 11:21:37 +0800

    Addedd tblCRMOpportunityOverviewConcern table for CRM

---
+++ i21Database/dbo/Tables/tblCRMOpportunityOverviewConcern.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[tblCRMOpportunityOverviewConcern]
+(
+	[intOpportunityOverviewConcernId] [int] IDENTITY(1,1) NOT NULL,
+	[intOpportunityId] [int] NOT NULL,
+	[strDescription] [nvarchar](MAX) COLLATE Latin1_General_CI_AS NOT NULL,
+	[intEntityId] [int] NULL,
+	[strOverviewType] [nvarchar](100) COLLATE Latin1_General_CI_AS NOT NULL,
+	[intConcurrencyId] [int] NOT NULL DEFAULT 1,
+	CONSTRAINT [PK_tblCRMOpportunityOverviewConcern_intOpportunityOverviewConcernId] PRIMARY KEY CLUSTERED ([intOpportunityOverviewConcernId] ASC),
+    CONSTRAINT [FK_tblCRMOpportunityOverviewConcern_tblCRMOpportunity_intOpportunityId] FOREIGN KEY ([intOpportunityId]) REFERENCES [dbo].[tblCRMOpportunity] ([intOpportunityId]),
+    CONSTRAINT [FK_tblCRMOpportunityOverviewConcern_tblEMEntity] FOREIGN KEY ([intEntityId]) REFERENCES [dbo].tblEMEntity ([intEntityId])
+)
+
