945d11fda9bdb0eaaa424b8b509a99c6ea23fa13
Joy Balangatan <joy.balangatan@irely.com>
2016-09-27 20:19:21 +0800

    IC-2072 Added New Feature for Incomping Inspection in IR

---
+++ i21Database/dbo/Stored Procedures/uspICSaveReceiptInspection.sql
@@ -0,0 +1,43 @@
+﻿CREATE PROCEDURE [dbo].[uspICSaveReceiptInspection]
+	@ReceiptId AS INT
+
+AS
+
+SET QUOTED_IDENTIFIER OFF
+SET ANSI_NULLS ON
+SET NOCOUNT ON
+SET XACT_ABORT ON
+SET ANSI_WARNINGS OFF
+
+BEGIN
+
+	-- Declare variables 
+	DECLARE @intUserId AS INT
+			,@intControlPointId AS INT = 3
+			,@intProductTypeId AS INT = 3
+			,@tmpQualityInspectionTable QualityInspectionTable
+
+	-- Add value for UserId
+	SELECT @intUserId = intCreatedUserId FROM tblICInventoryReceipt WHERE intInventoryReceiptId = @ReceiptId
+
+	-- Insert values for @QualityTableValues
+	INSERT INTO @tmpQualityInspectionTable (
+		intPropertyId
+		,strPropertyName
+		,strPropertyValue
+	)
+	SELECT 
+		ReceiptInspection.intQAPropertyId
+		,ReceiptInspection.strPropertyName
+		,CASE WHEN ReceiptInspection.ysnSelected = 1 THEN 'true' ELSE 'false' END
+	FROM tblICInventoryReceiptInspection ReceiptInspection
+	WHERE ReceiptInspection.intInventoryReceiptId = @ReceiptId
+
+	-- Save values to Quality
+	EXEC [uspQMInspectionSaveResult]
+		@intControlPointId
+		,@intProductTypeId
+		,@ReceiptId
+		,@intUserId
+		,@tmpQualityInspectionTable
+END
