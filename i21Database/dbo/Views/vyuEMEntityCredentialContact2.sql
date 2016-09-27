32b3f5ad1194e7434f2f1291cfdf84972c9c2f58
jesmar mendones <jesmar.mendones@irely.com>
2016-09-27 19:02:57 +0800

    FRM-3766

---
+++ i21Database/dbo/Views/vyuEMEntityCredentialContact2.sql
@@ -0,0 +1,46 @@
+﻿--this is for mention only, because original one doesn't give correct entityId
+CREATE VIEW [dbo].[vyuEMEntityCredentialContact2]
+	AS 
+
+SELECT
+	vEntityContact.intEntityId,
+	vEntityContact.intEntityContactId,
+	vEntityContact.strEntityName,
+	vEntityContact.strName,
+	vEntityContact.strEmail,
+	vEntityContact.strLocationName,
+	vEntityContact.strPhone,
+	vEntityContact.strMobile,
+	vEntityContact.strTimezone,
+	vEntityContact.strTitle,
+	vEntityContact.ysnPortalAccess,
+	vEntityContact.ysnActive,
+	vEntityContact.ysnDefaultContact,
+	vEntityContact.Customer,
+	vEntityContact.Vendor,
+	vEntityContact.Employee,
+	vEntityContact.Salesperson,
+	CASE WHEN ISNULL(UserSecurity.intEntityUserSecurityId, 0) = 0 THEN 0 ELSE vEntityContact.[User] END [User],
+	vEntityContact.FuturesBroker,
+	vEntityContact.ForwardingAgent,
+	vEntityContact.Terminal,
+	vEntityContact.ShippingLine,
+	vEntityContact.Trucker,
+	vEntityContact.strContactType,
+	vEntityContact.strEmailDistributionOption,
+	vEntityContact.imgPhoto,
+	vEntityContact.papit,
+	ysnReqTwoFactorAuth = Cast(isnull(SecurityPolicy.ysnReqTwoFactorAuth, 0) as bit),
+	UserSecurity.ysnDisabled
+FROM tblEMEntityCredential AS EntityCredential 
+	INNER JOIN vyuEMEntityContact AS vEntityContact
+		ON EntityCredential.intEntityId = (
+			CASE WHEN vEntityContact.ysnPortalAccess = 1
+				THEN vEntityContact.intEntityContactId
+				ELSE vEntityContact.intEntityId
+			END
+		)
+	LEFT JOIN tblSMUserSecurity UserSecurity 
+		ON EntityCredential.intEntityId = UserSecurity.intEntityUserSecurityId
+	LEFT JOIN tblSMSecurityPolicy SecurityPolicy
+		ON UserSecurity.intSecurityPolicyId = SecurityPolicy.intSecurityPolicyId 
