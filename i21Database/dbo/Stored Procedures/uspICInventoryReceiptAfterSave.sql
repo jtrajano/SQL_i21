e23356177e3f395bfb15277f148547a34edd8f73
Joy Balangatan <joy.balangatan@irely.com>
2016-09-28 18:55:13 +0800

    IC-2072 Updated Condition in uspICUpdateTableReceiptInspection for selecting values from Quality table; Added logic in uspICInventoryReceiptAfterSave when deleting IR to clear values in Quality Table

--- i21Database/dbo/Stored Procedures/uspICInventoryReceiptAfterSave.sql
+++ i21Database/dbo/Stored Procedures/uspICInventoryReceiptAfterSave.sql
@@ -57,6 +57,14 @@ BEGIN
 	EXEC uspGRReverseOnReceiptDelete @ReceiptId
 END 
 
+-- Remove values from Quality Table for Incoming Inspection Result
+DELETE
+FROM tblQMTestResult
+WHERE intSampleId IS NULL
+    AND intControlPointId = 3
+    AND intProductTypeId = 3
+    AND intProductValueId = @ReceiptId
+
 -- Validate. 
 BEGIN		
 	-- Do not proceed if receipt type is NOT a 'Purchase Contract' 
