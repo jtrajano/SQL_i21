e585dfbd0049e5dc63db948ee3d7420e55c99ca6
Cavinne Jandhrix Callado <cavinne.callado@irely.com>
2016-09-28 23:40:45 +0800

    TS-1367

--- tests-ui/SmokeTesting/ICSmokeTests.js
+++ tests-ui/SmokeTesting/ICSmokeTests.js
@@ -9,7 +9,7 @@ StartTest(function (t) {
     engine.start(t)
 
         /*Scenario 1: Opening Inventory Screens*/
-        .addFunction(function (next) { commonSM.commonLogin(t, next); }).wait(1000)
+        .addFunction(function (next) { commonSM.commonLogin(t, next); }).wait(2000d)
         .addFunction(function (next) { t.diag("Scenario 1. Open Inventory screens and check default controls' state"); next(); }).wait(1000)
       /*.waitTillVisible('frmintegrateddashboardgridpanel','Login Successful',60000).wait(1000)*/
         .expandMenu('Inventory').wait(1000)
@@ -41,6 +41,7 @@ StartTest(function (t) {
         .checkControlVisible(['#txtBlanketReleaseNumber', '#cboShipVia', '#txtVessel'], true).wait(500)
         .checkControlVisible(['#btnInsertInventoryReceipt', '#btnQuality', '#btnTaxDetails','#btnRemoveInventoryReceipt'], true).wait(500)
         .checkControlVisible(['#btnHelp', '#btnSupport', '#btnFieldName', '#btnEmailUrl'], true).wait(500)
+        .checkStatusMessage('Ready')
 
         .addFunction(function (next) { t.diag("Check IR Tabs and Fields"); next(); }).wait(300)
         .clickTab('#pgeFreightInvoice').wait(500)
@@ -79,10 +80,13 @@ StartTest(function (t) {
         .addFunction(function (next) { t.diag("Check Inventory Shipment Screen Fields"); next(); }).wait(500)
         .clickButton('#btnNew').wait(1000)
         .waitTillVisible('icinventoryshipment','Open New Inventory Shipment Screen Successful').wait(1000)
-        .checkControlVisible(['#btnNew', '#btnSave', '#btnSearch', '#btnDelete', '#btnUndo','#btnPrint','#btnPrintBOL','#btnShip','#btnRecap','#btnCustomer','#btnWarehouseInstruction','#btnClose'], true).wait(500)
-        .checkControlVisible(['#cboOrderType', '#cboSourceType', '#cboCustomer', '#dtmShipDate', '#txtReferenceNumber', '#dtmRequestedArrival','#cboFreightTerms','#txtShipmentNo'], true).wait(500)
+        .checkControlVisible(['#btnNew', '#btnSave', '#btnSearch', '#btnDelete', '#btnUndo','#btnPrint','#btnPrintBOL','#btnShip',
+             '#btnRecap','#btnCustomer','#btnWarehouseInstruction','#btnClose'], true).wait(500)
+        .checkControlVisible(['#cboOrderType', '#cboSourceType', '#cboCustomer', '#dtmShipDate', '#txtReferenceNumber', '#dtmRequestedArrival',
+             '#cboFreightTerms','#txtShipmentNo'], true).wait(500)
         .checkControlVisible(['#cboShipFromAddress', '#txtShipFromAddress', '#cboShipToAddress','#txtShipToAddress','#txtDeliveryInstructions','#txtComments'], true).wait(500)
         .checkControlVisible(['#btnHelp', '#btnSupport', '#btnFieldName', '#btnEmailUrl'], true).wait(500)
+        .checkStatusMessage('Ready').wait(200)
         /*.clickTab('#tabShipping').wait(500)
         .checkControlVisible(['#txtBOLNo', '#txtProNumber', '#cboShipVia', '#txtDriverID','#txtVesselVehicle','#txtSealNumber'], true).wait(500)
         .clickTab('#tabShipping').wait(500)
@@ -91,7 +95,8 @@ StartTest(function (t) {
         .clickTab('#pgeChargesInvoice').wait(500)
         .checkControlVisible(['#btnInsertCharge', '#btnRemoveCharge', '#btnGridLayout', '#btnInsertCriteria', '#txtFilterGrid'], true).wait(500)
         .clickTab('#pgeComments').wait(500)
-        .checkControlVisible(['#btnOpenActivity', '#btnNewEvent', '#btnNewTask', '#btnNewComment', '#btnLogCall', '#btnSendEmail','#btnGridLayout','#btnInsertCriteria','#txtFilterGrid','#btnMaximizeGrid'], true).wait(500)
+        .checkControlVisible(['#btnOpenActivity', '#btnNewEvent', '#btnNewTask', '#btnNewComment', '#btnLogCall', '#btnSendEmail','#btnGridLayout',
+             '#btnInsertCriteria','#txtFilterGrid','#btnMaximizeGrid'], true).wait(500)
         .checkControlVisible(['#btnInsertCriteria', '#txtFilterGrid'], true).wait(500)
         .clickButton('#btnClose').wait(500)
         .checkMessageBox('iRely i21','Do you want to save the changes you made?','yesnocancel','question').wait(1000)
@@ -113,9 +118,11 @@ StartTest(function (t) {
         .clickButton('#btnNew').wait(1000)
         .waitTillVisible('icinventorytransfer','Open New Inventory Transfer Screen Successful').wait(1000)
         .checkControlVisible(['#btnNew', '#btnSave', '#btnSearch', '#btnDelete', '#btnUndo','#btnPrint','#btnPost','#btnRecap','#btnClose'], true).wait(500)
-        .checkControlVisible(['#txtTransferNumber', '#dtmTransferDate', '#cboTransferType', '#cboSourceType', '#cboTransferredBy', '#cboFromLocation','#cboToLocation','#chkShipmentRequired','#cboStatus','#txtDescription'], true).wait(500)
+        .checkControlVisible(['#txtTransferNumber', '#dtmTransferDate', '#cboTransferType', '#cboSourceType', '#cboTransferredBy', '#cboFromLocation',
+             '#cboToLocation','#chkShipmentRequired','#cboStatus','#txtDescription'], true).wait(500)
         .checkControlVisible(['#btnAddItem', '#btnViewItem', '#btnRemoveItem', '#btnGridLayout', '#btnInsertCriteria', '#txtFilterGrid'], true).wait(500)
         .checkControlVisible(['#btnHelp', '#btnSupport', '#btnFieldName', '#btnEmailUrl'], true).wait(500)
+        .checkStatusMessage('Ready').wait(200)
         .clickButton('#btnClose').wait(500)
         .checkMessageBox('iRely i21','Do you want to save the changes you made?','yesnocancel','question').wait(1000)
         .clickMessageBoxButton('no').wait(300)
@@ -139,6 +146,7 @@ StartTest(function (t) {
         .checkControlVisible(['#cboLocation', '#dtmDate', '#cboAdjustmentType', '#txtAdjustmentNumber', '#txtDescription'], true).wait(500)
         .checkControlVisible(['#btnAddItem', '#btnViewItem', '#btnRemoveItem', '#btnGridLayout', '#btnInsertCriteria', '#txtFilterGrid'], true).wait(500)
         .checkControlVisible(['#btnHelp', '#btnSupport', '#btnFieldName', '#btnEmailUrl'], true).wait(500)
+        .checkStatusMessage('Ready').wait(200)
         .clickButton('#btnClose').wait(500)
         .checkMessageBox('iRely i21','Do you want to save the changes you made?','yesnocancel','question').wait(1000)
         .clickMessageBoxButton('no').wait(300)
@@ -157,9 +165,10 @@ StartTest(function (t) {
         .checkControlVisible(['#cboLocation', '#cboCategory', '#cboCommodity', '#cboCountGroup', '#dtpCountDate'], true).wait(500)
         .checkControlVisible(['#txtCountNumber', '#cboSubLocation', '#cboStorageLocation', '#txtDescription', '#btnFetch'], true).wait(500)
         .checkControlVisible(['#chkIncludeZeroOnHand', '#chkIncludeOnHand', '#chkScannedCountEntry', '#chkCountByLots', '#chkCountByPallets'], true).wait(500)
-        .checkControlVisible(['#chkRecountMismatch', '#chkExternal', '#chkRecount', '#txtReferenceCountNumber', '#cboStatus'], true).wait(500)
+        .checkControlVisible(['#chkRecountMismatch', '#chkExternal', '#chkRecount', '#txtReferenceCountNo', '#cboStatus'], true).wait(500)
         .checkControlVisible(['#btnInsert', '#btnRemove','#btnGridLayout', '#btnInsertCriteria', '#txtFilterGrid' ], true).wait(500)
         .checkControlVisible(['#btnHelp', '#btnSupport', '#btnFieldName', '#btnEmailUrl'], true).wait(500)
+        .checkStatusMessage('Ready').wait(200)
         .clickButton('#btnClose').wait(500)
         .checkMessageBox('iRely i21','Do you want to save the changes you made?','yesnocancel','question').wait(1000)
         .clickMessageBoxButton('no').wait(300)
@@ -170,18 +179,20 @@ StartTest(function (t) {
         /*Storage Measurement Reading Screen*/
         .openScreen('Storage Measurement Reading').wait(1000)
         .waitTillVisible('frmintegrateddashboardgridpanel','Open Storage Measurement Reading Search Screen Successful',60000).wait(1000)
-        .waitTillVisible('icstoragemeasurementreading','Open New Storage Measurement Reading Screen Successful').wait(1000)
-        .checkSearchToolbarButton({ new: true, view: true, openselected: false, openall: false, refresh: true, export: true, close: false }).wait(500)
+        .clickButton('#btnNew').wait(200)
+        .waitTillVisible('storagemeasurementreading','Open New Storage Measurement Reading Screen Successful').wait(1000)
+
 
         .addFunction(function (next) { t.diag("Check Inventory Count Screen Fields"); next(); }).wait(500)
         .checkControlVisible(['#btnNew', '#btnSave', '#btnSearch','#btnDelete', '#btnUndo', '#btnClose'], true).wait(500)
-        .checkControlVisible(['#cboLocation', '#dtmDate', '#txtReadingNumber', '#cboCountGroup', '#dtpCountDate'], true).wait(500)
+        .checkControlVisible(['#cboLocation', '#dtmDate', '#txtReadingNumber'], true).wait(500)
         .checkControlVisible(['#btnInsert', '#btnRemove','#btnGridLayout', '#btnInsertCriteria', '#txtFilterGrid' ], true).wait(500)
         .checkControlVisible(['#btnHelp', '#btnSupport', '#btnFieldName', '#btnEmailUrl'], true).wait(500)
+        .checkStatusMessage('Ready').wait(200)
         .clickButton('#btnClose').wait(500)
         .checkMessageBox('iRely i21','Do you want to save the changes you made?','yesnocancel','question').wait(1000)
-        .clickMessageBoxButton('no').wait(300)
-        .checkIfScreenClosed('icstoragemeasurementreading').wait(100)
+        .clickMessageBoxButton('no').wait(1000)
+
 
         /*Item Screen*/
         .openScreen('Items').wait(1000)
@@ -192,11 +203,37 @@ StartTest(function (t) {
         .clickButton('#btnNew').wait(200)
         .waitTillVisible('icitem','Open New Item Screen Successful').wait(500)
         .checkScreenShown('icitem').wait(500)
-        .checkControlVisible(['#btnNew', '#btnSave', '#btnSearch', '#btnDelete', '#btnUndo','#btnDuplicate','#btnClose'], true).wait(500)
-        .checkControlVisible(['#txtItemNo', '#cboType', '#cboStorageLocation', '#txtDescription', '#btnFetch'], true).wait(500)
+        .checkControlVisible(['#btnNew', '#btnSave', '#btnFind', '#btnDelete', '#btnUndo','#btnDuplicate','#btnClose'], true).wait(500)
+        .checkControlVisible(['#txtItemNo', '#cboType', '#txtShortName', '#txtDescription', '#cboManufacturer','#cboStatus','#cboCommodity',
+             '#cboLotTracking','#cboBrand','#txtModelNo','#cboCategory','#cboTracking'], true).wait(500)
         .checkControlVisible(['#btnInsertUom', '#btnDeleteUom', '#btnLoadUOM', '#btnGridLayout', '#btnInsertCriteria', '#txtFilterGrid'], true).wait(500)
         .checkControlVisible(['#btnHelp', '#btnSupport', '#btnFieldName', '#btnEmailUrl'], true).wait(500)
         .checkStatusMessage('Ready')
+        /*Setup Tabs*/
+        .clickTab('#cfgSetup').wait(500)
+        .checkControlVisible(['#btnAddRequiredAccounts', '#btnInsertGlAccounts', '#btnDeleteGlAccounts', '#btnGridLayout', '#btnInsertCriteria', '#txtFilterGrid'], true).wait(500)
+
+        .clickTab('#cfgLocation').wait(500)
+        .checkControlVisible(['#btnAddLocation', '#btnAddMultipleLocation', '#btnEditLocation', '#btnDeleteLocation', '#cboCopyLocation',
+                '#btnGridLayout', '#btnInsertCriteria', '#txtFilterGrid','#btnMaximizeGrid'], true).wait(500)
+
+        .clickTab('#cfgSales').wait(500)
+        .checkControlVisible(['#chkStockedItem', '#chkDyedFuel', '#cboBarcodePrint', '#chkMsdsRequired', '#txtEpaNumber',
+                '#chkInboundTax', '#chkOutboundTax', '#chkRestrictedChemical','#chkFuelItem'], true).wait(500)
+        .checkControlVisible(['#pnlRins', '#cboFuelInspectionFee', '#cboRinRequired', '#cboFuelCategory', '#txtPercentDenaturant'], true).wait(500)
+        .checkControlVisible(['#pnlFeed', '#chkTonnageTax', '#chkLoadTracking', '#txtMixOrder', '#chkHandAddIngredients','#cboMedicationTag',
+                '#cboIngredientTag','#txtVolumeRebateGroup'], true).wait(500)
+        .checkControlVisible(['#cboPhysicalItem', '#chkExtendOnPickTicket', '#chkExportEdi', '#chkHazardMaterial', '#chkMaterialFee','#chkAutoBlend',
+            '#txtUserGroupFee','#txtWgtTolerance','#txtOverReceiveTolerance','#txtMaintenanceCalculationMethod','#txtMaintenanceRate'], true).wait(500)
+
+        .clickTab('#cfgPOS').wait(500)
+        .checkControlVisible(['#pnlGeneral', '#txtNacsCategory', '#cboWicCode', '#chkReceiptCommentReq', '#cboCountCode'], true).wait(500)
+        .checkControlVisible(['#pnlInventorySetup', '#chkLandedCost', '#txtLeadTime', '#chkTaxable', '#txtKeywords','#txtCaseQty','#dtmDateShip','#txtTaxExempt','#chkDropship'], true).wait(500)
+        .checkControlVisible(['#pnlCommisionDetail', '#chkCommissionable', '#chkSpecialCommission'], true).wait(500)
+        .checkControlVisible(['#grdCategory', '#btnInsertCategories', '#btnDeleteCategories', '#colPOSCategoryName'], true).wait(500)
+        .checkControlVisible(['#grdServiceLevelAgreement', '#btnInsertSLA', '#btnDeleteSLA', '#colPOSSLAContract', '#colPOSSLAPrice','#ColPOSSLAWarranty'], true).wait(500)
+
+
         .clickButton('#btnClose').wait(100)
         .checkMessageBox('iRely i21','Do you want to save the changes you made?','yesnocancel','question').wait(1000)
         .clickMessageBoxButton('no').wait(100)
