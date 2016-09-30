47bfb38d0332110bbd74687258ca1713461b5e68
Joy Balangatan <joy.balangatan@irely.com>
2016-09-30 15:32:07 +0800

    IC-2722 Fixed also loading of Add Order Screen in IS

--- app/view/InventoryShipmentViewController.js
+++ app/view/InventoryShipmentViewController.js
@@ -2415,7 +2415,11 @@ Ext.define('Inventory.view.InventoryShipmentViewController', {
             });
             search.show();
         }
-        showAddScreen();
+        
+         var task = new Ext.util.DelayedTask(function () {
+            showAddScreen();          
+        });
+        task.delay(10);
     },
 
     onOrderTypeSelect: function(combo, records, eOpts) {
