4aec030e40023e949d8efc259c76cf7676dba660
jeffrey trajano <jeffrey.trajano@irely.com>
2016-10-03 10:41:30 +0800

    GL-2968 GL- Collation Issue

--- i21Database/dbo/Tables/tblGLCompanyPreferenceOption.sql
+++ i21Database/dbo/Tables/tblGLCompanyPreferenceOption.sql
@@ -2,8 +2,8 @@
 (
 	[intCompanyPreferenceOptionId] INT IDENTITY(1,1) NOT NULL,
 	[intConcurrencyId] INT NULL,
-	[PostRemind_Users] NVARCHAR (MAX) NULL,
-	[PostRemind_BeforeAfter] NVARCHAR(10) NULL,
+	[PostRemind_Users] NVARCHAR (MAX)  COLLATE Latin1_General_CI_AS NULL,
+	[PostRemind_BeforeAfter] NVARCHAR(10)  COLLATE Latin1_General_CI_AS NULL,
 	[PostRemind_Days] INT NULL,
 	[OriginConversion_OffsetAccountId] INT NULL,
     CONSTRAINT [PK_tblGLCompanyPreferenceOption] PRIMARY KEY ([intCompanyPreferenceOptionId])
--- i21Database/dbo/Tables/tblGLTempCOASegment.sql
+++ i21Database/dbo/Tables/tblGLTempCOASegment.sql
@@ -1,9 +1,9 @@
 CREATE TABLE [dbo].[tblGLTempCOASegment](
 	[intAccountId] [int] NOT NULL,
-	[strAccountId] [nvarchar](40) NOT NULL,
-	[Primary Account] [nvarchar](20) NULL,
-	[Location] [nvarchar](20) NULL,
-	[LOB] [nvarchar](20) NULL
+	[strAccountId] [nvarchar](40)  COLLATE Latin1_General_CI_AS NOT NULL,
+	[Primary Account] [nvarchar](20)  COLLATE Latin1_General_CI_AS NULL,
+	[Location] [nvarchar](20)  COLLATE Latin1_General_CI_AS NULL,
+	[LOB] [nvarchar](20)  COLLATE Latin1_General_CI_AS NULL
 ) ON [PRIMARY]
 
 
