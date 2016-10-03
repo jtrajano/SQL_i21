5830e6769603d686a8587fa8d61505ab6dd920ea
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2016-10-03 17:14:53 +0530

    RM-498

--- i21Database/dbo/Views/vyuRKSOptionMatchedTransaction.sql
+++ i21Database/dbo/Views/vyuRKSOptionMatchedTransaction.sql
@@ -1,7 +1,7 @@
 ﻿CREATE VIEW vyuRKSOptionMatchedTransaction
 AS
-SELECT *,((isnull(dblSPrice,0)-isnull(dblLPrice,0))*intMatchQty*dblContractSize)/ case when ysnSubCurrency = 'true' then intCent else 1 end as dblImpact FROM(
-SELECT m.intMatchOptionsPnSId,strTranNo,dtmMatchDate,intMatchQty,e.strName,b.strAccountNumber,t.strInternalTradeNo,scl.strLocationName,t.dblPrice as dblLPrice,
+SELECT top 100 percent *,((isnull(dblSPrice,0)-isnull(dblLPrice,0))*intMatchQty*dblContractSize)/ case when ysnSubCurrency = 'true' then intCent else 1 end as dblImpact FROM(
+SELECT m.intMatchOptionsPnSId,convert(int,strTranNo) strTranNo,dtmMatchDate,intMatchQty,e.strName,b.strAccountNumber,t.strInternalTradeNo,scl.strLocationName,t.dblPrice as dblLPrice,
 	   fm.strFutMarketName,om.strOptionMonth,t.dblStrike,t.strOptionType,fm.dblContractSize
 	   ,strCommodityCode,t.dtmFilledDate as dtmMLTransactionDate,t.strInternalTradeNo as strMLInternalTradeNo,cb.strBook as strMLBook,csb.strSubBook as strMLSubBook,
 	   (SELECT TOP 1 dtmFilledDate FROM tblRKOptionsMatchPnS om
@@ -29,4 +29,4 @@ JOIN tblRKOptionsMonth om on om.intOptionMonthId=t.intOptionMonthId
 join tblICCommodity ic on ic.intCommodityId=t.intCommodityId
 JOIN tblSMCompanyLocation scl on scl.intCompanyLocationId=t.intLocationId
 LEFT JOIN tblCTBook cb on cb.intBookId= t.intBookId
-LEFT join tblCTSubBook csb on csb.intSubBookId=t.intSubBookId)t 
+LEFT join tblCTSubBook csb on csb.intSubBookId=t.intSubBookId)t order by convert(int,strTranNo) Asc
