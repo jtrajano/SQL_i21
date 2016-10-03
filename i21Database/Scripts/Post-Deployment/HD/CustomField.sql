ad910380801b168490dc39e816efe3561bd8df9e
Jayson Ramos <jayson.ramos@iRely.com>
2016-10-03 09:31:12 +0800

    HD-1724

--- i21Database/Scripts/Post-Deployment/HD/CustomField.sql
+++ i21Database/Scripts/Post-Deployment/HD/CustomField.sql
@@ -1,12 +1,12 @@
-﻿IF NOT EXISTS (SELECT * FROM sys.tables WHERE object_id = object_id('cstHDOpportunityCampaign'))
+﻿IF NOT EXISTS (SELECT * FROM sys.tables WHERE object_id = object_id('cstCRMCampaign'))
 BEGIN
-    print('/*******************  BEGIN Creating Help Desk Campaign Custom Table *******************/')
+    print('/*******************  BEGIN Creating CRM Campaign Custom Table *******************/')
     EXEC('
-        CREATE TABLE [dbo].[cstHDOpportunityCampaign]
+        CREATE TABLE [dbo].[cstCRMCampaign]
         (
             [intId] INT NOT NULL,
-            CONSTRAINT [PK_cstHDOpportunityCampaign] PRIMARY KEY CLUSTERED ([intId] ASC),
-            CONSTRAINT [FK_cstHDOpportunityCampaign_tblHDOpportunityCampaign] FOREIGN KEY ([intId]) REFERENCES [dbo].[tblHDOpportunityCampaign] ([intOpportunityCampaignId]) ON DELETE CASCADE
+            CONSTRAINT [PK_cstCRMCampaign] PRIMARY KEY CLUSTERED ([intId] ASC),
+            CONSTRAINT [FK_cstCRMCampaign_tblCRMCampaign] FOREIGN KEY ([intId]) REFERENCES [dbo].[tblCRMCampaign] ([intCampaignId]) ON DELETE CASCADE
         );
     ')
     print('/*******************  END Creating Help Desk Campaign Custom Table *******************/')
