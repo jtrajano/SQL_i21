52176340d0c85cda87aeda877a3680da6d655861
Joy Balangatan <joy.balangatan@irely.com>
2016-10-03 14:00:47 +0800

    IC-2807 Rounded Lot Qty and Receipt Qty values to 6 decimal places

--- i21Database/dbo/Stored Procedures/uspICCreateLotNumberOnInventoryReceipt.sql
+++ i21Database/dbo/Stored Procedures/uspICCreateLotNumberOnInventoryReceipt.sql
@@ -105,7 +105,7 @@ BEGIN
 				ON ItemLot.intInventoryReceiptItemId = ReceiptItem.intInventoryReceiptItemId											
 	WHERE	dbo.fnGetItemLotType(ReceiptItem.intItemId) IN (@LotType_Manual, @LotType_Serial)	
 			AND Receipt.strReceiptNumber = @strTransactionId
-			AND ROUND(ISNULL(ItemLot.TotalLotQtyInItemUOM, 0), 2) <> ReceiptItem.dblOpenReceive
+			AND ROUND(ISNULL(ItemLot.TotalLotQtyInItemUOM, 0), 6) <> ROUND(ReceiptItem.dblOpenReceive,6)
 			--AND ReceiptItem.intWeightUOMId IS NULL 
 			
 	IF @intItemId IS NOT NULL 
