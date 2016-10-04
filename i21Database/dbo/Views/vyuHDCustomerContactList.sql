f856dc9f553203c1f016f090db4c4b73f8c2eb68
Jayson Ramos <jayson.ramos@iRely.com>
2016-10-04 16:07:38 +0800

    HD-1717

---
+++ i21Database/dbo/Views/vyuHDCustomerContactList.sql
@@ -0,0 +1,32 @@
+﻿CREATE VIEW [dbo].[vyuHDCustomerContactList]
+	AS
+		select
+			  c.strCustomerNumber
+			  ,strCompanyName = e.strName
+			  ,strContactName = ec.strName
+			  ,ec.strEmail
+			  ,ec.strTitle
+			  ,strPhone = ph.strPhone
+			  ,el.strLocationName
+			  ,el.strTimezone
+			  ,intContactId = ec.[intEntityId]
+			  ,intCustomerId = c.[intEntityCustomerId]
+			  ,ysnActiveContact = ec.ysnActive
+			  ,strMobile = ''
+			  ,intConcurrencyId = 1
+			  ,intTicketProductId = null
+			  ,intVersionId = null
+			  ,ysnActive = convert(bit, 1)
+			  ,imgPhoto = null
+			  ,ysnBillable = convert(bit, 0)
+			  ,strEntityType = null
+			  ,intModuleId = null
+			  ,strComapny = null
+			from
+			  tblARCustomer c
+			  inner join tblEMEntity e on e.intEntityId = c.[intEntityCustomerId]
+			  inner join tblEMEntityToContact etc on etc.intEntityId = c.intEntityCustomerId
+			  inner join tblEMEntity ec on ec.[intEntityId] = etc.[intEntityContactId]
+			  left join tblEMEntityPhoneNumber ph on ec.intEntityId = ph.intEntityId
+			  inner join tblEMEntityToContact etcc on etcc.intEntityContactId = ec.intEntityId
+			  inner join tblEMEntityLocation el on el.intEntityLocationId = etcc.intEntityLocationId
