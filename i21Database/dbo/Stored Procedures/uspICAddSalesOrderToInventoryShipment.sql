9757cecd870f4230228f251f0f6beb32c0773ae8
csMJardiolin <MJardiolin@COMTECH.COM>
2016-10-04 13:24:59 +0800

    AR-3574

--- i21Database/dbo/Stored Procedures/uspICAddSalesOrderToInventoryShipment.sql
+++ i21Database/dbo/Stored Procedures/uspICAddSalesOrderToInventoryShipment.sql
@@ -131,7 +131,7 @@ BEGIN
 			,intDockDoorId			= NULL
 			,strNotes				= SODetail.strComments
 			,intSort				= SODetail.intSalesOrderDetailId
-			,intOwnershipType		= CASE WHEN SODetail.intStorageScheduleTypeId = NULL THEN 0 ELSE 2 END
+			,intOwnershipType		= CASE WHEN SODetail.intStorageScheduleTypeId = NULL THEN 1 ELSE 2 END
 			,intCustomerStorageId	= SODetail.intCustomerStorageId
 			,intConcurrencyId		= 1
 	FROM	dbo.tblSOSalesOrderDetail SODetail INNER JOIN dbo.tblICItemUOM ItemUOM			
