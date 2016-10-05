19fe9652be8e4c8610c29f6fd66dedf74ea4b2ae
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2016-10-05 20:59:37 +0530

    RM-422

---
+++ i21Database/dbo/Views/vyuRKGetBrokerList.sql
@@ -0,0 +1,4 @@
+﻿CREATE VIEW vyuRKGetBrokerList
+AS
+SELECT  S.intEntityId, S.strName FROM  tblEMEntity S 
+JOIN tblEMEntityType SE ON S.intEntityId = SE.intEntityId and SE.strType = 'Futures Broker'
