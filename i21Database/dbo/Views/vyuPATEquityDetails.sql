28437a203414ba28405f905fd65d1a8b8da35b5f
Jb Mago <benedict.mago@irely.com>
2016-10-05 18:03:19 +0800

    PAT-247 PAT-415

--- i21Database/dbo/Views/vyuPATEquityDetails.sql
+++ i21Database/dbo/Views/vyuPATEquityDetails.sql
@@ -1,31 +1,35 @@
 ﻿CREATE VIEW [dbo].[vyuPATEquityDetails]
 	AS 
-	 SELECT CE.intCustomerId,
-			ENT.strName,
-			CE.intFiscalYearId,
-			FY.strFiscalYear,
-			AR.strStockStatus,
-			TC.strTaxCode,
-			dtmLastActivityDate = MAX(CE.dtmLastActivityDate),
-			CE.strEquityType,
-			dblEquity = SUM(CE.dblEquity),
-			CE.intConcurrencyId 
-	   FROM tblPATCustomerEquity CE
- INNER JOIN tblEMEntity ENT
-		 ON ENT.intEntityId = CE.intCustomerId
- INNER JOIN tblPATRefundRate RR
-		 ON RR.intRefundTypeId = CE.intRefundTypeId
- INNER JOIN tblGLFiscalYear FY
-		 ON FY.intFiscalYearId = CE.intFiscalYearId
- INNER JOIN tblARCustomer AR
-		 ON AR.intEntityCustomerId = CE.intCustomerId
-  LEFT JOIN tblSMTaxCode TC
-		 ON TC.intTaxCodeId = AR.intTaxCodeId
-		 GROUP BY	CE.intCustomerId,
-					ENT.strName,
-					CE.intFiscalYearId,
-					FY.strFiscalYear,
-					AR.strStockStatus,
-					TC.strTaxCode,
-					CE.strEquityType,
-					CE.intConcurrencyId
+SELECT	CE.intCustomerId,
+		ENT.strName,
+		CE.intFiscalYearId,
+		FY.strFiscalYear,
+		AR.strStockStatus,
+		TC.strTaxCode,
+		RR.intRefundTypeId,
+		RR.strRefundType,
+		dtmLastActivityDate = MAX(CE.dtmLastActivityDate),
+		CE.strEquityType,
+		dblEquity = SUM(CE.dblEquity),
+		CE.intConcurrencyId 
+	FROM tblPATCustomerEquity CE
+INNER JOIN tblEMEntity ENT
+		ON ENT.intEntityId = CE.intCustomerId
+INNER JOIN tblPATRefundRate RR
+		ON RR.intRefundTypeId = CE.intRefundTypeId
+INNER JOIN tblGLFiscalYear FY
+		ON FY.intFiscalYearId = CE.intFiscalYearId
+INNER JOIN tblARCustomer AR
+		ON AR.intEntityCustomerId = CE.intCustomerId
+LEFT JOIN tblSMTaxCode TC
+		ON TC.intTaxCodeId = AR.intTaxCodeId
+		GROUP BY	CE.intCustomerId,
+				ENT.strName,
+				CE.intFiscalYearId,
+				RR.intRefundTypeId,
+				RR.strRefundType,
+				FY.strFiscalYear,
+				AR.strStockStatus,
+				TC.strTaxCode,
+				CE.strEquityType,
+				CE.intConcurrencyId
