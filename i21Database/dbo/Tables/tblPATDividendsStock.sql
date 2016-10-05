01ca7da4c80adf5cbb5b958076c3aa038a6df2e3
Jb Mago <benedict.mago@irely.com>
2016-10-05 15:38:27 +0800

    PAT-413

--- i21Database/dbo/Tables/tblPATDividendsStock.sql
+++ i21Database/dbo/Tables/tblPATDividendsStock.sql
@@ -7,7 +7,7 @@
 	[intCustomerStockId] INT NOT NULL, 
     [dblParValue] NUMERIC(18, 6) NULL, 
     [dblSharesNo] NUMERIC(18, 6) NULL, 
-    [dblDividendPerShare] NUMERIC(18, 6) NULL, 
+    [intDividendsPerShare] INT NULL, 
     [dblDividendAmount] NUMERIC(18, 6) NULL, 
     [intConcurrencyId] INT NULL DEFAULT 0, 
     CONSTRAINT [PK_tblPATDividendsStock] PRIMARY KEY ([intDividendStockId]), 
