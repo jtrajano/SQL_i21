744022832a1e54873569b27afc9b9fe7bd60b1d2
bonn ponce <bonn.ponce@irely.com>
2016-10-06 19:03:31 +0800

    CF-418

---
+++ i21Database/dbo/Views/vyuCFDiscountSchedule.sql
@@ -0,0 +1,5 @@
+﻿CREATE VIEW dbo.vyuCFDiscountSchedule
+AS
+SELECT   cfAccnt.intAccountId, cfDsd.intFromQty, cfDsd.intThruQty, cfDsd.dblRate, cfDsd.intDiscountScheduleId, cfDsd.intDiscountSchedDetailId
+FROM         dbo.tblCFDiscountScheduleDetail AS cfDsd INNER JOIN
+                         dbo.tblCFAccount AS cfAccnt ON cfAccnt.intDiscountScheduleId = cfDsd.intDiscountScheduleId
