4f57957de2139eaaf1f3bc6dc68133edf90ce87a
kenneth lagunero <kenneth.lagunero@irely.com>
2016-10-08 06:17:11 +0800

    TM-2051

--- i21Database/dbo/Views/vyuTMCustomerEntityView.sql
+++ i21Database/dbo/Views/vyuTMCustomerEntityView.sql
@@ -67,7 +67,7 @@ SELECT
 	,vwcus_ptd_sls = ISNULL(CI.dblYTDSales,0.0)
 	,vwcus_lyr_sls = ISNULL(CI.dblLastYearSales,0.0)
 	,vwcus_acct_stat_x_1 = (SELECT strAccountStatusCode FROM tblARAccountStatus WHERE intAccountStatusId = Cus.intAccountStatusId)
-	,dblFutureCurrent = ISNULL(CI.dblFuture,0.0) + ISNULL(CI.dbl0Days,0.0) + ISNULL(CI.dbl0Days,0.0)
+	,dblFutureCurrent = ISNULL(CI.dblFuture,0.0) + ISNULL(CI.dbl10Days,0.0) + ISNULL(CI.dbl0Days,0.0)
 	,intConcurrencyId = 0
 	,strFullLocation =  ISNULL(Loc.strLocationName ,'')
 	,intTaxId = CAST(NULL AS INT)
