6ac864ba8d63f791b9468d094d7710f3a97b7941
Wendell Wayne Estrada <wendell.estrada@irely.com>
2016-10-07 17:26:46 +0800

    update unit tests

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,8 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
+var fcg = require('./lib/gulp-plugins/gulp-extjs-get-classname');
+var prettify = require('gulp-js-prettify');
 
 var scriptsPath = 'src/scripts';
 
@@ -24,18 +26,18 @@ function getFolders(dir) {
 gulp.task('publish', function () {
     gulp.src(['app/**/*.js'])
         .pipe(gulp.dest(destination))
-    //.pipe(livereload());
 });
 
 gulp.task('watch', function () {
-    // livereload.listen({
-    //     port: 80
-    // });
     gulp.watch('app/**/*.js', ['publish']);
 });
 
-gulp.task('generate-unit-tests', function () {
-
+gulp.task('generate-specs-model', function () {
+    gulp.src('app/model/*.js')
+        //.pipe(rename('main.min.js'))
+        .pipe(fcg())
+        .pipe(prettify({collapseWhitespace: true}))
+        .pipe(gulp.dest('test/specs'));
 });
 
 gulp.task('default', ['publish', 'watch']);
