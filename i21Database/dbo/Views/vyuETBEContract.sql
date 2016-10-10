d9a19daa423a8ac65590b963f3efc6aac6fab9b3
kenneth lagunero <kenneth.lagunero@irely.com>
2016-10-10 23:35:59 +0800

    IET-90

---
+++ i21Database/dbo/Views/vyuETBEContract.sql
@@ -0,0 +1,25 @@
+﻿CREATE VIEW [dbo].[vyuETBEContract]  
+AS 
+	SELECT
+		C.strEntityNo 
+		,E.strItemNo
+		,B.dblCashPrice
+		,B.dblBalance
+		,E.intItemId
+		,C.intEntityId
+		,B.intContractDetailId
+	FROM tblCTContractHeader A
+	INNER JOIN tblCTContractDetail B
+		ON A.intContractHeaderId = B.intContractHeaderId
+	INNER JOIN tblEMEntity C
+		ON A.intEntityId = C.intEntityId
+	INNER JOIN tblICItem E
+		ON B.intItemId = E.intItemId	
+	WHERE DATEADD(dd, DATEDIFF(dd, 0, B.dtmStartDate), 0) <= DATEADD(dd, DATEDIFF(dd, 0, GETDATE()), 0)
+		AND DATEADD(dd, DATEDIFF(dd, 0, B.dtmEndDate), 0) >= DATEADD(dd, DATEDIFF(dd, 0, GETDATE()), 0)
+		AND B.dblBalance > 0
+		AND B.intContractDetailId IN (SELECT MIN(intContractDetailId) FROM tblCTContractDetail WHERE intContractHeaderId = A.intContractHeaderId AND intItemId = E.intItemId)
+		AND B.intContractHeaderId IN (SELECT MIN(intContractHeaderId) FROM tblCTContractHeader WHERE intEntityId = A.intEntityId)
+		AND (E.ysnAvailableTM = 1 OR E.strType = 'Service')
+
+GO
--- i21Database/dbo/Views/vyuETBEExportContract.sql
+++ i21Database/dbo/Views/vyuETBEExportContract.sql
@@ -1,24 +1,11 @@
 ﻿CREATE VIEW [dbo].[vyuETBEExportContract]  
 AS 
 	SELECT
-		account=C.strEntityNo 
-		,productCode= E.strItemNo
-		,preBuyPrice= CAST(ISNULL(B.dblCashPrice,0.0) AS NUMERIC(18,4))
-		,preBuyQty= CAST(ISNULL(B.dblBalance,0.0) AS NUMERIC(18,2))
+		account= strEntityNo 
+		,productCode= strItemNo
+		,preBuyPrice= CAST(ISNULL(dblCashPrice,0.0) AS NUMERIC(18,4))
+		,preBuyQty= CAST(ISNULL(dblBalance,0.0) AS NUMERIC(18,2))
 		,contractPrice= CAST(0.0 AS NUMERIC(18,4))
 		,contractQty= CAST(0.0 AS NUMERIC(18,2))
-	FROM tblCTContractHeader A
-	INNER JOIN tblCTContractDetail B
-		ON A.intContractHeaderId = B.intContractHeaderId
-	INNER JOIN tblEMEntity C
-		ON A.intEntityId = C.intEntityId
-	INNER JOIN tblICItem E
-		ON B.intItemId = E.intItemId	
-	WHERE DATEADD(dd, DATEDIFF(dd, 0, B.dtmStartDate), 0) <= DATEADD(dd, DATEDIFF(dd, 0, GETDATE()), 0)
-		AND DATEADD(dd, DATEDIFF(dd, 0, B.dtmEndDate), 0) >= DATEADD(dd, DATEDIFF(dd, 0, GETDATE()), 0)
-		AND B.dblBalance > 0
-		AND B.intContractDetailId IN (SELECT MIN(intContractDetailId) FROM tblCTContractDetail WHERE intContractHeaderId = A.intContractHeaderId AND intItemId = E.intItemId)
-		AND B.intContractHeaderId IN (SELECT MIN(intContractHeaderId) FROM tblCTContractHeader WHERE intEntityId = A.intEntityId)
-		AND (E.ysnAvailableTM = 1 OR E.strType = 'Service')
-
+	FROM vyuETBEContract
 GO
