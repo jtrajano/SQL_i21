e675ca0b01311e3fea299d2a5f67ec3f269960c8
Feb Montefrio <feb.montefrio@irely.com>
2016-10-11 15:07:30 +0800

    IC-2844 IC-2817 IC-2838 IC-2839 IC-2842 IC-2831

---
+++ i21Database/dbo/Stored Procedures/uspICGetItemInTransitLocation.sql
@@ -0,0 +1,51 @@
+﻿CREATE PROCEDURE [dbo].[uspICGetItemInTransitLocation]
+	@intItemId INT 
+	,@strItemNo NVARCHAR(50) = NULL
+	,@intItemLocationId INT = NULL OUTPUT 
+AS
+
+-- Counter check the item id from the item table. 
+SELECT	@intItemId = i.intItemId	 
+FROM	tblICItem i
+WHERE	1 = CASE	WHEN i.intItemId = @intItemId THEN 1 
+					WHEN @intItemId IS NULL AND i.strItemNo = @strItemNo THEN 1 
+					ELSE 0 
+		END 
+
+-- Validate the item id. 
+IF @intItemId IS NULL 
+BEGIN
+	SET @intItemLocationId = NULL 
+	RETURN -1;
+END 
+
+-- Insert or get the item location id 
+MERGE 
+INTO	dbo.tblICItemLocation
+WITH	(HOLDLOCK) 
+AS		ItemLocation
+USING	(
+	SELECT	name = 'In-Transit'
+			,intItemId = @intItemId
+) InTransit
+	ON ItemLocation.intItemId = InTransit.intItemId
+	AND ItemLocation.strDescription = InTransit.name
+	
+WHEN MATCHED THEN 
+	UPDATE	
+	SET		@intItemLocationId = ItemLocation.intItemLocationId 
+
+WHEN NOT MATCHED THEN 
+	INSERT (
+		intItemId
+		,strDescription
+	)
+	VALUES (
+		InTransit.intItemId
+		,InTransit.name 
+	)
+;
+
+SET @intItemLocationId = ISNULL(@intItemLocationId, @@IDENTITY)
+
+RETURN 0
