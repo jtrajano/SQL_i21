1adbf654131b0db520d9d9aa67d2d9ee20cf5e3e
Lawrence Zabala <lawrence.zabala@irely.com>
2016-10-12 11:00:43 +0800

    TR-512

--- i21Database/dbo/Views/vyuTRDispatchedLoad.sql
+++ i21Database/dbo/Views/vyuTRDispatchedLoad.sql
@@ -23,11 +23,10 @@ SELECT LG.intLoadId
 							ELSE ISNULL(LG.dblSCashPrice, 0) END
 	, strTerminalName = CASE WHEN (LG.strType != 'Outbound') THEN LG.strVendor
 								ELSE NULL END
-	, strSupplyPoint = CASE WHEN (LG.strType != 'Outbound') THEN VendorLocation.strLocationName
-								ELSE NULL END
+	, strSupplyPoint = LG.strShipFrom
 	, strLocationName = CASE WHEN (LG.strType != 'Outbound') THEN LG.strPLocationName
 								ELSE LG.strSLocationName END
-	, strInboundItemNo = Item.strItemNo
+	, strInboundItemNo = LG.strItemNo
 	, strInboundContractNumber = CASE WHEN (LG.strType != 'Outbound') THEN LG.strPContractNumber
 										ELSE NULL END
 	, intEntityCustomerId = LG.intCustomerEntityId
@@ -36,14 +35,13 @@ SELECT LG.intLoadId
 	, intShipToLocationId = LG.intCustomerEntityLocationId
 	, intEntitySalespersonId = Customer.intSalespersonId
 	, strCustomerNumber = Customer.strName
-	, strOutboundLocationName = Location.strLocationName
+	, strOutboundLocationName = ISNULL(LG.strSLocationName, LG.strPLocationName)
 	, strOutboundSalespersonId = Salesperson.strName
-	, strShipTo = CASE WHEN (LG.strType != 'Outbound') THEN LG.strShipTo
-						ELSE CustomerLocation.strLocationName END
+	, LG.strShipTo
 	, intOutboundItemId = LG.intItemId
-	, dblOutboundQuantity = ISNULL(LG.dblQuantity, 0)
-	, dblOutboundPrice = ISNULL(LG.dblSCashPrice, 0)
-	, strOutboundItemNo = Item.strItemNo
+	, dblOutboundQuantity = ISNULL(LG.dblQuantity, 0.000000)
+	, dblOutboundPrice = ISNULL(LG.dblSCashPrice, 0.000000)
+	, strOutboundItemNo = LG.strItemNo
 	, strOutboundContractNumber = LG.strSContractNumber
 	, LG.dtmScheduledDate
 	, LG.dtmDispatchedDate
@@ -63,50 +61,27 @@ SELECT LG.intLoadId
 	, LG.ysnInProgress
 	, intOutboundLoadId = LG.intLoadId
 	, strSupplierLoadNumber = LG.strExternalLoadNumber
-	, strInboundPricingType = CASE WHEN (LG.strType != 'Outbound') THEN PurchaseContract.strPricingType
-									ELSE NULL END
-	, strOutboundPricingType = SalesContract.strPricingType
-	, dblInboundAdjustment = ISNULL(CASE WHEN (LG.strType != 'Outbound') THEN PurchaseContract.dblAdjustment
-									ELSE NULL END, 0)
-	, dblOutboundAdjustment = ISNULL(SalesContract.dblAdjustment, 0)
-	, strZipCode = VendorLocation.strZipCode
+	, LG.strZipCode
 	, intRackPriceSupplyPointId = CASE WHEN (LG.strType != 'Outbound') THEN SP.intRackPriceSupplyPointId
 										ELSE NULL END
-	, intItemUOMId = ItemUOM.intItemUOMId
-	, strInboundIndexType = CASE WHEN (LG.strType != 'Outbound') THEN PurchaseContract.strIndexType
-									ELSE NULL END
-	, strOutboundIndexType = SalesContract.strIndexType
-	, intInboundIndexRackPriceSupplyPointId = CASE WHEN (LG.strType != 'Outbound') THEN (CASE WHEN ISNULL(PurchaseContract.strIndexType, 0) = 'Fixed'
-																								THEN ISNULL(PurchaseContract.intRackPriceSupplyPointId, PurchaseContract.intSupplyPointId)
-																							WHEN ISNULL(PurchaseContract.strIndexType, 0) != 'Fixed'
-																								THEN NULL
-																							END)
-													ELSE NULL END
-	, intOutboundIndexRackPriceSupplyPointId  = CASE WHEN ISNULL(SalesContract.strIndexType, 0) = 'Fixed'
-														THEN ISNULL(SalesContract.intRackPriceSupplyPointId, SalesContract.intSupplyPointId)
-													WHEN ISNULL(SalesContract.strIndexType, 0) != 'Fixed'
-														THEN NULL
-													END
-	, intInboundTaxGroupId = CASE WHEN (LG.strType != 'Outbound') THEN SP.intTaxGroupId
-									ELSE NULL END
-	, strInboundTaxGroup = CASE WHEN (LG.strType != 'Outbound') THEN VendorTax.strTaxGroup
-								ELSE NULL END
-	, intOutboundTaxGroupId = CustomerLocation.intTaxGroupId
-	, strOutboundTaxGroup = CustomerTax.strTaxGroup
+	, LG.intItemUOMId
+	, LG.strInboundPricingType
+	, LG.strOutboundPricingType
+	, LG.dblInboundAdjustment
+	, LG.dblOutboundAdjustment
+	, LG.strInboundIndexType
+	, LG.strOutboundIndexType
+	, LG.intInboundIndexRackPriceSupplyPointId 
+	, LG.intOutboundIndexRackPriceSupplyPointId
+	, LG.intInboundTaxGroupId
+	, LG.strInboundTaxGroup
+	, LG.intOutboundTaxGroupId
+	, LG.strOutboundTaxGroup
 FROM vyuLGLoadDetailView LG
-LEFT JOIN tblSMCompanyLocation Location ON Location.intCompanyLocationId = ISNULL(LG.intSCompanyLocationId, LG.intPCompanyLocationId)
 LEFT JOIN tblTRCompanyPreference Config ON Config.intCompanyPreferenceId = Config.intCompanyPreferenceId
 LEFT JOIN tblEMEntity Seller ON Seller.intEntityId = Config.intSellerId
-LEFT JOIN tblICItem Item ON Item.intItemId = LG.intItemId
-CROSS APPLY (SELECT TOP 1 * FROM tblICItemUOM WHERE tblICItemUOM.intItemId = LG.intItemId ORDER BY tblICItemUOM.ysnStockUnit DESC) ItemUOM
-LEFT JOIN vyuCTContractDetailView PurchaseContract ON PurchaseContract.intContractDetailId = LG.intPContractDetailId
-LEFT JOIN vyuCTContractDetailView SalesContract ON SalesContract.intContractDetailId = LG.intSContractDetailId
 LEFT JOIN tblTRSupplyPoint SP ON SP.intEntityLocationId = LG.intVendorEntityLocationId AND SP.intEntityVendorId = LG.intVendorEntityId
-LEFT JOIN tblEMEntityLocation VendorLocation ON VendorLocation.intEntityLocationId = LG.intVendorEntityLocationId
-LEFT JOIN tblSMTaxGroup VendorTax ON VendorTax.intTaxGroupId = VendorLocation.intTaxGroupId
 LEFT JOIN vyuARCustomer Customer ON Customer.intEntityCustomerId = LG.intCustomerEntityId
-LEFT JOIN tblEMEntityLocation CustomerLocation ON CustomerLocation.intEntityLocationId = LG.intCustomerEntityLocationId
-LEFT JOIN tblSMTaxGroup CustomerTax ON CustomerTax.intTaxGroupId = CustomerLocation.intTaxGroupId
 LEFT JOIN vyuEMEntity Salesperson ON Salesperson.intEntityId = Customer.intSalespersonId AND Salesperson.strType = 'Salesperson'
 WHERE ISNULL(LG.ysnDispatched, 0) = 1
 	AND ISNULL(LG.dblDeliveredQuantity, 0) <= 0
