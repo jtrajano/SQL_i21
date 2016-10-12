e4ff96b7fdcadc35b0465eae54f2825092143c4a
Satya <ssahoo@BLRSATYAWIN7.irely.net>
2016-10-12 16:57:28 +0530

    CT-856

---
+++ i21Database/dbo/Views/vyuCTInventoryItem.sql
@@ -0,0 +1,23 @@
+﻿CREATE VIEW [dbo].[vyuCTInventoryItem]
+
+AS 
+
+	SELECT	intKey = CAST(ROW_NUMBER() OVER(ORDER BY IM.intItemId, IL.intLocationId) AS INT),
+			IM.intItemId,
+			IM.strItemNo,
+			IM.strType,
+			IM.strDescription,
+			IM.strLotTracking,
+			IM.strInventoryTracking,
+			IM.strStatus,
+			IL.intLocationId,
+			IM.intCategoryId,
+			CR.strCategoryCode,
+			IM.intCommodityId,
+			CO.strCommodityCode
+
+	FROM	tblICItem			IM
+	JOIN	tblICItemLocation	IL	ON	IL.intItemId		=	IM.intItemId		LEFT
+	JOIN	tblICCategory		CR	ON	CR.intCategoryId	=	IM.intCategoryId	LEFT
+	JOIN	tblICCommodity		CO	ON	CO.intCommodityId	=	IM.intCommodityId
+	WHERE	strType IN ('Inventory','Finished Good','Raw Material','Bundle')
