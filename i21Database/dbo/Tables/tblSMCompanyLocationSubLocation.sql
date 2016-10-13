faccc0a1852ea65da42477efdc705f2b88ed4981
Dan Michael Jalimao <dan.jalimao@irely.com>
2016-10-13 17:47:54 +0800

    FCOM-2176

--- i21Database/dbo/Tables/tblSMCompanyLocationSubLocation.sql
+++ i21Database/dbo/Tables/tblSMCompanyLocationSubLocation.sql
@@ -4,6 +4,7 @@
 	[intCompanyLocationId] INT NOT NULL, 
     [strSubLocationName] NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL, 
     [strSubLocationDescription] NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL, 
+	[intVendorId] INT NULL,
     [strClassification] NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL, 
 	[ysnExternal] BIT NULL DEFAULT (0),
 	[intNewLotBin] INT NULL, 
@@ -17,5 +18,6 @@
     [dblLongitude] NUMERIC (18, 6) DEFAULT ((0)) NOT NULL,
     [intConcurrencyId] INT NOT NULL DEFAULT 1, 
     CONSTRAINT [PK_tblSMCompanyLocationSubLocation] PRIMARY KEY ([intCompanyLocationSubLocationId]), 
-    CONSTRAINT [FK_tblSMCompanyLocationSubLocation_tblSMCompanyLocation] FOREIGN KEY (intCompanyLocationId) REFERENCES tblSMCompanyLocation(intCompanyLocationId) ON DELETE CASCADE 
+    CONSTRAINT [FK_tblSMCompanyLocationSubLocation_tblSMCompanyLocation] FOREIGN KEY (intCompanyLocationId) REFERENCES tblSMCompanyLocation(intCompanyLocationId) ON DELETE CASCADE, 
+	CONSTRAINT [FK_tblSMCompanyLocationSubLocation_tblEMEntity] FOREIGN KEY ([intVendorId]) REFERENCES tblEMEntity([intEntityId])
 )
