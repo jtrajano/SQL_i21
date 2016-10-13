9a25fd32ade4531f5cef33a9f8acc8214579e775
Lawrence Zabala <lawrence.zabala@irely.com>
2016-10-13 20:00:57 +0800

    TR-118

---
+++ i21Database/dbo/Tables/tblTRLoadBlendIngredient.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[tblTRLoadBlendIngredient]
+(
+	[intLoadBlendIngredientId] INT NOT NULL, 
+    [intLoadDistributionDetailId] INT NOT NULL, 
+    [intItemId] INT NOT NULL, 
+    [dblQuantity] NUMERIC(18, 6) NOT NULL DEFAULT ((0)), 
+    [intConcurrencyId] INT NOT NULL DEFAULT ((1)), 
+    CONSTRAINT [PK_tblTRLoadBlendIngredient] PRIMARY KEY ([intLoadBlendIngredientId]), 
+    CONSTRAINT [FK_tblTRLoadBlendIngredient_tblTRLoadDistributionDetail] FOREIGN KEY ([intLoadDistributionDetailId]) REFERENCES [tblTRLoadDistributionDetail]([intLoadDistributionDetailId]) ON DELETE CASCADE
+)
