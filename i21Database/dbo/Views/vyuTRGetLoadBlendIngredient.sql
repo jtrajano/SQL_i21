b377badea6d4a3785328a82e8bc51600afd8244f
Lawrence Zabala <lawrence.zabala@irely.com>
2016-10-14 20:37:18 +0800

    TR-118

---
+++ i21Database/dbo/Views/vyuTRGetLoadBlendIngredient.sql
@@ -0,0 +1,17 @@
+﻿CREATE VIEW [dbo].[vyuTRGetLoadBlendIngredient]
+	AS
+
+SELECT LoadIngredient.intLoadBlendIngredientId
+	, LoadIngredient.intLoadDistributionDetailId
+	, intLoadDistributionItemId = LoadDetail.intItemId
+	, dblLoadDistributionQty = LoadDetail.dblUnits
+	, intIngredientItemId = LoadIngredient.intItemId
+	, dblIngredientQty = LoadIngredient.dblQuantity
+	, Recipe.dblLowerTolerance
+	, Recipe.dblUpperTolerance
+	, dblRecipeQty = Recipe.dblQuantity
+	, dblActualQty = LoadDetail.dblUnits * Recipe.dblQuantity
+FROM tblTRLoadBlendIngredient LoadIngredient
+LEFT JOIN tblTRLoadDistributionDetail LoadDetail ON LoadDetail.intLoadDistributionDetailId = LoadIngredient.intLoadDistributionDetailId
+LEFT JOIN vyuMFGetRecipeItem Recipe ON Recipe.intRecipeHeaderItemId = LoadDetail.intItemId
+	AND Recipe.intRecipeIngredientItemId = LoadIngredient.intItemId
