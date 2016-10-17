567bf3128607fff8181a5564d1930a680e4c2128
csMJardiolin <MJardiolin@COMTECH.COM>
2016-10-17 14:55:17 +0800

    AR-3607

--- i21Database/dbo/Views/vyuARLetterCustomerSearch.sql
+++ i21Database/dbo/Views/vyuARLetterCustomerSearch.sql
@@ -4,7 +4,11 @@ SELECT
 	DISTINCT intEntityCustomerId	=	(CAST(intEntityCustomerId AS INT)) 
 	, strCustomerNumber
 	, strCustomerName 
-
+	, dbl0DaysSum
+	, dbl30DaysSum
+	, dbl60DaysSum
+	, dbl90DaysSum
+	, dblTotalARSum
 FROM 
 	vyuARCollectionOverdueReport
 
