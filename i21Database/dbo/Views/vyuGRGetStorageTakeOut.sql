eefdd5f3e12573d4447c5678841aacaefefca528
Gopinath Nayak <gopinath.n@irely.com>
2016-10-18 18:53:52 +0530

    AR-3645

---
+++ i21Database/dbo/Views/vyuGRGetStorageTakeOut.sql
@@ -0,0 +1,19 @@
+﻿CREATE VIEW [dbo].[vyuGRGetStorageTakeOut]
+AS
+SELECT 
+ intStorageTypeId
+,strStorageTypeDescription
+,intEntityId
+,strName
+,intItemId
+,strItemNo
+,SUM(dblOpenBalance) dblOpenBalance
+FROM vyuGRGetStorageTransferTicket
+WHERE ysnCustomerStorage=0 AND ysnDPOwnedType=0  
+GROUP BY 
+intStorageTypeId
+,strStorageTypeDescription
+,intEntityId
+,strName
+,intItemId
+,strItemNo
