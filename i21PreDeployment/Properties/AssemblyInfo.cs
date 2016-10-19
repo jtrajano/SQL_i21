9b2a9ced09438c5bb6a00441c94fe7ed7237d551
Lawrence Zabala <lawrence.zabala@irely.com>
2016-10-19 13:02:00 +0800

    FRM-3843

--- i21PreDeployment/Properties/AssemblyInfo.cs
+++ i21PreDeployment/Properties/AssemblyInfo.cs
@@ -5,19 +5,3 @@
 // associated with the SQLCLR assembly.
 [assembly: AssemblyTitle("i21PreDeployment")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("iRely LLC")]
-[assembly: AssemblyProduct("i21PreDeployment")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-//[assembly: AssemblyVersion("1.0.0.0")]
-//[assembly: AssemblyFileVersion("1.0.0.0")]
