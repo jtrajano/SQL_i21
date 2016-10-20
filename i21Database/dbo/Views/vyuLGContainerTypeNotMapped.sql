5ec4e1b658d5305a71c288ec4be1556f96f40272
Swetosuvro Ghosh <swetosuvro.g@irely.com>
2016-10-20 13:46:32 +0530

    LG-519 Container Type

---
+++ i21Database/dbo/Views/vyuLGContainerTypeNotMapped.sql
@@ -0,0 +1,21 @@
+﻿CREATE VIEW vyuLGContainerTypeNotMapped 
+AS
+SELECT CT.intContainerTypeId
+	  ,CT.intConcurrencyId
+	  ,CT.strContainerType
+	  ,CT.dblLength
+	  ,CT.dblWidth
+	  ,CT.dblHeight
+	  ,CT.intDimensionUnitMeasureId
+	  ,CT.dblNetWeight
+	  ,CT.dblEmptyWeight
+	  ,CT.dblGrossWeight
+	  ,CT.intWeightUnitMeasureId
+	  ,CTCQ.dblQuantity
+	  ,C.strCommodityCode
+	  ,C.strDescription AS strCommodityDescription
+	  ,UM.strUnitMeasure
+FROM tblLGContainerType CT
+JOIN tblLGContainerTypeCommodityQty CTCQ ON CT.intContainerTypeId = CTCQ.intContainerTypeId
+JOIN tblICCommodity C ON C.intCommodityId = CTCQ.intCommodityId
+JOIN tblICUnitMeasure UM ON UM.intUnitMeasureId = CT.intWeightUnitMeasureId
