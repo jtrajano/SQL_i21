c841468a933d50ec0aec5d86177dd29a3382b8ff
Jayson Ramos <jayson.ramos@iRely.com>
2016-10-21 11:07:09 +0800

    CRM-124

--- i21Database/dbo/Views/vyuCRMCampaignSearch.sql
+++ i21Database/dbo/Views/vyuCRMCampaignSearch.sql
@@ -5,10 +5,10 @@
 		,tblEMEntity.strName
 		,[tblSMLineOfBusiness].strLineOfBusiness
 		,tblCRMType.strType
-		,[tblCRMCampainStatus].strStatus
+		,[tblCRMStatus].strStatus
 	from
 		[tblCRMCampaign]
 		left outer join tblEMEntity on tblEMEntity.intEntityId = [tblCRMCampaign].intEntityId
 		left outer join [tblSMLineOfBusiness] on [tblSMLineOfBusiness].intLineOfBusinessId = [tblCRMCampaign].intLineOfBusinessId
 		left outer join tblCRMType on tblCRMType.intTypeId = [tblCRMCampaign].[intTypeId]
-		left outer join [tblCRMCampainStatus] on [tblCRMCampainStatus].intCampaignStatusId = [tblCRMCampaign].intStatusId
+		left outer join [tblCRMStatus] on [tblCRMStatus].intStatusId = [tblCRMCampaign].intStatusId
