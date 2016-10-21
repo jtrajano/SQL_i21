ed5e8509dbea883c2c3120a480597903f0a7b86d
jeffrey trajano <jeffrey.trajano@irely.com>
2016-10-21 09:49:19 +0800

    GL-2998 Performance Issue in General Journal

--- i21Database/dbo/Views/vyuGLSearchGJ.sql
+++ i21Database/dbo/Views/vyuGLSearchGJ.sql
@@ -1,14 +1,6 @@
 ﻿CREATE VIEW [dbo].[vyuGLSearchGJ]
 AS
-WITH total AS
-  (SELECT  j.strJournalId,
-          sum(d.dblCredit) dblCredit,
-          sum(d.dblDebit) dblDebit
-   FROM tblGLJournal j
-   JOIN tblGLJournalDetail d ON j.intJournalId = d.intJournalId
-   GROUP BY j.strJournalId)
-     
-   SELECT strJournalType,
+SELECT strJournalType,
            strTransactionType,
            strSourceType,
            j.strJournalId,
@@ -19,10 +11,57 @@ WITH total AS
            dtmReverseDate,
            dtmDateEntered,
            e.strName strUserName,
-           t.dblCredit,
-           t.dblDebit,
+           total.dblCredit,
+           total.dblDebit,
 		   strCurrency
    FROM tblGLJournal j
-   JOIN total t ON j.strJournalId = t.strJournalId
+   OUTER APPLY (SELECT  j.strJournalId,
+          SUM(ISNULL(d.dblCredit,0.0)) dblCredit,
+          SUM(ISNULL(d.dblDebit,0.0)) dblDebit
+   FROM tblGLJournalDetail d WHERE j.intJournalId = d.intJournalId) total
    LEFT JOIN tblEMEntity e ON e.intEntityId = j.intEntityId
    LEFT JOIN tblSMCurrency C ON C.intCurrencyID = j.intCurrencyId
+   where  strTransactionType IN ('General Journal','Recurring')
+   and  strSourceType <> 'AA'
+GO
+CREATE NONCLUSTERED INDEX [_dta_index_tblEMEntity_9_2007079032__K1_2] ON [dbo].[tblEMEntity]
+(
+	[intEntityId] ASC
+)
+INCLUDE ( 	[strName]) WITH (SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF) ON [PRIMARY]
+GO
+CREATE STATISTICS [_dta_stat_600545373_4_19_15] ON [dbo].[tblGLJournal]([strTransactionType], [strSourceType], [intEntityId])
+GO
+CREATE STATISTICS [_dta_stat_600545373_7_15_1_19] ON [dbo].[tblGLJournal]([intCurrencyId], [intEntityId], [intJournalId], [strSourceType])
+GO
+CREATE STATISTICS [_dta_stat_600545373_7_4_19_15] ON [dbo].[tblGLJournal]([intCurrencyId], [strTransactionType], [strSourceType], [intEntityId])
+GO
+CREATE STATISTICS [_dta_stat_600545373_19_7_15] ON [dbo].[tblGLJournal]([strSourceType], [intCurrencyId], [intEntityId])
+GO
+CREATE NONCLUSTERED INDEX [_dta_index_tblGLJournal_9_600545373__K15_K4_K19_K7_K1_2_3_5_10_11_13_17] ON [dbo].[tblGLJournal]
+(
+	[intEntityId] ASC,
+	[strTransactionType] ASC,
+	[strSourceType] ASC,
+	[intCurrencyId] ASC,
+	[intJournalId] ASC
+)
+INCLUDE ( 	[dtmReverseDate],
+	[strJournalId],
+	[dtmDate],
+	[strDescription],
+	[ysnPosted],
+	[dtmDateEntered],
+	[strJournalType]) WITH (SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF) ON [PRIMARY]
+GO
+CREATE STATISTICS [_dta_stat_600545373_1_4_19_7_15] ON [dbo].[tblGLJournal]([intJournalId], [strTransactionType], [strSourceType], [intCurrencyId], [intEntityId])
+GO
+CREATE STATISTICS [_dta_stat_600545373_19_4] ON [dbo].[tblGLJournal]([strSourceType], [strTransactionType])
+GO
+CREATE NONCLUSTERED INDEX [_dta_index_tblGLJournalDetail_9_691598348__K3_6_8] ON [dbo].[tblGLJournalDetail]
+(
+	[intJournalId] ASC
+)
+INCLUDE ( 	[dblDebit],
+	[dblCredit]) WITH (SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF) ON [PRIMARY]
+GO
