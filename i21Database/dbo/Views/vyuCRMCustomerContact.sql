ca3d18ce1bd3996a17da372ea4f7c3225992d12f
Jayson Ramos <jayson.ramos@iRely.com>
2016-10-21 10:58:54 +0800

    CRM-249

---
+++ i21Database/dbo/Views/vyuCRMCustomerContact.sql
@@ -0,0 +1,36 @@
+﻿CREATE VIEW [dbo].[vyuCRMCustomerContact]
+	AS
+	select
+		  intContactId = ec.[intEntityId]
+		  ,intCustomerId = c.[intEntityCustomerId]
+		  ,c.strCustomerNumber
+		  ,strCompanyName = e.strName
+		  ,strContactName = ec.strName
+		  ,ec.strEmail
+		  ,ec.strTitle
+		  ,strPhone = ph.strPhone
+		  ,strMobile = mob.strPhone
+		  ,el.strLocationName
+		  ,elc.strTimezone
+		  ,intConcurrencyId = 1
+		  ,intTicketProductId = (select top 1 intProductId from tblARCustomerProductVersion where intCustomerId = c.[intEntityCustomerId])
+		  ,intVersionId = (select top 1 intVersionId from tblARCustomerProductVersion where intCustomerId = c.[intEntityCustomerId])
+		  ,ysnActive = c.ysnActive
+		  ,ysnActiveContact = ec.ysnActive
+		  ,ec.imgPhoto
+		  ,ysnBillable = c.ysnHDBillableSupport
+		  ,strEntityType = (select top 1 et.strType from tblEMEntityType et where et.intEntityId = c.[intEntityCustomerId] and et.strType in ('Customer','Prospect'))
+		  ,intModuleId = (select top 1 intModuleId from tblARCustomerProductVersion where intCustomerId = c.[intEntityCustomerId])
+		  ,strComapny = (select top 1 strCompany from tblARCustomerProductVersion where intCustomerId = c.[intEntityCustomerId])
+		from
+			tblARCustomer c
+		  inner join tblEMEntityToContact etc on etc.intEntityId = c.intEntityCustomerId
+		  inner join tblEMEntity ec on ec.[intEntityId] = etc.[intEntityContactId]
+		  inner join tblEMEntity e on e.intEntityId = c.[intEntityCustomerId]
+		  left outer join tblEMEntityLocation el on el.intEntityLocationId = etc.intEntityLocationId
+		  left outer join tblEMEntityToContact etcc on etcc.intEntityContactId = etc.intEntityContactId
+		  left outer join tblEMEntityLocation elc on elc.intEntityLocationId = etcc.intEntityLocationId
+		  left join tblEMEntityPhoneNumber ph 
+		   on ec.intEntityId = ph.intEntityId
+		  left join tblEMEntityMobileNumber mob
+		   on ec.intEntityId = mob.intEntityId
---
+++ i21Database/dbo/Views/vyuCRMCustomerProspectType.sql
@@ -0,0 +1,15 @@
+﻿CREATE VIEW [dbo].[vyuCRMCustomerProspectType]
+	AS
+	select
+		distinct
+		intId = ROW_NUMBER() over (order by intEntityCustomerId)
+		,tblARCustomer.intEntityCustomerId
+		,tblARCustomer.strCustomerNumber
+		,strEntityName = tblEMEntity.strName
+		,tblEMEntityType.strType
+		,contact.strEmail
+	from tblARCustomer
+		left outer join tblEMEntity on tblEMEntity.intEntityId = tblARCustomer.intEntityCustomerId
+		left outer join tblEMEntityType on tblEMEntityType.intEntityId = tblARCustomer.intEntityCustomerId and tblEMEntityType.strType in ('Customer','Prospect')
+		left outer join tblEMEntityToContact on tblEMEntityToContact.intEntityId = tblARCustomer.intEntityCustomerId and tblEMEntityToContact.ysnDefaultContact = 1
+		left outer join tblEMEntity contact on contact.intEntityId = tblEMEntityToContact.intEntityContactId
