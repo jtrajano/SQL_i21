c1ba733985ed0fbb6b20f3f5c3d3c01b18a92cda
Eddzonne Barroga <eddzonne.barroga@irely.com>
2016-10-21 10:45:47 +0800

    CF-729

--- i21Database/dbo/Tables/tblCFProductAuthDetail.sql
+++ i21Database/dbo/Tables/tblCFProductAuthDetail.sql
@@ -5,7 +5,7 @@
     [intConcurrencyId]       INT CONSTRAINT [DF_tblCFProductAuth_intConcurrencyId] DEFAULT ((1)) NULL,
     CONSTRAINT [PK_tblCFProductAuth] PRIMARY KEY CLUSTERED ([intProductAuthDetailId] ASC),
     CONSTRAINT [FK_tblCFProductAuthDetail_tblCFItem] FOREIGN KEY ([intItemId]) REFERENCES [dbo].[tblCFItem] ([intItemId]),
-    CONSTRAINT [FK_tblCFProductAuthDetail_tblCFProductAuth] FOREIGN KEY ([intProductAuthId]) REFERENCES [dbo].[tblCFProductAuth] ([intProductAuthId])
+    CONSTRAINT [FK_tblCFProductAuthDetail_tblCFProductAuth] FOREIGN KEY ([intProductAuthId]) REFERENCES [dbo].[tblCFProductAuth] ([intProductAuthId]) ON DELETE CASCADE	
 );
 
 
