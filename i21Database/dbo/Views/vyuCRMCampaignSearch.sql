ee742f9fdd7675cb5627552f88bf7a3284d6813e
Jayson Ramos <jayson.ramos@iRely.com>
2016-10-21 11:01:20 +0800

    CRM-249

--- i21Database/dbo/Views/vyuCRMCampaignSearch.sql
+++ i21Database/dbo/Views/vyuCRMCampaignSearch.sql
@@ -1,14 +1,14 @@
-﻿CREATE VIEW [dbo].[vyuHDOpportunityCampaignSearch]
+﻿CREATE VIEW [dbo].[vyuCRMCampaignSearch]
 	AS
 	select
 		[tblCRMCampaign].*
 		,tblEMEntity.strName
 		,[tblSMLineOfBusiness].strLineOfBusiness
-		,tblHDTicketType.strType
+		,tblCRMType.strType
 		,[tblCRMCampainStatus].strStatus
 	from
 		[tblCRMCampaign]
 		left outer join tblEMEntity on tblEMEntity.intEntityId = [tblCRMCampaign].intEntityId
 		left outer join [tblSMLineOfBusiness] on [tblSMLineOfBusiness].intLineOfBusinessId = [tblCRMCampaign].intLineOfBusinessId
-		left outer join tblHDTicketType on tblHDTicketType.intTicketTypeId = [tblCRMCampaign].[intTypeId]
+		left outer join tblCRMType on tblCRMType.intTypeId = [tblCRMCampaign].[intTypeId]
 		left outer join [tblCRMCampainStatus] on [tblCRMCampainStatus].intCampaignStatusId = [tblCRMCampaign].intStatusId
