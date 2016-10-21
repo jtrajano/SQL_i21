811e1ccf208ffc0afa57d8c253ab5edaaa583de7
Jayson Ramos <jayson.ramos@iRely.com>
2016-10-21 15:14:31 +0800

    CRM-249

---
+++ i21Database/dbo/Views/vyuCRMSalesPerson.sql
@@ -0,0 +1,34 @@
+﻿CREATE VIEW [dbo].[vyuCRMSalesPerson]
+	AS
+	select
+			tblEMEntity.intEntityId
+			,tblEMEntity.strName
+			,strSalespersonId = (case when tblEMEntity.strEntityNo is null then tblARSalesperson.strSalespersonId else tblEMEntity.strEntityNo end)
+			,tblARSalesperson.strTitle
+			,tblARSalesperson.strType
+			,tblEMEntity.strEmail
+			,tblEMEntity.strPhone
+			,ysnActiveEntity = tblEMEntity.ysnActive
+			,ysnActiveSalesPerson = tblARSalesperson.ysnActive
+			,strSalesPersonType = 'Sales Rep Entity'
+	from tblARSalesperson, tblEMEntity
+	where
+		tblEMEntity.intEntityId = tblARSalesperson.intEntitySalespersonId
+
+	union all
+
+		select
+			intEntityId = tblEMEntityToContact.intEntityContactId
+			,e.strName
+			,strSalespersonId = (case when e.strEntityNo is null then tblARSalesperson.strSalespersonId else e.strEntityNo end)
+			,tblARSalesperson.strTitle
+			,tblARSalesperson.strType
+			,e.strEmail
+			,e.strPhone
+			,ysnActiveEntity = e.ysnActive
+			,ysnActiveSalesPerson = tblARSalesperson.ysnActive
+			,strSalesPersonType = 'Sales Rep Contact'
+	from tblARSalesperson, tblEMEntity e, tblEMEntityToContact
+	where
+		tblEMEntityToContact.intEntityId = tblARSalesperson.intEntitySalespersonId
+		and e.intEntityId = tblEMEntityToContact.intEntityContactId
