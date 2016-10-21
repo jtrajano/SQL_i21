c02720abb73a6b7c263c8b67f2b12aafa4d94293
jeffrey trajano <jeffrey.trajano@irely.com>
2016-10-21 08:51:52 +0800

    GL-2996 Performance issues with the GL Account detail screen

--- i21Database/dbo/Tables/tblGLDetail.sql
+++ i21Database/dbo/Tables/tblGLDetail.sql
@@ -38,4 +38,59 @@
     CONSTRAINT [PK_tblGL] PRIMARY KEY CLUSTERED ([intGLDetailId] ASC),
     CONSTRAINT [FK_tblGL_tblGLAccount] FOREIGN KEY ([intAccountId]) REFERENCES [dbo].[tblGLAccount] ([intAccountId])
 );
+GO
+
+CREATE NONCLUSTERED INDEX [_dta_index_tblEMEntityCredential_9_1290448367__K2_3] ON [dbo].[tblEMEntityCredential]
+(
+	[intEntityId] ASC
+)
+INCLUDE ( 	[strUserName]) WITH (SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF) ON [PRIMARY]
+GO
+CREATE STATISTICS [_dta_stat_1702557399_9_5_1] ON [dbo].[tblGLAccount]([intAccountUnitId], [intAccountGroupId], [intAccountId])
+GO
+CREATE STATISTICS [_dta_stat_1702557399_5_1] ON [dbo].[tblGLAccount]([intAccountGroupId], [intAccountId])
+GO
+CREATE STATISTICS [_dta_stat_1702557399_1_9] ON [dbo].[tblGLAccount]([intAccountId], [intAccountUnitId])
+GO
+CREATE STATISTICS [_dta_stat_424544746_2_4_18_20_1] ON [dbo].[tblGLDetail]([dtmDate], [intAccountId], [ysnIsUnposted], [intEntityId], [intGLDetailId])
+GO
+CREATE STATISTICS [_dta_stat_424544746_2_20_4] ON [dbo].[tblGLDetail]([dtmDate], [intEntityId], [intAccountId])
+GO
+CREATE STATISTICS [_dta_stat_424544746_20_1_2_4] ON [dbo].[tblGLDetail]([intEntityId], [intGLDetailId], [dtmDate], [intAccountId])
+GO
+CREATE STATISTICS [_dta_stat_424544746_20_1_17_2_4] ON [dbo].[tblGLDetail]([intEntityId], [intGLDetailId], [intJournalLineNo], [dtmDate], [intAccountId])
+GO
+CREATE STATISTICS [_dta_stat_424544746_1_17_2_4] ON [dbo].[tblGLDetail]([intGLDetailId], [intJournalLineNo], [dtmDate], [intAccountId])
+GO
+CREATE NONCLUSTERED INDEX [_dta_index_tblGLDetail_9_424544746__K4_K18_K2_K1_K17_K20_3_5_6_7_8_9_10_11_16_21_22_23_24_25_27_29] ON [dbo].[tblGLDetail]
+(
+	[intAccountId] ASC,
+	[ysnIsUnposted] ASC,
+	[dtmDate] ASC,
+	[intGLDetailId] ASC,
+	[intJournalLineNo] ASC,
+	[intEntityId] ASC
+)
+INCLUDE ( 	[strBatchId],
+	[dblDebit],
+	[dblCredit],
+	[dblDebitUnit],
+	[dblCreditUnit],
+	[strDescription],
+	[strCode],
+	[strReference],
+	[strJournalLineDescription],
+	[strTransactionId],
+	[intTransactionId],
+	[strTransactionType],
+	[strTransactionForm],
+	[strModuleName],
+	[dblDebitForeign],
+	[dblCreditForeign]) WITH (SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF) ON [PRIMARY]
+
+GO
+CREATE STATISTICS [_dta_stat_424544746_18_20_2] ON [dbo].[tblGLDetail]([ysnIsUnposted], [intEntityId], [dtmDate])
+GO
+CREATE STATISTICS [_dta_stat_424544746_17_4_18_2_1_20] ON [dbo].[tblGLDetail]([intJournalLineNo], [intAccountId], [ysnIsUnposted], [dtmDate], [intGLDetailId], [intEntityId])
+GO
 
