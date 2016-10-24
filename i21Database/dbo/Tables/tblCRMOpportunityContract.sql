1651565270d61f5247fc8feac0f24f73639bae11
Jayson Ramos <jayson.ramos@iRely.com>
2016-10-24 13:59:45 +0800

    CRM-255

---
+++ i21Database/dbo/Tables/tblCRMOpportunityContract.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tblCRMOpportunityContract]
+(
+	[intOpportunityContractId] [int] IDENTITY(1,1) NOT NULL,
+	[intOpportunityId] [int] NOT NULL,
+	[intContractHeaderId] [int] NOT NULL,
+	[intConcurrencyId] [int] NOT NULL DEFAULT 1,
+	CONSTRAINT [PK_tblCRMOpportunityContract_intOpportunityQuoteId] PRIMARY KEY CLUSTERED ([intOpportunityContractId] ASC),
+	CONSTRAINT [AK_tblCRMOpportunityContract_intOpportunityId_intContractHeaderId] UNIQUE ([intOpportunityId],[intContractHeaderId]),
+    CONSTRAINT [FK_tblCRMOpportunityContract_tblCRMOpportunity_intOpportunityId] FOREIGN KEY ([intOpportunityId]) REFERENCES [dbo].[tblCRMOpportunity] ([intOpportunityId]),
+    CONSTRAINT [FK_tblCRMOpportunityContract_tblCTContractHeader_intContractHeaderId] FOREIGN KEY ([intContractHeaderId]) REFERENCES [dbo].[tblCTContractHeader] ([intContractHeaderId])
+)
---
+++ i21Database/dbo/Tables/tblCRMOpportunityQuote.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tblCRMOpportunityQuote]
+(
+	[intOpportunityQuoteId] [int] IDENTITY(1,1) NOT NULL,
+	[intOpportunityId] [int] NOT NULL,
+	[intSalesOrderId] [int] NOT NULL,
+	[intConcurrencyId] [int] NOT NULL DEFAULT 1,
+	CONSTRAINT [PK_tblCRMOpportunityQuote_intOpportunityQuoteId] PRIMARY KEY CLUSTERED ([intOpportunityQuoteId] ASC),
+	CONSTRAINT [AK_tblCRMOpportunityQuote_intOpportunityId_intSalesOrderId] UNIQUE ([intOpportunityId],[intSalesOrderId]),
+    CONSTRAINT [FK_tblCRMOpportunityQuote_tblCRMOpportunity_intOpportunityId] FOREIGN KEY ([intOpportunityId]) REFERENCES [dbo].[tblCRMOpportunity] ([intOpportunityId]),
+    CONSTRAINT [FK_tblCRMOpportunityQuote_tblSOSalesorder_intSalesOrderId] FOREIGN KEY ([intSalesOrderId]) REFERENCES [dbo].[tblSOSalesOrder] ([intSalesOrderId])
+)
