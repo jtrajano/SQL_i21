6710bbcc5442dba85769f25a73954dc592cd0c31
Jayson Ramos <jayson.ramos@iRely.com>
2016-10-24 10:27:58 +0800

    CRM-253

---
+++ i21Database/dbo/Views/vyuCRMLostRevenue.sql
@@ -0,0 +1,26 @@
+﻿CREATE VIEW [dbo].[vyuCRMLostRevenue]
+	AS
+		select
+			tblSOSalesOrder.intEntityCustomerId
+			,tblEMEntity.strName
+			,intDate = convert(int,convert(nvarchar(8),tblSOSalesOrder.dtmDate,112))
+			,tblEMEntityToContact.intEntityContactId
+			,tblARCustomer.intSalespersonId
+			,dblSalesOrderTotal = sum(tblSOSalesOrder.dblSalesOrderTotal)
+		from
+			tblSOSalesOrder
+			,tblEMEntity
+			,tblEMEntityToContact
+			,tblARCustomer
+		where
+			tblSOSalesOrder.strTransactionType = 'Order'
+			and tblEMEntity.intEntityId = tblSOSalesOrder.intEntityCustomerId
+			and tblEMEntityToContact.intEntityId = tblEMEntity.intEntityId
+			and tblEMEntityToContact.ysnDefaultContact = 1
+			and tblARCustomer.intEntityCustomerId = tblEMEntity.intEntityId
+		group by
+			tblSOSalesOrder.intEntityCustomerId
+			,tblEMEntity.strName
+			,tblSOSalesOrder.dtmDate
+			,tblEMEntityToContact.intEntityContactId
+			,tblARCustomer.intSalespersonId
