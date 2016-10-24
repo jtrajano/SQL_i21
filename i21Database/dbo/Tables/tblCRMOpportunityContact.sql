f71a1c38e0a20390a55c4992e97495808ec8dbdf
Jayson Ramos <jayson.ramos@iRely.com>
2016-10-24 12:27:31 +0800

    CRM-255

---
+++ i21Database/dbo/Tables/tblCRMOpportunityContact.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[tblCRMOpportunityContact]
+(
+	[intOpportunityContactId] [int] IDENTITY(1,1) NOT NULL,
+	[intOpportunityId] [int] NOT NULL,
+	[intEntityId] [int] NOT NULL,
+	[strDecisionRole] [nvarchar](100) COLLATE Latin1_General_CI_AS NULL,
+	[strAttitude] [nvarchar](100) COLLATE Latin1_General_CI_AS NULL,
+	[strExtent] [nvarchar](100) COLLATE Latin1_General_CI_AS NULL,
+	[strConcerns] [nvarchar](MAX) COLLATE Latin1_General_CI_AS NULL,
+	[strExpectations] [nvarchar](MAX) COLLATE Latin1_General_CI_AS NULL,
+	[intSort] [int] NULL,
+	[intConcurrencyId] [int] NOT NULL DEFAULT 1,
+	CONSTRAINT [PK_tblCRMOpportunityContact_intOpportunityContactId] PRIMARY KEY CLUSTERED ([intOpportunityContactId] ASC),
+	CONSTRAINT [UQ_tblCRMOpportunityContact_intOpportunityId_intEnityId] UNIQUE ([intOpportunityId],[intEntityId]),
+    CONSTRAINT [FK_tblCRMOpportunityContact_tblCRMOpportunity_intOpportunityId] FOREIGN KEY ([intOpportunityId]) REFERENCES [dbo].[tblCRMOpportunity] ([intOpportunityId]) on delete cascade,
+	CONSTRAINT [FK_tblCRMOpportunityContact_tblEMEntity_intEntityId] FOREIGN KEY ([intEntityId]) REFERENCES [dbo].tblEMEntity ([intEntityId])
+)
