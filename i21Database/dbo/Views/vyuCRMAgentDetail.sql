69fdf8fb3d08f0293b49ec87b6f94a5e00b41b8e
Jayson Ramos <jayson.ramos@iRely.com>
2016-10-25 10:49:44 +0800

    CRM-255

---
+++ i21Database/dbo/Views/vyuCRMAgentDetail.sql
@@ -0,0 +1,32 @@
+﻿CREATE VIEW [dbo].[vyuCRMAgentDetail]
+	AS
+	select
+		strName = ec.strUserName,
+		intId = us.[intEntityUserSecurityId],
+		strFullName = en.strName,
+		strEmail = en.strEmail,
+		intEntityId = us.[intEntityUserSecurityId],
+		ysnDisabled = us.ysnDisabled
+	from
+		tblSMUserSecurity us
+		inner join vyuEMEntityContact en on en.intEntityId = us.[intEntityUserSecurityId] and en.ysnDefaultContact = 1
+		inner join [tblEMEntityCredential] ec on ec.intEntityId = us.[intEntityUserSecurityId]
+	where
+		us.[intEntityUserSecurityId] is not null
+
+	union all
+
+	select
+		strName = ec.strUserName
+		,intId = etc.intEntityContactId
+		,strFullName = en.strName
+		,strEmail = en.strEmail
+		,intEntityId = us.intEntitySalespersonId
+		,ysnDisabled = (case when convert(bit, us.ysnActive) = 0 then convert(bit, 1) else convert(bit, 0) end)
+	from
+		tblARSalesperson us, [tblEMEntityToContact] etc, tblEMEntity en, [tblEMEntityCredential] ec
+	where
+		us.intEntitySalespersonId is not null
+		and etc.intEntityId = us.intEntitySalespersonId
+		and en.intEntityId = etc.intEntityContactId
+		and ec.intEntityId = etc.intEntityContactId
