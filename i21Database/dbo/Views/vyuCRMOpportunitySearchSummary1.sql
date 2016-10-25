ca8ac6d223737a88ac7eed7af57efdf36bfd4f89
Jayson Ramos <jayson.ramos@iRely.com>
2016-10-25 10:10:55 +0800

    CRM-255

---
+++ i21Database/dbo/Views/vyuCRMOpportunitySearchSummary1.sql
@@ -0,0 +1,13 @@
+﻿CREATE VIEW [dbo].[vyuCRMOpportunitySearchSummary1]
+	AS
+		select
+			intId = ROW_NUMBER() over (order by strSalesPerson)
+			,strSalesPerson
+			,dblTotalNetOpportunityAmount = (case when sum(dblNetOpportunityAmmount) is null then 0 else sum(dblNetOpportunityAmmount) end)
+		from
+			vyuCRMOpportunitySearch
+		where
+			strOpportunityStatus <> 'Closed'
+			and strSalesPerson is not null
+		group by
+			strSalesPerson
