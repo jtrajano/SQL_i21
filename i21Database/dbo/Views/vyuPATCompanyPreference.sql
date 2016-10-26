1762624fa5d7478bd4b838a464ae87bb119c71e4
Jb Mago <benedict.mago@irely.com>
2016-10-26 19:18:14 +0800

    PAT-468

---
+++ i21Database/dbo/Views/vyuPATCompanyPreference.sql
@@ -0,0 +1,34 @@
+﻿CREATE VIEW [dbo].[vyuPATCompanyPreference]
+	AS
+SELECT   CP.intCompanyPreferenceId,
+         CP.intGrainDiscountId, 
+         CP.intGrainStorageId, 
+         CP.intServiceChargeId, 
+         CP.intDebitMemoId, 
+         CP.intDiscountGivenId, 
+         CP.strRefund, 
+         CP.dblMinimumRefund, 
+         CP.dblServiceFee, 
+         CP.dblCutoffAmount, 
+         CP.strCutoffTo, 
+         CP.strPayOnGrain, 
+         CP.strPrintCheck, 
+         CP.intPaymentItemId, 
+         CP.dblMinimumDividends, 
+         CP.ysnProRatedDividends, 
+         CP.dtmCutoffDate, 
+         CP.intVotingStockId, 
+         CP.intNonVotingStockId, 
+         CP.intFractionalShareId, 
+         CP.intServiceFeeIncomeId, 
+         CP.intFWTLiabilityAccountId, 
+         CP.intDividendsGLAccount,
+		 strDividendsGLAccount = DA.strAccountId, 
+         CP.intTreasuryGLAccount, 
+		 strTreasuryGLAccount = TA.strAccountId,
+         CP.intAPClearingGLAccount
+FROM tblPATCompanyPreference CP
+LEFT JOIN tblGLAccount DA
+	ON DA.intAccountId = CP.intDividendsGLAccount
+LEFT JOIN tblGLAccount TA
+	ON TA.intAccountId = CP.intTreasuryGLAccount
