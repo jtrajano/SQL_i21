8623d7e3c63d381f1434855d5f4d1c6e69a644a6
Kelvin Quinto <kelvin.quinto@irely.com>
2016-10-28 06:32:59 +0800

    AR-3678, AR-3679

--- i21Database/dbo/Views/vyuARUnappliedCreditsRegister.sql
+++ i21Database/dbo/Views/vyuARUnappliedCreditsRegister.sql
@@ -1,5 +1,8 @@
 ﻿CREATE VIEW [dbo].[vyuARUnappliedCreditsRegister]
 AS
+
+SELECT * FROM
+(
 SELECT DISTINCT 
       C.strCustomerNumber
 	, strName				= RTRIM(C.strCustomerNumber) + ' - ' + C.strName
@@ -18,8 +21,8 @@ FROM tblARInvoice I
 	INNER JOIN (vyuARCustomer C INNER JOIN vyuARCustomerContacts CC ON C.intEntityCustomerId = CC.intEntityCustomerId AND ysnDefaultContact = 1) 
 		ON I.intEntityCustomerId = C.intEntityCustomerId
 	INNER JOIN tblSMCompanyLocation L ON I.intCompanyLocationId = L.intCompanyLocationId
-	INNER JOIN tblARPayment P ON I.intPaymentId = P.intPaymentId AND P.ysnPosted = 1
-	INNER JOIN (
+	LEFT JOIN tblARPayment P ON I.intPaymentId = P.intPaymentId AND P.ysnPosted = 1
+	LEFT JOIN (
 		(SELECT SUM(dblPayment) AS dblPayment
 				 , PD.intInvoiceId
 			FROM tblARPaymentDetail PD INNER JOIN tblARPayment P ON PD.intPaymentId = P.intPaymentId AND P.ysnPosted = 1
@@ -28,5 +31,40 @@ FROM tblARInvoice I
 WHERE I.ysnPosted = 1
 AND I.ysnPaid = 0
 AND ((I.strType = 'Service Charge' AND I.ysnForgiven = 0) OR ((I.strType <> 'Service Charge' AND I.ysnForgiven = 1) OR (I.strType <> 'Service Charge' AND I.ysnForgiven = 0)))
-AND I.strTransactionType NOT IN ('Invoice', 'Debit Memo')
+AND I.strTransactionType NOT IN ('Invoice', 'Debit Memo', 'Customer Prepayment')
 AND I.intAccountId IN (SELECT intAccountId FROM vyuGLAccountDetail WHERE strAccountCategory IN ('AR Account', 'Customer Prepayments'))
+
+UNION ALL
+
+SELECT DISTINCT 
+      C.strCustomerNumber
+	, strName				= RTRIM(C.strCustomerNumber) + ' - ' + C.strName
+	, I.intEntityCustomerId
+	, strInvoiceNumber
+	, strTransactionType	
+	, L.strLocationName
+	, dtmDate
+	, dblAmount				= ISNULL(dblInvoiceTotal, 0) * -1
+	, dblUsed				= ISNULL(PD.dblPayment, 0)
+	, dblRemaining			= (ISNULL(I.dblInvoiceTotal, 0) + ISNULL(PD.dblPayment, 0)) * -1
+	, strContact			= [dbo].fnARFormatCustomerAddress(CC.strPhone, CC.strEmail, C.strBillToLocationName, C.strBillToAddress, C.strBillToCity, C.strBillToState, C.strBillToZipCode, C.strBillToCountry, NULL, 0)	
+    , strCompanyName		= (SELECT TOP 1 strCompanyName FROM tblSMCompanySetup)
+    , strCompanyAddress		= (SELECT TOP 1 dbo.[fnARFormatCustomerAddress](NULL, NULL, NULL, strAddress, strCity, strState, strZip, strCountry, NULL, NULL) FROM tblSMCompanySetup)
+FROM tblARInvoice I
+	INNER JOIN (vyuARCustomer C INNER JOIN vyuARCustomerContacts CC ON C.intEntityCustomerId = CC.intEntityCustomerId AND ysnDefaultContact = 1) 
+		ON I.intEntityCustomerId = C.intEntityCustomerId
+	INNER JOIN tblSMCompanyLocation L ON I.intCompanyLocationId = L.intCompanyLocationId
+	INNER JOIN tblARPayment P ON I.intPaymentId = P.intPaymentId AND P.ysnPosted = 1
+	LEFT JOIN (
+		(SELECT SUM(PD.dblPayment) AS dblPayment
+				 , PD.intInvoiceId
+			FROM tblARPaymentDetail PD 
+				INNER JOIN tblARPayment P ON PD.intPaymentId = P.intPaymentId AND P.ysnPosted = 1 AND P.ysnInvoicePrepayment = 0
+			GROUP BY PD.intInvoiceId) 
+		) PD ON I.intInvoiceId = PD.intInvoiceId
+WHERE I.ysnPosted = 1
+AND I.ysnPaid = 0
+AND ((I.strType = 'Service Charge' AND I.ysnForgiven = 0) OR ((I.strType <> 'Service Charge' AND I.ysnForgiven = 1) OR (I.strType <> 'Service Charge' AND I.ysnForgiven = 0)))
+AND I.strTransactionType = 'Customer Prepayment'
+AND I.intAccountId IN (SELECT intAccountId FROM vyuGLAccountDetail WHERE strAccountCategory IN ('AR Account', 'Customer Prepayments'))
+) AS A
