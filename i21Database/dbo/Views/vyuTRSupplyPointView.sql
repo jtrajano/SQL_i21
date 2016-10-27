78616ecf41f7cf770ca4be9591b986327611baea
Lawrence Zabala <lawrence.zabala@irely.com>
2016-10-27 13:45:22 +0800

    TR-599

--- i21Database/dbo/Views/vyuTRSupplyPointView.sql
+++ i21Database/dbo/Views/vyuTRSupplyPointView.sql
@@ -18,8 +18,10 @@ SELECT SupplyPoint.intSupplyPointId
 	, SupplyPoint.ysnMultipleDueDates
 	, SupplyPoint.ysnMultipleBolInvoiced
 	, SupplyPoint.intRackPriceSupplyPointId
+	, EntityLocation.ysnActive
 FROM tblTRSupplyPoint SupplyPoint
 LEFT JOIN tblSMTaxGroup TaxGroup ON TaxGroup.intTaxGroupId = SupplyPoint.intTaxGroupId
 LEFT JOIN vyuEMEntity Supplier ON Supplier.intEntityId = SupplyPoint.intEntityVendorId AND Supplier.strType = 'Vendor'
 LEFT JOIN tblEMEntityLocation EntityLocation ON EntityLocation.intEntityLocationId = SupplyPoint.intEntityLocationId
 LEFT JOIN tblTFTerminalControlNumber Terminal ON Terminal.intTerminalControlNumberId = SupplyPoint.intTerminalControlNumberId
+WHERE EntityLocation.ysnActive = 1
