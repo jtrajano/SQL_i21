134b275580120f58e4f1404a48f0215123428bfe
Premkumar S <prem.selladurai@irely.com>
2016-10-27 18:06:58 +0530

    MFG-2043

---
+++ i21Database/dbo/Stored Procedures/uspMFAdjustInventory.sql
@@ -0,0 +1,65 @@
+﻿CREATE PROCEDURE uspMFAdjustInventory (
+	@dtmDate DATETIME 
+	,@intTransactionTypeId INT 
+	,@intItemId INT 
+	,@intSourceLotId INT 
+	,@intDestinationLotId INT
+	,@dblQty NUMERIC(38, 20)
+	,@intItemUOMId INT
+	,@intOldItemId INT
+	,@dtmOldExpiryDate DATETIME
+	,@dtmNewExpiryDate DATETIME
+	,@intOldLotStatusId INT
+	,@intNewLotStatusId INT
+	,@intUserId INT 
+	,@strNote NVARCHAR(MAX)
+	,@strReason NVARCHAR(MAX)
+	)
+AS
+BEGIN TRY
+	DECLARE @ErrMsg NVARCHAR(MAX)
+
+	INSERT INTO tblMFInventoryAdjustment (
+		dtmDate
+		,intTransactionTypeId
+		,intItemId
+		,intSourceLotId
+		,intDestinationLotId
+		,dblQty
+		,intItemUOMId
+		,intOldItemId
+		,dtmOldExpiryDate
+		,dtmNewExpiryDate
+		,intOldLotStatusId
+		,intNewLotStatusId
+		,intUserId
+		,strNote
+		,strReason
+		)
+	SELECT @dtmDate
+		,@intTransactionTypeId
+		,@intItemId
+		,@intSourceLotId
+		,@intDestinationLotId
+		,@dblQty
+		,@intItemUOMId
+		,@intOldItemId
+		,@dtmOldExpiryDate
+		,@dtmNewExpiryDate
+		,@intOldLotStatusId
+		,@intNewLotStatusId
+		,@intUserId
+		,@strNote
+		,@strReason
+END TRY
+
+BEGIN CATCH
+	SET @ErrMsg = ERROR_MESSAGE()
+
+	RAISERROR (
+			@ErrMsg
+			,16
+			,1
+			,'WITH NOWAIT'
+			)
+END CATCH
---
+++ i21Database/dbo/Tables/tblMFInventoryAdjustment.sql
@@ -0,0 +1,25 @@
+﻿CREATE TABLE dbo.tblMFInventoryAdjustment (
+	intInventoryAdjustmentId INT identity(1, 1)
+	,dtmDate DATETIME NOT NULL
+	,intTransactionTypeId INT NOT NULL
+	,intItemId INT NOT NULL
+	,intSourceLotId INT NOT NULL
+	,intDestinationLotId INT
+	,dblQty NUMERIC(38, 20)
+	,intItemUOMId INT
+	,intOldItemId INT
+	,dtmOldExpiryDate DATETIME
+	,dtmNewExpiryDate DATETIME
+	,intOldLotStatusId INT
+	,intNewLotStatusId INT
+	,intUserId INT NOT NULL
+	,strNote NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NULL
+	,strReason NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NULL CONSTRAINT PK_tblMFInventoryAdjustment PRIMARY KEY (intInventoryAdjustmentId)
+	,CONSTRAINT FK_tblMFInventoryAdjustment_tblICItem_intItemId FOREIGN KEY (intItemId) REFERENCES tblICItem(intItemId)
+	,CONSTRAINT FK_tblMFInventoryAdjustment_tblICItem_intOldItemId FOREIGN KEY (intOldItemId) REFERENCES tblICItem(intItemId)
+	,CONSTRAINT FK_tblMFInventoryAdjustment_tblICItemUOM_intItemUOMId FOREIGN KEY (intItemUOMId) REFERENCES tblICItemUOM(intItemUOMId)
+	,CONSTRAINT FK_tblMFInventoryAdjustment_tblICLotStatus_intOldLotStatusId FOREIGN KEY (intOldLotStatusId) REFERENCES tblICLotStatus(intLotStatusId)
+	,CONSTRAINT FK_tblMFInventoryAdjustment_tblICLotStatus_intNewLotStatusId FOREIGN KEY (intNewLotStatusId) REFERENCES tblICLotStatus(intLotStatusId)
+	)
+
+
