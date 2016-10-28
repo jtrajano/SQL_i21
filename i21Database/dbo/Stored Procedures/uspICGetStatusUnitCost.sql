5c954e6325a3c3540912829cd175b284fe1b8083
Joy Balangatan <joy.balangatan@irely.com>
2016-10-28 19:01:22 +0800

    IC-2746 Added SP uspICGetStatusUnitCost

---
+++ i21Database/dbo/Stored Procedures/uspICGetStatusUnitCost.sql
@@ -0,0 +1,36 @@
+﻿CREATE PROCEDURE [dbo].[uspICGetStatusUnitCost]
+	@intReceiptId INT,
+	@intReceiptItemsStatusId INT OUTPUT
+AS
+SET QUOTED_IDENTIFIER OFF
+SET ANSI_NULLS ON
+SET NOCOUNT ON
+SET XACT_ABORT ON
+SET ANSI_WARNINGS OFF
+
+DECLARE @TotalCost AS NUMERIC(38,20),
+		@Status_AllZeroCost AS INT = 1,
+		@Status_SomeHaveZeroCost AS INT = 2,
+		@Status_NoZeroCost AS INT = 3
+
+BEGIN
+	IF EXISTS (SELECT * FROM tblICInventoryReceiptItem WHERE intInventoryReceiptId = @intReceiptId) 
+		BEGIN
+			SELECT @TotalCost = SUM(dblUnitCost) FROM tblICInventoryReceiptItem WHERE intInventoryReceiptId = @intReceiptId
+				IF @TotalCost = 0
+					BEGIN
+						SET @intReceiptItemsStatusId = @Status_AllZeroCost
+					END
+				ELSE
+					IF EXISTS(SELECT * FROM tblICInventoryReceiptItem WHERE intInventoryReceiptId = @intReceiptId AND dblUnitCost = 0)
+						BEGIN
+							SET @intReceiptItemsStatusId = @Status_SomeHaveZeroCost
+						END
+					ELSE
+						BEGIN
+							SET @intReceiptItemsStatusId = @Status_NoZeroCost
+						END
+		END
+	ELSE
+		RAISERROR('There are no receipt items to process.', 16, 1);
+END
