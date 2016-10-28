7fed8213397eb340161264a74f1f078d2d58203d
Jb Mago <benedict.mago@irely.com>
2016-10-28 14:27:39 +0800

    PAT-363

--- i21Database/dbo/Views/vyuPATVolumeDetails.sql
+++ i21Database/dbo/Views/vyuPATVolumeDetails.sql
@@ -1,29 +1,29 @@
 ﻿CREATE VIEW [dbo].[vyuPATVolumeDetails]
 		 AS 
-	 SELECT	CV.intCustomerPatronId,
-			ENT.strName,
-			CV.intFiscalYear,
-			FY.strFiscalYear,
-			AR.strStockStatus,
-			TC.strTaxCode,
-			dblPurchase = sum(CASE WHEN PC.strPurchaseSale = 'Purchase' THEN CV.dblVolume ELSE 0 END),
-			dblSale = sum(CASE WHEN PC.strPurchaseSale = 'Sale' THEN CV.dblVolume ELSE 0 END),
-			dtmLastActivityDate = max(CV.dtmLastActivityDate),
-			dblVolume = sum(CV.dblVolume),
-			CV.intConcurrencyId 
-	   FROM tblPATCustomerVolume CV
- INNER JOIN tblEMEntity ENT
-		 ON ENT.intEntityId = intCustomerPatronId
- INNER JOIN tblPATPatronageCategory PC
-		 ON PC.intPatronageCategoryId = CV.intPatronageCategoryId
- INNER JOIN tblGLFiscalYear FY
-		 ON FY.intFiscalYearId = CV.intFiscalYear
- INNER JOIN tblARCustomer AR
-		 ON AR.intEntityCustomerId = CV.intCustomerPatronId
-  LEFT JOIN tblSMTaxCode TC
-		 ON TC.intTaxCodeId = AR.intTaxCodeId
-		 group by CV.intCustomerPatronId,
-		 ENT.strName, CV.intFiscalYear,FY.strFiscalYear,
-		 AR.strStockStatus,TC.strTaxCode,CV.intConcurrencyId
-
-GO
+SELECT	CV.intCustomerVolumeId,
+		CV.intCustomerPatronId,
+		ENT.strName,
+		CV.intFiscalYear,
+		FY.strFiscalYear,
+		AR.strStockStatus,
+		TC.strTaxCode,
+		dblPurchase = sum(CASE WHEN PC.strPurchaseSale = 'Purchase' THEN CV.dblVolume ELSE 0 END),
+		dblSale = sum(CASE WHEN PC.strPurchaseSale = 'Sale' THEN CV.dblVolume ELSE 0 END),
+		dtmLastActivityDate = max(CV.dtmLastActivityDate),
+		dblVolume = sum(CV.dblVolume),
+		CV.ysnRefundProcessed,
+		CV.intConcurrencyId
+	FROM tblPATCustomerVolume CV
+INNER JOIN tblEMEntity ENT
+		ON ENT.intEntityId = intCustomerPatronId
+INNER JOIN tblPATPatronageCategory PC
+		ON PC.intPatronageCategoryId = CV.intPatronageCategoryId
+INNER JOIN tblGLFiscalYear FY
+		ON FY.intFiscalYearId = CV.intFiscalYear
+INNER JOIN tblARCustomer AR
+		ON AR.intEntityCustomerId = CV.intCustomerPatronId
+LEFT JOIN tblSMTaxCode TC
+		ON TC.intTaxCodeId = AR.intTaxCodeId
+		GROUP BY CV.intCustomerVolumeId,CV.intCustomerPatronId,
+		ENT.strName, CV.intFiscalYear,FY.strFiscalYear,
+		AR.strStockStatus,TC.strTaxCode,CV.ysnRefundProcessed,CV.intConcurrencyId
