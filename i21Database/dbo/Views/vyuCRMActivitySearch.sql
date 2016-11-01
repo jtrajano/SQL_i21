79c63cbc5815af3429668d38e931c36f68c500f4
Jayson Ramos <jayson.ramos@iRely.com>
2016-11-01 17:03:21 +0800

    CRM-261

---
+++ i21Database/dbo/Views/vyuCRMActivitySearch.sql
@@ -0,0 +1,31 @@
+﻿CREATE VIEW [dbo].[vyuCRMActivitySearch]
+	AS
+		select
+			strOpportunityName = a.strName
+			,d.strActivityNo
+			,d.strType
+			,d.strSubject
+			,d.strDetails
+			,strCustomerName = cusE.strName
+			,strContactName = conE.strName
+			,d.strStatus
+			,d.strPriority
+			,d.strCategory
+			,strAssignedTo = assE.strName
+			,d.dtmCreated
+			,a.intOpportunityId
+			,d.intActivityId
+			,d.intEntityId
+			,d.intEntityContactId
+			,d.intAssignedTo
+		from
+			tblCRMOpportunity a
+			,tblSMTransaction b
+			,tblSMActivity d
+			left join tblEMEntity conE on conE.intEntityId = d.intEntityContactId
+			left join tblEMEntity cusE on cusE.intEntityId = d.intEntityId
+			left join tblEMEntity assE on assE.intEntityId = d.intAssignedTo
+		where
+			b.strRecordNo = convert(nvarchar(50), a.intOpportunityId)
+			and b.intScreenId = (select top 1 c.intScreenId from tblSMScreen c where c.strModule = 'CRM' and c.strNamespace = 'CRM.view.Opportunity')
+			and d.intTransactionId = b.intTransactionId
