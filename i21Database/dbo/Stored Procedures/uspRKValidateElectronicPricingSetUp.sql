a9ea97c17b931dfe0fd7f1790e16518f4773879c
Gopinath Nayak <gopinath.n@irely.com>
2016-11-04 20:17:42 +0530

    GRN-274, GRN-344

--- i21Database/dbo/Stored Procedures/uspRKValidateElectronicPricingSetUp.sql
+++ i21Database/dbo/Stored Procedures/uspRKValidateElectronicPricingSetUp.sql
@@ -10,6 +10,7 @@ BEGIN TRY
 	DECLARE @strPassword NVARCHAR(100)
 	DECLARE @IntinterfaceSystem INT
 	DECLARE @StrQuoteProvider NVARCHAR(100)
+	DECLARE @strProviderAccessType NVARCHAR(30)
 	DECLARE @URL NVARCHAR(1000)
 	
 
@@ -27,16 +28,20 @@ BEGIN TRY
 	SELECT @StrQuoteProvider = strQuoteProvider
 	FROM tblGRUserPreference
 	WHERE [intEntityUserSecurityId] = @intUserId
+	
+	SELECT @strProviderAccessType=ISNULL(strProviderAccessType,'') 
+	FROM tblGRUserPreference 
+	WHERE intEntityUserSecurityId=@intUserId
 
 	SELECT @IntinterfaceSystem = intInterfaceSystemId
 	FROM tblSMCompanyPreference
 
-	IF  (	    @IntinterfaceSystem <> 1
+	IF  ((	    @IntinterfaceSystem <> 1
 			AND @IntinterfaceSystem <> 2
 		)
 		OR (ISNULL(@strUserName, '') = '')
 		OR (ISNULL(@URL, '') = '')
-		OR (ISNULL(@StrQuoteProvider, '') <> 'DTN/Agricharts')
+		OR (ISNULL(@StrQuoteProvider, '') <> 'DTN/Agricharts')) AND @strProviderAccessType <>'History Only'
 		
 		BEGIN
 			SET @ElectronicPricingSetUp = 0
