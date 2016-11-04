51561f251c7560c174b35a61bc7d7f18fdb301bb
Jb Mago <benedict.mago@irely.com>
2016-11-04 17:25:17 +0800

    PAT-111

---
+++ i21Database/dbo/Functions/fnPATCreateIssueStockGLEntries.sql
@@ -0,0 +1,214 @@
+﻿CREATE FUNCTION [dbo].[fnPATCreateIssueStockGLEntries]
+(
+	@transactionIds NVARCHAR(MAX),
+	@votingStock BIT,
+	@intUserId INT
+)
+RETURNS @returntable TABLE
+(
+	[dtmDate]                   DATETIME         NOT NULL,
+	[strBatchId]                NVARCHAR (20)    COLLATE Latin1_General_CI_AS NULL,
+	[intAccountId]              INT              NULL,
+	[dblDebit]                  NUMERIC (18, 6)  NULL,
+	[dblCredit]                 NUMERIC (18, 6)  NULL,
+	[dblDebitUnit]              NUMERIC (18, 6)  NULL,
+	[dblCreditUnit]             NUMERIC (18, 6)  NULL,
+	[strDescription]            NVARCHAR (255)   COLLATE Latin1_General_CI_AS NULL,
+	[strCode]                   NVARCHAR (40)    COLLATE Latin1_General_CI_AS NULL,    
+	[strReference]              NVARCHAR (255)   COLLATE Latin1_General_CI_AS NULL,
+	[intCurrencyId]             INT              NULL,
+	[dblExchangeRate]           NUMERIC (38, 20) DEFAULT 1 NOT NULL,
+	[dtmDateEntered]            DATETIME         NOT NULL,
+	[dtmTransactionDate]        DATETIME         NULL,
+	[strJournalLineDescription] NVARCHAR (250)   COLLATE Latin1_General_CI_AS NULL,
+	[intJournalLineNo]			INT              NULL,
+	[ysnIsUnposted]             BIT              NOT NULL,    
+	[intUserId]                 INT              NULL,
+	[intEntityId]				INT              NULL,
+	[strTransactionId]          NVARCHAR (40)    COLLATE Latin1_General_CI_AS NULL,
+	[intTransactionId]          INT              NULL,
+	[strTransactionType]        NVARCHAR (255)   COLLATE Latin1_General_CI_AS NULL,
+	[strTransactionForm]        NVARCHAR (255)   COLLATE Latin1_General_CI_AS NULL,
+	[strModuleName]             NVARCHAR (255)   COLLATE Latin1_General_CI_AS NULL,
+	[dblDebitForeign]           NUMERIC (18, 6)	NULL,
+    [dblDebitReport]            NUMERIC (18, 6) NULL,
+    [dblCreditForeign]          NUMERIC (18, 6) NULL,
+    [dblCreditReport]           NUMERIC (18, 6) NULL,
+    [dblReportingRate]          NUMERIC (18, 6) NULL,
+    [dblForeignRate]            NUMERIC (18, 6) NULL,
+	[intConcurrencyId]          INT              DEFAULT 1 NOT NULL
+)
+AS
+BEGIN
+
+	DECLARE @MODULE_NAME NVARCHAR(25) = 'Patronage'
+	DECLARE @SCREEN_NAME NVARCHAR(25) = 'Issue Stock'
+	DECLARE @MODULE_CODE NVARCHAR(5)  = 'PAT'
+
+	DECLARE @tmpTransacions TABLE (
+		[intTransactionId] [int] PRIMARY KEY,
+		UNIQUE (intTransactionId)
+	);
+
+	INSERT INTO @tmpTransacions SELECT [intID] AS intTransactionId FROM [dbo].fnGetRowsFromDelimitedValues(@transactionIds)
+
+	IF (@votingStock = 1) --POST VOTING STOCKS
+	BEGIN
+		INSERT INTO @returntable
+		--VOTING STOCK ISSUED
+		SELECT	
+			[dtmDate]						=	DATEADD(dd, DATEDIFF(dd, 0, A.dtmIssueDate), 0),
+			[strBatchID]					=	'',
+			[intAccountId]					=	ComPref.intVotingStockId,
+			[dblDebit]						=	0,
+			[dblCredit]						=	A.dblFaceValue,
+			[dblDebitUnit]					=	0,
+			[dblCreditUnit]					=	0,
+			[strDescription]				=	'Posted Voting Stock Issue',
+			[strCode]						=	@MODULE_CODE,
+			[strReference]					=	A.strCertificateNo,
+			[intCurrencyId]					=	0,
+			[dblExchangeRate]				=	1,
+			[dtmDateEntered]				=	GETDATE(),
+			[dtmTransactionDate]			=	NULL,
+			[strJournalLineDescription]		=	'Posted Voting Stock Issue',
+			[intJournalLineNo]				=	1,
+			[ysnIsUnposted]					=	0,
+			[intUserId]						=	@intUserId,
+			[intEntityId]					=	@intUserId,
+			[strTransactionId]				=	'PIS-' + CONVERT(NVARCHAR(50),A.intCustomerStockId), 
+			[intTransactionId]				=	A.intCustomerStockId, 
+			[strTransactionType]			=	'Voting Stock',
+			[strTransactionForm]			=	@SCREEN_NAME,
+			[strModuleName]					=	@MODULE_NAME,
+			[dblDebitForeign]				=	0,      
+			[dblDebitReport]				=	0,
+			[dblCreditForeign]				=	0,
+			[dblCreditReport]				=	0,
+			[dblReportingRate]				=	0,
+			[dblForeignRate]				=	0,
+			[intConcurrencyId]				=	1
+		FROM	[dbo].[tblPATCustomerStock] A
+				CROSS JOIN tblPATCompanyPreference ComPref
+		WHERE	A.intCustomerStockId IN (SELECT intTransactionId FROM @tmpTransacions)
+		UNION ALL
+		--TREASURY GL
+		SELECT	
+			[dtmDate]						=	DATEADD(dd, DATEDIFF(dd, 0, A.dtmIssueDate), 0),
+			[strBatchID]					=	'',
+			[intAccountId]					=	B.intTreasuryGLAccount, 
+			[dblDebit]						=	A.dblFaceValue,
+			[dblCredit]						=	0,
+			[dblDebitUnit]					=	0,
+			[dblCreditUnit]					=	0,
+			[strDescription]				=	'Posted Treasury GL from Voting Stock',
+			[strCode]						=	@MODULE_CODE,
+			[strReference]					=	A.strCertificateNo,
+			[intCurrencyId]					=	0,
+			[dblExchangeRate]				=	1,
+			[dtmDateEntered]				=	GETDATE(),
+			[dtmTransactionDate]			=	NULL,
+			[strJournalLineDescription]		=	'Posted Treasury GL from Voting Stock',
+			[intJournalLineNo]				=	1,
+			[ysnIsUnposted]					=	0,
+			[intUserId]						=	@intUserId,
+			[intEntityId]					=	@intUserId,
+			[strTransactionId]				=	'PIS-' + CONVERT(NVARCHAR(50),A.intCustomerStockId), 
+			[intTransactionId]				=	A.intCustomerStockId, 
+			[strTransactionType]			=	'Voting Stock',
+			[strTransactionForm]			=	@SCREEN_NAME,
+			[strModuleName]					=	@MODULE_NAME,
+			[dblDebitForeign]				=	0,      
+			[dblDebitReport]				=	0,
+			[dblCreditForeign]				=	0,
+			[dblCreditReport]				=	0,
+			[dblReportingRate]				=	0,
+			[dblForeignRate]				=	0,
+			[intConcurrencyId]				=	1
+		FROM	[dbo].[tblPATCustomerStock] A
+				INNER JOIN tblPATStockClassification B
+					ON B.intStockId = A.intStockId
+		WHERE	A.intCustomerStockId IN (SELECT intTransactionId FROM @tmpTransacions)
+	END
+	ELSE --POST NON-VOTING/OTHER STOCKS
+	BEGIN
+		INSERT INTO @returntable
+		--NON-VOTING/OTHER STOCK ISSUED
+		SELECT	
+			[dtmDate]						=	DATEADD(dd, DATEDIFF(dd, 0, A.dtmIssueDate), 0),
+			[strBatchID]					=	'',
+			[intAccountId]					=	ComPref.intNonVotingStockId,
+			[dblDebit]						=	0,
+			[dblCredit]						=	A.dblFaceValue,
+			[dblDebitUnit]					=	0,
+			[dblCreditUnit]					=	0,
+			[strDescription]				=	'Posted Non-Voting Stock Issue',
+			[strCode]						=	@MODULE_CODE,
+			[strReference]					=	A.strCertificateNo,
+			[intCurrencyId]					=	0,
+			[dblExchangeRate]				=	1,
+			[dtmDateEntered]				=	GETDATE(),
+			[dtmTransactionDate]			=	NULL,
+			[strJournalLineDescription]		=	'Posted Non-Voting Stock Issue',
+			[intJournalLineNo]				=	1,
+			[ysnIsUnposted]					=	0,
+			[intUserId]						=	@intUserId,
+			[intEntityId]					=	@intUserId,
+			[strTransactionId]				=	'PIS-' + CONVERT(NVARCHAR(50),A.intCustomerStockId), 
+			[intTransactionId]				=	A.intCustomerStockId, 
+			[strTransactionType]			=	'Non-Voting/Other Stock',
+			[strTransactionForm]			=	@SCREEN_NAME,
+			[strModuleName]					=	@MODULE_NAME,
+			[dblDebitForeign]				=	0,      
+			[dblDebitReport]				=	0,
+			[dblCreditForeign]				=	0,
+			[dblCreditReport]				=	0,
+			[dblReportingRate]				=	0,
+			[dblForeignRate]				=	0,
+			[intConcurrencyId]				=	1
+		FROM	[dbo].[tblPATCustomerStock] A
+				CROSS JOIN tblPATCompanyPreference ComPref
+		WHERE	A.intCustomerStockId IN (SELECT intTransactionId FROM @tmpTransacions)
+		UNION ALL
+		--TREASURY GL
+		SELECT	
+			[dtmDate]						=	DATEADD(dd, DATEDIFF(dd, 0, A.dtmIssueDate), 0),
+			[strBatchID]					=	'',
+			[intAccountId]					=	B.intDividendsGLAccount, 
+			[dblDebit]						=	A.dblFaceValue,
+			[dblCredit]						=	0,
+			[dblDebitUnit]					=	0,
+			[dblCreditUnit]					=	0,
+			[strDescription]				=	'Posted Treasury GL from Non-Voting Stock',
+			[strCode]						=	@MODULE_CODE,
+			[strReference]					=	A.strCertificateNo,
+			[intCurrencyId]					=	0,
+			[dblExchangeRate]				=	1,
+			[dtmDateEntered]				=	GETDATE(),
+			[dtmTransactionDate]			=	NULL,
+			[strJournalLineDescription]		=	'Posted Treasury GL from Non-Voting Stock',
+			[intJournalLineNo]				=	1,
+			[ysnIsUnposted]					=	0,
+			[intUserId]						=	@intUserId,
+			[intEntityId]					=	@intUserId,
+			[strTransactionId]				=	'PIS-' + CONVERT(NVARCHAR(50),A.intCustomerStockId), 
+			[intTransactionId]				=	A.intCustomerStockId, 
+			[strTransactionType]			=	'Non-Voting/Other Stock',
+			[strTransactionForm]			=	@SCREEN_NAME,
+			[strModuleName]					=	@MODULE_NAME,
+			[dblDebitForeign]				=	0,      
+			[dblDebitReport]				=	0,
+			[dblCreditForeign]				=	0,
+			[dblCreditReport]				=	0,
+			[dblReportingRate]				=	0,
+			[dblForeignRate]				=	0,
+			[intConcurrencyId]				=	1
+		FROM	[dbo].[tblPATCustomerStock] A
+				INNER JOIN tblPATStockClassification B
+					ON B.intStockId = A.intStockId
+		WHERE	A.intCustomerStockId IN (SELECT intTransactionId FROM @tmpTransacions)
+	END
+	RETURN
+END
+
+GO
---
+++ i21Database/dbo/Stored Procedures/uspPATPostIssueStock.sql
@@ -0,0 +1,106 @@
+﻿CREATE PROCEDURE [dbo].[uspPATPostIssueStock]
+	@intCustomerStockId INT = NULL,
+	@ysnPosted BIT = NULL,
+	@ysnVoting BIT = NULL,
+	@ysnRetired BIT = NULL,
+	@intUserId INT = NULL,
+	@successfulCount INT = 0 OUTPUT,
+	@invalidCount INT = 0 OUTPUT,
+	@success BIT = 0 OUTPUT
+AS
+BEGIN
+
+SET QUOTED_IDENTIFIER OFF
+SET ANSI_NULLS ON
+SET NOCOUNT ON
+SET XACT_ABORT ON
+SET ANSI_WARNINGS OFF
+
+BEGIN TRANSACTION -- START TRANSACTION
+
+DECLARE @dateToday AS DATETIME = GETDATE();
+DECLARE @GLEntries AS RecapTableType;
+DECLARE @totalRecords INT;
+DECLARE @error NVARCHAR(200);
+
+IF (@ysnRetired = 1)
+BEGIN
+	IF(@ysnPosted = 1)
+	BEGIN
+
+	------------------------CREATE GL ENTRIES---------------------
+		INSERT INTO @GLEntries
+		SELECT * FROM [dbo].[fnPATCreateRetireStockGLEntries](@intCustomerStockId, 0, @intUserId)
+
+	END
+	ELSE
+	BEGIN
+	------------------------REVERSE GL ENTRIES---------------------
+		INSERT INTO @GLEntries
+		SELECT * FROM [dbo].[fnPATReverseRetireStockGLEntries](@intCustomerStockId, @dateToday, @intUserId)
+
+		UPDATE tblGLDetail SET ysnIsUnposted = 1
+		WHERE intTransactionId = @intCustomerStockId 
+			AND strModuleName = N'Patronage' AND strTransactionForm = N'Retire Stock'
+	END
+END
+ELSE
+BEGIN
+	IF(@ysnVoting = 1)
+	BEGIN
+		IF(@ysnPosted = 1)
+		BEGIN
+
+		------------------------CREATE GL ENTRIES---------------------
+			INSERT INTO @GLEntries
+			SELECT * FROM [dbo].[fnPATCreateIssueStockGLEntries](@intCustomerStockId, @ysnVoting, @intUserId)
+
+		END
+		ELSE
+		BEGIN
+
+		------------------------REVERSE GL ENTRIES---------------------
+			INSERT INTO @GLEntries
+			SELECT * FROM [dbo].[fnPATReverseIssueStockGLEntries](@intCustomerStockId, @dateToday, @intUserId)
+
+			UPDATE tblGLDetail SET ysnIsUnposted = 1
+			WHERE intTransactionId = @intCustomerStockId 
+				AND strModuleName = N'Patronage' AND strTransactionForm = N'Issue Stock'
+		END
+	END
+END
+BEGIN TRY
+	SELECT * FROM @GLEntries
+	EXEC uspGLBookEntries @GLEntries, @ysnPosted
+END TRY
+BEGIN CATCH
+	SET @error = ERROR_MESSAGE()
+	RAISERROR(@error, 16, 1);
+	GOTO Post_Rollback
+END CATCH
+
+
+------------UPDATE CUSTOMER STOCK TABLE---------------
+
+	UPDATE tblPATCustomerStock SET ysnPosted = @ysnPosted WHERE intCustomerStockId = @intCustomerStockId
+
+---------------------------------------------------------------------------------------------------------------------------------------
+IF @@ERROR <> 0	GOTO Post_Rollback;
+
+GOTO Post_Commit;
+
+Post_Commit:
+	COMMIT TRANSACTION
+	SET @success = 1
+	SET @successfulCount = @totalRecords
+	
+	GOTO Post_Exit
+
+Post_Rollback:
+	ROLLBACK TRANSACTION	
+	SET @success = 0
+	GOTO Post_Exit
+
+Post_Exit:
+
+END
