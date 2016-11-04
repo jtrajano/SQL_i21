b55705df4f5753d40fffadc9b890ffecb81e9600
Wendell Wayne Estrada <wendell.estrada@irely.com>
2016-11-04 13:58:18 +0800

    IC-2967 CLONE - Error updating into 16.3.1025.266 (Movers and Shuckers TE)

---
+++ i21PreDeployment/Scripts/Pre-Deployment/11_IC/Remove_Unused_Shipment_Charges.sql
@@ -0,0 +1,9 @@
+﻿delete    sc 
+from    tblICInventoryShipmentCharge sc left join tblICInventoryShipment s
+            on sc.intInventoryShipmentId = s.intInventoryShipmentId
+where    s.intInventoryShipmentId is null 
+
+delete    rci 
+from    tblICInventoryReceiptChargePerItem rci left join tblICInventoryReceiptCharge rc
+            on rci.intInventoryReceiptChargeId = rc.intInventoryReceiptChargeId
+where    rc.intInventoryReceiptId is null 
