52277afa385aa386c8f65a65ae1b3a52191e1759
Gopinath Nayak <gopinath.n@irely.com>
2016-11-07 22:07:53 +0530

    GRN-445

---
+++ i21Database/dbo/Views/vyuGRDiscountScheduleNotMapped.sql
@@ -0,0 +1,11 @@
+﻿CREATE VIEW [dbo].[vyuGRDiscountScheduleNotMapped]
+AS
+SELECT
+ S.intDiscountScheduleId
+,S.intCurrencyId
+,ST.strCurrency
+,S.intCommodityId
+,Com.strCommodityCode	
+FROM tblGRDiscountSchedule S
+JOIN tblSMCurrency ST ON ST.intCurrencyID = S.intCurrencyId
+JOIN tblICCommodity Com ON Com.intCommodityId=S.intCommodityId
