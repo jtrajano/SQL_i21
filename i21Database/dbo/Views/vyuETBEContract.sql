d65e6c3a72cbd663e3cb80ce127c755358ab123c
kenneth lagunero <kenneth.lagunero@irely.com>
2016-11-07 17:30:10 +0800

    IET-90, IET-91

--- i21Database/dbo/Views/vyuETBEContract.sql
+++ i21Database/dbo/Views/vyuETBEContract.sql
@@ -19,7 +19,6 @@ AS
 		AND DATEADD(dd, DATEDIFF(dd, 0, B.dtmEndDate), 0) >= DATEADD(dd, DATEDIFF(dd, 0, GETDATE()), 0)
 		AND B.dblBalance > 0
 		AND B.intContractDetailId IN (SELECT MIN(intContractDetailId) FROM tblCTContractDetail WHERE intContractHeaderId = A.intContractHeaderId AND intItemId = E.intItemId)
-		AND B.intContractHeaderId IN (SELECT MIN(intContractHeaderId) FROM tblCTContractHeader WHERE intEntityId = A.intEntityId)
 		AND (E.ysnAvailableTM = 1 OR E.strType = 'Service')
 
 GO
