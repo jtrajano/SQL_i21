f72c35ca13aeaee9b37366f98270c75cbc6a25a6
Wendell Wayne Estrada <wendell.estrada@irely.com>
2016-11-08 11:36:06 +0800

    IC-2974 Failed deployment of 16.3.1107.273

--- i21PreDeployment/Scripts/Pre-Deployment/11_IC/Remove_Unused_Shipment_Charges.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/11_IC/Remove_Unused_Shipment_Charges.sql
@@ -1,9 +1,19 @@
-﻿delete    sc 
-from    tblICInventoryShipmentCharge sc left join tblICInventoryShipment s
-            on sc.intInventoryShipmentId = s.intInventoryShipmentId
-where    s.intInventoryShipmentId is null 
+﻿-- Remove duplicate feed stock uom
+PRINT N'Deleting inventory shipment charges without references to shipment...'
 
-delete    rci 
-from    tblICInventoryReceiptChargePerItem rci left join tblICInventoryReceiptCharge rc
-            on rci.intInventoryReceiptChargeId = rc.intInventoryReceiptChargeId
-where    rc.intInventoryReceiptId is null 
+IF EXISTS (SELECT * FROM sys.tables WHERE object_id = object_id('tblICInventoryShipmentCharge'))
+BEGIN
+	IF EXISTS (SELECT * FROM sys.tables WHERE object_id = object_id('tblICInventoryShipment'))
+	BEGIN
+		EXEC ('DELETE sc FROM tblICInventoryShipmentCharge sc LEFT JOIN tblICInventoryShipment s ON sc.intInventoryShipmentId = s.intInventoryShipmentId WHERE s.intInventoryShipmentId IS NULL');
+	END
+END
+
+
+IF EXISTS (SELECT * FROM sys.tables WHERE object_id = object_id('tblICInventoryReceiptChargePerItem'))
+BEGIN
+	IF EXISTS (SELECT * FROM sys.tables WHERE object_id = object_id('tblICInventoryReceiptCharge'))
+	BEGIN
+		EXEC ('DELETE rci FROM tblICInventoryReceiptChargePerItem rci LEFT JOIN tblICInventoryReceiptCharge rc ON rci.intInventoryReceiptChargeId = rc.intInventoryReceiptChargeId WHERE rc.intInventoryReceiptId IS NULL');
+	END
+END
