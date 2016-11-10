1bb56aabd305cee82044390d8033218e7068ac7d
jeffrey trajano <jeffrey.trajano@irely.com>
2016-11-10 14:24:23 +0800

    GL-3009 Create test cases for Journal Creation

---
+++ i21Database/dbo/Stored Procedures/uspGLGetTestAccounts.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[uspGLGetTestAccounts]
+AS
+declare @baseCurrency int
+select top 1 @baseCurrency = intDefaultCurrencyId from tblSMCompanyPreference 
+
+;with mul as
+(
+select top 2 a.intAccountId, count(1)cnt from vyuGLExchangeRate a join vyuGLAccountDetail b on a.intAccountId = b.intAccountId
+ group by a.intAccountId, intFromCurrencyId,intToCurrencyId,strAccountCategory 
+ having intToCurrencyId =@baseCurrency and strAccountCategory = 'General'  order by cnt
+)
+select top 2 strAccountId, a.strCurrency from vyuGLAccountDetail a join mul b 
+on a.intAccountId = b.intAccountId 
+join tblSMCurrency c on a.intCurrencyID  = c.intCurrencyID
+
+
+select top 2 strAccountId from vyuGLAccountDetail where intCurrencyID = @baseCurrency and strAccountCategory = 'General'
