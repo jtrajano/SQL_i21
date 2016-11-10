097c0ecd7c816bc13ea0218a6b814ce3502c62e7
Mon Gonzales <mon.gonzales@irely.com>
2016-11-10 11:57:06 +0800

    EM-1539

--- i21Database/dbo/Tables/tblAPVendorLien.sql
+++ i21Database/dbo/Tables/tblAPVendorLien.sql
@@ -6,8 +6,10 @@
 	[ysnActive] BIT NOT NULL DEFAULT(0),
 	[dtmStartDate] DATETIME NULL,
 	[dtmEndDate] DATETIME NULL,	
+	[intCommodityId] INT NULL,
 	[intConcurrencyId] INT DEFAULT ((0)) NOT NULL,	
 	CONSTRAINT [PK_tblAPVendorLien] PRIMARY KEY CLUSTERED ([intEntityVendorLienId] ASC),
 	CONSTRAINT [FK_tblAPVendorLien_tblAPVendor_intEntityVendorId] FOREIGN KEY ([intEntityVendorId]) REFERENCES [dbo].[tblAPVendor] ([intEntityVendorId]) ON DELETE CASCADE,	
-	CONSTRAINT [FK_tblAPVendorLien_tblEMEntity_intEntityLienId] FOREIGN KEY ([intEntityVendorId]) REFERENCES [dbo].[tblEMEntity] ([intEntityId])
+	CONSTRAINT [FK_tblAPVendorLien_tblEMEntity_intEntityLienId] FOREIGN KEY ([intEntityVendorId]) REFERENCES [dbo].[tblEMEntity] ([intEntityId]),	
+	CONSTRAINT [FK_tblAPVendorLien_tblICCommodity_intCommodityId] FOREIGN KEY([intCommodityId])REFERENCES [tblICCommodity] ([intCommodityId]),
 )
