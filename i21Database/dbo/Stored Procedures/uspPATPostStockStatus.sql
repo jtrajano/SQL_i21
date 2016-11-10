cd58406f75e041a52ddcad750b2d52dc996e5d5b
Jb Mago <benedict.mago@irely.com>
2016-11-10 18:50:10 +0800

    PAT-61

--- i21Database/dbo/Stored Procedures/uspPATPostStockStatus.sql
+++ i21Database/dbo/Stored Procedures/uspPATPostStockStatus.sql
@@ -3,59 +3,29 @@
 	@ysnPosted BIT = NULL
 AS
 BEGIN
-		SELECT CS.intUpdateId,
-			   CSD.intCustomerId,
-			   CSD.strCurrentStatus
-		  INTO #tempStatus
-		  FROM tblPATChangeStatus CS
-	INNER JOIN tblPATChangeStatusDetail CSD
-			ON CSD.intUpdateId = CS.intUpdateId
-		 WHERE CS.intUpdateId = @intUpdateId
+		IF(ISNULL(@ysnPosted,1) = 1)
+		BEGIN
+			UPDATE ARC SET ARC.strStockStatus = CSD.strNewStatus
+				FROM tblARCustomer ARC 
+				INNER JOIN tblPATChangeStatusDetail CSD 
+					ON CSD.intCustomerId = ARC.intEntityCustomerId 
+				WHERE CSD.intChangeStatusId = @intUpdateId
+				
+			UPDATE tblPATChangeStatus
+				SET ysnPosted = ISNULL(@ysnPosted,1)
+				WHERE intChangeStatusId = @intUpdateId
+		END
+		ELSE
+		BEGIN
+			UPDATE ARC SET ARC.strStockStatus = CSD.strCurrentStatus
+				FROM tblARCustomer ARC 
+				INNER JOIN tblPATChangeStatusDetail CSD 
+					ON CSD.intCustomerId = ARC.intEntityCustomerId 
+				WHERE CSD.intChangeStatusId = @intUpdateId
 
-
-			IF(@ysnPosted = 1)
-			BEGIN
-				UPDATE tblARCustomer
-				   SET strStockStatus = 'Voting'
-				 WHERE intEntityCustomerId IN (SELECT intCustomerId FROM #tempStatus)
-
-				UPDATE tblPATCustomerStock
-				   SET strStockStatus = 'Voting'
-				 WHERE intCustomerPatronId IN (SELECT intCustomerId FROM #tempStatus)
-
-				UPDATE tblPATChangeStatus
-				   SET ysnPosted = 1
-				 WHERE intUpdateId = @intUpdateId
-			END
-			ELSE
-			BEGIN
-				DECLARE @intCustomerId INT,
-						@strCurrentStatus NVARCHAR(MAX)
-
-				DECLARE stockCursor CURSOR FOR 	
-				SELECT DISTINCT intCustomerId, strCurrentStatus FROM #tempStatus
-				OPEN stockCursor
-				FETCH NEXT FROM stockCursor into @intCustomerId, @strCurrentStatus
-				WHILE (@@FETCH_STATUS <> -1)
-				BEGIN
-					UPDATE tblARCustomer
-					   SET strStockStatus = @strCurrentStatus
-					 WHERE intEntityCustomerId = @intCustomerId
-
-					UPDATE tblPATCustomerStock
-					   SET strStockStatus = @strCurrentStatus
-					 WHERE intCustomerPatronId = @intCustomerId
-
-					FETCH NEXT FROM stockCursor into @intCustomerId, @strCurrentStatus
-				END
-				CLOSE stockCursor
-				DEALLOCATE stockCursor
-
-				UPDATE tblPATChangeStatus
-				   SET ysnPosted = 0
-				 WHERE intUpdateId = @intUpdateId
-			END
-
-			DROP TABLE #tempStatus
+			UPDATE tblPATChangeStatus
+				SET ysnPosted = ISNULL(@ysnPosted,0)
+				WHERE intChangeStatusId = @intUpdateId
+		END
 END
 GO
