facfaa286499e71635fc14d75c7f04b00928e850
Jb Mago <benedict.mago@irely.com>
2016-11-14 10:12:07 +0800

    PAT-481

--- i21Database/dbo/Stored Procedures/uspPATProcessRefundsToVoucher.sql
+++ i21Database/dbo/Stored Procedures/uspPATProcessRefundsToVoucher.sql
@@ -30,7 +30,7 @@ BEGIN TRY
 		FETCH NEXT FROM the_cursor INTO @intRefundCustomerId,@intCustomerId,@dblCheckAmount,@dblServiceFee
 	END
 	
-	UPDATE tblPATRefund SET ysnVoucherProcessed = 1 WHERE intRefundID = @intRefundId
+	UPDATE tblPATRefund SET ysnVoucherProcessed = 1 WHERE intRefundId = @intRefundId
 
 	CLOSE the_cursor
 	DEALLOCATE the_cursor
