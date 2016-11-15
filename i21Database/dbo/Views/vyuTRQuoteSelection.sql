01a15f9c430bb7308518c67b0e901b4c11ac71f4
Lawrence Zabala <lawrence.zabala@irely.com>
2016-11-15 10:46:44 +0800

    TR-618 TR-619 TR-620

--- i21Database/dbo/Views/vyuTRQuoteSelection.sql
+++ i21Database/dbo/Views/vyuTRQuoteSelection.sql
@@ -1,16 +1,41 @@
 ﻿CREATE VIEW [dbo].[vyuTRQuoteSelection]
 	AS 
-select QT.intCustomerRackQuoteHeaderId,QT.intEntityCustomerLocationId,QT.intSupplyPointId,QT.intItemId,QT.intCategoryId,QT.intEntityCustomerId,QT.ysnQuote from
-             (select QH.intCustomerRackQuoteHeaderId,QV.intEntityCustomerLocationId, QV.intSupplyPointId,QI.intItemId,IC.intCategoryId,QH.intEntityCustomerId,QV.ysnQuote
-                     from tblARCustomerRackQuoteHeader QH            
-                         left join tblARCustomerRackQuoteItem QI on QI.intCustomerRackQuoteHeaderId = QH.intCustomerRackQuoteHeaderId
-                         left join tblARCustomerRackQuoteVendor QV on QV.intCustomerRackQuoteHeaderId = QH.intCustomerRackQuoteHeaderId
-             			 join vyuICGetItemStock IC on IC.intItemId = QI.intItemId
-             UNION ALL
-             select  QH.intCustomerRackQuoteHeaderId,QV.intEntityCustomerLocationId,QV.intSupplyPointId,IC.intItemId,QC.intCategoryId,QH.intEntityCustomerId,QV.ysnQuote
-                     from tblARCustomerRackQuoteHeader QH
-                         left join tblARCustomerRackQuoteCategory QC on QH.intCustomerRackQuoteHeaderId = QC.intCustomerRackQuoteHeaderId
-             			 left join tblARCustomerRackQuoteVendor QV on QV.intCustomerRackQuoteHeaderId = QH.intCustomerRackQuoteHeaderId
-                         join vyuICGetItemStock IC on IC.intCategoryId = QC.intCategoryId) QT
-group by QT.intCustomerRackQuoteHeaderId,QT.intEntityCustomerLocationId,QT.intSupplyPointId,QT.intItemId,QT.intCategoryId,QT.intEntityCustomerId,QT.ysnQuote
 
+SELECT QT.intCustomerRackQuoteHeaderId
+	, QT.intEntityCustomerLocationId
+	, QT.intSupplyPointId
+	, QT.intItemId
+	, QT.intCategoryId
+	, QT.intEntityCustomerId
+	, QT.ysnQuote
+FROM (SELECT QH.intCustomerRackQuoteHeaderId
+			, QV.intEntityCustomerLocationId
+			, QV.intSupplyPointId
+			, QI.intItemId
+			, IC.intCategoryId
+			, QH.intEntityCustomerId
+			, QV.ysnQuote
+		FROM tblARCustomerRackQuoteHeader QH
+		LEFT JOIN tblARCustomerRackQuoteItem QI ON QI.intCustomerRackQuoteHeaderId = QH.intCustomerRackQuoteHeaderId
+		LEFT JOIN tblARCustomerRackQuoteVendor QV ON QV.intCustomerRackQuoteHeaderId = QH.intCustomerRackQuoteHeaderId
+		JOIN vyuICGetItemStock IC ON IC.intItemId = QI.intItemId
+		
+		UNION ALL
+		SELECT QH.intCustomerRackQuoteHeaderId
+			, QV.intEntityCustomerLocationId
+			, QV.intSupplyPointId
+			, IC.intItemId
+			, QC.intCategoryId
+			, QH.intEntityCustomerId
+			, QV.ysnQuote
+		FROM tblARCustomerRackQuoteHeader QH
+		LEFT JOIN tblARCustomerRackQuoteCategory QC ON QH.intCustomerRackQuoteHeaderId = QC.intCustomerRackQuoteHeaderId
+		LEFT JOIN tblARCustomerRackQuoteVendor QV ON QV.intCustomerRackQuoteHeaderId = QH.intCustomerRackQuoteHeaderId
+		JOIN vyuICGetItemStock IC ON IC.intCategoryId = QC.intCategoryId) QT
+GROUP BY QT.intCustomerRackQuoteHeaderId
+	, QT.intEntityCustomerLocationId
+	, QT.intSupplyPointId
+	, QT.intItemId
+	, QT.intCategoryId
+	, QT.intEntityCustomerId
+	, QT.ysnQuote
