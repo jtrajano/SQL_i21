0f5befa35ff86fa384fd1235ef89e2350f9316e7
Mon Gonzales <mon.gonzales@irely.com>
2016-11-15 14:15:41 +0800

    EM-1553

--- i21Database/Scripts/Post-Deployment/EM/034_EntityClassData.sql
+++ i21Database/Scripts/Post-Deployment/EM/034_EntityClassData.sql
@@ -16,11 +16,11 @@ END
 IF(NOT EXISTS(SELECT TOP 1 1 from tblEMEntityClass where intEntityClassId = 2))
 BEGIN
 	INSERT INTO tblEMEntityClass(intEntityClassId, strClass, strModule, ysnActive)
-	SELECT 2, 'Cendor base', 'AP', 1
+	SELECT 2, 'Vendor base', 'AP', 1
 END
 ELSE
 BEGIN
-	UPDATE tblEMEntityClass SET strClass = 'Cendor base', strModule = 'AP',ysnActive = 1  where intEntityClassId = 2
+	UPDATE tblEMEntityClass SET strClass = 'Vendor base', strModule = 'AP',ysnActive = 1  where intEntityClassId = 2
 END
 
 PRINT '*** ----  End checking entity class information  ---- ***'
