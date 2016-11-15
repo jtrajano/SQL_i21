951acfd4a6914918590520164088abb300bcebbf
kenneth lagunero <kenneth.lagunero@irely.com>
2016-11-16 01:30:00 +0800

    TM-2119

--- i21Database/dbo/Views/vyuTMCustomerContractSubReport.sql
+++ i21Database/dbo/Views/vyuTMCustomerContractSubReport.sql
@@ -5,6 +5,7 @@ AS
 		,strContractNumber = A.strContractNumber
 		,dblUnitBalance = ISNULL(B.dblBalance,0.0)
 		,dblUnitPrice =  ISNULL(B.dblCashPrice,0.0)
+		,strItemNo = E.strItemNo
 	FROM tblCTContractHeader A
 	INNER JOIN tblCTContractDetail B
 		ON A.intContractHeaderId = B.intContractHeaderId
@@ -12,5 +13,8 @@ AS
 		ON A.intEntityId = D.intEntityId
 	INNER JOIN tblTMCustomer C
 		ON D.intEntityId = C.intCustomerNumber
+	LEFT JOIN tblICItem E
+		ON B.intItemId = E.intItemId
 	WHERE DATEADD(dd, DATEDIFF(dd, 0, B.dtmEndDate), 0) >= DATEADD(dd, DATEDIFF(dd, 0, GETDATE()), 0)
+		AND ISNULL(B.dblBalance,0.0) > 0
 GO
