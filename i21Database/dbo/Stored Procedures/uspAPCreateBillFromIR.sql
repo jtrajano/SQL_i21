43b3f26f39bc121f0a5e32b143bfc9a9589772b6
Lex Eugene Peregrino <lex.peregrino@irely.com>
2016-11-17 18:33:10 +0800

    AP-2936 - Unable to post voucher due to dblTotal not matched in header and detail

--- i21Database/dbo/Stored Procedures/uspAPCreateBillFromIR.sql
+++ i21Database/dbo/Stored Procedures/uspAPCreateBillFromIR.sql
@@ -21,6 +21,10 @@ DECLARE @receiptLocation INT;
 DECLARE @userLocation INT;
 DECLARE @location INT;
 DECLARE @cashPrice DECIMAL;
+DECLARE @receiptAmount DECIMAL(18,6);
+DECLARE @totalReceiptAmount DECIMAL(18,6);
+DECLARE @totalLineItem DECIMAL(18,6);
+DECLARE @totalCharges DECIMAL(18,6);
 
 CREATE TABLE #tmpReceiptIds (
 	[intInventoryReceiptId] [INT] PRIMARY KEY,
@@ -95,6 +99,18 @@ BEGIN
 		--GOTO DONE
 	END
 		
+	----GET THE TOTAL IR AMOUNT
+	SELECT @receiptAmount = SUM(A.dblLineTotal) FROM tblICInventoryReceiptItem A WHERE A.intInventoryReceiptId = @receiptId;
+	
+	SELECT @totalCharges = (SUM(dblUnitCost) + ISNULL(SUM(dblTax),0.00))
+	FROM vyuAPChargesForBilling WHERE intInventoryReceiptId = @receiptId
+	
+	SELECT @totalLineItem =  SUM(A.dblLineTotal)
+	FROM tblICInventoryReceiptItem A 
+	WHERE A.dblUnitCost > 0 AND A.intInventoryReceiptId = @receiptId
+	
+	SET @totalReceiptAmount = @totalLineItem + @totalCharges;
+
 	SET @cashPrice = (SELECT SUM(E1.dblCashPrice) FROM tblICInventoryReceipt A
 		INNER JOIN tblICInventoryReceiptItem B ON A.intInventoryReceiptId = B.intInventoryReceiptId
 		INNER JOIN tblICItem C ON B.intItemId = C.intItemId
@@ -153,8 +169,8 @@ BEGIN
 		[intAccountId] 			=	@APAccount,
 		[strBillId]				=	@generatedBillRecordId,
 		[strReference] 			=	A.strBillOfLading,
-		[dblTotal] 				=	A.dblInvoiceAmount,
-		[dblAmountDue]			=	A.dblInvoiceAmount,
+		[dblTotal] 				=	@totalReceiptAmount,
+		[dblAmountDue]			=	@totalReceiptAmount,
 		[intEntityId]			=	@userId,
 		[ysnPosted]				=	0,
 		[ysnPaid]				=	0,
