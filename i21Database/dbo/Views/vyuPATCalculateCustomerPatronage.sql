36bdf2fe246b1597d210274034fd11211275f923
Jb Mago <benedict.mago@irely.com>
2016-11-18 17:34:52 +0800

    PAT-462

--- i21Database/dbo/Views/vyuPATCalculateCustomerPatronage.sql
+++ i21Database/dbo/Views/vyuPATCalculateCustomerPatronage.sql
@@ -8,7 +8,7 @@ SELECT	RR.intRefundTypeId,
 		PC.strCategoryCode,
 		RRD.dblRate,
 		dblVolume = CV.dblVolume,
-		dblRefundAmount = ISNULL((RRD.dblRate * CV.dblVolume),0)
+		dblRefundAmount = CASE WHEN ComPref.dblMinimumRefund > ISNULL((RRD.dblRate * CV.dblVolume),0) THEN 0 ELSE (RRD.dblRate * CV.dblVolume) END
 FROM tblPATRefundRate RR
 INNER JOIN tblPATRefundRateDetail RRD
 	ON RRD.intRefundTypeId = RR.intRefundTypeId
@@ -16,3 +16,4 @@ INNER JOIN tblPATPatronageCategory PC
 	ON PC.intPatronageCategoryId = RRD.intPatronageCategoryId
 INNER JOIN tblPATCustomerVolume CV
 	ON CV.intPatronageCategoryId = RRD.intPatronageCategoryId AND CV.ysnRefundProcessed <> 1 AND CV.dblVolume <> 0
+CROSS APPLY tblPATCompanyPreference ComPref
