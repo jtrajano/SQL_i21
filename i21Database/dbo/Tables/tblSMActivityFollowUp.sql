be67bc59f4030d93b6e7fea8fab47d19d8cc6f17
jesmar mendones <jesmar.mendones@irely.com>
2016-11-22 18:49:40 +0800

    FRMC-27

---
+++ i21Database/dbo/Tables/tblSMActivityFollowUp.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[tblSMActivityFollowUp]
+(
+	[intActivityFollowUpId]	INT													NOT NULL PRIMARY KEY IDENTITY,
+	[strType]				[nvarchar](50)		COLLATE Latin1_General_CI_AS	NOT NULL,
+	[strSubject]			[nvarchar](100)		COLLATE Latin1_General_CI_AS	NOT NULL,
+	[dtmDueDate]			[datetime]											NULL, 
+	[dtmTime]				[datetime]											NULL, 
+	[intAssignedTo]			[int] NULL,
+	[strCategory]			[nvarchar](50)		COLLATE Latin1_General_CI_AS	NULL,
+	[strPriority]			[nvarchar](50)		COLLATE Latin1_General_CI_AS	NULL,
+	[strStatus]				[nvarchar](50)		COLLATE Latin1_General_CI_AS	NULL,
+	[strDetails]			[nvarchar](MAX)		COLLATE Latin1_General_CI_AS	NULL,
+	[intConcurrencyId]		[int]												NOT NULL DEFAULT ((1)),
+)
