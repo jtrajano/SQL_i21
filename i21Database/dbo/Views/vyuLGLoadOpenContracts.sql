f63137707b4f90e93b01698ca0e97b8682e5fe7f
Swetosuvro Ghosh <swetosuvro.g@irely.com>
2016-11-22 20:10:23 +0530

    LG-621

--- i21Database/dbo/Views/vyuLGLoadOpenContracts.sql
+++ i21Database/dbo/Views/vyuLGLoadOpenContracts.sql
@@ -27,12 +27,19 @@ SELECT CD.intContractDetailId
 	,CH.strCustomerContract
 	,ISNULL(CD.dblBalance, 0) AS dblBalance
 	,CASE 
-		WHEN ((CAST(CASE 
-						WHEN CD.intContractStatusId IN (1,4)
-							THEN 1
-						ELSE 0
-						END AS BIT) = 1
-			  ) AND ((ISNULL(CD.dblBalance, 0) - ISNULL(CD.dblScheduleQty, 0) > 0) OR (CH.ysnUnlimitedQuantity = 1)))
+		WHEN (
+				(
+					CAST(CASE 
+							WHEN CD.intContractStatusId IN (1,4)
+								THEN 1
+							ELSE 0
+							END AS BIT) = 1
+					)
+				AND (
+					(ISNULL(CD.dblBalance, 0) - ISNULL(CD.dblScheduleQty, 0) > 0)
+					OR (CH.ysnUnlimitedQuantity = 1)
+					)
+				)
 			THEN CAST(1 AS BIT)
 		ELSE CAST(0 AS BIT)
 		END AS ysnAllowedToShow
@@ -62,35 +69,225 @@ SELECT CD.intContractDetailId
 	,CONVERT(NVARCHAR(100), S.dtmTestingEndDate, 101) AS strTestingEndDate
 	,S.intCompanyLocationSubLocationId
 	,S.strSubLocationName
+	,intShipmentType = 1
 FROM tblCTContractHeader CH
 JOIN tblCTContractDetail CD ON CD.intContractHeaderId = CH.intContractHeaderId
 JOIN tblICItem Item ON Item.intItemId = CD.intItemId
 JOIN tblSMCompanyLocation CL ON CL.intCompanyLocationId = CD.intCompanyLocationId
 JOIN vyuCTEntity EY ON EY.intEntityId = CH.intEntityId
-	AND EY.strEntityType = (CASE WHEN CH.intContractTypeId = 1 THEN 'Vendor' ELSE 'Customer'END)
+	AND EY.strEntityType = (
+		CASE 
+			WHEN CH.intContractTypeId = 1
+				THEN 'Vendor'
+			ELSE 'Customer'
+			END
+		)
 LEFT JOIN tblICItemUOM IU ON IU.intItemUOMId = CD.intItemUOMId
 LEFT JOIN tblICUnitMeasure U1 ON U1.intUnitMeasureId = IU.intUnitMeasureId
 LEFT JOIN tblSMCity LoadingPort ON LoadingPort.intCityId = CD.intLoadingPortId
 LEFT JOIN tblSMCity DestPort ON DestPort.intCityId = CD.intDestinationPortId
 LEFT JOIN tblSMCity DestCity ON DestCity.intCityId = CD.intDestinationCityId
-LEFT JOIN (SELECT * FROM (
-			SELECT ROW_NUMBER() OVER (
-					PARTITION BY S.intContractDetailId ORDER BY S.intSampleId DESC
-					) intRowNum
-				,S.intContractDetailId
-				,S.strSampleNumber
-				,S.strContainerNumber
-				,ST.strSampleTypeName
-				,SS.strStatus AS strSampleStatus
-				,S.dtmTestingStartDate
-				,S.dtmTestingEndDate
-				,S.intCompanyLocationSubLocationId
-				,CLSL.strSubLocationName
-			FROM tblQMSample S
-			JOIN tblQMSampleType AS ST ON ST.intSampleTypeId = S.intSampleTypeId
-			JOIN tblQMSampleStatus AS SS ON SS.intSampleStatusId = S.intSampleStatusId
-			LEFT JOIN tblSMCompanyLocationSubLocation CLSL ON CLSL.intCompanyLocationSubLocationId = S.intCompanyLocationSubLocationId
-			WHERE S.intContractDetailId IS NOT NULL
-			) t
-		WHERE intRowNum = 1
-		) S ON S.intContractDetailId = CD.intContractDetailId
+LEFT JOIN (
+	SELECT *
+	FROM (
+		SELECT ROW_NUMBER() OVER (
+				PARTITION BY S.intContractDetailId ORDER BY S.intSampleId DESC
+				) intRowNum
+			,S.intContractDetailId
+			,S.strSampleNumber
+			,S.strContainerNumber
+			,ST.strSampleTypeName
+			,SS.strStatus AS strSampleStatus
+			,S.dtmTestingStartDate
+			,S.dtmTestingEndDate
+			,S.intCompanyLocationSubLocationId
+			,CLSL.strSubLocationName
+		FROM tblQMSample S
+		JOIN tblQMSampleType AS ST ON ST.intSampleTypeId = S.intSampleTypeId
+		JOIN tblQMSampleStatus AS SS ON SS.intSampleStatusId = S.intSampleStatusId
+		LEFT JOIN tblSMCompanyLocationSubLocation CLSL ON CLSL.intCompanyLocationSubLocationId = S.intCompanyLocationSubLocationId
+		WHERE S.intContractDetailId IS NOT NULL
+		) t
+	WHERE intRowNum = 1
+	) S ON S.intContractDetailId = CD.intContractDetailId
+
+UNION
+
+SELECT CD.intContractDetailId
+	,CD.intContractHeaderId
+	,CD.intContractSeq
+	,CD.intItemId
+	,Item.strDescription strItemDescription
+	,Item.strItemNo
+	,CD.dblQuantity AS dblDetailQuantity
+	,CD.intUnitMeasureId
+	,CD.intItemUOMId
+	,U1.strUnitMeasure AS strUnitMeasure
+	,CD.intCompanyLocationId
+	,CL.strLocationName AS strLocationName
+	,ISNULL(CD.dblQuantity, 0) - ISNULL((
+			SELECT SUM(LD.dblQuantity)
+			FROM tblLGLoadDetail LD
+			JOIN tblLGLoad L ON L.intLoadId = LD.intLoadId
+			WHERE LD.intPContractDetailId = CD.intContractDetailId
+				AND L.intShipmentType = 2
+			), 0) AS dblUnLoadedQuantity
+	,CH.intContractTypeId intPurchaseSale
+	,CH.intEntityId
+	,CH.strContractNumber
+	,CH.dtmContractDate
+	,EY.strEntityName
+	,CONVERT(NVARCHAR(100), CD.dtmStartDate, 101) AS strStartDate
+	,CONVERT(NVARCHAR(100), CD.dtmEndDate, 101) AS strEndDate
+	,CD.dtmStartDate
+	,CD.dtmEndDate
+	,EY.intDefaultLocationId
+	,ISNULL((SELECT SUM(LD.dblQuantity)
+			 FROM tblLGLoadDetail LD
+			 JOIN tblLGLoad L ON L.intLoadId = LD.intLoadId
+			 WHERE CD.intContractDetailId = (CASE WHEN CH.intContractTypeId = 1 THEN LD.intPContractDetailId ELSE LD.intSContractDetailId END)
+				 AND L.intShipmentType = 2
+			 ), 0) AS dblScheduleQty
+	,CH.strCustomerContract
+	,ISNULL(CD.dblBalance, 0) AS dblBalance
+	,CASE 
+		WHEN (
+				(
+					CAST(CASE 
+							WHEN CD.intContractStatusId IN (1,4)
+								THEN 1
+							ELSE 0
+							END AS BIT) = 1
+					)
+				AND (
+					(
+						ISNULL(CD.dblQuantity, 0) - ISNULL((
+								SELECT SUM(LD.dblQuantity)
+								FROM tblLGLoadDetail LD
+								JOIN tblLGLoad L ON L.intLoadId = LD.intLoadId
+								WHERE CD.intContractDetailId = (CASE WHEN CH.intContractTypeId = 1 THEN LD.intPContractDetailId ELSE LD.intSContractDetailId END)
+									AND L.intShipmentType = 2
+								), 0) > 0
+						)
+					OR (CH.ysnUnlimitedQuantity = 1)
+					)
+				)
+			THEN CAST(1 AS BIT)
+		ELSE CAST(0 AS BIT)
+		END AS ysnAllowedToShow
+	,CH.ysnUnlimitedQuantity
+	,CH.ysnLoad
+	,CD.dblQuantityPerLoad
+	,CD.intNoOfLoad
+	,Item.strType AS strItemType
+	,CH.intPositionId
+	,CD.intLoadingPortId
+	,CD.intDestinationPortId
+	,CD.intDestinationCityId
+	,LoadingPort.strCity AS strOriginPort
+	,DestPort.strCity AS strDestinationPort
+	,DestCity.strCity AS strDestinationCity
+	,CD.strPackingDescription
+	,CD.intShippingLineId AS intShippingLineEntityId
+	,CD.intNumberOfContainers
+	,CD.intContainerTypeId
+	,CD.strVessel
+	,CH.intContractTypeId
+	,S.strSampleStatus
+	,S.strSampleNumber
+	,S.strContainerNumber
+	,S.strSampleTypeName
+	,CONVERT(NVARCHAR(100), S.dtmTestingStartDate, 101) AS strTestingStartDate
+	,CONVERT(NVARCHAR(100), S.dtmTestingEndDate, 101) AS strTestingEndDate
+	,S.intCompanyLocationSubLocationId
+	,S.strSubLocationName
+	,intShipmentType = 2
+FROM tblCTContractHeader CH
+JOIN tblCTContractDetail CD ON CD.intContractHeaderId = CH.intContractHeaderId
+JOIN tblICItem Item ON Item.intItemId = CD.intItemId
+JOIN tblSMCompanyLocation CL ON CL.intCompanyLocationId = CD.intCompanyLocationId
+JOIN vyuCTEntity EY ON EY.intEntityId = CH.intEntityId
+	AND EY.strEntityType = (
+		CASE 
+			WHEN CH.intContractTypeId = 1
+				THEN 'Vendor'
+			ELSE 'Customer'
+			END
+		)
+LEFT JOIN tblICItemUOM IU ON IU.intItemUOMId = CD.intItemUOMId
+LEFT JOIN tblICUnitMeasure U1 ON U1.intUnitMeasureId = IU.intUnitMeasureId
+LEFT JOIN tblSMCity LoadingPort ON LoadingPort.intCityId = CD.intLoadingPortId
+LEFT JOIN tblSMCity DestPort ON DestPort.intCityId = CD.intDestinationPortId
+LEFT JOIN tblSMCity DestCity ON DestCity.intCityId = CD.intDestinationCityId
+LEFT JOIN (
+	SELECT *
+	FROM (
+		SELECT ROW_NUMBER() OVER (
+				PARTITION BY S.intContractDetailId ORDER BY S.intSampleId DESC
+				) intRowNum
+			,S.intContractDetailId
+			,S.strSampleNumber
+			,S.strContainerNumber
+			,ST.strSampleTypeName
+			,SS.strStatus AS strSampleStatus
+			,S.dtmTestingStartDate
+			,S.dtmTestingEndDate
+			,S.intCompanyLocationSubLocationId
+			,CLSL.strSubLocationName
+		FROM tblQMSample S
+		JOIN tblQMSampleType AS ST ON ST.intSampleTypeId = S.intSampleTypeId
+		JOIN tblQMSampleStatus AS SS ON SS.intSampleStatusId = S.intSampleStatusId
+		LEFT JOIN tblSMCompanyLocationSubLocation CLSL ON CLSL.intCompanyLocationSubLocationId = S.intCompanyLocationSubLocationId
+		WHERE S.intContractDetailId IS NOT NULL
+		) t
+	WHERE intRowNum = 1
+	) S ON S.intContractDetailId = CD.intContractDetailId
+GROUP BY CD.intContractDetailId
+	,CD.intContractHeaderId
+	,CD.intContractSeq
+	,CD.intItemId
+	,Item.strDescription
+	,Item.strItemNo
+	,CD.dblQuantity
+	,CD.intUnitMeasureId
+	,CD.intItemUOMId
+	,U1.strUnitMeasure
+	,CD.intCompanyLocationId
+	,CL.strLocationName
+	,CD.dblBalance
+	,CD.dblScheduleQty
+	,CH.intContractTypeId
+	,CH.intEntityId
+	,CH.strContractNumber
+	,CH.dtmContractDate
+	,EY.strEntityName
+	,CD.dtmStartDate
+	,CD.dtmEndDate
+	,EY.intDefaultLocationId
+	,CH.strCustomerContract
+	,CD.intContractStatusId
+	,CH.ysnUnlimitedQuantity
+	,CH.ysnLoad
+	,CD.dblQuantityPerLoad
+	,CD.intNoOfLoad
+	,Item.strType
+	,CH.intPositionId
+	,CD.intLoadingPortId
+	,CD.intDestinationPortId
+	,CD.intDestinationCityId
+	,LoadingPort.strCity
+	,DestCity.strCity
+	,DestPort.strCity
+	,CD.strPackingDescription
+	,CD.intShippingLineId
+	,CD.intNumberOfContainers
+	,CD.intContainerTypeId
+	,CD.strVessel
+	,S.strSampleStatus
+	,S.strSampleNumber
+	,S.strContainerNumber
+	,S.strSampleTypeName
+	,S.dtmTestingStartDate
+	,S.dtmTestingEndDate
+	,S.intCompanyLocationSubLocationId
+	,S.strSubLocationName
