f750f42bc8d9fb9ddc6e471d16c8677df5c74162
Joy Balangatan <joy.balangatan@irely.com>
2016-11-23 13:58:51 +0800

    IC-3021 Fixed Other Charges Tax Calculation

--- i21Database/dbo/Stored Procedures/uspICCalculateInventoryReceiptOtherChargesTaxes.sql
+++ i21Database/dbo/Stored Procedures/uspICCalculateInventoryReceiptOtherChargesTaxes.sql
@@ -180,13 +180,13 @@ BEGIN
 
 		-- Calculate the tax per line item 
 		UPDATE	Charge 
-		SET		dblTax = ROUND(
-					dbo.fnDivide(
-						ISNULL(Taxes.dblTaxPerLineItem, 0)
-						,ISNULL(Receipt.intSubCurrencyCents, 1) 
-					)
-				, 2) 
-
+		SET		dblTax = CASE 
+							-- Negate Tax if Other Charge is marked as Price Down
+							WHEN Charge.ysnPrice = 1 
+								THEN -(ROUND(dbo.fnDivide(ISNULL(Taxes.dblTaxPerLineItem, 0) ,ISNULL(Receipt.intSubCurrencyCents, 1)), 2))
+							ELSE
+								ROUND(dbo.fnDivide(ISNULL(Taxes.dblTaxPerLineItem, 0) ,ISNULL(Receipt.intSubCurrencyCents, 1)), 2) 
+						END	
 		FROM	dbo.tblICInventoryReceipt Receipt INNER JOIN dbo.tblICInventoryReceiptCharge Charge
 						ON Receipt.intInventoryReceiptId = Charge.intInventoryReceiptId
 				LEFT JOIN (
