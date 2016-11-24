808470f53cad7973af99dc090f5cd9b529cb860b
Lex Eugene Peregrino <lex.peregrino@irely.com>
2016-11-24 17:03:57 +0800

    AP-2940 - Modify Shipment Charges Tax Group parameter

--- i21Database/dbo/Views/vyuAPBillItemReceiptTaxes.sql
+++ i21Database/dbo/Views/vyuAPBillItemReceiptTaxes.sql
@@ -8,6 +8,7 @@ FROM (
 SELECT
 	[intInventoryReceiptItemId]	=	A.intInventoryReceiptItemId,
 	[intPurchaseDetailId]		=	NULL,
+	[intInventoryShipmentChargeId] = NULL,
 	--[intTaxGroupMasterId]		=	A.intTaxGroupMasterId, 
 	[intTaxGroupId]				=	A.intTaxGroupId, 
 	[intTaxCodeId]				=	A.intTaxCodeId, 
@@ -30,6 +31,7 @@ UNION ALL
 SELECT
 	[intInventoryReceiptItemId]	=	A.intInventoryReceiptItemId,
 	[intPurchaseDetailId]		=	B.intLineNo,
+	[intInventoryShipmentChargeId] = NULL,
 	--[intTaxGroupMasterId]		=	A.intTaxGroupMasterId, 
 	[intTaxGroupId]				=	A.intTaxGroupId, 
 	[intTaxCodeId]				=	A.intTaxCodeId, 
@@ -52,6 +54,7 @@ UNION ALL
 SELECT
 	[intInventoryReceiptItemId]	=	NULL,
 	[intPurchaseDetailId]		=	B.intPurchaseDetailId,
+	[intInventoryShipmentChargeId] = NULL,
 	--[intTaxGroupMasterId]		=	A.intTaxGroupMasterId, 
 	[intTaxGroupId]				=	A.intTaxGroupId, 
 	[intTaxCodeId]				=	A.intTaxCodeId, 
@@ -69,4 +72,37 @@ FROM tblPOPurchaseDetailTax A
 INNER JOIN tblPOPurchaseDetail B ON A.intPurchaseDetailId = B.intPurchaseDetailId
 INNER JOIN tblICItem C ON B.intItemId = C.intItemId
 WHERE C.strType IN ('Service','Software','Non-Inventory','Other Charge')
+UNION ALL
+-- INVENTORY SHIPMENT CHARGES
+SELECT DISTINCT
+		[intInventoryReceiptItemId]	= NULL,
+		[intPurchaseDetailId]		= NULL,
+		[intInventoryShipmentChargeId] = A.intInventoryShipmentChargeId,
+		[intTaxGroupId]				= (CASE WHEN VST.intTaxGroupId > 0 THEN VST.intTaxGroupId
+											WHEN CL.intTaxGroupId  > 0 THEN CL.intTaxGroupId 
+											WHEN EL.intTaxGroupId > 0 THEN EL.intTaxGroupId ELSE 0 END),
+		[intTaxCodeId]				= Taxes.intTaxCodeId,
+		[intTaxClassId]				= Taxes.intTaxClassId,
+		[strTaxableByOtherTaxes]	= Taxes.strTaxableByOtherTaxes COLLATE Latin1_General_CI_AS,
+		[strCalculationMethod]		= Taxes.strCalculationMethod COLLATE Latin1_General_CI_AS,
+		[dblRate]					= Taxes.dblRate,
+		[intAccountId]				= D.intSalesTaxAccountId,
+		[dblTax]					= Taxes.dblTax,
+		[dblAdjustedTax]			= ISNULL(Taxes.dblAdjustedTax,0),
+		[ysnTaxAdjusted]			= Taxes.ysnTaxAdjusted,
+		[ysnSeparateOnBill]			= Taxes.ysnSeparateOnInvoice,
+		[ysnCheckOffTax]			= Taxes.ysnCheckoffTax
+	FROM vyuICShipmentChargesForBilling A
+	INNER JOIN  (tblAPVendor D1 INNER JOIN tblEMEntity D2 ON D1.intEntityVendorId = D2.intEntityId) ON A.[intEntityVendorId] = D1.intEntityVendorId
+	INNER JOIN dbo.tblICItem I ON I.intItemId = A.intItemId
+	LEFT JOIN dbo.tblEMEntityLocation EL ON A.intEntityVendorId = EL.intEntityId AND D1.intShipFromId = EL.intEntityLocationId
+	LEFT JOIN dbo.tblAPVendorSpecialTax VST ON VST.intEntityVendorId = A.intEntityVendorId
+	LEFT JOIN tblSMCompanyLocation CL ON CL.intCompanyLocationId = (SELECT TOP 1 intCompanyLocationId  FROM tblSMUserRoleCompanyLocationPermission)
+	LEFT JOIN tblICCategoryTax B ON I.intCategoryId = B.intCategoryId
+	LEFT JOIN tblSMTaxClass C ON B.intTaxClassId = C.intTaxClassId 
+	LEFT JOIN tblSMTaxCode D ON D.intTaxClassId = C.intTaxClassId 
+	OUTER APPLY fnGetItemTaxComputationForVendor(A.intItemId, A.intEntityVendorId, A.dtmDate, A.dblUnitCost, 1, (CASE WHEN VST.intTaxGroupId > 0 THEN VST.intTaxGroupId
+																													  WHEN CL.intTaxGroupId  > 0 THEN CL.intTaxGroupId 
+																													  WHEN EL.intTaxGroupId > 0  THEN EL.intTaxGroupId ELSE 0 END), CL.intCompanyLocationId, D1.intShipFromId , 0, NULL) Taxes
 ) Items
+GO
