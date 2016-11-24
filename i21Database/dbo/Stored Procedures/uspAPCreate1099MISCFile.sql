8879bc390e30b36cb1b2743c0b87f9f7621a0aed
Erick Dela Cruz <erick.delacruz@irely.com>
2016-11-25 00:09:01 +0800

    AP-2930

--- i21Database/dbo/Stored Procedures/uspAPCreate1099MISCFile.sql
+++ i21Database/dbo/Stored Procedures/uspAPCreate1099MISCFile.sql
@@ -19,6 +19,8 @@ DECLARE @payee AS TABLE(strPayee NVARCHAR(MAX))
 DECLARE @endOfMISC AS TABLE(strEndOfMISC NVARCHAR(1500))
 DECLARE @endOfTransmitter AS TABLE(strEndOfTransmitter NVARCHAR(1500))
 DECLARE @totalPayee NVARCHAR(16)
+DECLARE @recordCSequence NVARCHAR(16)
+DECLARE @recordFSequence NVARCHAR(16)
 
 INSERT INTO @transmitter
 SELECT dbo.[fnAP1099EFileTransmitter](@year,@test)
@@ -35,11 +37,23 @@ SELECT dbo.fnAP1099EFileEndOfMISC(@year, @reprint, @corrected, @vendorFrom, @ven
 INSERT INTO @endOfTransmitter
 SELECT [dbo].[fnAP1099EFileEndOfTransmitter](1)
 
+--UPDATE LINE 296 OF 'A' RECORD
 SET @totalPayee = REPLICATE('0', 8 - LEN(CAST((SELECT COUNT(*) FROM @payee) AS NVARCHAR(100)))) + CAST((SELECT COUNT(*) FROM @payee) AS NVARCHAR(100))
-
 UPDATE A
 	SET A.strTransmitter = STUFF(A.strTransmitter, 296, 8, @totalPayee)
 FROM @transmitter A
+
+--UPDATE LINE 500 OF 'C' RECORD
+SET @recordCSequence = REPLICATE('0', 8 - LEN(CAST((SELECT COUNT(*)+3 FROM @payee) AS NVARCHAR(100)))) + CAST((SELECT COUNT(*)+3 FROM @payee) AS NVARCHAR(100))
+UPDATE A
+	SET A.strEndOfMISC = STUFF(A.strEndOfMISC, 500, 8, @recordCSequence)
+FROM @endOfMISC A
+
+--UPDATE LINE 500 OF 'F' RECORD
+SET @recordFSequence = REPLICATE('0', 8 - LEN(CAST((SELECT COUNT(*)+4 FROM @payee) AS NVARCHAR(100)))) + CAST((SELECT COUNT(*)+4 FROM @payee) AS NVARCHAR(100))
+UPDATE A
+	SET A.strEndOfTransmitter = STUFF(A.strEndOfTransmitter, 500, 8, @recordFSequence)
+FROM @endOfTransmitter A
 
 SELECT * FROM @transmitter
 UNION ALL
