951454ee1a35865299e516092a4b0e1f099dc96f
jesmar mendones <jesmar.mendones@irely.com>
2016-11-25 15:02:32 +0800

    FRMC-27

--- i21Database/dbo/Tables/tblSMActivityFollowUpTemplate.sql
+++ i21Database/dbo/Tables/tblSMActivityFollowUpTemplate.sql
@@ -1,6 +1,9 @@
 ﻿CREATE TABLE [dbo].[tblSMActivityFollowUpTemplate]
 (
-	[intActivityFollowUpTemplateId]	INT													NOT NULL PRIMARY KEY IDENTITY,
+	[intActivityFollowUpTemplateId]	INT													NOT NULL IDENTITY,
 	[strTemplateName]				[nvarchar](250)		COLLATE Latin1_General_CI_AS	NOT NULL,
-	[intConcurrencyId]				[int]												NOT NULL DEFAULT ((1))
+	[intEntityId]					[int]												NOT NULL,
+	[intConcurrencyId]				[int]												NOT NULL DEFAULT ((1)),
+	CONSTRAINT [PK_tblSMActivityFollowUpTemplate] PRIMARY KEY CLUSTERED ([intActivityFollowUpTemplateId] ASC),
+	CONSTRAINT [UQ_tblSMActivityFollowUpTemplate_strTemplateName] UNIQUE (strTemplateName)
 )
