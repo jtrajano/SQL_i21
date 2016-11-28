742d6e7c0b4daf1202b07edbc5827ce47c5b6878
jeffrey trajano <jeffrey.trajano@irely.com>
2016-11-28 08:49:41 +0800

    GL-3037 Unposted Reminder does not

--- i21Database/dbo/Views/vyuGLPostRemind.sql
+++ i21Database/dbo/Views/vyuGLPostRemind.sql
@@ -3,25 +3,27 @@ AS
 WITH cte AS(
 	SELECT A.PostRemind_Days,A.PostRemind_BeforeAfter, B.Item intEntity FROM tblGLCompanyPreferenceOption A
 	CROSS APPLY dbo.fnSplitString(A.PostRemind_Users,',')  B
-),
-cte2 as (
-	SELECT 
-	CASE WHEN Options.PostRemind_BeforeAfter = 'Before' THEN DATEADD (DAY, Options.PostRemind_Days* -1 , dtmEndDate)
-		ELSE DATEADD(DAY, Options.PostRemind_Days - 1  , dtmStartDate) END DateLimit1,
-	Options.PostRemind_BeforeAfter ,
-	CASE WHEN Options.PostRemind_BeforeAfter = 'Before' THEN dtmEndDate ELSE dtmStartDate END DateLimit2
-	FROM tblGLFiscalYearPeriod 
-	CROSS APPLY(SELECT TOP 1  PostRemind_Days,PostRemind_BeforeAfter FROM cte )Options
-	WHERE GETDATE() BETWEEN dtmStartDate AND dtmEndDate
 )
-SELECT intJournalId,intEntityId,dtmDate,Remind.DateLimit1,Remind.DateLimit2 FROM tblGLJournal J JOIN 
+,cte2 as (
+SELECT TOP 1
+case when Options.PostRemind_BeforeAfter = 'Before' then 
+	DATEADD(day, Options.PostRemind_Days *-1, dtmEndDate) 
+else
+	dtmStartDate end	
+as DateLimit1,
+case when Options.PostRemind_BeforeAfter = 'Before' then 
+	 dtmEndDate
+else
+	DATEADD(day,Options.PostRemind_Days, dtmStartDate)  end	
+as DateLimit2
+FROM tblGLFiscalYearPeriod 
+CROSS APPLY(SELECT TOP 1  PostRemind_Days,PostRemind_BeforeAfter FROM cte )Options
+where getdate() between dateadd(day,-1, dtmStartDate) and DATEADD(day,1, dtmEndDate)
+)
+SELECT  intJournalId, intEntityId, DateLimit1,DateLimit2 FROM tblGLJournal J JOIN 
 	cte ON J.intEntityId = cte.intEntity 
-	CROSS APPLY(
-			SELECT TOP 1 
-			CASE WHEN PostRemind_BeforeAfter = 'After' THEN DateLimit2 ELSE DateLimit1 END DateLimit1,
-			CASE WHEN PostRemind_BeforeAfter = 'After' THEN DateLimit1 ELSE DateLimit2 END DateLimit2
-			FROM cte2) Remind
+	join cte2 on J.dtmDate BETWEEN DateLimit1 and  DateLimit2
 	WHERE ysnPosted = 0
 	AND ISNULL(ysnRecurringTemplate,0) =0
-	AND J.dtmDate BETWEEN Remind.DateLimit1 AND DateLimit2
+	and getdate() between dateadd(day,-1, DateLimit1) and DATEADD(day,1, DateLimit2)
 GO
