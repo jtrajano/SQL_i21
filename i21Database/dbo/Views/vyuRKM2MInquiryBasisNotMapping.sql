7cfc60af87d8beed64848496402a90fc54a4177a
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2016-11-28 18:42:38 +0530

    RM-421

---
+++ i21Database/dbo/Views/vyuRKM2MInquiryBasisNotMapping.sql
@@ -0,0 +1,29 @@
+﻿CREATE VIEW  vyuRKM2MInquiryBasisNotMapping
+
+AS 
+
+SELECT intM2MBasisDetailId
+		,strCommodityCode
+		,strContractType
+		,strCurrency
+		,strMarketZoneCode
+		,strFutMarketName
+		,strFutureMonth
+		,strItemNo
+		,strPricingType
+		,strLocationName
+		,strUnitMeasure
+ FROM tblRKM2MBasisDetail bd
+join tblRKM2MBasis mb on mb.intM2MBasisId=bd.intM2MBasisId
+join tblICCommodity c on c.intCommodityId=bd.intCommodityId
+LEFT JOIN tblARMarketZone z on z.intMarketZoneId=bd.intMarketZoneId
+LEFT JOIN tblCTContractType ct on ct.intContractTypeId=bd.intContractTypeId
+LEFT JOIN tblICItem i on i.intItemId=bd.intItemId
+LEFT JOIN tblRKFutureMarket m on m.intFutureMarketId=bd.intFutureMarketId
+LEFT JOIN tblRKFuturesMonth mo on mo.intFutureMonthId=bd.intFutureMonthId
+LEFT JOIN tblSMCompanyLocation l on l.intCompanyLocationId=bd.intCompanyLocationId
+LEFT JOIN tblSMCurrency cur on cur.intCurrencyID=bd.intCurrencyId
+LEFT JOIN tblCTPricingType pt on pt.intPricingTypeId=bd.intPricingTypeId
+LEFT JOIN tblICUnitMeasure um on um.intUnitMeasureId=bd.intUnitMeasureId
+
+
---
+++ i21Database/dbo/Views/vyuRKM2MInquiryTranNotMapping.sql
@@ -0,0 +1,17 @@
+﻿CREATE VIEW  vyuRKM2MInquiryTranNotMapping
+
+AS 
+
+SELECT DISTINCT intM2MInquiryTransactionId
+		,strCommodityCode
+		,strFutMarketName
+		,strFutureMonth
+		,strName strEntityName
+		,strItemNo		
+ FROM tblRKM2MInquiryTransaction bd
+join tblRKM2MInquiry mb on mb.intM2MInquiryId=bd.intM2MInquiryId
+join tblICCommodity c on c.intCommodityId=bd.intCommodityId
+join tblEMEntity e on e.intEntityId=bd.intEntityId
+LEFT JOIN tblICItem i on i.intItemId=bd.intItemId
+LEFT JOIN tblRKFutureMarket m on m.intFutureMarketId=bd.intFutureMarketId
+LEFT JOIN tblRKFuturesMonth mo on mo.intFutureMonthId=bd.intFutureMonthId
