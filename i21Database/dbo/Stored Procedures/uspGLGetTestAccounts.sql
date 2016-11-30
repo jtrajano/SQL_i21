a86c0e1d98cc063aa78e2751a33d3eba6740dd09
jeffrey trajano <jeffrey.trajano@irely.com>
2016-11-30 18:41:40 +0800

    TS-1527 GL - Create and Update GJ

--- i21Database/dbo/Stored Procedures/uspGLGetTestAccounts.sql
+++ i21Database/dbo/Stored Procedures/uspGLGetTestAccounts.sql
@@ -5,13 +5,17 @@ select top 1 @baseCurrency = intDefaultCurrencyId from tblSMCompanyPreference
 
 ;with mul as
 (
-select top 2 a.intAccountId, count(1)cnt from vyuGLExchangeRate a join vyuGLAccountDetail b on a.intAccountId = b.intAccountId
- group by a.intAccountId, intFromCurrencyId,intToCurrencyId,strAccountCategory 
- having intToCurrencyId =@baseCurrency and strAccountCategory = 'General'  order by cnt
+
+select top 2 a.intAccountId,dblRate, dtmValidFromDate from vyuGLExchangeRate a join vyuGLAccountDetail b on a.intAccountId = b.intAccountId
+ group by a.intAccountId, intFromCurrencyId,intToCurrencyId,strAccountCategory, dblRate ,dtmValidFromDate
+ having intToCurrencyId =3 and strAccountCategory = 'General'  
+ order by dtmValidFromDate desc
+
 )
-select top 2 strAccountId, a.strCurrency from vyuGLAccountDetail a join mul b 
+select top 2 strAccountId, a.strCurrency,dblRate from vyuGLAccountDetail a join mul b 
 on a.intAccountId = b.intAccountId 
 join tblSMCurrency c on a.intCurrencyID  = c.intCurrencyID
 
 
 select top 2 strAccountId from vyuGLAccountDetail where intCurrencyID = @baseCurrency and strAccountCategory = 'General'
+select top 1 strCurrency basecurrency from tblSMCurrency where intCurrencyID = @baseCurrency
