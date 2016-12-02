e91b412cc0c3f9d683f73c4f726ec97bd60b490c
csMJardiolin <MJardiolin@COMTECH.COM>
2016-12-02 13:08:19 +0800

    AR-2850

--- i21Database/dbo/Views/vyuARCommissionRecap.sql
+++ i21Database/dbo/Views/vyuARCommissionRecap.sql
@@ -3,8 +3,19 @@ AS
 SELECT CR.*
 	 , CS.strCommissionScheduleName
 	 , CP.strCommissionPlanName
-	 , strDateRange = CONVERT(NVARCHAR(10), ISNULL(CR.dtmStartDate, '01/01/1900'), 1) + ' - ' + CONVERT(NVARCHAR(10), ISNULL(CR.dtmEndDate, GETDATE()), 1)
- FROM tblARCommissionRecap CR
-	INNER JOIN tblARCommissionRecapDetail CRD ON CR.intCommissionRecapId = CRD.intCommissionRecapId
-	LEFT JOIN tblARCommissionSchedule CS ON CR.intCommissionScheduleId = CS.intCommissionScheduleId
-	LEFT JOIN tblARCommissionPlan CP ON CR.intCommissionPlanId = CR.intCommissionPlanId
+	 , strDateRange					=	CONVERT(NVARCHAR(10), ISNULL(CR.dtmStartDate, '01/01/1900'), 1) + ' - ' + CONVERT(NVARCHAR(10), ISNULL(CR.dtmEndDate, GETDATE()), 1)
+FROM 
+	tblARCommissionPlan CP
+INNER JOIN 
+	tblARCommissionRecap CR ON CP.intCommissionPlanId = CR.intCommissionPlanId 
+INNER JOIN 
+	(SELECT 
+		intCommissionRecapId 
+	 FROM 
+		tblARCommissionRecapDetail) CRD ON CR.intCommissionRecapId = CRD.intCommissionRecapId
+LEFT JOIN 
+	(SELECT
+		intCommissionScheduleId
+		, strCommissionScheduleName
+	 FROM 
+		tblARCommissionSchedule) CS ON CR.intCommissionScheduleId = CS.intCommissionScheduleId
