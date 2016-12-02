879d523df7fa32875ed28b6b074e0f887441f37d
Gopinath Nayak <gopinath.n@irely.com>
2016-12-02 12:26:35 +0530

    GRN-492

--- i21Database/dbo/Stored Procedures/uspGRReverseSettleStorage.sql
+++ i21Database/dbo/Stored Procedures/uspGRReverseSettleStorage.sql
@@ -1,53 +1,33 @@
 ﻿CREATE PROCEDURE [dbo].[uspGRReverseSettleStorage]
-	 @InventoryReceiptId INT
-	,@UserKey INT
+	  @ItemId INT
+	 ,@SourceNumberId INT
+	 ,@Quantity	 DECIMAL(24,10)
+	 ,@UserKey INT
 AS
 BEGIN TRY
 	SET NOCOUNT ON
 
-	DECLARE @ErrMsg NVARCHAR(MAX)
-	DECLARE @intCustomerStorageId INT
-	DECLARE @intSourceType INT
-	DECLARE @dblLineItemQty DECIMAL(24, 10)
+	DECLARE @ErrMsg NVARCHAR(MAX)	
 	DECLARE @intContractDetailId INT
-	DECLARE @intContractHeaderId INT
-	DECLARE @ItemId INT
+	DECLARE @intContractHeaderId INT	
 	DECLARE @intUnitMeasureId INT
 	DECLARE @intSourceItemUOMId INT
-
-	SELECT TOP 1 @intSourceType = intSourceType
-	FROM dbo.tblICTransactionDetailLog
-	WHERE intTransactionId = @InventoryReceiptId
-
-	IF @intSourceType = 4
-	BEGIN
-		
-		SELECT TOP 1 @intCustomerStorageId = intSourceNumberId
-		FROM dbo.tblICTransactionDetailLog
-		WHERE intTransactionId = @InventoryReceiptId
-
-		SELECT @dblLineItemQty = SUM(dblQuantity)
-		FROM tblICTransactionDetailLog
-		WHERE intTransactionId = @InventoryReceiptId
-
-		--1. Updating Ticket Open Balance.
+	
+	--1. Updating Ticket Open Balance.
 		UPDATE tblGRCustomerStorage
-		SET dblOpenBalance = dblOpenBalance + @dblLineItemQty
-		FROM tblGRCustomerStorage WHERE intCustomerStorageId = @intCustomerStorageId
+		SET dblOpenBalance = dblOpenBalance + @Quantity
+		FROM tblGRCustomerStorage WHERE intCustomerStorageId = @SourceNumberId
 
 		--2. DP Contract Quantity Increase.
 		IF EXISTS (
 				SELECT 1
 				FROM tblGRStorageHistory
-				WHERE intCustomerStorageId = @intCustomerStorageId
+				WHERE intCustomerStorageId = @SourceNumberId
 					AND intContractHeaderId IS NOT NULL
 					AND strType IN ('From Scale','From Transfer')
 				)
 		BEGIN
-			SELECT TOP 1 @ItemId = intItemId
-			FROM tblICTransactionDetailLog
-			WHERE intTransactionId = @InventoryReceiptId
-
+		
 			SELECT @intUnitMeasureId = a.intUnitMeasureId
 			FROM tblICCommodityUnitMeasure a
 			JOIN tblICItem b ON b.intCommodityId = a.intCommodityId
@@ -59,7 +39,7 @@ BEGIN TRY
 
 			SELECT @intContractHeaderId = intContractHeaderId
 			FROM tblGRStorageHistory
-			WHERE intCustomerStorageId = @intCustomerStorageId
+			WHERE intCustomerStorageId = @SourceNumberId
 				AND intContractHeaderId IS NOT NULL
 				AND strType IN ('From Scale','From Transfer')
 
@@ -69,13 +49,13 @@ BEGIN TRY
 
 			EXEC uspCTUpdateSequenceQuantityUsingUOM 
 				 @intContractDetailId = @intContractDetailId
-				,@dblQuantityToUpdate = @dblLineItemQty
+				,@dblQuantityToUpdate = @Quantity
 				,@intUserId = @UserKey
-				,@intExternalId = @intCustomerStorageId
+				,@intExternalId = @SourceNumberId
 				,@strScreenName = 'Settle Storage'
 				,@intSourceItemUOMId = @intSourceItemUOMId
 		END
-	END
+
 END TRY
 
 BEGIN CATCH
