f045cb2124d53e988ac5061fe43038ed7ae1d5ae
Lex Eugene Peregrino <lex.peregrino@irely.com>
2016-12-05 15:21:21 +0800

    AP-2941 - If receipt CostUOM is Null, default the cost calculation of voucher to receipt UOM

---
+++ i21Database/dbo/Functions/fnAPGetBillDetailReceiptTotal.sql
@@ -0,0 +1,56 @@
+﻿CREATE FUNCTION [dbo].[fnAPGetBillDetailReceiptTotal]
+(
+	@receiptId INT
+)
+RETURNS DECIMAL(18,6)
+AS
+BEGIN
+	DECLARE @total DECIMAL(18,6);
+
+	SELECT @total =  ABS(CASE WHEN B.ysnSubCurrency > 0 --CHECK IF SUB-CURRENCY
+						 THEN (CASE WHEN B.intWeightUOMId > 0 
+									THEN CAST(CASE WHEN (E1.dblCashPrice > 0 AND B.dblUnitCost = 0) 
+												   THEN E1.dblCashPrice 
+												   ELSE B.dblUnitCost 
+											  END / ISNULL(A.intSubCurrencyCents,1)  * B.dblNet * ItemWeightUOM.dblUnitQty / ISNULL(ItemCostUOM.dblUnitQty,1) AS DECIMAL(18,2)) --Formula With Weight UOM
+									WHEN (B.intUnitMeasureId > 0 AND B.intCostUOMId > 0)
+									THEN CAST((B.dblOpenReceive - B.dblBillQty) * (CASE WHEN E1.dblCashPrice > 0 THEN E1.dblCashPrice ELSE B.dblUnitCost END / ISNULL(A.intSubCurrencyCents,1)) *  (ItemUOM.dblUnitQty/ ISNULL(ItemCostUOM.dblUnitQty,1)) AS DECIMAL(18,2))  --Formula With Receipt UOM and Cost UOM
+									ELSE CAST((B.dblOpenReceive - B.dblBillQty) * (CASE WHEN E1.dblCashPrice > 0 THEN E1.dblCashPrice ELSE B.dblUnitCost END / ISNULL(A.intSubCurrencyCents,1))  AS DECIMAL(18,2))  --Orig Calculation
+							   END) 
+						 ELSE (CASE WHEN B.intWeightUOMId > 0
+									THEN CAST(CASE WHEN (E1.dblCashPrice > 0 AND B.dblUnitCost = 0) 
+												   THEN E1.dblCashPrice 
+												   ELSE B.dblUnitCost 
+											  END * B.dblNet * ItemWeightUOM.dblUnitQty / ISNULL(ItemCostUOM.dblUnitQty,1) AS DECIMAL(18,2)) --Formula With Weight UOM
+									WHEN (B.intUnitMeasureId > 0  AND B.intCostUOMId > 0)
+									THEN CAST((B.dblOpenReceive - B.dblBillQty) * CASE WHEN E1.dblCashPrice > 0 THEN E1.dblCashPrice ELSE B.dblUnitCost END * (ItemUOM.dblUnitQty/ ISNULL(ItemCostUOM.dblUnitQty,1))  AS DECIMAL(18,2))  --Formula With Receipt UOM and Cost UOM
+									ELSE CAST((B.dblOpenReceive - B.dblBillQty) * CASE WHEN E1.dblCashPrice > 0 THEN E1.dblCashPrice ELSE B.dblUnitCost END  AS DECIMAL(18,2))  --Orig Calculation
+							   END)
+						 END)
+	FROM tblICInventoryReceipt A
+	INNER JOIN tblICInventoryReceiptItem B
+		ON A.intInventoryReceiptId = B.intInventoryReceiptId
+	INNER JOIN tblICItem C
+		ON B.intItemId = C.intItemId
+	INNER JOIN tblICItemLocation D
+		ON A.intLocationId = D.intLocationId AND B.intItemId = D.intItemId
+	LEFT JOIN (tblCTContractHeader E INNER JOIN tblCTContractDetail E1 ON E.intContractHeaderId = E1.intContractHeaderId) 
+		ON E.intEntityId = A.intEntityVendorId 
+				AND E.intContractHeaderId = B.intOrderId 
+				AND E1.intContractDetailId = B.intLineNo	
+	LEFT JOIN tblICItemUOM ItemWeightUOM 
+		ON ItemWeightUOM.intItemUOMId = B.intWeightUOMId
+	LEFT JOIN tblICUnitMeasure WeightUOM 
+		ON WeightUOM.intUnitMeasureId = ItemWeightUOM.intUnitMeasureId
+	LEFT JOIN tblICItemUOM ItemCostUOM 
+		ON ItemCostUOM.intItemUOMId = B.intCostUOMId
+	LEFT JOIN tblICUnitMeasure CostUOM 
+		ON CostUOM.intUnitMeasureId = ItemCostUOM.intUnitMeasureId
+	LEFT JOIN tblICItemUOM ItemUOM 
+		ON ItemUOM.intItemUOMId = B.intUnitMeasureId
+	LEFT JOIN tblICUnitMeasure UOM 
+		ON UOM.intUnitMeasureId = ItemUOM.intUnitMeasureId
+	WHERE A.intInventoryReceiptId = @receiptId AND A.ysnPosted = 1 AND B.dblUnitCost != 0
+
+	RETURN @total;
+END
--- i21Database/dbo/Stored Procedures/uspAPCreateBillFromIR.sql
+++ i21Database/dbo/Stored Procedures/uspAPCreateBillFromIR.sql
@@ -241,16 +241,7 @@ BEGIN
 		[ysnSubCurrency]			=	CASE WHEN B.ysnSubCurrency > 0 THEN 1 ELSE 0 END,
 		[intTaxGroupId]				=	NULL,
 		[intAccountId]				=	[dbo].[fnGetItemGLAccount](B.intItemId, D.intItemLocationId, 'AP Clearing'),
-		[dblTotal]					=	ABS(CASE WHEN B.ysnSubCurrency > 0 --SubCur True
-											 THEN (CASE WHEN B.intWeightUOMId > 0 
-														THEN CAST(CASE WHEN (E1.dblCashPrice > 0 AND B.dblUnitCost = 0) THEN E1.dblCashPrice ELSE B.dblUnitCost END / ISNULL(A.intSubCurrencyCents,1)  * B.dblNet * ItemWeightUOM.dblUnitQty / ISNULL(ItemCostUOM.dblUnitQty,1) AS DECIMAL(18,2)) --Calculate Sub-Cur Base Gross/Net UOM
-														ELSE CAST((B.dblOpenReceive - B.dblBillQty) * (CASE WHEN E1.dblCashPrice > 0 THEN E1.dblCashPrice ELSE B.dblUnitCost END / ISNULL(A.intSubCurrencyCents,1)) *  (ItemUOM.dblUnitQty/ ISNULL(ItemCostUOM.dblUnitQty,1)) AS DECIMAL(18,2))  --Calculate Sub-Cur 
-												   END) 
-											 ELSE (CASE WHEN B.intWeightUOMId > 0 --SubCur False
-														THEN CAST(CASE WHEN (E1.dblCashPrice > 0 AND B.dblUnitCost = 0) THEN E1.dblCashPrice ELSE B.dblUnitCost END * B.dblNet * ItemWeightUOM.dblUnitQty / ISNULL(ItemCostUOM.dblUnitQty,1) AS DECIMAL(18,2))--Base Gross/Net UOM 
-														ELSE CAST((B.dblOpenReceive - B.dblBillQty) * CASE WHEN E1.dblCashPrice > 0 THEN E1.dblCashPrice ELSE B.dblUnitCost END * (ItemUOM.dblUnitQty/ ISNULL(ItemCostUOM.dblUnitQty,1))  AS DECIMAL(18,2))  --Orig Calculation
-												   END)
-										END),
+		[dblTotal]					=	ISNULL((dbo.fnAPGetBillDetailReceiptTotal (@receiptId)),0),
 		[dblCost]					=	ABS(CASE WHEN (B.dblUnitCost IS NULL OR B.dblUnitCost = 0)
 											 THEN (CASE WHEN E1.dblCashPrice IS NOT NULL THEN E1.dblCashPrice ELSE B.dblUnitCost END)
 											 ELSE B.dblUnitCost
