1bafdde7d3a29869e9e1fb9c314db5afe6d71713
jeffrey trajano <jeffrey.trajano@irely.com>
2016-12-06 16:28:09 +0800

    GL-3028 The Variance column should be Ending less budget

--- i21Database/dbo/Stored Procedures/uspGLGetBudget.sql
+++ i21Database/dbo/Stored Procedures/uspGLGetBudget.sql
@@ -1,15 +1,12 @@
-﻿CREATE PROCEDURE uspGLGetBudget
+﻿CREATE PROCEDURE [dbo].[uspGLGetBudget]
 (
 	@budgetCode INT,
 	@intFiscalYearId INT,
-	@dtmDateFrom DATETIME,
 	@dtmDateTo DATETIME,
 	@intAccountId INT,
 	@budget DECIMAL (18,6) OUT
-
 )
 AS
---Select * from tblFRBudget WHERE intBudgetCode =@budgetCode
 SET @budget = 0
 IF EXISTS (SELECT TOP 1 1  FROM tblFRBudget WHERE intAccountId = @intAccountId AND intBudgetCode = @budgetCode)
 BEGIN
@@ -17,23 +14,21 @@ BEGIN
 	DECLARE @dtmPeriod2 DATE
 	DECLARE @periodOrder1 int
 	DECLARE @periodOrder2 int
-	DECLARE @strBudgetFieldName NVARCHAR(300) =''
-	DECLARE @sql NVARCHAR(500)
+	DECLARE @strBudgetFieldName NVARCHAR(500) =''
+	DECLARE @sql NVARCHAR(max)
 	DECLARE @ParamDefinition NVARCHAR(500)
 
-	SELECT @dtmPeriod1 = dtmStartDate from tblGLFiscalYearPeriod where @dtmDateFrom >= dtmStartDate and @dtmDateFrom <= dtmEndDate and intFiscalYearId = @intFiscalYearId
+	--SELECT @dtmPeriod1 = dtmStartDate from tblGLFiscalYearPeriod where @dtmDateFrom >= dtmStartDate and @dtmDateFrom <= dtmEndDate and intFiscalYearId = @intFiscalYearId
 	SELECT @dtmPeriod2 = dtmStartDate from tblGLFiscalYearPeriod where @dtmDateTo >= dtmStartDate and @dtmDateTo <= dtmEndDate and intFiscalYearId = @intFiscalYearId
-	
-	IF @dtmPeriod1 IS NOT NULL
-		SELECT @periodOrder1 =  COUNT(1) FROM tblGLFiscalYearPeriod WHERE dtmStartDate <= @dtmPeriod1 and intFiscalYearId = @intFiscalYearId
-	ELSE
-		SET @periodOrder1 = 1
-
-	IF @dtmPeriod2 IS NOT NULL
-		SELECT @periodOrder2 = COUNT(1) FROM tblGLFiscalYearPeriod WHERE dtmStartDate <= @dtmPeriod2 and intFiscalYearId = @intFiscalYearId
-	ELSE
-		SET @periodOrder2 = 13
-	
+	IF @dtmPeriod2 IS NULL RETURN -1 
+	SELECT @periodOrder2 = COUNT(1) FROM tblGLFiscalYearPeriod WHERE dtmStartDate <= @dtmPeriod2 and intFiscalYearId = @intFiscalYearId
+	IF NOT EXISTS
+	(
+		SELECT  TOP 1 1 FROM tblFRBudget A join tblFRBudgetCode B ON A.intBudgetCode = B.intBudgetCode 
+		WHERE A.intBudgetCode = @budgetCode	AND intFiscalYearId = @intFiscalYearId AND A.intAccountId = @intAccountId
+	)
+	RETURN -1 
+	SET @periodOrder1 = 1
 	--COMPUTE THE BUDGET
 	WHILE @periodOrder1 <= @periodOrder2
 	BEGIN
@@ -42,17 +37,17 @@ BEGIN
 	END
 	SELECT @strBudgetFieldName=  LEFT(@strBudgetFieldName ,LEN(@strBudgetFieldName)-1)
 		
-	SET @ParamDefinition = N'@intAccountId INT, @budgetCode INT,  @budgetOut DECIMAL(18,6) OUTPUT'
+	SET @ParamDefinition = N'@intAccountId INT,@intFiscalYearId INT, @budgetCode INT,  @budgetOut DECIMAL(18,6) OUTPUT'
 	SET @sql =
 	'SELECT @budgetOut =SUM(budgets)
 	FROM 
-	   (SELECT '+ @strBudgetFieldName +
-	   ' FROM tblFRBudget WHERE intAccountId = @intAccountId and intBudgetCode = @budgetCode) p
+	   (
+	   SELECT ' + @strBudgetFieldName + ' FROM tblFRBudget A join tblFRBudgetCode B ON A.intBudgetCode = B.intBudgetCode WHERE A.intBudgetCode = @budgetCode	
+	   AND intFiscalYearId = @intFiscalYearId AND A.intAccountId = @intAccountId
+	   ) p
 	UNPIVOT
 	   (budgets FOR Budgets IN 
 		  ('+ @strBudgetFieldName +')
 	)AS unpvt;'
-	
-	EXEC sp_executesql @sql,@ParamDefinition,@intAccountId = @intAccountId, @budgetCode = @budgetCode, @budgetOut = @budget OUTPUT;
-
+	EXEC sp_executesql @sql,@ParamDefinition,@intAccountId = @intAccountId, @intFiscalYearId = @intFiscalYearId, @budgetCode = @budgetCode, @budgetOut = @budget OUTPUT;
 END
