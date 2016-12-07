ca5ad3c7d914f305a9ab63fa492cc1c109cb3a44
Smith Wilson de Jesus <smith.dejesus@irely.com>
2016-12-07 15:36:32 +0800

    CM-1465 CLONE - Settlement Report is not printing the vendors primary address

--- i21Database/dbo/Stored Procedures/uspCMSettlementReport.sql
+++ i21Database/dbo/Stored Procedures/uspCMSettlementReport.sql
@@ -112,7 +112,7 @@ BEGIN
 
 	--Vendor Address
 	strEntityName = ENTITY.strName,
-	strVendorAddress = dbo.fnConvertToFullAddress(Bill.strShipToAddress, Bill.strShipToCity, Bill.strShipToState,Bill.strShipToZipCode),
+	strVendorAddress = dbo.fnConvertToFullAddress(Bill.strShipFromAddress, Bill.strShipFromCity, Bill.strShipFromState, Bill.strShipFromZipCode),
 	TICKET.intTicketId,
 	TICKET.strTicketNumber,
 	INVRCPT.strReceiptNumber,
