21943dc65d5add6ad94764be7df6cf6a7b25bca1
Mon Gonzales <mon.gonzales@irely.com>
2016-12-07 14:39:31 +0800

    EM-1570 EM-1569

---
+++ i21Database/dbo/Views/vyuEMEntityLocationWithType.sql
@@ -0,0 +1,40 @@
+﻿CREATE VIEW [dbo].[vyuEMEntityLocationWithType]
+	AS
+
+
+
+select 
+	a.intEntityLocationId,
+	a.intEntityId,
+	strLocationName,
+	strAddress,
+	strCity,
+	strCountry,
+	strState,
+	strZipCode,
+	a.ysnActive,
+	Vendor,
+	Customer,
+	Salesperson,
+	FuturesBroker,
+	ForwardingAgent,
+	Terminal,
+	ShippingLine,
+	Trucker,
+	ShipVia,
+	Insurer,
+	Employee,
+	Producer,
+	[User],
+	Prospect,
+	Competitor,
+	Buyer,
+	[Partner],
+	Lead,
+	Veterinary,
+	Lien
+
+
+from tblEMEntityLocation a
+	join vyuEMEntityType b
+		on a.intEntityId = b.intEntityId
