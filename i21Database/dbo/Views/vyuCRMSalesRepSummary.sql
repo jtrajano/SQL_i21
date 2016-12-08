5e97977f5d72e5934ebf2624a2e5d646d6608d28
Jayson Ramos <jayson.ramos@iRely.com>
2016-12-08 14:28:10 +0800

    CRM-350

--- i21Database/dbo/Views/vyuCRMSalesRepSummary.sql
+++ i21Database/dbo/Views/vyuCRMSalesRepSummary.sql
@@ -1,52 +1,68 @@
 ﻿CREATE VIEW [dbo].[vyuCRMSalesRepSummary]
 	AS
-		with so as
-		(
-			select
-				tblCRMOpportunity.intInternalSalesPerson
-				,tblSOSalesOrder.strSalesOrderNumber
-				,tblSOSalesOrder.strTransactionType
-				,tblSOSalesOrder.strType
-				,tblSOSalesOrder.strOrderStatus
-				,tblSOSalesOrder.dblSalesOrderTotal
-				,tblSOSalesOrder.dblSalesOrderSubtotal
-				,tblSOSalesOrder.dblTax
-				,tblSOSalesOrder.dblAmountDue
-			from
-				tblCRMOpportunity
-				,tblHDOpportunityQuote
-				,tblSOSalesOrder
-			where
-				tblHDOpportunityQuote.intProjectId = tblCRMOpportunity.intOpportunityId
-				and tblSOSalesOrder.intSalesOrderId = tblHDOpportunityQuote.intSalesOrderId
-		)
+with types as
+(
+	select distinct
+		a.intInternalSalesPerson
+		,c.strActivityNo
+		,c.strCategory
+	from
+		tblCRMOpportunity a
+		,tblSMTransaction b
+		,tblSMActivity c
+	where
+		a.intInternalSalesPerson is not null
+		and b.intRecordId = a.intOpportunityId
+		and b.intScreenId = (select top 1 d.intScreenId from tblSMScreen d where d.strNamespace = 'CRM.view.Opportunity')
+		and c.intTransactionId = b.intTransactionId
+),
+so as
+(
+	select
+		a.intInternalSalesPerson
+		,c.strSalesOrderNumber
+		,c.strTransactionType
+		,c.strType
+		,c.strOrderStatus
+		,c.dblSalesOrderTotal
+		,c.dblSalesOrderSubtotal
+		,c.dblTax
+		,c.dblAmountDue
+	from
+		tblCRMOpportunity a
+		,tblCRMOpportunityQuote b
+		,tblSOSalesOrder c
+	where
+		b.intOpportunityId = a.intOpportunityId
+		and c.intSalesOrderId = b.intSalesOrderId
+)
 
-				Select distinct
-					intId = ROW_NUMBER() over(order by RepName)
-					,RepId
-					,RepName
-					,Calls = 0
-					,Tasks = 0
-					,Meetings = 0
-					,Quotes = (select count(strType) from so where intInternalSalesPerson = RepId and strTransactionType = 'Quote')
-					,DollarValueOfQuotes = (select (case when sum(dblAmountDue) is null then 0 else sum(dblAmountDue) end) from so where intInternalSalesPerson = RepId and strTransactionType = 'Quote')
-					,Orders = (select count(strType) from so where intInternalSalesPerson = RepId and strTransactionType = 'Order')
-					,DollarValueOfOrders = (select (case when sum(dblAmountDue) is null then 0 else sum(dblAmountDue) end) from so where intInternalSalesPerson = RepId and strTransactionType = 'Order')
-				From
-				(
-				select
-					RepId = tblEMEntity.intEntityId
-					,RepName = (case when tblEMEntity.strName is null then 'No Name' else (case when rtrim(ltrim(tblEMEntity.strName)) = '' then 'No Name' else tblEMEntity.strName end) end)
-					,tblCRMOpportunity.intOpportunityId
-					,tblCRMOpportunity.strName
-					,tblCRMOpportunity.strOpportunityStatus
-				from
-					tblCRMOpportunity
-					left outer join tblARSalesperson on tblARSalesperson.intEntitySalespersonId = tblCRMOpportunity.intInternalSalesPerson
-					left outer join tblEMEntity on tblEMEntity.intEntityId = tblARSalesperson.intEntitySalespersonId
-					left outer join tblCRMOpportunityQuote on tblCRMOpportunityQuote.intOpportunityId = tblCRMOpportunity.intOpportunityId
-				where
-					tblCRMOpportunity.intInternalSalesPerson is not null 
-					and tblCRMOpportunity.intInternalSalesPerson > 0
-				) as q1
-				group by RepId, RepName
+		Select distinct
+			intId = ROW_NUMBER() over(order by RepName)
+			,RepId
+			,RepName
+			,Calls = (select count(strCategory) from types where intInternalSalesPerson = RepId and strCategory = 'Call')
+			,Tasks = (select count(strCategory) from types where intInternalSalesPerson = RepId and strCategory = 'Task')
+			,Meetings = (select count(strCategory) from types where intInternalSalesPerson = RepId and strCategory = 'Meeting')
+			,Quotes = (select count(strType) from so where intInternalSalesPerson = RepId and strTransactionType = 'Quote')
+			,DollarValueOfQuotes = (select (case when sum(dblAmountDue) is null then 0 else sum(dblAmountDue) end) from so where intInternalSalesPerson = RepId and strTransactionType = 'Quote')
+			,Orders = (select count(strType) from so where intInternalSalesPerson = RepId and strTransactionType = 'Order')
+			,DollarValueOfOrders = (select (case when sum(dblAmountDue) is null then 0 else sum(dblAmountDue) end) from so where intInternalSalesPerson = RepId and strTransactionType = 'Order')
+		From
+		(
+		select
+			RepId = tblEMEntity.intEntityId
+			,RepName = (case when tblEMEntity.strName is null then 'No Name' else (case when rtrim(ltrim(tblEMEntity.strName)) = '' then 'No Name' else tblEMEntity.strName end) end)
+			,tblCRMOpportunity.intOpportunityId
+			,tblCRMOpportunity.strName
+			,tblCRMOpportunity.strOpportunityStatus
+		from
+			tblCRMOpportunity
+			left outer join tblARSalesperson on tblARSalesperson.intEntitySalespersonId = tblCRMOpportunity.intInternalSalesPerson
+			left outer join tblEMEntity on tblEMEntity.intEntityId = tblARSalesperson.intEntitySalespersonId
+			left outer join tblCRMOpportunityQuote on tblCRMOpportunityQuote.intOpportunityId = tblCRMOpportunity.intOpportunityId
+		where
+			tblCRMOpportunity.intInternalSalesPerson is not null 
+			and tblCRMOpportunity.intInternalSalesPerson > 0
+		) as q1
+		group by RepId, RepName
