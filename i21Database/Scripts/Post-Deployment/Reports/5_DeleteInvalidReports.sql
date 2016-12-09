59271b7b2425d3f5b1654b6e8d3bf67269c038b7
Crisman Lazaga <cris.lazaga@irely.com>
2016-12-09 10:27:07 +0800

    RPT-1063

--- i21Database/Scripts/Post-Deployment/Reports/5_DeleteInvalidReports.sql
+++ i21Database/Scripts/Post-Deployment/Reports/5_DeleteInvalidReports.sql
@@ -1,26 +1,7 @@
-﻿PRINT('Clean Invalid Archived Reports - Start')
-IF NOT EXISTS (SELECT TOP 1 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE UPPER(TABLE_NAME) = 'TBLSRARCHIVE') 
-BEGIN
-	CREATE TABLE [dbo].[tblSRArchiveTemp](
-	[intArchiveId] [int] IDENTITY(1,1) NOT NULL,
-	[dtmDateTime] [datetime] NOT NULL,
-	[intUserId] [int] NOT NULL,
-	[strUserName] [nvarchar](max) NULL,
-	[strDocumentKey] [nvarchar](max) NULL,
-	[strName] [nvarchar](max) NULL,
-	[strDisplayName] [nvarchar](max) NULL,
-	[strDescription] [nvarchar](max) NULL,
-	[strModule] [nvarchar](100) NULL,
-	[blbReport] [varbinary](max) NULL,
-	[ysnIsActive] [bit] NULL,
-	[intConcurrencyId] [int] NOT NULL)
-END
-GO
-
+﻿PRINT('Set to Inactive all invalid archived reports - Start')
 IF EXISTS (SELECT TOP 1 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE UPPER(TABLE_NAME) = 'TBLSRARCHIVE' AND [COLUMN_NAME] = 'strModule') 
 BEGIN
-	EXEC ('INSERT INTO tblSRArchiveTemp SELECT dtmDateTime, intUserId, strUserName, strDocumentKey, strName, strDisplayName, strDescription, strModule, blbReport, ysnIsActive, intConcurrencyId FROM tblSRArchive WHERE strModule = '''' OR strModule IS NULL')
-	EXEC ('DELETE FROM tblSRArchive WHERE strModule = '''' OR strModule IS NULL')
+	EXEC ('UPDATE tblSRArchive SET ysnIsActive = 0 WHERE strModule = '''' OR strModule IS NULL')
 END
 GO
-PRINT('Clean Invalid Archived Reports - End')
+PRINT('Set to Inactive all invalid archived reports - End')
