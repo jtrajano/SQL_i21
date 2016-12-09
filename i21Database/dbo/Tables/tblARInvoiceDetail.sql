fa0c7460af4b9df98ebbe8aafe07fbbe9a4aee74
csMJardiolin <MJardiolin@COMTECH.COM>
2016-12-09 10:59:33 +0800

    AR-3845

--- i21Database/dbo/Tables/tblARInvoiceDetail.sql
+++ i21Database/dbo/Tables/tblARInvoiceDetail.sql
@@ -84,6 +84,7 @@
 	[dblMargin]								NUMERIC(18, 6)									NULL DEFAULT 0,
 	[dblRecipeQuantity]						NUMERIC(18, 6)									NULL DEFAULT 0,
 	[intStorageScheduleTypeId]				INT												NULL,
+	[strTicketNumber]						NVARCHAR(250)	COLLATE Latin1_General_CI_AS	NULL,
     CONSTRAINT [PK_tblARInvoiceDetail_intInvoiceDetailId] PRIMARY KEY CLUSTERED ([intInvoiceDetailId] ASC),
     CONSTRAINT [FK_tblARInvoiceDetail_tblARInvoice] FOREIGN KEY ([intInvoiceId]) REFERENCES [dbo].[tblARInvoice] ([intInvoiceId]) ON DELETE CASCADE,
 	CONSTRAINT [FK_tblARInvoiceDetail_tblGLAccount_intAccountId] FOREIGN KEY ([intAccountId]) REFERENCES [dbo].[tblGLAccount] ([intAccountId]),
