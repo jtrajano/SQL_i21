dca19ea80b34ad7fce9e26e6c2fe0205b1ee7e95
Dan Michael Jalimao <dan.jalimao@irely.com>
2016-12-12 16:59:54 +0800

    SM-2328

--- i21Database/dbo/Tables/tblSMCity.sql
+++ i21Database/dbo/Tables/tblSMCity.sql
@@ -4,6 +4,7 @@
     [strCity] NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL, 
     [intCountryId] INT NOT NULL, 
     [strState] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
+	[strVAT] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
 	[ysnRegion] BIT NOT NULL DEFAULT 0,
     [ysnPort] BIT NOT NULL DEFAULT 0,
 	[ysnArbitration] BIT NOT NULL DEFAULT 0,
