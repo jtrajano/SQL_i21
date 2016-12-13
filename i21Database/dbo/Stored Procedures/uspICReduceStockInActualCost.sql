562292a039d7ec45631d034093d932c55d78f280
Feb Montefrio <feb.montefrio@irely.com>
2016-12-13 14:00:41 +0800

    IC-3082

--- i21Database/dbo/Stored Procedures/uspICReduceStockInActualCost.sql
+++ i21Database/dbo/Stored Procedures/uspICReduceStockInActualCost.sql
@@ -36,51 +36,102 @@ SET @CostUsed = NULL;
 SET @QtyOffset = NULL;
 SET @ActualCostId = NULL;
 
+-- Validate if the cost bucket is negative. If Negative stock is not allowed, then block the posting. 
+BEGIN 
+	DECLARE @ALLOW_NEGATIVE_NO AS INT = 3
+
+	DECLARE @strItemNo AS NVARCHAR(50) 
+			,@strLocationName AS NVARCHAR(MAX) 
+			,@CostBucketId AS INT 
+			,@AllowNegativeInventory AS INT 
+			,@UnitsOnHand AS NUMERIC(38, 20)
+
+	-- Get the on-hand qty 
+	SELECT	@UnitsOnHand = s.dblUnitOnHand
+	FROM	tblICItemStock s
+	WHERE	s.intItemId = @intItemId
+			AND s.intItemLocationId = @intItemLocationId
+
+	SELECT	@strItemNo = i.strItemNo
+			,@CostBucketId = cb.intInventoryActualCostId
+			,@AllowNegativeInventory = il.intAllowNegativeInventory
+			,@strLocationName = cl.strLocationName
+	FROM	tblICItem i INNER JOIN tblICItemLocation il
+				ON i.intItemId = il.intItemId
+				AND il.intItemLocationId = @intItemLocationId
+			INNER JOIN tblSMCompanyLocation cl
+				ON cl.intCompanyLocationId = il.intLocationId
+			OUTER APPLY (
+				SELECT	TOP 1 *
+				FROM	tblICInventoryActualCost cb
+				WHERE	cb.strActualCostId = @strActualCostId 
+						AND cb.intItemId = @intItemId
+						AND cb.intItemLocationId = @intItemLocationId
+						AND cb.intItemUOMId = @intItemUOMId
+						AND ROUND((cb.dblStockIn - cb.dblStockOut), 6) > 0  
+						AND dbo.fnDateLessThanEquals(cb.dtmDate, @dtmDate) = 1
+			) cb 
+
+	IF @CostBucketId IS NULL AND @AllowNegativeInventory = @ALLOW_NEGATIVE_NO
+	BEGIN 
+		IF @UnitsOnHand > 0 
+		BEGIN 
+			DECLARE @strDate AS VARCHAR(20) = CONVERT(NVARCHAR(20), @dtmDate, 101) 
+			RAISERROR(80096, 11, 1, @strDate, @strItemNo, @strLocationName)
+		END 
+		ELSE 
+		BEGIN 
+			RAISERROR(80003, 11, 1, @strItemNo, @strLocationName)
+		END 
+		RETURN -1
+	END 
+END
+
 -- Upsert (update or insert) a record in the cost bucket.
 MERGE	TOP(1)
 INTO	dbo.tblICInventoryActualCost 
 WITH	(HOLDLOCK) 
-AS		ActualCost_bucket	
+AS		cb	
 USING (
 	SELECT	strActualCostId = @strActualCostId
 			,intItemId = @intItemId
 			,intItemLocationId = @intItemLocationId
 			,intItemUOMId = @intItemUOMId
 ) AS Source_Query  
-	ON ActualCost_bucket.strActualCostId = Source_Query.strActualCostId 
-	AND ActualCost_bucket.intItemId = Source_Query.intItemId
-	AND ActualCost_bucket.intItemLocationId = Source_Query.intItemLocationId
-	AND ActualCost_bucket.intItemUOMId = Source_Query.intItemUOMId
-	AND (ActualCost_bucket.dblStockIn - ActualCost_bucket.dblStockOut) > 0 
-	AND dbo.fnDateGreaterThanEquals(@dtmDate, ActualCost_bucket.dtmDate) = 1
+	ON cb.strActualCostId = Source_Query.strActualCostId 
+	AND cb.intItemId = Source_Query.intItemId
+	AND cb.intItemLocationId = Source_Query.intItemLocationId
+	AND cb.intItemUOMId = Source_Query.intItemUOMId
+	AND (cb.dblStockIn - cb.dblStockOut) > 0 
+	AND dbo.fnDateLessThanEquals(cb.dtmDate, @dtmDate) = 1
 
 -- Update an existing cost bucket
 WHEN MATCHED THEN 
 	UPDATE 
-	SET	ActualCost_bucket.dblStockOut = ISNULL(ActualCost_bucket.dblStockOut, 0) 
-					+ CASE	WHEN (ActualCost_bucket.dblStockIn - ActualCost_bucket.dblStockOut) >= @dblQty THEN @dblQty
-							ELSE (ActualCost_bucket.dblStockIn - ActualCost_bucket.dblStockOut) 
+	SET	cb.dblStockOut = ISNULL(cb.dblStockOut, 0) 
+					+ CASE	WHEN (cb.dblStockIn - cb.dblStockOut) >= @dblQty THEN @dblQty
+							ELSE (cb.dblStockIn - cb.dblStockOut) 
 					END 
 
-		,ActualCost_bucket.intConcurrencyId = ISNULL(ActualCost_bucket.intConcurrencyId, 0) + 1
+		,cb.intConcurrencyId = ISNULL(cb.intConcurrencyId, 0) + 1
 
 		-- update the remaining qty
 		,@RemainingQty = 
-					CASE	WHEN (ActualCost_bucket.dblStockIn - ActualCost_bucket.dblStockOut) >= @dblQty THEN 0
-							ELSE (ActualCost_bucket.dblStockIn - ActualCost_bucket.dblStockOut) - @dblQty
+					CASE	WHEN (cb.dblStockIn - cb.dblStockOut) >= @dblQty THEN 0
+							ELSE (cb.dblStockIn - cb.dblStockOut) - @dblQty
 					END
 
 		-- retrieve the cost from the ActualCost bucket. 
-		,@CostUsed = ActualCost_bucket.dblCost
+		,@CostUsed = cb.dblCost
 
 		-- retrieve the	qty reduced from a ActualCost bucket 
 		,@QtyOffset = 
-					CASE	WHEN (ActualCost_bucket.dblStockIn - ActualCost_bucket.dblStockOut) >= @dblQty THEN @dblQty
-							ELSE (ActualCost_bucket.dblStockIn - ActualCost_bucket.dblStockOut) 
+					CASE	WHEN (cb.dblStockIn - cb.dblStockOut) >= @dblQty THEN @dblQty
+							ELSE (cb.dblStockIn - cb.dblStockOut) 
 					END
 
 		-- retrieve the id of the matching ActualCost bucket 
-		,@ActualCostId = ActualCost_bucket.intInventoryActualCostId
+		,@ActualCostId = cb.intInventoryActualCostId
 
 -- Insert a new ActualCost bucket
 WHEN NOT MATCHED THEN
