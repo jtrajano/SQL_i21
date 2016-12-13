cb82afbecd7d0f47c6a6d56d0f608a5861ef8d48
Feb Montefrio <feb.montefrio@irely.com>
2016-12-13 14:35:42 +0800

    IC-3099

--- i21Database/dbo/Views/vyuICGetItemStockUOM.sql
+++ i21Database/dbo/Views/vyuICGetItemStockUOM.sql
@@ -30,7 +30,18 @@ SELECT
 	dblOnHand = (CASE WHEN ISNULL(Lot.intLotId, '') = '' THEN ISNULL(StockUOM.dblOnHand, 0) ELSE ISNULL(Lot.dblQty, 0) END),	
 	dblOnOrder = StockUOM.dblOnOrder,
 	dblReservedQty = ISNULL(Reserve.dblTotalQty, 0),
-	dblAvailableQty = (CASE WHEN ISNULL(Lot.intLotId, '') = '' THEN (ISNULL(StockUOM.dblOnHand, 0) - ISNULL(Reserve.dblTotalQty, 0)) ELSE ISNULL(Lot.dblQty, 0) END),
+	dblAvailableQty = 
+		CASE	WHEN Lot.intLotId IS NOT NULL THEN 
+					Lot.dblQty
+				ELSE 
+					ISNULL(StockUOM.dblOnHand, 0)  
+					- (
+							ISNULL(StockUOM.dblUnitReserved, 0) 
+							+ ISNULL(StockUOM.dblInTransitOutbound, 0) 
+							+ ISNULL(StockUOM.dblConsignedSale, 0)
+					)
+		END,
+
 	dblStorageQty = StockUOM.dblUnitStorage,
 	dblUnitQty = ItemUOM.dblUnitQty,
 	ysnStockUnit = ItemUOM.ysnStockUnit,
