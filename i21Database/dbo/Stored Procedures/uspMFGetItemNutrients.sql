416c3c50925db20cdd2c97932c46f47af04aa969
Chakra <CBehera@irely.net>
2016-12-14 19:29:57 +0530

    MFG-2067

---
+++ i21Database/dbo/Stored Procedures/uspMFGetItemNutrients.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[uspMFGetItemNutrients]
+	@intItemId int
+AS
+
+Select p.intProductValueId AS intItemId,pp.intPropertyId,vp.dblMinValue AS dblValue
+From tblQMProduct p 
+Join tblQMProductProperty pp on p.intProductId=pp.intProductId
+Join tblQMProductPropertyValidityPeriod vp on pp.intProductPropertyId=vp.intProductPropertyId
+Where p.intProductValueId=@intItemId AND p.intProductTypeId=2
---
+++ i21Database/dbo/Stored Procedures/uspMFGetRecipeGuideSOItems.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[uspMFGetRecipeGuideSOItems]
+	@intSalesOrderId int = 0,
+	@intRecipeGuideId int = 0
+AS
+
+If @intRecipeGuideId>0
+	Select TOP 1 @intSalesOrderId=intSalesOrderId From tblSOSalesOrder Where intRecipeGuideId=@intRecipeGuideId Order By intSalesOrderId Desc
+
+If ISNULL(@intSalesOrderId,0)>0
+Begin
+	Select i.intItemId,i.strItemNo,i.strDescription,sd.dblQtyOrdered AS dblQuantity,sd.intItemUOMId,um.strUnitMeasure AS strUOM,
+	sd.dblPrice AS dblCost
+	From tblSOSalesOrderDetail sd 
+	Join tblICItem i on sd.intItemId=i.intItemId 
+	Join tblICItemUOM iu on sd.intItemUOMId=iu.intItemUOMId
+	Join tblICUnitMeasure um on iu.intUnitMeasureId=um.intUnitMeasureId
+	Where intSalesOrderId=@intSalesOrderId
+
+	Return
+End
