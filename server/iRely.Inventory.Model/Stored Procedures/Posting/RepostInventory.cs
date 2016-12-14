420ee581ba5c074eb90b5446139fb9769b467a7b
Wendell Wayne Estrada <wendell.estrada@irely.com>
2016-12-14 12:00:43 +0800

    IC-1986 Repost utility to run from a fiscal month onwards and for a selected item (optional)

--- server/iRely.Inventory.Model/Stored Procedures/Posting/RepostInventory.cs
+++ server/iRely.Inventory.Model/Stored Procedures/Posting/RepostInventory.cs
@@ -10,16 +10,17 @@ namespace iRely.Inventory.Model
 {
     public partial class InventoryEntities : DbContext
     {
-        public async Task<int> RepostInventory(DateTime? startDate, string itemNo, bool isPeriodic = true, bool generateBillGLEntries = false)
+        public async Task<int> RepostInventory(DateTime? startDate, string itemNo, bool isPeriodic = true, bool generateBillGLEntries = false, int? userId = null)
         {
             SqlParameter paramStrNo = string.IsNullOrEmpty(itemNo) ? new SqlParameter("@strItemNo", DBNull.Value)  : new SqlParameter("@strItemNo", itemNo);
             this.Database.CommandTimeout = 120000;
             return await this.Database.ExecuteSqlCommandAsync(
-                "dbo.uspICRebuildInventoryValuation @dtmStartDate, @strItemNo, @isPeriodic, @ysnRegenerateBillGLEntries",
+                "dbo.uspICRebuildInventoryValuation @dtmStartDate, @strItemNo, @isPeriodic, @ysnRegenerateBillGLEntries, @intUserId",
                 new SqlParameter("@dtmStartDate", startDate),
                 paramStrNo,
                 new SqlParameter("@isPeriodic", isPeriodic),
-                new SqlParameter("@ysnRegenerateBillGLEntries", generateBillGLEntries)
+                new SqlParameter("@ysnRegenerateBillGLEntries", generateBillGLEntries),
+                new SqlParameter("@intUserId", userId)
             );
         }
     }
