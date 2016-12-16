c42e32455e9b8a043ef56d58335e97f708d27f98
Vinoth Kumar T <vinoth.t@irely.com>
2016-12-16 21:16:03 +0530

    MFG-2159

---
+++ i21Database/dbo/Views/vyuMFLabelValues.sql
@@ -0,0 +1,25 @@
+﻿CREATE VIEW vyuMFLabelValues
+AS
+SELECT CAST(ROW_NUMBER() OVER (
+			ORDER BY intTypeId
+				,ValueMember
+			) AS INT) AS intRowNo
+	,intTypeId
+	,ValueMember
+	,DisplayMember
+	,strDescription
+FROM (
+	SELECT DISTINCT 1 AS intTypeId
+		,intItemId AS ValueMember
+		,strItemNo AS DisplayMember
+		,strDescription
+	FROM tblICItem
+	
+	UNION
+	
+	SELECT DISTINCT 2 AS intTypeId
+		,intStorageLocationId AS ValueMember
+		,strName AS DisplayMember
+		,strDescription
+	FROM tblICStorageLocation
+	) t
