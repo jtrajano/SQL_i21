e20d61ddaa3358ac1ea7a31681fd15a282ff91a3
Vinoth Kumar T <vinoth.t@irely.com>
2016-12-19 18:16:38 +0530

    MFG-2159

---
+++ i21Database/dbo/Stored Procedures/uspMFReportStorageLocationLabel.sql
@@ -0,0 +1,69 @@
+﻿CREATE PROCEDURE uspMFReportStorageLocationLabel
+	@xmlParam NVARCHAR(MAX) = NULL
+AS
+SET QUOTED_IDENTIFIER OFF
+SET ANSI_NULLS ON
+SET NOCOUNT ON
+SET XACT_ABORT ON
+SET ANSI_WARNINGS OFF
+
+BEGIN TRY
+	DECLARE @strErrMsg NVARCHAR(MAX)
+	DECLARE @intStorageLocationId NVARCHAR(MAX)
+		,@xmlDocumentId INT
+
+	IF LTRIM(RTRIM(@xmlParam)) = ''
+		SET @xmlParam = NULL
+
+	DECLARE @temp_xml_table TABLE (
+		[fieldname] NVARCHAR(50)
+		,condition NVARCHAR(20)
+		,[from] NVARCHAR(50)
+		,[to] NVARCHAR(50)
+		,[join] NVARCHAR(10)
+		,[begingroup] NVARCHAR(50)
+		,[endgroup] NVARCHAR(50)
+		,[datatype] NVARCHAR(50)
+		)
+
+	EXEC sp_xml_preparedocument @xmlDocumentId OUTPUT
+		,@xmlParam
+
+	INSERT INTO @temp_xml_table
+	SELECT *
+	FROM OPENXML(@xmlDocumentId, 'xmlparam/filters/filter', 2) WITH (
+			[fieldname] NVARCHAR(50)
+			,condition NVARCHAR(20)
+			,[from] NVARCHAR(50)
+			,[to] NVARCHAR(50)
+			,[join] NVARCHAR(10)
+			,[begingroup] NVARCHAR(50)
+			,[endgroup] NVARCHAR(50)
+			,[datatype] NVARCHAR(50)
+			)
+
+	SELECT @intStorageLocationId = [from]
+	FROM @temp_xml_table
+	WHERE [fieldname] = 'intStorageLocationId'
+
+	SELECT intStorageLocationId
+		,strName
+		,strDescription
+	FROM tblICStorageLocation SL
+	WHERE SL.intStorageLocationId IN (
+			SELECT *
+			FROM dbo.fnSplitString(@intStorageLocationId, '^')
+			)
+	ORDER BY SL.strName
+END TRY
+
+BEGIN CATCH
+	SET @strErrMsg = 'uspMFReportStorageLocationLabel - ' + ERROR_MESSAGE()
+
+	RAISERROR (
+			@strErrMsg
+			,18
+			,1
+			,'WITH NOWAIT'
+			)
+END CATCH
