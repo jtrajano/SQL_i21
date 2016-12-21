723dfc259128a19f3606dc2795c169fdde913923
Wendell Wayne Estrada <wendell.estrada@irely.com>
2016-12-21 10:48:58 +0800

    removed minified js file

--- app/controller/Inventory.js
+++ app/controller/Inventory.js
@@ -91,7 +91,7 @@ Ext.define('Inventory.controller.Inventory', {
 
     init: function() {
         Ext.Loader.loadScript("../Inventory/app/lib/underscore.js");
-        Ext.Loader.loadScript("../Inventory/app/lib/rx.all.min.js");
+        Ext.Loader.loadScript("../Inventory/app/lib/rx.all.js");
         
         Ext.Ajax.request({
             timeout: 120000,
