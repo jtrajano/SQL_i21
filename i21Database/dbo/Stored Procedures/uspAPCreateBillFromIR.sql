6d49bd844513752adf9879b414a3a78b441f20ed
Joy Balangatan <joy.balangatan@irely.com>
2016-12-21 14:50:31 +0800

    IC-3127 Fixed issue in negative amount of other charges price down or discount in Voucher"

--- i21Database/dbo/Stored Procedures/uspAPCreateBillFromIR.sql
+++ i21Database/dbo/Stored Procedures/uspAPCreateBillFromIR.sql
@@ -325,8 +325,8 @@ BEGIN
 		[intInventoryReceiptItemId]	=	A.intInventoryReceiptItemId,
 		[intInventoryReceiptChargeId]	=	A.[intInventoryReceiptChargeId],
 		[intPODetailId]				=	NULL,
-		[dblQtyOrdered]				=	1,
-		[dblQtyReceived]			=	1,
+		[dblQtyOrdered]				=	A.dblOrderQty,
+		[dblQtyReceived]			=	A.dblQuantityToBill,
 		[dblTax]					=	ISNULL(A.dblTax,0),
 		[dblRate]					=	ISNULL(G.dblRate,0),
 		[ysnSubCurrency]			=	ISNULL(A.ysnSubCurrency,0),
@@ -353,7 +353,7 @@ BEGIN
 		[intStorageLocationId]		=	NULL,
 		[int1099Form]				=	0,
 		[int1099Category]			=	0       
-	FROM [vyuAPChargesForBilling] A
+	FROM [vyuICChargesForBilling] A
 	INNER JOIN tblICInventoryReceipt B ON A.intEntityVendorId = B.intEntityVendorId
 	AND A.intInventoryReceiptId = B.intInventoryReceiptId
 	LEFT JOIN tblSMCurrencyExchangeRate F ON  (F.intFromCurrencyId = (SELECT intDefaultCurrencyId FROM dbo.tblSMCompanyPreference) AND F.intToCurrencyId = A.intCurrencyId) 
