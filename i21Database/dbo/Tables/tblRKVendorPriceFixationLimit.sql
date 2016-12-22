c3aab672e354b1f3c1a59f5aff87e3237a031470
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2016-12-22 20:01:00 +0530

    RM-537

--- i21Database/dbo/Tables/tblRKVendorPriceFixationLimit.sql
+++ i21Database/dbo/Tables/tblRKVendorPriceFixationLimit.sql
@@ -5,7 +5,7 @@
 	[strRiskIndicator] nvarchar(20)  COLLATE Latin1_General_CI_AS NOT NULL,
 	[dblCompanyExposurePercentage] NUMERIC(18, 6) NOT NULL, 
 	[dblSupplierSalesPercentage] NUMERIC(18, 6) NOT NULL,  
-	[strPriceFixationLimit] NVARCHAR(4)  COLLATE Latin1_General_CI_AS NOT NULL,
+	[strPriceFixationLimit] NVARCHAR(50)  COLLATE Latin1_General_CI_AS NOT NULL,
 
    	CONSTRAINT [PK_tblRKVendorPriceFixationLimit_intVendorPriceFixationLimitId] PRIMARY KEY ([intVendorPriceFixationLimitId]), 
 )
