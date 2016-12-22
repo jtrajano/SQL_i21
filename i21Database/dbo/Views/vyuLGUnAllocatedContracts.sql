be073b1ed4b062413724bf7b44c483f462b73dfe
Swetosuvro Ghosh <swetosuvro.g@irely.com>
2016-12-22 16:25:50 +0530

    LG-727

---
+++ i21Database/dbo/Views/vyuLGUnAllocatedContracts.sql
@@ -0,0 +1,100 @@
+﻿CREATE VIEW vyuLGUnAllocatedContracts
+AS
+SELECT *
+FROM (
+	SELECT CD.intContractDetailId
+		,CD.intContractSeq
+		,CD.intCompanyLocationId
+		,CD.dtmStartDate
+		,CD.intItemId
+		,CD.dtmEndDate
+		,CD.intFreightTermId
+		,CD.intShipViaId
+		,CD.dblQuantity AS dblDetailQuantity
+		,CD.dblFutures
+		,CD.dblBasis
+		,CD.dblCashPrice
+		,CD.strBuyerSeller
+		,CD.strFobBasis
+		,CD.dblBalance
+		,CD.dblIntransitQty
+		,CD.dblScheduleQty
+		,CD.strPackingDescription
+		,CD.intPriceItemUOMId
+		,CD.intLoadingPortId
+		,CD.intDestinationPortId
+		,CD.strShippingTerm
+		,CD.intShippingLineId
+		,CD.strVessel
+		,IM.strItemNo
+		,IM.strDescription AS strItemDescription
+		,U1.strUnitMeasure AS strItemUOM
+		,CL.strLocationName
+		,CS.strContractStatus
+		,SUM(LR.dblReservedQuantity) AS dblReservedQuantity
+		,ISNULL(CD.dblQuantity, 0) - ISNULL(SUM(LR.dblReservedQuantity), 0) AS dblUnReservedQuantity
+		,ISNULL(SUM(PAL.dblPAllocatedQty), 0) + ISNULL(SUM(SAL.dblSAllocatedQty), 0) AS dblAllocatedQty
+		,ISNULL(CD.dblQuantity, 0) - ISNULL(SUM(PAL.dblPAllocatedQty), 0) - ISNULL(SUM(SAL.dblSAllocatedQty), 0) AS dblUnAllocatedQty
+		,CH.intContractHeaderId
+		,CH.intContractTypeId
+		,CT.strContractType
+		,CH.intCommodityId
+		,CY.strCommodityCode
+		,CY.strDescription AS strCommodityDescription
+		,CH.strContractNumber
+		,CH.dtmContractDate
+		,CH.strCustomerContract
+	FROM tblCTContractDetail CD
+	JOIN tblSMCompanyLocation CL ON CL.intCompanyLocationId = CD.intCompanyLocationId
+	JOIN tblCTContractHeader CH ON CH.intContractHeaderId = CD.intContractHeaderId
+	JOIN tblCTContractType CT ON CT.intContractTypeId = CH.intContractTypeId
+	LEFT JOIN tblICCommodity CY ON CY.intCommodityId = CH.intCommodityId
+	LEFT JOIN tblCTContractStatus CS ON CS.intContractStatusId = CD.intContractStatusId
+	LEFT JOIN tblICItem IM ON IM.intItemId = CD.intItemId
+	LEFT JOIN tblICItemUOM IU ON IU.intItemUOMId = CD.intItemUOMId
+	LEFT JOIN tblICUnitMeasure U1 ON U1.intUnitMeasureId = IU.intUnitMeasureId
+	LEFT JOIN tblLGReservation LR ON LR.intContractDetailId = CD.intContractDetailId
+	LEFT JOIN tblLGAllocationDetail PAL ON PAL.intPContractDetailId = CD.intContractDetailId
+	LEFT JOIN tblLGAllocationDetail SAL ON SAL.intSContractDetailId = CD.intContractDetailId
+	LEFT JOIN tblICUnitMeasure U5 ON U5.intUnitMeasureId = PAL.intPUnitMeasureId
+	LEFT JOIN tblICUnitMeasure U6 ON U6.intUnitMeasureId = SAL.intSUnitMeasureId
+	GROUP BY CD.intContractDetailId
+		,CD.intContractSeq
+		,CD.intCompanyLocationId
+		,CD.dtmStartDate
+		,CD.intItemId
+		,CD.dtmEndDate
+		,CD.intFreightTermId
+		,CD.intShipViaId
+		,CD.dblQuantity
+		,CD.dblFutures
+		,CD.dblBasis
+		,CD.dblCashPrice
+		,CD.strBuyerSeller
+		,CD.strFobBasis
+		,CD.dblBalance
+		,CD.dblIntransitQty
+		,CD.dblScheduleQty
+		,CD.strPackingDescription
+		,CD.intPriceItemUOMId
+		,CD.intLoadingPortId
+		,CD.intDestinationPortId
+		,CD.strShippingTerm
+		,CD.intShippingLineId
+		,CD.strVessel
+		,IM.strItemNo
+		,IM.strDescription
+		,U1.strUnitMeasure
+		,CL.strLocationName
+		,CS.strContractStatus
+		,CH.intContractHeaderId
+		,CH.intContractTypeId
+		,CT.strContractType
+		,CH.intCommodityId
+		,CY.strCommodityCode
+		,CY.strDescription
+		,CH.strContractNumber
+		,CH.dtmContractDate
+		,CH.strCustomerContract
+	) tbl
+WHERE dblUnAllocatedQty > 0
