647a1c984ab848146fbb335f08021054906384c3
Lawrence Zabala <lawrence.zabala@irely.com>
2016-12-23 14:20:00 +0800

    MFT-362

---
+++ i21Database/dbo/Tables/tblTFReportingComponentCustomer.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tblTFReportingComponentCustomer]
+(
+	[intReportingComponentCustomerId] INT NOT NULL ,
+	[intReportingComponentId] INT NOT NULL,
+	[intEntityCustomerId] INT NOT NULL,
+	[intConcurrencyId] INT NULL DEFAULT ((1)), 
+    CONSTRAINT [PK_tblTFReportingComponentCustomer] PRIMARY KEY ([intReportingComponentCustomerId]), 
+    CONSTRAINT [AK_tblTFReportingComponentCustomer] UNIQUE ([intReportingComponentId], [intEntityCustomerId]), 
+    CONSTRAINT [FK_tblTFReportingComponentCustomer_tblTFReportingComponent] FOREIGN KEY ([intReportingComponentId]) REFERENCES [tblTFReportingComponent]([intReportingComponentId]), 
+    CONSTRAINT [FK_tblTFReportingComponentCustomer_tblARCustomer] FOREIGN KEY ([intEntityCustomerId]) REFERENCES [tblARCustomer]([intEntityCustomerId])
+)
