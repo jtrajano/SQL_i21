892dbd26ef5a80d4b8baabc7f8b44f7bc934a3b0
Kelvin Quinto <kelvin.quinto@irely.com>
2016-12-23 14:58:10 +0800

    AR-3929

--- i21Database/dbo/Stored Procedures/uspARCustomerAgingDetailAsOfDateReport.sql
+++ i21Database/dbo/Stored Procedures/uspARCustomerAgingDetailAsOfDateReport.sql
@@ -282,7 +282,7 @@ SELECT DISTINCT
      , dblInvoiceTotal		= CASE WHEN I.strTransactionType IN ('Credit Memo', 'Overpayment', 'Credit', 'Customer Prepayment') AND ISNULL(P.dblAmountPaid, 0) = (I.dblInvoiceTotal * -1) 
 								THEN I.dblInvoiceTotal * -1 
 								ELSE 
-									CASE WHEN I.strTransactionType = 'Overpayment' AND ISNULL(P.dblAmountPaid, 0) < 0
+									CASE WHEN I.strTransactionType IN ('Overpayment', 'Customer Prepayment') AND ISNULL(P.dblAmountPaid, 0) < 0
 										THEN ISNULL(PD.dblInvoiceTotal, 0)
 										ELSE 0
 									END
@@ -532,7 +532,7 @@ SELECT DISTINCT
   , dblInvoiceTotal		= CASE WHEN I.strTransactionType IN ('Credit Memo', 'Overpayment', 'Credit', 'Customer Prepayment') AND ISNULL(P.dblAmountPaid, 0) = (I.dblInvoiceTotal * -1) 
 							THEN I.dblInvoiceTotal * -1 
 							ELSE 
-								CASE WHEN I.strTransactionType = 'Overpayment' AND ISNULL(P.dblAmountPaid, 0) < 0
+								CASE WHEN I.strTransactionType IN ('Overpayment', 'Customer Prepayment') AND ISNULL(P.dblAmountPaid, 0) < 0
 									THEN ISNULL(PD.dblInvoiceTotal, 0)
 									ELSE 0
 								END
