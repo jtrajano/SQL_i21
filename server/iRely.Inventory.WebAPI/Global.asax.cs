f385b929db138cce6ba70e48992728bad9c19779
Feb Montefrio <feb.montefrio@irely.com>
2016-12-27 19:00:22 +0800

    IC-1351

--- server/iRely.Inventory.WebAPI/Global.asax.cs
+++ server/iRely.Inventory.WebAPI/Global.asax.cs
@@ -8,7 +8,7 @@ using System.Reflection;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
-using System.Web.Optimization;
+//using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace iRely.Inventory.WebApi
@@ -49,7 +49,7 @@ namespace iRely.Inventory.WebApi
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //BundleConfig.RegisterBundles(BundleTable.Bundles);
 
             config.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
         }
