fefef6dc54d945adbd6e6a2da698d2ecf5506d5b
Feb Montefrio <feb.montefrio@irely.com>
2016-12-29 10:25:18 +0800

    IC-3155

--- server/iRely.Inventory.WebAPI/Global.asax.cs
+++ server/iRely.Inventory.WebAPI/Global.asax.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Reflection;
 using System.Web;
 using System.Web.Http;
-using System.Web.Mvc;
+//using System.Web.Mvc;
 //using System.Web.Optimization;
 using System.Web.Routing;
 
@@ -44,10 +44,10 @@ namespace iRely.Inventory.WebApi
             config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
             #endregion
 
-            AreaRegistration.RegisterAllAreas();
+            //AreaRegistration.RegisterAllAreas();
 
             WebApiConfig.Register(GlobalConfiguration.Configuration);
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            //FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             //BundleConfig.RegisterBundles(BundleTable.Bundles);
 
