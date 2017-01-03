a51b816de94463eb812564a1ace1c37418078239
Mark Paragas <mark.paragas@irely.com>
2017-01-03 17:08:03 +0800

    MFT-365

--- i21Database/dbo/Tables/tblTFReportingComponentCriteria.sql
+++ i21Database/dbo/Tables/tblTFReportingComponentCriteria.sql
@@ -1,6 +1,6 @@
-﻿CREATE TABLE [dbo].[tblTFTaxCriteria]
+﻿CREATE TABLE [dbo].[tblTFReportingComponentCriteria]
 (
-	[intTaxCriteriaId] INT IDENTITY (1,1) NOT NULL,
+	[intReportingComponentCriteriaId] INT IDENTITY (1,1) NOT NULL,
     [intTaxCategoryId] INT NOT NULL, 
     [intReportingComponentId] INT NOT NULL, 
     [strState] NVARCHAR(10) COLLATE Latin1_General_CI_AS NOT NULL, 
@@ -9,20 +9,21 @@
     [intConcurrencyId] INT NULL, 
     CONSTRAINT [PK_tblTFTaxCriteria] PRIMARY KEY CLUSTERED 
 (
-	[intTaxCriteriaId] ASC
+	[intReportingComponentCriteriaId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 
 GO
 
-ALTER TABLE [dbo].[tblTFTaxCriteria]  WITH CHECK ADD  CONSTRAINT [FK_tblTFTaxCriteria_tblTFReportingComponent] FOREIGN KEY([intReportingComponentId])
+ALTER TABLE [dbo].[tblTFReportingComponentCriteria]  WITH CHECK ADD  CONSTRAINT [FK_tblTFReportingComponentCriteria_tblTFReportingComponent] FOREIGN KEY([intReportingComponentId])
 REFERENCES [dbo].[tblTFReportingComponent] ([intReportingComponentId])
+ON UPDATE CASCADE
 ON DELETE CASCADE
 GO
 
-ALTER TABLE [dbo].[tblTFTaxCriteria] CHECK CONSTRAINT [FK_tblTFTaxCriteria_tblTFReportingComponent]
+ALTER TABLE [dbo].[tblTFReportingComponentCriteria] CHECK CONSTRAINT [FK_tblTFReportingComponentCriteria_tblTFReportingComponent]
 GO
 
-ALTER TABLE [dbo].[tblTFTaxCriteria] ADD  CONSTRAINT [DF_tblTFTaxCriteria_intConcurrencyId]  DEFAULT ((1)) FOR [intConcurrencyId]
+ALTER TABLE [dbo].[tblTFReportingComponentCriteria] ADD  CONSTRAINT [DF_tblTFReportingComponentCriteria_intConcurrencyId]  DEFAULT ((1)) FOR [intConcurrencyId]
 GO
 
