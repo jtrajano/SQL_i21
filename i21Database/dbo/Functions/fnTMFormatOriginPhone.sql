e255b1c72f88a37b5ba79a25e9ba44a1ba255a2a
kenneth lagunero <kenneth.lagunero@irely.com>
2017-01-04 18:29:16 +0800

    TM-2161

---
+++ i21Database/dbo/Functions/fnTMFormatOriginPhone.sql
@@ -0,0 +1,35 @@
+﻿CREATE FUNCTION [dbo].[fnTMFormatOriginPhone]
+(
+	@Value		NVARCHAR(200)
+)
+RETURNS NVARCHAR(MAX)
+AS
+BEGIN
+
+	DECLARE @RetValue NVARCHAR(400)
+
+	SET @Value = RTRIM(@Value)
+
+	IF LEN(@Value) > 3
+	BEGIN
+		SET @RetValue = SUBSTRING(@Value, 1, 3) + '-'
+
+		IF LEN(@Value) > 6
+		BEGIN
+			SET @RetValue = @RetValue + SUBSTRING(@Value, 4, 3) + '-' + SUBSTRING(@Value, 7, LEN(@Value) - 6)
+		END
+		ELSE
+		BEGIN
+			SET @RetValue = @RetValue + SUBSTRING(@Value, 4, LEN(@Value) - 3)
+		END
+	END
+	ELSE
+	BEGIN
+		SET @RetValue = @Value
+	END
+	
+	
+	RETURN @RetValue
+END
+
+GO 
