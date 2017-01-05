d9ad0444ab0dc8a77fb4a3db7ce79cc015dceb11
Satya <satya.sahoo@irely.com>
2017-01-05 19:03:06 +0530

    CT-975

---
+++ i21Database/dbo/Stored Procedures/uspCTContractApproved.sql
@@ -0,0 +1,62 @@
+﻿CREATE PROCEDURE [dbo].[uspCTContractApproved]
+	@intContractHeaderId	INT,
+	@intApprovedById		INT
+AS
+
+BEGIN TRY
+
+	DECLARE @ErrMsg	NVARCHAR(MAX)
+
+	INSERT INTO tblCTApprovedContract
+	(
+			intContractHeaderId,	intContractDetailId,	intEntityId,
+			intGradeId,				intWeightId,			intTermId,
+			intPositionId,			intContractBasisId,		intContractStatusId,
+			dtmStartDate,			dtmEndDate,				dtmPlannedAvailabilityDate,
+			intItemId,				dblQuantity,			intQtyUOMId,
+			intFutureMarketId,		intFutureMonthId,		dblFutures,
+			dblBasis,				dblCashPrice,			intCurrencyId,
+			intPriceUOMId,			intSubLocationId,		intStorageLocationId,
+			intPurchasingGroupId,	intApprovedById,		dtmApproved
+	)
+	SELECT	CD.intContractHeaderId,
+			CD.intContractDetailId,
+			CH.intEntityId,
+			CH.intGradeId,
+			CH.intWeightId,
+			CH.intTermId,
+			CH.intPositionId,
+			CH.intContractBasisId,
+			CD.intContractStatusId,
+			CD.dtmStartDate,
+			CD.dtmEndDate,
+			CD.dtmPlannedAvailabilityDate,
+			CD.intItemId,
+			CD.dblQuantity,
+			CD.intUnitMeasureId AS intQtyUOMId,
+			CD.intFutureMarketId,
+			CD.intFutureMonthId,
+			CD.dblFutures,
+			CD.dblBasis,
+			CD.dblCashPrice,
+			CD.intCurrencyId,
+			PU.intUnitMeasureId AS intPriceUOMId,
+			CD.intSubLocationId,
+			CD.intStorageLocationId,
+			CD.intPurchasingGroupId,
+			@intApprovedById,
+			GETDATE()
+
+	FROM	tblCTContractDetail CD 
+	JOIN	tblCTContractHeader CH	ON	CH.intContractHeaderId	=	CD.intContractHeaderId	LEFT
+	JOIN	tblICItemUOM		PU	ON	PU.intItemUOMId			=	CD.intPriceItemUOMId
+	WHERE	CD.intContractHeaderId	=	@intContractHeaderId
+
+END TRY
+
+BEGIN CATCH
+
+	SET @ErrMsg = ERROR_MESSAGE()  
+	RAISERROR (@ErrMsg,18,1,'WITH NOWAIT')  
+	
+END CATCH
---
+++ i21Database/dbo/Tables/tblCTApprovedContract.sql
@@ -0,0 +1,34 @@
+﻿CREATE TABLE [dbo].[tblCTApprovedContract]
+(
+	[intApprovedContractId] [int] IDENTITY(1,1) NOT NULL,
+	[intContractHeaderId] [int]  NOT NULL,
+	[intContractDetailId] [int] NOT NULL, 
+
+	[intEntityId] [int],
+	[intGradeId] [int],
+	[intWeightId] [int],
+	[intTermId] INT, 
+	[intPositionId] [INT],
+	[intContractBasisId] [INT],
+	
+	[intContractStatusId] [int],
+	[dtmStartDate] [datetime],
+	[dtmEndDate] [datetime],
+	[dtmPlannedAvailabilityDate] [datetime],
+	[intItemId] [int],
+	[dblQuantity] [numeric](18, 6),
+	[intQtyUOMId] [int], -- From tblICUnitMeasure not tblICItemUOM
+	[intFutureMarketId] [int],
+	[intFutureMonthId] INT,
+	[dblFutures] [numeric](18, 6),
+	[dblBasis] [numeric](18, 6),	
+	[dblCashPrice] [numeric](18, 6),
+	[intCurrencyId] [int],
+	[intPriceUOMId]  INT, -- From tblICUnitMeasure not tblICItemUOM
+	[intSubLocationId] INT,
+	[intStorageLocationId] INT,
+	[intPurchasingGroupId] INT,
+
+	[intApprovedById]	INT,
+	[dtmApproved]	DATETIME
+)
