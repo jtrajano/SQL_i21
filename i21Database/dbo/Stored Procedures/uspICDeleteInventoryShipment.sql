26d540164095ab47be9c6b3834ac695264b297e7
Feb Montefrio <feb.montefrio@irely.com>
2017-01-09 11:23:30 +0800

    IC-3068

---
+++ i21Database/dbo/Stored Procedures/uspICDeleteInventoryShipment.sql
@@ -0,0 +1,45 @@
+﻿CREATE PROCEDURE [dbo].[uspICDeleteInventoryShipment]
+	 @InventoryShipmentId INT
+	,@intEntityUserSecurityId INT
+AS
+SET QUOTED_IDENTIFIER OFF
+SET ANSI_NULLS ON
+SET NOCOUNT ON
+SET XACT_ABORT ON
+SET ANSI_WARNINGS OFF
+
+--------------------------------------------------------------------
+-- Validate
+--------------------------------------------------------------------
+DECLARE @strShipmentNumber AS NVARCHAR(50) 
+
+SELECT	TOP 1 
+		@strShipmentNumber = strShipmentNumber
+FROM	dbo.tblICInventoryShipment 
+WHERE	intInventoryShipmentId = @InventoryShipmentId 
+		AND ISNULL(ysnPosted, 0) = 1
+
+IF @strShipmentNumber IS NOT NULL 
+BEGIN 
+	-- 'Delete is not allowed. %s is posted.'
+	RAISERROR(80070, 11, 1, @strShipmentNumber)  
+	RETURN -1; 
+END 
+
+--------------------------------------------------------------------
+-- Delete the record
+--------------------------------------------------------------------
+DELETE FROM	dbo.tblICInventoryShipment 
+WHERE intInventoryShipmentId = @InventoryShipmentId
+
+--------------------------------------------------------------------
+-- Audit Log          
+--------------------------------------------------------------------
+EXEC	dbo.uspSMAuditLog 
+		@keyValue = @InventoryShipmentId						-- Primary Key Value of the Inventory Shipment. 
+		,@screenName = 'Inventory.view.InventoryShipment'       -- Screen Namespace
+		,@entityId = @intEntityUserSecurityId					-- Entity Id.
+		,@actionType = 'Deleted'								-- Action Type
+		,@changeDescription = ''								-- Description
+		,@fromValue = ''										-- Previous Value
+		,@toValue = ''											-- New Value
