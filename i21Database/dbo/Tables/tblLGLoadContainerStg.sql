78070d80fce22f586b7541fb33ee41aad03da71a
Swetosuvro Ghosh <swetosuvro.g@irely.com>
2017-01-10 16:39:13 +0530

    LG-728, LG-729, LG-730

---
+++ i21Database/dbo/Tables/tblLGLoadContainerStg.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[tblLGLoadContainerStg]
+(
+	[intLoadContainerStgId] INT PRIMARY KEY,
+	[intLoadStgId] INT,
+	[intLoadId] INT,
+	[strContainerNo] NVARCHAR(100)  COLLATE Latin1_General_CI_AS, 
+	[strContainerSizeCode] NVARCHAR(100) COLLATE Latin1_General_CI_AS, 
+	[strPackagingMaterialType] NVARCHAR(50) COLLATE Latin1_General_CI_AS,
+	[strExternalPONumber] NVARCHAR (100)  COLLATE Latin1_General_CI_AS,
+	[strSeq] NVARCHAR(100) COLLATE Latin1_General_CI_AS, 
+	[dblContainerQty] NUMERIC(18,6), 
+	[strContainerUOM] NVARCHAR(100), 
+	[strRowState] NVARCHAR(50) COLLATE Latin1_General_CI_AS, 
+
+	CONSTRAINT [FK_tblLGLoadContainerStg_tblLGLoadStg_intLoadStgId] FOREIGN KEY ([intLoadStgId]) REFERENCES [dbo].[tblLGLoadStg] ([intLoadStgId]) ON DELETE CASCADE,
+)
---
+++ i21Database/dbo/Tables/tblLGLoadStg.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE [dbo].[tblLGLoadStg]
+(
+	[intLoadStgId] INT PRIMARY KEY,
+	[intLoadId] INT,
+	[intRowNumber] INT, 
+	[strItemNo] NVARCHAR(100) COLLATE Latin1_General_CI_AS, 
+	[strSubLocationName] NVARCHAR(100) COLLATE Latin1_General_CI_AS, 
+	[strStorageLocationName] NVARCHAR(100) COLLATE Latin1_General_CI_AS, 
+	[strBatchNumber] NVARCHAR(100) COLLATE Latin1_General_CI_AS, 
+	[dblDeliveredQty] NUMERIC(18,6), 
+	[strUnitOfMeasure] NVARCHAR(100) COLLATE Latin1_General_CI_AS,  
+	[intHigherPositionRef] INT, 
+	[strDocumentCategory] NVARCHAR(10) COLLATE Latin1_General_CI_AS, 
+	[strReferenceDataInfo] NVARCHAR(10) COLLATE Latin1_General_CI_AS, 
+	[strExternalPONumber] NVARCHAR (100)  COLLATE Latin1_General_CI_AS, 
+	[strSeq] NVARCHAR(100) COLLATE Latin1_General_CI_AS, 
+	[strLoadNumber] NVARCHAR(100) COLLATE Latin1_General_CI_AS, 
+	[strChangeType] NVARCHAR(100) COLLATE Latin1_General_CI_AS,
+	[strRowState]	NVARCHAR(50) COLLATE Latin1_General_CI_AS, 
+	[strMessage] NVARCHAR(50) COLLATE Latin1_General_CI_AS, 
+)
