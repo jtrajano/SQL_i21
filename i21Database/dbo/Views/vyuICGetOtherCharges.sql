0e30e703eea65d1077ecd977ce3d2d5334af63a7
Wendell Wayne Estrada <wendell.estrada@irely.com>
2017-01-11 16:44:36 +0800

    IC-3199 Item - type "other charge" , need a new dropdown next to M2M checkbox

--- i21Database/dbo/Views/vyuICGetOtherCharges.sql
+++ i21Database/dbo/Views/vyuICGetOtherCharges.sql
@@ -17,9 +17,12 @@ SELECT Item.intItemId
 	, Item.intOnCostTypeId
 	, strOnCostType = OnCostType.strItemNo
 	, Item.ysnBasisContract
+	, Item.intM2MComputationId
+	, M2M.strM2MComputation
 FROM tblICItem Item
 	LEFT JOIN tblICItem OnCostType ON OnCostType.intItemId = Item.intOnCostTypeId
 	LEFT JOIN tblICItemUOM CostUOM ON CostUOM.intItemUOMId = Item.intCostUOMId
 	LEFT JOIN tblICUnitMeasure UOM ON UOM.intUnitMeasureId = CostUOM.intUnitMeasureId
+	LEFT JOIN tblICM2MComputation M2M ON M2M.intM2MComputationId = Item.intM2MComputationId
 WHERE Item.strType = 'Other Charge'
 
