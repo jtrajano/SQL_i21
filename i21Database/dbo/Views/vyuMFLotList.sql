c12bfd7cc2b4210138a02b3e99243c5e9352b64e
Vinoth Kumar T <vinoth.t@irely.com>
2017-01-11 19:42:58 +0530

    MFG-2158

---
+++ i21Database/dbo/Views/vyuMFLotList.sql
@@ -0,0 +1,13 @@
+﻿CREATE VIEW vyuMFLotList
+AS
+SELECT L.intLotId
+	,L.strLotNumber
+	,L.intItemId
+	,L.intLocationId
+FROM tblICLot L
+JOIN tblICStorageLocation SL ON SL.intStorageLocationId = L.intStorageLocationId
+JOIN tblICRestriction R1 ON R1.intRestrictionId = SL.intRestrictionId
+	AND R1.strInternalCode = 'STOCK'
+WHERE L.intLotStatusId = 1 -- Active
+	AND L.dtmExpiryDate >= GetDate()
+	AND L.dblQty > 0
