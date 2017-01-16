93cc92fbbc6281405677838836a19ac8902dc839
Dan Michael Jalimao <dan.jalimao@irely.com>
2017-01-16 13:46:48 +0800

    SM-2455

--- i21Database/dbo/Tables/tblSMPurchasingGroup.sql
+++ i21Database/dbo/Tables/tblSMPurchasingGroup.sql
@@ -3,5 +3,6 @@
 	[intPurchasingGroupId] INT NOT NULL PRIMARY KEY IDENTITY, 
     [strName] NVARCHAR(150) NOT NULL, 
     [strDescription] NVARCHAR(150) NULL, 
-    [intConcurrencyId] INT NOT NULL DEFAULT 1
+    [intConcurrencyId] INT NOT NULL DEFAULT 1, 
+    CONSTRAINT [AK_tblSMPurchasingGroup_strName] UNIQUE ([strName])
 )
