1dd7cc555b3813a082332f089828b07912b87adb
Erick Dela Cruz <erick.delacruz@irely.com>
2017-01-17 06:19:40 +0800

    AP-2152

--- i21Database/dbo/Functions/fnAP1099EFileMISCPayee.sql
+++ i21Database/dbo/Functions/fnAP1099EFileMISCPayee.sql
@@ -29,7 +29,8 @@ BEGIN
 		+ CASE WHEN @corrected = 1 THEN 'G' ELSE ' ' END
 		+ SPACE(4)
 		+ '1'
-		+ ISNULL(NULLIF(A.strFederalTaxId,''),SPACE(9)) -- 12-20
+		+ SPACE(9 - LEN(ISNULL(NULLIF(SUBSTRING(REPLACE(A.strFederalTaxId,'-',''),1,10),''),SPACE(9)))) 
+			+ ISNULL(NULLIF(SUBSTRING(REPLACE(A.strFederalTaxId,'-',''),1,10),''),SPACE(9)) -- 12-20
 		+ CAST(A.intEntityVendorId AS NVARCHAR) + SPACE(20 - LEN(CAST(A.intEntityVendorId AS NVARCHAR))) -- 21-40
 		+ SPACE(4) -- 41-44
 		+ SPACE(10) -- 45-54
