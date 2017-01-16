669316beb756d27ca630188188ed73989c183782
Joy Balangatan <joy.balangatan@irely.com>
2017-01-16 18:03:06 +0800

    IC-3004 Added Custom Tab to Item Screen

--- app/view/ItemViewController.js
+++ app/view/ItemViewController.js
@@ -1278,6 +1278,7 @@ Ext.define('Inventory.view.ItemViewController', {
             fieldTitle: 'strItemNo',
             enableAudit: true,
             enableActivity: true,
+            enableCustomTab: true,
             createTransaction: Ext.bind(me.createTransaction, me),
             onSaveClick: me.saveAndPokeGrid(win, grdUOM),
             attachment: Ext.create('iRely.mvvm.attachment.Manager', {
