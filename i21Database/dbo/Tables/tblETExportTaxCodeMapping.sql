de011d7b09dd3adb2b9c2534820d44faa10e1f22
Manuel Armas <manny.armas@irely.com>
2017-01-17 15:56:11 +0800

    IET-122 - Energy Trac Tax Cross Reference Table

---
+++ i21Database/dbo/Tables/tblETExportTaxCodeMapping.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE tblETExportTaxCodeMapping(
+	[intExportTaxCodeMappingId] int IDENTITY(1,1) NOT NULL,
+	[intTaxGroupId] int NOT NULL,
+	[strTaxCodeReference] NVARCHAR (50) NOT NULL,
+	[intConcurrencyId] [int] NOT NULL,
+CONSTRAINT [PK_tblETExportTaxCodeMapping] PRIMARY KEY CLUSTERED([intExportTaxCodeMappingId] ASC), 
+CONSTRAINT [FK_tblETExportTaxCodeMapping_tblSMTaxGroup] FOREIGN KEY ([intTaxGroupId]) REFERENCES [tblSMTaxGroup]([intTaxGroupId]) ON DELETE CASCADE)
