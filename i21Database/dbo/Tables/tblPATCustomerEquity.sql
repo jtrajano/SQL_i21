6241c954b004f63c3b35b30f0b4f6faf0dd19cb8
Jb Mago <benedict.mago@irely.com>
2017-01-17 10:42:17 +0800

    PAT-68 Additional Changes

--- i21Database/dbo/Tables/tblPATCustomerEquity.sql
+++ i21Database/dbo/Tables/tblPATCustomerEquity.sql
@@ -6,7 +6,7 @@
     [strEquityType] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
     [intRefundTypeId] INT NOT NULL, 
     [dblEquity] NUMERIC(18, 6) NOT NULL, 
-	[dblEquityPaid] NUMERIC(18, 6) NULL,
+	[dblEquityPaid] NUMERIC(18, 6) NULL DEFAULT 0,
     [intConcurrencyId] INT NULL DEFAULT 1, 
     CONSTRAINT [PK_tblPATCustomerEquity] PRIMARY KEY ([intCustomerEquityId]) 
 )
