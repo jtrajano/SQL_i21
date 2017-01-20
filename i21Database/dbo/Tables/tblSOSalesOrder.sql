f40ebb3fe065f61f916c54cddf30069566ae94cc
Kelvin Quinto <kelvin.quinto@irely.com>
2017-01-20 15:40:33 +0800

    AR-4015

--- i21Database/dbo/Tables/tblSOSalesOrder.sql
+++ i21Database/dbo/Tables/tblSOSalesOrder.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[tblSOSalesOrder] (
     [intSalesOrderId]			INT             IDENTITY (1, 1) NOT NULL,
     [strSalesOrderNumber]		NVARCHAR (25)   COLLATE Latin1_General_CI_AS NULL,
-    [strSalesOrderOriginId]		NVARCHAR (8)    COLLATE Latin1_General_CI_AS NULL,
+    [strSalesOrderOriginId]		NVARCHAR (25)	COLLATE Latin1_General_CI_AS NULL,
     [intEntityCustomerId]       INT             NULL,
     [dtmDate]					DATETIME        NOT NULL,
     [dtmDueDate]				DATETIME        NOT NULL,
