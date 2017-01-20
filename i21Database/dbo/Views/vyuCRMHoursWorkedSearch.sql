5a42fff3536c1413a99fe6a0c0d2bcaad877ef01
Jayson Ramos <jayson.ramos@iRely.com>
2017-01-20 09:06:37 +0800

    CRM-347

---
+++ i21Database/dbo/Views/vyuCRMHoursWorkedSearch.sql
@@ -0,0 +1,14 @@
+﻿CREATE VIEW [dbo].[vyuCRMHoursWorkedSearch]
+	AS
+		select
+			a.*
+			,strAgentName = b.strName
+			,strCreatedBy = c.strName
+			,d.strItemNo
+			,e.strInvoiceNumber
+		from
+			tblCRMHoursWorked a
+			left join tblEMEntity b on b.intEntityId = a.intEntityId
+			left join tblEMEntity c on c.intEntityId = a.intCreatedByEntityId
+			left join tblICItem d on d.intItemId = a.intItemId
+			left join tblARInvoice e on e.intInvoiceId = a.intInvoiceId
