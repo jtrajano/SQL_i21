a8681ae570ae96ad6e5f649d32921594239e7fc7
Chakra <CBehera@irely.net>
2017-01-23 19:47:20 +0530

    IN-17

---
+++ i21Database/dbo/Stored Procedures/uspIPProcessSAPPreShipmentSample.sql
@@ -0,0 +1,111 @@
+﻿CREATE PROCEDURE [dbo].[uspIPProcessSAPPreShipmentSample]
+AS
+BEGIN TRY
+
+SET QUOTED_IDENTIFIER OFF
+SET ANSI_NULLS ON
+SET NOCOUNT ON
+SET XACT_ABORT ON
+SET ANSI_WARNINGS OFF
+
+Declare @intMinRowNo int,
+		@dtmSampleDate DATETIME,
+		@strPONo NVARCHAR(100),
+		@strPOItemNo NVARCHAR(100),
+		@strItemNo NVARCHAR(100),
+		@dblQuantity NUMERIC(38,20),
+		@strUOM NVARCHAR(50),
+		@strSampleNo NVARCHAR(100),
+		@strReferenceNo NVARCHAR(100),
+		@strStatus NVARCHAR(50),
+		@strLotNo NVARCHAR(100),
+		@ErrMsg NVARCHAR(MAX),
+		@intItemId int,
+		@intContractDetailId int,
+		@strXml NVARCHAR(MAX),
+		@strSampleNoOut NVARCHAR(100),
+		@intSampleId int,
+		@strFinalErrMsg NVARCHAR(MAX)=''
+
+Select @intMinRowNo=Min(intStageSampleId) From tblIPPreShipmentSampleStage
+
+While(@intMinRowNo is not null)
+Begin
+	BEGIN TRY
+		Select  @dtmSampleDate=dtmSampleDate,
+				@strPONo=strPONo,
+				@strPOItemNo=strPOItemNo,
+				@strItemNo=strItemNo,
+				@dblQuantity=dblQuantity,
+				@strUOM=strUOM,
+				@strSampleNo=strSampleNo,
+				@strReferenceNo=strReferenceNo,
+				@strStatus=strStatus,
+				@strLotNo=strLotNo
+		From tblIPPreShipmentSampleStage Where intStageSampleId=@intMinRowNo
+
+		Select @intItemId=intItemId From tblICItem Where strItemNo=@strItemNo
+		Select @intContractDetailId=intContractDetailId 
+		From tblCTContractDetail Where strERPPONumber=@strPONo AND intItemId=@intItemId
+
+		If ISNULL(@intItemId,0)=0 RaisError('Item not found',16,1)
+		If ISNULL(@intContractDetailId,0)=0 RaisError('Contract Sequence not found',16,1)
+
+		Set @strXml = '<root>'
+		Set @strXml += '<strSampleNumber>' + ISNULL(@strSampleNo,'') + '</strSampleNumber>'
+		Set @strXml += '<intContractDetailId>' + CONVERT(VARCHAR,@intContractDetailId) + '</intContractDetailId>'
+		Set @strXml += '<dblSampleQty>' + CONVERT(VARCHAR,@dblQuantity) + '</dblSampleQty>'
+		Set @strXml += '<strSampleUOM>' + ISNULL(@strUOM,'') + '</strSampleUOM>'
+		Set @strXml += '<strRefNo>' + ISNULL(@strReferenceNo,'') + '</strRefNo>'
+		Set @strXml += '<strSampleStatus>' + ISNULL(CASE WHEN @strStatus='ACC' THEN 'Approved' ELSE 'Rejected' END,'') + '</strSampleStatus>'
+		Set @strXml += '<dtmSampleReceivedDate>' + ISNULL(CONVERT(VARCHAR(10),@dtmSampleDate,112),'') + '</dtmSampleReceivedDate>'
+		Set @strXml += '<strSampleNote>' + ISNULL(@strLotNo,'') + '</strSampleNote>'
+		Set @strXml += '</root>'
+
+		Begin Tran
+
+		Exec uspQMSamplePreShipment @strXml,@strSampleNoOut OUT,@intSampleId OUT
+
+		--Move to Archive
+		Insert Into tblIPPreShipmentSampleArchive(dtmSampleDate,strPONo,strPOItemNo,strItemNo,dblQuantity,strUOM,strSampleNo,strReferenceNo,strStatus,strLotNo,strImportStatus,strErrorMessage,strSessionId)
+		Select dtmSampleDate,strPONo,strPOItemNo,strItemNo,dblQuantity,strUOM,strSampleNo,strReferenceNo,strStatus,strLotNo,'Success','',strSessionId
+		From tblIPPreShipmentSampleStage Where intStageSampleId=@intMinRowNo
+
+		Delete From tblIPPreShipmentSampleStage Where intStageSampleId=@intMinRowNo
+
+		Commit Tran
+
+	END TRY
+	BEGIN CATCH
+		IF XACT_STATE() != 0
+			AND @@TRANCOUNT > 0
+			ROLLBACK TRANSACTION
+
+		SET @ErrMsg = ERROR_MESSAGE()
+		SET @strFinalErrMsg = @strFinalErrMsg + @ErrMsg
+
+		--Move to Error
+		Insert Into tblIPPreShipmentSampleError(dtmSampleDate,strPONo,strPOItemNo,strItemNo,dblQuantity,strUOM,strSampleNo,strReferenceNo,strStatus,strLotNo,strImportStatus,strErrorMessage,strSessionId)
+		Select dtmSampleDate,strPONo,strPOItemNo,strItemNo,dblQuantity,strUOM,strSampleNo,strReferenceNo,strStatus,strLotNo,'Failed',@ErrMsg,strSessionId
+		From tblIPPreShipmentSampleStage Where intStageSampleId=@intMinRowNo
+
+		Delete From tblIPPreShipmentSampleStage Where intStageSampleId=@intMinRowNo
+	END CATCH
+
+	Select @intMinRowNo=Min(intStageSampleId) From tblIPPreShipmentSampleStage Where intStageSampleId>@intMinRowNo
+End
+
+If ISNULL(@strFinalErrMsg,'')<>'' RaisError(@strFinalErrMsg,16,1)
+
+END TRY
+
+BEGIN CATCH
+	SET @ErrMsg = ERROR_MESSAGE()
+
+	RAISERROR (
+			@ErrMsg
+			,16
+			,1
+			,'WITH NOWAIT'
+			)
+END CATCH
---
+++ i21Database/dbo/Stored Procedures/uspIPStageSAPPreShipmentSample.sql
@@ -0,0 +1,65 @@
+﻿CREATE PROCEDURE [dbo].[uspIPStageSAPPreShipmentSample]
+	@strXml nvarchar(max)
+AS
+
+BEGIN TRY
+	SET QUOTED_IDENTIFIER OFF
+	SET ANSI_NULLS ON
+	SET NOCOUNT ON
+	SET XACT_ABORT ON
+	SET ANSI_WARNINGS OFF
+		
+	DECLARE @idoc INT
+	DECLARE @ErrMsg nvarchar(max)
+	DECLARE @strSessionId nvarchar(50)=NEWID()
+
+	EXEC sp_xml_preparedocument @idoc OUTPUT
+	,@strXml
+
+	Insert Into tblIPPreShipmentSampleStage(dtmSampleDate,strPOItemNo,strItemNo,dblQuantity,strUOM,strSampleNo,strReferenceNo,strStatus,strLotNo,strSessionId)
+	SELECT   DATUM + ' ' + (select STUFF(STUFF(REPLICATE('0',6-LEN(ISNULL(UZEIT,''))) + convert(VARCHAR(6),ISNULL(UZEIT,'')),3,0,':'),6,0,':'))
+			,POSNR
+			,ITEMNUM
+			,QUANTITY
+			,UNIT
+			,BELNR
+			,IHREZ
+			,SSFBIN
+			,INSPECTION_LOT
+			,@strSessionId
+	FROM OPENXML(@idoc, 'QALITY02/IDOC/E1EDLIN', 2) WITH (
+			 DATUM NVARCHAR(50) '../E1EDK03/DATUM'
+			,UZEIT NVARCHAR(50) '../E1EDK03/UZEIT'
+			,POSNR NVARCHAR(100) '../E1EDK02/POSNR'
+			,ITEMNUM NVARCHAR(100)
+			,QUANTITY NUMERIC(38,20)
+			,UNIT NVARCHAR(50)
+			,BELNR NVARCHAR(100) 'E1EDP02/BELNR'
+			,IHREZ NVARCHAR(100) 'E1EDP02/IHREZ'
+			,SSFBIN NVARCHAR(100) 'E1DSIGN/SSFBIN'
+			,INSPECTION_LOT NVARCHAR(100) 'E1CCI01/INSPECTION_LOT'
+			)
+
+	Update tblIPPreShipmentSampleStage Set strPONo= 
+	(
+		Select 	BELNR			
+			FROM OPENXML(@idoc, 'QALITY02/IDOC/E1EDK02', 2) WITH (
+				BELNR NVARCHAR(100)
+			)
+	) Where strSessionId=@strSessionId
+
+END TRY
+
+BEGIN CATCH
+	SET @ErrMsg = ERROR_MESSAGE()
+
+	IF @idoc <> 0
+		EXEC sp_xml_removedocument @idoc
+
+	RAISERROR (
+			@ErrMsg
+			,16
+			,1
+			,'WITH NOWAIT'
+			)
+END CATCH
