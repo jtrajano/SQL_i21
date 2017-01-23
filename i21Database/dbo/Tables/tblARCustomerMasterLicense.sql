deebc5f77ba23ec403f79439f754bdbffa025ede
Mon Gonzales <mon.gonzales@irely.com>
2017-01-23 15:52:30 +0800

    EM-1513

---
+++ i21Database/dbo/Tables/tblARCustomerMasterLicense.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[tblARCustomerMasterLicense]
+(
+	[intCustomerMasterLicenseId] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	[intEntityCustomerId] INT NOT NULL,
+	[intLicenseTypeId] INT NOT NULL,
+	[dtmBeginDate] DATETIME NULL,
+	[dtmEndDate] DATETIME NULL,
+	[strComment] NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL,
+	[ysnAcvite] BIT DEFAULT(0),
+	[intConcurrencyId] INT NOT NULL DEFAULT(0),
+    CONSTRAINT [FK_tblARCustomerMasterLicense_tblARCustomer] FOREIGN KEY ([intEntityCustomerId]) REFERENCES [dbo].[tblARCustomer] ([intEntityCustomerId]) ON DELETE CASCADE,
+    CONSTRAINT [FK_tblARCustomerMasterLicense_tblSMLicenseType] FOREIGN KEY ([intEntityCustomerId]) REFERENCES [dbo].[tblSMLicenseType] ([intLicenseTypeId]),
+)
