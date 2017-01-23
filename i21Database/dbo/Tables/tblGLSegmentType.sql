cd7da2696916a1831cee295c82117cd0ee110d5c
jeffrey trajano <jeffrey.trajano@irely.com>
2017-01-23 10:48:00 +0800

    GL-3146 Add a line of business selection to the chart of accounts GL-3147 Data conversion - Add a data conversion for segments that are named "location" make those segment type of location.

---
+++ i21Database/dbo/Tables/tblGLSegmentType.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[tblGLSegmentType]
+(
+	[intSegmentTypeId] INT NOT NULL PRIMARY KEY, 
+    [strSegmentType] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL,
+	[intConcurrencyId] INT NOT NULL
+)
