525d517488ee2f4741ac277d9de9d337964db193
jesmar mendones <jesmar.mendones@irely.com>
2017-01-24 15:58:36 +0800

    FRM-4331

---
+++ i21Database/dbo/Tables/tblSMAlternateApproverGroup.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[tblSMAlternateApproverGroup]
+(
+	[intAlternateApproverGroupId]	INT											NOT NULL	PRIMARY KEY IDENTITY, 
+    [strAlternateApproverGroup]		NVARCHAR(50) COLLATE Latin1_General_CI_AS	NOT NULL, 
+    [intConcurrencyId]				INT											NOT NULL	DEFAULT 1, 
+    CONSTRAINT [AK_tblSMAlternateApproverGroup_strAlternateApproverGroup] UNIQUE ([strAlternateApproverGroup])
+)
