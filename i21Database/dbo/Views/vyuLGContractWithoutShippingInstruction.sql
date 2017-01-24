92da38c1057139ebadc7778f4805ff828ec9f14d
Swetosuvro Ghosh <swetosuvro.g@irely.com>
2017-01-24 19:34:10 +0530

    LG-663

--- i21Database/dbo/Views/vyuLGContractWithoutShippingInstruction.sql
+++ i21Database/dbo/Views/vyuLGContractWithoutShippingInstruction.sql
@@ -11,14 +11,14 @@ FROM tblCTContractHeader CH
 JOIN tblCTContractDetail CD ON CD.intContractHeaderId = CH.intContractHeaderId
 JOIN tblCTContractType CT ON CT.intContractTypeId = CH.intContractTypeId
 JOIN tblEMEntity Producer ON Producer.intEntityId = CH.intProducerId
-JOIN tblEMEntity Vendor ON Vendor.intEntityId = CH.intEntityId
+LEFT JOIN tblEMEntity Vendor ON Vendor.intEntityId = CH.intEntityId
 WHERE CH.intContractTypeId = 1
 	AND CD.intContractDetailId NOT IN (
-		SELECT CASE 
-				WHEN L.intPurchaseSale = 1
-					THEN LD.intPContractDetailId
-				ELSE intSContractDetailId
-				END
+		SELECT ISNULL(CASE 
+					  WHEN L.intPurchaseSale = 1
+						 THEN LD.intPContractDetailId
+					  ELSE intSContractDetailId
+					  END,0)
 		FROM tblLGLoadDetail LD
 		JOIN tblLGLoad L ON L.intLoadId = LD.intLoadId
 		WHERE L.intShipmentType = 2
