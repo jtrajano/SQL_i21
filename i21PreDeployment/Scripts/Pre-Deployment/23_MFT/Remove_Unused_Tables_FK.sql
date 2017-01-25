addafbd879c0d5cb9dc936bba680f0591dc102fc
Mark Paragas <mark.paragas@irely.com>
2017-01-25 22:41:56 +0800

    MFT-418

--- i21PreDeployment/Scripts/Pre-Deployment/23_MFT/Remove_Unused_Tables_FK.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/23_MFT/Remove_Unused_Tables_FK.sql
@@ -3,14 +3,19 @@ PRINT 'MFT Remove FK of Obsolete table/s'
 GO
 	IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = N'tblTFTaxCriteria')
 		BEGIN
-			IF EXISTS(SELECT 1 FROM sys.foreign_keys WHERE parent_object_id = OBJECT_ID(N'dbo.tblTFTaxCriteria'))
-			 BEGIN 
-					ALTER TABLE tblTFTaxCriteria
-					DROP CONSTRAINT FK_tblTFTaxCriteria_tblTFReportingComponent
-					PRINT 'Foreign Key Removed'
+			IF EXISTS(SELECT TOP 1 * FROM INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS WHERE CONSTRAINT_NAME ='FK_tblTFTaxCriteria_tblTFReportingComponent')
+			 BEGIN
+				ALTER TABLE tblTFTaxCriteria
+				DROP CONSTRAINT FK_tblTFTaxCriteria_tblTFReportingComponent
+				PRINT 'tblTFTaxCriteria Foreign Key Removed'
 			END 
+			ELSE
+				BEGIN
+					PRINT 'tblTFTaxCriteria Foreign Key does not exist'
+				END
 		END
 	ELSE
 		BEGIN
 			PRINT 'TABLE tblTFTaxCriteria does NOT EXIST!'
 		END
+
