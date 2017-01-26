fef29c8caade605b8307c1d16d89a4b2a1dea161
jesmar mendones <jesmar.mendones@irely.com>
2017-01-26 13:11:06 +0800

    FRM-4331

---
+++ i21PreDeployment/Scripts/Pre-Deployment/4_SM/9_DropTblSMAlternateApproverGroup.sql
@@ -0,0 +1,7 @@
+﻿
+PRINT 'BEGIN Drop of table tblSMAlternateApproverGroup.'
+PRINT 'The table tblSMAlternateApproverGroup is going to be re-created as tblSMApproverGroup'
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[tblSMAlternateApproverGroup]'))
+	DROP TABLE [dbo].[tblSMAlternateApproverGroup]
+GO
+PRINT 'END Drop of table tblSMAlternateApproverGroup'
