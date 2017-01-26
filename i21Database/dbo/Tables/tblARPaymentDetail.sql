35de4359e36d92b1ffd8a4435025650a78a57683
Ruben Escarcha <ruben.escarcha@irely.com>
2017-01-27 05:19:39 +0800

    AR-3999 Pull Weight Claim transaction from AP

--- i21Database/dbo/Tables/tblARPaymentDetail.sql
+++ i21Database/dbo/Tables/tblARPaymentDetail.sql
@@ -1,7 +1,8 @@
 ﻿CREATE TABLE [dbo].[tblARPaymentDetail] (
     [intPaymentDetailId]		INT             IDENTITY (1, 1) NOT NULL,
     [intPaymentId]				INT             NOT NULL,
-    [intInvoiceId]				INT             NOT NULL,
+    [intInvoiceId]				INT             NULL,
+	[intBillId]					INT             NULL,
 	[intTermId]					INT             NULL,
     [intAccountId]				INT             NOT NULL,
     [dblInvoiceTotal]			NUMERIC (18, 6) NULL,
@@ -14,7 +15,9 @@
     [intConcurrencyId]			INT             NOT NULL,
     CONSTRAINT [PK_tblARPaymentDetail_intPaymentDetailId] PRIMARY KEY CLUSTERED ([intPaymentDetailId] ASC),
     CONSTRAINT [FK_tblARPaymentDetail_tblARPayment_intPaymentId] FOREIGN KEY ([intPaymentId]) REFERENCES [dbo].[tblARPayment] ([intPaymentId]) ON DELETE CASCADE,
-	CONSTRAINT [FK_tblARPaymentDetail_tblGLAccount_intAccountId] FOREIGN KEY ([intAccountId]) REFERENCES [dbo].[tblGLAccount] ([intAccountId])
+	CONSTRAINT [FK_tblARPaymentDetail_tblGLAccount_intAccountId] FOREIGN KEY ([intAccountId]) REFERENCES [dbo].[tblGLAccount] ([intAccountId]),
+	CONSTRAINT [FK_tblARPaymentDetail_tblARInvoice_intInvoiceId] FOREIGN KEY ([intInvoiceId]) REFERENCES [dbo].[tblARInvoice] ([intInvoiceId]),
+	CONSTRAINT [FK_tblARPaymentDetail_tblAPBill_intBillId] FOREIGN KEY ([intBillId]) REFERENCES [dbo].[tblAPBill] ([intBillId])
 );
 
 
---
+++ i21Database/dbo/Views/vyuARPaymentDetail.sql
@@ -0,0 +1,109 @@
+﻿CREATE VIEW [dbo].[vyuARPaymentDetail]
+AS
+SELECT 
+	 [intTransactionId]			= ARI.[intInvoiceId]
+	,[strTransactionNumber]		= ARI.[strInvoiceNumber]
+	,[intInvoiceId]				= ARI.[intInvoiceId]
+	,[strInvoiceNumber]			= ARI.[strInvoiceNumber]
+	,[intBillId]				= NULL
+	,[strBillId]				= ''
+	,[strTransactionType]		= ARI.[strTransactionType]
+	,[strType]					= ARI.[strType]
+	,[intEntityId]				= ARI.[intEntityCustomerId]
+	,[intCompanyLocationId]		= ARI.[intCompanyLocationId]
+	,[intAccountId]				= ARI.[intAccountId]
+	,[intCurrencyId]			= ARI.[intCurrencyId]
+	,[intTermId]				= ARI.[intTermId]
+	,[strTerm]					= SMT.[strTerm]
+	,[dtmDate]					= ARI.[dtmDate]
+	,[dtmDueDate]				= ARI.[dtmDueDate]
+	,[dtmPostDate]				= ARI.[dtmPostDate]
+	,[dblInvoiceTotal]			= ARI.[dblInvoiceTotal]
+	,[dblDiscount]				= ARI.[dblDiscount]
+	,[dblDiscountAvailable]		= ARI.[dblDiscountAvailable]
+	,[dblInterest]				= ARI.[dblInterest]
+	,[dblAmountDue]				= ARI.[dblAmountDue]
+	,[dblPayment]				= ARI.[dblPayment]
+	,[ysnPosted]				= ARI.[ysnPosted]
+	,[ysnPaid]					= ARI.[ysnPaid]
+	,[intPaymentId]				= ARI.[intPaymentId]
+	,[dblTotalTermDiscount]		= ARI.[dblTotalTermDiscount]
+	,[strInvoiceReportNumber]	= CFT.strInvoiceReportNumber
+	,[strTicketNumbers]			= dbo.fnARGetScaleTicketNumbersFromInvoice(ARI.intInvoiceId)
+	,[strCustomerReferences]	= dbo.fnARGetCustomerReferencesFromInvoice(ARI.intInvoiceId)
+FROM
+	[tblARInvoice] ARI
+LEFT OUTER JOIN 
+	(
+	SELECT
+		 [intInvoiceId]
+		,[strInvoiceReportNumber]
+	FROM
+		tblCFTransaction
+	) CFT 
+		ON ARI.[intInvoiceId] = CFT.[intInvoiceId]
+LEFT OUTER JOIN 
+	(
+	SELECT
+		 [intTermID]
+		,[strTerm]
+	FROM
+		tblSMTerm
+	) SMT
+		ON ARI.[intTermId] = SMT.[intTermID]		
+WHERE
+	[ysnPosted] = 1
+	
+	
+UNION ALL
+
+
+SELECT 
+	 [intTransactionId]			= APB.[intBillId]
+	,[strTransactionNumber]		= APB.[strBillId]
+	,[intInvoiceId]				= NULL
+	,[strInvoiceNumber]			= ''
+	,[intBillId]				= APB.[intBillId]
+	,[strBillId]				= APB.[strBillId]
+	,[strTransactionType]		= (CASE WHEN APB.[intTransactionType] = 11 THEN 'Weight Claim' WHEN APB.[intTransactionType] = 3 THEN 'Debit Memo' ELSE '' END)
+	,[strType]					= 'Voucher'
+	,[intEntityId]				= APB.[intEntityVendorId]
+	,[intCompanyLocationId]		= APB.intShipToId
+	,[intAccountId]				= APB.[intAccountId]
+	,[intCurrencyId]			= APB.[intCurrencyId]
+	,[intTermId]				= APB.[intTermsId]
+	,[strTerm]					= SMT.[strTerm]
+	,[dtmDate]					= APB.[dtmDate]
+	,[dtmDueDate]				= APB.[dtmDueDate]
+	,[dtmPostDate]				= APB.[dtmBillDate]
+	,[dblInvoiceTotal]			= APB.[dblTotal]
+	,[dblDiscount]				= APB.[dblDiscount]
+	,[dblDiscountAvailable]		= CAST(0 AS DECIMAL(18,6))
+	,[dblInterest]				= APB.[dblInterest]
+	,[dblAmountDue]				= APB.[dblAmountDue]
+	,[dblPayment]				= APB.[dblPayment]
+	,[ysnPosted]				= APB.[ysnPosted]
+	,[ysnPaid]					= APB.[ysnPaid]
+	,[intPaymentId]				= NULL
+	,[dblTotalTermDiscount]		= CAST(0 AS DECIMAL(18,6))
+	,[strInvoiceReportNumber]	= ''
+	,[strTicketNumbers]			= ''
+	,[strCustomerReferences]	= ''
+FROM
+	tblAPBill APB
+INNER JOIN
+	tblEMEntityType EMET
+		ON APB.[intEntityVendorId] = EMET.[intEntityId]
+		AND EMET.[strType] = 'Customer'
+LEFT OUTER JOIN 
+	(
+	SELECT
+		 [intTermID]
+		,[strTerm]
+	FROM
+		tblSMTerm
+	) SMT
+		ON APB.[intTermsId] = SMT.[intTermID]		
+WHERE
+	[intTransactionType] IN (11,3)
+	AND [ysnPosted] = 1
