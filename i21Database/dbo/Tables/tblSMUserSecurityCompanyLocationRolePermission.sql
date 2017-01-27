2d3131326b93bc9003e9ea5fab4a1083584bf671
Mon Gonzales <mon.gonzales@irely.com>
2017-01-27 11:58:39 +0800

    EM-1689

--- i21Database/dbo/Tables/tblSMUserSecurityCompanyLocationRolePermission.sql
+++ i21Database/dbo/Tables/tblSMUserSecurityCompanyLocationRolePermission.sql
@@ -11,3 +11,31 @@
 	CONSTRAINT [FK_tblSMUserSecurityCompanyLocationRolePermission_tblSMCompanyLocation] FOREIGN KEY ([intCompanyLocationId]) REFERENCES [tblSMCompanyLocation]([intCompanyLocationId]) ON DELETE CASCADE, 
     CONSTRAINT [AK_tblSMUserSecurityCompanyLocationRolePermission_Column] UNIQUE ([intEntityUserSecurityId], [intUserRoleId], [intCompanyLocationId])
 )
+
+
+GO
+
+CREATE TRIGGER [dbo].[trgAfterInsertUserSecurityCompanyLocationRolePermission]
+    ON [dbo].[tblSMUserSecurityCompanyLocationRolePermission]
+    AFTER INSERT
+    AS
+    declare
+		@newId  int
+		
+	begin transaction;
+
+		begin try			
+			select
+				@newId = i.intUserSecurityCompanyLocationRolePermissionId
+			from
+				inserted i;
+			
+			update tblSMUserSecurityCompanyLocationRolePermission set intEntityId = intEntityUserSecurityId where intUserSecurityCompanyLocationRolePermissionId = @newId;
+
+		end try
+		begin catch
+			rollback transaction;
+		end catch
+
+	commit transaction;
+GO
