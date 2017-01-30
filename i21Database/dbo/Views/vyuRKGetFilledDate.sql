b9517104f849b7074ce40b89ed9c8b198ebcb9ba
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2017-01-30 12:14:10 +0530

    RM-610

---
+++ i21Database/dbo/Views/vyuRKGetFilledDate.sql
@@ -0,0 +1,7 @@
+﻿CREATE VIEW vyuRKGetFilledDate
+AS
+SELECT CONVERT(INT,ROW_NUMBER() OVER(ORDER BY CONVERT(VARCHAR(10),dtmFilledDate,110))) intRowNum,CONVERT(VARCHAR(11),dtmFilledDate,106) dtmFilledDate from 
+(	SELECT DISTINCT  convert(datetime,CONVERT(VARCHAR(10),dtmFilledDate,110)) dtmFilledDate FROM  tblRKFutOptTransaction 
+	WHERE CONVERT(VARCHAR(10),dtmFilledDate,110) NOT IN(SELECT CONVERT(VARCHAR(10),dtmFilledDate,110) FROM tblRKReconciliationBrokerStatementHeader 
+															WHERE isnull(ysnFreezed,0) = 1) 
+)t
