8605774482b7532c8c6d15a44e97c1ceccac3961
Mon Gonzales <mon.gonzales@irely.com>
2017-01-30 13:48:44 +0800

    EM-1658

---
+++ i21Database/dbo/Functions/fnEMGetEntityCompetitors.sql
@@ -0,0 +1,18 @@
+﻿CREATE FUNCTION [dbo].[fnEMGetEntityCompetitors]
+(
+	@intEntityId		int
+)
+RETURNS NVARCHAR(MAX)
+BEGIN
+	DECLARE @col NVARCHAR(MAX);	
+	select @col = COALESCE(@col + ', ', '') + RTRIM(LTRIM(b.strName)) 
+		from tblARCustomerCompetitor a 
+			join vyuEMSearchEntityCompetitor b
+				on a.intEntityId = b.intEntityId
+	where a.intEntityCustomerId= @intEntityId
+	RETURN @col
+END
+
+
+
+
--- i21Database/dbo/Views/vyuEMSearchEntitySalesEntityContact.sql
+++ i21Database/dbo/Views/vyuEMSearchEntitySalesEntityContact.sql
@@ -17,30 +17,48 @@ select
 			case when Prospect = 1 then 'Prospect, ' else '' end,
 		intTicketIdDate = (select top 1 cast(intTicketId as nvarchar) + '|^|' + CONVERT(nvarchar(24),dtmCreated,101) + '|^|' + strTicketNumber from tblHDTicket where intCustomerId = a.intEntityId order by dtmCreated DESC),
 		
-		
-		g.strSuffix,
-		g.strTitle,
-		g.strNickName,
+		--Contact Information
+		strContactSuffix = g.strSuffix,
+		strContactTitle = g.strTitle,
+		strContactNickName = g.strNickName,
+		strContactEmail = g.strEmail,
+		strContactPhone = h.strPhone,
+		strContactMobile = i.strPhone,
+		--Contact Location
+		strContactLocationName = j.strLocationName,
+		strContactLocationAddress = j.strAddress,
+		strContactLocationZip = j.strZipCode,
+		strContactLocationState = j.strState,
+		strContactLocationCountry = j.strCountry,
+		strContactLocationCity = j.strCity,
+		strContactLocationTimezone = j.strTimezone,
+		strContactMethod = g.strContactMethod,
+		strContactDept = g.strDepartment,
+		strContactEmailDistribution = g.strEmailDistributionOption,
+		strContactType = g.strContactType,
+		--
+		strEntityLocationName = m.strLocationName,
+		strEntityLocationAddress = m.strAddress,
+		strEntityLocationZip = m.strZipCode,
+		strEntityLocationState = m.strState,
+		strEntityLocationCountry = m.strCountry,
+		strEntityLocationCity = m.strCity,
+
+
 		strMobile = i.strPhone,
-		j.strLocationName,
-		j.strAddress,
-		j.strCity,
-		j.strState,
-		j.strZipCode,
-		j.strCountry,
-		j.strTimezone,
-		g.strContactMethod,
 		f.ysnPortalAccess,
 		
 		strEntityAssociation = l.strName,
-		strContactLineOfBusiness = dbo.fnEMGetEntityLineOfBusiness(g.intEntityId),
+		strContactLineOfBusiness = dbo.fnEMGetEntityLineOfBusiness(g.intEntityId),		
 		strContactAreaOfInterest = dbo.fnEMGetEntityAreaOfInterest(g.intEntityId),
+		strEntityCurrentSystem = dbo.fnEMGetEntityCompetitors(a.intEntityId),
 		k.ysnOutOfAdvertising,
 		k.dtmOutDate
     FROM         
             tblEMEntity a
         join vyuEMEntityType b
             on b.intEntityId = a.intEntityId --and b.strType IN ('Buyer')        
+		
         left join [tblEMEntityToContact] f  
             on f.intEntityId = a.intEntityId --and f.ysnDefaultContact = 1  
         left join tblEMEntity g  
@@ -55,5 +73,7 @@ select
 			on g.intEntityId = k.intEntityId
 		left join tblEMEntity l
 			on k.intEntityId = l.intEntityId
+		left join tblEMEntityLocation m
+			on m.intEntityId = a.intEntityId and m.ysnDefaultLocation = 1
 
 	where Vendor = 1 or Customer = 1 or Competitor = 1 or [Partner] = 1 or Prospect = 1
