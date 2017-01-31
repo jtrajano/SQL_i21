ac2c716bdd79f39a3bb4d42f7b251f78e80d868c
Vinoth Kumar T <vinoth.t@irely.com>
2017-01-31 20:26:15 +0530

    QC-249

---
+++ i21Database/dbo/Tables/tblQMSampleTypeUserRole.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[tblQMSampleTypeUserRole]
+(
+	[intSampleTypeUserRoleId] INT NOT NULL IDENTITY, 
+	[intSampleTypeId] INT NOT NULL, 
+	[intUserRoleID] INT NOT NULL,
+	[intConcurrencyId] INT NULL CONSTRAINT [DF_tblQMSampleTypeUserRole_intConcurrencyId] DEFAULT 0,
+	
+	[intCreatedUserId] [int] NULL,
+	[dtmCreated] [datetime] NULL CONSTRAINT [DF_tblQMSampleTypeUserRole_dtmCreated] DEFAULT GetDate(),
+	[intLastModifiedUserId] [int] NULL,
+	[dtmLastModified] [datetime] NULL CONSTRAINT [DF_tblQMSampleTypeUserRole_dtmLastModified] DEFAULT GetDate(),
+		
+	CONSTRAINT [PK_tblQMSampleTypeUserRole] PRIMARY KEY ([intSampleTypeUserRoleId]), 
+	CONSTRAINT [FK_tblQMSampleTypeUserRole_tblQMSampleType] FOREIGN KEY ([intSampleTypeId]) REFERENCES [tblQMSampleType]([intSampleTypeId]) ON DELETE CASCADE, 
+	CONSTRAINT [FK_tblQMSampleTypeUserRole_tblSMUserRole] FOREIGN KEY ([intUserRoleID]) REFERENCES [tblSMUserRole]([intUserRoleID]) 
+)
