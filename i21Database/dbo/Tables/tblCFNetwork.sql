8323039d207d5966ff329c47ab869182637c00df
bonn ponce <bonn.ponce@irely.com>
2017-02-01 01:20:16 +0800

    CF-911 Network > Add Voyager Options

--- i21Database/dbo/Tables/tblCFNetwork.sql
+++ i21Database/dbo/Tables/tblCFNetwork.sql
@@ -47,6 +47,7 @@
     [strDecryptKeyFilePath]            NVARCHAR (MAX)  COLLATE Latin1_General_CI_AS NULL,
     [strDecryptPassphrase]             NVARCHAR (MAX)  COLLATE Latin1_General_CI_AS NULL,
     [intConcurrencyId]                 INT             CONSTRAINT [DF_tblCFNetwork_intConcurrencyId] DEFAULT ((1)) NULL,
+    [strDownloadFileName]			   NVARCHAR (MAX)  COLLATE Latin1_General_CI_AS NULL,
     CONSTRAINT [PK_tblCFNetwork] PRIMARY KEY CLUSTERED ([intNetworkId] ASC),
     CONSTRAINT [FK_tblCFNetwork_tblARCustomer] FOREIGN KEY ([intCustomerId]) REFERENCES [dbo].[tblARCustomer] ([intEntityCustomerId]),
     CONSTRAINT [FK_tblCFNetwork_tblSMCompanyLocation] FOREIGN KEY ([intLocationId]) REFERENCES [dbo].[tblSMCompanyLocation] ([intCompanyLocationId]),
