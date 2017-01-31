9fb1d0adef5af96cb71b71adc838e08ed345c8de
Gopinath Nayak <gopinath.n@irely.com>
2017-01-31 17:37:31 +0530

    CT-908

---
+++ i21Database/dbo/Views/vyuCTGetNeedPlanWeek.sql
@@ -0,0 +1,11 @@
+﻿CREATE VIEW [dbo].[vyuCTGetNeedPlanWeek]
+AS
+SELECT CONVERT(INT,ROW_NUMBER() OVER(ORDER BY (SELECT 1))) AS intNeedPlanId,strNeedPlan 
+FROM 
+(
+	SELECT DISTINCT CASE WHEN intWeek < 10 THEN '0'+LTRIM(intWeek) 
+							   ELSE LTRIM(intWeek)
+						  END 
+	+'-'+LTRIM(intYear) AS strNeedPlan
+	FROM tblRKStgBlendDemand
+)t
