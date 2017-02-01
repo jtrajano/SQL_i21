cfc6b8218c51f8f85996f2b4258cbcce754fb543
Smith Wilson de Jesus <smith.dejesus@irely.com>
2017-02-01 15:36:00 +0800

    FRM-4411

---
+++ i21Database/dbo/Stored Procedures/uspAESDecryptASym.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[uspAESDecryptASym]
+  @encryptedText VARCHAR(MAX),
+  @decryptedText VARCHAR(MAX) = NULL OUTPUT 
+AS
+BEGIN
+
+	SELECT @decryptedText = dbo.fnAESDecrypt(@encryptedText)
+
+    RETURN
+
+END
