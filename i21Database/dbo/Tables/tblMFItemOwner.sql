f7a5e905e3f41a5eb7520d808ea676a0ca3d3ddf
Vinoth Kumar T <vinoth.t@irely.com>
2017-02-02 20:47:35 +0530

    MFG-2301

--- i21Database/dbo/Tables/tblMFItemOwner.sql
+++ i21Database/dbo/Tables/tblMFItemOwner.sql
@@ -1,9 +1,17 @@
 ﻿CREATE TABLE tblMFItemOwner (
-	intItemOwnerId INT NOT NULL identity(1, 1)
-	,intOwnerId INT NOT NULL
+	intItemOwnerId INT NOT NULL IDENTITY(1, 1)
+	,intConcurrencyId INT NULL CONSTRAINT [DF_tblMFItemOwner_intConcurrencyId] DEFAULT 0
 	,intItemId INT NOT NULL
+	,intOwnerId INT NOT NULL
 	,intReceivedLife INT
+
+	,intCreatedUserId [int] NULL
+	,dtmCreated [datetime] NULL CONSTRAINT [DF_tblMFItemOwner_dtmCreated] DEFAULT GetDate()
+	,intLastModifiedUserId [int] NULL
+	,dtmLastModified [datetime] NULL CONSTRAINT [DF_tblMFItemOwner_dtmLastModified] DEFAULT GetDate()
+
 	,CONSTRAINT PK_tblMFItemOwner PRIMARY KEY (intItemOwnerId)
+	,CONSTRAINT [AK_tblMFItemOwner_intItemId_intOwnerId] UNIQUE ([intItemId],[intOwnerId])
 	,CONSTRAINT FK_tblMFItemOwner_tblICItem FOREIGN KEY (intItemId) REFERENCES tblICItem(intItemId)
 	,CONSTRAINT FK_tblMFItemOwner_tblEMEntity FOREIGN KEY (intOwnerId) REFERENCES tblEMEntity(intEntityId)
 	)
