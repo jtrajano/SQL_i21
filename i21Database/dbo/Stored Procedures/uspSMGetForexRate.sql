cf4fe29f14b12323c527f79747dbdab3feb3a294
Ruben Escarcha <ruben.escarcha@irely.com>
2017-02-04 06:08:17 +0800

    SM-2538

---
+++ i21Database/dbo/Stored Procedures/uspSMGetForexRate.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[uspSMGetForexRate]
+	 @TransactionDate	DATETIME
+	,@CurrencyId		INT
+	,@ForexRateTypeId	INT
+	,@ForexRate			NUMERIC(18,6)	= 1 OUTPUT
+	,@ForexRateDetailId	INT				= NULL OUTPUT
+AS	
+
+SELECT 
+	@ForexRate			=	[dblRate]
+	,@ForexRateDetailId	=	[intCurrencyExchangeRateDetailId] 
+FROM 
+	[vyuSMForex] 
+WHERE 
+	[intFromCurrencyId] = @CurrencyId 
+	AND [intCurrencyExchangeRateTypeId] = @ForexRateTypeId 
+	AND [intFunctionalCurrencyId] = [intToCurrencyId] 
+	AND CAST(@TransactionDate AS DATE) > CAST([dtmValidFromDate] AS DATE) 
+ORDER BY
+	[dtmValidFromDate]
