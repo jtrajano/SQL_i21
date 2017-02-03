2e9653e531089660c3ffa884833887b9861ae95a
kenneth lagunero <kenneth.lagunero@irely.com>
2017-02-03 23:46:28 +0800

    TM-1943

---
+++ i21Database/dbo/Views/vyuTMDegreeReadingSeasonYear.sql
@@ -0,0 +1,12 @@
+﻿CREATE VIEW [dbo].[vyuTMDegreeReadingSeasonYear]
+AS  
+	SELECT 
+		A.dblAccumulatedDegreeDay
+		,A.intDegreeDays
+		,A.dtmDate
+		,intClockId = A.intClockID
+		,intSeasonYear = (SELECT TOP 1 intSeasonYear FROM dbo.fnTMGetSeasonYear(A.dtmDate,A.intClockID))	
+		,intDegreeDayReadingId = A.intDegreeDayReadingID
+		,intConcurrencyId = 0
+	FROM tblTMDegreeDayReading A
+GO
