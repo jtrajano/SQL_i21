377f678cf192d7c7e64ddb41861ef77649e9b273
Swetosuvro Ghosh <swetosuvro.g@irely.com>
2017-02-05 03:18:09 +0530

    LG-784

---
+++ i21Database/dbo/Stored Procedures/uspLGLoadContractCopy.sql
@@ -0,0 +1,298 @@
+﻿CREATE PROCEDURE uspLGLoadContractCopy @intOldLoadDetailId INT
+	,@intNewContractDetailId INT
+	,@dblNewLoadDetailQuantity NUMERIC(18, 6)
+	,@intNewLoadDetailItemUOMId INT
+	,@intUserId INT
+AS
+BEGIN TRY
+
+	DECLARE @strErrMsg NVARCHAR(MAX)
+	DECLARE @strLoadSINumber NVARCHAR(MAX)
+	DECLARE @intNewLoadId INT
+	DECLARE @intOldLoadId INT
+
+	SELECT @intOldLoadId = intLoadId
+	FROM tblLGLoadDetail
+	WHERE intLoadDetailId = @intOldLoadDetailId
+
+	EXEC uspSMGetStartingNumber 106
+		,@strLoadSINumber OUTPUT
+		,NULL
+
+	INSERT INTO tblLGLoad (
+		intConcurrencyId
+		,strLoadNumber
+		,intCompanyLocationId
+		,intPurchaseSale
+		,dtmScheduledDate
+		,strCustomerReference
+		,strBookingReference
+		,intEquipmentTypeId
+		,intEntityId
+		,intEntityLocationId
+		,strComments
+		,intHaulerEntityId
+		,intTicketId
+		,ysnInProgress
+		,dblDeliveredQuantity
+		,dtmDeliveredDate
+		,intGenerateLoadId
+		,intGenerateSequence
+		,strTruckNo
+		,strTrailerNo1
+		,strTrailerNo2
+		,strTrailerNo3
+		,intUserSecurityId
+		,strExternalLoadNumber
+		,intTransportLoadId
+		,intDriverEntityId
+		,ysnDispatched
+		,dtmDispatchedDate
+		,intDispatcherId
+		,ysnDispatchMailSent
+		,dtmDispatchMailSent
+		,dtmCancelDispatchMailSent
+		,intLoadHeaderId
+		,intSourceType
+		,intPositionId
+		,intWeightUnitMeasureId
+		,strBLNumber
+		,dtmBLDate
+		,strOriginPort
+		,strDestinationPort
+		,strDestinationCity
+		,intTerminalEntityId
+		,intShippingLineEntityId
+		,strServiceContractNumber
+		,strPackingDescription
+		,strMVessel
+		,strMVoyageNumber
+		,strFVessel
+		,strFVoyageNumber
+		,intForwardingAgentEntityId
+		,strForwardingAgentRef
+		,intInsurerEntityId
+		,dblInsuranceValue
+		,intInsuranceCurrencyId
+		,dtmDocsToBroker
+		,strMarks
+		,strMarkingInstructions
+		,strShippingMode
+		,intNumberOfContainers
+		,intContainerTypeId
+		,intBLDraftToBeSentId
+		,strBLDraftToBeSentType
+		,strDocPresentationType
+		,intDocPresentationId
+		,dtmDocsReceivedDate
+		,dtmETAPOL
+		,dtmETSPOL
+		,dtmETAPOD
+		,dtmDeadlineCargo
+		,dtmDeadlineBL
+		,dtmISFReceivedDate
+		,dtmISFFiledDate
+		,dtmStuffingDate
+		,dblDemurrage
+		,intDemurrageCurrencyId
+		,dblDespatch
+		,intDespatchCurrencyId
+		,dblLoadingRate
+		,intLoadingUnitMeasureId
+		,strLoadingPerUnit
+		,dblDischargeRate
+		,intDischargeUnitMeasureId
+		,strDischargePerUnit
+		,intTransportationMode
+		,intShipmentStatus
+		,ysnPosted
+		,dtmPostedDate
+		,intTransUsedBy
+		,intShipmentType
+		,intLoadShippingInstructionId
+		,strExternalShipmentNumber
+		)
+	SELECT 1
+		,@strLoadSINumber
+		,intCompanyLocationId
+		,intPurchaseSale
+		,dtmScheduledDate
+		,strCustomerReference
+		,strBookingReference
+		,intEquipmentTypeId
+		,intEntityId
+		,intEntityLocationId
+		,strComments
+		,intHaulerEntityId
+		,intTicketId
+		,ysnInProgress
+		,dblDeliveredQuantity
+		,dtmDeliveredDate
+		,intGenerateLoadId
+		,intGenerateSequence
+		,strTruckNo
+		,strTrailerNo1
+		,strTrailerNo2
+		,strTrailerNo3
+		,intUserSecurityId
+		,strExternalLoadNumber
+		,intTransportLoadId
+		,intDriverEntityId
+		,ysnDispatched
+		,dtmDispatchedDate
+		,intDispatcherId
+		,ysnDispatchMailSent
+		,dtmDispatchMailSent
+		,dtmCancelDispatchMailSent
+		,intLoadHeaderId
+		,intSourceType
+		,intPositionId
+		,intWeightUnitMeasureId
+		,strBLNumber
+		,dtmBLDate
+		,strOriginPort
+		,strDestinationPort
+		,strDestinationCity
+		,intTerminalEntityId
+		,intShippingLineEntityId
+		,strServiceContractNumber
+		,strPackingDescription
+		,strMVessel
+		,strMVoyageNumber
+		,strFVessel
+		,strFVoyageNumber
+		,intForwardingAgentEntityId
+		,strForwardingAgentRef
+		,intInsurerEntityId
+		,dblInsuranceValue
+		,intInsuranceCurrencyId
+		,dtmDocsToBroker
+		,strMarks
+		,strMarkingInstructions
+		,strShippingMode
+		,intNumberOfContainers
+		,intContainerTypeId
+		,intBLDraftToBeSentId
+		,strBLDraftToBeSentType
+		,strDocPresentationType
+		,intDocPresentationId
+		,dtmDocsReceivedDate
+		,dtmETAPOL
+		,dtmETSPOL
+		,dtmETAPOD
+		,dtmDeadlineCargo
+		,dtmDeadlineBL
+		,dtmISFReceivedDate
+		,dtmISFFiledDate
+		,dtmStuffingDate
+		,dblDemurrage
+		,intDemurrageCurrencyId
+		,dblDespatch
+		,intDespatchCurrencyId
+		,dblLoadingRate
+		,intLoadingUnitMeasureId
+		,strLoadingPerUnit
+		,dblDischargeRate
+		,intDischargeUnitMeasureId
+		,strDischargePerUnit
+		,intTransportationMode
+		,intShipmentStatus
+		,ysnPosted
+		,dtmPostedDate
+		,intTransUsedBy
+		,intShipmentType
+		,intLoadShippingInstructionId
+		,strExternalShipmentNumber
+	FROM tblLGLoad
+	WHERE intLoadId = @intOldLoadId
+
+	SELECT @intNewLoadId = SCOPE_IDENTITY()
+
+	INSERT INTO tblLGLoadDetail (
+		intConcurrencyId
+		,intLoadId
+		,intVendorEntityId
+		,intVendorEntityLocationId
+		,intCustomerEntityId
+		,intCustomerEntityLocationId
+		,intItemId
+		,intPContractDetailId
+		,intSContractDetailId
+		,intPCompanyLocationId
+		,intSCompanyLocationId
+		,dblQuantity
+		,intItemUOMId
+		,dblGross
+		,dblTare
+		,dblNet
+		,intWeightItemUOMId
+		,dblDeliveredQuantity
+		,dblDeliveredGross
+		,dblDeliveredTare
+		,dblDeliveredNet
+		,strLotAlias
+		,strSupplierLotNumber
+		,dtmProductionDate
+		,strScheduleInfoMsg
+		,ysnUpdateScheduleInfo
+		,ysnPrintScheduleInfo
+		,strLoadDirectionMsg
+		,ysnUpdateLoadDirections
+		,ysnPrintLoadDirections
+		,strVendorReference
+		,strCustomerReference
+		,intAllocationDetailId
+		,intPickLotDetailId
+		,intPSubLocationId
+		,intSSubLocationId
+		,strExternalShipmentItemNumber
+		,strExternalBatchNo
+		)
+	SELECT 1
+		,@intNewLoadId
+		,intVendorEntityId
+		,intVendorEntityLocationId
+		,intCustomerEntityId
+		,intCustomerEntityLocationId
+		,intItemId
+		,intPContractDetailId
+		,intSContractDetailId
+		,intPCompanyLocationId
+		,intSCompanyLocationId
+		,@dblNewLoadDetailQuantity
+		,intItemUOMId
+		,dbo.fnCTConvertQtyToTargetItemUOM(intItemId,intWeightItemUOMId,@dblNewLoadDetailQuantity)
+		,0
+		,dbo.fnCTConvertQtyToTargetItemUOM(intItemId,intWeightItemUOMId,@dblNewLoadDetailQuantity)
+		,intWeightItemUOMId
+		,dblDeliveredQuantity
+		,dblDeliveredGross
+		,dblDeliveredTare
+		,dblDeliveredNet
+		,strLotAlias
+		,strSupplierLotNumber
+		,dtmProductionDate
+		,strScheduleInfoMsg
+		,ysnUpdateScheduleInfo
+		,ysnPrintScheduleInfo
+		,strLoadDirectionMsg
+		,ysnUpdateLoadDirections
+		,ysnPrintLoadDirections
+		,strVendorReference
+		,strCustomerReference
+		,intAllocationDetailId
+		,intPickLotDetailId
+		,intPSubLocationId
+		,intSSubLocationId
+		,strExternalShipmentItemNumber
+		,strExternalBatchNo
+	FROM tblLGLoadDetail
+	WHERE intLoadDetailId = @intOldLoadDetailId
+
+END TRY
+
+BEGIN CATCH
+	SET @strErrMsg = ERROR_MESSAGE()
+
+	RAISERROR (@strErrMsg,16,1,'WITH NOWAIT'			)
+END CATCH
---
+++ i21Database/dbo/Stored Procedures/uspLGLoadContractSlice.sql
@@ -0,0 +1,168 @@
+﻿CREATE PROCEDURE uspLGLoadContractSlice 
+	@intContractHeaderId INT
+AS
+BEGIN TRY
+	DECLARE @strErrMsg NVARCHAR(MAX)
+	DECLARE @strLoadNumber NVARCHAR(MAX)
+	DECLARE @intLoadId INT 
+	DECLARE @intParentContractDetailId INT
+	DECLARE @dblOrgContractDetailQty NUMERIC(18,6)
+	DECLARE @intOrgContractDetailQtyUOM INT
+	DECLARE @intOrgLoadId INT
+	DECLARE @intOrgLoadIDetaild INT
+	DECLARE @dblOrgLoadDetailQty NUMERIC(18,6)
+	DECLARE @intOrgLoadDetailItemUOM INT
+	DECLARE @intOrgLoadDetailWeightUOM INT
+	DECLARE @intMinLoadRecordId INT
+	DECLARE @intCRowNo INT
+	DECLARE @intCContractDetailId INT
+	DECLARE @intCParentDetailId INT
+	DECLARE @dblCQuantity NUMERIC(18, 6)
+	DECLARE @intCItemUOMId INT
+	DECLARE @intUserId INT
+
+	DECLARE @ContractSliceDetail TABLE (
+		 intCRowNo INT IDENTITY(1, 1)
+		,intCContractDetailId INT
+		,intCParentDetailId INT
+		,dblCQuantity NUMERIC(18, 6)
+		,intCItemUOMId INT
+		)
+	DECLARE @ParentLoad TABLE (
+		intLoadRecordId INT IDENTITY(1,1)
+	   ,intLoadId INT
+	   ,strLoadNumber NVARCHAR(100)
+	   ,intLoadDetailId INT
+	   ,dblLoadDetailQty NUMERIC(18,6)
+	   ,intItemUOMId INT
+	   ,intWeightUOMId INT)
+
+	-- Only sliced. ie., new contract sequence records only
+	INSERT INTO @ContractSliceDetail
+	SELECT intContractDetailId
+		  ,intParentDetailId
+		  ,dblQuantity
+		  ,intItemUOMId
+	FROM tblCTContractDetail
+	WHERE intContractHeaderId = @intContractHeaderId
+		AND ysnSlice = 1
+
+	IF ((SELECT COUNT(1) FROM @ContractSliceDetail) < 1)
+		RETURN;
+
+	-- Parent contract sequence. ie., from which sequence it sliced
+	SELECT TOP 1 @intParentContractDetailId = intCParentDetailId FROM @ContractSliceDetail
+
+	IF EXISTS (SELECT 1
+			   FROM tblLGLoad L
+			   JOIN tblLGLoadDetail LD ON LD.intLoadId = L.intLoadId
+			   WHERE LD.intPContractDetailId = @intParentContractDetailId
+					AND L.intShipmentType = 1)
+	BEGIN
+		RAISERROR ('Shipment exists for the contract sequence. Cannot proceed.',16,1)
+	END
+
+
+	SELECT @intUserId = intLastModifiedById
+	FROM tblCTContractDetail
+	WHERE intContractDetailId = @intParentContractDetailId
+
+	-- Parent contract sequence Load
+	INSERT INTO @ParentLoad 
+	SELECT L.intLoadId
+		,L.strLoadNumber
+		,LD.intLoadDetailId
+		,LD.dblQuantity
+		,LD.intItemUOMId
+		,LD.intWeightItemUOMId
+	FROM tblLGLoad L
+	JOIN tblLGLoadDetail LD ON LD.intLoadId = L.intLoadId
+	WHERE CASE 
+			WHEN L.intPurchaseSale = 1
+				THEN LD.intPContractDetailId
+			ELSE LD.intSContractDetailId
+			END = @intParentContractDetailId AND intShipmentType = 2
+
+
+	SELECT * FROM @ParentLoad--
+
+	SELECT @intMinLoadRecordId = MIN(intLoadRecordId) FROM @ParentLoad 
+
+	WHILE (@intMinLoadRecordId > 0)
+	BEGIN
+		SET @intOrgLoadId = NULL
+		SET @intOrgLoadIDetaild = NULL
+		SET @dblOrgLoadDetailQty = NULL
+		SET @dblOrgContractDetailQty = NULL
+		SET @intOrgContractDetailQtyUOM = NULL
+
+		SELECT @intOrgLoadId = intLoadId
+			  ,@intOrgLoadIDetaild= intLoadDetailId
+			  ,@dblOrgLoadDetailQty = dblLoadDetailQty
+			  ,@intOrgLoadDetailItemUOM = intItemUOMId
+			  ,@intOrgLoadDetailWeightUOM = intWeightUOMId
+		FROM @ParentLoad
+		WHERE intLoadRecordId = @intMinLoadRecordId
+
+		SELECT @dblOrgContractDetailQty = dblQuantity
+			  ,@intOrgContractDetailQtyUOM = intUnitMeasureId
+		FROM tblCTContractDetail
+		WHERE intContractDetailId = @intParentContractDetailId
+
+		IF (@dblOrgLoadDetailQty > @dblOrgContractDetailQty)
+		BEGIN
+			UPDATE tblLGLoadDetail 
+			SET dblQuantity = @dblOrgContractDetailQty
+			   ,dblNet = dbo.fnCTConvertQtyToTargetItemUOM(intItemId,intWeightItemUOMId,@dblOrgContractDetailQty)
+			   ,dblGross = dbo.fnCTConvertQtyToTargetItemUOM(intItemId,intWeightItemUOMId,@dblOrgContractDetailQty)
+			WHERE intLoadDetailId = @intOrgLoadIDetaild
+
+			SET @dblOrgLoadDetailQty = (@dblOrgLoadDetailQty - @dblOrgContractDetailQty)
+			
+			-- Create contract sequence samples for the remaining representing qty
+			SELECT @intCRowNo = MIN(intCRowNo)
+			FROM @ContractSliceDetail
+
+			WHILE (@intCRowNo > 0 AND @dblOrgLoadDetailQty > 0)
+			BEGIN
+				DECLARE @dblNewLoadDetailQty NUMERIC(18,6)
+
+				SELECT @intCRowNo = intCRowNo
+					,@intCContractDetailId = intCContractDetailId
+					,@dblCQuantity = dblCQuantity
+					,@intCItemUOMId = intCItemUOMId
+				FROM @ContractSliceDetail
+				WHERE intCRowNo = @intCRowNo
+
+				IF (@dblOrgLoadDetailQty > @dblCQuantity)
+					SET @dblNewLoadDetailQty = @dblCQuantity
+				ELSE
+					SET @dblNewLoadDetailQty = @dblOrgLoadDetailQty
+
+				-- Create new sample for seq @intCContractDetailId, @dblNewRepresentingQty, @intNewRepresentingUOMId. Take other values from existing sample @intSSampleId
+				EXEC uspLGLoadContractCopy @intOldLoadDetailId = @intOrgLoadIDetaild
+										  ,@intNewContractDetailId = @intCContractDetailId
+										  ,@dblNewLoadDetailQuantity = @dblNewLoadDetailQty
+										  ,@intNewLoadDetailItemUOMId = @intOrgLoadDetailItemUOM
+										  ,@intUserId = @intUserId
+
+				SET @dblOrgLoadDetailQty = (@dblOrgLoadDetailQty - @dblNewLoadDetailQty)
+								
+				SELECT @intCRowNo = MIN(intCRowNo)
+				FROM @ContractSliceDetail
+				WHERE intCRowNo > @intCRowNo
+			END
+		END
+
+		SELECT @intMinLoadRecordId = MIN(intLoadRecordId)
+		FROM @ParentLoad
+		WHERE intLoadRecordId > @intMinLoadRecordId
+
+	END
+
+END TRY
+	
+BEGIN CATCH
+	SET @strErrMsg = ERROR_MESSAGE()
+	RAISERROR (@strErrMsg,16,1,'WITH NOWAIT')
+END CATCH
