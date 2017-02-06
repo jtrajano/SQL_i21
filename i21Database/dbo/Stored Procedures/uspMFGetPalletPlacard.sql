af766883fcf4710041a85d3406afb57e744f2e0b
Premkumar <Prem.Selladurai@gmail.com>
2017-02-06 18:12:54 +0530

    MFG-2170

---
+++ i21Database/dbo/Stored Procedures/uspMFGetPalletPlacard.sql
@@ -0,0 +1,27 @@
+﻿CREATE PROCEDURE uspMFGetPalletPlacard (@strLotNumber NVARCHAR(50))
+AS
+BEGIN
+	SELECT DT.strItemNo
+		,DT.strDescription
+		,DT.strParentLotNumber
+		,DT.dblPhysicalCount
+		,DT.strUnitMeasure
+		,SUM(DT.dblPhysicalCount) OVER () dblTotalQty
+	FROM (
+		SELECT I.strItemNo
+			,I.strDescription
+			,strParentLotNumber
+			,SUM(WP.dblPhysicalCount) AS dblPhysicalCount
+			,UM.strUnitMeasure
+		FROM tblMFWorkOrderProducedLot WP
+		JOIN tblICLot L ON L.intLotId = WP.intLotId
+		JOIN tblICItem I ON I.intItemId = L.intItemId
+		JOIN dbo.tblICItemUOM IU ON IU.intItemUOMId = WP.intPhysicalItemUOMId
+		JOIN dbo.tblICUnitMeasure UM ON UM.intUnitMeasureId = IU.intUnitMeasureId
+		WHERE L.strLotNumber = @strLotNumber
+		GROUP BY I.strItemNo
+			,I.strDescription
+			,strParentLotNumber
+			,UM.strUnitMeasure
+		) AS DT
+END
