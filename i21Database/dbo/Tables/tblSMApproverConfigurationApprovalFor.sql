a0c16aa8605473b737de22724f4fc08c65dd825a
jesmar mendones <jesmar.mendones@irely.com>
2017-02-06 15:22:35 +0800

    FRM-4332, SM-2541

---
+++ i21Database/dbo/Tables/tblSMApproverConfigurationApprovalFor.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[tblSMApproverConfigurationApprovalFor]
+(
+	[intApprovalForId]		INT												NOT NULL	PRIMARY KEY IDENTITY, 
+    [strApprovalFor]		INT												NOT NULL, 
+	[strNamespace]			NVARCHAR(250) COLLATE Latin1_General_CI_AS		NOT NULL,
+	[strType]				NVARCHAR(50) COLLATE Latin1_General_CI_AS		NOT NULL,
+    [intConcurrencyId]		INT												NOT NULL	DEFAULT 1, 
+)
