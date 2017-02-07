255fc11d946448ed6511b2987fb96f73354a543f
Swetosuvro Ghosh <swetosuvro.g@irely.com>
2017-02-07 16:55:47 +0530

    LG-730

--- i21Database/dbo/Stored Procedures/uspLGProcessIntegrationLogData.sql
+++ i21Database/dbo/Stored Procedures/uspLGProcessIntegrationLogData.sql
@@ -19,7 +19,7 @@ BEGIN TRY
 	DECLARE @intMaxLoadContainerLogId INT
 	DECLARE @intMinLoadContainerRecordId INT
 	DECLARE @intLoadStdId INT
-
+	DECLARE @strShipmentType NVARCHAR(MAX)
 	DECLARE @strErrMsg NVARCHAR(MAX)
 
 	DECLARE @tblLoadContainerRecord TABLE 
@@ -36,7 +36,7 @@ BEGIN TRY
 			(intLoadRecordId INT Identity(1, 1)
 			,intLoadLogId INT
 			,intLoadId INT)
-
+	
 	INSERT INTO @tblLoadContainerRecord
 	SELECT intLoadContainerLogId,
 		   intLoadContainerId
@@ -72,14 +72,84 @@ BEGIN TRY
 		SELECT @intMaxLoadLogId = MAX(intLoadLogId)
 		FROM @tblLoadRecord
 		WHERE intLoadId = @intLoadId
+	
+		SELECT @strShipmentType = CASE L.intShipmentType
+				WHEN 1
+					THEN 'Shipment'
+				WHEN 2
+					THEN 'Shipping Instructions'
+				WHEN 3
+					THEN 'Vessel Nomination'
+				ELSE ''
+				END COLLATE Latin1_General_CI_AS 
+		FROM tblLGLoad L WHERE intLoadId = @intLoadId
 
-		EXEC uspLGProcessLoadToIntegration 'tblLGLoadLog'
-			,@intMinLoadLogId
-			,@intMaxLoadLogId
-			,NULL
-			,@intMaxLoadLogId
-			,0
-			,0
+		INSERT INTO tblLGLoadStg (
+			intLoadId
+			,strTransactionType
+			,strLoadNumber
+			,strShippingInstructionNumber
+			,strContractBasis
+			,strContractBasisDesc
+			,strBillOfLading
+			,strShippingLine
+			,strShippingLineAccountNo
+			,strExternalShipmentNumber
+			,strDateQualifier
+			,dtmScheduledDate
+			,dtmETAPOD
+			,dtmETAPOL
+			,dtmBLDate
+			,strRowState
+			,dtmFeedCreated
+			)
+		SELECT intLoadId
+			,strShipmentType = CASE L.intShipmentType
+				WHEN 1
+					THEN 'Shipment'
+				WHEN 2
+					THEN 'Shipping Instructions'
+				WHEN 3
+					THEN 'Vessel Nomination'
+				ELSE ''
+				END COLLATE Latin1_General_CI_AS
+			,strLoadNumber
+			,CASE 
+				WHEN ISNULL(L.strShippingInstructionNumber, '') = ''
+					THEN L.strLoadNumber
+				ELSE L.strShippingInstructionNumber
+				END
+            ,strContractBasis = (
+                SELECT TOP 1 CB.strContractBasis
+                FROM tblCTContractHeader CH
+                JOIN tblCTContractDetail CD ON CD.intContractHeaderId = CH.intContractHeaderId
+                JOIN tblCTContractBasis CB ON CB.intContractBasisId = CH.intContractBasisId
+                JOIN tblLGLoadDetail LD ON LD.intPContractDetailId = CD.intContractDetailId
+                WHERE LD.intLoadId = L.intLoadId
+                )
+            ,strContractBasisDesc = (
+                SELECT TOP 1 CB.strDescription
+                FROM tblCTContractHeader CH
+                JOIN tblCTContractDetail CD ON CD.intContractHeaderId = CH.intContractHeaderId
+                JOIN tblCTContractBasis CB ON CB.intContractBasisId = CH.intContractBasisId
+                JOIN tblLGLoadDetail LD ON LD.intPContractDetailId = CD.intContractDetailId
+                WHERE LD.intLoadId = L.intLoadId
+                )
+			,L.strBLNumber
+			,L.strShippingLine
+			,V.strVendorAccountNum
+			,L.strExternalShipmentNumber
+			,'015' AS strDateQualifier
+			,L.dtmScheduledDate
+			,L.dtmETAPOD
+			,L.dtmETAPOL
+			,L.dtmBLDate
+			,'Modified'
+			,GETDATE()
+		FROM vyuLGLoadView L
+		LEFT JOIN tblEMEntity E ON E.intEntityId = L.intShippingLineEntityId
+		LEFT JOIN tblAPVendor V ON V.intEntityVendorId = E.intEntityId
+		WHERE intLoadId = @intLoadId
 
 		DELETE FROM @tblLoadRecord WHERE intLoadId = @intLoadId
 
@@ -105,13 +175,111 @@ BEGIN TRY
 		FROM @tblLoadDetailRecord
 		WHERE intLoadDetailId = @intLoadDetailId
 
-		EXEC uspLGProcessLoadToIntegration 'tblLGLoadDetailLog'
-			,@intMinLoadDetailLogId
-			,@intMaxLoadDetailLogId
-			,'intLoadLogId,strRowState,intRowNumber'
-			,0
-			,@intMaxLoadDetailLogId
-			,0
+		SELECT @intLoadStdId = MAX(LS.intLoadStgId)
+		FROM tblLGLoadStg LS
+		JOIN tblLGLoadDetail LD ON LD.intLoadId = LS.intLoadId
+		JOIN @tblLoadDetailRecord LDR ON LDR.intLoadDetailId = LD.intLoadDetailId
+			AND LDR.intLoadDetailId = @intLoadDetailId
+
+		INSERT INTO tblLGLoadDetailStg(
+			 intLoadStgId
+			,intLoadId
+			,intSIDetailId
+			,intLoadDetailId
+			,intRowNumber
+			,strItemNo
+			,strSubLocationName
+			,strStorageLocationName
+			,strBatchNumber
+			,dblDeliveredQty
+			,strUnitOfMeasure
+			,dblNetWt
+			,dblGrossWt
+			,strWeightUOM
+			,intHigherPositionRef
+			,strDocumentCategory
+			,strReferenceDataInfo
+			,strSeq
+			,strLoadNumber
+			,strExternalPONumber
+			,strExternalPOItemNumber
+			,strExternalPOBatchNumber
+			,strExternalShipmentItemNumber
+			,strExternalBatchNo
+			,strChangeType
+			,strRowState
+			,dtmFeedCreated
+			,strCommodityCode)
+		SELECT @intLoadStdId
+			,@intLoadId
+			,CASE 
+				WHEN ISNULL(LSID.intLoadDetailId, 0) = 0
+					THEN LD.intLoadDetailId
+				ELSE LSID.intLoadDetailId
+				END AS intSIDetailId
+			,LD.intLoadDetailId
+			,Row_NUMBER() OVER (
+				PARTITION BY LD.intLoadId ORDER BY LD.intLoadId
+				) AS intRowNumber
+			,LD.strItemNo
+			,strSubLocationName = (
+				SELECT CLSL.strSubLocationName AS strStorageLocationName
+				FROM tblCTContractDetail CD
+				JOIN tblSMCompanyLocationSubLocation CLSL ON CLSL.intCompanyLocationSubLocationId = CD.intSubLocationId
+				WHERE CD.intContractDetailId = CASE 
+						WHEN LD.intPurchaseSale = 1
+							THEN LD.intPContractDetailId
+						ELSE LD.intSContractDetailId
+						END
+				)
+			,strStorageLocationName = (
+				SELECT SL.strName AS strStorageLocationName
+				FROM tblCTContractDetail CD
+				JOIN tblICStorageLocation SL ON SL.intStorageLocationId = CD.intStorageLocationId
+				WHERE CD.intContractDetailId = CASE 
+						WHEN LD.intPurchaseSale = 1
+							THEN LD.intPContractDetailId
+						ELSE LD.intSContractDetailId
+						END
+				)
+			,LD.strLoadNumber
+			,LD.dblQuantity
+			,LD.strItemUOM
+			,LD.dblGross
+			,LD.dblNet
+			,LD.strWeightItemUOM
+			,Row_NUMBER() OVER (
+				PARTITION BY LD.intLoadId ORDER BY LD.intLoadId
+				)
+			,'C' AS strDocumentCategory
+			,'001' AS strRefDataInfo
+			,0 AS strSeq
+			,LD.strLoadNumber
+			,CD.strERPPONumber
+			,CD.strERPItemNumber
+			,CD.strERPBatchNumber
+			,D.strExternalShipmentItemNumber
+			,D.strExternalBatchNo
+			,'QUA' AS strChangeType
+			,'Modified' AS strRowState
+			,GETDATE()
+			,C.strCommodityCode
+		FROM vyuLGLoadDetailView LD
+		JOIN tblCTContractDetail CD ON CD.intContractDetailId = CASE 
+				WHEN LD.intPurchaseSale = 1
+					THEN LD.intPContractDetailId
+				ELSE LD.intSContractDetailId
+				END
+		JOIN tblLGLoadDetail D ON D.intLoadDetailId = LD.intLoadDetailId
+		JOIN tblICCommodity C ON C.intCommodityId = CASE 
+				WHEN LD.intPurchaseSale = 1
+					THEN LD.intPCommodityId
+				ELSE LD.intSCommodityId
+				END
+		LEFT JOIN tblLGLoad L ON L.intLoadId = D.intLoadId
+		LEFT JOIN tblLGLoad LSI ON LSI.intLoadId = L.intLoadShippingInstructionId
+		LEFT JOIN tblLGLoadDetail LSID ON LSID.intLoadId = LSI.intLoadId AND D.intPContractDetailId = LSID.intPContractDetailId
+		WHERE D.intLoadDetailId = @intLoadDetailId
 
 		DELETE FROM @tblLoadDetailRecord WHERE intLoadDetailId = @intLoadDetailId
 
@@ -176,13 +344,37 @@ BEGIN TRY
 		END
 		ELSE
 		BEGIN
-			EXEC uspLGProcessLoadToIntegration 'tblLGLoadContainerLog'
-				,@intMinLoadContainerLogId
-				,@intMaxLoadContainerLogId
-				,'intLoadLogId,strRowState'
-				,0
-				,0
-				,@intMaxLoadContainerLogId
+		IF (@strShipmentType = 'Shipment')
+			BEGIN
+				INSERT INTO tblLGLoadContainerStg
+				SELECT @intLoadStdId
+					,@intLoadId
+					,LC.intLoadContainerId
+					,LC.strContainerNumber
+					,CASE 
+						WHEN CT.strContainerType LIKE '%20%'
+							THEN '000000000010003243'
+						WHEN CT.strContainerType LIKE '%40%'
+							THEN '000000000010003244'
+						ELSE CT.strContainerType
+						END
+					,'0002'
+					,L.strExternalShipmentNumber
+					,ROW_NUMBER() OVER (
+						PARTITION BY LC.intLoadId ORDER BY LC.intLoadId
+						) AS Seq
+					,LC.dblQuantity
+					,LC.strItemUOM
+					,LC.dblNetWt
+					,LC.dblGrossWt
+					,LC.strWeightUnitMeasure
+					,'Modified'
+					,GETDATE()
+				FROM vyuLGLoadContainerView LC
+				JOIN tblLGLoad L ON L.intLoadId = LC.intLoadId
+				LEFT JOIN tblLGContainerType CT ON CT.intContainerTypeId = L.intContainerTypeId
+				WHERE LC.intLoadId = @intLoadId
+			END
 		END
 
 		DELETE FROM @tblLoadContainerRecord WHERE intLoadContainerId = @intLoadContainerId
@@ -196,7 +388,7 @@ BEGIN TRY
 		DELETE FROM tblLGLoadDetailLog
 		DELETE FROM tblLGLoadLog
 END TRY
-BEGIN CATCH
+BEGIN CATCH	
 
 	SET @strErrMsg = ERROR_MESSAGE()
 	IF @strErrMsg != ''
