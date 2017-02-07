101cf414b3aed050efef19f8521de2a94f622594
Feb Montefrio <feb.montefrio@irely.com>
2017-02-07 15:55:49 +0800

    IC-3289 IC-3288

--- i21Database/dbo/Stored Procedures/uspICPostInventoryShipment.sql
+++ i21Database/dbo/Stored Procedures/uspICPostInventoryShipment.sql
@@ -3,6 +3,7 @@
 	,@ysnRecap BIT  = 0  
 	,@strTransactionId NVARCHAR(40) = NULL   
 	,@intEntityUserSecurityId AS INT = NULL 
+	,@strBatchId NVARCHAR(40) = NULL OUTPUT
 AS  
   
 SET QUOTED_IDENTIFIER OFF  
@@ -34,8 +35,7 @@ DECLARE	@ACCOUNT_CATEGORY_TO_COUNTER_INVENTORY AS NVARCHAR(255) = 'Cost of Goods
 DECLARE @DefaultCurrencyId AS INT = dbo.fnSMGetDefaultCurrency('FUNCTIONAL')
 
 -- Get the Inventory Shipment batch number
-DECLARE @strBatchId AS NVARCHAR(40) 
-		,@strItemNo AS NVARCHAR(50)
+DECLARE @strItemNo AS NVARCHAR(50)
 		,@ysnAllowBlankGLEntries AS BIT = 1
 		,@intItemId AS INT
 
@@ -273,9 +273,13 @@ BEGIN
 		END 
 	END
 END
+
 -- Get the next batch number
-EXEC dbo.uspSMGetStartingNumber @STARTING_NUMBER_BATCH, @strBatchId OUTPUT   
-IF @@ERROR <> 0 GOTO Post_Exit    
+BEGIN 
+	SET @strBatchId = NULL 
+	EXEC dbo.uspSMGetStartingNumber @STARTING_NUMBER_BATCH, @strBatchId OUTPUT   
+	IF @@ERROR <> 0 GOTO Post_Exit    
+END 
 
 --------------------------------------------------------------------------------------------  
 -- Begin a transaction and immediately create a save point 
