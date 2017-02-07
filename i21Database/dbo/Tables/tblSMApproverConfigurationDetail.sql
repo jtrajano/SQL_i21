75a3c550aca7495b09e57fcbe61c2e7f5422c3cf
Dan Michael Jalimao <dan.jalimao@irely.com>
2017-02-07 15:46:09 +0800

    SM-2541

--- i21Database/dbo/Tables/tblSMApproverConfigurationDetail.sql
+++ i21Database/dbo/Tables/tblSMApproverConfigurationDetail.sql
@@ -6,6 +6,7 @@
 	[strValue]								NVARCHAR(250) COLLATE Latin1_General_CI_AS		NOT NULL,
 	[intApproverConfigurationId]			INT												NOT NULL DEFAULT 0,
 	[intApprovalForId]						INT												NOT NULL DEFAULT 0,
+	[intSort]								INT												NOT NULL, 
     [intConcurrencyId]						INT												NOT NULL DEFAULT 1, 
     CONSTRAINT [FK_tblSMApproverConfigurationDetail_tblSMApproverConfigurationApprovalFor] FOREIGN KEY ([intApprovalForId]) REFERENCES tblSMApproverConfigurationApprovalFor([intApprovalForId]),
 )
