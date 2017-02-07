f9d52f5d251c8452d4c8d592b7b4ba402e4e1261
Jayson Ramos <jayson.ramos@iRely.com>
2017-02-07 15:24:33 +0800

    HD-1907

--- i21Database/dbo/Views/vyuHDCustomerProspectVendor.sql
+++ i21Database/dbo/Views/vyuHDCustomerProspectVendor.sql
@@ -4,7 +4,7 @@
 			intEntityCustomerId
 			,strCustomerNumber
 			,strName
-			,strType = (case when SUBSTRING(strType,1,1) = ',' then SUBSTRING(strType,2,len(strType)) when SUBSTRING(strType,len(strType),1) = ',' then SUBSTRING(strType,1,len(strType)-1) else strType end)
+			,strType
 			,ysnActive
 		from
 		(
@@ -12,12 +12,31 @@
 				intEntityCustomerId = a.intEntityId
 				,strCustomerNumber = a.strEntityNo
 				,strName = a.strName
-				,strType = (case when d.strType is null or LTRIM(RTRIM(d.strType)) = '' then '' else d.strType end) + ',' + (case when e.strType is null or LTRIM(RTRIM(d.strType)) = '' then '' else e.strType end)
+				,strType = (case when d.strType is null or LTRIM(RTRIM(d.strType)) = '' then '' else d.strType end)
+						 + (case when e.strType is null or LTRIM(RTRIM(e.strType)) = '' then '' when d.strType is not null or LTRIM(RTRIM(d.strType)) <> '' then ',' + e.strType else e.strType end)
+						 + (case when f.strType is null or LTRIM(RTRIM(f.strType)) = '' then '' when d.strType is not null or LTRIM(RTRIM(d.strType)) <> ''
+																								  or e.strType is not null or LTRIM(RTRIM(e.strType)) <> '' then ',' + f.strType else f.strType end)
+						 + (case when g.strType is null or LTRIM(RTRIM(g.strType)) = '' then '' when d.strType is not null or LTRIM(RTRIM(d.strType)) <> ''
+																								  or e.strType is not null or LTRIM(RTRIM(e.strType)) <> ''
+																								  or f.strType is not null or LTRIM(RTRIM(f.strType)) <> '' then ',' + g.strType else g.strType end)
+						 + (case when h.strType is null or LTRIM(RTRIM(h.strType)) = '' then '' when d.strType is not null or LTRIM(RTRIM(d.strType)) <> ''
+																								  or e.strType is not null or LTRIM(RTRIM(e.strType)) <> ''
+																								  or f.strType is not null or LTRIM(RTRIM(f.strType)) <> ''
+																								  or g.strType is not null or LTRIM(RTRIM(g.strType)) <> '' then ',' + h.strType else h.strType end)
+						 + (case when i.strType is null or LTRIM(RTRIM(i.strType)) = '' then '' when d.strType is not null or LTRIM(RTRIM(d.strType)) <> ''
+																								  or e.strType is not null or LTRIM(RTRIM(e.strType)) <> ''
+																								  or f.strType is not null or LTRIM(RTRIM(f.strType)) <> ''
+																								  or g.strType is not null or LTRIM(RTRIM(g.strType)) <> ''
+																								  or h.strType is not null or LTRIM(RTRIM(h.strType)) <> '' then ',' + i.strType else i.strType end)
 				,ysnActive = a.ysnActive
 			from
 				tblEMEntity a
 				left join tblEMEntityType d on d.intEntityId = a.intEntityId and d.strType = 'Customer'
 				left join tblEMEntityType e on e.intEntityId = a.intEntityId and e.strType = 'Vendor'
+				left join tblEMEntityType f on f.intEntityId = a.intEntityId and f.strType = 'Prospect'
+				left join tblEMEntityType g on g.intEntityId = a.intEntityId and g.strType = 'Competitor'
+				left join tblEMEntityType h on h.intEntityId = a.intEntityId and h.strType = 'Partner'
+				left join tblEMEntityType i on i.intEntityId = a.intEntityId and i.strType = 'Employee'
 			where
 				a.intEntityId in (select b.intEntityVendorId from tblAPVendor b union select c.intEntityCustomerId from tblARCustomer c)
 		) as result
