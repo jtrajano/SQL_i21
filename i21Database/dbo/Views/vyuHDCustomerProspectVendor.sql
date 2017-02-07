0cd7659c9aa71ccd384b0e7a88e976e745446859
Jayson Ramos <jayson.ramos@iRely.com>
2017-02-07 12:57:30 +0800

    HD-1907

--- i21Database/dbo/Views/vyuHDCustomerProspectVendor.sql
+++ i21Database/dbo/Views/vyuHDCustomerProspectVendor.sql
@@ -1,31 +1,24 @@
 ﻿CREATE VIEW [dbo].[vyuHDCustomerProspectVendor]
 	AS
 		select
-			intEntityCustomerId = b.intEntityId
-			,strCustomerNumber = a.strVendorId
-			,strName = b.strName
-			,strType = c.strType
-			,ysnActive = b.ysnActive
-		from
-			tblAPVendor a
-			,tblEMEntity b
-			,tblEMEntityType c
-		where b.intEntityId = a.intEntityVendorId
-			and c.intEntityId = b.intEntityId
-			and a.intEntityVendorId not in (select intEntityCustomerId from tblARCustomer)
-
-		union all
-
 		select
-			intEntityCustomerId = b.intEntityId
-			,strCustomerNumber = a.strCustomerNumber
-			,strName = b.strName
-			,strType = c.strType
-			,ysnActive = b.ysnActive
+			intEntityCustomerId
+			,strCustomerNumber
+			,strName
+			,strType = (case when SUBSTRING(strType,1,1) = ',' then SUBSTRING(strType,2,len(strType)) when SUBSTRING(strType,len(strType),1) = ',' then SUBSTRING(strType,1,len(strType)-1) else strType end)
+			,ysnActive
 		from
-			tblARCustomer a
-			,tblEMEntity b
-			,tblEMEntityType c
-		where b.intEntityId = a.intEntityCustomerId
-			and c.intEntityId = b.intEntityId
-			and a.intEntityCustomerId not in (select intEntityVendorId from tblAPVendor)
+		(
+			select
+				intEntityCustomerId = a.intEntityId
+				,strCustomerNumber = a.strEntityNo
+				,strName = a.strName
+				,strType = (case when d.strType is null or LTRIM(RTRIM(d.strType)) = '' then '' else d.strType end) + ',' + (case when e.strType is null or LTRIM(RTRIM(d.strType)) = '' then '' else e.strType end)
+				,ysnActive = a.ysnActive
+			from
+				tblEMEntity a
+				left join tblEMEntityType d on d.intEntityId = a.intEntityId and d.strType = 'Customer'
+				left join tblEMEntityType e on e.intEntityId = a.intEntityId and e.strType = 'Vendor'
+			where
+				a.intEntityId in (select b.intEntityVendorId from tblAPVendor b union select c.intEntityCustomerId from tblARCustomer c)
+		) as result
