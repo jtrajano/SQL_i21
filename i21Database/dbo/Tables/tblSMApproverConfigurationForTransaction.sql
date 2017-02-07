72991cdb8b39a8547e961226bf44b0772c04c2f2
jesmar mendones <jesmar.mendones@irely.com>
2017-02-07 18:30:49 +0800

    FRM-4332

---
+++ i21Database/dbo/Tables/tblSMApproverConfigurationForTransaction.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[tblSMApproverConfigurationForTransaction]
+(
+	[intApprovalForTransactionId]		INT												NOT NULL PRIMARY KEY IDENTITY, 
+    [strApproverConfiguration]			NVARCHAR(MAX) COLLATE Latin1_General_CI_AS		NOT NULL,
+    [intTransactionId]					INT												NOT NULL, 
+    [intConcurrencyId]					INT												NOT NULL DEFAULT 1, 
+    CONSTRAINT [FK_tblSMApproverConfigurationForTransaction_tblSMTransaction] FOREIGN KEY ([intTransactionId]) REFERENCES tblSMTransaction([intTransactionId]),
+)
