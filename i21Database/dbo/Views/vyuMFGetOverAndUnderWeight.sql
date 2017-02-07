2d1dcbc0c293466dc3069ac4ec40d331c78bbd14
Premkumar <Prem.Selladurai@gmail.com>
2017-02-07 17:08:07 +0530

    MFG-2162

---
+++ i21Database/dbo/Views/vyuMFGetOverAndUnderWeight.sql
@@ -0,0 +1,28 @@
+﻿CREATE VIEW vyuMFGetOverAndUnderWeight
+AS
+SELECT W.dtmPlannedDate AS [Production Date]
+	,I.strItemNo AS Item
+	,I.strDescription AS Description
+	,W.strWorkOrderNo AS [Job #]
+	,PL.strParentLotNumber AS [Production Lot]
+	,SUM(WP.dblPhysicalCount * I.intInnerUnits) AS [Good produced Pouches]
+	,IsNULL((
+			SELECT SUM(WP.dblPhysicalCount)
+			FROM tblMFWorkOrderProducedLot WP
+			WHERE WP.intWorkOrderId = W.intWorkOrderId
+				AND WP.intItemId <> W.intItemId
+			), 0) [Total sweeps]
+FROM dbo.tblMFWorkOrder W
+JOIN dbo.tblMFWorkOrderProducedLot WP ON WP.intWorkOrderId = W.intWorkOrderId
+JOIN dbo.tblICItem I ON I.intItemId = W.intItemId
+JOIN dbo.tblICLot L ON L.intLotId = WP.intLotId
+JOIN dbo.tblICParentLot PL ON PL.intParentLotId = L.intParentLotId
+JOIN dbo.tblICItemUOM IIU ON IIU.intItemUOMId = WP.intPhysicalItemUOMId
+WHERE WP.ysnProductionReversed = 0
+GROUP BY W.dtmPlannedDate
+	,W.intItemId
+	,I.strItemNo
+	,I.strDescription
+	,W.strWorkOrderNo
+	,PL.strParentLotNumber
+	,W.intWorkOrderId
---
+++ i21Database/dbo/Views/vyuMFGetProduction.sql
@@ -0,0 +1,28 @@
+﻿CREATE VIEW vyuMFGetProduction
+AS
+SELECT WP.dtmProductionDate AS [Production Date]
+	,I.strItemNo AS Item
+	,I.strDescription AS Description
+	,W.strWorkOrderNo AS [Job #]
+	,PL.strParentLotNumber AS [Production Lot]
+	,SUM(WP.dblPhysicalCount) AS [Quantity]
+	,IUM.strUnitMeasure AS [Quantity UOM]
+	,SUM(WP.dblQuantity) AS [Weight]
+	,UM.strUnitMeasure AS [Weight UOM]
+FROM dbo.tblMFWorkOrder W
+JOIN dbo.tblMFWorkOrderProducedLot WP ON WP.intWorkOrderId = W.intWorkOrderId
+JOIN dbo.tblICItem I ON I.intItemId = W.intItemId
+JOIN dbo.tblICLot L ON L.intLotId = WP.intLotId
+JOIN dbo.tblICParentLot PL ON PL.intParentLotId = L.intParentLotId
+JOIN dbo.tblICItemUOM IIU ON IIU.intItemUOMId = WP.intPhysicalItemUOMId
+JOIN dbo.tblICUnitMeasure IUM ON IUM.intUnitMeasureId = IIU.intUnitMeasureId
+JOIN dbo.tblICItemUOM IU ON IU.intItemUOMId = WP.intItemUOMId
+JOIN dbo.tblICUnitMeasure UM ON UM.intUnitMeasureId = IU.intUnitMeasureId
+WHERE WP.ysnProductionReversed = 0
+GROUP BY WP.dtmProductionDate
+	,I.strItemNo
+	,I.strDescription
+	,W.strWorkOrderNo
+	,PL.strParentLotNumber
+	,IUM.strUnitMeasure
+	,UM.strUnitMeasure
