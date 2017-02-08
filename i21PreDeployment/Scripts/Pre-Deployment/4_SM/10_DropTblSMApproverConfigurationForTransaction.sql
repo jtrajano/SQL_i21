934e78c9194cdae45e5d0a6cefe2042cfbd1b86c
jesmar mendones <jesmar.mendones@irely.com>
2017-02-08 19:40:51 +0800

    FRM-4332

---
+++ i21PreDeployment/Scripts/Pre-Deployment/4_SM/10_DropTblSMApproverConfigurationForTransaction.sql
@@ -0,0 +1,6 @@
+﻿
+PRINT 'BEGIN Drop of table tblSMApproverConfigurationForTransaction.'
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[tblSMApproverConfigurationForTransaction]'))
+	DROP TABLE [dbo].[tblSMApproverConfigurationForTransaction]
+GO
+PRINT 'END Drop of table tblSMApproverConfigurationForTransaction'
