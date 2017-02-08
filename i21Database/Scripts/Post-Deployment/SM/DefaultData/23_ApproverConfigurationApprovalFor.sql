551cd1e8c28d50ef2110daf42607c5a4987eec21
Dan Michael Jalimao <dan.jalimao@irely.com>
2017-02-08 15:51:08 +0800

    SM-2551

--- i21Database/Scripts/Post-Deployment/SM/DefaultData/23_ApproverConfigurationApprovalFor.sql
+++ i21Database/Scripts/Post-Deployment/SM/DefaultData/23_ApproverConfigurationApprovalFor.sql
@@ -4,7 +4,7 @@
 	DECLARE @PurchaseOrderId INT
 	SELECT @PurchaseOrderId = intScreenId FROM tblSMScreen WHERE strNamespace = 'AccountsPayable.view.PurchaseOrder'
 
-	IF NOT EXISTS(SELECT TOP 1 1 FROM tblSMApproverConfigurationApprovalFor WHERE intScreenId = @PurchaseOrderId AND strType = 'Vendor')
+	IF NOT EXISTS(SELECT TOP 1 1 FROM tblSMApproverConfigurationApprovalFor WHERE intScreenId = @PurchaseOrderId AND strApprovalFor = 'Vendor')
 	BEGIN
 		INSERT INTO tblSMApproverConfigurationApprovalFor(intScreenId, strNamespace, strType, strApprovalFor, strDisplayField, strValueField)
 		VALUES(@PurchaseOrderId, 'EntityManagement.common.combo.Vendor', 'Combobox', 'Vendor', 'strName', 'intEntityId')
@@ -13,7 +13,7 @@
 	DECLARE @VoucherId INT
 	SELECT @VoucherId = intScreenId FROM tblSMScreen WHERE strNamespace = 'AccountsPayable.view.Voucher'
 
-	IF NOT EXISTS(SELECT TOP 1 1 FROM tblSMApproverConfigurationApprovalFor WHERE intScreenId = @VoucherId AND strType = 'Vendor')
+	IF NOT EXISTS(SELECT TOP 1 1 FROM tblSMApproverConfigurationApprovalFor WHERE intScreenId = @VoucherId AND strApprovalFor = 'Vendor')
 	BEGIN
 		INSERT INTO tblSMApproverConfigurationApprovalFor(intScreenId, strNamespace, strType, strApprovalFor, strDisplayField, strValueField)
 		VALUES(@VoucherId, 'EntityManagement.common.combo.Vendor', 'Combobox', 'Vendor', 'strName', 'intEntityId')
@@ -22,19 +22,19 @@
 	DECLARE @ContractId INT
 	SELECT @ContractId = intScreenId FROM tblSMScreen WHERE strNamespace = 'ContractManagement.view.Contract'
 
-	IF NOT EXISTS(SELECT TOP 1 1 FROM tblSMApproverConfigurationApprovalFor WHERE intScreenId = @ContractId AND strType = 'Contract Type')
+	IF NOT EXISTS(SELECT TOP 1 1 FROM tblSMApproverConfigurationApprovalFor WHERE intScreenId = @ContractId AND strApprovalFor = 'Contract Type')
 	BEGIN
 		INSERT INTO tblSMApproverConfigurationApprovalFor(intScreenId, strNamespace, strType, strApprovalFor, strDisplayField, strValueField)
 		VALUES(@ContractId, 'i21.component.combobox.ContractType', 'Combobox', 'Contract Type', 'strContractType', 'intContractTypeId')
 	END
 
-	IF NOT EXISTS(SELECT TOP 1 1 FROM tblSMApproverConfigurationApprovalFor WHERE intScreenId = @ContractId AND strType = 'Product Type')
+	IF NOT EXISTS(SELECT TOP 1 1 FROM tblSMApproverConfigurationApprovalFor WHERE intScreenId = @ContractId AND strApprovalFor = 'Product Type')
 	BEGIN
 		INSERT INTO tblSMApproverConfigurationApprovalFor(intScreenId, strNamespace, strType, strApprovalFor, strDisplayField, strValueField)
 		VALUES(@ContractId, 'i21.component.combobox.ProductType', 'Combobox', 'Product Type', 'strDescription', 'intCommodityAttributeId')
 	END
 
-	IF NOT EXISTS(SELECT TOP 1 1 FROM tblSMApproverConfigurationApprovalFor WHERE intScreenId = @ContractId AND strType = 'Origin')
+	IF NOT EXISTS(SELECT TOP 1 1 FROM tblSMApproverConfigurationApprovalFor WHERE intScreenId = @ContractId AND strApprovalFor = 'Origin')
 	BEGIN
 		INSERT INTO tblSMApproverConfigurationApprovalFor(intScreenId, strNamespace, strType, strApprovalFor, strDisplayField, strValueField)
 		VALUES(@ContractId, '', 'Textfield', 'Origin', NULL, NULL)
