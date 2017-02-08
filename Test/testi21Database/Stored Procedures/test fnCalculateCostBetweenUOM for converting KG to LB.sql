281e689e83193428a8e47b8e865020a601caf8e7
Feb Montefrio <feb.montefrio@irely.com>
2017-02-08 13:03:59 +0800

    Refactor unit tests for [fnCalculateQtyBetweenUOM] and [fnCalculateCostBetweenUOM]

--- Test/testi21Database/Stored Procedures/test fnCalculateCostBetweenUOM for converting KG to LB.sql
+++ Test/testi21Database/Stored Procedures/test fnCalculateCostBetweenUOM for converting KG to LB.sql
@@ -9,20 +9,25 @@ BEGIN
 		,@50LB_BAG AS INT = 3
 		,@20KG_BAG AS INT = 4	
 		
-		,@LBS_UnitQty AS FLOAT = 1
-		,@KGS_UnitQty AS FLOAT = 0.453592
-		,@50LBBag_UnitQty AS FLOAT = 50.00
-		,@20KGBag_UnitQty AS FLOAT = 44.0925
+		,@LBS_UnitQty AS NUMERIC(38, 20) = 1
+		,@KGS_UnitQty AS NUMERIC(38, 20) = 2.20462262185
+		,@50LBBag_UnitQty AS NUMERIC(38, 20) = 50.00
+		,@20KGBag_UnitQty AS NUMERIC(38, 20) = 44.092452437
 
 	-- Arrange
-	DECLARE @dblCost AS FLOAT = 25.00
-
+	DECLARE @dblCost AS FLOAT = 25.00 -- /Kgs
 	DECLARE @result AS NUMERIC(38,20) 
-	DECLARE @expected AS NUMERIC(38,20) = 55.11561050459440000000 
+	DECLARE @expected AS NUMERIC(38,20) = 11.339809240
 
 	-- Act
 	SELECT @result = dbo.fnCalculateCostBetweenUOM(@KGS, @LBS, @dblCost)
 
 	-- Assert 
+	EXEC tSQLt.AssertEquals @expected, @result;
+
+	SET @expected = ROUND(@expected, 6)
+	SET @result = ROUND(@result, 6) 
+
+	-- Assert the rounded values 
 	EXEC tSQLt.AssertEquals @expected, @result;
 END
--- Test/testi21Database/Stored Procedures/test fnCalculateCostBetweenUOM for converting LB to 50 LB BAG.sql
+++ Test/testi21Database/Stored Procedures/test fnCalculateCostBetweenUOM for converting LB to 50 LB BAG.sql
@@ -10,15 +10,15 @@ BEGIN
 		,@20KG_BAG AS INT = 4	
 		
 		,@LBS_UnitQty AS NUMERIC(38, 20) = 1
-		,@KGS_UnitQty AS NUMERIC(38, 20) = 0.453592
+		,@KGS_UnitQty AS NUMERIC(38, 20) = 2.20462262185
 		,@50LBBag_UnitQty AS NUMERIC(38, 20) = 50.00
-		,@20KGBag_UnitQty AS NUMERIC(38, 20) = 44.0925
+		,@20KGBag_UnitQty AS NUMERIC(38, 20) = 44.092452437
 
 	-- Arrange
 	DECLARE @dblCost AS NUMERIC(18,6) = 25.00
 
 	DECLARE @result AS NUMERIC(18,6) 
-	DECLARE @Expected AS NUMERIC(18,6) = (@dblCost / @LBS_UnitQty * @50LBBag_UnitQty) -- Equals 1,250
+	DECLARE @Expected AS NUMERIC(18,6) = (@dblCost * @50LBBag_UnitQty) 
 
 	-- Act
 	SELECT @result = dbo.fnCalculateCostBetweenUOM(@LBS, @50LB_BAG, @dblCost)
