7949980671860fea119da58cfca2e4dd16824d9f
Satya <satya.sahoo@irely.com>
2017-02-08 22:40:42 +0530

    CT-1035

--- i21Database/dbo/Views/vyuCTContractDetailGrid.sql
+++ i21Database/dbo/Views/vyuCTContractDetailGrid.sql
@@ -34,7 +34,7 @@ AS
 			QA.strSampleTypeName,
 			QA.strSampleStatus,
 			QA.dtmTestingEndDate,
-			MA.strFutMarketName,
+			MA.strFutMarketName AS strFutureMarket,
 			REPLACE(MO.strFutureMonth, ' ', '(' + MO.strSymbol + ')') strFutureMonth,
 			CASE WHEN (SELECT COUNT(SA.intSpreadArbitrageId) FROM tblCTSpreadArbitrage SA  WHERE SA.intPriceFixationId = PF.intPriceFixationId) > 0
 			THEN CAST(1 AS BIT) ELSE CAST(0 AS BIT)END AS ysnSpreadAvailable, 
