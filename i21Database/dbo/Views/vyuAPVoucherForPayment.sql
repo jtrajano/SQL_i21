7a7140691e9f272d62bdd3ac4f11bc218b6a836b
Erick Dela Cruz <erick.delacruz@irely.com>
2017-02-10 07:42:10 +0800

    AP-3158

---
+++ i21Database/dbo/Views/vyuAPVoucherForPayment.sql
@@ -0,0 +1,31 @@
+﻿CREATE VIEW [dbo].[vyuAPVoucherForPayment]
+AS
+SELECT 
+	strTransactionType		=	'Voucher',
+	strTransactionId		=	A.strBillId,
+	strTransactionDate		=	A.dtmDate,
+	strTransactionDueDate	=	A.dtmDueDate,
+	strVendorName			=	B2.strName,
+	strCommodity			=	'',
+	strLineOfBusiness		=	'',
+	strLocation				=	'',
+	strTicket				=	'',
+	strContractNumber		=	'',
+	strItemId				=	'',
+	dblQuantity				=	0,
+	dblUnitPrice			=	0,
+	dblAmount				=	0,
+	intCurrencyId			=	0,
+	intForexRateType		=	0,
+	dblForexRate			=	0,
+	dblHistoricAmount		=	0,
+	dblNewForexRate			=	0,
+	dblNewAmount			=	0,
+	dblUnrealizedDebitGain	=	0,
+	dblUnrealizedCreditGain	=	0,
+	dblDebit				=	0,
+	dblCredit				=	0
+FROM tblAPBill A
+INNER JOIN (tblAPVendor B INNER JOIN tblEMEntity B2 ON B.intEntityVendorId = B2.intEntityId) 
+	ON A.intEntityVendorId = B.intEntityVendorId
+WHERE A.ysnPosted = 1 AND A.ysnPaid = 0
