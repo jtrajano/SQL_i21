26611ad73f75e46faf820808684d319797dac912
Mon Gonzales <mon.gonzales@irely.com>
2017-02-09 13:40:09 +0800

    EM-1633

--- i21Database/dbo/Views/vyuEMETExportCustomerSplit.sql
+++ i21Database/dbo/Views/vyuEMETExportCustomerSplit.sql
@@ -1,23 +1,26 @@
 ﻿CREATE VIEW [dbo].[vyuEMETExportCustomerSplit]
 	AS 
 	select 
-		sploc = d.strLocationName,
+		sploc = dd.strLocationNumber,
 		splot = a.strSplitNumber,
 		spcust = f.strEntityNo,
 		spprct = e.dblSplitPercent,
-		spcomt = a.strDescription,
-		spiscust = CASE WHEN (SELECT TOP 1 1 FROM tblEMEntityType where intEntityId = f.intEntityId and strType = 'Customer') = 1 THEN  1 ELSE 0 END
+		spcomt = a.strDescription
+
 
 	from tblEMEntitySplit a
 		join tblEMEntity b
 			on a.intEntityId = b.intEntityId	
 		join vyuEMEntityType c
 			on a.intEntityId = c.intEntityId 
-				and ( c.Customer = 1 or c.Vendor = 1)
+				and ( c.Customer = 1)
 		join tblEMEntityLocation d
 			on a.intEntityId = d.intEntityId and d.ysnDefaultLocation = 1
+		join tblSMCompanyLocation dd
+			on d.intWarehouseId = dd.intCompanyLocationId
 		left join tblEMEntitySplitDetail e
 			on a.intSplitId = e.intSplitId
 		left join tblEMEntity f
 			on e.intEntityId = f.intEntityId
+	
 	
