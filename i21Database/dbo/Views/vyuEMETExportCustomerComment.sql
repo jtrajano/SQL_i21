44c172469154974bf1d5f70f9dbb5e3f75d20695
Mon Gonzales <mon.gonzales@irely.com>
2017-02-09 18:39:25 +0800

    EM-1632

--- i21Database/dbo/Views/vyuEMETExportCustomerComment.sql
+++ i21Database/dbo/Views/vyuEMETExportCustomerComment.sql
@@ -4,9 +4,10 @@
 		cust_id  = b.strEntityNo,
 		comment_type = 'ETC',
 		line_no = Right('0000' + cast(c.RecordKey as nvarchar), 4),
-		comment = cast(c.Record as nvarchar(80)),
-		iscust =  CASE WHEN (SELECT TOP 1 1 FROM tblEMEntityType where intEntityId = b.intEntityId and strType = 'Customer') = 1 THEN  1 ELSE 0 END
+		comment = cast(c.Record as nvarchar(80))
 	 from tblEMEntityMessage a
 	join tblEMEntity b
 		on a.intEntityId = b.intEntityId and a.strMessageType = 'Energy Trac'
+	join vyuEMEntityType bb
+		on b.intEntityId = bb.intEntityId and bb.Customer = 1
 	CROSS APPLY dbo.fnCFSplitString(a.strMessage, char(10)) c
