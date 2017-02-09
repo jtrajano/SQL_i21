b6bac689f664424d2512807f89736ab8a8d2e0a2
Vinoth Kumar T <vinoth.t@irely.com>
2017-02-09 19:03:05 +0530

    QC-257

---
+++ i21Database/Scripts/Post-Deployment/CT/4_EventConfiguration.sql
@@ -0,0 +1,72 @@
+﻿-- Contains all event configuration scripts. Will run this only when customer needs it.
+
+-- Quality Event Configuration Scripts
+GO
+IF NOT EXISTS (SELECT TOP 1 1 FROM tblCTEvent WHERE strEventName = 'Unapproved Contract Samples')
+BEGIN
+	INSERT INTO tblCTEvent (
+		[strEventName]
+		,[strEventDescription]
+		,[intActionId]
+		,[strAlertType]
+		,[strNotificationType]
+		,[ysnSummarized]
+		,[ysnActive]
+		,[intDaysToRemind]
+		,[strReminderCondition]
+		,[intAlertFrequency]
+		,[strSubject]
+		,[strMessage]
+		,[intConcurrencyId]
+		)
+	VALUES (
+		'Unapproved Contract Samples'
+		,'Unapproved Contract Samples'
+		,NULL
+		,'Reminder'
+		,'Screen'
+		,0
+		,1
+		,15
+		,'day(s) before due date'
+		,1
+		,''
+		,''
+		,1
+		)
+END
+GO
+IF NOT EXISTS (SELECT TOP 1 1 FROM tblCTEvent WHERE strEventName = 'Unapproved FOB Contract Samples')
+BEGIN
+	INSERT INTO tblCTEvent (
+		[strEventName]
+		,[strEventDescription]
+		,[intActionId]
+		,[strAlertType]
+		,[strNotificationType]
+		,[ysnSummarized]
+		,[ysnActive]
+		,[intDaysToRemind]
+		,[strReminderCondition]
+		,[intAlertFrequency]
+		,[strSubject]
+		,[strMessage]
+		,[intConcurrencyId]
+		)
+	VALUES (
+		'Unapproved FOB Contract Samples'
+		,'Unapproved FOB Contract Samples'
+		,NULL
+		,'Reminder'
+		,'Screen'
+		,0
+		,1
+		,21
+		,'day(s) before due date'
+		,1
+		,''
+		,''
+		,1
+		)
+END
+GO
