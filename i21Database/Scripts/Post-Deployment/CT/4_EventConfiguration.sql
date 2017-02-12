9ef28e2d419444e2f9584d16e0a25e01404bc39c
Swetosuvro Ghosh <swetosuvro.g@irely.com>
2017-02-13 02:05:14 +0530

    LG-808

--- i21Database/Scripts/Post-Deployment/CT/4_EventConfiguration.sql
+++ i21Database/Scripts/Post-Deployment/CT/4_EventConfiguration.sql
@@ -70,3 +70,234 @@ BEGIN
 		)
 END
 GO
+IF NOT EXISTS(SELECT 1 FROM tblCTEvent WHERE strEventName = 'Contract Without Shipping Instruction')
+BEGIN
+	INSERT INTO tblCTEvent (
+		 [strEventName]
+		,[strEventDescription]
+		,[intActionId]
+		,[strAlertType]
+		,[strNotificationType]
+		,[ysnSummarized]
+		,[ysnActive]
+		,[intDaysToRemind]
+		,[strReminderCondition]
+		,[intAlertFrequency]
+		,[strSubject]
+		,[strMessage]
+		,[intConcurrencyId]
+		)
+	VALUES (
+		'Contract Without Shipping Instruction'
+		,'Contract Without Shipping Instruction'
+		,NULL
+		,'Reminder'
+		,'Screen'
+		,0
+		,1
+		,30
+		,'day(s) before due date'
+		,1
+		,''
+		,''
+		,1)
+END
+GO
+IF NOT EXISTS(SELECT 1 FROM tblCTEvent WHERE strEventName = 'Contract Without Shipping Advice')
+BEGIN
+	INSERT INTO tblCTEvent (
+		 [strEventName]
+		,[strEventDescription]
+		,[intActionId]
+		,[strAlertType]
+		,[strNotificationType]
+		,[ysnSummarized]
+		,[ysnActive]
+		,[intDaysToRemind]
+		,[strReminderCondition]
+		,[intAlertFrequency]
+		,[strSubject]
+		,[strMessage]
+		,[intConcurrencyId]
+		)
+	VALUES (
+		'Contract Without Shipping Advice'
+		,'Contract Without Shipping Advice'
+		,NULL
+		,'Reminder'
+		,'Screen'
+		,0
+		,1
+		,5
+		,'day(s) after due date'
+		,1
+		,''
+		,''
+		,1)
+END
+GO
+IF NOT EXISTS(SELECT 1 FROM tblCTEvent WHERE strEventName = 'Contract Without Document')
+BEGIN
+	INSERT INTO tblCTEvent (
+		 [strEventName]
+		,[strEventDescription]
+		,[intActionId]
+		,[strAlertType]
+		,[strNotificationType]
+		,[ysnSummarized]
+		,[ysnActive]
+		,[intDaysToRemind]
+		,[strReminderCondition]
+		,[intAlertFrequency]
+		,[strSubject]
+		,[strMessage]
+		,[intConcurrencyId]
+		)
+	VALUES (
+		'Contract Without Document'
+		,'Contract Without Document'
+		,NULL
+		,'Reminder'
+		,'Screen'
+		,0
+		,1
+		,7
+		,'day(s) before due date'
+		,1
+		,''
+		,''
+		,1)
+END
+GO
+IF NOT EXISTS(SELECT 1 FROM tblCTEvent WHERE strEventName = 'Contract Without Weight Claim')
+BEGIN
+	INSERT INTO tblCTEvent (
+		 [strEventName]
+		,[strEventDescription]
+		,[intActionId]
+		,[strAlertType]
+		,[strNotificationType]
+		,[ysnSummarized]
+		,[ysnActive]
+		,[intDaysToRemind]
+		,[strReminderCondition]
+		,[intAlertFrequency]
+		,[strSubject]
+		,[strMessage]
+		,[intConcurrencyId]
+		)
+	VALUES (
+		'Contract Without Weight Claim'
+		,'Contract Without Weight Claim'
+		,NULL
+		,'Reminder'
+		,'Screen'
+		,0
+		,1
+		,37
+		,'day(s) after due date'
+		,1
+		,''
+		,''
+		,1)
+END
+GO
+IF NOT EXISTS(SELECT 1 FROM tblCTEvent WHERE strEventName = 'Weight Claims w/o Debit Note')
+BEGIN
+	INSERT INTO tblCTEvent (
+		 [strEventName]
+		,[strEventDescription]
+		,[intActionId]
+		,[strAlertType]
+		,[strNotificationType]
+		,[ysnSummarized]
+		,[ysnActive]
+		,[intDaysToRemind]
+		,[strReminderCondition]
+		,[intAlertFrequency]
+		,[strSubject]
+		,[strMessage]
+		,[intConcurrencyId]
+		)
+	VALUES (
+		'Weight Claims w/o Debit Note'
+		,'Weight Claims w/o Debit Note'
+		,NULL
+		,'Reminder'
+		,'Screen'
+		,0
+		,1
+		,28
+		,'day(s) after due date'
+		,1
+		,''
+		,''
+		,1)
+END
+GO
+IF NOT EXISTS(SELECT 1 FROM tblCTEvent WHERE strEventName = 'Contracts w/o 4C')
+BEGIN
+	INSERT INTO tblCTEvent (
+		 [strEventName]
+		,[strEventDescription]
+		,[intActionId]
+		,[strAlertType]
+		,[strNotificationType]
+		,[ysnSummarized]
+		,[ysnActive]
+		,[intDaysToRemind]
+		,[strReminderCondition]
+		,[intAlertFrequency]
+		,[strSubject]
+		,[strMessage]
+		,[intConcurrencyId]
+		)
+	VALUES (
+		'Contracts w/o 4C'
+		,'Contracts w/o 4C'
+		,NULL
+		,'Reminder'
+		,'Screen'
+		,0
+		,1
+		,28
+		,'day(s) after due date'
+		,1
+		,''
+		,''
+		,1)
+END
+GO
+IF NOT EXISTS(SELECT 1 FROM tblCTEvent WHERE strEventName = 'Contracts w/o TC')
+BEGIN
+	INSERT INTO tblCTEvent (
+		 [strEventName]
+		,[strEventDescription]
+		,[intActionId]
+		,[strAlertType]
+		,[strNotificationType]
+		,[ysnSummarized]
+		,[ysnActive]
+		,[intDaysToRemind]
+		,[strReminderCondition]
+		,[intAlertFrequency]
+		,[strSubject]
+		,[strMessage]
+		,[intConcurrencyId]
+		)
+	VALUES (
+		'Contracts w/o TC'
+		,'Contracts w/o TC'
+		,NULL
+		,'Reminder'
+		,'Screen'
+		,0
+		,1
+		,28
+		,'day(s) after due date'
+		,1
+		,''
+		,''
+		,1)
+END
+GO
