5ce4400114f3a36213e5ad94fb9b541f4757a682
Chakra <CBehera@irely.net>
2017-02-13 23:27:26 +0530

    IN-42

--- i21Database/dbo/Functions/fnIPConvertSAPUOMToi21.sql
+++ i21Database/dbo/Functions/fnIPConvertSAPUOMToi21.sql
@@ -6,8 +6,11 @@ RETURNS NVARCHAR(100)
 AS
 BEGIN
 	Declare @strValue NVARCHAR(100)
+	Declare @strSymbol NVARCHAR(100)
 
-	Select TOP 1 @strValue=stri21UOM From tblIPSAPUOM Where strSAPUOM=@strSAPUOM
+	Select TOP 1 @strSymbol=stri21UOM From tblIPSAPUOM Where strSAPUOM=@strSAPUOM
+
+	Select TOP 1 @strValue=strUnitMeasure From tblICUnitMeasure Where strSymbol=@strSymbol
 
 	Return ISNULL(@strValue,'')
 END
