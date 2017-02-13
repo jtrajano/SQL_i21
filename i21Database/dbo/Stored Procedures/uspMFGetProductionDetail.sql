5406d2e806989ac095dfea257447596894519d87
Premkumar <Prem.Selladurai@gmail.com>
2017-02-13 22:42:59 +0530

    MFG-2311

--- i21Database/dbo/Stored Procedures/uspMFGetProductionDetail.sql
+++ i21Database/dbo/Stored Procedures/uspMFGetProductionDetail.sql
@@ -1,15 +1,31 @@
 ﻿CREATE PROCEDURE uspMFGetProductionDetail (@intWorkOrderId INT)
 AS
 BEGIN
-	SELECT W.intWorkOrderProducedLotId,
-		L.intLotId
+	SELECT W.intWorkOrderProducedLotId
+		,L.intLotId
 		,L.strLotNumber
 		,I.strItemNo
 		,I.strDescription
-		,CASE WHEN IU.ysnStockUnit =1 THEN W.dblQuantity ELSE W.dblPhysicalCount END AS dblQuantity
-		,CASE WHEN IU.ysnStockUnit =1 THEN IU.intItemUOMId ELSE IU1.intItemUOMId END AS intItemUOMId
-		,CASE WHEN IU.ysnStockUnit =1 THEN U.intUnitMeasureId ELSE U1.intUnitMeasureId END AS intUnitMeasureId
-		,CASE WHEN IU.ysnStockUnit =1 THEN U.strUnitMeasure ELSE U1.strUnitMeasure END AS strUnitMeasure
+		,CASE 
+			WHEN IU.ysnStockUnit = 1
+				THEN W.dblQuantity
+			ELSE W.dblPhysicalCount
+			END AS dblQuantity
+		,CASE 
+			WHEN IU.ysnStockUnit = 1
+				THEN IU.intItemUOMId
+			ELSE IU1.intItemUOMId
+			END AS intItemUOMId
+		,CASE 
+			WHEN IU.ysnStockUnit = 1
+				THEN U.intUnitMeasureId
+			ELSE U1.intUnitMeasureId
+			END AS intUnitMeasureId
+		,CASE 
+			WHEN IU.ysnStockUnit = 1
+				THEN U.strUnitMeasure
+			ELSE U1.strUnitMeasure
+			END AS strUnitMeasure
 		,W.dtmCreated
 		,W.intCreatedUserId
 		,US.strUserName
@@ -22,24 +38,28 @@ BEGIN
 		,C.strContainerId
 		,S.intShiftId
 		,S.strShiftName
-		,W.intBatchId 
-		,L.intParentLotId 
-		,W.strBatchId 
-		,W.ysnFillPartialPallet 
+		,W.intBatchId
+		,L.intParentLotId
+		,W.strBatchId
+		,W.ysnFillPartialPallet
 		,I.intCategoryId
 		,LS.strSecondaryStatus AS strLotStatus
+		,PL.strParentLotNumber
+		,L1.strLotNumber AS strSpecialPalletId
 	FROM dbo.tblMFWorkOrderProducedLot W
-	Left JOIN dbo.tblICLot L ON L.intLotId = W.intLotId
+	LEFT JOIN dbo.tblICLot L ON L.intLotId = W.intLotId
+	LEFT JOIN dbo.tblICParentLot PL ON PL.intParentLotId = L.intParentLotId
 	JOIN dbo.tblICItem I ON I.intItemId = W.intItemId
 	JOIN dbo.tblICItemUOM IU ON IU.intItemUOMId = W.intItemUOMId
 	JOIN dbo.tblICUnitMeasure U ON U.intUnitMeasureId = IU.intUnitMeasureId
-	JOIN dbo.tblICItemUOM IU1 ON IU1.intItemUOMId = W.intPhysicalItemUOMId 
+	JOIN dbo.tblICItemUOM IU1 ON IU1.intItemUOMId = W.intPhysicalItemUOMId
 	JOIN dbo.tblICUnitMeasure U1 ON U1.intUnitMeasureId = IU1.intUnitMeasureId
 	JOIN dbo.tblSMUserSecurity US ON US.[intEntityUserSecurityId] = W.intCreatedUserId
 	LEFT JOIN dbo.tblICStorageLocation SL ON SL.intStorageLocationId = W.intStorageLocationId
 	LEFT JOIN dbo.tblICContainer C ON C.intContainerId = W.intContainerId
 	LEFT JOIN dbo.tblMFShift S ON S.intShiftId = W.intShiftId
 	LEFT JOIN tblICLotStatus LS ON LS.intLotStatusId = L.intLotStatusId
+	LEFT JOIN dbo.tblICLot L1 ON L1.intLotId = W.intSpecialPalletLotId
 	WHERE intWorkOrderId = @intWorkOrderId
 	ORDER BY W.intWorkOrderProducedLotId
 END
