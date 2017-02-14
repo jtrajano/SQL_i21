f9aa250f7b6cc10eb64265c33a49cca5899cf899
Kelvin Quinto <kelvin.quinto@irely.com>
2017-02-14 14:51:42 +0800

    AR-4150

--- i21Database/dbo/Tables/tblARCustomerFailedImport.sql
+++ i21Database/dbo/Tables/tblARCustomerFailedImport.sql
@@ -2,6 +2,7 @@
 (
 	[intCustomerFailedImportId]			INT IDENTITY(1,1) NOT NULL,
 	[strCustomerNumber]					NVARCHAR(100)  COLLATE Latin1_General_CI_AS  NOT NULL,
-	[strReason]							NVARCHAR(MAX)  COLLATE Latin1_General_CI_AS  NULL
+	[strReason]							NVARCHAR(MAX)  COLLATE Latin1_General_CI_AS  NULL,
+	[intConcurrencyId]					INT NOT NULL DEFAULT ((0)), 
 	CONSTRAINT [PK_tblARCustomerFailedImport] PRIMARY KEY  CLUSTERED ([intCustomerFailedImportId] ASC)
 )
