0a3294442c1fe501cf76044a6d84e334e9de7bf8
Michelle Jardiolin <michelle.jardiolin@irely.com>
2017-02-14 17:23:01 +0800

    AR-4153

--- i21Database/dbo/Views/vyuARBatchPostingTransaction.sql
+++ i21Database/dbo/Views/vyuARBatchPostingTransaction.sql
@@ -7,10 +7,20 @@ SELECT
 	,COUNT(AR.intTransactionId) dblEntriesCount
 	,SUM(AR.dblTotal) dblTotalAmount
 	,AR.strUserName
-	,AR.strLocationName
-	
+	,AR.strLocationName	
 FROM
-	tblGLDetail GL
+	(SELECT 
+		strBatchId
+		, strCode
+		, intTransactionId
+		, strTransactionId
+		, dtmDate
+		, strTransactionType
+		, intAccountId
+	 FROM
+		tblGLDetail
+	 WHERE
+		ysnIsUnposted = 0) GL
 INNER JOIN
 	(
 		SELECT
@@ -22,11 +32,29 @@ INNER JOIN
 			,strUserName		= E.strName 
 			,strLocationName	= LOC.strLocationName
 		FROM
-			tblARInvoice INV
-			LEFT OUTER JOIN tblEMEntity E ON INV.intEntityId = E.intEntityId								
-			LEFT OUTER JOIN 	dbo.tblSMCompanyLocation AS LOC ON INV.intCompanyLocationId  = LOC.intCompanyLocationId 
-		WHERE
-			INV.ysnPosted = 1
+			(SELECT 
+				intInvoiceId
+				, strInvoiceNumber
+				, intEntityId
+				, intAccountId
+				, intCompanyLocationId
+				, dblInvoiceTotal
+			 FROM 
+				tblARInvoice
+			 WHERE
+				ysnPosted = 1) INV
+			 LEFT OUTER JOIN 
+				(SELECT 
+					intEntityId
+					, strName
+				 FROM 
+					tblEMEntity) E ON INV.intEntityId = E.intEntityId								
+			 LEFT OUTER JOIN 	
+				 (SELECT 
+					intCompanyLocationId
+					, strLocationName
+				  FROM 
+					dbo.tblSMCompanyLocation) LOC ON INV.intCompanyLocationId  = LOC.intCompanyLocationId 
 			
 		UNION
 		
@@ -39,29 +67,38 @@ INNER JOIN
 			,strUserName		= E.strName 
 			,strLocationName	= LOC.strLocationName
 		FROM
-			tblARPayment AR
-			LEFT OUTER JOIN tblEMEntity E ON AR.intEntityId = E.intEntityId			
-			LEFT OUTER JOIN 	dbo.tblSMCompanyLocation AS LOC ON AR.intLocationId  = LOC.intCompanyLocationId 
-		WHERE
-			AR.ysnPosted = 1
-			
+			(SELECT 
+				intPaymentId
+				, strRecordNumber
+				, intAccountId
+				, intEntityId
+				, dblAmountPaid
+				, intLocationId
+			 FROM 
+				tblARPayment
+			 WHERE 
+				ysnPosted = 1) AR
+			LEFT OUTER JOIN 
+				(SELECT 
+					intEntityId
+					, strName
+				 FROM 
+					tblEMEntity) E ON AR.intEntityId = E.intEntityId			
+			LEFT OUTER JOIN 
+				(SELECT
+					intCompanyLocationId
+					, strLocationName
+				 FROM 
+					dbo.tblSMCompanyLocation) LOC ON AR.intLocationId  = LOC.intCompanyLocationId 			
 	) AR
-
-		ON GL.intTransactionId = AR.intTransactionId
-
-	
-		AND GL.strTransactionType IN ('Invoice','Receive Payments')
-		AND GL.strCode = 'AR'		
-		AND GL.strTransactionId = AR.strTransactionId
-		AND GL.intAccountId = AR.intAccountId
-		AND GL.strTransactionType = AR.strPostingType
-		AND GL.ysnIsUnposted = 0			
+ON GL.intTransactionId = AR.intTransactionId
+AND GL.strTransactionType IN ('Invoice','Receive Payments', 'Credit Memo')
+AND GL.strCode = 'AR'		
+AND GL.strTransactionId = AR.strTransactionId
+AND GL.intAccountId = AR.intAccountId		
 GROUP BY
 	 GL.strBatchId
 	,GL.dtmDate
 	,GL.strTransactionType 
 	,AR.strUserName
 	,AR.strLocationName
-	
-	
-
