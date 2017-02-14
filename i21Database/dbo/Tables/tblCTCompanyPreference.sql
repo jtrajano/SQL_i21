695aa6c04f45a501dd7f834316ef6992a3f3ea1a
Satya <satya.sahoo@irely.com>
2017-02-14 23:03:27 +0530

    CT-1055

--- i21Database/dbo/Tables/tblCTCompanyPreference.sql
+++ i21Database/dbo/Tables/tblCTCompanyPreference.sql
@@ -32,6 +32,7 @@
 	ysnRoundLotsForMultiPrice BIT,
 	ysnMultiPriceOnBasis BIT,
 	intDefContainerTypeId INT,
+	ysnFeedOnApproval BIT,
 
     CONSTRAINT [PK_tblCTCompanyPreference_intCompanyPreferenceId] PRIMARY KEY CLUSTERED ([intCompanyPreferenceId] ASC),
 	CONSTRAINT [FK_tblCTCompanyPreference_tblSMCurrency_intCleanCostCurrencyId_intCurrencyId] FOREIGN KEY ([intCleanCostCurrencyId]) REFERENCES [tblSMCurrency]([intCurrencyID]),
