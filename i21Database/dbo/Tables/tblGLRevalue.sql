50a1966ec684f1f07b8348dac902051cd03b9eb4
jeffrey trajano <jeffrey.trajano@irely.com>
2017-02-15 17:13:56 +0800

    GL-3170 Revalue currency

--- i21Database/dbo/Tables/tblGLRevalue.sql
+++ i21Database/dbo/Tables/tblGLRevalue.sql
@@ -1,8 +1,11 @@
 ﻿CREATE TABLE [dbo].[tblGLRevalue](
 	[intConsolidationId] [int] IDENTITY(1,1) NOT NULL,
+	[strConsolidationNumber]  NVARCHAR(20) COLLATE Latin1_General_CI_AS ,
 	[intGLFiscalYearPeriodId] [int] NOT NULL,
+	[dtmDate] [date] NOT NULL,
 	[dtmReverseDate] [date] NOT NULL,
 	[intFunctionalCurrencyId] [int] NOT NULL,
+	[intTransactionCurrencyId] [int] NOT NULL,
 	[strTransactionType] [nvarchar](20) COLLATE Latin1_General_CI_AS NOT NULL,
 	[dblForexRate] [numeric](10, 6) NOT NULL,
 	[intConcurrencyId] [int] NOT NULL,
