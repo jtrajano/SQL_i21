248025cfe6d5c6b1eac09249c8f4efbb14e29509
Lex Eugene Peregrino <lex.peregrino@irely.com>
2017-02-16 13:58:06 +0800

    AP-3223 - Unable to Publish Database Project (Add the strRateType) Field on AP GL Entries function

--- i21Database/dbo/Functions/fnAPCreateWriteOffGLEntries.sql
+++ i21Database/dbo/Functions/fnAPCreateWriteOffGLEntries.sql
@@ -36,6 +36,7 @@ RETURNS @returntable TABLE
     [dblCreditReport]           NUMERIC (18, 6) NULL,
     [dblReportingRate]          NUMERIC (18, 6) NULL,
     [dblForeignRate]            NUMERIC (18, 6) NULL,
+	[strRateType]				NVARCHAR (255)   COLLATE Latin1_General_CI_AS NULL,
 	[intConcurrencyId]          INT              DEFAULT 1 NOT NULL
 )
 AS
@@ -76,6 +77,7 @@ BEGIN
 			[dblCreditReport]				=	0,
 			[dblReportingRate]				=	0,
 			[dblForeignRate]				=	0,
+			[strRateType]				=	NULL  ,
 			[intConcurrencyId]			=	1
 		FROM	[dbo].tblAPPayment A 
 		INNER JOIN tblAPVendor C
@@ -114,6 +116,7 @@ BEGIN
 			[dblCreditReport]				=	0,
 			[dblReportingRate]				=	0,
 			[dblForeignRate]				=	0,
+			[strRateType]				=	NULL  ,
 			[intConcurrencyId]			=	1
 		FROM	[dbo].tblAPPayment A 
 				INNER JOIN tblAPPaymentDetail B ON A.intPaymentId = B.intPaymentId
