d5e4c18b623ae0d096adcd8bfacc785051f0095d
Gopinath Nayak <gopinath.n@irely.com>
2017-02-17 15:00:32 +0530

    GRN-529

--- i21Database/dbo/Views/vyuGRCustomerStorageNotMapped.sql
+++ i21Database/dbo/Views/vyuGRCustomerStorageNotMapped.sql
@@ -11,7 +11,9 @@ SELECT
 ,CS.intCompanyLocationId  
 ,LOC.strLocationName
 ,ISNULL(CS.intCompanyLocationSubLocationId,0) intCompanyLocationSubLocationId
-,ISNULL(SLOC.strSubLocationName,'') strSubLocationName 
+,ISNULL(SLOC.strSubLocationName,'') strSubLocationName
+,CS.intDiscountScheduleId
+,DS.strDiscountDescription 
 FROM tblGRCustomerStorage CS  
 JOIN tblGRStorageType ST ON ST.intStorageScheduleTypeId = CS.intStorageTypeId  
 JOIN tblSMCompanyLocation LOC ON LOC.intCompanyLocationId = CS.intCompanyLocationId  
@@ -19,5 +21,6 @@ JOIN tblEMEntity E ON E.intEntityId = CS.intEntityId
 JOIN tblICCommodity COM ON COM.intCommodityId = CS.intCommodityId
 JOIN tblGRStorageScheduleRule SR ON SR.intStorageScheduleRuleId=CS.intStorageScheduleId
 JOIN tblICItem Item ON Item.intItemId = CS.intItemId
+JOIN tblGRDiscountSchedule DS ON DS.intDiscountScheduleId=CS.intDiscountScheduleId
 LEFT JOIN tblSMCompanyLocationSubLocation SLOC ON SLOC.intCompanyLocationSubLocationId=CS.intCompanyLocationSubLocationId
-WHERE  ISNULL(CS.strStorageType,'') <> 'ITR' 
+WHERE  ISNULL(CS.strStorageType,'') <> 'ITR'
