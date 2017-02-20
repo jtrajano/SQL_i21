28b6e535fcd3def15b41d0dbad7e5ea3d9fee42d
Mark Paragas <mark.paragas@irely.com>
2017-02-20 20:16:48 +0800

    MFT-362

--- i21Database/dbo/Tables/tblTFReportingComponentCustomer.sql
+++ i21Database/dbo/Tables/tblTFReportingComponentCustomer.sql
@@ -3,6 +3,8 @@
 	[intReportingComponentCustomerId] INT IDENTITY NOT NULL,
 	[intReportingComponentId] INT NOT NULL,
 	[intEntityCustomerId] INT NOT NULL,
+	[strCustomerNumber] NVARCHAR (150) COLLATE Latin1_General_CI_AS NULL,
+	[ysnInclude] [bit] NOT NULL,
 	[intConcurrencyId] INT DEFAULT ((1)) NULL, 
     CONSTRAINT [PK_tblTFReportingComponentCustomer] PRIMARY KEY ([intReportingComponentCustomerId]), 
     CONSTRAINT [AK_tblTFReportingComponentCustomer] UNIQUE ([intReportingComponentId], [intEntityCustomerId]), 
