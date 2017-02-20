5a92ebfc7cb7e74454e7f28285f3f3b7867ab3f2
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2017-02-20 17:25:52 +0530

    RM-653

--- i21Database/dbo/Stored Procedures/uspRKPostMatchPnS.sql
+++ i21Database/dbo/Stored Procedures/uspRKPostMatchPnS.sql
@@ -14,7 +14,7 @@ BEGIN TRY
 
 	SELECT TOP 1 @strUserName=strExternalERPId from tblEMEntity where strName=@strUserName
 
-	SELECT @intCurrencyId = intCurrencyId,@BrokerAccount=strAccountNumber,@BrokerName=strName FROM tblRKMatchFuturesPSHeader h
+	SELECT @intCurrencyId = intCurrencyId,@BrokerAccount=strClearingAccountNumber,@BrokerName=strName FROM tblRKMatchFuturesPSHeader h
 	JOIN tblRKFutureMarket fm on h.intFutureMarketId=fm.intFutureMarketId 
 	JOIN tblRKBrokerageAccount ba on ba.intBrokerageAccountId=h.intBrokerageAccountId
 	join tblEMEntity e on h.intEntityId=e.intEntityId
