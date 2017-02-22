3c16b17ed8cee09e36b60983b21b2ad4c0d04bd1
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2017-02-22 21:18:16 +0530

    RM-655

--- i21Database/dbo/Views/vyuRKM2MInquiryBasisNotMapping.sql
+++ i21Database/dbo/Views/vyuRKM2MInquiryBasisNotMapping.sql
@@ -2,7 +2,7 @@
 
 AS 
 
-SELECT intM2MBasisDetailId
+SELECT  intM2MInquiryBasisDetailId
 		,strCommodityCode
 		,strContractType
 		,strCurrency
@@ -13,17 +13,15 @@ SELECT intM2MBasisDetailId
 		,strPricingType
 		,strLocationName
 		,strUnitMeasure
- FROM tblRKM2MBasisDetail bd
-join tblRKM2MBasis mb on mb.intM2MBasisId=bd.intM2MBasisId
+ FROM tblRKM2MInquiry bd
+join tblRKM2MInquiryBasisDetail mb on mb.intM2MInquiryId=bd.intM2MInquiryId
 join tblICCommodity c on c.intCommodityId=bd.intCommodityId
 LEFT JOIN tblARMarketZone z on z.intMarketZoneId=bd.intMarketZoneId
-LEFT JOIN tblCTContractType ct on ct.intContractTypeId=bd.intContractTypeId
-LEFT JOIN tblICItem i on i.intItemId=bd.intItemId
-LEFT JOIN tblRKFutureMarket m on m.intFutureMarketId=bd.intFutureMarketId
-LEFT JOIN tblRKFuturesMonth mo on mo.intFutureMonthId=bd.intFutureMonthId
-LEFT JOIN tblSMCompanyLocation l on l.intCompanyLocationId=bd.intCompanyLocationId
-LEFT JOIN tblSMCurrency cur on cur.intCurrencyID=bd.intCurrencyId
-LEFT JOIN tblCTPricingType pt on pt.intPricingTypeId=bd.intPricingTypeId
-LEFT JOIN tblICUnitMeasure um on um.intUnitMeasureId=bd.intUnitMeasureId
-
-
+LEFT JOIN tblCTContractType ct on ct.intContractTypeId=mb.intContractTypeId
+LEFT JOIN tblICItem i on i.intItemId=mb.intItemId
+LEFT JOIN tblRKFutureMarket m on m.intFutureMarketId=mb.intFutureMarketId
+LEFT JOIN tblRKFuturesMonth mo on mo.intFutureMonthId=mb.intFutureMonthId
+LEFT JOIN tblSMCompanyLocation l on l.intCompanyLocationId=mb.intCompanyLocationId
+LEFT JOIN tblSMCurrency cur on cur.intCurrencyID=mb.intCurrencyId
+LEFT JOIN tblCTPricingType pt on pt.intPricingTypeId=mb.intPricingTypeId
+LEFT JOIN tblICUnitMeasure um on um.intUnitMeasureId=mb.intUnitMeasureId
