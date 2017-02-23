d1abf11e1170e4fdfad2b4da376c5efe3a2c0af3
Feb Montefrio <feb.montefrio@irely.com>
2017-02-24 04:09:39 +0800

    IC-3299 IC-3297 IC-3300 IC-3298

---
+++ i21Database/dbo/Views/vyuICMultiCurrencyRevalueShipmentOtherCharges.sql
@@ -0,0 +1,28 @@
+﻿CREATE VIEW [dbo].[vyuICMultiCurrencyRevalueShipmentOtherCharges]
+AS
+SELECT DISTINCT
+	 strTransactionType			= CAST(NULL AS NVARCHAR(50))
+	,strTransactionId			= CAST(NULL AS NVARCHAR(50))
+	,strTransactionDate			= CAST(NULL AS NVARCHAR(50))
+	,strTransactionDueDate		= CAST(NULL AS NVARCHAR(50))
+	,strVendorName				= CAST(NULL AS NVARCHAR(50))
+	,strCommodity				= CAST(NULL AS NVARCHAR(50))
+	,strLineOfBusiness			= CAST(NULL AS NVARCHAR(50))
+	,strLocation				= CAST(NULL AS NVARCHAR(50))
+	,strTicket					= CAST(NULL AS NVARCHAR(50))
+	,strContractNumber			= CAST(NULL AS NVARCHAR(50))
+	,strItemId					= CAST(NULL AS NVARCHAR(50))
+	,dblQuantity				= CAST(NULL AS NUMERIC(18, 6))
+	,dblUnitPrice				= CAST(NULL AS NUMERIC(18, 6))
+	,dblAmount					= CAST(NULL AS NUMERIC(18, 6))
+	,intCurrencyId				= CAST(NULL AS INT)
+	,intForexRateType			= CAST(NULL AS INT)
+	,strForexRateType			= CAST(NULL AS NVARCHAR(50))
+	,dblForexRate				= CAST(NULL AS NUMERIC(18, 6))
+	,dblHistoricAmount			= CAST(NULL AS NUMERIC(18, 6))
+	,dblNewForexRate			= 0 --Calcuate By GL
+	,dblNewAmount				= 0 --Calcuate By GL
+	,dblUnrealizedDebitGain		= 0 --Calcuate By GL
+	,dblUnrealizedCreditGain	= 0 --Calcuate By GL
+	,dblDebit					= 0 --Calcuate By GL
+	,dblCredit					= 0 --Calcuate By GL
