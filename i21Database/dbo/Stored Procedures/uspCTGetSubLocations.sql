b3d1b75a5fd1fd244987fe8579bde38d458d9582
Satya <satya.sahoo@irely.com>
2017-02-24 22:47:54 +0530

    CT-1070, CT-1080

---
+++ i21Database/dbo/Stored Procedures/uspCTGetSubLocations.sql
@@ -0,0 +1,43 @@
+﻿CREATE PROCEDURE [dbo].[uspCTGetSubLocations]
+	@intItemId				INT,
+	@intCompanyLocationId	INT,
+	@intSubLocationId		INT,
+	@strSubLocationName		NVARCHAR(200)
+AS
+
+BEGIN
+
+	IF EXISTS	(
+					SELECT	* 
+					FROM	tblICItemSubLocation SL
+					JOIN	tblSMCompanyLocationSubLocation CS	ON CS.intCompanyLocationSubLocationId = SL.intSubLocationId
+					WHERE	intItemId = @intItemId AND CS.intCompanyLocationId = @intCompanyLocationId
+				)
+	BEGIN
+		SELECT	SL.intItemSubLocationId,
+				CS.intCompanyLocationId,
+				CS.intCompanyLocationSubLocationId,
+				CS.strSubLocationName,
+				CS.intCountryId,
+				SL.intItemId 
+		FROM	tblICItemSubLocation SL
+		JOIN	tblSMCompanyLocationSubLocation CS	ON CS.intCompanyLocationSubLocationId = SL.intSubLocationId
+		WHERE	intItemId = @intItemId AND CS.intCompanyLocationId = @intCompanyLocationId
+		AND		CS.strSubLocationName  LIKE '%' + @strSubLocationName + '%'
+		AND		CS.intCompanyLocationSubLocationId =(CASE WHEN @intSubLocationId > 0 THEN @intSubLocationId ELSE CS.intCompanyLocationSubLocationId END)
+	END		
+	ELSE
+	BEGIN
+		SELECT	CS.intCompanyLocationSubLocationId AS intItemSubLocationId,
+				CS.intCompanyLocationId,
+				CS.intCompanyLocationSubLocationId,
+				CS.strSubLocationName,
+				CS.intCountryId,
+				@intItemId AS intItemId 
+		FROM	tblSMCompanyLocationSubLocation CS
+		WHERE	CS.intCompanyLocationId = @intCompanyLocationId
+		AND		CS.strSubLocationName  LIKE '%' + @strSubLocationName + '%'
+		AND		CS.intCompanyLocationSubLocationId =(CASE WHEN @intSubLocationId > 0 THEN @intSubLocationId ELSE CS.intCompanyLocationSubLocationId END)
+	END
+
+END
