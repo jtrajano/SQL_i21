7c568f99cd943d26c4b3ccb9183f757d694c1042
Jayson Ramos <jayson.ramos@iRely.com>
2017-02-28 10:15:47 +0800

    HD-1942

--- i21Database/dbo/Views/vyuHDParticipant.sql
+++ i21Database/dbo/Views/vyuHDParticipant.sql
@@ -1,5 +1,22 @@
 ﻿CREATE VIEW [dbo].[vyuHDParticipant]
 	AS
+		select
+			intId = vyuEMEntityContact2.intEntityContactId
+			,intEntityId = vyuEMEntityContact2.intEntityId
+			,strName = vyuEMEntityContact2.strName
+			,strFullName = vyuEMEntityContact2.strName
+			,strEmail = vyuEMEntityContact2.strEmail
+			,ysnDisabled = (case when vyuEMEntityContact2.ysnActive = convert(bit,1) then convert(bit,0) else convert(bit,1) end)
+			,strPhone = vyuEMEntityContact2.strPhone
+			,strMobile = vyuEMEntityContact2.strMobile
+			,strType = vyuEMEntityContact2.strContactType
+		from
+			vyuEMEntityContact2
+		where
+			ysnDefaultContact = convert(bit,1)
+			and strName is not null
+
+	/*
 	select
 		intId = (case when result.ysnPortalAccess = 1 then result.intEntityContactId else result.intEntityId end)
 		,intEntityId = (case when result.ysnPortalAccess = 1 then result.intEntityContactId else result.intEntityId end)
@@ -16,3 +33,4 @@
 	union all
 	select * from vyuEMEntityContact where [User] <> 1 and ysnPortalAccess = 1
 	) as result
+	*/
