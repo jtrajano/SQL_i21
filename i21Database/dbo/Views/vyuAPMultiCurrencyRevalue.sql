7eaef8ef193d554f65dcc63155fede6752c94e66
Lex Eugene Peregrino <lex.peregrino@irely.com>
2017-03-01 13:50:49 +0800

    AP-3158 - Multi Currency changes - Vouchers - DB View for Revalue screen 1.1

---
+++ i21Database/dbo/Views/vyuAPMultiCurrencyRevalue.sql
@@ -0,0 +1,47 @@
+﻿CREATE VIEW [dbo].[vyuAPMultiCurrencyRevalue]
+AS
+SELECT DISTINCT
+	strTransactionType		=	'Voucher',
+	strTransactionId		=	A.strBillId,
+	strTransactionDate		=	A.dtmDate,
+	strTransactionDueDate	=	A.dtmDueDate,
+	strVendorName			=	B2.strName,
+	strCommodity			=	CC.strDescription,
+	strLineOfBusiness		=	CT.strDescription,
+	strLocation				=	EL.strLocationName,
+	strTicket				=	'',
+	strContractNumber		=	CH.strContractNumber,
+	strItemId				=	strName,
+	dblQuantity				=	BD.dblQtyReceived,
+	dblUnitPrice			=	0,
+	dblAmount				=	A.dblTotal,
+	intCurrencyId			=	A.intCurrencyId,
+	intForexRateType		=	BD.intCurrencyExchangeRateTypeId,
+	strForexRateType		=	CER.strCurrencyExchangeRateType,
+	dblForexRate			=	BD.dblRate,
+	dblHistoricAmount		=	A.dblTotal * BD.dblRate,
+	dblNewForexRate         =    0, --Calcuate By GL
+    dblNewAmount            =    0, --Calcuate By GL
+    dblUnrealizedDebitGain  =    0, --Calcuate By GL
+    dblUnrealizedCreditGain =    0, --Calcuate By GL
+    dblDebit                =    0, --Calcuate By GL
+    dblCredit               =    0  --Calcuate By GL
+FROM tblAPBill A
+INNER JOIN tblAPBillDetail BD
+	ON BD.intBillId = A.intBillId
+INNER JOIN (tblAPVendor B INNER JOIN tblEMEntity B2 ON B.intEntityVendorId = B2.intEntityId) 
+	ON A.intEntityVendorId = B.intEntityVendorId
+LEFT JOIN dbo.tblEMEntityLocation EL
+	ON EL.intEntityLocationId = A.intShipFromId
+LEFT JOIN dbo.tblCTContractHeader CH 
+	ON CH.intContractHeaderId = BD.intContractHeaderId
+LEFT JOIN tblICItem IT
+	ON BD.intItemId = IT.intItemId
+LEFT JOIN dbo.tblICCategory CT
+	ON IT.intCategoryId = CT.intCategoryId
+LEFT JOIN dbo.tblICCommodity CC
+	ON CC.intCommodityId = IT.intCommodityId
+LEFT JOIN dbo.tblSMCurrencyExchangeRateType CER
+	ON CER.intCurrencyExchangeRateTypeId = BD.intCurrencyExchangeRateTypeId
+WHERE A.ysnPosted = 1 AND A.ysnPaid = 0
+
