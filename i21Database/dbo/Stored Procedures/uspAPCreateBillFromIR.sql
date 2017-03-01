1f9b85477cd67cf8a3849aecca31f981f1f7d145
Lex Eugene Peregrino <lex.peregrino@irely.com>
2017-03-01 19:02:36 +0800

    AP-3259 - Negative receipt is creating a negative debit memo in voucher

--- i21Database/dbo/Stored Procedures/uspAPCreateBillFromIR.sql
+++ i21Database/dbo/Stored Procedures/uspAPCreateBillFromIR.sql
@@ -169,7 +169,7 @@ BEGIN
 		[intAccountId] 			=	@APAccount,
 		[strBillId]				=	@generatedBillRecordId,
 		[strReference] 			=	A.strBillOfLading,
-		[dblTotal] 				=	@totalReceiptAmount,
+		[dblTotal] 				=	ABS(@totalReceiptAmount),
 		[dblAmountDue]			=	@totalReceiptAmount,
 		[intEntityId]			=	@userId,
 		[ysnPosted]				=	0,
@@ -246,19 +246,19 @@ BEGIN
 													THEN CAST(CASE WHEN (E1.dblCashPrice > 0 AND B.dblUnitCost = 0) 
 																   THEN E1.dblCashPrice 
 																   ELSE B.dblUnitCost 
-															  END / ISNULL(A.intSubCurrencyCents,1)  * B.dblNet * ItemWeightUOM.dblUnitQty / ISNULL(ItemCostUOM.dblUnitQty,1) AS DECIMAL(18,2)) --Formula With Weight UOM
+															  END / ISNULL(A.intSubCurrencyCents,1)  * ABS(B.dblNet) * ItemWeightUOM.dblUnitQty / ISNULL(ItemCostUOM.dblUnitQty,1) AS DECIMAL(18,2)) --Formula With Weight UOM
 													WHEN (B.intUnitMeasureId > 0 AND B.intCostUOMId > 0)
-													THEN CAST((B.dblOpenReceive - B.dblBillQty) * (CASE WHEN E1.dblCashPrice > 0 THEN E1.dblCashPrice ELSE B.dblUnitCost END / ISNULL(A.intSubCurrencyCents,1)) *  (ItemUOM.dblUnitQty/ ISNULL(ItemCostUOM.dblUnitQty,1)) AS DECIMAL(18,2))  --Formula With Receipt UOM and Cost UOM
-													ELSE CAST((B.dblOpenReceive - B.dblBillQty) * (CASE WHEN E1.dblCashPrice > 0 THEN E1.dblCashPrice ELSE B.dblUnitCost END / ISNULL(A.intSubCurrencyCents,1))  AS DECIMAL(18,2))  --Orig Calculation
+													THEN CAST(ABS((B.dblOpenReceive - B.dblBillQty)) * (CASE WHEN E1.dblCashPrice > 0 THEN E1.dblCashPrice ELSE B.dblUnitCost END / ISNULL(A.intSubCurrencyCents,1)) *  (ItemUOM.dblUnitQty/ ISNULL(ItemCostUOM.dblUnitQty,1)) AS DECIMAL(18,2))  --Formula With Receipt UOM and Cost UOM
+													ELSE CAST(ABS((B.dblOpenReceive - B.dblBillQty)) * (CASE WHEN E1.dblCashPrice > 0 THEN E1.dblCashPrice ELSE B.dblUnitCost END / ISNULL(A.intSubCurrencyCents,1))  AS DECIMAL(18,2))  --Orig Calculation
 											   END) 
 										 ELSE (CASE WHEN B.intWeightUOMId > 0
 													THEN CAST(CASE WHEN (E1.dblCashPrice > 0 AND B.dblUnitCost = 0) 
 																   THEN E1.dblCashPrice 
 																   ELSE B.dblUnitCost 
-															  END * B.dblNet * ItemWeightUOM.dblUnitQty / ISNULL(ItemCostUOM.dblUnitQty,1) AS DECIMAL(18,2)) --Formula With Weight UOM
+															  END * ABS(B.dblNet) * ItemWeightUOM.dblUnitQty / ISNULL(ItemCostUOM.dblUnitQty,1) AS DECIMAL(18,2)) --Formula With Weight UOM
 													WHEN (B.intUnitMeasureId > 0  AND B.intCostUOMId > 0)
-													THEN CAST((B.dblOpenReceive - B.dblBillQty) * CASE WHEN E1.dblCashPrice > 0 THEN E1.dblCashPrice ELSE B.dblUnitCost END * (ItemUOM.dblUnitQty/ ISNULL(ItemCostUOM.dblUnitQty,1))  AS DECIMAL(18,2))  --Formula With Receipt UOM and Cost UOM
-													ELSE CAST((B.dblOpenReceive - B.dblBillQty) * CASE WHEN E1.dblCashPrice > 0 THEN E1.dblCashPrice ELSE B.dblUnitCost END  AS DECIMAL(18,2))  --Orig Calculation
+													THEN CAST(ABS((B.dblOpenReceive - B.dblBillQty)) * CASE WHEN E1.dblCashPrice > 0 THEN E1.dblCashPrice ELSE B.dblUnitCost END * (ItemUOM.dblUnitQty/ ISNULL(ItemCostUOM.dblUnitQty,1))  AS DECIMAL(18,2))  --Formula With Receipt UOM and Cost UOM
+													ELSE CAST(ABS((B.dblOpenReceive - B.dblBillQty)) * CASE WHEN E1.dblCashPrice > 0 THEN E1.dblCashPrice ELSE B.dblUnitCost END  AS DECIMAL(18,2))  --Orig Calculation
 											   END)
 										 END),0),
 		[dblCost]					=	CASE WHEN (B.dblUnitCost IS NULL OR B.dblUnitCost = 0)
@@ -272,7 +272,7 @@ BEGIN
 										 (CASE WHEN B.dblNet > 0 THEN B.dblUnitCost * (CAST(ItemWeightUOM.dblUnitQty AS DECIMAL(18,6)) / CAST(ISNULL(ItemCostUOM.dblUnitQty,1)AS DECIMAL(18,6))) 
 											   WHEN B.intCostUOMId > 0 THEN B.dblUnitCost * ( CAST(ItemUOM.dblUnitQty AS DECIMAL(18,6)) / CAST(ISNULL(ItemCostUOM.dblUnitQty,1)AS DECIMAL(18,6))) ELSE B.dblUnitCost END) / CASE WHEN B.ysnSubCurrency > 0 THEN ISNULL(A.intSubCurrencyCents,1) ELSE 1 END
 										) ELSE 0.00 END,0),
-		[dblNetWeight]				=	ISNULL(B.dblNet,0),
+		[dblNetWeight]				=	ABS(ISNULL(B.dblNet,0)),
 		[dblNetShippedWeight]		=	ISNULL(Loads.dblNet,0),
 		[dblWeightLoss]				=	ISNULL(B.dblGross - B.dblNet,0),
 		[dblFranchiseWeight]		=	CASE WHEN J.dblFranchise > 0 THEN ISNULL(B.dblGross,0) * (J.dblFranchise / 100) ELSE 0 END,
