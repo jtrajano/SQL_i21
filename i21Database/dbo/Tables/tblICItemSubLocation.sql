76d071dbcb60ad706b7d08fb626ab3c98453c39f
Wendell Wayne Estrada <wendell.estrada@irely.com>
2017-03-01 18:39:38 +0800

    IC-3373 Allow user to associate sublocation with an item

--- i21Database/dbo/Tables/tblICItemSubLocation.sql
+++ i21Database/dbo/Tables/tblICItemSubLocation.sql
@@ -14,16 +14,15 @@ If no records is specified for an item, this means the item can be used in any s
 */
 CREATE TABLE tblICItemSubLocation (
 	intItemSubLocationId INT NOT NULL IDENTITY,
-	intItemId INT NOT NULL,
+	[intItemLocationId] INT NOT NULL,
 	intSubLocationId INT NOT NULL ,
 	[intConcurrencyId] INT NULL DEFAULT ((0)), 
 	CONSTRAINT [PK_tblICItemSubLocation] PRIMARY KEY ([intItemSubLocationId]), 
-	CONSTRAINT [FK_tblICItemSubLocation_tblICItem] FOREIGN KEY ([intItemId]) REFERENCES [tblICItem]([intItemId]) ON DELETE CASCADE, 
+	CONSTRAINT [FK_tblICItemSubLocation_tblICItemLocation] FOREIGN KEY ([intItemLocationId]) REFERENCES [tblICItemLocation] ([intItemLocationId]) ON DELETE CASCADE,
 	CONSTRAINT [FK_tblICItemSubLocation_tblSMCompanyLocationSubLocation] FOREIGN KEY ([intSubLocationId]) REFERENCES [tblSMCompanyLocationSubLocation]([intCompanyLocationSubLocationId]),
-	CONSTRAINT [AK_tblICItemSubLocation] UNIQUE ([intItemId], [intSubLocationId])
 )
 GO
 
-CREATE NONCLUSTERED INDEX [IX_tblICItemSubLocation_intItemId_intLocationId]
-	ON [dbo].[tblICItemSubLocation]([intItemId] ASC, [intSubLocationId] ASC);
+CREATE UNIQUE INDEX [IX_tblICItemSubLocation_intItemLocationId_intSubLocationId]
+	ON [dbo].[tblICItemSubLocation]([intItemLocationId] ASC, [intSubLocationId] ASC);
 GO
