598c23a7b83487608d899d9f89f196f5996e3956
jeffrey trajano <jeffrey.trajano@irely.com>
2017-03-03 01:55:37 +0800

    GL-3170 Revalue currency

---
+++ i21Database/dbo/Stored Procedures/uspGLPostRevalue.sql
@@ -0,0 +1,325 @@
+CREATE PROCEDURE [dbo].[uspGLPostRevalue]
+	@intConsolidationId			AS INT = 1,
+	@ysnRecap					AS BIT				= 0,
+	@intEntityId				AS INT				= 1
+AS
+
+DECLARE @PostGLEntries RecapTableType
+DECLARE @ReversePostGLEntries RecapTableType
+DECLARE @strPostBatchId NVARCHAR(100) = ''
+DECLARE @strReversePostBatchId NVARCHAR(100) = ''
+
+BEGIN TRY
+	BEGIN TRANSACTION
+		DECLARE @errorNum INT
+		IF EXISTS(SELECT TOP 1 'Transaction is already posted' FROM tblGLRevalue WHERE intConsolidationId = @intConsolidationId AND ysnPosted = 1)
+				RAISERROR (60006,11,1)
+		IF @ysnRecap = 0
+			EXEC [dbo].uspGLGetNewID 3, @strPostBatchId OUTPUT
+		ELSE
+			SELECT @strPostBatchId =  NEWID()
+
+		;WITH cte as(
+		SELECT
+			 [strTransactionId]		= B.strConsolidationNumber
+			,[intTransactionId]		= B.intConsolidationId
+			,[strDescription]		= A.strTransactionId
+
+			,[dtmTransactionDate]	= B.dtmDate
+			,[dblDebit]				= CASE	WHEN dblUnrealizedGain < 0 THEN ABS(dblUnrealizedGain)
+											WHEN dblUnrealizedLoss < 0 THEN 0
+											ELSE dblUnrealizedLoss END
+			,[dblCredit]			= CASE	WHEN dblUnrealizedLoss < 0 THEN ABS(dblUnrealizedLoss)
+											WHEN dblUnrealizedGain < 0 THEN 0
+											ELSE dblUnrealizedGain END
+			,[dtmDate]				= ISNULL(B.[dtmDate], GETDATE())
+			,[ysnIsUnposted]		= 0
+			,[intConcurrencyId]		= 1
+			,[intCurrencyId]		= B.intFunctionalCurrencyId
+			,[intUserId]			= 0
+			,[intEntityId]			= 1
+			,[dtmDateEntered]		= GETDATE()
+			,[strBatchId]			= @strPostBatchId
+			,[strCode]				= 'REVAL'
+			,[strJournalLineDescription] = ''
+			,[intJournalLineNo]		= A.[intConsolidationDetailId]
+			,[strTransactionType]	= 'Revalue Currency'
+			,[strTransactionForm]	= 'Revalue Currency'
+			,B.dtmReverseDate
+			,strModule = B.strTransactionType
+			,A.strType
+			,Offset = 0
+		FROM [dbo].tblGLRevalueDetails A INNER JOIN [dbo].tblGLRevalue B
+			ON A.intConsolidationId = B.intConsolidationId
+			WHERE B.intConsolidationId = 10
+		),cte1 AS
+		(
+			SELECT
+				 [strTransactionId]
+				,[intTransactionId]
+				,[strDescription]
+				,[dtmTransactionDate]
+				,[dblDebit]
+				,[dblCredit]
+				,[dtmDate]
+				,[ysnIsUnposted]
+				,[intConcurrencyId]
+				,[intCurrencyId]
+				,[intUserId]
+				,[intEntityId]
+				,[dtmDateEntered]
+				,strBatchId
+				,[strCode]
+				,[strJournalLineDescription]
+				,[intJournalLineNo]
+				,[strTransactionType]
+				,[strTransactionForm]
+				,strModule
+				,OffSet = 0
+				,strType
+			FROM
+			cte
+			UNION ALL
+			SELECT
+				 [strTransactionId]
+				,[intTransactionId]
+				,[strDescription]
+				,[dtmTransactionDate]
+				,[dblDebit]				= dblCredit
+				,[dblCredit]			= dblDebit
+				,[dtmDate]
+				,[ysnIsUnposted]
+				,[intConcurrencyId]
+				,[intCurrencyId]
+				,[intUserId]
+				,[intEntityId]
+				,[dtmDateEntered]
+				,strBatchId
+				,[strCode]
+				,[strJournalLineDescription]
+				,[intJournalLineNo]
+				,[strTransactionType]
+				,[strTransactionForm]
+				,strModule
+				,OffSet = 1
+				,strType
+			FROM
+			cte
+		)
+
+		--SELECT * FROM cte
+		INSERT INTO @PostGLEntries(
+			 [strTransactionId]
+			,[intTransactionId]
+			,[intAccountId]
+			,[strDescription]
+			,[dtmTransactionDate]
+			,[dblDebit]
+			,[dblCredit]
+			,[dtmDate]
+			,[ysnIsUnposted]
+			,[intConcurrencyId]
+			,[intCurrencyId]
+			,[intUserId]
+			,[intEntityId]
+			,[dtmDateEntered]
+			,[strBatchId]
+			,[strCode]
+			,[strJournalLineDescription]
+			,[intJournalLineNo]
+			,[strTransactionType]
+			,[strTransactionForm]
+			,strModuleName
+			)
+		SELECT
+			 [strTransactionId]
+			,[intTransactionId]
+			,[intAccountId]	= G.AccountId
+			,[strDescription]
+			,[dtmTransactionDate]
+			,[dblDebit]
+			,[dblCredit]
+			,[dtmDate]
+			,[ysnIsUnposted]
+			,[intConcurrencyId]
+			,[intCurrencyId]
+			,[intUserId]
+			,[intEntityId]
+			,[dtmDateEntered]
+			,[strBatchId]
+			,[strCode]
+			,[strJournalLineDescription]
+			,[intJournalLineNo]
+			,[strTransactionType]
+			,[strTransactionForm]
+			,''
+		FROM cte1 A
+		OUTER APPLY (
+			SELECT TOP 1 AccountId from dbo.fnGLGetRevalueAccountTable() f
+			WHERE A.strType COLLATE Latin1_General_CI_AS = f.strType COLLATE Latin1_General_CI_AS
+			AND f.strModule COLLATE Latin1_General_CI_AS = A.strModule COLLATE Latin1_General_CI_AS
+			AND f.OffSet  = A.OffSet
+		)G
+		DECLARE @dtmReverseDate DATETIME
+		SELECT TOP 1 @dtmReverseDate = dtmReverseDate FROM tblGLRevalue WHERE intConsolidationId = @intConsolidationId
+
+		IF @ysnRecap = 0
+			EXEC [dbo].uspGLGetNewID 3, @strReversePostBatchId OUTPUT
+		ELSE
+			SELECT @strReversePostBatchId =  NEWID()
+
+		INSERT INTO @ReversePostGLEntries(
+			 [strTransactionId]
+			,[intTransactionId]
+			,[intAccountId]
+			,[strDescription]
+			,[dtmTransactionDate]
+			,[dblDebit]
+			,[dblCredit]
+			,[dtmDate]
+			,[ysnIsUnposted]
+			,[intConcurrencyId]
+			,[intCurrencyId]
+			,[intUserId]
+			,[intEntityId]
+			,[dtmDateEntered]
+			,[strBatchId]
+			,[strCode]
+			,[strJournalLineDescription]
+			,[intJournalLineNo]
+			,[strTransactionType]
+			,[strTransactionForm]
+			,strModuleName
+			)
+		 SELECT
+			 [strTransactionId]
+			,[intTransactionId]
+			,[intAccountId]
+			,[strDescription]
+			,[dtmTransactionDate]
+			,[dblDebit] = [dblCredit]
+			,[dblCredit] = [dblDebit]
+			,[dtmDate] = @dtmReverseDate
+			,[ysnIsUnposted]
+			,[intConcurrencyId]
+			,[intCurrencyId]
+			,[intUserId]
+			,[intEntityId]
+			,[dtmDateEntered]
+			,[strBatchId] = @strReversePostBatchId
+			,[strCode]
+			,[strJournalLineDescription]
+			,[intJournalLineNo]
+			,[strTransactionType] = 'Reverse Revalue Currency'
+			,[strTransactionForm]
+			,''
+		FROM
+			 @PostGLEntries
+
+
+
+
+		IF @ysnRecap = 0
+		BEGIN
+			EXEC uspGLBookEntries @PostGLEntries, 1
+			EXEC uspGLBookEntries @ReversePostGLEntries, 1
+			UPDATE tblGLRevalue SET ysnPosted = 1 WHERE intConsolidationId = @intConsolidationId
+		END
+		ELSE
+		BEGIN
+			DECLARE @RecapTable RecapTableType
+			INSERT INTO @RecapTable (
+				 [strTransactionId]
+				,[intTransactionId]
+				,[intAccountId]
+				,[strDescription]
+				,[dtmTransactionDate]
+				,[dblDebit]
+				,[dblCredit]
+				,[dtmDate]
+				,[ysnIsUnposted]
+				,[intConcurrencyId]
+				,[intCurrencyId]
+				,[intUserId]
+				,[intEntityId]
+				,[dtmDateEntered]
+				,[strBatchId]
+				,[strCode]
+				,[strJournalLineDescription]
+				,[intJournalLineNo]
+				,[strTransactionType]
+				,[strTransactionForm]
+				,strModuleName
+			)
+			SELECT
+				 [strTransactionId]
+				,[intTransactionId]
+				,[intAccountId]
+				,[strDescription]
+				,[dtmTransactionDate]
+				,[dblDebit]
+				,[dblCredit]
+				,[dtmDate]
+				,[ysnIsUnposted]
+				,[intConcurrencyId]
+				,[intCurrencyId]
+				,[intUserId]
+				,[intEntityId]
+				,[dtmDateEntered]
+				,[strBatchId]= @strPostBatchId
+				,[strCode]
+				,[strJournalLineDescription]
+				,[intJournalLineNo]
+				,[strTransactionType]
+				,[strTransactionForm]
+				,strModuleName
+			FROM @PostGLEntries UNION ALL
+			SELECT
+				 [strTransactionId]
+				,[intTransactionId]
+				,[intAccountId]
+				,[strDescription]
+				,[dtmTransactionDate]
+				,[dblDebit]
+				,[dblCredit]
+				,[dtmDate]
+				,[ysnIsUnposted]
+				,[intConcurrencyId]
+				,[intCurrencyId]
+				,[intUserId]
+				,[intEntityId]
+				,[dtmDateEntered]
+				,[strBatchId]=@strReversePostBatchId
+				,[strCode]
+				,[strJournalLineDescription]
+				,[intJournalLineNo]
+				,[strTransactionType]
+				,[strTransactionForm]
+				,strModuleName
+			FROM @ReversePostGLEntries
+			EXEC uspGLPostRecap @RecapTable, @intEntityId
+		END
+
+
+
+	COMMIT TRANSACTION
+	SELECT @strPostBatchId PostBatchId,	@strReversePostBatchId ReversePostBatchId
+END TRY
+BEGIN CATCH
+
+	ROLLBACK TRANSACTION
+	DECLARE @ErrorMessage NVARCHAR(4000);
+    DECLARE @ErrorSeverity INT;
+    DECLARE @ErrorState INT;
+	 SELECT
+        @ErrorMessage = ERROR_MESSAGE(),
+        @ErrorSeverity = ERROR_SEVERITY(),
+        @ErrorState = ERROR_STATE();
+
+    RAISERROR (@ErrorMessage, -- Message text.
+               @ErrorSeverity, -- Severity.
+               @ErrorState -- State.
+               );
+END CATCH
+
+
--- i21Database/dbo/User Defined Types/RecapTableType.sql
+++ i21Database/dbo/User Defined Types/RecapTableType.sql
@@ -30,7 +30,6 @@
 	[dblCreditReport]			NUMERIC (18, 9) NULL,
 	[dblReportingRate]			NUMERIC (18, 9) NULL,
 	[dblForeignRate]			NUMERIC (18, 9) NULL,
-	[strRateType]				NVARCHAR (255)   COLLATE Latin1_General_CI_AS NULL
-
+	[strRateType]			    NVARCHAR(50)	COLLATE Latin1_General_CI_AS
 )
 
