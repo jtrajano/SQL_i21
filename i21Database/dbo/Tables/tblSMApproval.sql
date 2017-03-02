4b6ed06f2201704503fd93d72e81ab505246eed5
jesmar mendones <jesmar.mendones@irely.com>
2017-03-02 11:56:43 +0800

    FRM-4578

--- i21Database/dbo/Tables/tblSMApproval.sql
+++ i21Database/dbo/Tables/tblSMApproval.sql
@@ -14,6 +14,7 @@
 	[dtmDate]						[datetime]										NULL,
 	[ysnCurrent]					[bit]											NULL,
 	[ysnEmail]						[bit]											NULL,
+	[intScreenId]					[int]											NULL,
 	[intConcurrencyId]				[int]											NOT NULL DEFAULT ((1)), 
     CONSTRAINT [FK_tblSMApproval_tblSMTransaction] FOREIGN KEY ([intTransactionId]) REFERENCES [tblSMTransaction]([intTransactionId]) ON DELETE CASCADE, 
     CONSTRAINT [FK_tblSMApproval_tblEMEntity_Approver] FOREIGN KEY ([intApproverId]) REFERENCES [tblEMEntity]([intEntityId]), 
