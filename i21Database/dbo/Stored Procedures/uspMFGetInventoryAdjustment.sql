09b94aa783dec0df2e57bb4482a6f46f527715dc
Premkumar <Prem.Selladurai@gmail.com>
2017-03-03 19:08:59 +0530

    MFG-2375

--- i21Database/dbo/Stored Procedures/uspMFGetInventoryAdjustment.sql
+++ i21Database/dbo/Stored Procedures/uspMFGetInventoryAdjustment.sql
@@ -1,4 +1,4 @@
-﻿CREATE Procedure uspMFGetInventoryAdjustment(
+﻿CREATE PROCEDURE uspMFGetInventoryAdjustment (
 	@dtmFromDate DATETIME
 	,@dtmToDate DATETIME
 	,@strCustomerName NVARCHAR(50)
@@ -18,17 +18,18 @@ JOIN tblEMEntityType ET ON E.intEntityId = ET.intEntityId
 	AND ET.strType = 'Customer'
 WHERE strName = @strCustomerName
 	AND strEntityNo <> ''
-SELECT E.strName AS strOwner
-	,IT.strName AS strTransactionType
-	,SL.strName AS strStorageLocation
-	,L.strLotNumber AS strPalletId
-	,I.strItemNo
-	,PL.strParentLotNumber AS strLotId
-	,IA.dblQty
-	,IA.dtmBusinessDate AS dtmDate
-	,US.strUserName
-	,IA.strReason
-	,IA.strNote
+
+SELECT E.strName AS [Owner]
+	,IT.strName AS [Transaction Type]
+	,SL.strName AS [Storage Location]
+	,L.strLotNumber AS [Pallet No]
+	,I.strItemNo AS [Item No]
+	,PL.strParentLotNumber AS [Lot No]
+	,IA.dblQty AS Quantity
+	,IA.dtmBusinessDate AS [Date]
+	,US.strUserName AS [User]
+	,IA.strReason AS Reason
+	,IA.strNote AS Note
 FROM tblMFInventoryAdjustment IA
 JOIN dbo.tblICInventoryTransactionType IT ON IT.intTransactionTypeId = IA.intTransactionTypeId
 JOIN dbo.tblICStorageLocation SL ON SL.intStorageLocationId = IA.intStorageLocationId
@@ -37,9 +38,9 @@ JOIN dbo.tblICParentLot PL ON PL.intParentLotId = L.intParentLotId
 JOIN dbo.tblICItem I ON I.intItemId = IA.intItemId
 LEFT JOIN dbo.tblSMUserSecurity US ON US.intEntityUserSecurityId = IA.intUserId
 JOIN dbo.tblMFLotInventory LI ON LI.intLotId = IA.intSourceLotId
-Left JOIN dbo.tblICItemOwner IO1 ON IO1.intItemOwnerId = LI.intItemOwnerId
-Left JOIN dbo.tblEMEntity E ON E.intEntityId = IO1.intOwnerId
+LEFT JOIN dbo.tblICItemOwner IO1 ON IO1.intItemOwnerId = LI.intItemOwnerId
+LEFT JOIN dbo.tblEMEntity E ON E.intEntityId = IO1.intOwnerId
 WHERE IT.intTransactionTypeId = 10
 	AND IA.dtmBusinessDate BETWEEN @dtmFromDate
 		AND @dtmToDate
-	--AND IO1.intOwnerId = @intOwnerId
+			--AND IO1.intOwnerId = @intOwnerId
