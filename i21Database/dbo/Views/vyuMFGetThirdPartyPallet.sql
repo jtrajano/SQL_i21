5f95f2563effb81072320474a9e264b1c8b3c873
Premkumar <Prem.Selladurai@gmail.com>
2017-03-03 22:49:36 +0530

    MFG-2367

--- i21Database/dbo/Views/vyuMFGetThirdPartyPallet.sql
+++ i21Database/dbo/Views/vyuMFGetThirdPartyPallet.sql
@@ -36,14 +36,15 @@ SELECT L.dtmDateCreated
 		ELSE (
 				CASE 
 					WHEN IL.intLotId IS NULL
-						THEN 'Not Available'
+						THEN 'Unavailable'
 					ELSE 'Shipped out'
 					END
 				)
 		END AS strPalletStatus
 	,L1.strLotNumber AS strUsedIn
 FROM dbo.tblICLot AS L
-JOIN dbo.tblICItem AS I ON I.intItemId = L.intItemId and L.dblQty >0
+JOIN dbo.tblICItem AS I ON I.intItemId = L.intItemId
+	AND L.dblQty > 0
 JOIN dbo.tblICCategory AS C ON C.intCategoryId = I.intCategoryId
 JOIN dbo.tblICLotStatus AS LS ON LS.intLotStatusId = L.intLotStatusId
 JOIN dbo.tblICParentLot AS PL ON PL.intParentLotId = L.intParentLotId
@@ -51,13 +52,14 @@ JOIN dbo.tblICItemUOM AS IU ON IU.intItemUOMId = L.intItemUOMId
 JOIN dbo.tblICUnitMeasure AS UM ON UM.intUnitMeasureId = IU.intUnitMeasureId
 JOIN dbo.tblSMCompanyLocation AS CL ON CL.intCompanyLocationId = L.intLocationId
 LEFT JOIN dbo.tblICStorageLocation AS SL ON SL.intStorageLocationId = L.intStorageLocationId
-LEFT JOIN dbo.tblSMUserSecurity AS US ON US.intEntityUserSecurityId = L.intCreatedUserId
+LEFT JOIN dbo.tblSMUserSecurity AS US ON US.intEntityUserSecurityId = L.intCreatedEntityId
 LEFT JOIN dbo.tblICItemUOM AS IU1 ON IU1.intItemUOMId = ISNULL(L.intWeightUOMId, L.intItemUOMId)
 LEFT JOIN dbo.tblICUnitMeasure AS UM1 ON UM1.intUnitMeasureId = IU1.intUnitMeasureId
 LEFT JOIN dbo.tblMFLotInventory AS LI ON LI.intLotId = L.intLotId
 LEFT JOIN dbo.tblICItemOwner AS O ON O.intItemOwnerId = LI.intItemOwnerId
 LEFT JOIN dbo.tblEMEntity AS E ON E.intEntityId = O.intOwnerId
-LEFT JOIN dbo.tblMFWorkOrderProducedLot WP ON WP.intSpecialPalletLotId = L.intLotId and WP.ysnProductionReversed =0
+LEFT JOIN dbo.tblMFWorkOrderProducedLot WP ON WP.intSpecialPalletLotId = L.intLotId
+	AND WP.ysnProductionReversed = 0
 LEFT JOIN dbo.tblICLot AS L1 ON L1.intLotId = WP.intLotId
 LEFT JOIN dbo.tblICInventoryShipmentItemLot IL ON IL.intLotId = L1.intLotId
 WHERE I.intItemId IN (
@@ -66,7 +68,9 @@ WHERE I.intItemId IN (
 		WHERE intAttributeId = 88
 			AND strAttributeValue <> ''
 		)
+
 UNION
+
 SELECT L.dtmDateCreated
 	,US.strUserName
 	,L.strLotNumber
@@ -110,7 +114,8 @@ SELECT L.dtmDateCreated
 		END AS strPalletStatus
 	,L1.strLotNumber AS strUsedIn
 FROM dbo.tblICLot AS L
-JOIN dbo.tblICItem AS I ON I.intItemId = L.intItemId and L.dblQty =0
+JOIN dbo.tblICItem AS I ON I.intItemId = L.intItemId
+	AND L.dblQty = 0
 JOIN dbo.tblICCategory AS C ON C.intCategoryId = I.intCategoryId
 JOIN dbo.tblICLotStatus AS LS ON LS.intLotStatusId = L.intLotStatusId
 JOIN dbo.tblICParentLot AS PL ON PL.intParentLotId = L.intParentLotId
@@ -118,13 +123,14 @@ JOIN dbo.tblICItemUOM AS IU ON IU.intItemUOMId = L.intItemUOMId
 JOIN dbo.tblICUnitMeasure AS UM ON UM.intUnitMeasureId = IU.intUnitMeasureId
 JOIN dbo.tblSMCompanyLocation AS CL ON CL.intCompanyLocationId = L.intLocationId
 LEFT JOIN dbo.tblICStorageLocation AS SL ON SL.intStorageLocationId = L.intStorageLocationId
-LEFT JOIN dbo.tblSMUserSecurity AS US ON US.intEntityUserSecurityId = L.intCreatedUserId
+LEFT JOIN dbo.tblSMUserSecurity AS US ON US.intEntityUserSecurityId = L.intCreatedEntityId
 LEFT JOIN dbo.tblICItemUOM AS IU1 ON IU1.intItemUOMId = ISNULL(L.intWeightUOMId, L.intItemUOMId)
 LEFT JOIN dbo.tblICUnitMeasure AS UM1 ON UM1.intUnitMeasureId = IU1.intUnitMeasureId
 LEFT JOIN dbo.tblMFLotInventory AS LI ON LI.intLotId = L.intLotId
 LEFT JOIN dbo.tblICItemOwner AS O ON O.intItemOwnerId = LI.intItemOwnerId
 LEFT JOIN dbo.tblEMEntity AS E ON E.intEntityId = O.intOwnerId
-JOIN dbo.tblMFWorkOrderProducedLot WP ON WP.intSpecialPalletLotId = L.intLotId and WP.ysnProductionReversed =0
+JOIN dbo.tblMFWorkOrderProducedLot WP ON WP.intSpecialPalletLotId = L.intLotId
+	AND WP.ysnProductionReversed = 0
 LEFT JOIN dbo.tblICLot AS L1 ON L1.intLotId = WP.intLotId
 LEFT JOIN dbo.tblICInventoryShipmentItemLot IL ON IL.intLotId = L1.intLotId
 WHERE I.intItemId IN (
@@ -133,4 +139,3 @@ WHERE I.intItemId IN (
 		WHERE intAttributeId = 88
 			AND strAttributeValue <> ''
 		)
-
