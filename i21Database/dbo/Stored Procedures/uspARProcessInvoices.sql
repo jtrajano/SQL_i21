2c5fe641812224bd663b730c79f7c2627eb41a88
Jb Mago <benedict.mago@irely.com>
2017-03-06 10:48:23 +0800

    AR-4348

--- i21Database/dbo/Stored Procedures/uspARProcessInvoices.sql
+++ i21Database/dbo/Stored Procedures/uspARProcessInvoices.sql
@@ -703,7 +703,7 @@ BEGIN
 					,@ItemTermDiscount				= [dblItemTermDiscount]
 					,@ItemTermDiscountBy			= [strItemTermDiscountBy]
 					,@ItemPrice						= [dblPrice]
-					,@ItemPricing					= CASE WHEN ISNULL([strPricing],'') = '' THEN CONCAT('Subsystem - ', strSourceTransaction) ELSE [strPricing] END
+					,@ItemPricing					= CASE WHEN ISNULL([strPricing],'') = '' THEN CONCAT('Subsystem - ' COLLATE Latin1_General_CI_AS, strSourceTransaction COLLATE Latin1_General_CI_AS) ELSE [strPricing] COLLATE Latin1_General_CI_AS END
 					,@ItemVFDDocumentNumber			= [strVFDDocumentNumber]
 					,@RefreshPrice					= [ysnRefreshPrice]
 					,@ItemMaintenanceType			= [strMaintenanceType]
