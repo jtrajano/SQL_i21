40cf901ea36ae1ec06f9477de75878df239386cb
Von Carlo Pasion <von.pasion@irely.com>
2017-03-06 19:08:45 +0800

    SM-2458 - Added new field to tblSMScreen

--- i21Database/dbo/Tables/tblSMScreen.sql
+++ i21Database/dbo/Tables/tblSMScreen.sql
@@ -8,8 +8,9 @@
 	[ysnApproval]	   BIT NULL,
 	[ysnActivity]	   BIT NULL,
 	[ysnCustomTab]	   BIT NULL,
-    [intConcurrencyId] INT CONSTRAINT [DF__tblSMScre__intCo__381B131F] DEFAULT ((1)) NOT NULL,
-    CONSTRAINT [PK__tblSMScr__552B34714D4B3A2F] PRIMARY KEY CLUSTERED ([intScreenId] ASC)
+    [ysnDocumentSource] BIT NULL, 
+    [intConcurrencyId] INT CONSTRAINT [DF__tblSMScreen] DEFAULT ((1)) NOT NULL,
+    CONSTRAINT [PK_tblSMScreen] PRIMARY KEY CLUSTERED ([intScreenId] ASC)
 );
 
 
