201315a93c51fa14ee9f40c173042d50a7925113
Von Carlo Pasion <von.pasion@irely.com>
2017-03-06 14:34:21 +0800

    SM-2458

---
+++ i21Database/dbo/Tables/tblSMDocument.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE [dbo].[tblSMDocument] (
+    [intDocumentId]				INT             IDENTITY (1, 1) NOT NULL,
+    [strName]					NVARCHAR (MAX)  COLLATE Latin1_General_CI_AS NOT NULL,
+    [strType]					NVARCHAR (150)  COLLATE Latin1_General_CI_AS NULL,
+    [dtmDateModified]			DATETIME        NOT NULL,
+    [intSize]					INT             NOT NULL,
+	[intDocumentSourceFolderId] INT				NULL,
+	[intTransactionId]			INT				NOT NULL,
+	[intEntityId]				INT				NOT NULL,
+	[intUploadId]				INT				NOT NULL,
+    [intConcurrencyId]			INT				NOT NULL,
+	CONSTRAINT [FK_tblSMDocument_tblSMDocumentSourceFolder] FOREIGN KEY ([intDocumentSourceFolderId]) REFERENCES [dbo].[tblSMDocumentSourceFolder] ([intDocumentSourceFolderId]),
+	CONSTRAINT [FK_tblSMDocument_tblSMTransaction] FOREIGN KEY ([intTransactionId]) REFERENCES [dbo].[tblSMTransaction] ([intTransactionId]) ON DELETE CASCADE,
+	CONSTRAINT [FK_tblSMDocument_tblSMUpload] FOREIGN KEY ([intUploadId]) REFERENCES [dbo].[tblSMUpload] ([intUploadId]),
+	CONSTRAINT [FK_tblSMDocument_tblEMEntity] FOREIGN KEY ([intEntityId]) REFERENCES [dbo].[tblEMEntity] ([intEntityId]),
+    CONSTRAINT [PK_dbo.tblSMDocument] PRIMARY KEY CLUSTERED ([intDocumentId] ASC)
+);
+
+
+
+
+
+
+
---
+++ i21Database/dbo/Tables/tblSMDocumentTypeFieldValue.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[tblSMDocumentTypeFieldValue] (
+    [intDocumentTypeFieldValueId]	INT             IDENTITY (1, 1) NOT NULL,
+	[intDocumentTypeFieldId]		INT             NOT NULL,
+	[intDocumentId]					INT             NOT NULL,
+    [strValue]						NVARCHAR (MAX)  COLLATE Latin1_General_CI_AS NULL,
+    [intConcurrencyId]				INT				NOT NULL,
+	CONSTRAINT [FK_tblSMDocumentTypeFieldValue_tblSMDocumentTypeField] FOREIGN KEY ([intDocumentTypeFieldId]) REFERENCES [dbo].[tblSMDocumentTypeField] ([intDocumentTypeFieldId]),
+	CONSTRAINT [FK_tblSMDocumentTypeFieldValue_tblSMDocument] FOREIGN KEY ([intDocumentId]) REFERENCES [dbo].[tblSMDocument] ([intDocumentId]) ON DELETE CASCADE,
+    CONSTRAINT [PK_dbo.tblSMDocumentTypeValue] PRIMARY KEY CLUSTERED ([intDocumentTypeFieldId] ASC)
+);
+
+
+
+
+
+
+
