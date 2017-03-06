cb7af2e60424e468182e74ec117ba7f81a41cdf4
Mark Paragas <mark.paragas@irely.com>
2017-03-06 16:32:38 +0800

    MFT-476

---
+++ i21PreDeployment/Scripts/Pre-Deployment/23_MFT/1_CleanUp_Data.sql
@@ -0,0 +1,5 @@
+﻿IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = N'tblTFReportingComponentProductCode')
+		BEGIN
+			DELETE FROM tblTFReportingComponentProductCode
+			WHERE intProductCodeId IS NULL
+		END
