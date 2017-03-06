423c71c431498ab895db599f59f059c1f58a534e
Mark Paragas <mark.paragas@irely.com>
2017-03-06 12:42:29 +0800

    MFT-486

--- i21Database/dbo/Stored Procedures/uspTFGenerateGT103.sql
+++ i21Database/dbo/Stored Procedures/uspTFGenerateGT103.sql
@@ -164,7 +164,7 @@ DECLARE @EIN NVARCHAR(50)
 						--5. Collection Allowance. Do not calculate this allowance if your return and payment are late. Collection allowance rate is 0.73%
 								IF(@ScheduleCodeParam <> '')
 									BEGIN
-										SET @QueryTransaction = 'SELECT strConfiguration FROM tblTFReportingComponentConfiguration WHERE strTemplateItemId = ''' + @TemplateItemId + ''''  
+										SET @QueryTransaction = 'SELECT strColumnValue * ' + CONVERT(NVARCHAR(50), @TemplateConfiguration) + ' FROM tblTFTransactionSummary WHERE intItemNumber IN (''' + @TemplateScheduleCodeParam + ''') AND strSummaryGuid = ''' + @Guid + ''' AND strFormCode = ''' + @FormCodeParam + ''''  
 										INSERT INTO @TFTransactionSummaryTotal
 										EXEC(@QueryTransaction)
 									END
