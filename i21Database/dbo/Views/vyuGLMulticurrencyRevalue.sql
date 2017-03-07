61e80e486412d7775e8bac65a4bf248c7197dcb5
jeffrey trajano <jeffrey.trajano@irely.com>
2017-03-07 10:56:30 +0800

    GL-3309 Failed to build 17.1Prod script

--- i21Database/dbo/Views/vyuGLMulticurrencyRevalue.sql
+++ i21Database/dbo/Views/vyuGLMulticurrencyRevalue.sql
@@ -2,7 +2,7 @@
 AS
 SELECT 
 strTransactionType COLLATE Latin1_General_CI_AS strTransactionType,strTransactionId  COLLATE Latin1_General_CI_AS strTransactionId,strTransactionDate dtmDate,strTransactionDueDate dtmDueDate,strVendorName  COLLATE Latin1_General_CI_AS strVendorName,strCommodity  COLLATE Latin1_General_CI_AS strCommodity,strLineOfBusiness  COLLATE Latin1_General_CI_AS strLineOfBusiness,strLocation  COLLATE Latin1_General_CI_AS strLocation,strTicket  COLLATE Latin1_General_CI_AS strTicket,strContractNumber  COLLATE Latin1_General_CI_AS strContractId,strItemId  COLLATE Latin1_General_CI_AS strItemId,dblQuantity,dblUnitPrice, dblAmount,intCurrencyId, intForexRateType, strForexRateType  COLLATE Latin1_General_CI_AS strForexRateType,dblForexRate dblHistoricForexRate ,dblHistoricAmount, strModule = 'AP', strType = 'Payables'
-FROM vyuAPVoucherForPayment -- 'AP'
+FROM vyuARMultiCurrencyRevalue -- 'AP'
 UNION ALL
 SELECT 
 strTransactionType COLLATE Latin1_General_CI_AS ,strTransactionId  COLLATE Latin1_General_CI_AS ,strTransactionDate dtmDate,strTransactionDueDate dtmDueDate,strVendorName  COLLATE Latin1_General_CI_AS ,strCommodity  COLLATE Latin1_General_CI_AS strCommodity,strLineOfBusiness  COLLATE Latin1_General_CI_AS ,strLocation  COLLATE Latin1_General_CI_AS ,strTicket  COLLATE Latin1_General_CI_AS strTicket,strContractNumber  COLLATE Latin1_General_CI_AS strContractId,strItemId  COLLATE Latin1_General_CI_AS ,dblQuantity,dblUnitPrice, dblAmount,intCurrencyId, intForexRateType, strForexRateType,dblForexRate dblHistoricForexRate,dblHistoricAmount,strModule = 'CT', strType = CASE WHEN strTransactionType = 'Purchase' THEN 'Payables' ELSE CASE WHEN  strTransactionType = 'Sales' THEN 'Receivables' END END
