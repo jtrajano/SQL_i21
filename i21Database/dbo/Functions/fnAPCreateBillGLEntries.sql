090844379ec06efe13d8bea11252d99dd75fba00
Lex Eugene Peregrino <lex.peregrino@irely.com>
2017-03-07 16:10:29 +0800

    AP-3309 - Foreign currency to the recap should reflect the rate in the debit and credit side of the transaction. 1.0

--- i21Database/dbo/Functions/fnAPCreateBillGLEntries.sql
+++ i21Database/dbo/Functions/fnAPCreateBillGLEntries.sql
@@ -92,7 +92,7 @@ BEGIN
 		[strCode]						=	'AP',
 		[strReference]					=	C.strVendorId,
 		[intCurrencyId]					=	A.intCurrencyId,
-		[dblExchangeRate]				=	0,
+		[dblExchangeRate]				=	CASE WHEN ForexRateCounter.ysnUniqueForex = 0 THEN ForexRate.dblRate ELSE 0 END,
 		[dtmDateEntered]				=	GETDATE(),
 		[dtmTransactionDate]			=	A.dtmDate,
 		[strJournalLineDescription]		=	CASE WHEN intTransactionType = 1 THEN 'Posted Bill'
@@ -117,7 +117,7 @@ BEGIN
 											 ELSE A.dblAmountDue END) AS DECIMAL(18,2)),
 		[dblCreditReport]				=	0,
 		[dblReportingRate]				=	0,
-		[dblForeignRate]				=	0,
+		[dblForeignRate]				=	CASE WHEN ForexRateCounter.ysnUniqueForex = 0 THEN ForexRate.dblRate ELSE 0 END,
 		[strRateType]					=	ForexRate.strCurrencyExchangeRateType,
 		[intConcurrencyId]				=	1
 	FROM	[dbo].tblAPBill A
@@ -130,6 +130,12 @@ BEGIN
 				LEFT JOIN dbo.tblSMCurrencyExchangeRateType B ON A.intCurrencyExchangeRateTypeId = B.intCurrencyExchangeRateTypeId
 				WHERE A.intBillId IN (SELECT intTransactionId FROM @tmpTransacions)
 			) ForexRate
+			CROSS APPLY
+			(
+				SELECT CASE COUNT(DISTINCT A.dblRate) WHEN 1 THEN 0 ELSE 1 END AS ysnUniqueForex
+				FROM tblAPBillDetail A
+				WHERE A.intBillId = (SELECT intTransactionId FROM @tmpTransacions)
+			) ForexRateCounter
 			CROSS APPLY
 			(
 				SELECT SUM((dblTotal + dblTax) * A.dblRate) AS dblTotal
