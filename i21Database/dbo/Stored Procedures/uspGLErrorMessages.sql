2bf4507c94dd3c4e184157e713d70ec43db6313a
jeffrey trajano <jeffrey.trajano@irely.com>
2017-03-08 12:42:32 +0800

    GL-3170 Revalue Currency

--- i21Database/dbo/Stored Procedures/uspGLErrorMessages.sql
+++ i21Database/dbo/Stored Procedures/uspGLErrorMessages.sql
@@ -1,19 +1,4 @@
-﻿/*
-----------------------------------------------------------------------------------------------------------------------------------------------
-IMPORTANT NOTES: 
-----------------------------------------------------------------------------------------------------------------------------------------------
-
-The msg_id for Cash Management should be in the range of 60,001 to 70,000. 
-Please migrate all error messages from uspSMErrorMessage that is specific for CM into this stored procedure. 
-
-Template: 
-
-IF EXISTS(SELECT 1 FROM sys.messages WHERE message_id = 60001) EXEC sp_dropmessage 60001, 'us_english'	
-SET @strmessage = 'Invalid G/L account id found.'
-EXEC sp_addmessage 60001,11,@strmessage,'us_english','False'
-*/
-
-CREATE PROCEDURE uspGLErrorMessages
+﻿CREATE PROCEDURE [dbo].[uspGLErrorMessages]
 AS
 
 DECLARE @strmessage AS NVARCHAR(MAX)
@@ -57,3 +42,14 @@ EXEC sp_addmessage 60009,11,@strmessage,'us_english','False'
 IF EXISTS(SELECT 1 FROM sys.messages WHERE message_id = 60010) EXEC sp_dropmessage 60010, 'us_english'	
 SET @strmessage = 'Unable to recalculate summary. General Ledger Detail has out of balance transactions.'
 EXEC sp_addmessage 60010,11,@strmessage,'us_english','False'
+
+IF EXISTS(SELECT 1 FROM sys.messages WHERE message_id = 60011) EXEC sp_dropmessage 60011, 'us_english'	
+SET @strmessage = 'Fiscal period for %s is %s'
+EXEC sp_addmessage 60011,11,@strmessage,'us_english','False'
+
+
+IF EXISTS(SELECT 1 FROM sys.messages WHERE message_id = 60012) EXEC sp_dropmessage 60012, 'us_english'	
+SET @strmessage = 'Fiscal period of reverse date for %s is %s'
+EXEC sp_addmessage 60012,11,@strmessage,'us_english','False'
+
+
