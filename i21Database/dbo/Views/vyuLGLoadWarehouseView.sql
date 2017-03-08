9ee7e2440ec6aee21607af56e1e36c36bea60b88
Swetosuvro Ghosh <swetosuvro.g@irely.com>
2017-03-08 20:57:30 +0530

    LG-844, LG-832

--- i21Database/dbo/Views/vyuLGLoadWarehouseView.sql
+++ i21Database/dbo/Views/vyuLGLoadWarehouseView.sql
@@ -21,9 +21,12 @@ SELECT LW.intLoadWarehouseId
 	  ,WRMH.strServiceContractNo AS strServiceContract
 	  ,Hauler.strName AS strShipVia
 	  ,CLSL.strSubLocationName AS strWarehouse
+	  ,LW.intStorageLocationId
+	  ,SL.strName AS strStorageLocationName
 FROM tblLGLoadWarehouse LW
 JOIN tblLGLoad L ON L.intLoadId = LW.intLoadId
 JOIN tblSMCompanyLocationSubLocation CLSL ON CLSL.intCompanyLocationSubLocationId = LW.intSubLocationId
 LEFT JOIN tblEMEntity Hauler ON Hauler.intEntityId = LW.intHaulerEntityId
 LEFT JOIN tblEMEntity Driver ON Driver.intEntityId = L.intDriverEntityId
 LEFT JOIN tblLGWarehouseRateMatrixHeader WRMH ON WRMH.intWarehouseRateMatrixHeaderId = LW.intWarehouseRateMatrixHeaderId
+LEFT JOIN tblICStorageLocation SL ON SL.intStorageLocationId = LW.intStorageLocationId
