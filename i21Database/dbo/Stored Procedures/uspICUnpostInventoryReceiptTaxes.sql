8ba7b75e3ebb3be4783ce16d9c3da329e8abd909
Feb Montefrio <feb.montefrio@irely.com>
2017-03-08 18:44:41 +0800

    IC-3530

--- i21Database/dbo/Stored Procedures/uspICUnpostInventoryReceiptTaxes.sql
+++ i21Database/dbo/Stored Procedures/uspICUnpostInventoryReceiptTaxes.sql
@@ -8,11 +8,12 @@ AS
 -- Get the A/P Clearing account 
 BEGIN 
 	DECLARE @AccountCategory_APClearing AS NVARCHAR(30) = 'AP Clearing';
-	DECLARE @GLAccounts AS dbo.ItemGLAccount; 
+	DECLARE @GLAccounts AS dbo.ItemGLAccount;
+
 	INSERT INTO @GLAccounts (
 		intItemId 
 		,intItemLocationId 
-		,intContraInventoryId 
+		,intContraInventoryId
 		,intTransactionTypeId 
 	)
 	SELECT	Query.intItemId
@@ -27,6 +28,28 @@ BEGIN
 							ON Receipt.intInventoryReceiptId = ReceiptItem.intInventoryReceiptId
 						INNER JOIN dbo.tblICItemLocation ItemLocation
 							ON ItemLocation.intItemId = ReceiptItem.intItemId
+							AND ItemLocation.intLocationId = Receipt.intLocationId
+				WHERE	Receipt.intInventoryReceiptId = @intInventoryReceiptId
+			) Query
+
+	INSERT INTO @GLAccounts (
+		intItemId 
+		,intItemLocationId 
+		,intContraInventoryId
+		,intTransactionTypeId 
+	)
+	SELECT	Query.intChargeId
+			,Query.intItemLocationId
+			,intContraInventoryId = dbo.fnGetItemGLAccount(Query.intChargeId, Query.intItemLocationId, @AccountCategory_APClearing) 
+			,@intTransactionTypeId
+	FROM	(
+				SELECT	DISTINCT 
+						ReceiptCharge.intChargeId
+						,ItemLocation.intItemLocationId
+				FROM	dbo.tblICInventoryReceipt Receipt INNER JOIN dbo.tblICInventoryReceiptCharge ReceiptCharge
+							ON Receipt.intInventoryReceiptId = ReceiptCharge.intInventoryReceiptId
+						INNER JOIN dbo.tblICItemLocation ItemLocation
+							ON ItemLocation.intItemId = ReceiptCharge.intChargeId
 							AND ItemLocation.intLocationId = Receipt.intLocationId
 				WHERE	Receipt.intInventoryReceiptId = @intInventoryReceiptId
 			) Query
@@ -71,18 +94,18 @@ BEGIN
 
 END 
 
--- Get Total Value of Other Charges Taxes
-BEGIN
-	DECLARE @OtherChargeTaxes AS NUMERIC(18, 6);
+---- Get Total Value of Other Charges Taxes
+--BEGIN
+--	DECLARE @OtherChargeTaxes AS NUMERIC(18, 6);
 
-	SELECT @OtherChargeTaxes = SUM(CASE 
-										WHEN ReceiptCharge.ysnPrice = 1
-											THEN ISNULL(ReceiptCharge.dblTax,0) * -1
-										ELSE ISNULL(ReceiptCharge.dblTax,0) 
-									END )
-	FROM dbo.tblICInventoryReceiptCharge ReceiptCharge
-	WHERE ReceiptCharge.intInventoryReceiptId =  @intInventoryReceiptId
-END
+--	SELECT @OtherChargeTaxes = SUM(CASE 
+--										WHEN ReceiptCharge.ysnPrice = 1
+--											THEN ISNULL(ReceiptCharge.dblTax,0) * -1
+--										ELSE ISNULL(ReceiptCharge.dblTax,0) 
+--									END )
+--	FROM dbo.tblICInventoryReceiptCharge ReceiptCharge
+--	WHERE ReceiptCharge.intInventoryReceiptId =  @intInventoryReceiptId
+--END
 
 -- Get the functional currency
 BEGIN 
@@ -91,12 +114,12 @@ BEGIN
 END 
 ;
 
--- Generate the G/L Entries here: 
-BEGIN 
-	DECLARE @ModuleName AS NVARCHAR(50) = 'Inventory'
-			,@strTransactionForm  AS NVARCHAR(50) = 'Inventory Receipt'
-			,@strCode AS NVARCHAR(10) = 'IC';
+DECLARE @ModuleName AS NVARCHAR(50) = 'Inventory'
+		,@strTransactionForm  AS NVARCHAR(50) = 'Inventory Receipt'
+		,@strCode AS NVARCHAR(10) = 'IC';
 
+-- Generate the G/L Entries for the item taxes. 
+BEGIN 
 	WITH ForGLEntries_CTE (
 		dtmDate
 		,intItemId
@@ -111,37 +134,42 @@ BEGIN
 		,strInventoryTransactionTypeName
 		,strTransactionForm
 		,intPurchaseTaxAccountId
+		,dblForexRate 
 		,strRateType
-		,dblForexRate
+		,strItemNo
 	)
 	AS 
 	(
+		-- Item Taxes
 		SELECT	dtmDate								= Receipt.dtmReceiptDate
 				,intItemId							= ReceiptItem.intItemId
 				,intItemLocationId					= ItemLocation.intItemLocationId
 				,intTransactionId					= Receipt.intInventoryReceiptId				
 				,strTransactionId					= Receipt.strReceiptNumber
 				,intReceiptItemTaxId				= ReceiptTaxes.intInventoryReceiptItemTaxId
-				,dblTax								=	
-													CASE WHEN Receipt.strReceiptType = 'Inventory Return' 
-															THEN -(ReceiptTaxes.dblTax + ISNULL(@OtherChargeTaxes,0))
+				,dblTax								= 
+													-- Negate the tax if it is an Inventory Return 
+													CASE WHEN Receipt.strReceiptType = 'Inventory Return' THEN
+															-(ReceiptTaxes.dblTax)
 														ELSE
-															ReceiptTaxes.dblTax + ISNULL(@OtherChargeTaxes,0)
+															ReceiptTaxes.dblTax 
 													END 
-
-				,intTransactionTypeId				= @intTransactionTypeId
+				,intTransactionTypeId				= TransType.intTransactionTypeId
 				,intCurrencyId						= Receipt.intCurrencyId
 				,dblExchangeRate					= ISNULL(ReceiptItem.dblForexRate, 0)
 				,strInventoryTransactionTypeName	= TransType.strName
 				,strTransactionForm					= @strTransactionForm
 				,intPurchaseTaxAccountId			= TaxCode.intPurchaseTaxAccountId
-				,strRateType						= currencyRateType.strCurrencyExchangeRateType
 				,dblForexRate						= ISNULL(ReceiptItem.dblForexRate, 0)
+				,strRateType						= currencyRateType.strCurrencyExchangeRateType
+				,strItemNo							= item.strItemNo
 		FROM	dbo.tblICInventoryReceipt Receipt INNER JOIN dbo.tblICInventoryReceiptItem ReceiptItem
-					ON Receipt.intInventoryReceiptId = ReceiptItem.intInventoryReceiptId		
+					ON Receipt.intInventoryReceiptId = ReceiptItem.intInventoryReceiptId
 				INNER JOIN dbo.tblICItemLocation ItemLocation
 					ON ItemLocation.intItemId = ReceiptItem.intItemId
-					AND ItemLocation.intLocationId = Receipt.intLocationId
+					AND ItemLocation.intLocationId = Receipt.intLocationId		
+				INNER JOIN tblICItem item
+					ON item.intItemId = ReceiptItem.intItemId 
 				INNER JOIN dbo.tblICInventoryReceiptItemTax ReceiptTaxes
 					ON ReceiptItem.intInventoryReceiptItemId = ReceiptTaxes.intInventoryReceiptItemId
 				INNER JOIN dbo.tblSMTaxCode TaxCode
@@ -150,7 +178,48 @@ BEGIN
 					ON TransType.intTransactionTypeId = @intTransactionTypeId
 				LEFT JOIN tblSMCurrencyExchangeRateType currencyRateType
 					ON currencyRateType.intCurrencyExchangeRateTypeId = ReceiptItem.intForexRateTypeId
-		WHERE	Receipt.intInventoryReceiptId = @intInventoryReceiptId				
+		WHERE	Receipt.intInventoryReceiptId = @intInventoryReceiptId		
+		
+		-- Other Charge taxes
+		UNION ALL 
+		SELECT	dtmDate								= Receipt.dtmReceiptDate
+				,intItemId							= ReceiptCharge.intChargeId 
+				,intItemLocationId					= ItemLocation.intItemLocationId
+				,intTransactionId					= Receipt.intInventoryReceiptId				
+				,strTransactionId					= Receipt.strReceiptNumber
+				,intReceiptChargeTaxId				= ChargeTaxes.intInventoryReceiptChargeTaxId
+				,dblTax								= 
+													-- Negate the tax if it is an Inventory Return 
+													CASE WHEN Receipt.strReceiptType = 'Inventory Return' THEN
+															-(ChargeTaxes.dblTax)
+														ELSE
+															ChargeTaxes.dblTax 
+													END 
+				,intTransactionTypeId				= TransType.intTransactionTypeId
+				,intCurrencyId						= ReceiptCharge.intCurrencyId
+				,dblExchangeRate					= ISNULL(ReceiptCharge.dblForexRate, 0)
+				,strInventoryTransactionTypeName	= TransType.strName
+				,strTransactionForm					= @strTransactionForm
+				,intPurchaseTaxAccountId			= TaxCode.intPurchaseTaxAccountId
+				,dblForexRate						= ISNULL(ReceiptCharge.dblForexRate, 0)
+				,strRateType						= currencyRateType.strCurrencyExchangeRateType
+				,strItemNo							= item.strItemNo
+		FROM	dbo.tblICInventoryReceipt Receipt INNER JOIN dbo.tblICInventoryReceiptCharge ReceiptCharge
+					ON Receipt.intInventoryReceiptId = ReceiptCharge.intInventoryReceiptId
+				INNER JOIN dbo.tblICItemLocation ItemLocation
+					ON ItemLocation.intItemId = ReceiptCharge.intChargeId
+					AND ItemLocation.intLocationId = Receipt.intLocationId		
+				INNER JOIN tblICItem item
+					ON item.intItemId = ReceiptCharge.intChargeId 				
+				INNER JOIN dbo.tblICInventoryReceiptChargeTax ChargeTaxes
+					ON ReceiptCharge.intInventoryReceiptChargeId = ChargeTaxes.intInventoryReceiptChargeId
+				INNER JOIN dbo.tblSMTaxCode TaxCode
+					ON TaxCode.intTaxCodeId = ChargeTaxes.intTaxCodeId
+				LEFT JOIN dbo.tblICInventoryTransactionType TransType
+					ON TransType.intTransactionTypeId = @intTransactionTypeId
+				LEFT JOIN tblSMCurrencyExchangeRateType currencyRateType
+					ON currencyRateType.intCurrencyExchangeRateTypeId = ReceiptCharge.intForexRateTypeId
+		WHERE	Receipt.intInventoryReceiptId = @intInventoryReceiptId						
 	)
 	
 	-------------------------------------------------------------------------------------------
@@ -165,7 +234,7 @@ BEGIN
 			,dblCredit					= Debit.Value
 			,dblDebitUnit				= 0
 			,dblCreditUnit				= 0
-			,strDescription				= GLAccount.strDescription
+			,strDescription				= GLAccount.strDescription + ', ' +  ISNULL(strItemNo, '') 
 			,strCode					= @strCode
 			,strReference				= '' 
 			,intCurrencyId				= ForGLEntries_CTE.intCurrencyId
@@ -174,7 +243,7 @@ BEGIN
 			,dtmTransactionDate			= ForGLEntries_CTE.dtmDate
 			,strJournalLineDescription  = '' 
 			,intJournalLineNo			= ForGLEntries_CTE.intReceiptItemTaxId
-			,ysnIsUnposted				= 1
+			,ysnIsUnposted				= 0
 			,intUserId					= NULL 
 			,intEntityId				= @intEntityUserSecurityId 
 			,strTransactionId			= ForGLEntries_CTE.strTransactionId
@@ -183,13 +252,13 @@ BEGIN
 			,strTransactionForm			= ForGLEntries_CTE.strTransactionForm
 			,strModuleName				= @ModuleName
 			,intConcurrencyId			= 1
-			,dblDebitForeign			= CreditForeign.Value
+			,dblDebitForeign			= CASE WHEN intCurrencyId <> @intFunctionalCurrencyId THEN CreditForeign.Value ELSE 0 END 
 			,dblDebitReport				= NULL 
-			,dblCreditForeign			= DebitForeign.Value 
+			,dblCreditForeign			= CASE WHEN intCurrencyId <> @intFunctionalCurrencyId THEN DebitForeign.Value ELSE 0 END
 			,dblCreditReport			= NULL 
 			,dblReportingRate			= NULL 
-			,dblForeignRate				= ForGLEntries_CTE.dblForexRate
-			,strRateType				= ForGLEntries_CTE.strRateType
+			,dblForeignRate				= ForGLEntries_CTE.dblForexRate 
+			,strRateType				= ForGLEntries_CTE.strRateType 
 	FROM	ForGLEntries_CTE LEFT JOIN dbo.tblGLAccount GLAccount 
 				ON GLAccount.intAccountId = ForGLEntries_CTE.intPurchaseTaxAccountId
 			CROSS APPLY dbo.fnGetDebitFunctional(
@@ -203,7 +272,7 @@ BEGIN
 				,ForGLEntries_CTE.intCurrencyId
 				,@intFunctionalCurrencyId
 				,ForGLEntries_CTE.dblForexRate
-			) Credit			
+			) Credit
 			CROSS APPLY dbo.fnGetDebit(ForGLEntries_CTE.dblTax) DebitForeign
 			CROSS APPLY dbo.fnGetCredit(ForGLEntries_CTE.dblTax) CreditForeign
 
@@ -216,7 +285,7 @@ BEGIN
 			,dblCredit					= Credit.Value
 			,dblDebitUnit				= 0
 			,dblCreditUnit				= 0
-			,strDescription				= GLAccount.strDescription
+			,strDescription				= GLAccount.strDescription + ', ' + ISNULL(strItemNo, '') 
 			,strCode					= @strCode
 			,strReference				= '' 
 			,intCurrencyId				= ForGLEntries_CTE.intCurrencyId
@@ -225,7 +294,7 @@ BEGIN
 			,dtmTransactionDate			= ForGLEntries_CTE.dtmDate
 			,strJournalLineDescription  = '' 
 			,intJournalLineNo			= ForGLEntries_CTE.intReceiptItemTaxId
-			,ysnIsUnposted				= 1
+			,ysnIsUnposted				= 0
 			,intUserId					= NULL 
 			,intEntityId				= @intEntityUserSecurityId 
 			,strTransactionId			= ForGLEntries_CTE.strTransactionId
@@ -234,13 +303,13 @@ BEGIN
 			,strTransactionForm			= ForGLEntries_CTE.strTransactionForm
 			,strModuleName				= @ModuleName
 			,intConcurrencyId			= 1
-			,dblDebitForeign			= DebitForeign.Value
+			,dblDebitForeign			= CASE WHEN intCurrencyId <> @intFunctionalCurrencyId THEN DebitForeign.Value ELSE 0 END
 			,dblDebitReport				= NULL 
-			,dblCreditForeign			= CreditForeign.Value 
+			,dblCreditForeign			= CASE WHEN intCurrencyId <> @intFunctionalCurrencyId THEN CreditForeign.Value ELSE 0 END 
 			,dblCreditReport			= NULL 
 			,dblReportingRate			= NULL 
-			,dblForeignRate				= ForGLEntries_CTE.dblForexRate
-			,strRateType				= ForGLEntries_CTE.strRateType
+			,dblForeignRate				= ForGLEntries_CTE.dblForexRate 
+			,strRateType				= ForGLEntries_CTE.strRateType 
 	FROM	ForGLEntries_CTE INNER JOIN @GLAccounts InventoryAccounts
 				ON ForGLEntries_CTE.intItemId = InventoryAccounts.intItemId
 				AND ForGLEntries_CTE.intItemLocationId = InventoryAccounts.intItemLocationId
@@ -257,7 +326,7 @@ BEGIN
 				,ForGLEntries_CTE.intCurrencyId
 				,@intFunctionalCurrencyId
 				,ForGLEntries_CTE.dblForexRate
-			) Credit			
+			) Credit
 			CROSS APPLY dbo.fnGetDebit(ForGLEntries_CTE.dblTax) DebitForeign
 			CROSS APPLY dbo.fnGetCredit(ForGLEntries_CTE.dblTax) CreditForeign
 	;
