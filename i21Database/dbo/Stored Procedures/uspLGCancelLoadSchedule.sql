234f5a16f46c695f09a496cbca7424c911922114
Swetosuvro Ghosh <swetosuvro.g@irely.com>
2017-03-08 16:27:07 +0530

    LG-878

---
+++ i21Database/dbo/Stored Procedures/uspLGCancelLoadSchedule.sql
@@ -0,0 +1,163 @@
+﻿CREATE PROCEDURE uspLGCancelLoadSchedule 
+	 @intLoadId INT
+	,@ysnCancel BIT
+	,@intEntityUserSecurityId INT
+	,@intShipmentType INT
+AS
+BEGIN TRY
+	DECLARE @strErrMsg NVARCHAR(MAX)
+	DECLARE @intPurchaseSale INT
+	DECLARE @intMinLoadDetailId INT
+	DECLARE @intContractDetailId INT
+	DECLARE @dblQuantityToUpdate NUMERIC(18, 6)
+	DECLARE @intExternalId INT
+	DECLARE @strScreenName NVARCHAR(100)
+	DECLARE @tblLoadDetail TABLE (intLoadDetailRecordId INT Identity(1, 1)
+								 ,intLoadDetailId INT
+								 ,intContractDetailId INT
+								 ,dblLoadDetailQuantity NUMERIC(18, 6))
+
+	SELECT @intPurchaseSale = intPurchaseSale
+	FROM tblLGLoad
+	WHERE intLoadId = @intLoadId
+
+	INSERT INTO @tblLoadDetail
+	SELECT intLoadDetailId
+		,CASE 
+			WHEN @intPurchaseSale = 1
+				THEN intPContractDetailId
+			ELSE intSContractDetailId
+			END
+		,dblQuantity
+	FROM tblLGLoadDetail
+	WHERE intLoadId = @intLoadId
+
+	IF (@intShipmentType = 1)
+	BEGIN
+		IF (@ysnCancel = 1)
+		BEGIN
+			SELECT @intMinLoadDetailId = MIN(intLoadDetailId)
+			FROM @tblLoadDetail
+
+			WHILE (@intMinLoadDetailId IS NOT NULL)
+			BEGIN
+				SET @intContractDetailId = NULL
+				SET @dblQuantityToUpdate = NULL
+				SET @intExternalId = NULL
+				SET @strScreenName = NULL
+
+				SELECT @intContractDetailId = intContractDetailId
+					,@dblQuantityToUpdate = - dblLoadDetailQuantity
+					,@intExternalId = @intMinLoadDetailId
+					,@strScreenName = 'Load Schedule'
+				FROM @tblLoadDetail
+				WHERE intLoadDetailId = @intMinLoadDetailId
+
+				EXEC uspCTUpdateScheduleQuantity @intContractDetailId = @intContractDetailId
+					,@dblQuantityToUpdate = @dblQuantityToUpdate
+					,@intUserId = @intEntityUserSecurityId
+					,@intExternalId = @intExternalId
+					,@strScreenName = @strScreenName
+
+				SELECT @intMinLoadDetailId = MIN(intLoadDetailId)
+				FROM @tblLoadDetail
+				WHERE intLoadDetailId > @intMinLoadDetailId
+			END
+
+			UPDATE tblQMSample
+			SET intLoadContainerId = NULL
+				,intLoadDetailId = NULL
+				,intLoadDetailContainerLinkId = NULL
+				,intLoadId = NULL
+			WHERE intLoadId = @intLoadId
+
+			UPDATE tblLGLoad
+			SET intShipmentStatus = 10
+				,ysnCancelled = @ysnCancel
+			WHERE intLoadId = @intLoadId
+
+			EXEC [uspLGCreateLoadIntegrationLog] @intLoadId = @intLoadId
+				,@strRowState = 'Delete'
+				,@intShipmentType = @intShipmentType
+
+			EXEC [uspLGCreateLoadIntegrationLSPLog] @intLoadId = @intLoadId
+				,@strRowState = 'Delete'
+				,@intShipmentType = @intShipmentType
+		END
+		ELSE
+		BEGIN
+			SELECT @intMinLoadDetailId = MIN(intLoadDetailId)
+			FROM @tblLoadDetail
+
+			WHILE (@intMinLoadDetailId IS NOT NULL)
+			BEGIN
+				SET @intContractDetailId = NULL
+				SET @dblQuantityToUpdate = NULL
+				SET @intExternalId = NULL
+				SET @strScreenName = NULL
+
+				SELECT @intContractDetailId = intContractDetailId
+					,@dblQuantityToUpdate = dblLoadDetailQuantity
+					,@intExternalId = @intMinLoadDetailId
+					,@strScreenName = 'Load Schedule'
+				FROM @tblLoadDetail
+				WHERE intLoadDetailId = @intMinLoadDetailId
+
+				EXEC uspCTUpdateScheduleQuantity @intContractDetailId = @intContractDetailId
+					,@dblQuantityToUpdate = @dblQuantityToUpdate
+					,@intUserId = @intEntityUserSecurityId
+					,@intExternalId = @intExternalId
+					,@strScreenName = @strScreenName
+
+				SELECT @intMinLoadDetailId = MIN(intLoadDetailId)
+				FROM @tblLoadDetail
+				WHERE intLoadDetailId > @intMinLoadDetailId
+
+				UPDATE tblLGLoad
+				SET intShipmentStatus = 1
+					,ysnCancelled = @ysnCancel
+				WHERE intLoadId = @intLoadId
+
+				EXEC [uspLGCreateLoadIntegrationLog] @intLoadId = @intLoadId
+					,@strRowState = 'Added'
+					,@intShipmentType = @intShipmentType
+
+				EXEC [uspLGCreateLoadIntegrationLSPLog] @intLoadId = @intLoadId
+					,@strRowState = 'Added'
+					,@intShipmentType = @intShipmentType
+			END
+		END
+	END
+	ELSE IF (@intShipmentType = 2)
+	BEGIN
+		IF EXISTS (
+				SELECT 1
+				FROM tblLGLoad
+				WHERE intLoadShippingInstructionId = @intLoadId
+					AND intShipmentStatus <> 10
+				)
+		BEGIN
+			RAISERROR ('Shipment has already been created for the shipping instruction. Cannot cancel.',11,1)
+		END
+
+		UPDATE tblLGLoad
+		SET intShipmentStatus = 10
+			,ysnCancelled = @ysnCancel
+		WHERE intLoadId = @intLoadId
+
+		EXEC [uspLGCreateLoadIntegrationLog] @intLoadId = @intLoadId
+			,@strRowState = 'Delete'
+			,@intShipmentType = @intShipmentType
+	END
+END TRY
+
+BEGIN CATCH
+	SET @strErrMsg = ERROR_MESSAGE()
+
+	IF @strErrMsg != ''
+	BEGIN
+		SET @strErrMsg = @strErrMsg
+
+		RAISERROR (@strErrMsg,16,1,'WITH NOWAIT')
+	END
+END CATCH
