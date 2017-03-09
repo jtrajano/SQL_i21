5851540719508a02dbb2c04bcf0544a4509a983d
Swetosuvro Ghosh <swetosuvro.g@irely.com>
2017-03-09 21:11:57 +0530

    LG-865, LG-879

--- i21Database/dbo/Tables/tblLGLoad.sql
+++ i21Database/dbo/Tables/tblLGLoad.sql
@@ -81,6 +81,9 @@
 [dtmISFReceivedDate] DATETIME NULL,
 [dtmISFFiledDate] DATETIME NULL,
 [dtmStuffingDate] DATETIME NULL,
+[dtmStartDate] DATETIME NULL,
+[dtmEndDate] DATETIME NULL,
+[dtmPlannedAvailabilityDate] DATETIME NULL,
 
 [dblDemurrage] NUMERIC(18, 6) NULL,
 [intDemurrageCurrencyId] INT NULL,
@@ -102,6 +105,9 @@
 [intLoadShippingInstructionId]  INT NULL,
 [strExternalShipmentNumber]  NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NULL,
 [ysn4cRegistration] [bit] NULL,
+[ysnInvoice] [bit] NULL,
+[ysnProvisionalInvoice] [bit] NULL,
+[ysnQuantityFinal] [bit] NULL,
 [ysnCancelled] [bit] NULL,
 
 CONSTRAINT [PK_tblLGLoad] PRIMARY KEY ([intLoadId]), 
