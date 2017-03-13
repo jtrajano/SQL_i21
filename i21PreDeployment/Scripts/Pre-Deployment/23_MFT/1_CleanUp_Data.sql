fa6b195a01bb86948c3b2c87a34e801eb3a7c496
Mark Paragas <markparagas@irely.com>
2017-03-13 15:39:32 +0800

    MFT-450

--- i21PreDeployment/Scripts/Pre-Deployment/23_MFT/1_CleanUp_Data.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/23_MFT/1_CleanUp_Data.sql
@@ -2,4 +2,15 @@
 		BEGIN
 			DELETE FROM tblTFReportingComponentProductCode
 			WHERE intProductCodeId IS NULL
+
+			UPDATE tblTFReportingComponentProductCode
+			SET intConcurrencyId = 1
+			WHERE intConcurrencyId IS NULL
+		END
+
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = N'tblTFReportingComponent')
+		BEGIN
+			UPDATE tblTFReportingComponent
+			SET intConcurrencyId = 1
+			WHERE intConcurrencyId IS NULL
 		END
