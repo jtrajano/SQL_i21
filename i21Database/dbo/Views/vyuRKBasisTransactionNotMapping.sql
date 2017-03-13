435042642da68da12a48baba1c8746597e08f72d
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2017-03-13 12:08:48 +0530

    RM-663

---
+++ i21Database/dbo/Views/vyuRKBasisTransactionNotMapping.sql
@@ -0,0 +1,17 @@
+﻿CREATE VIEW  vyuRKBasisTransactionNotMapping
+
+AS 
+
+SELECT intM2MBasisTransactionId
+		,strCommodityCode
+		,strFutMarketName
+		,strCurrency
+		,strItemNo
+		,strUnitMeasure
+ FROM tblRKM2MBasisTransaction bd
+JOIN tblRKM2MBasis mb on mb.intM2MBasisId=bd.intM2MBasisId
+JOIN tblICCommodity c on c.intCommodityId=bd.intCommodityId
+JOIN tblICItem i on i.intItemId=bd.intItemId
+JOIN tblRKFutureMarket m on m.intFutureMarketId=bd.intFutureMarketId
+JOIN tblSMCurrency cur on cur.intCurrencyID=bd.intCurrencyId
+JOIN tblICUnitMeasure um on um.intUnitMeasureId=bd.intUnitMeasureId
