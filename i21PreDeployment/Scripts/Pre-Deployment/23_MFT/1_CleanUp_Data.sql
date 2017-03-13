07a21927c83e9e501bc56f91948414567b349ecc
Mark Paragas <markparagas@irely.com>
2017-03-13 18:16:35 +0800

    MFT-450

--- i21PreDeployment/Scripts/Pre-Deployment/23_MFT/1_CleanUp_Data.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/23_MFT/1_CleanUp_Data.sql
@@ -14,3 +14,10 @@ IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = N'tblTFRep
 			SET intConcurrencyId = 1
 			WHERE intConcurrencyId IS NULL
 		END
+
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = N'tblTFReportingComponentConfiguration')
+		BEGIN
+			UPDATE tblTFReportingComponentConfiguration
+			SET ysnConfiguration = 0
+			WHERE ysnConfiguration IS NULL
+		END
