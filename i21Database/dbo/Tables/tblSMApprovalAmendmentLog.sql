f2b8ab5250ad0629fd155faa5db0bc23aac84667
jesmar mendones <jesmar.mendones@irely.com>
2017-03-13 18:50:29 +0800

    FRM-4580

---
+++ i21Database/dbo/Tables/tblSMApprovalAmendmentLog.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tblSMApprovalAmendmentLog]
+(
+	[intApprovalAmendmentLogId]		INT              IDENTITY (1, 1)				NOT NULL,
+	[intTransactionId]				[int]											NOT NULL,
+	[strModifiedField]				[nvarchar](250) COLLATE Latin1_General_CI_AS	NOT NULL,
+	[strOldValue]					[nvarchar](max) COLLATE Latin1_General_CI_AS	NOT NULL,
+	[strNewValue]					[nvarchar](max) COLLATE Latin1_General_CI_AS	NOT NULL,
+	[intEntityId]					[int]											NOT NULL,
+	[intConcurrencyId]				[int]											NOT NULL DEFAULT (1), 
+	CONSTRAINT [FK_tblSMApprovalAmendmentLog_tblSMTransaction] FOREIGN KEY ([intTransactionId]) REFERENCES [tblSMTransaction]([intTransactionId]) ON DELETE CASCADE
+)
