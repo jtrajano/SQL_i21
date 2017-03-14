0e46628d2104770224fa020f38053cdcd6dde397
Lawrence Zabala <lawrence.zabala@irely.com>
2017-03-14 18:15:00 +0800

    MFT-492

--- i21Database/dbo/Stored Procedures/uspTFGenerateMF360.sql
+++ i21Database/dbo/Stored Procedures/uspTFGenerateMF360.sql
@@ -82,7 +82,7 @@ BEGIN TRY
 	FROM tblSMCompanySetup
 	
 	-- ======================== HEADER ==============================
-	INSERT INTO vyuTFGetTransactionSummary (strSummaryGuid
+	INSERT INTO tblTFTransactionSummary(strSummaryGuid
 		, intTaxAuthorityId
 		, strFormCode
 		, strScheduleCode
