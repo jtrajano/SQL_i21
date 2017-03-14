8badbf2dd02e4aa80ee744c5123a2ae673e3ef03
Swetosuvro Ghosh <swetosuvro.g@irely.com>
2017-03-14 18:46:34 +0530

    LG-889, LG-884

---
+++ i21Database/dbo/Tables/tblLGReasonCode.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[tblLGReasonCode]
+(
+ [intReasonCodeId] INT NOT NULL IDENTITY,
+ [intConcurrencyId] INT NOT NULL,
+ [strReasonCode] NVARCHAR(100) COLLATE Latin1_General_CI_AS NOT NULL,
+ [strReasonCodeDescription] NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NOT NULL,
+
+ CONSTRAINT [PK_tblLGReasonCode_intReasonCodeId] PRIMARY KEY ([intReasonCodeId]),
+ CONSTRAINT [UK_tblLGReasonCode_strReasonCode] UNIQUE ([strReasonCode])
+)
