4681876b952917b599db8de4f8b026e04c11c5f8
Wendell Wayne Estrada <wendell.estrada@irely.com>
2017-03-14 13:26:18 +0800

    Revert "IC-3493"
    This reverts commit 87bbef7cf6c2a3a8eb0905c78212b9af9dd40650.

--- i21Database/dbo/Views/vyuICGetItemStockUOMForAdjustment.sql
+++ i21Database/dbo/Views/vyuICGetItemStockUOMForAdjustment.sql
@@ -2,34 +2,37 @@
 AS 
 
 SELECT	StockUOM.intItemStockUOMId
-		,intItemId								= Item.intItemId
-		,strItemNo								= Item.strItemNo
-		,strItemDescription						= Item.strDescription
-		,strType								= Item.strType
-		,strLotTracking							= Item.strLotTracking
-		,intLocationId							= ISNULL(Location.intCompanyLocationId, ItemLoc2.intLocationId)
-		,intItemLocationId						= ISNULL(StockUOM.intItemLocationId, ItemLoc2.intItemLocationId)
-		,strLocationName						= ISNULL(Location.strLocationName, Location2.strLocationName)
-		,intItemUOMId							= ItemUOM.intItemUOMId
-		,strUnitMeasure							= UOM.strUnitMeasure
-		,strUnitType							= UOM.strUnitType
-		,intSubLocationId						= SubLocation.intCompanyLocationSubLocationId
-		,strSubLocationName						= SubLocation.strSubLocationName
-		,intStorageLocationId					= StorageLocation.intStorageLocationId
-		,strStorageLocationName					= StorageLocation.strName
-		,dblOnHand								= CAST(ISNULL(StockUOM.dblOnHand, 0) AS NUMERIC(18, 6)) 
-		,dblOnOrder								= CAST(ISNULL(StockUOM.dblOnOrder, 0) AS NUMERIC(18, 6)) 
-		,dblUnitQty								= CAST(ISNULL(ItemUOM.dblUnitQty, 0) AS NUMERIC(18, 6)) 
-		,ysnStockUnit							= CAST(ISNULL(ItemUOM.ysnStockUnit, 0) AS BIT)
-FROM tblICItemUOM ItemUOM 
-	INNER JOIN tblICItem Item ON Item.intItemId = ItemUOM.intItemId
-	CROSS APPLY tblICItemLocation ItemLoc2
-	LEFT JOIN tblSMCompanyLocation Location2 ON Location2.intCompanyLocationId = ItemLoc2.intLocationId
-	LEFT JOIN tblICItemStockUOM StockUOM ON ItemUOM.intItemUOMId = StockUOM.intItemUOMId
-	LEFT JOIN tblICItemLocation ItemLoc ON ItemLoc.intItemLocationId = StockUOM.intItemLocationId
-			AND ItemLoc.intItemId = Item.intItemId
-	LEFT JOIN tblSMCompanyLocation Location ON Location.intCompanyLocationId = ItemLoc.intLocationId
-	LEFT JOIN tblICUnitMeasure UOM ON UOM.intUnitMeasureId = ItemUOM.intUnitMeasureId
-	LEFT JOIN tblSMCompanyLocationSubLocation SubLocation ON SubLocation.intCompanyLocationSubLocationId = StockUOM.intSubLocationId
-	LEFT JOIN tblICStorageLocation StorageLocation ON StorageLocation.intStorageLocationId = StockUOM.intStorageLocationId
-WHERE ItemLoc2.intItemId = ItemUOM.intItemId
+		,Item.intItemId
+		,Item.strItemNo
+		,strItemDescription		= Item.strDescription
+		,Item.strType
+		,strLotTracking			= Item.strLotTracking
+		,intLocationId			= Location.intCompanyLocationId
+		,StockUOM.intItemLocationId
+		,Location.strLocationName
+		,ItemUOM.intItemUOMId
+		,UOM.strUnitMeasure
+		,UOM.strUnitType
+		,intSubLocationId		= SubLocation.intCompanyLocationSubLocationId
+		,SubLocation.strSubLocationName
+		,StorageLocation.intStorageLocationId
+		,strStorageLocationName	= StorageLocation.strName
+		,dblOnHand				= CAST(ISNULL(StockUOM.dblOnHand, 0) AS NUMERIC(18, 6)) 
+		,dblOnOrder				= CAST(ISNULL(StockUOM.dblOnOrder, 0) AS NUMERIC(18, 6)) 
+		,dblUnitQty				= CAST(ISNULL(ItemUOM.dblUnitQty, 0) AS NUMERIC(18, 6)) 
+		,ysnStockUnit			= CAST(ISNULL(ItemUOM.ysnStockUnit, 0) AS BIT)
+FROM	tblICItem Item INNER JOIN tblICItemUOM ItemUOM
+			ON Item.intItemId = ItemUOM.intItemId
+		LEFT JOIN tblICItemStockUOM StockUOM
+			ON ItemUOM.intItemUOMId = StockUOM.intItemUOMId
+		LEFT JOIN tblICItemLocation ItemLoc
+			ON ItemLoc.intItemLocationId = StockUOM.intItemLocationId
+				AND ItemLoc.intItemId = Item.intItemId
+		LEFT JOIN tblSMCompanyLocation Location 
+			ON Location.intCompanyLocationId = ItemLoc.intLocationId
+		LEFT JOIN tblICUnitMeasure UOM 
+			ON UOM.intUnitMeasureId = ItemUOM.intUnitMeasureId
+		LEFT JOIN tblSMCompanyLocationSubLocation SubLocation
+			ON SubLocation.intCompanyLocationSubLocationId = StockUOM.intSubLocationId
+		LEFT JOIN tblICStorageLocation StorageLocation 
+			ON StorageLocation.intStorageLocationId = StockUOM.intStorageLocationId
