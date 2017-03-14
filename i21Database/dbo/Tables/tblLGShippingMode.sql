a9ca85664d905468cc16797c7e0040c42cc91218
Swetosuvro Ghosh <swetosuvro.g@irely.com>
2017-03-14 18:46:34 +0530

    LG-889, LG-884

---
+++ i21Database/dbo/Tables/tblLGShippingMode.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[tblLGShippingMode]
+(
+ [intShippingModeId] INT NOT NULL IDENTITY,
+ [intConcurrencyId] INT NOT NULL,
+ [strShippingMode] NVARCHAR(100) COLLATE Latin1_General_CI_AS NOT NULL,
+
+ CONSTRAINT [PK_tblLGShippingMode_intShippingModeId] PRIMARY KEY ([intShippingModeId]),
+ CONSTRAINT [UK_tblLGShippingMode_strShippingMode] UNIQUE ([strShippingMode])
+ )
