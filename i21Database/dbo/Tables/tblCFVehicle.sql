414674510a5c2eaad64b3f8dbdc71cf1a1c94c88
bonn ponce <bonn.ponce@irely.com>
2017-03-15 14:42:59 +0800

    CF-971

--- i21Database/dbo/Tables/tblCFVehicle.sql
+++ i21Database/dbo/Tables/tblCFVehicle.sql
@@ -22,7 +22,7 @@
     [intConcurrencyId]        INT            CONSTRAINT [DF_tblCFVehicle_intConcurrencyId] DEFAULT ((1)) NULL,
     [dtmLastModified]         DATETIME       NULL,
     [ysnCardForOwnUse]        BIT            NULL,
-    [ysnActive] BIT NULL, 
+    [ysnActive]				  BIT			 CONSTRAINT [DF_tblCFVehicle_ysnActive]  DEFAULT ((1)) NOT NULL,
     CONSTRAINT [PK_tblCFVehicle] PRIMARY KEY CLUSTERED ([intVehicleId] ASC),
     CONSTRAINT [FK_tblCFVehicle_tblCFAccount] FOREIGN KEY ([intAccountId]) REFERENCES [dbo].[tblCFAccount] ([intAccountId]) ON DELETE CASCADE,
     CONSTRAINT [FK_tblCFVehicle_tblICItem] FOREIGN KEY ([intExpenseItemId]) REFERENCES [dbo].[tblICItem] ([intItemId])
