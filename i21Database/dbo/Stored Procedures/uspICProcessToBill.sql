ac84247b6274ebc87890e96f7180365df1a7a8ec
Wendell Wayne Estrada <wendell.estrada@irely.com>
2017-03-15 18:38:04 +0800

    IC-3565 Support multiple vouchers.

--- i21Database/dbo/Stored Procedures/uspICProcessToBill.sql
+++ i21Database/dbo/Stored Procedures/uspICProcessToBill.sql
@@ -1,13 +1,16 @@
 ﻿CREATE PROCEDURE [dbo].[uspICProcessToBill]
 	@intReceiptId int,
 	@intUserId int,
-	@intBillId int OUTPUT
+	@intBillId int OUTPUT,
+	@strBillIds NVARCHAR(MAX) = NULL OUTPUT,
+	@strPrefix NVARCHAR(10) = ',',
+	@strSuffix NVARCHAR(10) = ''
 AS
 SET QUOTED_IDENTIFIER OFF
 SET ANSI_NULLS ON
 SET NOCOUNT ON
 SET XACT_ABORT ON
-SET ANSI_WARNINGS OFF
+--SET ANSI_WARNINGS OFF
 
 DECLARE @ErrorMessage NVARCHAR(4000);
 DECLARE @ErrorSeverity INT;
@@ -27,6 +30,16 @@ DECLARE @ErrorState INT;
 		@intUserId
 
 	SELECT TOP 1 @intBillId = intBillId FROM #tmpBillIds
+
+	SELECT @strBillIds =
+	STUFF((
+		SELECT ISNULL(@strPrefix, ',') + CAST(ISNULL(intBillId, '') AS NVARCHAR(50)) + ISNULL(@strSuffix, '')
+		FROM #tmpBillIds
+		ORDER BY intBillId
+		FOR XML PATH(''),
+		TYPE
+	).value('.', 'NVARCHAR(MAX)'), 1, 1, '')
+
 	DROP TABLE #tmpBillIds
 
 --END TRY
