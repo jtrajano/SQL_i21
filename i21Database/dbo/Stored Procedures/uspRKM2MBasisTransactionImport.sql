e4a88e861064c72a0fa09ee5470e4d71083d5e44
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2017-03-15 16:41:24 +0530

    RM-681

--- i21Database/dbo/Stored Procedures/uspRKM2MBasisTransactionImport.sql
+++ i21Database/dbo/Stored Procedures/uspRKM2MBasisTransactionImport.sql
@@ -19,12 +19,12 @@ JOIN tblICItem it on it.strItemNo=i.strItemNo
 JOIN tblSMCurrency cu on cu.strCurrency=i.strCurrency
 JOIN tblICUnitMeasure um on um.strUnitMeasure=i.strUnitMeasure
 
-
 UPDATE tblRKM2MBasisDetail
 SET dblBasisOrDiscount = isnull(t2.dblBasis,null)
 FROM tblRKM2MBasisDetail t1
-LEFT JOIN tblRKM2MBasisTransaction t2 ON t1.intItemId = t2.intItemId
-WHERE t1.intM2MBasisId = @intM2MBasisId
+LEFT JOIN tblRKM2MBasisTransaction t2 ON t1.intM2MBasisId= t2.intM2MBasisId
+AND t1.intItemId = t2.intItemId and t1.intCommodityId=t2.intCommodityId and t1.intFutureMarketId=t2.intFutureMarketId
+WHERE  t1.intM2MBasisId= @intM2MBasisId
 
 
 COMMIT TRAN
@@ -34,7 +34,7 @@ SELECT 0 as intCuncurrencyId,* from tblRKSettlementPriceImport_ErrLog
 DELETE FROM tblRKM2MTransactionImport
 END TRY
 BEGIN CATCH
- IF XACT_STATE() != 0 AND @@TRANCOUNT > 0 ROLLBACK TRANSACTION      
- SET @ErrMsg = ERROR_MESSAGE()  
- RAISERROR(@ErrMsg, 16, 1, 'WITH NOWAIT') 
+	 IF XACT_STATE() != 0 AND @@TRANCOUNT > 0 ROLLBACK TRANSACTION      
+	 SET @ErrMsg = ERROR_MESSAGE()  
+	 RAISERROR(@ErrMsg, 16, 1, 'WITH NOWAIT') 
 End Catch
