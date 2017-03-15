28b391931d2ea7bfb69e73203be2df2fa899e32c
Swetosuvro Ghosh <swetosuvro.g@irely.com>
2017-03-15 14:04:18 +0530

    LG-884, LG-889, LG-894

--- i21Database/dbo/Tables/tblLGETATracking.sql
+++ i21Database/dbo/Tables/tblLGETATracking.sql
@@ -4,7 +4,9 @@
   [intLoadId] INT,
   [strTrackingType] NVARCHAR(100) COLLATE Latin1_General_CI_AS NOT NULL, 
   [dtmETAPOD] DATETIME NULL,
+  [strETAPODReasonCode] NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NULL, 
   [dtmETSPOL] DATETIME NULL,
+  [strETSPOLReasonCode] NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NULL, 
   [dtmModifiedOn] DATETIME,
   [intConcurrencyId] INT
 )
