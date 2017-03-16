3c803d8503d90386493c2e33f93965571974049a
Mark Paragas <markparagas@irely.com>
2017-03-16 15:58:25 +0800

    MFT-498

--- i21PreDeployment/Scripts/Pre-Deployment/23_MFT/1_CleanUp_Data.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/23_MFT/1_CleanUp_Data.sql
@@ -20,4 +20,7 @@ IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = N'tblTFRep
 			UPDATE tblTFReportingComponentConfiguration
 			SET ysnConfiguration = 0
 			WHERE ysnConfiguration IS NULL
+
+			DELETE FROM tblTFReportingComponentConfiguration
+			WHERE intReportingComponentId = 0 OR intReportingComponentId IS NULL
 		END
