2e9690571f9e8d3591b07f0fdaacecf6be0202bb
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2017-03-17 22:35:49 +0530

    RM-522

---
+++ i21Database/dbo/Functions/fnRKGetFutureAndBasisPriceForDate.sql
@@ -0,0 +1,38 @@
+﻿CREATE FUNCTION [dbo].[fnRKGetFutureAndBasisPriceForDate]
+(
+	@intCommodityId int ,
+	@dtmTicketDate datetime,
+	@intSequenceTypeId int, -- 1.	‘1’ Basis($) ,‘2’ Futures($), ‘3’ Futures and Basis 
+	@dblBasisCost NUMERIC(18, 6)
+)
+RETURNS NUMERIC(18, 6)
+AS
+BEGIN
+
+	DECLARE @strSeqMonth nvarchar(20)=''
+	DECLARE @intFutureMarketId int
+	DECLARE @intFutureMonthId int
+
+	SELECT @strSeqMonth=RIGHT(CONVERT(VARCHAR(11),DATEADD(dd, 0, DATEADD(month,  + DATEDIFF(month, 0, @dtmTicketDate),0)),6),6) 
+
+DECLARE @dblScaleBasisValue AS NUMERIC(18, 6)
+DECLARE @dblClosingPrice AS NUMERIC(18, 6)
+DECLARE @calculatedValue AS NUMERIC(18, 6)
+---Scale Basis
+SELECT TOP 1 @dblScaleBasisValue= isnull(dblBasis,0)+@dblBasisCost,@intFutureMonthId=bd.intFutureMonthId,@intFutureMarketId=bd.intFutureMarketId  FROM tblRKM2MBasis b
+JOIN tblRKM2MGrainBasis bd on b.intM2MBasisId=bd.intM2MBasisId 
+WHERE  intCommodityId = @intCommodityId and strDeliveryMonth = @strSeqMonth	AND ISNULL(dblBasis,0) <> 0 	  
+ORDER BY dtmM2MBasisDate Desc
+
+--FutureSettlemnt Price
+SELECT @dblClosingPrice=dbo.fnRKGetLatestClosingPrice(@intFutureMarketId,@intFutureMonthId,@dtmTicketDate)
+
+If @intSequenceTypeId = 1
+	SET @calculatedValue = isnull(@dblScaleBasisValue,0)
+ELSE IF @intSequenceTypeId = 2
+	SET @calculatedValue = isnull(@dblClosingPrice,0)
+ELSE
+	SET @calculatedValue = isnull(@dblClosingPrice,0)+isnull(@dblScaleBasisValue,0)
+
+	RETURN @calculatedValue
+END
---
+++ i21Database/dbo/Views/vyuRKGetSequenceMonthNotMapping.sql
@@ -0,0 +1,11 @@
+﻿CREATE VIEW vyuRKGetSequenceMonthNotMapping
+AS
+SELECT   intM2MGrainBasisId
+		,strCommodityCode
+		,strFutMarketName
+		,strFutureMonth +' ('+strSymbol+')' strFutureMonth
+ FROM tblRKM2MGrainBasis bd
+JOIN tblRKM2MBasis mb on mb.intM2MBasisId=bd.intM2MBasisId
+JOIN tblICCommodity c on c.intCommodityId=bd.intCommodityId
+JOIN tblRKFutureMarket m on m.intFutureMarketId=bd.intFutureMarketId
+JOIN tblRKFuturesMonth cur on cur.intFutureMonthId=bd.intFutureMonthId
