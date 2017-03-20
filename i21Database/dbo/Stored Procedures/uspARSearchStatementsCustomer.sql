710e6c88b176fc32da564496c10f40bf7eb1d23f
Michelle Jardiolin <michelle.jardiolin@irely.com>
2017-03-20 18:05:43 +0800

    AR-2891

--- i21Database/dbo/Stored Procedures/uspARSearchStatementsCustomer.sql
+++ i21Database/dbo/Stored Procedures/uspARSearchStatementsCustomer.sql
@@ -7,21 +7,53 @@ AS
 DECLARE @tmpstrStatementFormat NVARCHAR(50)
 SET @tmpstrStatementFormat = @strStatementFormat
 
-SELECT 
-	ARC.intEntityCustomerId,
-	ARC.strCustomerNumber,
-	strCustomerName		=	EM.strName 
-FROM
-	(SELECT 
-		intEntityCustomerId, 
-		strCustomerNumber 
-	 FROM 
-		tblARCustomer
-	 WHERE ysnActive = 1
-		AND strStatementFormat =  @strStatementFormat) ARC
-INNER JOIN 
-	(SELECT 
-		intEntityId, 
-		strName 
-	 FROM 
-		tblEMEntity) EM ON ARC.intEntityCustomerId = EM.intEntityId
+
+IF (@strStatementFormat IS NOT NULL)
+BEGIN
+	IF (@strStatementFormat = 'Open Item')
+	BEGIN
+		SELECT 
+			ARC.intEntityCustomerId,
+			ARC.strCustomerNumber,
+			strCustomerName		=	EM.strName 
+		FROM
+			(SELECT 
+				intEntityCustomerId, 
+				strCustomerNumber 
+			 FROM 
+				tblARCustomer
+			 WHERE ysnActive = 1
+				AND ((ISNULL(strStatementFormat, '') = '' OR strStatementFormat = @strStatementFormat))
+		
+			) ARC
+		INNER JOIN 
+			(SELECT 
+				intEntityId, 
+				strName 
+			 FROM 
+				tblEMEntity) EM ON ARC.intEntityCustomerId = EM.intEntityId		
+	END
+
+	ELSE 
+	BEGIN
+		SELECT 
+			ARC.intEntityCustomerId,
+			ARC.strCustomerNumber,
+			strCustomerName		=	EM.strName 
+		FROM
+			(SELECT 
+				intEntityCustomerId, 
+				strCustomerNumber 
+			 FROM 
+				tblARCustomer
+			 WHERE ysnActive = 1
+				AND strStatementFormat = @strStatementFormat		
+			) ARC
+		INNER JOIN 
+			(SELECT 
+				intEntityId, 
+				strName 
+			 FROM 
+				tblEMEntity) EM ON ARC.intEntityCustomerId = EM.intEntityId
+	END
+END
