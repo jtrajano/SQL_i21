ef0b280614208764a7fd633759b54fb4a16e232b
Gopinath Nayak <gopinath.n@irely.com>
2017-03-21 12:42:32 +0530

    GRN-551

--- i21Database/dbo/Views/vyuGRGetDiscountIdByLocation.sql
+++ i21Database/dbo/Views/vyuGRGetDiscountIdByLocation.sql
@@ -1,6 +1,6 @@
 ﻿CREATE VIEW [dbo].[vyuGRGetDiscountIdByLocation]
 AS     
-SELECT 
+SELECT DISTINCT
  DId.intDiscountId
 ,DId.strDiscountId
 ,DId.strDiscountDescription
@@ -10,7 +10,12 @@ SELECT
 ,LC.intCompanyLocationId
 ,L.strLocationName
 ,LC.ysnDiscountLocationActive
+,DSch.intCommodityId
+,Com.strCommodityCode
 FROM tblGRDiscountId DId
 JOIN tblSMCurrency Cur ON Cur.intCurrencyID = DId.intCurrencyId
 JOIN tblGRDiscountLocationUse LC ON LC.intDiscountId=DId.intDiscountId
 JOIN tblSMCompanyLocation L ON L.intCompanyLocationId = LC.intCompanyLocationId 
+JOIN tblGRDiscountCrossReference DCR ON DCR.intDiscountId=DId.intDiscountId
+JOIN tblGRDiscountSchedule DSch ON DSch.intDiscountScheduleId=DCR.intDiscountScheduleId
+JOIN tblICCommodity Com ON Com.intCommodityId=DSch.intCommodityId
