39f94f9c118a952a5ad701cfc02fdfbdab7ef54a
Mark Paragas <markparagas@irely.com>
2017-03-22 12:15:49 +0800

    MFT-507

--- i21PreDeployment/Scripts/Pre-Deployment/23_MFT/1_CleanUp_Data.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/23_MFT/1_CleanUp_Data.sql
@@ -14,52 +14,28 @@ IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = N'tblTFRep
 			SET intConcurrencyId = 1
 			WHERE intConcurrencyId IS NULL
 
-			DECLARE @RCID1 INT
-			
-			DECLARE @CountDeleted INT
-			SET @RCID1 = (SELECT TOP 1 intReportingComponentId 
-			FROM tblTFReportingComponent 
-			WHERE strType = 'NE EDI')
-
-			IF (@RCID1 IS NOT NULL)
-			BEGIN
-				DELETE FROM tblTFFilingPacket 
-				WHERE intReportingComponentId = @RCID1
-
-				SET @CountDeleted = (SELECT @@ROWCOUNT)
-				IF(@CountDeleted > 0)
-				BEGIN
-					DELETE FROM tblTFReportingComponent 
-					WHERE strType = 'NE EDI'
-				END
-			END
-
-			DECLARE @RCID2 INT
-			SET @CountDeleted = 0
-			SET @RCID2 = (SELECT TOP 1 intReportingComponentId 
-			FROM tblTFReportingComponent 
-			WHERE strType = 'EDI' AND strScheduleName = 'NE EDI file')
-
-			IF (@RCID2 IS NOT NULL)
-			BEGIN
-				DELETE FROM tblTFFilingPacket 
-				WHERE intReportingComponentId = @RCID2
-
-				SET @CountDeleted = (SELECT @@ROWCOUNT)
-				IF(@CountDeleted > 0)
-				BEGIN
-					DELETE FROM tblTFReportingComponent 
-					WHERE strType = 'EDI' AND strScheduleName = 'NE EDI file'
-				END
-			END
+			UPDATE tblTFReportingComponent SET strType = 'NE EDI' WHERE strScheduleName = 'NE EDI file'
 		END
 
 IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = N'tblTFReportingComponentConfiguration')
 		BEGIN
-			UPDATE tblTFReportingComponentConfiguration
-			SET ysnConfiguration = 0
-			WHERE ysnConfiguration IS NULL
-
-			DELETE FROM tblTFReportingComponentConfiguration
-			WHERE intReportingComponentId = 0 OR intReportingComponentId IS NULL
+		  IF EXISTS(SELECT *
+          FROM   INFORMATION_SCHEMA.COLUMNS
+          WHERE  TABLE_NAME = 'tblTFReportingComponentConfiguration'
+                 AND COLUMN_NAME = 'ysnConfiguration') 
+				 BEGIN
+					UPDATE tblTFReportingComponentConfiguration
+					SET ysnConfiguration = 0
+					WHERE ysnConfiguration IS NULL
+				 END
+				
+		  IF EXISTS(SELECT *
+          FROM   INFORMATION_SCHEMA.COLUMNS
+          WHERE  TABLE_NAME = 'tblTFReportingComponentConfiguration'
+                 AND COLUMN_NAME = 'intReportingComponentId') 
+				 BEGIN
+					DELETE FROM tblTFReportingComponentConfiguration
+					WHERE intReportingComponentId = 0 
+					OR intReportingComponentId IS NULL
+				 END
 		END
