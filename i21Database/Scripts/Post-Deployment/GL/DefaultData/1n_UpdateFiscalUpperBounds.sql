2bacfa6c769a49718320a6c21f24fb15c54436ac
jeffrey trajano <jeffrey.trajano@irely.com>
2017-03-23 12:00:41 +0800

    GL-3352 YTD of income statement accounts is not correct

---
+++ i21Database/Scripts/Post-Deployment/GL/DefaultData/1n_UpdateFiscalUpperBounds.sql
@@ -0,0 +1,13 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+UPDATE tblGLFiscalYear set dtmDateTo = DATEADD(SECOND,59, DATEADD(MINUTE, 59, DATEADD(HOUR, 23, DATEADD(dd, 0, DATEDIFF(dd, 0, dtmDateTo)))))
+UPDATE tblGLFiscalYearPeriod set dtmEndDate = DATEADD(SECOND,59, DATEADD(MINUTE, 59, DATEADD(HOUR, 23, DATEADD(dd, 0, DATEDIFF(dd, 0, dtmEndDate)))))
--- i21Database/dbo/Functions/fnGLGetMTDActivity.sql
+++ i21Database/dbo/Functions/fnGLGetMTDActivity.sql
@@ -1,5 +1,5 @@
-CREATE FUNCTION [dbo].[fnGLGetMTDActivity] 
-(	
+CREATE FUNCTION [dbo].[fnGLGetMTDActivity]
+(
 	@strAccountId NVARCHAR(100),
 	@dtmDate DATETIME
 )
@@ -10,19 +10,32 @@ BEGIN
 	DECLARE @activity DECIMAL(18,6)
 	SELECT @accountType= B.strAccountType  FROM tblGLAccount A JOIN tblGLAccountGroup B on A.intAccountGroupId = B.intAccountGroupId WHERE
 	A.strAccountId = @strAccountId and B.strAccountType IN ('Expense','Revenue','Cost of Goods Sold')
-	
-	SELECT  
-			@activity =
-			SUM ( CASE 
-					WHEN @accountType = 'Revenue' THEN (dblCredit - dblDebit)*-1
-					ELSE dblDebit - dblCredit
-			END)  
-					
-	FROM tblGLAccount A
-		LEFT JOIN tblGLDetail C ON A.intAccountId = C.intAccountId
-		CROSS APPLY (SELECT dtmStartDate,dtmEndDate from tblGLFiscalYearPeriod where @dtmDate between dtmStartDate AND  dtmEndDate) D
-	WHERE strAccountId = @strAccountId and ( C.dtmDate BETWEEN D.dtmStartDate and  D.dtmEndDate) and strCode <> ''  and ysnIsUnposted = 0
-	GROUP BY strAccountId
+    IF (@accountType is not null)
+        SELECT
+                @activity =
+                SUM ( CASE
+                        WHEN @accountType = 'Revenue' THEN (dblCredit - dblDebit)*-1
+                        ELSE dblDebit - dblCredit
+                END)
+
+        FROM tblGLAccount A
+            LEFT JOIN tblGLDetail C ON A.intAccountId = C.intAccountId
+            CROSS APPLY (SELECT dtmStartDate,dtmEndDate from tblGLFiscalYearPeriod where @dtmDate between dtmStartDate AND  dtmEndDate) D
+        WHERE strAccountId = @strAccountId and ( C.dtmDate BETWEEN D.dtmStartDate and  D.dtmEndDate) and strCode <> ''  and ysnIsUnposted = 0
+        GROUP BY strAccountId
+    ELSE
+         SELECT
+                @activity =
+                SUM ( CASE
+                        WHEN @accountType = 'Asset' THEN (dblDebit - dblCredit)*-1
+                        ELSE dblCredit - dblDebit
+                END)
+
+        FROM tblGLAccount A
+            LEFT JOIN tblGLDetail C ON A.intAccountId = C.intAccountId
+            CROSS APPLY (SELECT dtmStartDate,dtmEndDate from tblGLFiscalYearPeriod where @dtmDate between dtmStartDate AND  dtmEndDate) D
+        WHERE strAccountId = @strAccountId and ( C.dtmDate BETWEEN D.dtmStartDate and  D.dtmEndDate) and strCode <> ''  and ysnIsUnposted = 0
+        GROUP BY strAccountId
 
 	IF @activity IS NULL SET @activity = 0
 	RETURN @activity
