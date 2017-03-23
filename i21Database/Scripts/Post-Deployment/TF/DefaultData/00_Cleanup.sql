db9ace4034eaf1a5cd5db77b38a678897529ac31
Mark Paragas <markparagas@irely.com>
2017-03-23 18:26:42 +0800

    MFT-510

--- i21Database/Scripts/Post-Deployment/TF/DefaultData/00_Cleanup.sql
+++ i21Database/Scripts/Post-Deployment/TF/DefaultData/00_Cleanup.sql
@@ -1,15 +1,25 @@
 ﻿PRINT ('Cleanup Tax Form tables')
 
-IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = N'tblTFValidOriginState')
-BEGIN
-	DELETE FROM tblTFValidOriginState
-	WHERE ISNULL(strFilter, '') = ''
-END
-
-IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = N'tblTFReportingComponent')
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = N'tblTFValidOriginState' AND COLUMN_NAME = 'strFilter')
 		BEGIN
-			DELETE FROM tblTFReportingComponent WHERE strScheduleName = 'NE EDI file'
+			EXEC('DELETE FROM tblTFValidOriginState
+			WHERE ISNULL(strFilter, '''') = ''''')
 		END
+
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = N'tblTFReportingComponent' AND COLUMN_NAME = 'strScheduleName')
+		BEGIN
+			EXEC('DELETE FROM tblTFReportingComponent WHERE strScheduleName = ''NE EDI file''')
+		END
+
+IF EXISTS(SELECT *
+          FROM   INFORMATION_SCHEMA.COLUMNS
+          WHERE  TABLE_NAME = 'tblTFReportingComponentConfiguration'
+                 AND COLUMN_NAME = 'intReportingComponentId') 
+				 BEGIN
+					EXEC('DELETE FROM tblTFReportingComponentConfiguration
+					WHERE intReportingComponentId = 0 
+					OR intReportingComponentId IS NULL')
+				 END
 
 GO
 
