a846f9cbb3404e5b11181899919f27403cbcb786
jeffrey trajano <jeffrey.trajano@irely.com>
2017-03-23 17:39:29 +0800

    GL-3350 Trial Balance Tab is Out of Balance

--- i21Database/dbo/Functions/fnGLGetBeginningBalanceAndUnit.sql
+++ i21Database/dbo/Functions/fnGLGetBeginningBalanceAndUnit.sql
@@ -14,60 +14,44 @@ BEGIN
 	DECLARE @accountType NVARCHAR(30)
 	SELECT @accountType= B.strAccountType  FROM tblGLAccount A JOIN tblGLAccountGroup B on A.intAccountGroupId = B.intAccountGroupId WHERE
 	A.strAccountId = @strAccountId and B.strAccountType IN ('Expense','Revenue','Cost of Goods Sold')
-	
 	IF @accountType IS NOT NULL
-	BEGIN
-			
-			IF EXISTS(SELECT TOP 1 1 FROM tblGLFiscalYear WHERE dtmDateFrom = @dtmDate) 
-			INSERT @tbl
-				SELECT @strAccountId, 0, 0
-			ELSE
-			BEGIN
-			    SELECT @dtmDate = DATEADD(SECOND, -1, @dtmDate)
-				INSERT  @tbl
-				SELECT  
-						strAccountId,
-						SUM ( 
-							CASE 
-							  WHEN D.dtmDateFrom IS NULL THEN 0 
-							  WHEN @accountType = 'Revenue' THEN (dblCredit - dblDebit)*-1
-							  ELSE dblDebit - dblCredit
-						  
-						END)  beginBalance,
-						SUM( 
-						CASE 
-							WHEN D.dtmDateFrom IS NULL THEN 0 
-							WHEN @accountType = 'Revenue' THEN dblCreditUnit - dblDebitUnit
-								ELSE dblDebitUnit - dblCreditUnit
-						END)  beginBalanceUnit
-				FROM tblGLAccount A
-					LEFT JOIN tblGLAccountGroup B ON A.intAccountGroupId = B.intAccountGroupId
-					LEFT JOIN tblGLDetail C ON A.intAccountId = C.intAccountId
-					CROSS APPLY (SELECT dtmDateFrom,dtmDateTo from tblGLFiscalYear where @dtmDate between dtmDateFrom AND dtmDateTo) D
-				WHERE strAccountId = @strAccountId and ( C.dtmDate >= D.dtmDateFrom and  C.dtmDate < @dtmDate) and strCode <> ''  and ysnIsUnposted = 0
-				GROUP BY strAccountId
-			END
-		END
-	ELSE
-		BEGIN
-			SELECT @dtmDate = DATEADD(SECOND, -1, @dtmDate)
 			INSERT  @tbl
 			SELECT  
 					strAccountId,
-					SUM( 
-					CASE WHEN B.strAccountType = 'Asset' THEN dblDebit - dblCredit
-							ELSE (dblCredit - dblDebit)*-1
+					SUM ( CASE 
+						  WHEN D.dtmDateFrom IS NULL THEN 0 
+						  WHEN @accountType = 'Revenue' THEN (dblCredit - dblDebit)*-1
+						  ELSE dblDebit - dblCredit
 					END)  beginBalance,
 					SUM( 
-					CASE WHEN B.strAccountType = 'Asset' THEN dblDebitUnit - dblCreditUnit
-							ELSE dblCreditUnit - dblDebitUnit
+					CASE 
+						WHEN D.dtmDateFrom IS NULL THEN 0 
+						WHEN @accountType = 'Revenue' THEN dblCreditUnit - dblDebitUnit
+							ELSE dblDebitUnit - dblCreditUnit
 					END)  beginBalanceUnit
-		
 			FROM tblGLAccount A
 				LEFT JOIN tblGLAccountGroup B ON A.intAccountGroupId = B.intAccountGroupId
 				LEFT JOIN tblGLDetail C ON A.intAccountId = C.intAccountId
-			WHERE strAccountId = @strAccountId and C.dtmDate < @dtmDate and strCode <> '' and ysnIsUnposted = 0
+				CROSS APPLY (SELECT dtmDateFrom,dtmDateTo from tblGLFiscalYear where @dtmDate >= dtmDateFrom AND @dtmDate <= dtmDateTo) D
+			WHERE strAccountId = @strAccountId and ( C.dtmDate >= D.dtmDateFrom and  C.dtmDate < @dtmDate) and strCode <> ''  and ysnIsUnposted = 0
 			GROUP BY strAccountId
-		END
+	ELSE
+		INSERT  @tbl
+		SELECT  
+				strAccountId,
+				SUM( 
+				CASE WHEN B.strAccountType = 'Asset' THEN dblDebit - dblCredit
+						ELSE (dblCredit - dblDebit)*-1
+				END)  beginBalance,
+				SUM( 
+				CASE WHEN B.strAccountType = 'Asset' THEN dblDebitUnit - dblCreditUnit
+						ELSE dblCreditUnit - dblDebitUnit
+				END)  beginBalanceUnit
+		
+		FROM tblGLAccount A
+			LEFT JOIN tblGLAccountGroup B ON A.intAccountGroupId = B.intAccountGroupId
+			LEFT JOIN tblGLDetail C ON A.intAccountId = C.intAccountId
+		WHERE strAccountId = @strAccountId and C.dtmDate < @dtmDate and strCode <> '' and ysnIsUnposted = 0
+		GROUP BY strAccountId
 		RETURN
 END
