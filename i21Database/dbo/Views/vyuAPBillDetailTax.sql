f166833ba52013bb04e1afc804d3a58f14318ffb
Lex Eugene Peregrino <lex.peregrino@irely.com>
2017-03-23 18:06:44 +0800

    AP-3065 - Voucher: Taxing: Information detail issues on Voucher screen 1.0

---
+++ i21Database/dbo/Views/vyuAPBillDetailTax.sql
@@ -0,0 +1,24 @@
+﻿CREATE VIEW [dbo].[vyuAPBillDetailTax]
+AS
+SELECT          A.intBillDetailTaxId ,
+				A.intBillDetailId ,
+				A.intTaxGroupMasterId ,
+				A.intTaxGroupId ,
+				A.intTaxCodeId ,
+				A.intTaxClassId ,
+				A.strTaxableByOtherTaxes ,
+				A.strCalculationMethod ,
+				A.dblRate ,
+				A.intAccountId ,
+				A.dblTax ,
+				A.dblAdjustedTax ,
+				A.ysnTaxAdjusted ,
+				A.ysnSeparateOnBill ,
+				A.ysnCheckOffTax ,
+				A.ysnTaxExempt,
+				A.intConcurrencyId,
+				B.strTaxCode
+FROM dbo.tblAPBillDetailTax A
+INNER JOIN dbo.tblSMTaxCode B ON  A.intTaxCodeId  = B.intTaxCodeId
+GO
+
