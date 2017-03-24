fcea3811c190bf195781b38b0229476f40458e41
Feb Montefrio <feb.montefrio@irely.com>
2017-03-24 18:25:24 +0800

    IC-3477

--- i21Database/dbo/Views/vyuICGetInventoryReceiptVoucher2.sql
+++ i21Database/dbo/Views/vyuICGetInventoryReceiptVoucher2.sql
@@ -2,32 +2,37 @@
 AS 
 
 SELECT		
-		intId
-		,intInventoryReceiptId
-		,intInventoryReceiptItemId
-		,dtmReceiptDate
-		,strVendor
-		,strLocationName
-		,strReceiptNumber
-		,strBillOfLading
-		,strReceiptType
-		,strOrderNumber
-		,strItemNo
-		,strItemDescription
-		,dblUnitCost = ROUND(dblUnitCost, 6)
-		,dblReceiptQty = ROUND(dblReceiptQty, 6)
-		,dblVoucherQty = ROUND(dblVoucherQty, 6)
-		,dblReceiptLineTotal = CAST( ROUND(dblReceiptLineTotal, 6) AS NUMERIC(18, 6))
-		,dblVoucherLineTotal = CAST( ROUND(dblVoucherLineTotal, 6) AS NUMERIC(18, 6))
-		,dblReceiptTax = ROUND(dblReceiptTax, 6)
-		,dblVoucherTax = ROUND(dblVoucherTax, 6)
-		,dblOpenQty = ROUND(dblOpenQty, 6)
-		,dblItemsPayable = CAST( ROUND(dblItemsPayable, 6) AS NUMERIC(18, 6))
-		,dblTaxesPayable = CAST( ROUND(dblTaxesPayable, 6) AS NUMERIC(18, 6))
-		,dtmLastVoucherDate
-		,strAllVouchers
-		,strFilterString
-		,dtmCreated
-		,intCurrencyId
-		,strCurrency
-FROM	tblICSearchReceiptVoucher	
+		t.intId
+		,t.intInventoryReceiptId
+		,t.intInventoryReceiptItemId
+		,t.dtmReceiptDate
+		,t.strVendor
+		,t.strLocationName
+		,t.strReceiptNumber
+		,t.strBillOfLading
+		,t.strReceiptType
+		,t.strOrderNumber
+		,t.strItemNo
+		,t.strItemDescription
+		,dblUnitCost = ROUND(t.dblUnitCost, 6)
+		,dblReceiptQty = ROUND(t.dblReceiptQty, 6)
+		,dblVoucherQty = ROUND(t.dblVoucherQty, 6)
+		,dblReceiptLineTotal = CAST( ROUND(t.dblReceiptLineTotal, 6) AS NUMERIC(18, 6))
+		,dblVoucherLineTotal = CAST( ROUND(t.dblVoucherLineTotal, 6) AS NUMERIC(18, 6))
+		,dblReceiptTax = ROUND(t.dblReceiptTax, 6)
+		,dblVoucherTax = ROUND(t.dblVoucherTax, 6)
+		,dblOpenQty = ROUND(t.dblOpenQty, 6)
+		,dblItemsPayable = CAST( ROUND(t.dblItemsPayable, 6) AS NUMERIC(18, 6))
+		,dblTaxesPayable = CAST( ROUND(t.dblTaxesPayable, 6) AS NUMERIC(18, 6))
+		,t.dtmLastVoucherDate
+		,t.strAllVouchers
+		,t.strFilterString
+		,t.dtmCreated
+		,t.intCurrencyId
+		,t.strCurrency
+		,ri.intContainerId
+		,strContainer = c.strContainerNumber 
+FROM	tblICSearchReceiptVoucher t INNER JOIN tblICInventoryReceiptItem ri
+			ON t.intInventoryReceiptItemId = ri.intInventoryReceiptItemId
+		LEFT JOIN tblLGLoadContainer c
+			ON c.intLoadContainerId = ri.intContainerId
