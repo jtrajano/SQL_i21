c0e74d1d13f86b83644a6d5b2cee07689957bb8b
Arman Arenas <arman.arenas@irely.com>
2017-03-28 15:40:40 +0800

    PR-432 Added tblPRDepartmentShift

---
+++ i21Database/dbo/Tables/tblPRDepartmentShift.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[tblPRDepartmentShift]
+(
+	[intDepartmentShiftId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [intDepartmentId] INT NOT NULL, 
+    [intShiftNo] INT NOT NULL DEFAULT ((0)), 
+    [dtmStart] DATETIME NULL, 
+    [dtmEnd] DATETIME NULL, 
+    [dblRate] NUMERIC(18, 6) NULL DEFAULT ((0)), 
+    [dblRateType] NVARCHAR(30) NULL DEFAULT (('Per Hour')), 
+    [dblPayType] NVARCHAR(30) NULL DEFAULT (('Actual')), 
+    [intConcurrencyId] INT NULL DEFAULT ((1)),
+	CONSTRAINT [FK_tblPRDepartmentShift_tblPRDepartment] FOREIGN KEY ([intDepartmentId]) REFERENCES [dbo].[tblPRDepartment] ([intDepartmentId]) ON DELETE CASCADE
+)
