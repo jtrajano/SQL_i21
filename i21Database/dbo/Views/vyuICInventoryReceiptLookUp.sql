7148c041039f5661874c9a50eba65cdeb5f49bac
Joy Balangatan <joy.balangatan@irely.com>
2017-03-28 16:20:04 +0800

    IC-3620 Added fields to vyuICInventoryReceiptLookUp

--- i21Database/dbo/Views/vyuICInventoryReceiptLookUp.sql
+++ i21Database/dbo/Views/vyuICInventoryReceiptLookUp.sql
@@ -8,7 +8,11 @@ SELECT
 	, FreightTerm.strFobPoint
 	, Location.strLocationName
 	, Currency.strCurrency
-
+	, strFromLocation = FromLocation.strLocationName
+	, UserSecurity.strUserName
+	, strShipFrom = ShipFrom.strLocationName
+	, ShipVia.strShipVia
+	, FreightTerm.strFreightTerm
 FROM tblICInventoryReceipt Receipt LEFT JOIN vyuAPVendor Vendor 
 		ON Vendor.intEntityVendorId = Receipt.intEntityVendorId
 	LEFT JOIN tblSMCompanyLocation Location 
@@ -17,6 +21,14 @@ FROM tblICInventoryReceipt Receipt LEFT JOIN vyuAPVendor Vendor
 		ON FreightTerm.intFreightTermId = Receipt.intFreightTermId
 	LEFT JOIN tblSMCurrency Currency
 		ON Currency.intCurrencyID = Receipt.intCurrencyId
+	LEFT JOIN tblSMCompanyLocation FromLocation 
+		ON FromLocation.intCompanyLocationId = Receipt.intTransferorId
+	LEFT JOIN tblSMUserSecurity UserSecurity
+		ON UserSecurity.intEntityUserSecurityId = Receipt.intReceiverId
+	LEFT JOIN tblEMEntityLocation ShipFrom
+		ON ShipFrom.intEntityLocationId = Receipt.intShipFromId
+	LEFT JOIN tblSMShipVia ShipVia
+		ON ShipVia.intEntityShipViaId = Receipt.intShipViaId
 
 	--LEFT JOIN tblSMCompanyLocation Transferor 
 	--	ON Transferor.intCompanyLocationId = Receipt.intTransferorId
