5e66eeaa4a0e17c4d6eea6aedb0c8940984aa1bd
Jayson Ramos <jayson.ramos@iRely.com>
2017-03-28 17:14:56 +0800

    CRM-464

--- i21Database/dbo/Views/vyuHDItem.sql
+++ i21Database/dbo/Views/vyuHDItem.sql
@@ -3,6 +3,17 @@
 		select
 			intId = cast(ROW_NUMBER() over (order by item.intItemId) as int),
 			item.intItemId, item.strItemNo, item.strDescription, item.strType, item.strStatus,
+			intCompanyLocationId = null, strLocationName = null, strLocationNumber = null, strLocationType = null,
+			uom.intUnitMeasureId, uom.strUnitMeasure, uom.strSymbol, uom.strUnitType, itemUOM.intItemUOMId
+		from
+			tblICItem item
+			left outer join tblICItemUOM itemUOM on itemUOM.intItemId = item.intItemId
+			left outer join tblICUnitMeasure uom on uom.intUnitMeasureId = itemUOM.intUnitMeasureId
+
+		/*
+		select
+			intId = cast(ROW_NUMBER() over (order by item.intItemId) as int),
+			item.intItemId, item.strItemNo, item.strDescription, item.strType, item.strStatus,
 			comLoc.intCompanyLocationId, comLoc.strLocationName, comLoc.strLocationNumber, comLoc.strLocationType,
 			uom.intUnitMeasureId, uom.strUnitMeasure, uom.strSymbol, uom.strUnitType, itemUOM.intItemUOMId
 		from
@@ -11,3 +22,4 @@
 			left outer join tblSMCompanyLocation comLoc on comLoc.intCompanyLocationId = itemLoc.intLocationId
 			left outer join tblICItemUOM itemUOM on itemUOM.intItemId = item.intItemId
 			left outer join tblICUnitMeasure uom on uom.intUnitMeasureId = itemUOM.intUnitMeasureId
+		*/
