02e81a8b8fa3f0197f1a7d7645e5070070e161c6
Wendell Wayne Estrada <wendell.estrada@irely.com>
2017-03-29 16:41:15 +0800

    IC-3651 Inventory Storage Location - Standardize Gridcombobox to eliminate cachin

--- i21Database/dbo/Views/vyuICGetStorageLocation.sql
+++ i21Database/dbo/Views/vyuICGetStorageLocation.sql
@@ -35,9 +35,14 @@ SELECT StorageLocation.intStorageLocationId
 	, StorageLocation.dblEffectiveDepth
 	, StorageLocation.dblUnitPerFoot
 	, StorageLocation.dblResidualUnit
+	, strBatchSizeUOM = UnitMeasure.strUnitMeasure
+	, intItemId = StorageLocation.intItemId
+	, strItemNo = Item.strItemNo
 FROM tblICStorageLocation StorageLocation
 LEFT JOIN tblICStorageUnitType StorageUnitType ON StorageUnitType.intStorageUnitTypeId = StorageLocation.intStorageUnitTypeId
 LEFT JOIN tblSMCompanyLocation Location ON Location.intCompanyLocationId = StorageLocation.intLocationId
 LEFT JOIN tblSMCompanyLocationSubLocation SubLocation ON SubLocation.intCompanyLocationSubLocationId = StorageLocation.intSubLocationId
 LEFT JOIN tblICStorageLocation ParentStorageLocation ON ParentStorageLocation.intStorageLocationId = StorageLocation.intParentStorageLocationId
 LEFT JOIN tblICRestriction Restriction ON Restriction.intRestrictionId = StorageLocation.intRestrictionId
+LEFT JOIN tblICUnitMeasure UnitMeasure ON UnitMeasure.intUnitMeasureId = StorageLocation.intBatchSizeUOMId
+LEFT JOIN tblICItem Item ON Item.intItemId = StorageLocation.intItemId
