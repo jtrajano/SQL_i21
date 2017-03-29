72f2b88bedb70d3e700f4900af12d6784c00a68f
Michelle Jardiolin <michelle.jardiolin@irely.com>
2017-03-29 15:19:13 +0800

    AR-2891, AR-4455, AR-4456

--- i21Database/dbo/Stored Procedures/uspARCustomerStatementBalanceForwardReport.sql
+++ i21Database/dbo/Stored Procedures/uspARCustomerStatementBalanceForwardReport.sql
@@ -213,7 +213,14 @@ WHILE EXISTS(SELECT 1 FROM @temp_xml_table)
 BEGIN
 	SELECT @id = id, @fieldname = [fieldname], @condition = [condition], @from = [from], @to = [to], @join = [join], @datatype = [datatype] FROM @temp_xml_table
 	SET @filter = @filter + ' ' + dbo.fnAPCreateFilter(@fieldname, @condition, @from, @to, @join, null, null, @datatype)
-	
+
+		IF (@fieldname = 'strName' OR @fieldname = 'strCustomerName' )
+			SET @filter = REPLACE (@filter, '|^|', ''',''')
+			SET @filter = REPLACE (@filter, ''',''''', '''')
+			SET @filter = REPLACE (@filter, '=', 'IN (')
+			SET @filter = @filter + ')'
+			SET @filter = REPLACE (@filter, '))', ')')
+				
 	DELETE FROM @temp_xml_table WHERE id = @id
 
 	IF EXISTS(SELECT 1 FROM @temp_xml_table)
