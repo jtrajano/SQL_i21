2039710222324f80ba41aba3151f7edc51667b77
Erick F. Dela Cruz <erick.delacruz@irely.com>
2017-03-30 23:43:24 +0800

    AP-3116

--- i21Database/dbo/Views/vyuAPBill.sql
+++ i21Database/dbo/Views/vyuAPBill.sql
@@ -61,7 +61,7 @@ FROM
 			D.intBillId
 			,D.intPaymentId
 			,D.strPaymentInfo
-			,D.strBankAccountNo
+			,CONVERT(NVARCHAR(MAX),DecryptByKey(CAST(N'' as XML).value('xs:base64Binary(sql:column(''strBankAccountNo''))', 'varbinary(128)')))	 AS strBankAccountNo
 			,D.ysnPrinted
 			,D.ysnVoid
 			,D.ysnCleared
