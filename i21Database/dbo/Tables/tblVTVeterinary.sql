588ca276833a8926fed1cc51a9cc62803949e73f
Mon Gonzales <mon.gonzales@irely.com>
2017-04-03 17:51:18 +0800

    EM-1535

--- i21Database/dbo/Tables/tblVTVeterinary.sql
+++ i21Database/dbo/Tables/tblVTVeterinary.sql
@@ -1,12 +1,12 @@
 ﻿CREATE TABLE [dbo].[tblVTVeterinary]
 (
-	[intEntityVeterinaryId] INT NOT NULL,
+	[intEntityId] INT NOT NULL,
 	[strLicenseNumber] NVARCHAR(100) NOT NULL,
 	[strLicensingState] NVARCHAR(50) NULL,
 	[dtmLicenseExpiration] DATETIME NULL,
 	[ysnActive] BIT DEFAULT(1),
     [intConcurrencyId] INT DEFAULT ((0)) NOT NULL,
 
-	CONSTRAINT [PK_tblVTVeterinary_intEntityId] PRIMARY KEY CLUSTERED ([intEntityVeterinaryId] ASC),
-	CONSTRAINT [FK_tblVTVeterinary_tblEMEntity_intEntityId] FOREIGN KEY ([intEntityVeterinaryId]) REFERENCES [dbo].tblEMEntity ([intEntityId])  ON DELETE CASCADE,
+	CONSTRAINT [PK_tblVTVeterinary_intEntityId] PRIMARY KEY CLUSTERED ([intEntityId] ASC),
+	CONSTRAINT [FK_tblVTVeterinary_tblEMEntity_intEntityId] FOREIGN KEY ([intEntityId]) REFERENCES [dbo].tblEMEntity ([intEntityId])  ON DELETE CASCADE,
 )
