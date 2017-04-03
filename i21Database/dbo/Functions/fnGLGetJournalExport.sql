cfc85c032ab67c70854fe612b6f343e881eb22ed
jeffrey trajano <jeffrey.trajano@irely.com>
2017-04-03 17:10:29 +0800

    GL-2120 GL Import from CSV - Make import easier

--- i21Database/dbo/Functions/fnGLGetJournalExport.sql
+++ i21Database/dbo/Functions/fnGLGetJournalExport.sql
@@ -1,7 +1,6 @@
-﻿CREATE FUNCTION fnGLGetJournalExport(@intJournalId INT)
+﻿CREATE  FUNCTION [dbo].[fnGLGetJournalExport](@intJournalId INT)
 RETURNS  @tbl TABLE(
 	strAccountId NVARCHAR(40),
-	strCurrencyExchangeRateType NVARCHAR(20),
 	strDescription NVARCHAR(255),
 	dtmDate VARCHAR(20),
 	strDebitCredit VARCHAR(1),
@@ -11,10 +10,39 @@ RETURNS  @tbl TABLE(
 	intLineNo INT,
 	strDocument NVARCHAR(100),
 	strComments NVARCHAR(255),
-	strReference NVARCHAR(100)
+	strReference NVARCHAR(100),
+	strCurrencyExchangeRateType NVARCHAR(20)
 )
 as
 BEGIN
+	INSERT INTO @tbl
+	SELECT 
+		A.strAccountId 
+		,RTRIM(D.strDescription) strDescription
+		,CONVERT(VARCHAR(20),dtmDate,101) dtmDate
+		,CASE WHEN	dblDebit > dblCredit THEN 'D' ELSE 'C'END strDebitCredit
+		,CASE WHEN tsp.intDefaultCurrencyId = A.intCurrencyID 
+			THEN dblDebit + dblCredit 
+			ELSE D.dblDebitForeign + D.dblCreditForeign 
+			END dblAmount
+		,CASE WHEN dblDebit > dblCredit 
+			THEN dblDebitUnit ELSE dblCreditUnit 
+			END dblUnit
+		,CASE WHEN strCorrecting is null OR LEN(RTRIM(strCorrecting)) = 0 
+			THEN 'N' ELSE strCorrecting 
+			END AS strCorrecting
+		,ROW_NUMBER() OVER (ORDER BY dtmDate)as intLineNo
+		,strDocument
+		,D.strComments
+		,strReference 
+		,tsert.strCurrencyExchangeRateType
+	FROM tblGLJournalDetail D 
+		LEFT JOIN tblGLAccount A ON A.intAccountId = D.intAccountId 
+		LEFT JOIN dbo.tblSMCurrencyExchangeRateType tsert ON tsert.intCurrencyExchangeRateTypeId = D.intCurrencyExchangeRateTypeId
+		CROSS APPLY(SELECT intDefaultCurrencyId FROM dbo.tblSMCompanyPreference)tsp
+	WHERE intJournalId = @intJournalId
+	RETURN
+END
 	INSERT INTO @tbl
 	SELECT 
 		A.strAccountId 
