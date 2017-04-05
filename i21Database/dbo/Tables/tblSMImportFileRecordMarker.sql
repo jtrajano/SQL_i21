3420508e94ce248e067cb22287d9c627f4cc371a
Dan Michael Jalimao <dan.jalimao@irely.com>
2017-04-05 15:33:26 +0800

    SM-2710

--- i21Database/dbo/Tables/tblSMImportFileRecordMarker.sql
+++ i21Database/dbo/Tables/tblSMImportFileRecordMarker.sql
@@ -9,6 +9,7 @@
 	[intSequence] INT NULL,
     [intConcurrencyId] INT NULL DEFAULT ((0)), 
 	[strFormat] NVARCHAR(50)  COLLATE Latin1_General_CI_AS NULL,
+	[intRounding] INT NULL ,
     CONSTRAINT [PK_tblSMImportFileRecordMarker] PRIMARY KEY ([intImportFileRecordMarkerId]),
 	CONSTRAINT [FK_tblSMImportFileRecordMarker_tblSMImportHeader_intImportFileHeaderId] FOREIGN KEY ([intImportFileHeaderId]) REFERENCES [dbo].[tblSMImportFileHeader] ([intImportFileHeaderId]) ON DELETE CASCADE
 )
