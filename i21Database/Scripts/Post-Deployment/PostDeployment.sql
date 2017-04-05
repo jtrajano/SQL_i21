482b92067890c5e6affe873877fa60bd4b077e41
kenneth lagunero <kenneth.lagunero@irely.com>
2017-04-05 20:54:53 +0800

    TM-2310

--- i21Database/Scripts/Post-Deployment/PostDeployment.sql
+++ i21Database/Scripts/Post-Deployment/PostDeployment.sql
@@ -130,7 +130,7 @@ print 'BEGIN POST DEPLOYMENT'
 --:r "..\..\..\Integration\dbo\Views\vyuTMLeaseCode.sql"
 :r "..\..\..\Integration\dbo\Stored Procedures\uspTMAlterCobolWrite.sql"
 :r ".\TM\2_DataTransferAndCorrection.sql" 
-:r "..\..\..\Integration\dbo\Stored Procedures\uspTMRecreateBudgetCalculationItemPricingView.sql"
+--:r "..\..\..\Integration\dbo\Stored Procedures\uspTMRecreateBudgetCalculationItemPricingView.sql"
 --:r "..\..\..\Integration\dbo\Stored Procedures\uspTMRecreateBudgetCalculationSiteView.sql"
 --:r "..\..\..\Integration\dbo\Stored Procedures\uspTMRecreateBudgetCalculationSiteSP.sql"
 :r "..\..\..\Integration\dbo\Stored Procedures\uspTMRecreateLeaseSearchView.sql"
