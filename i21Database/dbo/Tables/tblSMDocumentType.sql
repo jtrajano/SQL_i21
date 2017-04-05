33978365d915d6da0f1f8980506aa997d6f8b629
Dan Michael Jalimao <dan.jalimao@irely.com>
2017-04-05 13:57:55 +0800

    SM-2632

--- i21Database/dbo/Tables/tblSMDocumentType.sql
+++ i21Database/dbo/Tables/tblSMDocumentType.sql
@@ -2,5 +2,6 @@
     [intDocumentTypeId]	INT             IDENTITY (1, 1) NOT NULL,
     [strName]			NVARCHAR (150)  COLLATE Latin1_General_CI_AS NOT NULL,
     [intConcurrencyId]	INT				NOT NULL,
-    CONSTRAINT [PK_dbo.tblSMDocumentType] PRIMARY KEY CLUSTERED ([intDocumentTypeId] ASC)
+    CONSTRAINT [PK_dbo.tblSMDocumentType] PRIMARY KEY CLUSTERED ([intDocumentTypeId] ASC), 
+    CONSTRAINT [AK_tblSMDocumentType_strName] UNIQUE ([strName])
 );
