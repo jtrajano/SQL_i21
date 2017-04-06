5b424757c40cd1818aea90d8ca472b70c04ffa45
Dan Michael Jalimao <dan.jalimao@irely.com>
2017-04-06 13:12:14 +0800

    SM-2632

--- i21Database/dbo/Tables/tblSMDocumentType.sql
+++ i21Database/dbo/Tables/tblSMDocumentType.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[tblSMDocumentType] (
     [intDocumentTypeId]	INT             IDENTITY (1, 1) NOT NULL,
     [strName]			NVARCHAR (150)  COLLATE Latin1_General_CI_AS NOT NULL,
+	[ysnCommit]			BIT				NOT NULL DEFAULT 0,
     [intConcurrencyId]	INT				NOT NULL,
     CONSTRAINT [PK_dbo.tblSMDocumentType] PRIMARY KEY CLUSTERED ([intDocumentTypeId] ASC), 
     CONSTRAINT [AK_tblSMDocumentType_strName] UNIQUE ([strName])
