e8d2859ce385dda22faffd9c7f82c08dbcf41b68
Vinoth Kumar T <vinoth.t@irely.com>
2017-04-06 19:52:04 +0530

    QC-305

--- i21Database/dbo/Views/vyuQMSampleTypeControlPoint.sql
+++ i21Database/dbo/Views/vyuQMSampleTypeControlPoint.sql
@@ -1,16 +1,47 @@
 ﻿CREATE VIEW vyuQMSampleTypeControlPoint
 AS
-SELECT ST.intControlPointId
-	,ST.intSampleTypeId
-	,ST.strSampleTypeName
-	,ST.strDescription
-	,PC.intProductId
-	,CAST(ROW_NUMBER() OVER (
-			ORDER BY PC.intProductId
+WITH Item
+AS (
+	SELECT ST.intControlPointId
+		,ST.intSampleTypeId
+		,ST.strSampleTypeName
+		,ST.strDescription
+		,PC.intProductId
+		,P.intProductTypeId
+		,P.intProductValueId
+		,P.ysnActive
+	FROM tblQMProductControlPoint PC
+	JOIN tblQMSampleType ST ON ST.intSampleTypeId = PC.intSampleTypeId
+	JOIN tblQMProduct P ON P.intProductId = PC.intProductId
+	)
+SELECT CAST(ROW_NUMBER() OVER (
+			ORDER BY intProductId
 			) AS INT) AS intRowNo
-	,P.intProductTypeId
-	,P.intProductValueId
-	,P.ysnActive
-FROM tblQMProductControlPoint PC
-JOIN tblQMSampleType ST ON ST.intSampleTypeId = PC.intSampleTypeId
-JOIN tblQMProduct P ON P.intProductId = PC.intProductId
+	,*
+FROM (
+	SELECT *
+	FROM Item
+	
+	UNION ALL
+	
+	SELECT ST.intControlPointId
+		,ST.intSampleTypeId
+		,ST.strSampleTypeName
+		,ST.strDescription
+		,PC.intProductId
+		,2 AS intProductTypeId -- Configuring as 2 to not to change client filter
+		,I.intItemId AS intProductValueId
+		,P.ysnActive
+	FROM tblQMProductControlPoint PC
+	JOIN tblQMSampleType ST ON ST.intSampleTypeId = PC.intSampleTypeId
+	JOIN tblQMProduct P ON P.intProductId = PC.intProductId
+	JOIN tblICItem I ON I.intCategoryId = P.intProductValueId
+		AND P.intProductTypeId = 1
+		AND NOT EXISTS (
+			SELECT intSampleTypeId
+				,intProductValueId
+			FROM Item x
+			WHERE x.intSampleTypeId = ST.intSampleTypeId
+				AND x.intProductValueId = I.intItemId
+			)
+	) t
