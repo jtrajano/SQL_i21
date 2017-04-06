2a0d571b6e1d4d9aa4198f7b21e2946e536e66ab
Dan Michael Jalimao <dan.jalimao@irely.com>
2017-04-06 13:12:14 +0800

    SM-2632

--- i21Database/dbo/Tables/tblSMDocumentTypeField.sql
+++ i21Database/dbo/Tables/tblSMDocumentTypeField.sql
@@ -5,6 +5,7 @@
 	[strFieldType]				NVARCHAR (100)  COLLATE Latin1_General_CI_AS NOT NULL,
 	[intSort]					INT             NOT NULL,
 	[ysnRequired]				BIT             NOT NULL,
+	[ysnCommit]					BIT				NOT NULL DEFAULT 0,
     [intConcurrencyId]			INT				NOT NULL,
 	CONSTRAINT [FK_tblSMDocumentTypeField_tblSMDocumentType] FOREIGN KEY ([intDocumentTypeId]) REFERENCES [dbo].[tblSMDocumentType] ([intDocumentTypeId]) ON DELETE CASCADE,
     CONSTRAINT [PK_dbo.tblSMDocumentTypeField] PRIMARY KEY CLUSTERED ([intDocumentTypeFieldId] ASC)
