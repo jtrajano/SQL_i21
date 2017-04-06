b7fa8060c5cc522cf74200f29a8fc6f9e2906fee
Jayson Ramos <jayson.ramos@iRely.com>
2017-04-06 14:03:17 +0800

    CRM-470

---
+++ i21Database/dbo/Views/vyuCRMCampaignEntityLink.sql
@@ -0,0 +1,14 @@
+﻿CREATE VIEW [dbo].[vyuCRMCampaignEntityLink]
+	AS
+		select * from (
+			select
+				intEntityId = a.intEntityContactId
+				,strContactName = b.strName
+				,strEntityName = c.strName
+				,strEntityType = (select top 1 d.strType from tblEMEntityType d where d.intEntityId = a.intEntityId	and d.strType in ('Customer','Prospect'))
+				,strEmail = b.strEmail
+			from tblEMEntityToContact a, tblEMEntity b, tblEMEntity c
+			where
+				b.intEntityId = a.intEntityContactId
+				and c.intEntityId = a.intEntityId
+		) as link where strEntityType is not null
