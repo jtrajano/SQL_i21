865853e31716c5da5a7033a5853f28c7c43aa4fd
Feb Montefrio <feb.montefrio@irely.com>
2017-04-07 15:35:58 +0800

    IC-3682

---
+++ i21Database/dbo/Views/vyuICItemStock.sql
@@ -0,0 +1,14 @@
+﻿CREATE VIEW [dbo].[vyuICItemStock]
+AS 
+
+SELECT	s.*
+		,stockUOM.strUnitMeasure 
+FROM	tblICItemStock s 
+		OUTER APPLY (
+			SELECT	TOP 1 
+					UOM.strUnitMeasure
+			FROM	tblICItemUOM iUOM INNER JOIN tblICUnitMeasure UOM
+						ON iUOM.intUnitMeasureId = UOM.intUnitMeasureId
+			WHERE	iUOM.intItemId = s.intItemId
+					AND iUOM.ysnStockUnit = 1
+		) stockUOM
