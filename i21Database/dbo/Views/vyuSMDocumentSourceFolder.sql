d5f51e2b1daebe41036453192139c5c3618c167a
Von Carlo Pasion <von.pasion@irely.com>
2017-04-07 21:49:00 +0800

    SM-2458

---
+++ i21Database/dbo/Views/vyuSMDocumentSourceFolder.sql
@@ -0,0 +1,13 @@
+﻿CREATE VIEW [dbo].vyuSMDocumentSourceFolder
+AS
+
+SELECT
+	intDocumentSourceFolderId,
+	intScreenId,
+	strName,
+	intSort,
+	intDocumentTypeId,
+	intConcurrencyId,
+	intDocumentFolderParentId,
+	leaf = CAST(CASE WHEN (SELECT COUNT(*) FROM tblSMDocumentSourceFolder B WHERE B.intDocumentFolderParentId = A.intDocumentSourceFolderId) = 0 THEN 1 ELSE 0 END AS BIT) 
+from tblSMDocumentSourceFolder A
