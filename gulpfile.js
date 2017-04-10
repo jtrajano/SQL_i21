b94c79f01337aabaef7c9ccbfcc0937f8080afa5
Feb Montefrio <feb.montefrio@irely.com>
2017-04-10 18:52:44 +0800

    Revert "IC-3700"
    This reverts commit 3cfd1e926693c020e0fc957417cb2d227e867618.

--- gulpfile.js
+++ gulpfile.js
@@ -1,107 +1,107 @@
 var source = './app';
-var destination = '../../Artifacts/1710/Inventory/app';
+var destination = '../../../artifacts/17.1/Inventory/debug/app';
 
 var gulp = require('gulp');
-// var prettify = require('gulp-js-prettify');
-// var changed = require('gulp-changed');
-// var Server = require('karma').Server;
-// var gen = require('gulp-extjs-spec-generator');
+var prettify = require('gulp-js-prettify');
+var changed = require('gulp-changed');
+var Server = require('karma').Server;
+var gen = require('gulp-extjs-spec-generator');
 
-// /**
-//  * **************************************************************************
-//  * *       *           *           *           *              *             *
-//  *                        Unit Test Specs Generation
-//  * *       *           *           *           *              *             *
-//  * ************************************************************************** 
-//  */
+/**
+ * **************************************************************************
+ * *       *           *           *           *              *             *
+ *                        Unit Test Specs Generation
+ * *       *           *           *           *              *             *
+ * ************************************************************************** 
+ */
 
-// var destDir = 'test/specs';
-// function getConfig(type) {
-//     return {
-//         type: type,
-//         moduleName: "Inventory",
-//         dependencyDir: "app/**/*.js",
-//         resolveModuleDependencies: true,
-//         destDir: destDir,
-//         formatContent: true,
-//         dependencyDestDir: "test/mock"    
-//     };
-// }
+var destDir = 'test/specs';
+function getConfig(type) {
+    return {
+        type: type,
+        moduleName: "Inventory",
+        dependencyDir: "app/**/*.js",
+        resolveModuleDependencies: true,
+        destDir: destDir,
+        formatContent: true,
+        dependencyDestDir: "test/mock"    
+    };
+}
 
-// /**
-//  * ===================================================
-//  *            Generate Specs Asynchronuously
-//  * ===================================================
-//  */
+/**
+ * ===================================================
+ *            Generate Specs Asynchronuously
+ * ===================================================
+ */
 
-// gulp.task('spec-m', function() {
-//     return gulp.src('app/model/**/*.js')
-//         .pipe(gen(getConfig("model")))
-//         .pipe(gulp.dest(destDir));
-// });
+gulp.task('spec-m', function() {
+    return gulp.src('app/model/**/*.js')
+        .pipe(gen(getConfig("model")))
+        .pipe(gulp.dest(destDir));
+});
 
-// gulp.task('spec-s', function() {
-//     return gulp.src('app/store/**/*.js')
-//         .pipe(gen(getConfig("store")))
-//         .pipe(gulp.dest(destDir));
-// });
+gulp.task('spec-s', function() {
+    return gulp.src('app/store/**/*.js')
+        .pipe(gen(getConfig("store")))
+        .pipe(gulp.dest(destDir));
+});
 
-// gulp.task('spec-vc', function() {
-//     return gulp.src('app/view/**/*.js')
-//         .pipe(gen(getConfig("viewcontroller")))
-//         .pipe(gulp.dest(destDir));
-// });
+gulp.task('spec-vc', function() {
+    return gulp.src('app/view/**/*.js')
+        .pipe(gen(getConfig("viewcontroller")))
+        .pipe(gulp.dest(destDir));
+});
 
-// gulp.task("spec",["spec-m", "spec-s", "spec-vc"]);
+gulp.task("spec",["spec-m", "spec-s", "spec-vc"]);
 
-// /**
-//  * ===================================================
-//  *            Generate Specs Synchronuously
-//  * ===================================================
-//  */
-// gulp.task('spec-s-sync', ['spec-m'], function() {
-//     return gulp.src('app/store/**/*.js')
-//         .pipe(gen(getConfig("store")))
-//         .pipe(gulp.dest(destDir));
-// });
+/**
+ * ===================================================
+ *            Generate Specs Synchronuously
+ * ===================================================
+ */
+gulp.task('spec-s-sync', ['spec-m'], function() {
+    return gulp.src('app/store/**/*.js')
+        .pipe(gen(getConfig("store")))
+        .pipe(gulp.dest(destDir));
+});
 
-// gulp.task('spec-vc-sync', ['spec-s-sync'], function() {
-//     return gulp.src('app/view/**/*.js')
-//         .pipe(gen(getConfig("viewcontroller")))
-//         .pipe(gulp.dest(destDir));
-// });
+gulp.task('spec-vc-sync', ['spec-s-sync'], function() {
+    return gulp.src('app/view/**/*.js')
+        .pipe(gen(getConfig("viewcontroller")))
+        .pipe(gulp.dest(destDir));
+});
 
-// gulp.task("spec-sync",["spec-vc-sync"]);
+gulp.task("spec-sync",["spec-vc-sync"]);
 
-// /**
-//  * **************************************************************************
-//  * *       *           *           *           *              *             *
-//  *                            Unit Testing
-//  * *       *           *           *           *              *             *
-//  * ************************************************************************** 
-//  */
-// gulp.task('test', function(done) {
-//     new Server({
-//         configFile: __dirname + '/karma.conf.js',
-//         singleRun: false,
-//     }, done).start();
-// });
+/**
+ * **************************************************************************
+ * *       *           *           *           *              *             *
+ *                            Unit Testing
+ * *       *           *           *           *              *             *
+ * ************************************************************************** 
+ */
+gulp.task('test', function(done) {
+    new Server({
+        configFile: __dirname + '/karma.conf.js',
+        singleRun: false,
+    }, done).start();
+});
 
-// gulp.task('test-mocha', function(done) {
-//     new Server({
-//         configFile: __dirname + '/karma.conf.js',
-//         singleRun: false,
-//         reporters: 'mocha'
-//     }, done).start();
-// });
+gulp.task('test-mocha', function(done) {
+    new Server({
+        configFile: __dirname + '/karma.conf.js',
+        singleRun: false,
+        reporters: 'mocha'
+    }, done).start();
+});
 
-// gulp.task('test-single', function(done) {
-//     new Server({
-//         configFile: __dirname + '/karma.single.conf.js',
-//         singleRun: false,
-//         reporters: 'mocha'
-//     }, done).start();
-// });
+gulp.task('test-single', function(done) {
+    new Server({
+        configFile: __dirname + '/karma.single.conf.js',
+        singleRun: false,
+        reporters: 'mocha'
+    }, done).start();
+});
 
 
 /**
