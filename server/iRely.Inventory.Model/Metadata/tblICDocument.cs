dcf723f539712cc0266ef74326a97412d39f167e
Satya <satya.sahoo@irely.com>
2017-04-10 20:54:30 +0530

    CT-1246

--- server/iRely.Inventory.Model/Metadata/tblICDocument.cs
+++ server/iRely.Inventory.Model/Metadata/tblICDocument.cs
@@ -19,6 +19,8 @@ namespace iRely.Inventory.Model
         public int? intCommodityId { get; set; }
         public bool ysnStandard { get; set; }
         public int? intCertificationId { get; set; }
+        public int? intOriginal { get; set; }
+        public int? intCopies { get; set; }
 
         public ICollection<tblICItemContractDocument> tblICItemContractDocuments { get; set; }
         public tblICCommodity tblICCommodity { get; set; }
