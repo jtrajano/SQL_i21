0e64f797b35c9fdb8c196be698ea633c318d6a2d
Kelvin Quinto <kelvin.quinto@irely.com>
2017-04-12 05:15:44 +0800

    AR-4518

--- i21Database/dbo/Stored Procedures/uspARCalculateServiceCharge.sql
+++ i21Database/dbo/Stored Procedures/uspARCalculateServiceCharge.sql
@@ -169,8 +169,8 @@ AS
 										   GROUP BY PD.intInvoiceId
 								) AS PAYMENTDATE ON PAYMENTDATE.intInvoiceId = I.intInvoiceId 
 							WHERE I.ysnPosted = 1 							  
-								AND I.strTransactionType = 'Invoice'
-								AND I.strType IN ('Standard', 'Transport Delivery')
+								AND (I.strTransactionType = 'Invoice' OR (I.strTransactionType = 'Debit Memo' AND I.strType = 'CF Invoice'))
+								AND I.strType IN ('Standard', 'Transport Delivery', 'CF Invoice')
 								AND I.intEntityCustomerId = @entityId
 								AND DATEADD(DAY, SC.intGracePeriod, CASE WHEN ISNULL(I.ysnForgiven, 0) = 0 AND ISNULL(I.ysnCalculated, 0) = 0 THEN I.dtmDueDate ELSE I.dtmCalculated END) < @asOfDate
 								AND (PAYMENTDATE.dtmDatePaid IS NOT NULL AND DATEADD(DAY, SC.intGracePeriod, CASE WHEN ISNULL(I.ysnForgiven, 0) = 0 AND ISNULL(I.ysnCalculated, 0) = 0 THEN I.dtmDueDate ELSE I.dtmCalculated END) < PAYMENTDATE.dtmDatePaid OR PAYMENTDATE.dtmDatePaid IS NULL)
