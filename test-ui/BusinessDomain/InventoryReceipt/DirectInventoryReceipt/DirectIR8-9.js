00d093033f63d98d9fbe916f3d1b175e1a27e400
Cavinne Jandhrix Callado <cavinne.callado@irely.com>
2017-04-12 14:43:09 +0800

    TS-2827 17.1 IC - Direct IR Scenario 8-9

--- test-ui/BusinessDomain/InventoryReceipt/DirectInventoryReceipt/DirectIR8-9.js
+++ test-ui/BusinessDomain/InventoryReceipt/DirectInventoryReceipt/DirectIR8-9.js
@@ -1,18 +1,9 @@
 StartTest (function (t) {
-        var commonIC = Ext.create('Inventory.CommonIC');
-        new iRely.FunctionalTest().start(t)
+    var commonIC = Ext.create('Inventory.CommonIC');
+    new iRely.FunctionalTest().start(t)
 
 
-        //Scenario 3: Check Gross/Net UOM and Line Total where Receipt UOM and Gross/Net UOM is the same
-        .displayText('===== Scenario 3: Check Gross/Net UOM and Line Total where Receipt UOM and Gross/Net UOM is the same =====')
-        .addFunction(function(next){
-            commonIC.addDirectIRLotted (t,next, 'ABC Trucking', 1, 'Direct - LTI - 01','LB', 100, 10, 'Raw Station', 'RM Storage', 'LOT-01', 'LB')
-        })
-
-
-        //Create Direct IR for Non Lotted Item
-        .displayText('===== Scenario 4: Check Gross/Net UOM and Line Total where Receipt UOM and Gross/Net UOM are different =====')
-
+        .displayText('===== Scenario 8: Create Direct Inventory Receipt Type. (Lotted Item) Check GL Entries =====')
         .displayText('===== Creeating Direct IR for Non Lotted Item  =====')
         .clickMenuFolder('Inventory','Folder')
         .clickMenuScreen('Inventory Receipts','Screen')
@@ -23,26 +14,25 @@ StartTest (function (t) {
         .selectComboBoxRowValue('Vendor', 'ABC Trucking' , 'Vendor',1)
         .selectComboBoxRowNumber('Location',1,0)
         .selectGridComboBoxRowValue('InventoryReceipt',1,'strItemNo', 'Direct - LTI - 01','strItemNo')
-        .enterUOMGridData('InventoryReceipt', 1, 'colUOMQtyToReceive', 'strUnitMeasure', 100, '50 lb bag')
+        .enterUOMGridData('InventoryReceipt', 1, 'colUOMQtyToReceive', 'strUnitMeasure', 1000, 'LB')
         .verifyGridData('InventoryReceipt', 1, 'colItemSubCurrency', 'USD')
         .enterGridData('InventoryReceipt', 1, 'colUnitCost', 10)
         .verifyGridData('InventoryReceipt', 1, 'colCostUOM', 'LB')
         .verifyGridData('InventoryReceipt', 1, 'colWeightUOM', 'LB')
-        .verifyGridData('InventoryReceipt', 1, 'colGross', '5000')
-        .verifyGridData('InventoryReceipt', 1, 'colNet', '5000')
-        .verifyGridData('InventoryReceipt', 1, 'colLineTotal', '50000')
+        .verifyGridData('InventoryReceipt', 1, 'colGross', '1000')
+        .verifyGridData('InventoryReceipt', 1, 'colNet', '1000')
+        .verifyGridData('InventoryReceipt', 1, 'colLineTotal', '10000')
         .selectGridComboBoxRowValue('InventoryReceipt',1,'strSubLocationName','Raw Station','strSubLocationName')
         .selectGridComboBoxRowValue('InventoryReceipt',1,'strStorageLocationName','RM Storage','strSubLocationName')
 
-        .enterGridData('LotTracking', 1, 'colLotId', 'LOT-02')
+        .selectGridComboBoxRowValue('LotTracking',1,'strLotNumber','LOT-01','strLotNumber')
         .selectGridComboBoxRowValue('LotTracking',1,'strUnitMeasure','LB' ,'strUnitMeasure')
-        .enterGridData('LotTracking', 1, 'colLotQuantity', 5000)
-        .verifyGridData('LotTracking', 1, 'colLotGrossWeight', '5000')
-        .verifyGridData('LotTracking', 1, 'colLotNetWeight', '5000')
+        .enterGridData('LotTracking', 1, 'colLotQuantity', 1000)
+        .verifyGridData('LotTracking', 1, 'colLotGrossWeight', '1000')
+        .verifyGridData('LotTracking', 1, 'colLotNetWeight', '1000')
         .verifyGridData('LotTracking', 1, 'colLotTareWeight', '0')
         .verifyGridData('LotTracking', 1, 'colLotWeightUOM','LB')
         .verifyGridData('LotTracking', 1, 'colLotStorageLocation', 'RM Storage')
-
 
         .clickTab('Post Preview')
         .waitUntilLoaded('')
@@ -51,20 +41,22 @@ StartTest (function (t) {
         .clickTab('Post Preview')
         .waitUntilLoaded('')
         .verifyGridData('RecapTransaction', 1, 'colAccountId', '16000-0001-000')
-        .verifyGridData('RecapTransaction', 1, 'colDebit', '50000')
+        .verifyGridData('RecapTransaction', 1, 'colDebit', '10000')
         .verifyGridData('RecapTransaction', 2, 'colAccountId', '21000-0001-000')
-        .verifyGridData('RecapTransaction', 2, 'colCredit', '50000')
+        .verifyGridData('RecapTransaction', 2, 'colCredit', '10000')
         .clickButton('Post')
         .waitUntilLoaded('')
         .addResult('Successfully Posted',2000)
         .waitUntilLoaded('')
+        .waitUntilLoaded('')
+        .addResult('Successfully Posted',2000)
         .clickButton('Close')
         .waitUntilLoaded('')
-        .displayText('===== Creating Direct IR for Non Lotted Done =====')
         .clickMenuFolder('Inventory','Folder')
+        .displayText('===== Scenario 8: Creating Direct IR for Non Lotted Done =====')
 
 
-        .displayText('===== Scenario 5: Receipt UOM 50 KG Bags Net Weight is Less than Gross Weight =====')
+        .displayText('===== Scenario 9:  Receipt Type: Direct Qty to Receive: 100 Receipt uom: Each (quantity) Lotted item =====')
         .displayText('===== Creeating Direct IR for Non Lotted Item  =====')
         .clickMenuFolder('Inventory','Folder')
         .clickMenuScreen('Inventory Receipts','Screen')
@@ -74,27 +66,19 @@ StartTest (function (t) {
         .selectComboBoxRowNumber('ReceiptType',4,0)
         .selectComboBoxRowValue('Vendor', 'ABC Trucking' , 'Vendor',1)
         .selectComboBoxRowNumber('Location',1,0)
-        .selectGridComboBoxRowValue('InventoryReceipt',1,'strItemNo', 'Direct - LTI - 02','strItemNo')
-        .enterUOMGridData('InventoryReceipt', 1, 'colUOMQtyToReceive', 'strUnitMeasure', 100, 'Test_50 KG Bag')
+        .selectGridComboBoxRowValue('InventoryReceipt',1,'strItemNo', 'Direct - LTI - 03','strItemNo')
+        .enterUOMGridData('InventoryReceipt', 1, 'colUOMQtyToReceive', 'strUnitMeasure', 100, 'Unit')
         .verifyGridData('InventoryReceipt', 1, 'colItemSubCurrency', 'USD')
         .enterGridData('InventoryReceipt', 1, 'colUnitCost', 10)
-        .verifyGridData('InventoryReceipt', 1, 'colCostUOM', 'Test_KG')
-        .verifyGridData('InventoryReceipt', 1, 'colWeightUOM', 'Test_KG')
-        .verifyGridData('InventoryReceipt', 1, 'colGross', '5000')
-        .enterGridData('InventoryReceipt', 1, 'colNet', 4900)
-
-        .verifyGridData('InventoryReceipt', 1, 'colLineTotal', '49000')
+        .verifyGridData('InventoryReceipt', 1, 'colCostUOM', 'Unit')
         .selectGridComboBoxRowValue('InventoryReceipt',1,'strSubLocationName','Raw Station','strSubLocationName')
         .selectGridComboBoxRowValue('InventoryReceipt',1,'strStorageLocationName','RM Storage','strSubLocationName')
 
-        .enterGridData('LotTracking', 1, 'colLotId', 'LOT-02')
-        .selectGridComboBoxRowValue('LotTracking',1,'strUnitMeasure','Test_50 KG Bag' ,'strUnitMeasure')
+        .enterGridData('LotTracking', 1, 'colLotId', 'LOT-01')
+        .selectGridComboBoxRowValue('LotTracking',1,'strUnitMeasure','Unit' ,'strUnitMeasure')
         .enterGridData('LotTracking', 1, 'colLotQuantity', 100)
-        .verifyGridData('LotTracking', 1, 'colLotGrossWeight', '5000')
-        .enterGridData('LotTracking', 1, 'colLotTareWeight', 100)
-        .verifyGridData('LotTracking', 1, 'colLotWeightUOM','Test_KG')
-        .verifyGridData('LotTracking', 1, 'colLotStorageLocation', 'RM Storage')
 
+        .verifyGridData('LotTracking', 1, 'colLotStorageLocation', 'RM Storage')
 
         .clickTab('Post Preview')
         .waitUntilLoaded('')
@@ -103,17 +87,20 @@ StartTest (function (t) {
         .clickTab('Post Preview')
         .waitUntilLoaded('')
         .verifyGridData('RecapTransaction', 1, 'colAccountId', '16000-0001-000')
-        .verifyGridData('RecapTransaction', 1, 'colDebit', '49000')
+        .verifyGridData('RecapTransaction', 1, 'colDebit', '1000')
         .verifyGridData('RecapTransaction', 2, 'colAccountId', '21000-0001-000')
-        .verifyGridData('RecapTransaction', 2, 'colCredit', '49000')
+        .verifyGridData('RecapTransaction', 2, 'colCredit', '1000')
         .clickButton('Post')
         .waitUntilLoaded('')
         .addResult('Successfully Posted',2000)
         .waitUntilLoaded('')
+        .waitUntilLoaded('')
+        .addResult('Successfully Posted',2000)
         .clickButton('Close')
         .waitUntilLoaded('')
-        .displayText('===== Creating Direct IR for Non Lotted Done =====')
         .clickMenuFolder('Inventory','Folder')
+        .displayText('===== Creating Direct IR for Non Lotted Done =====')
+
 
 
 
