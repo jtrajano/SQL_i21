89c0896d0ccfc1f2c4d49e75a606aac5535a65e1
Kelvin Quinto <kelvin.quinto@irely.com>
2017-04-19 04:54:44 +0800

    AR-4213

---
+++ i21Database/dbo/Functions/fnEliminateHTMLTags.sql
@@ -0,0 +1,22 @@
+﻿CREATE FUNCTION [dbo].[fnEliminateHTMLTags]
+(
+	@HTMLText NVARCHAR(MAX)
+)
+RETURNS NVARCHAR(MAX) AS
+BEGIN
+    DECLARE @intStart	INT
+		  , @intEnd		INT
+		  , @intLength	INT
+	
+    SET @intStart = CHARINDEX('<', @HTMLText)
+    SET @intEnd = CHARINDEX('>', @HTMLText, CHARINDEX('<', @HTMLText))
+    SET @intLength = (@intEnd - @intStart) + 1
+    WHILE @intStart > 0 AND @intEnd > 0 AND @intLength > 0
+    BEGIN
+        SET @HTMLText = STUFF(@HTMLText, @intStart, @intLength,'')
+        SET @intStart = CHARINDEX('<', @HTMLText)
+        SET @intEnd = CHARINDEX('>', @HTMLText, CHARINDEX('<', @HTMLText))
+        SET @intLength = (@intEnd - @intStart) + 1
+    END
+    RETURN LTRIM(RTRIM(@HTMLText))
+END
