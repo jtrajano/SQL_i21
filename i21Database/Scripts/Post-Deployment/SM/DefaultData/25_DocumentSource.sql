9333b205f8def638cd1b7ca21ec3f476b48829b1
Dan Michael Jalimao <dan.jalimao@irely.com>
2017-04-19 23:04:13 +0800

    SM-2740

---
+++ i21Database/Scripts/Post-Deployment/SM/DefaultData/25_DocumentSource.sql
@@ -0,0 +1,12 @@
+﻿PRINT N'BEGIN DOCUMENT SOURCE'
+GO
+	IF NOT EXISTS(SELECT TOP 1 1 FROM tblSMDocumentSourceFolder WHERE strName = 'Printed')
+	BEGIN
+		DECLARE @contractId INT
+		SELECT @contractId = intScreenId FROM tblSMScreen WHERE strScreenName = 'Contract' AND strModule = 'Contract Management'
+
+		INSERT INTO tblSMDocumentSourceFolder([intScreenId], [strName], [intSort], [intConcurrencyId])	
+		VALUES(@contractId, 'Printed', 0, 1)
+	END
+	PRINT N'END DOCUMENT SOURCE'
+GO
