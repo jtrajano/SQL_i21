3a3fbe3dc05848a1d6368e264875945b534ec8f2
Vinoth Kumar T <vinoth.t@irely.com>
2017-04-19 14:52:26 +0530

    MFG-2424

---
+++ i21Database/dbo/Views/vyuMFGetMachinesByCell.sql
@@ -0,0 +1,12 @@
+﻿CREATE VIEW vyuMFGetMachinesByCell
+AS
+SELECT DISTINCT PM.intManufacturingProcessMachineId
+	,PM.intManufacturingProcessId
+	,PM.intMachineId
+	,M.strName AS strMachineName
+	,MPT1.intManufacturingCellId
+	,PM.intLocationId
+FROM dbo.tblMFManufacturingProcessMachine AS PM
+JOIN dbo.tblMFMachine AS M ON PM.intMachineId = M.intMachineId
+JOIN dbo.tblMFMachinePackType MPT ON MPT.intMachineId = M.intMachineId
+JOIN dbo.tblMFManufacturingCellPackType MPT1 ON MPT1.intPackTypeId = MPT.intPackTypeId
