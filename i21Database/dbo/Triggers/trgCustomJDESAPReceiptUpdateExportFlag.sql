47cfe83ceef4e9e72eb10cc74b22255376272a5d
Chakra <chakradhar.behera@irely.com>
2017-04-19 20:28:20 +0530

    IN-73

---
+++ i21Database/dbo/Triggers/trgCustomJDESAPReceiptUpdateExportFlag.sql
@@ -0,0 +1,20 @@
+﻿CREATE TRIGGER [trgCustomJDESAPReceiptUpdateExportFlag]
+ON tblICInventoryReceipt
+FOR UPDATE
+AS
+Begin
+
+Declare @intInventoryReceiptId INT,
+		@ysnPosted Bit
+
+If Not Exists(
+	Select 1 From tblIPSAPIDOCTag Where strTag='UPDATE_RECEIPT_EXPORT_FLAG_ON_UNPOST' AND strValue='TRUE'
+)
+return
+
+Select @intInventoryReceiptId=intInventoryReceiptId,@ysnPosted=ISNULL(ysnPosted,0) From inserted
+
+If @ysnPosted=0
+	Update tblICInventoryReceiptItem Set ysnExported=null Where intInventoryReceiptId=@intInventoryReceiptId
+
+End
