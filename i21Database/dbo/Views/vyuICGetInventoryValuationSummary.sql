6df9d1cd6945fdcf5194780603f8de4078870808
Feb Montefrio <feb.montefrio@irely.com>
2017-04-20 17:26:10 +0800

    IC-3739

--- i21Database/dbo/Views/vyuICGetInventoryValuationSummary.sql
+++ i21Database/dbo/Views/vyuICGetInventoryValuationSummary.sql
@@ -1,41 +1,41 @@
 ﻿CREATE VIEW [dbo].[vyuICGetInventoryValuationSummary]
-	AS 
+AS 
 
-SELECT intInventoryValuationKeyId = CAST(ROW_NUMBER() OVER (ORDER BY Item.intItemId) AS INT), 
-	Item.intItemId, 
-	strItemNo = Item.strItemNo, 
-	strItemDescription = Item.strDescription, 
-	ISNULL([Transaction].intItemLocationId, 0) AS intItemLocationId, 
-	ISNULL(Location.strLocationName, ' ') AS strLocationName, 
-	ISNULL([Transaction].intSubLocationId, 0) AS intSubLocationId, 
-	ISNULL (SubLocation.strSubLocationName, ' ') AS strSubLocationName, 
-	ISNULL(dblQuantity, 0) AS dblQuantity, 
-	ISNULL(dblValue, 0) AS dblValue, 
-	ISNULL((dblQuantity * ItemPricing.dblLastCost), 0) AS dblLastCost, 
-	ISNULL((dblQuantity * ItemPricing.dblStandardCost),0) AS dblStandardCost, 
-	ISNULL((dblQuantity * ItemPricing.dblAverageCost),0) AS dblAverageCost,
-	[Transaction].strStockUOM,
-	[Transaction].dblQuantityInStockUOM
-FROM (
-	SELECT Item.intItemId, [Transaction].intItemLocationId, 
-		[Transaction].intSubLocationId, 
-		dblQuantity = SUM([Transaction].dblQty * [Transaction].dblUOMQty), 
-		dblValue = SUM(ISNULL([Transaction].dblQty, 0) * ISNULL([Transaction].dblCost, 0) + ISNULL([Transaction].dblValue, 0)),
-		strStockUOM = umStock.strUnitMeasure,
-		dblQuantityInStockUOM = SUM(ISNULL(dbo.fnCalculateQtyBetweenUOM([Transaction].intItemUOMId, iuStock.intItemUOMId, [Transaction].dblQty), 0))
-    FROM tblICItem Item
-		LEFT JOIN tblICInventoryTransaction [Transaction] ON [Transaction].intItemId = Item.intItemId
-		LEFT JOIN tblICItemUOM iuStock ON iuStock.intItemId = Item.intItemId
-			AND iuStock.ysnStockUnit = 1
-		LEFT JOIN tblICUnitMeasure umStock
-			ON iuStock.intUnitMeasureId = umStock.intUnitMeasureId
-	GROUP BY Item.intItemId, 
-		[Transaction].intItemLocationId, 
-		[Transaction].intSubLocationId, umStock.strUnitMeasure
-	) [Transaction]
-	LEFT JOIN tblICItem Item ON Item.intItemId = [Transaction].intItemId
-	LEFT JOIN tblICItemLocation ItemLocation ON ItemLocation.intItemLocationId = [Transaction].intItemLocationId
-	LEFT JOIN tblICItemPricing ItemPricing ON ItemPricing.intItemLocationId = ItemLocation.intItemLocationId
-	LEFT JOIN tblSMCompanyLocation Location ON Location.intCompanyLocationId = ItemLocation.intLocationId
-	LEFT JOIN tblSMCompanyLocationSubLocation SubLocation ON SubLocation.intCompanyLocationSubLocationId = [Transaction].intSubLocationId
+SELECT	intInventoryValuationKeyId = CAST(ROW_NUMBER() OVER (ORDER BY Item.intItemId) AS INT)
+		,Item.intItemId
+		,strItemNo = Item.strItemNo
+		,strItemDescription = Item.strDescription
+		,intItemLocationId = ISNULL([Transaction].intItemLocationId, 0)
+		,strLocationName = ISNULL(Location.strLocationName, ' ') 
+		,intSubLocationId = ISNULL([Transaction].intSubLocationId, 0)
+		,strSubLocationName = ISNULL(SubLocation.strSubLocationName, ' ') 
+		,dblQuantity = ISNULL(dblQuantity, 0)
+		,dblValue = ISNULL(dblValue, 0)
+		,dblLastCost = ISNULL( ROUND(dblQuantity * ItemPricing.dblLastCost, 2), 0)
+		,dblStandardCost = ISNULL( ROUND(dblQuantity * ItemPricing.dblStandardCost, 2),0)
+		,dblAverageCost = ISNULL( ROUND(dblQuantity * ItemPricing.dblAverageCost, 2),0)
+		,[Transaction].strStockUOM
+		,[Transaction].dblQuantityInStockUOM
+FROM	(
+			SELECT Item.intItemId, [Transaction].intItemLocationId, 
+				[Transaction].intSubLocationId, 
+				dblQuantity = SUM([Transaction].dblQty * [Transaction].dblUOMQty), 
+				dblValue = SUM(ROUND(ISNULL([Transaction].dblQty, 0) * ISNULL([Transaction].dblCost, 0) + ISNULL([Transaction].dblValue, 0), 2)),
+				strStockUOM = umStock.strUnitMeasure,
+				dblQuantityInStockUOM = SUM(ISNULL(dbo.fnCalculateQtyBetweenUOM([Transaction].intItemUOMId, iuStock.intItemUOMId, [Transaction].dblQty), 0))
+			FROM tblICItem Item
+				LEFT JOIN tblICInventoryTransaction [Transaction] ON [Transaction].intItemId = Item.intItemId
+				LEFT JOIN tblICItemUOM iuStock ON iuStock.intItemId = Item.intItemId
+					AND iuStock.ysnStockUnit = 1
+				LEFT JOIN tblICUnitMeasure umStock
+					ON iuStock.intUnitMeasureId = umStock.intUnitMeasureId
+			GROUP BY Item.intItemId, 
+				[Transaction].intItemLocationId, 
+				[Transaction].intSubLocationId, umStock.strUnitMeasure
+		) [Transaction]
+		LEFT JOIN tblICItem Item ON Item.intItemId = [Transaction].intItemId
+		LEFT JOIN tblICItemLocation ItemLocation ON ItemLocation.intItemLocationId = [Transaction].intItemLocationId
+		LEFT JOIN tblICItemPricing ItemPricing ON ItemPricing.intItemLocationId = ItemLocation.intItemLocationId
+		LEFT JOIN tblSMCompanyLocation Location ON Location.intCompanyLocationId = ItemLocation.intLocationId
+		LEFT JOIN tblSMCompanyLocationSubLocation SubLocation ON SubLocation.intCompanyLocationSubLocationId = [Transaction].intSubLocationId
 WHERE	Item.strType != 'Comment'
