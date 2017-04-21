a35aec39613459e4b381ff5876e9237758d8b452
kenneth lagunero <kenneth.lagunero@irely.com>
2017-04-21 14:43:55 +0800

    TM-2338

---
+++ Integration/Scripts/TM/CheckOriginTableAndTMPreference.sql
@@ -0,0 +1,13 @@
+﻿GO
+PRINT N'BEGIN Check Origin coctl config and tm preference'
+GO
+
+IF((SELECT TOP 1 coctl_ag FROM coctlmst) <> 'Y' AND (SELECT TOP 1 coctl_pt FROM coctlmst) <> 'Y')
+BEGIN
+	UPDATE tblTMPreferenceCompany
+	SET ysnUseOriginIntegration = 0
+END
+
+GO
+PRINT N'END Check Origin coctl config and tm preference'
+GO
