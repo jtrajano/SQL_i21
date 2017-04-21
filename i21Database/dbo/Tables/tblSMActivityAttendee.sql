8750bae1d53710399f90b7938500d45e1de059f0
jesmar mendones <jesmar.mendones@irely.com>
2017-04-21 15:51:45 +0800

    FRM-4406

--- i21Database/dbo/Tables/tblSMActivityAttendee.sql
+++ i21Database/dbo/Tables/tblSMActivityAttendee.sql
@@ -1,8 +1,9 @@
 ﻿CREATE TABLE [dbo].[tblSMActivityAttendee]
 (
-	[intActivityAttendeeId] INT NOT NULL PRIMARY KEY IDENTITY,
-	[intActivityId] [int] NOT NULL,
-	[intEntityId] [int] NOT NULL,
-	[intConcurrencyId] [int] NOT NULL DEFAULT ((1)), 
+	[intActivityAttendeeId]		INT		NOT NULL PRIMARY KEY IDENTITY,
+	[intActivityId]				[int]	NOT NULL,
+	[intEntityId]				[int]	NOT NULL,
+	[ysnAddCalendarEvent]		[bit]	NULL,
+	[intConcurrencyId]			[int]	NOT NULL DEFAULT ((1)), 
     CONSTRAINT [FK_tblSMActivityAttendee_tblSMActivity] FOREIGN KEY ([intActivityId]) REFERENCES [tblSMActivity]([intActivityId]) ON DELETE CASCADE
 )
