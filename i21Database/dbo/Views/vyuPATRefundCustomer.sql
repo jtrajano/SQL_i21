87b6389d05737dbcd1d1464207ae9027bffb4eaf
Jb Mago <benedict.mago@irely.com>
2017-04-24 22:18:30 +0800

    PAT-620

--- i21Database/dbo/Views/vyuPATRefundCustomer.sql
+++ i21Database/dbo/Views/vyuPATRefundCustomer.sql
@@ -18,9 +18,9 @@ SELECT	RC.intRefundCustomerId,
         RC.dblCashRefund,
         RC.dblEquityRefund,
 		ysnVouchered = CASE WHEN RC.intBillId IS NOT NULL THEN CAST(1 AS BIT) ELSE CAST(0 AS BIT) END,
-		dblLessFWT = CASE WHEN APV.ysnWithholding = 0 THEN 0 ELSE RC.dblCashRefund * (R.dblFedWithholdingPercentage/100) END,
-		dblLessServiceFee = CASE WHEN RC.ysnEligibleRefund = 1 THEN R.dblServiceFee ELSE 0 END,
-		dblCheckAmount = CASE WHEN (RC.dblCashRefund - (CASE WHEN APV.ysnWithholding = 0 THEN 0 ELSE RC.dblCashRefund * (R.dblFedWithholdingPercentage/100) END) - (R.dblServiceFee) < 0) THEN 0 ELSE RC.dblCashRefund - (CASE WHEN APV.ysnWithholding = 0 THEN 0 ELSE RC.dblCashRefund * (R.dblFedWithholdingPercentage/100) END) - (R.dblServiceFee) END,
+		dblLessFWT = CASE WHEN APV.ysnWithholding = 0 AND RC.dblCashRefund = 0 THEN 0 ELSE RC.dblCashRefund * (R.dblFedWithholdingPercentage/100) END,
+		dblLessServiceFee = CASE WHEN RC.ysnEligibleRefund = 1 AND RC.dblCashRefund > 0 THEN R.dblServiceFee ELSE 0 END,
+		dblCheckAmount = CASE WHEN (RC.dblCashRefund - (CASE WHEN APV.ysnWithholding = 0 THEN 0 ELSE RC.dblCashRefund * (R.dblFedWithholdingPercentage/100) END) - (R.dblServiceFee) < 0) AND RC.dblCashRefund = 0 THEN 0 ELSE RC.dblCashRefund - (CASE WHEN APV.ysnWithholding = 0 THEN 0 ELSE RC.dblCashRefund * (R.dblFedWithholdingPercentage/100) END) - (R.dblServiceFee) END,
 		RC.intConcurrencyId
 	FROM tblPATRefundCustomer RC
 	INNER JOIN tblPATRefund R
