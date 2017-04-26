dde0b3aaed89d9c42dee031dee8556be320ca0cf
Lex Eugene Peregrino <lex.peregrino@irely.com>
2017-04-26 16:19:39 +0800

    AP-3546 - Other Charge with 2 or more tax details was duplicated in Add Payables 1.0

--- i21Database/dbo/Views/vyuAPReceivedItems.sql
+++ i21Database/dbo/Views/vyuAPReceivedItems.sql
@@ -518,13 +518,16 @@ FROM
 	LEFT JOIN tblSMCurrencyExchangeRate F ON  (F.intFromCurrencyId = (SELECT intDefaultCurrencyId FROM dbo.tblSMCompanyPreference) AND F.intToCurrencyId = CASE WHEN A.ysnSubCurrency > 0 
 																																						   THEN (SELECT ISNULL(intMainCurrencyId,0) FROM dbo.tblSMCurrency WHERE intCurrencyID = ISNULL(A.intCurrencyId,0))
 																																						   ELSE  ISNULL(A.intCurrencyId,0) END) 
-	LEFT JOIN dbo.tblSMCurrencyExchangeRateDetail G1 ON F.intCurrencyExchangeRateId = G1.intCurrencyExchangeRateId and G1.dtmValidFromDate = (SELECT CONVERT(char(10), GETDATE(),126)) 
 	LEFT JOIN dbo.tblSMCurrency H1 ON H1.intCurrencyID = A.intCurrencyId
 	LEFT JOIN dbo.tblSMCurrency SubCurrency ON SubCurrency.intMainCurrencyId = A.intCurrencyId 
 	INNER JOIN  (tblAPVendor D1 INNER JOIN tblEMEntity D2 ON D1.intEntityVendorId = D2.intEntityId) ON A.[intEntityVendorId] = D1.intEntityVendorId
 	LEFT JOIN dbo.tblSMCurrencyExchangeRateType RT ON RT.intCurrencyExchangeRateTypeId = A.intForexRateTypeId
 	LEFT JOIN dbo.tblICInventoryReceipt IR ON IR.intInventoryReceiptId = A.intInventoryReceiptId
-	LEFT JOIN dbo.tblICInventoryReceiptChargeTax IRCT ON IRCT.intInventoryReceiptChargeId = A.intInventoryReceiptChargeId
+	OUTER APPLY
+	(
+		SELECT TOP 1 ysnCheckoffTax FROM tblICInventoryReceiptChargeTax IRCT
+		WHERE IRCT.intInventoryReceiptChargeId = A.intInventoryReceiptChargeId
+	)  IRCT
 	OUTER APPLY 
 	(
 		SELECT intEntityVendorId FROM tblAPBillDetail BD
