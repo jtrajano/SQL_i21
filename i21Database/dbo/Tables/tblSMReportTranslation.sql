9649ce489a72064473d46b5e4da8447ac883c69e
Von Carlo Pasion <von.pasion@irely.com>
2017-04-27 17:33:14 +0800

    FRM-4836

---
+++ i21Database/dbo/Tables/tblSMReportTranslation.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[tblSMReportTranslation]
+(
+	[intReportTranslationId]	INT	 PRIMARY KEY IDENTITY (1, 1) NOT NULL,
+    [strFieldName]				NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL, 
+	[strTranslation]			NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NULL, 
+	[intTransactionId]			INT  NOT NULL,
+	[intLanguageId]				INT  NOT NULL,
+    [intConcurrencyId]			INT	 NOT NULL DEFAULT (1),
+	CONSTRAINT [FK_tblSMReportTranslation_tblSMTransaction] FOREIGN KEY ([intTransactionId]) REFERENCES [tblSMTransaction]([intTransactionId]) ON DELETE CASCADE
+)
