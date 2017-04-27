905f5e8fa1e085c3f608b581bb8a699411fa8153
Jayson Ramos <jayson.ramos@iRely.com>
2017-04-27 09:56:06 +0800

    CRM-276

---
+++ i21Database/dbo/Functions/fnCRMCoalesceModule.sql
@@ -0,0 +1,20 @@
+﻿CREATE FUNCTION [dbo].[fnCRMCoalesceModule](@intEntityCustomerId int, @strCompanyId nvarchar(max))
+RETURNS nvarchar(max)
+AS
+BEGIN
+
+	declare @strModules nvarchar(max);
+	select
+		@strModules = COALESCE(@strModules + ', ', '') + b.strModuleName
+	from 
+		tblARCustomerLicenseInformation a
+		,tblARCustomerLicenseModule b
+	where
+		b.intCustomerLicenseInformationId = a.intCustomerLicenseInformationId
+		and a.intEntityCustomerId = @intEntityCustomerId
+		and a.strCompanyId = @strCompanyId
+		and b.ysnEnabled = convert(bit,1)
+
+	RETURN @strModules
+
+END
