05b1844d4ef5eac0e2b29d7d4bea5857974b9c82
Ruben Escarcha <ruben.escarcha@irely.com>
2017-05-02 21:28:52 +0800

    AR-4573 Item Committed value does not update when line item or invoice is deleted

--- i21Database/dbo/Stored Procedures/uspARUpdateCommitted.sql
+++ i21Database/dbo/Stored Procedures/uspARUpdateCommitted.sql
@@ -33,53 +33,19 @@ BEGIN
 		,[intLotId]				
 		,[intSubLocationId]		
 		,[intStorageLocationId]		
-	)
-	SELECT
-		[intItemId]					=	ARID.[intItemId]
-		,[intItemLocationId]		=	ICGIS.[intItemLocationId]
-		,[intItemUOMId]				=	ARID.[intItemUOMId]
-		,[dtmDate]					=	ARI.[dtmDate]
-		,[dblQty]					=	ARID.[dblQtyShipped]										
-		,[dblUOMQty]				=	ICIUOM.[dblUnitQty]
-		,[dblCost]					=	ICGIS.[dblLastCost]
-		,[dblValue]					=	0
-		,[dblSalesPrice]			=	ARID.[dblPrice]
-		,[intCurrencyId]			=	ARI.[intCurrencyId]
-		,[dblExchangeRate]			=	0
-		,[intTransactionId]			=	ARI.[intInvoiceId]
-		,[intTransactionDetailId]	=	ARID.[intInvoiceDetailId] 
-		,[strTransactionId]			=	ARI.[strInvoiceNumber]
-		,[intTransactionTypeId]		=	7
-		,[intLotId]					=	NULL
-		,[intSubLocationId]			=	ARID.[intSubLocationId]
-		,[intStorageLocationId]		=	ARID.[intStorageLocationId]
-	FROM 
-		tblARInvoiceDetail ARID
-	INNER JOIN
-		tblARInvoice ARI
-			ON ARID.[intInvoiceId] = ARI.[intInvoiceId]	
-	INNER JOIN
-		tblICItemUOM ICIUOM 
-			ON ICIUOM.[intItemUOMId] = ARID.[intItemUOMId]	
-	LEFT OUTER JOIN
-		vyuICGetItemStock ICGIS
-			ON ARID.[intItemId] = ICGIS.[intItemId] 
-			AND ARI.[intCompanyLocationId] = ICGIS.[intLocationId] 
-	WHERE [dbo].[fnIsStockTrackingItem](ARID.[intItemId]) = 1
-		AND ARI.[intInvoiceId] = @InvoiceId
-		AND ARI.[strTransactionType] IN ('Invoice', 'Cash')
-		AND ISNULL(ARID.[intInventoryShipmentItemId], 0) = 0 
-		AND ISNULL(ARID.[intSalesOrderDetailId], 0) = 0
-
-	UNION ALL
-
+	)	
 	--SO shipped
 	SELECT
 		[intItemId]					=	ARID.[intItemId]
 		,[intItemLocationId]		=	ICGIS.[intItemLocationId]
 		,[intItemUOMId]				=	SOTD.[intItemUOMId]
 		,[dtmDate]					=	ARI.[dtmDate]
-		,[dblQty]					=	ARID.[dblQtyShipped]	
+		,[dblQty]					=	(CASE
+											WHEN dbo.fnCalculateQtyBetweenUOM(ARID.intItemUOMId, SOTD.intItemUOMId, ARID.dblQtyShipped) > SOTD.dblQtyOrdered AND @Negate = 0 
+												THEN  SOTD.dblQtyOrdered
+											ELSE
+												dbo.fnCalculateQtyBetweenUOM(ARID.intItemUOMId, SOTD.intItemUOMId, ARID.dblQtyShipped)
+										END)
 		,[dblUOMQty]				=	ICIUOM.[dblUnitQty]
 		,[dblCost]					=	ICGIS.[dblLastCost]
 		,[dblValue]					=	0
@@ -90,16 +56,16 @@ BEGIN
 		,[intTransactionDetailId]	=	ARID.[intInvoiceDetailId] 
 		,[strTransactionId]			=	ARI.[strInvoiceNumber]
 		,[intTransactionTypeId]		=	7
-		,[intLotId]					=	NULL
+		,[intLotId]					=	ARID.[intLotId]
 		,[intSubLocationId]			=	SOTD.[intSubLocationId]
 		,[intStorageLocationId]		=	SOTD.[intStorageLocationId]
 	FROM 
-		(SELECT [intInvoiceId], [intInvoiceDetailId], [intItemId], [intSalesOrderDetailId], [dblPrice], [dblQtyShipped], [intInventoryShipmentItemId], [dblQtyOrdered] FROM tblARInvoiceDetail WITH(NOLOCK)) ARID
+		(SELECT [intInvoiceId], [intInvoiceDetailId], [intItemId], [intSalesOrderDetailId], [dblPrice], [intInventoryShipmentItemId], [dblQtyOrdered], [dblQtyShipped], [intLotId], [intItemUOMId] FROM tblARInvoiceDetail WITH(NOLOCK)) ARID
 	INNER JOIN
 		(SELECT [intInvoiceId], [strInvoiceNumber], [intCurrencyId], [dtmDate], [intCompanyLocationId], [strTransactionType] FROM tblARInvoice  WITH(NOLOCK)) ARI
 			ON ARID.[intInvoiceId] = ARI.[intInvoiceId]
 	INNER JOIN
-		(SELECT [intSalesOrderDetailId], [intItemId], [intSubLocationId], [intItemUOMId], [intStorageLocationId], [dblQtyOrdered] FROM tblSOSalesOrderDetail WITH(NOLOCK)) SOTD
+		(SELECT [intSalesOrderDetailId], [intItemId], [intSubLocationId], [intItemUOMId], [intStorageLocationId], [dblQtyOrdered], [dblQtyShipped] FROM tblSOSalesOrderDetail WITH(NOLOCK)) SOTD
 			ON ARID.[intSalesOrderDetailId] = SOTD.[intSalesOrderDetailId] 
 	INNER JOIN
 		(SELECT [intItemUOMId], [dblUnitQty] FROM tblICItemUOM WITH(NOLOCK)) ICIUOM 
@@ -116,13 +82,13 @@ BEGIN
 		AND @FromPosting = 1
 
 	UNION ALL
-	--SO shipped > ordered		--Component
+	--SO shipped --Component
 	SELECT
 		[intItemId]					=	ARIDC.[intComponentItemId]
 		,[intItemLocationId]		=	ICGIS.[intItemLocationId]
 		,[intItemUOMId]				=	ARIDC.[intItemUOMId]
 		,[dtmDate]					=	ARI.[dtmDate]
-		,[dblQty]					=	SOTD.[dblQtyOrdered] * ARIDC.[dblQuantity] 
+		,[dblQty]					=	dbo.fnCalculateQtyBetweenUOM(ARID.[intItemUOMId], SOTD.[intItemUOMId], ARID.[dblQtyShipped]) * ARIDC.[dblQuantity] 
 		,[dblUOMQty]				=	ARIDC.[dblUnitQuantity] 
 		,[dblCost]					=	ICGIS.[dblLastCost]
 		,[dblValue]					=	0
@@ -133,19 +99,19 @@ BEGIN
 		,[intTransactionDetailId]	=	ARID.[intInvoiceDetailId] 
 		,[strTransactionId]			=	ARI.[strInvoiceNumber]
 		,[intTransactionTypeId]		=	7
-		,[intLotId]					=	NULL
+		,[intLotId]					=	ARID.[intLotId]
 		,[intSubLocationId]			=	SOTD.[intSubLocationId]
 		,[intStorageLocationId]		=	SOTD.[intStorageLocationId]
 	FROM 
 		(SELECT [intComponentItemId], [intItemUOMId], [dblQuantity], [dblUnitQuantity], [intInvoiceDetailId] FROM tblARInvoiceDetailComponent WITH (NOLOCK)) ARIDC
 	INNER JOIN 
-		(SELECT [intInvoiceId], [intInvoiceDetailId], [intSalesOrderDetailId], [dblPrice], [intInventoryShipmentItemId] FROM tblARInvoiceDetail WITH (NOLOCK)) ARID
+		(SELECT [intInvoiceId], [intInvoiceDetailId], [intSalesOrderDetailId], [dblPrice], [intInventoryShipmentItemId], [dblQtyShipped], [intItemUOMId], [intLotId] FROM tblARInvoiceDetail WITH (NOLOCK)) ARID
 			ON ARIDC.[intInvoiceDetailId] = ARID.[intInvoiceDetailId] 
 	INNER JOIN
 		(SELECT [intInvoiceId], [dtmDate], [intCurrencyId], [strInvoiceNumber], [intCompanyLocationId], [strTransactionType] FROM tblARInvoice WITH (NOLOCK)) ARI
 			ON ARID.[intInvoiceId] = ARI.[intInvoiceId]
 	INNER JOIN
-		(SELECT [intSalesOrderDetailId], [intSubLocationId], [intStorageLocationId], [dblQtyOrdered] FROM tblSOSalesOrderDetail WITH (NOLOCK)) SOTD
+		(SELECT [intSalesOrderDetailId], [intSubLocationId], [intStorageLocationId], [dblQtyOrdered], [intItemUOMId] FROM tblSOSalesOrderDetail WITH (NOLOCK)) SOTD
 			ON ARID.[intSalesOrderDetailId] = SOTD.[intSalesOrderDetailId] 
 	LEFT OUTER JOIN
 		(SELECT [intItemId], [intLocationId], [intItemLocationId], [dblLastCost] FROM vyuICGetItemStock WITH (NOLOCK)) ICGIS
@@ -156,6 +122,7 @@ BEGIN
 		AND ARI.[strTransactionType] IN ('Invoice', 'Cash')
 		AND ISNULL(ARID.[intInventoryShipmentItemId], 0) = 0 
 		AND ISNULL(ARID.[intSalesOrderDetailId], 0) <> 0
+		AND @FromPosting = 1
 				
 		
 	UPDATE
