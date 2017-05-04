ecf765225986fab70d9a438472d3c0397902bbaa
Satya <satya.sahoo@irely.com>
2017-05-04 21:42:20 +0530

    CT-1294

---
+++ i21Database/dbo/Views/vyuCTSequenceCombo.sql
@@ -0,0 +1,13 @@
+﻿CREATE VIEW [dbo].[vyuCTSequenceCombo]
+
+AS
+
+	SELECT	CD.intContractDetailId,
+			CD.intContractSeq,
+			IM.strItemNo,
+			CH.strContractNumber + ' - ' +LTRIM(CD.intContractSeq)	AS	strSequenceNumber,	
+			CH.strContractNumber
+			
+	FROM	tblCTContractDetail		CD	
+	JOIN	tblCTContractHeader		CH	ON	CH.intContractHeaderId	=	CD.intContractHeaderId LEFT
+	JOIN	tblICItem				IM	ON	IM.intItemId			=	CD.intItemId
