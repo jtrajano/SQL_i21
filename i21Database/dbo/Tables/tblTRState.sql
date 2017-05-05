9015db6d36a4a43a3660eb691f0bda9faa6cbbe9
Jayson Ramos <jayson.ramos@iRely.com>
2017-05-05 10:47:23 +0800

    TR-726

--- i21Database/dbo/Tables/tblTRState.sql
+++ i21Database/dbo/Tables/tblTRState.sql
@@ -4,5 +4,7 @@
 	[strStateName] nvarchar(50) COLLATE Latin1_General_CI_AS NOT NULL,
 	[strStateAbbreviation] nvarchar(2) COLLATE Latin1_General_CI_AS NOT NULL,
 	[intConcurrencyId] [int] NOT NULL default 1,
-	CONSTRAINT [PK_tblTRState] PRIMARY KEY ([intStateId])
+	CONSTRAINT [PK_tblTRState] PRIMARY KEY ([intStateId]),
+	CONSTRAINT [UQ_tblTRState_strStateName] UNIQUE ([strStateName]),
+	CONSTRAINT [UQ_tblTRState_strStateAbbreviation] UNIQUE ([strStateAbbreviation])
 )
