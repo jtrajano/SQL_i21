825f86f141edfdfe0dd118fa0f334f99b78542f0
Jayson Ramos <jayson.ramos@iRely.com>
2017-05-08 10:13:58 +0800

    CRM-441

--- i21Database/dbo/Views/vyuCRMOpportunityCustomerContact.sql
+++ i21Database/dbo/Views/vyuCRMOpportunityCustomerContact.sql
@@ -1,6 +1,50 @@
 ﻿CREATE VIEW [dbo].[vyuCRMOpportunityCustomerContact]
 	AS
 		select
+			intEntityCustomerId = tblARCustomer.intEntityId
+			,tblARCustomer.strCustomerNumber
+			,tblEMEntity.intEntityId
+			,tblEMEntity.strName
+			,tblEMEntity.strTitle
+			,tblEMEntity.strEmail
+			,intEntityLocationId = (select top 1 [tblEMEntityLocation].intEntityLocationId from [tblEMEntityLocation] where [tblEMEntityLocation].intEntityLocationId = [tblEMEntityToContact].intEntityLocationId)
+			,strLocationName = (select top 1 [tblEMEntityLocation].strLocationName from [tblEMEntityLocation] where [tblEMEntityLocation].intEntityLocationId = [tblEMEntityToContact].intEntityLocationId)
+			,tblEMEntity.ysnActive
+			,strEntityType = (select top 1 et.strType from [tblEMEntityType] et where et.intEntityId = tblARCustomer.intEntityId and et.strType in ('Customer','Prospect'))
+			,tblEMEntityPhoneNumber.strPhone
+		from tblARCustomer
+			,[tblEMEntityToContact]
+			,tblEMEntity
+			,tblEMEntityPhoneNumber
+		where
+			[tblEMEntityToContact].intEntityId = tblARCustomer.intEntityId
+			and tblEMEntity.intEntityId = [tblEMEntityToContact].intEntityContactId
+			and tblEMEntityPhoneNumber.intEntityId = tblEMEntity.intEntityId
+
+		union all
+
+		select
+			intEntityCustomerId = tblAPVendor.intEntityId
+			,strCustomerNumber = tblAPVendor.strVendorId
+			,tblEMEntity.intEntityId
+			,tblEMEntity.strName
+			,tblEMEntity.strTitle
+			,tblEMEntity.strEmail
+			,intEntityLocationId = (select top 1 [tblEMEntityLocation].intEntityLocationId from [tblEMEntityLocation] where [tblEMEntityLocation].intEntityLocationId = [tblEMEntityToContact].intEntityLocationId)
+			,strLocationName = (select top 1 [tblEMEntityLocation].strLocationName from [tblEMEntityLocation] where [tblEMEntityLocation].intEntityLocationId = [tblEMEntityToContact].intEntityLocationId)
+			,tblEMEntity.ysnActive
+			,strEntityType = (select top 1 et.strType from [tblEMEntityType] et where et.intEntityId = tblAPVendor.intEntityId and et.strType in ('Vendor'))
+			,tblEMEntityPhoneNumber.strPhone
+		from tblAPVendor
+			,[tblEMEntityToContact]
+			,tblEMEntity
+			,tblEMEntityPhoneNumber
+		where
+			[tblEMEntityToContact].intEntityId = tblAPVendor.intEntityId
+			and tblEMEntity.intEntityId = [tblEMEntityToContact].intEntityContactId
+			and tblEMEntityPhoneNumber.intEntityId = tblEMEntity.intEntityId
+		/*
+		select
 			tblARCustomer.[intEntityId]
 			,tblARCustomer.strCustomerNumber			
 			,tblEMEntity.strName
@@ -41,3 +85,4 @@
 			[tblEMEntityToContact].intEntityId = tblAPVendor.[intEntityId]
 			and tblEMEntity.intEntityId = [tblEMEntityToContact].intEntityContactId
 			and tblEMEntityPhoneNumber.intEntityId = tblEMEntity.intEntityId
+		*/
