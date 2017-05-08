f995fd7b4089c5bba8d5cecb0c3737fbdf3030bb
Wendell Wayne Estrada <wendell.estrada@irely.com>
2017-05-08 12:40:14 +0800

    IC-3758 Error while importing Balances for Inventory

--- i21Database/dbo/Stored Procedures/uspICImportDataFromOrigin.sql
+++ i21Database/dbo/Stored Procedures/uspICImportDataFromOrigin.sql
@@ -1,6 +1,7 @@
 ﻿CREATE PROCEDURE [dbo].[uspICImportDataFromOrigin]
 	@strLineOfBusiness VARCHAR(50),
-	@strType VARCHAR(50)
+	@strType VARCHAR(50),
+	@intEntityUserSecurityId INT = 0
 AS
 
 IF @strLineOfBusiness IS NULL OR LTRIM(RTRIM(@strLineOfBusiness)) = ''
@@ -24,7 +25,7 @@ BEGIN
 	ELSE IF @strType = 'AdditionalGLAccts'	EXEC dbo.uspICDCCatExtraGLAccounts
 	ELSE IF @strType = 'Items'				EXEC dbo.uspICDCItemMigrationPt
 	ELSE IF @strType = 'ItemGLAccts'		EXEC dbo.uspICDCItmGLAcctsMigrationPt
-	ELSE IF @strType = 'Balance'			EXEC dbo.uspICDCBeginInventoryPt
+	ELSE IF @strType = 'Balance'			EXEC dbo.uspICDCBeginInventoryPt NULL, NULL, @intEntityUserSecurityId
 END
 ELSE IF @strLineOfBusiness = 'Ag'
 BEGIN
@@ -35,7 +36,7 @@ BEGIN
 	ELSE IF @strType = 'AdditionalGLAccts'	EXEC dbo.uspICDCCatExtraGLAccounts
 	ELSE IF @strType = 'Items'				EXEC dbo.uspICDCItemMigrationAg
 	ELSE IF @strType = 'ItemGLAccts'		EXEC dbo.uspICDCItmGLAcctsMigrationAg
-	ELSE IF @strType = 'Balance'			EXEC dbo.uspICDCBeginInventoryAg
+	ELSE IF @strType = 'Balance'			EXEC dbo.uspICDCBeginInventoryAg NULL, NULL, @intEntityUserSecurityId
 END
 ELSE IF @strLineOfBusiness = 'Grain'
 BEGIN
