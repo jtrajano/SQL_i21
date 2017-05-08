926a93e2ff6177164225289abc2360cfaeb6580e
Feb Montefrio <feb.montefrio@irely.com>
2017-05-08 19:52:01 +0800

    IC-3781

---
+++ i21Database/dbo/Stored Procedures/uspICPopulateShipmentInvoice.sql
@@ -0,0 +1,137 @@
+﻿CREATE PROCEDURE [dbo].[uspICPopulateShipmentInvoice]
+AS
+
+SET QUOTED_IDENTIFIER OFF
+SET ANSI_NULLS ON
+SET NOCOUNT ON
+SET XACT_ABORT ON
+SET ANSI_WARNINGS OFF
+
+DECLARE @dtmCreated AS DATETIME = GETDATE()
+
+-- Clean the contents of temp table. 
+BEGIN 
+	TRUNCATE TABLE [tblICSearchShipmentInvoice]
+END 
+
+-- Insert fresh data to the temp table. 
+BEGIN 
+	INSERT INTO [tblICSearchShipmentInvoice] (
+		intInventoryShipmentId
+		,intInventoryShipmentItemId
+		,intInventoryShipmentChargeId
+		,strShipmentNumber
+		,dtmShipDate
+		,strCustomer
+		,strLocationName
+		,strDestination
+		,strBOLNumber
+		,strOrderType
+		,strItemNo
+		,strItemDescription
+		,dblUnitCost
+		,dblShipmentQty
+		,dblInTransitQty
+		,dblInvoiceQty
+		,dblShipmentLineTotal
+		,dblInTransitTotal
+		,dblInvoiceLineTotal
+		,dblShipmentTax
+		,dblInvoiceTax
+		,dblOpenQty
+		,dblItemsReceivable
+		,dblTaxesReceivable
+		,dtmLastInvoiceDate
+		,strAllVouchers
+		,strFilterString
+		,dtmCreated
+		,intCurrencyId
+		,strCurrency
+		,strItemUOM
+		,intItemUOMId			
+	)
+	-- Insert the items: 
+	SELECT	
+			shipmentItem.intInventoryShipmentId
+			,shipmentItem.intInventoryShipmentItemId
+			,intInventoryShipmentChargeId = NULL 
+			,shipmentItem.strShipmentNumber
+			,shipmentItem.dtmShipDate
+			,strCustomer = customer.strCustomerNumber + ' ' + entity.strName
+			,shipmentItem.strLocationName
+			,shipmentItem.strDestination
+			,shipmentItem.strBOLNumber
+			,shipmentItem.strOrderType
+			,shipmentItem.strItemNo
+			,shipmentItem.strItemDescription
+			,dblUnitCost = shipmentItem.dblShipmentCost
+			,shipmentItem.dblShipmentQty
+			,shipmentItem.dblInTransitQty
+			,shipmentItem.dblInvoiceQty
+			,dblShipmentLineTotal = shipmentItem.dblShipmentItemTotal
+			,dblInTransitTotal = shipmentItem.dblItemsReceivable
+			,dblInvoiceLineTotal = shipmentItem.dblInvoiceItemTotal
+			,dblShipmentTax = 0 
+			,dblInvoiceTax = 0 
+			,dblOpenQty = shipmentItem.dblInTransitQty
+			,dblItemsReceivable = shipmentItem.dblItemsReceivable
+			,dblTaxesReceivable = 0 
+			,shipmentItem.dtmLastInvoiceDate
+			,shipmentItem.strAllVouchers
+			,shipmentItem.strFilterString
+			,dtmCreated = @dtmCreated
+			,shipmentItem.intCurrencyId
+			,shipmentItem.strCurrency
+			,shipmentItem.strItemUOM
+			,shipmentItem.intItemUOMId	
+	FROM	tblARCustomer customer INNER JOIN tblEMEntity entity
+				ON entity.intEntityId = customer.intEntityId
+			CROSS APPLY (
+				SELECT	* 
+				FROM	vyuICGetInventoryShipmentInvoiceItems items
+				WHERE	items.intEntityCustomerId = customer.intEntityId
+			) shipmentItem
+
+	-- Insert the price down charges (against the receipt vendor)
+	UNION ALL 
+	SELECT	
+			shipmentCharge.intInventoryShipmentId
+			,shipmentCharge.intInventoryShipmentChargeId
+			,intInventoryShipmentChargeId = NULL 
+			,shipmentCharge.strShipmentNumber
+			,shipmentCharge.dtmShipDate
+			,strCustomer = customer.strCustomerNumber + ' ' + entity.strName
+			,shipmentCharge.strLocationName
+			,shipmentCharge.strDestination
+			,shipmentCharge.strBOLNumber
+			,shipmentCharge.strOrderType
+			,shipmentCharge.strItemNo
+			,shipmentCharge.strItemDescription
+			,dblUnitCost = shipmentCharge.dblShipmentCost
+			,shipmentCharge.dblShipmentQty
+			,shipmentCharge.dblInTransitQty
+			,shipmentCharge.dblInvoiceQty
+			,dblShipmentLineTotal = shipmentCharge.dblShipmentChargeTotal
+			,dblInTransitTotal = 0
+			,dblInvoiceLineTotal = shipmentCharge.dblInvoiceItemTotal
+			,dblShipmentTax = 0 
+			,dblInvoiceTax = 0 
+			,dblOpenQty = 0
+			,dblItemsReceivable = shipmentCharge.dblItemsReceivable
+			,dblTaxesReceivable = 0 
+			,shipmentCharge.dtmLastInvoiceDate
+			,shipmentCharge.strAllVouchers
+			,shipmentCharge.strFilterString
+			,dtmCreated = @dtmCreated
+			,shipmentCharge.intCurrencyId
+			,shipmentCharge.strCurrency
+			,strItemUOM = NULL 
+			,intItemUOMId = NULL 
+	FROM	tblARCustomer customer INNER JOIN tblEMEntity entity
+				ON entity.intEntityId = customer.intEntityId
+			CROSS APPLY (
+				SELECT	* 
+				FROM	vyuICGetInventoryShipmentInvoicePriceCharges items
+				WHERE	items.intEntityCustomerId = customer.intEntityId
+			) shipmentCharge
+END 
---
+++ i21Database/dbo/Views/vyuICGetInventoryShipmentInvoiceItems.sql
@@ -0,0 +1,148 @@
+﻿CREATE VIEW [dbo].[vyuICGetInventoryShipmentInvoiceItems]
+AS 
+
+SELECT	Shipment.intInventoryShipmentId
+		,ShipmentItem.intInventoryShipmentItemId
+		,Shipment.strShipmentNumber
+		,Shipment.dtmShipDate
+		,Shipment.intEntityCustomerId
+		,strLocationName = fromLocation.strLocationName
+		,strDestination = toLocation.strLocationName
+		,strOrderType = ot.strOrderType
+		,Shipment.strBOLNumber
+		,i.strItemNo
+		,strItemDescription = i.strDescription
+		,Shipment.intCurrencyId
+		,strCurrency = currency.strCurrency 
+		,intItemUOMId = ShipmentItem.intItemUOMId
+		,strItemUOM = ItemUOMName.strUnitMeasure
+		,dblShipmentCost = ShipmentAndInvoicedItems.ShipmentCost
+		,dblShipmentQty = ShipmentAndInvoicedItems.QtyShipped
+		,dblInvoiceQty = ShipmentAndInvoicedItems.QtyInvoiced
+		,dblInTransitQty = ShipmentAndInvoicedItems.OpenQty
+		,dblShipmentItemTotal = ShipmentAndInvoicedItems.ShipmentItemTotal
+		,dblInvoiceItemTotal = ShipmentAndInvoicedItems.InvoiceItemTotal
+		,dblItemsReceivable = ShipmentAndInvoicedItems.dblItemsReceivable
+		,dtmLastInvoiceDate = topInvoice.dtmDate
+		,strAllVouchers = CAST( ISNULL(allLinkedInvoiceId.strInvoiceIds, 'New Invoice') AS NVARCHAR(MAX)) 
+		,strFilterString = CAST(filterString.strFilterString AS NVARCHAR(MAX)) 
+FROM	tblICInventoryShipment Shipment 
+		INNER JOIN tblICInventoryShipmentItem ShipmentItem
+			ON Shipment.intInventoryShipmentId = ShipmentItem.intInventoryShipmentId
+		INNER JOIN tblICItem i 
+			ON i.intItemId = ShipmentItem.intItemId
+
+		INNER JOIN (
+			tblICItemUOM ItemUOM INNER JOIN tblICUnitMeasure ItemUOMName
+				ON ItemUOM.intUnitMeasureId = ItemUOMName.intUnitMeasureId
+		)
+			ON ItemUOM.intItemUOMId = ShipmentItem.intItemUOMId
+
+		LEFT JOIN tblSMCompanyLocation fromLocation
+			ON fromLocation.intCompanyLocationId = Shipment.intShipFromLocationId
+
+		LEFT JOIN tblEMEntityLocation toLocation
+			ON toLocation.intEntityLocationId = Shipment.intShipToLocationId
+
+		OUTER APPLY (
+			SELECT	QtyShipped = d.dblQuantity
+					,QtyInvoiced = ISNULL(InvoicedItem.dblQuantity, 0) 
+					,ShipmentCost = ShipmentBasedOnInventoryTransaction.dblShipmentCost
+					,ShipmentItemTotal = d.dblQuantity * ShipmentBasedOnInventoryTransaction.dblShipmentCost
+					,InvoiceItemTotal = ISNULL(InvoicedItem.dblQuantity, 0) * ShipmentBasedOnInventoryTransaction.dblShipmentCost
+					,OpenQty = d.dblQuantity - ISNULL(InvoicedItem.dblQuantity, 0) 
+					,d.intInventoryShipmentItemId
+					,dblItemsReceivable = (d.dblQuantity - ISNULL(InvoicedItem.dblQuantity, 0)) * ShipmentBasedOnInventoryTransaction.dblShipmentCost
+			FROM	tblICInventoryShipmentItem d 
+					CROSS APPLY (
+						SELECT	dblShipmentQty = SUM(ISNULL(-t.dblQty, 0)) 
+								,dblShipmentValue = SUM(ISNULL(-t.dblQty, 0) * ISNULL(t.dblCost, 0)) 
+								,dblShipmentCost = CASE WHEN SUM(ISNULL(-t.dblQty, 0)) <> 0 THEN SUM(ISNULL(-t.dblQty, 0) * ISNULL(t.dblCost, 0)) / SUM(ISNULL(-t.dblQty, 0)) ELSE 0.00 END 
+						FROM	tblICInventoryTransaction t INNER JOIN tblICItemLocation il
+									ON t.intItemLocationId = il.intItemLocationId
+									AND il.intLocationId IS NOT NULL 
+						WHERE	t.strTransactionId = Shipment.strShipmentNumber
+								AND t.intTransactionId = Shipment.intInventoryShipmentId
+								AND t.intTransactionDetailId = d.intInventoryShipmentItemId
+								AND t.dblQty <> 0 
+								AND ISNULL(t.ysnIsUnposted, 0) = 0			
+					) ShipmentBasedOnInventoryTransaction
+					OUTER APPLY (
+						SELECT	dblQuantity = SUM(ISNULL(id.dblQtyShipped, 0))
+						FROM	tblARInvoice i INNER JOIN tblARInvoiceDetail id
+									ON i.intInvoiceId = id.intInvoiceId
+						WHERE	i.ysnPosted = 1
+								AND id.intInventoryShipmentItemId = d.intInventoryShipmentItemId
+					) InvoicedItem 
+			WHERE	d.intInventoryShipmentItemId = ShipmentItem.intInventoryShipmentItemId
+
+		) ShipmentAndInvoicedItems
+		OUTER APPLY (
+			SELECT	TOP 1 
+					h.strInvoiceNumber  
+					,h.intInvoiceId
+					,h.dtmDate
+			FROM	tblARInvoice h INNER JOIN tblARInvoiceDetail d
+						ON h.intInvoiceId = d.intInvoiceId
+			WHERE	d.intInventoryShipmentItemId = ShipmentItem.intInventoryShipmentItemId
+					AND d.intInventoryShipmentChargeId IS NULL 
+					AND h.ysnPosted = 1
+			ORDER BY h.intInvoiceId DESC 
+		) topInvoice
+		OUTER APPLY (
+			SELECT strFilterString = 
+				LTRIM(
+					STUFF(
+							' ' + (
+								SELECT  CONVERT(NVARCHAR(50), h.intInvoiceId) + '|^|'
+								FROM	tblARInvoice h INNER JOIN tblARInvoiceDetail d
+											ON h.intInvoiceId = d.intInvoiceId
+								WHERE	d.intInventoryShipmentItemId = ShipmentItem.intInventoryShipmentItemId
+										AND d.intInventoryShipmentChargeId IS NULL 
+										AND h.ysnPosted = 1
+								GROUP BY h.intInvoiceId
+								FOR xml path('')
+							)
+						, 1
+						, 1
+						, ''
+					)
+				)
+		) filterString 
+		OUTER APPLY (
+			SELECT strInvoiceIds = 
+				LTRIM(
+					STUFF(
+							(
+								SELECT  ', ' + h.strInvoiceNumber
+								FROM	tblARInvoice h INNER JOIN tblARInvoiceDetail d
+											ON h.intInvoiceId = d.intInvoiceId
+								WHERE	d.intInventoryShipmentItemId = ShipmentItem.intInventoryShipmentItemId
+										AND d.intInventoryShipmentChargeId IS NULL 
+										AND h.ysnPosted = 1
+								GROUP BY h.strInvoiceNumber
+								FOR xml path('')
+							)
+						, 1
+						, 1
+						, ''
+					)
+				)
+		) allLinkedInvoiceId  
+
+		LEFT JOIN tblSMCurrency currency
+			ON currency.intCurrencyID = Shipment.intCurrencyId
+
+		LEFT JOIN (
+			SELECT 1 intOrderTypeId, 'Sales Contract' strOrderType
+			UNION
+			SELECT 2 intOrderTypeId, 'Sales Order' strOrderType
+			UNION
+			SELECT 3 intOrderTypeId, 'Transfer Order' strOrderType
+			UNION
+			SELECT 4 intOrderTypeId, 'Direct' strOrderType
+		) AS ot 
+			ON ot.intOrderTypeId = Shipment.intOrderType
+
+
+WHERE	Shipment.ysnPosted = 1
