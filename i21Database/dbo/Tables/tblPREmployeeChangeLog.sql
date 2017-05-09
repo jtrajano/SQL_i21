9785ca0133eb44253334a7e0caa9dfa4a59a7c9b
Arman Arenas <arman.arenas@irely.com>
2017-05-09 15:10:43 +0800

    PR-846 added table tblPREmployeeChangeLog

---
+++ i21Database/dbo/Tables/tblPREmployeeChangeLog.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[tblPREmployeeChangeLog]
+(
+	[intEmployeeChangeLogId] INT NOT NULL PRIMARY KEY, 
+    [intEntityEmployeeId] INT NULL, 
+    [strEntityNo] NVARCHAR(100) NULL, 
+    [strName] NVARCHAR(100) NULL, 
+    [intEntityChangedId] INT NULL, 
+    [strChangedBy] NVARCHAR(100) NULL, 
+    [dtmChangedOn] DATETIME NULL, 
+    [strTableName] NVARCHAR(100) NULL, 
+    [strFieldName] NVARCHAR(100) NULL, 
+    [strKeyValue] NVARCHAR(100) NULL, 
+    [strFrom] NVARCHAR(MAX) NULL, 
+    [strTo] NVARCHAR(MAX) NULL, 
+    [intConcurrencyId] INT NULL DEFAULT ((1))
+)
