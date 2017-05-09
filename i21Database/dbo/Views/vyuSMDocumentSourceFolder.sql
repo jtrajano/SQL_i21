2a14cf7555c4cadc8404359688eaa6e1902df2cb
Kevin Estrada <kevin.estrada@irely.com>
2017-05-09 18:50:09 +0800

    FRM-4903

--- i21Database/dbo/Views/vyuSMDocumentSourceFolder.sql
+++ i21Database/dbo/Views/vyuSMDocumentSourceFolder.sql
@@ -9,5 +9,6 @@ SELECT
 	intDocumentTypeId,
 	intConcurrencyId,
 	intDocumentFolderParentId,
+	strDocumentType = (SELECT TOP 1 strName FROM tblSMDocumentType D WHERE D.intDocumentTypeId = A.intDocumentTypeId),
 	leaf = CAST(CASE WHEN (SELECT COUNT(*) FROM tblSMDocumentSourceFolder B WHERE B.intDocumentFolderParentId = A.intDocumentSourceFolderId) = 0 THEN 1 ELSE 0 END AS BIT) 
 from tblSMDocumentSourceFolder A
