b252e23c9f7e9269392a98a8039243d92b20e91a
Vinoth Kumar T <vinoth.t@irely.com>
2017-05-09 20:27:53 +0530

    QC-265

---
+++ i21Database/dbo/Views/vyuQMProductPropertyNotMapped.sql
@@ -0,0 +1,21 @@
+﻿CREATE VIEW vyuQMProductPropertyNotMapped
+AS
+SELECT PP.intProductPropertyId
+	,T.strTestName
+	,P.strPropertyName
+	,P.strDescription
+	,P.intAnalysisTypeId
+	,P.intDataTypeId
+	,P.intListId
+	,P.intDecimalPlaces
+	,P.ysnActive
+	,P.ysnNotify
+	,A.strAnalysisTypeName
+	,D.strDataTypeName
+	,L.strListName
+FROM tblQMProductProperty PP
+JOIN tblQMTest T ON T.intTestId = PP.intTestId
+JOIN tblQMProperty P ON P.intPropertyId = PP.intPropertyId
+JOIN tblQMAnalysisType A ON A.intAnalysisTypeId = P.intAnalysisTypeId
+JOIN tblQMDataType D ON D.intDataTypeId = P.intDataTypeId
+LEFT JOIN tblQMList L ON L.intListId = P.intListId
---
+++ i21Database/dbo/Views/vyuQMPropertyNotMapped.sql
@@ -0,0 +1,10 @@
+﻿CREATE VIEW vyuQMPropertyNotMapped
+AS
+SELECT P.intPropertyId
+	,A.strAnalysisTypeName
+	,D.strDataTypeName
+	,L.strListName
+FROM tblQMProperty P
+JOIN tblQMAnalysisType A ON A.intAnalysisTypeId = P.intAnalysisTypeId
+JOIN tblQMDataType D ON D.intDataTypeId = P.intDataTypeId
+LEFT JOIN tblQMList L ON L.intListId = P.intListId
