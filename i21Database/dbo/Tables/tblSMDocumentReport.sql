3719cc45bb4f77d057c5982139b131b032c3d613
Von Carlo Pasion <von.pasion@irely.com>
2017-05-10 15:45:38 +0800

    FRM-4914

--- i21Database/dbo/Tables/tblSMDocumentReport.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[tblSMDocumentReport] (
-    [intDocumentReportId]	INT             IDENTITY (1, 1) NOT NULL,
-    [strReportName]			NVARCHAR (150)  COLLATE Latin1_General_CI_AS NOT NULL,
-    [intConcurrencyId]		INT				NOT NULL,
-    CONSTRAINT [PK_dbo.tblSMDocumentReport] PRIMARY KEY CLUSTERED ([intDocumentReportId] ASC)
-);
