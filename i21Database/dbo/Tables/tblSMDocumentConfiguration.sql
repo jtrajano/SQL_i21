8cc497b4e9c2cab2bb740c5980ef8166c4ecb723
Von Carlo Pasion <von.pasion@irely.com>
2017-05-10 15:19:53 +0800

    Merging

--- i21Database/dbo/Tables/tblSMDocumentConfiguration.sql
+++ i21Database/dbo/Tables/tblSMDocumentConfiguration.sql
@@ -2,8 +2,10 @@
 (
 	intDocumentConfigurationId INT IDENTITY (1, 1) NOT NULL PRIMARY KEY, 
     [intScreenId] INT UNIQUE NULL,
+	[intDocumentReportId] INT NULL,
 	[intDocumentSourceFolderId] INT NULL,
 	[intConcurrencyId] INT NOT NULL DEFAULT 0,
 	CONSTRAINT [FK_tblSMDocumentConfiguration_tblSMDocumentSourceFolder] FOREIGN KEY ([intDocumentSourceFolderId]) REFERENCES [dbo].[tblSMDocumentSourceFolder] ([intDocumentSourceFolderId]),
+	CONSTRAINT [FK_tblSMDocumentConfiguration_tblSMDocumentReport] FOREIGN KEY ([intDocumentReportId]) REFERENCES [dbo].[tblSMDocumentReport] ([intDocumentReportId]),
 	CONSTRAINT [FK_tblSMDocumentConfiguration_tblSMScreen] FOREIGN KEY ([intScreenId]) REFERENCES [dbo].[tblSMScreen] ([intScreenId])
 )
