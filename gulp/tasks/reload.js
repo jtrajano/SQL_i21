e8252e5d211ef133c434e3a7a37a1447eb967ded
Wendell Wayne Estrada <wendell.estrada@irely.com>
2017-05-11 17:52:14 +0800

    Add live reload

---
+++ gulp/tasks/reload.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var changed = require('gulp-changed');
+var browserSync = require('browser-sync').create();
+var config = require('../config');
+var browserify = require('gulp-browserify');
+
+gulp.task('js', function () {
+    return gulp.src('app/view/**/*.{js,css}')
+        .pipe(browserify())
+        .pipe(gulp.dest(config.path.dest));
+});
+
+gulp.task('js-watch', ['publish', 'js'], function (done) {
+    browserSync.reload();
+    done();
+});
+
+// use default task to launch Browsersync and watch JS files
+gulp.task('reload', ['publish', 'js'], function () {
+
+    // Serve files from the root of this project
+    // browserSync.init({
+    //     server: {
+    //         baseDir: "./"
+    //     }
+    // });
+    
+    browserSync.init({
+        proxy: "http://localhost/17.2/i21/debug.html"
+    });
+
+    gulp.watch('app/**/*.{js,css}', ['js-watch']);
+});
