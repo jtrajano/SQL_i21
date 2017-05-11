300e549e14ac7310460f70f412eadd7d85d4ff83
Jb Mago <benedict.mago@irely.com>
2017-05-11 19:43:35 +0800

    PAT-631

--- i21Database/dbo/Stored Procedures/uspPATDeleteVoucherForPaidEquity.sql
+++ i21Database/dbo/Stored Procedures/uspPATDeleteVoucherForPaidEquity.sql
@@ -1,5 +1,6 @@
 ﻿CREATE PROCEDURE [dbo].[uspPATDeleteVoucherForPaidEquity]
 	@equityPaymentIds NVARCHAR(MAX) = NULL,
+	@intUserId INT = NULL,
 	@error NVARCHAR(MAX) = NULL OUTPUT,
 	@success BIT = 0 OUTPUT
 AS
@@ -26,8 +27,7 @@ BEGIN TRANSACTION
 
 	INSERT INTO #tempValidateTable
 	SELECT * FROM fnPATValidateAssociatedTransaction(@equityPaymentIds, 3)
-
-	SELECT * FROM #tempValidateTable
+	
 	IF EXISTS(SELECT 1 FROM #tempValidateTable)
 	BEGIN
 		SET @error = 'Selected vouchers are already paid.';
@@ -36,6 +36,34 @@ BEGIN TRANSACTION
 	END
 
 	INSERT INTO #tempEquityPayment SELECT [intID] AS intTransactionId FROM [dbo].fnGetRowsFromDelimitedValues(@equityPaymentIds)
+
+	BEGIN TRY
+	
+		SET ANSI_WARNINGS ON;
+		DECLARE @voucherId AS NVARCHAR(MAX);
+		SELECT @voucherId = STUFF((SELECT ',' + CONVERT(NVARCHAR(MAX),intBillId)  FROM tblPATEquityPaySummary 
+		WHERE intEquityPaySummaryId IN (SELECT intId from #tempEquityPayment) FOR XML PATH(''), TYPE).value('.','NVARCHAR(MAX)'),1,1,'');
+		SET ANSI_WARNINGS OFF;
+
+		EXEC [dbo].[uspAPPostBill]
+			@batchId = NULL,
+			@billBatchId = NULL,
+			@transactionType = NULL,
+			@post = 0,
+			@recap = 0,
+			@isBatch = 0,
+			@param = @voucherId,
+			@userId = @intUserId,
+			@beginTransaction = NULL,
+			@endTransaction = NULL,
+			@success = @success OUTPUT;
+
+	END TRY
+	BEGIN CATCH
+		SET @error = ERROR_MESSAGE();
+		RAISERROR(@error, 16, 1);
+		GOTO Post_Rollback;
+	END CATCH
 
 	DELETE FROM tblAPBill WHERE intBillId IN (SELECT intBillId FROM tblPATEquityPaySummary WHERE intEquityPaySummaryId IN (SELECT intId from #tempEquityPayment));
 	UPDATE tblPATEquityPaySummary SET intBillId = NULL WHERE intEquityPaySummaryId IN (SELECT intId from #tempEquityPayment);
@@ -47,7 +75,6 @@ GOTO Post_Commit;
 Post_Commit:
 	COMMIT TRANSACTION
 	SET @success = 1
-	
 	GOTO Post_Exit
 
 Post_Rollback:
