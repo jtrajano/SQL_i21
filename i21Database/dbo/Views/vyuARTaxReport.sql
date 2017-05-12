60f89fceea5d80ffc892862b6b081695a4f78bc8
Kelvin Quinto <kelvin.quinto@irely.com>
2017-05-13 06:12:23 +0800

    AR-4613

--- i21Database/dbo/Views/vyuARTaxReport.sql
+++ i21Database/dbo/Views/vyuARTaxReport.sql
@@ -20,64 +20,36 @@ SELECT TC.intTaxCodeId
 	 , I.strInvoiceNumber
 	 , I.dtmDate
 	 , C.strCustomerNumber
-	 , E.strName
-	 , dblUnitPrice     = (SELECT TOP 1 ISNULL(dblPrice, 0) AS dblPrice FROM tblARInvoiceDetail IID 
-									LEFT JOIN tblARInvoiceDetailTax IIDT ON IID.intInvoiceDetailId = IIDT.intInvoiceDetailId AND IIDT.intTaxCodeId = TC.intTaxCodeId AND IID.dblTotalTax > 0
-							WHERE intInvoiceId = I.intInvoiceId)
-	 , strCompanyName	= (SELECT TOP 1 strCompanyName FROM tblSMCompanySetup)
-	 , strCompanyAddress = (SELECT TOP 1 dbo.[fnARFormatCustomerAddress](NULL, NULL, NULL, strAddress, strCity, strState, strZip, strCountry, NULL, 0) FROM tblSMCompanySetup)
-	 , intItemId        = (SELECT TOP 1 IID.intItemId FROM tblARInvoiceDetail IID 
-									LEFT JOIN tblARInvoiceDetailTax IIDT ON IID.intInvoiceDetailId = IIDT.intInvoiceDetailId AND IIDT.intTaxCodeId = TC.intTaxCodeId AND IID.dblTotalTax > 0			
-							WHERE intInvoiceId = I.intInvoiceId)
-	 , strItemNo        = (SELECT TOP 1 ICI.strItemNo FROM tblARInvoiceDetail IID 
-									LEFT JOIN tblARInvoiceDetailTax IIDT ON IID.intInvoiceDetailId = IIDT.intInvoiceDetailId AND IIDT.intTaxCodeId = TC.intTaxCodeId AND IID.dblTotalTax > 0
-									LEFT JOIN tblICItem ICI ON IID.intItemId = ICI.intItemId
-							WHERE intInvoiceId = I.intInvoiceId)
-	 , dblQtyShipped	= (SELECT TOP 1 dblQtyShipped FROM tblARInvoiceDetail IID 
-									LEFT JOIN tblARInvoiceDetailTax IIDT ON IID.intInvoiceDetailId = IIDT.intInvoiceDetailId AND IIDT.intTaxCodeId = TC.intTaxCodeId AND IID.dblTotalTax > 0
-							WHERE intInvoiceId = I.intInvoiceId)
-	 , intCategoryId    = (SELECT TOP 1 ICI.intCategoryId FROM tblARInvoiceDetail IID 
-									LEFT JOIN tblARInvoiceDetailTax IIDT ON IID.intInvoiceDetailId = IIDT.intInvoiceDetailId AND IIDT.intTaxCodeId = TC.intTaxCodeId AND IID.dblTotalTax > 0
-									LEFT JOIN tblICItem ICI ON IID.intItemId = ICI.intItemId
-							WHERE intInvoiceId = I.intInvoiceId)
-	 , strItemCategory  = (SELECT TOP 1 ICC.strCategoryCode FROM tblARInvoiceDetail IID 
-									LEFT JOIN tblARInvoiceDetailTax IIDT ON IID.intInvoiceDetailId = IIDT.intInvoiceDetailId AND IIDT.intTaxCodeId = TC.intTaxCodeId AND IID.dblTotalTax > 0
-									LEFT JOIN tblICItem ICI ON IID.intItemId = ICI.intItemId
-									LEFT JOIN tblICCategory ICC ON ICI.intCategoryId = ICC.intCategoryId
-							WHERE intInvoiceId = I.intInvoiceId)
-	 , dblTaxDifference = CASE WHEN I.strTransactionType NOT IN ('Invoice', 'Debit Memo')
+	 , C.strName	 
+	 , strCompanyName	= COMPANY.strCompanyName
+	 , strCompanyAddress = COMPANY.strCompanyAddress
+	 , intItemId        = ITEMDETAIL.intItemId
+	 , strItemNo		= ITEMDETAIL.strItemNo
+	 , dblUnitPrice     = ITEMDETAIL.dblPrice
+	 , dblQtyShipped	= ITEMDETAIL.dblQtyShipped
+	 , intCategoryId	= ITEMDETAIL.intCategoryId
+	 , strItemCategory	= ITEMDETAIL.strCategoryCode	 
+	 , dblTaxDifference = CASE WHEN I.strTransactionType NOT IN ('Invoice', 'Debit Memo', 'Cash')
 								THEN SUM(IDT.dblAdjustedTax - IDT.dblTax) * -1 
 								ELSE SUM(IDT.dblAdjustedTax - IDT.dblTax) 
 						  END
-	 , dblTaxAmount     = CASE WHEN I.strTransactionType NOT IN ('Invoice', 'Debit Memo')
+	 , dblTaxAmount     = CASE WHEN I.strTransactionType NOT IN ('Invoice', 'Debit Memo', 'Cash')
 								THEN SUM(IDT.dblAdjustedTax) * -1 
 								ELSE SUM(IDT.dblAdjustedTax)
 						  END
-	 , dblNonTaxable    = CASE WHEN I.strTransactionType NOT IN ('Invoice', 'Debit Memo')
-								THEN (SELECT ISNULL(SUM(dblTotal), 0) FROM tblARInvoiceDetail WHERE dblTotalTax = 0 AND intInvoiceId = I.intInvoiceId) * -1 
-								ELSE (SELECT ISNULL(SUM(dblTotal), 0) FROM tblARInvoiceDetail WHERE dblTotalTax = 0 AND intInvoiceId = I.intInvoiceId) 
+	 , dblNonTaxable    = CASE WHEN I.strTransactionType NOT IN ('Invoice', 'Debit Memo', 'Cash')
+								THEN NONTAXABLE.dblNonTaxable * -1 
+								ELSE NONTAXABLE.dblNonTaxable 
 						  END
-	 , dblTaxable       = CASE WHEN I.strTransactionType NOT IN ('Invoice', 'Debit Memo') 
-								THEN (SELECT ISNULL(SUM(Taxable), 0)
-										FROM 
-										(
-										SELECT ISNULL(SUM(dblTotal), 0) Taxable FROM tblARInvoiceDetail WHERE intInvoiceId IN (Select intInvoiceId FROM tblARInvoice WHERE dblTotalTax > 0 AND intInvoiceId = I.intInvoiceId)
-										UNION ALL
-										SELECT ISNULL(SUM(dblTotal), 0) Taxable FROM tblARInvoiceDetail WHERE intInvoiceId IN (Select intInvoiceId FROM tblARInvoice WHERE dblTotalTax < 0 AND intInvoiceId = I.intInvoiceId)
-										) ABC) * -1 
-								ELSE (SELECT ISNULL(SUM(Taxable), 0)
-										FROM 
-										(
-										SELECT ISNULL(SUM(dblTotal), 0) Taxable FROM tblARInvoiceDetail WHERE intInvoiceId IN (Select intInvoiceId FROM tblARInvoice WHERE dblTotalTax > 0 AND intInvoiceId = I.intInvoiceId)
-										UNION ALL
-										SELECT ISNULL(SUM(dblTotal), 0) Taxable FROM tblARInvoiceDetail WHERE intInvoiceId IN (Select intInvoiceId FROM tblARInvoice WHERE dblTotalTax < 0 AND intInvoiceId = I.intInvoiceId)
-										) ABC) 
+	 , dblTaxable       = CASE WHEN I.strTransactionType NOT IN ('Invoice', 'Debit Memo', 'Cash') 
+								THEN TAXABLE.dblTaxable * -1
+								ELSE TAXABLE.dblTaxable
 						  END
-	 , dblTotalSales    = CASE WHEN I.strTransactionType NOT IN ('Invoice', 'Debit Memo')
+	 , dblTotalSales    = CASE WHEN I.strTransactionType NOT IN ('Invoice', 'Debit Memo', 'Cash')
 								THEN I.dblInvoiceTotal * -1 
 								ELSE I.dblInvoiceTotal
 						  END
-	 , dblTaxCollected  = CASE WHEN I.strTransactionType NOT IN ('Invoice', 'Debit Memo')
+	 , dblTaxCollected  = CASE WHEN I.strTransactionType NOT IN ('Invoice', 'Debit Memo', 'Cash')
 								THEN ISNULL(I.dblTax, 0) * -1 
 								ELSE ISNULL(I.dblTax, 0)
 						  END
@@ -85,20 +57,68 @@ SELECT TC.intTaxCodeId
 	 ,strCurrency				= SMC.strCurrency
 	 ,strCurrencyDescription	= SMC.strDescription
 FROM tblSMTaxCode TC
-	LEFT OUTER JOIN (SELECT intTaxClassId, strTaxClass FROM tblSMTaxClass) CL ON TC.intTaxClassId = CL.intTaxClassId
-	LEFT OUTER JOIN (SELECT intAccountId, strAccountId FROM tblGLAccount) SA ON TC.intSalesTaxAccountId = SA.intAccountId 
-	LEFT OUTER JOIN (SELECT intAccountId, strAccountId FROM tblGLAccount) PA ON TC.intPurchaseTaxAccountId = PA.intAccountId
-	LEFT OUTER JOIN (SELECT intInvoiceDetailId, intTaxCodeId, strCalculationMethod, dblRate, dblAdjustedTax, dblTax FROM tblARInvoiceDetailTax) IDT ON TC.intTaxCodeId = IDT.intTaxCodeId 
-	INNER JOIN (SELECT intInvoiceId, intInvoiceDetailId FROM tblARInvoiceDetail) ID ON IDT.intInvoiceDetailId = ID.intInvoiceDetailId
-	INNER JOIN (SELECT intInvoiceId, strInvoiceNumber, dtmDate, intEntityCustomerId, ysnPosted, intCurrencyId, dblTax, strTransactionType, dblInvoiceTotal, ysnPaid FROM tblARInvoice) I ON ID.intInvoiceId = I.intInvoiceId AND I.ysnPosted = 1
-	INNER JOIN (SELECT [intEntityId], strCustomerNumber FROM tblARCustomer) C ON I.intEntityCustomerId = C.[intEntityId]
-	INNER JOIN tblEMEntity E ON C.[intEntityId] = E.intEntityId
-	LEFT OUTER JOIN 
-		(SELECT intCurrencyID, 
-				strCurrency, 
-				strDescription 
-		FROM 
-			tblSMCurrency) SMC ON I.intCurrencyId = SMC.intCurrencyID	
+	LEFT OUTER JOIN (SELECT intTaxClassId
+	                      , strTaxClass 
+					 FROM dbo.tblSMTaxClass WITH (NOLOCK)) CL ON TC.intTaxClassId = CL.intTaxClassId
+	LEFT OUTER JOIN (SELECT intAccountId
+						  , strAccountId 
+					 FROM dbo.tblGLAccount WITH (NOLOCK)) SA ON TC.intSalesTaxAccountId = SA.intAccountId 
+	LEFT OUTER JOIN (SELECT intAccountId
+						  , strAccountId 
+					 FROM dbo.tblGLAccount WITH (NOLOCK)) PA ON TC.intPurchaseTaxAccountId = PA.intAccountId
+	LEFT OUTER JOIN (SELECT intInvoiceDetailId
+						  , intTaxCodeId
+						  , strCalculationMethod
+						  , dblRate
+						  , dblAdjustedTax
+						  , dblTax 
+					 FROM dbo.tblARInvoiceDetailTax WITH (NOLOCK)) IDT ON TC.intTaxCodeId = IDT.intTaxCodeId 
+	INNER JOIN (SELECT intInvoiceId
+					 , intInvoiceDetailId 
+				FROM dbo.tblARInvoiceDetail WITH (NOLOCK)) ID ON IDT.intInvoiceDetailId = ID.intInvoiceDetailId
+	INNER JOIN (SELECT intInvoiceId
+					 , strInvoiceNumber
+					 , dtmDate
+					 , intEntityCustomerId
+					 , ysnPosted
+					 , intCurrencyId
+					 , dblTax
+					 , strTransactionType
+					 , dblInvoiceTotal
+					 , ysnPaid 
+				FROM dbo.tblARInvoice WITH (NOLOCK)) I ON ID.intInvoiceId = I.intInvoiceId AND I.ysnPosted = 1
+	INNER JOIN (SELECT intEntityId
+					 , strCustomerNumber
+					 , strName
+			    FROM dbo.vyuARCustomer WITH (NOLOCK)) C ON I.intEntityCustomerId = C.intEntityId	
+	LEFT OUTER JOIN (SELECT intCurrencyID
+						  , strCurrency
+						  , strDescription 
+					 FROM dbo.tblSMCurrency WITH (NOLOCK)) SMC ON I.intCurrencyId = SMC.intCurrencyID
+	OUTER APPLY (SELECT TOP 1 strCompanyName
+							, strCompanyAddress = dbo.[fnARFormatCustomerAddress] (NULL, NULL, NULL, strAddress, strCity, strState, strZip, strCountry, NULL, 0) 
+				 FROM dbo.tblSMCompanySetup WITH (NOLOCK)) COMPANY
+	OUTER APPLY (SELECT TOP 1 IID.intItemId
+							, IID.dblPrice
+						    , IID.dblQtyShipped
+						    , ICI.strItemNo
+							, ICI.intCategoryId
+							, ICC.strCategoryCode 
+				 FROM dbo.tblARInvoiceDetail IID 					
+					LEFT JOIN (SELECT intItemId
+									, intCategoryId
+									, strItemNo
+							   FROM dbo.tblICItem WITH (NOLOCK)) ICI ON IID.intItemId = ICI.intItemId
+					LEFT JOIN (SELECT intCategoryId
+									, strCategoryCode 
+							   FROM dbo.tblICCategory WITH (NOLOCK)) ICC ON ICI.intCategoryId = ICC.intCategoryId
+				 WHERE intInvoiceId = I.intInvoiceId) ITEMDETAIL
+	OUTER APPLY (SELECT dblNonTaxable = ISNULL(SUM(dblTotal), 0) 
+				 FROM dbo.tblARInvoiceDetail WITH (NOLOCK) 
+				 WHERE dblTotalTax = 0 AND intInvoiceId = I.intInvoiceId) NONTAXABLE
+	OUTER APPLY (SELECT dblTaxable = ISNULL(SUM(dblTotal), 0) 
+				 FROM dbo.tblARInvoiceDetail WITH (NOLOCK) 
+				 WHERE dblTotalTax <> 0 AND intInvoiceId = I.intInvoiceId) TAXABLE
 GROUP BY
 	 TC.intTaxCodeId
 	,TC.strTaxAgency
@@ -118,7 +138,7 @@ GROUP BY
 	,I.strInvoiceNumber
 	,I.dtmDate
 	,C.strCustomerNumber
-	,E.strName	
+	,C.strName
 	,I.dblInvoiceTotal
 	,I.ysnPaid
 	,I.dblTax
@@ -127,4 +147,14 @@ GROUP BY
 	,IDT.dblRate
 	,I.intCurrencyId
 	,SMC.strCurrency
-	,SMC.strDescription  
+	,SMC.strDescription
+	,COMPANY.strCompanyName
+	,COMPANY.strCompanyAddress
+	,ITEMDETAIL.intItemId
+	,ITEMDETAIL.dblPrice
+	,ITEMDETAIL.dblQtyShipped
+	,ITEMDETAIL.strItemNo
+	,ITEMDETAIL.intCategoryId
+	,ITEMDETAIL.strCategoryCode
+	,NONTAXABLE.dblNonTaxable
+	,TAXABLE.dblTaxable
