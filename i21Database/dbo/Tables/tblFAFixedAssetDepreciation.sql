61ce0d7ddf065cb4eda53495399bf7f739afcc4e
axel_casauran <axel.casauran@irely.com>
2017-05-16 08:19:09 +0800

    FA-60

--- i21Database/dbo/Tables/tblFAFixedAssetDepreciation.sql
+++ i21Database/dbo/Tables/tblFAFixedAssetDepreciation.sql
@@ -8,7 +8,11 @@ CREATE TABLE [dbo].[tblFAFixedAssetDepreciation] (
 	[dtmDateInService]			DATETIME NULL,	
 	[dtmDispositionDate]		DATETIME NULL,
 	[dtmDepreciationToDate]		DATETIME NULL,
+	[dblDepreciationToDate]		NUMERIC (18, 6) NULL DEFAULT ((0)),
 	[dblSalvageValue]			NUMERIC (18, 6) NULL DEFAULT ((0)),
+	[strTransaction]			NVARCHAR(50)	NULL DEFAULT ((0)),
+	[strType]					NVARCHAR(50)	NULL DEFAULT ((0)),
+	[strConvention]				NVARCHAR(50)	NULL DEFAULT ((0)),
 
     [intConcurrencyId]          INT DEFAULT 1 NOT NULL,
     CONSTRAINT [PK_tblFAFixedAssetDepreciation] PRIMARY KEY CLUSTERED ([intAssetDepreciationId] ASC),
