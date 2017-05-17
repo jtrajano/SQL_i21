9e43dc27c8b7a7b5bc825589567137b8e9f1a53d
bonn ponce <bonn.ponce@irely.com>
2017-05-17 18:14:35 +0800

    CF-1031

--- i21Database/dbo/Views/vyuCFTransactionMargin.sql
+++ i21Database/dbo/Views/vyuCFTransactionMargin.sql
@@ -1,6 +1,10 @@
-﻿CREATE VIEW dbo.vyuCFTransactionMargin
+﻿
+
+CREATE VIEW [dbo].[vyuCFTransactionMargin]
 AS
 SELECT       ROW_NUMBER() OVER (ORDER BY intTransactionId) AS intTransactionMarginId, intTransactionId,
-dblMargin = ISNULL((SELECT TOP 1 dblMargin FROM dbo.fnCFGetTransactionMargin(intTransactionId,0,0,0,0,0,0)),0),
-dblCost = ISNULL((SELECT TOP 1 dblCost FROM dbo.fnCFGetTransactionMargin(intTransactionId,0,0,0,0,0,0)),0)
+dblMargin = ISNULL((SELECT TOP 1 dblMargin FROM dbo.fnCFGetTransactionMargin(intTransactionId,0,0,0,0,0,0,strPriceBasis)),0),
+dblCost = ISNULL((SELECT TOP 1 dblCost FROM dbo.fnCFGetTransactionMargin(intTransactionId,0,0,0,0,0,0,strPriceBasis)),0)
 FROM            dbo.tblCFTransaction
+
+
