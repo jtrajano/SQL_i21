edb7776a05415fafa25ad0ebfe2a3cb066ced11f
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2017-05-17 23:14:05 +0530

    RM-714

--- i21Database/dbo/Views/vyuRKM2MInquiryNotMapping.sql
+++ i21Database/dbo/Views/vyuRKM2MInquiryNotMapping.sql
@@ -7,14 +7,16 @@ SELECT intM2MInquiryId
 		,strCurrency
 		,strMarketZoneCode
 		,strLocationName
-		,strUnitMeasure
+		,um.strUnitMeasure
 		,dtmM2MBasisDate
 		,dtmPriceDate dtmFutureSettlementDate
+		,pu.strUnitMeasure as strPriceUnitMeasure
 FROM tblRKM2MInquiry bd
 JOIN tblRKM2MBasis bas on bas.intM2MBasisId=bd.intM2MBasisId
 JOIN tblSMCurrency cur on cur.intCurrencyID=bd.intCurrencyId
 JOIN tblRKFuturesSettlementPrice sc on sc.intFutureSettlementPriceId=bd.intFutureSettlementPriceId
 LEFT join tblICCommodity c on c.intCommodityId=bd.intCommodityId
 LEFT JOIN tblICUnitMeasure um on um.intUnitMeasureId=bd.intUnitMeasureId
+LEFT JOIN tblICUnitMeasure pu on pu.intUnitMeasureId=bd.intPriceItemUOMId
 LEFT JOIN tblARMarketZone z on z.intMarketZoneId=bd.intMarketZoneId
 LEFT JOIN tblSMCompanyLocation l on l.intCompanyLocationId=bd.intCompanyLocationId
