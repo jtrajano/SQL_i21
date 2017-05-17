22171ec198a4a738f6610ea4d9df0be025f8dabd
Chakra <chakradhar.behera@irely.com>
2017-05-17 22:17:23 +0530

    MFG-2463

--- i21Database/dbo/Views/vyuMFGetBlendItemByLocation.sql
+++ i21Database/dbo/Views/vyuMFGetBlendItemByLocation.sql
@@ -2,7 +2,7 @@
 	AS 
 Select DISTINCT 
 i.intItemId,i.strItemNo,i.strDescription,i.strType,i.intCategoryId,i.strStatus,i.strInventoryTracking,i.strLotTracking,
-il.intLocationId,iu.intItemUOMId AS intStockItemUOMId,iu.intUnitMeasureId AS intStockUOMId ,um.strUnitMeasure AS strStockUOM,cg.strCategoryCode
+il.intLocationId,iu.intItemUOMId AS intStockItemUOMId,iu.intUnitMeasureId AS intStockUOMId ,um.strUnitMeasure AS strStockUOM,cg.strCategoryCode,cl.strLocationName
 From tblICItem i 
 Join tblICItemLocation il on i.intItemId=il.intItemId
 Join tblICItemUOM iu on i.intItemId=iu.intItemId
@@ -10,4 +10,5 @@ Join tblICUnitMeasure um on iu.intUnitMeasureId=um.intUnitMeasureId
 Left Join tblICCategory cg on i.intCategoryId=cg.intCategoryId
 JOIN tblMFRecipe r on i.intItemId=r.intItemId
 JOIN tblMFManufacturingProcess mp on r.intManufacturingProcessId=mp.intManufacturingProcessId AND mp.intAttributeTypeId=2
+JOIN tblSMCompanyLocation cl on il.intLocationId=cl.intCompanyLocationId
 Where  iu.ysnStockUnit=1 AND r.ysnActive=1
