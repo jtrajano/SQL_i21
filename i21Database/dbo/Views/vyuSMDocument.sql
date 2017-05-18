7d382c56c98d396faa6b9145ff00de30db0750bc
Von Carlo Pasion <von.pasion@irely.com>
2017-05-18 16:15:42 +0800

    FRM-4964

--- i21Database/dbo/Views/vyuSMDocument.sql
+++ i21Database/dbo/Views/vyuSMDocument.sql
@@ -4,6 +4,7 @@ AS
 WITH FolderHeirarchy AS (
   SELECT 
 	intDocumentSourceFolderId,  
+	A.intDocumentTypeId,
 	CAST(strName AS VARCHAR(MAX)) AS strFolderPath
   FROM tblSMDocumentSourceFolder A
   WHERE intDocumentFolderParentId IS NULL
@@ -12,6 +13,7 @@ WITH FolderHeirarchy AS (
 
   SELECT 
 	B.intDocumentSourceFolderId, 
+	B.intDocumentTypeId,
 	CAST(strFolderPath + '\' + CAST(B.strName AS VARCHAR(MAX)) AS VARCHAR(MAX)) AS strFolderPath
   FROM tblSMDocumentSourceFolder B
 	INNER JOIN FolderHeirarchy C ON C.intDocumentSourceFolderId = B.intDocumentFolderParentId
@@ -29,9 +31,14 @@ SELECT
 	B.strFolderPath AS strFolderPath,
 	B.intDocumentSourceFolderId,
 	C.intRecordId,
-	E.strNamespace
+	E.strNamespace,
+	C.strTransactionNo,
+	E.strScreenName,
+	E.intScreenId,
+	F.strName AS strDocumentType
 FROM tblSMDocument A 
 	INNER JOIN FolderHeirarchy B ON A.intDocumentSourceFolderId = B.intDocumentSourceFolderId
 	INNER JOIN tblSMTransaction C ON A.intTransactionId = C.intTransactionId
 	INNER JOIN tblEMEntity D ON A.intEntityId = D.intEntityId
 	INNER JOIN tblSMScreen E ON C.intScreenId = E.intScreenId
+	LEFT JOIN tblSMDocumentType F ON F.intDocumentTypeId = B.intDocumentTypeId
