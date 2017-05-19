53a9e9f4bc343f64ca917d336a2ed7b3dad02e45
Smith Wilson de Jesus <smith.dejesus@irely.com>
2017-05-19 08:25:04 +0800

    CM-1698 [CM Import from Origin] Error Importing Data

--- Integration/dbo/Stored Procedures/uspCMImportValidations.sql
+++ Integration/dbo/Stored Procedures/uspCMImportValidations.sql
@@ -25,6 +25,7 @@ BEGIN
 			,@Missing_Cash_Account_Group AS BIT OUTPUT
 			,@Future_Clear_Date_Found AS BIT OUTPUT
 			,@Unbalance_Found AS BIT OUTPUT
+			,@Duplicate_Bank_Name_Found AS BIT OUTPUT
 		AS
 
 		DECLARE @CASH_ACCOUNT AS NVARCHAR(20) = ''Cash Account''
@@ -117,6 +118,15 @@ BEGIN
 		) AS T
 		WHERE NotBalance = 1
 
+		--Check for duplicate Bank Name
+		SELECT TOP 1 @Duplicate_Bank_Name_Found = 1 
+		FROM(
+				SELECT  ssbnk_name = i.ssbnk_name COLLATE Latin1_General_CI_AS
+				FROM ssbnkmst i
+				GROUP BY i.ssbnk_name COLLATE Latin1_General_CI_AS
+				HAVING (COUNT(i.ssbnk_name) > 1)
+			) Q
+
 
 		SELECT	@Invalid_UserId_Found = ISNULL(@Invalid_UserId_Found, 0)
 				,@Invalid_GL_Account_Id_Found = ISNULL(@Invalid_GL_Account_Id_Found, 0)
@@ -127,6 +137,7 @@ BEGIN
 				,@Missing_Cash_Account_Group = ISNULL(@Missing_Cash_Account_Group, 0)
 				,@Future_Clear_Date_Found = ISNULL(@Future_Clear_Date_Found, 0)	
 				,@Unbalance_Found = ISNULL(@Unbalance_Found, 0)	
+				,@Duplicate_Bank_Name_Found = ISNULL(@Duplicate_Bank_Name_Found, 0)	
 	')
 
 END
