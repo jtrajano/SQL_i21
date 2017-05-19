52a08289bdcf5eac4d8b5b522d77f74bdda80ed3
Kelvin Quinto <kelvin.quinto@irely.com>
2017-05-20 06:30:50 +0800

    AR-3061

--- i21Database/dbo/Views/vyuARInvoiceSearch.sql
+++ i21Database/dbo/Views/vyuARInvoiceSearch.sql
@@ -5,7 +5,7 @@ SELECT
 	,strInvoiceNumber				= I.strInvoiceNumber
 	,strCustomerName				= CE.strName
 	,strCustomerNumber				= C.strCustomerNumber
-	,intEntityCustomerId			= C.[intEntityId]
+	,intEntityCustomerId			= C.intEntityId
 	,strTransactionType				= I.strTransactionType
 	,strType						= ISNULL(I.strType, 'Standard')
 	,strPONumber					= I.strPONumber
@@ -23,15 +23,15 @@ SELECT
 	,ysnForgiven					= I.ysnForgiven
 	,ysnCalculated					= I.ysnCalculated
 	,ysnRecurring					= I.ysnRecurring
-	,dblInvoiceTotal				= CASE WHEN (I.strTransactionType  IN ('Invoice','Debit Memo', 'Cash')) THEN ISNULL(I.dblInvoiceTotal, 0)  ELSE  ISNULL(I.dblInvoiceTotal, 0) * -1 END
-	,dblDiscount					= CASE WHEN (I.strTransactionType  IN ('Invoice','Debit Memo', 'Cash')) THEN ISNULL(I.dblDiscount,0)  ELSE  ISNULL(I.dblDiscount,0) * -1 END
-	,dblDiscountAvailable			= CASE WHEN (I.strTransactionType  IN ('Invoice','Debit Memo', 'Cash')) THEN ISNULL(I.dblDiscountAvailable,0)  ELSE  ISNULL(I.dblDiscountAvailable,0) * -1 END
-	,dblInterest					= CASE WHEN (I.strTransactionType  IN ('Invoice','Debit Memo', 'Cash')) THEN ISNULL(I.dblInterest,0)  ELSE  ISNULL(I.dblInterest,0) * -1 END
-	,dblAmountDue					= CASE WHEN (I.strTransactionType  IN ('Invoice','Debit Memo', 'Cash')) THEN ISNULL(I.dblAmountDue,0)  ELSE  ISNULL(I.dblAmountDue,0) * -1 END
-	,dblPayment						= CASE WHEN (I.strTransactionType  IN ('Invoice','Debit Memo', 'Cash')) THEN ISNULL(I.dblPayment, 0)  ELSE  ISNULL(I.dblPayment, 0) * -1 END
-	,dblInvoiceSubtotal				= CASE WHEN (I.strTransactionType  IN ('Invoice','Debit Memo', 'Cash')) THEN ISNULL(I.dblInvoiceSubtotal, 0)  ELSE  ISNULL(I.dblInvoiceSubtotal, 0) * -1 END
-	,dblShipping					= CASE WHEN (I.strTransactionType  IN ('Invoice','Debit Memo', 'Cash')) THEN ISNULL(I.dblShipping, 0)  ELSE  ISNULL(I.dblShipping, 0) * -1 END
-	,dblTax							= CASE WHEN (I.strTransactionType  IN ('Invoice','Debit Memo', 'Cash')) THEN ISNULL(I.dblTax, 0)  ELSE  ISNULL(I.dblTax, 0) * -1 END
+	,dblInvoiceTotal				= ISNULL(I.dblInvoiceTotal, 0)
+	,dblDiscount					= ISNULL(I.dblDiscount,0)
+	,dblDiscountAvailable			= ISNULL(I.dblDiscountAvailable,0)
+	,dblInterest					= ISNULL(I.dblInterest,0)
+	,dblAmountDue					= ISNULL(I.dblAmountDue,0)
+	,dblPayment						= ISNULL(I.dblPayment, 0)
+	,dblInvoiceSubtotal				= ISNULL(I.dblInvoiceSubtotal, 0)
+	,dblShipping					= ISNULL(I.dblShipping, 0)
+	,dblTax							= ISNULL(I.dblTax, 0)
 	,intPaymentMethodId				= I.intPaymentMethodId
 	,intCompanyLocationId			= I.intCompanyLocationId
 	,strComments					= I.strComments
@@ -44,79 +44,30 @@ SELECT
 	,strCurrency					= CUR.strCurrency
 	,intEntredById					= I.intEntityId
 	,strEnteredBy					= EB.strName
-	,dtmBatchDate					= --GL.dtmDate	
-									  (
-										SELECT TOP 1
-											G.dtmDate
-										FROM
-											tblGLDetail G
-										WHERE
-											I.intInvoiceId = G.intTransactionId
-											AND I.strInvoiceNumber = G.strTransactionId
-											AND I.intAccountId = G.intAccountId
-											AND G.strTransactionType IN ('Invoice', 'Credit Memo', 'Debit Memo', 'Cash', 'Cash Refund', 'Customer Prepayment')
-											AND G.ysnIsUnposted = 0
-											AND G.strCode = 'AR'
-										)
+	,dtmBatchDate					= USERENTERED.dtmDate
 	,strBatchId						= CASE WHEN I.strTransactionType = 'Customer Prepayment' 
-										THEN  (SELECT TOP 1 strBatchId FROM tblARPayment A 
-												INNER JOIN (SELECT intPaymentId, intInvoiceId FROM tblARPaymentDetail ) B ON A.intPaymentId = B.intPaymentId 
-												INNER JOIN (SELECT strTransactionId, strBatchId FROM tblGLDetail) C ON A.strRecordNumber = C.strTransactionId 
-												WHERE B.intInvoiceId = I.intInvoiceId) 
-										ELSE  
-											--GL.strBatchId 
-											(
-											SELECT TOP 1
-												G.strBatchId
-											FROM
-												tblGLDetail G
-											WHERE
-												I.intInvoiceId = G.intTransactionId
-												AND I.strInvoiceNumber = G.strTransactionId
-												AND I.intAccountId = G.intAccountId
-												AND G.strTransactionType IN ('Invoice', 'Credit Memo', 'Debit Memo', 'Cash', 'Cash Refund', 'Customer Prepayment')
-												AND G.ysnIsUnposted = 0
-												AND G.strCode = 'AR'
-											)
-										END   
-	,strUserEntered					= --GL.strName
-									  (
-										SELECT TOP 1
-											E.strName
-										FROM
-											tblGLDetail G
-										INNER JOIN
-											(SELECT intEntityId,
-													strName
-											 FROM tblEMEntity) E ON G.intEntityId = E.intEntityId
-										WHERE
-											I.intInvoiceId = G.intTransactionId
-											AND I.strInvoiceNumber = G.strTransactionId
-											AND I.intAccountId = G.intAccountId
-											AND G.strTransactionType IN ('Invoice', 'Credit Memo', 'Debit Memo', 'Cash', 'Cash Refund', 'Customer Prepayment')
-											AND G.ysnIsUnposted = 0
-											AND G.strCode = 'AR'
-										)	
+										   THEN PAYMENT.strBatchId
+										   ELSE USERENTERED.strBatchId
+									  END   
+	,strUserEntered					= USERENTERED.strName
 	,intEntityContactId				= I.intEntityContactId
 	,strContactName					= EC.strName
 	,strTicketNumbers				= dbo.fnARGetScaleTicketNumbersFromInvoice(I.intInvoiceId)
 	,strCustomerReferences			= dbo.fnARGetCustomerReferencesFromInvoice(I.intInvoiceId)
-	,ysnHasEmailSetup				= CASE WHEN (SELECT COUNT(*) FROM vyuARCustomerContacts CC WHERE CC.intCustomerEntityId = I.intEntityCustomerId AND ISNULL(CC.strEmail, '') <> '' AND CC.strEmailDistributionOption LIKE '%' + I.strTransactionType + '%') > 0 THEN CONVERT(BIT, 1) ELSE CONVERT(BIT, 0) END	
+	,ysnHasEmailSetup				= CASE WHEN EMAILSETUP.intEmailSetupCount > 0 THEN CONVERT(BIT, 1) ELSE CONVERT(BIT, 0) END	
 	,strCurrencyDescription			= CUR.strDescription
-FROM  (SELECT strType, [intEntityCustomerId], intCompanyLocationId, intTermId, intEntityContactId, intPaymentMethodId, [intEntitySalespersonId], intCurrencyId, intShipViaId,[intEntityId],
+FROM (SELECT strType, [intEntityCustomerId], intCompanyLocationId, intTermId, intEntityContactId, intPaymentMethodId, [intEntitySalespersonId], intCurrencyId, intShipViaId,[intEntityId],
 		intInvoiceId, strInvoiceNumber, intAccountId, strTransactionType, strPONumber, strBOLNumber, strComments, dtmDate, dtmDueDate, dtmPostDate, dtmShipDate, ysnPosted, ysnPaid,
 		ysnProcessed, ysnRecurring, ysnForgiven, ysnCalculated, dblInvoiceTotal, dblDiscount, dblDiscountAvailable, dblInterest, dblAmountDue, dblPayment, dblInvoiceSubtotal, dblShipping, dblTax
 	 FROM dbo.tblARInvoice WITH (NOLOCK)) AS I 
-LEFT OUTER JOIN
-	(SELECT TOP 1 strName, strEmail, intEntityContactId FROM vyuEMEntityContact WITH (NOLOCK)) EC ON I.intEntityContactId = EC.intEntityContactId
 INNER JOIN
-	(SELECT [intEntityId], strCustomerNumber FROM dbo.tblARCustomer WITH (NOLOCK)) AS C 
-		ON I.[intEntityCustomerId] = C.[intEntityId] 
+	(SELECT intEntityId, strCustomerNumber FROM dbo.tblARCustomer WITH (NOLOCK)) AS C 
+		ON I.[intEntityCustomerId] = C.intEntityId 
 INNER JOIN
 	(SELECT intEntityId,
 			strName
 	 FROM 
-		dbo.tblEMEntity WITH (NOLOCK)) AS CE ON C.[intEntityId] = CE.intEntityId 
+		dbo.tblEMEntity WITH (NOLOCK)) AS CE ON C.intEntityId = CE.intEntityId 
 LEFT OUTER JOIN
 	(SELECT intTermID,
 			strTerm
@@ -136,7 +87,7 @@ LEFT OUTER JOIN
 	(SELECT intEntityId,
 			strShipVia
 	 FROM 
-		dbo.tblSMShipVia WITH (NOLOCK)) AS SV ON I.intShipViaId = SV.[intEntityId]
+		dbo.tblSMShipVia WITH (NOLOCK)) AS SV ON I.intShipViaId = SV.intEntityId
 LEFT OUTER JOIN
 	(SELECT intEntityId,
 			strName
@@ -151,3 +102,46 @@ LEFT OUTER JOIN
 LEFT OUTER JOIN
 	(SELECT intEntityId, strName FROM dbo.tblEMEntity WITH (NOLOCK)) AS EB 
 		ON I.[intEntityId] = EB.intEntityId
+OUTER APPLY (
+	SELECT TOP 1 E.strName
+			   , G.dtmDate
+			   , G.strBatchId
+	FROM dbo.tblGLDetail G WITH (NOLOCK)
+	LEFT JOIN (SELECT intEntityId
+				     , strName
+				FROM dbo.tblEMEntity WITH (NOLOCK)
+	) E ON G.intEntityId = E.intEntityId
+	WHERE I.intInvoiceId = G.intTransactionId
+	  AND I.strInvoiceNumber = G.strTransactionId
+	  AND I.intAccountId = G.intAccountId
+	  AND G.strTransactionType IN ('Invoice', 'Credit Memo', 'Debit Memo', 'Cash', 'Cash Refund', 'Customer Prepayment')
+	  AND G.ysnIsUnposted = 0
+	  AND G.strCode = 'AR'
+) USERENTERED
+OUTER APPLY (
+	SELECT TOP 1 strBatchId 
+	FROM dbo.tblARPayment A WITH (NOLOCK)
+	INNER JOIN (SELECT intPaymentId
+					 , intInvoiceId 
+				FROM dbo.tblARPaymentDetail WITH (NOLOCK)
+	) B ON A.intPaymentId = B.intPaymentId 
+	INNER JOIN (SELECT strTransactionId
+					 , strBatchId 
+				FROM dbo.tblGLDetail WITH (NOLOCK)
+	) C ON A.strRecordNumber = C.strTransactionId 
+	WHERE B.intInvoiceId = I.intInvoiceId
+) PAYMENT
+OUTER APPLY (
+	SELECT TOP 1 strName
+			   , strEmail
+			   , intEntityContactId 
+	FROM dbo.vyuEMEntityContact WITH (NOLOCK) 
+	WHERE I.intEntityContactId = intEntityContactId
+) EC
+OUTER APPLY (
+	SELECT intEmailSetupCount = COUNT(*) 
+	FROM dbo.vyuARCustomerContacts WITH (NOLOCK)
+	WHERE intCustomerEntityId = I.intEntityCustomerId 
+	  AND ISNULL(strEmail, '') <> '' 
+	  AND strEmailDistributionOption LIKE '%' + I.strTransactionType + '%'
+) EMAILSETUP
