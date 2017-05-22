15e34deb2af6595dcc2aab4c76f3820175e77894
Smith Wilson de Jesus <smith.dejesus@irely.com>
2017-05-23 04:48:33 +0800

    CM-1707 Records with intBankDepositId should not be allowed to be deleted from tblCMUndepositedFund

--- i21Database/dbo/Tables/tblCMUndepositedFund.sql
+++ i21Database/dbo/Tables/tblCMUndepositedFund.sql
@@ -35,3 +35,38 @@ GO
 CREATE NONCLUSTERED INDEX [IX_tblCMUndepositedFund_intBankDepositId]
     ON [dbo].[tblCMUndepositedFund]([intBankDepositId] ASC);
 GO
+
+CREATE TRIGGER trg_delete_tblCMUndepositedFund
+ON [dbo].tblCMUndepositedFund
+INSTEAD OF DELETE
+AS
+BEGIN 
+
+	SET NOCOUNT ON
+
+	------------------------------------------------------------------------------------------
+	-- Validate the undeposited fund first before deleting the record. Prevent delete if: 
+	------------------------------------------------------------------------------------------
+	-- 1. ...if undeposited fund is already deposited
+	IF EXISTS (
+		SELECT	TOP 1 1 
+		FROM	deleted d INNER JOIN dbo.tblCMUndepositedFund undep 
+					ON d.intUndepositedFundId = undep.intUndepositedFundId
+		WHERE	d.intBankDepositId IS NOT NULL
+	)
+	BEGIN
+		RAISERROR('Unable to delete undeposited fund because it is used in Bank Deposit transaction.', 11, 1)
+		GOTO EXIT_TRIGGER
+	END
+	IF @@ERROR <> 0 GOTO EXIT_TRIGGER
+
+	-- Delete records
+	DELETE	dbo.tblCMUndepositedFund
+	FROM	dbo.tblCMUndepositedFund 
+	WHERE	intUndepositedFundId IN (SELECT d.intUndepositedFundId FROM deleted d)
+	IF @@ERROR <> 0 GOTO EXIT_TRIGGER
+
+EXIT_TRIGGER:
+
+END
+GO
