e9850011dc84bb3c282ca5c3f51a38b2aaf60434
Premkumar <Prem.Selladurai@gmail.com>
2017-05-22 17:59:01 +0530

    MFG-2470

---
+++ i21Database/dbo/Views/vyuMFGetAvailableStorageLocation.sql
@@ -0,0 +1,12 @@
+﻿CREATE VIEW vyuMFGetAvailableStorageLocation
+AS
+SELECT DISTINCT SL.strName
+	,SL.strDescription
+	,CASE 
+		WHEN L.intLotId IS NULL
+			THEN 'Yes'
+		ELSE 'No'
+		END AS strAvailable
+FROM tblICStorageLocation SL
+LEFT JOIN tblICLot L ON L.intStorageLocationId = SL.intStorageLocationId
+	AND dblQty > 0
