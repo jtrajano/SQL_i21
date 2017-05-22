7de23fc71d68ef04f400622e4a809ccb3ff39be0
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2017-05-22 22:49:46 +0530

    RM-743

---
+++ i21Database/dbo/Views/vyuRKM2MContractCost.sql
@@ -0,0 +1,13 @@
+﻿CREATE VIEW vyuRKM2MContractCost
+AS
+SELECT CC.intContractDetailId, 		
+			CC.dblRate,
+			IU.intUnitMeasureId,
+			CC.intItemId,
+			CH.intContractBasisId
+
+FROM		tblCTContractCost	CC
+JOIN		tblCTContractDetail CD ON CD.intContractDetailId	=	CC.intContractDetailId
+join		tblCTContractHeader CH ON CH.intContractHeaderId	=	CD.intContractHeaderId
+JOIN  tblICItemUOM		IU 
+ON IU.intItemUOMId			=	CC.intItemUOMId
