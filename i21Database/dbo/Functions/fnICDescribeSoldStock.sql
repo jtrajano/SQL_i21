0fed9eb81f61c859df7b7ac961404ff358ce0984
Feb Montefrio <feb.montefrio@irely.com>
2017-05-22 14:59:25 +0800

    IC-3848

--- i21Database/dbo/Functions/fnICDescribeSoldStock.sql
+++ i21Database/dbo/Functions/fnICDescribeSoldStock.sql
@@ -7,23 +7,46 @@ RETURNS NVARCHAR(100)
 AS
 BEGIN 
 	DECLARE @result AS NVARCHAR(100) 
-	
-	DECLARE @strQty AS NVARCHAR(50) = CONVERT(NVARCHAR, CAST(@dblQty AS MONEY), 1)
-	DECLARE @strCost AS NVARCHAR(50) = CONVERT(NVARCHAR, CAST(@dblCost AS MONEY), 1)
 
-	-- [Item, Qty, Cost]: {Item No}, {Qty}, {Cost}.
-	SET @result = dbo.fnFormatMessage(
-					dbo.fnICGetErrorMessage(80159)
-					, @strItemNo
-					, @strQty
-					, @strCost
-					, DEFAULT
-					, DEFAULT
-					, DEFAULT
-					, DEFAULT
-					, DEFAULT
-					, DEFAULT
-					, DEFAULT
-				)	
+	-- If Qty and cost are both zero, then item name only. 
+	IF ISNULL(@dblQty, 0) = 0 AND ISNULL(@dblCost, 0) = 0 
+	BEGIN 
+		-- Item: %s
+		SET @result = dbo.fnFormatMessage(
+						dbo.fnICGetErrorMessage(80179)
+						, @strItemNo
+						, DEFAULT
+						, DEFAULT
+						, DEFAULT
+						, DEFAULT
+						, DEFAULT
+						, DEFAULT
+						, DEFAULT
+						, DEFAULT
+						, DEFAULT
+					)	
+		RETURN @result;	
+	END 
+
+	ELSE 
+	BEGIN 
+		DECLARE @strQty AS NVARCHAR(50) = CONVERT(NVARCHAR, CAST(@dblQty AS MONEY), 1)
+		DECLARE @strCost AS NVARCHAR(50) = CONVERT(NVARCHAR, CAST(@dblCost AS MONEY), 1)
+
+		-- 'Item: %s, Qty: %s, Cost: %s'
+		SET @result = dbo.fnFormatMessage(
+						dbo.fnICGetErrorMessage(80159)
+						, @strItemNo
+						, @strQty
+						, @strCost
+						, DEFAULT
+						, DEFAULT
+						, DEFAULT
+						, DEFAULT
+						, DEFAULT
+						, DEFAULT
+						, DEFAULT
+					)	
+	END 
 	RETURN @result;
 END
