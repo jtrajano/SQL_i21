b647756d6b7906bb25c789248213dd0962d5baeb
Emerson Versoza <emerson.versoza@irely.com>
2017-05-22 16:22:13 +0800

    SC-809

---
+++ i21Database/dbo/Views/vyuSCGradeReadingReport.sql
@@ -0,0 +1,17 @@
+﻿CREATE VIEW dbo.vyuSCGradeReadingReport
+AS SELECT 
+GR.intDiscountScheduleCodeId
+, GR.intItemId
+, IC.strShortName AS strDiscountCode
+, IC.strItemNo AS strDiscountCodeDescription
+, GR.intDiscountCalculationOptionId
+, GR.strDiscountChargeType
+, QM.dblGradeReading
+, QM.dblDiscountAmount
+, QM.dblShrinkPercent
+, QM.strShrinkWhat
+, QM.intTicketId 
+, (SELECT intCurrencyDecimal FROM tblSMCompanyPreference) AS intDecimalPrecision
+FROM tblGRDiscountScheduleCode GR 
+LEFT JOIN tblICItem IC on GR.intItemId = IC.intItemId 
+LEFT JOIN tblQMTicketDiscount QM on QM.intDiscountScheduleCodeId = GR.intDiscountScheduleCodeId
