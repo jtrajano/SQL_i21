4594009fca0780f3007ad88049102c6441b072f4
Erick F. Dela Cruz <erick.delacruz@irely.com>
2017-05-23 14:06:53 +0800

    AP-3404

--- i21Database/dbo/Views/vyuAPQuickVoucherSearch.sql
+++ i21Database/dbo/Views/vyuAPQuickVoucherSearch.sql
@@ -5,6 +5,7 @@ SELECT
 	A.intBillId,
 	CASE WHEN (A.intTransactionType IN (3,8)) OR (A.intTransactionType = 2 AND A.ysnPosted = 1) THEN A.dblTotal * -1 ELSE A.dblTotal END AS dblTotal,
 	CASE WHEN (A.intTransactionType IN (3,8)) OR (A.intTransactionType = 2 AND A.ysnPosted = 1) THEN A.dblAmountDue * -1 ELSE A.dblAmountDue END AS dblAmountDue,
+	CASE WHEN (A.intTransactionType IN (3,8)) OR (A.intTransactionType = 2 AND A.ysnPosted = 1) THEN A.dblPayment * -1 ELSE A.dblPayment END AS dblPayment,
 	A.dtmDate,
 	A.dtmBillDate,
 	A.dtmDueDate,
@@ -17,10 +18,13 @@ SELECT
 	CASE WHEN A.dtmDateCreated IS NULL THEN A.dtmDate ELSE A.dtmDateCreated END AS dtmDateCreated,
 	A.dblTax,
 	B1.strName,
-	F.strUserName AS strUserId
+	F.strUserName AS strUserId,
+	G.strLocationName AS strUserLocation
 FROM
 	dbo.tblAPBill A
 	INNER JOIN 
 		(dbo.tblAPVendor B INNER JOIN dbo.tblEMEntity B1 ON B.[intEntityVendorId] = B1.intEntityId)
 		ON A.[intEntityVendorId] = B.[intEntityVendorId]
 	LEFT JOIN dbo.[tblEMEntityCredential] F ON A.intEntityId = F.intEntityId
+	LEFT JOIN dbo.tblSMCompanyLocation G
+		ON A.intStoreLocationId = G.intCompanyLocationId
