8028fd34c0576028c56d9ea0ca7bf8332009d859
jeffrey trajano <jeffrey.trajano@irely.com>
2017-05-23 18:34:02 +0800

    GL-3482 SegmentAccountStructure store missing strStructureName field

--- i21Database/dbo/Views/vyuGLSegmentDetail.sql
+++ i21Database/dbo/Views/vyuGLSegmentDetail.sql
@@ -1,9 +1,18 @@
 ﻿CREATE VIEW [dbo].[vyuGLSegmentDetail]
 AS
-SELECT        S.intAccountSegmentId, U.intSort, S.strCode, C.strAccountCategory, S.intAccountCategoryId, S.strDescription, G.strAccountType, S.intAccountStructureId
-FROM            dbo.tblGLAccountSegment AS S LEFT OUTER JOIN
-                         dbo.tblGLAccountGroup AS G ON S.intAccountGroupId = G.intAccountGroupId LEFT OUTER JOIN
-                         dbo.tblGLAccountCategory AS C ON S.intAccountCategoryId = C.intAccountCategoryId LEFT OUTER JOIN
-                         dbo.tblGLAccountStructure AS U ON S.intAccountStructureId = U.intAccountStructureId
-
+SELECT        
+	S.intAccountSegmentId, 
+	U.intSort, S.strCode, 
+	C.strAccountCategory, 
+	S.intAccountCategoryId, 
+	S.strDescription, 
+	G.strAccountType, 
+	S.intAccountStructureId,
+	U.intStructureType,
+	U.strStructureName 
+FROM            
+	dbo.tblGLAccountSegment AS S LEFT OUTER JOIN
+	dbo.tblGLAccountGroup AS G ON S.intAccountGroupId = G.intAccountGroupId LEFT OUTER JOIN
+	dbo.tblGLAccountCategory AS C ON S.intAccountCategoryId = C.intAccountCategoryId LEFT OUTER JOIN
+	dbo.tblGLAccountStructure AS U ON S.intAccountStructureId = U.intAccountStructureId
 GO
