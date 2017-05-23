28d939f4d50a6a7f90459f845c1f3f50c70ad120
Kelvin Quinto <kelvin.quinto@irely.com>
2017-05-24 05:17:27 +0800

    AR-3061

--- i21Database/dbo/Functions/fnARGetScaleTicketNumbersFromPayment.sql
+++ i21Database/dbo/Functions/fnARGetScaleTicketNumbersFromPayment.sql
@@ -6,26 +6,13 @@ RETURNS NVARCHAR(MAX) AS
 BEGIN
 	DECLARE @strTicketNumbers NVARCHAR(MAX) = NULL
 
-	DECLARE @tmpTable TABLE(intInvoiceId INT)
-	INSERT INTO @tmpTable
-	SELECT intInvoiceId FROM tblARPaymentDetail WHERE intPaymentId = @intPaymentId AND intInvoiceId IS NOT NULL
-	
-	IF EXISTS(SELECT NULL FROM @tmpTable)
-		BEGIN
-			WHILE EXISTS(SELECT TOP 1 NULL FROM @tmpTable)
-			BEGIN
-				DECLARE @intInvoiceId INT
-				
-				SELECT TOP 1 @intInvoiceId = intInvoiceId FROM @tmpTable ORDER BY intInvoiceId
-				
-				IF (SELECT COUNT(*) FROM @tmpTable) > 1
-					SELECT @strTicketNumbers = ISNULL(@strTicketNumbers, '') + dbo.fnARGetScaleTicketNumbersFromInvoice(@intInvoiceId) + ', '
-				ELSE
-					SELECT @strTicketNumbers = ISNULL(@strTicketNumbers, '') + dbo.fnARGetScaleTicketNumbersFromInvoice(@intInvoiceId)
-
-				DELETE FROM @tmpTable WHERE intInvoiceId = @intInvoiceId
-			END
-		END
-
+	SELECT @strTicketNumbers = COALESCE(@strTicketNumbers + ', ', '') + RTRIM(LTRIM(T.strTicketNumber)) 
+	FROM dbo.tblARPaymentDetail PD WITH (NOLOCK)
+		INNER JOIN (SELECT intTicketId
+							, strTicketNumber
+					FROM dbo.tblSCTicket WITH (NOLOCK)
+		) T ON PD.intInvoiceId = T.intTicketId
+	WHERE PD.intPaymentId = @intPaymentId AND PD.intInvoiceId IS NOT NULL
+		
 	RETURN @strTicketNumbers
 END
