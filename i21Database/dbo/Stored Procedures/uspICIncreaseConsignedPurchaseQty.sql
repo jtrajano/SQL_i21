cf20fe6a177c389f1ba46a221dd1f0c0d855e572
Feb Montefrio <feb.montefrio@irely.com>
2017-05-23 10:36:09 +0800

    IC-3856

--- i21Database/dbo/Stored Procedures/uspICIncreaseConsignedPurchaseQty.sql
+++ i21Database/dbo/Stored Procedures/uspICIncreaseConsignedPurchaseQty.sql
@@ -8,27 +8,59 @@ SET NOCOUNT ON
 SET XACT_ABORT ON
 SET ANSI_WARNINGS OFF
 
--- Do an upsert for the Item Stock table when updating the Reserved Qty
+-- Validate the item-location. 
+BEGIN 
+	DECLARE @intItemId AS INT 
+			,@strItemNo AS NVARCHAR(50)
+
+	SELECT	@intItemId = Item.intItemId 
+			,@strItemNo = Item.strItemNo
+	FROM	@ItemsToIncreaseConsignedPurchase ItemsToValidate LEFT JOIN dbo.tblICItem Item
+				ON ItemsToValidate.intItemId = Item.intItemId
+	WHERE	NOT EXISTS (
+				SELECT TOP 1 1 
+				FROM	dbo.tblICItemLocation
+				WHERE	tblICItemLocation.intItemLocationId = ItemsToValidate.intItemLocationId
+						AND tblICItemLocation.intItemId = ItemsToValidate.intItemId
+			)
+			AND ItemsToValidate.intItemId IS NOT NULL 	
+			
+	-- 'Item-Location is invalid or missing for {Item}.'
+	IF @intItemId IS NOT NULL 
+	BEGIN 
+		EXEC uspICRaiseError 80002, @strItemNo;
+		GOTO _Exit
+	END 
+END 
+
+-- Do an upsert for the Item Stock table when updating the Consigned Purchase Qty
 MERGE	
 INTO	dbo.tblICItemStock 
 WITH	(HOLDLOCK) 
 AS		ItemStock	
 USING (
-		SELECT	ItemsToIncreaseConsignedPurchase.intItemId
-				,ItemsToIncreaseConsignedPurchase.intItemLocationId
-				,Aggregrate_ReserveQty = SUM(ISNULL(dblQty, 0) * ISNULL(tblICItemUOM.dblUnitQty, 0))					
-		FROM	@ItemsToIncreaseConsignedPurchase ItemsToIncreaseConsignedPurchase LEFT JOIN dbo.tblICItemUOM 
-					ON ItemsToIncreaseConsignedPurchase.intItemUOMId = tblICItemUOM.intItemUOMId
-		GROUP BY ItemsToIncreaseConsignedPurchase.intItemId
-				, ItemsToIncreaseConsignedPurchase.intItemLocationId
+		SELECT	cp.intItemId
+				,cp.intItemLocationId
+				,Aggregrate_Qty = SUM(dbo.fnCalculateQtyBetweenUOM(cp.intItemUOMId, StockUOM.intItemUOMId, cp.dblQty))  
+		FROM	@ItemsToIncreaseConsignedPurchase cp 
+				CROSS APPLY (
+					SELECT	TOP 1 
+							intItemUOMId
+							,dblUnitQty 
+					FROM	tblICItemUOM iUOM
+					WHERE	iUOM.intItemId = cp.intItemId
+							AND iUOM.ysnStockUnit = 1 
+				) StockUOM 
+		GROUP BY cp.intItemId
+				, cp.intItemLocationId
 ) AS Source_Query  
 	ON ItemStock.intItemId = Source_Query.intItemId
 	AND ItemStock.intItemLocationId = Source_Query.intItemLocationId
 
--- If matched, update the On-Order qty 
+-- If matched, update the Consigned Purchase qty 
 WHEN MATCHED THEN 
 	UPDATE 
-	SET		dblConsignedPurchase = CASE WHEN ISNULL(ItemStock.dblConsignedPurchase, 0) + Source_Query.Aggregrate_ReserveQty < 0 THEN 0 ELSE ISNULL(ItemStock.dblConsignedPurchase, 0) + Source_Query.Aggregrate_ReserveQty END 
+	SET		dblConsignedPurchase = CASE WHEN ISNULL(ItemStock.dblConsignedPurchase, 0) + Source_Query.Aggregrate_Qty < 0 THEN 0 ELSE ISNULL(ItemStock.dblConsignedPurchase, 0) + Source_Query.Aggregrate_Qty END 
 
 -- If none is found, insert a new item stock record
 WHEN NOT MATCHED THEN 
@@ -42,31 +74,55 @@ WHEN NOT MATCHED THEN
 	VALUES (
 		Source_Query.intItemId
 		,Source_Query.intItemLocationId
-		,CASE WHEN Source_Query.Aggregrate_ReserveQty < 0 THEN 0 ELSE Source_Query.Aggregrate_ReserveQty END -- dblConsignedPurchase
+		,Source_Query.Aggregrate_Qty 
 		,NULL 
 		,1	
 	)		
 ;
 
--- Do an upsert for the Item Stock UOM table when updating the Reserved Qty
+-- Do an upsert for the Item Stock UOM table when updating the Consigned Purchase Qty
 MERGE	
 INTO	dbo.tblICItemStockUOM
 WITH	(HOLDLOCK) 
 AS		ItemStockUOM
 USING (
+		
 		SELECT	ItemsToIncreaseConsignedPurchase.intItemId
 				,ItemsToIncreaseConsignedPurchase.intItemLocationId
 				,ItemsToIncreaseConsignedPurchase.intItemUOMId
 				,ItemsToIncreaseConsignedPurchase.intSubLocationId
 				,ItemsToIncreaseConsignedPurchase.intStorageLocationId
-				,Aggregrate_ReserveQty = SUM(ISNULL(dblQty, 0))
-		FROM	@ItemsToIncreaseConsignedPurchase ItemsToIncreaseConsignedPurchase LEFT JOIN dbo.tblICItemUOM 
-					ON ItemsToIncreaseConsignedPurchase.intItemUOMId = tblICItemUOM.intItemUOMId
+				,Aggregrate_Qty = SUM(ISNULL(dblQty, 0))
+		FROM	@ItemsToIncreaseConsignedPurchase ItemsToIncreaseConsignedPurchase 
 		GROUP BY ItemsToIncreaseConsignedPurchase.intItemId
 				, ItemsToIncreaseConsignedPurchase.intItemLocationId
 				, ItemsToIncreaseConsignedPurchase.intItemUOMId
 				, ItemsToIncreaseConsignedPurchase.intSubLocationId
 				, ItemsToIncreaseConsignedPurchase.intStorageLocationId
+		-- Convert the Consigned Purchase Qty to the Stock UOM before adding it into tblICItemStockUOM
+		UNION ALL 
+		SELECT	cp.intItemId
+				,cp.intItemLocationId
+				,StockUOM.intItemUOMId
+				,cp.intSubLocationId
+				,cp.intStorageLocationId
+				,Aggregrate_Qty = SUM(dbo.fnCalculateQtyBetweenUOM(cp.intItemUOMId, StockUOM.intItemUOMId, cp.dblQty))  
+		FROM	@ItemsToIncreaseConsignedPurchase cp 
+				CROSS APPLY (
+					SELECT	TOP 1 
+							intItemUOMId
+							,dblUnitQty 
+					FROM	tblICItemUOM iUOM
+					WHERE	iUOM.intItemId = cp.intItemId
+							AND iUOM.ysnStockUnit = 1 
+				) StockUOM 
+		WHERE	cp.intItemUOMId <> StockUOM.intItemUOMId
+		GROUP BY cp.intItemId
+				, cp.intItemLocationId
+				, StockUOM.intItemUOMId
+				, cp.intSubLocationId
+				, cp.intStorageLocationId
+
 ) AS Source_Query  
 	ON ItemStockUOM.intItemId = Source_Query.intItemId
 	AND ItemStockUOM.intItemLocationId = Source_Query.intItemLocationId
@@ -74,10 +130,10 @@ USING (
 	AND ISNULL(ItemStockUOM.intSubLocationId, 0) = ISNULL(Source_Query.intSubLocationId, 0)
 	AND ISNULL(ItemStockUOM.intStorageLocationId, 0) = ISNULL(Source_Query.intStorageLocationId, 0)
 
--- If matched, update the On-Order qty 
+-- If matched, update the Consigned Purchase qty 
 WHEN MATCHED THEN 
 	UPDATE 
-	SET		dblConsignedPurchase = CASE WHEN ISNULL(ItemStockUOM.dblConsignedPurchase, 0) + Source_Query.Aggregrate_ReserveQty < 0 THEN 0 ELSE ISNULL(ItemStockUOM.dblConsignedPurchase, 0) + Source_Query.Aggregrate_ReserveQty END 
+	SET		dblConsignedPurchase = CASE WHEN ISNULL(ItemStockUOM.dblConsignedPurchase, 0) + Source_Query.Aggregrate_Qty < 0 THEN 0 ELSE ISNULL(ItemStockUOM.dblConsignedPurchase, 0) + Source_Query.Aggregrate_Qty END 
 
 -- If none is found, insert a new item stock record
 WHEN NOT MATCHED THEN 
@@ -96,7 +152,9 @@ WHEN NOT MATCHED THEN
 		,Source_Query.intItemUOMId
 		,Source_Query.intSubLocationId
 		,Source_Query.intStorageLocationId
-		,Source_Query.Aggregrate_ReserveQty --CASE WHEN Source_Query.Aggregrate_ReserveQty < 0 THEN 0 ELSE Source_Query.Aggregrate_ReserveQty END
+		,Source_Query.Aggregrate_Qty 
 		,1	
 	)
 ;
+
+_Exit:
