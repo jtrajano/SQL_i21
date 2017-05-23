5ee5ba0012c0f6bb64d0ff35e9fab76711c89730
Feb Montefrio <feb.montefrio@irely.com>
2017-05-23 10:36:09 +0800

    IC-3856

--- i21Database/dbo/Stored Procedures/uspICIncreaseOrderCommitted.sql
+++ i21Database/dbo/Stored Procedures/uspICIncreaseOrderCommitted.sql
@@ -8,17 +8,52 @@ SET NOCOUNT ON
 SET XACT_ABORT ON
 SET ANSI_WARNINGS OFF
 
+-- Validate the item-location. 
+BEGIN 
+	DECLARE @intItemId AS INT 
+			,@strItemNo AS NVARCHAR(50)
+
+	SELECT	@intItemId = Item.intItemId 
+			,@strItemNo = Item.strItemNo
+	FROM	@ItemsToIncreaseOrderCommitted ItemsToValidate LEFT JOIN dbo.tblICItem Item
+				ON ItemsToValidate.intItemId = Item.intItemId
+	WHERE	NOT EXISTS (
+				SELECT TOP 1 1 
+				FROM	dbo.tblICItemLocation
+				WHERE	tblICItemLocation.intItemLocationId = ItemsToValidate.intItemLocationId
+						AND tblICItemLocation.intItemId = ItemsToValidate.intItemId
+			)
+			AND ItemsToValidate.intItemId IS NOT NULL 	
+			
+	-- 'Item-Location is invalid or missing for {Item}.'
+	IF @intItemId IS NOT NULL 
+	BEGIN 
+		EXEC uspICRaiseError 80002, @strItemNo;
+		GOTO _Exit
+	END 
+END 
+
 -- Do an upsert for the Item Stock table when updating the Order Committed
 MERGE	
 INTO	dbo.tblICItemStock 
 WITH	(HOLDLOCK) 
 AS		ItemStock	
 USING (
-		SELECT	intItemId
-				,intItemLocationId
-				,Aggregrate_OrderCommitted = SUM(ISNULL(dblQty, 0) * ISNULL(dblUOMQty, 0))					
-		FROM	@ItemsToIncreaseOrderCommitted
-		GROUP BY intItemId, intItemLocationId
+		SELECT	c.intItemId
+				,c.intItemLocationId
+				,Aggregrate_Qty = SUM(dbo.fnCalculateQtyBetweenUOM(c.intItemUOMId, StockUOM.intItemUOMId, c.dblQty)) 
+		FROM	@ItemsToIncreaseOrderCommitted c
+				CROSS APPLY (
+					SELECT	TOP 1 
+							intItemUOMId
+							,dblUnitQty 
+					FROM	tblICItemUOM iUOM
+					WHERE	iUOM.intItemId = c.intItemId
+							AND iUOM.ysnStockUnit = 1 
+				) StockUOM
+		GROUP BY 
+			c.intItemId
+			, c.intItemLocationId
 ) AS Source_Query  
 	ON ItemStock.intItemId = Source_Query.intItemId
 	AND ItemStock.intItemLocationId = Source_Query.intItemLocationId
@@ -26,7 +61,7 @@ USING (
 -- If matched, update the On-Order qty 
 WHEN MATCHED THEN 
 	UPDATE 
-	SET		dblOrderCommitted = CASE WHEN ISNULL(ItemStock.dblOrderCommitted, 0) + Source_Query.Aggregrate_OrderCommitted < 0 THEN 0 ELSE ISNULL(ItemStock.dblOrderCommitted, 0) + Source_Query.Aggregrate_OrderCommitted END 
+	SET		dblOrderCommitted = CASE WHEN ISNULL(ItemStock.dblOrderCommitted, 0) + Source_Query.Aggregrate_Qty < 0 THEN 0 ELSE ISNULL(ItemStock.dblOrderCommitted, 0) + Source_Query.Aggregrate_Qty END 
 
 -- If none is found, insert a new item stock record
 WHEN NOT MATCHED THEN 
@@ -44,7 +79,7 @@ WHEN NOT MATCHED THEN
 		Source_Query.intItemId
 		,Source_Query.intItemLocationId
 		,0
-		,CASE WHEN Source_Query.Aggregrate_OrderCommitted < 0 THEN 0 ELSE Source_Query.Aggregrate_OrderCommitted END -- dblOrderCommitted
+		,CASE WHEN Source_Query.Aggregrate_Qty < 0 THEN 0 ELSE Source_Query.Aggregrate_Qty END -- dblOrderCommitted
 		,0
 		,0
 		,NULL 
@@ -58,14 +93,43 @@ INTO	dbo.tblICItemStockUOM
 WITH	(HOLDLOCK) 
 AS		ItemStockUOM
 USING (
-		SELECT	intItemId
-				,intItemLocationId
+		SELECT	c.intItemId
+				,c.intItemLocationId
 				,intItemUOMId
-				,intSubLocationId
-				,intStorageLocationId
-				,Aggregrate_OrderCommitted = SUM(ISNULL(dblQty, 0))
-		FROM	@ItemsToIncreaseOrderCommitted
-		GROUP BY intItemId, intItemLocationId, intItemUOMId, intSubLocationId, intStorageLocationId
+				,c.intSubLocationId
+				,c.intStorageLocationId
+				,Aggregrate_Qty = SUM(c.dblQty) 
+		FROM	@ItemsToIncreaseOrderCommitted c
+		GROUP BY 
+			c.intItemId
+			, c.intItemLocationId
+			, c.intItemUOMId
+			, c.intSubLocationId
+			, c.intStorageLocationId
+		-- Convert the Committed Qty to 'Stock UOM' before adding it into tblICItemStockUOM
+		UNION ALL 
+		SELECT	c.intItemId
+				,c.intItemLocationId
+				,StockUOM.intItemUOMId
+				,c.intSubLocationId
+				,c.intStorageLocationId
+				,Aggregrate_Qty = SUM(dbo.fnCalculateQtyBetweenUOM(c.intItemUOMId, StockUOM.intItemUOMId, c.dblQty)) 
+		FROM	@ItemsToIncreaseOrderCommitted c
+				CROSS APPLY (
+					SELECT	TOP 1 
+							intItemUOMId
+							,dblUnitQty 
+					FROM	tblICItemUOM iUOM
+					WHERE	iUOM.intItemId = c.intItemId
+							AND iUOM.ysnStockUnit = 1 
+				) StockUOM
+		WHERE	c.intItemUOMId <> StockUOM.intItemUOMId 
+		GROUP BY 
+			c.intItemId
+			, c.intItemLocationId
+			, StockUOM.intItemUOMId
+			, c.intSubLocationId
+			, c.intStorageLocationId
 ) AS Source_Query  
 	ON ItemStockUOM.intItemId = Source_Query.intItemId
 	AND ItemStockUOM.intItemLocationId = Source_Query.intItemLocationId
@@ -76,7 +140,7 @@ USING (
 -- If matched, update the On-Order qty 
 WHEN MATCHED THEN 
 	UPDATE 
-	SET		dblOrderCommitted = CASE WHEN ISNULL(ItemStockUOM.dblOrderCommitted, 0) + Source_Query.Aggregrate_OrderCommitted < 0 THEN 0 ELSE ISNULL(ItemStockUOM.dblOrderCommitted, 0) + Source_Query.Aggregrate_OrderCommitted END 
+	SET		dblOrderCommitted = CASE WHEN ISNULL(ItemStockUOM.dblOrderCommitted, 0) + Source_Query.Aggregrate_Qty < 0 THEN 0 ELSE ISNULL(ItemStockUOM.dblOrderCommitted, 0) + Source_Query.Aggregrate_Qty END 
 
 -- If none is found, insert a new item stock record
 WHEN NOT MATCHED THEN 
@@ -99,7 +163,9 @@ WHEN NOT MATCHED THEN
 		,Source_Query.intStorageLocationId
 		,0
 		,0
-		,CASE WHEN Source_Query.Aggregrate_OrderCommitted < 0 THEN 0 ELSE Source_Query.Aggregrate_OrderCommitted END
+		,CASE WHEN Source_Query.Aggregrate_Qty < 0 THEN 0 ELSE Source_Query.Aggregrate_Qty END
 		,1	
 	)
 ;
+
+_Exit:
