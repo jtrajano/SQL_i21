be9df9fa34d14504c96d1686356e25a48e042b42
Kelvin Quinto <kelvin.quinto@irely.com>
2017-05-24 05:17:27 +0800

    AR-3061

--- i21Database/dbo/Functions/fnARGetScaleTicketNumbersFromInvoice.sql
+++ i21Database/dbo/Functions/fnARGetScaleTicketNumbersFromInvoice.sql
@@ -6,26 +6,9 @@ RETURNS NVARCHAR(MAX) AS
 BEGIN
 	DECLARE @strTicketNumbers NVARCHAR(MAX) = NULL
 
-	DECLARE @tmpTable TABLE(intTicketId INT)
-	INSERT INTO @tmpTable
-	SELECT intTicketId FROM tblARInvoiceDetail WHERE intInvoiceId = @intInvoiceId AND ISNULL(intTicketId, 0) > 0
+	SELECT @strTicketNumbers = COALESCE(@strTicketNumbers + ', ', '') + RTRIM(LTRIM(T.strTicketNumber)) 
+	FROM dbo.tblSCTicket T WITH(NOLOCK) 		
+	WHERE T.intTicketId = @intInvoiceId
 	
-	IF EXISTS(SELECT NULL FROM @tmpTable)
-		BEGIN
-			WHILE EXISTS(SELECT TOP 1 NULL FROM @tmpTable)
-			BEGIN
-				DECLARE @intTicketId INT
-				
-				SELECT TOP 1 @intTicketId = intTicketId FROM @tmpTable ORDER BY intTicketId
-				
-				IF (SELECT COUNT(*) FROM @tmpTable) > 1
-					SELECT @strTicketNumbers = ISNULL(@strTicketNumbers, '') + strTicketNumber + ', ' FROM tblSCTicket WHERE intTicketId = @intTicketId
-				ELSE
-					SELECT @strTicketNumbers = ISNULL(@strTicketNumbers, '') + strTicketNumber FROM tblSCTicket WHERE intTicketId = @intTicketId
-
-				DELETE FROM @tmpTable WHERE intTicketId = @intTicketId
-			END
-		END
-
 	RETURN @strTicketNumbers
 END
