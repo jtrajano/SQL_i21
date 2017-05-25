de3fd41e2c08a22c716ea58aab8972497dc02605
Dan Michael Jalimao <dan.jalimao@irely.com>
2017-05-25 13:19:09 +0800

    FRM-5006

---
+++ i21Database/dbo/Stored Procedures/uspSMDeleteDocumentSourceFolder.sql
@@ -0,0 +1,25 @@
+﻿CREATE PROCEDURE [dbo].[uspSMDeleteDocumentSourceFolder]
+	@FolderIdList NVARCHAR(MAX)
+AS
+BEGIN
+
+DECLARE @sql NVARCHAR(MAX)
+
+SET @sql ='
+			WITH relatedfolders as
+			(
+				SELECT e1.intDocumentSourceFolderId as intDocumentSourceFolderId--, 0 as lvl
+				FROM tblSMDocumentSourceFolder e1
+				WHERE e1.intDocumentSourceFolderId IN (' + @FolderIdList + ')
+				UNION ALL
+				SELECT e2.intDocumentSourceFolderId as intDocumentSourceFolderId--, lvl+1
+				FROM tblSMDocumentSourceFolder e2  
+				INNER JOIN relatedfolders g ON e2.intDocumentFolderParentId = g.intDocumentSourceFolderId
+			)
+			DELETE FROM tblSMDocumentSourceFolder 
+			WHERE intDocumentSourceFolderId IN (SELECT intDocumentSourceFolderId FROM relatedfolders)
+			'
+
+EXEC sp_executesql @sql
+
+END
