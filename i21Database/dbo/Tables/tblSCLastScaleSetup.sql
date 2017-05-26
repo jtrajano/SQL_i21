09ccfced69322edd1b9f6c9cde4c27b351200c1a
Emerson Versoza <emerson.versoza@irely.com>
2017-05-26 17:49:31 +0800

    SC-411

--- i21Database/dbo/Tables/tblSCLastScaleSetup.sql
+++ i21Database/dbo/Tables/tblSCLastScaleSetup.sql
@@ -2,6 +2,7 @@
 (
 	[intLastScaleSetupId] [int] IDENTITY(1,1) NOT NULL,
 	[intScaleSetupId] [int] NULL,
+	[intEntityId] [int] NULL,
 	[dtmScaleDate] [date] NULL,
 	[strScaleOperator] [varchar](40) COLLATE Latin1_General_CI_AS NULL,
 	[intConcurrencyId] [int] NULL, 
