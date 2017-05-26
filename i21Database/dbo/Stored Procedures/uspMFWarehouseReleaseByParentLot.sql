386eea3b6a0d8e9c5eb85ad249611eeaac3f6da4
Vinoth Kumar T <vinoth.t@irely.com>
2017-05-26 18:16:26 +0530

    MFG-2478

--- i21Database/dbo/Stored Procedures/uspMFWarehouseReleaseByParentLot.sql
+++ i21Database/dbo/Stored Procedures/uspMFWarehouseReleaseByParentLot.sql
@@ -1,7 +1,10 @@
-﻿CREATE PROCEDURE uspMFWarehouseReleaseByParentLot (@strXML NVARCHAR(MAX),@strFGReleaseMailTOAddress NVARCHAR(MAX)=NULL OUTPUT
-	,@strFGReleaseMailCCAddress NVARCHAR(MAX)=NULL OUTPUT
-	,@strSubject VARCHAR(MAX)=NULL OUTPUT
-	,@strBody NVARCHAR(MAX)=NULL OUTPUT)
+﻿CREATE PROCEDURE uspMFWarehouseReleaseByParentLot (
+	@strXML NVARCHAR(MAX)
+	,@strFGReleaseMailTOAddress NVARCHAR(MAX) = NULL OUTPUT
+	,@strFGReleaseMailCCAddress NVARCHAR(MAX) = NULL OUTPUT
+	,@strSubject VARCHAR(MAX) = NULL OUTPUT
+	,@strBody NVARCHAR(MAX) = NULL OUTPUT
+	)
 AS
 BEGIN
 	DECLARE @intRecordId INT
@@ -10,12 +13,13 @@ BEGIN
 		,@strInputXML NVARCHAR(MAX)
 		,@idoc INT
 		,@ErrMsg NVARCHAR(MAX)
-		,@intReleaseStatusId int
-		,@intLocationId int
+		,@intReleaseStatusId INT
+		,@intLocationId INT
 		,@dtmCurrentDate DATETIME
-		,@intWorkOrderId int
-		,@intManufacturingProcessId int
-		,@strAttributeValueByWorkOrder nvarchar(50)
+		,@intWorkOrderId INT
+		,@intManufacturingProcessId INT
+		,@strAttributeValueByWorkOrder NVARCHAR(50)
+		,@intReleaseShiftId INT
 
 	SELECT @dtmCurrentDate = GETDATE()
 
@@ -23,12 +27,21 @@ BEGIN
 		,@strXML
 
 	SELECT @intParentLotId = intParentLotId
-			,@intReleaseStatusId=intReleaseStatusId
-			,@intLocationId=intLocationId
-			,@intWorkOrderId=intWorkOrderId
-	FROM OPENXML(@idoc, 'root', 2) WITH (intParentLotId INT,intReleaseStatusId int,intLocationId int,intWorkOrderId int)
+		,@intReleaseStatusId = intReleaseStatusId
+		,@intLocationId = intLocationId
+		,@intWorkOrderId = intWorkOrderId
+		,@intReleaseShiftId = intReleaseShiftId
+	FROM OPENXML(@idoc, 'root', 2) WITH (
+			intParentLotId INT
+			,intReleaseStatusId INT
+			,intLocationId INT
+			,intWorkOrderId INT
+			,intReleaseShiftId INT
+			)
 
-	Select @intManufacturingProcessId=intManufacturingProcessId from tblMFWorkOrder Where intWorkOrderId =@intWorkOrderId
+	SELECT @intManufacturingProcessId = intManufacturingProcessId
+	FROM tblMFWorkOrder
+	WHERE intWorkOrderId = @intWorkOrderId
 
 	SELECT @strAttributeValueByWorkOrder = strAttributeValue
 	FROM tblMFManufacturingProcessAttribute
@@ -36,23 +49,29 @@ BEGIN
 		AND intLocationId = @intLocationId
 		AND intAttributeId = 91
 
-		if @strAttributeValueByWorkOrder is null or @strAttributeValueByWorkOrder=''
-		Select @strAttributeValueByWorkOrder='False'
+	IF @strAttributeValueByWorkOrder IS NULL
+		OR @strAttributeValueByWorkOrder = ''
+		SELECT @strAttributeValueByWorkOrder = 'False'
 
 	DECLARE @tblMFParentLot TABLE (
 		intRecordId INT identity(1, 1)
 		,intLotId INT
 		)
 
-	IF @strAttributeValueByWorkOrder='True'
+	IF @strAttributeValueByWorkOrder = 'True'
 	BEGIN
 		INSERT INTO @tblMFParentLot (intLotId)
-		SELECT Distinct L.intLotId
-		FROM dbo.tblMFWorkOrderProducedLot  PL
-		JOIN dbo.tblICLot L on L.intLotId=PL.intLotId
+		SELECT DISTINCT L.intLotId
+		FROM dbo.tblMFWorkOrderProducedLot PL
+		JOIN dbo.tblICLot L ON L.intLotId = PL.intLotId
 		WHERE PL.intWorkOrderId = @intWorkOrderId
 			AND L.intLotStatusId <> 1
-			AND L.dblQty>0
+			AND L.dblQty > 0
+			AND intShiftId = CASE 
+				WHEN @intReleaseShiftId = - 1
+					THEN intShiftId
+				ELSE @intReleaseShiftId
+				END
 	END
 	ELSE
 	BEGIN
@@ -61,7 +80,7 @@ BEGIN
 		FROM dbo.tblICLot L
 		WHERE L.intParentLotId = @intParentLotId
 			AND L.intLotStatusId <> 1
-			AND L.dblQty>0
+			AND L.dblQty > 0
 	END
 
 	SELECT @intRecordId = MIN(intRecordId)
@@ -77,13 +96,15 @@ BEGIN
 
 		SELECT @strInputXML = Replace(@strXML, '</root>', '<intLotId>' + ltrim(@intLotId) + '</intLotId></root>')
 
-		EXEC uspMFWarehouseReleaseLot @strXML = @strInputXML,@ysnBuildEMailContent=0
+		EXEC uspMFWarehouseReleaseLot @strXML = @strInputXML
+			,@ysnBuildEMailContent = 0
 
 		SELECT @intRecordId = MIN(intRecordId)
 		FROM @tblMFParentLot
 		WHERE intRecordId > @intRecordId
 	END
-	IF @intReleaseStatusId=2 
+
+	IF @intReleaseStatusId = 2
 	BEGIN
 		SELECT @strFGReleaseMailTOAddress = strFGReleaseMailTOAddress
 			,@strFGReleaseMailCCAddress = strFGReleaseMailCCAddress
@@ -93,18 +114,18 @@ BEGIN
 		SELECT @strSubject = 'FG Release List: ' + CONVERT(NVARCHAR, @dtmCurrentDate, 100)
 
 		SET @strBody = N'<H2>FG Release List</H2>' + N'<table border="1" bgcolor ="rgb(192, 255, 255)">' + N'<tr><th width="175">Lot No</th><th width="175">Item Name</th>' + N'<th width="850">Status</th></tr>' + CAST((
-		SELECT td = strLotNumber
-			,''
-			,td = strItemNo
-			,''
-			,td = 'Hold'
-		FROM dbo.tblICLot L
-		JOIN dbo.tblICItem I on I.intItemId=L.intItemId
-		WHERE L.intParentLotId = @intParentLotId
-		AND L.intLotStatusId = 3
-		AND L.dblQty>0
-		FOR XML PATH('tr')
-			,TYPE
-		) AS NVARCHAR(MAX)) + N'</table>';
+					SELECT td = strLotNumber
+						,''
+						,td = strItemNo
+						,''
+						,td = 'Hold'
+					FROM dbo.tblICLot L
+					JOIN dbo.tblICItem I ON I.intItemId = L.intItemId
+					WHERE L.intParentLotId = @intParentLotId
+						AND L.intLotStatusId = 3
+						AND L.dblQty > 0
+					FOR XML PATH('tr')
+						,TYPE
+					) AS NVARCHAR(MAX)) + N'</table>';
 	END
 END
