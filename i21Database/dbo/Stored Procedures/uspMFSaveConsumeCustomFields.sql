f21ae326c11f1ecaee123ee8f62ed6837c4902c1
Vinoth Kumar T <vinoth.t@irely.com>
2017-05-29 20:50:45 +0530

    MFG-2477

---
+++ i21Database/dbo/Stored Procedures/uspMFSaveConsumeCustomFields.sql
@@ -0,0 +1,31 @@
+﻿CREATE PROCEDURE uspMFSaveConsumeCustomFields @strXML NVARCHAR(MAX)
+	,@intWorkOrderInputLotId INT
+AS
+SET QUOTED_IDENTIFIER OFF
+SET ANSI_NULLS ON
+SET NOCOUNT ON
+SET XACT_ABORT ON
+SET ANSI_WARNINGS OFF
+
+DECLARE @idoc INT
+	,@ErrMsg NVARCHAR(MAX)
+
+EXEC sp_xml_preparedocument @idoc OUTPUT
+	,@strXML
+
+INSERT INTO tblMFCustomFieldValue (
+	intConcurrencyId
+	,intCustomTabDetailId
+	,intWorkOrderInputLotId
+	,strValue
+	)
+SELECT 1
+	,x.intCustomTabDetailId
+	,@intWorkOrderInputLotId
+	,x.strValue
+FROM OPENXML(@idoc, 'root/fields', 2) WITH (
+		intCustomTabDetailId INT
+		,strValue NVARCHAR(MAX)
+		) x
+
+EXEC sp_xml_removedocument @idoc
