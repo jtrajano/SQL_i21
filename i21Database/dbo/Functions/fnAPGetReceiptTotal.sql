398fb3bb2c24d3c7a42b9e29e7939dffb52b4aee
Lex Eugene Peregrino <lex.peregrino@irely.com>
2017-05-30 03:26:41 +0800

    AP-3669 - Voucher with negative amount was created after distributing ticket 1.0
    # Conflicts:
    #	i21Database/i21Database.sqlproj

---
+++ i21Database/dbo/Functions/fnAPGetReceiptTotal.sql
@@ -0,0 +1,27 @@
+﻿CREATE FUNCTION [dbo].[fnAPGetReceiptTotal]
+(
+	@receiptId INT
+)
+RETURNS DECIMAL(18,6)
+AS
+BEGIN
+	DECLARE @totalReceiptAmount DECIMAL(18,6);
+	DECLARE @totalReceipts INT;
+	DECLARE @totalReceiptDetails INT;
+	DECLARE @totalChargesCount INT;
+	DECLARE @totalCharges DECIMAL(18,6);
+	DECLARE @receiptAmount DECIMAL(18,6);
+	DECLARE @totalLineItem DECIMAL(18,6);
+
+	SELECT @totalCharges = ISNULL((SUM((CASE WHEN ysnPrice > 0 THEN dblUnitCost * -1 ELSE dblUnitCost END)) + ISNULL(SUM((CASE WHEN ysnPrice > 0 THEN dblTax * -1 ELSE dblTax END)),0.00)),0.00)
+	FROM vyuICChargesForBilling WHERE intInventoryReceiptId = @receiptId
+	
+	SELECT @totalLineItem =   SUM(A.dblLineTotal) + ISNULL(SUM(dblTax),0)
+	FROM dbo.tblICInventoryReceiptItem A 
+	WHERE A.dblUnitCost > 0 AND A.intInventoryReceiptId = @receiptId
+	
+	SET @totalReceiptAmount = @totalLineItem + @totalCharges;
+
+	RETURN @totalReceiptAmount;
+END
+GO
