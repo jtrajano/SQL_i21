4a88470afc8a7e4049b91c7ff5a105758bbc36f3
Vinoth Kumar T <vinoth.t@irely.com>
2017-05-29 20:50:45 +0530

    MFG-2477

---
+++ i21Database/dbo/Tables/tblMFCustomFieldValue.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[tblMFCustomFieldValue]
+(
+	intCustomFieldValueId INT NOT NULL IDENTITY(1,1),
+	intConcurrencyId INT NULL CONSTRAINT DF_tblMFCustomFieldValue_intConcurrencyId DEFAULT 0,
+	intCustomTabDetailId INT NOT NULL,
+	intWorkOrderInputLotId INT NOT NULL,
+	strValue NVARCHAR(MAX) COLLATE Latin1_General_CI_AS,
+
+    CONSTRAINT PK_tblMFCustomFieldValue PRIMARY KEY (intCustomFieldValueId)
+)
