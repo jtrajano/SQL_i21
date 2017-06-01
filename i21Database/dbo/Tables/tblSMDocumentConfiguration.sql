f5e7fe468e588b4d0c970fdce6ab4a60e7c531b5
Kevin Estrada <kevin.estrada@irely.com>
2017-06-01 22:53:14 +0800

    FRM-5015

--- i21Database/dbo/Tables/tblSMDocumentConfiguration.sql
+++ i21Database/dbo/Tables/tblSMDocumentConfiguration.sql
@@ -1,9 +1,11 @@
 ﻿CREATE TABLE [dbo].[tblSMDocumentConfiguration]
 (
 	intDocumentConfigurationId INT IDENTITY (1, 1) NOT NULL PRIMARY KEY, 
-    [intScreenId] INT UNIQUE NULL,
+    [intScreenId] INT NULL,
+	[intScreenReportId] INT NULL,
 	[intDocumentSourceFolderId] INT NULL,
 	[intConcurrencyId] INT NOT NULL DEFAULT 0,
 	CONSTRAINT [FK_tblSMDocumentConfiguration_tblSMDocumentSourceFolder] FOREIGN KEY ([intDocumentSourceFolderId]) REFERENCES [dbo].[tblSMDocumentSourceFolder] ([intDocumentSourceFolderId]),
+	CONSTRAINT [FK_tblSMDocumentConfiguration_tblSMScreenReport] FOREIGN KEY ([intScreenReportId]) REFERENCES [dbo].[tblSMScreenReport] ([intScreenReportId]),
 	CONSTRAINT [FK_tblSMDocumentConfiguration_tblSMScreen] FOREIGN KEY ([intScreenId]) REFERENCES [dbo].[tblSMScreen] ([intScreenId])
 )
