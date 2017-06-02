d5db9043d00816973b49c1712377542a70b809d8
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2017-06-02 21:35:01 +0530

    RM-706

--- i21Database/dbo/Views/vyuRKContractDetail.sql
+++ i21Database/dbo/Views/vyuRKContractDetail.sql
@@ -20,10 +20,11 @@ SELECT c.strCommodityCode,
 		intContractDetailId,
 		cd.intContractStatusId,
 		intEntityId
+		,cd.intCurrencyId
 FROM tblCTContractDetail cd
 	JOIN tblCTContractHeader ch on ch.intContractHeaderId=cd.intContractHeaderId AND cd.intContractStatusId <> 3
 	JOIN tblICCommodity c on ch.intCommodityId=c.intCommodityId
 	JOIN tblCTPricingType pt on pt.intPricingTypeId=cd.intPricingTypeId
 	JOIN tblCTContractType ct on ct.intContractTypeId=ch.intContractTypeId
 	JOIN tblSMCompanyLocation cl on cl.intCompanyLocationId		=	cd.intCompanyLocationId
-	JOIN tblICCommodityUnitMeasure ium on ium.intCommodityId=ch.intCommodityId AND cd.intUnitMeasureId=ium.intUnitMeasureId  
+	JOIN tblICCommodityUnitMeasure ium on ium.intCommodityId=ch.intCommodityId AND cd.intUnitMeasureId=ium.intUnitMeasureId 
