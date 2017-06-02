4e433b6e35d249412d54c6ebe63252787db3f9ba
Smith Wilson de Jesus <smith.dejesus@irely.com>
2017-06-03 04:18:34 +0800

    CM-1725 Scale Ticket with Storage settlement is not printing settlement report in pay voucher details

--- i21Database/dbo/Stored Procedures/uspCMCheckPrint_GetCheckSettlement.sql
+++ i21Database/dbo/Stored Procedures/uspCMCheckPrint_GetCheckSettlement.sql
@@ -14,8 +14,9 @@ SET ANSI_WARNINGS OFF
 
 
 --Check if there are any check overflow
+IF EXISTS (
 SELECT TOP 1
-@ysnCheckSettlement = 1	
+1 
 FROM	dbo.tblCMBankTransaction CHK 
 		INNER JOIN dbo.tblCMCheckPrintJobSpool PRINTSPOOL ON CHK.strTransactionId = PRINTSPOOL.strTransactionId
 			AND CHK.intBankAccountId = PRINTSPOOL.intBankAccountId
@@ -27,8 +28,33 @@ FROM	dbo.tblCMBankTransaction CHK
 		INNER JOIN tblICInventoryReceiptItem INVRCPTITEM ON BillDtl.intInventoryReceiptItemId = INVRCPTITEM.intInventoryReceiptItemId
 		INNER JOIN tblICInventoryReceipt INVRCPT ON INVRCPTITEM.intInventoryReceiptId = INVRCPT.intInventoryReceiptId
 		--INNER JOIN tblSCTicket TICKET ON INVRCPTITEM.intSourceId = TICKET.intTicketId			
-WHERE	CHK.intBankAccountId = @intBankAccountId
+WHERE	CHK.intBankAccountId = 3
 		AND CHK.strTransactionId = CHK.strTransactionId
 		AND INVRCPTITEM.intSourceId IS NOT NULL
 
+UNION ALL
+SELECT TOP 1
+1 
+FROM	dbo.tblCMBankTransaction CHK 
+		INNER JOIN dbo.tblCMCheckPrintJobSpool PRINTSPOOL ON CHK.strTransactionId = PRINTSPOOL.strTransactionId
+			AND CHK.intBankAccountId = PRINTSPOOL.intBankAccountId
+		INNER JOIN tblAPPayment PYMT ON CHK.strTransactionId = PYMT.strPaymentRecordNum
+		INNER JOIN tblAPPaymentDetail PYMTDTL ON PYMT.intPaymentId = PYMTDTL.intPaymentId
+		INNER JOIN tblAPBill Bill ON PYMTDTL.intBillId = Bill.intBillId
+		INNER JOIN tblAPBillDetail BillDtl ON Bill.intBillId = BillDtl.intBillId
+		INNER JOIN tblICItem Item ON BillDtl.intItemId = Item.intItemId
+		INNER JOIN tblGRStorageHistory StrgHstry ON Bill.intBillId = StrgHstry.intBillId
+		WHERE	CHK.intBankAccountId = 3
+		AND CHK.strTransactionId = CHK.strTransactionId
+)
+BEGIN
+SET @ysnCheckSettlement = 1
+END
+ELSE
+BEGIN
+SET @ysnCheckSettlement = 0
+END
 SET @ysnCheckSettlement = ISNULL(@ysnCheckSettlement, 0)
+
+
+
