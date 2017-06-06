ebf035280d3e74c1137e1e7a99f8fe25b8916958
Wendell Wayne Estrada <wendell.estrada@irely.com>
2017-06-06 17:30:28 +0800

    IC-3929 Error running predeployment due to collation issues

--- i21PreDeployment/Scripts/Pre-Deployment/11_IC/Remove_Accounts_With_Deleted_Category.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/11_IC/Remove_Accounts_With_Deleted_Category.sql
@@ -1,49 +1,49 @@
-﻿DECLARE @tblRemovedCategory TABLE (strName NVARCHAR(50) COLLATE Latin1_General_CI_AS)
+﻿EXEC(N'DECLARE @tblRemovedCategory TABLE (strName NVARCHAR(50) COLLATE Latin1_General_CI_AS)
 INSERT INTO @tblRemovedCategory(strName)
-SELECT'Begin Inventory'					COLLATE Latin1_General_CI_AS UNION 
-SELECT 'Broker Expense'					COLLATE Latin1_General_CI_AS UNION 
-SELECT 'Cash Over/Short'				COLLATE Latin1_General_CI_AS UNION
-SELECT 'Contract Equity'				COLLATE Latin1_General_CI_AS UNION
-SELECT 'Contract Purchase Gain/Loss'	COLLATE Latin1_General_CI_AS UNION
-SELECT 'Contract Sales Gain/Loss'		COLLATE Latin1_General_CI_AS UNION
-SELECT 'Credit Card Fee'				COLLATE Latin1_General_CI_AS UNION
-SELECT 'Currency Equity'				COLLATE Latin1_General_CI_AS UNION
-SELECT 'Currency Purchase Gain/Loss'	COLLATE Latin1_General_CI_AS UNION
-SELECT 'Currency Sales Gain/Loss'		COLLATE Latin1_General_CI_AS UNION
-SELECT 'Deferred Payable'				COLLATE Latin1_General_CI_AS UNION
-SELECT 'Deposit Account'				COLLATE Latin1_General_CI_AS UNION
-SELECT 'End Inventory'					COLLATE Latin1_General_CI_AS UNION
-SELECT 'Fee Income'						COLLATE Latin1_General_CI_AS UNION
-SELECT 'Freight AP Account'				COLLATE Latin1_General_CI_AS UNION
-SELECT 'Freight Expenses'				COLLATE Latin1_General_CI_AS UNION
-SELECT 'Freight Income'					COLLATE Latin1_General_CI_AS UNION
-SELECT 'Interest Expense'				COLLATE Latin1_General_CI_AS UNION 
-SELECT 'Options Expense'				COLLATE Latin1_General_CI_AS UNION
-SELECT 'Options Income'					COLLATE Latin1_General_CI_AS UNION
-SELECT 'Purchase Account'				COLLATE Latin1_General_CI_AS UNION
-SELECT 'Revalue Sold'					COLLATE Latin1_General_CI_AS UNION 
-SELECT 'Sales Adv Account'				COLLATE Latin1_General_CI_AS UNION 
-SELECT'Storage Expense'					COLLATE Latin1_General_CI_AS UNION 
-SELECT'Storage Income'					COLLATE Latin1_General_CI_AS UNION 
-SELECT 'Storage Receivable'				COLLATE Latin1_General_CI_AS UNION 
-SELECT'Variance Account'				COLLATE Latin1_General_CI_AS UNION 
-SELECT'Write-Off Sold'					COLLATE Latin1_General_CI_AS UNION 
-SELECT 'Fee Expense'					COLLATE Latin1_General_CI_AS UNION
-SELECT 'Auto-Variance'					COLLATE Latin1_General_CI_AS UNION
-SELECT 'Work In Progress'				COLLATE Latin1_General_CI_AS 
+SELECT ''Begin Inventory''					COLLATE Latin1_General_CI_AS UNION 
+SELECT ''Broker Expense''					COLLATE Latin1_General_CI_AS UNION 
+SELECT ''Cash Over/Short''				COLLATE Latin1_General_CI_AS UNION
+SELECT ''Contract Equity''				COLLATE Latin1_General_CI_AS UNION
+SELECT ''Contract Purchase Gain/Loss''	COLLATE Latin1_General_CI_AS UNION
+SELECT ''Contract Sales Gain/Loss''		COLLATE Latin1_General_CI_AS UNION
+SELECT ''Credit Card Fee''				COLLATE Latin1_General_CI_AS UNION
+SELECT ''Currency Equity''				COLLATE Latin1_General_CI_AS UNION
+SELECT ''Currency Purchase Gain/Loss''	COLLATE Latin1_General_CI_AS UNION
+SELECT ''Currency Sales Gain/Loss''		COLLATE Latin1_General_CI_AS UNION
+SELECT ''Deferred Payable''				COLLATE Latin1_General_CI_AS UNION
+SELECT ''Deposit Account''				COLLATE Latin1_General_CI_AS UNION
+SELECT ''End Inventory''					COLLATE Latin1_General_CI_AS UNION
+SELECT ''Fee Income''						COLLATE Latin1_General_CI_AS UNION
+SELECT ''Freight AP Account''				COLLATE Latin1_General_CI_AS UNION
+SELECT ''Freight Expenses''				COLLATE Latin1_General_CI_AS UNION
+SELECT ''Freight Income''					COLLATE Latin1_General_CI_AS UNION
+SELECT ''Interest Expense''				COLLATE Latin1_General_CI_AS UNION 
+SELECT ''Options Expense''				COLLATE Latin1_General_CI_AS UNION
+SELECT ''Options Income''					COLLATE Latin1_General_CI_AS UNION
+SELECT ''Purchase Account''				COLLATE Latin1_General_CI_AS UNION
+SELECT ''Revalue Sold''					COLLATE Latin1_General_CI_AS UNION 
+SELECT ''Sales Adv Account''				COLLATE Latin1_General_CI_AS UNION 
+SELECT ''Storage Expense''					COLLATE Latin1_General_CI_AS UNION 
+SELECT ''Storage Income''					COLLATE Latin1_General_CI_AS UNION 
+SELECT ''Storage Receivable''				COLLATE Latin1_General_CI_AS UNION 
+SELECT ''Variance Account''				COLLATE Latin1_General_CI_AS UNION 
+SELECT ''Write-Off Sold''					COLLATE Latin1_General_CI_AS UNION 
+SELECT ''Fee Expense''					COLLATE Latin1_General_CI_AS UNION
+SELECT ''Auto-Variance''					COLLATE Latin1_General_CI_AS UNION
+SELECT ''Work In Progress''				COLLATE Latin1_General_CI_AS 
 
-IF(EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'tblGLAccountCategory') AND EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'tblICCategoryAccount'))
+IF(EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = ''tblGLAccountCategory'') AND EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = ''tblICCategoryAccount''))
 BEGIN
 DELETE a
 FROM tblGLAccountCategory ac
-	INNER JOIN @tblRemovedCategory rc ON rc.strName = ac.strAccountCategory
+	INNER JOIN @tblRemovedCategory rc ON rc.strName COLLATE Latin1_General_CI_AS = ac.strAccountCategory COLLATE Latin1_General_CI_AS
 	INNER JOIN tblICCategoryAccount a ON a.intAccountCategoryId = ac.intAccountCategoryId
 END
 
-IF(EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'tblGLAccountCategory') AND EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'tblICItemAccount'))
+IF(EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = ''tblGLAccountCategory'') AND EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = ''tblICItemAccount''))
 BEGIN
 DELETE a
 FROM tblGLAccountCategory ac
-	INNER JOIN @tblRemovedCategory rc ON rc.strName = ac.strAccountCategory
+	INNER JOIN @tblRemovedCategory rc ON rc.strName COLLATE Latin1_General_CI_AS = ac.strAccountCategory COLLATE Latin1_General_CI_AS
 	INNER JOIN tblICItemAccount a ON a.intAccountCategoryId = ac.intAccountCategoryId
-END
+END')
