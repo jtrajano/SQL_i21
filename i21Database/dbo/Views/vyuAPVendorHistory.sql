d00dff2f3907ddb6cd0acccbc65b31aae883225b
Lex Eugene Peregrino <lex.peregrino@irely.com>
2017-06-07 05:38:43 +0800

    AP-3662 - Vouchers show twice on Vendor History 1.0

--- i21Database/dbo/Views/vyuAPVendorHistory.sql
+++ i21Database/dbo/Views/vyuAPVendorHistory.sql
@@ -16,33 +16,40 @@ SELECT
 	,strBillId = A.strBillId
 	,strInvoiceNumber = strVendorOrderNumber
 	,dblTotal = (CASE WHEN A.intTransactionType != 1 AND A.dblTotal > 0 THEN A.dblTotal * -1 ELSE ISNULL(A.dblTotal,0) END)
-	,dblDiscount = ISNULL(B.dblDiscount, 0)
-	,dblWithheld = ISNULL(B.dblWithheld, 0)
-	,dblAmountPaid = CASE WHEN A.ysnPaid = 1 THEN A.dblTotal ELSE ISNULL(SUM(B.dblPayment),0) END  
+	,dblDiscount = ISNULL(Payment.dblDiscount, 0)
+	,dblWithheld = ISNULL(Payment.dblWithheld, 0)
+	,dblAmountPaid = CASE WHEN A.ysnPaid = 1 THEN A.dblTotal ELSE ISNULL(SUM(Payment.dblPayment),0) END  
 	,A.ysnPaid
 	,dblAmountDue = (CASE WHEN A.intTransactionType != 1 AND A.dblAmountDue > 0 THEN A.dblAmountDue * -1 ELSE ISNULL(A.dblAmountDue,0) END) 
 	,A.ysnPosted
-	,B1.strPaymentInfo
-	,B1.dtmDatePaid
-	,B1.intPaymentId
+	,Payment.strPaymentInfo
+	,Payment.dtmDatePaid
+	,Payment.intPaymentId
 FROM dbo.tblAPBill A
-		LEFT JOIN (dbo.tblAPPayment B1 
-						INNER JOIN dbo.tblAPPaymentDetail B ON B1.intPaymentId = B.intPaymentId
-						LEFT JOIN dbo.tblCMBankTransaction C ON B1.strPaymentRecordNum = C.strTransactionId)
-		 ON A.intBillId = B.intBillId
+		--LEFT JOIN (dbo.tblAPPayment B1 
+		--				INNER JOIN dbo.tblAPPaymentDetail B ON B1.intPaymentId = B.intPaymentId
+		--				LEFT JOIN dbo.tblCMBankTransaction C ON B1.strPaymentRecordNum = C.strTransactionId)
+		-- ON A.intBillId = B.intBillId
+		 OUTER APPLY(
+			SELECT TOP 1 B1.strPaymentInfo,
+						 B1.dtmDatePaid,
+						 B1.intPaymentId,
+						 B.dblDiscount,
+						 B.dblWithheld,
+						 B.dblPayment FROM dbo.tblAPPayment B1
+			INNER JOIN dbo.tblAPPaymentDetail B ON B1.intPaymentId = B.intPaymentId
+			LEFT JOIN dbo.tblCMBankTransaction C ON B1.strPaymentRecordNum = C.strTransactionId 
+			WHERE B.intBillId = A.intBillId
+			ORDER BY dtmDatePaid DESC
+		 )  Payment     
 		LEFT JOIN dbo.tblAPVendor
 			ON tblAPVendor.intEntityVendorId = A.[intEntityVendorId]
---WHERE 
---1 = CASE WHEN B1.intPaymentId IS NULL 
---		THEN 1
---		ELSE
---			(CASE WHEN B1.ysnPosted = 1 OR C.strTransactionId IS NOT NULL THEN 1 ELSE 0 END)
---		END 
+
 GROUP BY A.intBillId,
 	A.dtmDate,
 	A.dblTotal,
-	B.dblDiscount,
-	B.dblWithheld,
+	Payment.dblDiscount,
+	Payment.dblWithheld,
 	tblAPVendor.intEntityVendorId,
 	tblAPVendor.strVendorId,
 	tblAPVendor.intEntityVendorId,
@@ -52,7 +59,7 @@ GROUP BY A.intBillId,
 	A.ysnPaid,
 	A.dblAmountDue,
     A.ysnPosted,
-	B1.strPaymentInfo,
-	B1.intPaymentId,
+	Payment.strPaymentInfo,
+	Payment.intPaymentId,
 	A.intTransactionType,
-	B1.dtmDatePaid
+	Payment.dtmDatePaid
