b9fd6038b697bf62a7aeab0c63db06d3f5665acf
bonn ponce <bonn.ponce@irely.com>
2017-06-06 17:26:59 +0800

    CF-1097

--- i21Database/dbo/Views/vyuCFAccountCreditThreshold.sql
+++ i21Database/dbo/Views/vyuCFAccountCreditThreshold.sql
@@ -1,10 +1,39 @@
-﻿CREATE VIEW dbo.vyuCFAccountCreditThreshold
+﻿CREATE VIEW [dbo].[vyuCFAccountCreditThreshold]
 AS
-SELECT mainQuery.intEntityCustomerId, mainQuery.strCustomerName, mainQuery.strCustomerNumber, mainQuery.dblCreditLimit, mainQuery.dblTotalDue, mainQuery.dblTotalAR, mainQuery.dblOverLimit, ARCust.strCreditCode, CFAccnt.intAccountId, 
-             CASE WHEN (dblOverLimit > 0 AND dblTotalDue > 0) THEN 'Both' WHEN (dblOverLimit <= 0 AND dblTotalDue > 0) THEN 'Past Due' WHEN (dblOverLimit > 0 AND dblTotalDue <= 0) THEN 'Over Credit Limit' END AS strReason
-FROM   (SELECT intEntityCustomerId, strCustomerName, strCustomerNumber, dblCreditLimit, dblTotalDue, dblTotalDue + dbl0Days AS dblTotalAR, CASE WHEN ((dblTotalDue + dbl0Days) - dblCreditLimit) <= 0 THEN 0 WHEN ((dblTotalDue + dbl0Days) - dblCreditLimit) 
-                           > 0 THEN ((dblTotalDue + dbl0Days) - dblCreditLimit) END AS dblOverLimit
-             FROM    dbo.vyuARCustomerInquiry) AS mainQuery INNER JOIN
+SELECT 
+mainQuery.intEntityCustomerId, 
+mainQuery.strCustomerName, 
+mainQuery.strCustomerNumber,
+mainQuery.dblCreditLimit, 
+mainQuery.dblTotalDue, 
+mainQuery.dblTotalAR, 
+mainQuery.dblOverLimit, 
+ARCust.strCreditCode, 
+CFAccnt.intAccountId, 
+CASE 
+WHEN (dblOverLimit > 0 AND dblTotalDue > 0) 
+	THEN 'Both' 
+WHEN (dblOverLimit <= 0 AND dblTotalDue > 0) 
+	THEN 'Past Due' 
+WHEN (dblOverLimit > 0 AND dblTotalDue <= 0) 
+	THEN 'Over Credit Limit' END AS strReason
+
+FROM   (
+SELECT 
+intEntityCustomerId, 
+strCustomerName, 
+strCustomerNumber, 
+dblCreditLimit, 
+dblTotalDue AS dblTotalAR, 
+dbl10Days + dbl30Days + dbl60Days + dbl90Days + dbl91Days AS dblTotalDue, 
+CASE 
+WHEN ((dblTotalDue) - dblCreditLimit) <= 0 
+THEN 0 
+WHEN ((dblTotalDue) - dblCreditLimit) > 0 
+THEN ((dblTotalDue) - dblCreditLimit) 
+END AS dblOverLimit
+             
+			 FROM    dbo.vyuARCustomerInquiry) AS mainQuery INNER JOIN
              dbo.tblARCustomer AS ARCust ON mainQuery.intEntityCustomerId = ARCust.intEntityCustomerId INNER JOIN
              dbo.tblCFAccount AS CFAccnt ON mainQuery.intEntityCustomerId = CFAccnt.intCustomerId INNER JOIN
                  (SELECT DISTINCT intAccountId
@@ -12,3 +41,4 @@ FROM   (SELECT intEntityCustomerId, strCustomerName, strCustomerNumber, dblCredi
                   WHERE (ysnCardLocked = 0)) AS CFCard ON CFAccnt.intAccountId = CFCard.intAccountId
 WHERE (mainQuery.dblOverLimit > 0) OR
              (mainQuery.dblTotalDue > 0)
+GO
