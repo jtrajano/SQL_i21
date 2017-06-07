7cc3845ee67d8412e1c3ddb437aedcff6485ebf2
Wendell Wayne Estrada <wendell.estrada@irely.com>
2017-06-07 13:46:27 +0800

    IC-3893 User can not count more than 50 pallets in a selected category in the inventory count screen

---
+++ i21Database/dbo/Stored Procedures/uspICUpdateInventoryCountDetails.sql
@@ -0,0 +1,105 @@
+﻿CREATE PROCEDURE [dbo].[uspICUpdateInventoryCountDetails]
+	  @intInventoryCountId INT
+	, @intEntityUserSecurityId INT
+	, @strHeaderNo NVARCHAR(50)
+	, @intLocationId INT = 0
+	, @intCategoryId INT = 0
+	, @intCommodityId INT = 0
+	, @intCountGroupId INT = 0
+	, @intSubLocationId INT = 0
+	, @intStorageLocationId INT = 0
+	, @ysnIncludeZeroOnHand BIT = 0
+	, @ysnCountByLots BIT = 0
+AS
+
+SET QUOTED_IDENTIFIER OFF
+SET ANSI_NULLS ON
+SET NOCOUNT ON
+SET XACT_ABORT ON
+SET ANSI_WARNINGS OFF
+
+DELETE FROM tblICInventoryCountDetail
+WHERE intInventoryCountId = @intInventoryCountId
+
+IF @ysnCountByLots = 1
+BEGIN
+	INSERT INTO tblICInventoryCountDetail(
+		  intInventoryCountId
+		, intItemId
+		, intItemLocationId
+		, intSubLocationId
+		, intStorageLocationId
+		, intLotId
+		, dblSystemCount
+		, dblLastCost
+		, strCountLine
+		, intItemUOMId
+		, ysnRecount
+		, intEntityUserSecurityId
+		, intConcurrencyId
+		, intSort)
+	SELECT 
+		  @intInventoryCountId
+		, intItemId
+		, intItemLocationId
+		, intSubLocationId
+		, intStorageLocationId
+		, intLotId
+		, dblSystemCount = dblOnHand
+		, dblLastCost
+		, strCountLine = @strHeaderNo + '-' + CAST(ROW_NUMBER() OVER(ORDER BY intKey ASC) AS NVARCHAR(50))
+		, intItemUOMId
+		, ysnRecount = 0
+		, intEntityUserSecurityId = @intEntityUserSecurityId
+		, intConcurrencyId = 1
+		, intSort = 1
+	FROM vyuICGetItemStockSummaryByLot
+	WHERE (intLocationId = @intLocationId OR ISNULL(@intLocationId, 0) = 0)
+		AND (intCategoryId = @intCategoryId OR ISNULL(@intCategoryId, 0) = 0)
+		AND (intCommodityId = @intCommodityId OR ISNULL(@intCommodityId, 0) = 0)
+		AND (intCountGroupId = @intCountGroupId OR ISNULL(@intCountGroupId, 0) = 0)
+		AND (intSubLocationId = @intSubLocationId OR ISNULL(@intSubLocationId, 0) = 0)
+		AND (intStorageLocationId = @intStorageLocationId OR ISNULL(@intStorageLocationId, 0) = 0)
+		AND ((dblOnHand > 0 AND @ysnIncludeZeroOnHand = 0) OR (@ysnIncludeZeroOnHand = 1))
+END
+ELSE
+BEGIN
+	INSERT INTO tblICInventoryCountDetail(
+		  intInventoryCountId
+		, intItemId
+		, intItemLocationId
+		, intSubLocationId
+		, intStorageLocationId
+		, intLotId
+		, dblSystemCount
+		, dblLastCost
+		, strCountLine
+		, intItemUOMId
+		, ysnRecount
+		, intEntityUserSecurityId
+		, intConcurrencyId
+		, intSort)
+	SELECT 
+		  @intInventoryCountId
+		, intItemId
+		, intItemLocationId
+		, intSubLocationId
+		, intStorageLocationId
+		, intLotId = NULL
+		, dblSystemCount = dblOnHand
+		, dblLastCost
+		, strCountLine = @strHeaderNo + '-' + CAST(ROW_NUMBER() OVER(ORDER BY intKey ASC) AS NVARCHAR(50))
+		, intItemUOMId
+		, ysnRecount = 0
+		, intEntityUserSecurityId = @intEntityUserSecurityId
+		, intConcurrencyId = 1
+		, intSort = 1
+	FROM vyuICGetItemStockSummary
+	WHERE (intLocationId = @intLocationId OR ISNULL(@intLocationId, 0) = 0)
+		AND (intCategoryId = @intCategoryId OR ISNULL(@intCategoryId, 0) = 0)
+		AND (intCommodityId = @intCommodityId OR ISNULL(@intCommodityId, 0) = 0)
+		AND (intCountGroupId = @intCountGroupId OR ISNULL(@intCountGroupId, 0) = 0)
+		AND (intSubLocationId = @intSubLocationId OR ISNULL(@intSubLocationId, 0) = 0)
+		AND (intStorageLocationId = @intStorageLocationId OR ISNULL(@intStorageLocationId, 0) = 0)
+		AND ((dblOnHand > 0 AND @ysnIncludeZeroOnHand = 0) OR (@ysnIncludeZeroOnHand = 1))
+END
