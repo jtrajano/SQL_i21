713590c2cfda5842b1d21c75242bf2f107af6cb1
Michelle Jardiolin <michelle.jardiolin@irely.com>
2017-06-08 16:12:50 +0800

    AR-4820

---
+++ i21PreDeployment/Scripts/Pre-Deployment/7_AR/3_InvoiceSetDefaultContact.sql
@@ -0,0 +1,11 @@
+﻿IF(EXISTS(SELECT NULL FROM sys.tables WHERE name = N'tblARInvoice') AND EXISTS(SELECT NULL FROM sys.columns WHERE [name] = N'intEntityContactId' AND [object_id] = OBJECT_ID(N'tblARInvoice')))
+BEGIN
+ 
+UPDATE 
+	tblARInvoice   
+SET 
+	[intEntityContactId] = [dbo].[fnARGetCustomerDefaultContact](intEntityCustomerId)
+WHERE
+	ISNULL([intEntityContactId], 0) = 0
+
+END
