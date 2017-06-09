771418d06823dec515cc266cd189332a19de31ac
Lex Eugene Peregrino <lex.peregrino@irely.com>
2017-06-09 22:32:17 +0800

    AP-3791 - [Script] - Publish Error on AP FnAPValidatePostBill 1.0
    (cherry picked from commit d5758078aa1a3174cf2db13d5bce6bdd68d2a7bb)

--- i21Database/dbo/Functions/fnAPValidatePostBill.sql
+++ i21Database/dbo/Functions/fnAPValidatePostBill.sql
@@ -273,9 +273,11 @@ BEGIN
 		FROM tblAPBill A 
 		CROSS APPLY (
 			SELECT
-				SUM(CASE WHEN A.intTransactionType != 1 THEN B.dblTotal * -1 ELSE B.dblTotal END) 
-					+ SUM(CASE WHEN A.intTransactionType != 1 THEN B.dblTax * -1 ELSE B.dblTax END)
-			FROM tblAPBillDetail B WHERE B.intBillId = A.intBillId
+				SUM(CASE WHEN C.intTransactionType != 1 THEN B.dblTotal * -1 ELSE B.dblTotal END) 
+					+ SUM(CASE WHEN C.intTransactionType != 1 THEN B.dblTax * -1 ELSE B.dblTax END) AS dblTotal
+			FROM tblAPBillDetail B 
+			INNER JOIN tblAPBill C ON B.intBillId = C.intBillId
+			WHERE B.intBillId = A.intBillId
 		) details
 		WHERE A.intBillId IN (SELECT [intBillId] FROM @tmpBills)
 			
