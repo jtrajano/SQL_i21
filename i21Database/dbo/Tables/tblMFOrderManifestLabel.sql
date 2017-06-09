7751e555c7f5ab78e86530c9817af259493fbcd6
Vinoth Kumar T <vinoth.t@irely.com>
2017-06-09 21:45:08 +0530

    MFG-2488

--- i21Database/dbo/Tables/tblMFOrderManifestLabel.sql
+++ i21Database/dbo/Tables/tblMFOrderManifestLabel.sql
@@ -5,6 +5,12 @@
 	,intCustomerLabelTypeId INT NOT NULL
 	,strSSCCNo NVARCHAR(MAX) COLLATE Latin1_General_CI_AS
 	,ysnPrinted BIT NOT NULL CONSTRAINT [DF_tblMFOrderManifestLabel_ysnActive] DEFAULT 0
+	,strBarcode1 NVARCHAR(MAX) COLLATE Latin1_General_CI_AS
+	,strBarcodeLabel1 NVARCHAR(MAX) COLLATE Latin1_General_CI_AS
+	,strBarcode2 NVARCHAR(MAX) COLLATE Latin1_General_CI_AS
+	,strBarcodeLabel2 NVARCHAR(MAX) COLLATE Latin1_General_CI_AS
+	,strBarcode3 NVARCHAR(MAX) COLLATE Latin1_General_CI_AS
+	,strBarcodeLabel3 NVARCHAR(MAX) COLLATE Latin1_General_CI_AS
 
 	,CONSTRAINT PK_tblMFOrderManifestLabel PRIMARY KEY (intOrderManifestLabelId)
 	,CONSTRAINT FK_tblMFOrderManifestLabel_tblMFOrderManifest FOREIGN KEY (intOrderManifestId) REFERENCES tblMFOrderManifest(intOrderManifestId) ON DELETE CASCADE
