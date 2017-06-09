4c83a5b97c1afb4f0c7eea4c67bcbbc8b7d8230f
Lex Eugene Peregrino <lex.peregrino@irely.com>
2017-06-10 00:10:24 +0800

    AP-3738 - [Pay Voucher Detail] - Update validation for Print Settlement Button 1.0
    (cherry picked from commit 5ded5c93f5e7246a4f9395733fb097abc2cf7a61)
    
    # Conflicts:
    #	i21Database/i21Database.sqlproj

---
+++ i21Database/dbo/Functions/fnAPGetSettlementStatus.sql
@@ -0,0 +1,44 @@
+﻿CREATE FUNCTION [dbo].[fnAPGetSettlementStatus]
+(
+	@strTransactionId NVARCHAR(100) = NULL
+)
+RETURNS BIT
+AS
+BEGIN
+	
+	DECLARE @hasSettlement BIT = 0;
+
+	IF EXISTS( 
+				SELECT TOP 1 1
+				FROM dbo.tblCMBankTransaction CHK 
+				  INNER JOIN tblAPPayment PYMT ON CHK.strTransactionId = PYMT.strPaymentRecordNum
+				  INNER JOIN tblAPPaymentDetail PYMTDTL ON PYMT.intPaymentId = PYMTDTL.intPaymentId
+				  INNER JOIN tblAPBill Bill ON PYMTDTL.intBillId = Bill.intBillId
+				  INNER JOIN tblAPBillDetail BillDtl ON Bill.intBillId = BillDtl.intBillId
+				  INNER JOIN tblICItem Item ON BillDtl.intItemId = Item.intItemId
+				  INNER JOIN tblICInventoryReceiptItem INVRCPTITEM ON BillDtl.intInventoryReceiptItemId = INVRCPTITEM.intInventoryReceiptItemId
+				  INNER JOIN tblICInventoryReceipt INVRCPT ON INVRCPTITEM.intInventoryReceiptId = INVRCPT.intInventoryReceiptId
+				WHERE  CHK.strTransactionId = CHK.strTransactionId
+				  AND INVRCPTITEM.intSourceId IS NOT NULL 
+				  AND PYMT.strPaymentRecordNum = @strTransactionId
+
+				UNION ALL
+				SELECT TOP 1 1 
+				FROM dbo.tblCMBankTransaction CHK 
+				  INNER JOIN tblAPPayment PYMT ON CHK.strTransactionId = PYMT.strPaymentRecordNum
+				  INNER JOIN tblAPPaymentDetail PYMTDTL ON PYMT.intPaymentId = PYMTDTL.intPaymentId
+				  INNER JOIN tblAPBill Bill ON PYMTDTL.intBillId = Bill.intBillId
+				  INNER JOIN tblAPBillDetail BillDtl ON Bill.intBillId = BillDtl.intBillId
+				  INNER JOIN tblICItem Item ON BillDtl.intItemId = Item.intItemId
+				  INNER JOIN tblGRStorageHistory StrgHstry ON Bill.intBillId = StrgHstry.intBillId
+				  WHERE CHK.strTransactionId = CHK.strTransactionId AND PYMT.strPaymentRecordNum = @strTransactionId
+			)
+	BEGIN
+		SET @hasSettlement = 1
+	END
+
+	RETURN @hasSettlement;
+
+END
+
+GO
