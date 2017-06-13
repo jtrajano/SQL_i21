6535e8df18ee89a2b645b3defda7af751cad78f7
jeffrey trajano <jeffrey.trajano@irely.com>
2017-06-13 16:35:57 +0800

    GL-2951 We need to be able to consolidate the financials of multiple i21 companies

--- i21Database/dbo/Stored Procedures/uspGLGetConsolidateWarning.sql
+++ i21Database/dbo/Stored Procedures/uspGLGetConsolidateWarning.sql
@@ -7,21 +7,21 @@
 AS
 DECLARE @strCommand NVARCHAR(MAX)
 SET @strCommand ='
-		DECLARE @ysnOpen BIT, @ysnWithUnpostedTrans BIT, @intFiscalYearId INT,@intFiscalPeriodId INT
-		SELECT @ysnOpen = 0 , @ysnWithUnpostedTrans = 0
-		use [consolidatingDb]
+		DECLARE @ysnOpen BIT, @ysnUnpostedTrans BIT, @intFiscalYearId INT,@intFiscalPeriodId INT
+		SELECT @ysnOpen = 0 , @ysnUnpostedTrans = 0
+     	use [consolidatingDb]
 		SELECT TOP 1
-		@ysnOpen= ysnOpen,
+		@ysnOpen=  ysnOpen,
 		@intFiscalYearId = intFiscalYearId,
 		@intFiscalPeriodId = intGLFiscalYearPeriodId
 		FROM dbo.tblGLFiscalYearPeriod
-		WHERE ''[dtmDateConsolidate]'' 
+		WHERE ''[dtmDateConsolidate]''
 		BETWEEN dtmStartDate and dtmEndDate
 		exec dbo.uspGLGetAllUnpostedTransactionsByFiscal @intFiscalYearId,
 		1,
 		@intFiscalPeriodId,
-		''All'' ,@ysnWithUnpostedTrans OUT
-		SELECT @ysnOpen ysnOpen, @ysnWithUnpostedTrans ysnWithUnposted'
+		''GL'' ,@ysnUnpostedTrans OUT
+		SELECT @ysnOpen ysnFiscalOpen, @ysnUnpostedTrans ysnUnpostedTrans'
 SELECT @strCommand = REPLACE ( REPLACE (@strCommand,'[dtmDateConsolidate]', @dtmDate), '[consolidatingDb]', @dbName)
 EXEC sp_executesql @strCommand
 
