03d1747a4a54168c425f2537cd615dec7c37192d
Dan Michael Jalimao <dan.jalimao@irely.com>
2017-06-13 16:23:06 +0800

    SM-2916

---
+++ i21Database/dbo/Stored Procedures/uspSMSetMenuAvailability.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[uspSMSetMenuAvailability]
+@menus NVARCHAR(MAX),
+@module NVARCHAR(50),
+@enable BIT
+AS
+BEGIN
+	IF EXISTS (SELECT TOP 1 1 FROM tblSMMasterMenu WHERE strMenuName IN (SELECT Item COLLATE Latin1_General_CI_AS FROM dbo.fnSplitString(@menus, ',')) AND strModuleName = @module)
+	BEGIN
+		UPDATE tblSMUserRoleMenu SET ysnVisible = @enable WHERE intMenuId IN (SELECT intMenuID FROM tblSMMasterMenu WHERE strMenuName IN (SELECT Item COLLATE Latin1_General_CI_AS FROM dbo.fnSplitString(@menus, ',')) AND strModuleName = @module)
+	END
+END
