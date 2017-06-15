6000d8768437a0bdd9e8f2331d8dd68e193d3e6b
kenneth lagunero <kenneth.lagunero@irely.com>
2017-06-15 14:15:59 +0800

    Merge issues fix

--- i21Database/dbo/Views/vyuTFGetReportingComponentConfiguration.sql
+++ i21Database/dbo/Views/vyuTFGetReportingComponentConfiguration.sql
@@ -18,6 +18,5 @@ SELECT intReportingComponentConfigurationId
 	, strLastIndexOf
 	, strSegment
 	, intConfigurationSequence
-	, RC.intTaxAuthorityId
 FROM tblTFReportingComponentConfiguration RCC
 LEFT JOIN tblTFReportingComponent RC ON RC.intReportingComponentId = RCC.intReportingComponentId
