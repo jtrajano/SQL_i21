a21a0642543a143dd808f2681f0ba22fbd373a24
Michelle Jardiolin <michelle.jardiolin@irely.com>
2017-06-16 12:45:27 +0800

    AR-4853

--- i21PreDeployment/Scripts/Pre-Deployment/PreDeployment.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/PreDeployment.sql
@@ -60,6 +60,7 @@
 
 -- AR
 :r .\7_AR\3_fnARGetCustomerDefaultContact.sql
+:r .\7_AR\4_UpdateInvoicePaymentMethod.sql
 :r .\7_AR\5_InvoiceCurrencyCleanUp.sql
 :r .\7_AR\6_PaymentDetailInvoiceCleanUp.sql
 :r .\7_AR\7_CompanyPreferenceCleanUp.sql
