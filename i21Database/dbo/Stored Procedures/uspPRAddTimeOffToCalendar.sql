2fd4b355b9354eaa2399adf84fc89bccd53ccb9b
Arman Arenas <arman.arenas@irely.com>
2017-06-16 18:04:35 +0800

    PR-1210 populate intSource field on process time approval and posting time off request

--- i21Database/dbo/Stored Procedures/uspPRAddTimeOffToCalendar.sql
+++ i21Database/dbo/Stored Procedures/uspPRAddTimeOffToCalendar.sql
@@ -175,6 +175,7 @@ SELECT @intTimeOffRequestId = @intTransactionId
 					,dblTotal
 					,dtmDateFrom
 					,dtmDateTo
+					,intSource
 					,intSort
 					,intConcurrencyId
 				)
@@ -201,6 +202,7 @@ SELECT @intTimeOffRequestId = @intTransactionId
 								END
 					,TOR.dtmDateFrom
 					,TOR.dtmDateTo
+					,4
 					,1
 					,1
 				FROM
@@ -212,6 +214,7 @@ SELECT @intTimeOffRequestId = @intTransactionId
 
 				/* Check if the corresponding Linked Earning to deduct exists in the Pay Group Detail */
 				DECLARE @intPayGroupDetail INT = NULL
+				DECLARE @intSource INT = 1
 				SELECT TOP 1 @intPayGroupDetail = intPayGroupDetailId FROM tblPRPayGroupDetail PGD 
 							INNER JOIN tblPREmployeeEarning EL
 								ON PGD.intEmployeeEarningId = EL.intEmployeeEarningId
@@ -222,6 +225,7 @@ SELECT @intTimeOffRequestId = @intTransactionId
 									AND EE.intEmployeeTimeOffId = TOR.intTypeTimeOffId
 									AND EL.intPayGroupId IS NOT NULL
 									AND PGD.dtmDateFrom <= ISNULL(TOR.dtmDateFrom, PGD.dtmDateFrom) AND PGD.dtmDateTo >= ISNULL(TOR.dtmDateFrom, PGD.dtmDateTo)
+									AND intSource IN (0, 3)
 
 				IF (@intPayGroupDetail IS NULL)
 					INSERT INTO tblPRPayGroupDetail (
@@ -237,6 +241,7 @@ SELECT @intTimeOffRequestId = @intTransactionId
 						,dblTotal
 						,dtmDateFrom
 						,dtmDateTo
+						,intSource
 						,intSort
 						,intConcurrencyId
 					)
@@ -263,6 +268,7 @@ SELECT @intTimeOffRequestId = @intTransactionId
 									END
 						,TOR.dtmDateFrom
 						,TOR.dtmDateTo
+						,0
 						,1
 						,1
 					FROM 
@@ -296,7 +302,7 @@ SELECT @intTimeOffRequestId = @intTransactionId
 								AND EE.intEmployeeTimeOffId = TOR.intTypeTimeOffId
 								AND EL.intPayGroupId IS NOT NULL
 					WHERE 
-						tblPRPayGroupDetail.intPayGroupDetailId = @intPayGroupDetail
+						tblPRPayGroupDetail.intPayGroupDetailId = @intPayGroupDetail AND tblPRPayGroupDetail.intSource = 0
 			END
 			ELSE
 			BEGIN
