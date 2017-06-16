e9a28d53e79a01f9b8fb9fe0ab0525f20206c99f
Feb Montefrio <feb.montefrio@irely.com>
2017-06-16 17:28:51 +0800

    IC-3987

--- i21Database/dbo/Stored Procedures/uspICProcessToBill.sql
+++ i21Database/dbo/Stored Procedures/uspICProcessToBill.sql
@@ -2,9 +2,7 @@
 	@intReceiptId int,
 	@intUserId int,
 	@intBillId int OUTPUT,
-	@strBillIds NVARCHAR(MAX) = NULL OUTPUT,
-	@strPrefix NVARCHAR(10) = ',',
-	@strSuffix NVARCHAR(10) = ''
+	@strBillIds NVARCHAR(MAX) = NULL OUTPUT
 AS
 SET QUOTED_IDENTIFIER OFF
 SET ANSI_NULLS ON
@@ -15,29 +13,33 @@ SET XACT_ABORT ON
 DECLARE @ErrorMessage NVARCHAR(4000);
 DECLARE @ErrorSeverity INT;
 DECLARE @ErrorState INT;
-
---BEGIN TRY
 	
-	CREATE TABLE #tmpBillIds (
-		[intBillId] [INT] PRIMARY KEY,
-		[intInventoryReceiptId] [INT],
-		[intEntityVendorId] INT
+CREATE TABLE #tmpBillIds (
+	[intBillId] [INT] PRIMARY KEY,
+	[intInventoryReceiptId] [INT],
+	[intEntityVendorId] INT
+)
+
+INSERT INTO #tmpBillIds
+EXEC dbo.[uspAPCreateBillFromIR] 
+	@intReceiptId,
+	@intUserId
+
+SELECT TOP 1 @intBillId = intBillId FROM #tmpBillIds
+
+SELECT @strBillIds = 
+	LTRIM(
+		STUFF(
+				' ' + (
+					SELECT  CONVERT(NVARCHAR(50), intBillId) + '|^|'
+					FROM	#tmpBillIds
+					ORDER BY intBillId
+					FOR xml path('')
+				)
+			, 1
+			, 1
+			, ''
+		)
 	)
 
-	INSERT INTO #tmpBillIds
-	EXEC dbo.[uspAPCreateBillFromIR] 
-		@intReceiptId,
-		@intUserId
-
-	SELECT TOP 1 @intBillId = intBillId FROM #tmpBillIds
-
-	SELECT @strBillIds =
-	STUFF((
-		SELECT ISNULL(@strPrefix, ',') + CAST(ISNULL(intBillId, '') AS NVARCHAR(50)) + ISNULL(@strSuffix, '')
-		FROM #tmpBillIds
-		ORDER BY intBillId
-		FOR XML PATH(''),
-		TYPE
-	).value('.', 'NVARCHAR(MAX)'), 1, 1, '')
-
-	DROP TABLE #tmpBillIds
+DROP TABLE #tmpBillIds
