4bf7f62d4ba135e5f21aa70d2e4812dbe55b0f1e
Feb Montefrio <feb.montefrio@irely.com>
2017-06-19 13:43:37 +0800

    IC-3984

---
+++ i21Database/dbo/Stored Procedures/uspICGetUnitConversion.sql
@@ -0,0 +1,15 @@
+﻿/*
+	This sp returns the converion of a Unit Measure id to another Unit Measure Id. 
+*/
+
+CREATE PROCEDURE [dbo].[uspICGetUnitConversion]
+	@fromUnitMeasureId AS INT 
+	,@toUnitMeasureId AS INT 
+	,@result AS NUMERIC(38,20) OUTPUT 
+AS
+
+SELECT	TOP 1 
+		@result = dblConversionToStock 
+FROM	tblICUnitMeasureConversion UOMConversion
+WHERE	intUnitMeasureId = @fromUnitMeasureId
+		AND intStockUnitMeasureId = @toUnitMeasureId
