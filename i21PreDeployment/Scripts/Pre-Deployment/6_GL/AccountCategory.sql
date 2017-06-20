c32cb0f099d0d999b95f5240097736e37d4094e5
jeffrey trajano <jeffrey.trajano@irely.com>
2017-06-20 16:09:19 +0800

    GL-3549 Pre deployment error in 17.2Prod (.39)

--- i21PreDeployment/Scripts/Pre-Deployment/6_GL/AccountCategory.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/6_GL/AccountCategory.sql
@@ -9,5 +9,7 @@
                SELECT * FROM [$(TableName)]					
 --------------------------------------------------------------------------------------
 */
+GO
 IF EXISTS (SELECT TOP 1 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[tblGLAccountCategory]') AND type in (N'U'))
 ALTER TABLE dbo.tblGLAccountCategory ALTER COLUMN strAccountCategory  [nvarchar](50) COLLATE Latin1_General_CI_AS NULL;
+GO
