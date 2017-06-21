e7b71a0b6b5555805b6fe0072a8b6021951fd781
bonn ponce <bonn.ponce@irely.com>
2017-06-21 11:08:46 +0800

    CF-1120

--- i21Database/dbo/Tables/tblCFExportTaxMapping.sql
+++ i21Database/dbo/Tables/tblCFExportTaxMapping.sql
@@ -1,9 +1,11 @@
 ﻿CREATE TABLE [dbo].[tblCFExportTaxMapping] (
-    [intExportTaxMappingId] INT           IDENTITY (1, 1) NOT NULL,
-    [strExportTaxMapping]   NVARCHAR (50) NOT NULL,
-    [intImportFileHeaderId] INT           NOT NULL,
-    [intConcurrencyId]      INT           CONSTRAINT [DF_tblCFExportTaxMapping_intConcurrencyId] DEFAULT ((1)) NULL,
+    [intExportTaxMappingId] INT            IDENTITY (1, 1) NOT NULL,
+    [strExportTaxMapping]   NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NULL,
+    [intImportFileHeaderId] INT            NOT NULL,
+    [intConcurrencyId]      INT            CONSTRAINT [DF_tblCFExportTaxMapping_intConcurrencyId] DEFAULT ((1)) NULL,
     CONSTRAINT [PK_tblCFExportTaxMapping] PRIMARY KEY CLUSTERED ([intExportTaxMappingId] ASC),
     CONSTRAINT [FK_tblCFExportTaxMapping_tblSMImportFileHeader] FOREIGN KEY ([intImportFileHeaderId]) REFERENCES [dbo].[tblSMImportFileHeader] ([intImportFileHeaderId])
 );
+
+
 
