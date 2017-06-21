11e929f5639e658817586d9fd5584f3ef86a133b
Gopinath Nayak <gopinath.n@irely.com>
2017-06-21 23:03:34 +0530

    GRN-622

--- i21Database/dbo/Stored Procedures/uspGRProcessSettleStorage.sql
+++ i21Database/dbo/Stored Procedures/uspGRProcessSettleStorage.sql
@@ -892,6 +892,12 @@ BEGIN TRY
 		   ,@ItemsToPost  AS ItemCostingTableType		   
 		   ,@strBatchId AS NVARCHAR(20)		   
 		   ,@intReceiptId AS INT
+		   ,@intInventoryReceiptItemId AS INT
+		   ,@intScaleTicketId AS INT
+		   ,@dblFreightRate NUMERIC(38, 20)
+		   ,@dblFreightAdjustment DECIMAL(7, 2)
+		   ,@dblGrossUnits NUMERIC(38, 20)
+		   ,@dblPerUnitFreight NUMERIC(38, 20)
 		   ,@intBillId AS INT
 		   ,@success AS BIT 
 		   
@@ -1192,11 +1198,17 @@ BEGIN TRY
 		IF @intReceiptId IS NOT NULL 
 		BEGIN 
 			-- Work-around. Add dummy cost on the IR before converting it to Voucher
+			SET @intInventoryReceiptItemId=NULL
+			SELECT @intInventoryReceiptItemId=MIN(intInventoryReceiptItemId) 
+			FROM tblICInventoryReceiptItem 
+			WHERE intInventoryReceiptId = @intReceiptId AND dblUnitCost=0
+
 			UPDATE	ri
-			SET		ri.dblUnitCost = 1
-					,ri.dblLineTotal = 1
+			SET		ri.dblUnitCost = SV.[dblCashPrice]					
 			FROM	tblICInventoryReceiptItem ri
-			WHERE	ri.intInventoryReceiptId = @intReceiptId
+			JOIN    @SettleVoucherCreate SV  ON SV.intItemId=ri.intItemId
+			WHERE	ri.intInventoryReceiptItemId=@intInventoryReceiptItemId 
+			AND     SV.intCustomerStorageId = @intCustomerStorageId AND SV.IsProcessed = 0 AND SV.intItemSort=1
 
 			EXEC uspICProcessToBill 
 					@intReceiptId
@@ -1204,11 +1216,10 @@ BEGIN TRY
 					, @intBillId OUTPUT 
 
 			-- Work-around. Put the cost back to zero. 
-			UPDATE	ri
-			SET		ri.dblUnitCost = 0
-					,ri.dblLineTotal = 0
-			FROM	tblICInventoryReceiptItem ri
-			WHERE	ri.intInventoryReceiptId = @intReceiptId
+			UPDATE	tblICInventoryReceiptItem
+			SET		dblUnitCost = 0					
+			FROM	tblICInventoryReceiptItem
+			WHERE	intInventoryReceiptItemId=@intInventoryReceiptItemId
 
 			-- Update the cost of the voucher detail. 
 			UPDATE	bd
