c7912b4e18a7853e2c6c65e42313dba1e3d699db
feb_montefrio <feb.montefrio@irely.com>
2017-06-21 12:10:30 +0800

    IC-3939

--- i21Database/dbo/Stored Procedures/uspICGetDefaultReceiptTaxGroupId.sql
+++ i21Database/dbo/Stored Procedures/uspICGetDefaultReceiptTaxGroupId.sql
@@ -14,53 +14,18 @@ SET XACT_ABORT ON
 SET ANSI_WARNINGS OFF
 
 BEGIN
-	DECLARE @strFobPoint AS NVARCHAR(50)
-
-	-- Get the Fob Point of the Freight Terms
-	SELECT	@strFobPoint = FreightTerms.strFobPoint
-	FROM	tblSMFreightTerms FreightTerms
-	WHERE	FreightTerms.intFreightTermId = @intFreightTermId
-
-	-- Get intTaxGroupId from Company Location for FOB destination
-	IF RTRIM(LTRIM(LOWER(@strFobPoint))) = 'destination'
-	BEGIN
-		SELECT @intTaxGroupId = CompanyLocation.intTaxGroupId
-				,@strTaxGroup = SMTaxGroup.strTaxGroup
-		FROM	tblSMCompanyLocation CompanyLocation LEFT JOIN tblSMTaxGroup SMTaxGroup 
-					ON CompanyLocation.intTaxGroupId = SMTaxGroup.intTaxGroupId
-		WHERE	CompanyLocation.intCompanyLocationId = @intLocationId
-	END
-
-	-- Assign intTaxGroupId based on Hierarchy
-	IF @intTaxGroupId IS NULL
-	BEGIN
-		-- Get intTaxGroupId from Vendor Special Pricing
-		BEGIN			
-			SELECT	@intTaxGroupId = Vendor.intTaxGroupId
-			       ,@strTaxGroup = SMTaxGroup.strTaxGroup
-			FROM	tblAPVendorSpecialTax Vendor LEFT JOIN tblSMTaxGroup SMTaxGroup 
-						ON Vendor.intTaxGroupId = SMTaxGroup.intTaxGroupId
-			WHERE	Vendor.intEntityVendorId = @intEntityVendorId
-		END
-
-		-- Get intTaxGroupId from Company Location
-		IF @intTaxGroupId IS NULL
-		BEGIN
-			SELECT @intTaxGroupId = CompanyLocation.intTaxGroupId
-				    ,@strTaxGroup = SMTaxGroup.strTaxGroup
-			FROM	tblSMCompanyLocation CompanyLocation LEFT JOIN tblSMTaxGroup SMTaxGroup 
-						ON CompanyLocation.intTaxGroupId = SMTaxGroup.intTaxGroupId
-			WHERE CompanyLocation.intCompanyLocationId = @intLocationId
-		END
-
-		IF @intTaxGroupId IS NULL
-		BEGIN
-			SELECT	@intTaxGroupId = EntityLocation.intTaxGroupId
-				    ,@strTaxGroup = SMTaxGroup.strTaxGroup
-			FROM	tblEMEntityLocation EntityLocation LEFT JOIN tblSMTaxGroup SMTaxGroup 
-						ON EntityLocation.intTaxGroupId = SMTaxGroup.intTaxGroupId
-			WHERE	EntityLocation.intEntityId = @intEntityVendorId 
-					AND EntityLocation.intEntityLocationId = @intEntityLocationId 
-		END
-	END
+	SELECT	TOP 1
+			@intTaxGroupId = taxGroup.intTaxGroupId
+			,@strTaxGroup = taxGroup.strTaxGroup
+	FROM	(
+				SELECT id = dbo.fnGetTaxGroupIdForVendor(
+					@intEntityVendorId		-- @VendorId
+					,@intLocationId			--,@CompanyLocationId
+					,NULL					--,@ItemId
+					,@intEntityLocationId	--,@VendorLocationId
+					,@intFreightTermId		--,@FreightTermId
+				)			
+			) taxHierarchy
+			INNER JOIN tblSMTaxGroup taxGroup
+				ON taxGroup.intTaxGroupId = taxHierarchy.id 
 END
