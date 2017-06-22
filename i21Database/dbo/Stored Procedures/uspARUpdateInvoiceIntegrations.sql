b208ac04293ed7ae0b2cd6bbb54cfc05fb9413a2
Michelle Jardiolin <michelle.jardiolin@irely.com>
2017-06-22 13:23:27 +0800

    AR-4757

--- i21Database/dbo/Stored Procedures/uspARUpdateInvoiceIntegrations.sql
+++ i21Database/dbo/Stored Procedures/uspARUpdateInvoiceIntegrations.sql
@@ -3,38 +3,36 @@
 	,@ForDelete		BIT = 0    
 	,@UserId		INT = NULL     
 AS  
-
+  
 SET QUOTED_IDENTIFIER OFF  
 SET ANSI_NULLS ON  
 SET NOCOUNT ON  
 SET XACT_ABORT ON  
 SET ANSI_WARNINGS OFF
- 
+
 DECLARE @intInvoiceId INT
 	  , @intUserId    INT
-	  , @ysnForDelete BIT
 
 SET @intInvoiceId = @InvoiceId
 SET @intUserId = @UserId
-SET @ysnForDelete = @ForDelete
 
 IF (@intInvoiceId IS NULL)
 	BEGIN
-		SET @ysnForDelete = 0
+		SET @ForDelete = 0
 	END
 ELSE
 	BEGIN
-		SET @ysnForDelete = 1
+		SET @ForDelete = 1
 	END
 
 EXEC dbo.[uspARUpdatePricingHistory] 2, @intInvoiceId, @intUserId
-EXEC dbo.[uspARUpdateSOStatusFromInvoice] @intInvoiceId, @ysnForDelete
+EXEC dbo.[uspARUpdateSOStatusFromInvoice] @intInvoiceId, @ForDelete
 EXEC dbo.[uspARUpdateItemComponent] @intInvoiceId, 0
-EXEC dbo.[uspARUpdateReservedStock] @intInvoiceId, @ysnForDelete, @intUserId, 0
+EXEC dbo.[uspARUpdateReservedStock] @intInvoiceId, @ForDelete, @intUserId, 0
 EXEC dbo.[uspARUpdateItemComponent] @intInvoiceId, 1
-EXEC dbo.[uspARUpdateInboundShipmentOnInvoice] @intInvoiceId, @ysnForDelete, @intUserId
-EXEC dbo.[uspARUpdateProvisionalOnStandardInvoice] @intInvoiceId, @ysnForDelete, @intUserId
-EXEC dbo.[uspARUpdateCommitted] @intInvoiceId, @ysnForDelete, @intUserId, 0
+EXEC dbo.[uspARUpdateInboundShipmentOnInvoice] @intInvoiceId, @ForDelete, @intUserId
+EXEC dbo.[uspARUpdateProvisionalOnStandardInvoice] @intInvoiceId, @ForDelete, @intUserId
+EXEC dbo.[uspARUpdateCommitted] @intInvoiceId, @ForDelete, @intUserId, 0
 
 DELETE FROM [tblARTransactionDetail] WHERE [intTransactionId] = @intInvoiceId AND [strTransactionType] = (SELECT TOP 1 [strTransactionType] FROM tblARInvoice WHERE intInvoiceId = @intInvoiceId)
 
