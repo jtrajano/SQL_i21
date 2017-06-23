fad3963a7c6406f071fb66e2d97781c4361bfa49
Jayson Ramos <jayson.ramos@iRely.com>
2017-06-23 15:41:28 +0800

    HD-2106

--- i21Database/dbo/Views/vyuHDJiraRollUp.sql
+++ i21Database/dbo/Views/vyuHDJiraRollUp.sql
@@ -2,34 +2,60 @@
 	AS
 		select
 			intId = ROW_NUMBER() over (order by strKey)
-			,a.intProjectId
-			,a.strProjectName
-			,a.dtmCreated
-			,a.dtmGoLive
-			,b.intTicketId
-			,b.strTicketNumber
-			,d.strKey
-			,d.intJiraKeyId
-			,d.strJiraKey
-			,d.strJiraUrl
-			,d.strTypeIconUrl
-			,d.strSummary
-			,d.strDescription
-			,d.strReporter
-			,d.strAssignee
-			,d.strFixedBy
-			,d.strPriorityIconUrl
-			,d.strStatusIconUrl
-			,d.strResolution
-			,d.dtmJiraCreated
-			,d.dtmJiraUpdated
-			,d.strFixedVersion
+			,intProjectId
+			,strProjectName
+			,dtmCreated
+			,dtmGoLive
+			,intTicketId
+			,strTicketNumber
+			,strKey
+			,intJiraKeyId
+			,strJiraKey
+			,strJiraUrl
+			,strTypeIconUrl
+			,strSummary
+			,strDescription
+			,strReporter
+			,strAssignee
+			,strFixedBy
+			,strPriorityIconUrl
+			,strStatusIconUrl
+			,strResolution
+			,dtmJiraCreated
+			,dtmJiraUpdated
+			,strFixedVersion
 		from
-			tblHDProject a
-			,tblHDTicket b
-			,tblHDProjectTask c
-			,tblHDTicketJIRAIssue d
-		where
-			d.intTicketId = c.intTicketId
-			and c.intProjectId = a.intProjectId
-			and b.intTicketId = c.intTicketId
+		(
+			select distinct
+				a.intProjectId
+				,a.strProjectName
+				,a.dtmCreated
+				,a.dtmGoLive
+				,b.intTicketId
+				,b.strTicketNumber
+				,d.strKey
+				,d.intJiraKeyId
+				,d.strJiraKey
+				,d.strJiraUrl
+				,d.strTypeIconUrl
+				,d.strSummary
+				,d.strDescription
+				,d.strReporter
+				,d.strAssignee
+				,d.strFixedBy
+				,d.strPriorityIconUrl
+				,d.strStatusIconUrl
+				,d.strResolution
+				,d.dtmJiraCreated
+				,d.dtmJiraUpdated
+				,d.strFixedVersion
+			from
+				tblHDProject a
+				,tblHDTicket b
+				,tblHDProjectTask c
+				,tblHDTicketJIRAIssue d
+			where
+				d.intTicketId = c.intTicketId
+				and c.intProjectId = a.intProjectId
+				and b.intTicketId = c.intTicketId
+		) as result
