6dc97773a0813e85734de49d853c6b40ea0f93dc
Manuel Armas <manny.armas@irely.com>
2017-06-23 23:05:20 +0800

    IET-164 - Show real time truck GPS position

--- i21Database/dbo/Tables/tblETGPSCoordinates.sql
+++ i21Database/dbo/Tables/tblETGPSCoordinates.sql
@@ -1,9 +1,10 @@
 ﻿CREATE TABLE [dbo].[tblETGPSCoordinates](
 	[intGPSCoordinatesId] INT IDENTITY(1,1) NOT NULL,
-	[strTruckId] NVARCHAR(50) NULL,
-	[strDriverId] NVARCHAR(50) NULL,
+	[strTruckId] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL,
+	[strDriverId] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL,
 	[dblLatitude] NUMERIC(9, 6) NOT NULL,
-	[dblLongitute] NUMERIC(9, 6) NOT NULL,
+	[dblLongitude] NUMERIC(9, 6) NOT NULL,
 	[dtmRecordDate] DATETIME NOT NULL,
+	[intConcurrencyId] INT NOT NULL
 	CONSTRAINT [PK_tblETGPSCoordinates_intGPSCoordinatesId] PRIMARY KEY CLUSTERED ([intGPSCoordinatesId] ASC)    
 )
