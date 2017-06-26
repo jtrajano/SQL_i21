76286662cb1bc74167b2c1964ee11f0d631dd13a
Kelvin Quinto <kelvin.quinto@irely.com>
2017-06-27 00:40:38 +0800

    AR-4504

--- i21Database/dbo/Stored Procedures/uspARCustomerStatementBalanceForwardReport.sql
+++ i21Database/dbo/Stored Procedures/uspARCustomerStatementBalanceForwardReport.sql
@@ -232,7 +232,7 @@ INSERT INTO @temp_aging_table
 EXEC dbo.[uspARCustomerAgingAsOfDateReport] NULL, @dtmDateTo, NULL, NULL, NULL, @strLocationName, @ysnIncludeBudget, @ysnPrintCreditBalance
 
 INSERT INTO @temp_balanceforward_table
-EXEC dbo.[uspARCustomerAgingAsOfDateReport] NULL, @dtmDateTo, NULL, NULL, NULL, @strLocationName, @ysnIncludeBudget, @ysnPrintCreditBalance
+EXEC dbo.[uspARCustomerAgingAsOfDateReport] NULL, @dtmDateFrom, NULL, NULL, NULL, @strLocationName, @ysnIncludeBudget, @ysnPrintCreditBalance
 
 SET @query = CAST('' AS NVARCHAR(MAX)) + 'SELECT * FROM
 (SELECT intEntityCustomerId	= C.intEntityId
