278afaa23de527a7866b6ddbdb08957b1e91ab43
bonn ponce <bonn.ponce@irely.com>
2017-06-27 21:18:38 +0800

    CF-334

--- i21Database/dbo/Tables/tblCFPriceProfileDetail.sql
+++ i21Database/dbo/Tables/tblCFPriceProfileDetail.sql
@@ -9,6 +9,7 @@
     [dblRate]                 NUMERIC (18, 6) NULL,
     [strBasis]                NVARCHAR (250)  COLLATE Latin1_General_CI_AS NULL,
     [intConcurrencyId]        INT             CONSTRAINT [DF_tblCFPriceProfileDetail_intConcurrencyId] DEFAULT ((1)) NULL,
+    [ysnForceRounding] BIT NULL, 
     CONSTRAINT [PK_tblCFPriceProfileDetail] PRIMARY KEY CLUSTERED ([intPriceProfileDetailId] ASC),
     CONSTRAINT [FK_tblCFPriceProfileDetail_tblCFPriceIndex] FOREIGN KEY ([intLocalPricingIndex]) REFERENCES [dbo].[tblCFPriceIndex] ([intPriceIndexId]),
     CONSTRAINT [FK_tblCFPriceProfileDetail_tblCFPriceProfileHeader] FOREIGN KEY ([intPriceProfileHeaderId]) REFERENCES [dbo].[tblCFPriceProfileHeader] ([intPriceProfileHeaderId]) ON DELETE CASCADE,
