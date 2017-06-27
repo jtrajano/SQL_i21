33ffce709753b83e175f8aee09691da2107b84ff
Jb Mago <benedict.mago@irely.com>
2017-06-27 13:51:43 +0800

    PAT-682

---
+++ i21Database/dbo/Views/vyuPATStockClassification.sql
@@ -0,0 +1,13 @@
+﻿CREATE VIEW [dbo].[vyuPATStockClassification]
+	AS
+SELECT	SC.intStockId,
+		SC.strStockName,
+		SC.strStockDescription,
+		SC.dblParValue,
+		SC.intDividendsGLAccount,
+		strDividendsAccount = GL.strAccountId,
+		SC.intDividendsPerShare,
+		SC.intConcurrencyId
+FROM tblPATStockClassification SC
+LEFT JOIN tblGLAccount GL
+	ON GL.intAccountId = SC.intDividendsGLAccount
