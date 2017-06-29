d5cf12b9825e933282c5dd89a13782c59bcdfec7
Feb Montefrio <feb.montefrio@irely.com>
2017-06-29 12:31:32 +0800

    Fix created while creating the patch for IC-4028

--- i21Database/dbo/Triggers/trgCustomJDESAPReceiptUpdateExportFlag.sql
+++ i21Database/dbo/Triggers/trgCustomJDESAPReceiptUpdateExportFlag.sql
@@ -2,19 +2,13 @@
 ON tblICInventoryReceipt
 FOR UPDATE
 AS
-Begin
-
-Declare @intInventoryReceiptId INT,
-		@ysnPosted Bit
-
-If Not Exists(
-	Select 1 From tblIPSAPIDOCTag Where strTag='UPDATE_RECEIPT_EXPORT_FLAG_ON_UNPOST' AND strValue='TRUE'
-)
-return
-
-Select @intInventoryReceiptId=intInventoryReceiptId,@ysnPosted=ISNULL(ysnPosted,0) From inserted
-
-If @ysnPosted=0
-	Update tblICInventoryReceiptItem Set ysnExported=null Where intInventoryReceiptId=@intInventoryReceiptId
-
-End
+BEGIN
+	IF EXISTS(SELECT TOP 1 1 FROM tblIPSAPIDOCTag WHERE strTag ='UPDATE_RECEIPT_EXPORT_FLAG_ON_UNPOST' AND strValue = 'TRUE')
+	BEGIN
+		UPDATE	ri	
+		SET		ysnExported = NULL 
+		FROM	tblICInventoryReceiptItem ri INNER JOIN inserted i
+					ON ri.intInventoryReceiptId = i.intInventoryReceiptId
+		WHERE	ISNULL(i.ysnPosted, 0) = 0 
+	END 
+END
