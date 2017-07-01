182e27bf79ca4fab9812dbbe7add8f07235b6bf5
James Balachander <balachander.j@irely.com>
2017-06-30 21:46:51 -0400

    LG-1070

---
+++ i21Database/dbo/Stored Procedures/uspLGGetSalesInvoiceDetailReport.sql
@@ -0,0 +1,45 @@
+﻿CREATE PROCEDURE [dbo].[uspLGGetSalesInvoiceDetailReport]
+		@xmlParam NVARCHAR(MAX) = NULL  
+AS
+BEGIN
+	DECLARE @intInvoiceId INT
+
+	SET @intInvoiceId = @xmlParam
+    
+	SELECT 
+		Inv.intInvoiceId,
+		intSerialNo = ROW_NUMBER() OVER (ORDER BY InvDet.intInvoiceDetailId),
+		Inv.strInvoiceNumber,
+		strCustomer = EN.strEntityName,
+		Inv.strBillToAddress,
+		Inv.strBillToCity,
+		Inv.strBillToState,
+		Inv.strBillToZipCode,
+		Inv.strBillToCountry,
+		Inv.strComments,
+		Inv.strFooterComments,
+		Inv.strTransactionType,
+		Inv.strType,
+		InvDet.strItemDescription,
+		InvDet.dblPrice,
+		InvDet.dblQtyShipped,
+		InvDet.dblShipmentGrossWt,
+		InvDet.dblShipmentTareWt,
+		InvDet.dblShipmentNetWt,
+		InvDet.dblTotal,
+		strInvoiceCurrency = InvCur.strCurrency,
+		strPriceCurrency = PriceCur.strCurrency,
+		strPriceUOM = PriceUOM.strUnitMeasure,
+		strWeightUOM = WtUOM.strUnitMeasure
+	FROM tblARInvoice Inv
+	JOIN vyuCTEntity EN ON EN.intEntityId = Inv.intEntityCustomerId
+	JOIN tblARInvoiceDetail InvDet ON InvDet.intInvoiceId = Inv.intInvoiceId
+	JOIN tblSMCurrency InvCur ON InvCur.intCurrencyID = Inv.intCurrencyId
+	LEFT JOIN tblCTContractDetail CD ON CD.intContractDetailId = InvDet.intContractDetailId 
+	LEFT JOIN tblSMCurrency PriceCur ON PriceCur.intCurrencyID = CD.intCurrencyId
+	LEFT JOIN tblICItemUOM PriceItemUOM ON PriceItemUOM.intItemUOMId = CD.intPriceItemUOMId
+	LEFT JOIN tblICUnitMeasure PriceUOM ON PriceUOM.intUnitMeasureId = PriceItemUOM.intUnitMeasureId
+	LEFT JOIN tblICItemUOM WtItemUOM ON WtItemUOM.intItemUOMId = InvDet.intItemWeightUOMId
+	LEFT JOIN tblICUnitMeasure WtUOM ON WtUOM.intUnitMeasureId = WtItemUOM.intUnitMeasureId
+	WHERE Inv.intInvoiceId = @intInvoiceId
+END
---
+++ i21Database/dbo/Stored Procedures/uspLGGetSalesInvoiceHeaderReport.sql
@@ -0,0 +1,66 @@
+﻿CREATE PROCEDURE [dbo].[uspLGGetSalesInvoiceHeaderReport]
+		@xmlParam NVARCHAR(MAX) = NULL  
+AS
+BEGIN
+	DECLARE @intInvoiceId		INT,
+			@xmlDocumentId		INT 
+
+	IF	LTRIM(RTRIM(@xmlParam)) = ''   
+		SET @xmlParam = NULL   
+      
+	DECLARE @temp_xml_table TABLE 
+	(  
+			[fieldname]		NVARCHAR(50),  
+			condition		NVARCHAR(20),        
+			[from]			NVARCHAR(50), 
+			[to]			NVARCHAR(50),  
+			[join]			NVARCHAR(10),  
+			[begingroup]	NVARCHAR(50),  
+			[endgroup]		NVARCHAR(50),  
+			[datatype]		NVARCHAR(50) 
+	)  
+  
+	EXEC sp_xml_preparedocument @xmlDocumentId output, @xmlParam  
+  
+	INSERT INTO @temp_xml_table  
+	SELECT	*  
+	FROM	OPENXML(@xmlDocumentId, 'xmlparam/filters/filter', 2)  
+	WITH (  
+				[fieldname]		NVARCHAR(50),  
+				condition		NVARCHAR(20),        
+				[from]			NVARCHAR(50), 
+				[to]			NVARCHAR(50),  
+				[join]			NVARCHAR(10),  
+				[begingroup]	NVARCHAR(50),  
+				[endgroup]		NVARCHAR(50),  
+				[datatype]		NVARCHAR(50)  
+	)  
+    
+	SELECT	@intInvoiceId = [from]
+	FROM	@temp_xml_table   
+	WHERE	[fieldname] = 'intInvoiceId' 
+    
+	SELECT 
+		Inv.intInvoiceId,
+		Inv.dtmDate,
+		Inv.strInvoiceNumber,
+		strCustomer = EN.strEntityName,
+		Inv.strBillToAddress,
+		Inv.strBillToCity,
+		Inv.strBillToState,
+		Inv.strBillToZipCode,
+		Inv.strBillToCountry,
+		Inv.strComments,
+		Inv.strFooterComments,
+		Inv.strTransactionType,
+		Inv.strType,
+		strInvoiceCurrency = InvCur.strCurrency,
+		Term.strTerm,
+		strCompanyName = Comp.strLocationName
+	FROM tblARInvoice Inv
+	JOIN vyuCTEntity EN ON EN.intEntityId = Inv.intEntityCustomerId
+	JOIN tblSMCurrency InvCur ON InvCur.intCurrencyID = Inv.intCurrencyId
+	JOIN tblSMTerm Term ON Term.intTermID = Inv.intTermId
+	JOIN tblSMCompanyLocation Comp ON Comp.intCompanyLocationId = Inv.intCompanyLocationId
+	WHERE Inv.intInvoiceId = @intInvoiceId
+END
