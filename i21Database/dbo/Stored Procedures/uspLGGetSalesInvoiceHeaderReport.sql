e3024a2166a5df94c4e0b374453009c3341911aa
James Balachander <balachander.j@irely.com>
2017-07-01 11:37:32 -0400

    LG-1069

--- i21Database/dbo/Stored Procedures/uspLGGetSalesInvoiceHeaderReport.sql
+++ i21Database/dbo/Stored Procedures/uspLGGetSalesInvoiceHeaderReport.sql
@@ -1,4 +1,4 @@
-﻿CREATE PROCEDURE [dbo].[uspLGGetSalesInvoiceHeaderReport]
+﻿ALTER PROCEDURE [dbo].[uspLGGetSalesInvoiceHeaderReport]
 		@xmlParam NVARCHAR(MAX) = NULL  
 AS
 BEGIN
@@ -40,9 +40,10 @@ BEGIN
 	FROM	@temp_xml_table   
 	WHERE	[fieldname] = 'intInvoiceId' 
     
-	SELECT 
+	SELECT Top(1)
 		Inv.intInvoiceId,
 		Inv.dtmDate,
+		Inv.dtmDueDate,
 		Inv.strInvoiceNumber,
 		strCustomer = EN.strEntityName,
 		Inv.strBillToAddress,
@@ -50,17 +51,34 @@ BEGIN
 		Inv.strBillToState,
 		Inv.strBillToZipCode,
 		Inv.strBillToCountry,
+		Inv.strShipToAddress,
+		Inv.strShipToCity,
+		Inv.strShipToCountry,
+		Inv.strShipToState,
+		Inv.strShipToZipCode,
+		Inv.strShipToLocationName,
 		Inv.strComments,
 		Inv.strFooterComments,
 		Inv.strTransactionType,
 		Inv.strType,
 		strInvoiceCurrency = InvCur.strCurrency,
 		Term.strTerm,
-		strCompanyName = Comp.strLocationName
+		strCompanyName = Comp.strLocationName,
+		L.strLoadNumber,
+		L.dtmScheduledDate,
+		L.dtmDeliveredDate,
+		CB.strContractBasis,
+		C.strFLOId
 	FROM tblARInvoice Inv
 	JOIN vyuCTEntity EN ON EN.intEntityId = Inv.intEntityCustomerId
+	JOIN tblARCustomer C ON C.intEntityId = Inv.intEntityCustomerId
 	JOIN tblSMCurrency InvCur ON InvCur.intCurrencyID = Inv.intCurrencyId
 	JOIN tblSMTerm Term ON Term.intTermID = Inv.intTermId
 	JOIN tblSMCompanyLocation Comp ON Comp.intCompanyLocationId = Inv.intCompanyLocationId
+	LEFT JOIN tblLGLoad L ON L.intLoadId = Inv.intLoadId
+	LEFT JOIN tblLGLoadDetail LD ON LD.intLoadId = L.intLoadId
+	LEFT JOIN tblCTContractDetail CD on CD.intContractDetailId = LD.intSContractDetailId
+	LEFT JOIN tblCTContractHeader CH ON CH.intContractHeaderId = CD.intContractHeaderId
+	LEFT JOIN tblCTContractBasis CB ON CB.intContractBasisId = CH.intContractBasisId
 	WHERE Inv.intInvoiceId = @intInvoiceId
 END
