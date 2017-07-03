35c9421c3f0eaaecc75f42ee695c2caa8fa01ed3
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2017-07-03 16:10:44 +0530

    RM-252 Resolved Merging issue

--- i21Database/dbo/Stored Procedures/uspRKPnLBySalesContractResult.sql
+++ i21Database/dbo/Stored Procedures/uspRKPnLBySalesContractResult.sql
@@ -4,7 +4,7 @@
 		@intContractDetailId int = null
 AS
 
---DECLARE @intContractDetailId INT = 298
+--DECLARE @intContractDetailId INT = 2690
 
 DECLARE @Result TABLE
 (
@@ -16,72 +16,85 @@ DECLARE @Result TABLE
   dblQty NUMERIC(18, 6),
   dblUSD NUMERIC(18, 6),
   dblBasis NUMERIC(18, 6),
-  dblAllocatedQty NUMERIC(18, 6)
+  dblAllocatedQty NUMERIC(18, 6),
+  intContractTypeId int,
+  dblInvoicePrice NUMERIC(18, 6)
 )
+
+DECLARE @PhysicalFuturesResult TABLE
+(
+	intRowNum INT,
+	strContractType NVARCHAR(50),
+	strNumber NVARCHAR(50),
+	strDescription NVARCHAR(50),
+	strConfirmed NVARCHAR(50),
+	dblAllocatedQty NUMERIC(18, 6),
+	dblPrice NUMERIC(18, 6),
+	strCurrency NVARCHAR(50),
+	dblFX NUMERIC(18, 6),
+	dblBooked NUMERIC(18, 6),
+	dblAccounting NUMERIC(18, 6),
+	dtmDate datetime,
+	dblTransactionValue NUMERIC(18, 6),
+	dblForecast NUMERIC(18, 6)
+)
+
+INSERT INTO @PhysicalFuturesResult (intRowNum,strContractType,strNumber,strDescription,strConfirmed,dblAllocatedQty,dblPrice,strCurrency,dblFX,dblBooked,dblAccounting,dtmDate,dblTransactionValue,dblForecast)
+EXEC uspRKPNLPhysicalFuturesResult @intContractDetailId,16--- Hard code
 
 DECLARE @dblAllocatedQty  NUMERIC(18, 6)
 
 SELECT @dblAllocatedQty=sum(isnull(sa.dblAllocatedQty,0))
-FROM vyuCTContStsAllocation sa
-JOIN tblLGAllocationDetail ad on ad.intPContractDetailId=sa.intContractDetailId
-JOIN vyuCTContractDetailView dv on dv.intContractDetailId=ad.intPContractDetailId 
-WHERE sa.strContractType='Sale' and sa.intContractDetailId=@intContractDetailId
-GROUP BY dv.intContractHeaderId, dv.intContractDetailId,dv.strContractNumber,dv.strContractType
+FROM vyuRKPnLGetAllocationDetail sa
+WHERE sa.intContractTypeId=2 and sa.intContractDetailId=@intContractDetailId
+
+
+INSERT INTO @Result(intContractHeaderId,intContractDetailId,strContractNumber,strContractType,dblQty,dblUSD,dblBasis,dblAllocatedQty,dblInvoicePrice)
+SELECT distinct intContractHeaderId, intContractDetailId,strSequenceNumber,'Invoices',
+(SELECT ISNULL(SUM(dblBooked),0) FROM @PhysicalFuturesResult WHERE strDescription='Invoice') dblQty,
+sum(dblSaleBasis)/count(dblSaleBasis),@dblAllocatedQty dblAllocatedQty, null,
+(SELECT ISNULL(SUM(dblPrice),0) FROM @PhysicalFuturesResult WHERE strDescription='Invoice')/(SELECT case when count(isnull(dblPrice,0))=0 then 1 else count(dblPrice) end  FROM @PhysicalFuturesResult WHERE strDescription='Invoice')
+FROM vyuRKPnLGetAllocationDetail 
+WHERE intContractTypeId=2 and intContractDetailId=@intContractDetailId
+group by intContractHeaderId, intContractDetailId,strSequenceNumber
 
 INSERT INTO @Result(intContractHeaderId,intContractDetailId,strContractNumber,strContractType,dblQty,dblUSD,dblBasis,dblAllocatedQty)
-SELECT  dv.intContractHeaderId, dv.intContractDetailId,dv.strContractNumber,dv.strContractType,null,null,sum(dv.dblBasis),@dblAllocatedQty dblAllocatedQty
-FROM vyuCTContStsAllocation sa
-JOIN tblLGAllocationDetail ad on ad.intPContractDetailId=sa.intContractDetailId
-JOIN vyuCTContractDetailView dv on dv.intContractDetailId=ad.intSContractDetailId 
-WHERE  sa.strContractType='Sale'  and sa.intContractDetailId=@intContractDetailId
-GROUP BY dv.intContractHeaderId, dv.intContractDetailId,dv.strContractNumber,dv.strContractType
+SELECT distinct intContractHeaderId, intContractDetailId,strSequenceNumber,'Purchase',null,null,sum(dblPurchaseBasis)/count(dblPurchaseBasis),@dblAllocatedQty dblAllocatedQty
+FROM vyuRKPnLGetAllocationDetail 
+WHERE intContractTypeId=2 and intContractDetailId=@intContractDetailId
+GROUP BY intContractHeaderId, intContractDetailId,strSequenceNumber
 
-INSERT INTO @Result(intContractHeaderId,intContractDetailId,strContractNumber,strContractType,dblQty,dblUSD,dblBasis,dblAllocatedQty)
-SELECT  dv.intContractHeaderId, dv.intContractDetailId,dv.strContractNumber,dv.strContractType,@dblAllocatedQty,@dblAllocatedQty*dv.dblCashPrice ,sum(dv.dblBasis),@dblAllocatedQty dblAllocatedQty
-FROM vyuCTContStsAllocation sa
-JOIN tblLGAllocationDetail ad on ad.intPContractDetailId=sa.intContractDetailId
-JOIN vyuCTContractDetailView dv on dv.intContractDetailId=ad.intPContractDetailId 
-WHERE  sa.strContractType='Sale' and sa.intContractDetailId=@intContractDetailId
-GROUP BY dv.intContractHeaderId, dv.intContractDetailId,dv.strContractNumber,dv.strContractType,dv.dblCashPrice
+DECLARE @dblSaleBasis NUMERIC(18,6)
+DECLARE @dblPurchaseBasis NUMERIC(18,6)
+SELECT @dblSaleBasis=sum(dblBasis) FROM @Result where strContractType='Invoices'
+SELECT @dblPurchaseBasis=sum(dblBasis) FROM @Result where strContractType='Purchase'
+
 
 --RESULT
-INSERT INTO @Result(intContractHeaderId,intContractDetailId,strContractNumber,strContractType,dblBasis,dblAllocatedQty)
-SELECT NULL,NULL,NULL,'Gross Profit - USD',(dblBasis-ISNULL((SELECT SUM(dblBasis) FROM @Result WHERE strContractType='Purchase' ) ,0))*@dblAllocatedQty,null	
-FROM @Result WHERE strContractType='Sale' 
+INSERT INTO @Result(intContractHeaderId,intContractDetailId,strContractNumber,strContractType,dblBasis,dblAllocatedQty,intContractTypeId)
+SELECT DISTINCT NULL,NULL,NULL,'Gross Profit - USD',(@dblSaleBasis-@dblPurchaseBasis)*@dblAllocatedQty ,null,intContractTypeId	
+FROM @Result
+
+INSERT INTO @Result(intContractHeaderId,intContractDetailId,strContractNumber,strContractType,dblBasis,dblAllocatedQty,intContractTypeId)
+SELECT DISTINCT NULL,NULL,NULL,'Gross Profit - Rate',(@dblSaleBasis-@dblPurchaseBasis),null,intContractTypeId	FROM @Result 
 
 INSERT INTO @Result(intContractHeaderId,intContractDetailId,strContractNumber,strContractType,dblBasis,dblAllocatedQty)
-SELECT NULL,NULL,NULL,'Gross Profit - Rate',(dblBasis-ISNULL((SELECT SUM(dblBasis) FROM @Result WHERE strContractType='Purchase' ) ,0)),null	
-FROM @Result WHERE strContractType='Sale' 
+SELECT distinct  NULL,NULL,NULL,'PO Costs', sum(isnull(dblRate,0)),NULL
+FROM vyuRKPnLGetAllocationDetail dv
+JOIN vyuCTContractCostView cv on cv.intContractDetailId=dv.intPContractDetailId
+WHERE intContractTypeId=2 and dv.intContractDetailId=2690
 
-INSERT INTO @Result(intContractHeaderId,intContractDetailId,strContractNumber,strContractType,dblBasis,dblAllocatedQty)
-
-SELECT  NULL,NULL,NULL,'SO Costs',
-(SELECT dblCosts from
-(SELECT sum(isnull(dc.dblRate,0)) dblCosts
-        FROM vyuCTContractCostView dc 
-        LEFT JOIN tblICCommodityUnitMeasure cu1 on dv.intCommodityId=cu1.intCommodityId and cu1.intUnitMeasureId=dc.intUnitMeasureId
-        LEFT JOIN tblICCommodityUnitMeasure cu on dv.intCommodityId=cu.intCommodityId and cu.intUnitMeasureId=1
-        WHERE  dc.intContractDetailId=dv.intContractDetailId
-        GROUP BY cu.intCommodityUnitMeasureId,cu1.intCommodityUnitMeasureId)t) dblCosts,NULL
-FROM vyuCTContStsAllocation sa
-JOIN tblLGAllocationDetail ad on ad.intPContractDetailId=sa.intContractDetailId
-JOIN vyuCTContractDetailView dv on dv.intContractDetailId=ad.intPContractDetailId 
-WHERE  sa.strContractType='Sale' and sa.intContractDetailId=@intContractDetailId
 UNION
-SELECT  null,null,null,'PO Costs',
+
+SELECT DISTINCT  NULL,NULL,NULL,'SO Costs',
 (SELECT dblCosts from
 (SELECT sum(isnull(dc.dblRate,0)) dblCosts
         FROM vyuCTContractCostView dc 
-        LEFT JOIN tblICCommodityUnitMeasure cu1 on dv.intCommodityId=cu1.intCommodityId and cu1.intUnitMeasureId=dc.intUnitMeasureId
-        LEFT JOIN tblICCommodityUnitMeasure cu on dv.intCommodityId=cu.intCommodityId and cu.intUnitMeasureId=1
-        WHERE  dc.intContractDetailId=dv.intContractDetailId
-        group by cu.intCommodityUnitMeasureId,cu1.intCommodityUnitMeasureId)t) dblCosts,null
-FROM vyuCTContStsAllocation sa
-JOIN tblLGAllocationDetail ad on ad.intPContractDetailId=sa.intContractDetailId
-JOIN vyuCTContractDetailView dv on dv.intContractDetailId=ad.intSContractDetailId 
-WHERE  sa.strContractType='Sale'  and sa.intContractDetailId=@intContractDetailId
+        WHERE  dc.intContractDetailId=dv.intContractDetailId)t) dblCosts,NULL
+FROM vyuRKPnLGetAllocationDetail dv
+WHERE intContractTypeId=2 and intContractDetailId=@intContractDetailId
 
---Rate
+----Rate
 INSERT INTO @Result(intContractHeaderId,intContractDetailId,strContractNumber,strContractType,dblBasis,dblAllocatedQty)
 SELECT NULL,NULL,NULL,'Total Costs USD',sum(dblBasis)*@dblAllocatedQty,null	
 FROM @Result WHERE strContractType in('PO Costs','SO Costs') 
@@ -90,7 +103,7 @@ INSERT INTO @Result(intContractHeaderId,intContractDetailId,strContractNumber,st
 SELECT NULL,NULL,NULL,'Total Costs - Rate',sum(dblBasis),null	
 FROM @Result WHERE strContractType in('PO Costs','SO Costs') 
 
--- Profit
+---- Profit
 INSERT INTO @Result(intContractHeaderId,intContractDetailId,strContractNumber,strContractType,dblBasis,dblAllocatedQty)
 SELECT NULL,NULL,NULL,'Physical Profit - USD',(dblBasis-ISNULL((SELECT SUM(dblBasis) FROM @Result WHERE strContractType='Total Costs USD' ) ,0)),NULL	
 FROM @Result WHERE strContractType='Gross Profit - USD' 
@@ -99,9 +112,9 @@ INSERT INTO @Result(intContractHeaderId,intContractDetailId,strContractNumber,st
 SELECT NULL,NULL,NULL,'Physical Profit - Rate',(dblBasis-ISNULL((SELECT SUM(dblBasis) FROM @Result WHERE strContractType='Total Costs - Rate' ) ,0)),NULL	
 FROM @Result WHERE strContractType='Gross Profit - Rate' 
 
--- Net
+---- Net
 
--- Profit
+---- Profit
 INSERT INTO @Result(intContractHeaderId,intContractDetailId,strContractNumber,strContractType,dblBasis,dblAllocatedQty)
 SELECT NULL,NULL,NULL,'Net SO Profit - USD',isnull(dblBasis,0),NULL from @Result WHERE strContractType='Physical Profit - USD' 
 
---
+++ i21Database/dbo/Views/vyuRKPnLContractDetailView.sql
@@ -0,0 +1,19 @@
+﻿CREATE VIEW [dbo].[vyuRKPnLContractDetailView]
+
+AS
+
+SELECT	DISTINCT
+   CH.strContractNumber + '-' + LTRIM(CD.intContractSeq) strSequenceNumber,
+	CD.intContractDetailId,
+	CH.intContractTypeId,
+	IM.strItemNo,
+	IM.intItemId,
+	CH.strEntityName,
+	CH.intEntityId,
+	CD.dblQuantity	AS	dblDetailQuantity,
+	CD.dblBasis,
+	CH.intContractHeaderId			
+FROM	tblCTContractDetail		CD		
+JOIN	vyuCTContractHeaderView	CH	ON	CH.intContractHeaderId		=	CD.intContractHeaderId					
+JOIN	tblICItem				IM	ON	IM.intItemId				=	CD.intItemId				
+	
