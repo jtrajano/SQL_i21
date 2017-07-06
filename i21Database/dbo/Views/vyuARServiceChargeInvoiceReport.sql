f543f4ec029e642b49b0fe821b0fcc3ed18e7013
Kelvin Quinto <kelvin.quinto@irely.com>
2017-07-06 18:31:57 +0800

    AR-4949

--- i21Database/dbo/Views/vyuARServiceChargeInvoiceReport.sql
+++ i21Database/dbo/Views/vyuARServiceChargeInvoiceReport.sql
@@ -1,30 +1,29 @@
 ﻿CREATE VIEW [dbo].[vyuARServiceChargeInvoiceReport]
 AS
-SELECT 
-	ARI.intInvoiceId
+SELECT ARI.intInvoiceId
 	, ARID.intInvoiceDetailId	
 	, ARI.strInvoiceNumber
 	, ARI.dtmDate
 	, ARI.dtmDueDate
 	, ARI.intTermId
 	, SMT.strTerm
-	, dblInvoiceTotal			= Summary.dblTotal
-	, dblBaseInvoiceTotal		= Summary.dblBaseTotal
+	, dblInvoiceTotal			= SUMMARY.dblTotal
+	, dblBaseInvoiceTotal		= SUMMARY.dblBaseTotal
 	, dblTotalDue				= ARID.dblTotal
 	, dblBaseTotalDue			= ARID.dblBaseTotal
 	, ARI.intEntityCustomerId
-	, ARC.strCustomerNumber
-	, strCustomerName			= EME.strName
-	, ARC.strAccountNumber
-	, strCustomerAddress		= [dbo].fnARFormatCustomerAddress(NULL, NULL, EME.strName, EMELoc.strBillToAddress, EMELoc.strBillToCity, EMELoc.strBillToState, EMELoc.strBillToZipCode, EMELoc.strBillToCountry, NULL, NULL)
+	, CUSTOMER.strCustomerNumber
+	, strCustomerName			= CUSTOMER.strName
+	, CUSTOMER.strAccountNumber
+	, strCustomerAddress		= dbo.fnARFormatCustomerAddress(NULL, NULL, CUSTOMER.strName, CUSTOMER.strBillToAddress, CUSTOMER.strBillToCity, CUSTOMER.strBillToState, CUSTOMER.strBillToZipCode, CUSTOMER.strBillToCountry, NULL, NULL)
 	, intCompanyLocationId		= SMCS.intCompanyLocationId
 	, strCompanyName			= SMCS.strCompanyName
 	, strCompanyPhone			= SMCS.strCompanyPhone
 	, strCompanyFax				= SMCS.strCompanyFax
 	, strCompanyEmail			= SMCS.strCompanyEmail
-	, dtmLetterDate					= GETDATE()
-FROM 
-	(SELECT intInvoiceId
+	, dtmLetterDate				= GETDATE()
+FROM (
+	SELECT intInvoiceId
 		, intEntityCustomerId
 		, strInvoiceNumber
 		, dtmDate
@@ -32,77 +31,51 @@ FROM
 		, intTermId
 		, dblInvoiceTotal
 		, dblBaseInvoiceTotal		
-	 FROM 
-		tblARInvoice WITH (NOLOCK)
-	 WHERE 
-		strType = 'Service Charge') ARI 
-INNER JOIN 
-	(
-		SELECT 
-			intInvoiceId
-			, intInvoiceDetailId
-			, dblTotal
-			, dblBaseTotal
-		FROM	
-			tblARInvoiceDetail
-	) ARID ON ARI.intInvoiceId = ARID.intInvoiceId
-INNER JOIN
-	(SELECT 
-		intTermID
-		, strTerm 
-	 FROM 
-		tblSMTerm WITH (NOLOCK)) SMT ON ARI.intTermId = SMT.intTermID
-INNER JOIN (SELECT intEntityId
-				, strCustomerNumber
-				, strAccountNumber
-				, intBillToId 
-		    FROM 
-				tblARCustomer WITH(NOLOCK)) ARC ON ARI.intEntityCustomerId = ARC.intEntityId
-INNER JOIN (SELECT intEntityId
-				, strName 
-			FROM 
-				tblEMEntity WITH(NOLOCK)) EME ON ARC.intEntityId = EME.intEntityId
-INNER JOIN (SELECT intEntityId
-				, intEntityLocationId
-				, strBillToAddress		= strAddress
-				, strBillToCity			= strCity
-				, strBillToLocationName	= strLocationName
-				, strBillToCountry		= strCountry
-				, strBillToState		= strState
-				, strBillToZipCode		= strZipCode
-			FROM 
-				tblEMEntityLocation WITH(NOLOCK)
-			) EMELoc ON ARC.intEntityId = EMELoc.intEntityId AND ARC.intBillToId = EMELoc.intEntityLocationId
-INNER JOIN 	
-	(
-		SELECT intEntityCustomerId
-			, dblTotal				= SUM(dblTotal)
-			, dblBaseTotal			= SUM(dblBaseTotal)
-		FROM 
-			(
-				SELECT 
-					ARI.intEntityCustomerId  
-					, ARID.dblTotal
-					, ARID.dblBaseTotal
-				FROM 
-					tblARInvoice ARI WITH (NOLOCK) 
-				INNER JOIN 
-					(SELECT 
-						intInvoiceId
-						, dblTotal
-						, dblBaseTotal
-					 FROM 
-						tblARInvoiceDetail WITH (NOLOCK)) ARID ON ARI.intInvoiceId = ARID.intInvoiceId
-				WHERE 
-					ARI.strType = 'Service Charge'
-			)  Totals
-		GROUP BY intEntityCustomerId
-	) Summary ON ARC.intEntityId = Summary.intEntityCustomerId
- CROSS JOIN (SELECT intCompanyLocationId	= intCompanySetupID
-					, strCompanyName		= strCompanyName
-					, strCompanyAddress		= [dbo].fnARFormatCustomerAddress(NULL, NULL, NULL, strAddress, strCity, strState, strZip, strCountry, NULL, NULL)
-					, strCompanyPhone		= strPhone
-					, strCompanyFax			= strFax
-					, strCompanyEmail		= strEmail
-			 FROM	
-				tblSMCompanySetup WITH(NOLOCK)) SMCS
+	FROM dbo.tblARInvoice WITH (NOLOCK)
+	WHERE strType = 'Service Charge'
+	  AND ysnForgiven = 0
+) ARI 
+INNER JOIN (
+	SELECT intInvoiceId
+		, intInvoiceDetailId
+		, dblTotal
+		, dblBaseTotal
+	FROM dbo.tblARInvoiceDetail WITH (NOLOCK)
+) ARID ON ARI.intInvoiceId = ARID.intInvoiceId
+INNER JOIN (
+	SELECT intTermID
+		 , strTerm 
+	FROM dbo.tblSMTerm WITH (NOLOCK)
+) SMT ON ARI.intTermId = SMT.intTermID
+INNER JOIN (
+	SELECT intEntityId 
+		 , strCustomerNumber
+		 , strName
+		 , strAccountNumber
+		 , strBillToAddress
+		 , strBillToCity
+		 , strBillToLocationName
+		 , strBillToCountry
+		 , strBillToState
+		 , strBillToZipCode
+	FROM dbo.vyuARCustomerSearch WITH (NOLOCK)
+) CUSTOMER ON ARI.intEntityCustomerId = CUSTOMER.intEntityId
+INNER JOIN (
+	SELECT intEntityCustomerId
+		, dblTotal				= SUM(dblInvoiceTotal)
+		, dblBaseTotal			= SUM(dblBaseInvoiceTotal)
+	FROM dbo.tblARInvoice WITH (NOLOCK)
+	WHERE strType = 'Service Charge'
+	  AND ysnForgiven = 0		
+	GROUP BY intEntityCustomerId
+) SUMMARY ON CUSTOMER.intEntityId = SUMMARY.intEntityCustomerId
+OUTER APPLY (
+	SELECT TOP 1 
+		  intCompanyLocationId	= intCompanySetupID
+		, strCompanyName		= strCompanyName
+		, strCompanyAddress		= [dbo].fnARFormatCustomerAddress(NULL, NULL, NULL, strAddress, strCity, strState, strZip, strCountry, NULL, NULL)
+		, strCompanyPhone		= strPhone
+		, strCompanyFax			= strFax
+		, strCompanyEmail		= strEmail
+	FROM dbo.tblSMCompanySetup WITH (NOLOCK)
+) SMCS
