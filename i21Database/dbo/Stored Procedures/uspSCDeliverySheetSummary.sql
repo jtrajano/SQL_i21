caf05b5a8d19608f335d406416c5927926474f8d
Emerson Versoza <emerson.versoza@irely.com>
2017-07-07 17:34:36 +0800

    SC-775 SC-764

---
+++ i21Database/dbo/Stored Procedures/uspSCDeliverySheetSummary.sql
@@ -0,0 +1,96 @@
+﻿CREATE PROCEDURE [dbo].[uspSCDeliverySheetSummary]
+	@intDeliverySheetId AS INT
+AS
+SET QUOTED_IDENTIFIER OFF
+SET ANSI_NULLS ON
+SET NOCOUNT ON
+SET XACT_ABORT ON
+SET ANSI_WARNINGS OFF
+
+IF OBJECT_ID (N'tempdb.dbo.#tblSCDeliverySheetSummary') IS NOT NULL
+   DROP TABLE #temp
+
+DECLARE @temp TABLE (fields NVARCHAR(50))
+INSERT INTO @temp (fields)
+VALUES ('Id') ,('Contract') ,('Cash') ,('Storage') ,('DP') ,('Basis'),('WHGB') ,('Hold')
+
+SELECT *
+INTO #temp
+FROM (
+    SELECT fields, a = CAST(NULL AS NUMERIC(38,6)) 
+    FROM @temp
+) src
+PIVOT 
+(
+    MAX(a) 
+    FOR fields IN (Id, Contract, Cash, Storage, DP, Basis, WHGB, Hold)
+) unpvt
+
+ALTER TABLE #temp
+DROP COLUMN Id
+
+ALTER TABLE #temp
+ADD Id INT
+
+UPDATE #temp SET Id = 1
+--For priced contract
+UPDATE #temp SET Contract = 
+(SELECT SUM(CTD.dblQuantity - CTD.dblBalance) AS dblAppliedQty FROM tblSCDeliverySheet SCD
+LEFT JOIN tblSCTicket SCT ON SCD.intDeliverySheetId = SCT.intDeliverySheetId
+LEFT JOIN tblCTContractDetail CTD ON SCT.intContractId = CTD.intContractDetailId
+LEFT JOIN tblCTContractHeader CTH ON CTH.intContractHeaderId = CTD.intContractHeaderId
+WHERE CTH.intPricingTypeId = 1 AND SCD.intDeliverySheetId = @intDeliverySheetId)
+
+
+--For cash contract
+UPDATE #temp SET Cash = 
+ISNULL((SELECT SUM(CTD.dblQuantity - CTD.dblBalance) AS dblAppliedQty FROM tblSCDeliverySheet SCD
+LEFT JOIN tblSCTicket SCT ON SCD.intDeliverySheetId = SCT.intDeliverySheetId
+LEFT JOIN tblCTContractDetail CTD ON SCT.intContractId = CTD.intContractDetailId
+LEFT JOIN tblCTContractHeader CTH ON CTH.intContractHeaderId = CTD.intContractHeaderId
+WHERE CTH.intPricingTypeId = 5 AND SCD.intDeliverySheetId = @intDeliverySheetId), 0)
+
+--For storage
+UPDATE #temp SET Storage = 
+ISNULL((SELECT SUM(SCT.dblNetUnits) FROM tblSCDeliverySheet SCD
+LEFT JOIN tblSCTicket SCT ON SCD.intDeliverySheetId = SCT.intDeliverySheetId
+LEFT JOIN tblGRStorageType GR ON GR.intStorageScheduleTypeId = SCT.intStorageScheduleTypeId
+WHERE GR.ysnReceiptedStorage = 0 AND GR.ysnDPOwnedType = 0 AND GR.ysnGrainBankType = 0 AND GR.ysnCustomerStorage = 0
+AND GR.intStorageScheduleTypeId > 0 AND SCD.intDeliverySheetId = @intDeliverySheetId), 0)
+
+--For DP contract
+UPDATE #temp SET DP = 
+ISNULL((SELECT SUM(SCT.dblNetUnits) FROM tblSCDeliverySheet SCD
+LEFT JOIN tblSCTicket SCT ON SCD.intDeliverySheetId = SCT.intDeliverySheetId
+LEFT JOIN tblGRStorageType GR ON GR.intStorageScheduleTypeId = SCT.intStorageScheduleTypeId
+WHERE GR.ysnDPOwnedType = 1 AND GR.ysnCustomerStorage = 0 AND GR.intStorageScheduleTypeId > 0
+AND SCD.intDeliverySheetId = @intDeliverySheetId), 0)
+
+--For basis contract
+UPDATE #temp SET Basis = 
+ISNULL((SELECT SUM(CTD.dblQuantity - CTD.dblBalance) AS dblAppliedQty FROM tblSCDeliverySheet SCD
+LEFT JOIN tblSCTicket SCT ON SCD.intDeliverySheetId = SCT.intDeliverySheetId
+LEFT JOIN tblCTContractDetail CTD ON SCT.intContractId = CTD.intContractDetailId
+LEFT JOIN tblCTContractHeader CTH ON CTH.intContractHeaderId = CTD.intContractHeaderId
+WHERE CTH.intPricingTypeId = 2  AND SCD.intDeliverySheetId = @intDeliverySheetId), 0)
+
+--For warehouse and grainbank
+UPDATE #temp SET WHGB = 
+ISNULL((SELECT SUM(SCT.dblNetUnits) FROM tblSCDeliverySheet SCD
+LEFT JOIN tblSCTicket SCT ON SCD.intDeliverySheetId = SCT.intDeliverySheetId
+LEFT JOIN tblGRStorageType GR ON GR.intStorageScheduleTypeId = SCT.intStorageScheduleTypeId
+WHERE (GR.ysnReceiptedStorage = 1 OR GR.ysnGrainBankType = 1) AND GR.ysnDPOwnedType = 0 AND GR.ysnCustomerStorage = 0 
+AND GR.intStorageScheduleTypeId > 0 AND SCD.intDeliverySheetId = @intDeliverySheetId), 0)
+
+--For hold
+UPDATE #temp SET Hold = 
+ISNULL((SELECT SUM(SCT.dblNetUnits) FROM tblSCDeliverySheet SCD
+LEFT JOIN tblSCTicket SCT ON SCD.intDeliverySheetId = SCT.intDeliverySheetId
+WHERE SCT.intStorageScheduleTypeId = -5 OR SCT.strDistributionOption = 'HLD' AND SCD.intDeliverySheetId = @intDeliverySheetId), 0)
+
+ALTER TABLE #temp
+ADD intDeliverySheetId INT
+
+UPDATE #temp SET intDeliverySheetId = @intDeliverySheetId
+
+SELECT * FROM #temp
--- i21Database/dbo/Tables/tblSCDeliverySheet.sql
+++ i21Database/dbo/Tables/tblSCDeliverySheet.sql
@@ -3,6 +3,8 @@
 	[intDeliverySheetId] INT NOT NULL IDENTITY,
 	[intEntityId] INT NOT NULL, 
     [intCompanyLocationId] INT NOT NULL, 
+    [intItemId] INT NULL, 
+    [intDiscountId] INT NULL, 
 	[strDeliverySheetNumber] NVARCHAR(MAX) NULL,
     [dtmDeliverySheetDate] DATETIME NULL DEFAULT GETDATE(), 
     [ysnPost] BIT NULL DEFAULT (0),
@@ -10,6 +12,8 @@
 	CONSTRAINT [PK_tblSCDeliverySheet_intDeliverySheetId] PRIMARY KEY ([intDeliverySheetId]),
 	CONSTRAINT [FK_tblSCDeliverySheet_tblEMEntity_intEntityId] FOREIGN KEY ([intEntityId]) REFERENCES tblEMEntity([intEntityId]),
 	CONSTRAINT [FK_tblSCDeliverySheet_tblSMCompanyLocation_intCompanyLocationId] FOREIGN KEY ([intCompanyLocationId]) REFERENCES [tblSMCompanyLocation]([intCompanyLocationId]),
+	CONSTRAINT [FK_tblSCDeliverySheet_tblICItem_intItemId] FOREIGN KEY ([intItemId]) REFERENCES [tblICItem]([intItemId]),
+	CONSTRAINT [FK_tblSCDeliverySheet_tblGRDiscountId_intDiscountId] FOREIGN KEY ([intDiscountId]) REFERENCES [tblGRDiscountId]([intDiscountId])
 )
 GO
 EXEC sp_addextendedproperty @name = N'MS_Description',
@@ -56,3 +60,22 @@ EXEC sp_addextendedproperty @name = N'MS_Description',
     @level1name = N'tblSCDeliverySheet',
     @level2type = N'COLUMN',
     @level2name = N'dtmDeliverySheetDate'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Identity Column',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'tblSCDeliverySheet',
+    @level2type = N'COLUMN',
+    @level2name = N'intItemId'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Identity Column',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'tblSCDeliverySheet',
+    @level2type = N'COLUMN',
+    @level2name = N'intDiscountId'
+GO
