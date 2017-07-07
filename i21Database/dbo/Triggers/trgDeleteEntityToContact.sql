495ed4e9a0eb8d1666eb88d14a96141e69abb84f
Mon Gonzales <mon.gonzales@irely.com>
2017-07-07 19:39:21 +0800

    EM-2049

---
+++ i21Database/dbo/Triggers/trgDeleteEntityToContact.sql
@@ -0,0 +1,5 @@
+﻿CREATE TRIGGER [trgDeleteEntityToContact]
+	 ON [dbo].[tblEMEntityToContact] 
+FOR DELETE
+AS
+	delete from tblEMEntity where intEntityId in (Select intEntityContactId from deleted)
