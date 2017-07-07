ab5d35e2e6db6a9b7601952d614f4df43823b940
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2017-07-07 11:58:30 +0530

    RM-803

--- i21Database/dbo/Views/vyuRKGetBrokerByMarket.sql
+++ i21Database/dbo/Views/vyuRKGetBrokerByMarket.sql
@@ -8,4 +8,24 @@ JOIN tblRKBrokersAccountMarketMapping amm on amm.intBrokerageAccountId=ba.intBro
 JOIN vyuRKGetBrokerInstrumentType it on it.intBrokerageAccountId=ba.intBrokerageAccountId and it.intEntityId=ba.intEntityId
 JOIN tblRKFutureMarket fm on amm.intFutureMarketId=fm.intFutureMarketId
 JOIN tblRKCommodityMarketMapping cmm on fm.intFutureMarketId=cmm.intFutureMarketId
-JOIN tblEMEntity e on e.intEntityId=ba.intEntityId
+JOIN tblEMEntity e on e.intEntityId=ba.intEntityId and it.intInstrumentTypeId<>0
+UNION
+SELECT DISTINCT convert(int,row_number() OVER(ORDER BY amm.intFutureMarketId)) intRowNum, amm.intFutureMarketId,strFutMarketName,ba.intEntityId,e.strName,
+ba.intBrokerageAccountId,
+strAccountNumber,1,'Futures',cmm.intCommodityId
+FROM tblRKBrokerageAccount ba
+JOIN tblRKBrokersAccountMarketMapping amm on amm.intBrokerageAccountId=ba.intBrokerageAccountId
+JOIN vyuRKGetBrokerInstrumentType it on it.intBrokerageAccountId=ba.intBrokerageAccountId and it.intEntityId=ba.intEntityId
+JOIN tblRKFutureMarket fm on amm.intFutureMarketId=fm.intFutureMarketId
+JOIN tblRKCommodityMarketMapping cmm on fm.intFutureMarketId=cmm.intFutureMarketId
+JOIN tblEMEntity e on e.intEntityId=ba.intEntityId and it.intInstrumentTypeId=0
+UNION
+SELECT DISTINCT convert(int,row_number() OVER(ORDER BY amm.intFutureMarketId)) intRowNum, amm.intFutureMarketId,strFutMarketName,ba.intEntityId,e.strName,
+ba.intBrokerageAccountId,
+strAccountNumber,2,'Options',cmm.intCommodityId
+FROM tblRKBrokerageAccount ba
+JOIN tblRKBrokersAccountMarketMapping amm on amm.intBrokerageAccountId=ba.intBrokerageAccountId
+JOIN vyuRKGetBrokerInstrumentType it on it.intBrokerageAccountId=ba.intBrokerageAccountId and it.intEntityId=ba.intEntityId
+JOIN tblRKFutureMarket fm on amm.intFutureMarketId=fm.intFutureMarketId
+JOIN tblRKCommodityMarketMapping cmm on fm.intFutureMarketId=cmm.intFutureMarketId
+JOIN tblEMEntity e on e.intEntityId=ba.intEntityId and it.intInstrumentTypeId=0
