5c0d1f934328e6325a6a4c361f7f733d5ac1567d
Jayson Ramos <jayson.ramos@iRely.com>
2017-07-11 22:52:44 +0800

    CRM-479

---
+++ i21Database/dbo/Tables/tblCRMOpportunityPropectRequirement.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[tblCRMOpportunityPropectRequirement]
+(
+	[intOpportunityPropectRequirementId] [int] IDENTITY(1,1) NOT NULL,
+	[intOpportunityId] [int] NOT NULL,
+	[intProspectRequirementId] [int] NOT NULL,
+	[intEntityId] [int] NOT NULL,
+	[strRespondentAnswer] [nvarchar](MAX) COLLATE Latin1_General_CI_AS NOT NULL,
+	[dtmDate] [datetime] null,
+	[intConcurrencyId] [int] NOT NULL DEFAULT 1,
+	CONSTRAINT [PK_tblCRMOpportunityPropectRequirement_intOpportunityPropectRequirementId] PRIMARY KEY CLUSTERED ([intOpportunityPropectRequirementId] ASC),
+	CONSTRAINT [FK_tblCRMOpportunityPropectRequirement_tblCRMOpportunity_intOpportunityId] FOREIGN KEY ([intOpportunityId]) REFERENCES [dbo].[tblCRMOpportunity] ([intOpportunityId]),
+    CONSTRAINT [FK_tblCRMOpportunityPropectRequirement_tblCRMProspectRequirement_intProspectRequirementId] FOREIGN KEY ([intProspectRequirementId]) REFERENCES [dbo].[tblCRMProspectRequirement] ([intProspectRequirementId]),
+    CONSTRAINT [FK_tblCRMOpportunityPropectRequirement_tblEMENtity_intEntityId] FOREIGN KEY ([intEntityId]) REFERENCES [dbo].[tblEMEntity] ([intEntityId])
+)
