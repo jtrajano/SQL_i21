c83510d8dc15b38031bdd22cb9c5384df975e2b3
Jayson Ramos <jayson.ramos@iRely.com>
2017-07-12 04:16:57 +0800

    HD-1769

---
+++ i21Database/dbo/Tables/tblHDTicketStatusWorkflow.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[tblHDTicketStatusWorkflow]
+(
+	[intTicketStatusWorkflowId] [int] IDENTITY(1,1) NOT NULL,
+	[intFromStatusId] [int] NOT NULL,
+	[intToStatusId] [int] NOT NULL,
+	[strTiggerBy] [nvarchar](100) COLLATE Latin1_General_CI_AS NOT NULL default 'Customer Responds',
+	[intConcurrencyId] [int] NOT NULL DEFAULT 1,
+	CONSTRAINT [PK_tblHDTicketStatusWorkflow_intTicketStatusWorkflowId] PRIMARY KEY CLUSTERED ([intTicketStatusWorkflowId] ASC),
+	CONSTRAINT [FK_tblHDTicketStatusWorkflow_tblHDTicketStatus_intFromStatusId] FOREIGN KEY ([intFromStatusId]) REFERENCES [dbo].[tblHDTicketStatus] ([intTicketStatusId]),
+    CONSTRAINT [FK_tblHDTicketStatusWorkflow_tblHDTicketStatus_intToStatusId] FOREIGN KEY ([intToStatusId]) REFERENCES [dbo].[tblHDTicketStatus] ([intTicketStatusId]),
+    CONSTRAINT [UQ_tblHDTicketStatusWorkflow] UNIQUE ([intFromStatusId],[intToStatusId],[strTiggerBy])
+)
