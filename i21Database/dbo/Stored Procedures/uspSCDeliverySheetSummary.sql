64f3846ec16341e234973d0e24d065af9817ebb9
Emerson Versoza <emerson.versoza@irely.com>
2017-07-12 17:52:41 +0800

    SC-773

--- i21Database/dbo/Stored Procedures/uspSCDeliverySheetSummary.sql
+++ i21Database/dbo/Stored Procedures/uspSCDeliverySheetSummary.sql
@@ -41,8 +41,6 @@ PIVOT
     FOR fields IN (Id, Contract, Cash, Storage, DP, Basis, WHGB, Hold)
 ) unpvt
 
-TRUNCATE TABLE #temp
-
 ALTER TABLE #temp
 DROP COLUMN Id
 
@@ -58,6 +56,74 @@ ADD EntityName NVARCHAR(MAX)
 ALTER TABLE #temp
 ADD SplitPercentage INT
 
+--UPDATE #temp SET Id = 1
+----For priced contract
+--UPDATE #temp SET Contract = 
+--ISNULL((SELECT SUM(SCC.dblScheduleQty) AS dblAppliedQty FROM tblSCDeliverySheet SCD
+--LEFT JOIN tblSCTicket SCT ON SCD.intDeliverySheetId = SCT.intDeliverySheetId
+--LEFT JOIN tblCTContractDetail CTD ON SCT.intContractId = CTD.intContractDetailId
+--LEFT JOIN tblCTContractHeader CTH ON CTH.intContractHeaderId = CTD.intContractHeaderId
+--INNER JOIN tblSCTicketContractUsed SCC ON SCT.intTicketId = SCC.intTicketId AND SCC.intContractDetailId = SCT.intContractId AND SCC.intEntityId = SCT.intEntityId
+--WHERE CTH.intPricingTypeId = 1 AND SCD.intDeliverySheetId = @intDeliverySheetId
+--AND SCT.intTicketId = @intTicketId AND SCT.strDistributionOption = 'CNT'), 0)
+
+
+----For cash contract
+--UPDATE #temp SET Cash = 
+--ISNULL((SELECT SUM(CTD.dblQuantity - CTD.dblBalance) AS dblAppliedQty FROM tblSCDeliverySheet SCD
+--LEFT JOIN tblSCTicket SCT ON SCD.intDeliverySheetId = SCT.intDeliverySheetId
+--LEFT JOIN tblCTContractDetail CTD ON SCT.intContractId = CTD.intContractDetailId
+--LEFT JOIN tblCTContractHeader CTH ON CTH.intContractHeaderId = CTD.intContractHeaderId
+--WHERE CTH.intPricingTypeId = 5 AND SCT.strDistributionOption = 'CNT'
+--AND SCD.intDeliverySheetId = @intDeliverySheetId), 0)
+
+----For storage
+--UPDATE #temp SET Storage = 
+--ISNULL((SELECT SUM(SCT.dblNetUnits) FROM tblSCDeliverySheet SCD
+--LEFT JOIN tblSCTicket SCT ON SCD.intDeliverySheetId = SCT.intDeliverySheetId
+--LEFT JOIN tblGRStorageType GR ON GR.intStorageScheduleTypeId = SCT.intStorageScheduleTypeId
+--WHERE GR.ysnReceiptedStorage = 0 AND GR.ysnDPOwnedType = 0 AND GR.ysnGrainBankType = 0 AND GR.ysnCustomerStorage = 0
+--AND GR.intStorageScheduleTypeId > 0 AND SCD.intDeliverySheetId = @intDeliverySheetId), 0)
+
+----For DP contract
+--UPDATE #temp SET DP = 
+--ISNULL((SELECT SUM(SCT.dblNetUnits) FROM tblSCDeliverySheet SCD
+--LEFT JOIN tblSCTicket SCT ON SCD.intDeliverySheetId = SCT.intDeliverySheetId
+--LEFT JOIN tblGRStorageType GR ON GR.intStorageScheduleTypeId = SCT.intStorageScheduleTypeId
+--WHERE GR.ysnDPOwnedType = 1 AND GR.ysnCustomerStorage = 0 AND GR.intStorageScheduleTypeId > 0
+--AND SCD.intDeliverySheetId = @intDeliverySheetId), 0)
+
+----For basis contract
+--UPDATE #temp SET Basis = 
+--ISNULL((SELECT SUM(CTD.dblQuantity - CTD.dblBalance) AS dblAppliedQty FROM tblSCDeliverySheet SCD
+--LEFT JOIN tblSCTicket SCT ON SCD.intDeliverySheetId = SCT.intDeliverySheetId
+--LEFT JOIN tblCTContractDetail CTD ON SCT.intContractId = CTD.intContractDetailId
+--LEFT JOIN tblCTContractHeader CTH ON CTH.intContractHeaderId = CTD.intContractHeaderId
+--WHERE CTH.intPricingTypeId = 2 AND SCT.strDistributionOption = 'CNT' AND SCD.intDeliverySheetId = @intDeliverySheetId), 0)
+
+----For warehouse and grainbank
+--UPDATE #temp SET WHGB = 
+--ISNULL((SELECT SUM(SCT.dblNetUnits) FROM tblSCDeliverySheet SCD
+--LEFT JOIN tblSCTicket SCT ON SCD.intDeliverySheetId = SCT.intDeliverySheetId
+--LEFT JOIN tblGRStorageType GR ON GR.intStorageScheduleTypeId = SCT.intStorageScheduleTypeId
+--WHERE (GR.ysnReceiptedStorage = 1 OR GR.ysnGrainBankType = 1) AND GR.ysnDPOwnedType = 0 AND GR.ysnCustomerStorage = 0 
+--AND GR.intStorageScheduleTypeId > 0 AND SCD.intDeliverySheetId = @intDeliverySheetId), 0)
+
+----For hold
+--UPDATE #temp SET Hold = 
+--ISNULL((SELECT SUM(SCT.dblNetUnits) FROM tblSCDeliverySheet SCD
+--LEFT JOIN tblSCTicket SCT ON SCD.intDeliverySheetId = SCT.intDeliverySheetId
+--WHERE SCT.intStorageScheduleTypeId = -5 AND SCD.intDeliverySheetId = @intDeliverySheetId), 0)
+
+--SELECT @intEntityId = SCT.intEntityId, @strName = SCT.strName FROM tblSCDeliverySheet SCD
+--LEFT JOIN vyuSCTicketView SCT ON SCD.intDeliverySheetId = SCT.intDeliverySheetId
+--WHERE SCD.intDeliverySheetId = @intDeliverySheetId
+
+--update #temp SET EntityId = @intEntityId, EntityName = @strName, SplitPercentage = 100
+
+DELETE FROM #temp
+
+--FOR ticket splits
 DECLARE intListCursor CURSOR LOCAL FAST_FORWARD
 FOR
 SELECT SCS.intCustomerId,EM.strName, SCS.intTicketId, SCS.dblSplitPercent FROM tblSCTicket SC
@@ -74,19 +140,15 @@ FETCH NEXT FROM intListCursor INTO @intEntityId, @strName, @intTicketId, @SplitA
 
 WHILE @@FETCH_STATUS = 0
 BEGIN
-	--SET @SplitAverage = 
-	--ISNULL((SELECT AVG(SCS.dblSplitPercent) FROM tblSCDeliverySheet SCD
-	--LEFT JOIN tblSCTicket SCT ON SCD.intDeliverySheetId = SCT.intDeliverySheetId
-	--INNER JOIN tblSCTicketSplit SCS ON SCT.intTicketId = SCS.intTicketId
-	--WHERE SCD.intDeliverySheetId = @intDeliverySheetId AND SCS.intCustomerId = @intEntityId), 0)
 
 	--For priced contract
 	SET @Contract = 
-	ISNULL((SELECT SUM(CTD.dblQuantity - CTD.dblBalance) AS dblAppliedQty FROM tblSCDeliverySheet SCD
+	ISNULL((SELECT SUM(SCC.dblScheduleQty) AS dblAppliedQty FROM tblSCDeliverySheet SCD
 	LEFT JOIN tblSCTicket SCT ON SCD.intDeliverySheetId = SCT.intDeliverySheetId
 	LEFT JOIN tblCTContractDetail CTD ON SCT.intContractId = CTD.intContractDetailId
 	LEFT JOIN tblCTContractHeader CTH ON CTH.intContractHeaderId = CTD.intContractHeaderId
 	INNER JOIN tblSCTicketSplit SCS ON SCT.intTicketId = SCS.intTicketId
+	INNER JOIN tblSCTicketContractUsed SCC ON SCS.intTicketId = SCC.intTicketId AND SCC.intContractDetailId = SCT.intContractId AND SCC.intEntityId = SCS.intCustomerId
 	WHERE CTH.intPricingTypeId = 1 AND SCD.intDeliverySheetId = @intDeliverySheetId AND SCS.intCustomerId = @intEntityId
 	AND SCT.intTicketId = @intTicketId),0)
 
@@ -97,6 +159,7 @@ BEGIN
 	LEFT JOIN tblCTContractDetail CTD ON SCT.intContractId = CTD.intContractDetailId
 	LEFT JOIN tblCTContractHeader CTH ON CTH.intContractHeaderId = CTD.intContractHeaderId
 	INNER JOIN tblSCTicketSplit SCS ON SCT.intTicketId = SCS.intTicketId
+	INNER JOIN tblSCTicketContractUsed SCC ON SCS.intTicketId = SCC.intTicketId AND SCC.intContractDetailId = SCT.intContractId AND SCC.intEntityId = SCS.intCustomerId
 	WHERE CTH.intPricingTypeId = 5 AND SCD.intDeliverySheetId = @intDeliverySheetId AND SCS.intCustomerId = @intEntityId
 	AND SCT.intTicketId = @intTicketId), 0)
 
@@ -127,6 +190,7 @@ BEGIN
 	LEFT JOIN tblCTContractDetail CTD ON SCT.intContractId = CTD.intContractDetailId
 	LEFT JOIN tblCTContractHeader CTH ON CTH.intContractHeaderId = CTD.intContractHeaderId
 	INNER JOIN tblSCTicketSplit SCS ON SCT.intTicketId = SCS.intTicketId
+	INNER JOIN tblSCTicketContractUsed SCC ON SCS.intTicketId = SCC.intTicketId AND SCC.intContractDetailId = SCT.intContractId AND SCC.intEntityId = SCS.intCustomerId
 	WHERE CTH.intPricingTypeId = 2  AND SCD.intDeliverySheetId = @intDeliverySheetId AND SCS.intCustomerId = @intEntityId
 	AND SCT.intTicketId = @intTicketId), 0)
 
