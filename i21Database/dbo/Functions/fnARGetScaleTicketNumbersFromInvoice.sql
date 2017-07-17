21b845326c832a8389d44135b5735dff47ba8128
Kelvin Quinto <kelvin.quinto@irely.com>
2017-07-17 12:18:29 +0800

    AR-4993

--- i21Database/dbo/Functions/fnARGetScaleTicketNumbersFromInvoice.sql
+++ i21Database/dbo/Functions/fnARGetScaleTicketNumbersFromInvoice.sql
@@ -6,9 +6,14 @@ RETURNS NVARCHAR(MAX) AS
 BEGIN
 	DECLARE @strTicketNumbers NVARCHAR(MAX) = NULL
 
-	SELECT @strTicketNumbers = COALESCE(@strTicketNumbers + ', ', '') + RTRIM(LTRIM(T.strTicketNumber)) 
-	FROM dbo.tblSCTicket T WITH(NOLOCK) 		
-	WHERE T.intTicketId = @intInvoiceId
+	SELECT @strTicketNumbers = COALESCE(@strTicketNumbers + ', ', '') + RTRIM(LTRIM(T.strTicketNumber))
+	FROM dbo.tblARInvoiceDetail ID WITH (NOLOCK)
+		INNER JOIN (SELECT intTicketId
+						 , strTicketNumber 
+					FROM dbo.tblSCTicket WITH(NOLOCK)
+		) T ON ID.intTicketId = T.intTicketId
+	GROUP BY intInvoiceId, ID.intTicketId, T.strTicketNumber
+	HAVING ID.intInvoiceId = @intInvoiceId
 	
 	RETURN @strTicketNumbers
 END
