8987b7716d2a34d7c09b9638282f160793a7206f
Satya S Sahoo <satya.sahoo@irely.com>
2017-07-17 19:14:21 +0530

    CT-1375

--- i21Database/dbo/Views/vyuCTContStsPricingAndHedging.sql
+++ i21Database/dbo/Views/vyuCTContStsPricingAndHedging.sql
@@ -3,6 +3,7 @@
 AS 
 
 	SELECT	SY.intAssignFuturesToContractSummaryId,
+			PD.intPriceFixationDetailId,
 			CD.intContractDetailId,
 			PD.dtmFixationDate,
 			PD.[dblNoOfLots],
@@ -11,17 +12,20 @@ AS
 			SY.dtmMatchDate,
 			SY.intHedgedLots,
 			FO.dblPrice,
-			MM.strUnitMeasure strHedgeUOM
-	FROM	tblRKAssignFuturesToContractSummary SY 
-	JOIN	tblRKFutOptTransaction				FO	ON	FO.intFutOptTransactionId		=	SY.intFutOptTransactionId	
-	JOIN	tblRKFutureMarket					MA	ON	MA.intFutureMarketId			=	FO.intFutureMarketId		
-	JOIn	tblICUnitMeasure					MM	ON	MM.intUnitMeasureId				=	MA.intUnitMeasureId			LEFT
-	JOIN	tblCTPriceFixationDetail			PD	ON	PD.intFutOptTransactionId		=	SY.intFutOptTransactionId	LEFT
-	JOIN	tblCTPriceFixation					PF	ON	PF.intPriceFixationId			=	PD.intPriceFixationId		LEFT
+			MM.strUnitMeasure strHedgeUOM,
+			PF.intPriceContractId,
+			PC.strPriceContractNo
+
+	FROM	tblCTPriceFixationDetail			PD
+	JOIN	tblCTPriceFixation					PF	ON	PF.intPriceFixationId			=	PD.intPriceFixationId		
+	JOIN	tblCTPriceContract					PC	ON	PC.intPriceContractId			=	PF.intPriceContractId		LEFT
+	JOIN	tblRKAssignFuturesToContractSummary SY 	ON	SY.intFutOptTransactionId		=	PD.intFutOptTransactionId	LEFT
+	JOIN	tblRKFutOptTransaction				FO	ON	FO.intFutOptTransactionId		=	SY.intFutOptTransactionId	LEFT
+	JOIN	tblRKFutureMarket					MA	ON	MA.intFutureMarketId			=	FO.intFutureMarketId		LEFT
+	JOIN	tblICUnitMeasure					MM	ON	MM.intUnitMeasureId				=	MA.intUnitMeasureId			LEFT
 	JOIN	tblCTContractHeader					CH	ON	CH.intContractHeaderId			=	PF.intContractHeaderId		LEFT
 	JOIN	tblCTContractDetail					CD	ON	CD.intContractDetailId = CASE WHEN CH.ysnMultiplePriceFixation = 1 THEN  CD.intContractDetailId	ELSE PF.intContractDetailId	END
 													AND	CD.intContractHeaderId = CASE WHEN CH.ysnMultiplePriceFixation = 1 THEN  PF.intContractHeaderId	ELSE CD.intContractHeaderId	END	LEFT
-	
 	JOIN	tblICCommodityUnitMeasure			CU	ON	CU.intCommodityUnitMeasureId	=	PF.intFinalPriceUOMId		LEFT
 	JOIN	tblICItemUOM						IU	ON	IU.intItemId					=	CD.intItemId				
 													AND	IU.intUnitMeasureId				=	CU.intUnitMeasureId			LEFT
