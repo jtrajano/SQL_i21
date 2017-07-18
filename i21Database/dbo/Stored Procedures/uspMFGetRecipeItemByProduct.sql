28e787b9ba53d8697881d95d20ecb5cdefcb5251
Vinoth Kumar T <vinoth.t@irely.com>
2017-07-18 18:28:43 +0530

    MFG-2573

--- i21Database/dbo/Stored Procedures/uspMFGetRecipeItemByProduct.sql
+++ i21Database/dbo/Stored Procedures/uspMFGetRecipeItemByProduct.sql
@@ -71,6 +71,62 @@ BEGIN
 						AND DATEPART(dy, ri.dtmValidTo)
 					)
 				)
+		
+		UNION
+		
+		SELECT I.strItemNo
+			,I.strDescription
+			,ri.dblCalculatedQuantity
+			,UM.strUnitMeasure
+			,ri.strItemGroupName
+			,ri.dblUpperTolerance
+			,ri.dblLowerTolerance
+			,ri.dblCalculatedUpperTolerance
+			,ri.dblCalculatedLowerTolerance
+			,ri.dblShrinkage
+			,ri.ysnScaled
+			,CM.strName AS strConsumptionMethodName
+			,SL.strName AS strStorageLocationName
+			,ri.dtmValidFrom
+			,ri.dtmValidTo
+			,ri.ysnYearValidationRequired
+			,U.strUserName AS strCreatedUserName
+			,ri.dtmCreated
+			,U1.strUserName AS strLastModifiedUserName
+			,ri.dtmLastModified
+			,r.intVersionNo
+			,ri.ysnPartialFillConsumption
+			,CONVERT(BIT, 1) AS ysnSubstituteItem
+			,I1.strItemNo AS strMainRecipeItem
+			,ri.intRecipeItemId
+		FROM dbo.tblMFRecipeItem ri
+		JOIN dbo.tblMFRecipeSubstituteItem RSI ON RSI.intRecipeItemId = ri.intRecipeItemId
+			AND ri.intRecipeId = RSI.intRecipeId
+		JOIN dbo.tblMFRecipe r ON r.intRecipeId = ri.intRecipeId
+		JOIN dbo.tblICItem I ON I.intItemId = RSI.intSubstituteItemId
+		JOIN dbo.tblICItem I1 ON I1.intItemId = ri.intItemId
+		JOIN tblICItemUOM iu ON RSI.intItemUOMId = iu.intItemUOMId
+		JOIN tblICUnitMeasure UM ON iu.intUnitMeasureId = UM.intUnitMeasureId
+		JOIN dbo.tblMFConsumptionMethod CM ON CM.intConsumptionMethodId = ri.intConsumptionMethodId
+		JOIN dbo.tblSMUserSecurity U ON U.[intEntityUserSecurityId] = RSI.intCreatedUserId
+		JOIN dbo.tblSMUserSecurity U1 ON U1.[intEntityUserSecurityId] = RSI.intLastModifiedUserId
+		LEFT JOIN dbo.tblICStorageLocation SL ON SL.intStorageLocationId = ri.intStorageLocationId
+		WHERE r.intItemId = @intItemId
+			AND r.intLocationId = @intLocationId
+			AND r.ysnActive = 1
+			AND ri.intRecipeItemTypeId = 1
+			AND (
+				(
+					ri.ysnYearValidationRequired = 1
+					AND @dtmCurrentDate BETWEEN ri.dtmValidFrom
+						AND ri.dtmValidTo
+					)
+				OR (
+					ri.ysnYearValidationRequired = 0
+					AND @intDayOfYear BETWEEN DATEPART(dy, ri.dtmValidFrom)
+						AND DATEPART(dy, ri.dtmValidTo)
+					)
+				)
 		ORDER BY ri.intRecipeItemId
 	END
 	ELSE
