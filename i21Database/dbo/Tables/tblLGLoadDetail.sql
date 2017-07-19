89f66b49f61068dc83e3919b6ef4a057e026285d
IRELY\Swetosuvro <swetosuvro.g@irely.com>
2017-07-19 20:47:15 +0530

    LG-1103

--- i21Database/dbo/Tables/tblLGLoadDetail.sql
+++ i21Database/dbo/Tables/tblLGLoadDetail.sql
@@ -41,6 +41,7 @@
 	[intNumberOfContainers] INT NULL, 
 	[strExternalShipmentItemNumber] NVARCHAR(200) COLLATE Latin1_General_CI_AS NULL,
 	[strExternalBatchNo] NVARCHAR(200) COLLATE Latin1_General_CI_AS NULL,
+	[ysnNoClaim] BIT,
 
     CONSTRAINT [PK_tblLGLoadDetail] PRIMARY KEY ([intLoadDetailId]),
     CONSTRAINT [FK_tblLGLoadDetail_tblLGLoad_intLoadId] FOREIGN KEY ([intLoadId]) REFERENCES [tblLGLoad]([intLoadId]) ON DELETE CASCADE, 
