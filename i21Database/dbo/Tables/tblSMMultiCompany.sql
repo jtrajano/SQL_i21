55b7d1ad2815eae8ffd0b8d8b64fea4997fa5128
Dan Michael Jalimao <dan.jalimao@irely.com>
2017-07-19 17:34:33 +0800

    SM-3019

---
+++ i21Database/dbo/Tables/tblSMMultiCompany.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[tblSMMultiCompany]
+(
+	[intMultiCompanyId]			INT NOT NULL PRIMARY KEY IDENTITY, 
+    [strCompanyName]			NVARCHAR (150)	COLLATE Latin1_General_CI_AS NULL,
+	[strDatabaseName]			NVARCHAR (50)	COLLATE Latin1_General_CI_AS NULL,
+	[strServer]					NVARCHAR (50)	COLLATE Latin1_General_CI_AS NULL,
+	[strAuthentication]			NVARCHAR (30)	COLLATE Latin1_General_CI_AS NULL, 
+	[strUserName]				NVARCHAR (30)	COLLATE Latin1_General_CI_AS NULL, 
+	[strPassword]				NVARCHAR (30)	COLLATE Latin1_General_CI_AS NULL, 
+	[strType]					NVARCHAR (20)	COLLATE Latin1_General_CI_AS NULL, 
+	[intMultiCompanyParentId]	INT NULL,
+    [intConcurrencyId]			INT NOT NULL DEFAULT 1
+)
