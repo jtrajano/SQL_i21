8403fca005097faf0034d400eda6ad5ad247a67c
Vinoth Kumar T <vinoth.t@irely.com>
2017-07-21 23:38:38 +0530

    MFG-2587

---
+++ i21Database/dbo/Stored Procedures/uspMFValidateWorkOrder.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE uspMFValidateWorkOrder @intWorkOrderId INT
+	,@intNewStatusId INT
+	,@intStatusId INT = NULL
+AS
+BEGIN TRY
+	DECLARE @ErrMsg NVARCHAR(MAX)
+
+END TRY
+
+BEGIN CATCH
+	SET @ErrMsg = ERROR_MESSAGE()
+
+	RAISERROR (
+			@ErrMsg
+			,16
+			,1
+			,'WITH NOWAIT'
+			)
+END CATCH
