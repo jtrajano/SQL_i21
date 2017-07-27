76a1a6c17676223e6a84cfa48e8593a2e38c2774
Smith Wilson de Jesus <smith.dejesus@irely.com>
2017-07-27 22:38:20 +0800

    CM-925 (include Card Exp Date)

--- i21Database/dbo/Tables/tblEMEntityCardInformation.sql
+++ i21Database/dbo/Tables/tblEMEntityCardInformation.sql
@@ -4,11 +4,12 @@
     [strCreditCardNumber]	NVARCHAR (50)	COLLATE Latin1_General_CI_AS NULL,
     [strCardHolderName]		NVARCHAR (50)	COLLATE Latin1_General_CI_AS NULL,
     [strCardType]			NVARCHAR (50)	COLLATE Latin1_General_CI_AS NULL,
+	[strCardExpDate]		NVARCHAR (10)	COLLATE Latin1_General_CI_AS NULL ,
     [strFrequency]			NVARCHAR (10)	COLLATE Latin1_General_CI_AS NOT NULL,
     [strToken]				NVARCHAR(MAX)	COLLATE Latin1_General_CI_AS NULL,
     [strReference]			NVARCHAR(20)	COLLATE Latin1_General_CI_AS NULL,
     [dtmDateCreated]		DATETIME	NULL,
-    [dtmDateExpired]		DATETIME	NULL,
+    [dtmTokenExpired]		DATETIME	NULL,
     [intEntityUserId]		INT			NOT NULL,
     [intConcurrencyId]		INT			NOT NULL,
     CONSTRAINT [PK_tblEMEntityCardInformation] PRIMARY KEY CLUSTERED ([intEntityCardInfoId] ASC)
