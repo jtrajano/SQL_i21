69c3ac90d68db184f37e1da1b9f60065ffc5edd5
Michelle Jardiolin <michelle.jardiolin@irely.com>
2017-07-27 16:53:36 +0800

    AR-5008

--- i21Database/dbo/Functions/fnARGetInvoiceTypeAccount.sql
+++ i21Database/dbo/Functions/fnARGetInvoiceTypeAccount.sql
@@ -6,9 +6,11 @@
 RETURNS INT
 AS
 BEGIN
-
 	DECLARE @ARAccountId INT
+		, @ProfitCenterId INT
+
 	SET @ARAccountId = NULL
+	SET @ProfitCenterId = (SELECT TOP 1 intProfitCenter FROM tblSMCompanyLocation where intCompanyLocationId = @CompanyLocationId)
 
 	IF @TransactionType NOT IN ('Customer Prepayment', 'Cash', 'Cash Refund')
 		SET @ARAccountId = (SELECT TOP 1 [intARAccountId] FROM tblARCompanyPreference WHERE [intARAccountId] IS NOT NULL AND intARAccountId <> 0)
@@ -18,6 +20,8 @@ BEGIN
 		
 	IF @TransactionType = 'Customer Prepayment'
 		SET @ARAccountId = (SELECT TOP 1 [intSalesAdvAcct] FROM tblSMCompanyLocation WHERE [intCompanyLocationId] = @CompanyLocationId)
+		
+	SET @ARAccountId = ISNULL([dbo].[fnGetGLAccountIdFromProfitCenter](@ARAccountId, @ProfitCenterId), @ARAccountId)
 
 	RETURN @ARAccountId
 END
