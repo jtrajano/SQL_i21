d06b340684648bc5a749fb7748eab51a9aa384dd
Kelvin Quinto <kelvin.quinto@irely.com>
2017-07-27 16:46:41 +0800

    AR-5034

--- i21Database/dbo/Views/vyuARPaymentSearch.sql
+++ i21Database/dbo/Views/vyuARPaymentSearch.sql
@@ -7,15 +7,16 @@ SELECT
 	,P.intEntityCustomerId
 	,P.intBankAccountId
 	,strBankAccountNo		= LTRIM(RTRIM(BA.strBankAccountNo))
-	,strCustomerName		= LTRIM(RTRIM(E.strName))
-	,strCustomerNumber		= ISNULL(C.strCustomerNumber, E.strEntityNo)
+	,strCustomerName		= LTRIM(RTRIM(C.strName))
+	,strCustomerNumber		= C.strCustomerNumber
 	,P.dtmDatePaid
 	,P.intPaymentMethodId
 	,strPaymentMethod		= PM.strPaymentMethod
 	,dblAmountPaid			= P.dblAmountPaid
+	,dblDiscount		    = ISNULL(PD.dblDiscount, 0)
 	,P.ysnPosted
 	,strPaymentType			= 'Payment'
-	,strInvoices			= dbo.fnARGetInvoiceNumbersFromPayment(intPaymentId)
+	,strInvoices			= dbo.fnARGetInvoiceNumbersFromPayment(P.intPaymentId)
 	,P.intLocationId 
 	,CL.strLocationName
 	,dtmBatchDate			= GL.dtmDate
@@ -26,86 +27,73 @@ SELECT
 	,intCurrencyId			= P.intCurrencyId
 	,strCurrency			= SMC.strCurrency
     ,strCurrencyDescription	= SMC.strDescription	 
-FROM
-	(SELECT intPaymentId, 
-			strRecordNumber, 
-			intEntityId, 
-			intEntityCustomerId, 
-			intBankAccountId, 
-			dtmDatePaid,
-			intPaymentMethodId, 
-			dblAmountPaid, 
-			ysnPosted, 
-			intLocationId, 
-			intAccountId, 
-			intCurrencyId  
-	FROM 
-		tblARPayment) P 
-LEFT OUTER JOIN 
-	(SELECT intEntityId, 
-			strName 
-	 FROM 
-		tblEMEntity ) EM ON P.intEntityId = EM.intEntityId
-LEFT OUTER JOIN 
-	(SELECT intPaymentMethodID, 
-			strPaymentMethod 
-	 FROM 
-		tblSMPaymentMethod) PM ON P.intPaymentMethodId = PM.intPaymentMethodID
-INNER JOIN 
-	(SELECT intEntityId, 
-			strEntityNo, 
-			strName 
-	FROM 
-		tblEMEntity) E ON P.intEntityCustomerId = E.intEntityId
-LEFT OUTER JOIN 
-	(SELECT [intEntityId], 
-			strCustomerNumber 
-	 FROM 
-		tblARCustomer) C ON E.intEntityId = C.[intEntityId]
-LEFT OUTER JOIN 
-	(SELECT intBankAccountId, 
-			strBankAccountNo 
-	 FROM 
-		tblCMBankAccount) BA ON P.intBankAccountId = BA.intBankAccountId
-LEFT OUTER JOIN
-	(SELECT intCompanyLocationId, 
-			strLocationName 
-	 FROM 
-		tblSMCompanyLocation) CL ON P.intLocationId = CL.intCompanyLocationId
-LEFT OUTER JOIN
-	(
-		SELECT --TOP 1
-			 G.intTransactionId
-			,G.strTransactionId
-			,G.intAccountId
-			,G.strTransactionType
-			,G.dtmDate
-			,G.strBatchId
-			,E.intEntityId
-			,E.strName
-		FROM
-			(SELECT intTransactionId, 
-					strTransactionId, 
-					intAccountId, 
-					strTransactionType, 
-					dtmDate, 
-					strBatchId, 
-					intEntityId 
-			 FROM 
-				tblGLDetail
-			 WHERE 
-				strTransactionType IN ('Receive Payments') 
-				AND ysnIsUnposted = 0 
-				AND strCode = 'AR') G
-		LEFT OUTER JOIN
-			(SELECT intEntityId, 
-					strName 
-			 FROM 
-				tblEMEntity) E ON G.intEntityId = E.intEntityId
-	) GL ON P.intPaymentId = GL.intTransactionId AND P.intAccountId = GL.intAccountId AND P.strRecordNumber = GL.strTransactionId
-LEFT OUTER JOIN 
-	(SELECT intCurrencyID, 
-			strCurrency, 
-			strDescription 
-	FROM 
-		tblSMCurrency) SMC ON P.intCurrencyId = SMC.intCurrencyID		
+FROM (
+	SELECT intPaymentId
+		 , strRecordNumber
+		 , intEntityId
+		 , intEntityCustomerId
+		 , intBankAccountId
+		 , dtmDatePaid
+		 , intPaymentMethodId
+		 , dblAmountPaid
+		 , ysnPosted
+		 , intLocationId
+		 , intAccountId
+		 , intCurrencyId  
+	FROM dbo.tblARPayment WITH (NOLOCK)
+) P
+LEFT JOIN (
+	SELECT intPaymentId
+	     , dblDiscount = SUM(ISNULL(dblDiscount, 0))
+	FROM dbo.tblARPaymentDetail WITH (NOLOCK)
+	WHERE ISNULL(dblDiscount, 0) <> 0
+	GROUP BY intPaymentId
+) PD ON P.intPaymentId = PD.intPaymentId
+LEFT OUTER JOIN (
+	SELECT intEntityId
+		 , strName
+	FROM dbo.tblEMEntity WITH (NOLOCK)
+) EM ON P.intEntityId = EM.intEntityId
+LEFT OUTER JOIN (
+	SELECT intPaymentMethodID
+		 , strPaymentMethod
+	FROM dbo.tblSMPaymentMethod WITH (NOLOCK)
+) PM ON P.intPaymentMethodId = PM.intPaymentMethodID
+INNER JOIN (
+	SELECT intEntityId
+		 , strName
+		 , strCustomerNumber
+	FROM dbo.vyuARCustomer WITH (NOLOCK)
+) C ON P.intEntityCustomerId = C.intEntityId
+LEFT OUTER JOIN (
+	SELECT intBankAccountId
+		 , strBankAccountNo 
+	FROM dbo.tblCMBankAccount WITH (NOLOCK)
+) BA ON P.intBankAccountId = BA.intBankAccountId
+LEFT OUTER JOIN (
+	SELECT intCompanyLocationId
+		 , strLocationName 
+	FROM dbo.tblSMCompanyLocation WITH (NOLOCK)
+) CL ON P.intLocationId = CL.intCompanyLocationId
+LEFT OUTER JOIN (
+	SELECT intCurrencyID
+		 , strCurrency 
+		 , strDescription 
+	FROM dbo.tblSMCurrency WITH (NOLOCK)
+) SMC ON P.intCurrencyId = SMC.intCurrencyID
+OUTER APPLY (
+	SELECT TOP 1 E.strName
+			   , G.dtmDate
+			   , G.strBatchId
+	FROM dbo.tblGLDetail G WITH (NOLOCK)
+	LEFT JOIN (SELECT intEntityId
+				     , strName
+				FROM dbo.tblEMEntity WITH (NOLOCK)
+	) E ON G.intEntityId = E.intEntityId
+	WHERE P.intPaymentId = G.intTransactionId
+	  AND P.strRecordNumber = G.strTransactionId
+	  AND P.intAccountId = G.intAccountId
+	  AND G.strTransactionType = 'Receive Payments'
+	  AND G.ysnIsUnposted = 0
+	  AND G.strCode = 'AR'
+) GL
