e2c9f2cd8048556a635767bf9d73fbddc870c2fa
Arman Arenas <arman.arenas@irely.com>
2017-07-28 13:54:16 +0800

    Revert "Remove Expense Account from Deduction Types that are Paid By Employee"
    This reverts commit 3ee4ebc8269b6d8d700b637f656a7bceb801120c.

--- i21Database/Scripts/Post-Deployment/PR/DataFixes/UpdateOldData.sql
+++ i21Database/Scripts/Post-Deployment/PR/DataFixes/UpdateOldData.sql
@@ -5,7 +5,7 @@
 /*
 * Employee Workers Compensation 
 * 1. Change Rate Type from "Amount" to "Per Dollar"
-* 2..
+* 2....
 */
 
 IF EXISTS(SELECT * FROM sys.tables WHERE object_id = object_id('tblPRWorkersCompensation'))
@@ -15,30 +15,4 @@ BEGIN
 		SET strCalculationType = ''Per Dollar''
 	WHERE strCalculationType IS NULL OR strCalculationType = ''Amount''
 	')
-END
-
-/*
-* Deduction Types
-* 1. Remove Expense Account from Deduction Types that are Paid By Employee
-* 2...
-*/
-
-IF EXISTS(SELECT * FROM sys.tables WHERE object_id = object_id('tblPRTypeDeduction'))
-BEGIN
-	EXEC ('UPDATE tblPRTypeDeduction SET intExpenseAccountId = NULL WHERE strPaidBy = ''Employee'' AND intExpenseAccountId IS NOT NULL')
-END
-
-IF EXISTS(SELECT * FROM sys.tables WHERE object_id = object_id('tblPRTemplateDeduction'))
-BEGIN
-	EXEC ('UPDATE tblPRTemplateDeduction SET intExpenseAccountId = NULL WHERE strPaidBy = ''Employee'' AND intExpenseAccountId IS NOT NULL')
-END
-
-IF EXISTS(SELECT * FROM sys.tables WHERE object_id = object_id('tblPREmployeeDeduction'))
-BEGIN
-	EXEC ('UPDATE tblPREmployeeDeduction SET intExpenseAccountId = NULL WHERE strPaidBy = ''Employee'' AND intExpenseAccountId IS NOT NULL')
-END
-
-IF EXISTS(SELECT * FROM sys.tables WHERE object_id = object_id('tblPRPaycheckDeduction'))
-BEGIN
-	EXEC ('UPDATE tblPRPaycheckDeduction SET intExpenseAccountId = NULL WHERE strPaidBy = ''Employee'' AND intExpenseAccountId IS NOT NULL')
 END
