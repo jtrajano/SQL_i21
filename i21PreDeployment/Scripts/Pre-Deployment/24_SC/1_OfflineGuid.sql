a625e2085ccb563d8c3f88313ddbe185de4532da
Kevin Estrada <kevin.estrada@irely.com>
2017-07-30 16:39:16 +0800

    FRM-5317

---
+++ i21PreDeployment/Scripts/Pre-Deployment/24_SC/1_OfflineGuid.sql
@@ -0,0 +1,8 @@
+﻿PRINT N'***** BEGIN INSERT OFFLINE GUID (SCALE) *****'
+GO
+IF OBJECT_ID('dbo.[UK_tblSCTicket_strOfflineGuid]') IS NULL
+BEGIN
+	UPDATE [dbo].[tblSCTicket] SET strOfflineGuid = NEWID()
+END
+GO
+PRINT N'***** END INSERT OFFLINE GUID (SCALE)*****'
