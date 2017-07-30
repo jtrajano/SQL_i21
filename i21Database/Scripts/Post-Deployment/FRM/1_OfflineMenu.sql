2a63f3247bb803c4f7bfdc33c619a08b95cab54d
Kevin Estrada <kevin.estrada@irely.com>
2017-07-30 17:00:28 +0800

    FRm-5317

---
+++ i21Database/Scripts/Post-Deployment/FRM/1_OfflineMenu.sql
@@ -0,0 +1,26 @@
+﻿PRINT N'*** BEGIN - INSERT DEFAULT OFFLINE MENU ***'
+
+DECLARE @count INT
+SELECT @count = COUNT(*) FROM tblSMOfflineMenu  
+
+IF @count = 0
+BEGIN
+	
+
+	INSERT INTO [dbo].[tblSMOfflineMenu]
+           ([strModuleName]
+           ,[strSubMenus]
+           ,[intConcurrencyId])
+     VALUES
+           (N'Ticket Management',NULL,0)
+
+    INSERT INTO [dbo].[tblSMOfflineMenu]
+           ([strModuleName]
+           ,[strSubMenus]
+           ,[intConcurrencyId])
+     VALUES
+           (N'System Manager',NULL,0)
+
+
+END
+
