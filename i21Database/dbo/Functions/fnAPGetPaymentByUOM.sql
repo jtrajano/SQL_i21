0933955aa06333538e43d08c1ac7bb00c777a33d
Erick F. Dela Cruz <erick.delacruz@irely.com>
2017-07-31 12:31:47 +0800

    AP-3889

--- i21Database/dbo/Functions/fnAPGetPaymentByUOM.sql
+++ i21Database/dbo/Functions/fnAPGetPaymentByUOM.sql
@@ -3,21 +3,37 @@
 	@vendorId INT,
 	@currency INT,
 	@unitMeasureId INT,
-	@quantity DECIMAL(18,6)
+	@quantity DECIMAL(18,6),
+	@paymentId INT = NULL
 )
 RETURNS @returnTable TABLE(
 	intBillId INT PRIMARY KEY,
-	dblPayment DECIMAL(18,6)
+	dblPayment DECIMAL(18,2)
 )
 AS
 BEGIN
 
+	DECLARE @billId INT;
+	DECLARE @convertedQty DECIMAL(18,2);
+	DECLARE @amountToPay DECIMAL(18,6);
+	DECLARE @qtyToPay DECIMAL(18,6);
+	DECLARE @convertedDetailCostFromPaymentUOM DECIMAL(18,6);
+	DECLARE @convertedQtyToPaymentUOM DECIMAL(18,6);
+	DECLARE @remainingQty DECIMAL(18, 6);
+	DECLARE @voucherDetailPayment TABLE(intBillId INT, dblPayment DECIMAL(18,2));
+
+	DECLARE c CURSOR LOCAL STATIC READ_ONLY FORWARD_ONLY
+	FOR
 	WITH voucherDetails (
 		intBillId
 		,intBillDetailId
 		,dtmDueDate
 		,intVoucherDetailItemUOMId
+		,intPaymentItemUOMId
+		,dblVoucherDetailCost
 		,dblQuantityToPay
+		,dblAmountToPay
+		,dblVoucherDetailQty
 	) AS (
 		SELECT
 			intBIllId = voucher.intBillId
@@ -26,28 +42,55 @@ BEGIN
 			,intVoucherDetailItemUOMId = CASE WHEN voucherDetail.intWeightUOMId > 0 
 										THEN voucherDetail.intWeightUOMId 
 										ELSE voucherDetail.intUnitOfMeasureId END
-			,CASE WHEN itemUOMQty.intUnitMeasureId IS NOT NULL THEN uomQty.intUnitMeasureId
-						WHEN itemUOMNetWeight.intUnitMeasureId IS NOT NULL THEN uomNetWeight.intUnitMeasureId
-						ELSE NULL END
-			,CASE WHEN voucherDetail.dblNetWeight > 0 THEN voucherDetail.dblNetWeight ELSE voucherDetail.dblQtyReceived END
+			,intPaymentItemUOMId = paymentItemUOM.intItemUOMId
+			,dblVoucherDetailCost = voucherDetail.dblCost
+			,dblQtyToPay = @quantity
+			,dblAmountToPay = voucherDetail.dblTotal
+			,dblVoucherDetailQty = CASE WHEN voucherDetail.dblNetWeight > 0 THEN voucherDetail.dblNetWeight ELSE voucherDetail.dblQtyReceived END
 		FROM tblAPBill voucher 
 		INNER JOIN tblAPBillDetail voucherDetail ON voucher.intBillId = voucherDetail.intBillId
-		LEFT JOIN (tblICItemUOM itemUOMQty INNER JOIN tblICUnitMeasure uomQty 
-											ON itemUOMQty.intUnitMeasureId = uomQty.intUnitMeasureId)
-			ON itemUOMQty.intItemUOMId = voucherDetail.intUnitOfMeasureId
-		LEFT JOIN (tblICItemUOM itemUOMNetWeight INNER JOIN tblICUnitMeasure uomNetWeight
-											ON itemUOMNetWeight.intUnitMeasureId = uomNetWeight.intUnitMeasureId) 
-			ON itemUOMNetWeight.intItemUOMId = voucherDetail.intWeightUOMId 
+		LEFT JOIN tblICItemUOM paymentItemUOM ON voucherDetail.intItemId = paymentItemUOM.intItemId AND paymentItemUOM.intUnitMeasureId = @unitMeasureId
 		WHERE voucher.intEntityVendorId = @vendorId AND voucher.intCurrencyId = @currency 
-		AND voucherDetail.intUnitOfMeasureId > 0
-		AND (uomQty.intUnitMeasureId = @unitMeasureId OR uomNetWeight.intUnitMeasureId = @unitMeasureId)
+		AND voucherDetail.intUnitOfMeasureId > 0 --get only those have UOM
 	)
-
 	SELECT
-		*
+		voucherDetailsUOM.intBillId
+		,voucherDetailsUOM.dblQuantityToPay
+		,voucherDetailsUOM.dblAmountToPay
+		,dbo.fnCalculateQtyBetweenUOM(voucherDetailsUOM.intVoucherDetailItemUOMId, intPaymentItemUOMId, dblVoucherDetailQty)
+		,dbo.fnCalculateCostBetweenUOM(intPaymentItemUOMId, voucherDetailsUOM.intVoucherDetailItemUOMId, dblVoucherDetailCost)
 	FROM voucherDetails voucherDetailsUOM
-	GROUP BY voucherDetailsUOM.intBillId
 	ORDER BY voucherDetailsUOM.dtmDueDate DESC
+
+	OPEN c;
+
+	FETCH c INTO @billId, @qtyToPay, @amountToPay, @convertedQtyToPaymentUOM, @convertedDetailCostFromPaymentUOM;
+
+	WHILE @@FETCH_STATUS = 0 AND @remainingQty != 0
+	BEGIN
+		IF @convertedQty <= @qtyToPay
+		BEGIN
+			INSERT INTO @voucherDetailPayment
+			SELECT @billId, @amountToPay --pay to whole total of voucher detail
+			SET @remainingQty = @remainingQty - @convertedQtyToPaymentUOM
+		END
+		ELSE
+		BEGIN
+			INSERT INTO @voucherDetailPayment
+			SELECT @billId, CAST(@convertedDetailCostFromPaymentUOM * @remainingQty AS DECIMAL(18,2)) --pay the remaining available qty
+			SET @remainingQty = 0
+		END
+		FETCH c INTO @billId, @qtyToPay, @amountToPay, @convertedQty;;
+	END
+
+	CLOSE c; DEALLOCATE c;
+
+	INSERT INTO @returnTable
+	SELECT
+		intBillId
+		,CAST(SUM(dblPayment) AS DECIMAL(18,2))
+	FROM @voucherDetailPayment 
+	GROUP BY intBillId
 
 	RETURN;
 END 
