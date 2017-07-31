a6374ed799fa38333adb66462511744915630f6c
jeffrey trajano <jeffrey.trajano@irely.com>
2017-08-01 00:17:35 +0800

    GL-3590 [GL Accounts] Cannot Delete Accounts

--- i21Database/dbo/Stored Procedures/uspGLDeleteAccount.sql
+++ i21Database/dbo/Stored Procedures/uspGLDeleteAccount.sql
@@ -4,34 +4,31 @@ AS
 BEGIN TRY
 	BEGIN TRANSACTION
 		DECLARE @intLegacyReferenceId INT  = 0
-		SELECT @intLegacyReferenceId = intLegacyReferenceId
-		FROM tblGLCOACrossReference  WHERE inti21Id = @intAccountId
+		SELECT @intLegacyReferenceId = intLegacyReferenceId  
+		FROM tblGLCOACrossReference  WHERE inti21Id = @intAccountId 
 			AND ISNULL(ysnOrigin,0) = 0
-		
-		DELETE FROM tblGLCOACrossReference where intLegacyReferenceId = @intLegacyReferenceId
-		IF EXISTS (SELECT TOP 1 1 FROM sys.tables where tables.name = 'glactmst')
+		IF @intLegacyReferenceId > 0
 		BEGIN
-			DECLARE @strSQL NVARCHAR(300)
-			SELECT @strSQL = 'DELETE FROM glactmst where A4GLIdentity = ' + CAST( @intLegacyReferenceId AS NVARCHAR(10))
-			EXEC(@strSQL)
+			DELETE FROM tblGLCOACrossReference where intLegacyReferenceId = @intLegacyReferenceId
+			IF EXISTS (SELECT TOP 1 1 FROM sys.tables where tables.name = 'glactmst')
+				DELETE FROM glactmst where A4GLIdentity = @intLegacyReferenceId 
 		END
 		DELETE FROM tblGLCrossReferenceMapping WHERE intAccountId = @intAccountId
 		DELETE FROM tblGLAccountSegmentMapping WHERE intAccountId = @intAccountId
 		DELETE FROM tblGLAccount where intAccountId = @intAccountId
-		
 	IF @@TRANCOUNT > 0 COMMIT TRANSACTION;
 END TRY
 BEGIN CATCH
 	IF @@TRANCOUNT > 0 ROLLBACK TRANSACTION;
-	DECLARE @ErrorMessage NVARCHAR(4000);
-	DECLARE @ErrorSeverity INT;
-	DECLARE @ErrorState INT;
-	SELECT
-	@ErrorMessage = ERROR_MESSAGE(),
-	@ErrorSeverity = ERROR_SEVERITY(),
-	@ErrorState = ERROR_STATE();
-	RAISERROR (@ErrorMessage, -- Message text.
-	@ErrorSeverity, -- Severity.
-	@ErrorState -- State.
-	);
+	DECLARE @ErrorMessage NVARCHAR(4000);  
+	DECLARE @ErrorSeverity INT;  
+	DECLARE @ErrorState INT;  
+	SELECT   
+	@ErrorMessage = ERROR_MESSAGE(),  
+	@ErrorSeverity = ERROR_SEVERITY(),  
+	@ErrorState = ERROR_STATE();  
+	RAISERROR (@ErrorMessage, -- Message text.  
+	@ErrorSeverity, -- Severity.  
+	@ErrorState -- State.  
+	);  
 END CATCH
