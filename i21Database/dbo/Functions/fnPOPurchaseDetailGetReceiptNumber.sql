91314a1422a8d2b614bdbd8e664812a9ed446ac5
Mon Gonzales <mon.gonzales@irely.com>
2017-08-01 14:11:29 +0800

    AP-3105

---
+++ i21Database/dbo/Functions/fnPOPurchaseDetailGetReceiptNumber.sql
@@ -0,0 +1,22 @@
+﻿CREATE FUNCTION [dbo].[fnPOPurchaseDetailGetReceiptNumber]
+(
+	@PurchaseDetailId int
+)
+RETURNS NVARCHAR(MAX)
+AS
+BEGIN
+	DECLARE @col NVARCHAR(MAX);	
+	select @col = COALESCE(@col + ', ', '') + RTRIM(LTRIM( a.strReceiptNumber)) 
+	from tblICInventoryReceipt a
+		join tblICInventoryReceiptItem b
+			on a.intInventoryReceiptId = b.intInventoryReceiptId
+				and a.strReceiptType = 'Purchase Order'
+		join tblPOPurchaseDetail c
+			on c.intPurchaseDetailId = b.intLineNo
+	where c.intPurchaseDetailId = @PurchaseDetailId
+
+	order by c.intPurchaseDetailId
+
+	RETURN @col
+
+END
---
+++ i21Database/dbo/Views/vyuPOPurchaseDetailReceiptNumber.sql
@@ -0,0 +1,13 @@
+﻿CREATE VIEW [dbo].[vyuPOPurchaseDetailReceiptNumber]
+	AS 
+
+
+
+	select
+		DISTINCT
+		intId = a.intPurchaseDetailId,
+		intPurchaseDetailId,
+		strSource = dbo.fnPOPurchaseDetailGetReceiptNumber(a.intPurchaseDetailId),
+		strFilterId = dbo.fnPOPurchaseDetailGetReceiptId(a.intPurchaseDetailId)
+	from 
+	tblPOPurchaseDetail a
