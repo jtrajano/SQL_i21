675b4e88fbf596e6ae68c380b654e3400bf9f536
Kevin Estrada <kevin.estrada@irely.com>
2017-08-03 17:34:51 +0800

    FRM-5348

--- i21PreDeployment/Scripts/Pre-Deployment/24_SC/1_OfflineGuid.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/24_SC/1_OfflineGuid.sql
@@ -1,12 +1,14 @@
 ﻿PRINT N'***** BEGIN INSERT OFFLINE GUID (SCALE) *****'
 GO
+IF EXISTS(SELECT TOP 1 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE [TABLE_NAME] = 'tblSCTicket')
+BEGIN
 IF OBJECT_ID('dbo.[UK_tblSCTicket_strOfflineGuid]') IS NULL
 BEGIN
 EXEC('ALTER TABLE tblSCTicket  ADD [strOfflineGuid] NVARCHAR(100) COLLATE Latin1_General_CI_AS')
 EXEC('UPDATE tblSCTicket SET [strOfflineGuid] = NEWID()')
 EXEC('ALTER TABLE tblSCTicket ALTER COLUMN strOfflineGuid nvarchar(100)')
 EXEC('CREATE UNIQUE NONCLUSTERED INDEX UK_tblSCTicket_strOfflineGuid ON tblSCTicket(strOfflineGuid) WHERE strOfflineGuid IS NOT NULL') 
-
+END
 
 --EXEC('ALTER TABLE tblSCTicket ADD CONSTRAINT UK_tblSCTicket_strOfflineGuid UNIQUE (strOfflineGuid)')
 
