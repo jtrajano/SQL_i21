efed7f561dc49a8ce72c7764e2228c42b8e322b5
Vinoth Kumar T <vinoth.t@irely.com>
2017-08-07 19:52:20 +0530

    QC-342

--- i21Database/dbo/Tables/tblQMCompanyPreference.sql
+++ i21Database/dbo/Tables/tblQMCompanyPreference.sql
@@ -13,6 +13,7 @@
 	ysnFilterContractByERPPONumber BIT NULL CONSTRAINT [DF_tblQMCompanyPreference_ysnFilterContractByERPPONumber] DEFAULT 0,
 	ysnEnableSampleTypeByUserRole BIT NULL CONSTRAINT [DF_tblQMCompanyPreference_ysnEnableSampleTypeByUserRole] DEFAULT 0,
 	ysnShowSampleFromAllLocation BIT NULL CONSTRAINT [DF_tblQMCompanyPreference_ysnShowSampleFromAllLocation] DEFAULT 0,
+	ysnValidateMultipleValuesInTestResult BIT NULL CONSTRAINT [DF_tblQMCompanyPreference_ysnValidateMultipleValuesInTestResult] DEFAULT 0,
 
     CONSTRAINT [PK_tblQMCompanyPreference_intCompanyPreferenceId] PRIMARY KEY ([intCompanyPreferenceId]),
 	CONSTRAINT [FK_tblQMCompanyPreference_tblICLotStatus_intApproveLotStatus] FOREIGN KEY ([intApproveLotStatus]) REFERENCES [tblICLotStatus]([intLotStatusId]),
