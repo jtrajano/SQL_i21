3030547f6ac99910fc4c41a302d0ad8e7c4b3da2
bonn ponce <bonn.ponce@irely.com>
2017-08-09 10:00:30 +0800

    CF-1169

--- i21Database/dbo/Stored Procedures/uspCFInsertTransactionRecord.sql
+++ i21Database/dbo/Stored Procedures/uspCFInsertTransactionRecord.sql
@@ -620,7 +620,8 @@ BEGIN
 				SET @intVehicleId =
 				(SELECT TOP 1 intVehicleId
 				FROM @tblCFNumericVehicle
-				WHERE CAST(strVehicleNumber AS INT) = CAST(@strVehicleId AS INT))
+				WHERE CAST(strVehicleNumber AS BIGINT) = CAST(@strVehicleId AS BIGINT))
+
 
 			END
 			ELSE
