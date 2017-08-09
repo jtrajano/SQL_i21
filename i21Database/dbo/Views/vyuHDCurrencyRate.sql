970f845511d014f39038dcd1f65410c825915c57
Jayson Ramos <jayson.ramos@iRely.com>
2017-08-10 02:41:16 +0800

    HD-2160

---
+++ i21Database/dbo/Views/vyuHDCurrencyRate.sql
@@ -0,0 +1,13 @@
+﻿CREATE VIEW [dbo].[vyuHDCurrencyRate]
+	AS
+		select
+			a.intCurrencyExchangeRateId
+			,a.intFromCurrencyId
+			,a.intToCurrencyId
+			,strCurrencyExchangeRate = b.strCurrency + ' To ' + c.strCurrency
+			,strFromCurrency = b.strCurrency
+			,strToCurrency = c.strCurrency 
+		from
+			tblSMCurrencyExchangeRate a
+			left join tblSMCurrency b on b.intCurrencyID = a.intFromCurrencyId
+			left join tblSMCurrency c on c.intCurrencyID = a.intToCurrencyId
