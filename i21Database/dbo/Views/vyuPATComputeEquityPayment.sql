54fa787bedeb6b5ef78faa8d9de95374e99e37b6
Jb Mago <benedict.mago@irely.com>
2017-08-10 16:08:57 +0800

    PAT-755

--- i21Database/dbo/Views/vyuPATComputeEquityPayment.sql
+++ i21Database/dbo/Views/vyuPATComputeEquityPayment.sql
@@ -7,12 +7,12 @@ SELECT	NEWID() as id,
 		RR.ysnQualified,
 		dblEquityAvailable  = CE.dblEquity - CE.dblEquityPaid,
 		CompLoc.intCompanyLocationId,
-		dblWithholdPercent = CASE WHEN APV.ysnWithholding = 1 THEN (CompLoc.dblWithholdPercent/100) ELSE 0 END
+		dblWithholdPercent = CASE WHEN ISNULL(APV.ysnWithholding,0) = 1 THEN (CompLoc.dblWithholdPercent/100) ELSE 0 END
 FROM tblPATCustomerEquity CE
 INNER JOIN tblEMEntity EM
 	ON EM.intEntityId = CE.intCustomerId
-INNER JOIN tblAPVendor APV
-	ON APV.[intEntityId] = CE.intCustomerId
+LEFT OUTER JOIN tblAPVendor APV
+	ON APV.intEntityId = CE.intCustomerId
 INNER JOIN tblPATRefundRate RR
 	ON RR.intRefundTypeId = CE.intRefundTypeId
 CROSS APPLY (SELECT intCompanyLocationId,dblWithholdPercent FROM tblSMCompanyLocation) CompLoc
