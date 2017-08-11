7123c87c63c905d7e1baff0f3a0c1efbe7d08206
Erick F. Dela Cruz <erick.delacruz@irely.com>
2017-08-11 10:48:26 +0800

    AP-3887

--- i21Database/dbo/Views/vyuAPVoucherCommodity.sql
+++ i21Database/dbo/Views/vyuAPVoucherCommodity.sql
@@ -1,15 +1,15 @@
 ﻿CREATE VIEW [dbo].[vyuAPVoucherCommodity]
 AS
 
-SELECT
+SELECT 
 	intBillId,
-	MAX(commodity.intCount) intCount,
 	commodity.intCommodityId, 
+	commodity.intCount,
 	ISNULL(commodity.strCommodityCode, 'None') AS strCommodityCode
 FROM tblAPBill A
 CROSS APPLY (
-	SELECT 
-		COUNT(DISTINCT commodity.intCommodityId) intCount, 
+	SELECT TOP 1
+		COUNT(commodity.intCommodityId) intCount, 
 		commodity.intCommodityId,
 		commodity.strCommodityCode
 	FROM tblAPBillDetail detail
@@ -17,5 +17,5 @@ CROSS APPLY (
 	LEFT JOIN tblICCommodity commodity ON item.intCommodityId = commodity.intCommodityId
 	WHERE detail.intBillId = A.intBillId
 	GROUP BY commodity.intCommodityId, commodity.strCommodityCode
+	ORDER BY COUNT(commodity.intCommodityId) DESC
 ) commodity
-GROUP BY commodity.intCommodityId, commodity.strCommodityCode, intBillId
