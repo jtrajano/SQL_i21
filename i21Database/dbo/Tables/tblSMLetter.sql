0c622f9166041a4b48521b91b77bc5f8a8e8858f
Dan Michael Jalimao <dan.jalimao@irely.com>
2017-08-12 14:27:41 +0800

    SM-3039

--- i21Database/dbo/Tables/tblSMLetter.sql
+++ i21Database/dbo/Tables/tblSMLetter.sql
@@ -6,7 +6,8 @@
     [blbMessage] VARBINARY(MAX) NULL, 
 	[strModuleName] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
 	[ysnSystemDefined] BIT NULL DEFAULT 0,
-    [intConcurrencyId] INT NOT NULL, 
+    [intSourceLetterId] INT NULL, 
+    [intConcurrencyId] INT NOT NULL DEFAULT 1, 
     CONSTRAINT [PK_tblSMLetter] PRIMARY KEY ([intLetterId]), 
     CONSTRAINT [UQ_tblSMLetter_strName] UNIQUE ([strName]) 
 )
