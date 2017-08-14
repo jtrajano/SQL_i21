17fbf7e40a80f3aaeb8bc85911f713b0e5216684
Smith Wilson de Jesus <smith.dejesus@irely.com>
2017-08-14 17:27:17 +0800

    AR-5165 Receive Payment Detail - No Payment Method Available

---
+++ i21Database/dbo/Views/vyuARPaymentMethodForReceivePayments.sql
@@ -0,0 +1,23 @@
+﻿CREATE VIEW [dbo].[vyuARPaymentMethodForReceivePayments]
+AS 
+
+SELECT
+	ROW_NUMBER() OVER(ORDER BY intPaymentMethodID DESC) AS intId
+	,intPaymentMethodID 
+	,strPaymentMethod
+	,ysnActive
+	,intEntityCardInfoId = NULL
+	,intEntityId = 0 
+FROM tblSMPaymentMethod
+WHERE strPaymentMethod <> 'Credit Card' and ysnActive = 1
+
+UNION ALL 
+SELECT
+	ROW_NUMBER() OVER(ORDER BY intEntityCardInfoId) + (SELECT COUNT(*) FROM tblSMPaymentMethod WHERE strPaymentMethod <> 'Credit Card' and ysnActive = 1) AS intId  
+	,11
+	,strCreditCardNumber
+	,1 
+	,intEntityCardInfoId
+	,intEntityId
+FROM tblEMEntityCardInformation
+WHERE strToken is not null
