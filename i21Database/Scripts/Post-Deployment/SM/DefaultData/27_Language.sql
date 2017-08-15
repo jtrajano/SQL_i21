adf3b9e2acef3c1c59b24bea3a747ae434b1032e
Dan Michael Jalimao <dan.jalimao@irely.com>
2017-08-15 09:11:45 +0800

    SM-3130

--- i21Database/Scripts/Post-Deployment/SM/DefaultData/27_Language.sql
+++ i21Database/Scripts/Post-Deployment/SM/DefaultData/27_Language.sql
@@ -1,4 +1,11 @@
 ﻿GO
-	INSERT INTO [tblSMLanguage] ([strLanguage], [ysnDefault], [intConcurrencyId])
-     VALUES ('English', 1, 1)
+	IF NOT EXISTS (SELECT TOP 1 1 FROM tblSMLanguage WHERE intLanguageId = 1)
+	BEGIN
+		SET IDENTITY_INSERT [dbo].[tblSMLanguage] ON
+
+		INSERT INTO [tblSMLanguage] ([intLanguageId], [strLanguage], [ysnDefault], [intConcurrencyId])
+		VALUES (1, 'English', 1, 1)
+
+		SET IDENTITY_INSERT [dbo].[tblSMUserRole] OFF
+	END
 GO
