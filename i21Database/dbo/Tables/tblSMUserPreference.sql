06d040f62d653b44dbae893dfcb74619a13a7acb
Dan Michael Jalimao <dan.jalimao@irely.com>
2017-08-18 14:44:58 +0800

    SM-3088

--- i21Database/dbo/Tables/tblSMUserPreference.sql
+++ i21Database/dbo/Tables/tblSMUserPreference.sql
@@ -12,7 +12,8 @@
 	--[imgMenuBackground] VARBINARY (MAX) NULL, 
 	--[dblImageSizePercent] DECIMAL (18, 6) NULL DEFAULT 0,
 	[ysnPreviewAttachment] BIT NULL DEFAULT 1, 
-	[ysnEnableScreenLockout] BIT NULL DEFAULT 0, 
+	[ysnEnableScreenLockout] BIT NULL DEFAULT 0,
+	[ysnKeepSearchScreensOpen] BIT NOT NULL DEFAULT 0, 
 	[intScreenTimeout] INT NOT NULL DEFAULT 30, 
     [intConcurrencyId] INT NOT NULL DEFAULT 1--, 
     --CONSTRAINT [FK_tblSMUserPreference_tblSMUserSecurity] FOREIGN KEY ([intUserSecurityId]) REFERENCES tblSMUserSecurity(intUserSecurityID) ON DELETE CASCADE
