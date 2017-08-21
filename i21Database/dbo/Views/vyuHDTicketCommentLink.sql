5c978a1884dd4e8c08ebb3ca44fa3f683eb1acbd
Jayson Ramos <jayson.ramos@iRely.com>
2017-08-22 04:32:25 +0800

    HD-2168

---
+++ i21Database/dbo/Views/vyuHDTicketCommentLink.sql
@@ -0,0 +1,14 @@
+﻿CREATE VIEW [dbo].[vyuHDTicketCommentLink]
+	AS
+	select
+		a.*
+		,strCreatedFullName = c.strName + (case when b.strLocation is null then ' (User)' else ' (Agent)' end)
+		,strCreatedLocation = (case when b.strLocation is null then e.strLocationName else b.strLocation end)
+		,imgCreatedPhoto = c.imgPhoto
+		,strMessage = (case when a.dtmSent is null then 'Draft' else dbo.fnHDTicketCommentSentMessage(a.dtmSent, GETDATE()) end) 
+	from
+		tblHDTicketComment a
+		left join tblEMEntity c on c.intEntityId = a.intCreatedUserEntityId
+		left join tblEMEntityToContact d on d.intEntityContactId = a.intCreatedUserEntityId
+		left join tblEMEntityLocation e on e.intEntityLocationId = d.intEntityLocationId
+		left join tblSMUserSecurity b on b.intEntityId = a.intCreatedUserEntityId
