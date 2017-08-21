4ad18322eec9984f49d1145960c5b984ea43d0d6
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2017-08-21 12:49:42 +0530

    Merged RM-848

---
+++ i21Database/dbo/Views/vyuRKMarketDetail.sql
@@ -0,0 +1,12 @@
+﻿CREATE VIEW vyuRKMarketDetail
+AS
+SELECT intFutureMarketId
+	,strFutMarketName
+	,strFutSymbol AS strFutSymbol
+	,um1.strUnitMeasure
+	,c.strCurrency strCurrency
+	,um.strUnitMeasure strForecastUnitMeasure
+FROM tblRKFutureMarket fm
+INNER JOIN tblSMCurrency c ON c.intCurrencyID = fm.intCurrencyId
+INNER JOIN tblICUnitMeasure um1 ON fm.intUnitMeasureId = um1.intUnitMeasureId
+LEFT JOIN tblICUnitMeasure um ON fm.intForecastWeeklyConsumptionUOMId = um.intUnitMeasureId
