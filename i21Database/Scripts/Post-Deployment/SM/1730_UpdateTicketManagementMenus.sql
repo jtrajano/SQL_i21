83eb8cb37f53b2d709c6fa56c4145432daaa9bf3
JeffTrajano <jlt212000@yahoo.com>
2017-08-22 15:43:16 +0800

    SM-3129 Delivery Sheet screen is not opening; with console error message

--- i21Database/Scripts/Post-Deployment/SM/1730_UpdateTicketManagementMenus.sql
+++ i21Database/Scripts/Post-Deployment/SM/1730_UpdateTicketManagementMenus.sql
@@ -3,4 +3,10 @@
 	FROM tblSMUserRoleMenu rm
 	INNER JOIN tblSMMasterMenu mm ON rm.intMenuId = mm.intMenuID
 	WHERE strMenuName = 'Delivery Sheets' AND ysnAvailable IS NULL
+
+	IF EXISTS (SELECT TOP 1 1 FROM tblGLCompanyPreference WHERE ysnDeliverySheet = 1)
+    	EXEC uspSMSetMenuAvailability 'Delivery Sheets', 'Ticket Management', 1 
+	ELSE
+    	EXEC uspSMSetMenuAvailability 'Delivery Sheets', 'Ticket Management', 0
+
 GO
