4ed36167cff4594c17e1717f39927565e9ade3b1
Jb Mago <benedict.mago@irely.com>
2017-08-23 21:56:15 +0800

    PAT-775

--- i21Database/dbo/Stored Procedures/uspPATCreateVoucherForProcessRefund.sql
+++ i21Database/dbo/Stored Procedures/uspPATCreateVoucherForProcessRefund.sql
@@ -161,8 +161,9 @@ BEGIN
 			,@voucherDate = @dtmDate
 			,@billId = @intCreatedBillId OUTPUT;
 
-		UPDATE tblAPBillDetail SET int1099Form = 4, int1099Category= 0, dbl1099 = ROUND(@dbl1099Amount, 2), intCurrencyId = [dbo].[fnSMGetDefaultCurrency]('FUNCTIONAL') 
-		WHERE intBillId = @intCreatedBillId;
+		UPDATE tblAPBillDetail SET int1099Form = 4, int1099Category = 1, dbl1099 = ROUND(@dbl1099Amount, 2), intCurrencyId = [dbo].[fnSMGetDefaultCurrency]('FUNCTIONAL') 
+		WHERE intBillId = @intCreatedBillId AND strMiscDescription != N'Service Fee';
+
 		UPDATE tblPATRefundCustomer SET intBillId = @intCreatedBillId WHERE intRefundCustomerId = @intRefundCustomerId;
 
 		IF EXISTS(SELECT 1 FROM tblAPBillDetailTax WHERE intBillDetailId IN (SELECT intBillDetailId FROM tblAPBillDetail WHERE intBillId = @intCreatedBillId))
