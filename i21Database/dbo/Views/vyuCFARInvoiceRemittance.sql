6b123e160bc00e7f74a8cade0e8cd0de4d8bf686
bonn ponce <bonn.ponce@irely.com>
2017-08-24 06:43:41 +0800

    CF-802

---
+++ i21Database/dbo/Views/vyuCFARInvoiceRemittance.sql
@@ -0,0 +1,28 @@
+﻿
+CREATE VIEW [dbo].[vyuCFARInvoiceRemittance]
+AS
+SELECT
+ intEntityCustomerId					AS intCustomerId
+,strCFTempInvoiceReportNumber			AS strTempInvoiceReportNumber
+,dblCFAccountTotalAmount				AS dblAccountTotalAmount				
+,dblCFAccountTotalDiscount				AS dblAccountTotalDiscount				
+,intCFTermID							AS intTermID							
+,dtmCFInvoiceDate						AS dtmInvoiceDate						
+,dblCFFeeTotalAmount 					AS dblFeeTotalAmount 					
+,dblCFEligableGallon					AS dblEligableGallon					
+,strCustomerName						AS strCustomerName
+,strCFEmail								AS strEmail								
+,strCFEmailDistributionOption			AS strEmailDistributionOption			
+FROM            dbo.tblARCustomerStatementStagingTable 
+GROUP BY 
+intEntityCustomerId, 
+strCFTempInvoiceReportNumber, 
+dblCFAccountTotalAmount, 
+dblCFAccountTotalDiscount, 
+intCFTermID, 
+dtmCFInvoiceDate, 
+dblCFFeeTotalAmount, 
+dblCFEligableGallon, 
+strCustomerName,
+strCFEmail, 
+strCFEmailDistributionOption
