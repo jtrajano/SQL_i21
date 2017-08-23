834a472af88526398679b63c64481abf6f3e6122
JeffTrajano <jlt212000@yahoo.com>
2017-08-23 10:48:35 +0800

    GL-3688 17.3 RC SQL script is failing

--- i21Database/Scripts/Post-Deployment/SM/1730_UpdateTicketManagementMenus.sql
+++ i21Database/Scripts/Post-Deployment/SM/1730_UpdateTicketManagementMenus.sql
@@ -4,7 +4,7 @@
 	INNER JOIN tblSMMasterMenu mm ON rm.intMenuId = mm.intMenuID
 	WHERE strMenuName = 'Delivery Sheets' AND ysnAvailable IS NULL
 
-	IF EXISTS (SELECT TOP 1 1 FROM tblLGCompanyPreference WHERE ysnDeliverySheet = 1)
+	IF EXISTS (SELECT TOP 1 1 FROM tblGRCompanyPreference WHERE ysnDeliverySheet = 1)
     	EXEC uspSMSetMenuAvailability 'Delivery Sheets', 'Ticket Management', 1 
 	ELSE
     	EXEC uspSMSetMenuAvailability 'Delivery Sheets', 'Ticket Management', 0
