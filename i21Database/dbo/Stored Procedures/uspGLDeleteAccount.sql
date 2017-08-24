79e826c20d37b0df56f144779fe4c7c78faf4b0a
JeffTrajano <jlt212000@yahoo.com>
2017-08-24 14:41:57 +0800

    GL-2951 We need to be able to consolidate the financials of multiple i21 companies

--- i21Database/dbo/Stored Procedures/uspGLDeleteAccount.sql
+++ i21Database/dbo/Stored Procedures/uspGLDeleteAccount.sql
@@ -4,15 +4,21 @@ AS
 BEGIN TRY
 	BEGIN TRANSACTION
 		DECLARE @intLegacyReferenceId INT  = 0
-		SELECT @intLegacyReferenceId = intLegacyReferenceId  
+		DECLARE @strSQL NVARCHAR(500)
+		SELECT @intLegacyReferenceId = intLegacyReferenceId
 		FROM tblGLCOACrossReference  WHERE inti21Id = @intAccountId 
-			AND ISNULL(ysnOrigin,0) = 0
 		IF @intLegacyReferenceId > 0
 		BEGIN
+			IF EXISTS (SELECT TOP 1 1 FROM tblGLCOACrossReference WHERE intLegacyReferenceId = @intLegacyReferenceId AND ysnOrigin = 1)
+			BEGIN
+				RAISERROR('Origin Accounts are not allowed to be deleted.',16,1)
+				RETURN
+			END
+
 			DELETE FROM tblGLCOACrossReference where intLegacyReferenceId = @intLegacyReferenceId
 			IF EXISTS (SELECT TOP 1 1 FROM sys.tables where tables.name = 'glactmst')
 			BEGIN
-				DECLARE @strSQL NVARCHAR(300)
+
 				SELECT @strSQL = 'DELETE FROM glactmst where A4GLIdentity = ' + CAST( @intLegacyReferenceId AS NVARCHAR(10))
 				EXEC(@strSQL)
 		    END
--- i21Database/dbo/Stored Procedures/uspGLGetConsolidateWarnings.sql
+++ i21Database/dbo/Stored Procedures/uspGLGetConsolidateWarnings.sql
@@ -1,18 +1,14 @@
 ﻿CREATE PROCEDURE [dbo].[uspGLGetConsolidateWarnings]
-(@dtmDate DATETIME,
-@strCompanyName nvarchar(100),
-@resultId  INT OUT
+(
+	@dtmDate DATETIME,
+	@strCompanyName nvarchar(100)
 )
-
 AS
-
-
-DECLARE @ysnOpen BIT, @ysnUnpostedTrans BIT, 
+DECLARE @ysnOpen BIT, @ysnUnpostedTrans BIT,
 	@intFiscalYearId INT,@intFiscalPeriodId INT,
 	@dtmStartDate DATETIME,	@dtmEndDate DATETIME
 
 SELECT @ysnOpen = 0 , @ysnUnpostedTrans = 0
-
 SELECT TOP 1
 @ysnOpen=  ysnOpen,
 @intFiscalYearId = intFiscalYearId,
@@ -21,25 +17,16 @@ FROM dbo.tblGLFiscalYearPeriod
 WHERE @dtmDate
 BETWEEN dtmStartDate and dtmEndDate
 
-
 IF @intFiscalYearId IS NULL
 BEGIN
-	INSERT INTO tblGLConsolidateResult ([ysnFiscalOpen] , [ysnUnpostedTrans],[strResult], strCompanyName)
-	SELECT  0 , 0, ' Fiscal Period not existing in subsidiary company.', @strCompanyName
-	SELECT @resultId = SCOPE_IDENTITY()
+	SELECT  0 ysFiscalOpen , 0 ysnUnpostedTrans, ' Fiscal Period not existing in subsidiary company.' strResult, @strCompanyName strCompanyName
 	RETURN
 END
 
+EXEC dbo.uspGLGetAllUnpostedTransactionsByFiscal @intFiscalYearId,1,@intFiscalPeriodId,'GL' ,@ysnUnpostedTrans OUT
 
-
-exec dbo.uspGLGetAllUnpostedTransactionsByFiscal @intFiscalYearId,
-1,
-@intFiscalPeriodId,
-'GL' ,@ysnUnpostedTrans OUT
-	
-IF @ysnOpen = 1 or @ysnUnpostedTrans = 1 
+IF @ysnOpen = 1 or @ysnUnpostedTrans = 1
 BEGIN
-	
 	DECLARE @strResult NVARCHAR(1000)
 	IF @ysnOpen =1 and @ysnUnpostedTrans = 0 SET @strResult = 'Fiscal Period is still open in the subsidiary company.'
 	IF @ysnOpen =0 and @ysnUnpostedTrans = 1 SET @strResult = 'Fiscal Period have unposted transactions in the subsidiary company.'
@@ -47,11 +34,5 @@ BEGIN
 	
 	SELECT @ysnOpen = CASE WHEN @ysnOpen = 1 THEN 0 ELSE 1 END
 	SELECT @ysnUnpostedTrans = CASE WHEN @ysnUnpostedTrans = 1 THEN 0 ELSE 1 END
-	
-	INSERT INTO tblGLConsolidateResult ([ysnFiscalOpen] , [ysnUnpostedTrans],[strResult], strCompanyName)
-	SELECT @ysnOpen ysnFiscalOpen, @ysnUnpostedTrans ysnUnpostedTrans, @strResult, @strCompanyName
-
-	
-	SELECT @resultId = SCOPE_IDENTITY()
-	
+	SELECT @ysnOpen ysnFiscalOpen, @ysnUnpostedTrans ysnUnpostedTrans, @strResult strResult, @strCompanyName
 END
