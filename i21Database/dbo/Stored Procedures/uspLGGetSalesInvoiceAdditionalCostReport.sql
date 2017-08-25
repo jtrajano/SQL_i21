f0856c79b9765e4dba2ff370ec2c18cb197e7dd1
James Balachander <balachander.j@irely.com>
2017-08-25 12:33:32 +0530

    LG-1143

--- i21Database/dbo/Stored Procedures/uspLGGetSalesInvoiceAdditionalCostReport.sql
+++ i21Database/dbo/Stored Procedures/uspLGGetSalesInvoiceAdditionalCostReport.sql
@@ -6,27 +6,16 @@ BEGIN
 
 	SET @intInvoiceId = @xmlParam
 
-	    SELECT 
-		strItemDescription = WS.strActivity,
-		dblAmount = WS.dblBillAmount,
-		InvCur.strCurrency
-	FROM tblARInvoice Inv
-	JOIN tblSMCurrency InvCur ON InvCur.intCurrencyID = Inv.intCurrencyId
-	JOIN tblLGLoad L ON L.intLoadId = Inv.intLoadId
-	LEFT JOIN tblLGLoadWarehouse LW ON LW.intLoadId = L.intLoadId
-	LEFT JOIN tblLGLoadWarehouseServices WS ON WS.intLoadWarehouseId = LW.intLoadWarehouseId
-	WHERE Inv.intInvoiceId = @intInvoiceId AND WS.dblBillAmount > 0.00
-
-	UNION ALL
-
-	SELECT 
+	SELECT
+		Inv.intLoadId,
+		InvDet.intItemId,
 		strItemDescription = Item.strDescription,
-		dblAmount = LC.dblAmount,
+		dblAmount = InvDet.dblTotal,
 		InvCur.strCurrency
 	FROM tblARInvoice Inv
+	JOIN tblARInvoiceDetail InvDet ON InvDet.intInvoiceId = Inv.intInvoiceId 
+		AND InvDet.intItemId NOT IN (SELECT IsNull(SC.intCostType, 0) FROM tblLGLoadStorageCost SC WHERE SC.intLoadId=Inv.intLoadId)
+	JOIN tblICItem Item ON Item.intItemId = InvDet.intItemId
 	JOIN tblSMCurrency InvCur ON InvCur.intCurrencyID = Inv.intCurrencyId
-	JOIN tblLGLoad L ON L.intLoadId = Inv.intLoadId
-	JOIN tblLGLoadCost LC ON LC.intLoadId = L.intLoadId AND LC.strEntityType='Customer'
-	JOIN tblICItem Item on Item.intItemId = LC.intItemId
-	WHERE Inv.intInvoiceId = @intInvoiceId AND LC.dblAmount > 0.0
+	WHERE Inv.intInvoiceId = @intInvoiceId AND Item.strType = 'Other Charge'
 END
