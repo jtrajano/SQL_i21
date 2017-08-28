bbc51d7ede4b9392aec8039654e3ea07c062c4c7
Gopinath Nayak <gopinath.n@irely.com>
2017-08-28 16:19:11 +0530

    GRN-672

---
+++ i21Database/dbo/Views/vyuGRSettlementInboundSubReport.sql
@@ -0,0 +1,92 @@
+﻿CREATE VIEW [dbo].[vyuGRSettlementInboundSubReport]
+AS
+SELECT 
+	 intPaymentId
+	,strDiscountCode
+	,strDiscountCodeDescription
+	,(SUM(WeightedAverageReading) / SUM(Net)) AS WeightedAverageReading
+	,(SUM(WeightedAverageShrink) / SUM(Net)) AS WeightedAverageShrink
+	,SUM(dblDiscountAmount) AS Discount
+	,SUM(dblAmount) AS Amount
+	,SUM(dblTax) AS Tax
+FROM (
+		 SELECT 
+		 intPaymentId
+		,strDiscountCode
+		,strDiscountCodeDescription
+		,Net
+		,(Net * dblGradeReading) AS WeightedAverageReading
+		,(Net * dblShrinkPercent) AS WeightedAverageShrink
+		,dblDiscountAmount
+		,dblAmount
+		,dblTax
+	FROM (
+		SELECT --Discount Detail 
+			 PYMT.intPaymentId
+			,Bill.strBillId AS strId
+			,BillDtl.intBillId
+			,BillDtl.intItemId
+			,Item.strShortName AS strDiscountCode
+			,Item.strItemNo AS strDiscountCodeDescription
+			,INVRCPTCHR.dblRate AS dblDiscountAmount
+			,dblShrinkPercent = ISNULL((
+										SELECT dblShrinkPercent
+										FROM tblQMTicketDiscount TD
+										JOIN tblGRDiscountScheduleCode DS ON TD.intDiscountScheduleCodeId = DS.intDiscountScheduleCodeId
+										WHERE TD.intTicketId = CASE 
+												WHEN INVRCPT.intSourceType = 4
+													THEN (
+															SELECT TOP 1 SC.intTicketId
+															FROM tblGRCustomerStorage GR
+															JOIN tblSCTicket SC ON GR.intTicketId = SC.intTicketId
+															WHERE intCustomerStorageId = INVRCPTITEM.intSourceId
+															)
+												ELSE (
+														SELECT TOP 1 SC.intTicketId
+														FROM tblSCTicket SC
+														WHERE intTicketId = INVRCPTITEM.intSourceId
+														)
+												END
+											AND DS.intItemId = BillDtl.intItemId
+										), 0)
+			
+			,dblGradeReading = ISNULL((
+										SELECT dblGradeReading
+										FROM tblQMTicketDiscount TD
+										JOIN tblGRDiscountScheduleCode DS ON TD.intDiscountScheduleCodeId = DS.intDiscountScheduleCodeId
+										WHERE TD.intTicketId = CASE 
+												WHEN INVRCPT.intSourceType = 4
+													THEN (
+															SELECT TOP 1 SC.intTicketId
+															FROM tblGRCustomerStorage GR
+															JOIN tblSCTicket SC ON GR.intTicketId = SC.intTicketId
+															WHERE intCustomerStorageId = INVRCPTITEM.intSourceId
+															)
+												ELSE (
+														SELECT TOP 1 SC.intTicketId
+														FROM tblSCTicket SC
+														WHERE intTicketId = INVRCPTITEM.intSourceId
+														)
+												END
+											AND DS.intItemId = BillDtl.intItemId
+										), 0)
+			
+			,BillDtl.dblTotal AS dblAmount
+			,BillDtl.dblTax AS dblTax
+			,PYMTDTL.dblTotal AS Net
+		
+		FROM tblAPPayment PYMT
+		JOIN tblAPPaymentDetail PYMTDTL ON PYMT.intPaymentId = PYMTDTL.intPaymentId
+		JOIN tblAPBill Bill ON PYMTDTL.intBillId = Bill.intBillId
+		JOIN tblAPBillDetail BillDtl ON BillDtl.intBillId = Bill.intBillId
+		JOIN tblICInventoryReceiptCharge INVRCPTCHR ON BillDtl.intInventoryReceiptChargeId = INVRCPTCHR.intInventoryReceiptChargeId
+		JOIN tblICItem Item ON BillDtl.intItemId = Item.intItemId
+		JOIN tblICInventoryReceipt INVRCPT ON INVRCPTCHR.intInventoryReceiptId = INVRCPT.intInventoryReceiptId
+		JOIN tblICInventoryReceiptItem INVRCPTITEM ON INVRCPT.intInventoryReceiptId = INVRCPTITEM.intInventoryReceiptId		
+		WHERE BillDtl.intInventoryReceiptChargeId IS NOT NULL
+		) tbl
+	) tbl
+GROUP BY intPaymentId
+	,strDiscountCode
+	,strDiscountCodeDescription
+
---
+++ i21Database/dbo/Views/vyuGRSettlementSubReport.sql
@@ -0,0 +1,163 @@
+﻿CREATE VIEW [dbo].[vyuGRSettlementSubReport]
+AS
+SELECT *
+FROM (
+	SELECT 
+		 strId = Bill.strBillId
+		,intBillId = BillDtl.intBillId
+		,intItemId = BillDtl.intItemId
+		,strDiscountCode =Item.strShortName
+		,strDiscountCodeDescription= Item.strItemNo
+		,dblDiscountAmount = CASE 
+								WHEN INVRCPTCHR.strCostMethod = 'Per Unit' THEN INVRCPTCHR.dblRate
+								WHEN INVRCPTCHR.strCostMethod = 'Amount' THEN INVRCPTCHR.dblAmount
+							 END
+		,dblShrinkPercent = ISNULL(ScaleDiscount.dblShrinkPercent, 0)
+		,dblGradeReading =  ISNULL(ScaleDiscount.dblGradeReading, 0)
+		,dblAmount = BillDtl.dblTotal
+		,intInventoryReceiptItemId = ISNULL(BillDtl.intInventoryReceiptItemId, 0)
+		,intInventoryReceiptChargeId = ISNULL(BillDtl.intInventoryReceiptChargeId, 0)
+		,intContractDetailId = ISNULL(BillDtl.intContractDetailId, 0)
+	FROM tblAPBillDetail BillDtl
+	JOIN tblAPBill Bill ON BillDtl.intBillId = Bill.intBillId
+	JOIN tblICItem Item ON BillDtl.intItemId = Item.intItemId
+	JOIN tblICInventoryReceiptCharge INVRCPTCHR ON BillDtl.intInventoryReceiptChargeId = INVRCPTCHR.intInventoryReceiptChargeId	
+	JOIN tblICInventoryReceipt INVRCPT ON INVRCPTCHR.intInventoryReceiptId = INVRCPT.intInventoryReceiptId
+	JOIN tblICInventoryReceiptItem INVRCPTITEM ON INVRCPT.intInventoryReceiptId = INVRCPTITEM.intInventoryReceiptId
+	LEFT JOIN (
+			    SELECT 
+				QM.intTicketId
+			   ,DCode.intItemId
+			   ,QM.dblGradeReading
+			   ,QM.dblShrinkPercent
+				FROM tblQMTicketDiscount QM
+				JOIN tblGRDiscountScheduleCode DCode ON DCode.intDiscountScheduleCodeId = QM.intDiscountScheduleCodeId
+				WHERE QM.strSourceType = 'Scale'
+		  ) ScaleDiscount 
+	 ON ScaleDiscount.intTicketId = INVRCPTITEM.intSourceId AND ScaleDiscount.intItemId = INVRCPTCHR.intChargeId AND INVRCPT.intSourceType = 1
+	WHERE BillDtl.intInventoryReceiptChargeId IS NOT NULL AND Item.strType = 'Other Charge'	
+	
+	UNION ALL
+	
+	SELECT 
+		 strId = Inv.strInvoiceNumber
+		,intInvoiceId=InvDtl.intInvoiceId
+		,intItemId = InvDtl.intItemId
+		,strDiscountCode = Item.strShortName 
+		,strDiscountCodeDescription = Item.strItemNo
+		,dblDiscountAmount = ISNULL((
+				SELECT dblDiscountAmount
+				FROM tblQMTicketDiscount TD
+				JOIN tblGRDiscountScheduleCode DS ON TD.intDiscountScheduleCodeId = DS.intDiscountScheduleCodeId
+				WHERE TD.intTicketId = CASE 
+						WHEN INVSHIP.intSourceType = 4
+							THEN (
+									SELECT TOP 1 SC.intTicketId
+									FROM tblGRCustomerStorage GR
+									JOIN tblSCTicket SC ON GR.intTicketId = SC.intTicketId
+									WHERE intCustomerStorageId = INVSHIPITEM.intSourceId
+									)
+						ELSE (
+								SELECT TOP 1 SC.intTicketId
+								FROM tblSCTicket SC
+								WHERE intTicketId = INVSHIPITEM.intSourceId
+								)
+						END
+					AND DS.intItemId = InvDtl.intItemId
+				), 0)
+		,dblShrinkPercent = ISNULL((
+				SELECT dblShrinkPercent
+				FROM tblQMTicketDiscount TD
+				JOIN tblGRDiscountScheduleCode DS ON TD.intDiscountScheduleCodeId = DS.intDiscountScheduleCodeId
+				WHERE TD.intTicketId = CASE 
+						WHEN INVSHIP.intSourceType = 4
+							THEN (
+									SELECT TOP 1 SC.intTicketId
+									FROM tblGRCustomerStorage GR
+									JOIN tblSCTicket SC ON GR.intTicketId = SC.intTicketId
+									WHERE intCustomerStorageId = INVSHIPITEM.intSourceId
+									)
+						ELSE (
+								SELECT TOP 1 SC.intTicketId
+								FROM tblSCTicket SC
+								WHERE intTicketId = INVSHIPITEM.intSourceId
+								)
+						END
+					AND DS.intItemId = InvDtl.intItemId
+				), 0)
+		,dblGradeReading = ISNULL((
+				SELECT dblGradeReading
+				FROM tblQMTicketDiscount TD
+				JOIN tblGRDiscountScheduleCode DS ON TD.intDiscountScheduleCodeId = DS.intDiscountScheduleCodeId
+				WHERE TD.intTicketId = CASE 
+						WHEN INVSHIP.intSourceType = 4
+							THEN (
+									SELECT TOP 1 SC.intTicketId
+									FROM tblGRCustomerStorage GR
+									JOIN tblSCTicket SC ON GR.intTicketId = SC.intTicketId
+									WHERE intCustomerStorageId = INVSHIPITEM.intSourceId
+									)
+						ELSE (
+								SELECT TOP 1 SC.intTicketId
+								FROM tblSCTicket SC
+								WHERE intTicketId = INVSHIPITEM.intSourceId
+								)
+						END
+					AND DS.intItemId = InvDtl.intItemId
+				), 0)
+		,InvDtl.dblTotal AS dblAmount
+		,0
+		,0
+		,0
+	FROM tblARInvoiceDetail InvDtl
+	JOIN tblARInvoice Inv ON InvDtl.intInvoiceId = Inv.intInvoiceId
+	JOIN tblICInventoryShipmentCharge INVSHIPCHR ON InvDtl.intInventoryShipmentChargeId = INVSHIPCHR.intInventoryShipmentChargeId
+	JOIN tblICItem Item ON InvDtl.intItemId = Item.intItemId
+	JOIN tblICInventoryShipment INVSHIP ON INVSHIPCHR.intInventoryShipmentId = INVSHIP.intInventoryShipmentId
+	JOIN tblICInventoryShipmentItem INVSHIPITEM ON INVSHIP.intInventoryShipmentId = INVSHIPITEM.intInventoryShipmentId
+	JOIN tblSCTicket TICKET ON INVSHIPITEM.intSourceId = TICKET.intTicketId	
+	WHERE InvDtl.intInventoryShipmentChargeId IS NOT NULL
+	
+	UNION ALL
+	
+	SELECT 
+		 strId = Bill.strBillId
+		,intBillId = BillDtl.intBillId
+		,intItemId = BillDtl.intItemId
+		,strDiscountCode = Item.strShortName
+		,strDiscountCodeDescription = Item.strItemNo
+		,dblDiscountAmount = BillDtl.dblCost
+		,dblShrinkPercent = ISNULL(StorageDiscount.dblShrinkPercent, 0)
+		,dblGradeReading =  ISNULL(StorageDiscount.dblGradeReading, 0)
+		,dblAmount = BillDtl.dblTotal
+		,intInventoryReceiptItemId = 0 
+		,intInventoryReceiptChargeId = 0 
+		,intContractDetailId = ISNULL(BillDtl.intContractDetailId, 0)
+	FROM tblAPBillDetail BillDtl
+	JOIN tblAPBill Bill ON BillDtl.intBillId = Bill.intBillId
+	JOIN tblICItem Item ON BillDtl.intItemId = Item.intItemId AND Item.strType = 'Other Charge'
+	JOIN tblGRStorageHistory StrgHstry ON Bill.intBillId = StrgHstry.intBillId
+	LEFT JOIN (
+				 SELECT 
+				 QM.intTicketFileId
+				,DCode.intItemId
+				,QM.dblGradeReading
+				,QM.dblShrinkPercent
+				FROM tblQMTicketDiscount QM
+				JOIN tblGRDiscountScheduleCode DCode ON DCode.intDiscountScheduleCodeId = QM.intDiscountScheduleCodeId
+				WHERE QM.strSourceType = 'Storage'
+		  ) StorageDiscount ON StorageDiscount.intTicketFileId = BillDtl.intCustomerStorageId AND StorageDiscount.intItemId = BillDtl.intItemId
+      WHERE Item.strType = 'Other Charge'
+	) tbl 
+GROUP BY strId
+	,intBillId
+	,intItemId
+	,strDiscountCode
+	,strDiscountCodeDescription
+	,dblDiscountAmount
+	,dblShrinkPercent
+	,dblGradeReading
+	,dblAmount
+	,intInventoryReceiptItemId
+	,intInventoryReceiptChargeId
+	,intContractDetailId
