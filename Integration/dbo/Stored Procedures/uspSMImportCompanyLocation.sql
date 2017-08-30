34fe43e5989db23564b2e19dec226ec7e772d248
IRELY\kkarthick <karthick.k@irely.com>
2017-08-30 12:06:56 +0530

    SM-2832

--- Integration/dbo/Stored Procedures/uspSMImportCompanyLocation.sql
+++ Integration/dbo/Stored Procedures/uspSMImportCompanyLocation.sql
@@ -5,7 +5,8 @@ IF EXISTS(select top 1 1 from sys.procedures where name = 'uspSMImportCompanyLoc
 GO
 
 
-IF (SELECT TOP 1 ysnUsed FROM ##tblOriginMod WHERE strPrefix = 'AG' and strDBName = db_name()) = 1
+IF (SELECT TOP 1 ysnUsed FROM ##tblOriginMod WHERE strPrefix = 'AG' and strDBName = db_name()) = 1 
+OR (SELECT TOP 1 ysnUsed FROM ##tblOriginMod WHERE strPrefix = 'GR' and strDBName = db_name()) = 1
 BEGIN
 
 	EXEC('CREATE PROCEDURE uspSMImportCompanyLocation
