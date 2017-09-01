59249b57b6c3a3e91e463a01c2e80adbf36637a4
Premkumar <Prem.Selladurai@gmail.com>
2017-09-01 19:26:09 +0530

    MFG-2655

--- i21Database/dbo/Tables/tblMFProductionSummary.sql
+++ i21Database/dbo/Tables/tblMFProductionSummary.sql
@@ -20,7 +20,8 @@
 	,intCreatedUserId int NULL
 	,dtmCreated datetime NULL CONSTRAINT DF_tblMFProductionSummary_dtmCreated DEFAULT GetDate()
 	,intConcurrencyId INT NULL CONSTRAINT DF_tblMFProductionSummary_intConcurrencyId DEFAULT 0 
-	,CONSTRAINT PK_tblMFProductionSummary_intProductionSummaryId PRIMARY KEY (intProductionSummaryId)
+	,dblRequiredQty NUMERIC(18, 6) NULL, 
+    CONSTRAINT PK_tblMFProductionSummary_intProductionSummaryId PRIMARY KEY (intProductionSummaryId)
 	,CONSTRAINT FK_tblMFProductionSummary_tblMFWorkOrder_intWorkOrderId FOREIGN KEY (intWorkOrderId) REFERENCES dbo.tblMFWorkOrder(intWorkOrderId)
 	,CONSTRAINT FK_tblMFProductionSummary_tblICItem_intItemId FOREIGN KEY (intItemId) REFERENCES dbo.tblICItem(intItemId)
 	)
