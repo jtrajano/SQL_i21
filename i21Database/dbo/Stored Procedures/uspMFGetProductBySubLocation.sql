9c1f420e94738286b85a04016d9363ed25e6454e
Premkumar <Prem.Selladurai@gmail.com>
2017-09-04 20:15:05 +0530

    MFG-2662

--- i21Database/dbo/Stored Procedures/uspMFGetProductBySubLocation.sql
+++ i21Database/dbo/Stored Procedures/uspMFGetProductBySubLocation.sql
@@ -12,37 +12,78 @@ BEGIN
 	FROM tblSMCompanyLocationSubLocation
 	WHERE intCompanyLocationSubLocationId = @intSubLocationId
 
-	SELECT I.intItemId
-		,I.strItemNo
-		,I.strDescription
-		,IU.intItemUOMId
-		,U.intUnitMeasureId
-		,U.strUnitMeasure
-		,R.intManufacturingProcessId
-	FROM dbo.tblMFRecipe R
-	JOIN dbo.tblICItem I ON I.intItemId = R.intItemId
-	JOIN dbo.tblICItemUOM IU ON IU.intItemId = I.intItemId
-	JOIN dbo.tblICUnitMeasure U ON U.intUnitMeasureId = IU.intUnitMeasureId
-	JOIN dbo.tblSMCompanyLocationSubLocationCategory C ON C.intCategoryId = I.intCategoryId
-		AND C.intCompanyLocationSubLocationId = @intSubLocationId
-		AND R.intLocationId = @intLocationId
-		AND R.ysnActive = 1
-		AND IU.ysnStockUnit = 1
-		AND I.strStatus = 'Active'
-		AND I.strItemNo LIKE @strItemNo + '%'
-		AND I.intItemId = (
-			CASE 
-				WHEN @intItemId > 0
-					THEN @intItemId
-				ELSE I.intItemId
-				END
+	IF EXISTS (
+			SELECT *
+			FROM tblSMCompanyLocationSubLocationCategory
+			WHERE intCompanyLocationSubLocationId = @intSubLocationId
 			)
-		AND R.intManufacturingProcessId = (
-			CASE 
-				WHEN @intManufacturingProcessId > 0
-					THEN @intManufacturingProcessId
-				ELSE R.intManufacturingProcessId
-				END
-			)
-	ORDER BY I.strItemNo
+	BEGIN
+		SELECT I.intItemId
+			,I.strItemNo
+			,I.strDescription
+			,IU.intItemUOMId
+			,U.intUnitMeasureId
+			,U.strUnitMeasure
+			,R.intManufacturingProcessId
+		FROM dbo.tblMFRecipe R
+		JOIN dbo.tblICItem I ON I.intItemId = R.intItemId
+		JOIN dbo.tblICItemUOM IU ON IU.intItemId = I.intItemId
+		JOIN dbo.tblICUnitMeasure U ON U.intUnitMeasureId = IU.intUnitMeasureId
+		JOIN dbo.tblSMCompanyLocationSubLocationCategory C ON C.intCategoryId = I.intCategoryId
+			AND C.intCompanyLocationSubLocationId = @intSubLocationId
+			AND R.intLocationId = @intLocationId
+			AND R.ysnActive = 1
+			AND IU.ysnStockUnit = 1
+			AND I.strStatus = 'Active'
+			AND I.strItemNo LIKE @strItemNo + '%'
+			AND I.intItemId = (
+				CASE 
+					WHEN @intItemId > 0
+						THEN @intItemId
+					ELSE I.intItemId
+					END
+				)
+			AND R.intManufacturingProcessId = (
+				CASE 
+					WHEN @intManufacturingProcessId > 0
+						THEN @intManufacturingProcessId
+					ELSE R.intManufacturingProcessId
+					END
+				)
+		ORDER BY I.strItemNo
+	END
+	ELSE
+	BEGIN
+		SELECT I.intItemId
+			,I.strItemNo
+			,I.strDescription
+			,IU.intItemUOMId
+			,U.intUnitMeasureId
+			,U.strUnitMeasure
+			,R.intManufacturingProcessId
+		FROM dbo.tblMFRecipe R
+		JOIN dbo.tblICItem I ON I.intItemId = R.intItemId
+		JOIN dbo.tblICItemUOM IU ON IU.intItemId = I.intItemId
+		JOIN dbo.tblICUnitMeasure U ON U.intUnitMeasureId = IU.intUnitMeasureId
+			AND R.intLocationId = @intLocationId
+			AND R.ysnActive = 1
+			AND IU.ysnStockUnit = 1
+			AND I.strStatus = 'Active'
+			AND I.strItemNo LIKE @strItemNo + '%'
+			AND I.intItemId = (
+				CASE 
+					WHEN @intItemId > 0
+						THEN @intItemId
+					ELSE I.intItemId
+					END
+				)
+			AND R.intManufacturingProcessId = (
+				CASE 
+					WHEN @intManufacturingProcessId > 0
+						THEN @intManufacturingProcessId
+					ELSE R.intManufacturingProcessId
+					END
+				)
+		ORDER BY I.strItemNo
+	END
 END
