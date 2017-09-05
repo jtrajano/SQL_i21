200ed6863e22dc9cbde7632965a866adab8ec7b3
Dan Michael Jalimao <dan.jalimao@irely.com>
2017-09-05 11:54:53 +0800

    SM-3211

--- i21Database/Scripts/Post-Deployment/PostDeployment.sql
+++ i21Database/Scripts/Post-Deployment/PostDeployment.sql
@@ -45,6 +45,7 @@ print 'BEGIN POST DEPLOYMENT'
 :r .\SM\DefaultData\26_ActivitySource.sql
 :r .\SM\DefaultData\27_Language.sql
 :r .\SM\DefaultData\28_MultiCompany.sql
+:r .\SM\DefaultData\29_TransportationMode.sql
 :r .\SM\CreateEncryptionCertificateAndSymmetricKey.sql
 :r .\SM\CustomField.sql
 :r .\SM\1730_UpdateOriginSubMenusSorting.sql
