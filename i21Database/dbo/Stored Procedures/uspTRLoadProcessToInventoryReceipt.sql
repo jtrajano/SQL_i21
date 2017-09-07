7e4e39647d15ce525508f35c96a8340ef52a099d
Jayson Ramos <jayson.ramos@iRely.com>
2017-09-07 09:58:20 +0800

    TR-960

--- i21Database/dbo/Stored Procedures/uspTRLoadProcessToInventoryReceipt.sql
+++ i21Database/dbo/Stored Procedures/uspTRLoadProcessToInventoryReceipt.sql
@@ -247,13 +247,7 @@ END
 															min(RE.intShipViaId)
 												END
 			,[dblAmount]						= 0
-			,[strAllocateCostBy]				= (CASE WHEN @FreightCostAllocationMethod = 2 THEN 'Unit'
-														ELSE (CASE WHEN EXISTS(SELECT TOP 1 1 
-																				FROM tblTRLoadDistributionHeader TempDist
-																				WHERE TempDist.intLoadHeaderId = MIN(TLR.intLoadHeaderId)
-																					AND TempDist.strDestination = 'Location') THEN 'Unit'
-																ELSE NULL END)
-														END)
+			,[strAllocateCostBy]				= 'Unit'
 			,[intContractHeaderId]				= min(RE.intContractHeaderId)
 			,[intContractDetailId]				= min(RE.intContractDetailId)
 			,[ysnAccrue]						= CASE	WHEN min(SM.strFreightBilledBy) = 'Vendor' THEN 
@@ -301,13 +295,7 @@ END
 															min(RE.intShipViaId)
 												END
 			,[dblAmount]						= 0
-			,[strAllocateCostBy]				= (CASE WHEN @FreightCostAllocationMethod = 2 THEN 'Unit'
-														ELSE (CASE WHEN EXISTS(SELECT TOP 1 1 
-																				FROM tblTRLoadDistributionHeader TempDist
-																				WHERE TempDist.intLoadHeaderId = MIN(LTE.intLoadHeaderId)
-																					AND TempDist.strDestination = 'Location') THEN 'Unit'
-																ELSE NULL END)
-														END)
+			,[strAllocateCostBy]				= 'Unit'
 			,[intContractHeaderId]				= min(RE.intContractHeaderId)
 			,[intContractDetailId]				= min(RE.intContractDetailId)
 			,[ysnAccrue]						= CASE	WHEN min(SM.strFreightBilledBy) = 'Vendor' THEN 
