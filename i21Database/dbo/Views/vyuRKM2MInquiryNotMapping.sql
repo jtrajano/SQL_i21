217f1ac97e9295477b787ea6f9770b7a4637b3c4
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2017-09-09 15:31:45 +0530

    RM-864

--- i21Database/dbo/Views/vyuRKM2MInquiryNotMapping.sql
+++ i21Database/dbo/Views/vyuRKM2MInquiryNotMapping.sql
@@ -4,15 +4,17 @@ AS
 
 SELECT intM2MInquiryId
 		,strCommodityCode
-		,strCurrency
+		,cur.strCurrency
 		,strMarketZoneCode
 		,strLocationName
 		,strUnitMeasure
 		,dtmM2MBasisDate
 		,dtmPriceDate dtmFutureSettlementDate
+		,pum.strCurrency strPriceUnitMeasure
 FROM tblRKM2MInquiry bd
 JOIN tblRKM2MBasis bas on bas.intM2MBasisId=bd.intM2MBasisId
 JOIN tblSMCurrency cur on cur.intCurrencyID=bd.intCurrencyId
+JOIN tblSMCurrency pum on pum.intCurrencyID=bd.intPriceItemUOMId
 JOIN tblRKFuturesSettlementPrice sc on sc.intFutureSettlementPriceId=bd.intFutureSettlementPriceId
 LEFT join tblICCommodity c on c.intCommodityId=bd.intCommodityId
 LEFT JOIN tblICUnitMeasure um on um.intUnitMeasureId=bd.intUnitMeasureId
