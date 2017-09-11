71daa30d132ab701cfabd84e8b016a38ae01d89b
Gopinath Nayak <gopinath.n@irely.com>
2017-09-11 22:29:42 +0530

    GRN-660

--- i21Database/dbo/Views/vyuGRStorageHistoryNotMapped.sql
+++ i21Database/dbo/Views/vyuGRStorageHistoryNotMapped.sql
@@ -1,19 +1,21 @@
 ﻿CREATE VIEW [dbo].[vyuGRStorageHistoryNotMapped]
 AS
 SELECT    
- SH.intStorageHistoryId
-,SH.intEntityId  
-,E.strName  
-,SH.intCompanyLocationId  
-,LOC.strLocationName
-,SH.intContractHeaderId
-,CH.strContractNumber
-,SH.intInvoiceId
-,Inv.strInvoiceNumber
-,SH.intBillId
-,Bill.strBillId
-,SH.intSettleStorageId
-,SettleStorage.strStorageTicket AS SettleStorageTicket 
+ intStorageHistoryId	= SH.intStorageHistoryId
+,intEntityId			= SH.intEntityId  
+,strName				= E.strName  
+,intCompanyLocationId	= SH.intCompanyLocationId  
+,strLocationName		= LOC.strLocationName
+,intContractHeaderId	= SH.intContractHeaderId
+,strContractNumber		= CH.strContractNumber
+,intInvoiceId			= SH.intInvoiceId
+,strInvoiceNumber		= Inv.strInvoiceNumber
+,intBillId				= SH.intBillId
+,strBillId				= Bill.strBillId
+,intSettleStorageId		= SH.intSettleStorageId
+,SettleStorageTicket	= SettleStorage.strStorageTicket
+,intDeliverySheetId		= SH.intDeliverySheetId
+,strDeliverySheetNumber = DS.strDeliverySheetNumber 
 FROM tblGRStorageHistory SH
 LEFT JOIN tblEMEntity E ON E.intEntityId = SH.intEntityId
 LEFT JOIN tblSMCompanyLocation LOC ON LOC.intCompanyLocationId = SH.intCompanyLocationId
@@ -21,3 +23,4 @@ LEFT JOIN vyuCTContractHeaderView CH ON CH.intContractHeaderId=SH.intContractHea
 LEFT JOIN tblARInvoice Inv ON Inv.intInvoiceId=SH.intInvoiceId
 LEFT JOIN tblAPBill Bill ON Bill.intBillId=SH.intBillId
 LEFT JOIN tblGRSettleStorage SettleStorage ON SettleStorage.intSettleStorageId=SH.intSettleStorageId
+LEFT JOIN tblSCDeliverySheet DS ON DS.intDeliverySheetId=SH.intDeliverySheetId
