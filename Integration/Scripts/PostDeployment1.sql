00cb4ad5f70b4b6fed3158dac772fdcd9a470e47
JeffTrajano <jeffrey.trajano@irely.com>
2017-09-15 14:38:35 +0800

    GL-3746 Unable to import Historical journal

--- Integration/Scripts/PostDeployment1.sql
+++ Integration/Scripts/PostDeployment1.sql
@@ -15,6 +15,7 @@ Post-Deployment Script Template
 --General Ledger
 :r "..\Scripts\GL\1a_OriginCrossReferenceMapping.sql"
 :r "..\dbo\Stored Procedures\uspGLImportOriginCOA.sql"
+:r "..\dbo\Stored Procedures\uspGLImportOriginHistoricalJournal.sql"
 :r "..\dbo\Stored Procedures\uspGLGetImportOriginHistoricalJournalError.sql"
 :r "..\dbo\Stored Procedures\uspGLImportSubLedger.sql"
 
