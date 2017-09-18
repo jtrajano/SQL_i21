a9440e25e9e32ff1806fe94bf83e4457d0a9caf5
Wendell Wayne Estrada <wendell.estrada@irely.com>
2017-09-18 11:37:28 +0800

    IC-4288 Inventory Count - make weights for lots mandatory conditionally

--- i21Database/dbo/Functions/fnICGetErrorMessage.sql
+++ i21Database/dbo/Functions/fnICGetErrorMessage.sql
@@ -203,7 +203,7 @@ BEGIN
 		WHEN @msgId = 80187 THEN 'You are not allowed to change the Sub Location. Item %s still has stock at %s.'
 		WHEN @msgId = 80188 THEN 'You are not allowed to change the Sub Location. Item %s, plus %i more, still has stock at %s.'
 		WHEN @msgId = 80189 THEN 'Sub Location or Storage Location is missing for Item %s, Lot No. %s.'
-		WHEN @msgId = 80190 THEN 'Gross/Net UOM, Weight and Net Qty are required to be filled out for Item %s.'
+		WHEN @msgId = 80190 THEN 'Gross/Net UOM and weights are required for item %s.'
 	END 
 
 	RETURN @msg
