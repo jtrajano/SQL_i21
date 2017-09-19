2fb9b91a9a4f3dcdb6ff9f0fa8c9b104771908f8
Vinoth Kumar T <vinoth.t@irely.com>
2017-09-19 14:27:28 +0530

    QC-338

---
+++ i21Database/dbo/Stored Procedures/uspQMSampleImport.sql
@@ -0,0 +1,46 @@
+﻿CREATE PROCEDURE uspQMSampleImport
+AS
+BEGIN TRY
+	SET QUOTED_IDENTIFIER OFF
+	SET ANSI_NULLS ON
+	SET NOCOUNT ON
+	SET XACT_ABORT ON
+	SET ANSI_WARNINGS OFF
+
+	DECLARE @ErrMsg NVARCHAR(Max)
+
+	SELECT intSampleImportErrorId
+		,intSampleImportId
+		,intConcurrencyId
+		,dtmSampleReceivedDate
+		,strSampleNumber
+		,strItemShortName
+		,strSampleTypeName
+		,strVendorName
+		,strContractNumber
+		,strContainerNumber
+		,strMarks
+		,dblSequenceQuantity
+		,strSampleStatus
+		,strPropertyName
+		,strPropertyValue
+		,strErrorMsg
+		,intCreatedUserId
+		,dtmCreated
+	FROM tblQMSampleImportError
+END TRY
+
+BEGIN CATCH
+	IF XACT_STATE() != 0
+		AND @@TRANCOUNT > 0
+		ROLLBACK TRANSACTION
+
+	SET @ErrMsg = ERROR_MESSAGE()
+
+	RAISERROR (
+			@ErrMsg
+			,16
+			,1
+			,'WITH NOWAIT'
+			)
+END CATCH
---
+++ i21Database/dbo/Tables/tblQMSampleImportError.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE [dbo].[tblQMSampleImportError]
+(
+	intSampleImportErrorId INT IDENTITY(1,1) NOT NULL,
+	intSampleImportId INT,
+	intConcurrencyId INT NULL CONSTRAINT DF_tblQMSampleImportError_intConcurrencyId DEFAULT 0, 
+	dtmSampleReceivedDate NVARCHAR(50) COLLATE Latin1_General_CI_AS,
+	strSampleNumber NVARCHAR(30) COLLATE Latin1_General_CI_AS,
+	strItemShortName NVARCHAR(50) COLLATE Latin1_General_CI_AS,
+	strSampleTypeName NVARCHAR(50) COLLATE Latin1_General_CI_AS,
+	strVendorName NVARCHAR(100) COLLATE Latin1_General_CI_AS,
+	strContractNumber NVARCHAR(50) COLLATE Latin1_General_CI_AS,
+	strContainerNumber NVARCHAR(100) COLLATE Latin1_General_CI_AS,
+	strMarks NVARCHAR(100) COLLATE Latin1_General_CI_AS,
+	dblSequenceQuantity NUMERIC(18, 6),
+	strSampleStatus NVARCHAR(30) COLLATE Latin1_General_CI_AS,
+	strPropertyName NVARCHAR(100) COLLATE Latin1_General_CI_AS,
+	strPropertyValue NVARCHAR(MAX) COLLATE Latin1_General_CI_AS,
+	strErrorMsg NVARCHAR(MAX) COLLATE Latin1_General_CI_AS,
+
+	intCreatedUserId INT,
+	dtmCreated DATETIME NULL CONSTRAINT DF_tblQMSampleImportError_dtmCreated DEFAULT GETDATE()
+)
