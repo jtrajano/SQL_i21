23a035af785ce25c2b1dce83784613247aadb153
Feb Montefrio <feb.montefrio@irely.com>
2017-09-19 12:03:07 +0800

    TR-997 Fix publish error in Integration project.

---
+++ Integration/dbo/Stored Procedures/deleteSP.sql
@@ -0,0 +1,43 @@
+﻿IF EXISTS(select top 1 1 from sys.procedures where name = 'uspGRImportStorageType')
+	DROP PROCEDURE uspGRImportStorageType
+GO
+
+IF EXISTS(select top 1 1 from sys.procedures where name = 'uspGRImportWeightGrades')
+	DROP PROCEDURE uspGRImportWeightGrades
+GO
+
+IF EXISTS(select top 1 1 from sys.procedures where name = 'uspGRImportShipTo')
+	DROP PROCEDURE uspGRImportShipTo
+GO
+
+IF EXISTS(select top 1 1 from sys.procedures where name = 'uspMFImportRecipe')
+	DROP PROCEDURE uspMFImportRecipe
+GO
+
+IF EXISTS (select top 1 1 from sys.procedures where name = 'uspTRImportRackPrice')
+	DROP PROCEDURE uspTRImportRackPrice
+GO
+
+IF EXISTS (select top 1 1 from sys.procedures where name = 'uspTRImportOriginHistory')
+	DROP PROCEDURE uspTRImportOriginHistory
+GO
+
+IF EXISTS (select top 1 1 from sys.procedures where name = 'uspTRImportSupplyPoint')
+	DROP PROCEDURE uspTRImportSupplyPoint
+GO
+
+IF EXISTS (select top 1 1 from sys.procedures where name = 'uspTRImportSupplyPointProductSearch')
+	DROP PROCEDURE uspTRImportSupplyPointProductSearch
+GO
+
+IF EXISTS (select top 1 1 from sys.procedures where name = 'uspTRImportSupplyPointRackPriceEquation')
+	DROP PROCEDURE uspTRImportSupplyPointRackPriceEquation
+GO
+
+IF EXISTS (select top 1 1 from sys.procedures where name = 'uspTRImportTruck')
+	DROP PROCEDURE uspTRImportTruck
+GO
+
+IF EXISTS (SELECT TOP 1 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[uspETRecreateDeliveryMetricsView]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [dbo].uspETRecreateDeliveryMetricsView
+GO
