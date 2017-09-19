a89353e5cc791d41fe00696671b02cdb42a27b6e
Lex Eugene Peregrino <lex.peregrino@irely.com>
2017-09-19 17:23:37 +0800

    AP-4302 [DB Upgrade] - Upgrade db from 16.x version to 17.1 1.0

--- i21Database/dbo/Stored Procedures/uspAPFixAmount.sql
+++ i21Database/dbo/Stored Procedures/uspAPFixAmount.sql
@@ -48,4 +48,28 @@ WHERE C.ysnCheckVoid = 0
 --AND B.ysnOrigin = 0
 AND C.ysnClr = 0
 
+--PAYMENT TRANSACTION THAT IS NEED TO BECOME NEGATIVE
+--VOIDED TRANSACTION
+UPDATE A
+SET dblDiscount		= CASE WHEN A.dblDiscount < 0 THEN A.dblDiscount ELSE A.dblDiscount * -1 END,
+	dblAmountDue	= CASE WHEN A.dblAmountDue < 0 THEN A.dblAmountDue ELSE A.dblAmountDue * -1 END,
+	dblPayment		= CASE WHEN A.dblPayment < 0 THEN A.dblPayment ELSE A.dblPayment * -1 END,
+	dblInterest		= CASE WHEN A.dblInterest < 0 THEN A.dblInterest ELSE A.dblInterest * -1 END,
+	dblTotal		= CASE WHEN A.dblTotal < 0 THEN A.dblTotal ELSE A.dblTotal * -1 END
+FROM dbo.tblAPPaymentDetail A
+INNER JOIN dbo.tblAPPayment B ON A.intPaymentId = B.intPaymentId
+INNER JOIN tblCMBankTransaction C ON B.strPaymentRecordNum = C.strTransactionId
+WHERE strNotes  LIKE '%Void transaction for%' and C.ysnCheckVoid = 1
+
+--PAYMENT TRANSACTION THAT IS NEED TO BECOME POSITIVE
+UPDATE B
+	SET B.dblPayment = CASE WHEN dblPayment < 0 THEN dblPayment * -1 ELSE dblPayment END,
+		B.dblAmountDue = CASE WHEN dblAmountDue < 0 THEN dblAmountDue * -1 ELSE dblAmountDue END,
+		B.dblDiscount = CASE WHEN dblDiscount < 0 THEN dblDiscount * -1 ELSE dblDiscount END,
+		B.dblInterest = CASE WHEN dblInterest < 0 THEN dblInterest * -1 ELSE dblInterest END,
+		B.dblTotal = CASE WHEN dblTotal < 0 THEN dblTotal * -1 ELSE dblTotal END
+FROM tblAPPayment A
+INNER JOIN tblAPPaymentDetail B ON A.intPaymentId = B.intPaymentId
+WHERE strNotes LIKE 'Transaction Voided on%' 
+
 RETURN 0
