530c833a0c0c10f1b909ca66e2223772f28562c9
JeffTrajano <jeffrey.trajano@irely.com>
2017-09-20 15:14:53 +0800

    GL-3732 Add Document number column of tblGLDetail with vendor invoice number GL-3724 Send Reference from Voucher to Comment in G/L Journal Detail

--- i21Database/Scripts/Post-Deployment/GL/GLEntryDataFix.sql
+++ i21Database/Scripts/Post-Deployment/GL/GLEntryDataFix.sql
@@ -8,18 +8,48 @@
 ---------------------------------------------------------------------------------------------------------------------------------------
 
 GO
-	PRINT N'BEGIN Normalize tblGLDetail Fields'
+	PRINT N'Begin Updating dblDebitForeign, dblCreditForeign'
 GO
 
 
-UPDATE tblGLDetail SET dblDebitForeign =  0 WHERE dblDebitForeign IS NULL
-UPDATE tblGLDetail SET dblCreditForeign =  0 WHERE dblCreditForeign IS NULL
-UPDATE tblGLDetail SET dblDebitReport =  0 WHERE dblDebitReport IS NULL
-UPDATE tblGLDetail SET dblCreditReport =  0 WHERE dblCreditReport IS NULL
-UPDATE tblGLDetail SET dblReportingRate =  0 WHERE dblReportingRate IS NULL
-
-IF EXISTS(SELECT TOP 1 1 FROM sys.objects WHERE name = 'tblGLViewCache' AND type = 'U')
-	DROP TABLE [dbo].[tblGLViewCache]
+	UPDATE tblGLDetail SET dblDebitForeign =  0 WHERE dblDebitForeign IS NULL
+	UPDATE tblGLDetail SET dblCreditForeign =  0 WHERE dblCreditForeign IS NULL
+	UPDATE tblGLDetail SET dblDebitReport =  0 WHERE dblDebitReport IS NULL
+	UPDATE tblGLDetail SET dblCreditReport =  0 WHERE dblCreditReport IS NULL
+	UPDATE tblGLDetail SET dblReportingRate =  0 WHERE dblReportingRate IS NULL
 GO
-	PRINT N'END Normalize tblGLDetail Fields'
+	PRINT N'Finished Updating dblDebitForeign, dblCreditForeign'
+GO
+
+	PRINT N'Start updating nmwly added columnt(strDocument, strComments) column in tblGLDetail table'
+GO
+	--Applied on 17.1
+	UPDATE G SET strComments = B.strComments
+	FROM tblGLDetail G
+	JOIN tblGLJournalDetail B
+	ON G.intJournalLineNo = B.intJournalDetailId
+	JOIN tblGLJournal C
+	ON B.intJournalId = C.intJournalId
+	WHERE B.intJournalId = G.intTransactionId
+	AND C.strJournalId = G.strTransactionId
+	AND ISNULL(G.strComments,'') = ''
+
+	UPDATE G SET strDocument = B.strDocument
+	FROM tblGLDetail G
+	JOIN tblGLJournalDetail B
+	ON G.intJournalLineNo = B.intJournalDetailId
+	JOIN tblGLJournal C
+	ON B.intJournalId = C.intJournalId
+	WHERE B.intJournalId = G.intTransactionId
+	AND C.strJournalId = G.strTransactionId
+	AND ISNULL(G.strDocument,'') = ''
+
+	UPDATE A
+	SET A.strComments = B.strReference
+	FROM tblGLDetail A
+	JOIN tblAPBill B ON A.intTransactionId = B.intBillId
+	AND A.strTransactionId = B.strBillId
+	AND ISNULL(A.strComments,'') = ''
+GO
+	PRINT N'Finished updating nmwly added columnt(strDocument, strComments) column in tblGLDetail table'
 GO
