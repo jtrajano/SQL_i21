8d6edf5bedaef2779a4bd12511e6c761c06424d8
IRELY\kkarthick <karthick.k@irely.com>
2017-09-22 21:09:16 +0530

    LG-1183

--- Integration/dbo/Stored Procedures/deleteSP.sql
+++ Integration/dbo/Stored Procedures/deleteSP.sql
@@ -38,6 +38,10 @@ IF EXISTS (select top 1 1 from sys.procedures where name = 'uspTRImportTruck')
 	DROP PROCEDURE uspTRImportTruck
 GO
 
+IF EXISTS (select top 1 1 from sys.procedures where name = 'uspLGImportEquipmentType')
+	DROP PROCEDURE uspLGImportEquipmentType
+GO
+
 IF EXISTS (SELECT TOP 1 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[uspETRecreateDeliveryMetricsView]') AND type in (N'P', N'PC'))
 DROP PROCEDURE [dbo].uspETRecreateDeliveryMetricsView
 GO
---
+++ Integration/dbo/Stored Procedures/uspLGImportEquipmentType.sql
@@ -0,0 +1,41 @@
+IF EXISTS(select top 1 1 from sys.procedures where name = 'uspLGImportEquipmentType')
+	DROP PROCEDURE uspLGImportEquipmentType
+GO
+CREATE PROCEDURE uspLGImportEquipmentType
+	@Checking BIT = 0,
+	@UserId INT = 0,
+	@Total INT = 0 OUTPUT
+	AS
+BEGIN
+	--================================================
+	--     IMPORT GRAIN Weight/Grades
+	--================================================
+
+	IF (@Checking = 1)
+	BEGIN
+			 SELECT @Total = COUNT (*) FROM gactlmst s
+		UNPIVOT
+		(
+		  strEquipmentDesc
+		  FOR records IN (gac60_equip_desc_1, gac60_equip_desc_2, gac60_equip_desc_3, gac60_equip_desc_4, gac60_equip_desc_5, gac60_equip_desc_6, gac60_equip_desc_7, gac60_equip_desc_8, gac60_equip_desc_9,gac60_equip_desc_10)
+		) x
+		WHERE NOT EXISTS (SELECT strEquipmentType from tblLGEquipmentType
+		WHERE  strEquipmentType COLLATE SQL_Latin1_General_CP1_CS_AS = x.strEquipmentDesc COLLATE SQL_Latin1_General_CP1_CS_AS)		
+			 
+			 RETURN @Total
+	END
+
+	INSERT INTO tblLGEquipmentType(intConcurrencyId,strEquipmentType)
+	SELECT 1,x.strEquipmentType FROM gactlmst s
+	UNPIVOT
+	(
+	  strEquipmentType
+	  FOR records IN (gac60_equip_desc_1, gac60_equip_desc_2, gac60_equip_desc_3, gac60_equip_desc_4, gac60_equip_desc_5, gac60_equip_desc_6, gac60_equip_desc_7, gac60_equip_desc_8, gac60_equip_desc_9,gac60_equip_desc_10)
+	) x
+	WHERE NOT EXISTS (SELECT strEquipmentType from tblLGEquipmentType
+	WHERE  strEquipmentType COLLATE SQL_Latin1_General_CP1_CS_AS = x.strEquipmentType COLLATE SQL_Latin1_General_CP1_CS_AS)	
+	ORDER BY x.strEquipmentType ASC	
+END	
+
+
+
