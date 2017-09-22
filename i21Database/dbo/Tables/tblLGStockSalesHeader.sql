68cdeef476996b6944372323e6f3b2ff968ac8fa
IRELY\Swetosuvro <swetosuvro.g@irely.com>
2017-09-22 13:51:19 +0530

    LG-1106

--- i21Database/dbo/Tables/tblLGStockSalesHeader.sql
+++ i21Database/dbo/Tables/tblLGStockSalesHeader.sql
@@ -14,6 +14,9 @@
 	[intContractHeaderId] INT NULL,
 	[intAllocationHeaderId] INT NULL,
 	[intPickLotHeaderId] INT NULL,
+	[intContractCountryId] INT NULL,
+	[intContractCityId] INT NULL,
+	[dblHedgePrice] NUMERIC(18,6),
 
     CONSTRAINT [PK_tblLGStockSalesHeader_intStockSalesHeaderId] PRIMARY KEY ([intStockSalesHeaderId]), 
 	CONSTRAINT [UK_tblLGStockSalesHeader_intReferenceNumber] UNIQUE ([strStockSalesNumber]),
