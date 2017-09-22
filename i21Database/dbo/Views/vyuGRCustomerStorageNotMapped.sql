2aeb83284e199d2e711f103561104fa33f8e57dc
Gopinath Nayak <gopinath.n@irely.com>
2017-09-22 16:01:23 +0530

    GRN-635

--- i21Database/dbo/Views/vyuGRCustomerStorageNotMapped.sql
+++ i21Database/dbo/Views/vyuGRCustomerStorageNotMapped.sql
@@ -1,21 +1,23 @@
 ﻿CREATE VIEW [dbo].[vyuGRCustomerStorageNotMapped]
 AS
 SELECT    
- CS.intCustomerStorageId
-,CS.intEntityId  
-,E.strName  
-,CS.intStorageTypeId  
-,ST.strStorageTypeDescription
-,CS.intItemId
-,Item.strItemNo  
-,CS.intCompanyLocationId  
-,LOC.strLocationName
-,ISNULL(CS.intCompanyLocationSubLocationId,0) intCompanyLocationSubLocationId
-,ISNULL(SLOC.strSubLocationName,'') strSubLocationName
-,CS.intDiscountScheduleId
-,DS.strDiscountDescription
-,CS.intStorageScheduleId
-,SR.strScheduleId 
+ intCustomerStorageId				= CS.intCustomerStorageId
+,intEntityId						= CS.intEntityId  
+,strName							= E.strName  
+,intStorageTypeId					= CS.intStorageTypeId  
+,strStorageTypeDescription			= ST.strStorageTypeDescription
+,intItemId							= CS.intItemId
+,strItemNo						    = Item.strItemNo  
+,intCompanyLocationId				= CS.intCompanyLocationId  
+,strLocationName					= LOC.strLocationName
+,intCompanyLocationSubLocationId	= ISNULL(CS.intCompanyLocationSubLocationId,0) 
+,strSubLocationName					= ISNULL(SLOC.strSubLocationName,'') 
+,intStorageLocationId				= ISNULL(CS.intStorageLocationId,0) 
+,strStorageLocation					= ISNULL(Bin.strName,'') 
+,intDiscountScheduleId				= CS.intDiscountScheduleId
+,strDiscountDescription				= DS.strDiscountDescription
+,intStorageScheduleId				= CS.intStorageScheduleId
+,strScheduleId						= SR.strScheduleId 
 FROM tblGRCustomerStorage CS  
 JOIN tblGRStorageType ST ON ST.intStorageScheduleTypeId = CS.intStorageTypeId  
 JOIN tblSMCompanyLocation LOC ON LOC.intCompanyLocationId = CS.intCompanyLocationId  
@@ -23,6 +25,7 @@ JOIN tblEMEntity E ON E.intEntityId = CS.intEntityId
 JOIN tblICCommodity COM ON COM.intCommodityId = CS.intCommodityId
 JOIN tblGRStorageScheduleRule SR ON SR.intStorageScheduleRuleId=CS.intStorageScheduleId
 JOIN tblICItem Item ON Item.intItemId = CS.intItemId
-JOIN tblGRDiscountSchedule DS ON DS.intDiscountScheduleId=CS.intDiscountScheduleId
-LEFT JOIN tblSMCompanyLocationSubLocation SLOC ON SLOC.intCompanyLocationSubLocationId=CS.intCompanyLocationSubLocationId
+JOIN tblGRDiscountSchedule DS ON DS.intDiscountScheduleId = CS.intDiscountScheduleId
+LEFT JOIN tblSMCompanyLocationSubLocation SLOC ON SLOC.intCompanyLocationSubLocationId = CS.intCompanyLocationSubLocationId
+LEFT JOIN tblICStorageLocation Bin ON Bin.intStorageLocationId = CS.intStorageLocationId
 WHERE  ISNULL(CS.strStorageType,'') <> 'ITR'
