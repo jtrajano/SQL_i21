c64389834776806a4482b487d112ee0ebca58434
Feb Montefrio <feb.montefrio@irely.com>
2017-09-25 15:03:22 +0800

    IC-3768

--- app/search/InventoryValuation.js
+++ app/search/InventoryValuation.js
@@ -58,7 +58,29 @@ Ext.define('Inventory.search.InventoryValuation', {
                     width: 80
                 }
             ]
-        }
+        },
+        {
+            title: 'Inventory Valuation Summary',
+            url: '../inventory/api/item/searchinventoryvaluationsummary',
+            columns: [
+                { dataIndex: 'strItemNo', text: 'Item No', allowSort: true, flex: 1, dataType: 'string', key: true, drillDownText: 'View Item', drillDownClick: 'onViewItem' },
+                { dataIndex: 'strItemDescription', text: 'Description', allowSort: true, flex: 1, dataType: 'string', drillDownText: 'View Item'},
+                { dataIndex: 'strLocationName', text: 'Location', allowSort: true, flex: 1, dataType: 'string', drillDownText: 'View Location', drillDownClick: 'onViewLocation' },
+                { dataIndex: 'strSubLocationName', text: 'Storage Location', allowSort: false, flex: 1, dataType: 'string' },
+                { dataIndex: 'strCategoryCode', text: 'Category', allowSort: true, flex: 1, dataType: 'string', drillDownText: 'View Category', drillDownClick: 'onViewSummaryCategory'  },
+                { dataIndex: 'strCommodityCode', text: 'Commodity', allowSort: true, flex: 1, dataType: 'string' },
+                { dataIndex: 'strStockUOM', text: 'Stock UOM', allowSort: true, flex: 1, dataType: 'string' },
+                { xtype: 'numbercolumn', format: '#,##0.0000', summaryType: 'sum', dataIndex: 'dblQuantityInStockUOM', text: 'Stock Quantity', allowSort: true, flex: 1, dataType: 'float' },
+                { xtype: 'numbercolumn', format: '#,##0.0000', summaryType: 'sum', dataIndex: 'dblValue', text: 'Value', allowSort: true, flex: 1, dataType: 'float' },
+                { xtype: 'numbercolumn', format: '#,##0.0000', summaryType: 'sum', dataIndex: 'dblLastCost', text: 'Last Cost', allowSort: true, flex: 1, dataType: 'float' },
+                { xtype: 'numbercolumn', format: '#,##0.0000', summaryType: 'sum', dataIndex: 'dblStandardCost', text: 'Standard Cost', allowSort: true, flex: 1, dataType: 'float' },
+                { xtype: 'numbercolumn', format: '#,##0.0000', summaryType: 'sum', dataIndex: 'dblAverageCost', text: 'Average Cost', allowSort: true, flex: 1, dataType: 'float' },
+                { dataIndex: 'strInTransitLocationName', text: 'InTransit Location', hidden: true, allowSort: false, dataType: 'string', required: true }
+            ],
+            showNew: false,
+            showOpenSelected: false,
+            enableDblClick: false
+        }        
     ],
 
     //Drilldown functions 
@@ -66,6 +88,11 @@ Ext.define('Inventory.search.InventoryValuation', {
         var locationName = record.get('strCategory');
         i21.ModuleMgr.Inventory.showScreen(locationName, 'Category');
     },
+
+    onViewSummaryCategory: function (value, record) {
+        var locationName = record.get('strCategoryCode');
+        i21.ModuleMgr.Inventory.showScreen(locationName, 'Category');
+    },    
 
     onViewLocation: function (value, record) {
         var locationName = record.get('strLocationName');
