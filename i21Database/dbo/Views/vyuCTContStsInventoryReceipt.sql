5a1b741d686f117efe97fcd71ab984b2cdd40ac1
Satya S Sahoo <satya.sahoo@irely.com>
2017-10-03 14:08:42 +0530

    CT-1454

--- i21Database/dbo/Views/vyuCTContStsInventoryReceipt.sql
+++ i21Database/dbo/Views/vyuCTContStsInventoryReceipt.sql
@@ -2,63 +2,63 @@
 
 AS 
 
-	SELECT	CAST(ROW_NUMBER() OVER (ORDER BY intContractDetailId ASC) AS INT) intUniqueId,*
-	FROM
-	(		
-		SELECT	RI.intLineNo AS intContractDetailId,
-				IR.strReceiptNumber, 
-				RL.strLotNumber, 
-				CASE	WHEN IR.strReceiptType = 'Inventory Return' 
-							THEN -1*RL.dblQuantity
-							ELSE	RL.dblQuantity
-				END AS dblQuantity,
-				dbo.fnCTConvertQuantityToTargetItemUOM(RI.intItemId,IU.intUnitMeasureId,LP.intWeightUOMId,
+		SELECT	CAST(ROW_NUMBER() OVER (ORDER BY intContractDetailId ASC) AS INT) intUniqueId,*
+		FROM
+		(		
+			SELECT	RI.intLineNo AS intContractDetailId,
+					IR.strReceiptNumber, 
+					RL.strLotNumber, 
 					CASE	WHEN IR.strReceiptType = 'Inventory Return' 
-							THEN -1*(RL.dblGrossWeight - RL.dblTareWeight) 
-							ELSE	(RL.dblGrossWeight - RL.dblTareWeight) 
-					END
-				) AS dblNetWeight,
-				RI.intWeightUOMId intItemUOMId,
-				IM.strUnitMeasure,
-				SL.strSubLocationName,
-				IR.intInventoryReceiptId
+								THEN -1*ISNULL(RL.dblQuantity,RI.dblNetReturned)
+								ELSE	ISNULL(RL.dblQuantity,RI.dblNetReturned)
+					END AS dblQuantity,
+					dbo.fnCTConvertQuantityToTargetItemUOM(RI.intItemId,IU.intUnitMeasureId,LP.intWeightUOMId,
+						CASE	WHEN IR.strReceiptType = 'Inventory Return' 
+								THEN -1*ISNULL((RL.dblGrossWeight - RL.dblTareWeight),RI.dblNet) 
+								ELSE	ISNULL((RL.dblGrossWeight - RL.dblTareWeight),RI.dblNet)
+						END
+					) AS dblNetWeight,
+					RI.intWeightUOMId intItemUOMId,
+					IM.strUnitMeasure,
+					SL.strSubLocationName,
+					IR.intInventoryReceiptId
 				
-		FROM	tblICInventoryReceiptItemLot	RL
-		JOIN	tblICInventoryReceiptItem		RI	ON	RI.intInventoryReceiptItemId		=	RL.intInventoryReceiptItemId
-		JOIN	tblICInventoryReceipt			IR	ON	IR.intInventoryReceiptId			=	RI.intInventoryReceiptId 
-													AND IR.strReceiptType					IN	('Purchase Contract','Inventory Return')
-		JOIN	tblSMCompanyLocationSubLocation SL	ON	SL.intCompanyLocationSubLocationId	=	RI.intSubLocationId
-		JOIN	tblICItemUOM					IU	ON	IU.intItemUOMId						=	RI.intWeightUOMId
-		JOIN	tblICUnitMeasure				IM	ON	IM.intUnitMeasureId					=	IU.intUnitMeasureId		CROSS	
-		APPLY	tblLGCompanyPreference			LP 	
+			FROM	tblICInventoryReceiptItem		RI	
+			JOIN	tblICInventoryReceipt			IR	ON	IR.intInventoryReceiptId			=	RI.intInventoryReceiptId 
+														AND IR.strReceiptType					IN	('Purchase Contract','Inventory Return')
+			JOIN	tblSMCompanyLocationSubLocation SL	ON	SL.intCompanyLocationSubLocationId	=	RI.intSubLocationId
+			JOIN	tblICItemUOM					IU	ON	IU.intItemUOMId						=	RI.intWeightUOMId
+			JOIN	tblICUnitMeasure				IM	ON	IM.intUnitMeasureId					=	IU.intUnitMeasureId		
+	LEFT	JOIN	tblICInventoryReceiptItemLot	RL	ON	RI.intInventoryReceiptItemId		=	RL.intInventoryReceiptItemId	CROSS	
+			APPLY	tblLGCompanyPreference			LP 	
 
-		UNION ALL
+			UNION ALL
 
-		SELECT	AD.intSContractDetailId,
-				IR.strReceiptNumber, 
-				RL.strLotNumber, 
-				CASE	WHEN IR.strReceiptType = 'Inventory Return' 
-							THEN -1*RL.dblQuantity
-							ELSE	RL.dblQuantity
-				END AS dblQuantity,
-				dbo.fnCTConvertQuantityToTargetItemUOM(RI.intItemId,IU.intUnitMeasureId,LP.intWeightUOMId,
+			SELECT	AD.intSContractDetailId,
+					IR.strReceiptNumber, 
+					RL.strLotNumber, 
 					CASE	WHEN IR.strReceiptType = 'Inventory Return' 
-							THEN -1*(RL.dblGrossWeight - RL.dblTareWeight) 
-							ELSE	(RL.dblGrossWeight - RL.dblTareWeight) 
-					END
-				) AS dblNetWeight,
-				RI.intWeightUOMId intItemUOMId,
-				IM.strUnitMeasure,
-				SL.strSubLocationName,
-				IR.intInventoryReceiptId
+								THEN -1*RL.dblQuantity
+								ELSE	RL.dblQuantity
+					END AS dblQuantity,
+					dbo.fnCTConvertQuantityToTargetItemUOM(RI.intItemId,IU.intUnitMeasureId,LP.intWeightUOMId,
+						CASE	WHEN IR.strReceiptType = 'Inventory Return' 
+								THEN -1*(RL.dblGrossWeight - RL.dblTareWeight) 
+								ELSE	(RL.dblGrossWeight - RL.dblTareWeight) 
+						END
+					) AS dblNetWeight,
+					RI.intWeightUOMId intItemUOMId,
+					IM.strUnitMeasure,
+					SL.strSubLocationName,
+					IR.intInventoryReceiptId
 
-		FROM	tblICInventoryReceiptItemLot	RL
-		JOIN	tblICInventoryReceiptItem		RI	ON	RI.intInventoryReceiptItemId		=	RL.intInventoryReceiptItemId
-		JOIN	tblICInventoryReceipt			IR	ON	IR.intInventoryReceiptId			=	RI.intInventoryReceiptId 
-													AND IR.strReceiptType					=	'Purchase Contract'
-		JOIN	tblSMCompanyLocationSubLocation SL	ON	SL.intCompanyLocationSubLocationId	=	RI.intSubLocationId
-		JOIN	tblICItemUOM					IU	ON	IU.intItemUOMId						=	RI.intWeightUOMId
-		JOIN	tblICUnitMeasure				IM	ON	IM.intUnitMeasureId					=	IU.intUnitMeasureId
-		JOIN	tblLGAllocationDetail			AD	ON	AD.intPContractDetailId				=	RI.intLineNo		CROSS	
-		APPLY	tblLGCompanyPreference			LP 	
-	)t
+			FROM	tblICInventoryReceiptItemLot	RL
+			JOIN	tblICInventoryReceiptItem		RI	ON	RI.intInventoryReceiptItemId		=	RL.intInventoryReceiptItemId
+			JOIN	tblICInventoryReceipt			IR	ON	IR.intInventoryReceiptId			=	RI.intInventoryReceiptId 
+														AND IR.strReceiptType					=	'Purchase Contract'
+			JOIN	tblSMCompanyLocationSubLocation SL	ON	SL.intCompanyLocationSubLocationId	=	RI.intSubLocationId
+			JOIN	tblICItemUOM					IU	ON	IU.intItemUOMId						=	RI.intWeightUOMId
+			JOIN	tblICUnitMeasure				IM	ON	IM.intUnitMeasureId					=	IU.intUnitMeasureId
+			JOIN	tblLGAllocationDetail			AD	ON	AD.intPContractDetailId				=	RI.intLineNo		CROSS	
+			APPLY	tblLGCompanyPreference			LP 	
+		)t
