61f2835d399784eef87d3cc8ff10120f21f68605
Gopinath Nayak <gopinath.n@irely.com>
2017-10-03 21:33:19 +0530

    GRN-739

--- i21Database/dbo/Views/vyuGRStorageHistoryNotMapped.sql
+++ i21Database/dbo/Views/vyuGRStorageHistoryNotMapped.sql
@@ -15,7 +15,9 @@ SELECT
 ,intSettleStorageId		= SH.intSettleStorageId
 ,SettleStorageTicket	= SettleStorage.strStorageTicket
 ,intDeliverySheetId		= SH.intDeliverySheetId
-,strDeliverySheetNumber = DS.strDeliverySheetNumber 
+,strDeliverySheetNumber = DS.strDeliverySheetNumber
+,intInventoryShipmentId = SH.intInventoryShipmentId
+,strShipmentNumber      = Shipment.strShipmentNumber 
 FROM tblGRStorageHistory SH
 LEFT JOIN tblEMEntity E ON E.intEntityId = SH.intEntityId
 LEFT JOIN tblSMCompanyLocation LOC ON LOC.intCompanyLocationId = SH.intCompanyLocationId
@@ -24,3 +26,4 @@ LEFT JOIN tblARInvoice Inv ON Inv.intInvoiceId=SH.intInvoiceId
 LEFT JOIN tblAPBill Bill ON Bill.intBillId=SH.intBillId
 LEFT JOIN tblGRSettleStorage SettleStorage ON SettleStorage.intSettleStorageId=SH.intSettleStorageId
 LEFT JOIN tblSCDeliverySheet DS ON DS.intDeliverySheetId=SH.intDeliverySheetId
+LEFT JOIN  tblICInventoryShipment Shipment ON Shipment.intInventoryShipmentId=SH.intInventoryShipmentId
