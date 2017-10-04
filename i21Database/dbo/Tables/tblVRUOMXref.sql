6921d98659e96ceede1d655b167768af7803a273
Kenneth Lew Lagunero <kenneth.lagunero@irely.com>
2017-10-05 07:05:07 +0800

    VR-10

---
+++ i21Database/dbo/Tables/tblVRUOMXref.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tblVRUOMXref] (
+   [intUOMXrefId] [int] IDENTITY(1,1) NOT NULL,
+	[intEntityId] [int] NOT NULL,
+	[intUnitMeasureId] [int] NOT NULL,
+	[strVendorUOM] [nvarchar](50) NOT NULL,
+	[strEquipmentType] [nvarchar](50) NULL,
+	[intConcurrencyId] [int] NOT NULL CONSTRAINT [DF_tblVRUOMXref_intConcurrencyId]  DEFAULT ((0)),
+	CONSTRAINT [PK_tblVRUOMXref] PRIMARY KEY CLUSTERED ([intUOMXrefId] ASC),
+	CONSTRAINT [UQ_tblVRUOMXref_intUnitMeasureId_intVendorId] UNIQUE NONCLUSTERED ([intUnitMeasureId] ASC,[intEntityId] ASC)
+);
+
