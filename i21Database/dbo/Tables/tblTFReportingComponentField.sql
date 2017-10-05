d44a77c19f80a8682e4afdbfea9404a24497386e
Crisman Lazaga <cris.lazaga@irely.com>
2017-10-05 11:24:39 +0800

    MFT-698

--- i21Database/dbo/Tables/tblTFReportingComponentField.sql
+++ i21Database/dbo/Tables/tblTFReportingComponentField.sql
@@ -1,13 +1,14 @@
 ﻿CREATE TABLE [dbo].[tblTFReportingComponentField](
 	[intReportingComponentFieldId] [int] IDENTITY(1,1) NOT NULL,
 	[intReportingComponentId] [int] NULL,
-	[strColumn] [nvarchar](max) COLLATE Latin1_General_CI_AS NULL,
-	[strCaption] [nvarchar](max) COLLATE Latin1_General_CI_AS NULL,
+	[strColumn] [nvarchar](200) COLLATE Latin1_General_CI_AS NULL,
+	[strCaption] [nvarchar](200) COLLATE Latin1_General_CI_AS NULL,
 	[strFormat] [nvarchar](50) COLLATE Latin1_General_CI_AS NULL,
 	[strFooter] [nvarchar](50) COLLATE Latin1_General_CI_AS NULL,
 	[intWidth] [int] NULL,
 	[intMasterId] INT NULL,
 	[intConcurrencyId] [int] DEFAULT ((1)) NULL,
 	CONSTRAINT [PK_tblTFReportingComponentField] PRIMARY KEY ([intReportingComponentFieldId] ASC),
-	CONSTRAINT [FK_tblTFReportingComponentField_tblTFReportingComponent] FOREIGN KEY([intReportingComponentId]) REFERENCES [dbo].[tblTFReportingComponent] ([intReportingComponentId]) ON DELETE CASCADE
+	CONSTRAINT [FK_tblTFReportingComponentField_tblTFReportingComponent] FOREIGN KEY([intReportingComponentId]) REFERENCES [dbo].[tblTFReportingComponent] ([intReportingComponentId]) ON DELETE CASCADE, 
+    CONSTRAINT [UK_tblTFReportingComponentField_ComponentColumnCaption] UNIQUE ([intReportingComponentId],[strColumn],[strCaption]) 
 )
