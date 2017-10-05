60ca0c33e68e5ed404453f93ebdfea39aef02559
Dan Michael Jalimao <dan.jalimao@irely.com>
2017-10-05 11:41:58 +0800

    SM-3074

---
+++ Integration/Scripts/SM/CopyCashToUndepositedFund.sql
@@ -0,0 +1,14 @@
+﻿GO
+	/* COPYING CASH ACCOUNT TO UNDEPOSITED FUND */
+	IF NOT EXISTS(SELECT TOP 1 1 FROM tblMigrationLog WHERE strModule = 'System Manager' AND strEvent = 'Copying Cash Account to Undeposited Fund (System Manager)')
+	BEGIN
+		PRINT 'Copying Cash Account to Undeposited Fund (System Manager)'
+
+		UPDATE t SET intUndepositedFundsId = intCashAccount
+		FROM tblSMCompanyLocation t
+
+		PRINT N'ADD LOG TO tblMigrationLog'
+		INSERT INTO tblMigrationLog([strModule], [strEvent], [strDescription], [dtmMigrated]) 
+		VALUES('System Manager', 'Copying Cash Account to Undeposited Fund (System Manager)', 'Copying Cash Account to Undeposited Fund (System Manager)', GETDATE())
+	END
+GO
