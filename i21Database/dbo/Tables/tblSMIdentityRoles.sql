552636c632f9b679b2b0f9d8f83d7d044e9bac2e
Ian Ocampo <ian.ocampo@irely.com>
2017-10-05 15:59:11 +0800

    FRM-5568

---
+++ i21Database/dbo/Tables/tblSMIdentityRoles.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[tblSMIdentityRoles] (
+    [intRoleId]   NVARCHAR (128) NOT NULL,
+    [strName]     NVARCHAR (256) COLLATE Latin1_General_CI_AS NOT NULL,
+    CONSTRAINT [PK_dbo.tblSMIdentityRoles] PRIMARY KEY CLUSTERED ([intRoleId] ASC)
+);
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [RoleNameIndex]
+    ON [dbo].[tblSMIdentityRoles]([strName] ASC);
