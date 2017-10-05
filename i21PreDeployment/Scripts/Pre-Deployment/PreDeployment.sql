b8389190fb377dc61397d65fb435cad6b421a6bf
JeffTrajano <jeffrey.trajano@irely.com>
2017-10-05 11:06:38 +0800

    GL-3788 17.3 Upgrade error (FK contraint error on tblGLCurrentFIscalYear)

--- i21PreDeployment/Scripts/Pre-Deployment/PreDeployment.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/PreDeployment.sql
@@ -55,7 +55,7 @@
 :r .\6_GL\2_1430_to_1440.sql
 --:r .\6_GL\3_1440_to_1510.sql
 :r .\6_GL\4_1440_to_1530.sql
-:r .\6_GL\AccountCategory.sql
+:r .\6_GL\1730.sql
 
 -- AR
 :r .\7_AR\3_fnARGetCustomerDefaultContact.sql
