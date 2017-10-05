7e24756b9ff9b6d5b6055d480a9ffa50db2f8c3b
Mon Gonzales <mon.gonzales@irely.com>
2017-10-05 16:13:48 +0800

    AR-5549

--- Integration/dbo/Stored Procedures/uspARImportCustomer.sql
+++ Integration/dbo/Stored Procedures/uspARImportCustomer.sql
@@ -576,6 +576,8 @@ CREATE PROCEDURE [dbo].[uspARImportCustomer]
 			
 					SET @ContactEntityId = SCOPE_IDENTITY()
 					
+					if @strPhone <> ''''
+						INSERT INTO tblEMEntityPhoneNumber(intEntityId, strPhone) VALUES (@ContactEntityId, @strPhone)
 					
 					-- RULE: when creating a default contact from agcusmst.agcus_contact, trim tblEMEntityContact.strContactNumber to 20 characters				
 							
@@ -1239,6 +1241,8 @@ CREATE PROCEDURE [dbo].[uspARImportCustomer]
 
 					SET @ContactEntityId = SCOPE_IDENTITY()
 
+					if @strPhone <> ''''
+						INSERT INTO tblEMEntityPhoneNumber(intEntityId, strPhone) VALUES (@ContactEntityId, @strPhone)
 					-- RULE: when creating a default contact from agcusmst.agcus_contact, trim tblEMEntityContact.strContactNumber to 20 characters				
 
 					--Get intContactId				
