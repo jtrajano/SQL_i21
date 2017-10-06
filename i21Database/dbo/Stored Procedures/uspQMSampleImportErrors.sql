75bf33d63903c2ea2b21dc576261c5e83dd31389
Vinoth Kumar T <vinoth.t@irely.com>
2017-10-06 23:12:08 +0530

    QC-338

---
+++ i21Database/dbo/Stored Procedures/uspQMSampleImportErrors.sql
@@ -0,0 +1,29 @@
+﻿CREATE PROCEDURE uspQMSampleImportErrors
+AS
+SET QUOTED_IDENTIFIER OFF
+SET ANSI_NULLS ON
+SET NOCOUNT ON
+SET XACT_ABORT ON
+SET ANSI_WARNINGS OFF
+
+SELECT intSampleImportErrorId
+	,intSampleImportId
+	,intConcurrencyId
+	,CONVERT(DATETIME, dtmSampleReceivedDate, 101) dtmSampleReceivedDate
+	,strSampleNumber
+	,strItemShortName
+	,strSampleTypeName
+	,strVendorName
+	,strContractNumber
+	,strContainerNumber
+	,strMarks
+	,dblSequenceQuantity
+	,strSampleStatus
+	,strPropertyName
+	,strPropertyValue
+	,strComment
+	,strResult
+	,strErrorMsg
+	,intCreatedUserId
+	,dtmCreated
+FROM tblQMSampleImportError
