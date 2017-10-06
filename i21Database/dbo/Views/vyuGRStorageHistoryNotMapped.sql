6391cf218e71fa67a9159aeeea90a4db22fced35
Gopinath Nayak <gopinath.n@irely.com>
2017-10-06 17:04:01 +0530

    GRN-745

--- i21Database/dbo/Views/vyuGRStorageHistoryNotMapped.sql
+++ i21Database/dbo/Views/vyuGRStorageHistoryNotMapped.sql
@@ -16,14 +16,20 @@ SELECT
 ,SettleStorageTicket	= SettleStorage.strStorageTicket
 ,intDeliverySheetId		= SH.intDeliverySheetId
 ,strDeliverySheetNumber = DS.strDeliverySheetNumber
+,intInventoryReceiptId  = CASE 
+								WHEN SH.intTransactionTypeId =1 THEN SH.intInventoryReceiptId 
+								ELSE NULL 
+						  END
+,strReceiptNumber		= Receipt.strReceiptNumber
 ,intInventoryShipmentId = SH.intInventoryShipmentId
 ,strShipmentNumber      = Shipment.strShipmentNumber 
 FROM tblGRStorageHistory SH
 LEFT JOIN tblEMEntity E ON E.intEntityId = SH.intEntityId
 LEFT JOIN tblSMCompanyLocation LOC ON LOC.intCompanyLocationId = SH.intCompanyLocationId
-LEFT JOIN vyuCTContractHeaderView CH ON CH.intContractHeaderId=SH.intContractHeaderId
+LEFT JOIN tblCTContractHeader CH ON CH.intContractHeaderId=SH.intContractHeaderId
 LEFT JOIN tblARInvoice Inv ON Inv.intInvoiceId=SH.intInvoiceId
 LEFT JOIN tblAPBill Bill ON Bill.intBillId=SH.intBillId
 LEFT JOIN tblGRSettleStorage SettleStorage ON SettleStorage.intSettleStorageId=SH.intSettleStorageId
 LEFT JOIN tblSCDeliverySheet DS ON DS.intDeliverySheetId=SH.intDeliverySheetId
+LEFT JOIN  tblICInventoryReceipt Receipt ON Receipt.intInventoryReceiptId=SH.intInventoryReceiptId
 LEFT JOIN  tblICInventoryShipment Shipment ON Shipment.intInventoryShipmentId=SH.intInventoryShipmentId
