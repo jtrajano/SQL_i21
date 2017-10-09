b66941d3240801dc84df9f03966a04e830befca0
JBorlagdan <jc.borlagdan@irely.com>
2017-10-09 08:31:35 +0800

    AR-4747

---
+++ i21Database/dbo/Stored Procedures/uspEMUpdateEntityType.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[uspEMUpdateEntityType]
+	@intEntityId AS INT
+AS
+
+	DECLARE @entityId AS INT
+	SET @entityId = (SELECT intEntityId FROM tblEMEntity WHERE intEntityId = @intEntityId)
+
+	IF (@entityId IS NOT NULL)
+	BEGIN
+		UPDATE tblEMEntityType
+		SET strType = 'Customer'
+		WHERE intEntityId = @intEntityId
+	END
+
+RETURN 0
