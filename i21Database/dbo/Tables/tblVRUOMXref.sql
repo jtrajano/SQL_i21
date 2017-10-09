028508fab5fbbe6e71294af4a518cce8baae623a
Kenneth Lew Lagunero <kenneth.lagunero@irely.com>
2017-10-10 06:20:52 +0800

    VR-12

--- i21Database/dbo/Tables/tblVRUOMXref.sql
+++ i21Database/dbo/Tables/tblVRUOMXref.sql
@@ -7,7 +7,7 @@
 	[intConcurrencyId] [int] NOT NULL CONSTRAINT [DF_tblVRUOMXref_intConcurrencyId]  DEFAULT ((0)),
 	CONSTRAINT [PK_tblVRUOMXref] PRIMARY KEY CLUSTERED ([intUOMXrefId] ASC),
 	CONSTRAINT [UQ_tblVRUOMXref_intUnitMeasureId_intVendorSetupId] UNIQUE NONCLUSTERED ([intUnitMeasureId] ASC,[intVendorSetupId] ASC),
-	CONSTRAINT [UQ_tblVRUOMXref_strVendorUOM] UNIQUE NONCLUSTERED ([strVendorUOM] ASC),
+	CONSTRAINT [UQ_tblVRUOMXref_strVendorUOM_intVendorSetupId] UNIQUE NONCLUSTERED ([strVendorUOM] ASC, intVendorSetupId ASC),
 	CONSTRAINT [FK_tblVRUOMXref_tblVRVendorSetup] FOREIGN KEY([intVendorSetupId]) REFERENCES [dbo].[tblVRVendorSetup] ([intVendorSetupId])
 
 );
