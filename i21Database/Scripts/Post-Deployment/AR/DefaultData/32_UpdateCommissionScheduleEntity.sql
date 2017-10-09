bc8c0cd99a3563744b397e6e5a13be9f490fa30d
Ruben Escarcha <ruben.escarcha@irely.com>
2017-10-09 14:46:38 +0800

    AR-5484 Commission Schedule: Entity used is missing

---
+++ i21Database/Scripts/Post-Deployment/AR/DefaultData/32_UpdateCommissionScheduleEntity.sql
@@ -0,0 +1,13 @@
+﻿print('/*******************  BEGIN Update Commission Schedule Entity *******************/')
+GO
+
+UPDATE
+	tblARCommissionSchedule
+SET
+	intEntityId = (SELECT TOP 1 ID.[strDataType] FROM dbo.fnARSplitValues(tblARCommissionSchedule.strEntityIds, ',') ID INNER JOIN tblARCustomer ARC ON CAST(ID.[strDataType] AS INT) = ARC.intEntityId)
+WHERE
+	LEN(LTRIM(RTRIM(ISNULL(tblARCommissionSchedule.strEntityIds, '')))) > 0
+	AND tblARCommissionSchedule.strScheduleType = 'Individual'
+
+GO
+print('/*******************  Update Commission Schedule Entity  *******************/')
