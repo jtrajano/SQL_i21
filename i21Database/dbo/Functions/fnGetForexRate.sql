038037cdc4df74e1c78e4bfdb8f71de9bf1accab
Smith Wilson de Jesus <smith.dejesus@irely.com>
2017-10-12 18:17:15 +0800

    CM-1875 Revalue Currency

--- i21Database/dbo/Functions/fnGetForexRate.sql
+++ i21Database/dbo/Functions/fnGetForexRate.sql
@@ -14,7 +14,7 @@ SET @ForexRate = CAST(1 AS NUMERIC(18,6))
 
 DECLARE @FunctionalCurrencyId INT
 
-SET @ForexRate = ISNULL((SELECT TOP 1 [dblRate] FROM [vyuSMForex] WHERE [intFromCurrencyId] = @CurrencyId AND [intCurrencyExchangeRateTypeId] = @ForexRateTypeId AND [intFunctionalCurrencyId] = [intToCurrencyId] AND CAST(@TransactionDate AS DATE) > CAST([dtmValidFromDate] AS DATE) ORDER BY [dtmValidFromDate]), CAST(1 AS NUMERIC(18,6)))
+SET @ForexRate = ISNULL((SELECT TOP 1 [dblRate] FROM [vyuSMForex] WHERE [intFromCurrencyId] = @CurrencyId AND [intCurrencyExchangeRateTypeId] = @ForexRateTypeId AND [intFunctionalCurrencyId] = [intToCurrencyId] AND CAST(@TransactionDate AS DATE) > CAST([dtmValidFromDate] AS DATE) ORDER BY [dtmValidFromDate] DESC), CAST(1 AS NUMERIC(18,6)))
 
 RETURN @ForexRate;
 
