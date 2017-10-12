8ca5211ffae6f90e02cb507c258fc5fb6fada5ee
Lex Eugene Peregrino <lex.peregrino@irely.com>
2017-10-12 18:04:56 +0800

    AP-4478 , AP-4479 , AP-3157 - 1099 Summary Report 1.0

--- i21Database/dbo/Views/vyuAP1099.sql
+++ i21Database/dbo/Views/vyuAP1099.sql
@@ -56,52 +56,52 @@ SELECT
 	    THEN (A.dbl1099 + A.dblTax) / B.dblTotal  * ISNULL(B2.dblPayment,A.dbl1099)
      ELSE 0 END   
 	, dblOrdinaryDividends = CASE WHEN A.int1099Form = 5 AND A.int1099Category = 0--'OrdinaryDividends'     
-		THEN (A.dbl1099 + A.dblTax) / B.dblTotal  * ISNULL(B2.dblPayment,A.dbl1099)
+		THEN  (A.dblTotal + A.dblTax)
      ELSE 0 END  
 	 , dblQualified = CASE WHEN A.int1099Form = 5 AND A.int1099Category = 1--'Qualified'     
-		THEN (A.dbl1099 + A.dblTax) / B.dblTotal  * ISNULL(B2.dblPayment,A.dbl1099)
+		THEN (A.dblTotal + A.dblTax) 
      ELSE 0 END  
 	 , dblCapitalGain = CASE WHEN A.int1099Form = 5 AND A.int1099Category = 2--'CapitalGain'     
-		THEN (A.dbl1099 + A.dblTax) / B.dblTotal  * ISNULL(B2.dblPayment,A.dbl1099)
+		THEN (A.dblTotal + A.dblTax)
      ELSE 0 END  
 	 , dblUnrecapGain = CASE WHEN A.int1099Form = 5 AND A.int1099Category = 3--'UnrecapGain'     
-		THEN (A.dbl1099 + A.dblTax) / B.dblTotal  * ISNULL(B2.dblPayment,A.dbl1099)
+		THEN (A.dblTotal + A.dblTax) 
      ELSE 0 END
 	, dblSection1202 = CASE WHEN A.int1099Form = 5 AND A.int1099Category = 4--'Section1202'     
-		THEN (A.dbl1099 + A.dblTax) / B.dblTotal  * ISNULL(B2.dblPayment,A.dbl1099)
+		THEN (A.dblTotal + A.dblTax) 
      ELSE 0 END
 	, dblCollectibles = CASE WHEN A.int1099Form = 5 AND A.int1099Category = 5--'Collectibles'     
-		THEN (A.dbl1099 + A.dblTax) / B.dblTotal  * ISNULL(B2.dblPayment,A.dbl1099)
+		THEN (A.dblTotal + A.dblTax) 
      ELSE 0 END
 	, dblNonDividends = CASE WHEN A.int1099Form = 5 AND A.int1099Category = 6--'NonDividends'     
-		THEN (A.dbl1099 + A.dblTax) / B.dblTotal  * ISNULL(B2.dblPayment,A.dbl1099)
+		THEN (A.dbl1099 + A.dblTax)
      ELSE 0 END
 	, dblFITW = CASE WHEN A.int1099Form = 5 AND A.int1099Category = 7--'FITW'     
-		THEN (A.dbl1099 + A.dblTax) / B.dblTotal  * ISNULL(B2.dblPayment,A.dbl1099)
+		THEN (A.dblTotal + A.dblTax) 
      ELSE 0 END
 	, dblInvestment = CASE WHEN A.int1099Form = 5 AND A.int1099Category = 8--'Investment'     
-		THEN (A.dbl1099 + A.dblTax) / B.dblTotal  * ISNULL(B2.dblPayment,A.dbl1099)
+		THEN (A.dblTotal + A.dblTax)
      ELSE 0 END
 	, dblForeignTax = CASE WHEN A.int1099Form = 5 AND A.int1099Category = 9--'ForeignTax'     
-		THEN (A.dbl1099 + A.dblTax) / B.dblTotal  * ISNULL(B2.dblPayment,A.dbl1099)
+		THEN (A.dblTotal + A.dblTax) 
      ELSE 0 END
 	, dblForeignCountry = CASE WHEN A.int1099Form = 5 AND A.int1099Category = 10--'ForeignCountry'     
-		THEN (A.dbl1099 + A.dblTax) / B.dblTotal  * ISNULL(B2.dblPayment,A.dbl1099)
+		THEN (A.dblTotal + A.dblTax) 
      ELSE 0 END   
 	, dblCash = CASE WHEN A.int1099Form = 5 AND A.int1099Category = 11--'Cash Liquidation'     
-		THEN (A.dbl1099 + A.dblTax) / B.dblTotal  * ISNULL(B2.dblPayment,A.dbl1099)
+		THEN (A.dblTotal + A.dblTax)
      ELSE 0 END 
 	, dblNonCash = CASE WHEN A.int1099Form = 5 AND A.int1099Category = 12--'Non Cash Liquidation'     
-		THEN (A.dbl1099 + A.dblTax) / B.dblTotal  * ISNULL(B2.dblPayment,A.dbl1099)
+		THEN (A.dblTotal + A.dblTax) 
      ELSE 0 END      
 	, dblExempt = CASE WHEN A.int1099Form = 5 AND A.int1099Category = 13--'Exempt Interest Dividends'     
-		THEN (A.dbl1099 + A.dblTax) / B.dblTotal  * ISNULL(B2.dblPayment,A.dbl1099)
+		THEN (A.dblTotal + A.dblTax) 
      ELSE 0 END
 	, dblPrivate = CASE WHEN A.int1099Form = 5 AND A.int1099Category = 14--'Private Activity Dividends'     
-		THEN (A.dbl1099 + A.dblTax) / B.dblTotal  * ISNULL(B2.dblPayment,A.dbl1099)
+		THEN (A.dblTotal + A.dblTax) 
      ELSE 0 END 
 	, dblState = CASE WHEN A.int1099Form = 5 AND A.int1099Category = 15--'State Tax Withheld'     
-		THEN (A.dbl1099 + A.dblTax) / B.dblTotal  * ISNULL(B2.dblPayment,A.dbl1099)
+		THEN (A.dblTotal + A.dblTax) 
      ELSE 0 END   
 	, dbl1099 = (A.dbl1099 + A.dblTax) / B.dblTotal  * ISNULL(B2.dblPayment,A.dbl1099)
     , intYear = YEAR(ISNULL(B2.dtmDatePaid, B.dtmDate))
