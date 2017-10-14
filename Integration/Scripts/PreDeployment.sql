f38d0c88c4cdf4ea223a241774499e00b7357b7d
IRELY\kkarthick <karthick.k@irely.com>
2017-10-15 01:12:01 +0530

    AR-5673, AR-5674

--- Integration/Scripts/PreDeployment.sql
+++ Integration/Scripts/PreDeployment.sql
@@ -236,6 +236,9 @@
 
 :r "..\dbo\Stored Procedures\deleteSP.sql"
 
+:r "..\dbo\Stored Procedures\uspARImportPTTaxExemption.sql" 
+:r "..\dbo\Stored Procedures\uspEMImportPTTerminalToCustomer.sql" 
+
 --GRAIN
 :r "..\dbo\Stored Procedures\uspGRImportStorageType.sql"
 :r "..\dbo\Stored Procedures\uspGRImportWeightGrades.sql"
