e5991a5cc2094b6bce13aea5b248fbdff1f4116f
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2017-10-16 23:26:55 +0530

    RM-918

--- i21Database/dbo/Views/vyuRKGetCommodityUnitMeasure.sql
+++ i21Database/dbo/Views/vyuRKGetCommodityUnitMeasure.sql
@@ -1,4 +1,5 @@
 ﻿CREATE View vyuRKGetCommodityUnitMeasure
 AS
-SELECT convert(int,row_number() OVER(ORDER BY c.intCommodityId DESC)) RowNum, c.intCommodityId,c.strCommodityCode,intUnitMeasureId,ysnDefault from tblICCommodity c
-JOIN tblICCommodityUnitMeasure um on c.intCommodityId=um.intCommodityId and ysnDefault=1
+SELECT convert(int,row_number() OVER(ORDER BY c.intCommodityId DESC)) RowNum, c.intCommodityId,c.strCommodityCode,um.intUnitMeasureId,ysnDefault,u.strUnitMeasure from tblICCommodity c
+JOIN tblICCommodityUnitMeasure um on c.intCommodityId=um.intCommodityId
+join tblICUnitMeasure u on u.intUnitMeasureId=um.intUnitMeasureId and ysnDefault=1
