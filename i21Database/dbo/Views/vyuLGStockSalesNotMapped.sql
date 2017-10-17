fbd416acad340e6568cd5103bc68612b8ee711d5
IRELY\Swetosuvro <swetosuvro.g@irely.com>
2017-10-17 18:38:16 +0530

    LG-1219

--- i21Database/dbo/Views/vyuLGStockSalesNotMapped.sql
+++ i21Database/dbo/Views/vyuLGStockSalesNotMapped.sql
@@ -1,37 +1,48 @@
 ﻿CREATE VIEW vyuLGStockSalesNotMapped
 AS
 SELECT DISTINCT SSH.intStockSalesHeaderId
-	  ,SSH.strStockSalesNumber
-	  ,SSH.dtmTransDate
-	  ,CH.strContractNumber
-	  ,AH.intAllocationHeaderId
-	  ,AH.strAllocationNumber
-	  ,PLH.intPickLotHeaderId
-	  ,PLH.strPickLotNumber
-	  ,CL.intCompanyLocationId
-	  ,CL.strLocationName AS strCompanyLocation
-	  ,CLSL.intCompanyLocationSubLocationId 
-	  ,CLSL.strSubLocationName AS strWarehouse
-	  ,CH.dtmContractDate AS dtmSalesContractDate
-	  ,CB.strContractBasis	AS strINCOTerm
-	  ,CEN.strName AS strCustomerName
-	  ,CH.intCommodityId
-	  ,CO.strCommodityCode
-	  ,PT.strPricingType
-	  ,0 AS intLoadId
-	  ,'' AS strLoadNumber
-	  ,CAST(0 AS BIT) AS ysnDelivered
-	  ,(SELECT TOP 1 dtmStartDate FROM tblCTContractDetail D WHERE D.intContractDetailId = CD.intContractDetailId) AS dtmDeliveryFrom
-	  ,(SELECT TOP 1 dtmEndDate FROM tblCTContractDetail D WHERE D.intContractDetailId = CD.intContractDetailId) AS dtmDeliveryTo
-	  ,(SELECT TOP 1 intFutureMarketId FROM tblCTContractDetail D WHERE D.intContractDetailId = CD.intContractDetailId) AS intFutureMarketId
-	  ,(SELECT TOP 1 intFutureMonthId FROM tblCTContractDetail D WHERE D.intContractDetailId = CD.intContractDetailId) AS intFutureMonthId
-	  ,(SELECT TOP 1 strFixationBy FROM tblCTContractDetail D WHERE D.intContractDetailId = CD.intContractDetailId) AS strFixationBy
-	  ,(SELECT TOP 1 dblCashPrice FROM tblCTContractDetail D WHERE D.intContractDetailId = CD.intContractDetailId) AS dblCashPrice
-	  ,CH.ysnMultiplePriceFixation
-	  ,CH.strPrintableRemarks
-	  ,(SELECT TOP 1 strCurrency FROM tblSMCurrency C WHERE C.intCurrencyID = CD.intCurrencyId) AS strCurrency
-	  ,(SELECT TOP 1 intCurrencyId FROM tblSMCurrency C WHERE C.intCurrencyID = CD.intCurrencyId) AS intCurrencyId
-	  ,(SELECT TOP 1 U2.strUnitMeasure FROM tblICItemUOM PU LEFT JOIN tblICUnitMeasure U2 ON U2.intUnitMeasureId = PU.intUnitMeasureId WHERE PU.intItemUOMId = CD.intPriceItemUOMId) AS strPriceUOM
+	,SSH.strStockSalesNumber
+	,SSH.dtmTransDate
+	,CH.strContractNumber
+	,AH.intAllocationHeaderId
+	,AH.strAllocationNumber
+	,PLH.intPickLotHeaderId
+	,PLH.strPickLotNumber
+	,CL.intCompanyLocationId
+	,CL.strLocationName AS strCompanyLocation
+	,CLSL.intCompanyLocationSubLocationId
+	,CLSL.strSubLocationName AS strWarehouse
+	,CH.dtmContractDate AS dtmSalesContractDate
+	,CB.strContractBasis AS strINCOTerm
+	,CEN.strName AS strCustomerName
+	,CH.intCommodityId
+	,CO.strCommodityCode
+	,PT.strPricingType
+	,0 AS intLoadId
+	,'' AS strLoadNumber
+	,CAST(0 AS BIT) AS ysnDelivered
+	,dtmStartDate AS dtmDeliveryFrom
+	,dtmEndDate AS dtmDeliveryTo
+	,CD.intFutureMarketId AS intFutureMarketId
+	,CD.intFutureMonthId AS intFutureMonthId
+	,CD.strFixationBy AS strFixationBy
+	,CD.dblCashPrice AS dblCashPrice
+	,CH.ysnMultiplePriceFixation
+	,CH.strPrintableRemarks
+	,C.strCurrency AS strCurrency
+	,CD.intCurrencyId AS intCurrencyId
+	,(
+		SELECT TOP 1 U2.strUnitMeasure
+		FROM tblICItemUOM PU
+		LEFT JOIN tblICUnitMeasure U2 ON U2.intUnitMeasureId = PU.intUnitMeasureId
+		WHERE PU.intItemUOMId = CD.intPriceItemUOMId
+		) AS strPriceUOM
+	,CASE 
+		WHEN CB.strINCOLocationType = 'City'
+			THEN CT.strCity
+		ELSE SL.strSubLocationName
+		END AS strContractCity
+	,CN.strCountry AS strContractCountry
 FROM tblLGStockSalesHeader SSH
 JOIN tblLGStockSalesLotDetail SSLD ON SSH.intStockSalesHeaderId = SSLD.intStockSalesHeaderId
 JOIN tblLGAllocationHeader AH ON AH.intAllocationHeaderId = SSH.intAllocationHeaderId
@@ -44,4 +55,8 @@ JOIN tblSMCompanyLocation CL ON CL.intCompanyLocationId = SSH.intCompanyLocation
 JOIN tblSMCompanyLocationSubLocation CLSL ON CLSL.intCompanyLocationSubLocationId = SSH.intSubLocationId
 JOIN tblICCommodity CO ON CO.intCommodityId = CH.intCommodityId
 JOIN tblCTPricingType PT ON PT.intPricingTypeId = CD.intPricingTypeId
+LEFT JOIN tblSMCity CT ON CT.intCityId = CH.intINCOLocationTypeId
+LEFT JOIN tblSMCompanyLocationSubLocation SL ON SL.intCompanyLocationSubLocationId = CH.intINCOLocationTypeId
+LEFT JOIN tblSMCurrency C ON C.intCurrencyID = CD.intCurrencyId
 LEFT JOIN tblCTContractBasis CB ON CB.intContractBasisId = CH.intContractBasisId
+LEFT JOIN tblSMCountry CN ON CN.intCountryID = CH.intCountryId
