12d8d5d9dd2695e59076455b5f961f7bfbec76c8
JeffTrajano <jeffrey.trajano@irely.com>
2017-10-17 14:26:21 +0800

    GL-3806 Allowed to delete built account segment

--- i21Database/dbo/Tables/tblGLAccountSegmentMapping.sql
+++ i21Database/dbo/Tables/tblGLAccountSegmentMapping.sql
@@ -4,8 +4,8 @@
     [intAccountSegmentId]        INT NULL,
     [intConcurrencyId]           INT DEFAULT 1 NOT NULL,
     CONSTRAINT [PK_tblGLAccountSegmentMapping] PRIMARY KEY CLUSTERED ([intAccountSegmentMappingId] ASC),
-    CONSTRAINT [FK_tblGLAccountSegmentMapping_tblGLAccount] FOREIGN KEY ([intAccountId]) REFERENCES [dbo].[tblGLAccount] ([intAccountId]) ON DELETE CASCADE,
-    CONSTRAINT [FK_tblGLAccountSegmentMapping_tblGLAccountSegment] FOREIGN KEY ([intAccountSegmentId]) REFERENCES [dbo].[tblGLAccountSegment] ([intAccountSegmentId]) ON DELETE CASCADE
+    CONSTRAINT [FK_tblGLAccountSegmentMapping_tblGLAccount] FOREIGN KEY ([intAccountId]) REFERENCES [dbo].[tblGLAccount] ([intAccountId]),
+    CONSTRAINT [FK_tblGLAccountSegmentMapping_tblGLAccountSegment] FOREIGN KEY ([intAccountSegmentId]) REFERENCES [dbo].[tblGLAccountSegment] ([intAccountSegmentId]) 
 );
 GO
 
