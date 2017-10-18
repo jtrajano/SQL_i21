fed6763d1b0dfc4014d50ff4ff375351fe707025
Kenneth Lew Lagunero <kenneth.lagunero@irely.com>
2017-10-19 05:32:11 +0800

    VR-11

--- i21Database/dbo/Tables/tblVRProgram.sql
+++ i21Database/dbo/Tables/tblVRProgram.sql
@@ -4,7 +4,7 @@
 	[intVendorSetupId] [int] NOT NULL,
 	[strVendorProgram] [nvarchar](20) NULL,
 	[strProgramDescription] [nvarchar](200) NULL,
-	[intConcurrencyId] [int] NOT NULL,
+	[intConcurrencyId] [int] NOT NULL CONSTRAINT [DF_tblVRProgram_intConcurrencyId]  DEFAULT ((0)),
 	CONSTRAINT [PK_tblVRProgram] PRIMARY KEY CLUSTERED ([intProgramId] ASC),
 );
 GO
---
+++ i21Database/dbo/Tables/tblVRProgramItem.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[tblVRProgramItem](
+	[intProgramItemId] [int] IDENTITY(1,1) NOT NULL,
+	[intItemId] [int] NULL,
+	[intCategoryId] [int] NULL,
+	[strRebateBy] [nvarchar](15) NOT NULL CONSTRAINT [DF_tblVRProgramItem_strRebateBy]  DEFAULT (N'Amount'),
+	[dblRebateRate] [numeric](18, 6) NULL,
+	[dtmBeginDate] [datetime] NULL,
+	[dtmEndDate] [datetime] NULL,
+	[intProgramId] [int] NOT NULL,
+	[intConcurrencyId] [int] NOT NULL CONSTRAINT [DF_tblVRProgramItem_intConcurrencyId]  DEFAULT ((0)),
+	CONSTRAINT [PK_tblVRProgramItem] PRIMARY KEY CLUSTERED ([intProgramItemId] ASC),
+	CONSTRAINT [FK_tblVRProgramItem_tblICCategory] FOREIGN KEY([intCategoryId]) REFERENCES [dbo].[tblICCategory] ([intCategoryId]),
+	CONSTRAINT [FK_tblVRProgramItem_tblICItem] FOREIGN KEY([intItemId]) REFERENCES [dbo].[tblICItem] ([intItemId]),
+	CONSTRAINT [FK_tblVRProgramItem_tblVRProgramItem] FOREIGN KEY([intProgramItemId])REFERENCES [dbo].[tblVRProgramItem] ([intProgramItemId]),
+)
+GO
