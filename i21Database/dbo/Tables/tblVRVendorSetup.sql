296dac375a222f791857fa9da34580121cd9cbf8
Kenneth Lew Lagunero <kenneth.lagunero@irely.com>
2017-10-19 23:33:08 +0800

    VR-13 Changes on Schema added strVendorType

--- i21Database/dbo/Tables/tblVRVendorSetup.sql
+++ i21Database/dbo/Tables/tblVRVendorSetup.sql
@@ -1,14 +1,15 @@
 ﻿CREATE TABLE [dbo].[tblVRVendorSetup](
-	[intVendorSetupId] [int] IDENTITY(1,1) NOT NULL,
+	[intVendorSetupId] INT IDENTITY(1,1) NOT NULL,
 	[intEntityId] [int] NOT NULL,
-	[strVendorExportId] [nvarchar](10) NULL,
-	[strExportFileType] [nvarchar](3) NULL,
-	[strExportFilePath] [nvarchar](200) NULL,
-	[strCompany1Id] [nvarchar](20) NULL,
-	[strCompany2Id] [nvarchar](20) NULL,
-	[intConcurrencyId] [int] NOT NULL CONSTRAINT [DF_tblVRVendorSetup_intConcurrencyId]  DEFAULT ((0)),
+	[strVendorExportId] NVARCHAR(10) COLLATE Latin1_General_CI_AS NULL,
+	[strExportFileType] NVARCHAR(3) COLLATE Latin1_General_CI_AS NULL,
+	[strExportFilePath] NVARCHAR(200) COLLATE Latin1_General_CI_AS NULL,
+	[strCompany1Id] NVARCHAR(20) COLLATE Latin1_General_CI_AS NULL,
+	[strCompany2Id] NVARCHAR(20) COLLATE Latin1_General_CI_AS NULL,
+	[strVendorType] NVARCHAR(10) COLLATE Latin1_General_CI_AS NOT NULL,
+	[intConcurrencyId] INT DEFAULT 0 NOT NULL ,
 	CONSTRAINT [PK_tblVRVendorSetup] PRIMARY KEY CLUSTERED([intVendorSetupId] ASC),
-	CONSTRAINT [UQ_tblVRVendorSetup_intEntityId] UNIQUE NONCLUSTERED ([intEntityId] ASC), 
+	CONSTRAINT [UQ_tblVRVendorSetup_intEntityId] UNIQUE NONCLUSTERED ([intEntityId] ASC, [strVendorType] ASC), 
 	CONSTRAINT [FK_tblVRVendorSetup_tblAPVendor] FOREIGN KEY([intEntityId]) REFERENCES [dbo].[tblAPVendor] ([intEntityId]),
 )
 GO
