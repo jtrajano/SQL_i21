fdc3854306156aa880e148744df8c37052468044
Mon Gonzales <mon.gonzales@irely.com>
2017-10-19 16:54:25 +0800

    AR-5706

--- i21Database/dbo/Views/vyuEMETExportCustomerTaxExemption.sql
+++ i21Database/dbo/Views/vyuEMETExportCustomerTaxExemption.sql
@@ -20,6 +20,7 @@ SELECT DISTINCT
 	FROM	
 (
 SELECT 
+	 --(ISNULL(D.intTaxGroupId, E.intTaxGroupId)),
 	CustomerNumber = B.strCustomerNumber,
 	ItemNumber = C.strItemNo,	
 	--state = 'test',
@@ -30,90 +31,108 @@ SELECT
 					ON sic1.intCategoryId = sict1.intCategoryId
 				JOIN tblSMTaxCode sst1
 					ON sst1.intTaxClassId = sict1.intTaxClassId
+				JOIN tblSMTaxGroupCode ssg1	
+					ON ssg1.intTaxCodeId = sst1.intTaxCodeId and ssg1.intTaxGroupId = (ISNULL(D.intTaxGroupId, E.intTaxGroupId))
 				JOIN tblETExportTaxCodeMapping sem1
-					ON sem1.intTaxCodeId = sst1.intTaxCodeId			
+					ON sem1.intTaxCodeId = ssg1.intTaxCodeId 			
 						AND sem1.strTaxCodeReference = 'FET'
-			WHERE sic1.intCategoryId = A.intCategoryId  ) , 'N'),
+			WHERE sic1.intCategoryId = A.intCategoryId  AND ssg1.intTaxCodeId <> A.intTaxCodeId) , 'N'),
 
 	[SETCharge] = ISNULL((SELECT TOP 1 'Y'  from tblICCategory sic1 
 				JOIN tblICCategoryTax sict1
 					ON sic1.intCategoryId = sict1.intCategoryId
 				JOIN tblSMTaxCode sst1
 					ON sst1.intTaxClassId = sict1.intTaxClassId
+				JOIN tblSMTaxGroupCode ssg1	
+					ON ssg1.intTaxCodeId = sst1.intTaxCodeId and ssg1.intTaxGroupId = (ISNULL(D.intTaxGroupId, E.intTaxGroupId))
 				JOIN tblETExportTaxCodeMapping sem1
-					ON sem1.intTaxCodeId = sst1.intTaxCodeId			
+					ON sem1.intTaxCodeId = ssg1.intTaxCodeId 			
 						AND sem1.strTaxCodeReference = 'SET'
-			WHERE sic1.intCategoryId = A.intCategoryId  ) , 'N'),
+			WHERE sic1.intCategoryId = A.intCategoryId  AND ssg1.intTaxCodeId <> A.intTaxCodeId) , 'N'),
 
 	SSTCharge = ISNULL((SELECT TOP 1 'Y'  from tblICCategory sic1 
 				JOIN tblICCategoryTax sict1
 					ON sic1.intCategoryId = sict1.intCategoryId
 				JOIN tblSMTaxCode sst1
 					ON sst1.intTaxClassId = sict1.intTaxClassId
+				JOIN tblSMTaxGroupCode ssg1	
+					ON ssg1.intTaxCodeId = sst1.intTaxCodeId and ssg1.intTaxGroupId = (ISNULL(D.intTaxGroupId, E.intTaxGroupId))
 				JOIN tblETExportTaxCodeMapping sem1
-					ON sem1.intTaxCodeId = sst1.intTaxCodeId			
+					ON sem1.intTaxCodeId = ssg1.intTaxCodeId 			
 						AND sem1.strTaxCodeReference = 'SST'
-			WHERE sic1.intCategoryId = A.intCategoryId  ) , 'N'),
+			WHERE sic1.intCategoryId = A.intCategoryId  AND ssg1.intTaxCodeId <> A.intTaxCodeId) , 'N'),
 
 	Locale1Charge = ISNULL((SELECT TOP 1 'Y'  from tblICCategory sic1 
 				JOIN tblICCategoryTax sict1
 					ON sic1.intCategoryId = sict1.intCategoryId
 				JOIN tblSMTaxCode sst1
 					ON sst1.intTaxClassId = sict1.intTaxClassId
+				JOIN tblSMTaxGroupCode ssg1	
+					ON ssg1.intTaxCodeId = sst1.intTaxCodeId and ssg1.intTaxGroupId = (ISNULL(D.intTaxGroupId, E.intTaxGroupId))
 				JOIN tblETExportTaxCodeMapping sem1
-					ON sem1.intTaxCodeId = sst1.intTaxCodeId			
+					ON sem1.intTaxCodeId = ssg1.intTaxCodeId 			
 						AND sem1.strTaxCodeReference = 'LC1'
-			WHERE sic1.intCategoryId = A.intCategoryId  ) , 'N'),
+			WHERE sic1.intCategoryId = A.intCategoryId  AND ssg1.intTaxCodeId <> A.intTaxCodeId) , 'N'),
 
 	Locale2Charge = ISNULL((SELECT TOP 1 'Y'  from tblICCategory sic1 
 				JOIN tblICCategoryTax sict1
 					ON sic1.intCategoryId = sict1.intCategoryId
 				JOIN tblSMTaxCode sst1
 					ON sst1.intTaxClassId = sict1.intTaxClassId
+				JOIN tblSMTaxGroupCode ssg1	
+					ON ssg1.intTaxCodeId = sst1.intTaxCodeId and ssg1.intTaxGroupId = (ISNULL(D.intTaxGroupId, E.intTaxGroupId))
 				JOIN tblETExportTaxCodeMapping sem1
-					ON sem1.intTaxCodeId = sst1.intTaxCodeId			
+					ON sem1.intTaxCodeId = ssg1.intTaxCodeId 			
 						AND sem1.strTaxCodeReference = 'LC2'
-			WHERE sic1.intCategoryId = A.intCategoryId  ) , 'N'),
+			WHERE sic1.intCategoryId = A.intCategoryId  AND ssg1.intTaxCodeId <> A.intTaxCodeId) , 'N'),
 
 	Locale3Charge = ISNULL((SELECT TOP 1 'Y'  from tblICCategory sic1 
 				JOIN tblICCategoryTax sict1
 					ON sic1.intCategoryId = sict1.intCategoryId
 				JOIN tblSMTaxCode sst1
 					ON sst1.intTaxClassId = sict1.intTaxClassId
+				JOIN tblSMTaxGroupCode ssg1	
+					ON ssg1.intTaxCodeId = sst1.intTaxCodeId and ssg1.intTaxGroupId = (ISNULL(D.intTaxGroupId, E.intTaxGroupId))
 				JOIN tblETExportTaxCodeMapping sem1
-					ON sem1.intTaxCodeId = sst1.intTaxCodeId			
+					ON sem1.intTaxCodeId = ssg1.intTaxCodeId 			
 						AND sem1.strTaxCodeReference = 'LC3'
-			WHERE sic1.intCategoryId = A.intCategoryId  ) , 'N'),
+			WHERE sic1.intCategoryId = A.intCategoryId  AND ssg1.intTaxCodeId <> A.intTaxCodeId) , 'N'),
 
 	Locale4Charge = ISNULL((SELECT TOP 1 'Y'  from tblICCategory sic1 
 				JOIN tblICCategoryTax sict1
 					ON sic1.intCategoryId = sict1.intCategoryId
 				JOIN tblSMTaxCode sst1
 					ON sst1.intTaxClassId = sict1.intTaxClassId
+				JOIN tblSMTaxGroupCode ssg1	
+					ON ssg1.intTaxCodeId = sst1.intTaxCodeId and ssg1.intTaxGroupId = (ISNULL(D.intTaxGroupId, E.intTaxGroupId))
 				JOIN tblETExportTaxCodeMapping sem1
-					ON sem1.intTaxCodeId = sst1.intTaxCodeId			
+					ON sem1.intTaxCodeId = ssg1.intTaxCodeId 			
 						AND sem1.strTaxCodeReference = 'LC4'
-			WHERE sic1.intCategoryId = A.intCategoryId  ) , 'N'),
+			WHERE sic1.intCategoryId = A.intCategoryId  AND ssg1.intTaxCodeId <> A.intTaxCodeId) , 'N'),
 
 	Locale5Charge = ISNULL((SELECT TOP 1 'Y'  from tblICCategory sic1 
 				JOIN tblICCategoryTax sict1
 					ON sic1.intCategoryId = sict1.intCategoryId
 				JOIN tblSMTaxCode sst1
 					ON sst1.intTaxClassId = sict1.intTaxClassId
+				JOIN tblSMTaxGroupCode ssg1	
+					ON ssg1.intTaxCodeId = sst1.intTaxCodeId and ssg1.intTaxGroupId = (ISNULL(D.intTaxGroupId, E.intTaxGroupId))
 				JOIN tblETExportTaxCodeMapping sem1
-					ON sem1.intTaxCodeId = sst1.intTaxCodeId			
+					ON sem1.intTaxCodeId = ssg1.intTaxCodeId 			
 						AND sem1.strTaxCodeReference = 'LC5'
-			WHERE sic1.intCategoryId = A.intCategoryId  ) , 'N'),
+			WHERE sic1.intCategoryId = A.intCategoryId  AND ssg1.intTaxCodeId <> A.intTaxCodeId) , 'N'),
 
 	Locale6Charge = ISNULL((SELECT TOP 1 'Y'  from tblICCategory sic1 
 				JOIN tblICCategoryTax sict1
 					ON sic1.intCategoryId = sict1.intCategoryId
 				JOIN tblSMTaxCode sst1
 					ON sst1.intTaxClassId = sict1.intTaxClassId
+				JOIN tblSMTaxGroupCode ssg1	
+					ON ssg1.intTaxCodeId = sst1.intTaxCodeId and ssg1.intTaxGroupId = (ISNULL(D.intTaxGroupId, E.intTaxGroupId))
 				JOIN tblETExportTaxCodeMapping sem1
-					ON sem1.intTaxCodeId = sst1.intTaxCodeId			
+					ON sem1.intTaxCodeId = ssg1.intTaxCodeId 			
 						AND sem1.strTaxCodeReference = 'LC6'
-			WHERE sic1.intCategoryId = A.intCategoryId  ) , 'N'),
+			WHERE sic1.intCategoryId = A.intCategoryId  AND ssg1.intTaxCodeId <> A.intTaxCodeId) , 'N'),
 	
 	intTaxGroupId = isnull(D.intTaxGroupId, E.intTaxGroupId)
 	FROM 
@@ -123,10 +142,11 @@ SELECT
 			JOIN tblICItem C
 				ON A.intItemId = C.intItemId
 			JOIN tblEMEntityLocation D
-				ON A.intEntityCustomerId = D.intEntityId 
+				ON A.intEntityCustomerId = D.intEntityId --AND D.ysnDefaultLocation = 1
 			LEFT JOIN tblSMCompanyLocation E
 				on D.intWarehouseId = E.intCompanyLocationId			
-	WHERE A.intItemId in (select intItemId from tblETExportFilterItem) or A.intCategoryId in (select intCategoryId from tblETExportFilterCategory )
+	WHERE (A.intItemId in (select intItemId from tblETExportFilterItem) or A.intCategoryId in (select intCategoryId from tblETExportFilterCategory ))			
+			AND A.intTaxCodeId IN (SELECT intTaxCodeId FROM tblSMTaxGroupCode WHERE intTaxGroupId = (ISNULL(D.intTaxGroupId, E.intTaxGroupId)))
 ) A 
 LEFT JOIN tblSMTaxGroup G
 			on A.intTaxGroupId = G.intTaxGroupId
