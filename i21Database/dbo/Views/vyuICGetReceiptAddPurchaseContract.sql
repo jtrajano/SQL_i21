c3b52c98418fd5016e2a0160106caa254f8abffb
Wendell Wayne Estrada <wendell.estrada@irely.com>
2017-10-23 11:45:58 +0800

    IC-4060

--- i21Database/dbo/Views/vyuICGetReceiptAddPurchaseContract.sql
+++ i21Database/dbo/Views/vyuICGetReceiptAddPurchaseContract.sql
@@ -1,39 +1,39 @@
 ﻿CREATE  VIEW [dbo].[vyuICGetReceiptAddPurchaseContract]
 AS
 
-SELECT intKey = CAST(ROW_NUMBER() OVER(ORDER BY intLocationId, intEntityVendorId, intLineNo) AS INT)
+SELECT intKey = CAST(ROW_NUMBER() OVER(ORDER BY intLocationId, intEntityId, intLineNo) AS INT)
 , * 
 FROM (
 	SELECT 	
-		intLocationId				= intCompanyLocationId			
-		, intEntityVendorId			= intEntityId
-		, strVendorId				= strVendorId
-		, strVendorName				= strEntityName
+		intLocationId				= ContractView.intCompanyLocationId			
+		, intEntityId			= ContractView.intEntityId
+		, strVendorId				= ContractView.strVendorId
+		, strVendorName				= ContractView.strEntityName
 		, strReceiptType			= 'Purchase Contract'
-		, intLineNo					= intContractDetailId
-		, intOrderId				= intContractHeaderId
-		, strOrderNumber			= strContractNumber
-		, dblOrdered				= CASE WHEN ysnLoad = 1 THEN intNoOfLoad ELSE dblDetailQuantity END
-		, dblReceived				= CASE WHEN ysnLoad = 1 THEN intLoadReceived ELSE dblDetailQuantity - dblBalance END
+		, intLineNo					= ContractView.intContractDetailId
+		, intOrderId				= ContractView.intContractHeaderId
+		, strOrderNumber			= ContractView.strContractNumber
+		, dblOrdered				= CASE WHEN ContractView.ysnLoad = 1 THEN ContractView.intNoOfLoad ELSE ContractView.dblDetailQuantity END
+		, dblReceived				= CASE WHEN ContractView.ysnLoad = 1 THEN ContractView.intLoadReceived ELSE ContractView.dblDetailQuantity - ContractView.dblBalance END
 		, intSourceType				= CAST(0 AS INT)
 		, intSourceId				= CAST(NULL AS INT) 
 		, strSourceNumber			= CAST(NULL AS NVARCHAR(50)) 
 		, intItemId					= ContractView.intItemId
-		, strItemNo					= strItemNo
-		, strItemDescription		= strItemDescription
-		, dblQtyToReceive			= dblDetailQuantity - (dblDetailQuantity - dblBalance)
-		, intLoadToReceive			= intNoOfLoad - intLoadReceived
-		, dblUnitCost				= dblSeqPrice
+		, strItemNo					= ContractView.strItemNo
+		, strItemDescription		= ContractView.strItemDescription
+		, dblQtyToReceive			= ContractView.dblDetailQuantity - (ContractView.dblDetailQuantity - ContractView.dblBalance)
+		, intLoadToReceive			= ContractView.intNoOfLoad - ContractView.intLoadReceived
+		, dblUnitCost				= ContractView.dblSeqPrice
 		, dblTax					= CAST(0 AS NUMERIC(18, 6))
-		, dblLineTotal				= CAST((dblDetailQuantity - (dblDetailQuantity - dblBalance)) * dblSeqPrice AS NUMERIC(18, 6))
-		, strLotTracking			= strLotTracking
-		, intCommodityId			= intCommodityId
+		, dblLineTotal				= CAST((ContractView.dblDetailQuantity - (ContractView.dblDetailQuantity - ContractView.dblBalance)) * ContractView.dblSeqPrice AS NUMERIC(18, 6))
+		, strLotTracking			= ContractView.strLotTracking
+		, intCommodityId			= ContractView.intCommodityId
 		, intContainerId			= CAST(NULL AS INT) 
 		, strContainer				= CAST(NULL AS NVARCHAR(50)) 
-		, intSubLocationId			= intCompanyLocationSubLocationId
-		, strSubLocationName		= strSubLocationName
-		, intStorageLocationId		= intStorageLocationId
-		, strStorageLocationName	= strStorageLocationName
+		, intSubLocationId			= ContractView.intCompanyLocationSubLocationId
+		, strSubLocationName		= ContractView.strSubLocationName
+		, intStorageLocationId		= ContractView.intStorageLocationId
+		, strStorageLocationName	= ContractView.strStorageLocationName
 		, intOrderUOMId				= ItemUOM.intItemUOMId
 		, strOrderUOM				= ItemUnitMeasure.strUnitMeasure
 		, dblOrderUOMConvFactor		= ItemUOM.dblUnitQty
@@ -51,8 +51,8 @@ FROM (
 		, strCostUOM				= ContractView.strSeqPriceUOM
 		--, dblCostUOMConvFactor		= ContractView.dblQtyToPriceUOMConvFactor
 		, dblCostUOMConvFactor		= (SELECT dblUnitQty from tblICItemUOM WHERE intItemUOMId = ContractView.intSeqPriceUOMId)
-		, intLifeTime				= intLifeTime
-		, strLifeTimeType			= strLifeTimeType
+		, intLifeTime				= ContractView.intLifeTime
+		, strLifeTimeType			= ContractView.strLifeTimeType
 		, ysnLoad					= CAST(0 AS BIT)
 		, dblAvailableQty			= CAST(0 AS NUMERIC(38, 20))
 		, strBOL					= CAST(NULL AS NVARCHAR(50))
@@ -66,32 +66,26 @@ FROM (
 		, intForexRateTypeId		= ContractView.intRateTypeId
 		, strForexRateType			= ContractView.strCurrencyExchangeRateType
 		, dblForexRate				= ContractView.dblRate
-		, ysnBundleItem				= ContractView.ysnBundleItem
+		, ysnBundleItem				= CAST(0 AS BIT)
 		, intBundledItemId			= CAST(NULL AS INT)
 		, strBundledItemNo			= CAST(NULL AS NVARCHAR(50))
 		, strBundledItemDescription = CAST(NULL AS NVARCHAR(50))
 		, ysnIsBasket 				= CAST(0 AS BIT)
-	FROM	vyuCTContractDetailView ContractView LEFT JOIN dbo.tblICItemUOM ItemUOM
-				ON ContractView.intItemUOMId = ItemUOM.intItemUOMId
-			LEFT JOIN dbo.tblICUnitMeasure ItemUnitMeasure
-				ON ItemUnitMeasure.intUnitMeasureId = ItemUOM.intUnitMeasureId
-			LEFT JOIN dbo.tblICItemUOM GrossNetUOM
-				ON ContractView.intNetWeightUOMId = GrossNetUOM.intItemUOMId
-			LEFT JOIN dbo.tblICUnitMeasure GrossNetUnitMeasure
-				ON GrossNetUnitMeasure.intUnitMeasureId = GrossNetUOM.intUnitMeasureId
-			LEFT JOIN dbo.tblICItemUOM CostUOM
-				ON CostUOM.intItemUOMId = dbo.fnGetMatchingItemUOMId(ContractView.intItemId, intPriceItemUOMId)
-			LEFT JOIN dbo.tblICUnitMeasure CostUnitMeasure
-				ON CostUnitMeasure.intUnitMeasureId = CostUOM.intUnitMeasureId
-	WHERE	ysnAllowedToShow = 1
-			AND strContractType = 'Purchase'
-			AND ISNULL(ysnIsBasket, 0) = 0
+	FROM vyuCTContractAddOrdersLookup ContractView
+		LEFT JOIN dbo.tblICItemUOM ItemUOM ON ContractView.intItemUOMId = ItemUOM.intItemUOMId
+		LEFT JOIN dbo.tblICUnitMeasure ItemUnitMeasure ON ItemUnitMeasure.intUnitMeasureId = ItemUOM.intUnitMeasureId
+		LEFT JOIN dbo.tblICItemUOM GrossNetUOM ON ContractView.intNetWeightUOMId = GrossNetUOM.intItemUOMId
+		LEFT JOIN dbo.tblICUnitMeasure GrossNetUnitMeasure ON GrossNetUnitMeasure.intUnitMeasureId = GrossNetUOM.intUnitMeasureId
+		LEFT JOIN dbo.tblICItemUOM CostUOM ON CostUOM.intItemUOMId = dbo.fnGetMatchingItemUOMId(ContractView.intItemId, intPriceItemUOMId)
+		LEFT JOIN dbo.tblICUnitMeasure CostUnitMeasure ON CostUnitMeasure.intUnitMeasureId = CostUOM.intUnitMeasureId
+	WHERE ContractView.ysnAllowedToShow = 1
+		AND ContractView.strContractType = 'Purchase'
 	
 	UNION ALL
 	
 	SELECT 	
 		intLocationId				= intCompanyLocationId			
-		, intEntityVendorId			= intEntityId
+		, intEntityId			= intEntityId
 		, strVendorId				= strVendorId
 		, strVendorName				= strEntityName
 		, strReceiptType			= 'Purchase Contract'
