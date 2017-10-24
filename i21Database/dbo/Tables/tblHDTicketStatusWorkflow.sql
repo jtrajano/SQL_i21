05d687d29f7b2b48b9da8ce6cd7e2f2a2f75ed24
Jayson Ramos <jayson.ramos@iRely.com>
2017-10-25 03:02:57 +0800

    HD-2194

--- i21Database/dbo/Tables/tblHDTicketStatusWorkflow.sql
+++ i21Database/dbo/Tables/tblHDTicketStatusWorkflow.sql
@@ -9,5 +9,6 @@
 	CONSTRAINT [PK_tblHDTicketStatusWorkflow_intTicketStatusWorkflowId] PRIMARY KEY CLUSTERED ([intTicketStatusWorkflowId] ASC),
 	CONSTRAINT [FK_tblHDTicketStatusWorkflow_tblHDTicketStatus_intFromStatusId] FOREIGN KEY ([intFromStatusId]) REFERENCES [dbo].[tblHDTicketStatus] ([intTicketStatusId]),
     CONSTRAINT [FK_tblHDTicketStatusWorkflow_tblHDTicketStatus_intToStatusId] FOREIGN KEY ([intToStatusId]) REFERENCES [dbo].[tblHDTicketStatus] ([intTicketStatusId]),
+    CONSTRAINT [UQ_tblHDTicketStatusWorkflow_intFromStatusId] UNIQUE ([intFromStatusId]),
     CONSTRAINT [UQ_tblHDTicketStatusWorkflow] UNIQUE ([intFromStatusId],[intToStatusId],[strTiggerBy])
 )
