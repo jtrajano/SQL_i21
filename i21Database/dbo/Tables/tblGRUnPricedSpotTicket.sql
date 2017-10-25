16c7a4bf0a77205fb8276dca68d90b10f6084e56
Gopinath Nayak <gopinath.n@irely.com>
2017-10-25 14:19:29 +0530

    GRN-417

---
+++ i21Database/dbo/Tables/tblGRUnPricedSpotTicket.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[tblGRUnPricedSpotTicket]
+(
+	[intUnPricedSpotTicketId] INT NOT NULL IDENTITY,
+	[intConcurrencyId] INT NULL, 
+	[intUnPricedId] INT NOT NULL,
+    [intTicketId] INT NULL,
+	[intBillId] INT NULL,
+	[intInvoiceId] INT NULL,
+	[dblUnits] DECIMAL(24, 10) NULL,
+	CONSTRAINT [PK_tblGRUnPricedSpotTicket_intUnPricedSpotTicketId] PRIMARY KEY ([intUnPricedSpotTicketId]),
+	CONSTRAINT [FK_tblGRUnPricedSpotTicket_tblGRUnPriced_intUnPricedId] FOREIGN KEY ([intUnPricedId]) REFERENCES [dbo].[tblGRUnPriced] ([intUnPricedId]) ON DELETE CASCADE,	
+	CONSTRAINT [FK_tblGRUnPricedSpotTicket_tblSCTicket_intTicketId] FOREIGN KEY ([intTicketId]) REFERENCES [tblSCTicket]([intTicketId]),	
+)
---
+++ i21Database/dbo/Views/vyuGRGetUnPriced.sql
@@ -0,0 +1,21 @@
+﻿CREATE VIEW [dbo].[vyuGRGetUnPriced]
+AS
+SELECT 
+	 intUnPricedId		  = UnPriced.intUnPricedId
+	,strTicketType		  = UnPriced.strTicketType
+	,intItemId			  = UnPriced.intItemId
+	,strItemNo			  = Item.strItemNo
+	,intCompanyLocationId = UnPriced.intCompanyLocationId
+	,strLocationName	  = L.strLocationName
+	,strPriceTicket		  = UnPriced.strPriceTicket
+	,dblFuturesPrice	  = dblFuturesPrice
+	,dblFuturesBasis	  = dblFuturesBasis
+	,dblCashPrice		  = dblCashPrice
+	,intCreatedUserId     = UnPriced.intCreatedUserId
+	,strUserName          = Entity.strUserName
+	,dtmCreated           = UnPriced.dtmCreated
+	,ysnPosted            = UnPriced.ysnPosted
+FROM tblGRUnPriced UnPriced
+JOIN tblSMCompanyLocation L ON L.intCompanyLocationId = UnPriced.intCompanyLocationId
+JOIN tblICItem Item ON Item.intItemId = UnPriced.intItemId
+JOIN tblSMUserSecurity Entity ON Entity.intEntityId = UnPriced.intCreatedUserId
