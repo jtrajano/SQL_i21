41e8121c2498cfda232473e17102def68a88c067
Gopinath Nayak <gopinath.n@irely.com>
2017-10-26 12:45:19 +0530

    GRN-698

---
+++ i21Database/dbo/Tables/tblGRSellContract.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tblGRSellContract]
+(
+	[intSellContractId] INT NOT NULL IDENTITY,
+	[intConcurrencyId] INT NULL,
+	[intSellOffsiteId] INT NOT NULL, 
+    [intContractDetailId] INT NULL,
+	[dblUnits] DECIMAL(24, 10) NULL
+	CONSTRAINT [PK_tblGRSellContract_intSellContractId] PRIMARY KEY ([intSellContractId]),
+	CONSTRAINT [FK_tblGRSellContract_tblGRSellOffsite_intSellOffsiteId] FOREIGN KEY ([intSellOffsiteId]) REFERENCES [dbo].[tblGRSellOffsite] ([intSellOffsiteId]) ON DELETE CASCADE,
+	CONSTRAINT [FK_tblGRSellContract_tblCTContractDetail_intContractDetailId] FOREIGN KEY ([intContractDetailId]) REFERENCES [tblCTContractDetail]([intContractDetailId]),	
+)
