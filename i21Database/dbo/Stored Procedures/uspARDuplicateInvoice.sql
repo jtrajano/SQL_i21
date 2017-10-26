09460e658c262bd768e6a810d14f787925c6039b
Ruben Escarcha <ruben.escarcha@irely.com>
2017-10-26 22:54:33 +0800

    AR-5655 Recurring - Wrong Tax Group for Genearated Invoice

--- i21Database/dbo/Stored Procedures/uspARDuplicateInvoice.sql
+++ i21Database/dbo/Stored Procedures/uspARDuplicateInvoice.sql
@@ -546,7 +546,7 @@ END CATCH
 BEGIN TRY
 	IF ISNULL(@ForRecurring, 0) = 1
 		BEGIN
-			UPDATE tblARInvoiceDetail SET intTaxGroupId = NULL WHERE intInvoiceId = @CreatedInvoiceId
+			--UPDATE tblARInvoiceDetail SET intTaxGroupId = NULL WHERE intInvoiceId = @CreatedInvoiceId
 			EXEC [dbo].[uspARReComputeInvoiceTaxes]
 				 @InvoiceId	= @CreatedInvoiceId
 				,@DetailId	= NULL
