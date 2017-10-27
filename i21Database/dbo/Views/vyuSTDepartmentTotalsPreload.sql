a52e849b8e242f1723de49e60052488a1bbf0a3c
Henry Zaragoza Jr <henry.zaragoza@irely.com>
2017-10-27 15:07:16 +0800

    ST-547 Department totals does not display matching categories

---
+++ i21Database/dbo/Views/vyuSTDepartmentTotalsPreload.sql
@@ -0,0 +1,11 @@
+﻿CREATE VIEW [dbo].[vyuSTDepartmentTotalsPreload]
+AS
+SELECT DISTINCT 
+	Cat.intCategoryId
+	, ST.intStoreId
+	, Cat.strCategoryCode
+	, Cat.strDescription
+FROM tblICCategory Cat
+JOIN tblICCategoryLocation CatLoc ON CatLoc.intCategoryId = Cat.intCategoryId
+JOIN tblSMCompanyLocation L ON L.intCompanyLocationId = CatLoc.intLocationId
+JOIN tblSTStore ST ON ST.intCompanyLocationId = L.intCompanyLocationId
