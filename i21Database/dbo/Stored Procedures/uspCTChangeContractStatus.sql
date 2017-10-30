c35de9849571a0c886a9b8d4ba38164fc84c6693
Satya S Sahoo <satya.sahoo@irely.com>
2017-10-30 19:58:41 +0530

    CT-749

--- i21Database/dbo/Stored Procedures/uspCTChangeContractStatus.sql
+++ i21Database/dbo/Stored Procedures/uspCTChangeContractStatus.sql
@@ -1,7 +1,8 @@
 ﻿CREATE PROCEDURE [dbo].[uspCTChangeContractStatus]
-@strIds					NVARCHAR(MAX),
+	@strIds					NVARCHAR(MAX),
 	@intContractStatusId	INT,
-	@intEntityId			INT
+	@intEntityId			INT,
+	@strIdType				NVARCHAR(50) = 'Detail'
 AS
 
 BEGIN TRY
@@ -16,7 +17,16 @@ BEGIN TRY
 
 	DECLARE @ids TABLE (intId INT)
 
-	INSERT INTO @ids SELECT * FROM dbo.fnSplitStringWithTrim(@strIds,',') WHERE LTRIM(RTRIM(ISNULL(Item,''))) <> ''
+	IF @strIdType = 'Detail'
+	BEGIN
+		INSERT INTO @ids SELECT * FROM dbo.fnSplitStringWithTrim(@strIds,',') WHERE LTRIM(RTRIM(ISNULL(Item,''))) <> ''
+	END
+	IF @strIdType = 'Header'
+	BEGIN
+		INSERT INTO @ids SELECT intContractDetailId FROM tblCTContractDetail WHERE intContractHeaderId IN 
+							(SELECT * FROM dbo.fnSplitStringWithTrim(@strIds,',') WHERE LTRIM(RTRIM(ISNULL(Item,''))) <> '')
+	END
+
 	SELECT @intId = MIN(intId) FROM @ids
 
 	WHILE ISNULL(@intId,0) > 0
@@ -24,6 +34,11 @@ BEGIN TRY
 		SELECT	@intContractHeaderid = intContractHeaderId,@intOldContractStatusId = intContractStatusId FROM tblCTContractDetail WHERE intContractDetailId = @intId
 		SELECT	@strNewStatus = strContractStatus FROM tblCTContractStatus WHERE intContractStatusId = @intContractStatusId
 		SELECT	@strOldStatus = strContractStatus FROM tblCTContractStatus WHERE intContractStatusId = @intOldContractStatusId
+
+		IF @strOldStatus NOT IN ('Open', 'Unconfirmed', 'Re-Open') AND @intContractStatusId = 3
+		BEGIN
+			RAISERROR('Only Open, Unconfirmed and Re-Open contract can be changed to cancel.',16,1)
+		END
 
 		SET @details = '{"change": "tblCTContractDetails","children": [{"action": "Updated","change": "Updated - Record: ' + LTRIM(@intId)+ '","iconCls": "small-tree-modified","children": [{"change": "Contract Status","from": "' + @strOldStatus + '","to": "' + @strNewStatus + ' ","leaf": true,"iconCls": "small-gear"}]}],"iconCls":"small-tree-grid"}';
 
@@ -38,13 +53,10 @@ BEGIN TRY
 				@toValue				= '',
 				@details				= @details 		
 
-		DELETE FROM @ids WHERE intId = @intId 
-
-		SELECT @intId = MIN(intId) FROM @ids
+		SELECT @intId = MIN(intId) FROM @ids WHERE intId > @intId
 	END
 
-	UPDATE tblCTContractDetail SET intContractStatusId = @intContractStatusId WHERE intContractDetailId IN 
-	(SELECT Item FROM dbo.fnSplitStringWithTrim(@strIds,',') WHERE LTRIM(RTRIM(ISNULL(Item,''))) <> '')
+	UPDATE tblCTContractDetail SET intContractStatusId = @intContractStatusId WHERE intContractDetailId IN  (SELECT intId FROM @ids)
 
 END TRY
 
