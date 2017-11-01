a9515514804e1905e0df94749d6de635631a8e2f
Ruben Escarcha <ruben.escarcha@irely.com>
2017-11-02 00:29:22 +0800

    AR-5850 Aging details not showing at the bottom of the statement

--- i21Database/dbo/Stored Procedures/uspARCustomerStatementPaymentActivityReport.sql
+++ i21Database/dbo/Stored Procedures/uspARCustomerStatementPaymentActivityReport.sql
@@ -192,10 +192,12 @@ INSERT INTO tblARCustomerAgingStagingTable (
 		, strCompanyName
 		, strCompanyAddress
 )
-EXEC dbo.[uspARCustomerAgingAsOfDateReport] @dtmDateTo = @dtmDateToLocal
-										  , @strCompanyLocation = @strLocationNameLocal
-										  , @ysnIncludeBudget = @ysnIncludeBudgetLocal
-										  , @ysnIncludeCredits = @ysnPrintCreditBalanceLocal
+EXEC dbo.[uspARCustomerAgingAsOfDateReport] 
+										 --@dtmDateTo = @dtmDateToLocal
+										  --, 
+										  @strCompanyLocation = @strLocationNameLocal
+										  --, @ysnIncludeBudget = @ysnIncludeBudgetLocal
+										  --, @ysnIncludeCredits = @ysnPrintCreditBalanceLocal
 										  , @strCustomerName = @strCustomerNameLocal
 
 SET @query = CAST('' AS NVARCHAR(MAX)) + 
@@ -407,19 +409,19 @@ IF @ysnIncludeBudgetLocal = 1
 IF @ysnPrintOnlyPastDueLocal = 1
     BEGIN        
 		DELETE FROM @temp_statement_table WHERE DATEDIFF(DAYOFYEAR, dtmDueDate, @dtmDateToLocal) > 0
-        UPDATE tblARCustomerAgingStagingTable SET dblTotalAR = dblTotalAR - dbl0Days , dbl0Days = 0
+        --UPDATE tblARCustomerAgingStagingTable SET dblTotalAR = dblTotalAR - dbl0Days , dbl0Days = 0
     END
 
 IF @ysnPrintZeroBalanceLocal = 0
     BEGIN
         DELETE FROM @temp_statement_table WHERE ISNULL(dblBalance, 0) = 0
-        DELETE FROM tblARCustomerAgingStagingTable WHERE dblTotalAR = 0
+        --DELETE FROM tblARCustomerAgingStagingTable WHERE dblTotalAR = 0
     END
 
 IF @ysnPrintCreditBalanceLocal = 0
 	BEGIN
 		DELETE FROM @temp_statement_table WHERE strTransactionType IN ('Credit Memo', 'Customer Prepayment', 'Overpayment')
-		DELETE FROM tblARCustomerAgingStagingTable WHERE dblTotalAR < 0 
+		--DELETE FROM tblARCustomerAgingStagingTable WHERE dblTotalAR < 0 
 	END
 
 INSERT INTO @temp_cf_table (
