f095cacfb184d15d5912f75510eee65de4ae218a
Dan Jalimao <dan.jalimao@irely.com>
2017-11-01 16:41:43 +0800

    SM-3050

--- i21Database/dbo/Stored Procedures/uspSMDuplicateTaxCode.sql
+++ i21Database/dbo/Stored Procedures/uspSMDuplicateTaxCode.sql
@@ -11,14 +11,14 @@ BEGIN
 
 	INSERT dbo.tblSMTaxCode([strTaxCode], [intTaxClassId], [strDescription], /*[strCalculationMethod], [numRate], */
 							[strTaxAgency], [strAddress], [strZipCode], [strState], [strCity], [strCountry], [strCounty], 
-							[intSalesTaxAccountId], [intPurchaseTaxAccountId], [strTaxableByOtherTaxes], [ysnTaxOnly], [ysnBalance], 
+							[intSalesTaxAccountId], [intPurchaseTaxAccountId], [strTaxableByOtherTaxes], [ysnTaxOnly], --[ysnBalance], 
 							[ysnCheckoffTax], [strStoreTaxNumber])
 	SELECT CASE @intCount WHEN 0 
 		   THEN 'DUP: ' + [strTaxCode] 
 		   ELSE 'DUP: ' + [strTaxCode] + ' (' + @intCount + ')' END,
 	[intTaxClassId], [strDescription], /*[strCalculationMethod], [numRate], */
 	[strTaxAgency], [strAddress], [strZipCode], [strState], [strCity],[strCountry], [strCounty], 
-	[intSalesTaxAccountId], [intPurchaseTaxAccountId], [strTaxableByOtherTaxes], [ysnTaxOnly], [ysnBalance], 
+	[intSalesTaxAccountId], [intPurchaseTaxAccountId], [strTaxableByOtherTaxes], [ysnTaxOnly], --[ysnBalance], 
 	[ysnCheckoffTax], [strStoreTaxNumber]
 	FROM dbo.tblSMTaxCode 
 	WHERE [intTaxCodeId] = @intTaxCodeId;
