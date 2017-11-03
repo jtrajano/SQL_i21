7e232ecf92c77966b376cf0d37ddf8a214746540
Erick <erick.delacruz@irely.com>
2017-11-03 13:56:05 +0800

    AP-3934

--- i21Database/dbo/Views/vyuAPSearchVendorInquiry.sql
+++ i21Database/dbo/Views/vyuAPSearchVendorInquiry.sql
@@ -1,7 +1,7 @@
 ﻿CREATE VIEW [dbo].[vyuAPSearchVendorInquiry]
 AS
 
-SELECT 
+SELECT TOP 100 PERCENT
 	entity.intEntityId
 	,entity.strName
 	,CASE WHEN vendor.strVendorId = '' THEN entity.strEntityNo ELSE vendor.strVendorId END AS strEntityNo
@@ -14,3 +14,4 @@ INNER JOIN dbo.tblEMEntity entity ON vendor.intEntityId = entity.intEntityId
 INNER JOIN dbo.tblEMEntityToContact entityToContact ON entityToContact.intEntityId = entity.intEntityId
 INNER JOIN dbo.tblEMEntity entityContact
 		ON entityToContact.intEntityContactId = entityContact.intEntityId AND entityToContact.ysnDefaultContact = 1
+ORDER BY entity.strName
