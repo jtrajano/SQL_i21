cf6349f9d8a26bf878879708fafc05ff267566d4
JeffTrajano <jeffrey.trajano@irely.com>
2017-11-03 08:52:47 +0800

    GL-3832 Unable to update/add Segment Account

--- i21Database/dbo/Views/vyuGLSegmentDetail.sql
+++ i21Database/dbo/Views/vyuGLSegmentDetail.sql
@@ -1,20 +1,24 @@
 ﻿CREATE VIEW [dbo].[vyuGLSegmentDetail]
 AS
-SELECT      
-    S.intAccountSegmentId, 
-    U.intSort, S.strCode, 
-    C.strAccountCategory, 
-    S.intAccountCategoryId, 
-    S.strDescription, 
-    G.strAccountType, 
-    S.intAccountStructureId,
+SELECT        
+	S.intAccountSegmentId, 
+	U.intSort, S.strCode, 
+	C.strAccountCategory, 
+	S.intAccountCategoryId, 
+	S.strDescription, 
+	G.strAccountType, 
+	S.intAccountStructureId,
     G.intAccountGroupId,
     G.strAccountGroup,
-    U.intStructureType,
-    U.strStructureName
+	U.intStructureType,
+	U.strStructureName,
+	ISNULL(Mapping.c,0)  ysnUsed
 FROM            
-    dbo.tblGLAccountSegment AS S LEFT OUTER JOIN
-    dbo.tblGLAccountGroup AS G ON S.intAccountGroupId = G.intAccountGroupId LEFT OUTER JOIN
-    dbo.tblGLAccountCategory AS C ON S.intAccountCategoryId = C.intAccountCategoryId LEFT OUTER JOIN
-    dbo.tblGLAccountStructure AS U ON S.intAccountStructureId = U.intAccountStructureId
+	dbo.tblGLAccountSegment AS S LEFT OUTER JOIN
+	dbo.tblGLAccountGroup AS G ON S.intAccountGroupId = G.intAccountGroupId LEFT OUTER JOIN
+	dbo.tblGLAccountCategory AS C ON S.intAccountCategoryId = C.intAccountCategoryId LEFT OUTER JOIN
+	dbo.tblGLAccountStructure AS U ON S.intAccountStructureId = U.intAccountStructureId 
+	outer APPLY (
+		SELECT  TOP 1 CONVERT(BIT,1)    c FROM tblGLAccountSegmentMapping M WHERE M.intAccountSegmentId = S.intAccountSegmentId
+	)Mapping
 GO
