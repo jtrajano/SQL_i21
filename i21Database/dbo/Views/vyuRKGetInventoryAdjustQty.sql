45d14b31951e253d542d8a5e0aa48d8c5f5f3adc
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2017-11-03 19:09:36 +0530

    RM-934

--- i21Database/dbo/Views/vyuRKGetInventoryAdjustQty.sql
+++ i21Database/dbo/Views/vyuRKGetInventoryAdjustQty.sql
@@ -3,6 +3,7 @@
 AS
 
 SELECT DISTINCT ISNULL(dblBalance,0)+(isnull(dblQuantity,0)-(case when dblAdjustByQuantity > 0 then -(isnull(dblAdjustByQuantity,0)) else abs(isnull(dblAdjustByQuantity,0)) end)) as dblDetailQuantity,(isnull(dblOpenReceive,0)-isnull(dblAdjustByQuantity,0)
+
 ) OpenQty,
 isnull(dblBalance,0) dblBalance,intContractDetailId,isnull(dblInvoiceQty,0) dblInvoiceQty FROM (
 SELECT min(isnull(cd.dblBalance,0)) dblBalance,
@@ -12,10 +13,7 @@ SELECT min(isnull(cd.dblBalance,0)) dblBalance,
         SUM(a.dblAdjustByQuantity) dblAdjustByQuantity,
 		isnull((
 		select dblQtyShipped from (
-							SELECT distinct (isnull(id.dblQtyShipped,0)) dblQtyShipped,i.intInvoiceId from tblARInvoice i
-							JOIN tblARInvoiceDetail id on i.intInvoiceId=id.intInvoiceId and i.ysnPosted=1 
-							JOIN vyuLGDeliveryOpenPickLotDetails pld on pld.intSContractDetailId=id.intContractDetailId
-							where pld.intPContractDetailId=cd.intContractDetailId )t		
+							SELECT isnull(sum(dblSalesInvoiceQty),0) dblQtyShipped FROM vyuRKGetInvoicedQty inv WHERE inv.intSContractDetailId=cd.intContractDetailId )t		
 		 ),0) dblInvoiceQty
 FROM tblCTContractDetail cd
 LEFT JOIN tblICInventoryReceiptItem ri on cd.intContractDetailId=ri.intLineNo
