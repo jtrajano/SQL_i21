7b3a60822e957e45e9b4cc6214d69c54470162e1
Jb Mago <benedict.mago@irely.com>
2017-11-03 21:26:15 +0800

    IC-4450

--- i21Database/dbo/Tables/tblICItemVendorXref.sql
+++ i21Database/dbo/Tables/tblICItemVendorXref.sql
@@ -16,7 +16,7 @@ Type the overview for the table here.
 		[intItemId] INT NOT NULL, 
 		[intItemLocationId] INT NULL, 
 		[intVendorId] INT NOT NULL, 
-		[intVendorSetupId] INT NOT NULL,
+		[intVendorSetupId] INT NULL,
 		[strVendorProduct] NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL, 
 		[strProductDescription] NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NULL, 
 		[dblConversionFactor] NUMERIC(38, 20) NOT NULL DEFAULT ((0)), 
