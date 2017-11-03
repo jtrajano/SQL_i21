a2bbda806e73644d8525d8a73fa865c31feb8389
IRELY\Swetosuvro <swetosuvro.g@irely.com>
2017-11-03 16:47:50 +0530

    LG-1258

--- i21Database/dbo/Views/vyuLGContainerPositionView.sql
+++ i21Database/dbo/Views/vyuLGContainerPositionView.sql
@@ -88,6 +88,6 @@ FROM (
 	WHERE ISNULL(LC.ysnRejected, 0) = 0
 	GROUP BY CD.intItemId,I.strItemNo,I.strDescription,CH.strContractNumber,CH.intContractHeaderId,CH.strCustomerContract
 		,Pos.strPosition,EY.strName,CB.strDescription,CH.dblQuantity
-		,CH.strInternalComment,CD.dblBasis
+		,CH.strInternalComment,CD.dblBasis, CD.dtmStartDate, CD.dtmEndDate
 	) tbl
 WHERE intTrucksRemaining > 0
