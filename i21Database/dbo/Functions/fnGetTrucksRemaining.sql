3d09f2fcd79e1c59a6152b17cbafed92132e931e
IRELY\Swetosuvro <swetosuvro.g@irely.com>
2017-11-06 13:22:52 +0530

    LG-1262

--- i21Database/dbo/Functions/fnGetTrucksRemaining.sql
+++ i21Database/dbo/Functions/fnGetTrucksRemaining.sql
@@ -2,6 +2,8 @@
 	@intContractHeaderId INT
 	,@intItemId INT
 	,@dblBasis NUMERIC(18, 6)
+	,@dtmStartDate DATETIME
+	,@dtmEndDate DATETIME
 	)
 RETURNS INT
 AS
@@ -18,6 +20,8 @@ BEGIN
 		AND CH.intContractHeaderId = @intContractHeaderId
 		AND CD.dblBasis = @dblBasis
 		AND CD.intItemId = @intItemId
+		AND CD.dtmStartDate = @dtmStartDate
+		AND CD.dtmEndDate = @dtmEndDate
 	GROUP BY CH.intContractHeaderId
 		,CD.intItemId
 		,CD.dtmStartDate
