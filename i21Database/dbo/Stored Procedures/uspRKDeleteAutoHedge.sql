f957458a176b3cbc4ee3ce4e4b148b2a38b5d422
Satya S Sahoo <satya.sahoo@irely.com>
2017-11-07 21:09:13 +0530

    CT-1688

--- i21Database/dbo/Stored Procedures/uspRKDeleteAutoHedge.sql
+++ i21Database/dbo/Stored Procedures/uspRKDeleteAutoHedge.sql
@@ -14,17 +14,17 @@ BEGIN
       SET @TransId = 1
 END
 
-IF (@TransId = 1)
-BEGIN
-      RAISERROR('The selected transaction is used for match PnS. Cannot delete this transaction.',16,1)
-END
+--IF (@TransId = 1)
+--BEGIN
+--      RAISERROR('The selected transaction is used for match PnS. Cannot delete this transaction.',16,1)
+--END
 
 IF EXISTS
 (SELECT 1 FROM  tblRKFutOptTransaction t WHERE intFutOptTransactionId=@intFutOptTransactionId  AND ISNULL(ysnFreezed,0) = 1)
 BEGIN
 RAISERROR('The selected transaction is already reconciled. Cannot delete this transaction.',16,1)
 END
-
+	  DELETE FROM tblRKMatchFuturesPSDetail WHERE @intFutOptTransactionId IN (intLFutOptTransactionId,intSFutOptTransactionId)
       DELETE FROM tblRKAssignFuturesToContractSummary WHERE intFutOptTransactionId=@intFutOptTransactionId 
       DELETE FROM tblRKFutOptTransaction WHERE intFutOptTransactionId=@intFutOptTransactionId 
 
