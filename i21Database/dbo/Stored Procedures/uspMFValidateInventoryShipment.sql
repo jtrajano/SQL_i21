1172d8c9400c522a30e8b17cd80bd133bdd8033f
Premkumar <Prem.Selladurai@gmail.com>
2017-11-07 17:01:08 +0530

    MFG-2929

---
+++ i21Database/dbo/Stored Procedures/uspMFValidateInventoryShipment.sql
@@ -0,0 +1,41 @@
+﻿CREATE PROCEDURE uspMFValidateInventoryShipment (@intInventoryShipmentId INT)
+AS
+BEGIN
+	DECLARE @intBondStatusId INT
+		,@strLotNumber NVARCHAR(50)
+		,@strMessage NVARCHAR(max)
+
+	SELECT @intBondStatusId = intBondStatusId
+	FROM tblMFCompanyPreference
+
+	IF @intBondStatusId IS NULL
+		RETURN
+
+	IF @intBondStatusId IS NOT NULL
+		AND EXISTS (
+			SELECT *
+			FROM tblICInventoryShipmentItem SI
+			JOIN tblICInventoryShipmentItemLot SL ON SI.intInventoryShipmentItemId = SL.intInventoryShipmentItemId
+			JOIN tblMFLotInventory LI ON LI.intLotId = SL.intLotId
+			WHERE SI.intInventoryShipmentId = @intInventoryShipmentId
+				AND LI.intBondStatusId = @intBondStatusId
+			)
+	BEGIN
+		SELECT @strLotNumber = L.strLotNumber
+		FROM dbo.tblICInventoryShipmentItem SI
+		JOIN dbo.tblICInventoryShipmentItemLot SL ON SI.intInventoryShipmentItemId = SL.intInventoryShipmentItemId
+		JOIN dbo.tblICLot L ON L.intLotId = SL.intLotId
+		JOIN dbo.tblMFLotInventory LI ON LI.intLotId = SL.intLotId
+		WHERE SI.intInventoryShipmentId = @intInventoryShipmentId
+			AND LI.intBondStatusId = @intBondStatusId
+
+		SELECT @strMessage = 'Lot/Pallet ' + @strLotNumber + ' is not bond released. Please choose bond released lot/pallet to continue.'
+
+		RAISERROR (
+				@strMessage
+				,16
+				,1
+				)
+	END
+END
+
