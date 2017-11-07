11f73fdd5047bcc92b39a0a5967d98307333a1bc
Kelvin Quinto <kelvin.quinto@irely.com>
2017-11-07 17:04:01 +0800

    AR-5870

--- i21Database/dbo/Stored Procedures/uspARForgiveServiceChargeInvoices.sql
+++ i21Database/dbo/Stored Procedures/uspARForgiveServiceChargeInvoices.sql
@@ -11,6 +11,7 @@ SET ANSI_WARNINGS OFF
 IF ISNULL(@InvoiceIds, '') <> ''
 	BEGIN
 		DECLARE @ServiceChargeToForgive TABLE (intInvoiceId INT, strInvoiceNumber NVARCHAR(50) COLLATE Latin1_General_CI_AS)
+		DECLARE @ServiceChargeHasPayments NVARCHAR(MAX)
 
 		INSERT INTO @ServiceChargeToForgive
 		SELECT SCI.intInvoiceId
@@ -27,7 +28,26 @@ IF ISNULL(@InvoiceIds, '') <> ''
 		) SCI ON DV.intID = SCI.intInvoiceId
 
 		IF EXISTS(SELECT NULL FROM @ServiceChargeToForgive)
-		BEGIN
+		BEGIN			
+			SELECT @ServiceChargeHasPayments = COALESCE(@ServiceChargeHasPayments + ', ', '') + RTRIM(LTRIM(I.strInvoiceNumber)) 
+			FROM @ServiceChargeToForgive I
+			INNER JOIN (
+				SELECT PD.intPaymentId
+					 , PD.intInvoiceId
+				FROM dbo.tblARPaymentDetail PD WITH (NOLOCK)
+				INNER JOIN (
+					SELECT intPaymentId
+					FROM dbo.tblARPayment WITH (NOLOCK)
+				) P ON PD.intPaymentId = P.intPaymentId
+			) P ON I.intInvoiceId = P.intInvoiceId
+
+			IF ISNULL(@ServiceChargeHasPayments, '') <> ''
+				BEGIN
+					SET @ServiceChargeHasPayments = 'These Service Charges Invoice has payments: ' + @ServiceChargeHasPayments
+					RAISERROR(@ServiceChargeHasPayments, 16, 1)
+					RETURN;
+				END
+
 			UPDATE INV
 			SET INV.ysnForgiven = 1
 			FROM tblARInvoice INV
