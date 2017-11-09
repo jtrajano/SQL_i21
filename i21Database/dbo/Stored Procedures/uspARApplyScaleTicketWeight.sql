087d211c64f6bd53d59d959ca4f7148c5e106611
Kelvin Quinto <kelvin.quinto@irely.com>
2017-11-09 16:59:33 +0800

    AR-4808

---
+++ i21Database/dbo/Stored Procedures/uspARApplyScaleTicketWeight.sql
@@ -0,0 +1,85 @@
+﻿CREATE PROCEDURE [dbo].[uspARApplyScaleTicketWeight]
+	  @intSalesOrderId	INT
+	, @intScaleUOMId    INT = NULL
+	, @intUserId		INT = NULL
+	, @dblGrossWeight	NUMERIC(18, 6) = 0
+    , @dblTareWeight	NUMERIC(18, 6) = 0
+	, @dblNetWeight		NUMERIC(18, 6) = 0
+	, @intNewInvoiceId	INT = NULL OUTPUT
+AS
+BEGIN
+	DECLARE @intScaleItem			INT = NULL
+		  , @intStockUOMId			INT = NULL
+		  , @intItemUOMId			INT = NULL
+		  , @intSalesOrderDetailId	INT = NULL
+		  , @dblTotalTreatment		NUMERIC(18, 6) = 0
+
+	SELECT TOP 1 @intScaleItem			= SOD.intItemId
+			   , @intStockUOMId			= I.intUnitMeasureId
+			   , @intItemUOMId			= SOD.intItemUOMId
+			   , @intSalesOrderDetailId = SOD.intSalesOrderDetailId
+	FROM dbo.tblSOSalesOrderDetail SOD WITH (NOLOCK)
+	INNER JOIN (
+		SELECT I.intItemId
+			 , IUOM.intUnitMeasureId
+		FROM dbo.tblICItem I WITH (NOLOCK)
+		INNER JOIN (
+			SELECT intItemId
+				 , intUnitMeasureId
+			FROM dbo.tblICItemUOM WITH (NOLOCK)
+			WHERE ysnStockUnit = 1
+		) IUOM ON I.intItemId = IUOM.intItemId
+		WHERE I.ysnUseWeighScales = 1
+	) I ON SOD.intItemId = I.intItemId
+	WHERE intSalesOrderId = @intSalesOrderId
+
+	IF ISNULL(@intSalesOrderId, 0) = 0
+		BEGIN
+			RAISERROR('Sales Order ID is required.', 16, 1)
+			RETURN;
+		END
+
+	IF ISNULL(@dblNetWeight, 0) = 0
+		BEGIN
+			RAISERROR('Net Weight should not be zero.', 16, 1)
+			RETURN;
+		END
+
+	IF NOT EXISTS (SELECT NULL FROM tblSOSalesOrder WHERE intSalesOrderId = @intSalesOrderId)
+		BEGIN
+			RAISERROR('Sales Order is not existing.', 16, 1)
+			RETURN;
+		END
+
+	IF ISNULL(@intScaleItem, 0) = 0
+		BEGIN
+			RAISERROR('Sales Order doesn''t have scale item.', 16, 1)
+			RETURN;
+		END
+
+	SELECT @dblTotalTreatment = ISNULL(dbo.fnCalculateQtyBetweenUOM(@intScaleUOMId, SOD.intItemUOMId, SUM(dblQtyOrdered)), 0)
+	FROM dbo.tblSOSalesOrderDetail SOD WITH (NOLOCK)
+	INNER JOIN (
+		SELECT intItemId
+		FROM dbo.tblICItem WITH (NOLOCK)
+		WHERE ysnUseWeighScales = 0
+	) ITEM ON SOD.intItemId = ITEM.intItemId
+	WHERE SOD.intSalesOrderId = @intSalesOrderId
+	GROUP BY SOD.intItemUOMId
+
+	UPDATE tblSOSalesOrderDetailId
+	SET dblQtyOrdered = ISNULL(dbo.fnCalculateQtyBetweenUOM(@intItemUOMId, @intScaleUOMId, @dblNetWeight - @dblTotalTreatment), 0)
+	WHERE intSalesOrderDetailId = @intSalesOrderDetailId
+	  AND intSalesOrderId = @intSalesOrderId
+	  AND intItemId = @intScaleItem
+
+	EXEC dbo.uspSOProcessToInvoice @SalesOrderId = @intSalesOrderId
+								 , @UserId = @intUserId
+								 , @NewInvoiceId = @intNewInvoiceId OUT
+
+	IF ISNULL(@intNewInvoiceId, 0) = 0
+		BEGIN
+			RAISERROR('Failed to Create Invoice.', 16, 1)
+			RETURN;
+		END	
+END
---
+++ i21Database/dbo/Views/vyuARGetSalesOrdersToWeight.sql
@@ -0,0 +1,27 @@
+﻿CREATE VIEW [dbo].[vyuARGetSalesOrdersToWeight]
+AS
+SELECT DISTINCT
+	   SO.intSalesOrderId
+	 , SO.intCompanyLocationId
+	 , SO.strLocationName
+	 , SO.strSalesOrderNumber
+FROM dbo.tblSOSalesOrderDetail SOD WITH (NOLOCK)
+INNER JOIN (
+	SELECT SO.intSalesOrderId
+		 , SO.intCompanyLocationId
+		 , SO.strSalesOrderNumber
+		 , L.strLocationName
+	FROM dbo.tblSOSalesOrder SO WITH (NOLOCK)
+	INNER JOIN (
+		SELECT intCompanyLocationId
+			 , strLocationName
+		FROM dbo.tblSMCompanyLocation WITH (NOLOCK)
+	) L ON SO.intCompanyLocationId = L.intCompanyLocationId
+	WHERE SO.strOrderStatus NOT IN ('Closed', 'Short Closed', 'Cancelled')
+	  AND SO.strTransactionType = 'Order'
+) SO ON SOD.intSalesOrderId = SO.intSalesOrderId
+INNER JOIN (
+	SELECT intItemId
+	FROM dbo.tblICItem WITH (NOLOCK)
+	WHERE ysnUseWeighScales = 1
+) ITEMS ON SOD.intItemId = ITEMS.intItemId
