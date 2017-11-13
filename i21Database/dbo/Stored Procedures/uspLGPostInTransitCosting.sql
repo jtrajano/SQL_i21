34104fc2de1f12bd9ba017f7b270b6510ac8c4ab
IRELY\Swetosuvro <swetosuvro.g@irely.com>
2017-11-13 16:53:25 +0530

    LG-1261

--- i21Database/dbo/Stored Procedures/uspLGPostInTransitCosting.sql
+++ i21Database/dbo/Stored Procedures/uspLGPostInTransitCosting.sql
@@ -22,6 +22,7 @@ BEGIN TRY
 	DECLARE @ysnAllowBlankGLEntries AS BIT = 1
 	DECLARE @dummyGLEntries AS RecapTableType
 	DECLARE @intSalesContractId INT
+	DECLARE @intItemLocationId INT
 
 	SELECT @strBatchIdUsed = strBatchId
 		,@strLoadNumber = strLoadNumber
@@ -170,6 +171,16 @@ BEGIN TRY
 
 			EXEC dbo.uspGLBookEntries @GLEntries
 				,@ysnPost
+
+			SELECT TOP 1 @intItemLocationId = intItemLocationId
+			FROM @ItemsToPost
+			WHERE strTransactionId = @strLoadNumber
+				AND intTransactionId = @intLoadId
+
+			UPDATE tblICInventoryTransaction
+			SET intItemLocationId = @intItemLocationId
+			WHERE intTransactionId = @intLoadId
+				AND strTransactionId = @strLoadNumber
 		END
 
 	IF @intPurchaseSale = 3
@@ -247,204 +258,6 @@ BEGIN TRY
 		LEFT JOIN tblICItemUOM LotItemUOM ON LotItemUOM.intItemUOMId = Lot.intItemUOMId
 		WHERE LOAD.intLoadId = @intLoadId
 
-		-- Call the post routine 
-		--IF EXISTS (SELECT TOP 1 1 FROM @ItemsForPost)
-		--BEGIN
-		--	SET @ysnAllowBlankGLEntries = 0
-
-		--	-- Call the post routine 
-		--	INSERT INTO @dummyGLEntries (
-		--		[dtmDate]
-		--		,[strBatchId]
-		--		,[intAccountId]
-		--		,[dblDebit]
-		--		,[dblCredit]
-		--		,[dblDebitUnit]
-		--		,[dblCreditUnit]
-		--		,[strDescription]
-		--		,[strCode]
-		--		,[strReference]
-		--		,[intCurrencyId]
-		--		,[dblExchangeRate]
-		--		,[dtmDateEntered]
-		--		,[dtmTransactionDate]
-		--		,[strJournalLineDescription]
-		--		,[intJournalLineNo]
-		--		,[ysnIsUnposted]
-		--		,[intUserId]
-		--		,[intEntityId]
-		--		,[strTransactionId]
-		--		,[intTransactionId]
-		--		,[strTransactionType]
-		--		,[strTransactionForm]
-		--		,[strModuleName]
-		--		,[intConcurrencyId]
-		--		,[dblDebitForeign]
-		--		,[dblDebitReport]
-		--		,[dblCreditForeign]
-		--		,[dblCreditReport]
-		--		,[dblReportingRate]
-		--		,[dblForeignRate]
-		--		,[strRateType]
-		--		)
-		--	EXEC @intReturnValue = dbo.uspICPostCosting @ItemsForPost
-		--		,@strBatchId
-		--		,NULL
-		--		,@intEntityUserSecurityId
-
-		--	--IF @intReturnValue < 0
-		--	--	GOTO With_Rollback_Exit
-
-		--	INSERT INTO @GLEntries (
-		--		[dtmDate]
-		--		,[strBatchId]
-		--		,[intAccountId]
-		--		,[dblDebit]
-		--		,[dblCredit]
-		--		,[dblDebitUnit]
-		--		,[dblCreditUnit]
-		--		,[strDescription]
-		--		,[strCode]
-		--		,[strReference]
-		--		,[intCurrencyId]
-		--		,[dblExchangeRate]
-		--		,[dtmDateEntered]
-		--		,[dtmTransactionDate]
-		--		,[strJournalLineDescription]
-		--		,[intJournalLineNo]
-		--		,[ysnIsUnposted]
-		--		,[intUserId]
-		--		,[intEntityId]
-		--		,[strTransactionId]
-		--		,[intTransactionId]
-		--		,[strTransactionType]
-		--		,[strTransactionForm]
-		--		,[strModuleName]
-		--		,[intConcurrencyId]
-		--		,[dblDebitForeign]
-		--		,[dblDebitReport]
-		--		,[dblCreditForeign]
-		--		,[dblCreditReport]
-		--		,[dblReportingRate]
-		--		,[dblForeignRate]
-		--		,[strRateType]
-		--		)
-		--	EXEC @intReturnValue = dbo.uspICCreateGLEntries @strBatchId
-		--		,NULL
-		--		,@intEntityUserSecurityId
-
-		--	--IF @intReturnValue < 0
-		--	--	GOTO With_Rollback_Exit
-		--END
-
-			INSERT INTO @ItemsToPost (
-				intItemId
-				,intItemLocationId
-				,intItemUOMId
-				,dtmDate
-				,dblQty
-				,dblUOMQty
-				,dblCost
-				,dblValue
-				,dblSalesPrice
-				,intCurrencyId
-				,dblExchangeRate
-				,intTransactionId
-				,intTransactionDetailId
-				,strTransactionId
-				,intTransactionTypeId
-				,intLotId
-				,intSourceTransactionId
-				,strSourceTransactionId
-				,intSourceTransactionDetailId
-				,intFobPointId
-				,intInTransitSourceLocationId
-				,intForexRateTypeId
-				,dblForexRate
-				)
-			SELECT [intItemId]
-				,[intItemLocationId]
-				,[intItemUOMId]
-				,[dtmDate]
-				,- [dblQty]
-				,[dblUOMQty]
-				,[dblCost]
-				,[dblValue]
-				,[dblSalesPrice]
-				,[intCurrencyId]
-				,[dblExchangeRate]
-				,[intTransactionId]
-				,[intTransactionDetailId]
-				,[strTransactionId]
-				,[intTransactionTypeId]
-				,[intLotId]
-				,[intTransactionId]
-				,[strTransactionId]
-				,[intTransactionDetailId]
-				,[intFobPointId] = 2
-				,[intInTransitSourceLocationId] = t.intItemLocationId
-				,[intForexRateTypeId] = t.intForexRateTypeId
-				,[dblForexRate] = t.dblForexRate
-			FROM tblICInventoryTransaction t
-			WHERE t.strTransactionId = @intLoadId
-				AND t.ysnIsUnposted = 0
-				AND t.strBatchId = @strBatchId
-				AND @intFOBPointId = 2
-				AND t.dblQty < 0 -- Ensure the Qty is negative. Credit Memo are positive Qtys.  Credit Memo does not ship out but receives stock. 
-
-			IF EXISTS (
-					SELECT TOP 1 1
-					FROM @ItemsToPost
-					)
-			BEGIN
-				SET @ysnAllowBlankGLEntries = 0
-
-				INSERT INTO @GLEntries (
-					[dtmDate]
-					,[strBatchId]
-					,[intAccountId]
-					,[dblDebit]
-					,[dblCredit]
-					,[dblDebitUnit]
-					,[dblCreditUnit]
-					,[strDescription]
-					,[strCode]
-					,[strReference]
-					,[intCurrencyId]
-					,[dblExchangeRate]
-					,[dtmDateEntered]
-					,[dtmTransactionDate]
-					,[strJournalLineDescription]
-					,[intJournalLineNo]
-					,[ysnIsUnposted]
-					,[intUserId]
-					,[intEntityId]
-					,[strTransactionId]
-					,[intTransactionId]
-					,[strTransactionType]
-					,[strTransactionForm]
-					,[strModuleName]
-					,[intConcurrencyId]
-					,[dblDebitForeign]
-					,[dblDebitReport]
-					,[dblCreditForeign]
-					,[dblCreditReport]
-					,[dblReportingRate]
-					,[dblForeignRate]
-					)
-				EXEC @intReturnValue = dbo.uspICPostInTransitCosting @ItemsToPost = @ItemsToPost
-					,@strBatchId = @strBatchId
-					,@strAccountToCounterInventory = NULL
-					,@intEntityUserSecurityId = @intEntityUserSecurityId
-					,@strGLDescription = ''
-
-				--IF @intReturnValue < 0
-				--	GOTO With_Rollback_Exit
-			END
-		END
-	END
-	ELSE
-	BEGIN
 		INSERT INTO @ItemsToPost (
 			intItemId
 			,intItemLocationId
@@ -470,59 +283,43 @@ BEGIN TRY
 			,intForexRateTypeId
 			,dblForexRate
 			)
-		SELECT LD.intItemId
-			,IL.intItemLocationId
-			,LD.intItemUOMId
-			,GETDATE()
-			,LD.dblQuantity * - 1
-			,IU.dblUnitQty
-			,CASE WHEN AD.ysnSeqSubCurrency = 1 THEN AD.dblQtyToPriceUOMConvFactor*ISNULL(AD.dblSeqPrice, 0)/100 ELSE AD.dblQtyToPriceUOMConvFactor*ISNULL(AD.dblSeqPrice, 0) END * - 1
-			,CASE WHEN AD.ysnSeqSubCurrency = 1 THEN AD.dblQtyToPriceUOMConvFactor*ISNULL(AD.dblSeqPrice, 0)/100 ELSE AD.dblQtyToPriceUOMConvFactor*ISNULL(AD.dblSeqPrice, 0) END * LD.dblQuantity * - 1
-			,0.0
-			,L.intCurrencyId
-			,AD.dblNetWtToPriceUOMConvFactor
-			,L.intLoadId
-			,intLoadDetailId
-			,L.strLoadNumber
-			,44 intTransactionTypeId
-			,NULL
-			,L.intLoadId
-			,strLoadNumber
-			,0
-			,FP.intFobPointId
-			,IL.intItemLocationId
-			,CASE WHEN CD.ysnUseFXPrice = 1 THEN CD.intRateTypeId ELSE NULL END
-			,CASE WHEN CD.ysnUseFXPrice = 1 THEN CD.dblRate ELSE 1 END
-		FROM tblLGLoad L
-		JOIN tblLGLoadDetail LD ON L.intLoadId = LD.intLoadId
-		JOIN tblICItemLocation IL ON IL.intItemId = LD.intItemId
-			AND LD.intPCompanyLocationId = IL.intLocationId
-		JOIN tblICItemUOM IU ON IU.intItemUOMId = LD.intItemUOMId
-		JOIN tblCTContractDetail CD ON CD.intContractDetailId = LD.intPContractDetailId
-		JOIN tblCTContractHeader CH ON CH.intContractHeaderId = CD.intContractHeaderId
-		CROSS APPLY dbo.fnCTGetAdditionalColumnForDetailView(CD.intContractDetailId) AD
-		LEFT JOIN tblSMFreightTerms FT ON FT.intFreightTermId = L.intFreightTermId
-		LEFT JOIN tblICFobPoint FP ON FP.strFobPoint = FT.strFobPoint
-		WHERE L.intLoadId = @intLoadId
-		GROUP BY LD.intItemId
-			,IL.intItemLocationId
-			,LD.intItemUOMId
-			,LD.dblQuantity
-			,IU.dblUnitQty
-			,AD.dblSeqPrice
-			,L.intLoadId
-			,L.intCurrencyId
-			,AD.dblNetWtToPriceUOMConvFactor
-			,LD.intLoadDetailId
-			,L.strLoadNumber
-			,FP.intFobPointId
-			,AD.dblQtyToPriceUOMConvFactor
-			,AD.ysnSeqSubCurrency
-			,CD.intRateTypeId
-			,CD.dblRate
-			,CD.ysnUseFXPrice
+		SELECT [intItemId]
+			,[intItemLocationId]
+			,[intItemUOMId]
+			,[dtmDate]
+			,- [dblQty]
+			,[dblUOMQty]
+			,[dblCost]
+			,[dblValue]
+			,[dblSalesPrice]
+			,[intCurrencyId]
+			,[dblExchangeRate]
+			,[intTransactionId]
+			,[intTransactionDetailId]
+			,[strTransactionId]
+			,[intTransactionTypeId]
+			,[intLotId]
+			,[intTransactionId]
+			,[strTransactionId]
+			,[intTransactionDetailId]
+			,[intFobPointId] = 2
+			,[intInTransitSourceLocationId] = t.intItemLocationId
+			,[intForexRateTypeId] = t.intForexRateTypeId
+			,[dblForexRate] = t.dblForexRate
+		FROM tblICInventoryTransaction t
+		WHERE t.strTransactionId = @intLoadId
+			AND t.ysnIsUnposted = 0
+			AND t.strBatchId = @strBatchId
+			AND @intFOBPointId = 2
+			AND t.dblQty < 0 -- Ensure the Qty is negative. Credit Memo are positive Qtys.  Credit Memo does not ship out but receives stock. 
 
+		IF EXISTS (
+				SELECT TOP 1 1
+				FROM @ItemsToPost
+				)
 		BEGIN
+			SET @ysnAllowBlankGLEntries = 0
+
 			INSERT INTO @GLEntries (
 				[dtmDate]
 				,[strBatchId]
@@ -557,19 +354,66 @@ BEGIN TRY
 				,[dblForeignRate]
 				)
 			EXEC @intReturnValue = dbo.uspICPostInTransitCosting @ItemsToPost = @ItemsToPost
-				,@strBatchId = @strBatchIdUsed
-				,@strAccountToCounterInventory = 'AP Clearing'
+				,@strBatchId = @strBatchId
+				,@strAccountToCounterInventory = NULL
 				,@intEntityUserSecurityId = @intEntityUserSecurityId
 				,@strGLDescription = ''
 
-			IF @intReturnValue < 0
-			BEGIN
-				RAISERROR (@strErrMsg,16,1)
+			--IF @intReturnValue < 0
+			--	GOTO With_Rollback_Exit
 			END
-
-			EXEC dbo.uspGLBookEntries @GLEntries
-				,@ysnPost
 		END
+	END
+	ELSE
+	BEGIN
+		INSERT INTO @GLEntries (
+				[dtmDate] 
+				,[strBatchId]
+				,[intAccountId]
+				,[dblDebit]
+				,[dblCredit]
+				,[dblDebitUnit]
+				,[dblCreditUnit]
+				,[strDescription]
+				,[strCode]
+				,[strReference]
+				,[intCurrencyId]
+				,[dblExchangeRate]
+				,[dtmDateEntered]
+				,[dtmTransactionDate]
+				,[strJournalLineDescription]
+				,[intJournalLineNo]
+				,[ysnIsUnposted]
+				,[intUserId]
+				,[intEntityId]
+				,[strTransactionId]
+				,[intTransactionId]
+				,[strTransactionType]
+				,[strTransactionForm]
+				,[strModuleName]
+				,[intConcurrencyId]
+				,[dblDebitForeign]	
+				,[dblDebitReport]	
+				,[dblCreditForeign]	
+				,[dblCreditReport]	
+				,[dblReportingRate]	
+				,[dblForeignRate]
+				,[strRateType]
+		)
+		EXEC	@intReturnValue = dbo.uspICUnpostCosting
+					@intLoadId
+				,@strLoadNumber
+				,@strBatchIdUsed
+				,@intEntityUserSecurityId	
+				,0
+
+		IF @intReturnValue < 0
+		BEGIN
+			RAISERROR (@strErrMsg,16,1)
+		END
+
+		EXEC dbo.uspGLBookEntries @GLEntries
+			,@ysnPost
 	END
 END TRY
 
