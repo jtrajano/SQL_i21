e8a1485f7cb2391730c66670525c2a494b195f94
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2017-11-14 14:17:03 +0530

    M-941

--- i21Database/dbo/Views/vyuRKRiskPositionContractDetail.sql
+++ i21Database/dbo/Views/vyuRKRiskPositionContractDetail.sql
@@ -27,9 +27,10 @@ SELECT DISTINCT CT.strContractType
 	,CH.dblNoOfLots dblHeaderNoOfLots
 	,CH.ysnMultiplePriceFixation
 	,CD.dblNoOfLots dblDetailNoOfLots
-	,CT.intContractTypeId
+	,CT.intContractTypeId,FM.strFutureMonth
 FROM tblCTContractHeader CH
 JOIN tblCTContractDetail CD ON CH.intContractHeaderId = CD.intContractHeaderId AND CD.intContractStatusId not in(2,3)
+JOIN tblRKFuturesMonth FM on FM.intFutureMonthId=CD.intFutureMonthId
 JOIN tblCTContractType CT ON CT.intContractTypeId = CH.intContractTypeId
 JOIN tblICItemUOM IU ON IU.intItemUOMId = CD.intItemUOMId
 JOIN tblEMEntity EY ON EY.intEntityId = CH.intEntityId 
