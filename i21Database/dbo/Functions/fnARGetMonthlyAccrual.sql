68602d510620782dadf368f128675d20c3a6dd2b
Kelvin Quinto <kelvin.quinto@irely.com>
2017-11-14 09:48:47 +0800

    AR-2650

---
+++ i21Database/dbo/Functions/fnARGetMonthlyAccrual.sql
@@ -0,0 +1,38 @@
+﻿CREATE FUNCTION [dbo].[fnARGetMonthlyAccrual]
+(
+	  @intInvoiceId			INT
+	, @dtmAsOfDate			DATETIME
+)
+RETURNS @tblMonthlyAccrual TABLE
+(
+	  intInvoiceId				INT				NOT NULL
+	, strMonthAccrued			NVARCHAR(100)	COLLATE Latin1_General_CI_AS NOT NULL
+	, dblRunningAccrualBalance	NUMERIC(18, 6)	NOT NULL DEFAULT 0
+)
+AS
+BEGIN
+	DECLARE @intPeriodsAccrue			INT
+		  , @intCounter					INT	= 1
+		  , @dtmPostDate				DATETIME
+		  , @dblInvoiceTotal			NUMERIC(18, 6)	= 0
+		  , @dblRunningAccrualBalance	NUMERIC(18, 6)	= 0		  
+
+	SELECT TOP 1 @intPeriodsAccrue			= intPeriodsToAccrue
+			   , @dtmPostDate				= dtmPostDate
+			   , @dblInvoiceTotal			= dblInvoiceTotal
+			   , @dblRunningAccrualBalance	= dblInvoiceTotal
+	FROM dbo.tblARInvoice WITH (NOLOCK)
+	WHERE intInvoiceId = @intInvoiceId
+
+	WHILE (@intCounter <= @intPeriodsAccrue + 1)
+		BEGIN
+			INSERT @tblMonthlyAccrual
+			SELECT @intInvoiceId, CONVERT(CHAR(4), @dtmPostDate, 100) + CONVERT(CHAR(4), @dtmPostDate, 120), @dblRunningAccrualBalance
+
+			SET @intCounter = @intCounter + 1
+			SET @dtmPostDate = DATEADD(MONTH, 1, @dtmPostDate)
+			SET @dblRunningAccrualBalance = dbo.fnRoundBanker(@dblRunningAccrualBalance - (@dblInvoiceTotal / @intPeriodsAccrue), 2)
+		END
+
+	RETURN
+END
