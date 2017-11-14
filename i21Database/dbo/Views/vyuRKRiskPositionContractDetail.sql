ee8e011a5b3cdd8282e35c9d7f9d0f940615f888
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2017-11-14 14:09:43 +0530

    RM-941

--- i21Database/dbo/Views/vyuRKRiskPositionContractDetail.sql
+++ i21Database/dbo/Views/vyuRKRiskPositionContractDetail.sql
@@ -1,16 +1,37 @@
 ﻿CREATE VIEW vyuRKRiskPositionContractDetail
+AS
+SELECT * FROM (
+SELECT DISTINCT CT.strContractType
+	,CD.dblBalance
+	,isnull(CD.dblQuantity,0) - 
+	(case when CH.intContractTypeId=1 then 
+			isnull((select sum(a.dblPurchaseInvoiceQty)  from vyuRKGetInvoicedQty a where a.intPContractDetailId=CD.intContractDetailId),0) 
+			ELSE 
+			isnull((select sum(b.dblSalesInvoiceQty) from vyuRKGetInvoicedQty b where b.intSContractDetailId=CD.intContractDetailId),0)  end) AS dblDetailQuantity
+	,CH.strContractNumber
+	,CD.intContractSeq
+	,CD.dtmStartDate
+	,EY.strName strEntityName
+	,CD.dblNoOfLots
+	,CH.intContractHeaderId
+	,CD.intPricingTypeId
+	,CH.intCommodityId
+	,CD.intCompanyLocationId
+	,CD.intFutureMarketId
+	,CD.intFutureMonthId
+	,CD.intItemUOMId
+	,CD.intItemId
+	,IU.intUnitMeasureId
+	,CD.intContractStatusId
+	,CD.intContractDetailId
+	,CH.dblNoOfLots dblHeaderNoOfLots
+	,CH.ysnMultiplePriceFixation
+	,CD.dblNoOfLots dblDetailNoOfLots
+	,CT.intContractTypeId
+FROM tblCTContractHeader CH
+JOIN tblCTContractDetail CD ON CH.intContractHeaderId = CD.intContractHeaderId AND CD.intContractStatusId not in(2,3)
+JOIN tblCTContractType CT ON CT.intContractTypeId = CH.intContractTypeId
+JOIN tblICItemUOM IU ON IU.intItemUOMId = CD.intItemUOMId
+JOIN tblEMEntity EY ON EY.intEntityId = CH.intEntityId 
+)t WHERE  dblDetailQuantity >0
 
-as
-
-	SELECT	CT.strContractType,	CD.dblBalance,	CD.dblQuantity	AS	dblDetailQuantity,	CH.strContractNumber,
-			CD.intContractSeq,	CD.dtmStartDate, 	EY.strEntityName,	CD.dblNoOfLots,		CH.intContractHeaderId, CD.intPricingTypeId,	
-			CH.intCommodityId,	CD.intCompanyLocationId,	CD.intFutureMarketId, CD.intFutureMonthId,CD.intItemUOMId, CD.intItemId,IU.intUnitMeasureId,
-			 MO.strFutureMonth,	CD.intContractStatusId,CD.intContractDetailId
-			 ,CH.dblNoOfLots dblHeaderNoOfLots	,CH.ysnMultiplePriceFixation,CD.dblNoOfLots dblDetailNoOfLots,CT.intContractTypeId			
-	FROM	tblCTContractHeader				CH	
-	JOIN	tblCTContractDetail				CD	ON	CH.intContractHeaderId		=	CD.intContractHeaderId		
-	join	tblCTContractType				CT  ON  CT.intContractTypeId		=	CH.intContractTypeId		
-	JOIN	tblICItemUOM					IU	ON	IU.intItemUOMId				=	CD.intItemUOMId
-	JOIN	vyuCTEntity						EY	ON	EY.intEntityId				=	CH.intEntityId			AND
-										EY.strEntityType						=	(CASE WHEN CH.intContractTypeId = 1 THEN 'Vendor' ELSE 'Customer' END)
-	LEFT JOIN	tblRKFuturesMonth				MO	ON	MO.intFutureMonthId		=	CD.intFutureMonthId		
