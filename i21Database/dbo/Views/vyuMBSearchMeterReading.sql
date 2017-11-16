e2d460e2c6a4cec94930f1248716ae6705bad869
Lawrence Zabala <lawrence.zabala@irely.com>
2017-11-16 14:49:36 +0800

    MB-172

--- i21Database/dbo/Views/vyuMBSearchMeterReading.sql
+++ i21Database/dbo/Views/vyuMBSearchMeterReading.sql
@@ -33,8 +33,8 @@ SELECT MRDetail.intMeterReadingDetailId
 	, MRDetail.dblLastDollars
 	, MRDetail.dblCurrentDollars
 	, MRDetail.dblDollarsSold
-	, dblUnitCost = ISNULL(ICTransaction.dblCost, 0)
-	, dblTotalCost = (ISNULL(ICTransaction.dblCost, 0) * ISNULL(MRDetail.dblQuantitySold, 0))
+	, dblUnitCost = ISNULL(Trans.dblCost, 0)
+	, dblTotalCost = (ISNULL(Trans.dblCost, 0) * ISNULL(MRDetail.dblQuantitySold, 0))
 	, MRDetail.dblDollarsOwed
 	, MRDetail.dblDifference
 	, dblTotalTax = (CASE WHEN ISNULL(Invoice.intInvoiceId, '') = '' OR ISNULL(Invoice.dblQtyShipped, 0) = 0 THEN 0.00
@@ -42,18 +42,18 @@ SELECT MRDetail.intMeterReadingDetailId
 	, dblJobberMargin = (CASE WHEN ISNULL(Invoice.intInvoiceId, '') = '' OR ISNULL(Invoice.dblQtyShipped, 0) = 0 THEN (CASE WHEN ConRate.strRateType = 'Jobber' THEN ISNULL(ConRate.dblBaseRate, 0)
 																					ELSE MRDetail.dblGrossPrice - ISNULL(ConRate.dblBaseRate, 0) END)
 							ELSE (CASE WHEN ConRate.strRateType = 'Jobber' THEN ISNULL(ConRate.dblBaseRate, 0)
-									ELSE MRDetail.dblGrossPrice - (ISNULL(ICTransaction.dblCost, 0) + ((ISNULL(Invoice.dblTotalTax, 0) / ISNULL(Invoice.dblQtyShipped, 0))) + ISNULL(ConRate.dblBaseRate, 0)) END) END)	
+									ELSE MRDetail.dblGrossPrice - (ISNULL(Trans.dblCost, 0) + ((ISNULL(Invoice.dblTotalTax, 0) / ISNULL(Invoice.dblQtyShipped, 0))) + ISNULL(ConRate.dblBaseRate, 0)) END) END)	
 	, dblJobberProfit = (CASE WHEN ISNULL(Invoice.intInvoiceId, '') = '' OR ISNULL(Invoice.dblQtyShipped, 0) = 0 THEN (CASE WHEN ConRate.strRateType = 'Jobber' THEN ISNULL(ConRate.dblBaseRate, 0) * MRDetail.dblQuantitySold
 																					ELSE (MRDetail.dblGrossPrice - ISNULL(ConRate.dblBaseRate, 0)) * MRDetail.dblQuantitySold END)
 							ELSE (CASE WHEN ConRate.strRateType = 'Jobber' THEN ISNULL(ConRate.dblBaseRate, 0) * MRDetail.dblQuantitySold
-									ELSE (MRDetail.dblGrossPrice - (ISNULL(ICTransaction.dblCost, 0) + ((ISNULL(Invoice.dblTotalTax, 0) / ISNULL(Invoice.dblQtyShipped, 0))) + ISNULL(ConRate.dblBaseRate, 0))) * MRDetail.dblQuantitySold END) END)
+									ELSE (MRDetail.dblGrossPrice - (ISNULL(Trans.dblCost, 0) + ((ISNULL(Invoice.dblTotalTax, 0) / ISNULL(Invoice.dblQtyShipped, 0))) + ISNULL(ConRate.dblBaseRate, 0))) * MRDetail.dblQuantitySold END) END)
 	, dblDealerMargin = (CASE WHEN ISNULL(Invoice.intInvoiceId, '') = '' OR ISNULL(Invoice.dblQtyShipped, 0) = 0 THEN ((CASE WHEN ConRate.strRateType = 'Jobber' THEN MRDetail.dblGrossPrice - ISNULL(ConRate.dblBaseRate, 0)
 																					ELSE ISNULL(ConRate.dblBaseRate, 0) END))
-							ELSE ((CASE WHEN ConRate.strRateType = 'Jobber' THEN MRDetail.dblGrossPrice - (ISNULL(ICTransaction.dblCost, 0) + ((ISNULL(Invoice.dblTotalTax, 0) / ISNULL(Invoice.dblQtyShipped, 0))) + ISNULL(ConRate.dblBaseRate, 0))
+							ELSE ((CASE WHEN ConRate.strRateType = 'Jobber' THEN MRDetail.dblGrossPrice - (ISNULL(Trans.dblCost, 0) + ((ISNULL(Invoice.dblTotalTax, 0) / ISNULL(Invoice.dblQtyShipped, 0))) + ISNULL(ConRate.dblBaseRate, 0))
 									ELSE ISNULL(ConRate.dblBaseRate, 0) END)) END)
 	, dblDealerProfit = (CASE WHEN ISNULL(Invoice.intInvoiceId, '') = '' OR ISNULL(Invoice.dblQtyShipped, 0) = 0 THEN ((CASE WHEN ConRate.strRateType = 'Jobber' THEN (MRDetail.dblGrossPrice - ISNULL(ConRate.dblBaseRate, 0)) * MRDetail.dblQuantitySold
 																					ELSE ISNULL(ConRate.dblBaseRate, 0) * MRDetail.dblQuantitySold END))
-							ELSE ((CASE WHEN ConRate.strRateType = 'Jobber' THEN (MRDetail.dblGrossPrice - (ISNULL(ICTransaction.dblCost, 0) + ((ISNULL(Invoice.dblTotalTax, 0) / ISNULL(Invoice.dblQtyShipped, 0))) + ISNULL(ConRate.dblBaseRate, 0))) * MRDetail.dblQuantitySold
+							ELSE ((CASE WHEN ConRate.strRateType = 'Jobber' THEN (MRDetail.dblGrossPrice - (ISNULL(Trans.dblCost, 0) + ((ISNULL(Invoice.dblTotalTax, 0) / ISNULL(Invoice.dblQtyShipped, 0))) + ISNULL(ConRate.dblBaseRate, 0))) * MRDetail.dblQuantitySold
 									ELSE ISNULL(ConRate.dblBaseRate, 0) * MRDetail.dblQuantitySold END)) END)
 	, MRDetail.ysnPosted
 	, MRDetail.dtmPostedDate
@@ -62,10 +62,8 @@ SELECT MRDetail.intMeterReadingDetailId
 FROM vyuMBGetMeterReadingDetail MRDetail
 LEFT JOIN tblARInvoiceDetail Invoice ON Invoice.intInvoiceId = MRDetail.intInvoiceId
 	AND Invoice.intItemId = MRDetail.intItemId
-LEFT JOIN tblICInventoryTransaction ICTransaction ON ICTransaction.intTransactionId = Invoice.intInvoiceId
-	AND ICTransaction.strTransactionForm = 'Invoice'
-	AND ICTransaction.intItemId = MRDetail.intItemId
-	AND ISNULL(ICTransaction.ysnIsUnposted, 0) = 0
+LEFT JOIN dbo.fnMBGetWeightedCostAverage() Trans ON Trans.intMeterReadingId = MRDetail.intMeterReadingId
+	AND Trans.intMeterReadingDetailId = MRDetail.intMeterReadingDetailId
 OUTER APPLY (
 	SELECT TOP 100 PERCENT * FROM vyuMBGetConsignmentRateDetail ConDetail
 	WHERE ConDetail.intConsignmentGroupId = MRDetail.intConsignmentGroupId
