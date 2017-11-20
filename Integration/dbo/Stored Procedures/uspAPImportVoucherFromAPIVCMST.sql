d4ae73b43756e2da10bafab8999fad3753bcf4a8
Erick <erick.delacruz@irely.com>
2017-11-20 17:09:06 +0800

    AP-4337

--- Integration/dbo/Stored Procedures/uspAPImportVoucherFromAPIVCMST.sql
+++ Integration/dbo/Stored Procedures/uspAPImportVoucherFromAPIVCMST.sql
@@ -422,6 +422,7 @@ SELECT
 FROM tblAPBill A
 INNER JOIN tblAPVendor B
 	ON A.intEntityVendorId = B.intEntityId
+INNER JOIN #tmpVouchersWithRecordNumber tmpCreatedVouchers ON A.intBillId = tmpCreatedVouchers.intBillId
 INNER JOIN (tmp_apivcmstImport C2 INNER JOIN tmp_aphglmstImport C 
 			ON C2.apivc_ivc_no = C.aphgl_ivc_no 
 			AND C2.apivc_vnd_no = C.aphgl_vnd_no)
