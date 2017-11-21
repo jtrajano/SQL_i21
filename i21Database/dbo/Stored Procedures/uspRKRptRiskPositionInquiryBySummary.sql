71efa896f8205f4be0eb38b33e2a0cf576d0944d
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2017-11-21 21:29:38 +0530

    RM-950

--- i21Database/dbo/Stored Procedures/uspRKRptRiskPositionInquiryBySummary.sql
+++ i21Database/dbo/Stored Procedures/uspRKRptRiskPositionInquiryBySummary.sql
@@ -53,7 +53,7 @@ SELECT @intForecastWeeklyConsumptionUOMId = [from] FROM @temp_xml_table WHERE [f
 
 DECLARE @temp as Table (  
      intRowNumber int,
-  strGroup  nvarchar(200) COLLATE Latin1_General_CI_AS, 
+     strGroup  nvarchar(200) COLLATE Latin1_General_CI_AS, 
      Selection  nvarchar(200) COLLATE Latin1_General_CI_AS,  
      PriceStatus  nvarchar(50) COLLATE Latin1_General_CI_AS,  
      strFutureMonth  nvarchar(20) COLLATE Latin1_General_CI_AS,  
@@ -69,8 +69,50 @@ DECLARE @temp as Table (
      intContractHeaderId int ,
      intFutOptTransactionHeaderId int       
      )
+DECLARE @strRiskView nvarchar(100)
+SELECT TOP 1 @strRiskView = strRiskView from tblRKCompanyPreference
+if(@strRiskView='Trader/Elevator') 
+BEGIN
+INSERT INTO @temp (intRowNumber,strGroup,Selection,PriceStatus,strFutureMonth,strAccountNumber,dblNoOfContract,strTradeNo,TransactionDate,  
+     TranType, CustVendor,dblNoOfLot,dblQuantity,intOrderByHeading,intContractHeaderId,intFutOptTransactionHeaderId)
+Exec uspRKRiskPositionInquiry  @intCommodityId=@intCommodityId,  
+        @intCompanyLocationId=@intCompanyLocationId,  
+        @intFutureMarketId = @intFutureMarketId,  
+        @intFutureMonthId = @intFutureMonthId,  
+        @intUOMId = @intUOMId,  
+        @intDecimal = @intDecimal,
+        @intForecastWeeklyConsumption  =@intForecastWeeklyConsumption ,
+        @intForecastWeeklyConsumptionUOMId =@intForecastWeeklyConsumptionUOMId
 
 
+UPDATE @temp
+SET strGroup =  case when Selection IN ('Physical position / Differential cover', 'Physical position / Basis risk') then '01.'+ strGroup
+				     when Selection = 'Specialities & Low grades' then  '02.'+ strGroup 
+					 when Selection = 'Total speciality delta fixed' then  '03.'+ strGroup 
+					 when Selection = 'Terminal position (a. in lots )' then  '04.'+ strGroup 
+					 when Selection = 'Terminal position (Avg Long Price)' then  '05.'+ strGroup 
+					 when Selection LIKE ('%Terminal position (b.%') then  '06.'+ strGroup 
+					 when Selection = 'Delta options' then  '07.'+ strGroup 
+					 when Selection = 'F&O' then  '08.'+ strGroup 
+					 when Selection LIKE ('%Total F&O(b. in%') then  '09.'+ strGroup 
+					 when Selection IN ('Outright coverage', 'Net market risk') then  '10.'+ strGroup 
+					 when Selection IN ('Switch position', 'Futures required') then  '11.'+ strGroup 
+					 end
+					 		
+SELECT  intRowNumber ,
+	strGroup
+   ,Selection,PriceStatus,strFutureMonth,strAccountNumber,  
+            CONVERT(DOUBLE PRECISION,ROUND(dblNoOfContract,@intDecimal))  dblNoOfContract,
+   CONVERT(NUMERIC(24,10),CONVERT(NVARCHAR,DENSE_RANK() OVER   
+   (PARTITION BY NULL ORDER BY 
+   CASE WHEN  strFutureMonth ='Previous' THEN '01/01/1900'  WHEN  strFutureMonth ='Total' THEN '01/01/9999' ELSE CONVERT(DATETIME,'01 '+strFutureMonth) END ))+ '.1234567890') AS [Rank] 
+  FROM @temp 
+
+END
+
+ELSE
+
+BEGIN
 INSERT INTO @temp (intRowNumber,strGroup,Selection,PriceStatus,strFutureMonth,strAccountNumber,dblNoOfContract,strTradeNo,TransactionDate,  
      TranType, CustVendor,dblNoOfLot,dblQuantity,intOrderByHeading,intContractHeaderId,intFutOptTransactionHeaderId)
 Exec uspRKRiskPositionInquiryBySummary  @intCommodityId=@intCommodityId,  
@@ -96,3 +138,4 @@ FROM @temp where isnull(dblNoOfContract,0) <> 0
 SELECT * FROM #temp
 ORDER BY strGroup,PriceStatus, 
 CASE WHEN  strFutureMonth ='Previous' THEN '01/01/1900'  WHEN  strFutureMonth ='Total' THEN '01/01/9999' ELSE CONVERT(DATETIME,'01 '+strFutureMonth) END
+END
