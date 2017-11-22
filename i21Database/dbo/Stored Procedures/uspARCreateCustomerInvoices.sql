d107e64b1caaafcd270001e27163bf5b628a44bf
Ruben Escarcha <ruben.escarcha@irely.com>
2017-11-22 15:30:54 +0800

    AR-5987 Batch posting failed

--- i21Database/dbo/Stored Procedures/uspARCreateCustomerInvoices.sql
+++ i21Database/dbo/Stored Procedures/uspARCreateCustomerInvoices.sql
@@ -15,8 +15,13 @@ SET NOCOUNT ON
 SET XACT_ABORT ON
 SET ANSI_WARNINGS OFF
 
-DECLARE @ZeroDecimal NUMERIC(18, 6) = 0.000000
-		,@DateOnly DATETIME = CAST(GETDATE() AS DATE)
+DECLARE @ZeroDecimal	NUMERIC(18, 6) = 0.000000
+		,@DateOnly		DATETIME = CAST(GETDATE() AS DATE)
+		,@InitTranCount	INT
+		,@Savepoint		NVARCHAR(32)
+
+SET @InitTranCount = @@TRANCOUNT
+SET @Savepoint = SUBSTRING(('ARCreateCustomerInvoices' + CONVERT(VARCHAR, @InitTranCount)), 1, 32)
 
 DECLARE @InvoicesToGenerate AS InvoiceStagingTable
 DELETE FROM @InvoicesToGenerate
@@ -846,7 +851,12 @@ END
 
 
 IF ISNULL(@RaiseError,0) = 0	
-	BEGIN TRANSACTION
+BEGIN
+	IF @InitTranCount = 0
+		BEGIN TRANSACTION
+	ELSE
+		SAVE TRANSACTION @Savepoint
+END
 
 DECLARE  @AddDetailError NVARCHAR(MAX)
 		,@IntegrationLog InvoiceIntegrationLogStagingTable
@@ -1313,7 +1323,15 @@ VALUES(
 END TRY
 BEGIN CATCH
 	IF ISNULL(@RaiseError,0) = 0
-		ROLLBACK TRANSACTION
+	BEGIN
+		IF @InitTranCount = 0
+			IF (XACT_STATE()) <> 0
+				ROLLBACK TRANSACTION
+		ELSE
+			IF (XACT_STATE()) <> 0
+				ROLLBACK TRANSACTION @Savepoint
+	END
+
 	SET @ErrorMessage = ERROR_MESSAGE();
 	IF ISNULL(@RaiseError,0) = 1
 		RAISERROR(@ErrorMessage, 16, 1);
@@ -1616,7 +1634,15 @@ BEGIN TRY
 		IF LEN(ISNULL(@AddDetailError,'')) > 0
 			BEGIN
 				IF ISNULL(@RaiseError,0) = 0
-					ROLLBACK TRANSACTION
+				BEGIN
+					IF @InitTranCount = 0
+						IF (XACT_STATE()) <> 0
+							ROLLBACK TRANSACTION
+					ELSE
+						IF (XACT_STATE()) <> 0
+							ROLLBACK TRANSACTION @Savepoint
+				END
+
 				SET @ErrorMessage = @AddDetailError;
 				IF ISNULL(@RaiseError,0) = 1
 					RAISERROR(@ErrorMessage, 16, 1);
@@ -1625,7 +1651,15 @@ BEGIN TRY
 END TRY
 BEGIN CATCH
 	IF ISNULL(@RaiseError,0) = 0
-		ROLLBACK TRANSACTION
+	BEGIN
+		IF @InitTranCount = 0
+			IF (XACT_STATE()) <> 0
+				ROLLBACK TRANSACTION
+		ELSE
+			IF (XACT_STATE()) <> 0
+				ROLLBACK TRANSACTION @Savepoint
+	END
+
 	SET @ErrorMessage = ERROR_MESSAGE();
 	IF ISNULL(@RaiseError,0) = 1
 		RAISERROR(@ErrorMessage, 16, 1);
@@ -1688,7 +1722,15 @@ BEGIN TRY
 END TRY
 BEGIN CATCH
 	IF ISNULL(@RaiseError,0) = 0
-		ROLLBACK TRANSACTION
+	BEGIN
+		IF @InitTranCount = 0
+			IF (XACT_STATE()) <> 0
+				ROLLBACK TRANSACTION
+		ELSE
+			IF (XACT_STATE()) <> 0
+				ROLLBACK TRANSACTION @Savepoint
+	END
+
 	SET @ErrorMessage = ERROR_MESSAGE();
 	IF ISNULL(@RaiseError,0) = 1
 		RAISERROR(@ErrorMessage, 16, 1);
@@ -1696,7 +1738,24 @@ BEGIN CATCH
 END CATCH
 
 IF ISNULL(@RaiseError,0) = 0
-	COMMIT TRANSACTION
+BEGIN
+
+	IF @InitTranCount = 0
+		BEGIN
+			IF (XACT_STATE()) = -1
+				ROLLBACK TRANSACTION
+			IF (XACT_STATE()) = 1
+				COMMIT TRANSACTION
+		END		
+	ELSE
+		BEGIN
+			IF (XACT_STATE()) = -1
+				ROLLBACK TRANSACTION  @Savepoint
+			--IF (XACT_STATE()) = 1
+			--	COMMIT TRANSACTION  @Savepoint
+		END	
+END
+
 SET @ErrorMessage = NULL;
 RETURN 1;
 	
