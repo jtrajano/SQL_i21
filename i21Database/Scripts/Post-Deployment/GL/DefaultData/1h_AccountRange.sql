efa60b6c1adbe06e7b9451b90bcd233070f79d41
JeffTrajano <jeffrey.trajano@irely.com>
2017-11-22 13:39:36 +0800

    GL-3834 Adjust the chart of accounts to allow for multiple ranges of revenues

--- i21Database/Scripts/Post-Deployment/GL/DefaultData/1h_AccountRange.sql
+++ i21Database/Scripts/Post-Deployment/GL/DefaultData/1h_AccountRange.sql
@@ -1,7 +1,36 @@
 ﻿GO
-	PRINT N'Begin Generating Account Range'
-	
-	EXEC dbo.uspGLGenerateAccountRange
-	
-	PRINT N'Finished Generating Account Range'
+	PRINT 'Start generating default account categories'
+GO
+	MERGE 
+	INTO	dbo.tblGLAccountRange
+	WITH	(HOLDLOCK) 
+	AS		RangeTable
+	USING	(
+			SELECT strAccountType = 'All',			intConcurrencyId = 1, intAccountGroupId = 0 union all
+			SELECT strAccountType = 'Asset',		intConcurrencyId = 1, intAccountGroupId = 1 union all
+			SELECT strAccountType = 'Liability',	intConcurrencyId = 1, intAccountGroupId = 2 union all
+			SELECT strAccountType = 'Revenue',		intConcurrencyId = 1, intAccountGroupId = 4 union all
+			SELECT strAccountType = 'Revenue 2',	intConcurrencyId = 1, intAccountGroupId = 4 union all
+			SELECT strAccountType = 'Revenue 3',	intConcurrencyId = 1, intAccountGroupId = 4 union all
+			SELECT strAccountType = 'Revenue 4',	intConcurrencyId = 1, intAccountGroupId = 4 union all
+			SELECT strAccountType = 'Expense',		intConcurrencyId = 1, intAccountGroupId = 5 union all
+			SELECT strAccountType = 'Expense 2',	intConcurrencyId = 1, intAccountGroupId = 5 union all
+			SELECT strAccountType = 'Expense 3',	intConcurrencyId = 1, intAccountGroupId = 5 union all
+			SELECT strAccountType = 'Expense 4',	intConcurrencyId = 1, intAccountGroupId = 5 
+	) AS RangeHardCodedValues
+		ON  RangeTable.strAccountType = RangeHardCodedValues.strAccountType
+	WHEN MATCHED THEN 
+		UPDATE 
+		SET 	RangeTable.intAccountGroupId = RangeHardCodedValues.intAccountGroupId
+	WHEN NOT MATCHED THEN
+		INSERT (
+			strAccountType
+			,intAccountGroupId
+			,intConcurrencyId
+		)
+		VALUES (
+			RangeHardCodedValues.strAccountType
+			,RangeHardCodedValues.intAccountGroupId
+			,1
+		);
 GO
