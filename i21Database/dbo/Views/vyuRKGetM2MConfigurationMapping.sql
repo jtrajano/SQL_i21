7fcc67ab478e4b6d82f3a76728de6c18b5300c7d
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2017-11-23 22:21:47 +0530

    RM-951

---
+++ i21Database/dbo/Views/vyuRKGetM2MConfigurationMapping.sql
@@ -0,0 +1,5 @@
+﻿CREATE VIEW vyuRKGetM2MConfigurationMapping
+AS
+select c.intM2MConfigurationId,i.intItemId,i.strItemNo,b.intContractBasisId,b.strContractBasis, c.intConcurrencyId,strAdjustmentType from tblRKM2MConfiguration c
+join tblICItem i on c.intItemId=i.intItemId
+join tblCTContractBasis b on b.intContractBasisId=c.intContractBasisId
