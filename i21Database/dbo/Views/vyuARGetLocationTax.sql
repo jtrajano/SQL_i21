1f6142ac780f12b91ec84cf485f18658e88be6b0
Kelvin Quinto <kelvin.quinto@irely.com>
2017-11-23 18:28:40 +0800

    AR-2177

---
+++ i21Database/dbo/Views/vyuARGetLocationTax.sql
@@ -0,0 +1,58 @@
+﻿CREATE VIEW [dbo].[vyuARGetLocationTax]
+AS
+SELECT intTransactionId					= INVOICE.intInvoiceId
+	 , intCompanyLocationId				= INVOICE.intCompanyLocationId
+	 , intFreightTermId					= INVOICE.intFreightTermId
+	 , intCompanyLocationTaxGroupId		= TAX.intTaxGroupId
+	 , strCompanyLocationName			= CASE WHEN ISNULL(FREIGHT.strFobPoint, '') = 'Destination' THEN EL.strLocationName ELSE COMPANY.strLocationName END
+	 , strFreightTerm					= FREIGHT.strFreightTerm
+	 , strFobPoint						= FREIGHT.strFobPoint
+	 , strTaxGroup						= TAX.strTaxGroup
+	 , strTaxCode						= TAX.strTaxCode
+	 , strTaxClass						= TAX.strTaxClass
+	 , strTransactionType				= INVOICE.strTransactionType
+FROM dbo.tblARInvoice INVOICE WITH (NOLOCK)
+INNER JOIN (
+	SELECT intCompanyLocationId
+		 , intTaxGroupId
+		 , strLocationName
+	FROM dbo.tblSMCompanyLocation WITH (NOLOCK)
+) COMPANY ON INVOICE.intCompanyLocationId = COMPANY.intCompanyLocationId
+LEFT JOIN (
+	SELECT intFreightTermId		 
+		 , strFreightTerm
+		 , strFobPoint
+	FROM dbo.tblSMFreightTerms FT WITH (NOLOCK)	
+) FREIGHT ON INVOICE.intFreightTermId = FREIGHT.intFreightTermId
+LEFT JOIN (
+	SELECT intEntityLocationId
+		 , intTaxGroupId
+		 , intEntityId
+		 , strLocationName
+	FROM dbo.tblEMEntityLocation WITH (NOLOCK)		
+) EL ON INVOICE.intShipToLocationId = EL.intEntityLocationId
+	AND INVOICE.intEntityCustomerId = EL.intEntityId
+LEFT JOIN (
+	SELECT TG.intTaxGroupId
+		 , strTaxGroup		= TG.strDescription
+		 , strTaxCode		= TC.strDescription
+		 , TCC.strTaxClass
+	FROM dbo.tblSMTaxGroup TG WITH (NOLOCK)
+	INNER JOIN (
+		SELECT intTaxCodeId
+			 , intTaxGroupId
+		FROM dbo.tblSMTaxGroupCode WITH (NOLOCK)
+	) TGC ON TG.intTaxGroupId = TGC.intTaxGroupId
+	INNER JOIN (
+		SELECT intTaxCodeId
+			 , intTaxClassId
+			 , strTaxCode
+			 , strDescription
+		FROM dbo.tblSMTaxCode WITH (NOLOCK)
+	) TC ON TGC.intTaxCodeId = TC.intTaxCodeId
+	INNER JOIN (
+		SELECT intTaxClassId
+			 , strTaxClass			
+		FROM dbo.tblSMTaxClass WITH (NOLOCK)
+	) TCC ON TC.intTaxClassId = TCC.intTaxClassId
+) TAX ON TAX.intTaxGroupId = CASE WHEN ISNULL(INVOICE.intFreightTermId, 0) <> 0 AND ISNULL(FREIGHT.strFobPoint, '') = 'Destination' THEN EL.intTaxGroupId ELSE COMPANY.intTaxGroupId END
---
+++ i21Database/dbo/Views/vyuARGetTaxDetail.sql
@@ -0,0 +1,39 @@
+﻿CREATE VIEW [dbo].[vyuARGetTaxDetail]
+AS 
+SELECT intItemId			= ID.intItemId
+	 , intCategoryId		= ITEM.intCategoryId
+	 , strItemNo			= ITEM.strItemNo
+	 , strCategoryCode		= ITEMCATEGORY.strCategoryCode
+	 , strTransactionType   = 'Invoice'
+	 , TAXDETAILS.*	 
+FROM dbo.tblARInvoiceDetail ID WITH (NOLOCK)
+INNER JOIN (
+	SELECT intTransactionTid = IDT.intInvoiceDetailId
+		 , IDT.intTaxClassId
+		 , IDT.intTaxCodeId
+		 , ysnSpecialTax	= CONVERT(BIT, 1)
+		 , IDT.ysnTaxExempt
+		 , dblAdjustedTax	= ISNULL(IDT.dblAdjustedTax, 0.00)
+	FROM dbo.tblARInvoiceDetailTax IDT WITH (NOLOCK)
+	INNER JOIN (
+		SELECT intTaxCodeId
+			 , strTaxCode
+		FROM dbo.tblSMTaxCode WITH (NOLOCK)
+	) TAXCODE ON IDT.intTaxCodeId = TAXCODE.intTaxCodeId
+	INNER JOIN (
+		SELECT intTaxClassId
+			 , strTaxClass
+		FROM dbo.tblSMTaxClass WITH (NOLOCK)
+	) TAXCLASS ON IDT.intTaxClassId = TAXCLASS.intTaxClassId
+) TAXDETAILS ON ID.intInvoiceDetailId = TAXDETAILS.intTransactionTid
+INNER JOIN (
+	SELECT intItemId
+		 , intCategoryId
+		 , strItemNo
+	FROM dbo.tblICItem WITH (NOLOCK)
+) ITEM ON ID.intItemId = ITEM.intItemId
+LEFT JOIN (
+	SELECT intCategoryId
+		, strCategoryCode
+	FROM dbo.tblICCategory
+) ITEMCATEGORY ON ITEM.intCategoryId = ITEMCATEGORY.intCategoryId
