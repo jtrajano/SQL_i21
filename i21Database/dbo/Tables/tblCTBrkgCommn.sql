198c1ac56ed4ea17626d750eabc49e5a5478f610
Satya S Sahoo <satya.sahoo@irely.com>
2017-11-23 21:57:59 +0530

    CT-963 - BrkgCommn

---
+++ i21Database/dbo/Tables/tblCTBrkgCommn.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[tblCTBrkgCommn]
+(
+	intBrkgCommnId		   INT NOT NULL    IDENTITY, 
+	strBatchNumber		   NVARCHAR(50)    COLLATE Latin1_General_CI_AS NOT NULL, 
+	dtmBatchDate		   DATETIME	    NOT NULL,
+	dtmPaymentDate		   DATETIME,
+	intCreatedById		   INT,
+	dtmCreated			   DATETIME,
+	intConcurrencyId	   INT		    NOT NULL,
+
+	CONSTRAINT [PK_tblCTBrkgCommn_intBrkgCommnId] PRIMARY KEY CLUSTERED (intBrkgCommnId ASC),
+	CONSTRAINT [UQ_tblCTBrkgCommn_strBatchNumber] UNIQUE (strBatchNumber)
+)
---
+++ i21Database/dbo/Tables/tblCTBrkgCommnDetail.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[tblCTBrkgCommnDetail]
+(
+	intBrkgCommnDetailId	INT NOT NULL IDENTITY, 
+    intBrkgCommnId			INT NOT NULL,
+    intContractCostId		INT NOT NULL,
+    intCreatedById			INT,
+    dtmCreated				DATETIME,
+    intLastModifiedById		INT,
+    dtmLastModified			DATETIME,
+    intConcurrencyId		INT NOT NULL,
+
+	CONSTRAINT [PK_tblCTBrkgCommnDetail_intBrkgCommnDetailId] PRIMARY KEY CLUSTERED (intBrkgCommnDetailId ASC),
+	CONSTRAINT [FK_tblCTBrkgCommnDetail_tblCTBrkgCommn_intBrkgCommnId] FOREIGN KEY (intBrkgCommnId) REFERENCES tblCTBrkgCommn(intBrkgCommnId) ON DELETE CASCADE	
+)
