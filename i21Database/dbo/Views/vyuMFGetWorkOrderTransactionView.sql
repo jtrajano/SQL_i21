34a0ea4c177918b6d879c072087ca01364e06d2f
Premkumar <Prem.Selladurai@gmail.com>
2017-11-24 18:32:45 +0530

    MFG-2918

--- i21Database/dbo/Views/vyuMFGetWorkOrderTransactionView.sql
+++ i21Database/dbo/Views/vyuMFGetWorkOrderTransactionView.sql
@@ -74,6 +74,16 @@ SELECT W.intWorkOrderId
 	,O.intOwnerId 
 	,PL.strParentLotNumber
 	,Convert(nvarchar(50),'') As strSpecialPalletId
+	,NULL AS [strRelatedPalletNo]
+	,NULL AS [strRelatedStorageLocation]
+	,NULL AS [strOldItemNo]
+	,NULL AS [dtmOldExpiryDate]
+	,NULL AS [dtmNewExpiryDate]
+	,NULL AS [strOldPalletStatus]
+	,NULL AS [strNewPalletStatus]
+	,NULL AS [strOldOwnerName]
+	,NULL AS [strNewOwnerName]
+	,NULL AS [strReason]
 FROM dbo.tblMFWorkOrder W
 JOIN dbo.tblMFWorkOrderInputLot WI ON WI.intWorkOrderId = W.intWorkOrderId
 JOIN dbo.tblICItem I ON I.intItemId = W.intItemId
@@ -174,6 +184,16 @@ SELECT W.intWorkOrderId
 	,O.intOwnerId
 	,PL.strParentLotNumber
 	,Convert(nvarchar(50),'') As strSpecialPalletId
+	,NULL AS [strRelatedPalletNo]
+	,NULL AS [strRelatedStorageLocation]
+	,NULL AS [strOldItemNo]
+	,NULL AS [dtmOldExpiryDate]
+	,NULL AS [dtmNewExpiryDate]
+	,NULL AS [strOldPalletStatus]
+	,NULL AS [strNewPalletStatus]
+	,NULL AS [strOldOwnerName]
+	,NULL AS [strNewOwnerName]
+	,NULL AS [strReason]
 FROM dbo.tblMFWorkOrder W
 JOIN dbo.tblMFWorkOrderConsumedLot WI ON WI.intWorkOrderId = W.intWorkOrderId
 Left JOIN dbo.tblMFWorkOrderProducedLot WP ON WP.intBatchId = WI.intBatchId
@@ -273,6 +293,16 @@ SELECT W.intWorkOrderId
 	,O.intOwnerId
 	,PL.strParentLotNumber
 	,Convert(nvarchar(50),'') As strSpecialPalletId
+	,NULL AS [strRelatedPalletNo]
+	,NULL AS [strRelatedStorageLocation]
+	,NULL AS [strOldItemNo]
+	,NULL AS [dtmOldExpiryDate]
+	,NULL AS [dtmNewExpiryDate]
+	,NULL AS [strOldPalletStatus]
+	,NULL AS [strNewPalletStatus]
+	,NULL AS [strOldOwnerName]
+	,NULL AS [strNewOwnerName]
+	,NULL AS [strReason]
 FROM dbo.tblMFWorkOrder W
 JOIN dbo.tblMFWorkOrderInputLot WI ON WI.intWorkOrderId = W.intWorkOrderId
 JOIN dbo.tblICItem I ON I.intItemId = W.intItemId
@@ -373,6 +403,16 @@ SELECT W.intWorkOrderId
 	,O.intOwnerId
 	,PL.strParentLotNumber
 	,L1.strLotNumber AS strSpecialPalletId
+	,NULL AS [strRelatedPalletNo]
+	,NULL AS [strRelatedStorageLocation]
+	,NULL AS [strOldItemNo]
+	,NULL AS [dtmOldExpiryDate]
+	,NULL AS [dtmNewExpiryDate]
+	,NULL AS [strOldPalletStatus]
+	,NULL AS [strNewPalletStatus]
+	,NULL AS [strOldOwnerName]
+	,NULL AS [strNewOwnerName]
+	,NULL AS [strReason]
 FROM dbo.tblMFWorkOrder W
 JOIN dbo.tblMFWorkOrderProducedLot WP ON WP.intWorkOrderId = W.intWorkOrderId
 JOIN dbo.tblICItem I ON I.intItemId = W.intItemId
@@ -474,6 +514,16 @@ SELECT W.intWorkOrderId
 	,O.intOwnerId
 	,PL.strParentLotNumber
 	,L1.strLotNumber AS strSpecialPalletId
+	,NULL AS [strRelatedPalletNo]
+	,NULL AS [strRelatedStorageLocation]
+	,NULL AS [strOldItemNo]
+	,NULL AS [dtmOldExpiryDate]
+	,NULL AS [dtmNewExpiryDate]
+	,NULL AS [strOldPalletStatus]
+	,NULL AS [strNewPalletStatus]
+	,NULL AS [strOldOwnerName]
+	,NULL AS [strNewOwnerName]
+	,NULL AS [strReason]
 FROM dbo.tblMFWorkOrder W
 JOIN dbo.tblMFWorkOrderProducedLot WP ON WP.intWorkOrderId = W.intWorkOrderId
 JOIN dbo.tblICItem I ON I.intItemId = W.intItemId
@@ -498,3 +548,107 @@ JOIN dbo.tblSMUserSecurity US ON US.[intEntityId] = WP.intLastModifiedUserId
 Left JOIN dbo.tblICItemOwner O on O.intItemOwnerId=L.intItemOwnerId
 LEFT JOIN dbo.tblICLot L1 ON L1.intLotId = WP.intSpecialPalletLotId
 WHERE WP.ysnProductionReversed = 1
+UNION
+SELECT NULL As  intWorkOrderId
+	,NULL AS strWorkOrderNo
+	,IA.dtmDate AS [Transaction Date]
+	,NULL AS intTargetItemId
+	,NULL AS strTargetItemType
+	,NULL AS strTargetItemNo
+	,NULL AS strTargetItemDesc
+	,NULL AS strTargetItemShortDesc
+	,NULL As dblQuantity
+	,NULL As intUnitMeasureId
+	,NULL As strUnitMeasure
+	,NULL As strStatusName
+	,NULL AS intManufacturingCellId
+	,NULL AS strCellName
+	,NULL AS dtmPlannedDate
+	,NULL AS intShiftId
+	,NULL AS strPlannedShiftName
+	,NULL AS intManufacturingProcessId
+	,NULL AS strProcessName
+	,I1.intItemId
+	,I1.strType
+	,I1.strItemNo AS [Item No]
+	,I1.strDescription AS [Item Desc]
+	,I1.strShortName AS strShortDesc
+	,IA.dblQty AS dblQuantity
+	,UM.intUnitMeasureId
+	,UM.strUnitMeasure 
+	,dtmBusinessDate AS [Business Date]
+	,S.intShiftId
+	,S.strShiftName AS strBusinessShiftName
+	,SL1.intStorageLocationId 
+	,SL1.strName AS [FROM/To]
+	,NULL As intMachineId
+	,NULL As strMachineName
+	,NULL As strReferenceNo
+	,IA.dtmDate AS dtmLastModified
+	,NULL As intContainerId
+	,NULL As strContainerId
+	,NULL As intContainerTypeId
+	,NULL As strContainerType
+	,IA.dtmDate AS dtmLastModified
+	,US.intEntityId 
+	,US.strUserName 
+	,TT.strName AS TransactionName
+	,NULL AS dblReadingQty
+	,NULL AS dblTotalizerStartReading
+	,NULL AS dblTotalizerEndReading
+	,NULL AS dblTotalizerQty
+	,NULL AS dblPulseStartReading
+	,NULL AS dblPulseEndReading
+	,NULL AS dblPulseQty
+	,IA.strNote As strComment
+	,NULL AS strFormulaUsedForTotalizerReading
+	,NULL AS strFormulaUsedForPulseReading
+	,NULL AS dblMinMoisture
+	,NULL AS dblMoisture
+	,NULL AS dblMaxMoisture
+	,NULL AS dblMinDensity
+	,NULL AS dblDensity
+	,NULL AS dblMaxDensity
+	,NULL AS dblMinColor
+	,NULL AS dblColor
+	,NULL AS dblMaxColor
+	,L1.intLotId
+	,L1.strLotNumber 
+	,L1.strLotAlias 
+	,S.intShiftId As intShiftId
+	,S.strShiftName As strShiftName
+	,IA.dtmDate AS dtmProductionDate
+	,5 AS intSequenceNo
+	,NULL As strBatchId
+	,NULL As intOwnerId
+	,PL.strParentLotNumber 
+	,NULL As strSpecialPalletId
+	,L2.strLotNumber AS [Related Pallet No]
+	,SL2.strName AS [Related Storage Location]
+	,I2.strItemNo AS [Old Item No]
+	,dtmOldExpiryDate AS [Old Expiry Date]
+	,dtmNewExpiryDate AS [New Expiry Date]
+	,LS1.strSecondaryStatus AS [Old Pallet Status]
+	,LS2.strSecondaryStatus AS [New Pallet Status]
+	,E1.strName AS [Old Owner Name]
+	,E2.strName AS [New Owner Name]
+	,IA.strReason AS [Reason]
+FROM tblMFInventoryAdjustment IA
+JOIN tblICItem I1 ON I1.intItemId = IA.intItemId
+JOIN tblICItemUOM IU ON IU.intItemUOMId = IA.intItemUOMId
+JOIN tblICUnitMeasure UM ON UM.intUnitMeasureId = IU.intUnitMeasureId
+JOIN tblICStorageLocation SL1 ON SL1.intStorageLocationId = IA.intStorageLocationId
+JOIN tblICLot L1 ON L1.intLotId = IA.intSourceLotId
+JOIN tblICParentLot PL On PL.intParentLotId =L1.intParentLotId 
+LEFT JOIN tblICLot L2 ON L2.intLotId = IA.intDestinationLotId
+LEFT JOIN tblICStorageLocation SL2 ON SL2.intStorageLocationId = IA.intDestinationStorageLocationId
+JOIN tblICInventoryTransactionType TT ON TT.intTransactionTypeId = IA.intTransactionTypeId
+LEFT JOIN tblICItem I2 ON I2.intItemId = IA.intOldItemId
+LEFT JOIN tblICLotStatus LS1 ON LS1.intLotStatusId = IA.intOldLotStatusId
+LEFT JOIN tblICLotStatus LS2 ON LS2.intLotStatusId = IA.intNewLotStatusId
+JOIN tblSMUserSecurity US ON US.intEntityId = IA.intUserId
+LEFT JOIN tblMFShift S ON S.intShiftId = IA.intBusinessShiftId
+LEFT JOIN tblICItemOwner IO1 ON IO1.intItemOwnerId = IA.intOldItemOwnerId
+LEFT JOIN tblEMEntity E1 ON E1.intEntityId = IO1.intOwnerId
+LEFT JOIN tblICItemOwner IO2 ON IO2.intItemOwnerId = IA.intNewItemOwnerId
+LEFT JOIN tblEMEntity E2 ON E2.intEntityId = IO2.intOwnerId
