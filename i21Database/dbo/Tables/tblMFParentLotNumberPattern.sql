fd7c693c4dcbae9b7ae6c50a83a391b789661197
Premkumar <Prem.Selladurai@gmail.com>
2017-11-27 18:45:37 +0530

    MFG-3043

---
+++ i21Database/dbo/Tables/tblMFParentLotNumberPattern.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE tblMFParentLotNumberPattern (
+	intParentLotNumberPattern INT identity(1, 1) CONSTRAINT [PK_tblMFParentLotNumberPattern_intParentLotNumberPattern] PRIMARY KEY (intParentLotNumberPattern)
+	,intInventoryReceiptItemId INT
+	,strPatternString NVARCHAR(50) COLLATE Latin1_General_CI_AS
+	)
