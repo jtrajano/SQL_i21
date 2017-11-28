f2cac77ff56d4576dfa06e2052b6a9ddc9d3c871
Satya S Sahoo <satya.sahoo@irely.com>
2017-11-28 22:22:00 +0530

    CT-963 - BrkgCmmn

--- i21Database/dbo/Stored Procedures/uspCTGetBrokerageCommission.sql
+++ i21Database/dbo/Stored Procedures/uspCTGetBrokerageCommission.sql
@@ -5,10 +5,37 @@
     @ysnSummary				BIT			= 0
 AS 
 
-		SELECT  CST.intContractCostId,
+	IF @strStatus = 'All' SET @strStatus = NULL
+
+	IF OBJECT_ID('tempdb..#Status') IS NOT NULL  					
+		DROP TABLE #Status
+	
+	SELECT strCurrency,strStatus COLLATE Latin1_General_CI_AS strStatus INTO #Status
+	FROM(
+		SELECT 'Not yet due' AS strStatus UNION ALL
+		SELECT 'Due' AS strStatus UNION ALL
+		SELECT 'Requested' AS strStatus UNION ALL
+		SELECT 'Received/Paid' AS strStatus
+	)t,tblSMCurrency
+
+	IF OBJECT_ID('tempdb..#BrkgDetail') IS NOT NULL  					
+		DROP TABLE #BrkgDetail					
+
+	SELECT	*	INTO #BrkgDetail FROM
+	(
+		SELECT  CST.intContractCostId AS intBrkgCommnDetailId,
+				CST.intContractCostId,
 				CST.dblReqstdAmount,
 				CST.dblRcvdPaidAmount,
-				CST.strStatus,
+				ISNULL(CST.strStatus,
+				CASE WHEN CST.dtmDueDate <= DATEADD(d, 0, DATEDIFF(d, 0, GETDATE())) THEN 'Due' ELSE 'Not yet due' END) AS strStatus,
+				CST.dtmDueDate,
+				CST.strCurrency,
+				CST.ysnReceivable,
+				CST.dblRate,
+				CST.intUnitMeasureId AS intCostUOMId,
+				CST.intItemId,
+
 				dbo.fnRemoveTrailingZeroes(CST.dblRate) + ' ' +CST.strCurrency + '/' + CST.strUOM AS strRateUnit,
 	   
 				SEQ.strSequenceNumber,
@@ -19,19 +46,67 @@ AS
 				SEQ.dtmEndDate,
 				SEQ.dblQuantity,
 				SEQ.strItemUOM,
+				SEQ.intUnitMeasureId	AS	intQtyUOMId,
 
 				HDR.strCustomerContract AS	 strBuyerRef,
 				HDR.strCPContract		AS	 strSellerRef,
 	   
-				SEY.strName				AS	 strSeller
+				SEY.strName				AS	 strSeller,
+
+				dbo.fnCTConvertQuantityToTargetItemUOM(SEQ.intItemId,SEQ.intUnitMeasureId,CST.intUnitMeasureId,SEQ.dblQuantity)*CST.dblRate AS dblEstimatedAmount,
+				dblNet
 
 		FROM	vyuCTContractCostView	CST
 		JOIN	vyuCTContractSequence	SEQ	ON	SEQ.intContractDetailId =   CST.intContractDetailId
+											AND CST.intVendorId			=   ISNULL(@intEntityId,CST.intVendorId)
+											AND	SEQ.intContractHeaderId	=	ISNULL(@intContractHeaderId,SEQ.intContractHeaderId)
 		JOIN	tblCTContractHeader		HDR ON	HDR.intContractHeaderId =   CST.intContractHeaderId
 		JOIN	tblEMEntity				SEY ON	SEY.intEntityId			=   HDR.intCounterPartyId
 		JOIN	tblICItem				CIM	ON	CIM.intItemId		    =   CST.intItemId 
 											AND	CIM.strCostType			=   'Commission'
+		LEFT	JOIN
+		(
+				SELECT  intPContractDetailId,
+						SUM(dbo.fnCTConvertQtyToTargetItemUOM(LD.intWeightItemUOMId,CD.intItemUOMId,dblNet)) dblNet
+	    
+				FROM	tblLGLoadDetail		LD 
+				JOIN	tblLGLoad			LO  ON	 LO.intLoadId			=   LD.intLoadId
+				JOIN	tblCTContractDetail CD  ON	 CD.intContractDetailId	=   LD.intPContractDetailId
+				WHERE   LO.intShipmentStatus <> 10
+				AND	    LO.ysnPosted = 1 
+				AND	    LO.intPurchaseSale  = 1
+				GROUP BY intPContractDetailId
+		)								LDL ON	LDL.intPContractDetailId =	SEQ.intContractDetailId
+	)t
+	WHERE	ISNULL(strStatus,'')	=   ISNULL(@strStatus,ISNULL(strStatus,''))
 
-		WHERE	CST.intVendorId				=   ISNULL(@intEntityId,CST.intVendorId)
-		AND		ISNULL(CST.strStatus,'')	=   ISNULL(@strStatus,ISNULL(CST.strStatus,''))
-		AND		HDR.intContractHeaderId		=	ISNULL(@intContractHeaderId,HDR.intContractHeaderId)
+	IF @ysnSummary = 0
+	BEGIN
+		SELECT * FROM #BrkgDetail
+	END
+	ELSE
+	BEGIN
+		SELECT	CAST(ROW_NUMBER() OVER (ORDER BY strCurrency ASC) AS INT) AS intBrkgCommnDetailId,
+				*,
+				dblRecEstimated - dblPayEstimated AS dblNetEstimated,
+				dblRecActual - dblPayActual AS dblNetActual
+		FROM
+		(
+			SELECT	S.strCurrency,
+					S.strStatus,
+					SUM(CASE WHEN ysnReceivable = 1 THEN dblRate ELSE 0 END) dblRecEstimated, 
+					SUM(CASE WHEN ysnReceivable = 1 THEN 0 ELSE dblRate END) dblPayEstimated,
+					SUM(CASE	WHEN	S.strStatus IN ('Not yet due', 'Due') OR ysnReceivable <> 1
+							THEN	0 
+							ELSE	dbo.fnCTConvertQuantityToTargetItemUOM(intItemId,intQtyUOMId,intCostUOMId,dblNet)*dblRate 
+					END) AS dblRecActual,
+					SUM(CASE	WHEN	S.strStatus IN ('Not yet due', 'Due') OR ysnReceivable = 1
+							THEN	0 
+							ELSE	dbo.fnCTConvertQuantityToTargetItemUOM(intItemId,intQtyUOMId,intCostUOMId,dblNet)*dblRate 
+					END) AS dblPayActual
+			FROM #Status S
+			LEFT JOIN #BrkgDetail B ON B.strStatus = S.strStatus AND B.strCurrency = S.strCurrency
+			GROUP BY S.strStatus,S.strCurrency
+		)t
+		WHERE strCurrency IN (SELECT DISTINCT  strCurrency FROM #BrkgDetail)
+	END
