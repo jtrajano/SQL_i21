46987650bba688c2eb30f7529250956a603afeba
IRELY\kkarthick <karthick.k@irely.com>
2017-11-30 11:54:02 +0530

    AR-5782,AR-5783,AR-5788,AR-5789,AR-5791

--- Integration/dbo/Stored Procedures/uspARImportInvoice.sql
+++ Integration/dbo/Stored Procedures/uspARImportInvoice.sql
@@ -1,4 +1,4 @@
-﻿IF EXISTS(select top 1 1 from sys.procedures where name = 'uspARImportInvoice')
+IF EXISTS(select top 1 1 from sys.procedures where name = 'uspARImportInvoice')
 	DROP PROCEDURE uspARImportInvoice
 GO
 
@@ -150,8 +150,8 @@ BEGIN
 						THEN 'Cash'
 					WHEN agivc_type = 'R' 
 						THEN 'Cash Refund' 
-					WHEN agivc_type = 'X' 
-						THEN 'Transfer'
+					--WHEN agivc_type = 'X' 
+					--	THEN 'Transfer'
 				 END),--[strTransactionType]
 				NULL, --agivc_pay_type [intPaymentMethodId]
 				agivc_comment, --[strComments]
@@ -233,7 +233,7 @@ BEGIN
 				(CASE WHEN ISDATE(ptivc_rev_dt) = 1 THEN CONVERT(DATE, CAST(ptivc_rev_dt AS CHAR(12)), 112) ELSE GETDATE() END),--[dtmPostDate]
 				(select intDefaultCurrencyId from tblSMCompanyPreference),--[intCurrencyId]
 				(SELECT intCompanyLocationId FROM tblSMCompanyLocation WHERE strLocationNumber  COLLATE Latin1_General_CI_AS = ptivc_loc_no COLLATE Latin1_General_CI_AS),--[intCompanyLocationId]
-				Salesperson.intEntityId,--[intEntitySalespersonId]
+				ISNULL((SELECT intEntityId FROM tblARSalesperson Salesperson WHERE strSalespersonId COLLATE Latin1_General_CI_AS = ptivc_sold_by COLLATE Latin1_General_CI_AS),Cus.intSalespersonId),--[intEntitySalespersonId],--[intEntitySalespersonId]
 				NULL, -- [dtmShipDate]
 				NULL, --to do [intShipViaId]
 				ptivc_po_no, --[strPONumber]
@@ -244,7 +244,13 @@ BEGIN
 				ROUND(ISNULL(ptivc_sold_by_tot, @ZeroDecimal), [dbo].[fnARGetDefaultDecimal]()),--[dblInvoiceTotal]
 				ROUND(ISNULL(ptivc_disc_amt, @ZeroDecimal), [dbo].[fnARGetDefaultDecimal]()),--[dblDiscount]
 				ROUND(ISNULL(ptivc_bal_due, @ZeroDecimal), [dbo].[fnARGetDefaultDecimal]()),--[dblAmountDue]
-				ROUND(ISNULL(ptivc_amt_applied, @ZeroDecimal), [dbo].[fnARGetDefaultDecimal]()),--[dblPayment]
+				Case ptivc_type when 'C' then 
+					(select ROUND(ISNULL(ptcrd_amt_used, @ZeroDecimal), [dbo].[fnARGetDefaultDecimal]()) from ptcrdmst 
+					where ptcrd_type = 'C' and ptivc_cus_no = ptcrd_cus_no and ptivc_invc_no = ptcrd_invc_no and ptcrd_rev_dt = ptivc_rev_dt and ptcrd_loc_no = ptivc_loc_no
+					and ptcrd_amt = ptivc_sold_by_tot)
+				else
+					ROUND(ISNULL(ptivc_amt_applied, @ZeroDecimal), [dbo].[fnARGetDefaultDecimal]())
+				end,--[dblPayment]
 				ROUND(ISNULL(ptivc_serv_chg, @ZeroDecimal), [dbo].[fnARGetDefaultDecimal]()),--[dblInterest]				
 				(CASE 
 					WHEN ptivc_type = 'I' 
@@ -257,8 +263,8 @@ BEGIN
 						THEN 'Cash'
 					WHEN ptivc_type = 'R' 
 						THEN 'Cash Refund' 
-					WHEN ptivc_type = 'X' 
-						THEN 'Transfer'
+					--WHEN ptivc_type = 'X' 
+					--	THEN 'Transfer'
 				 END),--[strTransactionType]
 				NULL, --ptivc_pay_type [intPaymentMethodId]
 				ptivc_comment, --[strComments]
@@ -268,15 +274,15 @@ BEGIN
 				1,
 				1,
 				@EntityId,
-				LTRIM(RTRIM(ptivc_invc_no)) + LTRIM(RTRIM(ptivc_sold_to))		
+				LTRIM(RTRIM(ptivc_invc_no)) + LTRIM(RTRIM(ptivc_cus_no))		
 			FROM ptivcmst
 			LEFT JOIN tblARInvoice Inv 
 				ON ptivcmst.ptivc_invc_no COLLATE Latin1_General_CI_AS = Inv.strInvoiceOriginId COLLATE Latin1_General_CI_AS
 				AND Inv.[dtmDate] = CONVERT(DATE, CAST(ptivc_rev_dt AS CHAR(12)), 112)
 				AND Inv.[dblInvoiceTotal] = ROUND(ISNULL(ptivc_sold_by_tot, 0), [dbo].[fnARGetDefaultDecimal]())
 				AND ISNULL(Inv.[ysnImportedFromOrigin],0) = 1 AND ISNULL(Inv.[ysnImportedAsPosted],0) = 1
-			INNER JOIN tblARCustomer Cus ON  strCustomerNumber COLLATE Latin1_General_CI_AS = ptivc_sold_to COLLATE Latin1_General_CI_AS
-			INNER JOIN tblARSalesperson Salesperson ON strSalespersonId COLLATE Latin1_General_CI_AS = ptivc_sold_by COLLATE Latin1_General_CI_AS
+			INNER JOIN tblARCustomer Cus ON  strCustomerNumber COLLATE Latin1_General_CI_AS = ptivc_cus_no COLLATE Latin1_General_CI_AS
+			--INNER JOIN tblARSalesperson Salesperson ON strSalespersonId COLLATE Latin1_General_CI_AS = ptivc_sold_by COLLATE Latin1_General_CI_AS
 			LEFT JOIN tblSMTerm Term ON Term.strTermCode COLLATE Latin1_General_CI_AS = CONVERT(NVARCHAR(10),CONVERT(INT,ptivc_terms_code)) COLLATE Latin1_General_CI_AS
             --LEFT JOIN tblSMCurrency Cur ON Cur.strCurrency COLLATE Latin1_General_CI_AS = ptivc_currency COLLATE Latin1_General_CI_AS			
 			WHERE 
@@ -291,7 +297,111 @@ BEGIN
 			UPDATE  IVC SET ysnPaid = 0 FROM tblARInvoice IVC
 			INNER JOIN ptcrdmst CRD ON CRD.ptcrd_invc_no COLLATE SQL_Latin1_General_CP1_CS_AS = IVC.strInvoiceOriginId COLLATE SQL_Latin1_General_CP1_CS_AS
 			INNER JOIN tblARCustomer CUS ON CUS.strCustomerNumber COLLATE SQL_Latin1_General_CP1_CS_AS = CRD.ptcrd_cus_no COLLATE SQL_Latin1_General_CP1_CS_AS
-			WHERE intInvoiceId > @maxInvoiceId AND strTransactionType = 'Credit Memo' AND (CRD.ptcrd_amt - CRD.ptcrd_amt_used) > 0				
+			WHERE intInvoiceId > @maxInvoiceId AND strTransactionType = 'Credit Memo' AND (CRD.ptcrd_amt - CRD.ptcrd_amt_used) > 0	
+			
+			--IMPORT PREPAYMENT INVOICES FOR THE PREPAID/REGULAR CREDITS
+			INSERT INTO [dbo].[tblARInvoice]
+			   ([strInvoiceOriginId]
+			   ,[intEntityCustomerId]
+			   ,[dtmDate]
+			   ,[dtmDueDate]
+			   ,[dtmPostDate]
+			   ,[intCurrencyId]
+			   ,[intCompanyLocationId]
+			   ,[intEntitySalespersonId]
+			   ,[dtmShipDate]
+			   ,[intShipViaId]
+			   ,[strPONumber]
+			   ,[intTermId]
+			   ,[dblInvoiceSubtotal]
+			   ,[dblShipping]
+			   ,[dblTax]
+			   ,[dblInvoiceTotal]
+			   ,[dblAmountDue]
+			   ,[dblPayment]
+			   ,[strTransactionType]
+			   ,[intPaymentMethodId]
+			   ,[strComments]
+			   ,[intAccountId]
+			   ,[ysnPosted]
+			   ,[ysnPaid]
+			   ,[ysnImportedFromOrigin]
+			   ,[ysnImportedAsPosted]
+			   ,[intEntityId]
+			   ,[strShipToAddress] --just for insertion of identity field from origin in format LTRIM(RTRIM(agivc_ivc_no)) + LTRIM(RTRIM(agivc_bill_to_cus))
+			   )
+		SELECT
+				 LTRIM(RTRIM(ptcrd_invc_no))+'_'+CONVERT(CHAR(3),ptcrd_seq_no),--[strInvoiceOriginId]		
+				Cus.intEntityId,--[intEntityCustomerId]		
+				(CASE WHEN ISDATE(ptcrd_rev_dt) = 1 THEN CONVERT(DATE, CAST(ptcrd_rev_dt AS CHAR(12)), 112) ELSE GETDATE() END),--[dtmDate]
+				(CASE WHEN ISDATE(ptcrd_rev_dt) = 1 THEN CONVERT(DATE, CAST(ptcrd_rev_dt AS CHAR(12)), 112) ELSE GETDATE() END),--[dtmDueDate]
+				(CASE WHEN ISDATE(ptcrd_rev_dt) = 1 THEN CONVERT(DATE, CAST(ptcrd_rev_dt AS CHAR(12)), 112) ELSE GETDATE() END),--[dtmPostDate]
+				(select intDefaultCurrencyId from tblSMCompanyPreference),--[intCurrencyId]
+				LOC.intCompanyLocationId,--[intCompanyLocationId]
+				Cus.intSalespersonId,--[intEntitySalespersonId]
+				NULL, -- [dtmShipDate]
+				NULL, --to do [intShipViaId]
+				null, --[strPONumber]
+				1,-- [intTermId]
+				0,--[dblInvoiceSubtotal]
+				0,--[dblShipping]
+				0,--[dblTax]
+				ROUND(ISNULL(ptcrd_amt, @ZeroDecimal), [dbo].[fnARGetDefaultDecimal]()),--[dblInvoiceTotal]
+				--CASE WHEN CRD.ptcrd_cred_ind = 'P' AND (SELECT COUNT (ptcrd_amt) FROM ptcrdmst WHERE ptcrd_cus_no = CRD.ptcrd_cus_no AND ptcrd_invc_no = CRD.ptcrd_invc_no AND ptcrd_note = 'xfer PPD to REG')>0 
+				--THEN CRD.ptcrd_amt - (SELECT ptcrd_amt FROM ptcrdmst WHERE ptcrd_cus_no = CRD.ptcrd_cus_no AND ptcrd_invc_no = CRD.ptcrd_invc_no AND ptcrd_note = 'xfer PPD to REG') ELSE ptcrd_amt END,--[dblInvoiceTotal]
+				ROUND(ISNULL((ptcrd_amt-ptcrd_amt_used), @ZeroDecimal), [dbo].[fnARGetDefaultDecimal]()),--[dblAmountDue]
+				--CASE WHEN CRD.ptcrd_cred_ind = 'P' AND (SELECT COUNT (ptcrd_amt) FROM ptcrdmst WHERE ptcrd_cus_no = CRD.ptcrd_cus_no AND ptcrd_invc_no = CRD.ptcrd_invc_no AND ptcrd_note = 'xfer PPD to REG')>0 
+				--THEN CRD.ptcrd_amt - (SELECT ptcrd_amt FROM ptcrdmst WHERE ptcrd_cus_no = CRD.ptcrd_cus_no AND ptcrd_invc_no = CRD.ptcrd_invc_no AND ptcrd_note = 'xfer PPD to REG') ELSE ptcrd_amt_used END,--[dblPayment]			
+				ROUND(ISNULL(ptcrd_amt, @ZeroDecimal), [dbo].[fnARGetDefaultDecimal]()),--[dblPayment]
+				'Customer Prepayment',--[strTransactionType]
+				PM.intPaymentMethodID, --ptivc_pay_type [intPaymentMethodId]
+				ptcrd_note, --[strComments]
+				[dbo].[fnARGetInvoiceTypeAccount]('Customer Prepayment', LOC.intCompanyLocationId), --to do [intAccountId]
+				1, --"If Invoice exists in the ptivcmst, that means it is posted" -Joe [ysnPosted]
+				(CASE WHEN (ptcrd_amt-ptcrd_amt_used) = 0 THEN 1 ELSE 0 END),--"If the ptivc-bal-due equals zero, then it is paid." -Joe [ysnPaid]
+				1,
+				1,
+				@EntityId,
+				LTRIM(RTRIM(ptcrd_invc_no COLLATE Latin1_General_CI_AS))+LTRIM(RTRIM(ptcrd_cus_no COLLATE Latin1_General_CI_AS))
+				+LTRIM(RTRIM(CONVERT(CHAR(10),ptcrd_rev_dt)))+LTRIM(RTRIM(CONVERT(CHAR(3),ptcrd_seq_no)))			
+			FROM ptcrdmst CRD
+			INNER JOIN tblARCustomer Cus ON  strCustomerNumber COLLATE Latin1_General_CI_AS = ptcrd_cus_no COLLATE Latin1_General_CI_AS
+			LEFT JOIN tblARInvoice Inv 
+				ON LTRIM(RTRIM(CRD.ptcrd_invc_no))+'_'+CONVERT(CHAR(3),CRD.ptcrd_seq_no) COLLATE Latin1_General_CI_AS = Inv.strInvoiceOriginId COLLATE Latin1_General_CI_AS
+				AND Inv.[dtmDate] = CONVERT(DATE, CAST(CRD.ptcrd_rev_dt AS CHAR(12)), 112)
+				--AND Inv.[dblInvoiceTotal] = ROUND(ISNULL(ptcrd_amt, 0), [dbo].[fnARGetDefaultDecimal]())
+				AND ISNULL(Inv.[ysnImportedFromOrigin],0) = 1 AND ISNULL(Inv.[ysnImportedAsPosted],0) = 1 AND Inv.strTransactionType = 'Customer Prepayment'
+			INNER JOIN tblSMCompanyLocation LOC ON strLocationNumber  COLLATE Latin1_General_CI_AS = CRD.ptcrd_loc_no COLLATE Latin1_General_CI_AS
+			LEFT OUTER JOIN [tblSMPaymentMethod] PM	ON CRD.ptcrd_pay_type COLLATE Latin1_General_CI_AS = PM.strPaymentMethodCode COLLATE Latin1_General_CI_AS
+			WHERE Inv.strInvoiceNumber IS NULL AND  CRD.ptcrd_type IN ( 'P','U') --AND ISNULL(CRD.ptcrd_note,'') <> 'xfer PPD to REG'
+
+		-- UPDATE PREPAYMENT INVOICE TOTALS FOR THE PREPAID/REGULAR CREDITS - XREF FROM PPD TO REG
+				DECLARE @ptcrd_cus_no  AS VARCHAR (10), @ptcrd_invc_no   AS VARCHAR (6), @ptcrd_amt AS NUMERIC (18,2), @ptcrd_loc_no as CHAR(3) , @ptcrd_rev_dt AS INT
+				DECLARE ppd_cursor CURSOR
+				FOR
+					SELECT  ptcrd_cus_no, ptcrd_invc_no, ptcrd_amt  FROM ptcrdmst WHERE ptcrd_cus_no = ptcrd_cus_no AND ptcrd_invc_no = ptcrd_invc_no AND ptcrd_note = 'xfer PPD to REG'
+				OPEN ppd_cursor
+				FETCH NEXT
+				FROM ppd_cursor
+				INTO  @ptcrd_cus_no, @ptcrd_invc_no, @ptcrd_amt
+				WHILE @@FETCH_STATUS = 0
+				BEGIN
+					UPDATE I SET I.dblInvoiceTotal = I.dblInvoiceTotal - @ptcrd_amt,I.dblPayment = I.dblPayment - @ptcrd_amt  FROM tblARInvoice I
+					JOIN (
+							SELECT TOP (1) I.intInvoiceId--,I.dblInvoiceTotal,*--,c.ptcrd_cus_no,c.ptcrd_invc_no, I.strComments
+							FROM ptcrdmst c
+							JOIN tblARCustomer cr on cr.strCustomerNumber = c.ptcrd_cus_no collate SQL_Latin1_General_CP1_CS_AS
+							JOIN tblARInvoice I on cr.intEntityId = I.intEntityCustomerId and I.strInvoiceOriginId = LTRIM(RTRIM(c.ptcrd_invc_no))+'_'+CONVERT(CHAR(3),c.ptcrd_seq_no) collate SQL_Latin1_General_CP1_CS_AS
+							WHERE c.ptcrd_cus_no = @ptcrd_cus_no  AND c.ptcrd_invc_no = @ptcrd_invc_no AND 	c.ptcrd_note <> 'xfer PPD to REG' AND I.strComments <> 'xfer PPD to REG'
+							ORDER BY I.dtmDate desc ) AS PPDI 
+							on I.intInvoiceId = PPDI.intInvoiceId			
+				FETCH NEXT
+				FROM ppd_cursor
+				INTO @ptcrd_cus_no, @ptcrd_invc_no, @ptcrd_amt			
+				END	
+				CLOSE ppd_cursor
+				DEALLOCATE ppd_cursor
+			
 		 End
 
 			
@@ -439,9 +549,9 @@ BEGIN
 				ITM.intItemId,
 				ITM.strDescription,
 				NULL,
-				ptstm_un,
+				CASE WHEN INV.strTransactionType IN ('Credit Memo', 'Cash Refund') THEN ptstm_un * -1 ELSE ptstm_un END,
 				ptstm_un_prc,
-				ptstm_net
+				CASE WHEN INV.strTransactionType IN ('Credit Memo', 'Cash Refund') THEN ptstm_net * -1 ELSE ptstm_net END 
 			FROM ptstmmst
 			INNER JOIN tblARInvoice INV ON INV.strShipToAddress COLLATE Latin1_General_CI_AS = LTRIM(RTRIM(ptstm_ivc_no COLLATE Latin1_General_CI_AS)) + LTRIM(RTRIM(ptstm_bill_to_cus COLLATE Latin1_General_CI_AS))
 			INNER JOIN tblICItem ITM ON ITM.strItemNo COLLATE Latin1_General_CI_AS = RTRIM(ptstm_itm_no  COLLATE Latin1_General_CI_AS)
@@ -463,9 +573,9 @@ BEGIN
 				ITM.intItemId,
 				ITM.strDescription,
 				NULL,
-				ptstm_un,
+				CASE WHEN INV.strTransactionType IN ('Credit Memo', 'Cash Refund') THEN ptstm_un * -1 ELSE ptstm_un END,
 				0,
-				ptstm_set_amt
+				CASE WHEN INV.strTransactionType IN ('Credit Memo', 'Cash Refund') THEN ptstm_set_amt * -1 ELSE ptstm_set_amt END 
 			FROM ptstmmst
 			INNER JOIN tblARInvoice INV ON INV.strShipToAddress COLLATE Latin1_General_CI_AS = LTRIM(RTRIM(ptstm_ivc_no COLLATE Latin1_General_CI_AS)) + LTRIM(RTRIM(ptstm_bill_to_cus COLLATE Latin1_General_CI_AS))
 			INNER JOIN tblICItem ITM ON ITM.strItemNo = 'SET'-- COLLATE Latin1_General_CI_AS = RTRIM(ptstm_itm_no  COLLATE Latin1_General_CI_AS)
@@ -485,9 +595,9 @@ BEGIN
 				ITM.intItemId,
 				ITM.strDescription,
 				NULL,
-				ptstm_un,
+				CASE WHEN INV.strTransactionType IN ('Credit Memo', 'Cash Refund') THEN ptstm_un * -1 ELSE ptstm_un END,
 				0,
-				ptstm_fet_amt
+				CASE WHEN INV.strTransactionType IN ('Credit Memo', 'Cash Refund') THEN ptstm_fet_amt * -1 ELSE ptstm_fet_amt END 
 			FROM ptstmmst
 			INNER JOIN tblARInvoice INV ON INV.strShipToAddress COLLATE Latin1_General_CI_AS = LTRIM(RTRIM(ptstm_ivc_no COLLATE Latin1_General_CI_AS)) + LTRIM(RTRIM(ptstm_bill_to_cus COLLATE Latin1_General_CI_AS))
 			INNER JOIN tblICItem ITM ON ITM.strItemNo = 'FET'-- COLLATE Latin1_General_CI_AS = RTRIM(ptstm_itm_no  COLLATE Latin1_General_CI_AS)
@@ -507,9 +617,9 @@ BEGIN
 				ITM.intItemId,
 				ITM.strDescription,
 				NULL,
-				ptstm_un,
+				CASE WHEN INV.strTransactionType IN ('Credit Memo', 'Cash Refund') THEN ptstm_un * -1 ELSE ptstm_un END,
 				0,
-				ptstm_sst_amt
+				CASE WHEN INV.strTransactionType IN ('Credit Memo', 'Cash Refund') THEN ptstm_sst_amt * -1 ELSE ptstm_sst_amt END 
 			FROM ptstmmst
 			INNER JOIN tblARInvoice INV ON INV.strShipToAddress COLLATE Latin1_General_CI_AS = LTRIM(RTRIM(ptstm_ivc_no COLLATE Latin1_General_CI_AS)) + LTRIM(RTRIM(ptstm_bill_to_cus COLLATE Latin1_General_CI_AS))
 			INNER JOIN tblICItem ITM ON ITM.strItemNo = 'SST'-- COLLATE Latin1_General_CI_AS = RTRIM(ptstm_itm_no  COLLATE Latin1_General_CI_AS)
@@ -532,9 +642,9 @@ BEGIN
 						ITM.intItemId,
 						ITM.strDescription,
 						NULL,
-						ptstm_un,
+						CASE WHEN INV.strTransactionType IN (''Credit Memo'', ''Cash Refund'') THEN ptstm_un * -1 ELSE ptstm_un END,
 						0,
-						ptstm_lc'+CAST(@cnt AS NVARCHAR)+'_amt
+						CASE WHEN INV.strTransactionType IN (''Credit Memo'', ''Cash Refund'') THEN ptstm_lc'+CAST(@cnt AS NVARCHAR)+'_amt * -1 ELSE ptstm_lc'+CAST(@cnt AS NVARCHAR)+'_amt END 
 					FROM ptstmmst
 					INNER JOIN tblARInvoice INV ON INV.strShipToAddress COLLATE Latin1_General_CI_AS = LTRIM(RTRIM(ptstm_ivc_no COLLATE Latin1_General_CI_AS)) + LTRIM(RTRIM(ptstm_bill_to_cus COLLATE Latin1_General_CI_AS))
 					INNER JOIN tblICItem ITM ON ITM.strItemNo = ''LC'+CAST(@cnt AS NVARCHAR)+'-''+SUBSTRING (ptstm_tax_key ,11 , 2 ) COLLATE Latin1_General_CI_AS
@@ -543,8 +653,51 @@ BEGIN
 					   EXEC (@SQLCMD)
 
 					   SET @cnt = @cnt + 1;
-					END			
+					END	
+					
+			--IMPORT DEBIT MEMO DETAILS	
+			INSERT INTO [dbo].[tblARInvoiceDetail]
+			   ([intInvoiceId]
+			   ,[intItemId]
+			   ,[strItemDescription]
+			   ,[dblQtyOrdered]
+			   ,[dblQtyShipped]
+			   ,[dblPrice]
+			   ,[dblTotal])			
+			SELECT 
+				INV.intInvoiceId,
+				NULL,
+				ptivc_comment,
+				NULL,
+				1,
+				ptivc_sold_by_tot,
+				ptivc_sold_by_tot
+			FROM ptivcmst
+			INNER JOIN tblARInvoice INV ON INV.strShipToAddress COLLATE Latin1_General_CI_AS = LTRIM(RTRIM(ptivc_invc_no COLLATE Latin1_General_CI_AS)) + LTRIM(RTRIM(ptivc_cus_no COLLATE Latin1_General_CI_AS))
+			WHERE  ptivc_type = 'D'
 
+			--IMPORT PREPAID/REGULAR CREDIT DETAILS	
+			INSERT INTO [dbo].[tblARInvoiceDetail]
+			   ([intInvoiceId]
+			   ,[intItemId]
+			   ,[strItemDescription]
+			   ,[dblQtyOrdered]
+			   ,[dblQtyShipped]
+			   ,[dblPrice]
+			   ,[dblTotal])		
+			SELECT 
+				INV.intInvoiceId,
+				NULL,
+				ptcrd_note,
+				NULL,
+				1,
+				INV.dblInvoiceTotal, 
+				INV.dblInvoiceTotal 
+			FROM ptcrdmst 
+			INNER JOIN tblARInvoice INV ON INV.strShipToAddress COLLATE Latin1_General_CI_AS = LTRIM(RTRIM(ptcrd_invc_no COLLATE Latin1_General_CI_AS)) 
+			+ LTRIM(RTRIM(ptcrd_cus_no COLLATE Latin1_General_CI_AS))+LTRIM(RTRIM(CONVERT(CHAR(10),ptcrd_rev_dt)))+LTRIM(RTRIM(CONVERT(CHAR(3),ptcrd_seq_no)))			
+			WHERE ptcrd_type  in ('P','U') 
+									
 		 end
 
 		 			
