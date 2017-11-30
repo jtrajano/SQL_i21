dcfa721cd7a34e5fa4e12550eed4ee8f72b1c76b
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2017-11-30 16:05:35 +0530

    RM-971

--- i21Database/dbo/Tables/tblCTContractDetail.sql
+++ i21Database/dbo/Tables/tblCTContractDetail.sql
@@ -134,7 +134,7 @@ CREATE TABLE [dbo].[tblCTContractDetail]
     [dblReservedQty] NUMERIC(18, 6) NULL, 
     [dblAllocationAdjQty] NUMERIC(18, 6) NULL, 
 	ysnPriceChanged BIT,
-
+	[dblInvoicedQty] NUMERIC(18, 6) NULL, 
     CONSTRAINT [PK_tblCTContractDetail_intContractDetailId] PRIMARY KEY CLUSTERED ([intContractDetailId] ASC),
 	CONSTRAINT [UQ_tblCTContractDetail_intContractHeaderId_intContractSeq] UNIQUE ([intContractHeaderId],[intContractSeq]), 
 
