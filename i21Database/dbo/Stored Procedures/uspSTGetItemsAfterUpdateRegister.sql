86b2b1380ee898899a1b10616640ae31b62b5c37
Henry Zaragoza Jr <henry.zaragoza@irely.com>
2017-12-02 04:08:45 +0800

    ST-536 Update Register should show what is being sent to the register

--- i21Database/dbo/Stored Procedures/uspSTGetItemsAfterUpdateRegister.sql
+++ i21Database/dbo/Stored Procedures/uspSTGetItemsAfterUpdateRegister.sql
@@ -29,25 +29,21 @@ BEGIN TRY
 									, ysnIdRequiredLiquor BIT
 									, ysnIdRequiredCigarette BIT
 									, strRegProdCode NVARCHAR(200)
+									, intItemId INT
 								)
+	
+	DECLARE @tablePricebookFileOne TABLE(intItemId int, strActionType nvarchar(20), dtmDate DATETIME)
+	--DECLARE @tablePricebookFileTwo TABLE(intItemId int, strActionType nvarchar(20), dtmDate DATETIME)
 
-
-	--PricebookFile @StoreId @Register, @Category, @BeginingChangeDate, @EndingChangeDate, @ExportEntirePricebookFile
-	IF(@ysnPricebookFile = 1)
-		BEGIN
-
-			DECLARE @tablePricebookFileOne TABLE(intItemId int, strActionType nvarchar(20), dtmDate DATETIME)
-			DECLARE @tablePricebookFileTwo TABLE(intItemId int, strActionType nvarchar(20), dtmDate DATETIME)
-
-			--Insert to table1
-			INSERT INTO @tablePricebookFileOne
-			SELECT
-				DISTINCT CAST(strRecordNo as int) [intItemId]
-				, strActionType
-				, dtmDate
-			FROM dbo.tblSMAuditLog
-		    WHERE strTransactionType = 'Inventory.view.Item'
-		    AND ( CHARINDEX('strItemNo', strJsonData) > 0  OR CHARINDEX('strUnitMeasure', strJsonData) > 0 
+	--Insert to table1
+	INSERT INTO @tablePricebookFileOne
+	SELECT
+		DISTINCT CAST(strRecordNo as int) [intItemId]
+		, strActionType
+		, dtmDate
+	FROM dbo.tblSMAuditLog
+	WHERE strTransactionType = 'Inventory.view.Item'
+	AND ( CHARINDEX('strItemNo', strJsonData) > 0  OR CHARINDEX('strUnitMeasure', strJsonData) > 0 
 				  OR CHARINDEX('strStatus', strJsonData) > 0 OR CHARINDEX('dblSalePrice', strJsonData) > 0  
 				  OR CHARINDEX('strCategoryCode', strJsonData) > 0 OR CHARINDEX('dtmBeginDate', strJsonData) > 0  
 				  OR CHARINDEX('dtmEndDate', strJsonData) > 0 OR CHARINDEX('strDescription', strJsonData) > 0  
@@ -63,55 +59,85 @@ BEGIN TRY
 				  OR CHARINDEX('strLongUPCCode', strJsonData) > 0 OR CHARINDEX('ysnSaleable', strJsonData) > 0  
 				  OR CHARINDEX('ysnReturnable', strJsonData) > 0 OR CHARINDEX('intDepositPLUId', strJsonData) > 0 
 				  OR CHARINDEX('Created', strJsonData) > 0)
-		   AND dtmDate BETWEEN @dtmBeginningChangeDate AND @dtmEndingChangeDate
+	AND dtmDate BETWEEN @dtmBeginningChangeDate AND @dtmEndingChangeDate
 
-		   --Insert to table2
-		   INSERT INTO @tablePricebookFileTwo
-		   SELECT t1.intItemId, t1.strActionType, t1.dtmDate
-				FROM(
-					SELECT *,
-						rn = ROW_NUMBER() OVER(PARTITION BY t.intItemId ORDER BY (SELECT NULL))
-					FROM @tablePricebookFileOne as t
-		   )t1
-		   WHERE rn = 1
+	----Insert to table2
+	--INSERT INTO @tablePricebookFileTwo
+	--SELECT t1.intItemId, t1.strActionType, t1.dtmDate
+	--	FROM(
+	--		SELECT *,
+	--			rn = ROW_NUMBER() OVER(PARTITION BY t.intItemId ORDER BY (SELECT NULL))
+	--		FROM @tablePricebookFileOne as t
+	--)t1
+	--WHERE rn = 1
 
 
-		   INSERT INTO @tableGetItems
-		   SELECT 
-		    CASE WHEN tmpItem.strActionType = 'Created' THEN 'ADD' ELSE 'CHG' END AS strActionType
-			, IUOM.strUpcCode AS strUpcCode
-			, I.strDescription AS strDescription
-			, Prc.dblSalePrice AS dblSalePrice
-			, 1 AS ysnSalesTaxed
-			, IL.ysnIdRequiredLiquor AS ysnIdRequiredLiquor
-			, IL.ysnIdRequiredCigarette AS ysnIdRequiredCigarette
-			, SubCat.strRegProdCode AS strRegProdCode
-				   from tblICItem I
-				   JOIN tblICCategory Cat ON Cat.intCategoryId = I.intCategoryId
-				   JOIN @tablePricebookFileTwo tmpItem ON tmpItem.intItemId = I.intItemId
-				   JOIN tblICItemLocation IL ON IL.intItemId = I.intItemId
-				   LEFT JOIN tblSTSubcategoryRegProd SubCat ON SubCat.intRegProdId = IL.intProductCodeId
-				   JOIN tblSTStore ST ON ST.intStoreId = SubCat.intStoreId
-				   JOIN tblSMCompanyLocation L ON L.intCompanyLocationId = IL.intLocationId
-				   JOIN tblICItemUOM IUOM ON IUOM.intItemId = I.intItemId
-				   JOIN tblICUnitMeasure IUM ON IUM.intUnitMeasureId = IUOM.intUnitMeasureId
-				   JOIN tblSTRegister R ON R.intStoreId = ST.intStoreId
-				   JOIN tblICItemPricing Prc ON Prc.intItemLocationId = IL.intItemLocationId
-				   JOIN tblICItemSpecialPricing SplPrc ON SplPrc.intItemId = I.intItemId
-			WHERE I.ysnFuelItem = 0 
-			AND R.intRegisterId = @intRegisterId 
-			AND ST.intStoreId = @intStoreId
-			AND ((@strCategoryCode <>'whitespaces' 
-			AND Cat.intCategoryId IN(select * from dbo.fnSplitString(@strCategoryCode,',')))
-			OR (@strCategoryCode ='whitespaces'  AND Cat.intCategoryId = Cat.intCategoryId))
-			AND I.intItemId IN (SELECT intItemId FROM @tablePricebookFileTwo)
+	--PricebookFile @StoreId @Register, @Category, @BeginingChangeDate, @EndingChangeDate, @ExportEntirePricebookFile
+	IF(@ysnPricebookFile = 1)
+		BEGIN
+			INSERT INTO @tableGetItems
+			SELECT strActionType
+					, strUpcCode
+					, strDescription
+					, dblSalePrice
+					, ysnSalesTaxed
+					, ysnIdRequiredLiquor
+					, ysnIdRequiredCigarette
+					, strRegProdCode 
+					, intItemId 
+			FROM  
+			(
+			SELECT *,
+					rn = ROW_NUMBER() OVER(PARTITION BY t.intItemId ORDER BY (SELECT NULL))
+				FROM 
+					(
+						SELECT 
+						CASE WHEN tmpItem.strActionType = 'Created' THEN 'ADD' ELSE 'CHG' END AS strActionType
+						, IUOM.strUpcCode AS strUpcCode
+						, I.strDescription AS strDescription
+						, Prc.dblSalePrice AS dblSalePrice
+						, IL.ysnTaxFlag1 AS ysnSalesTaxed
+						, IL.ysnIdRequiredLiquor AS ysnIdRequiredLiquor
+						, IL.ysnIdRequiredCigarette AS ysnIdRequiredCigarette
+						, SubCat.strRegProdCode AS strRegProdCode
+						, I.intItemId AS intItemId
+							   FROM tblICItem I
+							   JOIN tblICCategory Cat ON Cat.intCategoryId = I.intCategoryId
+							   JOIN @tablePricebookFileOne tmpItem ON tmpItem.intItemId = I.intItemId
+							   JOIN tblICItemLocation IL ON IL.intItemId = I.intItemId
+							   LEFT JOIN tblSTSubcategoryRegProd SubCat ON SubCat.intRegProdId = IL.intProductCodeId
+							   JOIN tblSTStore ST ON ST.intStoreId = SubCat.intStoreId
+							   JOIN tblSMCompanyLocation L ON L.intCompanyLocationId = IL.intLocationId
+							   JOIN tblICItemUOM IUOM ON IUOM.intItemId = I.intItemId
+							   JOIN tblICUnitMeasure IUM ON IUM.intUnitMeasureId = IUOM.intUnitMeasureId
+							   JOIN tblSTRegister R ON R.intStoreId = ST.intStoreId
+							   JOIN tblICItemPricing Prc ON Prc.intItemLocationId = IL.intItemLocationId
+							   JOIN tblICItemSpecialPricing SplPrc ON SplPrc.intItemId = I.intItemId
+
+							   LEFT JOIN tblSTPromotionItemList PIL ON PIL.intStoreId = ST.intStoreId -- Promo Item
+
+							   LEFT JOIN tblSTPromotionSalesList PSL ON PSL.intStoreId = ST.intStoreId -- Promo Sales
+
+						WHERE I.ysnFuelItem = 0 
+						AND R.intRegisterId = @intRegisterId 
+						AND ST.intStoreId = @intStoreId
+						AND ((@strCategoryCode <>'whitespaces' 
+						AND Cat.intCategoryId IN(select * from dbo.fnSplitString(@strCategoryCode,',')))
+						OR (@strCategoryCode ='whitespaces'  AND Cat.intCategoryId = Cat.intCategoryId))
+						AND I.intItemId NOT IN (SELECT intItemId FROM @tableGetItems)
+
+						OR PIL.intPromoItemListId BETWEEN @intBeginningPromoItemListId AND @intEndingPromoItemListId -- Promo Item
+
+						OR PSL.strPromoType = 'C' AND PSL.intPromoSalesId BETWEEN @intBeginningPromoSalesId AND @intEndingPromoSalesId -- Promo Sales 
+					) as t
+			) t1
+			WHERE rn = 1
 		END
 
 
 	----PromotionItemListFile @StoreId , @Register, @BeginningItemListId, @EndingItemListId
 	--IF(@ysnPromotionItemList = 1)
 	--	BEGIN
-	--		Print('')
 	--	END
 
 
