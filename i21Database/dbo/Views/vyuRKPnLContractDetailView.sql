0b2f6c8deb9c41a8f2749fb2182ba0e0625379fd
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2017-12-01 22:24:18 +0530

    RM-842

--- i21Database/dbo/Views/vyuRKPnLContractDetailView.sql
+++ i21Database/dbo/Views/vyuRKPnLContractDetailView.sql
@@ -12,8 +12,10 @@ SELECT	DISTINCT
 	CH.intEntityId,
 	CD.dblQuantity	AS	dblDetailQuantity,
 	CD.dblConvertedBasis dblBasis,
-	CH.intContractHeaderId			
+	CH.intContractHeaderId, CD.intItemUOMId , C.ysnSubCurrency, intPriceItemUOMId	,um.intUnitMeasureId	intPriceUomId	
 FROM	tblCTContractDetail		CD		
 JOIN	vyuCTContractHeaderView	CH	ON	CH.intContractHeaderId		=	CD.intContractHeaderId					
-JOIN	tblICItem				IM	ON	IM.intItemId				=	CD.intItemId				
+JOIN	tblICItem				IM	ON	IM.intItemId				=	CD.intItemId	
+JOIN    tblSMCurrency			C   ON  C.intCurrencyID			    =	CD.intCurrencyId
+join tblICItemUOM um on um.intItemUOMId=CD.intPriceItemUOMId
 	
