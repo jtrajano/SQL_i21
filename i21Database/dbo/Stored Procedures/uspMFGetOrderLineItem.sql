4c111c125669a2a17a77b8519770692c73109b5b
Premkumar <Prem.Selladurai@gmail.com>
2017-12-05 22:00:17 +0530

    MFG-3062

--- i21Database/dbo/Stored Procedures/uspMFGetOrderLineItem.sql
+++ i21Database/dbo/Stored Procedures/uspMFGetOrderLineItem.sql
@@ -4,12 +4,21 @@
 	,@strItemNo NVARCHAR(50) = '%'
 	)
 AS
-SELECT I.intItemId
+SELECT
+	OD.intOrderDetailId 
+	,I.intItemId
 	,I.strItemNo
 	,I.strDescription
+	,OD.intLineNo 
+	,OD.dblQty 
+	,IU.intItemUOMId
+	,UM.intUnitMeasureId
+	,UM.strUnitMeasure 
 FROM tblICItem I
 JOIN tblMFOrderDetail OD ON I.intItemId = OD.intItemId
 	AND intOrderHeaderId = @intOrderHeaderId
+JOIN tblICItemUOM IU on IU.intItemUOMId =OD.intItemUOMId 
+JOIN tblICUnitMeasure UM on UM.intUnitMeasureId =IU.intUnitMeasureId 
 WHERE I.intItemId = (
 		CASE 
 			WHEN @intItemId > 0
