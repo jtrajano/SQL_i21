c434593144c81645e8298be1b902813fb7b7bcdd
Feb Montefrio <feb.montefrio@irely.com>
2017-12-05 17:14:25 +0800

    IC-4597

--- app/view/InventoryShipment.js
+++ app/view/InventoryShipment.js
@@ -2271,15 +2271,15 @@ Ext.define('Inventory.view.InventoryShipment', {
                                                                     hidden: true
                                                                 },
                                                                 {
-                                                                    dataIndex: 'strVendorId',
-                                                                    dataType: 'string',
-                                                                    text: 'Vendor No',
-                                                                    flex: 1
-                                                                },
-                                                                {
                                                                     dataIndex: 'strName',
                                                                     dataType: 'string',
                                                                     text: 'Vendor Name',
+                                                                    flex: 1
+                                                                },
+                                                                {
+                                                                    dataIndex: 'strVendorId',
+                                                                    dataType: 'string',
+                                                                    text: 'Vendor No',
                                                                     flex: 1
                                                                 }
                                                             ],
