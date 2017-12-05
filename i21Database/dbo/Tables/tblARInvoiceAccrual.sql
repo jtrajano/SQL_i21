49cc48be95f71a4a4ef9eb284c1979ed97c1fd8d
Robin Panganiban <robin.panganiban@irely.com>
2017-12-05 16:27:51 +0800

    AR-1162

---
+++ i21Database/dbo/Tables/tblARInvoiceAccrual.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[tblARInvoiceAccrual](
+	[intInvoiceAccrualId] [int] IDENTITY(1,1) NOT NULL,
+	[intInvoiceId] [int] NOT NULL,
+	[dtmAccrualDate] [datetime] NULL,
+	[dblAmount] [numeric](18, 6) NULL,
+	[intConcurrencyId] INT CONSTRAINT [DF_tblARInvoiceAccrual_intConcurrencyId] DEFAULT ((0)) NOT NULL,
+	CONSTRAINT [PK_tblARInvoiceAccrual_intInvoiceAccrualId] PRIMARY KEY CLUSTERED ([intInvoiceAccrualId] ASC),
+	CONSTRAINT [FK_tblARInvoiceAccrual_tblARInvoice] FOREIGN KEY ([intInvoiceId]) REFERENCES [dbo].[tblARInvoice] ([intInvoiceId])  ON DELETE CASCADE
+)
