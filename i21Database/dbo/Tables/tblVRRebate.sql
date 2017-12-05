9912e600b5e660f6229dc9b90d7fefb86194a6d4
Kenneth Lew Lagunero <kenneth.lagunero@irely.com>
2017-12-05 15:55:55 +0800

    VR-16

--- i21Database/dbo/Tables/tblVRRebate.sql
+++ i21Database/dbo/Tables/tblVRRebate.sql
@@ -7,7 +7,7 @@
     [dtmDate] DATETIME NOT NULL, 
     [dblRebateRate] NUMERIC(18, 6) NOT NULL DEFAULT 0, 
     [dblRebateAmount] NUMERIC(18, 6) NOT NULL DEFAULT 0, 
-	[strSubmitted] [nvarchar](1) NOT NULL CONSTRAINT [DF_tblVRRebate_strSubmitted]  DEFAULT (N'N'),
+	[strSubmitted] [nvarchar](1) COLLATE Latin1_General_CI_AS NOT NULL CONSTRAINT [DF_tblVRRebate_strSubmitted]  DEFAULT (N'N'),
     CONSTRAINT [PK_tblVRRebate] PRIMARY KEY CLUSTERED([intRebateId] ASC), 
     CONSTRAINT [FK_tblVRRebate_tblARInvoiceDetail] FOREIGN KEY (intInvoiceDetailId) REFERENCES [tblARInvoiceDetail]([intInvoiceDetailId]),
 	CONSTRAINT [FK_tblVRRebate_tblVRProgram] FOREIGN KEY (intProgramId) REFERENCES [tblVRProgram]([intProgramId]),
