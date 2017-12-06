4bab21651599048414729d148d0a969a59555b36
bonn ponce <bonn.ponce@irely.com>
2017-12-06 22:48:13 +0800

    CF-1355

---
+++ i21Database/dbo/Views/vyuCFTopCardLockCustomer.sql
@@ -0,0 +1,23 @@
+﻿
+
+
+CREATE VIEW [dbo].[vyuCFTopCardLockCustomer]
+AS
+SELECT 
+ intEntityCustomerId
+,strCustomerNumber
+,emEnt.strName
+,dblQtyShipped
+,dblQtyOrdered
+,dblInvoiceTotal
+,dtmDate
+FROM tblCFAccount as cfAccnt
+INNER JOIN tblARCustomer as arCust
+ON cfAccnt.intCustomerId = arCust.intEntityId
+INNER JOIN tblEMEntity as emEnt
+ON arCust.intEntityId = emEnt.intEntityId
+INNER JOIN tblARInvoice as arInv
+ON arCust.intEntityId = arInv.intEntityCustomerId 
+INNER JOIN tblARInvoiceDetail as arInvDetail
+ON arInv.intInvoiceId = arInvDetail.intInvoiceId
+WHERE arInv.strInvoiceNumber like '%CFDT%'
