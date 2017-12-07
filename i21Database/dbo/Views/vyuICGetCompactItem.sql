acb741916b8968a4f8d777fd1c19c5a4730929eb
Feb Montefrio <feb.montefrio@irely.com>
2017-12-07 15:19:53 +0800

    IC-4607 Fix the Hazmat. Replace intHazmatMessage with intHazmatTag.

--- i21Database/dbo/Views/vyuICGetCompactItem.sql
+++ i21Database/dbo/Views/vyuICGetCompactItem.sql
@@ -38,8 +38,6 @@ SELECT
 	, strTonnageTaxUOM = TonnageUOM.strUnitMeasure
 	, Item.intTonnageTaxUOMId
 	, strFuelCategory 		= FuelCategory.strRinFuelCategoryCode
-	, strMedicationTag 		= Medication.strDescription
-	, strIngredientTag 		= Ingredient.strDescription
 	, strPhysicalItem 		= PhysicalItem.strItemNo
 	, strPatronageCategory 	= PatronageCategory.strCategoryCode
 	, strPatronageDirect 	= PatronageDirect.strCategoryCode
@@ -50,13 +48,19 @@ SELECT
 	, strSeason 			= Season.strDescription
 	, strClass 				= Class.strDescription
 	, strProductLine 		= ProductLine.strDescription
-	, strHazmatMessage		= HazMat.strDescription
 	, ysnUseWeighScales		= Item.ysnUseWeighScales
 	, ysnIsBasket			= Item.ysnIsBasket
 	, strDimensionUOM		= mfgDimensionUOM.strUnitMeasure
 	, strWeightUOM			= mfgWeightUOM.strUnitMeasure
 	, strSecondaryStatus    = LotStatus.strSecondaryStatus
 	, Item.ysnLotWeightsRequired
+	, strMedicationTag 		= Medication.strTagNumber
+	, strIngredientTag 		= Ingredient.strTagNumber
+	, strHazmatTag			= HazMat.strTagNumber
+	, strMedicationMessage 		= Medication.strDescription
+	, strIngredientMessage 		= Ingredient.strDescription
+	, strHazmatMessage			= HazMat.strDescription
+
 FROM tblICItem Item
 LEFT JOIN tblICCommodity Commodity ON Commodity.intCommodityId = Item.intCommodityId
 LEFT JOIN tblICCategory Category ON Category.intCategoryId = Item.intCategoryId
@@ -71,7 +75,7 @@ LEFT JOIN tblICM2MComputation M2M ON M2M.intM2MComputationId = Item.intM2MComput
 LEFT JOIN tblICRinFuelCategory FuelCategory ON FuelCategory.intRinFuelCategoryId = Item.intRINFuelTypeId
 LEFT JOIN tblICTag Medication ON Medication.intTagId = Item.intMedicationTag
 LEFT JOIN tblICTag Ingredient ON Ingredient.intTagId = Item.intIngredientTag
-LEFT JOIN tblICTag HazMat ON HazMat.intTagId = Item.intHazmatMessage
+LEFT JOIN tblICTag HazMat ON HazMat.intTagId = Item.intHazmatTag
 LEFT JOIN tblICItem PhysicalItem ON PhysicalItem.intItemId = Item.intPhysicalItem
 LEFT JOIN tblPATPatronageCategory PatronageCategory ON PatronageCategory.intPatronageCategoryId = Item.intPatronageCategoryId
 LEFT JOIN tblPATPatronageCategory PatronageDirect ON PatronageDirect.intPatronageCategoryId = Item.intPatronageCategoryDirectId
