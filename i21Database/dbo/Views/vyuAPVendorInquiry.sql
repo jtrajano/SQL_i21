27bdea2aae75855c267b28e0d0e4005428fbf468
Erick <erick.delacruz@irely.com>
2017-12-08 14:22:23 +0800

    AP-4741

--- i21Database/dbo/Views/vyuAPVendorInquiry.sql
+++ i21Database/dbo/Views/vyuAPVendorInquiry.sql
@@ -9,8 +9,20 @@ SELECT
 	,entityContact.strEmail
 	,entityContact.strMobile
 	,entityContact.strName AS strContactName
+	,fi.strLastVoucher
+	,fi.intLastBillId
+	,fi.dtmLastPaymentDate
+	,fi.intLastPaymentId
+	,fi.dblYTDVouchers
+	,fi.dblYTDPayments
+	,balances.dblFuture
+	,balances.dbl0To30Days
+	,balances.dbl31To60Days
+	,balances.dbl61To90Days
 FROM dbo.tblAPVendor vendor
 INNER JOIN dbo.tblEMEntity entity ON vendor.intEntityId = entity.intEntityId
 INNER JOIN dbo.tblEMEntityToContact entityToContact ON entityToContact.intEntityId = entity.intEntityId
 INNER JOIN dbo.tblEMEntity entityContact
 		ON entityToContact.intEntityContactId = entityContact.intEntityId AND entityToContact.ysnDefaultContact = 1
+OUTER APPLY dbo.fnAPVendorInquiryFinancialInfo(vendor.intEntityId) fi
+OUTER APPLY dbo.fnAPGetVendorBalances(vendor.intEntityId) balances
