f40ad01f5026fcb2e170667450d5fbaf0d06672b
Jb Mago <benedict.mago@irely.com>
2017-12-12 18:47:03 +0800

    IC-4592

--- server/iRely.Inventory.WebAPI/Reports/TransferOrderReport.cs
+++ server/iRely.Inventory.WebAPI/Reports/TransferOrderReport.cs
@@ -11,6 +11,28 @@ namespace iRely.Inventory.WebApi.Reporting
         public TransferOrderReport()
         {
             InitializeComponent();
+            xrTableCellQuantity.BeforePrint += xrTableCellQuantity_BeforePrint;
+            xrTableCellNetWgt.BeforePrint += xrTableCellNetWgt_BeforePrint;
+        }
+
+        public void xrTableCellQuantity_BeforePrint(object sender, System.Drawing.Printing.PrintEventArgs e)
+        {
+            if (GetCurrentRow() == null) return;
+
+            string quantity = GetCurrentColumnValue("dblQuantity") as string;
+            string qtyUOM = GetCurrentColumnValue("strUnitMeasureSymbol") as string;
+
+            ((XRTableCell)sender).Text += " " + qtyUOM;
+        }
+
+        public void xrTableCellNetWgt_BeforePrint(object sender, System.Drawing.Printing.PrintEventArgs e)
+        {
+            if (GetCurrentRow() == null) return;
+
+            string netWgt = GetCurrentColumnValue("dblNet") as string;
+            string netWgtUOM = GetCurrentColumnValue("strGrossNetUOMSymbol") as string;
+
+            ((XRTableCell)sender).Text += " " + netWgtUOM;
         }
     }
 }
