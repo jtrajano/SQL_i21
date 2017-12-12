e6935f8f1af4234838475d86f3a8230732a19d4d
Lex Eugene Peregrino <lex.peregrino@irely.com>
2017-12-12 17:46:25 +0800

    AP-4596 -[1099] Allow generating e-file for 1099 DIV 1.0

---
+++ i21Database/dbo/Stored Procedures/uspAPCreate1099DIVFile.sql
@@ -0,0 +1,53 @@
+﻿CREATE PROCEDURE [dbo].[uspAPCreate1099DIVFile]
+	@vendorFrom NVARCHAR(100) = NULL,
+	@vendorTo NVARCHAR(100) = NULL,
+	@year INT,
+	@reprint BIT = 0,
+	@corrected BIT = 0,
+	@test BIT = 0
+AS
+
+SET QUOTED_IDENTIFIER OFF
+SET ANSI_NULLS ON
+SET NOCOUNT ON
+SET XACT_ABORT ON
+SET ANSI_WARNINGS OFF;
+
+DECLARE @transmitter AS TABLE(strTransmitter NVARCHAR(1500))
+DECLARE @payer AS TABLE(strPayer NVARCHAR(1500))
+DECLARE @payee AS TABLE(strPayee NVARCHAR(MAX))
+DECLARE @endOfDIV AS TABLE(strEndOfINT NVARCHAR(1500))
+DECLARE @endOfTransmitter AS TABLE(strEndOfTransmitter NVARCHAR(1500))
+DECLARE @totalPayee NVARCHAR(16)
+
+
+INSERT INTO @transmitter
+SELECT dbo.[fnAP1099EFileTransmitter](@year,@test)
+
+INSERT INTO @payer
+SELECT dbo.[fnAP1099EFilePayer](@year, 2, @vendorFrom, @vendorTo)
+
+INSERT INTO @payee
+SELECT * FROM dbo.[fnAP1099EFileDIVPayee](@year, @reprint, @corrected, @vendorFrom, @vendorTo)
+
+INSERT INTO @endOfDIV
+SELECT dbo.fnAP1099EFileEndOfDIV(@year, @reprint, @corrected, @vendorFrom, @vendorTo)
+
+INSERT INTO @endOfTransmitter
+SELECT [dbo].[fnAP1099EFileEndOfTransmitter](1)
+
+SET @totalPayee = REPLICATE('0', 8 - LEN(CAST((SELECT COUNT(*) FROM @payee) AS NVARCHAR(100)))) + CAST((SELECT COUNT(*) FROM @payee) AS NVARCHAR(100))
+
+UPDATE A
+	SET A.strTransmitter = STUFF(A.strTransmitter, 296, 8, @totalPayee)
+FROM @transmitter A
+
+SELECT * FROM @transmitter
+UNION ALL
+SELECT * FROM @payer
+UNION ALL
+SELECT * FROM @payee
+UNION ALL
+SELECT * FROM @endOfDIV
+UNION ALL 
+SELECT * FROM @endOfTransmitter
