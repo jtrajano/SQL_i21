3f8bc96ad72eb3915da42f3ccc5fe843b2e0f7ad
Vinoth Kumar T <vinoth.t@irely.com>
2017-12-14 17:50:46 +0530

    MFG-2844

---
+++ i21Database/dbo/Tables/tblMFReportLabel.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE dbo.tblMFReportLabel
+(
+	intReportLabelId INT NOT NULL IDENTITY(1, 1) CONSTRAINT PK_tblMFReportLabel_intReportLabelId PRIMARY KEY,
+	strReportName [NVARCHAR](100) COLLATE Latin1_General_CI_AS NOT NULL,
+	ysnShow BIT NOT NULL DEFAULT 0,
+	[intConcurrencyId] INT NOT NULL, 
+
+	CONSTRAINT [UK_tblMFReportLabel_strReportName] UNIQUE (strReportName)
+)
