80734154e15e2cd2fbf5aa1867703bf74b362f80
Lex Eugene Peregrino <lex.peregrino@irely.com>
2017-12-22 11:23:43 +0800

    AP-4835 [Open Clearing/Voucher] Tax group not displayed on voucher for other charge 1.0

--- i21Database/dbo/Stored Procedures/uspAPCreateBillFromIR.sql
+++ i21Database/dbo/Stored Procedures/uspAPCreateBillFromIR.sql
@@ -786,7 +786,7 @@ BEGIN
 				[dblForexRate]				=	ISNULL(A.dblForexRate,1),
 				[intForexRateTypeId]		=   A.intForexRateTypeId,
 				[ysnSubCurrency]			=	ISNULL(A.ysnSubCurrency,0),
-				[intTaxGroupId]				=	NULL,
+				[intTaxGroupId]				=	A.intTaxGroupId,
 				[intAccountId]				=	[dbo].[fnGetItemGLAccount](A.intItemId,D.intItemLocationId, 'AP Clearing'),
 				[dblTotal]					=	(CASE WHEN A.ysnSubCurrency > 0 THEN A.dblUnitCost / A.intSubCurrencyCents ELSE A.dblUnitCost END) * A.dblQuantityToBill,
 				[dblCost]					=	ABS(A.dblUnitCost),
@@ -980,7 +980,7 @@ BEGIN
 						[dblForexRate]				=	ISNULL(A.dblForexRate,1),
 						[intForexRateTypeId]		=   A.intForexRateTypeId,
 						[ysnSubCurrency]			=	ISNULL(A.ysnSubCurrency,0),
-						[intTaxGroupId]				=	NULL,
+						[intTaxGroupId]				=	A.intTaxGroupId,
 						[intAccountId]				=	[dbo].[fnGetItemGLAccount](A.intItemId, D.intItemLocationId, 'AP Clearing'),
 						--[dblTotal]					=	(CASE WHEN A.ysnSubCurrency > 0 THEN A.dblUnitCost / A.intSubCurrencyCents ELSE A.dblUnitCost END),
 						[dblTotal]					=	(CASE WHEN A.ysnSubCurrency > 0 THEN A.dblUnitCost / A.intSubCurrencyCents ELSE A.dblUnitCost END) * A.dblQuantityToBill, --3RD PARTY TOTAL WILL BE POSSTIVE / RECEIPT VENDOR PRICE = Y WILL BE NEGATIVE
