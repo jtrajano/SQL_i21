776251f7042bd9e83a03b67a26805f43cf5a271a
JeffTrajano <jeffrey.trajano@irely.com>
2017-12-27 21:26:24 +0800

    GL-3676 Revalue Currency - Add CM

--- i21Database/dbo/Views/vyuCMMultiCurrencyRevalue.sql
+++ i21Database/dbo/Views/vyuCMMultiCurrencyRevalue.sql
@@ -3,8 +3,8 @@ AS
 SELECT DISTINCT
 	strTransactionType		=	'Cash Balances',
 	strTransactionId		=	BA.strBankAccountNo,
-	strTransactionDate		=	EOM.Value,
-	strTransactionDueDate	=	'',
+	strTransactionDate		=	replace(convert( varchar(10), EOP.Value, 102),'.', '-'),
+	strTransactionDueDate	=	NULL,
 	strVendorName			=	'',
 	strCommodity			=	'',
 	strLineOfBusiness		=	'',
@@ -12,8 +12,8 @@ SELECT DISTINCT
 	strTicket				=	'',
 	strContractNumber		=	'',
 	strItemId				=	'',
-	dblQuantity				=	0,
-	dblUnitPrice			=	0,
+	dblQuantity				=	NULL,
+	dblUnitPrice			=	NULL,
 	dblAmount				=	BankBalance.Value/Rate.Value, -- this is in functional currency
 	intCurrencyId			=	BA.intCurrencyId,
 	intForexRateType		=	NewRateTypeId.intCashManagementRateTypeId,
@@ -31,22 +31,22 @@ FROM
   tblCMBankTransaction BT
   JOIN vyuCMBankAccount BA ON BT.intBankAccountId = BA.intBankAccountId
   OUTER APPLY(
-		SELECT CAST(DATEADD(MONTH,DATEDIFF(MONTH,0,dtmDate)+1,0)-1 AS DATE) Value
-  )EOM
+		select dtmEndDate Value from tblGLFiscalYearPeriod where dtmDate between dtmStartDate and dtmEndDate
+  )EOP
   OUTER APPLY(
 	SELECT TOP 1 intCashManagementRateTypeId, Rt.strCurrencyExchangeRateType FROM tblSMMultiCurrency MC
 	JOIN tblSMCurrencyExchangeRateType Rt ON MC.intCashManagementRateTypeId = Rt.intCurrencyExchangeRateTypeId
 	WHERE intMultiCurrencyId = 1
   ) NewRateTypeId
   OUTER APPLY(
-		SELECT dblRate from [dbo].[fnSMGetForexRate] (BA.intCurrencyId,NewRateTypeId.intCashManagementRateTypeId,EOM.Value)
+		SELECT dblRate from [dbo].[fnSMGetForexRate] (BA.intCurrencyId,NewRateTypeId.intCashManagementRateTypeId,EOP.Value)
   )NewRate
  
   OUTER APPLY(
-		SELECT [dbo].[fnGetBankBalance] (BT.intBankAccountId, EOM.Value) Value
+		SELECT [dbo].[fnGetBankBalance] (BT.intBankAccountId, EOP.Value) Value
   ) BankBalance
   OUTER APPLY (
-		SELECT [dbo].fnGLGetCMGLDetailBalance(EOM.Value, BA.intGLAccountId) Value -- this is in us / functional currency
+		SELECT [dbo].fnGLGetCMGLDetailBalance(EOP.Value, BA.intGLAccountId) Value -- this is in us / functional currency
   )GLBalance
   OUTER APPLY(
 		Select GLBalance.Value / BankBalance.Value Value
