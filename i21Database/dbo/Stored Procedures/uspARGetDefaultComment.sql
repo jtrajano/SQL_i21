5b2013d7fe1b6de866f2c05a7362b6aded784705
Mon Gonzales <mon.gonzales@irely.com>
2017-12-27 16:18:54 +0800

    AR-6183

--- i21Database/dbo/Stored Procedures/uspARGetDefaultComment.sql
+++ i21Database/dbo/Stored Procedures/uspARGetDefaultComment.sql
@@ -5,8 +5,20 @@
 	@strType                NVARCHAR(50) = NULL,
 	@strHeaderComment		NVARCHAR(500) = NULL OUTPUT,
 	@strFooterComment		NVARCHAR(500) = NULL OUTPUT,
-	@DocumentMaintenanceId	INT = NULL
+	@DocumentMaintenanceId	INT = NULL,
+	@intDocumentUsedId		INT NULL OUTPUT,
+	@strDocumentUsedCode	NVARCHAR(10) NULL OUTPUT
 AS
 
-SET @strHeaderComment = [dbo].[fnARGetDefaultComment](@intCompanyLocationId, @intEntityCustomerId, @strTransactionType, @strType, 'Header', @DocumentMaintenanceId, 0)
+SELECT TOP 1 @strHeaderComment =  strDefaultComment,
+		@intDocumentUsedId = intDocumentMaintenanceId
+	FROM 
+		[dbo].[fnARGetDefaultCommentTable](@intCompanyLocationId, @intEntityCustomerId, @strTransactionType, @strType, 'Header', @DocumentMaintenanceId, 0)
+
+IF @intDocumentUsedId IS NOT NULL
+BEGIN
+	SELECT TOP 1 @strDocumentUsedCode = strCode FROM tblSMDocumentMaintenance 
+		WHERE intDocumentMaintenanceId = @intDocumentUsedId
+END
+
 SET @strFooterComment = [dbo].[fnARGetDefaultComment](@intCompanyLocationId, @intEntityCustomerId, @strTransactionType, NULL, 'Footer', @DocumentMaintenanceId, 0)
