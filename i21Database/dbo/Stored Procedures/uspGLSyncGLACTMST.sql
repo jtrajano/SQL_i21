e38bf03872c984977165c979f4f5e9a67074a844
JeffTrajano <jeffrey.trajano@irely.com>
2017-12-27 21:56:26 +0800

    GL-3676 Revalue Currency - Add CM

--- i21Database/dbo/Stored Procedures/uspGLSyncGLACTMST.sql
+++ i21Database/dbo/Stored Procedures/uspGLSyncGLACTMST.sql
@@ -1,5 +1,3 @@
 CREATE procedure uspGLSyncGLACTMST
 @intAccountId INT
 AS
-IF EXISTS (SELECT TOP 1 1 FROM tblSMCompanyPreference WHERE ysnLegacyIntegration = 1)
-	RAISERROR('Sync GLACTMST Procedure is not available', 16, 1);
