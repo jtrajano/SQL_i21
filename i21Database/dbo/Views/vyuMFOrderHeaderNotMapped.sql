a9a21c7baf8e4822c3bceaf35e1b0c703d3755ce
Vinoth Kumar T <vinoth.t@irely.com>
2017-12-28 22:28:43 +0530

    MFG-3103

---
+++ i21Database/dbo/Views/vyuMFOrderHeaderNotMapped.sql
@@ -0,0 +1,22 @@
+﻿CREATE VIEW vyuMFOrderHeaderNotMapped
+AS
+SELECT SW.intOrderHeaderId
+	,W.dtmPlannedDate AS dtmRequiredDate
+	,OT.strOrderType
+	,OS.strOrderStatus
+FROM tblMFOrderHeader OH
+JOIN tblMFOrderType OT ON OT.intOrderTypeId = OH.intOrderTypeId
+JOIN tblMFOrderStatus OS ON OS.intOrderStatusId = OH.intOrderStatusId
+JOIN tblMFStageWorkOrder SW ON SW.intOrderHeaderId = OH.intOrderHeaderId
+JOIN tblMFWorkOrder W ON W.intWorkOrderId = SW.intWorkOrderId
+
+UNION ALL
+
+SELECT OH.intOrderHeaderId
+	,S.dtmShipDate AS dtmRequiredDate
+	,OT.strOrderType
+	,OS.strOrderStatus
+FROM tblMFOrderHeader OH
+JOIN tblMFOrderType OT ON OT.intOrderTypeId = OH.intOrderTypeId
+JOIN tblMFOrderStatus OS ON OS.intOrderStatusId = OH.intOrderStatusId
+JOIN tblICInventoryShipment S ON S.strShipmentNumber = OH.strReferenceNo
