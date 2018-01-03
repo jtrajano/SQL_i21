e692bed69dc135144923f88cc7b2dcb277668bc6
JeffTrajano <jeffrey.trajano@irely.com>
2018-01-03 15:12:37 +0800

    GL-3626 Automatic assigning "Current Fiscal Year"

--- i21Database/dbo/Stored Procedures/uspGLUpdateFiscalYearStatus.sql
+++ i21Database/dbo/Stored Procedures/uspGLUpdateFiscalYearStatus.sql
@@ -11,40 +11,31 @@ DECLARE  @strRetainedAcctGroup	 NVARCHAR(50)
 		,@intYear				INT
 		,@strRetainedAccount	NVARCHAR(50)	= ''
 
-BEGIN TRANSACTION;
+IF @ysnPost = 1
+BEGIN
+	UPDATE tblGLFiscalYear SET ysnStatus = 0 WHERE intFiscalYearId = @intFiscalYearId	
+	UPDATE tblGLFiscalYearPeriod SET ysnOpen = 0,ysnAROpen = 0, ysnAPOpen = 0, ysnINVOpen = 0, ysnCMOpen = 0, ysnPROpen = 0, ysnCTOpen = 0, ysnFAOpen = 0 where intFiscalYearId = @intFiscalYearId
 
-BEGIN TRY
-	IF @ysnPost = 1
+	IF EXISTS(SELECT TOP 1 1 FROM tblGLCurrentFiscalYear WHERE intFiscalYearId = @intFiscalYearId)
 	BEGIN
-		UPDATE tblGLFiscalYear SET ysnStatus = 0 WHERE intFiscalYearId = @intFiscalYearId	
-		UPDATE tblGLFiscalYearPeriod SET ysnOpen = 0,ysnAROpen = 0, ysnAPOpen = 0, ysnINVOpen = 0, ysnCMOpen = 0, ysnPROpen = 0, ysnCTOpen = 0, ysnFAOpen = 0 where intFiscalYearId = @intFiscalYearId
-	END	
-	ELSE IF @ysnPost = 0 
-	BEGIN
-		SET @intYear			= (SELECT TOP 1 CAST(strFiscalYear as INT) FROM tblGLFiscalYear WHERE intFiscalYearId = @intFiscalYearId) 		
-		SET @strRetainedAccount = (SELECT TOP 1 strAccountId FROM tblGLAccount WHERE intAccountId = (SELECT TOP 1 intRetainAccount FROM tblGLFiscalYear WHERE intFiscalYearId = @intFiscalYearId))
-		SET @intAccountId			= (SELECT intAccountId FROM tblGLAccount WHERE strAccountId = @strRetainedAccount)
-		SET @strRetainedAcctGroup	= ISNULL((SELECT TOP 1 strAccountGroup FROM tblGLAccount A
-													LEFT JOIN tblGLAccountGroup B
-													ON A.intAccountGroupId = B.intAccountGroupId WHERE A.strAccountId = @strRetainedAccount), '')
-		UPDATE tblGLDetail SET ysnIsUnposted = 1 WHERE strTransactionId = CAST(@intYear as NVARCHAR(10)) + '-' + @strRetainedAccount and ysnIsUnposted = 0
-		UPDATE tblGLFiscalYear SET ysnStatus = 1 WHERE intFiscalYearId = @intFiscalYearId
-		UPDATE tblGLFiscalYearPeriod SET ysnOpen = 1,ysnAROpen = 1, ysnAPOpen = 1, ysnINVOpen = 1, ysnCMOpen = 1, ysnPROpen = 1, ysnCTOpen = 1, ysnFAOpen = 1 where intFiscalYearId = @intFiscalYearId
+		--update the current fiscal year to next open fiscal year
+		DECLARE @currentYear  NVARCHAR(4)
+		SELECT TOP 1 @currentYear =  strFiscalYear  FROM tblGLCurrentFiscalYear CF JOIN tblGLFiscalYear FY ON FY.intFiscalYearId = CF.intFiscalYearId
+		UPDATE CF  SET intFiscalYearId = FY.intFiscalYearId , dtmBeginDate = FY.dtmDateFrom, dtmEndDate = FY.dtmDateTo FROM tblGLCurrentFiscalYear CF 
+		CROSS APPLY(
+			SELECT TOP 1 intFiscalYearId, dtmDateFrom,dtmDateTo  FROM tblGLFiscalYear WHERE strFiscalYear > @currentYear AND ysnStatus = 1 ORDER BY strFiscalYear 
+		)FY
 	END
-	
-	
-	
-END TRY
-BEGIN CATCH
-
-	SELECT  0
-	IF @@TRANCOUNT > 0 ROLLBACK TRANSACTION;
-	
-END CATCH;
-
-IF @@TRANCOUNT > 0
-	BEGIN
-		SELECT 1
-		COMMIT TRANSACTION;
-	END
-GO
+END	
+ELSE IF @ysnPost = 0 
+BEGIN
+	SET @intYear			= (SELECT TOP 1 CAST(strFiscalYear as INT) FROM tblGLFiscalYear WHERE intFiscalYearId = @intFiscalYearId) 		
+	SET @strRetainedAccount = (SELECT TOP 1 strAccountId FROM tblGLAccount WHERE intAccountId = (SELECT TOP 1 intRetainAccount FROM tblGLFiscalYear WHERE intFiscalYearId = @intFiscalYearId))
+	SET @intAccountId			= (SELECT intAccountId FROM tblGLAccount WHERE strAccountId = @strRetainedAccount)
+	SET @strRetainedAcctGroup	= ISNULL((SELECT TOP 1 strAccountGroup FROM tblGLAccount A
+												LEFT JOIN tblGLAccountGroup B
+												ON A.intAccountGroupId = B.intAccountGroupId WHERE A.strAccountId = @strRetainedAccount), '')
+	UPDATE tblGLDetail SET ysnIsUnposted = 1 WHERE strTransactionId = CAST(@intYear as NVARCHAR(10)) + '-' + @strRetainedAccount and ysnIsUnposted = 0
+	UPDATE tblGLFiscalYear SET ysnStatus = 1 WHERE intFiscalYearId = @intFiscalYearId
+	UPDATE tblGLFiscalYearPeriod SET ysnOpen = 1,ysnAROpen = 1, ysnAPOpen = 1, ysnINVOpen = 1, ysnCMOpen = 1, ysnPROpen = 1, ysnCTOpen = 1, ysnFAOpen = 1 where intFiscalYearId = @intFiscalYearId
+END
