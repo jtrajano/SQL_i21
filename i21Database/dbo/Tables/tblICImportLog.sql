d53e465d7d749cffbff93ed2e73d5976a2e7a270
Wendell Wayne Estrada <wendell.estrada@irely.com>
2018-01-09 14:15:23 +0800

    IC-4391 Improvements on Inventory Import

---
+++ i21Database/dbo/Tables/tblICImportLog.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE [dbo].[tblICImportLog]
+(
+	[intImportLogId] INT IDENTITY(1, 1) NOT NULL,
+	[strDescription] NVARCHAR(500) COLLATE Latin1_General_CI_AS NULL,
+	[intTotalRows] INT NULL,
+	[intRowsImported] INT NULL,
+	[intTotalErrors] INT NULL,
+	[intTotalWarnings] INT NULL,
+	[dblTimeSpentInSeconds] NUMERIC(18, 6) NULL,
+	[intUserEntityId] INT NULL,
+	[strType] NVARCHAR(200) COLLATE Latin1_General_CI_AS NULL,
+	[strFileType] NVARCHAR(200) COLLATE Latin1_General_CI_AS NULL,
+	[strFileName] NVARCHAR(300) COLLATE Latin1_General_CI_AS NULL,
+	[dtmDateImported] DATETIME NULL,
+	[ysnAllowDuplicates] BIT NULL,
+	[ysnAllowOverwriteOnImport] BIT NULL,
+	[strLineOfBusiness] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL,
+	[ysnContinueOnFailedImports] BIT NULL,
+	[intConcurrencyId] INT NULL, 
+    CONSTRAINT [PK_tblICImportLog] PRIMARY KEY NONCLUSTERED ([intImportLogId])
+)
+GO
