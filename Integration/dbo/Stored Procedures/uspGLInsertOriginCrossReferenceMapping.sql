b2423c612865493696ae6992aaa9466158bc66de
JeffTrajano <jeffrey.trajano@irely.com>
2018-01-10 11:39:39 +0800

    GL-2183 Add cross reference type to the tblGLCOACrossReference table

--- Integration/dbo/Stored Procedures/uspGLInsertOriginCrossReferenceMapping.sql
+++ Integration/dbo/Stored Procedures/uspGLInsertOriginCrossReferenceMapping.sql
@@ -2,53 +2,41 @@
 AS
 IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[glactmst]') AND type IN (N'U'))
 BEGIN
-	EXEC('
-	UPDATE coa
-	SET strOldId = CAST(CAST(glact_acct1_8 AS INT) AS NVARCHAR(50)) + ''-'' + CAST( CAST(glact_acct9_16 AS INT) AS NVARCHAR(50))
-	FROM tblGLCOACrossReference coa JOIN glactmst orig ON coa.intLegacyReferenceId = orig.A4GLIdentity
+EXEC('UPDATE coa
+SET strOldId = CAST(CAST(glact_acct1_8 AS INT) AS NVARCHAR(50)) + ''-'' + CAST( CAST(glact_acct9_16 AS INT) AS NVARCHAR(50))
+FROM tblGLCOACrossReference coa JOIN glactmst orig ON coa.intLegacyReferenceId = orig.A4GLIdentity
 
-	IF NOT EXISTS (SELECT TOP 1 1 FROM tblGLAccountSystem WHERE strAccountSystemDescription = ''Origin'')
-	BEGIN
-		SET IDENTITY_INSERT tblGLAccountSystem ON
-		INSERT INTO tblGLAccountSystem(intAccountSystemId, strAccountSystemDescription, intConcurrencyId)
-		VALUES( 1, ''Origin'',1)
-		SET IDENTITY_INSERT tblGLAccountSystem OFF
-	END
-	DELETE FROM dbo.tblGLCrossReferenceMapping WHERE intAccountId IS NULL
+DELETE FROM dbo.tblGLCrossReferenceMapping WHERE intAccountId IS NULL
+DECLARE @originId INT
+IF NOT EXISTS (SELECT TOP 1 1 FROM tblGLAccountSystem WHERE strAccountSystemDescription = ''Origin'')
+BEGIN
+		
+	INSERT INTO tblGLAccountSystem(strAccountSystemDescription, intConcurrencyId)
+	VALUES(''Origin'',1)
+	SELECT @originId=SCOPE_IDENTITY()
+END
+ELSE
+BEGIN
+	SELECT TOP 1 @originId = intAccountSystemId FROM tblGLAccountSystem WHERE strAccountSystemDescription = ''Origin''
+END
 	MERGE dbo.tblGLCrossReferenceMapping as map
 	USING dbo.tblGLCOACrossReference AS coa
-	ON map.intAccountId = coa.inti21Id
+	ON map.intAccountId = coa.inti21Id 
+	AND map.intAccountSystemId = @originId
+	AND coa.ysnOrigin =1
 	WHEN MATCHED THEN
-	UPDATE SET
-	--intOldAccountId = coa.intLegacyReferenceId,
-	strOldAccountId = coa.strOldId
+		UPDATE SET strOldAccountId = coa.strOldId
 	WHEN NOT MATCHED THEN
-
-	INSERT (
-	--[intOldAccountId],
-	[strOldAccountId],
-	[intAccountId],
-	[ysnInbound],
-	[ysnOutbound],
-	--[stri21AccountId], 
-	[intAccountSystemId],
-	[intConcurrencyId]
-	)
-	VALUES 
-	(
-	--intLegacyReferenceId,
-	strOldId,
-	inti21Id,
-	1,
-	0,
-	--stri21Id,
-	1,
-	1);
+		INSERT (
+			[strOldAccountId],[intAccountId],[ysnInbound],[ysnOutbound],[intAccountSystemId],[intConcurrencyId])
+		VALUES (
+			strOldId,inti21Id,1,0,@originId,1);
 	
-	IF NOT EXISTS(SELECT TOP 1 1 FROM tblGLCompanyPreferenceOption)
-		INSERT INTO tblGLCompanyPreferenceOption(intDefaultVisibleOldAccountSystemId,intConcurrencyId) VALUES(1,1)
 	
-	UPDATE tblGLCompanyPreferenceOption SET intDefaultVisibleOldAccountSystemId = 1 WHERE intDefaultVisibleOldAccountSystemId IS NULL')
+IF NOT EXISTS(SELECT TOP 1 1 FROM tblGLCompanyPreferenceOption)
+	INSERT INTO tblGLCompanyPreferenceOption(intDefaultVisibleOldAccountSystemId,intConcurrencyId) VALUES(1,1)
+	
+UPDATE tblGLCompanyPreferenceOption SET intDefaultVisibleOldAccountSystemId = 1 WHERE intDefaultVisibleOldAccountSystemId IS NULL')
 END
 	
 
