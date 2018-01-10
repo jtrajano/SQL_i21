2301630a562d18df33e3317139da63f9e1d0817a
Jb Mago <benedict.mago@irely.com>
2018-01-10 15:47:16 +0800

    IC-4724

--- i21Database/Scripts/Post-Deployment/IC/24_ImplementBasketChanges.sql
+++ i21Database/Scripts/Post-Deployment/IC/24_ImplementBasketChanges.sql
@@ -1,23 +1,24 @@
 ﻿PRINT N'BEGIN - Migration for Bundle Type Items'
 GO
-IF EXISTS(SELECT TOP 1 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE [TABLE_NAME] = N'tblICItem')
+IF EXISTS(SELECT TOP 1 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE [TABLE_NAME] = N'tmptblICItem' )
 BEGIN
-
-	IF EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE [TABLE_NAME] = N'tblICItem' AND [COLUMN_NAME] = N'ysnIsBasket_TEMP')
-	BEGIN
-		EXEC('
+	EXEC('
 			UPDATE [dbo].[tblICItem] SET [strBundleType] = N''Bundle''
-			WHERE [strType] = N''Bundle'' AND ysnIsBasket_TEMP != 1;
+			FROM [dbo].[tblICItem] IC
+			INNER JOIN [tmptblICItem] tmpIC ON [IC].[intItemId] = [tmpIC].[intItemId]
+			WHERE [IC].[strType] = N''Bundle'' AND [tmpIC].[ysnIsBasket] != 1;
 
 			UPDATE [dbo].[tblICItem] SET [strBundleType] = N''Basket''
-			WHERE [strType] = N''Bundle'' AND ysnIsBasket_TEMP = 1;
+			FROM [dbo].[tblICItem] IC
+			INNER JOIN [tmptblICItem] tmpIC ON [IC].[intItemId] = [tmpIC].[intItemId]
+			WHERE [IC].[strType] = N''Bundle'' AND [tmpIC].[ysnIsBasket] = 1;
 
-			ALTER TABLE [dbo].[tblICItem] 
-				DROP COLUMN [ysnIsBasket_TEMP]
+			DROP TABLE tmptblICItem
 		')
-	END
+END
 
-	IF EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE [TABLE_NAME] = N'tblICItem' AND [COLUMN_NAME] = N'strBundleType')
+IF EXISTS(SELECT  TOP 1 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE [TABLE_NAME] = N'tblICItem' AND [COLUMN_NAME] = N'strBundleType')
+BEGIN
 	EXEC('
 		UPDATE [dbo].[tblICItem] SET [strType] = N''Inventory''
 		WHERE [strBundleType] = N''Bundle'' OR [strBundleType] = N''Basket'';
--- i21PreDeployment/Scripts/Pre-Deployment/11_IC/CreateTempBasketColumn.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/11_IC/CreateTempBasketColumn.sql
@@ -1,20 +1,25 @@
 ﻿PRINT N'BEGIN - Add TEMP ysnIsBasket to tblICItem'
-GO
 IF EXISTS(SELECT TOP 1 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE [TABLE_NAME] = N'tblICItem' AND [COLUMN_NAME] = N'ysnIsBasket')
 BEGIN
 	EXEC('
-		IF EXISTS(SELECT TOP 1 1 FROM tblICItem WHERE ysnIsBasket = 1)
+		IF EXISTS(SELECT TOP 1 1 FROM tblICItem WHERE strType = ''Bundle'' AND ysnIsBasket = 1)
 		BEGIN
-			ALTER TABLE [dbo].[tblICItem]
-				ADD [ysnIsBasket_TEMP] BIT NULL;
+
+			CREATE TABLE tmptblICItem (
+				[inttmptblICItemId] [INT] IDENTITY(1,1) NOT NULL,
+				[intItemId] [INT] NULL,
+				[ysnIsBasket] [BIT] NULL
+			)
+
+			INSERT INTO tmptblICItem(
+				[intItemId],
+				[ysnIsBasket]
+			)
+			SELECT intItemId,
+					ysnIsBasket
+			FROM tblICItem
+			WHERE strType = ''Bundle''
 		END
 		')
-	EXEC('
-		IF EXISTS(SELECT TOP 1 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE [TABLE_NAME] = N''tblICItem'' AND [COLUMN_NAME] = N''ysnIsBasket_TEMP'')
-		BEGIN
-			UPDATE [dbo].[tblICItem] SET [ysnIsBasket_TEMP] = [ysnIsBasket];
-		END
-	')
 END
-GO
 PRINT N'END - Add TEMP ysnIsBasket to tblICItem'
