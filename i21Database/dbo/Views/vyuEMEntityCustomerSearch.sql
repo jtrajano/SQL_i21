d1f1923d333860f84b04436c8a4fadcccf2d7827
JBorlagdan <jc.borlagdan@irely.com>
2018-01-10 10:33:20 +0800

    AR-5095

--- i21Database/dbo/Views/vyuEMEntityCustomerSearch.sql
+++ i21Database/dbo/Views/vyuEMEntityCustomerSearch.sql
@@ -4,11 +4,8 @@
 	cust.strCustomerNumber, 
 	entityToCustomer.strName AS strCustomerName,
 	entityPhone.strPhone,
-	custLocation.strAddress,
-	entityToSalesperson.strName AS strSalesPerson, 
-	custLocation.strCity,
-	custLocation.strState,
-	custLocation.strZipCode,
+	entityToSalesperson.strName AS strSalesPersonName,
+	cust.intSalespersonId,
 	custCurrency.strCurrency,
 	entityLocation.strLocationName AS strWarehouse,
 	intWarehouseId = ISNULL(entityLocation.intCompanyLocationId, -99),
@@ -21,18 +18,43 @@
 	cust.ysnActive,
 	entityContact.strMobile,
 	entityContact.strEmail,
+	entityContact.strName AS strContactName,
 	LOB.strLineOfBusiness,
 	entityClass.strClass,
 	ysnHasBudgetSetup = CAST(CASE WHEN (SELECT TOP 1 1 FROM tblARCustomerBudget WHERE intEntityCustomerId = cust.intEntityId) = 1 THEN 1 ELSE 0 END AS BIT),
 	cust.intPaymentMethodId,
-	custPaymentMethod.strPaymentMethod
-
+	custPaymentMethod.strPaymentMethod,
+	custLocation.strLocationName,
+	custLocation.strAddress,
+	custLocation.strCity,
+	custLocation.strState,
+	custLocation.strZipCode,
+	custLocation.strCountry,
+	shipLocation.strLocationName AS strShipToLocationName,
+	shipLocation.strAddress AS strShipToAddress,
+	shipLocation.strCity AS strShipToCity,
+	shipLocation.strState AS strShipToState,
+	shipLocation.strZipCode AS strShipToZipCode,
+	shipLocation.strCountry AS strShipToCountry,
+	billLocation.strLocationName AS strBillToLocationName,
+	billLocation.strAddress AS strBillToAddress,
+	billLocation.strCity AS strBillToCity,
+	billLocation.strState AS strBillToState,
+	billLocation.strZipCode AS strBillToZipCode,
+	billLocation.strCountry AS strBillToCountry,
+	cust.intShipToId,
+	cust.intBillToId,
+	strCustomerTerm = custTerm.strTerm,
+	cust.intCurrencyId,
+	cust.intTermsId
 FROM tblARCustomer cust
 INNER JOIN tblEMEntity entityToCustomer ON cust.intEntityId = entityToCustomer.intEntityId
 LEFT JOIN tblEMEntity entityToSalesperson ON cust.intSalespersonId = entityToSalesperson.intEntityId
 LEFT JOIN tblEMEntityToContact entityToContact ON entityToCustomer.intEntityId = entityToContact.intEntityId AND entityToContact.ysnDefaultContact = 1
 LEFT JOIN tblEMEntity entityContact ON entityContact.intEntityId = entityToContact.intEntityContactId AND entityToContact.ysnDefaultContact = 1
 LEFT JOIN tblEMEntityLocation custLocation ON cust.intEntityId = custLocation.intEntityId AND custLocation.ysnDefaultLocation = 1
+LEFT JOIN tblEMEntityLocation shipLocation ON cust.intShipToId = shipLocation.intEntityLocationId
+LEFT JOIN tblEMEntityLocation billLocation ON cust.intBillToId = billLocation.intEntityLocationId
 LEFT JOIN tblEMEntityPhoneNumber entityPhone ON entityToContact.intEntityContactId = entityPhone.intEntityId
 LEFT JOIN tblSMTerm entityLocationTerm ON custLocation.intTermsId = entityLocationTerm.intTermID
 LEFT JOIN tblSMCurrency custCurrency ON cust.intCurrencyId = custCurrency.intCurrencyID
@@ -45,7 +67,7 @@ LEFT JOIN tblEMEntityLineOfBusiness entityLOB ON cust.intEntityId = entityLOB.in
 LEFT JOIN tblSMLineOfBusiness LOB ON entityLOB.intLineOfBusinessId = LOB.intLineOfBusinessId
 LEFT JOIN tblEMEntityClass entityClass ON entityToCustomer.intEntityClassId = entityClass.intEntityClassId
 LEFT JOIN tblSMPaymentMethod custPaymentMethod ON cust.intPaymentMethodId = custPaymentMethod.intPaymentMethodID
-
+LEFT JOIN tblSMTerm custTerm ON cust.intTermsId = custTerm.intTermID
 WHERE		
 		entityType.Customer = 1 -- check if entity is a customer
 		OR custInvoice.dtmDate = (SELECT MAX(dtmDate) FROM tblARInvoice x WHERE x.intEntityCustomerId = x.intEntityCustomerId)
@@ -61,7 +83,7 @@ GROUP BY
 	custCurrency.strCurrency,
 	entityLocation.strLocationName,
 	entityLocation.intCompanyLocationId,
-	intWarehouseId,
+	custLocation.intWarehouseId,
 	cust.dblCreditLimit,
 	entityLocationPricingLevel.strPricingLevelName,
 	entityToCustomer.dtmOriginationDate,
@@ -70,8 +92,33 @@ GROUP BY
 	entityPhone.strPhone,
 	entityContact.strMobile,
 	entityContact.strEmail,
+	entityContact.strName,
 	LOB.strLineOfBusiness,
 	entityClass.strClass,
 	custPaymentMethod.strPaymentMethod,
-	cust.intPaymentMethodId
+	cust.intPaymentMethodId,
+	custLocation.strLocationName,
+	custLocation.strAddress,
+	custLocation.strCity,
+	custLocation.strState,
+	custLocation.strZipCode,
+	custLocation.strCountry,
+	shipLocation.strLocationName,
+	shipLocation.strAddress,
+	shipLocation.strCity,
+	shipLocation.strState,
+	shipLocation.strZipCode,
+	shipLocation.strCountry,
+	billLocation.strLocationName,
+	billLocation.strAddress,
+	billLocation.strCity,
+	billLocation.strState,
+	billLocation.strZipCode,
+	billLocation.strCountry,
+	cust.intShipToId,
+	cust.intBillToId,
+	custTerm.strTerm,
+	cust.intSalespersonId,
+	cust.intCurrencyId,
+	cust.intTermsId
 GO
