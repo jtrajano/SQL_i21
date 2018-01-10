1518f0002600b0f1193662cc8a5dc9acf605d6c2
Lawrence Zabala <lawrence.zabala@irely.com>
2018-01-10 12:00:44 +0800

    MFT-827

--- i21Database/dbo/Tables/tblTFTaxAuthorityBeginEndInventory.sql
+++ i21Database/dbo/Tables/tblTFTaxAuthorityBeginEndInventory.sql
@@ -1,10 +1,13 @@
 ﻿CREATE TABLE [dbo].[tblTFTaxAuthorityBeginEndInventory]
 (
-	[intTaxAuthorityBeginEndInventoryId] INT NOT NULL, 
+	[intTaxAuthorityBeginEndInventoryId] INT NOT NULL IDENTITY, 
     [intTaxAuthorityId] INT NOT NULL, 
-    [dtmBeginDate] DATETIME NOT NULL, 
-    [dtmEndDate] DATETIME NOT NULL, 
+	[intProductCodeId] INT NOT NULL, 
+    [dblBeginInventory] NUMERIC(18, 6) NULL DEFAULT ((0)), 
+    [dblEndInventory] NUMERIC(18, 6) NULL DEFAULT ((0)), 
     [intConcurrencyId] INT NULL DEFAULT ((1)), 
     CONSTRAINT [PK_tblTFTaxAuthorityBeginEndInventory] PRIMARY KEY ([intTaxAuthorityBeginEndInventoryId]), 
-    CONSTRAINT [FK_tblTFTaxAuthorityBeginEndInventory_tblTFTaxAuthority] FOREIGN KEY ([intTaxAuthorityId]) REFERENCES [tblTFTaxAuthority]([intTaxAuthorityId]) 
+    CONSTRAINT [FK_tblTFTaxAuthorityBeginEndInventory_tblTFTaxAuthority] FOREIGN KEY ([intTaxAuthorityId]) REFERENCES [tblTFTaxAuthority]([intTaxAuthorityId]), 
+    CONSTRAINT [AK_tblTFTaxAuthorityBeginEndInventory] UNIQUE ([intTaxAuthorityId], [intProductCodeId]), 
+    CONSTRAINT [FK_tblTFTaxAuthorityBeginEndInventory_tblTFProductCode] FOREIGN KEY ([intProductCodeId]) REFERENCES [tblTFProductCode]([intProductCodeId]) 
 )
