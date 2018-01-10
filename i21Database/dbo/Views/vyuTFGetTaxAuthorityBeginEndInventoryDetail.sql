fafc5c66ec907effad1174b9228c9cf3eea1ee54
Lawrence Zabala <lawrence.zabala@irely.com>
2018-01-10 12:00:44 +0800

    MFT-827

---
+++ i21Database/dbo/Views/vyuTFGetTaxAuthorityBeginEndInventoryDetail.sql
@@ -0,0 +1,19 @@
+﻿CREATE VIEW [dbo].[vyuTFGetTaxAuthorityBeginEndInventory]
+
+AS
+
+SELECT Detail.intTaxAuthorityBeginEndInventoryId
+	, Detail.intTaxAuthorityId
+	, TA.strTaxAuthorityCode
+	, strTADescription = TA.strDescription
+	, TA.dtmBeginDate
+	, TA.dtmEndDate
+	, Detail.intProductCodeId
+	, PC.strProductCode
+	, strProductCodeDescription = PC.strDescription
+	, Detail.dblBeginInventory
+	, Detail.dblEndInventory
+	, Detail.intConcurrencyId
+FROM tblTFTaxAuthorityBeginEndInventory Detail
+LEFT JOIN tblTFProductCode PC ON PC.intProductCodeId = Detail.intProductCodeId AND Detail.intTaxAuthorityId = PC.intTaxAuthorityId
+LEFT JOIN tblTFTaxAuthority TA ON TA.intTaxAuthorityId = Detail.intTaxAuthorityId
