f8bbdd6b1b62a435ce53a73154e773df4301f52c
Robin Panganiban <robin.panganiban@irely.com>
2018-01-11 16:09:47 +0800

    AR-4544

--- i21Database/dbo/Views/vyuEMEntityCustomerSearch.sql
+++ i21Database/dbo/Views/vyuEMEntityCustomerSearch.sql
@@ -47,7 +47,8 @@
 	cust.intBillToId,
 	strCustomerTerm = custTerm.strTerm,
 	cust.intCurrencyId,
-	cust.intTermsId
+	cust.intTermsId,
+	STUFF((SELECT '|^|' + CONVERT(VARCHAR,intLineOfBusinessId) FROM tblEMEntityLineOfBusiness te WHERE te.intEntityId = cust.intEntityId FOR XML PATH('')),1,3,'') as intLineOfBusinessIds
 FROM tblARCustomer cust
 INNER JOIN tblEMEntity entityToCustomer ON cust.intEntityId = entityToCustomer.intEntityId
 LEFT JOIN tblEMEntity entityToSalesperson ON cust.intSalespersonId = entityToSalesperson.intEntityId
