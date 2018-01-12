6b1ff41631116eac04d750d33fe3c03cd82108a7
Liz Dorado <liz.dorado@irely.com>
2018-01-12 20:41:26 +0800

    Revert "AR-5314"
    This reverts commit 089cfe4cb42808c862717fc533bc48fa3ea90f60.

--- i21Database/dbo/Tables/tblARInvoiceDetailTax.sql
+++ i21Database/dbo/Tables/tblARInvoiceDetailTax.sql
@@ -19,13 +19,11 @@
 	[ysnTaxExempt]              BIT NULL DEFAULT ((0)), 
 	[ysnTaxOnly]				BIT	NOT NULL DEFAULT 0,
 	[strNotes]                  NVARCHAR(500) COLLATE Latin1_General_CI_AS NULL, 
-	[intUnitMeasureId]			INT NULL,
     [intConcurrencyId]          INT CONSTRAINT [DF_tblARInvoiceDetailTax_intConcurrencyId] DEFAULT ((0)) NOT NULL,
 	CONSTRAINT [PK_tblARInvoiceDetailTax_intInvoiceDetailTaxId] PRIMARY KEY CLUSTERED ([intInvoiceDetailTaxId] ASC),
 	CONSTRAINT [FK_tblARInvoiceDetailTax_tblARInvoiceDetail_intInvoiceDetailId] FOREIGN KEY ([intInvoiceDetailId]) REFERENCES [dbo].[tblARInvoiceDetail] ([intInvoiceDetailId]) ON DELETE CASCADE,
 	CONSTRAINT [FK_tblARInvoiceDetailTax_tblSMTaxGroup_intTaxGroupId] FOREIGN KEY ([intTaxGroupId]) REFERENCES [dbo].[tblSMTaxGroup] ([intTaxGroupId]),
 	CONSTRAINT [FK_tblARInvoiceDetailTax_tblSMTaxCode_intTaxCodeId] FOREIGN KEY ([intTaxCodeId]) REFERENCES [dbo].[tblSMTaxCode] ([intTaxCodeId]),
 	CONSTRAINT [FK_tblARInvoiceDetailTax_tblSMTaxClass_intTaxClassId] FOREIGN KEY ([intTaxClassId]) REFERENCES [dbo].[tblSMTaxClass] ([intTaxClassId]),
-	CONSTRAINT [FK_tblARInvoiceDetailTax_tblGLAccount_intSalesTaxAccountId] FOREIGN KEY ([intSalesTaxAccountId]) REFERENCES [dbo].[tblGLAccount] ([intAccountId]),
-	CONSTRAINT [FK_tblARInvoiceDetailTax_tblICUnitMeasure_intUnitMeasureId] FOREIGN KEY ([intUniteMeasureId]) REFERENCES [dbo].[tblICUnitMeasure] ([intUnitMeasureId])
+	CONSTRAINT [FK_tblARInvoiceDetailTax_tblGLAccount_intSalesTaxAccountId] FOREIGN KEY ([intSalesTaxAccountId]) REFERENCES [dbo].[tblGLAccount] ([intAccountId])
 )
