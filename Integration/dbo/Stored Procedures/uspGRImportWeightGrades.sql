f93384826af3d1f0615edeac99859006f8c6d20b
IRELY\kkarthick <karthick.k@irely.com>
2018-01-12 16:19:39 +0530

    GRN-690

--- Integration/dbo/Stored Procedures/uspGRImportWeightGrades.sql
+++ Integration/dbo/Stored Procedures/uspGRImportWeightGrades.sql
@@ -13,42 +13,244 @@ BEGIN
 
 	IF (@Checking = 1)
 	BEGIN
-			 SELECT @Total = COUNT(*) FROM gactlmst s
-		UNPIVOT
-		(
-		  strWeightGradeDesc
-		  FOR records IN (gact5_grd_wgt_desc_1, gact5_grd_wgt_desc_2, gact5_grd_wgt_desc_3, gact5_grd_wgt_desc_4, gact5_grd_wgt_desc_5, gact5_grd_wgt_desc_6, gact5_grd_wgt_desc_7, gact5_grd_wgt_desc_8, gact5_grd_wgt_desc_9)
-		) x
-		WHERE NOT EXISTS (SELECT strWeightGradeDesc from tblCTWeightGrade
-		WHERE  strWeightGradeDesc COLLATE SQL_Latin1_General_CP1_CS_AS = x.strWeightGradeDesc COLLATE SQL_Latin1_General_CP1_CS_AS)		
+			 SELECT @Total = COUNT(*) FROM gactlmst WHERE gactl_key = '5' AND gact5_grd_wgt_desc_1 != '            '
+				AND NOT EXISTS (SELECT strWeightGradeDesc from tblCTWeightGrade WHERE  strWeightGradeDesc COLLATE SQL_Latin1_General_CP1_CS_AS = gact5_grd_wgt_desc_1 COLLATE SQL_Latin1_General_CP1_CS_AS)
+			 SELECT @Total = @Total + COUNT(*)  FROM gactlmst  WHERE gactl_key = '5' AND gact5_grd_wgt_desc_2 != '            '
+				AND NOT EXISTS (SELECT strWeightGradeDesc from tblCTWeightGrade WHERE  strWeightGradeDesc COLLATE SQL_Latin1_General_CP1_CS_AS = gact5_grd_wgt_desc_2 COLLATE SQL_Latin1_General_CP1_CS_AS)
+			 SELECT @Total = @Total + COUNT(*)  FROM gactlmst  WHERE gactl_key = '5' AND gact5_grd_wgt_desc_3 != '            '
+				AND NOT EXISTS (SELECT strWeightGradeDesc from tblCTWeightGrade WHERE  strWeightGradeDesc COLLATE SQL_Latin1_General_CP1_CS_AS = gact5_grd_wgt_desc_3 COLLATE SQL_Latin1_General_CP1_CS_AS)
+			 SELECT @Total = @Total + COUNT(*)  FROM gactlmst  WHERE gactl_key = '5' AND gact5_grd_wgt_desc_4 != '            '
+				AND NOT EXISTS (SELECT strWeightGradeDesc from tblCTWeightGrade WHERE  strWeightGradeDesc COLLATE SQL_Latin1_General_CP1_CS_AS = gact5_grd_wgt_desc_4 COLLATE SQL_Latin1_General_CP1_CS_AS)
+			 SELECT @Total = @Total + COUNT(*)  FROM gactlmst  WHERE gactl_key = '5' AND gact5_grd_wgt_desc_5 != '            '
+				AND NOT EXISTS (SELECT strWeightGradeDesc from tblCTWeightGrade WHERE  strWeightGradeDesc COLLATE SQL_Latin1_General_CP1_CS_AS = gact5_grd_wgt_desc_5 COLLATE SQL_Latin1_General_CP1_CS_AS)
+			 SELECT @Total = @Total + COUNT(*)  FROM gactlmst  WHERE gactl_key = '5' AND gact5_grd_wgt_desc_6 != '            '
+				AND NOT EXISTS (SELECT strWeightGradeDesc from tblCTWeightGrade WHERE  strWeightGradeDesc COLLATE SQL_Latin1_General_CP1_CS_AS = gact5_grd_wgt_desc_6 COLLATE SQL_Latin1_General_CP1_CS_AS)
+			 SELECT @Total = @Total + COUNT(*)  FROM gactlmst  WHERE gactl_key = '5' AND gact5_grd_wgt_desc_7 != '            '
+				AND NOT EXISTS (SELECT strWeightGradeDesc from tblCTWeightGrade WHERE  strWeightGradeDesc COLLATE SQL_Latin1_General_CP1_CS_AS = gact5_grd_wgt_desc_7 COLLATE SQL_Latin1_General_CP1_CS_AS)
+			 SELECT @Total = @Total + COUNT(*)  FROM gactlmst  WHERE gactl_key = '5' AND gact5_grd_wgt_desc_8 != '            '
+				AND NOT EXISTS (SELECT strWeightGradeDesc from tblCTWeightGrade WHERE  strWeightGradeDesc COLLATE SQL_Latin1_General_CP1_CS_AS = gact5_grd_wgt_desc_8 COLLATE SQL_Latin1_General_CP1_CS_AS)
+			 SELECT @Total = @Total + COUNT(*)  FROM gactlmst  WHERE gactl_key = '5' AND gact5_grd_wgt_desc_9 != '            '
+				AND NOT EXISTS (SELECT strWeightGradeDesc from tblCTWeightGrade WHERE  strWeightGradeDesc COLLATE SQL_Latin1_General_CP1_CS_AS = gact5_grd_wgt_desc_9 COLLATE SQL_Latin1_General_CP1_CS_AS)		
 			 
 			 RETURN @Total
 	END
 
-	INSERT INTO tblCTWeightGrade(intConcurrencyId,strWeightGradeDesc,strWhereFinalized,ysnActive,ysnWeight,ysnGrade)
-	SELECT 1,y.strWeightGradeDesc,CASE WHEN t.strOriginDest = 'D' THEN 'Designation' ELSE 'Origin' END,1,1,1 
-	FROM(
-		SELECT  SUBSTRING(x.records,LEN(x.records),LEN(x.records)-1) id, x.strWeightGradeDesc
-		FROM gactlmst s
-		UNPIVOT
-		(
-		  strWeightGradeDesc
-		  FOR records in (gact5_grd_wgt_desc_1, gact5_grd_wgt_desc_2, gact5_grd_wgt_desc_3, gact5_grd_wgt_desc_4, gact5_grd_wgt_desc_5, gact5_grd_wgt_desc_6, gact5_grd_wgt_desc_7, gact5_grd_wgt_desc_8, gact5_grd_wgt_desc_9)
-		) x
-	)y
-	JOIN(
-		SELECT  SUBSTRING(u.records,LEN(u.records),LEN(u.records)-1) id, u.strOriginDest
-		FROM gactlmst s
-		UNPIVOT
-		(
-		  strOriginDest
-		  FOR records in (gact5_grd_wgt_type_1, gact5_grd_wgt_type_2, gact5_grd_wgt_type_3, gact5_grd_wgt_type_4, gact5_grd_wgt_type_5, gact5_grd_wgt_type_6, gact5_grd_wgt_type_7, gact5_grd_wgt_type_8, gact5_grd_wgt_type_9)
-		) u
-	) t ON t.id = y.id
-	WHERE NOT EXISTS (SELECT strWeightGradeDesc from tblCTWeightGrade
-	WHERE  strWeightGradeDesc COLLATE SQL_Latin1_General_CP1_CS_AS = y.strWeightGradeDesc COLLATE SQL_Latin1_General_CP1_CS_AS)	
-	ORDER BY y.id ASC	
+	INSERT INTO tblCTWeightGrade (
+	intConcurrencyId
+	,strWeightGradeDesc
+	,strWhereFinalized
+	,ysnActive
+	,ysnWeight
+	,ysnGrade
+	)
+SELECT 1
+	,gact5_grd_wgt_desc_1
+	,CASE 
+		WHEN gact5_grd_wgt_type_1 = 'D'
+			THEN 'Designation'
+		ELSE 'Origin'
+		END
+	,1
+	,1
+	,1
+FROM gactlmst
+WHERE gactl_key = '5'
+	AND gact5_grd_wgt_desc_1 != '            '
+	AND NOT EXISTS (SELECT strWeightGradeDesc FROM tblCTWeightGrade
+		WHERE strWeightGradeDesc COLLATE SQL_Latin1_General_CP1_CS_AS = gact5_grd_wgt_desc_1 COLLATE SQL_Latin1_General_CP1_CS_AS)
+
+	INSERT INTO tblCTWeightGrade (
+	intConcurrencyId
+	,strWeightGradeDesc
+	,strWhereFinalized
+	,ysnActive
+	,ysnWeight
+	,ysnGrade
+	)
+SELECT 1
+	,gact5_grd_wgt_desc_2
+	,CASE 
+		WHEN gact5_grd_wgt_type_2 = 'D'
+			THEN 'Designation'
+		ELSE 'Origin'
+		END
+	,1
+	,1
+	,1
+FROM gactlmst
+WHERE gactl_key = '5'
+	AND gact5_grd_wgt_desc_2 != '            '
+	AND NOT EXISTS (SELECT strWeightGradeDesc FROM tblCTWeightGrade
+		WHERE strWeightGradeDesc COLLATE SQL_Latin1_General_CP1_CS_AS = gact5_grd_wgt_desc_2 COLLATE SQL_Latin1_General_CP1_CS_AS)
 		
+	INSERT INTO tblCTWeightGrade (
+	intConcurrencyId
+	,strWeightGradeDesc
+	,strWhereFinalized
+	,ysnActive
+	,ysnWeight
+	,ysnGrade
+	)
+SELECT 1
+	,gact5_grd_wgt_desc_3
+	,CASE 
+		WHEN gact5_grd_wgt_type_3 = 'D'
+			THEN 'Designation'
+		ELSE 'Origin'
+		END
+	,1
+	,1
+	,1
+FROM gactlmst
+WHERE gactl_key = '5'
+	AND gact5_grd_wgt_desc_3 != '            '
+	AND NOT EXISTS (SELECT strWeightGradeDesc FROM tblCTWeightGrade
+		WHERE strWeightGradeDesc COLLATE SQL_Latin1_General_CP1_CS_AS = gact5_grd_wgt_desc_3 COLLATE SQL_Latin1_General_CP1_CS_AS)
+
+	INSERT INTO tblCTWeightGrade (
+	intConcurrencyId
+	,strWeightGradeDesc
+	,strWhereFinalized
+	,ysnActive
+	,ysnWeight
+	,ysnGrade
+	)
+SELECT 1
+	,gact5_grd_wgt_desc_4
+	,CASE 
+		WHEN gact5_grd_wgt_type_4 = 'D'
+			THEN 'Designation'
+		ELSE 'Origin'
+		END
+	,1
+	,1
+	,1
+FROM gactlmst
+WHERE gactl_key = '5'
+	AND gact5_grd_wgt_desc_4 != '            '
+	AND NOT EXISTS (SELECT strWeightGradeDesc FROM tblCTWeightGrade
+		WHERE strWeightGradeDesc COLLATE SQL_Latin1_General_CP1_CS_AS = gact5_grd_wgt_desc_4 COLLATE SQL_Latin1_General_CP1_CS_AS)		
+		
+	INSERT INTO tblCTWeightGrade (
+	intConcurrencyId
+	,strWeightGradeDesc
+	,strWhereFinalized
+	,ysnActive
+	,ysnWeight
+	,ysnGrade
+	)
+SELECT 1
+	,gact5_grd_wgt_desc_5
+	,CASE 
+		WHEN gact5_grd_wgt_type_5 = 'D'
+			THEN 'Designation'
+		ELSE 'Origin'
+		END
+	,1
+	,1
+	,1
+FROM gactlmst
+WHERE gactl_key = '5'
+	AND gact5_grd_wgt_desc_5 != '            '
+	AND NOT EXISTS (SELECT strWeightGradeDesc FROM tblCTWeightGrade
+		WHERE strWeightGradeDesc COLLATE SQL_Latin1_General_CP1_CS_AS = gact5_grd_wgt_desc_5 COLLATE SQL_Latin1_General_CP1_CS_AS)		
+
+	INSERT INTO tblCTWeightGrade (
+	intConcurrencyId
+	,strWeightGradeDesc
+	,strWhereFinalized
+	,ysnActive
+	,ysnWeight
+	,ysnGrade
+	)
+SELECT 1
+	,gact5_grd_wgt_desc_6
+	,CASE 
+		WHEN gact5_grd_wgt_type_6 = 'D'
+			THEN 'Designation'
+		ELSE 'Origin'
+		END
+	,1
+	,1
+	,1
+FROM gactlmst
+WHERE gactl_key = '5'
+	AND gact5_grd_wgt_desc_6 != '            '
+	AND NOT EXISTS (SELECT strWeightGradeDesc FROM tblCTWeightGrade
+		WHERE strWeightGradeDesc COLLATE SQL_Latin1_General_CP1_CS_AS = gact5_grd_wgt_desc_6 COLLATE SQL_Latin1_General_CP1_CS_AS)		
+
+	INSERT INTO tblCTWeightGrade (
+	intConcurrencyId
+	,strWeightGradeDesc
+	,strWhereFinalized
+	,ysnActive
+	,ysnWeight
+	,ysnGrade
+	)
+SELECT 1
+	,gact5_grd_wgt_desc_7
+	,CASE 
+		WHEN gact5_grd_wgt_type_7 = 'D'
+			THEN 'Designation'
+		ELSE 'Origin'
+		END
+	,1
+	,1
+	,1
+FROM gactlmst
+WHERE gactl_key = '5'
+	AND gact5_grd_wgt_desc_7 != '            '
+	AND NOT EXISTS (SELECT strWeightGradeDesc FROM tblCTWeightGrade
+		WHERE strWeightGradeDesc COLLATE SQL_Latin1_General_CP1_CS_AS = gact5_grd_wgt_desc_7 COLLATE SQL_Latin1_General_CP1_CS_AS)	
+
+	INSERT INTO tblCTWeightGrade (
+	intConcurrencyId
+	,strWeightGradeDesc
+	,strWhereFinalized
+	,ysnActive
+	,ysnWeight
+	,ysnGrade
+	)
+SELECT 1
+	,gact5_grd_wgt_desc_8
+	,CASE 
+		WHEN gact5_grd_wgt_type_8 = 'D'
+			THEN 'Designation'
+		ELSE 'Origin'
+		END
+	,1
+	,1
+	,1
+FROM gactlmst
+WHERE gactl_key = '5'
+	AND gact5_grd_wgt_desc_8 != '            '
+	AND NOT EXISTS (SELECT strWeightGradeDesc FROM tblCTWeightGrade
+		WHERE strWeightGradeDesc COLLATE SQL_Latin1_General_CP1_CS_AS = gact5_grd_wgt_desc_8 COLLATE SQL_Latin1_General_CP1_CS_AS)		
+		
+	INSERT INTO tblCTWeightGrade (
+	intConcurrencyId
+	,strWeightGradeDesc
+	,strWhereFinalized
+	,ysnActive
+	,ysnWeight
+	,ysnGrade
+	)
+SELECT 1
+	,gact5_grd_wgt_desc_9
+	,CASE 
+		WHEN gact5_grd_wgt_type_9 = 'D'
+			THEN 'Designation'
+		ELSE 'Origin'
+		END
+	,1
+	,1
+	,1
+FROM gactlmst
+WHERE gactl_key = '5'
+	AND gact5_grd_wgt_desc_9 != '            '
+	AND NOT EXISTS (SELECT strWeightGradeDesc FROM tblCTWeightGrade
+		WHERE strWeightGradeDesc COLLATE SQL_Latin1_General_CP1_CS_AS = gact5_grd_wgt_desc_9 COLLATE SQL_Latin1_General_CP1_CS_AS)		
+	
 END	
 
 
