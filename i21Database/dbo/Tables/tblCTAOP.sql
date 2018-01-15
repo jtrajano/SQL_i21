145df976dc066051c93fbbb48b2d8682586cd53d
Satya S Sahoo <satya.sahoo@irely.com>
2018-01-15 16:34:15 +0530

    CT-1843

--- i21Database/dbo/Tables/tblCTAOP.sql
+++ i21Database/dbo/Tables/tblCTAOP.sql
@@ -2,7 +2,13 @@
 (
 	intAOPId int IDENTITY(1,1) NOT NULL,
 	strYear nvarchar(100) COLLATE Latin1_General_CI_AS  NOT NULL,
+	dtmFromDate DATETIME,
+	dtmToDate DATETIME,
+	intBookId INT,
+	intSubBookId INT,	
 	intConcurrencyId INT NOT NULL, 
 	CONSTRAINT PK_tblCTAOP_intAOPId PRIMARY KEY CLUSTERED (intAOPId ASC),
-	CONSTRAINT UQ_tblCTAOP_strYear UNIQUE (strYear)
+	CONSTRAINT [FK_tblCTAOP_tblCTBook_intBookId] FOREIGN KEY ([intBookId]) REFERENCES [tblCTBook]([intBookId]),
+	CONSTRAINT [FK_tblCTAOP_tblCTSubBook_intSubBookId] FOREIGN KEY ([intSubBookId]) REFERENCES [tblCTSubBook]([intSubBookId]),
+	CONSTRAINT UQ_tblCTAOP_strYear_dtmFromDate_dtmTodate UNIQUE (strYear,dtmFromDate,dtmToDate)
 )
---
+++ i21Database/dbo/Views/vyuCTSearchAOP.sql
@@ -0,0 +1,11 @@
+﻿CREATE VIEW [dbo].[vyuCTSearchAOP]
+
+AS
+
+		SELECT		AO.*,
+					BK.strBook,
+					SB.strSubBook
+
+			FROM	tblCTAOP		AO
+	LEFT	JOIN	tblCTBook		BK  ON	BK.intBookId	=	AO.intBookId
+	LEFT	JOIN	tblCTSubBook	SB  ON	SB.intSubBookId	=	AO.intSubBookId
