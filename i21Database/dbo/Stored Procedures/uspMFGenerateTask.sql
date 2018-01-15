931c96af5dbb9740a9cb9675b26e5c98fa114ff4
Premkumar S <prem.selladurai@irely.com>
2018-01-15 17:49:08 +0530

    MFG-3197

--- i21Database/dbo/Stored Procedures/uspMFGenerateTask.sql
+++ i21Database/dbo/Stored Procedures/uspMFGenerateTask.sql
@@ -1134,7 +1134,8 @@ BEGIN TRY
 				)
 	END
 
-	IF @strOrderType = 'INVENTORY SHIPMENT STAGING' and @intInventoryShipmentId IS NOT NULL
+	IF @strOrderType = 'INVENTORY SHIPMENT STAGING'
+		AND @intInventoryShipmentId IS NOT NULL
 	BEGIN
 		SELECT @intTransactionId = @intInventoryShipmentId
 
@@ -1216,6 +1217,17 @@ BEGIN TRY
 		EXEC dbo.uspICCreateStockReservation @ItemsToReserve
 			,@intOrderId
 			,34
+	END
+
+	IF EXISTS (
+			SELECT 1
+			FROM tblMFTask
+			WHERE intOrderHeaderId = @intOrderHeaderId
+			)
+	BEGIN
+		UPDATE tblMFOrderHeader
+		SET intOrderStatusId = 2
+		WHERE intOrderHeaderId = @intOrderHeaderId
 	END
 END TRY
 
