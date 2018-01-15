95eb83e4fe303724f3850dbac29213f23d4259e3
Premkumar S <prem.selladurai@irely.com>
2018-01-15 17:37:58 +0530

    MFG-3191

--- i21Database/dbo/Tables/tblMFWorkOrderInputLot.sql
+++ i21Database/dbo/Tables/tblMFWorkOrderInputLot.sql
@@ -26,6 +26,7 @@
 	intBusinessShiftId int NULL,
 	dtmProductionDate datetime NULL,
 	[intBatchId] INT NULL, 
+	intDestinationLotId int NULL,
     CONSTRAINT [PK_tblMFWorkOrderInputLot_intWorkOrderInputLotId] PRIMARY KEY ([intWorkOrderInputLotId]),
 	CONSTRAINT [FK_tblMFWorkOrderInputLot_tblMFWorkOrder_intWorkOrderId] FOREIGN KEY ([intWorkOrderId]) REFERENCES [tblMFWorkOrder]([intWorkOrderId]) ON DELETE CASCADE,
 	CONSTRAINT [FK_tblMFWorkOrderInputLot_tblICItem_inItemId] FOREIGN KEY ([intItemId]) REFERENCES [tblICItem]([intItemId]),
