affa3aa0f775cbec996bc9ad7c5b8dd41d599e16
jessa delo-ag <jessa.delo-ag@irely.com>
2018-01-16 17:07:21 +0800

    RPT-1109 - Language Update

--- i21Database/dbo/Tables/tblSMLanguage.sql
+++ i21Database/dbo/Tables/tblSMLanguage.sql
@@ -2,9 +2,6 @@
 (
 	[intLanguageId]				INT	 PRIMARY KEY IDENTITY (1, 1) NOT NULL,
     [strLanguage]				NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL, 
-	[intReportLabelId]			INT  NULL,
 	[ysnDefault]				BIT  NULL,
-    [intConcurrencyId]			INT	 NOT NULL DEFAULT (1), 
-    CONSTRAINT [AK_tblSMLanguage_strLanguage_intReportLabelId] UNIQUE ([strLanguage], [intReportLabelId]), 
-    CONSTRAINT [FK_tblSMLanguage_ToTable] FOREIGN KEY ([intReportLabelId]) REFERENCES [tblSMReportLabels]([intReportLabelsId]) 
+    [intConcurrencyId]			INT	 NOT NULL DEFAULT (1) 
 )
