7fe6796b445765f96782d3116a6990d626365040
Lawrence Zabala <lawrence.zabala@irely.com>
2018-01-17 14:29:02 +0800

    Fix merge conflicts on MFT

---
+++ i21Database/dbo/Views/vyuTFGetReportingComponentField.sql
@@ -0,0 +1,12 @@
+﻿CREATE VIEW [dbo].[vyuTFGetReportingComponentField]
+	AS
+	
+SELECT RCField.intReportingComponentFieldId
+	, RCField.intReportingComponentId
+	, RCField.strColumn
+	, RCField.strCaption
+	, RCField.strFormat
+	, RCField.strFooter
+	, RCField.intWidth
+	, RCField.intMasterId
+FROM tblTFReportingComponentField RCField
