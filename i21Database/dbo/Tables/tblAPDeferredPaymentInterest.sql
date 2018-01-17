fdebe82f95b7b0d01b49bf442a84889e585eb3ec
Erick <erick.delacruz@irely.com>
2018-01-18 04:59:53 +0800

    AP-4584, AP-4581, AP-4400

---
+++ i21Database/dbo/Tables/tblAPDeferredPaymentInterest.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[tblAPDeferredPaymentInterest]
+(
+	[intDeferredPaymentInterestId] INT NOT NULL PRIMARY KEY, 
+    [dtmCalculationDate] DATETIME NULL, 
+    [dtmPaymentPostDate] DATETIME NULL, 
+    [dtmPaymentInvoiceDate] DATETIME NULL, 
+    [dtmPaymentDueDateOverride] DATETIME NULL, 
+    [strTerm] NVARCHAR(100) NULL, 
+    [dblMinimum] DECIMAL(18, 6) NOT NULL DEFAULT 0
+)
---
+++ i21Database/dbo/Views/vyuAPDeferredPayment.sql
@@ -0,0 +1,16 @@
+﻿CREATE VIEW [dbo].[vyuAPDeferredPayment]
+AS
+
+SELECT
+	A.intBillId,
+	ISNULL(commodity.strCommodityCode, 'None') AS strCommodityCode,
+	A.strBillId,
+	B.strName,
+	A.dtmBillDate,
+	A.dtmDate,
+	term.strTerm,
+	A.dtmDueDate
+FROM tblAPBill A
+INNER JOIN tblEMEntity B ON A.intEntityVendorId = B.intEntityId
+CROSS APPLY [dbo].[fnAPGetVoucherCommodity](A.intBillId) commodity
+LEFT JOIN tblSMTerm term ON A.intTermsId = term.intTermID
