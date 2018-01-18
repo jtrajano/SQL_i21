64f014a5f0b5f6d7cf703172a050f1b23767cc2d
COMTECH\RRiguer <ruzel.riguer@irely.com>
2018-01-18 14:54:34 +0800

    SM-3429

---
+++ i21Database/dbo/Views/vyuSMRecurringTransaction.sql
@@ -0,0 +1,33 @@
+﻿CREATE VIEW [dbo].[vyuSMRecurringTransaction]
+
+AS
+
+SELECT 
+	   rectrans.intRecurringId
+	  ,ent.strName
+	  ,'' as strAssignedUser
+      ,rectrans.intTransactionId
+      ,rectrans.strTransactionNumber
+      ,rectrans.strTransactionType
+      ,rectrans.strReference
+      ,rectrans.strResponsibleUser
+      ,rectrans.intEntityId
+      ,rectrans.intWarningDays
+      ,rectrans.strFrequency
+      ,rectrans.dtmLastProcess
+      ,rectrans.dtmNextProcess
+      ,rectrans.ysnDue
+      ,rectrans.strRecurringGroup
+      ,rectrans.strDayOfMonth
+      ,rectrans.dtmStartDate
+      ,rectrans.dtmEndDate
+      ,rectrans.ysnActive
+      ,rectrans.intIteration
+      ,rectrans.intUserId
+      ,rectrans.ysnAvailable
+      ,rectrans.dtmPreviousLastProcess
+      ,rectrans.dtmPreviousNextProcess
+      ,rectrans.intConcurrencyId
+	FROM tblSMRecurringTransaction rectrans
+	LEFT JOIN tblARInvoice inv ON rectrans.strTransactionNumber = inv.strInvoiceNumber
+	LEFT JOIN tblEMEntity ent ON inv.intEntityCustomerId  = ent.intEntityId
