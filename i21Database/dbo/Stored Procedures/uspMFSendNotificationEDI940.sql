8c7c6a7d1ef97e8641c1125f5c0ba51cfd0dd611
Premkumar S <prem.selladurai@irely.com>
2018-01-18 20:40:15 +0530

    MFG-3227

---
+++ i21Database/dbo/Stored Procedures/uspMFSendNotificationEDI940.sql
@@ -0,0 +1,59 @@
+﻿CREATE PROCEDURE uspMFSendNotificationEDI940
+AS
+BEGIN
+	IF NOT EXISTS (
+			SELECT *
+			FROM tblMFEDI940Archive
+			WHERE ysnNotify = 1
+				AND IsNULL(ysnSentEMail, 0) = 0
+			)
+	BEGIN
+		RAISERROR (
+				'No data.'
+				,16
+				,1
+				)
+
+		RETURN
+	END
+
+	SELECT intTransactionId
+		,strCustomerId
+		,strPurpose
+		,strDepositorOrderNumber
+		,strPONumber
+		,strShipToName
+		,strShipToAddress1
+		,strShipToAddress2
+		,strShipToCity
+		,strShipToState
+		,strShipToZip
+		,strShipToCode
+		,strBuyerIdentification
+		,strPODate
+		,strDeliveryRequestedDate
+		,intLineNumber
+		,strCustomerItemNumber
+		,strUPCCaseCode
+		,strDescription
+		,dblQtyOrdered
+		,strUOM
+		,dblInnerPacksPerOuterPack
+		,dblTotalQtyOrdered
+		,dtmCreated
+		,strFileName
+		,strShipmentDate
+		,strTransportationMethod
+		,strSCAC
+		,strRouting
+		,strShipmentMethodOfPayment
+		,strCustomerCode
+	FROM tblMFEDI940Archive
+	WHERE ysnNotify = 1
+		AND IsNULL(ysnSentEMail, 0) = 0
+
+	UPDATE tblMFEDI940Archive
+	SET ysnSentEMail = 1
+	WHERE ysnNotify = 1
+		AND IsNULL(ysnSentEMail, 0) = 0
+END
