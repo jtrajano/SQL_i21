8828da1ba1978c09ec4d0677541c6adebcac3613
jessa delo-ag <jessa.delo-ag@irely.com>
2018-01-18 16:57:06 +0800

    RPT-1109 - Report label update

--- i21Database/dbo/Tables/tblSMReportLabels.sql
+++ i21Database/dbo/Tables/tblSMReportLabels.sql
@@ -2,5 +2,7 @@
 (
 	[intReportLabelsId] INT NOT NULL PRIMARY KEY IDENTITY, 
     [strName] NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL, 
-    [intConcurrencyId] INT NOT NULL DEFAULT 1
+	[intLanguageId]				INT  NOT NULL,
+    [intConcurrencyId] INT NOT NULL DEFAULT 1,
+	CONSTRAINT [FK_tblSMReportLabels_tblSMLanguage] FOREIGN KEY ([intLanguageId]) REFERENCES [tblSMLanguage]([intLanguageId])
 )
