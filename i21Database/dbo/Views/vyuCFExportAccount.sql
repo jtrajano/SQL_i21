254199212b2d4cdc363398a11284a6d63b0767d6
bonn ponce <bonn.ponce@irely.com>
2018-01-19 11:02:40 +0800

    CF-633

--- i21Database/dbo/Views/vyuCFExportAccount.sql
+++ i21Database/dbo/Views/vyuCFExportAccount.sql
@@ -1,9 +1,48 @@
-﻿CREATE VIEW [dbo].[vyuCFExportAccount]
+﻿
+
+CREATE VIEW [dbo].[vyuCFExportAccount]
 AS
-SELECT   emEnt.intEntityId, emEnt.strEntityNo, arCus.ysnActive, REPLACE(REPLACE(SUBSTRING(emEntLoc.strAddress, 1, 30), CHAR(13), ''), CHAR(10), '') AS strAddress1, 
-                         REPLACE(REPLACE(SUBSTRING(emEntLoc.strAddress, 30, 30), CHAR(13), ''), CHAR(10), '') AS strAddress2, emEntLoc.strCity, emEntLoc.strPhone, emEnt.strName, 
-                         emEntLoc.strZipCode, emEntLoc.strFax, emEntLoc.strState, emEnt.strEmail, '' AS dtmDateLastModified, '' AS dtmTimeLastModified, CONVERT(bit, 0) AS ysnUpdateFlag, 
-                         CONVERT(numeric(18, 6), 0) AS dblCreditLimit, CONVERT(bit, 0) AS ysnCreditIndicator, emEnt.intConcurrencyId
-FROM         dbo.tblEMEntity AS emEnt INNER JOIN
-                         dbo.tblARCustomer AS arCus ON emEnt.intEntityId = arCus.intEntityId INNER JOIN
-                         dbo.tblEMEntityLocation AS emEntLoc ON emEnt.intEntityId = emEntLoc.intEntityId
+
+SELECT   
+cfNet.strParticipant,
+cfNet.intNetworkId,
+emEnt.intEntityId, 
+emEnt.strEntityNo, 
+arCus.ysnActive, 
+REPLACE(REPLACE(SUBSTRING(emEntLoc.strAddress, 1, 30), CHAR(13), ''), CHAR(10), '') AS strAddress1, 
+REPLACE(REPLACE(SUBSTRING(emEntLoc.strAddress, 30, 30), CHAR(13), ''), CHAR(10), '') AS strAddress2, 
+emEntLoc.strCity, 
+emEntLoc.strPhone, 
+emEnt.strName, 
+emEntLoc.strZipCode, 
+emEntLoc.strFax, 
+emEntLoc.strState, 
+emEnt.strEmail, 
+'' AS dtmDateLastModified, 
+'' AS dtmTimeLastModified, 
+cfNet.strNetworkType,
+CONVERT(bit, 0) AS ysnUpdateFlag, 
+CONVERT(numeric(18, 6), 0) AS dblCreditLimit, 
+CONVERT(bit, 0) AS ysnCreditIndicator
+FROM        
+dbo.tblEMEntity AS emEnt INNER JOIN
+dbo.tblARCustomer AS arCus ON emEnt.intEntityId = arCus.intEntityId INNER JOIN
+dbo.tblEMEntityLocation AS emEntLoc ON emEnt.intEntityId = emEntLoc.intEntityId INNER JOIN 
+dbo.tblCFAccount AS cfAcc ON arCus.intEntityId = cfAcc.intCustomerId INNER JOIN
+dbo.tblCFCard AS cfCrd ON cfAcc.intAccountId = cfCrd.intAccountId INNER JOIN
+dbo.tblCFNetwork AS cfNet ON cfCrd.intNetworkId = cfNet.intNetworkId
+GROUP BY 
+cfNet.strParticipant,
+cfNet.intNetworkId,
+emEnt.intEntityId, 
+emEnt.strEntityNo, 
+arCus.ysnActive, 
+emEntLoc.strAddress,
+emEntLoc.strCity, 
+emEntLoc.strPhone, 
+emEnt.strName, 
+emEntLoc.strZipCode, 
+emEntLoc.strFax, 
+emEntLoc.strState, 
+emEnt.strEmail,
+cfNet.strNetworkType
