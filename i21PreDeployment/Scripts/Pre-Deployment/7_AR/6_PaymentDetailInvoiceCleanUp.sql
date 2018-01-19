1c25c3a61d83708482af483424268004a0bf80c2
Ruben Escarcha <ruben.escarcha@irely.com>
2018-01-19 19:27:59 +0800

    AR-6387 Error on 18.1 dev deployment

--- i21PreDeployment/Scripts/Pre-Deployment/7_AR/6_PaymentDetailInvoiceCleanUp.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/7_AR/6_PaymentDetailInvoiceCleanUp.sql
@@ -1,14 +1,35 @@
 ﻿--Activate this on 17.3 for AR-3714
 
---IF(EXISTS(SELECT NULL FROM sys.tables WHERE name = N'tblARPaymentDetail') AND EXISTS(SELECT NULL FROM sys.columns WHERE [name] = N'intInvoiceId' AND [object_id] = OBJECT_ID(N'tblARPaymentDetail'))
---			AND EXISTS(SELECT NULL FROM sys.tables WHERE name = N'tblARInvoice') AND EXISTS(SELECT NULL FROM sys.columns WHERE [name] = N'intInvoiceId' AND [object_id] = OBJECT_ID(N'tblARInvoice')))
---BEGIN
+IF	(
+	EXISTS(SELECT NULL FROM sys.tables WHERE name = N'tblARPaymentDetail') AND EXISTS(SELECT NULL FROM sys.columns WHERE [name] = N'intInvoiceId' AND [object_id] = OBJECT_ID(N'tblARPaymentDetail'))
+	AND
+	EXISTS(SELECT NULL FROM sys.tables WHERE name = N'tblARPaymentDetail') AND EXISTS(SELECT NULL FROM sys.columns WHERE [name] = N'intBillId' AND [object_id] = OBJECT_ID(N'tblARPaymentDetail'))
+	AND
+	EXISTS(SELECT NULL FROM sys.tables WHERE name = N'tblARInvoice') AND EXISTS(SELECT NULL FROM sys.columns WHERE [name] = N'intInvoiceId' AND [object_id] = OBJECT_ID(N'tblARInvoice'))
+	AND
+	EXISTS(SELECT NULL FROM sys.tables WHERE name = N'tblAPBill') AND EXISTS(SELECT NULL FROM sys.columns WHERE [name] = N'intBillId' AND [object_id] = OBJECT_ID(N'tblAPBill'))
+	)
+BEGIN
 
---UPDATE tblARPaymentDetail
---SET
---	[intInvoiceId] = NULL
---WHERE
---	[intInvoiceId] IS NOT NULL
---	AND NOT EXISTS (SELECT NULL FROM tblARInvoice WHERE tblARInvoice.[intInvoiceId] = tblARPaymentDetail.[intInvoiceId])
-
---END
+UPDATE tblARPaymentDetail
+SET
+	[intInvoiceId] = NULL
+WHERE
+	[intInvoiceId] IS NOT NULL
+	AND [intBillId] IS NOT NULL
+	AND NOT EXISTS (SELECT NULL FROM tblARInvoice WHERE tblARInvoice.[intInvoiceId] = tblARPaymentDetail.[intInvoiceId])
+	
+UPDATE tblARPaymentDetail
+SET
+	[intBillId] = NULL
+WHERE
+	[intBillId] IS NOT NULL
+	AND [intInvoiceId] IS NOT NULL
+	AND NOT EXISTS (SELECT NULL FROM tblAPBill WHERE tblAPBill.[intBillId] = tblARPaymentDetail.[intBillId])
+	
+DELETE FROM tblARPaymentDetail
+WHERE
+	NOT EXISTS (SELECT NULL FROM tblARInvoice WHERE tblARInvoice.[intInvoiceId] = tblARPaymentDetail.[intInvoiceId])
+	AND NOT EXISTS (SELECT NULL FROM tblAPBill WHERE tblAPBill.[intBillId] = tblARPaymentDetail.[intBillId])
+	
+END
