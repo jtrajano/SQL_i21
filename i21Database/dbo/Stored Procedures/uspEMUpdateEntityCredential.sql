be71685b272dc9ec0df9300fd7f78cff8f853699
JBorlagdan <jc.borlagdan@irely.com>
2018-01-23 07:58:33 +0800

    AR-6383

---
+++ i21Database/dbo/Stored Procedures/uspEMUpdateEntityCredential.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[uspEMUpdateEntityCredential]
+	@intEntityContactId  INT,
+	@strUsername AS NVARCHAR(50)
+AS
+BEGIN
+	DECLARE @isExist AS BIT
+	SET @isExist = (SELECT 1 FROM tblEMEntityCredential WHERE intEntityId = @intEntityContactId)
+
+	IF(@isExist = 1)
+	BEGIN
+		UPDATE tblEMEntityCredential
+		SET strUserName = @strUsername
+		WHERE intEntityId = @intEntityContactId
+	END
+END
+GO
