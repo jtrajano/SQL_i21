f990289eb3eb000fb0871497c42d4bfb6c9fd70a
Jb Mago <benedict.mago@irely.com>
2018-01-25 17:06:47 +0800

    IC-4729

---
+++ server/iRely.Inventory.BusinessLayer/AspectInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PostSharp.Patterns.Diagnostics;
+using PostSharp.Extensibility;
+
+[assembly: Log("Detailed", AttributePriority = 1, AttributeTargetMemberAttributes = MulticastAttributes.Protected | MulticastAttributes.Internal | MulticastAttributes.Public)]
+[assembly: Log("Detailed", AttributePriority = 2, AttributeExclude = true, AttributeTargetMembers = "regex:^get_|^set_|Dispose|.ctor")]
+namespace iRely.Inventory.BusinessLayer
+{
+    public class AspectInfo
+    {
+
+    }
+}
