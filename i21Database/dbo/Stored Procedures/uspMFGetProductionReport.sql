7223f7cf2257a4404b12f998955e626892c65d72
Premkumar S <prem.selladurai@irely.com>
2018-01-26 00:14:45 +0530

    MFG-3246

--- i21Database/dbo/Stored Procedures/uspMFGetProductionReport.sql
+++ i21Database/dbo/Stored Procedures/uspMFGetProductionReport.sql
@@ -1,4 +1,4 @@
-﻿CREATE PROCEDURE uspMFGetProductionReport @intDay int=1
+﻿CREATE PROCEDURE uspMFGetProductionReport @intDay INT = 1
 AS
 DECLARE @dtmProductionDate DATETIME
 
@@ -10,43 +10,187 @@ ELSE
 BEGIN
 	SELECT @dtmProductionDate = convert(DATETIME, Convert(CHAR, @dtmProductionDate, 101))
 END
-if @intDay=-1
-Begin
-	SELECT *
+
+IF @intDay = - 1
+BEGIN
+	SELECT [Production Date]
+		,[Item]
+		,[Description]
+		,[Work Order #]
+		,[Job #]
+		,[Production Lot]
+		,[Pallet No]
+		,[Quantity]
+		,[Quantity UOM]
+		,[Weight]
+		,[Weight UOM]
+		,[Line]
 	FROM vyuMFGetProduction
+	ORDER BY dtmPlannedDate
+		,intWorkOrderId
 
-	SELECT *
+	SELECT [Dump Date]
+		,[Product]
+		,[Product Description]
+		,[Production Lot]
+		,Line
+		,[Work Order #]
+		,[Job #]
+		,[WSI Item]
+		,[WSI Item Description]
+		,dblRequiredQty
+		,[Pallet Id]
+		,[Lot #]
+		,[Quantity]
+		,[Quantity UOM]
+		,[Weight]
+		,[Weight UOM]
 	FROM vyuMFGetRMUsage
+	ORDER BY dtmPlannedDate
+		,intWorkOrderId
 
-	SELECT *
+	SELECT [Dump Date]
+		,[Product]
+		,[Product Description]
+		,[Production Lot]
+		,Line
+		,[Work Order #]
+		,[Job #]
+		,[WSI Item]
+		,[WSI Item Description]
+		,[Lot #]
+		,Quantity
+		,[Quantity UOM]
+		,[Weight]
+		,[Weight UOM]
 	FROM vyuMFGetRMUsageByLot
+	ORDER BY dtmPlannedDate
+		,intWorkOrderId
 
-	SELECT *
+	SELECT [Dump Date]
+		,[Product]
+		,[Product Description]
+		,[Production Lot]
+		,Line
+		,[Work Order #]
+		,[Job #]
+		,[WSI Item]
+		,[WSI Item Description]
+		,dblRequiredQty
+		,[Total Consumed Quantity]
+		,[Used in Packaging]
+		,[UOM]
+		,[Damaged]
 	FROM vyuMFGetPMUsage
+	ORDER BY dtmPlannedDate
+		,intWorkOrderId
 
-	SELECT *
+	SELECT [Production Date]
+		,Item
+		,[Description]
+		,[Work Order #]
+		,[Job #]
+		,[Production Lot]
+		,[Good produced Pouches]
+		,[Total Pouches passed through counter]
+		,[Underweight Pouches]
+		,[Overweight Pouches]
+		,[Total sweeps (lb)]
 	FROM vyuMFGetOverAndUnderWeight
-
-End
-Else
-Begin
-	SELECT *
+	ORDER BY dtmPlannedDate
+		,intWorkOrderId
+END
+ELSE
+BEGIN
+	SELECT [Production Date]
+		,[Item]
+		,[Description]
+		,[Work Order #]
+		,[Job #]
+		,[Production Lot]
+		,[Pallet No]
+		,[Quantity]
+		,[Quantity UOM]
+		,[Weight]
+		,[Weight UOM]
+		,[Line]
 	FROM vyuMFGetProduction
 	WHERE [Production Date] = @dtmProductionDate
+	ORDER BY dtmPlannedDate
+		,intWorkOrderId
 
-	SELECT *
+	SELECT [Dump Date]
+		,[Product]
+		,[Product Description]
+		,[Production Lot]
+		,Line
+		,[Work Order #]
+		,[Job #]
+		,[WSI Item]
+		,[WSI Item Description]
+		,dblRequiredQty
+		,[Pallet Id]
+		,[Lot #]
+		,[Quantity]
+		,[Quantity UOM]
+		,[Weight]
+		,[Weight UOM]
 	FROM vyuMFGetRMUsage
 	WHERE [Dump Date] = @dtmProductionDate
+	ORDER BY dtmPlannedDate
+		,intWorkOrderId
 
-	SELECT *
+	SELECT [Dump Date]
+		,[Product]
+		,[Product Description]
+		,[Production Lot]
+		,Line
+		,[Work Order #]
+		,[Job #]
+		,[WSI Item]
+		,[WSI Item Description]
+		,[Lot #]
+		,Quantity
+		,[Quantity UOM]
+		,[Weight]
+		,[Weight UOM]
 	FROM vyuMFGetRMUsageByLot
 	WHERE [Dump Date] = @dtmProductionDate
+	ORDER BY dtmPlannedDate
+		,intWorkOrderId
 
-	SELECT *
+	SELECT [Dump Date]
+		,[Product]
+		,[Product Description]
+		,[Production Lot]
+		,Line
+		,[Work Order #]
+		,[Job #]
+		,[WSI Item]
+		,[WSI Item Description]
+		,dblRequiredQty
+		,[Total Consumed Quantity]
+		,[Used in Packaging]
+		,[UOM]
+		,[Damaged]
 	FROM vyuMFGetPMUsage
 	WHERE [Dump Date] = @dtmProductionDate
+	ORDER BY dtmPlannedDate
+		,intWorkOrderId
 
-	SELECT *
+	SELECT [Production Date]
+		,Item
+		,[Description]
+		,[Work Order #]
+		,[Job #]
+		,[Production Lot]
+		,[Good produced Pouches]
+		,[Total Pouches passed through counter]
+		,[Underweight Pouches]
+		,[Overweight Pouches]
+		,[Total sweeps (lb)]
 	FROM vyuMFGetOverAndUnderWeight
 	WHERE [Production Date] = @dtmProductionDate
-End
+	ORDER BY dtmPlannedDate
+		,intWorkOrderId
+END
