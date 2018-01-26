ef5f00992abe8e9d56346e766e3160458b929742
jessa delo-ag <jessa.delo-ag@irely.com>
2018-01-26 18:24:33 +0800

    RPT-1109

---
+++ i21Database/dbo/Stored Procedures/uspSMGetReportLabel.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[uspSMGetReportLabel]
+	@LanguageId	INT
+AS	
+
+select * from tblSMReportLabelDetail RD 
+INNER JOIN tblSMReportLabels RL 
+ON RD.intReportLabelsId = RL.intReportLabelsId 
+WHERE RL.intLanguageId = @LanguageId
