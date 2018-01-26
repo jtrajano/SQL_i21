88dcd569f2491bb97f29befdf4704dd913988834
Arman Arenas <arman.arenas@irely.com>
2018-01-26 13:54:02 +0800

    PR-1567 Updated W2 and W3 calculations

--- i21Database/dbo/Views/vyuPRPaycheckDeduction.sql
+++ i21Database/dbo/Views/vyuPRPaycheckDeduction.sql
@@ -48,18 +48,37 @@ SELECT
 	,ysnFITTaxable = CAST(ISNULL((SELECT TOP 1 1 FROM tblPRPaycheckDeductionTax 
 								WHERE intPaycheckDeductionId = tblPRPaycheckDeduction.intPaycheckDeductionId 
 								AND intTypeTaxId IN (SELECT intTypeTaxId FROM tblPRPaycheckTax 
-													 WHERE intPaycheckId = tblPRPaycheck.intPaycheckId AND dblTotal > 0
+													 WHERE intPaycheckId = tblPRPaycheck.intPaycheckId
 													 AND strCalculationType = 'USA Federal Tax')), 0) AS BIT)
 	,ysnStateTaxable = CAST(ISNULL((SELECT TOP 1 1 FROM tblPRPaycheckDeductionTax 
 								WHERE intPaycheckDeductionId = tblPRPaycheckDeduction.intPaycheckDeductionId 
 								AND intTypeTaxId IN (SELECT intTypeTaxId FROM tblPRPaycheckTax 
-													 WHERE intPaycheckId = tblPRPaycheck.intPaycheckId AND dblTotal > 0
-													 AND strCalculationType = 'USA State')), 0) AS BIT)
+													 WHERE intPaycheckId = tblPRPaycheck.intPaycheckId
+													 AND strCalculationType = 'USA State'
+													 AND (intTypeTaxStateId NOT IN (41, 45)
+														 OR ((intTypeTaxStateId = 41 AND strVal1 = 'None' AND strVal2 = 'None')
+															OR (intTypeTaxStateId = 45 AND strVal2 = 'None (None)' AND strVal3 = 'None (None)'))
+														 )
+													 )), 0) AS BIT)
 	,ysnLocalTaxable = CAST(ISNULL((SELECT TOP 1 1 FROM tblPRPaycheckDeductionTax 
 								WHERE intPaycheckDeductionId = tblPRPaycheckDeduction.intPaycheckDeductionId 
 								AND intTypeTaxId IN (SELECT intTypeTaxId FROM tblPRPaycheckTax 
-													 WHERE intPaycheckId = tblPRPaycheck.intPaycheckId AND dblTotal > 0
+													 WHERE intPaycheckId = tblPRPaycheck.intPaycheckId
 													 AND strCalculationType = 'USA Local')), 0) AS BIT)
+	,ysnSchoolDistrictTaxable = CAST(ISNULL((SELECT TOP 1 1 FROM tblPRPaycheckDeductionTax 
+								WHERE intPaycheckDeductionId = tblPRPaycheckDeduction.intPaycheckDeductionId 
+								AND intTypeTaxId IN (SELECT intTypeTaxId FROM tblPRPaycheckTax 
+													 WHERE intPaycheckId = tblPRPaycheck.intPaycheckId
+													 AND strCalculationType = 'USA State' 
+													 AND ((intTypeTaxStateId = 41 AND strVal1 <> 'None')
+														OR (intTypeTaxStateId = 45 AND strVal2 <> 'None (None)')))), 0) AS BIT)
+	,ysnMunicipalityTaxable = CAST(ISNULL((SELECT TOP 1 1 FROM tblPRPaycheckDeductionTax 
+								WHERE intPaycheckDeductionId = tblPRPaycheckDeduction.intPaycheckDeductionId 
+								AND intTypeTaxId IN (SELECT intTypeTaxId FROM tblPRPaycheckTax 
+													 WHERE intPaycheckId = tblPRPaycheck.intPaycheckId
+													 AND strCalculationType = 'USA State' 
+													 AND ((intTypeTaxStateId = 41 AND strVal2 <> 'None')
+														OR (intTypeTaxStateId = 45 AND strVal3 <> 'None (None)')))), 0) AS BIT)
 	,tblPRPaycheckDeduction.intAccountId
 	,tblPRPaycheckDeduction.intExpenseAccountId
 	,tblPRPaycheckDeduction.strPaidBy
