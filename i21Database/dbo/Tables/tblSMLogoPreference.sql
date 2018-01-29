5ba00a122a67abb8d9cafd4e21841997994225d1
COMTECH\RRiguer <ruzel.riguer@irely.com>
2018-01-29 18:42:48 +0800

    SM-2021

---
+++ i21Database/dbo/Tables/tblSMLogoPreference.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[tblSMLogoPreference]
+(
+	[intLogoPreferenceId] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+    [strLogoName] NVARCHAR(50) NULL, 
+    [imgLogo] VARBINARY(MAX) NULL, 
+    [ysnDefault] BIT NULL, 
+    [ysnARInvoice] BIT NULL, 
+    [ysnARStatement] BIT NULL, 
+    [ysnContract] BIT NULL, 
+    [ysnAllOtherReports] BIT NULL, 
+    [intCompanyLocationId] INT NULL, 
+    [intConcurrencyId] INT NOT NULL DEFAULT 1
+)
