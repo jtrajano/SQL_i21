c59e66e4c8a83caed08480e9263a5417bb4b11bb
Feb Montefrio <feb.montefrio@irely.com>
2018-01-29 13:28:53 +0800

    IC-3706

---
+++ i21Database/dbo/Views/vyuICGetShipmentChargeTaxDetails.sql
@@ -0,0 +1,25 @@
+﻿CREATE VIEW [dbo].[vyuICGetShipmentChargeTaxDetails]
+AS
+
+SELECT
+	ChargeTax.intInventoryShipmentChargeTaxId,
+	Charge.intInventoryShipmentId,
+	Charge.intInventoryShipmentChargeId,
+	Charge.intChargeId,
+	Item.strItemNo,
+	TaxGroup.strTaxGroup,
+	TaxCode.strTaxCode,
+	ChargeTax.strCalculationMethod,
+	ChargeTax.dblRate,
+	ChargeTax.dblTax,
+	TaxClass.strTaxClass,
+	ChargeTax.dblQty,
+	ChargeTax.dblCost,
+	ChargeTax.ysnCheckoffTax,
+	ChargeTax.ysnTaxAdjusted
+FROM	dbo.tblICInventoryShipmentChargeTax ChargeTax
+		LEFT JOIN dbo.tblICInventoryShipmentCharge Charge on Charge.intInventoryShipmentChargeId = ChargeTax.intInventoryShipmentChargeId
+		LEFT JOIN dbo.tblICItem Item on Item.intItemId = Charge.intChargeId 
+		LEFT JOIN dbo.tblSMTaxGroup TaxGroup on TaxGroup.intTaxGroupId = ChargeTax.intTaxGroupId			
+		LEFT JOIN dbo.tblSMTaxCode TaxCode on TaxCode.intTaxCodeId = ChargeTax.intTaxCodeId
+		LEFT JOIN dbo.tblSMTaxClass TaxClass ON TaxClass.intTaxClassId = TaxCode.intTaxClassId
