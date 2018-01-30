317197d9b700adf1da20c036072eb21874bd57bd
Dan Jalimao <dan.jalimao@irely.com>
2018-01-30 13:07:19 +0800

    SM-3739

--- i21Database/Scripts/Post-Deployment/SM/SMDataMigrations.sql
+++ i21Database/Scripts/Post-Deployment/SM/SMDataMigrations.sql
@@ -49,12 +49,12 @@ GO
 	SELECT TOP 1 @AdminId = intUserRoleID FROM tblSMUserRole WHERE strName = 'ADMIN'
 	--SELECT TOP 1 @UserId = intUserRoleID FROM tblSMUserRole WHERE strName = 'USER'
 
+	EXEC uspSMUpdateUserRoleMenus @AdminId
+	--EXEC uspSMUpdateUserRoleMenus @UserId
+
 	UPDATE tblSMUserRoleMenu
 	SET ysnVisible = 1
 	WHERE intUserRoleId IN (@AdminId)--, @UserId)
-
-	EXEC uspSMUpdateUserRoleMenus @AdminId
-	--EXEC uspSMUpdateUserRoleMenus @UserId
 
 GO
 	-- Add Help Desk role menus if not existing
