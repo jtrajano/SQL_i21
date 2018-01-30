8d819a3d9426001bdaf91fa7c655c2e9871b756b
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2018-01-31 02:33:46 +0530

    RM-740

--- i21Database/dbo/Views/vyuRKM2MInquirySearch.sql
+++ i21Database/dbo/Views/vyuRKM2MInquirySearch.sql
@@ -3,7 +3,8 @@
 AS 
 
 SELECT i.intM2MInquiryId,i.strRecordName,b.dtmM2MBasisDate,p.dtmPriceDate,m.strUnitMeasure,m1.strUnitMeasure as strPriceUOM,c.strCurrency,
-	   i.dtmTransactionUpTo,i.strRateType,cc.strCommodityCode,cl.strLocationName,z.strMarketZoneCode,ysnPost,i.strPricingType,dtmLastReversalPostDate
+	   i.dtmTransactionUpTo,i.strRateType,cc.strCommodityCode,cl.strLocationName,z.strMarketZoneCode,ysnPost,i.strPricingType,dtmLastReversalPostDate,
+	   strBatchId,dtmGLReverseDate,dtmGLPostDate
 FROM tblRKM2MInquiry i
 JOIN tblICUnitMeasure m on m.intUnitMeasureId=i.intUnitMeasureId
 JOIN tblICUnitMeasure m1 on m1.intUnitMeasureId=i.intPriceItemUOMId
