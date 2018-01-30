2b79dbbdcfa91b6bc40f7d077052206786a2e282
Satya S Sahoo <satya.sahoo@irely.com>
2018-01-30 21:34:31 +0530

    CT-1679 - Contract Import - Increase timeout and create log file. - 18.1

---
+++ i21Database/dbo/Stored Procedures/uspCTGetImportedSessionSummary.sql
@@ -0,0 +1,27 @@
+﻿CREATE PROCEDURE [dbo].[uspCTGetImportedSessionSummary]
+	@intSession BIGINT
+AS
+
+BEGIN TRY
+
+    DECLARE @ErrMsg			  NVARCHAR(MAX),
+		  @intTotalCount	  INT,
+		  @intSuccessCount	  INT
+
+    SELECT @intTotalCount = COUNT(1) FROM tblCTContractImport WHERE intSession = @intSession
+    SELECT @intSuccessCount = COUNT(1) FROM tblCTContractImport WHERE intSession = @intSession AND strErrorMsg IS NULL
+
+    SELECT  strErrorMsg,  strContractNumber, intContractSeq, 'Fail' AS strImportStatus 
+    FROM	  tblCTContractImport WHERE strErrorMsg IS NOT NULL
+    AND	  intSession = @intSession
+    UNION ALL
+    SELECT  LTRIM(@intSuccessCount) + ' of ' + LTRIM(@intTotalCount) + ' contracts imported successfully.' strErrorMsg,  NULL AS strContractNumber, NULL AS intContractSeq, 'Success' AS strImportStatus 
+
+END TRY
+
+BEGIN CATCH
+
+    SET @ErrMsg = ERROR_MESSAGE()  
+    RAISERROR (@ErrMsg,18,1,'WITH NOWAIT')  
+	
+END CATCH
