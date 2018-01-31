8bdde082f6bf9cba8b2cf7e4377d28b8e57527d4
JeffTrajano <jeffrey.trajano@irely.com>
2018-01-31 11:49:53 +0800

    GL-4077 [Deployment] duplication in intAccountSystemId GL-4076 [Deployment] duplication in intAccountSystemId

--- Integration/dbo/Stored Procedures/uspGLInsertOriginCrossReferenceMapping.sql
+++ Integration/dbo/Stored Procedures/uspGLInsertOriginCrossReferenceMapping.sql
@@ -1,12 +1,16 @@
-﻿CREATE PROCEDURE uspGLInsertOriginCrossReferenceMapping
-AS
-IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[glactmst]') AND type IN (N'U'))
-
-BEGIN
+﻿GO
 EXEC('
+ALTER PROCEDURE uspGLInsertOriginCrossReferenceMapping
+AS
+BEGIN
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N''[dbo].[glactmst]'') AND type IN (N''U'')) 
+BEGIN
+	PRINT ''Insert Origin xref mapping  aborted  - Origin table glactmst is not available''
+	RETURN
+END
 IF EXISTS (select inti21Id, count(1) from tblGLCOACrossReference group by inti21Id having count(1) > 1)
 BEGIN
-PRINT ''Inserting in cross reference mapping table was aborted due to duplicate entry in tblGLCOACrossReference''
+PRINT ''Insert Origin xref mapping  aborted - Duplicate entry in tblGLCOACrossReference''
 RETURN
 END
 
@@ -27,13 +31,13 @@ END
 ELSE
 BEGIN
 	SELECT TOP 1 @originId = intAccountSystemId FROM tblGLAccountSystem WHERE strAccountSystemDescription = ''Origin''
+	DELETE FROM tblGLCrossReferenceMapping WHERE intAccountSystemId = @originId AND intAccountId IN (
+			SELECT intAccountId FROM tblGLCrossReferenceMapping WHERE intAccountSystemId =@originId GROUP BY intAccountId HAVING COUNT(1) > 1)
 END
 	MERGE dbo.tblGLCrossReferenceMapping as map
 	USING dbo.tblGLCOACrossReference AS coa
 	ON map.intAccountId = coa.inti21Id 
-	AND map.intAccountSystemId = @originId
-	AND coa.ysnOrigin =1
-	WHEN MATCHED THEN
+	WHEN MATCHED AND map.intAccountSystemId = @originId AND coa.ysnOrigin =1 THEN
 		UPDATE SET strOldAccountId = coa.strOldId
 	WHEN NOT MATCHED THEN
 		INSERT (
@@ -45,9 +49,11 @@ END
 IF NOT EXISTS(SELECT TOP 1 1 FROM tblGLCompanyPreferenceOption)
 	INSERT INTO tblGLCompanyPreferenceOption(intDefaultVisibleOldAccountSystemId,intConcurrencyId) VALUES(1,1)
 	
-UPDATE tblGLCompanyPreferenceOption SET intDefaultVisibleOldAccountSystemId = 1 WHERE intDefaultVisibleOldAccountSystemId IS NULL')
-END
-	
+UPDATE tblGLCompanyPreferenceOption SET intDefaultVisibleOldAccountSystemId = 1 WHERE intDefaultVisibleOldAccountSystemId IS NULL
+END')
 
+GO
+EXEC dbo.uspGLInsertOriginCrossReferenceMapping
+GO
 
 
