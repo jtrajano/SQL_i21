11610cf47ef76c0288b1a545e4ce8ad6752cc566
Kenneth Lew Lagunero <kenneth.lagunero@irely.com>
2018-01-31 22:53:33 +0800

    BB-53

--- i21Database/dbo/Tables/tblBBProgramCharge.sql
+++ i21Database/dbo/Tables/tblBBProgramCharge.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[tblBBProgramCharge](
 	[intProgramChargeId] [int] IDENTITY(1,1) NOT NULL,
 	[intProgramId] INT NOT NULL,
-	[strCharge] INT NOT NULL,
+	[strCharge] NVARCHAR(50) NOT NULL,
 	[intConcurrencyId] [int] NOT NULL CONSTRAINT [DF_tblBBProgramCharge_intConcurrencyId]  DEFAULT ((0)), 
     CONSTRAINT [PK_tblBBProgramCharge] PRIMARY KEY ([intProgramChargeId]), 
     CONSTRAINT [FK_tblBBProgramCharge_tblBBProgram] FOREIGN KEY (intProgramId) REFERENCES [tblBBProgram]([intProgramId]) ON DELETE CASCADE, 
