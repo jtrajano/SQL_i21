899ba8ff334eb8bb6739c8735a9b61934f5d3aae
Satya S Sahoo <satya.sahoo@irely.com>
2018-01-31 17:26:17 +0530

    CT-1890 - Limit Contract Application by Location is not saving - 17.3

--- i21Database/dbo/Tables/tblCTCompanyPreference.sql
+++ i21Database/dbo/Tables/tblCTCompanyPreference.sql
@@ -51,6 +51,7 @@
 	ysnEnableMultiProducer BIT,
 	intDefStorageSchedule INT,
 	ysnAmdWoAppvl         BIT,
+	ysnLimitCTByLocation BIT,
 
     CONSTRAINT [PK_tblCTCompanyPreference_intCompanyPreferenceId] PRIMARY KEY CLUSTERED ([intCompanyPreferenceId] ASC),
 	CONSTRAINT [FK_tblCTCompanyPreference_tblSMCurrency_intCleanCostCurrencyId_intCurrencyId] FOREIGN KEY ([intCleanCostCurrencyId]) REFERENCES [tblSMCurrency]([intCurrencyID]),
