16e45fa2ad0f0cb010891f9aa3035aae59950a0d
Erick <erick.delacruz@irely.com>
2018-02-03 03:42:48 +0800

    AP-4905

--- i21Database/dbo/Views/vyuAPForApprovalTransaction.sql
+++ i21Database/dbo/Views/vyuAPForApprovalTransaction.sql
@@ -9,10 +9,18 @@ SELECT
 FROM tblSMScreen A
 CROSS APPLY (
 	SELECT
-		B.strTransactionNo, B.intRecordId intTransactionId
+		B.strTransactionNo, B.intRecordId intTransactionId, B.intEntityId
 	FROM tblSMTransaction B
 	WHERE A.intScreenId = B.intScreenId
 	AND (B.strApprovalStatus IS NOT NULL AND B.strApprovalStatus NOT IN ('No Need for Approval','Approved'))
 ) APTransaction
 WHERE A.strScreenName IN ('Purchase Order','Voucher')
+AND EXISTS (
+	--MAKE SURE THERE STILL HAS APPROVAL SETUP
+	SELECT TOP 1 1 FROM tblEMEntityRequireApprovalFor em WHERE em.intScreenId = A.intScreenId AND em.intEntityId = APTransaction.intEntityId
+	UNION ALL
+	SELECT TOP 1 1 FROM tblSMCompanyLocationRequireApprovalFor sm WHERE sm.intScreenId = A.intScreenId
+	UNION ALL
+	SELECT TOP 1 1 FROM tblSMUserSecurityRequireApprovalFor usr WHERE usr.intScreenId = A.intScreenId AND usr.intEntityUserSecurityId = APTransaction.intEntityId
+)
 
