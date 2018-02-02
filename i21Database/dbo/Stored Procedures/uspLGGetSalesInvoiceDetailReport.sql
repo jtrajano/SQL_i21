be494a137574ccf9b04851586437bb761fabc0e3
IRELY\swghosh <swetosuvro.g@irely.com>
2018-02-02 21:00:25 +0530

    LG-1350

--- i21Database/dbo/Stored Procedures/uspLGGetSalesInvoiceDetailReport.sql
+++ i21Database/dbo/Stored Procedures/uspLGGetSalesInvoiceDetailReport.sql
@@ -4,8 +4,41 @@
 AS
 BEGIN
 	DECLARE @intInvoiceId INT
-
+	DECLARE @intLineCount INT
 	SET @intInvoiceId = @xmlParam
+
+	SELECT @intLineCount = COUNT(*)
+	FROM tblARInvoice Inv
+	JOIN vyuCTEntity EN ON EN.intEntityId = Inv.intEntityCustomerId
+		AND EN.strEntityType = 'Customer'
+	JOIN tblARInvoiceDetail InvDet ON InvDet.intInvoiceId = Inv.intInvoiceId
+	JOIN tblICItem Item ON Item.intItemId = InvDet.intItemId
+	JOIN tblSMCurrency InvCur ON InvCur.intCurrencyID = Inv.intCurrencyId
+	LEFT JOIN tblSMTaxGroup TaxG ON TaxG.intTaxGroupId = InvDet.intTaxGroupId
+	LEFT JOIN tblCTContractDetail CD ON CD.intContractDetailId = InvDet.intContractDetailId
+	LEFT JOIN tblCTContractHeader CH ON CH.intContractHeaderId = CD.intContractHeaderId
+	LEFT JOIN tblSMCurrency PriceCur ON PriceCur.intCurrencyID = CD.intCurrencyId
+	LEFT JOIN tblICItemUOM PriceItemUOM ON PriceItemUOM.intItemUOMId = CD.intPriceItemUOMId
+	LEFT JOIN tblICUnitMeasure PriceUOM ON PriceUOM.intUnitMeasureId = PriceItemUOM.intUnitMeasureId
+	LEFT JOIN tblICItemUOM WtItemUOM ON WtItemUOM.intItemUOMId = InvDet.intItemWeightUOMId
+	LEFT JOIN tblICUnitMeasure WtUOM ON WtUOM.intUnitMeasureId = WtItemUOM.intUnitMeasureId
+	LEFT JOIN tblLGLoad L ON L.intLoadId = Inv.intLoadId
+	LEFT JOIN tblLGLoadDetail LD ON LD.intLoadId = L.intLoadId
+		AND LD.intLoadDetailId = InvDet.intLoadDetailId
+	LEFT JOIN tblLGLoadDetailLot LDL ON LDL.intLoadDetailId = LD.intLoadDetailId
+	LEFT JOIN tblICLot Lot ON Lot.intLotId = LDL.intLotId
+	LEFT JOIN tblICInventoryReceiptItemLot ReceiptLot ON ReceiptLot.intParentLotId = Lot.intParentLotId
+	LEFT JOIN tblICInventoryReceiptItem ReceiptItem ON ReceiptItem.intInventoryReceiptItemId = ReceiptLot.intInventoryReceiptItemId
+	LEFT JOIN tblLGLoadDetailContainerLink LDCLink ON LDCLink.intLoadDetailId = ReceiptItem.intSourceId
+		AND LDCLink.intLoadContainerId = ReceiptItem.intContainerId
+	LEFT JOIN tblLGLoadContainer Cont ON Cont.intLoadContainerId = LDCLink.intLoadContainerId
+	WHERE Inv.intInvoiceId = @intInvoiceId
+		AND Item.strType <> CASE 
+			WHEN @ysnIncludeOtherChargeItems <> 1
+				THEN 'Other Charge'
+			ELSE ''
+			END
+
     SELECT 
 		Inv.intInvoiceId,
 		intSerialNo = ROW_NUMBER() OVER (ORDER BY InvDet.intInvoiceDetailId),
@@ -41,9 +74,10 @@ BEGIN
 		Inv.dblInvoiceSubtotal,
 		Inv.dblTax,
 		Inv.dblInvoiceTotal,
-		strTaxDescription = TaxG.strDescription
+		strTaxDescription = TaxG.strDescription,
+		intLineCount = @intLineCount 
 	FROM tblARInvoice Inv
-	JOIN vyuCTEntity EN ON EN.intEntityId = Inv.intEntityCustomerId
+	JOIN vyuCTEntity EN ON EN.intEntityId = Inv.intEntityCustomerId AND strEntityType = 'Customer'
 	JOIN tblARInvoiceDetail InvDet ON InvDet.intInvoiceId = Inv.intInvoiceId
 	JOIN tblICItem Item ON Item.intItemId = InvDet.intItemId
 	JOIN tblSMCurrency InvCur ON InvCur.intCurrencyID = Inv.intCurrencyId
