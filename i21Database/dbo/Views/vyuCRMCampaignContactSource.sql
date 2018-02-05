1414c2b5b121e066bec44387e6142184fe91cdf0
Jayson Ramos <jayson.ramos@iRely.com>
2018-02-05 14:38:46 +0800

    CRM-584

--- i21Database/dbo/Views/vyuCRMCampaignContactSource.sql
+++ i21Database/dbo/Views/vyuCRMCampaignContactSource.sql
@@ -10,6 +10,38 @@
 			,d.intActivityId
 			,strActivityType = d.strType
 			,d.strActivityNo
+			,result.intEntityId
+				,e.dblQuote
+				,e.dblSalesOrder
+				,e.intOpportunityId
+				,e.strOpportunityName
+		from
+			(
+				select distinct
+					strContactName = vyuEMEntityContact.strName
+					,strCompanyName = vyuEMEntityContact.strEntityName
+					,vyuEMEntityContact.strEmail
+					,intEntityId = vyuEMEntityContact.intEntityId
+					,intContactId = vyuEMEntityContact.intEntityContactId
+					,strEntityType = 
+						dbo.fnCRMCoalesceEntityType((select top 1 a.intEntityId from tblEMEntityToContact a where a.intEntityContactId = vyuEMEntityContact.intEntityContactId))
+				from
+					vyuEMEntityContact
+			) as result
+			left join tblSMActivity d on d.intActivityId = (select max(d.intActivityId) from tblSMActivity d where d.intEntityContactId = result.intContactId)
+			left join vyuCRMCampaignEntitySalesOrder e on e.intEntityContactId = result.intContactId
+
+		/*
+		select
+			intId = convert(int, ROW_NUMBER() over (order by strContactName))
+			,strContactName
+			,strCompanyName
+			,strEmail
+			,intContactId
+			,strEntityType = strEntityType
+			,d.intActivityId
+			,strActivityType = d.strType
+			,d.strActivityNo
 		from
 			(
 				select distinct
@@ -23,3 +55,4 @@
 					vyuEMEntityContact
 			) as result
 			left join tblSMActivity d on d.intActivityId = (select max(d.intActivityId) from tblSMActivity d where d.intEntityContactId = result.intContactId)
+		*/
--- i21Database/dbo/Views/vyuCRMCampaignEntityLink.sql
+++ i21Database/dbo/Views/vyuCRMCampaignEntityLink.sql
@@ -11,8 +11,31 @@
 				,strActivityType = d.strType
 				,d.strActivityNo
 				,intEntityEntityId = c.intEntityId
+				,e.dblQuote
+				,e.dblSalesOrder
+				,e.intOpportunityId
+				,e.strOpportunityName
+			from tblEMEntityToContact a
+			join tblEMEntity b on b.intEntityId = a.intEntityContactId
+			join tblEMEntity c on c.intEntityId = a.intEntityId
+			left join tblSMActivity d on d.intActivityId = (select max(d.intActivityId) from tblSMActivity d where d.intEntityContactId = a.intEntityContactId)
+			left join vyuCRMCampaignEntitySalesOrder e on e.intEntityContactId = a.intEntityContactId
+		) as link where strEntityType is not null
+		/*
+		select * from (
+			select
+				intEntityId = a.intEntityContactId
+				,strContactName = b.strName
+				,strEntityName = c.strName
+				,strEntityType = (select top 1 d.strType from tblEMEntityType d where d.intEntityId = a.intEntityId)
+				,strEmail = b.strEmail
+				,d.intActivityId
+				,strActivityType = d.strType
+				,d.strActivityNo
+				,intEntityEntityId = c.intEntityId
 			from tblEMEntityToContact a
 			join tblEMEntity b on b.intEntityId = a.intEntityContactId
 			join tblEMEntity c on c.intEntityId = a.intEntityId
 			left join tblSMActivity d on d.intActivityId = (select max(d.intActivityId) from tblSMActivity d where d.intEntityContactId = a.intEntityContactId)
 		) as link where strEntityType is not null
+		*/
