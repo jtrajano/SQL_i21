d2112e9fef713f9c4be790b2151c431f26176d6f
Gopinath Nayak <gopinath.n@irely.com>
2018-02-05 22:21:23 +0530

    CT-278

--- i21Database/dbo/Tables/tblCTCondition.sql
+++ i21Database/dbo/Tables/tblCTCondition.sql
@@ -5,6 +5,7 @@
     [strConditionName] NVARCHAR(200) COLLATE Latin1_General_CI_AS NOT NULL, 
     [strConditionDesc] NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NOT NULL, 
     [ysnActive] BIT NULL,
+	[ysnStandard] BIT NULL,
 	CONSTRAINT [PK_tblCTCondition_intConditionId] PRIMARY KEY CLUSTERED (intConditionId ASC), 
     CONSTRAINT [UK_tblCTCondition_strConditionName] UNIQUE ([strConditionName])
 )
--- i21Database/dbo/Views/vyuCTAmendmentHistory.sql
+++ i21Database/dbo/Views/vyuCTAmendmentHistory.sql
@@ -1,30 +1,30 @@
 ﻿CREATE VIEW [dbo].[vyuCTAmendmentHistory]
 AS
 SELECT 
-	 intSequenceAmendmentLogId = SAL.intSequenceAmendmentLogId
-	,intContractHeaderId       = SAL.intContractHeaderId
-	,intContractDetailId       = SAL.intContractDetailId
-	,dtmHistoryCreated	       = SAL.dtmHistoryCreated
-	,strContractNumber         = CH.strContractNumber
-	,intContractSeq            = CD.intContractSeq
-	,intEntityId		       = CH.intEntityId
-	,strEntityName		       = EY.strName
-	,intContractTypeId         = CH.intContractTypeId
-	,strContractType	       = TP.strContractType
-	,strItemChanged		       = SAL.strItemChanged
-	,strOldValue		       = SAL.strOldValue
-	,strNewValue		       = SAL.strNewValue
-	,intCommodityId		       = CH.intCommodityId
-	,strCommodityCode	       = CO.strCommodityCode
-	,ysnPrinted			       = CH.ysnPrinted
-	,intCompanyLocationId      = CD.intCompanyLocationId
-	,strLocationName           = CL.strLocationName
-	,strAmendmentNumber		   = SAL.strAmendmentNumber
-FROM tblCTSequenceAmendmentLog SAL
-JOIN tblCTContractHeader		   CH  ON CH.intContractHeaderId = SAL.intContractHeaderId
-LEFT JOIN tblCTContractDetail	   CD  ON CD.intContractDetailId = SAL.intContractDetailId
-JOIN tblEMEntity				   EY  ON EY.intEntityId = CH.intEntityId
-JOIN tblCTContractType			   TP  ON TP.intContractTypeId = CH.intContractTypeId
-LEFT JOIN tblSMCompanyLocation		   CL  ON CL.intCompanyLocationId = CD.intCompanyLocationId
-JOIN tblCTAmendmentApproval        AMP ON AMP.intAmendmentApprovalId = SAL.intAmendmentApprovalId AND ISNULL(AMP.ysnAmendment,0) = 1
-LEFT JOIN tblICCommodity	       CO  ON CO.intCommodityId = CH.intCommodityId
+	 intSequenceAmendmentLogId      =  SAL.intSequenceAmendmentLogId
+	,intContractHeaderId            =  SAL.intContractHeaderId
+	,intContractDetailId            =  SAL.intContractDetailId
+	,dtmHistoryCreated	            =  SAL.dtmHistoryCreated
+	,strContractNumber              =  CH.strContractNumber
+	,intContractSeq                 =  CD.intContractSeq
+	,intEntityId		            =  CH.intEntityId
+	,strEntityName		            =  EY.strName
+	,intContractTypeId              =  CH.intContractTypeId
+	,strContractType	            =  TP.strContractType
+	,strItemChanged		            =  SAL.strItemChanged
+	,strOldValue		            =  SAL.strOldValue
+	,strNewValue		            =  SAL.strNewValue
+	,intCommodityId		            =  CH.intCommodityId
+	,strCommodityCode	            =  CO.strCommodityCode
+	,ysnPrinted			            =  CH.ysnPrinted
+	,intCompanyLocationId           =  CD.intCompanyLocationId
+	,strLocationName                =  CL.strLocationName
+	,strAmendmentNumber		        =  SAL.strAmendmentNumber
+FROM tblCTSequenceAmendmentLog	   SAL
+JOIN tblCTContractHeader		   CH  ON CH.intContractHeaderId     = SAL.intContractHeaderId
+JOIN tblEMEntity				   EY  ON EY.intEntityId		     = CH.intEntityId
+JOIN tblCTContractType			   TP  ON TP.intContractTypeId       = CH.intContractTypeId
+JOIN tblCTAmendmentApproval        AMP ON AMP.intAmendmentApprovalId = SAL.intAmendmentApprovalId  AND ISNULL(AMP.ysnAmendment,0) = 1
+LEFT JOIN tblCTContractDetail	   CD  ON CD.intContractDetailId     = SAL.intContractDetailId
+LEFT JOIN tblICCommodity	       CO  ON CO.intCommodityId			 = CH.intCommodityId
+LEFT JOIN tblSMCompanyLocation	   CL  ON CL.intCompanyLocationId    = CD.intCompanyLocationId
