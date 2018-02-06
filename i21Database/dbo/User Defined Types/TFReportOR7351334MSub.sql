fa75f21ec43022c1aa6ad3299ed8cf12c15d798f
Crisman Lazaga <cris.lazaga@irely.com>
2018-02-06 17:18:32 +0800

    MFT-835

---
+++ i21Database/dbo/User Defined Types/TFReportOR7351334MSub.sql
@@ -0,0 +1,10 @@
+﻿CREATE TYPE [dbo].[TFReportOR7351334MSub] AS TABLE
+(
+	intId INT NOT NULL,
+	strLine NVARCHAR(1000),
+	strFacilityNumber NVARCHAR(200),
+	strProductCode NVARCHAR(100),
+	strData NVARCHAR(200),
+	dtmBeginDate DATETIME,
+	dtmEndDate DATETIME
+)
---
+++ i21Database/dbo/User Defined Types/TFReportTransaction.sql
@@ -0,0 +1,8 @@
+﻿CREATE TYPE [dbo].[TFReportTransaction] AS TABLE
+(
+	strFormCode NVARCHAR(100),
+	strScheduleCode NVARCHAR(100),
+	strType NVARCHAR(100),
+	strProductCode NVARCHAR(100),
+	dblReceived NUMERIC(18, 6)
+)
