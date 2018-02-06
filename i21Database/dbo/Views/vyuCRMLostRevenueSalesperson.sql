72e9e0ad367be6ec0b34cc922fe7d92397ecdf60
Jayson Ramos <jayson.ramos@iRely.com>
2018-02-06 14:52:48 +0800

    CRM-773

---
+++ i21Database/dbo/Views/vyuCRMLostRevenueSalesperson.sql
@@ -0,0 +1,20 @@
+﻿CREATE VIEW [dbo].[vyuCRMLostRevenueSalesperson]
+	AS
+	select
+		a.intEntityId
+		,a.strContactNumber
+		,a.strTitle
+		,a.strMobile
+		,a.strPhone
+		,a.ysnActive
+		,a.strName
+		,a.strEmail
+		,a.strEntityNo
+		,imgPhoto = null
+	from
+		tblEMEntity a
+		,tblEMEntityType b
+	where
+		a.ysnActive = convert(bit, 1)
+		and b.intEntityId = a.intEntityId
+		and b.strType = 'Salesperson'
