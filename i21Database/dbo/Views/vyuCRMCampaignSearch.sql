59deab3100435ad4ec9570ca8c7052343f3ddfc6
Jayson Ramos <jayson.ramos@iRely.com>
2018-02-06 14:53:13 +0800

    CRM-775

--- i21Database/dbo/Views/vyuCRMCampaignSearch.sql
+++ i21Database/dbo/Views/vyuCRMCampaignSearch.sql
@@ -1,6 +1,43 @@
 ﻿CREATE VIEW [dbo].[vyuCRMCampaignSearch]
 	AS
 	select
+		a.intCampaignId
+		,a.strCampaignName
+		,a.strDescription
+		,a.intTypeId
+		,a.intLineOfBusinessId
+		,a.dtmStartDate
+		,a.dtmEndDate
+		,a.dblOpenRate
+		,a.dblBaseCost
+		,a.dblTotalCost
+		,a.dblExpectedRevenue
+		,a.dtmCreateDate
+		,a.intStatusId
+		,a.ysnHold
+		,a.ysnActive
+		,a.intEntityId
+		,a.strRetrospective
+		,a.strImageId
+		,a.intNumberOfAttendee
+		,a.strURL
+		,a.intConcurrencyId
+		,b.strName
+		,c.strLineOfBusiness
+		,d.strType
+		,e.strStatus
+		,strApprovalStatus = h.strStatus
+	from
+		tblCRMCampaign a
+		left join tblEMEntity b on b.intEntityId = a.intEntityId
+		left join tblSMLineOfBusiness c on c.intLineOfBusinessId = a.intLineOfBusinessId
+		left join tblCRMType d on d.intTypeId = a.intTypeId
+		left join tblCRMStatus e on e.intStatusId = a.intStatusId
+		left join tblSMScreen f on f.strModule = 'CRM' and f.strNamespace = 'CRM.view.Campaign'
+		left join tblSMTransaction g on g.intRecordId = a.intCampaignId and g.intScreenId = f.intScreenId
+		left join tblSMApproval h on h.intTransactionId = g.intTransactionId
+	/*
+	select
 		[tblCRMCampaign].*
 		,tblEMEntity.strName
 		,[tblSMLineOfBusiness].strLineOfBusiness
@@ -12,3 +49,4 @@
 		left outer join [tblSMLineOfBusiness] on [tblSMLineOfBusiness].intLineOfBusinessId = [tblCRMCampaign].intLineOfBusinessId
 		left outer join tblCRMType on tblCRMType.intTypeId = [tblCRMCampaign].[intTypeId]
 		left outer join [tblCRMStatus] on [tblCRMStatus].intStatusId = [tblCRMCampaign].intStatusId
+		*/
