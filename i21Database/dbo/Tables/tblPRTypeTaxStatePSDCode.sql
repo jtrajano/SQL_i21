7606028c1710bf1b27a4efbb7a67a12029339898
Arman Arenas <arman.arenas@irely.com>
2018-02-06 14:43:15 +0800

    PR-1579 Re-added tblPRTypeTaxStatePSDCode and default data

---
+++ i21Database/dbo/Tables/tblPRTypeTaxStatePSDCode.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[tblPRTypeTaxStatePSDCode]
+(
+	[intTypeTaxStatePSDCodeId] INT NOT NULL PRIMARY KEY IDENTITY,
+	[strCounty] NVARCHAR(255) COLLATE Latin1_General_CI_AS NULL,
+	[strSchoolDistrict] NVARCHAR(255) COLLATE Latin1_General_CI_AS NULL,
+	[strMunicipality] NVARCHAR(255) COLLATE Latin1_General_CI_AS NULL,
+	[strPSDCode] NVARCHAR(20) COLLATE Latin1_General_CI_AS NULL,
+	[intConcurrencyId] INT NULL DEFAULT ((1))
+)
