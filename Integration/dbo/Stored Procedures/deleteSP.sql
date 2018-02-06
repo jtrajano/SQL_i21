8a71bcb94f70bad5babd203f475e4c9234e4f058
IRELY\kkarthick <karthick.k@irely.com>
2018-02-06 12:30:11 +0530

    CT-1912

--- Integration/dbo/Stored Procedures/deleteSP.sql
+++ Integration/dbo/Stored Procedures/deleteSP.sql
@@ -93,12 +93,12 @@ GO
 IF EXISTS(select top 1 1 from sys.procedures where name = 'uspGRImportCustomerSplit')
 	DROP PROCEDURE uspGRImportCustomerSplit
 GO
-IF EXISTS(select top 1 1 from sys.procedures where name = 'uspGRImportCustomerSplit')
+IF EXISTS(select top 1 1 from sys.procedures where name = 'uspCTImportContract')
 	DROP PROCEDURE uspCTImportContract
 GO
-IF EXISTS(select top 1 1 from sys.procedures where name = 'uspGRImportCustomerSplit')
+IF EXISTS(select top 1 1 from sys.procedures where name = 'uspCTImportContractPlanPt')
 	DROP PROCEDURE uspCTImportContractPlanPt
 GO
-IF EXISTS(select top 1 1 from sys.procedures where name = 'uspGRImportCustomerSplit')
+IF EXISTS(select top 1 1 from sys.procedures where name = 'uspCTImportContractText')
 	DROP PROCEDURE uspCTImportContractText
 GO
