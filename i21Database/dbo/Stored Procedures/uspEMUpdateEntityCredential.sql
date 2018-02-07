e638e889847fa58c9020a98a3f4f96f82a98a117
JBorlagdan <jc.borlagdan@irely.com>
2018-02-07 13:32:12 +0800

    AR-6607

--- i21Database/dbo/Stored Procedures/uspEMUpdateEntityCredential.sql
+++ i21Database/dbo/Stored Procedures/uspEMUpdateEntityCredential.sql
@@ -3,10 +3,12 @@
 	@strUsername AS NVARCHAR(50)
 AS
 BEGIN
+	DECLARE @isDefaultContact AS BIT
+	SET @isDefaultContact = (SELECT ysnDefaultContact FROM tblEMEntityToContact WHERE intEntityContactId = @intEntityContactId)
 	DECLARE @isExist AS BIT
 	SET @isExist = (SELECT 1 FROM tblEMEntityCredential WHERE intEntityId = @intEntityContactId)
 
-	IF(@isExist = 1)
+	IF(@isExist = 1 AND @isDefaultContact = 1)
 	BEGIN
 		UPDATE tblEMEntityCredential
 		SET strUserName = @strUsername
