91a3f606fbc8576bd0ce419ac17469ed17339384
Robin Panganiban <robin.panganiban@irely.com>
2018-02-08 07:34:26 +0800

    AR-6612

---
+++ i21PreDeployment/Scripts/Pre-Deployment/7_AR/11_FixSalesOrderNullysnQuote.sql
@@ -0,0 +1,10 @@
+﻿PRINT '********************** BEGIN updating null ysnQuote For SalesOrder - Quote Type **********************'
+GO
+IF EXISTS(SELECT NULL FROM tblSOSalesOrder WHERE ysnQuote IS NULL)	
+	BEGIN
+		UPDATE tblSOSalesOrder 
+		SET ysnQuote = CASE WHEN (strType = 'Standard - Quote' OR strType = 'Software - Quote') THEN 1 ELSE 0 END
+		WHERE ysnQuote IS NULL
+	END
+GO
+PRINT ' ********************** END updating null ysnQuote For SalesOrder - Quote Type **********************'
