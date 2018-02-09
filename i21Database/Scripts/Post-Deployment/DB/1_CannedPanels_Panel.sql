78699a34fac260ad8aa50584178602efed9d1785
COMTECH\JMendones <jesmar.mendones@irely.com>
2018-02-09 10:07:07 +0800

    DASH-2241

--- i21Database/Scripts/Post-Deployment/DB/1_CannedPanels_Panel.sql
+++ i21Database/Scripts/Post-Deployment/DB/1_CannedPanels_Panel.sql
@@ -820,86 +820,16 @@ print('/*******************  END INSERTING canned panels on temporary panel tabl
 print('/*******************  BEGIN INSERTING payroll canned panels on temporary panel table  *******************/')
 
 INSERT INTO #TempCannedPanels VALUES (15561, 0, 20, 100, 250, 0, 0, 0, 0, 0, 1, 0, N'Master', N'i21 Payroll - Paycheck Tax', N'Grid', N'', N'Paycheck Tax', N'Bar', N'outside', N'Chameleon', N'i21', N'Last Week', N'Last Week', N'PayDate', N'',
-N'Declare @cols nvarchar(max)
-select @cols = stuff( ( select distinct  '',['' + Ltrim(rtrim(strTax)) +'']'' from tblPRTypeTax FOR XML PATH('''')),1,1,'''')
-EXEC(''select * from 
-(
- select distinct
- EmployeeName = strFirstName + '''' '''' + strMiddleName + '''' '''' + strLastName 
- , CheckNo = PCheck.strPaycheckId
- , PayDate = PCheck.dtmPayDate
- , TaxTotal = PTax.dblTotal
- , TaxCode = TType.strTax
- --, TaxDescription = TType.strDescription
- , CheckTotal = PCheck.dblGross
- , CheckNet = PCheck.dblNetPayTotal
- from 
- tblPRPaycheck PCheck
- inner join tblPRPaycheckTax PTax on PCheck.intPaycheckId = PTax.intPaycheckId
- inner join tblPRTypeTax TType on PTax.intTypeTaxId = TType.intTypeTaxId
- inner join tblPREmployee E on E.intEntityEmployeeId = PCheck.intEntityEmployeeId
-) as s PIVOT
-(
- SUM(TaxTotal)
- FOR TaxCode IN ('' + @cols + '')
-)as pvt 
-where @DATE@ '')',
-N'', N'@DATE@', N'@DATE@', N'', N'', N'', N'', N'', N'None', N'', N'', N'', N'', N'', N'', N'', 0, 0, NULL, NULL, N'16.2', NULL, 2, 131, N'')
+N'SELECT * FROM vyuPRPaycheckTaxPanel WHERE @DATE@',
+N'', N'@DATE@', N'@DATE@', N'', N'', N'', N'', N'', N'None', N'', N'', N'', N'', N'', N'', N'', 0, 0, NULL, NULL, N'18.1', NULL, 2, 131, N'')
 
 INSERT INTO #TempCannedPanels VALUES (15562, 50, 15, 100, 250, 0, 0, 0, 0, 0, 1, 0, N'Master', N'i21 Payroll - Paycheck Earnings', N'Grid', N'', N'Paycheck Earnings', N'Bar', N'outside', N'Chameleon', N'i21', N'Last Week', N'Last Week', N'PayDate', N'',
-N'Declare @cols nvarchar(max)
-select @cols = stuff( ( select distinct  '',['' + Ltrim(rtrim(strEarning)) +'']'' from tblPRTypeEarning FOR XML PATH('''')),1,1,'''')
-EXEC(N''select * from 
-(
-	select distinct
-	EmployeeName = strFirstName + '''' '''' + strMiddleName + '''' '''' + strLastName 
-	, CheckNo = PCheck.strPaycheckId
-	, PayDate = PCheck.dtmPayDate
-	, EarningTotal = PTax.dblTotal
-	, EarningCode = TType.strEarning 
-	, CheckTotal = PCheck.dblGross
-	, CheckNet = PCheck.dblNetPayTotal
-	from 
-	tblPRPaycheck PCheck
-	inner join tblPRPaycheckEarning PTax on PCheck.intPaycheckId = PTax.intPaycheckId
-	inner join tblPRTypeEarning TType on PTax.intTypeEarningId = TType.intTypeEarningId
-	inner join tblPREmployee E on E.intEntityEmployeeId = PCheck.intEntityEmployeeId
-) as s
-PIVOT
-(	SUM(EarningTotal)
-	FOR EarningCode IN ('' + @cols + '')
-)as pvt
-where @DATE@ '')' ,
-N'', N'@DATE@', N'@DATE@', N'', N'', N'', N'', N'', N'None', N'', N'', N'', N'', N'', N'', N'', 0, 0, NULL, NULL, N'16.2', NULL, 1, 132, N'')
-
-
+N'SELECT * FROM vyuPRPaycheckEarningPanel WHERE @DATE@',
+N'', N'@DATE@', N'@DATE@', N'', N'', N'', N'', N'', N'None', N'', N'', N'', N'', N'', N'', N'', 0, 0, NULL, NULL, N'18.1', NULL, 1, 132, N'')
 
 INSERT INTO #TempCannedPanels VALUES (15563, 50, 15, 100, 250, 0, 0, 0, 0, 0, 1, 0, N'Master', N'i21 Payroll - Paycheck Deductions', N'Grid', N'', N'Paycheck Deductions', N'Bar', N'outside', N'Chameleon', N'i21', N'Last Week', N'Last Week', N'PayDate', N'',
-N'Declare @cols nvarchar(max)
-select @cols = stuff( ( select distinct '',['' + Ltrim(rtrim(strDeduction)) +'']'' from tblPRTypeDeduction FOR XML PATH('''')),1,1,'''')
-EXEC(N''select * from 
-(
-	select distinct
-	EmployeeName = strFirstName + '''' '''' + strMiddleName + '''' '''' + strLastName 
-	, CheckNo = PCheck.strPaycheckId
-	, PayDate = PCheck.dtmPayDate
-	, DeductionTotal = PTax.dblTotal
-	, DeductionCode = TType.strDeduction 
-	--, TaxDescription = TType.strDescription
-	, CheckTotal = PCheck.dblGross
-	, CheckNet = PCheck.dblNetPayTotal
-	from 
-	tblPRPaycheck PCheck
-	inner join tblPRPaycheckDeduction PTax on PCheck.intPaycheckId = PTax.intPaycheckId
-	inner join tblPRTypeDeduction TType on PTax.intTypeDeductionId = TType.intTypeDeductionId
-	inner join tblPREmployee E on E.intEntityEmployeeId = PCheck.intEntityEmployeeId
-) as s
-PIVOT
-(	SUM(DeductionTotal)
-	FOR DeductionCode IN ('' + @cols + '')
-)as pvt 
-where @DATE@ '')' ,
-N'', N'@DATE@', N'@DATE@', N'', N'', N'', N'', N'', N'None', N'', N'', N'', N'', N'', N'', N'', 0, 0, NULL, NULL, N'16.2', NULL, 1, 133, N'')
+N'SELECT * FROM vyuPRPaycheckDeductionPanel WHERE @DATE@',
+N'', N'@DATE@', N'@DATE@', N'', N'', N'', N'', N'', N'None', N'', N'', N'', N'', N'', N'', N'', 0, 0, NULL, NULL, N'18.1', NULL, 1, 133, N'')
 
 
 print('/*******************  END INSERTING payroll canned panels on temporary panel table  *******************/')
@@ -979,55 +909,55 @@ BEGIN
 			
 
 			UPDATE [dbo].[tblDBPanel]
-			SET intRowsReturned = CannedPanels.intRowsReturned,
-				intRowsVisible = CannedPanels.intRowsVisible,
-				intChartZoom = CannedPanels.intChartZoom,
-				intChartHeight = CannedPanels.intChartHeight,
-				intUserId = CannedPanels.intUserId,	
-				intDefaultColumn = CannedPanels.intDefaultColumn,
-				intDefaultRow = CannedPanels.intDefaultRow,
-				intDefaultWidth = CannedPanels.intDefaultWidth,
-				intSourcePanelId = CannedPanels.intSourcePanelId,
-				intConnectionId = CannedPanels.intConnectionId,
-				intDrillDownPanel = @intDrillDownPanel,
-				strClass = CannedPanels.strClass,
-				strPanelName = CannedPanels.strPanelName,
-				strStyle = CannedPanels.strStyle,
-				strAccessType = CannedPanels.strAccessType,
-				strCaption = CannedPanels.strCaption,
-				strChart = CannedPanels.strChart,
-				strChartPosition = CannedPanels.strChartPosition,
-				strChartColor = CannedPanels.strChartColor,
-				strConnectionName = CannedPanels.strConnectionName,
-				strDateCondition = CannedPanels.strDateCondition,
-				strDateCondition2 = CannedPanels.strDateCondition2,
-				strDateFieldName = CannedPanels.strDateFieldName,
-				strDateFieldName2 = CannedPanels.strDateFieldName2,
-				strDataSource = CannedPanels.strDataSource,
-				strDataSource2 = CannedPanels.strDataSource2,
-				strDateVariable = CannedPanels.strDateVariable,
-				strDateVariable2 = CannedPanels.strDateVariable2,
-				strDefaultTab = CannedPanels.strDefaultTab,
-				strDescription = CannedPanels.strDescription,
-				strPanelNameDuplicate = CannedPanels.strPanelNameDuplicate,
-				strPanelType = CannedPanels.strPanelType,
-				strQBCriteriaOptions = CannedPanels.strQBCriteriaOptions,
-				strFilterCondition = CannedPanels.strFilterCondition,
-				strFilterVariable = CannedPanels.strFilterVariable,
-				strFilterFieldName = CannedPanels.strFilterFieldName,
-				strFilterVariable2 = CannedPanels.strFilterVariable2,
-				strFilterFieldName2 = CannedPanels.strFilterFieldName2,
-				strGroupFields = CannedPanels.strGroupFields,	
-				strFilters = CannedPanels.strFilters,
-				strConfigurator	= CannedPanels.strConfigurator,
-				ysnChartLegend = CannedPanels.ysnChartLegend,
-				ysnShowInGroups = CannedPanels.ysnShowInGroups,
-				imgLayoutGrid = CannedPanels.imgLayoutGrid,
-				imgLayoutPivotGrid = CannedPanels.imgLayoutPivotGrid,			
-				strPanelVersion = CannedPanels.strPanelVersion,	
-				intFilterId = CannedPanels.intFilterId,
-				intConcurrencyId = CannedPanels.intConcurrencyId,
-				strSortValue = CannedPanels.strSortValue			
+			SET intRowsReturned			= CannedPanels.intRowsReturned,
+				intRowsVisible			= CannedPanels.intRowsVisible,
+				intChartZoom			= CannedPanels.intChartZoom,
+				intChartHeight			= CannedPanels.intChartHeight,
+				intUserId				= CannedPanels.intUserId,	
+				intDefaultColumn		= CannedPanels.intDefaultColumn,
+				intDefaultRow			= CannedPanels.intDefaultRow,
+				intDefaultWidth			= CannedPanels.intDefaultWidth,
+				intSourcePanelId		= CannedPanels.intSourcePanelId,
+				intConnectionId			= CannedPanels.intConnectionId,
+				intDrillDownPanel		= @intDrillDownPanel,
+				strClass				= CannedPanels.strClass,
+				strPanelName			= CannedPanels.strPanelName,
+				strStyle				= CannedPanels.strStyle,
+				strAccessType			= CannedPanels.strAccessType,
+				strCaption				= CannedPanels.strCaption,
+				strChart				= CannedPanels.strChart,
+				strChartPosition		= CannedPanels.strChartPosition,
+				strChartColor			= CannedPanels.strChartColor,
+				strConnectionName		= CannedPanels.strConnectionName,
+				strDateCondition		= CannedPanels.strDateCondition,
+				strDateCondition2		= CannedPanels.strDateCondition2,
+				strDateFieldName		= CannedPanels.strDateFieldName,
+				strDateFieldName2		= CannedPanels.strDateFieldName2,
+				strDataSource			= CannedPanels.strDataSource,
+				strDataSource2			= CannedPanels.strDataSource2,
+				strDateVariable			= CannedPanels.strDateVariable,
+				strDateVariable2		= CannedPanels.strDateVariable2,
+				strDefaultTab			= CannedPanels.strDefaultTab,
+				strDescription			= CannedPanels.strDescription,
+				strPanelNameDuplicate	= CannedPanels.strPanelNameDuplicate,
+				strPanelType			= CannedPanels.strPanelType,
+				strQBCriteriaOptions	= CannedPanels.strQBCriteriaOptions,
+				strFilterCondition		= CannedPanels.strFilterCondition,
+				strFilterVariable		= CannedPanels.strFilterVariable,
+				strFilterFieldName		= CannedPanels.strFilterFieldName,
+				strFilterVariable2		= CannedPanels.strFilterVariable2,
+				strFilterFieldName2		= CannedPanels.strFilterFieldName2,
+				strGroupFields			= CannedPanels.strGroupFields,	
+				strFilters				= CannedPanels.strFilters,
+				strConfigurator			= CannedPanels.strConfigurator,
+				ysnChartLegend			= CannedPanels.ysnChartLegend,
+				ysnShowInGroups			= CannedPanels.ysnShowInGroups,
+				imgLayoutGrid			= CannedPanels.imgLayoutGrid,
+				imgLayoutPivotGrid		= CannedPanels.imgLayoutPivotGrid,			
+				strPanelVersion			= CannedPanels.strPanelVersion,	
+				intFilterId				= CannedPanels.intFilterId,
+				intConcurrencyId		= CannedPanels.intConcurrencyId,
+				strSortValue			= CannedPanels.strSortValue			
 			FROM (SELECT * FROM #TempCannedPanels where intCannedPanelId = @intCannedPanelId) AS CannedPanels
 			WHERE [dbo].[tblDBPanel].[intCannedPanelId] = @intCannedPanelId;
 		END	  
