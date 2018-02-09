8bf2b609d718e8dfd1f5373c930bb5c0812a7701
Dan Jalimao <dan.jalimao@irely.com>
2018-02-09 15:27:32 +0800

    EM-2384

--- i21Database/dbo/Triggers/trgDeleteEntityToContact.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE TRIGGER [trgDeleteEntityToContact]
-	 ON [dbo].[tblEMEntityToContact] 
-FOR DELETE
-AS
-	delete from tblEMEntity where intEntityId in (Select intEntityContactId from deleted)
