e95cee88ab35f05779cb2bc9ce18ef6d520a98a7
Gopinath Nayak <gopinath.n@irely.com>
2018-02-13 17:38:14 +0530

    GRN-874

--- Integration/dbo/Stored Procedures/uspGRImportCustomerSplit.sql
+++ Integration/dbo/Stored Procedures/uspGRImportCustomerSplit.sql
@@ -45,7 +45,8 @@ BEGIN
 		  ,strSplitType		= 'Both'
 		  ,intConcurrencyId = 1
 		  FROM sssplmst OSplit
-		  JOIN	tblEMEntity EY	ON	LTRIM(RTRIM(EY.strEntityNo)) collate Latin1_General_CI_AS	= LTRIM(RTRIM(OSplit.ssspl_bill_to_cus))
+		  JOIN	tblEMEntity EY		ON	LTRIM(RTRIM(EY.strEntityNo)) collate Latin1_General_CI_AS	= LTRIM(RTRIM(OSplit.ssspl_bill_to_cus))
+		  JOIN	tblEMEntityType ET	ON	ET.intEntityId = EY.intEntityId AND ET.strType = 'Customer'
 
 	    SET IDENTITY_INSERT tblEMEntitySplit OFF
 
@@ -62,25 +63,39 @@ BEGIN
 		  intSplitId				   = t.A4GLIdentity
 		 ,intEntityId			       = EY.intEntityId
 		 ,dblSplitPercent			   = t.Percentage
-		 ,strOption				       = t.strStorageCode
+		 ,strOption				       = t.strOption
 		 ,intStorageScheduleTypeId     = St.intStorageScheduleTypeId
 		 ,intConcurrencyId             = 1
 		 FROM
 		 ( 
-					   SELECT A4GLIdentity,Entity = ssspl_cus_no_1 , Percentage = ssspl_pct_1,strStorageCode=ssspl_option_1 FROM sssplmst 
-			  UNION ALL SELECT A4GLIdentity,ssspl_cus_no_2,ssspl_pct_2,ssspl_option_2    FROM sssplmst  
-			  UNION ALL SELECT A4GLIdentity,ssspl_cus_no_3,ssspl_pct_3,ssspl_option_3    FROM sssplmst 
-			  UNION ALL SELECT A4GLIdentity,ssspl_cus_no_4,ssspl_pct_4,ssspl_option_4    FROM sssplmst 
-			  UNION ALL SELECT A4GLIdentity,ssspl_cus_no_5,ssspl_pct_5,ssspl_option_5    FROM sssplmst 
-			  UNION ALL SELECT A4GLIdentity,ssspl_cus_no_6,ssspl_pct_6,ssspl_option_6    FROM sssplmst 
-			  UNION ALL SELECT A4GLIdentity,ssspl_cus_no_7,ssspl_pct_7,ssspl_option_7    FROM sssplmst 
-			  UNION ALL SELECT A4GLIdentity,ssspl_cus_no_8,ssspl_pct_8,ssspl_option_8    FROM sssplmst 
-			  UNION ALL SELECT A4GLIdentity,ssspl_cus_no_9,ssspl_pct_9,ssspl_option_9    FROM sssplmst 
-			  UNION ALL SELECT A4GLIdentity,ssspl_cus_no_10,ssspl_pct_10,ssspl_option_10 FROM sssplmst 
-			  UNION ALL SELECT A4GLIdentity,ssspl_cus_no_11,ssspl_pct_11,ssspl_option_11 FROM sssplmst 
-			  UNION ALL SELECT A4GLIdentity,ssspl_cus_no_12,ssspl_pct_12,ssspl_option_12 FROM sssplmst
+			SELECT 
+			 A4GLIdentity
+			,Entity
+			,Percentage
+			,strOption = CASE 
+								  WHEN LTRIM(RTRIM(t1.strStorageCode))='S' THEN 'Spot Sale'
+								  WHEN LTRIM(RTRIM(t1.strStorageCode))='C' THEN 'Contract'
+								  ELSE 'StorageType'
+						 END
+            ,strStorageCode = t1.strStorageCode
+			FROM 
+			  (
+					SELECT A4GLIdentity,Entity = ssspl_cus_no_1 , Percentage = ssspl_pct_1,strStorageCode=ssspl_option_1 FROM sssplmst 
+					UNION ALL SELECT A4GLIdentity,ssspl_cus_no_2,ssspl_pct_2,ssspl_option_2    FROM sssplmst  
+					UNION ALL SELECT A4GLIdentity,ssspl_cus_no_3,ssspl_pct_3,ssspl_option_3    FROM sssplmst 
+					UNION ALL SELECT A4GLIdentity,ssspl_cus_no_4,ssspl_pct_4,ssspl_option_4    FROM sssplmst 
+					UNION ALL SELECT A4GLIdentity,ssspl_cus_no_5,ssspl_pct_5,ssspl_option_5    FROM sssplmst 
+					UNION ALL SELECT A4GLIdentity,ssspl_cus_no_6,ssspl_pct_6,ssspl_option_6    FROM sssplmst 
+					UNION ALL SELECT A4GLIdentity,ssspl_cus_no_7,ssspl_pct_7,ssspl_option_7    FROM sssplmst 
+					UNION ALL SELECT A4GLIdentity,ssspl_cus_no_8,ssspl_pct_8,ssspl_option_8    FROM sssplmst 
+					UNION ALL SELECT A4GLIdentity,ssspl_cus_no_9,ssspl_pct_9,ssspl_option_9    FROM sssplmst 
+					UNION ALL SELECT A4GLIdentity,ssspl_cus_no_10,ssspl_pct_10,ssspl_option_10 FROM sssplmst 
+					UNION ALL SELECT A4GLIdentity,ssspl_cus_no_11,ssspl_pct_11,ssspl_option_11 FROM sssplmst 
+					UNION ALL SELECT A4GLIdentity,ssspl_cus_no_12,ssspl_pct_12,ssspl_option_12 FROM sssplmst
+			 )t1
 		 )t 
 		 JOIN	tblEMEntity EY	ON	LTRIM(RTRIM(EY.strEntityNo)) collate Latin1_General_CI_AS	= LTRIM(RTRIM(t.Entity))
+		 JOIN	tblEMEntityType ET	ON	ET.intEntityId = EY.intEntityId AND ET.strType = 'Customer'
 		 LEFT  JOIN tblGRStorageType St ON St.strStorageTypeCode collate Latin1_General_CI_AS = t.strStorageCode
 		 WHERE t.Entity IS NOT NULL AND t.Percentage >0
 
