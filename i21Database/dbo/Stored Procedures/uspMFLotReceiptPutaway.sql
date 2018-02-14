e37952ea3bae95ce30b166951aee228d9be53b2d
Premkumar S <prem.selladurai@irely.com>
2018-02-14 22:26:58 +0530

    MFG-3274

--- i21Database/dbo/Stored Procedures/uspMFLotReceiptPutaway.sql
+++ i21Database/dbo/Stored Procedures/uspMFLotReceiptPutaway.sql
@@ -1,28 +1,63 @@
 ﻿CREATE PROCEDURE uspMFLotReceiptPutaway (
-	@strReceiptNumber NVARCHAR(50)
-	,@strLotNumber NVARCHAR(50)
+	@intInventoryReceiptItemLotId INT
 	,@strStorageLocationName NVARCHAR(50)
+	,@intLocationId INT
+	,@intUserId INT
 	)
 AS
 BEGIN TRY
 	DECLARE @intInventoryReceiptId INT
 		,@ErrMsg NVARCHAR(MAX)
 		,@intStorageLocationId INT
-
-	SELECT @intInventoryReceiptId = intInventoryReceiptId
-	FROM dbo.tblICInventoryReceipt
-	WHERE strReceiptNumber = @strReceiptNumber
+		,@intSubLocationId INT
+		,@strSubLocationName NVARCHAR(50)
 
 	SELECT @intStorageLocationId = intStorageLocationId
+		,@intSubLocationId = intSubLocationId
 	FROM dbo.tblICStorageLocation
 	WHERE strName = @strStorageLocationName
+		AND intLocationId = @intLocationId
+
+	IF @intStorageLocationId IS NULL
+	BEGIN
+		RAISERROR (
+				'INVALID LOCATION.'
+				,16
+				,1
+				)
+
+		RETURN
+	END
+
+	IF NOT EXISTS (
+			SELECT 1
+			FROM dbo.tblICInventoryReceiptItemLot IRL
+			JOIN dbo.tblICInventoryReceiptItem IRI ON IRI.intInventoryReceiptItemId = IRL.intInventoryReceiptItemId
+			WHERE IRL.intInventoryReceiptItemLotId = @intInventoryReceiptItemLotId
+				AND IRI.intSubLocationId = @intSubLocationId
+			)
+	BEGIN
+		SELECT @strSubLocationName = SL.strSubLocationName
+		FROM dbo.tblICInventoryReceiptItemLot IRL
+		JOIN dbo.tblICInventoryReceiptItem IRI ON IRI.intInventoryReceiptItemId = IRL.intInventoryReceiptItemId
+		JOIN dbo.tblSMCompanyLocationSubLocation SL ON SL.intCompanyLocationSubLocationId = IRI.intSubLocationId
+		WHERE IRL.intInventoryReceiptItemLotId = @intInventoryReceiptItemLotId
+
+		SET @ErrMsg = 'SCANNED LOCATION DOES NOT BELONG TO THE SUB LOCATION ''' + @strSubLocationName+''''
+
+		RAISERROR (
+				@ErrMsg
+				,16
+				,1
+				)
+
+		RETURN
+	END
 
 	UPDATE dbo.tblICInventoryReceiptItemLot
 	SET intStorageLocationId = @intStorageLocationId
-	FROM dbo.tblICInventoryReceiptItemLot IRL
-	JOIN dbo.tblICInventoryReceiptItem IRI ON IRI.intInventoryReceiptItemId = IRL.intInventoryReceiptItemId
-	WHERE IRL.strLotNumber = @strLotNumber
-		AND IRI.intInventoryReceiptId = @intInventoryReceiptId
+		,intConcurrencyId = intConcurrencyId + 1
+	WHERE intInventoryReceiptItemLotId = @intInventoryReceiptItemLotId
 END TRY
 
 BEGIN CATCH
