8e85d31da1de2e29740f1f5d4aed76c4a84ee9a6
IRELY\swghosh <swetosuvro.g@irely.com>
2018-02-15 21:57:39 +0530

    LG-1368

--- i21Database/dbo/Views/vyuLGStockValuationForBank.sql
+++ i21Database/dbo/Views/vyuLGStockValuationForBank.sql
@@ -62,23 +62,29 @@ SELECT DISTINCT CH.intContractHeaderId
 	,U.intUnitMeasureId
 	,CD.intPriceItemUOMId
 	,ROUND(CASE 
-		WHEN C.ysnSubCurrency = 1
-			THEN dbo.fnCTConvertQtyToTargetItemUOM((
+			WHEN C.ysnSubCurrency = 1
+				THEN dbo.fnCTConvertQtyToTargetItemUOM((
+							SELECT intItemUOMId
+							FROM tblICItemUOM IU
+							JOIN tblICUnitMeasure UM ON UM.intUnitMeasureId = IU.intUnitMeasureId
+							WHERE IU.intItemId = CD.intItemId
+								AND UM.strUnitMeasure IN (
+									'Metric Ton'
+									,'MT'
+									)
+							), CD.intPriceItemUOMId, 1) * CD.dblCashPrice / 100
+			ELSE dbo.fnCTConvertQtyToTargetItemUOM((
 						SELECT intItemUOMId
 						FROM tblICItemUOM IU
 						JOIN tblICUnitMeasure UM ON UM.intUnitMeasureId = IU.intUnitMeasureId
 						WHERE IU.intItemId = CD.intItemId
-							AND UM.strUnitMeasure = 'MT'
-						), CD.intPriceItemUOMId, 1) * CD.dblCashPrice / 100
-		ELSE dbo.fnCTConvertQtyToTargetItemUOM((
-					SELECT intItemUOMId
-					FROM tblICItemUOM IU
-					JOIN tblICUnitMeasure UM ON UM.intUnitMeasureId = IU.intUnitMeasureId
-					WHERE IU.intItemId = CD.intItemId
-						AND UM.strUnitMeasure = 'MT'
-					), CD.intPriceItemUOMId, 1) * CD.dblCashPrice
-		END,4) AS dblPriceInMT
-	,'Warehouse' strStorageLocation
+							AND UM.strUnitMeasure IN (
+								'Metric Ton'
+								,'MT'
+								)
+						), CD.intPriceItemUOMId, 1) * CD.dblCashPrice
+			END, 4) AS dblPriceInMT
+	,SL.strName AS strStorageLocation
 	,CD.dblQuantity AS dblPurchaseQty
 	,UM.strUnitMeasure AS strPurchaseQtyUOM
 	,dbo.fnCTConvertQtyToTargetItemUOM(CD.intItemUOMId, (
@@ -86,45 +92,96 @@ SELECT DISTINCT CH.intContractHeaderId
 			FROM tblICItemUOM IU
 			JOIN tblICUnitMeasure UM ON UM.intUnitMeasureId = IU.intUnitMeasureId
 			WHERE IU.intItemId = CD.intItemId
-				AND UM.strUnitMeasure = 'MT'
+				AND UM.strUnitMeasure IN (
+					'Metric Ton'
+					,'MT'
+					)
 			), 1) * (CD.dblQuantity - ISNULL(CD.dblAllocatedQty, 0)) AS dblUnAllocatedQtyInMT
-	,'Unsold Commodity Value' AS dblUnsoldCommodityValue
 	,(
 		dbo.fnCTConvertQtyToTargetItemUOM(CD.intItemUOMId, (
 				SELECT intItemUOMId
 				FROM tblICItemUOM IU
 				JOIN tblICUnitMeasure UM ON UM.intUnitMeasureId = IU.intUnitMeasureId
 				WHERE IU.intItemId = CD.intItemId
-					AND UM.strUnitMeasure = 'MT'
+					AND UM.strUnitMeasure IN (
+						'Metric Ton'
+						,'MT'
+						)
 				), 1) * (CD.dblQuantity - ISNULL(CD.dblAllocatedQty, 0))
-		) * dbo.fnRKGetLatestClosingPrice(3, 17, GeTdate()) AS dblUnsoldCommodityMarketValue
+		) * ROUND(CASE 
+			WHEN C.ysnSubCurrency = 1
+				THEN dbo.fnCTConvertQtyToTargetItemUOM((
+							SELECT intItemUOMId
+							FROM tblICItemUOM IU
+							JOIN tblICUnitMeasure UM ON UM.intUnitMeasureId = IU.intUnitMeasureId
+							WHERE IU.intItemId = CD.intItemId
+								AND UM.strUnitMeasure IN (
+									'Metric Ton'
+									,'MT'
+									)
+							), CD.intPriceItemUOMId, 1) * CD.dblCashPrice / 100
+			ELSE dbo.fnCTConvertQtyToTargetItemUOM((
+						SELECT intItemUOMId
+						FROM tblICItemUOM IU
+						JOIN tblICUnitMeasure UM ON UM.intUnitMeasureId = IU.intUnitMeasureId
+						WHERE IU.intItemId = CD.intItemId
+							AND UM.strUnitMeasure IN (
+								'Metric Ton'
+								,'MT'
+								)
+						), CD.intPriceItemUOMId, 1) * CD.dblCashPrice
+			END, 4) AS dblUnsoldCommodityValue
+	,(
+		dbo.fnCTConvertQtyToTargetItemUOM(CD.intItemUOMId, (
+				SELECT intItemUOMId
+				FROM tblICItemUOM IU
+				JOIN tblICUnitMeasure UM ON UM.intUnitMeasureId = IU.intUnitMeasureId
+				WHERE IU.intItemId = CD.intItemId
+					AND UM.strUnitMeasure IN (
+						'Metric Ton'
+						,'MT'
+						)
+				), 1) * (CD.dblQuantity - ISNULL(CD.dblAllocatedQty, 0))
+		) * dbo.fnRKGetLatestClosingPrice(CD.intFutureMarketId, CD.intFutureMonthId, GeTdate()) AS dblUnsoldCommodityMarketValue
 	,LD.dblQuantity AS dblLGLoadQty
-	,(SELECT TOP 1 strLocationName FROM tblSMCompanyLocationSubLocation CLSL WHERE CLSL.intCompanyLocationSubLocationId = LW.intSubLocationId) AS strWarehouse
+	,(
+		SELECT TOP 1 strLocationName
+		FROM tblSMCompanyLocationSubLocation CLSL
+		WHERE CLSL.intCompanyLocationSubLocationId = LW.intSubLocationId
+		) AS strWarehouse
 	,dbo.fnCTConvertQtyToTargetItemUOM(CD.intItemUOMId, (
 			SELECT intItemUOMId
 			FROM tblICItemUOM IU
 			JOIN tblICUnitMeasure UM ON UM.intUnitMeasureId = IU.intUnitMeasureId
 			WHERE IU.intItemId = CD.intItemId
-				AND UM.strUnitMeasure = 'MT'
-			), 1) * (CD.dblQuantity - ISNULL(CD.dblAllocatedQty, 0)) *	ROUND(CASE 
-		WHEN C.ysnSubCurrency = 1
-			THEN dbo.fnCTConvertQtyToTargetItemUOM((
+				AND UM.strUnitMeasure IN (
+					'Metric Ton'
+					,'MT'
+					)
+			), 1) * (CD.dblQuantity - ISNULL(CD.dblAllocatedQty, 0)) * ROUND(CASE 
+			WHEN C.ysnSubCurrency = 1
+				THEN dbo.fnCTConvertQtyToTargetItemUOM((
+							SELECT intItemUOMId
+							FROM tblICItemUOM IU
+							JOIN tblICUnitMeasure UM ON UM.intUnitMeasureId = IU.intUnitMeasureId
+							WHERE IU.intItemId = CD.intItemId
+								AND UM.strUnitMeasure IN (
+									'Metric Ton'
+									,'MT'
+									)
+							), CD.intPriceItemUOMId, 1) * CD.dblCashPrice / 100
+			ELSE dbo.fnCTConvertQtyToTargetItemUOM((
 						SELECT intItemUOMId
 						FROM tblICItemUOM IU
 						JOIN tblICUnitMeasure UM ON UM.intUnitMeasureId = IU.intUnitMeasureId
 						WHERE IU.intItemId = CD.intItemId
-							AND UM.strUnitMeasure = 'MT'
-						), CD.intPriceItemUOMId, 1) * CD.dblCashPrice / 100
-		ELSE dbo.fnCTConvertQtyToTargetItemUOM((
-					SELECT intItemUOMId
-					FROM tblICItemUOM IU
-					JOIN tblICUnitMeasure UM ON UM.intUnitMeasureId = IU.intUnitMeasureId
-					WHERE IU.intItemId = CD.intItemId
-						AND UM.strUnitMeasure = 'MT'
-					), CD.intPriceItemUOMId, 1) * CD.dblCashPrice
-		END,4) * dblRate AS dblUnsoldCommodityInUSD
-	, dbo.fnRKGetLatestClosingPrice(CD.intFutureMarketId,CD.intFutureMonthId, GeTdate()) AS dblMarketClosingPrice
-
+							AND UM.strUnitMeasure IN (
+								'Metric Ton'
+								,'MT'
+								)
+						), CD.intPriceItemUOMId, 1) * CD.dblCashPrice
+			END, 4) * dblRate AS dblUnsoldCommodityInUSD
+	,dbo.fnRKGetLatestClosingPrice(CD.intFutureMarketId, CD.intFutureMonthId, GETDATE()) AS dblMarketClosingPrice
 FROM tblCTContractHeader CH
 JOIN tblCTContractDetail CD ON CD.intContractHeaderId = CH.intContractHeaderId
 	AND CH.intContractTypeId = 1
@@ -135,12 +192,15 @@ JOIN tblICItemUOM IU ON IU.intItemUOMId = CD.intPriceItemUOMId
 JOIN tblICUnitMeasure U ON U.intUnitMeasureId = IU.intUnitMeasureId
 JOIN tblICItemUOM IUM ON IUM.intItemUOMId = CD.intItemUOMId
 JOIN tblICUnitMeasure UM ON UM.intUnitMeasureId = IUM.intUnitMeasureId
-LEFT JOIN tblLGLoadDetail LD ON LD.intPContractDetailId = CD.intContractDetailId 
+LEFT JOIN tblLGLoadDetail LD ON LD.intPContractDetailId = CD.intContractDetailId
 LEFT JOIN tblLGLoad L ON L.intLoadId = LD.intLoadId
+	AND L.intShipmentType = 1
 LEFT JOIN tblLGLoadWarehouse LW ON LW.intLoadId = L.intLoadId
+LEFT JOIN tblSMCompanyLocationSubLocation CLSL ON CLSL.intCompanyLocationSubLocationId = LW.intSubLocationId
+LEFT JOIN tblICStorageLocation SL ON SL.intStorageLocationId = LW.intStorageLocationId
 LEFT JOIN tblSMCurrency CU ON CU.intCurrencyID = C.intMainCurrencyId
 LEFT JOIN tblICCommodityAttribute ProductType ON ProductType.intCommodityAttributeId = I.intProductTypeId
 LEFT JOIN tblICCommodityProductLine ProductLine ON ProductLine.intCommodityProductLineId = I.intProductLineId
 LEFT JOIN tblRKFutureMarket FMA ON FMA.intFutureMarketId = CD.intFutureMarketId
 LEFT JOIN tblRKFuturesMonth FMO ON FMO.intFutureMonthId = CD.intFutureMonthId
-WHERE L.intShipmentType = 1
+LEFT JOIN tblARInvoiceDetail ID ON ID.intLoadDetailId = LD.intLoadDetailId
