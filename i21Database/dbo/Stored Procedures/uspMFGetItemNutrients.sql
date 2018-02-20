4a6575b6c55a942e1cfb260073717a4ddd15f1cf
Chakra <chakradhar.behera@irely.com>
2018-02-20 22:25:20 +0530

    MFG-3271

--- i21Database/dbo/Stored Procedures/uspMFGetItemNutrients.sql
+++ i21Database/dbo/Stored Procedures/uspMFGetItemNutrients.sql
@@ -1,9 +1,9 @@
 ﻿CREATE PROCEDURE [dbo].[uspMFGetItemNutrients]
-	@intItemId int
+	@strItemIds NVARCHAR(MAX)
 AS
 
 Select p.intProductValueId AS intItemId,pp.intPropertyId,vp.dblMinValue AS dblValue
 From tblQMProduct p 
 Join tblQMProductProperty pp on p.intProductId=pp.intProductId
 Join tblQMProductPropertyValidityPeriod vp on pp.intProductPropertyId=vp.intProductPropertyId
-Where p.intProductValueId=@intItemId AND p.intProductTypeId=2
+Where p.intProductValueId in (Select * from dbo.[fnCommaSeparatedValueToTable](@strItemIds)) AND p.intProductTypeId=2
