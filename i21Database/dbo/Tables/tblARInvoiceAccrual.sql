eeb34c36af73a8b1f0f5914f6591f18d02413fa5
Robin Panganiban <robin.panganiban@irely.com>
2018-02-21 15:38:33 +0800

    AR-6705

--- i21Database/dbo/Tables/tblARInvoiceAccrual.sql
+++ i21Database/dbo/Tables/tblARInvoiceAccrual.sql
@@ -7,6 +7,6 @@
 	[intConcurrencyId] INT CONSTRAINT [DF_tblARInvoiceAccrual_intConcurrencyId] DEFAULT ((0)) NOT NULL,
 	CONSTRAINT [PK_tblARInvoiceAccrual_intInvoiceAccrualId] PRIMARY KEY CLUSTERED ([intInvoiceAccrualId] ASC),
 	CONSTRAINT [FK_tblARInvoiceAccrual_tblARInvoice] FOREIGN KEY ([intInvoiceId]) REFERENCES [dbo].[tblARInvoice] ([intInvoiceId]),
-	CONSTRAINT [FK_tblARInvoiceACcrual_tblARInvoiceDetail] FOREIGN KEY ([intInvoiceDetailId]) REFERENCES [dbo].[tblARInvoiceDetail] ([intInvoiceDetailId])
+	CONSTRAINT [FK_tblARInvoiceACcrual_tblARInvoiceDetail] FOREIGN KEY ([intInvoiceDetailId]) REFERENCES [dbo].[tblARInvoiceDetail] ([intInvoiceDetailId]) ON DELETE CASCADE
 
 )
