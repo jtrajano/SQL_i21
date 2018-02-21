35c7aa9aacd11b4cf1856697c35529241629a007
Wendell Wayne Estrada <wendell.estrada@irely.com>
2018-02-21 15:59:37 +0800

    IC-4898 Inventory Shipment - Per unit tax is not calculating for other charges

--- i21Database/dbo/Views/vyuICGetShipmentChargeTaxDetails.sql
+++ i21Database/dbo/Views/vyuICGetShipmentChargeTaxDetails.sql
@@ -16,10 +16,16 @@ SELECT
 	ChargeTax.dblQty,
 	ChargeTax.dblCost,
 	ChargeTax.ysnCheckoffTax,
-	ChargeTax.ysnTaxAdjusted
+	ChargeTax.ysnTaxAdjusted,
+	ChargeTax.intUnitMeasureId,
+	UnitMeasure.strUnitMeasure
 FROM	dbo.tblICInventoryShipmentChargeTax ChargeTax
 		LEFT JOIN dbo.tblICInventoryShipmentCharge Charge on Charge.intInventoryShipmentChargeId = ChargeTax.intInventoryShipmentChargeId
 		LEFT JOIN dbo.tblICItem Item on Item.intItemId = Charge.intChargeId 
 		LEFT JOIN dbo.tblSMTaxGroup TaxGroup on TaxGroup.intTaxGroupId = ChargeTax.intTaxGroupId			
 		LEFT JOIN dbo.tblSMTaxCode TaxCode on TaxCode.intTaxCodeId = ChargeTax.intTaxCodeId
 		LEFT JOIN dbo.tblSMTaxClass TaxClass ON TaxClass.intTaxClassId = TaxCode.intTaxClassId
+		LEFT JOIN tblSMTaxCodeRate TaxCodeRate ON TaxCodeRate.intTaxCodeId = TaxCode.intTaxCodeId
+			AND TaxCodeRate.intUnitMeasureId = ChargeTax.intUnitMeasureId
+		LEFT JOIN tblICItemUOM ItemUOM ON ItemUOM.intItemUOMId = ChargeTax.intUnitMeasureId
+		LEFT JOIN tblICUnitMeasure UnitMeasure ON UnitMeasure.intUnitMeasureId = ItemUOM.intUnitMeasureId
