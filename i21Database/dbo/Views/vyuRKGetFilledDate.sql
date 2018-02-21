55b7708783d84a9914a76c2fc9bd820934277f95
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2018-02-21 20:19:09 +0530

    RM-1069

--- i21Database/dbo/Views/vyuRKGetFilledDate.sql
+++ i21Database/dbo/Views/vyuRKGetFilledDate.sql
@@ -6,4 +6,4 @@ SELECT top 100 percent CONVERT(INT,ROW_NUMBER() OVER(ORDER BY CONVERT(VARCHAR(10
 															WHERE isnull(t.ysnFreezed,0) = 1 and t.intFutureMarketId=t1.intFutureMarketId
 																and t.intBrokerageAccountId=t1.intBrokerageAccountId and t.intCommodityId=t1.intCommodityId
 																and t.intEntityId=t1.intEntityId) 	
-)t order by convert(datetime,CONVERT(VARCHAR(10),dtmFilledDate,110)) asc
+)t  WHERE dtmFilledDate IS NOT NULL ORDER BY CONVERT(DATETIME,CONVERT(VARCHAR(10),dtmFilledDate,110)) asc
