2dc4ebab30431008e9a6ff2873292661720d041b
JBorlagdan <jc.borlagdan@irely.com>
2018-02-21 15:51:02 +0800

    AR-6692

--- i21Database/dbo/Stored Procedures/uspARCustomerStatementReport.sql
+++ i21Database/dbo/Stored Procedures/uspARCustomerStatementReport.sql
@@ -427,15 +427,27 @@ IF @ysnIncludeBudgetLocal = 1
 		EXEC sp_executesql @queryBudget
 	END
 
+
 IF @ysnPrintOnlyPastDueLocal = 0
 	BEGIN		
-		DELETE FROM @temp_statement_table WHERE DATEDIFF(DAYOFYEAR, dtmDueDate, @dtmDateToLocal) > 0		
+		DELETE FROM @temp_statement_table WHERE DATEDIFF(DAYOFYEAR, dtmDueDate, @dtmDateToLocal) > 0
+
+		UPDATE tblARCustomerAgingStagingTable
+		SET 
+			dbl10Days = 0,
+			dbl30Days = 0,
+			dbl60Days = 0,
+			dbl90Days = 0,
+			dbl91Days = 0
 	END
-	
 ELSE IF @ysnPrintOnlyPastDueLocal = 1
-BEGIN
-	DELETE FROM @temp_statement_table WHERE strTransactionType = 'Invoice' AND dblPastDue <= 0
-END
+	BEGIN
+		DELETE FROM @temp_statement_table WHERE strTransactionType = 'Invoice' AND dblPastDue <= 0
+
+		UPDATE tblARCustomerAgingStagingTable
+		SET dbl0Days = 0
+	END
+
 
 IF @ysnPrintZeroBalanceLocal = 0
 	BEGIN
