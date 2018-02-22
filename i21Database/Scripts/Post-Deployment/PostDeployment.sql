ded5905ba8897aaf205812e626811029dd1763a8
KEstrada <kevin.estrada@irely.com>
2018-02-22 15:43:27 +0800

    SM-3757: postdeployment.sql

--- i21Database/Scripts/Post-Deployment/PostDeployment.sql
+++ i21Database/Scripts/Post-Deployment/PostDeployment.sql
@@ -48,6 +48,8 @@ print 'BEGIN POST DEPLOYMENT'
 :r .\SM\DefaultData\29_TransportationMode.sql
 :r .\SM\DefaultData\30_ImportFileHeader.sql
 :r .\SM\DefaultData\31_Calendar.sql
+:r .\SM\DefaultData\32_ReplicationResult.sql
+:r .\SM\DefaultData\33_ReplicationInitStatus.sql
 :r .\SM\CreateEncryptionCertificateAndSymmetricKey.sql
 :r .\SM\CustomField.sql
 :r .\SM\1730_UpdateOriginSubMenusSorting.sql
