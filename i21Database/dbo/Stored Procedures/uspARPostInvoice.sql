cc4206d1d2d865b9984ff3a1eb03892c57d2872f
Kelvin Quinto <kelvin.quinto@irely.com>
2018-02-22 11:43:09 +0800

    AR-6771

--- i21Database/dbo/Stored Procedures/uspARPostInvoice.sql
+++ i21Database/dbo/Stored Procedures/uspARPostInvoice.sql
@@ -1365,6 +1365,7 @@ BEGIN TRY
 			WHERE I.intInvoiceId IN (SELECT intInvoiceId FROM @PostInvoiceData)
 			AND ID.ysnBlended <> @post
 			AND ICI.ysnAutoBlend = 1
+			AND I.strTransactionType NOT IN ('Credit Memo', 'Customer Prepayment', 'Overpayment')
 			AND ISNULL(ICI.strType,'') = 'Finished Good'
 
 			WHILE EXISTS (SELECT NULL FROM @FinishedGoodItems)
