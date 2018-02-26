d45b7900917f62ad018774390c733008dd363069
Ruben Escarcha <ruben.escarcha@irely.com>
2018-02-27 06:21:04 +0800

    AR-6810 [Invoice] - Invoice is not using Inventory Shipment unit Price.

--- i21Database/dbo/Functions/fnARGetInvalidInvoicesForPosting.sql
+++ i21Database/dbo/Functions/fnARGetInvalidInvoicesForPosting.sql
@@ -1122,11 +1122,11 @@ IF(ISNULL(@Post,0)) = 1
 			,[intInvoiceDetailId]	= I.[intInvoiceDetailId]
 			,[intItemId]			= I.[intItemId]
 			,[strBatchId]			= I.[strBatchId]
-			,[strPostingError]		= 'The contract item - ' + ICI.[strItemNo] + ' price(' + CONVERT(NVARCHAR(100),CAST(ISNULL(ARID.[dblPrice],@ZeroDecimal) AS MONEY),2) + ') is not equal to the contract sequence cash price(' + CONVERT(NVARCHAR(100),CAST(ISNULL(CASE WHEN ARID.[intLoadDetailId] IS NULL AND ARID.[intShipmentId] IS NULL THEN ARCC.[dblUnitPrice] ELSE ARCC.[dblCashPrice] END, @ZeroDecimal) AS MONEY),2) + ').'
+			,[strPostingError]		= 'The contract item - ' + ICI.[strItemNo] + ' price(' + CONVERT(NVARCHAR(100),CAST(ISNULL(ARID.[dblPrice],@ZeroDecimal) AS MONEY),2) + ') is not equal to the contract sequence cash price(' + CONVERT(NVARCHAR(100),CAST(ISNULL(ARCC.[dblCashPrice], @ZeroDecimal) AS MONEY),2) + ').'
 		FROM 					
 			@Invoices I
 		INNER JOIN
-			(SELECT [intInvoiceId], [intItemId], [intContractHeaderId], [intContractDetailId], [dblPrice], [intLoadDetailId], [intShipmentId] FROM tblARInvoiceDetail WITH (NOLOCK)) ARID
+			(SELECT [intInvoiceId], [intItemId], [intContractHeaderId], [intContractDetailId], [dblPrice], [intLoadDetailId], [intShipmentId], [intInventoryShipmentItemId] FROM tblARInvoiceDetail WITH (NOLOCK)) ARID
 				ON I.[intInvoiceId] = ARID.[intInvoiceId]
 		INNER JOIN
 			(SELECT [intItemId], [strItemNo] FROM tblICItem WITH (NOLOCK) WHERE strType NOT IN ('Other Charge')) ICI
@@ -1140,9 +1140,11 @@ IF(ISNULL(@Post,0)) = 1
 				AND ARID.[intContractDetailId] = ARCC.[intContractDetailId] 			 				
 		WHERE
 			ARID.[dblPrice] <> @ZeroDecimal				
-			AND (CAST(ISNULL(CASE WHEN ARID.[intLoadDetailId] IS NULL AND ARID.[intShipmentId] IS NULL THEN ARCC.[dblUnitPrice] ELSE ARCC.[dblCashPrice] END, @ZeroDecimal) AS MONEY) <> CAST(ISNULL(ARID.[dblPrice], @ZeroDecimal) AS MONEY))
+			AND (CAST(ISNULL(ARCC.[dblCashPrice], @ZeroDecimal) AS MONEY) <> CAST(ISNULL(ARID.[dblPrice], @ZeroDecimal) AS MONEY))
 			AND ARCC.[strPricingType] <> 'Index'
 			AND ISNULL(ARID.[intLoadDetailId],0) = 0
+			AND ISNULL(ARID.[intShipmentId],0) = 0
+			AND ISNULL(ARID.[intInventoryShipmentItemId],0) = 0
 
 		UNION
 		--Lot Tracked Item - Direct Invoice
