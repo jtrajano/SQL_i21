6797116cb55361499876ada8885bab4195701389
Jayson Ramos <jayson.ramos@iRely.com>
2018-02-27 14:43:18 +0800

    CRM-775

--- i21Database/dbo/Views/vyuCRMCampaignSearch.sql
+++ i21Database/dbo/Views/vyuCRMCampaignSearch.sql
@@ -1,8 +1,5 @@
 ﻿CREATE VIEW [dbo].[vyuCRMCampaignSearch]
 	AS
-	with result as (
-		select z.intApprovalId, z.intTransactionId, z.strStatus from tblSMApproval z
-	)
 	select
 		a.intCampaignId
 		,a.strCampaignName
@@ -29,7 +26,7 @@
 		,c.strLineOfBusiness
 		,d.strType
 		,e.strStatus
-		,strApprovalStatus = (select i.strStatus from result i where i.intApprovalId = max(h.intApprovalId))
+		,strApprovalStatus = h.strStatus
 	from
 		tblCRMCampaign a
 		left join tblEMEntity b on b.intEntityId = a.intEntityId
@@ -38,30 +35,4 @@
 		left join tblCRMStatus e on e.intStatusId = a.intStatusId
 		left join tblSMScreen f on f.strModule = 'CRM' and f.strNamespace = 'CRM.view.Campaign'
 		left join tblSMTransaction g on g.intRecordId = a.intCampaignId and g.intScreenId = f.intScreenId
-		left join tblSMApproval h on h.intTransactionId = g.intTransactionId
-	group by
-		a.intCampaignId
-		,a.strCampaignName
-		,a.strDescription
-		,a.intTypeId
-		,a.intLineOfBusinessId
-		,a.dtmStartDate
-		,a.dtmEndDate
-		,a.dblOpenRate
-		,a.dblBaseCost
-		,a.dblTotalCost
-		,a.dblExpectedRevenue
-		,a.dtmCreateDate
-		,a.intStatusId
-		,a.ysnHold
-		,a.ysnActive
-		,a.intEntityId
-		,a.strRetrospective
-		,a.strImageId
-		,a.intNumberOfAttendee
-		,a.strURL
-		,a.intConcurrencyId
-		,b.strName
-		,c.strLineOfBusiness
-		,d.strType
-		,e.strStatus
+		left join tblSMApproval h on h.intTransactionId = g.intTransactionId and h.ysnCurrent = convert(bit,1)
