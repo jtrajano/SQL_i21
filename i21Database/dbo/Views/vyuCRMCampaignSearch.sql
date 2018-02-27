9f9192975da782698519d1c44580be58fac95d26
Jayson Ramos <jayson.ramos@iRely.com>
2018-02-27 13:59:14 +0800

    CRM-783

--- i21Database/dbo/Views/vyuCRMCampaignSearch.sql
+++ i21Database/dbo/Views/vyuCRMCampaignSearch.sql
@@ -1,5 +1,8 @@
 ﻿CREATE VIEW [dbo].[vyuCRMCampaignSearch]
 	AS
+	with result as (
+		select z.intApprovalId, z.intTransactionId, z.strStatus from tblSMApproval z
+	)
 	select
 		a.intCampaignId
 		,a.strCampaignName
@@ -26,7 +29,7 @@
 		,c.strLineOfBusiness
 		,d.strType
 		,e.strStatus
-		,strApprovalStatus = h.strStatus
+		,strApprovalStatus = (select i.strStatus from result i where i.intApprovalId = max(h.intApprovalId))
 	from
 		tblCRMCampaign a
 		left join tblEMEntity b on b.intEntityId = a.intEntityId
@@ -36,17 +39,29 @@
 		left join tblSMScreen f on f.strModule = 'CRM' and f.strNamespace = 'CRM.view.Campaign'
 		left join tblSMTransaction g on g.intRecordId = a.intCampaignId and g.intScreenId = f.intScreenId
 		left join tblSMApproval h on h.intTransactionId = g.intTransactionId
-	/*
-	select
-		[tblCRMCampaign].*
-		,tblEMEntity.strName
-		,[tblSMLineOfBusiness].strLineOfBusiness
-		,tblCRMType.strType
-		,[tblCRMStatus].strStatus
-	from
-		[tblCRMCampaign]
-		left outer join tblEMEntity on tblEMEntity.intEntityId = [tblCRMCampaign].intEntityId
-		left outer join [tblSMLineOfBusiness] on [tblSMLineOfBusiness].intLineOfBusinessId = [tblCRMCampaign].intLineOfBusinessId
-		left outer join tblCRMType on tblCRMType.intTypeId = [tblCRMCampaign].[intTypeId]
-		left outer join [tblCRMStatus] on [tblCRMStatus].intStatusId = [tblCRMCampaign].intStatusId
-		*/
+	group by
+		a.intCampaignId
+		,a.strCampaignName
+		,a.strDescription
+		,a.intTypeId
+		,a.intLineOfBusinessId
+		,a.dtmStartDate
+		,a.dtmEndDate
+		,a.dblOpenRate
+		,a.dblBaseCost
+		,a.dblTotalCost
+		,a.dblExpectedRevenue
+		,a.dtmCreateDate
+		,a.intStatusId
+		,a.ysnHold
+		,a.ysnActive
+		,a.intEntityId
+		,a.strRetrospective
+		,a.strImageId
+		,a.intNumberOfAttendee
+		,a.strURL
+		,a.intConcurrencyId
+		,b.strName
+		,c.strLineOfBusiness
+		,d.strType
+		,e.strStatus
