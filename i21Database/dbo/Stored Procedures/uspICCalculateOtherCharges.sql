6ce4826b0c7bb5555ba38623ea928ff626fed961
Feb Montefrio <feb.montefrio@irely.com>
2018-02-28 03:20:01 +0800

    IC-4942

--- i21Database/dbo/Stored Procedures/uspICCalculateOtherCharges.sql
+++ i21Database/dbo/Stored Procedures/uspICCalculateOtherCharges.sql
@@ -6,9 +6,31 @@ BEGIN
 	-- Update the currency fields 
 	UPDATE	ReceiptCharges
 	SET		intCent = Currency.intCent
-			,ysnSubCurrency = Currency.ysnSubCurrency
+			,ysnSubCurrency = 
+				CASE 
+					WHEN Currency.ysnSubCurrency = 1 AND ReceiptCharges.strCostMethod = 'Amount' AND MainCurrency.intCurrencyID IS NOT NULL THEN 
+						0 
+					ELSE 
+						Currency.ysnSubCurrency
+				END 
+			,intCurrencyId = 
+				CASE 
+					WHEN Currency.ysnSubCurrency = 1 AND ReceiptCharges.strCostMethod = 'Amount' AND MainCurrency.intCurrencyID IS NOT NULL THEN 
+						MainCurrency.intCurrencyID
+					ELSE 
+						ReceiptCharges.intCurrencyId
+				END 
+			,dblAmount = 
+				CASE 
+					WHEN Currency.ysnSubCurrency = 1 AND ReceiptCharges.strCostMethod = 'Amount' AND MainCurrency.intCurrencyID IS NOT NULL THEN 
+						ReceiptCharges.dblAmount / CASE WHEN ISNULL(Currency.intCent, 0) = 0  THEN 1 ELSE Currency.intCent END 
+					ELSE 
+						ReceiptCharges.dblAmount
+				END 
 	FROM	dbo.tblICInventoryReceiptCharge ReceiptCharges INNER JOIN dbo.tblSMCurrency Currency
 				ON ReceiptCharges.intCurrencyId = Currency.intCurrencyID
+			LEFT JOIN dbo.tblSMCurrency MainCurrency
+				ON MainCurrency.intCurrencyID = Currency.intMainCurrencyId
 	WHERE	ReceiptCharges.intInventoryReceiptId = @intInventoryReceiptId
 
 	-- Calculate the other charges. 
