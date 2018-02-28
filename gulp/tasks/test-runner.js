f8521a0992a7f129cc9eaf9a776a69a3dc255756
Wendell Wayne Estrada <wendell.estrada@irely.com>
2018-02-28 13:23:16 +0800

    Add styling to Code Coverage

--- gulp/tasks/test-runner.js
+++ gulp/tasks/test-runner.js
@@ -9,6 +9,7 @@
 var gulp = require('gulp');
 var Server = require('karma').Server;
 var config = require('../config');
+var inlinesource = require('gulp-inline-source');
 
 gulp.task('test', function (done) {
     new Server({
@@ -17,6 +18,18 @@ gulp.task('test', function (done) {
         reporters: ['mocha', 'junit', 'coverage']
     }, done).start();
 });
+
+/* VSTS blocked css & scripts for code coverage so we need to generate a new html file with all scripts and css transformed inline. */
+gulp.task('style-coverage', function (done) {
+    return gulp.src('./coverage/html/*.html')
+        .pipe(inlinesource({attribute: false}))
+        .pipe(gulp.dest('./coverage/html/inline-index.html'));
+});
+
+gulp.task('test-coverage', ['test'], function() {
+    gulp.start('style-coverage');
+});
+/* --------------------------------------------- */
 
 gulp.task('test-mocha', function (done) {
     new Server({
