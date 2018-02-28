50786f597635b347cc414c3cb30f5408d745e31b
Smith Wilson de Jesus <smith.dejesus@irely.com>
2018-02-28 16:45:17 +0800

    Revert "Revert "RM-1014""
    This reverts commit ddda7475f2ee407d21923c9e92c3b7b57f91ef0f.

--- i21Database/dbo/Functions/fnRKGetFutureAndBasisPrice.sql
+++ i21Database/dbo/Functions/fnRKGetFutureAndBasisPrice.sql
@@ -1,81 +1,80 @@
-﻿CREATE FUNCTION [dbo].[fnRKGetFutureAndBasisPrice]
-(
-	@intTicketType int = 1 , -- 1- purchase. 2- sale.
-	@intCommodityId int = null,
-	@strSeqMonth nvarchar(10),--'Dec 16'
-	@intSequenceTypeId int, -- 1.	‘01’ – Returns, Basis($) and Unit of Measure 2.	‘02’ – Returns, Futures($) and Unit of Measure   3. 	‘03’ – Returns, Futures($), Basis ($) and Unit of Measure
-	@intFutureMarketId int, 
-	@intLocationId int = null,
-	@dblBasisCost NUMERIC(18, 6)
-)
+﻿CREATE FUNCTION [dbo].[fnRKGetFutureAndBasisPrice] (
+	 @intTicketType INT = 1	,-- 1- purchase. 2- sale.
+	 @intCommodityId INT = NULL
+	,@strSeqMonth NVARCHAR(10)	,--'Dec 2016'
+	 @intSequenceTypeId INT,   -- 1.	‘01’ – Basis 2.	‘02’ – HTA   3. ‘03’ – DP    (PricingType Need to pass)
+	 @intFutureMarketId INT= NULL  -- Contract Futre market Name
+	,@intFutureMonthId INT= NULL -- Contract Future Month Id
+	,@intLocationId INT = NULL
+	,@intMarketZoneId INT = NULL
+	,@dblBasisCost NUMERIC(18, 6)
+	,@intItemId int = null
+	)
 RETURNS NUMERIC(18, 6)
 AS
 BEGIN
-	DECLARE @calculatedValue AS NUMERIC(18, 6); 
-	IF @intSequenceTypeId = 1
+DECLARE @calculatedValueBasis AS NUMERIC(18, 6);
+	DECLARE @calculatedValueFuture AS NUMERIC(18, 6);
+	DECLARE @calculatedValue AS NUMERIC(18, 6);
+	DECLARE @ysnEnterForwardCurveForMarketBasisDifferential BIT
+		
+	SELECT @ysnEnterForwardCurveForMarketBasisDifferential = isnull(ysnEnterForwardCurveForMarketBasisDifferential, 0)
+	FROM tblRKCompanyPreference
+	
+	IF(isnull(@intFutureMonthId,0)=0 AND isnull(@intFutureMarketId,0)=0)
 	BEGIN
-		SELECT TOP 1 @calculatedValue= isnull(dblBasisOrDiscount,0) FROM tblRKM2MBasis b
-		JOIN tblRKM2MBasisDetail bd on b.intM2MBasisId=bd.intM2MBasisId 
-		WHERE intContractTypeId= @intTicketType
-			AND intCommodityId = case when isnull(@intCommodityId,0)=0 then intCommodityId else @intCommodityId end 
-			AND strPeriodTo = @strSeqMonth
-			AND bd.intFutureMarketId=@intFutureMarketId
-			AND isnull(bd.intCompanyLocationId,0) = case when isnull(@intLocationId,0) = 0 then isnull(bd.intCompanyLocationId,0) else @intLocationId end
-			AND ISNULL(dblBasisOrDiscount,0) <> 0 	  
-		ORDER BY dtmM2MBasisDate Desc
+		SELECT TOP 1 @intFutureMarketId=intFutureMarketId FROM tblRKCommodityMarketMapping where intCommodityId=@intCommodityId
+				
+		SELECT TOP 1 @intFutureMonthId=intFutureMonthId
+			FROM tblRKFuturesMonth WHERE ysnExpired = 0 AND  dtmSpotDate <= GETDATE() AND intFutureMarketId = @intFutureMarketId ORDER BY 1 DESC
+			
 	END
-	ELSE IF @intSequenceTypeId = 2
-		BEGIN
-		SELECT TOP 1 @calculatedValue=isnull(dblLastSettle,0) + @dblBasisCost
-		FROM tblRKFuturesSettlementPrice sp
-		INNER JOIN tblRKFutSettlementPriceMarketMap mm ON sp.intFutureSettlementPriceId = mm.intFutureSettlementPriceId
-		INNER JOIN tblRKFutureMarket m on sp.intFutureMarketId=m.intFutureMarketId
-		INNER JOIN tblRKFuturesMonth fm on mm.intFutureMonthId=fm.intFutureMonthId
-		WHERE sp.intFutureMarketId=@intFutureMarketId 
-		AND  RIGHT(CONVERT(NVARCHAR,dtmFutureMonthsDate,106),8)  = @strSeqMonth	AND dblLastSettle IS NOT NULL
-		ORDER BY dtmPriceDate DESC
-		END
-	ELSE
+	IF @intSequenceTypeId in(1,3)
 	BEGIN
+		
+		IF @ysnEnterForwardCurveForMarketBasisDifferential = 0
+		BEGIN
+		
+			SELECT TOP 1 @calculatedValueBasis = ISNULL(dblBasisOrDiscount, 0)
+			FROM tblRKM2MBasis b
+			JOIN tblRKM2MBasisDetail bd ON b.intM2MBasisId = bd.intM2MBasisId
+			WHERE bd.intItemId = @intItemId AND isnull(bd.intCompanyLocationId, 0) = CASE WHEN isnull(@intLocationId, 0) = 0 THEN isnull(bd.intCompanyLocationId, 0) ELSE @intLocationId END AND ISNULL(dblBasisOrDiscount, 0) <> 0 
+				AND strContractInventory = 'Contract'
+				AND isnull(bd.intMarketZoneId, 0) = CASE WHEN isnull(@intMarketZoneId, 0) = 0 THEN isnull(bd.intMarketZoneId, 0) ELSE @intMarketZoneId END
+			ORDER BY dtmM2MBasisDate DESC
 
-	DECLARE @tblRKFutureBasis AS TABLE 
-		(
-			[dblBasis] NUMERIC(18, 6),
-			[strMonth] nvarchar(20)
-		) 
+			SET @calculatedValue=@calculatedValueBasis
 
-	DECLARE @tblRKFuturePrice AS TABLE 
-		(
-			[dblSettlementPrice] NUMERIC(18, 6),
-			[strMonth] nvarchar(20)
-		) 
-	insert into @tblRKFutureBasis([dblBasis],[strMonth])
-	SELECT TOP 1 dblBasisOrDiscount dblBasis,@strSeqMonth strMonth 
-	 FROM tblRKM2MBasis b
-	JOIN tblRKM2MBasisDetail bd on b.intM2MBasisId=bd.intM2MBasisId 
-	JOIN tblICUnitMeasure u on bd.intUnitMeasureId=u.intUnitMeasureId
-	WHERE intContractTypeId= @intTicketType
-		  AND intCommodityId = case when isnull(@intCommodityId,0)=0 then intCommodityId else @intCommodityId end 
-		  AND strPeriodTo = @strSeqMonth
-		  AND bd.intFutureMarketId=@intFutureMarketId
-		  AND isnull(bd.intCompanyLocationId,0) = case when isnull(@intLocationId,0) = 0 then isnull(bd.intCompanyLocationId,0) else @intLocationId end
-		  AND ISNULL(dblBasisOrDiscount,0) <> 0 	  
-	ORDER BY dtmM2MBasisDate Desc
+		END
 
-	INSERT INTO @tblRKFuturePrice([dblSettlementPrice],[strMonth])
-	SELECT TOP 1 isnull(dblLastSettle,0) dblSettlementPrice,@strSeqMonth strMonth
-	FROM tblRKFuturesSettlementPrice sp
-	INNER JOIN tblRKFutSettlementPriceMarketMap mm ON sp.intFutureSettlementPriceId = mm.intFutureSettlementPriceId
-	INNER JOIN tblRKFutureMarket m on sp.intFutureMarketId=m.intFutureMarketId
-	INNER JOIN tblICUnitMeasure u on m.intUnitMeasureId=u.intUnitMeasureId
-	INNER JOIN tblRKFuturesMonth fm on mm.intFutureMonthId=fm.intFutureMonthId
-	WHERE sp.intFutureMarketId=@intFutureMarketId 
-		 AND  RIGHT(CONVERT(NVARCHAR,dtmFutureMonthsDate,106),8)  = @strSeqMonth AND dblLastSettle IS NOT NULL
-	ORDER BY dtmPriceDate DESC
-
-	SELECT @calculatedValue=isnull(dblSettlementPrice,0)+isnull(dblBasis,0) FROM @tblRKFutureBasis t
-	full join @tblRKFuturePrice t1 on t.strMonth=t1.strMonth
-
- END
+		BEGIN
+		
+			SELECT TOP 1 @calculatedValueBasis =ISNULL(dblBasisOrDiscount, 0) 
+			FROM tblRKM2MBasis b
+			JOIN tblRKM2MBasisDetail bd ON b.intM2MBasisId = bd.intM2MBasisId
+			WHERE intContractTypeId = @intTicketType AND intCommodityId = CASE WHEN isnull(@intCommodityId, 0) = 0 THEN intCommodityId ELSE @intCommodityId END AND strPeriodTo = @strSeqMonth AND bd.intFutureMarketId = @intFutureMarketId AND isnull(bd.intCompanyLocationId, 0) = CASE WHEN isnull(@intLocationId, 0) = 0 THEN isnull(bd.intCompanyLocationId, 0) ELSE @intLocationId END AND ISNULL(dblBasisOrDiscount, 0) <> 0 AND strContractInventory = 'Contract'
+			 AND isnull(bd.intMarketZoneId, 0) = CASE WHEN isnull(@intMarketZoneId, 0) = 0 THEN isnull(bd.intMarketZoneId, 0) ELSE @intMarketZoneId END
+			 AND strContractInventory = 'Contract'
+			ORDER BY dtmM2MBasisDate DESC
+			set @calculatedValue=@calculatedValueBasis
+		END
+	END
+	IF @intSequenceTypeId in(2,3)
+	BEGIN			
+	
+			SELECT TOP 1 @calculatedValueFuture = isnull(dblLastSettle, 0) + isnull(@dblBasisCost,0)
+			FROM tblRKFuturesSettlementPrice sp
+			INNER JOIN tblRKFutSettlementPriceMarketMap mm ON sp.intFutureSettlementPriceId = mm.intFutureSettlementPriceId
+			INNER JOIN tblRKFutureMarket m ON sp.intFutureMarketId = m.intFutureMarketId
+			INNER JOIN tblRKFuturesMonth fm ON mm.intFutureMonthId = fm.intFutureMonthId
+			WHERE sp.intFutureMarketId = @intFutureMarketId AND fm.intFutureMonthId=@intFutureMonthId  AND dblLastSettle IS NOT NULL
+			ORDER BY dtmPriceDate DESC
+			SET @calculatedValue=@calculatedValueFuture		
+	END
+	IF @intSequenceTypeId = 3
+	BEGIN
+		SELECT @calculatedValue= isnull(@calculatedValueBasis,0)+isnull(@calculatedValueFuture,0)
+	END
+	
 	RETURN @calculatedValue
 END
