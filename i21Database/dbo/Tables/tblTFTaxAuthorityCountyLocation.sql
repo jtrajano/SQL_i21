51f58cc491783202d7c8e275c12ba7136a28fda9
Lawrence Zabala <lawrence.zabala@irely.com>
2018-02-28 19:38:41 +0800

    MFT-961

---
+++ i21Database/dbo/Tables/tblTFTaxAuthorityCountyLocation.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tblTFTaxAuthorityCountyLocation]
+(
+	[intTaxAuthorityCountyLocationId] INT IDENTITY NOT NULL,
+	[intTaxAuthorityId] INT NOT NULL, 
+    [intCountyLocationId] INT NOT NULL, 
+    [intConcurrencyId] INT NULL DEFAULT ((1)), 
+    CONSTRAINT [PK_tblTFTaxAuthorityCountyLocation] PRIMARY KEY ([intTaxAuthorityCountyLocationId]), 
+    CONSTRAINT [AK_tblTFTaxAuthorityCountyLocation] UNIQUE ([intTaxAuthorityId], [intCountyLocationId]), 
+    CONSTRAINT [FK_tblTFTaxAuthorityCountyLocation_tblTFTaxAuthority] FOREIGN KEY ([intTaxAuthorityId]) REFERENCES [tblTFTaxAuthority]([intTaxAuthorityId]), 
+    CONSTRAINT [FK_tblTFTaxAuthorityCountyLocation_tblTFCountyLocation] FOREIGN KEY ([intCountyLocationId]) REFERENCES [tblTFCountyLocation]([intCountyLocationId])
+)
---
+++ i21Database/dbo/Views/vyuTFGetTaxAuthorityCountyLocation.sql
@@ -0,0 +1,12 @@
+﻿CREATE VIEW [dbo].[vyuTFGetTaxAuthorityCountyLocation]
+	AS
+	
+SELECT TACL.intTaxAuthorityCountyLocationId
+	, TA.intTaxAuthorityId
+	, TA.strTaxAuthorityCode
+	, CL.intCountyLocationId
+	, CL.strCounty
+	, CL.strLocation
+FROM tblTFTaxAuthorityCountyLocation TACL
+LEFT JOIN tblTFTaxAuthority TA ON TA.intTaxAuthorityId = TACL.intTaxAuthorityId
+LEFT JOIN tblTFCountyLocation CL ON CL.intCountyLocationId = TACL.intCountyLocationId
