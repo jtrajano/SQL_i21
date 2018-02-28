3dd8f5ce929c397dd790a4203fff6e056063abc3
Feb Montefrio <feb.montefrio@irely.com>
2018-02-28 23:36:22 +0800

    MFT-961

---
+++ i21Database/dbo/Tables/tblTFNMCountyLocation.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[tblTFNMCountyLocation]
+(
+    [intCompanyLocationId] INT  IDENTITY (1, 1) NOT NULL,
+    [strCounty] NVARCHAR(100) COLLATE Latin1_General_CI_AS NOT NULL,
+    [strLocation] NVARCHAR(100) COLLATE Latin1_General_CI_AS NOT NULL, 
+    CONSTRAINT [PK_tblTFNMCountyLocation] PRIMARY KEY ([intCompanyLocationId])
+)
