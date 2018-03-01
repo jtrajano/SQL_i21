5e0cdf382183784cff30031399674177b82aaf47
Jayson Ramos <jayson.ramos@iRely.com>
2018-03-01 16:48:59 +0800

    CRM-785

--- i21Database/dbo/Views/vyuCRMCampaignContactSource.sql
+++ i21Database/dbo/Views/vyuCRMCampaignContactSource.sql
@@ -1,20 +1,23 @@
 ﻿CREATE VIEW [dbo].[vyuCRMCampaignContactSource]
 	AS
+		with opportunity as(
+			select z.intOpportunityId, z.intCustomerContactId, z.strName from tblCRMOpportunity z
+		)
 		select
-			intId = convert(int, ROW_NUMBER() over (order by strContactName))
-			,strContactName
-			,strCompanyName
-			,strEmail
-			,intContactId
-			,strEntityType = strEntityType
+			intId = convert(int, ROW_NUMBER() over (order by result.strContactName))
+			,result.strContactName
+			,result.strCompanyName
+			,result.strEmail
+			,result.intContactId
+			,strEntityType = result.strEntityType
 			,d.intActivityId
 			,strActivityType = d.strType
 			,d.strActivityNo
 			,result.intEntityId
-				,e.dblQuote
-				,e.dblSalesOrder
-				,e.intOpportunityId
-				,e.strOpportunityName
+			,e.dblQuote
+			,e.dblSalesOrder
+			,intOpportunityId = max(f.intOpportunityId)
+			,strOpportunityName = (select strName from opportunity where intOpportunityId = max(f.intOpportunityId))
 		from
 			(
 				select distinct
@@ -30,29 +33,17 @@
 			) as result
 			left join tblSMActivity d on d.intActivityId = (select max(d.intActivityId) from tblSMActivity d where d.intEntityContactId = result.intContactId)
 			left join vyuCRMCampaignEntitySalesOrder e on e.intEntityContactId = result.intContactId
-
-		/*
-		select
-			intId = convert(int, ROW_NUMBER() over (order by strContactName))
-			,strContactName
-			,strCompanyName
-			,strEmail
-			,intContactId
-			,strEntityType = strEntityType
+			left join opportunity f on f.intCustomerContactId = result.intContactId
+		group by
+			result.strContactName
+			,result.strContactName
+			,result.strCompanyName
+			,result.strEmail
+			,result.intContactId
+			,result.strEntityType
 			,d.intActivityId
-			,strActivityType = d.strType
+			,d.strType
 			,d.strActivityNo
-		from
-			(
-				select distinct
-					strContactName = vyuEMEntityContact.strName
-					,strCompanyName = vyuEMEntityContact.strEntityName
-					,vyuEMEntityContact.strEmail
-					,intContactId = vyuEMEntityContact.intEntityContactId
-					,strEntityType = 
-						dbo.fnCRMCoalesceEntityType((select top 1 a.intEntityId from tblEMEntityToContact a where a.intEntityContactId = vyuEMEntityContact.intEntityContactId))
-				from
-					vyuEMEntityContact
-			) as result
-			left join tblSMActivity d on d.intActivityId = (select max(d.intActivityId) from tblSMActivity d where d.intEntityContactId = result.intContactId)
-		*/
+			,result.intEntityId
+			,e.dblQuote
+			,e.dblSalesOrder
--- i21Database/dbo/Views/vyuCRMCampaignEntityLink.sql
+++ i21Database/dbo/Views/vyuCRMCampaignEntityLink.sql
@@ -1,6 +1,23 @@
 ﻿CREATE VIEW [dbo].[vyuCRMCampaignEntityLink]
 	AS
-		select * from (
+		with opportunity as (
+			select z.intOpportunityId, z.intCustomerContactId, z.strName from tblCRMOpportunity z
+		)
+		select
+			link.intEntityId
+			,link.strContactName
+			,link.strEntityName
+			,link.strEntityType
+			,link.strEmail
+			,link.intActivityId
+			,link.strActivityType
+			,link.strActivityNo
+			,link.intEntityEntityId
+			,link.dblQuote
+			,link.dblSalesOrder
+			,intOpportunityId = max(f.intOpportunityId)
+			,strOpportunityName = (select top 1 g.strName from opportunity g where g.intOpportunityId = max(f.intOpportunityId))
+		from (
 			select
 				intEntityId = a.intEntityContactId
 				,strContactName = b.strName
@@ -13,29 +30,23 @@
 				,intEntityEntityId = c.intEntityId
 				,e.dblQuote
 				,e.dblSalesOrder
-				,e.intOpportunityId
-				,e.strOpportunityName
 			from tblEMEntityToContact a
 			join tblEMEntity b on b.intEntityId = a.intEntityContactId
 			join tblEMEntity c on c.intEntityId = a.intEntityId
 			left join tblSMActivity d on d.intActivityId = (select max(d.intActivityId) from tblSMActivity d where d.intEntityContactId = a.intEntityContactId)
 			left join vyuCRMCampaignEntitySalesOrder e on e.intEntityContactId = a.intEntityContactId
-		) as link where strEntityType is not null
-		/*
-		select * from (
-			select
-				intEntityId = a.intEntityContactId
-				,strContactName = b.strName
-				,strEntityName = c.strName
-				,strEntityType = (select top 1 d.strType from tblEMEntityType d where d.intEntityId = a.intEntityId)
-				,strEmail = b.strEmail
-				,d.intActivityId
-				,strActivityType = d.strType
-				,d.strActivityNo
-				,intEntityEntityId = c.intEntityId
-			from tblEMEntityToContact a
-			join tblEMEntity b on b.intEntityId = a.intEntityContactId
-			join tblEMEntity c on c.intEntityId = a.intEntityId
-			left join tblSMActivity d on d.intActivityId = (select max(d.intActivityId) from tblSMActivity d where d.intEntityContactId = a.intEntityContactId)
-		) as link where strEntityType is not null
-		*/
+		) as link 
+		left join opportunity f on f.intCustomerContactId = link.intEntityId
+		where link.strEntityType is not null
+		group by
+			link.intEntityId
+			,link.strContactName
+			,link.strEntityName
+			,link.strEntityType
+			,link.strEmail
+			,link.intActivityId
+			,link.strActivityType
+			,link.strActivityNo
+			,link.intEntityEntityId
+			,link.dblQuote
+			,link.dblSalesOrder
