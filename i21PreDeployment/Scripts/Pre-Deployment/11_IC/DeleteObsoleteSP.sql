19023f6dc0dca98f007617e3acd3f116d9663ef6
Feb Montefrio <feb.montefrio@irely.com>
2018-03-06 05:07:15 +0800

    IC-2906 Remove obsolete sp. Do cost adjustment instead of rebuilding stocks from receipts with zero cost.

--- i21PreDeployment/Scripts/Pre-Deployment/11_IC/DeleteObsoleteSP.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/11_IC/DeleteObsoleteSP.sql
@@ -37,3 +37,6 @@ GO
 IF EXISTS(select top 1 1 from sys.procedures where name = 'uspICUnpostInventoryShipmentOtherCharges')
 	EXEC('DROP PROCEDURE uspICUnpostInventoryShipmentOtherCharges')
 GO
+IF EXISTS(select top 1 1 from sys.procedures where name = 'uspICRebuildZeroCostReceipts')
+	EXEC('DROP PROCEDURE uspICRebuildZeroCostReceipts')
+GO
