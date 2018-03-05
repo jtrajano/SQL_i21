a40c55944d83b6b72b4536b1cfa7f9e400eab7a8
Kelvin Quinto <kelvin.quinto@irely.com>
2018-03-05 17:22:05 +0800

    AR-6874

--- i21PreDeployment/Scripts/Pre-Deployment/7_AR/6_PaymentDetailInvoiceCleanUp.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/7_AR/6_PaymentDetailInvoiceCleanUp.sql
@@ -10,26 +10,27 @@ IF	(
 	EXISTS(SELECT NULL FROM sys.tables WHERE name = N'tblAPBill') AND EXISTS(SELECT NULL FROM sys.columns WHERE [name] = N'intBillId' AND [object_id] = OBJECT_ID(N'tblAPBill'))
 	)
 BEGIN
+	DECLARE @strQuery NVARCHAR(MAX) = CAST('' AS NVARCHAR(MAX)) + '
+		UPDATE tblARPaymentDetail
+		SET
+			[intInvoiceId] = NULL
+		WHERE
+			[intInvoiceId] IS NOT NULL
+			AND [intBillId] IS NOT NULL
+			AND NOT EXISTS (SELECT NULL FROM tblARInvoice WHERE tblARInvoice.[intInvoiceId] = tblARPaymentDetail.[intInvoiceId])
+	
+		UPDATE tblARPaymentDetail
+		SET
+			[intBillId] = NULL
+		WHERE
+			[intBillId] IS NOT NULL
+			AND [intInvoiceId] IS NOT NULL
+			AND NOT EXISTS (SELECT NULL FROM tblAPBill WHERE tblAPBill.[intBillId] = tblARPaymentDetail.[intBillId])
+	
+		DELETE FROM tblARPaymentDetail
+		WHERE
+			NOT EXISTS (SELECT NULL FROM tblARInvoice WHERE tblARInvoice.[intInvoiceId] = tblARPaymentDetail.[intInvoiceId])
+			AND NOT EXISTS (SELECT NULL FROM tblAPBill WHERE tblAPBill.[intBillId] = tblARPaymentDetail.[intBillId])'
 
-UPDATE tblARPaymentDetail
-SET
-	[intInvoiceId] = NULL
-WHERE
-	[intInvoiceId] IS NOT NULL
-	AND [intBillId] IS NOT NULL
-	AND NOT EXISTS (SELECT NULL FROM tblARInvoice WHERE tblARInvoice.[intInvoiceId] = tblARPaymentDetail.[intInvoiceId])
-	
-UPDATE tblARPaymentDetail
-SET
-	[intBillId] = NULL
-WHERE
-	[intBillId] IS NOT NULL
-	AND [intInvoiceId] IS NOT NULL
-	AND NOT EXISTS (SELECT NULL FROM tblAPBill WHERE tblAPBill.[intBillId] = tblARPaymentDetail.[intBillId])
-	
-DELETE FROM tblARPaymentDetail
-WHERE
-	NOT EXISTS (SELECT NULL FROM tblARInvoice WHERE tblARInvoice.[intInvoiceId] = tblARPaymentDetail.[intInvoiceId])
-	AND NOT EXISTS (SELECT NULL FROM tblAPBill WHERE tblAPBill.[intBillId] = tblARPaymentDetail.[intBillId])
-	
+	EXEC sp_executesql @strQuery	
 END
