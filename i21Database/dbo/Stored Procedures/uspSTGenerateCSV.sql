0fdd61472399c545387c71c04249bf209724f8bd
Henry Zaragoza Jr <henry.zaragoza@irely.com>
2018-03-08 15:14:57 +0800

    ST-614 Rebate - Reconfigure csv generated file for RJ Reynolds

--- i21Database/dbo/Stored Procedures/uspSTGenerateCSV.sql
+++ i21Database/dbo/Stored Procedures/uspSTGenerateCSV.sql
@@ -133,7 +133,8 @@ BEGIN
 				
 				--Optional Fields
 				intStoreTelephone int NULL,
-				strStoreContactName nvarchar(100) COLLATE Latin1_General_CI_AS NULL,				strStoreContactEmail nvarchar(100) COLLATE Latin1_General_CI_AS NULL,
+				strStoreContactName nvarchar(100) COLLATE Latin1_General_CI_AS NULL,
+				strStoreContactEmail nvarchar(100) COLLATE Latin1_General_CI_AS NULL,
 				strProductGroupingCode nvarchar(10) COLLATE Latin1_General_CI_AS NULL,
 				strProductGroupingName nvarchar(20) COLLATE Latin1_General_CI_AS NULL,
 				strLoyaltyIDRewardsNumber nvarchar(20) COLLATE Latin1_General_CI_AS NULL
@@ -237,12 +238,21 @@ BEGIN
 								, replace(convert(NVARCHAR, dtmDate, 111), '/', '') as dtmTransactionDate 
 								, convert(NVARCHAR, dtmDate, 108) as strTransactionTime
 								, intTermMsgSN as strTransactionIdCode
+
 								, ST.intStoreNo as strStoreNumber
 								, ST.strDescription as strStoreName
-								, REPLACE(REPLACE(REPLACE(REPLACE(ST.strAddress, CHAR(10), ''), CHAR(13), ''), @Delimiter, ''), ',', '') as strStoreAddress
-								, ST.strCity as strStoreCity
-								, UPPER(LEFT(ST.strState, 2)) as strStoreState
-								, ST.strZipCode as intStoreZipCode
+								--, REPLACE(REPLACE(REPLACE(REPLACE(ST.strAddress, CHAR(10), ''), CHAR(13), ''), @Delimiter, ''), ',', '') as strStoreAddress
+								--, ST.strCity as strStoreCity
+								--, UPPER(LEFT(ST.strState, 2)) as strStoreState
+								--, ST.strZipCode as intStoreZipCode
+
+								--, STMAP.intStoreNo as strStoreNumber
+								--, STMAP.strDescription as strStoreName
+								, REPLACE(REPLACE(REPLACE(REPLACE(STMAP.strAddress, CHAR(10), ''), CHAR(13), ''), @Delimiter, ''), ',', '') as strStoreAddress
+								, STMAP.strCity as strStoreCity
+								, UPPER(LEFT(STMAP.strState, 2)) as strStoreState
+								, STMAP.strZipCode as intStoreZipCode
+
 								, strTrlDept as strCategory
 								, EM.strName as strManufacturerName
 								, strTrlUPC as strSKUCode --14
@@ -251,9 +261,14 @@ BEGIN
 								, 'PACK' as strUnitOfMeasure
 								, CAST(CASE WHEN strTrpPaycode = 'CASH' THEN dblTrlQty ELSE 0 END as INT) as intQuantitySold
 								, 1 as intConsumerUnits
-								, CASE WHEN strTrpPaycode = 'CASH' AND dblTrlQty >= 2 THEN 'Y' ELSE 'N' END as strMultiPackIndicator
-								, CASE WHEN strTrpPaycode = 'CASH' THEN dblTrlQty ELSE 0 END as intMultiPackRequiredQuantity
-								, CASE WHEN strTrpPaycode = 'CASH' AND dblTrlQty >= 2 THEN 0.50 ELSE 0 END as dblMultiPackDiscountAmount
+
+								--, CASE WHEN strTrpPaycode = 'CASH' AND dblTrlQty >= 2 THEN 'Y' ELSE 'N' END as strMultiPackIndicator
+								--, CASE WHEN strTrpPaycode = 'CASH' THEN dblTrlQty ELSE 0 END as intMultiPackRequiredQuantity
+								--, CASE WHEN strTrpPaycode = 'CASH' AND dblTrlQty >= 2 THEN 0.50 ELSE 0 END as dblMultiPackDiscountAmount
+								, 'N' as strMultiPackIndicator
+								, 0 as intMultiPackRequiredQuantity
+								, 0 as dblMultiPackDiscountAmount
+
 								, REPLACE(CRP.strProgramName, ',','') as strRetailerFundedDiscountName
 								, CRP.dblManufacturerBuyDownAmount as dblRetailerFundedDiscountAmount
 								, CASE WHEN strTrpPaycode = 'COUPONS' THEN 'Coupon' ELSE '' END as strMFGDealNameONE
@@ -262,11 +277,14 @@ BEGIN
 								, 0 as dblMFGDealDiscountAmountTWO
 								, '' as strMFGDealNameTHREE
 								, 0 as dblMFGDealDiscountAmountTHREE
-								, CASE WHEN strTrpPaycode = 'CASH' THEN dblTrlLineTot ELSE 0 END as dblFinalSalesPrice
+								, CASE WHEN CRP.strProgramName IS NOT NULL THEN 0
+										WHEN strTrpPaycode = 'CASH' THEN dblTrlLineTot 
+										ELSE 0 END as dblFinalSalesPrice
 								
 								--Optional Fields
 								, NULL AS intStoreTelephone
-								, '' AS strStoreContactName								, '' strStoreContactEmail
+								, '' AS strStoreContactName
+								, '' strStoreContactEmail
 								, '' strProductGroupingCode
 								, '' strProductGroupingName
 								, '' strLoyaltyIDRewardsNumber
@@ -283,8 +301,11 @@ BEGIN
 				JOIN tblSTStore ST ON ST.intStoreId = TR.intStoreId
 				JOIN tblSTRetailAccount STRT ON STRT.intStoreId = ST.intStoreId AND STRT.intEntityId = @intVendorId
 				JOIN tblEMEntity EM ON EM.intEntityId = @intVendorId
+				JOIN tblAPVendor APV ON APV.intEntityId = EM.intEntityId
+				JOIN tblSTStore STMAP ON STMAP.intStoreId = APV.intStoreStoreId
 				LEFT JOIN vyuSTCigaretteRebatePrograms CRP ON TR.strTrlUPC = CRP.strLongUPCCode 
 						AND (CAST(TR.dtmDate AS DATE) BETWEEN CRP.dtmStartDate AND CRP.dtmEndDate)
+						AND TR.strTrpPaycode IN ('Change', 'CREDIT')
 				LEFT JOIN
 				(
 					SELECT [intID] 
@@ -336,6 +357,7 @@ BEGIN
 
 								, CASE WHEN strTrpPaycode IN ('COUPONS') THEN 'COUPONS' ELSE '' END as strAccountPromotionName --21
 								--, CASE WHEN strTrlDesc like '% OFF%' THEN strTrlDesc ELSE '' END as strAccountPromotionName --21
+
 								, CASE WHEN strTrpPaycode IN ('COUPONS') THEN dblTrpAmt ELSE 0 END as dblAccountDiscountAmount --22
 
 								, CASE WHEN strTrpPaycode IN ('COUPONS') THEN dblTrpAmt ELSE 0 END as dblManufacturerDiscountAmount
