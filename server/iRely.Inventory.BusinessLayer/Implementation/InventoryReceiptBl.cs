bc8541488e4c1ae0e9b0d898640ecac74dc33310
Jb Mago <benedict.mago@irely.com>
2018-03-12 11:40:32 +0800

    Fixed build error

--- server/iRely.Inventory.BusinessLayer/Implementation/InventoryReceiptBl.cs
+++ server/iRely.Inventory.BusinessLayer/Implementation/InventoryReceiptBl.cs
@@ -189,10 +189,6 @@ namespace iRely.Inventory.BusinessLayer
                     s.intParentItemLinkId,
                     s.intChildItemLinkId,
 
-                    s.strItemType,
-                    s.intParentItemLinkId,
-                    s.intChildItemLinkId,
-
                     // PROJECTED
                     strItemNo = s.tblICItem.strItemNo,
                     strItemDescription = s.tblICItem.strDescription,
