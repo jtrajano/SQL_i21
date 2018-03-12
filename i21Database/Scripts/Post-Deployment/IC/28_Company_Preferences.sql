3dde157cc464394cc160b25f8296445198b55fb7
Wendell Wayne Estrada <wendell.estrada@irely.com>
2018-03-13 02:06:26 +0800

    IC-5026 Inventory Import - Remove Additional GL accounts option

---
+++ i21Database/Scripts/Post-Deployment/IC/28_Company_Preferences.sql
@@ -0,0 +1,5 @@
+﻿-- AdditionalGLAccts was removed, so update the current origin task
+SELECT * FROM tblICCompanyPreference
+UPDATE tblICCompanyPreference
+SET strOriginLastTask = 'Items'
+WHERE strOriginLastTask = 'AdditionalGLAccts'
