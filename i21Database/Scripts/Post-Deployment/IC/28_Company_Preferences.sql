af928263b485f259fc49acd000c306e35b525954
Wendell Wayne Estrada <wendell.estrada@irely.com>
2018-03-14 02:41:47 +0800

    [Chore] Code cleanup

--- i21Database/Scripts/Post-Deployment/IC/28_Company_Preferences.sql
+++ i21Database/Scripts/Post-Deployment/IC/28_Company_Preferences.sql
@@ -1,5 +1,4 @@
 ﻿-- AdditionalGLAccts was removed, so update the current origin task
-SELECT * FROM tblICCompanyPreference
 UPDATE tblICCompanyPreference
 SET strOriginLastTask = 'Items'
 WHERE strOriginLastTask = 'AdditionalGLAccts'
