8d240daa72eea6e714b0eb8ed0190b68957ecdb5
Robin Panganiban <robin.panganiban@irely.com>
2018-03-13 09:14:45 +0800

    AR-6905

--- i21Database/dbo/Functions/fnARGetMonthlyAccrual.sql
+++ i21Database/dbo/Functions/fnARGetMonthlyAccrual.sql
@@ -11,31 +11,41 @@ RETURNS @tblMonthlyAccrual TABLE
 )
 AS
 BEGIN
-	DECLARE @intPeriodsAccrue			INT
-		  , @intCounter					INT	= 1
-		  , @dtmPostDate				DATETIME
-		  , @dblInvoiceTotal			NUMERIC(18, 6)	= 0
-		  , @dblRunningAccrualBalance	NUMERIC(18, 6)	= 0		  
+	INSERT @tblMonthlyAccrual
+	SELECT intInvoiceId ,
+		   dtmAccrualDate,
+		   CASE WHEN [ROW] = 1 THEN dblInvoiceSubtotal + dblTax ELSE dblInvoiceSubtotal END - SUM(dblAmount) OVER (ORDER BY intInvoiceAccrualId)  
+		   FROM (
+	SELECT ROW_NUMBER() OVER (ORDER BY I.intInvoiceId) [ROW],I.intInvoiceId, intInvoiceAccrualId, I.dblTax ,I.dblInvoiceSubtotal,AI.dblAmount,CONVERT(CHAR(4), dtmAccrualDate, 100) + CONVERT(CHAR(4), dtmAccrualDate, 120) dtmAccrualDate
+	FROM tblARInvoiceAccrual AI
+	INNER JOIN tblARInvoice I
+		ON AI.intInvoiceId = I.intInvoiceId
+	WHERE AI.intInvoiceId = @intInvoiceId) Accrual
+	--DECLARE @intPeriodsAccrue			INT
+	--	  , @intCounter					INT	= 1
+	--	  , @dtmPostDate				DATETIME
+	--	  , @dblInvoiceTotal			NUMERIC(18, 6)	= 0
+	--	  , @dblRunningAccrualBalance	NUMERIC(18, 6)	= 0		  
 
-	SELECT TOP 1 @intPeriodsAccrue			= intPeriodsToAccrue
-			   , @dtmPostDate				= dtmPostDate
-			   , @dblInvoiceTotal			= dblInvoiceTotal
-			   , @dblRunningAccrualBalance	= dblInvoiceTotal
-	FROM dbo.tblARInvoice WITH (NOLOCK)
-	WHERE intInvoiceId = @intInvoiceId
+	--SELECT TOP 1 @intPeriodsAccrue			= intPeriodsToAccrue
+	--		   , @dtmPostDate				= dtmPostDate
+	--		   , @dblInvoiceTotal			= dblInvoiceTotal
+	--		   , @dblRunningAccrualBalance	= dblInvoiceTotal
+	--FROM dbo.tblARInvoice WITH (NOLOCK)
+	--WHERE intInvoiceId = @intInvoiceId
 
-	WHILE (@intCounter <= @intPeriodsAccrue + 1)
-		BEGIN
-			IF (@dtmPostDate >= @dtmAsOfDate)
-				BEGIN
-					INSERT @tblMonthlyAccrual
-					SELECT @intInvoiceId, CONVERT(CHAR(4), @dtmPostDate, 100) + CONVERT(CHAR(4), @dtmPostDate, 120), @dblRunningAccrualBalance
-				END
+	--WHILE (@intCounter <= @intPeriodsAccrue + 1)
+	--	BEGIN
+	--		IF (@dtmPostDate >= @dtmAsOfDate)
+	--			BEGIN
+	--				INSERT @tblMonthlyAccrual
+	--				SELECT @intInvoiceId, CONVERT(CHAR(4), @dtmPostDate, 100) + CONVERT(CHAR(4), @dtmPostDate, 120), @dblRunningAccrualBalance
+	--			END
 
-			SET @intCounter = @intCounter + 1
-			SET @dtmPostDate = DATEADD(MONTH, 1, @dtmPostDate)
-			SET @dblRunningAccrualBalance = dbo.fnRoundBanker(@dblRunningAccrualBalance - (@dblInvoiceTotal / @intPeriodsAccrue), 2)
-		END
+	--		SET @intCounter = @intCounter + 1
+	--		SET @dtmPostDate = DATEADD(MONTH, 1, @dtmPostDate)
+	--		SET @dblRunningAccrualBalance = dbo.fnRoundBanker(@dblRunningAccrualBalance - (@dblInvoiceTotal / @intPeriodsAccrue), 2)
+	--	END
 
 	RETURN
 END
