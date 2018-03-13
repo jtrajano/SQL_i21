6bc7d9509eaf0521cf28c3c9d9a6afd861f09d6c
Crisman Lazaga <cris.lazaga@irely.com>
2018-03-13 16:09:58 +0800

    MFT-1001

--- i21Database/dbo/Tables/tblTFTaxAuthorityCountyLocation.sql
+++ i21Database/dbo/Tables/tblTFTaxAuthorityCountyLocation.sql
@@ -7,7 +7,7 @@
     [intCountyLocationId] INT NOT NULL, 
     [intConcurrencyId] INT NULL DEFAULT ((1)), 
     CONSTRAINT [PK_tblTFTaxAuthorityCountyLocation] PRIMARY KEY ([intTaxAuthorityCountyLocationId]), 
-    CONSTRAINT [AK_tblTFTaxAuthorityCountyLocation] UNIQUE ([intTaxAuthorityId], [intCountyLocationId]), 
+    CONSTRAINT [AK_tblTFTaxAuthorityCountyLocation] UNIQUE ([intTaxAuthorityId], [intEntityId], [intEntityLocationId], [intCountyLocationId]), 
 	CONSTRAINT [FK_tblTFTaxAuthorityCountyLocation_tblTFTaxAuthority] FOREIGN KEY ([intTaxAuthorityId]) REFERENCES [tblTFTaxAuthority]([intTaxAuthorityId]) ON DELETE CASCADE, 
 	CONSTRAINT [FK_tblTFTaxAuthorityCountyLocation_tblARCustomer] FOREIGN KEY ([intEntityId]) REFERENCES [tblARCustomer]([intEntityId]), 
 	CONSTRAINT [FK_tblTFTaxAuthorityCountyLocation_tblEMEntityLocation] FOREIGN KEY ([intEntityLocationId]) REFERENCES [tblEMEntityLocation]([intEntityLocationId]), 
