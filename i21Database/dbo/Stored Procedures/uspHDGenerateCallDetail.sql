5ce581df322f6d23bf60dfcbc53c758a5486e82f
Jayson Ramos <jayson.ramos@iRely.com>
2018-03-16 10:50:53 +0800

    HD-2461

---
+++ i21Database/dbo/Stored Procedures/uspHDGenerateCallDetail.sql
@@ -0,0 +1,109 @@
+﻿CREATE PROCEDURE [dbo].[uspHDGenerateCallDetail]
+	@DateFrom int
+	,@DateTo int
+	,@strIdentifier nvarchar(36)
+AS
+BEGIN
+
+SET QUOTED_IDENTIFIER OFF;
+SET ANSI_NULLS ON;
+SET NOCOUNT ON;
+SET XACT_ABORT ON;
+SET ANSI_WARNINGS OFF;
+
+delete from tblHDCallDetail where intCreatedDate < convert(int, convert(nvarchar(8), DATEADD(day,-1,getdate()), 112)) or strFilterKey = @strIdentifier;
+
+with closedCalls as
+(
+	select
+		b.intEntityId
+		,b.strName
+		,intClosedCalls = count(a.intTicketId)
+	from
+		tblHDTicket a
+		,tblEMEntity b
+	where
+		a.intAssignedToEntity is not null
+		and b.intEntityId = a.intAssignedToEntity
+		and convert(int, convert(nvarchar(8), a.dtmCreated, 112)) between @DateFrom and @DateTo
+		and a.intTicketStatusId = (select top 1 intTicketStatusId from tblHDTicketStatus where strStatus = 'Closed')
+	group by
+		b.intEntityId
+		,b.strName
+),
+openCalls as
+(
+	select
+		b.intEntityId
+		,b.strName
+		,intOpenCalls = count(a.intTicketId)
+	from
+		tblHDTicket a
+		,tblEMEntity b
+	where
+		a.intAssignedToEntity is not null
+		and b.intEntityId = a.intAssignedToEntity
+		and convert(int, convert(nvarchar(8), a.dtmCreated, 112)) between @DateFrom and @DateTo
+		and a.intTicketStatusId <> (select top 1 intTicketStatusId from tblHDTicketStatus where strStatus = 'Closed')
+	group by
+		b.intEntityId
+		,b.strName
+),
+totalCalls as
+(
+	select
+		b.intEntityId
+		,b.strName
+		,intTotalCalls = count(a.intTicketId)
+	from
+		tblHDTicket a
+		,tblEMEntity b
+	where
+		a.intAssignedToEntity is not null
+		and b.intEntityId = a.intAssignedToEntity
+		and convert(int, convert(nvarchar(8), a.dtmCreated, 112)) between @DateFrom and @DateTo
+	group by
+		b.intEntityId
+		,b.strName
+)
+
+INSERT INTO tblHDCallDetail
+           (
+		   intEntityId
+           ,strName
+           ,strFirstName
+           ,intClosedCalls
+           ,intOpenCalls
+           ,intTotalCalls
+           ,intStartDate
+           ,intEndDate
+           ,strFilterKey
+           ,intRequestedByEntityId
+           ,intCreatedDate
+           ,intConcurrencyId
+		   )
+
+select distinct
+		intEntityId = b.intEntityId
+		,strName = ltrim(rtrim(b.strName))
+		,strFirstName = SUBSTRING(ltrim(rtrim(b.strName)),1,CHARINDEX(' ',ltrim(rtrim(b.strName)),1))
+		,intClosedCalls = isnull((select intClosedCalls from closedCalls where intEntityId = b.intEntityId),0)
+		,intOpenCalls = isnull((select intOpenCalls from openCalls where intEntityId = b.intEntityId),0)
+		,intTotalCalls = isnull((select intTotalCalls from totalCalls where intEntityId = b.intEntityId),0)
+		,intStartDate = @DateFrom
+		,intEndDate = @DateTo
+		,strFilterKey = @strIdentifier
+		,intRequestedByEntityId = 0
+		,intCreatedDate = convert(int, convert(nvarchar(8), getdate(), 112))
+		,intConcurrencyId = 1
+from
+		tblHDTicket a
+		,tblEMEntity b
+where
+		a.intAssignedToEntity is not null
+		and b.intEntityId = a.intAssignedToEntity
+		and convert(int, convert(nvarchar(8), a.dtmCreated, 112)) between @DateFrom and @DateTo
+order by
+		strName
+
+END
---
+++ i21Database/dbo/Tables/tblHDCallDetail.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[tblHDCallDetail]
+(
+	[intCallDetailId] [int] IDENTITY(1,1) NOT NULL,
+	[intEntityId] [int] not null,
+	[strName] [nvarchar](100) COLLATE Latin1_General_CI_AS NULL,
+	[strFirstName] [nvarchar](100) COLLATE Latin1_General_CI_AS NULL,
+	[intClosedCalls] [int] not null default 0,
+	[intOpenCalls] [int] not null default 0,
+	[intTotalCalls] [int] not null default 0,
+	[intStartDate] [int] not NULL,
+	[intEndDate] [int] not NULL,
+	[strFilterKey] [nvarchar](36) COLLATE Latin1_General_CI_AS NULL,
+	[intRequestedByEntityId] [int] null,
+	[intCreatedDate] [int] not null,
+	[intConcurrencyId] [int] not null default 1,
+	CONSTRAINT [PK_tblHDCallDetail_intCallDetailId] PRIMARY KEY CLUSTERED ([intCallDetailId] ASC)
+)
