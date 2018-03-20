011b6fe79c1c854f5296c612224a114b3c9244c3
COMTECH\JMendones <jesmar.mendones@irely.com>
2018-03-20 15:26:47 +0800

    FRM-7027

--- i21Database/dbo/Views/vyuSMEmailHistory.sql
+++ i21Database/dbo/Views/vyuSMEmailHistory.sql
@@ -1,6 +1,11 @@
 ﻿CREATE VIEW [dbo].[vyuSMEmailHistory]
 AS 
-SELECT Activity.intActivityId, Activity.dtmModified, Activity.strSubject, Activity.strDetails, strRecipient = dbo.fnSMConcatRecipients(Activity.intActivityId)
+SELECT 
+	Activity.intActivityId, 
+	Activity.dtmModified, 
+	Activity.strSubject, 
+	strDetails = Replace(Replace(dbo.fnStripHtml(Activity.strDetails), CHAR(10), ''), CHAR(13), ''), 
+	strRecipient = dbo.fnSMConcatRecipients(Activity.intActivityId)
 FROM tblSMActivity Activity
 INNER JOIN tblSMEmailRecipient Recipient on Activity.intActivityId = Recipient.intEmailId
 LEFT JOIN tblEMEntity Entity on Recipient.intEntityContactId = Entity.intEntityId
