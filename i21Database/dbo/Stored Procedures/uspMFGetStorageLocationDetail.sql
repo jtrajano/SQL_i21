ccc374068aa692b835ee0feb7963ca5e0e8d10f7
Vinoth Kumar T <vinoth.t@irely.com>
2018-03-21 21:47:56 +0530

    MFG-3287

---
+++ i21Database/dbo/Stored Procedures/uspMFGetStorageLocationDetail.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE uspMFGetStorageLocationDetail @strStorageLocation NVARCHAR(50)
+	,@intLocationId INT
+AS
+BEGIN
+	SELECT TOP 1 SL.intStorageLocationId
+		,SL.strName AS strStorageLocationName
+		,SL.intSubLocationId
+		,COUNT(SL.intStorageLocationId) OVER () AS intStorageLocationCount
+	FROM tblICStorageLocation SL
+	WHERE SL.intLocationId = @intLocationId
+		AND SL.strName = @strStorageLocation
+END
