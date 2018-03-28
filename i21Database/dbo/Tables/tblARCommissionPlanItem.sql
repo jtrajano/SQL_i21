ba1008b227dce060d585465c349731373441631c
Kelvin Quinto <kelvin.quinto@irely.com>
2018-03-28 09:45:56 +0800

    AR-6471

---
+++ i21Database/dbo/Tables/tblARCommissionPlanItem.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[tblARCommissionPlanItem]
+(
+	[intCommissionPlanItemId]		INT NOT NULL IDENTITY,
+	[intCommissionPlanId]			INT NOT NULL,
+	[intItemId]						INT NULL,
+	[intConcurrencyId]				INT NOT NULL DEFAULT ((0)),
+	CONSTRAINT [PK_tblARCommissionPlanItem_intCommissionPlanItemId] PRIMARY KEY CLUSTERED ([intCommissionPlanItemId] ASC),
+	CONSTRAINT [FK_tblARCommissionPlanItem_tblARCommissionPlan_intCommissionPlanId] FOREIGN KEY ([intCommissionPlanId]) REFERENCES [tblARCommissionPlan] ([intCommissionPlanId]),
+	CONSTRAINT [FK_tblARCommissionPlanItem_tblICItem_intItemId] FOREIGN KEY ([intItemId]) REFERENCES [tblICItem] ([intItemId]) ON DELETE CASCADE
+)
