9d44aaff4c3e4f89e86ace326ec6bd2de94720cc
Vinoth Kumar T <vinoth.t@irely.com>
2018-03-29 20:45:39 +0530

    MFG-2817

--- i21Database/dbo/Stored Procedures/uspWMGetUserLocationListWMS.sql
+++ i21Database/dbo/Stored Procedures/uspWMGetUserLocationListWMS.sql
@@ -26,7 +26,8 @@ BEGIN
 	  '' [language],  
 	  '' ERPCompanyID,  
 	  '' ERPWareHouseID,  
-	  0 WarehouseID, '' AS WarehouseName, '' AS AddressType  
+	  0 WarehouseID, '' AS WarehouseName, '' AS AddressType,
+	  ISNULL(smus.intCompanyLocationId, 0) AS intDefaultCompanyLocationId
 	 FROM tblSMUserSecurity smus,tblSMCompanyLocation cl --,tblSMCompanyLocationSubLocation clsl
 	 --LEFT JOIN tblSMCompanyLocation cl ON smus.intCompanyLocationId = cl.intCompanyLocationId
 	 --LEFT JOIN tblSMCompanyLocationSubLocation clsl ON clsl.intCompanyLocationId = cl.intCompanyLocationId
@@ -34,6 +35,17 @@ BEGIN
  END
  ELSE    
  BEGIN
+	DECLARE @intDefaultCompanyLocationId INT
+
+	SELECT @intDefaultCompanyLocationId = smus.intCompanyLocationId
+	FROM tblSMUserSecurity smus
+	JOIN tblSMUserSecurityCompanyLocationRolePermission CLP ON CLP.intEntityId = smus.intEntityId
+		AND CLP.intCompanyLocationId = smus.intCompanyLocationId
+	WHERE smus.strUserName = @UserName
+	
+	IF @intDefaultCompanyLocationId IS NULL
+		SELECT @intDefaultCompanyLocationId = 0
+
  	  SELECT cl.strLocationName FactoryLocation, -- + ' - ' + ISNULL(clsl.strSubLocationName,'') FactoryLocation,  
 	  smus.strUserName UserName,  
   	  smus.[intEntityId] UserKey, 
@@ -55,10 +67,13 @@ BEGIN
 	  '' [language],  
 	  '' ERPCompanyID,  
 	  '' ERPWareHouseID,  
-	  0 WarehouseID, '' AS WarehouseName, '' AS AddressType  
+	  0 WarehouseID, '' AS WarehouseName, '' AS AddressType,
+	  @intDefaultCompanyLocationId AS intDefaultCompanyLocationId
 	 FROM tblSMUserSecurity smus
-	 JOIN tblSMCompanyLocation cl ON smus.intCompanyLocationId = cl.intCompanyLocationId 
+	 JOIN tblSMUserSecurityCompanyLocationRolePermission CLP ON CLP.intEntityId = smus.intEntityId
+	 JOIN tblSMCompanyLocation cl ON cl.intCompanyLocationId = CLP.intCompanyLocationId
 	 WHERE smus.strUserName = @UserName
+	 ORDER BY cl.strLocationName
  END
   
 END  
