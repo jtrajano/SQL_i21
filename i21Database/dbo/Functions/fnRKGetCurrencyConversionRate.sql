a783adf84d3c9eaecda25cd2702a56a90670599c
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2018-04-03 04:26:04 +0530

    RM-1262

--- i21Database/dbo/Functions/fnRKGetCurrencyConversionRate.sql
+++ i21Database/dbo/Functions/fnRKGetCurrencyConversionRate.sql
@@ -19,12 +19,30 @@ BEGIN
 	DECLARE @intCurrencyExchangeRateId INT
 	DECLARE @intExchangeRateFromId INT
 	DECLARE @intExchangeRateToId INT
-
-	SELECT  @intFromCurrencyId=case when isnull(@intCostCurrencyId,0)<> 0 then @intCostCurrencyId else intCurrencyId end, @intItemId=intItemId ,@intFromUom=intPriceUnitMeasureId ,@dblRate = dblRate ,@intCurrencyExchangeRateId=intCurrencyExchangeRateId
+	
+	SELECT  @intFromCurrencyId=case when isnull(@intCostCurrencyId,0)<> 0 then @intCostCurrencyId else intCurrencyId end, @intItemId=intItemId ,
+			@intFromUom=intPriceUnitMeasureId ,@dblRate = dblRate ,@intCurrencyExchangeRateId=intCurrencyExchangeRateId
 	FROM  vyuRKPositionByPeriodContDetView WHERE intContractDetailId=@intContractDetailId
-
+	
+	IF (ISNULL(@intCurrencyExchangeRateId,0)<>0)
+	BEGIN
 	SELECT @intExchangeRateFromId=intFromCurrencyId, @intExchangeRateToId= intToCurrencyId 
 	FROM tblSMCurrencyExchangeRate WHERE intCurrencyExchangeRateId=@intCurrencyExchangeRateId
+	END
+	ELSE
+	BEGIN
+				
+		SELECT top 1 @intCurrencyExchangeRateId=intCurrencyExchangeRateId FROM tblSMCurrencyExchangeRate WHERE intFromCurrencyId= @intFromCurrencyId 
+				AND intToCurrencyId=@intToCurrencyId
+		IF (ISNULL(@intCurrencyExchangeRateId,0)=0)
+		BEGIN
+			SELECT top 1 @intCurrencyExchangeRateId=intCurrencyExchangeRateId FROM tblSMCurrencyExchangeRate WHERE intFromCurrencyId= @intToCurrencyId 
+				AND intToCurrencyId=@intFromCurrencyId
+		END
+		
+		SELECT @intExchangeRateFromId=intFromCurrencyId, @intExchangeRateToId= intToCurrencyId 
+		FROM tblSMCurrencyExchangeRate WHERE intCurrencyExchangeRateId=@intCurrencyExchangeRateId
+	END
 
 	SELECT @ysnSubCurrency=ysnSubCurrency FROM tblSMCurrency WHERE intCurrencyID=@intFromCurrencyId and ysnSubCurrency=1
 
@@ -38,8 +56,10 @@ BEGIN
 
 	IF (@intFromCurrencyId <> @intToCurrencyId)
 			BEGIN
+			
 				if (@intExchangeRateFromId = @intFromCurrencyId)
 					BEGIN
+					
 						IF (isnull(@dblRate,0)<> 0)
 						BEGIN
 							SELECT @dblResult = @Price* @dblRate
@@ -49,22 +69,24 @@ BEGIN
 							SELECT	TOP 1 @dblResult = @Price * RD.[dblRate]
 							FROM	tblSMCurrencyExchangeRate ER
 							JOIN	tblSMCurrencyExchangeRateDetail RD ON RD.intCurrencyExchangeRateId = ER.intCurrencyExchangeRateId
-							WHERE	(ER.intFromCurrencyId = @intFromCurrencyId AND ER.intToCurrencyId = @intToCurrencyId) 
+							WHERE	ER.intCurrencyExchangeRateId=@intCurrencyExchangeRateId
 							ORDER BY RD.dtmValidFromDate DESC
 						END
 					END
 					ELSE
 					BEGIN
+					
 						IF (isnull(@dblRate,0)<> 0)
 						BEGIN
 							SELECT @dblResult = @Price/@dblRate
 						END
 						ELSE
 						BEGIN
+						
 							SELECT	TOP 1 @dblResult = @Price/ RD.[dblRate]
 							FROM	tblSMCurrencyExchangeRate ER
 							JOIN	tblSMCurrencyExchangeRateDetail RD ON RD.intCurrencyExchangeRateId = ER.intCurrencyExchangeRateId
-							WHERE	(ER.intFromCurrencyId = @intFromCurrencyId AND ER.intToCurrencyId = @intToCurrencyId) 
+							WHERE	ER.intCurrencyExchangeRateId=@intCurrencyExchangeRateId
 							ORDER BY RD.dtmValidFromDate DESC
 						END
 
