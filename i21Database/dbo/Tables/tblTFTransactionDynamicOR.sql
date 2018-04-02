9c6ecf8b5f78109bfb9fc2b84a6f327d6e26d061
Lawrence Zabala <lawrence.zabala@irely.com>
2018-04-02 21:56:33 +0800

    MFT-1035

--- i21Database/dbo/Tables/tblTFTransactionDynamicOR.sql
+++ i21Database/dbo/Tables/tblTFTransactionDynamicOR.sql
@@ -7,6 +7,12 @@
     [strAltDocumentNumber] NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL, 
     [strExplanation] NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL, 
     [strInvoiceNumber] NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL, 
+	[strBeginningReading] NVARCHAR(10) COLLATE Latin1_General_CI_AS NULL,
+	[strEndingReading] NVARCHAR(10) COLLATE Latin1_General_CI_AS NULL,
+	[strLicenseNumber] NVARCHAR(10) COLLATE Latin1_General_CI_AS NULL,
+	[strLicenseState] NVARCHAR(10) COLLATE Latin1_General_CI_AS NULL,
+	[strPumpNumber] NVARCHAR(10) COLLATE Latin1_General_CI_AS NULL,
+	[strTransferType] NVARCHAR(10) COLLATE Latin1_General_CI_AS NULL,
     [intConcurrencyId] INT NULL DEFAULT ((1)), 
     CONSTRAINT [PK_tblTFTransactionDynamicOR] PRIMARY KEY ([intTransactionDynamicId]), 
     CONSTRAINT [FK_tblTFTransactionDynamicOR_tblTFTransaction] FOREIGN KEY ([intTransactionId]) REFERENCES [tblTFTransaction]([intTransactionId]) ON DELETE CASCADE
