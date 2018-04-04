342a06b4f1dd369682fd033ee0f45487244cd50e
IRELY\swghosh <swetosuvro.g@irely.com>
2018-04-04 14:36:17 +0530

    LG-1424

--- i21Database/dbo/Views/vyuLGLoadScheduleForInvoice.sql
+++ i21Database/dbo/Views/vyuLGLoadScheduleForInvoice.sql
@@ -1,159 +1,138 @@
 ﻿CREATE VIEW [dbo].[vyuLGLoadScheduleForInvoice]
 AS
-SELECT 
-	 strTransactionType				= 'Load Schedule'
-	,strTransactionNumber			= L.strLoadNumber
-	,strShippedItemId				= 'lgis:' + CAST(L.intLoadId AS NVARCHAR(250))
-	,intEntityCustomerId			= LD.intCustomerEntityId
-	,intCurrencyId					= ARCC.intCurrencyId --For Review - Invoice Header Currency--
-	,intSalesOrderId				= NULL
-	,intSalesOrderDetailId			= NULL
-	,strSalesOrderNumber			= CAST('' AS NVARCHAR(50)) COLLATE Latin1_General_CI_AS
-	,dtmProcessDate					= L.dtmScheduledDate
-	,intInventoryShipmentId			= NULL
-	,intInventoryShipmentItemId		= NULL
-	,intInventoryShipmentChargeId	= NULL
-	,strInventoryShipmentNumber		= CAST('' AS NVARCHAR(50)) COLLATE Latin1_General_CI_AS
-	,intShipmentId					= NULL
-	,strShipmentNumber				= CAST('' AS NVARCHAR(50)) COLLATE Latin1_General_CI_AS
-	,intLoadId						= L.intLoadId
-	,intLoadDetailId				= LD.intLoadDetailId
-	,intLotId						= NULL
-	,strLoadNumber					= L.strLoadNumber
-	,intRecipeItemId				= NULL
-	,intContractHeaderId			= ARCC.intContractHeaderId --For Review--
-	,intContractDetailId			= ISNULL(ARCC.intContractDetailId, LD.intPContractDetailId) --For Review--
-	,intCompanyLocationId			= LD.intSCompanyLocationId
-	,intShipToLocationId			= NULL
-	,intFreightTermId				= ARCC.intFreightTermId
-	,intItemId						= LD.intItemId
-	,strItemDescription				= ICI.[strDescription]
-	,intItemUOMId					= ISNULL(ARCC.intPriceItemUOMId,LD.intItemUOMId) --For Review--
-	,intOrderUOMId					= ARCC.intOrderUOMId
-	,intShipmentItemUOMId			= ISNULL(ARCC.intPriceItemUOMId,LD.intItemUOMId) --For Review--
-	,intWeightUOMId					= LD.intWeightItemUOMId --For Review--
-	,dblWeight						= dbo.fnCalculateQtyBetweenUOM(LD.intWeightItemUOMId, ISNULL(ARCC.intItemUOMId, LD.intItemUOMId), 1.000000) --For Review--
-	,dblQtyShipped					= dbo.fnCalculateQtyBetweenUOM(ISNULL(ARCC.intOrderUOMId, LD.intWeightItemUOMId), ISNULL(ARCC.intItemUOMId, LD.intItemUOMId), LD.dblQuantity) --For Review--
-	,dblQtyOrdered					= ISNULL(LD.dblQuantity, 0) --For Review--
-	,dblShipmentQuantity			= dbo.fnCalculateQtyBetweenUOM(ISNULL(LD.intItemUOMId, ARCC.intItemUOMId), ISNULL(ARCC.intPriceItemUOMId,LD.intItemUOMId), ISNULL(LD.dblQuantity, ARCC.dblShipQuantity)) --For Review--
-	,dblShipmentQtyShippedTotal		= dbo.fnCalculateQtyBetweenUOM(ISNULL(LD.intItemUOMId, ARCC.intItemUOMId), ISNULL(ARCC.intPriceItemUOMId,LD.intItemUOMId), ISNULL(LD.dblQuantity, ARCC.dblShipQuantity)) --For Review--
-	,dblQtyRemaining				= dbo.fnCalculateQtyBetweenUOM(ISNULL(LD.intItemUOMId, ARCC.intItemUOMId), ISNULL(ARCC.intPriceItemUOMId,LD.intItemUOMId), ISNULL(LD.dblQuantity, ARCC.dblShipQuantity)) --For Review--
-	,dblDiscount					= 0.0000000
-	,dblPrice						= ARCC.dblOrderPrice --For Review--
-	,dblShipmentUnitPrice			= ((ARCC.dblUnitPrice) / dbo.fnCalculateQtyBetweenUOM(ISNULL(ARCC.intPriceItemUOMId,LD.intItemUOMId), ISNULL(LD.intWeightItemUOMId, LDL.intWeightUOMId), 1)) --For Review--
-	,strPricing						= CAST('' AS NVARCHAR(50)) COLLATE Latin1_General_CI_AS
-	,strVFDDocumentNumber			= CAST('' AS NVARCHAR(50)) COLLATE Latin1_General_CI_AS
-	,dblTotalTax					= 0.000000
-	,dblTotal						= ((ARCC.dblUnitPrice) / dbo.fnCalculateQtyBetweenUOM(ISNULL(ARCC.intPriceItemUOMId,LD.intItemUOMId), ISNULL(LD.intWeightItemUOMId, LDL.intWeightUOMId), 1))
-										* dbo.fnCalculateQtyBetweenUOM(ISNULL(LD.intWeightItemUOMId,LDL.intWeightUOMId), ISNULL(LD.intWeightItemUOMId, LD.intItemUOMId), ISNULL(LDL.dblNet,LD.dblNet)) --For Review--
-	,intStorageLocationId			= NULL
-	,intTermId						= NULL
-	,intEntityShipViaId				= NULL
-	,intTicketId					= NULL
-	,intTaxGroupId					= NULL
-	,dblGrossWt						= dbo.fnCalculateQtyBetweenUOM(ISNULL(LD.intWeightItemUOMId, LDL.intWeightUOMId), ISNULL(LD.intWeightItemUOMId, LD.intItemUOMId), ISNULL(LDL.dblGross,LD.dblGross)) --For Review--
-	,dblTareWt						= dbo.fnCalculateQtyBetweenUOM(ISNULL(LD.intWeightItemUOMId, LDL.intWeightUOMId), ISNULL(LD.intWeightItemUOMId, LD.intItemUOMId), ISNULL(LDL.dblTare,LD.dblTare)) --For Review--
-	,dblNetWt						= dbo.fnCalculateQtyBetweenUOM(ISNULL(LD.intWeightItemUOMId, LDL.intWeightUOMId), ISNULL(LD.intWeightItemUOMId, LD.intItemUOMId), ISNULL(LDL.dblNet,LD.dblNet)) --For Review--
-	,strPONumber					= CAST('' AS NVARCHAR(50)) COLLATE Latin1_General_CI_AS
-	,strBOLNumber					= CAST('' AS NVARCHAR(50)) COLLATE Latin1_General_CI_AS
-	,intSplitId						= NULL
-	,intEntitySalespersonId			= NULL
-	,ysnBlended						= CAST(0 AS BIT)
-	,intRecipeId					= NULL
-	,intSubLocationId				= NULL
-	,intCostTypeId					= NULL
-	,intMarginById					= NULL
-	,intCommentTypeId				= NULL
-	,dblMargin						= NULL
-	,dblRecipeQuantity				= NULL
-	,intStorageScheduleTypeId		= NULL
-	,intDestinationGradeId			= ARCC.intDestinationGradeId
-	,intDestinationWeightId			= ARCC.intDestinationWeightId
-	,intCurrencyExchangeRateTypeId	= ARCC.intCurrencyExchangeRateTypeId --For Review--
-	,intCurrencyExchangeRateId		= ARCC.intCurrencyExchangeRateId --For Review--
-	,dblCurrencyExchangeRate		= ARCC.dblCurrencyExchangeRate --For Review--
-	,intSubCurrencyId				= ARCC.intSubCurrencyId --For Review--
-	,dblSubCurrencyRate				= ARCC.dblSubCurrencyRate --For Review--
+SELECT strTransactionType = 'Load Schedule'
+	,strTransactionNumber = L.strLoadNumber
+	,strShippedItemId = 'lgis:' + CAST(L.intLoadId AS NVARCHAR(250))
+	,intEntityCustomerId = LD.intCustomerEntityId
+	,intCurrencyId = AD.intSeqCurrencyId
+	,intSalesOrderId = NULL
+	,intSalesOrderDetailId = NULL
+	,strSalesOrderNumber = CAST('' AS NVARCHAR(50)) COLLATE Latin1_General_CI_AS
+	,dtmProcessDate = L.dtmScheduledDate
+	,intInventoryShipmentId = NULL
+	,intInventoryShipmentItemId = NULL
+	,intInventoryShipmentChargeId = NULL
+	,strInventoryShipmentNumber = CAST('' AS NVARCHAR(50)) COLLATE Latin1_General_CI_AS
+	,intShipmentId = NULL
+	,strShipmentNumber = CAST('' AS NVARCHAR(50)) COLLATE Latin1_General_CI_AS
+	,intLoadId = L.intLoadId
+	,intLoadDetailId = LD.intLoadDetailId
+	,intLotId = NULL
+	,strLoadNumber = L.strLoadNumber
+	,intRecipeItemId = NULL
+	,intContractHeaderId = CH.intContractHeaderId
+	,intContractDetailId = ISNULL(CD.intContractDetailId, LD.intPContractDetailId)
+	,intCompanyLocationId = LD.intSCompanyLocationId
+	,intShipToLocationId = NULL
+	,intFreightTermId = CD.intFreightTermId
+	,intItemId = LD.intItemId
+	,strItemDescription = ICI.[strDescription]
+	,intItemUOMId = ISNULL(AD.intSeqPriceUOMId, LD.intItemUOMId)
+	,intOrderUOMId = CD.intItemUOMId
+	,intShipmentItemUOMId = ISNULL(AD.intSeqPriceUOMId, LD.intItemUOMId)
+	,intWeightUOMId = LD.intWeightItemUOMId
+	,dblWeight = dbo.fnCalculateQtyBetweenUOM(LD.intWeightItemUOMId, ISNULL(CD.intItemUOMId, LD.intItemUOMId), 1.000000)
+	,dblQtyShipped = dbo.fnCalculateQtyBetweenUOM(ISNULL(CD.intItemUOMId, LD.intWeightItemUOMId), ISNULL(CD.intItemUOMId, LD.intItemUOMId), LD.dblQuantity)
+	,dblQtyOrdered = ISNULL(LD.dblQuantity, 0)
+	,dblShipmentQuantity = dbo.fnCalculateQtyBetweenUOM(ISNULL(LD.intItemUOMId, CD.intItemUOMId), ISNULL(AD.intSeqPriceUOMId, LD.intItemUOMId), ISNULL(LD.dblQuantity, CD.dblQuantity))
+	,dblShipmentQtyShippedTotal = dbo.fnCalculateQtyBetweenUOM(ISNULL(LD.intItemUOMId, CD.intItemUOMId), ISNULL(AD.intSeqPriceUOMId, LD.intItemUOMId), ISNULL(LD.dblQuantity, CD.dblQuantity))
+	,dblQtyRemaining = dbo.fnCalculateQtyBetweenUOM(ISNULL(LD.intItemUOMId, CD.intItemUOMId), ISNULL(AD.intSeqPriceUOMId, LD.intItemUOMId), ISNULL(LD.dblQuantity, CD.dblQuantity))
+	,dblDiscount = 0.0000000
+	,dblPrice = AD.dblSeqPrice
+	,dblShipmentUnitPrice = (
+		(
+			CASE 
+				WHEN CD.intPricingTypeId = 2
+					THEN dbo.fnRKGetLatestClosingPrice(CD.intFutureMarketId, CD.intFutureMonthId, GETDATE()) + CD.dblBasis
+				ELSE AD.dblSeqPrice
+				END
+			) / dbo.fnCalculateQtyBetweenUOM(ISNULL(AD.intSeqPriceUOMId, LD.intItemUOMId), ISNULL(LD.intWeightItemUOMId, LDL.intWeightUOMId), 1)
+		)
+	,strPricing = CAST('' AS NVARCHAR(50)) COLLATE Latin1_General_CI_AS
+	,strVFDDocumentNumber = CAST('' AS NVARCHAR(50)) COLLATE Latin1_General_CI_AS
+	,dblTotalTax = 0.000000
+	,dblTotal = (
+		(
+			CASE 
+				WHEN CD.intPricingTypeId = 2
+					THEN dbo.fnRKGetLatestClosingPrice(CD.intFutureMarketId, CD.intFutureMonthId, GETDATE()) + CD.dblBasis
+				ELSE AD.dblSeqPrice
+				END
+			) / dbo.fnCalculateQtyBetweenUOM(ISNULL(AD.intSeqPriceUOMId, LD.intItemUOMId), ISNULL(LD.intWeightItemUOMId, LDL.intWeightUOMId), 1)
+		) * dbo.fnCalculateQtyBetweenUOM(ISNULL(LD.intWeightItemUOMId, LDL.intWeightUOMId), ISNULL(LD.intWeightItemUOMId, LD.intItemUOMId), ISNULL(LDL.dblNet, LD.dblNet))
+	,intStorageLocationId = NULL
+	,intTermId = NULL
+	,intEntityShipViaId = NULL
+	,intTicketId = NULL
+	,intTaxGroupId = NULL
+	,dblGrossWt = dbo.fnCalculateQtyBetweenUOM(ISNULL(LD.intWeightItemUOMId, LDL.intWeightUOMId), ISNULL(LD.intWeightItemUOMId, LD.intItemUOMId), ISNULL(LDL.dblGross, LD.dblGross))
+	,dblTareWt = dbo.fnCalculateQtyBetweenUOM(ISNULL(LD.intWeightItemUOMId, LDL.intWeightUOMId), ISNULL(LD.intWeightItemUOMId, LD.intItemUOMId), ISNULL(LDL.dblTare, LD.dblTare))
+	,dblNetWt = dbo.fnCalculateQtyBetweenUOM(ISNULL(LD.intWeightItemUOMId, LDL.intWeightUOMId), ISNULL(LD.intWeightItemUOMId, LD.intItemUOMId), ISNULL(LDL.dblNet, LD.dblNet))
+	,strPONumber = CAST('' AS NVARCHAR(50)) COLLATE Latin1_General_CI_AS
+	,strBOLNumber = CAST('' AS NVARCHAR(50)) COLLATE Latin1_General_CI_AS
+	,intSplitId = NULL
+	,intEntitySalespersonId = NULL
+	,ysnBlended = CAST(0 AS BIT)
+	,intRecipeId = NULL
+	,intSubLocationId = NULL
+	,intCostTypeId = NULL
+	,intMarginById = NULL
+	,intCommentTypeId = NULL
+	,dblMargin = NULL
+	,dblRecipeQuantity = NULL
+	,intStorageScheduleTypeId = NULL
+	,intDestinationGradeId = CH.intGradeId
+	,intDestinationWeightId = CH.intWeightId
+	,intCurrencyExchangeRateTypeId = CD.intRateTypeId
+	,intCurrencyExchangeRateId = CD.intCurrencyExchangeRateId
+	,dblCurrencyExchangeRate = CD.dblRate
+	,intSubCurrencyId = AD.intSeqCurrencyId
+	,dblSubCurrencyRate = CASE 
+		WHEN AD.ysnSeqSubCurrency = 1
+			THEN CU.intCent
+		ELSE 1.000000
+		END
 FROM (
 	SELECT intLoadId
-		 , strLoadNumber
-		 , dtmScheduledDate
+		,strLoadNumber
+		,dtmScheduledDate
 	FROM dbo.tblLGLoad WITH (NOLOCK)
-	WHERE ysnPosted = 1 --AND intShipmentStatus = 6
-) L
+	WHERE ysnPosted = 1
+		AND intShipmentStatus = 6
+	) L
 JOIN (
 	SELECT intLoadId
-		 , intLoadDetailId
-		 , intCustomerEntityId
-		 , intItemId
-		 , intSContractDetailId
-		 , intItemUOMId
-		 , intWeightItemUOMId
-		 , intSCompanyLocationId
-		 , intPContractDetailId
-		 , dblQuantity
-		 , dblGross
-		 , dblTare
-		 , dblNet
-	FROM 
-		dbo.tblLGLoadDetail WITH (NOLOCK)		
-
-) LD ON L.intLoadId  = LD.intLoadId
+		,intLoadDetailId
+		,intCustomerEntityId
+		,intItemId
+		,intSContractDetailId
+		,intItemUOMId
+		,intWeightItemUOMId
+		,intSCompanyLocationId
+		,intPContractDetailId
+		,dblQuantity
+		,dblGross
+		,dblTare
+		,dblNet
+	FROM dbo.tblLGLoadDetail WITH (NOLOCK)
+	) LD ON L.intLoadId = LD.intLoadId
 LEFT JOIN (
 	SELECT intLoadDetailId
-		 , intWeightUOMId
-		 , dblGross	= SUM(dblGross)
-		 , dblTare	= SUM(dblTare)
-		 , dblNet	= SUM(dblNet)
-	FROM dbo.tblLGLoadDetailLot WITH (NOLOCK) 
-	GROUP BY
-		 intLoadDetailId
 		,intWeightUOMId
-) LDL ON LDL.intLoadDetailId = LD.intLoadDetailId
---LEFT JOIN (
---	SELECT intLotId
---		 , intStorageLocationId
---	FROM dbo.tblICLot WITH (NOLOCK)
---) LO ON LO.intLotId = LDL.intLotId
---LEFT OUTER JOIN (
---	SELECT intInventoryShipmentItemId
---		 , intRecipeItemId
---		 , strShipmentNumber
---		 , intLoadDetailId
---	 FROM tblARInvoiceDetail WITH (NOLOCK)
---	 WHERE ISNULL(intLoadDetailId, 0) = 0
---) ARID ON LDL.intLoadDetailId = ARID.intLoadDetailId
-LEFT OUTER JOIN (
-SELECT intContractHeaderId
-	 , intContractDetailId
-	 , strContractNumber
-	 , intContractSeq
-	 , intDestinationGradeId
-	 , strDestinationGrade
-	 , intDestinationWeightId
-	 , strDestinationWeight
-	 , intSubCurrencyId
-	 , intCurrencyId
-	 , strUnitMeasure
-	 , intOrderUOMId
-	 , intPriceItemUOMId
-	 , intItemUOMId
-	 , strOrderUnitMeasure
-	 , intItemWeightUOMId	 
-	 , dblCashPrice
-	 , dblOrderPrice
-	 , dblUnitPrice
-	 , dblDetailQuantity
-	 , intFreightTermId			 
-	 , dblShipQuantity
-	 , dblOrderQuantity
-	 , dblSubCurrencyRate
-	 , intCurrencyExchangeRateTypeId
-	 , strCurrencyExchangeRateType
-	 , intCurrencyExchangeRateId
-	 , dblCurrencyExchangeRate
- FROM dbo.vyuCTCustomerContract WITH (NOLOCK) --For Review - vyuARCustomerContract will be deleted once CT-1873 is completed--
-) ARCC ON LD.intSContractDetailId = ARCC.intContractDetailId
-INNER JOIN
-	(SELECT [intItemId], [strItemNo], [strDescription] FROM tblICItem WITH(NOLOCK)) ICI
-		ON LD.[intItemId] = ICI.[intItemId]
+		,dblGross = SUM(dblGross)
+		,dblTare = SUM(dblTare)
+		,dblNet = SUM(dblNet)
+	FROM dbo.tblLGLoadDetailLot WITH (NOLOCK)
+	GROUP BY intLoadDetailId
+		,intWeightUOMId
+	) LDL ON LDL.intLoadDetailId = LD.intLoadDetailId
+INNER JOIN (
+	SELECT [intItemId]
+		,[strItemNo]
+		,[strDescription]
+	FROM tblICItem WITH (NOLOCK)
+	) ICI ON LD.[intItemId] = ICI.[intItemId]
+LEFT JOIN tblCTContractDetail CD ON CD.intContractDetailId = LD.intSContractDetailId
+LEFT JOIN tblCTContractHeader CH ON CH.intContractHeaderId = CD.intContractHeaderId
+OUTER APPLY dbo.fnCTGetAdditionalColumnForDetailView(CD.intContractDetailId) AD
+LEFT JOIN tblSMCurrency CU ON CU.intCurrencyID = AD.intSeqCurrencyId
