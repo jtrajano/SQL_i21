f7a9c402a3d49f8b6f32875d608489530572fa5d
Ruben Escarcha <ruben.escarcha@irely.com>
2018-04-10 14:35:42 +0800

    AR-7104 Receive Payments - Incorrect Debit and Credit values

--- i21Database/dbo/Stored Procedures/uspARGetItemPrice.sql
+++ i21Database/dbo/Stored Procedures/uspARGetItemPrice.sql
@@ -1,71 +1,71 @@
 ﻿CREATE PROCEDURE [dbo].[uspARGetItemPrice]
-	 @ItemId					INT
-	,@CustomerId				INT	
-	,@LocationId				INT				= NULL
-	,@ItemUOMId					INT				= NULL
-	,@TransactionDate			DATETIME		= NULL
-	,@Quantity					NUMERIC(18,6)
-	,@Price						NUMERIC(18,6)	= NULL OUTPUT
-	,@Pricing					NVARCHAR(250)	= NULL OUTPUT	
-	,@ContractHeaderId			INT				= NULL OUTPUT
-	,@ContractDetailId			INT				= NULL OUTPUT
-	,@ContractNumber			NVARCHAR(50)	= NULL OUTPUT
-	,@ContractSeq				INT				= NULL OUTPUT
-	,@NewQuantity				NUMERIC(18,6)   = NULL OUTPUT
-	,@PriceUOMId				INT			    = NULL OUTPUT
-	,@PriceUOM					NVARCHAR(50)	= NULL OUTPUT
-	,@AvailableQuantity			NUMERIC(18,6)   = NULL OUTPUT
-	,@UnlimitedQuantity			BIT             = 0    OUTPUT
-	,@Deviation					NUMERIC(18,6)	= NULL OUTPUT
-	,@TermDiscount				NUMERIC(18,6)	= NULL OUTPUT
-	,@TermDiscountBy			NVARCHAR(50)	= NULL OUTPUT
-	,@OriginalQuantity			NUMERIC(18,6)	= NULL
-	,@CustomerPricingOnly		BIT				= 0
-	,@ItemPricingOnly			BIT				= 0
-	,@ExcludeContractPricing	BIT				= 0
-	,@VendorId					INT				= NULL
-	,@SupplyPointId				INT				= NULL
-	,@LastCost					NUMERIC(18,6)	= NULL
-	,@ShipToLocationId			INT				= NULL
-	,@VendorLocationId			INT				= NULL
-	,@PricingLevelId			INT				= NULL
-	,@AllowQtyToExceedContract	BIT				= 0
-	,@InvoiceType				NVARCHAR(200)	= NULL
-	,@TermId					INT				= NULL
-	,@CurrencyId				INT				= NULL
-	,@SubCurrencyId				INT				= NULL OUTPUT
-	,@SubCurrency				NVARCHAR(250)	= NULL OUTPUT
-	,@SubCurrencyRate			NUMERIC(18,6)	= NULL OUTPUT
-	,@PricingType				NVARCHAR(50)	= NULL OUTPUT
-	,@TermIdOut					INT				= NULL OUTPUT
-	,@GetAllAvailablePricing	BIT				= 0	
-	,@SpecialPriceId			INT				= NULL OUTPUT
-	,@ProgramId					INT				= NULL OUTPUT
-	,@ProgramType				NVARCHAR(100)	= NULL OUTPUT
+	 @ItemId						INT
+	,@CustomerId					INT	
+	,@LocationId					INT				= NULL
+	,@ItemUOMId						INT				= NULL
+	,@TransactionDate				DATETIME		= NULL
+	,@Quantity						NUMERIC(18,6)
+	,@Price							NUMERIC(18,6)	= NULL OUTPUT
+	,@Pricing						NVARCHAR(250)	= NULL OUTPUT	
+	,@ContractHeaderId				INT				= NULL OUTPUT
+	,@ContractDetailId				INT				= NULL OUTPUT
+	,@ContractNumber				NVARCHAR(50)	= NULL OUTPUT
+	,@ContractSeq					INT				= NULL OUTPUT
+	,@AvailableQuantity				NUMERIC(18,6)   = NULL OUTPUT
+	,@UnlimitedQuantity				BIT             = 0    OUTPUT
+	,@Deviation						NUMERIC(18,6)	= NULL OUTPUT
+	,@TermDiscount					NUMERIC(18,6)	= NULL OUTPUT
+	,@TermDiscountBy				NVARCHAR(50)	= NULL OUTPUT
+	,@OriginalQuantity				NUMERIC(18,6)	= NULL
+	,@CustomerPricingOnly			BIT				= 0
+	,@ItemPricingOnly				BIT				= 0
+	,@ExcludeContractPricing		BIT				= 0
+	,@VendorId						INT				= NULL
+	,@SupplyPointId					INT				= NULL
+	,@LastCost						NUMERIC(18,6)	= NULL
+	,@ShipToLocationId				INT				= NULL
+	,@VendorLocationId				INT				= NULL
+	,@PricingLevelId				INT				= NULL
+	,@AllowQtyToExceedContract		BIT				= 0
+	,@InvoiceType					NVARCHAR(200)	= NULL
+	,@TermId						INT				= NULL
+	,@CurrencyId					INT				= NULL
+	,@CurrencyExchangeRateTypeId	INT			= NULL OUTPUT
+	,@CurrencyExchangeRateType		NVARCHAR(20)	= NULL OUTPUT
+	,@CurrencyExchangeRate			NUMERIC(18,6)	= NULL OUTPUT
+	,@SubCurrencyId					INT				= NULL OUTPUT
+	,@SubCurrency					NVARCHAR(250)	= NULL OUTPUT
+	,@SubCurrencyRate				NUMERIC(18,6)	= NULL OUTPUT
+	,@PricingType					NVARCHAR(50)	= NULL OUTPUT
+	,@TermIdOut						INT				= NULL OUTPUT
+	,@GetAllAvailablePricing		BIT				= 0	
+	,@SpecialPriceId				INT				= NULL OUTPUT
+	,@ProgramId						INT				= NULL OUTPUT
+	,@ProgramType					NVARCHAR(100)	= NULL OUTPUT
 	
 AS	
 
 	SELECT
-		 @Price				= dblPrice
-		,@Pricing			= strPricing
-		,@ContractHeaderId	= intContractHeaderId
-		,@ContractDetailId	= intContractDetailId
-		,@ContractNumber	= strContractNumber
-		,@ContractSeq		= intContractSeq
-		,@PriceUOMId		= intPriceUOMId
-		,@PriceUOM			= strPriceUOM
-		,@NewQuantity		= dblQuantity
-		,@AvailableQuantity = dblAvailableQty
-		,@UnlimitedQuantity = ysnUnlimitedQty
-		,@Deviation			= dblDeviation
-		,@TermDiscount		= dblTermDiscount  
-		,@PricingType		= strPricingType
-		,@TermIdOut			= intTermId
-		,@TermDiscountBy	= strTermDiscountBy
-		,@SubCurrencyId		= intSubCurrencyId
-		,@SubCurrency		= strSubCurrency
-		,@SubCurrencyRate	= dblSubCurrencyRate
-		,@SpecialPriceId	= intSpecialPriceId
+		 @Price							= dblPrice
+		,@Pricing						= strPricing
+		,@ContractHeaderId				= intContractHeaderId
+		,@ContractDetailId				= intContractDetailId
+		,@ContractNumber				= strContractNumber
+		,@ContractSeq					= intContractSeq
+		,@AvailableQuantity				= dblAvailableQty
+		,@UnlimitedQuantity				= ysnUnlimitedQty
+		,@Deviation						= dblDeviation
+		,@TermDiscount					= dblTermDiscount  
+		,@PricingType					= strPricingType
+		,@TermIdOut						= intTermId
+		,@TermDiscountBy				= strTermDiscountBy
+		,@CurrencyExchangeRateTypeId	= intCurrencyExchangeRateTypeId
+		,@CurrencyExchangeRateType		= strCurrencyExchangeRateType
+		,@CurrencyExchangeRate			= dblCurrencyExchangeRate
+		,@SubCurrencyId					= intSubCurrencyId
+		,@SubCurrency					= strSubCurrency
+		,@SubCurrencyRate				= dblSubCurrencyRate
+		,@SpecialPriceId				= intSpecialPriceId
 	FROM
 		[dbo].[fnARGetItemPricingDetails](
 			 @ItemId
