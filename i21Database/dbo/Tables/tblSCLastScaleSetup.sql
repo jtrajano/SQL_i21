3b90294887c8f3c01202b50be2df86965530be71
Emerson Versoza <emerson.versoza@irely.com>
2018-04-11 17:00:45 +0800

    SC-1498

--- i21Database/dbo/Tables/tblSCLastScaleSetup.sql
+++ i21Database/dbo/Tables/tblSCLastScaleSetup.sql
@@ -3,9 +3,11 @@
 	[intLastScaleSetupId] [int] IDENTITY(1,1) NOT NULL,
 	[intScaleSetupId] [int] NULL,
 	[intEntityId] [int] NULL,
+	[intEntityScaleOperatorId] [int] NULL,
 	[dtmScaleDate] [date] NULL,
-	[strScaleOperator] [varchar](40) COLLATE Latin1_General_CI_AS NULL,
 	[intConcurrencyId] [int] NULL, 
     CONSTRAINT [PK_tblSCLastScaleSetup] PRIMARY KEY ([intLastScaleSetupId]),
 	CONSTRAINT [FK_tblSCLastScaleSetup_tblSCScaleSetup_intScaleSetupId] FOREIGN KEY ([intScaleSetupId]) REFERENCES [tblSCScaleSetup]([intScaleSetupId]),  
+	CONSTRAINT [FK_tblSCLastScaleSetup_tblEMEntity_intEntityId] FOREIGN KEY ([intEntityScaleOperatorId]) REFERENCES [tblEMEntity]([intEntityId]),
+	CONSTRAINT [FK_tblSCLastScaleSetup_tblSMUserSecurity_intEntityId] FOREIGN KEY ([intEntityId]) REFERENCES [tblSMUserSecurity]([intEntityId])
 )
