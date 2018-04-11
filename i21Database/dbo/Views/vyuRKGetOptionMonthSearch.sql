9ad37a9c8ccc5890a1b35393ff018f39acd383d0
Smith Wilson de Jesus <smith.dejesus@irely.com>
2018-04-11 21:17:57 +0800

    RM-1087

--- i21Database/dbo/Views/vyuRKGetOptionMonthSearch.sql
+++ i21Database/dbo/Views/vyuRKGetOptionMonthSearch.sql
@@ -10,10 +10,11 @@ SELECT intOptionMonthId ,
 	dtmExpirationDate ,
 	strOptMonthSymbol,
 	strFutureMonth,
-	strOptMarketName,
+	fmar.strOptMarketName,
 	strOptionMonth strOptionMonthOriginal ,
 	strCommodityCode
 FROM tblRKOptionsMonth m
 JOIN tblRKFuturesMonth fm on fm.intFutureMonthId=m.intFutureMonthId
 JOIN tblRKFutureMarket fmar on m.intFutureMarketId=fmar.intFutureMarketId
-JOIN tblICCommodity c on m.intCommodityMarketId=c.intCommodityId
+JOIN tblRKCommodityMarketMapping cmm on fmar.intFutureMarketId = cmm.intFutureMarketId
+JOIN tblICCommodity c on cmm.intCommodityId =c.intCommodityId
