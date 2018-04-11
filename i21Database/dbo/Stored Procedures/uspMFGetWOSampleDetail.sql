fe694550ca13b5088995674a4d4bbbf0b969a711
Vinoth Kumar T <vinoth.t@irely.com>
2018-04-11 19:34:57 +0530

    MFG-3404

---
+++ i21Database/dbo/Stored Procedures/uspMFGetWOSampleDetail.sql
@@ -0,0 +1,31 @@
+﻿CREATE PROCEDURE uspMFGetWOSampleDetail @intWorkOrderId INT
+AS
+BEGIN
+	SELECT S.intSampleId
+		,S.strSampleNumber
+		,ST.strSampleTypeName
+		,SS.strStatus AS strSampleStatus
+		,I.strItemNo
+		,I.strDescription
+		,S.dtmSampleReceivedDate
+		,S.strSampleNote
+		,S.strComment
+		,E.strName AS strPartyName
+		,S.dblSampleQty
+		,UM.strUnitMeasure AS strSampleUOM
+		,S.dblRepresentingQty
+		,UM1.strUnitMeasure AS strRepresentingUOM
+		,S.dtmTestingStartDate
+		,S.dtmTestingEndDate
+		,S.dtmSamplingEndDate
+	FROM tblQMSample S
+	JOIN tblQMSampleType ST ON ST.intSampleTypeId = S.intSampleTypeId
+		AND S.intProductTypeId = 12
+		AND S.intProductValueId = @intWorkOrderId
+	JOIN tblQMSampleStatus SS ON SS.intSampleStatusId = S.intSampleStatusId
+	LEFT JOIN tblICItem I ON I.intItemId = S.intItemId
+	LEFT JOIN tblEMEntity E ON E.intEntityId = S.intEntityId
+	LEFT JOIN tblICUnitMeasure UM ON UM.intUnitMeasureId = S.intSampleUOMId
+	LEFT JOIN tblICUnitMeasure UM1 ON UM1.intUnitMeasureId = S.intRepresentingUOMId
+	ORDER BY S.intSampleId DESC
+END
