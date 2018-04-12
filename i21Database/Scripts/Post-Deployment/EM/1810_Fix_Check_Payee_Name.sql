2d99fe3ba71679d615cf0e51b9650e7f863c259e
Dan Jalimao <dan.jalimao@irely.com>
2018-04-12 15:07:41 +0800

    EM-2467

---
+++ i21Database/Scripts/Post-Deployment/EM/1810_Fix_Check_Payee_Name.sql
@@ -0,0 +1,15 @@
+﻿PRINT '*** ----  Checking Fix Check Payee Name  ---- ***'
+
+IF NOT EXISTS(SELECT TOP 1 1 FROM [tblEMEntityPreferences] WHERE strPreference = 'Fix Check Payee Name')
+BEGIN
+
+PRINT '*** ----  Start Fix Check Payee Name  ---- ***'
+
+	update tblEMEntityLocation set strCheckPayeeName = strLocationName where strCheckPayeeName is null
+
+INSERT INTO [tblEMEntityPreferences] ( strPreference, strValue)
+	VALUES('Fix Check Payee Name', 1)
+
+PRINT '*** ----  End Check Payee Name ---- ***'
+
+END
