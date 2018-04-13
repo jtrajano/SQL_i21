1cdf4e06a55345b69e401e55bb5818f9d13ec847
Henry Zaragoza Jr <henry.zaragoza@irely.com>
2018-04-13 10:13:42 +0800

    ST-690 Checkout - Sales tax totals were not generated dynamically

--- i21Database/dbo/Stored Procedures/uspSTGetSalesTaxTotalsPreload.sql
+++ i21Database/dbo/Stored Procedures/uspSTGetSalesTaxTotalsPreload.sql
@@ -3,22 +3,34 @@
 AS
 BEGIN
 
-	DECLARE @tbl TABLE (intCnt int, intAccountId int)
+	DECLARE @tbl TABLE (strTaxNumber NVARCHAR(20), intAccountId INT)
 
-	DECLARE @intCnt int
-	SET @intCnt = 1
-	WHILE(@intCnt <= 4)
-	BEGIN
-		INSERT INTO @tbl
-		Select @intCnt [Cnt], MAX(intSalesTaxAccountId) [intSalesTaxAccountId] FROM dbo.tblSMTaxCode TC
-		JOIN dbo.tblSMTaxGroupCode TGC ON TGC.intTaxCodeId = TC.intTaxCodeId
-		JOIN dbo.tblSTStore ST ON ST.intTaxGroupId = TGC.intTaxGroupId
-		WHERE ISNULL(strStoreTaxNumber,'') = CAST(@intCnt as nvarchar(10)) AND ST.intStoreId = @intStoreId
+	--DECLARE @intCnt int
+	--SET @intCnt = 1
+	--WHILE(@intCnt <= 4)
+	--BEGIN
+	--	INSERT INTO @tbl
+	--	Select @intCnt [Cnt], MAX(intSalesTaxAccountId) [intSalesTaxAccountId] FROM dbo.tblSMTaxCode TC
+	--	JOIN dbo.tblSMTaxGroupCode TGC ON TGC.intTaxCodeId = TC.intTaxCodeId
+	--	JOIN dbo.tblSTStore ST ON ST.intTaxGroupId = TGC.intTaxGroupId
+	--	WHERE ISNULL(strStoreTaxNumber,'') = CAST(@intCnt as nvarchar(10)) AND ST.intStoreId = @intStoreId
 
-		SET @intCnt = @intCnt + 1
-	END
+	--	SET @intCnt = @intCnt + 1
+	--END
 
-	Select t.*, Acc.strAccountId from @tbl t
+	INSERT INTO @tbl
+	SELECT TC.strStoreTaxNumber AS strTaxNumber
+			, TC.intSalesTaxAccountId AS intAccountId
+	FROM dbo.tblSMTaxCode TC 
+	JOIN dbo.tblSMTaxGroupCode TGC ON TGC.intTaxCodeId = TC.intTaxCodeId
+	JOIN dbo.tblSTStore ST ON ST.intTaxGroupId = TGC.intTaxGroupId
+	LEFT JOIN dbo.tblGLAccount GLA ON GLA.intAccountId = TC.intSalesTaxAccountId
+	WHERE ST.intStoreId = @intStoreId
+
+	SELECT t.strTaxNumber
+		 , t.intAccountId
+		 , Acc.strAccountId 
+	FROM @tbl t
 	LEFT JOIN dbo.tblGLAccount Acc ON Acc.intAccountId = t.intAccountId
 
 END
