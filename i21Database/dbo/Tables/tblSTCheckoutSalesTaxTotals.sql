3141d133914482babea44d895eecdc7222b8620a
Henry Zaragoza Jr <henry.zaragoza@irely.com>
2018-04-16 20:38:53 +0800

    ST-696 [Checkout] Changes in Pump Totals

--- i21Database/dbo/Tables/tblSTCheckoutSalesTaxTotals.sql
+++ i21Database/dbo/Tables/tblSTCheckoutSalesTaxTotals.sql
@@ -2,7 +2,7 @@
 (
 	[intSalesTaxTotalsId] INT NOT NULL IDENTITY, 
 	[intCheckoutId] INT,
-    [intTaxNo] INT NULL, 
+    [strTaxNo] NVARCHAR(200) COLLATE Latin1_General_CI_AS NULL, 
     [dblTotalTax] DECIMAL(18, 6) NULL, 
     [dblTaxableSales] DECIMAL(18, 6) NULL, 
     [dblTaxExemptSales] DECIMAL(18, 6) NULL, 
