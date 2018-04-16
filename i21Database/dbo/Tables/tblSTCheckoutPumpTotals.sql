8193354dda0204af328585dc723116a9a553196c
Henry Zaragoza Jr <henry.zaragoza@irely.com>
2018-04-16 16:07:29 +0800

    ST-696 [Checkout] Changes in Pump Totals

--- i21Database/dbo/Tables/tblSTCheckoutPumpTotals.sql
+++ i21Database/dbo/Tables/tblSTCheckoutPumpTotals.sql
@@ -4,6 +4,7 @@
 	[intCheckoutId] INT,
     [intPumpCardCouponId] INT NOT NULL,
 	[intCategoryId] int NULL, 
+	[intItemId] INT NULL, 
     [strDescription] NVARCHAR(200) COLLATE Latin1_General_CI_AS NULL, 
     [dblPrice] DECIMAL(18, 6) NULL, 
     [dblQuantity] DECIMAL(18, 6) NULL, 
@@ -12,5 +13,6 @@
     CONSTRAINT [PK_tblSTCheckoutPumpTotals_intPumpTotalsId] PRIMARY KEY ([intPumpTotalsId]), 
     CONSTRAINT [FK_tblSTCheckoutPumpTotals_tblSTCheckoutHeader] FOREIGN KEY ([intCheckoutId]) REFERENCES [tblSTCheckoutHeader]([intCheckoutId]) ON DELETE CASCADE, 
     CONSTRAINT [FK_tblSTCheckoutPumpTotals_tblICItemUOM] FOREIGN KEY ([intPumpCardCouponId]) REFERENCES [tblICItemUOM]([intItemUOMId]) ,
-	CONSTRAINT [FK_tblSTCheckoutPumpTotals_tblICCategory] FOREIGN KEY ([intCategoryId]) REFERENCES [dbo].[tblICCategory] ([intCategoryId])
+	CONSTRAINT [FK_tblSTCheckoutPumpTotals_tblICCategory] FOREIGN KEY ([intCategoryId]) REFERENCES [dbo].[tblICCategory] ([intCategoryId]),
+	CONSTRAINT [FK_tblSTCheckoutPumpTotals_tblICItem] FOREIGN KEY ([intItemId]) REFERENCES [dbo].[tblICItem] ([intItemId])
 )
