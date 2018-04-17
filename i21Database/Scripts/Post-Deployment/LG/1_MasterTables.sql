67797fb5c54b270ee49ea4d85f2592f4721708c1
IRELY\swghosh <swetosuvro.g@irely.com>
2018-04-17 19:49:27 +0530

    LG-1437

--- i21Database/Scripts/Post-Deployment/LG/1_MasterTables.sql
+++ i21Database/Scripts/Post-Deployment/LG/1_MasterTables.sql
@@ -2,3 +2,10 @@
 BEGIN
 	UPDATE tblLGLoad SET intShipmentType = 1 WHERE ISNULL(intShipmentType,0) = 0
 END
+GO
+IF NOT EXISTS(SELECT 1 FROM tblLGCompanyPreference)
+BEGIN
+	INSERT INTO tblLGCompanyPreference(intConcurrencyId)
+	SELECT 1
+END
+GO
