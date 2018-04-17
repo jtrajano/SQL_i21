fde8d3b49c0a3cb41c0ce0b7b5278a008f54a2f8
IRELY\swghosh <swetosuvro.g@irely.com>
2018-04-17 22:35:58 +0530

    LG-1313, LG-1312

---
+++ i21Database/dbo/Tables/tblLGInsurancePremiumFactorDetail.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[tblLGInsurancePremiumFactorDetail]
+(
+	intInsurancePremiumFactorDetailId INT NOT NULL PRIMARY KEY IDENTITY (1, 1),
+	intInsurancePremiumFactorId INT NOT NULL,
+	dtmValidFrom DATETIME NOT NULL,
+	dtmValidTo DATETIME NOT NULL,
+	dblCost NUMERIC(18,6) NOT NULL,
+	intCostUOM INT NOT NULL, 
+	dblInsurancePremiumFactor NUMERIC(18,6),
+	dblProfitMarkup NUMERIC(18,6),
+	intConcurrencyId INT NOT NULL, 
+
+	CONSTRAINT [FK_tblLGInsurancePremiumFactorDetail_tblLGInsurancePremiumFactor_intInsurancePremiumFactorId] FOREIGN KEY ([intInsurancePremiumFactorId]) REFERENCES [tblLGInsurancePremiumFactor]([intInsurancePremiumFactorId])
+)
