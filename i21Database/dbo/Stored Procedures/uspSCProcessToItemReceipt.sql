029a805f6f121df71bb229365320b56fb1a7d3a0
Emerson Versoza <emerson.versoza@irely.com>
2018-04-18 15:45:16 +0800

    Revert "SC-1570"
    This reverts commit b8d40306c14477b0084cf0d54512f8a766f2c35a.

--- i21Database/dbo/Stored Procedures/uspSCProcessToItemReceipt.sql
+++ i21Database/dbo/Stored Procedures/uspSCProcessToItemReceipt.sql
@@ -660,7 +660,7 @@ BEGIN TRY
 				[intId]
 			)
 			SELECT [intId] = intPrepayId
-			FROM #tmpContractPrepay where intPrepayId > 0
+			FROM #tmpContractPrePay where intPrepayId > 0
 			
 			EXEC uspAPApplyPrepaid @intBillId, @prePayId
 			update tblAPBillDetail set intScaleTicketId = @intTicketId WHERE intBillId = @intBillId
