58470b89d387ffd989f95ab14d61c687aacee90c
Wendell Wayne Estrada <wendell.estrada@irely.com>
2018-04-19 16:30:36 +0800

    [B] IC-5195 Recount option in Inventory Count is not working

---
+++ i21Database/dbo/Stored Procedures/uspICUpdateDetailsForRecount.sql
@@ -0,0 +1,78 @@
+﻿CREATE PROCEDURE [dbo].[uspICUpdateDetailsForRecount]
+	  @intReferenceCountId INT
+	, @intNewCountId INT
+	, @intEntityUserSecurityId INT
+AS
+
+SET QUOTED_IDENTIFIER OFF
+SET ANSI_NULLS ON
+SET NOCOUNT ON
+SET XACT_ABORT ON
+SET ANSI_WARNINGS OFF
+
+INSERT INTO tblICInventoryCountDetail(
+			  intInventoryCountId
+			, intItemId
+			, intItemLocationId
+			, intSubLocationId
+			, intStorageLocationId
+			, intCountGroupId
+			, intLotId
+			, strLotNo
+			, strLotAlias
+			, intParentLotId
+			, strParentLotNo
+			, strParentLotAlias
+			, intStockUOMId
+			, dblSystemCount
+			, dblLastCost
+			, strAutoCreatedLotNumber
+			, strCountLine
+			, dblPallets
+			, dblQtyPerPallet
+			, dblPhysicalCount
+			, intItemUOMId
+			, intWeightUOMId
+			, dblWeightQty
+			, dblNetQty
+			, ysnRecount
+			, dblQtyReceived
+			, dblQtySold
+			, intEntityUserSecurityId
+			, ysnFetched
+			, intSort
+			, intConcurrencyId)
+SELECT @intNewCountId
+		, intItemId
+		, intItemLocationId
+		, intSubLocationId
+		, intStorageLocationId
+		, intCountGroupId
+		, intLotId
+		, strLotNo
+		, strLotAlias
+		, intParentLotId
+		, strParentLotNo
+		, strParentLotAlias
+		, intStockUOMId
+		, dblSystemCount
+		, dblLastCost
+		, strAutoCreatedLotNumber
+		, strCountLine
+		, dblPallets
+		, dblQtyPerPallet
+		, dblPhysicalCount
+		, intItemUOMId
+		, intWeightUOMId
+		, dblWeightQty
+		, dblNetQty
+		, ysnRecount = 0
+		, dblQtyReceived
+		, dblQtySold
+		, intEntityUserSecurityId
+		, ysnFetched
+		, intSort
+		, intConcurrencyId
+FROM tblICInventoryCountDetail
+WHERE ysnRecount = 1
+	AND intInventoryCountId = @intReferenceCountId
