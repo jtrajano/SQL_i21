1b6cf13340a02a2e39e85bc238f9fb913aaa7878
Smith Wilson de Jesus <smith.dejesus@irely.com>
2018-04-19 18:41:12 +0800

    RM-1289

--- i21Database/dbo/Views/vyuRKGetBrokerTrader.sql
+++ i21Database/dbo/Views/vyuRKGetBrokerTrader.sql
@@ -3,9 +3,7 @@ AS
 SELECT 
 	e.intEntityId intEntitySalespersonId
 	,strName strSalespersonId
-	,strName strSalespersonName 
-	,bt.intBrokerageAccountId
+	,strName strSalespersonName
 from tblEMEntity e 
 JOIN tblEMEntityType et on e.intEntityId=et.intEntityId
-JOIN tblRKTradersbyBrokersAccountMapping bt on e.intEntityId = bt.intEntitySalespersonId
  where strType='Salesperson'
