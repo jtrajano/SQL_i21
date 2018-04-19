48210358ff08cbd8fecd227afca58608739a13ed
Satya S Sahoo <satya.sahoo@irely.com>
2018-04-19 21:54:15 +0530

    CT-2059 - Book vs Entities Master - 18.3Dev

--- i21Database/dbo/Views/vyuCTContStsMain.sql
+++ i21Database/dbo/Views/vyuCTContStsMain.sql
@@ -10,17 +10,20 @@ AS
 			ISNULL(MA.strFutMarketName  + ' - ','') +
 			ISNULL(MO.strFutureMonth  + ' - ','') +
 			ISNULL(CB.strContractBasis  + ' - ','') +
+			ISNULL(BK.strBook + ISNULL(' ('+SB.strSubBook+')','')  + ' - ','') +
 			ISNULL(EY.strName,'') +
 			'('+SS.strContractStatus+')'
 			strInfo
 
 	FROM	tblCTContractDetail CD
-	JOIN	tblCTContractHeader	CH	ON	CH.intContractHeaderId	=	CD.intContractHeaderId
-	JOIN	tblCTContractStatus	SS	ON	SS.intContractStatusId	=	CD.intContractStatusId	LEFT
-	JOIN	tblICItem			IM	ON	IM.intItemId			=	CD.intItemId			LEFT
-	JOIN	tblRKFutureMarket	MA	ON	MA.intFutureMarketId	=	CD.intFutureMarketId	LEFT
-	JOIN	tblRKFuturesMonth	MO	ON	MO.intFutureMonthId		=	CD.intFutureMonthId		LEFT
-	JOIN	tblCTContractBasis	CB	ON	CB.intContractBasisId	=	CH.intContractBasisId	LEFT
-	JOIN	tblEMEntity			EY	ON	EY.intEntityId			=	CH.intEntityId
+			JOIN	tblCTContractHeader	CH	ON	CH.intContractHeaderId	=	CD.intContractHeaderId
+			JOIN	tblCTContractStatus	SS	ON	SS.intContractStatusId	=	CD.intContractStatusId	
+	LEFT	JOIN	tblICItem			IM	ON	IM.intItemId			=	CD.intItemId			
+	LEFT	JOIN	tblRKFutureMarket	MA	ON	MA.intFutureMarketId	=	CD.intFutureMarketId	
+	LEFT	JOIN	tblRKFuturesMonth	MO	ON	MO.intFutureMonthId		=	CD.intFutureMonthId		
+	LEFT	JOIN	tblCTContractBasis	CB	ON	CB.intContractBasisId	=	CH.intContractBasisId	
+	LEFT	JOIN	tblEMEntity			EY	ON	EY.intEntityId			=	CH.intEntityId
+	LEFT	JOIN	tblCTBook			BK	ON	BK.intBookId			=	CD.intBookId						
+	LEFT	JOIN	tblCTSubBook		SB	ON	SB.intSubBookId			=	CD.intSubBookId	
 
 	
