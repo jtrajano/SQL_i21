87e620fb80bb47bbfb4a438f2c631bd648a0874e
Crisman Lazaga <cris.lazaga@irely.com>
2018-04-20 16:57:44 +0800

    RPT-1226

--- i21Database/dbo/Tables/tblSRDirectFilter.sql
+++ i21Database/dbo/Tables/tblSRDirectFilter.sql
@@ -3,5 +3,6 @@
 	[intDirectFilterId] [int] NOT NULL IDENTITY(1, 1),
 	[strFilter] [nvarchar] (max) COLLATE Latin1_General_CI_AS NULL,
 	[intConcurrencyId] INT NOT NULL DEFAULT 1,
-	CONSTRAINT [PK_tblSRDirectFilter] PRIMARY KEY CLUSTERED ([intDirectFilterId] ASC)
+	[dtmDate] DATETIME NOT NULL, 
+    CONSTRAINT [PK_tblSRDirectFilter] PRIMARY KEY CLUSTERED ([intDirectFilterId] ASC)
 )
