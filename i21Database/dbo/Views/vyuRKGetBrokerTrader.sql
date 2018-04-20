e31acf679fec7f9397c0b76294a4dacc0f957e18
Smith Wilson de Jesus <smith.dejesus@irely.com>
2018-04-20 10:03:24 +0800

    Revert "RM-1289"
    This reverts commit 1b6cf13340a02a2e39e85bc238f9fb913aaa7878.

--- i21Database/dbo/Views/vyuRKGetBrokerTrader.sql
+++ i21Database/dbo/Views/vyuRKGetBrokerTrader.sql
@@ -3,7 +3,9 @@ AS
 SELECT 
 	e.intEntityId intEntitySalespersonId
 	,strName strSalespersonId
-	,strName strSalespersonName
+	,strName strSalespersonName 
+	,bt.intBrokerageAccountId
 from tblEMEntity e 
 JOIN tblEMEntityType et on e.intEntityId=et.intEntityId
+JOIN tblRKTradersbyBrokersAccountMapping bt on e.intEntityId = bt.intEntitySalespersonId
  where strType='Salesperson'
