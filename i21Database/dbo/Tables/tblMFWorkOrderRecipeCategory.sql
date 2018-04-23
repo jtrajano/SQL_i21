b9e0a532ff22a34a516f7726f5da6a8a531a134e
Chakra <chakradhar.behera@irely.com>
2018-04-23 15:37:07 +0530

    MFG-3451

---
+++ i21Database/dbo/Tables/tblMFWorkOrderRecipeCategory.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[tblMFWorkOrderRecipeCategory]
+(
+	[intWorkOrderId] INT NOT NULL,
+	[intRecipeCategoryId] INT NOT NULL,
+	[intRecipeId] INT NOT NULL,
+	[intCategoryId] INT NOT NULL,
+	[intRecipeItemTypeId] INT NOT NULL,
+	CONSTRAINT [PK_tblMFWorkOrderRecipeCategory_intRecipeCategoryId] PRIMARY KEY ([intRecipeCategoryId],[intWorkOrderId]),
+	CONSTRAINT [FK_tblMFWorkOrderRecipeCategory_tblMFWorkOrder_intWorkOrderId] FOREIGN KEY ([intWorkOrderId]) REFERENCES [tblMFWorkOrder]([intWorkOrderId]) ON DELETE CASCADE,
+	CONSTRAINT [FK_tblMFWorkOrderRecipeCategory_tblMFRecipe_intRecipeId] FOREIGN KEY ([intRecipeId]) REFERENCES [tblMFRecipe]([intRecipeId]), 
+	CONSTRAINT [FK_tblMFWorkOrderRecipeCategory_tblICCategory_intCategoryId] FOREIGN KEY ([intCategoryId]) REFERENCES [tblICCategory]([intCategoryId]),
+	CONSTRAINT [FK_tblMFWorkOrderRecipeCategory_tblMFRecipeItemType_intRecipeItemTypeId] FOREIGN KEY ([intRecipeItemTypeId]) REFERENCES [tblMFRecipeItemType]([intRecipeItemTypeId])
+)
