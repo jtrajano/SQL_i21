cdf766b03ae7bb34b41dd7083efea821ce0afeb2
Kenneth Lew Lagunero <kenneth.lagunero@irely.com>
2018-04-24 02:20:54 +0800

    CF-1550

---
+++ i21Database/dbo/Tables/tblCFNetworkCost.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[tblCFNetworkCost](
+	[intNetworkCost] [int] NOT NULL,
+	[intSiteId] [int] NULL,
+	[dtmDate] [datetime] NULL,
+	[intItemId] [int] NULL,
+	[dblTransferCost] [numeric](18, 6) NULL,
+	[dblTaxesPerUnit] [numeric](18, 6) NULL,
+	CONSTRAINT [PK_tblCFNetworkCost] PRIMARY KEY CLUSTERED ([intNetworkCost] ASC),
+ );
+GO
