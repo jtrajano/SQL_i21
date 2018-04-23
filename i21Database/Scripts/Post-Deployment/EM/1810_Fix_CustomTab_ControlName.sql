968e6d62ca20a969dc1daad1a7fe3dc3bc814e93
jessa delo-ag <jessa.delo-ag@irely.com>
2018-04-23 17:33:03 +0800

    FRM-7169

---
+++ i21Database/Scripts/Post-Deployment/EM/1810_Fix_CustomTab_ControlName.sql
@@ -0,0 +1,12 @@
+﻿GO
+	PRINT N'START UPDATE CUSTOMTAB DETAIL'
+
+	BEGIN
+		UPDATE tblSMCustomTabDetail
+		SET strControlName = REPLACE(strControlName,'#','Number'),
+			strFieldName = REPLACE(strFieldName,'#','Number')
+	END
+
+	PRINT N'END UPDATE CUSTOMTAB DETAIL'
+
+GO
