396c03828f4e0c02274353888d4573bd9815c135
COMTECH\BPonce <bonn.ponce@irely.com>
2018-04-27 10:34:03 +0800

    CF-1560 CF-1561

---
+++ i21Database/dbo/Stored Procedures/uspCFGenerateQuoting.sql
@@ -0,0 +1,285 @@
+﻿CREATE PROCEDURE [dbo].[uspCFGenerateQuoting](
+	 @intCustomerId				NVARCHAR(MAX)  
+	,@intSiteId					NVARCHAR(MAX)  = NULL
+	,@intProductId				NVARCHAR(MAX)  = NULL 
+	,@dtmDate					DATETIME
+	,@strCity					NVARCHAR(MAX)  = ''	
+	,@strState					NVARCHAR(MAX)  = ''	
+)
+AS
+BEGIN
+
+	DELETE FROM tblCFCSRSingleQuote
+	DELETE FROM tblCFCSRSingleQuoteDetailTax
+
+	
+DECLARE @tblNetworkSiteItem TABLE (
+		 intNetworkId		   INT
+		,strNetwork			   NVARCHAR(max)
+		,intSiteId			   INT
+		,strSiteNumber		   NVARCHAR(max)
+		,strSiteName		   NVARCHAR(max)
+		,strSiteType		   NVARCHAR(max)
+		,strSiteAddress		   NVARCHAR(max)
+		,strSiteCity		   NVARCHAR(max)
+		,strTaxState		   NVARCHAR(max)
+		,intItemId			   INT
+		,intARItemId		   INT
+		,strProductNumber	   NVARCHAR(max)
+		,strItemNo			   NVARCHAR(max)
+		,strDescription		   NVARCHAR(max)
+	)
+
+	DECLARE @where NVARCHAR(MAX) 
+	DECLARE @firstAppend BIT	= 0
+
+	--IF OBJECT_ID('tempdb..@tblNetworkSiteItem') IS NOT NULL
+ --   begin
+ --           drop table @tblNetworkSiteItem
+ --   end
+	SET @where = 'WHERE '
+	IF(ISNULL(@intProductId,0) != 0)
+	BEGIN
+		IF(@firstAppend = 1)
+		BEGIN
+			SET @where += ' AND cfItem.intARItemId = ' + @intProductId
+		END
+		ELSE
+		BEGIN
+			SET @firstAppend = 1
+			SET @where += 'cfItem.intARItemId = ' + @intProductId
+		END
+	END
+	
+	IF(ISNULL(@intSiteId,0) != 0)
+	BEGIN
+		IF(@firstAppend = 1)
+		BEGIN
+			SET @where += ' AND cfSite.intSiteId = ' + @intSiteId
+		END
+		ELSE
+		BEGIN
+			SET @firstAppend = 1
+			SET @where += 'cfSite.intSiteId = ' + @intSiteId
+		END
+	END
+	ELSE
+	BEGIN
+		IF(ISNULL(@strCity,'') != '')
+		BEGIN
+			IF(@firstAppend = 1)
+			BEGIN
+				SET @where += ' AND cfSite.strSiteCity = ' + '''' +  @strCity + ''''
+			END
+			ELSE
+			BEGIN
+				SET @firstAppend = 1
+				SET @where += 'cfSite.strSiteCity = ' + '''' + @strCity + ''''
+			END
+		END
+
+		IF(ISNULL(@strState,'') != '')
+		BEGIN
+			IF(@firstAppend = 1)
+			BEGIN
+				SET @where += ' AND cfSite.strTaxState = ' + '''' + @strState + ''''
+			END
+			ELSE
+			BEGIN
+				SET @firstAppend = 1
+				SET @where += 'cfSite.strTaxState = ' + '''' + @strState + ''''
+			END
+		END
+
+	END
+
+	DECLARE @q  NVARCHAR(MAX) = ''
+	SET @q = 'SELECT
+	cfNetwork.intNetworkId, 
+	strNetwork , 
+	cfSite.intSiteId, 
+	strSiteNumber ,
+	strSiteName, 
+	cfSite.strSiteType, 
+	cfSite.strSiteAddress, 
+	cfSite.strSiteCity,
+	cfSite.strTaxState,
+	cfItem.intItemId,
+	cfItem.intARItemId,
+	ISNULL(icItem.strItemNo,'''') + '' - '' + ISNULL(icItem.strDescription,'''') as strProductNumber,
+	icItem.strItemNo, 
+	icItem.strDescription
+	FROM tblCFNetwork as cfNetwork 
+	INNER JOIN tblCFSite as cfSite ON cfNetwork.intNetworkId = cfSite.intNetworkId
+	INNER JOIN tblCFItem as cfItem ON cfNetwork.intNetworkId = cfItem.intNetworkId
+	INNER JOIN tblICItem as icItem ON cfItem.intARItemId = icItem.intItemId
+	INNER JOIN tblCFItemCategory as cfItemCat ON icItem.intCategoryId = cfItemCat.intCategoryId' + ' ' + @where
+
+	INSERT INTO @tblNetworkSiteItem
+	EXEC(@q)
+
+	DECLARE @loopNetworkId	INT	
+	DECLARE @loopSiteId		INT
+	DECLARE @loopItemId		INT
+	DECLARE @loopARItemId	INT
+	DECLARE	@loopSiteType	NVARCHAR(MAX)		
+	DECLARE @networkCost	NUMERIC(18,6)
+	DECLARE @pk				INT
+
+	
+
+	WHILE (EXISTS(SELECT 1 FROM @tblNetworkSiteItem))
+	BEGIN
+		SELECT 
+		 @loopNetworkId			= 	 intNetworkId	
+		,@loopSiteId			= 	 intSiteId	
+		,@loopItemId			= 	 intItemId	
+		,@loopARItemId			= 	 intARItemId	
+		,@loopSiteType			=	 strSiteType
+		FROM @tblNetworkSiteItem
+
+
+		SET @networkCost = (SELECT TOP 1 ISNULL(dblTransferCost,0) FROM tblCFNetworkCost 
+		WHERE intNetworkId = @loopNetworkId 
+		AND intSiteId = @loopSiteId
+		AND intItemId = @loopARItemId
+		AND CONVERT( varchar, dtmDate, 101) = CONVERT( varchar, @dtmDate, 101)
+		)
+		
+		EXEC dbo.uspCFRecalculateTransaciton 
+		@CustomerId = @intCustomerId,
+		@ProductId=@loopItemId,
+		@SiteId=@loopSiteId,
+		@NetworkId=@loopNetworkId,
+		@TransactionDate=@dtmDate,
+		@TransactionType=N'Local/Network',
+		---------STATIC VALUE----------
+		@TransactionId=0,
+		@CreditCardUsed=0,
+		@PumpId=0,
+		@VehicleId=0,
+		@CardId=0,
+		@Quantity=1,
+		@OriginalPrice=@networkCost, -- NETWORK COST
+		@TransferCost=@networkCost,	-- NETWORK COST
+		@IsImporting = 1
+
+
+		INSERT INTO tblCFCSRSingleQuote(
+			 intNetworkId
+			,intSiteId
+			,strAddress
+			,strCity
+			,strState
+			,intItem
+			,dblUnitCost
+			,dblProfileRate
+			,dblAdjRate
+			,dblNetPrice
+			,dblTaxes
+			,dblGrossPrice
+			,strNetwork
+			,strSite
+			,strItem
+		)
+		SELECT 
+		 intNetworkId
+		,intSiteId
+		,strSiteAddress
+		,strSiteCity
+		,strTaxState
+		,intARItemId
+		,@networkCost
+		,0
+		,0
+		,0
+		,0
+		,0
+		,strNetwork
+		,strSiteNumber + ' - ' + strSiteName
+		,strProductNumber
+		FROM @tblNetworkSiteItem
+		WHERE 
+		intNetworkId	 = @loopNetworkId	
+		AND intSiteId	 = @loopSiteId	
+		AND intItemId	 = @loopItemId	
+		AND intARItemId	 = @loopARItemId	
+		AND strSiteType	 = @loopSiteType	
+
+
+		SET @pk = SCOPE_IDENTITY()
+
+		UPDATE tblCFCSRSingleQuote
+		SET
+		 dblProfileRate = (SELECT TOP 1 ISNULL(dblPriceProfileRate,0)		FROM ##tblCFTransactionPricingType)
+		,dblAdjRate		= (SELECT TOP 1 ISNULL(dblAdjustmentRate,0)			FROM ##tblCFTransactionPricingType)
+		,dblNetPrice	= (SELECT TOP 1 ISNULL(dblTaxCalculatedAmount,0)	FROM ##tblCFTransactionPriceType WHERE strTransactionPriceId = 'Net Price')
+		,dblGrossPrice	= (SELECT TOP 1 ISNULL(dblTaxCalculatedAmount,0)	FROM ##tblCFTransactionPriceType WHERE strTransactionPriceId = 'Gross Price')
+		,dblTaxes		= (SELECT	ISNULL(SUM(dblTaxCalculatedAmount),0)	FROM ##tblCFTransactionTaxType)
+		WHERE 
+		intCSRSingleQuoteId = @pk
+		
+
+		INSERT INTO tblCFCSRSingleQuoteDetailTax
+		(
+			 intCSRSingleQuoteId
+			,intTaxGroupId
+			,intTaxCodeId
+			,strCalculationMethod
+			,dblRate
+			,dblTax
+			,strTaxCode
+			,strTaxGroup
+			,dblAdjustedTax
+		)
+		SELECT
+		 @pk
+		,intTaxGroupId
+		,intTaxCodeId
+		,strCalculationMethod
+		,dblTaxRate
+		,dblTaxCalculatedAmount
+		,strTaxCode
+		,strTaxGroup
+		,dblTaxCalculatedAmount
+		FROM ##tblCFTransactionTaxType
+
+
+		IF OBJECT_ID('tempdb..##tblCFTransactionTaxType') IS NOT NULL
+		begin
+				drop table ##tblCFTransactionTaxType
+		end
+
+		IF OBJECT_ID('tempdb..##tblCFTransactionPriceType') IS NOT NULL
+		begin
+				drop table ##tblCFTransactionPriceType
+		end
+
+		IF OBJECT_ID('tempdb..##tblCFTransactionPricingType') IS NOT NULL
+		begin
+				drop table ##tblCFTransactionPricingType
+		end
+
+		PRINT 'S------------------'
+		PRINT @loopNetworkId
+		PRINT @loopSiteId	
+		PRINT @loopItemId	
+		PRINT @loopARItemId	
+		PRINT 'E------------------'
+
+		DELETE FROM  @tblNetworkSiteItem
+		WHERE 
+			intNetworkId		= @loopNetworkId		
+			AND intSiteId		= @loopSiteId			
+			AND intItemId		= @loopItemId			
+			AND intARItemId		= @loopARItemId		
+			AND strSiteType		 = @loopSiteType		
+		
+
+		
+
+	END
+
+
+	
+END
--- i21Database/dbo/Tables/tblCFAccountQuote.sql
+++ i21Database/dbo/Tables/tblCFAccountQuote.sql
@@ -1,15 +1,21 @@
-﻿CREATE TABLE [dbo].[tblCFAccountQuote](
-	[intAccountQuoteId] [int] IDENTITY(1,1) NOT NULL,
-	[intCustomerGroupId] [int] NOT NULL,
-	[inAccountId] INT NOT NULL, 
-	[intQuoteProduct1] [int] NULL,
-	[intQuoteProduct2] [int] NULL,
-	[intQuoteProduct3] [int] NULL,
-	[intQuoteProduct4] [int] NULL,
-	[intQuoteProduct5] [int] NULL,
-	[ysnTaxExempt] [bit] CONSTRAINT [DF_tblCFAccountQuote_ysnTaxExempt] DEFAULT ((0)) NOT NULL,
-	[intConcurrencyId] [int]  CONSTRAINT [DF_tblCFAccountQuote_intConcurrencyId] DEFAULT ((1)) NOT NULL,
-    CONSTRAINT [PK_tblCFAccountQuote] PRIMARY KEY CLUSTERED ([intAccountQuoteId] ASC),
-
+﻿CREATE TABLE [dbo].[tblCFAccountQuote] (
+    [intAccountQuoteId] INT            IDENTITY (1, 1) NOT NULL,
+    [intSiteId]         INT            NULL,
+    [strSite]           NVARCHAR (50)  NULL,
+    [strAddress]        NVARCHAR (500) NULL,
+    [strCity]           NVARCHAR (50)  NULL,
+    [strState]          NVARCHAR (50)  NULL,
+    [intItem1]          INT            NULL,
+    [strItem1]          NVARCHAR (50)  NULL,
+    [intItem2]          INT            NULL,
+    [strItem2]          NVARCHAR (50)  NULL,
+    [intItem3]          INT            NULL,
+    [strItem3]          NVARCHAR (50)  NULL,
+    [intItem4]          INT            NULL,
+    [strItem4]          NVARCHAR (50)  NULL,
+    [intItem5]          INT            NULL,
+    [strItem5]          NVARCHAR (50)  NULL,
+    [intConcurrencyId]  INT            CONSTRAINT [DF_tblCFAccountQuote_intConcurrencyId] DEFAULT ((1)) NULL,
+    CONSTRAINT [PK_tblCFAccountQuote] PRIMARY KEY CLUSTERED ([intAccountQuoteId] ASC)
 );
-GO
+
