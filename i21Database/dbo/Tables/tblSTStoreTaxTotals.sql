704e15f9abb4f986a10ae0921688f4dda268df65
COMTECH\HZaragoza <henry.zaragoza@irely.com>
2018-04-30 17:33:46 +0800

    ST-699 [Store] Add New Field for Item Id to Taxes

---
+++ i21Database/dbo/Tables/tblSTStoreTaxTotals.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[tblSTStoreTaxTotals]
+(
+	[intStoreTaxTotalId] INT NOT NULL IDENTITY, 
+	[intStoreId] INT NOT NULL, 
+	[intTaxCodeId] INT NOT NULL,
+    [intItemId] INT NOT NULL, 
+    [intConcurrencyId] INT NOT NULL,
+	CONSTRAINT [PK_tblSTStoreTaxTotals] PRIMARY KEY ([intStoreTaxTotalId]),
+	CONSTRAINT [FK_tblSTStoreTaxTotals_tblSTStore] FOREIGN KEY ([intStoreId]) REFERENCES [tblSTStore]([intStoreId]),
+	CONSTRAINT [FK_tblSTStoreTaxTotals_tblICItem_intItemId] FOREIGN KEY ([intItemId]) REFERENCES [dbo].[tblICItem] ([intItemId]),
+	CONSTRAINT [FK_tblSTStoreTaxTotals_tblSMTaxCode_intTaxCodeId] FOREIGN KEY ([intTaxCodeId]) REFERENCES [dbo].[tblSMTaxCode] ([intTaxCodeId])
+)
