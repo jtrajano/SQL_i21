8b96dc94f25e05cba98d6630a49e9c8ae9329e99
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2018-04-30 15:54:39 +0530

    RM-1331

---
+++ i21Database/dbo/Stored Procedures/uspRKDPRContractDetail.sql
@@ -0,0 +1,191 @@
+﻿CREATE PROC uspRKDPRContractDetail
+	@intCommodityId int,
+	@dtmToDate datetime=null
+
+AS
+
+SELECT * FROM (
+  SELECT ROW_NUMBER() OVER (PARTITION BY intContractDetailId ORDER BY dtmHistoryCreated DESC) intRowNum, 
+		strCommodity strCommodityCode,
+		h.intCommodityId intCommodityId,
+		intContractHeaderId,
+	     strContractNumber +'-' +Convert(nvarchar,intContractSeq)  strContractNumber
+		,strLocation strLocationName,
+		dtmEndDate,
+		dblQuantity - isnull(dblQuantity-dblBalance,0)  dblBalance,
+		intDtlQtyUnitMeasureId intUnitMeasureId
+		,intPricingTypeId,
+		intContractTypeId
+		,intCompanyLocationId
+		,strContractType 
+		,strPricingType
+		,intDtlQtyInCommodityUOMId intCommodityUnitMeasureId,
+		intContractDetailId,
+		intContractStatusId,
+		e.intEntityId intEntityId
+		,intCurrencyId
+		,strContractType+' Priced' AS strType	
+		,i.intItemId intItemId
+		,strItemNo,getdate() dtmContractDate,e.strName strEntityName,'' strCustomerContract
+	FROM    tblCTSequenceHistory h
+	JOIN tblICItem i on h.intItemId=i.intItemId
+	JOIN tblEMEntity e on e.intEntityId=h.intEntityId 
+    WHERE intPricingTypeId=1 and dtmHistoryCreated <= @dtmToDate and h.intCommodityId=@intCommodityId
+		  and intContractStatusId  not in(2,3,6)
+	) a where a.intRowNum =1 
+
+    UNION ALL
+
+   select * from (
+  SELECT ROW_NUMBER() OVER (PARTITION BY intContractDetailId ORDER BY dtmHistoryCreated DESC) intRowNum, 
+		strCommodity strCommodityCode,
+		h.intCommodityId intCommodityId,
+		intContractHeaderId,
+	     strContractNumber +'-' +Convert(nvarchar,intContractSeq)  strContractNumber
+		,strLocation strLocationName,
+		dtmEndDate,
+		dblQtyUnpriced dblBalance,  -- wrong need to check
+		intDtlQtyUnitMeasureId intUnitMeasureId
+		,intPricingTypeId,
+		intContractTypeId
+		,intCompanyLocationId
+		,strContractType 
+		,strPricingType
+		,intDtlQtyInCommodityUOMId intCommodityUnitMeasureId,
+		intContractDetailId,
+		intContractStatusId,
+		e.intEntityId intEntityId
+		,intCurrencyId
+		,strContractType+' Basis' AS strType
+		,i.intItemId intItemId
+		,strItemNo,getdate() dtmContractDate,e.strName strEntityName,'' strCustomerContract
+	FROM    tblCTSequenceHistory h
+	JOIN tblICItem i on h.intItemId=i.intItemId
+	JOIN tblEMEntity e on e.intEntityId=h.intEntityId
+    WHERE  dblQuantity >= (dblQuantity-dblBalance) and dtmHistoryCreated <= @dtmToDate and h.intCommodityId=@intCommodityId
+	and intContractStatusId  not in(2,3,6)
+	) a where a.intRowNum =1 
+
+ UNION ALL
+	  SELECT * FROM (
+  SELECT ROW_NUMBER() OVER (PARTITION BY intContractDetailId ORDER BY dtmHistoryCreated DESC) intRowNum, 
+		strCommodity strCommodityCode,
+		h.intCommodityId intCommodityId,
+		intContractHeaderId,
+	     strContractNumber +'-' +Convert(nvarchar,intContractSeq)  strContractNumber
+		,strLocation strLocationName,
+		dtmEndDate,
+		dblQtyPriced-(dblQuantity-dblBalance)  dblBalance,  -- wrong need to check
+		intDtlQtyUnitMeasureId intUnitMeasureId
+		,intPricingTypeId,
+		intContractTypeId
+		,intCompanyLocationId
+		,strContractType 
+		,strPricingType
+		,intDtlQtyInCommodityUOMId intCommodityUnitMeasureId,
+		intContractDetailId,
+		intContractStatusId,
+		e.intEntityId intEntityId
+		,intCurrencyId
+		,strContractType+' Priced' AS strType
+		,i.intItemId intItemId
+		,strItemNo,getdate() dtmContractDate,e.strName strEntityName,'' strCustomerContract
+	FROM    tblCTSequenceHistory h
+	JOIN tblICItem i on h.intItemId=i.intItemId
+	JOIN tblEMEntity e on e.intEntityId=h.intEntityId
+    WHERE  dblQuantity > (dblQuantity-dblBalance) and dtmHistoryCreated <= @dtmToDate and h.intCommodityId=@intCommodityId
+	and intContractStatusId  not in(2,3,6)
+	) a where a.intRowNum =1 
+
+ UNION ALL
+	  SELECT * FROM (
+  SELECT ROW_NUMBER() OVER (PARTITION BY intContractDetailId ORDER BY dtmHistoryCreated DESC) intRowNum, 
+		strCommodity strCommodityCode,
+		h.intCommodityId intCommodityId,
+		intContractHeaderId,
+	     strContractNumber +'-' +Convert(nvarchar,intContractSeq)  strContractNumber
+		,strLocation strLocationName,
+		dtmEndDate,
+		dblBalance dblBalance,  -- wrong need to check
+		intDtlQtyUnitMeasureId intUnitMeasureId
+		,intPricingTypeId,
+		intContractTypeId
+		,intCompanyLocationId
+		,strContractType 
+		,strPricingType
+		,intDtlQtyInCommodityUOMId intCommodityUnitMeasureId,
+		intContractDetailId,
+		intContractStatusId,
+		e.intEntityId intEntityId
+		,intCurrencyId
+		,strContractType+' Basis' AS strType
+		,i.intItemId intItemId
+		,strItemNo,getdate() dtmContractDate,e.strName strEntityName,'' strCustomerContract
+	FROM    tblCTSequenceHistory h
+	JOIN tblICItem i on h.intItemId=i.intItemId
+	JOIN tblEMEntity e on e.intEntityId=h.intEntityId
+    WHERE  dblQuantity < (dblQuantity-dblBalance) and dtmHistoryCreated <= @dtmToDate and h.intCommodityId=@intCommodityId
+	and intContractStatusId  not in(2,3,6)
+	) a where a.intRowNum =1 
+
+ UNION ALL
+	  SELECT * FROM (
+  SELECT ROW_NUMBER() OVER (PARTITION BY intContractDetailId ORDER BY dtmHistoryCreated DESC) intRowNum, 
+		strCommodity strCommodityCode,
+		h.intCommodityId intCommodityId,
+		intContractHeaderId,
+	     strContractNumber +'-' +Convert(nvarchar,intContractSeq)  strContractNumber
+		,strLocation strLocationName,
+		dtmEndDate,
+		dblBalance  dblBalance,  
+		intDtlQtyUnitMeasureId intUnitMeasureId
+		,intPricingTypeId,
+		intContractTypeId
+		,intCompanyLocationId
+		,strContractType 
+		,strPricingType
+		,intDtlQtyInCommodityUOMId intCommodityUnitMeasureId,
+		intContractDetailId,
+		intContractStatusId,
+		e.intEntityId intEntityId
+		,intCurrencyId
+		,case when intPricingTypeId=1 then strContractType+' Priced'  else  strContractType+' Basis' end AS strType
+		,i.intItemId intItemId
+		,strItemNo,getdate() dtmContractDate,e.strName strEntityName,'' strCustomerContract
+	FROM    tblCTSequenceHistory h
+	JOIN tblICItem i on h.intItemId=i.intItemId
+	JOIN tblEMEntity e on e.intEntityId=h.intEntityId
+        WHERE   intContractDetailId NOT IN (SELECT intContractDetailId FROM tblCTPriceFixation) and dtmHistoryCreated <= @dtmToDate and h.intCommodityId=@intCommodityId
+		and intContractStatusId  not in(2,3,6)
+	) a where a.intRowNum =1 
+
+	UNION ALL
+  SELECT * FROM (
+  SELECT ROW_NUMBER() OVER (PARTITION BY intContractDetailId ORDER BY dtmHistoryCreated DESC) intRowNum, 
+		strCommodity strCommodityCode,
+		h.intCommodityId intCommodityId,
+		intContractHeaderId,
+	     strContractNumber +'-' +Convert(nvarchar,intContractSeq)  strContractNumber
+		,strLocation strLocationName,
+		dtmEndDate,
+		dblBalance  dblBalance,  
+		intDtlQtyUnitMeasureId intUnitMeasureId
+		,intPricingTypeId,
+		intContractTypeId
+		,intCompanyLocationId
+		,strContractType 
+		,strPricingType
+		,intDtlQtyInCommodityUOMId intCommodityUnitMeasureId,
+		intContractDetailId,
+		intContractStatusId,
+		e.intEntityId intEntityId
+		,intCurrencyId
+		,CASE WHEN intPricingTypeId = 3 THEN strContractType+'HTA' END AS strType
+		,i.intItemId intItemId
+		,strItemNo,getdate() dtmContractDate,e.strName strEntityName,'' strCustomerContract
+	FROM    tblCTSequenceHistory h
+	JOIN tblICItem i on h.intItemId=i.intItemId
+	JOIN tblEMEntity e on e.intEntityId=h.intEntityId
+        WHERE   intContractDetailId NOT IN (SELECT intContractDetailId FROM tblCTPriceFixation) and dtmHistoryCreated <= @dtmToDate and h.intCommodityId=@intCommodityId
+		and intContractStatusId  not in(2,3,6)
+	) a WHERE a.intRowNum =1 
