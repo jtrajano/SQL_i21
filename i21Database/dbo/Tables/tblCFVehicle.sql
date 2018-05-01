20068f3e0a73dd8107395e47ec98406c0823286b
Kenneth Lew Lagunero <kenneth.lagunero@irely.com>
2018-05-02 05:54:49 +0800

    CF-1055

--- i21Database/dbo/Tables/tblCFVehicle.sql
+++ i21Database/dbo/Tables/tblCFVehicle.sql
@@ -24,6 +24,7 @@
     [ysnCardForOwnUse]        BIT            NULL,
     [ysnActive]				  BIT			 CONSTRAINT [DF_tblCFVehicle_ysnActive]  DEFAULT ((1)) NOT NULL,
     [intDepartmentId] INT NULL, 
+	[strComment]     NVARCHAR (250) COLLATE Latin1_General_CI_AS NULL,
     CONSTRAINT [PK_tblCFVehicle] PRIMARY KEY CLUSTERED ([intVehicleId] ASC),
     CONSTRAINT [FK_tblCFVehicle_tblCFAccount] FOREIGN KEY ([intAccountId]) REFERENCES [dbo].[tblCFAccount] ([intAccountId]) ON DELETE CASCADE,
     CONSTRAINT [FK_tblCFVehicle_tblICItem] FOREIGN KEY ([intExpenseItemId]) REFERENCES [dbo].[tblICItem] ([intItemId])
