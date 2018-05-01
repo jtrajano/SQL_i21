2fa89a0a29a5a3b9a5bac1c835ee41f840553403
Erick <erick.delacruz@irely.com>
2018-05-01 21:35:41 +0800

    AP-5416

--- i21Database/dbo/Stored Procedures/uspAPCreateBasisAdvance.sql
+++ i21Database/dbo/Stored Procedures/uspAPCreateBasisAdvance.sql
@@ -316,9 +316,52 @@ SELECT intBillId FROM #tmpVoucherCreated
 
 EXEC uspAPUpdateVoucherTotal @voucherIds
 
+--DELETE STAGING
 DELETE A
 FROM tblAPBasisAdvanceStaging A
 INNER JOIN #tmpVoucherCreated B ON A.intContractDetailId = B.intContractDetailId AND B.intTicketId = A.intTicketId
+
+--tblAPBasisAdvanceFuture
+DELETE A
+FROM tblAPBasisAdvanceFuture A
+OUTER APPLY (
+	SELECT
+		1 AS ysnSelected
+	FROM tblAPBasisAdvanceStaging staging
+	INNER JOIN vyuAPBasisAdvance basisAdvance 
+		ON staging.intTicketId = basisAdvance.intTicketId 
+			AND staging.intContractDetailId = basisAdvance.intContractDetailId
+	WHERE basisAdvance.intFutureMarketId = A.intFutureMarketId AND basisAdvance.intFutureMonthId = A.intMonthId
+) ticketSelected
+WHERE ticketSelected.ysnSelected IS NULL
+
+--DELETE DUPLICATE in tblAPBasisAdvanceFuture
+DELETE A
+FROM tblAPBasisAdvanceFuture A
+WHERE A.intBasisAdvanceFuturesId IN (
+	SELECT intBasisAdvanceFuturesId
+	FROM (
+		SELECT 
+			intBasisAdvanceFuturesId
+			,ROW_NUMBER() OVER(PARTITION BY intFutureMarketId, intMonthId ORDER BY intBasisAdvanceFuturesId) AS intCount
+		FROM tblAPBasisAdvanceFuture
+	) duplicateFutures
+	WHERE intCount != 1
+)
+
+--tblAPBasisAdvanceCommodity
+DELETE A
+FROM tblAPBasisAdvanceCommodity A
+OUTER APPLY (
+	SELECT
+		1 AS ysnSelected
+	FROM tblAPBasisAdvanceStaging staging
+	INNER JOIN vyuAPBasisAdvance basisAdvance 
+		ON staging.intTicketId = basisAdvance.intTicketId 
+			AND staging.intContractDetailId = basisAdvance.intContractDetailId
+	WHERE basisAdvance.intCommodityId = A.intCommodityId
+) ticketSelected
+WHERE ticketSelected.ysnSelected IS NULL
 
 SELECT @createdBasisAdvance = COALESCE(@createdBasisAdvance + ',', '') +  CONVERT(VARCHAR(12),intBillId)
 FROM #tmpVoucherCreated
