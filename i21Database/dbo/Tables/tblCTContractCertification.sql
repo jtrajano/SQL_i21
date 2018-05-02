e17c4f7bc0a0888575acbcab65a541472fa369bd
Satya S Sahoo <satya.sahoo@irely.com>
2018-05-02 13:54:21 +0530

    CT-2087 - Allow user to enter quantity for each producer in contract sequence - 18.3Dev

--- i21Database/dbo/Tables/tblCTContractCertification.sql
+++ i21Database/dbo/Tables/tblCTContractCertification.sql
@@ -4,6 +4,7 @@
     [intContractDetailId]	INT  NOT NULL, 
     [intCertificationId]	INT  NULL, 
 	strCertificationId      NVARCHAR(50) COLLATE Latin1_General_CI_AS,
+	strTrackingNumber       NVARCHAR(50) COLLATE Latin1_General_CI_AS,
 	intProducerId			INT  NULL,
 	dblQuantity				NUMERIC(18,6),
     [intConcurrencyId]		INT NOT NULL,
