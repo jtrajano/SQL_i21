4ace500c3e0e43f8c0ae9f4575f706208dee69c0
COMTECH\HZaragoza <henry.zaragoza@irely.com>
2018-05-02 15:14:26 +0800

    ST-700 Add New Customer Charge Item in Store

--- i21Database/dbo/Tables/tblSTStore.sql
+++ i21Database/dbo/Tables/tblSTStore.sql
@@ -110,6 +110,8 @@
 	[intTaxGroupId] int NULL,
 	[intCheckoutCustomerId] int NULL,
 	[strDepartment] nvarchar(max) COLLATE Latin1_General_CI_AS NULL,
+	[intCustomerChargesItemId] int NULL,
+	[intCustomerPaymentItemId] int NULL,
     [intConcurrencyId] INT NOT NULL, 
     CONSTRAINT [PK_tblSTStore] PRIMARY KEY CLUSTERED ([intStoreId] ASC),
     CONSTRAINT [AK_tblSTStore_intStoreNo] UNIQUE NONCLUSTERED ([intStoreNo] ASC), 
@@ -126,5 +128,7 @@
 	CONSTRAINT [FK_tblSTStore_tblSTPaymentOption_intRemovePropCardMopId] FOREIGN KEY ([intRemovePropCardMopId]) REFERENCES [tblSTPaymentOption]([intPaymentOptionId]), 
 	CONSTRAINT [FK_tblSTStore_tblSTPaymentOption_intAddPropCardMopId] FOREIGN KEY ([intAddPropCardMopId]) REFERENCES [tblSTPaymentOption]([intPaymentOptionId]),
 	CONSTRAINT [FK_tblSTStore_tblSMTaxGroup_intTaxGroupId] FOREIGN KEY ([intTaxGroupId]) REFERENCES [dbo].[tblSMTaxGroup] ([intTaxGroupId]),
-	CONSTRAINT [FK_tblSTStore_tblEMEntity_intCheckoutCustomerId] FOREIGN KEY ([intCheckoutCustomerId]) REFERENCES [dbo].[tblEMEntity] ([intEntityId])
+	CONSTRAINT [FK_tblSTStore_tblEMEntity_intCheckoutCustomerId] FOREIGN KEY ([intCheckoutCustomerId]) REFERENCES [dbo].[tblEMEntity] ([intEntityId]),
+	CONSTRAINT [FK_tblSTStore_tblICItem_intCustomerChargesItemId] FOREIGN KEY ([intCustomerChargesItemId]) REFERENCES [tblICItem]([intItemId]),
+	CONSTRAINT [FK_tblSTStore_tblICItem_intCustomerPaymentItemId] FOREIGN KEY ([intCustomerPaymentItemId]) REFERENCES [tblICItem]([intItemId]),
    );
