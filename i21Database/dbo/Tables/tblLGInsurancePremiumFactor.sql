ddd6144ad14ded5adf8d3f8a8cebbd8c6392c18b
IRELY\swghosh <swetosuvro.g@irely.com>
2018-05-02 19:35:47 +0530

    LG-1312

--- i21Database/dbo/Tables/tblLGInsurancePremiumFactor.sql
+++ i21Database/dbo/Tables/tblLGInsurancePremiumFactor.sql
@@ -3,6 +3,7 @@
 	[intInsurancePremiumFactorId]  INT NOT NULL PRIMARY KEY IDENTITY (1, 1),
 	[intEntityId] INT NOT NULL,
 	[intConcurrencyId] INT NOT NULL, 
+	[dtmDate] DATETIME NULL,
 
 	CONSTRAINT [FK_tblLGInsurancePremiumFactor_tblEntities_intEntityId] FOREIGN KEY ([intEntityId]) REFERENCES [tblEMEntity]([intEntityId])
 )
