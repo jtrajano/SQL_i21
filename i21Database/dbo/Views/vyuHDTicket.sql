f68694dca97ada4fe86c34e54df9be57b14c96a7
Jayson Ramos <jayson.ramos@iRely.com>
2018-05-02 12:19:52 +0800

    HD-2546

--- i21Database/dbo/Views/vyuHDTicket.sql
+++ i21Database/dbo/Views/vyuHDTicket.sql
@@ -45,6 +45,7 @@ AS
 		,strEntityLocation = enloc.strLocationName
 		,tic.strDescription
 		,tic.strResolution
+		,tic.strImageId
 	from
 		tblHDTicket tic
 		left outer join tblHDTicketType typ on typ.intTicketTypeId = tic.intTicketTypeId
