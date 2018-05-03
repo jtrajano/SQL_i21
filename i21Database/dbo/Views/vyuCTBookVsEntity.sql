cac7414b0a2161e2bc47fd6a3650030535348b08
Satya S Sahoo <satya.sahoo@irely.com>
2018-05-03 14:27:10 +0530

    CT-2102 - Book vs Entities - Saved Book Vs Entity disappear after reopening screen. - 18.3Dev

--- i21Database/dbo/Views/vyuCTBookVsEntity.sql
+++ i21Database/dbo/Views/vyuCTBookVsEntity.sql
@@ -11,5 +11,5 @@ AS
 	FROM	tblCTBookVsEntity			BE
 			JOIN	tblCTBook			BK  ON  BK.intBookId			=   BE.intBookId
 			JOIN	tblEMEntity			EY  ON  EY.intEntityId			=   BE.intEntityId
-			JOIN	tblSMMultiCompany	MC  ON  MC.intMultiCompanyId	=	BE.intMultiCompanyId
+	LEFT	JOIN	tblSMMultiCompany	MC  ON  MC.intMultiCompanyId	=	BE.intMultiCompanyId
 	LEFT	JOIN	tblCTSubBook		SB  ON  SB.intSubBookId			=	BE.intSubBookId
