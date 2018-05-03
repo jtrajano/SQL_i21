0968d7888bac15a07793fc1aec71ab6dae764687
Erick <erick.delacruz@irely.com>
2018-05-03 20:11:05 +0800

    AP-5432

--- i21Database/dbo/Functions/fnAPGetPaymentForexRate.sql
+++ i21Database/dbo/Functions/fnAPGetPaymentForexRate.sql
@@ -11,7 +11,7 @@ BEGIN
 	FROM (
 		SELECT
 			payment.intPaymentId
-			,CASE WHEN voucherDetail.intContractDetailId > 0 AND ISNULL(voucherDetail.dblRate,1) != 1 THEN voucherDetail.dblRate ELSE ISNULL(forexRate.dblRate,1) END dblRate
+			,CASE WHEN voucherDetail.intContractDetailId > 0 AND ISNULL(voucherDetail.dblRate,1) != 1 THEN payment.dblExchangeRate ELSE ISNULL(forexRate.dblRate,1) END dblRate
 		FROM tblAPPayment payment
 		INNER JOIN tblAPPaymentDetail paymentDetail ON payment.intPaymentId = paymentDetail.intPaymentId
 		INNER JOIN tblAPBill voucher ON ISNULL(paymentDetail.intBillId, paymentDetail.intOrigBillId) = voucher.intBillId
@@ -27,6 +27,7 @@ BEGIN
 			AND exchangeRateDetail.dtmValidFromDate <= payment.dtmDatePaid
 			ORDER BY exchangeRateDetail.dtmValidFromDate DESC
 		) forexRate
+		WHERE paymentDetail.dblPayment != 0
 	) paymentRate
 	GROUP BY intPaymentId
 
