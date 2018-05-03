6307ddbacf3012a512b289e7362915a6a07bcf13
COMTECH\HZaragoza <henry.zaragoza@irely.com>
2018-05-03 15:43:35 +0800

    ST-701 Add Customer Payments Item in Store

--- i21Database/dbo/Tables/tblSTCheckoutCustomerPayments.sql
+++ i21Database/dbo/Tables/tblSTCheckoutCustomerPayments.sql
@@ -9,8 +9,10 @@
     [strType] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
     [strCheckNo] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
     [strComment] NVARCHAR(200) COLLATE Latin1_General_CI_AS NULL, 
+	[intItemId] INT NULL, 
     [intConcurrencyId] INT NULL, 
     CONSTRAINT [PK_tblSTCheckoutCustomerPayments_intCustPaymentsId] PRIMARY KEY ([intCustPaymentsId]), 
 	CONSTRAINT [FK_tblSTCheckoutCustomerPayments_tblSTCheckoutHeader] FOREIGN KEY ([intCheckoutId]) REFERENCES [tblSTCheckoutHeader]([intCheckoutId]) ON DELETE CASCADE ,
-    CONSTRAINT [FK_tblSTCheckoutCustomerPayments_tblEMEntity] FOREIGN KEY ([intCustomerId]) REFERENCES tblEMEntity([intEntityId]) 
+    CONSTRAINT [FK_tblSTCheckoutCustomerPayments_tblEMEntity] FOREIGN KEY ([intCustomerId]) REFERENCES tblEMEntity([intEntityId]),
+	CONSTRAINT [FK_tblSTCheckoutCustomerPayments_tblICItem] FOREIGN KEY ([intItemId]) REFERENCES tblICItem([intItemId]) 
 )
