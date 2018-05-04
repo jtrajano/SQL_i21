48aea2023a7e3289c82f6b1def2e9398aeeeebe5
Erick <erick.delacruz@irely.com>
2018-05-04 20:37:27 +0800

    AP-5350

--- i21Database/dbo/User Defined Types/VoucherDetailLoadNonInv.sql
+++ i21Database/dbo/User Defined Types/VoucherDetailLoadNonInv.sql
@@ -4,6 +4,7 @@
 	intContractDetailId INT NOT NULL,
 	intItemId			INT NOT NULL,
 	intItemUOMId		INT NOT NULL,
+	intCostUOMId		INT NULL,
 	intAccountId		INT NOT NULL,
 	intLoadDetailId		INT NOT NULL,
 	dblQtyReceived		DECIMAL(18,6) NOT NULL DEFAULT 0,
