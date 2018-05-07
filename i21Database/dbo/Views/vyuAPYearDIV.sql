110056ab8f8ec15d988bbe86f49eec98720aeefc
Lex Eugene Peregrino <lexeugene14@yahoo.com>
2018-05-07 16:06:26 +0800

    AP-4909 1.0

---
+++ i21Database/dbo/Views/vyuAPYearDIV.sql
@@ -0,0 +1,23 @@
+﻿CREATE VIEW [dbo].[vyuAPYearDIV]
+AS
+
+WITH DIV1099 (
+	intYear
+)
+AS
+(
+	SELECT
+	 A.intYear
+																  
+	FROM vyuAP1099 A
+	CROSS JOIN tblSMCompanySetup B
+	CROSS JOIN tblAP1099Threshold C
+	WHERE A.int1099Form = 5
+	
+	)
+	SELECT
+		*
+	FROM DIV1099 A
+GO
+
+
