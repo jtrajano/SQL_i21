c4a6837d363a56b1fe780b4271c51f12f6267c62
Crisman Lazaga <cris.lazaga@irely.com>
2018-05-08 09:15:37 +0800

    ST-648

--- i21Database/dbo/Views/vyuSTFuelSummaryReport.sql
+++ i21Database/dbo/Views/vyuSTFuelSummaryReport.sql
@@ -7,12 +7,13 @@ SELECT ST.intStoreNo
 , CAT.strDescription strCategoryDescription
 , IT.strItemNo
 , IT.strDescription strItemDescription
-, CPT.dblQuantity
-, CPT.dblPrice
-, CPT.dblAmount 
+, SUM(CPT.dblQuantity) dblQuantity
+, SUM(CPT.dblAmount) dblAmount
+, SUM(CPT.dblAmount) / SUM(CPT.dblQuantity) dblPrice
 FROM tblSTCheckoutHeader CH INNER JOIN tblSTStore ST ON ST.intStoreId = CH.intStoreId 
 INNER JOIN tblSTCheckoutPumpTotals CPT ON CPT.intCheckoutId = CH.intCheckoutId 
 INNER JOIN tblICItemUOM IU ON IU.intItemUOMId = CPT.intPumpCardCouponId 
 INNER JOIN tblICItem IT ON IT.intItemId = IU.intItemId 
 INNER JOIN tblICCategory CAT ON CAT.intCategoryId = IT.intCategoryId
-WHERE CPT.dblAmount > 0
+WHERE CPT.dblAmount > 0	
+GROUP BY ST.intStoreNo, ST.strRegion, ST.strDistrict, ST.strDescription, CAT.strDescription, IT.strItemNo, IT.strDescription
