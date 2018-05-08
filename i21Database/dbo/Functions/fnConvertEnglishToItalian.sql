f0b70bcc25ecc8eba300450db7c2951d445632b8
Satya S Sahoo <satya.sahoo@irely.com>
2018-05-08 15:48:22 +0530

    CT-2081 - Multi-lingual prints for Buyer / Seller Contract - iDeal - 18.3Dev

---
+++ i21Database/dbo/Functions/fnConvertEnglishToItalian.sql
@@ -0,0 +1,16 @@
+﻿CREATE FUNCTION [dbo].[fnConvertEnglishToItalian]
+(
+	@strEnglish NVARCHAR(MAX)
+	
+)
+RETURNS NVARCHAR(400)
+AS
+BEGIN 
+	DECLARE @strItalian NVARCHAR(MAX)
+
+	SELECT @strItalian = strCustomLabel 
+	FROM tblSMReportLabelDetail
+	WHERE strLabelName = @strEnglish
+
+	RETURN @strItalian
+END
