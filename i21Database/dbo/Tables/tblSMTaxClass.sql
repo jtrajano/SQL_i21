f93330a7f799c376658e6c10aec59f48ea93279c
Dan Jalimao <dan.jalimao@irely.com>
2018-05-08 10:44:29 +0800

    SM-2818

--- i21Database/dbo/Tables/tblSMTaxClass.sql
+++ i21Database/dbo/Tables/tblSMTaxClass.sql
@@ -2,5 +2,6 @@
 (
 	[intTaxClassId] INT NOT NULL PRIMARY KEY IDENTITY, 
     [strTaxClass] NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL, 
+    [strOriginTaxType] NVARCHAR(5) COLLATE Latin1_General_CI_AS NULL, 
     [intConcurrencyId] INT NOT NULL DEFAULT 1
 )
