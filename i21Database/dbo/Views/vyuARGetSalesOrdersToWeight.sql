abb423d5e4fb22311f96ad79b959bb82b2deb87e
Kelvin Quinto <kelvin.quinto@irely.com>
2018-05-10 05:09:36 +0800

    Revert "AR-7334"
    This reverts commit 97dfd7f3bfb481ff78343394cc85927c041ed9e9.

--- i21Database/dbo/Views/vyuARGetSalesOrdersToWeight.sql
+++ i21Database/dbo/Views/vyuARGetSalesOrdersToWeight.sql
@@ -6,43 +6,66 @@ SELECT intSalesOrderId			= SO.intSalesOrderId
 	 , intCurrencyId			= SO.intCurrencyId
 	 , intFreightTermId			= SO.intFreightTermId
 	 , intShipToLocationId		= SO.intShipToLocationId
-	 , intItemId				= NULL
-	 , intItemUOMId				= NULL
-	 , intSalesOrderDetailId	= NULL
-	 , intContractHeaderId		= NULL
-	 , intContractDetailId		= NULL
-	 , intSubLocationId			= NULL
-	 , intStorageLocationId		= NULL
-	 , intCommodityId			= NULL
-	 , intCategoryId			= NULL
+	 , intItemId				= SOD.intItemId
+	 , intItemUOMId				= SOD.intItemUOMId
+	 , intSalesOrderDetailId	= SOD.intSalesOrderDetailId
+	 , intContractHeaderId		= SOD.intContractHeaderId
+	 , intContractDetailId		= SOD.intContractDetailId
+	 , intSubLocationId			= SOD.intSubLocationId
+	 , intStorageLocationId		= SOD.intStorageLocationId
+	 , intCommodityId			= ITEMS.intCommodityId
+	 , intCategoryId			= ITEMS.intCategoryId
 	 , strSalesOrderNumber		= SO.strSalesOrderNumber
 	 , strCustomerName			= CUSTOMER.strName
 	 , strCustomerNumber		= CUSTOMER.strCustomerNumber
 	 , strLocationName			= LOCATIONS.strLocationName
 	 , strCurrency				= CURRENCY.strCurrency
-	 , strItemNo				= NULL
-	 , strDescription			= NULL
-	 , strLotTracking			= NULL
-	 , strUnitMeasure			= NULL
-	 , strCommodityCode			= NULL
-	 , strCategoryCode			= NULL
-	 , strSubLocationName		= NULL
-	 , strStorageLocationName   = NULL
-	 , strContractNumber		= NULL
-	 , intContractSeq			= NULL
-	 , dblQtyOrdered			= NULL
-	 , dblQtyShipped			= NULL
-	 , dblQtyAllocated			= NULL
-	 , dblPrice					= NULL
-	 , dblDiscount				= NULL
-	 , dblTotal					= NULL
+	 , strItemNo				= ITEMS.strItemNo
+	 , strDescription			= ITEMS.strDescription
+	 , strLotTracking			= ITEMS.strLotTracking
+	 , strUnitMeasure			= UOM.strUnitMeasure
+	 , strCommodityCode			= COMMODITY.strCommodityCode
+	 , strCategoryCode			= CATEGORY.strCategoryCode
+	 , strSubLocationName		= SUBLOCATION.strSubLocationName
+	 , strStorageLocationName   = STORAGELOCATION.strName
+	 , strContractNumber		= CONTRACTS.strContractNumber
+	 , intContractSeq			= CONTRACTS.intContractSeq
+	 , dblQtyOrdered			= ISNULL(SOD.dblQtyOrdered, 0)
+	 , dblQtyShipped			= ISNULL(SOD.dblQtyShipped, 0)
+	 , dblQtyAllocated			= ISNULL(SOD.dblQtyAllocated, 0)
+	 , dblPrice					= ISNULL(SOD.dblPrice, 0)
+	 , dblDiscount				= ISNULL(SOD.dblDiscount, 0)
+	 , dblTotal					= ISNULL(SOD.dblTotal, 0)
 	 , dtmDate					= SO.dtmDate
-FROM dbo.tblSOSalesOrder SO WITH (NOLOCK)
+FROM dbo.tblSOSalesOrderDetail SOD WITH (NOLOCK)
+INNER JOIN (
+	SELECT SO.intSalesOrderId
+		 , SO.intCompanyLocationId
+		 , SO.intEntityCustomerId
+		 , SO.intCurrencyId
+		 , SO.intShipToLocationId
+		 , SO.intFreightTermId
+		 , SO.strSalesOrderNumber
+		 , SO.dtmDate
+	FROM dbo.tblSOSalesOrder SO WITH (NOLOCK)	
+	WHERE SO.strOrderStatus NOT IN ('Closed', 'Short Closed', 'Cancelled')
+	  AND SO.strTransactionType = 'Order'
+) SO ON SOD.intSalesOrderId = SO.intSalesOrderId
 INNER JOIN (
 	SELECT intCompanyLocationId
 		 , strLocationName
 	FROM dbo.tblSMCompanyLocation WITH (NOLOCK)
 ) LOCATIONS ON SO.intCompanyLocationId = LOCATIONS.intCompanyLocationId
+INNER JOIN (
+	SELECT intItemId
+		 , intCommodityId
+		 , intCategoryId
+		 , strItemNo
+		 , strDescription
+		 , strLotTracking
+	FROM dbo.tblICItem WITH (NOLOCK)
+	WHERE ysnUseWeighScales = 1
+) ITEMS ON SOD.intItemId = ITEMS.intItemId
 INNER JOIN (
 	SELECT C.intEntityId
 		 , strCustomerNumber
@@ -59,16 +82,56 @@ INNER JOIN (
 	     , strCurrency
 	FROM dbo.tblSMCurrency WITH (NOLOCK)
 ) CURRENCY ON SO.intCurrencyId = CURRENCY.intCurrencyID
-CROSS APPLY (
-	SELECT intSalesOrderId
-	FROM dbo.tblSOSalesOrderDetail SOD WITH (NOLOCK)
+LEFT JOIN (
+	SELECT intCompanyLocationSubLocationId
+		 , intCompanyLocationId
+		 , strSubLocationName
+	FROM dbo.tblSMCompanyLocationSubLocation WITH (NOLOCK)
+) SUBLOCATION ON SOD.intSubLocationId = SUBLOCATION.intCompanyLocationSubLocationId
+             AND SO.intCompanyLocationId = SUBLOCATION.intCompanyLocationId
+LEFT JOIN (
+	SELECT intStorageLocationId
+		 , strName
+	FROM dbo.tblICStorageLocation WITH (NOLOCK)
+) STORAGELOCATION ON SOD.intStorageLocationId = STORAGELOCATION.intStorageLocationId
+LEFT JOIN (
+	SELECT intItemId
+		 , intUnitMeasureId
+		 , intItemUOMId
+	FROM dbo.tblICItemUOM WITH (NOLOCK)
+) ICUOM ON SOD.intItemId = ICUOM.intItemId
+       AND SOD.intItemUOMId = ICUOM.intItemUOMId
+INNER JOIN (
+	SELECT intUnitMeasureId
+		 , strUnitMeasure
+	FROM dbo.tblICUnitMeasure WITH (NOLOCK)
+) UOM ON ICUOM.intUnitMeasureId = UOM.intUnitMeasureId
+LEFT JOIN (
+	SELECT intCommodityId
+		 , strCommodityCode
+	FROM dbo.tblICCommodity WITH (NOLOCK)
+) COMMODITY ON ITEMS.intCommodityId = COMMODITY.intCommodityId
+LEFT JOIN (
+	SELECT intCategoryId
+		 , strCategoryCode
+	FROM dbo.tblICCategory WITH (NOLOCK)
+) CATEGORY ON ITEMS.intCategoryId = CATEGORY.intCategoryId
+LEFT JOIN (
+	SELECT CH.intContractHeaderId
+		 , CD.intContractDetailId
+		 , CD.intContractSeq
+		 , strContractNumber
+	FROM dbo.tblCTContractHeader CH WITH (NOLOCK)
 	INNER JOIN (
-		SELECT intItemId
-		FROM dbo.tblICItem WITH (NOLOCK)
-		WHERE ysnUseWeighScales = 1
-	) ITEM ON SOD.intItemId = ITEM.intItemId
-	WHERE SOD.intSalesOrderId = SO.intSalesOrderId
-	GROUP BY SOD.intSalesOrderId
-) DETAILS
-WHERE SO.strOrderStatus NOT IN ('Closed', 'Short Closed', 'Cancelled')
-	  AND SO.strTransactionType = 'Order'
+		SELECT intContractHeaderId
+			 , intContractDetailId
+			 , intContractSeq
+		FROM dbo.tblCTContractDetail WITH (NOLOCK)
+	) CD ON CH.intContractHeaderId = CD.intContractHeaderId
+) CONTRACTS ON SOD.intContractHeaderId = CONTRACTS.intContractHeaderId
+	       AND SOD.intContractDetailId = CONTRACTS.intContractDetailId
+LEFT JOIN (
+	SELECT intFreightTermId
+		 , strFreightTerm
+	FROM dbo.tblSMFreightTerms WITH (NOLOCK)
+) FREIGHT ON SO.intFreightTermId = FREIGHT.intFreightTermId
