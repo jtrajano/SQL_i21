cc138445e1fd162105f4a10bdc237929dbe66442
Chakra <chakradhar.behera@irely.com>
2018-05-10 14:27:50 +0530

    MFG-2900

---
+++ i21Database/dbo/Views/vyuMFGetInventoryByParentLot.sql
@@ -0,0 +1,18 @@
+﻿CREATE VIEW [dbo].[vyuMFGetInventoryByParentLot]
+	AS 
+Select 
+v.intParentLotId,
+v.strParentLotNumber,
+v.strItemNo,
+v.strItemDescription AS strDescription,
+SUM(ISNULL(v.dblQty,0)) AS dblQty,
+MAX(v.strQtyUOM) AS strQtyUOM,
+SUM(ISNULL(v.dblWeight,0)) AS dblWeight,
+MAX(v.strWeightUOM) AS strWeightUOM,
+v.intLocationId,
+v.strCompanyLocationName AS strLocationName,
+v.strSubLocationName,
+v.intSubLocationId
+FROM vyuMFInventoryView v 
+Where v.dblQty>0 AND v.intParentLotId>0
+Group By v.intParentLotId,v.intLocationId,v.strItemNo,v.strItemDescription,strParentLotNumber,v.strCompanyLocationName,v.strSubLocationName,v.intSubLocationId
