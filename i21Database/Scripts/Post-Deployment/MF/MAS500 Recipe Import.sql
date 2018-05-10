903babbbf78ce162714f7220379fe67af2b77e31
Chakra <chakradhar.behera@irely.com>
2018-05-10 14:17:42 +0530

    MFG-2722

--- i21Database/Scripts/Post-Deployment/MF/MAS500 Recipe Import.sql
+++ i21Database/Scripts/Post-Deployment/MF/MAS500 Recipe Import.sql
@@ -22,6 +22,22 @@ FROM         dbo.tmfRoutHead_HAI AS a INNER JOIN
 
 WHERE a.Active = 1 AND a.RollUpFlag = 1 and ISNUll(WorkCenterId,'') Not in ('','SDRY') 
 
+GO
+
+CREATE VIEW [dbo].[viRelyM_sp_Router_Detail] AS
+SELECT     a.CompanyID, a.RoutingKey, b.RoutingId, c.ItemID, b.VersionId, ISNULL(d.ItemID, ' ') AS MatItemID, ISNULL(e.WhseID, ' ') AS MatWhseID, 
+                      a.OperationType, a.QTYCycle, a.MaterReqPer as MaterReqPer, Sum(a.MatReqPc / MaterReqPer) as MatReqPc , ISNULL(g.UnitMeasID, ' ') AS UnitMeasID, b.UpdateDate,itmcls.itemclassname as ItemClassName
+FROM         dbo.tmfRoutDetl_HAI AS a INNER JOIN
+                      dbo.tmfRoutHead_HAI AS b ON a.RoutingKey = b.RoutingKey INNER JOIN
+                      dbo.timItem AS c ON c.ItemKey = b.ItemKey LEFT OUTER JOIN
+                      dbo.timItem AS d ON a.MatItemKey = d.ItemKey LEFT OUTER JOIN
+                      dbo.timWarehouse AS e ON a.WhseKey = e.WhseKey LEFT OUTER JOIN
+                      dbo.tciUnitMeasure AS g ON a.MaterValUOMKey = g.UnitMeasKey LEFT JOIN 
+					   dbo.timItemClass AS itmcls ON itmcls.ItemClassKey = d.ItemClassKey
+WHERE a.ProgressStep <> 'Y' and 
+b.Active=1 and b.RollupFlag=1 and a.OperationType='M' and MatReqPc != 0
+group by   a.CompanyID, a.RoutingKey, b.RoutingId, c.ItemID, b.VersionId, ISNULL(d.ItemID, ' ') , ISNULL(e.WhseID, ' ') , 
+                      a.OperationType, a.QTYCycle, a.MaterReqPer, ISNULL(g.UnitMeasID, ' ') , b.UpdateDate,itmcls.itemclassname
 
 GO
 
