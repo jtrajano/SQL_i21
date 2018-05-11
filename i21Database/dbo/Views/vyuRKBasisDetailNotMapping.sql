2dcad2d3885a114efa50943da861c9ebaf53a5a5
Smith Wilson de Jesus <smith.dejesus@irely.com>
2018-05-11 20:06:06 +0800

    RM-731

--- i21Database/dbo/Views/vyuRKBasisDetailNotMapping.sql
+++ i21Database/dbo/Views/vyuRKBasisDetailNotMapping.sql
@@ -31,6 +31,9 @@ SELECT
 		,pt.strPricingType
 		,strLocationName
 		,strUnitMeasure
+		,CAST(CASE WHEN IBD.intM2MBasisDetailId IS NULL THEN 0 ELSE 1 END AS bit) AS ysnUsed
+		,M2M.strRecordName as strM2MBatch
+		,M2M.dtmTransactionUpTo as dtmM2MDate
 FROM tblRKM2MBasisDetail bd
 JOIN tblRKM2MBasis mb on mb.intM2MBasisId=bd.intM2MBasisId
 JOIN tblICCommodity c on c.intCommodityId=bd.intCommodityId
@@ -43,3 +46,5 @@ LEFT JOIN tblSMCompanyLocation l on l.intCompanyLocationId=bd.intCompanyLocation
 LEFT JOIN tblSMCurrency cur on cur.intCurrencyID=bd.intCurrencyId
 LEFT JOIN tblCTPricingType pt on pt.intPricingTypeId=bd.intPricingTypeId
 LEFT JOIN tblICUnitMeasure um on um.intUnitMeasureId=bd.intUnitMeasureId
+LEFT JOIN tblRKM2MInquiryBasisDetail IBD on bd.intM2MBasisDetailId = IBD.intM2MBasisDetailId
+LEFT JOIN tblRKM2MInquiry M2M ON IBD.intM2MInquiryId = M2M.intM2MInquiryId
