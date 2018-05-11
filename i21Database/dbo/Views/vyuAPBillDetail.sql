de20edaab849d7f12beb68ae62c77f8d001f00c0
Erick <erick.delacruz@irely.com>
2018-05-11 23:02:10 +0800

    AP-5440

--- i21Database/dbo/Views/vyuAPBillDetail.sql
+++ i21Database/dbo/Views/vyuAPBillDetail.sql
@@ -85,7 +85,7 @@ LEFT JOIN dbo.tblICItem C
 LEFT JOIN dbo.tblAP1099Category D 
 	ON D.int1099CategoryId = B.int1099Category
 LEFT JOIN dbo.tblSMTaxGroup E 
-	ON BD.intTaxGroupId = E.intTaxGroupId
+	ON B.intTaxGroupId = E.intTaxGroupId
 LEFT JOIN dbo.tblSMTaxGroup F 
 	ON BD.intTaxGroupId = F.intTaxGroupId
 LEFT JOIN dbo.tblSCTicket SC 
