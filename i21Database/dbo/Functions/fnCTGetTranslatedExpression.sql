7b30663efb4387cf9fcd2477598f8ea278019853
Jayson Ramos <jayson.ramos@iRely.com>
2018-05-12 09:30:53 +0800

    CT-1355

---
+++ i21Database/dbo/Functions/fnCTGetTranslatedExpression.sql
@@ -0,0 +1,25 @@
+﻿create FUNCTION [dbo].[fnCTGetTranslatedExpression]
+(
+	@strLabelName nvarchar(50),
+	@intLanguageId int,
+	@strExpression nvarchar(500)
+
+)
+RETURNS NVARCHAR(500)
+AS
+BEGIN 
+	DECLARE @strCustomLabel NVARCHAR(MAX);
+
+	select top 1
+		@strCustomLabel = b.strCustomLabel
+	from
+		tblSMReportLabels a
+		,tblSMReportLabelDetail b
+	where
+		b.intReportLabelsId = a.intReportLabelsId
+		and a.strName = isnull(@strLabelName, '')
+		and a.intLanguageId = isnull(@intLanguageId, 0)
+		and b.strLabelName = isnull(@strExpression, '');
+
+	RETURN @strCustomLabel;
+END
