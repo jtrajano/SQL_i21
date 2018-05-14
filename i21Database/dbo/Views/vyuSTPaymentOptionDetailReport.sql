8bd737726b62cef8e34cdfc9065cdb3c510885f5
Crisman Lazaga <cris.lazaga@irely.com>
2018-05-14 10:02:01 +0800

    ST-643

---
+++ i21Database/dbo/Views/vyuSTPaymentOptionDetailReport.sql
@@ -0,0 +1,19 @@
+﻿CREATE VIEW [dbo].[vyuSTPaymentOptionDetailReport]
+	AS 
+SELECT ST.intStoreId
+, ST.intStoreNo
+, ST.strRegion
+, ST.strDistrict
+, ST.strDescription strStoreDescription
+, CH.dtmCheckoutDate
+, CH.ysnPosted
+, CPO.intPaymentOptionId
+, PO.strPaymentOptionId
+, PO.strDescription
+, CPO.dblAmount
+FROM tblSTCheckoutHeader CH INNER JOIN tblSTStore ST ON ST.intStoreId = CH.intStoreId 
+INNER JOIN tblSTCheckoutPaymentOptions CPO ON CPO.intCheckoutId = CH.intCheckoutId 
+INNER JOIN tblSTPaymentOption PO ON PO.intPaymentOptionId = CPO.intPaymentOptionId
+INNER JOIN tblICItemUOM IU ON IU.intItemUOMId = CPO.intItemId 
+INNER JOIN tblICItem IT ON IT.intItemId = IU.intItemId 
+WHERE CPO.dblAmount > 0
