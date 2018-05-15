d5882124291cc62cd9b17ce51753ca088f9c4f2d
axel_casauran <axel.casauran@irely.com>
2018-05-16 03:20:12 +0800

    TM-2829

--- i21Database/dbo/Views/vyuTMMobileBillingOrder.sql
+++ i21Database/dbo/Views/vyuTMMobileBillingOrder.sql
@@ -8,10 +8,12 @@ SELECT
 	,A.strOrderNumber
 	,A.dtmRequestedDate
 	,strCustomerNumber = D.strEntityNo
+	,D.intEntityId
 	,intSiteId = B.intSiteID
 	,B.intSiteNumber
 	,E.intItemId
 	,E.strItemNo
+	,M.intItemUOMId
 	,dblQuantity = CASE WHEN ISNULL(A.dblMinimumQuantity,0) = 0 THEN A.dblQuantity ELSE A.dblMinimumQuantity END
 	,F.intContractDetailId
 	,G.strContractNumber
@@ -28,7 +30,17 @@ SELECT
 	,intRouteId = K.intRouteId
 	,K.strRouteNumber
 	,intStopNumber = L.intSequence
+	,B.intTaxStateID
+	,B.strDescription as strSiteName
+	,N.intShipToId
+	,O.intFreightTermId
+	,B.strLocation
+	,B.intLocationId
+	,E.strDescription as strItemDescription
+	,Q.strSerialNumber
+	,Q.dblTankCapacity
 	,intConcurrencyId = A.intConcurrencyId
+
 FROM tblTMDispatch A
 INNER JOIN tblTMSite B
 	ON A.intSiteID = B.intSiteID
@@ -53,5 +65,30 @@ LEFT JOIN tblLGRoute K
 LEFT JOIN tblLGRouteOrder L
 	ON K.intRouteId = L.intRouteId
 		AND A.intDispatchID = L.intDispatchID
+LEFT JOIN tblICItemUOM M
+	ON M.intItemId = E.intItemId
+		AND M.ysnStockUnit = 1
+LEFT JOIN tblARCustomer N
+	ON N.strCustomerNumber = D.strEntityNo
+LEFT JOIN tblEMEntityLocation O
+	ON O.intEntityLocationId = N.intShipToId
+
+LEFT JOIN (
+	  SELECT 
+	   AA.intSiteID
+	   ,AA.intDeviceId
+	   ,BB.strSerialNumber
+	   ,BB.dblTankCapacity
+	   ,intRecNo = ROW_NUMBER() OVER ( PARTITION BY intSiteID ORDER BY intSiteDeviceID)
+	  FROM tblTMSiteDevice AA
+	  INNER JOIN tblTMDevice BB
+	   ON AA.intDeviceId = BB.intDeviceId
+	  INNER JOIN tblTMDeviceType CC
+	   ON CC.intDeviceTypeId = BB.intDeviceTypeId
+	  WHERE CC.strDeviceType = 'Tank'
+	   AND BB.ysnAppliance = 0
+	) Q
+	ON A.intSiteID = Q.intSiteID
+	  AND intRecNo = 1
 		
 GO
