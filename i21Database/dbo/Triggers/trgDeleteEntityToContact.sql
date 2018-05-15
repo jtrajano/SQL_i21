b1ad3d3804d2dd9ebddcd51a772768de1156c961
Dan Jalimao <dan.jalimao@irely.com>
2018-05-15 09:38:45 +0800

    SM-3843

--- i21Database/dbo/Triggers/trgDeleteEntityToContact.sql
+++ i21Database/dbo/Triggers/trgDeleteEntityToContact.sql
@@ -1,5 +1,8 @@
-﻿CREATE TRIGGER [trgDeleteEntityToContact]
+﻿CREATE TRIGGER [dbo].[trgDeleteEntityToContact]
 	 ON [dbo].[tblEMEntityToContact] 
 FOR DELETE
 AS
+IF NOT EXISTS(SELECT TOP 1 1 FROM tblEMEntityToContact WHERE intEntityContactId IN (SELECT intEntityContactId FROM deleted))
+BEGIN
 	DELETE FROM tblEMEntity WHERE intEntityId IN (SELECT intEntityContactId FROM deleted)
+END
