3a7b617ddfa5da83f3be05b132815f98701c043e
COMTECH\EVersoza <emerson.versoza@irely.com>
2018-05-16 19:30:36 +0800

    SC-1593

--- i21Database/dbo/Stored Procedures/uspSCManualDistribution.sql
+++ i21Database/dbo/Stored Procedures/uspSCManualDistribution.sql
@@ -489,7 +489,8 @@ END
 		FROM tblICInventoryReceipt r 
 		INNER JOIN tblICInventoryReceiptItem ri ON ri.intInventoryReceiptId = r.intInventoryReceiptId
 		LEFT JOIN tblCTContractDetail CT ON CT.intContractDetailId = ri.intLineNo
-		WHERE ri.intInventoryReceiptId = @InventoryReceiptId AND ri.intOwnershipType = 1
+		LEFT JOIN tblCTPriceFixation CTP ON CTP.intContractDetailId = CT.intContractDetailId
+		WHERE ri.intInventoryReceiptId = @InventoryReceiptId AND ri.intOwnershipType = 1 AND CTP.intPriceFixationId IS NULL
 		-- Assemble the voucher items 
 		BEGIN 
 			INSERT INTO @voucherItems (
