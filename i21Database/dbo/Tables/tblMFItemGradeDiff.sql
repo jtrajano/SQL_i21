7210b8ea8215dede7d5b809dbfdf0e2a926e3ffc
Vinoth Kumar T <vinoth.t@irely.com>
2018-05-16 23:13:47 +0530

    MFG-3455

--- i21Database/dbo/Tables/tblMFItemGradeDiff.sql
+++ i21Database/dbo/Tables/tblMFItemGradeDiff.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE tblMFItemGradeDiff (
 	intGradeDiffId INT identity(1, 1) CONSTRAINT PK_tblMFItemGradeDiff_intGradeDiffId PRIMARY KEY
-	,intItemId INT CONSTRAINT UQ_tblMFItemGradeDiff_intItemId UNIQUE
-	,dblGradeDiff Numeric(18,6)
-	,ysnZeroCost BIT CONSTRAINT DF_tblMFItemGradeDiff_ysnZeroCost DEFAULT 0
+	,intItemId INT NOT NULL CONSTRAINT UQ_tblMFItemGradeDiff_intItemId UNIQUE
+	,dblGradeDiff Numeric(18,6) NOT NULL
+	,ysnZeroCost BIT NOT NULL CONSTRAINT DF_tblMFItemGradeDiff_ysnZeroCost DEFAULT 0
 	,intCreatedUserId INT 
 	,dtmCreated DATETIME CONSTRAINT DF_tblMFItemGradeDiff_dtmCreated DEFAULT GetDate()
 	,intLastModifiedUserId INT 	
