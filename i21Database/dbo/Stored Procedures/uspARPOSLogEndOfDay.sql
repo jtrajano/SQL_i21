6d474966604964e5c40cfe4c86f865b083c1857a
JBorlagdan <jc.borlagdan@irely.com>
2018-05-17 14:05:44 +0800

    AR-7349

---
+++ i21Database/dbo/Stored Procedures/uspARPOSLogEndOfDay.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[uspARPOSLogEndOfDay]
+	@intPOSLogId AS INT
+AS
+	IF(@intPOSLogId > 0)
+	BEGIN
+		UPDATE tblARPOSLog
+		SET
+			dtmLogout = GETDATE(),
+			ysnLoggedIn = 0
+		WHERE intPOSLogId = @intPOSLogId OR intPOSLogOriginId = @intPOSLogId
+	END
+	ELSE
+	BEGIN
+		RETURN;
+	END
+
+RETURN 0
