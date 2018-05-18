3a47b9abf9d65d85ef9cc8c999caac1fdf9f2314
Mon Gonzales <mon.gonzales@irely.com>
2018-05-18 19:11:19 +0800

    AR-7118 - Invoice - Units for Other Charge item

---
+++ i21Database/dbo/Functions/fnARCalculateQtyBetweenUOM.sql
@@ -0,0 +1,38 @@
+﻿CREATE FUNCTION [dbo].[fnARCalculateQtyBetweenUOM]
+(
+	@intItemUOMIdFrom 				INT
+	,@intItemUOMIdTo				INT 
+	,@dblQty 						NUMERIC(38, 20)
+	,@intItemId 					INT
+	,@strType	 					NVARCHAR(50)
+)
+RETURNS NUMERIC(38,20)
+AS
+BEGIN
+	
+	IF ISNULL(@dblQty, 0) = 0 
+		RETURN 0;
+	DECLARE @isStockTracking BIT 
+
+	if isnull(@intItemId, 0) <> 0 or isnull(@strType, '') <> ''
+	begin
+		SELECT @isStockTracking = dbo.fnARIsStockTrackingItem(@strType, @intItemId)
+
+		IF(@isStockTracking = 0)
+		BEGIN
+			RETURN 0;
+		END
+	end
+	
+	
+	
+
+	DECLARE @dblConvertedQty NUMERIC(38,20)
+
+	SELECT @dblConvertedQty = [dbo].[fnCalculateQtyBetweenUOM](@intItemUOMIdFrom, ISNULL(@intItemUOMIdTo, @intItemUOMIdFrom), @dblQty)
+
+
+	RETURN @dblConvertedQty;
+
+
+END
