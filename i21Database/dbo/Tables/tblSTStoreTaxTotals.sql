f6399379f040b976915d99b93db659eeaee38f40
COMTECH\HZaragoza <henry.zaragoza@irely.com>
2018-05-18 20:31:12 +0800

    ST-729 [Store] Unable to enter tax total details

--- i21Database/dbo/Tables/tblSTStoreTaxTotals.sql
+++ i21Database/dbo/Tables/tblSTStoreTaxTotals.sql
@@ -6,8 +6,8 @@
     [intItemId] INT NOT NULL, 
     [intConcurrencyId] INT NOT NULL,
 	CONSTRAINT [PK_tblSTStoreTaxTotals] PRIMARY KEY CLUSTERED ([intStoreTaxTotalId]),
-	CONSTRAINT [AK_tblSTStoreTaxTotals_intTaxCodeId] UNIQUE NONCLUSTERED ([intTaxCodeId] ASC), 
-	CONSTRAINT [AK_tblSTStoreTaxTotals_intItemId] UNIQUE NONCLUSTERED ([intItemId] ASC),
+	CONSTRAINT [AK_tblSTStoreTaxTotals_intStoreId_intTaxCodeId] UNIQUE ([intStoreId], [intTaxCodeId]),
+	CONSTRAINT [AK_tblSTStoreTaxTotals_intStoreId_intItemId] UNIQUE ([intStoreId], [intItemId]),
 	CONSTRAINT [FK_tblSTStoreTaxTotals_tblSTStore] FOREIGN KEY ([intStoreId]) REFERENCES [tblSTStore]([intStoreId]) ON DELETE CASCADE,
 	CONSTRAINT [FK_tblSTStoreTaxTotals_tblICItem_intItemId] FOREIGN KEY ([intItemId]) REFERENCES [dbo].[tblICItem] ([intItemId]),
 	CONSTRAINT [FK_tblSTStoreTaxTotals_tblSMTaxCode_intTaxCodeId] FOREIGN KEY ([intTaxCodeId]) REFERENCES [dbo].[tblSMTaxCode] ([intTaxCodeId])
