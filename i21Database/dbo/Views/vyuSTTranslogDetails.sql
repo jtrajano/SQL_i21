e62292a1707a97367a863f2d29c1f30b0c569992
COMTECH\HZaragoza <henry.zaragoza@irely.com>
2018-05-18 15:19:49 +0800

    ST-646 Report > Checkout Transaction Journal

---
+++ i21Database/dbo/Views/vyuSTTranslogDetails.sql
@@ -0,0 +1,15 @@
+﻿CREATE VIEW dbo.vyuSTTranslogDetails
+AS
+SELECT ROW_NUMBER() OVER (ORDER BY intTermMsgSN ASC) AS intId
+	   , CAST(TR.intCheckoutId AS NVARCHAR(MAX)) + '0' + CAST(TR.intTermMsgSN AS NVARCHAR(MAX)) AS strUniqueId -- ROW_NUMBER() OVER (ORDER BY intTermMsgSN ASC) AS intUniqeId
+       , TR.intTrlDeptNumber
+	   , TR.strTrlDept
+	   , TR.strTrlNetwCode
+	   , TR.strTrlUPC
+	   , TR.strTrlDesc
+	   , TR. dblTrlQty
+	   , TR.dblTrlUnitPrice
+	   , TR.dblTrlLineTot
+FROM tblSTTranslogRebates TR
+JOIN tblSTCheckoutHeader CH ON TR.intCheckoutId = CH.intCheckoutId
+JOIN tblSTStore ST ON CH.intStoreId = ST.intStoreId 
---
+++ i21Database/dbo/Views/vyuSTTranslogHeader.sql
@@ -0,0 +1,24 @@
+﻿CREATE VIEW dbo.vyuSTTranslogHeader
+AS
+SELECT  CAST(TRR.intCheckoutId AS NVARCHAR(MAX)) + '0' + CAST(TRR.intTermMsgSN AS NVARCHAR(MAX)) AS strUniqueId
+      , TRR.intTermMsgSN
+	  , TRR.intStoreId
+	  , TRR.intCheckoutId
+	  , TRR.dtmCheckoutDate
+	  , CAST(TRR.intStoreNo AS NVARCHAR(100)) + ' ' + TRR.strStoreDescription AS strStoreDescription
+	  , TRR.strTransType
+	  , TRR.dtmDate
+	  , TRR.strCashier
+	  , TRR.dblTrValueTrTotWTax
+FROM
+(   
+	SELECT TR.*
+	       , ST.intStoreNo
+		   , ST.strDescription AS strStoreDescription
+		   , CH.dtmCheckoutDate
+		   , ROW_NUMBER() OVER (PARTITION BY TR.intTermMsgSN, TR.intStoreId, TR.intCheckoutId ORDER BY TR.intTermMsgSN ASC) AS rn
+	FROM tblSTTranslogRebates TR
+	JOIN tblSTCheckoutHeader CH ON TR.intCheckoutId = CH.intCheckoutId
+	JOIN tblSTStore ST ON CH.intStoreId = ST.intStoreId 
+) TRR 
+WHERE TRR.rn = 1
