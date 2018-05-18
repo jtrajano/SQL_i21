fae643c27d66486252effafc1e2092293fbf68da
Feb Montefrio <feb.montefrio@irely.com>
2018-05-18 18:58:27 +0800

    [B] IC-5369

--- i21Database/dbo/Stored Procedures/uspICReturnStockInLIFO.sql
+++ i21Database/dbo/Stored Procedures/uspICReturnStockInLIFO.sql
@@ -9,7 +9,7 @@ CREATE PROCEDURE [dbo].[uspICReturnStockInLIFO]
 	,@intItemLocationId AS INT
 	,@intItemUOMId AS INT
 	,@strBatchId AS NVARCHAR(40)
-	,@dtmDate AS DATETIME
+	,@dtmReturnDate AS DATETIME
 	,@dblQty NUMERIC(18,6) 
 	,@dblCost AS NUMERIC(38, 20)
 	,@strTransactionId AS NVARCHAR(40)
@@ -46,104 +46,7 @@ DECLARE @cbOutOutId AS INT
 		,@cost AS NUMERIC(38, 20)
 		,@intTransactionTypeId AS INT 
 
--- Check if there is available stocks to return from the Inventory Adjustment - Quantity Change. 
-BEGIN 
-	SELECT	TOP 1 
-			@cbId = cb.intInventoryLIFOId
-			,@cbOutOutId = cbOut.intId
-			,@cbOutInventoryTransactionId = cbOut.intInventoryTransactionId
-			,@cost = cb.dblCost --cbOut.dblCost
-			,@intTransactionTypeId = cbOut.intTransactionTypeId
-	FROM	tblICInventoryLIFO cb INNER JOIN (
-				tblICInventoryReceipt rSource INNER JOIN tblICInventoryReceipt r
-					ON rSource.intInventoryReceiptId = r.intSourceInventoryReceiptId
-			)
-				ON cb.intTransactionId = rSource.intInventoryReceiptId
-				AND cb.strTransactionId = rSource.strReceiptNumber
-			OUTER APPLY (
-				SELECT  TOP 1 
-						cbOut.intId
-						,cbOut.intInventoryTransactionId							
-						,t.intTransactionTypeId
-						,t.dblCost
-				FROM	tblICInventoryLIFOOut cbOut INNER JOIN tblICInventoryTransaction t
-							ON cbOut.intInventoryTransactionId = t.intInventoryTransactionId
-						INNER JOIN tblICInventoryTransactionType ty
-							ON ty.intTransactionTypeId = t.intTransactionTypeId
-						OUTER APPLY (
-							SELECT 	TOP 1 
-									strReturnPostMode = ISNULL(strReturnPostMode, 'Default')
-							FROM 	tblICCompanyPreference
-						) returnPostMode																					
-				WHERE	cbOut.intInventoryLIFOId = cb.intInventoryLIFOId 
-						AND ty.strName = 'Inventory Adjustment - Quantity Change'
-						AND (cbOut.dblQty - ISNULL(cbOut.dblQtyReturned, 0)) > 0 
-						AND ISNULL(t.ysnIsUnposted, 0) = 0 
-						AND returnPostMode.strReturnPostMode = 'Allow Return After Qty Change'
-			) cbOut
-	WHERE	r.intInventoryReceiptId = @intTransactionId
-			AND r.strReceiptNumber = @strTransactionId
-			AND cb.intItemId = @intItemId
-			AND cb.intItemLocationId = @intItemLocationId
-			AND cb.intItemUOMId = @intItemUOMId
-
-	IF @cbOutOutId IS NOT NULL 
-	BEGIN 
-		UPDATE	cbOut 
-		SET		-- Increase the return qty 
-				dblQtyReturned = 
-					ISNULL(cbOut.dblQtyReturned, 0) 
-					+ CASE	WHEN (cbOut.dblQty - ISNULL(cbOut.dblQtyReturned, 0)) >= @dblQty THEN @dblQty
-							ELSE (cbOut.dblQty - ISNULL(cbOut.dblQtyReturned, 0)) 
-					END 
-				-- update the remaining qty
-				,@RemainingQty = 
-						CASE	WHEN (cbOut.dblQty - ISNULL(cbOut.dblQtyReturned, 0)) >= @dblQty THEN 0
-								ELSE (cbOut.dblQty - ISNULL(cbOut.dblQtyReturned, 0)) - @dblQty
-						END
-				-- retrieve the cost from the lifo bucket. 
-				,@CostUsed = NULL 
-
-				-- retrieve the	qty reduced from a lifo bucket 
-				,@QtyOffset = 							
-							CASE	WHEN (cbOut.dblQty - ISNULL(cbOut.dblQtyReturned, 0)) >= @dblQty THEN -@dblQty
-									ELSE -(cbOut.dblQty - ISNULL(cbOut.dblQtyReturned, 0)) 
-							END
-
-				-- retrieve the id of the matching lifo bucket 
-				,@LIFOId = NULL 
-		FROM	tblICInventoryLIFOOut cbOut
-		WHERE	cbOut.intId = @cbOutOutId 
-
-		-- Create a log of the return transaction. 
-		INSERT INTO tblICInventoryReturned (
-			intInventoryLIFOId
-			,intInventoryTransactionId
-			,intOutId
-			,dblQtyReturned
-			,dblCost
-			,intTransactionId
-			,strTransactionId
-			,strBatchId
-			,intTransactionTypeId 
-			,intTransactionDetailId
-		)
-		SELECT 
-			intInventoryLIFOId			= @cbId
-			,intInventoryTransactionId	= @cbOutInventoryTransactionId
-			,intOutId					= @cbOutOutId
-			,dblQtyReturned				= -@QtyOffset
-			,dblCost					= @cost
-			,intTransactionId			= @intTransactionId 
-			,strTransactionId			= @strTransactionId
-			,strBatchId					= @strBatchId
-			,intTransactionTypeId		= @intTransactionTypeId
-			,intTransactionDetailId		= @intTransactionDetailId
-	END 
-END 
-
 -- Validate if the cost bucket is negative. If Negative stock is not allowed, then block the posting. 
-IF @cbOutOutId IS NULL 
 BEGIN 
 	DECLARE @ALLOW_NEGATIVE_NO AS INT = 3
 
@@ -153,23 +56,27 @@ BEGIN
 			,@AllowNegativeInventory AS INT 
 			,@UnitsOnHand AS NUMERIC(38, 20)
 
-	-- Get the on-hand qty 
-	SELECT	@UnitsOnHand = s.dblUnitOnHand
+	-- Get the on-hand qty for the item location regardless of date. 
+	SELECT	@UnitsOnHand = s.dblUnitOnHand 
 	FROM	tblICItemStock s
 	WHERE	s.intItemId = @intItemId
 			AND s.intItemLocationId = @intItemLocationId
 
 	SELECT	@strItemNo = i.strItemNo
-			,@CostBucketId = cb.intInventoryLIFOId
 			,@AllowNegativeInventory = il.intAllowNegativeInventory
 			,@strLocationName = cl.strLocationName
+			,@CostBucketId = cb.intInventoryLIFOId
+			,@dtmReceiptDate = cb.dtmDate
+			,@strReceiptSourceNumber = cb.strTransactionId
+			,@CostUsed = cb.dblCost 
 	FROM	tblICItem i INNER JOIN tblICItemLocation il
 				ON i.intItemId = il.intItemId
 				AND il.intItemLocationId = @intItemLocationId
 			INNER JOIN tblSMCompanyLocation cl
 				ON cl.intCompanyLocationId = il.intLocationId
 			OUTER APPLY (
-				SELECT	TOP 1 cb.*
+				SELECT	TOP 1 
+						cb.*
 				FROM	tblICInventoryLIFO cb INNER JOIN (
 							tblICInventoryReceipt rSource INNER JOIN tblICInventoryReceipt r
 								ON rSource.intInventoryReceiptId = r.intSourceInventoryReceiptId
@@ -179,59 +86,31 @@ BEGIN
 				WHERE	cb.intItemId = @intItemId
 						AND cb.intItemLocationId = @intItemLocationId
 						AND cb.intItemUOMId = @intItemUOMId
-						AND ROUND((cb.dblStockIn - cb.dblStockOut), 6) > 0  
-						AND dbo.fnDateLessThanEquals(cb.dtmDate, @dtmDate) = 1
 						AND r.intInventoryReceiptId = @intTransactionId
 						AND r.strReceiptNumber = @strTransactionId
 			) cb 
 
-	IF @CostBucketId IS NULL  
+	IF (@UnitsOnHand - @dblQty) < 0 AND @AllowNegativeInventory = @ALLOW_NEGATIVE_NO
 	BEGIN 
-		-- Check the stock from the cb. 
-		SET @UnitsOnHand = 0
-		SELECT	@UnitsOnHand = ISNULL(ROUND((cb.dblStockIn - cb.dblStockOut), 6), 0) 
-				,@strReceiptSourceNumber = cb.strTransactionId
-				,@dtmReceiptDate = cb.dtmDate
-		FROM	tblICItem i INNER JOIN tblICItemLocation il
-					ON i.intItemId = il.intItemId
-					AND il.intItemLocationId = @intItemLocationId
-				INNER JOIN tblSMCompanyLocation cl
-					ON cl.intCompanyLocationId = il.intLocationId
-				OUTER APPLY (
-					SELECT	TOP 1 cb.*
-					FROM	tblICInventoryLIFO cb INNER JOIN (
-								tblICInventoryReceipt rSource INNER JOIN tblICInventoryReceipt r
-									ON rSource.intInventoryReceiptId = r.intSourceInventoryReceiptId
-							)
-								ON cb.intTransactionId = rSource.intInventoryReceiptId
-								AND cb.strTransactionId = rSource.strReceiptNumber
-					WHERE	cb.intItemId = @intItemId
-							AND cb.intItemLocationId = @intItemLocationId
-							AND cb.intItemUOMId = @intItemUOMId
-							AND r.intInventoryReceiptId = @intTransactionId
-							AND r.strReceiptNumber = @strTransactionId
-				) cb 
+		-- 'Negative stock quantity is not allowed for {Item No} at {Location}.'
+		EXEC uspICRaiseError 80003, @strItemNo, @strLocationName;
+		RETURN -80003
+	END
 
-		IF @UnitsOnHand > 0 
-		BEGIN 
-			DECLARE @strReturnDate AS VARCHAR(20) = CONVERT(NVARCHAR(20), @dtmDate, 101) 
-			DECLARE @strReceiptDate AS VARCHAR(20) = CONVERT(NVARCHAR(20), @dtmReceiptDate, 101) 
+	IF dbo.fnDateLessThan(@dtmReceiptDate, @dtmReturnDate) = 1
+	BEGIN 
+		DECLARE @strReturnDate AS VARCHAR(20) = CONVERT(NVARCHAR(20), @dtmReturnDate, 101) 
+		DECLARE @strReceiptDate AS VARCHAR(20) = CONVERT(NVARCHAR(20), @dtmReceiptDate, 101) 
 			
-			-- 'Check the return date on the transaction. Return date is {Return date}, while {Item Id} in {Receipt Id} is dated {Receipt date}.'
-			EXEC uspICRaiseError 80108, @strReturnDate, @strItemNo, @strReceiptSourceNumber, @strReceiptDate;
-		END 
-		ELSE 
-		BEGIN 
-			-- 'Unable to do the return. All the stocks in {item id} from {receipt id} are fully returned already.'
-			EXEC uspICRaiseError 80109, @strItemNo, @strReceiptSourceNumber;
-		END 
-		RETURN -1
+		-- 'Check the return date on the transaction. Return date is {Return date}, while {Item Id} in {Receipt Id} is dated {Receipt date}.'
+		EXEC uspICRaiseError 80108, @strReturnDate, @strItemNo, @strReceiptSourceNumber, @strReceiptDate;
+		RETURN -80108
 	END 
 END 
 
 -- Upsert (update or insert) a record in the cost bucket.
 IF @cbOutOutId IS NULL 
-BEGIN
+BEGIN 
 	MERGE	TOP(1)
 	INTO	dbo.tblICInventoryLIFO 
 	WITH	(HOLDLOCK) 
@@ -246,22 +125,23 @@ BEGIN
 					ON rSource.intInventoryReceiptId = r.intSourceInventoryReceiptId
 		WHERE	r.intInventoryReceiptId = @intTransactionId
 				AND r.strReceiptNumber = @strTransactionId
-	) AS Source_Query 
+	) AS Source_Query  
 		ON cb.intItemId = Source_Query.intItemId
 		AND cb.intItemLocationId = Source_Query.intItemLocationId
 		AND cb.intItemUOMId = Source_Query.intItemUOMId
 		AND (cb.dblStockIn - cb.dblStockOut) > 0 
-		AND dbo.fnDateLessThanEquals(cb.dtmDate, @dtmDate) = 1
+		AND dbo.fnDateLessThanEquals(cb.dtmDate, @dtmReturnDate) = 1
 		AND cb.intTransactionId = Source_Query.intTransactionId
 		AND cb.strTransactionId = Source_Query.strTransactionId 
 
 	-- Update an existing cost bucket
 	WHEN MATCHED THEN 
 		UPDATE 
-		SET	cb.dblStockOut = ISNULL(cb.dblStockOut, 0) 
-						+ CASE	WHEN (cb.dblStockIn - cb.dblStockOut) >= @dblQty THEN @dblQty
-								ELSE (cb.dblStockIn - cb.dblStockOut) 
-						END 
+		SET	cb.dblStockOut = 
+				ISNULL(cb.dblStockOut, 0) 
+				+ CASE	WHEN (cb.dblStockIn - cb.dblStockOut) >= @dblQty THEN @dblQty
+						ELSE (cb.dblStockIn - cb.dblStockOut) 
+				END 
 
 			,cb.intConcurrencyId = ISNULL(cb.intConcurrencyId, 0) + 1
 
@@ -284,7 +164,7 @@ BEGIN
 			,@LIFOId = cb.intInventoryLIFOId
 
 	-- Insert a new LIFO bucket
-	WHEN NOT MATCHED THEN 
+	WHEN NOT MATCHED THEN
 		INSERT (
 			[intItemId]
 			,[intItemLocationId]
@@ -303,7 +183,7 @@ BEGIN
 			@intItemId
 			,@intItemLocationId
 			,@intItemUOMId
-			,@dtmDate
+			,@dtmReturnDate
 			,0
 			,@dblQty
 			,@dblCost
@@ -311,7 +191,13 @@ BEGIN
 			,@intTransactionId
 			,GETDATE()
 			,@intEntityUserSecurityId
-			,1	
+			,1
 		)
 	;
+
+	-- If Update was not performed, assume an insert was done. 
+	SELECT @LIFOId = SCOPE_IDENTITY() WHERE @LIFOId IS NULL; 
+
+	-- Update the Qty Offset 
+	SELECT @QtyOffset = -@dblQty WHERE @QtyOffset IS NULL; 
 END 
