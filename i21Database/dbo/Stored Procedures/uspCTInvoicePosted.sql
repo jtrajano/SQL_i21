3bcc91134923304e0fe11ae96651d5147c9149ed
Kelvin Quinto <kelvin.quinto@irely.com>
2018-05-21 21:57:44 +0800

    AR-5083

--- i21Database/dbo/Stored Procedures/uspCTInvoicePosted.sql
+++ i21Database/dbo/Stored Procedures/uspCTInvoicePosted.sql
@@ -16,6 +16,10 @@ BEGIN TRY
 				@intFromItemUOMId				INT,
 				@intToItemUOMId					INT,
 				@intUniqueId					INT,
+				@intTicketId					INT = NULL,
+				@intTicketTypeId				INT = NULL,
+			    @intTicketType					INT = NULL,
+			    @strInOutFlag					NVARCHAR(MAX) = NULL,
 				@dblQty							NUMERIC(18,6),
 				@dblConvertedQty				NUMERIC(18,6),
 				@ErrMsg							NVARCHAR(MAX),
@@ -32,6 +36,7 @@ BEGIN TRY
 		intInvoiceDetailId			INT,
 		intContractDetailId			INT,
 		intItemUOMId				INT,
+		intTicketId					INT,
 		dblQty						NUMERIC(18,6)	
 	)
 
@@ -39,12 +44,14 @@ BEGIN TRY
 		 [intInvoiceDetailId]
 		,[intContractDetailId]
 		,[intItemUOMId]
-		,[dblQty])
+		,[dblQty]
+		,[intTicketId])
 	SELECT
 		 I.[intInvoiceDetailId]
 		,I.[intContractDetailId]
 		,I.[intItemUOMId]
 		,I.[dblQtyShipped]
+		,I.[intTicketId]
 	FROM
 		@ItemsFromInvoice I
 	WHERE
@@ -62,13 +69,22 @@ BEGIN TRY
 		SELECT	@intContractDetailId			=	NULL,
 				@intFromItemUOMId				=	NULL,
 				@dblQty							=	NULL,
-				@intInvoiceDetailId	=	NULL
+				@intInvoiceDetailId				=	NULL,
+				@intTicketId					=   NULL,
+				@intTicketTypeId				=	NULL,
+				@intTicketType					=   NULL,
+				@strInOutFlag					=   NULL
 
 		SELECT	@intContractDetailId			=	[intContractDetailId],
 				@intFromItemUOMId				=	[intItemUOMId],
 				@dblQty							=	[dblQty],
-				@intInvoiceDetailId				=	[intInvoiceDetailId]
-		FROM	@tblToProcess 
+				@intInvoiceDetailId				=	[intInvoiceDetailId],
+				@intTicketId					=   T.[intTicketId],
+				@intTicketTypeId				=   T.[intTicketTypeId],
+				@intTicketType					=   T.[intTicketType],
+				@strInOutFlag					=   T.[strInOutFlag]
+		FROM	@tblToProcess P
+		LEFT JOIN  tblSCTicket T ON T.intTicketId = P.intTicketId
 		WHERE	[intUniqueId]					=	 @intUniqueId
 
 		IF NOT EXISTS(SELECT * FROM tblCTContractDetail WHERE intContractDetailId = @intContractDetailId)
@@ -94,12 +110,15 @@ BEGIN TRY
 
 		SELECT	@dblSchQuantityToUpdate = - @dblConvertedQty
 					
-		EXEC	uspCTUpdateScheduleQuantity
-				@intContractDetailId	=	@intContractDetailId,
-				@dblQuantityToUpdate	=	@dblSchQuantityToUpdate,
-				@intUserId				=	@intUserId,
-				@intExternalId			=	@intInvoiceDetailId,
-				@strScreenName			=	'Invoice' 
+		IF (ISNULL(@intTicketTypeId, 0) <> 9 AND (ISNULL(@intTicketType, 0) <> 6 AND ISNULL(@strInOutFlag, '') <> 'O'))
+			BEGIN
+				EXEC	uspCTUpdateScheduleQuantity
+						@intContractDetailId	=	@intContractDetailId,
+						@dblQuantityToUpdate	=	@dblSchQuantityToUpdate,
+						@intUserId				=	@intUserId,
+						@intExternalId			=	@intInvoiceDetailId,
+						@strScreenName			=	'Invoice' 
+			END
 
 		SELECT @intUniqueId = MIN(intUniqueId) FROM @tblToProcess WHERE intUniqueId > @intUniqueId
 	END
