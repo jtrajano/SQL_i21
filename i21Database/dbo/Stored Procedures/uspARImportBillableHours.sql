9bef7fb9b3b22814250e9d259a2d634e80daf8ec
Kelvin Quinto <kelvin.quinto@irely.com>
2018-05-21 21:57:06 +0800

    AR-7507

--- i21Database/dbo/Stored Procedures/uspARImportBillableHours.sql
+++ i21Database/dbo/Stored Procedures/uspARImportBillableHours.sql
@@ -7,7 +7,7 @@
 	,@SuccessfulCount		AS INT				= 0		OUTPUT
 	,@InvalidCount			AS INT				= 0		OUTPUT
 	,@DocumentMaintenanceId AS INT				= NULL
-	,@DefaultLocationId AS INT				= NULL
+	,@DefaultLocationId		AS INT				= NULL
 AS
 
 BEGIN
@@ -18,205 +18,163 @@ SET NOCOUNT ON
 SET XACT_ABORT ON
 SET ANSI_WARNINGS OFF
 
-DECLARE @ZeroDecimal		NUMERIC(18,6)
-	  , @DateOnly			DATETIME
-	  , @CompanyLocationId	INT
+DECLARE @tblInvoiceEntries		InvoiceIntegrationStagingTable
+DECLARE	@tblTaxEntries			LineItemTaxDetailStagingTable
+DECLARE @dblZeroDecimal			NUMERIC(18,6)	= 0
+	  , @dtmDateOnly			DATETIME		= CAST(GETDATE() AS DATE)
+	  , @intCompanyLocationId	INT				= @DefaultLocationId
+	  , @strErrorMessage		NVARCHAR(MAX)	= ''
+	  , @strCreatedInvoices		NVARCHAR(MAX)	= ''
 
-SET @ZeroDecimal = 0.000000	
-SET @DateOnly = CAST(GETDATE() AS DATE)
-SET @CompanyLocationId = @DefaultLocationId
-
-DECLARE @TicketHoursWorked TABLE(intTicketHoursWorkedId INT, intEntityCustomerId INT, intTicketId INT)
-		
-IF (@HoursWorkedIDs IS NOT NULL) 
+IF(OBJECT_ID('tempdb..#BILLABLE') IS NOT NULL)
 BEGIN
-	IF(@HoursWorkedIDs = 'all')
-	BEGIN
-		INSERT INTO @TicketHoursWorked SELECT [intTicketHoursWorkedId], [intEntityId], [intTicketId] FROM vyuARBillableHoursForImport
-	END
-	ELSE
-	BEGIN
-		INSERT INTO @TicketHoursWorked SELECT [intTicketHoursWorkedId], [intEntityId], [intTicketId] FROM vyuARBillableHoursForImport WHERE [intTicketHoursWorkedId] IN (SELECT intID FROM fnGetRowsFromDelimitedValues(@HoursWorkedIDs))
-	END
+    DROP TABLE #BILLABLE
 END
 
---VALIDATE NULL TERMS
-IF EXISTS(SELECT THW.intEntityCustomerId 
-		  FROM @TicketHoursWorked THW
-		  INNER JOIN (
-				SELECT intEntityId
-					 , intTermsId 
-				FROM dbo.tblARCustomer WITH (NOLOCK)
-		  ) ARC ON THW.intEntityCustomerId = ARC.intEntityId
-		  WHERE ARC.intTermsId IS NULL)
-BEGIN
-	RAISERROR('Some of the customers doesn''t have Terms setup.', 16, 1) 
-	RETURN 0
-END
-
-DECLARE @NewInvoices TABLE(intEntityCustomerId INT, intCompanyLocationId INT)
-DECLARE @NewlyCreatedInvoices TABLE(intInvoiceId INT)
-
-INSERT INTO @NewInvoices
-SELECT DISTINCT V.intEntityId
-			  , ISNULL(V.intEntityWarehouseId, ISNULL(@CompanyLocationId,V.intCompanyLocationId))
-FROM vyuARBillableHoursForImport V
-INNER JOIN @TicketHoursWorked HW ON V.intTicketId = HW.intTicketId
-INNER JOIN tblARCustomer C ON V.intEntityId = C.intEntityId
-GROUP BY V.intEntityId, ISNULL(V.intEntityWarehouseId, ISNULL(@CompanyLocationId,V.intCompanyLocationId))
-
-WHILE EXISTS(SELECT TOP 1 NULL FROM @NewInvoices)
+SELECT intEntityCustomerId		= intEntityId
+	 , intTicketId				= intTicketId
+	 , intTicketHoursWorkedId	= intTicketHoursWorkedId
+	 , intCompanyLocationId		= intCompanyLocationId
+	 , intItemId				= intItemId
+	 , intItemUOMId				= intItemUOMId	 
+	 , strItemNo				= strItemNo
+	 , dblHours					= CAST(intHours AS NUMERIC(18, 6))
+	 , dblPrice					= dblPrice
+	 , strTicketNumber			= CAST(strTicketNumber COLLATE Latin1_General_CI_AS AS NVARCHAR(100))
+INTO #BILLABLE
+FROM vyuARBillableHoursForImport
+WHERE 1 = 0
+		
+IF ISNULL(@HoursWorkedIDs, 'all') = 'all'
 	BEGIN
-		DECLARE @EntityCustomerId INT
-			  , @ComLocationId INT
-			  , @NewInvoiceId INT
-			  , @ErrorMessage NVARCHAR(250)
-			  , @CustomerName NVARCHAR(250)
-				
-		SELECT TOP 1 @EntityCustomerId = intEntityCustomerId
-		           , @ComLocationId = intCompanyLocationId 
-		FROM @NewInvoices
-				
-		EXEC [dbo].[uspARCreateCustomerInvoice]
-			@EntityCustomerId = @EntityCustomerId,
-			@InvoiceDate = @DateOnly,
-			@CompanyLocationId = @ComLocationId,
-			@EntityId = @UserId,
-			@NewInvoiceId = @NewInvoiceId OUTPUT,
-			@ErrorMessage = @ErrorMessage OUTPUT,
-			@DocumentMaintenanceId = @DocumentMaintenanceId
-			
-		IF ISNULL(@NewInvoiceId, 0) = 0 OR ISNULL(@ErrorMessage, '') <> ''
-		BEGIN
-			IF NOT EXISTS (SELECT NULL FROM tblSMCompanyLocation WHERE intCompanyLocationId = @ComLocationId AND ysnLocationActive = 1)
-				BEGIN
-					SELECT TOP 1 @CustomerName = strName FROM tblEMEntity WHERE intEntityId = @EntityCustomerId
-					SET @ErrorMessage = 'The Company Location provided for Customer: ' + @CustomerName + ' is not active!'
-				END
-
-			DELETE FROM @TicketHoursWorked WHERE intEntityCustomerId = @EntityCustomerId
-			SET @NewInvoiceId = NULL
-			RAISERROR(@ErrorMessage, 11, 1) 
-			BREAK
-		END
-		
-		INSERT INTO [tblARInvoiceDetail]
-			([intInvoiceId]
-			,[intItemId]
-			,[strItemDescription]
-			,[intItemUOMId]
-			--,[intOrderUOMId]
-			,[dblQtyOrdered]
-			,[dblQtyShipped]
-			,[dblPrice]
-			,[dblTotal]
-			,[intAccountId]
-			,[intCOGSAccountId]
-			,[intSalesAccountId]
-			,[intInventoryAccountId]
-			,[intTicketHoursWorkedId]
-			,[intConcurrencyId])
-		SELECT
-			@NewInvoiceId												--[intInvoiceId]
-			,V.[intItemId]												--[intItemId]
-			,IC.[strDescription] + ' - ' + V.strTicketNumber			--strItemDescription] 
-			,V.intItemUOMId												--[intItemUOMId]
-			--,ISNULL(IL.intIssueUOMId, V.intItemUOMId)					--[intOrderUOMId]
-			,V.[intHours]												--[dblQtyOrdered]
-			,V.[intHours]												--[dblQtyShipped]
-			,V.[dblPrice] 												--[dblPrice]
-			,V.[dblTotal]  												--[dblTotal]
-			,Acct.[intAccountId]										--[intAccountId]
-			,Acct.[intCOGSAccountId]									--[intCOGSAccountId]
-			,Acct.[intSalesAccountId]									--[intSalesAccountId]
-			,Acct.[intInventoryAccountId]								--[intInventoryAccountId]
-			,V.[intTicketHoursWorkedId]									--[intTicketHoursWorkedId]
-			,1															--[intConcurrencyId]
-		FROM vyuARBillableHoursForImport V
-		INNER JOIN @TicketHoursWorked HW ON V.intTicketHoursWorkedId = HW.intTicketHoursWorkedId
-		INNER JOIN tblICItem IC ON V.intItemId = IC.intItemId
-		--INNER JOIN tblICItemLocation IL ON V.intItemId = IL.intItemId
-		--							   AND V.intCompanyLocationId = IL.intLocationId
-		LEFT OUTER JOIN vyuARGetItemAccount Acct ON V.intItemId = Acct.intItemId
-									  AND V.intCompanyLocationId = Acct.intLocationId
-		WHERE V.intEntityId = @EntityCustomerId
-			AND ISNULL(@CompanyLocationId, V.intCompanyLocationId) = @ComLocationId
-		
-		EXEC dbo.uspARReComputeInvoiceTaxes @InvoiceId = @NewInvoiceId
-		
-		IF ISNULL(@NewInvoiceId, 0) <> 0
-			BEGIN
-				INSERT INTO @NewlyCreatedInvoices
-				SELECT @NewInvoiceId
-			END
-		
-		DELETE FROM @NewInvoices WHERE intEntityCustomerId = @EntityCustomerId AND intCompanyLocationId = @ComLocationId
+		INSERT INTO #BILLABLE 
+		SELECT intEntityCustomerId		= BILLABLE.intEntityId
+			 , intTicketId				= BILLABLE.intTicketId
+			 , intTicketHoursWorkedId	= BILLABLE.intTicketHoursWorkedId
+			 , intCompanyLocationId		= ISNULL(BILLABLE.intEntityWarehouseId, ISNULL(@intCompanyLocationId, BILLABLE.intCompanyLocationId))
+			 , intItemId				= BILLABLE.intItemId
+			 , intItemUOMId				= BILLABLE.intItemUOMId	 
+			 , strItemNo				= BILLABLE.strItemNo
+			 , dblHours					= CAST(BILLABLE.intHours AS NUMERIC(18, 6))
+			 , dblPrice					= BILLABLE.dblPrice
+			 , strTicketNumber			= BILLABLE.strTicketNumber
+		FROM vyuARBillableHoursForImport BILLABLE
+	END
+ELSE
+	BEGIN
+		INSERT INTO #BILLABLE 
+		SELECT intEntityCustomerId		= BILLABLE.intEntityId
+			 , intTicketId				= BILLABLE.intTicketId
+			 , intTicketHoursWorkedId	= BILLABLE.intTicketHoursWorkedId
+			 , intCompanyLocationId		= ISNULL(BILLABLE.intEntityWarehouseId, ISNULL(@intCompanyLocationId, BILLABLE.intCompanyLocationId))
+			 , intItemId				= BILLABLE.intItemId
+			 , intItemUOMId				= BILLABLE.intItemUOMId	 
+			 , strItemNo				= BILLABLE.strItemNo
+			 , dblHours					= CAST(BILLABLE.intHours AS NUMERIC(18, 6))
+			 , dblPrice					= BILLABLE.dblPrice
+			 , strTicketNumber			= BILLABLE.strTicketNumber
+		FROM vyuARBillableHoursForImport BILLABLE
+		INNER JOIN fnGetRowsFromDelimitedValues(@HoursWorkedIDs) SELECTED
+		ON SELECTED.intID = BILLABLE.intTicketHoursWorkedId
 	END
 	
-UPDATE 
-	tblHDTicketHoursWorked
-SET
-	 tblHDTicketHoursWorked.intInvoiceId = I.intInvoiceId
-	,tblHDTicketHoursWorked.ysnBilled = convert(bit,1)
-	,tblHDTicketHoursWorked.dtmBilled = GETDATE()
-FROM 
-	@NewlyCreatedInvoices I
-INNER JOIN
-	tblARInvoiceDetail D 
-		ON I.intInvoiceId = D.intInvoiceId
-INNER JOIN
-	tblHDTicketHoursWorked V 
-		ON D.intTicketHoursWorkedId = V.intTicketHoursWorkedId
-INNER JOIN
-	@TicketHoursWorked HW 
-		ON V.intTicketId = HW.intTicketId
-		AND V.intTicketHoursWorkedId = HW.intTicketHoursWorkedId
-		                    
-IF @Post = 1
-	BEGIN
-		DECLARE	@return_value	INT,
-				@success		BIT,
-				@params			NVARCHAR(MAX),
-				@batchId		NVARCHAR(20),
-				@SuccessCount	INT,
-				@InvCount		INT
-				
-		WHILE EXISTS(SELECT TOP 1 NULL FROM @NewlyCreatedInvoices)
-			BEGIN
-				DECLARE @intInvoiceId INT
-				
-				SELECT TOP 1 @intInvoiceId = intInvoiceId FROM @NewlyCreatedInvoices ORDER BY intInvoiceId
-				
-				IF (SELECT COUNT(*) FROM @NewlyCreatedInvoices) > 1
-					SELECT @params = ISNULL(@params, '') + CONVERT(NVARCHAR(50), intInvoiceId) + ', ' FROM @NewlyCreatedInvoices WHERE intInvoiceId = @intInvoiceId
-				ELSE
-					SELECT @params = ISNULL(@params, '') + CONVERT(NVARCHAR(50), intInvoiceId) FROM @NewlyCreatedInvoices WHERE intInvoiceId = @intInvoiceId
+INSERT INTO @tblInvoiceEntries (
+	 [strSourceTransaction]
+	,[intSourceId]
+	,[strSourceId]
+	,[intEntityCustomerId]
+	,[intCompanyLocationId]
+	,[dtmDate]
+	,[dtmPostDate]
+	,[intEntityId]
+	,[ysnPost]
+	,[intItemId]
+	,[ysnInventory]
+	,[strItemDescription]
+	,[intOrderUOMId]
+	,[dblQtyOrdered]
+	,[intItemUOMId]
+	,[dblQtyShipped]
+	,[dblPrice]
+	,[ysnRefreshPrice]
+	,[ysnRecomputeTax]
+	,[intTicketHoursWorkedId]
+	,[dblCurrencyExchangeRate]
+)
+SELECT 
+	 [strSourceTransaction]		= 'Direct'
+	,[intSourceId]				= BILLABLE.intTicketId
+	,[strSourceId]				= BILLABLE.strTicketNumber
+	,[intEntityCustomerId]		= BILLABLE.intEntityCustomerId
+	,[intCompanyLocationId]		= BILLABLE.intCompanyLocationId
+	,[dtmDate]					= @dtmDateOnly
+	,[dtmPostDate]				= @dtmDateOnly
+	,[intEntityId]				= @UserId
+	,[ysnPost]					= 0
+	,[intItemId]				= BILLABLE.intItemId
+	,[ysnInventory]				= 0
+	,[strItemDescription]		= BILLABLE.strTicketNumber
+	,[intOrderUOMId]			= BILLABLE.intItemUOMId
+	,[dblQtyOrdered]			= BILLABLE.dblHours
+	,[intItemUOMId]				= BILLABLE.intItemUOMId
+	,[dblQtyShipped]			= BILLABLE.dblHours
+	,[dblPrice]					= BILLABLE.dblPrice
+	,[ysnRefreshPrice]			= 0
+	,[ysnRecomputeTax]			= 1
+	,[intTicketHoursWorkedId]	= BILLABLE.intTicketHoursWorkedId
+	,[dblCurrencyExchangeRate]	= 1.000000
+FROM #BILLABLE BILLABLE
 
-				DELETE FROM @NewlyCreatedInvoices WHERE intInvoiceId = @intInvoiceId
+IF EXISTS (SELECT TOP 1 NULL FROM @tblInvoiceEntries)
+	BEGIN
+		EXEC dbo.[uspARProcessInvoices]
+			 @InvoiceEntries				= @tblInvoiceEntries
+			,@LineItemTaxEntries			= @tblTaxEntries
+			,@UserId						= @UserId
+			,@GroupingOption				= 3	
+			,@RaiseError					= 0
+			,@ErrorMessage					= @strErrorMessage OUT
+			,@CreatedIvoices				= @strCreatedInvoices OUT
+
+		IF ISNULL(@strCreatedInvoices, '') <> ''
+			BEGIN
+				UPDATE TICKETS
+				SET	TICKETS.intInvoiceId	= ID.intInvoiceId
+				   ,TICKETS.ysnBilled		= CONVERT(BIT, 1)
+				   ,TICKETS.dtmBilled		= @dtmDateOnly
+				FROM tblHDTicketHoursWorked TICKETS
+				INNER JOIN (
+					SELECT intTicketHoursWorkedId
+						 , intInvoiceId
+					FROM dbo.tblARInvoiceDetail ID
+					INNER JOIN fnGetRowsFromDelimitedValues(@strCreatedInvoices) I
+					ON ID.intInvoiceId = I.intID
+				) ID ON TICKETS.intTicketHoursWorkedId = ID.intTicketHoursWorkedId
+				INNER JOIN #BILLABLE HW 
+				ON TICKETS.intTicketId = HW.intTicketId
+				AND TICKETS.intTicketHoursWorkedId = HW.intTicketHoursWorkedId
+	
+				IF @Post = 1
+				BEGIN
+					DECLARE	@batchId		NVARCHAR(20),
+							@SuccessCount	INT,
+							@InvCount		INT
+				
+					EXEC [dbo].[uspARPostInvoice]
+							@post				= 1,
+							@recap				= 0,
+							@param				= @strCreatedInvoices,
+							@userId				= @UserId,
+							@successfulCount	= @SuccessCount OUT,
+							@invalidCount		= @InvCount OUT,
+							@success			= @IsSuccess OUT,
+							@batchIdUsed		= @BatchIdUsed OUT,
+							@recapId			= NULL,
+							@transType			= N'Invoice'
+				
+				END 
 			END
-		
-		SELECT @params = intInvoiceId FROM @NewlyCreatedInvoices									
-				
-		EXEC	@return_value = [dbo].[uspARPostInvoice]
-				@batchId = NULL,
-				@post = 1,
-				@recap = 0,
-				@param = @params,
-				@userId = @UserId,
-				@beginDate = NULL,
-				@endDate = NULL,
-				@exclude = NULL,
-				@successfulCount = @SuccessCount OUTPUT,
-				@invalidCount = @InvCount OUTPUT,
-				@success = @IsSuccess OUTPUT,
-				@batchIdUsed = @batchId OUTPUT,
-				@recapId = NULL,
-				@transType = N'Invoice'
-				
-		SET @BatchIdUsed = @batchId
-		SET @SuccessfulCount = @SuccessCount
-		SET @InvalidCount = @InvCount
-	END 
+	END
 	        
 SET @IsSuccess = 1           
 RETURN 1
