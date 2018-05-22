bb545cc17054caeaf768c1beca596d70d3222b0b
Leslie L Arat <leslie.arat@irely.com>
2018-05-22 13:51:05 +0800

    GRN-962 (Add Tickets - misspelled Receipt and Receipt not shown)

--- i21Database/dbo/Views/vyuGRGetStorageTransferTicket.sql
+++ i21Database/dbo/Views/vyuGRGetStorageTransferTicket.sql
@@ -28,6 +28,7 @@ SELECT TOP 100 PERCENT
 ,ISNULL(SH1.intTicketId,0) AS intTicketId
 ,ISNULL(dblDiscountsDue,0)-ISNULL(dblDiscountsPaid,0) AS dblDiscountUnPaid
 ,ISNULL(dblStorageDue,0)-ISNULL(dblStoragePaid,0) AS dblStorageUnPaid
+,IR.strReceiptNumber
 FROM tblGRCustomerStorage CS  
 JOIN tblGRStorageType ST ON ST.intStorageScheduleTypeId = CS.intStorageTypeId  
 JOIN tblSMCompanyLocation LOC ON LOC.intCompanyLocationId = CS.intCompanyLocationId  
@@ -40,5 +41,6 @@ LEFT JOIN tblSMCompanyLocationSubLocation SLOC ON SLOC.intCompanyLocationSubLoca
 LEFT JOIN tblGRStorageHistory SH ON SH.intCustomerStorageId=CS.intCustomerStorageId
 LEFT JOIN tblGRStorageHistory SH1 ON SH1.intCustomerStorageId=CS.intCustomerStorageId AND SH1.strType='From Scale'
 LEFT JOIN tblCTContractHeader CH ON CH.intContractHeaderId=SH.intContractHeaderId  
+LEFT JOIN tblICInventoryReceipt IR ON IR.intInventoryReceiptId = SH1.intInventoryReceiptId
 WHERE CS.dblOpenBalance >0 AND ISNULL(CS.strStorageType,'') <> 'ITR'AND SH.strType IN('From Scale','From Transfer','From Delivery Sheet')  
 ORDER BY CS.dtmDeliveryDate
