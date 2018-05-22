6742fac03cc46bade2a3c501e3e14b59605ca88e
IRELY\kkarthick <karthick.k@irely.com>
2018-05-22 13:44:46 +0530

    IC-5378

--- Integration/Scripts/IC/uspICDCItmGLAcctsMigrationPt.sql
+++ Integration/Scripts/IC/uspICDCItmGLAcctsMigrationPt.sql
@@ -61,6 +61,36 @@ USING
 	Cross Apply
 	(SELECT top 1 inv.intItemId
 		--,seg.intAccountCategoryId
+		,(select intAccountCategoryId from tblGLAccountCategory where strAccountCategory = 'Cost of Goods') intAccountCategoryId
+		,act.intAccountId
+		FROM ptitmmst AS itm 
+		INNER JOIN tblICItem AS inv ON (itm.ptitm_itm_no COLLATE SQL_Latin1_General_CP1_CS_AS = inv.strItemNo COLLATE SQL_Latin1_General_CP1_CS_AS) 
+		INNER JOIN tblGLCOACrossReference AS coa ON coa.strExternalId = itm.ptitm_pur_acct 
+		INNER JOIN tblGLAccount AS act ON act.intAccountId = coa.inti21Id 
+		WHERE coa.strExternalId = itm.ptitm_pur_acct
+		and inv.strType in ('Inventory', 'Finished Good', 'Raw Material') 
+		and I.intItemId = inv.intItemId
+	) as ac
+) AS [Source] (intItemId, intAccountCategoryId, intAccountId, intConcurrencyId)
+ON [Target].intItemId = [Source].intItemId
+	AND [Target].intAccountCategoryId = [Source].intAccountCategoryId
+WHEN NOT MATCHED THEN
+INSERT (intItemId, intAccountCategoryId, intAccountId, intConcurrencyId)
+VALUES ([Source].intItemId, [Source].intAccountCategoryId, [Source].intAccountId, [Source].intConcurrencyId);
+
+--Inventory Adjustment
+MERGE tblICItemAccount AS [Target]
+USING
+(
+	SELECT
+		  intItemId				= I.intItemId
+		, intAccountCategoryId	= ac.intAccountCategoryId
+		, intAccountId			= ac.intAccountId
+		, intConcurrencyId		= 1
+	from tblICItem I
+	Cross Apply
+	(SELECT top 1 inv.intItemId
+		--,seg.intAccountCategoryId
 		,(select intAccountCategoryId from tblGLAccountCategory where strAccountCategory = 'Inventory Adjustment') intAccountCategoryId
 		,act.intAccountId
 		FROM ptitmmst AS itm 
