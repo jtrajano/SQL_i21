45ce5f906e88fb9315d18fbd46890508a988a7ce
JeffTrajano <jeffrey.trajano@irely.com>
2018-05-23 14:46:24 +0800

    GL-6286 Vendor Cross Reference

---
+++ i21Database/dbo/Tables/tblGLVendorMappingDetail.sql
@@ -0,0 +1,26 @@
+﻿CREATE TABLE [dbo].[tblGLVendorMappingDetail](
+	[intVendorMappingDetailId] [int] IDENTITY(1,1) NOT NULL,
+	[intVendorMappingId] [int] NOT NULL,
+	[strMapVendorName] [nvarchar](100) COLLATE Latin1_General_CI_AS NOT NULL,
+	[intEntityVendorId] [int] NOT NULL,
+	[intConcurrencyId] [int] NULL,
+ CONSTRAINT [PK_tblGLVendorMappingDetail] PRIMARY KEY CLUSTERED 
+(
+	[intVendorMappingDetailId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Table Primary Key' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'tblGLVendorMappingDetail', @level2type=N'COLUMN',@level2name=N'intVendorMappingDetailId'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Foreign key to tblGLVendorMapping' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'tblGLVendorMappingDetail', @level2type=N'COLUMN',@level2name=N'intVendorMappingId'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Vendor Name Mapping' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'tblGLVendorMappingDetail', @level2type=N'COLUMN',@level2name=N'strMapVendorName'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Foreign key to tblAPVendor' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'tblGLVendorMappingDetail', @level2type=N'COLUMN',@level2name=N'intEntityVendorId'
+GO
+
