e56dbc43044770b2ef1a571caf4e3b1738b76930
Dan Jalimao <dan.jalimao@irely.com>
2018-05-23 09:59:22 +0800

    SM-4066

--- i21Database/dbo/Views/vyuSMUserRoleMenu.sql
+++ i21Database/dbo/Views/vyuSMUserRoleMenu.sql
@@ -20,5 +20,5 @@ SELECT intUserRoleMenuId
 ,RoleMenu.intConcurrencyId
 FROM tblSMUserRoleMenu RoleMenu
 LEFT JOIN tblSMMasterMenu Menu ON Menu.intMenuID = RoleMenu.intMenuId
-WHERE ISNULL(ysnAvailable, 1) = 1 
+WHERE ISNULL(ysnAvailable, 1) = 1 AND (ysnIsLegacy = 0 OR ((SELECT COUNT(*) FROM tblSMCompanyPreference WHERE ysnLegacyIntegration = 1) = 1 AND ysnIsLegacy = 1))
 --WHERE  strMenuName NOT IN  (CASE  (SELECT TOP 1 ysnLegacyIntegration FROM tblSMCompanyPreference) WHEN 0 THEN ('Import GL from Subledger') ELSE ('')  END)
