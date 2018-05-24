7e45926e55989e0f7b019431307d03f05c992508
Arman Arenas <arman.arenas@irely.com>
2018-05-24 16:56:59 +0800

    PR-680 Optimized vyuPRElectronicFilingSUIDetail; minor fix on MSSUI format

--- i21Database/dbo/Views/vyuPRElectronicFilingSUIDetail.sql
+++ i21Database/dbo/Views/vyuPRElectronicFilingSUIDetail.sql
@@ -1,10 +1,10 @@
-﻿CREATE VIEW [dbo].[vyuPRElectronicFilingSUIDetail]
+﻿CREATE VIEW [dbo].[vyuPRElectronicFilingSUIDetail] 
 AS
-SELECT DISTINCT
+SELECT
 	intYear = SUI.intYear
 	,intQuarter = SUI.intQuarter
 	,intEntityId = SUI.intEntityId
-	,strSSN = LEFT(dbo.fnAPRemoveSpecialChars(ISNULL(EMP.strSocialSecurity, '')), 9)
+	,strSSN = LEFT(REPLACE(ISNULL(EMP.strSocialSecurity, ''), '-',''), 9)
 	,strFirstName = UPPER(LEFT(ISNULL(EMP.strFirstName, ''), 15))
 	,strMiddleName = UPPER(LEFT(ISNULL(EMP.strMiddleName, ''), 15))
 	,strLastName = UPPER(LEFT(ISNULL(EMP.strLastName, ''), 20))
@@ -18,7 +18,7 @@ SELECT DISTINCT
 	,dblTips = ISNULL(TIP.dblTotal, 0)
 	,intWeeks = CONVERT(INT, ISNULL(SUI.dblTotalHours, 0)) / 40
 	,intHours = CONVERT(INT, ISNULL(SUI.dblTotalHours, 0))
-	,strSUIAccount = ISNULL(LEFT(dbo.fnAPRemoveSpecialChars(ESUI.strSUIAccountNumber), 10), '')
+	,strSUIAccount = LEFT(REPLACE(ISNULL(ESUI.strSUIAccountNumber, ''), '-',''), 10)
 	,strWorkSiteID = ISNULL(ESUI.strAuthorizationNumber, '')
 	,dblTaxableState = ISNULL(ST.dblAdjustedGross, 0)
 	,dblStateTax = ISNULL(ST.dblStateTotal, 0)
@@ -40,22 +40,14 @@ SELECT DISTINCT
 	,strMonthYearTerminated = ISNULL(RIGHT('000000'+ CONVERT(nvarchar(10), MONTH(EMP.dtmTerminated)) + CONVERT(nvarchar(10), YEAR(EMP.dtmTerminated)), 6), '')
 FROM
 	vyuPRReportQuarterlySUI SUI
-	LEFT JOIN tblPRElectronicFilingSUI ESUI 
+	INNER JOIN (SELECT intEntityId, strLastName, strFirstName, strMiddleName, strSocialSecurity, 
+					strEEOCCode, dtmOriginalDateHired, dtmTerminated FROM tblPREmployee) EMP ON SUI.intEntityId = EMP.[intEntityId]
+	INNER JOIN (SELECT intYear, intQuarter, strState, strSUIAccountNumber, strAuthorizationNumber FROM tblPRElectronicFilingSUI) ESUI 
 		ON ESUI.intYear = SUI.intYear AND ESUI.intQuarter = SUI.intQuarter
-	LEFT JOIN vyuPRReportQuarterlyStateTax ST 
+	LEFT JOIN (SELECT intEntityId, intYear, intQuarter, dblAdjustedGross, dblStateTotal FROM vyuPRReportQuarterlyStateTax) ST 
 		ON SUI.intEntityId = ST.intEntityId AND SUI.intYear = ST.intYear AND SUI.intQuarter = ST.intQuarter
-	LEFT JOIN (SELECT 
-					intEntityId = intEntityEmployeeId, 
-					intYear = YEAR(dtmPayDate), 
-					intQuarter = DATEPART(QQ, dtmPayDate), 
-					dblTotal = SUM(dblTotal) 
+	LEFT JOIN (SELECT intEntityId = intEntityEmployeeId, intYear = YEAR(dtmPayDate), intQuarter = DATEPART(QQ, dtmPayDate), dblTotal = SUM(dblTotal) 
 				FROM vyuPRPaycheckEarning WHERE ysnVoid = 0 AND strCalculationType = 'Tip'
-				GROUP BY
-					intEntityEmployeeId, 
-					YEAR(dtmPayDate), 
-					DATEPART(QQ, dtmPayDate)) TIP
+				GROUP BY intEntityEmployeeId, YEAR(dtmPayDate), DATEPART(QQ, dtmPayDate)) TIP
 		ON SUI.intEntityId = TIP.intEntityId AND SUI.intYear = TIP.intYear AND SUI.intQuarter = TIP.intQuarter
-	INNER JOIN tblPREmployee EMP ON SUI.intEntityId = EMP.[intEntityId]
-	INNER JOIN tblEMEntity ENT ON ENT.intEntityId = EMP.[intEntityId]
-	INNER JOIN tblEMEntityLocation ENL ON ENT.intEntityId = ENL.intEntityId AND ENL.ysnDefaultLocation = 1
 GO
