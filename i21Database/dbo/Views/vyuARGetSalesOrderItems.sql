d11b6f7c46c56e8268788b42c833207ea8cb5ecc
Kelvin Quinto <kelvin.quinto@irely.com>
2018-05-25 01:17:39 +0800

    AR-7490

--- i21Database/dbo/Views/vyuARGetSalesOrderItems.sql
+++ i21Database/dbo/Views/vyuARGetSalesOrderItems.sql
@@ -47,10 +47,11 @@ SELECT intSalesOrderId			= SO.intSalesOrderId
 	 , dtmDate					= SO.dtmDate
 	 , ysnBlended				= SODETAIL.ysnBlended
 	 , intTaxGroupId			= SODETAIL.intTaxGroupId
-	 , strSubCurrency				= CURRENCY.strCurrency
+	 , strSubCurrency			= CURRENCY.strCurrency
 	 , strStorageLocation		= STOLOC.strName
 	 , strSubLocationName		= SUBLOC.strSubLocationName
-	 , intStorageLocationId		=  SODETAIL.intStorageLocationId
+	 , intStorageLocationId		= SODETAIL.intStorageLocationId
+	 , strTaxGroup				= TAXGROUP.strTaxGroup
 FROM dbo.tblSOSalesOrder SO WITH (NOLOCK)
 INNER JOIN (
 	SELECT intSalesOrderId
@@ -148,20 +149,27 @@ LEFT JOIN (
 	) CD ON CH.intContractHeaderId = CD.intContractHeaderId
 ) CONTRACTS ON SODETAIL.intContractHeaderId = CONTRACTS.intContractHeaderId
 	       AND SODETAIL.intContractDetailId = CONTRACTS.intContractDetailId
-
 LEFT OUTER JOIN (
 	SELECT intCurrencyID
 		 , intCent
 		 , strCurrency
 	FROM dbo.tblSMCurrency WITH (NOLOCK)
 ) CURRENCY ON SODETAIL.intSubCurrencyId = CURRENCY.intCurrencyID
-
-LEFT JOIN ( SELECT intStorageLocationId, strName FROM tblICStorageLocation WITH (NOLOCK) ) STOLOC
-	ON STOLOC.intStorageLocationId = SODETAIL.intStorageLocationId
-
-LEFT JOIN ( SELECT intCompanyLocationSubLocationId,  strSubLocationName FROM tblSMCompanyLocationSubLocation WITH (NOLOCK) ) SUBLOC
-	ON SUBLOC.intCompanyLocationSubLocationId = SODETAIL.intSubLocationId
-
+LEFT JOIN (
+	SELECT intStorageLocationId
+		 , strName 
+	FROM tblICStorageLocation WITH (NOLOCK)
+) STOLOC ON STOLOC.intStorageLocationId = SODETAIL.intStorageLocationId
+LEFT JOIN (
+	SELECT intCompanyLocationSubLocationId
+		 , strSubLocationName 
+	FROM tblSMCompanyLocationSubLocation WITH (NOLOCK) 
+) SUBLOC ON SUBLOC.intCompanyLocationSubLocationId = SODETAIL.intSubLocationId
+LEFT JOIN (
+	SELECT intTaxGroupId
+		 , strTaxGroup
+	FROM dbo.tblSMTaxGroup WITH (NOLOCK)
+) TAXGROUP ON TAXGROUP.intTaxGroupId = SODETAIL.intTaxGroupId
 WHERE SO.strTransactionType = 'Order'
   AND SO.strOrderStatus NOT IN ('Cancelled', 'Closed', 'Short Closed')
   AND ((dbo.fnIsStockTrackingItem(SODETAIL.intItemId) = 0 OR ISNULL(strLotTracking, 'No') = 'No') OR (SODETAIL.intItemId IS NULL AND ISNULL(SODETAIL.strItemDescription, '') <> ''))
