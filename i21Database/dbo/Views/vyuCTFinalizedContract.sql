efa48864773d0135f694be98fcf654ff41ee9829
Gopinath Nayak <gopinath.n@irely.com>
2018-05-25 20:53:08 +0530

    CT-998 - Contracts Finalized Report

---
+++ i21Database/dbo/Views/vyuCTFinalizedContract.sql
@@ -0,0 +1,29 @@
+﻿CREATE VIEW [dbo].[vyuCTFinalizedContract]
+ AS
+ SELECT
+ intContractHeaderId						= PD.intContractHeaderId
+,intContractTypeId							= PH.intContractTypeId
+,intContractDetailId						= PD.intContractDetailId
+,strPONumber								= PH.strContractNumber+ '-' + LTRIM(PD.intContractSeq)
+,strSalesEntity								= Entity.strEntityName
+,strSONumber								= SH.strContractNumber+ '-' + LTRIM(SD.intContractSeq)
+,dblPOQuantity								= ISNULL(PD.dblBalance,0)-ISNULL(PD.dblScheduleQty,0)
+,strPOUOM									= UnitMeasure.strUnitMeasure
+,strInvoiceNumber							= Invoice.strInvoiceNumber
+,dtmInvoiceDate								= CONVERT(DATETIME, CONVERT(VARCHAR, Invoice.dtmPostDate, 101), 101)
+,dblContractAdjustQuantity					= NULL
+,strContractAdjustUOM						= NULL
+
+FROM tblCTContractDetail	PD
+JOIN tblCTContractHeader	PH			     ON  PH.intContractHeaderId				   = PD.intContractHeaderId 
+											AND  PD.intContractStatusId				   = 5 --Contract Seq(Status = Complete)
+											AND  PH.intContractTypeId				   = 1 --Contract (Type = Purchase)
+JOIN tblICItemUOM		    ItemUOM		     ON  ItemUOM.intItemUOMId				   = PD.intItemUOMId
+JOIN tblICUnitMeasure	    UnitMeasure	     ON	 UnitMeasure.intUnitMeasureId		   = ItemUOM.intUnitMeasureId
+JOIN tblLGAllocationDetail  AllocationDetail ON  AllocationDetail.intPContractDetailId = PD.intContractDetailId
+JOIN tblCTContractDetail	SD				 ON  SD.intContractDetailId				   = AllocationDetail.intSContractDetailId
+JOIN tblCTContractHeader	SH				 ON  SH.intContractHeaderId				   = SD.intContractHeaderId
+JOIN vyuCTEntity			Entity			 ON  Entity.intEntityId					   = SH.intEntityId AND Entity.strEntityType = 'Customer'
+JOIN tblLGLoadDetail		LoadDetail		 ON  LoadDetail.intAllocationDetailId	   = AllocationDetail.intAllocationDetailId
+JOIN tblARInvoiceDetail		InvoiceDetail	 ON  InvoiceDetail.intLoadDetailId		   = LoadDetail.intLoadDetailId
+JOIN tblARInvoice			Invoice			 ON  Invoice.intInvoiceId				   = InvoiceDetail.intInvoiceId 
