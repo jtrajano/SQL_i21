09d916d5a496b76456b10f5716a75d84e0a1a558
JeffTrajano <jeffrey.trajano@irely.com>
2018-05-28 16:24:46 +0800

    RM-1240 Futures Market - Product Type should not be able to delete

---
+++ i21Database/dbo/Stored Procedures/uspRKSyncCommodityMarketAttribute.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE uspRKSyncCommodityMarketAttribute
+AS
+DECLARE @List NVARCHAR(800)
+SELECT @List= COALESCE(@List + CASE WHEN strCommodityAttributeId <> '' THEN ',' ELSE '' END, '') + LTRIM(strCommodityAttributeId) 
+FROM tblRKCommodityMarketMapping WHERE strCommodityAttributeId is not null
+MERGE INTO tblRKCommodityMarketMappingAttribute
+WITH (HOLDLOCK)
+AS RMAttributeTable
+USING(
+	select Value from dbo.[fnICSplitStringToTable](@List,',') WHERE Value is not null
+) AS Source
+ON 
+RMAttributeTable.intCommodityAttributeId = Source.Value
+WHEN NOT MATCHED BY Target
+THEN INSERT (intCommodityAttributeId) VALUES(Source.Value)
+WHEN NOT MATCHED BY SOURCE THEN DELETE;
+
+
