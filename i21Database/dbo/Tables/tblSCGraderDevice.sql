a3ec6c4c1e8bc1142f31c1c54e42c9b1a1f4e0e0
COMTECH\EVersoza <emerson.versoza@irely.com>
2018-05-28 20:45:33 +0800

    SC-18

---
+++ i21Database/dbo/Tables/tblSCGraderDevice.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[tblSCGraderDevice]
+(
+	[intGraderDeviceId] INT NOT NULL IDENTITY, 
+	[intScaleDeviceId] INT NULL, 
+    [strFieldName] NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NOT NULL, 
+    [intFieldColumnNumber] INT NOT NULL, 
+    [intConcurrencyId] INT NULL DEFAULT ((0)), 
+    CONSTRAINT [PK_tblSCGraderDevice_intGraderDeviceId] PRIMARY KEY ([intGraderDeviceId]),
+	CONSTRAINT [FK_tblSCGraderDevice_tblSCScaleDevice_intScaleDeviceId] FOREIGN KEY ([intScaleDeviceId]) REFERENCES [tblSCScaleDevice]([intScaleDeviceId])
+)
