b3691b1adbbf12c753d11e209883fb416f73da14
Jayson Ramos <jayson.ramos@iRely.com>
2018-05-28 20:40:09 +0800

    HD-2694

--- i21Database/dbo/Tables/tblHDTicketHoursWorked.sql
+++ i21Database/dbo/Tables/tblHDTicketHoursWorked.sql
@@ -28,7 +28,7 @@
     CONSTRAINT [FK_TicketHoursWorked_Ticket] FOREIGN KEY ([intTicketId]) REFERENCES [dbo].[tblHDTicket] ([intTicketId])  on delete cascade,
     CONSTRAINT [FK_TicketHoursWorked_tblICItem_intItemId] FOREIGN KEY ([intItemId]) REFERENCES [dbo].[tblICItem] ([intItemId]),
     CONSTRAINT [FK_TicketHoursWorked_tblICItemUOM_intItemUOMId] FOREIGN KEY ([intItemUOMId]) REFERENCES [dbo].[tblICItemUOM] ([intItemUOMId]),
-    CONSTRAINT [FK_TicketHoursWorked_tblHDTimeEntry_intTimeEntryId] FOREIGN KEY ([intTimeEntryId]) REFERENCES [dbo].[tblHDTimeEntry] ([intTimeEntryId]),
+    --CONSTRAINT [FK_TicketHoursWorked_tblHDTimeEntry_intTimeEntryId] FOREIGN KEY ([intTimeEntryId]) REFERENCES [dbo].[tblHDTimeEntry] ([intTimeEntryId]),
 	CONSTRAINT [FK_tblHDTicketHoursWorked_tblARInvoice_intInvoiceId] FOREIGN KEY ([intInvoiceId]) REFERENCES [dbo].[tblARInvoice] ([intInvoiceId])
 )
 
