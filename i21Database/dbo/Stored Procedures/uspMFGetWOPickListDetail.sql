444f4227b2bc537a3348249028eea81e2fbab78b
Vinoth Kumar T <vinoth.t@irely.com>
2018-05-29 18:24:01 +0530

    MFG-3381

--- i21Database/dbo/Stored Procedures/uspMFGetWOPickListDetail.sql
+++ i21Database/dbo/Stored Procedures/uspMFGetWOPickListDetail.sql
@@ -6,9 +6,11 @@ BEGIN
 		,OH.dtmOrderDate
 		,OS.strOrderStatus
 		,OH.strComment
+		,US.strUserName
 	FROM tblMFOrderHeader OH
 	JOIN tblMFStageWorkOrder SW ON SW.intOrderHeaderId = OH.intOrderHeaderId
 	JOIN tblMFOrderStatus OS ON OS.intOrderStatusId = OH.intOrderStatusId
+	JOIN tblSMUserSecurity US ON US.intEntityId = OH.intCreatedById
 	WHERE SW.intWorkOrderId = @intWorkOrderId
 		AND OH.intOrderTypeId = 1 -- WO PROD STAGING
 END
