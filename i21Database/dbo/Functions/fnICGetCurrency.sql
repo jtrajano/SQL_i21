1cdfd5dee7165c8f1fee71c0096cc34239e4f3e1
Feb Montefrio <feb.montefrio@irely.com>
2018-05-29 09:37:49 +0800

    [B] IC-5428

--- i21Database/dbo/Functions/fnICGetCurrency.sql
+++ i21Database/dbo/Functions/fnICGetCurrency.sql
@@ -4,28 +4,23 @@ AS
 BEGIN
 	DECLARE @defaultCurrency INT = NULL
 
-	IF @ysnSubCurrency=1
-		BEGIN
-			SELECT @defaultCurrency = 
-					CASE 
-						WHEN ContractDetail.ysnUseFXPrice = 1 THEN ContractDetail.intSeqCurrencyId			
-						ELSE ISNULL(ISNULL(ContractDetail.intCurrencyId, ContractDetail.intMainCurrencyId), dbo.fnSMGetDefaultCurrency('FUNCTIONAL'))
-					END
-			FROM vyuCTContractDetailView ContractDetail 
-			WHERE ContractDetail.intContractDetailId = @contractDetailId
-		END
-	ELSE
-		BEGIN
-			SELECT @defaultCurrency = 
-					CASE 
-						WHEN ContractDetail.ysnUseFXPrice = 1
-						THEN ContractDetail.intSeqCurrencyId			
-						ELSE ISNULL(ISNULL(ContractDetail.intMainCurrencyId, ContractDetail.intCurrencyId), dbo.fnSMGetDefaultCurrency('FUNCTIONAL'))
-					END
-			FROM vyuCTContractDetailView ContractDetail 
-			WHERE ContractDetail.intContractDetailId = @contractDetailId
-		END
-
+	SELECT	@defaultCurrency = 
+				CASE 
+					WHEN ContractDetail.ysnUseFXPrice = 1 THEN 
+						ContractDetail.intSeqCurrencyId			
+					ELSE 
+						ISNULL(
+							CASE 
+								WHEN @ysnSubCurrency = 1 THEN 
+									ISNULL(ContractDetail.intCurrencyId, ContractDetail.intMainCurrencyId)
+								ELSE 
+									ISNULL(ContractDetail.intMainCurrencyId, ContractDetail.intCurrencyId)
+							END 
+							, dbo.fnSMGetDefaultCurrency('FUNCTIONAL')
+						)
+				END
+	FROM	vyuCTContractDetailView ContractDetail 
+	WHERE	ContractDetail.intContractDetailId = @contractDetailId
 	
 	RETURN @defaultCurrency
 END
