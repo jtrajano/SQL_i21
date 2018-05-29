9464964725e9000ca68c45886919974fa05fb328
IRELY\swghosh <swetosuvro.g@irely.com>
2018-05-29 20:04:49 +0530

    LG-1480

--- i21Database/dbo/Functions/fnLGGetTotalAmountPerLoad.sql
+++ i21Database/dbo/Functions/fnLGGetTotalAmountPerLoad.sql
@@ -7,6 +7,7 @@ RETURNS @returntable TABLE (
 	,strAmountCurrency NVARCHAR(100) COLLATE Latin1_General_CI_AS
 	,dblTotalCostPerContainer NUMERIC(18, 6)
 	,intContainerRateCurrency INT
+	,strContainerRateCurrency NVARCHAR(100) COLLATE Latin1_General_CI_AS
 	,dblCurrencyExchangeRate NUMERIC(18, 6)
 	,dblBrokerage NUMERIC(18, 6)
 	)
@@ -21,18 +22,37 @@ BEGIN
 		,@dtmScheduledDate DATETIME
 		,@dblTotalCostPerContainer NUMERIC(26, 16)
 		,@intTotalCostPerContainerCurrency INT
+		,@strOriginPort NVARCHAR(100)
+		,@strDestinationCity NVARCHAR(100)
+		,@strTotalCostPerContainerCurrency NVARCHAR(100)
+		,@dblCurrencyExchangeRate NUMERIC(26,16)
 
 	SELECT @intShippingLineEntityId = intShippingLineEntityId
 		,@dtmScheduledDate = dtmScheduledDate
+		,@strOriginPort = strOriginPort
+		,@strDestinationCity = strDestinationCity 
 	FROM tblLGLoad
 	WHERE intLoadId = @intLoadId
 
 	SELECT @dblTotalCostPerContainer = dblTotalCostPerContainer
 		,@intTotalCostPerContainerCurrency = intCurrencyId
-	FROM tblLGFreightRateMatrix
+		,@strTotalCostPerContainerCurrency = CU.strCurrency
+	FROM tblLGFreightRateMatrix FRM
+	JOIN tblSMCurrency CU ON CU.intCurrencyID = FRM.intCurrencyId
 	WHERE intEntityId = @intShippingLineEntityId
+		AND strOriginPort = @strOriginPort
+		AND strDestinationCity = @strDestinationCity
 		AND @dtmScheduledDate BETWEEN dtmValidFrom
 			AND dtmValidTo
+	
+	SELECT @dblCurrencyExchangeRate = CASE 
+			WHEN ISNULL(CD.intInvoiceCurrencyId,0) = @intTotalCostPerContainerCurrency
+				THEN dblRate
+			ELSE 1
+			END
+	FROM tblCTContractDetail CD
+	JOIN tblLGLoadDetail LD ON LD.intPContractDetailId = CD.intContractDetailId
+	WHERE LD.intLoadId = @intLoadId
 
 	INSERT INTO @returntable
 	SELECT SUM(dblTotalAmount) AS dblTotalAmount
@@ -40,8 +60,9 @@ BEGIN
 		,intAmountCurrency
 		,strAmountCurrency
 		,ISNULL(@dblTotalCostPerContainer, 0)
-		,@intTotalCostPerContainerCurrency
-		,dblCurrencyExchangeRate
+		,ISNULL(@intTotalCostPerContainerCurrency,intAmountCurrency)
+		,ISNULL(@strTotalCostPerContainerCurrency,strAmountCurrency)
+		,ISNULL(dblCurrencyExchangeRate,1)
 		,SUM(dblBrokerage * dblTotalAmount)
 	FROM (
 		SELECT dblNet
@@ -71,7 +92,7 @@ BEGIN
 					THEN MCU.strCurrency
 				ELSE CU.strCurrency
 				END
-			,dblCurrencyExchangeRate = 1
+			,dblCurrencyExchangeRate = @dblCurrencyExchangeRate
             ,dblBrokerage = (
 								SELECT SUM(CASE 
 											WHEN  BCU.ysnSubCurrency = 1
@@ -86,6 +107,7 @@ BEGIN
 									AND I.strCostType = 'Commission'
 							)
 		FROM tblLGLoadDetail LD
+		JOIN tblLGLoad L ON L.intLoadId = LD.intLoadId 
 		JOIN tblCTContractDetail CD ON CD.intContractDetailId = LD.intPContractDetailId
 		JOIN tblICItemUOM IU ON IU.intItemUOMId = LD.intWeightItemUOMId
 		JOIN tblICUnitMeasure UM ON UM.intUnitMeasureId = IU.intUnitMeasureId
