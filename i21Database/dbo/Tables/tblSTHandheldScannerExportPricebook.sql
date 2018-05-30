045aa14b17a056652c807c3c910a59f159e0264c
Lawrence Zabala <lawrence.zabala@irely.com>
2018-05-30 11:11:59 +0800

    ST-639

---
+++ i21Database/dbo/Tables/tblSTHandheldScannerExportPricebook.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[tblSTHandheldScannerExportPricebook]
+(
+	[intHandheldScannerExportPricebookId] INT NOT NULL IDENTITY, 
+    [intHandheldScannerId] INT NOT NULL, 
+    [strUPCNo] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL,
+	[strCaseUPC] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL,
+	[strPOSDescription] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL,
+	[intCaseSize] INT NULL,
+	[dblLastCaseCost] NUMERIC(18, 6) NULL DEFAULT ((0)),
+	[dblUnitPrice] NUMERIC (18, 6) NULL DEFAULT ((0)),
+	[strItemUOM] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL,
+	[strVendorId] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL,
+	[strDeptNo] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL,
+	[dblOnHandQty] NUMERIC(18, 6) NULL DEFAULT ((0)),
+	[intConcurrencyId] INT NULL DEFAULT ((1)), 
+    CONSTRAINT [PK_tblSTHandheldScannerExportPricebook] PRIMARY KEY ([intHandheldScannerExportPricebookId]), 
+    CONSTRAINT [FK_tblSTHandheldScannerExportPricebook_tblSTHandheldScanner] FOREIGN KEY ([intHandheldScannerId]) REFERENCES [tblSTHandheldScanner]([intHandheldScannerId]) ON DELETE CASCADE 
+)
