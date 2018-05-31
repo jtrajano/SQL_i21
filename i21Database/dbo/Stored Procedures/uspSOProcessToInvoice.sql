939807783c1882a124fdb9fa0a9935200a31980f
Robin Panganiban <robin.panganiban@irely.com>
2018-05-31 15:58:22 +0800

    AR-7549

--- i21Database/dbo/Stored Procedures/uspSOProcessToInvoice.sql
+++ i21Database/dbo/Stored Procedures/uspSOProcessToInvoice.sql
@@ -10,7 +10,7 @@ SET ANSI_NULLS ON
 SET NOCOUNT ON  
 SET XACT_ABORT ON  
 SET ANSI_WARNINGS OFF
-
+DECLARE @strErrorMessage NVARCHAR(MAX)
 --VALIDATE IF SO IS ALREADY CLOSED
 IF EXISTS(SELECT NULL FROM tblSOSalesOrder WHERE [intSalesOrderId] = @SalesOrderId AND [strOrderStatus] = 'Closed') 
 	BEGIN
@@ -40,47 +40,51 @@ IF NOT EXISTS (SELECT NULL FROM tblSOSalesOrder SO INNER JOIN vyuARGetSalesOrder
 	END
 ELSE
 	BEGIN
-		--AUTO-BLEND ITEMS
-		DECLARE @strErrorMessage NVARCHAR(MAX)
-
 		BEGIN TRY
-			EXEC dbo.uspARAutoBlendSalesOrderItems @intSalesOrderId = @SalesOrderId, @intUserId = @UserId
+			BEGIN TRANSACTION
+				--AUTO-BLEND ITEMS
+				
+				EXEC dbo.uspARAutoBlendSalesOrderItems @intSalesOrderId = @SalesOrderId, @intUserId = @UserId
+						
+				--CONVERT PROSPECT TO CUSTOMER
+				DECLARE @intEntityCustomerId	INT
+					, @intTermsId				INT
+
+				SELECT @intEntityCustomerId = intEntityCustomerId
+					, @intTermsId			= intTermId
+				FROM dbo.tblSOSalesOrder WITH(NOLOCK) 
+				where intSalesOrderId = @SalesOrderId
+
+				IF EXISTS(SELECT top 1 1 FROM tblEMEntityType WITH(NOLOCK) WHERE intEntityId = @intEntityCustomerId AND strType = 'Prospect')
+					AND NOT EXISTS(SELECT TOP 1 1 FROM tblEMEntityType WITH(NOLOCK) WHERE intEntityId = @intEntityCustomerId AND strType = 'Customer')
+				BEGIN
+					UPDATE tblEMEntityType 
+					SET strType = 'Customer'
+					WHERE intEntityId = @intEntityCustomerId 
+					AND strType = 'Prospect'
+					AND NOT EXISTS (SELECT top 1 1 FROM tblEMEntityType WHERE intEntityId = @intEntityCustomerId AND strType ='Customer')
+
+					UPDATE tblARCustomer 
+					SET intTermsId = @intTermsId 
+					WHERE intEntityId = @intEntityCustomerId
+				END
+
+
+				--INSERT TO INVOICE
+				EXEC dbo.uspARInsertToInvoice @SalesOrderId, @UserId, NULL, 0, @NewInvoiceId OUTPUT
+			
+				IF ISNULL(@NewInvoiceId, 0) > 0
+					EXEC dbo.uspARUpdateGrainOpenBalance @NewInvoiceId, 0, @UserId
+			COMMIT TRANSACTION
 		END TRY
 		BEGIN CATCH
+			IF XACT_STATE() != 0 AND @@TRANCOUNT > 0 
+				ROLLBACK TRANSACTION      
+
 			SET @strErrorMessage = ERROR_MESSAGE()
 			RAISERROR(@strErrorMessage, 11, 1)
 			RETURN
 		END CATCH
-		
-		--CONVERT PROSPECT TO CUSTOMER
-		DECLARE @intEntityCustomerId	INT
-			  , @intTermsId				INT
-
-		SELECT @intEntityCustomerId = intEntityCustomerId
-			 , @intTermsId			= intTermId
-		FROM dbo.tblSOSalesOrder WITH(NOLOCK) 
-		where intSalesOrderId = @SalesOrderId
-
-		IF EXISTS(SELECT top 1 1 FROM tblEMEntityType WITH(NOLOCK) WHERE intEntityId = @intEntityCustomerId AND strType = 'Prospect')
-			AND NOT EXISTS(SELECT TOP 1 1 FROM tblEMEntityType WITH(NOLOCK) WHERE intEntityId = @intEntityCustomerId AND strType = 'Customer')
-		BEGIN
-			UPDATE tblEMEntityType 
-			SET strType = 'Customer'
-			WHERE intEntityId = @intEntityCustomerId 
-			 AND strType = 'Prospect'
-			 AND NOT EXISTS (SELECT top 1 1 FROM tblEMEntityType WHERE intEntityId = @intEntityCustomerId AND strType ='Customer')
-
-			UPDATE tblARCustomer 
-			SET intTermsId = @intTermsId 
-			WHERE intEntityId = @intEntityCustomerId
-		END
-
-
-		--INSERT TO INVOICE
-		EXEC dbo.uspARInsertToInvoice @SalesOrderId, @UserId, NULL, 0, @NewInvoiceId OUTPUT
-		
-		IF ISNULL(@NewInvoiceId, 0) > 0
-			EXEC dbo.uspARUpdateGrainOpenBalance @NewInvoiceId, 0, @UserId
 	END
 
 END
