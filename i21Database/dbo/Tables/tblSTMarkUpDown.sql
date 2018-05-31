cc9e53511d8f32076460a5e219bde11612e39dba
COMTECH\HZaragoza <henry.zaragoza@irely.com>
2018-06-01 07:47:09 +0800

    ST-751 http://jira.irelyserver.com/browse/ST-751

--- i21Database/dbo/Tables/tblSTMarkUpDown.sql
+++ i21Database/dbo/Tables/tblSTMarkUpDown.sql
@@ -11,7 +11,7 @@
 	[strBatchId] NVARCHAR(200) COLLATE Latin1_General_CI_AS NULL,
     [intConcurrencyId] INT NULL,
     CONSTRAINT [PK_tblSTMarkUpDown] PRIMARY KEY CLUSTERED ([intMarkUpDownId] ASC), 
-	CONSTRAINT [AK_tblSTMarkUpDown_strMarkUpDownNumber] UNIQUE ([strMarkUpDownNumber]),
+	--CONSTRAINT [AK_tblSTMarkUpDown_strMarkUpDownNumber] UNIQUE ([strMarkUpDownNumber]),
 	CONSTRAINT [AK_tblSTMarkUpDown_intStoreId_dtmMarkUpDownDate_intShiftNo] UNIQUE NONCLUSTERED ([intStoreId],[dtmMarkUpDownDate],[intShiftNo] ASC), 
     CONSTRAINT [FK_tblSTMarkUpDown_tblSTStore] FOREIGN KEY ([intStoreId]) REFERENCES [tblSTStore]([intStoreId]), 
 	CONSTRAINT [FK_tblSTMarkUpDown_tblSTCheckoutHeader] FOREIGN KEY ([intCheckoutId]) REFERENCES [tblSTCheckoutHeader]([intCheckoutId]), 
