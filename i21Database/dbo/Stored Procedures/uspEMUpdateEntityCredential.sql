e9c81845f873042846380f5265df5d5427117767
Dan Jalimao <dan.jalimao@irely.com>
2018-06-01 15:03:40 +0800

    EM-2506

--- i21Database/dbo/Stored Procedures/uspEMUpdateEntityCredential.sql
+++ i21Database/dbo/Stored Procedures/uspEMUpdateEntityCredential.sql
@@ -1,10 +1,11 @@
 ﻿CREATE PROCEDURE [dbo].[uspEMUpdateEntityCredential]
+	@intEntityId  INT,
 	@intEntityContactId  INT,
-	@strUsername AS NVARCHAR(50)
+	@strUsername AS NVARCHAR(50)	
 AS
 BEGIN
 	DECLARE @isDefaultContact AS BIT
-	SET @isDefaultContact = (SELECT ysnDefaultContact FROM tblEMEntityToContact WHERE intEntityContactId = @intEntityContactId)
+	SET @isDefaultContact = (SELECT ysnDefaultContact FROM tblEMEntityToContact WHERE intEntityId = @intEntityId AND intEntityContactId = @intEntityContactId)
 	DECLARE @isExist AS BIT
 	SET @isExist = (SELECT 1 FROM tblEMEntityCredential WHERE intEntityId = @intEntityContactId)
 
