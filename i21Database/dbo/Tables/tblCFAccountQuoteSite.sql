ae0b9b6468287e08b92aa6b4b838eb7144c0a5d0
COMTECH\BPonce <bonn.ponce@irely.com>
2018-06-01 13:09:46 +0800

    CF-1376

--- i21Database/dbo/Tables/tblCFAccountQuoteSite.sql
+++ i21Database/dbo/Tables/tblCFAccountQuoteSite.sql
@@ -1,9 +1,11 @@
-﻿CREATE TABLE [dbo].[tblCFAccountQuoteSite](
-	[intAccountQuoteSiteId] INT IDENTITY(1,1) NOT NULL,
-	[intSiteId] INT  NOT NULL,
-	[intAccountId] INT  NOT NULL,
-	[intConcurrencyId] INT CONSTRAINT [DF_tblCFAccountQuoteSite_intConcurrencyId] DEFAULT ((1)) NOT NULL,
-	CONSTRAINT [PK_tblCFAccountQuoteSite] PRIMARY KEY CLUSTERED ([intAccountQuoteSiteId] ASC),
-	CONSTRAINT [UQ_tblCFAccountQuoteSite_intCategoryId] UNIQUE ([intSiteId]), 
+﻿CREATE TABLE [dbo].[tblCFAccountQuoteSite] (
+    [intAccountQuoteSiteId] INT IDENTITY (1, 1) NOT NULL,
+    [intSiteId]             INT NOT NULL,
+    [intAccountId]          INT NOT NULL,
+    [intConcurrencyId]      INT CONSTRAINT [DF_tblCFAccountQuoteSite_intConcurrencyId] DEFAULT ((1)) NOT NULL,
+    CONSTRAINT [PK_tblCFAccountQuoteSite] PRIMARY KEY CLUSTERED ([intAccountQuoteSiteId] ASC),
+    CONSTRAINT [UQ_tblCFAccountQuoteSite_intAccountId_intSiteId] UNIQUE NONCLUSTERED ([intSiteId] ASC, [intAccountId] ASC)
 );
+
+
 GO
