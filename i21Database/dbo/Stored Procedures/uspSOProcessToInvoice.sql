7d6048460462d5c678fea214314d82106aabbc4f
Robin Panganiban <robin.panganiban@irely.com>
2018-06-07 12:19:12 +0800

    AR-7673

--- i21Database/dbo/Stored Procedures/uspSOProcessToInvoice.sql
+++ i21Database/dbo/Stored Procedures/uspSOProcessToInvoice.sql
@@ -8,9 +8,9 @@ BEGIN
 SET QUOTED_IDENTIFIER OFF  
 SET ANSI_NULLS ON  
 SET NOCOUNT ON  
-SET XACT_ABORT ON  
+SET XACT_ABORT OFF  
 SET ANSI_WARNINGS OFF
-DECLARE @strErrorMessage NVARCHAR(MAX)
+
 --VALIDATE IF SO IS ALREADY CLOSED
 IF EXISTS(SELECT NULL FROM tblSOSalesOrder WHERE [intSalesOrderId] = @SalesOrderId AND [strOrderStatus] = 'Closed') 
 	BEGIN
@@ -34,51 +34,47 @@ IF NOT EXISTS (SELECT NULL FROM tblSOSalesOrder SO INNER JOIN vyuARGetSalesOrder
 	END
 ELSE
 	BEGIN
+		--AUTO-BLEND ITEMS
+		DECLARE @strErrorMessage NVARCHAR(MAX)
+
 		BEGIN TRY
-			BEGIN TRANSACTION
-				--AUTO-BLEND ITEMS
-				
-				EXEC dbo.uspARAutoBlendSalesOrderItems @intSalesOrderId = @SalesOrderId, @intUserId = @UserId
-						
-				--CONVERT PROSPECT TO CUSTOMER
-				DECLARE @intEntityCustomerId	INT
-					, @intTermsId				INT
-
-				SELECT @intEntityCustomerId = intEntityCustomerId
-					, @intTermsId			= intTermId
-				FROM dbo.tblSOSalesOrder WITH(NOLOCK) 
-				where intSalesOrderId = @SalesOrderId
-
-				IF EXISTS(SELECT top 1 1 FROM tblEMEntityType WITH(NOLOCK) WHERE intEntityId = @intEntityCustomerId AND strType = 'Prospect')
-					AND NOT EXISTS(SELECT TOP 1 1 FROM tblEMEntityType WITH(NOLOCK) WHERE intEntityId = @intEntityCustomerId AND strType = 'Customer')
-				BEGIN
-					UPDATE tblEMEntityType 
-					SET strType = 'Customer'
-					WHERE intEntityId = @intEntityCustomerId 
-					AND strType = 'Prospect'
-					AND NOT EXISTS (SELECT top 1 1 FROM tblEMEntityType WHERE intEntityId = @intEntityCustomerId AND strType ='Customer')
-
-					UPDATE tblARCustomer 
-					SET intTermsId = @intTermsId 
-					WHERE intEntityId = @intEntityCustomerId
-				END
-
-
-				--INSERT TO INVOICE
-				EXEC dbo.uspARInsertToInvoice @SalesOrderId, @UserId, NULL, 0, @NewInvoiceId OUTPUT
-			
-				IF ISNULL(@NewInvoiceId, 0) > 0
-					EXEC dbo.uspARUpdateGrainOpenBalance @NewInvoiceId, 0, @UserId
-			COMMIT TRANSACTION
+			EXEC dbo.uspARAutoBlendSalesOrderItems @intSalesOrderId = @SalesOrderId, @intUserId = @UserId
 		END TRY
 		BEGIN CATCH
-			IF XACT_STATE() != 0 AND @@TRANCOUNT > 0 
-				ROLLBACK TRANSACTION      
-
 			SET @strErrorMessage = ERROR_MESSAGE()
 			RAISERROR(@strErrorMessage, 11, 1)
 			RETURN
 		END CATCH
+		
+		--CONVERT PROSPECT TO CUSTOMER
+		DECLARE @intEntityCustomerId	INT
+			  , @intTermsId				INT
+
+		SELECT @intEntityCustomerId = intEntityCustomerId
+			 , @intTermsId			= intTermId
+		FROM dbo.tblSOSalesOrder WITH(NOLOCK) 
+		where intSalesOrderId = @SalesOrderId
+
+		IF EXISTS(SELECT top 1 1 FROM tblEMEntityType WITH(NOLOCK) WHERE intEntityId = @intEntityCustomerId AND strType = 'Prospect')
+			AND NOT EXISTS(SELECT TOP 1 1 FROM tblEMEntityType WITH(NOLOCK) WHERE intEntityId = @intEntityCustomerId AND strType = 'Customer')
+		BEGIN
+			UPDATE tblEMEntityType 
+			SET strType = 'Customer'
+			WHERE intEntityId = @intEntityCustomerId 
+			 AND strType = 'Prospect'
+			 AND NOT EXISTS (SELECT top 1 1 FROM tblEMEntityType WHERE intEntityId = @intEntityCustomerId AND strType ='Customer')
+
+			UPDATE tblARCustomer 
+			SET intTermsId = @intTermsId 
+			WHERE intEntityId = @intEntityCustomerId
+		END
+
+
+		--INSERT TO INVOICE
+		EXEC dbo.uspARInsertToInvoice @SalesOrderId, @UserId, NULL, 0, @NewInvoiceId OUTPUT
+		
+		IF ISNULL(@NewInvoiceId, 0) > 0
+			EXEC dbo.uspARUpdateGrainOpenBalance @NewInvoiceId, 0, @UserId
 	END
 
 END
--- i21Database/dbo/Stored Procedures/uspSOProcessToItemShipment.sql
+++ i21Database/dbo/Stored Procedures/uspSOProcessToItemShipment.sql
@@ -1,339 +1,80 @@
-﻿CREATE PROCEDURE [dbo].[uspSOProcessToItemShipment]
-	@SalesOrderId			INT,
-	@UserId					INT,
-	@Unship					BIT = 0,
-	@InventoryShipmentId	INT OUTPUT 
+﻿CREATE PROCEDURE [dbo].[uspSOProcessToInvoice]
+	@SalesOrderId		INT,
+	@UserId				INT,
+	@NewInvoiceId		INT = NULL OUTPUT
 AS
 BEGIN
 
-SET QUOTED_IDENTIFIER OFF
-SET ANSI_NULLS ON
-SET NOCOUNT ON
-SET XACT_ABORT ON
+SET QUOTED_IDENTIFIER OFF  
+SET ANSI_NULLS ON  
+SET NOCOUNT ON  
+SET XACT_ABORT ON  
 SET ANSI_WARNINGS OFF
-BEGIN TRY
-	BEGIN TRANSACTION
-	DECLARE @strErrorMessage	NVARCHAR(MAX)
-	DECLARE	 @ShipmentId INT
-			,@InvoiceId  INT = 0	    
 
-	--VALIDATE IF SO IS ALREADY CLOSED
-	IF EXISTS(SELECT NULL FROM tblSOSalesOrder WHERE [intSalesOrderId] = @SalesOrderId AND [strOrderStatus] = 'Closed' AND @Unship = 0)
-		BEGIN
-			RAISERROR('Sales Order already closed.', 16, 1)
-			RETURN;
-		END
-
-	--VALIDATE IF SO HAS ZERO TOTAL AMOUNT
-	IF EXISTS(SELECT NULL FROM tblSOSalesOrder WHERE [intSalesOrderId] = @SalesOrderId AND [dblSalesOrderTotal]  = 0 AND @Unship = 0)
-		BEGIN
-			RAISERROR('Cannot process Sales Order with zero(0) amount.', 16, 1)
-			RETURN;
-		END
-
-	--IF UNSHIP
-	IF @Unship = 1
-		BEGIN
-			--VALIDATE IF SO HAS POSTED SHIPMENT RECORDS
-			DECLARE @shipmentNos NVARCHAR(MAX) = NULL
-
-			SELECT @shipmentNos = COALESCE(@shipmentNos + ', ' ,'') + ISH.strShipmentNumber 
-			FROM (
-				SELECT intInventoryShipmentId
-				FROM dbo.tblICInventoryShipmentItem WITH (NOLOCK)
-				WHERE intOrderId = @SalesOrderId
-				GROUP BY intInventoryShipmentId
-			) ISHI 
-			INNER JOIN (
-				SELECT intInventoryShipmentId
-					 , strShipmentNumber
-				FROM dbo.tblICInventoryShipment WITH (NOLOCK)
-				WHERE ysnPosted = 1
-			) ISH ON ISHI.intInventoryShipmentId = ISH.intInventoryShipmentId
-
-			IF ISNULL(@shipmentNos, '') <> ''
-				BEGIN				
-					RAISERROR('Failed to unship Sales Order. Unpost this Shipment Record first: %s', 16, 1, @shipmentNos)
-					RETURN
-				END
-			ELSE
-				BEGIN
-					-- Delete shipment and decrease Item Stock Reservation
-					BEGIN
-						DECLARE @intInventoryShipmentId INT
-						SELECT DISTINCT TOP 1 @intInventoryShipmentId = ISH.intInventoryShipmentId
-						FROM tblICInventoryShipmentItem ISHI
-							INNER JOIN tblICInventoryShipment ISH ON ISHI.intInventoryShipmentId = ISH.intInventoryShipmentId
-						WHERE intOrderId = @SalesOrderId
-					
-						EXEC dbo.uspICUnshipInventoryItem @intInventoryShipmentId, @UserId
-						EXEC dbo.uspSOUpdateOrderShipmentStatus @intInventoryShipmentId, 'Inventory', 1
-					END
-			
-					UPDATE tblSOSalesOrder SET ysnShipped = 0 WHERE intSalesOrderId = @SalesOrderId
-					RETURN 1
-				END
-		END
-
-	--VALIDATE IF THERE ARE STOCK ITEMS TO SHIP
-	IF(OBJECT_ID('tempdb..#ITEMSTOSHIP') IS NOT NULL)
+--VALIDATE IF SO IS ALREADY CLOSED
+IF EXISTS(SELECT NULL FROM tblSOSalesOrder WHERE [intSalesOrderId] = @SalesOrderId AND [strOrderStatus] = 'Closed') 
 	BEGIN
-		DROP TABLE #ITEMSTOSHIP
+		RAISERROR('Sales Order already closed.', 16, 1)
+		RETURN;
 	END
 
-	SELECT SOD.intItemId
-		 , IC.strBundleType
-		 , IC.strType
-	INTO #ITEMSTOSHIP
-	FROM tblSOSalesOrderDetail SOD
-	INNER JOIN tblICItem IC ON SOD.intItemId = IC.intItemId 
-	WHERE intSalesOrderId = @SalesOrderId 
-	AND (dbo.fnIsStockTrackingItem(SOD.intItemId) = 1 OR (dbo.fnIsStockTrackingItem(SOD.intItemId) = 0 AND (IC.strType = 'Bundle' AND ISNULL(IC.strBundleType, 'Kit') = 'Kit')))
-	AND (dblQtyOrdered - dblQtyShipped > 0)
+--VALIDATE IF SO HAS ZERO TOTAL AMOUNT
+IF EXISTS(SELECT NULL FROM tblSOSalesOrder WHERE [intSalesOrderId] = @SalesOrderId AND [dblSalesOrderTotal]  = 0)
+	BEGIN
+		RAISERROR('Cannot process Sales Order with zero(0) amount.', 16, 1)
+		RETURN;
+	END
 
-	IF NOT EXISTS(SELECT TOP 1 NULL FROM #ITEMSTOSHIP)
-		BEGIN
-			RAISERROR('Shipping Failed. There is no shippable item on this sales order.', 16, 1);
-			RETURN
-		END
-	ELSE
-		BEGIN
-			--AUTO-BLEND ITEMS
+--VALIDATE IF HAS NON-STOCK ITEMS
+IF NOT EXISTS (SELECT NULL FROM tblSOSalesOrder SO INNER JOIN vyuARGetSalesOrderItems SI ON SO.intSalesOrderId = SI.intSalesOrderId
+				LEFT JOIN tblICItem I ON SI.intItemId = I.intItemId WHERE ISNULL(I.strLotTracking, 'No') = 'No' AND SO.intSalesOrderId = @SalesOrderId AND SI.dblQtyRemaining > 0)
+	BEGIN
+		RAISERROR('Process To Invoice Failed. There is no item to process to Invoice.', 16, 1);
+        RETURN;
+	END
+ELSE
+	BEGIN
+		--AUTO-BLEND ITEMS
+		DECLARE @strErrorMessage NVARCHAR(MAX)
 
+		BEGIN TRY
 			EXEC dbo.uspARAutoBlendSalesOrderItems @intSalesOrderId = @SalesOrderId, @intUserId = @UserId
-	 
-			DECLARE @Items				ShipmentStagingTable
-				  , @Charges			ShipmentChargeStagingTable
-				  , @Lots				ShipmentItemLotStagingTable			  
-				  , @SALES_ORDER		NVARCHAR(50) = 'Sales Order'
-				  , @SALES_ORDER_TYPE	INT = 2
-				  , @intReturn			INT 
-
-			INSERT INTO @Items (
-				  intOrderType 
-				, intSourceType
-				, intEntityCustomerId
-				, dtmShipDate
-				, intShipFromLocationId
-				, intShipToLocationId
-				, intFreightTermId
-				, strSourceScreenName
-				, strReferenceNumber
-				, dtmRequestedArrivalDate
-				, intShipToCompanyLocationId
-				, strBOLNumber
-				, intShipViaId
-				, strVessel
-				, strProNumber
-				, strDriverId
-				, strSealNumber
-				, strDeliveryInstruction
-				, dtmAppointmentTime
-				, dtmDepartureTime
-				, dtmArrivalTime
-				, dtmDeliveredDate
-				, dtmFreeTime
-				, strFreeTime
-				, strReceivedBy
-				, strComment
-				, intItemId
-				, intOwnershipType
-				, dblQuantity
-				, intItemUOMId
-				, intPriceUOMId
-				, intItemLotGroup
-				, intOrderId
-				, intSourceId
-				, intLineNo
-				, intSubLocationId
-				, intStorageLocationId
-				, intCurrencyId
-				, intWeightUOMId
-				, dblUnitPrice
-				, intDockDoorId
-				, strNotes
-				, intGradeId
-				, intDiscountSchedule
-				, intStorageScheduleTypeId
-				, intForexRateTypeId
-				, dblForexRate	
-			)
-			-- Insert stock keeping items, except kit items. 
-			SELECT intOrderType					= @SALES_ORDER_TYPE
-				, intSourceType					= 0
-				, intEntityCustomerId			= SO.intEntityCustomerId
-				, dtmShipDate					= SO.dtmDate
-				, intShipFromLocationId			= SO.intCompanyLocationId
-				, intShipToLocationId			= SO.intShipToLocationId
-				, intFreightTermId				= SO.intFreightTermId
-				, strSourceScreenName			= @SALES_ORDER
-				, strReferenceNumber			= SO.strSalesOrderNumber
-				, dtmRequestedArrivalDate		= NULL
-				, intShipToCompanyLocationId	= NULL
-				, strBOLNumber					= SO.strBOLNumber
-				, intShipViaId					= SO.intShipViaId
-				, strVessel						= NULL
-				, strProNumber					= NULL
-				, strDriverId					= NULL
-				, strSealNumber					= NULL
-				, strDeliveryInstruction		= NULL
-				, dtmAppointmentTime			= NULL
-				, dtmDepartureTime				= NULL
-				, dtmArrivalTime				= NULL
-				, dtmDeliveredDate				= NULL
-				, dtmFreeTime					= NULL
-				, strFreeTime					= NULL
-				, strReceivedBy					= NULL
-				, strComment					= SO.strComments
-				, intItemId						= SODETAIL.intItemId
-				, intOwnershipType				= CASE WHEN SODETAIL.intStorageScheduleTypeId IS NULL THEN 1 ELSE 2 END
-				, dblQuantity					= SODETAIL.dblQtyOrdered - ISNULL(INVOICEDETAIL.dblQtyShipped, SODETAIL.dblQtyShipped)
-				, intItemUOMId					= ITEMUOM.intItemUOMId
-				, intPriceUOMId					= SODETAIL.intItemUOMId
-				, intItemLotGroup				= NULL
-				, intOrderId					= SODETAIL.intSalesOrderId
-				, intSourceId					= NULL
-				, intLineNo						= SODETAIL.intSalesOrderDetailId
-				, intSubLocationId				= COALESCE(SODETAIL.intSubLocationId, STORAGELOCATION.intSubLocationId, ITEMLOCATION.intSubLocationId)
-				, intStorageLocationId			= COALESCE(SODETAIL.intStorageLocationId, ITEMLOCATION.intStorageLocationId)
-				, intCurrencyId					= SO.intCurrencyId
-				, intWeightUOMId				= NULL
-				, dblUnitPrice					= SODETAIL.dblPrice
-				, intDockDoorId					= NULL
-				, strNotes						= SODETAIL.strComments
-				, intGradeId					= NULL
-				, intDiscountSchedule			= NULL
-				, intStorageScheduleTypeId		= SODETAIL.intStorageScheduleTypeId
-				, intForexRateTypeId			= SODETAIL.intCurrencyExchangeRateTypeId
-				, dblForexRate					= SODETAIL.dblCurrencyExchangeRate
-		FROM dbo.tblSOSalesOrder SO	
-		INNER JOIN dbo.tblSOSalesOrderDetail SODETAIL 
-			ON SO.intSalesOrderId = SODETAIL.intSalesOrderId
-		INNER JOIN tblICItem ITEM
-			ON ITEM.intItemId = SODETAIL.intItemId 
-			AND ((ITEM.strType <> 'Bundle' AND dbo.fnIsStockTrackingItem(SODETAIL.intItemId) = 1) 
-			  OR (ITEM.strType = 'Bundle' AND ISNULL(ITEM.strBundleType, 'Kit') = 'Kit'))
-			AND  CASE WHEN (ITEM.strType = 'Bundle') THEN 
-					CASE WHEN ISNULL(ITEM.ysnListBundleSeparately, 0) = 0 THEN 1 ELSE 0 END
-				 ELSE 1 END = 1
-		INNER JOIN dbo.tblICItemUOM ITEMUOM 
-			ON ITEMUOM.intItemId = SODETAIL.intItemId 
-			AND ITEMUOM.intItemUOMId = SODETAIL.intItemUOMId 
-		INNER JOIN dbo.tblICItemLocation ITEMLOCATION 
-			ON ITEMLOCATION.intItemId = SODETAIL.intItemId 
-			AND SO.intCompanyLocationId = ITEMLOCATION.intLocationId
-		LEFT JOIN tblSMCompanyLocationSubLocation SUBLOCATION
-			ON SUBLOCATION.intCompanyLocationId = ITEMLOCATION.intLocationId
-			AND SUBLOCATION.intCompanyLocationSubLocationId = SODETAIL.intSubLocationId				
-		LEFT JOIN dbo.tblICStorageLocation STORAGELOCATION 
-			ON STORAGELOCATION.intLocationId = ITEMLOCATION.intLocationId
-			AND STORAGELOCATION.intSubLocationId = SUBLOCATION.intCompanyLocationSubLocationId
-			AND STORAGELOCATION.intStorageLocationId = SODETAIL.intStorageLocationId	
-		OUTER APPLY (
-			SELECT intSalesOrderDetailId
-					, dblQtyShipped = SUM(dblQtyShipped)
-			FROM dbo.tblARInvoiceDetail ID
-			WHERE ID.intSalesOrderDetailId = SODETAIL.intSalesOrderDetailId
-			GROUP BY ID.intSalesOrderDetailId
-		) INVOICEDETAIL
-		WHERE SO.intSalesOrderId = @SalesOrderId
-		  AND (SODETAIL.dblQtyOrdered - ISNULL(INVOICEDETAIL.dblQtyShipped, SODETAIL.dblQtyShipped)) > 0
-
-			INSERT INTO @Charges(
-				  intOrderType
-				, intSourceType
-				, intEntityCustomerId
-				, dtmShipDate
-				, intShipFromLocationId
-				, intShipToLocationId
-				, intFreightTermId
-				, intContractId
-				, intContractDetailId
-				, intChargeId
-				, strCostMethod
-				, dblRate
-				, intCostUOMId
-				, intCurrency
-				, dblAmount
-				, ysnAccrue
-				, intEntityVendorId
-				, ysnPrice
-				, intForexRateTypeId
-				, dblForexRate
-				, strChargesLink
-				, strAllocatePriceBy
-			)
-
-			SELECT intOrderType			= @SALES_ORDER_TYPE
-				, intSourceType			= 0
-				, intEntityCustomerId	= SO.intEntityCustomerId
-				, dtmShipDate			= SO.dtmDate
-				, intShipFromLocationId	= SO.intCompanyLocationId
-				, intShipToLocationId	= SO.intShipToLocationId
-				, intFreightTermId		= SO.intFreightTermId
-				, intContractId			= Header.intContractHeaderId
-				, intContractDetailId	= Detail.intContractDetailId
-				, intChargeId			= Cost.intItemId
-				, strCostMethod			= Cost.strCostMethod
-				, dblRate				= Cost.dblRate
-				, intCostUOMId			= Cost.intItemUOMId
-				, intCurrency			= Cost.intCurrencyId
-				, dblAmount				= NULL
-				, ysnAccrue				= Cost.ysnAccrue
-				, intEntityVendorId		= NULL
-				, ysnPrice				= Cost.ysnPrice
-				, intForexRateTypeId	= SODETAIL.intCurrencyExchangeRateTypeId
-				, dblForexRate			= SODETAIL.dblCurrencyExchangeRate
-				, strChargesLink		= NULL
-				, strAllocatePriceBy	= NULL
-			FROM dbo.tblSOSalesOrder SO	
-			INNER JOIN dbo.tblSOSalesOrderDetail SODETAIL 
-					ON SO.intSalesOrderId = SODETAIL.intSalesOrderId
-			INNER JOIN tblCTContractHeader Header
-				ON Header.intContractHeaderId = SODETAIL.intContractHeaderId
-			INNER JOIN tblCTContractDetail Detail
-				ON Detail.intContractHeaderId = Header.intContractHeaderId
-			INNER JOIN tblCTContractCost Cost
-				ON Detail.intContractDetailId = Cost.intContractDetailId
-			WHERE SO.intSalesOrderId = @SalesOrderId
-
-			IF NOT EXISTS (SELECT 1 FROM tempdb..sysobjects WHERE id = OBJECT_ID('tempdb..#tmpAddItemShipmentResult')) 
-			BEGIN 
-				CREATE TABLE #tmpAddItemShipmentResult (
-					intInventoryShipmentId INT
-				)
-			END 
-
-			EXEC @intReturn = dbo.uspICAddItemShipment @Items		= @Items
-													 , @Charges		= @Charges
-													 , @Lots		= @Lots
-													 , @intUserId	= @UserId	
-				
-			IF @intReturn <> 0 
-				RETURN @intReturn
-
-			SELECT	TOP 1 
-					@InventoryShipmentId = intInventoryShipmentId 
-			FROM	#tmpAddItemShipmentResult
-
-			EXEC dbo.uspSOUpdateOrderShipmentStatus @InventoryShipmentId, 'Inventory', 0
-
-			UPDATE tblSOSalesOrder
-			SET dtmProcessDate = GETDATE()
-			  , ysnProcessed   = 1
-			  , ysnShipped     = 1
-			WHERE intSalesOrderId = @SalesOrderId
-
-			RETURN 1
-		END 
-	COMMIT TRANSACTION
-END TRY
-BEGIN CATCH
-IF XACT_STATE() != 0 AND @@TRANCOUNT > 0 
-				ROLLBACK TRANSACTION      
-
+		END TRY
+		BEGIN CATCH
 			SET @strErrorMessage = ERROR_MESSAGE()
 			RAISERROR(@strErrorMessage, 11, 1)
 			RETURN
-END CATCH
+		END CATCH
+		
+		--CONVERT PROSPECT TO CUSTOMER
+		DECLARE @intEntityCustomerId	INT
+			  , @intTermsId				INT
+
+		SELECT @intEntityCustomerId = intEntityCustomerId
+			 , @intTermsId			= intTermId
+		FROM dbo.tblSOSalesOrder WITH(NOLOCK) 
+		where intSalesOrderId = @SalesOrderId
+
+		IF EXISTS(SELECT top 1 1 FROM tblEMEntityType WITH(NOLOCK) WHERE intEntityId = @intEntityCustomerId AND strType = 'Prospect')
+			AND NOT EXISTS(SELECT TOP 1 1 FROM tblEMEntityType WITH(NOLOCK) WHERE intEntityId = @intEntityCustomerId AND strType = 'Customer')
+		BEGIN
+			UPDATE tblEMEntityType 
+			SET strType = 'Customer'
+			WHERE intEntityId = @intEntityCustomerId 
+			 AND strType = 'Prospect'
+			 AND NOT EXISTS (SELECT top 1 1 FROM tblEMEntityType WHERE intEntityId = @intEntityCustomerId AND strType ='Customer')
+
+			UPDATE tblARCustomer 
+			SET intTermsId = @intTermsId 
+			WHERE intEntityId = @intEntityCustomerId
+		END
+
+
+		--INSERT TO INVOICE
+		EXEC dbo.uspARInsertToInvoice @SalesOrderId, @UserId, NULL, 0, @NewInvoiceId OUTPUT
+		
+		IF ISNULL(@NewInvoiceId, 0) > 0
+			EXEC dbo.uspARUpdateGrainOpenBalance @NewInvoiceId, 0, @UserId
+	END
+
 END
