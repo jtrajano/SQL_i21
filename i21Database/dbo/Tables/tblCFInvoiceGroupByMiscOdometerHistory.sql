d12e9386692f66fb19693dffdbb6bb1afbaf5070
COMTECH\BPonce <bonn.ponce@irely.com>
2018-06-11 19:27:45 +0800

    CF-1698

--- i21Database/dbo/Tables/tblCFInvoiceGroupByMiscOdometerHistory.sql
+++ i21Database/dbo/Tables/tblCFInvoiceGroupByMiscOdometerHistory.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [dbo].[tblCFInvoiceGroupByMiscOdometerHistory] (
-    [strMiscellaneous]        NVARCHAR (MAX) NULL,
+    [strMiscellaneous]        NVARCHAR (MAX)  COLLATE Latin1_General_CI_AS NULL,
     [intAccountId]            INT            NULL,
     [intLastOdometer]         INT            NULL,
     [dtmMinDate]              DATETIME       NULL,
-    [strInvoiceNumberHistory] NVARCHAR (MAX) NULL
+    [strInvoiceNumberHistory] NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NULL,
 );
 
