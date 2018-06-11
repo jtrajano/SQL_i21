578f59f14269299a20ba85abbe5a96d76a817461
COMTECH\BPonce <bonn.ponce@irely.com>
2018-06-11 19:27:45 +0800

    CF-1698

--- i21Database/dbo/Tables/tblCFInvoiceGroupByCardOdometerHistory.sql
+++ i21Database/dbo/Tables/tblCFInvoiceGroupByCardOdometerHistory.sql
@@ -3,6 +3,6 @@
     [intAccountId]            INT            NULL,
     [intLastOdometer]         INT            NULL,
     [dtmMinDate]              DATETIME       NULL,
-    [strInvoiceNumberHistory] NVARCHAR (MAX) NULL
+    [strInvoiceNumberHistory] NVARCHAR (MAX)  COLLATE Latin1_General_CI_AS NULL,
 );
 
