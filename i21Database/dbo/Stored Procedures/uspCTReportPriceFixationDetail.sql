879e8a7fbe60fbf0e18a7522450c124a8f62ee80
Satya S Sahoo <satya.sahoo@irely.com>
2018-06-13 21:00:44 +0530

    CT-2246 - Contract > Sequence - FX Price in FX tab does not show computed price - 18.3Dev

--- i21Database/dbo/Stored Procedures/uspCTReportPriceFixationDetail.sql
+++ i21Database/dbo/Stored Procedures/uspCTReportPriceFixationDetail.sql
@@ -96,11 +96,11 @@ BEGIN TRY
 	JOIN	tblICCommodityUnitMeasure	CU	ON	CU.intCommodityUnitMeasureId	=	PD.intPricingUOMId		LEFT	
 	JOIN	tblICUnitMeasure			CM	ON	CM.intUnitMeasureId				=	CU.intUnitMeasureId
 
-	inner join tblSMScreen				rts on rts.strNamespace = 'RiskManagement.view.FuturesMarket'
+	left join tblSMScreen				rts on rts.strNamespace = 'RiskManagement.view.FuturesMarket'
 	left join tblSMTransaction			rtt on rtt.intScreenId = rts.intScreenId and rtt.intRecordId = MA.intFutureMarketId
 	left join tblSMReportTranslation	rtrt on rtrt.intLanguageId = @intLaguageId and rtrt.intTransactionId = rtt.intTransactionId and rtrt.strFieldName = 'Market Name'
 	
-	inner join tblSMScreen				rts2 on rts2.strNamespace = 'Inventory.view.ReportTranslation'
+	left join tblSMScreen				rts2 on rts2.strNamespace = 'Inventory.view.ReportTranslation'
 	left join tblSMTransaction			rtt2 on rtt2.intScreenId = rts2.intScreenId and rtt2.intRecordId = CM.intUnitMeasureId
 	left join tblSMReportTranslation	rtrt2 on rtrt2.intLanguageId = @intLaguageId and rtrt2.intTransactionId = rtt2.intTransactionId and rtrt2.strFieldName = 'Name'
 			
