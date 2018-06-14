dcc327519835daebfcc6abab7bad52d53ffbf136
COMTECH\BPonce <bonn.ponce@irely.com>
2018-06-14 15:03:53 +0800

    CF-1701

--- i21Database/dbo/Stored Procedures/uspCFInvoiceProcess.sql
+++ i21Database/dbo/Stored Procedures/uspCFInvoiceProcess.sql
@@ -87,7 +87,7 @@ BEGIN TRY
 	--EXEC "dbo"."uspCFInvoiceReportDiscount" @xmlParam=@xmlParam
 	--------------------------------------
 
-	UPDATE tblCFTransaction SET strInvoiceReportNumber = strTempInvoiceReportNumber WHERE intTransactionId IN (SELECT intTransactionId FROM tblCFInvoiceStagingTable WHERE ISNULL(intInvoiceId,0) != 0 AND strUserId = @username) -- AND (strTransactionType != 'Foreign Sale' OR ISNULL(ysnPostForeignSales,0) != 0)
+	UPDATE tblCFTransaction SET strInvoiceReportNumber = strTempInvoiceReportNumber WHERE intTransactionId IN (SELECT intTransactionId FROM tblCFInvoiceStagingTable WHERE strUserId = @username) -- AND (strTransactionType != 'Foreign Sale' OR ISNULL(ysnPostForeignSales,0) != 0)
 	SELECT TOP 1 @dtmInvoiceDate = dtmInvoiceDate FROM #tblCFInvoice
 
 	------------GROUP BY CUSTOMER-----------
