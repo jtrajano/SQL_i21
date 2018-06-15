5863abe7c170dea4ee7665db664c173f5e1ac993
Dan Jalimao <dan.jalimao@irely.com>
2018-06-15 11:18:17 +0800

    SM-4187

--- i21Database/Scripts/Post-Deployment/SM/DefaultData/35_Control.sql
+++ i21Database/Scripts/Post-Deployment/SM/DefaultData/35_Control.sql
@@ -12,6 +12,18 @@ GO
 			VALUES (@entityCustomerId, N'btnDeleteLoc', N'Remove', N'', 'Button')
 		END
 	END
+
+	DECLARE @entityVendorId INT
+	SELECT @entityVendorId = intScreenId FROM tblSMScreen WHERE strNamespace = 'AccountsPayable.view.EntityVendor' AND strScreenName = 'Vendors'
+
+	IF @entityVendorId IS NOT NULL
+	BEGIN
+		IF NOT EXISTS (SELECT TOP 1 1 FROM tblSMControl WHERE intScreenId = @entityVendorId AND strControlId = 'txtVendorAccountNo') 
+		BEGIN
+			INSERT [dbo].[tblSMControl] ([intScreenId],[strControlId],[strControlName],[strContainer],[strControlType])
+			VALUES (@entityVendorId, N'txtVendorAccountNo', N'Vendor Account No', N'', 'Text')
+		END
+	END
 GO
 	PRINT N'END INSERT DEFAULT CONTROL'
 GO
