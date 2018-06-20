925450fe65debfb77b37b31f8d88b89408a45178
Vinoth Kumar T <vinoth.t@irely.com>
2018-06-20 19:40:18 +0530

    MFG-3549

--- i21Database/dbo/Tables/tblMFReportCategory.sql
+++ i21Database/dbo/Tables/tblMFReportCategory.sql
@@ -2,6 +2,7 @@
 	intReportCategoryId INT NOT NULL identity(1, 1) CONSTRAINT PK_tblMFReportCategory_intReportCategoryId PRIMARY KEY
 	,strReportName NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL
 	,intCategoryId INT NOT NULL
+	,intNoOfLabel INT
 	,CONSTRAINT PK_tblMFReportCategory_intCategoryId FOREIGN KEY (intCategoryId) REFERENCES tblICCategory(intCategoryId)
 	,CONSTRAINT UQ_tblMFReportCategory_strReportName_intCategoryId UNIQUE (
 		strReportName
