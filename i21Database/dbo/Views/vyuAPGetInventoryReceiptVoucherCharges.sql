62fd81efcf4909fd16ccaf791ef4696db5a18ff5
Lex Eugene Peregrino <lexeugene14@yahoo.com>
2018-06-20 13:17:42 +0800

    Revert "AP-5744 1.0"
    This reverts commit 3f9cfadb975890525ee29075629559c21031fd23.

--- i21Database/dbo/Views/vyuAPGetInventoryReceiptVoucherCharges.sql
+++ i21Database/dbo/Views/vyuAPGetInventoryReceiptVoucherCharges.sql
@@ -110,7 +110,6 @@ FROM	tblICInventoryReceipt Receipt
 
 			WHERE	rc.intInventoryReceiptId = Receipt.intInventoryReceiptId
 					AND rc.intInventoryReceiptChargeId = ReceiptCharge.intInventoryReceiptChargeId
-					AND ct.intPricingTypeId != 2
 		) receiptAndVoucheredCharges
 		OUTER APPLY (					
 			SELECT	TOP 1 
