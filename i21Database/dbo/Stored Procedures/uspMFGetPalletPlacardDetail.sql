8bcdb54b851e96331b03cbc0d6781a4815556438
Premkumar S <prem.selladurai@irely.com>
2018-06-22 22:24:23 +0530

    MFG-3549

--- i21Database/dbo/Stored Procedures/uspMFGetPalletPlacardDetail.sql
+++ i21Database/dbo/Stored Procedures/uspMFGetPalletPlacardDetail.sql
@@ -1,40 +1,45 @@
-﻿CREATE PROCEDURE uspMFGetPalletPlacardDetail (@intLotId INT = NULL)
+﻿CREATE PROCEDURE uspMFGetPalletPlacardDetail (@strLotNumber nvarchar(50),@intLocationId INT)
 AS
 BEGIN
 	SELECT PL.strParentLotNumber
 		,SUM(L.dblQty) AS dblPhysicalCount
 		,UM.strUnitMeasure
-		,L.intLotId
+		,L.strLotNumber
+		,L.intLocationId
 	FROM tblICLot L
-	JOIN dbo.tblICItemUOM IU ON IU.intItemUOMId = L.intItemUOMId
+	JOIN dbo.tblICItemUOM IU ON IU.intItemUOMId = L.intItemUOMId 
 	JOIN dbo.tblICUnitMeasure UM ON UM.intUnitMeasureId = IU.intUnitMeasureId
 	JOIN tblICParentLot PL ON PL.intParentLotId = L.intParentLotId
-	WHERE L.intLotId = @intLotId
+	WHERE L.strLotNumber = @strLotNumber and L.intLocationId =@intLocationId
 		AND L.ysnProduced = 0
 	GROUP BY PL.strParentLotNumber
 		,UM.strUnitMeasure
-		,L.intLotId
+		,L.strLotNumber
+		,L.intLocationId
 	
 	UNION
 	
 	SELECT DT.strParentLotNumber
 		,DT.dblPhysicalCount
 		,DT.strUnitMeasure
-		,DT.intLotId
+		,DT.strLotNumber
+		,DT.intLocationId
 	FROM (
 		SELECT strParentLotNumber
 			,SUM(WP.dblPhysicalCount) AS dblPhysicalCount
 			,UM.strUnitMeasure
-			,L.intLotId
+			,L.strLotNumber
+			,L.intLocationId
 		FROM tblMFWorkOrderProducedLot WP
 		JOIN tblICLot L ON L.intLotId = WP.intLotId
 			AND WP.ysnProductionReversed = 0
-		JOIN dbo.tblICItemUOM IU ON IU.intItemUOMId = WP.intPhysicalItemUOMId
+		JOIN dbo.tblICItemUOM IU ON IU.intItemUOMId = WP.intPhysicalItemUOMId 
 		JOIN dbo.tblICUnitMeasure UM ON UM.intUnitMeasureId = IU.intUnitMeasureId
-		WHERE L.intLotId = @intLotId
+		WHERE L.strLotNumber = @strLotNumber and L.intLocationId =@intLocationId
 			AND L.ysnProduced = 1
 		GROUP BY strParentLotNumber
 			,UM.strUnitMeasure
-			,L.intLotId
+			,L.strLotNumber
+			,L.intLocationId
 		) AS DT
 END
