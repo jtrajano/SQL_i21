4e2b053023b7ef8a629ab5e18c4450ca6ec9bc7a
Jayson Ramos <jayson.ramos@iRely.com>
2018-06-22 16:40:17 +0800

    HD-2746

--- i21Database/dbo/Views/vyuHDTimeHoursReport.sql
+++ i21Database/dbo/Views/vyuHDTimeHoursReport.sql
@@ -17,8 +17,8 @@
 			,intBilledAmount = (case when isnull(a.ysnBillable, convert(bit,0)) = convert(bit,0) then 0 else a.intHours end) * a.dblRate
 			,intBillableHours = (case when isnull(a.ysnBillable, convert(bit,0)) = convert(bit,0) then 0 else a.intHours end)
 			,intNonBillableHours = (case when isnull(a.ysnBillable, convert(bit,0)) = convert(bit,0) then a.intHours else 0 end)
-			,h.intJobCodeId
-			,h.strJobCode
+			,intJobCodeId = h.intItemId
+			,strJobCode = h.strItemNo
 			,a.dblRate
 			,a.ysnBillable
 			,a.strDescription
@@ -36,7 +36,7 @@
 			left join tblARInvoice e on e.intInvoiceId = a.intInvoiceId
 			left join tblEMEntity f on f.intEntityId = b.intCustomerId
 			left join tblEMEntity g on g.intEntityId = a.intAgentEntityId
-			left join tblHDJobCode h on h.intJobCodeId = a.intJobCodeId
+			left join tblICItem h on h.intItemId = a.intItemId
 			left join tblEMEntity i on i.intEntityId = d.intInternalProjectManager
 			left join tblHDModule j on j.intModuleId = b.intModuleId
 			left join tblSMModule k on k.intModuleId = j.intSMModuleId
