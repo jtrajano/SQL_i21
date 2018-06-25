721a2354ffb840b271b187221e596f4286c13e15
Jayson Ramos <jayson.ramos@iRely.com>
2018-06-25 11:37:09 +0800

    HD-2750

--- i21Database/dbo/Stored Procedures/uspHDGenerateCallDetail.sql
+++ i21Database/dbo/Stored Procedures/uspHDGenerateCallDetail.sql
@@ -162,6 +162,36 @@ INSERT INTO tblHDCallDetail
 		   ,intDaysOutstanding
            ,intConcurrencyId
 		   )
+		select distinct
+				intEntityId = b.intEntityId
+				,strName = ltrim(rtrim(b.strName))
+				,strFirstName = SUBSTRING(ltrim(rtrim(b.strName)),1,CHARINDEX(' ',ltrim(rtrim(b.strName)),1))
+				,intClosedCalls = isnull((select intClosedCalls from closedCalls where intEntityId = b.intEntityId),0)
+				,intOpenCalls = isnull((select intOpenCalls from openCalls where intEntityId = b.intEntityId),0)
+				,intTotalCalls = isnull((select intTotalCalls from totalCalls where intEntityId = b.intEntityId),0)
+				,intReopenCalls = isnull((select intReopenCalls from reopenCalls where intEntityId = b.intEntityId),0)
+				,intStartDate = @DateFrom
+				,intEndDate = @DateTo
+				,strFilterKey = @strIdentifier
+				,intRequestedByEntityId = 0
+				,intCreatedDate = convert(int, convert(nvarchar(8), getdate(), 112))
+				,intTotalBilledHours = isnull((select intTotalBilledHours from billedhours where intEntityId = b.intEntityId),0)
+				,dblTotalBillableAmount = isnull((select dblTotalBillableAmount from billedhours where intEntityId = b.intEntityId),0.00)
+				,intCallsRated = isnull((select intCallsRated from rating where intEntityId = b.intEntityId),0)
+				,dblAverageRating = isnull((select isnull(dblAverageRating,0.00) from rating where intEntityId = b.intEntityId),0.00)
+				,intDaysOutstanding = isnull((select intDaysOutstanding from daysoutstanding where intEntityId = b.intEntityId),0)
+				,intConcurrencyId = 1
+		from
+				tblHDTicket a
+				,tblEMEntity b
+		where
+				a.intAssignedToEntity is not null
+				and a.strType = 'HD'
+				and b.intEntityId = a.intAssignedToEntity
+				and convert(int, convert(nvarchar(8), a.dtmCreated, 112)) between @DateFrom and @DateTo
+		order by
+				strName
+/*
 select
 		intEntityId
 		,strName
@@ -254,6 +284,7 @@ group by
 		,intConcurrencyId
 order by
 		strName
+*/
 
 	exec('IF EXISTS(select * FROM sys.views where name = ''vyuHDCallDetail'') begin drop view vyuHDCallDetail; end');
 	exec('
