0e6fcdf34512fbd52f4fe21c29c832b1c12660c9
Kelvin Quinto <kelvin.quinto@irely.com>
2018-06-28 12:15:43 +0800

    AR-7924

--- i21Database/dbo/Views/vyuARInvoicePaymentInfo.sql
+++ i21Database/dbo/Views/vyuARInvoicePaymentInfo.sql
@@ -1,57 +1,56 @@
 ﻿CREATE VIEW [dbo].[vyuARInvoicePaymentInfo]
 AS
-SELECT I.intInvoiceId
-	   , I.strInvoiceNumber
-	   , I.intEntityCustomerId 
-	   , strCustomerName = C.strName
-	   , strCustomerNumber = C.strCustomerNumber
-	   , I.intEntityContactId
-	   , strContactName = EC.strName
-	   , I.strType
-	   , I.strTransactionType
-	   , I.intTermId
-	   , T.strTerm
-	   , I.strBOLNumber
-	   , strTicketNumbers = SCALETICKETS.strTicketNumbers
-	   , strCustomerReferences = CUSTOMERREFERENCES.strCustomerReferences
-	   , I.dtmDate
-	   , I.dblInvoiceTotal
-	   , I.dtmDueDate
-	   , P.intPaymentId
-	   , P.strRecordNumber
-	   , dblDiscount = CASE WHEN (I.strTransactionType  IN ('Invoice','Debit Memo', 'Cash')) THEN ISNULL(I.dblDiscount,0)  ELSE  ISNULL(I.dblDiscount,0) * -1 END
-	   , dblDiscountAvailable = CASE WHEN (I.strTransactionType  IN ('Invoice','Debit Memo', 'Cash')) THEN ISNULL(I.dblDiscountAvailable,0)  ELSE  ISNULL(I.dblDiscountAvailable,0) * -1 END
-	   , dblInterest = CASE WHEN (I.strTransactionType  IN ('Invoice','Debit Memo', 'Cash')) THEN ISNULL(I.dblInterest,0)  ELSE  ISNULL(I.dblInterest,0) * -1 END
-	   , P.dblAmountDue
-	   , P.dblPayment
-	   , P.intPaymentMethodId
-	   , P.strPaymentMethod
-	   , P.dtmDatePaid
-	   , I.ysnPosted
-	   , I.dtmPostDate
-	   , I.ysnPaid
-	   , I.ysnProcessed
-	   , I.ysnRecurring
-	   , I.strComments
-	   , strBatchId = CASE WHEN I.strTransactionType = 'Customer Prepayment' THEN P.strBatchId ELSE I.strBatchId END
-	   , I.intCompanyLocationId
-	   , strLocationName = L.strLocationName
-	   , strEnteredBy = EB.strName
-	   , strUserEntered = USERENTERED.strName
-	   , I.intCurrencyId
-	   , strCurrency = CUR.strCurrency
-FROM tblARInvoice I
-INNER JOIN (
+SELECT intInvoiceId				= I.intInvoiceId
+	 , strInvoiceNumber			= I.strInvoiceNumber
+	 , intEntityCustomerId		= I.intEntityCustomerId 
+	 , strCustomerName			= C.strName
+	 , strCustomerNumber		= C.strCustomerNumber
+	 , intEntityContactId		= I.intEntityContactId
+	 , strContactName			= EC.strName
+	 , strType					= I.strType
+	 , strTransactionType		= I.strTransactionType
+	 , intTermId				= I.intTermId
+	 , strTerm					= T.strTerm
+	 , strBOLNumber				= I.strBOLNumber
+	 , strTicketNumbers			= SCALETICKETS.strTicketNumbers
+	 , strCustomerReferences	= CUSTOMERREFERENCES.strCustomerReferences
+	 , dtmDate					= I.dtmDate
+	 , dblInvoiceTotal			= I.dblInvoiceTotal
+	 , dtmDueDate				= I.dtmDueDate
+	 , intPaymentId				= P.intPaymentId
+	 , strRecordNumber			= P.strRecordNumber
+	 , dblDiscount				= CASE WHEN (I.strTransactionType  IN ('Invoice','Debit Memo', 'Cash')) THEN ISNULL(I.dblDiscount,0)  ELSE  ISNULL(I.dblDiscount,0) * -1 END
+	 , dblDiscountAvailable		= CASE WHEN (I.strTransactionType  IN ('Invoice','Debit Memo', 'Cash')) THEN ISNULL(I.dblDiscountAvailable,0)  ELSE  ISNULL(I.dblDiscountAvailable,0) * -1 END
+	 , dblInterest				= CASE WHEN (I.strTransactionType  IN ('Invoice','Debit Memo', 'Cash')) THEN ISNULL(I.dblInterest,0)  ELSE  ISNULL(I.dblInterest,0) * -1 END
+	 , dblAmountDue				= P.dblAmountDue
+	 , dblPayment				= CASE WHEN I.strTransactionType = 'Cash' THEN I.dblPayment ELSE P.dblPayment END 
+	 , intPaymentMethodId		= CASE WHEN I.strTransactionType = 'Cash' THEN I.intPaymentMethodId ELSE P.intPaymentMethodId END
+	 , strPaymentMethod			= PM.strPaymentMethod
+	 , dtmDatePaid				= CASE WHEN I.strTransactionType = 'Cash' THEN I.dtmPostDate ELSE P.dtmDatePaid END
+	 , ysnPosted				= I.ysnPosted
+	 , dtmPostDate				= I.dtmPostDate
+	 , ysnPaid					= I.ysnPaid
+	 , ysnProcessed				= I.ysnProcessed
+	 , ysnRecurring				= I.ysnRecurring
+	 , strComments				= I.strComments
+	 , strBatchId				= CASE WHEN I.strTransactionType = 'Customer Prepayment' THEN P.strBatchId ELSE I.strBatchId END
+	 , intCompanyLocationId		= I.intCompanyLocationId
+	 , strLocationName			= L.strLocationName
+	 , strEnteredBy				= EB.strName
+	 , strUserEntered			= USERENTERED.strName
+	 , intCurrencyId			= I.intCurrencyId
+	 , strCurrency				= CUR.strCurrency
+FROM dbo.tblARInvoice I WITH (NOLOCK)
+LEFT JOIN (
 	SELECT AP.intPaymentId
-		,  APD.intInvoiceId
-		,  AP.ysnPosted
-		,  AP.intPaymentMethodId
-		,  strPaymentMethod = CASE WHEN LEN(RTRIM(LTRIM(AP.strPaymentMethod))) = 0 THEN PM.strPaymentMethod  ELSE ISNULL(AP.strPaymentMethod, PM.strPaymentMethod) END
-		,  APD.dblPayment
-		,  APD.dblAmountDue
-		,  AP.strBatchId
-		,  AP.strRecordNumber
-		,  AP.dtmDatePaid
+		 , APD.intInvoiceId
+		 , AP.ysnPosted
+		 , AP.intPaymentMethodId
+		 , APD.dblPayment
+		 , APD.dblAmountDue
+		 , AP.strBatchId
+		 , AP.strRecordNumber
+		 , AP.dtmDatePaid
 	FROM tblARPayment AP
 	LEFT JOIN (
 		SELECT intPaymentId
@@ -59,20 +58,24 @@ INNER JOIN (
 			, dblPayment
 			, dblAmountDue 
 		FROM tblARPaymentDetail
-	) APD ON APD.intPaymentId = AP.intPaymentId
-	LEFT JOIN (
-		SELECT intPaymentMethodID, strPaymentMethod FROM tblSMPaymentMethod
-	) PM ON PM.intPaymentMethodID = AP.intPaymentMethodId
+	) APD ON APD.intPaymentId = AP.intPaymentId	
 	WHERE AP.ysnPosted = 1
 ) P ON P.intInvoiceId = I.intInvoiceId
-INNER JOIN (SELECT EME.intEntityId
-		, EME.strName
-		, ARC.strCustomerNumber
-FROM dbo.tblEMEntity EME WITH (NOLOCK)  
-LEFT JOIN (SELECT intEntityId
-				, strCustomerNumber
-				FROM 
-				tblARCustomer WITH (NOLOCK)) ARC ON EME.intEntityId = ARC.intEntityId
+LEFT JOIN (
+	SELECT intPaymentMethodID
+		 , strPaymentMethod 
+	FROM tblSMPaymentMethod
+) PM ON PM.intPaymentMethodID = CASE WHEN I.strTransactionType = 'Cash' THEN I.intPaymentMethodId ELSE P.intPaymentMethodId END
+INNER JOIN (
+	SELECT EME.intEntityId
+		 , EME.strName
+		 , ARC.strCustomerNumber
+	FROM dbo.tblEMEntity EME WITH (NOLOCK)  
+	LEFT JOIN (
+		SELECT intEntityId
+			 , strCustomerNumber
+		FROM dbo.tblARCustomer WITH (NOLOCK)
+	) ARC ON EME.intEntityId = ARC.intEntityId
 ) C ON I.intEntityCustomerId = C.intEntityId
 OUTER APPLY (
 	SELECT TOP 1 strName , strEmail, intEntityContactId 
@@ -119,17 +122,19 @@ OUTER APPLY (
 	) INV (strCustomerReference)
 ) CUSTOMERREFERENCES
 LEFT OUTER JOIN (
-	SELECT intEntityId, strName 
+	SELECT intEntityId
+		 , strName 
 	FROM dbo.tblEMEntity WITH (NOLOCK)
 ) EB ON I.intEntityId = EB.intEntityId
 LEFT OUTER JOIN (
 	SELECT intCurrencyID
-			, strCurrency
-			, strDescription
+		  , strCurrency
+		 , strDescription
 	FROM dbo.tblSMCurrency WITH (NOLOCK)
 ) CUR ON I.intCurrencyId = CUR.intCurrencyID
 LEFT OUTER JOIN (
 	SELECT intEntityId
-			, strName 
+		 , strName 
 	FROM dbo.tblEMEntity WITH (NOLOCK)
 ) USERENTERED ON USERENTERED.intEntityId = I.intPostedById
+WHERE ((P.intPaymentId IS NOT NULL AND I.strTransactionType <> 'Cash') OR I.strTransactionType = 'Cash')
