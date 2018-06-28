f2b3d72212d61d66e384adbb3c64f03c9fc4086e
COMTECH\HZaragoza <henry.zaragoza@irely.com>
2018-06-28 21:41:52 +0800

    ST-815 [Checkout] do not allow lotted item in checkout

---
+++ i21Database/dbo/Views/vyuSTStoreItems.sql
@@ -0,0 +1,17 @@
+﻿CREATE VIEW vyuSTStoreItems
+AS
+SELECT I.intItemId
+       , I.strItemNo
+       , I.strDescription
+          , I.strLotTracking
+          , I.intCategoryId
+          , IL.intLocationId
+          , CL.strLocationName
+          , I.ysnFuelItem
+          , I.strType
+          , I.strStatus
+FROM tblICItem I
+JOIN tblICItemLocation IL
+       ON I.intItemId = IL.intItemId
+JOIN tblSMCompanyLocation CL
+       ON IL.intLocationId = CL.intCompanyLocationId
