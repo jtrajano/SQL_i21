bd9acd175f75eca5bbbba1f68a9c2c70471c758a
COMTECH\JMendones <jesmar.mendones@irely.com>
2018-06-28 20:13:15 +0800

    FRM-7490

---
+++ i21Database/dbo/Views/vyuSMAuditLogPanel.sql
@@ -0,0 +1,15 @@
+﻿CREATE VIEW [dbo].[vyuSMAuditLogPanel]
+AS
+SELECT 
+intAuditLogId,
+en.strName,
+[dbo].[fnSMAddSpaceToTitleCase](right(strTransactionType, CHARINDEX('.', REVERSE(strTransactionType)) - 1), 0) as strTransactionType,
+strActionType + ' a record' as strActionType,
+strRecordNo
+strDescription,
+strRoute,
+dtmDate,
+strRecordNo,
+auditLog.intEntityId
+FROM tblSMAuditLog auditLog
+inner join tblEMEntity en on en.intEntityId = auditLog.intEntityId
