eed38cda34b790220080a81d57d32b825c3d6bee
Feb Montefrio <feb.montefrio@irely.com>
2018-06-28 14:32:11 +0800

    [B] IC-5611

---
+++ i21Database/dbo/Stored Procedures/uspICGetChargeAddOns.sql
@@ -0,0 +1,35 @@
+﻿CREATE PROCEDURE [dbo].[uspICGetChargeAddOns]
+	@intItemId AS INT,
+	@intLocationId AS INT
+AS
+BEGIN
+
+	SELECT	OtherCharge.intItemId
+			, OtherCharge.strItemNo
+			, OtherCharge.strDescription
+			, OtherCharge.ysnInventoryCost
+			, OtherCharge.ysnAccrue
+			, OtherCharge.ysnMTM
+			, OtherCharge.ysnPrice
+			, OtherCharge.strCostMethod
+			, OtherCharge.dblAmount
+			, OtherCharge.intCostUOMId
+			, OtherCharge.strCostUOM 
+			, OtherCharge.strUnitType
+			, OtherCharge.strCostType
+			, OtherCharge.intOnCostTypeId
+			, OtherCharge.strOnCostType 
+			, OtherCharge.ysnBasisContract
+			, OtherCharge.intM2MComputationId
+			, OtherCharge.strM2MComputation
+	FROM	tblICItemAddOn ItemWithAddOn 
+			INNER JOIN vyuICGetOtherCharges OtherCharge 
+				ON OtherCharge.intItemId = ItemWithAddOn.intAddOnItemId
+			LEFT JOIN (
+				tblICItemLocation ItemLocation INNER JOIN tblSMCompanyLocation l 
+					ON l.intCompanyLocationId = ItemLocation.intLocationId
+			) ON ItemLocation.intItemId = OtherCharge.intItemId
+				AND ItemLocation.intLocationId IS NOT NULL
+	WHERE	ItemWithAddOn.intItemId = @intItemId
+			AND l.intCompanyLocationId = @intLocationId
+END
