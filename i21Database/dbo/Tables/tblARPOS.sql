997dffb68286acfdf4d9c7d17203be36fb3b0645
JBorlagdan <jc.borlagdan@irely.com>
2018-06-29 17:26:30 +0800

    AR-7943

--- i21Database/dbo/Tables/tblARPOS.sql
+++ i21Database/dbo/Tables/tblARPOS.sql
@@ -19,7 +19,10 @@
 	[intPOSLogId]		   INT             NOT NULL,
     [intConcurrencyId]     INT             NOT NULL,
 	[ysnReturn]			   BIT             CONSTRAINT [DF_tblARPOS_ysnReturn] DEFAULT ((0)) NOT NULL,
+	[strPONumber]		   NVARCHAR(25)   COLLATE Latin1_General_CI_AS NULL,
+	[intDocumentMaintenanceId]	INT			NULL,
     CONSTRAINT [PK_tblARPOS] PRIMARY KEY CLUSTERED ([intPOSId] ASC),
-	CONSTRAINT [FK_tblARPOSLog] FOREIGN KEY ([intPOSLogId]) REFERENCES [dbo].[tblARPOSLog] ([intPOSLogId]) 
+	CONSTRAINT [FK_tblARPOSLog] FOREIGN KEY ([intPOSLogId]) REFERENCES [dbo].[tblARPOSLog] ([intPOSLogId]),
+	CONSTRAINT [FK_tblSMDocumentMaintenance] FOREIGN KEY ([intDocumentMaintenanceId]) REFERENCES [dbo].[tblSMDocumentMaintenance] ([intDocumentMaintenanceId])
 );
 
