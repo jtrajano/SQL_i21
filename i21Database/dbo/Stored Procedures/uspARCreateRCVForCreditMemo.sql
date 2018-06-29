17b37cfb37449cfee7013a9ea54120786035db06
Kelvin Quinto <kelvin.quinto@irely.com>
2018-06-29 14:40:48 +0800

    AR-7948

--- i21Database/dbo/Stored Procedures/uspARCreateRCVForCreditMemo.sql
+++ i21Database/dbo/Stored Procedures/uspARCreateRCVForCreditMemo.sql
@@ -42,6 +42,8 @@ INSERT INTO tblARPayment (
    , intCurrencyId
    , intPaymentMethodId
    , intLocationId
+   , intBankAccountId
+   , intEntityId
    , dtmDatePaid
    , strReceivePaymentType
    , strPaymentMethod
@@ -55,22 +57,26 @@ INSERT INTO tblARPayment (
    , ysnApplytoBudget
 )
 SELECT TOP 1 
-     intEntityCustomerId	= I.intEntityCustomerId
-   , intCurrencyId			= I.intCurrencyId
-   , intPaymentMethodId		= @intPaymentMethodId
-   , intLocationId			= I.intCompanyLocationId
-   , dtmDatePaid			= I.dtmPostDate
+     intEntityCustomerId	  = I.intEntityCustomerId
+   , intCurrencyId			    = I.intCurrencyId
+   , intPaymentMethodId		  = @intPaymentMethodId
+   , intLocationId			    = I.intCompanyLocationId
+   , intBankAccountId				= BA.intBankAccountId
+   , intEntityId            = ISNULL(@intUserId, I.intEntityId)
+   , dtmDatePaid			      = I.dtmPostDate
    , strReceivePaymentType	= 'Cash Receipts'
-   , strPaymentMethod		= @strPaymentMethod
-   , dblAmountPaid			= 0.00
-   , dblBaseAmountPaid		= 0.00
-   , dblUnappliedAmount		= 0.00
+   , strPaymentMethod		    = @strPaymentMethod
+   , dblAmountPaid			    = 0.00
+   , dblBaseAmountPaid		  = 0.00
+   , dblUnappliedAmount		  = 0.00
    , dblBaseUnappliedAmount	= 0.00
-   , dblOverpayment			= 0.00
-   , dblBaseOverpayment		= 0.00
-   , dblExchangeRate		= 1.00
-   , ysnApplytoBudget		= 0
+   , dblOverpayment			    = 0.00
+   , dblBaseOverpayment		  = 0.00
+   , dblExchangeRate		    = 1.00
+   , ysnApplytoBudget		    = 0
 FROM dbo.tblARInvoice I WITH (NOLOCK)
+INNER JOIN tblSMCompanyLocation CL ON I.intCompanyLocationId = CL.intCompanyLocationId
+LEFT JOIN tblCMBankAccount BA ON CL.intCashAccount = BA.intGLAccountId
 WHERE intInvoiceId = @intInvoiceId
 
 SET @intPaymentId = SCOPE_IDENTITY()
