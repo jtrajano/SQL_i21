25d5d66a9bf37ed565dfacba1a1dedbf7e8f7fa7
Leslie L Arat <leslie.arat@irely.com>
2018-06-29 15:13:07 +0800

    GRN-1031 (Disconnected Model - Error message when updating local server after adding new locations in Discounts)

--- i21Database/dbo/Stored Procedures/uspDMMergeGRTables.sql
+++ i21Database/dbo/Stored Procedures/uspDMMergeGRTables.sql
@@ -39,59 +39,7 @@ BEGIN
     SET IDENTITY_INSERT tblGRStorageType ON
     EXECUTE sp_executesql @SQLString;
     SET IDENTITY_INSERT tblGRStorageType OFF
-
-    -- tblGRStorageScheduleLocationUse
-    SELECT	@Columns = NULL, @InsertColumns = NULL, @ValueColumns = NULL
-	SELECT	@Columns = COALESCE(@Columns + ',', '') + 'Target.' + COLUMN_NAME + ' = Source.' + COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'tblGRStorageScheduleLocationUse' AND ORDINAL_POSITION > 1
-	SELECT	@InsertColumns	=	COALESCE(@InsertColumns + ',', '') + COLUMN_NAME ,
-			@ValueColumns	=	COALESCE(@ValueColumns + ',', '') + 'Source.' + COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'tblGRStorageScheduleLocationUse'
-
-    SET @SQLString = N'MERGE tblGRStorageScheduleLocationUse AS Target
-        USING (SELECT * FROM REMOTEDBSERVER.[repDB].[dbo].[tblGRStorageScheduleLocationUse]) AS Source
-        ON (Target.intStorageScheduleLocationUseId = Source.intStorageScheduleLocationUseId)
-        WHEN MATCHED THEN
-            UPDATE SET ' + @Columns + '
-		WHEN NOT MATCHED BY TARGET THEN
-			INSERT (
-				' + @InsertColumns + '
-			)
-			VALUES(
-				' + @ValueColumns + '
-			)
-        WHEN NOT MATCHED BY SOURCE THEN
-            DELETE;';
-
-    SET @SQLString = 'Exec('' ' + Replace(@SQLString, 'repDB', @remoteDB) + ' '')'
-    SET IDENTITY_INSERT tblGRStorageScheduleLocationUse ON
-    EXECUTE sp_executesql @SQLString;
-    SET IDENTITY_INSERT tblGRStorageScheduleLocationUse OFF
-
-    -- tblGRStorageSchedulePeriod
-    SELECT	@Columns = NULL, @InsertColumns = NULL, @ValueColumns = NULL
-	SELECT	@Columns = COALESCE(@Columns + ',', '') + 'Target.' + COLUMN_NAME + ' = Source.' + COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'tblGRStorageSchedulePeriod' AND ORDINAL_POSITION > 1
-	SELECT	@InsertColumns	=	COALESCE(@InsertColumns + ',', '') + COLUMN_NAME ,
-			@ValueColumns	=	COALESCE(@ValueColumns + ',', '') + 'Source.' + COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'tblGRStorageSchedulePeriod'
-
-    SET @SQLString = N'MERGE tblGRStorageSchedulePeriod AS Target
-        USING (SELECT * FROM REMOTEDBSERVER.[repDB].[dbo].[tblGRStorageSchedulePeriod]) AS Source
-        ON (Target.intStorageSchedulePeriodId = Source.intStorageSchedulePeriodId)
-        WHEN MATCHED THEN
-            UPDATE SET ' + @Columns + '
-		WHEN NOT MATCHED BY TARGET THEN
-			INSERT (
-				' + @InsertColumns + '
-			)
-			VALUES(
-				' + @ValueColumns + '
-			)
-        WHEN NOT MATCHED BY SOURCE THEN
-            DELETE;';
-
-    SET @SQLString = 'Exec('' ' + Replace(@SQLString, 'repDB', @remoteDB) + ' '')'
-    SET IDENTITY_INSERT tblGRStorageSchedulePeriod ON
-    EXECUTE sp_executesql @SQLString;
-    SET IDENTITY_INSERT tblGRStorageSchedulePeriod OFF
-
+    
     -- tblGRStorageScheduleRule
     SELECT	@Columns = NULL, @InsertColumns = NULL, @ValueColumns = NULL
 	SELECT	@Columns = COALESCE(@Columns + ',', '') + 'Target.' + COLUMN_NAME + ' = Source.' + COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'tblGRStorageScheduleRule' AND ORDINAL_POSITION > 1
@@ -118,6 +66,58 @@ BEGIN
     EXECUTE sp_executesql @SQLString;
     SET IDENTITY_INSERT tblGRStorageScheduleRule OFF
 
+    -- tblGRStorageScheduleLocationUse
+    SELECT	@Columns = NULL, @InsertColumns = NULL, @ValueColumns = NULL
+	SELECT	@Columns = COALESCE(@Columns + ',', '') + 'Target.' + COLUMN_NAME + ' = Source.' + COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'tblGRStorageScheduleLocationUse' AND ORDINAL_POSITION > 1
+	SELECT	@InsertColumns	=	COALESCE(@InsertColumns + ',', '') + COLUMN_NAME ,
+			@ValueColumns	=	COALESCE(@ValueColumns + ',', '') + 'Source.' + COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'tblGRStorageScheduleLocationUse'
+
+    SET @SQLString = N'MERGE tblGRStorageScheduleLocationUse AS Target
+        USING (SELECT * FROM REMOTEDBSERVER.[repDB].[dbo].[tblGRStorageScheduleLocationUse]) AS Source
+        ON (Target.intStorageScheduleLocationUseId = Source.intStorageScheduleLocationUseId)
+        WHEN MATCHED THEN
+            UPDATE SET ' + @Columns + '
+		WHEN NOT MATCHED BY TARGET THEN
+			INSERT (
+				' + @InsertColumns + '
+			)
+			VALUES(
+				' + @ValueColumns + '
+			)
+        WHEN NOT MATCHED BY SOURCE THEN
+            DELETE;';
+
+    SET @SQLString = 'Exec('' ' + Replace(@SQLString, 'repDB', @remoteDB) + ' '')'
+    SET IDENTITY_INSERT tblGRStorageScheduleLocationUse ON
+    EXECUTE sp_executesql @SQLString;
+    SET IDENTITY_INSERT tblGRStorageScheduleLocationUse OFF    
+    
+    -- tblGRStorageSchedulePeriod
+    SELECT	@Columns = NULL, @InsertColumns = NULL, @ValueColumns = NULL
+	SELECT	@Columns = COALESCE(@Columns + ',', '') + 'Target.' + COLUMN_NAME + ' = Source.' + COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'tblGRStorageSchedulePeriod' AND ORDINAL_POSITION > 1
+	SELECT	@InsertColumns	=	COALESCE(@InsertColumns + ',', '') + COLUMN_NAME ,
+			@ValueColumns	=	COALESCE(@ValueColumns + ',', '') + 'Source.' + COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'tblGRStorageSchedulePeriod'
+
+    SET @SQLString = N'MERGE tblGRStorageSchedulePeriod AS Target
+        USING (SELECT * FROM REMOTEDBSERVER.[repDB].[dbo].[tblGRStorageSchedulePeriod]) AS Source
+        ON (Target.intStorageSchedulePeriodId = Source.intStorageSchedulePeriodId)
+        WHEN MATCHED THEN
+            UPDATE SET ' + @Columns + '
+		WHEN NOT MATCHED BY TARGET THEN
+			INSERT (
+				' + @InsertColumns + '
+			)
+			VALUES(
+				' + @ValueColumns + '
+			)
+        WHEN NOT MATCHED BY SOURCE THEN
+            DELETE;';
+
+    SET @SQLString = 'Exec('' ' + Replace(@SQLString, 'repDB', @remoteDB) + ' '')'
+    SET IDENTITY_INSERT tblGRStorageSchedulePeriod ON
+    EXECUTE sp_executesql @SQLString;
+    SET IDENTITY_INSERT tblGRStorageSchedulePeriod OFF    
+
     -- tblGRDiscountSchedule
     SELECT	@Columns = NULL, @InsertColumns = NULL, @ValueColumns = NULL
 	SELECT	@Columns = COALESCE(@Columns + ',', '') + 'Target.' + COLUMN_NAME + ' = Source.' + COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'tblGRDiscountSchedule' AND ORDINAL_POSITION > 1
@@ -143,6 +143,54 @@ BEGIN
     SET IDENTITY_INSERT tblGRDiscountSchedule ON
     EXECUTE sp_executesql @SQLString;
     SET IDENTITY_INSERT tblGRDiscountSchedule OFF
+
+    -- tblGRDiscountCalculationOption
+    SELECT	@Columns = NULL, @InsertColumns = NULL, @ValueColumns = NULL
+	SELECT	@Columns = COALESCE(@Columns + ',', '') + 'Target.' + COLUMN_NAME + ' = Source.' + COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'tblGRDiscountCalculationOption' AND ORDINAL_POSITION > 1
+	SELECT	@InsertColumns	=	COALESCE(@InsertColumns + ',', '') + COLUMN_NAME ,
+			@ValueColumns	=	COALESCE(@ValueColumns + ',', '') + 'Source.' + COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'tblGRDiscountCalculationOption'
+
+    SET @SQLString = N'MERGE tblGRDiscountCalculationOption AS Target
+        USING (SELECT * FROM REMOTEDBSERVER.[repDB].[dbo].[tblGRDiscountCalculationOption]) AS Source
+        ON (Target.intDiscountCalculationOptionId  = Source.intDiscountCalculationOptionId )
+        WHEN MATCHED THEN
+            UPDATE SET ' + @Columns + '
+		WHEN NOT MATCHED BY TARGET THEN
+			INSERT (
+				' + @InsertColumns + '
+			)
+			VALUES(
+				' + @ValueColumns + '
+			)
+        WHEN NOT MATCHED BY SOURCE THEN
+            DELETE;';
+
+    SET @SQLString = 'Exec('' ' + Replace(@SQLString, 'repDB', @remoteDB) + ' '')'
+    EXECUTE sp_executesql @SQLString;
+
+    -- tblGRShrinkCalculationOption
+    SELECT	@Columns = NULL, @InsertColumns = NULL, @ValueColumns = NULL
+	SELECT	@Columns = COALESCE(@Columns + ',', '') + 'Target.' + COLUMN_NAME + ' = Source.' + COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'tblGRShrinkCalculationOption' AND ORDINAL_POSITION > 1
+	SELECT	@InsertColumns	=	COALESCE(@InsertColumns + ',', '') + COLUMN_NAME ,
+			@ValueColumns	=	COALESCE(@ValueColumns + ',', '') + 'Source.' + COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'tblGRShrinkCalculationOption'
+
+    SET @SQLString = N'MERGE tblGRShrinkCalculationOption AS Target
+        USING (SELECT * FROM REMOTEDBSERVER.[repDB].[dbo].[tblGRShrinkCalculationOption]) AS Source
+        ON (Target.intShrinkCalculationOptionId  = Source.intShrinkCalculationOptionId )
+        WHEN MATCHED THEN
+            UPDATE SET ' + @Columns + '
+		WHEN NOT MATCHED BY TARGET THEN
+			INSERT (
+				' + @InsertColumns + '
+			)
+			VALUES(
+				' + @ValueColumns + '
+			)
+        WHEN NOT MATCHED BY SOURCE THEN
+            DELETE;';
+
+    SET @SQLString = 'Exec('' ' + Replace(@SQLString, 'repDB', @remoteDB) + ' '')'
+    EXECUTE sp_executesql @SQLString;
 
     -- tblGRDiscountScheduleCode
     SELECT	@Columns = NULL, @InsertColumns = NULL, @ValueColumns = NULL
@@ -195,16 +243,16 @@ BEGIN
     SET IDENTITY_INSERT tblGRDiscountScheduleLine ON
     EXECUTE sp_executesql @SQLString;
     SET IDENTITY_INSERT tblGRDiscountScheduleLine OFF
-
-    -- tblGRDiscountCalculationOption
+    
+    -- tblGRDiscountId
     SELECT	@Columns = NULL, @InsertColumns = NULL, @ValueColumns = NULL
-	SELECT	@Columns = COALESCE(@Columns + ',', '') + 'Target.' + COLUMN_NAME + ' = Source.' + COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'tblGRDiscountCalculationOption' AND ORDINAL_POSITION > 1
+	SELECT	@Columns = COALESCE(@Columns + ',', '') + 'Target.' + COLUMN_NAME + ' = Source.' + COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'tblGRDiscountId' AND ORDINAL_POSITION > 1
 	SELECT	@InsertColumns	=	COALESCE(@InsertColumns + ',', '') + COLUMN_NAME ,
-			@ValueColumns	=	COALESCE(@ValueColumns + ',', '') + 'Source.' + COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'tblGRDiscountCalculationOption'
+			@ValueColumns	=	COALESCE(@ValueColumns + ',', '') + 'Source.' + COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'tblGRDiscountId'
 
-    SET @SQLString = N'MERGE tblGRDiscountCalculationOption AS Target
-        USING (SELECT * FROM REMOTEDBSERVER.[repDB].[dbo].[tblGRDiscountCalculationOption]) AS Source
-        ON (Target.intDiscountCalculationOptionId  = Source.intDiscountCalculationOptionId )
+    SET @SQLString = N'MERGE tblGRDiscountId AS Target
+        USING (SELECT * FROM REMOTEDBSERVER.[repDB].[dbo].[tblGRDiscountId]) AS Source
+        ON (Target.intDiscountCrossReferenceId = Source.intDiscountCrossReferenceId)
         WHEN MATCHED THEN
             UPDATE SET ' + @Columns + '
 		WHEN NOT MATCHED BY TARGET THEN
@@ -218,7 +266,9 @@ BEGIN
             DELETE;';
 
     SET @SQLString = 'Exec('' ' + Replace(@SQLString, 'repDB', @remoteDB) + ' '')'
+    SET IDENTITY_INSERT tblGRDiscountId ON
     EXECUTE sp_executesql @SQLString;
+    SET IDENTITY_INSERT tblGRDiscountId OFF
 
     -- tblGRDiscountCrossReference
     SELECT	@Columns = NULL, @InsertColumns = NULL, @ValueColumns = NULL
@@ -245,32 +295,6 @@ BEGIN
     SET IDENTITY_INSERT tblGRDiscountCrossReference ON
     EXECUTE sp_executesql @SQLString;
     SET IDENTITY_INSERT tblGRDiscountCrossReference OFF
-
-    -- tblGRDiscountId
-    SELECT	@Columns = NULL, @InsertColumns = NULL, @ValueColumns = NULL
-	SELECT	@Columns = COALESCE(@Columns + ',', '') + 'Target.' + COLUMN_NAME + ' = Source.' + COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'tblGRDiscountId' AND ORDINAL_POSITION > 1
-	SELECT	@InsertColumns	=	COALESCE(@InsertColumns + ',', '') + COLUMN_NAME ,
-			@ValueColumns	=	COALESCE(@ValueColumns + ',', '') + 'Source.' + COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'tblGRDiscountId'
-
-    SET @SQLString = N'MERGE tblGRDiscountId AS Target
-        USING (SELECT * FROM REMOTEDBSERVER.[repDB].[dbo].[tblGRDiscountId]) AS Source
-        ON (Target.intDiscountId = Source.intDiscountId)
-        WHEN MATCHED THEN
-            UPDATE SET ' + @Columns + '
-		WHEN NOT MATCHED BY TARGET THEN
-			INSERT (
-				' + @InsertColumns + '
-			)
-			VALUES(
-				' + @ValueColumns + '
-			)
-        WHEN NOT MATCHED BY SOURCE THEN
-            DELETE;';
-
-    SET @SQLString = 'Exec('' ' + Replace(@SQLString, 'repDB', @remoteDB) + ' '')'
-    SET IDENTITY_INSERT tblGRDiscountId ON
-    EXECUTE sp_executesql @SQLString;
-    SET IDENTITY_INSERT tblGRDiscountId OFF
 
     -- tblGRDiscountLocationUse
     SELECT	@Columns = NULL, @InsertColumns = NULL, @ValueColumns = NULL
