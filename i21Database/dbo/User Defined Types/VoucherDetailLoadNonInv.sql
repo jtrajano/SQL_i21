e36c7fc354a0cec81463a51b31b4cf056c310e97
IRELY\swghosh <swetosuvro.g@irely.com>
2018-07-02 19:00:20 +0530

    LG-1517

--- i21Database/dbo/User Defined Types/VoucherDetailLoadNonInv.sql
+++ i21Database/dbo/User Defined Types/VoucherDetailLoadNonInv.sql
@@ -1,14 +1,14 @@
 ﻿CREATE TYPE [dbo].[VoucherDetailLoadNonInv] AS TABLE
 (
-	intContractHeaderId INT NOT NULL,
-	intContractDetailId INT NOT NULL,
-	intItemId			INT NOT NULL,
-	intItemUOMId		INT NOT NULL,
-	intCostUOMId		INT NULL,
-	intAccountId		INT NOT NULL,
-	intLoadDetailId		INT NOT NULL,
-	dblQtyReceived		DECIMAL(18,6) NOT NULL DEFAULT 0,
-	dblUnitQty			DECIMAL(38,20) NOT NULL DEFAULT 0,
-	dblCost				DECIMAL(38,20) NOT NULL DEFAULT 0,
-	dblCostUnitQty		DECIMAL(38,20) NOT NULL DEFAULT 0
+	[intContractHeaderId] INT NOT NULL,
+	[intContractDetailId] INT NOT NULL,
+	[intItemId]			  INT NOT NULL,
+	[intItemUOMId]		  INT NOT NULL,
+	[intCostUOMId]		  INT NULL,
+	[intAccountId]		  INT NOT NULL,
+	[intLoadDetailId]	  INT NOT NULL,
+	[dblQtyReceived]	  DECIMAL(18,6) NOT NULL DEFAULT 0,
+	[dblUnitQty]	 	  DECIMAL(38, 20) NOT NULL DEFAULT ((0)),
+	[dblCost]			  DECIMAL(38, 20) NOT NULL DEFAULT ((0)),
+	[dblCostUnitQty]	  DECIMAL(38, 20) NOT NULL DEFAULT ((0))
 )
