6ddc9f523c6836fb3fa13456bf8c18cd6316ce54
JeffreyTrajano <jeffrey.trajano@irely.com>
2018-07-05 10:44:35 +0800

    [Revalue] SQL error in trying to revalue with closed period reversed date

--- i21Database/dbo/Stored Procedures/uspGLUpdateOldAccountId.sql
+++ i21Database/dbo/Stored Procedures/uspGLUpdateOldAccountId.sql
@@ -2,9 +2,12 @@ CREATE PROCEDURE [dbo].[uspGLUpdateOldAccountId]
 @intAccountSystemId INT
 AS
 BEGIN
-	UPDATE account 
-	SET strOldAccountId = mapping.strOldAccountId 
-	FROM tblGLAccount account join tblGLCrossReferenceMapping mapping
-	ON account.intAccountId = mapping.intAccountId
-	WHERE intAccountSystemId = @intAccountSystemId
+	IF @intAccountSystemId = 0
+		UPDATE tblGLAccount set strOldAccountId = ''
+	ELSE
+		UPDATE account 
+		SET strOldAccountId = mapping.strOldAccountId 
+		FROM tblGLAccount account join tblGLCrossReferenceMapping mapping
+		ON account.intAccountId = mapping.intAccountId
+		WHERE intAccountSystemId = @intAccountSystemId
 END
