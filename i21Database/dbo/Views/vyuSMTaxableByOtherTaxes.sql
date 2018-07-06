cf051768609e04040f3afcf8247122ed718ef5bb
Dan Jalimao <dan.jalimao@irely.com>
2018-07-06 14:30:31 +0800

    SM-4257

--- i21Database/dbo/Views/vyuSMTaxableByOtherTaxes.sql
+++ i21Database/dbo/Views/vyuSMTaxableByOtherTaxes.sql
@@ -1,10 +1,22 @@
 ﻿CREATE VIEW [dbo].[vyuSMTaxableByOtherTaxes]
 AS 
-SELECT b.intID as intTaxableByOtherTaxesId
-,a.intTaxCodeId
-,c.strTaxCode
-FROM tblSMTaxCode a
-CROSS APPLY fnGetRowsFromDelimitedValues(a.strTaxableByOtherTaxes) b 
-INNER JOIN tblSMTaxCode c ON b.intID = c.intTaxCodeId
+SELECT	intTaxableByOtherTaxesId, intTaxCodeId, strTaxCode,
+		strTaxCodes = STUFF((SELECT	', ' + strTaxCode
+				FROM	(SELECT b.intID as intTaxableByOtherTaxesId
+						   ,a.intTaxCodeId
+						   ,c.strTaxCode
+						   FROM tblSMTaxCode a
+						   CROSS APPLY fnGetRowsFromDelimitedValues(a.strTaxableByOtherTaxes) b 
+						   INNER JOIN tblSMTaxCode c ON b.intID = c.intTaxCodeId) AS B2
+				WHERE B2.intTaxCodeId = B1.intTaxCodeId	
+				ORDER BY strTaxCode
+				FOR XML	PATH('')
+				), 1, 1, '')
+FROM (SELECT b.intID as intTaxableByOtherTaxesId
+		,a.intTaxCodeId
+		,c.strTaxCode
+		FROM tblSMTaxCode a
+		CROSS APPLY fnGetRowsFromDelimitedValues(a.strTaxableByOtherTaxes) b 
+		INNER JOIN tblSMTaxCode c ON b.intID = c.intTaxCodeId) AS B1
 
 
