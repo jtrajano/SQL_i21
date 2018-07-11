c386e704bf842bc4539f441e712cf5354ba7d387
Gopinath Nayak <gopinath.n@irely.com>
2018-07-11 17:14:33 +0530

    CT-2242 - Provide stored procedure to update the actual amount of contract cost.

--- i21Database/dbo/Stored Procedures/uspCTUpdateActualAmount.sql
+++ i21Database/dbo/Stored Procedures/uspCTUpdateActualAmount.sql
@@ -1,11 +1,12 @@
 ﻿CREATE PROCEDURE [dbo].[uspCTUpdateActualAmount]
+	@ContractCostId AS Id  READONLY
 AS
 BEGIN TRY
 	
 	DECLARE @ErrMsg NVARCHAR(MAX)
 	
 	UPDATE  CC
-	SET     CC.dblActualAmount = tblBilled.dblTotal
+	SET     CC.dblActualAmount = ISNULL(tblBilled.dblTotal,0)
 	FROM tblCTContractCost CC
 	JOIN ( 
 	   SELECT intContractCostId,SUM(dblTotal) dblTotal 
@@ -13,6 +14,9 @@ BEGIN TRY
 	   WHERE intContractCostId > 0 
 	   GROUP BY intContractCostId
 	) tblBilled ON tblBilled.intContractCostId = CC.intContractCostId
+	JOIN @ContractCostId t ON t.intId = CC.intContractCostId
+	WHERE  ISNULL(CC.dblActualAmount,0) <> ISNULL(tblBilled.dblTotal,0)
+	
 
 END TRY
 
