d76f5aedf7cd92838d7b185b933c283b07fb4cae
Satya S Sahoo <satya.sahoo@irely.com>
2018-07-12 20:51:25 +0530

    CT-2241 - Provide stored procedure when unposting voucher from pricing contract. - 183Q

---
+++ i21Database/dbo/Stored Procedures/uspCTBeforeVoucherDelete.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROCEDURE [dbo].[uspCTBeforeVoucherDelete]
+
+	@intBillId	 INT,
+	@intUserId	 INT -- User Who is deleting the voucher.
+AS
+
+BEGIN TRY
+
+	DECLARE	@ErrMsg	NVARCHAR(MAX)
+
+	UPDATE	tblCTPriceFixationDetail SET intBillId = NULL, intBillDetailId = NULL WHERE intBillId = @intBillId
+
+END TRY
+
+BEGIN CATCH
+
+	SET @ErrMsg = ERROR_MESSAGE()  
+	RAISERROR (@ErrMsg,18,1,'WITH NOWAIT')  
+	
+END CATCH
+
