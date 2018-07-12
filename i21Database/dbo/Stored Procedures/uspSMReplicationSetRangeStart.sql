6478b0555f8e07511c17418fbfa820a95a4a0e71
Randy Sagum <randy.sagum@irely.com>
2018-07-12 17:47:35 +0800

    FRM-7527

--- i21Database/dbo/Stored Procedures/uspSMReplicationSetRangeStart.sql
+++ i21Database/dbo/Stored Procedures/uspSMReplicationSetRangeStart.sql
@@ -36,13 +36,13 @@ CREATE PROCEDURE [dbo].[uspSMReplicationSetRangeStart]
 	   DECLARE @sql NVARCHAR(MAX) = N'';
 	   DECLARE @insertSQL NVARCHAR(MAX) = '';
 
-	    SET @insertSQL = N'INSERT INTO #ListOfArticles
+	   SET @insertSQL = N'INSERT INTO #ListOfArticles
 		SELECT DISTINCT Tab.strTableName FROM [parentDB].[dbo].[tblSMReplicationConfiguration] AS Con
 		INNER JOIN [parentDB].[dbo].[tblSMReplicationConfigurationTable] AS ConTab
 		ON Con.intReplicationConfigurationId = ConTab.intReplicationConfigurationId
 		INNER JOIN [parentDB].[dbo].[tblSMReplicationTable] AS Tab
 		ON ConTab.intReplicationTableId = Tab.intReplicationTableId
-		WHERE strType = ''Subsidiary'' AND ysnCommitted = 1 AND ysnEnabled = 1 '
+		WHERE strType = ''Subsidiary'' OR (strType = ''Parent'' AND strTableName LIKE ''%tblGL%'') AND ysnCommitted = 1 AND ysnEnabled = 1 '
 
 		SET @insertSQL =  Replace(@insertSQL, 'parentDB', @parentDB)
 		EXECUTE sp_executesql @insertSQL;
