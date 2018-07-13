5e347b865bdcf61a88c81c9ddf417176e3a87185
Wendell Wayne Estrada <wendell.estrada@irely.com>
2018-07-14 05:15:43 +0800

    [B] IC-5683 Stock Details - Filter not working in Sub location & storage unit tabs

--- i21Database/dbo/Views/vyuICGetStorageBins.sql
+++ i21Database/dbo/Views/vyuICGetStorageBins.sql
@@ -1,8 +1,13 @@
 ﻿CREATE VIEW [dbo].[vyuICGetStorageBins]
 AS
-SELECT companyLocation.intCompanyLocationId, storageLocation.intStorageLocationId
-	, companyLocation.strLocationName strLocation, storageLocation.strName strStorageLocation
-	, storageLocation.dblEffectiveDepth, storageLocation.dblPackFactor, storageLocation.dblUnitPerFoot
+SELECT 
+	companyLocation.intCompanyLocationId, 
+	storageLocation.intStorageLocationId, 
+	companyLocation.strLocationName strLocation, 
+	storageLocation.strName strStorageLocation, 
+	storageLocation.dblEffectiveDepth, 
+	storageLocation.dblPackFactor, 
+	storageLocation.dblUnitPerFoot
 	, CAST(dbo.fnMaxNumeric(summary.dblStock, 0) AS NUMERIC(16, 8)) dblStock
 	, CAST(storageLocation.dblEffectiveDepth * storageLocation.dblUnitPerFoot AS NUMERIC(16, 8)) dblCapacity
 	, CAST(dbo.fnMaxNumeric((storageLocation.dblEffectiveDepth * storageLocation.dblUnitPerFoot) - dbo.fnMaxNumeric(summary.dblStock, 0), 0) AS NUMERIC(16, 8)) dblAvailable
@@ -15,4 +20,4 @@ FROM tblICStorageLocation storageLocation
 		GROUP BY storageLocation.intStorageLocationId
 	) summary ON summary.intStorageLocationId = storageLocation.intStorageLocationId
 	INNER JOIN tblSMCompanyLocation companyLocation ON companyLocation.intCompanyLocationId = storageLocation.intLocationId
-WHERE summary.dblStock <> 0
+WHERE CAST(dbo.fnMaxNumeric(summary.dblStock, 0) AS NUMERIC(16, 8)) <> 0
