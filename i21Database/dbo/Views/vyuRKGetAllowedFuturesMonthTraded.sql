daa69a4eb9ab6971fe1b582ffc3bccaf81fbc277
Smith Wilson de Jesus <smith.dejesus@irely.com>
2018-07-17 05:13:54 +0800

    RM-1465

---
+++ i21Database/dbo/Views/vyuRKGetAllowedFuturesMonthTraded.sql
@@ -0,0 +1,19 @@
+﻿CREATE VIEW vyuRKGetAllowedFuturesMonthTraded
+AS
+SELECT	CONVERT(INT,ROW_NUMBER() OVER (ORDER BY intFutureMarketId)) AS intRow, REPLACE(strMonth,'ysnFut' ,'') strFutureMonth, intFutureMarketId	
+FROM	(	SELECT	ysnFutJan,	ysnFutFeb,	ysnFutMar,	ysnFutApr,	ysnFutMay,	ysnFutJun,
+					ysnFutJul,	ysnFutAug,	ysnFutSep,	ysnFutOct,	ysnFutNov,	ysnFutDec, intFutureMarketId
+			FROM	tblRKFutureMarket
+		) p
+		UNPIVOT
+		(
+			ysnSelect FOR strMonth IN 
+			(
+				ysnFutJan,	ysnFutFeb,	ysnFutMar,	ysnFutApr,	ysnFutMay,	ysnFutJun,
+				ysnFutJul,	ysnFutAug,	ysnFutSep,	ysnFutOct,	ysnFutNov,	ysnFutDec
+			)
+		)AS unpvt
+WHERE	ysnSelect = 1
+
+	
+	
