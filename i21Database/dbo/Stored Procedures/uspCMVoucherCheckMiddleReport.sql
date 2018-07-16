74fb7ee3de56b0ef60c20a9edb4cb9e6317e6aff
JeffreyTrajano <jeffrey.trajano@irely.com>
2018-07-16 13:52:35 +0800

    GL-6505 [Print Check middle AP] due date is incorrect

--- i21Database/dbo/Stored Procedures/uspCMVoucherCheckMiddleReport.sql
+++ i21Database/dbo/Stored Procedures/uspCMVoucherCheckMiddleReport.sql
@@ -110,14 +110,10 @@ WHERE [fieldname] = 'strBatchId'
 SET @strTransactionId = CASE WHEN LTRIM(RTRIM(ISNULL(@strTransactionId, ''))) = '' THEN NULL ELSE @strTransactionId END  
 SET @strBatchId = CASE WHEN LTRIM(RTRIM(ISNULL(@strBatchId, ''))) = '' THEN NULL ELSE @strBatchId END  
 
-DECLARE @isCAD BIT = 0
-SELECT TOP 1 @isCAD = 1 FROM  tblCMBankAccount BA JOIN tblSMCurrency SMC ON SMC.intCurrencyID = BA.intCurrencyId 
-WHERE intBankAccountId = @intBankAccountId AND SMC.strCurrency = 'CAD'
 
 -- Report Query:  
 SELECT	
-		strCADDate =	REPLACE(CONVERT(NVARCHAR(10), CHK.dtmDate, 111), '/', '-')
-		,CHK.dtmDate
+		 CHK.dtmDate
 		,strCheckNumber = CHK.strReferenceNo
 		,CHK.dblAmount
 		,strPayee = CASE
