04041a6babec54be133fa4a4ed1eaaa77c79bab8
Kevin Estrada <kevin.estrada@irely.com>
2018-07-16 19:57:44 +0800

    FRM-7130: disconnected scripts

---
+++ i21Database/dbo/Stored Procedures/uspSMDisconReplicationSetIdentityNotForReplication.sql
@@ -0,0 +1,52 @@
+﻿CREATE PROCEDURE [dbo].[uspSMDisconReplicationSetIdentityNotForReplication] 
+
+AS
+BEGIN
+
+ IF object_id('tempdb..#ListOfArticles') IS NOT NULL
+	DROP TABLE #ListOfArticles
+
+	   CREATE TABLE dbo.#ListOfArticles 
+	   (   
+		  strArticle NVARCHAR(MAX),
+		  intId INT
+	   ) 
+
+   		DECLARE @sql NVARCHAR(MAX) = N'';
+		DECLARE @insertSQL NVARCHAR(MAX) = '';
+	
+
+		 BEGIN
+				SET @insertSQL = N'INSERT INTO #ListOfArticles
+								SELECT DISTINCT Tab.strTableName, object_id(Tab.strTableName) FROM [tblSMReplicationConfiguration] AS Con
+								INNER JOIN [tblSMReplicationConfigurationTable] AS ConTab
+								ON Con.intReplicationConfigurationId = ConTab.intReplicationConfigurationId
+								INNER JOIN [tblSMReplicationTable] AS Tab
+								ON ConTab.intReplicationTableId = Tab.intReplicationTableId
+								WHERE strType = ''Parent'' OR strTableName LIKE ''%tblSC%'' '
+
+
+			
+	    END
+
+			      
+	   EXECUTE sp_executesql @insertSQL;
+
+
+
+	   SELECT @sql += N'exec sys.sp_identitycolumnforreplication '
+				    + N''+CAST(intId AS NVARCHAR(MAX))+ ', 1 '
+					FROM #ListOfArticles
+		
+				
+       EXEC  sp_executesql @sql;
+
+
+
+
+
+   
+END
+
+
+
