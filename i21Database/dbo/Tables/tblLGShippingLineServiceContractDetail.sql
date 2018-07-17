a3d4c4e5fb0d61116c3af5626831214f0d744fb8
IRELY\swghosh <swetosuvro.g@irely.com>
2018-07-17 18:42:05 +0530

    LG-1526

--- i21Database/dbo/Tables/tblLGShippingLineServiceContractDetail.sql
+++ i21Database/dbo/Tables/tblLGShippingLineServiceContractDetail.sql
@@ -4,6 +4,7 @@
 	[intShippingLineServiceContractId] INT NOT NULL,
 	[strServiceContractNumber] NVARCHAR(100) COLLATE Latin1_General_CI_AS NOT NULL,
 	[intOriginId] INT NULL,
+	[strOrigin] NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL,
 	[dtmContractDate] DATETIME NOT NULL,
 	[strAmendmentNumber] NVARCHAR(100) COLLATE Latin1_General_CI_AS,
 	[dtmAmendmentDate] DATETIME,
@@ -11,5 +12,7 @@
 	[dtmValidTo] DATETIME,
 	[intConcurrencyId] INT NOT NULL, 
 
-	CONSTRAINT [FK_tblLGShippingLineServiceContractDetail_tblLGShippingLineServiceContract_intShippingLineServiceContractId] FOREIGN KEY ([intShippingLineServiceContractId]) REFERENCES [tblLGShippingLineServiceContract]([intShippingLineServiceContractId])
+	CONSTRAINT [FK_tblLGShippingLineServiceContractDetail_tblLGShippingLineServiceContract_intShippingLineServiceContractId] FOREIGN KEY ([intShippingLineServiceContractId]) REFERENCES [tblLGShippingLineServiceContract]([intShippingLineServiceContractId]),
+	CONSTRAINT [FK_tblLGShippingLineServiceContractDetail_tblICCommodityAttribute_intOriginId] FOREIGN KEY ([intOriginId]) REFERENCES [tblICCommodityAttribute]([intCommodityAttributeId])
+
 )
