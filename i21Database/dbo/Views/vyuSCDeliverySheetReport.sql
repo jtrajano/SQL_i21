eae8dd06734869845d4f7e0ac40f33988d6288f8
COMTECH\EVersoza <emerson.versoza@irely.com>
2018-07-17 16:59:24 +0800

    SC-1910

--- i21Database/dbo/Views/vyuSCDeliverySheetReport.sql
+++ i21Database/dbo/Views/vyuSCDeliverySheetReport.sql
@@ -18,9 +18,15 @@ SELECT
 	,SCD.dblShrink
 	,SCD.dblNet
 	,SCD.ysnPost
+	,EM.strAddress AS strEntityAddress
 	,EMS.strSplitNumber
 	,ICC.strCommodityCode	
+	,SMC.strCompanyName
 	,(SELECT intCurrencyDecimal FROM tblSMCompanyPreference) AS intDecimalPrecision
 FROM vyuSCDeliverySheetView SCD
+INNER JOIN tblEMEntityLocation EM ON EM.intEntityId = SCD.intEntityId AND EM.ysnDefaultLocation = 1
 LEFT JOIN tblEMEntitySplit EMS ON EMS.intSplitId = SCD.intSplitId
 LEFT JOIN tblICCommodity ICC ON ICC.intCommodityId = SCD.intCommodityId
+OUTER APPLY(
+	SELECT TOP 1 * FROM tblSMCompanySetup
+) SMC
