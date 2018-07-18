8ed7ef259c71091ded2744582a1766891505ce88
jessa delo-ag <jessa.delo-ag@irely.com>
2018-07-18 13:52:26 +0800

    SM-4282

--- i21Database/Scripts/Post-Deployment/SM/1830_DeleteDuplicatetblSMScreenData.sql
+++ i21Database/Scripts/Post-Deployment/SM/1830_DeleteDuplicatetblSMScreenData.sql
@@ -3,4 +3,14 @@
 	BEGIN
 		DELETE FROM tblSMScreen WHERE strScreenId = 'My Company (Portal)'
 	END
+
+	IF EXISTS(SELECT * FROM tblSMScreen WHERE strNamespace LIKE '%AccountsReceivable.view.EntityCustomer%')
+	BEGIN
+		WITH cte AS (
+		SELECT row_number() OVER(PARTITION BY strNamespace ORDER BY intScreenId ) AS [rn]
+		FROM tblSMScreen
+		where strNamespace like '%AccountsReceivable.view.EntityCustomer%'
+		)
+		DELETE cte WHERE [rn] > 1
+	END
 GO
