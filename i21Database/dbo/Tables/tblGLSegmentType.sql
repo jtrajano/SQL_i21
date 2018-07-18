5068a2b8cf59b34833e3fa510fce9ae825253160
JeffreyTrajano <jeffrey.trajano@irely.com>
2018-07-18 15:29:59 +0800

    GL-6510 Make tblGLSegmentType primary key identity seed

--- i21Database/dbo/Tables/tblGLSegmentType.sql
+++ i21Database/dbo/Tables/tblGLSegmentType.sql
@@ -1,13 +1,21 @@
-﻿CREATE TABLE [dbo].[tblGLSegmentType]
+﻿CREATE TABLE [dbo].[tblGLSegmentType](
+	[intSegmentTypeId] [int] IDENTITY(1,1) NOT NULL,
+	[strSegmentType] [nvarchar](50) NULL,
+	[intConcurrencyId] [int] NOT NULL,
+ CONSTRAINT [PK_tblGLSegmentType] PRIMARY KEY CLUSTERED 
 (
-	[intSegmentTypeId] INT IDENTITY(1,1) NOT NULL,
-    [strSegmentType] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL,
-	[intConcurrencyId] INT NOT NULL
-)
+	[intSegmentTypeId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
 GO
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Segment Type Id' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'tblGLSegmentType', @level2type=N'COLUMN',@level2name=N'intSegmentTypeId' 
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Segment Type Id' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'tblGLSegmentType', @level2type=N'COLUMN',@level2name=N'intSegmentTypeId'
 GO
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Segment Type' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'tblGLSegmentType', @level2type=N'COLUMN',@level2name=N'strSegmentType' 
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Segment Type' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'tblGLSegmentType', @level2type=N'COLUMN',@level2name=N'strSegmentType'
 GO
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Concurrency Id' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'tblGLSegmentType', @level2type=N'COLUMN',@level2name=N'intConcurrencyId' 
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Concurrency Id' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'tblGLSegmentType', @level2type=N'COLUMN',@level2name=N'intConcurrencyId'
 GO
+
+
