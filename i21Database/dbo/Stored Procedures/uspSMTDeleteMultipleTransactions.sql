6f56620b633702ccfc92bef650856ef00ca76f22
COMTECH\JMendones <jesmar.mendones@irely.com>
2018-07-19 15:42:05 +0800

    FRM-7556

---
+++ i21Database/dbo/Stored Procedures/uspSMTDeleteMultipleTransactions.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[uspSMTDeleteMultipleTransactions]
+	@recordIds nvarchar(500),
+	@namespace nvarchar(500)
+AS
+BEGIN TRY
+	BEGIN TRANSACTION
+		IF ISNULL(@recordIds, 0) <> 0 and ISNULL(@namespace, 0) <> 0
+		BEGIN
+			DELETE FROM tblSMTransaction where intTransactionId in (select * from dbo.fnSplitStringWithTrim(@recordIds,','))
+			AND intScreenId = (SELECT top 1 intScreenId from tblSMScreen where strNamespace = @namespace)
+		END
+	COMMIT TRANSACTION
+END TRY
+BEGIN CATCH
+	IF @@TRANCOUNT > 0
+		ROLLBACK TRANSACTION
+END CATCH
