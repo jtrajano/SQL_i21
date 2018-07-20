45acf02df5fece2122bc0844590abb34f21902fb
Feb Montefrio <feb.montefrio@irely.com>
2018-07-20 15:26:20 +0800

    [B] IC-5781

--- i21Database/dbo/Stored Procedures/uspICCreateGLEntriesForInTransitCosting.sql
+++ i21Database/dbo/Stored Procedures/uspICCreateGLEntriesForInTransitCosting.sql
@@ -46,13 +46,15 @@ INSERT INTO @GLAccounts (
 )
 SELECT	Query.intItemId
 		,Query.intItemLocationId
-		,intInventoryId = dbo.fnGetItemGLAccount(Query.intItemId, ISNULL(Query.intInTransitSourceLocationId, Query.intItemLocationId), @AccountCategory_Inventory_In_Transit) 
-		,intContraInventoryId = dbo.fnGetItemGLAccount(Query.intItemId, ISNULL(Query.intInTransitSourceLocationId, Query.intItemLocationId), @AccountCategory_ContraInventory) 
-		,intAutoNegativeId = dbo.fnGetItemGLAccount(Query.intItemId, ISNULL(Query.intInTransitSourceLocationId, Query.intItemLocationId), @AccountCategory_Auto_Variance) 
+		,intInventoryId = dbo.fnGetItemGLAccount(Query.intItemId, Query.intItemLocationId, @AccountCategory_Inventory_In_Transit) 
+		,intContraInventoryId = dbo.fnGetItemGLAccount(Query.intItemId, Query.intItemLocationId, @AccountCategory_ContraInventory) 
+		,intAutoNegativeId = dbo.fnGetItemGLAccount(Query.intItemId, Query.intItemLocationId, @AccountCategory_Auto_Variance) 
 		,intTransactionTypeId
 FROM	(
 			SELECT	DISTINCT 
-					intItemId, intItemLocationId, intTransactionTypeId, intInTransitSourceLocationId
+					intItemId
+					, intItemLocationId = ISNULL(intInTransitSourceLocationId, intItemLocationId)
+					, intTransactionTypeId
 			FROM	dbo.tblICInventoryTransaction t 
 			WHERE	t.strBatchId = @strBatchId
 					AND t.intItemId = ISNULL(@intRebuildItemId, t.intItemId) 
@@ -239,7 +241,7 @@ AS
 (
 	SELECT	t.dtmDate
 			,t.intItemId
-			,t.intItemLocationId
+			,intItemLocationId = ISNULL(t.intInTransitSourceLocationId, t.intItemLocationId)
 			,t.intTransactionId
 			,t.strTransactionId
 			,t.dblQty
