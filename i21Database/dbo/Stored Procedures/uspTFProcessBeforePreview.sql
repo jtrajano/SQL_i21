af41d4118ae13fd9a97f39473dd6f2db97b22d83
Crisman Lazaga <cris.lazaga@irely.com>
2018-07-20 15:48:30 +0800

    MFT-1204

--- i21Database/dbo/Stored Procedures/uspTFProcessBeforePreview.sql
+++ i21Database/dbo/Stored Procedures/uspTFProcessBeforePreview.sql
@@ -70,7 +70,6 @@ BEGIN TRY
 			WHERE intTransactionId IN (SELECT DISTINCT intTransactionId FROM #tmpTransaction)
 				AND (ISNULL(strOriginTCN, '') <> '' OR ISNULL(strDestinationTCN, '') <> '')
 		END
-
 		ELSE IF (@TaxAuthorityCode = 'OR')
 		BEGIN
 			DELETE FROM tblTFTransactionDynamicOR
@@ -198,31 +197,35 @@ BEGIN TRY
 
 			INSERT INTO tblTFTransactionDynamicMI(
 				intTransactionId
-				, strMIOriginCountry
+				, strMIDestinationAddress
 				, strMIDestinationCountry
-			)
-			SELECT Trans.intTransactionId
-				, strCounty = 'USA'
-				, strLocation = 'USA'
-			FROM #tmpTransaction Trans
-		END
-
-		ELSE IF (@TaxAuthorityCode = 'MI')
-		BEGIN
-			DELETE FROM tblTFTransactionDynamicMI
-			WHERE intTransactionId IN (
-				SELECT intTransactionId FROM #tmpTransaction
-			)
-
-			INSERT INTO tblTFTransactionDynamicMI(
-				intTransactionId
+				, strMIDestinationTCN
+				, strMIDestinationZipCode
+				, strMIOriginAddress
 				, strMIOriginCountry
-				, strMIDestinationCountry
+				, strMIOriginZipCode
 			)
-			SELECT Trans.intTransactionId
-				, strCounty = 'USA'
-				, strLocation = 'USA'
-			FROM #tmpTransaction Trans
+			SELECT intTransactionId, UnCommonField.*  
+			FROM (
+				SELECT [strMIDestinationAddress], [strMIDestinationCountry], [strMIDestinationTCN], [strMIDestinationZipCode], [strMIOriginAddress] , [strMIOriginCountry], [strMIOriginZipCode]
+				FROM  
+				(
+					SELECT RCFIELD.strColumn, RCCONFIG.strConfiguration FROM
+					tblTFReportingComponentField RCFIELD
+					INNER JOIN tblTFReportingComponentConfiguration RCCONFIG ON RCCONFIG.strDescription = RCFIELD.strColumn AND RCCONFIG.intReportingComponentId = RCFIELD.intReportingComponentId
+					WHERE RCFIELD.ysnFromConfiguration = 1
+					AND RCCONFIG.ysnOutputDesigner = 1
+					AND ISNULL(RCCONFIG.strConfiguration, '') <> ''
+					AND RCFIELD.intReportingComponentId = @RCId
+				) AS SourceTable  
+				PIVOT  
+				(  
+					MAX(strConfiguration)  
+					FOR strColumn IN ([strMIDestinationAddress], [strMIDestinationCountry], [strMIDestinationTCN], [strMIDestinationZipCode], [strMIOriginAddress] , [strMIOriginCountry], [strMIOriginZipCode])  
+				) AS PvtTbl
+			) UnCommonField 
+			CROSS JOIN tblTFTransaction
+
 		END
 
 		DELETE FROM #tmpRC WHERE intReportingComponentId = @RCId
