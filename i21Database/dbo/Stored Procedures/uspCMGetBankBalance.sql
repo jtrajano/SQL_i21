378fde4f3e038f3dd4dc437493d3a632ac7f11f1
JeffreyTrajano <jeffrey.trajano@irely.com>
2018-07-24 13:13:06 +0800

    GL-6521 [Bank account register] balances in CAD account is not the same with bank account balance GL-6522 [Bank Reconciliation] Bank account in CAD is not balance

--- i21Database/dbo/Stored Procedures/uspCMGetBankBalance.sql
+++ i21Database/dbo/Stored Procedures/uspCMGetBankBalance.sql
@@ -1,7 +1,8 @@
 ﻿
 CREATE PROCEDURE [dbo].[uspCMGetBankBalance]
 	@intBankAccountId INT = NULL,
-	@dtmDate DATETIME = NULL
+	@dtmDate DATETIME = NULL,
+	@isForeignCurrency BIT = 0
 AS
 
 SET QUOTED_IDENTIFIER OFF
@@ -9,24 +10,6 @@ SET ANSI_NULLS ON
 SET NOCOUNT ON
 SET XACT_ABORT ON
 SET ANSI_WARNINGS OFF
-
-DECLARE @isForeignCurrency AS BIT = 1
-DECLARE @intDefaultCurrencyId INT , @intCurrencyId INT
-
-SELECT TOP 1 @intCurrencyId = intCurrencyId FROM tblCMBankAccount  WHERE intBankAccountId = @intBankAccountId 
-SELECT TOP 1 @intDefaultCurrencyId = intDefaultCurrencyId FROM tblSMCompanyPreference
-
-IF @intCurrencyId IS NULL 
-BEGIN
-	RAISERROR('Bank Currency was not set.', 11, 1)
-	RETURN 0
-END
-IF @intDefaultCurrencyId IS NULL 
-BEGIN
-	RAISERROR('Default Currency was not set.', 11, 1)
-	RETURN 0
-END
-IF @intCurrencyId = @intDefaultCurrencyId SET @isForeignCurrency = 0
 
 
 SELECT	intBankAccountId = @intBankAccountId,
