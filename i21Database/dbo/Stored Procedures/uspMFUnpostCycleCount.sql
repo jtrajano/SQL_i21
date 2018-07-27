8c9de61e5717ae75d15a5cd057711535d02acb7d
Premkumar S <prem.selladurai@irely.com>
2018-07-27 20:17:40 +0530

    MFG-3654,MFG-3696,MFG-3692

--- i21Database/dbo/Stored Procedures/uspMFUnpostCycleCount.sql
+++ i21Database/dbo/Stored Procedures/uspMFUnpostCycleCount.sql
@@ -33,10 +33,12 @@ BEGIN TRY
 		,@dblOtherCharges DECIMAL(38, 24)
 		,@dblProduceQty NUMERIC(38, 20)
 		,@dtmCurrentDateTime DATETIME
-						,@ysnCostEnabled BIT
+		,@ysnCostEnabled BIT
+		,@intWOItemUOMId int
+		,@intUnitMeasureId int
 
 	SELECT TOP 1 @ysnCostEnabled=ysnCostEnabled
-				FROM tblMFCompanyPreference
+		FROM tblMFCompanyPreference
 
 	EXEC sp_xml_preparedocument @idoc OUTPUT
 		,@strXML
@@ -51,8 +53,13 @@ BEGIN TRY
 	SELECT @intManufacturingProcessId = intManufacturingProcessId
 		,@strCostAdjustmentBatchId = strCostAdjustmentBatchId
 		,@intLocationId = intLocationId
+		,@intWOItemUOMId=intItemUOMId
 	FROM tblMFWorkOrder
 	WHERE intWorkOrderId = @intWorkOrderId
+
+			Select @intUnitMeasureId=intUnitMeasureId
+	From tblICItemUOM
+	Where intItemUOMId=@intWOItemUOMId
 
 	SELECT @strAttributeValue = strAttributeValue
 	FROM tblMFManufacturingProcessAttribute
@@ -60,8 +67,9 @@ BEGIN TRY
 		AND intAttributeId = 20 --Is Instant Consumption
 		AND intLocationId = @intLocationId
 
-	SELECT @dblProduceQty = SUM(dblQuantity)
+	SELECT @dblProduceQty = SUM(dbo.fnMFConvertQuantityToTargetItemUOM(WP.intItemUOMId,IsNULL(IU.intItemUOMId,WP.intItemUOMId),WP.dblQuantity))
 	FROM dbo.tblMFWorkOrderProducedLot WP
+	Left JOIN dbo.tblICItemUOM IU on IU.intItemId=WP.intItemId and IU.intUnitMeasureId=@intUnitMeasureId
 	WHERE WP.intWorkOrderId = @intWorkOrderId
 		AND WP.ysnProductionReversed = 0
 		AND WP.intItemId IN (
@@ -191,10 +199,10 @@ BEGIN TRY
 			,[dblQty] = PL.dblQuantity
 			,[dblUOMQty] = 1
 			,[intCostUOMId] = PL.intItemUOMId
-			,[dblNewCost] = CASE 
+					,[dblNewCost] = CASE 
 				WHEN IsNULL(RI.dblPercentage, 0) = 0
-					THEN @dblNewUnitCost * PL.dblQuantity
-				ELSE ((@dblNewUnitCost * RI.dblPercentage / 100) * PL.dblQuantity)
+					THEN @dblNewUnitCost * dbo.fnMFConvertQuantityToTargetItemUOM(PL.intItemUOMId,IsNULL(IU.intItemUOMId,PL.intItemUOMId),PL.dblQuantity)
+				ELSE ((@dblNewCost * RI.dblPercentage / 100/SUM(dbo.fnMFConvertQuantityToTargetItemUOM(PL.intItemUOMId,IsNULL(IU.intItemUOMId,PL.intItemUOMId),PL.dblQuantity)) Over(Partition By PL.intItemId)) * dbo.fnMFConvertQuantityToTargetItemUOM(PL.intItemUOMId,IsNULL(IU.intItemUOMId,PL.intItemUOMId),PL.dblQuantity))
 				END
 			,[intCurrencyId] = (
 				SELECT TOP 1 intDefaultReportingCurrencyId
@@ -216,6 +224,7 @@ BEGIN TRY
 			,intFobPointId = 2
 		FROM dbo.tblMFWorkOrderProducedLot PL
 		JOIN dbo.tblMFWorkOrder W ON W.intWorkOrderId = PL.intWorkOrderId
+		Left JOIN dbo.tblICItemUOM IU on IU.intItemId=PL.intItemId and IU.intUnitMeasureId=@intUnitMeasureId
 		JOIN tblICLot L ON L.intLotId = PL.intProducedLotId
 		LEFT JOIN tblMFWorkOrderRecipeItem RI ON RI.intWorkOrderId = W.intWorkOrderId
 			AND RI.intItemId = PL.intItemId
