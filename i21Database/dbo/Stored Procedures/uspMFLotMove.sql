8260bd5fb1ccb66fe0ae4db6de03f477b7c78fe1
Premkumar S <prem.selladurai@irely.com>
2018-07-30 22:26:30 +0530

    MFG-3688

--- i21Database/dbo/Stored Procedures/uspMFLotMove.sql
+++ i21Database/dbo/Stored Procedures/uspMFLotMove.sql
@@ -200,10 +200,8 @@ BEGIN TRY
 				SELECT intLotId
 				FROM tblICLot
 				WHERE intStorageLocationId = @intNewStorageLocationId
-					AND (
-						dblQty > 0
-						OR dblWeight > 0
-						)
+					AND dblQty > 0 
+					and intItemId <> @intItemId
 				)
 		BEGIN
 			RAISERROR (
