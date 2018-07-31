1b7db822e204ff9823dbc962d79e31e4125aaac3
Satya S Sahoo <satya.sahoo@irely.com>
2018-07-31 22:18:02 +0530

    CT-2570 - Add dblBalanceInItemStockUOM and dblBalance - Clone - 18.1Q

--- i21Database/dbo/Views/vyuCTGetContractForScaleTicket.sql
+++ i21Database/dbo/Views/vyuCTGetContractForScaleTicket.sql
@@ -8,7 +8,8 @@ AS
 			CD.dtmStartDate,				
 			CD.intItemId,																			
 			CD.dtmEndDate,																										
-			CD.dblBasis,																				
+			CD.dblBasis,
+			CD.dblBalance,																				
 			CD.dblCashPrice,																						
 			CD.intContractStatusId,																			
 			IM.strItemNo,										
@@ -36,7 +37,8 @@ AS
 			CH.strContractNumber,
 			CH.ysnUnlimitedQuantity,
 			CD.intContractSeq,
-			CD.intPricingTypeId
+			CD.intPricingTypeId,
+			dbo.fnCTConvertQtyToTargetItemUOM(CD.intItemUOMId,SK.intStockUOMId,ISNULL(CD.dblBalance,0))			AS	dblBalanceInItemStockUOM
 																												
 	FROM	tblCTContractDetail				CD	CROSS																						
 	JOIN	tblCTCompanyPreference			CP																									
