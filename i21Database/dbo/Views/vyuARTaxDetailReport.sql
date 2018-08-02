79692a602d0ed1ac83b194c656d861f7babe2c2e
Kelvin Quinto <kelvin.quinto@irely.com>
2018-08-02 15:03:16 +0800

    AR-8302, AR-8310

--- i21Database/dbo/Views/vyuARTaxDetailReport.sql
+++ i21Database/dbo/Views/vyuARTaxDetailReport.sql
@@ -2,26 +2,31 @@
 AS 
 SELECT TAXDETAIL.*
      , SMT.strTaxCode
+	 , SMT.strDescription
 FROM (
-	SELECT intTransactionDetailTaxId	=  IDT.intInvoiceDetailTaxId
+	SELECT intTransactionDetailTaxId	= IDT.intInvoiceDetailTaxId
 		 , intTransactionDetailId		= IDT.intInvoiceDetailId
-		 , IDT.dblAdjustedTax
-		 , IDT.intTaxCodeId
+		 , dblAdjustedTax				= IDT.dblAdjustedTax
+		 , dblRate						= IDT.dblRate
+		 , intTaxCodeId					= IDT.intTaxCodeId
+		 , strCalculationMethod			= IDT.strCalculationMethod
 		 , strTaxTransactionType		= 'Invoice'
-	FROM tblARInvoiceDetailTax IDT 
-		INNER JOIN tblARInvoiceDetail ID ON IDT.intInvoiceDetailId = ID.intInvoiceDetailId
-			WHERE IDT.dblAdjustedTax <> 0 
-			  AND ID.intItemId <> ISNULL((SELECT TOP 1 intItemForFreightId FROM tblTRCompanyPreference), 0)
+	FROM tblARInvoiceDetailTax IDT 	
+	INNER JOIN tblARInvoiceDetail ID ON IDT.intInvoiceDetailId = ID.intInvoiceDetailId
+	WHERE IDT.dblAdjustedTax <> 0 
+	  AND ID.intItemId <> ISNULL((SELECT TOP 1 intItemForFreightId FROM tblTRCompanyPreference), 0)
 
 	UNION ALL
 
 	SELECT intTransactionDetailTaxId	= SDT.intSalesOrderDetailTaxId
 		 , intTransactionDetailId		= SDT.intSalesOrderDetailId
-		 , SDT.dblAdjustedTax
-		 , SDT.intTaxCodeId
-		 , strTaxTransactionType   = 'Sales Order'
+		 , dblAdjustedTax				= SDT.dblAdjustedTax
+		 , dblRate						= SDT.dblRate
+		 , intTaxCodeId					= SDT.intTaxCodeId
+		 , strCalculationMethod			= SDT.strCalculationMethod
+		 , strTaxTransactionType   		= 'Sales Order'
 	FROM tblSOSalesOrderDetailTax SDT
-		WHERE SDT.dblAdjustedTax <> 0
+	WHERE SDT.dblAdjustedTax <> 0
 ) AS TAXDETAIL
 LEFT JOIN tblSMTaxCode SMT ON
        TAXDETAIL.intTaxCodeId = SMT.intTaxCodeId 
