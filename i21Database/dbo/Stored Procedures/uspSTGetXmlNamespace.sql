30688700f85eaf4785849233f1acf76f07baaf02
COMTECH\HZaragoza <henry.zaragoza@irely.com>
2018-08-03 22:07:17 +0800

    ST-402

---
+++ i21Database/dbo/Stored Procedures/uspSTGetXmlNamespace.sql
@@ -0,0 +1,60 @@
+﻿CREATE PROCEDURE [dbo].[uspSTGetXmlNamespace]
+	@intStoreNo INT
+	, @strFilePrefix NVARCHAR(20)
+	, @strRegisterClass NVARCHAR(50) OUTPUT
+	, @strXmlNamespace NVARCHAR(50) OUTPUT
+	, @stri21FolderPath NVARCHAR(500) OUTPUT
+	, @ysnSuccess BIT OUTPUT
+	, @strStatusMsg NVARCHAR(1000) OUTPUT
+AS
+BEGIN TRY
+	
+	SET @strXmlNamespace = ''
+
+	DECLARE @intStoreId AS INT
+			, @strPrefix AS NVARCHAR(20)
+
+	SELECT @intStoreId = ST.intStoreId 
+		   , @strRegisterClass = R.strRegisterClass
+		   , @stri21FolderPath = REPLACE((R.strRegisterInboxPath + RFC.strFolderPath), '/', '\')
+	FROM tblSTStore ST
+	JOIN tblSTRegister R
+		ON ST.intRegisterId = R.intRegisterId
+	JOIN tblSTRegisterFileConfiguration RFC
+		ON R.intRegisterId = RFC.intRegisterId
+	WHERE ST.intStoreNo = @intStoreNo
+	AND RFC.strFileType = 'Inbound'
+	AND RFC.strFilePrefix = @strFilePrefix
+
+
+	SELECT @strXmlNamespace = strDefaultValue 
+	FROM tblSMXMLTagAttribute
+	WHERE intImportFileColumnDetailId = (
+											SELECT intImportFileColumnDetailId 
+											FROM tblSMImportFileColumnDetail
+											WHERE intImportFileHeaderId = (
+																			SELECT TOP 1 intImportFileHeaderId 
+																			FROM tblSTRegisterFileConfiguration
+																			WHERE intRegisterId = (
+																									SELECT intRegisterId 
+																									FROM tblSTStore
+																									WHERE intStoreId = @intStoreId
+																								  )
+																			AND strFileType = 'Inbound'
+																			AND strFilePrefix = @strFilePrefix
+																		  )
+											AND intLevel = 1
+											AND intPosition = 0
+											AND ysnActive = CAST(1 AS BIT)
+										)
+	AND intSequence = 2
+	AND ysnActive = CAST(1 AS BIT)
+
+	SET @ysnSuccess = CAST(1 AS BIT)
+	SET @strStatusMsg = ''
+END TRY
+
+BEGIN CATCH
+	SET @ysnSuccess = CAST(0 AS BIT)
+	SET @strStatusMsg = ERROR_MESSAGE()
+END CATCH
---
+++ i21Database/dbo/Tables/tblSTStoreAppUploadHistory.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[tblSTStoreAppUploadHistory]
+(
+    [intStoreAppUploadHistoryId] INT				NOT NULL IDENTITY,
+	[intStoreNo]				INT					NULL,
+	[strLocalFolderPath]		NVARCHAR(500)		COLLATE Latin1_General_CI_AS NULL,				-- 'C:\Users\HZaragoza\Desktop\Register\Passport\Inbound'
+	[strLocalFilename]			NVARCHAR(100)		COLLATE Latin1_General_CI_AS NULL,				-- Include the extension name 'FGM20180801102433.xml'
+	[stri21FolderPath]			NVARCHAR(500)		COLLATE Latin1_General_CI_AS NULL,				-- 'C:\Users\HZaragoza\Desktop\Register\Passport\Inbound'
+	[stri21ConvertedFilename]	NVARCHAR(100)		COLLATE Latin1_General_CI_AS NULL,				-- Converted Filename will be based on Shift Closed date 'Prefix+[yyyyMMddHHmmss]' ISM20180801102433.xml
+	[strFilePrefix]				NVARCHAR(20)		COLLATE Latin1_General_CI_AS NULL,
+	[dtmShiftCloseDate]			DATETIME			NULL,
+	[ysnUploadSuccess]			BIT					DEFAULT CAST(0 AS BIT) NOT NULL,
+	[strRemark]					NVARCHAR(1000)		COLLATE Latin1_General_CI_AS NULL,
+	[intConcurrencyId]          INT					DEFAULT CAST(1 AS BIT) NOT NULL
+    CONSTRAINT [PK_tblSTStoreAppUploadHistory] PRIMARY KEY CLUSTERED (
+		[intStoreAppUploadHistoryId] ASC
+	)
+)
