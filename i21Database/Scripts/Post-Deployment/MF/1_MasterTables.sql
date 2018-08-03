33ff5599c52661e4c71b151ff3db310f4d19f2b4
Premkumar S <prem.selladurai@irely.com>
2018-08-03 18:09:15 +0530

    MFG-3660

--- i21Database/Scripts/Post-Deployment/MF/1_MasterTables.sql
+++ i21Database/Scripts/Post-Deployment/MF/1_MasterTables.sql
@@ -3407,3 +3407,7 @@ UPDATE tblMFRecipeItem
 SET dblCalculatedQuantity = dblQuantity
 WHERE intRecipeItemTypeId = 2 AND dblCalculatedQuantity = 0
 GO
+UPDATE tblMFCompanyPreference
+SET ysnWorkOrderPlannedDateByBusinessDate = 1
+Where ysnWorkOrderPlannedDateByBusinessDate IS NULL
+GO
