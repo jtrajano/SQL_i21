8baa1303020609a86b719162bbe97432730e6d13
Feb Montefrio <feb.montefrio@irely.com>
2018-08-03 18:09:17 +0800

    [B] IC-5903

---
+++ i21Database/dbo/Stored Procedures/uspICRepostReturnCosting.sql
@@ -0,0 +1,358 @@
+﻿CREATE PROCEDURE [dbo].[uspICRepostReturnCosting]
+	@strBatchId AS NVARCHAR(40)
+	,@strAccountToCounterInventory AS NVARCHAR(255) = 'AP Clearing'
+	,@intEntityUserSecurityId AS INT
+	,@strGLDescription AS NVARCHAR(255) = NULL 
+	,@ItemsToReturn AS ItemCostingTableType READONLY 
+AS
+
+SET QUOTED_IDENTIFIER OFF
+SET ANSI_NULLS ON
+SET NOCOUNT ON
+SET XACT_ABORT ON
+SET ANSI_WARNINGS OFF
+
+-- Declare the variables to use for the cursor
+DECLARE @intId AS INT 
+		,@intItemId AS INT
+		,@intItemLocationId AS INT 
+		,@intItemUOMId AS INT 
+		,@dtmDate AS DATETIME
+		,@dblQty AS NUMERIC(38,20) 
+		,@dblUOMQty AS NUMERIC(38,20)
+		,@dblCost AS NUMERIC(38,20)
+		,@dblSalesPrice AS NUMERIC(18, 6)
+		,@intCurrencyId AS INT 
+		--,@dblExchangeRate AS NUMERIC (38,20) 
+		,@intTransactionId AS INT
+		,@intTransactionDetailId AS INT 
+		,@strTransactionId AS NVARCHAR(40) 
+		,@intTransactionTypeId AS INT 
+		,@intLotId AS INT
+		,@intSubLocationId AS INT
+		,@intStorageLocationId AS INT 
+		,@strActualCostId AS NVARCHAR(50)
+		,@intForexRateTypeId AS INT
+		,@dblForexRate NUMERIC(38, 20)
+
+DECLARE @CostingMethod AS INT 
+		,@strTransactionForm AS NVARCHAR(255)
+
+-- Declare the costing methods
+DECLARE @AVERAGECOST AS INT = 1
+		,@FIFO AS INT = 2
+		,@LIFO AS INT = 3
+		,@LOTCOST AS INT = 4 	
+		,@ACTUALCOST AS INT = 5	
+
+-- Create the variables for the internal transaction types used by costing. 
+DECLARE @AUTO_NEGATIVE AS INT = 1
+		,@WRITE_OFF_SOLD AS INT = 2
+		,@REVALUE_SOLD AS INT = 3
+
+DECLARE @intReturnValue AS INT 
+
+-----------------------------------------------------------------------------------------------------------------------------
+-- Do the Validation
+-----------------------------------------------------------------------------------------------------------------------------
+BEGIN 
+	DECLARE @returnValue AS INT 
+
+	EXEC @returnValue = dbo.uspICValidateCostingOnPost
+		@ItemsToValidate = @ItemsToReturn
+
+	IF @returnValue < 0 RETURN -1;
+END
+
+-----------------------------------------------------------------------------------------------------------------------------
+-- Create the cursor
+-- Make sure the following options are used: 
+-- LOCAL >> It specifies that the scope of the cursor is local to the stored procedure where it was created. The cursor name is only valid within this scope. 
+-- FAST_FORWARD >> It specifies a FORWARD_ONLY, READ_ONLY cursor with performance optimizations enabled. 
+-----------------------------------------------------------------------------------------------------------------------------
+DECLARE loopItems CURSOR LOCAL FAST_FORWARD
+FOR 
+SELECT  intId
+		,intItemId
+		,intItemLocationId
+		,intItemUOMId
+		,dtmDate
+		,-dblQty
+		,dblUOMQty
+		,dblCost
+		,dblSalesPrice
+		,intCurrencyId
+		--,dblExchangeRate
+		,intTransactionId
+		,intTransactionDetailId
+		,strTransactionId
+		,intTransactionTypeId
+		,intLotId
+		,intSubLocationId
+		,intStorageLocationId
+		,strActualCostId
+		,intForexRateTypeId
+		,dblForexRate 
+FROM	@ItemsToReturn
+
+OPEN loopItems;
+
+-- Initial fetch attempt
+FETCH NEXT FROM loopItems INTO 
+	@intId
+	,@intItemId
+	,@intItemLocationId
+	,@intItemUOMId
+	,@dtmDate
+	,@dblQty
+	,@dblUOMQty
+	,@dblCost
+	,@dblSalesPrice
+	,@intCurrencyId
+	--,@dblExchangeRate
+	,@intTransactionId
+	,@intTransactionDetailId
+	,@strTransactionId
+	,@intTransactionTypeId
+	,@intLotId
+	,@intSubLocationId
+	,@intStorageLocationId
+	,@strActualCostId
+	,@intForexRateTypeId
+	,@dblForexRate;
+	
+-----------------------------------------------------------------------------------------------------------------------------
+-- Start of the loop
+-----------------------------------------------------------------------------------------------------------------------------
+WHILE @@FETCH_STATUS = 0
+BEGIN 
+	-- Initialize the costing method and negative inventory option. 
+	SET @CostingMethod = NULL;
+
+	-- Initialize the transaction form
+	SELECT	@strTransactionForm = strTransactionForm
+	FROM	dbo.tblICInventoryTransactionType
+	WHERE	intTransactionTypeId = @intTransactionTypeId
+			AND strTransactionForm IS NOT NULL 
+
+	-- Get the costing method of an item 
+	SELECT	@CostingMethod = CostingMethod 
+	FROM	dbo.fnGetCostingMethodAsTable(@intItemId, @intItemLocationId)
+
+	-- Initialize the dblUOMQty
+	SELECT	@dblUOMQty = dblUnitQty
+	FROM	dbo.tblICItemUOM
+	WHERE	intItemId = @intItemId
+			AND intItemUOMId = @intItemUOMId
+
+	--------------------------------------------------------------------------------
+	-- Call the SP that can process the item's costing method
+	--------------------------------------------------------------------------------
+	-- Average Cost
+	IF (@CostingMethod = @AVERAGECOST AND @strActualCostId IS NULL)
+	BEGIN 
+		EXEC @intReturnValue = dbo.uspICPostReturnAverageCosting
+			@intItemId
+			,@intItemLocationId
+			,@intItemUOMId
+			,@intSubLocationId
+			,@intStorageLocationId
+			,@dtmDate
+			,@dblQty
+			,@dblUOMQty
+			,@dblCost
+			,@dblSalesPrice
+			,@intCurrencyId
+			--,@dblExchangeRate
+			,@intTransactionId
+			,@intTransactionDetailId
+			,@strTransactionId
+			,@strBatchId
+			,@intTransactionTypeId
+			,@strTransactionForm
+			,@intEntityUserSecurityId
+			,@intForexRateTypeId
+			,@dblForexRate
+
+		IF @intReturnValue < 0 GOTO _TerminateLoop
+	END
+
+	-- FIFO 
+	IF (@CostingMethod = @FIFO AND @strActualCostId IS NULL)
+	BEGIN 
+		EXEC @intReturnValue = dbo.uspICPostReturnFIFO
+			@intItemId
+			,@intItemLocationId
+			,@intItemUOMId
+			,@intSubLocationId
+			,@intStorageLocationId
+			,@dtmDate
+			,@dblQty
+			,@dblUOMQty
+			,@dblCost
+			,@dblSalesPrice
+			,@intCurrencyId
+			--,@dblExchangeRate
+			,@intTransactionId
+			,@intTransactionDetailId
+			,@strTransactionId
+			,@strBatchId
+			,@intTransactionTypeId
+			,@strTransactionForm
+			,@intEntityUserSecurityId
+			,@intForexRateTypeId
+			,@dblForexRate
+
+		IF @intReturnValue < 0 GOTO _TerminateLoop
+	END
+
+	-- LIFO 
+	IF (@CostingMethod = @LIFO AND @strActualCostId IS NULL)
+	BEGIN 
+		EXEC @intReturnValue = dbo.uspICPostReturnLIFO
+			@intItemId
+			,@intItemLocationId
+			,@intItemUOMId
+			,@intSubLocationId
+			,@intStorageLocationId
+			,@dtmDate
+			,@dblQty
+			,@dblUOMQty
+			,@dblCost
+			,@dblSalesPrice
+			,@intCurrencyId
+			--,@dblExchangeRate
+			,@intTransactionId
+			,@intTransactionDetailId
+			,@strTransactionId
+			,@strBatchId
+			,@intTransactionTypeId
+			,@strTransactionForm
+			,@intEntityUserSecurityId
+			,@intForexRateTypeId
+			,@dblForexRate
+
+		IF @intReturnValue < 0 GOTO _TerminateLoop
+	END
+
+	-- LOT 
+	IF (@CostingMethod = @LOTCOST AND @strActualCostId IS NULL)
+	BEGIN 
+		EXEC @intReturnValue = dbo.uspICPostReturnLot
+			@intItemId
+			,@intItemLocationId
+			,@intItemUOMId
+			,@intSubLocationId
+			,@intStorageLocationId
+			,@dtmDate
+			,@intLotId
+			,@dblQty
+			,@dblUOMQty
+			,@dblCost
+			,@dblSalesPrice
+			,@intCurrencyId
+			--,@dblExchangeRate
+			,@intTransactionId
+			,@intTransactionDetailId
+			,@strTransactionId
+			,@strBatchId
+			,@intTransactionTypeId
+			,@strTransactionForm
+			,@intEntityUserSecurityId
+			,@intForexRateTypeId
+			,@dblForexRate
+
+		IF @intReturnValue < 0 GOTO _TerminateLoop
+	END
+
+	-- ACTUAL COST 
+	IF (@strActualCostId IS NOT NULL)
+	BEGIN 
+		EXEC @intReturnValue = dbo.uspICPostReturnActualCost
+			@strActualCostId 
+			,@intItemId 
+			,@intItemLocationId 
+			,@intItemUOMId 
+			,@intSubLocationId 
+			,@intStorageLocationId 
+			,@dtmDate 
+			,@dblQty 
+			,@dblUOMQty 
+			,@dblCost 
+			,@dblSalesPrice 
+			,@intCurrencyId 
+			--,@dblExchangeRate 
+			,@intTransactionId 
+			,@intTransactionDetailId 
+			,@strTransactionId 
+			,@strBatchId 
+			,@intTransactionTypeId 
+			,@strTransactionForm 
+			,@intEntityUserSecurityId
+			,@intForexRateTypeId
+			,@dblForexRate
+
+		IF @intReturnValue < 0 GOTO _TerminateLoop
+	END
+
+	--------------------------------------------------
+	-- Adjust the average cost and units on hand. 
+	--------------------------------------------------
+	BEGIN 
+		-- Recalculate the average cost from the inventory transaction table. 
+		UPDATE	ItemPricing
+		SET		dblAverageCost = ISNULL(
+					dbo.fnRecalculateAverageCost(intItemId, intItemLocationId)
+					, dblAverageCost
+				) 
+				,ysnIsPendingUpdate = 1
+		FROM	dbo.tblICItemPricing ItemPricing	
+		WHERE	ItemPricing.intItemId = @intItemId
+				AND ItemPricing.intItemLocationId = @intItemLocationId
+				AND @strActualCostId IS NULL 
+
+		-- Recalculate the sales price because of the new average cost. 
+		EXEC @intReturnValue = uspICUpdateItemPricing
+			@intItemId
+			,@intItemLocationId
+
+		IF @intReturnValue < 0 GOTO _TerminateLoop
+	END 
+
+	-- Attempt to fetch the next row from cursor. 
+	FETCH NEXT FROM loopItems INTO 
+		@intId
+		,@intItemId
+		,@intItemLocationId
+		,@intItemUOMId
+		,@dtmDate
+		,@dblQty
+		,@dblUOMQty
+		,@dblCost
+		,@dblSalesPrice
+		,@intCurrencyId
+		--,@dblExchangeRate
+		,@intTransactionId
+		,@intTransactionDetailId
+		,@strTransactionId
+		,@intTransactionTypeId
+		,@intLotId
+		,@intSubLocationId
+		,@intStorageLocationId
+		,@strActualCostId 
+		,@intForexRateTypeId
+		,@dblForexRate
+END;
+-----------------------------------------------------------------------------------------------------------------------------
+-- End of the loop
+-----------------------------------------------------------------------------------------------------------------------------
+
+_TerminateLoop:
+
+CLOSE loopItems;
+DEALLOCATE loopItems;
+
+IF @intReturnValue < 0 RETURN @intReturnValue;
+
+_Exit: 
+RETURN 1
