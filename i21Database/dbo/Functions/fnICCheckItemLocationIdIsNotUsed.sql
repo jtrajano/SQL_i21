88bdef45618df88822854321417481f0442bf23d
Jb Mago <jb.mago@irely.com>
2018-08-06 13:01:36 +0800

    [B] IC-5917

---
+++ i21Database/dbo/Functions/fnICCheckItemLocationIdIsNotUsed.sql
@@ -0,0 +1,22 @@
+﻿-- Used in tblICItemLocation > [CK_ItemLocation_IS_NOT_USED] CONSTRAINT. 
+-- It will check if the item location already has a transaction. 
+CREATE FUNCTION [dbo].[fnICCheckItemLocationIdIsNotUsed](
+	@intItemLocationId AS INT,
+	@intLocationId AS INT
+)
+RETURNS BIT
+AS
+BEGIN
+
+	IF(@intLocationId IS NOT NULL
+		AND(
+			ISNULL((SELECT SUM([dbo].[fnMultiply](dblQty, dblUOMQty)) as AggregateQty FROM tblICInventoryTransaction WHERE intItemLocationId = @intItemLocationId 
+				AND ysnIsUnposted <> 1), 0) > 0
+		)
+	)
+	BEGIN
+		RETURN 1;
+	END
+
+	RETURN 0;
+END
