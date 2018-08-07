d718bcd14851cfc52d933b8c1cea3e05b85ea50d
Kelvin Quinto <kelvin.quinto@irely.com>
2018-08-07 18:18:08 +0800

    AR-8344

---
+++ i21Database/dbo/Views/vyuARPOSReceiptPaymentReport.sql
@@ -0,0 +1,15 @@
+﻿CREATE VIEW [dbo].[vyuARPOSReceiptPaymentReport]
+AS
+SELECT intPOSId			= POS.intPOSId
+	 , dblTotal			= POS.dblTotal
+	 , strPaymentMethod	= PAYMENT.strPaymentMethod
+	 , strReferenceNo	= PAYMENT.strReferenceNo
+	 , dblAmount		= PAYMENT.dblAmountTendered
+	 , dblTotalAmount	= TOTAL.dblTotalAmount
+FROM tblARPOS POS 
+INNER JOIN tblARPOSPayment PAYMENT ON POS.intPOSId = PAYMENT.intPOSId
+CROSS APPLY (
+	SELECT dblTotalAmount = SUM(POSP.dblAmountTendered) 
+	FROM tblARPOSPayment POSP
+	WHERE POSP.intPOSId = POS.intPOSId
+) TOTAL
