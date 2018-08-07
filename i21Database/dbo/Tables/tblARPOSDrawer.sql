86e51a5dbd40089911607560efa8c6ac5f142e4a
JBorlagdan <jc.borlagdan@irely.com>
2018-08-07 10:38:39 +0800

    AR-8139

---
+++ i21Database/dbo/Tables/tblARPOSDrawer.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[tblARPOSDrawer]
+(
+	[intPOSDrawerId] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	[strDrawerName] NVARCHAR(50) NOT NULL,
+	[ysnAllowMultipleUser] BIT NOT NULL,
+	[intCompanyLocationId] INT NOT NULL,
+	[intConcurrencyId] INT NOT NULL,
+	CONSTRAINT [FK_tblARPOSDrawer_tblSMCompanyLocation] FOREIGN KEY ([intCompanyLocationId]) REFERENCES [dbo].[tblSMCompanyLocation] ([intCompanyLocationId])
+)
