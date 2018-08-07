ea199a2ee84b0d6f061015a46864583b7c9d102c
manny armas <manny.armas@iRely.com>
2018-08-07 15:11:09 +0800

    IET-330 - error importing Item Messages to Energy Trac

--- i21Database/dbo/Views/vyuICETExportItemMessages.sql
+++ i21Database/dbo/Views/vyuICETExportItemMessages.sql
@@ -1,6 +1,6 @@
 ﻿CREATE VIEW [dbo].[vyuICETExportItemMessages]
 	AS SELECT 	 imsgno = strItemNo
-				,imsg1 = strInvoiceComments
+				,imsg1 = SUBSTRING(strInvoiceComments,1,60)
 				,imsg2 = ''
 				,imsg3 = ''
 				,iamsgno = 0
