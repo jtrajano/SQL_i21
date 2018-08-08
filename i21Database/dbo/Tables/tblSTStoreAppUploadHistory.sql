61bf2dff736b1d15cbe03abff6a94b72a49a2d9c
COMTECH\HZaragoza <henry.zaragoza@irely.com>
2018-08-08 12:10:40 +0800

    ST-891 - Store Local Register App > Delete after Upload

--- i21Database/dbo/Tables/tblSTStoreAppUploadHistory.sql
+++ i21Database/dbo/Tables/tblSTStoreAppUploadHistory.sql
@@ -8,6 +8,7 @@
 	[stri21ConvertedFilename]	NVARCHAR(100)		COLLATE Latin1_General_CI_AS NULL,				-- Converted Filename will be based on Shift Closed date 'Prefix+[yyyyMMddHHmmss]' ISM20180801102433.xml
 	[strFilePrefix]				NVARCHAR(20)		COLLATE Latin1_General_CI_AS NULL,
 	[dtmShiftCloseDate]			DATETIME			NULL,
+	[dtmDateUploaded]			DATETIME			NULL,
 	[ysnUploadSuccess]			BIT					DEFAULT CAST(0 AS BIT) NOT NULL,
 	[strRemark]					NVARCHAR(1000)		COLLATE Latin1_General_CI_AS NULL,
 	[intConcurrencyId]          INT					DEFAULT CAST(1 AS BIT) NOT NULL
