fbe3168d5fe8f33f4805605f3803d113e4ace4be
Lex Peregrino <Lex.Peregrino@irely.com>
2018-08-08 17:34:55 +0800

    AP-6066 1.0

---
+++ i21Database/dbo/Views/vyuAPBasisAdvanceTicket.sql
@@ -0,0 +1,17 @@
+﻿CREATE VIEW [dbo].[vyuAPBasisAdvanceTicket]
+
+AS
+
+SELECT 
+AP.intBillId
+,AP.strBillId
+,APBD.ysnRestricted
+,AP.ysnPosted
+,APBD.intScaleTicketId
+,SC.strTicketNumber
+FrOM tblAPBill AP
+INNER JOIN tblAPBillDetail APBD ON APBD.intBillId = AP.intBillId
+INNER JOIN tblSCTicket SC ON SC.intTicketId = APBD.intScaleTicketId
+where intTransactionType = 13 
+
+GO
