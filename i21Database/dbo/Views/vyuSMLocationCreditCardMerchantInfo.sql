09d990c05ce0995fde24d4833d4a688acbd57dcb
csJSarcilla <JSarcilla@COMTECH.COM>
2018-08-08 15:20:29 +0800

    AR-8346

---
+++ i21Database/dbo/Views/vyuSMLocationCreditCardMerchantInfo.sql
@@ -0,0 +1,4 @@
+﻿CREATE VIEW [dbo].[vyuSMLocationCreditCardMerchantInfo]
+	AS SELECT        intCompanyLocationId, strPaymentServer, strMerchantId, dbo.fnAESDecryptASym(strMerchantPassword) AS strMerchantPassword,
+	strCreditCardProcessingType, ysnEnableCreditCardProcessing, strPaymentPortal, strPaymentExternalLink
+	FROM	dbo.tblSMCompanyLocation
