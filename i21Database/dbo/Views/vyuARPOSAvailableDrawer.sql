6dac60abdc6c2ab940d2b000964c21c0973b3c0f
JBorlagdan <jc.borlagdan@irely.com>
2018-08-10 10:20:54 +0800

    AR-8139

---
+++ i21Database/dbo/Views/vyuARPOSAvailableDrawer.sql
@@ -0,0 +1,18 @@
+﻿CREATE VIEW [dbo].[vyuARPOSAvailableDrawer]
+	AS 
+	SELECT DISTINCT intPOSLogId,
+	   DRAWER.intCompanyLocationPOSDrawerId, 
+	   intCompanyLocationId, 
+	   strPOSDrawerName, 
+	   ysnAllowMultipleUser, 
+	   ysnLoggedIn
+
+FROM tblSMCompanyLocationPOSDrawer DRAWER
+LEFT JOIN (
+	SELECT intPOSLogId,
+			intCompanyLocationPOSDrawerId,
+			ysnLoggedIn
+	FROM vyuPOSGetLoggedIn
+) POSLOG ON  POSLOG.intCompanyLocationPOSDrawerId = DRAWER.intCompanyLocationPOSDrawerId
+WHERE ysnAllowMultipleUser = 1
+OR (ysnAllowMultipleUser = 0 AND ISNULL(ysnLoggedIn, 0) = 0) --single user drawer with no logged user
