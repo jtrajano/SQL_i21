fc914015c0f3b32c6f2fa544f49fc26cc5d54e67
JBorlagdan <jc.borlagdan@irely.com>
2018-08-10 08:09:55 +0800

    AR-8139

---
+++ i21Database/dbo/Stored Procedures/uspARUpdatePOSLog.sql
@@ -0,0 +1,38 @@
+﻿CREATE PROCEDURE [dbo].[uspARUpdatePOSLog]
+	@intPOSLogId AS INT,
+	@intCompanyLocationPOSDrawerId AS INT = NULL,
+	@intEntityUserId AS INT = NULL,
+	@intCompanyLocationId AS INT = NULL,
+	@intStoreId AS INT = NULL,
+	@intPOSLogOriginId AS INT = NULL,
+	@intBankDepositId AS INT = NULL,
+	@dblOpeningBalance AS DECIMAL(18,6) = NULL,
+	@dblEndingBalance AS DECIMAL(18,6) = NULL,
+	@dtmLogin AS DATETIME = NULL,
+	@dtmLogout AS DATETIME = NULL,
+	@ysnLoggedIn AS BIT = NULL
+AS
+BEGIN
+	
+	BEGIN TRANSACTION
+		UPDATE tblARPOSLog
+		SET
+				intEntityUserId				= ISNULL(@intEntityUserId, intEntityUserId),
+				intCompanyLocationPOSDrawerId	= ISNULL(@intCompanyLocationPOSDrawerId, intCompanyLocationPOSDrawerId),
+				intCompanyLocationId			= ISNULL(@intCompanyLocationId, intCompanyLocationId),
+				intStoreId						= ISNULL(@intStoreId, intStoreId),
+				intPOSLogOriginId				= ISNULL(@intPOSLogOriginId, intPOSLogOriginId),
+				intBankDepositId				= ISNULL(@intBankDepositId, intBankDepositId),
+				dblOpeningBalance				= ISNULL(@dblOpeningBalance, dblOpeningBalance),
+				dblEndingBalance				= ISNULL(@dblEndingBalance, dblEndingBalance),
+				dtmLogin						= ISNULL(@dtmLogin, dtmLogin),
+				dtmLogout						= ISNULL(@dtmLogout, dtmLogout),
+				ysnLoggedIn					= ISNULL(@ysnLoggedIn, ysnLoggedIn)
+		WHERE intPOSLogId = @intPOSLogId
+
+	IF(ISNULL(@intPOSLogId,0) != 0)
+		COMMIT
+	ELSE
+		ROLLBACK
+
+END
