ffca65115e4d58c9b98d8ed9d1f138ff88e602fe
Dan Jalimao <dan.jalimao@irely.com>
2018-08-13 14:51:35 +0800

    SM-4369

---
+++ i21Database/Scripts/Post-Deployment/SM/1810_Fix_GL_Account.sql
@@ -0,0 +1,29 @@
+﻿GO
+	PRINT N'START FIX GL ACCOUNTS'
+
+	UPDATE tblSMCompanyLocation SET intAPAccount = NULL WHERE intAPAccount NOT IN (SELECT intAccountId FROM tblGLAccount)
+	UPDATE tblSMCompanyLocation SET intARAccount = NULL WHERE intARAccount NOT IN (SELECT intAccountId FROM tblGLAccount)
+	UPDATE tblSMCompanyLocation SET intCashAccount = NULL WHERE intCashAccount NOT IN (SELECT intAccountId FROM tblGLAccount)
+	UPDATE tblSMCompanyLocation SET intDepositAccount = NULL WHERE intDepositAccount NOT IN (SELECT intAccountId FROM tblGLAccount)
+	UPDATE tblSMCompanyLocation SET intPurchaseAdvAccount = NULL WHERE intPurchaseAdvAccount NOT IN (SELECT intAccountId FROM tblGLAccount)
+	UPDATE tblSMCompanyLocation SET intSalesAdvAcct = NULL WHERE intSalesAdvAcct NOT IN (SELECT intAccountId FROM tblGLAccount)
+	UPDATE tblSMCompanyLocation SET intServiceCharges = NULL WHERE intServiceCharges NOT IN (SELECT intAccountId FROM tblGLAccount)
+	UPDATE tblSMCompanyLocation SET intSalesDiscounts = NULL WHERE intSalesDiscounts NOT IN (SELECT intAccountId FROM tblGLAccount)
+	UPDATE tblSMCompanyLocation SET intCashOverShort = NULL WHERE intCashOverShort NOT IN (SELECT intAccountId FROM tblGLAccount)
+	UPDATE tblSMCompanyLocation SET intWriteOff = NULL WHERE intWriteOff NOT IN (SELECT intAccountId FROM tblGLAccount)
+	UPDATE tblSMCompanyLocation SET intCreditCardFee = NULL WHERE intCreditCardFee NOT IN (SELECT intAccountId FROM tblGLAccount)
+	UPDATE tblSMCompanyLocation SET intSalesAccount = NULL WHERE intSalesAccount NOT IN (SELECT intAccountId FROM tblGLAccount)
+	UPDATE tblSMCompanyLocation SET intDiscountAccountId = NULL WHERE intDiscountAccountId NOT IN (SELECT intAccountId FROM tblGLAccount)
+	UPDATE tblSMCompanyLocation SET intInterestAccountId = NULL WHERE intInterestAccountId NOT IN (SELECT intAccountId FROM tblGLAccount)
+	UPDATE tblSMCompanyLocation SET intWithholdAccountId = NULL WHERE intWithholdAccountId NOT IN (SELECT intAccountId FROM tblGLAccount)
+	UPDATE tblSMCompanyLocation SET intUndepositedFundsId = NULL WHERE intUndepositedFundsId NOT IN (SELECT intAccountId FROM tblGLAccount)
+	UPDATE tblSMCompanyLocation SET intPrepaidAccountId = NULL WHERE intPrepaidAccountId NOT IN (SELECT intAccountId FROM tblGLAccount)
+	UPDATE tblSMCompanyLocation SET intDeferredPayableId = NULL WHERE intDeferredPayableId NOT IN (SELECT intAccountId FROM tblGLAccount)
+	UPDATE tblSMCompanyLocation SET intPettyCash = NULL WHERE intPettyCash NOT IN (SELECT intAccountId FROM tblGLAccount)
+	UPDATE tblSMCompanyLocation SET intDeferredRevenueId = NULL WHERE intDeferredRevenueId NOT IN (SELECT intAccountId FROM tblGLAccount)
+	UPDATE tblSMCompanyLocation SET intFreightAPAccount = NULL WHERE intFreightAPAccount NOT IN (SELECT intAccountId FROM tblGLAccount)
+	UPDATE tblSMCompanyLocation SET intFreightExpenses = NULL WHERE intFreightExpenses NOT IN (SELECT intAccountId FROM tblGLAccount)
+	UPDATE tblSMCompanyLocation SET intDeferredPayableInterestId = NULL WHERE intDeferredPayableInterestId NOT IN (SELECT intAccountId FROM tblGLAccount)
+	
+	PRINT N'END FIX GL ACCOUNTS'
+GO
