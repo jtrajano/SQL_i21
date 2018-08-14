87b20c9193dcf99db67cb96b45db952642533dc1
Lawrence Zabala <lawrence.zabala@irely.com>
2018-08-14 13:03:14 +0800

    MBIL-103

---
+++ i21Database/dbo/Stored Procedures/uspMBILCreateCustomer.sql
@@ -0,0 +1,45 @@
+﻿CREATE PROCEDURE [dbo].[uspMBILCreateCustomer]
+	@UserId INT,
+	@EntityId INT OUTPUT
+AS
+
+SET QUOTED_IDENTIFIER OFF
+SET ANSI_NULLS ON
+SET NOCOUNT ON
+SET XACT_ABORT ON
+SET ANSI_WARNINGS OFF
+
+DECLARE @ErrorMessage NVARCHAR(4000)
+DECLARE @ErrorSeverity INT
+DECLARE @ErrorState INT
+
+BEGIN TRY
+	
+	DECLARE @Message NVARCHAR(MAX)
+		, @DefaultCustomerNo NVARCHAR(50)
+		, @CountNo INT
+
+	EXEC uspEMCreateEntityById
+		@Id = @DefaultCustomerNo,
+		@Type = 'Customer',
+		@UserId = @UserId,
+		@Message = @Message OUTPUT,
+		@EntityId = @EntityId OUTPUT
+
+
+END TRY
+BEGIN CATCH
+	SELECT 
+		@ErrorMessage = ERROR_MESSAGE(),
+		@ErrorSeverity = ERROR_SEVERITY(),
+		@ErrorState = ERROR_STATE();
+
+	-- Use RAISERROR inside the CATCH block to return error
+	-- information about the original error that caused
+	-- execution to jump to the CATCH block.
+	RAISERROR (
+		@ErrorMessage, -- Message text.
+		@ErrorSeverity, -- Severity.
+		@ErrorState -- State.
+	);
+END CATCH
