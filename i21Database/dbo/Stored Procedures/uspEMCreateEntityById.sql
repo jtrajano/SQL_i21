9a2ca9fa400d8657bfce8f5aa30688628fabd2c9
Dan Jalimao <dan.jalimao@irely.com>
2018-08-14 16:04:46 +0800

    EM-2556

--- i21Database/dbo/Stored Procedures/uspEMCreateEntityById.sql
+++ i21Database/dbo/Stored Procedures/uspEMCreateEntityById.sql
@@ -9,13 +9,34 @@ BEGIN
 	SET NOCOUNT ON
 	DECLARE @EntityContactId int
 	DECLARE @EntityLocationId int
-		
+
+	-- Parameter Validations ------------------------------------------
 	IF EXISTS(SELECT TOP 1 1 FROM tblEMEntity where strEntityNo = @Id)
 	BEGIN	
 		SET @Message = 'Entity No already exists.'		
 		RETURN 0
 	END
-	--Add validation for existing entity no
+
+	IF ISNULL(@Id, '') = ''
+	BEGIN
+		SET @Message = 'Entity No cannot be empty.'
+		RETURN 0
+	END
+
+	IF ISNULL(@Type, '') = ''
+	BEGIN
+		SET @Message = 'Entity Type cannot be empty.'
+		RETURN 0
+	END
+
+	IF NOT EXISTS(SELECT TOP 1 1 FROM tblSMUserSecurity where intEntityId = @UserId)
+	BEGIN
+		SET @Message = 'User Id is not existing.'
+		RETURN 0
+	END
+	---------------------------------------------------------------------
+
+	-- Insert records ---------------------------------------------------
 	INSERT INTO tblEMEntity (strName, strContactNumber, strEntityNo)
 	SELECT @Id,'', @Id
 
@@ -34,24 +55,36 @@ BEGIN
 
 	INSERT INTO [tblEMEntityType](intEntityId, strType, intConcurrencyId)
 	SELECT @EntityId, @Type, 0
+	----------------------------------------------------------------------
+	
+	-- Insert record in the specific table and set the screen name -------
+	DECLARE @screen NVARCHAR(100)
+	SET @screen = 'EntityManagement.view.EntityDirect'
 
 	IF @Type = 'Vendor'
 	BEGIN
 		INSERT INTO tblAPVendor([intEntityId], strVendorId, intVendorType, ysnWithholding, dblCreditLimit)
 		SELECT @EntityId, @Id, 0, 0, 0
+
+		SET @screen = 'AccountsPayable.view.EntityVendor'
 	END
 	ELSE IF @Type = 'Customer'
 	BEGIN
-		INSERT INTO tblARCustomer(intEntityId, strCustomerNumber, strType, dblARBalance)
-		SELECT @EntityId, @Id, 'Person', 0.00
-	END
+		DECLARE @termId INT
+		SELECT @termId = intDefaultTermId FROM tblSMCompanyPreference
 
+		INSERT INTO tblARCustomer(intEntityId, strCustomerNumber, strType, dblARBalance, intTermsId)
+		SELECT @EntityId, @Id, 'Person', 0.00, @termId
+
+		SET @screen = 'AccountsReceivable.view.EntityCustomer'
+	END
+	----------------------------------------------------------------------
+
+	-- Add to audit log --------------------------------------------------
 	EXEC uspSMAuditLog
         @keyValue = @EntityId,
-        @screenName = 'EntityManagement.view.Entity',
+        @screenName = @screen,
         @entityId = @UserId,
         @actionType = 'Created'
-
+	----------------------------------------------------------------------
 END
-
-go
