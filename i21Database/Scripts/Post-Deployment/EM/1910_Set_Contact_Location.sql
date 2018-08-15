e9e3e5b2ce3f084c7447e6c3fa5209aabf82eba8
Dan Jalimao <dan.jalimao@irely.com>
2018-08-15 18:25:17 +0800

    EM-2558

---
+++ i21Database/Scripts/Post-Deployment/EM/1910_Set_Contact_Location.sql
@@ -0,0 +1,10 @@
+﻿GO
+	PRINT ('*****Begin Set Default Location*****')
+
+	UPDATE a SET a.intEntityLocationId = b.intEntityLocationId
+	FROM tblEMEntityToContact a
+	INNER JOIN tblEMEntityLocation b ON a.intEntityId = b.intEntityId AND b.ysnDefaultLocation = 1
+	WHERE a.intEntityLocationId IS NULL
+
+	PRINT ('*****End Set Default Location*****')
+GO
