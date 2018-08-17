b2f42d5b3908f69d2e4855288d0f29b6a08e443a
COMTECH\HZaragoza <henry.zaragoza@irely.com>
2018-08-17 11:50:26 +0800

    ST-893 Store Local Register App > Add Support for Downloading Pricebook Files

---
+++ i21Database/dbo/Views/vyuSTStoreAppRegistered.sql
@@ -0,0 +1,16 @@
+﻿CREATE VIEW [dbo].[vyuSTStoreAppRegistered]
+AS
+SELECT ST.intStoreId 
+, ST.strStoreAppMacAddress
+, ST.strStoreAppWebUrl
+, ST.intStoreNo
+, ST.guidStoreAppConnectionId
+, ST.dtmStoreAppLastDateLog
+, CASE
+	WHEN ISNULL(CU.strContextId, '') = ''
+		THEN CAST(0 AS BIT)
+	ELSE CAST(1 AS BIT)
+	END AS ysnStoreAppOnline
+FROM tblSTStore ST
+LEFT JOIN tblSMConnectedUser CU 
+	ON ST.guidStoreAppConnectionId = CU.strContextId
