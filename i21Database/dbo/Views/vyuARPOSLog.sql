881fd3a5cef35eba92455fdda17962f5def08fb9
JBorlagdan <jc.borlagdan@irely.com>
2018-08-17 11:13:53 +0800

    AR-8475, AR-8474

---
+++ i21Database/dbo/Views/vyuARPOSLog.sql
@@ -0,0 +1,40 @@
+﻿CREATE VIEW [dbo].[vyuARPOSLog]
+	AS 
+SELECT
+		 intPOSLogId					=	POSLog.intPOSLogId
+		,intEntityId					=	POSLog.intEntityUserId
+		,intCompanyLocationId			=	POSLog.intCompanyLocationId
+		,intCompanyLocationPOSDrawerId	=	POSLog.intCompanyLocationPOSDrawerId
+		,intStoreId						=	POSLog.intStoreId
+		,intPOSLogOriginId				=	POSLog.intPOSLogOriginId
+		,strUsername					=	ENTITY.strUserName
+		,strLocationName				=	LOC.strLocationName
+		,strPOSDrawerName				=	DRAWER.strPOSDrawerName
+		,strStoreName					=	STORE.strDescription
+		,dtmLogin						=	POSLog.dtmLogin
+		,dtmLogout						=	POSLog.dtmLogout
+		,ysnLoggedIn					=	POSLog.ysnLoggedIn
+ FROM tblARPOSLog POSLog WITH (NOLOCK)
+INNER JOIN (
+	SELECT
+		intCompanyLocationId
+		,strLocationName
+	FROM tblSMCompanyLocation WITH (NOLOCK)
+ ) LOC ON POSLog.intCompanyLocationId = LOC.intCompanyLocationId
+LEFT JOIN(
+	SELECT
+		intCompanyLocationPOSDrawerId
+		,strPOSDrawerName
+	FROM tblSMCompanyLocationPOSDrawer WITH (NOLOCK)
+) DRAWER ON POSLog.intCompanyLocationPOSDrawerId = DRAWER.intCompanyLocationPOSDrawerId
+LEFT JOIN (
+	SELECT 
+		intStoreId
+		,strDescription
+	FROM tblSTStore WITH (NOLOCK)
+) STORE ON POSLog.intStoreId = STORE.intStoreId
+INNER JOIN (
+	SELECT intEntityId
+		 , strUserName 
+	FROM dbo.tblEMEntityCredential WITH (NOLOCK)
+) ENTITY ON POSLog.intEntityUserId = ENTITY.intEntityId
