91495b44073d23352be871e9ac2af5a65426d7a2
COMTECH\BPonce <bonn.ponce@irely.com>
2018-08-21 12:56:23 +0800

    CF-1863

--- i21Database/dbo/Tables/tblCFCardType.sql
+++ i21Database/dbo/Tables/tblCFCardType.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[tblCFCardType] (
     [intCardTypeId]    INT            IDENTITY (1, 1) NOT NULL,
     [strCardType]      NVARCHAR (250) COLLATE Latin1_General_CI_AS NULL,
+    [strCSUCardType]   NVARCHAR (250) COLLATE Latin1_General_CI_AS NULL,
     [intNetworkId]     INT            NULL,
     [intConcurrencyId] INT            CONSTRAINT [DF_tblCFCardType_intConcurrencyId] DEFAULT ((1)) NULL,
     [strDescription]   NVARCHAR (250) COLLATE Latin1_General_CI_AS NULL,
