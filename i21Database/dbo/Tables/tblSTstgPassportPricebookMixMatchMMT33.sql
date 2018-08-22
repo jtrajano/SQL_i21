e025d9098121b90cdea723fcd7c1afe37f233585
COMTECH\HZaragoza <henry.zaragoza@irely.com>
2018-08-22 15:07:01 +0800

    ST-899 Passport > Create Outbound Pricebook and Related files

--- i21Database/dbo/Tables/tblSTstgPassportPricebookMixMatchMMT33.sql
+++ i21Database/dbo/Tables/tblSTstgPassportPricebookMixMatchMMT33.sql
@@ -1,23 +1,22 @@
-﻿CREATE TABLE [dbo].[tblSTstgPassportPricebookComboCBT33]
+﻿CREATE TABLE [dbo].[tblSTstgPassportPricebookMixMatchMMT33]
 (
-	[intComboSalesFile] INT NOT NULL IDENTITY,
+	[intMixMatchId] INT NOT NULL IDENTITY,
 	[StoreLocationID] INT NULL, 
     [VendorName] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
     [VendorModelVersion] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
-    [TableActionType] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
     [RecordActionType] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
-    [CBTDetailRecordActionType] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
+    [MMTDetailRecordActionType] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
     [PromotionID] INT NULL, 
 	[PromotionReason] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
-    [ComboDescription] NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL,
-	[ComboPrice] NUMERIC(18, 2) NULL,
+    [MixMatchDescription] NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL,
+	[TransactionLimit] INT NULL,
 	[ItemListID] INT NULL,
-	[ComboItemQuantity] INT NULL,
-	[ComboItemUnitPrice] NUMERIC(18, 2) NULL,
 	[StartDate] DATE NULL,
 	[StartTime] TIME(0) NULL,
 	[StopDate] DATE NULL,
 	[StopTime] TIME(0) NULL,
+	[MixMatchUnits] INT NULL,
+	[MixMatchPrice] NUMERIC(18, 2) NULL,
 	[strUniqueGuid]  NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL,
-    CONSTRAINT [PK_tblSTstgPassportPricebookComboCBT33] PRIMARY KEY ([intComboSalesFile])  
+    CONSTRAINT [PK_tblSTstgPassportPricebookMixMatchMMT33] PRIMARY KEY ([intMixMatchId])  
 )
