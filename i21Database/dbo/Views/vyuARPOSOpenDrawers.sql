f4ee110cb2e2cdad8a1d90e4b5df4a17ababcd05
JBorlagdan <jc.borlagdan@irely.com>
2018-08-23 16:23:48 +0800

    AR-8476

---
+++ i21Database/dbo/Views/vyuARPOSOpenDrawers.sql
@@ -0,0 +1,18 @@
+﻿CREATE VIEW [dbo].[vyuARPOSOpenDrawers]
+	AS 
+	SELECT DISTINCT 
+			intCompanyLocationPOSDrawerId	=	POSLOG.intCompanyLocationPOSDrawerId,
+			strPOSDrawerName				=	DRAWER.strPOSDrawerName,
+			dblOpeningBalance				=	POSLOG.dblOpeningBalance,
+			dblEndingBalance				=	SUM(ISNULL(dblEndingBalance,0))
+
+	FROM tblARPOSLog POSLOG
+	LEFT JOIN (
+		SELECT
+			intCompanyLocationPOSDrawerId,
+			strPOSDrawerName
+		FROM tblSMCompanyLocationPOSDrawer
+	) DRAWER ON POSLOG.intCompanyLocationPOSDrawerId = DRAWER.intCompanyLocationPOSDrawerId
+	WHERE POSLOG.intCompanyLocationPOSDrawerId IS NOT NULL AND ysnLoggedIn = 1
+	GROUP BY POSLOG.intCompanyLocationPOSDrawerId, dblOpeningBalance, DRAWER.strPOSDrawerName
+
