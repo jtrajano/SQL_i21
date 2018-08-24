5fa92216bad31ff499a8292ca1083ca1d2988207
Kevin Estrada <kevin.estrada@irely.com>
2018-08-24 16:01:12 +0800

    FRM-7675

---
+++ i21Database/dbo/Stored Procedures/uspSMReplicationAddSubscriptionForRemote.sql
@@ -0,0 +1,19 @@
+﻿		CREATE PROCEDURE [dbo].[uspSMReplicationAddSubscriptionForRemote]
+		 @PublicationName NVARCHAR(50),
+		 @RemoteServerName NVARCHAR(50),
+		 @RemoteDB NVARCHAR(50)
+
+		AS
+		BEGIN
+		  exec sp_addsubscription 
+			@publication = @PublicationName, 
+			@subscriber = @RemoteServerName, 
+			@destination_db = @RemoteDB, 
+			@subscription_type = N'Pull', 
+			@sync_type = N'replication support only', 
+			@article = N'all', 
+			@update_mode = N'read only', 
+			@subscriber_type = 0, 
+			@status = N'active' 
+
+		END
