7ded1ef47285886c5f1075d753ea67c5d01ec32e
Jayson Ramos <jayson.ramos@iRely.com>
2018-08-24 14:22:52 +0800

    HD-2871

--- i21Database/dbo/Functions/fnHDCoalesceJiraKey.sql
+++ i21Database/dbo/Functions/fnHDCoalesceJiraKey.sql
@@ -1,11 +1,11 @@
-﻿CREATE FUNCTION [dbo].[fnHDCoalesceJiraKey](@intTicketId int)
+﻿CREATE FUNCTION [dbo].[fnHDCoalesceJiraKey](@intTicketId int,@ysnDisplay bit)
 RETURNS nvarchar(max)
 AS
 BEGIN
 
 	DECLARE @strJiraKeys nvarchar(max);
 	SELECT
-		@strJiraKeys = COALESCE(@strJiraKeys + ',<br>', '') + '<a target="_blank" title="Click to go to ' + tblHDTicketJIRAIssue.strKey + '." href="http://jira.irelyserver.com/browse/' + tblHDTicketJIRAIssue.strKey + '">' + tblHDTicketJIRAIssue.strKey + '</a>'
+		@strJiraKeys = (case when @ysnDisplay = convert(bit,1) then COALESCE(@strJiraKeys + ',<br>', '') + '<a target="_blank" title="Click to go to ' + tblHDTicketJIRAIssue.strKey + '." href="http://jira.irelyserver.com/browse/' + tblHDTicketJIRAIssue.strKey + '">' + tblHDTicketJIRAIssue.strKey + '</a>' else COALESCE(@strJiraKeys + ',', '') + tblHDTicketJIRAIssue.strKey end)
 	FROM
 		tblHDTicketJIRAIssue
 	WHERE
