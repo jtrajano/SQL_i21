cffc31261ddaf1465700a1348296ab59d18fb3db
Satya S Sahoo <satya.sahoo@irely.com>
2018-08-24 20:03:59 +0530

    CT-2449 - Instructed ETA column in the Overview & Historical tabs - 18.1F CT-2475 - Column as Purchasing Group to be added in Contract Details search screen - 18.1F

--- i21Database/dbo/Views/vyuCTSearchContractDetail.sql
+++ i21Database/dbo/Views/vyuCTSearchContractDetail.sql
@@ -71,7 +71,7 @@ AS
 			SB.strSubLocationName,
 			BK.strBook,
 			SK.strSubBook,
-
+			PG.strName AS strPurchasingGroup,
 			--Required by other modules
 		
 			IM.strLotTracking,				
@@ -134,6 +134,7 @@ AS
 	JOIN	tblSMCountry					OG	ON	OG.intCountryID				=	EO.intCountryID				LEFT
 	JOIN	tblSMCountry					IG	ON	IG.intCountryID				=	IC.intCountryId				LEFT
 	JOIN	tblSMCompanyLocationSubLocation	SB	ON	SB.intCompanyLocationSubLocationId	= CD.intSubLocationId 	LEFT
+	JOIN	tblSMPurchasingGroup			PG	ON	PG.intPurchasingGroupId		=	CD.intPurchasingGroupId		LEFT
 	JOIN	tblICCommodityAttribute			CA	ON	CA.intCommodityAttributeId	=	IM.intProductTypeId
 												AND	CA.strType					=	'ProductType'				LEFT
 	JOIN	(
