abfaa0d5f79ab63e94ed1da93574c6fbca11a0ea
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2018-08-27 00:44:01 +0530

    RM-1794

--- i21Database/dbo/Stored Procedures/uspRKSavePnsOptionsMatched.sql
+++ i21Database/dbo/Stored Procedures/uspRKSavePnsOptionsMatched.sql
@@ -313,8 +313,8 @@ SELECT @NewFutOptTransactionId = SCOPE_IDENTITY();
 		   WHEN (strBuySell = 'Sell' AND strOptionType= 'Put') THEN 'Buy' End  
 		   FROM tblRKFutOptTransaction Where intFutOptTransactionId=@NewFutOptTransactionId  
 
-	SELECT @intInternalTradeNo1 = Max(convert(numeric(24,10),REPLACE(REPLACE(REPLACE(strInternalTradeNo,'-S' ,''),'O-' ,''),'-H',''))) + 1  from tblRKFutOptTransaction
-	UPDATE tblSMStartingNumber set intNumber = @intInternalTradeNo1 where strModule='Risk Management' and strTransactionType='FutOpt Transaction'
+	--SELECT @intInternalTradeNo1 = Max(convert(numeric(24,10),REPLACE(REPLACE(REPLACE(strInternalTradeNo,'-S' ,''),'O-' ,''),'-H',''))) + 1  from tblRKFutOptTransaction
+	UPDATE tblSMStartingNumber set intNumber = isnull(intNumber,0)+ 1 where strModule='Risk Management' and strTransactionType='FutOpt Transaction'
 	UPDATE tblRKFutOptTransaction  set strBuySell=@NewBuySell,strOptionType=null,intOptionMonthId=null Where intFutOptTransactionId = @NewFutOptTransactionId   
 	UPDATE tblRKOptionsPnSExercisedAssigned set intFutTransactionId= @NewFutOptTransactionId Where intOptionsPnSExercisedAssignedId=@intOptionsPnSExercisedAssignedId
 	SELECT @mRowNumber = MIN(RowNumber) FROM @tblExercisedAssignedDetail WHERE RowNumber>@mRowNumber    
