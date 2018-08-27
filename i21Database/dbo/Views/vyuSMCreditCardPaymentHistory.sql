1cda3e919bb02afe29cbb057d1a9340e9b3b584e
csJSarcilla <JSarcilla@COMTECH.COM>
2018-08-27 08:39:47 +0800

    AR-8265 View Update

--- i21Database/dbo/Views/vyuSMCreditCardPaymentHistory.sql
+++ i21Database/dbo/Views/vyuSMCreditCardPaymentHistory.sql
@@ -2,6 +2,6 @@
 	AS SELECT SP.intTransactionId, ARP.intEntityCardInfoId, SP.strFrequency, SP.strPaymentMethod
 FROM tblSMPayment SP
 INNER JOIN (
-	SELECT * FROM tblARPayment
+	SELECT intEntityCardInfoId,  intPaymentId FROM tblARPayment
 ) ARP ON ARP.intPaymentId = SP.intTransactionId
 WHERE SP.strPaymentMethod = 'Credit Card'
