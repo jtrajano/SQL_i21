1484e27200fb4ec4ab9ade7d1ad348a17b329304
JeffreyTrajano <jeffrey.trajano@irely.com>
2018-08-29 08:15:48 +0800

    GL-6595 [CM Batch posting] Error in loading batch posting

--- i21PreDeployment/Scripts/Pre-Deployment/2_CM/1_CM.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/2_CM/1_CM.sql
@@ -8,7 +8,6 @@ BEGIN
 
 	EXEC sp_rename 'apcbkmst', 'apcbkmst_origin'
 END
-
 GO
 
 IF EXISTS(select 1  from INFORMATION_SCHEMA.TABLES where TABLE_NAME = N'apchkmst' and TABLE_TYPE = N'BASE TABLE')
@@ -21,5 +20,14 @@ BEGIN
 
 	EXEC sp_rename 'apchkmst', 'apchkmst_origin'
 END
+GO
+
+-- UPDATES NULL intEntityId columns that is causing batch post error GL-6595
+UPDATE A SET A.intEntityId = B.intEntityId FROM
+tblCMBankTransaction A
+cross apply(
+	SELECT TOP 1 isnull( D.intEntityId,D.intCreatedUserId) intEntityId from tblCMBankTransactionDetail D where  D.intTransactionId = A.intTransactionId
+)B
+WHERE A.intEntityId is null AND ysnPosted = 0
 GO
 
