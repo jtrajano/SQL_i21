605d91c92301978deab45f9e0782cdf0f86c5d08
JeffreyTrajano <jeffrey.trajano@irely.com>
2018-09-03 21:09:39 +0800

    GL-6623 Voiding ACH Doubles The Amount in the Bank Account Balance

--- i21Database/dbo/Stored Procedures/uspCMBankTransactionReversal.sql
+++ i21Database/dbo/Stored Procedures/uspCMBankTransactionReversal.sql
@@ -88,7 +88,7 @@ IF (@dtmReverseDate IS NULL)
 					ON F.strTransactionId = TMP.strTransactionId
 		WHERE	F.intBankTransactionTypeId IN (@AP_PAYMENT, @AR_PAYMENT, @MISC_CHECKS, @ORIGIN_CHECKS, @PAYCHECK)		
 				-- Condition #1:
-				AND F.strReferenceNo NOT IN (@CASH_PAYMENT) 
+				AND ISNULL(F.strReferenceNo,'') NOT IN (@CASH_PAYMENT) 
 				-- Condition #2:		
 				AND F.dtmCheckPrinted IS NOT NULL 
 
@@ -110,8 +110,11 @@ ELSE
 		**/
 
 		/** Clean-up Reversing Date parameter **/
-		SELECT @dtmReversalDate = ISNULL(@dtmReverseDate, dtmDate), @intTransactionId = intTransactionId FROM tblCMBankTransaction F INNER JOIN #tmpCMBankTransaction TMP ON F.strTransactionId = TMP.strTransactionId
-		WHERE F.intBankTransactionTypeId IN (@AP_PAYMENT, @AR_PAYMENT, @MISC_CHECKS, @ORIGIN_CHECKS, @PAYCHECK, @DIRECT_DEPOSIT, @ACH) AND F.strReferenceNo NOT IN (@CASH_PAYMENT) AND F.dtmCheckPrinted IS NOT NULL 
+		SELECT @dtmReversalDate = ISNULL(@dtmReverseDate, dtmDate), @intTransactionId = intTransactionId FROM tblCMBankTransaction F INNER JOIN #tmpCMBankTransaction TMP 
+		ON F.strTransactionId = TMP.strTransactionId
+		WHERE F.intBankTransactionTypeId IN (@AP_PAYMENT, @AR_PAYMENT, @MISC_CHECKS, @ORIGIN_CHECKS, @PAYCHECK, @DIRECT_DEPOSIT, @ACH) 
+		AND ISNULL(F.strReferenceNo,'') NOT IN (@CASH_PAYMENT) 
+		AND F.dtmCheckPrinted IS NOT NULL 
 
 		/** Insert Reversal Entry Header **/
 		INSERT INTO tblCMBankTransaction
@@ -128,7 +131,7 @@ ELSE
 					ON F.strTransactionId = TMP.strTransactionId
 		WHERE	F.intBankTransactionTypeId IN (@AP_PAYMENT, @AR_PAYMENT, @MISC_CHECKS, @ORIGIN_CHECKS, @PAYCHECK, @DIRECT_DEPOSIT, @ACH)		
 				-- Condition #1:
-				AND F.strReferenceNo NOT IN (@CASH_PAYMENT) 
+				AND ISNULL(F.strReferenceNo,'') NOT IN (@CASH_PAYMENT) 
 				-- Condition #2:		
 				AND F.dtmCheckPrinted IS NOT NULL 
 		SELECT @intVoidTransactionId = @@IDENTITY
@@ -145,7 +148,7 @@ ELSE
 			LEFT JOIN (SELECT F.intTransactionId FROM tblCMBankTransaction F INNER JOIN #tmpCMBankTransaction TMP
 						ON F.strTransactionId = TMP.strTransactionId
 						WHERE F.intBankTransactionTypeId IN (@AP_PAYMENT, @AR_PAYMENT, @MISC_CHECKS, @ORIGIN_CHECKS)		
-						AND F.strReferenceNo NOT IN (@CASH_PAYMENT) 
+						AND ISNULL(F.strReferenceNo,'') NOT IN (@CASH_PAYMENT) 
 						AND F.dtmCheckPrinted IS NOT NULL
 						) M ON D.intTransactionId = M.intTransactionId
 						WHERE D.intTransactionId = @intTransactionId 
@@ -158,7 +161,7 @@ ELSE
 			(SELECT F.strTransactionId + 'V' AS strTransactionId, F.intEntityId, F.intBankTransactionTypeId FROM tblCMBankTransaction F INNER JOIN #tmpCMBankTransaction TMP
 				ON F.strTransactionId = TMP.strTransactionId
 				WHERE F.intBankTransactionTypeId IN (@AP_PAYMENT, @AR_PAYMENT, @MISC_CHECKS, @ORIGIN_CHECKS, @PAYCHECK, @DIRECT_DEPOSIT, @ACH)		
-				AND F.strReferenceNo NOT IN (@CASH_PAYMENT)
+				AND ISNULL(F.strReferenceNo,'') NOT IN (@CASH_PAYMENT) 
 				AND F.dtmCheckPrinted IS NOT NULL) X
 
 		/* Execute Posting Procedure for each Void Check entry*/
@@ -227,7 +230,7 @@ ELSE
 					ON F.strTransactionId = TMP.strTransactionId
 		WHERE	F.intBankTransactionTypeId IN (@AP_PAYMENT, @AR_PAYMENT, @MISC_CHECKS, @ORIGIN_CHECKS, @PAYCHECK, @DIRECT_DEPOSIT, @ACH)		
 				-- Condition #1:
-				AND F.strReferenceNo NOT IN (@CASH_PAYMENT) 
+				AND ISNULL(F.strReferenceNo,'') NOT IN (@CASH_PAYMENT) 
 				-- Condition #2:		
 				AND F.dtmCheckPrinted IS NOT NULL 
 		IF @@ERROR <> 0	GOTO Exit_BankTransactionReversal_WithErrors
@@ -262,7 +265,7 @@ FROM	tblCMCheckNumberAudit AUDIT INNER JOIN tblCMBankTransaction F
 		INNER JOIN #tmpCMBankTransaction TMP
 			ON F.strTransactionId = TMP.strTransactionId
 WHERE	-- Condition #1:
-		F.strReferenceNo NOT IN (@CASH_PAYMENT)	
+		ISNULL(F.strReferenceNo,'') NOT IN (@CASH_PAYMENT) 
 		-- Condition #2:
 		AND F.dtmCheckPrinted IS NOT NULL 
 		-- Condition #3:
