b44a73b7e82b7354d004beae3d087547251ecf35
Dan Jalimao <dan.jalimao@irely.com>
2018-09-04 16:22:02 +0800

    SM-4411

---
+++ i21Database/dbo/Stored Procedures/uspSMStartingNumberResetNumber.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[uspSMStartingNumberResetNumber]
+AS
+BEGIN
+
+	UPDATE tblSMStartingNumber SET intNumber = 1, dtmResetDate = SYSDATETIME()
+	WHERE CAST(dtmResetDate AS DATE) <> CAST(SYSDATETIME() AS DATE) AND ysnResetNumber = 1 AND ysnUseLocation = 0
+
+	UPDATE b SET b.intNumber = 1
+	FROM tblSMStartingNumber a
+	INNER JOIN tblSMStartingNumberLocation b ON a.intStartingNumberId = b.intStartingNumberId
+	WHERE CAST(dtmResetDate AS DATE) <> CAST(SYSDATETIME() AS DATE) AND ysnResetNumber = 1
+
+	UPDATE a SET a.dtmResetDate = SYSDATETIME() 
+	FROM tblSMStartingNumber a
+	INNER JOIN tblSMStartingNumberLocation b ON a.intStartingNumberId = b.intStartingNumberId
+	WHERE CAST(dtmResetDate AS DATE) <> CAST(SYSDATETIME() AS DATE) AND ysnResetNumber = 1
+
+END
