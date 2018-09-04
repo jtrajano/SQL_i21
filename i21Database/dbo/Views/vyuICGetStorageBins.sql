95292c4f871cd39ea677eb15487a3a089ebd6441
Wendell Wayne Estrada <wendell.estrada@irely.com>
2018-09-04 10:03:16 +0800

    [B] IC-6071 Stock Details - Storage unit chart is reversed when bin shows negative stock [B] IC-6066 Stock details by location doesn't match stock details by unit [B] IC-5984 Stock Details - Storage Location tab not showing negative stock [B] IC-5978 Stock Detail - Storage unit tab shows wrong stock [B] IC-5937 Stock Status screen showing stock twice

--- i21Database/dbo/Views/vyuICGetStorageBins.sql
+++ i21Database/dbo/Views/vyuICGetStorageBins.sql
@@ -4,7 +4,7 @@ SELECT companyLocation.intCompanyLocationId, storageLocation.intStorageLocationI
 	, companyLocation.strLocationName strLocation, storageLocation.strName strStorageLocation
 	, CAST(SUM(bd.dblStock) AS NUMERIC(16, 8)) dblStock
 	, CAST(SUM(bd.dblCapacity) AS NUMERIC(16, 8)) dblCapacity
-	, CAST(SUM(bd.dblAvailable) AS NUMERIC(16, 8)) dblAvailable
+	, CAST(dbo.fnMaxNumeric(SUM(bd.dblAvailable), 0) AS NUMERIC(16, 8)) dblAvailable
 FROM tblICStorageLocation storageLocation
 	INNER JOIN vyuICGetStorageBinDetails bd ON bd.intStorageLocationId = storageLocation.intStorageLocationId
 	INNER JOIN tblSMCompanyLocation companyLocation ON companyLocation.intCompanyLocationId = storageLocation.intLocationId
