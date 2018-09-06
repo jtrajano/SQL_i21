3d63fc1170fb2f1abf45fb5a63c49a043508dd92
Ruben Escarcha <ruben.escarcha@irely.com>
2018-09-07 05:45:11 +0800

    AR-8703 MCP Invoice: Wrong Tax

--- i21Database/dbo/Views/vyuARTaxDetailMCPReport.sql
+++ i21Database/dbo/Views/vyuARTaxDetailMCPReport.sql
@@ -8,7 +8,13 @@ SELECT strTaxCode			= TC.strTaxCode
 	 , dblTax				= SUM(ISNULL(IDT.dblTax, 0))
 	 , dblAdjustedTax		= SUM(ISNULL(IDT.dblAdjustedTax, 0))
 	 , intInvoiceId			= DETAIL.intInvoiceId
+	 , intCategoryId		= ICI.[intCategoryId]
 FROM dbo.tblARInvoiceDetail DETAIL WITH (NOLOCK)
+LEFT OUTER JOIN (
+	SELECT [intItemId]
+		 , [intCategoryId]
+	FROM dbo.tblICItem WITH (NOLOCK)
+) ICI ON DETAIL.[intItemId] = ICI.[intItemId]
 INNER JOIN (
 	SELECT intInvoiceDetailId
 		 , intTaxCodeId
@@ -24,5 +30,5 @@ INNER JOIN (
 		 , strDescription
 	FROM dbo.tblSMTaxCode WITH (NOLOCK)
 ) TC ON IDT.intTaxCodeId = TC.intTaxCodeId
-GROUP BY DETAIL.intInvoiceId, TC.strTaxCode, TC.strDescription, IDT.strCalculationMethod, IDT.dblRate
+GROUP BY DETAIL.intInvoiceId, TC.strTaxCode, TC.strDescription, IDT.strCalculationMethod, IDT.dblRate, ICI.[intCategoryId]
 HAVING SUM(ISNULL(IDT.dblAdjustedTax, 0)) <> 0
