c84d814f63bb15b4f47899471b0fb6af9519f0e3
csJSarcilla <JSarcilla@COMTECH.COM>
2018-09-07 16:56:49 +0800

    AR-8720

---
+++ i21Database/dbo/Views/vyuARPOSSearchLoggedIn.sql
@@ -0,0 +1,8 @@
+﻿CREATE VIEW [dbo].[vyuARPOSSearchLoggedIn]
+AS 
+SELECT DISTINCT POS.intEntityId, EN.strUserName, POS.ysnLoggedIn
+FROM dbo.tblARPOSLog POS WITH(NOLOCK)
+INNER JOIN (
+	SELECT intEntityId, strUserName 
+	FROM dbo.tblSMUserSecurity WITH(NOLOCK)
+)EN ON POS.intEntityId = EN.intEntityId
