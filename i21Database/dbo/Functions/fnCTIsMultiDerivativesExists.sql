a028d90bc07dcac3914f1b08e804c16cd0a9fad1
Satya S Sahoo <satya.sahoo@irely.com>
2018-09-10 20:55:24 +0530

    CT-2311 - Performance Issue on Contracts - 18.3Q

---
+++ i21Database/dbo/Functions/fnCTIsMultiDerivativesExists.sql
@@ -0,0 +1,23 @@
+﻿CREATE FUNCTION [dbo].[fnCTIsMultiDerivativesExists]
+(
+	@intContractDetailId	INT
+)
+RETURNS BIT
+AS 
+BEGIN 
+	DECLARE	 @ysnMultiDerivatives BIT
+	
+    SELECT	 @ysnMultiDerivatives = CASE 
+									WHEN ISNULL(
+													(
+														SELECT COUNT(1)
+														FROM tblRKAssignFuturesToContractSummary SM
+														WHERE SM.intContractDetailId = @intContractDetailId
+													), 0
+												) > 1
+										THEN CAST(1 AS BIT)
+										ELSE CAST(0 AS BIT)
+									END
+	
+	RETURN	@ysnMultiDerivatives
+END
