382b56355fd42ee93f96bd63b34ad2cb79f6a9f4
Kelvin Quinto <kelvin.quinto@irely.com>
2018-09-11 15:48:49 +0800

    AR-8727

--- i21Database/dbo/Views/vyuARGetItemStockPOS.sql
+++ i21Database/dbo/Views/vyuARGetItemStockPOS.sql
@@ -1,35 +1,67 @@
 ﻿CREATE VIEW [dbo].[vyuARGetItemStockPOS]
 AS 
-SELECT intItemId						= ITEM.intItemId
+SELECT intItemId						= ITEMS.intItemId
 	 , intStorageLocationId				= IL.intStorageLocationId
 	 , intIssueUOMId					= IUOM.intItemUOMId
 	 , intCompanyLocationId				= CL.intCompanyLocationId
-	 , strItemNo						= ITEM.strItemNo
-	 , strDescription					= ITEM.strDescription
-	 , strStatus						= ITEM.strStatus
+	 , strItemNo						= ITEMS.strItemNo
+	 , strDescription					= ITEMS.strDescription
+	 , strStatus						= ITEMS.strStatus
 	 , strLocationName					= CL.strLocationName
-	 , strType							= ITEM.strType
+	 , strType							= ITEMS.strType
 	 , strStorageLocationName			= SL.strName
 	 , strIssueUOM						= UM.strUnitMeasure
-	 , strIssueUPC						= CASE WHEN ISNULL(ALTUPC.intItemUomUpcId,0) <> 0 THEN ALTUPC.strUpcCode ELSE IUOM.strUpcCode END 
-	 , strIssueLongUPC					= CASE WHEN ISNULL(ALTUPC.intItemUomUpcId,0) <> 0 THEN ALTUPC.strLongUpcCode ELSE IUOM.strLongUPCCode END
-	 , strLotTracking					= ITEM.strLotTracking
-	 , strMaintenanceCalculationMethod	= ITEM.strMaintenanceCalculationMethod
+	 , strIssueUPC						= ITEMS.strIssueUPC
+	 , strIssueLongUPC					= ITEMS.strIssueLongUPC
+	 , strLotTracking					= ITEMS.strLotTracking
+	 , strMaintenanceCalculationMethod	= ITEMS.strMaintenanceCalculationMethod
 	 , dblSalePrice						= ISNULL(IP.dblSalePrice, 0)
 	 , dblUnitOnHand					= ISNULL(ISTOCK.dblUnitOnHand, 0)
 	 , dblOnOrder						= ISNULL(ISTOCK.dblOnOrder, 0)
 	 , dblOrderCommitted				= ISNULL(ISTOCK.dblOrderCommitted, 0)
 	 , dblAvailable						= ISNULL(ISTOCK.dblUnitOnHand, 0) - (ISNULL(ISTOCK.dblUnitReserved, 0) + ISNULL(ISTOCK.dblConsignedSale, 0))	 
-	 , dblMaintenanceRate				= ISNULL(ITEM.dblMaintenanceRate, 0)
-	 , dblDefaultFull					= ISNULL(ITEM.dblDefaultFull, 0)
-	 , ysnListBundleSeparately			= ISNULL(ITEM.ysnListBundleSeparately, CAST(0 AS BIT))
+	 , dblMaintenanceRate				= ISNULL(ITEMS.dblMaintenanceRate, 0)
+	 , dblDefaultFull					= ISNULL(ITEMS.dblDefaultFull, 0)
+	 , ysnListBundleSeparately			= ISNULL(ITEMS.ysnListBundleSeparately, CAST(0 AS BIT))
 	 , intDepositPLUId					= IL.intDepositPLUId
-FROM dbo.tblICItem ITEM WITH (NOLOCK)
-INNER JOIN dbo.tblICItemLocation IL WITH (NOLOCK) ON ITEM.intItemId = IL.intItemId
+FROM (
+	SELECT intItemId		= ITEM.intItemId
+		 , strItemNo
+		 , strDescription
+		 , strStatus
+		 , strType
+		 , strIssueUPC		= IUOM.strUpcCode
+		 , strIssueLongUPC	= IUOM.strLongUPCCode
+		 , strLotTracking
+		 , strMaintenanceCalculationMethod
+		 , dblMaintenanceRate
+		 , dblDefaultFull
+		 , ysnListBundleSeparately
+	FROM dbo.tblICItem ITEM WITH (NOLOCK)
+	INNER JOIN dbo.tblICItemUOM IUOM WITH (NOLOCK) ON ITEM.intItemId = IUOM.intItemId
+
+	UNION ALL
+
+	SELECT intItemId		= ITEM.intItemId
+		 , strItemNo
+		 , strDescription
+		 , strStatus
+		 , strType
+		 , strIssueUPC		= ALTUPC.strUpcCode
+		 , strIssueLongUPC	= ALTUPC.strLongUpcCode
+		 , strLotTracking
+		 , strMaintenanceCalculationMethod
+		 , dblMaintenanceRate
+		 , dblDefaultFull
+		 , ysnListBundleSeparately
+	FROM dbo.tblICItem ITEM WITH (NOLOCK)
+	INNER JOIN dbo.tblICItemUOM IUOM WITH (NOLOCK) ON ITEM.intItemId = IUOM.intItemId
+	INNER JOIN dbo.tblICItemUomUpc ALTUPC WITH (NOLOCK) ON IUOM.intItemUOMId = ALTUPC.intItemUOMId
+) ITEMS
+INNER JOIN dbo.tblICItemLocation IL WITH (NOLOCK) ON ITEMS.intItemId = IL.intItemId
 INNER JOIN dbo.tblSMCompanyLocation CL WITH (NOLOCK) ON IL.intLocationId = CL.intCompanyLocationId
-INNER JOIN dbo.tblICItemUOM IUOM WITH (NOLOCK) ON ITEM.intItemId = IUOM.intItemId
+INNER JOIN dbo.tblICItemUOM IUOM WITH (NOLOCK) ON ITEMS.intItemId = IUOM.intItemId
 INNER JOIN dbo.tblICUnitMeasure UM WITH (NOLOCK) ON IUOM.intUnitMeasureId = UM.intUnitMeasureId
 LEFT JOIN dbo.tblICItemPricing IP WITH (NOLOCK) ON IL.intItemId = IP.intItemId AND IL.intItemLocationId = IP.intItemLocationId
-LEFT JOIN dbo.tblICItemStock ISTOCK WITH (NOLOCK) ON ITEM.intItemId = ISTOCK.intItemId AND IL.intItemLocationId = ISTOCK.intItemLocationId
+LEFT JOIN dbo.tblICItemStock ISTOCK WITH (NOLOCK) ON ITEMS.intItemId = ISTOCK.intItemId AND IL.intItemLocationId = ISTOCK.intItemLocationId
 LEFT JOIN dbo.tblICStorageLocation SL WITH (NOLOCK) ON IL.intStorageLocationId = SL.intStorageLocationId
-LEFT JOIN dbo.tblICItemUomUpc ALTUPC WITH (NOLOCK) ON IUOM.intItemUOMId = ALTUPC.intItemUOMId
