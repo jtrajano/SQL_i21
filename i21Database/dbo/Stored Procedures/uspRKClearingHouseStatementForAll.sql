1adc644aa868fe67ab976aef5d895aed34913b8c
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2018-09-18 09:54:37 +0530

    RM-1927

--- i21Database/dbo/Stored Procedures/uspRKClearingHouseStatementForAll.sql
+++ i21Database/dbo/Stored Procedures/uspRKClearingHouseStatementForAll.sql
@@ -1,16 +1,19 @@
 ﻿CREATE PROC [dbo].[uspRKClearingHouseStatementForAll] 
-		@xmlParam NVARCHAR(MAX) = NULL		
-AS
+		 @xmlParam NVARCHAR(MAX) = NULL		
+AS 
+
+--declare @xmlParam NVARCHAR(MAX) = NULL	
+--set @xmlParam=N'<?xml version="1.0" encoding="utf-16"?><xmlparam><filters><filter><fieldname>dtmTransactionFromDate</fieldname><condition>Between</condition><from>01/01/2018</from><to>09/17/2018</to><join>AND</join><begingroup /><endgroup /><datatype>DateTime</datatype></filter><filter><fieldname>strName</fieldname><condition>Equal To</condition><from>Marex</from><join>AND</join><begingroup /><endgroup /><datatype>String</datatype></filter><filter><fieldname>intSrCurrentUserId</fieldname><condition>Dummy</condition><from>1</from><join>OR</join><begingroup /><endgroup /><datatype>int</datatype></filter><filter><fieldname>intSrLanguageId</fieldname><condition>Dummy</condition><from>0</from><join>OR</join><begingroup /><endgroup /><datatype>int</datatype></filter></filters><sorts /></xmlparam>'
 DECLARE @idoc INT,
 		@strName nvarchar(100) = null,
 		@strAccountNumber nvarchar(100) = null,
 		@dtmTransactionFromDate  datetime = null,
-		@dtmTransactionToDate datetime = null
-
+		@dtmTransactionToDate  datetime = null
 IF LTRIM(RTRIM(@xmlParam)) = ''
   SET @xmlParam = NULL
 
 DECLARE @temp_xml_table TABLE (
+		id INT IDENTITY(1,1),
 		fieldname NVARCHAR(50)
 		,condition NVARCHAR(20)
 		,[from] NVARCHAR(50)
@@ -24,7 +27,7 @@ DECLARE @temp_xml_table TABLE (
 	EXEC sp_xml_preparedocument @idoc OUTPUT
 		,@xmlParam
 
-	INSERT INTO @temp_xml_table
+INSERT INTO @temp_xml_table
 SELECT *
 	FROM OPENXML(@idoc, 'xmlparam/filters/filter', 2) WITH (
 			 fieldname NVARCHAR(50)
@@ -40,52 +43,110 @@ SELECT *
 SELECT @strName = [from] FROM @temp_xml_table WHERE [fieldname] = 'strName'	
 SELECT @strAccountNumber = [from] FROM @temp_xml_table	WHERE [fieldname] = 'strAccountNumber'
 SELECT @dtmTransactionFromDate = [from] FROM @temp_xml_table WHERE [fieldname] = 'dtmTransactionFromDate'
-SELECT @dtmTransactionToDate = [from] FROM @temp_xml_table WHERE [fieldname] = 'dtmTransactionToDate'
 
-SELECT *,'Trades between ''' +Left(replace(convert(varchar(9), @dtmTransactionFromDate, 6), ' ', '-') + ' ' + convert(varchar(8), @dtmTransactionFromDate, 8),9) + ''' and ''' + Left(replace(convert(varchar(9), @dtmTransactionToDate, 6), ' ', '-') + ' ' + convert(varchar(8), @dtmTransactionToDate, 8),9)+'''' as strDateHeading,
-@strAccountNumber strAccountNumber,@strName strBroker,isnull(@dtmTransactionFromDate,min(dtmTransactionDate) over(partition by strFutMarketName)) dtmTransactionFromDate ,isnull(@dtmTransactionToDate,max(dtmTransactionDate) over(partition by strFutMarketName)) as dtmTransactionToDate FROM 
-(
-SELECT strInternalTradeNo,strFutMarketName,dtmTransactionDate,Left(replace(convert(varchar(9), dtmTransactionDate, 6), ' ', '-') + ' ' + convert(varchar(8), dtmTransactionDate, 8),9) LdtmTransactionDate,
-	   intNoOfContract LintNoOfContract,strFutureMonth LstrFutureMonth,dblPrice LdblPrice,
-		null SdtmTransactionDate,null SintNoOfContract,null SstrFutureMonth,null SdblPrice,CASE WHEN bc.intFuturesRateType= 1 then 0 else  -isnull(bc.dblFutCommission,0) end*intNoOfContract as dblFutCommission
+DECLARE @query NVARCHAR(MAX), 
+	    @innerQuery NVARCHAR(MAX), 
+		@filter NVARCHAR(MAX) = '';
+DECLARE @dtmFromDate DATETIME = NULL;
+DECLARE @dtmToDate DATETIME = NULL;
+DECLARE @count INT = 0;
+DECLARE @fieldname NVARCHAR(50)
+DECLARE @condition NVARCHAR(20)     
+DECLARE @id INT 
+DECLARE @from NVARCHAR(50)
+DECLARE @to NVARCHAR(50)
+DECLARE @join NVARCHAR(10)
+DECLARE @begingroup NVARCHAR(50)
+DECLARE @endgroup NVARCHAR(50)
+DECLARE @datatype NVARCHAR(50) 
+
+SELECT @dtmFromDate = [from], @dtmToDate = [to], @condition = [condition] FROM @temp_xml_table WHERE [fieldname] = 'dtmTransactionFromDate';
+
+IF @dtmTransactionFromDate IS NOT NULL
+BEGIN	
+	IF @condition = 'Equal To'
+	BEGIN 
+	
+		SET @innerQuery = ' WHERE DATEADD(dd, DATEDIFF(dd, 0,dtmTransactionDate), 0) = ''' + CONVERT(VARCHAR(10), @dtmFromDate, 110) + ''''
+		set @dtmTransactionFromDate=CONVERT(VARCHAR(10), @dtmFromDate, 110)
+		set @dtmTransactionToDate=CONVERT(VARCHAR(10), @dtmFromDate, 110)
 		
+	END
+    ELSE 
+	BEGIN 
+		SET @innerQuery = ' WHERE DATEADD(dd, DATEDIFF(dd, 0,dtmTransactionDate), 0) BETWEEN ''' + CONVERT(VARCHAR(10), @dtmFromDate, 110) + ''' AND '''  + CONVERT(VARCHAR(10), @dtmToDate, 110) + ''''	
+		set @dtmTransactionFromDate=CONVERT(VARCHAR(10), @dtmFromDate, 110)
+		set @dtmTransactionToDate=CONVERT(VARCHAR(10), @dtmToDate, 110)
+	END  
+END
+ELSE
+BEGIN
+	SET @dtmFromDate = CONVERT(VARCHAR(10), '1/1/1900', 110)
+	SET @dtmToDate = CONVERT(VARCHAR(10), GETDATE(), 110)
+	set @dtmTransactionFromDate=CONVERT(VARCHAR(10), '1/1/1900', 110)
+	set @dtmTransactionToDate=CONVERT(VARCHAR(10), GETDATE(), 110)
+END
+
+SET @query = '
+SELECT * 
+FROM 
+(
+SELECT strInternalTradeNo,strFutMarketName,CONVERT(VARCHAR(10), dtmFilledDate, 110) dtmTransactionDate,Left(replace(convert(varchar(9), dtmFilledDate, 6), '' '', ''-'') + '' '' + convert(varchar(8), dtmFilledDate, 8),9) LdtmTransactionDate,
+	   intNoOfContract LintNoOfContract,strFutureMonth LstrFutureMonth,dblPrice LdblPrice,
+		null SdtmTransactionDate,null SintNoOfContract,null SstrFutureMonth,null SdblPrice,CASE WHEN bc.intFuturesRateType= 1 then 0 else  -isnull(bc.dblFutCommission,0) end*intNoOfContract as dblFutCommission		
 FROM tblRKFutOptTransaction t
 JOIN tblRKFutureMarket m on m.intFutureMarketId=t.intFutureMarketId
-JOIN tblRKFuturesMonth fm ON fm.intFutureMonthId=t.intFutureMonthId AND intSelectedInstrumentTypeId=1 AND intInstrumentTypeId=1 AND strBuySell='Buy'
+JOIN tblRKFuturesMonth fm ON fm.intFutureMonthId=t.intFutureMonthId AND intSelectedInstrumentTypeId=1 AND intInstrumentTypeId=1 AND strBuySell=''Buy''
 JOIN tblEMEntity e on e.intEntityId=t.intEntityId
 JOIN tblRKBrokerageAccount ba on ba.intBrokerageAccountId=t.intBrokerageAccountId
 LEFT JOIN tblRKBrokerageCommission bc on bc.intFutureMarketId= t.intFutureMarketId AND t.intBrokerageAccountId=bc.intBrokerageAccountId  
-WHERE  strName=ISNULL(@strName ,strName)
-AND strAccountNumber = ISNULL(@strAccountNumber ,strAccountNumber)
-AND convert(datetime,CONVERT(VARCHAR(10),dtmTransactionDate,110),110) between convert(datetime,CONVERT(VARCHAR(10),isnull(@dtmTransactionFromDate,dtmTransactionDate),110),110) and  convert(datetime,CONVERT(VARCHAR(10),isnull(@dtmTransactionToDate,dtmTransactionDate),110),110)
-
+WHERE  strName='''+ISNULL(@strName,'strName')+'''
+AND strAccountNumber = '''+ISNULL(@strAccountNumber,'strAccountNumber')+'''
 
 UNION 
 
-SELECT strInternalTradeNo,strFutMarketName,dtmTransactionDate,null LdtmTransactionDate,null LintNoOfContract,null LstrFutureMonth,null LdblPrice,
-LEFT(REPLACE(CONVERT(VARCHAR(9), dtmTransactionDate, 6), ' ', '-') + ' ' + convert(varchar(8), dtmTransactionDate, 8),9) SdtmTransactionDate,
+SELECT strInternalTradeNo,strFutMarketName,CONVERT(VARCHAR(10), dtmFilledDate, 110) dtmTransactionDate,null LdtmTransactionDate,null LintNoOfContract,null LstrFutureMonth,null LdblPrice,
+LEFT(REPLACE(CONVERT(VARCHAR(9), dtmFilledDate, 6), '' '', ''-'') + '' '' + convert(varchar(8), dtmFilledDate, 8),9) SdtmTransactionDate,
 		intNoOfContract SintNoOfContract,strFutureMonth SstrFutureMonth,dblPrice SdblPrice,CASE WHEN bc.intFuturesRateType= 1 then 0 else  -isnull(bc.dblFutCommission,0) end*intNoOfContract as dblFutCommission  
 FROM tblRKFutOptTransaction t
 JOIN tblRKFutureMarket m on m.intFutureMarketId=t.intFutureMarketId
-JOIN tblRKFuturesMonth fm ON fm.intFutureMonthId=t.intFutureMonthId AND intSelectedInstrumentTypeId=1 AND intInstrumentTypeId=1 AND strBuySell='Sell'
+JOIN tblRKFuturesMonth fm ON fm.intFutureMonthId=t.intFutureMonthId AND intSelectedInstrumentTypeId=1 AND intInstrumentTypeId=1 AND strBuySell=''Sell''
 JOIN tblEMEntity e on e.intEntityId=t.intEntityId
 JOIN tblRKBrokerageAccount ba on ba.intBrokerageAccountId=t.intBrokerageAccountId
 LEFT JOIN tblRKBrokerageCommission bc on bc.intFutureMarketId= t.intFutureMarketId AND t.intBrokerageAccountId=bc.intBrokerageAccountId 
-WHERE  strName=ISNULL(@strName ,strName)
-AND strAccountNumber = ISNULL(@strAccountNumber ,strAccountNumber)
-AND convert(datetime,CONVERT(VARCHAR(10),dtmTransactionDate,110),110) between convert(datetime,CONVERT(VARCHAR(10),isnull(@dtmTransactionFromDate,dtmTransactionDate),110),110) and  convert(datetime,CONVERT(VARCHAR(10),isnull(@dtmTransactionToDate,dtmTransactionDate),110),110) 
+WHERE  strName='''+ISNULL(@strName,'strName')+'''
+AND strAccountNumber = '''+ISNULL(@strAccountNumber,'strAccountNumber')+'''
 )t 
-GROUP BY
-strInternalTradeNo
-,strFutMarketName
-,dtmTransactionDate
-,LdtmTransactionDate
-,LintNoOfContract
-,LstrFutureMonth
-,LdblPrice
-,SdtmTransactionDate
-,SintNoOfContract
-,SstrFutureMonth
-,SdblPrice
-,dblFutCommission
-ORDER BY dtmTransactionDate
+'
+
+IF ISNULL(@innerQuery,'') != ''
+BEGIN
+	SET @query = @query +  @innerQuery
+END
+
+declare @tempTable TABLE (strItnernalTradeNo nvarchar(100),
+strFutMarketName nvarchar(100),
+dtmTransactionDate datetime,
+LdtmTransactionDate datetime,
+LintNoOfContract int,
+LstrFutureMonth nvarchar(100),
+LdblPrice numeric(24,10),
+SdtmTransactionDate datetime,
+SintNoOfContract int,
+SstrFutureMonth nvarchar(100),
+SdblPrice numeric(24,10),
+dblFutCommission numeric(24,10))
+INSERT INTO @tempTable (strItnernalTradeNo,strFutMarketName,dtmTransactionDate,LdtmTransactionDate,LintNoOfContract,LstrFutureMonth,
+						LdblPrice,SdtmTransactionDate,SintNoOfContract,SstrFutureMonth,SdblPrice,dblFutCommission)
+EXEC sp_executesql @query
+
+
+IF Exists(select 1 from @tempTable)
+BEGIN
+select @strAccountNumber strAccountNumber,@strName strBroker,@dtmTransactionFromDate dtmTransactionFromDate ,@dtmTransactionToDate as dtmTransactionToDate,* from @tempTable
+ENd
+ELSE
+BEGIN
+select 
+@strAccountNumber strAccountNumber,@strName strBroker,@dtmTransactionFromDate dtmTransactionFromDate ,@dtmTransactionToDate as dtmTransactionToDate
+END
--- i21Database/dbo/Stored Procedures/uspRKClearingHouseStatementForUnRealized.sql
+++ i21Database/dbo/Stored Procedures/uspRKClearingHouseStatementForUnRealized.sql
@@ -16,20 +16,9 @@ BEGIN
 	SET @strAccountNumber = NULL
 END
 
-IF @dtmTransactionFromDate = ''
-BEGIN
-	SET @dtmTransactionFromDate = NULL
-END
-
-IF @dtmTransactionToDate = ''
-BEGIN
-	SET @dtmTransactionToDate = NULL
-END
-
 
 SELECT * FROM (
-SELECT RANK() OVER(PARTITION BY strFutMarketName order by strFutMarketName,CONVERT(DATETIME,'01 '+strFutureMonth) Asc) AS intRowNumber ,*,(GrossPnL*(dblClosingPrice-dblPrice))-dblFutCommission as dblNetPnL,
- 'Open futures lots between ''' +LEFT(REPLACE(CONVERT(VARCHAR(9), @dtmTransactionFromDate, 6), ' ', '-') + ' ' + convert(varchar(8), @dtmTransactionFromDate, 8),9) + ''' and ''' + Left(replace(convert(varchar(9), @dtmTransactionToDate, 6), ' ', '-') + ' ' + convert(varchar(8), @dtmTransactionToDate, 8),9)+'''' as strDateHeading
+SELECT RANK() OVER(PARTITION BY strFutMarketName order by strFutMarketName,CONVERT(DATETIME,'01 '+strFutureMonth) Asc) AS intRowNumber ,*,(GrossPnL*(dblClosingPrice-dblPrice))-dblFutCommission as dblNetPnL
 FROM (
 SELECT strFutMarketName,strInternalTradeNo,strFutureMonth,
 LEFT(REPLACE(CONVERT(VARCHAR(9), dtmTradeDate, 6), ' ', '-') + ' ' + convert(varchar(8), dtmTradeDate, 8),9) dtmTradeDate,
@@ -38,5 +27,5 @@ dblLong,dblShort,isnull(dblLong,0) - isnull(dblShort,0) dblNet,dblPrice,
 FROM vyuRKUnrealizedPnL 
 WHERE  strName=isnull(@strName,strName) 
 AND strAccountNumber = isnull(@strAccountNumber,strAccountNumber)
-AND convert(datetime,CONVERT(VARCHAR(10),dtmTradeDate,110),110) between convert(datetime,CONVERT(VARCHAR(10),isnull(@dtmTransactionFromDate,dtmTradeDate),110),110) and  convert(datetime,CONVERT(VARCHAR(10),isnull(@dtmTransactionToDate,dtmTradeDate),110),110)
+AND CONVERT(VARCHAR(10), dtmTradeDate, 110) between CONVERT(VARCHAR(10), @dtmTransactionFromDate, 110) and CONVERT(VARCHAR(10), @dtmTransactionToDate, 110) 
 )t )t1 ORDER BY strFutMarketName,intRowNumber
