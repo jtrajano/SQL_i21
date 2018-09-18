ce57b180c492c47cc75b73373d486b5eafe348fb
Satya S Sahoo <satya.sahoo@irely.com>
2018-09-18 17:00:33 +0530

    CT-2311 - Performance Issue on Contracts - 18.3Q

---
+++ i21Database/dbo/Functions/fnCTGetSeqContainerInfo.sql
@@ -0,0 +1,44 @@
+﻿CREATE FUNCTION [dbo].[fnCTGetSeqContainerInfo]
+(
+	@intCommodityId	   INT,
+	@intContainerTypeId	   INT,
+	@intCountryId		   INT
+)
+
+RETURNS	@returntable	TABLE
+(
+    dblBulkQuantity	    NUMERIC(18,6),
+    dblBagQuantity	    NUMERIC(18,6),
+    strContainerType    NVARCHAR(200),
+    strContainerUOM	    NVARCHAR(100)
+)
+
+AS
+BEGIN
+    IF EXISTS(SELECT TOP 1 1 FROM tblLGContainerType WHERE intContainerTypeId = ISNULL(@intContainerTypeId,0))
+    BEGIN
+	   INSERT INTO @returntable(dblBulkQuantity,dblBagQuantity,strContainerType,strContainerUOM)	
+	   SELECT 	dblBulkQuantity,dblBagQuantity,strContainerType,RM.strUnitMeasure
+	   FROM 
+	   (
+			SELECT	CQ.intContainerTypeId,
+					CQ.intCommodityAttributeId,
+					CQ.intUnitMeasureId,
+					CQ.dblBulkQuantity ,
+					CQ.dblQuantity AS dblBagQuantity,
+					CQ.intCommodityId,
+					CA.intCountryID AS intCountryId,
+					CT.strContainerType
+
+			FROM		tblLGContainerTypeCommodityQty	CQ	
+			JOIN		tblLGContainerType				CT	ON	CT.intContainerTypeId		=	CQ.intContainerTypeId
+			JOIN		tblICCommodityAttribute			CA	ON	CQ.intCommodityAttributeId	=	CA.intCommodityAttributeId
+	    )CQ	
+	    JOIN	tblICUnitMeasure	RM	ON	RM.intUnitMeasureId		=	CQ.intUnitMeasureId	
+	    WHERE	CQ.intCommodityId			=	@intCommodityId 
+	    AND		CQ.intContainerTypeId		=	@intContainerTypeId 
+	    AND		CQ.intCountryId				=	@intCountryId
+    END
+
+    RETURN;
+END
---
+++ i21Database/dbo/Functions/fnCTGetSeqDisplayField.sql
@@ -0,0 +1,50 @@
+﻿CREATE FUNCTION [dbo].[fnCTGetSeqDisplayField]
+(
+	@intId	 INT,
+	@strTable	 NVARCHAR(100)
+)
+RETURNS NVARCHAR(MAX)
+AS 
+BEGIN 
+	DECLARE	 @strDisplayField NVARCHAR(MAX)
+
+	   IF @intId IS NULL
+		  SELECT @strDisplayField = NULL
+	   ELSE IF @strTable = 'tblCTDiscountType'
+		  SELECT @strDisplayField = strDiscountType FROM tblCTDiscountType WHERE intDiscountTypeId = @intId
+	   ELSE IF @strTable = 'tblGRDiscountId'
+		  SELECT @strDisplayField = strDiscountId FROM tblGRDiscountId WHERE intDiscountId = @intId
+	   ELSE IF @strTable = 'tblGRDiscountSchedule'
+		  SELECT @strDisplayField = strDiscountDescription FROM tblGRDiscountSchedule WHERE intDiscountScheduleId = @intId
+	   ELSE IF @strTable = 'tblSMCompanyLocationSubLocation'
+		  SELECT @strDisplayField = strSubLocationName FROM tblSMCompanyLocationSubLocation WHERE intCompanyLocationSubLocationId = @intId
+	   ELSE IF @strTable = 'tblICStorageLocation'
+		  SELECT @strDisplayField = strName FROM tblICStorageLocation WHERE intStorageLocationId = @intId
+	   ELSE IF @strTable = 'tblEMEntity'
+		  SELECT @strDisplayField = strName FROM tblEMEntity WHERE intEntityId = @intId
+	   ELSE IF @strTable = 'tblEMEntityLocation'
+		  SELECT @strDisplayField = strLocationName FROM tblEMEntityLocation WHERE intEntityLocationId = @intId
+	   ELSE IF @strTable = 'tblEMEntitySplit'
+		  SELECT @strDisplayField = strSplitNumber FROM tblEMEntitySplit WHERE intSplitId = @intId
+	   ELSE IF @strTable = 'tblGRDiscountScheduleCode'
+		  SELECT	@strDisplayField = strDescription 
+		  FROM	tblGRDiscountScheduleCode		SC	  
+		  JOIN	tblICItem						SI	ON	SI.intItemId	=  SC.intItemId 
+		  WHERE	intDiscountScheduleCodeId = @intId
+	   ELSE IF @strTable = 'tblGRStorageScheduleRule'
+		  SELECT @strDisplayField = strScheduleDescription FROM tblGRStorageScheduleRule WHERE intStorageScheduleRuleId = @intId
+	   ELSE IF @strTable = 'tblCTFreightRate'
+		  SELECT @strDisplayField = strOrigin + strDest FROM tblCTFreightRate WHERE intFreightRateId = @intId
+	   ELSE IF @strTable = 'tblCTIndex'
+		  SELECT @strDisplayField = strIndex FROM tblCTIndex WHERE intIndexId = @intId
+	   ELSE IF @strTable = 'tblARMarketZone'
+		  SELECT @strDisplayField = strMarketZoneCode FROM tblARMarketZone WHERE intMarketZoneId = @intId
+	   ELSE IF @strTable = 'tblCTBook'
+		  SELECT @strDisplayField = strBook FROM tblCTBook WHERE intBookId = @intId
+	   ELSE IF @strTable = 'tblCTSubBook'
+		  SELECT @strDisplayField = strSubBook FROM tblCTSubBook WHERE intSubBookId = @intId
+	   ELSE IF @strTable = 'tblCTRailGrade'
+		  SELECT @strDisplayField = strRailGrade FROM tblCTRailGrade WHERE intRailGradeId = @intId
+
+	RETURN @strDisplayField;
+END
