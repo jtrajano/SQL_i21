aa3b92af6d91df6e51d10177fc82e2ef1b9f6258
COMTECH\BPonce <bonn.ponce@irely.com>
2018-09-19 14:51:57 +0800

    CF-1876

--- i21Database/dbo/Tables/tblCFBatchUnpostStagingTable.sql
+++ i21Database/dbo/Tables/tblCFBatchUnpostStagingTable.sql
@@ -1,9 +1,9 @@
 ﻿CREATE TABLE [dbo].[tblCFBatchUnpostStagingTable](
-	[intBatchUnpostStagingTableId]		int IDENTITY(1,1) NOT NULL,
-	[intTransactionId]					int NULL,
-	[strTransactionId]					nvarchar (max) NULL,
-	[strGuid]							nvarchar (max) NULL,
-	[strResult]							nvarchar (max) NULL,
-	[intConcurrencyId]					int CONSTRAINT [DF_tblCFBatchUnpostStagingTable_intConcurrencyId] DEFAULT ((1)) NULL,
- CONSTRAINT [PK_tblCFBatchUnpostStagingTable] PRIMARY KEY CLUSTERED ([intBatchUnpostStagingTableId] ASC
+	[intBatchUnpostStagingTableId]		INT IDENTITY(1,1) NOT NULL,
+	[intTransactionId]					INT NULL,
+	[strTransactionId]					NVARCHAR (max) NULL,
+	[strGuid]							NVARCHAR (max) NULL,
+	[strResult]							NVARCHAR (max) NULL,
+	[intConcurrencyId]					INT CONSTRAINT [DF_tblCFBatchUnpostStagingTable_intConcurrencyId] DEFAULT ((1)) NULL,
+ CONSTRAINT [PK_tblCFBatchUnpostStagingTable] PRIMARY KEY CLUSTERED ([intBatchUnpostStagingTableId] ASC)
 );
