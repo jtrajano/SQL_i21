1fef89afb8da17044248b907573592aa37fb22ca
Kelvin Quinto <kelvin.quinto@irely.com>
2018-09-19 11:25:33 +0800

    AR-8658

--- i21Database/dbo/Tables/tblARInvoice.sql
+++ i21Database/dbo/Tables/tblARInvoice.sql
@@ -180,7 +180,7 @@ DECLARE @intMaxCount INT = 0
 DECLARE @intStartingNumberId INT = 0
 
 INSERT INTO @inserted
-SELECT intInvoiceId, strTransactionType, strType, intCompanyLocationId FROM INSERTED WHERE strInvoiceNumber IS NULL ORDER BY intInvoiceId
+SELECT intInvoiceId, strTransactionType, strType, intCompanyLocationId FROM INSERTED WHERE ISNULL(RTRIM(LTRIM(strInvoiceNumber)), '') = '' ORDER BY intInvoiceId
 
 WHILE((SELECT TOP 1 1 FROM @inserted) IS NOT NULL)
 BEGIN
