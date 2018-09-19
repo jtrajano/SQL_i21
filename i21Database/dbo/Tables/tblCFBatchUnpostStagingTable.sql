5a9f41edeaf835befc3a6f3f115f7c495118c220
COMTECH\BPonce <bonn.ponce@irely.com>
2018-09-19 14:22:08 +0800

    CF-1876

--- i21Database/dbo/Tables/tblCFBatchUnpostStagingTable.sql
+++ i21Database/dbo/Tables/tblCFBatchUnpostStagingTable.sql
@@ -1,16 +1,9 @@
-﻿CREATE TABLE [dbo].[tblCFAccountQuote] (
-    [intAccountQuoteId]   INT             IDENTITY (1, 1) NOT NULL,
-    [intSiteId]           INT             NULL,
-    [strSite]             NVARCHAR (500)   NULL,
-    [strAddress]          NVARCHAR (500)  NULL,
-    [strCity]             NVARCHAR (50)   NULL,
-    [strState]            NVARCHAR (50)   NULL,
-    [intItem]			  INT             NULL,
-    [strItem]			  NVARCHAR (50)   NULL,
-    [dblItemPrice]		  NUMERIC (18, 6) NULL,
-    [intEntityCustomerId] INT             NULL,
-	[dtmEffectiveDate]	  DATETIME		  NULL,
-	[intEntityUserId]	  INT			  NULL,
-    [intConcurrencyId]    INT             CONSTRAINT [DF_tblCFAccountQuote_intConcurrencyId] DEFAULT ((1)) NULL,
-    CONSTRAINT [PK_tblCFAccountQuote] PRIMARY KEY CLUSTERED ([intAccountQuoteId] ASC)
+﻿CREATE TABLE [dbo].[tblCFBatchUnpostStagingTable](
+	[intBatchUnpostStagingTableId]		int IDENTITY(1,1) NOT NULL,
+	[intTransactionId]					int NULL,
+	[strTransactionId]					nvarchar (max) NULL,
+	[strGuid]							nvarchar (max) NULL,
+	[strResult]							nvarchar (max) NULL,
+	[intConcurrencyId]					int CONSTRAINT [DF_tblCFBatchUnpostStagingTable_intConcurrencyId] DEFAULT ((1)) NULL,
+ CONSTRAINT [PK_tblCFBatchUnpostStagingTable] PRIMARY KEY CLUSTERED ([intBatchUnpostStagingTableId] ASC
 );
