b210dc063765093cbe5832af130863898944d145
Arman Arenas <arman.arenas@irely.com>
2018-09-21 11:50:59 +0800

    PR-1798 Update taxable amount and ytd for existing data

--- i21Database/Scripts/Post-Deployment/PostDeployment.sql
+++ i21Database/Scripts/Post-Deployment/PostDeployment.sql
@@ -328,9 +328,9 @@ print 'BEGIN POST DEPLOYMENT'
 -- Payroll
 :r .\PR\DefaultData\1_TaxStatesAndLocalities.sql
 :r .\PR\DefaultData\2_ElectronicFileFormats.sql
-:r .\PR\DataFixes\AddDefaultEmployeeEarningDistribution.sql
-:r .\PR\DataFixes\AddPaycheckDirectDepositEntries.sql
-:r .\PR\DataFixes\ResetEaningHoursToProcess.sql
+--:r .\PR\DataFixes\AddDefaultEmployeeEarningDistribution.sql
+--:r .\PR\DataFixes\AddPaycheckDirectDepositEntries.sql
+--:r .\PR\DataFixes\ResetEaningHoursToProcess.sql
 :r .\PR\DataFixes\SynchronizePaycheckCheckNumber.sql
 :r .\PR\DataFixes\UpdateEarningDeductionTaxId.sql
 :r .\PR\DataFixes\UpdatePaycheckTotalHours.sql
