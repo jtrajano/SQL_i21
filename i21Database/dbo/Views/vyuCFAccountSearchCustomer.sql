64ba34f99a54d41f7b7e4134240af236ddfe7da8
COMTECH\BPonce <bonn.ponce@irely.com>
2018-09-21 15:17:16 +0800

    CF-325

--- i21Database/dbo/Views/vyuCFAccountSearchCustomer.sql
+++ i21Database/dbo/Views/vyuCFAccountSearchCustomer.sql
@@ -1,9 +1,54 @@
 ﻿CREATE VIEW dbo.vyuCFAccountSearchCustomer
 AS
-SELECT     intEntityId, strName, strCustomerNumber, strType, strPhone, strAddress, strCity, strState, strZipCode, ysnActive, intSalespersonId, intCurrencyId, 
-                      intTermsId, intShipViaId, strShipToLocationName, strShipToAddress, strShipToCity, strShipToState, strShipToZipCode, strShipToCountry, strBillToLocationName, 
-                      strBillToAddress, strBillToCity, strBillToState, strBillToZipCode, strBillToCountry
-FROM         dbo.vyuCFCustomerEntity
-WHERE     ([intEntityId] NOT IN
+
+SELECT     
+cfCustomerEntity.intEntityId, 
+strName, 
+strCustomerNumber, 
+cfCustomerEntity.strType, 
+cfCustomerEntity.strPhone, 
+cfCustomerEntity.strAddress, 
+cfCustomerEntity.strCity, 
+cfCustomerEntity.strState, 
+cfCustomerEntity.strZipCode, 
+cfCustomerEntity.ysnActive, 
+intSalespersonId,
+arSalesPerson.strEntityName as strSalesPersonName,
+intCurrencyId, 
+intTermsId, 
+smTerm.strTerm,
+smTerm.strTermCode,
+intShipViaId, 
+strShipToLocationName, 
+strShipToAddress, 
+strShipToCity, 
+strShipToState, 
+strShipToZipCode, 
+strShipToCountry,
+strBillToLocationName, 
+strBillToAddress, 
+strBillToCity, 
+strBillToState, 
+strBillToZipCode, 
+strBillToCountry, 
+accountStatus.*
+FROM  dbo.vyuCFCustomerEntity AS cfCustomerEntity
+---------------------------------------------------------------------
+LEFT JOIN tblSMTerm as smTerm
+ON cfCustomerEntity.intTermsId = smTerm.intTermID
+---------------------------------------------------------------------
+LEFT JOIN vyuEMEntityBasicWithType as arSalesPerson
+ON cfCustomerEntity.intSalespersonId = arSalesPerson.intEntityId
+---------------------------------------------------------------------
+OUTER APPLY 
+	(SELECT TOP 1 strAccountStatusCode, iASC.intAccountStatusId 
+	FROM tblARCustomerAccountStatus as iCASC
+	LEFT JOIN tblARAccountStatus iASC
+	ON iCASC.intAccountStatusId = iASC.intAccountStatusId
+	WHERE intEntityCustomerId = cfCustomerEntity.intEntityId
+	ORDER BY intCustomerAccountStatusId ASC 
+)   AS accountStatus
+---------------------------------------------------------------------
+WHERE     (cfCustomerEntity.[intEntityId] NOT IN
                           (SELECT     intCustomerId
                             FROM          dbo.tblCFAccount))
