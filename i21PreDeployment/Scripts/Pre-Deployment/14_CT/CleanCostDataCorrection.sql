6f6fe9eb5834f28ec40c57bb35da52fa4411b266
Satya S Sahoo <satya.sahoo@irely.com>
2018-09-21 13:54:38 +0530

    CT-2564 - Error on installing 18.13.0920.183 - 18.1B

--- i21PreDeployment/Scripts/Pre-Deployment/14_CT/CleanCostDataCorrection.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/14_CT/CleanCostDataCorrection.sql
@@ -5,6 +5,10 @@ BEGIN
   WHERE intShipmentId NOT IN (SELECT intLoadDetailId from tblLGLoadDetail)'
 END
 GO
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES
+           WHERE TABLE_NAME = N'tblCTContractPlan')
+BEGIN
  exec sp_executesql N'UPDATE tblCTContractPlan SET intWeightId = NULL WHERE intWeightId NOT IN (SELECT intWeightGradeId FROM tblCTWeightGrade)'
  exec sp_executesql N'UPDATE tblCTContractPlan SET intGradeId = NULL WHERE intGradeId NOT IN (SELECT intWeightGradeId FROM tblCTWeightGrade)'
+ END
 GO
