1b7f8ba9eafca3855b88daf4703741b5e0d26922
Kelvin Quinto <kelvin.quinto@irely.com>
2018-09-25 17:17:26 +0800

    AR-8871

---
+++ i21Database/dbo/User Defined Types/InvoiceReportTable.sql
@@ -0,0 +1,7 @@
+﻿CREATE TYPE [dbo].[InvoiceReportTable] AS TABLE
+(
+	 [intInvoiceId]		INT	NULL
+	,[intEntityUserId]	INT	NULL
+	,[strInvoiceFormat]	NVARCHAR(200)	COLLATE Latin1_General_CI_AS	NULL
+	,[strType]			NVARCHAR(200)	COLLATE Latin1_General_CI_AS	NULL
+)
