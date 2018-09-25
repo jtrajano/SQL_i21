2dd9aea7ca679a057d5da774eb648c15773c308f
Satya S Sahoo <satya.sahoo@irely.com>
2018-09-25 20:54:34 +0530

    CT-2580 - Future month is not updating properly during slice - 18.3B

--- i21Database/dbo/Stored Procedures/uspCTSaveContract.sql
+++ i21Database/dbo/Stored Procedures/uspCTSaveContract.sql
@@ -182,7 +182,7 @@ BEGIN TRY
 
 		IF @intConcurrencyId = 1 AND ISNULL(@ysnAutoEvaluateMonth,0) = 1 AND @intPricingTypeId IN (1,2,3,8) AND @ysnSlice = 1
 		BEGIN
-			UPDATE tblCTContractDetail SET dtmPlannedAvailabilityDate = DATEADD(DAY,@intNoOfDays,dtmEndDate), @dtmPlannedAvalability = DATEADD(DAY,@intNoOfDays,dtmEndDate)  WHERE intContractDetailId = @intContractDetailId
+			UPDATE tblCTContractDetail SET dtmPlannedAvailabilityDate = DATEADD(DAY,@intNoOfDays,dtmStartDate), @dtmPlannedAvalability = DATEADD(DAY,@intNoOfDays,dtmStartDate)  WHERE intContractDetailId = @intContractDetailId
 			
 			DECLARE @FutureMonthId INT
 
