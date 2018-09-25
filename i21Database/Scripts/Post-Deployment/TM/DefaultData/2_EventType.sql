0f95cf9049f361d2ccbfc52fed611f7717e3b969
Kenneth Lew Lagunero <kenneth.lagunero@irely.com>
2018-09-25 17:06:08 +0800

    TM-2568

--- i21Database/Scripts/Post-Deployment/TM/DefaultData/2_EventType.sql
+++ i21Database/Scripts/Post-Deployment/TM/DefaultData/2_EventType.sql
@@ -27,9 +27,11 @@ BEGIN
 	IF NOT EXISTS(SELECT TOP 1 1 FROM [dbo].tblTMEventType WHERE strEventType = 'Event-022' AND ysnDefault = 1) INSERT INTO tblTMEventType (strEventType,strDescription,ysnDefault) VALUES ('Event-022','Season Change', 1)
 
 
-	---Device Bought
+	
 	DECLARE @strEventType NVARCHAR(50)
 	DECLARE @intEventId INT
+
+	---Device Bought
 	SET @intEventId = 23
 	SET @strEventType = 'Event-' + RIGHT('000' + CAST(@intEventId AS NVARCHAR(3)),3)
 	
@@ -41,6 +43,19 @@ BEGIN
 			SET @strEventType = 'Event-' + RIGHT('000' + CAST(@intEventId AS NVARCHAR(3)),3)	
 		END
 		INSERT INTO tblTMEventType (strEventType,strDescription,ysnDefault,strDefaultEventType) VALUES (@strEventType,'Device Bought', 1,'Event-023')
+	END
+
+	----Order Abandoned
+	SET @intEventId = 24
+	SET @strEventType = 'Event-' + RIGHT('000' + CAST(@intEventId AS NVARCHAR(3)),3)
+	IF NOT EXISTS(SELECT TOP 1 1 FROM [dbo].tblTMEventType WHERE strDefaultEventType = 'Event-024' AND ysnDefault = 1)
+	BEGIN
+		WHILE EXISTS(SELECT TOP 1 1 FROM tblTMEventType WHERE strEventType = @strEventType)
+		BEGIN
+			SET @intEventId = @intEventId + 1
+			SET @strEventType = 'Event-' + RIGHT('000' + CAST(@intEventId AS NVARCHAR(3)),3)	
+		END
+		INSERT INTO tblTMEventType (strEventType,strDescription,ysnDefault,strDefaultEventType) VALUES (@strEventType,'Order Abandoned', 1,'Event-024')
 	END
 
 
