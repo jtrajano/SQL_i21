4d7ca01802e538fde6f0cf303c79b78e28af74c3
Crisman Lazaga <cris.lazaga@irely.com>
2018-09-26 23:28:10 +0800

    MFT-1282

--- i21Database/dbo/User Defined Types/TFInventoryDetailTax.sql
+++ i21Database/dbo/User Defined Types/TFInventoryDetailTax.sql
@@ -4,6 +4,5 @@
 	intTaxCodeId INT NULL,
 	strCriteria NVARCHAR(100) NULL,
 	dblTax NUMERIC(18, 8) NULL,
-	PRIMARY KEY CLUSTERED ([intInventoryDetailId] ASC) WITH (IGNORE_DUP_KEY = OFF),
 	UNIQUE NONCLUSTERED ([intInventoryDetailId] ASC, [intTaxCodeId] ASC)
 )
