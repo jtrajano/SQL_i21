de9cd202bd3b3b3a15800b5e9a2474174f20b86b
Kenneth Lew Lagunero <kenneth.lagunero@irely.com>
2018-09-27 15:24:30 +0800

    CF-1947

--- i21Database/dbo/Stored Procedures/uspCFImportOriginItem.sql
+++ i21Database/dbo/Stored Procedures/uspCFImportOriginItem.sql
@@ -30,12 +30,33 @@
 		,ysnSuccessful = 0
 		,strFailedReason = 'Duplicate product number on i21 Card Fueling item'
 		,strOriginTable = 'cfitmmst'
-		,strOriginIdentityId = cfitm_prod_no
+		,strOriginIdentityId = A.cfitm_prod_no
 		,strI21Table = 'tblCFItem'
-		,intI21IdentityId = (SELECT TOP 1 intItemId FROM tblCFItem WHERE strProductNumber = cfitm_prod_no COLLATE Latin1_General_CI_AS)
+		,intI21IdentityId = (SELECT TOP 1 intItemId 
+								FROM tblCFItem 
+								WHERE strProductNumber = A.cfitm_prod_no COLLATE Latin1_General_CI_AS
+									AND ISNULL(intSiteId,0) = ISNULL(C.intSiteId,0)
+									AND ISNULL(intNetworkId,0) = ISNULL(B.intNetworkId,0) 
+							)
 		,strUserId = ''
-		FROM cfitmmst 
-		WHERE EXISTS (SELECT TOP 1 1 FROM tblCFItem WHERE strProductNumber = cfitm_prod_no COLLATE Latin1_General_CI_AS)
+		FROM cfitmmst A
+		OUTER APPLY (
+			SELECT TOP 1 intNetworkId FROM tblCFNetwork WHERE strNetwork = A.cfitm_network_id COLLATE Latin1_General_CI_AS
+				
+		)B
+		OUTER APPLY (
+			SELECT TOP 1 
+				intSiteId = CASE WHEN cfitm_site_no = 'NETWRK' THEN NULL ELSE intSiteId END
+			FROM tblCFSite 
+			WHERE strSiteNumber = cfitm_site_no  COLLATE Latin1_General_CI_AS  
+				
+		)C
+		WHERE EXISTS	(SELECT TOP 1 intItemId 
+								FROM tblCFItem 
+								WHERE strProductNumber = A.cfitm_prod_no COLLATE Latin1_General_CI_AS
+									AND ISNULL(intSiteId,0) = ISNULL(C.intSiteId,0)
+									AND ISNULL(intNetworkId,0) = ISNULL(B.intNetworkId,0) 
+						)
 		--------------------------------------------------------------------
 		--------------------------------------------------------------------------
 
@@ -44,116 +65,128 @@
 
 
 		
-		----------------------------------------------------
-		---------Check AR Item.
-		-------------------------------------------------------
-		INSERT INTO tblCFImportResult(
-						dtmImportDate
-						,strSetupName
-						,ysnSuccessful
-						,strFailedReason
-						,strOriginTable
-						,strOriginIdentityId
-						,strI21Table
-						,intI21IdentityId
-						,strUserId
-					)
-		SELECT 
-		 dtmImportDate = GETDATE()
-		,strSetupName = 'Item'
-		,ysnSuccessful = 0
-		,strFailedReason = 'AR product does not exists in i21'
-		,strOriginTable = 'cfitmmst'
-		,strOriginIdentityId = cfitm_prod_no
-		,strI21Table = 'tblICItem'
-		,intI21IdentityId = (SELECT TOP 1 intItemId FROM tblICItem WHERE cfitm_ar_itm_no = cfitm_ar_itm_no COLLATE Latin1_General_CI_AS)
-		,strUserId = ''
-		FROM cfitmmst 
-		WHERE NOT EXISTS (SELECT TOP 1 1 FROM tblCFItem WHERE strProductNumber = cfitm_prod_no COLLATE Latin1_General_CI_AS)
-			AND NOT EXISTS (SELECT TOP 1 1 FROM tblICItem WHERE strItemNo = cfitm_ar_itm_no  COLLATE Latin1_General_CI_AS )
+		------------------------------------------------------
+		-----------Check AR Item.
+		---------------------------------------------------------
+		--INSERT INTO tblCFImportResult(
+		--				dtmImportDate
+		--				,strSetupName
+		--				,ysnSuccessful
+		--				,strFailedReason
+		--				,strOriginTable
+		--				,strOriginIdentityId
+		--				,strI21Table
+		--				,intI21IdentityId
+		--				,strUserId
+		--			)
+		--SELECT 
+		-- dtmImportDate = GETDATE()
+		--,strSetupName = 'Item'
+		--,ysnSuccessful = 0
+		--,strFailedReason = 'AR product does not exists in i21'
+		--,strOriginTable = 'cfitmmst'
+		--,strOriginIdentityId = cfitm_prod_no
+		--,strI21Table = 'tblICItem'
+		--,intI21IdentityId = (SELECT TOP 1 intItemId FROM tblICItem WHERE cfitm_ar_itm_no = cfitm_ar_itm_no COLLATE Latin1_General_CI_AS)
+		--,strUserId = ''
+		--FROM cfitmmst 
+		--WHERE NOT EXISTS (SELECT TOP 1 1 FROM tblCFItem WHERE strProductNumber = cfitm_prod_no COLLATE Latin1_General_CI_AS)
+		--	AND NOT EXISTS (SELECT TOP 1 1 FROM tblICItem WHERE strItemNo = cfitm_ar_itm_no  COLLATE Latin1_General_CI_AS )
 		
-		--------------------------------------------------------------------
-		--------------------------------------------------------------------------
+		----------------------------------------------------------------------
+		----------------------------------------------------------------------------
 
 
 
-		----------------------------------------------------
-		---------Check Site .
-		-------------------------------------------------------
-		INSERT INTO tblCFImportResult(
-						dtmImportDate
-						,strSetupName
-						,ysnSuccessful
-						,strFailedReason
-						,strOriginTable
-						,strOriginIdentityId
-						,strI21Table
-						,intI21IdentityId
-						,strUserId
-					)
-		SELECT 
-		 dtmImportDate = GETDATE()
-		,strSetupName = 'Item'
-		,ysnSuccessful = 0
-		,strFailedReason = 'Site does not exists in i21 Card fueling'
-		,strOriginTable = 'cfitmmst'
-		,strOriginIdentityId = cfitm_site_no
-		,strI21Table = 'tblCFSite'
-		,intI21IdentityId = (SELECT TOP 1 intSiteId FROM tblCFSite WHERE strSiteNumber = cfitm_site_no  COLLATE Latin1_General_CI_AS)
-		,strUserId = ''
-		FROM cfitmmst 
-		WHERE NOT EXISTS (SELECT TOP 1 1 FROM tblCFItem WHERE strProductNumber = cfitm_prod_no COLLATE Latin1_General_CI_AS)
-			AND EXISTS (SELECT TOP 1 1 FROM tblICItem WHERE strItemNo = cfitm_ar_itm_no  COLLATE Latin1_General_CI_AS )
-			AND NOT EXISTS (SELECT TOP 1 1 FROM tblCFSite WHERE strSiteNumber = cfitm_site_no  COLLATE Latin1_General_CI_AS AND cfitm_site_no <> 'NETWRK' )
+		------------------------------------------------------
+		-----------Check Site .
+		---------------------------------------------------------
+		--INSERT INTO tblCFImportResult(
+		--				dtmImportDate
+		--				,strSetupName
+		--				,ysnSuccessful
+		--				,strFailedReason
+		--				,strOriginTable
+		--				,strOriginIdentityId
+		--				,strI21Table
+		--				,intI21IdentityId
+		--				,strUserId
+		--			)
+		--SELECT 
+		-- dtmImportDate = GETDATE()
+		--,strSetupName = 'Item'
+		--,ysnSuccessful = 0
+		--,strFailedReason = 'Site does not exists in i21 Card fueling'
+		--,strOriginTable = 'cfitmmst'
+		--,strOriginIdentityId = cfitm_site_no
+		--,strI21Table = 'tblCFSite'
+		--,intI21IdentityId = (SELECT TOP 1 intSiteId FROM tblCFSite WHERE strSiteNumber = cfitm_site_no  COLLATE Latin1_General_CI_AS)
+		--,strUserId = ''
+		--FROM cfitmmst 
+		--WHERE NOT EXISTS (SELECT TOP 1 1 FROM tblCFItem WHERE strProductNumber = cfitm_prod_no COLLATE Latin1_General_CI_AS)
+		--	AND EXISTS (SELECT TOP 1 1 FROM tblICItem WHERE strItemNo = cfitm_ar_itm_no  COLLATE Latin1_General_CI_AS )
+		--	AND NOT EXISTS (SELECT TOP 1 1 FROM tblCFSite WHERE strSiteNumber = cfitm_site_no  COLLATE Latin1_General_CI_AS AND cfitm_site_no <> 'NETWRK' )
 
-		--------------------------------------------------------------------
-		--------------------------------------------------------------------------
+		----------------------------------------------------------------------
+		----------------------------------------------------------------------------
 
 
 
-		----------------------------------------------------
-		---------Check Network .
-		-------------------------------------------------------
-		INSERT INTO tblCFImportResult(
-						dtmImportDate
-						,strSetupName
-						,ysnSuccessful
-						,strFailedReason
-						,strOriginTable
-						,strOriginIdentityId
-						,strI21Table
-						,intI21IdentityId
-						,strUserId
-					)
-		SELECT 
-		 dtmImportDate = GETDATE()
-		,strSetupName = 'Item'
-		,ysnSuccessful = 0
-		,strFailedReason = 'Network does not exists in i21 Card fueling'
-		,strOriginTable = 'cfitmmst'
-		,strOriginIdentityId = cfitm_network_id
-		,strI21Table = 'tblCFNetwork'
-		,intI21IdentityId = (SELECT TOP 1 intNetworkId FROM tblCFNetwork WHERE strNetwork = cfitm_network_id  COLLATE Latin1_General_CI_AS)
-		,strUserId = ''
-		FROM cfitmmst 
-		WHERE NOT EXISTS (SELECT TOP 1 1 FROM tblCFItem WHERE strProductNumber = cfitm_prod_no COLLATE Latin1_General_CI_AS)
-			AND EXISTS (SELECT TOP 1 1 FROM tblICItem WHERE strItemNo = cfitm_ar_itm_no  COLLATE Latin1_General_CI_AS )
-			AND EXISTS (SELECT TOP 1 1 FROM tblCFSite WHERE strSiteNumber = cfitm_site_no  COLLATE Latin1_General_CI_AS AND cfitm_site_no <> 'NETWRK' )
-			AND NOT EXISTS(SELECT TOP 1 1 FROM tblCFNetwork WHERE strNetwork = cfitm_network_id COLLATE Latin1_General_CI_AS )
-		--------------------------------------------------------------------
-		--------------------------------------------------------------------------
+		------------------------------------------------------
+		-----------Check Network .
+		---------------------------------------------------------
+		--INSERT INTO tblCFImportResult(
+		--				dtmImportDate
+		--				,strSetupName
+		--				,ysnSuccessful
+		--				,strFailedReason
+		--				,strOriginTable
+		--				,strOriginIdentityId
+		--				,strI21Table
+		--				,intI21IdentityId
+		--				,strUserId
+		--			)
+		--SELECT 
+		-- dtmImportDate = GETDATE()
+		--,strSetupName = 'Item'
+		--,ysnSuccessful = 0
+		--,strFailedReason = 'Network does not exists in i21 Card fueling'
+		--,strOriginTable = 'cfitmmst'
+		--,strOriginIdentityId = cfitm_network_id
+		--,strI21Table = 'tblCFNetwork'
+		--,intI21IdentityId = (SELECT TOP 1 intNetworkId FROM tblCFNetwork WHERE strNetwork = cfitm_network_id  COLLATE Latin1_General_CI_AS)
+		--,strUserId = ''
+		--FROM cfitmmst 
+		--WHERE NOT EXISTS (SELECT TOP 1 1 FROM tblCFItem WHERE strProductNumber = cfitm_prod_no COLLATE Latin1_General_CI_AS)
+		--	AND EXISTS (SELECT TOP 1 1 FROM tblICItem WHERE strItemNo = cfitm_ar_itm_no  COLLATE Latin1_General_CI_AS )
+		--	AND EXISTS (SELECT TOP 1 1 FROM tblCFSite WHERE strSiteNumber = cfitm_site_no  COLLATE Latin1_General_CI_AS AND cfitm_site_no <> 'NETWRK' )
+		--	AND NOT EXISTS(SELECT TOP 1 1 FROM tblCFNetwork WHERE strNetwork = cfitm_network_id COLLATE Latin1_General_CI_AS )
+		----------------------------------------------------------------------
+		----------------------------------------------------------------------------
 		
 
 		IF(OBJECT_ID('tempdb..#tblOriginItem') IS NOT NULL) DROP TABLE #tblOriginItem
-
-		SELECT 
-			* 
+		
+		SELECT A.*
 		INTO #tblOriginItem
-		FROM cfitmmst
-		WHERE NOT EXISTS (SELECT TOP 1 1 FROM tblCFItem WHERE strProductNumber = cfitm_prod_no COLLATE Latin1_General_CI_AS)
-			AND EXISTS (SELECT TOP 1 1 FROM tblICItem WHERE strItemNo = cfitm_ar_itm_no  COLLATE Latin1_General_CI_AS )
-			AND EXISTS (SELECT TOP 1 1 FROM tblCFSite WHERE strSiteNumber = cfitm_site_no  COLLATE Latin1_General_CI_AS AND cfitm_site_no <> 'NETWRK' )
-			AND EXISTS (SELECT TOP 1 1 FROM tblCFNetwork WHERE strNetwork = cfitm_network_id COLLATE Latin1_General_CI_AS)
+		FROM cfitmmst A
+		OUTER APPLY (
+			SELECT TOP 1 intNetworkId FROM tblCFNetwork WHERE strNetwork = A.cfitm_network_id COLLATE Latin1_General_CI_AS
+				
+		)B
+		OUTER APPLY (
+			SELECT TOP 1 
+				intSiteId = CASE WHEN cfitm_site_no = 'NETWRK' THEN NULL ELSE intSiteId END
+			FROM tblCFSite 
+			WHERE strSiteNumber = cfitm_site_no  COLLATE Latin1_General_CI_AS  
+				
+		)C
+		WHERE NOT EXISTS (SELECT TOP 1 intItemId 
+								FROM tblCFItem 
+								WHERE strProductNumber = A.cfitm_prod_no COLLATE Latin1_General_CI_AS
+									AND ISNULL(intSiteId,0) = ISNULL(C.intSiteId,0)
+									AND ISNULL(intNetworkId,0) = ISNULL(B.intNetworkId,0) 
+						)
 
 
 		INSERT INTO [dbo].[tblCFItem]
