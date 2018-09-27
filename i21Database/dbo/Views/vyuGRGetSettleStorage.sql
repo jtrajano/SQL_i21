24c85c45d5664ff62a5f87bfa860ae60c6eff5b1
Gopinath Nayak <gopinath.n@irely.com>
2018-09-27 16:15:10 +0530

    GRN-1187 - HDTN-218348 - Wheat Inventory off due to purchasing same crop multiple times

--- i21Database/dbo/Views/vyuGRGetSettleStorage.sql
+++ i21Database/dbo/Views/vyuGRGetSettleStorage.sql
@@ -1,6 +1,6 @@
 ﻿CREATE VIEW [dbo].[vyuGRGetSettleStorage]
 AS     
- SELECT DISTINCT
+ SELECT 
  intSettleStorageId			= SS.intSettleStorageId
 ,intEntityId				= SS.intEntityId
 ,strEntityName				= E.strName
@@ -62,12 +62,76 @@ LEFT JOIN tblGRCustomerStorage CS
 	ON T.intCustomerStorageId = CS.intCustomerStorageId
 LEFT JOIN tblGRStorageHistory SH 
 	ON CS.intCustomerStorageId = SH.intCustomerStorageId 
-		AND SH.intSettleStorageId IS NOT NULL
+		AND SH.intSettleStorageId = SS.intSettleStorageId
 LEFT JOIN tblGRSettleContract SCT
 	ON SS.intSettleStorageId = SCT.intSettleStorageId
 LEFT JOIN tblCTContractDetail CD 
-	ON SCT.intContractDetailId = CD.intContractDetailId
+	ON SCT.intContractDetailId = CD.intContractDetailId AND CD.intContractHeaderId = SH.intContractHeaderId
 LEFT JOIN tblSCTicket SC				
 	ON SC.intTicketId = CS.intTicketId
 LEFT JOIN tblCTContractHeader CH 
 	ON CH.intContractHeaderId = ISNULL(SC.intContractId, CD.intContractHeaderId)
+WHERE (SS.intParentSettleStorageId >0 AND (ISNULL(CD.intContractHeaderId,0) >0 OR  ISNULL(SS.dblSpotUnits,0) > 0))
+
+UNION ALL
+
+ SELECT 
+ intSettleStorageId			= SS.intSettleStorageId
+,intEntityId				= SS.intEntityId
+,strEntityName				= E.strName
+,intCompanyLocationId		= SS.intCompanyLocationId
+,strLocationName			= L.strLocationName
+,intItemId					= SS.intItemId
+,strItemNo					= Item.strItemNo
+,dblSpotUnits				= SS.dblSpotUnits
+,dblFuturesPrice			= SS.dblFuturesPrice
+,dblFuturesBasis			= SS.dblFuturesBasis
+,dblCashPrice				= SS.dblCashPrice
+,strStorageAdjustment		= SS.strStorageAdjustment
+,dtmCalculateStorageThrough = SS.dtmCalculateStorageThrough
+,dblAdjustPerUnit			= SS.dblAdjustPerUnit
+,dblStorageDue				= SS.dblStorageDue
+,strStorageTicket			= SS.strStorageTicket
+,dblSelectedUnits			= SS.dblSelectedUnits
+,dblUnpaidUnits				= SS.dblUnpaidUnits
+,dblSettleUnits				= SS.dblSettleUnits
+,dblDiscountsDue			= SS.dblDiscountsDue
+,dblNetSettlement			= SS.dblNetSettlement
+,intCreatedUserId			= SS.intCreatedUserId
+,strUserName				= Entity.strUserName
+,dtmCreated					= SS.dtmCreated
+,ysnPosted					= SS.ysnPosted
+,intBillId					= SS.intBillId
+,strBillId					= ISNULL(Bill.strBillId,'')
+,intContractId				= SH.intContractHeaderId
+,strContractNumber			= NULL
+,intTicketId				= SC.intTicketId
+,strTicketNumber			= SC.strTicketNumber
+,strUnits					= CONVERT(VARCHAR(MAX),dbo.fnRemoveTrailingZeroes(T.dblUnits)) + ' ' + UOM.strSymbol
+FROM tblGRSettleStorage SS
+JOIN tblEMEntity E 
+	ON E.intEntityId = SS.intEntityId
+JOIN tblSMCompanyLocation L 
+	ON L.intCompanyLocationId = SS.intCompanyLocationId  
+JOIN tblICItem Item 
+	ON Item.intItemId = SS.intItemId
+LEFT JOIN tblICItemUOM ItemUOM 
+	ON SS.intItemId = ItemUOM.intItemId 
+		AND ItemUOM.ysnStockUOM = 1
+LEFT JOIN tblICUnitMeasure UOM 
+	ON ItemUOM.intUnitMeasureId = UOM.intUnitMeasureId
+JOIN tblSMUserSecurity Entity 
+	ON Entity.intEntityId = SS.intCreatedUserId
+LEFT JOIN tblAPBill Bill 
+	ON Bill.intBillId = SS.intBillId
+LEFT JOIN tblGRSettleStorageTicket T 
+	ON T.intSettleStorageId = SS.intSettleStorageId 
+		AND SS.intParentSettleStorageId IS NOT NULL
+LEFT JOIN tblGRCustomerStorage CS 
+	ON T.intCustomerStorageId = CS.intCustomerStorageId
+LEFT JOIN tblGRStorageHistory SH 
+	ON CS.intCustomerStorageId = SH.intCustomerStorageId 
+		AND SH.intSettleStorageId = SS.intSettleStorageId
+LEFT JOIN tblSCTicket SC				
+	ON SC.intTicketId = CS.intTicketId
+WHERE ISNULL(SS.intParentSettleStorageId,0) = 0 
