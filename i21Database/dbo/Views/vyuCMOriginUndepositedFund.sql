aa6d561c66b3950ff61f7529d17327dcfef7faab
Jeffrey <jlt212000@yahoo.com>
2018-10-02 13:35:37 +0800

    GL-6684 Show EOD in Undeposited Funds

--- i21Database/dbo/Views/vyuCMOriginUndepositedFund.sql
+++ i21Database/dbo/Views/vyuCMOriginUndepositedFund.sql
@@ -3,9 +3,16 @@
 -- The real view is in the integration script. A stub view is used to avoid errors in the undeposited screen process when the 
 -- origin AP module is not installed. 
 
-CREATE VIEW vyuCMOriginUndepositedFund
+
+CREATE VIEW [dbo].[vyuCMOriginUndepositedFund]
 AS
-WITH C AS (
+WITH AR AS (
+	SELECT intAccountId,  intEntityCustomerId,intCurrencyId, intPaymentId intSourceTransactionId, strRecordNumber strSourceTransactionId, 'Payment' strType FROM tblARPayment UNION
+	SELECT intAccountId,  intEntityCustomerId,intCurrencyId, intInvoiceId intSourceTransactionId, strInvoiceNumber strSourceTransactionId, 'Invoice' strType FROM tblARInvoice UNION
+	SELECT intCashOverShortId intAccountId,intEntityId intEntityCustomerId,intCurrencyId, intPOSEndOfDayId intSourceTransactionId, strEODNo strSourceTransactionId, 'EndOfDay' strType FROM tblARPOSEndOfDay
+),
+
+C AS (
 	SELECT
 		id						= CAST(ROW_NUMBER() OVER (ORDER BY CMUF.intUndepositedFundId) AS INT), 
 		intUndepositedFundId	= CMUF.intUndepositedFundId, 
@@ -16,47 +23,39 @@ WITH C AS (
 		intEntityCustomerId		= ARP.intEntityCustomerId,
 		dtmDate					= CMUF.dtmDate,
 		intCurrencyId			= ARP.intCurrencyId,
-		 dblWeightRate			= case when F.dblWeightRate is null then 1 else F.dblWeightRate end
+		dblWeightRate			= CASE WHEN strType = 'Payment'
+										THEN 
+											CASE WHEN F.dblWeightRate is null THEN 1 ELSE F.dblWeightRate END
+								  WHEN strType = 'Invoice'
+										THEN
+											case WHEN G.dblWeightRate is null THEN 1 ELSE G.dblWeightRate END
+								  ELSE
+										1
+								  END
+
 	FROM
 		tblCMUndepositedFund CMUF
 	INNER JOIN
-		tblARPayment ARP
-			ON CMUF.intSourceTransactionId = ARP.intPaymentId
-			AND CMUF.strSourceTransactionId = ARP.strRecordNumber
+		AR ARP
+			ON CMUF.intSourceTransactionId = ARP.intSourceTransactionId
+			AND CMUF.strSourceTransactionId = ARP.strSourceTransactionId
 	OUTER APPLY(
 		SELECT dblWeightRate = 
 			SUM(CASE WHEN ARPD.dblCurrencyExchangeRate > 0 
 				THEN ARPD.dblCurrencyExchangeRate
 				ELSE 1 END * ARPD.dblPayment )/
 			NULLIF(SUM(ARPD.dblPayment), 0) 
-		FROM tblARPaymentDetail ARPD WHERE ARPD.intPaymentId = ARP.intPaymentId
+		FROM tblARPaymentDetail ARPD WHERE ARPD.intPaymentId = ARP.intSourceTransactionId
 	)F
-	UNION
-	SELECT
-	    id						= CAST(ROW_NUMBER() OVER (ORDER BY CMUF.intUndepositedFundId) AS INT), 
-		intUndepositedFundId	= CMUF.intUndepositedFundId, 
-		intBankAccountId		= CMUF.intBankAccountId, 
-		intGLAccountId			= ARI.intAccountId,
-		dblAmount				= CMUF.dblAmount,
-		strName					= CMUF.strName, 
-		intEntityCustomerId		= ARI.intEntityCustomerId,
-		dtmDate					= CMUF.dtmDate,
-		intCurrencyId			= ARI.intCurrencyId,
-		 dblWeightRate			= case when G.dblWeightRate is null then 1 else G.dblWeightRate end
-	FROM
-		tblCMUndepositedFund CMUF
-	INNER JOIN
-		tblARInvoice ARI
-			ON CMUF.intSourceTransactionId = ARI.intInvoiceId
-			AND CMUF.strSourceTransactionId = ARI.strInvoiceNumber
 	OUTER APPLY(
 		SELECT dblWeightRate = 
 			SUM(CASE WHEN ARID.dblCurrencyExchangeRate > 0 
 				THEN ARID.dblCurrencyExchangeRate
 				ELSE 1 END * ARID.dblTotal )/
 			NULLIF(SUM(ARID.dblTotal), 0)
-		FROM tblARInvoiceDetail ARID WHERE ARID.intInvoiceId = ARI.intInvoiceId
+		FROM tblARInvoiceDetail ARID WHERE ARID.intInvoiceId = ARP.intSourceTransactionId
 	)G
+	
 )
 SELECT
 	id,
@@ -74,3 +73,4 @@ SELECT
 OUTER APPLY(
 	SELECT GL.strDescription FROM tblGLAccount GL WHERE GL.intAccountId = c.intGLAccountId
 )Account
+GO
