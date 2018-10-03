a9436ef0f111df812d9c76a51467e168f29deea1
Crisman Lazaga <cris.lazaga@irely.com>
2018-10-03 14:59:05 +0800

    MFT-1233

--- i21Database/dbo/Stored Procedures/uspTFGetTransporterInvoiceTax.sql
+++ i21Database/dbo/Stored Procedures/uspTFGetTransporterInvoiceTax.sql
@@ -858,18 +858,18 @@ BEGIN TRY
 				, strOriginCounty
 				, strOriginState
 				, strCustomerName
-				, strCustomerFEIN
+				, REPLACE(strCustomerFEIN, '-', '')
 				, strShipVia
 				, strTransporterLicense
 				, strTransportationMode
 				, strTransporterName
-				, strTransporterFEIN
+				, REPLACE(strTransporterFEIN, '-', '')
 				, strConsignorName
-				, strConsignorFEIN
+				, REPLACE(strConsignorFEIN, '-', '')
 				, strTaxCode
 				, strTerminalControlNumber
 				, strVendorName
-				, strVendorFederalTaxId
+				, REPLACE(strVendorFederalTaxId, '-', '')
 				, strHeaderCompanyName
 				, strHeaderAddress
 				, strHeaderCity
@@ -877,7 +877,7 @@ BEGIN TRY
 				, strHeaderZip
 				, strHeaderPhone
 				, strHeaderStateTaxID
-				, strHeaderFederalTaxID
+				, REPLACE(strHeaderFederalTaxID, '-', '')
 				, @DateFrom
 				, @DateTo
 				, strTransporterIdType
