6b3a2eb7ad35156f6cddb62d136b77204aa12434
Leslie L Arat <leslie.arat@irely.com>
2018-10-06 08:09:41 +0800

    GRN-1211 ([settlment report] - Tickets with portions of the quantity at different prices is not calculating correctly on the report.)

--- i21Database/dbo/Views/vyuGRSettlementSubReport.sql
+++ i21Database/dbo/Views/vyuGRSettlementSubReport.sql
@@ -27,7 +27,7 @@ FROM
 	,t3.dblGradeReading
 	,t3.dblAmount * (t1.dblQtyOrdered / t2.dblTotalQty)dblAmount	
 	,t3.intContractDetailId
-	,CASE WHEN t3.intContractDetailId = 0 THEN t3.dblTax ELSE 0 END AS dblTax
+	,t3.dblTax * (t1.dblQtyOrdered / t2.dblTotalQty)dblTax
 	,t3.dblNetTotal* (t1.dblQtyOrdered / t2.dblTotalQty) dblNetTotal
 	FROM (
 			 SELECT 
@@ -147,10 +147,10 @@ FROM
 	,t3.dblDiscountAmount * (t1.dblQtyOrdered / t2.dblTotalQty) dblDiscountAmount
 	,t3.dblShrinkPercent * (t1.dblQtyOrdered / t2.dblTotalQty) dblShrinkPercent
 	,t3.dblGradeReading
-	,t3.dblAmount * (t1.dblQtyOrdered / t2.dblTotalQty)dblAmount	
+	,t3.dblAmount * (t1.dblQtyOrdered / t2.dblTotalQty) dblAmount	
 	,t3.intContractDetailId
-	,CASE WHEN t3.intContractDetailId <> 0 THEN t3.dblTax ELSE 0 END AS dblTax
-	,t3.dblNetTotal* (t1.dblQtyOrdered / t2.dblTotalQty) dblNetTotal
+	,t3.dblTax * (t1.dblQtyOrdered / t2.dblTotalQty) dblTax
+	,t3.dblNetTotal * (t1.dblQtyOrdered / t2.dblTotalQty) dblNetTotal
 	FROM (
 			 SELECT 
 			 BillDtl.intBillDetailId
@@ -237,7 +237,9 @@ FROM
 				WHERE QM.strSourceType = 'Storage'
 		  ) StorageDiscount ON StorageDiscount.intTicketFileId = BillDtl.intCustomerStorageId AND StorageDiscount.intItemId = BillDtl.intItemId
 
-	WHERE  Item.strType = 'Other Charge' AND ISNULL(BillDtl.intContractDetailId, 0) > 0
+	WHERE  Item.strType = 'Other Charge'
+		AND ((StrgHstry.intContractHeaderId IS NOT NULL) --settlement with contract
+			OR (BillDtl.intInventoryReceiptChargeId IS NOT NULL AND BillDtl.intContractDetailId IS NOT NULL)) 
 
 	) tbl 
 	GROUP BY 
