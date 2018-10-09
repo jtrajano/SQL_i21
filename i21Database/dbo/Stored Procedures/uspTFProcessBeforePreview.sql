55dc8ec98ff305190a6c4b178d191f02dc84f269
Crisman Lazaga <cris.lazaga@irely.com>
2018-10-09 08:29:21 +0800

    MFT-1296

--- i21Database/dbo/Stored Procedures/uspTFProcessBeforePreview.sql
+++ i21Database/dbo/Stored Procedures/uspTFProcessBeforePreview.sql
@@ -102,12 +102,15 @@ BEGIN TRY
 
 			SELECT Trans.intTransactionId
 				, [strOriginAltFacilityNumber] = CASE WHEN @ScheduleCode IN ('5', '5LO', '6', '7', '5BLK', '6BLK') THEN Origin.strOregonFacilityNumber WHEN @ScheduleCode IN ('5CRD', '6CRD') THEN tblCFSite.strOregonFacilityNumber ELSE NULL END
-				, [strDestinationAltFacilityNumber] = CASE WHEN @ScheduleCode IN ('5', '5LO', '6', '7', '5BLK', '6BLK') THEN Destination.strOregonFacilityNumber ELSE NULL END
+				, [strDestinationAltFacilityNumber] = CASE WHEN @ScheduleCode IN ('5', '5LO', '6', '7', '5BLK', '6BLK', '5CRD', '6CRD') AND Invoice.strType = 'Tank Delivery' THEN tblTMSite.strFacilityNumber ELSE Destination.strOregonFacilityNumber END
 				, [strAltDocumentNumber] = CASE WHEN Invoice.strType = 'CF Tran' AND @ScheduleCode IN ('5CRD', '6CRD') THEN tblCFCard.strCardNumber ELSE NULL END
 				, [strExplanation] = CASE WHEN Invoice.strType = 'CF Tran' AND @ScheduleCode IN ('5CRD', '6CRD') THEN tblCFVehicle.strVehicleDescription ELSE NULL END
 				, [strInvoiceNumber] = CASE WHEN @ScheduleCode IN ('5BLK', '6BLK', '5CRD', '6CRD') THEN Invoice.strInvoiceNumber ELSE NULL END
 			FROM vyuTFGetTransaction Trans
 			LEFT JOIN tblARInvoiceDetail InvoiceDetail ON InvoiceDetail.intInvoiceDetailId = Trans.intTransactionNumberId
+				LEFT JOIN tblTMDeliveryHistoryDetail ON tblTMDeliveryHistoryDetail.intInvoiceDetailId = InvoiceDetail.intInvoiceDetailId
+				LEFT JOIN tblTMDeliveryHistory ON tblTMDeliveryHistory.intDeliveryHistoryID = tblTMDeliveryHistoryDetail.intDeliveryHistoryID
+				LEFT JOIN tblTMSite ON tblTMSite.intSiteID = tblTMDeliveryHistory.intSiteID
 			LEFT JOIN tblARInvoice Invoice ON Invoice.intInvoiceId = InvoiceDetail.intInvoiceId
 			LEFT JOIN tblSMCompanyLocation Origin ON Origin.intCompanyLocationId = Invoice.intCompanyLocationId
 			LEFT JOIN tblEMEntityLocation Destination ON Destination.intEntityLocationId = Invoice.intShipToLocationId
