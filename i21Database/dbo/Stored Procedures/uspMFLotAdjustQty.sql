b5658d94a54381ae9df9b2ebf92215e4b03ab4b0
Premkumar S <prem.selladurai@irely.com>
2018-10-11 22:40:43 +0530

    MFG-3793

--- i21Database/dbo/Stored Procedures/uspMFLotAdjustQty.sql
+++ i21Database/dbo/Stored Procedures/uspMFLotAdjustQty.sql
@@ -191,20 +191,20 @@ BEGIN TRY
 
 	SET @strNote1 = isNULL(@strReasonCode, '') + ' ' + IsNULL(@strNotes, '')
 
-	EXEC uspICInventoryAdjustment_CreatePostQtyChange @intItemId
-		,@dtmDate
-		,@intLocationId
-		,@intSubLocationId
-		,@intStorageLocationId
-		,@strLotNumber
-		,@dblAdjustByQuantity
-		,@dblNewUnitCost
-		,@intAdjustItemUOMId
-		,@intSourceId
-		,@intSourceTransactionTypeId
-		,@intUserId
-		,@intInventoryAdjustmentId OUTPUT
-		,@strDescription
+	EXEC uspICInventoryAdjustment_CreatePostQtyChange @intItemId=@intItemId
+		,@dtmDate=@dtmDate
+		,@intLocationId=@intLocationId
+		,@intSubLocationId=@intSubLocationId
+		,@intStorageLocationId=@intStorageLocationId
+		,@strLotNumber=@strLotNumber
+		,@dblAdjustByQuantity=@dblAdjustByQuantity
+		,@dblNewUnitCost=@dblNewUnitCost
+		,@intItemUOMId=@intAdjustItemUOMId
+		,@intSourceId=@intSourceId
+		,@intSourceTransactionTypeId=@intSourceTransactionTypeId
+		,@intEntityUserSecurityId=@intUserId
+		,@intInventoryAdjustmentId=@intInventoryAdjustmentId OUTPUT
+		,@strDescription=@strDescription
 
 	EXEC dbo.uspMFAdjustInventory @dtmDate = @dtmDate
 		,@intTransactionTypeId = 10
