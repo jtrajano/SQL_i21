2a7966920f452e07f15da8588643747041ccbedf
Vinoth Kumar T <vinoth.t@irely.com>
2018-10-12 22:51:14 +0530

    IN-287

---
+++ i21Database/dbo/Tables/tblIPEntityTermError.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[tblIPEntityTermError]
+(
+	[intStageEntityTermId] INT IDENTITY(1,1),
+	[intStageEntityId] INT,
+	strEntityName NVARCHAR(100) COLLATE Latin1_General_CI_AS,
+	strLocation NVARCHAR(50) COLLATE Latin1_General_CI_AS,
+	strTerm NVARCHAR(100) COLLATE Latin1_General_CI_AS,
+	strCurrency NVARCHAR(50) COLLATE Latin1_General_CI_AS,
+	CONSTRAINT [PK_tblIPEntityTermError_intStageEntityTermId] PRIMARY KEY ([intStageEntityTermId])
+)
