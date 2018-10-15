4bc796db81af3eeca011805e1d5638b784762977
COMTECH\EVersoza <emerson.versoza@irely.com>
2018-10-15 18:24:11 +0800

    SC-1791

---
+++ i21Database/Scripts/Post-Deployment/GR/2_ScaleTrigger.sql
@@ -0,0 +1,42 @@
+﻿PRINT '********************** BEGIN SC Triggers **********************'
+GO
+
+
+IF  EXISTS (SELECT * FROM sys.triggers WHERE object_id = OBJECT_ID(N'[dbo].[trg_tblSCTicket]'))
+DROP TRIGGER [dbo].[trg_tblSCTicket]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+CREATE TRIGGER [dbo].[trg_tblSCTicket] ON [dbo].[tblSCTicket]
+INSTEAD OF DELETE 
+AS
+BEGIN
+	DECLARE @strTicketStatus NVARCHAR(4);
+	DECLARE @strTicketNumber NVARCHAR(MAX);
+	DECLARE @error NVARCHAR(500);
+	DECLARE @ysnHasGeneratedTicketNumber BIT;
+
+	SELECT @strTicketNumber = strTicketNumber, @strTicketStatus = strTicketStatus, @ysnHasGeneratedTicketNumber = ysnHasGeneratedTicketNumber FROM deleted
+
+	IF ISNULL(@ysnHasGeneratedTicketNumber, 0) = 1
+		BEGIN
+			SET @error = 'You cannot delete Scale Ticket (' + @strTicketNumber + ') already had valid ticket number';
+			RAISERROR(@error, 16, 1);
+		END
+	ELSE
+		BEGIN
+			DELETE A
+			FROM [tblSCTicket] A
+			INNER JOIN DELETED B ON A.intTicketId = B.intTicketId
+		END
+END
+GO
+
+PRINT '********************** END SC Triggers **********************'
+GO
