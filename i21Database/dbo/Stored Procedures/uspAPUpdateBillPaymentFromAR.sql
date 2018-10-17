552d57c37f8fc2b5055283219ecc8ba833d15d35
Erick <erick.delacruz@irely.com>
2018-10-17 14:14:54 +0800

    AP-6230

--- i21Database/dbo/Stored Procedures/uspAPUpdateBillPaymentFromAR.sql
+++ i21Database/dbo/Stored Procedures/uspAPUpdateBillPaymentFromAR.sql
@@ -12,26 +12,36 @@ SET NOCOUNT ON
 SET XACT_ABORT ON
 SET ANSI_WARNINGS OFF
 
-UPDATE C
-SET	C.dblPayment = (CASE WHEN @post = 1 THEN C.dblPayment + ABS(B.dblPayment) ELSE C.dblPayment - ABS(B.dblPayment) END),
-	C.ysnPrepayHasPayment = 1
-FROM
-	tblARPayment A
-INNER JOIN
-	tblARPaymentDetail B 
-		ON A.intPaymentId = B.intPaymentId
-INNER JOIN
-	tblAPBill C
-		ON B.intBillId = C.intBillId
-WHERE
-	B.dblPayment <> 0 
-	AND A.intPaymentId IN (SELECT intId FROM @paymentIds)
+-- UPDATE C
+-- SET	C.dblPayment = (CASE WHEN @post = 1 THEN C.dblPayment + ABS(B.dblPayment) ELSE C.dblPayment - ABS(B.dblPayment) END),
+-- 	C.ysnPrepayHasPayment = 1
+-- FROM
+-- 	tblARPayment A
+-- INNER JOIN
+-- 	tblARPaymentDetail B 
+-- 		ON A.intPaymentId = B.intPaymentId
+-- INNER JOIN
+-- 	tblAPBill C
+-- 		ON B.intBillId = C.intBillId
+-- WHERE
+-- 	B.dblPayment <> 0 
+-- 	AND A.intPaymentId IN (SELECT intId FROM @paymentIds)
 
 UPDATE C
 SET 
-	C.dblAmountDue = (C.dblTotal + C.dblInterest) - (C.dblPayment + C.dblDiscount),
-	C.ysnPaid = (CASE WHEN ((C.dblTotal + C.dblInterest) - (C.dblPayment + C.dblDiscount)) = 0 THEN 1 ELSE 0 END),
-	C.dtmDatePaid = (CASE WHEN ((C.dblTotal + C.dblInterest) - (C.dblPayment + C.dblDiscount)) = 0 THEN A.dtmDatePaid ELSE NULL END)			
+	C.dblAmountDue = CASE WHEN @post = 1 
+						THEN C.dblAmountDue - (ABS(B.dblPayment) + ABS(B.dblInterest) - ABS(B.dblDiscount))--(C.dblTotal + C.dblInterest) - (C.dblPayment + C.dblDiscount),
+						ELSE C.dblAmountDue + (ABS(B.dblPayment) + ABS(B.dblInterest) - ABS(B.dblDiscount))
+					END,
+	C.ysnPaid = CASE WHEN @post = 1 AND C.dblAmountDue - (ABS(B.dblPayment) + ABS(B.dblInterest) - ABS(B.dblDiscount)) = 0 THEN 1 ELSE 0 END,
+	C.dblPayment = CASE WHEN @post = 1
+						THEN C.dblPayment + (ABS(B.dblPayment) + ABS(B.dblInterest) - ABS(B.dblDiscount))
+						ELSE C.dblPayment - (ABS(B.dblPayment) + ABS(B.dblInterest) - ABS(B.dblDiscount))
+					END,
+	C.dtmDatePaid = CASE WHEN @post = 1 AND C.dblAmountDue - (ABS(B.dblPayment) + ABS(B.dblInterest) - ABS(B.dblDiscount)) = 0 
+					THEN A.dtmDatePaid
+					ELSE NULL 
+					END
 FROM
 	tblARPayment A
 INNER JOIN
@@ -40,10 +50,93 @@ INNER JOIN
 INNER JOIN
 	tblAPBill C
 		ON B.intBillId = C.intBillId
+OUTER APPLY (
+	SELECT TOP 1
+		dblFranchiseAmount
+	FROM tblAPBillDetail voucherDetail
+	WHERE voucherDetail.intBillId = C.intBillId
+) claimDetail
 WHERE
 	B.dblPayment <> 0 
 	AND A.intPaymentId IN (SELECT intId FROM @paymentIds)
 
+--UPDATE PREPAID FOR CLAIM TRANSACTION
+UPDATE D
+	SET D.dblPayment = CAST(
+						CASE WHEN @post = 1
+							THEN D.dblPayment + ABS(B.dblPayment)
+							ELSE D.dblPayment - ABS(B.dblPayment)
+						END
+						--weight claim
+						+ (
+							CASE WHEN C.intTransactionType = 11 
+								THEN (
+										CASE WHEN @post = 1 AND C.dblAmountDue = 0 --add the franchise amount on prepaid if claim amount due is 0
+											THEN ISNULL(claimPrepaid.dblFranchiseAmount,0) 
+											ELSE ISNULL(-claimPrepaid.dblFranchiseAmount,0) 
+										END
+									)
+								ELSE 0
+							END
+						) AS DECIMAL(18,2))
+	,D.dblAmountDue = CAST(
+						CASE WHEN @post = 1
+							THEN D.dblAmountDue - ABS(B.dblPayment) 
+							ELSE D.dblAmountDue + ABS(B.dblPayment) 
+						END
+						--weight claim
+						- (
+							CASE WHEN C.intTransactionType = 11 
+								THEN (
+										CASE WHEN @post = 1 AND C.dblAmountDue = 0
+											THEN ISNULL(claimPrepaid.dblFranchiseAmount,0) 
+										ELSE ISNULL(-claimPrepaid.dblFranchiseAmount,0) 
+										END
+									)
+								ELSE 0
+							END
+						) AS DECIMAL(18,2))
+	,D.ysnPaid = CASE WHEN 
+				(
+					CAST(
+						CASE WHEN @post = 1
+							THEN D.dblAmountDue - ABS(B.dblPayment) 
+							ELSE D.dblAmountDue + ABS(B.dblPayment) 
+						END
+						--weight claim
+						- (
+							CASE WHEN C.intTransactionType = 11 
+								THEN (
+										CASE WHEN @post = 1 AND C.dblAmountDue = 0
+											THEN ISNULL(claimPrepaid.dblFranchiseAmount,0) 
+										ELSE ISNULL(-claimPrepaid.dblFranchiseAmount,0) 
+										END
+									)
+								ELSE 0
+							END
+						) AS DECIMAL(18,2))	
+				) = 0
+				THEN 1 ELSE 0 END
+FROM
+	tblARPayment A
+INNER JOIN
+	tblARPaymentDetail B 
+	ON A.intPaymentId = B.intPaymentId
+INNER JOIN
+	tblAPBill C
+		ON B.intBillId = C.intBillId
+CROSS APPLY (
+	SELECT TOP 1
+		C2.intPrepayTransactionId,
+		C2.dblFranchiseAmount
+	FROM tblAPBillDetail C2
+	WHERE C2.intBillId = C.intBillId
+) claimPrepaid
+INNER JOIN tblAPBill D
+	ON claimPrepaid.intPrepayTransactionId = D.intBillId
+WHERE
+	B.dblPayment <> 0 
+	AND A.intPaymentId IN (SELECT intId FROM @paymentIds)
 --UPDATE B
 --SET
 --	B.dblAmountDue = -((C.dblTotal + ABS(B.dblInterest)) - (ABS(B.dblPayment) + ABS(B.dblDiscount))),
