fba3803ace8047ba44231bcfde3ee1cf2e0140ab
Jayson Ramos <jayson.ramos@iRely.com>
2018-10-19 17:26:49 +0800

    HD-2735

---
+++ i21Database/dbo/Tables/tblHDTicketSQLQuery.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[tblHDTicketSQLQuery]
+(
+	[intTicketSQLQueryId] [int] IDENTITY(1,1) NOT NULL,
+	[intTicketId] [int] NOT NULL,
+	[strSQLQuery] [nvarchar](max) COLLATE Latin1_General_CI_AS NULL,
+	[intCreatedByEntityId] [int] NULL,
+	[dtmCreated] [datetime] NULL,
+	[intLastModifiedByEntityId] [int] NULL,
+	[dtmLastModified] [datetime] NULL,
+	[intConcurrencyId] [int] NOT NULL DEFAULT 1,
+	CONSTRAINT [PK_tblHDTicketSQLQuery_intTicketSQLQueryId] PRIMARY KEY CLUSTERED ([intTicketSQLQueryId] ASC),
+    CONSTRAINT [FK_tblHDTicketSQLQuery_tblHDTicket_intTicketId] FOREIGN KEY ([intTicketId]) REFERENCES [dbo].[tblHDTicket] ([intTicketId]) on delete cascade
+)
