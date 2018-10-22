2b56acf6408160cd4f694979d2a1f895b3a16ac5
Arman Arenas <arman.arenas@irely.com>
2018-10-22 17:53:54 +0800

    LG-1678 Added LG PreDeployment Cleanup script

---
+++ i21PreDeployment/Scripts/Pre-Deployment/26_LG/0_DataCleanup.sql
@@ -0,0 +1,22 @@
+﻿
+/*
+* Container Types 
+* 1. Append (n) to duplicate Container Types
+* 2..
+*/
+
+IF EXISTS(SELECT * FROM sys.tables WHERE object_id = object_id('tblLGContainerType'))
+BEGIN
+
+EXEC('
+	UPDATE CT
+	SET strContainerType = CT.strContainerType + ''('' + CAST(CT_Ranked.intRank AS nvarchar(10)) + '')''
+	FROM tblLGContainerType CT
+	INNER JOIN (
+		SELECT intContainerTypeId, strContainerType, 
+		DENSE_RANK() OVER(PARTITION BY strContainerType ORDER BY intContainerTypeId) intRank 
+			FROM tblLGContainerType) CT_Ranked ON CT.intContainerTypeId = CT_Ranked.intContainerTypeId
+	WHERE CT_Ranked.intRank > 1
+')
+
+END
