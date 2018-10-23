0b636c66cb2e79d08adfbbcdd00ddc95e6193769
Smith Wilson de Jesus <smith.dejesus@irely.com>
2018-10-23 13:45:15 +0800

    RM-2102

--- i21Database/dbo/Stored Procedures/uspRKRptDPRInvDailyPositionDetail.sql
+++ i21Database/dbo/Stored Procedures/uspRKRptDPRInvDailyPositionDetail.sql
@@ -85,6 +85,68 @@ SELECT @strPurchaseSales='Purchase'
 END
 END
 
+DECLARE @FinalTable AS TABLE (
+					intRow int , 
+					intSeqId int, 
+					strSeqHeader nvarchar(100),
+					strCommodityCode nvarchar(100),
+					strType nvarchar(100),
+					dblTotal DECIMAL(24,10),
+					intCollateralId int,
+					strLocationName nvarchar(250),
+					strCustomer nvarchar(250),
+					intReceiptNo nvarchar(250),
+					intContractHeaderId int,
+					strContractNumber nvarchar(100),
+					strCustomerReference nvarchar(100),
+					strDistributionOption nvarchar(100),
+					strDPAReceiptNo nvarchar(100),
+					dblDiscDue DECIMAL(24,10),
+					[Storage Due] DECIMAL(24,10),	
+					dtmLastStorageAccrueDate datetime,
+					strScheduleId nvarchar(100),
+					strTicket nvarchar(100),
+					dtmOpenDate datetime,
+					dtmDeliveryDate datetime,
+					dtmTicketDateTime datetime,
+					dblOriginalQuantity  DECIMAL(24,10),
+					dblRemainingQuantity DECIMAL(24,10),
+					intCommodityId int,
+					strItemNo nvarchar(100),
+					strUnitMeasure nvarchar(100)
+					,intFromCommodityUnitMeasureId int
+					,intToCommodityUnitMeasureId int
+					,strTruckName  nvarchar(100)
+					,strDriverName  nvarchar(100)
+					,intCompanyLocationId int
+					,intItemId int
+					,intTicketId int,
+					strTicketNumber nvarchar(100)
+					,strShipmentNumber nvarchar(100)
+					,intInventoryShipmentId int
+					,intInventoryReceiptId int, 
+					strReceiptNumber  nvarchar(100)
+)
+
+
+INSERT INTO @FinalTable(intRow,intSeqId,strSeqHeader, strCommodityCode ,strType ,dblTotal ,strUnitMeasure, intCollateralId,strLocationName,strCustomer,
+							intReceiptNo,intContractHeaderId,strContractNumber ,dtmOpenDate,dblOriginalQuantity ,dblRemainingQuantity ,intCommodityId,
+							strCustomerReference ,strDistributionOption ,strDPAReceiptNo,dblDiscDue ,[Storage Due] ,dtmLastStorageAccrueDate ,strScheduleId ,strTicket ,
+							dtmDeliveryDate ,dtmTicketDateTime,strItemNo,strTruckName,strDriverName	,intInventoryReceiptId,strReceiptNumber,intTicketId,strShipmentNumber,
+							intInventoryShipmentId,intItemId, strTicketNumber)
 EXEC uspRKDPRInvDailyPositionDetail @intCommodityId, @intLocationId, @intVendorId, @strPurchaseSales, @strPositionIncludes, @dtmToDate
+
+SELECT 
+	intSeqId
+	,strSeqHeader
+	,strCommodityCode
+	,dblTotal 
+FROM 
+(
+	select intSeqId,strSeqHeader,strCommodityCode,sum(dblTotal) as dblTotal 
+	from @FinalTable
+	group by intSeqId,strSeqHeader,strCommodityCode
+) a 
+WHERE round(dblTotal,2) <> 0
 
 END
