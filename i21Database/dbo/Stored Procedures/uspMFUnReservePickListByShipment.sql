dbdd590d7bed0b6808637720a97aae67a2fbf32e
Premkumar S <prem.selladurai@irely.com>
2018-10-23 19:52:33 +0530

    MFG-3808

--- i21Database/dbo/Stored Procedures/uspMFUnReservePickListByShipment.sql
+++ i21Database/dbo/Stored Procedures/uspMFUnReservePickListByShipment.sql
@@ -1,13 +1,27 @@
-﻿CREATE PROCEDURE [dbo].[uspMFUnReservePickListByShipment]
-	@intInventoryShipmentId int
+﻿CREATE PROCEDURE [dbo].[uspMFUnReservePickListByShipment] @intInventoryShipmentId INT
 AS
+DECLARE @tblTransaction TABLE (intTransactionId INT)
+DECLARE @intTransactionId INT
 
-Update tblICStockReservation Set ysnPosted=0 
-Where intTransactionId IN
-(
-	Select pl.intPickListId From tblMFPickList pl Join tblSOSalesOrder so on pl.intSalesOrderId=so.intSalesOrderId 
-	Join tblICInventoryShipmentItem si on si.intOrderId=so.intSalesOrderId 
-	Join tblICInventoryShipment sh on si.intInventoryShipmentId=sh.intInventoryShipmentId
-	Where si.intInventoryShipmentId=@intInventoryShipmentId AND sh.intOrderType=2
-)
-AND intInventoryTransactionType=34
+INSERT INTO @tblTransaction
+SELECT pl.intPickListId
+FROM tblMFPickList pl
+JOIN tblSOSalesOrder so ON pl.intSalesOrderId = so.intSalesOrderId
+JOIN tblICInventoryShipmentItem si ON si.intOrderId = so.intSalesOrderId
+JOIN tblICInventoryShipment sh ON si.intInventoryShipmentId = sh.intInventoryShipmentId
+WHERE si.intInventoryShipmentId = @intInventoryShipmentId
+	AND sh.intOrderType = 2
+
+SELECT @intTransactionId = MIN(intTransactionId)
+FROM @tblTransaction
+
+WHILE @intTransactionId IS NOT NULL
+BEGIN
+	EXEC dbo.uspICPostStockReservation @intTransactionId
+		,34
+		,0
+
+	SELECT @intTransactionId = MIN(intTransactionId)
+	FROM @tblTransaction
+	WHERE intTransactionId > @intTransactionId
+END
