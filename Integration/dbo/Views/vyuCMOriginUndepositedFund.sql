431e82e8b7307c2383c15364df26ba0a2e71a5a7
Jeff Trajano <jeffrey.trajano@irely.com>
2018-10-26 14:38:16 +0800

    GL-6741 POS: EOD with cash over crediting wrong account

--- Integration/dbo/Views/vyuCMOriginUndepositedFund.sql
+++ Integration/dbo/Views/vyuCMOriginUndepositedFund.sql
@@ -11,109 +11,93 @@ BEGIN
 	')
 
 	EXEC(
-		'CREATE VIEW [dbo].[vyuCMOriginUndepositedFund]
-		AS
-		WITH C AS(
-		SELECT    id = CAST( ROW_NUMBER() OVER( ORDER BY intUndepositedFundId ) AS INT) 
-		     	,uf.intUndepositedFundId
-				,uf.intBankAccountId
-				,intGLAccountId = dbo.fnGetGLAccountIdFromOriginToi21(gl.apegl_gl_acct)
-				,strAccountDescription = (SELECT strDescription FROM tblGLAccount WHERE intAccountId = dbo.fnGetGLAccountIdFromOriginToi21(gl.apegl_gl_acct))
-				,dblAmount = gl.apegl_gl_amt * -1
-				,uf.strName
-				,intEntityCustomerId = null
-				,uf.dtmDate
-				,intCurrencyId = null
-				,dblWeightRate = 1
-		FROM	apeglmst gl INNER JOIN vyuCMOriginDepositEntry v
-					ON gl.apegl_cbk_no = v.aptrx_cbk_no
-					AND gl.apegl_vnd_no = v.aptrx_vnd_no
-					AND gl.apegl_ivc_no = v.aptrx_ivc_no
-				INNER JOIN tblCMUndepositedFund uf
-					ON uf.strSourceTransactionId = ( 
-						CAST(v.aptrx_vnd_no AS NVARCHAR(10)) 
-						+ CAST(v.aptrx_ivc_no AS NVARCHAR(18)) 
-						+ CAST(v.aptrx_cbk_no AS NVARCHAR(2)) 
-						+ CAST(v.aptrx_chk_no AS NVARCHAR(8))
-					) COLLATE Latin1_General_CI_AS
+'CREATE VIEW [dbo].[vyuCMOriginUndepositedFund]
+AS
 
-		UNION 
-		SELECT
-			id						= CAST(ROW_NUMBER() OVER (ORDER BY CMUF.intUndepositedFundId) AS INT), 
-			intUndepositedFundId	= CMUF.intUndepositedFundId, 
-			intBankAccountId		= CMUF.intBankAccountId, 
-			intGLAccountId			= ARP.intAccountId,
-			strAccountDescription   = Account.strDescription,
-			dblAmount				= CMUF.dblAmount,
-			strName					= CMUF.strName, 
-			intEntityCustomerId		= ARP.intEntityCustomerId,
-			dtmDate					= CMUF.dtmDate,
-			intCurrencyId			= ARP.intCurrencyId,
-			dblWeightRate			= F.dblWeightRate
-		FROM
-			tblCMUndepositedFund CMUF
-		INNER JOIN
-		tblARPayment ARP
-			ON CMUF.intSourceTransactionId = ARP.intPaymentId
-			AND CMUF.strSourceTransactionId = ARP.strRecordNumber
-		OUTER APPLY(
+WITH AR AS (
+	SELECT    
+		     	dbo.fnGetGLAccountIdFromOriginToi21(gl.apegl_gl_acct) intAccountId
+				,DefaultCurrency.Val intCurrencyId
+				,null intEntityCustomerId 
+				,null intSourceTransactionId
+				, CAST(v.aptrx_vnd_no AS NVARCHAR(10)) 
+				+ CAST(v.aptrx_ivc_no AS NVARCHAR(18)) 
+				+ CAST(v.aptrx_cbk_no AS NVARCHAR(2)) 
+				+ CAST(v.aptrx_chk_no AS NVARCHAR(8))  COLLATE Latin1_General_CI_AS strSourceTransactionId 
+				,'''' strType
+	FROM	apeglmst gl INNER JOIN vyuCMOriginDepositEntry v
+	ON gl.apegl_cbk_no = v.aptrx_cbk_no
+	AND gl.apegl_vnd_no = v.aptrx_vnd_no
+	AND gl.apegl_ivc_no = v.aptrx_ivc_no
+	OUTER APPLY (
+		SELECT TOP 1 intDefaultCurrencyId Val FROM tblSMCompanyPreference
+	)DefaultCurrency
+	UNION
+	SELECT intAccountId,  intEntityCustomerId,intCurrencyId, intPaymentId intSourceTransactionId, strRecordNumber strSourceTransactionId, ''Payment'' strType FROM tblARPayment UNION
+	SELECT intAccountId,  intEntityCustomerId,intCurrencyId, intInvoiceId intSourceTransactionId, strInvoiceNumber strSourceTransactionId, ''Invoice'' strType FROM tblARInvoice UNION
+	SELECT intUndepositedFundsId intAccountId,intEntityId intEntityCustomerId,intCurrencyId, intPOSEndOfDayId intSourceTransactionId, strEODNo strSourceTransactionId, ''EndOfDay'' strType FROM tblARPOSEndOfDay
+),
+
+C AS (
+	SELECT
+		id						= CAST(ROW_NUMBER() OVER (ORDER BY CMUF.intUndepositedFundId) AS INT), 
+		intUndepositedFundId	= CMUF.intUndepositedFundId, 
+		intBankAccountId		= CMUF.intBankAccountId, 
+		intGLAccountId			= ARP.intAccountId ,
+		dblAmount				= CMUF.dblAmount,
+		strName					= CMUF.strName, 
+		intEntityCustomerId		= ARP.intEntityCustomerId,
+		dtmDate					= CMUF.dtmDate,
+		intCurrencyId			= ARP.intCurrencyId,
+		dblWeightRate			= CASE WHEN strType = ''Payment''
+										THEN 
+											CASE WHEN F.dblWeightRate is null THEN 1 ELSE F.dblWeightRate END
+								  WHEN strType = ''Invoice''
+										THEN
+											case WHEN G.dblWeightRate is null THEN 1 ELSE G.dblWeightRate END
+								  ELSE
+										1
+								  END
+
+	FROM
+		tblCMUndepositedFund CMUF
+	INNER JOIN
+		AR ARP
+			ON CMUF.intSourceTransactionId = ARP.intSourceTransactionId
+			AND CMUF.strSourceTransactionId = ARP.strSourceTransactionId
+	OUTER APPLY(
 		SELECT dblWeightRate = 
 			SUM(CASE WHEN ARPD.dblCurrencyExchangeRate > 0 
 				THEN ARPD.dblCurrencyExchangeRate
 				ELSE 1 END * ARPD.dblPayment )/
 			NULLIF(SUM(ARPD.dblPayment), 0) 
-			FROM tblARPaymentDetail ARPD WHERE ARPD.intPaymentId = ARP.intPaymentId
-		)F
-		OUTER APPLY(
-			SELECT GL.strDescription FROM tblGLAccount GL WHERE GL.intAccountId = ARP.intAccountId
-		)Account
-		UNION
-		SELECT
-		    id						= CAST(ROW_NUMBER() OVER (ORDER BY CMUF.intUndepositedFundId) AS INT), 
-			intUndepositedFundId	= CMUF.intUndepositedFundId, 
-			intBankAccountId		= CMUF.intBankAccountId, 
-			intGLAccountId			= ARI.intAccountId,
-			strAccountDescription   = Account.strDescription,
-			dblAmount				= CMUF.dblAmount,
-			strName					= CMUF.strName, 
-			intEntityCustomerId		= ARI.intEntityCustomerId,
-			dtmDate					= CMUF.dtmDate,
-			intCurrencyId			= ARI.intCurrencyId,
-			 dblWeightRate			= case when G.dblWeightRate is null then 1 else G.dblWeightRate end
-		FROM
-			tblCMUndepositedFund CMUF
-		INNER JOIN
-			tblARInvoice ARI
-				ON CMUF.intSourceTransactionId = ARI.intInvoiceId
-				AND CMUF.strSourceTransactionId = ARI.strInvoiceNumber
-		OUTER APPLY(
-			SELECT dblWeightRate = 
-				SUM(CASE WHEN ARID.dblCurrencyExchangeRate > 0 
-					THEN ARID.dblCurrencyExchangeRate
-					ELSE 1 END * ARID.dblTotal )/
-				NULLIF(SUM(ARID.dblTotal), 0)
-			FROM tblARInvoiceDetail ARID WHERE ARID.intInvoiceId = ARI.intInvoiceId
-		)G
-		OUTER APPLY(
-			SELECT GL.strDescription FROM tblGLAccount GL WHERE GL.intAccountId = ARI.intAccountId
-		)Account
-		)
-		SELECT
-			id,
-			intUndepositedFundId,
-			intBankAccountId,
-			intGLAccountId,
-			strAccountDescription,
-			dblAmount,
-			strName,		
-			intEntityCustomerId,
-			dtmDate,			
-			intCurrencyId,
-			dblWeightRate
-			FROM C c
-		')
-
-
-END
-
+		FROM tblARPaymentDetail ARPD WHERE ARPD.intPaymentId = ARP.intSourceTransactionId
+	)F
+	OUTER APPLY(
+		SELECT dblWeightRate = 
+			SUM(CASE WHEN ARID.dblCurrencyExchangeRate > 0 
+				THEN ARID.dblCurrencyExchangeRate
+				ELSE 1 END * ARID.dblTotal )/
+			NULLIF(SUM(ARID.dblTotal), 0)
+		FROM tblARInvoiceDetail ARID WHERE ARID.intInvoiceId = ARP.intSourceTransactionId
+	)G
+	
+)
+SELECT
+	id,
+	intUndepositedFundId,
+	intBankAccountId,
+	intGLAccountId,
+	Account.strDescription strAccountDescription,
+	dblAmount,
+	strName,		
+	intEntityCustomerId,
+	dtmDate,			
+	intCurrencyId,
+	dblWeightRate
+	FROM C c
+OUTER APPLY(
+	SELECT GL.strDescription FROM tblGLAccount GL WHERE GL.intAccountId = c.intGLAccountId
+)Account'
+)
 GO
