44cb612ce6015ce73e899bc91ec6e44002e8098c
jessa delo-ag <jessa.delo-ag@irely.com>
2018-10-30 15:51:43 +0800

    AR-9226 - [POS] Receipt Number starts at zero (0)

--- i21Database/dbo/Stored Procedures/uspARGetReceiptNumber.sql
+++ i21Database/dbo/Stored Procedures/uspARGetReceiptNumber.sql
@@ -10,8 +10,9 @@ SET XACT_ABORT ON
 SET ANSI_WARNINGS OFF
 
 DECLARE @intStartingNumberId INT = 0
+DECLARE @intStartingNumber INT = 0
 
-SELECT TOP 1 @intStartingNumberId = intStartingNumberId 
+SELECT TOP 1 @intStartingNumberId = intStartingNumberId, @intStartingNumber = intNumber  
 	FROM tblSMStartingNumber 
 	WHERE strTransactionType = 'Sales Receipt'
 
@@ -20,7 +21,7 @@ FROM tblARPOS POS
 INNER JOIN tblSMStartingNumber SN 
 ON POS.strReceiptNumber = SN.strPrefix + CAST(SN.intNumber-1 AS NVARCHAR)
 
-IF(@intStartingNumberId <> 0 AND @strReceiptNumber IS NOT NULL)
+IF((@intStartingNumberId <> 0 AND @strReceiptNumber IS NOT NULL) OR @intStartingNumber = 1)
 	EXEC uspSMGetStartingNumber @intStartingNumberId, @strReceiptNumber OUT
 ELSE 
 	SELECT @strReceiptNumber = (SELECT strPrefix + CAST(intNumber-1 AS NVARCHAR) FROM tblSMStartingNumber WHERE strTransactionType = 'Sales Receipt')
