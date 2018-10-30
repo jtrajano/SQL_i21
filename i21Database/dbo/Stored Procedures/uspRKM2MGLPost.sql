4e8db6ed41d2d8823b8ec7303c8381d0c7564d3c
csJSarcilla <JSarcilla@COMTECH.COM>
2018-10-30 16:52:05 +0800

    RM-2178

--- i21Database/dbo/Stored Procedures/uspRKM2MGLPost.sql
+++ i21Database/dbo/Stored Procedures/uspRKM2MGLPost.sql
@@ -125,7 +125,8 @@ BEGIN
 		--Parse strTransactionId to get strContractNumber and intContractSeq
 		--Before dash(-) is the contract number after that is the contract sequence
 		SET @strContractNumber = SUBSTRING(@strTransactionId,0,CHARINDEX('-',@strTransactionId))
-		SET @intContractSeq = SUBSTRING(@strTransactionId,CHARINDEX('-',@strTransactionId) + 1,LEN(@strTransactionId) - CHARINDEX('-',@strTransactionId)) 
+		-- SET @intContractSeq = SUBSTRING(@strTransactionId,CHARINDEX('-',@strTransactionId) + 1,LEN(@strTransactionId) - CHARINDEX('-',@strTransactionId)) 
+		SET @intContractSeq = RIGHT(@strTransactionId , CHARINDEX ('-',REVERSE(@strTransactionId))-1)
 
 		--Get the used Commodity and Location in the Contract
 		SELECT 
