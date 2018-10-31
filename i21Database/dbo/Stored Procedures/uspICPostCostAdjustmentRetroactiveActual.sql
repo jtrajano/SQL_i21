6b6547821b346bcf1f82ffd81d9a9972b469a613
Feb Montefrio <feb.montefrio@irely.com>
2018-11-01 05:39:26 +0800

    [B] IC-6217

--- i21Database/dbo/Stored Procedures/uspICPostCostAdjustmentRetroactiveActual.sql
+++ i21Database/dbo/Stored Procedures/uspICPostCostAdjustmentRetroactiveActual.sql
@@ -632,11 +632,17 @@ END
 BEGIN 
 	-- Calculate the value to book. 
 	-- Formula: (Remaining Qty x New Cost) - (Remaining Qty x Original Cost)
-	SELECT	@CurrentCostAdjustment = 
-				(cb.dblStockIn - cb.dblStockOut) * cb.dblCost
-				- (cb.dblStockIn - cb.dblStockOut) * @CostBucketOriginalCost
-	FROM	tblICInventoryActualCost cb
-	WHERE	cb.intInventoryActualCostId = @CostBucketId
+	-- SELECT	@CurrentCostAdjustment = 
+	-- 			(cb.dblStockIn - cb.dblStockOut) * cb.dblCost
+	-- 			- (cb.dblStockIn - cb.dblStockOut) * @CostBucketOriginalCost
+	-- FROM	tblICInventoryActualCost cb
+	-- WHERE	cb.intInventoryActualCostId = @CostBucketId
+
+	SET 	@CurrentCostAdjustment = NULL 
+	SELECT	@CurrentCostAdjustment = SUM(ROUND(ISNULL(dblValue, 0), 2)) 
+	FROM	tblICInventoryActualCostAdjustmentLog	
+	WHERE	intInventoryTransactionId = @DummyInventoryTransactionId
+			AND intInventoryCostAdjustmentTypeId <> @COST_ADJ_TYPE_Original_Cost	
 
 	SET @strNewCost = CONVERT(NVARCHAR, CAST(ISNULL(@CostAdjustment, 0) AS MONEY), 1)
 
