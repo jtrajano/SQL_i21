2a7e638f363b3ff14e5ab6cfff4f1629afeaacb5
Kelvin Quinto <kelvin.quinto@irely.com>
2018-11-06 00:25:41 +0800

    AR-9254

--- i21Database/dbo/Stored Procedures/uspARCustomerStatementPaymentActivityReport.sql
+++ i21Database/dbo/Stored Procedures/uspARCustomerStatementPaymentActivityReport.sql
@@ -420,7 +420,7 @@ IF @ysnIncludeBudgetLocal = 1
 				  , strPaymentInfo				= NULL
 				  , dtmDatePaid					= NULL
 				  , dblPayment					= dblAmountPaid
-				  , dblBalance					= dblBudgetAmount - dblAmountPaid
+				  , dblBalance					= 0.00
 				  , strSalespersonName			= NULL				  
 				  , strTicketNumbers			= NULL
 				  , strLocationName				= NULL
@@ -478,7 +478,7 @@ IF @ysnPrintOnlyPastDueLocal = 1
 
 IF @ysnPrintZeroBalanceLocal = 0
     BEGIN
-        DELETE FROM @temp_statement_table WHERE (((ABS(dblBalance) * 10000) - CONVERT(FLOAT, (ABS(dblBalance) * 10000))) <> 0) OR ISNULL(dblBalance, 0) = 0
+        DELETE FROM @temp_statement_table WHERE ((((ABS(dblBalance) * 10000) - CONVERT(FLOAT, (ABS(dblBalance) * 10000))) <> 0) OR ISNULL(dblBalance, 0) = 0) AND strTransactionType <> 'Customer Budget'
 		DELETE FROM tblARCustomerAgingStagingTable WHERE ((((ABS(dblTotalAR) * 10000) - CONVERT(FLOAT, (ABS(dblTotalAR) * 10000))) <> 0) OR ISNULL(dblTotalAR, 0) = 0) AND intEntityUserId = @intEntityUserIdLocal AND strAgingType = 'Summary'
     END
 	
