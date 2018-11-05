f9b392674602704e5dfd2417c29878208259d859
Leslie L Arat <leslie.arat@irely.com>
2018-11-05 23:58:56 +0800

    GRN-1303

--- i21Database/dbo/Stored Procedures/uspGRCheckStorageTicketStatus.sql
+++ i21Database/dbo/Stored Procedures/uspGRCheckStorageTicketStatus.sql
@@ -1,13 +1,13 @@
 CREATE PROCEDURE [dbo].[uspGRCheckStorageTicketStatus]
 (
-	@intTransactionId INT, --deliverysheet/scaleticket id
+	@intTransactionId INT, --deliverysheet / scaleticket id
+	@strTransactionCode NVARCHAR(20), --DS (delivery sheet) / SC (scale)
 	@intUserId INT
 )
 AS
 BEGIN
 	DECLARE @ErrMsg AS NVARCHAR(MAX)
 	DECLARE @transCount INT
-	DECLARE @intCustomerStorageId INT
 
 	IF OBJECT_ID (N'tempdb.dbo.#tmpCustomerStorage') IS NOT NULL
 		DROP TABLE #tmpCustomerStorage
@@ -15,12 +15,23 @@ BEGIN
 		[intCustomerStorageId] INT PRIMARY KEY,
 		UNIQUE ([intCustomerStorageId])
 	);
-	INSERT INTO #tmpCustomerStorage
-	SELECT 
-		intCustomerStorageId
-	FROM tblGRCustomerStorage
-	WHERE intTicketId = @intTransactionId
-		OR intDeliverySheetId = @intTransactionId
+
+	IF @strTransactionCode = 'SC'
+	BEGIN
+		INSERT INTO #tmpCustomerStorage
+		SELECT 
+			intCustomerStorageId
+		FROM tblGRCustomerStorage
+		WHERE intTicketId = @intTransactionId
+	END
+	ELSE IF @strTransactionCode = 'DS'
+	BEGIN
+		INSERT INTO #tmpCustomerStorage
+		SELECT 
+			intCustomerStorageId
+		FROM tblGRCustomerStorage
+		WHERE intDeliverySheetId = @intTransactionId
+	END
 
 	--Check if storage has been settled or transferred
 	IF EXISTS(SELECT TOP 1 1 
@@ -35,7 +46,7 @@ BEGIN
 			ON CS.intCustomerStorageId = TSS.intSourceCustomerStorageId
 	)
 	BEGIN
-		SET @ErrMsg = 'Unable to unpost this Delivery Sheet. <br/>The Open Balance of one or more Storage Tickets no longer matches its Original Balance.'
+		SET @ErrMsg = 'Unable to unpost this transaction. <br/>The Open Balance of one or more Storage Tickets no longer matches its Original Balance.'
 		
 		RAISERROR(@ErrMsg,16,1)
 		RETURN;
