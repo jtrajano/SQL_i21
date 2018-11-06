2741630ea51517b1aae443e87e5c14f90411a9b5
Crisman Lazaga <cris.lazaga@irely.com>
2018-11-06 10:38:52 +0800

    MFT-1329

---
+++ i21Database/dbo/User Defined Types/TFCommonDetailTax.sql
@@ -0,0 +1,8 @@
+﻿CREATE TYPE [dbo].[TFCommonDetailTax] AS TABLE
+(
+	intTransactionDetailId INT, 
+	intTaxCodeId INT NULL,
+	strCriteria NVARCHAR(100) NULL,
+	dblTax NUMERIC(18, 8) NULL,
+	UNIQUE NONCLUSTERED ([intTransactionDetailId] ASC, [intTaxCodeId] ASC)
+)
