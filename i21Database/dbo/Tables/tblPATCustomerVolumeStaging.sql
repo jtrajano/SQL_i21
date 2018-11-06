d388d274d283db5f1bff2ce68512becb2ac8024d
Jb Mago <jb.mago@irely.com>
2018-11-07 05:54:03 +0800

    [B] PAT-986

---
+++ i21Database/dbo/Tables/tblPATCustomerVolumeStaging.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[tblPATCustomerVolumeStaging]
+(
+	[intCustomerVolumeStagingId] INT NOT NULL IDENTITY, 
+    [intCustomerPatronId] INT NULL, 
+    [intPatronageCategoryId] INT NULL, 
+    [intFiscalYear] INT NULL, 
+    [dblVolume] NUMERIC(18, 6) NULL, 
+    [intConcurrencyId] INT NULL DEFAULT 0, 
+    CONSTRAINT [PK_tblPATCustomerVolumeStaging] PRIMARY KEY ([intCustomerVolumeStagingId])
+)
