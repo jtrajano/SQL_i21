5f6d9ac79b2d11e929af02c699d6a84c1a50bb22
COMTECH\HZaragoza <henry.zaragoza@irely.com>
2018-11-08 00:30:07 +0800

    ST-1037 Handheld Scanner > Import Receipts/Counts Cleanup

--- i21Database/dbo/Views/vyuSTGetHandheldScannerImportReceipt.sql
+++ i21Database/dbo/Views/vyuSTGetHandheldScannerImportReceipt.sql
@@ -1,6 +1,5 @@
 ﻿CREATE VIEW [dbo].[vyuSTGetHandheldScannerImportReceipt]
-	AS
-
+AS
 SELECT IR.intHandheldScannerImportReceiptId
 	, IR.intHandheldScannerId
 	, HS.intStoreId
@@ -14,10 +13,35 @@ SELECT IR.intHandheldScannerImportReceiptId
 	, IR.strUPCNo
 	, IR.intItemId
 	, Item.strItemNo
-	, strDescription = CASE WHEN ISNULL(IR.intItemId, '') != '' THEN Item.strDescription ELSE 'UPC Not Found!' END
+	, strDescription = CASE 
+							WHEN ISNULL(IR.intItemId, '') != '' 
+								THEN Item.strDescription 
+							ELSE 'UPC Not Found!' 
+					END
 	, ItemLoc.intItemLocationId
-	, ItemUOM.intItemUOMId
-	, strUnitMeasure = CASE WHEN ISNULL(ItemUOM.intItemUOMId, '') != '' THEN UOM.strUnitMeasure ELSE 'UPC Not Found!' END
+	--, ItemUOM.intItemUOMId
+	, intItemUOMId = CASE
+						WHEN ISNULL(ItemUOM.intItemUOMId, '') != ''
+							THEN ItemUOM.intItemUOMId
+						WHEN ISNULL(IR.intItemId, '') != '' AND ISNULL(ItemUOM.intItemUOMId, '') = ''
+							THEN (SELECT intItemUOMId FROM tblICItemUOM WHERE intItemId = IR.intItemId AND ysnStockUnit = CAST(1 AS BIT))
+						ELSE NULL
+					END
+	, strUnitMeasure = CASE 
+							WHEN ISNULL(ItemUOM.intItemUOMId, '') != '' 
+								THEN UOM.strUnitMeasure 
+							WHEN ISNULL(IR.intItemId, '') != '' AND ISNULL(ItemUOM.intItemUOMId, '') = '' 
+								THEN (
+										SELECT strUnitMeasure 
+										FROM tblICUnitMeasure 
+										WHERE intUnitMeasureId = (
+																	SELECT intUnitMeasureId
+																	FROM tblICItemUOM
+																	WHERE intItemId = IR.intItemId 
+																	AND ysnStockUnit = CAST(1 AS BIT)
+																 )
+									 )
+							ELSE 'UPC Not Found!' END
 	, IR.dtmReceiptDate
 	, IR.dblReceivedQty
 	, IR.dblCaseCost
@@ -25,11 +49,21 @@ SELECT IR.intHandheldScannerImportReceiptId
 	, IR.strCostChange
 	, IR.strRetailChange
 FROM tblSTHandheldScannerImportReceipt IR
-LEFT JOIN tblSTHandheldScanner HS ON HS.intHandheldScannerId = IR.intHandheldScannerId
-LEFT JOIN tblSTStore Store ON Store.intStoreId = HS.intStoreId
-LEFT JOIN tblAPVendor Vendor ON Vendor.intEntityId = IR.intVendorId
-LEFT JOIN tblEMEntity Entity ON Entity.intEntityId = Vendor.intEntityId
-LEFT JOIN tblICItem Item ON Item.intItemId = IR.intItemId
-LEFT JOIN tblICItemLocation ItemLoc ON ItemLoc.intItemId = IR.intItemId AND ItemLoc.intLocationId = Store.intCompanyLocationId
-LEFT JOIN tblICItemUOM ItemUOM ON ItemUOM.intItemId = IR.intItemId AND ItemUOM.strLongUPCCode = strUPCNo
-LEFT JOIN tblICUnitMeasure UOM ON UOM.intUnitMeasureId = ItemUOM.intUnitMeasureId
+LEFT JOIN tblSTHandheldScanner HS 
+	ON HS.intHandheldScannerId = IR.intHandheldScannerId
+LEFT JOIN tblSTStore Store 
+	ON Store.intStoreId = HS.intStoreId
+LEFT JOIN tblAPVendor Vendor 
+	ON Vendor.intEntityId = IR.intVendorId
+LEFT JOIN tblEMEntity Entity 
+	ON Entity.intEntityId = Vendor.intEntityId
+LEFT JOIN tblICItem Item 
+	ON Item.intItemId = IR.intItemId
+LEFT JOIN tblICItemLocation ItemLoc 
+	ON ItemLoc.intItemId = IR.intItemId 
+	AND ItemLoc.intLocationId = Store.intCompanyLocationId
+LEFT JOIN tblICItemUOM ItemUOM 
+	ON ItemUOM.intItemId = IR.intItemId 
+	AND ItemUOM.strLongUPCCode = IR.strUPCNo
+LEFT JOIN tblICUnitMeasure UOM 
+	ON UOM.intUnitMeasureId = ItemUOM.intUnitMeasureId
