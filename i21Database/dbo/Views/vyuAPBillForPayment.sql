d47c1b13ed01036b7dbb98fb73b244c0ed84f021
Lex Peregrino <lex.peregrino@irely.com>
2018-11-08 01:16:09 +0800

    AP-6433 1.0

--- i21Database/dbo/Views/vyuAPBillForPayment.sql
+++ i21Database/dbo/Views/vyuAPBillForPayment.sql
@@ -32,7 +32,7 @@ SELECT
 	,vendor.ysnOneBillPerPayment
 	,vendor.ysnWithholding
 	,payMethod.strPaymentMethod
-	,vendor.strVendorId
+	,ISNULL(vendor.strVendorId, entity.strEntityNo) as strVendorId
 	,commodity.strCommodityCode
 	,term.strTerm
 	,entity.strName
