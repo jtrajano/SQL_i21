449683af88bd07b96f9317ba3806d58c7e2f64fa
jessa delo-ag <jessa.delo-ag@irely.com>
2018-11-08 18:29:57 +0800

    AR-9280, AR-9272

--- i21Database/dbo/Stored Procedures/uspARProcessPOSToInvoice.sql
+++ i21Database/dbo/Stored Procedures/uspARProcessPOSToInvoice.sql
@@ -253,38 +253,38 @@ BEGIN
 	SELECT @dblTotalAmountPaid = SUM(dblAmount) FROM #POSPAYMENTS
 
 	--UPDATE OVERPAYMENTS
-	--IF ISNULL(@dblTotalAmountPaid, 0) > ISNULL(@dblInvoiceTotal, 0)
-	--	BEGIN
-	--		SET @dblCounter = 0
+	IF ISNULL(@dblTotalAmountPaid, 0) > ISNULL(@dblInvoiceTotal, 0)
+		BEGIN
+			SET @dblCounter = 0
 
-	--		WHILE EXISTS (SELECT TOP 1 NULL FROM #POSPAYMENTS WHERE ysnComputed = 0)
-	--			BEGIN
-	--				DECLARE @intPOSPaymentId	INT
-	--					  , @dblAmount			NUMERIC(18, 6)	= 0
-	--					  , @dblDiscrepancy		NUMERIC(18, 6)	= 0
+			WHILE EXISTS (SELECT TOP 1 NULL FROM #POSPAYMENTS WHERE ysnComputed = 0)
+				BEGIN
+					DECLARE @intPOSPaymentId	INT
+						  , @dblAmount			NUMERIC(18, 6)	= 0
+						  , @dblDiscrepancy		NUMERIC(18, 6)	= 0
 
-	--				SELECT TOP 1 @intPOSPaymentId = intPOSPaymentId
-	--						   , @dblAmount		  = dblAmount
-	--				FROM #POSPAYMENTS
-	--				WHERE ysnComputed = 0
+					SELECT TOP 1 @intPOSPaymentId = intPOSPaymentId
+							   , @dblAmount		  = dblAmount
+					FROM #POSPAYMENTS
+					WHERE ysnComputed = 0
 
-	--				IF @dblCounter + @dblAmount <= @dblInvoiceTotal
-	--					BEGIN
-	--						SET @dblCounter = @dblCounter + @dblAmount
-	--						UPDATE #POSPAYMENTS SET ysnComputed = 1 WHERE intPOSPaymentId = @intPOSPaymentId
-	--					END
-	--				ELSE 
-	--					BEGIN
-	--						SET @dblDiscrepancy = (@dblCounter + @dblAmount) - @dblInvoiceTotal
+					IF @dblCounter + @dblAmount <= @dblInvoiceTotal
+						BEGIN
+							SET @dblCounter = @dblCounter + @dblAmount
+							UPDATE #POSPAYMENTS SET ysnComputed = 1 WHERE intPOSPaymentId = @intPOSPaymentId
+						END
+					ELSE 
+						BEGIN
+							SET @dblDiscrepancy = (@dblCounter + @dblAmount) - @dblInvoiceTotal
 
-	--						UPDATE #POSPAYMENTS SET ysnComputed = 1, dblAmount = (@dblAmount - @dblDiscrepancy) WHERE intPOSPaymentId = @intPOSPaymentId
-	--						UPDATE tblARPOSPayment SET dblAmount = (@dblAmount - @dblDiscrepancy) WHERE intPOSPaymentId = @intPOSPaymentId
+							UPDATE #POSPAYMENTS SET ysnComputed = 1, dblAmount = (@dblAmount - @dblDiscrepancy) WHERE intPOSPaymentId = @intPOSPaymentId
+							UPDATE tblARPOSPayment SET dblAmount = (@dblAmount - @dblDiscrepancy) WHERE intPOSPaymentId = @intPOSPaymentId
 
-	--						DELETE FROM tblARPOSPayment WHERE intPOSPaymentId IN (SELECT intPOSPaymentId FROM #POSPAYMENTS WHERE ysnComputed = 0)
-	--						DELETE FROM #POSPAYMENTS WHERE ysnComputed = 0
-	--					END
-	--			END 
-	--	END
+							DELETE FROM tblARPOSPayment WHERE intPOSPaymentId IN (SELECT intPOSPaymentId FROM #POSPAYMENTS WHERE ysnComputed = 0)
+							DELETE FROM #POSPAYMENTS WHERE ysnComputed = 0
+						END
+				END 
+		END
 	
 	IF EXISTS (SELECT TOP 1 NULL FROM #POSPAYMENTS)
 		BEGIN
