0abff39d239c167f5606bb8fcd6060b42e73a8b3
Jayson Ramos <jayson.ramos@iRely.com>
2018-11-13 14:23:19 +0800

    HD-2903

---
+++ i21Database/dbo/Views/vyuHDTicketRACI.sql
@@ -0,0 +1,19 @@
+﻿CREATE VIEW [dbo].[vyuHDTicketRACI]
+	AS
+	select
+		a.intTicketRACIId
+		,a.intTicketId
+		,a.intResponsibleId
+		,a.intCompanyEntityId
+		,a.intThirdPartyEntityId
+		,a.intEntityContactId
+		,a.intConcurrencyId
+		,b.strResponsible
+		,strCompany = c.strName
+		,strThirdParty = d.strName
+		,strContactName = e.strName
+	from tblHDTicketRACI a
+	left join vyuHDTicketRACIResponsible b on b.intResponsibleId = a.intResponsibleId
+	left join tblEMEntity c on c.intEntityId = a.intCompanyEntityId
+	left join tblEMEntity d on d.intEntityId = a.intThirdPartyEntityId
+	left join tblEMEntity e on e.intEntityId = a.intEntityContactId
---
+++ i21Database/dbo/Views/vyuHDTicketRACIResponsible.sql
@@ -0,0 +1,9 @@
+﻿CREATE VIEW [dbo].[vyuHDTicketRACIResponsible]
+	AS
+	select intResponsibleId = 1, strResponsible = 'Responsible'
+	union all
+	select intResponsibleId = 2, strResponsible = 'Accountable'
+	union all
+	select intResponsibleId = 3, strResponsible = 'Consulted'
+	union all
+	select intResponsibleId = 4, strResponsible = 'Informed'
