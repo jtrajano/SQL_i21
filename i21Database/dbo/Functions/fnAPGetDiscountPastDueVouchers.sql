18682ec3709eccf2dff14347fbac85230b1605f9
Erick <erick.delacruz@irely.com>
2018-11-15 13:43:39 +0800

    AP-6478

--- i21Database/dbo/Functions/fnAPGetDiscountPastDueVouchers.sql
+++ i21Database/dbo/Functions/fnAPGetDiscountPastDueVouchers.sql
@@ -5,7 +5,8 @@
 	@datePaid DATETIME,
 	@vendorId INT = NULL,
 	@payToAddressId INT = NULL,
-	@showDeferred BIT = 0
+	@showDeferred BIT = 0,
+	@paymentId INT = NULL
 )
 RETURNS NVARCHAR(MAX)
 AS
@@ -29,19 +30,46 @@ BEGIN
 	END
 	ELSE
 	BEGIN
-		SELECT
-			@vouchers = COALESCE(@vouchers + CHAR(10) + CHAR(13), '') +  voucher.strBillId
-		FROM vyuAPBillForPayment forPay
-		INNER JOIN tblAPBill voucher ON voucher.intBillId = forPay.intBillId
-		WHERE 
-			voucher.intEntityVendorId = @vendorId
-		AND ISNULL(voucher.intPayToAddressId,0) = CASE WHEN @payToAddressId IS NULL THEN ISNULL(voucher.intPayToAddressId,0) ELSE @payToAddressId END
-		AND forPay.intCurrencyId = @currencyId
-		AND 1 = (CASE WHEN @showDeferred = 1 THEN 1
-					ELSE (CASE WHEN forPay.intTransactionType = 14 THEN 0 ELSE 1 END) END)
-		AND voucher.ysnDiscountOverride = 1
-		AND voucher.dblDiscount != 0
-		AND dbo.fnIsDiscountPastDue(voucher.intTermsId, @datePaid, voucher.dtmDate) = 1
+		IF NULLIF(@paymentId,0) IS NULL
+		BEGIN
+			SELECT
+				@vouchers = COALESCE(@vouchers + CHAR(10) + CHAR(13), '') +  voucher.strBillId
+			FROM vyuAPBillForPayment forPay
+			INNER JOIN tblAPBill voucher ON voucher.intBillId = forPay.intBillId
+			WHERE 
+				voucher.intEntityVendorId = @vendorId
+			AND ISNULL(voucher.intPayToAddressId,0) = CASE WHEN @payToAddressId IS NULL THEN ISNULL(voucher.intPayToAddressId,0) ELSE @payToAddressId END
+			AND forPay.intCurrencyId = @currencyId
+			AND 1 = (CASE WHEN @showDeferred = 1 THEN 1
+						ELSE (CASE WHEN forPay.intTransactionType = 14 THEN 0 ELSE 1 END) END)
+			AND voucher.ysnDiscountOverride = 1
+			AND voucher.dblDiscount != 0
+			AND dbo.fnIsDiscountPastDue(voucher.intTermsId, @datePaid, voucher.dtmDate) = 1
+		END
+		ELSE
+		BEGIN
+			SELECT
+				@vouchers = COALESCE(@vouchers + CHAR(10) + CHAR(13), '') +  voucher.strBillId
+			FROM vyuAPBillForPayment forPay
+			INNER JOIN tblAPBill voucher ON voucher.intBillId = forPay.intBillId
+			WHERE 
+				voucher.intEntityVendorId = @vendorId
+			AND ISNULL(voucher.intPayToAddressId,0) = CASE WHEN @payToAddressId IS NULL THEN ISNULL(voucher.intPayToAddressId,0) ELSE @payToAddressId END
+			AND forPay.intCurrencyId = @currencyId
+			AND 1 = (CASE WHEN @showDeferred = 1 THEN 1
+						ELSE (CASE WHEN forPay.intTransactionType = 14 THEN 0 ELSE 1 END) END)
+			AND voucher.ysnDiscountOverride = 1
+			AND voucher.dblDiscount != 0
+			AND dbo.fnIsDiscountPastDue(voucher.intTermsId, @datePaid, voucher.dtmDate) = 1
+			AND voucher.intBillId
+			IN
+			(
+				SELECT 
+					payDetail.intBillId
+				FROM tblAPPaymentDetail payDetail
+				WHERE payDetail.intPaymentId = @paymentId
+			)
+		END
 	END
 	RETURN @vouchers;
 END
