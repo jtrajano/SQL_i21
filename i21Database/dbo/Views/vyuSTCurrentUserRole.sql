a92029e1d38b886a455c349b802d008c5532b9e0
COMTECH\HZaragoza <henry.zaragoza@irely.com>
2018-11-17 01:37:42 +0800

    ST-1061 [Checkout] Admin does not display all the store location in checkout

---
+++ i21Database/dbo/Views/vyuSTCurrentUserRole.sql
@@ -0,0 +1,18 @@
+﻿CREATE VIEW [dbo].[vyuSTCurrentUserRole]
+AS
+SELECT 
+	CLRP.intUserSecurityCompanyLocationRolePermissionId
+	, CL.strLocationName
+	, CL.intCompanyLocationId
+	, CLRP.intEntityId
+	, UR.strRoleType
+	, UR.ysnAdmin
+	, US.intCompanyLocationId AS intDefaultCompanyLocationId
+FROM tblSMUserSecurityCompanyLocationRolePermission CLRP
+INNER JOIN tblSMUserSecurity US
+	ON CLRP.intEntityId = US.intEntityId
+INNER JOIN tblSMUserRole UR
+	ON CLRP.intUserRoleId = UR.intUserRoleID
+INNER JOIN tblSMCompanyLocation CL
+	ON CLRP.intCompanyLocationId = CL.intCompanyLocationId
+	AND US.intCompanyLocationId = CL.intCompanyLocationId
