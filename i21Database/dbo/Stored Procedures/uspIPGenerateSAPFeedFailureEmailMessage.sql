efb4712bed59eb652d3d0648221213b021b13713
Vinoth Kumar T <vinoth.t@irely.com>
2018-11-19 19:39:36 +0530

    IN-304

--- i21Database/dbo/Stored Procedures/uspIPGenerateSAPFeedFailureEmailMessage.sql
+++ i21Database/dbo/Stored Procedures/uspIPGenerateSAPFeedFailureEmailMessage.sql
@@ -80,6 +80,7 @@ Begin
 
 	Update tblCTContractFeed Set ysnMailSent=1
 	Where strFeedStatus='Ack Rcvd' AND ISNULL(strMessage,'') NOT IN ('', 'Success') AND GETDATE() > DATEADD(MI,@intDuration,dtmFeedCreated)
+	AND ISNULL(ysnMailSent,0)=0
 End
 
 If @strMessageType='PO1'
@@ -108,6 +109,7 @@ Begin
 
 	Update tblCTContractFeed Set ysnMailSent=1
 	Where ISNULL(strMessage,'') NOT IN ('', 'Success') AND GETDATE() > DATEADD(MI,@intDuration,dtmFeedCreated)
+	AND ISNULL(ysnMailSent,0)=0
 End
 
 If @strMessageType='Shipment'
@@ -134,6 +136,7 @@ Begin
 
 	Update tblLGLoadStg Set ysnMailSent=1
 	Where strFeedStatus='Ack Rcvd' AND ISNULL(strMessage,'') NOT IN ('', 'Success') AND GETDATE() > DATEADD(MI,@intDuration,dtmFeedCreated)
+	AND ISNULL(ysnMailSent,0)=0
 End
 
 If @strMessageType='Receipt'
@@ -158,6 +161,7 @@ Begin
 
 	Update tblIPReceiptError Set ysnMailSent=1
 	Where strPartnerNo='i212SAP' AND GETDATE() > DATEADD(MI,@intDuration,dtmTransactionDate) AND ISNULL(strErrorMessage,'') <>'Success'
+	AND ISNULL(ysnMailSent,0)=0
 End
 
 If @strMessageType='MBN Receipt'
@@ -178,6 +182,7 @@ Begin
 
 	Update tblIPReceiptError Set ysnMailSent=1
 	Where strPartnerNo='0012XI01' AND ISNULL(strErrorMessage,'') <>'Success'
+	AND ISNULL(ysnMailSent,0)=0
 End
 
 If @strMessageType='GL'
@@ -206,6 +211,7 @@ Begin
 
 	Update tblRKStgMatchPnS Set ysnMailSent=1
 	Where strStatus='Ack Rcvd' AND ISNULL(strMessage,'') NOT IN ('', 'Success') AND GETDATE() > DATEADD(MI,@intDuration,dtmPostingDate)
+	AND ISNULL(ysnMailSent,0)=0
 
 	Select @strDetail=@strDetail + 
 	'<tr>
@@ -222,6 +228,7 @@ Begin
 
 	Update tblRKStgOptionMatchPnS Set ysnMailSent=1
 	Where strStatus='Ack Rcvd' AND ISNULL(strMessage,'') NOT IN ('', 'Success') AND GETDATE() > DATEADD(MI,@intDuration,dtmPostingDate)
+	AND ISNULL(ysnMailSent,0)=0
 End
 
 Set @strHtml=REPLACE(@strHtml,'@header',@strHeader)
