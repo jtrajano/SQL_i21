7b352bdd82d0067608e578dff772043777523e17
Satya S Sahoo <satya.sahoo@irely.com>
2018-11-20 21:58:21 +0530

    CT-2708 - Contract Inquiry - Failed to load data - 17.1B

--- i21Database/dbo/Views/vyuCTContractCostEnquiryHedge.sql
+++ i21Database/dbo/Views/vyuCTContractCostEnquiryHedge.sql
@@ -1,11 +1,27 @@
 ﻿CREATE VIEW [dbo].[vyuCTContractCostEnquiryHedge]
 	
 AS
-	SELECT	*,
+	SELECT	o.intAssignFuturesToContractSummaryId,
+			o.intContractDetailId,
+			o.strInternalTradeNo,
+			o.dtmTransactionDate,
+			o.strFutMarketName,
+			o.strFutureMonth,
+			o.dblNoOfLots,
+			o.dblLots,
+			o.dblPrice,
+			o.dblLatestPrice,
+			o.dblGross,
+			o.dblCommission,
+			o.dblNet,
+			o.dblDefaultCurrencyFactor,
+			o.intCent,
 			dblGross / intCent AS dblGrossImpact,
 			dblNet / intCent AS dblNetImpact,
 			dblGross * dblDefaultCurrencyFactor	AS  dblGrossImpactInDefCurrency,
-			dblNet * dblDefaultCurrencyFactor		AS  dblNetImpactInDefCurrency
+			dblNet * dblDefaultCurrencyFactor		AS  dblNetImpactInDefCurrency,
+			CASE WHEN o.dblLots < 0 THEN o.dblLots ELSE 0 END dblNegLots,
+			CASE WHEN o.dblLots > 0 THEN o.dblLots ELSE 0 END dblPosLots
 	FROM
 	(
 		SELECT	intAssignFuturesToContractSummaryId,
@@ -33,17 +49,27 @@ AS
 						MO.strFutureMonth,
 						CAST(ISNULL(SY.intHedgedLots,0) AS NUMERIC(18, 6))+ISNULL(SY.dblAssignedLots,0) 					AS	dblLots,
 						FT.dblPrice,
-						dbo.fnCTGetLastSettlementPrice(FT.intFutureMarketId,FT.intFutureMonthId)							AS	dblLatestPrice,
+						--dbo.fnCTGetLastSettlementPrice(FT.intFutureMarketId,FT.intFutureMonthId)							AS	dblLatestPrice,
+						dblLastSettle AS	dblLatestPrice,
 						dbo.fnCTGetBrokerageCommission(FT.intBrokerageAccountId,FT.intFutureMarketId,FT.dtmTransactionDate) AS	dblCommission,
 						MA.dblContractSize,
 						CASE WHEN FT.strBuySell = 'Sell' THEN -1 ELSE 1 END intProffitLoss,
 						dbo.fnCTCalculateAmountBetweenCurrency(FT.intCurrencyId,null,1,1)									AS	dblDefaultCurrencyFactor,
 						ISNULL(CY.intCent,1) AS intCent
-				FROM	tblRKAssignFuturesToContractSummary SY
-				JOIN	tblRKFutOptTransaction				FT	ON	FT.intFutOptTransactionId	=	SY.intFutOptTransactionId
-				JOIN	tblRKFutureMarket					MA	ON	MA.intFutureMarketId		=	FT.intFutureMarketId
-				JOIN	tblRKFuturesMonth					MO	ON	MO.intFutureMonthId			=	FT.intFutureMonthId
-				JOIN	tblSMCurrency						CY	ON	CY.intCurrencyID			=	FT.intCurrencyId		LEFT
-				JOIN	tblCTContractDetail					CD	ON	CD.intContractHeaderId		=	SY.intContractHeaderId
+				FROM	dbo.tblRKAssignFuturesToContractSummary SY
+				JOIN	dbo.tblCTContractDetail					CD	ON	CD.intContractHeaderId		=	SY.intContractHeaderId
+				JOIN	dbo.tblRKFutOptTransaction				FT	ON	FT.intFutOptTransactionId	=	SY.intFutOptTransactionId
+				JOIN	dbo.tblRKFutureMarket					MA	ON	MA.intFutureMarketId		=	FT.intFutureMarketId
+				JOIN	dbo.tblRKFuturesMonth					MO	ON	MO.intFutureMonthId			=	FT.intFutureMonthId
+				JOIN	dbo.tblSMCurrency						CY	ON	CY.intCurrencyID			=	FT.intCurrencyId		
+		   LEFT JOIN 
+				(
+					SELECT		SP.intFutureMarketId,MP.intFutureMonthId,MP.dblLastSettle 
+					FROM		tblRKFutSettlementPriceMarketMap MP
+					JOIN		(
+									SELECT		ROW_NUMBER() OVER (PARTITION BY intFutureMarketId ORDER BY dtmPriceDate DESC) intRowNum,  intFutureMarketId,intFutureSettlementPriceId
+									FROM		tblRKFuturesSettlementPrice
+					) SP ON SP.intRowNum = 1 AND MP.intFutureSettlementPriceId = SP.intFutureSettlementPriceId
+				)	CM ON CM.intFutureMarketId = FT.intFutureMarketId AND CM.intFutureMonthId = FT.intFutureMonthId
 		)t
 	)o
