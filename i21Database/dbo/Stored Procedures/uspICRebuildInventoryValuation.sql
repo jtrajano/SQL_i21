6104079b45544791eb8e92e8d7c1568722f67df5
Feb Montefrio <feb.montefrio@irely.com>
2018-11-25 03:10:54 +0800

    [B] IC-6397

--- i21Database/dbo/Stored Procedures/uspICRebuildInventoryValuation.sql
+++ i21Database/dbo/Stored Procedures/uspICRebuildInventoryValuation.sql
@@ -2628,7 +2628,7 @@ BEGIN
 						[intItemId]					= t.intItemId
 						,[intItemLocationId]		= t.intItemLocationId
 						,[intItemUOMId]				= t.intItemUOMId
-						,[dtmDate]					= i.dtmDate
+						,[dtmDate]					= t.dtmDate--i.dtmDate
 						,[dblQty]					= t.dblQty
 						,[dblUOMQty]				= t.dblUOMQty
 						,[dblCost]					= t.dblCost
