950c22d28732816091737bddd4e0b9d1dd9278c6
COMTECH\EVersoza <emerson.versoza@irely.com>
2018-11-27 04:39:49 +0800

    SC-2362

--- i21Database/dbo/Stored Procedures/uspSCProcessToInventoryShipment.sql
+++ i21Database/dbo/Stored Procedures/uspSCProcessToInventoryShipment.sql
@@ -55,7 +55,8 @@ DECLARE @intInventoryShipmentItemId AS INT
 		,@strWhereFinalizedWeight NVARCHAR(20)
 		,@strWhereFinalizedGrade NVARCHAR(20)
 		,@ysnDPStorage AS BIT
-		,@intContractDetailId INT;
+		,@intContractDetailId INT
+		,@ysnPriceFixation BIT = 0;
 
 SELECT @intLoadId = intLoadId
 	, @dblTicketFreightRate = dblFreightRate
@@ -534,6 +535,7 @@ BEGIN TRY
     BEGIN
         IF EXISTS(SELECT TOP 1 1 FROM tblCTPriceFixation WHERE intContractDetailId = @intContractDetailId)
         BEGIN
+			SET @ysnPriceFixation = 1;
             EXEC uspCTCreateVoucherInvoiceForPartialPricing @intContractDetailId, @intUserId
         END
         SELECT @intContractDetailId = MIN(si.intLineNo)
@@ -547,7 +549,7 @@ BEGIN TRY
 	LEFT JOIN tblCTContractDetail CTD ON CTD.intContractDetailId = ISI.intLineNo
 	WHERE intInventoryShipmentId = @InventoryShipmentId
 
-	IF ISNULL(@InventoryShipmentId, 0) != 0 AND (ISNULL(@intPricingTypeId,0) <= 1 OR ISNULL(@intPricingTypeId,0) = 6) AND ISNULL(@strWhereFinalizedWeight, 'Origin') = 'Origin' AND ISNULL(@strWhereFinalizedGrade, 'Origin') = 'Origin'
+	IF ISNULL(@InventoryShipmentId, 0) != 0 AND (ISNULL(@intPricingTypeId,0) <= 1 OR ISNULL(@intPricingTypeId,0) = 6) AND ISNULL(@strWhereFinalizedWeight, 'Origin') = 'Origin' AND ISNULL(@strWhereFinalizedGrade, 'Origin') = 'Origin' AND @ysnPriceFixation = 0
 	BEGIN
 		EXEC @intInvoiceId = dbo.uspARCreateInvoiceFromShipment @InventoryShipmentId, @intUserId, NULL, 1;
 	END
