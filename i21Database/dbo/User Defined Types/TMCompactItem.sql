8f3bc4964d54807a5268cb4ecebf426da8c463ff
Kenneth Lew Lagunero <kenneth.lagunero@irely.com>
2018-11-27 20:56:34 +0800

    TM-2966

--- i21Database/dbo/User Defined Types/TMCompactItem.sql
+++ i21Database/dbo/User Defined Types/TMCompactItem.sql
@@ -6,7 +6,7 @@ CREATE TYPE [dbo].[TMCompactItem] AS TABLE
 	[intCntId] INT NOT NULL IDENTITY PRIMARY KEY CLUSTERED, 
 	[intItemId] INT NOT NULL, 
     [strItemNo] NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL , 
-    [strLocation] NVARCHAR(5) COLLATE Latin1_General_CI_AS  NULL  
+    [strLocation] NVARCHAR(500) COLLATE Latin1_General_CI_AS  NULL  
     
 )
 
