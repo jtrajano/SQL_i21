24c13ef7141f6730b517a26d3cd2449f38ac5d33
Jeff Trajano <jeffrey.trajano@irely.com>
2018-11-29 16:08:36 +0800

    GL-6802
    This reverts commit a5b49c6391820c1c9405b095db5f4849c8975dc8.

--- i21Database/Scripts/Post-Deployment/GL/DefaultData/1t_InsertTrialBalanceData.sql
+++ i21Database/Scripts/Post-Deployment/GL/DefaultData/1t_InsertTrialBalanceData.sql
@@ -1,42 +1,12 @@
-﻿GO
-PRINT ('Begin updating Trial Balance table')
-GO
-DECLARE @dateTime DATETIME = GETDATE()
-MERGE 
-	INTO	dbo.tblGLTrialBalance
-	WITH	(HOLDLOCK) 
-	AS		TrialBalanceTable
-	USING(
-		select intAccountId, sum(MTDBalance)MTDBalance, sum(YTDBalance)YTDBalance , intGLFiscalYearPeriodId  FROM  [vyuGLTrialBalanceRE_NonRE]
-		GROUP BY intAccountId, intGLFiscalYearPeriodId
-	)AS GLEntries
-	ON GLEntries.intAccountId = TrialBalanceTable.intAccountId  AND GLEntries.intGLFiscalYearPeriodId = TrialBalanceTable.intGLFiscalYearPeriodId 
-	WHEN MATCHED AND ( GLEntries.MTDBalance <> TrialBalanceTable.MTDBalance OR GLEntries.YTDBalance <> TrialBalanceTable.YTDBalance)
-		THEN
-		Update SET TrialBalanceTable.MTDBalance =  GLEntries.MTDBalance,
-		TrialBalanceTable.YTDBalance = GLEntries.YTDBalance,
-		TrialBalanceTable.intConcurrencyId = TrialBalanceTable.intConcurrencyId + 1,
-		dtmDateModified = @dateTime
-
-	WHEN NOT MATCHED BY TARGET THEN
-		INSERT (
-			intAccountId
-			,intGLFiscalYearPeriodId
-			,MTDBalance
-			,YTDBalance
-			,dtmDateModified
-			,intConcurrencyId
-		)
-		VALUES (
-			GLEntries.intAccountId
-			,GLEntries.intGLFiscalYearPeriodId
-			,GLEntries.MTDBalance
-			,GLEntries.YTDBalance
-			,@dateTime
-			,1
-		)
-	WHEN NOT MATCHED BY SOURCE THEN
-	DELETE;
-GO
-PRINT ('Finished updating Trial Balance table')
-GO
+﻿IF NOT EXISTS (SELECT TOP 1 1 FROM tblGLTrialBalance)
+BEGIN
+	INSERT INTO tblGLTrialBalance ( [intAccountId],[MTDBalance],[YTDBalance],[intGLFiscalYearPeriodId], intConcurrencyId, dtmDateModified)
+	 SELECT 
+	[intAccountId], 
+	[MTDBalance], 
+	[YTDBalance], 
+	[intGLFiscalYearPeriodId],
+	1,
+	GETDATE()
+	FROM [dbo].[vyuGLTrialBalanceRE_NonRE] AS [Extent1]
+END
