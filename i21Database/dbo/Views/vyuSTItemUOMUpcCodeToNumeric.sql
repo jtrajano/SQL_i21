9d97f741be6822b56ec2d2f5ccc1f5d888ce75b9
COMTECH\HZaragoza <henry.zaragoza@irely.com>
2018-12-01 06:26:26 +0800

    ST-1099 Scanner > Cleanup

---
+++ i21Database/dbo/Views/vyuSTItemUOMUpcCodeToNumeric.sql
@@ -0,0 +1,17 @@
+﻿CREATE VIEW vyuSTItemUOMUpcCodeToNumeric
+AS
+SELECT intItemUOMId
+					, intItemId
+					, strUpcCode
+					, strLongUPCCode
+					, CASE 
+						WHEN strUpcCode NOT LIKE '%[^0-9]%' 
+							THEN CONVERT(NUMERIC(32, 0),CAST(strUpcCode AS FLOAT))
+						ELSE NULL
+					END AS intUpcCode
+					, CASE 
+						WHEN strLongUPCCode NOT LIKE '%[^0-9]%' 
+							THEN CONVERT(NUMERIC(32, 0),CAST(strLongUPCCode AS FLOAT))
+						ELSE NULL
+					END AS intLongUpcCode 
+				FROM dbo.tblICItemUOM
