43a7ec7a6d8dc1d322fd99746439976cea081838
Jb Mago <jb.mago@irely.com>
2018-12-01 06:18:02 +0800

    [B] PAT-992

--- i21PreDeployment/Scripts/Pre-Deployment/22_PAT/6_VolumeConstraintFix.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/22_PAT/6_VolumeConstraintFix.sql
@@ -1,68 +1,71 @@
 ﻿GO
 PRINT N'*** BEGIN - Volume Constraint Fix for tblPATCustomerVolume ***'
 GO
-IF EXISTS(SELECT TOP 1 intCustomerPatronId, intPatronageCategoryId, intFiscalYear FROM tblPATCustomerVolume GROUP BY intCustomerPatronId, intPatronageCategoryId, intFiscalYear HAVING COUNT(*) > 1) 
+IF EXISTS(SELECT TOP 1 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE [TABLE_NAME] = 'tblPATCustomerVolume')
 BEGIN
 	EXEC('
-		CREATE TABLE #tempCustomerVolume(
-			[intId] INT NOT NULL IDENTITY,
-			[intCustomerPatronId] INT NULL, 
-			[intPatronageCategoryId] INT NULL, 
-			[intFiscalYear] INT NULL, 
-			[dblVolume] NUMERIC(18, 6) NULL, 
-			[dblVolumeProcessed] NUMERIC(18, 6) NULL DEFAULT(0)
-		);
+		IF EXISTS(SELECT TOP 1 intCustomerPatronId, intPatronageCategoryId, intFiscalYear FROM tblPATCustomerVolume GROUP BY intCustomerPatronId, intPatronageCategoryId, intFiscalYear HAVING COUNT(*) > 1) 
+		BEGIN
+			CREATE TABLE #tempCustomerVolume(
+				[intId] INT NOT NULL IDENTITY,
+				[intCustomerPatronId] INT NULL, 
+				[intPatronageCategoryId] INT NULL, 
+				[intFiscalYear] INT NULL, 
+				[dblVolume] NUMERIC(18, 6) NULL, 
+				[dblVolumeProcessed] NUMERIC(18, 6) NULL DEFAULT(0)
+			);
 	
-		INSERT INTO #tempCustomerVolume(
-			[intCustomerPatronId], 
-			[intPatronageCategoryId], 
-			[intFiscalYear], 
-			[dblVolume], 
-			[dblVolumeProcessed]
-		)
-		SELECT	intCustomerPatronId, 
-				intPatronageCategoryId, 
-				intFiscalYear, 
-				dblVolume = SUM(dblVolume), 
-				dblVolumeProcessed = SUM(dblVolumeProcessed)
-		FROM tblPATCustomerVolume
-		GROUP BY intCustomerPatronId, intPatronageCategoryId, intFiscalYear
-		HAVING COUNT(*) > 1
-
-		DELETE Volume FROM tblPATCustomerVolume Volume
-		INNER JOIN #tempCustomerVolume tempVolume
-			ON tempVolume.intCustomerPatronId = Volume.intCustomerPatronId 
-			AND tempVolume.intPatronageCategoryId = Volume.intPatronageCategoryId
-			AND tempVolume.intFiscalYear = Volume.intFiscalYear
-
-		MERGE 
-		INTO [dbo].[tblPATCustomerVolume]
-		WITH (HOLDLOCK)
-		AS VolumeMaster
-		USING #tempCustomerVolume tempVolume
-			ON tempVolume.intCustomerPatronId = VolumeMaster.intCustomerPatronId 
-			AND tempVolume.intPatronageCategoryId = VolumeMaster.intPatronageCategoryId
-			AND tempVolume.intFiscalYear = VolumeMaster.intFiscalYear
-		WHEN MATCHED THEN
-			UPDATE SET
-				VolumeMaster.dblVolumeProcessed = VolumeMaster.dblVolumeProcessed + tempVolume.dblVolume,
-				VolumeMaster.dblVolume = VolumeMaster.dblVolumeProcessed + tempVolume.dblVolume
-		WHEN NOT MATCHED THEN
-			INSERT (
-				intCustomerPatronId,
-				intPatronageCategoryId,
-				intFiscalYear,
-				dblVolume,
-				dblVolumeProcessed
+			INSERT INTO #tempCustomerVolume(
+				[intCustomerPatronId], 
+				[intPatronageCategoryId], 
+				[intFiscalYear], 
+				[dblVolume], 
+				[dblVolumeProcessed]
 			)
-			VALUES(
-				tempVolume.intCustomerPatronId,
-				tempVolume.intPatronageCategoryId,
-				tempVolume.intFiscalYear,
-				tempVolume.dblVolume,
-				tempVolume.dblVolumeProcessed
-			)
-		;
+			SELECT	intCustomerPatronId, 
+					intPatronageCategoryId, 
+					intFiscalYear, 
+					dblVolume = SUM(dblVolume), 
+					dblVolumeProcessed = SUM(dblVolumeProcessed)
+			FROM tblPATCustomerVolume
+			GROUP BY intCustomerPatronId, intPatronageCategoryId, intFiscalYear
+			HAVING COUNT(*) > 1
+
+			DELETE Volume FROM tblPATCustomerVolume Volume
+			INNER JOIN #tempCustomerVolume tempVolume
+				ON tempVolume.intCustomerPatronId = Volume.intCustomerPatronId 
+				AND tempVolume.intPatronageCategoryId = Volume.intPatronageCategoryId
+				AND tempVolume.intFiscalYear = Volume.intFiscalYear
+
+			MERGE 
+			INTO [dbo].[tblPATCustomerVolume]
+			WITH (HOLDLOCK)
+			AS VolumeMaster
+			USING #tempCustomerVolume tempVolume
+				ON tempVolume.intCustomerPatronId = VolumeMaster.intCustomerPatronId 
+				AND tempVolume.intPatronageCategoryId = VolumeMaster.intPatronageCategoryId
+				AND tempVolume.intFiscalYear = VolumeMaster.intFiscalYear
+			WHEN MATCHED THEN
+				UPDATE SET
+					VolumeMaster.dblVolumeProcessed = VolumeMaster.dblVolumeProcessed + tempVolume.dblVolume,
+					VolumeMaster.dblVolume = VolumeMaster.dblVolumeProcessed + tempVolume.dblVolume
+			WHEN NOT MATCHED THEN
+				INSERT (
+					intCustomerPatronId,
+					intPatronageCategoryId,
+					intFiscalYear,
+					dblVolume,
+					dblVolumeProcessed
+				)
+				VALUES(
+					tempVolume.intCustomerPatronId,
+					tempVolume.intPatronageCategoryId,
+					tempVolume.intFiscalYear,
+					tempVolume.dblVolume,
+					tempVolume.dblVolumeProcessed
+				)
+			;
+		END
 	')
 END
 GO
