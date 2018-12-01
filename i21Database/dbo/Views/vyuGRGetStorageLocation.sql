153a5f5c6fee64dbd771d35a7177752153eccbc6
Leslie L Arat <leslie.arat@irely.com>
2018-12-01 08:28:08 +0800

    GRN-1349

--- i21Database/dbo/Views/vyuGRGetStorageLocation.sql
+++ i21Database/dbo/Views/vyuGRGetStorageLocation.sql
@@ -1,11 +1,23 @@
 CREATE VIEW [dbo].[vyuGRGetStorageLocation]  
 AS  
 SELECT DISTINCT
- CS.intEntityId 
-,CS.intCompanyLocationId  
-,LOC.strLocationName  
+	 CS.intEntityId 
+	,CS.intCompanyLocationId  
+	,LOC.strLocationName
+	,ST.ysnCustomerStorage
+	,ysnStorageLocationReady = CAST(
+                                    CASE
+                                        WHEN ysnTransferStorage = 1 THEN 1
+                                        ELSE 
+                                            CASE
+                                                WHEN CS.intTicketId IS NOT NULL THEN 1
+                                                WHEN CS.intDeliverySheetId IS NOT NULL THEN (SELECT ysnPost FROM tblSCDeliverySheet WHERE intDeliverySheetId = CS.intDeliverySheetId)
+                                            END
+                                    END AS BIT
+                                )
 FROM tblGRCustomerStorage CS
-JOIN tblSMCompanyLocation LOC ON LOC.intCompanyLocationId =CS.intCompanyLocationId
-JOIN tblGRStorageType ST ON ST.intStorageScheduleTypeId=CS.intStorageTypeId  
-WHERE CS.dblOpenBalance >0 AND ISNULL(CS.strStorageType,'') <> 'ITR' AND ST.ysnCustomerStorage=0
-
+JOIN tblSMCompanyLocation LOC 
+	ON LOC.intCompanyLocationId = CS.intCompanyLocationId
+JOIN tblGRStorageType ST 
+	ON ST.intStorageScheduleTypeId = CS.intStorageTypeId  
+WHERE CS.dblOpenBalance > 0
