1f3a84bd6808ef025e67685d08078e202b4abc1a
Kelvin Quinto <kelvin.quinto@irely.com>
2018-12-04 15:29:33 +0800

    AR-9423

--- i21Database/dbo/Stored Procedures/uspARUpdateInTransit.sql
+++ i21Database/dbo/Stored Procedures/uspARUpdateInTransit.sql
@@ -49,11 +49,8 @@ BEGIN
 					ON TICKET.intTicketId = ID.intTicketId
 			WHERE ID.intInvoiceId = @TransactionId 
 			AND ISNULL(ID.intInventoryShipmentItemId, 0) > 0
-			AND (ID.intTicketId IS NULL OR (ID.intTicketId IS NOT NULL AND ISNULL(TICKET.strInOutFlag, '') <> 'O' AND ISNULL(TICKET.intStorageScheduleTypeId, 0) <> 1))
+			AND (ID.intTicketId IS NULL OR (ID.intTicketId IS NOT NULL AND ISNULL(TICKET.strInOutFlag, '') = 'O' AND ISNULL(TICKET.intStorageScheduleTypeId, 0) <> 1))
 			AND (
-				--	(I.[strType] <> 'Provisional' AND NOT EXISTS(SELECT NULL FROM tblARInvoice ARI WHERE ARI.[intInvoiceId] = I.[intOriginalInvoiceId]))
-				--OR
-					--(I.[strType] <> 'Provisional' AND EXISTS(SELECT NULL FROM tblARInvoice ARI WHERE ARI.[intInvoiceId] = I.[intOriginalInvoiceId] AND ARI.[strType] = 'Provisional' AND ARI.[ysnPosted] = 0))
 					(I.[strType] <> 'Provisional' AND I.[ysnProvisionalWithGL] = 0)
 				OR
 					(I.[strType] = 'Provisional' AND I.[ysnProvisionalWithGL] = 1)
