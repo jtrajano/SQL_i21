a330688452b76070d67cc13f9f72d96220814701
Satya S Sahoo <satya.sahoo@irely.com>
2018-12-04 15:57:24 +0530

    CT-2723 - Delete of Brokerage Commission Processing is throwing error - 18.3B

---
+++ i21Database/dbo/Stored Procedures/uspCTBeforeSaveBrokerageCommission.sql
@@ -0,0 +1,31 @@
+﻿CREATE PROCEDURE [dbo].[uspCTBeforeSaveBrokerageCommission]
+		
+	@intBrkgCommnId	INT,
+	@intUserId		INT,
+	@strXML			NVARCHAR(MAX)
+	
+AS
+
+BEGIN TRY
+	
+	DECLARE @ErrMsg	NVARCHAR(MAX)
+
+	IF @strXML = 'Delete'
+	BEGIN
+
+	   UPDATE CC
+	   SET CC.strStatus = NULL
+	   FROM tblCTBrkgCommnDetail BD
+	   JOIN	 tblCTContractCost CC ON BD.intContractCostId = CC.intContractCostId
+	   WHERE BD.intBrkgCommnId = @intBrkgCommnId
+
+	END
+
+END TRY
+
+BEGIN CATCH
+
+	SET @ErrMsg = ERROR_MESSAGE()  
+	RAISERROR (@ErrMsg,18,1,'WITH NOWAIT')  
+	
+END CATCH
