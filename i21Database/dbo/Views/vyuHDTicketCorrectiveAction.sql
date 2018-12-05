843ceb7b31d6a3ef77f0add418b5f9939b4392d2
Jayson Ramos <jayson.ramos@iRely.com>
2018-12-05 14:06:11 +0800

    HD-2806

---
+++ i21Database/dbo/Views/vyuHDTicketCorrectiveAction.sql
@@ -0,0 +1,3 @@
+﻿CREATE VIEW [dbo].[vyuHDTicketCorrectiveAction]
+	AS
+	select a.intCorrectiveActionId, a.intTicketId, a.intCorrectiveActionTicketId, b.strTicketNumber, b.strSubject, a.intConcurrencyId from tblHDTicketCorrectiveAction a, tblHDTicket b where b.intTicketId = a.intCorrectiveActionTicketId
