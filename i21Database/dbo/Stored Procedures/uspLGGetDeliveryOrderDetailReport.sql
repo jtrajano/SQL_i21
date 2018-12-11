c48bebaaae87b8c0e8a2e78e32a7135743540dfc
Arman Arenas <arman.arenas@irely.com>
2018-12-12 04:20:22 +0800

    LG-1754 added uspLGGetDeliveryOrderDetailReport.strWeightUOMSymbol

--- i21Database/dbo/Stored Procedures/uspLGGetDeliveryOrderDetailReport.sql
+++ i21Database/dbo/Stored Procedures/uspLGGetDeliveryOrderDetailReport.sql
@@ -18,6 +18,7 @@ BEGIN
 		  ,IU.strUnitMeasure AS strQtyUOM
 		  ,LDL.dblNet AS dblWeight
 		  ,WU.strUnitMeasure AS strWeightUOM
+		  ,WU.strSymbol AS strWeightUOMSymbol
 		  ,IRIL.strContainerNo AS strContainerNumber
 		  ,LC.strMarks
 		  ,LW.strDeliveryNoticeNumber
