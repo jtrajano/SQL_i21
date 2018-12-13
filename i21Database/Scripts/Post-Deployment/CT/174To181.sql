eea766c4022014ce7c73bf4531fded86bdb9dfee
Satya S Sahoo <satya.sahoo@irely.com>
2018-12-13 23:24:56 +0530

    CT-2780 - Invoice are not created for the last scale ticket - 18.3B

--- i21Database/Scripts/Post-Deployment/CT/174To181.sql
+++ i21Database/Scripts/Post-Deployment/CT/174To181.sql
@@ -16,15 +16,7 @@ GO
 
 PRINT('CT - 174To181 End')
 
-GO
-PRINT('CT - 183')
-IF NOT EXISTS(SELECT TOP 1 1 FROM tblCTPriceFixationDetailAPAR)
-BEGIN
-	INSERT INTO tblCTPriceFixationDetailAPAR(intPriceFixationDetailId,intBillId,intBillDetailId,intInvoiceId,intInvoiceDetailId,intConcurrencyId)
-	SELECT intPriceFixationDetailId,intBillId,intBillDetailId,intInvoiceId,intInvoiceDetailId,1 FROM tblCTPriceFixationDetail WHERE (intInvoiceId IS NOT NULL OR intBillId IS NOT NULL) AND intInvoiceId NOT IN (SELECT intInvoiceId FROM tblCTPriceFixationDetailAPAR)
-END
-PRINT('End CT - 183')
-GO
+------------183----------
 
 GO
 PRINT('Udate existing sequence History')
