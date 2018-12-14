be05f993d26e7e254e7733fa8764ac254a2def35
COMTECH\HZaragoza <henry.zaragoza@irely.com>
2018-12-15 03:59:08 +0800

    ST-1106 [Checkout] Allow re-importing in checkout

---
+++ i21Database/dbo/Tables/tblSTStoreAppHistoryReports.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[tblSTStoreAppHistoryReports](
+    [intStoreAppHistoryReportId] [int] IDENTITY(1,1) NOT NULL,    
+	[strReportType] [nvarchar](50) NOT NULL, -- If Upload or Download
+    [intStoreNo] [int] NULL,
+    [strLocalFolderPath] [nvarchar](500) NULL,
+    [strLocalFilename] [nvarchar](100) NULL,
+    [strServerFolderPath] [nvarchar](500) NULL,
+    [strServerFilename] [nvarchar](100) NULL,
+    [strFilePrefix] [nvarchar](20) NULL,
+    [dtmShiftCloseDate] [datetime] NULL,
+    [dtmDateUploaded] [datetime] NULL,
+    [ysnUploadSuccess] [bit] NOT NULL,
+	[ysnDownloadSuccess] [bit] NOT NULL,
+    [strRemark] [nvarchar](1000) NULL,
+    [intConcurrencyId] [int] NOT NULL,
+)
