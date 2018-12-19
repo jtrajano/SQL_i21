c417aad07544f27ab4448fc83cb534c41ebab21f
Jeff Trajano <jeffrey.trajano@irely.com>
2018-12-19 10:37:55 +0800

    GL-6824 GL - SQL Views and Stored Procedures string fields must be set to case insensitive

--- i21Database/dbo/Views/vyuGLFiscalYearPeriod.sql
+++ i21Database/dbo/Views/vyuGLFiscalYearPeriod.sql
@@ -3,13 +3,12 @@ AS
 
 SELECT A.[intGLFiscalYearPeriodId]
 	,A.[intFiscalYearId]
-	,A.[strPeriod]
+	,A.[strPeriod] COLLATE Latin1_General_CI_AS strPeriod
 	,A.[dtmStartDate]
 	,A.[dtmEndDate]
 	,A.[ysnOpen]
 	,A.[intConcurrencyId]
-	,B.[strFiscalYear]
-
+	,B.[strFiscalYear] COLLATE Latin1_General_CI_AS strFiscalYear
 FROM tblGLFiscalYearPeriod A
 INNER JOIN tblGLFiscalYear B
 ON A.intFiscalYearId = B.intFiscalYearId
--- i21Database/dbo/Views/vyuGLSearchMulticurrencyRevalue.sql
+++ i21Database/dbo/Views/vyuGLSearchMulticurrencyRevalue.sql
@@ -2,12 +2,12 @@
 CREATE VIEW [dbo].[vyuGLSearchMulticurrencyRevalue]
 AS
 select 
-strConsolidationNumber,
+strConsolidationNumber COLLATE Latin1_General_CI_AS strConsolidationNumber,
 intConsolidationId,
-B.strPeriod,
-D.strCurrency,
-strTransactionType,
-C.strCurrencyExchangeRateType,
+B.strPeriod COLLATE Latin1_General_CI_AS strPeriod,
+D.strCurrency COLLATE Latin1_General_CI_AS strCurrency,
+strTransactionType COLLATE Latin1_General_CI_AS strTransactionType,
+C.strCurrencyExchangeRateType COLLATE Latin1_General_CI_AS strCurrencyExchangeRateType,
 A.ysnPosted
 from tblGLRevalue A
 LEFT JOIN tblGLFiscalYearPeriod B
