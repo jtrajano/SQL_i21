30f44aa1c8c66247cf062da94c869b8aad2731a4
Erick <erick.delacruz@irely.com>
2018-12-19 16:07:29 +0800

    AP-6640

--- i21Database/dbo/Functions/fnAPValidatePostBill.sql
+++ i21Database/dbo/Functions/fnAPValidatePostBill.sql
@@ -436,7 +436,20 @@ BEGIN
 		AND 1 = CASE WHEN D.intTransactionId IS NOT NULL
 					THEN CASE WHEN D.ysnCheckVoid = 0 THEN 1 ELSE 0 END
 				ELSE 1 END
-
+		UNION ALL
+		SELECT
+			'You cannot unpost this voucher. ' + A.strRecordNumber + ' payment was already made on this voucher. You must delete the payment first.',
+			'Bill',
+			C.strBillId,
+			C.intBillId,
+			22
+		FROM tblARPayment A
+			INNER JOIN tblARPaymentDetail B 
+				ON A.intPaymentId = B.intPaymentId
+			INNER JOIN tblAPBill C
+				ON B.intBillId = C.intBillId
+		WHERE  C.[intBillId] IN (SELECT [intBillId] FROM @tmpBills)
+		AND A.ysnPosted = 1
 		--NO FISCAL PERIOD
 		--INSERT INTO @returntable(strError, strTransactionType, strTransactionId, intTransactionId, intErrorKey)
 		--SELECT 
