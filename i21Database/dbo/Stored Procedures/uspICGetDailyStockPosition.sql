1c305d7916c78c53581584f0350da1894000068c
Wendell Wayne Estrada <wendell.estrada@irely.com>
2018-12-20 11:09:42 +0800

    [B] IC-6577
    [Search Screens] Filter is case sensitive

--- i21Database/dbo/Stored Procedures/uspICGetDailyStockPosition.sql
+++ i21Database/dbo/Stored Procedures/uspICGetDailyStockPosition.sql
@@ -1,5 +1,6 @@
 ﻿CREATE PROCEDURE [dbo].[uspICGetDailyStockPosition]
-	@dtmDate AS DATETIME
+	@dtmDate AS DATETIME,
+	@guidSessionId UNIQUEIDENTIFIER
 AS
 
 SET QUOTED_IDENTIFIER OFF
@@ -337,8 +338,11 @@ CREATE TABLE #tmpDailyStockPosition
 			intLotId,
 			intInTransitSourceLocationId
 
+	DELETE FROM tblICStagingDailyStockPosition WHERE (guidSessionId = @guidSessionId OR DATEDIFF(SECOND, dtmDateCreated, GETDATE()) > 10)
 	-----===== READ DAILY STOCK POSITION
-	SELECT	intKey					= CAST(ROW_NUMBER() OVER(ORDER BY Item.intCommodityId, Item.intItemId) AS INT),
+	INSERT INTO tblICStagingDailyStockPosition
+	SELECT	guidSessionId			= @guidSessionId,
+			intKey					= CAST(ROW_NUMBER() OVER(ORDER BY Item.intCommodityId, Item.intItemId) AS INT),
 			intCommodityId			= Item.intCommodityId,
 			strCommodityCode		= ISNULL(Commodity.strCommodityCode, ''),
 			dtmDate					= CAST(CONVERT(VARCHAR(10),@dtmDate,112) AS datetime),
@@ -371,7 +375,12 @@ CREATE TABLE #tmpDailyStockPosition
 										+ tmpDSP.dblInTransitInbound 
 										- tmpDSP.dblInTransitOutbound 
 										- tmpDSP.dblConsumedQty 
-										+ tmpDSP.dblProduced
+										+ tmpDSP.dblProduced,
+			intConcurrencyId		= 1,
+			dtmDateModified			= NULL,
+			dtmDateCreated			= GETDATE(),
+			intModifiedByUserId		= NULL,
+			intCreatedByUserId		= NULL
 	FROM tblICItem Item
 		INNER JOIN (tblICItemUOM StockUOM
 			INNER JOIN tblICUnitMeasure sUOM ON StockUOM.intUnitMeasureId = sUOM.intUnitMeasureId
