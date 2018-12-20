6cae7b88af635a44e9696d14f313719d32f43c42
csJSarcilla <JSarcilla@COMTECH.COM>
2018-12-20 19:07:06 +0800

    RM-2423

--- i21Database/dbo/Stored Procedures/uspRKRiskPositionInquiryChart.sql
+++ i21Database/dbo/Stored Procedures/uspRKRiskPositionInquiryChart.sql
@@ -24,7 +24,8 @@ DECLARE @strRiskView nvarchar(50)
 SELECT @strRiskView = strRiskView FROM tblRKCompanyPreference 
 
 DECLARE @tblFinalDetail TABLE (
-		intRowNumber INT
+		intRowNumberFinal INT DEFAULT 0
+		,intRowNumber INT
 		,strGroup  NVARCHAR(500) COLLATE Latin1_General_CI_AS
 		,Selection NVARCHAR(500) COLLATE Latin1_General_CI_AS
 		,PriceStatus NVARCHAR(500) COLLATE Latin1_General_CI_AS
@@ -53,7 +54,7 @@ DECLARE @tblMonthFinal TABLE (
 IF @strRiskView <> 'Processor'
 BEGIN
 
-		INSERT INTO @tblFinalDetail (intRowNumber,Selection ,PriceStatus ,strFutureMonth ,strAccountNumber ,dblNoOfContract ,strTradeNo ,TransactionDate
+		INSERT INTO @tblFinalDetail (intRowNumber,strGroup,Selection ,PriceStatus ,strFutureMonth ,strAccountNumber ,dblNoOfContract ,strTradeNo ,TransactionDate
 										,TranType ,CustVendor ,dblNoOfLot ,dblQuantity ,intOrderByHeading ,intContractHeaderId ,intFutOptTransactionHeaderId)
 		EXEC uspRKRiskPositionInquiry @intCommodityId = @intCommodityId
 		 ,@intCompanyLocationId = @intCompanyLocationId
@@ -67,18 +68,28 @@ BEGIN
 		 @intSubBookId = @intSubBookId 
 		
 
-	SELECT CONVERT(INT,ROW_NUMBER() OVER(ORDER BY strFutureMonth ASC)) intRowNum,
-		(select SUM(dblNoOfContract) FROM @tblFinalDetail t1 WHERE t1.Selection = 'Physical position / Basis risk' and t1.strFutureMonth= t.strFutureMonth) dblPhysicalPosition,
-		(select SUM(dblNoOfContract) FROM @tblFinalDetail t1 WHERE t1.strAccountNumber='Market risk' and t1.strFutureMonth= t.strFutureMonth) dblNetMarketRisk,
-		 strFutureMonth,'' as Selection  FROM @tblFinalDetail t WHERE (Selection = 'Physical position / Basis risk' or strAccountNumber='Market risk') 
-	group by  strFutureMonth
-	ORDER BY  CASE WHEN  strFutureMonth <>'Previous' THEN CONVERT(DATETIME,'01 '+strFutureMonth) END
+	-- SELECT CONVERT(INT,ROW_NUMBER() OVER(ORDER BY strFutureMonth ASC)) intRowNum,
+	-- 	(select SUM(dblNoOfContract) FROM @tblFinalDetail t1 WHERE t1.Selection = 'Physical position / Basis risk' and t1.strFutureMonth= t.strFutureMonth) dblPhysicalPosition,
+	-- 	(select SUM(dblNoOfContract) FROM @tblFinalDetail t1 WHERE t1.strAccountNumber='Market risk' and t1.strFutureMonth= t.strFutureMonth) dblNetMarketRisk,
+	-- 	 strFutureMonth,'' as Selection  FROM @tblFinalDetail t WHERE (Selection = 'Physical position / Basis risk' or strAccountNumber='Market risk') 
+	-- group by  strFutureMonth
+	-- ORDER BY  CASE WHEN  strFutureMonth <>'Previous' THEN CONVERT(DATETIME,'01 '+strFutureMonth) END
 
+	SELECT CONVERT(INT,ROW_NUMBER() OVER(ORDER BY strFutureMonth ASC)) intRowNum,
+		(select SUM(dblNoOfContract) FROM @tblFinalDetail t1 WHERE t1.strGroup = 'Physical position / Basis risk' and t1.strFutureMonth= t.strFutureMonth) dblPhysicalPosition,
+		(select SUM(dblNoOfContract) FROM @tblFinalDetail t1 WHERE t1.strAccountNumber='Market risk' and t1.strFutureMonth= t.strFutureMonth) dblNetMarketRisk,
+		FORMAT(CONVERT(DATETIME, strFutureMonth), 'MMM yyyy') strFutureMonth,'' as Selection  FROM @tblFinalDetail t 
+	WHERE (strGroup = 'Physical position / Basis risk' or strAccountNumber='Market risk') 
+		AND ISNULL(strFutureMonth, '') <> ''
+	group by  strFutureMonth
+	ORDER BY CASE WHEN  strFutureMonth ='Previous' THEN '01/01/1900' 
+		  WHEN  strFutureMonth ='Total' THEN '01/01/9999'
+		else FORMAT(CONVERT(DATETIME, strFutureMonth), 'MMM yyyy') END
 
 END
 ELSE
 BEGIN
-		INSERT INTO @tblFinalDetail (intRowNumber,strGroup,Selection ,PriceStatus ,strFutureMonth ,strAccountNumber ,dblNoOfContract ,strTradeNo ,TransactionDate
+		INSERT INTO @tblFinalDetail (intRowNumberFinal,intRowNumber,strGroup,Selection ,PriceStatus ,strFutureMonth ,strAccountNumber ,dblNoOfContract ,strTradeNo ,TransactionDate
 										,TranType ,CustVendor ,dblNoOfLot ,dblQuantity ,intOrderByHeading ,intContractHeaderId ,intFutOptTransactionHeaderId)
 		EXEC uspRKRiskPositionInquiryBySummary @intCommodityId = @intCommodityId
 		 ,@intCompanyLocationId = @intCompanyLocationId
@@ -92,12 +103,21 @@ BEGIN
 		 @intSubBookId = @intSubBookId
 
 
+	-- SELECT CONVERT(INT,ROW_NUMBER() OVER(ORDER BY strFutureMonth ASC)) intRowNum,
+	-- 	(select SUM(dblNoOfContract) FROM @tblFinalDetail t1 WHERE t1.PriceStatus = 'Outright coverage' and t1.strFutureMonth= t.strFutureMonth) dblPhysicalPosition,
+	-- 	(select SUM(dblNoOfContract) FROM @tblFinalDetail t1 WHERE t1.PriceStatus='Futures Required' and t1.strFutureMonth= t.strFutureMonth) dblNetMarketRisk,
+	-- 	 strFutureMonth,'' as Selection  FROM @tblFinalDetail t WHERE PriceStatus in('Outright coverage','Futures Required') 
+	-- group by  strFutureMonth
+	-- ORDER BY  CASE WHEN  strFutureMonth <>'Previous' THEN CONVERT(DATETIME,'01 '+strFutureMonth) END
+
 	SELECT CONVERT(INT,ROW_NUMBER() OVER(ORDER BY strFutureMonth ASC)) intRowNum,
-		(select SUM(dblNoOfContract) FROM @tblFinalDetail t1 WHERE t1.PriceStatus = 'Outright coverage' and t1.strFutureMonth= t.strFutureMonth) dblPhysicalPosition,
-		(select SUM(dblNoOfContract) FROM @tblFinalDetail t1 WHERE t1.PriceStatus='Futures Required' and t1.strFutureMonth= t.strFutureMonth) dblNetMarketRisk,
-		 strFutureMonth,'' as Selection  FROM @tblFinalDetail t WHERE PriceStatus in('Outright coverage','Futures Required') 
+		(select SUM(dblNoOfContract) FROM @tblFinalDetail t1 WHERE LOWER(t1.Selection) = LOWER('Outright coverage') and t1.strFutureMonth= t.strFutureMonth) dblPhysicalPosition,
+		(select SUM(dblNoOfContract) FROM @tblFinalDetail t1 WHERE LOWER(t1.Selection) = LOWER('Futures Required') and t1.strFutureMonth= t.strFutureMonth) dblNetMarketRisk,
+		 strFutureMonth,'' as Selection  FROM @tblFinalDetail t WHERE LOWER(Selection) in('outright coverage','futures required') 
 	group by  strFutureMonth
-	ORDER BY  CASE WHEN  strFutureMonth <>'Previous' THEN CONVERT(DATETIME,'01 '+strFutureMonth) END
+	ORDER BY CASE WHEN  strFutureMonth ='Previous' THEN '01/01/1900' 
+		  WHEN  strFutureMonth ='Total' THEN '01/01/9999'
+		else CONVERT(DATETIME,'01 '+strFutureMonth) END
 
 END
 
