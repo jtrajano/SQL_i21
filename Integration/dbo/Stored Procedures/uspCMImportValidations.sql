ae84edb7460ef9152a1fb7b50f40f7bd20b70074
Jeff Trajano <jeffrey.trajano@irely.com>
2018-12-26 15:17:24 +0800

    GL-6823 Undefined error while importing cash from Origin

--- Integration/dbo/Stored Procedures/uspCMImportValidations.sql
+++ Integration/dbo/Stored Procedures/uspCMImportValidations.sql
@@ -25,7 +25,6 @@ BEGIN
 			,@Missing_Cash_Account_Group AS BIT OUTPUT
 			,@Future_Clear_Date_Found AS BIT OUTPUT
 			,@Unbalance_Found AS BIT OUTPUT
-			,@Duplicate_Bank_Name_Found AS BIT OUTPUT
 		AS
 
 		DECLARE @CASH_ACCOUNT AS NVARCHAR(20) = ''Cash Account''
@@ -118,20 +117,6 @@ BEGIN
 		) AS T
 		WHERE NotBalance = 1
 
-		--Check for duplicate Bank Name
-		SELECT TOP 1 @Duplicate_Bank_Name_Found = 1  
-		FROM (
-			SELECT	
-				strBankName	= LTRIM(RTRIM(ISNULL(apcbk_desc COLLATE Latin1_General_CI_AS, ''''))) 
-			FROM apcbkmst
-
-			UNION ALL SELECT 
-				strBankName	= LTRIM(RTRIM(ISNULL(ssbnk_name COLLATE Latin1_General_CI_AS, '''')))		
-			FROM ssbnkmst
-		) tbl
-		GROUP BY strBankName COLLATE Latin1_General_CI_AS
-		HAVING (COUNT(strBankName) > 1)
-
 
 		SELECT	@Invalid_UserId_Found = ISNULL(@Invalid_UserId_Found, 0)
 				,@Invalid_GL_Account_Id_Found = ISNULL(@Invalid_GL_Account_Id_Found, 0)
@@ -142,7 +127,6 @@ BEGIN
 				,@Missing_Cash_Account_Group = ISNULL(@Missing_Cash_Account_Group, 0)
 				,@Future_Clear_Date_Found = ISNULL(@Future_Clear_Date_Found, 0)	
 				,@Unbalance_Found = ISNULL(@Unbalance_Found, 0)	
-				,@Duplicate_Bank_Name_Found = ISNULL(@Duplicate_Bank_Name_Found, 0)	
 	')
 
 END
