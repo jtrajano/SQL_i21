a7109634044743387780a54e1371e82a60bff8d1
csJSarcilla <JSarcilla@COMTECH.COM>
2018-12-28 16:29:51 +0800

    RM-2411

--- i21Database/dbo/Stored Procedures/uspRKDPRPreCrushPositionDetail.sql
+++ i21Database/dbo/Stored Procedures/uspRKDPRPreCrushPositionDetail.sql
@@ -1754,6 +1754,7 @@ UPDATE @ListFinal SET strFutureMonth = CASE
 	WHEN LEN(LTRIM(RTRIM(F.strFutureMonth))) > 6 AND ISNULL(LTRIM(RTRIM(F.strFutureMonth)), '') <> '' THEN LTRIM(RTRIM(F.strFutureMonth))
 	WHEN ISNULL(F.intFutOptTransactionHeaderId, '') <> '' AND ISNULL(LTRIM(RTRIM(F.strFutureMonth)), '') = '' THEN FOT.strFutureMonth
 END COLLATE Latin1_General_CI_AS
+	, strDeliveryDate = CT.strDeliveryDate
 FROM @ListFinal F
 LEFT JOIN (
 	SELECT intFutOptTransactionHeaderId
@@ -1761,6 +1762,15 @@ LEFT JOIN (
 		,strFutureMonth = ISNULL(FORMAT(CONVERT(DATETIME,'01 '+ strFutureMonth), 'MMM yyyy'),'Near By') COLLATE Latin1_General_CI_AS
 	FROM vyuRKFutOptTransaction
 )FOT ON FOT.intFutOptTransactionHeaderId = F.intFutOptTransactionHeaderId AND FOT.strInternalTradeNo COLLATE Latin1_General_CI_AS = F.strInternalTradeNo COLLATE Latin1_General_CI_AS
+LEFT JOIN (
+	SELECT intContractHeaderId
+	,REPLACE(strSequenceNumber,' ','') COLLATE Latin1_General_CI_AS AS strContractNumber
+	,FORMAT(dtmEndDate, 'MMM yyyy') COLLATE Latin1_General_CI_AS AS strDeliveryDate
+	,ISNULL(FORMAT(CONVERT(DATETIME,'01 '+ strFutureMonth), 'MMM yyyy'),'Near By') COLLATE Latin1_General_CI_AS AS strFutureMonth
+	,strContractType
+	FROM vyuCTContractDetailView
+)CT ON CT.intContractHeaderId = F.intContractHeaderId
+	AND CT.strContractNumber = (CASE WHEN PATINDEX('%,%', F.strContractNumber) = 0 THEN F.strContractNumber ELSE LEFT(F.strContractNumber, PATINDEX('%,%', F.strContractNumber) - 1)END)
 	
 UPDATE @ListFinal SET strContractEndMonthNearBy = CASE 
 		WHEN @strPositionBy = 'Futures Month'  THEN ISNULL(NULLIF(LTRIM(RTRIM(strFutureMonth)),''),'Near By')
