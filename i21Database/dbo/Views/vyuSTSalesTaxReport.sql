0c363db5525e286e77105ef9bb72a2995ada2c57
COMTECH\HZaragoza <henry.zaragoza@irely.com>
2019-01-02 16:22:33 +0800

    ST-1113 [Checkout] Incorrect consolidate report

--- i21Database/dbo/Views/vyuSTSalesTaxReport.sql
+++ i21Database/dbo/Views/vyuSTSalesTaxReport.sql
@@ -8,14 +8,23 @@ SELECT ST.intStoreId
 , CH.dtmCheckoutDate
 , Inv.ysnPosted
 , ACC.strAccountId
+, STT.strTaxNo
 , SUM(STT.dblTotalTax) dblTotalTax
 , SUM(STT.dblTaxableSales) dblTaxableSales
 , SUM(STT.dblTaxExemptSales) dblTaxExemptSales
-FROM tblSTCheckoutHeader CH INNER JOIN tblSTStore ST ON ST.intStoreId = CH.intStoreId 
-LEFT JOIN tblARInvoice Inv ON Inv.intInvoiceId = CH.intInvoiceId
-INNER JOIN tblSTCheckoutSalesTaxTotals STT ON STT.intCheckoutId = CH.intCheckoutId 
-INNER JOIN tblGLAccount ACC ON ACC.intAccountId = STT.intSalesTaxAccount
-INNER JOIN tblICItem IT ON IT.intItemId = STT.intItemId 
-INNER JOIN tblICCategory CAT ON CAT.intCategoryId = IT.intCategoryId
-WHERE STT.dblTotalTax > 0	
-GROUP BY ST.intStoreId, ST.intStoreNo, ST.strRegion, ST.strDistrict, ST.strDescription, CH.dtmCheckoutDate, Inv.ysnPosted, ACC.strAccountId
+FROM tblSTCheckoutHeader CH 
+INNER JOIN tblSTStore ST 
+	ON ST.intStoreId = CH.intStoreId 
+LEFT JOIN tblARInvoice Inv 
+	ON Inv.intInvoiceId = CH.intInvoiceId
+INNER JOIN tblSTCheckoutSalesTaxTotals STT 
+	ON STT.intCheckoutId = CH.intCheckoutId 
+INNER JOIN tblGLAccount ACC 
+	ON ACC.intAccountId = STT.intSalesTaxAccount
+INNER JOIN tblICItem IT 
+	ON IT.intItemId = STT.intItemId 
+INNER JOIN tblICCategory CAT 
+	ON CAT.intCategoryId = IT.intCategoryId
+WHERE STT.dblTotalTax <> 0	
+	OR STT.dblTaxableSales <> 0	
+GROUP BY ST.intStoreId, ST.intStoreNo, ST.strRegion, ST.strDistrict, ST.strDescription, CH.dtmCheckoutDate, Inv.ysnPosted, ACC.strAccountId, STT.strTaxNo
