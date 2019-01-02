9bf31fda4dba8bfb2a38e16a918184292f039b72
COMTECH\HZaragoza <henry.zaragoza@irely.com>
2019-01-02 16:22:33 +0800

    ST-1113 [Checkout] Incorrect consolidate report

--- i21Database/dbo/Views/vyuSTDepartmentSummaryReport.sql
+++ i21Database/dbo/Views/vyuSTDepartmentSummaryReport.sql
@@ -18,5 +18,5 @@ LEFT JOIN tblARInvoice Inv ON Inv.intInvoiceId = CH.intInvoiceId
 INNER JOIN tblSTCheckoutDepartmetTotals CDT ON CDT.intCheckoutId = CH.intCheckoutId 
 INNER JOIN tblICItem IT ON IT.intItemId = CDT.intItemId 
 INNER JOIN tblICCategory CAT ON CAT.intCategoryId = IT.intCategoryId
-WHERE CDT.dblTotalSalesAmountComputed > 0	
+WHERE CDT.dblTotalSalesAmountComputed <> 0	
 GROUP BY ST.intStoreId, ST.intStoreNo, ST.strRegion, ST.strDistrict, ST.strDescription, CAT.intCategoryId, CAT.strCategoryCode, CAT.strDescription, CH.dtmCheckoutDate, ISNULL(Inv.ysnPosted, 0)
