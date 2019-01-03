ce5bb3cb8ae890945056c28b6f108eedc145e855
COMTECH\HZaragoza <henry.zaragoza@irely.com>
2019-01-03 13:08:53 +0800

    ST-1113 [Checkout] Incorrect consolidate report

--- i21Database/dbo/Views/vyuSTFuelSummaryReport.sql
+++ i21Database/dbo/Views/vyuSTFuelSummaryReport.sql
@@ -20,5 +20,5 @@ INNER JOIN tblSTCheckoutPumpTotals CPT ON CPT.intCheckoutId = CH.intCheckoutId
 INNER JOIN tblICItemUOM IU ON IU.intItemUOMId = CPT.intPumpCardCouponId 
 INNER JOIN tblICItem IT ON IT.intItemId = IU.intItemId 
 INNER JOIN tblICCategory CAT ON CAT.intCategoryId = IT.intCategoryId
-WHERE CPT.dblAmount > 0	
+WHERE CPT.dblAmount <> 0	
 GROUP BY ST.intStoreId, ST.intStoreNo, ST.strRegion, ST.strDistrict, ST.strDescription, CAT.intCategoryId, CAT.strDescription, IT.intItemId, IT.strItemNo, IT.strDescription, CH.dtmCheckoutDate, ISNULL(Inv.ysnPosted, 0)
