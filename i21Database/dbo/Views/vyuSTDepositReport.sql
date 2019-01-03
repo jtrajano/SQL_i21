204f16aabbb12fb2e3ebf04ab92eb9d47d93ca19
COMTECH\HZaragoza <henry.zaragoza@irely.com>
2019-01-03 10:39:36 +0800

    ST-1113 [Checkout] Incorrect consolidate report

--- i21Database/dbo/Views/vyuSTDepositReport.sql
+++ i21Database/dbo/Views/vyuSTDepositReport.sql
@@ -1,6 +1,7 @@
 ﻿CREATE VIEW [dbo].[vyuSTDepositReport]
 	AS 
-SELECT ST.intStoreId
+SELECT D.intDepositId 
+, ST.intStoreId
 , ST.intStoreNo
 , ST.strRegion
 , ST.strDistrict
@@ -10,9 +11,13 @@ SELECT ST.intStoreId
 , SUM(D.dblCash) dblCash
 , SUM(D.dblCoin) dblCoin
 , SUM(D.dblTotalDeposit) dblTotalDeposit
-FROM tblSTCheckoutHeader CH INNER JOIN tblSTStore ST ON ST.intStoreId = CH.intStoreId 
-LEFT JOIN tblARInvoice Inv ON Inv.intInvoiceId = CH.intInvoiceId
-INNER JOIN tblSTCheckoutDeposits D ON D.intCheckoutId = CH.intCheckoutId 
+FROM tblSTCheckoutHeader CH 
+INNER JOIN tblSTStore ST 
+	ON ST.intStoreId = CH.intStoreId 
+LEFT JOIN tblARInvoice Inv 
+	ON Inv.intInvoiceId = CH.intInvoiceId
+INNER JOIN tblSTCheckoutDeposits D 
+	ON D.intCheckoutId = CH.intCheckoutId 
 WHERE D.dblTotalDeposit > 0	
-GROUP BY ST.intStoreId, ST.intStoreNo, ST.strRegion, ST.strDistrict, ST.strDescription, CH.dtmCheckoutDate, ISNULL(Inv.ysnPosted, 0)
+GROUP BY D.intDepositId, ST.intStoreId, ST.intStoreNo, ST.strRegion, ST.strDistrict, ST.strDescription, CH.dtmCheckoutDate, ISNULL(Inv.ysnPosted, 0)
 
