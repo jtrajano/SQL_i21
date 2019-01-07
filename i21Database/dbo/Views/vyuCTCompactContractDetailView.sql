662c58f8ac4bda41ded3171561a8a2404f58a864
Feb Montefrio <feb.montefrio@irely.com>
2019-01-07 08:44:15 +0800

    [B] IC-6630

--- i21Database/dbo/Views/vyuCTCompactContractDetailView.sql
+++ i21Database/dbo/Views/vyuCTCompactContractDetailView.sql
@@ -13,9 +13,23 @@ AS
 			,ISNULL(CD.dblBalance,0) - ISNULL(CD.dblScheduleQty,0) AS dblAvailableQty
 			,PT.strPricingType
 			,CD.intContractSeq
-	FROM	tblCTContractDetail				CD	
+			,CD.strERPPONumber
+			,CD.strERPItemNumber
+			,strOrigin = ISNULL(RY.strCountry,OG.strCountry)
+			,strPurchasingGroup = PG.strName 
+			,strINCOShipTerm = CB.strContractBasis
+			,CD.intCompanyLocationId
+	FROM	tblCTContractDetail					CD	
 	CROSS APPLY tblCTCompanyPreference			CP	
-	LEFT JOIN	vyuCTContractHeaderView			CH	ON	CH.intContractHeaderId		=	CD.intContractHeaderId				
+	LEFT JOIN	tblCTContractHeader				CH	ON	CH.intContractHeaderId		=	CD.intContractHeaderId				
 	LEFT JOIN	tblCTPricingType				PT	ON	PT.intPricingTypeId			=	CD.intPricingTypeId					
 	LEFT JOIN	tblICItemUOM					IU	ON	IU.intItemUOMId				=	CD.intItemUOMId				
 	LEFT JOIN	tblICUnitMeasure				U1	ON	U1.intUnitMeasureId			=	IU.intUnitMeasureId			
+	LEFT JOIN	tblICItem						IM	ON	IM.intItemId				=	CD.intItemId				--strItemNo
+	LEFT JOIN	tblICItemContract				IC	ON	IC.intItemContractId		=	CD.intItemContractId		--strContractItemName
+	LEFT JOIN	tblSMCountry					RY	ON	RY.intCountryID				=	IC.intCountryId
+	LEFT JOIN	tblICCommodityAttribute			CA	ON	CA.intCommodityAttributeId  =	IM.intOriginId												
+														AND	CA.strType				=	'Origin'			
+	LEFT JOIN	tblSMCountry					OG	ON	OG.intCountryID				=	CA.intCountryID	
+	LEFT JOIN	tblSMPurchasingGroup			PG	ON	PG.intPurchasingGroupId		=	CD.intPurchasingGroupId
+	LEFT JOIN	tblCTContractBasis				CB	ON	CB.intContractBasisId		=	CH.intContractBasisId		
