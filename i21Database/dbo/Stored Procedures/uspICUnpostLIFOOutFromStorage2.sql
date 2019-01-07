d182b9f14a683dc4c25df14b4247aa4659c84f09
Feb Montefrio <feb.montefrio@irely.com>
2019-01-07 12:47:21 +0800

    [B] IC-6636

--- i21Database/dbo/Stored Procedures/uspICUnpostLIFOOutFromStorage2.sql
+++ i21Database/dbo/Stored Procedures/uspICUnpostLIFOOutFromStorage2.sql
@@ -49,10 +49,7 @@ FROM	(
 					SELECT	strTransactionId = @strTransactionId
 							,intTransactionId = @intTransactionId
 				) AS Source_Query  
-					ON ISNULL(inventory_transaction.ysnIsUnposted, 0) = 0
-					AND dbo.fnGetCostingMethod(inventory_transaction.intItemId,inventory_transaction.intItemLocationId) IN (@LIFO) 
-					AND inventory_transaction.intTransactionTypeId <> @AUTO_NEGATIVE
-					AND 
+					ON  
 					(
 						-- Link to the main transaction
 						(	
@@ -66,6 +63,10 @@ FROM	(
 							AND inventory_transaction.intRelatedTransactionId = Source_Query.intTransactionId
 						)
 					)
+					AND ISNULL(inventory_transaction.ysnIsUnposted, 0) = 0
+					AND inventory_transaction.intCostingMethod IN (@LIFO) --dbo.fnGetCostingMethod(inventory_transaction.intItemId,inventory_transaction.intItemLocationId) IN (@LIFO) 
+					AND inventory_transaction.intTransactionTypeId <> @AUTO_NEGATIVE
+					
 				-- If matched, update the ysnIsUnposted and set it to true (1) 
 				WHEN MATCHED THEN 
 					UPDATE 
