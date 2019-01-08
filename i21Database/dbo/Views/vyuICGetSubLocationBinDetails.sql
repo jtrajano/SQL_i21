b7d9b09f5a9efa54b5378a1f897daf8fac28249d
Jb Mago <jb.mago@irely.com>
2019-01-08 16:59:34 +0800

    [B] IC-6613 [B] IC-6616 [B] IC-6617

--- i21Database/dbo/Views/vyuICGetSubLocationBinDetails.sql
+++ i21Database/dbo/Views/vyuICGetSubLocationBinDetails.sql
@@ -19,7 +19,7 @@ SELECT
 								)
 	, strCommodityCode			= cd.strCommodityCode
 FROM tblICItemStockUOM sm
-	INNER JOIN tblICItemUOM im ON im.intItemUOMId = sm.intItemUOMId
+	INNER JOIN tblICItemUOM im ON im.intItemUOMId = sm.intItemUOMId AND im.ysnStockUnit = 1
 	INNER JOIN tblICItem i ON i.intItemId = sm.intItemId
 	INNER JOIN tblICItemLocation il ON il.intItemId = sm.intItemId
 		AND il.intItemLocationId = sm.intItemLocationId
