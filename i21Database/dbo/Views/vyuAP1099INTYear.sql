38dc02024129fdd3a57c80c76ff40a3d195cbf6c
Lex Peregrino <lex.peregrino@irely.com>
2019-01-11 11:22:05 +0800

    AP-6735 1.0

---
+++ i21Database/dbo/Views/vyuAP1099INTYear.sql
@@ -0,0 +1,11 @@
+﻿CREATE VIEW [dbo].[vyuAP1099INTYear]
+AS
+
+SELECT DISTINCT
+	A.intYear
+FROM vyuAP1099 A
+CROSS JOIN tblSMCompanySetup B
+CROSS JOIN tblAP1099Threshold C
+WHERE A.int1099Form = 2
+
+GO
