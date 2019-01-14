f468081349998999c4a66253e743f5972effe883
Jeff Trajano <jeffrey.trajano@irely.com>
2019-01-14 15:12:49 +0800

    GL-6863 Reverse commit in 18.3

--- i21Database/Scripts/Post-Deployment/CM/1_BankTransactionTypes.sql
+++ i21Database/Scripts/Post-Deployment/CM/1_BankTransactionTypes.sql
@@ -5,65 +5,197 @@
 -- Date Created: 10/09/2013 5:32 PM
 -- Created by: Feb Montefrio
 -- --------------------------------------------------
-GO
-PRINT('/*******************  BEGIN Populate Bank Transaction Types *******************/')
-GO
-	MERGE 
-	INTO	dbo.tblCMBankTransactionType
-	WITH	(HOLDLOCK) 
-	AS		BankTransactionTypeTable
-	USING	(
-			SELECT id = 1,		name = 'Bank Deposit'			UNION ALL 
-			SELECT id = 2,		name = 'Bank Withdrawal'		UNION ALL 
-			SELECT id = 3,		name = 'Misc Checks'			UNION ALL 
-			SELECT id = 4,		name = 'Bank Transfer'			UNION ALL 
-			SELECT id = 5,		name = 'Bank Transaction'		UNION ALL 
-			SELECT id = 6,		name = 'Credit Card Charge'		UNION ALL 
-			SELECT id = 7,		name = 'Credit Card Returns'	UNION ALL 
-			SELECT id = 8,		name = 'Credit Card Payments'	UNION ALL 
-			SELECT id = 9,		name = 'Bank Transfer (WD)'		UNION ALL 
-			SELECT id = 10,		name = 'Bank Transfer (DEP)'	UNION ALL 
-			SELECT id = 11,		name = 'Origin Deposit'			UNION ALL 
-			SELECT id = 12,		name = 'Origin Checks'			UNION ALL 
-			SELECT id = 13,		name = 'Origin EFT'				UNION ALL 
-			SELECT id = 14,		name = 'Origin Withdrawal'		UNION ALL 
-			SELECT id = 15,		name = 'Origin Wire'			UNION ALL 
-			SELECT id = 16,		name = 'AP Payment'				UNION ALL 
-			SELECT id = 17,		name = 'Bank Stmt Import'		UNION ALL 
-			SELECT id = 18,		name = 'AR Payment'				UNION ALL 
-			SELECT id = 19,		name = 'Void Check'				UNION ALL 
-			SELECT id = 20,		name = 'AP eCheck'				UNION ALL 
-			SELECT id = 21,		name = 'Paycheck'				UNION ALL 
-			SELECT id = 22,		name = 'ACH'					UNION ALL 
-			SELECT id = 23,		name = 'Direct Deposit'			UNION ALL 
-			SELECT id = 24,		name = 'Write Off'				UNION ALL 
-			--SELECT id = 25,		name = 'NSF'					UNION ALL 
-			SELECT id = 103,	name = 'Void Misc Check'		UNION ALL 
-			SELECT id = 116,	name = 'Void AP Payment'		UNION ALL 
-			SELECT id = 121,	name = 'Void Paycheck'			UNION ALL 
-			SELECT id = 122,	name = 'Void ACH'				UNION ALL 
-			SELECT id = 123,	name = 'Void Direct Deposit'
-	) AS BankTransactionTypeHardCodedValues
-		ON  BankTransactionTypeTable.intBankTransactionTypeId = BankTransactionTypeHardCodedValues.id
 
-	-- When id is matched, make sure the name and form are up-to-date.
-	WHEN MATCHED THEN 
-		UPDATE 
-		SET 	BankTransactionTypeTable.strBankTransactionTypeName = BankTransactionTypeHardCodedValues.name
-	-- When id is missing, then do an insert. 
-	WHEN NOT MATCHED BY TARGET THEN
-		INSERT (
-			intBankTransactionTypeId
-			,strBankTransactionTypeName
-			,intConcurrencyId
-		)
-		VALUES (
-			BankTransactionTypeHardCodedValues.id
-			,BankTransactionTypeHardCodedValues.name
-			,1
-		);
-	--WHEN NOT MATCHED BY SOURCE THEN
-	--DELETE;
-GO
-PRINT('/*******************  END Populate Bank Transaction Types *******************/')
-GO
+print('/*******************  BEGIN Populate Bank Transaction Types *******************/')
+
+INSERT INTO dbo.[tblCMBankTransactionType] (
+	[intBankTransactionTypeId]
+	,[strBankTransactionTypeName]
+	,[intConcurrencyId]
+)
+SELECT 
+	[intBankTransactionTypeId]		= 1
+	,[strBankTransactionTypeName]	= 'Bank Deposit'
+	,[intConcurrencyId]				= 1
+WHERE NOT EXISTS (SELECT TOP 1 1 FROM dbo.[tblCMBankTransactionType] WHERE [intBankTransactionTypeId] = 1)
+	
+UNION ALL 
+SELECT 
+	[intBankTransactionTypeId]		= 2
+	,[strBankTransactionTypeName]	= 'Bank Withdrawal'
+	,[intConcurrencyId]				= 1
+WHERE NOT EXISTS (SELECT TOP 1 1 FROM dbo.[tblCMBankTransactionType] WHERE [intBankTransactionTypeId] = 2)
+	
+UNION ALL 
+SELECT 
+	[intBankTransactionTypeId]		= 3
+	,[strBankTransactionTypeName]	= 'Misc Checks'
+	,[intConcurrencyId]				= 1
+WHERE NOT EXISTS (SELECT TOP 1 1 FROM dbo.[tblCMBankTransactionType] WHERE [intBankTransactionTypeId] = 3)
+	
+UNION ALL 
+SELECT 
+	[intBankTransactionTypeId]		= 4
+	,[strBankTransactionTypeName]	= 'Bank Transfer'
+	,[intConcurrencyId]				= 1
+WHERE NOT EXISTS (SELECT TOP 1 1 FROM dbo.[tblCMBankTransactionType] WHERE [intBankTransactionTypeId] = 4)
+
+UNION ALL 
+SELECT 
+	[intBankTransactionTypeId]		= 5
+	,[strBankTransactionTypeName]	= 'Bank Transaction'
+	,[intConcurrencyId]				= 1
+WHERE NOT EXISTS (SELECT TOP 1 1 FROM dbo.[tblCMBankTransactionType] WHERE [intBankTransactionTypeId] = 5)
+	
+UNION ALL 
+SELECT 
+	[intBankTransactionTypeId]		= 6
+	,[strBankTransactionTypeName]	= 'Credit Card Charge'
+	,[intConcurrencyId]				= 1
+WHERE NOT EXISTS (SELECT TOP 1 1 FROM dbo.[tblCMBankTransactionType] WHERE [intBankTransactionTypeId] = 6)
+	
+UNION ALL 
+SELECT 
+	[intBankTransactionTypeId]		= 7
+	,[strBankTransactionTypeName]	= 'Credit Card Returns'
+	,[intConcurrencyId]				= 1
+WHERE NOT EXISTS (SELECT TOP 1 1 FROM dbo.[tblCMBankTransactionType] WHERE [intBankTransactionTypeId] = 7)
+	
+UNION ALL 
+SELECT 
+	[intBankTransactionTypeId]		= 8
+	,[strBankTransactionTypeName]	= 'Credit Card Payments'
+	,[intConcurrencyId]				= 1
+WHERE NOT EXISTS (SELECT TOP 1 1 FROM dbo.[tblCMBankTransactionType] WHERE [intBankTransactionTypeId] = 8)	
+	
+UNION ALL 
+SELECT 
+	[intBankTransactionTypeId]		= 9
+	,[strBankTransactionTypeName]	= 'Bank Transfer (WD)'
+	,[intConcurrencyId]				= 1
+WHERE NOT EXISTS (SELECT TOP 1 1 FROM dbo.[tblCMBankTransactionType] WHERE [intBankTransactionTypeId] = 9)	
+	
+UNION ALL 
+SELECT 
+	[intBankTransactionTypeId]		= 10
+	,[strBankTransactionTypeName]	= 'Bank Transfer (DEP)'
+	,[intConcurrencyId]				= 1
+WHERE NOT EXISTS (SELECT TOP 1 1 FROM dbo.[tblCMBankTransactionType] WHERE [intBankTransactionTypeId] = 10)		
+	
+UNION ALL 
+SELECT 
+	[intBankTransactionTypeId]		= 11
+	,[strBankTransactionTypeName]	= 'Origin Deposit'
+	,[intConcurrencyId]				= 1	
+WHERE NOT EXISTS (SELECT TOP 1 1 FROM dbo.[tblCMBankTransactionType] WHERE [intBankTransactionTypeId] = 11)	
+	
+UNION ALL 
+SELECT 
+	[intBankTransactionTypeId]		= 12
+	,[strBankTransactionTypeName]	= 'Origin Checks'
+	,[intConcurrencyId]				= 1	
+WHERE NOT EXISTS (SELECT TOP 1 1 FROM dbo.[tblCMBankTransactionType] WHERE [intBankTransactionTypeId] = 12)	
+UNION ALL 
+SELECT 
+	[intBankTransactionTypeId]		= 13
+	,[strBankTransactionTypeName]	= 'Origin EFT'
+	,[intConcurrencyId]				= 1			
+WHERE NOT EXISTS (SELECT TOP 1 1 FROM dbo.[tblCMBankTransactionType] WHERE [intBankTransactionTypeId] = 13)
+UNION ALL 
+SELECT 
+	[intBankTransactionTypeId]		= 14
+	,[strBankTransactionTypeName]	= 'Origin Withdrawal'
+	,[intConcurrencyId]				= 1			
+WHERE NOT EXISTS (SELECT TOP 1 1 FROM dbo.[tblCMBankTransactionType] WHERE [intBankTransactionTypeId] = 14)
+UNION ALL 
+SELECT 
+	[intBankTransactionTypeId]		= 15
+	,[strBankTransactionTypeName]	= 'Origin Wire'
+	,[intConcurrencyId]				= 1			
+WHERE NOT EXISTS (SELECT TOP 1 1 FROM dbo.[tblCMBankTransactionType] WHERE [intBankTransactionTypeId] = 15)
+UNION ALL 
+SELECT 
+	[intBankTransactionTypeId]		= 16
+	,[strBankTransactionTypeName]	= 'AP Payment'
+	,[intConcurrencyId]				= 1			
+WHERE NOT EXISTS (SELECT TOP 1 1 FROM dbo.[tblCMBankTransactionType] WHERE [intBankTransactionTypeId] = 16)
+UNION ALL 
+SELECT 
+	[intBankTransactionTypeId]		= 17
+	,[strBankTransactionTypeName]	= 'Bank Stmt Import'
+	,[intConcurrencyId]				= 1			
+WHERE NOT EXISTS (SELECT TOP 1 1 FROM dbo.[tblCMBankTransactionType] WHERE [intBankTransactionTypeId] = 17)
+UNION ALL 
+SELECT 
+	[intBankTransactionTypeId]		= 18
+	,[strBankTransactionTypeName]	= 'AR Payment'
+	,[intConcurrencyId]				= 1			
+WHERE NOT EXISTS (SELECT TOP 1 1 FROM dbo.[tblCMBankTransactionType] WHERE [intBankTransactionTypeId] = 18)
+UNION ALL 
+SELECT 
+	[intBankTransactionTypeId]		= 19
+	,[strBankTransactionTypeName]	= 'Void Check'
+	,[intConcurrencyId]				= 1			
+WHERE NOT EXISTS (SELECT TOP 1 1 FROM dbo.[tblCMBankTransactionType] WHERE [intBankTransactionTypeId] = 19)
+UNION ALL
+SELECT 
+	[intBankTransactionTypeId]		= 20
+	,[strBankTransactionTypeName]	= 'AP eCheck'
+	,[intConcurrencyId]				= 1			
+WHERE NOT EXISTS (SELECT TOP 1 1 FROM dbo.[tblCMBankTransactionType] WHERE [intBankTransactionTypeId] = 20)
+UNION ALL
+SELECT 
+	[intBankTransactionTypeId]		= 21
+	,[strBankTransactionTypeName]	= 'Paycheck'
+	,[intConcurrencyId]				= 1			
+WHERE NOT EXISTS (SELECT TOP 1 1 FROM dbo.[tblCMBankTransactionType] WHERE [intBankTransactionTypeId] = 21)
+UNION ALL
+SELECT 
+	[intBankTransactionTypeId]		= 22
+	,[strBankTransactionTypeName]	= 'ACH'
+	,[intConcurrencyId]				= 1			
+WHERE NOT EXISTS (SELECT TOP 1 1 FROM dbo.[tblCMBankTransactionType] WHERE [intBankTransactionTypeId] = 22)
+UNION ALL
+SELECT 
+	[intBankTransactionTypeId]		= 23
+	,[strBankTransactionTypeName]	= 'Direct Deposit'
+	,[intConcurrencyId]				= 1			
+WHERE NOT EXISTS (SELECT TOP 1 1 FROM dbo.[tblCMBankTransactionType] WHERE [intBankTransactionTypeId] = 23)
+UNION ALL
+SELECT 
+	[intBankTransactionTypeId]		= 24
+	,[strBankTransactionTypeName]	= 'Write Off'
+	,[intConcurrencyId]				= 1			
+WHERE NOT EXISTS (SELECT TOP 1 1 FROM dbo.[tblCMBankTransactionType] WHERE [intBankTransactionTypeId] = 24)
+UNION ALL
+SELECT 
+	[intBankTransactionTypeId]		= 103
+	,[strBankTransactionTypeName]	= 'Void Misc Check'
+	,[intConcurrencyId]				= 1
+WHERE NOT EXISTS (SELECT TOP 1 1 FROM dbo.[tblCMBankTransactionType] WHERE [intBankTransactionTypeId] = 103)
+UNION ALL 
+SELECT 
+	[intBankTransactionTypeId]		= 116
+	,[strBankTransactionTypeName]	= 'Void AP Payment'
+	,[intConcurrencyId]				= 1			
+WHERE NOT EXISTS (SELECT TOP 1 1 FROM dbo.[tblCMBankTransactionType] WHERE [intBankTransactionTypeId] = 116)
+UNION ALL 
+SELECT 
+	[intBankTransactionTypeId]		= 121
+	,[strBankTransactionTypeName]	= 'Void Paycheck'
+	,[intConcurrencyId]				= 1			
+WHERE NOT EXISTS (SELECT TOP 1 1 FROM dbo.[tblCMBankTransactionType] WHERE [intBankTransactionTypeId] = 121)
+UNION ALL 
+SELECT 
+	[intBankTransactionTypeId]		= 122
+	,[strBankTransactionTypeName]	= 'Void ACH'
+	,[intConcurrencyId]				= 1			
+WHERE NOT EXISTS (SELECT TOP 1 1 FROM dbo.[tblCMBankTransactionType] WHERE [intBankTransactionTypeId] = 122)
+UNION ALL 
+SELECT 
+	[intBankTransactionTypeId]		= 123
+	,[strBankTransactionTypeName]	= 'Void Direct Deposit'
+	,[intConcurrencyId]				= 1			
+WHERE NOT EXISTS (SELECT TOP 1 1 FROM dbo.[tblCMBankTransactionType] WHERE [intBankTransactionTypeId] = 123)
+
+print('/*******************  END Populate Bank Transaction Types *******************/')
