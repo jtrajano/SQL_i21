af0d981c151816b563faeb7f219a0c429816b698
Jeff Trajano <jeffrey.trajano@irely.com>
2019-01-22 13:58:17 +0800

    GL-6875

--- i21Database/dbo/Tables/tblCMBankTransactionDetail.sql
+++ i21Database/dbo/Tables/tblCMBankTransactionDetail.sql
@@ -20,8 +20,7 @@
 	[intCurrencyExchangeRateTypeId] INT NULL,
     CONSTRAINT [PK_tblCMBankTransactionDetail] PRIMARY KEY CLUSTERED ([intTransactionDetailId] ASC),
     CONSTRAINT [FK_tblCMBankTransactiontblCMBankTransactionDetail] FOREIGN KEY ([intTransactionId]) REFERENCES [dbo].[tblCMBankTransaction] ([intTransactionId]) ON DELETE CASCADE,
-	CONSTRAINT [FK_tblGLAccounttblCMBankTransactionDetail] FOREIGN KEY ([intGLAccountId]) REFERENCES [dbo].[tblGLAccount] ([intAccountId]),
-    CONSTRAINT [FK_tblCMBankTransactionDetail_tblCMUndepositedFund] FOREIGN KEY ([intUndepositedFundId])  REFERENCES [dbo].[tblCMUndepositedFund] ([intUndepositedFundId])
+	CONSTRAINT [FK_tblGLAccounttblCMBankTransactionDetail] FOREIGN KEY ([intGLAccountId]) REFERENCES [dbo].[tblGLAccount] ([intAccountId])
 );
 
 
