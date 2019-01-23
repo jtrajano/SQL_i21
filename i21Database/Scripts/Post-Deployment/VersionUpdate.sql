ceda3a90c6e33e7d59a7b699d1f7908caba8efc4
Dan Jalimao <dan.jalimao@irely.com>
2019-01-23 14:34:22 +0800

    SM-4546

--- i21Database/Scripts/Post-Deployment/VersionUpdate.sql
+++ i21Database/Scripts/Post-Deployment/VersionUpdate.sql
@@ -2,7 +2,7 @@
 	PRINT N'BEGIN INSERT VERSION UPDATE'
 GO
 	INSERT INTO tblSMBuildNumber (strVersionNo, dtmLastUpdate)
-	SELECT '18.3', getdate()
+	SELECT '19.1', getdate()
 GO
 	PRINT N'END INSERT VERSION UPDATE'
 GO
