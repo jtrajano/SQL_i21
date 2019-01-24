23eb8cdad5146233ae74a3a18e369468ff830bb8
Jeff Trajano <jeffrey.trajano@irely.com>
2019-01-24 15:54:29 +0800

    GL-6882 Trial balance under G/L not updating correctly

--- i21Database/dbo/Stored Procedures/uspGLUpdateTrialBalance.sql
+++ i21Database/dbo/Stored Procedures/uspGLUpdateTrialBalance.sql
@@ -1,26 +1,46 @@
 ﻿CREATE PROCEDURE uspGLUpdateTrialBalance 
 	@GLEntries RecapTableType READONLY
 AS
-DECLARE @intGLFiscalYearPeriodId INT, @intAccountId INT,@MTDBalance numeric(38,6), @YTDBalance numeric(38,6)
+DECLARE @intGLFiscalYearPeriodId INT, @intAccountId INT,@MTDBalance NUMERIC(38,6), @YTDBalance NUMERIC(38,6)
 
 DECLARE @temp TABLE
 (
-  intAccountId int, 
-  dblAmount numeric(18,6),
+  intAccountId INT, 
+  dblAmount NUMERIC(18,6),
   intGLFiscalYearPeriodId int
 )
-INSERT INTO @temp
-select intAccountId, sum(dblDebit-dblCredit) dblAmount , F.intGLFiscalYearPeriodId  FROM  @GLEntries
-JOIN tblGLFiscalYearPeriod F on dtmDate between F.dtmStartDate and F.dtmEndDate
-GROUP BY intAccountId, intGLFiscalYearPeriodId
+DECLARE @dtmDate DATETIME = GETDATE();
 
+;WITH CTE AS(
+	SELECT intAccountId, 
+	sum(dblDebit-dblCredit) dblAmount,
+	F.intGLFiscalYearPeriodId  intGLFiscalYearPeriodId
+	FROM  @GLEntries,  tblGLFiscalYearPeriod F 
+	JOIN tblGLFiscalYear FY ON FY.intFiscalYearId = F.intFiscalYearId
+	where dtmDate < F.dtmEndDate
+	GROUP BY intAccountId, intGLFiscalYearPeriodId
+)
+UPDATE TB SET 
+	YTDBalance= TB.YTDBalance + dblAmount,
+	dtmDateModified = @dtmDate,
+	intConcurrencyId = ISNULL(intConcurrencyId,0) + 1
+FROM tblGLTrialBalance TB 
+JOIN CTE C ON C.intAccountId = TB.intAccountId 
+AND C.intGLFiscalYearPeriodId = TB.intGLFiscalYearPeriodId
 
-UPDATE tblGLTrialBalance set 
-	MTDBalance = MTDBalance + T.dblAmount, 
-	YTDBalance= YTDBalance + T.dblAmount,
-	dtmDateModified = GETDATE(), 
-	intConcurrencyId = ISNULL(intConcurrencyId,0) +1
-FROM tblGLTrialBalance TB JOIN
-@temp T ON T.intAccountId = TB.intAccountId AND T.intGLFiscalYearPeriodId = TB.intGLFiscalYearPeriodId
-
+;WITH CTE AS(
+	select intAccountId, 
+	sum(dblDebit-dblCredit) dblAmount,
+	F.intGLFiscalYearPeriodId  intGLFiscalYearPeriodId
+	FROM  @GLEntries G JOIN  tblGLFiscalYearPeriod F ON
+	dtmDate BETWEEN F.dtmStartDate AND F.dtmEndDate
+	GROUP BY intAccountId, intGLFiscalYearPeriodId
+)
+UPDATE TB SET 
+	MTDBalance= TB.MTDBalance + dblAmount,
+	dtmDateModified = @dtmDate,
+	intConcurrencyId = ISNULL(intConcurrencyId,0) + CASE WHEN dtmDateModified = @dtmDate THEN 0 ELSE  1 END
+FROM tblGLTrialBalance TB 
+JOIN CTE C ON C.intAccountId = TB.intAccountId 
+AND C.intGLFiscalYearPeriodId = TB.intGLFiscalYearPeriodId
 GO
