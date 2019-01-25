404c866332edd56ae14185a9bb856d124acb703f
Vinoth Kumar T <vinoth.t@irely.com>
2019-01-25 17:40:04 +0530

    MFG-3924, MFG-3925

--- i21Database/dbo/Views/vyuMFGetInventoryByParentLot.sql
+++ i21Database/dbo/Views/vyuMFGetInventoryByParentLot.sql
@@ -1,18 +1,134 @@
-﻿CREATE VIEW [dbo].[vyuMFGetInventoryByParentLot]
-	AS 
-Select 
-v.intParentLotId,
-v.strParentLotNumber,
-v.strItemNo,
-v.strItemDescription AS strDescription,
-SUM(ISNULL(v.dblQty,0)) AS dblQty,
-MAX(v.strQtyUOM) AS strQtyUOM,
-SUM(ISNULL(v.dblWeight,0)) AS dblWeight,
-MAX(v.strWeightUOM) AS strWeightUOM,
-v.intLocationId,
-v.strCompanyLocationName AS strLocationName,
-v.strSubLocationName,
-v.intSubLocationId
-FROM vyuMFInventoryView v 
-Where v.dblQty>0 AND v.intParentLotId>0
-Group By v.intParentLotId,v.intLocationId,v.strItemNo,v.strItemDescription,strParentLotNumber,v.strCompanyLocationName,v.strSubLocationName,v.intSubLocationId
+﻿CREATE VIEW vyuMFGetInventoryByParentLot
+AS
+SELECT CAST(ROW_NUMBER() OVER (
+			ORDER BY V.intParentLotId
+				,V.intItemId
+			) AS INT) intKey
+	,V.dblLastCost
+	,MIN(V.dtmDateCreated) AS dtmDateCreated
+	,V.dtmExpiryDate
+	,V.strLotAlias
+	,SUM(ISNULL(V.dblQty, 0)) AS dblQty
+	,V.dblWeightPerQty
+	,V.strVendorLotNo
+	,V.intItemId
+	,V.intLocationId
+	,V.intItemUOMId
+	,V.intSubLocationId
+	,V.intStorageLocationId
+	,V.intLotStatusId
+	,V.intParentLotId
+	,SUM(ISNULL(V.dblWeight, 0)) AS dblWeight
+	,V.intWeightUOMId
+	,V.strBOLNo
+	,V.strVessel
+	,V.strMarkings
+	,V.strNotes
+	,V.strGarden
+	,V.strContractNo
+	,V.dtmManufacturedDate
+	,V.strOwnershipType
+	--,V.intConcurrencyId
+	,V.strItemNo
+	,V.strItemDescription
+	,V.strItemType
+	,V.strItemCategory
+	,V.intCategoryId
+	,V.strVendor
+	,V.strPrimaryStatus
+	,V.strSecondaryStatus
+	,V.strUserName
+	,V.strQtyUOM
+	,V.strSubLocationName
+	,V.intCompanyLocationSubLocationId
+	,V.strStorageLocationName
+	,V.strCompanyLocationName
+	,V.intCompanyLocationId
+	,V.strWeightUOM
+	,V.strParentLotNumber
+	,V.strOwner
+	,V.intEntityId
+	,V.strCurrency
+	,V.strCostUOM
+	,V.strContainerNo
+	,SUM(ISNULL(V.dblReservedQty, 0)) AS dblReservedQty
+	,SUM(ISNULL(V.dblReservedNoOfPacks, 0)) AS dblReservedNoOfPacks
+	,SUM(ISNULL(V.dblAvailableQty, 0)) AS dblAvailableQty
+	,SUM(ISNULL(V.dblAvailableNoOfPacks, 0)) AS dblAvailableNoOfPacks
+	,V.strReservedQtyUOM
+	,V.strGrade
+	,V.strRestrictionType
+	,V.strBondStatus
+	,V.strVendorRefNo
+	,V.strWarehouseRefNo
+	,V.strReceiptNo
+	,V.dtmReceiptDate
+	,V.intUnitPallet
+	,MIN(V.intAge) AS intAge
+	,MIN(V.intRemainingLife) AS intRemainingLife
+	,MAX(V.dtmLastMoveDate) AS dtmLastMoveDate
+	,V.intCropYear
+	,V.strProducer
+	,V.strCertification
+	,V.strCertificationId
+	,V.strTrackingNumber
+FROM dbo.vyuMFInventoryView V
+WHERE V.dblQty > 0
+GROUP BY V.dblLastCost
+	,V.dtmExpiryDate
+	,V.strLotAlias
+	,V.dblWeightPerQty
+	,V.strVendorLotNo
+	,V.intItemId
+	,V.intLocationId
+	,V.intItemUOMId
+	,V.intSubLocationId
+	,V.intStorageLocationId
+	,V.intLotStatusId
+	,V.intParentLotId
+	,V.intWeightUOMId
+	,V.strBOLNo
+	,V.strVessel
+	,V.strMarkings
+	,V.strNotes
+	,V.strGarden
+	,V.strContractNo
+	,V.dtmManufacturedDate
+	,V.strOwnershipType
+	--,V.intConcurrencyId
+	,V.strItemNo
+	,V.strItemDescription
+	,V.strItemType
+	,V.strItemCategory
+	,V.intCategoryId
+	,V.strVendor
+	,V.strPrimaryStatus
+	,V.strSecondaryStatus
+	,V.strUserName
+	,V.strQtyUOM
+	,V.strSubLocationName
+	,V.intCompanyLocationSubLocationId
+	,V.strStorageLocationName
+	,V.strCompanyLocationName
+	,V.intCompanyLocationId
+	,V.strWeightUOM
+	,V.strParentLotNumber
+	,V.strOwner
+	,V.intEntityId
+	,V.strCurrency
+	,V.strCostUOM
+	,V.strContainerNo
+	,V.strReservedQtyUOM
+	,V.strGrade
+	,V.strRestrictionType
+	,V.strBondStatus
+	,V.strVendorRefNo
+	,V.strWarehouseRefNo
+	,V.strReceiptNo
+	,V.dtmReceiptDate
+	,V.intUnitPallet
+	,V.intCropYear
+	,V.strProducer
+	,V.strCertification
+	,V.strCertificationId
+	,V.strTrackingNumber
