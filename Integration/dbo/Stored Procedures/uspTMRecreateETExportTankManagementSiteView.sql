3184db7a6e94f47dc4c9cc88d515642999795d2e
Kenneth Lew Lagunero <kenneth.lagunero@irely.com>
2019-01-31 16:28:11 +0800

    TM-3001

--- Integration/dbo/Stored Procedures/uspTMRecreateETExportTankManagementSiteView.sql
+++ Integration/dbo/Stored Procedures/uspTMRecreateETExportTankManagementSiteView.sql
@@ -213,7 +213,7 @@ BEGIN
 					,ISNULL(RTRIM(LTRIM(F.strItemNo)),'''') Product
 					,CASE ISNULL(A.ysnTaxable,0) WHEN 1 THEN ''Yes'' ELSE ''No'' END  COLLATE Latin1_General_CI_AS  SalesTax
 					,ISNULL(LEFT(G.strTaxGroup,2),'''') TaxStateID
-					,ISNULL(G.intTaxGroupId,'''')  COLLATE Latin1_General_CI_AS TaxLocale1
+					,ISNULL(G.intTaxGroupId,'''')  TaxLocale1
 					,''''  COLLATE Latin1_General_CI_AS TaxLocale2
 					,CASE A.intDeliveryTermID WHEN NULL THEN
 							ISNULL(C.strTermCode,'''')
