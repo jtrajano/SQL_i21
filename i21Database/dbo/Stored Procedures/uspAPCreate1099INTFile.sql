7ee3f2ea1c3cc6885e0fc5fa5fe7ff3fb0013e63
Erick <erick.delacruz@irely.com>
2019-02-01 21:09:47 +0800

    AP-6805

--- i21Database/dbo/Stored Procedures/uspAPCreate1099INTFile.sql
+++ i21Database/dbo/Stored Procedures/uspAPCreate1099INTFile.sql
@@ -16,9 +16,10 @@ SET ANSI_WARNINGS OFF;
 DECLARE @transmitter AS TABLE(strTransmitter NVARCHAR(1500))
 DECLARE @payer AS TABLE(strPayer NVARCHAR(1500))
 DECLARE @payee AS TABLE(strPayee NVARCHAR(MAX))
-DECLARE @endOfINT AS TABLE(strEndOfINT NVARCHAR(1500))
+DECLARE @endOfINT AS TABLE(strEndOfINT NVARCHAR(1500), intTotalINT INT)
 DECLARE @endOfTransmitter AS TABLE(strEndOfTransmitter NVARCHAR(1500))
 DECLARE @totalPayee NVARCHAR(16)
+DECLARE @totalINT INT;
 
 
 INSERT INTO @transmitter
@@ -31,12 +32,14 @@ INSERT INTO @payee
 SELECT * FROM dbo.fnAP1099EFileINTPayee(@year, @reprint, @corrected, @vendorFrom, @vendorTo)
 
 INSERT INTO @endOfINT
-SELECT dbo.fnAP1099EFileEndOfINT(@year, @reprint, @corrected, @vendorFrom, @vendorTo)
+SELECT * FROM dbo.fnAP1099EFileEndOfINT(@year, @reprint, @corrected, @vendorFrom, @vendorTo)
+
+SET @totalINT = (SELECT intTotalINT FROM @endOfINT)
 
 INSERT INTO @endOfTransmitter
-SELECT [dbo].[fnAP1099EFileEndOfTransmitter](1)
+SELECT [dbo].[fnAP1099EFileEndOfTransmitter](@totalINT)
 
-SET @totalPayee = REPLICATE('0', 8 - LEN(CAST((SELECT COUNT(*) FROM @payee) AS NVARCHAR(100)))) + CAST((SELECT COUNT(*) FROM @payee) AS NVARCHAR(100))
+SET @totalPayee = REPLICATE('0', 8 - LEN(CAST(@totalINT AS NVARCHAR(100)))) + CAST(@totalINT AS NVARCHAR(100))
 
 UPDATE A
 	SET A.strTransmitter = STUFF(A.strTransmitter, 296, 8, @totalPayee)
@@ -48,6 +51,6 @@ SELECT * FROM @payer
 UNION ALL
 SELECT * FROM @payee
 UNION ALL
-SELECT * FROM @endOfINT
+SELECT strEndOfINT FROM @endOfINT
 UNION ALL 
 SELECT * FROM @endOfTransmitter
