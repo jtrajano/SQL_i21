593e9261aaf36b9fccd59634ceee56a5700bc839
Robin Panganiban <robin.panganiban@irely.com>
2019-02-04 14:05:56 +0800

    SC-2459

--- i21Database/dbo/Stored Procedures/uspSCAddScaleTicketToItemShipment.sql
+++ i21Database/dbo/Stored Procedures/uspSCAddScaleTicketToItemShipment.sql
@@ -1374,8 +1374,9 @@ BEGIN
 	SELECT @total = COUNT(*) FROM @ShipmentStagingTable;
 	IF (@total = 0)
 		RETURN;
+END
 
-	IF @intLotType != 0
+IF @intLotType != 0
 	BEGIN 
 		INSERT INTO @ShipmentItemLotStagingTable(
 			intOrderType
@@ -1412,7 +1413,7 @@ BEGIN
 			INNER JOIN tblSCTicket SC ON SC.intTicketId = SE.intSourceId
 			INNER JOIN tblICItem IC ON IC.intItemId = SE.intItemId
 	END
-END
+
 EXEC dbo.uspICAddItemShipment
 		@Items = @ShipmentStagingTable
 		,@Charges = @ShipmentChargeStagingTable
