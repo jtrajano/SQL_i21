77f1d738b648445c6ec3ebf7ff21819fee4f1eef
Ruben Escarcha <ruben.escarcha@irely.com>
2019-02-04 20:27:26 +0800

    AR-9781 CLONE - HDTN-224687 - Available Stock

--- i21Database/dbo/Stored Procedures/uspARPopulateItemsForCosting.sql
+++ i21Database/dbo/Stored Procedures/uspARPopulateItemsForCosting.sql
@@ -351,7 +351,7 @@ SELECT
 	,[intStorageScheduleTypeId] = ARID.[intStorageScheduleTypeId]
     ,[dblUnitRetail]			= CASE WHEN IST.ysnRetailValuation = @OneBit THEN ARID.dblPrice ELSE NULL END
 	,[intCategoryId]			= IST.[intCategoryId]
-	,[dblAdjustRetailValue]		= CASE WHEN dbo.fnGetCostingMethod(ARID.[intItemId], IST.[intItemLocationId]) = @CATEGORYCOST THEN ARID.[dblQtyShipped] * ARID.[dblPrice] ELSE NULL END
+	,[dblAdjustRetailValue]		= CASE WHEN dbo.fnGetCostingMethod(ARIC.[intComponentItemId], IST.[intItemLocationId]) = @CATEGORYCOST THEN (ARID.[dblQtyShipped] * ARIC.[dblQuantity]) * ARID.[dblPrice] ELSE NULL END
 	,[ysnForValidation]         = NULL
 FROM
 	(SELECT [intComponentItemId], [intItemUnitMeasureId], [intCompanyLocationId],[dblQuantity], [intItemId], [strType] FROM vyuARGetItemComponents WITH (NOLOCK)) ARIC
