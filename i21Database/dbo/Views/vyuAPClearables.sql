deebd7730d089d23559cb71be2c117f7a7bec5ae
Lex Peregrino <lex.peregrino@irely.com>
2019-02-07 03:14:30 +0800

    AP-6788, AP-6586 1.0

--- i21Database/dbo/Views/vyuAPClearables.sql
+++ i21Database/dbo/Views/vyuAPClearables.sql
@@ -116,9 +116,9 @@ SELECT	DISTINCT
 			,dblAmountPaid = 0
 			,dblTotal = ISNULL(dblReceiptLineTotal + dblReceiptTax,0)
 			,dblAmountDue = ABS(ISNULL(dblItemsPayable + dblTaxesPayable,0))
-			,dblVoucherAmount = CASE 
-								WHEN (bill.ysnPosted = 1 OR bill.ysnPosted IS NULL)  AND  (dblReceiptQty - dblVoucherQty) != 0 THEN ISNULL((CASE WHEN dblVoucherLineTotal = 0 THEN totalVouchered.dblTotal ELSE  dblVoucherLineTotal + dblVoucherTax  END),0)
-								ELSE 0 END    
+			,dblVoucherAmount = 0--CASE 
+								--WHEN (bill.ysnPosted = 1 OR bill.ysnPosted IS NULL)  AND  (dblReceiptQty - dblVoucherQty) != 0 THEN ISNULL((CASE WHEN dblVoucherLineTotal = 0 THEN totalVouchered.dblTotal ELSE  dblVoucherLineTotal + dblVoucherTax  END),0)
+								--ELSE 0 END    
 			,dblWithheld = 0
 			,dblDiscount = 0 
 			,dblInterest = 0 
@@ -130,12 +130,13 @@ SELECT	DISTINCT
 			,(CASE WHEN bill.ysnPosted = 1 THEN bill.strTerm ELSE '' END) AS strTerm
 			,(SELECT TOP 1 dbo.[fnAPFormatAddress](NULL, NULL, NULL, strAddress, strCity, strState, strZip, strCountry, NULL) FROM tblSMCompanySetup) as strCompanyAddress
 			,dblQtyToReceive = dblReceiptQty
-			,dblQtyVouchered = CASE WHEN (bill.ysnPosted = 1 OR bill.ysnPosted IS NULL)  AND  (dblReceiptQty - dblVoucherQty) != 0 THEN dblVoucherQty ELSE 0 END
-			,dblQtyToVoucher = dblOpenQty
-			,dblAmountToVoucher = CASE 
-									WHEN (bill.ysnPosted = 1 OR bill.ysnPosted IS NULL) AND  (dblReceiptQty - dblVoucherQty) != 0 THEN ISNULL((dblReceiptLineTotal + dblReceiptTax)	,0) -  ISNULL((totalVouchered.dblTotal),0)
-									WHEN bill.ysnPosted = 0 AND  (dblReceiptQty - dblVoucherQty) != 0 THEN ISNULL(dblItemsPayable + dblTaxesPayable,0)
-									ELSE (dblReceiptLineTotal + dblReceiptTax)  END                                    
+			,dblQtyVouchered = 0--CASE WHEN (bill.ysnPosted = 1 OR bill.ysnPosted IS NULL)  AND  (dblReceiptQty - dblVoucherQty) != 0 THEN dblVoucherQty ELSE 0 END
+			,dblQtyToVoucher = dblReceiptQty--dblOpenQty
+			,dblAmountToVoucher = (dblReceiptLineTotal + dblReceiptTax)  
+									--CASE 
+									--WHEN (bill.ysnPosted = 1 OR bill.ysnPosted IS NULL) AND  (dblReceiptQty - dblVoucherQty) != 0 THEN ISNULL((dblReceiptLineTotal + dblReceiptTax)	,0) -  ISNULL((totalVouchered.dblTotal),0)
+									--WHEN bill.ysnPosted = 0 AND  (dblReceiptQty - dblVoucherQty) != 0 THEN ISNULL(dblItemsPayable + dblTaxesPayable,0)
+									--ELSE (dblReceiptLineTotal + dblReceiptTax)  END                                    
 			,dblChargeAmount = 0
 			,strContainer = strContainerNumber
 			,dblVoucherQty = 0
