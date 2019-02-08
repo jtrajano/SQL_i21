c3a008ecbc28e9b159fd9614cb342a8cb3f19dfb
Crisman Lazaga <cris.lazaga@irely.com>
2019-02-08 15:09:41 +0800

    CCR-183

---
+++ i21Database/dbo/Tables/tblCCImportStatus.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tblCCImportStatus]
+(
+	[intImportStatusId] INT NOT NULL IDENTITY,
+    [strImportType] NVARCHAR(200) COLLATE Latin1_General_CI_AS NOT NULL,
+    [strDescription] NVARCHAR(500) COLLATE Latin1_General_CI_AS NULL,
+    [strSource] NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL,
+    [ysnActive] BIT NOT NULL,    
+    [dtmImportDate] DATETIME NOT NULL,
+    [intUserId] INT NULL,
+    CONSTRAINT [PK_tblCCImportStatus] PRIMARY KEY ([intImportStatusId])
+)
