4d4b59de5d0d1966a5d981bbcb0c8a3bf52cf6ad
Kevin Estrada <kevin.estrada@irely.com>
2019-02-14 13:12:29 +0800

    FRM-7852

--- i21Database/dbo/Views/vyuSMAuditLogPanel.sql
+++ i21Database/dbo/Views/vyuSMAuditLogPanel.sql
@@ -1,15 +1,35 @@
 ﻿CREATE VIEW [dbo].[vyuSMAuditLogPanel]
 AS
+--SELECT 
+--intAuditLogId,
+--en.strName,
+--[dbo].[fnSMAddSpaceToTitleCase](right(strTransactionType, CHARINDEX('.', REVERSE(strTransactionType)) - 1), 0) COLLATE Latin1_General_CI_AS as strTransactionType,
+--strActionType + ' a record' COLLATE Latin1_General_CI_AS as strActionType,
+--strRecordNo
+--strDescription,
+--strRoute,
+--dtmDate,
+--strRecordNo,
+--auditLog.intEntityId
+--FROM tblSMAuditLog auditLog
+--inner join tblEMEntity en on en.intEntityId = auditLog.intEntityId
 SELECT 
-intAuditLogId,
+tblSMAudit.intAuditId as 'intAuditLogId',
 en.strName,
-[dbo].[fnSMAddSpaceToTitleCase](right(strTransactionType, CHARINDEX('.', REVERSE(strTransactionType)) - 1), 0) COLLATE Latin1_General_CI_AS as strTransactionType,
-strActionType + ' a record' COLLATE Latin1_General_CI_AS as strActionType,
-strRecordNo
+[dbo].[fnSMAddSpaceToTitleCase](right(tblSMScreen.strNamespace, CHARINDEX('.', REVERSE(tblSMScreen.strNamespace)) - 1), 0) as strTransactionType,
+tblSMAudit.strAction + ' a record' COLLATE Latin1_General_CI_AS as strActionType,
+CAST(tblSMTransaction.intRecordId AS NVARCHAR(MAX))
 strDescription,
 strRoute,
-dtmDate,
-strRecordNo,
-auditLog.intEntityId
-FROM tblSMAuditLog auditLog
-inner join tblEMEntity en on en.intEntityId = auditLog.intEntityId
+tblSMLog.dtmDate,
+CAST(tblSMTransaction.intRecordId AS NVARCHAR(MAX)) as strRecordNo,
+tblSMLog.intEntityId,
+case when tblSMTransaction.strTransactionNo is null then CAST(tblSMTransaction.intTransactionId AS NVARCHAR(MAX))
+else tblSMTransaction.strTransactionNo end as 'strReference',
+tblSMTransaction.dtmDate as 'dtmTransactionDate'
+FROM tblSMLog tblSMLog
+inner join tblSMAudit tblSMAudit on tblSMAudit.intLogId = tblSMLog.intLogId
+inner join tblEMEntity en on en.intEntityId = tblSMLog.intEntityId
+inner join tblSMTransaction tblSMTransaction on tblSMTransaction.intTransactionId = tblSMLog.intTransactionId
+inner join tblSMScreen on tblSMScreen.intScreenId = tblSMTransaction.intScreenId
+where tblSMAudit.intParentAuditId IS NULL  --parent only 
