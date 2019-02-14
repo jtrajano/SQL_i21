4dec4068165d18674c4bc497e1fb110345032c1d
Lawrence Zabala <lawrence.zabala@irely.com>
2019-02-14 13:44:09 +0800

    RM-2569

--- i21Database/dbo/Stored Procedures/uspRKUnrealizedPnL.sql
+++ i21Database/dbo/Stored Procedures/uspRKUnrealizedPnL.sql
@@ -13,16 +13,16 @@
 
 AS
 
-SET @dtmFromDate = convert(DATETIME, CONVERT(VARCHAR(10), @dtmFromDate, 110), 110)
-SET @dtmToDate = convert(DATETIME, CONVERT(VARCHAR(10), isnull(@dtmToDate, getdate()), 110), 110)
+SET @dtmFromDate = CONVERT(DATETIME, CONVERT(VARCHAR(10), @dtmFromDate, 110), 110)
+SET @dtmToDate = CONVERT(DATETIME, CONVERT(VARCHAR(10), ISNULL(@dtmToDate, GETDATE()), 110), 110)
 
 SELECT CONVERT(INT, DENSE_RANK() OVER (ORDER BY CONVERT(DATETIME, '01 ' + strFutureMonth))) RowNum
-	, strFutMarketName + ' - ' + strFutureMonth + ' - ' + strName strMonthOrder
+	, strMonthOrder = (strFutMarketName + ' - ' + strFutureMonth + ' - ' + strName)
 	, intFutOptTransactionId
 	, GrossPnL dblGrossPnL
 	, dblLong
 	, dblShort
-	, - abs(dblFutCommission) dblFutCommission
+	, dblFutCommission = - ABS(dblFutCommission)
 	, strFutMarketName
 	, strFutureMonth
 	, dtmTradeDate
@@ -41,7 +41,7 @@ SELECT CONVERT(INT, DENSE_RANK() OVER (ORDER BY CONVERT(DATETIME, '01 ' + strFut
 	, dblClosing
 	, dblPrice
 	, dblContractSize
-	, - abs(dblFutCommission1) dblFutCommission1
+	, dblFutCommission1 = - ABS(dblFutCommission1)
 	, MatchLong dblMatchLong
 	, MatchShort dblMatchShort
 	, NetPnL dblNetPnL
@@ -53,23 +53,24 @@ SELECT CONVERT(INT, DENSE_RANK() OVER (ORDER BY CONVERT(DATETIME, '01 ' + strFut
 	, ysnExpired
 	, dblVariationMargin = intNet * VM.dblVariationMargin1
 	, 0.0 dblInitialMargin
-	, LongWaitedPrice / case when isnull(dblLongTotalLotByMonth,0)=0 then 1 else dblLongTotalLotByMonth end LongWaitedPrice
-	, ShortWaitedPrice / case when isnull(dblShortTotalLotByMonth,0)=0 then 1 else dblShortTotalLotByMonth end ShortWaitedPrice
+	, LongWaitedPrice = LongWaitedPrice / CASE WHEN ISNULL(dblLongTotalLotByMonth,0)=0 THEN 1 ELSE dblLongTotalLotByMonth END
+	, ShortWaitedPrice = ShortWaitedPrice / CASE WHEN ISNULL(dblShortTotalLotByMonth,0)=0 THEN 1 ELSE dblShortTotalLotByMonth END
 FROM (
 	SELECT *
-		, (GrossPnL1 * (dblClosing - dblPrice) - dblFutCommission2) NetPnL
-		, GrossPnL1 * (dblClosing - dblPrice) GrossPnL
-		, - dblFutCommission2 dblFutCommission
-		, sum(dblShort) OVER (PARTITION BY intFutureMonthId,strName) dblShortTotalLotByMonth
-		, sum(dblLong) OVER (PARTITION BY intFutureMonthId,strName) dblLongTotalLotByMonth
-		, (dblLong*dblPrice) LongWaitedPrice,(dblShort*dblPrice) ShortWaitedPrice
+		, NetPnL = (GrossPnL1 * (dblClosing - dblPrice) - dblFutCommission2)
+		, GrossPnL = GrossPnL1 * (dblClosing - dblPrice)
+		, dblFutCommission = - dblFutCommission2
+		, dblShortTotalLotByMonth = SUM(dblShort) OVER (PARTITION BY intFutureMonthId,strName)
+		, dblLongTotalLotByMonth = SUM(dblLong) OVER (PARTITION BY intFutureMonthId,strName)
+		, LongWaitedPrice = (dblLong*dblPrice)
+		, ShortWaitedPrice = (dblShort*dblPrice)
 	FROM (
-		SELECT (convert(INT, isnull((Long1 - MatchLong), 0) - isnull(Sell1 - MatchShort, 0))) * dblContractSize / CASE WHEN ysnSubCurrency = 1 THEN intCent ELSE 1 END GrossPnL1
-			, isnull((Long1 - MatchLong), 0) AS dblLong
-			, isnull(Sell1 - MatchShort, 0) AS dblShort
-			, convert(INT, isnull((Long1 - MatchLong), 0) - isnull(Sell1 - MatchShort, 0)) * - dblFutCommission1 / CASE WHEN ComSubCurrency = 1 THEN ComCent ELSE 1 END AS dblFutCommission2
-			, convert(INT, isnull((Long1 - MatchLong), 0) - isnull(Sell1 - MatchShort, 0)) AS intNet
-			, dblLastSettle AS dblClosing
+		SELECT GrossPnL1 = (CONVERT(INT, ISNULL((Long1 - MatchLong), 0) - ISNULL(Sell1 - MatchShort, 0))) * dblContractSize / CASE WHEN ysnSubCurrency = 1 THEN intCent ELSE 1 END
+			, dblLong = ISNULL((Long1 - MatchLong), 0)
+			, dblShort = ISNULL(Sell1 - MatchShort, 0)
+			, dblFutCommission2 = CONVERT(INT, ISNULL((Long1 - MatchLong), 0) - ISNULL(Sell1 - MatchShort, 0)) * - dblFutCommission1 / CASE WHEN ComSubCurrency = 1 THEN ComCent ELSE 1 END
+			, intNet = CONVERT(INT, ISNULL((Long1 - MatchLong), 0) - ISNULL(Sell1 - MatchShort, 0))
+			, dblClosing = dblLastSettle
 			, *
 		FROM (
 			SELECT intFutOptTransactionId
@@ -78,7 +79,7 @@ FROM (
 				, ot.intFutureMonthId
 				, ot.intCommodityId
 				, ot.intFutureMarketId
-				, convert(DATETIME, CONVERT(VARCHAR(10), ot.dtmFilledDate, 110), 110) AS dtmTradeDate
+				, dtmTradeDate = CONVERT(DATETIME, CONVERT(VARCHAR(10), ot.dtmFilledDate, 110), 110)
 				, ot.strInternalTradeNo
 				, e.strName
 				, acc.strAccountNumber
@@ -88,39 +89,39 @@ FROM (
 				, icc.strCommodityCode
 				, sl.strLocationName
 				, ot.intNoOfContract AS intOriginalQty
-				, isnull(CASE WHEN ot.strBuySell = 'Buy' THEN isnull(ot.intNoOfContract, 0) ELSE NULL END, 0) Long1
-				, isnull(CASE WHEN ot.strBuySell = 'Sell' THEN isnull(ot.intNoOfContract, 0) ELSE NULL END, 0) Sell1
-				, ot.intNoOfContract AS intNet1
-				, ot.dblPrice AS dblActual
-				, isnull(ot.dblPrice, 0) dblPrice
-				, fm.dblContractSize dblContractSize
-				, 0 AS intConcurrencyId
-				, dblFutCommission1 = ISNULL((SELECT TOP 1 (CASE WHEN bc.intFuturesRateType = 1 THEN 0 ELSE isnull(bc.dblFutCommission, 0) / CASE WHEN cur.ysnSubCurrency = 1 THEN cur.intCent ELSE 1 END END)
+				, Long1 = ISNULL(CASE WHEN ot.strBuySell = 'Buy' THEN ISNULL(ot.intNoOfContract, 0) ELSE NULL END, 0)
+				, Sell1 = ISNULL(CASE WHEN ot.strBuySell = 'Sell' THEN ISNULL(ot.intNoOfContract, 0) ELSE NULL END, 0)
+				, intNet1 = ot.intNoOfContract
+				, dblActual = ot.dblPrice
+				, dblPrice = ISNULL(ot.dblPrice, 0)
+				, fm.dblContractSize
+				, intConcurrencyId = 0
+				, dblFutCommission1 = ISNULL((SELECT TOP 1 (CASE WHEN bc.intFuturesRateType = 1 THEN 0 ELSE ISNULL(bc.dblFutCommission, 0) / CASE WHEN cur.ysnSubCurrency = 1 THEN cur.intCent ELSE 1 END END)
 											FROM tblRKBrokerageCommission bc
 											LEFT JOIN tblSMCurrency cur ON cur.intCurrencyID = bc.intFutCurrencyId
 											WHERE bc.intFutureMarketId = ot.intFutureMarketId
 												AND bc.intBrokerageAccountId = ot.intBrokerageAccountId
 												AND ot.dtmTransactionDate BETWEEN bc.dtmEffectiveDate
-												AND isnull(bc.dtmEndDate,getdate())), 0)
-				, ISNULL((SELECT SUM(dblMatchQty)
-						FROM tblRKMatchFuturesPSDetail psd
-						JOIN tblRKMatchFuturesPSHeader h ON psd.intMatchFuturesPSHeaderId = h.intMatchFuturesPSHeaderId
-						WHERE psd.intLFutOptTransactionId = ot.intFutOptTransactionId
-							AND h.strType = 'Realize'
-							AND convert(DATETIME, CONVERT(VARCHAR(10), h.dtmMatchDate, 110), 110) <= @dtmToDate), 0) AS MatchLong
-				, ISNULL((SELECT sum(dblMatchQty)
-						FROM tblRKMatchFuturesPSDetail psd
-						JOIN tblRKMatchFuturesPSHeader h ON psd.intMatchFuturesPSHeaderId = h.intMatchFuturesPSHeaderId
-						WHERE psd.intSFutOptTransactionId = ot.intFutOptTransactionId
-							AND h.strType = 'Realize'
-							AND convert(DATETIME, CONVERT(VARCHAR(10), h.dtmMatchDate, 110), 110) <= @dtmToDate), 0) AS MatchShort
-				, c.intCurrencyID AS intCurrencyId
+												AND ISNULL(bc.dtmEndDate,getdate())), 0)
+				, MatchLong = ISNULL((SELECT SUM(dblMatchQty)
+									FROM tblRKMatchFuturesPSDetail psd
+									JOIN tblRKMatchFuturesPSHeader h ON psd.intMatchFuturesPSHeaderId = h.intMatchFuturesPSHeaderId
+									WHERE psd.intLFutOptTransactionId = ot.intFutOptTransactionId
+										AND h.strType = 'Realize'
+										AND CONVERT(DATETIME, CONVERT(VARCHAR(10), h.dtmMatchDate, 110), 110) <= @dtmToDate), 0)
+				, MatchShort = ISNULL((SELECT SUM(dblMatchQty)
+									FROM tblRKMatchFuturesPSDetail psd
+									JOIN tblRKMatchFuturesPSHeader h ON psd.intMatchFuturesPSHeaderId = h.intMatchFuturesPSHeaderId
+									WHERE psd.intSFutOptTransactionId = ot.intFutOptTransactionId
+										AND h.strType = 'Realize'
+										AND CONVERT(DATETIME, CONVERT(VARCHAR(10), h.dtmMatchDate, 110), 110) <= @dtmToDate), 0)
+				, intCurrencyId = c.intCurrencyID
 				, c.intCent
 				, c.ysnSubCurrency
 				, intFutOptTransactionHeaderId
 				, ysnExpired
-				, c.intCent ComCent
-				, c.ysnSubCurrency ComSubCurrency
+				, ComCent = c.intCent
+				, ComSubCurrency = c.ysnSubCurrency
 				, LS.dblLastSettle
 				, ot.dtmFilledDate
 			FROM tblRKFutOptTransaction ot
@@ -143,16 +144,16 @@ FROM (
 					AND CONVERT(Nvarchar, dtmPriceDate, 111) <= CONVERT(Nvarchar, @dtmToDate, 111)
 				ORDER BY dtmPriceDate DESC
 			) LS
-			WHERE isnull(ot.intCommodityId, 0) = CASE WHEN ISNULL(@intCommodityId, 0) = 0 THEN isnull(ot.intCommodityId, 0) ELSE @intCommodityId END
-				AND isnull(ot.intFutureMarketId, 0) = CASE WHEN ISNULL(@intFutureMarketId, 0) = 0 THEN isnull(ot.intFutureMarketId, 0) ELSE @intFutureMarketId END
-				AND isnull(ot.intBookId, 0) = CASE WHEN ISNULL(@intBookId, 0) = 0 THEN isnull(ot.intBookId, 0) ELSE @intBookId END
-				AND isnull(ot.intSubBookId, 0) = CASE WHEN ISNULL(@intSubBookId, 0) = 0 THEN isnull(ot.intSubBookId, 0) ELSE @intSubBookId END
-				AND isnull(ot.intEntityId, 0) = CASE WHEN ISNULL(@intEntityId, 0) = 0 THEN ot.intEntityId ELSE @intEntityId END
-				AND isnull(ot.intBrokerageAccountId, 0) = CASE WHEN ISNULL(@intBrokerageAccountId, 0) = 0 THEN ot.intBrokerageAccountId ELSE @intBrokerageAccountId END
-				AND isnull(ot.intFutureMonthId, 0) = CASE WHEN ISNULL(@intFutureMonthId, 0) = 0 THEN ot.intFutureMonthId ELSE @intFutureMonthId END
+			WHERE ISNULL(ot.intCommodityId, 0) = CASE WHEN ISNULL(@intCommodityId, 0) = 0 THEN ISNULL(ot.intCommodityId, 0) ELSE @intCommodityId END
+				AND ISNULL(ot.intFutureMarketId, 0) = CASE WHEN ISNULL(@intFutureMarketId, 0) = 0 THEN ISNULL(ot.intFutureMarketId, 0) ELSE @intFutureMarketId END
+				AND ISNULL(ot.intBookId, 0) = CASE WHEN ISNULL(@intBookId, 0) = 0 THEN ISNULL(ot.intBookId, 0) ELSE @intBookId END
+				AND ISNULL(ot.intSubBookId, 0) = CASE WHEN ISNULL(@intSubBookId, 0) = 0 THEN ISNULL(ot.intSubBookId, 0) ELSE @intSubBookId END
+				AND ISNULL(ot.intEntityId, 0) = CASE WHEN ISNULL(@intEntityId, 0) = 0 THEN ot.intEntityId ELSE @intEntityId END
+				AND ISNULL(ot.intBrokerageAccountId, 0) = CASE WHEN ISNULL(@intBrokerageAccountId, 0) = 0 THEN ot.intBrokerageAccountId ELSE @intBrokerageAccountId END
+				AND ISNULL(ot.intFutureMonthId, 0) = CASE WHEN ISNULL(@intFutureMonthId, 0) = 0 THEN ot.intFutureMonthId ELSE @intFutureMonthId END
 				AND ot.strBuySell = CASE WHEN ISNULL(@strBuySell, '0') = '0' THEN ot.strBuySell ELSE @strBuySell END
-				AND convert(DATETIME, CONVERT(VARCHAR(10), dtmFilledDate, 110), 110) BETWEEN @dtmFromDate AND @dtmToDate
-				AND isnull(ysnExpired, 0) = CASE WHEN isnull(@ysnExpired, 0) = 1 THEN isnull(ysnExpired, 0) ELSE @ysnExpired END
+				AND CONVERT(DATETIME, CONVERT(VARCHAR(10), dtmFilledDate, 110), 110) <= @dtmToDate
+				AND ISNULL(ysnExpired, 0) = CASE WHEN ISNULL(@ysnExpired, 0) = 1 THEN ISNULL(ysnExpired, 0) ELSE @ysnExpired END
 				AND ot.intInstrumentTypeId = 1
 			) t1
 		) t1
