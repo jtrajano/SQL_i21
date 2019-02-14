d1b87ac5ee1ac88c286f8804f9a37cabca4c922d
Erick <erick.delacruz@irely.com>
2019-02-14 16:46:01 +0800

    AP-6884

--- i21Database/dbo/Views/vyuAPRptVoucherCheckOff.sql
+++ i21Database/dbo/Views/vyuAPRptVoucherCheckOff.sql
@@ -71,7 +71,7 @@ SELECT	DISTINCT
 			INNER JOIN dbo.tblAPPaymentDetail B ON B1.intPaymentId = B.intPaymentId
 			LEFT JOIN dbo.tblCMBankTransaction C ON B1.strPaymentRecordNum = C.strTransactionId 
 			WHERE B.intBillId = APB.intBillId 
-				  AND intPaymentMethodId = 7 --WILL SHOW TRANSACTION THAT WAS PAID USING CHECK ONLY
+				--   AND intPaymentMethodId = 7 --WILL SHOW TRANSACTION THAT WAS PAID USING CHECK ONLY
 			ORDER BY dtmDatePaid DESC
 			)  Payment     
 	WHERE APB.ysnPosted = 1 
