a6ac6f5364f9ab0a98dd1fa4d6f3f32d83ab7da1
Dan Jalimao <dan.jalimao@irely.com>
2019-02-15 14:33:36 +0800

    CT-2977

--- i21Database/dbo/Stored Procedures/uspCTReportContractDetailGrain.sql
+++ i21Database/dbo/Stored Procedures/uspCTReportContractDetailGrain.sql
@@ -61,7 +61,10 @@ BEGIN TRY
 			)
 			 SELECT 
 			 strItemNo					 = strItemNo
-			,dblDetailQuantity			 = dblDetailQuantity
+			,dblDetailQuantity			 = CASE
+													WHEN strItemUOM IN ('lb', 'Metric Ton') THEN FORMAT(dblDetailQuantity,'#,0.00##')
+													ELSE FORMAT(dblDetailQuantity,'#,0')
+										   END --FORMAT(dblDetailQuantity,'#,#.00##')
 			,dblPrice					 = CASE	
 													WHEN intPricingTypeId IN (1,6)	THEN	CAST(ISNULL(dblCashPrice,0) AS DECIMAL(24,4))
 													WHEN intPricingTypeId = 2		THEN	CAST(ISNULL(dblBasis,0)		AS DECIMAL(24,4))
