1eb4644ed4d85943a3342bf3f9c1528e196f6d3c
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2019-02-18 13:05:31 +0530

    RM-2608

--- i21Database/dbo/Stored Procedures/uspRKRealizedPnL.sql
+++ i21Database/dbo/Stored Procedures/uspRKRealizedPnL.sql
@@ -1,13 +1,13 @@
 ﻿CREATE PROC uspRKRealizedPnL  
-	 @dtmFromDate DATETIME
-	,@dtmToDate DATETIME
-	,@intCommodityId INT = NULL
-	,@ysnExpired BIT
-	,@intFutureMarketId INT = NULL
-	,@intEntityId INT = NULL
-	,@intBrokerageAccountId INT = NULL
-	,@intFutureMonthId INT = NULL
-	,@strBuySell nvarchar(10)=NULL
+                 @dtmFromDate DATETIME
+                ,@dtmToDate DATETIME
+                ,@intCommodityId INT = NULL
+                ,@ysnExpired BIT
+                ,@intFutureMarketId INT = NULL
+                ,@intEntityId INT = NULL
+                ,@intBrokerageAccountId INT = NULL
+                ,@intFutureMonthId INT = NULL
+                ,@strBuySell nvarchar(10)=NULL
 AS  
 SET @dtmFromDate = convert(DATETIME, CONVERT(VARCHAR(10), @dtmFromDate, 110), 110)
 SET @dtmToDate = convert(DATETIME, CONVERT(VARCHAR(10), @dtmToDate, 110), 110)
@@ -38,8 +38,8 @@ strCommodityCode,
 strLocationName,
 intFutureMarketId ,
 intCommodityId ,
-ysnExpired ,intFutureMonthId
- from (
+ysnExpired ,intFutureMonthId,strLInternalTradeNo,strSInternalTradeNo,strLRollingMonth,strSRollingMonth,intLFutOptTransactionHeaderId,intSFutOptTransactionHeaderId
+from (
 SELECT * FROM(  
 SELECT   
 ((dblSPrice - dblLPrice)*dblMatchQty*dblContractSize) as dblGrossPL1,((dblSPrice - dblLPrice)*dblMatchQty*dblContractSize)/ case when ysnSubCurrency = 'true' then intCent else 1 end as dblGrossPL,* FROM  
@@ -58,7 +58,7 @@ SELECT psh.intMatchFuturesPSHeaderId,
     ot1.strInternalTradeNo strSBrokerTradeNo,  
     fm.dblContractSize dblContractSize,0 as intConcurrencyId,  
     --CASE WHEN bc.intFuturesRateType= 2 then isnull(bc.dblFutCommission,0)* isnull(psd.dblMatchQty,0)*2 else  isnull(bc.dblFutCommission,0)* isnull(psd.dblMatchQty,0) end as dblFutCommission1,  
-	psd.dblFutCommission,
+                psd.dblFutCommission,
     fm.strFutMarketName,  
     om.strFutureMonth,  
     psh.intMatchNo,  
@@ -67,7 +67,13 @@ SELECT psh.intMatchFuturesPSHeaderId,
     acc.strAccountNumber,  
     icc.strCommodityCode,  
     sl.strLocationName,ot.intFutureMonthId,ot.intCommodityId,ot.intFutureMarketId,
-	c.intCurrencyID as intCurrencyId,c.intCent,c.ysnSubCurrency,ysnExpired,c.intCent ComCent,c.ysnSubCurrency ComSubCurrency                  
+                c.intCurrencyID as intCurrencyId,c.intCent,c.ysnSubCurrency,ysnExpired,c.intCent ComCent,c.ysnSubCurrency ComSubCurrency, 
+                ot.strInternalTradeNo strLInternalTradeNo,  
+    ot1.strInternalTradeNo strSInternalTradeNo,   
+                (select strFutureMonth from tblRKFuturesMonth fm where fm.intFutureMonthId=ot.intRollingMonthId)   strLRollingMonth,   
+                (select strFutureMonth from tblRKFuturesMonth fm where fm.intFutureMonthId=ot1.intRollingMonthId)   strSRollingMonth   ,
+                ot.intFutOptTransactionHeaderId intLFutOptTransactionHeaderId,  
+    ot1.intFutOptTransactionHeaderId intSFutOptTransactionHeaderId                   
  FROM tblRKMatchFuturesPSHeader psh  
  JOIN tblRKMatchFuturesPSDetail psd on psd.intMatchFuturesPSHeaderId=psh.intMatchFuturesPSHeaderId   
  JOIN tblRKFutOptTransaction ot on psd.intLFutOptTransactionId= ot.intFutOptTransactionId  
@@ -78,15 +84,15 @@ SELECT psh.intMatchFuturesPSHeaderId,
  JOIN tblEMEntity e on e.intEntityId=ot.intEntityId  
  JOIN tblRKFutureMarket fm on ot.intFutureMarketId=fm.intFutureMarketId  
  JOIN tblSMCurrency c on c.intCurrencyID=fm.intCurrencyId
- JOIN tblRKFutOptTransaction ot1 on psd.intSFutOptTransactionId= ot1.intFutOptTransactionId  
+JOIN tblRKFutOptTransaction ot1 on psd.intSFutOptTransactionId= ot1.intFutOptTransactionId  
  WHERE ot.intCommodityId= CASE WHEN ISNULL(@intCommodityId,0)=0 then ot.intCommodityId else @intCommodityId end
-	AND ot.intFutureMarketId= CASE WHEN ISNULL(@intFutureMarketId,0)=0 then ot.intFutureMarketId else @intFutureMarketId end
-	AND ot.intEntityId= CASE WHEN ISNULL(@intEntityId,0)=0 then ot.intEntityId else @intEntityId end
-	AND ot.intBrokerageAccountId= CASE WHEN ISNULL(@intBrokerageAccountId,0)=0 then ot.intBrokerageAccountId else @intBrokerageAccountId end
-	AND ot.intFutureMonthId= CASE WHEN ISNULL(@intFutureMonthId,0)=0 then ot.intFutureMonthId else @intFutureMonthId end
-	AND ot.strBuySell= CASE WHEN ISNULL(@strBuySell,'0')= '0' then ot.strBuySell else @strBuySell end
-	AND CONVERT(DATETIME,CONVERT(VARCHAR(10),psh.dtmMatchDate,110),110) BETWEEN @dtmFromDate AND @dtmToDate
-	AND isnull(ysnExpired,0) = case when isnull(@ysnExpired,'false')= 'true' then isnull(ysnExpired,0) else @ysnExpired end
-	AND ot.intInstrumentTypeId =1
+                AND ot.intFutureMarketId= CASE WHEN ISNULL(@intFutureMarketId,0)=0 then ot.intFutureMarketId else @intFutureMarketId end
+                AND ot.intEntityId= CASE WHEN ISNULL(@intEntityId,0)=0 then ot.intEntityId else @intEntityId end
+                AND ot.intBrokerageAccountId= CASE WHEN ISNULL(@intBrokerageAccountId,0)=0 then ot.intBrokerageAccountId else @intBrokerageAccountId end
+                AND ot.intFutureMonthId= CASE WHEN ISNULL(@intFutureMonthId,0)=0 then ot.intFutureMonthId else @intFutureMonthId end
+                AND ot.strBuySell= CASE WHEN ISNULL(@strBuySell,'0')= '0' then ot.strBuySell else @strBuySell end
+                AND CONVERT(DATETIME,CONVERT(VARCHAR(10),psh.dtmMatchDate,110),110) BETWEEN @dtmFromDate AND @dtmToDate
+                AND isnull(ysnExpired,0) = case when isnull(@ysnExpired,'false')= 'true' then isnull(ysnExpired,0) else @ysnExpired end
+                AND ot.intInstrumentTypeId =1
   )t)t1
   )t  ORDER BY RowNum ASC
