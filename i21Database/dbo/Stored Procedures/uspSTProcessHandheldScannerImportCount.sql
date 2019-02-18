6e9aa181188d5cb8c050110bb8f66f93932ffafb
COMTECH\HZaragoza <henry.zaragoza@irely.com>
2019-02-18 15:03:11 +0800

    ST-1169 Scanner > Append Handheld Scanner Counts for Existing Inventory

--- i21Database/dbo/Stored Procedures/uspSTProcessHandheldScannerImportCount.sql
+++ i21Database/dbo/Stored Procedures/uspSTProcessHandheldScannerImportCount.sql
@@ -39,10 +39,29 @@ BEGIN TRY
 	-------------------- End Validate if has record to Process ---------------------------
 	--------------------------------------------------------------------------------------
 
-	SELECT *
+	--SELECT *
+	--INTO #ImportCounts
+	--FROM vyuSTGetHandheldScannerImportCount ImportCount
+	--WHERE ImportCount.intHandheldScannerId = @HandheldScannerId
+
+	SELECT intHandheldScannerId
+		   , intStoreId
+		   , intStoreNo
+		   , intCompanyLocationId 
+		   , intUnitMeasureId
+		   , strUPCNo
+		   , intItemId
+		   , strItemNo
+		   , strDescription
+		   , intItemUOMId
+		   , strUnitMeasure
+		   , intItemLocationId
+		   , SUM(dblComputedCount) AS dblComputedCount
 	INTO #ImportCounts
-	FROM vyuSTGetHandheldScannerImportCount ImportCount
-	WHERE ImportCount.intHandheldScannerId = @HandheldScannerId
+	FROM vyuSTGetHandheldScannerImportCountWithRecipe
+	WHERE intHandheldScannerId = @HandheldScannerId
+	GROUP BY intHandheldScannerId, intStoreId, intStoreNo, intCompanyLocationId, intUnitMeasureId, strUPCNo, intItemId, strItemNo, strDescription, intItemUOMId, strUnitMeasure, intItemLocationId
+
 
 	DECLARE @NewId INT,
 		--@CountDate DATETIME = @dtmCountDate,
@@ -56,7 +75,7 @@ BEGIN TRY
 		,dblPhysicalCount)
 	SELECT intItemId
 		, intItemUOMId
-		, dblCountQty
+		, dblComputedCount
 	FROM #ImportCounts
 
 	SET @strStatusMsg = ''
@@ -145,12 +164,11 @@ BEGIN TRY
 				BEGIN
 					-- Get values
 					SELECT TOP 1 
-						@intPrimaryCountId = Imports.intHandheldScannerImportCountId
-						, @dblCountQty = Imports.dblCountQty
-						, @intItemId = Imports.intItemId
-						, @intItemLocationId = IL.intItemLocationId
+						@intItemId = Imports.intItemId
 						, @intItemUOMId = Imports.intItemUOMId
+						, @intItemLocationId = IL.intItemLocationId
 						, @intCompanyLocationId = Store.intCompanyLocationId
+						, @dblCountQty = Imports.dblComputedCount
 					FROM #ImportCounts Imports
 					INNER JOIN tblSTStore Store
 						ON Imports.intStoreId = Store.intStoreId
@@ -163,8 +181,8 @@ BEGIN TRY
 					BEGIN TRY
 						EXEC uspICUpdateInventoryPhysicalCount
 							-- Count No and Physical Count are required
-							@strCountNo = @strCountNo,
-							@dblPhysicalCount = @dblCountQty,
+							@strCountNo			= @strCountNo,
+							@dblPhysicalCount	= @dblCountQty,
 
 							-- ========================================
 							--    Required for a lotted item
@@ -173,20 +191,20 @@ BEGIN TRY
 							@intLotId = NULL,
 
 							-- This is also required
-							@intUserSecurityId = @UserId,
+							@intUserSecurityId	= @UserId,
 
 							-- ========================================
 							--    Parameters for a non-lotted item
 							-- ========================================
 							-- Required for a non-lotted item
-							@intItemId = @intItemId,
-							@intItemLocationId = @intItemLocationId,
+							@intItemId			= @intItemId,
+							@intItemLocationId	= @intItemLocationId,
 
 							-- Set this to change the Count UOM 
-							@intItemUOMId = @intItemUOMId,
+							@intItemUOMId		= @intItemUOMId,
 							-- Set these to change the storage unit/loc
 							@intStorageLocationId = NULL,
-							@intStorageUnitId = NULL
+							@intStorageUnitId	= NULL
 					END TRY
 					BEGIN CATCH
 						-- Flag Success
@@ -198,113 +216,11 @@ BEGIN TRY
 					END CATCH
 
 
-					-- ===================================================================================
-					-- Start - Check if Item Has recipe --------------------------------------------------
-					-- ===================================================================================
-					-- If Output Item exists
-					IF EXISTS (SELECT TOP 1 1 FROM vyuSTItemRecipeAndStockQty WHERE intItemId = @intItemId AND intCompanyLocationId = @intCompanyLocationId AND intRecipeItemTypeId = 2)
-						BEGIN
-							-- PRINT 'Output Item exists'
-
-							DECLARE @dblHandheldOutputItemCount AS DECIMAL(18, 6) = @dblCountQty
-									, @dblRecipeInputStockQty AS DECIMAL(18, 6)
-									, @dblRecipeInputQty AS DECIMAL(18, 6)
-									, @intRecipeId AS INT
-									, @dblInputItemComputedQty DECIMAL(18, 6)
-									, @intInputItemId AS INT
-									, @intInputItemUOMId AS INT
-
-							SELECT @intRecipeId = intRecipeId 
-							FROM vyuSTItemRecipeAndStockQty 
-							WHERE intItemId = @intItemId 
-								AND intCompanyLocationId = @intCompanyLocationId
-								AND intRecipeItemTypeId = 2
-
-							
-							-- If Input Item exists
-							IF EXISTS(SELECT TOP 1 1 FROM vyuSTItemRecipeAndStockQty WHERE intRecipeId = @intRecipeId AND intRecipeItemTypeId = 1)
-								BEGIN
-									-- PRINT 'Input Item exists'
-
-									SELECT @dblRecipeInputStockQty = ISNULL(dblItemStockUnitOnHand, 0)
-										   , @dblRecipeInputQty = ISNULL(dblRecipeQuantity, 0)
-										   , @intInputItemId = intItemId
-										   , @intInputItemUOMId = intItemUOMId
-										   , @intItemLocationId = intItemLocationId
-									FROM vyuSTItemRecipeAndStockQty
-									WHERE intRecipeId = @intRecipeId 
-										AND intRecipeItemTypeId = 1
-
-									--PRINT '@dblHandheldOutputItemCount: ' + CAST(@dblHandheldOutputItemCount AS NVARCHAR(15))
-									--PRINT '@dblRecipeInputStockQty: ' + CAST(@dblRecipeInputStockQty AS NVARCHAR(50))
-									--PRINT '@dblRecipeInputQty: ' + CAST(@dblRecipeInputQty AS NVARCHAR(50))
-									--PRINT CAST(@dblRecipeInputStockQty + (@dblHandheldOutputItemCount * @dblRecipeInputQty) AS NVARCHAR(50))
-
-									SET @dblInputItemComputedQty = @dblRecipeInputStockQty + (@dblHandheldOutputItemCount * @dblRecipeInputQty)
-
-									-- UPDATE INPUT Item
-									BEGIN TRY
----- TEST
---PRINT 'Will update Input Item'
---PRINT '@strCountNo: ' + @strCountNo
---PRINT '@dblInputItemComputedQty: ' + CAST(@dblInputItemComputedQty AS NVARCHAR(50)) 
---PRINT '@UserId: ' + CAST(@UserId AS NVARCHAR(10))
---PRINT '@intInputItemId: ' + CAST(@intInputItemId AS NVARCHAR(10))
---PRINT '@intItemLocationId: ' + CAST(@intItemLocationId AS NVARCHAR(10))
---PRINT '@intInputItemUOMId: ' + CAST(@intInputItemUOMId AS NVARCHAR(10))
-
-
-
-										EXEC uspICUpdateInventoryPhysicalCount
-											-- Count No and Physical Count are required
-											@strCountNo = @strCountNo,
-											@dblPhysicalCount = @dblInputItemComputedQty,
-
-											-- ========================================
-											--    Required for a lotted item
-											-- ========================================
-											-- Set this to NULL for a non-lotted item
-											@intLotId = NULL,
-
-											-- This is also required
-											@intUserSecurityId = @UserId,
-
-											-- ========================================
-											--    Parameters for a non-lotted item
-											-- ========================================
-											-- Required for a non-lotted item
-											@intItemId = @intInputItemId,
-											@intItemLocationId = @intItemLocationId,
-
-											-- Set this to change the Count UOM 
-											@intItemUOMId = @intInputItemUOMId,
-											-- Set these to change the storage unit/loc
-											@intStorageLocationId = NULL,
-											@intStorageUnitId = NULL
-
----- TEST
---PRINT 'SUCCESS'
-									END TRY
-									BEGIN CATCH
-										-- Flag Success
-										SET @ysnSuccess = CAST(0 AS BIT)
-										SET @strStatusMsg = 'Updating Recipe Input Item: ' + ERROR_MESSAGE()
----- TEST
---PRINT 'FAILED: ' + @strStatusMsg
-										-- ROLLBACK
-										GOTO ExitWithRollback
-									END CATCH
-								END
-						
-						END
-					-- ===================================================================================
-					-- End - Check if Item Has recipe ----------------------------------------------------
-					-- ===================================================================================
-
-
 					-- Remove record after use
 					DELETE FROM #ImportCounts
-					WHERE intHandheldScannerImportCountId = @intPrimaryCountId
+					WHERE intItemId				= @intItemId
+						AND intItemUOMId		= @intItemUOMId
+						AND intItemLocationId	= @intItemLocationId
 				END
 
 			-- ======================================================================================================================
