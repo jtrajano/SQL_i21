613b287a6da2cf74cf306f6a149f843d38eb0de8
Mon Gonzales <mon.gonzales@irely.com>
2019-02-19 17:09:35 +0800

    AR-7869 - Conversion - Origin Tax Group Header Conversion

--- Integration/dbo/Stored Procedures/uspSMImportTaxGroup.sql
+++ Integration/dbo/Stored Procedures/uspSMImportTaxGroup.sql
@@ -18,8 +18,44 @@ BEGIN
 
 	SELECT TOP 1 @ysnAG = CASE WHEN ISNULL(coctl_ag, '') = 'Y' THEN 1 ELSE 0 END
 			   , @ysnPT = CASE WHEN ISNULL(coctl_pt, '') = 'Y' THEN 1 ELSE 0 END 
-	FROM coctlmst	
+	FROM coctlmst
 
+	declare @tax_group_table table (
+		strGroup		nvarchar(50),
+		strDescription	nvarchar(50)
+	)
+
+	IF @ysnPT = 1 AND EXISTS(SELECT TOP 1 1 from INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'ptlclmst')
+	BEGIN
+		
+		
+		insert into @tax_group_table(strGroup, strDescription)
+		select 
+						isnull(a.ptlcl_state,'') + ' ' + isnull(a.ptlcl_local1_id,'') + ' ' + ISNULL(a.ptlcl_local2_id,'')
+						, isnull(a.ptlcl_desc, '')
+					from ptlclmst a
+						left join tblSMTaxGroup b
+							on (isnull(a.ptlcl_state,'') + ' ' 
+							+ isnull(a.ptlcl_local1_id,'') + ' ' 
+							+ ISNULL(a.ptlcl_local2_id,'')  ) COLLATE Latin1_General_CI_AS =  b.strTaxGroup  COLLATE Latin1_General_CI_AS
+					where b.intTaxGroupId is null
+
+		insert into @tax_group_table(strGroup, strDescription)
+		select 
+			distinct(isnull(ssvnc_seq_cd,'') + ' ' + isnull(ssvnc_tx_auth_id1,'') + ' ' + ISNULL(ssvnc_tx_auth_id2,'')) as strTaxGroup
+			,'Origin Tax Group' as strOriginDescription
+		from ssvncmst a
+			left join tblSMTaxGroup b
+				on (isnull(a.ssvnc_seq_cd,'') + ' ' 
+				+ isnull(a.ssvnc_tx_auth_id1,'') + ' ' 
+				+ ISNULL(a.ssvnc_tx_auth_id2,'')  ) COLLATE Latin1_General_CI_AS =  b.strTaxGroup  COLLATE Latin1_General_CI_AS
+			where ltrim(rtrim(( isnull(ssvnc_seq_cd,'') + ' ' + isnull(ssvnc_tx_auth_id1,'') + ' ' + ISNULL(ssvnc_tx_auth_id2,'') ))) <> ''
+				and b.intTaxGroupId is null
+				and ltrim(rtrim(( isnull(ssvnc_seq_cd,'') + ' ' + isnull(ssvnc_tx_auth_id1,'') + ' ' + ISNULL(ssvnc_tx_auth_id2,'') ))) not in ( select strGroup from @tax_group_table)
+
+
+
+	END
 	IF(@Checking = 0) 
 	BEGIN
 		
@@ -38,6 +74,8 @@ BEGIN
 		DECLARE @ysnAllowEFT		BIT
 		DECLARE @isTermExists		BIT
 
+		
+
 		IF @ysnAG = 1 AND EXISTS(SELECT TOP 1 1 from INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'aglclmst')
 		BEGIN
 			INSERT INTO tblSMTaxGroup(strTaxGroup, strDescription)
@@ -53,18 +91,7 @@ BEGIN
 		END
 		IF @ysnPT = 1 AND EXISTS(SELECT TOP 1 1 from INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'ptlclmst')
 		BEGIN
-			
-			INSERT INTO tblSMTaxGroup(strTaxGroup, strDescription)
-			select 
-				isnull(a.ptlcl_state,'') + ' ' + isnull(a.ptlcl_local1_id,'') + ' ' + ISNULL(a.ptlcl_local2_id,'')
-				, isnull(a.ptlcl_desc, '')
-			from ptlclmst a
-				left join tblSMTaxGroup b
-					on (isnull(a.ptlcl_state,'') + ' ' 
-					+ isnull(a.ptlcl_local1_id,'') + ' ' 
-					+ ISNULL(a.ptlcl_local2_id,'')  ) COLLATE Latin1_General_CI_AS =  b.strTaxGroup  COLLATE Latin1_General_CI_AS
-			where b.intTaxGroupId is null
-
+			insert into tblSMTaxGroup(strTaxGroup, strDescription) select strGroup, strDescription from @tax_group_table order by strGroup
 		END
 	END
 
@@ -84,15 +111,8 @@ BEGIN
 		 END
 
 		IF @ysnPT = 1 AND EXISTS(SELECT TOP 1 1 from INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'ptlclmst')
-		 BEGIN
-			
-			select @Total = Count(a.A4GLIdentity)
-				from ptlclmst a
-					left join tblSMTaxGroup b
-						on (isnull(a.ptlcl_state,'') + ' ' 
-						+ isnull(a.ptlcl_local1_id,'') + ' ' 
-						+ ISNULL(a.ptlcl_local2_id,'')  ) COLLATE Latin1_General_CI_AS =  b.strTaxGroup  COLLATE Latin1_General_CI_AS
-				where b.intTaxGroupId is null
+		 BEGIN			
+			select @Total = count(strGroup) from @tax_group_table
 		 END		
 	 END	
 END
