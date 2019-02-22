ef8374094aa779623367f26aa522d81bf5537af0
Mon Gonzales <mon.gonzales@irely.com>
2019-02-22 14:27:47 +0800

    SM-4476

--- i21Database/dbo/Tables/tblSMFreightTerm.sql
+++ i21Database/dbo/Tables/tblSMFreightTerm.sql
@@ -4,6 +4,20 @@
     [strFreightTerm]	 NVARCHAR(100)		COLLATE Latin1_General_CI_AS	NOT NULL, 
     [strFobPoint]		 NVARCHAR(100)		COLLATE Latin1_General_CI_AS	NOT NULL,
 	[ysnActive]			 BIT				DEFAULT (1)						NOT NULL,
+
+	--From Contract Basis
+	[strContractBasis] [nvarchar](100) COLLATE Latin1_General_CI_AS NOT NULL,
+	[strDescription] [nvarchar](500) COLLATE Latin1_General_CI_AS NOT NULL,
+	[ysnDefault] [bit] NULL,
+	[intInsuranceById] [int] NULL,
+	[intInvoiceTypeId] [int] NULL,
+	[intPositionId] [int] NULL,
+	[strINCOLocationType] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
+
+	--From Contract Basis
 	[intConcurrencyId]	 INT				DEFAULT (1)						NOT NULL,
-	CONSTRAINT [AK_tblSMFreightTerms_strFreightTerm] UNIQUE NONCLUSTERED ([strFreightTerm] ASC)
+	CONSTRAINT [AK_tblSMFreightTerms_strFreightTerm] UNIQUE NONCLUSTERED ([strFreightTerm] ASC),	
+	CONSTRAINT [FK_tblSMFreightTerms_tblCTInsuranceBy_intInsuranceById] FOREIGN KEY ([intInsuranceById]) REFERENCES [tblCTInsuranceBy]([intInsuranceById]),
+	CONSTRAINT [FK_tblSMFreightTerms_tblCTInvoiceType_intInvoiceTypeId] FOREIGN KEY ([intInvoiceTypeId]) REFERENCES [tblCTInvoiceType]([intInvoiceTypeId]),
+	CONSTRAINT [FK_tblSMFreightTerms_tblCTPosition_intPositionId] FOREIGN KEY ([intPositionId]) REFERENCES [tblCTPosition]([intPositionId])
 );
