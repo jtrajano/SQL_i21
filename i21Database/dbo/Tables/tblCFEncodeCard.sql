4b0b660b1ace9362ad18cc86496f7a69c975e07d
COMTECH\BPonce <bonn.ponce@irely.com>
2019-02-22 15:52:44 +0800

    CF-2171

--- i21Database/dbo/Tables/tblCFEncodeCard.sql
+++ i21Database/dbo/Tables/tblCFEncodeCard.sql
@@ -1,9 +1,9 @@
 ﻿CREATE TABLE [dbo].[tblCFEncodeCard] (
     [intEncodeCardId]         INT            IDENTITY (1, 1) NOT NULL,
-	[intAccountId]			  INT			  NULL,
-    [intCardId]				  INT             NULL,
-    [strCardNumber]           NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NULL,
-    [intNetworkId]			  INT             NULL,
+    [intAccountId]			  INT			 NULL,
+	[intNetworkId]			  INT			 NULL,
+	[strCardNumber]           NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NULL,
     [strNetwork]			  NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NULL,
+	[intConcurrencyId]		  INT             NULL,
     CONSTRAINT [PK_tblCFEncodeCard] PRIMARY KEY CLUSTERED ([intEncodeCardId] ASC)
 );
