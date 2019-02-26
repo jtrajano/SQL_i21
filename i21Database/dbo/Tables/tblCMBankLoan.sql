6d3b3ec935b8eb33afb2b1a788770c48ef6972ed
Jeff Trajano <jeffrey.trajano@irely.com>
2019-02-26 15:59:54 +0800

    GL-6720 GL-6722 GL-6335 GL-6787

--- i21Database/dbo/Tables/tblCMBankLoan.sql
+++ i21Database/dbo/Tables/tblCMBankLoan.sql
@@ -1,25 +1,19 @@
-﻿CREATE TABLE [dbo].[tblCMBankLoan](
-	[strBankLoanId] [nvarchar](20) COLLATE Latin1_General_CI_AS NOT NULL,
+﻿
+CREATE TABLE [dbo].[tblCMBankLoan](
+	[strBankLoanId] [nvarchar](20) NOT NULL,
 	[intBankLoanId] [int] IDENTITY(1,1) NOT NULL,
-	[intBankAccountId] [int] NOT NULL,
 	[dtmOpened] [date] NOT NULL,
 	[dtmMaturity] [date] NOT NULL,
 	[dtmEntered] [datetime] NOT NULL,
 	[decAnnualInterest] [decimal](8, 4) NOT NULL,
 	[ysnOpen] [bit] NOT NULL,
-	[strComments] [nvarchar](800) COLLATE Latin1_General_CI_AS NULL,
+	[strComments] [nvarchar](800) NULL,
 	[intConcurrencyId] [int] NULL,
+	[dblLoanAmount] [decimal](18, 6) NULL,
  CONSTRAINT [PK_tblBankLoan] PRIMARY KEY CLUSTERED 
 (
 	[intBankLoanId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 70) ON [PRIMARY]
 ) ON [PRIMARY]
-GO
-
-ALTER TABLE [dbo].[tblCMBankLoan]  WITH CHECK ADD  CONSTRAINT [FK_tblBankLoan_tblCMBank] FOREIGN KEY([intBankAccountId])
-REFERENCES [dbo].[tblCMBankAccount] ([intBankAccountId])
-GO
-
-ALTER TABLE [dbo].[tblCMBankLoan] CHECK CONSTRAINT [FK_tblBankLoan_tblCMBank]
 GO
 
