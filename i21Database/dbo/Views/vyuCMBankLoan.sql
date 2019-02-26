1caca2004f1557039ca4e38ef42aa6b4ef674c74
Jeff Trajano <jeffrey.trajano@irely.com>
2019-02-26 15:55:33 +0800

    GL-6720 GL-6722 GL-6335 GL-6787

---
+++ i21Database/dbo/Views/vyuCMBankLoan.sql
@@ -0,0 +1,30 @@
+﻿CREATE VIEW [dbo].[vyuCMBankLoan]
+AS
+SELECT 
+L.*,
+T.intBankAccountId,
+T.strTransactionId,
+BA.strBankAccountNo,
+U.dblBalance,
+intGLLoanAccountId = D.intGLAccountId,
+strGLLoanAccountId = GL.strAccountId,
+BA.strBankName,
+BA.strCurrency,
+T.intCurrencyId,
+strStatus = CASE WHEN L.ysnOpen = 1 THEN  'Open' ELSE 'Closed' END COLLATE Latin1_General_CI_AS ,
+T.ysnPosted
+from tblCMBankLoan L 
+JOIN tblCMBankTransaction T
+ON L.intBankLoanId = L.intBankLoanId
+LEFT JOIN tblCMBankTransactionDetail D
+on D.intTransactionId = T.intTransactionId
+LEFT JOIN tblGLAccount GL 
+ON GL.intAccountId = D.intGLAccountId
+LEFT JOIN vyuCMBankAccount BA
+ON BA.intBankAccountId = T.intBankAccountId
+CROSS APPLY
+(
+	SELECT SUM(dblAmount) dblBalance from tblCMBankTransaction WHERE intBankLoanId = L.intBankLoanId 
+)U
+WHERE
+T.intBankTransactionTypeId = 52
---
+++ i21Database/dbo/Views/vyuCMBankLoanDetail.sql
@@ -0,0 +1,27 @@
+﻿CREATE VIEW [dbo].[vyuCMBankLoanDetail] AS
+SELECT 
+D.intTransactionDetailId,
+D.intTransactionId,
+T.intBankTransactionTypeId,
+strGLAccountIdDetail = A.strAccountId,
+P.strBankTransactionTypeName,
+intGLAccountIdDetail = D.intGLAccountId,
+T.strTransactionId,
+L.intBankLoanId,
+T.dtmDate,
+D.dblDebit,
+T.strMemo,
+T.ysnPosted,
+T.intConcurrencyId
+FROM tblCMBankLoan L
+LEFT JOIN tblCMBankTransaction T
+on L.intBankLoanId = T.intBankLoanId
+LEFT JOIN tblCMBankTransactionDetail D
+ON D.intTransactionId = T.intTransactionId
+LEFT JOIN tblGLAccount A
+on A.intAccountId = D.intGLAccountId
+LEFT JOIN
+tblCMBankTransactionType P
+on P.intBankTransactionTypeId = T.intBankTransactionTypeId
+GO
+
