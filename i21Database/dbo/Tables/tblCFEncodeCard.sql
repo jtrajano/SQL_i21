a4889a7bf70ca4079af12fe61aaea881ac1685a4
COMTECH\BPonce <bonn.ponce@irely.com>
2019-02-27 19:05:13 +0800

    CF-2172

--- i21Database/dbo/Tables/tblCFEncodeCard.sql
+++ i21Database/dbo/Tables/tblCFEncodeCard.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[tblCFEncodeCard] (
     [intEncodeCardId]         INT            IDENTITY (1, 1) NOT NULL,
     [intAccountId]			  INT			 NULL,
+    [intCardId]				  INT			 NULL,
 	[intNetworkId]			  INT			 NULL,
 	[strCardNumber]           NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NULL,
     [strNetwork]			  NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NULL,
