6b0d443e3f2685cf980b36fdbe5aab181a334525
Kevin Estrada <kevin.estrada@irely.com>
2019-03-01 13:37:11 +0800

    FRM-7212: checkin on pre-dep sysadmin

--- i21PreDeployment/Scripts/Pre-Deployment/4_SM/1910_StopAuditMigrationJob.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/4_SM/1910_StopAuditMigrationJob.sql
@@ -5,6 +5,31 @@
 		DECLARE @CURRENT_DB nvarchar(max)
 		SET @CURRENT_DB = DB_NAME()
 
+		DECLARE @isUserHasRole bit
+		SET @isUserHasRole = 0
+
+		IF OBJECT_ID('tempdb..#TempSysAdmin2') IS NOT NULL
+		DROP TABLE #TempSysAdmin2
+
+	Create TABLE #TempSysAdmin2
+	(
+		[ServerRole]	sysname,
+		[MemberName]	sysname,
+		[MemberSID]		varbinary(85)
+	)
+	INSERT INTO #TempSysAdmin2 exec sp_helpsrvrolemember 'sysadmin'
+
+	DECLARE @loginUser varchar(250)
+	SET @loginUser = SYSTEM_USER
+
+	SELECT @isUserHasRole = 1 FROM #TempSysAdmin2 WHERE MemberName = @loginUser
+	IF ISNULL(@isUserHasRole, 0) = 0
+		begin
+			PRINT 'CURRENT USER IS NOT SYSADMIN, CANNOT CONTINUE ACCESSING MSDB'
+		end
+	ELSE
+		BEGIN
+
 		SELECT @jobId = job_id FROM msdb.dbo.sysjobs where [name] = 'i21_AuditLog_Migration_Job_' +@CURRENT_DB
 
 		IF(@jobId is not null)
@@ -19,5 +44,6 @@
 				exec(' update tblSMCompanySetup set ysnAuditBatchMigrated = null ')
 			end
 	END
+END
 	PRINT N'END REMOVE AUDIT MIGRATION JOB'
 GO
