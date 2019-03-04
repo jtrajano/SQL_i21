d8e6d1ec4b4d3efa33695ce906a16d721f2eb49b
Jeff Trajano <jeffrey.trajano@irely.com>
2019-03-04 11:11:10 +0800

    GL-6266 Bank Transactions - Import bank transactions

--- i21Database/dbo/Tables/tblCMImportBankTransactionLogDetail.sql
+++ i21Database/dbo/Tables/tblCMImportBankTransactionLogDetail.sql
@@ -2,8 +2,12 @@ create table [dbo].[tblCMImportBankTransactionLogDetail] (
     [intImportLogDetailId] [int] NOT NULL IDENTITY(1,1),
     [intImportLogId] [int] NOT NULL,
     [strTransactionId] [nvarchar](20) COLLATE Latin1_General_CI_AS NULL,
-    [strDescription] [nvarchar](100) COLLATE Latin1_General_CI_AS NULL,
-    [intConcurrencyId] [int] NOT NULL,
+	[strDescription] [nvarchar](100) COLLATE Latin1_General_CI_AS NULL,
+	[intConcurrencyId] [int] NOT NULL,
+	[ysnPosted] [bit] NULL,
+	[intTransactionId] [int] NULL,
+	[intLineNo] [int] NULL,
+	[intBankTransactionTypeId] [int] NULL,
     primary key ([intImportLogDetailId])
 );
 GO
