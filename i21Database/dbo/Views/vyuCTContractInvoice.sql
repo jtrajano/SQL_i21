a25a5c32d511b52e93b2e96f08fd6100c718ee8b
Dan Jalimao <dan.jalimao@irely.com>
2019-03-04 17:28:27 +0800

    Add Invoice Tab

---
+++ i21Database/dbo/Views/vyuCTContractInvoice.sql
@@ -0,0 +1,17 @@
+﻿CREATE VIEW [dbo].[vyuCTContractInvoice]
+AS	
+
+SELECT		CI.intContractInvoiceId,
+			CI.intContractDetailId,	
+			CI.strInvoiceNumber,		
+			CI.dtmDate,				
+			CI.strDescription,		
+			CI.intCurrencyId,			
+			CI.dblAmount,				
+			CI.strCounterParty,		
+			CI.strRemark,				
+			CI.intConcurrencyId,
+			C.strCurrency
+
+FROM		tblCTContractInvoice	CI	
+LEFT JOIN	tblSMCurrency			C		ON		C.intCurrencyID = CI.intCurrencyId
