7fb571e14cd4d1bdab29c55f9ab14b638e226541
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2019-03-07 22:14:52 +0530

    RM-2693

--- i21Database/dbo/Stored Procedures/uspRKRiskPositionInquiryBySummary.sql
+++ i21Database/dbo/Stored Procedures/uspRKRiskPositionInquiryBySummary.sql
@@ -454,7 +454,7 @@ SELECT 1 intRowNumber,'1.Outright Coverage','Outright Coverage' Selection,'1.Pri
               @strParamFutureMonth strFutureMonth,strAccountNumber,sum(dblNoOfLot) dblNoOfLot,null,getdate() TransactionDate,'Inventory' TranType,
               null, 0.0 ,sum(dblNoOfLot) dblQuantity,1
 FROM (
-  SELECT distinct    
+  SELECT distinct  ic.intItemId,  
   'Purchase'+' - '+isnull(c.strDescription,'') as strAccountNumber,
   dbo.fnCTConvertQuantityToTargetCommodityUOM(um.intCommodityUnitMeasureId,@intUOMId,iis.dblUnitOnHand) dblNoOfLot
   FROM tblICCommodity co
