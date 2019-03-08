7d20ebd04e642678ae4dd8330a9d541413aa24ca
Mon Gonzales <mon.gonzales@irely.com>
2019-03-08 18:58:52 +0800

    SM-4619 - Email Log

--- i21Database/dbo/Tables/tblSMActivityEmailResult.sql
+++ i21Database/dbo/Tables/tblSMActivityEmailResult.sql
@@ -4,7 +4,7 @@ CREATE TABLE [dbo].[tblSMActivityEmailResult]
 	[intActivityId]                     INT NOT NULL,
     [strResult]                         NVARCHAR(4000) NULL,
     [dtmTransactionDate]                DATETIME NOT NULL DEFAULT(GETDATE()),
-    [intEntityUserId]                   INT NOT NULL,
+    [intEntityUserId]                   INT NULL,
 	[intConcurrencyId]		            INT NOT NULL DEFAULT ((1)), 
     CONSTRAINT [FK_tblSMActivityEmailResult_tblSMActivity] FOREIGN KEY ([intActivityId]) REFERENCES [tblSMActivity]([intActivityId]) ON DELETE CASCADE,
     CONSTRAINT [FK_tblSMActivityEmailResult_tblEMEntity] FOREIGN KEY ([intEntityUserId]) REFERENCES [tblEMEntity]([intEntityId]) ON DELETE SET NULL,
