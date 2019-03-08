6587c7aa1b87ad0bb8fca10a065758fe8d319a43
COMTECH\HZaragoza <henry.zaragoza@irely.com>
2019-03-08 16:03:03 +0800

    ST-1222 Department Summary > Cleanup

--- i21Database/dbo/Views/vyuSTDepartmentDetailReport.sql
+++ i21Database/dbo/Views/vyuSTDepartmentDetailReport.sql
@@ -1,22 +1,33 @@
 ﻿CREATE VIEW [dbo].[vyuSTDepartmentDetailReport]
 	AS 
 SELECT ST.intStoreId
-, ST.intStoreNo
-, ST.strRegion
-, ST.strDistrict
-, ST.strDescription strStoreDescription
-, CAT.intCategoryId
-, CAT.strCategoryCode
-, CAT.strDescription strCategoryDescription
-, CH.dtmCheckoutDate
-, ISNULL(Inv.ysnPosted, 0) ysnPosted
-, CDT.intItemsSold 
-, CDT.intTotalSalesCount
-, CDT.dblTotalSalesAmountComputed
-FROM tblSTCheckoutHeader CH INNER JOIN tblSTStore ST ON ST.intStoreId = CH.intStoreId 
-LEFT JOIN tblARInvoice Inv ON Inv.intInvoiceId = CH.intInvoiceId
-INNER JOIN tblSTCheckoutDepartmetTotals CDT ON CDT.intCheckoutId = CH.intCheckoutId 
-INNER JOIN tblICItem IT ON IT.intItemId = CDT.intItemId 
-INNER JOIN tblICCategory CAT ON CAT.intCategoryId = IT.intCategoryId
+	, ST.intStoreNo
+	, ST.strRegion
+	, ST.strDistrict
+	, CL.strCity
+	, CL.strStateProvince
+	, CL.strZipPostalCode
+	, ST.strDescription strStoreDescription
+	, CAT.intCategoryId
+	, CAT.strCategoryCode
+	, CAT.strDescription strCategoryDescription
+	, CH.dtmCheckoutDate
+	, ISNULL(Inv.ysnPosted, 0) ysnPosted
+	, CDT.intItemsSold 
+	, CDT.intTotalSalesCount
+	, CDT.dblTotalSalesAmountComputed
+FROM tblSTCheckoutHeader CH 
+INNER JOIN tblSTStore ST 
+	ON ST.intStoreId = CH.intStoreId 
+INNER JOIN tblSMCompanyLocation CL
+	ON ST.intCompanyLocationId = CL.intCompanyLocationId
+LEFT JOIN tblARInvoice Inv 
+	ON Inv.intInvoiceId = CH.intInvoiceId
+INNER JOIN tblSTCheckoutDepartmetTotals CDT 
+	ON CDT.intCheckoutId = CH.intCheckoutId 
+INNER JOIN tblICItem IT 
+	ON IT.intItemId = CDT.intItemId 
+INNER JOIN tblICCategory CAT 
+	ON CAT.intCategoryId = IT.intCategoryId
 WHERE (CDT.dblTotalSalesAmountComputed <> 0	
 	OR CDT.intTotalSalesCount <> 0)
