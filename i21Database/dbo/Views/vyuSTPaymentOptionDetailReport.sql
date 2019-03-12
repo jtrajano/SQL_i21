941f1a0fde583c1ecbcb374a52ef01fdb6f53d12
COMTECH\HZaragoza <henry.zaragoza@irely.com>
2019-03-12 16:01:53 +0800

    ST-1224 Payment Option Summary > Cleanup

--- i21Database/dbo/Views/vyuSTPaymentOptionDetailReport.sql
+++ i21Database/dbo/Views/vyuSTPaymentOptionDetailReport.sql
@@ -1,19 +1,24 @@
 ﻿CREATE VIEW [dbo].[vyuSTPaymentOptionDetailReport]
 	AS 
 SELECT ST.intStoreId
-, ST.intStoreNo
-, ST.strRegion
-, ST.strDistrict
-, ST.strDescription strStoreDescription
-, CH.dtmCheckoutDate
-, ISNULL(Inv.ysnPosted, 0) ysnPosted
-, CPO.intPaymentOptionId
-, PO.strPaymentOptionId
-, PO.strDescription
-, CPO.dblAmount
+	, ST.intStoreNo
+	, ST.strRegion
+	, ST.strDistrict
+	, CL.strCity
+	, CL.strStateProvince
+	, CL.strZipPostalCode
+	, ST.strDescription strStoreDescription
+	, CH.dtmCheckoutDate
+	, ISNULL(Inv.ysnPosted, 0) ysnPosted
+	, CPO.intPaymentOptionId
+	, PO.strPaymentOptionId
+	, PO.strDescription
+	, CPO.dblAmount
 FROM tblSTCheckoutHeader CH 
 INNER JOIN tblSTStore ST 
 	ON ST.intStoreId = CH.intStoreId 
+INNER JOIN tblSMCompanyLocation CL
+	ON ST.intCompanyLocationId = CL.intCompanyLocationId
 LEFT JOIN tblARInvoice Inv 
 	ON Inv.intInvoiceId = CH.intInvoiceId
 INNER JOIN tblSTCheckoutPaymentOptions CPO 
