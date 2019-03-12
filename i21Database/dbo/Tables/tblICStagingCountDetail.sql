5259b5f50955704331446a7bbfe7a373b49388f4
Wendell Wayne Estrada <wendell.estrada@irely.com>
2019-03-12 20:50:31 +0800

    [F] IC-5836 Easy Automation - Inventory Adjustments (count) Import - SQL Proc

---
+++ i21Database/dbo/Tables/tblICStagingCountDetail.sql
@@ -0,0 +1,21 @@
+CREATE TABLE tblICStagingCountDetail (
+	  intStagingCountDetailId INT IDENTITY(1, 1)
+	, strCountNo NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL -- Used for grouping or when exporting
+	, strItemNo NVARCHAR(100) COLLATE Latin1_General_CI_AS NOT NULL
+	, dblPhysicalCount NUMERIC(38, 20) NULL
+	, strCountUom NVARCHAR(100) COLLATE Latin1_General_CI_AS NOT NULL
+	, strStorageLocation NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL
+	, strStorageUnit NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL
+	, strCountGroup NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL
+	, strLotNo NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL
+	, strLotAlias NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL
+	, strParentLotNo NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL
+	, strParentLotAlias NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL
+	, strWeightUom NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL
+	, dblLastCost NUMERIC(38, 20) NULL
+	, dblPallets NUMERIC(38, 20) NULL
+	, dblQtyPerPallet NUMERIC(38, 20) NULL
+	, ysnRecount BIT NULL
+	, dblWeightQty NUMERIC(38, 20) NULL
+	, CONSTRAINT PK_tblICStagingCountDetail_intStagingCountDetailId PRIMARY KEY(intStagingCountDetailId)
+)
