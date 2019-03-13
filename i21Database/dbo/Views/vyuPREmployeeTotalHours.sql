280c0644fa914e43d52dfc407add1658733a3a13
Jonas Rafael Sarcilla <jonas.sarcilla@irely.com>
2019-03-13 13:45:28 +0800

    PR-1870

---
+++ i21Database/dbo/Views/vyuPREmployeeTotalHours.sql
@@ -0,0 +1,19 @@
+﻿CREATE VIEW [dbo].[vyuPREmployeeTotalHours]
+AS 
+SELECT TC.intEntityEmployeeId
+	,TC.intWorkersCompensationId
+	,TH.dblTotalHours
+FROM (
+	SELECT DISTINCT intEntityEmployeeId
+		,intWorkersCompensationId
+	FROM tblPRTimecard
+) TC
+LEFT JOIN (
+	SELECT intEntityEmployeeId
+		,intWorkersCompensationId
+		,dblTotalHours = SUM(ISNULL(dblHours,0))
+	FROM tblPRTimecard
+	GROUP BY
+		intEntityEmployeeId
+		,intWorkersCompensationId
+)TH ON TC.intEntityEmployeeId = TH.intEntityEmployeeId AND TC.intWorkersCompensationId = TH.intWorkersCompensationId
