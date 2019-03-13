ace7febbbc380c57b15d31cb8f557d95bf2e4ab1
Mon Gonzales <mon.gonzales@irely.com>
2019-03-13 11:47:26 +0800

    SM-4625 - Add Tax Report Type in Tax Class

--- i21Database/dbo/Tables/tblSMTaxClass.sql
+++ i21Database/dbo/Tables/tblSMTaxClass.sql
@@ -3,6 +3,9 @@
 	[intTaxClassId] INT NOT NULL PRIMARY KEY IDENTITY, 
     [strTaxClass] NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL, 
     [strOriginTaxType] NVARCHAR(5) COLLATE Latin1_General_CI_AS NULL, 
-    [strTaxReportType] NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL, 
-    [intConcurrencyId] INT NOT NULL DEFAULT 1
+    -- [strTaxReportType] NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL, 
+    [intTaxReportTypeId] INT NULL, 
+    [intConcurrencyId] INT NOT NULL DEFAULT 1,
+    
+    CONSTRAINT [FK_tblSMTaxClass_tblSMTaxReportType] FOREIGN KEY (intTaxReportTypeId) REFERENCES tblSMTaxReportType(intTaxReportTypeId),
 )
