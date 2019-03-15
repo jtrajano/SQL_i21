c7d7853b11273ece0768ca2f3206fdb151bd06cf
Arman Arenas <arman.arenas@irely.com>
2019-03-15 13:36:47 +0800

    LG-1918 Added Post-Deployment Scripts to migrate Generate Load data to the new fields

--- i21Database/Scripts/Post-Deployment/LG/1_MasterTables.sql
+++ i21Database/Scripts/Post-Deployment/LG/1_MasterTables.sql
@@ -29,6 +29,7 @@ BEGIN
 			WHERE ysnDocumentsReceived IS NULL AND EXISTS(SELECT TOP 1 1 FROM tblLGLoadDocuments WHERE intLoadId = tblLGLoad.intLoadId)
 	')
 END
+GO
 
 /*
 * Set Container Numbers value 
@@ -48,3 +49,74 @@ BEGIN
 													 WHERE LDCL.intLoadDetailId = LD.intLoadDetailId)
 	')
 END
+
+/* 
+* Generate Load - Data Migration to New Fields 
+*/
+
+--Transport Mode
+IF EXISTS(SELECT * FROM sys.columns WHERE object_id = object_id('tblLGGenerateLoad') AND name = 'intTransportationMode')
+BEGIN
+	EXEC('UPDATE tblLGGenerateLoad
+		SET intTransportationMode = ISNULL((SELECT TOP 1 intTransportationMode FROM tblLGLoad WHERE intGenerateLoadId = tblLGGenerateLoad.intGenerateLoadId),
+										   (SELECT TOP 1 intDefaultTransportationMode FROM tblLGCompanyPreference))
+		WHERE intTransportationMode IS NULL
+	')
+END
+GO
+
+--Hauler
+IF EXISTS(SELECT * FROM sys.columns WHERE object_id = object_id('tblLGGenerateLoad') AND name = 'intHaulerEntityId')
+BEGIN
+	EXEC('UPDATE tblLGGenerateLoad
+		SET intHaulerEntityId = CASE WHEN (intType IN (1, 3)) THEN intPHaulerEntityId ELSE intSHaulerEntityId END
+		WHERE intHaulerEntityId IS NULL
+	')
+END
+GO
+
+--Equipment Type
+IF EXISTS(SELECT * FROM sys.columns WHERE object_id = object_id('tblLGGenerateLoad') AND name = 'intEquipmentTypeId')
+BEGIN
+	EXEC('UPDATE tblLGGenerateLoad
+		SET intEquipmentTypeId = CASE WHEN (intType IN (1, 3)) THEN intPHaulerEntityId ELSE intSHaulerEntityId END
+		WHERE intEquipmentTypeId IS NULL
+	')
+END
+GO
+
+--Item Id
+IF EXISTS(SELECT * FROM sys.columns WHERE object_id = object_id('tblLGGenerateLoad') AND name = 'intItemId')
+BEGIN
+	EXEC('UPDATE GL
+		SET intItemId = CASE WHEN (GL.intType IN (1, 3)) THEN VGL.intPItemId ELSE VGL.intSItemId END
+		FROM tblLGGenerateLoad GL
+		INNER JOIN vyuLGGenerateLoad VGL ON GL.intGenerateLoadId = VGL.intGenerateLoadId
+		WHERE GL.intItemId IS NULL
+	')
+END
+GO
+
+--Ship Date
+IF EXISTS(SELECT * FROM sys.columns WHERE object_id = object_id('tblLGGenerateLoad') AND name = 'dtmShipDate')
+BEGIN
+	EXEC('UPDATE tblLGGenerateLoad
+		SET dtmShipDate = CASE WHEN (intType IN (1, 3)) THEN dtmPArrivalDate ELSE dtmSShipToDate END
+		WHERE intEquipmentTypeId IS NULL
+	')
+END
+GO
+
+--End Date
+IF EXISTS(SELECT * FROM sys.columns WHERE object_id = object_id('tblLGGenerateLoad') AND name = 'dtmEndDate')
+BEGIN
+	EXEC('UPDATE GL
+		SET dtmEndDate = CASE WHEN (intType IN (1, 3)) THEN PCD.dtmEndDate ELSE SCD.dtmEndDate END
+		FROM tblLGGenerateLoad GL
+		LEFT JOIN tblCTContractDetail PCD ON PCD.intContractDetailId = GL.intPContractDetailId
+		LEFT JOIN tblCTContractDetail SCD ON SCD.intContractDetailId = GL.intSContractDetailId
+		WHERE GL.dtmEndDate IS NULL
+	')
+END
+GO
+
