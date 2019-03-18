8d1ae963ddaf79ec3866de7b40cc514796694be0
Mon Gonzales <mon.gonzales@irely.com>
2019-03-18 11:46:09 +0800

    SM-4632 - Rename Vantiv to Worldpay

--- i21Database/Scripts/Post-Deployment/PostDeployment.sql
+++ i21Database/Scripts/Post-Deployment/PostDeployment.sql
@@ -85,6 +85,7 @@ print 'BEGIN POST DEPLOYMENT'
 :r .\SM\1830_DeleteDuplicatetblSMScreenData.sql
 :r .\SM\1830_Encrypt_Merchant_Password.sql
 :r .\SM\1910_MigrateINCOToFreightTerm.sql
+:r .\SM\1910_UpdateVantivToWorldPay.sql
 
 -- Canned Report
 :r .\Reports\1_ReportDisableConstraints.sql
