2328f236321ac230b8d6db70d71bdb246d15278c
Robin Panganiban <robin.panganiban@irely.com>
2019-03-19 11:43:03 +0800

    SC-2574

--- i21Database/dbo/Stored Procedures/uspSCAddScaleTicketToItemShipment.sql
+++ i21Database/dbo/Stored Procedures/uspSCAddScaleTicketToItemShipment.sql
@@ -178,7 +178,7 @@ BEGIN
 												LEFT JOIN tblICItemUOM futureUOM ON futureUOM.intUnitMeasureId = intSettlementUOMId 
 												WHERE futureUOM.intItemId = LI.intItemId),0)
 												+ ISNULL((SELECT ISNULL(dbo.fnCalculateCostBetweenUOM(SC.intItemUOMIdTo,futureUOM.intItemUOMId,dblBasis),0) --basis entry price
-												FROM dbo.fnRKGetFutureAndBasisPrice (NULL,SC.intCommodityId,right(convert(varchar, CNT.dtmEndDate, 106),8),1,CNT.intFutureMarketId,CNT.intFutureMonthId,SC.intProcessingLocationId,NULL,0 ,SC.intItemId,ISNULL(CNT.intInvoiceCurrencyId,CNT.intCurrencyId))
+												FROM dbo.fnRKGetFutureAndBasisPrice (2,SC.intCommodityId,right(convert(varchar, CNT.dtmEndDate, 106),8),1,CNT.intFutureMarketId,CNT.intFutureMonthId,SC.intProcessingLocationId,NULL,0 ,SC.intItemId,ISNULL(CNT.intInvoiceCurrencyId,CNT.intCurrencyId))
 												LEFT JOIN tblICItemUOM futureUOM ON futureUOM.intUnitMeasureId = intBasisUOMId 
 												WHERE futureUOM.intItemId = LI.intItemId),0)
 										END 
