c5f81670d16fd73713ec55c090e03594fb933e43
Kelvin Quinto <kelvin.quinto@irely.com>
2019-03-19 16:52:06 +0800

    AR-9898, AR-9976, AR-9978

--- i21Database/dbo/Views/vyuARTaxDetailReport.sql
+++ i21Database/dbo/Views/vyuARTaxDetailReport.sql
@@ -6,10 +6,12 @@ SELECT TAXDETAIL.*
 FROM (
 	SELECT intTransactionDetailTaxId	= IDT.intInvoiceDetailTaxId
 		 , intTransactionDetailId		= IDT.intInvoiceDetailId
+		 , intTransactionId				= ID.intInvoiceId
 		 , dblAdjustedTax				= IDT.dblAdjustedTax
 		 , dblRate						= IDT.dblRate
 		 , intTaxCodeId					= IDT.intTaxCodeId
 		 , strCalculationMethod			= IDT.strCalculationMethod
+		 , dblComputedGrossPrice		= ISNULL(ID.dblComputedGrossPrice, 0)
 		 , strTaxTransactionType		= 'Invoice' COLLATE Latin1_General_CI_AS
 	FROM tblARInvoiceDetailTax IDT 	
 	INNER JOIN tblARInvoiceDetail ID ON IDT.intInvoiceDetailId = ID.intInvoiceDetailId
@@ -24,13 +26,22 @@ FROM (
 
 	SELECT intTransactionDetailTaxId	= SDT.intSalesOrderDetailTaxId
 		 , intTransactionDetailId		= SDT.intSalesOrderDetailId
+		 , intTransactionId				= 0
 		 , dblAdjustedTax				= SDT.dblAdjustedTax
 		 , dblRate						= SDT.dblRate
 		 , intTaxCodeId					= SDT.intTaxCodeId
 		 , strCalculationMethod			= SDT.strCalculationMethod
+		 , dblComputedGrossPrice		= 0
 		 , strTaxTransactionType   		= 'Sales Order' COLLATE Latin1_General_CI_AS
 	FROM tblSOSalesOrderDetailTax SDT
 	WHERE SDT.dblAdjustedTax <> 0
 ) AS TAXDETAIL
-LEFT JOIN tblSMTaxCode SMT ON
-       TAXDETAIL.intTaxCodeId = SMT.intTaxCodeId 
+INNER JOIN tblSMTaxCode SMT ON TAXDETAIL.intTaxCodeId = SMT.intTaxCodeId
+INNER JOIN tblSMTaxClass TC ON SMT.intTaxClassId = TC.intTaxClassId 
+OUTER APPLY (
+	SELECT TOP 1 strInvoiceReportName
+	FROM tblARCompanyPreference
+) CP
+WHERE ISNULL(strInvoiceReportName, 'Standard') <> 'Format 2 - Mcintosh'
+  OR ((ISNULL(strInvoiceReportName, 'Standard') = 'Format 2 - Mcintosh' AND TAXDETAIL.dblComputedGrossPrice = 0)
+   OR (ISNULL(strInvoiceReportName, 'Standard') = 'Format 2 - Mcintosh' AND TAXDETAIL.dblComputedGrossPrice <> 0 AND UPPER(strTaxClass) = 'STATE SALES TAX (SST)'))
