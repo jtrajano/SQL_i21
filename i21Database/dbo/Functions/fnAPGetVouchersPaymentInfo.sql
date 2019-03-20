a9ed964cc8974ee1b0dbb216b357dff0d04848e1
Erick <erick.delacruz@irely.com>
2019-03-20 20:33:41 +0800

    AP-6112

---
+++ i21Database/dbo/Functions/fnAPGetVouchersPaymentInfo.sql
@@ -0,0 +1,36 @@
+﻿CREATE FUNCTION [dbo].[fnAPGetVouchersPaymentInfo]
+(
+	
+)
+RETURNS @returntable TABLE
+(
+	intBillId INT,
+	ysnPaid BIT,
+	strPaymentInfo NVARCHAR(MAX),
+	strPaymentInfoKey NVARCHAR(MAX)
+)
+AS
+BEGIN
+	INSERT @returntable
+	SELECT 
+		A.intBillId,
+		A.ysnPaid,
+		strPaymentInfo		=	STUFF((
+									SELECT ',' + CAST(B.strPaymentRecordNum AS NVARCHAR)
+									FROM tblAPPayment B
+									INNER JOIN tblAPPaymentDetail C
+										ON B.intPaymentId = C.intPaymentId
+									WHERE A.ysnPosted = 1 AND C.intBillId = A.intBillId
+									FOR XML PATH('')),1,1,''
+								),
+		strPaymentInfoKey	=	STUFF((
+									SELECT ',' + CAST(B.intPaymentId AS NVARCHAR)
+									FROM tblAPPayment B
+									INNER JOIN tblAPPaymentDetail C
+										ON B.intPaymentId = C.intPaymentId
+									WHERE A.ysnPosted = 1 AND C.intBillId = A.intBillId
+									FOR XML PATH('')),1,1,''
+								)
+	FROM tblAPBill A
+	RETURN;
+END
