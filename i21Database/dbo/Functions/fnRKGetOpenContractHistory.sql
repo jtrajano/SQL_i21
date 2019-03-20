a3345d9544a8c6276f7843ec767da4c4ae859565
Lawrence Zabala <lawrence.zabala@irely.com>
2019-03-20 17:42:45 +0800

    RM-2724

--- i21Database/dbo/Functions/fnRKGetOpenContractHistory.sql
+++ i21Database/dbo/Functions/fnRKGetOpenContractHistory.sql
@@ -8,12 +8,27 @@ AS
 
 BEGIN
 	DECLARE @dblMatchContract NUMERIC(18, 6)
+		, @strBuySell NVARCHAR(10)
 	SET @dtmDateAsOf = CONVERT(DATETIME, CONVERT(VARCHAR(10), @dtmDateAsOf, 110), 110)
+	SELECT TOP 1 @strBuySell = strNewBuySell
+	FROM vyuRKGetFutOptTransactionHistory
+	WHERE intFutOptTransactionId = @intFutOptTransactionId
+		AND dtmTransactionDate <= DATEADD(MILLISECOND, -2, DATEADD(DAY, 1, CAST(FLOOR(CAST(@dtmDateAsOf AS FLOAT)) AS DATETIME)))
 
-	SELECT @dblMatchContract = SUM(mf.dblMatchQty)
-	FROM tblRKMatchDerivativesHistory mf
-	WHERE mf.intLFutOptTransactionId = @intFutOptTransactionId
-		AND mf.dtmMatchDate <= @dtmDateAsOf
+	IF (@strBuySell = 'Buy')
+	BEGIN
+		SELECT @dblMatchContract = SUM(mf.dblMatchQty)
+		FROM tblRKMatchDerivativesHistory mf
+		WHERE mf.intLFutOptTransactionId = @intFutOptTransactionId
+			AND mf.dtmMatchDate <= @dtmDateAsOf
+	END
+	ELSE
+	BEGIN
+		SELECT @dblMatchContract = - SUM(mf.dblMatchQty)
+		FROM tblRKMatchDerivativesHistory mf
+		WHERE mf.intSFutOptTransactionId = @intFutOptTransactionId
+			AND mf.dtmMatchDate <= @dtmDateAsOf
+	END
 
 	RETURN @dblMatchContract
 END
