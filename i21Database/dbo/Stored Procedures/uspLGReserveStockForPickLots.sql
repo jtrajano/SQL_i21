ab86f3b8f71817927c750ec709cd20d88a6362ac
Arman Arenas <arman.arenas@irely.com>
2019-03-22 10:25:35 +0800

    LG-2005 expanded uspLGReserveStockForPickLots to support reservation for Outbound LS

--- i21Database/dbo/Stored Procedures/uspLGReserveStockForPickLots.sql
+++ i21Database/dbo/Stored Procedures/uspLGReserveStockForPickLots.sql
@@ -1,5 +1,6 @@
 ﻿CREATE PROCEDURE dbo.uspLGReserveStockForPickLots
-	@intPickLotHeaderId AS INT
+	@intPickLotHeaderId AS INT = NULL
+	,@intLoadDetailId AS INT = NULL
 AS
 
 SET QUOTED_IDENTIFIER OFF
@@ -12,17 +13,17 @@ DECLARE @ItemsToReserve AS dbo.ItemReservationTableType;
 DECLARE @intInventoryTransactionType AS INT
 DECLARE @strInvalidItemNo AS NVARCHAR(50) 
 DECLARE @intInvalidItemId AS INT 
+DECLARE @intLoadId INT
 
 -- Get the transaction type id
-BEGIN 
-	SELECT TOP 1 
-			@intInventoryTransactionType = intTransactionTypeId
-	FROM	dbo.tblICInventoryTransactionType
-	WHERE	strName = 'Pick Lots'
-END
+SELECT TOP 1 
+		@intInventoryTransactionType = intTransactionTypeId
+FROM	dbo.tblICInventoryTransactionType
+WHERE	strName = CASE WHEN (@intLoadDetailId IS NULL) THEN 'Pick Lots' ELSE 'Outbound Shipment' END
 
 -- Get the items to reserve
-BEGIN 
+IF (@intLoadDetailId IS NULL)
+BEGIN
 	INSERT INTO @ItemsToReserve (
 			intItemId
 			,intItemLocationId
@@ -50,6 +51,38 @@ BEGIN
 			JOIN tblICLot Lot ON Lot.intLotId = PLDetail.intLotId
 	WHERE	PLHeader.intPickLotHeaderId = @intPickLotHeaderId
 END
+ELSE
+BEGIN
+	SELECT @intLoadId = intLoadId FROM tblLGLoadDetail WHERE intLoadDetailId = @intLoadDetailId 
+
+	INSERT INTO @ItemsToReserve (
+			intItemId
+			,intItemLocationId
+			,intItemUOMId
+			,intLotId
+			,intSubLocationId
+			,intStorageLocationId
+			,dblQty
+			,intTransactionId
+			,strTransactionId
+			,intTransactionTypeId
+	)
+	SELECT	intItemId = Lot.intItemId
+			,intItemLocationId = Lot.intItemLocationId
+			,intItemUOMId = Lot.intItemUOMId
+			,intLotId = Lot.intLotId
+			,intSubLocationId = Lot.intSubLocationId
+			,intStorageLocationId = Lot.intStorageLocationId
+			,dblQty = LDLot.dblLotQuantity
+			,intTransactionId = L.intLoadId
+			,strTransactionId = CAST(L.strLoadNumber AS VARCHAR(100))
+			,intTransactionTypeId = @intInventoryTransactionType
+	FROM	tblLGLoadDetail LD
+			JOIN tblLGLoad L ON L.intLoadId = LD.intLoadId
+			JOIN tblLGLoadDetailLot LDLot ON LD.intLoadDetailId = LDLot.intLoadDetailId
+			JOIN tblICLot Lot ON Lot.intLotId = LDLot.intLotId
+	WHERE	LD.intLoadDetailId = @intLoadDetailId
+END
 
 -- Do the reservations
 BEGIN 
@@ -62,11 +95,16 @@ BEGIN
 	-- If there are enough stocks, let the system create the reservations
 	IF (@intInvalidItemId IS NULL)	
 	BEGIN 
-		EXEC dbo.uspICCreateStockReservation
-			@ItemsToReserve
-			,@intPickLotHeaderId
-			,@intInventoryTransactionType	
+		IF (@intLoadId IS NOT NULL)
+			EXEC dbo.uspICCreateStockReservation
+				@ItemsToReserve
+				,@intLoadId
+				,@intInventoryTransactionType	
+		ELSE
+			EXEC dbo.uspICCreateStockReservation
+				@ItemsToReserve
+				,@intPickLotHeaderId
+				,@intInventoryTransactionType	
 	END 	
 
 END 
-
