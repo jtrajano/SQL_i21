3ccd0a06c02554eed2ed3d4eb9dcbf99b3bfe7de
Ian Ocampo <ian.ocampo@irely.com>
2019-03-22 22:10:15 +0800

    FRM-8273

--- i21Database/dbo/Stored Procedures/uspSMInsertTransaction.sql
+++ i21Database/dbo/Stored Procedures/uspSMInsertTransaction.sql
@@ -7,15 +7,15 @@
 @output INT OUTPUT
 AS
 BEGIN
-	
+
 	DECLARE @screenId INT = (SELECT intScreenId FROM tblSMScreen WHERE strNamespace = @screenNamespace)
 	DECLARE @transactionId INT;
 	  (SELECT @transactionId = transactions.intTransactionId
 				 FROM  tblSMScreen screen
-				INNER JOIN tblSMTransaction transactions ON screen.intScreenId = transactions.intScreenId 
+				INNER JOIN tblSMTransaction transactions ON screen.intScreenId = transactions.intScreenId
 				where strNamespace = @screenNamespace AND  transactions.intRecordId =  @intKeyValue)
 
-	IF(ISNULL(@transactionId,'') = '')
+	IF(ISNULL(@transactionId,'') = '' AND @screenId IS NOT NULL)
 		BEGIN
 
 			INSERT INTO tblSMTransaction
@@ -27,13 +27,13 @@ BEGIN
 				intRecordId,
 				intConcurrencyId
 			)
-			VALUES (@screenId, 
+			VALUES (@screenId,
 					@strTransactionNo,
 					@intEntityId,
 					@dtmDate,
 					@intKeyValue,
 					1)
-		
+
 		SET @output = (SELECT SCOPE_IDENTITY())
 		END
 	ELSE
