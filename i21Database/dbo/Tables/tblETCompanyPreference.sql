2b3577132a130ec6ad5ac105caf618d44c661f1a
manny armas <manny.armas@iRely.com>
2019-03-25 09:53:37 +0800

    IET-338 - Import/Export > Add Support for Multiple In Truck Integrations

--- i21Database/dbo/Tables/tblETCompanyPreference.sql
+++ i21Database/dbo/Tables/tblETCompanyPreference.sql
@@ -8,5 +8,15 @@
 	[strUploadPath] [nvarchar](max) COLLATE Latin1_General_CI_AS NULL,
 	[strArchivePath] [nvarchar](150) COLLATE Latin1_General_CI_AS NULL, 
     [strIntegration] NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL DEFAULT 'Energy Trac',
-
+    [ysnETEnableIntegration] BIT NULL DEFAULT ((0)), 
+	[strBEBasePath] [nvarchar](max) COLLATE Latin1_General_CI_AS NULL,
+	[strBEExportPath] [nvarchar](max) COLLATE Latin1_General_CI_AS NULL,
+	[strBEUploadPath] [nvarchar](max) COLLATE Latin1_General_CI_AS NULL,
+	[strBEArchivePath] [nvarchar](150) COLLATE Latin1_General_CI_AS NULL, 
+	[ysnBEEnableIntegration] BIT NULL DEFAULT ((0)), 
+	[strDDBasePath] [nvarchar](max) COLLATE Latin1_General_CI_AS NULL,
+	[strDDExportPath] [nvarchar](max) COLLATE Latin1_General_CI_AS NULL,
+	[strDDUploadPath] [nvarchar](max) COLLATE Latin1_General_CI_AS NULL,
+	[strDDArchivePath] [nvarchar](150) COLLATE Latin1_General_CI_AS NULL, 
+	[ysnDDEnableIntegration] BIT NULL DEFAULT ((0))
 )
