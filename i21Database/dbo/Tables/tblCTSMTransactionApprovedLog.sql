739dae321ddf6fa657dbe39f5be04e92812e5a5f
Satya S Sahoo <satya.sahoo@irely.com>
2019-03-25 21:40:28 +0530

    CT-3084 - Missing PO for contract 1005476.25 which is approved - 18.1B

---
+++ i21Database/dbo/Tables/tblCTSMTransactionApprovedLog.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[tblCTSMTransactionApprovedLog]
+(
+	[intTransactionApprovedLogId] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	strType NVARCHAR(MAX),
+	intRecordId INT,
+	dtmLog DATETIME,
+	strErrMsg NVARCHAR(MAX)
+)
