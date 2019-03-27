78a0d72cb956fb8f3bbebd6fd61175dd4743532f
COMTECH\BPonce <bonn.ponce@irely.com>
2019-03-27 19:28:04 +0800

    CF-2227

--- i21Database/dbo/Tables/tblCFFactorTaxGroupXRef.sql
+++ i21Database/dbo/Tables/tblCFFactorTaxGroupXRef.sql
@@ -1,10 +1,15 @@
 ﻿CREATE TABLE [dbo].[tblCFFactorTaxGroupXRef] (
     [intFactorTaxGroupXRefId]  INT            IDENTITY (1, 1) NOT NULL,
 	[intCustomerId]		INT NULL,
-	[intTaxGroupId]		INT NULL,
+    [strFactorTaxGroup]   NVARCHAR (250) COLLATE Latin1_General_CI_AS NULL,
     [strState]   NVARCHAR (250) COLLATE Latin1_General_CI_AS NULL,
     [intConcurrencyId] INT            CONSTRAINT [DF_tblCFFactorTaxGroupXRef_intConcurrencyId] DEFAULT ((1)) NULL,
     CONSTRAINT [PK_tblCFFactorTaxGroupXRef] PRIMARY KEY CLUSTERED ([intFactorTaxGroupXRefId] ASC)
 );
+GO
 
+
+CREATE UNIQUE NONCLUSTERED INDEX [tblCFFactorTaxGroupXRef_UniqueCustomerState]
+    ON [dbo].[tblCFFactorTaxGroupXRef]([intCustomerId] ASC, [strState] ASC);
+GO
 
