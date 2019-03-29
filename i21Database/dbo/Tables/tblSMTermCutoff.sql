3fbfedc6aebf43ca62257236d53abefbc0b52155
Mon Gonzales <mon.gonzales@irely.com>
2019-03-29 18:44:28 +0800

    SM-4620 - CLONE - Multiple due date and cutoff days in Terms Code by Specific Date

---
+++ i21Database/dbo/Tables/tblSMTermCutoff.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[tblSMTermCutoff]
+(
+	[intTermCutoffId]		INT PRIMARY KEY IDENTITY (1, 1)		NOT NULL,
+	[intTermId]				INT									NOT NULL,
+	--[dtmStartDate]			DATETIME							NOT NULL, 
+	--[dtmEndDate]			DATETIME							NOT NULL, 
+	[intFromDate]			INT									NOT NULL,
+	[intToDate]				INT									NOT NULL,
+	[intCutoff]				INT									NOT NULL,
+	[intConcurrencyId]		[int]												NOT NULL DEFAULT ((1)), 
+	CONSTRAINT [FK_tblSMTermCutoff_tblSMTerm] FOREIGN KEY ([intTermId]) REFERENCES [tblSMTerm]([intTermID]),
+)
