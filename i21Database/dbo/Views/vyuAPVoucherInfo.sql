db29007cca9b5e9424c58c46f41ffc790e430c01
Erick <erick.delacruz@irely.com>
2019-03-31 11:07:27 +0800

    AP-6931

---
+++ i21Database/dbo/Views/vyuAPVoucherInfo.sql
@@ -0,0 +1,32 @@
+﻿CREATE VIEW [dbo].[vyuAPVoucherInfo]
+AS 
+
+WITH 
+lastVoucher
+(
+	dblLastVoucherTotal,
+	dblLastVoucherDate
+)
+AS
+(
+	SELECT TOP 1
+		dblLastVoucherTotal		=	A.dblTotal,
+		dblLastVoucherDate		=	A.dtmDate
+	FROM tblAPBill A
+	ORDER BY intBillId DESC
+),
+lastYearVoucherTotal(dblLastYearVoucherTotal)
+AS
+(
+	SELECT dbo.[fnAPGetLYVoucher]()
+),
+ytdVouchers(dblYTDVoucherTotal)
+AS
+(
+	SELECT dbo.[fnAPGetYTDPurchases]()
+)
+
+SELECT 
+	CAST(ROW_NUMBER() OVER (ORDER BY (SELECT 1)) AS INT) AS intVoucherInfoId,
+	* 
+FROM lastVoucher, lastYearVoucherTotal, ytdVouchers;
