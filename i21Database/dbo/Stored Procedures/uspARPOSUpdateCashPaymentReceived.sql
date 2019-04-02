0201910296ed0617354aa811bb79c3aa3f7e83d6
Jc Borlagdan <jcborlagdan@ymail.com>
2019-04-03 05:44:55 +0800

    AR-9994

---
+++ i21Database/dbo/Stored Procedures/uspARPOSUpdateCashPaymentReceived.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[uspARPOSUpdateCashPaymentReceived]
+	 @intPOSEndOfDayId INT
+	,@intPaymentId INT
+	,@strMessage AS VARCHAR(50) OUTPUT 
+AS
+BEGIN
+	UPDATE POSPAYMENT
+	SET
+		  POSPAYMENT.intPOSEndOfDayId = @intPOSEndOfDayId
+		, POSPAYMENT.intPaymentId = @intPaymentId
+	FROM tblARPaymentDetail PAYMENTDETAIL
+	INNER JOIN tblARPOS POS ON PAYMENTDETAIL.intInvoiceId = POS.intInvoiceId
+	INNER JOIN tblARPOSPayment POSPAYMENT ON POS.intPOSId = POSPAYMENT.intPOSId
+	WHERE PAYMENTDETAIL.intPaymentId = @intPaymentId
+
+END
+GO
