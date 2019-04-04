7dc979260ef65c9aec01176d72572962b3774fbf
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2019-04-04 12:00:08 +0530

    Fix Merge issue on RK

--- i21Database/dbo/Views/vyuRKGetContractBalanceLotForHeader.sql
+++ i21Database/dbo/Views/vyuRKGetContractBalanceLotForHeader.sql
@@ -13,6 +13,6 @@ CASE WHEN ISNULL(dblAvailableLot,0)-dblSelectedLot <=0 then 0 else isnull(dblAva
 FROM vyuCTContractDetailView cd
 WHERE cd.intFutureMarketId IS NOT NULL --AND cd.intFutureMonthId IS NOT NULL  
 and intContractHeaderId not IN (select top 1 intContractHeaderId from tblCTContractDetail where intContractStatusId not in(2,3)) 
-and isnull(ysnMultiplePriceFixation,0) = 0
+and isnull(ysnMultiplePriceFixation,0) = 1
 GROUP BY strContractNumber,cd.intContractHeaderId,cd.intFutureMarketId,cd.intFutureMonthId,cd.strContractType,cd.intContractTypeId,cd.strEntityName, cd.intContractStatusId)t
 WHERE intContractStatusId NOT IN (3,5,6)
