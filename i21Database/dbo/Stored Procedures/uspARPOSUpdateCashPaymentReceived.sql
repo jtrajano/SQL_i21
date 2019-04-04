bc7bb8da9b1ade93707c9247ba3422f2feb18a66
Jc Borlagdan <jcborlagdan@ymail.com>
2019-04-04 21:46:59 +0800

    AR-9994

--- i21Database/dbo/Stored Procedures/uspARPOSUpdateCashPaymentReceived.sql
+++ i21Database/dbo/Stored Procedures/uspARPOSUpdateCashPaymentReceived.sql
@@ -4,14 +4,28 @@
 	,@strMessage AS VARCHAR(50) OUTPUT 
 AS
 BEGIN
-	UPDATE POSPAYMENT
-	SET
-		  POSPAYMENT.intPOSEndOfDayId = @intPOSEndOfDayId
-		, POSPAYMENT.intPaymentId = @intPaymentId
-	FROM tblARPaymentDetail PAYMENTDETAIL
-	INNER JOIN tblARPOS POS ON PAYMENTDETAIL.intInvoiceId = POS.intInvoiceId
-	INNER JOIN tblARPOSPayment POSPAYMENT ON POS.intPOSId = POSPAYMENT.intPOSId
-	WHERE PAYMENTDETAIL.intPaymentId = @intPaymentId
+
+	IF EXISTS(SELECT TOP 1 NULL FROM tblARPayment WHERE intPaymentId = @intPaymentId)
+	BEGIN
+		UPDATE POSPAYMENT
+		SET
+			  POSPAYMENT.intPOSEndOfDayId = @intPOSEndOfDayId
+			, POSPAYMENT.intPaymentId = @intPaymentId
+		FROM tblARPaymentDetail PAYMENTDETAIL
+		INNER JOIN tblARPOS POS ON PAYMENTDETAIL.intInvoiceId = POS.intInvoiceId
+		INNER JOIN tblARPOSPayment POSPAYMENT ON POS.intPOSId = POSPAYMENT.intPOSId
+		WHERE PAYMENTDETAIL.intPaymentId = @intPaymentId
+
+		UPDATE EOD 
+		SET
+			dblCashPaymentReceived = ISNULL(dblCashPaymentReceived, 0.000000) + PAYMENTDETAIL.dblPayment
+		FROM tblARPOSEndOfDay EOD
+		INNER JOIN tblARPOSPayment POSPAYMENT ON EOD.intPOSEndOfDayId = POSPAYMENT.intPOSEndOfDayId
+		INNER JOIN tblARPaymentDetail PAYMENTDETAIL ON POSPAYMENT.intPaymentId = PAYMENTDETAIL.intPaymentId
+		INNER JOIN tblARPayment PAYMENT ON POSPAYMENT.intPaymentId = PAYMENT.intPaymentId
+		INNER JOIN tblSMPaymentMethod PAYMENTMETHOD ON PAYMENT.intPaymentMethodId = PAYMENTMETHOD.intPaymentMethodID
+		WHERE POSPAYMENT.intPaymentId = @intPaymentId AND PAYMENTMETHOD.strPaymentMethod IN('Cash', 'Check')
+	END
 
 END
 GO
