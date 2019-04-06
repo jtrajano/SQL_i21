6a5e4087b22cfc460036352f0ac4748cef756aff
Ruben Escarcha <ruben.escarcha@irely.com>
2019-04-06 15:32:43 +0800

    AR-10093 Outbound Tax Report - Multi-select not implemented for filters Company Location, Category, Account Status Codes, Invoice Source Type

--- i21Database/dbo/Functions/fnARParseReportParameter.sql
+++ i21Database/dbo/Functions/fnARParseReportParameter.sql
@@ -11,87 +11,87 @@ RETURNS NVARCHAR(MAX)
 AS
 BEGIN
 DECLARE @Where AS NVARCHAR(MAX)
-      , @From  AS NVARCHAR(100)
-      , @To    AS NVARCHAR(100)
+      , @LocalFrom  AS NVARCHAR(100)
+      , @LocalTo    AS NVARCHAR(100)
 SET @Where = NULL
 
 
 IF(@datatype = 'DateTime')
 BEGIN
-    SET @From = CAST(CAST((CASE WHEN ISNULL(@from,'') <> '' THEN @from ELSE CAST(-53690 AS DATETIME) END) AS DATE) AS NVARCHAR(100))
-    SET @To = CAST(CAST((CASE WHEN ISNULL(@to,'') <> '' THEN @to ELSE GETDATE() END) AS DATE) AS NVARCHAR(100))
+    SET @LocalFrom = CAST(CAST((CASE WHEN ISNULL(@from,'') <> '' THEN @from ELSE CAST(-53690 AS DATETIME) END) AS DATE) AS NVARCHAR(100))
+    SET @LocalTo = CAST(CAST((CASE WHEN ISNULL(@to,'') <> '' THEN @to ELSE GETDATE() END) AS DATE) AS NVARCHAR(100))
 	SET @fieldname = 'CAST(' + @fieldname + ' AS DATE)'
 END
 
 IF(@datatype = 'Boolean')
 BEGIN
-    SET @From = CAST(CAST((CASE WHEN ISNULL(ISNULL(@from, @to),'') <> '' THEN ISNULL(@from, @to) ELSE 0 END) AS BIT) AS NVARCHAR(100))
-    SET @To = CAST(CAST((CASE WHEN ISNULL(@to,'') <> '' THEN @to ELSE 0 END) AS BIT) AS NVARCHAR(100))
+    SET @LocalFrom = CAST(CAST((CASE WHEN ISNULL(ISNULL(@from, @to),'') <> '' THEN ISNULL(@from, @to) ELSE 0 END) AS BIT) AS NVARCHAR(100))
+    SET @LocalTo = CAST(CAST((CASE WHEN ISNULL(@to,'') <> '' THEN @to ELSE 0 END) AS BIT) AS NVARCHAR(100))
 END
 
 IF(@datatype = 'int')
 BEGIN
-    SET @From = CAST(CAST((CASE WHEN ISNULL(ISNULL(@from, @to),'') <> '' THEN ISNULL(@from, @to) ELSE 0 END) AS INT) AS NVARCHAR(100))
-    SET @To = CAST(CAST((CASE WHEN ISNULL(@to,'') <> '' THEN @to ELSE 0 END) AS INT) AS NVARCHAR(100))
+    SET @LocalFrom = CAST(CAST((CASE WHEN ISNULL(ISNULL(@from, @to),'') <> '' THEN ISNULL(@from, @to) ELSE 0 END) AS INT) AS NVARCHAR(100))
+    SET @LocalTo = CAST(CAST((CASE WHEN ISNULL(@to,'') <> '' THEN @to ELSE 0 END) AS INT) AS NVARCHAR(100))
 END
 
 IF(@datatype = 'String')
 BEGIN
-    SET @From = ISNULL(@from,'')
-    SET @To = ISNULL(@to,'')
+    SET @LocalFrom = ISNULL(@from,'')
+    SET @LocalTo = ISNULL(@to,'')
 END
 
 IF UPPER(@condition) = UPPER('Equal To')
 BEGIN
     IF @datatype IN ('DateTime','String')
     BEGIN
-        SET @From = '''' + @From + ''''
-        SET @To = '''' + @To + ''''
+        SET @LocalFrom = '''' + @LocalFrom + ''''
+        SET @LocalTo = '''' + @LocalTo + ''''
     END
 
-    SET @Where = @fieldname + ' = ' + @From
+    SET @Where = @fieldname + ' = ' + @LocalFrom
 END
 
 IF UPPER(@condition) = UPPER('Not Equal To')
 BEGIN
     IF @datatype IN ('DateTime','String')
     BEGIN
-        SET @From = '''' + @From + ''''
-        SET @To = '''' + @To + ''''
+        SET @LocalFrom = '''' + @LocalFrom + ''''
+        SET @LocalTo = '''' + @LocalTo + ''''
     END
 
-    SET @Where = @fieldname + ' <> ' + @From
+    SET @Where = @fieldname + ' <> ' + @LocalFrom
 END
 
 IF UPPER(@condition) = UPPER('Like')
 BEGIN
-    SET @Where = @fieldname + ' LIKE ''%' + @From + '%'''
+    SET @Where = @fieldname + ' LIKE ''%' + @LocalFrom + '%'''
 END
 
 IF UPPER(@condition) = UPPER('Not Like')
 BEGIN
-    SET @Where = @fieldname + ' NOT LIKE ''%' + @From + '%'''
+    SET @Where = @fieldname + ' NOT LIKE ''%' + @LocalFrom + '%'''
 END
 
 IF UPPER(@condition) = UPPER('Starts With')
 BEGIN
-    SET @Where = @fieldname + ' LIKE ''' + @From + '%'''
+    SET @Where = @fieldname + ' LIKE ''' + @LocalFrom + '%'''
 END
 
 IF UPPER(@condition) = UPPER('Ends With')
 BEGIN
-    SET @Where = @fieldname + ' LIKE ''%' + @From + ''''
+    SET @Where = @fieldname + ' LIKE ''%' + @LocalFrom + ''''
 END
 
 IF UPPER(@condition) = UPPER('Between')
 BEGIN
     IF @datatype IN ('DateTime','String')
     BEGIN
-        SET @From = '''' + @From + ''''
-        SET @To = '''' + @To + ''''
+        SET @LocalFrom = '''' + @LocalFrom + ''''
+        SET @LocalTo = '''' + @LocalTo + ''''
     END
 
-    SET @Where = @fieldname + ' BETWEEN ' + @From + ' AND ' + @To
+    SET @Where = @fieldname + ' BETWEEN ' + @LocalFrom + ' AND ' + @LocalTo
 END
 
 RETURN @Where + ' ' + ISNULL(@join, '') + ' '
