fa1a2f27f1d574e508091607d5a57265b54c2777
Premkumar S <prem.selladurai@irely.com>
2019-04-08 20:32:17 +0530

    IN-366

---
+++ i21Database/dbo/Views/vyuMFGetInventoryBySubLocation.sql
@@ -0,0 +1,35 @@
+﻿CREATE VIEW vyuMFGetInventoryBySubLocation
+AS
+SELECT [Location Name] = c.strLocationName
+	,[Commodity Code] = cd.strCommodityCode
+	,[Product Type] = att.strDescription
+	,[Item No] = i.strItemNo
+	,[Item Description] = i.strDescription
+	,[Sub Location Name] = sc.strSubLocationName
+	,Stock = SUM((sm.dblOnHand + sm.dblUnitStorage))
+	,[Unit Measure] = um.strUnitMeasure
+FROM tblICItemStockUOM sm
+INNER JOIN tblICItemUOM im ON im.intItemUOMId = sm.intItemUOMId
+	AND im.ysnStockUnit = 1
+INNER JOIN tblICItem i ON i.intItemId = sm.intItemId
+INNER JOIN tblICItemLocation il ON il.intItemId = sm.intItemId
+	AND il.intItemLocationId = sm.intItemLocationId
+	AND il.intLocationId = 1
+INNER JOIN tblICUnitMeasure um ON um.intUnitMeasureId = im.intUnitMeasureId
+JOIN tblSMCompanyLocationSubLocation sc ON sc.intCompanyLocationSubLocationId = sm.intSubLocationId
+LEFT OUTER JOIN tblICCommodity cd ON cd.intCommodityId = i.intCommodityId
+INNER JOIN tblSMCompanyLocation c ON c.intCompanyLocationId = il.intLocationId
+LEFT JOIN tblICCommodityAttribute att ON att.intCommodityAttributeId = i.intProductTypeId
+WHERE i.strType IN (
+		N'Inventory'
+		,N'Finished Good'
+		,N'Raw Material'
+		)
+GROUP BY sm.intItemId
+	,sc.strSubLocationName
+	,c.strLocationName
+	,i.strItemNo
+	,i.strDescription
+	,cd.strCommodityCode
+	,um.strUnitMeasure
+	,att.strDescription
