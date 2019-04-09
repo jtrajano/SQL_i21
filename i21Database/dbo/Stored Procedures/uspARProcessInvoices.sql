cc3404e361a8164d10774b32243160a9e4fa896a
Kelvin Quinto <kelvin.quinto@irely.com>
2019-04-09 10:54:10 +0800

    AR-10053

--- i21Database/dbo/Stored Procedures/uspARProcessInvoices.sql
+++ i21Database/dbo/Stored Procedures/uspARProcessInvoices.sql
@@ -386,7 +386,7 @@ BEGIN
 		,@TransactionId 				= (CASE WHEN ISNULL([strSourceTransaction],'') IN ('Card Fueling Transaction', 'CF Tran') THEN ISNULL([intTransactionId], [intSourceId]) ELSE NULL END)
 		,@MeterReadingId				= (CASE WHEN ISNULL([strSourceTransaction],'') = 'Meter Billing' THEN ISNULL([intMeterReadingId], [intSourceId]) ELSE NULL END)
 		,@ContractHeaderId				= (CASE WHEN ISNULL([strSourceTransaction],'') = 'Sales Contract' THEN ISNULL([intContractHeaderId], [intSourceId]) ELSE NULL END)
-		,@LoadId						= (CASE WHEN ISNULL([strSourceTransaction],'') IN ('Load Schedule', 'Weight Claim') THEN ISNULL([intLoadId], [intSourceId]) ELSE NULL END)
+		,@LoadId						= (CASE WHEN ISNULL([strSourceTransaction],'') IN ('Load Schedule', 'Weight Claim') OR [strTransactionType] = 'Credit Memo' THEN ISNULL([intLoadId], [intSourceId]) ELSE NULL END)
 		,@OriginalInvoiceId				= (CASE WHEN ISNULL([strSourceTransaction],'') = 'Provisional' OR ((ISNULL([strSourceTransaction],'') = 'Invoice' OR ISNULL([strSourceTransaction],'') = 'Direct') AND ISNULL([strTransactionType],'') = 'Credit Memo') OR ISNULL([strTransactionType],'') = 'Cash Refund' THEN ISNULL([intOriginalInvoiceId], [intSourceId]) ELSE NULL END)
 		,@EntityId						= [intEntityId]
 		,@TruckDriverId					= [intTruckDriverId]
