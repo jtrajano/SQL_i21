80ca026b6666ecb16ab0e072a47dce10fafa0347
Erick <erick.delacruz@irely.com>
2019-04-10 13:23:19 +0800

    AP-7118

--- i21Database/dbo/Stored Procedures/uspAPCreateBillReversal.sql
+++ i21Database/dbo/Stored Procedures/uspAPCreateBillReversal.sql
@@ -15,6 +15,19 @@ DECLARE @postParam NVARCHAR(50);
 DECLARE @batchId NVARCHAR(50);
 DECLARE @error NVARCHAR(200);
 DECLARE @debitMemoRecordNum NVARCHAR(50);
+DECLARE @SavePoint NVARCHAR(32) = 'uspAPCreateBillReversal';
+
+BEGIN TRY
+
+--MAKE SURE BILL TYPE ONLY
+IF (SELECT intTransactionType FROM tblAPBill WHERE intBillId = @billId) <> 1
+BEGIN
+	RAISERROR('Invalid transaction type for bill reversal.', 16, 1);
+	RETURN;
+END
+
+IF @transCount = 0 BEGIN TRANSACTION
+ELSE SAVE TRAN @SavePoint
 
 EXEC uspAPDuplicateBill @billId = @billId, @userId = @userId, @type = 3, @billCreatedId = @createdReversal OUT
 EXEC uspSMGetStartingNumber 18, @debitMemoRecordNum OUTPUT
@@ -59,3 +72,57 @@ BEGIN
 	FROM tblAPBill A
 	WHERE A.intBillId = @billId
 END
+
+IF @transCount = 0
+BEGIN
+	IF (XACT_STATE()) = -1
+	BEGIN
+		ROLLBACK TRANSACTION
+	END
+	ELSE IF (XACT_STATE()) = 1
+	BEGIN
+		COMMIT TRANSACTION
+	END
+END	
+
+END TRY
+BEGIN CATCH
+	DECLARE @ErrorSeverity INT,
+			@ErrorNumber   INT,
+			@ErrorMessage nvarchar(4000),
+			@ErrorState INT,
+			@ErrorLine  INT,
+			@ErrorProc nvarchar(200);
+	-- Grab error information from SQL functions
+	SET @ErrorSeverity = ERROR_SEVERITY()
+	SET @ErrorNumber   = ERROR_NUMBER()
+	SET @ErrorMessage  = ERROR_MESSAGE()
+	SET @ErrorState    = ERROR_STATE()
+	SET @ErrorLine     = ERROR_LINE()
+
+	IF @transCount = 0
+		BEGIN
+			IF (XACT_STATE()) = -1
+			BEGIN
+				ROLLBACK TRANSACTION
+			END
+			ELSE IF (XACT_STATE()) = 1
+			BEGIN
+				COMMIT TRANSACTION
+			END
+		END		
+	-- ELSE
+	-- 	BEGIN
+	-- 		IF (XACT_STATE()) = -1
+	-- 		BEGIN
+	-- 			ROLLBACK TRANSACTION  @SavePoint
+	-- 		END
+	-- 	END	
+
+	SET @error = @ErrorMessage;
+	
+	IF @throwError = 1
+	BEGIN
+		RAISERROR (@ErrorMessage , @ErrorSeverity, @ErrorState, @ErrorNumber)
+	END
+END CATCH
