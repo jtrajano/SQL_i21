2adc230a8a3a01afde0585a3860ead97580280ea
Jeff Trajano <jeffrey.trajano@irely.com>
2019-04-11 11:33:16 +0800

    GL-7076 [Trial Balance] Inventory Valuation: Doesn't balance to GL and Trial Balance

---
+++ i21Database/dbo/Stored Procedures/uspGLRebuildSummary.sql
@@ -0,0 +1,40 @@
+﻿CREATE PROCEDURE [dbo].[uspGLRebuildSummary]
+AS
+TRUNCATE TABLE [dbo].[tblGLSummary]
+DECLARE @intCompanyId INT
+SELECT TOP 1 @intCompanyId = intMultiCompanyId FROM tblSMCompanySetup 
+
+INSERT INTO tblGLSummary(
+	intAccountId
+	,intMultiCompanyId
+	,dtmDate
+	,dblDebit
+	,dblCredit
+	,dblDebitForeign
+	,dblCreditForeign 
+	,dblDebitUnit
+	,dblCreditUnit
+	,strCode
+	,intConcurrencyId
+)
+SELECT
+	intAccountId
+	,intMultiCompanyId = ISNULL(intMultiCompanyId, @intCompanyId) 
+	,dtmDate
+	,dblDebit = SUM(ISNULL(dblDebit,0)) 
+	,dblCredit = SUM(ISNULL(dblCredit,0)) 
+	,dblDebitForeign = SUM(ISNULL(dblDebitForeign,0))
+	,dblCreditForeign = SUM(ISNULL(dblCreditForeign,0)) 
+	,dblDebitUnit = SUM(ISNULL(dblDebitUnit,0))
+	,dblCreditUnit = SUM(ISNULL(dblCreditUnit,0)) 
+	,strCode
+	,1 
+FROM tblGLDetail
+WHERE ysnIsUnposted = 0	
+GROUP BY   
+	intAccountId
+	,ISNULL(intMultiCompanyId, @intCompanyId)
+	,dtmDate
+	,strCode
+
+EXEC uspGLRecalcTrialBalance
