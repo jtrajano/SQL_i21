7653c715826e6ac69cf06be35962dba4bc639818
Jeff Trajano <jeffrey.trajano@irely.com>
2019-04-11 09:06:31 +0800

    GL-7013 Bank Account Register does not load any transaction detail.

--- i21Database/dbo/Views/vyuCMBankAccountRegisterRunningBalance.sql
+++ i21Database/dbo/Views/vyuCMBankAccountRegisterRunningBalance.sql
@@ -9,22 +9,18 @@ cteRunningTotal as (
 select a.rowId, sum(b.dblAmount) balance from cteOrdered a join cteOrdered b on a.rowId>= b.rowId 
 group by a.rowId , a.intBankAccountId
 ),
-
 cteBalance as(
-select 
-a.intRunningBalanceId,
-a.rowId, 
-a.intTransactionId, 
-a.dtmDate, 
-intBankAccountId,
-ISNULL(BankRecon.dblStatementOpeningBalance, 0) + 
-( b.balance - a.dblAmount)   dblOpeningBalance,
-CASE WHEN a.dblAmount >= 0 THEN a.dblAmount ELSE 0 END dblDeposit,
-CASE WHEN a.dblAmount < 0 THEN a.dblAmount ELSE 0 END dblPayment,
-ISNULL(BankRecon.dblStatementOpeningBalance, 0) + 
-b.balance dblEndingBalance,
-a.intConcurrencyId
-
+	select 
+	a.intRunningBalanceId,
+	a.rowId, 
+	a.intTransactionId, 
+	a.dtmDate, 
+	intBankAccountId,
+	ISNULL(BankRecon.dblStatementOpeningBalance, 0) + 
+	( b.balance - a.dblAmount)   dblOpeningBalance,
+	ISNULL(BankRecon.dblStatementOpeningBalance, 0) + 
+	b.balance dblEndingBalance,
+	a.intConcurrencyId
 FROM cteOrdered a 
 JOIN cteRunningTotal b on a.rowId = b.rowId 
 OUTER APPLY (
@@ -32,21 +28,42 @@ OUTER APPLY (
 
 ) BankRecon
 )
-select Balance.*,
-dtmDateReconciled,
+select
+Balance.intRunningBalanceId,
+Balance.rowId, 
+Balance.intTransactionId, 
+Balance.dtmDate, 
+Balance.intBankAccountId,
+Balance.dblOpeningBalance,
+Balance.dblEndingBalance,
+Balance.intConcurrencyId,
+CM.dtmDateReconciled,
 CM.intBankTransactionTypeId,
 CM.intCompanyLocationId,
 T.strBankTransactionTypeName,
 L.strLocationName,
-strMemo,
-strPayee,
-strReferenceNo,
-strTransactionId,
-ysnCheckVoid,
-ysnClr
+strMemo = 
+	CASE WHEN CM.strMemo = '' AND CM.ysnCheckVoid = 1 THEN 'Void' 
+	ELSE ISNULL(CM.strMemo, '') END,
+strPayee = 
+	CASE WHEN Employee.ysnMaskEmployeeName = 1 AND CM.intBankTransactionTypeId IN ( 21, 23 ) THEN '(restricted information)' 
+	ELSE ISNULL(CM.strPayee, '') END,
+dblPayment = 
+	CASE WHEN CM.intBankTransactionTypeId IN ( 3, 9, 12, 13, 14, 15, 16, 20, 21, 22, 23 ) THEN CM.dblAmount 
+	WHEN CM.intBankTransactionTypeId IN ( 2, 5 ) AND ISNULL(CM.dblAmount,0) < 0 THEN CM.dblAmount * -1 ELSE 0 END                        , 
+dblDeposit = CASE WHEN CM.intBankTransactionTypeId IN ( 1, 10, 11, 18, 19, 103, 116, 121, 122, 123 ) THEN CM.dblAmount 
+	WHEN CM.intBankTransactionTypeId = 5 AND ISNULL(CM.dblAmount,0) > 0 THEN dblAmount ELSE 0 END, 
+CM.strReferenceNo,
+CM.strTransactionId,
+CM.ysnCheckVoid,
+CM.ysnClr
 FROM tblCMBankTransaction CM 
 JOIN cteBalance Balance ON CM.intTransactionId= Balance.intTransactionId
 LEFT JOIN tblCMBankTransactionType T on T.intBankTransactionTypeId = CM.intBankTransactionTypeId
 LEFT JOIN tblSMCompanyLocation L on L.intCompanyLocationId = CM.intCompanyLocationId
-
-
+OUTER APPLY 
+(
+ 
+    SELECT TOP 1 ysnMaskEmployeeName 
+    FROM   tblPRCompanyPreference 
+) Employee
