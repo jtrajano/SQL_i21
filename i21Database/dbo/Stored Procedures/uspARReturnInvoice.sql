b33593c9f36b5cce93b16a2efb2a040c411d2d6f
Kelvin Quinto <kelvin.quinto@irely.com>
2019-04-11 14:44:36 +0800

    AR-10162

--- i21Database/dbo/Stored Procedures/uspARReturnInvoice.sql
+++ i21Database/dbo/Stored Procedures/uspARReturnInvoice.sql
@@ -165,7 +165,7 @@ BEGIN TRY
 		 [strTransactionType]					= 'Credit Memo'
 		,[strType]								= ARI.[strType]
 		,[strSourceTransaction]					= 'Direct'--'Invoice'
-		,[intSourceId]							= ARI.[intInvoiceId] 
+		,[intSourceId]							= NULL 
 		,[strSourceId]							= ARI.[strInvoiceNumber]
 		,[intInvoiceId]							= NULL
 		,[intEntityCustomerId]					= ARI.[intEntityCustomerId]
@@ -394,16 +394,6 @@ BEGIN CATCH
 END CATCH
 		
 SELECT TOP 1 @NewInvoiceId = intInvoiceId FROM tblARInvoice WHERE intInvoiceId IN (SELECT intID FROM fnGetRowsFromDelimitedValues(@CreatedIvoices))
-
-UPDATE I 
-SET ysnReturned = 1 
-  , dblDiscountAvailable = @ZeroDecimal
-  , dblBaseDiscountAvailable = @ZeroDecimal
-FROM tblARInvoice I
-INNER JOIN (
-	SELECT intID 
-	FROM fnGetRowsFromDelimitedValues(@CreatedIvoices)
-) CI ON I.intInvoiceId = CI.intID
 
 UPDATE tblARInvoice 
 SET ysnReturned = 1 
