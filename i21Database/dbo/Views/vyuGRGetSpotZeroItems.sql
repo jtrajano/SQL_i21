d9b293823a0c9c45d3aca0f0b8e0e863186195d7
Leslie L Arat <leslie.arat@irely.com>
2019-04-12 08:36:40 +0800

    GRN-1483

--- i21Database/dbo/Views/vyuGRGetSpotZeroItems.sql
+++ i21Database/dbo/Views/vyuGRGetSpotZeroItems.sql
@@ -1,19 +1,23 @@
 ﻿CREATE VIEW [dbo].[vyuGRGetSpotZeroItems]
 AS   
 SELECT DISTINCT
- CASE WHEN TicketType.strInOutIndicator='I' THEN 'Inbound' ELSE 'Outbound' END COLLATE Latin1_General_CI_AS as strInOutIndicator
-,intItemId		   = SC.intItemId
-,strItemNo		   = Item.strItemNo
-,ysnExchangeTraded = Com.ysnExchangeTraded
-,intFutureMarketId = ISNULL(Com.intFutureMarketId,0)
-,dblPriceCheckMin  = Com.dblPriceCheckMin
-,dblPriceCheckMax  = Com.dblPriceCheckMax
+	 strInOutIndicator	= CASE WHEN TicketType.strInOutIndicator = 'I' THEN 'Inbound' ELSE 'Outbound' END COLLATE Latin1_General_CI_AS
+	,intItemId			= SC.intItemId
+	,strItemNo			= Item.strItemNo
+	,ysnExchangeTraded	= Com.ysnExchangeTraded
+	,intFutureMarketId	= ISNULL(Com.intFutureMarketId,0)
+	,dblPriceCheckMin	= Com.dblPriceCheckMin
+	,dblPriceCheckMax	= Com.dblPriceCheckMax
 FROM tblSCTicket SC
-JOIN tblICItem Item ON Item.intItemId = SC.intItemId
-JOIN tblSCListTicketTypes TicketType ON TicketType.intTicketTypeId = SC.intTicketTypeId
-JOIN tblICCommodity Com ON Com.intCommodityId=Item.intCommodityId
+JOIN tblICItem Item 
+	ON Item.intItemId = SC.intItemId
+JOIN tblSCListTicketTypes TicketType 
+	ON TicketType.intTicketTypeId = SC.intTicketTypeId
+JOIN tblICCommodity Com 
+	ON Com.intCommodityId = Item.intCommodityId
 WHERE 
-ISNULL(SC.dblUnitPrice,0) = 0 
-AND ISNULL(SC.dblUnitBasis,0) = 0
-AND SC.intStorageScheduleTypeId = -3
-AND SC.strTicketStatus = 'C'
+	ISNULL(SC.dblUnitPrice,0) = 0 
+	AND ISNULL(SC.dblUnitBasis,0) = 0
+	AND SC.intStorageScheduleTypeId = -3
+	AND SC.strTicketStatus = 'C'
+	AND SC.ysnDestinationWeightGradePost = 1
