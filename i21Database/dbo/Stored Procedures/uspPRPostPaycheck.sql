7f205523048a28c4cb8a1e9af69e319fdf919a40
Jonas Rafael Sarcilla <jonas.sarcilla@irely.com>
2019-04-15 13:00:46 +0800

    PR-1936 - Payroll GL distribution not working correctly

--- i21Database/dbo/Stored Procedures/uspPRPostPaycheck.sql
+++ i21Database/dbo/Stored Procedures/uspPRPostPaycheck.sql
@@ -359,7 +359,7 @@ BEGIN
 	--PERFORM GL ACCOUNT SEGMENT SWITCHING
 	UPDATE #tmpDeduction 
 	SET intAccountId = dbo.fnPRGetAccountIdWithThisLocationLOB(intAccountId, intProfitCenter, intLOB)
-	WHERE ysnSplit = 1 OR (ysnSplit = 0 AND intDepartmentId IS NOT NULL)
+	WHERE ysnSplit = 1 OR (ysnSplit = 0 AND intDepartmentId IS NOT NULL AND intLOB IS NOT NULL)
 
 	--PERFORM AMOUNT DISTRIBUTION
 	--Place Deduction to Temporary Table to Distribute Amounts
@@ -512,7 +512,7 @@ BEGIN
 	--PERFORM GL ACCOUNT SEGMENT SWITCHING
 	UPDATE #tmpTax
 	SET intAccountId = dbo.fnPRGetAccountIdWithThisLocationLOB(intAccountId, intProfitCenter, intLOB)
-	WHERE ysnSplit = 1 OR (ysnSplit = 0 AND intDepartmentId IS NOT NULL)
+	WHERE ysnSplit = 1 OR (ysnSplit = 0 AND intDepartmentId IS NOT NULL AND intLOB IS NOT NULL)
 
 	--PERFORM AMOUNT DISTRIBUTION
 	--Place Tax to Temporary Table to Distribute Amounts
