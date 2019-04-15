31a28480a4f714a567d3af718c0a8738e8c662e1
Leslie L Arat <leslie.arat@irely.com>
2019-04-15 15:21:03 +0800

    Script Corrections (GR)

--- i21Database/dbo/Views/vyuGRStorageStatement.sql
+++ i21Database/dbo/Views/vyuGRStorageStatement.sql
@@ -1,4 +1,19 @@
 ﻿CREATE VIEW [dbo].[vyuGRStorageStatement]
 AS 
-SELECT DISTINCT 1 AS intStorageStatementId,
-strFormNumber,dtmIssueDate,strItemNo,strStorageType FROM tblGRStorageStatement
+SELECT DISTINCT 
+	intStorageStatementId = 1
+	,strFormNumber		  = SS.strFormNumber
+	,dtmIssueDate		  = SS.dtmIssueDate
+	,strItemNo			  = SS.strItemNo
+	,strStorageType		  = SS.strStorageType
+	,strName			  = SV.strName
+	,strScheduleId		  = SV.strScheduleId
+FROM tblGRStorageStatement SS
+JOIN vyuGRStorageSearchView SV 
+	ON SV.intCustomerStorageId = (
+									SELECT TOP 1 intCustomerStorageId
+									FROM tblGRStorageStatement
+									WHERE intStorageStatementId = SS.intStorageStatementId
+								)
+
+
