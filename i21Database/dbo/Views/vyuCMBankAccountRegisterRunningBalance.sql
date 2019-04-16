6c63b23183632cff7ef5cd6989dfab1bf2452ecf
Jeff Trajano <jeffrey.trajano@irely.com>
2019-04-16 10:43:01 +0800

    GL-7066 [Bank Account Register] Wrong sorting of the grid GL-7013 Bank Account Register does not load any transaction detail

--- i21Database/dbo/Views/vyuCMBankAccountRegisterRunningBalance.sql
+++ i21Database/dbo/Views/vyuCMBankAccountRegisterRunningBalance.sql
@@ -1,42 +1,37 @@
+
 CREATE VIEW [dbo].[vyuCMBankAccountRegisterRunningBalance]
 AS
 WITH cteOrdered as
 (
 	select row_number() over(order by dtmDate, intTransactionId) rowId, 
-	dblAmount, 
-	b.intTransactionId,
-	b.intBankAccountId,
-	b.dtmDate 
+	CM.dblAmount,
+	CM.intTransactionId,
+	CM.intBankAccountId,
+	CM.dtmDate ,
+	dblPayment = 
+		CASE WHEN CM.intBankTransactionTypeId IN ( 3, 9, 12, 13, 14, 15, 16, 20, 21, 22, 23 ) THEN CM.dblAmount 
+		WHEN CM.intBankTransactionTypeId IN ( 2, 5 ) AND ISNULL(CM.dblAmount,0) < 0 THEN CM.dblAmount * -1 ELSE 0 END                        , 
+		dblDeposit = CASE WHEN CM.intBankTransactionTypeId IN ( 1, 10, 11, 18, 19, 103, 116, 121, 122, 123 ) THEN CM.dblAmount 
+	WHEN CM.intBankTransactionTypeId = 5 AND ISNULL(CM.dblAmount,0) > 0 THEN CM.dblAmount ELSE 0 END
 	from
-	tblCMBankTransaction b
-	where b.ysnPosted = 1
+	tblCMBankTransaction CM
+	where CM.ysnPosted = 1
 ),
-cteRunningTotal as (
-select a.rowId, sum(b.dblAmount) balance from cteOrdered a join cteOrdered b on a.rowId>= b.rowId 
-group by a.rowId , a.intBankAccountId
-),
-cteBalance as(
-	select 
-	a.rowId, 
-	a.intTransactionId, 
-	a.dtmDate, 
-	intBankAccountId,
-	( b.balance - a.dblAmount)   dblOpeningBalance,
-	b.balance dblEndingBalance
-FROM cteOrdered a 
-JOIN cteRunningTotal b on a.rowId = b.rowId 
-OUTER APPLY (
-    SELECT TOP 1 dblStatementOpeningBalance FROM tblCMBankReconciliation WHERE intBankAccountId = a.intBankAccountId
-
-) BankRecon
+cteRunningTotal as 
+(
+	select a.rowId, sum(b.dblDeposit - b.dblPayment) balance from cteOrdered a join cteOrdered b on a.rowId>= b.rowId 
+	group by a.rowId , a.intBankAccountId
 )
-select --TOP 100
-Balance.rowId, 
-Balance.intTransactionId, 
-Balance.dtmDate, 
-Balance.intBankAccountId,
-Balance.dblOpeningBalance,
-Balance.dblEndingBalance,
+
+select
+Ordered.rowId, 
+dblPayment = Ordered.dblPayment,
+dblDeposit = Ordered.dblDeposit,
+ISNULL(BankRecon.dblStatementOpeningBalance, 0) + ( Total.balance - (Ordered.dblDeposit-Ordered.dblPayment) )   dblOpeningBalance,
+ISNULL(BankRecon.dblStatementOpeningBalance, 0) +  Total.balance dblEndingBalance,
+Ordered.intTransactionId, 
+Ordered.dtmDate, 
+Ordered.intBankAccountId,
 CM.dtmDateReconciled,
 CM.intBankTransactionTypeId,
 CM.intCompanyLocationId,
@@ -48,25 +43,25 @@ strMemo =
 strPayee = 
 	CASE WHEN Employee.ysnMaskEmployeeName = 1 AND CM.intBankTransactionTypeId IN ( 21, 23 ) THEN '(restricted information)' 
 	ELSE ISNULL(CM.strPayee, '') END,
-dblPayment = 
-	CASE WHEN CM.intBankTransactionTypeId IN ( 3, 9, 12, 13, 14, 15, 16, 20, 21, 22, 23 ) THEN CM.dblAmount 
-	WHEN CM.intBankTransactionTypeId IN ( 2, 5 ) AND ISNULL(CM.dblAmount,0) < 0 THEN CM.dblAmount * -1 ELSE 0 END                        , 
-dblDeposit = CASE WHEN CM.intBankTransactionTypeId IN ( 1, 10, 11, 18, 19, 103, 116, 121, 122, 123 ) THEN CM.dblAmount 
-	WHEN CM.intBankTransactionTypeId = 5 AND ISNULL(CM.dblAmount,0) > 0 THEN dblAmount ELSE 0 END, 
 CM.strReferenceNo,
 CM.strTransactionId,
 CM.ysnCheckVoid,
 CM.ysnClr
 FROM tblCMBankTransaction CM 
-JOIN cteBalance Balance ON CM.intTransactionId= Balance.intTransactionId
+JOIN cteOrdered Ordered ON CM.intTransactionId= Ordered.intTransactionId
+JOIN cteRunningTotal Total ON Ordered.rowId = Total.rowId
 LEFT JOIN tblCMBankTransactionType T on T.intBankTransactionTypeId = CM.intBankTransactionTypeId
 LEFT JOIN tblSMCompanyLocation L on L.intCompanyLocationId = CM.intCompanyLocationId
 OUTER APPLY 
 (
- 
     SELECT TOP 1 ysnMaskEmployeeName 
     FROM   tblPRCompanyPreference 
 ) Employee
+OUTER APPLY (
+    SELECT TOP 1 dblStatementOpeningBalance FROM tblCMBankReconciliation WHERE intBankAccountId = CM.intBankAccountId
+
+) BankRecon
 GO
 
 
+--select count(1) from tblCMBankTransaction where ysnPosted =1
