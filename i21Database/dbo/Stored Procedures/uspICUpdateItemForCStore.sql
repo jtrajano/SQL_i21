47498ce8a8757bc61a30fd60ae290e7da3cc9384
Feb Montefrio <feb.montefrio@irely.com>
2019-04-17 14:48:00 +0800

    [B] IC-7106

--- i21Database/dbo/Stored Procedures/uspICUpdateItemForCStore.sql
+++ i21Database/dbo/Stored Procedures/uspICUpdateItemForCStore.sql
@@ -178,92 +178,164 @@ END
 
 IF EXISTS (SELECT TOP 1 1 FROM #tmpUpdateItemForCStore_itemAuditLog)
 BEGIN 
-	DECLARE @json1 AS NVARCHAR(2000) = '{"action":"Updated","change":"Updated - Record: %s","iconCls":"small-menu-maintenance","children":[%s]}'
+	--DECLARE @json1 AS NVARCHAR(2000) = '{"action":"Updated","change":"Updated - Record: %s","iconCls":"small-menu-maintenance","children":[%s]}'
 	
-	DECLARE @json2_int AS NVARCHAR(2000) = '{"change":"%s","iconCls":"small-menu-maintenance","from":"%i","to":"%i","leaf":true}'
-	DECLARE @json2_float AS NVARCHAR(2000) = '{"change":"%s","iconCls":"small-menu-maintenance","from":"%f","to":"%f","leaf":true}'
-	DECLARE @json2_string AS NVARCHAR(2000) = '{"change":"%s","iconCls":"small-menu-maintenance","from":"%s","to":"%s","leaf":true}'
-	DECLARE @json2_date AS NVARCHAR(2000) = '{"change":"%s","iconCls":"small-menu-maintenance","from":"%d","to":"%d","leaf":true}'
+	--DECLARE @json2_int AS NVARCHAR(2000) = '{"change":"%s","iconCls":"small-menu-maintenance","from":"%i","to":"%i","leaf":true}'
+	--DECLARE @json2_float AS NVARCHAR(2000) = '{"change":"%s","iconCls":"small-menu-maintenance","from":"%f","to":"%f","leaf":true}'
+	--DECLARE @json2_string AS NVARCHAR(2000) = '{"change":"%s","iconCls":"small-menu-maintenance","from":"%s","to":"%s","leaf":true}'
+	--DECLARE @json2_date AS NVARCHAR(2000) = '{"change":"%s","iconCls":"small-menu-maintenance","from":"%d","to":"%d","leaf":true}'
 
-	-- Add audit logs for Standard Cost changes. 
-	INSERT INTO tblSMAuditLog(
-			strActionType
-			, strTransactionType
-			, strRecordNo
-			, strDescription
-			, strRoute
-			, strJsonData
-			, dtmDate
-			, intEntityId
-			, intConcurrencyId
-	)
-	SELECT 
-			strActionType = 'Updated'
-			, strTransactionType =  'Inventory.view.Item'
-			, strRecordNo = auditLog.intItemId
-			, strDescription = ''
-			, strRoute = null 
-			, strJsonData = auditLog.strJsonData
-			, dtmDate = GETUTCDATE()
-			, intEntityId = @intEntityUserSecurityId 
-			, intConcurrencyId = 1
-	FROM	(
-		SELECT	intItemId
-				,strJsonData = 
-					dbo.fnFormatMessage(
-						@json1
-						, CAST(intItemId AS NVARCHAR(20)) 
-						, dbo.fnFormatMessage(
-							@json2_string
-							, 'C-Store updates the Category'
-							, category_Original.strCategoryCode
-							, category_New.strCategoryCode
-							, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT
-						) 
-						, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT
-					) 
-		FROM	#tmpUpdateItemForCStore_itemAuditLog auditLog
-				LEFT JOIN tblICCategory category_Original
-					ON auditLog.intCategoryId_Original = category_Original.intCategoryId
-				LEFT JOIN tblICCategory category_New
-					ON auditLog.intCategoryId_New = category_New.intCategoryId
+	---- Add audit logs for Standard Cost changes. 
+	--INSERT INTO tblSMAuditLog(
+	--		strActionType
+	--		, strTransactionType
+	--		, strRecordNo
+	--		, strDescription
+	--		, strRoute
+	--		, strJsonData
+	--		, dtmDate
+	--		, intEntityId
+	--		, intConcurrencyId
+	--)
+	--SELECT 
+	--		strActionType = 'Updated'
+	--		, strTransactionType =  'Inventory.view.Item'
+	--		, strRecordNo = auditLog.intItemId
+	--		, strDescription = ''
+	--		, strRoute = null 
+	--		, strJsonData = auditLog.strJsonData
+	--		, dtmDate = GETUTCDATE()
+	--		, intEntityId = @intEntityUserSecurityId 
+	--		, intConcurrencyId = 1
+	--FROM	(
+	--	SELECT	intItemId
+	--			,strJsonData = 
+	--				dbo.fnFormatMessage(
+	--					@json1
+	--					, CAST(intItemId AS NVARCHAR(20)) 
+	--					, dbo.fnFormatMessage(
+	--						@json2_string
+	--						, 'C-Store updates the Category'
+	--						, category_Original.strCategoryCode
+	--						, category_New.strCategoryCode
+	--						, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT
+	--					) 
+	--					, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT
+	--				) 
+	--	FROM	#tmpUpdateItemForCStore_itemAuditLog auditLog
+	--			LEFT JOIN tblICCategory category_Original
+	--				ON auditLog.intCategoryId_Original = category_Original.intCategoryId
+	--			LEFT JOIN tblICCategory category_New
+	--				ON auditLog.intCategoryId_New = category_New.intCategoryId
 
-		WHERE	ISNULL(intCategoryId_Original, 0) <> ISNULL(intCategoryId_New, 0)
-		UNION ALL
-		SELECT	intItemId
-				, strJsonData = 
-					dbo.fnFormatMessage(
-						@json1
-						, CAST(intItemId AS NVARCHAR(20)) 
-						, dbo.fnFormatMessage(
-							@json2_string
-							, 'C-Store updates the Count Code'
-							, strCountCode_Original
-							, strCountCode_New
-							, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT
-						) 
-						, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT
-					)
-		FROM	#tmpUpdateItemForCStore_itemAuditLog 
-		WHERE	ISNULL(strCountCode_Original, 0) <> ISNULL(strCountCode_New, 0)
-		UNION ALL
-		SELECT	intItemId
-				, strJsonData = 
-					dbo.fnFormatMessage(
-						@json1
-						, CAST(intItemId AS NVARCHAR(20)) 
-						, dbo.fnFormatMessage(
-							@json2_string
-							, 'C-Store updates the Description'
-							, strDescription_Original
-							, strDescription_New
-							, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT
-						) 
-						, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT
-					)
-		FROM	#tmpUpdateItemForCStore_itemAuditLog 
-		WHERE	ISNULL(strDescription_Original, '') <> ISNULL(strDescription_New, '')
+	--	WHERE	ISNULL(intCategoryId_Original, 0) <> ISNULL(intCategoryId_New, 0)
+	--	UNION ALL
+	--	SELECT	intItemId
+	--			, strJsonData = 
+	--				dbo.fnFormatMessage(
+	--					@json1
+	--					, CAST(intItemId AS NVARCHAR(20)) 
+	--					, dbo.fnFormatMessage(
+	--						@json2_string
+	--						, 'C-Store updates the Count Code'
+	--						, strCountCode_Original
+	--						, strCountCode_New
+	--						, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT
+	--					) 
+	--					, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT
+	--				)
+	--	FROM	#tmpUpdateItemForCStore_itemAuditLog 
+	--	WHERE	ISNULL(strCountCode_Original, 0) <> ISNULL(strCountCode_New, 0)
+	--	UNION ALL
+	--	SELECT	intItemId
+	--			, strJsonData = 
+	--				dbo.fnFormatMessage(
+	--					@json1
+	--					, CAST(intItemId AS NVARCHAR(20)) 
+	--					, dbo.fnFormatMessage(
+	--						@json2_string
+	--						, 'C-Store updates the Description'
+	--						, strDescription_Original
+	--						, strDescription_New
+	--						, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT
+	--					) 
+	--					, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT
+	--				)
+	--	FROM	#tmpUpdateItemForCStore_itemAuditLog 
+	--	WHERE	ISNULL(strDescription_Original, '') <> ISNULL(strDescription_New, '')
 
-	) auditLog
-	WHERE auditLog.strJsonData IS NOT NULL 
+	--) auditLog
+	--WHERE auditLog.strJsonData IS NOT NULL 
+
+	DECLARE @auditLog_strDescription AS NVARCHAR(255) 
+			,@auditLog_actionType AS NVARCHAR(50) = 'Updated'
+			,@auditLog_id AS INT 
+			,@auditLog_Old AS NVARCHAR(255)
+			,@auditLog_New AS NVARCHAR(255)
+
+	DECLARE loopAuditLog CURSOR LOCAL FAST_FORWARD
+	FOR 	
+	SELECT	intItemId
+			,strDescription = 
+				CASE 
+					WHEN ISNULL(intCategoryId_Original, 0) <> ISNULL(intCategoryId_New, 0) THEN 
+						'C-Store updates the Category'
+					WHEN ISNULL(strCountCode_Original, 0) <> ISNULL(strCountCode_New, 0) THEN 
+						'C-Store updates the Count Code'
+					WHEN ISNULL(strDescription_Original, '') <> ISNULL(strDescription_New, '') THEN 
+						'C-Store updates the Description'
+				END 
+			,strOld = 
+				CASE 
+					WHEN ISNULL(intCategoryId_Original, 0) <> ISNULL(intCategoryId_New, 0) THEN 
+						category_Original.strCategoryCode
+					WHEN ISNULL(strCountCode_Original, 0) <> ISNULL(strCountCode_New, 0) THEN 
+						strCountCode_Original
+					WHEN ISNULL(strDescription_Original, '') <> ISNULL(strDescription_New, '') THEN 
+						strDescription_Original
+				END 
+			,strNew = 
+				CASE 
+					WHEN ISNULL(intCategoryId_Original, 0) <> ISNULL(intCategoryId_New, 0) THEN 
+						category_New.strCategoryCode
+					WHEN ISNULL(strCountCode_Original, 0) <> ISNULL(strCountCode_New, 0) THEN 
+						strCountCode_New
+					WHEN ISNULL(strDescription_Original, '') <> ISNULL(strDescription_New, '') THEN 
+						strDescription_New
+				END 
+	FROM	#tmpUpdateItemForCStore_itemAuditLog auditLog
+			LEFT JOIN tblICCategory category_Original
+				ON auditLog.intCategoryId_Original = category_Original.intCategoryId
+			LEFT JOIN tblICCategory category_New
+				ON auditLog.intCategoryId_New = category_New.intCategoryId
+	OPEN loopAuditLog;
+
+	FETCH NEXT FROM loopAuditLog INTO 
+		@auditLog_id
+		,@auditLog_strDescription
+		,@auditLog_Old
+		,@auditLog_New
+	;
+	WHILE @@FETCH_STATUS = 0
+	BEGIN 
+		IF @auditLog_strDescription IS NOT NULL 
+		BEGIN 
+			EXEC dbo.uspSMAuditLog 
+				@keyValue = @auditLog_id
+				,@screenName = 'Inventory.view.Item'
+				,@entityId = @intEntityUserSecurityId
+				,@actionType = @auditLog_actionType
+				,@changeDescription = @auditLog_strDescription
+				,@fromValue = @auditLog_Old
+				,@toValue = @auditLog_New
+		END
+		FETCH NEXT FROM loopAuditLog INTO 
+			@auditLog_id
+			,@auditLog_strDescription
+			,@auditLog_Old
+			,@auditLog_New
+		;
+	END 
+	CLOSE loopAuditLog;
+	DEALLOCATE loopAuditLog;
 END 
