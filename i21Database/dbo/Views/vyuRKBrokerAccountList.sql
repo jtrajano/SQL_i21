3d5c9849935463b6353aed7c6bb0781b400054d1
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2019-04-19 00:00:51 +0530

    RM-2882

---
+++ i21Database/dbo/Views/vyuRKBrokerAccountList.sql
@@ -0,0 +1,6 @@
+﻿CREATE VIEW vyuRKBrokerAccountList
+AS
+SELECT DISTINCT ft.intFutureMarketId,ft.intCommodityId,ft.intEntityId,ft.intBrokerageAccountId,b.strAccountNumber,
+		ISNULL(ysnOTCOthers,0) ysnOTCOthers,ISNULL(ft.intInstrumentTypeId,0) intInstrumentTypeId
+FROM tblRKFutOptTransaction ft
+JOIN tblRKBrokerageAccount b ON ft.intBrokerageAccountId=b.intBrokerageAccountId
