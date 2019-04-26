a70deaf0fcef8c712264b5ce1cb0df7f1f86e871
jayson_ramos <jayson.ramos@irely.com>
2019-04-26 14:10:13 +0800

    TM-3047

--- i21Database/dbo/Views/vyuTMSiteOrder.sql
+++ i21Database/dbo/Views/vyuTMSiteOrder.sql
@@ -70,7 +70,7 @@ LEFT JOIN tblICItem D
 	ON A.intProduct = D.intItemId
 LEFT JOIN tblTMRoute F
 	ON A.intRouteId = F.intRouteId	
-LEFT JOIN tblTMDispatch G
+INNER JOIN tblTMDispatch G
 	ON A.intSiteID = G.intSiteID
 LEFT JOIN tblICCategory H
 	ON D.intCategoryId = H.intCategoryId	
