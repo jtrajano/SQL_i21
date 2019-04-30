988450b3753a850c9d8f8ea75ae00422f02c70e9
Feb Montefrio <feb.montefrio@irely.com>
2019-04-30 19:04:10 +0800

    [B] IC-7183

---
+++ i21Database/dbo/Functions/fnDateFromParts.sql
@@ -0,0 +1,13 @@
+﻿CREATE FUNCTION [fnDateFromParts]
+(
+    @year int,
+    @month int,
+    @day int
+)
+RETURNS DATETIME
+AS
+BEGIN
+	DECLARE @d datetime
+	SELECT @d = CAST(CONVERT(VARCHAR, @year) + '-' + CONVERT(VARCHAR, @month) + '-' + CONVERT(VARCHAR, @day) AS DATETIME)
+    RETURN  @d 
+END
