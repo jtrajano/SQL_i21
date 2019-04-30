cb810a3733338a11e40ef2f57a0ce3afee231b88
jayson_ramos <jayson.ramos@irely.com>
2019-04-30 22:13:36 +0800

    HD-3130

---
+++ i21Database/dbo/Views/vyuHDTimeEntryDailySummary.sql
@@ -0,0 +1,27 @@
+﻿CREATE VIEW [dbo].[vyuHDTimeEntryDailySummary]
+	AS
+		select
+			intTimeEntryDailySummaryId
+			,intTimeEntryId
+			,intEntityId
+			,dtmDate
+			,intJiraId
+			,strJiraKey
+			,strJiraStatus
+			,strJiraStatusIconUrl
+			,strTicketId
+			,strTicketNumber
+			,strTicketNumbersDisplay
+			,dtmDateStarted
+			,dtmDateEnded
+			,dblTimeSpent
+			,strDescription
+			,strComments
+			,dblBillableHours
+			,ysnSent
+			,intSendByEntityId
+			,strJIRAUserName
+			,intConcurrencyId
+			,intDate = convert(int, convert(nvarchar(8), dtmDate, 112))
+		from tblHDTimeEntryDailySummary
+
