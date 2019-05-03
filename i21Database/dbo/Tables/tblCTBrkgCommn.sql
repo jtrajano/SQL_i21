f965447e89060edc404a2762dded1575e864590b
Satya S Sahoo <satya.sahoo@irely.com>
2019-05-03 17:49:35 +0530

    CT-3180 - Issues in Brokerage Commission Processing - 19.1

--- i21Database/dbo/Tables/tblCTBrkgCommn.sql
+++ i21Database/dbo/Tables/tblCTBrkgCommn.sql
@@ -10,6 +10,11 @@
 	strComments			   NVARCHAR(MAX)   COLLATE Latin1_General_CI_AS NOT NULL, 
 	intVoucherId		   INT,
 	intInvoiceId	       INT,
+	strType				   NVARCHAR(200),
+	strEntityType		   NVARCHAR(200),
+	strEntityName		   NVARCHAR(200),
+	strStatus			   NVARCHAR(200),
+	strContractNumber	   NVARCHAR(200)
 
 	CONSTRAINT [PK_tblCTBrkgCommn_intBrkgCommnId] PRIMARY KEY CLUSTERED (intBrkgCommnId ASC),
 	CONSTRAINT [UQ_tblCTBrkgCommn_strBatchNumber] UNIQUE (strBatchNumber)
