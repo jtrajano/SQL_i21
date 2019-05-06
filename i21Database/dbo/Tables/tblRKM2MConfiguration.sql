d812be9bed86d687f734ecf637c3ac7f194b86d0
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2019-05-06 19:32:09 +0530

    RM-2865

--- i21Database/dbo/Tables/tblRKM2MConfiguration.sql
+++ i21Database/dbo/Tables/tblRKM2MConfiguration.sql
@@ -4,10 +4,9 @@
 	[intConcurrencyId] INT NOT NULL,
 	[intItemId] INT NOT NULL,
 	[strAdjustmentType] nvarchar(20)  COLLATE Latin1_General_CI_AS NOT NULL,
-	[intContractBasisId] INT NOT NULL,
+	[intContractBasisId] INT NULL,
    	[intFreightTermId] INT NULL, 
     CONSTRAINT [PK_tblRKM2MConfiguration_intM2MConfigurationId] PRIMARY KEY ([intM2MConfigurationId]), 
-    CONSTRAINT [FK_tblRKM2MConfiguration_tblICItem_intItemId] FOREIGN KEY ([intItemId]) REFERENCES [tblICItem]([intItemId]),
-	CONSTRAINT [FK_tblRKM2MConfiguration_tblCTContractBasis_intContractBasisId] FOREIGN KEY ([intContractBasisId]) REFERENCES [tblCTContractBasis]([intContractBasisId]),
+    CONSTRAINT [FK_tblRKM2MConfiguration_tblICItem_intItemId] FOREIGN KEY ([intItemId]) REFERENCES [tblICItem]([intItemId]),	
     CONSTRAINT [FK_tblRKM2MConfiguration_tblSMFreightTerm] FOREIGN KEY ([intFreightTermId]) REFERENCES [tblSMFreightTerms]([intFreightTermId]) 
 )
