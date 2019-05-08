0d7eef6c9166a7ddec5e8183d565a9a63e835e42
henry_zaragoza <henry.zaragoza@irely.com>
2019-05-08 12:21:35 +0800

    ST-1279 [Store] Unable to enter items in store due to POS issue

---
+++ i21Database/dbo/Views/vyuSTLocationItems.sql
@@ -0,0 +1,18 @@
+﻿CREATE VIEW vyuSTLocationItems
+AS
+SELECT DISTINCT
+	I.intItemId -- Unique by intItemId
+    , I.strItemNo
+    , I.strDescription
+    , I.strLotTracking
+    , I.intCategoryId
+    , IL.intLocationId
+    , CL.strLocationName
+    , I.ysnFuelItem
+    , I.strType
+    , I.strStatus
+FROM tblICItem I
+JOIN tblICItemLocation IL
+	ON I.intItemId = IL.intItemId
+JOIN tblSMCompanyLocation CL
+    ON IL.intLocationId = CL.intCompanyLocationId
