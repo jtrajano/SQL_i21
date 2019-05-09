81e3d54ddf0a321b0193911daa8ffa4d40b9ba72
Mon Gonzales <mon.gonzales@irely.com>
2019-05-09 15:22:15 +0800

    IC-6961 - [Items] Able to change Lot Tracking for Item with Transaction

---
+++ i21Database/Scripts/Post-Deployment/IC/17_Inventory_Constraints.sql
@@ -0,0 +1,16 @@
+﻿PRINT N'START- IC Add Constraint'
+GO
+
+-- Add the CHECK CONSTRAINTS in tblICItemUOM
+IF NOT EXISTS(SELECT TOP 1 1 FROM sys.objects WHERE name = 'CK_AllowLotTrackingChange' AND type = 'C' AND parent_object_id = OBJECT_ID('tblICItem', 'U'))
+BEGIN
+	EXEC('
+		ALTER TABLE tblICItem
+		WITH NOCHECK ADD CONSTRAINT CK_AllowLotTrackingChange
+		CHECK (dbo.fnAllowLotTrackingToChange(intItemId, strLotTracking) = 1)'
+	);
+
+END
+
+PRINT N'END - IC Add Constraint'
+GO
