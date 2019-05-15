da28e751a7fc54438343f994a15e8899068778ec
Premkumar S <prem.selladurai@irely.com>
2019-05-15 19:07:52 +0530

    MFG-4044

--- i21Database/dbo/Stored Procedures/uspMFGetTraceabilityInvoiceFromOutboundShipment.sql
+++ i21Database/dbo/Stored Procedures/uspMFGetTraceabilityInvoiceFromOutboundShipment.sql
@@ -11,7 +11,7 @@ SELECT DISTINCT 'Invoice' AS strTransactionName
 	,i.strDescription
 	,mt.intCategoryId
 	,mt.strCategoryCode
-	,ivd.dblQtyOrdered AS dblQuantity
+	,ivd.dblQtyShipped AS dblQuantity
 	,um.strUnitMeasure AS strUOM
 	,iv.dtmDate AS dtmTransactionDate
 	,c.strName AS strVendor
@@ -19,10 +19,10 @@ SELECT DISTINCT 'Invoice' AS strTransactionName
 FROM tblARInvoice iv
 JOIN tblARInvoiceDetail ivd ON iv.intInvoiceId = ivd.intInvoiceId
 JOIN tblLGLoadDetail lg ON lg.intLoadDetailId = ivd.intLoadDetailId
-JOIN tblLGLoad l ON l.intLoadId = lg.intLoadId
+--JOIN tblLGLoad l ON l.intLoadId = lg.intLoadId
 JOIN tblICItem i ON lg.intItemId = i.intItemId
 JOIN tblICCategory mt ON mt.intCategoryId = i.intCategoryId
 LEFT JOIN tblICItemUOM iu ON ivd.intItemUOMId = iu.intItemUOMId
 LEFT JOIN tblICUnitMeasure um ON iu.intUnitMeasureId = um.intUnitMeasureId
 LEFT JOIN vyuARCustomer c ON iv.intEntityCustomerId = c.[intEntityId]
-WHERE l.intLoadId = @intLoadId
+WHERE lg.intLoadId = @intLoadId
