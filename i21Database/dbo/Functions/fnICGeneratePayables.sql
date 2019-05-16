af463e1180850bd819a88c77138bca53076c6745
Mon Gonzales <mon.gonzales@irely.com>
2019-05-16 16:27:53 +0800

    IC-7310 - [Receipts-DebitMemo] Unable to post Debit Memo

--- i21Database/dbo/Functions/fnICGeneratePayables.sql
+++ i21Database/dbo/Functions/fnICGeneratePayables.sql
@@ -109,7 +109,7 @@ DECLARE
 INSERT INTO @table
 SELECT DISTINCT
 	[intEntityVendorId]			=	A.intEntityVendorId
-	,[intTransactionType]		=	CASE WHEN A.strReceiptType = 'Inventory Return' THEN 3 ELSE 1 END 
+	,[intTransactionType]		=	CASE WHEN A.strReceiptType = 'Inventory Return' THEN 3 ELSE @billTypeToUse	 END 
 	,[dtmDate]					=	A.dtmReceiptDate
 	,[strReference]				=	A.strVendorRefNo
 	,[strSourceNumber]			=	A.strReceiptNumber
