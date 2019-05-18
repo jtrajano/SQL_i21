bdbca6ce162a3994769af53629ba3d9f134f1d6a
COMTECH\BPonce <bonn.ponce@irely.com>
2019-05-18 08:26:09 +0800

    CF-2305

--- i21Database/dbo/Tables/tblCFEncodingPrinterSoftware.sql
+++ i21Database/dbo/Tables/tblCFEncodingPrinterSoftware.sql
@@ -1,12 +1,12 @@
 ﻿CREATE TABLE [dbo].[tblCFEncodingPrinterSoftware] (
     [intKeyId]      INT            IDENTITY (1, 1) NOT NULL,
-    [AcctName]      NVARCHAR (MAX) NOT NULL,
-    [DispCardId]    NVARCHAR (MAX) NOT NULL,
-    [Notation]      NVARCHAR (MAX) NOT NULL,
-    [Exp]           NVARCHAR (MAX) NOT NULL,
+    [AcctName]      NVARCHAR (100) NOT NULL,
+    [DispCardId]    NVARCHAR (100) NOT NULL,
+    [Notation]      NVARCHAR (100) NOT NULL,
+    [Exp]           NVARCHAR (100) NOT NULL,
     --[CardType]      NVARCHAR (MAX) NOT NULL,
-    [EncodeTrack2]  NVARCHAR (MAX) NOT NULL,
-    [EncodeTrack1]  NVARCHAR (MAX) NOT NULL,
+    [EncodeTrack2]  NVARCHAR (100) NOT NULL,
+    [EncodeTrack1]  NVARCHAR (100) NOT NULL,
     --[CardNo]        NVARCHAR (MAX) NOT NULL,
     CONSTRAINT [PK_tblCFEncodingPrinterSoftware] PRIMARY KEY CLUSTERED (intKeyId ASC)
 );
