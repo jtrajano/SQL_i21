7fbf5d97aaf79ea3c972dc91496c4e3c40ebc617
Feb Montefrio <feb.montefrio@irely.com>
2019-05-23 10:09:26 +0800

    [B] IC-7330

--- i21Database/dbo/Stored Procedures/uspICPostInventoryAdjustmentItemChange.sql
+++ i21Database/dbo/Stored Procedures/uspICPostInventoryAdjustmentItemChange.sql
@@ -519,7 +519,7 @@ BEGIN
 									--)
 									CASE 
 										WHEN Detail.dblNewCost IS NULL THEN 
-											Detail.dblCost
+											SourceTransaction.dblCost
 										ELSE
 											dbo.fnCalculateCostBetweenUOM ( 
 												dbo.fnGetItemStockUOM(Detail.intNewItemId)
