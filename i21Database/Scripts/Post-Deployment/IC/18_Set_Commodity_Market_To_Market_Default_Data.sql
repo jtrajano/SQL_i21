b96b6313abda6a251ea311f7b9a59aa2698c3b76
Mon Gonzales <mon.gonzales@irely.com>
2019-06-04 15:29:39 +0800

    IC-7031 - [Commodity Maintenance] - Add Mark to Market Checkbox

---
+++ i21Database/Scripts/Post-Deployment/IC/18_Set_Commodity_Market_To_Market_Default_Data.sql
@@ -0,0 +1,15 @@
+﻿
+
+PRINT ('*****BEGIN CHECKING For IC - Set Commodity Market to market default data*****')
+if not exists (select top 1 1 from tblEMEntityPreferences where strPreference = 'For IC - Set Commodity Market to market default data')
+begin
+	PRINT ('*****RUNNING For IC - Set Commodity Market to market default data*****')
+	
+	UPDATE tblICCommodity set ysnMarketToMarket = 1 where ysnMarketToMarket is null
+
+	INSERT INTO tblEMEntityPreferences(strPreference,strValue)
+	select 'For IC - Set Commodity Market to market default data', '1'
+	
+
+end
+PRINT ('*****END CHECKING For IC - Set Commodity Market to market default data*****')
