702ea11649f3775a03dd5a58f7714e649c276905
Smith Wilson G. de Jesus <smith.dejesus@irely.com>
2019-06-07 09:01:12 +0800

    RM-3020

--- i21Database/dbo/Stored Procedures/uspRKDPRPreCrushPositionDetail.sql
+++ i21Database/dbo/Stored Procedures/uspRKDPRPreCrushPositionDetail.sql
@@ -998,7 +998,7 @@ BEGIN
 				JOIN tblICCommodityUnitMeasure ium ON ium.intCommodityId = ch.intCommodityId AND cd.intUnitMeasureId = ium.intUnitMeasureId
 				LEFT JOIN tblICUnitMeasure um ON um.intUnitMeasureId = ium.intUnitMeasureId
 				INNER JOIN tblSMCompanyLocation cl ON cl.intCompanyLocationId = cd.intCompanyLocationId
-				LEFT JOIN tblAPBillDetail bd on ch.intContractHeaderId = bd.intContractHeaderId and bd.intInventoryReceiptItemId = ri.intInventoryReceiptItemId
+				LEFT JOIN tblAPBillDetail bd on ch.intContractHeaderId = bd.intContractHeaderId and bd.intInventoryReceiptItemId = ri.intInventoryReceiptItemId and bd.intInventoryReceiptChargeId IS NULL
 				WHERE v.strTransactionType = 'Inventory Receipt' AND ch.intCommodityId = @intCommodityId
 					AND cl.intCompanyLocationId = ISNULL(@intLocationId, cl.intCompanyLocationId)
 					AND CONVERT(DATETIME, CONVERT(VARCHAR(10), v.dtmDate, 110), 110) <= CONVERT(DATETIME, @dtmToDate) 
