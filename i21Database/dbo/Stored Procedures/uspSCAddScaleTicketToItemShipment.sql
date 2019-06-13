4a3bb72c5864f9ebd45800b5baa120b530c7c92f
Robin Panganiban <robin.panganiban@irely.com>
2019-06-14 07:53:13 +0800

    SC-2819

--- i21Database/dbo/Stored Procedures/uspSCAddScaleTicketToItemShipment.sql
+++ i21Database/dbo/Stored Procedures/uspSCAddScaleTicketToItemShipment.sql
@@ -174,14 +174,10 @@ BEGIN
 												LEFT JOIN tblICItemUOM futureUOM ON futureUOM.intUnitMeasureId = intSettlementUOMId 
 												WHERE futureUOM.intItemId = LI.intItemId)
 											ELSE 
-												ISNULL((SELECT ISNULL(dbo.fnCalculateCostBetweenUOM(SC.intItemUOMIdTo,futureUOM.intItemUOMId,dblSettlementPrice),0)  --settlement price
+												(SELECT ISNULL(dbo.fnCTConvertQtyToTargetItemUOM(SC.intItemUOMIdTo,futureUOM.intItemUOMId,dblSettlementPrice + ISNULL(dbo.fnCTConvertQtyToTargetItemUOM(futureUOM.intItemUOMId,CNT.intBasisUOMId,LI.dblCost),0)),0) 
 												FROM dbo.fnRKGetFutureAndBasisPrice (2,SC.intCommodityId,right(convert(varchar, CNT.dtmEndDate, 106),8),2,CNT.intFutureMarketId,CNT.intFutureMonthId,NULL,NULL,0 ,SC.intItemId,ISNULL(CNT.intInvoiceCurrencyId,CNT.intCurrencyId))
 												LEFT JOIN tblICItemUOM futureUOM ON futureUOM.intUnitMeasureId = intSettlementUOMId 
-												WHERE futureUOM.intItemId = LI.intItemId),0)
-												+ ISNULL((SELECT ISNULL(dbo.fnCalculateCostBetweenUOM(SC.intItemUOMIdTo,futureUOM.intItemUOMId,dblBasis),0) --basis entry price
-												FROM dbo.fnRKGetFutureAndBasisPrice (2,SC.intCommodityId,right(convert(varchar, CNT.dtmEndDate, 106),8),1,CNT.intFutureMarketId,CNT.intFutureMonthId,SC.intProcessingLocationId,NULL,0 ,SC.intItemId,ISNULL(CNT.intInvoiceCurrencyId,CNT.intCurrencyId))
-												LEFT JOIN tblICItemUOM futureUOM ON futureUOM.intUnitMeasureId = intBasisUOMId 
-												WHERE futureUOM.intItemId = LI.intItemId),0)
+												WHERE futureUOM.intItemId = LI.intItemId)
 										END 
 										ELSE
 											CASE WHEN ISNULL(SC.intLotId,0) > 0 THEN  
