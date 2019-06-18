4ffb70f6464d243da3d1c4e54efabfff3c161770
COMTECH\JMendones <jesmar.mendones@irely.com>
2019-06-18 08:07:53 +0800

    RPT-1309

---
+++ i21Database/dbo/tblSRReportLog.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[tblSRReportLog]
+(
+	[intId] [int] IDENTITY(1,1) NOT NULL,
+	[strReportLogId] [nvarchar](50) NOT NULL,
+	[dtmDate] [datetime] NOT NULL,
+	CONSTRAINT [PK_tblSRReportLog_intId] PRIMARY KEY CLUSTERED ([intId] ASC)
+)
+
+GO
+
+CREATE NONCLUSTERED INDEX [IX_tblSRReportLog_strReportLogId] ON [dbo].[tblSRReportLog] ([strReportLogId] ASC)
+
+GO
