5a2490937b08a2c0bf1a7d6132e5107f925574c0
Kelvin Quinto <kelvin.quinto@irely.com>
2019-06-18 11:25:25 +0800

    AR-10507

---
+++ i21Database/dbo/Views/vyuARServiceChargeInvoice.sql
@@ -0,0 +1,15 @@
+﻿CREATE VIEW [dbo].[vyuARServiceChargeInvoice]
+AS
+SELECT intInvoiceId			= INV.intInvoiceId
+	 , intEntityCustomerId	= INV.intEntityCustomerId
+	 , strInvoiceNumber		= INV.strInvoiceNumber
+	 , strCustomerName		= EM.strName
+	 , dblInvoiceTotal		= INV.dblInvoiceTotal
+	 , ysnPosted			= INV.ysnPosted
+	 , ysnForgiven			= INV.ysnForgiven
+	 , ysnPaid				= INV.ysnPaid
+	 , dtmDate				= INV.dtmDate
+	 , dtmForgiveDate		= INV.dtmForgiveDate
+FROM tblARInvoice INV 
+INNER JOIN tblEMEntity EM ON INV.intEntityCustomerId = EM.intEntityId
+WHERE strType = 'Service Charge'
