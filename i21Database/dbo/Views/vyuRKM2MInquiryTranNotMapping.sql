2097118242326b337dcc67e579b270db985afcc2
Lawrence Zabala <lawrence.zabala@irely.com>
2019-06-19 09:24:23 +0800

    RM-2348

--- i21Database/dbo/Views/vyuRKM2MInquiryTranNotMapping.sql
+++ i21Database/dbo/Views/vyuRKM2MInquiryTranNotMapping.sql
@@ -1,23 +1,25 @@
-﻿CREATE VIEW  vyuRKM2MInquiryTranNotMapping
+﻿CREATE VIEW vyuRKM2MInquiryTranNotMapping
 
-AS 
+AS
 
-SELECT DISTINCT 
-bd.intM2MInquiryId,
-		intM2MInquiryTransactionId
-		,strCommodityCode
-		,strFutMarketName
-		,strFutureMonth
-		,strName strEntityName
-		,strItemNo		
-		,mz.strMarketZoneCode strMarketZoneCode
-		,strLocationName strLocationName
+SELECT DISTINCT bd.intM2MInquiryId
+	, intM2MInquiryTransactionId
+	, strCommodityCode
+	, strFutMarketName
+	, strFutureMonth
+	, strName strEntityName
+	, strItemNo
+	, mz.strMarketZoneCode strMarketZoneCode
+	, strLocationName strLocationName
+	, cd.dtmStartDate
+	, cd.dtmEndDate
 FROM tblRKM2MInquiryTransaction bd
-join tblRKM2MInquiry mb on mb.intM2MInquiryId=bd.intM2MInquiryId
-join tblICCommodity c on c.intCommodityId=bd.intCommodityId
-LEFT join tblEMEntity e on e.intEntityId=bd.intEntityId
-LEFT JOIN tblICItem i on i.intItemId=bd.intItemId
-LEFT JOIN tblRKFutureMarket m on m.intFutureMarketId=bd.intFutureMarketId
-LEFT JOIN tblRKFuturesMonth mo on mo.intFutureMonthId=bd.intFutureMonthId
-LEFT JOIN tblARMarketZone mz on mz.intMarketZoneId=bd.intMarketZoneId
-LEFT JOIN tblSMCompanyLocation co on co.intCompanyLocationId=bd.intCompanyLocationId
+JOIN tblRKM2MInquiry mb ON mb.intM2MInquiryId = bd.intM2MInquiryId
+JOIN tblICCommodity c ON c.intCommodityId = bd.intCommodityId
+LEFT JOIN tblEMEntity e ON e.intEntityId = bd.intEntityId
+LEFT JOIN tblICItem i ON i.intItemId = bd.intItemId
+LEFT JOIN tblRKFutureMarket m ON m.intFutureMarketId = bd.intFutureMarketId
+LEFT JOIN tblRKFuturesMonth mo ON mo.intFutureMonthId = bd.intFutureMonthId
+LEFT JOIN tblARMarketZone mz ON mz.intMarketZoneId = bd.intMarketZoneId
+LEFT JOIN tblSMCompanyLocation co ON co.intCompanyLocationId = bd.intCompanyLocationId
+LEFT JOIN tblCTContractDetail cd ON cd.intContractDetailId = bd.intContractDetailId
