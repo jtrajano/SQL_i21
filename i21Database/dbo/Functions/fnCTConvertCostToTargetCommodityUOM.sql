3522532b5abc7abbc0f90da463ff135f7ec12be2
jayson_ramos <jayson.ramos@irely.com>
2019-06-20 14:23:11 +0800

    CT-3296

--- i21Database/dbo/Functions/fnCTConvertCostToTargetCommodityUOM.sql
+++ i21Database/dbo/Functions/fnCTConvertCostToTargetCommodityUOM.sql
@@ -1,4 +1,24 @@
-﻿CREATE FUNCTION [dbo].[fnCTConvertCostToTargetCommodityUOM]
+﻿CREATE FUNCTION [dbo].[fnCTConvertCostToTargetCommodityUOM]  
+(  
+ @intCommodityId INT,  
+ @intFromCommodityUOMId INT,  
+ @intToCommodityUOMId INT,  
+ @dblCost  NUMERIC(38,20)  
+)  
+RETURNS NUMERIC(38,20)  
+AS  
+BEGIN  
+ Declare @dblUnitQtyFrom NUMERIC(38,20)=1  
+ Declare @dblUnitQtyTo NUMERIC(38,20)=1  
+  
+ Select @dblUnitQtyFrom=ISNULL(dblUnitQty,1) From tblICCommodityUnitMeasure Where intCommodityId = @intCommodityId and intUnitMeasureId =@intFromCommodityUOMId
+ Select @dblUnitQtyTo=ISNULL(dblUnitQty,1) From tblICCommodityUnitMeasure Where intCommodityUnitMeasureId=@intToCommodityUOMId  
+  
+ return @dblCost / (NULLIF(@dblUnitQtyFrom,0)/NULLIF(@dblUnitQtyTo,0))  
+END
+
+/*
+CREATE FUNCTION [dbo].[fnCTConvertCostToTargetCommodityUOM]
 (
 	@intCommodityId	INT,
 	@intFromCommodityUOMId	INT,
@@ -16,3 +36,4 @@ BEGIN
 
 	return @dblCost / (NULLIF(@dblUnitQtyFrom,0)/NULLIF(@dblUnitQtyTo,0))
 END
+*/
