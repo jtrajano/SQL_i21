2a78094180721092d78bed029cba8ce2ee2c65b7
henry_zaragoza <henry.zaragoza@irely.com>
2019-06-25 13:48:06 +0800

    ST-959 Update Item Data > How to roll back mass changes?

---
+++ i21Database/dbo/Views/vyuSTSearchRevertHolderDetail.sql
@@ -0,0 +1,28 @@
+﻿CREATE VIEW [dbo].[vyuSTSearchRevertHolderDetail]
+AS
+SELECT 
+	RHD.intRevertHolderDetailId
+	, RHD.intRevertHolderId
+	, RHD.strTableName
+	, RHD.strTableColumnName
+	, RHD.strTableColumnDataType
+	, RHD.intPrimaryKeyId
+	, RHD.intItemId
+	, RHD.intItemUOMId
+	, RHD.intItemLocationId
+	, RHD.intCompanyLocationId
+	, RHD.dtmDateModified
+	, RHD.strChangeDescription
+	, RHD.strOldData
+	, RHD.strNewData
+	, Item.strItemNo
+	, Item.strDescription AS strItemDescription
+	, Uom.strLongUPCCode
+	, CompanyLoc.strLocationName
+FROM tblSTRevertHolderDetail RHD
+INNER JOIN tblICItem Item
+	ON RHD.intItemId = Item.intItemId
+INNER JOIN tblICItemUOM Uom
+	ON RHD.intItemUOMId = Uom.intItemUOMId
+INNER JOIN tblSMCompanyLocation CompanyLoc
+	ON RHD.intCompanyLocationId = CompanyLoc.intCompanyLocationId
