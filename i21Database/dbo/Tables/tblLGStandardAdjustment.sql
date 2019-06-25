a64711cf379cd7f9b985cf12ee3d25ef54b7e1cf
Arman Arenas <arman.arenas@irely.com>
2019-06-25 13:14:33 +0800

    LG-1815 Added new table tblLGStandardAdjustment

---
+++ i21Database/dbo/Tables/tblLGStandardAdjustment.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[tblLGStandardAdjustment]
+(
+	[intStandardAdjustmentId] INT NOT NULL PRIMARY KEY,
+	[intAdjustmentType] INT NULL,
+	[intRecordId] INT NULL,
+	[dblRate] NUMERIC(18, 6),
+	[intCurrencyId] INT NULL,
+	[intItemUOMId] INT NULL,
+	[dtmValidFrom] DATETIME,
+	[dtmValidTo] DATETIME,
+	[intConcurrencyId] INT NOT NULL DEFAULT 1
+)
