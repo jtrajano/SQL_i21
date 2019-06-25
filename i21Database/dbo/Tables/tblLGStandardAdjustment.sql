dad45551fec2b66afeed73e763820d82e30379f5
Arman Arenas <arman.arenas@irely.com>
2019-06-25 13:58:22 +0800

    LG-1815 adjustment on some fields

--- i21Database/dbo/Tables/tblLGStandardAdjustment.sql
+++ i21Database/dbo/Tables/tblLGStandardAdjustment.sql
@@ -2,10 +2,10 @@
 (
 	[intStandardAdjustmentId] INT NOT NULL PRIMARY KEY,
 	[intAdjustmentType] INT NULL,
-	[intRecordId] INT NULL,
+	[strMasterRecord] NVARCHAR(500) COLLATE Latin1_General_CI_AS NULL,
 	[dblRate] NUMERIC(18, 6),
 	[intCurrencyId] INT NULL,
-	[intItemUOMId] INT NULL,
+	[intUnitMeasureId] INT NULL,
 	[dtmValidFrom] DATETIME,
 	[dtmValidTo] DATETIME,
 	[intConcurrencyId] INT NOT NULL DEFAULT 1
