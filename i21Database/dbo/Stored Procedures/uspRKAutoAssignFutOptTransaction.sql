abcb4e3c87875e1cf5a0d69cad61794ace77c75d
Ramachandran <Ramachandran.Shanmuganathan@irely.com>
2019-06-25 20:15:10 +0530

    RM-3075

--- i21Database/dbo/Stored Procedures/uspRKAutoAssignFutOptTransaction.sql
+++ i21Database/dbo/Stored Procedures/uspRKAutoAssignFutOptTransaction.sql
@@ -72,17 +72,18 @@ BEGIN
                         --           NOT IN(SELECT   intContractDetailId   
                         --                  FROM tblRKAssignFuturesToContractSummary WHERE intFutOptTransactionId=@intLFutOptTransactionId)  
                         --                  AND ISNULL(cd.intContractDetailId,0)  <> 0  
-                                    INSERT INTO tblRKAssignFuturesToContractSummary   
-                                    SELECT TOP 1 @intAssignFuturesToContractHeaderId,  
-                                                1,  
-                                                s.intContractHeaderId,
-                                                NULL,   
-                                                GETDATE(),@intLFutOptTransactionId,dblMatchQty,0,0,null FROM tblRKMatchFuturesPSDetail ps  
-                                    JOIN tblRKAssignFuturesToContractSummary s on ps.intSFutOptTransactionId=intFutOptTransactionId   
-                                    WHERE intMatchFuturesPSHeaderId= @intMatchFuturesPSHeaderId  and intLFutOptTransactionId=@intLFutOptTransactionId  
-                                    AND s.intContractHeaderId    
-                                    IN(SELECT   intContractHeaderId   
-                                                FROM tblRKAssignFuturesToContractSummary WHERE intFutOptTransactionId=@intSFutOptTransactionId)  
+							INSERT INTO tblRKAssignFuturesToContractSummary   
+							SELECT TOP 1 @intAssignFuturesToContractHeaderId,  
+								1,  
+								s.intContractHeaderId,
+								NULL,   
+								GETDATE(),@intLFutOptTransactionId,dblMatchQty,0,0,null FROM tblRKMatchFuturesPSDetail ps  
+							JOIN tblRKAssignFuturesToContractSummary s on ps.intSFutOptTransactionId=intFutOptTransactionId   
+							WHERE intMatchFuturesPSHeaderId= @intMatchFuturesPSHeaderId  and intLFutOptTransactionId=@intLFutOptTransactionId  
+							AND s.intFutOptTransactionId  = @intSFutOptTransactionId  
+                                  --  AND s.intContractHeaderId    
+                                  --not  IN(SELECT   intContractHeaderId   
+                                  --              FROM tblRKAssignFuturesToContractSummary WHERE intFutOptTransactionId=@intSFutOptTransactionId)  
                                                 AND ISNULL(s.intContractHeaderId,0)  <> 0
                              
 							END
