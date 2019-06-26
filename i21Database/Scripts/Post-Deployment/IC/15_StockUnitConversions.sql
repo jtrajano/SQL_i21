a160de008ce475c5f24dd2590202c74f905ae791
Wendell Wayne Estrada <wendell.estrada@irely.com>
2019-06-26 17:45:57 +0800

    [B] IC-7436

---
+++ i21Database/Scripts/Post-Deployment/IC/15_StockUnitConversions.sql
@@ -0,0 +1,7 @@
+IF EXISTS(SELECT TOP 1 1 FROM sys.objects WHERE name = 'CK_ItemUOMId_IS_NOT_USED' AND type = 'C' AND parent_object_id = OBJECT_ID('tblICItemUOM', 'U'))
+BEGIN 
+	EXEC ('
+		ALTER TABLE tblICItemUOM
+		DROP CONSTRAINT CK_ItemUOMId_IS_NOT_USED		
+	')
+END
