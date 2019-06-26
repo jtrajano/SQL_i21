a225d6ca211692b6106a2f18e4c7c0263662e0ee
Wendell Wayne Estrada <wendell.estrada@irely.com>
2019-06-26 17:45:57 +0800

    [B] IC-7436

--- i21Database/Scripts/Post-Deployment/PostDeployment.sql
+++ i21Database/Scripts/Post-Deployment/PostDeployment.sql
@@ -335,6 +335,7 @@ print 'BEGIN POST DEPLOYMENT'
 :r .\IC\Data_Fix_For_18.3\12_PopulateGLEntityForICTransactions.sql
 :r .\IC\Data_Fix_For_18.3\13_PopulateLotInTransitQtyAndWgt.sql
 :r .\IC\Data_Fix_For_18.3\14_PopulateShipFromEntity.sql
+:r .\IC\15_StockUnitConversions.sql 
 
 -- Patronage
 :r .\PAT\DefaultData\1_AddDefaultLetters.sql 
