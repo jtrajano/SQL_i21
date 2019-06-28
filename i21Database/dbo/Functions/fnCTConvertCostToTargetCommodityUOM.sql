c87b192e9bf3893ad5ed108e1321c461fce7de65
Dan Jalimeo <dan.jalimao@irely.com>
2019-06-28 11:27:49 +0800

    CT-3296

--- i21Database/dbo/Functions/fnCTConvertCostToTargetCommodityUOM.sql
+++ i21Database/dbo/Functions/fnCTConvertCostToTargetCommodityUOM.sql
@@ -3,18 +3,18 @@
  @intCommodityId INT,  
  @intFromCommodityUOMId INT,  
  @intToCommodityUOMId INT,  
- @dblCost  NUMERIC(38,20)  
+ @dblCost  NUMERIC(26,12)  
 )  
-RETURNS NUMERIC(38,20)  
+RETURNS NUMERIC(26,12)  
 AS  
 BEGIN  
- Declare @dblUnitQtyFrom NUMERIC(38,20)=1  
- Declare @dblUnitQtyTo NUMERIC(38,20)=1  
+ Declare @dblUnitQtyFrom NUMERIC(26,12)=1  
+ Declare @dblUnitQtyTo NUMERIC(26,12)=1  
   
  Select @dblUnitQtyFrom=ISNULL(dblUnitQty,1) From tblICCommodityUnitMeasure Where intCommodityId = @intCommodityId and intUnitMeasureId =@intFromCommodityUOMId
- Select @dblUnitQtyTo=ISNULL(dblUnitQty,1) From tblICCommodityUnitMeasure Where intCommodityUnitMeasureId=@intToCommodityUOMId  
+ --Select @dblUnitQtyTo=ISNULL(dblUnitQty,1) From tblICCommodityUnitMeasure Where intCommodityUnitMeasureId=@intToCommodityUOMId  
   
- return @dblCost / (NULLIF(@dblUnitQtyFrom,0)/NULLIF(@dblUnitQtyTo,0))  
+ return @dblCost / (NULLIF(@dblUnitQtyFrom,0)/1)--NULLIF(@dblUnitQtyTo,0))  
 END
 
 /*
