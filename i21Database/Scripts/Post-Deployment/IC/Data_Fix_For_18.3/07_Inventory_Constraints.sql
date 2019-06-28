41d3a7983cf419653e8b51dc6f9229e7c45bbee9
Wendell Wayne Estrada <wendell.estrada@irely.com>
2019-06-28 11:57:14 +0800

    [B] IC-7436

--- i21Database/Scripts/Post-Deployment/IC/Data_Fix_For_18.3/07_Inventory_Constraints.sql
+++ i21Database/Scripts/Post-Deployment/IC/Data_Fix_For_18.3/07_Inventory_Constraints.sql
@@ -30,6 +30,13 @@ BEGIN
 	END
 END
 
+IF EXISTS(SELECT TOP 1 1 FROM sys.objects WHERE name = 'CK_ItemUOMId_IS_NOT_USED' AND type = 'C' AND parent_object_id = OBJECT_ID('tblICItemUOM', 'U'))
+BEGIN 
+	EXEC ('
+		ALTER TABLE tblICItemUOM
+		DROP CONSTRAINT CK_ItemUOMId_IS_NOT_USED		
+	')
+END
 
 PRINT N'END - IC Data Fix for 18.3. #7'
 GO
