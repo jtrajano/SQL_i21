868d9cb52c1b7911c04c40360c090387cb133305
Jeff Trajano <jeffrey.trajano@irely.com>
2019-06-28 10:34:12 +0800

    GL-7183 TB and GL details do not match

--- i21Database/dbo/Tables/tblGLTrialBalance.sql
+++ i21Database/dbo/Tables/tblGLTrialBalance.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[tblGLTrialBalance](
 	[intTrialBalanceId] [int] IDENTITY(1,1) NOT NULL,
 	[intAccountId] [int] NOT NULL,
+    [strTransactionId][nvarchar(40)] COLLATE Latin1_General_CI_AS NULL,
 	[MTDBalance] [numeric](38, 6) NULL,
 	[YTDBalance] [numeric](38, 6) NULL,
 	[intGLFiscalYearPeriodId] [int] NULL,
