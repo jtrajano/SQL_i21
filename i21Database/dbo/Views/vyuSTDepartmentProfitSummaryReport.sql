1966d91a7d6a1f012bbf43a4479441c636359465
COMTECH\BPonce <bonn.ponce@irely.com>
2019-07-02 18:54:17 +0800

    ST-1377

--- i21Database/dbo/Views/vyuSTDepartmentProfitSummaryReport.sql
+++ i21Database/dbo/Views/vyuSTDepartmentProfitSummaryReport.sql
@@ -17,27 +17,34 @@ SELECT ST.intStoreId
 	, SUM(CDT.intTotalSalesCount) intTotalSalesCount
 	, SUM(CDT.dblRegisterSalesAmountComputed) dblTotalRegisterAmount
 	, SUM(CDT.dblTotalSalesAmountComputed) dblTotalSalesAmount
-	, SUM(CDT.dblRegisterSalesAmountComputed) - SUM(CDT.dblTotalSalesAmountComputed) dblDiffRegisterAndSales
-	, SUM(InvTran.dblTotalCost) dblCost
+	, SUM(CDT.dblRegisterSalesAmountComputed) - SUM(CDT.dblTotalSalesAmountComputed) as  dblDiffRegisterAndSales
+	, SUM(tblARSalesAnalysisReport.dblTotalCost) dblCost
 FROM tblSTCheckoutHeader CH 
 INNER JOIN tblSTStore ST 
 	ON ST.intStoreId = CH.intStoreId 
+INNER JOIN tblSTCheckoutDepartmetTotals CDT 
+	ON CDT.intCheckoutId = CH.intCheckoutId 
 INNER JOIN tblSMCompanyLocation CL
 	ON ST.intCompanyLocationId = CL.intCompanyLocationId
 LEFT JOIN tblARInvoice Inv 
 	ON Inv.intInvoiceId = CH.intInvoiceId
-LEFT JOIN vyuARSalesAnalysisReport InvTran
-	ON Inv.intInvoiceId = InvTran.intTransactionId
-INNER JOIN tblSTCheckoutDepartmetTotals CDT 
-	ON CDT.intCheckoutId = CH.intCheckoutId 
+LEFT JOIN (
+	SELECT 
+		 SUM(dblTotalCost) as dblTotalCost , intCategoryId , intTransactionId
+	FROM vyuARSalesAnalysisReport 
+	GROUP BY   intCategoryId , intTransactionId
+) as tblARSalesAnalysisReport
+ON tblARSalesAnalysisReport.intTransactionId = CH.intInvoiceId
+AND tblARSalesAnalysisReport.intCategoryId = CDT.intCategoryId
 INNER JOIN tblICItem IT 
 	ON IT.intItemId = CDT.intItemId 
 INNER JOIN tblICCategory CAT 
 	ON CAT.intCategoryId = IT.intCategoryId
 WHERE (CDT.dblTotalSalesAmountComputed <> 0	
 	OR CDT.intTotalSalesCount <> 0)
-GROUP BY ST.intStoreId, ST.intStoreNo, ST.strRegion, ST.strDistrict, CL.strCity, CL.strStateProvince, CL.strZipPostalCode, ST.strDescription, CAT.intCategoryId, CAT.strCategoryCode, CAT.strDescription, CH.dtmCheckoutDate, ISNULL(Inv.ysnPosted, 0)
+GROUP BY 
+ST.intStoreId, ST.intStoreNo, ST.strRegion, ST.strDistrict, CL.strCity, CL.strStateProvince, CL.strZipPostalCode, ST.strDescription, CAT.intCategoryId, CAT.strCategoryCode, CAT.strDescription, CH.dtmCheckoutDate, ISNULL(Inv.ysnPosted, 0)
 
-
+GO
 
 
