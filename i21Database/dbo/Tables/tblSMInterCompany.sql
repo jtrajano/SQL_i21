9a4fb53560023303058e0b02e8df504855ac8403
Kevin Estrada <kevin.estrada@irely.com>
2019-07-02 18:29:33 +0800

    FRM-7953: mapping & intercompany table

---
+++ i21Database/dbo/Tables/tblSMInterCompany.sql
@@ -0,0 +1,11 @@
+﻿  CREATE TABLE  tblSMInterCompany
+  (
+	[intInterCompanyId] INT IDENTITY(1,1),
+	[strCompanyName] nvarchar(max),
+	[strServerName] nvarchar(max),
+	[strDatabaseName] nvarchar(max),
+	[strUserName] nvarchar(max),
+	[strPassword] nvarchar(max),
+	[intConcurrencyId] int default(0),
+	constraint [PK_dbo.tblSMInterCompany] PRIMARY KEY CLUSTERED ([intInterCompanyId] ASC)
+  )
---
+++ i21Database/dbo/Tables/tblSMInterCompanyMapping.sql
@@ -0,0 +1,9 @@
+﻿  CREATE TABLE  tblSMInterCompanyMapping
+  (
+	[intInterCompanyMappingId] INT IDENTITY(1,1),
+	[intSourceTransactionId] INT NULL,
+	[intDestinationCompanyId] INT NULL,
+	[intDestinationTransactionId] INT NULL,
+	[intConcurrencyId] int default(0),
+	constraint [PK_dbo.tblSMInterCompanyMapping] PRIMARY KEY CLUSTERED ([intInterCompanyMappingId] ASC)
+  )
