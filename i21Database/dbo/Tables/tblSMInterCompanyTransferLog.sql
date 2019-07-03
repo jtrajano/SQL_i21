1279838f5513784db542bea5b027e22efed79da4
Kevin Estrada <kevin.estrada@irely.com>
2019-07-03 21:58:12 +0800

    FRM-7953: transferlog table

---
+++ i21Database/dbo/Tables/tblSMInterCompanyTransferLog.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE tblSMInterCompanyTransferLog
+(
+ [intInterCompanyTransferLogId] int identity(1,1),
+ [strType] nvarchar(100) collate Latin1_General_CI_AS  null,
+ [intSourceRecordId] int null,
+ [intDestinationRecordId] int null,
+ CONSTRAINT [PK_tblSMInterCompanyTransferLog] PRIMARY KEY CLUSTERED ([intInterCompanyTransferLogId] ASC) 
+)
