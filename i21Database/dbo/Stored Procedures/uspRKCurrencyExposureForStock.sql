ad60828bdaef95a625efa6201e899533fac3a53d
Lawrence Zabala <lawrence.zabala@irely.com>
2019-07-04 15:39:54 +0800

    RM-3018

--- i21Database/dbo/Stored Procedures/uspRKCurrencyExposureForStock.sql
+++ i21Database/dbo/Stored Procedures/uspRKCurrencyExposureForStock.sql
@@ -1,55 +1,60 @@
-﻿CREATE PROC [dbo].[uspRKCurrencyExposureForStock]
-	  @intCommodityId int
-	, @dtmClosingPrice datetime = null
-	, @intCurrencyId int
+﻿CREATE PROCEDURE [dbo].[uspRKCurrencyExposureForStock]
+	@intCommodityId INT
+	, @dtmClosingPrice DATETIME = NULL
+	, @intCurrencyId INT
 
 AS
-SET @dtmClosingPrice = CONVERT(DATETIME, CONVERT(VARCHAR(10), @dtmClosingPrice, 110), 110)
 
 BEGIN
-SELECT CONVERT(INT, ROW_NUMBER() OVER(ORDER BY strContractNumber)) as intRowNum
-	, (isnull(dblSettlementPrice,0)+isnull(dblMarketPremium,0)) dblMarketPrice
-	, dblQty * (isnull(dblSettlementPrice,0)+isnull(dblMarketPremium,0)) dblValue
-	, *
-FROM (
-	SELECT (ch.strContractNumber + '-' + convert(nvarchar, cd.intContractSeq)) COLLATE Latin1_General_CI_AS strContractNumber
-		, l.strLotNumber
-		, sl.strName
-		, item.strItemNo
-		, fm.strFutMarketName
-		, (SELECT TOP 1 replace(strFutureMonth,' ','('+strSymbol+') ' ) COLLATE Latin1_General_CI_AS strFutureMonth
-			FROM tblRKFuturesMonth mon
-			WHERE ysnExpired = 0 AND dtmSpotDate <= GETDATE() AND mon.intFutureMarketId = fm.intFutureMarketId ORDER BY 1 DESC) strSpotMonth
-		, 
-		[dbo].[fnRKGetCurrencyConvertion](fm.intCurrencyId,@intCurrencyId)*
-		dbo.fnRKGetLatestClosingPrice(fm.intFutureMarketId, (SELECT TOP 1 intFutureMonthId FROM tblRKFuturesMonth mon
-																WHERE ysnExpired = 0 AND  dtmSpotDate <= GETDATE() AND mon.intFutureMarketId = fm.intFutureMarketId 
-																ORDER BY 1 DESC), @dtmClosingPrice) dblSettlementPrice
-		,[dbo].[fnRKGetCurrencyConvertion](fm.intCurrencyId,@intCurrencyId)
-			*dbo.[fnCTConvertQuantityToTargetItemUOM](cd.intItemId,um.intUnitMeasureId,fm.intUnitMeasureId,dblBasis) dblMarketPremium
-		, (c.strCurrency + '/' + um.strUnitMeasure) COLLATE Latin1_General_CI_AS strMarketPremiumUOM
-		, cur.strCurrency strMarketPriceUOM
-		, l.dblQty
-		, strCompanyName
-		, 1 as intConcurrencyId,
-		cd.intContractDetailId,
-		sl.intStorageLocationId,
-		um.intUnitMeasureId intMarketPremiumUOMId,
-		cur.intCurrencyID intMarketPriceCurrencyId,l.intItemId,cd.intFutureMarketId,ch.intCompanyId
-	FROM tblCTContractHeader ch
-	JOIN tblCTContractDetail cd on ch.intContractHeaderId=cd.intContractHeaderId
-	join tblRKFutureMarket fm on cd.intFutureMarketId=fm.intFutureMarketId
-	JOIN tblICInventoryReceiptItem ri on cd.intContractDetailId=ri.intLineNo
-	JOIN tblICInventoryReceiptItemLot rl on ri.intInventoryReceiptItemId=rl.intInventoryReceiptItemId
-	join tblICStorageLocation sl on sl.intStorageLocationId=rl.intStorageLocationId
-	JOIN tblICLot l on rl.intLotId=l.intLotId
-	JOIN tblICItem item on item.intItemId=l.intItemId
-	join tblSMCurrency c on c.intCurrencyID=cd.intBasisCurrencyId
-	join tblICItemUOM iu on iu.intItemUOMId=cd.intBasisUOMId
-	join tblICUnitMeasure um on um.intUnitMeasureId=iu.intUnitMeasureId
-	join tblSMCurrency cur on cur.intCurrencyID=fm.intCurrencyId
-	LEFT join tblSMMultiCompany mc on mc.intMultiCompanyId=ch.intCompanyId
-	WHERE ch.intCommodityId=@intCommodityId  and dblQty<>0 
+	SET @dtmClosingPrice = CONVERT(DATETIME, CONVERT(VARCHAR(10), @dtmClosingPrice, 110), 110)
 
-) t
+	SELECT intRowNum = CONVERT(INT, ROW_NUMBER() OVER(ORDER BY strContractNumber))
+		, dblMarketPrice = (ISNULL(dblSettlementPrice, 0) + ISNULL(dblMarketPremium, 0))
+		, dblValue = dblQty * (ISNULL(dblSettlementPrice, 0) + ISNULL(dblMarketPremium, 0))
+		, *
+	FROM (
+		SELECT strContractNumber = (ch.strContractNumber + '-' + CONVERT(NVARCHAR, cd.intContractSeq)) COLLATE Latin1_General_CI_AS
+			, l.strLotNumber
+			, sl.strName
+			, item.strItemNo
+			, fm.strFutMarketName
+			, strSpotMonth = (SELECT TOP 1 REPLACE(strFutureMonth, ' ', '(' + strSymbol + ') ') COLLATE Latin1_General_CI_AS strFutureMonth
+							FROM tblRKFuturesMonth mon
+							WHERE ysnExpired = 0
+								AND dtmSpotDate <= GETDATE()
+								AND mon.intFutureMarketId = fm.intFutureMarketId
+							ORDER BY 1 DESC)
+			, dblSettlementPrice = [dbo].[fnRKGetCurrencyConvertion](fm.intCurrencyId, @intCurrencyId)
+									* dbo.fnRKGetLatestClosingPrice(fm.intFutureMarketId, (SELECT TOP 1 intFutureMonthId FROM tblRKFuturesMonth mon
+																							WHERE ysnExpired = 0 AND  dtmSpotDate <= GETDATE() AND mon.intFutureMarketId = fm.intFutureMarketId
+																							ORDER BY 1 DESC), @dtmClosingPrice)
+			, dblMarketPremium = [dbo].[fnRKGetCurrencyConvertion](fm.intCurrencyId, @intCurrencyId)
+								* dbo.[fnCTConvertQuantityToTargetItemUOM](cd.intItemId, um.intUnitMeasureId, fm.intUnitMeasureId, dblBasis)
+			, strMarketPremiumUOM = (c.strCurrency + '/' + um.strUnitMeasure) COLLATE Latin1_General_CI_AS
+			, strMarketPriceUOM = cur.strCurrency
+			, l.dblQty
+			, strCompanyName
+			, intConcurrencyId = 1
+			, cd.intContractDetailId
+			, sl.intStorageLocationId
+			, intMarketPremiumUOMId = um.intUnitMeasureId
+			, intMarketPriceCurrencyId = cur.intCurrencyID
+			, l.intItemId
+			, cd.intFutureMarketId
+			, ch.intCompanyId
+		FROM tblCTContractHeader ch
+		JOIN tblCTContractDetail cd ON ch.intContractHeaderId = cd.intContractHeaderId
+		JOIN tblRKFutureMarket fm ON cd.intFutureMarketId = fm.intFutureMarketId
+		JOIN tblICInventoryReceiptItem ri ON cd.intContractDetailId = ri.intLineNo
+		JOIN tblICInventoryReceiptItemLot rl ON ri.intInventoryReceiptItemId = rl.intInventoryReceiptItemId
+		JOIN tblICStorageLocation sl ON sl.intStorageLocationId = rl.intStorageLocationId
+		JOIN tblICLot l ON rl.intLotId = l.intLotId
+		JOIN tblICItem item ON item.intItemId = l.intItemId
+		JOIN tblSMCurrency c ON c.intCurrencyID = cd.intBasisCurrencyId
+		JOIN tblICItemUOM iu ON iu.intItemUOMId = cd.intBasisUOMId
+		JOIN tblICUnitMeasure um ON um.intUnitMeasureId = iu.intUnitMeasureId
+		JOIN tblSMCurrency cur ON cur.intCurrencyID = fm.intCurrencyId
+		LEFT JOIN tblSMMultiCompany mc ON mc.intMultiCompanyId = ch.intCompanyId
+		WHERE ch.intCommodityId=@intCommodityId AND dblQty <> 0
+	) t
 END
