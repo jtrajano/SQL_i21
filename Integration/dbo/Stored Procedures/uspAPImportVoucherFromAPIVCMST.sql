b43be6550a74cd0bb7a4b37b95614db7c02d26c0
Erick <erick.delacruz@irely.com>
2019-07-04 18:26:20 +0800

    AP-7379

--- Integration/dbo/Stored Procedures/uspAPImportVoucherFromAPIVCMST.sql
+++ Integration/dbo/Stored Procedures/uspAPImportVoucherFromAPIVCMST.sql
@@ -318,7 +318,7 @@ UPDATE A
 						WHEN 3
 							THEN @debitMemo
 						END) + (CAST(B.intRecordNumber AS NVARCHAR)),
-		A.ysnDiscountOverride = CASE WHEN A.dblDiscount != 0 AND A.ysnPaid = 0 THEN 1 ELSE 0 END
+		A.ysnDiscountOverride = CASE WHEN A.dblDiscount != 0 THEN 1 ELSE 0 END
 FROM tblAPBill A
 INNER JOIN #tmpVouchersWithRecordNumber B ON A.intBillId = B.intBillId
 
