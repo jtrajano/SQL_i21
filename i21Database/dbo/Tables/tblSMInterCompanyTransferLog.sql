facb3b2a8a8d6dcc84bd0861d465dd167e5bac5d
Kevin Estrada <kevin.estrada@irely.com>
2019-07-04 10:22:18 +0800

    FRM-7953

--- i21Database/dbo/Tables/tblSMInterCompanyTransferLog.sql
+++ i21Database/dbo/Tables/tblSMInterCompanyTransferLog.sql
@@ -4,5 +4,7 @@
  [strType] nvarchar(100) collate Latin1_General_CI_AS  null,
  [intSourceRecordId] int null,
  [intDestinationRecordId] int null,
- CONSTRAINT [PK_tblSMInterCompanyTransferLog] PRIMARY KEY CLUSTERED ([intInterCompanyTransferLogId] ASC) 
+ [intDestinationCompanyId] int null,
+ CONSTRAINT [PK_tblSMInterCompanyTransferLog] PRIMARY KEY CLUSTERED ([intInterCompanyTransferLogId] ASC) ,
+ CONSTRAINT [FK_dbo.tblSMInterCompanyTransferLog_tblSMInterCompany] FOREIGN KEY ([intDestinationCompanyId]) REFERENCES [tblSMInterCompany](intInterCompanyId)
 )
