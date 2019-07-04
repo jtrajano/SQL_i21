d2644b17e816f38a08e216161e3bbf16a8051af9
Lawrence Zabala <lawrence.zabala@irely.com>
2019-07-04 15:39:54 +0800

    RM-3018

--- i21Database/dbo/Stored Procedures/uspRKSaveCurrencyExposure.sql
+++ i21Database/dbo/Stored Procedures/uspRKSaveCurrencyExposure.sql
@@ -1,301 +1,285 @@
-﻿CREATE PROC [dbo].[uspRKSaveCurrencyExposure]
-	@intCommodityId int,
-	@dtmFutureClosingDate datetime,
-	@intCurrencyId int,
-	@intCurrencyExposureId int
-	,@dblAP numeric(24,10)
-	,@dblAR numeric(24,10)	
+﻿CREATE PROCEDURE [dbo].[uspRKSaveCurrencyExposure]
+	@intCommodityId INT
+	, @dtmFutureClosingDate DATETIME
+	, @intCurrencyId INT
+	, @intCurrencyExposureId INT
+	, @dblAP NUMERIC(24, 10)
+	, @dblAR NUMERIC(24, 10)	
 
 AS
 
 BEGIN TRY
-DECLARE @ErrMsg nvarchar(Max)  
+	DECLARE @ErrMsg NVARCHAR(MAX)
+	
+	DECLARE @tblRKStock TABLE (intRowNum INT
+		, dblMarketPrice NUMERIC(24, 10)
+		, dblValue NUMERIC(24, 10)
+		, strContractNumber NVARCHAR(100)
+		, strLotNumber NVARCHAR(100)
+		, strName NVARCHAR(100)
+		, strItemNo NVARCHAR(100)
+		, strFutMarketName NVARCHAR(100)
+		, strSpotMonth NVARCHAR(100)
+		, dblSettlementPrice NUMERIC(24, 10)
+		, dblMarketPremium NUMERIC(24, 10)
+		, strMarketPremiumUOM NVARCHAR(100)
+		, strMarketPriceUOM NVARCHAR(100)
+		, dblQty NUMERIC(24, 10)
+		, strCompanyName NVARCHAR(100)
+		, intConcurrencyId INT
+		, intContractDetailId INT
+		, intStorageLocationId INT
+		, intMarketPremiumUOMId INT
+		, intMarketPriceCurrencyId INT
+		, intItemId INT
+		, intFutureMarketId INT
+		, intCompanyId INT)
 
-DECLARE @tblRKStock TABLE (
-						intRowNum   int,
-						dblMarketPrice numeric(24,10) ,                        
-						dblValue   numeric(24,10) ,                             
-						strContractNumber nvarchar(100) ,                                                               
-						strLotNumber  nvarchar(100) ,                                     
-						strName  nvarchar(100),                                            
-						strItemNo  nvarchar(100),                                         
-						strFutMarketName  nvarchar(100),              
-						strSpotMonth  nvarchar(100) ,                                                                                                                                                                                                                                                   
-						dblSettlementPrice numeric(24,10),               
-						dblMarketPremium   numeric(24,10), 
-						strMarketPremiumUOM  nvarchar(100),                                                                        
-						strMarketPriceUOM  nvarchar(100),   
-						dblQty  numeric(24,10) ,                                
-						strCompanyName  nvarchar(100),                                                                                                                                       
-						intConcurrencyId  int,
-						intContractDetailId  int,
-						intStorageLocationId  int,
-						intMarketPremiumUOMId int,
-						intMarketPriceCurrencyId int,
-						intItemId int,
-						intFutureMarketId int,
-						intCompanyId int)
-
-INSERT INTO @tblRKStock (intRowNum,   
-						dblMarketPrice,                         
-						dblValue,                               
-						strContractNumber,          
-						strLotNumber,                                      
-						strName,                                           
-						strItemNo,                                       
-						strFutMarketName,            
-						strSpotMonth,                                                                                                                                                                                                                                                
-						dblSettlementPrice,                
-						dblMarketPremium, 
-						strMarketPremiumUOM ,                                                                
-						strMarketPriceUOM ,                  
-						dblQty,                             
-						strCompanyName ,                                                                                                                                
-						intConcurrencyId,
-						intContractDetailId , 
-						intStorageLocationId , 
-						intMarketPremiumUOMId ,
-						intMarketPriceCurrencyId,
-						intItemId,
-						intFutureMarketId,
-						intCompanyId )
-EXEC uspRKCurrencyExposureForStock
-					 @intCommodityId =@intCommodityId
-					, @dtmClosingPrice  = @dtmFutureClosingDate
-					, @intCurrencyId =@intCurrencyId
-
-DECLARE @tblRKExposureForOTC TABLE (
-						intRowNum int,
-						strInternalTradeNo nvarchar(100)
-						,dtmFilledDate datetime
-						,strBuySell nvarchar(100)
-						,intBankId int
-						,strBankName nvarchar(100)
-						,dtmMaturityDate datetime
-						,intCurrencyExchangeRateTypeId int
-						,strCurrencyExchangeRateType nvarchar(100)
-						,dblContractAmount numeric(24,10)
-						,dblExchangeRate numeric(24,10)
-						,strExchangeFromCurrency  nvarchar(100)
-						,dblMatchAmount numeric(24,10)
-						,strMatchedFromCurrency  nvarchar(100)
-						,strCompanyName  nvarchar(100)
-						,intConcurrencyId int
-						,intFutOptTransactionId int
-						,intExchangeRateCurrencyId int
-						,intAmountCurrencyId int
-						,intCompanyId int
-						)
+	INSERT INTO @tblRKStock (intRowNum
+		, dblMarketPrice
+		, dblValue
+		, strContractNumber
+		, strLotNumber
+		, strName
+		, strItemNo
+		, strFutMarketName
+		, strSpotMonth
+		, dblSettlementPrice
+		, dblMarketPremium
+		, strMarketPremiumUOM
+		, strMarketPriceUOM
+		, dblQty
+		, strCompanyName
+		, intConcurrencyId
+		, intContractDetailId
+		, intStorageLocationId
+		, intMarketPremiumUOMId
+		, intMarketPriceCurrencyId
+		, intItemId
+		, intFutureMarketId
+		, intCompanyId)
+	EXEC uspRKCurrencyExposureForStock @intCommodityId = @intCommodityId
+		, @dtmClosingPrice = @dtmFutureClosingDate
+		, @intCurrencyId = @intCurrencyId
+	
+	DECLARE @tblRKExposureForOTC TABLE (intRowNum INT
+		, strInternalTradeNo NVARCHAR(100)
+		, dtmFilledDate DATETIME
+		, strBuySell NVARCHAR(100)
+		, intBankId INT
+		, strBankName NVARCHAR(100)
+		, dtmMaturityDate DATETIME
+		, intCurrencyExchangeRateTypeId INT
+		, strCurrencyExchangeRateType NVARCHAR(100)
+		, dblContractAmount NUMERIC(24, 10)
+		, dblExchangeRate NUMERIC(24, 10)
+		, strExchangeFromCurrency NVARCHAR(100)
+		, dblMatchAmount NUMERIC(24, 10)
+		, strMatchedFromCurrency NVARCHAR(100)
+		, strCompanyName NVARCHAR(100)
+		, intConcurrencyId INT
+		, intFutOptTransactionId INT
+		, intExchangeRateCurrencyId INT
+		, intAmountCurrencyId INT
+		, intCompanyId INT)
 						
-INSERT INTO @tblRKExposureForOTC (	intRowNum ,
-						strInternalTradeNo 
-						,dtmFilledDate 
-						,strBuySell  
-						,intBankId 
-						,strBankName 
-						,dtmMaturityDate 
-						,intCurrencyExchangeRateTypeId 
-						,strCurrencyExchangeRateType 
-						,dblContractAmount 
-						,dblExchangeRate 
-						,strExchangeFromCurrency 
-						,dblMatchAmount 
-						,strMatchedFromCurrency 
-						,strCompanyName  
-						,intConcurrencyId 
-						,intFutOptTransactionId
-						,intExchangeRateCurrencyId 
-						,intAmountCurrencyId 
-						,intCompanyId ) 
+	INSERT INTO @tblRKExposureForOTC (intRowNum
+		, strInternalTradeNo
+		, dtmFilledDate
+		, strBuySell
+		, intBankId
+		, strBankName
+		, dtmMaturityDate
+		, intCurrencyExchangeRateTypeId
+		, strCurrencyExchangeRateType
+		, dblContractAmount
+		, dblExchangeRate
+		, strExchangeFromCurrency
+		, dblMatchAmount
+		, strMatchedFromCurrency
+		, strCompanyName
+		, intConcurrencyId
+		, intFutOptTransactionId
+		, intExchangeRateCurrencyId
+		, intAmountCurrencyId
+		, intCompanyId) 
+	EXEC uspRKCurrencyExposureForOTC @intCommodityId = @intCommodityId
+	
+	DECLARE @tblRKExposureForNonOTC TABLE (intRowNum INT
+		, strContractNumber NVARCHAR(100)
+		, strName NVARCHAR(100)
+		, dblQuantity NUMERIC(24, 10)
+		, strUnitMeasure NVARCHAR(100)
+		, dblOrigPrice NUMERIC(24, 10)
+		, strOrigPriceUOM NVARCHAR(100)
+		, dtmPeriod NVARCHAR(100)
+		, strContractType NVARCHAR(100)
+		, strCompanyName NVARCHAR(100)
+		, intConcurrencyId INT
+		, intContractDetailId INT
+		, intEntityId INT
+		, intUnitMeasureId INT
+		, intCurrencyId INT
+		, intCompanyId INT
+		, dblPrice NUMERIC(24, 10)
+		, dblUSDValue NUMERIC(24, 10))
 
-EXEC uspRKCurrencyExposureForOTC 		 @intCommodityId =@intCommodityId
+	INSERT INTO @tblRKExposureForNonOTC (intRowNum
+		, strContractNumber
+		, strName
+		, dblQuantity
+		, strUnitMeasure
+		, dblOrigPrice
+		, strOrigPriceUOM
+		, dtmPeriod
+		, strContractType
+		, strCompanyName
+		, intConcurrencyId
+		, intContractDetailId
+		, intEntityId
+		, intUnitMeasureId
+		, intCurrencyId
+		, intCompanyId
+		, dblPrice
+		, dblUSDValue)
+	EXEC uspRKCurExpForNonSelectedCurrency @intCommodityId = @intCommodityId
+		, @dtmClosingPrice = @dtmFutureClosingDate
+		, @intCurrencyId =@intCurrencyId
 
-DECLARE @tblRKExposureForNonOTC TABLE (
-						  intRowNum int
-						 ,strContractNumber nvarchar(100)
-						 ,strName  nvarchar(100)                                                                                           
-						 ,dblQuantity  numeric(24,10)                           
-						 ,strUnitMeasure nvarchar(100)                                      
-						 ,dblOrigPrice  numeric(24,10)                          
-						 ,strOrigPriceUOM  nvarchar(100)                                                                           
-						 ,dtmPeriod  nvarchar(100)             
-						 ,strContractType nvarchar(100)						
-						 ,strCompanyName  nvarchar(100)
-						 ,intConcurrencyId int
-						 ,intContractDetailId int
-						 ,intEntityId int
-						 ,intUnitMeasureId int
-						 ,intCurrencyId int
-						 ,intCompanyId int
-						 ,dblPrice numeric(24,10)
-						 ,dblUSDValue numeric(24,10)
-						)
+	DECLARE @tblRKExposureSummary TABLE (intRowNum int
+		, strSum NVARCHAR(100)
+		, dblUSD NUMERIC(24, 10)
+		, intConcurrencyId INT)
+	
+	INSERT INTO @tblRKExposureSummary (intRowNum
+		, strSum
+		, dblUSD
+		, intConcurrencyId)
+	EXEC uspRKCurrencyExposureSummary @intCommodityId = @intCommodityId
+		, @dtmFutureClosingDate = @dtmFutureClosingDate
+		, @intCurrencyId = @intCurrencyId
+		, @dblAP = @dblAP
+		, @dblAR = @dblAR
+		
+	-- Addded money market and bank balance value while saving -- start
+	DECLARE @intRowNum int
+	SELECT @intRowNum=max(intRowNum) from @tblRKExposureSummary 
+	INSERT INTO @tblRKExposureSummary (	intRowNum 
+								,strSum 
+								,dblUSD 
+								,intConcurrencyId 
+								) 
+	SELECT ISNULL(@intRowNum,0)+1,'1. Treasury',sum(dblAmount),1 from(
+	SELECT ISNULL(SUM(dblAmount),0) dblAmount FROM tblRKCurExpBankBalance WHERE intCurrencyExposureId=@intCurrencyExposureId
+	UNION ALL
+	SELECT ISNULL(SUM(dblAmount),0) dblAmount FROM tblRKCurExpMoneyMarket WHERE intCurrencyExposureId=@intCurrencyExposureId)t
+	-- end 
 
-INSERT INTO @tblRKExposureForNonOTC (	intRowNum 
-							,strContractNumber 
-							,strName                                                                                       
-							,dblQuantity                            
-							,strUnitMeasure                                    
-							,dblOrigPrice                         
-							,strOrigPriceUOM                                                                             
-							,dtmPeriod             
-							,strContractType 
-							,strCompanyName  
-							,intConcurrencyId 
-							,intContractDetailId 
-							,intEntityId 
-							,intUnitMeasureId 
-							,intCurrencyId 
-							,intCompanyId 
-							,dblPrice							
-							,dblUSDValue) 
+	BEGIN TRANSACTION 
 
-EXEC uspRKCurExpForNonSelectedCurrency 	
-					  @intCommodityId =@intCommodityId					
-					, @dtmClosingPrice  = @dtmFutureClosingDate
-					, @intCurrencyId =@intCurrencyId
+	INSERT INTO tblRKCurExpStock (intConcurrencyId,
+									intCurrencyExposureId,
+									intContractDetailId,
+									strLotNumber,
+									intStorageLocationId,
+									intItemId,
+									intFutureMarketId,
+									strSpotMonth,
+									dblClosingPrice,
+									dblMarketPremium,
+									intMarketPremiumUOMId,
+									dblMarketPrice,
+									intMarketPriceUOMId,
+									dblQuantity,
+									dblValue,
+									intCompanyId)
+	SELECT 1,
+			@intCurrencyExposureId,
+			intContractDetailId,
+			strLotNumber,
+			intStorageLocationId,
+			intItemId,
+			intFutureMarketId,
+			strSpotMonth,
+			dblSettlementPrice,
+			dblMarketPremium,
+			intMarketPremiumUOMId,
+			dblMarketPrice,
+			intMarketPriceCurrencyId,
+			dblQty,
+			isnull(dblValue,0.0),
+			intCompanyId FROM @tblRKStock
+
+	insert into tblRKCurExpCurrencyContract(intConcurrencyId,
+	intCurrencyExposureId,
+	intFutOptTransactionId,
+	dtmDate,
+	strBuySell,
+	intBankId,
+	dtmMaturityDate,
+	strCurrencyPair,
+	dblAmount,
+	intAmountCurrencyId,
+	dblExchangeRate,
+	intExchangeRateCurrencyId,
+	dblBalanceAmount,
+	intBalanceAmountCurrencyId,
+	intCompanyId)
+	SELECT 1,@intCurrencyExposureId,
+			intFutOptTransactionId,
+			dtmFilledDate,
+			strBuySell,
+			intBankId,
+			dtmMaturityDate,
+			strCurrencyExchangeRateType,
+			dblContractAmount,
+			intAmountCurrencyId,
+			dblExchangeRate,
+			intExchangeRateCurrencyId,
+			dblMatchAmount,
+			intAmountCurrencyId,
+			intCompanyId 
+	FROM @tblRKExposureForOTC
+
+	INSERT INTO tblRKCurExpNonOpenSales(intConcurrencyId,
+	intCurrencyExposureId,
+	intCustomerId,
+	dblQuantity,
+	intQuantityUOMId,
+	dblOrigPrice,
+	intOrigPriceUOMId,
+	intOrigPriceCurrencyId,
+	dblPrice,
+	strPeriod,
+	strContractType,
+	dblValueUSD,
+	intCompanyId,intContractDetailId)
+	SELECT 1,@intCurrencyExposureId,intEntityId,
+	dblQuantity,
+	intUnitMeasureId,
+	dblOrigPrice,
+	intUnitMeasureId,
+	intCurrencyId,
+	dblPrice,
+	dtmPeriod,
+	strContractType,
+	dblUSDValue,
+	intCompanyId,intContractDetailId from @tblRKExposureForNonOTC
+
+	INSERT INTO tblRKCurExpSummary(intConcurrencyId,
+	intCurrencyExposureId,
+	strTotalSum,
+	dblUSD)
+	SELECT 1,@intCurrencyExposureId,strSum,sum(dblUSD) FROM @tblRKExposureSummary group by strSum
 					
-
-DECLARE @tblRKExposureSummary TABLE (
-						  intRowNum int
-						 ,strSum nvarchar(100)
-						 ,dblUSD  numeric(24,10)                                                                                             
-						 ,intConcurrencyId  int   
-						)
-
-INSERT INTO @tblRKExposureSummary (	intRowNum 
-							,strSum 
-							,dblUSD                                                                                       
-							,intConcurrencyId                            
-							) 
-EXEC uspRKCurrencyExposureSummary 	
-					 @intCommodityId =@intCommodityId
-					, @dtmFutureClosingDate  = @dtmFutureClosingDate
-					, @intCurrencyId =@intCurrencyId
-					, @dblAP = @dblAP
-					, @dblAR = @dblAR					
--- Addded money market and bank balance value while saving -- start
-DECLARE @intRowNum int
-SELECT @intRowNum=max(intRowNum) from @tblRKExposureSummary 
-INSERT INTO @tblRKExposureSummary (	intRowNum 
-							,strSum 
-							,dblUSD                                                                                       
-							,intConcurrencyId                            
-							) 
-SELECT ISNULL(@intRowNum,0)+1,'1. Treasury',sum(dblAmount),1 from(
-SELECT  ISNULL(SUM(dblAmount),0) dblAmount FROM tblRKCurExpBankBalance WHERE intCurrencyExposureId=@intCurrencyExposureId
-UNION ALL
-SELECT  ISNULL(SUM(dblAmount),0) dblAmount FROM tblRKCurExpMoneyMarket WHERE intCurrencyExposureId=@intCurrencyExposureId)t
--- end 
-
-BEGIN TRANSACTION    
-
-INSERT INTO tblRKCurExpStock (intConcurrencyId,
-								intCurrencyExposureId,
-								intContractDetailId,
-								strLotNumber,
-								intStorageLocationId,
-								intItemId,
-								intFutureMarketId,
-								strSpotMonth,
-								dblClosingPrice,
-								dblMarketPremium,
-								intMarketPremiumUOMId,
-								dblMarketPrice,
-								intMarketPriceUOMId,
-								dblQuantity,
-								dblValue,
-								intCompanyId)
-SELECT 1,
-		@intCurrencyExposureId,
-		intContractDetailId,
-		strLotNumber,
-		intStorageLocationId,
-		intItemId,
-		intFutureMarketId,
-		strSpotMonth,
-		dblSettlementPrice,
-		dblMarketPremium,
-		intMarketPremiumUOMId,
-		dblMarketPrice,
-		intMarketPriceCurrencyId,
-		dblQty,
-		isnull(dblValue,0.0),
-		intCompanyId FROM @tblRKStock
-
-insert into tblRKCurExpCurrencyContract(intConcurrencyId,
-intCurrencyExposureId,
-intFutOptTransactionId,
-dtmDate,
-strBuySell,
-intBankId,
-dtmMaturityDate,
-strCurrencyPair,
-dblAmount,
-intAmountCurrencyId,
-dblExchangeRate,
-intExchangeRateCurrencyId,
-dblBalanceAmount,
-intBalanceAmountCurrencyId,
-intCompanyId)
-SELECT 1,@intCurrencyExposureId,
-		intFutOptTransactionId,
-		dtmFilledDate,
-		strBuySell,
-		intBankId,
-		dtmMaturityDate,
-		strCurrencyExchangeRateType,
-		dblContractAmount,
-		intAmountCurrencyId,
-		dblExchangeRate,
-		intExchangeRateCurrencyId,
-		dblMatchAmount,
-		intAmountCurrencyId,
-		intCompanyId 
-FROM @tblRKExposureForOTC
-
-INSERT INTO tblRKCurExpNonOpenSales(intConcurrencyId,
-intCurrencyExposureId,
-intCustomerId,
-dblQuantity,
-intQuantityUOMId,
-dblOrigPrice,
-intOrigPriceUOMId,
-intOrigPriceCurrencyId,
-dblPrice,
-strPeriod,
-strContractType,
-dblValueUSD,
-intCompanyId,intContractDetailId)
-SELECT 1,@intCurrencyExposureId,intEntityId,
-dblQuantity,
-intUnitMeasureId,
-dblOrigPrice,
-intUnitMeasureId,
-intCurrencyId,
-dblPrice,
-dtmPeriod,
-strContractType,
-dblUSDValue,
-intCompanyId,intContractDetailId from @tblRKExposureForNonOTC
-
-INSERT INTO tblRKCurExpSummary(intConcurrencyId,
-intCurrencyExposureId,
-strTotalSum,
-dblUSD)
-SELECT 1,@intCurrencyExposureId,strSum,sum(dblUSD) FROM @tblRKExposureSummary group by  strSum
-					
-COMMIT TRAN    
-    
-END TRY      
-      
-BEGIN CATCH  
-   
- SET @ErrMsg = ERROR_MESSAGE()  
- IF XACT_STATE() != 0 ROLLBACK TRANSACTION   
- If @ErrMsg != ''   
- BEGIN  
-  RAISERROR(@ErrMsg, 16, 1, 'WITH NOWAIT')  
- END  
-   
-END CATCH  
+	COMMIT TRAN 
+ 
+END TRY 
+BEGIN CATCH
+	SET @ErrMsg = ERROR_MESSAGE()
+	IF XACT_STATE() != 0 ROLLBACK TRANSACTION
+	IF @ErrMsg != ''
+	BEGIN
+		RAISERROR(@ErrMsg, 16, 1, 'WITH NOWAIT')
+	END 
+END CATCH 
