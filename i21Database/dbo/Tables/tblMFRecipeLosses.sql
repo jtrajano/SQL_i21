77623fc69cf842cdfb4cf38783ff2ba756c03748
Vinoth Kumar T <vinoth.t@irely.com>
2019-07-05 21:30:37 +0530

    MFG-3868

---
+++ i21Database/dbo/Tables/tblMFRecipeLosses.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE tblMFRecipeLosses (
+	intRecipeLossesId INT NOT NULL identity(1, 1)
+	,intConcurrencyId INT
+	,intRecipeId INT NOT NULL
+	,intItemId INT
+	,intBundleItemId INT
+	,dblLoss1 NUMERIC(18, 6)
+	,dblLoss2 NUMERIC(18, 6)
+	,intCreatedUserId INT NULL
+	,dtmCreated DATETIME NULL CONSTRAINT DF_tblMFRecipeLosses_dtmCreated DEFAULT GETDATE()
+	,intLastModifiedUserId INT NULL
+	,dtmLastModified DATETIME NULL CONSTRAINT DF_tblMFRecipeLosses_dtmLastModified DEFAULT GETDATE()
+	,CONSTRAINT PK_tblMFRecipeLosses_intRecipeLossesId PRIMARY KEY (intRecipeLossesId)
+	,CONSTRAINT UQ_tblMFRecipeLosses_intRecipeId_intItemId_intBundleItemId UNIQUE (
+		intRecipeId
+		,intItemId
+		,intBundleItemId
+		)
+	,CONSTRAINT FK_tblMFRecipeLosses_tblMFRecipe_intRecipeId FOREIGN KEY (intRecipeId) REFERENCES tblMFRecipe(intRecipeId)
+	,CONSTRAINT FK_tblMFRecipeLosses_tblICItem_intItemId FOREIGN KEY (intItemId) REFERENCES tblICItem(intItemId)
+	)
