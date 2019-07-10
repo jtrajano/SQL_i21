145d5ebaaf87cd9082a934109e79ca3d5ba1701e
henry_zaragoza <henry.zaragoza@irely.com>
2019-07-10 14:46:32 +0800

    ST-966 Inventory Mass Maintenance Revamp

---
+++ i21Database/dbo/User Defined Types/StoreItemPricing.sql
@@ -0,0 +1,9 @@
+﻿CREATE TYPE [dbo].[StoreItemPricing] AS TABLE
+(
+	intItemPricingId			INT				NOT NULL
+	, intItemId					INT				NOT NULL 
+	, dblStandardCost			NUMERIC(18, 6)	NULL
+	, dblLastCost				NUMERIC(18, 6)	NULL
+)
+
+--DROP TYPE [dbo].[StoreItemPricing]
