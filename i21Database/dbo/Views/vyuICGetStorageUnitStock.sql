33676af54a4db5033b402c5b2deebcbc2cbbfab8
Feb Montefrio <feb.montefrio@irely.com>
2019-07-12 10:46:47 +0800

    [B] IC-6047

--- i21Database/dbo/Views/vyuICGetStorageUnitStock.sql
+++ i21Database/dbo/Views/vyuICGetStorageUnitStock.sql
@@ -1,19 +1,18 @@
 ﻿CREATE VIEW [dbo].[vyuICGetStorageUnitStock]
 AS
 SELECT
-      intItemStockUOMId		= StockUOM.intItemStockUOMId
+    intStorageUnitId		= StorageLocation.intStorageLocationId
+	, strStorageUnit		= StorageLocation.strName
 	, strItemNo				= Item.strItemNo
 	, intItemId				= Item.intItemId
 	, intCommodityId		= Item.intCommodityId
 	, strCommodityCode		= Commodity.strCommodityCode
-	, intLocationId			= ItemLoc.intLocationId
+	, intLocationId			= StorageLocation.intLocationId
 	, strLocation			= CompLocation.strLocationName
 	, intStorageLocationId	= SubLocation.intCompanyLocationSubLocationId
-	, strStorageLocation	= SubLocation.strSubLocationName
-	, intStorageUnitId		= StorageLocation.intStorageLocationId
-	, strStorageUnit		= StorageLocation.strName
+	, strStorageLocation	= SubLocation.strSubLocationName		
 	, dblLastCost			= ISNULL(ItemPricing.dblLastCost, ItemPricing.dblStandardCost)
-	, dblOnHand				= ISNULL(StockUOM.dblOnHand, 0)--(CASE WHEN ISNULL(Lot.intLotId, '') = '' THEN ISNULL(StockUOM.dblOnHand, 0) ELSE ISNULL(Lot.dblQty, 0) END)
+	, dblOnHand				= ISNULL(StockUOM.dblOnHand, 0)
 	, strUnitMeasure		= UOM.strUnitMeasure
 	, dblEffectiveDepth		= StorageLocation.dblEffectiveDepth
 	, dblResidualUnit		= StorageLocation.dblResidualUnit
@@ -21,20 +20,30 @@ SELECT
 	, dblPackFactor			= StorageLocation.dblPackFactor
 	, strUPC				= ItemUOM.strUpcCode
 	, strLongUPC			= ItemUOM.strLongUPCCode
-	--, intLotId				= Lot.intLotId
-	--, strLotNumber			= Lot.strLotNumber
-FROM tblICItemStockUOM StockUOM
-	INNER JOIN tblICItem Item ON Item.intItemId = StockUOM.intItemId
-	LEFT OUTER JOIN tblICCommodity Commodity ON Commodity.intCommodityId = Item.intCommodityId
-	LEFT JOIN tblICItemLocation ItemLoc ON ItemLoc.intItemLocationId = StockUOM.intItemLocationId
-	LEFT JOIN tblICItemPricing ItemPricing ON ItemPricing.intItemLocationId = ItemLoc.intItemLocationId
-	LEFT OUTER JOIN tblSMCompanyLocation CompLocation ON CompLocation.intCompanyLocationId = ItemLoc.intLocationId
-	LEFT OUTER JOIN tblICStorageLocation StorageLocation ON StorageLocation.intStorageLocationId = StockUOM.intStorageLocationId
-	LEFT OUTER JOIN tblSMCompanyLocationSubLocation SubLocation ON SubLocation.intCompanyLocationSubLocationId = StockUOM.intSubLocationId
-	INNER JOIN tblICItemUOM ItemUOM ON ItemUOM.intItemUOMId = StockUOM.intItemUOMId AND ItemUOM.ysnStockUnit = 1
-	INNER JOIN tblICUnitMeasure UOM ON UOM.intUnitMeasureId = ItemUOM.intUnitMeasureId AND UOM.strUnitType IN('Volume', 'Weight')
-	--LEFT OUTER JOIN tblICLot Lot ON Lot.intItemId = StockUOM.intItemId
-	--	AND Lot.intItemLocationId = StockUOM.intItemLocationId
-	--	AND Lot.intItemUOMId = StockUOM.intItemUOMId
-	--	AND Lot.intSubLocationId = StockUOM.intSubLocationId
-	--	AND Lot.intStorageLocationId = StockUOM.intStorageLocationId
+FROM 
+	tblICStorageLocation StorageLocation 
+	LEFT JOIN (
+		tblICItem Item INNER JOIN tblICItemUOM ItemUOM 
+			ON ItemUOM.intItemId = Item.intItemId AND ItemUOM.ysnStockUnit = 1
+		INNER JOIN tblICUnitMeasure UOM 
+			ON UOM.intUnitMeasureId = ItemUOM.intUnitMeasureId AND UOM.strUnitType IN('Volume', 'Weight')
+		LEFT JOIN tblICItemStockUOM StockUOM 
+			ON StockUOM.intItemId = Item.intItemId AND StockUOM.intItemUOMId = ItemUOM.intItemUOMId
+		LEFT JOIN tblICCommodity Commodity 
+			ON Commodity.intCommodityId = Item.intCommodityId
+	)
+		ON StorageLocation.intStorageLocationId = StockUOM.intStorageLocationId
+	LEFT JOIN tblSMCompanyLocation CompLocation 
+		ON CompLocation.intCompanyLocationId = StorageLocation.intLocationId
+	LEFT JOIN tblSMCompanyLocationSubLocation SubLocation 
+		ON SubLocation.intCompanyLocationSubLocationId = StorageLocation.intSubLocationId
+	OUTER APPLY (
+		SELECT 
+			ItemPricing.*
+		FROM 
+			tblICItemLocation ItemLoc LEFT JOIN tblICItemPricing ItemPricing 
+				ON ItemPricing.intItemLocationId = ItemLoc.intItemLocationId
+		WHERE
+			ItemLoc.intItemId = Item.intItemId
+			AND ItemLoc.intLocationId = StorageLocation.intLocationId	
+	) ItemPricing
