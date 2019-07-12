a4f782b6098b19dc956c7fa01bb0a581b6ae840d
Mon Gonzales <mon.gonzales@irely.com>
2019-07-12 10:04:27 +0800

    CT-3405 - Contract sequence- Item dropdown should filter the list by selected book selected in the contract sequence

---
+++ i21Database/dbo/Views/vyuCTBookItem.sql
@@ -0,0 +1,34 @@
+﻿CREATE VIEW [dbo].[vyuCTBookItem]
+	AS 
+
+
+select 
+	intId = CAST(ROW_NUMBER() OVER (ORDER BY c.intBookId, c.intItemId) AS INT), 
+	c.intBookId,
+	c.strBook,
+	c.strBookDescription,
+	c.ysnActive,
+	c.intItemId
+from 
+(
+
+select 
+		a.intBookId, 
+		a.strBook, 
+		a.strBookDescription, 
+		a.ysnActive,
+		-99 as intItemId
+	from tblCTBook as a
+
+union all
+select 
+		a.intBookId, 
+		a.strBook, 
+		a.strBookDescription, 
+		a.ysnActive,
+		b.intItemId
+	from tblCTBook as a
+	join tblICItemBook b
+		on a.intBookId = b.intBookId
+
+) c
