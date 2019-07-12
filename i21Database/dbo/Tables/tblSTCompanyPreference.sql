2bec91013cd6d69d6cdd2ec158a81df414087370
COMTECH\BPonce <bonn.ponce@irely.com>
2019-07-12 10:55:09 +0800

    ST-1392

---
+++ i21Database/dbo/Tables/tblSTCompanyPreference.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[tblSTCompanyPreference] (
+    [intCompanyPreferenceId]		INT            IDENTITY (1, 1) NOT NULL,
+    [strDailySecurityCode]			NVARCHAR (MAX) NULL,
+    [dtmDateEntered]				DATETIME	   NULL,
+    [ysnEnableLotteryManagement]	BIT			   NULL,
+    [intConcurrencyId] INT          CONSTRAINT [DF_tblSTCompanyPreference_intConcurrencyId] DEFAULT ((1)) NULL,
+    CONSTRAINT [PK_tblSTCompanyPreference] PRIMARY KEY CLUSTERED ([intCompanyPreferenceId] ASC)
+);
+
