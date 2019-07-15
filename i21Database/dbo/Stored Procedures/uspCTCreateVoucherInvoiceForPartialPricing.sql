434d0ed74c521e16a1d1bd591847195eceb7279b
Mon Gonzales <mon.gonzales@irely.com>
2019-07-15 12:32:21 +0800

    CT-2884 - [Price Contracts] - Load Contracts needs ability to price by number of loads

--- i21Database/dbo/Stored Procedures/uspCTCreateVoucherInvoiceForPartialPricing.sql
+++ i21Database/dbo/Stored Procedures/uspCTCreateVoucherInvoiceForPartialPricing.sql
@@ -708,6 +708,7 @@ BEGIN TRY
 				OUTER APPLY ( select top 1 intInvoiceDetailId 
 								from tblARInvoiceDetail ARD 
 									where ARD.intContractDetailId = CD.intContractDetailId 
+									and ARD.intInventoryShipmentItemId = RI.intInventoryShipmentItemId
 									and ARD.intInventoryShipmentChargeId is null
 								) ARD
 					
