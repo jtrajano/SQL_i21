a652b1da6baebba7c9030d2e6ec976050f267208
henry_zaragoza <henry.zaragoza@irely.com>
2019-07-16 13:31:46 +0800

    ST-1413 Store - Unable to Delete Store

--- i21Database/dbo/Tables/tblSTSubcategoryRegProd.sql
+++ i21Database/dbo/Tables/tblSTSubcategoryRegProd.sql
@@ -8,5 +8,5 @@
     [intConcurrencyId] INT NOT NULL, 
     CONSTRAINT [PK_tblSTSubcategoryRegProd] PRIMARY KEY CLUSTERED ([intRegProdId] ASC),
     CONSTRAINT [AK_tblSTSubcategoryRegProd_strRegProdCode] UNIQUE NONCLUSTERED ([intStoreId],[strRegProdCode] ASC), 
-    CONSTRAINT [FK_tblSTSubcategoryRegProd_tblSTStore] FOREIGN KEY ([intStoreId]) REFERENCES [tblSTStore]([intStoreId]),
+    CONSTRAINT [FK_tblSTSubcategoryRegProd_tblSTStore] FOREIGN KEY ([intStoreId]) REFERENCES [tblSTStore]([intStoreId]) ON DELETE CASCADE,
 );
