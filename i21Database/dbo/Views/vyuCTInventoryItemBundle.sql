687fa4da863a6b229b32a53352d3eef9f76edfcb
Mon Gonzales <mon.gonzales@irely.com>
2019-07-17 08:30:08 +0800

    CT-3416 - [Contract-Sequence] Able to select a bundle and an item outside the bundle at the same time
    Reverted the change in the view

---
+++ i21Database/dbo/Views/vyuCTInventoryItemBundle.sql
@@ -0,0 +1,152 @@
+﻿CREATE VIEW [dbo].[vyuCTInventoryItemBundle]
+
+AS 
+	--column mapping change here should also update vyuCTInventoryItem
+	SELECT intKey = CAST(ROW_NUMBER() OVER(ORDER BY intItemId, intLocationId) AS INT), 
+	* FROM (
+	SELECT	
+			IM.intItemId,
+			IM.strItemNo,
+			IM.strType,
+			IM.strDescription,
+			IM.strLotTracking,
+			IM.strInventoryTracking,
+			IM.strStatus,
+			IL.intLocationId,
+			IM.intCategoryId,
+			CR.strCategoryCode,
+			IM.intCommodityId,
+			CO.strCommodityCode,
+			OG.strCountry AS strOrigin,
+			CA.intPurchasingGroupId,
+			IM.intProductTypeId,
+			PG.strName	AS	strPurchasingGroup,
+			IM.strCostType,
+			CR.dblEstimatedYieldRate,
+			IM.ysnInventoryCost,
+			IM.ysnBasisContract,
+			NULL AS	intBundleId, 
+			NULL as intBundleItemId
+	FROM	tblICItem				IM
+	JOIN	tblICItemLocation		IL	ON	IL.intItemId				=	IM.intItemId		LEFT
+	JOIN	tblICCategory			CR	ON	CR.intCategoryId			=	IM.intCategoryId	LEFT
+	JOIN	tblICCommodity			CO	ON	CO.intCommodityId			=	IM.intCommodityId	LEFT
+	JOIN	tblICCommodityAttribute	CA	ON	CA.intCommodityAttributeId	=	IM.intOriginId
+										AND	CA.strType					=	'Origin'			LEFT
+	JOIN	tblSMCountry			OG	ON	OG.intCountryID				=	CA.intCountryID		LEFT
+	JOIN	tblSMPurchasingGroup	PG	ON	PG.intPurchasingGroupId		=	CA.intPurchasingGroupId		
+	WHERE	IM.strType IN ('Inventory','Finished Good','Raw Material')
+
+
+	UNION ALL
+
+	SELECT	
+			IM.intItemId,
+			IM.strItemNo,
+			IM.strType,
+			IM.strDescription,
+			IM.strLotTracking,
+			IM.strInventoryTracking,
+			IM.strStatus,
+			IL.intLocationId,
+			IM.intCategoryId,
+			CR.strCategoryCode,
+			IM.intCommodityId,
+			CO.strCommodityCode,
+			OG.strCountry AS strOrigin,
+			CA.intPurchasingGroupId,
+			IM.intProductTypeId,
+			PG.strName	AS	strPurchasingGroup,
+			IM.strCostType,
+			CR.dblEstimatedYieldRate,
+			IM.ysnInventoryCost,
+			IM.ysnBasisContract,
+			null AS	intBundleId, 
+			null as intBundleItemId
+	FROM	tblICItem				IM
+	JOIN	tblICItemLocation		IL	ON	IL.intItemId				=	IM.intItemId		
+	LEFT JOIN	tblICCategory			CR	ON	CR.intCategoryId			=	IM.intCategoryId	
+	LEFT JOIN	tblICCommodity			CO	ON	CO.intCommodityId			=	IM.intCommodityId	
+	LEFT JOIN	tblICCommodityAttribute	CA	ON	CA.intCommodityAttributeId	=	IM.intOriginId
+										AND	CA.strType					=	'Origin'			
+	LEFT JOIN	tblSMCountry			OG	ON	OG.intCountryID				=	CA.intCountryID		
+	LEFT JOIN	tblSMPurchasingGroup	PG	ON	PG.intPurchasingGroupId		=	CA.intPurchasingGroupId			
+	WHERE	IM.strType IN ('Bundle')
+
+	UNION ALL
+
+	SELECT	
+			IM.intItemId,
+			IM.strItemNo,
+			IM.strType,
+			IM.strDescription,
+			IM.strLotTracking,
+			IM.strInventoryTracking,
+			IM.strStatus,
+			IL.intLocationId,
+			IM.intCategoryId,
+			CR.strCategoryCode,
+			IM.intCommodityId,
+			CO.strCommodityCode,
+			OG.strCountry AS strOrigin,
+			CA.intPurchasingGroupId,
+			IM.intProductTypeId,
+			PG.strName	AS	strPurchasingGroup,
+			IM.strCostType,
+			CR.dblEstimatedYieldRate,
+			IM.ysnInventoryCost,
+			IM.ysnBasisContract,
+			BI.intItemId AS	intBundleId, 
+			NULL as intBundleItemId
+	FROM	tblICItem				IM
+	JOIN	tblICItemLocation		IL	ON	IL.intItemId				=	IM.intItemId		
+	LEFT JOIN	tblICCategory			CR	ON	CR.intCategoryId			=	IM.intCategoryId	
+	LEFT JOIN	tblICCommodity			CO	ON	CO.intCommodityId			=	IM.intCommodityId	
+	LEFT JOIN	tblICCommodityAttribute	CA	ON	CA.intCommodityAttributeId	=	IM.intOriginId
+										AND	CA.strType					=	'Origin'			
+	LEFT JOIN	tblSMCountry			OG	ON	OG.intCountryID				=	CA.intCountryID		
+	LEFT JOIN	tblSMPurchasingGroup	PG	ON	PG.intPurchasingGroupId		=	CA.intPurchasingGroupId		
+	JOIN	vyuICGetBundleItem		BI	ON	BI.intBundleItemId			=	IM.intItemId 
+	--LEFT JOIN	vyuICGetBundleItem		B2	ON	B2.intItemId				=	IM.intItemId
+	WHERE	IM.strType IN ('Inventory','Finished Good','Raw Material','Bundle')
+	
+	UNION ALL
+
+	SELECT	
+			IM.intItemId,
+			IM.strItemNo,
+			IM.strType,
+			IM.strDescription,
+			IM.strLotTracking,
+			IM.strInventoryTracking,
+			IM.strStatus,
+			IL.intLocationId,
+			IM.intCategoryId,
+			CR.strCategoryCode,
+			IM.intCommodityId,
+			CO.strCommodityCode,
+			OG.strCountry AS strOrigin,
+			CA.intPurchasingGroupId,
+			IM.intProductTypeId,
+			PG.strName	AS	strPurchasingGroup,
+			IM.strCostType,
+			CR.dblEstimatedYieldRate,
+			IM.ysnInventoryCost,
+			IM.ysnBasisContract,
+			NULL AS	intBundleId, 
+			B2.intBundleItemId as intBundleItemId
+	FROM	tblICItem				IM
+	JOIN	tblICItemLocation		IL	ON	IL.intItemId				=	IM.intItemId		
+	LEFT JOIN	tblICCategory			CR	ON	CR.intCategoryId			=	IM.intCategoryId	
+	LEFT JOIN	tblICCommodity			CO	ON	CO.intCommodityId			=	IM.intCommodityId	
+	LEFT JOIN	tblICCommodityAttribute	CA	ON	CA.intCommodityAttributeId	=	IM.intOriginId
+										AND	CA.strType					=	'Origin'			
+	LEFT JOIN	tblSMCountry			OG	ON	OG.intCountryID				=	CA.intCountryID		
+	LEFT JOIN	tblSMPurchasingGroup	PG	ON	PG.intPurchasingGroupId		=	CA.intPurchasingGroupId		
+	JOIN	vyuICGetBundleItem		B2	ON	B2.intItemId				=	IM.intItemId
+	WHERE	IM.strType IN ('Inventory','Finished Good','Raw Material','Bundle')
+
+
+	) fq
+
+
