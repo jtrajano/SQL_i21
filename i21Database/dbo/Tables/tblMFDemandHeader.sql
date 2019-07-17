620a5d7eaeb6360572cd12d7b98f67cc59c0f420
Vinoth Kumar T <vinoth.t@irely.com>
2019-07-17 16:58:40 +0530

    MFG-3938

---
+++ i21Database/dbo/Tables/tblMFDemandHeader.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE tblMFDemandHeader (
+	intDemandHeaderId INT NOT NULL IDENTITY
+	,intConcurrencyId INT NOT NULL
+	,strDemandNo NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL
+	,strDemandName NVARCHAR(100) COLLATE Latin1_General_CI_AS NOT NULL
+	,dtmDate DATETIME NOT NULL
+	,intBookId INT
+	,intSubBookId INT
+
+	,CONSTRAINT [PK_tblMFDemandHeader] PRIMARY KEY (intDemandHeaderId)
+	,CONSTRAINT [UK_tblMFDemandHeader_strDemandName] UNIQUE (strDemandName)
+	,CONSTRAINT [FK_tblMFDemandHeader_tblCTBook_intBookId] FOREIGN KEY (intBookId) REFERENCES [tblCTBook](intBookId)
+	,CONSTRAINT [FK_tblMFDemandHeader_tblCTSubBook_intSubBookId] FOREIGN KEY (intSubBookId) REFERENCES [tblCTSubBook](intSubBookId)
+	)
