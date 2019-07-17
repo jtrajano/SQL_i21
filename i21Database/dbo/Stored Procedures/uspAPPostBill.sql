cffc06125f3836cc47380e2027f12dc627ff4a68
Erick <erick.delacruz@irely.com>
2019-07-17 13:02:24 +0800

    AP-7480

--- i21Database/dbo/Stored Procedures/uspAPPostBill.sql
+++ i21Database/dbo/Stored Procedures/uspAPPostBill.sql
@@ -182,7 +182,7 @@ BEGIN
 		,strTransactionNo
 		,intTransactionId
 		,26
-	FROM dbo.fnCCValidateAssociatedTransaction(@billIds, @transactionType)
+	FROM dbo.fnCCValidateAssociatedTransaction(@billIds, 1)
 	
 	--if there are invalid applied amount, undo updating of amountdue and payment
 	IF EXISTS(SELECT 1 FROM #tmpInvalidBillData WHERE intErrorKey = 1 OR intErrorKey = 33)
