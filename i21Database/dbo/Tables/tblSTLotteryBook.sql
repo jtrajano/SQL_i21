7f54553317037fca160846e3baa5bb981676628c
COMTECH\BPonce <bonn.ponce@irely.com>
2019-07-17 19:57:45 +0800

    ST-1396 ST-1394 ST-1393

---
+++ i21Database/dbo/Tables/tblSTLotteryBook.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[tblSTLotteryBook] (
+    [intLotteryBookId]       INT              IDENTITY (1, 1) NOT NULL,
+    [intStoreId]             INT              NULL,
+    [strBookNumber]          NVARCHAR (MAX)   COLLATE Latin1_General_CI_AS NULL,
+    [intLotteryGameId]       INT              NULL,
+    [strBinNumber]           NVARCHAR (200)   COLLATE Latin1_General_CI_AS NULL,
+    [dtmReceiptDate]         DATETIME         NULL,
+    [dblQuantityRemaining]   NUMERIC (18, 15) NULL,
+    [dtmActivateDate]        DATETIME         NULL,
+    [strStatus]              NVARCHAR (MAX)   NULL,
+    [dblTicketValue]         NUMERIC (18, 15) NULL,
+    [dblTicketCost]          NUMERIC (18, 15) NULL,
+    [dblTotalInventoryValue] NUMERIC (18, 15) NULL,
+    [dblTotalInventoryCost]  NUMERIC (18, 15) NULL,
+    [intConcurrencyId]       INT              NOT NULL,
+    CONSTRAINT [PK_tblSTLotteryBook] PRIMARY KEY CLUSTERED ([intLotteryBookId] ASC),
+    CONSTRAINT [FK_tblSTLotteryBook_tblSTLotteryGame] FOREIGN KEY ([intLotteryGameId]) REFERENCES [dbo].[tblSTLotteryGame] ([intLotteryGameId]) ON DELETE CASCADE,
+    CONSTRAINT [FK_tblSTLotteryBook_tblSTStore] FOREIGN KEY ([intStoreId]) REFERENCES [dbo].[tblSTStore] ([intStoreId])
+);
+
