398de46835552f4fe72c0c12f60bf801aa85a41b
lawrence_zabala <lawrence.zabala@irely.com>
2019-07-19 15:50:18 +0800

    RM-2409

--- i21Database/dbo/Stored Procedures/uspRKInsertM2MBasisDetails.sql
+++ i21Database/dbo/Stored Procedures/uspRKInsertM2MBasisDetails.sql
@@ -1,83 +1,81 @@
-﻿ CREATE PROC [dbo].[uspRKInsertM2MBasisDetails]
-		@intM2MBasisId int
-	   ,@intRowNumbers nvarchar(max)
+﻿CREATE PROCEDURE [dbo].[uspRKInsertM2MBasisDetails]
+	@intM2MBasisId INT
+	, @intRowNumbers NVARCHAR(MAX)
+
 AS
- 
-  DECLARE @tempBasis TABLE(  
-       intRowNumber int
-      ,strCommodityCode nvarchar(50)  
-      ,strItemNo nvarchar(50)  
-      ,strOriginDest nvarchar(50)  
-      ,strFutMarketName nvarchar(50)  
-      ,strFutureMonth nvarchar(50)  
-      ,strPeriodTo nvarchar(50) COLLATE Latin1_General_CI_AS  
-      ,strLocationName nvarchar(50)  
-      ,strMarketZoneCode nvarchar(50)  
-      ,strCurrency nvarchar(50)  
-      ,strPricingType nvarchar(50)  
-      ,strContractInventory nvarchar(50)  
-      ,strContractType nvarchar(50)  
-      ,dblCashOrFuture numeric(16,10)  
-      ,dblBasisOrDiscount numeric(16,10)
-      ,dblRatio numeric(16,10)  
-      ,strUnitMeasure nvarchar(50)  
-      ,intCommodityId int  
-      ,intItemId int  
-      ,intOriginId int  
-      ,intFutureMarketId int  
-      ,intFutureMonthId int  
-      ,intCompanyLocationId int  
-      ,intMarketZoneId int  
-      ,intCurrencyId int  
-      ,intPricingTypeId int  
-      ,intContractTypeId int  
-      ,intUnitMeasureId  int  
-      ,intConcurrencyId int
-      ,strMarketValuation nvarchar(250)  
-      ,ysnLicensed BIT)  
 
- INSERT INTO @tempBasis
- EXEC uspRKGetM2MBasis
+DECLARE @tempBasis TABLE(intRowNumber INT
+	, strCommodityCode NVARCHAR(50)
+	, strItemNo NVARCHAR(50)
+	, strOriginDest NVARCHAR(50)
+	, strFutMarketName NVARCHAR(50)
+	, strFutureMonth NVARCHAR(50)
+	, strPeriodTo NVARCHAR(50) COLLATE Latin1_General_CI_AS
+	, strLocationName NVARCHAR(50)
+	, strMarketZoneCode NVARCHAR(50)
+	, strCurrency NVARCHAR(50)
+	, strPricingType NVARCHAR(50)
+	, strContractInventory NVARCHAR(50)
+	, strContractType NVARCHAR(50)
+	, dblCashOrFuture NUMERIC(16, 10)
+	, dblBasisOrDiscount NUMERIC(16, 10)
+	, dblRatio NUMERIC(16, 10)
+	, strUnitMeasure NVARCHAR(50)
+	, intCommodityId INT
+	, intItemId INT
+	, intOriginId INT
+	, intFutureMarketId INT
+	, intFutureMonthId INT
+	, intCompanyLocationId INT
+	, intMarketZoneId INT
+	, intCurrencyId INT
+	, intPricingTypeId INT
+	, intContractTypeId INT
+	, intUnitMeasureId INT
+	, intConcurrencyId INT
+	, strMarketValuation NVARCHAR(250)
+	, ysnLicensed BIT
+	, intBoardMonthId INT
+	, strBoardMonth NVARCHAR(50))  
 
- INSERT INTO [dbo].[tblRKM2MBasisDetail]
-           ([intM2MBasisId]
-           ,[intConcurrencyId]
-           ,[intCommodityId]
-           ,[intItemId]
-           ,[strOriginDest]
-           ,[intFutureMarketId]
-           ,[intFutureMonthId]
-           ,[strPeriodTo]
-           ,[intCompanyLocationId]
-           ,[intMarketZoneId]
-           ,[intCurrencyId]
-           ,[intPricingTypeId]
-           ,[strContractInventory]
-           ,[intContractTypeId]
-           ,[dblCashOrFuture]
-           ,[dblBasisOrDiscount]
-		   ,[dblRatio]
-           ,[intUnitMeasureId])
-     SELECT
-           @intM2MBasisId
-		   ,1
-		   ,intCommodityId
-		   ,intItemId
-		   ,strOriginDest
-           ,intFutureMarketId
-           ,intFutureMonthId
-           ,strPeriodTo
-           ,intCompanyLocationId
-           ,intMarketZoneId
-           ,intCurrencyId
-           ,intPricingTypeId
-           ,strContractInventory
-           ,intContractTypeId
-           ,dblCashOrFuture
-           ,dblBasisOrDiscount
-		   ,dblRatio
-           ,intUnitMeasureId
-	FROM @tempBasis
-	WHERE intRowNumber NOT IN (SELECT * FROM dbo.[fnCommaSeparatedValueToTable](@intRowNumbers)) 
+INSERT INTO @tempBasis
+EXEC uspRKGetM2MBasis
 
-
+INSERT INTO tblRKM2MBasisDetail (intM2MBasisId
+	, intConcurrencyId
+	, intCommodityId
+	, intItemId
+	, strOriginDest
+	, intFutureMarketId
+	, intFutureMonthId
+	, strPeriodTo
+	, intCompanyLocationId
+	, intMarketZoneId
+	, intCurrencyId
+	, intPricingTypeId
+	, strContractInventory
+	, intContractTypeId
+	, dblCashOrFuture
+	, dblBasisOrDiscount
+	, dblRatio
+	, intUnitMeasureId)
+SELECT @intM2MBasisId
+	, 1
+	, intCommodityId
+	, intItemId
+	, strOriginDest
+	, intFutureMarketId
+	, intFutureMonthId
+	, strPeriodTo
+	, intCompanyLocationId
+	, intMarketZoneId
+	, intCurrencyId
+	, intPricingTypeId
+	, strContractInventory
+	, intContractTypeId
+	, dblCashOrFuture
+	, dblBasisOrDiscount
+	, dblRatio
+	, intUnitMeasureId
+FROM @tempBasis
+WHERE intRowNumber NOT IN (SELECT * FROM dbo.[fnCommaSeparatedValueToTable](@intRowNumbers))
