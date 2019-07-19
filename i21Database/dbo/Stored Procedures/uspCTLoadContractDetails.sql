a0b6a99562d525b0e4a5b7f6a07ce7171ccc87e3
Dan Jalimeo <dan.jalimao@irely.com>
2019-07-19 16:51:53 +0800

    CT-3041

--- i21Database/dbo/Stored Procedures/uspCTLoadContractDetails.sql
+++ i21Database/dbo/Stored Procedures/uspCTLoadContractDetails.sql
@@ -305,7 +305,7 @@ CROSS	APPLY	dbo.fnCTGetAdditionalColumnForDetailView(CD.intContractDetailId)	AD
 		OUTER	APPLY	dbo.fnCTGetShipmentStatus(CD.intContractDetailId) LD
 		OUTER	APPLY	dbo.fnCTGetFinancialStatus(CD.intContractDetailId) FS
 		LEFT	JOIN	tblAPBillDetail					BD	ON	BD.intContractDetailId				=		CD.intContractDetailId
-		LEFT	JOIN	tblLGAllocationDetail			AD	ON AD.intPContractDetailId = CD.intContractDetailId
+		LEFT	JOIN	tblLGAllocationDetail			AD	ON AD.intSContractDetailId = CD.intContractDetailId
 	)t ORDER BY intContractSeq
 
 END TRY
