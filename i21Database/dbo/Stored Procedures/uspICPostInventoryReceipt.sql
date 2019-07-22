412cd10601eb8c89d39d44e5d23598fb9bc526a9
Feb Montefrio <feb.montefrio@irely.com>
2019-07-22 13:51:33 +0800

    [B] IC-7545

--- i21Database/dbo/Stored Procedures/uspICPostInventoryReceipt.sql
+++ i21Database/dbo/Stored Procedures/uspICPostInventoryReceipt.sql
@@ -1593,59 +1593,6 @@ BEGIN
 							,@ACCOUNT_CATEGORY_TO_COUNTER_INVENTORY
 							,@intEntityUserSecurityId
 					IF @intReturnValue < 0 GOTO With_Rollback_Exit
-					
-					-- Create GL Entries for Non-Stock Items
-					INSERT INTO @GLEntries (
-							[dtmDate] 
-							,[strBatchId]
-							,[intAccountId]
-							,[dblDebit]
-							,[dblCredit]
-							,[dblDebitUnit]
-							,[dblCreditUnit]
-							,[strDescription]
-							,[strCode]
-							,[strReference]
-							,[intCurrencyId]
-							,[dblExchangeRate]
-							,[dtmDateEntered]
-							,[dtmTransactionDate]
-							,[strJournalLineDescription]
-							,[intJournalLineNo]
-							,[ysnIsUnposted]
-							,[intUserId]
-							,[intEntityId]
-							,[strTransactionId]
-							,[intTransactionId]
-							,[strTransactionType]
-							,[strTransactionForm]
-							,[strModuleName]
-							,[intConcurrencyId]
-							,[dblDebitForeign]	
-							,[dblDebitReport]	
-							,[dblCreditForeign]	
-							,[dblCreditReport]	
-							,[dblReportingRate]	
-							,[dblForeignRate]
-							,[strRateType]
-					)
-					EXEC	@intReturnValue = uspICCreateReceiptGLEntriesForNonStockItems
-							@strBatchId 
-							,@ACCOUNT_CATEGORY_TO_COUNTER_INVENTORY
-							,@intEntityUserSecurityId				
-					IF @intReturnValue < 0 GOTO With_Rollback_Exit
-
-					--BEGIN 
-					--	-- Create an auto-variance for inventory returns posted in IR. 
-					--	EXEC @intReturnValue = [uspICCreateReceiptGLEntriesForReturnVariance]
-					--		@intTransactionId 
-					--		,@strTransactionId
-					--		,@strBatchId
-					--		,@intEntityUserSecurityId
-
-					--	IF @intReturnValue < 0 GOTO With_Rollback_Exit
-					--END
-
 				END 			
 			END
 		END
