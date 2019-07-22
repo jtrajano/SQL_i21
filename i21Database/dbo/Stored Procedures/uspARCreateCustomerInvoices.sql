71b26c3e6070d2f6b856f9aca13b000b71ec1933
Kelvin Quinto <kelvin.quinto@irely.com>
2019-07-22 16:23:53 +0800

    AR-10617, AR-10594, AR-10551

--- i21Database/dbo/Stored Procedures/uspARCreateCustomerInvoices.sql
+++ i21Database/dbo/Stored Procedures/uspARCreateCustomerInvoices.sql
@@ -639,7 +639,7 @@ SELECT
 FROM
 	@InvoicesToGenerate ITG --WITH (NOLOCK)
 WHERE
-	ITG.[strType] NOT IN ('Meter Billing', 'Standard', 'Software', 'Tank Delivery', 'Provisional', 'Service Charge', 'Transport Delivery', 'Store', 'Card Fueling', 'CF Tran', 'CF Invoice', 'Store Checkout')
+	ITG.[strType] NOT IN ('Meter Billing', 'Standard', 'POS', 'Software', 'Tank Delivery', 'Provisional', 'Service Charge', 'Transport Delivery', 'Store', 'Card Fueling', 'CF Tran', 'CF Invoice', 'Store Checkout')
 
 INSERT INTO #ARInvalidInvoiceRecords
     ([intId]
