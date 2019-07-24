9b1288888c1f5a9b296ab4474a330cc13df3d47d
COMTECH\BPonce <bonn.ponce@irely.com>
2019-07-24 15:39:33 +0800

    ST-1392

--- i21Database/dbo/Tables/tblSTCompanyPreference.sql
+++ i21Database/dbo/Tables/tblSTCompanyPreference.sql
@@ -3,6 +3,7 @@
     [strDailySecurityCode]			NVARCHAR (MAX) NULL,
     [dtmDateEntered]				DATETIME	   NULL,
     [ysnEnableLotteryManagement]	BIT			   NULL,
+	[ysnUnlockedLotteryModule]		BIT			   NULL,
     [intConcurrencyId] INT          CONSTRAINT [DF_tblSTCompanyPreference_intConcurrencyId] DEFAULT ((1)) NULL,
     CONSTRAINT [PK_tblSTCompanyPreference] PRIMARY KEY CLUSTERED ([intCompanyPreferenceId] ASC)
 );
