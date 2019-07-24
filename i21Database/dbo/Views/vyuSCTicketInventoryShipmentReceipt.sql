c79a3cf607c2b854cd23e9dc315c4cfcbc667312
jayson_ramos <jayson.ramos@irely.com>
2019-07-24 11:59:35 +0800

    CT-3455

--- i21Database/dbo/Views/vyuSCTicketInventoryShipmentReceipt.sql
+++ i21Database/dbo/Views/vyuSCTicketInventoryShipmentReceipt.sql
@@ -1,4 +1,43 @@
-﻿CREATE VIEW [dbo].[vyuSCTicketInventoryShipmentReceipt]
+﻿CREATE VIEW [dbo].[vyuSCTicketInventoryShipmentReceipt]  
+AS   
+SELECT distinct * FROM   
+(  
+ SELECT a.intTicketId  
+ ,a.intContractId  
+ ,a.intContractSequence  
+ ,a.strTicketNumber  
+ ,a.intInventoryShipmentId AS intInventoryShipmentReceiptId  
+ ,a.strShipmentNumber AS strShipmentReceiptNumber  
+ ,a.dblNetUnits
+ ,a.strUnitMeasure  
+ ,a.strContractNumber  
+ ,a.dtmTicketDateTime  
+ ,b.dblScheduleQty
+ FROM vyuSCTicketInventoryShipmentView a, tblSCTicket b
+ where b.intTicketId = a.intTicketId
+ and b.intContractId = a.intContractId
+   
+ UNION ALL  
+   
+ SELECT a.intTicketId  
+ ,a.intContractId  
+ ,a.intContractSequence  
+ ,a.strTicketNumber  
+ ,a.intInventoryReceiptId AS intInventoryShipmentReceiptId  
+ ,a.strReceiptNumber AS strShipmentReceiptNumber  
+ ,a.dblNetUnits
+ ,a.strUnitMeasure  
+ ,a.strContractNumber  
+ ,a.dtmTicketDateTime  
+ ,b.dblScheduleQty
+ FROM vyuSCTicketInventoryReceiptView a, tblSCTicket b
+ where b.intTicketId = a.intTicketId
+ and b.intContractId = a.intContractId
+  
+) tbl
+
+/*
+CREATE VIEW [dbo].[vyuSCTicketInventoryShipmentReceipt]
 AS 
 SELECT * FROM 
 (
@@ -29,3 +68,4 @@ SELECT * FROM
 	FROM vyuSCTicketInventoryReceiptView
 
 ) tbl
+*/
