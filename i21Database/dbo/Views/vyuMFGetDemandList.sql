6109b02bb37649a59853b52c5fb50df13584dac1
Vinoth Kumar T <vinoth.t@irely.com>
2019-07-26 20:51:55 +0530

    MFG-3938

--- i21Database/dbo/Views/vyuMFGetDemandList.sql
+++ i21Database/dbo/Views/vyuMFGetDemandList.sql
@@ -6,6 +6,7 @@ SELECT DH.intDemandHeaderId
 	,DH.dtmDate
 	,B.strBook
 	,SB.strSubBook
+	,DH.ysnImported
 FROM tblMFDemandHeader DH
 LEFT JOIN tblCTBook B ON B.intBookId = DH.intBookId
 LEFT JOIN tblCTSubBook SB ON SB.intSubBookId = DH.intSubBookId
