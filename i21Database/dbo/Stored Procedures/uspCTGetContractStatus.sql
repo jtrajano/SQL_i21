62d99988f303d3bfd8e39aa7ab6677e7249e0094
jayson_ramos <jayson.ramos@irely.com>
2019-07-29 16:42:57 +0800

    CT-2968

--- i21Database/dbo/Stored Procedures/uspCTGetContractStatus.sql
+++ i21Database/dbo/Stored Procedures/uspCTGetContractStatus.sql
@@ -9,7 +9,8 @@ BEGIN TRY
 			@dblAppliedQty			NUMERIC(18,6),
 			@intContractStatusId	INT,
 			@dblScheduleQty			NUMERIC(18,6),
-			@dblBalance				NUMERIC(18,6)
+			@dblBalance				NUMERIC(18,6),
+			@intShortCloseId		INT
 
 	SELECT	@dblQuantity			=	ISNULL(dblQuantity,0),
 			@dblAppliedQty			=	ISNULL(dblAppliedQty,0),
@@ -19,24 +20,30 @@ BEGIN TRY
 	FROM	vyuCTGridContractDetail
 	WHERE	intContractDetailId		=	@intContractDetailId
 
+	set @intShortCloseId = 0;
+	if (@dblBalance > 0 and @dblBalance < @dblQuantity)
+	begin
+		set @intShortCloseId = (select intContractStatusId from tblCTContractStatus where strContractStatus = 'Short Close');
+	end
+
 	IF	@intContractStatusId	=	1 --Open
 	BEGIN
 		DECLARE @shortCloseId INT
 		SELECT @shortCloseId = CASE WHEN dbo.fnCTGetSequenceReceiptReturnTotal(@intContractDetailId) = @dblQuantity THEN 6 ELSE 0 END
 
 		IF @dblAppliedQty > 0 
-			SELECT * FROM tblCTContractStatus WHERE intContractStatusId IN (@intContractStatusId,6)
+			SELECT * FROM tblCTContractStatus WHERE intContractStatusId IN (@intContractStatusId,6,@intShortCloseId)
 		ELSE IF @dblScheduleQty > 0
-			SELECT * FROM tblCTContractStatus WHERE intContractStatusId IN (@intContractStatusId,@shortCloseId,3)
+			SELECT * FROM tblCTContractStatus WHERE intContractStatusId IN (@intContractStatusId,@shortCloseId,3,@intShortCloseId)
 		ELSE
-			SELECT * FROM tblCTContractStatus WHERE intContractStatusId IN (@intContractStatusId,@shortCloseId,2,3)
+			SELECT * FROM tblCTContractStatus WHERE intContractStatusId IN (@intContractStatusId,@shortCloseId,2,3,@intShortCloseId)
 	END
 	ELSE IF	@intContractStatusId	=	2 --Unconfirmed
 	BEGIN
 		IF	@dblAppliedQty > 0 
-			SELECT * FROM tblCTContractStatus WHERE intContractStatusId IN (@intContractStatusId,1,6)
+			SELECT * FROM tblCTContractStatus WHERE intContractStatusId IN (@intContractStatusId,1,6,@intShortCloseId)
 		ELSE
-			SELECT * FROM tblCTContractStatus WHERE intContractStatusId IN (@intContractStatusId,1,3)
+			SELECT * FROM tblCTContractStatus WHERE intContractStatusId IN (@intContractStatusId,1,3,@intShortCloseId)
 	END
 	ELSE IF	@intContractStatusId	=	3 --Cancelled
 	BEGIN
@@ -45,11 +52,11 @@ BEGIN TRY
 	ELSE IF	@intContractStatusId	=	4 --Re-Open
 	BEGIN
 		IF	@dblBalance = 0 
-			SELECT * FROM tblCTContractStatus WHERE intContractStatusId IN (@intContractStatusId,5)
+			SELECT * FROM tblCTContractStatus WHERE intContractStatusId IN (@intContractStatusId,5,@intShortCloseId)
 		ELSE IF @dblAppliedQty > 0
-			SELECT * FROM tblCTContractStatus WHERE intContractStatusId IN (@intContractStatusId,6)
+			SELECT * FROM tblCTContractStatus WHERE intContractStatusId IN (@intContractStatusId,6,@intShortCloseId)
 		ELSE
-			SELECT * FROM tblCTContractStatus WHERE intContractStatusId IN (@intContractStatusId,3)
+			SELECT * FROM tblCTContractStatus WHERE intContractStatusId IN (@intContractStatusId,3,@intShortCloseId)
 	END
 	ELSE IF	@intContractStatusId	=	5 --Complete
 	BEGIN
