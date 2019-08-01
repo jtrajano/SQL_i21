51b5a35c796b4d8aff592498c03343e94bd7d9e0
Premkumar S <prem.selladurai@irely.com>
2019-08-01 19:06:14 +0530

    IN-392

---
+++ i21Database/dbo/Tables/tblIPContractFeedHeader.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE tblIPContractFeedHeader (
+	intContractFeedHeaderId INT identity(1, 1)
+	,intContractHeaderId INT
+	,strFeedStatus NVARCHAR(50) Collate Latin1_General_CI_AS
+	,dtmFeedDate DATETIME CONSTRAINT DF_tblIPContractFeedDetail_dtmFeedDate DEFAULT GETDATE()
+	,strMessage NVARCHAR(MAX) Collate Latin1_General_CI_AS
+	,strApproverXML NVARCHAR(MAX) Collate Latin1_General_CI_AS
+	,intContractFeedHeaderRefId INT CONSTRAINT [PK_tblIPContractFeedHeader_intContractFeedHeaderId] PRIMARY KEY (intContractFeedHeaderId)
+	)
