6814626f0f4c575312eab063d1dbb705cfbb6246
Kelvin Quinto <kelvin.quinto@irely.com>
2019-08-02 10:52:59 +0800

    AR-10717

--- i21Database/dbo/Stored Procedures/uspEMEACustomerExport.sql
+++ i21Database/dbo/Stored Procedures/uspEMEACustomerExport.sql
@@ -1,24 +1,59 @@
 ﻿CREATE PROCEDURE [dbo].[uspEMEACustomerExport]
-	@dtmFrom	date,
-	@dtmTo		date
+	@dtmFrom	DATE,
+	@dtmTo		DATE
 AS
-	truncate table tblAREACustomerExport
 
-	insert into tblAREACustomerExport
+TRUNCATE TABLE tblAREACustomerExport
 
-	(intEntityId, strEntityNo, strName, ysnGroupRequired, ysnLocationRequired, ysnCreditHold, ysnTaxable, ysnVFDDealer, ysnVFDAcknowledged, intOrganicType, 
-	strLastName, strFirstName, strAddress1, strAddress2, strCity, strStateProv, strPostalCode, strPhone, strMobile, strFax, strEmail, strWebsite, strComment)
-
-	select	intId, Id, [Description], GroupRequired, LocationRequired, CreditHold, Taxable, VFDDealer, VFDAcknowledged, OrganicType,
-		LastName, FirstName, Address1, Address2, City, StateProv, PostalCode, Phone, Mobile, Fax, Email, Website, Comment
-	from vyuEMEAExportCustomer 
-		where (cast(ModifiedDate as date) between @dtmFrom and @dtmTo) OR (@dtmFrom IS NULL OR @dtmTo IS NULL)
-
-	--select 
-	--	intEntityId, strEntityNo, strName, ysnGroupRequired, ysnLocationRequired, ysnCreditHold, ysnTaxable, ysnVFDDealer, ysnVFDAcknowledged, intOrganicType, 
-	--	strLastName, strFirstName, strAddress1, strAddress2, strCity, strStateProv, strPostalCode, strPhone, strMobile, strFax, strEmail, strWebsite
-	--from tblAREACustomerExport
-
-
+INSERT INTO tblAREACustomerExport (
+	  intEntityId
+	, strEntityNo
+	, strName
+	, ysnGroupRequired
+	, ysnLocationRequired
+	, ysnCreditHold
+	, ysnTaxable
+	, ysnVFDDealer
+	, ysnVFDAcknowledged
+	, intOrganicType
+	, strLastName
+	, strFirstName
+	, strAddress1
+	, strAddress2
+	, strCity
+	, strStateProv
+	, strPostalCode
+	, strPhone
+	, strMobile
+	, strFax
+	, strEmail
+	, strWebsite
+	, strComment
+)
+SELECT intEntityId			= intId
+	, strEntityNo			= LTRIM(RTRIM(Id))
+	, strName				= LTRIM(RTRIM([Description]))
+	, ysnGroupRequired		= GroupRequired
+	, ysnLocationRequired	= LocationRequired
+	, ysnCreditHold			= CreditHold
+	, ysnTaxable			= Taxable
+	, ysnVFDDealer			= VFDDealer
+	, ysnVFDAcknowledged	= VFDAcknowledged
+	, intOrganicType		= OrganicType
+	, strLastName			= LTRIM(RTRIM(LastName))
+	, strFirstName			= LTRIM(RTRIM(FirstName))
+	, strAddress1			= LTRIM(RTRIM(Address1))
+	, strAddress2			= LTRIM(RTRIM(Address2))
+	, strCity				= LTRIM(RTRIM(City))
+	, strStateProv			= LTRIM(RTRIM(StateProv))
+	, strPostalCode			= LTRIM(RTRIM(PostalCode))
+	, strPhone				= LTRIM(RTRIM(Phone))
+	, strMobile				= LTRIM(RTRIM(Mobile))
+	, strFax				= LTRIM(RTRIM(Fax))
+	, strEmail				= LTRIM(RTRIM(Email))
+	, strWebsite			= LTRIM(RTRIM(Website))
+	, strComment			= LTRIM(RTRIM(Comment))
+FROM vyuEMEAExportCustomer 
+WHERE (CAST(ModifiedDate AS DATE) BETWEEN @dtmFrom AND @dtmTo) OR (@dtmFrom IS NULL OR @dtmTo IS NULL)
 
 RETURN 0
--- i21Database/dbo/Views/vyuEMEAExportCustomer.sql
+++ i21Database/dbo/Views/vyuEMEAExportCustomer.sql
@@ -1,65 +1,55 @@
 ﻿CREATE VIEW [dbo].[vyuEMEAExportCustomer]
-	AS 
-	
-	
-	select 
-		intId 				= a.intEntityId,
-		[Id]				= a.strEntityNo COLLATE Latin1_General_CI_AS,
-		[Description]		= a.strName COLLATE Latin1_General_CI_AS,
-		[GroupRequired]		= cast(0 as bit),
-		[LocationRequired]	= cast(1 as bit),
-		[CreditHold]		= cast(c.ysnCreditHold as bit),
-		[Taxable]			= cast(c.ysnApplySalesTax as bit),
-		[VFDDealer]			= cast(0 as bit),
-		[VFDAcknowledged]	= cast(0 as bit),
-		[OrganicType]		= cast(0 as int),	
-	
-		LastName			= ISNULL ( SUBSTRING((CASE WHEN CHARINDEX(' ', e.strName) > 0 THEN SUBSTRING(SUBSTRING(e.strName,1,30),CHARINDEX(' ',e.strName) + 1, LEN(e.strName))END), 1, 20) , '') COLLATE Latin1_General_CI_AS,	
-		FirstName			= ISNULL ( SUBSTRING((CASE WHEN CHARINDEX(' ', e.strName) > 0 THEN SUBSTRING(SUBSTRING(e.strName,1,30), 0, CHARINDEX(' ',e.strName)) ELSE SUBSTRING(e.strName,1,30)END), 1, 20) , '') COLLATE Latin1_General_CI_AS,
-		[Name]				= ISNULL ( e.strName, '') COLLATE Latin1_General_CI_AS,
-	
-		Address1			= ISNULL ( dbo.fnEMSplitWithGetByIdx(f.strAddress,char(10),1) , '') COLLATE Latin1_General_CI_AS,
-		Address2			= ISNULL ( dbo.fnEMSplitWithGetByIdx(f.strAddress,char(10),2) , '') COLLATE Latin1_General_CI_AS,
-		City				= ISNULL ( f.strCity, '') COLLATE Latin1_General_CI_AS,
-		StateProv			= ISNULL ( f.strState, '') COLLATE Latin1_General_CI_AS,
-		PostalCode			= ISNULL ( f.strZipCode, '') COLLATE Latin1_General_CI_AS,
-		[Phone]				= ISNULL ( g.strPhone, '') COLLATE Latin1_General_CI_AS,
-		[Mobile]			= ISNULL ( h.strPhone, '') COLLATE Latin1_General_CI_AS,
-	
-		Fax					= ISNULL ( j.strFax, '') COLLATE Latin1_General_CI_AS,
-		Email				= ISNULL ( e.strEmail, '') COLLATE Latin1_General_CI_AS,
-		Website				= ISNULL ( i.strWebsite, '') COLLATE Latin1_General_CI_AS,
-		ModifiedDate		= coalesce(c.dtmDateModified,c.dtmDateCreated ),
-		Comment				= ''
-
-	from tblEMEntity a
-	join tblEMEntityType b
-		on a.intEntityId = b.intEntityId and b.strType = 'Customer'
-	join tblARCustomer c 
-		on a.intEntityId = c.intEntityId
-	join tblEMEntityToContact d
-		on a.intEntityId = d.intEntityId and d.ysnDefaultContact = 1
-	join tblEMEntity e
-		on d.intEntityContactId = e.intEntityId
-	join tblEMEntityLocation f
-		on a.intEntityId = f.intEntityId and f.ysnDefaultLocation = 1
-	outer apply ( 
-			select top 1 strPhone = isnull(strPhone, '') from tblEMEntityPhoneNumber where intEntityId = d.intEntityContactId
-		) g
-	outer apply ( 
-			select top 1 strPhone = isnull(strPhone, '') from tblEMEntityMobileNumber where intEntityId = d.intEntityContactId
-		) h
-	outer apply ( 
-			select top 1 isnull(aa.strValue, '') strWebsite from tblEMContactDetail aa
-				join tblEMContactDetailType bb
-					on aa.intContactDetailTypeId = bb.intContactDetailTypeId and strField = 'Website'
-				where aa.intEntityId = d.intEntityContactId
-	) i
-	
-	outer apply ( 
-			select top 1 isnull(aa.strValue, '') strFax from tblEMContactDetail aa
-				join tblEMContactDetailType bb
-					on aa.intContactDetailTypeId = bb.intContactDetailTypeId and strField = 'Fax'
-				where aa.intEntityId = d.intEntityContactId
-	) j
-	
+AS 
+SELECT [intId] 				= a.intEntityId
+	 , [Id]					= LTRIM(RTRIM(a.strEntityNo)) COLLATE Latin1_General_CI_AS
+	 , [Description]		= LTRIM(RTRIM(a.strName)) COLLATE Latin1_General_CI_AS
+	 , [GroupRequired]		= CAST(0 AS BIT)
+	 , [LocationRequired]	= CAST(1 AS BIT)
+	 , [CreditHold]			= CAST(c.ysnCreditHold AS BIT)
+	 , [Taxable]			= CAST(c.ysnApplySalesTax AS BIT)
+	 , [VFDDealer]			= CAST(0 AS BIT)
+	 , [VFDAcknowledged]	= CAST(0 AS BIT)
+	 , [OrganicType]		= CAST(0 AS INT)
+	 , [LastName]			= ISNULL(SUBSTRING((CASE WHEN CHARINDEX(' ', e.strName) > 0 THEN SUBSTRING(SUBSTRING(e.strName,1,30),CHARINDEX(' ',e.strName) + 1, LEN(e.strName))END), 1, 20) , '') COLLATE Latin1_General_CI_AS
+	 , [FirstName]			= ISNULL(SUBSTRING((CASE WHEN CHARINDEX(' ', e.strName) > 0 THEN SUBSTRING(SUBSTRING(e.strName,1,30), 0, CHARINDEX(' ',e.strName)) ELSE SUBSTRING(e.strName,1,30)END), 1, 20) , '') COLLATE Latin1_General_CI_AS
+	 , [Name]				= ISNULL(e.strName, '') COLLATE Latin1_General_CI_AS
+	 , [Address1]			= ISNULL(dbo.fnEMSplitWithGetByIdx(f.strAddress,char(10),1) , '') COLLATE Latin1_General_CI_AS
+	 , [Address2]			= ISNULL(dbo.fnEMSplitWithGetByIdx(f.strAddress,char(10),2) , '') COLLATE Latin1_General_CI_AS
+	 , [City]				= ISNULL(f.strCity, '') COLLATE Latin1_General_CI_AS
+	 , [StateProv]			= ISNULL(f.strState, '') COLLATE Latin1_General_CI_AS
+	 , [PostalCode]			= ISNULL(f.strZipCode, '') COLLATE Latin1_General_CI_AS
+	 , [Phone]				= ISNULL(PHONE.strPhone, '') COLLATE Latin1_General_CI_AS
+	 , [Mobile]				= ISNULL(MOBILE.strPhone, '') COLLATE Latin1_General_CI_AS
+	 , [Fax]				= ISNULL(FAX.strFax, '') COLLATE Latin1_General_CI_AS
+	 , [Email]				= ISNULL(e.strEmail, '') COLLATE Latin1_General_CI_AS
+	 , [Website]			= ISNULL(WEBSITE.strWebsite, '') COLLATE Latin1_General_CI_AS
+	 , [ModifiedDate]		= COALESCE(c.dtmDateModified, c.dtmDateCreated)
+	 , [Comment]			= ''
+FROM tblEMEntity a
+INNER JOIN tblEMEntityType b ON a.intEntityId = b.intEntityId and b.strType = 'Customer'
+INNER JOIN tblARCustomer c ON a.intEntityId = c.intEntityId
+INNER JOIN tblEMEntityToContact d ON a.intEntityId = d.intEntityId and d.ysnDefaultContact = 1
+INNER JOIN tblEMEntity e ON d.intEntityContactId = e.intEntityId
+INNER JOIN tblEMEntityLocation f ON a.intEntityId = f.intEntityId and f.ysnDefaultLocation = 1
+OUTER APPLY ( 
+	SELECT TOP 1 strPhone = ISNULL(strPhone, '') 
+	FROM tblEMEntityPhoneNumber 
+	WHERE intEntityId = d.intEntityContactId
+) PHONE
+OUTER APPLY ( 
+	SELECT TOP 1 strPhone = ISNULL(strPhone, '') 
+	FROM tblEMEntityMobileNumber 
+	WHERE intEntityId = d.intEntityContactId
+) MOBILE
+OUTER APPLY ( 
+	SELECT TOP 1 strWebsite = ISNULL(aa.strValue, '') 
+	FROM tblEMContactDetail aa
+	INNER JOIN tblEMContactDetailType bb ON aa.intContactDetailTypeId = bb.intContactDetailTypeId AND strField = 'Website'
+	WHERE aa.intEntityId = d.intEntityContactId
+) WEBSITE
+OUTER APPLY ( 
+	SELECT TOP 1 strFax = ISNULL(aa.strValue, '') 
+	FROM tblEMContactDetail aa
+	INNER JOIN tblEMContactDetailType bb ON aa.intContactDetailTypeId = bb.intContactDetailTypeId AND strField = 'Fax'
+	WHERE aa.intEntityId = d.intEntityContactId
+) FAX
