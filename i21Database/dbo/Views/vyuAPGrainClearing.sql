1742fc3a24b60bc7b798e80c893f0d646752dd4b
Erick <erick.delacruz@irely.com>
2019-08-06 17:10:25 +0800

    AP-7614

--- i21Database/dbo/Views/vyuAPGrainClearing.sql
+++ i21Database/dbo/Views/vyuAPGrainClearing.sql
@@ -73,7 +73,7 @@ INNER JOIN vyuGLDetail GD
 		AND GD.intTransactionId = SS.intSettleStorageId
 		AND GD.strTransactionType = 'Storage Settlement'
 		AND GD.ysnIsUnposted = 0
-		AND GD.strCode = 'IC' --get only the AP Clearing for item
+		AND GD.strCode = 'STR' --get only the AP Clearing for item
 INNER JOIN vyuGLAccountDetail AD
 	ON GD.intAccountId = AD.intAccountId AND AD.intAccountCategoryId = 45
 LEFT JOIN 
@@ -95,7 +95,7 @@ SELECT
 	,billDetail.intItemId
 	,CS.intItemUOMId  AS intItemUOMId
     ,unitMeasure.strUnitMeasure AS strUOM 
-	,billDetail.dblTotal + billDetail.dblTax AS dblVoucherTotal
+	,billDetail.dblTotal AS dblVoucherTotal
     ,CASE 
 		WHEN billDetail.intWeightUOMId IS NULL THEN 
 			ISNULL(billDetail.dblQtyReceived, 0) 
@@ -198,7 +198,7 @@ SELECT
 	,billDetail.intItemId
 	,CS.intItemUOMId  AS intItemUOMId
     ,unitMeasure.strUnitMeasure AS strUOM 
-	,billDetail.dblTotal + billDetail.dblTax AS dblVoucherTotal
+	,billDetail.dblTotal AS dblVoucherTotal
     ,CASE 
 		WHEN billDetail.intWeightUOMId IS NULL THEN 
 			ISNULL(billDetail.dblQtyReceived, 0) 
@@ -333,7 +333,7 @@ SELECT
 	,billDetail.intItemId
 	,CS.intItemUOMId  AS intItemUOMId
     ,unitMeasure.strUnitMeasure AS strUOM 
-	,billDetail.dblTotal + billDetail.dblTax AS dblVoucherTotal
+	,billDetail.dblTotal AS dblVoucherTotal
     ,CASE 
 		WHEN billDetail.intWeightUOMId IS NULL THEN 
 			ISNULL(billDetail.dblQtyReceived, 0) 
