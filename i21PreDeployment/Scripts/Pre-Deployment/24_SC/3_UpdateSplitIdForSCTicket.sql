71db0fb36596d17ba094538d33887293e40adada
Mon Gonzales <mon.gonzales@irely.com>
2019-08-09 10:51:04 +0800

    SC-3010 - 19.2 Script ERROR

--- i21PreDeployment/Scripts/Pre-Deployment/24_SC/3_UpdateSplitIdForSCTicket.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/24_SC/3_UpdateSplitIdForSCTicket.sql
@@ -2,7 +2,7 @@ PRINT N'***** BEGIN UPDATE Update split that does not exist in the entity split 
 GO
 
 
-IF OBJECT_ID('FK_tblSCTicket_tblEMEntitySplit_intSplitId') IS NULL
+--IF OBJECT_ID('FK_tblSCTicket_tblEMEntitySplit_intSplitId') IS NULL
 	BEGIN
 		IF EXISTS(SELECT TOP 1 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE [TABLE_NAME] = 'tblEMEntitySplit' and [COLUMN_NAME] = 'intSplitId')
 		 AND EXISTS(SELECT TOP 1 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE [TABLE_NAME] = 'tblSCTicket' and [COLUMN_NAME] = 'intSplitId')
