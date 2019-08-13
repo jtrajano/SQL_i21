dc010f0d0db8a1f2981f90dc7dbb4d47688da512
Wendell Wayne Estrada <wendell.estrada@irely.com>
2019-08-13 13:03:46 +0800

    [P] IC-7648 Easy Automation - Item Export UOM

--- i21Database/dbo/Tables/tblICStagingItemLocation.sql
+++ i21Database/dbo/Tables/tblICStagingItemLocation.sql
@@ -11,6 +11,9 @@ CREATE TABLE [dbo].[tblICStagingItemLocation] (
 	, ysnRequireStorageUnit BIT NULL
 	, dblReorderPoint NUMERIC(18, 6) NULL
 	, dblLeadTime NUMERIC(18, 6) NULL
+	, intDefaultGrossUomId INT NULL
+	, intDefaultPurchaseUomId INT NULL
+	, intDefaultSaleUomId INT NULL
 	, intReorderPoint AS CAST(ROUND(dblReorderPoint, 2) AS INT)
 	, intLeadTime AS CAST(ROUND(dblLeadTime, 2) AS INT)
 	, strDefaultVendorNo NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL
