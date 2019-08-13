b32f3102a68089f2acfb6897c29cdc6a242d4922
Erick <erick.delacruz@irely.com>
2019-08-13 13:08:27 +0800

    AP-7643

--- i21Database/dbo/Functions/fnAPValidateVoucherPayable.sql
+++ i21Database/dbo/Functions/fnAPValidateVoucherPayable.sql
@@ -16,15 +16,15 @@ BEGIN
 
 	--DO NOT ALLOW THE intItemId TO BE NULL IF UOM PROVIDED
 	SELECT TOP 1
-		'Payable id(' + CAST(A.intVoucherPayableId AS NVARCHAR) + ') did not provide an item id.'
-		,A.intVoucherPayableId
+		A.intVoucherPayableId
+		,'Payable id(' + CAST(A.intVoucherPayableId AS NVARCHAR) + ') did not provide an item id.'
 	FROM @voucherPayables A
 	WHERE NULLIF(A.intItemId,0) IS NULL AND (A.intQtyToBillUOMId > 0 OR A.intOrderUOMId > 0 OR A.intWeightUOMId > 0)
 	UNION ALL
 	--IF dblTax IS NOT 0, MAKE SURE THAT THEY PROVIDED VoucherDetailTax
 	SELECT TOP 1
-		'Payable id(' + CAST(A.intVoucherPayableId AS NVARCHAR) + ') did not provide tax data.'
-		,A.intVoucherPayableId
+		A.intVoucherPayableId
+		,'Payable id(' + CAST(A.intVoucherPayableId AS NVARCHAR) + ') did not provide tax data.'
 	FROM @voucherPayables A
 	WHERE A.dblTax != 0 AND NOT EXISTS (
 		SELECT 1 FROM @voucherPayableTax taxes WHERE taxes.intVoucherPayableId = A.intVoucherPayableId
@@ -32,8 +32,8 @@ BEGIN
 	UNION ALL
 	--IF intPartitionId was provided, make sure all payables have values
 	SELECT TOP 1
-		'Not all payables have intPartitionId provided.'
-		,A.intVoucherPayableId
+		A.intVoucherPayableId
+		,'Not all payables have intPartitionId provided.'
 	FROM @voucherPayables A
 	WHERE A.intPartitionId > 0 AND EXISTS (
 		SELECT TOP 1 1 FROM @voucherPayables WHERE NULLIF(A.intPartitionId,0) IS NULL
@@ -42,8 +42,8 @@ BEGIN
 	--IF intPartitionId was provided, make sure we can group to these fields and it matched with the partition
 	--intEntityVendorId,intTransactionType,intLocationId,intShipToId,intShipFromId,intShipFromEntityId,intPayToAddressId,intCurrencyId,strVendorOrderNumber
 	SELECT TOP 1
-		'Payables partition provided is invalid. Please check the values.'
-		,A.intVoucherPayableId
+		A.intVoucherPayableId
+		,'Payables partition provided is invalid. Please check the values.'
 	FROM @voucherPayables A
 	WHERE A.intPartitionId > 0
 	AND EXISTS (
