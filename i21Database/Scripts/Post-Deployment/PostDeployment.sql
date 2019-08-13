2034086a2b8f731d0fabd91d8f03102fad5dbeee
Randy Sagum <randy.sagum@irely.com>
2019-08-13 14:14:12 +0800

    PAT-1042

--- i21Database/Scripts/Post-Deployment/PostDeployment.sql
+++ i21Database/Scripts/Post-Deployment/PostDeployment.sql
@@ -345,6 +345,7 @@ print 'BEGIN POST DEPLOYMENT'
 :r .\PAT\3_UpdateIssueStockNo.sql
 :r .\PAT\4_UpdateRetiredStockNo.sql
 :r .\PAT\5_UpdatePayoutType.sql
+:r .\PAT\6_UpdateVolumeLogPostDate.sql
 
 --Contract Management
 :R .\CT\1_MasterTables.sql
