c03b9a7c92d07d8ee3e8fb114ed6bb371e496870
lawrence_zabala <lawrence.zabala@irely.com>
2019-08-13 17:49:18 +0800

    RM-3101

--- i21Database/dbo/Stored Procedures/uspRKGetPositionAnalysisReportDailyAverage.sql
+++ i21Database/dbo/Stored Procedures/uspRKGetPositionAnalysisReportDailyAverage.sql
@@ -35,7 +35,7 @@ BEGIN
 			, CD.intCurrencyId
 			, dblOutrightPhysicalDeltaQty = dbo.fnCTConvertQtyToTargetItemUOM(CD.intItemUOMId, QtyUOM.intItemUOMId, CASE WHEN ITM.intProductLineId IS NOT NULL THEN ISNULL(CD.dblQuantity, 0) * (ISNULL(CPL.dblDeltaPercent, 0) /100) ELSE ISNULL(CD.dblQuantity, 0) END)
 			, dblOutrightPhysicalQty = dbo.fnCTConvertQtyToTargetItemUOM(CD.intItemUOMId, QtyUOM.intItemUOMId, ISNULL(CD.dblQuantity, 0))
-			, dblOutrightPhysicalPrice = ISNULL(dbo.[fnRKConvertUOMCurrency]('ItemUOM', CD.intPriceItemUOMId, PriceUOM.intItemUOMId, 1, CD.intCurrencyId, @intCurrencyId, ISNULL(CD.dblFutures, 0)), 0.00)
+			, dblOutrightPhysicalPrice = ISNULL(dbo.[fnRKConvertUOMCurrency]('ItemUOM', CD.intPriceItemUOMId, PriceUOM.intItemUOMId, 1, CD.intCurrencyId, @intCurrencyId, ISNULL(CD.dblFutures, 0), CD.intContractDetailId), 0.00)
 			, dblFutureTradeDeltaQty = 0.00
 			, dblFutureTradeQty = 0.00
 			, dblFutureTradePrice = 0.00
@@ -67,7 +67,7 @@ BEGIN
 			, dblFutureTradePrice = 0.00
 			, dblPriceFixationDeltaQty = dbo.fnCTConvertQtyToTargetItemUOM(CD.intItemUOMId, QtyUOM.intItemUOMId, CASE WHEN ITM.intProductLineId IS NOT NULL THEN ISNULL(PFD.dblQuantity,0) * (ISNULL(CPL.dblDeltaPercent,0) /100) ELSE ISNULL(PFD.dblQuantity,0) END)
 			, dblPriceFixationQty = dbo.fnCTConvertQtyToTargetItemUOM(CD.intItemUOMId, QtyUOM.intItemUOMId, ISNULL(PFD.dblQuantity,0))
-			, dblPriceFixationPrice = ISNULL(dbo.[fnRKConvertUOMCurrency]('ItemUOM', CD.intPriceItemUOMId, PriceUOM.intItemUOMId, 1, CD.intCurrencyId, @intCurrencyId, ISNULL(PFD.dblFixationPrice, 0)), 0.00)
+			, dblPriceFixationPrice = ISNULL(dbo.[fnRKConvertUOMCurrency]('ItemUOM', CD.intPriceItemUOMId, PriceUOM.intItemUOMId, 1, CD.intCurrencyId, @intCurrencyId, ISNULL(PFD.dblFixationPrice, 0), CD.intContractDetailId), 0.00)
 			, strBuySell = CASE WHEN CH.intContractTypeId = 1 THEN 'Buy' ELSE 'Sell' END COLLATE Latin1_General_CI_AS
 		FROM tblCTContractHeader CH
 		INNER JOIN tblCTContractDetail CD ON CH.intContractHeaderId = CD.intContractHeaderId
@@ -92,7 +92,7 @@ BEGIN
 			, dblOutrightPhysicalPrice = 0.00
 			, dblFutureTradeDeltaQty = 0.00
 			, dblFutureTradeQty = dbo.fnCTConvertQtyToTargetCommodityUOM(DD.intCommodityId, FM.intUnitMeasureId, ISNULL(@intQtyUOMId, 0), ISNULL(DD.dblNoOfContract,0) * ISNULL(FM.dblContractSize,0))
-			, dblFutureTradePrice = ISNULL(dbo.[fnRKConvertUOMCurrency]('CommodityUOM', MarketUOM.intCommodityUnitMeasureId, PriceUOM.intCommodityUnitMeasureId, 1, FM.intCurrencyId, @intCurrencyId, ISNULL(DD.dblPrice, 0)), 0.00)
+			, dblFutureTradePrice = ISNULL(dbo.[fnRKConvertUOMCurrency]('CommodityUOM', MarketUOM.intCommodityUnitMeasureId, PriceUOM.intCommodityUnitMeasureId, 1, FM.intCurrencyId, @intCurrencyId, ISNULL(DD.dblPrice, 0), NULL), 0.00)
 			, dblPriceFixationDeltaQty = 0.00
 			, dblPriceFixationQty = 0.00
 			, dblPriceFixationPrice = 0.00
