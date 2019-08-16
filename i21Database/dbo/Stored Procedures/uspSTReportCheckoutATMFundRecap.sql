c044619ab7a21e5e4b5afa5192bccd68cdc9c4c4
henry_zaragoza <henry.zaragoza@irely.com>
2019-08-16 22:01:35 +0800

    ST-1355

---
+++ i21Database/dbo/Stored Procedures/uspSTReportCheckoutATMFundRecap.sql
@@ -0,0 +1,31 @@
+﻿CREATE PROCEDURE [dbo].[uspSTReportCheckoutATMFundRecap]
+	@intCheckoutId INT 
+AS
+BEGIN TRY
+	
+	DECLARE @ErrMsg NVARCHAR(MAX)
+
+	SELECT 
+		atm.intCheckoutId,
+		atm.strType,
+		atm.intItemId,
+		atm.dblItemAmount,
+		item.strItemNo,
+		SUM(atm.dblItemAmount) OVER() AS dblTotalAmount 
+	FROM vyuSTCheckoutATMFund atm
+	INNER JOIN tblICItem item
+		ON atm.intItemId = item.intItemId
+	WHERE intCheckoutId = @intCheckoutId
+	GROUP BY
+		atm.intCheckoutId,
+		atm.strType,
+		atm.intItemId,
+		atm.dblItemAmount,
+		item.strItemNo
+	--ORDER BY intCheckoutId ASC
+
+END TRY
+BEGIN CATCH
+	SET @ErrMsg = ERROR_MESSAGE()  
+	RAISERROR (@ErrMsg,18,1,'WITH NOWAIT')
+END CATCH
