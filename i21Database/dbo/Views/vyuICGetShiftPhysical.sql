4ae61d4b93ba681a77759b4718e37402a2beeb6e
Feb Montefrio <feb.montefrio@irely.com>
2019-08-16 18:40:36 +0800

    [G] IC-7673

---
+++ i21Database/dbo/Views/vyuICGetShiftPhysical.sql
@@ -0,0 +1,32 @@
+﻿CREATE VIEW [dbo].[vyuICGetShiftPhysical]
+AS 
+
+SELECT 
+	s.intInventoryShiftPhysicalCountId
+	,c.strCountGroup
+	,s.dtmDate
+	,dblBegin = s.dblSystemCount
+	,dblQtyReceived = s.dblQtyReceived
+	,dblQtySold = s.dblQtySold
+	,dblEnding = 
+		ISNULL(s.dblSystemCount, 0) 
+		+ ISNULL(s.dblQtyReceived, 0) 
+		- ISNULL(s.dblQtySold, 0)
+	,dblCalculated = s.dblPhysicalCount
+	,dblDifference = 
+		ISNULL(s.dblSystemCount, 0) 
+		+ ISNULL(s.dblQtyReceived, 0) 
+		- ISNULL(s.dblQtySold, 0)
+		- ISNULL(s.dblPhysicalCount, 0)
+	,dblPrice = CAST(0 AS NUMERIC(18, 6))
+	,dblValue =  CAST(0 AS NUMERIC(18, 6))
+	,cnt.strCountBy
+FROM 
+	tblICInventoryShiftPhysicalHistory s INNER JOIN tblICInventoryCount cnt
+		ON s.intTransactionId = cnt.intInventoryCountId AND s.strTransactionId = cnt.strCountNo
+	LEFT JOIN tblICCountGroup c
+		ON s.intCountGroupId = c.intCountGroupId
+WHERE
+	s.ysnIsUnposted = 0 
+	AND s.dblPhysicalCount IS NOT NULL 
+	AND cnt.strCountBy = 'Item Group'
