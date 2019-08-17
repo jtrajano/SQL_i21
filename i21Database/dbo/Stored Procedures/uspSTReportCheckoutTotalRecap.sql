7731a85ee6e594d06d9f0f7e0b1a779ecd465529
henry_zaragoza <henry.zaragoza@irely.com>
2019-08-17 10:17:31 +0800

    ST-1355

--- i21Database/dbo/Stored Procedures/uspSTReportCheckoutTotalRecap.sql
+++ i21Database/dbo/Stored Procedures/uspSTReportCheckoutTotalRecap.sql
@@ -7,43 +7,47 @@ BEGIN TRY
 	
    DECLARE @ErrMsg NVARCHAR(MAX)
 
-   SELECT ISNULL(SUM(A.dblTotalSalesAmountComputed) OVER (),0) AS CategoryTotalSale
-		, ISNULL(C.dblTotalTax ,0) AS TotalTax
-		, ISNULL(D.dblAmount,0) AS TotalPayment
-		, ISNULL(E.dblAmount,0) AS TotalCustomerCharges
-		, ISNULL(F.dblAmount,0) AS TotalCustomerPayments
-		, (ISNULL(SUM (A.dblTotalSalesAmountComputed) OVER(),0) + ISNULL(C.dblTotalTax,0) - ISNULL(D.dblAmount,0) - ISNULL(E.dblAmount,0) + ISNULL(F.dblAmount,0)) AS TotalToDeposit
-		, ISNULL(G.dblTotalDeposit,0) AS TotalDeposits
-		, (ISNULL(G.dblTotalDeposit,0) - (ISNULL(SUM (A.dblTotalSalesAmountComputed) OVER(),0) + ISNULL(C.dblTotalTax,0) - ISNULL(D.dblAmount,0) - ISNULL(E.dblAmount,0) + ISNULL(F.dblAmount,0))) AS CashOverShort
-   FROM tblSTCheckoutDepartmetTotals A  
-   LEFT OUTER JOIN tblICCategory B 
-		ON A.intCategoryId = B.intCategoryId 
+   SELECT DISTINCT
+        ISNULL(SUM(deptTotals.dblTotalSalesAmountComputed) OVER (), 0)	AS CategoryTotalSale
+		, ISNULL(taxTotals.dblTotalTax, 0)								AS TotalTax
+		, ISNULL(paymentOp.dblAmount, 0)								AS TotalPayment
+		, ISNULL(customerChange.dblAmount, 0)							AS TotalCustomerCharges
+		, ISNULL(customerPayments.dblAmount, 0)							AS TotalCustomerPayments
+		, (ISNULL(SUM (deptTotals.dblTotalSalesAmountComputed) OVER(), 0) + ISNULL(taxTotals.dblTotalTax,0) - ISNULL(paymentOp.dblAmount,0) - ISNULL(customerChange.dblAmount,0) + ISNULL(customerPayments.dblAmount,0)) AS TotalToDeposit
+		, ISNULL(deposits.dblTotalDeposit, 0) AS TotalDeposits
+		, (ISNULL(deposits.dblTotalDeposit, 0) - (ISNULL(SUM (deptTotals.dblTotalSalesAmountComputed) OVER(),0) + ISNULL(taxTotals.dblTotalTax,0) - ISNULL(paymentOp.dblAmount,0) - ISNULL(customerChange.dblAmount,0) + ISNULL(customerPayments.dblAmount,0))) AS CashOverShort
+   FROM tblSTCheckoutDepartmetTotals deptTotals  
+   LEFT OUTER JOIN tblICCategory cat 
+		ON deptTotals.intCategoryId = cat.intCategoryId 
    LEFT OUTER JOIN (
 					SELECT intCheckoutId, SUM(dblTotalTax) AS dblTotalTax 
 					FROM tblSTCheckoutSalesTaxTotals 
 					GROUP BY intCheckoutId
-				   ) C ON A.intCheckoutId = C.intCheckoutId 
+				   ) taxTotals ON deptTotals.intCheckoutId = taxTotals.intCheckoutId 
    LEFT OUTER JOIN (
-					SELECT intCheckoutId, SUM(dblAmount) AS dblAmount 
-					FROM tblSTCheckoutPaymentOptions 
-					GROUP BY intCheckoutId
-				   ) D ON A.intCheckoutId = D.intCheckoutId 
+					SELECT a.intCheckoutId, (SUM(a.dblAmount) + b.dblChangeFundIncreaseDecrease) AS dblAmount 
+					FROM tblSTCheckoutPaymentOptions a
+					INNER JOIN tblSTCheckoutHeader b
+						ON a.intCheckoutId = b.intCheckoutId
+					GROUP BY a.intCheckoutId,
+							b.dblChangeFundIncreaseDecrease
+				   ) paymentOp ON deptTotals.intCheckoutId = paymentOp.intCheckoutId 
    LEFT OUTER JOIN (
 					SELECT intCheckoutId, SUM(dblAmount) AS dblAmount 
 					FROM tblSTCheckoutCustomerCharges 
 					GROUP BY intCheckoutId
-				   ) E ON A.intCheckoutId = E.intCheckoutId 
+				   ) customerChange ON deptTotals.intCheckoutId = customerChange.intCheckoutId 
    LEFT OUTER JOIN (
 					SELECT intCheckoutId, SUM(dblPaymentAmount) AS dblAmount 
 					FROM tblSTCheckoutCustomerPayments 
 					GROUP BY intCheckoutId
-				   ) F ON A.intCheckoutId = F.intCheckoutId 
+				   ) customerPayments ON deptTotals.intCheckoutId = customerPayments.intCheckoutId 
    LEFT OUTER JOIN (
 					SELECT intCheckoutId, SUM(dblTotalDeposit) AS dblTotalDeposit 
 					FROM tblSTCheckoutDeposits 
 					GROUP BY intCheckoutId
-				   ) G ON A.intCheckoutId =G.intCheckoutId 
-   WHERE A.intCheckoutId = @intCheckoutId 
+				   ) deposits ON deptTotals.intCheckoutId = deposits.intCheckoutId 
+   WHERE deptTotals.intCheckoutId = @intCheckoutId 
     
 END TRY
 
