e66e54393873d960de7cdcf0e51770fb62a903a0
COMTECH\BPonce <bonn.ponce@irely.com>
2019-08-20 18:48:12 +0800

    IC-7674

---
+++ i21Database/dbo/Tables/tblICInventoryCountByCategory.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[tblICInventoryCountByCategory] (
+    [intInventoryCountByCategoryId] INT           IDENTITY (1, 1) NOT NULL,
+    [intLocationId]                 INT           NOT NULL,
+    [dtmCountDate]                  DATETIME      NULL,
+    [strCountNo]                    NVARCHAR (50) NULL,
+    [ysnPosted]                     BIT           CONSTRAINT [DF__tblICInve__ysnPo__27586B36] DEFAULT ((0)) NOT NULL,
+    [dtmPosted]                     DATETIME      NULL,
+    [intCompanyId]                  INT           NULL,
+    [intSort]                       INT           NULL,
+    [intUserId]                     INT           NULL,
+    [intConcurrencyId]              INT           CONSTRAINT [DF__tblICInve__intCo__284C8F6F] DEFAULT ((0)) NULL,
+    [dtmDateCreated]                DATETIME      NULL,
+    [dtmDateModified]               DATETIME      NULL,
+    [intCreatedByUserId]            INT           NULL,
+    [intModifiedByUserId]           INT           NULL,
+    CONSTRAINT [PK_tblICInventoryCountByCategory] PRIMARY KEY CLUSTERED ([intInventoryCountByCategoryId] ASC) WITH (FILLFACTOR = 70),
+    CONSTRAINT [FK_tblICInventoryCountByCategory_tblSMCompanyLocation] FOREIGN KEY ([intLocationId]) REFERENCES [dbo].[tblSMCompanyLocation] ([intCompanyLocationId])
+);
+
---
+++ i21Database/dbo/Views/vyuICGetInventoryCountByCategoryDetail.sql
@@ -0,0 +1,9 @@
+﻿CREATE VIEW [dbo].[vyuICGetInventoryCountByCategoryDetail]
+	AS 
+
+SELECT 
+	InventoryCountByCategoryDetail.*,
+	Category.strCategoryCode,
+	Category.strDescription
+FROM tblICInventoryCountByCategoryDetail InventoryCountByCategoryDetail
+	LEFT JOIN tblICCategory Category ON Category.intCategoryId = InventoryCountByCategoryDetail.intCategoryId
