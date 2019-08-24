9bd0f7807dee006b165b15b6ccbdc93df39ebf9f
jayson_ramos <jayson.ramos@irely.com>
2019-08-24 13:23:43 +0800

    CT-3552

---
+++ i21Database/dbo/Stored Procedures/uspCTSyncPriceAndDerivative.sql
@@ -0,0 +1,28 @@
+﻿CREATE PROCEDURE [dbo].[uspCTSyncPriceAndDerivative]
+	@intFutOptTransactionId INT, --> tblRKFutOptTransaction.intFutOptTransactionId
+	@strAction nvarchar(10) --> Transaction type - accept Update and Delete case sensitive
+AS
+
+BEGIN TRY
+	
+	DECLARE	@ErrMsg	NVARCHAR(MAX),
+			@dblPrice numeric(18,6);
+
+	if (@strAction = 'Update')
+	begin
+		set  @dblPrice = (select dblPrice from tblRKFutOptTransaction where intFutOptTransactionId = @intFutOptTransactionId);
+		update tblCTPriceFixationDetail set dblHedgePrice = @dblPrice where intFutOptTransactionId = @intFutOptTransactionId;
+	end
+	if (@strAction = 'Delete')
+	begin
+		update tblCTPriceFixationDetail set dblHedgePrice = null, intFutOptTransactionId = null, ysnHedge = convert(bit,0) where intFutOptTransactionId = @intFutOptTransactionId;
+	end	
+
+END TRY
+
+BEGIN CATCH
+
+	SET @ErrMsg = ERROR_MESSAGE()  
+	RAISERROR (@ErrMsg,18,1,'WITH NOWAIT')  
+	
+END CATCH
