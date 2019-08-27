dc48f17e7d949582ad90df6c51477d343ac6659a
Jeff Trajano <jeffrey.trajano@irely.com>
2019-08-27 11:57:40 +0800

    GL-7251 GL Account Ranges Off Again After Upgrade-

--- i21Database/Scripts/Post-Deployment/GL/DefaultData/1h_AccountRange.sql
+++ i21Database/Scripts/Post-Deployment/GL/DefaultData/1h_AccountRange.sql
@@ -1,7 +1,8 @@
 ﻿GO
-	PRINT 'Start generating default account range'
-GO
-	EXEC uspGLGenerateAccountRange
-GO
-	PRINT 'Fnished generating default account range'
+	IF NOT EXISTS(SELECT TOP 1 1 FROM tblGLAccountRange)
+	BEGIN
+		PRINT 'Start generating default account range'
+		EXEC uspGLGenerateAccountRange
+		PRINT 'Fnished generating default account range'
+	END
 GO
