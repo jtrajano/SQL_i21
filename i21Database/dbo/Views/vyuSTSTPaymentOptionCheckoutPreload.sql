17b0333e7692e8130d7582d7aef7853615f1eec1
henry_zaragoza <henry.zaragoza@irely.com>
2019-08-28 15:18:03 +0800

    ST-1492 Checkout > Cash MOP should not import into payment options
    (cherry picked from commit 6d9b82f756bdeddbd67067ec8bb063de1a91efe7)

--- i21Database/dbo/Views/vyuSTSTPaymentOptionCheckoutPreload.sql
+++ i21Database/dbo/Views/vyuSTSTPaymentOptionCheckoutPreload.sql
@@ -9,8 +9,19 @@ SELECT
     stpo.[intAccountId], 
     stpo.[strRegisterMop],  
 	stpo.[ysnDepositable], 
-    stpo.[intConcurrencyId]
+    stpo.[intConcurrencyId],
+
+	-- Not Map
+	item.strItemNo,
+	item.strDescription		AS strItemDescription,
+	st.intStoreNo,
+	gl.strAccountId,
+	gl.strDescription		AS strAccountDescription
 FROM tblSTPaymentOption stpo
 INNER JOIN tblSTStore st
 	ON stpo.intStoreId = st.intStoreId
+INNER JOIN tblICItem item
+	ON stpo.intItemId = item.intItemId
+LEFT JOIN tblGLAccount gl
+	ON stpo.intAccountId = gl.intAccountId
 WHERE stpo.intPaymentOptionId NOT IN (ISNULL(st.intCustomerChargeMopId,0) , ISNULL(st.intCashTransctionMopId, 0))
