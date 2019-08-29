a030144990a0855fee492b8c1ca6b2dc436d2b01
Arman Arenas <arman.arenas@irely.com>
2019-08-29 15:39:37 +0800

    LG-2292 display currency description as currency text

--- i21Database/dbo/Stored Procedures/uspLGGetSalesInvoiceReport.sql
+++ i21Database/dbo/Stored Procedures/uspLGGetSalesInvoiceReport.sql
@@ -167,7 +167,7 @@ BEGIN
 		,InvDet.dblPrice
 		,strPrice2Decimals = LTRIM(CAST(ROUND(InvDet.dblPrice, 2) AS NUMERIC(18, 2)))
 		,strPrice4Decimals = LTRIM(CAST(ROUND(InvDet.dblPrice, 4) AS NUMERIC(18, 4)))
-		,strCurrencyText = CASE WHEN (PriceCur.strCurrency = 'USD') THEN 'USDOLL..' ELSE PriceCur.strCurrency END
+		,strCurrencyText = CASE WHEN (ISNULL(PriceCur.strDescription, '') <> '') THEN PriceCur.strDescription ELSE PriceCur.strCurrency END + '.'
 		,InvDet.dblQtyShipped
 		,InvDet.dblShipmentGrossWt
 		,InvDet.dblShipmentTareWt
