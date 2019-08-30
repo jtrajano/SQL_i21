16627911b8729bcfd33e9c3a9adb1add9924d2a0
COMTECH\BPonce <bonn.ponce@irely.com>
2019-08-30 13:13:25 +0800

    IC-7674

---
+++ i21Database/dbo/Views/vyuICGetRetailCategoryCount.sql
@@ -0,0 +1,25 @@
+﻿
+CREATE VIEW [dbo].[vyuICGetRetailCategoryCount]
+AS
+SELECT 
+intId = CAST(ROW_NUMBER() OVER(ORDER BY tblICCategory.intCategoryId ASC) AS INT)
+,tblICCategory.intCategoryId 
+,tblICCategory.strCategoryCode
+,tblICCategory.strDescription
+,tblICCategoryLocation.intLocationId
+,dblTargetMargin = tblICCategoryLocation.dblTargetGrossProfit - tblICCategoryLocation.dblTargetInventoryCost
+,dblCurrentMargin = vyuICGetRetailValuationByLocation.dblGrossMarginPct
+,dblCurrentRetail = vyuICGetRetailValuationByLocation.dblEndingRetail
+,dblCurrentCost = vyuICGetRetailValuationByLocation.dblEndingCost
+,dtmRetailDate = vyuICGetRetailValuationByLocation.dtmDate
+,intUserId
+FROM tblICCategory 
+INNER JOIN tblICCategoryLocation
+ON tblICCategory.intCategoryId = tblICCategoryLocation.intCategoryId
+INNER JOIN vyuICGetRetailValuationByLocation 
+ON tblICCategory.intCategoryId = vyuICGetRetailValuationByLocation.intCategoryId 
+AND tblICCategoryLocation.intLocationId = vyuICGetRetailValuationByLocation.intLocationId
+WHERE tblICCategory.ysnRetailValuation = 1
+GO
+
+
