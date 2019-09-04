17bec9085ba18387dad7c177b8e4835ab59a238d
COMTECH\JMendones <jesmar.mendones@irely.com>
2019-09-04 16:55:44 +0800

    FRM-8613

--- i21Database/dbo/Views/vyuSMContactControl.sql
+++ i21Database/dbo/Views/vyuSMContactControl.sql
@@ -5,11 +5,22 @@ SELECT intControlId
 ,strControlId
 ,strControlName
 ,ISNULL(sc.strGroupName,mm.strCategory) COLLATE Latin1_General_CI_AS AS strGroupName 
-,strContainer
+,CASE WHEN ISNULL(cl.strContainer, 0) <> 0 THEN cl.strContainer ELSE sc.strScreenName END AS strContainer
 ,strControlType
 ,cl.intConcurrencyId
-,REPLACE(mm.strMenuName, '(Portal)','') AS strScreenName
+,CASE WHEN ISNULL(mm.strMenuName,'') <> '' THEN REPLACE(mm.strMenuName, '(Portal)', '') ELSE sc.strScreenName END AS strScreenName
 FROM tblSMControl cl
 INNER JOIN tblSMScreen sc ON cl.intScreenId = sc.intScreenId
-INNER JOIN tblSMMasterMenu mm ON sc.strNamespace = LEFT(mm.strCommand, (CASE WHEN (CHARINDEX('?', mm.strCommand) - 1) < 0 THEN LEN(mm.strCommand) ELSE (CHARINDEX('?', mm.strCommand) - 1) END))
-INNER JOIN tblSMContactMenu cm ON mm.intMenuID = cm.intMasterMenuId
+LEFT JOIN tblSMMasterMenu mm ON sc.strNamespace = LEFT(mm.strCommand, (CASE WHEN (CHARINDEX('?', mm.strCommand) - 1) < 0 THEN LEN(mm.strCommand) ELSE (CHARINDEX('?', mm.strCommand) - 1) END))
+--LEFT JOIN tblSMContactMenu cm ON mm.intMenuID = cm.intMasterMenuId
+where sc.strNamespace in (
+	select 	LEFT(strCommand, (		CASE WHEN (CHARINDEX('?', strCommand) - 1) < 0 		THEN LEN(strCommand) 		ELSE (CHARINDEX('?', strCommand) - 1) 		END 	))
+	from tblSMMasterMenu a
+	inner join tblSMContactMenu b on b.intMasterMenuId = a.intMenuID
+)
+OR
+ sc.strNamespace in (
+	SELECT REPLACE(	LEFT(strCommand, (		CASE WHEN (CHARINDEX('?', strCommand) - 1) < 0 		THEN LEN(strCommand) 		ELSE (CHARINDEX('?', strCommand) - 1) 		END 	)), '.view.', '.search.')
+	from tblSMMasterMenu a
+	inner join tblSMContactMenu b on b.intMasterMenuId = a.intMenuID
+)
