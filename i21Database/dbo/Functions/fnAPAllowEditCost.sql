3e613f91fe0ff89e9a12ed92e6a9a67458bfffae
Erick <erick.delacruz@irely.com>
2019-09-04 15:24:45 +0800

    AP-7733

---
+++ i21Database/dbo/Functions/fnAPAllowEditCost.sql
@@ -0,0 +1,19 @@
+﻿CREATE FUNCTION [dbo].[fnAPAllowEditCost]
+(
+	@inventoryChargeId INT = NULL
+)
+RETURNS BIT
+AS
+BEGIN
+	DECLARE @allowed BIT = 1;
+
+	IF @inventoryChargeId > 0
+	BEGIN
+		SELECT
+			@allowed = CASE WHEN ysnInventoryCost = 0 THEN 1 ELSE 0 END
+		FROM tblICInventoryReceiptCharge A
+		WHERE intInventoryReceiptChargeId = @inventoryChargeId
+	END
+
+	RETURN @allowed;
+END
