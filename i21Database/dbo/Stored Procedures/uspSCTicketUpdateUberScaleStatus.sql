efe38240c31cfe295450ed94685ae6b080d4cf05
Mon Gonzales <mon.gonzales@irely.com>
2019-09-05 08:40:48 +0800

    SC-2700 - Add new status codes and functions to scale tickets w/date-time stamp

--- i21Database/dbo/Stored Procedures/uspSCTicketUpdateUberScaleStatus.sql
+++ i21Database/dbo/Stored Procedures/uspSCTicketUpdateUberScaleStatus.sql
@@ -3,7 +3,7 @@
 	@strUberStatusCode nvarchar(3)
 AS
 	
-	if exists(select top 1 1 from tblSCTicket where intTicketId = @intTicketId and strUberStatusCode <> @strUberStatusCode)
+	if exists(select top 1 1 from tblSCTicket where intTicketId = @intTicketId and isnull(strUberStatusCode, '') <> @strUberStatusCode)
 	begin
 		update tblSCTicket set strUberStatusCode = @strUberStatusCode where intTicketId = @intTicketId
 		insert into tblSCTicketUberScaleStatusUpdate(intTicketId, dtmTransactionDate, strUberStatusCode)
