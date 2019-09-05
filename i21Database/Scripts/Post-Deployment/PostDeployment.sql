820214164ccf90189c07882e813d2da16dc888b8
Matt Purugganan <matt.purugganan@irely.com>
2019-09-05 15:46:34 +0800

    SC-3045
    Revert Changes

--- i21Database/Scripts/Post-Deployment/PostDeployment.sql
+++ i21Database/Scripts/Post-Deployment/PostDeployment.sql
@@ -372,7 +372,6 @@ print 'BEGIN POST DEPLOYMENT'
 :R .\GR\GR_FarmField.sql
 :R .\GR\ContractPrice.sql
 :R .\GR\SC_ScaleLVControlIntegration.sql
-:R .\GR\UpdateDeliverySheetDate.sql
 
 --Manufacturing
 :R .\MF\1_MasterTables.sql
