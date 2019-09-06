d02a0429d95f51a7f679895740e295291294ba48
jayson_ramos <jayson.ramos@irely.com>
2019-09-06 09:31:40 +0800

    Revert "TM-3088"
    This reverts commit 634a81afc62078dc1da8065c8bfa4326863de6ad.

--- i21Database/Scripts/Post-Deployment/TM/DefaultData/11_ApplianceType.sql
+++ i21Database/Scripts/Post-Deployment/TM/DefaultData/11_ApplianceType.sql
@@ -21,27 +21,4 @@ END
 
 GO
 	PRINT N'END INSERT DEFAULT TM Appliance TYPE '
-	PRINT N'BEGIN FIXING SITE DEVICE '
-GO
-
-	update
-		a
-	set
-		strDescription = b.strDescription
-		,strManufacturerName = c.strManufacturerName
-		,strModelNumber = b.strModelNumber
-		,strSerialNumber = b.strSerialNumber
-		,dtmPurchaseDate = b.dtmPurchaseDate
-		,dtmManufacturedDate = b.dtmManufacturedDate
-		,strComment = b.strComment
-		,ysnFixed = 1
-	from
-		tblTMSiteDevice a
-		join tblTMDevice b on b.intDeviceId = a.intDeviceId
-		left join tblTMManufacturer c on c.intManufacturerId = b.intManufacturerId
-	where
-		isnull(a.ysnFixed,0) = convert(bit,0)
-
-GO
-	PRINT N'END FIXING SITE DEVICE '
 GO
