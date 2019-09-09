918568c5b25bce8cb2b4971d4a5a54e850d93a3c
Feb Montefrio <feb.montefrio@irely.com>
2019-09-09 11:27:35 +0800

    [G] IC-7480

---
+++ i21Database/dbo/Views/vyuICGetCountGroup.sql
@@ -0,0 +1,9 @@
+﻿CREATE VIEW [dbo].[vyuICGetCountGroup]
+AS
+
+SELECT 
+	c.*
+	,strCountWithGroup = countWithGroup.strCountGroup	
+FROM
+	tblICCountGroup c LEFT JOIN tblICCountGroup countWithGroup
+		ON c.intCountWithGroupId = countWithGroup.intCountGroupId
