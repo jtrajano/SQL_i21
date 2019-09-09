4eac3a85bc529067c239da17a51a37421550b526
Erick <erick.delacruz@irely.com>
2019-09-09 18:52:36 +0800

    AP-7772

--- i21Database/Scripts/Post-Deployment/PostDeployment.sql
+++ i21Database/Scripts/Post-Deployment/PostDeployment.sql
@@ -307,6 +307,7 @@ print 'BEGIN POST DEPLOYMENT'
 :r .\AP\UpdatePrepayDetailAccount.sql
 :r .\AP\UpdateVoucherPayableTranType.sql
 :r .\AP\PurgePayableCompleted.sql
+:r .\AP\UpdatePaymentDetailOffset.sql
 
 -- Inventory 
 :r .\IC\01_InventoryTransactionTypes.sql 
