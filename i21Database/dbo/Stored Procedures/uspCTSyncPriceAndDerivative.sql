014e28d95a2dc6199a194ff48afac401aeb52576
jayson_ramos <jayson.ramos@irely.com>
2019-09-11 10:59:46 +0800

    CT-3761
    (cherry picked from commit 252000cc48ab97a5fb5d35f5a9eb80f5915136c5)

--- i21Database/dbo/Stored Procedures/uspCTSyncPriceAndDerivative.sql
+++ i21Database/dbo/Stored Procedures/uspCTSyncPriceAndDerivative.sql
@@ -6,12 +6,44 @@ AS
 BEGIN TRY
 	
 	DECLARE	@ErrMsg	NVARCHAR(MAX),
-			@dblPrice numeric(18,6);
+			@dblPrice numeric(18,6),
+			@dblNoOfContract numeric(18,6),
+			@intFutureMonthId int,
+			@intBrokerageAccountId int,
+			@dblNoOfLots numeric(18,6);
 
 	if (@strAction = 'Update')
 	begin
-		set  @dblPrice = (select dblPrice from tblRKFutOptTransaction where intFutOptTransactionId = @intFutOptTransactionId);
-		update tblCTPriceFixationDetail set dblHedgePrice = @dblPrice where intFutOptTransactionId = @intFutOptTransactionId;
+		--set  @dblPrice = (select dblPrice from tblRKFutOptTransaction where intFutOptTransactionId = @intFutOptTransactionId);
+		select
+			@dblPrice = dblPrice
+			,@dblNoOfContract = dblNoOfContract
+			,@intFutureMonthId = intFutureMonthId
+			,@intBrokerageAccountId = intBrokerageAccountId
+		from
+			tblRKFutOptTransaction
+		where
+			intFutOptTransactionId = @intFutOptTransactionId;
+
+		set @dblNoOfLots = (select dblNoOfLots from tblCTPriceFixationDetail where intFutOptTransactionId = @intFutOptTransactionId);
+
+		if (@dblNoOfContract > @dblNoOfLots)
+		begin
+			SET @ErrMsg = 'No. of Contract should not more than No. of priced Lots in Contract Pricing.';
+			RAISERROR (@ErrMsg,18,1,'WITH NOWAIT');
+		end
+		else
+		begin
+			update
+				tblCTPriceFixationDetail
+			set
+				dblHedgePrice = @dblPrice
+				,dblHedgeNoOfLots = @dblNoOfContract
+				,intHedgeFutureMonthId = @intFutureMonthId
+				,intBrokerageAccountId = @intBrokerageAccountId
+			where
+				intFutOptTransactionId = @intFutOptTransactionId;
+		end
 	end
 	if (@strAction = 'Delete')
 	begin
