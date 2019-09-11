44cff8ddf8f374f513f21f5ffa98d07420a6af28
Kevin Estrada <kevin.estrada@irely.com>
2019-09-11 17:28:56 +0800

    SM-4793

--- i21Database/dbo/Stored Procedures/uspSMInterCompanyDeleteMovedDMS.sql
+++ i21Database/dbo/Stored Procedures/uspSMInterCompanyDeleteMovedDMS.sql
@@ -9,6 +9,7 @@ AS
 BEGIN
 
 DECLARE @DBNameToUse NVARCHAR(MAX);
+DECLARE @sql NVARCHAR(MAX) = N'';
 
 
 	IF(ISNULL(@referenceCompanyId,'') = '')
@@ -22,14 +23,39 @@ DECLARE @DBNameToUse NVARCHAR(MAX);
 		END
 
 	BEGIN TRY 
-		BEGIN TRANSACTION;
-	DECLARE @sql NVARCHAR(MAX) = N'';
+	BEGIN TRANSACTION;	
+/**********THIS WILL DELETE INDIRECT DOCUMENT ASSOCIATION TO OTHER DB. EQ. A-B, B-C. we delete A so we should delete it also on C**************/
+   DECLARE @intReferenceTransactionId INT
+   DECLARE @intReferenceCompanyId INT
 
-						SET @sql = N'
-						DELETE FROM ['+ @DBNameToUse +'].dbo.[tblSMDocument] 
-						WHERE intDocumentId = ' + CONVERT(VARCHAR, @intRecordId) + ' AND intDocumentId NOT IN ('+ CONVERT(VARCHAR,ISNULL(@intRecordIdExcludeDelete, 0)) +') ';
 
-						EXEC sp_executesql @sql
+   select @intReferenceTransactionId = intReferenceTransactionId,
+		  @intReferenceCompanyId = intReferenceCompanyId
+    from tblSMInterCompanyMapping where intCurrentTransactionId = ( select intTransactionId from tblSMDocument where intDocumentId = @intRecordId) and isnull(intReferenceCompanyId,0) <> 0 
+
+   --on other db
+   DECLARE @otherDB nvarchar(20) = (select strDatabaseName from tblSMInterCompany where intInterCompanyId = @intReferenceCompanyId)
+
+   declare @docId INT
+   declare @sourceDocId INT
+   set @sql = N'SELECT @docId = intDestinationRecordId , @sourceDocId = intSourceRecordId FROM ['+@otherDB +'].dbo.tblSMInterCompanyTransferLogForDMS WHERE intDestinationRecordId = ' +  CAST(ISNULL(@intRecordId,0) AS nvarchar) +' AND ISNULL(intDestinationCompanyId, 0) <> 0 '
+   exec sp_executesql @sql, N'@docId INT OUTPUT, @sourceDocId INT OUTPUT', @docId OUTPUT, @sourceDocId OUTPUT
+
+   if(ISNULL(@docId,0) <> 0) 
+   begin
+		set @sql = N'DELETE FROM ['+@otherDB+'].dbo.tblSMDocument WHERE intDocumentId = '+ CONVERT(VARCHAR,ISNULL(@sourceDocId, 0))
+		exec sp_executesql @sql
+   end
+
+/***END REGION**/
+
+
+
+	SET @sql = N'
+	DELETE FROM ['+ @DBNameToUse +'].dbo.[tblSMDocument] 
+	WHERE intDocumentId = ' + CONVERT(VARCHAR, @intRecordId) + ' AND intDocumentId NOT IN ('+ CONVERT(VARCHAR,ISNULL(@intRecordIdExcludeDelete, 0)) +') ';
+
+	EXEC sp_executesql @sql
 						
 		COMMIT TRANSACTION;
    END TRY
