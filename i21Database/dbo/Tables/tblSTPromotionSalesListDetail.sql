bad6f5d7616fc6879741c4a674c134b5dbc0838f
henry_zaragoza <henry.zaragoza@irely.com>
2019-09-11 14:24:54 +0800

    ST-1524 Promotion Item List > Able to delete Promotion Item used in Promotion Sales

--- i21Database/dbo/Tables/tblSTPromotionSalesListDetail.sql
+++ i21Database/dbo/Tables/tblSTPromotionSalesListDetail.sql
@@ -8,6 +8,6 @@
     [intConcurrencyId] INT NOT NULL, 
     CONSTRAINT [PK_tblSTPromotionSalesListDetail] PRIMARY KEY ([intPromoSalesListDetailId]), 
 	CONSTRAINT [FK_tblSTPromotionSalesListDetail_tblSTPromotionSalesList] FOREIGN KEY ([intPromoSalesListId]) REFERENCES [tblSTPromotionSalesList]([intPromoSalesListId]),
-    CONSTRAINT [FK_tblSTPromotionSalesListDetail_tblSTPromotionItemList] FOREIGN KEY ([intPromoItemListId]) REFERENCES [tblSTPromotionItemList]([intPromoItemListId]) ON DELETE CASCADE,
+    CONSTRAINT [FK_tblSTPromotionSalesListDetail_tblSTPromotionItemList] FOREIGN KEY ([intPromoItemListId]) REFERENCES [tblSTPromotionItemList]([intPromoItemListId]),
 	CONSTRAINT [FK_tblSTPromotionSalesListDetail_tblSTPromotionSalesList_intPromoSalesListId] FOREIGN KEY ([intPromoSalesListId]) REFERENCES [tblSTPromotionSalesList]([intPromoSalesListId]) ON DELETE CASCADE
 );
