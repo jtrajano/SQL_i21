939aa12d229392ffa3ba991d1b437817e8e92d99
COMTECH\BPonce <bonn.ponce@irely.com>
2019-09-11 16:31:15 +0800

    CF-2365

--- i21Database/dbo/Tables/tblCFTransactionNote.sql
+++ i21Database/dbo/Tables/tblCFTransactionNote.sql
@@ -5,9 +5,9 @@
     [dtmProcessDate]       DATETIME       NULL,
     [strNote]              NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NULL,
     [strGuid]              NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NULL,
-    [ysnCurrentError]      BIT              NULL,
+    [ysnCurrentError]      BIT            CONSTRAINT [DF_tblCFTransactionNote_ysnCurrentError]  DEFAULT ((1)) NULL,
     [intConcurrencyId]     INT            CONSTRAINT [DF_tblCFTransactionNote_intConcurrencyId] DEFAULT ((1)) NULL,
-    [strErrorTitle]        NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NULL,
+    [strErrorTitle]        NVARCHAR (MAX) CONSTRAINT [DF_tblCFTransactionNote_strErrorTitle]  DEFAULT ('Current Error') COLLATE Latin1_General_CI_AS NULL,
     CONSTRAINT [PK_tblCFTransactionNote] PRIMARY KEY CLUSTERED ([intTransactionNoteId] ASC),
     CONSTRAINT [FK_tblCFTransactionNote_tblCFTransaction] FOREIGN KEY ([intTransactionId]) REFERENCES [dbo].[tblCFTransaction] ([intTransactionId]) ON DELETE CASCADE
 );
