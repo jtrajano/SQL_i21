ab0174d0a602b620b0592f55b38cedd0281da2f2
Kevin Estrada <kevin.estrada@irely.com>
2019-09-18 15:13:09 +0800

    SM-4820

--- i21Database/Scripts/Post-Deployment/SM/1810_Reset_Hours_TaxCodeRate.sql
+++ i21Database/Scripts/Post-Deployment/SM/1810_Reset_Hours_TaxCodeRate.sql
@@ -1,7 +1,7 @@
 ﻿PRINT N'BEGIN - RESET HOURS TAX CODE Fix for 18.1. '
 GO
 
-IF EXISTS( SELECT 1 FROM (SELECT TOP 1 dblVersion = CAST(LEFT(strVersionNo, 4) AS NUMERIC(18,1)) FROM tblSMBuildNumber ORDER BY intVersionID DESC) v WHERE v.dblVersion <= 18.1)
+IF EXISTS( SELECT 1 FROM (SELECT TOP 1 dblVersion = CAST(LEFT(strVersionNo, 4) AS NUMERIC(18,1)) FROM tblSMBuildNumber ORDER BY intVersionID DESC) v WHERE v.dblVersion <= 19.1)
 BEGIN
 	UPDATE t SET dtmEffectiveDate = CAST(t.dtmEffectiveDate AS DATE)
 	FROM tblSMTaxCodeRate t
