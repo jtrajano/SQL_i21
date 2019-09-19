87ee6cc4acb9d72bc3f1b833f58e3b4114bca471
Kelvin Quinto <kelvin.quinto@irely.com>
2019-09-19 12:45:09 +0800

    AR-10735

--- i21Database/dbo/Stored Procedures/uspARUpdateInvoiceIntegrations.sql
+++ i21Database/dbo/Stored Procedures/uspARUpdateInvoiceIntegrations.sql
@@ -75,6 +75,7 @@ BEGIN TRY
 END TRY
 BEGIN CATCH
 	DECLARE @strErrorMsg varchar(4000) = ERROR_MESSAGE()
+	DECLARE @strThrow	 NVARCHAR(MAX) = 'THROW 51000, ''' + @strErrorMsg + ''', 1'
 
 	IF XACT_STATE() = -1
 		ROLLBACK;
@@ -83,6 +84,6 @@ BEGIN CATCH
 	IF XACT_STATE() = 1 AND @intTranCount > 0
 		ROLLBACK TRANSACTION uspARUpdateInvoiceIntegrations;
 
-	THROW 51000, @strErrorMsg, 1
+	EXEC sp_executesql @strThrow
 
 END CATCH
