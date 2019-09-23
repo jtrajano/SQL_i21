83403094d2cd97bc1354ec62f220902ee74a2d18
Vinoth Kumar T <vinoth.t@irely.com>
2019-09-23 20:38:45 +0530

    QC-541

--- i21Database/dbo/Views/vyuMFCompanyPreference.sql
+++ i21Database/dbo/Views/vyuMFCompanyPreference.sql
@@ -85,6 +85,7 @@ SELECT CP.intCompanyPreferenceId
 	,CP.dblDemandGrowthPerc
 	,CP.ysnComputeDemandUsingRecipe
 	,CP.ysnDisplayDemandWithItemNoAndDescription
+	,CP.ysnDisplayRestrictedBookInDemandView
 	,SL.strName AS strShipmentStagingLocation
 	,SL1.strName AS strShipmentDockDoorLocation
 	,LS.strSecondaryStatus AS strBondLotStatus
