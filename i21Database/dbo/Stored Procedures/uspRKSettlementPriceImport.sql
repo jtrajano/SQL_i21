c165a9cd352c6dae3c255ad453bcc264064e2bcd
lawrence_zabala <lawrence.zabala@irely.com>
2019-09-26 16:24:23 +0800

    RM-3288 RM-3287

--- i21Database/dbo/Stored Procedures/uspRKSettlementPriceImport.sql
+++ i21Database/dbo/Stored Procedures/uspRKSettlementPriceImport.sql
@@ -1,116 +1,123 @@
-﻿CREATE PROC [dbo].[uspRKSettlementPriceImport]
+﻿CREATE PROCEDURE [dbo].[uspRKSettlementPriceImport]
 	@intEntityUserId VARCHAR (100) = NULL
+
 AS
+
 BEGIN TRY
-
-DECLARE @ErrMsg nvarchar(Max)
-
-DECLARE @strDateTimeFormat nvarchar(50)
-DECLARE @ConvertYear int
-DECLARE @dtmPriceDate1 nvarchar(50)
-
-	DECLARE @strMarket nvarchar(50) = null
-	DECLARE @strSettlementDate nvarchar(50) = null
-	DECLARE @intFutureMarketId int = null
-	DECLARE @intFutureSettlementPriceId int = null
-	DECLARE @newlyCreatedIds nvarchar(max) = ''
-
-SELECT @strDateTimeFormat = strDateTimeFormat FROM tblRKCompanyPreference
-
-IF (@strDateTimeFormat = 'MM DD YYYY HH:MI' OR @strDateTimeFormat ='YYYY MM DD HH:MI' OR ISNULL(@strDateTimeFormat,'') = '')
-SELECT @ConvertYear=101
-ELSE IF (@strDateTimeFormat = 'DD MM YYYY HH:MI' OR @strDateTimeFormat ='YYYY DD MM HH:MI')
-SELECT @ConvertYear=103
-
-
-BEGIN TRAN
-
-IF NOT EXISTS(SELECT intImportSettlementPriceId FROM tblRKSettlementPriceImport_ErrLog)
-BEGIN
-DECLARE @mRowNumber INT
-SELECT ROW_NUMBER()  OVER (ORDER BY strFutureMarket) intRowNum,strFutureMarket INTO #temp from(
-SELECT DISTINCT strFutureMarket  FROM tblRKSettlementPriceImport)t
-
-
-SELECT @mRowNumber = MIN(intRowNum) FROM #temp
-WHILE @mRowNumber > 0
-	BEGIN
-
-	set @strMarket  = ''
-	set @strSettlementDate  = ''
-	set @intFutureMarketId = null
-	set @intFutureSettlementPriceId = null
-
-	SELECT @strMarket=strFutureMarket from #temp where intRowNum=@mRowNumber
-
+	DECLARE @ErrMsg NVARCHAR(MAX)
+		, @strDateTimeFormat NVARCHAR(50)
+		, @ConvertYear INT
+		, @dtmPriceDate1 NVARCHAR(50)
+		, @strMarket NVARCHAR(50) = NULL
+		, @strSettlementDate NVARCHAR(50) = NULL
+		, @intFutureMarketId INT = NULL
+		, @intFutureSettlementPriceId int = NULL
+		, @newlyCreatedIds NVARCHAR(MAX) = ''
 	
-	SELECT @strMarket=ltrim(rtrim(strFutureMarket)),@strSettlementDate=CONVERT(DATETIME,strPriceDate,@ConvertYear) FROM tblRKSettlementPriceImport where strFutureMarket=@strMarket
-	SELECT @intFutureMarketId=intFutureMarketId from tblRKFutureMarket where strFutMarketName=@strMarket
+	SELECT @strDateTimeFormat = strDateTimeFormat FROM tblRKCompanyPreference
 
+	IF (@strDateTimeFormat = 'MM DD YYYY HH:MI' OR @strDateTimeFormat ='YYYY MM DD HH:MI' OR ISNULL(@strDateTimeFormat,'') = '')
+		SELECT @ConvertYear = 101
+	ELSE IF (@strDateTimeFormat = 'DD MM YYYY HH:MI' OR @strDateTimeFormat ='YYYY DD MM HH:MI')
+		SELECT @ConvertYear = 103
+		
+	BEGIN TRAN
 
-		DECLARE @intCommodityMarketId INT = NULL
-	SELECT @intCommodityMarketId=intCommodityMarketId FROM tblRKFutureMarket m
-	JOIN tblRKCommodityMarketMapping mm on m.intFutureMarketId=mm.intFutureMarketId 
-	WHERE m.intFutureMarketId=@intFutureMarketId
-
-  INSERT INTO tblRKFuturesSettlementPrice(intFutureMarketId,dtmPriceDate,intConcurrencyId,intCommodityMarketId,strPricingType)
-   VALUES(@intFutureMarketId,@strSettlementDate,1,@intCommodityMarketId,'Mark To Market')
-
-	SELECT @intFutureSettlementPriceId = scope_Identity()
-
-	SET @newlyCreatedIds = @newlyCreatedIds +  CAST(@intFutureSettlementPriceId as nvarchar(50)) + ','
-
---Insert Futures Month settlement Price	
-	INSERT INTO tblRKFutSettlementPriceMarketMap (intConcurrencyId,intFutureSettlementPriceId,intFutureMonthId,dblLastSettle,dblLow,dblHigh,strComments)
-	SELECT 1,@intFutureSettlementPriceId,intFutureMonthId,dblLastSettle,dblLow,dblHigh,strFutComments FROM tblRKSettlementPriceImport i
-	JOIN tblRKFuturesMonth fm on fm.strFutureMonth=replace(i.strFutureMonth ,'-',' ') and intFutureMarketId=@intFutureMarketId
-	WHERE strInstrumentType='Futures' and strFutureMarket=@strMarket
-
--- Insert Options Month settlement Price	
-	INSERT INTO tblRKOptSettlementPriceMarketMap (intConcurrencyId,intFutureSettlementPriceId,intOptionMonthId,dblStrike,intTypeId,dblSettle,dblDelta,strComments)
-	SELECT 1,@intFutureSettlementPriceId,intOptionMonthId,dblStrike,CASE WHEN strType = 'Put' THEN 1 
-																		WHEN strType = 'Call' THEN 2 ELSE 0 END,
-			  dblSettle,dblDelta,strFutComments FROM tblRKSettlementPriceImport i
-	JOIN tblRKOptionsMonth fm on fm.strOptionMonth=replace(i.strFutureMonth ,'-',' ')  and intFutureMarketId=@intFutureMarketId
-	WHERE strInstrumentType like 'Opt%' and strFutureMarket=@strMarket
-
+	DECLARE @mRowNumber INT
+	SELECT ROW_NUMBER() OVER (ORDER BY strFutureMarket) intRowNum
+		, strFutureMarket
+	INTO #temp
+	FROM (
+		SELECT DISTINCT strFutureMarket FROM tblRKSettlementPriceImport
+	) t
+	
+	SELECT @mRowNumber = MIN(intRowNum) FROM #temp
+	WHILE @mRowNumber > 0
 	BEGIN
-		EXEC	dbo.uspSMAuditLog 
-				@keyValue = @intFutureSettlementPriceId									-- Primary Key Value of the Settlement Price. 
-				,@screenName = 'RiskManagement.view.FuturesOptionsSettlementPrices'     -- Screen Namespace
-				,@entityId = @intEntityUserId                   						-- Entity Id
-				,@actionType = 'Imported'				                                -- Action Type
-				,@changeDescription = ''												-- Description
-				,@fromValue = ''														-- Previous Value
-				,@toValue = ''															-- New Value
+		SELECT @strMarket = ''
+			, @strSettlementDate = ''
+			, @intFutureMarketId = NULL
+			, @intFutureSettlementPriceId = NULL
+		
+		SELECT @strMarket = strFutureMarket
+		FROM #temp WHERE intRowNum = @mRowNumber
+		SELECT @strMarket = LTRIM(RTRIM(strFutureMarket))
+			, @strSettlementDate = CONVERT(DATETIME, strPriceDate, @ConvertYear)
+		FROM tblRKSettlementPriceImport
+		WHERE strFutureMarket = @strMarket
+		
+		SELECT @intFutureMarketId = intFutureMarketId FROM tblRKFutureMarket WHERE strFutMarketName = @strMarket
+		
+		DECLARE @intCommodityMarketId INT = NULL
+		SELECT @intCommodityMarketId = intCommodityMarketId FROM tblRKFutureMarket m
+		JOIN tblRKCommodityMarketMapping mm ON m.intFutureMarketId = mm.intFutureMarketId
+		WHERE m.intFutureMarketId = @intFutureMarketId
+		
+		INSERT INTO tblRKFuturesSettlementPrice(intFutureMarketId, dtmPriceDate, intConcurrencyId, intCommodityMarketId, strPricingType)
+		VALUES(@intFutureMarketId, @strSettlementDate, 1, @intCommodityMarketId, 'Mark To Market')
+		
+		SELECT @intFutureSettlementPriceId = SCOPE_IDENTITY()
+		
+		SET @newlyCreatedIds = @newlyCreatedIds + CAST(@intFutureSettlementPriceId as NVARCHAR(50)) + ','
+		
+		--Insert Futures Month settlement Price	
+		INSERT INTO tblRKFutSettlementPriceMarketMap (intConcurrencyId, intFutureSettlementPriceId, intFutureMonthId, dblLastSettle, dblLow, dblHigh, strComments)
+		SELECT 1, @intFutureSettlementPriceId, intFutureMonthId, dblLastSettle, dblLow, dblHigh, strFutComments
+		FROM tblRKSettlementPriceImport i
+		JOIN tblRKFuturesMonth fm ON fm.strFutureMonth = REPLACE(i.strFutureMonth, '-', ' ') AND intFutureMarketId = @intFutureMarketId
+		WHERE strInstrumentType = 'Futures' AND strFutureMarket = @strMarket
+		
+		-- Insert Options Month settlement Price
+		INSERT INTO tblRKOptSettlementPriceMarketMap (intConcurrencyId, intFutureSettlementPriceId, intOptionMonthId, dblStrike, intTypeId, dblSettle, dblDelta, strComments)
+		SELECT 1
+			, @intFutureSettlementPriceId
+			, intOptionMonthId
+			, dblStrike
+			, CASE WHEN strType = 'Put' THEN 1 WHEN strType = 'Call' THEN 2 ELSE 0 END
+			, dblSettle
+			, dblDelta
+			, strFutComments
+		FROM tblRKSettlementPriceImport i
+		JOIN tblRKOptionsMonth fm ON fm.strOptionMonth = REPLACE(i.strFutureMonth, '-', ' ') AND intFutureMarketId = @intFutureMarketId
+		WHERE strInstrumentType LIKE 'Opt%' AND strFutureMarket = @strMarket
+		
+		EXEC uspSMAuditLog @keyValue = @intFutureSettlementPriceId
+			, @screenName = 'RiskManagement.view.FuturesOptionsSettlementPrices'
+			, @entityId = @intEntityUserId
+			, @actionType = 'Imported'
+			, @changeDescription = ''
+			, @fromValue = ''
+			, @toValue = ''
+		
+		SELECT @mRowNumber = MIN(intRowNum)	FROM #temp WHERE intRowNum > @mRowNumber
 	END
 
-SELECT @mRowNumber = MIN(intRowNum)	FROM #temp	WHERE intRowNum > @mRowNumber
-END
-
-END
-
-COMMIT TRAN
-
---This will return the newly created Settlement Price
-SELECT FM.strFutMarketName AS Result1,SP.strPricingType AS Result2,SP.dtmPriceDate AS Result3,strFutureMonth Result4,'Futures' Result5 
-FROM tblRKFuturesSettlementPrice SP 
-INNER JOIN tblRKFutSettlementPriceMarketMap MM on MM.intFutureSettlementPriceId=SP.intFutureSettlementPriceId
-INNER JOIN tblRKFutureMarket FM ON SP.intFutureMarketId = FM.intFutureMarketId 
-INNER JOIN tblRKFuturesMonth MO on MO.intFutureMonthId=MM.intFutureMonthId
-WHERE MM.intFutureSettlementPriceId IN (SELECT intID FROM dbo.fnGetRowsFromDelimitedValues(@newlyCreatedIds))  
-UNION ALL
-SELECT FM.strFutMarketName AS Result1,SP.strPricingType AS Result2,SP.dtmPriceDate AS Result3,strOptionMonth Result4,'Options' Result5 
-FROM tblRKFuturesSettlementPrice SP 
-INNER JOIN tblRKOptSettlementPriceMarketMap MM on MM.intFutureSettlementPriceId=SP.intFutureSettlementPriceId
-INNER JOIN tblRKFutureMarket FM ON SP.intFutureMarketId = FM.intFutureMarketId 
-INNER JOIN tblRKOptionsMonth MO on MO.intOptionMonthId=MM.intOptionMonthId
-WHERE MM.intFutureSettlementPriceId IN (SELECT intID FROM dbo.fnGetRowsFromDelimitedValues(@newlyCreatedIds))   
-
+	COMMIT TRAN
+	
+	SELECT FM.strFutMarketName AS Result1
+		, SP.strPricingType AS Result2
+		, SP.dtmPriceDate AS Result3
+		, strFutureMonth Result4
+		, 'Futures' Result5
+	FROM tblRKFuturesSettlementPrice SP
+	INNER JOIN tblRKFutSettlementPriceMarketMap MM ON MM.intFutureSettlementPriceId = SP.intFutureSettlementPriceId
+	INNER JOIN tblRKFutureMarket FM ON SP.intFutureMarketId = FM.intFutureMarketId 
+	INNER JOIN tblRKFuturesMonth MO ON MO.intFutureMonthId = MM.intFutureMonthId
+	WHERE MM.intFutureSettlementPriceId IN (SELECT intID FROM dbo.fnGetRowsFromDelimitedValues(@newlyCreatedIds))
+	
+	UNION ALL SELECT FM.strFutMarketName AS Result1
+		, SP.strPricingType AS Result2
+		, SP.dtmPriceDate AS Result3
+		, strOptionMonth Result4
+		, 'Options' Result5
+	FROM tblRKFuturesSettlementPrice SP 
+	INNER JOIN tblRKOptSettlementPriceMarketMap MM ON MM.intFutureSettlementPriceId = SP.intFutureSettlementPriceId
+	INNER JOIN tblRKFutureMarket FM ON SP.intFutureMarketId = FM.intFutureMarketId 
+	INNER JOIN tblRKOptionsMonth MO ON MO.intOptionMonthId = MM.intOptionMonthId
+	WHERE MM.intFutureSettlementPriceId IN (SELECT intID FROM dbo.fnGetRowsFromDelimitedValues(@newlyCreatedIds)) 
 END TRY
 BEGIN CATCH
- IF XACT_STATE() != 0 AND @@TRANCOUNT > 0 ROLLBACK TRANSACTION    
- SET @ErrMsg = ERROR_MESSAGE()  
- RAISERROR(@ErrMsg, 16, 1, 'WITH NOWAIT') 
-End Catch
-
+	IF XACT_STATE() != 0 AND @@TRANCOUNT > 0 ROLLBACK TRANSACTION
+	SET @ErrMsg = ERROR_MESSAGE()
+	RAISERROR(@ErrMsg, 16, 1, 'WITH NOWAIT')
+END CATCH
