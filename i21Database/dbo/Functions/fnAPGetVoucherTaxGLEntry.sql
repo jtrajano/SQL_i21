870004fc875dcefd39c22b2c62739ee5efff43ab
Erick <erick.delacruz@irely.com>
2019-09-27 15:48:07 +0800

    AP-8109

--- i21Database/dbo/Functions/fnAPGetVoucherTaxGLEntry.sql
+++ i21Database/dbo/Functions/fnAPGetVoucherTaxGLEntry.sql
@@ -13,7 +13,8 @@ RETURNS TABLE AS RETURN
 		,CAST((D.dblTax) 
 			* (CASE WHEN A.intTransactionType != 1 THEN -1 ELSE 1 END) AS DECIMAL(18,2)) AS dblForeignTotal
 		,0 as dblTotalUnits
-		,CASE WHEN B.intInventoryReceiptItemId IS NOT NULL OR B.intInventoryReceiptChargeId IS NOT NULL OR B.intInventoryShipmentChargeId IS NOT NULL
+		,CASE WHEN (B.intInventoryReceiptItemId IS NOT NULL AND receiptItem.intTaxGroupId > 0)
+				 OR B.intInventoryReceiptChargeId IS NOT NULL OR B.intInventoryShipmentChargeId IS NOT NULL
 			THEN  dbo.[fnGetItemGLAccount](F.intItemId, ISNULL(detailloc.intItemLocationId, loc.intItemLocationId), 'AP Clearing')
 			ELSE D.intAccountId
 		END AS intAccountId
@@ -24,6 +25,8 @@ RETURNS TABLE AS RETURN
 	INNER JOIN tblAPBillDetail B ON A.intBillId = B.intBillId
 	INNER JOIN tblAPBillDetailTax D
 		ON B.intBillDetailId = D.intBillDetailId
+	LEFT JOIN tblICInventoryReceiptItem receiptItem
+		ON B.intInventoryReceiptItemId = receiptItem.intInventoryReceiptItemId
 	LEFT JOIN tblICInventoryReceiptCharge charges
 		ON B.intInventoryReceiptChargeId = charges.intInventoryReceiptChargeId
 	LEFT JOIN tblICInventoryReceipt receipts
