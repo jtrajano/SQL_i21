7b2d57c13d700bab8329485cca5ebd8ccca781f9
henry_zaragoza <henry.zaragoza@irely.com>
2019-09-30 14:38:32 +0800

    ST-1543 Store - Unable to Load Store

--- i21Database/dbo/Tables/tblSTPaymentOption.sql
+++ i21Database/dbo/Tables/tblSTPaymentOption.sql
@@ -11,7 +11,19 @@
     [intConcurrencyId] INT NOT NULL, 
     CONSTRAINT [PK_tblSTPaymentOption] PRIMARY KEY CLUSTERED ([intPaymentOptionId]), 
     CONSTRAINT [AK_tblSTPaymentOption_intStoreId_strPaymentOptionId] UNIQUE NONCLUSTERED ([intStoreId],[strPaymentOptionId]), 
+	--CONSTRAINT [AK_tblSTPaymentOption_intStoreId_strRegisterMop] UNIQUE NONCLUSTERED ([intStoreId],[strRegisterMop]), 
 	CONSTRAINT [FK_tblSTPaymentOption_tblSTStore] FOREIGN KEY ([intStoreId]) REFERENCES [tblSTStore]([intStoreId]) ON DELETE CASCADE,
     CONSTRAINT [FK_tblSTPaymentOption_tblGLAccount] FOREIGN KEY ([intAccountId]) REFERENCES [tblGLAccount]([intAccountId]), 
 	CONSTRAINT [FK_tblSTPaymentOption_tblICItem] FOREIGN KEY ([intItemId]) REFERENCES [tblICItem]([intItemId]),
  );
+ 
+ GO
+
+ CREATE UNIQUE NONCLUSTERED INDEX [UK_tblSTPaymentOption_intStoreId_strRegisterMop]
+  ON dbo.tblSTPaymentOption
+  (
+	[intStoreId]
+	,[strRegisterMop]
+  )
+  WHERE [strRegisterMop] IS NOT NULL
+  AND [strRegisterMop] <> ''
