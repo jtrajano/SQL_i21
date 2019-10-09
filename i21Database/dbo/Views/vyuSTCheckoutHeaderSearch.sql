289efe75f76c3ef4a9c649c895cf7d027fd594d8
henry_zaragoza <henry.zaragoza@irely.com>
2019-10-09 16:21:52 +0800

    ST-1562 Missing Stores in Checkout Dropdown

--- i21Database/dbo/Views/vyuSTCheckoutHeaderSearch.sql
+++ i21Database/dbo/Views/vyuSTCheckoutHeaderSearch.sql
@@ -24,31 +24,82 @@ SELECT DISTINCT
 	 , USec.ysnAdmin AS ysnIsUserAdmin
 	 , USec.strDashboardRole
 	 , USec.intEntityId
-FROM tblSTCheckoutHeader Chk
-INNER JOIN tblSTStore ST
-	ON Chk.intStoreId = ST.intStoreId
+FROM tblEMEntity em
+INNER JOIN tblSMUserSecurity USec
+	ON em.intEntityId = USec.intEntityId
+LEFT JOIN tblSMUserSecurityCompanyLocationRolePermission Perm
+	ON em.intEntityId = Perm.intEntityId 
+LEFT JOIN tblSTStore ST
+	ON ST.intCompanyLocationId = CASE
+										WHEN USec.ysnAdmin = 1
+											THEN ST.intCompanyLocationId
+										WHEN USec.ysnStoreManager = 1
+											THEN Perm.intCompanyLocationId
+									END
+INNER JOIN tblSTCheckoutHeader Chk
+	ON ST.intStoreId = Chk.intStoreId
 LEFT JOIN tblARInvoice Inv
 	ON Chk.intInvoiceId = Inv.intInvoiceId
-INNER JOIN tblSMUserSecurity USec
-	ON ST.intCompanyLocationId = USec.intCompanyLocationId
-	AND (Chk.strCheckoutStatus != CASE
-									WHEN USec.ysnStoreManager = CAST(0 AS BIT)
-										THEN ''
-								END
-		OR Chk.strCheckoutStatus = CASE
-									WHEN USec.ysnStoreManager = CAST(1 AS BIT)
-										THEN 'Open'
-								END)
-	OR 1 = CASE
-				WHEN USec.ysnStoreManager = CAST(0 AS BIT)
-					THEN 1
-				ELSE 0
-			END
-INNER JOIN tblSMUserSecurityCompanyLocationRolePermission Perm
-	ON USec.intEntityId = Perm.intEntityId
-	AND ST.intCompanyLocationId = Perm.intCompanyLocationId
-	OR 1 = CASE
-				WHEN NOT EXISTS(SELECT TOP 1 1 FROM tblSMUserSecurityCompanyLocationRolePermission WHERE intEntityId = USec.intEntityId AND intCompanyLocationId = USec.intCompanyLocationId)
-					THEN 1
-				ELSE 0
-			END
+INNER JOIN tblSMCompanyLocation CL
+	ON ST.intCompanyLocationId = CL.intCompanyLocationId
+LEFT JOIN tblSTHandheldScanner HS
+	ON ST.intStoreId = HS.intStoreId
+LEFT JOIN tblSTRegister R 
+	ON ST.intRegisterId = R.intRegisterId
+
+
+
+
+
+--SELECT DISTINCT 
+--     Chk.intCheckoutId
+--     , ST.intStoreNo
+--	 , Chk.dtmCheckoutDate
+--	 , Chk.intShiftNo
+--	 , Chk.dtmShiftClosedDate
+--	 , Chk.strCheckoutType
+--	 , CASE 
+--			WHEN Inv.ysnPosted = 1
+--				THEN 'Posted'
+--			ELSE Chk.strCheckoutStatus
+--	 END AS strCheckoutStatus
+--	 , Chk.dblCustomerCount
+--	 , Chk.dblTotalSales
+--	 , Chk.dblTotalTax
+--	 , Chk.dblTotalPaidOuts
+--	 , Chk.dblTotalToDeposit
+--	 , Chk.dblTotalDeposits
+--	 , Chk.dblCashOverShort   
+--	 , ST.intCompanyLocationId AS intStoreCompanyLocationId
+--	 , USec.ysnStoreManager AS ysnIsUserStoreManager
+--	 , USec.ysnAdmin AS ysnIsUserAdmin
+--	 , USec.strDashboardRole
+--	 , USec.intEntityId
+--FROM tblSTCheckoutHeader Chk
+--INNER JOIN tblSTStore ST
+--	ON Chk.intStoreId = ST.intStoreId
+--LEFT JOIN tblARInvoice Inv
+--	ON Chk.intInvoiceId = Inv.intInvoiceId
+--INNER JOIN tblSMUserSecurity USec
+--	ON ST.intCompanyLocationId = USec.intCompanyLocationId
+--	AND (Chk.strCheckoutStatus != CASE
+--									WHEN USec.ysnStoreManager = CAST(0 AS BIT)
+--										THEN ''
+--								END
+--		OR Chk.strCheckoutStatus = CASE
+--									WHEN USec.ysnStoreManager = CAST(1 AS BIT)
+--										THEN 'Open'
+--								END)
+--	OR 1 = CASE
+--				WHEN USec.ysnStoreManager = CAST(0 AS BIT)
+--					THEN 1
+--				ELSE 0
+--			END
+--INNER JOIN tblSMUserSecurityCompanyLocationRolePermission Perm
+--	ON USec.intEntityId = Perm.intEntityId
+--	AND ST.intCompanyLocationId = Perm.intCompanyLocationId
+--	OR 1 = CASE
+--				WHEN NOT EXISTS(SELECT TOP 1 1 FROM tblSMUserSecurityCompanyLocationRolePermission WHERE intEntityId = USec.intEntityId AND intCompanyLocationId = USec.intCompanyLocationId)
+--					THEN 1
+--				ELSE 0
+--			END
