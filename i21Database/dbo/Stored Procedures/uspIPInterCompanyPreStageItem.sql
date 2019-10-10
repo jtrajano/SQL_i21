a8ffdf36c700cf3def6c7bfa2d3587b6e92158ad
Premkumar S <prem.selladurai@irely.com>
2019-10-10 19:03:07 +0530

    IN-311

---
+++ i21Database/dbo/Stored Procedures/uspIPInterCompanyPreStageItem.sql
@@ -0,0 +1,36 @@
+﻿CREATE PROCEDURE uspIPInterCompanyPreStageItem @intItemId INT
+	,@strRowState NVARCHAR(50)
+	,@intUserId INT
+AS
+BEGIN TRY
+	SET NOCOUNT ON
+
+	DECLARE @ErrMsg NVARCHAR(MAX)
+
+	DELETE
+	FROM dbo.tblICItemPreStage
+	WHERE strFeedStatus IS NULL
+		AND intItemId = @intItemId
+
+	INSERT INTO dbo.tblICItemPreStage (
+		intItemId
+		,strRowState
+		,intUserId
+		)
+	SELECT @intItemId
+		,@strRowState
+		,@intUserId
+
+END TRY
+
+BEGIN CATCH
+	SET @ErrMsg = ERROR_MESSAGE()
+
+	RAISERROR (
+			@ErrMsg
+			,16
+			,1
+			,'WITH NOWAIT'
+			)
+END CATCH
+
---
+++ i21Database/dbo/Tables/tblICItemPreStage.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE tblICItemPreStage
+(
+	intItemPreStageId	INT IDENTITY(1,1) CONSTRAINT PK_tblICItemPreStage_intItemPreStageId PRIMARY KEY, 
+	intItemId			INT NOT NULL,
+	strRowState					NVARCHAR(100) COLLATE Latin1_General_CI_AS,
+	strFeedStatus				NVARCHAR(50) COLLATE Latin1_General_CI_AS,
+	dtmFeedDate					DATETIME CONSTRAINT DF_tblICItemPreStage_dtmFeedDate DEFAULT GETDATE(),
+	strMessage					NVARCHAR(MAX) COLLATE Latin1_General_CI_AS,
+	intUserId int
+)
