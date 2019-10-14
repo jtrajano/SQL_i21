1612b97fd1bf0afd0b03214a32db8511c7e25fa7
Premkumar S <prem.selladurai@irely.com>
2019-10-14 18:25:02 +0530

    IN-330

---
+++ i21Database/dbo/Stored Procedures/uspIPInterCompanyPreStageFreightRateMatrix.sql
@@ -0,0 +1,37 @@
+﻿CREATE PROCEDURE uspIPInterCompanyPreStageFreightRateMatrix @intFreightRateMatrixId INT
+	,@strRowState NVARCHAR(50)
+	,@intUserId INT
+AS
+BEGIN TRY
+	SET NOCOUNT ON
+
+	DECLARE @ErrMsg NVARCHAR(MAX)
+
+	DELETE
+	FROM dbo.tblLGFreightRateMatrixPreStage
+	WHERE strFeedStatus IS NULL
+		AND intFreightRateMatrixId = @intFreightRateMatrixId
+
+	INSERT INTO dbo.tblLGFreightRateMatrixPreStage (
+		intFreightRateMatrixId
+		,strRowState
+		,intUserId
+		)
+	SELECT @intFreightRateMatrixId
+		,@strRowState
+		,@intUserId
+
+END TRY
+
+BEGIN CATCH
+	SET @ErrMsg = ERROR_MESSAGE()
+
+	RAISERROR (
+			@ErrMsg
+			,16
+			,1
+			,'WITH NOWAIT'
+			)
+END CATCH
+
+
---
+++ i21Database/dbo/Tables/tblLGFreightRateMatrixPreStage.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE tblLGFreightRateMatrixPreStage
+(
+	intFreightRateMatrixPreStageId	INT IDENTITY(1,1) CONSTRAINT PK_tblLGFreightRateMatrixPreStage_intFreightRateMatrixPreStageId PRIMARY KEY, 
+	intFreightRateMatrixId			INT NOT NULL,
+	strRowState					NVARCHAR(100) COLLATE Latin1_General_CI_AS,
+	strFeedStatus				NVARCHAR(50) COLLATE Latin1_General_CI_AS,
+	dtmFeedDate					DATETIME CONSTRAINT DF_tblLGFreightRateMatrixPreStage_dtmFeedDate DEFAULT GETDATE(),
+	strMessage					NVARCHAR(MAX) COLLATE Latin1_General_CI_AS,
+	intUserId int
+)
