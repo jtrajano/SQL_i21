b329ac35ba15a0fbf3c2909f50298f7139a65afd
henry_zaragoza <henry.zaragoza@irely.com>
2019-10-18 16:00:16 +0800

    ST-1553 Commander TransLog Process > Improve Performance
    (cherry picked from commit 62a6871fba400f501051089b5448deee30857c42)

--- i21Database/Scripts/Post-Deployment/ST/25_DataFix.sql
+++ i21Database/Scripts/Post-Deployment/ST/25_DataFix.sql
@@ -14,4 +14,35 @@ IF EXISTS(SELECT * FROM  INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'tblSTChe
 			')
 	END
 PRINT N'END - STORE - Item Movement Data Fix for Gross Sales = NULL'
+
+----------------------------------------------------------------------------------------------------------------------------------
+-- [START]: Update tblSTRegister.ysnTransctionLog to tblSTRegisterFileConfiguration.ysnActive
+----------------------------------------------------------------------------------------------------------------------------------
+IF EXISTS(
+			SELECT TOP 1 1
+			FROM tblSTRegisterFileConfiguration fc
+			INNER JOIN tblSTRegister r
+				ON fc.intRegisterId = r.intRegisterId
+			WHERE r.strRegisterClass = N'SAPPHIRE/COMMANDER'
+				AND fc.strFileType = N'Inbound'
+				AND fc.strFilePrefix = N'vtransset-tlog'
+				AND ISNULL(r.ysnTransctionLog, 0) != ISNULL(fc.ysnActive, 0)
+		 )
+	BEGIN
+
+		UPDATE r
+			SET r.ysnTransctionLog = ISNULL(fc.ysnActive, 0)
+		FROM tblSTRegisterFileConfiguration fc
+		INNER JOIN tblSTRegister r
+			ON fc.intRegisterId = r.intRegisterId
+		WHERE r.strRegisterClass = N'SAPPHIRE/COMMANDER'
+			AND fc.strFileType = N'Inbound'
+			AND fc.strFilePrefix = N'vtransset-tlog'
+			AND ISNULL(r.ysnTransctionLog, 0) != ISNULL(fc.ysnActive, 0)
+
+	END
+----------------------------------------------------------------------------------------------------------------------------------
+-- [END]: Update tblSTRegister.ysnTransctionLog to tblSTRegisterFileConfiguration.ysnActive
+----------------------------------------------------------------------------------------------------------------------------------
+
 GO
