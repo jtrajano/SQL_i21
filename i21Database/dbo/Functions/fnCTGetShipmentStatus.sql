11944809b0c90986f6ccbf4de8a01d56dd46a7b6
Dan Jalimeo <dan.jalimao@irely.com>
2019-10-18 15:38:11 +0800

    CT-3905

--- i21Database/dbo/Functions/fnCTGetShipmentStatus.sql
+++ i21Database/dbo/Functions/fnCTGetShipmentStatus.sql
@@ -10,9 +10,14 @@ AS
 BEGIN
 	INSERT INTO @returntable	
 	SELECT TOP 1 strShipmentStatus
-    FROM vyuCTShipmentStatus
-    WHERE (intPContractDetailId = @intContractDetailId or intSContractDetailId = @intContractDetailId)
-    AND ((intShipmentType = 2 AND strShipmentStatus <> 'Scheduled') OR intShipmentType = 1)
-    ORDER BY dtmScheduledDate, intLoadDetailId DESC
+	FROM
+	(
+		SELECT ROW_NUMBER() OVER(PARTITION BY intLoadDetailId ORDER BY dtmScheduledDate) AS intNumberId, strShipmentStatus, intPriorityId = CASE WHEN strShipmentStatus = 'Cancelled' THEN 2 ELSE 1 END
+		FROM vyuCTShipmentStatus
+		WHERE (intPContractDetailId = @intContractDetailId or intSContractDetailId = @intContractDetailId)
+		AND ((intShipmentType = 2 AND strShipmentStatus <> 'Scheduled') OR intShipmentType = 1)
+	) tbl
+	WHERE intNumberId = 1
+	ORDER BY intPriorityId ASC
 	RETURN;
 END
