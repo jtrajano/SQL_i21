d7478209d821dea101fd4ade24e3b1ad976fe0de
Kelvin Quinto <kelvin.quinto@irely.com>
2019-10-21 17:26:51 +0800

    AR-11081

--- i21Database/dbo/Stored Procedures/uspCTInvoicePosted.sql
+++ i21Database/dbo/Stored Procedures/uspCTInvoicePosted.sql
@@ -67,6 +67,7 @@ BEGIN TRY
 		, [intOrderUOMId]
 		, [dblQty]
 		, [dblQtyOrdered]
+		, [ysnDestWtGrd]
 		, [intTicketId]
 		, [intLoadDetailId]
 		, [ysnFromReturn]
@@ -78,6 +79,7 @@ BEGIN TRY
 		, [intOrderUOMId]			= ID.[intOrderUOMId]
 		, [dblQty]					= I.[dblQtyShipped]
 		, [dblQtyOrdered]			= CASE WHEN ID.intSalesOrderDetailId IS NOT NULL OR ID.intTicketId IS NOT NULL THEN ID.[dblQtyOrdered] ELSE 0 END
+		, [ysnDestWtGrd]			= CAST(0 AS BIT)
 		, [intTicketId]				= NULL
 		, [intLoadDetailId]			= ID.[intLoadDetailId]
 		, [ysnFromReturn]			= CASE WHEN ISNULL(RI.intInvoiceId, 0) = 0 THEN CAST(0 AS BIT) ELSE CAST(1 AS BIT) END
@@ -92,6 +94,12 @@ BEGIN TRY
 		AND INV.strInvoiceOriginId = I.strInvoiceNumber
 		AND INV.intOriginalInvoiceId = I.intInvoiceId
 	) RI
+	WHERE I.intContractDetailId IS NOT NULL
+	AND (
+		(I.strTransactionType <> 'Credit Memo' AND I.[intInventoryShipmentItemId] IS NULL AND I.[intShipmentPurchaseSalesContractId] IS NULL AND I.[intLoadDetailId] IS  NULL)
+		OR
+		(I.strTransactionType = 'Credit Memo' AND (I.[intInventoryShipmentItemId] IS NOT NULL OR I.[intShipmentPurchaseSalesContractId] IS NOT NULL OR I.[intLoadDetailId] IS NOT NULL))
+	)
 
 	IF NOT EXISTS(SELECT * FROM @tblToProcess)
 	BEGIN
