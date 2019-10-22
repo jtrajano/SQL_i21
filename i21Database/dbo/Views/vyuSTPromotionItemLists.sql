73d1dce36add1e27d1d0796d11b08a6a859b533a
henry_zaragoza <henry.zaragoza@irely.com>
2019-10-22 13:58:55 +0800

    ST-1557 Store Item Movements > Vendor ID is missing

--- i21Database/dbo/Views/vyuSTPromotionItemLists.sql
+++ i21Database/dbo/Views/vyuSTPromotionItemLists.sql
@@ -1,27 +1,29 @@
 ﻿CREATE VIEW vyuSTPromotionItemLists
 AS
 SELECT       
-intKey = CAST(ROW_NUMBER() OVER (ORDER BY UOM.intItemUOMId, IL.intLocationId) AS INT)
-, CL.strLocationName
-, UOM.strUpcCode
-, UOM.strLongUPCCode
-, UOM.intItemUOMId
-, I.strItemNo
-, I.strDescription AS strPumpItemDescription
-, Cat.intCategoryId
-, Cat.strCategoryCode
-, Cat.strDescription AS categoryDesc
-, I.ysnFuelItem
-, IP.dblSalePrice AS dblPrice
-, IL.intFamilyId
-, IL.intClassId
-, Family.strSubcategoryId AS strFamily
-, Class.strSubcategoryId AS strClass
-, CL.intCompanyLocationId
-, I.strLotTracking
-, I.strType
-, I.strStatus
-, ST.intStoreId
+	intKey = CAST(ROW_NUMBER() OVER (ORDER BY UOM.intItemUOMId, IL.intLocationId) AS INT)
+	, CL.strLocationName
+	, CL.intCompanyLocationId
+	, UOM.strUpcCode
+	, UOM.strLongUPCCode
+	, UOM.intItemUOMId
+	, I.strItemNo
+	, I.strDescription AS strPumpItemDescription
+	, I.ysnFuelItem
+	, I.strLotTracking
+	, I.strType
+	, I.strStatus
+	, Cat.intCategoryId
+	, Cat.strCategoryCode
+	, Cat.strDescription AS categoryDesc
+	, IP.dblSalePrice AS dblPrice
+	, IL.intFamilyId
+	, IL.intClassId
+	, IL.intVendorId
+	, vendor.strVendorId AS strVendorID
+	, Family.strSubcategoryId AS strFamily
+	, Class.strSubcategoryId AS strClass
+	, ST.intStoreId
 FROM tblICItemUOM UOM
 JOIN tblICItemLocation IL 
 	ON UOM.intItemId = IL.intItemId 
@@ -31,6 +33,8 @@ LEFT JOIN tblSTSubcategory AS Family
 	ON Family.intSubcategoryId = IL.intFamilyId 
 LEFT JOIN tblSTSubcategory AS Class 
 	ON Class.intSubcategoryId = IL.intClassId 
+LEFT JOIN tblAPVendor vendor
+	ON IL.intVendorId = vendor.intEntityId
 JOIN tblSMCompanyLocation CL 
 	ON CL.intCompanyLocationId = IL.intLocationId 
 JOIN tblICItemPricing IP 
