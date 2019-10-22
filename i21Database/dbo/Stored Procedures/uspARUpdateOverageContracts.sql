f6297a950ab92124e77b977ea93bbb567090fa0c
Kelvin Quinto <kelvin.quinto@irely.com>
2019-10-22 11:35:41 +0800

    AR-11068

--- i21Database/dbo/Stored Procedures/uspARUpdateOverageContracts.sql
+++ i21Database/dbo/Stored Procedures/uspARUpdateOverageContracts.sql
@@ -194,7 +194,11 @@ WHILE EXISTS (SELECT TOP 1 NULL FROM #INVOICEDETAILS)
 			BEGIN
 				UPDATE ID
 				SET dblQtyShipped	= CASE WHEN @dblNetWeight > 0 AND ISNULL(dbo.fnCalculateQtyBetweenUOM(@intScaleUOMId, ID.intItemUOMId, @dblNetWeight), 0) > CTD.dblBalance 
-										   THEN CTD.dblBalance 
+										   THEN 
+												CASE WHEN CTD.dblBalance = CTD.dblOriginalQty AND CTD.dblScheduleQty = CTD.dblOriginalQty
+													 THEN ID.dblQtyOrdered 
+													 ELSE CTD.dblBalance 
+												END
 										   ELSE 
 												CASE WHEN @dblNetWeight > ID.dblQtyOrdered 
 										  	     	 THEN ID.dblQtyOrdered
@@ -202,7 +206,11 @@ WHILE EXISTS (SELECT TOP 1 NULL FROM #INVOICEDETAILS)
 												END
 									  END
 				  , dblUnitQuantity	= CASE WHEN @dblNetWeight > 0 AND ISNULL(dbo.fnCalculateQtyBetweenUOM(@intScaleUOMId, ID.intItemUOMId, @dblNetWeight), 0) > CTD.dblBalance 
-										   THEN CTD.dblBalance 
+										   THEN 
+												CASE WHEN CTD.dblBalance = CTD.dblOriginalQty AND CTD.dblScheduleQty = CTD.dblOriginalQty
+													 THEN ID.dblQtyOrdered 
+													 ELSE CTD.dblBalance 
+												END
 										   ELSE 
 												CASE WHEN @dblNetWeight > ID.dblQtyOrdered 
 										  	     	 THEN ID.dblQtyOrdered
