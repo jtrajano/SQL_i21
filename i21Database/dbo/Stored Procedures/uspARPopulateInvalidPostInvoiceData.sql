9d14561202123a8e5b480bea41e63528565d7415
Kelvin Quinto <kelvin.quinto@irely.com>
2019-10-24 23:43:02 +0800

    AR-11098

--- i21Database/dbo/Stored Procedures/uspARPopulateInvalidPostInvoiceData.sql
+++ i21Database/dbo/Stored Procedures/uspARPopulateInvalidPostInvoiceData.sql
@@ -2172,29 +2172,6 @@ BEGIN
 		,[intItemId]
 		,[strBatchId]
 		,[strPostingError])
-	SELECT 
-		 [intInvoiceId]			= I.[intInvoiceId]
-		,[strInvoiceNumber]		= I.[strInvoiceNumber]		
-		,[strTransactionType]	= I.[strTransactionType]
-		,[intInvoiceDetailId]	= I.[intInvoiceDetailId]
-		,[intItemId]			= I.[intItemId]
-		,[strBatchId]			= I.[strBatchId]
-		,[strPostingError]		= 'You cannot Unpost transactions with Voucher.'
-	FROM
-		#ARPostInvoiceHeader I
-	INNER JOIN tblAPBill A
-		ON A.strVendorOrderNumber = I.strInvoiceNumber
-	WHERE
-		@Recap = @ZeroBit
-
-	INSERT INTO #ARInvalidInvoiceData
-		([intInvoiceId]
-		,[strInvoiceNumber]
-		,[strTransactionType]
-		,[intInvoiceDetailId]
-		,[intItemId]
-		,[strBatchId]
-		,[strPostingError])
 	--Payments from Pay Voucher
 	SELECT
 		 [intInvoiceId]			= I.[intInvoiceId]
