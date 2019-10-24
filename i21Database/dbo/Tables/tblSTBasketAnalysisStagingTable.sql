4bea0674f437cc267ef3c1609b23150d02707448
COMTECH\BPonce <bonn.ponce@irely.com>
2019-10-24 19:48:23 +0800

    ST-1565 ST-1566

---
+++ i21Database/dbo/Tables/tblSTBasketAnalysisStagingTable.sql
@@ -0,0 +1,20 @@
+﻿
+CREATE TABLE [dbo].[tblSTBasketAnalysisStagingTable](
+   [intBasketAnalysisId]	 int IDENTITY(1,1) NOT NULL,
+	[intUserId]				 int NULL,
+	[intItemId]				 int NULL,
+	[intCategoryId]			 int NULL,
+	[strDescription]		 nvarchar(max) NULL,
+	[intRank]				 int NULL,
+	[dblBasketAverage]		 numeric(18, 6) NULL,
+	[intTotalBasket]		 int NULL,
+	[intTotalItem]			 int NULL,
+	[strItemId]				 nvarchar(max) NULL,
+	[strCategoryId]			 nvarchar(max) NULL,
+	[strItemDescription]	 nvarchar(max) NULL,
+	[strCategoryDescription] nvarchar(max) NULL,
+	[strItemUPC]			 nvarchar(max) NULL,
+	CONSTRAINT [PK_tblSTBasketAnalysisStagingTable] PRIMARY KEY CLUSTERED ([intBasketAnalysisId] ASC)
+);
+
+
