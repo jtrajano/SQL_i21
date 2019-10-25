c97a84d779f57f4cb712edf95012bac1aa056446
Matt Purugganan <matt.purugganan@irely.com>
2019-10-25 14:28:41 +0800

    GRN-1774
    Settlement Report is not generated on voucher created from Pricing Basis Contract

--- i21Database/dbo/Stored Procedures/uspGRPostSettleStorage.sql
+++ i21Database/dbo/Stored Procedures/uspGRPostSettleStorage.sql
@@ -2065,6 +2065,10 @@ BEGIN TRY
 	SET ysnPosted = 1
 	WHERE intSettleStorageId = @intParentSettleStorageId
 
+	UPDATE tblGRStorageHistory
+	SET intBillId = @createdVouchersId
+	WHERE intSettleStorageId = @intParentSettleStorageId
+
 	SettleStorage_Exit:
 END TRY
 
