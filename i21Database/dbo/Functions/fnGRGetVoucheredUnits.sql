c82f9ab5e950889cab906f9dc23f19fca16eece0
Mon Gonzales <mon.gonzales@irely.com>
2019-10-25 12:54:26 +0800

    GRN-1733 Cherry Pick

---
+++ i21Database/dbo/Functions/fnGRGetVoucheredUnits.sql
@@ -0,0 +1,12 @@
+﻿CREATE FUNCTION [dbo].[fnGRGetVoucheredUnits]
+(
+	-- Add the parameters for the function here
+	@intSettleStorageId INT
+)
+RETURNS DECIMAL(24,10)
+AS
+BEGIN
+	
+	return (select sum(dblQtyReceived) from tblAPBillDetail where intBillId in (select intBillId from tblAPBill where strVendorOrderNumber = (select strStorageTicket from tblGRSettleStorage where intSettleStorageId = @intSettleStorageId)) and intContractSeq is not null)
+END
+
