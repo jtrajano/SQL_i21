6416cf3a1cb5c495f0d00dbf4846b49df5cb5f95
henry_zaragoza <henry.zaragoza@irely.com>
2019-10-28 21:01:59 +0800

    ST-1554 Adjust all Store Pathing to be from Base + Calculated

--- i21Database/dbo/Stored Procedures/uspSTGetXmlNamespace.sql
+++ i21Database/dbo/Stored Procedures/uspSTGetXmlNamespace.sql
@@ -15,45 +15,65 @@ BEGIN TRY
 	DECLARE @intStoreId AS INT
 			, @strPrefix AS NVARCHAR(20)
 
-	SELECT @intStoreId = ST.intStoreId 
-		   , @strRegisterClass = R.strRegisterClass
-		   , @stri21FolderPath = REPLACE(REPLACE((R.strRegisterInboxPath + '\' + RFC.strFolderPath), '/', '\'), '\\', '\') -- Make sure that there is a \ separator between Inbound folder and xml register folder
-		   , @strFileNamePattern = RFC.strFileNamePattern
-	FROM tblSTStore ST
-	JOIN tblSTRegister R
-		ON ST.intRegisterId = R.intRegisterId
-	JOIN tblSTRegisterFileConfiguration RFC
-		ON R.intRegisterId = RFC.intRegisterId
-	WHERE ST.intStoreNo = @intStoreNo
-	AND RFC.strFileType = 'Inbound'
-	AND RFC.strFilePrefix = @strFilePrefix
+	IF EXISTS(SELECT TOP 1 1 FROM tblSTCompanyPreference)
+		BEGIN
+			-- Base Path
+			SELECT 
+				@stri21FolderPath = strStoreBasePath
+			FROM tblSTCompanyPreference
+
+			-- Register configs
+			SELECT @intStoreId = ST.intStoreId 
+				   , @strRegisterClass = R.strRegisterClass
+				   --, @stri21FolderPath = REPLACE(REPLACE((R.strRegisterInboxPath + '\' + RFC.strFolderPath), '/', '\'), '\\', '\') -- Make sure that there is a \ separator between Inbound folder and xml register folder
+				   , @strFileNamePattern = RFC.strFileNamePattern
+			FROM tblSTStore ST
+			JOIN tblSTRegister R
+				ON ST.intRegisterId = R.intRegisterId
+			JOIN tblSTRegisterFileConfiguration RFC
+				ON R.intRegisterId = RFC.intRegisterId
+			WHERE ST.intStoreNo = @intStoreNo
+				AND RFC.strFileType = 'Inbound'
+				AND RFC.strFilePrefix = @strFilePrefix
 
 
-	SELECT @strXmlNamespace = strDefaultValue 
-	FROM tblSMXMLTagAttribute
-	WHERE intImportFileColumnDetailId = (
-											SELECT intImportFileColumnDetailId 
-											FROM tblSMImportFileColumnDetail
-											WHERE intImportFileHeaderId = (
-																			SELECT TOP 1 intImportFileHeaderId 
-																			FROM tblSTRegisterFileConfiguration
-																			WHERE intRegisterId = (
-																									SELECT intRegisterId 
-																									FROM tblSTStore
-																									WHERE intStoreId = @intStoreId
-																								  )
-																			AND strFileType = 'Inbound'
-																			AND strFilePrefix = @strFilePrefix
-																		  )
-											AND intLevel = 1
-											AND intPosition = 0
-											AND ysnActive = CAST(1 AS BIT)
-										)
-	AND intSequence = 2
-	AND ysnActive = CAST(1 AS BIT)
+			SELECT @strXmlNamespace = strDefaultValue 
+			FROM tblSMXMLTagAttribute
+			WHERE intImportFileColumnDetailId = (
+													SELECT intImportFileColumnDetailId 
+													FROM tblSMImportFileColumnDetail
+													WHERE intImportFileHeaderId = (
+																					SELECT TOP 1 intImportFileHeaderId 
+																					FROM tblSTRegisterFileConfiguration
+																					WHERE intRegisterId = (
+																											SELECT intRegisterId 
+																											FROM tblSTStore
+																											WHERE intStoreId = @intStoreId
+																										  )
+																					AND strFileType = 'Inbound'
+																					AND strFilePrefix = @strFilePrefix
+																				  )
+													AND intLevel = 1
+													AND intPosition = 0
+													AND ysnActive = CAST(1 AS BIT)
+												)
+			AND intSequence = 2
+			AND ysnActive = CAST(1 AS BIT)
 
-	SET @ysnSuccess = CAST(1 AS BIT)
-	SET @strStatusMsg = ''
+			SET @ysnSuccess = CAST(1 AS BIT)
+			SET @strStatusMsg = ''
+		END
+	ELSE
+		BEGIN
+			SET @ysnSuccess = CAST(0 AS BIT)
+			SET @strStatusMsg = 'No base path setup on company preference.'
+
+			SET @strRegisterClass = ''
+			SET @strXmlNamespace = ''
+			SET @stri21FolderPath = ''
+			SET @strFileNamePattern = ''
+		END
+	
 END TRY
 
 BEGIN CATCH
