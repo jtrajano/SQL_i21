cd10762801d58f9424599f562f855fd629d25bb6
Erick <erick.delacruz@irely.com>
2019-10-31 19:13:49 +0800

    AP-8002

--- i21Database/Scripts/Post-Deployment/AP/UpdatePaymentDetailOffset.sql
+++ i21Database/Scripts/Post-Deployment/AP/UpdatePaymentDetailOffset.sql
@@ -9,7 +9,7 @@ FROM tblAPPaymentDetail A
 INNER JOIN tblAPPayment A2
 	ON A.intPaymentId = A2.intPaymentId
 INNER JOIN tblAPBill B
-	ON A.intBillId = B.intBillId
+	ON ISNULL(A.intBillId,A.intOrigBillId) = B.intBillId
 WHERE B.ysnPrepayHasPayment = 1 AND A.ysnOffset = 1
 
 UPDATE A
@@ -40,16 +40,34 @@ WHERE A.ysnOffset = 1 AND A.dblInterest > 0
 --UPDATE ALL VOID TO POSITIVE, SINCE ORIGINAL IS ALREADY NEGATIVE
 UPDATE A
 SET
-	A.dblTotal = A.dblTotal * (CASE WHEN A.dblTotal > 0 THEN -1 ELSE 1 END),
-	A.dblPayment = A.dblPayment * (CASE WHEN A.dblPayment > 0 THEN -1 ELSE 1 END),
-	A.dblAmountDue = A.dblAmountDue * (CASE WHEN A.dblAmountDue > 0 THEN -1 ELSE 1 END),
-	A.dblDiscount = A.dblDiscount * (CASE WHEN A.dblDiscount > 0 THEN -1 ELSE 1 END),
-	A.dblInterest = A.dblInterest * (CASE WHEN A.dblInterest > 0 THEN -1 ELSE 1 END)
+	A.dblTotal = A.dblTotal * (CASE WHEN A.dblTotal < 0 THEN -1 ELSE 1 END),
+	A.dblPayment = A.dblPayment * (CASE WHEN A.dblPayment < 0 THEN -1 ELSE 1 END),
+	A.dblAmountDue = A.dblAmountDue * (CASE WHEN A.dblAmountDue < 0 THEN -1 ELSE 1 END),
+	A.dblDiscount = A.dblDiscount * (CASE WHEN A.dblDiscount < 0 THEN -1 ELSE 1 END),
+	A.dblInterest = A.dblInterest * (CASE WHEN A.dblInterest < 0 THEN -1 ELSE 1 END)
 FROM tblAPPaymentDetail A
 INNER JOIN tblAPPayment A2
 	ON A.intPaymentId = A2.intPaymentId
 INNER JOIN tblAPBill B
-	ON A.intBillId = B.intBillId
+	ON ISNULL(A.intBillId,A.intOrigBillId) = B.intBillId
 INNER JOIN tblCMBankTransaction C
 	ON C.strTransactionId = A2.strPaymentRecordNum
 WHERE C.intBankTransactionTypeId IN (116, 19, 122)
+AND B.intTransactionType IN (3)
+
+UPDATE A
+SET
+	A.dblTotal = A.dblTotal * (CASE WHEN A.dblTotal < 0 THEN -1 ELSE 1 END),
+	A.dblPayment = A.dblPayment * (CASE WHEN A.dblPayment < 0 THEN -1 ELSE 1 END),
+	A.dblAmountDue = A.dblAmountDue * (CASE WHEN A.dblAmountDue < 0 THEN -1 ELSE 1 END),
+	A.dblDiscount = A.dblDiscount * (CASE WHEN A.dblDiscount < 0 THEN -1 ELSE 1 END),
+	A.dblInterest = A.dblInterest * (CASE WHEN A.dblInterest < 0 THEN -1 ELSE 1 END)
+FROM tblAPPaymentDetail A
+INNER JOIN tblAPPayment A2
+	ON A.intPaymentId = A2.intPaymentId
+INNER JOIN tblAPBill B
+	ON ISNULL(A.intBillId,A.intOrigBillId) = B.intBillId
+INNER JOIN tblCMBankTransaction C
+	ON C.strTransactionId = A2.strPaymentRecordNum
+WHERE C.intBankTransactionTypeId IN (116, 19, 122)
+AND B.intTransactionType IN (2) AND A.ysnOffset = 1
