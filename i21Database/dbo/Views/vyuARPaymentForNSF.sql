4afb340b89d0b3294c1107e0533e4bbaf312868d
kelvin_quinto <kelvin.quinto@irely.com>
2019-11-01 11:51:50 +0800

    AR-11115

---
+++ i21Database/dbo/Views/vyuARPaymentForNSF.sql
@@ -0,0 +1,44 @@
+CREATE VIEW [dbo].[vyuARPaymentForNSF]
+AS
+SELECT intTransactionId		= P.intPaymentId
+	 , intEntityCustomerId	= P.intEntityCustomerId
+	 , intCurrencyId		= P.intCurrencyId
+	 , intCompanyLocationId	= P.intLocationId
+	 , intAccountId			= P.intAccountId
+	 , strTransactionNumber	= P.strRecordNumber	 
+	 , strCustomerName		= LTRIM(RTRIM(E.strName))	 
+	 , strPaymentInfo		= P.strPaymentInfo
+	 , strTransactionType	= 'Payment' COLLATE Latin1_General_CI_AS
+	 , dblAmountPaid		= P.dblAmountPaid
+	 , dblUnappliedAmount	= P.dblUnappliedAmount
+	 , dtmDatePaid			= P.dtmDatePaid
+	 , ysnInvoicePrepayment	= P.ysnInvoicePrepayment
+FROM dbo.tblARPayment P WITH (NOLOCK)
+INNER JOIN dbo.tblEMEntity E WITH (NOLOCK) ON P.intEntityCustomerId = E.intEntityId
+WHERE P.ysnProcessedToNSF = 0
+  AND P.ysnPosted = 1
+  AND strPaymentMethod IN ('Check', 'eCheck', 'ACH')
+
+UNION ALL
+
+SELECT intTransactionId		= I.intInvoiceId
+	 , intEntityCustomerId	= I.intEntityCustomerId
+	 , intCurrencyId		= I.intCurrencyId
+	 , intCompanyLocationId	= I.intCompanyLocationId
+	 , intAccountId			= I.intAccountId
+	 , strTransactionNumber	= I.strInvoiceNumber	 
+	 , strCustomerName		= LTRIM(RTRIM(E.strName))	 
+	 , strPaymentInfo		= I.strPaymentInfo
+	 , strTransactionType	= 'Cash' COLLATE Latin1_General_CI_AS
+	 , dblAmountPaid		= I.dblInvoiceTotal
+	 , dblUnappliedAmount	= 0.000000
+	 , dtmDatePaid			= I.dtmPostDate
+	 , ysnInvoicePrepayment	= CAST(0 AS BIT)
+FROM dbo.tblARInvoice I WITH (NOLOCK)
+INNER JOIN dbo.tblEMEntity E WITH (NOLOCK) ON I.intEntityCustomerId = E.intEntityId
+INNER JOIN dbo.tblSMPaymentMethod SM WITH (NOLOCK) ON I.intPaymentMethodId = SM.intPaymentMethodID
+WHERE I.ysnProcessedToNSF = 0
+  AND I.ysnPosted = 1
+  AND I.intPaymentMethodId IS NOT NULL
+  AND I.strTransactionType = 'Cash'
+  AND SM.strPaymentMethod IN ('Check', 'eCheck', 'ACH')
