268f54eaaff0850072371b62f08a8d60d4804875
kelvin_quinto <kelvin.quinto@irely.com>
2019-11-05 18:26:00 +0800

    AR-11114
    (cherry picked from commit 85a41cdc482b66ce9486c7f8605a88b849d25001)
    (cherry picked from commit 6ab8be489719cf90b002ec4db5663cc21651ffd6)

--- i21Database/dbo/Stored Procedures/uspARCustomerAgingAsOfDateReport.sql
+++ i21Database/dbo/Stored Procedures/uspARCustomerAgingAsOfDateReport.sql
@@ -258,7 +258,7 @@ INNER JOIN @tblCompanyLocation CL ON I.intCompanyLocationId = CL.intCompanyLocat
 WHERE ysnPosted = 1
 	AND ysnCancelled = 0	
 	AND strTransactionType <> 'Cash Refund'
-	AND ((I.strType = 'Service Charge' AND (@ysnFromBalanceForward = 0 AND @dtmDateToLocal < CONVERT(DATETIME, FLOOR(CONVERT(DECIMAL(18,6), I.dtmForgiveDate))))) OR (I.strType = 'Service Charge' AND I.ysnForgiven = 0) OR ((I.strType <> 'Service Charge' AND I.ysnForgiven = 1) OR (I.strType <> 'Service Charge' AND I.ysnForgiven = 0)))
+	-- AND ((I.strType = 'Service Charge' AND (@ysnFromBalanceForward = 0 AND @dtmDateToLocal < CONVERT(DATETIME, FLOOR(CONVERT(DECIMAL(18,6), I.dtmForgiveDate))))) OR (I.strType = 'Service Charge' AND I.ysnForgiven = 0) OR ((I.strType <> 'Service Charge' AND I.ysnForgiven = 1) OR (I.strType <> 'Service Charge' AND I.ysnForgiven = 0)))
 	AND I.intAccountId IN (
 		SELECT A.intAccountId
 		FROM dbo.tblGLAccount A WITH (NOLOCK)
@@ -309,6 +309,13 @@ IF (@ysnPrintFromCFLocal = 1)
 											   AND LOWER(IST.strStatementType) = 'invoice'
 		WHERE I.strType = 'CF Tran'
 		  AND I.intInvoiceId IS NULL
+
+		DELETE I 
+		FROM #POSTEDINVOICES I
+		INNER JOIN tblCFTransaction CF ON I.strInvoiceNumber = CF.strTransactionId
+		WHERE I.strType = 'CF Tran'
+		  AND ISNULL(CF.ysnInvoiced, 0) = 1
+		  AND I.dtmPostDate <= @dtmDateToLocal
 	END
 
 --#CASHREFUNDS
