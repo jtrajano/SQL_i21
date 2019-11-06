5ea5e332569eb0eb62e59b0e1d205e972d82dc44
kelvin_quinto <kelvin.quinto@irely.com>
2019-11-06 13:34:20 +0800

    Revert "AR-11109"
    This reverts commit 203d117aebcaa113aed4ef256220eca2e3ed623e.

--- i21Database/dbo/Stored Procedures/uspARPOSMixedTransaction.sql
+++ i21Database/dbo/Stored Procedures/uspARPOSMixedTransaction.sql
@@ -175,7 +175,7 @@ BEGIN
 		,[intItemUOMId]							= NULL
 		,[dblQtyShipped]						= 1.000000
 		,[dblDiscount]							= NULL
-		,[dblPrice]								= ABS(POS.dblDiscount) * -1
+		,[dblPrice]								= POS.dblDiscount * -1
 		,[ysnRefreshPrice]						= 0
 		,[ysnRecomputeTax]						= CASE WHEN ISNULL(POS.ysnTaxExempt,0) = 0 THEN 1 ELSE 0 END
 		,[ysnClearDetailTaxes]					= 1
