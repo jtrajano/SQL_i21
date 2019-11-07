fec58b82188d9d1ecb6a9550dbae9f42c6755a0f
Vinoth Kumar T <vinoth.t@irely.com>
2019-11-07 20:12:12 +0530

    IN-330

---
+++ i21Database/dbo/Stored Procedures/uspIPFreightRateMatrixProcessPreStage.sql
@@ -0,0 +1,68 @@
+﻿CREATE PROCEDURE uspIPFreightRateMatrixProcessPreStage
+AS
+BEGIN TRY
+	SET NOCOUNT ON
+
+	DECLARE @ErrMsg NVARCHAR(MAX)
+	DECLARE @intToCompanyId INT
+	DECLARE @intToEntityId INT
+	DECLARE @intCompanyLocationId INT
+	DECLARE @strToTransactionType NVARCHAR(100)
+		,@intToBookId INT
+		,@intFreightRateMatrixId INT
+		,@strRowState NVARCHAR(50) = NULL
+		,@intUserId INT
+		,@intFreightRateMatrixPreStageId INT
+	DECLARE @tblLGFreightRateMatrixPreStage TABLE (intFreightRateMatrixPreStageId INT)
+
+	INSERT INTO @tblLGFreightRateMatrixPreStage (intFreightRateMatrixPreStageId)
+	SELECT intFreightRateMatrixPreStageId
+	FROM tblLGFreightRateMatrixPreStage
+	WHERE ISNULL(strFeedStatus, '') = ''
+
+	SELECT @intFreightRateMatrixPreStageId = MIN(intFreightRateMatrixPreStageId)
+	FROM @tblLGFreightRateMatrixPreStage
+
+	WHILE @intFreightRateMatrixPreStageId IS NOT NULL
+	BEGIN
+		SELECT @intFreightRateMatrixId = NULL
+			,@strRowState = NULL
+			,@intUserId = NULL
+
+		SELECT @intFreightRateMatrixId = intFreightRateMatrixId
+			,@strRowState = strRowState
+			,@intUserId = intUserId
+		FROM tblLGFreightRateMatrixPreStage
+		WHERE intFreightRateMatrixPreStageId = @intFreightRateMatrixPreStageId
+
+		EXEC uspIPFreightRateMatrixPopulateStgXML @intFreightRateMatrixId
+			,@intToEntityId
+			,@intCompanyLocationId
+			,@strToTransactionType
+			,@intToCompanyId
+			,@strRowState
+			,0
+			,@intToBookId
+			,@intUserId
+
+		UPDATE tblLGFreightRateMatrixPreStage
+		SET strFeedStatus = 'Processed'
+			,strMessage = 'Success'
+		WHERE intFreightRateMatrixPreStageId = @intFreightRateMatrixPreStageId
+
+		SELECT @intFreightRateMatrixPreStageId = MIN(intFreightRateMatrixPreStageId)
+		FROM @tblLGFreightRateMatrixPreStage
+		WHERE intFreightRateMatrixPreStageId > @intFreightRateMatrixPreStageId
+	END
+END TRY
+
+BEGIN CATCH
+	SET @ErrMsg = ERROR_MESSAGE()
+
+	RAISERROR (
+			@ErrMsg
+			,16
+			,1
+			,'WITH NOWAIT'
+			)
+END CATCH
---
+++ i21Database/dbo/Views/vyuIPGetFreightRateMatrix.sql
@@ -0,0 +1,28 @@
+﻿CREATE VIEW vyuIPGetFreightRateMatrix
+AS
+SELECT F.intFreightRateMatrixId
+	,F.intEntityId
+	,F.intType
+	,F.strServiceContractNo
+	,F.dtmDate
+	,F.dtmValidFrom
+	,F.dtmValidTo
+	,F.strOriginPort
+	,F.strDestinationCity
+	,F.intLeadTime
+	,F.dblBasicCost
+	,F.intCurrencyId
+	,F.intContainerTypeId
+	,F.dblFuelCost
+	,F.dblAdditionalCost
+	,F.dblTerminalHandlingCharges
+	,F.dblDestinationDeliveryCharges
+	,F.dblTotalCostPerContainer
+	,F.intConcurrencyId
+	,C.strCurrency
+	,CT.strContainerType
+	,E.strName
+FROM tblLGFreightRateMatrix F
+JOIN tblSMCurrency C ON C.intCurrencyID = F.intCurrencyId
+JOIN tblLGContainerType CT ON CT.intContainerTypeId = F.intContainerTypeId
+JOIN tblEMEntity E ON E.intEntityId = F.intEntityId
