c264b407dfbd8a53a00e8b3df897c5874c456006
Vinoth Kumar T <vinoth.t@irely.com>
2019-11-08 15:20:35 +0530

    IN-323

---
+++ i21Database/dbo/Stored Procedures/uspIPInterCompanyPreStageDailyAveragePrice.sql
@@ -0,0 +1,38 @@
+﻿CREATE PROCEDURE uspIPInterCompanyPreStageDailyAveragePrice @intDailyAveragePriceId INT
+	,@strRowState NVARCHAR(50) = NULL
+	,@intUserId INT = NULL
+AS
+BEGIN TRY
+	SET NOCOUNT ON
+
+	DECLARE @ErrMsg NVARCHAR(MAX)
+
+	DELETE
+	FROM tblRKDailyAveragePricePreStage
+	WHERE ISNULL(strFeedStatus, '') = ''
+		AND intDailyAveragePriceId = @intDailyAveragePriceId
+
+	INSERT INTO tblRKDailyAveragePricePreStage (
+		intDailyAveragePriceId
+		,strRowState
+		,intUserId
+		,strFeedStatus
+		,strMessage
+		)
+	SELECT @intDailyAveragePriceId
+		,@strRowState
+		,@intUserId
+		,''
+		,''
+END TRY
+
+BEGIN CATCH
+	SET @ErrMsg = ERROR_MESSAGE()
+
+	RAISERROR (
+			@ErrMsg
+			,16
+			,1
+			,'WITH NOWAIT'
+			)
+END CATCH
---
+++ i21Database/dbo/Views/vyuIPGetDailyAveragePrice.sql
@@ -0,0 +1,14 @@
+﻿CREATE VIEW vyuIPGetDailyAveragePrice
+AS
+SELECT D.intDailyAveragePriceId
+	,D.strAverageNo
+	,D.dtmDate
+	,D.intBookId
+	,D.intSubBookId
+	,D.ysnPosted
+	,D.intConcurrencyId
+	,B.strBook
+	,SB.strSubBook
+FROM tblRKDailyAveragePrice D
+LEFT JOIN tblCTBook B ON B.intBookId = D.intBookId
+LEFT JOIN tblCTSubBook SB ON SB.intSubBookId = D.intSubBookId
