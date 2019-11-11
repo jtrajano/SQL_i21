0318dd3a017940cec54b0cb45bb6ee2ea1596b6e
lawrence_zabala <lawrence.zabala@irely.com>
2019-11-11 18:28:57 +0800

    RM-2404

---
+++ i21Database/dbo/Stored Procedures/uspRKGetConsolidatedCoverageEntryDetail.sql
@@ -0,0 +1,87 @@
+﻿CREATE PROCEDURE [dbo].[uspRKGetConsolidatedCoverageEntryDetail]
+	@IDs NVARCHAR(MAX)
+
+AS
+
+BEGIN
+	DECLARE @intCoverageEntryId INT
+	DECLARE @CoverageEntryDetails TABLE (intProductTypeId INT
+		, strProductType NVARCHAR(100)
+		, intBookId INT
+		, strBook NVARCHAR(100)
+		, intSubBookId INT
+		, strSubBook NVARCHAR(100)
+		, dblOpenContract NUMERIC(24, 10)
+		, dblInTransit NUMERIC(24, 10)
+		, dblStock NUMERIC(24, 10)
+		, dblTotalPhysical NUMERIC(24, 10)
+		, dblOpenFutures NUMERIC(24, 10)
+		, dblTotalPosition NUMERIC(24, 10)
+		, intMonthsCovered INT
+		, dblAveragePrice NUMERIC(24, 10)
+		, dblOptionsCovered NUMERIC(24, 10)
+		, dblFuturesM2M NUMERIC(24, 10)
+		, dblM2MPlus10 NUMERIC(24, 10)
+		, dblM2MMinus10 NUMERIC(24, 10))
+
+	SELECT DISTINCT intId = Item Collate Latin1_General_CI_AS
+	INTO #tmpIdList
+	FROM [dbo].[fnSplitString](@IDs, ',')
+
+	WHILE EXISTS(SELECT TOP 1 1 FROM #tmpIdList)
+	BEGIN
+		SELECT TOP 1 @intCoverageEntryId = intId FROM #tmpIdList
+
+		INSERT INTO @CoverageEntryDetails
+		SELECT intProductTypeId
+			, strProductType
+			, intBookId
+			, strBook
+			, intSubBookId
+			, strSubBook
+			, dblOpenContract
+			, dblInTransit
+			, dblStock
+			, dblTotalPhysical
+			, dblOpenFutures
+			, dblTotalPosition
+			, intMonthsCovered
+			, dblAveragePrice
+			, dblOptionsCovered
+			, dblFuturesM2M
+			, dblM2MPlus10
+			, dblM2MMinus10
+		FROM vyuRKGetCoverageEntryDetail WHERE intCoverageEntryId = @intCoverageEntryId
+
+		DELETE FROM #tmpIdList WHERE intId = @intCoverageEntryId
+	END
+
+	DROP TABLE #tmpIdList
+
+	SELECT intProductTypeId
+		, strProductType
+		, intBookId
+		, strBook
+		, intSubBookId
+		, strSubBook
+		, dblOpenContract = SUM(ISNULL(dblOpenContract, 0))
+		, dblInTransit = SUM(ISNULL(dblInTransit, 0))
+		, dblStock = SUM(ISNULL(dblStock, 0))
+		, dblTotalPhysical = SUM(ISNULL(dblTotalPhysical, 0))
+		, dblOpenFutures = SUM(ISNULL(dblOpenFutures, 0))
+		, dblTotalPosition = SUM(ISNULL(dblTotalPosition, 0))
+		, intMonthsCovered = SUM(ISNULL(intMonthsCovered, 0))
+		, dblAveragePrice = SUM(ISNULL(dblAveragePrice, 0))
+		, dblOptionsCovered = SUM(ISNULL(dblOptionsCovered, 0))
+		, dblFuturesM2M = SUM(ISNULL(dblFuturesM2M, 0))
+		, dblM2MPlus10 = SUM(ISNULL(dblM2MPlus10, 0))
+		, dblM2MMinus10 = SUM(ISNULL(dblM2MMinus10, 0))
+	FROM @CoverageEntryDetails
+	GROUP BY intProductTypeId
+		, strProductType
+		, intBookId
+		, strBook
+		, intSubBookId
+		, strSubBook
+
+END
--- i21Database/dbo/Stored Procedures/uspRKGetCoverageEntryDetail.sql
+++ i21Database/dbo/Stored Procedures/uspRKGetCoverageEntryDetail.sql
@@ -1,6 +1,5 @@
 ﻿CREATE PROCEDURE [dbo].[uspRKGetCoverageEntryDetail]
-	@BatchName NVARCHAR(50)
-	, @Date DATETIME
+	@Date DATETIME
 	, @CommodityId INT
 	, @UOMType NVARCHAR(50)
 	, @UOMId INT
@@ -11,22 +10,34 @@
 AS
 
 BEGIN
+--DECLARE @Date DATETIME = GETDATE()
+--	, @CommodityId INT = 7
+--	, @UOMType NVARCHAR(50) = 'By Lot'
+--	, @UOMId INT
+--	, @BookId INT = NULL
+--	, @SubBookId INT = NULL
+--	, @Decimal INT = 2
+
 	DECLARE @intUnitMeasureId INT
 	SET @Date = CAST(FLOOR(CAST(@Date AS FLOAT)) AS DATETIME)
 
 	DECLARE @strUnitMeasure NVARCHAR(100)
 
-	SELECT @intUnitMeasureId = c.intUnitMeasureId
-	FROM tblICCommodityUnitMeasure c
-	WHERE c.intCommodityUnitMeasureId = @UOMId	
-
-	SELECT intCommodityAttributeId
-		, strProductType = strDescription
-	FROM tblICCommodityAttribute
-	WHERE ISNULL(intCommodityId, 0) = ISNULL(@CommodityId, 0)
-		AND strType = 'ProductType'
-
-	SELECT intBookId, intSubBookId, intCommodityId, dblContracts = ISNULL([Contracts], 0), dblInTransit = ISNULL([In-Transit], 0), dblStock = ISNULL([Stock], 0), dblFutures = ISNULL([Futures], 0)
+	IF (ISNULL(@UOMId, 0) = 0)
+	BEGIN
+		SELECT TOP 1 @intUnitMeasureId = c.intUnitMeasureId
+		FROM tblICCommodityUnitMeasure c
+		WHERE intCommodityId = @CommodityId
+			AND ysnStockUnit = 1
+	END
+	ELSE
+	BEGIN
+		SELECT TOP 1 @intUnitMeasureId = c.intUnitMeasureId
+		FROM tblICCommodityUnitMeasure c
+		WHERE c.intCommodityUnitMeasureId = @UOMId
+	END
+	
+	SELECT intBookId, intSubBookId, intCommodityId, intProductTypeId, dblContracts = ISNULL([Contracts], 0), dblInTransit = ISNULL([In-Transit], 0), dblStock = ISNULL([Stock], 0), dblFutures = ISNULL([Futures], 0)
 	INTO #tmpBalances
 	FROM (
 		SELECT DISTINCT strType = 'Contracts'
@@ -34,8 +45,10 @@ BEGIN
 			, Detail.intSubBookId
 			, dblBalance = SUM(ISNULL(dbo.[fnCTConvertQuantityToTargetItemUOM](Detail.intItemId, Detail.intUnitMeasureId, @intUnitMeasureId, Detail.dblBalance), 0))
 			, Header.intCommodityId
+			, Item.intProductTypeId
 		FROM tblCTContractDetail Detail
 		JOIN tblCTContractHeader Header ON Header.intContractHeaderId = Detail.intContractHeaderId
+		JOIN tblICItem Item ON Item.intItemId = Detail.intItemId
 		WHERE Detail.intContractStatusId = 1
 			AND Detail.intPricingTypeId = 1
 			AND Detail.dblBalance <> 0
@@ -47,19 +60,23 @@ BEGIN
 		GROUP BY Detail.intBookId
 			, Detail.intSubBookId
 			, Header.intCommodityId
+			, Item.intProductTypeId
 
 		UNION ALL SELECT DISTINCT strType = 'In-Transit'
 			, L.intBookId
 			, L.intSubBookId
 			, dblBalance = SUM(ISNULL(dbo.[fnCTConvertQuantityToTargetItemUOM](LD.intItemId, ItemUOM.intUnitMeasureId, @intUnitMeasureId, LD.dblQuantity), 0))
 			, CH.intCommodityId
+			, Item.intProductTypeId
 		FROM tblLGLoad L
 		JOIN tblLGLoadDetail LD ON L.intLoadId = LD.intLoadId AND L.intPurchaseSale = 1
 		JOIN tblICItemUOM ItemUOM ON ItemUOM.intItemUOMId = LD.intItemUOMId
 		JOIN tblCTContractDetail CD ON CD.intContractDetailId = LD.intPContractDetailId
 		JOIN tblCTContractHeader CH ON CH.intContractHeaderId = CD.intContractHeaderId
+		JOIN tblICItem Item ON Item.intItemId = LD.intItemId
 		WHERE L.ysnPosted = 1
-			AND L.intShipmentStatus = 3
+			AND L.intShipmentStatus <> 10
+			AND L.intShipmentType = 1
 			AND ISNULL(CH.intCommodityId, 0) = ISNULL(@CommodityId, 0)
 			AND CAST(FLOOR(CAST(L.dtmDispatchedDate AS FLOAT)) AS DATETIME) <= @Date
 			AND ISNULL(L.intBookId, 0) = ISNULL(@BookId, 0)
@@ -67,27 +84,32 @@ BEGIN
 		GROUP BY L.intBookId
 			, L.intSubBookId
 			, CH.intCommodityId
+			, Item.intProductTypeId
 
 		UNION ALL SELECT DISTINCT strType = 'Stock'
 			, Lots.intBookId
 			, Lots.intSubBookId
 			, dblBalance = SUM(ISNULL(dbo.[fnCTConvertQuantityToTargetItemUOM](Lots.intItemId, ItemUOM.intUnitMeasureId, @intUnitMeasureId, Lots.dblBalance), 0))
 			, Lots.intCommodityId
+			, Item.intProductTypeId
 		FROM vyuLGPickOpenInventoryLots Lots
 		JOIN tblICItemUOM ItemUOM ON ItemUOM.intItemUOMId = Lots.intItemUOMId
-		WHERE ISNULL(intCommodityId, 0) = ISNULL(@CommodityId, 0)
+		JOIN tblICItem Item ON Item.intItemId = Lots.intItemId
+		WHERE ISNULL(Lots.intCommodityId, 0) = ISNULL(@CommodityId, 0)
 			AND CAST(FLOOR(CAST(dtmEndDate AS FLOAT)) AS DATETIME) <= @Date
 			AND ISNULL(Lots.intBookId, 0) = ISNULL(@BookId, 0)
 			AND ISNULL(Lots.intSubBookId, 0) = ISNULL(@SubBookId, 0)
 		GROUP BY Lots.intBookId
 			, Lots.intSubBookId
 			, Lots.intCommodityId
+			, Item.intProductTypeId
 		
 		UNION ALL SELECT DISTINCT strType = 'Futures'
 			, DAP.intBookId
 			, DAP.intSubBookId
 			, dblBalance = SUM(ISNULL(DAP.dblNoOfLots * FM.dblContractSize, 0))
 			, DAP.intCommodityId
+			, NULL
 		FROM vyuRKGetDailyAveragePriceDetail DAP
 		JOIN tblRKFutureMarket FM ON FM.intFutureMarketId = DAP.intFutureMarketId
 		WHERE intDailyAveragePriceId = (SELECT TOP 1 intDailyAveragePriceId
@@ -106,7 +128,7 @@ BEGIN
 		SUM(dblBalance)
 		FOR strType IN ([Contracts], [In-Transit], [Stock], [Futures])
 	) tblPivot
-
+	
 	SELECT intProductTypeId = ComAtt.intCommodityAttributeId
 		, strProductType = ComAtt.strDescription
 		, Book.intBookId
@@ -120,20 +142,18 @@ BEGIN
 		, dblOpenFutures = Balance.dblFutures
 		, dblTotalPosition = Balance.dblContracts + Balance.dblInTransit + Balance.dblStock + Balance.dblFutures
 		, intMonthsCovered = 0
-		, dblAveragePrice = 0
-		, dblOptionsCovered = 0
-		, dblFuturesM2M = 0
-		, dblM2MPlus10 = 0
-		, dblM2MMinus10 = 0
+		, dblAveragePrice = 0.00
+		, dblOptionsCovered = 0.00
+		, dblFuturesM2M = 0.00
+		, dblM2MPlus10 = 0.00
+		, dblM2MMinus10 = 0.00
 	FROM tblICCommodityAttribute ComAtt
-	LEFT JOIN #tmpBalances Balance ON Balance.intCommodityId = ComAtt.intCommodityId
+	LEFT JOIN #tmpBalances Balance ON Balance.intCommodityId = ComAtt.intCommodityId AND ComAtt.intCommodityAttributeId = Balance.intProductTypeId
 	LEFT JOIN tblCTBook Book ON Book.intBookId = Balance.intBookId
 	LEFT JOIN tblCTSubBook SubBook ON SubBook.intSubBookId = Balance.intSubBookId
 	WHERE ISNULL(ComAtt.intCommodityId, 0) = ISNULL(@CommodityId, 0)
 		AND strType = 'ProductType'
+		AND (Balance.dblContracts IS NOT NULL AND Balance.dblInTransit IS NOT NULL AND Balance.dblStock IS NOT NULL AND Balance.dblFutures IS NOT NULL)
 
 	DROP TABLE #tmpBalances
-
-	
-
 END
