8e3284b85d17a9e5067fcbc8827e59e14d80f19c
kelvin_quinto <kelvin.quinto@irely.com>
2019-11-15 15:29:10 +0800

    AR-11197

--- i21Database/dbo/Stored Procedures/uspARUpdateLineItemLotDetail.sql
+++ i21Database/dbo/Stored Procedures/uspARUpdateLineItemLotDetail.sql
@@ -8,7 +8,15 @@ BEGIN
 	SET NOCOUNT ON
 	SET XACT_ABORT ON
 	SET ANSI_WARNINGS OFF
-					
+
+    DECLARE @tblLotIds TABLE (intLotId INT)
+    
+    INSERT INTO @tblLotIds
+    SELECT IDL.intLotId
+    FROM tblARInvoice I
+    INNER JOIN tblARInvoiceDetail ID ON I.intInvoiceId = ID.intInvoiceId
+    INNER JOIN tblARInvoiceDetailLot IDL ON ID.intInvoiceDetailId = IDL.intInvoiceDetailId
+    WHERE I.intInvoiceId = @InvoiceId
 
     INSERT INTO [dbo].[tblARInvoiceDetailLot]
         ([intInvoiceDetailId]
@@ -38,22 +46,15 @@ BEGIN
         ,[intCreatedByUserId]       = ISIL.[intCreatedByUserId]
         ,[intModifiedByUserId]      = ISIL.[intModifiedByUserId]
         ,[intConcurrencyId]         = ISIL.[intConcurrencyId]
-	FROM
-		tblARInvoiceDetail ARID
-	INNER JOIN
-		tblARInvoice ARI
-			ON ARID.[intInvoiceId] = ARI.[intInvoiceId]
-	INNER JOIN
-		tblICInventoryShipmentItem ISI
-			ON ARID.[intInventoryShipmentItemId] = ISI.[intInventoryShipmentItemId]
-	INNER JOIN
-		tblICInventoryShipmentItemLot ISIL
-			ON ISI.[intInventoryShipmentItemId] = ISIL.[intInventoryShipmentItemId]
-	WHERE 
-		ARI.[intInvoiceId] = @InvoiceId
-		AND ARID.[intInvoiceDetailId] NOT IN (SELECT [intTransactionDetailId] FROM tblARTransactionDetail WHERE [intTransactionId] = @InvoiceId)
-		AND ISNULL(ARID.[intInventoryShipmentItemId], 0) <> 0
-		AND ISNULL(ARID.[intLoadDetailId], 0) = 0		
+	FROM tblARInvoiceDetail ARID
+	INNER JOIN tblARInvoice ARI ON ARID.[intInvoiceId] = ARI.[intInvoiceId]
+	INNER JOIN tblICInventoryShipmentItem ISI ON ARID.[intInventoryShipmentItemId] = ISI.[intInventoryShipmentItemId]
+	INNER JOIN tblICInventoryShipmentItemLot ISIL ON ISI.[intInventoryShipmentItemId] = ISIL.[intInventoryShipmentItemId]
+	WHERE ARI.[intInvoiceId] = @InvoiceId
+      AND ARID.[intInvoiceDetailId] NOT IN (SELECT [intTransactionDetailId] FROM tblARTransactionDetail WHERE [intTransactionId] = @InvoiceId)
+      AND ISNULL(ARID.[intInventoryShipmentItemId], 0) <> 0
+      AND ISNULL(ARID.[intLoadDetailId], 0) = 0
+      AND ISIL.intLotId NOT IN (SELECT intLotId FROM @tblLotIds)
 	 
 END
 
