2d5c734be36d0b4b78c11323df476ce08abcc876
Erick <erick.delacruz@irely.com>
2019-11-19 12:07:01 +0800

    AP-8000

---
+++ i21Database/dbo/Tables/tblAPBalanceDifference.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[tblAPBalanceDifference]
+(
+	[intId] INT IDENTITY (1, 1) NOT NULL PRIMARY KEY,
+	[ysnOrigin] BIT NOT NULL DEFAULT 0,
+	[dblAPBalance] DECIMAL (18, 6) NOT NULL DEFAULT 0,
+	[dblAPGLBalance] DECIMAL (18, 6) NOT NULL DEFAULT 0,
+	[dblDifference] DECIMAL (18, 6) NOT NULL DEFAULT 0,
+	[strTransactionId] NVARCHAR (50) COLLATE Latin1_General_CI_AS NULL
+)
