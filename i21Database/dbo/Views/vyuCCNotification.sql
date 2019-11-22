451da67e3ef8ec29618dad0609c3c4bb7e546288
Crisman Lazaga <cris.lazaga@irely.com>
2019-11-22 18:48:50 +0800

    CCR-400

--- i21Database/dbo/Views/vyuCCNotification.sql
+++ i21Database/dbo/Views/vyuCCNotification.sql
@@ -13,21 +13,21 @@ SELECT ccSiteHeader.intSiteHeaderId,
        ccSiteHeader.strCcdReference,
        ccSiteDetail.dblGross,
        ccSiteDetail.dblFees,
-       ccSiteDetail.dblNet,
-       ccSiteBatchDetail.strBatch,
-       ccSiteBatchDetail.dblGross AS dblBatchGross,
-       ccSiteBatchDetail.dblFees AS dblBatchFees,
-       ccSiteBatchDetail.dblNet AS dblBatchNet
+       ccSiteDetail.dblNet
+    --    ccSiteBatchDetail.strBatch,
+    --    ccSiteBatchDetail.dblGross AS dblBatchGross,
+    --    ccSiteBatchDetail.dblFees AS dblBatchFees,
+    --    ccSiteBatchDetail.dblNet AS dblBatchNet
 FROM dbo.tblCCSiteHeader AS ccSiteHeader
 	INNER JOIN dbo.vyuCCVendor AS ccVendor 
 		ON ccVendor.intVendorDefaultId = ccSiteHeader.intVendorDefaultId
 	INNER JOIN dbo.tblCCSiteDetail AS ccSiteDetail
 		ON ccSiteDetail.intSiteHeaderId = ccSiteHeader.intSiteHeaderId
-	LEFT OUTER JOIN dbo.tblCCSiteBatchDetail AS ccSiteBatchDetail
-		ON ccSiteBatchDetail.intSiteDetailId = ccSiteDetail.intSiteDetailId
+	--LEFT OUTER JOIN dbo.tblCCSiteBatchDetail AS ccSiteBatchDetail
+	--	ON ccSiteBatchDetail.intSiteDetailId = ccSiteDetail.intSiteDetailId
 	LEFT OUTER JOIN dbo.vyuCCSite AS ccSite
 		ON ccSite.intSiteId = ccSiteDetail.intSiteId
 	LEFT OUTER JOIN dbo.vyuCCCustomer AS ccCustomer
-       ON ccCustomer.intCustomerId = ccSite.intCustomerId AND ccCustomer.intDealerSiteId = ccSite.intDealerSiteId 
+       ON ccCustomer.intCustomerId = ccSite.intCustomerId --AND ccCustomer.intDealerSiteId = ccSite.intDealerSiteId 
 
 GO
