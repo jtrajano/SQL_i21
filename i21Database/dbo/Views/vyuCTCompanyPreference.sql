b19009b1f4e271d771d2945db119a7c7db9e5e6d
jayson_ramos <jayson.ramos@irely.com>
2019-11-22 14:31:35 +0800

    CT-4121

--- i21Database/dbo/Views/vyuCTCompanyPreference.sql
+++ i21Database/dbo/Views/vyuCTCompanyPreference.sql
@@ -92,6 +92,7 @@ AS
 			,CP.ysnAllowPartialHedgeLots
 			,CP.ysnDefaultCommodityUOMtoStockHeader
 			,CP.ysnForexRatePriceOptionalOnContract
+			,CP.ysnAllowSignedWhenContractHasAmendment
 	FROM	tblCTCompanyPreference		CP LEFT
 	JOIN	tblICUnitMeasure			U1	ON	U1.intUnitMeasureId			=	CP.intCleanCostUOMId		LEFT
 	JOIN	tblSMCurrency				C1	ON	C1.intCurrencyID			=	CP.intCleanCostCurrencyId	LEFT
