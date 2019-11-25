931f949af88c2cd02a7f79c7fe293fb679799128
Matt Purugganan <matt.purugganan@irely.com>
2019-11-25 11:02:46 +0800

    GRN-1877
    [Settle Storage] Unable to post Settlement of DP storage record from Transfer

--- i21Database/dbo/Functions/fnAPCreateReceiptItemCostAdjustment.sql
+++ i21Database/dbo/Functions/fnAPCreateReceiptItemCostAdjustment.sql
@@ -378,5 +378,66 @@ BEGIN
 		-- WHERE H.dblDiscountPaid != B.dblCost AND B.intCustomerStorageId > 0 AND D.strType != 'Inventory'
 		-- AND H.strSourceType = 'Storage'
 
+		UNION ALL
+		--TRANSFER STORAGE
+		--ITEM
+		SELECT
+			[intItemId]							=	C.intItemId
+			,[intItemLocationId]				=	E.intItemLocationId
+			,[intItemUOMId]						=	F.intItemUOMId
+			,[dtmDate]							=	A.dtmDate
+			,[dblQty] 							=	CASE WHEN B.intWeightUOMId IS NULL THEN B.dblQtyReceived ELSE B.dblNetWeight END 
+			,[dblUOMQty] 						=	F.dblUnitQty
+			,[intCostUOMId]						=	B.intUnitOfMeasureId 
+			,[dblNewValue]						=	CAST(
+													dbo.fnMultiply(
+														--[Voucher Qty]
+														CASE WHEN B.intWeightUOMId IS NULL THEN B.dblQtyReceived ELSE B.dblNetWeight END
+														--[Voucher Cost]
+														,CASE WHEN A.intCurrencyId <> 1 THEN 														
+																dbo.fnCalculateCostBetweenUOM(voucherCostUOM.intItemUOMId,
+																	COALESCE(B.intWeightUOMId, B.intUnitOfMeasureId),
+																	(B.dblCost - (B.dblCost * (ISNULL(B.dblDiscount,0) / 100)))) * ISNULL(B.dblRate, 0) 
+															ELSE 
+																dbo.fnCalculateCostBetweenUOM(voucherCostUOM.intItemUOMId, 
+																	COALESCE(B.intWeightUOMId, B.intUnitOfMeasureId),
+																	(B.dblCost - (B.dblCost * (ISNULL(B.dblDiscount,0) / 100))))
+														END 													
+													)
+													AS DECIMAL(18,2)) 
+													- sh.dblPaidAmount
+			,[intCurrencyId] 					=	1 -- It is always in functional currency. 
+			,[intTransactionId]					=	A.intBillId
+			,[intTransactionDetailId] 			=	B.intBillDetailId
+			,[strTransactionId] 				=	A.strBillId
+			,[intTransactionTypeId] 			=	27
+			,[intLotId] 						=	NULL 
+			,[intSubLocationId] 				=	C.intCompanyLocationSubLocationId
+			,[intStorageLocationId] 			=	C.intStorageLocationId
+			,[ysnIsStorage] 					=	0
+			,[strActualCostId] 					=	NULL
+			,[intSourceTransactionId] 			=	TS.intTransferStorageId
+			,[intSourceTransactionDetailId] 	=	TSS.intTransferStorageSplitId
+			,[strSourceTransactionId] 			=	TS.strTransferStorageTicket
+			,[intFobPointId]					=	NULL
+			,[intInTransitSourceLocationId]		=	NULL
+		FROM tblAPBill A 
+		INNER JOIN tblAPBillDetail B ON A.intBillId = B.intBillId
+		INNER JOIN tblGRSettleStorage C3 ON A.intBillId = C3.intBillId
+		INNER JOIN tblGRSettleStorageTicket C2 ON C3.intSettleStorageId = C2.intSettleStorageId
+		INNER JOIN tblGRCustomerStorage C ON C2.intCustomerStorageId = C.intCustomerStorageId AND B.intCustomerStorageId = C.intCustomerStorageId
+		INNER JOIN tblGRStorageHistory sh 
+			ON sh.intCustomerStorageId = C2.intCustomerStorageId AND sh.intSettleStorageId = C3.intSettleStorageId 
+		INNER JOIN tblGRTransferStorageReference TSR ON TSR.intToCustomerStorageId = C.intCustomerStorageId
+		INNER JOIN tblGRTransferStorage TS ON TS.intTransferStorageId = TSR.intTransferStorageId
+		INNER JOIN tblGRTransferStorageSplit TSS on TSS.intTransferStorageId = TS.intTransferStorageId
+		INNER JOIN tblICItem D ON B.intItemId = D.intItemId
+		INNER JOIN tblICItemLocation E ON C.intCompanyLocationId = E.intLocationId AND E.intItemId = D.intItemId
+		INNER JOIN tblICItemUOM F ON D.intItemId = F.intItemId AND C.intItemUOMId = F.intItemUOMId
+		INNER JOIN tblSCTicket G ON C.intTicketId = G.intTicketId
+		LEFT JOIN tblICItemUOM voucherCostUOM
+			ON voucherCostUOM.intItemUOMId = ISNULL(B.intCostUOMId, B.intUnitOfMeasureId)
+		WHERE sh.dblPaidAmount != B.dblCost AND B.intCustomerStorageId > 0 AND D.strType = 'Inventory'
+
 	RETURN;
 END
