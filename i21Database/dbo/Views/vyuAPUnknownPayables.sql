297d75bd29f331b1bdbe597548947ab9a5fa0b18
Erick <erick.delacruz@irely.com>
2019-11-27 15:04:12 +0800

    AP-8000

---
+++ i21Database/dbo/Views/vyuAPUnknownPayables.sql
@@ -0,0 +1,18 @@
+﻿CREATE VIEW [dbo].[vyuAPUnknownPayables]
+AS 
+
+SELECT 
+	A.intGLDetailId,
+	A.strTransactionId,
+	A.dtmDate,
+	A.dtmDateEntered,
+	A.dblDebit,
+	A.dblCredit,
+	strUserId = C.strUserName
+FROM tblGLDetail A
+INNER JOIN vyuGLAccountDetail B ON A.intAccountId = B.intAccountId
+LEFT JOIN tblEMEntityCredential C ON A.intEntityId = C.intEntityId
+WHERE 
+	B.intAccountCategoryId IN (1, 53)
+AND A.ysnIsUnposted = 0
+AND A.strModuleName NOT IN ('Accounts Payable', 'Accounts Receivable')
