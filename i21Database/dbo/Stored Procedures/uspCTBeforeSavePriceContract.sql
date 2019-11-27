29b15833da747a94fbe25e45c0b55dff7aaaded5
Axel Casauran <axel.casauran@irely.com>
2019-11-27 14:23:47 +0800

    CT-4136
    (cherry picked from commit 776a3229f9babc539012cdeae5abe9bbe03aa6b1)

--- i21Database/dbo/Stored Procedures/uspCTBeforeSavePriceContract.sql
+++ i21Database/dbo/Stored Procedures/uspCTBeforeSavePriceContract.sql
@@ -22,18 +22,18 @@ BEGIN TRY
 			@strAction					NVARCHAR(50) = '',
 			@intFutOptTransactionHeaderId INT = NULL
 
-	IF @strXML = 'Delete'
-	BEGIN
-		IF EXISTS(SELECT TOP 1 1 FROM tblAPBill WHERE intBillId IN (SELECT intBillId FROM tblAPBillDetail WHERE intContractDetailId IN (SELECT intContractDetailId FROM tblCTPriceFixation WHERE intPriceContractId = @intPriceContractId)) AND ysnPosted = 1)
-		BEGIN
+	--IF @strXML = 'Delete'
+	--BEGIN
+	--	IF EXISTS(SELECT TOP 1 1 FROM tblAPBill WHERE intBillId IN (SELECT intBillId FROM tblAPBillDetail WHERE intContractDetailId IN (SELECT intContractDetailId FROM tblCTPriceFixation WHERE intPriceContractId = @intPriceContractId)) AND ysnPosted = 1)
+	--	BEGIN
 
-			DECLARE @TransactionID NVARCHAR(MAX)
-			SELECT @TransactionID = COALESCE(@TransactionID + ', ', '') + strBillId FROM tblAPBill WHERE intBillId IN (SELECT intBillId FROM tblAPBillDetail WHERE intContractDetailId IN (SELECT intContractDetailId FROM tblCTPriceFixation WHERE intPriceContractId = @intPriceContractId)) AND ysnPosted = 1
+	--		DECLARE @TransactionID NVARCHAR(MAX)
+	--		SELECT @TransactionID = COALESCE(@TransactionID + ', ', '') + strBillId FROM tblAPBill WHERE intBillId IN (SELECT intBillId FROM tblAPBillDetail WHERE intContractDetailId IN (SELECT intContractDetailId FROM tblCTPriceFixation WHERE intPriceContractId = @intPriceContractId)) AND ysnPosted = 1
 			
-			SET @ErrMsg = 'Cannot delete pricing as following Invoice/Vouchers are available. ' + @TransactionID + '. Unpost those Invoice/Voucher to continue delete the price.'
-			RAISERROR(@ErrMsg,16,1)
-		END
-	END
+	--		SET @ErrMsg = 'Cannot delete pricing as following Invoice/Vouchers are available. ' + @TransactionID + '. Unpost those Invoice/Voucher to continue delete the price.'
+	--		RAISERROR(@ErrMsg,16,1)
+	--	END
+	--END
 
 
 	IF @strXML = 'Delete'
@@ -105,7 +105,7 @@ BEGIN TRY
 		
 		IF @strRowState = 'Delete'
 		BEGIN
-			EXEC uspCTValidatePriceFixationDetailUpdateDelete @intPriceFixationId = @intPriceFixationId
+			--EXEC uspCTValidatePriceFixationDetailUpdateDelete @intPriceFixationId = @intPriceFixationId
 			EXEC uspCTPriceFixationDetailDelete @intPriceFixationId = @intPriceFixationId, @intUserId = @intUserId
 		END
 
@@ -156,7 +156,7 @@ BEGIN TRY
 		
 		IF @strRowState = 'Delete'
 		BEGIN
-			EXEC uspCTValidatePriceFixationDetailUpdateDelete @intPriceFixationDetailId = @intPriceFixationDetailId
+			--EXEC uspCTValidatePriceFixationDetailUpdateDelete @intPriceFixationDetailId = @intPriceFixationDetailId
 			EXEC uspCTPriceFixationDetailDelete @intPriceFixationDetailId = @intPriceFixationDetailId, @intUserId = @intUserId
 		END
 
@@ -189,7 +189,7 @@ BEGIN TRY
 		
 		IF @strRowState = 'Delete'
 		BEGIN
-			EXEC uspCTValidatePriceFixationDetailUpdateDelete @intPriceFixationTicketId = @intPriceFixationTicketId
+			--EXEC uspCTValidatePriceFixationDetailUpdateDelete @intPriceFixationTicketId = @intPriceFixationTicketId
 			EXEC uspCTPriceFixationDetailDelete @intPriceFixationTicketId = @intPriceFixationTicketId, @intUserId = @intUserId
 		END
 				
