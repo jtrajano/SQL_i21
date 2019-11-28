25e63a4bc9348cfd7b2ac75ff838ee5992516a9d
kenneth <kenneth.lagunero@irely.com>
2019-11-28 16:21:48 +0800

    SC-3134

--- i21Database/dbo/Tables/tblSCCertificateOfAnalysis.sql
+++ i21Database/dbo/Tables/tblSCCertificateOfAnalysis.sql
@@ -3,6 +3,7 @@
 	[intCertificateOfAnalysisId] INT NOT NULL IDENTITY, 
     [strCertificate] NVARCHAR(200) COLLATE Latin1_General_CI_AS NULL,
     [intConcurrencyId] INT NOT NULL DEFAULT 0, 
+    [intItemId] INT NOT NULL, 
     CONSTRAINT [PK_tblSCCertificateOfAnalysis_intCertificateOfAnalysisId] PRIMARY KEY ([intCertificateOfAnalysisId]) ,
 )
 
