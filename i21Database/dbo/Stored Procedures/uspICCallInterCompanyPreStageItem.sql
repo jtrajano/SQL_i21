1698d5d16949b7690789b794db00d50b863c3cd8
Feb Montefrio <feb.montefrio@irely.com>
2019-12-02 18:08:32 +0800

    [B] IC-8066

---
+++ i21Database/dbo/Stored Procedures/uspICCallInterCompanyPreStageItem.sql
@@ -0,0 +1,31 @@
+﻿alter PROCEDURE uspICCallInterCompanyPreStageItem 
+	@strItemNo NVARCHAR(50) 
+	,@strRowState NVARCHAR(50)
+	,@intUserId INT
+AS
+SET NOCOUNT ON
+
+DECLARE @intItemId AS INT
+DECLARE @ErrMsg NVARCHAR(MAX)
+
+SELECT TOP 1 @intItemId = intItemId FROM tblICItem i WHERE i.strItemNo = @strItemNo
+
+IF @intItemId IS NOT NULL 
+BEGIN
+	BEGIN TRY
+		EXEC uspIPInterCompanyPreStageItem	
+			@intItemId 
+			,@strRowState 
+			,@intUserId 
+	END TRY
+	BEGIN CATCH		
+		SET @ErrMsg = ERROR_MESSAGE()
+
+		RAISERROR (
+				@ErrMsg
+				,16
+				,1
+				,'WITH NOWAIT'
+				)
+	END CATCH
+END
