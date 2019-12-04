3cc2b1a09ae9af68f3be2080795b470684462081
Wendell Wayne Estrada <wendell.estrada@irely.com>
2019-12-04 10:36:10 +0800

    [P] IC-8050 HDTN-238466 - Add Editable Storage Unit No (ie. Bin Loc Origin) in the Update Reorder Point

--- i21Database/dbo/Stored Procedures/uspICUpdateReorderPointFromStaging.sql
+++ i21Database/dbo/Stored Procedures/uspICUpdateReorderPointFromStaging.sql
@@ -5,6 +5,7 @@ SET	  il.dblLeadTime = rp.dblLeadTime
 	, il.dblMinOrder = rp.dblMinOrder
 	, il.dblReorderPoint = rp.dblReorderPoint
 	, il.dblSuggestedQty = rp.dblSuggestedQty
+	, il.strStorageUnitNo = NULLIF(rp.strStorageUnitNo '')
 FROM tblICItemLocation il
 	INNER JOIN tblICStagingReorderPoint rp ON rp.intItemLocationId = il.intItemLocationId
 
