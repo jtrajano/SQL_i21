992689e9440ea59c63b7d4930136768f9a459853
Premkumar S <prem.selladurai@irely.com>
2019-12-05 15:11:06 +0530

    IN-321

---
+++ i21Database/dbo/Stored Procedures/uspMFDemandTransferStgXML.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[uspMFDemandTransferStgXML]
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	SELECT *
+	FROM tblMFDemandStage
+	WHERE ISNULL(strFeedStatus, '') = ''
+
+	UPDATE tblMFDemandStage
+	SET strFeedStatus = 'Awt Ack'
+	WHERE ISNULL(strFeedStatus, '') = ''
+END
+
