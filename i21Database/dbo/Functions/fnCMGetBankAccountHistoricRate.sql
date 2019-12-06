0fdab602db684f3ae64324f68d55e4449c093662
Jeff Trajano <jeffrey.trajano@irely.com>
2019-12-06 13:39:51 +0800

    GL-7414 Multi currency - Fix gain loss calculation for bank transfers

--- i21Database/dbo/Functions/fnCMGetBankAccountHistoricRate.sql
+++ i21Database/dbo/Functions/fnCMGetBankAccountHistoricRate.sql
@@ -11,25 +11,18 @@ BEGIN
 DECLARE @result DECIMAL(18,6)
 DECLARE @bankBalance decimal(18,6)
 DECLARE @glBalance decimal(18,6)
+DECLARE @intGLAccountId INT
+DECLARE @intCurrencyId INT
 
+SELECT TOP 1 @intGLAccountId =intGLAccountId, @intCurrencyId = intCurrencyId
+FROM tblCMBankAccount where intBankAccountId = @intBankAccountId
 
-SELECT @glBalance =SUM(ABS(ISNULL(dblDebit,0.00) - ISNULL(dblCredit,0.00)))
-FROM	[dbo].[tblGLDetail] INNER JOIN [dbo].[tblCMBankAccount]
-			ON [dbo].[tblGLDetail].intAccountId = [dbo].[tblCMBankAccount].intGLAccountId
-WHERE	tblCMBankAccount.intBankAccountId = @intBankAccountId
-		AND CAST(FLOOR(CAST(tblGLDetail.dtmDate AS FLOAT)) AS DATETIME) <= CAST(FLOOR(CAST(ISNULL(@dtmDate, tblGLDetail.dtmDate) AS FLOAT)) AS DATETIME)
-		AND ysnIsUnposted = 0
-
-SELECT @bankBalance =SUM(ABS(ISNULL(dblAmount,0.00))) frOm tblCMBankTransaction 
+SELECT  @result= AVG(dblExchangeRate) FROM tblCMBankTransaction 
 WHERE intBankAccountId = @intBankAccountId
-AND CAST(FLOOR(CAST(dtmDate AS FLOAT)) AS DATETIME) <= CAST(FLOOR(CAST(ISNULL(@dtmDate, dtmDate) AS FLOAT)) AS DATETIME)
-AND ysnPosted = 1
+AND CAST(FLOOR(CAST(dtmDate AS FLOAT)) AS DATETIME) <= CAST(FLOOR(CAST(@dtmDate AS FLOAT)) AS DATETIME)
+AND ysnPosted =1
 
 
-IF @glBalance = 0 RETURN -1
-IF @bankBalance = 0 RETURN -2
-
-SELECT @result = @glBalance/@bankBalance
 RETURN @result
 END
 
