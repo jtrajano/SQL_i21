d48a657b3a5806fb76964dd4ee59bee006e082f6
Vinoth Kumar T <vinoth.t@irely.com>
2019-12-10 21:30:42 +0530

    MFG-3867

---
+++ i21Database/dbo/Views/vyuMFGetDemandItemList.sql
@@ -0,0 +1,7 @@
+﻿CREATE VIEW vyuMFGetDemandItemList
+AS
+SELECT DISTINCT I.intItemId
+	,I.strItemNo
+	,RV.intInvPlngReportMasterID
+FROM tblCTInvPlngReportAttributeValue RV
+JOIN tblICItem I ON I.intItemId = RV.intItemId
