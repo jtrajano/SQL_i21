ef9bd855cc218668b71219a9549a883c1692275f
COMTECH\BPonce <bonn.ponce@irely.com>
2019-12-11 12:33:29 +0800

    CF-2452

---
+++ i21Database/dbo/Stored Procedures/uspCFDeleteEncodeCardData.sql
@@ -0,0 +1,30 @@
+﻿CREATE PROCEDURE [dbo].[uspCFDeleteEncodeCardData]
+	 @userId INT
+AS
+BEGIN
+	
+	BEGIN TRY
+		BEGIN TRANSACTION
+	
+		DELETE FROM tblCFEncodeCard WHERE intEncodeCardId IN (SELECT intEncodeCardId FROM tblCFEncodeCardStagingTable WHERE ISNULL(intUserId,0) = 0 OR intUserId = @userId)
+		DELETE FROM tblCFEncodeCardStagingTable WHERE ISNULL(intUserId,0) = 0 OR intUserId = @userId
+		DELETE FROM tblCFEncodingPrinterSoftware 
+
+		COMMIT TRANSACTION
+
+		SELECT 
+		 ysnSuccess = CAST(1 AS BIT)
+		,strMessage = ''
+
+	END TRY
+	BEGIN CATCH
+
+		ROLLBACK TRANSACTION
+
+		SELECT 
+		 ysnSuccess = CAST(0 AS BIT)
+		,strMessage = ERROR_MESSAGE()
+
+	END CATCH
+
+END
