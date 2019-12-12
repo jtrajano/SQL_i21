4ddeef927636ab4840d75efe447c9881c0ddea0b
Premkumar S <prem.selladurai@irely.com>
2019-12-12 20:07:54 +0530

    MFG-3867

--- i21Database/dbo/Tables/tblMFInvPlngSummaryBatch.sql
+++ i21Database/dbo/Tables/tblMFInvPlngSummaryBatch.sql
@@ -2,6 +2,8 @@
 	intInvPlngSummaryBatchId INT NOT NULL IDENTITY
 	,intInvPlngSummaryId INT
 	,strBatch NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL
+	,intInvPlngReportMasterID int
 	,CONSTRAINT PK_tblMFInvPlngSummaryBatch PRIMARY KEY (intInvPlngSummaryBatchId)
 	,CONSTRAINT FK_tblMFInvPlngSummaryBatch_tblMFInvPlngSummary FOREIGN KEY (intInvPlngSummaryId) REFERENCES tblMFInvPlngSummary(intInvPlngSummaryId) ON DELETE CASCADE
+	,CONSTRAINT FK_tblMFInvPlngSummaryBatch_tblCTInvPlngReportMaster FOREIGN KEY (intInvPlngReportMasterID) REFERENCES tblCTInvPlngReportMaster(intInvPlngReportMasterID) 
 	)
