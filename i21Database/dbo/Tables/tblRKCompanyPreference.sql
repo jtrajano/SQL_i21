cc84e32a8bb76de76829ba08644c20b794c08645
lawrence_zabala <lawrence.zabala@irely.com>
2019-12-16 17:57:33 +0800

    RM-3464

--- i21Database/dbo/Tables/tblRKCompanyPreference.sql
+++ i21Database/dbo/Tables/tblRKCompanyPreference.sql
@@ -70,6 +70,7 @@
 	[ysnIncludeDerivatives] BIT NULL DEFAULT((1)),
 	[ysnUseBoardMonth] BIT NULL DEFAULT((0)),
 	[ysnAllowEditAvgLongPrice] BIT NULL DEFAULT((0)),
+	[ysnImposeReversalTransaction] BIT NULL DEFAULT((0)),
     CONSTRAINT [PK_tblRKCompanyPreference_intCompanyPreferenceId] PRIMARY KEY ([intCompanyPreferenceId]), 
 	CONSTRAINT [FK_tblRKCompanyPreference_tblSMCurrency_intCurrencyId] FOREIGN KEY([intCurrencyId])REFERENCES [dbo].[tblSMCurrency] ([intCurrencyID]),
 	CONSTRAINT [FK_tblRKCompanyPreference_tblRKInterfaceSystem_intCurrencyId] FOREIGN KEY([intInterfaceSystemId])REFERENCES [dbo].[tblRKInterfaceSystem] ([intInterfaceSystemId]),
