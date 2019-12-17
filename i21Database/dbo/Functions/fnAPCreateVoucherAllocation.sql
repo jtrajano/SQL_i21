bee5e6ccda4a25e410ca462a291f02bcf5c1b6ec
csEDelaCruz <EDelaCruz@COMTECH.COM>
2019-12-17 20:17:37 +0800

    AP-8068

---
+++ i21Database/dbo/Functions/fnAPCreateVoucherAllocation.sql
@@ -0,0 +1,25 @@
+﻿CREATE FUNCTION [dbo].[fnAPCreateVoucherAllocation]
+(
+	@allocationId INT,
+	@amount DECIMAL(18,2),
+	@units DECIMAL(18,2)
+)
+RETURNS @returntable TABLE
+(
+	intGLAllocationId INT,
+	intAccountId INT,
+	dblTotal DECIMAL(18,2),
+	dblUnits DECIMAL(18,2)
+)
+AS
+BEGIN
+	INSERT @returntable
+	SELECT
+		@allocationId,
+		A.intAccountId,
+		CAST(@amount * (A.dblPercentage / 100) AS DECIMAL(18,2)),
+		CAST(@units * (A.dblPercentage / 100) AS DECIMAL(18,2))
+	FROM tblGLAccountReallocationDetail A
+	WHERE A.intAccountReallocationId = @allocationId
+	RETURN;
+END
