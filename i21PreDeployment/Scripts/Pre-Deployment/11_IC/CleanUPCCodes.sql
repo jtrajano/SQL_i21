48ea88c882d5aa43bfe0d9658b58976a5d007822
Feb Montefrio <feb.montefrio@irely.com>
2019-12-18 08:42:20 +0800

    [B] IC-8109

--- i21PreDeployment/Scripts/Pre-Deployment/11_IC/CleanUPCCodes.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/11_IC/CleanUPCCodes.sql
@@ -7,24 +7,28 @@ BEGIN
 	FROM INFORMATION_SCHEMA.COLUMNS
 	WHERE [TABLE_NAME] = 'tblICItemUOM' AND [COLUMN_NAME] = 'strUpcCode')
 	BEGIN
+		EXEC ('
 		UPDATE tblICItemUOM set strUpcCode = NULL 
-		WHERE strUpcCode = '' OR strUpcCode = '0' OR
+		WHERE strUpcCode = '''' OR strUpcCode = ''0'' OR
 			strUpcCode IN (SELECT strUpcCode
 			FROM tblICItemUOM
 			GROUP BY strUpcCode
-			HAVING (COUNT(strUpcCode) > 1))
+			HAVING (COUNT(strUpcCode) > 1))		
+		')
 	END
 	IF EXISTS(SELECT TOP 1
 		1
 	FROM INFORMATION_SCHEMA.COLUMNS
 	WHERE [TABLE_NAME] = 'tblICItemUOM' AND [COLUMN_NAME] = 'strLongUPCCode')
 	BEGIN
+		EXEC ('
 		UPDATE tblICItemUOM set strLongUPCCode = NULL 
-		WHERE strLongUPCCode = '' OR strLongUPCCode = '0' OR
+		WHERE strLongUPCCode = '''' OR strLongUPCCode = ''0'' OR
 			strLongUPCCode IN (SELECT strLongUPCCode
 			FROM tblICItemUOM
 			GROUP BY strLongUPCCode
 			HAVING (COUNT(strLongUPCCode) > 1))
+		')
 	END
 END
 GO
