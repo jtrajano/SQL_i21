ddd131e1b0228dfd3a60ae5675346d11c7554379
Kevin Estrada <kevin.estrada@irely.com>
2019-12-19 15:28:05 +0800

    SM-4876

--- i21Database/Scripts/Post-Deployment/VersionUpdate.sql
+++ i21Database/Scripts/Post-Deployment/VersionUpdate.sql
@@ -2,7 +2,7 @@
 	PRINT N'BEGIN INSERT VERSION UPDATE'
 GO
 	INSERT INTO tblSMBuildNumber (strVersionNo, dtmLastUpdate)
-	SELECT '19.2', getdate()
+	SELECT '20.1', getdate()
 GO
 	PRINT N'END INSERT VERSION UPDATE'
 GO
