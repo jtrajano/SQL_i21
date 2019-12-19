98ee85b152aeeb9fac14b8a125be7614f4fda392
kenneth <kenneth.lagunero@irely.com>
2019-12-19 21:04:33 +0800

    SC-3303

---
+++ i21Database/dbo/Tables/tblSCDeliverySheetContractAdjustment.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[tblSCDeliverySheetContractAdjustment]
+(
+	[intDeliverySheetContractAdjustmentId] INT NOT NULL IDENTITY, 
+	[intDeliverySheetId] INT NULL, 
+    [intContractDetailId] INT NULL, 
+    [intEntityId] INT NULL, 
+    [dblQuantity] DECIMAL(18,6) NULL,
+	[intItemUOMId] INT NOT NULL,
+	[intConcurrencyId] INT NOT NULL DEFAULT ((0)),
+	CONSTRAINT [PK_tblSCDeliverySheetContractAdjustment_intDeliverySheetContractAdjustmentId] PRIMARY KEY ([intDeliverySheetContractAdjustmentId]), 
+   
+)
