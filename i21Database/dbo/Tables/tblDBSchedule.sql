ac2a7dfecd9d15f8b4549dbd6b5bdf011fd67b81
COMTECH\JMendones <jesmar.mendones@irely.com>
2019-12-19 17:44:49 +0800

    DASH-2394

---
+++ i21Database/dbo/Tables/tblDBSchedule.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE [dbo].[tblDBSchedule]
+(
+	[intScheduleId]				INT NOT NULL PRIMARY KEY IDENTITY,
+	[strDescription]			[nvarchar](500) COLLATE Latin1_General_CI_AS NOT NULL,
+	[strFrequency]				[nvarchar](500) COLLATE Latin1_General_CI_AS NULL,
+	[ysnMonday]					[bit] NULL,
+	[ysnTuesday]				[bit] NULL,
+	[ysnWednesday]				[bit] NULL,
+	[ysnThursday]				[bit] NULL,
+	[ysnFriday]					[bit] NULL,
+	[ysnSaturday]				[bit] NULL,
+	[ysnSunday]					[bit] NULL,
+	[intDayOfMonth]				[int] NULL,
+	[dtmRunTime]				DATE NULL,
+	[intConcurrencyId]			[int] DEFAULT 1,
+
+	CONSTRAINT [UC_tblDBSchedule] UNIQUE (strDescription)
+)
+GO
+
+CREATE INDEX [IX_tblDBSchedule_intScheduleId] ON [dbo].[tblDBSchedule] ([intScheduleId])
+GO
