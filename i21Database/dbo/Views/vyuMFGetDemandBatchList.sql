0e6d749ca0e3542e397c8b434d783a817f399026
Vinoth Kumar T <vinoth.t@irely.com>
2019-12-20 23:07:36 +0530

    MFG-3867

--- i21Database/dbo/Views/vyuMFGetDemandBatchList.sql
+++ i21Database/dbo/Views/vyuMFGetDemandBatchList.sql
@@ -7,5 +7,10 @@ SELECT RM.intInvPlngReportMasterID
 	,RM.intBookId
 	,RM.intSubBookId
 	,DATEDIFF(d, RM.dtmPostDate, GETDATE()) AS intDiff
+	,B.strBook
+	,SB.strSubBook
+	,ISNULL(B.strBook, '') + ' - ' + ISNULL(SB.strSubBook, '') AS strDisplay
 FROM tblCTInvPlngReportMaster RM
+LEFT JOIN tblCTBook B ON B.intBookId = RM.intBookId
+LEFT JOIN tblCTSubBook SB ON SB.intSubBookId = RM.intSubBookId
 WHERE IsNULL(ysnPost, 0) = 1
