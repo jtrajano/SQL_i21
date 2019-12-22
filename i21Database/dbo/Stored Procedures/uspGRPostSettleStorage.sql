f48abe2dc3628bac119f76e3ae8f74168df91393
Mon Gonzales <Mon.Gonzales@irely.com>
2019-12-23 07:13:55 +0800

    GRN-1929 - Settle Storage to Unpriced Basis Contract

--- i21Database/dbo/Stored Procedures/uspGRPostSettleStorage.sql
+++ i21Database/dbo/Stored Procedures/uspGRPostSettleStorage.sql
@@ -2932,7 +2932,7 @@ BEGIN TRY
 							update @avqty set ysnApplied = 1, intBillDetailId = @cur_bid where id = @cur_id
 						end
 
-						
+						EXEC [uspAPUpdateVoucherDetailTax] @detailCreated
 
 					end
 
