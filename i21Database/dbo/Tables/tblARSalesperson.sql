ed278c24cf928c0d855d949a70f39c438414ecb7
Randy Sagum <randy.sagum@irely.com>
2019-12-23 13:04:04 +0800

    AR-11278

--- i21Database/dbo/Tables/tblARSalesperson.sql
+++ i21Database/dbo/Tables/tblARSalesperson.sql
@@ -38,6 +38,13 @@
     --CONSTRAINT [UQ_tblARSalesperson_strSalespersonId] UNIQUE NONCLUSTERED ([strSalespersonId] ASC)
 
 );
+GO
+
+CREATE UNIQUE NONCLUSTERED INDEX [IX_tblARSalesperson_strDriverNumber]
+ ON [dbo].[tblARSalesperson]([strDriverNumber])
+ Where [strDriverNumber] IS NOT NULL AND [strDriverNumber] != ''; 
+GO
+ 
 
 
 
