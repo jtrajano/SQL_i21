13a2d940a115964e63ffd6961c647eaae3cf13bf
csEDelaCruz <EDelaCruz@COMTECH.COM>
2019-12-26 16:42:50 +0800

    AP-8068

--- i21Database/dbo/Functions/fnAPCreateVoucherAllocation.sql
+++ i21Database/dbo/Functions/fnAPCreateVoucherAllocation.sql
@@ -8,7 +8,8 @@ RETURNS @returntable TABLE
 (
 	intGLAllocationId INT,
 	intAccountId INT,
-	dblTotal DECIMAL(18,2),
+	strAccountId NVARCHAR(50),
+	dblTotal DECIMAL(18,6),
 	dblUnits DECIMAL(18,2)
 )
 AS
@@ -17,9 +18,11 @@ BEGIN
 	SELECT
 		@allocationId,
 		A.intAccountId,
+		B.strAccountId,
 		CAST(@amount * (A.dblPercentage / 100) AS DECIMAL(18,2)),
 		CAST(@units * (A.dblPercentage / 100) AS DECIMAL(18,2))
 	FROM tblGLAccountReallocationDetail A
+	INNER JOIN tblGLAccount B ON A.intAccountId = B.intAccountId
 	WHERE A.intAccountReallocationId = @allocationId
 	RETURN;
 END
