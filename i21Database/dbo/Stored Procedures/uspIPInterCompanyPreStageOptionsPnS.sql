7c57f42c75c12081e4e6ab81b253d6916c418632
Vinoth Kumar T <vinoth.t@irely.com>
2019-12-27 19:30:06 +0530

    IN-325

---
+++ i21Database/dbo/Stored Procedures/uspIPInterCompanyPreStageOptionsPnS.sql
@@ -0,0 +1,38 @@
+﻿CREATE PROCEDURE uspIPInterCompanyPreStageOptionsPnS @intOptionsMatchPnSHeaderId INT
+	,@strRowState NVARCHAR(50) = NULL
+	,@intUserId INT = NULL
+AS
+BEGIN TRY
+	SET NOCOUNT ON
+
+	DECLARE @ErrMsg NVARCHAR(MAX)
+
+	DELETE
+	FROM tblRKOptionsMatchPnSHeaderPreStage
+	WHERE ISNULL(strFeedStatus, '') = ''
+		AND intOptionsMatchPnSHeaderId = @intOptionsMatchPnSHeaderId
+
+	INSERT INTO tblRKOptionsMatchPnSHeaderPreStage (
+		intOptionsMatchPnSHeaderId
+		,strRowState
+		,intUserId
+		,strFeedStatus
+		,strMessage
+		)
+	SELECT @intOptionsMatchPnSHeaderId
+		,@strRowState
+		,@intUserId
+		,''
+		,''
+END TRY
+
+BEGIN CATCH
+	SET @ErrMsg = ERROR_MESSAGE()
+
+	RAISERROR (
+			@ErrMsg
+			,16
+			,1
+			,'WITH NOWAIT'
+			)
+END CATCH
---
+++ i21Database/dbo/Tables/tblRKOptionsMatchPnSHeaderPreStage.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE tblRKOptionsMatchPnSHeaderPreStage
+(
+	intOptionsMatchPnSHeaderPreStageId	INT IDENTITY(1,1) PRIMARY KEY, 
+	intOptionsMatchPnSHeaderId			INT,
+	strRowState							NVARCHAR(100) COLLATE Latin1_General_CI_AS,
+	intUserId							INT,
+	strFeedStatus						NVARCHAR(MAX) COLLATE Latin1_General_CI_AS,
+	dtmFeedDate							DATETIME CONSTRAINT DF_tblRKOptionsMatchPnSHeaderPreStage_dtmFeedDate DEFAULT GETDATE(),
+	strMessage							NVARCHAR(MAX) COLLATE Latin1_General_CI_AS
+)
