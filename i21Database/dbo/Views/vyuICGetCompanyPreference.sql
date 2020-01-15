e2c7e600ef6d46ec4420c7a2bb58209d6a6d7a32
Feb Montefrio <feb.montefrio@irely.com>
2020-01-15 15:59:45 +0800

    [Cost Savings] IC-8001 IC-8002

---
+++ i21Database/dbo/Views/vyuICGetCompanyPreference.sql
@@ -0,0 +1,15 @@
+﻿CREATE VIEW [dbo].[vyuICSearchCompanyPreference]
+AS
+
+SELECT 
+	ic.* 
+	-- Get the company preference from the other modules that is related with IC. 
+	,rk.ysnImposeReversalTransaction
+FROM 
+	tblICCompanyPreference ic
+	OUTER APPLY (
+		SELECT TOP 1 
+			rk.*
+		FROM 
+			tblRKCompanyPreference rk
+	) rk 
