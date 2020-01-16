f03d50917aec227e1eb05ca24866927f70bbfb76
Jeff Trajano <jeffrey.trajano@irely.com>
2020-01-16 15:26:45 +0800

    GL-7420 [ACH from Customer] - html is displayed in the generated file

--- i21Database/dbo/Views/vyuCMRemittanceAdviceReport.sql
+++ i21Database/dbo/Views/vyuCMRemittanceAdviceReport.sql
@@ -1,37 +1,27 @@
 CREATE VIEW vyuCMRemittanceAdviceReport
 AS
 SELECT 
-CHK.dtmDate, strCheckNumber = CHK.strReferenceNo,CHK.dblAmount, 
-CHK.strName, strAmountInWords = LTRIM (RTRIM(REPLACE (CHK.strAmountInWords, '*', ''))) 
-    + REPLICATE (' *',30), 
 CHK.strRecordNo, 
 CHK.intTransactionId,
 CHK.intBankAccountId, 
 strCompanyName = COMPANY.strCompanyName,
 strCompanyAddress = dbo.fnConvertToFullAddress(COMPANY.strAddress, COMPANY.strCity,COMPANY.strState,COMPANY.strZip),
 strBank = UPPER (Bank.strBankName),
-strBankAddress = dbo.fnConvertToFullAddress (Bank.strAddress,Bank.strCity, Bank.strState, Bank.strZipCode),
-strCustomerId = ISNULL (CHK.strEntityNo, '--'),
 strCustomerName = CHK.strName, 
-strCustomerAccount = ISNULL(RIGHT (CHK.strPayeeBankAccountNumber, 4), ''),
 strCustomerAddress = 
     CASE WHEN ISNULL(dbo.fnConvertToFullAddress (LOCATION.strAddress,LOCATION.strCity, LOCATION.strState, 
         LOCATION.strZipCode),'') <> '' 
     THEN dbo.fnConvertToFullAddress (LOCATION.strAddress,LOCATION.strCity, LOCATION.strState, LOCATION.strZipCode) 
     ELSE '' 
     END, 
-CHK.intBankTransactionTypeId, 
-INV.strInvoiceNumber,dtmDetailDate = INV.dtmDate, 
+INV.strInvoiceNumber,
+dtmDetailDate = INV.dtmDate, 
 strComment = INV.strComments,
 dblDetailAmount = INV.dblInvoiceTotal, 
-PYMT.dtmDatePaid,
 dblDiscount = PYMTDTL.dblDiscount, 
 dblNet = CASE WHEN INV.intPaymentId IS NULL  THEN PYMTDTL.dblPayment ELSE INV.dblInvoiceTotal END,
-strMessage = 'The following items(s) will be presented to ' + 
-CHK.strPayeeBankName + ' account ending ' + ISNULL (RIGHT(RTRIM (COALESCE (CHK.strPayeeBankAccountNumber,
-CHK.strPayeeBankAccountNumber COLLATE Latin1_General_CI_AS, N'')), 4), '') + ' on ' + CONVERT (VARCHAR, PYMT.dtmDatePaid,107), 
-strBankAccountNo = STUFF (ACCT.strBankAccountNo, 1, LEN(ACCT.strBankAccountNo) - 4, REPLICATE ('x', LEN(ACCT.strBankAccountNo) - 4)), 
-CHK.strPayeeBankName,CHK.strPayeeBankAccountNumber, 
+strMessage = 'The following items(s) will be presented to ' + CHK.strPayeeBankName + ' account ending ' + ISNULL (RIGHT(RTRIM (COALESCE (CHK.strPayeeBankAccountNumber,CHK.strPayeeBankAccountNumber COLLATE Latin1_General_CI_AS, N'')), 4), '') + ' on ' + CONVERT (VARCHAR, PYMT.dtmDatePaid,107), 
+strBankAccountNo = STUFF (ACCT.strBankAccountNo, 1, LEN(ACCT.strBankAccountNo) - 4, REPLICATE ('x', LEN(ACCT.strBankAccountNo) - 4)),
 PYMTDTL.intPaymentDetailId
 FROM dbo.vyuCMACHFromCustomer CHK 
 LEFT JOIN tblARPayment PYMT ON CHK.strRecordNo = PYMT.strRecordNumber 
