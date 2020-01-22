9c385718e7ec6539e754eeece47100c74f779536
jayson.ramos <jayson.ramos@irely.com>
2020-01-22 15:59:43 +0800

    CT-4299

--- i21Database/dbo/Stored Procedures/uspCTBeforeInvoiceDelete.sql
+++ i21Database/dbo/Stored Procedures/uspCTBeforeInvoiceDelete.sql
@@ -1,14 +1,23 @@
 ﻿CREATE PROCEDURE [dbo].[uspCTBeforeInvoiceDelete]
 	@intInvoiceId	 INT,
-	@intUserId		 INT -- User Who is deleting the Invoice.
+	@intUserId		 INT, -- User Who is deleting the Invoice.
+	@intInvoiceDetailId INT null
 AS
 
 BEGIN TRY
 
 	DECLARE	@ErrMsg	NVARCHAR(MAX)
 
-	UPDATE	tblCTPriceFixationDetail SET intInvoiceId = NULL, intInvoiceDetailId = NULL WHERE intInvoiceId = @intInvoiceId
-	DELETE FROM  tblCTPriceFixationDetailAPAR  WHERE intInvoiceId = @intInvoiceId
+	if (isnull(@intInvoiceDetailId,0) < 1)
+	begin
+		UPDATE tblCTPriceFixationDetail SET intInvoiceId = NULL, intInvoiceDetailId = NULL WHERE intInvoiceId = @intInvoiceId  
+		DELETE FROM  tblCTPriceFixationDetailAPAR  WHERE intInvoiceId = @intInvoiceId  
+	end
+	else
+	begin
+		UPDATE tblCTPriceFixationDetail SET intInvoiceId = NULL, intInvoiceDetailId = NULL WHERE intInvoiceId = @intInvoiceId    and intInvoiceDetailId = @intInvoiceDetailId
+		DELETE FROM  tblCTPriceFixationDetailAPAR  WHERE intInvoiceId = @intInvoiceId  and intInvoiceDetailId = @intInvoiceDetailId
+	end
 
 END TRY
 
