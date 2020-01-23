6b1b0f68bd155c09a3a1f8fe1f112f43866df779
Vinoth Kumar T <vinoth.t@irely.com>
2020-01-23 21:55:14 +0530

    IN-325

---
+++ i21Database/dbo/Views/vyuIPGetOptionsMatchPnS.sql
@@ -0,0 +1,19 @@
+﻿CREATE VIEW vyuIPGetOptionsMatchPnS
+AS
+SELECT M.intMatchOptionsPnSId
+	,M.intOptionsMatchPnSHeaderId
+	,M.strTranNo
+	,M.dtmMatchDate
+	,M.dblMatchQty
+	,M.intLFutOptTransactionId
+	,M.intSFutOptTransactionId
+	,M.intConcurrencyId
+	,M.ysnPost
+	,M.dtmPostDate
+	,M.intMatchNo
+	,M.intMatchOptionsPnSRefId
+	,FOT.strInternalTradeNo AS strLongInternalTradeNo
+	,FOT1.strInternalTradeNo AS strShortInternalTradeNo
+FROM tblRKOptionsMatchPnS M
+LEFT JOIN tblRKFutOptTransaction FOT ON FOT.intFutOptTransactionId = M.intLFutOptTransactionId
+LEFT JOIN tblRKFutOptTransaction FOT1 ON FOT1.intFutOptTransactionId = M.intLFutOptTransactionId
---
+++ i21Database/dbo/Views/vyuIPGetOptionsPnSExpired.sql
@@ -0,0 +1,13 @@
+﻿CREATE VIEW vyuIPGetOptionsPnSExpired
+AS
+SELECT E.intOptionsPnSExpiredId
+	,E.intOptionsMatchPnSHeaderId
+	,E.strTranNo
+	,E.dtmExpiredDate
+	,E.dblLots
+	,E.intFutOptTransactionId
+	,E.intOptionsPnSExpiredRefId
+	,E.intConcurrencyId
+	,FOT.strInternalTradeNo
+FROM tblRKOptionsPnSExpired E
+LEFT JOIN tblRKFutOptTransaction FOT ON FOT.intFutOptTransactionId = E.intFutOptTransactionId
