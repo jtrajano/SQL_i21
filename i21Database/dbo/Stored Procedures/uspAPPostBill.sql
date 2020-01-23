597c1d9af59b9a580591851266274bf1dfb44f69
Erick dela Cruz <EDelaCruz@IRELY.PH>
2020-01-23 14:12:24 +0800

    AP-8267

--- i21Database/dbo/Stored Procedures/uspAPPostBill.sql
+++ i21Database/dbo/Stored Procedures/uspAPPostBill.sql
@@ -13,12 +13,12 @@
 	@beginTransaction	AS NVARCHAR(50)		= NULL,
 	@endTransaction		AS NVARCHAR(50)		= NULL,
 	@exclude			AS NVARCHAR(MAX)	= NULL,
-	@isPricingContract	AS INT				= 0,
 	@successfulCount	AS INT				= 0 OUTPUT,
 	@invalidCount		AS INT				= 0 OUTPUT,
 	@success			AS BIT				= 0 OUTPUT,
 	@batchIdUsed		AS NVARCHAR(40)		= NULL OUTPUT,
-	@recapId			AS NVARCHAR(250)	= NEWID OUTPUT
+	@recapId			AS NVARCHAR(250)	= NEWID OUTPUT,
+	@isPricingContract	AS INT				= 0
 AS
 
 SET QUOTED_IDENTIFIER OFF
