212468bcd90736e2e6819fe9bbccea871e8f06aa
Erick dela Cruz <EDelaCruz@IRELY.PH>
2020-01-24 11:35:02 +0800

    AP-8274

--- i21Database/dbo/Stored Procedures/uspAPGetBalance.sql
+++ i21Database/dbo/Stored Procedures/uspAPGetBalance.sql
@@ -1,5 +1,7 @@
 ﻿CREATE PROCEDURE [dbo].[uspAPGetBalance]
 	@userId INT,
+	@dateFrom DATETIME = NULL,
+	@dateTo DATETIME = NULL,
 	@apBalance DECIMAL(18,6) OUTPUT,
 	@apGLBalance DECIMAL(18,6) OUTPUT
 AS
@@ -9,8 +11,8 @@ BEGIN
 	DECLARE @gl DECIMAL(18,6);
 	DECLARE @log NVARCHAR(100);
 
-	EXEC uspAPBalance @UserId = @userId, @balance = @ap OUT, @logKey = @log OUT;
-	EXEC uspAPGLBalance @UserId = @userId, @balance = @gl OUT, @logKey = @log OUT;
+	EXEC uspAPBalance @UserId = @userId, @dateFrom = @dateFrom, @dateTo = @dateTo, @balance = @ap OUT, @logKey = @log OUT;
+	EXEC uspAPGLBalance @UserId = @userId, @dateFrom = @dateFrom, @dateTo = @dateTo, @balance = @gl OUT, @logKey = @log OUT;
 
 	IF @ap != @gl
 	BEGIN
