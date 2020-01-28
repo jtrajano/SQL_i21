b553a5195c67de173874d19129a18b5a92aeacc8
crisman_lazaga <cris.lazaga@irely.com>
2020-01-28 17:09:57 +0800

    ST-1619

--- i21Database/dbo/Views/vyuSTSalesTaxReport.sql
+++ i21Database/dbo/Views/vyuSTSalesTaxReport.sql
@@ -9,29 +9,22 @@ SELECT ST.intStoreId
 	, ISNULL(Inv.ysnPosted, CAST(0 AS BIT)) AS ysnPosted
 	, ACC.strAccountId
 	, STT.strTaxNo
-	--, SMTax.strTaxCode
-	, IT.strItemNo strTaxCode
-	, SUM(STT.dblTotalTax) dblTotalTax
-	, SUM(STT.dblTaxableSales) dblTaxableSales
-	, SUM(STT.dblTaxExemptSales) dblTaxExemptSales
-FROM tblSTCheckoutHeader CH 
+	, TC.strTaxCode strTaxCode
+	,SUM(STT.dblTotalTax) dblTotalTax
+	,SUM(STT.dblTaxableSales) dblTaxableSales
+	,SUM(STT.dblTaxExemptSales) dblTaxExemptSales
+FROM tblSTCheckoutSalesTaxTotals STT 
+	INNER JOIN tblSTCheckoutHeader CH ON STT.intCheckoutId = CH.intCheckoutId 
 INNER JOIN tblSTStore ST 
 	ON ST.intStoreId = CH.intStoreId 
 INNER JOIN tblSTStoreTaxTotals STTax
-	ON ST.intStoreId = STTax.intStoreId
+		ON ST.intStoreId = STTax.intStoreId
+		AND STT.strTaxNo = STTax.strTaxCodeNumber
 LEFT JOIN tblARInvoice Inv 
 	ON Inv.intInvoiceId = CH.intInvoiceId
-INNER JOIN tblSTCheckoutSalesTaxTotals STT 
-	ON STT.intCheckoutId = CH.intCheckoutId 
--- INNER JOIN tblSMTaxCode SMTax
--- 	ON STTax.intTaxCodeId = SMTax.intTaxCodeId
--- 	AND STT.strTaxNo = SMTax.strStoreTaxNumber
-INNER JOIN tblGLAccount ACC 
+LEFT JOIN tblGLAccount ACC 
 	ON ACC.intAccountId = STT.intSalesTaxAccount
-INNER JOIN tblICItem IT 
-	ON IT.intItemId = STT.intItemId 
-INNER JOIN tblICCategory CAT 
-	ON CAT.intCategoryId = IT.intCategoryId
-WHERE STT.dblTotalTax <> 0	
-	OR STT.dblTaxableSales <> 0	
-GROUP BY ST.intStoreId, ST.intStoreNo, ST.strRegion, ST.strDistrict, ST.strDescription, CH.dtmCheckoutDate, Inv.ysnPosted, ACC.strAccountId, STT.strTaxNo, IT.strItemNo
+LEFT JOIN tblSMTaxCode TC
+		ON STTax.intTaxCodeId = TC.intTaxCodeId
+WHERE (STT.dblTotalTax <> 0	OR STT.dblTaxableSales <> 0)
+GROUP BY ST.intStoreId, ST.intStoreNo, ST.strRegion, ST.strDistrict, ST.strDescription, CH.dtmCheckoutDate, Inv.ysnPosted, ACC.strAccountId, STT.strTaxNo, TC.strTaxCode
