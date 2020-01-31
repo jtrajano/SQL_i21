f2b8b6b626ae6192d9cdbc160a549f94c089bb1e
Erick dela Cruz <EDelaCruz@IRELY.PH>
2020-01-31 12:16:11 +0800

    AP-8303

--- i21Database/dbo/Functions/fnAP1099EFileTransmitter.sql
+++ i21Database/dbo/Functions/fnAP1099EFileTransmitter.sql
@@ -15,7 +15,7 @@ BEGIN
 		+ CAST(@year AS NVARCHAR(10))  --Position 2-5
 		+ CASE WHEN @year <= YEAR(GETDATE()) - 1 THEN ' ' ELSE 'P' END -- Position 6
 		+ ISNULL(NULLIF(LEFT(A.strFederalTaxID,9),''),SPACE(9)) --Position 7-15 TIN
-		+ '33A19' --Position 16-20 TCC
+		+ C.strTransmitterCode --'33A19' --Position 16-20 TCC
 		+ SPACE(7) -- 21-27Position 
 		+ CASE WHEN @test = 1 THEN 'T' ELSE ' ' END --Position 28
 		+ ' ' COLLATE Latin1_General_CI_AS --Position 29 Foreign Indicator
