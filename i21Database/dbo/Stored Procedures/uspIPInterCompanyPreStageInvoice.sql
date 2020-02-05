bc08ceb3fe618e5df5d0d1ea4033a831ec60d044
Premkumar S <prem.selladurai@irely.com>
2020-02-05 16:09:37 +0530

    IN-455

---
+++ i21Database/dbo/Stored Procedures/uspIPInterCompanyPreStageInvoice.sql
@@ -0,0 +1,34 @@
+﻿CREATE PROCEDURE uspIPInterCompanyPreStageInvoice( @intInvoiceId INT
+	,@strRowState NVARCHAR(50) = NULL
+	,@intUserId INT = NULL)
+AS
+BEGIN TRY
+	SET NOCOUNT ON
+
+	DECLARE @ErrMsg NVARCHAR(MAX)
+
+	DELETE
+	FROM tblARInvoicePreStage
+	WHERE ISNULL(strFeedStatus, '') = ''
+		AND intInvoiceId = @intInvoiceId
+
+	INSERT INTO tblARInvoicePreStage (
+		intInvoiceId
+		,strRowState
+		,intUserId
+		)
+	SELECT @intInvoiceId
+		,@strRowState
+		,@intUserId
+END TRY
+
+BEGIN CATCH
+	SET @ErrMsg = ERROR_MESSAGE()
+
+	RAISERROR (
+			@ErrMsg
+			,16
+			,1
+			,'WITH NOWAIT'
+			)
+END CATCH
