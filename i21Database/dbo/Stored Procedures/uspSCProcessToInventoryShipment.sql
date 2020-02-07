d0b0bcb682936bae7f0e70938e0d11f6baacc371
Kenneth Lew Lagunero <kenneth.lagunero@irely.com>
2020-02-08 05:21:35 +0800

    SC-3267

--- i21Database/dbo/Stored Procedures/uspSCProcessToInventoryShipment.sql
+++ i21Database/dbo/Stored Procedures/uspSCProcessToInventoryShipment.sql
@@ -561,7 +561,7 @@ BEGIN TRY
 	LEFT JOIN tblCTContractDetail CTD ON CTD.intContractDetailId = ISI.intLineNo
 	WHERE intInventoryShipmentId = @InventoryShipmentId
 
-	IF ISNULL(@InventoryShipmentId, 0) != 0 AND (ISNULL(@intPricingTypeId,0) <= 1 OR ISNULL(@intPricingTypeId,0) = 6) AND ISNULL(@strWhereFinalizedWeight, 'Origin') = 'Origin' AND ISNULL(@strWhereFinalizedGrade, 'Origin') = 'Origin' AND @ysnPriceFixation = 0
+	IF ISNULL(@InventoryShipmentId, 0) != 0 AND EXISTS(SELECT TOP 1 1 FROM tblICInventoryShipmentItem WHERE ysnAllowInvoice = 1 AND intInventoryShipmentId = InventoryShipmentId)
 	BEGIN
 		EXEC @intInvoiceId = dbo.uspARCreateInvoiceFromShipment @InventoryShipmentId, @intUserId, NULL, 1, 1;
 	END
