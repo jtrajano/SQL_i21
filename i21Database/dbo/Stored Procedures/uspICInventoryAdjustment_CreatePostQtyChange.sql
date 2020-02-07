18119aeab36b0afedeaa63a29f6410f5c8b81793
Wendell Wayne Estrada <wendell.estrada@irely.com>
2020-02-07 16:01:58 +0800

    [B] IC-8156 CLONE - HDTN-240761 - Unable to checkout store IC-8211 CLONE - CLONE - HDTN-240761 - Unable to checkout store

--- i21Database/dbo/Stored Procedures/uspICInventoryAdjustment_CreatePostQtyChange.sql
+++ i21Database/dbo/Stored Procedures/uspICInventoryAdjustment_CreatePostQtyChange.sql
@@ -100,7 +100,9 @@ BEGIN
 	)
 	BEGIN 
 		-- Item UOM is invalid or missing.
-		EXEC uspICRaiseError 80048;   
+		DECLARE @strText NVARCHAR(200)
+		SET @strText = (SELECT ISNULL(strItemNo, '') FROM tblICItem WHERE intItemId = @intItemId)
+		EXEC uspICRaiseError 80048, @strText
 		GOTO _Exit
 	END 
 END 
@@ -122,7 +124,9 @@ IF NOT EXISTS (
 )
 BEGIN 
 	-- Item UOM is invalid or missing.
-	EXEC uspICRaiseError 80048;   
+	DECLARE @strText NVARCHAR(200)
+		SET @strText = (SELECT ISNULL(strItemNo, '') FROM tblICItem WHERE intItemId = @intItemId)
+		EXEC uspICRaiseError 80048, @strText
 	GOTO _Exit
 END
 
