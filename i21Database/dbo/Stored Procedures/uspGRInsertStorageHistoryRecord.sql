70dc79a5815121c15d276f5b749cc4cd26032189
Wendell Wayne Estrada <wendell.estrada@irely.com>
2020-02-07 15:48:22 +0800

    [I] GRN-1855 Implement RM-3463 in uspGRInsertStorageHistoryRecord

--- i21Database/dbo/Stored Procedures/uspGRInsertStorageHistoryRecord.sql
+++ i21Database/dbo/Stored Procedures/uspGRInsertStorageHistoryRecord.sql
@@ -197,12 +197,79 @@ BEGIN TRY
 		FROM tblICInventoryTransaction t
 			INNER JOIN @StorageHistoryData h ON t.intTransactionId = h.intTransferStorageId
 			INNER JOIN tblGRTransferStorageReference sr ON sr.intTransferStorageId = h.intTransferStorageId
+				AND sr.intToCustomerStorageId = h.intCustomerStorageId
+			INNER JOIN tblGRCustomerStorage fs ON fs.intCustomerStorageId = sr.intSourceCustomerStorageId
+			INNER JOIN tblGRStorageType ft ON ft.intStorageScheduleTypeId = fs.intStorageTypeId
+			INNER JOIN tblGRCustomerStorage tos ON tos.intCustomerStorageId = sr.intToCustomerStorageId
 			INNER JOIN tblICItem i ON i.intItemId = t.intItemId
 			INNER JOIN tblICItemUOM iu ON iu.intItemUOMId = t.intItemUOMId
 			INNER JOIN tblICUnitMeasure u ON u.intUnitMeasureId = iu.intUnitMeasureId
 			INNER JOIN tblICItemLocation il ON il.intItemLocationId = t.intItemLocationId
 			INNER JOIN tblGRTransferStorageSplit spt ON spt.intTransferStorageSplitId = sr.intTransferStorageSplitId
 			LEFT OUTER JOIN tblCTContractDetail ct ON ct.intContractDetailId = spt.intContractDetailId
+		WHERE t.intTransactionTypeId = 44 -- Magic string for Settle Storage
+
+		-- Split Storage Transfer
+		INSERT INTO @SummaryLogs ( 
+			  strBatchId
+			, strTransactionType
+			, intTransactionRecordId 
+			, strTransactionNumber 
+			, dtmTransactionDate 
+			, intContractDetailId 
+			, intContractHeaderId 
+			, intTicketId 
+			, intCommodityId 
+			, intCommodityUOMId 
+			, intItemId 
+			, intBookId 
+			, intSubBookId 
+			, intLocationId 
+			, intFutureMarketId 
+			, intFutureMonthId 
+			, dblNoOfLots 
+			, dblQty 
+			, dblPrice 
+			, intEntityId 
+			, ysnDelete 
+			, intUserId 
+			, strNotes  
+		)
+		SELECT 
+			  strBatchId = t.strBatchId
+			, strTransactionType = h.strType
+			, intTransactionRecordId = h.intTransferStorageId
+			, strTransactionNumber = t.strTransactionId
+			, dtmTransactionDate = t.dtmDate
+			, intContractDetailId = ct.intContractDetailId
+			, intContractHeaderId = ct.intContractHeaderId
+			, intTicketId = NULL
+			, intCommodityId = i.intCommodityId
+			, intCommodityUOMId = u.intUnitMeasureId
+			, intItemId = t.intItemId
+			, intBookId = NULL
+			, intSubBookId = NULL
+			, intLocationId = il.intLocationId
+			, intFutureMarketId = NULL
+			, intFutureMonthId = NULL
+			, dblNoOfLots = NULL
+			, dblQty = t.dblQty
+			, dblPrice = t.dblCost
+			, intEntityId = NULL
+			, ysnDelete = 0
+			, intUserId = h.intUserId
+			, strNotes = t.strDescription
+		FROM tblICInventoryTransaction t
+			INNER JOIN @StorageHistoryData h ON t.intTransactionId = h.intTransferStorageId
+			INNER JOIN tblGRTransferStorageSourceSplit spt ON spt.intTransferStorageId = h.intTransferStorageId
+				AND spt.intSourceCustomerStorageId = h.intCustomerStorageId
+			LEFT JOIN tblCTContractDetail ct ON ct.intContractDetailId = spt.intContractDetailId
+			INNER JOIN tblGRTransferStorage ts ON ts.intTransferStorageId = spt.intTransferStorageId
+			INNER JOIN tblGRTransferStorageSplit tss ON tss.intTransferStorageId = spt.intTransferStorageId
+			INNER JOIN tblICItem i ON i.intItemId = t.intItemId
+			INNER JOIN tblICItemUOM iu ON iu.intItemUOMId = t.intItemUOMId
+			INNER JOIN tblICUnitMeasure u ON u.intUnitMeasureId = iu.intUnitMeasureId
+			INNER JOIN tblICItemLocation il ON il.intItemLocationId = t.intItemLocationId
 		WHERE t.intTransactionTypeId = 44 -- Magic string for Settle Storage
 
 		EXEC uspRKLogRiskPosition @SummaryLogs
