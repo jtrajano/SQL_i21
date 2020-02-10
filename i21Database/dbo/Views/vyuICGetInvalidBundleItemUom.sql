4326d9f5381a03a34097df0f068db4469bc7e81b
Wendell Wayne Estrada <wendell.estrada@irely.com>
2020-02-10 18:41:40 +0800

    [B] IC-8211

--- i21Database/dbo/Views/vyuICGetInvalidBundleItemUom.sql
+++ i21Database/dbo/Views/vyuICGetInvalidBundleItemUom.sql
@@ -1,7 +1,9 @@
 CREATE VIEW dbo.vyuICGetInvalidBundleItemUom
 AS
 
-SELECT c.strItemNo [strParentItemNo], b.intItemId [intParentItemId], d.strItemNo [strBundleItemNo], b.intBundleItemId, b.intItemUnitMeasureId, m.strUnitMeasure
+SELECT b.intItemBundleId [intId], c.strItemNo [strParentItemNo], b.intItemId [intParentItemId], 
+	d.strItemNo [strBundleItemNo], b.intBundleItemId, b.intItemUnitMeasureId [intItemUOMId], m.strUnitMeasure,
+	ISNULL(r.intItemUOMId, s.intItemUOMId) [intAlternativeUOMId], ISNULL(rm.strUnitMeasure, sm.strUnitMeasure) [strAlternativeUnitMeasure]
 FROM tblICItemBundle b
 	INNER JOIN tblICItemUOM u ON u.intItemUOMId = b.intItemUnitMeasureId
 	INNER JOIN tblICUnitMeasure m ON m.intUnitMeasureId = u.intUnitMeasureId
@@ -9,4 +11,9 @@ FROM tblICItemBundle b
 		AND t.intItemUOMId = b.intItemUnitMeasureId
 	INNER JOIN tblICItem c ON c.intItemId = b.intBundleItemId
 	INNER JOIN tblICItem d ON d.intItemId = b.intItemId
+	LEFT OUTER JOIN tblICItemUOM r ON r.intItemUOMId = dbo.fnGetMatchingItemUOMId(b.intBundleItemId, b.intItemUnitMeasureId)
+	LEFT OUTER JOIN tblICUnitMeasure rm ON rm.intUnitMeasureId = r.intUnitMeasureId
+	LEFT OUTER JOIN tblICItemUOM s ON s.intItemId = b.intBundleItemId
+		AND s.ysnStockUnit = 1
+	LEFT OUTER JOIN tblICUnitMeasure sm ON sm.intUnitMeasureId = s.intUnitMeasureId
 WHERE t.intItemUOMId IS NULL
