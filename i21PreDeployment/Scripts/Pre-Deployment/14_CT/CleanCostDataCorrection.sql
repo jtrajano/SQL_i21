8c20379a36455d0cd5939bb438866b74d563f51d
jayson.ramos <jayson.ramos@irely.com>
2020-02-12 10:10:18 +0800

    CT-4374

--- i21PreDeployment/Scripts/Pre-Deployment/14_CT/CleanCostDataCorrection.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/14_CT/CleanCostDataCorrection.sql
@@ -8,9 +8,10 @@ GO
 IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES
            WHERE TABLE_NAME = N'tblCTContractPlan')
 BEGIN
- exec sp_executesql N'UPDATE tblCTContractPlan SET intWeightId = NULL WHERE intWeightId NOT IN (SELECT intWeightGradeId FROM tblCTWeightGrade)'
- exec sp_executesql N'UPDATE tblCTContractPlan SET intGradeId = NULL WHERE intGradeId NOT IN (SELECT intWeightGradeId FROM tblCTWeightGrade)'
- END
+	exec sp_executesql N'UPDATE tblCTContractPlan SET intWeightId = NULL WHERE intWeightId NOT IN (SELECT intWeightGradeId FROM tblCTWeightGrade)'
+	exec sp_executesql N'UPDATE tblCTContractPlan SET intGradeId = NULL WHERE intGradeId NOT IN (SELECT intWeightGradeId FROM tblCTWeightGrade)'
+	exec('update tblCTContractPlan set intContractBasisId = null where intContractBasisId not in (select intContractBasisId from tblCTContractBasis)');
+END
 
  IF EXISTS(SELECT * FROM sys.columns  WHERE name = N'intPricingStatus' AND object_id = OBJECT_ID(N'tblCTContractDetail'))
 BEGIN
