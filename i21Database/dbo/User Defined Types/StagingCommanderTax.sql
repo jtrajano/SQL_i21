768b3ae7ee9887f7b18593c566a0acbcafc753c1
bonn_ponce <bonn.ponce@irely.com>
2020-02-17 21:45:16 +0800

    ST-1628
    (cherry picked from commit 52ae830bf3cfe803d54b156ab1a335432a9a1f3f)
    (cherry picked from commit 5238b80690f87b5d6742993951817936aa9ef620)

---
+++ i21Database/dbo/User Defined Types/StagingCommanderTax.sql
@@ -0,0 +1,23 @@
+﻿CREATE TYPE StagingCommanderTax AS TABLE
+(
+	[intRowCount] 										  INT				NULL,
+	[dtmPeriodEndDate]                                    DATETIME			NULL,
+	[dtmPeriodBeginDate]                                  DATETIME			NULL,
+	[intPeriodSeqNum]                                     INT				NULL,
+	[strPeriodName]                                       NVARCHAR(MAX)		NULL,
+	[strPeriodType]                                       NVARCHAR(MAX)		NULL,
+	[strSysId] 										      NVARCHAR(MAX)		NULL,
+	[strTaxRateBaseName]                                  NVARCHAR(MAX)		NULL,
+	[dblTaxRateBaseTaxRate]                               NUMERIC(18,6)		NULL,
+	[dblTaxInfoActualTaxRate]                             NUMERIC(18,6)		NULL,
+	[dblTaxInfoTaxableSales]                              NUMERIC(18,6)		NULL,
+	[dblTaxInfoNonTaxableSales]                           NUMERIC(18,6)		NULL,
+	[dblTaxInfoSalesTax]                                  NUMERIC(18,6)		NULL,
+	[dblTaxInfoRefundTax]                                 NUMERIC(18,6)		NULL,
+	[dblTaxInfoNetTax]                                    NUMERIC(18,6)		NULL,
+	[dblTaxInfoTaxableRefunds]                            NUMERIC(18,6)		NULL,
+	[dblTaxInfoTaxExemptSales]                            NUMERIC(18,6)		NULL,
+	[dblTaxInfoTaxExemptRefunds]                          NUMERIC(18,6)		NULL,
+	[dblTaxInfoTaxForgivenSales]                          NUMERIC(18,6)		NULL,
+	[dblTaxInfoTaxForgivenRefunds]                        NUMERIC(18,6)		NULL
+)
