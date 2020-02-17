5eafe3ce274cbceb04f1a393d9722e990c69dc6f
manny_armas <manny.armas@iRely.com>
2020-02-17 13:23:19 +0800

    IET-478 - Drivers getting error when receiving files from office

--- i21Database/dbo/Views/vyuETExportConsumptionSite.sql
+++ i21Database/dbo/Views/vyuETExportConsumptionSite.sql
@@ -33,7 +33,7 @@ SELECT
 			ELSE
 				''
 			END  COLLATE Latin1_General_CI_AS 
-,dmserl = ISNULL((SELECT TOP 1 strSerialNumber FROM tblTMDevice WHERE intDeviceId IN 
+,dmserl = ISNULL((SELECT TOP 1 SUBSTRING(strSerialNumber,1,20) FROM tblTMDevice WHERE intDeviceId IN 
 				(SELECT intDeviceId FROM tblTMSiteDevice WHERE intSiteID = A.intSiteID) 
 				and ysnAppliance = 0 
 				AND intDeviceTypeId = (SELECT TOP 1 intDeviceTypeId 
