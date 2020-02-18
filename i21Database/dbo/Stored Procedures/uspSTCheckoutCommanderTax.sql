0254c6105f827c8858b24c258a2ab7ff61c15548
bonn_ponce <bonn.ponce@irely.com>
2020-02-18 18:33:06 +0800

    ST-1628
    (cherry picked from commit 8cd0f19b288d838fc1e19629f77a2beb59eaceff)

--- i21Database/dbo/Stored Procedures/uspSTCheckoutCommanderTax.sql
+++ i21Database/dbo/Stored Procedures/uspSTCheckoutCommanderTax.sql
@@ -1,59 +1,91 @@
 ﻿CREATE PROCEDURE [dbo].[uspSTCheckoutCommanderTax]
-	@intCheckoutId INT,
-	@strStatusMsg NVARCHAR(250) OUTPUT,
-	@intCountRows INT OUTPUT
+	@intCheckoutId							INT,
+	@UDT_TransTax							StagingCommanderTax		READONLY,
+	@ysnSuccess								BIT				OUTPUT,
+	@strMessage								NVARCHAR(1000)	OUTPUT,
+	@intCountRows							INT				OUTPUT
 AS
 BEGIN
 
-	--SET NOCOUNT ON
-    SET XACT_ABORT ON
+	SET ANSI_WARNINGS OFF;
+	SET NOCOUNT ON;
+    DECLARE @InitTranCount INT;
+    SET @InitTranCount = @@TRANCOUNT
+	DECLARE @Savepoint NVARCHAR(32) = SUBSTRING(('uspSTCheckoutCommanderTax' + CONVERT(VARCHAR, @InitTranCount)), 1, 32)
        
-	   BEGIN TRY
+	BEGIN TRY
               
-			  BEGIN TRANSACTION
+			  
+		IF @InitTranCount = 0
+			BEGIN
+				BEGIN TRANSACTION
+			END
+
+		ELSE
+			BEGIN
+				SAVE TRANSACTION @Savepoint
+			END
 
 
+		-- ==================================================================================================================  
+		-- Start Validate if Translog xml file matches the Mapping on i21 
+		-- ------------------------------------------------------------------------------------------------------------------
+		IF NOT EXISTS(SELECT TOP 1 1 FROM @UDT_TransTax)
+		BEGIN
+				-- Add to error logging
+				INSERT INTO tblSTCheckoutErrorLogs 
+				(
+					strErrorType
+					, strErrorMessage 
+					, strRegisterTag
+					, strRegisterTagValue
+					, intCheckoutId
+					, intConcurrencyId
+				)
+				VALUES
+				(
+					'XML LAYOUT MAPPING'
+					, 'Commander Translog XML file did not match the layout mapping'
+					, ''
+					, ''
+					, @intCheckoutId
+					, 1
+				)
 
-			  DECLARE @intStoreId int
-              SELECT @intStoreId = intStoreId
-              FROM dbo.tblSTCheckoutHeader
-              WHERE intCheckoutId = @intCheckoutId
+				SET @intCountRows = 0
+				SET @strMessage = 'Commander Translog XML file did not match the layout mapping'
+
+				GOTO ExitWithCommit
+		END
+		-- ------------------------------------------------------------------------------------------------------------------
+		-- End Validate if Translog xml file matches the Mapping on i21   
+		-- ==================================================================================================================
 
 
-			-- ==================================================================================================================  
-			-- Start Validate if Tax xml file matches the Mapping on i21 
-			-- ------------------------------------------------------------------------------------------------------------------
-			IF NOT EXISTS(SELECT TOP 1 1 FROM #tempCheckoutInsert)
-				BEGIN
-						-- Add to error logging
-						INSERT INTO tblSTCheckoutErrorLogs 
-						(
-							strErrorType
-							, strErrorMessage 
-							, strRegisterTag
-							, strRegisterTagValue
-							, intCheckoutId
-							, intConcurrencyId
-						)
-						VALUES
-						(
-							'XML LAYOUT MAPPING'
-							, 'Commander Tax XML file did not match the layout mapping'
-							, ''
-							, ''
-							, @intCheckoutId
-							, 1
-						)
+		-- COUNT
+		DECLARE @intTableRowCount AS INT = 0
+		SELECT @intTableRowCount = COUNT(*) FROM @UDT_TransTax
 
-						SET @intCountRows = 0
-						SET @strStatusMsg = 'Commander Tax XML file did not match the layout mapping'
+		IF(@intTableRowCount > 0)
+			BEGIN
 
-						GOTO ExitWithCommit
-				END
-			-- ------------------------------------------------------------------------------------------------------------------
-			-- End Validate if Tax xml file matches the Mapping on i21   
-			-- ==================================================================================================================
+				--Get StoreId
+				DECLARE @intStoreId			INT,
+						@strRegisterClass	NVARCHAR(50),
+						@intRegisterClassId INT
 
+				SELECT
+					@intStoreId			= chk.intStoreId,
+					@strRegisterClass	= r.strRegisterClass,
+					@intRegisterClassId = setup.intRegisterSetupId
+				FROM tblSTCheckoutHeader chk
+				INNER JOIN tblSTStore st
+					ON chk.intStoreId = st.intStoreId
+				INNER JOIN tblSTRegister r
+					ON st.intRegisterId = r.intRegisterId
+				INNER JOIN tblSTRegisterSetup setup
+					ON r.strRegisterClass = setup.strRegisterClass
+				WHERE chk.intCheckoutId = @intCheckoutId
 
 
 			  -- ================================================================================================================== 
@@ -73,27 +105,27 @@ BEGIN
 					strErrorType			= 'NO MATCHING TAG'
 					, strErrorMessage		= 'No Matching Register Tax Code'
 					, strRegisterTag		= 'taxrateBase sysid'
-					, strRegisterTagValue	= ISNULL(Chk.taxrateBasesysid, '')
+					, strRegisterTagValue	= ISNULL(Chk.strSysId, '')
 					, intCheckoutId			= @intCheckoutId
 					, intConcurrencyId		= 1
-				FROM #tempCheckoutInsert Chk
-				WHERE ISNULL(Chk.taxrateBasesysid, '') NOT IN
+				FROM @UDT_TransTax Chk
+				WHERE ISNULL(Chk.strSysId, '') NOT IN
 				(
 					SELECT DISTINCT 
 						tbl.strXmlRegisterTaxLevelID
 					FROM
 					(
 						SELECT DISTINCT
-							Chk.taxrateBasesysid AS strXmlRegisterTaxLevelID
-						FROM #tempCheckoutInsert Chk
+							Chk.strSysId AS strXmlRegisterTaxLevelID
+						FROM @UDT_TransTax Chk
 						JOIN tblSTCheckoutSalesTaxTotals STT
-							ON ISNULL(Chk.taxrateBasesysid, '') COLLATE DATABASE_DEFAULT = STT.strTaxNo
+							ON ISNULL(Chk.strSysId, '') COLLATE DATABASE_DEFAULT = STT.strTaxNo
 						WHERE STT.intCheckoutId = @intCheckoutId
-							AND ISNULL(Chk.taxrateBasesysid, '') != ''
+							AND ISNULL(Chk.strSysId, '') != ''
 							--AND CAST(ISNULL(Chk.taxrateBasetaxRate, 0) AS DECIMAL(18,6)) != 0.000000
 					) AS tbl
 				)
-					AND ISNULL(Chk.taxrateBasesysid, '') != ''
+					AND ISNULL(Chk.strSysId, '') != ''
 					--AND CAST(ISNULL(Chk.taxrateBasetaxRate, 0) AS DECIMAL(18,6)) != 0.000000
 				-- ------------------------------------------------------------------------------------------------------------------  
 				-- END Get Error logs. Check Register XML that is not configured in i21.  
@@ -101,53 +133,111 @@ BEGIN
 
 			  
 
+				--INSERT INTO tblSTCheckoutSalesTaxTotals
+				--(
+				--	 dblTotalTax		   
+				--	,dblTaxableSales	   
+				--	,dblTaxExemptSales	          
+				--	,intCheckoutId		   
+				--)
+				--SELECT
+				--	 CAST(ISNULL(Chk.dblTaxInfoNetTax, 0) AS DECIMAL(18,6))
+				--	,CAST(ISNULL(Chk.dblTaxInfoTaxableSales, 0) AS DECIMAL(18,6))         
+				--	,CAST(ISNULL(Chk.dblTaxInfoTaxExemptSales, 0) AS DECIMAL(18,6))  
+				--	,@intCheckoutId
+				--FROM 
+				--@UDT_TransTax Chk
 
-				-- Tax FILE
+				---- Tax FILE
+				--  UPDATE STT
+				--  SET dblTotalTax		= CAST(ISNULL(Chk.dblTaxInfoNetTax, 0) AS DECIMAL(18,6))
+				--  --SET dblTotalTax		= CAST(ISNULL(Chk.taxInfosalesTax, 0) AS DECIMAL(18,6)) http://jira.irelyserver.com/browse/ST-1587
+				--	, dblTaxableSales	= CAST(ISNULL(Chk.dblTaxInfoTaxableSales, 0) AS DECIMAL(18,6))         
+				--	, dblTaxExemptSales = CAST(ISNULL(Chk.dblTaxInfoTaxExemptSales, 0) AS DECIMAL(18,6))  
+				--  FROM @UDT_TransTax Chk
+				--  INNER JOIN dbo.tblSTCheckoutSalesTaxTotals STT
+				--	ON ISNULL(Chk.strSysId, '') COLLATE DATABASE_DEFAULT = STT.strTaxNo
+				--  WHERE STT.intCheckoutId = @intCheckoutId
+				--	AND CAST(ISNULL(Chk.dblTaxRateBaseTaxRate, 0) AS DECIMAL(18,6)) != 0.000000
+
 				  UPDATE STT
-				  SET dblTotalTax		= CAST(ISNULL(Chk.taxInfonetTax, 0) AS DECIMAL(18,6))
+				  SET dblTotalTax		= CAST(ISNULL(Chk.dblTaxInfoNetTax, 0) AS DECIMAL(18,6))
 				  --SET dblTotalTax		= CAST(ISNULL(Chk.taxInfosalesTax, 0) AS DECIMAL(18,6)) http://jira.irelyserver.com/browse/ST-1587
-					, dblTaxableSales	= CAST(ISNULL(Chk.taxInfotaxableSales, 0) AS DECIMAL(18,6))         
-					, dblTaxExemptSales = CAST(ISNULL(Chk.taxInfotaxExemptSales, 0) AS DECIMAL(18,6))  
-				  FROM #tempCheckoutInsert Chk
+					, dblTaxableSales	= CAST(ISNULL(Chk.dblTaxInfoTaxableSales, 0) AS DECIMAL(18,6))         
+					, dblTaxExemptSales = CAST(ISNULL(Chk.dblTaxInfoTaxExemptSales, 0) AS DECIMAL(18,6))  
+				  FROM @UDT_TransTax Chk
 				  INNER JOIN dbo.tblSTCheckoutSalesTaxTotals STT
-					ON ISNULL(Chk.taxrateBasesysid, '') COLLATE DATABASE_DEFAULT = STT.strTaxNo
+					ON ISNULL(Chk.strSysId, '') COLLATE DATABASE_DEFAULT = STT.strTaxNo
 				  WHERE STT.intCheckoutId = @intCheckoutId
-					AND CAST(ISNULL(Chk.taxrateBasetaxRate, 0) AS DECIMAL(18,6)) != 0.000000
+					AND CAST(ISNULL(Chk.dblTaxRateBaseTaxRate, 0) AS DECIMAL(18,6)) != 0.000000
+
 
               -- Difference between Passport and Radiant
               -- 1. Passport does not have 'TenderTransactionsCount' tag in MSM register file
               -- 2. Passport does not have Lottery Winners MOP in tblSTRegister (Register screen)
 
               SET @intCountRows = 1
-              SET @strStatusMsg = 'Success'
+              --SET @strStatusMsg = 'Success'
 
 			  --PRINT 'SUCCESS'
 
               -- COMMIT
 			  GOTO ExitWithCommit
-       END TRY
 
-       BEGIN CATCH
-			SET @intCountRows = 0
-			SET @strStatusMsg = ERROR_MESSAGE()
+ END
 
-			-- ROLLBACK
-			GOTO ExitWithRollback
+
+	END TRY
+	BEGIN CATCH
+		SET @ysnSuccess = CAST(0 AS BIT)
+		SET @strMessage = 'End script error: ' + ERROR_MESSAGE()
+
+		GOTO ExitWithRollback
 	END CATCH
 END
 
 
+
+
+
+
+
 ExitWithCommit:
-	-- Commit Transaction
-	COMMIT TRANSACTION --@TransactionName
+	IF @InitTranCount = 0
+		BEGIN
+			COMMIT TRANSACTION
+		END
+
 	GOTO ExitPost
-	
+
+
 
 ExitWithRollback:
-    -- Rollback Transaction here
-	IF @@TRANCOUNT > 0
-		BEGIN
-			ROLLBACK TRANSACTION --@TransactionName
-		END
-	
+
+		IF @InitTranCount = 0
+			BEGIN
+				IF ((XACT_STATE()) <> 0)
+				BEGIN
+					SET @strMessage = @strMessage + '. Will Rollback Transaction.'
+
+					ROLLBACK TRANSACTION
+				END
+			END
+
+		ELSE
+			BEGIN
+				IF ((XACT_STATE()) <> 0)
+					BEGIN
+						SET @strMessage = @strMessage + '. Will Rollback to Save point.'
+
+						ROLLBACK TRANSACTION @Savepoint
+					END
+			END
+
+
+
+
+
+
+
 ExitPost:
