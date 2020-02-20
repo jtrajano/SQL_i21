8999acb1145270d9a60c475754993e2a8a054951
crisman_lazaga <cris.lazaga@irely.com>
2020-02-20 13:44:13 +0800

    ST-1634
    (cherry picked from commit c86cf491cf78507f76bddab495f68a1fe1e7f187)
    (cherry picked from commit c6def73946a7592a2ee1a12dbce3f169ae5ce41e)

--- i21Database/dbo/Tables/tblSTStoreMetrics.sql
+++ i21Database/dbo/Tables/tblSTStoreMetrics.sql
@@ -8,7 +8,7 @@
     [intRegisterImportFieldId]													INT						NOT NULL,   
     [intConcurrencyId]															INT						NOT NULL, 
     CONSTRAINT [PK_tblSTStoreMetrics]											PRIMARY KEY CLUSTERED	([intStoreMetricsId]), 
-    CONSTRAINT [AK_tblSTStoreMetrics_intStoreId_ImportField_MetricItemId_OffsetItemId]			UNIQUE	NONCLUSTERED	([intStoreId],[intRegisterImportFieldId],[intMetricItemId],[intOffsetItemId]),
+    CONSTRAINT [AK_tblSTStoreMetrics_intStoreId_strMetricsDescription]			UNIQUE	NONCLUSTERED	([intStoreId],[strMetricsDescription]),
 	CONSTRAINT [FK_tblSTStoreMetrics_tblSTStore]								FOREIGN KEY				([intStoreId])							REFERENCES [tblSTStore]([intStoreId]) ON DELETE CASCADE,
 	CONSTRAINT [FK_tblSTStoreMetrics_tblICItemMetrics]							FOREIGN KEY				([intMetricItemId])						REFERENCES [tblICItem]([intItemId]),
 	CONSTRAINT [FK_tblSTStoreMetrics_tblICItemOffset]							FOREIGN KEY				([intOffsetItemId])						REFERENCES [tblICItem]([intItemId])
