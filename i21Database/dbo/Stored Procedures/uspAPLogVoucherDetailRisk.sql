dea5761762799418b4592120d65fc4348ef31a9e
Erick dela Cruz <EDelaCruz@IRELY.PH>
2020-02-21 16:29:57 +0800

    AP-8053

--- i21Database/dbo/Stored Procedures/uspAPLogVoucherDetailRisk.sql
+++ i21Database/dbo/Stored Procedures/uspAPLogVoucherDetailRisk.sql
@@ -12,8 +12,54 @@ SET ANSI_WARNINGS OFF
 BEGIN
 
 	DECLARE @rkSummaryLog AS RKSummaryLog;
-	EXEC [dbo].[uspRKLogRiskPosition]
-			@SummaryLogs = rkSummaryLog
-			,@Rebuild = 0
+
+	INSERT INTO @rkSummaryLog(
+		strBatchId,
+		dtmCreatedDate,
+		strBucketType,
+		strTransactionType,
+		intTransactionRecordId,
+		intTransactionHeaderRecordId,
+		strDistributionType,
+		strTransactionNumber,
+		dtmTransactionDate,
+		intCommodityId,
+		intItemId,
+		intOrigUOMId,
+		intLocationId,
+		dblOrigQty,
+		dblPrice,
+		intEntityId,
+		intUserId,
+		strMiscFields
+	)
+	SELECT 
+		strBatchId = NULL
+		, dtmCreatedDate = b.dtmDateCreated
+		, strBucketType = 'Accounts Payables'
+		, strTransactionType = 'Voucher'
+		, intTransactionRecordId = bd.intBillDetailId
+		, intTransactionHeaderRecordId = bd.intBillId
+		, strDistributionType = ''
+		, strTransactionNumber = b.strBillId
+		, dtmTransactionDate = b.dtmBillDate 
+		, c.intCommodityId
+		, bd.intItemId
+		, intOrigUOMId = cum.intCommodityUnitMeasureId
+		, intLocationId = b.intShipToId
+		, dblOrigQty = CASE WHEN @remove = 1 THEN -bd.dblQtyReceived ELSE bd.dblQtyReceived END
+		, dblPrice = CASE WHEN @remove = 1 THEN -b.dblTotal ELSE b.dblTotal END
+		, intEntityId = b.intEntityVendorId
+		, intUserId = b.intUserId
+		, strMiscFields = '{intInventoryReceiptItemId = "'+ CAST(ISNULL(bd.intInventoryReceiptItemId,'') AS NVARCHAR) +'"} {intLoadDetailId = "' + CAST(ISNULL(bd.intLoadDetailId,'') AS NVARCHAR) +'"}'
+	FROM tblAPBill b
+	INNER JOIN tblAPBillDetail bd ON bd.intBillId = b.intBillId
+	CROSS APPLY (
+		SELECT * FROM dbo.fnAPGetVoucherCommodity(b.intBillId)
+	) c
+	INNER JOIN tblICItemUOM iuom ON iuom.intItemUOMId = bd.intUnitOfMeasureId
+	INNER JOIN tblICCommodityUnitMeasure cum ON cum.intCommodityId = c.intCommodityId AND cum.intUnitMeasureId = iuom.intUnitMeasureId
+	
+	EXEC [dbo].[uspRKLogRiskPosition] @SummaryLogs = @rkSummaryLog, @Rebuild = 0
 
 END
