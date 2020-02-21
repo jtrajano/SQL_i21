8f9f508c4653833b1e91bebefebc4903dd27d9f3
Feb Montefrio <feb.montefrio@irely.com>
2020-02-21 11:45:03 +0800

    [B] IC-8166

--- i21Database/dbo/Stored Procedures/uspICFixICvsGLAmountDisrecpany.sql
+++ i21Database/dbo/Stored Procedures/uspICFixICvsGLAmountDisrecpany.sql
@@ -1,6 +1,6 @@
 CREATE PROCEDURE uspICFixICvsGLAmountDisrecpany
 	@dtmTargetOpenDate AS DATETIME 
-	,@strType AS NVARCHAR(500) = 'Inventory Adjustment - Quantity Change' -- 'Inventory Auto Variance on Negatively Sold or Used Stock'	
+	,@strType AS NVARCHAR(500) 
 	,@intMonth AS INT = NULL 
 	,@intYear AS INT = NULL 
 AS
@@ -10,14 +10,16 @@ DECLARE
 	,@glAmount AS NUMERIC(18,6)
 	,@strTransactionId AS NVARCHAR(50)
 	,@strBatchId AS NVARCHAR(50)
-	,@dtmDate AS DATETIME
-	,@dblDiff AS NUMERIC(18,6)
 	,@intInventoryTransactionId AS INT
+	,@dtmDate AS DATETIME 
+	,@dblDiff AS NUMERIC(18,6)
  
 DECLARE	@GLEntries AS RecapTableType 
 
 DECLARE @discrepancyList AS TABLE (
 	strTransactionId NVARCHAR(50)
+	,strBatchId NVARCHAR(50)
+	,intInventoryTransactionId INT
 	,dtmDate DATETIME
 	,dblICAmount NUMERIC(18, 6) NULL
 	,dblGLAmount NUMERIC(18, 6) NULL
@@ -27,6 +29,8 @@ DECLARE @discrepancyList AS TABLE (
 -- Get the list of discrepancy
 INSERT INTO @discrepancyList (
 	strTransactionId
+	,strBatchId
+	,intInventoryTransactionId
 	,dtmDate
 	,dblICAmount
 	,dblGLAmount
@@ -34,15 +38,23 @@ INSERT INTO @discrepancyList (
 )
 SELECT 
 	ic.strTransactionId
+	,ic.strBatchId
+	,ic.intInventoryTransactionId
 	,ic.dtmDate
 	,ic.[ic amount]
 	,gl.[gl amount]
-	,[diff] = isnull(gl.[gl amount], 0) - isnull(ic.[ic amount], 0)
+	,[diff] = 
+		CASE 
+			WHEN SIGN(ISNULL(ic.[ic amount], 0)) = 1 THEN ISNULL(ic.[ic amount], 0) - ISNULL(gl.[gl amount], 0)
+			ELSE -(ISNULL(ic.[ic amount], 0) - ISNULL(gl.[gl amount], 0))
+		END
 FROM 
 	(
 
 	SELECT	--'IC Only'
 			t.strTransactionId
+			,t.strBatchId
+			,t.intInventoryTransactionId
 			,t.dtmDate
 			,[ic amount] = SUM (ROUND(dbo.fnMultiply(t.dblQty, t.dblCost) + t.dblValue, 2))
 	FROM	tblICInventoryTransaction t INNER JOIN tblICInventoryTransactionType ty
@@ -52,12 +64,16 @@ FROM
 			AND (YEAR(t.dtmDate) = @intYear OR @intYear IS NULL) 
 			AND ty.strName = @strType
 			AND t.ysnIsUnposted = 0 
-	GROUP BY t.strTransactionId, ty.strName, t.dtmDate
+	GROUP BY 
+			t.strTransactionId
+			,t.strBatchId
+			,t.intInventoryTransactionId
+			,t.dtmDate
 	) ic
 	OUTER APPLY (
 		SELECT 
 			--'GL Only'
-			[gl amount] = SUM(ROUND(dblDebit - dblCredit,2))	
+			[gl amount] = SUM(ROUND(dblDebit - dblCredit, 2))	
 		FROM	tblGLDetail gd INNER JOIN tblGLAccount ga
 					ON gd.intAccountId = ga.intAccountId
 				INNER JOIN tblGLAccountSegmentMapping gs
@@ -70,10 +86,14 @@ FROM
 			(MONTH(gd.dtmDate) = @intMonth OR @intMonth IS NULL)
 			AND (YEAR(gd.dtmDate) = @intYear OR @intYear IS NULL) 
 			AND gd.strTransactionType = @strType
+			--AND dbo.fnDateEquals(gd.dtmDate, ic.dtmDate) = 1
 			AND ac.strAccountCategory IN ('Inventory', 'Inventory In-Transit')
 			AND gd.ysnIsUnposted = 0 
 			AND gm.intAccountStructureId = 1
 			AND gd.strTransactionId = ic.strTransactionId
+			AND gd.strBatchId = ic.strBatchId
+			AND gd.intJournalLineNo = ic.intInventoryTransactionId
+			
 	) gl 
 where
 	ic.[ic amount] <> isnull(gl.[gl amount], 0) 
@@ -86,6 +106,8 @@ DECLARE loopDiscrepancy CURSOR LOCAL FAST_FORWARD
 FOR 
 SELECT 
 	strTransactionId
+	,strBatchId
+	,intInventoryTransactionId
 	,dtmDate
 	,dblDiff
 FROM 
@@ -95,6 +117,8 @@ OPEN loopDiscrepancy;
 
 FETCH NEXT FROM loopDiscrepancy INTO 
 	@strTransactionId
+	,@strBatchId
+	,@intInventoryTransactionId
 	,@dtmDate
 	,@dblDiff
 ;
@@ -104,41 +128,42 @@ FETCH NEXT FROM loopDiscrepancy INTO
 -----------------------------------------------------------------------------------------------------------------------------
 WHILE @@FETCH_STATUS = 0
 BEGIN 
-	SET @strBatchId = NULL 
-	SET @intInventoryTransactionId = NULL 
 	DELETE FROM @GLEntries
-	
-	-- Get the IC transaction id
-	SELECT TOP 1
-		@intInventoryTransactionId = t.intInventoryTransactionId
-		,@strBatchId = t.strBatchId 
-	FROM	
-		tblICInventoryTransaction t INNER JOIN tblICInventoryTransactionType ty
-			ON t.intTransactionTypeId = ty.intTransactionTypeId
-	WHERE	
-		t.strTransactionId = @strTransactionId
-		AND ty.strName = @strType
-		AND t.ysnIsUnposted = 0 
 
 	-- Check if the fiscal year period is open
 	IF dbo.isOpenAccountingDate(@dtmDate) = 1
 	BEGIN 
+		--print 'fyp is open'
 		-- When FYP is open, update the existing GL Entries and match it with the value from IC. 
 		UPDATE gd
 		SET 
-			gd.dblDebit = CASE WHEN gd.dblDebit <> 0 THEN gd.dblDebit - @dblDiff ELSE gd.dblDebit END 
-			,gd.dblCredit = CASE WHEN gd.dblCredit <> 0 THEN gd.dblCredit - @dblDiff ELSE gd.dblCredit END 
+			gd.dblDebit = gd.dblDebit + @dblDiff
 		FROM
 			tblGLDetail gd
+			--CROSS APPLY dbo.fnGetDebit(@dblDiff) Debit
 		WHERE	
 			gd.strTransactionId = @strTransactionId
 			AND gd.strBatchId = @strBatchId
 			AND gd.intJournalLineNo = @intInventoryTransactionId
+			AND gd.dblDebit <> 0 
+
+		UPDATE gd
+		SET 
+			gd.dblCredit = gd.dblCredit + @dblDiff
+		FROM
+			tblGLDetail gd
+			--CROSS APPLY dbo.fnGetCredit(@dblDiff) Credit
+		WHERE	
+			gd.strTransactionId = @strTransactionId
+			AND gd.strBatchId = @strBatchId
+			AND gd.intJournalLineNo = @intInventoryTransactionId
+			AND gd.dblCredit <> 0 
 	END 
 
 	-- FYP is closed. 
 	ELSE 
-	BEGIN 
+	BEGIN
+		--print 'fyp is closed' 
 		INSERT INTO @GLEntries (
 			[dtmDate] 
 			,[strBatchId]
@@ -178,8 +203,8 @@ BEGIN
 			[dtmDate] = @dtmTargetOpenDate
 			,[strBatchId] = gd.strBatchId
 			,[intAccountId] = gd.intAccountId
-			,[dblDebit] = CASE WHEN gd.dblDebit <> 0 THEN -@dblDiff ELSE 0 END 
-			,[dblCredit] = CASE WHEN gd.dblCredit <> 0 THEN -@dblDiff ELSE 0 END 
+			,[dblDebit] = Debit.[Value] 
+			,[dblCredit] = Credit.[Value] 
 			,[dblDebitUnit] = 0
 			,[dblCreditUnit] = 0 
 			,[strDescription] = 'Previous year inventory account out of balance correction'
@@ -209,16 +234,64 @@ BEGIN
 			,[strRateType] = ''
 		FROM
 			tblGLDetail gd
+			CROSS APPLY dbo.fnGetDebit(@dblDiff) Debit
+			CROSS APPLY dbo.fnGetCredit(@dblDiff) Credit
 		WHERE	
 			gd.strTransactionId = @strTransactionId
 			AND gd.strBatchId = @strBatchId
 			AND gd.intJournalLineNo = @intInventoryTransactionId
+			AND gd.dblDebit <> 0
+		UNION ALL
+		SELECT 
+			[dtmDate] = @dtmTargetOpenDate
+			,[strBatchId] = gd.strBatchId
+			,[intAccountId] = gd.intAccountId
+			,[dblDebit] = Credit.[Value] 
+			,[dblCredit] = Debit.[Value] 
+			,[dblDebitUnit] = 0
+			,[dblCreditUnit] = 0 
+			,[strDescription] = 'Previous year inventory account out of balance correction'
+			,[strCode] = gd.strCode
+			,[strReference] = gd.strReference
+			,[intCurrencyId] = gd.intCurrencyId
+			,[dblExchangeRate] = gd.dblExchangeRate
+			,[dtmDateEntered] = GETDATE()
+			,[dtmTransactionDate] = @dtmTargetOpenDate --gd.dtmTransactionDate
+			,[strJournalLineDescription] = 'Previous year inventory account out of balance correction'
+			,[intJournalLineNo] = gd.intJournalLineNo
+			,[ysnIsUnposted] = 0 
+			,[intUserId] = gd.intUserId
+			,[intEntityId] = gd.intEntityId
+			,[strTransactionId] = gd.strTransactionId
+			,[intTransactionId] = gd.intTransactionId
+			,[strTransactionType] = gd.strTransactionType
+			,[strTransactionForm] = gd.strTransactionForm
+			,[strModuleName] = gd.strModuleName
+			,[intConcurrencyId] = 1
+			,[dblDebitForeign] = 0
+			,[dblDebitReport] = 0
+			,[dblCreditForeign]	= 0
+			,[dblCreditReport] = 0
+			,[dblReportingRate]	= gd.dblReportingRate
+			,[dblForeignRate] = gd.dblForeignRate
+			,[strRateType] = ''
+		FROM
+			tblGLDetail gd
+			CROSS APPLY dbo.fnGetDebit(@dblDiff) Debit
+			CROSS APPLY dbo.fnGetCredit(@dblDiff) Credit
+		WHERE	
+			gd.strTransactionId = @strTransactionId
+			AND gd.strBatchId = @strBatchId
+			AND gd.intJournalLineNo = @intInventoryTransactionId
+			AND gd.dblCredit <> 0
 
 		EXEC dbo.uspGLBookEntries @GLEntries, 1
 	END 
 
 	FETCH NEXT FROM loopDiscrepancy INTO 
 		@strTransactionId
+		,@strBatchId
+		,@intInventoryTransactionId
 		,@dtmDate
 		,@dblDiff
 	;
