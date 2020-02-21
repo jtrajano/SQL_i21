91c0f77622fa5cba0bcec6a98c4b415dbd7d4368
bonn_ponce <bonn.ponce@irely.com>
2020-02-21 16:51:12 +0800

    CF-2495
    (cherry picked from commit 1dd8a118fa8680cc73c74bf93a3b767a67175963)

--- i21Database/dbo/Tables/tblCFInvoiceStagingTable.sql
+++ i21Database/dbo/Tables/tblCFInvoiceStagingTable.sql
@@ -159,6 +159,7 @@
     [ysnShowVehicleDescriptionOnly]   BIT             NULL,
     [ysnShowDriverPinDescriptionOnly] BIT             NULL,
 	[ysnPageBreakByPrimarySortOrder]  BIT			  NULL,
+    [strProductDescriptionForTotals]  NVARCHAR (MAX)  COLLATE Latin1_General_CI_AS NULL,
     CONSTRAINT [UserAndTransactionIdAndStatementType] UNIQUE NONCLUSTERED ([intTransactionId] ASC, [strUserId] ASC, [strStatementType] ASC) WITH (FILLFACTOR = 70)
 );
 
