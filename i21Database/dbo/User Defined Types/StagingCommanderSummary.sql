2094a042991ff2f440ddc8e90b325f16be2aebab
bonn_ponce <bonn.ponce@irely.com>
2020-02-26 16:48:48 +0800

    ST-1628

---
+++ i21Database/dbo/User Defined Types/StagingCommanderSummary.sql
@@ -0,0 +1,11 @@
+﻿CREATE TYPE StagingCommanderSummary AS TABLE
+(
+	[intRowCount] 								INT					NULL,
+	[strSysId] 								    NVARCHAR(MAX)		NULL,
+	[dblMopInfoAmount]                     		NUMERIC(18,6)		NULL,
+	[dblMopInfoCount]                  			NUMERIC(18,6)		NULL,
+	[dblSummaryInfoCustomerCount]               INT					NULL,
+	[dblSummaryInfoTotalSalesTaxes]             NUMERIC(18,6)		NULL,
+	[dblSummaryInfoNoSaleCount]                 NUMERIC(18,6)		NULL,
+	[dblSummaryInfoTotalPaymentOut]				INT					NULL
+)
