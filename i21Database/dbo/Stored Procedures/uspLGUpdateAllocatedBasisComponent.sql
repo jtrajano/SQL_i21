5b812ce1ecb331f77597daf1e08a86a699d2b60d
Arman Arenas <arman.arenas@irely.com>
2020-02-26 12:01:10 +0800

    LG-2540 LG-2543 implement decimal rounding; apply logic to basis contract only

--- i21Database/dbo/Stored Procedures/uspLGUpdateAllocatedBasisComponent.sql
+++ i21Database/dbo/Stored Procedures/uspLGUpdateAllocatedBasisComponent.sql
@@ -8,6 +8,7 @@ DECLARE @intSContractDetailId INT
 DECLARE @intPContractDetailId INT
 DECLARE @intBasisUnitMeasureId INT
 DECLARE @intBasisCurrencyId INT
+DECLARE @intBasisDecimals INT
 DECLARE @intSubCurrencyCents INT = 1
 
 SELECT TOP 1 
@@ -25,12 +26,17 @@ SELECT @intSContractDetailId = ALD.intSContractDetailId
 		,@intBasisUnitMeasureId = UOM.intUnitMeasureId
 		,@intBasisCurrencyId = SCD.intBasisCurrencyId
 		,@intSubCurrencyCents = ISNULL(BCUR.intCent, 1)
+		,@intBasisDecimals = ISNULL(FM.intNoOfDecimal, CP.intCurrencyDecimal)
 FROM tblLGAllocationDetail ALD
 	INNER JOIN tblCTContractDetail PCD ON PCD.intContractDetailId = ALD.intPContractDetailId
 	INNER JOIN tblCTContractDetail SCD ON SCD.intContractDetailId = ALD.intSContractDetailId
+	INNER JOIN tblCTContractHeader SCH ON SCH.intContractHeaderId = SCD.intContractHeaderId
 	LEFT JOIN tblICItemUOM UOM ON UOM.intItemUOMId = SCD.intBasisUOMId
 	LEFT JOIN tblSMCurrency BCUR ON BCUR.intCurrencyID = SCD.intBasisCurrencyId
+	LEFT JOIN tblRKFutureMarket FM ON FM.intFutureMarketId = SCD.intFutureMarketId
+	OUTER APPLY (SELECT TOP 1 intCurrencyDecimal FROM tblSMCompanyPreference) CP
 WHERE intAllocationDetailId = @intAllocationDetailId
+	AND SCH.intPricingTypeId = 2
 
 /* Place Reserve B Components with zero rate to temp table */
 SELECT 
@@ -41,36 +47,37 @@ FROM tblCTContractCost CC
 WHERE intContractDetailId = @intSContractDetailId
 	AND I.intCategoryId = @intReserveBCategoryId 
 
-/* Apply Reserves B Standard Costs */
-UPDATE RB
-SET RB.dblRate = CASE WHEN ISNULL(RB.dblRate, 0) = 0 THEN
-						CASE WHEN (I.intCostUOMId IS NULL) 
-							THEN I.dblAmount * @intSubCurrencyCents
-						WHEN (I.intCostUOMId <> BUOM.intItemUOMId) 
-							THEN dbo.fnCalculateCostBetweenUOM(I.intCostUOMId, BUOM.intItemUOMId, I.dblAmount) * @intSubCurrencyCents
-						ELSE I.dblAmount * @intSubCurrencyCents END 
-					ELSE RB.dblRate END
-	,RB.intItemUOMId = BUOM.intItemUOMId
-FROM #tmpReserveB RB
-INNER JOIN tblICItem I ON RB.intItemId = I.intItemId
-LEFT JOIN tblICItemUOM UOM ON UOM.intItemUOMId = RB.intItemUOMId
-OUTER APPLY (SELECT intItemUOMId FROM tblICItemUOM WHERE intItemId = RB.intItemId AND intUnitMeasureId = @intBasisUnitMeasureId) BUOM
+IF EXISTS(SELECT TOP 1 1 FROM #tmpReserveB)
+BEGIN
+	/* Apply Reserves B Standard Costs */
+	UPDATE RB
+	SET RB.dblRate = CASE WHEN ISNULL(RB.dblRate, 0) = 0 THEN
+							CASE WHEN (I.intCostUOMId IS NULL) 
+								THEN I.dblAmount * @intSubCurrencyCents
+							WHEN (I.intCostUOMId <> BUOM.intItemUOMId) 
+								THEN dbo.fnCalculateCostBetweenUOM(I.intCostUOMId, BUOM.intItemUOMId, I.dblAmount) * @intSubCurrencyCents
+							ELSE I.dblAmount * @intSubCurrencyCents END 
+						ELSE RB.dblRate END
+		,RB.intItemUOMId = BUOM.intItemUOMId
+	FROM #tmpReserveB RB
+	INNER JOIN tblICItem I ON RB.intItemId = I.intItemId
+	LEFT JOIN tblICItemUOM UOM ON UOM.intItemUOMId = RB.intItemUOMId
+	OUTER APPLY (SELECT intItemUOMId FROM tblICItemUOM WHERE intItemId = RB.intItemId AND intUnitMeasureId = @intBasisUnitMeasureId) BUOM
 
-/* Update Sales Contract Basis Components with the recalculated amounts */
-UPDATE CC
-SET CC.dblRate = RB.dblRate
-	,CC.intItemUOMId = RB.intItemUOMId
-FROM tblCTContractCost CC
-INNER JOIN #tmpReserveB RB ON CC.intContractCostId = RB.intContractCostId
+	/* Update Sales Contract Basis Components with the recalculated amounts */
+	UPDATE CC
+	SET CC.dblRate = ROUND(RB.dblRate, @intBasisDecimals)
+		,CC.intItemUOMId = RB.intItemUOMId
+	FROM tblCTContractCost CC
+	INNER JOIN #tmpReserveB RB ON CC.intContractCostId = RB.intContractCostId
 
-/* Recalculate Sales Basis Total and Contract Cash Price*/
-UPDATE SCD
-	SET dblBasis = BS.dblBasisTotal
-		,dblCashPrice = SCD.dblFutures + BS.dblBasisTotal
-FROM tblCTContractDetail SCD
-OUTER APPLY (SELECT dblBasisTotal = SUM(dblRate) FROM tblCTContractCost 
-			WHERE ysnBasis = 1 AND intContractDetailId = @intSContractDetailId) BS
-WHERE SCD.intContractDetailId = @intSContractDetailId
-
+	/* Recalculate Sales Basis Total and Contract Cash Price*/
+	UPDATE SCD
+		SET dblBasis = BS.dblBasisTotal
+			,dblCashPrice = SCD.dblFutures + BS.dblBasisTotal
+	FROM tblCTContractDetail SCD
+	OUTER APPLY (SELECT dblBasisTotal = SUM(dblRate) FROM tblCTContractCost 
+				WHERE ysnBasis = 1 AND intContractDetailId = @intSContractDetailId) BS
+	WHERE SCD.intContractDetailId = @intSContractDetailId
+END
 GO
-
