d0eabd7d0d9462c57489c94ba3d9245238c54857
crisman_lazaga <cris.lazaga@irely.com>
2020-02-26 16:42:26 +0800

    CCR-410
    (cherry picked from commit 6a749ea2f9afd7e1d21a2200778bd44d4d975640)

--- i21Database/dbo/Views/vyuCCCustomer.sql
+++ i21Database/dbo/Views/vyuCCCustomer.sql
@@ -1,7 +1,24 @@
 ﻿CREATE VIEW [dbo].[vyuCCCustomer]
 WITH SCHEMABINDING
 AS
-	select
+SELECT A.intSiteId,
+	A.intDealerSiteId,
+	A.intCompanyOwnedSiteId,
+	A.strSite,
+	A.strSiteDescription,
+	A.intPaymentMethodId,
+	A.intCustomerId,
+	B.intTermsId,
+	B.intSalespersonId,
+	C.strEntityNo AS strCustomerEntityNo,
+	C.strName AS strCustomerName
+	FROM dbo.tblCCSite AS A
+	INNER JOIN dbo.tblARCustomer AS B
+		ON B.intEntityId = A.intCustomerId
+	INNER JOIN dbo.tblEMEntity AS C
+		ON C.intEntityId = B.intEntityId
+		
+/*	select
 		intSiteId,
 		intDealerSiteId,
 		intCompanyOwnedSiteId,
@@ -35,6 +52,7 @@ AS
 				LEFT OUTER JOIN dbo.[tblEMEntityLocation] AS D
 					ON D.intEntityId = B.[intEntityId] AND D.ysnDefaultLocation = 1
 		)as result 
+*/
 /*
 SELECT A.intSiteId,
        A.intDealerSiteId,
