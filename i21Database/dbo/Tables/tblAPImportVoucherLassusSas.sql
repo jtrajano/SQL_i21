26e4c22465e8ae7f4676dac1731d1ce4bc1591f5
Erick F Dela Cruz <erick.delacruz@irely.com>
2020-02-27 19:12:38 +0800

    LassusSasv1

---
+++ i21Database/dbo/Tables/tblAPImportVoucherLassusSas.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tblAPImportVoucherLassusSas]
+(
+	[strStoreNumber] NVARCHAR(200) NOT NULL,
+	[dtmDate] NVARCHAR(100) NOT NULL,
+	[strSasInvoiceNumber] NVARCHAR(100) NULL,
+	[strInvoiceNumber] NVARCHAR(100) NOT NULL,
+	[dblAmount] NVARCHAR(500) NOT NULL,
+	[strNotUsed] NVARCHAR(100) NULL,
+	[strGLAccount] NVARCHAR(100) NOT NULL,
+	[strDistributionType] NVARCHAR(50) NOT NULL
+)
