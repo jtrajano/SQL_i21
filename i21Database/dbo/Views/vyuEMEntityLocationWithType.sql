8bd048ecbd3ac964a318b5efee1e1d1b6b4722e9
kelvin_quinto <kelvin.quinto@irely.com>
2020-02-28 10:52:47 +0800

    AR-11554
    (cherry picked from commit 56e28ebbbad2c52ed2b65be28630483a520b5fbb)
    (cherry picked from commit 1155f92587356e0de9f227df72735a6a7e51b61a)

--- i21Database/dbo/Views/vyuEMEntityLocationWithType.sql
+++ i21Database/dbo/Views/vyuEMEntityLocationWithType.sql
@@ -1,61 +1,67 @@
 ﻿CREATE VIEW [dbo].[vyuEMEntityLocationWithType]
-	AS
-		select 
-			a.intEntityLocationId,
-			a.intEntityId,
-			a.strLocationName,
-			a.strAddress,
-			a.strCity,
-			a.strCountry,
-			a.strState,
-			a.strZipCode,
-			strSupplyPoint = a.strLocationName,
-			c.intRackPriceSupplyPointId,
-			c.strGrossOrNet,
-			a.ysnActive,
-			b.Vendor,
-			b.Customer,
-			b.Salesperson,
-			b.FuturesBroker,
-			b.ForwardingAgent,
-			b.Terminal,
-			b.ShippingLine,
-			b.Trucker,
-			b.ShipVia,
-			b.Insurer,
-			b.Employee,
-			b.Producer,
-			b.[User],
-			b.Prospect,
-			b.Competitor,
-			b.Buyer,
-			b.[Partner],
-			b.Lead,
-			b.Veterinary,
-			b.Lien,
-			--Added by Jayson
-			strRackPriceSupplyPoint = g.strLocationName,
-			e.intTerminalControlNumberId,
-			e.strTerminalControlNumber,
-			c.strFuelDealerId1,
-			c.strFuelDealerId2,
-			c.strDefaultOrigin,
-			c.ysnMultipleDueDates,
-			a.intTaxGroupId,
-			h.strTaxGroup,
-			c.intSupplyPointId
-			--End of Added by Jayson
-		from tblEMEntityLocation a
-			join vyuEMEntityType b
-				on a.intEntityId = b.intEntityId
-			left join tblTRSupplyPoint c
-				on c.intEntityLocationId = a.intEntityLocationId 
-					and c.intEntityVendorId = a.intEntityId
-			left join tblTFTerminalControlNumber e
-				on e.intTerminalControlNumberId = c.intTerminalControlNumberId
-			left join tblTRSupplyPoint f
-				on f.intSupplyPointId = c.intRackPriceSupplyPointId
-			left join tblEMEntityLocation g
-				on g.intEntityLocationId = f.intEntityLocationId
-			left join tblSMTaxGroup h
-				on h.intTaxGroupId = a.intTaxGroupId
+AS
+SELECT intEntityLocationId			= EL.intEntityLocationId
+	 , intEntityId					= EL.intEntityId
+	 , strLocationName				= EL.strLocationName
+	 , strAddress					= EL.strAddress
+	 , strCity						= EL.strCity
+	 , strCountry					= EL.strCountry
+	 , strState						= EL.strState
+	 , strZipCode					= EL.strZipCode
+	 , strSupplyPoint				= EL.strLocationName
+	 , intRackPriceSupplyPointId	= SP.intRackPriceSupplyPointId
+	 , strGrossOrNet				= SP.strGrossOrNet
+	 , ysnActive					= EL.ysnActive
+	 , Vendor						= ET.Vendor
+	 , Customer						= ET.Customer
+	 , Salesperson					= ET.Salesperson
+	 , FuturesBroker				= ET.FuturesBroker
+	 , ForwardingAgent				= ET.ForwardingAgent
+	 , Terminal						= ET.Terminal
+	 , ShippingLine					= ET.ShippingLine
+	 , Trucker						= ET.Trucker
+	 , ShipVia						= ET.ShipVia
+	 , Insurer						= ET.Insurer
+	 , Employee						= ET.Employee
+	 , Producer						= ET.Producer
+	 , [User]						= ET.[User]
+	 , Prospect						= ET.Prospect
+	 , Competitor					= ET.Competitor
+	 , Buyer						= ET.Buyer
+	 , [Partner]					= ET.[Partner]
+	 , Lead							= ET.Lead
+	 , Veterinary					= ET.Veterinary
+	 , Lien							= ET.Lien
+	 , strRackPriceSupplyPoint		= SPEL.strLocationName
+	 , intTerminalControlNumberId	= TCN.intTerminalControlNumberId
+	 , strTerminalControlNumber		= TCN.strTerminalControlNumber
+	 , strFuelDealerId1				= SP.strFuelDealerId1
+	 , strFuelDealerId2				= SP.strFuelDealerId2
+	 , strDefaultOrigin				= SP.strDefaultOrigin
+	 , ysnMultipleDueDates			= SP.ysnMultipleDueDates
+	 , intTaxGroupId				= EL.intTaxGroupId
+	 , strTaxGroup					= TG.strTaxGroup
+	 , intSupplyPointId				= SP.intSupplyPointId
+	 , intSalespersonId				= SALESPERSON.intEntityId
+	 , strSalespersonId				= SALESPERSON.strSalespersonId
+	 , strSalesPersonName			= SALESPERSON.strSalesPersonName
+FROM tblEMEntityLocation EL
+JOIN vyuEMEntityType ET ON EL.intEntityId = ET.intEntityId
+LEFT JOIN tblTRSupplyPoint SP ON SP.intEntityLocationId = EL.intEntityLocationId 
+							 AND SP.intEntityVendorId = EL.intEntityId
+LEFT JOIN tblTFTerminalControlNumber TCN ON TCN.intTerminalControlNumberId = SP.intTerminalControlNumberId
+LEFT JOIN tblTRSupplyPoint RPSP ON SP.intSupplyPointId = SP.intRackPriceSupplyPointId
+LEFT JOIN tblEMEntityLocation SPEL ON SPEL.intEntityLocationId = RPSP.intEntityLocationId
+LEFT JOIN tblSMTaxGroup TG ON TG.intTaxGroupId = EL.intTaxGroupId
+LEFT JOIN (
+	SELECT S.intEntityId
+		 , strSalespersonId	    = CASE WHEN ISNULL(S.strSalespersonId, '') = '' THEN ST.strEntityNo ELSE S.strSalespersonId END
+		 , strSalesPersonName	= ST.strName
+	FROM dbo.tblARSalesperson S WITH (NOLOCK)
+	INNER JOIN (
+		SELECT intEntityId
+			 , strName
+			 , strEntityNo
+		FROM dbo.tblEMEntity WITH (NOLOCK)
+	) ST on S.intEntityId = ST.intEntityId
+) SALESPERSON ON SALESPERSON.intEntityId = EL.intSalespersonId
