ebed4575b236b8c4acae7061a301fa0b4c710c66
Premkumar S <prem.selladurai@irely.com>
2020-03-02 18:42:44 +0530

    IN-480

--- i21Database/dbo/Tables/tblIPInventoryReceiptWeightClaim.sql
+++ i21Database/dbo/Tables/tblIPInventoryReceiptWeightClaim.sql
@@ -1,5 +1,6 @@
 ﻿CREATE TABLE tblIPInventoryReceiptWeightClaim (
 	intInventoryReceiptWeightClaimId INT IDENTITY(1, 1) CONSTRAINT [PK_tblIPInventoryReceiptWeightClaim_intInventoryReceiptWeightClaimId] PRIMARY KEY
 	,intInventoryReceiptId INT
+	,intLoadId INT
 	,dtmCreated DATETIME CONSTRAINT DF_tblIPInventoryReceiptWeightClaim_dtmCreated DEFAULT GETDATE()
 	)
