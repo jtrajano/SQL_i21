a1ca0b8a5c727ff0be4193ce08d675fc0a291de8
Jeff Trajano <jeffrey.trajano@irely.com>
2020-03-04 12:45:12 +0800

    AP-6617 Pay Voucher Detail - Should have deposit button

--- i21Database/dbo/User Defined Types/BillPayment.sql
+++
@@ -1,7 +0,0 @@
-/****** Object:  UserDefinedTableType [dbo].[BillPayment]    Script Date: 02/03/2020 9:50:56 PM ******/
-CREATE TYPE [dbo].[BillPayment] AS TABLE(
-	[intBillId] [int],
-	[dblPayment] [decimal](18, 2) 
-)
-GO
-
