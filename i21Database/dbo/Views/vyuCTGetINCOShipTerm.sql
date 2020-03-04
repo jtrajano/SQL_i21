7726713979637b89b826cd0a759a137c01fd74fb
jayson.ramos <jayson.ramos@irely.com>
2020-03-04 15:22:46 +0800

    CT-4442

--- i21Database/dbo/Views/vyuCTGetINCOShipTerm.sql
+++ i21Database/dbo/Views/vyuCTGetINCOShipTerm.sql
@@ -2,11 +2,21 @@
 
 AS 
 
-		SELECT  CB.*,
+SELECT 
+				intContractBasisId = CB.intFreightTermId,
+				strContractBasis = CB.strFreightTerm,
+				CB.strDescription,
+				CB.ysnDefault,
+				CB.intInsuranceById,
+				CB.intInvoiceTypeId,
+				CB.intPositionId,
+				CB.intConcurrencyId,
+				CB.strINCOLocationType,
+				CB.ysnActive,
 				IB.strInsuranceBy,
 				IT.strInvoiceType,
 				PO.strPosition
-	   FROM		tblCTContractBasis	 CB
+	   FROM		tblSMFreightTerms	 CB
   LEFT JOIN		tblCTInsuranceBy	 IB	ON  IB.intInsuranceById	    =   CB.intInsuranceById
   LEFT JOIN		tblCTInvoiceType	 IT ON  IT.intInvoiceTypeId	    =   CB.intInvoiceTypeId
   LEFT JOIN		tblCTPosition		 PO	ON  PO.intPositionId	    =   CB.intPositionId
