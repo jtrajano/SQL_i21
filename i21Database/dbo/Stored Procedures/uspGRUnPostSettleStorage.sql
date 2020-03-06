7c41dc169d81cd9b10bc847713b78231f11ad344
Mon Gonzales <mon.gonzales@irely.com>
2020-03-06 14:02:18 +0800

    GRN-2043 - HDTN-243066 - No Voucher created for Settlement STR-4348

--- i21Database/dbo/Stored Procedures/uspGRUnPostSettleStorage.sql
+++ i21Database/dbo/Stored Procedures/uspGRUnPostSettleStorage.sql
@@ -517,6 +517,16 @@ BEGIN TRY
 			END
 
 			DELETE FROM tblGRSettleStorage WHERE intSettleStorageId = @intSettleStorageId
+
+			---This is just to insure that the parent is delete if there is no child in existence
+			IF @isParentSettleStorage = 0
+			BEGIN
+				IF NOT EXISTS(SELECT 1 FROM tblGRSettleStorage WHERE intParentSettleStorageId = @intParentSettleStorageId)
+				BEGIN				
+					DELETE FROM tblGRSettleStorage WHERE intSettleStorageId = @intParentSettleStorageId
+				END
+			END
+
 			--5. Removing Voucher
 			BEGIN
 			IF @BillId IS NOT NULL
