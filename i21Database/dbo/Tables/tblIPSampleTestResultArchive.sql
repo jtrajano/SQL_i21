b4a34f782f1764042a295c615a0bcd963a6116d8
Vinoth Kumar T <vinoth.t@irely.com>
2020-03-09 21:34:32 +0530

    IN-350

---
+++ i21Database/dbo/Tables/tblIPSampleTestResultArchive.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE tblIPSampleTestResultArchive
+(
+	intStageSampleTestResultId	INT IDENTITY(1,1),
+	intStageSampleId			INT NOT NULL,
+	strSampleNumber				NVARCHAR(30) COLLATE Latin1_General_CI_AS,
+	strTestName					NVARCHAR(50) COLLATE Latin1_General_CI_AS,
+	strPropertyName				NVARCHAR(100) COLLATE Latin1_General_CI_AS,
+	strActualValue				NVARCHAR(MAX) COLLATE Latin1_General_CI_AS,
+	strTestComment				NVARCHAR(MAX) COLLATE Latin1_General_CI_AS,
+
+	CONSTRAINT [PK_tblIPSampleTestResultArchive_intStageSampleTestResultId] PRIMARY KEY ([intStageSampleTestResultId]),
+	CONSTRAINT [FK_tblIPSampleTestResultArchive_tblIPSampleArchive_intStageSampleId] FOREIGN KEY ([intStageSampleId]) REFERENCES [tblIPSampleArchive]([intStageSampleId]) ON DELETE CASCADE
+)
