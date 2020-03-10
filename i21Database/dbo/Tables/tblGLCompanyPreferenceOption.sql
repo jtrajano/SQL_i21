dd7f5f380e9c82149b6011cfaf98f0781e8452c9
Jeff Trajano <jeffrey.trajano@irely.com>
2020-03-10 13:36:24 +0800

    GL-7335 Accounting Period Configuration setup

--- i21Database/dbo/Tables/tblGLCompanyPreferenceOption.sql
+++ i21Database/dbo/Tables/tblGLCompanyPreferenceOption.sql
@@ -10,6 +10,7 @@
 	[intDefaultVisibleOldAccountSystemId] INT NULL,
 	[intDBVersion] INT NULL,
 	[ysnHistoricalJournalImported] BIT NULL,
+	[ysnShowAccountingPeriod] BIT NULL,
     CONSTRAINT [PK_tblGLCompanyPreferenceOption] PRIMARY KEY ([intCompanyPreferenceOptionId])
 )
 GO
@@ -32,4 +33,6 @@ GO
 EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'SQL Database version (year)' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'tblGLCompanyPreferenceOption', @level2type=N'COLUMN',@level2name=N'intDBVersion' 
 GO
 EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Indicates if Historical journal importing was already performed' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'tblGLCompanyPreferenceOption', @level2type=N'COLUMN',@level2name=N'ysnHistoricalJournalImported' 
+GO
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Indicates if Accounting Period is shown in related search grids' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'tblGLCompanyPreferenceOption', @level2type=N'COLUMN',@level2name=N'ysnShowAccountingPeriod' 
 GO
