621716fc099d7091c133a64e93f2281902fe7e6c
Mon Gonzales <mon.gonzales@irely.com>
2020-03-10 10:36:45 +0800

    GRN-2039 - [Open Clearing Detail] - STR still appears in the report after pricing the contract

---
+++ i21Database/dbo/Tables/tblGROldTransactionMapping.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[tblGROldTransactionMapping]
+(
+
+	intId						int identity(1,1) primary key,
+	intSettleStorageId			int not null,
+	intSettleStorageTicketId	int not null,
+	intSettleContractId			int not null
+		
+)
