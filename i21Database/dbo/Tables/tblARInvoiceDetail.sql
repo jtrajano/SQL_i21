68602c6bad56d75798075b5004548163f0ca0201
Feb Montefrio <feb.montefrio@irely.com>
2020-03-11 11:24:43 +0800

    [B] IC-8303

--- i21Database/dbo/Tables/tblARInvoiceDetail.sql
+++ i21Database/dbo/Tables/tblARInvoiceDetail.sql
@@ -180,3 +180,8 @@ CREATE NONCLUSTERED INDEX [IX_tblARInvoiceDetail_forStockRebuild]
 	ON [dbo].[tblARInvoiceDetail] (intInvoiceDetailId, intItemId)
 	INCLUDE ([intInvoiceId])
 GO
+
+CREATE NONCLUSTERED INDEX [IX_tblARInvoiceDetail_intInventoryShipmentChargeId]
+	ON [dbo].[tblARInvoiceDetail] ([intInventoryShipmentChargeId])
+	INCLUDE ([intInvoiceId])
+GO
