c1a8471dc6b19c1a115c8c9de7755f6769d8d7e7
Premkumar S <prem.selladurai@irely.com>
2020-03-12 15:35:46 +0530

    IN-455

--- i21Database/dbo/Views/vyuIPGetInvoiceDetail.sql
+++ i21Database/dbo/Views/vyuIPGetInvoiceDetail.sql
@@ -1,12 +1,8 @@
 ﻿CREATE VIEW dbo.vyuIPGetInvoiceDetail
 AS
-SELECT strTransactionType
-	,B.strBook
-	,SB.strSubBook
-	,IV.strInvoiceNumber
-	,I.strItemNo
-	,CH.strContractNumber
-	,CD.intContractSeq
+SELECT I.strItemNo
+	,IVD.intContractHeaderId
+	,IVD.intContractDetailId
 	,IVD.dblQtyOrdered
 	,IVD.dblQtyShipped
 	,UM.strUnitMeasure
@@ -21,8 +17,6 @@ JOIN dbo.tblARInvoiceDetail IVD ON IV.intInvoiceId = IVD.intInvoiceId
 JOIN dbo.tblCTBook B ON B.intBookId = IV.intBookId
 LEFT JOIN dbo.tblCTSubBook SB ON SB.intSubBookId = IV.intSubBookId
 JOIN dbo.tblICItem I ON I.intItemId = IVD.intItemId
-JOIN dbo.tblCTContractHeader CH ON CH.intContractHeaderId = IVD.intContractHeaderId
-JOIN dbo.tblCTContractDetail CD ON CD.intContractDetailId = IVD.intContractDetailId
 JOIN dbo.tblICItemUOM IU ON IU.intItemUOMId = IVD.intItemUOMId
 JOIN dbo.tblICUnitMeasure UM ON UM.intUnitMeasureId = IU.intUnitMeasureId
 Left JOIN dbo.tblICItemUOM WIU ON WIU.intItemUOMId = IVD.intItemWeightUOMId
