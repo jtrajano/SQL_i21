5672093761668bef21b6220d46d2accf337a962d
leslie_arat <leslie.arat@irely.com>
2020-03-12 14:21:27 +0800

    GRN-2048 (Transaction Qty in the DP Contract is positive after settling multiple storages)

--- i21Database/dbo/Stored Procedures/uspGRPostSettleStorage.sql
+++ i21Database/dbo/Stored Procedures/uspGRPostSettleStorage.sql
@@ -3479,7 +3479,7 @@ BEGIN TRY
 							
 						if( (@dblCurrentContractBalance) + (@dblUnits)  < 0.01)
 						begin
-							set @dblUnits = @dblCurrentContractBalance
+							set @dblUnits = @dblCurrentContractBalance * -1
 						end
 						
 
