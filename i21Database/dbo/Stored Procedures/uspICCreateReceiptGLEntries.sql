26c0ef1171fa83ff6a6a236663b7c04128ba56a2
Feb Montefrio <feb.montefrio@irely.com>
2020-03-14 00:27:30 +0800

    [B] IC-8276

--- i21Database/dbo/Stored Procedures/uspICCreateReceiptGLEntries.sql
+++ i21Database/dbo/Stored Procedures/uspICCreateReceiptGLEntries.sql
@@ -389,7 +389,7 @@ AS
 			,ISNULL(t.intCurrencyId, @intFunctionalCurrencyId) intCurrencyId
 			,t.dblExchangeRate
 			,t.intInventoryTransactionId
-			,strInventoryTransactionTypeName = TransType.strName
+			,strInventoryTransactionTypeName = t.strTransactionType
 			,t.strTransactionForm 
 			,strDescription = 
 				dbo.fnFormatMessage(
@@ -479,17 +479,20 @@ AS
 			) topRi 		
 
 			CROSS APPLY (
-				SELECT TOP 1 t.* 
-				FROM tblICInventoryTransaction t 
+				SELECT TOP 1 
+					t.* 
+					,strTransactionType = ty.strName
+				FROM 
+					tblICInventoryTransaction t INNER JOIN tblICInventoryTransactionType ty
+						ON t.intTransactionTypeId = ty.intTransactionTypeId
 				WHERE
 					t.strTransactionId = ri.strReceiptNumber
 					AND t.intTransactionId = ri.intInventoryReceiptId
 					AND t.intTransactionDetailId = ri.intInventoryReceiptItemId
+					AND ty.strName = 'Inventory Receipt'
 				ORDER BY t.intInventoryTransactionId DESC 
 			) t
 
-			INNER JOIN dbo.tblICInventoryTransactionType TransType
-				ON t.intTransactionTypeId = TransType.intTransactionTypeId
 			LEFT JOIN tblSMCurrencyExchangeRateType currencyRateType
 				ON currencyRateType.intCurrencyExchangeRateTypeId = t.intForexRateTypeId
 
