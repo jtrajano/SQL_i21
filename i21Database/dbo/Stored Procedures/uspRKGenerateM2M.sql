680fc25d7739839a3b1c800ed20f8cda36fde026
lawrence_zabala <lawrence.zabala@irely.com>
2020-03-17 09:17:02 +0800

    RM-3526

---
+++ i21Database/dbo/Stored Procedures/uspRKGenerateM2M.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [dbo].[uspRKGenerateM2M]
+	@param1 int = 0,
+	@param2 int
+AS
+	SELECT @param1, @param2
+RETURN 0
---
+++ i21Database/dbo/Tables/tblRKM2MTransaction.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[tblRKM2MTransaction]
+(
+	[intM2MTransactionId] INT NOT NULL IDENTITY, 
+    [intM2MHeaderId] INT NOT NULL, 
+    CONSTRAINT [PK_tblRKM2MTransaction] PRIMARY KEY ([intM2MTransactionId]), 
+    CONSTRAINT [FK_tblRKM2MTransaction_tblRKM2MHeader] FOREIGN KEY ([intM2MHeaderId]) REFERENCES [tblRKM2MHeader]([intM2MHeaderId]) ON DELETE CASCADE
+)
