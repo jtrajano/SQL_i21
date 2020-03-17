863f2840896c14c91c1b439e102ee491fc6d7001
bonn ponce <bonn.ponce@irely.com>
2020-03-17 09:31:55 +0800

    ST-932

--- i21Database/dbo/Tables/tblSTSubcategoryRegProd.sql
+++ i21Database/dbo/Tables/tblSTSubcategoryRegProd.sql
@@ -6,5 +6,5 @@
     [strRegProdComment] NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NULL, 
     [intConcurrencyId] INT NOT NULL, 
     CONSTRAINT [PK_tblSTSubcategoryRegProd] PRIMARY KEY CLUSTERED ([intRegProdId] ASC),
-    --CONSTRAINT [AK_tblSTSubcategoryRegProd_strRegProdCode] UNIQUE NONCLUSTERED ([strRegProdCode] ASC), 
+    CONSTRAINT [AK_tblSTSubcategoryRegProd_strRegProdCode] UNIQUE NONCLUSTERED ([strRegProdCode] ASC), 
 );
