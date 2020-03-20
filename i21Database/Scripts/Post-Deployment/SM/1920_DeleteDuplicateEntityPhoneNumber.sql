db22ce34b1e60eefc4ac9596daddfa8190adaf6a
IRELY\JMendones <jesmar.mendones@irely.com>
2020-03-20 16:31:26 +0800

    SM-4859

---
+++ i21Database/Scripts/Post-Deployment/SM/1920_DeleteDuplicateEntityPhoneNumber.sql
@@ -0,0 +1,14 @@
+﻿print 'START DELETE DUPLICATE ENTITY PHONE NUMBER'
+
+EXEC(
+'with cte as
+(
+	select ROW_NUMBER() over(partition by intEntityId order by intEntityId) as [pk], intEntityId, strPhone
+	from tblEMEntityPhoneNumber
+) 
+delete cte where [pk] > 1 and ISNULL([strPhone], '''') = ''''
+
+'
+)
+
+print 'END DELETE DUPLICATE ENTITY PHONE NUMBER'
