2e3ac4298bac4288082f182d50633430842db312
IRELY\JMendones <jesmar.mendones@irely.com>
2020-03-20 16:31:26 +0800

    SM-4859

--- i21Database/Scripts/Post-Deployment/PostDeployment.sql
+++ i21Database/Scripts/Post-Deployment/PostDeployment.sql
@@ -91,9 +91,7 @@ print 'BEGIN POST DEPLOYMENT'
 :r .\SM\1910_UpdateNoSpaceModuleName.sql
 :r .\SM\1910_DeleteDuplicateUserPreference.sql
 :r .\SM\2010_RemoveF9Shortcut.sql
-
-
-
+:r .\SM\1920_DeleteDuplicateEntityPhoneNumber.sql
 
 
 -- Canned Report
