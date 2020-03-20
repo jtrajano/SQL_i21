0c699279b5f86e781b392db7e404505fa72bd963
Vinoth Kumar T <vinoth.t@irely.com>
2020-03-20 22:34:19 +0530

    IN-460

---
+++ i21Database/dbo/Tables/tblIPLoadDetailError.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE tblIPLoadDetailError
+(
+	intStageLoadDetailId		INT IDENTITY(1,1),
+	intStageLoadId				INT NOT NULL,
+	strCustomerReference		NVARCHAR(100) COLLATE Latin1_General_CI_AS,
+	strCommodityCode			NVARCHAR(50) COLLATE Latin1_General_CI_AS,
+	strItemNo					NVARCHAR(50) COLLATE Latin1_General_CI_AS,
+	strContractItemName			NVARCHAR(100) COLLATE Latin1_General_CI_AS,
+	dblQuantity					NUMERIC(18, 6),
+	dblGrossWeight				NUMERIC(18, 6),
+	strPackageType				NVARCHAR(50) COLLATE Latin1_General_CI_AS,
+
+	CONSTRAINT [PK_tblIPLoadDetailError_intStageLoadDetailId] PRIMARY KEY ([intStageLoadDetailId]),
+	CONSTRAINT [FK_tblIPLoadDetailError_tblIPLoadError_intStageLoadId] FOREIGN KEY ([intStageLoadId]) REFERENCES [tblIPLoadError]([intStageLoadId]) ON DELETE CASCADE
+)
