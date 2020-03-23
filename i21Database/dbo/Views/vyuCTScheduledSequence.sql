019a7ef5623004503137187714a56fe054c38fb7
jayson.ramos <jayson.ramos@irely.com>
2020-03-23 16:34:32 +0800

    CT-4379

--- i21Database/dbo/Views/vyuCTScheduledSequence.sql
+++ i21Database/dbo/Views/vyuCTScheduledSequence.sql
@@ -44,6 +44,7 @@ AS
 		left join Invoice on Invoice.intContractDetailId = LD.intPContractDetailId and Invoice.intLoadId = LO.intLoadId
 		WHERE   NOT (LO.intPurchaseSale	    =   1 AND LO.intShipmentStatus    IN(4,11,7))
 		AND	LD.intPContractDetailId IS NOT NULL
+		AND isnull(LO.ysnCancelled,convert(bit,0)) = convert(bit,0)
 
 		UNION ALL
 
@@ -53,6 +54,7 @@ AS
 		left join Invoice on Invoice.intContractDetailId = LD.intSContractDetailId and Invoice.intLoadId = LO.intLoadId
 		WHERE   NOT (LO.intPurchaseSale	    =   2 AND LO.intShipmentStatus    IN(6,11,7))
 		AND	LD.intSContractDetailId IS NOT NULL
+		AND isnull(LO.ysnCancelled,convert(bit,0)) = convert(bit,0)
 
 		UNION ALL
 
