66b405ba4fc367a7f22a1743511c9366a437fe39
Premkumar S <prem.selladurai@irely.com>
2020-03-25 22:03:28 +0530

    IN-456

--- i21Database/dbo/Tables/tblIPCompanyPreference.sql
+++ i21Database/dbo/Tables/tblIPCompanyPreference.sql
@@ -4,5 +4,6 @@
 	[strCommonDataFolderPath] NVARCHAR(250) COLLATE Latin1_General_CI_AS NULL,
 	strCustomerCode nvarchar(50),
 	ysnReplicationEnabled BIT CONSTRAINT [DF_ttblIPCompanyPreference_ysnReplicationEnabled] DEFAULT 1, 
-    CONSTRAINT [PK_tblIPCompanyPreference_intCompanyPreferenceId] PRIMARY KEY ([intCompanyPreferenceId]) 
+	intThirdPartyContractWaitingPeriod int,
+	CONSTRAINT [PK_tblIPCompanyPreference_intCompanyPreferenceId] PRIMARY KEY ([intCompanyPreferenceId]) 
 )
