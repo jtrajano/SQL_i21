e9cb68d934e095c06f442476ddba8a7ddf604928
Premkumar S <prem.selladurai@irely.com>
2020-03-26 23:22:32 +0530

    IN-505

--- i21Database/dbo/Views/vyuIPGetInvoiceDetail.sql
+++ i21Database/dbo/Views/vyuIPGetInvoiceDetail.sql
@@ -4,6 +4,7 @@ SELECT I.strItemNo
 	,IVD.intContractHeaderId
 	,IVD.intContractDetailId
 	,IVD.dblQtyOrdered
+	,OUM.strUnitMeasure As strOrderUnitMeasure
 	,IVD.dblQtyShipped
 	,UM.strUnitMeasure
 	,IVD.dblUnitPrice AS dblPrice
@@ -16,6 +17,8 @@ SELECT I.strItemNo
 	,LD.intLoadDetailId
 FROM dbo.tblARInvoice IV
 JOIN dbo.tblARInvoiceDetail IVD ON IV.intInvoiceId = IVD.intInvoiceId
+JOIN dbo.tblICItemUOM OIU ON OIU.intItemUOMId = IVD.intOrderUOMId
+JOIN dbo.tblICUnitMeasure OUM ON OUM.intUnitMeasureId = OIU.intUnitMeasureId
 JOIN dbo.tblCTBook B ON B.intBookId = IV.intBookId
 LEFT JOIN tblLGLoadDetail LD on LD.intLoadDetailId =IVD.intLoadDetailId
 LEFT JOIN dbo.tblCTSubBook SB ON SB.intSubBookId = IV.intSubBookId
