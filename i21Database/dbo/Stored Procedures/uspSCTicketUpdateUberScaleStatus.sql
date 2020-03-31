a604c0f54138039d1321ef44e5470dd0f4a378e3
Von Carlo Pasion <von.pasion@irely.com>
2020-03-31 19:41:41 +0800

    FRM-7270 - Put some logic before setting status (Uber Scale)

--- i21Database/dbo/Stored Procedures/uspSCTicketUpdateUberScaleStatus.sql
+++ i21Database/dbo/Stored Procedures/uspSCTicketUpdateUberScaleStatus.sql
@@ -3,12 +3,18 @@
 	@strUberStatusCode nvarchar(3)
 AS
 	
-	if exists(select top 1 1 from tblSCTicket where intTicketId = @intTicketId and isnull(strTicketStatus, '') <> @strUberStatusCode)
-	begin
-		update tblSCTicket set strTicketStatus = @strUberStatusCode where intTicketId = @intTicketId
-		insert into tblSCTicketUberScaleStatusUpdate(intTicketId, dtmTransactionDate, strUberStatusCode)
-		select @intTicketId, getdate(), @strUberStatusCode
-	end
+	IF EXISTS(SELECT TOP 1 1 FROM tblSCTicket WHERE intTicketId = @intTicketId and ISNULL(strTicketStatus, '') <> @strUberStatusCode)
+	BEGIN
+
+		DECLARE @ticketStatus AS NVARCHAR(3) = (SELECT TOP 1 strTicketStatus FROM tblSCTicket WHERE intTicketId = @intTicketId)
+
+		IF @ticketStatus <> '' AND (@ticketStatus = 'S' OR @ticketStatus = 'I')
+		BEGIN
+			UPDATE tblSCTicket set strTicketStatus = @strUberStatusCode where intTicketId = @intTicketId
+			INSERT INTO tblSCTicketUberScaleStatusUpdate(intTicketId, dtmTransactionDate, strUberStatusCode)
+			SELECT @intTicketId, getdate(), @strUberStatusCode
+		END
+	END
 
 
 RETURN 0
