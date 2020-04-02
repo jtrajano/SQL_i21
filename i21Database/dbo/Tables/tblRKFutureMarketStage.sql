ee83e55ef0f256c17af85756e10e436b2cd49d70
Vinoth Kumar T <vinoth.t@irely.com>
2020-04-02 21:23:04 +0530

    IN-500

---
+++ i21Database/dbo/Tables/tblRKFutureMarketStage.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE tblRKFutureMarketStage
+(
+	intFutureMarketStageId		INT IDENTITY(1,1) PRIMARY KEY, 
+	intFutureMarketId			INT,
+	strFutMarketName			NVARCHAR(30) COLLATE Latin1_General_CI_AS,
+	dblForecastPrice			NUMERIC(18, 6) CONSTRAINT DF_tblRKFutureMarketStage_dblForecastPrice DEFAULT 0,
+	strHeaderXML				NVARCHAR(MAX) COLLATE Latin1_General_CI_AS,
+	strRowState					NVARCHAR(100) COLLATE Latin1_General_CI_AS,
+	strUserName					NVARCHAR(100) COLLATE Latin1_General_CI_AS,
+	strFeedStatus				NVARCHAR(MAX) COLLATE Latin1_General_CI_AS,
+	dtmFeedDate					DATETIME CONSTRAINT DF_tblRKFutureMarketStage_dtmFeedDate DEFAULT GETDATE(),
+	strMessage					NVARCHAR(MAX) COLLATE Latin1_General_CI_AS,
+	intMultiCompanyId			INT,
+	intEntityId					INT,
+	intCompanyLocationId		INT,
+	strTransactionType			NVARCHAR(100) COLLATE Latin1_General_CI_AS,
+	intToBookId					INT,
+	ysnMailSent					BIT CONSTRAINT DF_tblRKFutureMarketStage_ysnMailSent DEFAULT 0
+)
