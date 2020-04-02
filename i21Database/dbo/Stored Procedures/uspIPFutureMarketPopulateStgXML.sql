403498a5568e53f2f53515b756b458c28ede91d0
Vinoth Kumar T <vinoth.t@irely.com>
2020-04-02 21:23:04 +0530

    IN-500

---
+++ i21Database/dbo/Stored Procedures/uspIPFutureMarketPopulateStgXML.sql
@@ -0,0 +1,105 @@
+﻿CREATE PROCEDURE uspIPFutureMarketPopulateStgXML @intFutureMarketId INT
+	,@intToEntityId INT
+	,@intCompanyLocationId INT
+	,@strToTransactionType NVARCHAR(100)
+	,@intToCompanyId INT
+	,@strRowState NVARCHAR(100)
+	,@ysnReplication BIT = 1
+	,@intToBookId INT = NULL
+	,@intUserId INT = NULL
+AS
+BEGIN TRY
+	SET NOCOUNT ON
+
+	DECLARE @ErrMsg NVARCHAR(MAX)
+		,@strHeaderXML NVARCHAR(MAX)
+		,@strHeaderCondition NVARCHAR(MAX)
+		,@strObjectName NVARCHAR(50)
+		,@strLastModifiedUser NVARCHAR(100)
+		,@strFutMarketName NVARCHAR(30)
+		,@dblForecastPrice NUMERIC(18, 6)
+
+	SET @strHeaderXML = NULL
+	SET @strHeaderCondition = NULL
+	SET @strLastModifiedUser = NULL
+	SET @strFutMarketName = NULL
+	SET @dblForecastPrice = NULL
+
+	SELECT @strFutMarketName = strFutMarketName
+		,@dblForecastPrice = dblForecastPrice
+	FROM tblRKFutureMarket WITH (NOLOCK)
+	WHERE intFutureMarketId = @intFutureMarketId
+
+	-------------------------Header-----------------------------------------------------------
+	SELECT @strHeaderCondition = 'intFutureMarketId = ' + LTRIM(@intFutureMarketId)
+
+	SELECT @strObjectName = 'vyuIPGetFutureMarket'
+
+	EXEC [dbo].[uspCTGetTableDataInXML] @strObjectName
+		,@strHeaderCondition
+		,@strHeaderXML OUTPUT
+		,NULL
+		,NULL
+
+	SELECT @strLastModifiedUser = t.strName
+	FROM tblEMEntity t
+	JOIN tblEMEntityType ET ON ET.intEntityId = t.intEntityId
+	WHERE ET.strType = 'User'
+		AND t.intEntityId = @intUserId
+		AND t.strEntityNo <> ''
+
+	IF @strLastModifiedUser IS NULL
+	BEGIN
+		IF EXISTS (
+				SELECT 1
+				FROM tblSMUserSecurity
+				WHERE strUserName = 'irelyadmin'
+				)
+			SELECT TOP 1 @intUserId = intEntityId
+				,@strLastModifiedUser = strUserName
+			FROM tblSMUserSecurity
+			WHERE strUserName = 'irelyadmin'
+		ELSE
+			SELECT TOP 1 @intUserId = intEntityId
+				,@strLastModifiedUser = strUserName
+			FROM tblSMUserSecurity
+	END
+
+	INSERT INTO tblRKFutureMarketStage (
+		intFutureMarketId
+		,strFutMarketName
+		,dblForecastPrice
+		,strHeaderXML
+		,strRowState
+		,strUserName
+		,intMultiCompanyId
+		,intEntityId
+		,intCompanyLocationId
+		,strTransactionType
+		,intToBookId
+		)
+	SELECT intFutureMarketId = @intFutureMarketId
+		,strFutureMarketName = @strFutMarketName
+		,dblForecastPrice = @dblForecastPrice
+		,strHeaderXML = @strHeaderXML
+		,strRowState = @strRowState
+		,strUserName = @strLastModifiedUser
+		,intMultiCompanyId = intCompanyId
+		,intEntityId = @intToEntityId
+		,intCompanyLocationId = @intCompanyLocationId
+		,strTransactionType = @strToTransactionType
+		,intToBookId = @intToBookId
+	FROM tblIPMultiCompany
+	WHERE ysnParent = 0
+END TRY
+
+BEGIN CATCH
+	SET @ErrMsg = ERROR_MESSAGE()
+
+	RAISERROR (
+			@ErrMsg
+			,16
+			,1
+			,'WITH NOWAIT'
+			)
+END CATCH
---
+++ i21Database/dbo/Stored Procedures/uspIPFutureMarketProcessStgXML.sql
@@ -0,0 +1,171 @@
+﻿CREATE PROCEDURE uspIPFutureMarketProcessStgXML
+AS
+BEGIN TRY
+	SET NOCOUNT ON
+
+	DECLARE @idoc INT
+		,@intTransactionCount INT
+		,@ErrMsg NVARCHAR(MAX)
+		,@strErrorMessage NVARCHAR(MAX)
+	DECLARE @intFutureMarketStageId INT
+		,@intFutureMarketId INT
+		,@strHeaderXML NVARCHAR(MAX)
+		,@strRowState NVARCHAR(MAX)
+		,@intMultiCompanyId INT
+		,@strUserName NVARCHAR(100)
+	DECLARE @strFutMarketName NVARCHAR(30)
+	DECLARE @intLastModifiedUserId INT
+		,@intNewFutureMarketId INT
+		,@dblOldForecastPrice NUMERIC(18, 6)
+		,@dblNewForecastPrice NUMERIC(18, 6)
+
+	SELECT @intFutureMarketStageId = MIN(intFutureMarketStageId)
+	FROM tblRKFutureMarketStage
+	WHERE ISNULL(strFeedStatus, '') = ''
+
+	WHILE @intFutureMarketStageId > 0
+	BEGIN
+		SELECT @intFutureMarketId = NULL
+			,@strHeaderXML = NULL
+			,@strRowState = NULL
+			,@intMultiCompanyId = NULL
+			,@strUserName = NULL
+			,@dblOldForecastPrice = NULL
+			,@dblNewForecastPrice = NULL
+
+		SELECT @intFutureMarketId = intFutureMarketId
+			,@strHeaderXML = strHeaderXML
+			,@strRowState = strRowState
+			,@intMultiCompanyId = intMultiCompanyId
+			,@strUserName = strUserName
+			,@dblNewForecastPrice = dblForecastPrice
+		FROM tblRKFutureMarketStage
+		WHERE intFutureMarketStageId = @intFutureMarketStageId
+
+		BEGIN TRY
+			SELECT @intTransactionCount = @@TRANCOUNT
+
+			IF @intTransactionCount = 0
+				BEGIN TRANSACTION
+
+			------------------Header------------------------------------------------------
+			EXEC sp_xml_preparedocument @idoc OUTPUT
+				,@strHeaderXML
+
+			SELECT @strFutMarketName = NULL
+
+			SELECT @strFutMarketName = strFutMarketName
+			FROM OPENXML(@idoc, 'vyuIPGetFutureMarkets/vyuIPGetFutureMarket', 2) WITH (
+					strFutMarketName NVARCHAR(50) Collate Latin1_General_CI_AS
+					) x
+
+			SELECT @intLastModifiedUserId = NULL
+
+			SELECT @intLastModifiedUserId = t.intEntityId
+			FROM tblEMEntity t
+			JOIN tblEMEntityType ET ON ET.intEntityId = t.intEntityId
+			WHERE ET.strType = 'User'
+				AND t.strName = @strUserName
+				AND t.strEntityNo <> ''
+
+			IF @intLastModifiedUserId IS NULL
+			BEGIN
+				IF EXISTS (
+						SELECT 1
+						FROM tblSMUserSecurity
+						WHERE strUserName = 'irelyadmin'
+						)
+					SELECT TOP 1 @intLastModifiedUserId = intEntityId
+					FROM tblSMUserSecurity
+					WHERE strUserName = 'irelyadmin'
+				ELSE
+					SELECT TOP 1 @intLastModifiedUserId = intEntityId
+					FROM tblSMUserSecurity
+			END
+
+			IF @strRowState = 'Modified'
+			BEGIN
+				SELECT @dblOldForecastPrice = dblForecastPrice
+				FROM tblRKFutureMarket
+				WHERE strFutMarketName = @strFutMarketName
+
+				UPDATE tblRKFutureMarket
+				SET intConcurrencyId = intConcurrencyId + 1
+					,dblForecastPrice = @dblNewForecastPrice
+				WHERE tblRKFutureMarket.strFutMarketName = @strFutMarketName
+
+				SELECT @intNewFutureMarketId = intFutureMarketId
+				FROM tblRKFutureMarket
+				WHERE strFutMarketName = @strFutMarketName
+			END
+
+			EXEC sp_xml_removedocument @idoc
+
+			UPDATE tblRKFutureMarketStage
+			SET strFeedStatus = 'Processed'
+				,strMessage = 'Success'
+			WHERE intFutureMarketStageId = @intFutureMarketStageId
+
+			-- Audit Log
+			IF (@intNewFutureMarketId > 0)
+			BEGIN
+				DECLARE @strDetails AS NVARCHAR(MAX)
+
+				IF @strRowState = 'Modified'
+				BEGIN
+					SELECT @strDetails = ''
+
+					IF (@dblOldForecastPrice <> @dblNewForecastPrice)
+						SET @strDetails += '{"change":"dblForecastPrice","iconCls":"small-gear","from":"' + LTRIM(ISNULL(@dblOldForecastPrice, 0)) + '","to":"' + LTRIM(ISNULL(@dblNewForecastPrice, 0)) + '","leaf":true,"changeDescription":"Forecast Price from Inter Company"},'
+
+					IF (LEN(@strDetails) > 1)
+					BEGIN
+						SET @strDetails = SUBSTRING(@strDetails, 0, LEN(@strDetails))
+
+						EXEC uspSMAuditLog @keyValue = @intNewFutureMarketId
+							,@screenName = 'RiskManagement.view.FuturesMarket'
+							,@entityId = @intLastModifiedUserId
+							,@actionType = 'Updated'
+							,@actionIcon = 'small-tree-modified'
+							,@details = @strDetails
+					END
+				END
+			END
+
+			IF @intTransactionCount = 0
+				COMMIT TRANSACTION
+		END TRY
+
+		BEGIN CATCH
+			SET @ErrMsg = ERROR_MESSAGE()
+
+			IF @idoc <> 0
+				EXEC sp_xml_removedocument @idoc
+
+			IF XACT_STATE() != 0
+				AND @intTransactionCount = 0
+				ROLLBACK TRANSACTION
+
+			UPDATE tblRKFutureMarketStage
+			SET strFeedStatus = 'Failed'
+				,strMessage = @ErrMsg
+			WHERE intFutureMarketStageId = @intFutureMarketStageId
+		END CATCH
+
+		SELECT @intFutureMarketStageId = MIN(intFutureMarketStageId)
+		FROM tblRKFutureMarketStage
+		WHERE intFutureMarketStageId > @intFutureMarketStageId
+			AND ISNULL(strFeedStatus, '') = ''
+	END
+END TRY
+
+BEGIN CATCH
+	SET @ErrMsg = ERROR_MESSAGE()
+
+	RAISERROR (
+			@ErrMsg
+			,16
+			,1
+			,'WITH NOWAIT'
+			)
+END CATCH
