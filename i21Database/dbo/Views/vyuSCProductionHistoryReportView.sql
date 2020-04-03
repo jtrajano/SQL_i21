d58079d521e6ea6b1abbe8ba7d9c3e8a2f5b8cf1
Christian Joel Martinez <Christian.Martinez@iRely.com>
2020-04-03 15:50:23 +0800

    GRN-2041, GRN-2065

--- i21Database/dbo/Views/vyuSCProductionHistoryReportView.sql
+++ i21Database/dbo/Views/vyuSCProductionHistoryReportView.sql
@@ -1,6 +1,6 @@
 ﻿CREATE VIEW [dbo].[vyuSCProductionHistoryReportView]
 	AS 	
-	SELECT DISTINCT SC.intTicketId, (CASE WHEN
+		SELECT SC.intTicketId, (CASE WHEN
     SC.strTicketStatus = 'O' THEN 'OPEN' WHEN
     SC.strTicketStatus = 'A' THEN 'PRINTED' WHEN
     SC.strTicketStatus = 'C' THEN 'COMPLETED' WHEN
@@ -61,7 +61,7 @@
 	SC.intContractId,
 	SC.intDiscountLocationId,
 	SC.intItemId,
-	SC.intEntityId,
+	IR.intEntityVendorId AS intEntityId,
 	SC.intLoadId,
 	SC.intMatchTicketId,
 	SC.intSubLocationId,
@@ -94,7 +94,7 @@
 	tblSMCompanyLocation.strLocationNumber,
 	tblSMCompanyLocation.strLocationName,
 	tblSMCompanyLocationSubLocation.strSubLocationName, 
-	tblEMEntitySplit.strSplitNumber,
+	strSplitNumber = ISNULL(DS.strSplitNumber, tblEMEntitySplit.strSplitNumber),
 	tblGRStorageScheduleRule.strScheduleId,
 	ICCommodity.strCommodityCode,
 	tblICStorageLocation.strDescription,
@@ -109,17 +109,66 @@
 	--ReceiptItem.intInventoryReceiptId,
 	----ReceiptItem.intInventoryReceiptItemId,
 	--ReceiptItem.strReceiptNumber,
-	SC.dblGrossUnits dblGross,
-	SC.dblNetUnits dblNet,
-	(SC.dblGrossWeight + SC.dblGrossWeight1 + SC.dblGrossWeight2) AS dblLineGrossWeight,
-	(SC.dblTareWeight + SC.dblTareWeight1 + SC.dblTareWeight2) AS dblLineNetWeight,
+    IRD.dblGross,
+    dblNet =  CASE WHEN DS.intDeliverySheetId IS NULL
+			THEN IRD.dblNet
+			ELSE IRD.dblGross - (CASE WHEN ISNULL(DS.dblGross,0) = 0 THEN 0 ELSE (DS.dblShrink / DS.dblGross) * IRD.dblGross END)
+			END,
+	(SC.dblGrossWeight + SC.dblGrossWeight1 + SC.dblGrossWeight2) * (CASE WHEN TS.intTicketSplitId IS NULL THEN 1 ELSE TS.dblSplitPercent / 100 END) AS dblLineGrossWeight,
+	(SC.dblTareWeight + SC.dblTareWeight1 + SC.dblTareWeight2) * (CASE WHEN TS.intTicketSplitId IS NULL THEN 1 ELSE TS.dblSplitPercent / 100 END) AS dblLineNetWeight,
 	tblGRDiscountId.strDiscountId,
-	ISNULL(Voucher.dtmDate, ReceiptItem.dtmReceiptDate) AS dtmReceiptDate,
-	(SELECT intCurrencyDecimal FROM tblSMCompanyPreference) AS intDecimalPrecision
+	ISNULL(Voucher.dtmDate, IR.dtmReceiptDate) AS dtmReceiptDate,
+	(SELECT intCurrencyDecimal FROM tblSMCompanyPreference) AS intDecimalPrecision,
+    dblShrinkage = CASE WHEN DS.intDeliverySheetId IS NULL
+			THEN 
+				CASE WHEN ISNULL(SC.dblShrink,0) = 0 THEN 0
+				ELSE 
+					SC.dblShrink * (CASE WHEN TS.intTicketSplitId IS NULL THEN 1 ELSE TS.dblSplitPercent / 100 END)
+				END
+			ELSE CASE WHEN ISNULL(DS.dblGross,0) = 0 THEN 0 ELSE (DS.dblShrink / DS.dblGross) * IRD.dblGross END
+			END,
+	CASE WHEN SC.intDeliverySheetId IS NULL 
+		THEN SUBSTRING((
+				SELECT ','+ ICI2.strItemNo + '=' + LTRIM(STR(QMD2.dblGradeReading, 10, 2))
+				FROM tblSCTicket SC2
+				INNER JOIN tblQMTicketDiscount QMD2
+					ON QMD2.intTicketId = SC2.intTicketId and QMD2.strSourceType = 'Scale'
+				INNER JOIN tblGRDiscountScheduleCode DSC2
+					ON QMD2.intDiscountScheduleCodeId = DSC2.intDiscountScheduleCodeId
+				INNER JOIN tblICItem ICI2
+					ON ICI2.intItemId = DSC2.intItemId
+				WHERE QMD2.dblGradeReading > 0 and SC2.intTicketId = SC.intTicketId
+				FOR XML PATH('')
+			),2,1000) 
+		ELSE SUBSTRING((
+				SELECT ','+ ICI2.strItemNo + '=' + LTRIM(STR(QMD2.dblGradeReading, 10, 2)) FROM tblSCDeliverySheet DSS2
+				INNER JOIN tblSCTicket SC2
+					ON DSS2.intDeliverySheetId = SC2.intDeliverySheetId
+				INNER JOIN tblQMTicketDiscount QMD2
+					ON QMD2.intTicketFileId = DSS2.intDeliverySheetId and QMD2.strSourceType = 'Delivery Sheet'
+				INNER JOIN tblGRDiscountScheduleCode DSC2
+					ON QMD2.intDiscountScheduleCodeId = DSC2.intDiscountScheduleCodeId
+				INNER JOIN tblICItem ICI2
+					ON ICI2.intItemId = DSC2.intItemId
+				WHERE QMD2.dblGradeReading > 0 and SC2.intTicketId = SC.intTicketId
+				FOR XML PATH('')
+			),2,1000)
+	END strGradeReading
+	,IR.intEntityVendorId
+	,DS.intDeliverySheetId
+	,DS.strDeliverySheetNumber
+	,DS.strSplitDescription
   FROM tblSCTicket SC
-  LEFT JOIN tblICCommodity ICCommodity ON ICCommodity.intCommodityId = SC.intCommodityId
-  LEFT JOIN tblEMEntity tblEMEntity on tblEMEntity.intEntityId = SC.intEntityId
-  LEFT JOIN tblEMEntityLocation EMLocation ON EMLocation.intEntityId = SC.intEntityId AND EMLocation.ysnDefaultLocation = 1
+  INNER JOIN tblICCommodity ICCommodity ON ICCommodity.intCommodityId = SC.intCommodityId
+  LEFT JOIN tblICInventoryReceiptItem IRD
+		ON SC.intTicketId = IRD.intSourceId	
+        AND SC.intItemId = IRD.intItemId
+  LEFT JOIN tblICInventoryReceipt IR
+		ON IRD.intInventoryReceiptId = IR.intInventoryReceiptId
+        AND IR.intSourceType = 1
+  LEFT JOIN tblEMEntity tblEMEntity ON tblEMEntity.intEntityId = IR.intEntityVendorId
+  LEFT JOIN tblSCTicketSplit TS ON TS.intTicketId = SC.intTicketId AND TS.intCustomerId = IR.intEntityVendorId
+  LEFT JOIN tblEMEntityLocation EMLocation ON EMLocation.intEntityId = tblEMEntity.intEntityId AND EMLocation.ysnDefaultLocation = 1
   LEFT JOIN vyuEMSearchShipVia vyuEMSearchShipVia on vyuEMSearchShipVia.intEntityId = SC.intHaulerId
   LEFT JOIN tblEMEntitySplit tblEMEntitySplit on tblEMEntitySplit.intSplitId = SC.intSplitId
   LEFT JOIN tblSMCompanyLocation tblSMCompanyLocation on tblSMCompanyLocation.intCompanyLocationId = SC.intProcessingLocationId
@@ -139,27 +188,28 @@
 		,strCountry AS strCompanyCountry
 	 FROM tblSMCompanySetup
   )AS tblSMCompanySetup
- OUTER APPLY(
-	SELECT IC.intInventoryReceiptId
-		,IC.strReceiptNumber
-		,IC.dtmReceiptDate 
-		,ICI.intInventoryReceiptItemId
-		,ICI.dblGross
-		,ICI.dblNet
-		,(ICI.dblGross - ICI.dblNet) [dblShrinkage]
-		,LineCtr.totalTicket
-	FROM tblICInventoryReceipt IC 
-	INNER JOIN tblICInventoryReceiptItem ICI ON IC.intInventoryReceiptId = ICI.intInventoryReceiptId
-	OUTER APPLY(
-		SELECT COUNT(*) as totalTicket FROM tblICInventoryReceipt IR
-		INNER JOIN tblICInventoryReceiptItem IRI ON IR.intInventoryReceiptId = IRI.intInventoryReceiptId
-		WHERE intSourceId = SC.intTicketId AND IR.intSourceType = 1
-	)LineCtr
-	WHERE ICI.intSourceId = SC.intTicketId AND IC.intSourceType = 1
-  )AS ReceiptItem
+  OUTER APPLY
+	(	
+		SELECT
+			DS.strSplitDescription
+			,DS.strDeliverySheetNumber
+			,DS.intDeliverySheetId
+			,DS.dblShrink
+			,DS.dblGross
+			,DS.ysnPost
+			,tblEMEntitySplit.strSplitNumber
+			,DSS.intEntityId
+		FROM tblSCDeliverySheet DS
+		INNER JOIN tblSCDeliverySheetSplit DSS
+			ON DS.intDeliverySheetId = DSS.intDeliverySheetId
+		LEFT JOIN tblEMEntitySplit tblEMEntitySplit
+			ON tblEMEntitySplit.intSplitId = DS.intSplitId
+		WHERE DS.intDeliverySheetId = SC.intDeliverySheetId
+			AND DSS.intEntityId = IR.intEntityVendorId
+	) DS
   OUTER APPLY(
 	SELECT TOP 1 AP.dtmDate from tblAPBillDetail APD 
 	INNER JOIN tblAPBill AP ON AP.intBillId = APD.intBillId
-	WHERE APD.intInventoryReceiptItemId = ReceiptItem.intInventoryReceiptItemId
+	WHERE APD.intInventoryReceiptItemId = IRD.intInventoryReceiptItemId
   )AS Voucher
   WHERE SC.strTicketStatus = 'C' AND SC.intEntityId > 0
