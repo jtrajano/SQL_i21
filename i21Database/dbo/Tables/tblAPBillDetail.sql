5dd3dcdc76dea6a361486579fd4661c9d68c45d0
Jopet Lambinicio <jopet.lambinicio@irely.com>
2020-04-06 14:45:53 +0800

    AP-8505

--- i21Database/dbo/Tables/tblAPBillDetail.sql
+++ i21Database/dbo/Tables/tblAPBillDetail.sql
@@ -19,6 +19,7 @@
 	[intContractHeaderId]    INT             NULL,
 	[intContractDetailId]    INT             NULL,
 	[intCustomerStorageId]    INT             NULL,
+	[intSettleStorageId] INT NULL,
 	[intStorageLocationId] INT             NULL,
 	[intSubLocationId] INT             NULL,
 	[intLocationId] INT             NULL,
