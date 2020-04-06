03aa1566cd213b05cebbc66a176b550a0078246a
Feb Montefrio <feb.montefrio@irely.com>
2020-04-06 20:40:54 +0800

    [B] IC-8376

--- i21Database/dbo/Functions/fnICGetErrorMessage.sql
+++ i21Database/dbo/Functions/fnICGetErrorMessage.sql
@@ -256,6 +256,15 @@ BEGIN
 		WHEN @msgId = 80240 THEN 'Inventory Count is ongoing and is locked for item %s in storage unit %s.'
 		WHEN @msgId = 80241 THEN 'Inventory Count is ongoing and is locked for item %s in lot number %s.'
 		WHEN @msgId = 80242 THEN 'Unable to update the Other Charge. The Inventory Receipt total is going to be negative.'
+		WHEN @msgId = 80243 THEN 'Cannot process %s because it is already included in the Add Payables by %s.'
+		WHEN @msgId = 80244 THEN 'Reverse can only be used on posted transaction.'
+		WHEN @msgId = 80245 THEN '%s failed to create its transaction id.'
+		WHEN @msgId = 80246 THEN 'Entity user id is invalid.'
+		WHEN @msgId = 80247 THEN '%s already has an existing reversal.'
+		WHEN @msgId = 80248 THEN 'Back-dated reversal is not allowed.'
+		WHEN @msgId = 80249 THEN 'Shipment reversal is not allowed. It already has a credit memo. See %s.'
+		WHEN @msgId = 80250 THEN 'Inventory Account is set to %s for item %s.'
+		WHEN @msgId = 80251 THEN 'Work In Progress Account is set to %s for item %s.'
 	END 
 
 	RETURN @msg
