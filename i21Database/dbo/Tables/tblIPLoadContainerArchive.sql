72b3ac583715947db2858512961c749fac3c5c09
Vinoth Kumar T <vinoth.t@irely.com>
2020-04-08 21:26:02 +0530

    IN-460

---
+++ i21Database/dbo/Tables/tblIPLoadContainerArchive.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE tblIPLoadContainerArchive
+(
+	intStageLoadContainerId		INT IDENTITY(1,1),
+	intStageLoadId				INT NOT NULL,
+	strCustomerReference		NVARCHAR(100) COLLATE Latin1_General_CI_AS,
+	strContainerNumber			NVARCHAR(100) COLLATE Latin1_General_CI_AS,
+	strContainerType			NVARCHAR(50) COLLATE Latin1_General_CI_AS,
+	dblGrossWt					NUMERIC(18, 6),
+	dblTareWt					NUMERIC(18, 6),
+	dblQuantity					NUMERIC(18, 6),
+
+	CONSTRAINT [PK_tblIPLoadContainerArchive_intStageLoadContainerId] PRIMARY KEY (intStageLoadContainerId),
+	CONSTRAINT [FK_tblIPLoadContainerArchive_tblIPLoadArchive_intStageLoadId] FOREIGN KEY ([intStageLoadId]) REFERENCES [tblIPLoadArchive]([intStageLoadId]) ON DELETE CASCADE
+)
