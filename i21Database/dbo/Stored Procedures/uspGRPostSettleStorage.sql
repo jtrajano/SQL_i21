33405743a53db1f57d6084ab60743b895a0cb5ea
Leslie Guevarra <leslie.arat@irely.com>
2020-04-08 13:48:23 +0800

    GRN-2072 (CLONE - CLONE - HDTN-243739 - Fix STR number in voucher)

--- i21Database/dbo/Stored Procedures/uspGRPostSettleStorage.sql
+++ i21Database/dbo/Stored Procedures/uspGRPostSettleStorage.sql
@@ -2363,6 +2363,7 @@ BEGIN TRY
 					,[intContractDetailId]
 					,[intInventoryReceiptItemId]
 					,[intCustomerStorageId]
+					,[intSettleStorageId]
 					,[dblOrderQty]
 					,[dblOrderUnitQty]
 					,[intOrderUOMId]	
@@ -2424,6 +2425,7 @@ BEGIN TRY
 																				END
 																END
 					,[intCustomerStorageId]			= a.[intCustomerStorageId]
+					,[intSettleStorageId]			= @intSettleStorageId
 					,[dblOrderQty]					= CASE	
 														WHEN CD.intContractDetailId is not null and intItemType = 1 then ROUND(dbo.fnCalculateQtyBetweenUOM(b.intItemUOMId,@intUnitMeasureId, CD.dblQuantity),6) 
 														WHEN ISNULL(availableQtyForVoucher.dblContractUnits,0) > 0 THEN availableQtyForVoucher.dblContractUnits
@@ -2991,6 +2993,7 @@ BEGIN TRY
 					,[intContractDetailId]
 					,[intInventoryReceiptChargeId]
 					,[intCustomerStorageId]
+					,[intSettleStorageId]
 					,[dblOrderQty]
 					,[dblOrderUnitQty]
 					,[intOrderUOMId]
@@ -3020,6 +3023,7 @@ BEGIN TRY
 					,[intContractDetailId] 			= NULL
 					,[intInventoryReceiptChargeId]	= CASE WHEN @ysnDPOwnedType = 0 THEN NULL ELSE ReceiptCharge.intInventoryReceiptChargeId END
 					,[intCustomerStorageId] 		= SST.intCustomerStorageId
+					,[intSettleStorageId]			= @intSettleStorageId
 					,[dblOrderQty]					= CASE WHEN ISNULL(Item.strCostMethod,'') = 'Gross Unit' THEN (SC.dblGrossUnits/SC.dblNetUnits) * SST.dblUnits ELSE SST.dblUnits END
 					,[dblOrderUnitQty]				= 1		
 					,[intOrderUOMId] 				= CASE 
@@ -3106,6 +3110,7 @@ BEGIN TRY
 					,[intContractHeaderId]
 					,[intContractDetailId]
 					,[intCustomerStorageId]
+					,[intSettleStorageId]
 					,[dblOrderQty]
 					,[dblOrderUnitQty]
 					,[intOrderUOMId]
@@ -3132,6 +3137,7 @@ BEGIN TRY
 					,[intContractHeaderId]	= CD.[intContractHeaderId]
 					,[intContractDetailId]  = CD.[intContractDetailId]
 					,[intCustomerStorageId]	= SV.[intCustomerStorageId]
+					,[intSettleStorageId]	= @intSettleStorageId
 					,[dblOrderQty]			= CASE 
 												WHEN CC.intItemUOMId IS NOT NULL THEN  dbo.fnCTConvertQuantityToTargetItemUOM(CC.intItemId,UOM.intUnitMeasureId,@intUnitMeasureId,SV.dblUnits)
 												ELSE SV.dblUnits 
