c7cfdf5dd312680b585feb2e3242996179626df3
jayson.ramos <jayson.ramos@irely.com>
2020-04-09 20:15:19 +0800

    CT-4615
    (cherry picked from commit 747b323499c0bb0d0039b105ed2f74bc7c7099fb)

--- i21Database/dbo/Stored Procedures/uspCTPriceFixationDetailDelete.sql
+++ i21Database/dbo/Stored Procedures/uspCTPriceFixationDetailDelete.sql
@@ -168,7 +168,7 @@ BEGIN TRY
 
 	SELECT @List = NULL
 
-	SELECT  DA.intInvoiceId AS Id, DA.intInvoiceDetailId AS DetailId
+	SELECT  DA.intInvoiceId AS Id, isnull(FT.intDetailId,DA.intInvoiceDetailId) AS DetailId
 	INTO	#ItemInvoice
 	FROM	tblCTPriceFixationDetailAPAR	DA	LEFT
 	JOIN    vyuCTPriceFixationTicket        FT	ON  FT.intDetailId				=   DA.intInvoiceDetailId
