a644c50fa8ec57c64c941c51a9e3df305b1bcfa4
lawrence_zabala <lawrence.zabala@irely.com>
2020-04-13 11:36:24 +0800

    RM-3721

--- i21Database/dbo/Views/vyuRKGetCoverageEntryDetail.sql
+++ i21Database/dbo/Views/vyuRKGetCoverageEntryDetail.sql
@@ -27,8 +27,8 @@ SELECT Header.intCoverageEntryId
     , Detail.dblAveragePrice
     , Detail.dblOptionsCovered
     , Detail.dblFuturesM2M
-	, dblM2MPlus10 = Detail.dblFuturesM2M
-	, dblM2MMinus10 = Detail.dblFuturesM2M
+	, dblM2MPlus10 = ISNULL(Detail.dblM2MPlus10, 0.000000)
+	, dblM2MMinus10 = ISNULL(Detail.dblM2MMinus10, 0.000000)
     , Detail.intConcurrencyId
 FROM tblRKCoverageEntryDetail Detail
 LEFT JOIN vyuRKGetCoverageEntry Header ON Header.intCoverageEntryId = Detail.intCoverageEntryId
