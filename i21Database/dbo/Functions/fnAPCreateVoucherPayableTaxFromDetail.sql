49f95baa1b002f711c9f1b86ac02e7b77e4ab23e
Erick Dela Cruz <erick.delacruz@irely.com>
2020-04-15 17:37:17 +0800

    AP-8523

---
+++ i21Database/dbo/Functions/fnAPCreateVoucherPayableTaxFromDetail.sql
@@ -0,0 +1,28 @@
+﻿CREATE FUNCTION [dbo].[fnAPCreateVoucherPayableTaxFromDetail]
+(
+	@voucherDetailIds AS Id READONLY
+)
+RETURNS TABLE AS RETURN
+(
+	SELECT
+		D.[intBillDetailId]
+		,D.[intTaxGroupId]				
+		,D.[intTaxCodeId]				
+		,D.[intTaxClassId]				
+		,D.[strTaxableByOtherTaxes]	
+		,D.[strCalculationMethod]		
+		,D.[dblRate]					
+		,D.[intAccountId]				
+		,D.[dblTax]					
+		,D.[dblAdjustedTax]			
+		,D.[ysnTaxAdjusted]			
+		,D.[ysnSeparateOnBill]			
+		,D.[ysnCheckOffTax]			
+		,D.[ysnTaxExempt]              
+		,D.[ysnTaxOnly]	
+		,B.[ysnStage]			
+	FROM tblAPBill A
+	INNER JOIN tblAPBillDetail B ON A.intBillId = B.intBillId
+	INNER JOIN @voucherDetailIds C ON B.intBillDetailId = C.intId
+	INNER JOIN tblAPBillDetailTax D ON C.intId = D.intBillDetailId
+)
