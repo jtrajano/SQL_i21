e9713245e3f653037e36c32acb5ee332805182af
kelvin_quinto <kelvin.quinto@irely.com>
2020-04-18 13:24:07 +0800

    AR-11769

--- i21Database/dbo/Stored Procedures/uspARUpdateLineItemsLotDetail.sql
+++ i21Database/dbo/Stored Procedures/uspARUpdateLineItemsLotDetail.sql
@@ -10,8 +10,8 @@ BEGIN
 	SET ANSI_WARNINGS OFF
 					
 
-    INSERT INTO [dbo].[tblARInvoiceDetailLot]
-        ([intInvoiceDetailId]
+    INSERT INTO [dbo].[tblARInvoiceDetailLot] (
+         [intInvoiceDetailId]
         ,[intLotId]
         ,[dblQuantityShipped]
         ,[dblGrossWeight]
@@ -23,39 +23,37 @@ BEGIN
         ,[dtmDateModified]
         ,[intCreatedByUserId]
         ,[intModifiedByUserId]
-        ,[intConcurrencyId])
-	SELECT 
-         [intInvoiceDetailId]       = ARID.[intInvoiceDetailId]
-        ,[intLotId]                 = ISIL.[intLotId]
-        ,[dblQuantityShipped]       = ISIL.[dblQuantityShipped]
-        ,[dblGrossWeight]           = ISIL.[dblGrossWeight]
-        ,[dblTareWeight]            = ISIL.[dblTareWeight]
-        ,[dblWeightPerQty]          = ISIL.[dblWeightPerQty]
-        ,[strWarehouseCargoNumber]  = ISIL.[strWarehouseCargoNumber]
-        ,[intSort]                  = ISIL.[intSort]
-        ,[dtmDateCreated]           = ISIL.[dtmDateCreated]
-        ,[dtmDateModified]          = ISIL.[dtmDateModified]
-        ,[intCreatedByUserId]       = ISIL.[intCreatedByUserId]
-        ,[intModifiedByUserId]      = ISIL.[intModifiedByUserId]
-        ,[intConcurrencyId]         = ISIL.[intConcurrencyId]
-	FROM
-		tblARInvoiceDetail ARID
-	INNER JOIN
-		tblARInvoice ARI
-			ON ARID.[intInvoiceId] = ARI.[intInvoiceId]
-	INNER JOIN
-		tblICInventoryShipmentItem ISI
-			ON ARID.[intInventoryShipmentItemId] = ISI.[intInventoryShipmentItemId]
-	INNER JOIN
-		tblICInventoryShipmentItemLot ISIL
-			ON ISI.[intInventoryShipmentItemId] = ISIL.[intInventoryShipmentItemId]
-    INNER JOIN
-        @InvoiceIds II
-			ON ARI.[intInvoiceId] = II.[intHeaderId]
-	WHERE 
-		ARID.[intInvoiceDetailId] NOT IN (SELECT [intTransactionDetailId] FROM tblARTransactionDetail WHERE [intTransactionId] = ARI.[intInvoiceId] AND [intTransactionDetailId] = ARID.[intInvoiceDetailId])
-		AND ISNULL(ARID.[intInventoryShipmentItemId], 0) <> 0
-		AND ISNULL(ARID.[intLoadDetailId], 0) = 0		
+        ,[intConcurrencyId]
+    )
+	SELECT [intInvoiceDetailId]       = ARID.[intInvoiceDetailId]
+         , [intLotId]                 = ISIL.[intLotId]
+         , [dblQuantityShipped]       = dbo.fnCalculateQtyBetweenUOM(ARID.[intItemUOMId], LOT.[intItemUOMId], ARID.[dblQtyShipped])
+         , [dblGrossWeight]           = ISIL.[dblGrossWeight]
+         , [dblTareWeight]            = ISIL.[dblTareWeight]
+         , [dblWeightPerQty]          = ISIL.[dblWeightPerQty]
+         , [strWarehouseCargoNumber]  = ISIL.[strWarehouseCargoNumber]
+         , [intSort]                  = ISIL.[intSort]
+         , [dtmDateCreated]           = ISIL.[dtmDateCreated]
+         , [dtmDateModified]          = ISIL.[dtmDateModified]
+         , [intCreatedByUserId]       = ISIL.[intCreatedByUserId]
+         , [intModifiedByUserId]      = ISIL.[intModifiedByUserId]
+         , [intConcurrencyId]         = ISIL.[intConcurrencyId]	    
+    FROM tblARInvoiceDetail ARID
+    INNER JOIN tblARInvoice ARI ON ARID.[intInvoiceId] = ARI.[intInvoiceId]
+    INNER JOIN tblICInventoryShipmentItem ISI ON ARID.[intInventoryShipmentItemId] = ISI.[intInventoryShipmentItemId]
+    INNER JOIN tblICInventoryShipmentItemLot ISIL ON ISI.[intInventoryShipmentItemId] = ISIL.[intInventoryShipmentItemId]
+    INNER JOIN tblICInventoryLot LOT ON ISIL.[intLotId] = LOT.[intLotId] AND LOT.[intTransactionId] = ISI.[intInventoryShipmentId]
+                                                                         AND LOT.[intTransactionDetailId] = ISI.[intInventoryShipmentItemId]
+    INNER JOIN @InvoiceIds II ON ARI.[intInvoiceId] = II.[intHeaderId]
+    LEFT JOIN tblARTransactionDetail ARTD ON ARTD.intTransactionId = ARI.intInvoiceId AND ARTD.intTransactionDetailId = ARID.intInvoiceDetailId
+    OUTER APPLY (
+        SELECT TOP 1 intInvoiceDetailId 
+        FROM tblARInvoiceDetailLot ARIDL
+        WHERE ARIDL.intInvoiceDetailId = ARID.intInvoiceDetailId
+    ) ARIDL
+    WHERE ISNULL(ARID.[intInventoryShipmentItemId], 0) <> 0 
+      AND ISNULL(ARID.[intLoadDetailId], 0) = 0		
+      AND (ARTD.intTransactionDetailId IS NULL OR ARIDL.intInvoiceDetailId IS NULL)		
 	 
 END
 
