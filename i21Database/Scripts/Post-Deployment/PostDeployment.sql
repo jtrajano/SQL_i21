a20d2a5f59f832b7a90e3f391a5da009e0d3354d
Erick Dela Cruz <erick.delacruz@irely.com>
2020-04-24 19:44:32 +0800

    AP-8598

--- i21Database/Scripts/Post-Deployment/PostDeployment.sql
+++ i21Database/Scripts/Post-Deployment/PostDeployment.sql
@@ -315,6 +315,7 @@ print 'BEGIN POST DEPLOYMENT'
 :r .\AP\UpdateVoucherPayableTranType.sql
 :r .\AP\PurgePayableCompleted.sql
 :r .\AP\UpdatePaymentDetailOffset.sql
+:r .\AP\UpdateOriginDMFor20_1.sql
 
 -- Inventory 
 :r .\IC\01_InventoryTransactionTypes.sql 
