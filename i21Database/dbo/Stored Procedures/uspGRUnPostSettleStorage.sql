82767f3cad46b60c54cf88099caf3e07c1e4be6b
Leslie Guevarra <leslie.arat@irely.com>
2020-04-25 12:21:15 +0800

    GRN-2122 (19.1 RC Error on deployment )

--- i21Database/dbo/Stored Procedures/uspGRUnPostSettleStorage.sql
+++ i21Database/dbo/Stored Procedures/uspGRUnPostSettleStorage.sql
@@ -426,7 +426,6 @@ BEGIN TRY
 					,[intUserId]
 					,[intEntityId]
 					,[strSettleTicket]
-					,[strVoucher]
 					,[intTransactionTypeId]
 					,[dblPaidAmount]
 					,[intBillId]
@@ -443,7 +442,6 @@ BEGIN TRY
 					,[intUserId]			= @UserId
 					,[intEntityId]			= [intEntityId]
 					,[strSettleTicket]		= [strSettleTicket]
-					,[strVoucher]			= [strVoucher]
 					,[intTransactionTypeId]	= 4
 					,[dblPaidAmount]		= [dblPaidAmount]
 					,[intBillId]			= NULL
