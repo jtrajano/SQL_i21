2e3552889787f6dd7a5be75d29b6b8b2e94f3c13
Erick Dela Cruz <erick.delacruz@irely.com>
2020-04-25 20:07:21 +0800

    AP-8465

--- i21Database/dbo/Views/vyuAPReceiptClearingGL.sql
+++ i21Database/dbo/Views/vyuAPReceiptClearingGL.sql
@@ -1,24 +1,45 @@
 ﻿CREATE VIEW [dbo].[vyuAPReceiptClearingGL]
 AS 
 
-SELECT 
-DISTINCT
-    ad.strAccountId
-    ,ad.intAccountId
-    ,t.strTransactionId
-    ,t.intTransactionDetailId
-    ,t.intTransactionId
-    ,t.intItemId
-FROM 
-	tblICInventoryTransaction t 
-	INNER JOIN tblGLDetail gd
-		ON t.strTransactionId = gd.strTransactionId
-		AND t.intInventoryTransactionId = gd.intJournalLineNo
-	INNER JOIN vyuGLAccountDetail ad
-		ON gd.intAccountId = ad.intAccountId
-WHERE
-	--t.strTransactionId = receipt.strReceiptNumber
-	--AND t.intItemId = receiptItem.intItemId
-	ad.intAccountCategoryId = 45
-	AND t.ysnIsUnposted = 0 
+SELECT DISTINCT * FROM (
+	SELECT   
+	DISTINCT  
+		ad.strAccountId  
+		,ad.intAccountId  
+		,t.strTransactionId  
+		,t.intTransactionDetailId  
+		,t.intTransactionId  
+		,t.intItemId  
+	FROM   
+	tblICInventoryTransaction t   
+	INNER JOIN tblGLDetail gd  
+	ON t.strTransactionId = gd.strTransactionId  
+	AND t.intInventoryTransactionId = gd.intJournalLineNo  
+	INNER JOIN vyuGLAccountDetail ad  
+	ON gd.intAccountId = ad.intAccountId  
+	WHERE  
+	--t.strTransactionId = receipt.strReceiptNumber  
+	--AND t.intItemId = receiptItem.intItemId  
+	ad.intAccountCategoryId = 45  
+	AND t.ysnIsUnposted = 0   
 	AND (gd.dblCredit != 0 OR gd.dblDebit != 0)
+	UNION ALL
+	SELECT DISTINCT
+			ad.strAccountId
+			,ad.intAccountId
+			,A.strReceiptNumber
+			,B.intInventoryReceiptItemId
+			,A.intInventoryReceiptId
+			,B.intItemId
+	FROM tblICInventoryReceipt A
+	INNER JOIN tblICInventoryReceiptItem  B ON A.intInventoryReceiptId = B.intInventoryReceiptId
+	INNER JOIN tblGLDetail C 
+		ON C.strTransactionId = A.strReceiptNumber
+			AND C.intJournalLineNo = B.intInventoryReceiptItemId
+	INNER JOIN vyuGLAccountDetail ad
+		ON ad.intAccountId = C.intAccountId
+	WHERE 
+		ad.intAccountCategoryId = 45  
+	AND C.ysnIsUnposted = 0   
+	AND (C.dblCredit != 0 OR C.dblDebit != 0)
+) tmp
