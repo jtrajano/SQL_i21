62d62b8bca337ff5ca7de37c62f34ac25c481a0b
jayson.ramos <jayson.ramos@irely.com>
2020-04-27 16:56:38 +0800

    Revert "CT-4647"
    This reverts commit 890cf964a01d101aa6409848ec12040cba3120ff.

--- i21Database/dbo/Stored Procedures/uspCTCreateVoucherInvoiceForPartialPricing.sql
+++ i21Database/dbo/Stored Procedures/uspCTCreateVoucherInvoiceForPartialPricing.sql
@@ -437,16 +437,16 @@ BEGIN TRY
 							FROM tblAPBill BL
 							INNER JOIN tblAPBillDetail BD ON BL.intBillId = BD.intBillId
 							LEFT JOIN @tblCreatedTransaction CT ON CT.intTransactionId = BL.intBillId
-							WHERE BL.intTransactionType <> 13 and  BD.intInventoryReceiptItemId = @intInventoryReceiptItemId
+							WHERE BD.intInventoryReceiptItemId = @intInventoryReceiptItemId
 							AND (BL.ysnPosted = 0 OR ISNULL(CT.intTransactionId, 0) <> 0)
 						)
 						BEGIN
 							SET @allowAddDetail = 1
 						END
 
-						IF EXISTS(SELECT top 1 1 FROM tblAPBillDetail a, tblAPBill b WHERE a.intInventoryReceiptItemId = @intInventoryReceiptItemId AND a.intInventoryReceiptChargeId IS NULL AND b.intBillId = a.intBillId and  b.intTransactionType <> 13 AND @allowAddDetail = 1)
+						IF EXISTS(SELECT * FROM tblAPBillDetail WHERE intInventoryReceiptItemId = @intInventoryReceiptItemId AND intInventoryReceiptChargeId IS	NULL AND @allowAddDetail = 1)
 						BEGIN
-							SELECT	@intBillId = a.intBillId, @dblQtyReceived = a.dblQtyReceived FROM tblAPBillDetail a, tblAPBill b WHERE a.intInventoryReceiptItemId = @intInventoryReceiptItemId and b.intBillId = a.intBillId and b.intTransactionType <> 13
+							SELECT	@intBillId = intBillId, @dblQtyReceived = dblQtyReceived FROM tblAPBillDetail WHERE intInventoryReceiptItemId = @intInventoryReceiptItemId
 
 							SELECT  @ysnBillPosted = ysnPosted FROM tblAPBill WHERE intBillId = @intBillId
 
@@ -464,14 +464,12 @@ BEGIN TRY
 					    
 							-- Get the intBillDetailId of the newly inserted voucher item. 
 							SELECT	TOP 1	
-									@intBillDetailId = a.intBillDetailId 
-							FROM	tblAPBillDetail a, tblAPBill b
-							WHERE	a.intBillId = @intBillId 
-									AND a.intContractDetailId = @intContractDetailId 
-									AND a.intInventoryReceiptChargeId IS NULL
-									and b.intBillId = a.intBillId
-									and b.intTransactionType <> 13
-							ORDER BY a.intBillDetailId DESC 
+									@intBillDetailId = intBillDetailId 
+							FROM	tblAPBillDetail 
+							WHERE	intBillId = @intBillId 
+									AND intContractDetailId = @intContractDetailId 
+									AND intInventoryReceiptChargeId IS NULL
+							ORDER BY intBillDetailId DESC 
 							--UPDATING OF QUANTITY Manual
 							UPDATE tblAPBillDetail SET dblQtyOrdered = @dblReceived WHERE intBillDetailId = @intBillDetailId
 					    
@@ -747,9 +745,9 @@ BEGIN TRY
 					BEGIN
 						IF(@ysnDoUpdateCost = 1)
 						BEGIN
-							IF EXISTS(SELECT top 1 1 FROM tblAPBillDetail a, tblAPBill b WHERE a.intInventoryReceiptItemId = @intInventoryReceiptItemId AND a.intInventoryReceiptChargeId IS	NULL and b.intBillId = a.intBillId and b.intTransactionType <> 13)
+							IF EXISTS(SELECT * FROM tblAPBillDetail WHERE intInventoryReceiptItemId = @intInventoryReceiptItemId AND intInventoryReceiptChargeId IS	NULL)
 							BEGIN 
-								SELECT	@intBillId = a.intBillId, @dblQtyReceived = a.dblQtyReceived, @dblVoucherPrice = a.dblCost FROM tblAPBillDetail a, tblAPBill b WHERE a.intInventoryReceiptItemId = @intInventoryReceiptItemId and b.intBillId = a.intBillId and b.intTransactionType <> 13
+								SELECT	@intBillId = intBillId, @dblQtyReceived = dblQtyReceived, @dblVoucherPrice = dblCost FROM tblAPBillDetail WHERE intInventoryReceiptItemId = @intInventoryReceiptItemId
 					    
 								SELECT  @ysnBillPosted = ysnPosted, @ysnBillPaid = ysnPaid FROM tblAPBill WHERE intBillId = @intBillId
 								
@@ -763,7 +761,7 @@ BEGIN TRY
 								BEGIN
 									EXEC [dbo].[uspAPPostBill] @post = 0,@recap = 0,@isBatch = 0,@param = @intBillId,@userId = @intUserId,@success = @ysnSuccess OUTPUT
 								END
-								SELECT	@intBillDetailId = a.intBillDetailId FROM tblAPBillDetail a, tblAPBill b WHERE a.intBillId = @intBillId AND a.intContractDetailId = @intContractDetailId AND a.intInventoryReceiptChargeId IS NULL and b.intBillId = a.intBillId and b.intTransactionType <> 13
+								SELECT	@intBillDetailId = intBillDetailId FROM tblAPBillDetail WHERE intBillId = @intBillId AND intContractDetailId = @intContractDetailId AND intInventoryReceiptChargeId IS NULL
 					    
 								--UPDATE	tblAPBillDetail SET  dblQtyOrdered = @dblQtyToBill, dblQtyReceived = @dblQtyToBill,dblNetWeight = dbo.fnCTConvertQtyToTargetItemUOM(intUnitOfMeasureId, intWeightUOMId, @dblQtyToBill) WHERE intBillDetailId = @intBillDetailId
 
--- i21Database/dbo/Stored Procedures/uspCTSequencePriceChanged.sql
+++ i21Database/dbo/Stored Procedures/uspCTSequencePriceChanged.sql
@@ -103,7 +103,7 @@ BEGIN TRY
 		RETURN
 
 
-	if (@intPricingTypeId = 2 or (@intPricingTypeId = 1 and (SELECT count(*) FROM tblAPBillDetail a, tblAPBill b WHERE a.intContractDetailId = @intContractDetailId and b.intBillId = a.intBillId and b.intTransactionType <> 13) > 0))
+	if (@intPricingTypeId = 2 or (@intPricingTypeId = 1 and (SELECT count(*) FROM tblAPBillDetail WHERE intContractDetailId = @intContractDetailId) > 0))
 	BEGIN
 
 		if (@ysnDelete = 1) return;
@@ -150,7 +150,7 @@ BEGIN TRY
 	END
 	ELSE
 	BEGIN
-		IF NOT EXISTS(SELECT top 1 1 FROM tblAPBillDetail a, tblAPBill b WHERE a.intContractDetailId = @intContractDetailId AND a.intContractCostId IS NULL AND a.intInventoryReceiptChargeId IS NULL and b.intBillId = a.intBillId and b.intTransactionType <> 13)
+		IF NOT EXISTS(SELECT * FROM tblAPBillDetail WHERE intContractDetailId = @intContractDetailId AND intContractCostId IS NULL AND intInventoryReceiptChargeId IS NULL)
 		BEGIN
 			SELECT	@dblCashPrice = dbo.fnCTConvertQtyToTargetItemUOM(@intStockUOMId,@intSeqPriceUOMId,@dblCashPrice)
 			EXEC uspCTCreateBillForBasisContract @intContractDetailId, @dblCashPrice, null
