ed31f8fed49b0d0a117ea38af820112b5c90d23c
Feb Montefrio <feb.montefrio@irely.com>
2020-04-27 14:59:04 +0800

    [B] IC-8311

---
+++ i21Database/dbo/Functions/fnMonthName.sql
@@ -0,0 +1,24 @@
+﻿CREATE FUNCTION [dbo].[fnMonthName] (
+	  @intMonth INT
+)
+RETURNS NVARCHAR(50)
+AS
+BEGIN
+	RETURN 
+		CASE 
+			 WHEN @intMonth = 1 THEN 'January'
+			 WHEN @intMonth = 2 THEN 'February'
+			 WHEN @intMonth = 3 THEN 'March'
+			 WHEN @intMonth = 4 THEN 'April'
+			 WHEN @intMonth = 5 THEN 'May'
+			 WHEN @intMonth = 6 THEN 'June'
+			 WHEN @intMonth = 7 THEN 'July'
+			 WHEN @intMonth = 8 THEN 'August'
+			 WHEN @intMonth = 9 THEN 'September'
+			 WHEN @intMonth = 10 THEN 'October'
+			 WHEN @intMonth = 11 THEN 'November'
+			 WHEN @intMonth = 12 THEN 'December'
+			 ELSE 
+				NULL
+		END 
+END
