4779ffd1e5e4ec4e64693d0dffb410f54b8aa68f
Wendell Wayne Estrada <wendell.estrada@irely.com>
2020-04-30 10:33:58 +0800

    Revert "[B] (critical) (uat) IC-8466"
    This reverts commit 01bc8daff0ae69de2f97267c60a2008ef3e6dcce.

--- i21Database/dbo/Stored Procedures/uspICGetItemRunningStock.sql
+++ i21Database/dbo/Stored Procedures/uspICGetItemRunningStock.sql
@@ -234,7 +234,6 @@ SELECT
 	, strStorageLocationName		= strgLoc.strName
 	, intOwnershipType				= @intOwnershipType
 	, strOwnershipType				= dbo.fnICGetOwnershipType(@intOwnershipType)
-<<<<<<< Updated upstream
 	, dblRunningAvailableQty		= ROUND(t.dblQty, 6)
 	, dblStorageAvailableQty		= ROUND(t.dblUnitStorage, 6) 
 	, dblCost = CASE 
@@ -242,11 +241,6 @@ SELECT
 				WHEN CostMethod.intCostingMethodId = 2 THEN dbo.fnCalculateCostBetweenUOM(FIFO.intItemUOMId, StockUOM.intItemUOMId, FIFO.dblCost)
 				ELSE t.dblCost
 			END
-=======
-	, dblRunningAvailableQty		= t.dblQty 
-	, dblStorageAvailableQty		= t.dblUnitStorage
-	, dblCost = COALESCE(Transactions.dblCost, NULLIF(ItemPricing.dblLastCost, 0), ItemPricing.dblStandardCost)
->>>>>>> Stashed changes
 FROM @tblInventoryTransactionGrouped t INNER JOIN tblICItem i
 		ON i.intItemId = t.intItemId
 	INNER JOIN (
@@ -286,29 +280,23 @@ FROM @tblInventoryTransactionGrouped t INNER JOIN tblICItem i
 			, i.dblUnitQty
 			, i.ysnStockUnit
 	) stock 
-	CROSS APPLY (
+	OUTER APPLY(
 		SELECT TOP 1
-			  ts.intItemId
-			, ts.intItemLocationId
-			, ts.intSubLocationId
-			, ts.intStorageLocationId
-			, ts.intItemUOMId
-			, ts.intCostingMethod
-			, dtmDate = CAST(CONVERT(VARCHAR(10), ts.dtmDate,112) AS DATETIME)
-			, dblCost = ts.dblCost
-		FROM tblICInventoryTransaction ts
-		WHERE ts.intItemId = t.intItemId
-			AND ts.intItemLocationId = t.intItemLocationId 
-			AND ItemUOM.intItemUOMId = t.intItemUOMId
-		ORDER BY ts.dtmDate DESC, ts.dtmCreated DESC
-	) Transactions
+			dblCost
+			, intItemUOMId
+		FROM	
+			tblICInventoryFIFO FIFO
+		WHERE	
+			t.intItemId = FIFO.intItemId
+			AND t.intItemLocationId = FIFO.intItemLocationId
+			AND dblStockIn- dblStockOut > 0
+		ORDER BY dtmDate ASC
+	) FIFO
 	LEFT JOIN tblICItemUOM StockUOM
 		ON StockUOM.intItemId = t.intItemId
 		AND StockUOM.ysnStockUnit = 1
 	LEFT JOIN tblICItemLocation ItemLocation
 		ON ItemLocation.intItemLocationId = t.intItemLocationId
-	LEFT JOIN tblICItemPricing ItemPricing ON ItemPricing.intItemLocationId = ItemLocation.intItemLocationId
-    	AND ItemPricing.intItemId = ItemLocation.intItemId
 	LEFT JOIN tblSMCompanyLocation CompanyLocation
 		ON CompanyLocation.intCompanyLocationId = ItemLocation.intLocationId
 	LEFT JOIN tblSMCompanyLocationSubLocation SubLocation
