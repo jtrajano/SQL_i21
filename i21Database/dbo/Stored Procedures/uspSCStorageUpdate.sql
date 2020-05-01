02446c2ce1e5433e0068ed2623020e735d7e1a81
Kenneth Lew Lagunero <kenneth.lagunero@irely.com>
2020-05-01 12:49:15 +0800

    SC-3542

--- i21Database/dbo/Stored Procedures/uspSCStorageUpdate.sql
+++ i21Database/dbo/Stored Procedures/uspSCStorageUpdate.sql
@@ -484,7 +484,7 @@ BEGIN TRY
 				WHERE intCustomerStorageId = @intHoldCustomerStorageId 
 					AND ISNULL(dblBasis,0) = 0 
 					AND ISNULL(dblSettlementPrice,0) = 0
-	   AND ISNULL(@intDeliverySheetId,0) > 0
+	   AND ISNULL(@intTicketDeliverySheetId,0) > 0
 	   AND ISNULL(@intDPContractId,0) > 0
 	)
 	BEGIN
