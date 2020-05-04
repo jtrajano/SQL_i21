a325eac71e8e7b786963259336a385bc5fe043f5
Premkumar S <prem.selladurai@irely.com>
2020-05-04 17:09:06 +0530

    IN-555

--- i21Database/dbo/Tables/tblIPCompanyPreference.sql
+++ i21Database/dbo/Tables/tblIPCompanyPreference.sql
@@ -4,5 +4,6 @@
 	[strCommonDataFolderPath] NVARCHAR(250) COLLATE Latin1_General_CI_AS NULL,
 	strCustomerCode nvarchar(50),
 	ysnReplicationEnabled BIT CONSTRAINT [DF_ttblIPCompanyPreference_ysnReplicationEnabled] DEFAULT 1, 
+	ysnDestinationPortMandatoryInPOExport BIT,
     CONSTRAINT [PK_tblIPCompanyPreference_intCompanyPreferenceId] PRIMARY KEY ([intCompanyPreferenceId]) 
 )
