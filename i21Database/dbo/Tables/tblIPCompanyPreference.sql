a57b3670d0d1babaeeacae97fcdb0681d7c2e8d9
Premkumar S <prem.selladurai@irely.com>
2020-05-04 17:17:22 +0530

    IN-555

--- i21Database/dbo/Tables/tblIPCompanyPreference.sql
+++ i21Database/dbo/Tables/tblIPCompanyPreference.sql
@@ -5,6 +5,6 @@
 	strCustomerCode nvarchar(50),
 	ysnReplicationEnabled BIT CONSTRAINT [DF_ttblIPCompanyPreference_ysnReplicationEnabled] DEFAULT 1, 
 	intThirdPartyContractWaitingPeriod int,
-	ysnDestinationPortMandatoryInPOExport BIT
+	ysnDestinationPortMandatoryInPOExport BIT,
 	CONSTRAINT [PK_tblIPCompanyPreference_intCompanyPreferenceId] PRIMARY KEY ([intCompanyPreferenceId]) 
 )
