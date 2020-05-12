c360aea1d63c5b080f071ba70b9ccd08f673ef6c
manny_armas <manny.armas@iRely.com>
2020-05-12 10:13:34 +0800

    PR-2106 - HDTN-243842 - vacation Is still not Accruing correctly for 2020

--- i21Database/dbo/Stored Procedures/uspPRUpdateEmployeeTimeOff.sql
+++ i21Database/dbo/Stored Procedures/uspPRUpdateEmployeeTimeOff.sql
@@ -65,8 +65,10 @@ BEGIN
 	
 	--Calculate if Time Off is Scheduled for Reset
 	UPDATE #tmpEmployees
-	SET ysnForReset = CASE WHEN ((strAwardPeriod IN ('Anniversary Date', 'End of Year') AND GETDATE() >= dtmNextAward)
-								OR (strAwardPeriod NOT IN ('Anniversary Date', 'End of Year') AND YEAR(GETDATE()) > YEAR(dtmLastAward))) THEN 1 
+	SET ysnForReset = CASE WHEN (
+								 (strAwardPeriod IN ('Anniversary Date', 'End of Year') AND GETDATE() >= dtmNextAward AND YEAR(dtmLastAward) < YEAR(dtmNextAward)  )
+								OR (strAwardPeriod NOT IN ('Anniversary Date', 'End of Year') AND YEAR(GETDATE()) > YEAR(dtmLastAward))
+								) THEN 1 
 							ELSE 0 END
 
 	DECLARE @intEmployeeId INT
@@ -129,7 +131,7 @@ BEGIN
 			LEFT JOIN (SELECT * FROM vyuPREmployeeTimeOffUsedYTD WHERE intTypeTimeOffId = @intTypeTimeOffId) YTD
 				ON T.intEntityId = YTD.intEntityEmployeeId
 				AND YTD.intTypeTimeOffId = @intTypeTimeOffId
-				AND YTD.intYear = YEAR(T.dtmLastAward)
+				--AND YTD.intYear = YEAR(T.dtmLastAward)
 		WHERE T.[intEntityId] = @intEmployeeId
 			AND EOT.intTypeTimeOffId = @intTypeTimeOffId
 
