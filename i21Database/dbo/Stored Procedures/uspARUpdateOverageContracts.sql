9b293f8c7007cec3c327af22e4dce66964889903
kelvin_quinto <kelvin.quinto@irely.com>
2020-05-14 16:23:00 +0800

    AR-11897
    (cherry picked from commit f6991f553e03e818322b4f8945ba838ab386fb9c)

--- i21Database/dbo/Stored Procedures/uspARUpdateOverageContracts.sql
+++ i21Database/dbo/Stored Procedures/uspARUpdateOverageContracts.sql
@@ -515,6 +515,7 @@ IF EXISTS (SELECT TOP 1 NULL FROM #INVOICEDETAILSTOADD)
 			, intStorageLocationId
 			, intSubLocationId
 			, intCompanyLocationSubLocationId
+			, strDocumentNumber
 		)
 		SELECT intInvoiceDetailId				= NULL
 		    , strSourceTransaction				= 'Direct'
@@ -546,6 +547,7 @@ IF EXISTS (SELECT TOP 1 NULL FROM #INVOICEDETAILSTOADD)
 			, intStorageLocationId				= ID.intStorageLocationId
 			, intSubLocationId					= ID.intSubLocationId
 			, intCompanyLocationSubLocationId	= ID.intCompanyLocationSubLocationId
+			, strDocumentNumber					= ID.strDocumentNumber
 		FROM #INVOICEDETAILSTOADD IDTOADD
 		CROSS APPLY (
 			SELECT TOP 1 ID.*
@@ -593,6 +595,7 @@ IF EXISTS (SELECT TOP 1 NULL FROM #INVOICEDETAILSTOADD)
 			, intStorageLocationId				= NULL
 			, intSubLocationId					= NULL
 			, intCompanyLocationSubLocationId	= NULL
+			, strDocumentNumber					= ID.strDocumentNumber
 		FROM #INVOICEDETAILSTOADD IDTOADD
 		CROSS APPLY (
 			SELECT TOP 1 ID.*
