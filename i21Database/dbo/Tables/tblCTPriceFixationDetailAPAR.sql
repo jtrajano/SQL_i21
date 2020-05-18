4e37844e6539389dc984de810ffe3f29044948b3
jayson.ramos <jayson.ramos@irely.com>
2020-05-18 21:50:34 +0800

    CT-4747
    (cherry picked from commit deaa0174b7459c8b78ba2e5b7ecf034bd6fbf74e)

--- i21Database/dbo/Tables/tblCTPriceFixationDetailAPAR.sql
+++ i21Database/dbo/Tables/tblCTPriceFixationDetailAPAR.sql
@@ -15,3 +15,78 @@
 	CONSTRAINT [FK_tblCTPriceFixationDetailAPAR_tblARInvoice_intInvoiceId] FOREIGN KEY (intInvoiceId) REFERENCES tblARInvoice(intInvoiceId),
 	CONSTRAINT [FK_tblCTPriceFixationDetailAPAR_tblARInvoiceDetail_intInvoiceDetailId] FOREIGN KEY (intInvoiceDetailId) REFERENCES tblARInvoiceDetail(intInvoiceDetailId) ON DELETE CASCADE
 )
+
+GO
+
+CREATE TRIGGER [dbo].[trgCTPriceFixationDetailAPARInsertDelete]
+	ON [dbo].[tblCTPriceFixationDetailAPAR]
+    AFTER DELETE
+AS
+BEGIN	
+    SET NOCOUNT ON;
+
+    declare @intPriceFixationId int;
+    declare @intContractPriceId int;
+    declare @intPriceFixationDetailId int;
+    declare @intInvoiceDetailId int;
+	declare @dblPricedQuantity numeric(18,6);
+	declare @dblInvoiceDetailQuantity numeric(18,6);
+	declare @intContractDetailId int;
+
+	select
+		@intInvoiceDetailId = intInvoiceDetailId
+		,@intPriceFixationDetailId = intPriceFixationDetailId
+	from
+		DELETED;
+
+	SELECT top 1
+		@dblInvoiceDetailQuantity = dblQtyShipped
+	FROM
+		tblARTransactionDetail
+	where
+		intTransactionDetailId = @intInvoiceDetailId
+	ORDER BY intId DESC
+
+	select
+		@dblPricedQuantity = dblQuantity
+		,@intPriceFixationId = intPriceFixationId
+	from
+		tblCTPriceFixationDetail
+	where
+		intPriceFixationDetailId = @intPriceFixationDetailId
+
+	select @intContractPriceId = intPriceContractId,@intContractDetailId = intContractDetailId from tblCTPriceFixation where intPriceFixationId = @intPriceFixationId;
+
+	set @dblPricedQuantity = isnull(@dblPricedQuantity,0)
+	set @dblInvoiceDetailQuantity = isnull(@dblInvoiceDetailQuantity,0)
+
+	if (@dblPricedQuantity > @dblInvoiceDetailQuantity)
+	begin
+		update
+			tblCTPriceFixationDetail
+		set
+			dblQuantity = @dblPricedQuantity - @dblInvoiceDetailQuantity
+		where
+			intPriceFixationDetailId = @intPriceFixationDetailId;
+	end
+	else
+	begin
+		if ((select count(*) from tblCTPriceFixationDetail where intPriceFixationId = @intPriceFixationId) = 1)
+		begin
+			delete from tblCTPriceFixation where intPriceFixationId = @intPriceFixationId;
+			if ((select count(*) from tblCTPriceFixation where intPriceContractId = @intContractPriceId) = 0)
+			begin
+				delete from tblCTPriceFixation where intPriceContractId = @intContractPriceId;
+			end
+		end
+		else
+		begin
+			delete from tblCTPriceFixationDetail where intPriceFixationDetailId = @intPriceFixationDetailId;
+		end
+	end
+	
+	update tblCTContractDetail set intConcurrencyId = intConcurrencyId where intContractDetailId = @intContractDetailId;
+
+END
+
+GO
