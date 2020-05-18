ac05b1ef234a1a000d97092bb85e5ada9cbfa7f8
lawrence_zabala <lawrence.zabala@irely.com>
2020-05-18 15:55:41 +0800

    RM-3765

---
+++ i21Database/dbo/Tables/tblRKLogAction.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[tblRKLogAction]
+(
+	[intLogActionId] INT NOT NULL IDENTITY,
+	[strActionIn] NVARCHAR(100) COLLATE Latin1_General_CI_AS NOT NULL,
+	[strActionOut] NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL,
+	[strDescription] NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NULL,
+	CONSTRAINT [PK_tblRKLogAction] PRIMARY KEY ([intLogActionId])
+)
