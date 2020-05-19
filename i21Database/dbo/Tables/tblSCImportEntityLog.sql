fd987b2b082d132f7eb7a402f0b71dbafe577697
Kevin Estrada <kevin.estrada@irely.com>
2020-05-19 19:54:24 +0800

    FRM-9008

--- i21Database/dbo/Tables/tblSCImportEntityLog.sql
+++ i21Database/dbo/Tables/tblSCImportEntityLog.sql
@@ -2,8 +2,10 @@
 (
     [intImportEntityLogId] INT IDENTITY(1,1) NOT NULL,
     [strFileName] NVARCHAR(MAX) NULL,
+    [intEntityId] INT NULL,
     [dtmDate] DATETIME NOT NULL,
     [intConcurrencyId] INT NOT NULL DEFAULT(0)
-    CONSTRAINT [PK_dbo.tblSCImportEntityLog] PRIMARY KEY CLUSTERED ([intImportEntityLogId] ASC)
+    CONSTRAINT [PK_dbo.tblSCImportEntityLog] PRIMARY KEY CLUSTERED ([intImportEntityLogId] ASC),
+    CONSTRAINT [FK_tblSCImportEntityLog_tblEMEntity] FOREIGN KEY ([intEntityId]) REFERENCES [dbo].[tblEMEntity] ([intEntityId])
 
 )
