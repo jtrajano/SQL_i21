a3677727feceb819b9c929c4d24f39a9ab6daa2d
Smith Wilson G. de Jesus <smith.dejesus@irely.com>
2020-05-19 17:30:54 +0800

    RM-3768

---
+++ i21Database/dbo/Views/vyuRKGetMatchDerivativesFromSummaryLog.sql
@@ -0,0 +1,70 @@
+﻿CREATE VIEW [dbo].[vyuRKGetMatchDerivativesFromSummaryLog]
+
+AS
+
+SELECT intSummaryLogId
+	, strTransactionType
+	, intTransactionRecordId
+	, strTransactionNumber
+	, dtmCreatedDate
+	, dtmTransactionDate
+	, intFutureMarketId
+	, strFutureMarket
+	, intFutureMonthId
+	, strFutureMonth
+	, intFutOptTransactionId
+	, intCommodityId
+	, strCommodityCode
+	, intOrigUOMId
+	, intBookId
+	, strBook
+	, intSubBookId
+	, strSubBook
+	, intLocationId
+	, strLocationName
+	, dblOrigNoOfLots
+	, dblContractSize
+	, dblOrigQty
+	, dblPrice
+	, intEntityId
+	, intUserId
+	, intMatchNo
+	, intMatchDerivativesHeaderId
+	, intMatchDerivativesDetailId
+	, strBuySell
+	, strInstrumentType
+	, strBrokerAccount
+	, strBroker
+	, ysnPreCrush = CAST(ISNULL(ysnPreCrush, 0) AS BIT)
+	, strBrokerTradeNo
+	, strNotes
+FROM vyuRKGetSummaryLog sl
+CROSS APPLY (
+	SELECT [intMatchDerivativesHeaderId]
+		, [intMatchDerivativesDetailId]
+		, [intMatchNo]
+		, [strBuySell]
+		, [strInstrumentType]
+		, [strBrokerAccount]
+		, [strBroker]
+		, [ysnPreCrush]
+		, [strBrokerTradeNo]
+	FROM (
+		SELECT strFieldName
+			, strValue 
+		FROM dbo.fnRKGetMiscFieldTable(sl.strMiscField)
+	) t 
+	PIVOT(
+		MIN(strValue)
+		FOR strFieldName IN ([intMatchDerivativesHeaderId]
+			, [intMatchDerivativesDetailId]
+			, [intMatchNo]
+			, [strBuySell]
+			, [strInstrumentType]
+			, [strBrokerAccount]
+			, [strBroker]
+			, [ysnPreCrush]
+			, [strBrokerTradeNo])
+	) AS pivot_table
+) pt
+WHERE strTransactionType = 'Match Derivatives'
