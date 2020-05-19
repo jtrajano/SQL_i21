3f2f682f36f1df44d00cfc6662ed3bc120c4cad7
Erick Dela Cruz <erick.delacruz@irely.com>
2020-05-19 15:18:27 +0800

    AP-8691

--- i21Database/dbo/Stored Procedures/uspAPLogVoucherDetailRisk.sql
+++ i21Database/dbo/Stored Procedures/uspAPLogVoucherDetailRisk.sql
@@ -71,4 +71,79 @@ BEGIN
 	
 	EXEC [dbo].[uspRKLogRiskPosition] @SummaryLogs = @rkSummaryLog, @Rebuild = 0
 
+	DECLARE @contractBalance AS CTContractBalanceLog
+
+	INSERT INTO @contractBalance
+	(
+		strTransactionType
+		, intContractDetailId
+		, intContractHeaderId
+		, intContractTypeId
+		, intContractSeq
+		, intEntityId
+		, intCommodityId
+		, intItemId
+		, intLocationId
+		, intPricingTypeId
+		, intFutureMarketId
+		, intFutureMonthId
+		, dblBasis
+		, dblFutures
+		, intQtyUOMId
+		, intQtyCurrencyId
+		, intBasisUOMId
+		, intBasisCurrencyId
+		, intPriceUOMId
+		, dtmStartDate
+		, dtmEndDate
+		, dblQty
+		, intContractStatusId
+		, intBookId
+		, intSubBookId
+		, intUserId
+		, intActionId
+	)
+	SELECT 
+		 strTransactionType = 'Bill'
+		, intContractDetailId = bd.intContractDetailId
+		, intContractHeaderId = bd.intContractHeaderId
+		, intContractTypeId = ct.intContractTypeId
+		, intContractSeq	=	bd.intContractSeq
+		, intEntityId		=	b.intEntityVendorId
+		, c.intCommodityId
+		, bd.intItemId
+		, b.intShipToId
+		, cd.intPricingTypeId
+		, cd.intFutureMarketId
+		, cd.intFutureMonthId
+		, 0 --cd.dblBasis
+		, 0 --cd.dblFutures
+		, ISNULL(bd.intWeightUOMId, bd.intUnitOfMeasureId)--cd.intUnitMeasureId
+		, b.intCurrencyId
+		, NULL--cd.intBasisUOMId
+		, NULL--cd.intBasisCurrencyId
+		, intPriceUOMId = bd.intCostUOMId--cd.intPriceItemUOMId
+		, cd.dtmStartDate
+		, cd.dtmEndDate
+		, dblQty = CASE WHEN @remove = 1 THEN -bd.dblQtyReceived ELSE bd.dblQtyReceived END
+		, cd.intContractStatusId
+		, cd.intBookId
+		, cd.intSubBookId
+		, b.intUserId
+		, intActionId = 15
+	FROM tblAPBill b
+	INNER JOIN tblAPBillDetail bd ON bd.intBillId = b.intBillId
+	INNER JOIN (tblCTContractDetail cd INNER JOIN tblCTContractHeader ct ON ct.intContractHeaderId = cd.intContractHeaderId)
+		ON cd.intContractDetailId = cd.intContractDetailId
+	INNER JOIN @voucherDetailIds bb
+		on bb.intId = bd.intBillDetailId
+	CROSS APPLY (
+		SELECT * FROM dbo.fnAPGetVoucherCommodity(b.intBillId)
+	) c
+	INNER JOIN tblICItemUOM iuom ON iuom.intItemUOMId = bd.intUnitOfMeasureId
+	INNER JOIN tblICCommodityUnitMeasure cum ON cum.intCommodityId = c.intCommodityId AND cum.intUnitMeasureId = iuom.intUnitMeasureId
+	WHERE bd.intItemId > 0
+
+	EXEC uspCTLogContractBalance @contractBalance, 0
+
 END
