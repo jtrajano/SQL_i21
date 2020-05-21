d264d86377a46bd84e4d740c026ddedd01a7d51d
Feb Montefrio <feb.montefrio@irely.com>
2020-05-21 15:51:07 +0800

    [F] IC-8361
    # Conflicts:
    #	i21Database/Scripts/Post-Deployment/IC/17_Inventory_Constraints.sql
    #	i21Database/i21Database.sqlproj

---
+++ i21Database/dbo/Functions/fnAllowItemTypeChange.sql
@@ -0,0 +1,19 @@
+﻿CREATE FUNCTION fnAllowItemTypeChange (
+	@intItemId AS INT 
+	,@strType AS NVARCHAR(50) 
+)
+RETURNS BIT
+AS
+BEGIN
+	IF (
+		@intItemId IS NOT NULL 
+		AND (
+			EXISTS (SELECT TOP 1 1 FROM tblICInventoryTransaction t WHERE t.intItemId = @intItemId AND ISNULL(t.ysnIsUnposted,0) = 0) 
+		)
+	)
+	BEGIN 
+		RETURN 0
+	END 
+
+	RETURN 1
+END
