4690669e6e3cff7a942a4482c1a6a4c79c844a9f
Wendell Wayne Estrada <wendell.estrada@irely.com>
2020-05-22 11:41:35 +0800

    [B] IN-574 Missing API credentials table

--- i21Database/dbo/Tables/tblApiUserAccount.sql
+++ i21Database/dbo/Tables/tblApiUserAccount.sql
@@ -1,6 +1,6 @@
 CREATE TABLE [dbo].[tblApiUserAccount]
 (
-	[guiApiUserAccountId] UNIQUEIDENTIFIER NOT NULL IDENTITY, 
+	[guiApiUserAccountId] UNIQUEIDENTIFIER NOT NULL, 
     [strUsername] NVARCHAR(200) COLLATE Latin1_General_CI_AS NULL, 
     [strPasswordHash] NVARCHAR(200) COLLATE Latin1_General_CI_AS NULL, 
     [ysnActive] BIT NULL DEFAULT(1), 
