4fe2dc1ec53768f1da06ef2342b4e45d8d7a3f85
Premkumar S <prem.selladurai@irely.com>
2020-05-25 02:14:12 +0530

    IN-568, IN-569, IN-570, IN-573

---
+++ i21Database/dbo/Stored Procedures/uspIPValidateContractFeed_CA.sql
@@ -0,0 +1,177 @@
+﻿CREATE PROCEDURE uspIPValidateContractFeed_CA (
+	@strInfo1 NVARCHAR(MAX) = '' OUTPUT
+	,@strInfo2 NVARCHAR(MAX) = '' OUTPUT
+	,@intNoOfRowsAffected INT = 0 OUTPUT
+	)
+AS
+BEGIN
+	DECLARE @intContractDetailId INT
+		,@strContractNumber NVARCHAR(50)
+		,@strContractSeq NVARCHAR(50)
+		,@intContractHeaderId INT
+		,@strReference NVARCHAR(50)
+		,@intShipperId INT
+		,@intDestinationCityId INT
+		,@intDestinationPortId INT
+		,@strVendorRefNo NVARCHAR(50)
+
+	SELECT TOP 1 @intContractDetailId = L.intContractDetailId
+	FROM tblIPContractFeedLog L
+	JOIN tblCTContractHeader CH ON CH.intContractHeaderId = L.intContractHeaderId
+	JOIN tblCTContractDetail CD ON CD.intContractDetailId = L.intContractDetailId
+	WHERE L.strCustomerContract <> CH.strCustomerContract
+		OR L.intShipperId <> CD.intShipperId
+		OR L.intDestinationCityId <> CD.intDestinationCityId
+		OR L.intDestinationPortId <> CD.intDestinationPortId
+
+	IF @intContractDetailId IS NOT NULL
+	BEGIN
+		INSERT INTO tblCTContractFeed (
+			intContractHeaderId
+			,intContractDetailId
+			,strCommodityCode
+			,strCommodityDesc
+			,strContractBasis
+			,strContractBasisDesc
+			,strSubLocation
+			,strCreatedBy
+			,strCreatedByNo
+			,strEntityNo
+			,strTerm
+			,strPurchasingGroup
+			,strContractNumber
+			,strERPPONumber
+			,intContractSeq
+			,strItemNo
+			,strStorageLocation
+			,dblQuantity
+			,dblCashPrice
+			,strQuantityUOM
+			,dtmPlannedAvailabilityDate
+			,dblBasis
+			,strCurrency
+			,dblUnitCashPrice
+			,strPriceUOM
+			,strRowState
+			,dtmContractDate
+			,dtmStartDate
+			,dtmEndDate
+			,dtmFeedCreated
+			,strSubmittedBy
+			,strSubmittedByNo
+			,strOrigin
+			,dblNetWeight
+			,strNetWeightUOM
+			,strVendorAccountNum
+			,strTermCode
+			,strContractItemNo
+			,strContractItemName
+			,strERPItemNumber
+			,strERPBatchNumber
+			,strLoadingPoint
+			,strPackingDescription
+			,ysnMaxPrice
+			,ysnSubstituteItem
+			,strLocationName
+			,strSalesperson
+			,strSalespersonExternalERPId
+			,strProducer
+			,intItemId
+			,strFeedStatus
+			)
+		SELECT intContractHeaderId
+			,intContractDetailId
+			,strCommodityCode
+			,strCommodityDesc
+			,strContractBasis
+			,strContractBasisDesc
+			,strSubLocation
+			,strCreatedBy
+			,strCreatedByNo
+			,strEntityNo
+			,strTerm
+			,strPurchasingGroup
+			,strContractNumber
+			,strERPPONumber
+			,intContractSeq
+			,strItemNo
+			,strStorageLocation
+			,dblQuantity
+			,dblCashPrice
+			,strQuantityUOM
+			,dtmPlannedAvailabilityDate
+			,dblBasis
+			,strCurrency
+			,dblUnitCashPrice
+			,strPriceUOM
+			,'Modified'
+			,dtmContractDate
+			,dtmStartDate
+			,dtmEndDate
+			,GETDATE()
+			,strSubmittedBy
+			,strSubmittedByNo
+			,strOrigin
+			,dblNetWeight
+			,strNetWeightUOM
+			,strVendorAccountNum
+			,strTermCode
+			,strContractItemNo
+			,strContractItemName
+			,strERPItemNumber
+			,strERPBatchNumber
+			,strLoadingPoint
+			,strPackingDescription
+			,ysnMaxPrice
+			,ysnSubstituteItem
+			,strLocationName
+			,strSalesperson
+			,strSalespersonExternalERPId
+			,strProducer
+			,intItemId
+			,'IGNORE'
+		FROM vyuCTContractFeed
+		WHERE intContractDetailId = @intContractDetailId
+
+		SELECT TOP 1 @strContractSeq = CONVERT(VARCHAR, intContractSeq)
+			,@intContractHeaderId = intContractHeaderId
+			,@strReference = strERPPONumber
+			,@intShipperId = intShipperId
+			,@intDestinationCityId = intDestinationCityId
+			,@intDestinationPortId = intDestinationPortId
+		FROM tblCTContractDetail WITH (NOLOCK)
+		WHERE intContractDetailId = @intContractDetailId
+
+		SELECT @strContractNumber = strContractNumber
+			,@strVendorRefNo = strCustomerContract
+		FROM tblCTContractHeader WITH (NOLOCK)
+		WHERE intContractHeaderId = @intContractHeaderId
+
+		DELETE
+		FROM dbo.tblIPContractFeedLog
+		WHERE intContractDetailId = @intContractDetailId
+
+		INSERT INTO dbo.tblIPContractFeedLog (
+			intContractHeaderId
+			,intContractDetailId
+			--,intEntityId
+			,strCustomerContract
+			,intShipperId
+			,intDestinationCityId
+			,intDestinationPortId
+			)
+		SELECT @intContractHeaderId
+			,@intContractDetailId
+			--,@intEntityId
+			,@strVendorRefNo
+			,@intShipperId
+			,@intDestinationCityId
+			,@intDestinationPortId
+
+		SELECT @strInfo1 = @strContractNumber + ' / ' + ISNULL(@strContractSeq, '')
+
+		SELECT @strInfo2 = @strReference
+
+		SELECT @intNoOfRowsAffected = 1
+	END
+END
---
+++ i21Database/dbo/Tables/tblIPThirdPartyContractFeed.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tblIPThirdPartyContractFeed]					
+(					
+	intThirdPartyContractFeedId		INT NOT NULL Identity(1,1),		
+	intContractFeedId		INT NOT NULL,		
+	strERPPONumber NVARCHAR(50) COLLATE Latin1_General_CI_AS,				
+	strThirdPartyFeedStatus NVARCHAR(50) COLLATE Latin1_General_CI_AS,				
+	strThirdPartyMessage NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NULL,				
+	ysnThirdPartyMailSent				BIT DEFAULT 0,
+	dtmProcessedDate DateTime Constraint DF_tblIPThirdPartyContractFeed_dtmProcessedDate Default GetDATE(),				
+	CONSTRAINT [PK_tblIPThirdPartyContractFeed_intThirdPartyContractFeedId] PRIMARY KEY CLUSTERED (intThirdPartyContractFeedId ASC)				
+)					
