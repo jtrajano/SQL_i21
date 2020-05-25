a4aea8e0bc663236670acb00faa044f3d4893436
Premkumar S <prem.selladurai@irely.com>
2020-05-25 19:11:35 +0530

    IN-568

--- i21Database/dbo/Tables/tblIPThirdPartyContractFeed.sql
+++ i21Database/dbo/Tables/tblIPThirdPartyContractFeed.sql
@@ -7,5 +7,6 @@
 	strThirdPartyMessage NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NULL,				
 	ysnThirdPartyMailSent				BIT DEFAULT 0,
 	dtmProcessedDate DateTime Constraint DF_tblIPThirdPartyContractFeed_dtmProcessedDate Default GetDATE(),				
+	strRowState				NVARCHAR(50) COLLATE Latin1_General_CI_AS,
 	CONSTRAINT [PK_tblIPThirdPartyContractFeed_intThirdPartyContractFeedId] PRIMARY KEY CLUSTERED (intThirdPartyContractFeedId ASC)				
 )					
