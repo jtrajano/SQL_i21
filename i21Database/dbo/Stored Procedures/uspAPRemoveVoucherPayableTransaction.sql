009a1ba9c230748442b1461ccd8c303c2ed0223e
Jopet Peavey A. Lambinicio <jopet.lambinicio@irely.com>
2020-05-25 18:09:30 +0800

    AP-8729

--- i21Database/dbo/Stored Procedures/uspAPRemoveVoucherPayableTransaction.sql
+++ i21Database/dbo/Stored Procedures/uspAPRemoveVoucherPayableTransaction.sql
@@ -1,70 +1,140 @@
-﻿CREATE PROCEDURE [dbo].[uspAPRemoveVoucherPayableTransaction]
-	@intTransactionId INT = NULL,
-	@intUserId INT
-AS
-BEGIN
-
-SET QUOTED_IDENTIFIER OFF  
-SET ANSI_NULLS ON  
-SET NOCOUNT ON  
-SET XACT_ABORT OFF
-SET ANSI_WARNINGS OFF  
-
-BEGIN TRY
-	IF @intTransactionId IS NOT NULL
+﻿	CREATE PROCEDURE [dbo].[uspAPRemoveVoucherPayableTransaction]
+		@intTransactionId INT = NULL,
+		@intUserId INT
+	AS
 	BEGIN
-		--GET intId and strId OF WILL BE DELETED VOUCHER PAYABLES
-		DECLARE @intPayableIds AS TABLE (
-			intVoucherPayableId INT NOT NULL,
-			strReceiptNumber VARCHAR(20) COLLATE Latin1_General_CI_AS NOT NULL 
-		)
 
-		INSERT INTO @intPayableIds (intVoucherPayableId, strReceiptNumber)
-		SELECT P.intVoucherPayableId, IR.strReceiptNumber
-		FROM tblAPVoucherPayable P 
-		INNER JOIN tblICInventoryReceipt IR ON P.strSourceNumber = IR.strReceiptNumber
-		WHERE IR.intInventoryReceiptId = @intTransactionId
+	SET QUOTED_IDENTIFIER OFF
+	SET ANSI_NULLS ON
+	SET NOCOUNT ON
+	SET XACT_ABORT OFF
+	SET ANSI_WARNINGS OFF
 
-		DECLARE @intCompletedPayableIds AS TABLE (
-			intVoucherPayableId INT NOT NULL,
-			strReceiptNumber VARCHAR(20) COLLATE Latin1_General_CI_AS NOT NULL
-		)
+	BEGIN TRY
+		IF @intTransactionId IS NOT NULL
+		BEGIN
+			--GET intId and strId OF WILL BE DELETED PAYABLE
+			DECLARE @intPayableIds AS TABLE (
+				intVoucherPayableId INT NOT NULL
+				,strReceiptNumber VARCHAR(20) COLLATE Latin1_General_CI_AS NOT NULL
+				,dblQuantityToBill DECIMAL
+				--START PAYABLE LINKS TO VOUCHER
+				,intEntityVendorId INT NULL
+				,intPurchaseDetailId INT NULL
+				,intContractDetailId INT NULL
+				,intScaleTicketId INT NULL
+				,intInventoryReceiptChargeId INT NULL
+				,intInventoryReceiptItemId INT NULL
+				,intInventoryShipmentChargeId INT NULL
+				,intLoadShipmentDetailId INT NULL
+				,intLoadShipmentCostId INT NULL
+				,intCustomerStorageId INT NULL
+				,intSettleStorageId INT NULL
+				,intItemId INT NULL
+				,intTransactionType INT NOT NULL
+			);
 
-		INSERT INTO @intCompletedPayableIds (intVoucherPayableId, strReceiptNumber)
-		SELECT PC.intVoucherPayableId, IR.strReceiptNumber
-		FROM tblAPVoucherPayableCompleted PC 
-		INNER JOIN tblICInventoryReceipt IR ON PC.strSourceNumber = IR.strReceiptNumber
-		WHERE IR.intInventoryReceiptId = @intTransactionId
+			INSERT INTO @intPayableIds
+			SELECT P.intVoucherPayableId, IR.strReceiptNumber, P.dblQuantityToBill
+				--START PAYABLE LINKS TO VOUCHER
+				,P.intEntityVendorId
+				,P.intPurchaseDetailId
+				,P.intContractDetailId
+				,P.intScaleTicketId
+				,P.intInventoryReceiptChargeId
+				,P.intInventoryReceiptItemId
+				,P.intInventoryShipmentChargeId
+				,P.intLoadShipmentDetailId
+				,P.intLoadShipmentCostId
+				,P.intCustomerStorageId
+				,P.intSettleStorageId
+				,P.intItemId
+				,P.intTransactionType
+			FROM tblAPVoucherPayable P 
+			INNER JOIN tblICInventoryReceipt IR ON P.strSourceNumber = IR.strReceiptNumber
+			WHERE IR.intInventoryReceiptId = @intTransactionId
 
-		--DELETE VOUCHER PAYABLE AND TAX STAGING
-		DELETE P
-		FROM tblAPVoucherPayable P
-		WHERE P.strSourceNumber IN (SELECT strReceiptNumber FROM @intPayableIds)
+			--VALIDATE IF PAYABLE IS ALREADY VOUCHERED
+			DECLARE @vouchers AS TABLE(
+				strBillId NVARCHAR(50)
+				,intEntityVendorId INT NULL
+				,intPurchaseDetailId INT NULL
+				,intContractDetailId INT NULL
+				,intScaleTicketId INT NULL
+				,intInventoryReceiptChargeId INT NULL
+				,intInventoryReceiptItemId INT NULL
+				,intInventoryShipmentChargeId INT NULL
+				,intLoadShipmentDetailId INT NULL
+				,intLoadShipmentCostId INT NULL
+				,intCustomerStorageId INT NULL
+				,intSettleStorageId INT NULL
+				,intItemId INT NULL
+				,intTransactionType INT NOT NULL
+			)
 
-		DELETE T
-		FROM tblAPVoucherPayableTaxStaging T
-		WHERE T.intVoucherPayableId IN (SELECT intVoucherPayableId FROM @intPayableIds)
+			INSERT INTO @vouchers
+			SELECT TOP 10
+				A.strBillId
+				,A.intEntityVendorId
+				,B.intPurchaseDetailId
+				,B.intContractDetailId
+				,B.intScaleTicketId
+				,B.intInventoryReceiptChargeId
+				,B.intInventoryReceiptItemId
+				,B.intInventoryShipmentChargeId
+				,B.intLoadDetailId
+				,B.intLoadShipmentCostId
+				,B.intCustomerStorageId
+				,B.intSettleStorageId
+				,B.intItemId
+				,A.intTransactionType
+			FROM tblAPBill A
+			INNER JOIN tblAPBillDetail B ON A.intBillId = B.intBillId
+			INNER JOIN @intPayableIds C
+				ON 	C.intTransactionType = A.intTransactionType
+				AND	ISNULL(C.intPurchaseDetailId,-1) = ISNULL(B.intPurchaseDetailId,-1)
+				AND ISNULL(C.intContractDetailId,-1) = ISNULL(B.intContractDetailId,-1)
+				AND ISNULL(C.intScaleTicketId,-1) = ISNULL(B.intScaleTicketId,-1)
+				AND ISNULL(C.intInventoryReceiptChargeId,-1) = ISNULL(B.intInventoryReceiptChargeId,-1)
+				AND ISNULL(C.intInventoryReceiptItemId,-1) = ISNULL(B.intInventoryReceiptItemId,-1)
+				AND ISNULL(C.intLoadShipmentDetailId,-1) = ISNULL(B.intLoadDetailId,-1)
+				AND ISNULL(C.intLoadShipmentCostId,-1) = ISNULL(B.intLoadShipmentCostId,-1)
+				AND ISNULL(C.intInventoryShipmentChargeId,-1) = ISNULL(B.intInventoryShipmentChargeId,-1)
+				AND ISNULL(C.intEntityVendorId,-1) = ISNULL(A.intEntityVendorId,-1)
+				AND ISNULL(C.intLoadShipmentCostId,-1) = ISNULL(B.intLoadShipmentCostId,-1)
+				AND ISNULL(C.intCustomerStorageId,-1) = ISNULL(B.intCustomerStorageId,-1)
+				AND ISNULL(C.intSettleStorageId,-1) = ISNULL(B.intSettleStorageId,-1)
+				AND ISNULL(C.intItemId,-1) = ISNULL(B.intItemId,-1)
+			WHERE C.dblQuantityToBill <> 0
 
-		--DELETE COMPLETED VOUCHER PAYABLE AND TAX STAGING
-		DELETE PC
-		FROM tblAPVoucherPayableCompleted PC
-		WHERE PC.strSourceNumber IN (SELECT strReceiptNumber FROM @intCompletedPayableIds)
+			IF EXISTS(SELECT 1 FROM @vouchers)
+			BEGIN
+				RAISERROR('Unable to delete payable. Payable is already Vouchered', 16, 1);
+				RETURN;
+			END
+			ELSE
+			BEGIN
+				--DELETE VOUCHER PAYABLE AND TAX STAGING
+				DELETE P
+				FROM tblAPVoucherPayable P
+				WHERE P.strSourceNumber IN (SELECT strReceiptNumber FROM @intPayableIds)
 
-		DELETE TC
-		FROM tblAPVoucherPayableTaxCompleted TC
-		WHERE TC.intVoucherPayableId IN (SELECT intVoucherPayableId FROM @intCompletedPayableIds)
+				DELETE T
+				FROM tblAPVoucherPayableTaxStaging T
+				WHERE T.intVoucherPayableId IN (SELECT intVoucherPayableId FROM @intPayableIds)
+			END
+		END
+	END TRY
+
+	BEGIN CATCH	
+
+		DECLARE @ErrorMerssage NVARCHAR(MAX)
+		SELECT @ErrorMerssage = ERROR_MESSAGE()									
+		RAISERROR(@ErrorMerssage, 11, 1);
+		RETURN 0	
+
+	END CATCH		
+
+	RETURN 1		                     
+								
 	END
-END TRY
-
-BEGIN CATCH	
-
-	DECLARE @ErrorMerssage NVARCHAR(MAX)
-	SELECT @ErrorMerssage = ERROR_MESSAGE()									
-	RAISERROR(@ErrorMerssage, 11, 1);
-	RETURN 0	
-
-END CATCH		
-
-RETURN 1		                     
-		                     
-END
