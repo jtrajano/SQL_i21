6ead466427325c173093f5f42964bc2fe0c558bc
Premkumar S <prem.selladurai@irely.com>
2020-05-26 21:32:26 +0530

    MFG-4192

--- i21Database/dbo/Stored Procedures/uspMFGetBlendProductionAvailableLots.sql
+++ i21Database/dbo/Stored Procedures/uspMFGetBlendProductionAvailableLots.sql
@@ -1,27 +1,27 @@
-﻿
-CREATE PROCEDURE [dbo].[uspMFGetBlendProductionAvailableLots]
-	@intParentLotId int,
-	@intItemId int,
-	@intLocationId int,
-	@ysnShowAllPallets bit,
-	@intItemUOMId int=0,
-	@intManufacturingProcessId int=0
+﻿CREATE PROCEDURE [dbo].[uspMFGetBlendProductionAvailableLots] @intParentLotId INT
+	,@intItemId INT
+	,@intLocationId INT
+	,@ysnShowAllPallets BIT
+	,@intItemUOMId INT = 0
+	,@intManufacturingProcessId INT = 0
 AS
-
 SET QUOTED_IDENTIFIER OFF
 SET ANSI_NULLS ON
 SET NOCOUNT ON
 SET XACT_ABORT ON
 SET ANSI_WARNINGS OFF
 
-Declare @ysnEnableParentLot bit=0
-Declare @strRecipeItemUOM nvarchar(50)
-Declare @strSourceLocationIds NVARCHAR(MAX)
+DECLARE @ysnEnableParentLot BIT = 0
+DECLARE @strRecipeItemUOM NVARCHAR(50)
+DECLARE @strSourceLocationIds NVARCHAR(MAX)
 
-Select TOP 1 @ysnEnableParentLot=ISNULL(ysnEnableParentLot,0) From tblMFCompanyPreference
-Select @strRecipeItemUOM=um.strUnitMeasure 
-From tblICItemUOM iu join tblICUnitMeasure um on iu.intUnitMeasureId=um.intUnitMeasureId
-Where iu.intItemUOMId=@intItemUOMId
+SELECT TOP 1 @ysnEnableParentLot = ISNULL(ysnEnableParentLot, 0)
+FROM tblMFCompanyPreference
+
+SELECT @strRecipeItemUOM = um.strUnitMeasure
+FROM tblICItemUOM iu
+JOIN tblICUnitMeasure um ON iu.intUnitMeasureId = um.intUnitMeasureId
+WHERE iu.intItemUOMId = @intItemUOMId
 
 SELECT @strSourceLocationIds = ISNULL(pa.strAttributeValue, '')
 FROM tblMFManufacturingProcessAttribute pa
@@ -30,88 +30,139 @@ WHERE intManufacturingProcessId = @intManufacturingProcessId
 	AND intLocationId = @intLocationId
 	AND at.strAttributeName = 'Source Location'
 
-Declare @tblSourceStorageLocation AS Table
-(
-	intStorageLocationId int
-)
+DECLARE @tblSourceStorageLocation AS TABLE (intStorageLocationId INT)
 
-If ISNULL(@strSourceLocationIds,'')<>''
-Begin
-	Insert Into @tblSourceStorageLocation
-	Select * from dbo.fnCommaSeparatedValueToTable(@strSourceLocationIds)
-End
-Else
-Begin
-	Insert Into @tblSourceStorageLocation
-	Select intStorageLocationId from tblICStorageLocation Where intLocationId=@intLocationId AND ISNULL(ysnAllowConsume,0)=1
-End
+IF Ltrim(ISNULL(@strSourceLocationIds, '')) <> ''
+BEGIN
+	INSERT INTO @tblSourceStorageLocation
+	SELECT *
+	FROM dbo.fnCommaSeparatedValueToTable(@strSourceLocationIds)
+END
+ELSE
+BEGIN
+	INSERT INTO @tblSourceStorageLocation
+	SELECT intStorageLocationId
+	FROM tblICStorageLocation
+	WHERE intLocationId = @intLocationId
+		AND ISNULL(ysnAllowConsume, 0) = 1
+END
 
-Declare @tblReservedQty table
-(
-	intLotId int,
-	dblReservedQty numeric(38,20)
-)
+DECLARE @tblReservedQty TABLE (
+	intLotId INT
+	,dblReservedQty NUMERIC(38, 20)
+	)
 
-Insert into @tblReservedQty
-Select sr.intLotId,Sum(sr.dblQty) AS dblReservedQty 
-From tblICStockReservation sr 
-where sr.intItemId=@intItemId AND ISNULL(sr.ysnPosted,0)=0
-group by sr.intLotId
+INSERT INTO @tblReservedQty
+SELECT sr.intLotId
+	,Sum(sr.dblQty) AS dblReservedQty
+FROM tblICStockReservation sr
+WHERE sr.intItemId = @intItemId
+	AND ISNULL(sr.ysnPosted, 0) = 0
+GROUP BY sr.intLotId
 
-Select l.intLotId,l.strLotNumber,l.intItemId,i.strItemNo,i.strDescription,ISNULL(l.strLotAlias,'') AS strLotAlias,
-CASE WHEN isnull(l.dblWeight,0)>0 Then l.dblWeight Else dbo.fnMFConvertQuantityToTargetItemUOM(l.intItemUOMId,@intItemUOMId,l.dblQty) End AS dblPhysicalQty,
-ISNULL(c.dblReservedQty,0) AS dblReservedQty, 
-ISNULL((ISNULL(CASE WHEN isnull(l.dblWeight,0)>0 Then l.dblWeight Else dbo.fnMFConvertQuantityToTargetItemUOM(l.intItemUOMId,@intItemUOMId,l.dblQty) End,0) 
-- ISNULL(c.dblReservedQty,0)),0) AS dblAvailableQty,
-ISNULL(l.intWeightUOMId,@intItemUOMId) AS intItemUOMId ,ISNULL(u.strUnitMeasure,@strRecipeItemUOM) AS strUOM, 
-ROUND((ISNULL((ISNULL(CASE WHEN isnull(l.dblWeight,0)>0 Then l.dblWeight Else dbo.fnMFConvertQuantityToTargetItemUOM(l.intItemUOMId,@intItemUOMId,l.dblQty) End,0) 
-- ISNULL(c.dblReservedQty,0)),0)/ case when ISNULL(iu1.dblUnitQty,0)=0 then 1 else iu1.dblUnitQty end ),0) AS dblAvailableUnit,
-l.dblLastCost AS dblUnitCost,iu1.dblUnitQty AS dblWeightPerUnit,u.strUnitMeasure AS strWeightPerUnitUOM,
-l.intItemUOMId  AS intPhysicalItemUOMId,l.dtmDateCreated AS dtmReceiveDate,l.dtmExpiryDate,ISNULL(' ','') AS strVendorId,ISNULL(l.strVendorLotNo,'') AS strVendorLotNo,
-l.strGarden AS strGarden,l.intLocationId,
-cl.strLocationName AS strLocationName,
-sbl.strSubLocationName,
-sl.strName AS strStorageLocationName,
-l.strNotes AS strRemarks,
-i.dblRiskScore,
-ISNULL(l.intParentLotId,0) AS intParentLotId,
-sl.intStorageLocationId,
-ISNULL(pl.strParentLotNumber,'') AS strParentLotNumber,
-i.strLotTracking,
-i.intCategoryId
-into #tempLot
-from tblICLot l
-Left Join @tblReservedQty c on l.intLotId=c.intLotId
-Join tblICItem i on l.intItemId=i.intItemId
-Left Join tblICItemUOM iu on l.intWeightUOMId=iu.intItemUOMId
-Left Join tblICUnitMeasure u on iu.intUnitMeasureId=u.intUnitMeasureId
-Join tblSMCompanyLocation cl on cl.intCompanyLocationId=l.intLocationId
-Left Join tblSMCompanyLocationSubLocation sbl on sbl.intCompanyLocationSubLocationId=l.intSubLocationId
-Left Join tblICStorageLocation sl on sl.intStorageLocationId=l.intStorageLocationId
-Left Join tblICStorageUnitType ut on sl.intStorageUnitTypeId=ut.intStorageUnitTypeId AND ut.strInternalCode <> 'PROD_STAGING'
-Join @tblSourceStorageLocation tsl on sl.intStorageLocationId=tsl.intStorageLocationId
-Join tblICItemUOM iu1 on l.intItemUOMId=iu1.intItemUOMId
+SELECT l.intLotId
+	,l.strLotNumber
+	,l.intItemId
+	,i.strItemNo
+	,i.strDescription
+	,ISNULL(l.strLotAlias, '') AS strLotAlias
+	,CASE 
+		WHEN isnull(l.dblWeight, 0) > 0
+			THEN l.dblWeight
+		ELSE dbo.fnMFConvertQuantityToTargetItemUOM(l.intItemUOMId, @intItemUOMId, l.dblQty)
+		END AS dblPhysicalQty
+	,ISNULL(c.dblReservedQty, 0) AS dblReservedQty
+	,ISNULL((
+			ISNULL(CASE 
+					WHEN isnull(l.dblWeight, 0) > 0
+						THEN l.dblWeight
+					ELSE dbo.fnMFConvertQuantityToTargetItemUOM(l.intItemUOMId, @intItemUOMId, l.dblQty)
+					END, 0) - ISNULL(c.dblReservedQty, 0)
+			), 0) AS dblAvailableQty
+	,ISNULL(l.intWeightUOMId, @intItemUOMId) AS intItemUOMId
+	,ISNULL(u.strUnitMeasure, @strRecipeItemUOM) AS strUOM
+	,ROUND((
+			ISNULL((
+					ISNULL(CASE 
+							WHEN isnull(l.dblWeight, 0) > 0
+								THEN l.dblWeight
+							ELSE dbo.fnMFConvertQuantityToTargetItemUOM(l.intItemUOMId, @intItemUOMId, l.dblQty)
+							END, 0) - ISNULL(c.dblReservedQty, 0)
+					), 0) / CASE 
+				WHEN ISNULL(iu1.dblUnitQty, 0) = 0
+					THEN 1
+				ELSE iu1.dblUnitQty
+				END
+			), 0) AS dblAvailableUnit
+	,l.dblLastCost AS dblUnitCost
+	,iu1.dblUnitQty AS dblWeightPerUnit
+	,u.strUnitMeasure AS strWeightPerUnitUOM
+	,l.intItemUOMId AS intPhysicalItemUOMId
+	,l.dtmDateCreated AS dtmReceiveDate
+	,l.dtmExpiryDate
+	,ISNULL(' ', '') AS strVendorId
+	,ISNULL(l.strVendorLotNo, '') AS strVendorLotNo
+	,l.strGarden AS strGarden
+	,l.intLocationId
+	,cl.strLocationName AS strLocationName
+	,sbl.strSubLocationName
+	,sl.strName AS strStorageLocationName
+	,l.strNotes AS strRemarks
+	,i.dblRiskScore
+	,ISNULL(l.intParentLotId, 0) AS intParentLotId
+	,sl.intStorageLocationId
+	,ISNULL(pl.strParentLotNumber, '') AS strParentLotNumber
+	,i.strLotTracking
+	,i.intCategoryId
+INTO #tempLot
+FROM tblICLot l
+LEFT JOIN @tblReservedQty c ON l.intLotId = c.intLotId
+JOIN tblICItem i ON l.intItemId = i.intItemId
+LEFT JOIN tblICItemUOM iu ON l.intWeightUOMId = iu.intItemUOMId
+LEFT JOIN tblICUnitMeasure u ON iu.intUnitMeasureId = u.intUnitMeasureId
+JOIN tblSMCompanyLocation cl ON cl.intCompanyLocationId = l.intLocationId
+LEFT JOIN tblSMCompanyLocationSubLocation sbl ON sbl.intCompanyLocationSubLocationId = l.intSubLocationId
+LEFT JOIN tblICStorageLocation sl ON sl.intStorageLocationId = l.intStorageLocationId
+LEFT JOIN tblICStorageUnitType ut ON sl.intStorageUnitTypeId = ut.intStorageUnitTypeId
+	AND ut.strInternalCode <> 'PROD_STAGING'
+JOIN @tblSourceStorageLocation tsl ON sl.intStorageLocationId = tsl.intStorageLocationId
+JOIN tblICItemUOM iu1 ON l.intItemUOMId = iu1.intItemUOMId
 --Left Join vyuAPVendor v on l.intVendorId=v.intVendorId
-Join tblICLotStatus ls on l.intLotStatusId=ls.intLotStatusId
-Left Join tblICParentLot pl on l.intParentLotId=pl.intParentLotId
-Where l.intItemId=@intItemId and l.dblQty>0 and ls.strPrimaryStatus='Active' 
-And l.intLocationId = @intLocationId AND ISNULL(sl.ysnAllowConsume,0)=1
-Order by l.dtmExpiryDate, l.dtmDateCreated
+JOIN tblICLotStatus ls ON l.intLotStatusId = ls.intLotStatusId
+LEFT JOIN tblICParentLot pl ON l.intParentLotId = pl.intParentLotId
+WHERE l.intItemId = @intItemId
+	AND l.dblQty > 0
+	AND ls.strPrimaryStatus = 'Active'
+	AND l.intLocationId = @intLocationId
+	AND ISNULL(sl.ysnAllowConsume, 0) = 1
+ORDER BY l.dtmExpiryDate
+	,l.dtmDateCreated
 
-If @ysnEnableParentLot=0
-Begin
-	Select * from #tempLot
-End
-Else
-Begin
-	if @ysnShowAllPallets=0
-	Begin
-		Select * from #tempLot --where intParentLotId=@intParentLotId
-	End
-	Else
-	Begin
-		Select * from #tempLot where intParentLotId <> 0
-	End
-End
-
-
+IF @ysnEnableParentLot = 0
+BEGIN
+	SELECT *
+	FROM #tempLot
+END
+ELSE
+BEGIN
+	IF @ysnShowAllPallets = 0
+	BEGIN
+		IF @intParentLotId = 0
+		BEGIN
+			SELECT *
+			FROM #tempLot --where intParentLotId=@intParentLotId
+		END
+		ELSE
+		BEGIN
+			SELECT *
+			FROM #tempLot
+			WHERE intParentLotId = @intParentLotId
+		END
+	END
+	ELSE
+	BEGIN
+		SELECT *
+		FROM #tempLot
+		WHERE intParentLotId <> 0
+	END
+END
