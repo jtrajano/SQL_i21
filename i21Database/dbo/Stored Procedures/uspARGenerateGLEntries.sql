e12be0678ab5eea5e67228fb7ef9950c3ce1b845
kelvin_quinto <kelvin.quinto@irely.com>
2020-05-29 10:10:26 +0800

    AR-11810
    (cherry picked from commit ad01c84efe315d45c424dae4c2ac69e53d515e6f)
    (cherry picked from commit 0f73572017f07d7963435654b6f379394f75cb29)
    (cherry picked from commit b1f47d52491a36f9022d624b1ed125ff4068d04f)

--- i21Database/dbo/Stored Procedures/uspARGenerateGLEntries.sql
+++ i21Database/dbo/Stored Procedures/uspARGenerateGLEntries.sql
@@ -4,6 +4,7 @@
     ,@PostDate          DATETIME        = NULL
     ,@BatchId           NVARCHAR(40)    = NULL
     ,@UserId            INT             = NULL
+    ,@raiseError        BIT             = 1
 AS
 SET QUOTED_IDENTIFIER OFF
 SET ANSI_NULLS ON
@@ -14,6 +15,26 @@ DECLARE @MODULE_NAME NVARCHAR(25) = 'Accounts Receivable'
 DECLARE @SCREEN_NAME NVARCHAR(25) = 'Invoice'
 DECLARE @CODE NVARCHAR(25) = 'AR'
 DECLARE @POSTDESC NVARCHAR(10) = 'Posted '
+
+DECLARE @ErrorMerssage		NVARCHAR(MAX) = NULL	  
+	  , @InitTranCount		INT
+	  , @CurrentTranCount	INT
+	  , @Savepoint			NVARCHAR(32)
+	  , @CurrentSavepoint	NVARCHAR(32)
+
+SET @InitTranCount = @@TRANCOUNT
+SET @Savepoint = 'uspARGenerateGLEntries'
+
+IF ISNULL(@raiseError,0) = 0	
+	BEGIN
+		IF @InitTranCount = 0
+			BEGIN TRANSACTION
+		ELSE
+			BEGIN
+				COMMIT TRANSACTION
+				BEGIN TRANSACTION @Savepoint
+			END
+	END
 
 IF @Post = 1
 BEGIN
@@ -248,48 +269,106 @@ WHERE
 -- Call the post routine 
 IF EXISTS (SELECT TOP 1 1 FROM @ItemsForPost)
 BEGIN
-    -- Call the post routine 
-    INSERT INTO #ARInvoiceGLEntries (
-         [dtmDate]
-        ,[strBatchId]
-        ,[intAccountId]
-        ,[dblDebit]
-        ,[dblCredit]
-        ,[dblDebitUnit]
-        ,[dblCreditUnit]
-        ,[strDescription]
-        ,[strCode]
-        ,[strReference]
-        ,[intCurrencyId]
-        ,[dblExchangeRate]
-        ,[dtmDateEntered]
-        ,[dtmTransactionDate]
-        ,[strJournalLineDescription]
-        ,[intJournalLineNo]
-        ,[ysnIsUnposted]
-        ,[intUserId]
-        ,[intEntityId]
-        ,[strTransactionId]
-        ,[intTransactionId]
-        ,[strTransactionType]
-        ,[strTransactionForm]
-        ,[strModuleName]
-        ,[intConcurrencyId]
-        ,[dblDebitForeign]
-        ,[dblDebitReport]
-        ,[dblCreditForeign]
-        ,[dblCreditReport]
-        ,[dblReportingRate]
-        ,[dblForeignRate]
-        ,[strRateType]
-		,[intSourceEntityId]
-		,[intCommodityId]
-	)
-    EXEC dbo.uspICPostCosting  
-         @ItemsForPost  
-        ,@BatchId  
-        ,@ACCOUNT_CATEGORY_TO_COUNTER_INVENTORY
-        ,@UserId
+	BEGIN TRY
+		-- Call the post routine 
+		INSERT INTO #ARInvoiceGLEntries (
+			 [dtmDate]
+			,[strBatchId]
+			,[intAccountId]
+			,[dblDebit]
+			,[dblCredit]
+			,[dblDebitUnit]
+			,[dblCreditUnit]
+			,[strDescription]
+			,[strCode]
+			,[strReference]
+			,[intCurrencyId]
+			,[dblExchangeRate]
+			,[dtmDateEntered]
+			,[dtmTransactionDate]
+			,[strJournalLineDescription]
+			,[intJournalLineNo]
+			,[ysnIsUnposted]
+			,[intUserId]
+			,[intEntityId]
+			,[strTransactionId]
+			,[intTransactionId]
+			,[strTransactionType]
+			,[strTransactionForm]
+			,[strModuleName]
+			,[intConcurrencyId]
+			,[dblDebitForeign]
+			,[dblDebitReport]
+			,[dblCreditForeign]
+			,[dblCreditReport]
+			,[dblReportingRate]
+			,[dblForeignRate]
+			,[strRateType]
+			,[intSourceEntityId]
+			,[intCommodityId]
+		)
+		EXEC dbo.uspICPostCosting  
+			 @ItemsForPost  
+			,@BatchId  
+			,@ACCOUNT_CATEGORY_TO_COUNTER_INVENTORY
+			,@UserId
+	END TRY
+	BEGIN CATCH
+		SELECT @ErrorMerssage = ERROR_MESSAGE()
+		IF @raiseError = 0
+			BEGIN
+				ROLLBACK TRANSACTION @Savepoint
+				
+				SET @CurrentTranCount = @@TRANCOUNT
+				SET @CurrentSavepoint = SUBSTRING(('uspARPostInvoiceNew' + CONVERT(VARCHAR, @CurrentTranCount)), 1, 32)										
+			
+				IF @CurrentTranCount = 0
+					BEGIN TRANSACTION
+				ELSE
+					SAVE TRANSACTION @CurrentSavepoint
+						
+				INSERT INTO #ARInvalidInventories (
+					 [strMessage]
+					,[strTransactionType]
+					,[strTransactionId]
+					,[strBatchNumber]
+					,[intTransactionId]
+				)
+				SELECT DISTINCT
+					 [strError]             = @ErrorMerssage
+					,[strTransactionType]   = IH.[strTransactionType] 
+					,[strTransactionId]     = IH.[strInvoiceNumber]
+					,[strBatchNumber]       = @BatchId
+					,[intTransactionId]     = IH.[intInvoiceId] 
+				FROM #ARPostInvoiceHeader IH
+				INNER JOIN #ARItemsForCosting COSTING ON IH.strInvoiceNumber = COSTING.strTransactionId AND IH.intInvoiceId = COSTING.intTransactionId
+				LEFT JOIN (
+					SELECT DISTINCT intTransactionId
+								  , strTransactionId
+					FROM tblICInventoryTransaction
+					WHERE strTransactionForm = 'Invoice'
+					  AND ysnIsUnposted = 0
+				) ICT ON IH.strInvoiceNumber = ICT.strTransactionId AND IH.intInvoiceId = ICT.intTransactionId
+				WHERE ICT.intTransactionId IS NULL
+
+				SET @CurrentTranCount = @@TRANCOUNT
+
+				IF @CurrentTranCount = 0
+					BEGIN
+						IF (XACT_STATE()) = -1
+							ROLLBACK TRANSACTION
+						IF (XACT_STATE()) = 1
+							COMMIT TRANSACTION @CurrentSavepoint
+					END		
+				ELSE
+					BEGIN
+						IF (XACT_STATE()) = -1
+							ROLLBACK TRANSACTION  @CurrentSavepoint
+					END	
+			END						
+		IF @raiseError = 1
+			RAISERROR(@ErrorMerssage, 11, 1)
+	END CATCH
 END
 
 DECLARE  @InTransitItems                ItemInTransitCostingTableType 
@@ -349,48 +428,106 @@ FROM
 	#ARItemsForInTransitCosting
 
 IF EXISTS (SELECT TOP 1 1 FROM @InTransitItems)
-BEGIN 
-    -- Call the post routine 
-    INSERT INTO #ARInvoiceGLEntries
-        ([dtmDate] 
-        ,[strBatchId]
-        ,[intAccountId]
-        ,[dblDebit]
-        ,[dblCredit]
-        ,[dblDebitUnit]
-        ,[dblCreditUnit]
-        ,[strDescription]
-        ,[strCode]
-        ,[strReference]
-        ,[intCurrencyId]
-        ,[dblExchangeRate]
-        ,[dtmDateEntered]
-        ,[dtmTransactionDate]
-        ,[strJournalLineDescription]
-        ,[intJournalLineNo]
-        ,[ysnIsUnposted]
-        ,[intUserId]
-        ,[intEntityId]
-        ,[strTransactionId]
-        ,[intTransactionId]
-        ,[strTransactionType]
-        ,[strTransactionForm]
-        ,[strModuleName]
-        ,[intConcurrencyId]
-        ,[dblDebitForeign]
-        ,[dblDebitReport]
-        ,[dblCreditForeign]
-        ,[dblCreditReport]
-        ,[dblReportingRate]
-        ,[dblForeignRate]
-		,[intSourceEntityId]
-		,[intCommodityId]
-	)
-	EXEC dbo.uspICPostInTransitCosting  
-         @InTransitItems  
-        ,@BatchId  
-        ,@ACCOUNT_CATEGORY_TO_COUNTER_INVENTORY
-        ,@UserId
+BEGIN
+	BEGIN TRY 
+		 --Call the post routine 
+		INSERT INTO #ARInvoiceGLEntries
+			([dtmDate] 
+			,[strBatchId]
+			,[intAccountId]
+			,[dblDebit]
+			,[dblCredit]
+			,[dblDebitUnit]
+			,[dblCreditUnit]
+			,[strDescription]
+			,[strCode]
+			,[strReference]
+			,[intCurrencyId]
+			,[dblExchangeRate]
+			,[dtmDateEntered]
+			,[dtmTransactionDate]
+			,[strJournalLineDescription]
+			,[intJournalLineNo]
+			,[ysnIsUnposted]
+			,[intUserId]
+			,[intEntityId]
+			,[strTransactionId]
+			,[intTransactionId]
+			,[strTransactionType]
+			,[strTransactionForm]
+			,[strModuleName]
+			,[intConcurrencyId]
+			,[dblDebitForeign]
+			,[dblDebitReport]
+			,[dblCreditForeign]
+			,[dblCreditReport]
+			,[dblReportingRate]
+			,[dblForeignRate]
+			,[intSourceEntityId]
+			,[intCommodityId]
+		)
+		EXEC dbo.uspICPostInTransitCosting  
+			 @InTransitItems  
+			,@BatchId  
+			,@ACCOUNT_CATEGORY_TO_COUNTER_INVENTORY
+			,@UserId
+	END TRY
+	BEGIN CATCH
+		SELECT @ErrorMerssage = ERROR_MESSAGE()
+		IF @raiseError = 0
+			BEGIN
+				ROLLBACK TRANSACTION @Savepoint
+				
+				SET @CurrentTranCount = @@TRANCOUNT
+				SET @CurrentSavepoint = SUBSTRING(('uspARPostInvoiceNew' + CONVERT(VARCHAR, @CurrentTranCount)), 1, 32)										
+			
+				IF @CurrentTranCount = 0
+					BEGIN TRANSACTION
+				ELSE
+					SAVE TRANSACTION @CurrentSavepoint
+						
+				INSERT INTO #ARInvalidInventories (
+					 [strMessage]
+					,[strTransactionType]
+					,[strTransactionId]
+					,[strBatchNumber]
+					,[intTransactionId]
+				)
+				SELECT DISTINCT
+					 [strError]             = @ErrorMerssage
+					,[strTransactionType]   = IH.[strTransactionType] 
+					,[strTransactionId]     = IH.[strInvoiceNumber]
+					,[strBatchNumber]       = @BatchId
+					,[intTransactionId]     = IH.[intInvoiceId] 
+				FROM #ARPostInvoiceHeader IH
+				INNER JOIN #ARItemsForInTransitCosting INTRANSIT ON IH.strInvoiceNumber = INTRANSIT.strTransactionId AND IH.intInvoiceId = INTRANSIT.intTransactionId
+				LEFT JOIN (
+					SELECT DISTINCT intTransactionId
+								  , strTransactionId
+					FROM tblICInventoryTransaction
+					WHERE strTransactionForm = 'Invoice'
+					  AND ysnIsUnposted = 0
+				) ICT ON IH.strInvoiceNumber = ICT.strTransactionId AND IH.intInvoiceId = ICT.intTransactionId
+				WHERE ICT.intTransactionId IS NULL
+
+				SET @CurrentTranCount = @@TRANCOUNT
+
+				IF @CurrentTranCount = 0
+					BEGIN
+						IF (XACT_STATE()) = -1
+							ROLLBACK TRANSACTION
+						IF (XACT_STATE()) = 1
+							COMMIT TRANSACTION @CurrentSavepoint
+					END		
+				ELSE
+					BEGIN
+						IF (XACT_STATE()) = -1
+							ROLLBACK TRANSACTION  @CurrentSavepoint
+					END	
+			END						
+		IF @raiseError = 1
+			RAISERROR(@ErrorMerssage, 11, 1)
+	END CATCH
 
 	UPDATE B
 	SET intAccountId = dbo.fnGetItemGLAccount(C.intLinkedItemId, A.intItemLocationId, 'Cost Of Goods')		
@@ -457,46 +594,104 @@ FROM
 -- Call the post routine 
 IF EXISTS (SELECT TOP 1 1 FROM @StorageItemsForPost) 
 BEGIN 
-    -- Call the post routine 
-    INSERT INTO #ARInvoiceGLEntries
-        ([dtmDate] 
-        ,[strBatchId]
-        ,[intAccountId]
-        ,[dblDebit]
-        ,[dblCredit]
-        ,[dblDebitUnit]
-        ,[dblCreditUnit]
-        ,[strDescription]
-        ,[strCode]
-        ,[strReference]
-        ,[intCurrencyId]
-        ,[dblExchangeRate]
-        ,[dtmDateEntered]
-        ,[dtmTransactionDate]
-        ,[strJournalLineDescription]
-        ,[intJournalLineNo]
-        ,[ysnIsUnposted]
-        ,[intUserId]
-        ,[intEntityId]
-        ,[strTransactionId]
-        ,[intTransactionId]
-        ,[strTransactionType]
-        ,[strTransactionForm]
-        ,[strModuleName]
-        ,[intConcurrencyId]
-        ,[dblDebitForeign]
-        ,[dblDebitReport]
-        ,[dblCreditForeign]
-        ,[dblCreditReport]
-        ,[dblReportingRate]
-        ,[dblForeignRate]
-		,[intSourceEntityId]
-		,[intCommodityId]
-	)
-    EXEC dbo.uspICPostStorage  
-             @StorageItemsForPost  
-            ,@BatchId  		
-            ,@UserId			
+	BEGIN TRY
+		-- Call the post routine 
+		INSERT INTO #ARInvoiceGLEntries
+			([dtmDate] 
+			,[strBatchId]
+			,[intAccountId]
+			,[dblDebit]
+			,[dblCredit]
+			,[dblDebitUnit]
+			,[dblCreditUnit]
+			,[strDescription]
+			,[strCode]
+			,[strReference]
+			,[intCurrencyId]
+			,[dblExchangeRate]
+			,[dtmDateEntered]
+			,[dtmTransactionDate]
+			,[strJournalLineDescription]
+			,[intJournalLineNo]
+			,[ysnIsUnposted]
+			,[intUserId]
+			,[intEntityId]
+			,[strTransactionId]
+			,[intTransactionId]
+			,[strTransactionType]
+			,[strTransactionForm]
+			,[strModuleName]
+			,[intConcurrencyId]
+			,[dblDebitForeign]
+			,[dblDebitReport]
+			,[dblCreditForeign]
+			,[dblCreditReport]
+			,[dblReportingRate]
+			,[dblForeignRate]
+			,[intSourceEntityId]
+			,[intCommodityId]
+		)
+		EXEC dbo.uspICPostStorage  
+				 @StorageItemsForPost  
+				,@BatchId  		
+				,@UserId
+	END TRY
+	BEGIN CATCH
+		SELECT @ErrorMerssage = ERROR_MESSAGE()
+		IF @raiseError = 0
+			BEGIN
+				ROLLBACK TRANSACTION @Savepoint
+				
+				SET @CurrentTranCount = @@TRANCOUNT
+				SET @CurrentSavepoint = SUBSTRING(('uspARPostInvoiceNew' + CONVERT(VARCHAR, @CurrentTranCount)), 1, 32)										
+			
+				IF @CurrentTranCount = 0
+					BEGIN TRANSACTION
+				ELSE
+					SAVE TRANSACTION @CurrentSavepoint
+						
+				INSERT INTO #ARInvalidInventories (
+					 [strMessage]
+					,[strTransactionType]
+					,[strTransactionId]
+					,[strBatchNumber]
+					,[intTransactionId]
+				)
+				SELECT DISTINCT
+					 [strError]             = @ErrorMerssage
+					,[strTransactionType]   = IH.[strTransactionType] 
+					,[strTransactionId]     = IH.[strInvoiceNumber]
+					,[strBatchNumber]       = @BatchId
+					,[intTransactionId]     = IH.[intInvoiceId] 
+				FROM #ARPostInvoiceHeader IH
+				INNER JOIN #ARItemsForStorageCosting STORAGE ON IH.strInvoiceNumber = STORAGE.strTransactionId AND IH.intInvoiceId = STORAGE.intTransactionId
+				LEFT JOIN (
+					SELECT DISTINCT intTransactionId
+								  , strTransactionId
+					FROM tblICInventoryTransaction
+					WHERE strTransactionForm = 'Invoice'
+					  AND ysnIsUnposted = 0
+				) ICT ON IH.strInvoiceNumber = ICT.strTransactionId AND IH.intInvoiceId = ICT.intTransactionId				
+				WHERE ICT.intTransactionId IS NULL
+
+				SET @CurrentTranCount = @@TRANCOUNT
+
+				IF @CurrentTranCount = 0
+					BEGIN
+						IF (XACT_STATE()) = -1
+							ROLLBACK TRANSACTION
+						IF (XACT_STATE()) = 1
+							COMMIT TRANSACTION @CurrentSavepoint
+					END		
+				ELSE
+					BEGIN
+						IF (XACT_STATE()) = -1
+							ROLLBACK TRANSACTION  @CurrentSavepoint
+					END	
+			END						
+		IF @raiseError = 1
+			RAISERROR(@ErrorMerssage, 11, 1)
+	END CATCH
 END
 
 IF @Post = 0
