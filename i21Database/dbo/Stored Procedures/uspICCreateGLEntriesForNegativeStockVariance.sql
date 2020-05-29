9753a81a27cb5d9f1b5a9e492af380119f4cbb7b
Feb Montefrio <feb.montefrio@irely.com>
2020-05-29 15:23:41 +0800

    [B] IC-8576
    # Conflicts:
    #	i21Database/dbo/Stored Procedures/uspICCreateGLEntriesForNegativeStockVariance.sql

--- i21Database/dbo/Stored Procedures/uspICCreateGLEntriesForNegativeStockVariance.sql
+++ i21Database/dbo/Stored Procedures/uspICCreateGLEntriesForNegativeStockVariance.sql
@@ -16,6 +16,20 @@ SET NOCOUNT ON
 SET XACT_ABORT ON
 SET ANSI_WARNINGS OFF
 
+-- Create the temp table for the specific items/categories to rebuild
+IF OBJECT_ID('tempdb..#tmpRebuildList') IS NULL  
+BEGIN 
+	CREATE TABLE #tmpRebuildList (
+		intItemId INT NULL 
+		,intCategoryId INT NULL 
+	)	
+END 
+
+IF NOT EXISTS (SELECT TOP 1 1 FROM #tmpRebuildList)
+BEGIN 
+	INSERT INTO #tmpRebuildList VALUES (@intRebuildItemId, @intRebuildCategoryId) 
+END 
+
 -- Create the variables used by fnGetItemGLAccount
 DECLARE @AccountCategory_Inventory AS NVARCHAR(30) = 'Inventory'
 DECLARE @AccountCategory_Auto_Variance AS NVARCHAR(30) = 'Inventory Adjustment' 
@@ -55,14 +69,14 @@ FROM	(
 					t.intItemId, t.intItemLocationId, t.intTransactionTypeId
 			FROM	dbo.tblICInventoryTransaction t INNER JOIN tblICItem i
 						ON t.intItemId = i.intItemId 
-			WHERE	t.strBatchId = @strBatchId
-					AND t.intItemId = ISNULL(@intRebuildItemId, t.intItemId) 
-					AND t.strTransactionId = ISNULL(@strRebuildTransactionId, t.strTransactionId)
-					AND ISNULL(i.intCategoryId, 0) = COALESCE(@intRebuildCategoryId, i.intCategoryId, 0) 
-					AND i.strType <> 'Non-Inventory'
+					INNER JOIN #tmpRebuildList list	
+						ON i.intItemId = COALESCE(list.intItemId, i.intItemId)
+						AND i.intCategoryId = COALESCE(list.intCategoryId, i.intCategoryId)
+			WHERE	t.strBatchId = @strBatchId					
+					AND t.strTransactionId = ISNULL(@strRebuildTransactionId, t.strTransactionId)					
+					--AND i.strType <> 'Non-Inventory'
 					AND (dbo.fnDateEquals(t.dtmDate, @dtmRebuildDate) = 1 OR @dtmRebuildDate IS NULL)
-					AND t.intTransactionTypeId = @InventoryTransactionTypeId_Auto_Variance_On_Sold_Or_Used_Stock
-
+					AND t.intTransactionTypeId IN (@InventoryTransactionTypeId_Auto_Variance_On_Sold_Or_Used_Stock, @InventoryTransactionTypeId_AutoNegative)
 		) Query
 
 -- Validate the GL Accounts
@@ -147,13 +161,14 @@ BEGIN
 							ON t.intTransactionTypeId = TransType.intTransactionTypeId
 						INNER JOIN tblICItem i
 							ON i.intItemId = t.intItemId
+						INNER JOIN #tmpRebuildList list	
+							ON i.intItemId = COALESCE(list.intItemId, i.intItemId)
+							AND i.intCategoryId = COALESCE(list.intCategoryId, i.intCategoryId)
 				WHERE	t.strBatchId = @strBatchId
-						AND TransType.intTransactionTypeId IN (@InventoryTransactionTypeId_Auto_Variance_On_Sold_Or_Used_Stock)
-						AND t.intItemId = ISNULL(@intRebuildItemId, t.intItemId) 
+						AND TransType.intTransactionTypeId IN (@InventoryTransactionTypeId_Auto_Variance_On_Sold_Or_Used_Stock, @InventoryTransactionTypeId_AutoNegative)						
 						AND t.intItemId = Item.intItemId
 						AND t.dblQty * t.dblCost + t.dblValue <> 0
 						AND t.strTransactionId = ISNULL(@strRebuildTransactionId, t.strTransactionId)
-						AND ISNULL(i.intCategoryId, 0) = COALESCE(@intRebuildCategoryId, i.intCategoryId, 0) 
 						AND (dbo.fnDateEquals(t.dtmDate, @dtmRebuildDate) = 1 OR @dtmRebuildDate IS NULL) 
 			)
 
@@ -206,18 +221,19 @@ FROM	dbo.tblICInventoryTransaction t INNER JOIN dbo.tblICInventoryTransactionTyp
 			ON t.intTransactionTypeId = TransType.intTransactionTypeId
 		INNER JOIN tblICItem i
 			ON i.intItemId = t.intItemId 
+		INNER JOIN #tmpRebuildList list	
+			ON i.intItemId = COALESCE(list.intItemId, i.intItemId)
+			AND i.intCategoryId = COALESCE(list.intCategoryId, i.intCategoryId)
 		INNER JOIN @GLAccounts GLAccounts
 			ON t.intItemId = GLAccounts.intItemId
 			AND t.intItemLocationId = GLAccounts.intItemLocationId
 			AND t.intTransactionTypeId = GLAccounts.intTransactionTypeId
 		INNER JOIN dbo.tblGLAccount
 			ON tblGLAccount.intAccountId = GLAccounts.intInventoryId
-WHERE	t.strBatchId = @strBatchId
-		AND t.intItemId = ISNULL(@intRebuildItemId, t.intItemId) 
+WHERE	t.strBatchId = @strBatchId		
 		AND t.strTransactionId = ISNULL(@strRebuildTransactionId, t.strTransactionId)
-		AND ISNULL(i.intCategoryId, 0) = COALESCE(@intRebuildCategoryId, i.intCategoryId, 0) 
 		AND (dbo.fnDateEquals(t.dtmDate, @dtmRebuildDate) = 1 OR @dtmRebuildDate IS NULL) 
-		AND t.intTransactionTypeId = @InventoryTransactionTypeId_Auto_Variance_On_Sold_Or_Used_Stock
+		AND t.intTransactionTypeId IN (@InventoryTransactionTypeId_Auto_Variance_On_Sold_Or_Used_Stock, @InventoryTransactionTypeId_AutoNegative)
 ;
 
 -- Get the functional currency
@@ -278,15 +294,16 @@ AS
 				ON t.intTransactionTypeId = TransType.intTransactionTypeId
 			INNER JOIN tblICItem i
 				ON i.intItemId = t.intItemId
+			INNER JOIN #tmpRebuildList list	
+				ON i.intItemId = COALESCE(list.intItemId, i.intItemId)
+				AND i.intCategoryId = COALESCE(list.intCategoryId, i.intCategoryId)
 			LEFT JOIN tblSMCurrencyExchangeRateType currencyRateType
 				ON currencyRateType.intCurrencyExchangeRateTypeId = t.intForexRateTypeId
 	WHERE	t.strBatchId = @strBatchId
-			AND t.intItemId = ISNULL(@intRebuildItemId, t.intItemId) 
 			AND t.strTransactionId = ISNULL(@strRebuildTransactionId, t.strTransactionId)
 			AND t.intInTransitSourceLocationId IS NULL -- If there is a value in intInTransitSourceLocationId, then it is for In-Transit costing. Use uspICCreateGLEntriesForInTransitCosting instead of this sp. 
-			AND ISNULL(i.intCategoryId, 0) = COALESCE(@intRebuildCategoryId, i.intCategoryId, 0) 
 			AND (dbo.fnDateEquals(t.dtmDate, @dtmRebuildDate) = 1 OR @dtmRebuildDate IS NULL) 
-			AND t.intTransactionTypeId = @InventoryTransactionTypeId_Auto_Variance_On_Sold_Or_Used_Stock
+			AND t.intTransactionTypeId IN (@InventoryTransactionTypeId_Auto_Variance_On_Sold_Or_Used_Stock, @InventoryTransactionTypeId_AutoNegative)
 )
 -----------------------------------------------------------------------------------
 -- This part is for the Inventory Auto Variance on Negatively Sold or Used Stock
@@ -358,7 +375,7 @@ FROM	ForGLEntries_CTE
 			dbo.fnMultiply(ISNULL(dblQty, 0), ISNULL(dblUOMQty, 1)) 
 		) CreditUnit 
 
-WHERE	ForGLEntries_CTE.intTransactionTypeId = @InventoryTransactionTypeId_Auto_Variance_On_Sold_Or_Used_Stock
+WHERE	ForGLEntries_CTE.intTransactionTypeId IN (@InventoryTransactionTypeId_Auto_Variance_On_Sold_Or_Used_Stock, @InventoryTransactionTypeId_AutoNegative) 
 		AND ROUND(ISNULL(dblQty, 0) * ISNULL(dblCost, 0) + ISNULL(dblValue, 0), 2) <> 0 
 
 UNION ALL 
@@ -429,6 +446,6 @@ FROM	ForGLEntries_CTE
 			dbo.fnMultiply(ISNULL(dblQty, 0), ISNULL(dblUOMQty, 1)) 
 		) CreditUnit 
 
-WHERE	ForGLEntries_CTE.intTransactionTypeId  = @InventoryTransactionTypeId_Auto_Variance_On_Sold_Or_Used_Stock
+WHERE	ForGLEntries_CTE.intTransactionTypeId IN (@InventoryTransactionTypeId_Auto_Variance_On_Sold_Or_Used_Stock, @InventoryTransactionTypeId_AutoNegative)
 		AND ROUND(ISNULL(dblQty, 0) * ISNULL(dblCost, 0) + ISNULL(dblValue, 0), 2) <> 0 
 ;
