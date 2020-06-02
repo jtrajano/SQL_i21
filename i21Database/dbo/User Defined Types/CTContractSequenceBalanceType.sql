5f0f5c0aa9d5289d450a03840c9dabd1c57e7e92
jayson.ramos <jayson.ramos@irely.com>
2020-06-02 14:25:43 +0800

    CT-4808

---
+++ i21Database/dbo/User Defined Types/CTContractSequenceBalanceType.sql
@@ -0,0 +1,10 @@
+﻿CREATE TYPE [dbo].[CTContractSequenceBalanceType] AS TABLE (
+	intId INT IDENTITY PRIMARY KEY CLUSTERED
+	, intExternalId int NOT NULL										--> Shipment Item Id
+	, intContractDetailId int NOT NULL									--> Contract Detail Id
+	, dblOldQuantity NUMERIC(24, 10) NOT NULL DEFAULT((0))				--> Shipment quantity (NOTE: If reposting DWG, this should be the previous DWG quantity)
+	, dblQuantity NUMERIC(24, 10) NOT NULL DEFAULT((0))					--> DWG quantity
+	, intItemUOMId int NOT NULL											--> Shipment Item Item UOM Id
+	, strScreenName NVARCHAR (50) COLLATE Latin1_General_CI_AS NULL		--> 'Inventory'
+	, intUserId INT NULL												--> Current User Id
+)
