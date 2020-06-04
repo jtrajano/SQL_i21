8342ea44c90c690ac43e44c4178c18e4899aeade
Wendell Wayne Estrada <wendell.estrada@irely.com>
2020-06-04 11:00:44 +0800

    Revert "[B] IC-8532"
    This reverts commit 68dc66186f2ecc66da980f5e109e00edd44e1c99.

--- i21Database/dbo/Stored Procedures/uspICPopulateShipmentInvoice.sql
+++ i21Database/dbo/Stored Procedures/uspICPopulateShipmentInvoice.sql
@@ -18,15 +18,17 @@ END
 BEGIN 
 	INSERT INTO [tblICSearchShipmentInvoice] (
 		intInventoryShipmentId
+		,intInventoryShipmentItemId
+		,intInventoryShipmentChargeId
 		,strShipmentNumber
 		,dtmShipDate
 		,strCustomer
 		,strLocationName
+		,strDestination
 		,strBOLNumber
 		,strOrderType
 		,strItemNo
 		,strItemDescription
-		,strDestination
 		,dblUnitCost
 		,dblShipmentQty
 		,dblInTransitQty
@@ -34,34 +36,54 @@ BEGIN
 		,dblShipmentLineTotal
 		,dblInTransitTotal
 		,dblInvoiceLineTotal
+		,dblShipmentTax
+		,dblInvoiceTax
 		,dblOpenQty
+		,dblItemsReceivable
+		,dblTaxesReceivable
 		,dtmLastInvoiceDate
 		,strAllVouchers
+		,strFilterString
 		,dtmCreated
+		,intCurrencyId
+		,strCurrency
+		,strItemUOM
+		,intItemUOMId			
 	)
 	-- Insert the items: 
 	SELECT	
 			shipmentItem.intInventoryShipmentId
+			,shipmentItem.intInventoryShipmentItemId
+			,intInventoryShipmentChargeId = NULL 
 			,shipmentItem.strShipmentNumber
 			,shipmentItem.dtmShipDate
 			,strCustomer = customer.strCustomerNumber + ' ' + entity.strName
 			,shipmentItem.strLocationName
+			,shipmentItem.strDestination
 			,shipmentItem.strBOLNumber
 			,shipmentItem.strOrderType
 			,shipmentItem.strItemNo
 			,shipmentItem.strItemDescription
-			,shipmentItem.strDestination
-			,MAX(shipmentItem.dblShipmentCost) dblUnitCost
-			,SUM(shipmentItem.dblShipmentQty) dblShipmentQty
-			,SUM(shipmentItem.dblInTransitQty) dblInTransitQty
-			,SUM(shipmentItem.dblInvoiceQty) dblInvoiceQty
-			,dblShipmentLineTotal = SUM(shipmentItem.dblShipmentItemTotal)
-			,dblInTransitTotal = SUM(shipmentItem.dblItemsReceivable)
-			,dblInvoiceLineTotal = SUM(shipmentItem.dblInvoiceItemTotal)
-			,dblOpenQty = SUM(ISNULL(shipmentItem.dblShipmentQty, 0)) - SUM(ISNULL(shipmentItem.dblInvoiceQty, 0)) --shipmentItem.dblInTransitQty
-			,MAX(shipmentItem.dtmLastInvoiceDate) dtmLastInvoiceDate
-			,strAllVouchers = invoices.strInvoiceNumbers
-			,dtmCreated = @dtmCreated	
+			,dblUnitCost = shipmentItem.dblShipmentCost
+			,shipmentItem.dblShipmentQty
+			,shipmentItem.dblInTransitQty
+			,shipmentItem.dblInvoiceQty
+			,dblShipmentLineTotal = shipmentItem.dblShipmentItemTotal
+			,dblInTransitTotal = shipmentItem.dblItemsReceivable
+			,dblInvoiceLineTotal = shipmentItem.dblInvoiceItemTotal
+			,dblShipmentTax = 0 
+			,dblInvoiceTax = 0 
+			,dblOpenQty = ISNULL(shipmentItem.dblShipmentQty, 0) - ISNULL(shipmentItem.dblInvoiceQty, 0) --shipmentItem.dblInTransitQty
+			,dblItemsReceivable = shipmentItem.dblItemsReceivable
+			,dblTaxesReceivable = 0 
+			,shipmentItem.dtmLastInvoiceDate
+			,shipmentItem.strAllVouchers
+			,shipmentItem.strFilterString
+			,dtmCreated = @dtmCreated
+			,shipmentItem.intCurrencyId
+			,shipmentItem.strCurrency
+			,shipmentItem.strItemUOM
+			,shipmentItem.intItemUOMId	
 	FROM	tblARCustomer customer INNER JOIN tblEMEntity entity
 				ON entity.intEntityId = customer.intEntityId
 			CROSS APPLY (
@@ -69,34 +91,22 @@ BEGIN
 				FROM	vyuICGetInventoryShipmentInvoiceItems items
 				WHERE	items.intEntityCustomerId = customer.intEntityId
 			) shipmentItem
-			OUTER APPLY (
-				SELECT dbo.fnICGetConcatenatedInvoiceNumbersByCustomer(shipmentItem.intEntityCustomerId, shipmentItem.intInventoryShipmentId) as strInvoiceNumbers
-			) invoices
-	GROUP BY shipmentItem.intInventoryShipmentId
-			,shipmentItem.strShipmentNumber
-			,shipmentItem.dtmShipDate
-			,shipmentItem.strLocationName
-			,shipmentItem.strBOLNumber
-			,shipmentItem.strOrderType
-			,shipmentItem.strItemNo
-			,shipmentItem.strItemDescription
-			,shipmentItem.strDestination
-			,customer.strCustomerNumber
-			,entity.strName
-			,invoices.strInvoiceNumbers
 
+	-- Insert the price down charges (against the receipt vendor)
 	UNION ALL 
 	SELECT	
-			 shipmentCharge.intInventoryShipmentId
+			shipmentCharge.intInventoryShipmentId
+			,shipmentCharge.intInventoryShipmentChargeId
+			,intInventoryShipmentChargeId = NULL 
 			,shipmentCharge.strShipmentNumber
 			,shipmentCharge.dtmShipDate
 			,strCustomer = customer.strCustomerNumber + ' ' + entity.strName
 			,shipmentCharge.strLocationName
+			,shipmentCharge.strDestination
 			,shipmentCharge.strBOLNumber
 			,shipmentCharge.strOrderType
 			,shipmentCharge.strItemNo
 			,shipmentCharge.strItemDescription
-			,shipmentCharge.strDestination
 			,dblUnitCost = shipmentCharge.dblShipmentCost
 			,shipmentCharge.dblShipmentQty
 			,shipmentCharge.dblInTransitQty
@@ -104,10 +114,19 @@ BEGIN
 			,dblShipmentLineTotal = shipmentCharge.dblShipmentChargeTotal
 			,dblInTransitTotal = 0
 			,dblInvoiceLineTotal = shipmentCharge.dblInvoiceItemTotal
+			,dblShipmentTax = 0 
+			,dblInvoiceTax = 0 
 			,dblOpenQty = 0
+			,dblItemsReceivable = shipmentCharge.dblItemsReceivable
+			,dblTaxesReceivable = 0 
 			,shipmentCharge.dtmLastInvoiceDate
 			,shipmentCharge.strAllVouchers
+			,shipmentCharge.strFilterString
 			,dtmCreated = @dtmCreated
+			,shipmentCharge.intCurrencyId
+			,shipmentCharge.strCurrency
+			,strItemUOM = NULL 
+			,intItemUOMId = NULL 
 	FROM	tblARCustomer customer INNER JOIN tblEMEntity entity
 				ON entity.intEntityId = customer.intEntityId
 			CROSS APPLY (
@@ -115,5 +134,4 @@ BEGIN
 				FROM	vyuICGetInventoryShipmentInvoicePriceCharges items
 				WHERE	items.intEntityCustomerId = customer.intEntityId
 			) shipmentCharge
-
 END 
