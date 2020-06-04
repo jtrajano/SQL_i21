76bfc02cc3811d75ab9debcc17b0c505e5009c87
Arman Arenas <arman.arenas@irely.com>
2020-06-04 19:43:27 +0800

    LG-2319 additional fields for DeliveryOrder4; optimized uspLGGetDeliveryOrderReport

--- i21Database/dbo/Stored Procedures/uspLGGetDeliveryOrderReport.sql
+++ i21Database/dbo/Stored Procedures/uspLGGetDeliveryOrderReport.sql
@@ -1,5 +1,5 @@
 ﻿CREATE PROCEDURE uspLGGetDeliveryOrderReport
-			@xmlParam NVARCHAR(MAX) = NULL
+	@xmlParam NVARCHAR(MAX) = NULL
 AS
 BEGIN
 	DECLARE @strLoadNumber			NVARCHAR(MAX),
@@ -13,6 +13,8 @@ BEGIN
 			@strZip					NVARCHAR(12),
 			@strCountry				NVARCHAR(25),
 			@strPhone				NVARCHAR(50),
+			@strFax					NVARCHAR(50),
+			@strWeb					NVARCHAR(200),
 			@strHaulerAddress		NVARCHAR(MAX), 
 			@strHaulerCity			NVARCHAR(MAX), 
 			@strHaulerCountry		NVARCHAR(MAX), 
@@ -63,6 +65,8 @@ BEGIN
 		,@strZip = strZip
 		,@strCountry = strCountry
 		,@strPhone = strPhone
+		,@strFax = strFax
+		,@strWeb = strWebSite
 	FROM tblSMCompanySetup
 
 	SELECT
@@ -76,144 +80,57 @@ BEGIN
 	SELECT DISTINCT L.strLoadNumber
 		  ,L.intLoadId
 		  ,LW.strDeliveryNoticeNumber
-		  ,ISNULL(WHVendor.strName,CLSL.strSubLocationName) AS strSubLocationName
+		  ,strSubLocationName = ISNULL(WHVendor.strName,CLSL.strSubLocationName)
 		  ,ContactEntity.strEmail
 		  ,ContactEntity.strPhone
 		  ,ContactEntity.strInternalNotes
-		  ,CLSL.strAddress AS strSubLocationAddress
-		  ,CLSL.strCity AS strSubLocationCity
-		  ,CLSL.strState AS strSubLocationState
-		  ,CLSL.strZipCode AS strSubLocationZip
-		  ,CLSL.strCity + ', ' + CLSL.strState + ', ' + CLSL.strZipCode AS strSubLocationCityStateZip
-		  ,EM.strName AS strCustomer
-		  ,CASE 
-			WHEN ISNULL(EL.strAddress, '') = ''
-				THEN (
-						SELECT EL1.strAddress
-						FROM tblEMEntityLocation EL1
-						JOIN tblEMEntity EM1 ON EL1.intEntityId = EM.intEntityId
-						WHERE EL1.ysnDefaultLocation = 1
-							AND EM.intEntityId = EM1.intEntityId
-						)
-			ELSE EL.strAddress
-			END AS strCustomerLocationAddress
-		  ,CASE 
-			WHEN ISNULL(EL.strCity, '') = ''
-				THEN (
-						SELECT EL1.strCity
-						FROM tblEMEntityLocation EL1
-						JOIN tblEMEntity EM1 ON EL1.intEntityId = EM.intEntityId
-						WHERE EL1.ysnDefaultLocation = 1
-							AND EM.intEntityId = EM1.intEntityId
-						)
-			ELSE EL.strCity
-			END AS strCustomerLocationCity
-		  ,CASE 
-			WHEN ISNULL(EL.strState, '') = ''
-				THEN (
-						SELECT EL1.strState
-						FROM tblEMEntityLocation EL1
-						JOIN tblEMEntity EM1 ON EL1.intEntityId = EM.intEntityId
-						WHERE EL1.ysnDefaultLocation = 1
-							AND EM.intEntityId = EM1.intEntityId
-						)
-			ELSE EL.strState
-			END AS strCustomerLocationState
-		  ,CASE 
-			WHEN ISNULL(EL.strZipCode, '') = ''
-				THEN (
-						SELECT EL1.strZipCode
-						FROM tblEMEntityLocation EL1
-						JOIN tblEMEntity EM1 ON EL1.intEntityId = EM.intEntityId
-						WHERE EL1.ysnDefaultLocation = 1
-							AND EM.intEntityId = EM1.intEntityId
-						)
-			ELSE EL.strZipCode
-			END AS strCustomerLocationZipCode
-		  ,CASE 
-			WHEN ISNULL(EL.strCity, '') = ''
-				THEN (
-						SELECT EL1.strCity
-						FROM tblEMEntityLocation EL1
-						JOIN tblEMEntity EM1 ON EL1.intEntityId = EM.intEntityId
-						WHERE EL1.ysnDefaultLocation = 1
-							AND EM.intEntityId = EM1.intEntityId
-						)
-			ELSE EL.strCity
-			END + ', ' + CASE 
-			WHEN ISNULL(EL.strState, '') = ''
-				THEN (
-						SELECT EL1.strState
-						FROM tblEMEntityLocation EL1
-						JOIN tblEMEntity EM1 ON EL1.intEntityId = EM.intEntityId
-						WHERE EL1.ysnDefaultLocation = 1
-							AND EM.intEntityId = EM1.intEntityId
-						)
-			ELSE EL.strState
-			END + ', ' + CASE 
-			WHEN ISNULL(EL.strZipCode, '') = ''
-				THEN (
-						SELECT EL1.strZipCode
-						FROM tblEMEntityLocation EL1
-						JOIN tblEMEntity EM1 ON EL1.intEntityId = EM.intEntityId
-						WHERE EL1.ysnDefaultLocation = 1
-							AND EM.intEntityId = EM1.intEntityId
-						)
-			ELSE EL.strZipCode
-			END AS strCustomerLocationCityStateZip
-		  ,(SELECT AC.strFLOId
-			FROM tblARCustomer AC
-			WHERE AC.[intEntityId]= EM.intEntityId
-			) strFLOId		  
-		  ,(SELECT ETCN.strName
-			FROM tblEMEntity EM1 
-			JOIN tblEMEntityToContact ETC ON ETC.intEntityId = EM1.intEntityId
-			JOIN tblEMEntity ETCN ON ETCN.intEntityId = ETC.intEntityContactId
-			WHERE EM.intEntityId = EM1.intEntityId AND ETC.ysnDefaultContact = 1
-			)strContactName
+		  ,strSubLocationAddress = CLSL.strAddress
+		  ,strSubLocationCity = CLSL.strCity
+		  ,strSubLocationState = CLSL.strState
+		  ,strSubLocationZip = CLSL.strZipCode
+		  ,strSubLocationCityStateZip = CLSL.strCity + ', ' + CLSL.strState + ', ' + CLSL.strZipCode
+		  ,strCustomer = EM.strName
+		  ,strCustomerLocationAddress = ISNULL(EL.strAddress, EDL.strAddress)
+		  ,strCustomerLocationCity = ISNULL(EL.strCity, EDL.strCity)
+		  ,strCustomerLocationState = ISNULL(EL.strState, EDL.strState) 
+		  ,strCustomerLocationZipCode = ISNULL(EL.strZipCode, EDL.strZipCode)
+		  ,strCustomerLocationCityStateZip = COALESCE(EL.strCity, EDL.strCity, '') + ', ' + COALESCE(EL.strState, EDL.strState, '') + ', ' + COALESCE(EL.strZipCode, EDL.strZipCode, '') 
+		  ,strFLOId = (SELECT AC.strFLOId FROM tblARCustomer AC WHERE AC.[intEntityId]= EM.intEntityId) 		  
+		  ,strContactName = EDC.strName
 		  ,L.dtmScheduledDate
 		  ,LW.dtmPickupDate
 		  ,LW.dtmLastFreeDate
-		  ,LW.dtmDeliveryDate AS dtmDODate
-		  ,(SELECT TOP 1 CH.strCustomerContract
-			FROM tblCTContractDetail CD
-			JOIN tblCTContractHeader CH ON CH.intContractHeaderId = CD.intContractHeaderId
-			WHERE CD.intContractDetailId = LD.intSContractDetailId
-			) AS strBuyersPONo
-		  ,(SELECT TOP 1 CH.strContractNumber + ' / ' + LTRIM(CD.intContractSeq)
-			FROM tblCTContractDetail CD
-			JOIN tblCTContractHeader CH ON CH.intContractHeaderId = CD.intContractHeaderId
-			WHERE CD.intContractDetailId = LD.intSContractDetailId
-			) AS strSalesContractNo
-		  ,(SELECT TOP 1 CB.strContractBasis
-			FROM tblCTContractHeader CH
-			JOIN tblCTContractDetail CD ON CD.intContractHeaderId = CH.intContractHeaderId
-			JOIN tblSMFreightTerms CB ON CB.intFreightTermId = CH.intFreightTermId
-			WHERE CD.intContractDetailId = LD.intSContractDetailId
-			) AS strSalesContractIncoTerm
-		  ,(SELECT E1.strPhone
-			FROM tblEMEntity E
-			JOIN tblEMEntityToContact EC ON EC.intEntityId = E.intEntityId
-			JOIN tblEMEntity E1 ON E1.intEntityId = EC.intEntityContactId
-			WHERE E.intEntityId = EM.intEntityId AND EC.ysnDefaultContact = 1
-			) strCustomerContactNumber
-		  ,@strCompanyName AS strCompanyName
-		  ,@strCompanyAddress AS strCompanyAddress
-		  ,@strContactName AS strCompanyContactName 
-		  ,@strCounty AS strCompanyCounty 
-		  ,@strCity AS strCompanyCity 
-		  ,@strState AS strCompanyState 
-		  ,@strZip AS strCompanyZip 
-		  ,@strCountry AS strCompanyCountry 
-		  ,@strPhone AS strCompanyPhone
-		  ,@strCity + ', ' + @strState + ', ' + @strZip + ',' AS strCityStateZip
-		  ,HEM.strName AS strShipVia
+		  ,LW.dtmCustomsEntrySent
+		  ,LW.dtmSampleAuthorizedDate
+		  ,dtmApprovalDate = SMPL.dtmTestingEndDate
+		  ,dtmFreeTimeExpires = DATEADD(DD, ISNULL(SCH.intClaimValidTill, 0), LW.dtmDeliveryDate)
+		  ,dtmDODate = LW.dtmDeliveryDate
+		  ,strBuyersPONo = SCH.strCustomerContract
+		  ,strSalesContractNo = SCH.strContractNumber + ' / ' + LTRIM(SCH.intContractSeq)
+		  ,strSalesContractNoDashSeq = SCH.strContractNumber + '-' + LTRIM(SCH.intContractSeq)
+		  ,strSalesContractIncoTerm = SCH.strContractBasis
+		  ,strSalesContractWeightTerms = SCH.strWeightGradeDesc
+		  ,strCustomerContactNumber = EDC.strPhone
+		  ,strCompanyName = @strCompanyName
+		  ,strCompanyAddress = @strCompanyAddress
+		  ,strCompanyContactName = @strContactName 
+		  ,strCompanyCounty = @strCounty 
+		  ,strCompanyCity = @strCity 
+		  ,strCompanyState = @strState 
+		  ,strCompanyZip = @strZip 
+		  ,strCompanyCountry = @strCountry 
+		  ,strCompanyPhone = @strPhone
+		  ,strCompanyFax = @strFax
+		  ,strCompanyWebSite = @strWeb
+		  ,strCityStateZip = @strCity + ', ' + @strState + ', ' + @strZip + ','
+		  ,strShipVia = HEM.strName 
 		  ,L.strComments
-		  ,dbo.fnSMGetCompanyLogo('FullHeaderLogo') AS blbFullHeaderLogo
-		  ,dbo.fnSMGetCompanyLogo('FullFooterLogo') AS blbFullFooterLogo
-		  ,CASE WHEN ISNULL(CP.ysnFullHeaderLogo,0) = 1 THEN 'true' else 'false' END ysnFullHeaderLogo
-		  ,ISNULL(CP.intReportLogoHeight,0) AS intReportLogoHeight
-		  ,ISNULL(CP.intReportLogoWidth,0) AS intReportLogoWidth		  
+		  ,strOutboundInstructions = SCH.strInstructions
+		  ,blbFullHeaderLogo = dbo.fnSMGetCompanyLogo('FullHeaderLogo')
+		  ,blbFullFooterLogo = dbo.fnSMGetCompanyLogo('FullFooterLogo')
+		  ,ysnFullHeaderLogo = CASE WHEN ISNULL(CP.ysnFullHeaderLogo,0) = 1 THEN 'true' ELSE 'false' END
+		  ,intReportLogoHeight = ISNULL(CP.intReportLogoHeight,0)
+		  ,intReportLogoWidth = ISNULL(CP.intReportLogoWidth,0) 
 	FROM tblLGLoad L
 	JOIN tblLGLoadDetail LD ON L.intLoadId = LD.intLoadId
 	JOIN tblLGLoadWarehouse LW ON LW.intLoadId = L.intLoadId
@@ -224,6 +141,38 @@ BEGIN
 	LEFT JOIN tblEMEntity EM ON EM.intEntityId = LD.intCustomerEntityId
 	LEFT JOIN tblEMEntityLocation EL ON EL.intEntityLocationId = LD.intCustomerEntityLocationId
 	LEFT JOIN tblEMEntity HEM ON HEM.intEntityId = LW.intHaulerEntityId
+	OUTER APPLY (SELECT TOP 1 
+						CH.strCustomerContract
+						,CH.strContractNumber
+						,CD.intContractSeq
+						,CD.intContractDetailId
+						,CB.strContractBasis
+						,CB.strInstructions
+						,strAssociation = ASN.strName
+						,ASN.intClaimValidTill 
+						,WG.strWeightGradeDesc
+					FROM tblCTContractDetail CD
+					JOIN tblCTContractHeader CH ON CH.intContractHeaderId = CD.intContractHeaderId
+					LEFT JOIN tblCTWeightGrade WG ON WG.intWeightGradeId = CH.intWeightId
+					LEFT JOIN tblCTAssociation ASN ON ASN.intAssociationId = CH.intAssociationId
+					LEFT JOIN tblSMFreightTerms CB ON CB.intFreightTermId = CH.intFreightTermId
+					WHERE CD.intContractDetailId = LD.intSContractDetailId) SCH
+	OUTER APPLY (SELECT TOP 1 EL1.strAddress, EL1.strCity, EL1.strState, EL1.strZipCode
+					FROM tblEMEntityLocation EL1
+					JOIN tblEMEntity EM1 ON EL1.intEntityId = EM.intEntityId
+					WHERE EL1.ysnDefaultLocation = 1 AND EM.intEntityId = EM1.intEntityId) EDL
+	OUTER APPLY (SELECT ETCN.strName, ETCN.strPhone
+					FROM tblEMEntity EM1 
+					JOIN tblEMEntityToContact ETC ON ETC.intEntityId = EM1.intEntityId
+					JOIN tblEMEntity ETCN ON ETCN.intEntityId = ETC.intEntityContactId
+					WHERE EM.intEntityId = EM1.intEntityId AND ETC.ysnDefaultContact = 1) EDC
+	OUTER APPLY (SELECT TOP 1 SMP.strSampleNumber, SMP.intSampleStatusId, SMP.dtmTestingEndDate
+					FROM tblQMSample SMP 
+					INNER JOIN tblQMSampleType SMPT ON SMPT.intSampleTypeId = SMP.intSampleTypeId
+					LEFT JOIN tblQMSampleStatus SMPS ON SMPS.intSampleStatusId = SMP.intSampleStatusId 
+					WHERE SMP.intContractDetailId = SCH.intContractDetailId 
+						AND SMPS.strStatus = 'Approved'
+						AND ISNULL(SMPT.ysnFinalApproval, 0) = 1) SMPL
 	CROSS APPLY tblLGCompanyPreference CP
 	WHERE strLoadNumber = @strLoadNumber
 END
