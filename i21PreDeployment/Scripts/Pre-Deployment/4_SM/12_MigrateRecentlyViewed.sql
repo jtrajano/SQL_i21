6226d95f68e015f20e2a15711460acadab6e3478
Von Pasion <von.pasion@irely.com>
2020-06-04 19:18:47 +0800

    FRM-7918 (Recently Viewed)

---
+++ i21PreDeployment/Scripts/Pre-Deployment/4_SM/12_MigrateRecentlyViewed.sql
@@ -0,0 +1,19 @@
+﻿IF NOT EXISTS(SELECT TOP 1 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE UPPER(TABLE_NAME) = 'TBLSMRECENTLYVIEWED' AND UPPER(COLUMN_NAME) = 'INTLOGID')
+BEGIN
+	ALTER TABLE tblSMLog ADD intRecentlyViewedId INT NULL
+	ALTER TABLE tblSMRecentlyViewed ADD intLogId INT NULL
+
+	EXEC('
+			INSERT INTO tblSMLog (strType, dtmDate, intEntityId, strRoute, intConcurrencyId, intRecentlyViewedId)
+			SELECT ''Recently Viewed'', dtmDateEntered, intEntityId, strRoute, 1, intRecentlyViewedId from tblSMRecentlyViewed;
+	')
+
+	EXEC('
+			UPDATE tblSMRecentlyViewed
+			SET intLogId = B.intLogId
+			FROM tblSMLog B
+			WHERE tblSMRecentlyViewed.intRecentlyViewedId = B.intRecentlyViewedId;		
+	')
+
+	ALTER TABLE tblSMLog DROP COLUMN intRecentlyViewedId
+END
