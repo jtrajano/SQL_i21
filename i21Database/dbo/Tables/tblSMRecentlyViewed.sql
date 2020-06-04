9cf6b7adac152e9fa6e4dd66843f98f2c8a9468a
Von Pasion <von.pasion@irely.com>
2020-06-04 19:18:47 +0800

    FRM-7918

--- i21Database/dbo/Tables/tblSMRecentlyViewed.sql
+++ i21Database/dbo/Tables/tblSMRecentlyViewed.sql
@@ -1,13 +1,14 @@
 ﻿CREATE TABLE [dbo].[tblSMRecentlyViewed]
 (
 	[intRecentlyViewedId] INT IDENTITY (1, 1) NOT NULL, 
-    [intEntityId] INT NOT NULL, 
+    [intLogId] INT NOT NULL, 
+    --[intEntityId] INT NOT NULL, 
     [strTitle] NVARCHAR(255) COLLATE Latin1_General_CI_AS NOT NULL, 
-    [strRoute] NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NOT NULL, 
-    [dtmDateEntered] DATETIME NOT NULL, 
-    [dtmDateModified] DATETIME NULL, 
+    --[strRoute] NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NOT NULL, 
+    --[dtmDateEntered] DATETIME NOT NULL, 
+    --[dtmDateModified] DATETIME NULL, 
 	[strAction] NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL, 
     [intConcurrencyId] INT NOT NULL DEFAULT 0, 
     CONSTRAINT [PK_tblSMRecentlyViewed] PRIMARY KEY Clustered ([intRecentlyViewedId] ASC), 
-    CONSTRAINT [FK_tblSMRecentlyViewed_tblEMEntity_intEntityId] FOREIGN KEY ([intEntityId]) REFERENCES tblEMEntity([intEntityId]) 
+    --CONSTRAINT [FK_tblSMRecentlyViewed_tblEMEntity_intEntityId] FOREIGN KEY ([intEntityId]) REFERENCES tblEMEntity([intEntityId]) 
 )
