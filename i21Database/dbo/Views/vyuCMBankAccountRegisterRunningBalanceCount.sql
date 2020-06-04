8a7f3ff58f4277385d9ccca83c67584c27df3844
Jeffrey Trajano <jlt212000@yahoo.com>
2020-06-04 18:31:00 +0800

    GL-7508 CLONE - Accounting Period on Transaction screens (GL/CM)

--- i21Database/dbo/Views/vyuCMBankAccountRegisterRunningBalanceCount.sql
+++ i21Database/dbo/Views/vyuCMBankAccountRegisterRunningBalanceCount.sql
@@ -2,33 +2,31 @@ CREATE VIEW [dbo].[vyuCMBankAccountRegisterRunningBalanceCount]
 AS
 
 select
-CM.intTransactionId,
+intTransactionId,
 dtmDate, 
 intBankAccountId,
-CM.dtmDateReconciled,
-CM.intBankTransactionTypeId,
-CM.intCompanyLocationId,
-T.strBankTransactionTypeName,
-L.strLocationName,
+dtmDateReconciled,
+intBankTransactionTypeId,
+intCompanyLocationId,
+strBankTransactionTypeName,
+strLocationName,
 strMemo = 
-	CASE WHEN CM.strMemo = '' AND CM.ysnCheckVoid = 1 THEN 'Void' 
-	ELSE ISNULL(CM.strMemo, '') END,
+	CASE WHEN strMemo = '' AND ysnCheckVoid = 1 THEN 'Void' 
+	ELSE ISNULL(strMemo, '') END,
 strPayee = 
-	CASE WHEN Employee.ysnMaskEmployeeName = 1 AND CM.intBankTransactionTypeId IN ( 21, 23 ) THEN '(restricted information)' 
-	ELSE ISNULL(CM.strPayee, '') END,
+	CASE WHEN Employee.ysnMaskEmployeeName = 1 AND intBankTransactionTypeId IN ( 21, 23 ) THEN '(restricted information)' 
+	ELSE ISNULL(strPayee, '') END,
 dblPayment = 
-	CASE WHEN CM.intBankTransactionTypeId IN ( 3, 9, 12, 13, 14, 15, 16, 20, 21, 22, 23 ) THEN CM.dblAmount 
-	WHEN CM.intBankTransactionTypeId IN ( 2, 5, 51 ) AND ISNULL(CM.dblAmount,0) < 0 THEN CM.dblAmount * -1 ELSE 0 END                        , 
-dblDeposit = CASE WHEN CM.intBankTransactionTypeId IN ( 1, 10, 11, 18, 19, 103, 116, 121, 122, 123 ) THEN CM.dblAmount 
-	WHEN CM.intBankTransactionTypeId = 5 AND ISNULL(CM.dblAmount,0) > 0 THEN dblAmount ELSE 0 END, 
-CM.strReferenceNo,
-CM.strTransactionId,
-CM.ysnCheckVoid,
-CM.ysnClr,
-CM.strPeriod
+	CASE WHEN intBankTransactionTypeId IN ( 3, 9, 12, 13, 14, 15, 16, 20, 21, 22, 23 ) THEN dblAmount 
+	WHEN intBankTransactionTypeId IN ( 2, 5, 51 ) AND ISNULL(dblAmount,0) < 0 THEN dblAmount * -1 ELSE 0 END                        , 
+dblDeposit = CASE WHEN intBankTransactionTypeId IN ( 1, 10, 11, 18, 19, 103, 116, 121, 122, 123 ) THEN dblAmount 
+	WHEN intBankTransactionTypeId = 5 AND ISNULL(dblAmount,0) > 0 THEN dblAmount ELSE 0 END, 
+strReferenceNo,
+strTransactionId,
+ysnCheckVoid,
+ysnClr,
+strPeriod
 FROM vyuCMGetBankTransaction CM 
-LEFT JOIN tblCMBankTransactionType T on T.intBankTransactionTypeId = CM.intBankTransactionTypeId
-LEFT JOIN tblSMCompanyLocation L on L.intCompanyLocationId = CM.intCompanyLocationId
 OUTER APPLY 
 (
  
