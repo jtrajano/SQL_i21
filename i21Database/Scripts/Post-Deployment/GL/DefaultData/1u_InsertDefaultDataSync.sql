cf7057273a55a808ad871a089964f0825a327c96
Jeffrey Trajano <jlt212000@yahoo.com>
2020-06-05 18:12:29 +0800

    GL-7508

--- i21Database/Scripts/Post-Deployment/GL/DefaultData/1u_InsertDefaultDataSync.sql
+++ i21Database/Scripts/Post-Deployment/GL/DefaultData/1u_InsertDefaultDataSync.sql
@@ -6,12 +6,10 @@ GO
 		;WITH cte as(
 		    SELECT TOP 1000 intGLDetailId from tblGLDetail WHERE intFiscalPeriodId IS NULL
 		)
-
         UPDATE T SET intFiscalPeriodId = F.intGLFiscalYearPeriodId FROM tblGLDetail T 
 		JOIN cte C ON C.intGLDetailId = T.intGLDetailId
         CROSS APPLY dbo.fnGLGetFiscalPeriod(T.dtmDate) F
         SELECT  @rowUpdated = CONVERT( NVARCHAR(20) , @@ROWCOUNT )
-
 		PRINT ('Updated fiscal period of ' +  @rowUpdated +  ' records in tblGLDetail')
 	 END
 GO
