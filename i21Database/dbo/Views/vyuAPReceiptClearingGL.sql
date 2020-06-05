afc2a50675082182e937f3640fd2c33e7f7d0512
Erick Dela Cruz <erick.delacruz@irely.com>
2020-06-05 18:22:04 +0800

    AP-8251

--- i21Database/dbo/Views/vyuAPReceiptClearingGL.sql
+++ i21Database/dbo/Views/vyuAPReceiptClearingGL.sql
@@ -25,25 +25,27 @@ SELECT DISTINCT * FROM (
  AND t.ysnIsUnposted = 0       
  AND (gd.dblCredit != 0 OR gd.dblDebit != 0)    
  UNION ALL    
- SELECT DISTINCT    
-   ad.strAccountId    
-   ,ad.intAccountId    
-   ,A.strReceiptNumber    
-   ,B.intInventoryReceiptItemId   
-   ,NULL AS intTransactionChargeId 
-   ,A.intInventoryReceiptId    
-   ,B.intItemId    
- FROM tblICInventoryReceipt A    
- INNER JOIN tblICInventoryReceiptItem  B ON A.intInventoryReceiptId = B.intInventoryReceiptId    
- INNER JOIN tblGLDetail C     
-  ON C.strTransactionId = A.strReceiptNumber    
-   AND C.intJournalLineNo = B.intInventoryReceiptItemId    
- INNER JOIN vyuGLAccountDetail ad    
-  ON ad.intAccountId = C.intAccountId    
- WHERE     
-  ad.intAccountCategoryId = 45      
- AND C.ysnIsUnposted = 0       
- AND (C.dblCredit != 0 OR C.dblDebit != 0)   
+ SELECT DISTINCT      
+   ad.strAccountId      
+   ,ad.intAccountId      
+   ,A.strReceiptNumber      
+   ,B.intInventoryReceiptItemId     
+   ,NULL AS intTransactionChargeId   
+   ,A.intInventoryReceiptId      
+   ,B.intItemId      
+ FROM tblICInventoryReceipt A      
+ INNER JOIN tblICInventoryReceiptItem  B ON A.intInventoryReceiptId = B.intInventoryReceiptId   
+ INNER JOIN tblICItem item ON B.intItemId = item.intItemId   
+ INNER JOIN tblGLDetail C       
+  ON C.strTransactionId = A.strReceiptNumber      
+   AND C.intJournalLineNo = B.intInventoryReceiptItemId      
+ INNER JOIN vyuGLAccountDetail ad      
+  ON ad.intAccountId = C.intAccountId      
+ WHERE       
+  ad.intAccountCategoryId = 45        
+ AND C.ysnIsUnposted = 0         
+ AND (C.dblCredit != 0 OR C.dblDebit != 0) 
+ AND item.strType = 'Non-Inventory' 
  UNION ALL 
  --ADD THIS TO GET THE ACCOUNT OF CHARGE FROM IR-39345 (MCP)
  SELECT DISTINCT    
