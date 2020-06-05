fa3684d109bc8f5a35f704f95181e0f46c3e2daa
Wendell Wayne Estrada <wendell.estrada@irely.com>
2020-06-05 10:46:01 +0800

    [B] IC-8532 CLONE - In-transit not matching between reports

--- i21Database/dbo/Functions/fnICGetConcatenatedInvoiceNumbersByCustomer.sql
+++ i21Database/dbo/Functions/fnICGetConcatenatedInvoiceNumbersByCustomer.sql
@@ -14,6 +14,7 @@ LEFT JOIN tblARInvoiceDetail d ON d.intInvoiceId = i.intInvoiceId
 LEFT JOIN tblICInventoryShipmentItem si ON si.intInventoryShipmentItemId = d.intInventoryShipmentItemId
 	AND d.intInventoryShipmentChargeId IS NULL
 WHERE i.intEntityCustomerId = @intCustomerId
+	AND si.intInventoryShipmentId = @intShipmentId
 	AND i.ysnPosted = 1
 
 RETURN @listStr
