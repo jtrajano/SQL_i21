b410deb0357870dbcce0174b4a609fe352653395
Jeffrey Trajano <jlt212000@yahoo.com>
2020-06-05 17:41:40 +0800

    GL-7509

--- i21Database/Scripts/Post-Deployment/GL/DefaultData/1u_InsertDefaultDataSync.sql
+++ i21Database/Scripts/Post-Deployment/GL/DefaultData/1u_InsertDefaultDataSync.sql
@@ -5,7 +5,6 @@ GO
         PRINT N'BEGIN INSERTING DEFAULT DATA FOR GL'
         UPDATE T set intFiscalPeriodId = F.intGLFiscalYearPeriodId FROM tblGLDetail T 
         CROSS APPLY dbo.fnGLGetFiscalPeriod(T.dtmDate) F
-        WHERE T.ysnIsUnposted = 0
 
         INSERT INTO tblGLDataFix VALUES( 'tblGLDetail_FiscalPeriod')
         PRINT N'FINISHED INSERTING DEFAULT DATA FOR GL'
