22cb36ffb792ff2a6c0112c6d2bbbd87c02519e7
Kevin Estrada <kevin.estrada@irely.com>
2020-06-08 20:17:02 +0800

    SC-2834

---
+++ i21Database/dbo/Tables/tblSCDisconnectedImportLog.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tblSCDisconnectedImportLog]
+(
+    [intDisconnectedImportLogId] INT IDENTITY(1,1) NOT NULL,
+    [strFileName] NVARCHAR(MAX) NULL,
+    [intRecordId] INT NULL,
+    [dtmDate] DATETIME NOT NULL,
+    [intConcurrencyId] INT NOT NULL DEFAULT(0)
+    CONSTRAINT [PK_dbo.tblSCDisconnectedImportLog] PRIMARY KEY CLUSTERED ([intDisconnectedImportLogId] ASC)
+  
+
+)
