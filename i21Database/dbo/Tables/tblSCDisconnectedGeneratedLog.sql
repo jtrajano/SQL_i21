b0a0133560a08e648a139de6076736c58a402759
Kevin Estrada <kevin.estrada@irely.com>
2020-06-08 20:17:02 +0800

    SC-2834

---
+++ i21Database/dbo/Tables/tblSCDisconnectedGeneratedLog.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tblSCDisconnectedGeneratedLog]
+(
+    [intDisconnectedGeneratedLogId] INT IDENTITY(1,1) NOT NULL,
+    [intRecordId] INT NOT NULL,
+    [intLogId] INT NULL,
+    [strType] NVARCHAR(100) NULL,
+    [dtmDate] DATETIME NULL,
+    [intConcurrencyId] INT DEFAULT(1),
+    CONSTRAINT [PK_dbo.tblSCDisconnectedGeneratedLog] PRIMARY KEY CLUSTERED ([intDisconnectedGeneratedLogId] ASC)
+
+)
