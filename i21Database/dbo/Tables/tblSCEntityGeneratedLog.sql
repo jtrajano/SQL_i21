334cd9920dd94708bef75ccb4501a07fdd034ae8
Kevin Estrada <kevin.estrada@irely.com>
2020-06-09 18:42:37 +0800

    SC-2835

---
+++ i21Database/dbo/Tables/tblSCEntityGeneratedLog.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[tblSCEntityGeneratedLog]
+(
+    [intEntityGeneratedLogId] INT IDENTITY(1,1) NOT NULL,
+    [intRecordId] INT NOT NULL,
+    [intLogId] INT NOT NULL,
+    [dtmDate] DATETIME NULL,
+    [intConcurrencyId] INT DEFAULT(1),
+    CONSTRAINT [PK_dbo.tblSCEntityGeneratedLog] PRIMARY KEY CLUSTERED ([intEntityGeneratedLogId] ASC)
+
+)
