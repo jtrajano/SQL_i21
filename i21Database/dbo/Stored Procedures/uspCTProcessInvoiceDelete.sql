431c51ad8a7e556a7d4644001a1db36f31e97377
jayson.ramos <jayson.ramos@irely.com>
2020-06-09 13:15:00 +0800

    CT-4872
    (cherry picked from commit c2ab3b88b07a5d61dbaeaf3e459f0318735d8a59)
    (cherry picked from commit 2f9f4def9c036f95324a3c34f114484c15d3765d)

--- i21Database/dbo/Stored Procedures/uspCTProcessInvoiceDelete.sql
+++ i21Database/dbo/Stored Procedures/uspCTProcessInvoiceDelete.sql
@@ -201,6 +201,24 @@ BEGIN
 			end
 
 		end
+
+		update
+			pf
+		set
+			pf.dblLotsFixed = tbl.dblLotsFixed
+		from tblCTPriceFixation pf
+			left join
+			(
+			select
+				pfd.intPriceFixationId
+				,dblLotsFixed = sum(pfd.dblNoOfLots)
+			from
+				tblCTPriceFixationDetail pfd
+			group by
+				pfd.intPriceFixationId
+			) tbl on tbl.intPriceFixationId = pf.intPriceFixationId
+		where
+			pf.intPriceFixationId = @intPriceFixationId;
     
 	end try
 	begin catch
