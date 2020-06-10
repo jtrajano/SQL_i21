4fd5efb54fdd729650cab71d9a5cf4cb02e877d5
Mon Gonzales <Mon.Gonzales@irely.com>
2020-06-10 16:32:18 +0800

    GRN-2195 - Object reference error when printing settlements

---
+++ i21Database/dbo/Stored Procedures/uspGRSettlementSummaryReportForView.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[uspGRSettlementSummaryReportForView]
+	@intPaymentId	int
+as
+begin
+	if @intPaymentId is null 
+		select * from vyuGRSettlementSummaryReport where 1 = 0
+	else
+		select * from vyuGRSettlementSummaryReport where intPaymentId = @intPaymentId
+end
+
+
+GO
