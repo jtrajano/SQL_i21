1856b1d5c735556a92dabd87a54f315d56cede34
Jopet Peavey A. Lambinicio <jopet.lambinicio@irely.com>
2020-06-11 11:45:06 +0800

    AP-8776

--- i21Database/dbo/Views/vyuAPPrepayHistory.sql
+++ i21Database/dbo/Views/vyuAPPrepayHistory.sql
@@ -15,7 +15,7 @@ FROM (
     ,b.dtmDate
     ,sum(bd.dblTotal+bd.dblTax) AS dblTotal
     ,b.strBillId AS strTransactionId
-    ,b.dtmDate AS dtmTransactionDate
+    ,b.dtmDateCreated AS dtmTransactionDate
     ,1 intSequence
     FROM tblAPBill b
     JOIN tblAPBillDetail bd ON b.intBillId = bd.intBillId
@@ -24,7 +24,7 @@ FROM (
     --WHERE (b.intTransactionType IN (13,2) AND b.ysnPosted = 1) OR (b.intTransactionType IN (3) AND b.ysnPosted = 1 AND b.dblAmountDue != 0)
     WHERE b.intTransactionType IN (13, 2, 3)
     AND b.ysnPosted = 1
-    GROUP BY b.intEntityVendorId, v.strVendorId, e.strName, b.intBillId, b.strBillId, b.dtmDate
+    GROUP BY b.intEntityVendorId, v.strVendorId, e.strName, b.intBillId, b.strBillId, b.dtmDate, b.dtmDateCreated
 
     UNION
     --Voucher Payment
@@ -37,7 +37,7 @@ FROM (
     ,b.dtmDate
     ,pd.dblPayment
     ,p.strPaymentRecordNum
-    ,p.dtmDatePaid
+    ,p.dtmDateCreated
     ,2
     FROM tblAPPaymentDetail pd 
     JOIN tblAPPayment p ON p.intPaymentId = pd.intPaymentId
@@ -59,7 +59,7 @@ FROM (
     ,b.dtmDate
     ,pd.dblPayment
     ,p.strPaymentRecordNum
-    ,p.dtmDatePaid
+    ,p.dtmDateCreated
     ,3
     FROM tblAPPaymentDetail pd 
     JOIN tblAPPayment p ON p.intPaymentId = pd.intPaymentId
@@ -81,7 +81,7 @@ FROM (
     ,b.dtmDate
     ,o.dblAmountApplied*-1
     ,b1.strBillId
-    ,b1.dtmDate
+    ,b1.dtmDateCreated
     ,4
     FROM tblAPAppliedPrepaidAndDebit o
     JOIN tblAPBill b ON o.intTransactionId = b.intBillId
