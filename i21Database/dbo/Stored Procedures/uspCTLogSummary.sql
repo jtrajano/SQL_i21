8730b81d4529ab7eac8d422049bdd03fdf0d3bdc
Dan Jalimeo <dan.jalimao@irely.com>
2020-06-12 09:54:10 +0800

    CT-4841

--- i21Database/dbo/Stored Procedures/uspCTLogSummary.sql
+++ i21Database/dbo/Stored Procedures/uspCTLogSummary.sql
@@ -2108,7 +2108,12 @@ BEGIN TRY
 						WHERE strProcess = 'Price Fixation'
 						AND intContractHeaderId = @intContractHeaderId
 						AND intContractDetailId = ISNULL(@intContractDetailId, cd.intContractDetailId)
-						AND (pfd.dblQuantity <> dblQty or pfd.dblFutures <> dblFutures)
+						AND 
+						(
+							pfd.dblQuantity <> CAST(REPLACE(strNotes, 'Priced Quantity is ', '') AS NUMERIC(24, 10)) 
+							OR 
+							pfd.dblFutures <> dblFutures
+						)
 					)
 				) tbl
 				WHERE intContractHeaderId = @intContractHeaderId
