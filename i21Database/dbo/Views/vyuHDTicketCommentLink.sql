3f5545c4edb1da8eeb50d6845ccdc4c0684bb736
IRELY\JMendones <jesmar.mendones@irely.com>
2020-06-12 14:41:07 +0800

    HD-3548

--- i21Database/dbo/Views/vyuHDTicketCommentLink.sql
+++ i21Database/dbo/Views/vyuHDTicketCommentLink.sql
@@ -3,7 +3,7 @@
 	select
 		a.*
 		,strCreatedFullName = c.strName + (case when b.strLocation is null then ' (User)' else ' (Agent)' end)
-		,strCreatedLocation = (case when b.strLocation is null then e.strLocationName else b.strLocation end)
+		,strCreatedLocation = e.strLocationName
 		,imgCreatedPhoto = c.imgPhoto
 		,strMessage = (case when a.dtmSent is null then 'Draft' else dbo.fnHDTicketCommentSentMessage(a.dtmSent, GETDATE()) end)  COLLATE Latin1_General_CI_AS
 	from
