9a275ab96db0e189380de58624bfc440898b9721
IRELY\JMendones <jesmar.mendones@irely.com>
2020-06-15 19:52:45 +0800

    SM-4965

--- i21PreDeployment/Scripts/Pre-Deployment/4_SM/12_MigrateRecentlyViewed.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/4_SM/12_MigrateRecentlyViewed.sql
@@ -1,19 +1,22 @@
-﻿IF NOT EXISTS(SELECT TOP 1 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE UPPER(TABLE_NAME) = 'TBLSMRECENTLYVIEWED' AND UPPER(COLUMN_NAME) = 'INTLOGID')
+﻿IF (EXISTS (SELECT TOP 1 1 FROM INFORMATION_SCHEMA.TABLES WHERE UPPER(TABLE_NAME) = 'TBLSMRECENTLYVIEWED'))
 BEGIN
-	ALTER TABLE tblSMLog ADD intRecentlyViewedId INT NULL
-	ALTER TABLE tblSMRecentlyViewed ADD intLogId INT NULL
-
-	EXEC('
-			INSERT INTO tblSMLog (strType, dtmDate, intEntityId, strRoute, intConcurrencyId, intRecentlyViewedId)
-			SELECT ''Recently Viewed'', dtmDateEntered, intEntityId, strRoute, 1, intRecentlyViewedId from tblSMRecentlyViewed;
-	')
-
-	EXEC('
-			UPDATE tblSMRecentlyViewed
-			SET intLogId = B.intLogId
-			FROM tblSMLog B
-			WHERE tblSMRecentlyViewed.intRecentlyViewedId = B.intRecentlyViewedId;		
-	')
-
-	ALTER TABLE tblSMLog DROP COLUMN intRecentlyViewedId
+  IF NOT EXISTS(SELECT TOP 1 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE UPPER(TABLE_NAME) = 'TBLSMRECENTLYVIEWED' AND UPPER(COLUMN_NAME) = 'INTLOGID')
+  BEGIN
+  	ALTER TABLE tblSMLog ADD intRecentlyViewedId INT NULL
+  	ALTER TABLE tblSMRecentlyViewed ADD intLogId INT NULL
+  
+  	EXEC('
+  			INSERT INTO tblSMLog (strType, dtmDate, intEntityId, strRoute, intConcurrencyId, intRecentlyViewedId)
+  			SELECT ''Recently Viewed'', dtmDateEntered, intEntityId, strRoute, 1, intRecentlyViewedId from tblSMRecentlyViewed;
+  	')
+  
+  	EXEC('
+  			UPDATE tblSMRecentlyViewed
+  			SET intLogId = B.intLogId
+  			FROM tblSMLog B
+  			WHERE tblSMRecentlyViewed.intRecentlyViewedId = B.intRecentlyViewedId;		
+  	')
+  
+  	ALTER TABLE tblSMLog DROP COLUMN intRecentlyViewedId
+  END
 END
