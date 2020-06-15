3b5cb99cbed4c9b05d488c39e51187f80eee7958
IRELY\JMendones <jesmar.mendones@irely.com>
2020-06-15 19:48:10 +0800

    SM-4965

--- i21PreDeployment/Scripts/Pre-Deployment/4_SM/12_MigrateRecentlyViewed.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/4_SM/12_MigrateRecentlyViewed.sql
@@ -1,32 +1,35 @@
-﻿IF NOT EXISTS(SELECT TOP 1 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE UPPER(TABLE_NAME) = 'TBLSMRECENTLYVIEWED' AND UPPER(COLUMN_NAME) = 'INTLOGID')
+﻿IF (EXISTS (SELECT TOP 1 1 FROM INFORMATION_SCHEMA.TABLES WHERE UPPER(TABLE_NAME) = 'TBLSMRECENTLYVIEWED'))
 BEGIN
-	ALTER TABLE tblSMLog ADD intRecentlyViewedId INT NULL
-	ALTER TABLE tblSMRecentlyViewed ADD intLogId INT NULL
+	IF NOT EXISTS(SELECT TOP 1 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE UPPER(TABLE_NAME) = 'TBLSMRECENTLYVIEWED' AND UPPER(COLUMN_NAME) = 'INTLOGID')
+	BEGIN
+		ALTER TABLE tblSMLog ADD intRecentlyViewedId INT NULL
+		ALTER TABLE tblSMRecentlyViewed ADD intLogId INT NULL
 
-	PRINT('START MIGRATING RECENTLY VIEWED')
+		PRINT('START MIGRATING RECENTLY VIEWED')
 
-	EXEC('
-			INSERT INTO tblSMLog (strType, dtmDate, intEntityId, strRoute, intConcurrencyId, intRecentlyViewedId)
-			SELECT ''Recently Viewed'', dtmDateEntered, intEntityId, strRoute, 1, intRecentlyViewedId from tblSMRecentlyViewed;
-	')
+		EXEC('
+				INSERT INTO tblSMLog (strType, dtmDate, intEntityId, strRoute, intConcurrencyId, intRecentlyViewedId)
+				SELECT ''Recently Viewed'', dtmDateEntered, intEntityId, strRoute, 1, intRecentlyViewedId from tblSMRecentlyViewed;
+		')
 
-	EXEC('
-			UPDATE tblSMRecentlyViewed
-			SET intLogId = B.intLogId
-			FROM tblSMLog B
-			WHERE tblSMRecentlyViewed.intRecentlyViewedId = B.intRecentlyViewedId;		
-	')
+		EXEC('
+				UPDATE tblSMRecentlyViewed
+				SET intLogId = B.intLogId
+				FROM tblSMLog B
+				WHERE tblSMRecentlyViewed.intRecentlyViewedId = B.intRecentlyViewedId;		
+		')
 	
-	PRINT('END MIGRATIMG RECENTLY VIEWED')
+		PRINT('END MIGRATIMG RECENTLY VIEWED')
 
-	PRINT('START DROPING COLUMNS RECENTLY VIEWED')
+		PRINT('START DROPING COLUMNS RECENTLY VIEWED')
 
-	ALTER TABLE tblSMRecentlyViewed DROP CONSTRAINT FK_tblSMRecentlyViewed_tblEMEntity_intEntityId
-	ALTER TABLE tblSMLog DROP COLUMN intRecentlyViewedId
-	ALTER TABLE tblSMRecentlyViewed DROP COLUMN intEntityId
-	ALTER TABLE tblSMRecentlyViewed DROP COLUMN strRoute
-	ALTER TABLE tblSMRecentlyViewed DROP COLUMN dtmDateEntered
-	ALTER TABLE tblSMRecentlyViewed DROP COLUMN dtmDateModified
+		ALTER TABLE tblSMRecentlyViewed DROP CONSTRAINT FK_tblSMRecentlyViewed_tblEMEntity_intEntityId
+		ALTER TABLE tblSMLog DROP COLUMN intRecentlyViewedId
+		ALTER TABLE tblSMRecentlyViewed DROP COLUMN intEntityId
+		ALTER TABLE tblSMRecentlyViewed DROP COLUMN strRoute
+		ALTER TABLE tblSMRecentlyViewed DROP COLUMN dtmDateEntered
+		ALTER TABLE tblSMRecentlyViewed DROP COLUMN dtmDateModified
 
-	PRINT('END DROPING COLUMNS RECENTLY VIEWED')
+		PRINT('END DROPING COLUMNS RECENTLY VIEWED')
+	END
 END
