059d5975e2c0450cd7b92fc66f0bd32659032253
Roudiee Panteria <roudinee.panteria@irely.com>
2020-06-17 14:03:13 +0800

    AR-12045

--- i21Database/dbo/Views/vyuARPaymentForNSF.sql
+++ i21Database/dbo/Views/vyuARPaymentForNSF.sql
@@ -19,6 +19,7 @@ INNER JOIN dbo.tblSMPaymentMethod SM WITH (NOLOCK) ON P.intPaymentMethodId = SM.
 INNER JOIN dbo.tblCMUndepositedFund UF WITH (NOLOCK) ON P.intPaymentId = UF.intSourceTransactionId
 											        AND P.strRecordNumber = UF.strSourceTransactionId
 INNER JOIN dbo.tblCMBankTransactionDetail BTD WITH (NOLOCK) ON UF.intUndepositedFundId = BTD.intUndepositedFundId
+INNER JOIN dbo.tblCMBankTransaction BT WITH (NOLOCK) ON BTD.intTransactionId = BT.intTransactionId 
 LEFT JOIN (
 	SELECT intPaymentId
 		 , intInvoiceId
@@ -37,6 +38,7 @@ WHERE P.ysnProcessedToNSF = 0
   AND P.ysnPosted = 1
     AND SM.strPaymentMethod IN ('Check', 'eCheck', 'ACH', 'Manual Credit Card', 'Credit Card')
   AND (ISNULL(CPP.intInvoiceId, 0) = 0 OR (ISNULL(CPP.intInvoiceId, 0) <> 0 AND ISNULL(PAYMENT.intPaymentId, 0) = 0))
+  AND BT.ysnPosted = 1
 
 UNION ALL
 
@@ -59,8 +61,10 @@ INNER JOIN dbo.tblSMPaymentMethod SM WITH (NOLOCK) ON I.intPaymentMethodId = SM.
 INNER JOIN dbo.tblCMUndepositedFund UF WITH (NOLOCK) ON I.intInvoiceId = UF.intSourceTransactionId
 											        AND I.strInvoiceNumber = UF.strSourceTransactionId
 INNER JOIN dbo.tblCMBankTransactionDetail BTD WITH (NOLOCK) ON UF.intUndepositedFundId = BTD.intUndepositedFundId
+INNER JOIN dbo.tblCMBankTransaction BT WITH (NOLOCK) ON BTD.intTransactionId = BT.intTransactionId 
 WHERE I.ysnProcessedToNSF = 0
   AND I.ysnPosted = 1
   AND I.intPaymentMethodId IS NOT NULL
   AND I.strTransactionType = 'Cash'
   AND SM.strPaymentMethod IN ('Check', 'eCheck', 'ACH', 'Manual Credit Card', 'Credit Card')
+  AND BT.ysnPosted = 1
