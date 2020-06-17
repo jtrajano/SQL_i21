c5781e045c3d9325ac3e244a45deb5ebe56145d0
Erick Dela Cruz <erick.delacruz@irely.com>
2020-06-17 17:19:02 +0800

    AP-8830

--- i21Database/dbo/Functions/fnAPValidatePostBill.sql
+++ i21Database/dbo/Functions/fnAPValidatePostBill.sql
@@ -409,7 +409,6 @@ BEGIN
 			WHERE B.intBillId = A.intBillId
 		) details
 		WHERE A.intBillId IN (SELECT [intBillId] FROM @tmpBills) AND details.dblTotal < 0
-		AND A.intTransactionType = 1
 			
 		--DO NOT ALLOW TO POST IF BILL HAS CONTRACT ITEMS AND CONTRACT PRICE ON CONTRACT RECORD DID NOT MATCHED
 		--COMPARE THE CASH PRICE
