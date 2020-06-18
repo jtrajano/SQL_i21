fe95307be1ab8fa66a1dd02f3f7a3f71f2e949a6
Feb Montefrio <feb.montefrio@irely.com>
2020-06-18 19:43:40 +0800

    [B] IC-8610

--- i21Database/dbo/Tables/tblICDataSource.sql
+++ i21Database/dbo/Tables/tblICDataSource.sql
@@ -1,5 +1,5 @@
 ﻿CREATE TABLE [dbo].[tblICDataSource]
 (
 	[intDataSourceId] TINYINT NOT NULL PRIMARY KEY, 
-    [strSourceName] NVARCHAR(200) NULL
+    [strSourceName] NVARCHAR(200) COLLATE Latin1_General_CI_AS NULL 
 )
