1f87e5e2369cfd4abe99f1dc4d99e7986e358aea
manny_armas <manny.armas@iRely.com>
2020-06-18 21:12:59 +0800

    IET-501 - Customer Screen - Correct the 'Current' calculation (match i21 and ET A/R customer fields)

--- i21Database/dbo/Views/vyuETExportCustomerBalances.sql
+++ i21Database/dbo/Views/vyuETExportCustomerBalances.sql
@@ -15,11 +15,11 @@ SELECT
 																EL.[intEntityId] = ARCI.[intEntityCustomerId]
 															)
 								)
-	,blterm				= SMT.[strTermCode]
-	,blov30				= ISNULL(ARCI.[dbl60Days], 0.000000)
-	,blov60				= ISNULL(ARCI.[dbl90Days], 0.000000)
-	,blov90				= ISNULL(ARCI.[dbl91Days], 0.000000)
-	,blcchr				= ISNULL(ARCI.dbl30Days, 0.000000) + ISNULL(ARCI.[dbl0Days], 0.000000) + ISNULL(ARCI.[dbl10Days], 0.000000)
+	,blterm    = SMT.[strTermCode]  
+	,blov30    = ISNULL(ARCI.[dbl60Days], 0.000000)  
+	,blov60    = ISNULL(ARCI.[dbl90Days], 0.000000)  
+	,blov90    = ISNULL(ARCI.[dbl91Days], 0.000000)  
+	,blcchr    = ISNULL(ARCI.[dbl0Days], 0.000000) -- Current
 	,blcpay				= ISNULL(ARCI.[dblUnappliedCredits], 0.000000) + ISNULL(ARCI.[dblPrepaids], 0.000000)
 	,decLastPaymentAmt	= ISNULL(ARCI.[dblLastPayment], 0.000000)
 	,dtLastPaymentDate	= CONVERT(NVARCHAR(8), ARCI.[dtmLastPaymentDate], 112) COLLATE Latin1_General_CI_AS
