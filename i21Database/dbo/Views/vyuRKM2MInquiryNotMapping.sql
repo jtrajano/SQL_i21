078e1f1eb302e171bf00dd8666304b41fb77528b
lawrence_zabala <lawrence.zabala@irely.com>
2020-06-19 12:01:39 +0800

    RM-3630

--- i21Database/dbo/Views/vyuRKM2MInquiryNotMapping.sql
+++ i21Database/dbo/Views/vyuRKM2MInquiryNotMapping.sql
@@ -1,19 +1,21 @@
 ﻿CREATE VIEW vyuRKM2MInquiryNotMapping
+
 AS
+
 SELECT intM2MInquiryId
-	,strCommodityCode
-	,cur.strCurrency
-	,strMarketZoneCode
-	,strLocationName
-	,um.strUnitMeasure
-	,dtmM2MBasisDate
-	,dtmPriceDate dtmFutureSettlementDate
-	,pum.strUnitMeasure strPriceUnitMeasure
+	, strCommodityCode
+	, cur.strCurrency
+	, strMarketZoneCode
+	, strLocationName
+	, um.strUnitMeasure
+	, dtmM2MBasisDate
+	, dtmFutureSettlementDate = dtmPriceDate 
+	, strPriceUnitMeasure = pum.strUnitMeasure
 FROM tblRKM2MInquiry bd
-JOIN tblRKM2MBasis bas ON bas.intM2MBasisId = bd.intM2MBasisId
-JOIN tblSMCurrency cur ON cur.intCurrencyID = bd.intCurrencyId
-JOIN tblICUnitMeasure pum ON pum.intUnitMeasureId = bd.intPriceItemUOMId
-JOIN tblRKFuturesSettlementPrice sc ON sc.intFutureSettlementPriceId = bd.intFutureSettlementPriceId
+LEFT JOIN tblRKM2MBasis bas ON bas.intM2MBasisId = bd.intM2MBasisId
+LEFT JOIN tblSMCurrency cur ON cur.intCurrencyID = bd.intCurrencyId
+LEFT JOIN tblICUnitMeasure pum ON pum.intUnitMeasureId = bd.intPriceItemUOMId
+LEFT JOIN tblRKFuturesSettlementPrice sc ON sc.intFutureSettlementPriceId = bd.intFutureSettlementPriceId
 LEFT JOIN tblICCommodity c ON c.intCommodityId = bd.intCommodityId
 LEFT JOIN tblICUnitMeasure um ON um.intUnitMeasureId = bd.intUnitMeasureId
 LEFT JOIN tblARMarketZone z ON z.intMarketZoneId = bd.intMarketZoneId
