ac2095fd5d842c0979f4b261591841ea70e3c435
Dan Jalimeo <dan.jalimao@irely.com>
2020-06-20 18:28:36 +0800

    CT-4868

--- i21Database/dbo/Stored Procedures/uspCTGetTerms.sql
+++ i21Database/dbo/Stored Procedures/uspCTGetTerms.sql
@@ -12,7 +12,7 @@ BEGIN
 		SELECT	TM.intTermID,
 				TM.strTerm
 		FROM	tblSMTerm	TM
-		WHERE	TM.strTerm    LIKE	'%' + @strTerm + '%'
+		WHERE	TM.strTerm    LIKE	'%' + @strTerm COLLATE Latin1_General_CI_AS + '%'
 		AND		TM.intTermID	=	(CASE WHEN @intTermID > 0 THEN @intTermID ELSE TM.intTermID END)
 		AND		TM.ysnActive	=	(CASE WHEN ISNULL(@ysnActive,0) = 0 THEN TM.ysnActive ELSE @ysnActive END)
 	END		
@@ -22,7 +22,7 @@ BEGIN
 				TM.strTerm
 		FROM	tblAPVendorTerm	VT
 		JOIN	tblSMTerm	TM ON VT.intTermId = TM.intTermID AND VT.intEntityVendorId = @intEntityId
-		WHERE	TM.strTerm	  LIKE	'%' + @strTerm + '%'
+		WHERE	TM.strTerm	  LIKE	'%' + @strTerm COLLATE Latin1_General_CI_AS + '%'
 		AND		TM.intTermID	=	(CASE WHEN @intTermID > 0 THEN @intTermID ELSE TM.intTermID END)
 		AND		TM.ysnActive	=	(CASE WHEN ISNULL(@ysnActive,0) = 0 THEN TM.ysnActive ELSE @ysnActive END)
 	END
