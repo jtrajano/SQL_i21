4a76c98c89ea2587b0c6286692e5639d0e7872c1
Dan Jalimeo <dan.jalimao@irely.com>
2020-06-23 09:57:34 +0800

    CT-4915

--- i21Database/Scripts/Post-Deployment/CT/2010_AddMissingHistory.sql
+++ i21Database/Scripts/Post-Deployment/CT/2010_AddMissingHistory.sql
@@ -4,19 +4,28 @@
 
 	IF OBJECT_ID('tempdb..#TempContractHeaders') IS NOT NULL DROP TABLE #TempContractHeaders
 
-	SELECT DISTINCT a.intContractHeaderId, c.strContractNumber
+	SELECT DISTINCT a.intContractHeaderId, c.strContractNumber, intUserId = ISNULL(a.intCreatedById, a.intLastModifiedById)
 	INTO #TempContractHeaders
 	FROM tblCTContractDetail a
 	INNER JOIN tblCTContractHeader c ON a.intContractHeaderId = c.intContractHeaderId
 	WHERE intContractDetailId NOT IN (SELECT intContractDetailId FROM tblCTSequenceHistory)
 	ORDER BY a.intContractHeaderId ASC
 
-	DECLARE @currentId INT
+	DECLARE @currentId INT,
+			@userId INT,
+			@irelyAdmin INT
 	WHILE EXISTS(SELECT TOP 1 1 FROM #TempContractHeaders)
 	BEGIN
-		SELECT TOP 1 @currentId = intContractHeaderId FROM #TempContractHeaders
+		SELECT TOP 1 @currentId = intContractHeaderId, @userId = intUserId FROM #TempContractHeaders
+
+		SELECT @irelyAdmin = intEntityId FROM tblEMEntityCredential WHERE UPPER(strUserName) = 'IRELYADMIN'
+		SELECT @userId = ISNULL(@userId, @irelyAdmin)
 	
-		EXEC uspCTCreateDetailHistory @intContractHeaderId =  @currentId, @ysnUseContractDate = 1
+		EXEC uspCTCreateDetailHistory	@intContractHeaderId 	= @currentId,
+										@ysnUseContractDate 	= 1,
+										@strSource 				= 'Contract',
+										@strProcess 			= 'Save Contract',
+										@intUserId				= @userId	
 
 		DELETE FROM #TempContractHeaders WHERE intContractHeaderId = @currentId
 	END
