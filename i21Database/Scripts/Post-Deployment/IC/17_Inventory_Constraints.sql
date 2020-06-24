2ce21ee441888280b3278405d9a1fec8bf9362eb
Feb Montefrio <feb.montefrio@irely.com>
2020-06-24 12:13:53 +0800

    [B] IC-8652
    # Conflicts:
    #	i21Database/Scripts/Post-Deployment/IC/17_Inventory_Constraints.sql
    #	i21Database/i21Database.sqlproj

--- i21Database/Scripts/Post-Deployment/IC/17_Inventory_Constraints.sql
+++ i21Database/Scripts/Post-Deployment/IC/17_Inventory_Constraints.sql
@@ -37,5 +37,31 @@ BEGIN
 END
 GO
 
+GO
+
+-- Add the CHECK CONSTRAINTS in tblICItem
+IF NOT EXISTS(SELECT TOP 1 1 FROM sys.objects WHERE name = 'CK_AllowItemTypeChange' AND type = 'C' AND parent_object_id = OBJECT_ID('tblICItem', 'U'))
+BEGIN
+	EXEC('
+		ALTER TABLE tblICItem
+		WITH NOCHECK ADD CONSTRAINT CK_AllowItemTypeChange
+		CHECK (dbo.fnAllowItemTypeChange(intItemId, strType) = 1)'
+	);
+
+END
+GO
+
+---- Add the CHECK CONSTRAINTS in tblICItemStockUOM
+--IF NOT EXISTS(SELECT TOP 1 1 FROM sys.objects WHERE name = 'CK_IsValidStorageLocation' AND type = 'C' AND parent_object_id = OBJECT_ID('tblICItemStockUOM', 'U'))
+--BEGIN
+--	EXEC('
+--		ALTER TABLE tblICItemStockUOM
+--		WITH NOCHECK ADD CONSTRAINT CK_IsValidStorageLocation
+--		CHECK (dbo.fnICIsValidStorageLocation(intItemLocationId, intSubLocationId, intStorageLocationId) = 1)'
+--	);
+
+--END
+--GO
+
 PRINT N'END - IC Add Constraint'
 GO
