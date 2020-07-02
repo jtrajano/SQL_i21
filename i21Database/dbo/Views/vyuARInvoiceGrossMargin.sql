c26c2b5d93c8219dd8e9f7d0b9b2d7ab7632dd84
Jeff Trajano <jeffrey.trajano@irely.com>
2020-07-02 16:11:42 +0800

    GL-7703 Create sql view for Gross Margin (Dashboard widget)

---
+++ i21Database/dbo/Views/vyuARInvoiceGrossMargin.sql
@@ -0,0 +1,17 @@
+CREATE VIEW vyuARInvoiceGrossMargin
+AS
+SELECT  
+ARI.intInvoiceId,
+ARI.strInvoiceNumber, 
+ARI.dblPayment dblRevenue,
+SUM( dblTotalCost)dblExpense,
+ARI.dblPayment- sum( dblTotalCost) dblNet, 
+(ARI.dblPayment- sum( dblTotalCost)) /  ARI.dblPayment dblGrossMargin
+FROM tblARInvoiceDetail ARID 
+JOIN 
+[vyuARSalesAnalysisReport] SA
+ON ARID.intInvoiceDetailId = SA.intInvoiceDetailId
+JOIN tblARInvoice ARI ON ARI.intInvoiceId = ARID.intInvoiceId
+WHERE ARI.ysnPosted =1 and ysnPaid = 1
+and ARI.dblInvoiceTotal <> 0
+GROUP by ARI.intInvoiceId, ARI.strInvoiceNumber, ARI.dblPayment
