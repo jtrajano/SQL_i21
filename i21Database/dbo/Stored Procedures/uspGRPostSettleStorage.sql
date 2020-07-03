ef23eb92217cd191515747585d59ae564f9140c0
Mon Gonzales <Mon.Gonzales@irely.com>
2020-07-03 11:14:52 +0800

    GRN-2218 - Unable to price the contract that was used in settle storage

--- i21Database/dbo/Stored Procedures/uspGRPostSettleStorage.sql
+++ i21Database/dbo/Stored Procedures/uspGRPostSettleStorage.sql
@@ -2162,9 +2162,7 @@ BEGIN TRY
 					,a.intItemType				
 				 
 
-				 
-
-							 
+				update @voucherPayable set dblOldCost = null where dblCost = dblOldCost
 				 ---we should delete priced contracts that has a voucher already
 					delete from @voucherPayable 
 						where intContractDetailId in (
