825a54fa11f34acaa26ec7cbd7834697c3c236e9
bonn ponce <bonn.ponce@irely.com>
2020-07-03 15:24:08 +0800

    CF-2179

---
+++ i21Database/dbo/Views/vyuCFEntityCustomerSearch.sql
@@ -0,0 +1,32 @@
+﻿
+
+CREATE VIEW vyuCFEntityCustomerSearch
+AS
+
+SELECT 
+intId = CAST(ROW_NUMBER() OVER (ORDER BY (SELECT 1)) AS INT),
+tblEMEntity.intEntityId,
+tblEMEntity.strEntityNo,
+intCustomerId = tblEMEntity.intEntityId,
+strCustomerNo = tblEMEntity.strEntityNo,
+tblEMEntity.strName,
+tblSMTerm.strTerm,
+intTermId = tblSMTerm.intTermID,
+intSalesPersonId = tblEMSalesperson.intEntityId,
+strSalesPersonId = tblEMSalesperson.strEntityNo,
+strSalesPersonName = tblEMSalesperson.strName,
+tblARAccountStatus.intAccountStatusId,
+tblARAccountStatus.strAccountStatusCode
+FROM tblARCustomer
+INNER JOIN tblEMEntity
+ON tblARCustomer.intEntityId = tblEMEntity.intEntityId
+LEFT JOIN tblEMEntity AS tblEMSalesperson
+ON tblEMSalesperson.intEntityId = tblARCustomer.intSalespersonId
+LEFT JOIN tblSMTerm
+ON tblARCustomer.intTermsId = tblSMTerm.intTermID
+LEFT JOIN tblARCustomerAccountStatus
+ON tblEMEntity.intEntityId = tblARCustomerAccountStatus.intEntityCustomerId
+LEFT JOIN tblARAccountStatus
+ON tblARAccountStatus.intAccountStatusId = tblARCustomerAccountStatus.intAccountStatusId
+
+GO
