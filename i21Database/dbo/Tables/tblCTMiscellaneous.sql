92c0e28ba759b3c444e34465bc8a00c41472b91c
jayson.ramos <jayson.ramos@irely.com>
2020-07-03 10:19:31 +0800

    CT-4944

---
+++ i21Database/dbo/Tables/tblCTMiscellaneous.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[tblCTMiscellaneous]
+(
+	[intMiscellaneousId] INT IDENTITY(1,1) NOT NULL,
+	[ysnContractBalanceInProgress] BIT NOT NULL DEFAULT 0,
+	[ysnDestinationWeightsAndGradesFixed] BIT NOT NULL DEFAULT 0,
+	[ysnFixedSMTransactionWithWrongPricingScreenId] BIT NOT NULL DEFAULT 0
+)
