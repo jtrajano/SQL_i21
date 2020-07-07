fa4ec199389965eec90bd964b688c0388e70d050
Kenneth Lew Lagunero <kenneth.lagunero@irely.com>
2020-07-07 18:08:26 +0800

    SC-3698

--- i21Database/dbo/Stored Procedures/uspSCUndistributeTicket.sql
+++ i21Database/dbo/Stored Procedures/uspSCUndistributeTicket.sql
@@ -269,8 +269,7 @@ BEGIN TRY
 
 								SET @_intStorageHistoryId = ISNULL((SELECT TOP 1 MIN(intStorageHistoryId) 
 																	FROM #tmpSCStorageHistory 
-																	WHERE intInventoryReceiptId = @InventoryReceiptId 
-																	ORDER BY intInventoryReceiptId),0)
+																	WHERE intInventoryReceiptId = @InventoryReceiptId),0)
 							
 								WHILE ISNULL(@_intStorageHistoryId,0) > 0
 								BEGIN
