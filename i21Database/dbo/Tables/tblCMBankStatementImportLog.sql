143ca6979c3f5b5cf1e502357438f0880cd796ba
Jeff Trajano <jeffrey.trajano@irely.com>
2020-07-08 12:14:23 +0800

    GL-7678 Import Bank Statement

--- i21Database/dbo/Tables/tblCMBankStatementImportLog.sql
+++ i21Database/dbo/Tables/tblCMBankStatementImportLog.sql
@@ -3,13 +3,14 @@ CREATE TABLE tblCMBankStatementImportLog
     intImportBankStatementLogId INT IDENTITY(1,1) NOT NULL,
     strBankStatementImportId  NVARCHAR(40) COLLATE Latin1_General_CI_AS  NULL,
     strDescription NVARCHAR(200)COLLATE Latin1_General_CI_AS  NULL,
+    intEntityId INT NULL,
     intTaskCount INT NULL,
     intBTransactionCount INT NULL,
     dtmDate DATETIME,
     intConcurrencyId INT NULL,
     CONSTRAINT [PK_tblCMBankStatementImportLog] PRIMARY KEY CLUSTERED 
 (
-	[intSummaryId] ASC
+	[intImportBankStatementLogId] ASC
 )
 WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
