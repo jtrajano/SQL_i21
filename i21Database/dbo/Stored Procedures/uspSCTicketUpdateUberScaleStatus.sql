521fed3a21da5f961e696e07f5501a5262d453ff
Von Pasion <von.pasion@irely.com>
2020-07-08 18:06:50 +0800

    UBS-29

--- i21Database/dbo/Stored Procedures/uspSCTicketUpdateUberScaleStatus.sql
+++ i21Database/dbo/Stored Procedures/uspSCTicketUpdateUberScaleStatus.sql
@@ -6,13 +6,24 @@ AS
 	IF EXISTS(SELECT TOP 1 1 FROM tblSCTicket WHERE intTicketId = @intTicketId and ISNULL(strTicketStatus, '') <> @strUberStatusCode)
 	BEGIN
 
-		DECLARE @ticketStatus AS NVARCHAR(3) = (SELECT TOP 1 strTicketStatus FROM tblSCTicket WHERE intTicketId = @intTicketId)
+		DECLARE @ticketStatus AS NVARCHAR(3)
+		DECLARE @hasGeneratedTicket AS NVARCHAR(3)
 
-		IF @ticketStatus <> '' AND (@ticketStatus = 'S' OR @ticketStatus = 'I')
+		SELECT TOP 1 
+			@ticketStatus = strTicketStatus,
+			@hasGeneratedTicket = ysnHasGeneratedTicketNumber
+		FROM tblSCTicket WHERE intTicketId = @intTicketId
+
+		IF @ticketStatus <> '' AND (@ticketStatus = 'S' OR @ticketStatus = 'I' OR @ticketStatus = 'O') 
 		BEGIN
-			UPDATE tblSCTicket set strTicketStatus = @strUberStatusCode where intTicketId = @intTicketId
-			INSERT INTO tblSCTicketUberScaleStatusUpdate(intTicketId, dtmTransactionDate, strUberStatusCode)
-			SELECT @intTicketId, getdate(), @strUberStatusCode
+			IF @hasGeneratedTicket = 0
+			BEGIN
+				UPDATE tblSCTicket set strTicketStatus = @strUberStatusCode where intTicketId = @intTicketId
+				INSERT INTO tblSCTicketUberScaleStatusUpdate(intTicketId, dtmTransactionDate, strUberStatusCode)
+				SELECT @intTicketId, getdate(), @strUberStatusCode
+			END
+			ELSE
+				THROW 99999, 'Ticket number has been generated.', 1;  
 		END
 	END
 
