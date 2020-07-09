cbb37915b2cacf0c4b151a6d4f39364c09d59f6f
kelvin_quinto <kelvin.quinto@irely.com>
2020-07-09 16:40:24 +0800

    AR-12117
    (cherry picked from commit d0f4d36a4321b0f717f3c7cce8d3bd94950202ac)
    (cherry picked from commit 613a09af1ecb5049343602aa9e271b7ed4c4e1b5)

--- i21Database/Scripts/Post-Deployment/AR/DefaultData/13_FixOnOrderCommittedQuantity.sql
+++ i21Database/Scripts/Post-Deployment/AR/DefaultData/13_FixOnOrderCommittedQuantity.sql
@@ -71,14 +71,20 @@ SET dblOrderCommitted = ISNULL(SALES.dblCommitted, 0)
 FROM (
 	SELECT SO.intItemId
 		 , IIL.intItemLocationId
+		 , SO.intSubLocationId
+		 , SO.intStorageLocationId
 		 , CASE WHEN SO.dblCommitted < 0 THEN 0 ELSE SO.dblCommitted END dblCommitted
 	FROM (
 		SELECT intItemId
 			 , intCompanyLocationId
+			 , intSubLocationId
+			 , intStorageLocationId
 			 , dblCommitted = SUM(ISNULL(dblCommitted, 0))
 		FROM (SELECT SOD.intItemId
-					, SO.intCompanyLocationId			 
-					, dblCommitted = SUM(dbo.fnICConvertUOMtoStockUnit(SOD.intItemId, SOD.intItemUOMId, SOD.dblQtyOrdered)) 
+				   , SO.intCompanyLocationId
+				   , SOD.intSubLocationId
+			 	   , SOD.intStorageLocationId
+				   , dblCommitted = SUM(dbo.fnICConvertUOMtoStockUnit(SOD.intItemId, SOD.intItemUOMId, SOD.dblQtyOrdered)) 
 									- ISNULL(SUM(dbo.fnICConvertUOMtoStockUnit(ID.intItemId, ID.intItemUOMId, ID.dblQtyShipped)), 0)
 									- ISNULL(SUM(dbo.fnICConvertUOMtoStockUnit(ISHI.intItemId, ISHI.intItemUOMId, ISHI.dblQuantity)), 0)
 			FROM tblSOSalesOrderDetail SOD INNER JOIN tblSOSalesOrder SO 
@@ -97,13 +103,15 @@ FROM (
 					ON SOD.intSalesOrderDetailId = ISHI.intLineNo
 			WHERE dbo.fnIsStockTrackingItem(SOD.intItemId) = 1		  
 				AND SO.strTransactionType = 'Order'
-			GROUP BY SOD.intItemId, SO.intCompanyLocationId
+			GROUP BY SOD.intItemId, SO.intCompanyLocationId, SOD.intSubLocationId, SOD.intStorageLocationId
 
 			UNION ALL
 
 			SELECT ID.intItemId
-					, I.intCompanyLocationId
-					, dblCommitted = SUM(dbo.fnICConvertUOMtoStockUnit(ID.intItemId, ID.intItemUOMId, ID.dblQtyShipped))
+				 , I.intCompanyLocationId
+				 , ID.intSubLocationId
+				 , ID.intStorageLocationId
+				 , dblCommitted = SUM(dbo.fnICConvertUOMtoStockUnit(ID.intItemId, ID.intItemUOMId, ID.dblQtyShipped))
 			FROM tblARInvoiceDetail ID
 				INNER JOIN tblARInvoice I ON ID.intInvoiceId = I.intInvoiceId
 			WHERE dbo.fnIsStockTrackingItem(ID.intItemId) = 1
@@ -111,14 +119,15 @@ FROM (
 				AND I.strTransactionType NOT IN ('Service Charge', 'Credit Memo')
 				AND ISNULL(ID.intInventoryShipmentItemId, 0) = 0
 				AND ISNULL(ID.intSalesOrderDetailId, 0) = 0
-			GROUP BY ID.intItemId, I.intCompanyLocationId) TBL
-		GROUP BY intItemId, intCompanyLocationId
+			GROUP BY ID.intItemId, I.intCompanyLocationId, ID.intSubLocationId, ID.intStorageLocationId) TBL
+		GROUP BY intItemId, intCompanyLocationId, intSubLocationId, intStorageLocationId
 	) AS SO
 	LEFT JOIN tblICItemLocation IIL ON SO.intItemId = IIL.intItemId AND SO.intCompanyLocationId = IIL.intLocationId
 ) SALES
 WHERE tblICItemStockUOM.intItemId = SALES.intItemId
   AND tblICItemStockUOM.intItemLocationId = SALES.intItemLocationId
-
+  AND ISNULL(tblICItemStockUOM.intSubLocationId, 0) = ISNULL(SALES.intSubLocationId, 0)
+  AND ISNULL(tblICItemStockUOM.intStorageLocationId, 0) = ISNULL(SALES.intStorageLocationId, 0)
   	
 GO
 print('/*******************  END Update tblICItemStockUOM.dblOrderCommitted  *******************/')
