15a665fef4214741688c3f9f4495122e8df5c7d3
jayson.ramos <jayson.ramos@irely.com>
2020-07-09 13:20:06 +0800

    CT-4964
    (cherry picked from commit 5e1eae06e67694d987b1344ecf970ea9145da6c3)
    (cherry picked from commit f78d9ca8ba5c68c65e6b3e4064124ff51ea97eca)

--- i21Database/dbo/Stored Procedures/uspCTCreateVoucherInvoiceForPartialPricing.sql
+++ i21Database/dbo/Stored Procedures/uspCTCreateVoucherInvoiceForPartialPricing.sql
@@ -1502,7 +1502,14 @@ BEGIN TRY
 									@intEntityId		=	@intUserId, 
 									@dblQtyShipped		=	@dblQuantityForInvoice
 
-							select top 1 @ContractPriceItemUOMId = intItemUOMId from tblICItemUOM where intItemId = @ContractDetailItemId and intUnitMeasureId = @ContractPriceUnitMeasureId;
+
+							--select top 1 @ContractPriceItemUOMId = intItemUOMId from tblICItemUOM where intItemId = @ContractDetailItemId and intUnitMeasureId = @ContractPriceUnitMeasureId;
+							select top 1 @ContractPriceItemUOMId = a.intItemUOMId
+							from tblICItemUOM a 
+							JOIN tblICCommodityUnitMeasure	PU	ON	PU.intCommodityUnitMeasureId	=	@ContractPriceUnitMeasureId
+							JOIN tblICUnitMeasure			PM	ON	PM.intUnitMeasureId				=	PU.intUnitMeasureId
+							where a.intItemId = @ContractDetailItemId and a.intUnitMeasureId = PM.intUnitMeasureId;
+
 							set @dblFinalPrice = dbo.fnCTConvertQtyToTargetItemUOM(@intItemUOMId,@ContractPriceItemUOMId,@dblFinalPrice);
 
 							EXEC	uspARUpdateInvoicePrice 
@@ -2020,7 +2027,13 @@ BEGIN TRY
 										@intEntityId		=	@intUserId, 
 										@dblQtyShipped		=	@dblQuantityForInvoice
 
-							select top 1 @ContractPriceItemUOMId = intItemUOMId from tblICItemUOM where intItemId = @ContractDetailItemId and intUnitMeasureId = @ContractPriceUnitMeasureId;
+							--select top 1 @ContractPriceItemUOMId = intItemUOMId from tblICItemUOM where intItemId = @ContractDetailItemId and intUnitMeasureId = @ContractPriceUnitMeasureId;
+							select top 1 @ContractPriceItemUOMId = a.intItemUOMId
+							from tblICItemUOM a 
+							JOIN tblICCommodityUnitMeasure	PU	ON	PU.intCommodityUnitMeasureId	=	@ContractPriceUnitMeasureId
+							JOIN tblICUnitMeasure			PM	ON	PM.intUnitMeasureId				=	PU.intUnitMeasureId
+							where a.intItemId = @ContractDetailItemId and a.intUnitMeasureId = PM.intUnitMeasureId;
+
 							set @dblFinalPrice = dbo.fnCTConvertQtyToTargetItemUOM(@intItemUOMId,@ContractPriceItemUOMId,@dblFinalPrice);
 
 								EXEC	uspARUpdateInvoicePrice 
