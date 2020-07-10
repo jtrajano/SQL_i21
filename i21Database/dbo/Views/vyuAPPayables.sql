85a321ebad01ddcf3c1b79e37a6a7e7ac5b48a7e
Jeff Trajano <jeffrey.trajano@irely.com>
2020-07-10 21:30:55 +0800

    GL-7716 SQL2008R2 FW Server Deployment error Build

--- i21Database/dbo/Views/vyuAPPayables.sql
+++ i21Database/dbo/Views/vyuAPPayables.sql
@@ -3,7 +3,7 @@
 	Note: Origin transaction do not have multi currency implementation, also to handle issue (see 792717-000, CISCO transaction of COPP)
 	Note: Handle negative quantity received
 */
-CREATE VIEW dbo.vyuAPPayables
+CREATE VIEW vyuAPPayables
 --WITH SCHEMABINDING
 AS 
 SELECT 
