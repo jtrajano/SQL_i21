083a04102bbbdd3a8e67373a91c739142e666778
Jeff Trajano <jeffrey.trajano@irely.com>
2020-07-10 16:13:39 +0800

    GL-7716 SQL2008R2 FW Server Deployment error Build:

--- i21Database/Scripts/Post-Deployment/PostDeployment.sql
+++ i21Database/Scripts/Post-Deployment/PostDeployment.sql
@@ -234,7 +234,6 @@ print 'BEGIN POST DEPLOYMENT'
 :r .\CM\3_PopulateSourceSystemData.sql
 :r .\CM\4_DataFix.sql
 :r .\CM\5_UndepositedFund.sql
-:r .\CM\6_InsertDefaultDataSync.sql
 :r .\CM\7_AlterCashProjectionView.sql
 
 --Accounts Receivable
