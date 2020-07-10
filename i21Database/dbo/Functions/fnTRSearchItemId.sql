d5172924a4b323cf451b0c2e1ab23be8d5478d3e
cris.lazaga <cris.lazaga@irely.com>
2020-07-10 10:11:19 +0800

    TR-1506

--- i21Database/dbo/Functions/fnTRSearchItemId.sql
+++ i21Database/dbo/Functions/fnTRSearchItemId.sql
@@ -9,33 +9,54 @@ RETURNS INT
 AS
 
 BEGIN
-	DECLARE @Location NVARCHAR(50)
-		, @Supplier NVARCHAR(50)
-		, @ItemNo NVARCHAR(50)
-		, @Id INT
+	DECLARE @Id INT = NULL
 
-	SELECT TOP 1 @Location = SearchValue.strLocation
-		, @Supplier = SearchValue.strSupplier
-		, @ItemNo = SearchValue.strItemNo
-	FROM vyuTRGetSupplyPointSearchValue SearchValue
-	CROSS APPLY (
-		SELECT Total.strLocation, Total.strSupplier, Total.strItemNo, dblTotal = COUNT(*) FROM vyuTRGetSupplyPointSearchValue Total
-		WHERE SearchValue.strLocation = Total.strLocation
-			AND SearchValue.strSupplier = Total.strSupplier
-			AND SearchValue.strItemNo = Total.strItemNo
-		GROUP BY Total.strLocation, Total.strSupplier, Total.strItemNo
-	) Total
-	WHERE @SupplierName LIKE '%' + SearchValue.strSearchValue + '%'
-			OR @SupplyPoint LIKE '%' + SearchValue.strSearchValue + '%'
-			OR @Item LIKE '%' + SearchValue.strSearchValue + '%'
-	GROUP BY SearchValue.strLocation, SearchValue.strSupplier, SearchValue.strItemNo, Total.dblTotal
-	HAVING COUNT(*) = Total.dblTotal
-	ORDER BY Total.dblTotal DESC
+	SELECT TOP 1  @Id = D.intSupplyPointProductSearchHeaderId 
+	FROM tblTRSupplyPointProductSearchDetail D
+	INNER JOIN tblTRSupplyPointProductSearchHeader H ON H.intSupplyPointProductSearchHeaderId = D.intSupplyPointProductSearchHeaderId 
+	INNER JOIN tblTRSupplyPoint SP ON SP.intSupplyPointId = H.intSupplyPointId
+	INNER JOIN tblEMEntity E ON E.intEntityId = SP.intEntityVendorId
+	INNER JOIN tblEMEntityLocation EL ON EL.intEntityLocationId = SP.intEntityLocationId 
+	INNER JOIN (
+		SELECT H.intSupplyPointProductSearchHeaderId,  Condition = COUNT(H.intSupplyPointProductSearchHeaderId) FROM tblTRSupplyPointProductSearchDetail D
+		INNER JOIN tblTRSupplyPointProductSearchHeader H ON H.intSupplyPointProductSearchHeaderId = D.intSupplyPointProductSearchHeaderId  
+		WHERE strSearchValue LIKE '%' + @SupplierName + '%'
+		OR strSearchValue LIKE '%' + @SupplyPoint + '%'
+		OR strSearchValue LIKE '%' + @Item + '%'
+		GROUP BY  H.intSupplyPointProductSearchHeaderId
+	) A ON A.intSupplyPointProductSearchHeaderId = H.intSupplyPointProductSearchHeaderId
+	WHERE strSearchValue LIKE '%' + @SupplierName + '%'
+		OR strSearchValue LIKE '%' + @SupplyPoint + '%'
+		OR strSearchValue LIKE '%' + @Item + '%'
+	ORDER BY A.Condition DESC, H.intSupplyPointProductSearchHeaderId ASC
 
-	SELECT @Id = intKeyId FROM vyuTRGetSupplyPointSearchValue
-	WHERE @Location = strLocation
-		AND @Supplier = strSupplier
-		AND @ItemNo = strItemNo
+	-- DECLARE @Location NVARCHAR(50)
+	-- 	, @Supplier NVARCHAR(50)
+	-- 	, @ItemNo NVARCHAR(50)
+	-- 	, @Id INT
+
+	-- SELECT TOP 1 @Location = SearchValue.strLocation
+	-- 	, @Supplier = SearchValue.strSupplier
+	-- 	, @ItemNo = SearchValue.strItemNo
+	-- FROM vyuTRGetSupplyPointSearchValue SearchValue
+	-- CROSS APPLY (
+	-- 	SELECT Total.strLocation, Total.strSupplier, Total.strItemNo, dblTotal = COUNT(*) FROM vyuTRGetSupplyPointSearchValue Total
+	-- 	WHERE SearchValue.strLocation = Total.strLocation
+	-- 		AND SearchValue.strSupplier = Total.strSupplier
+	-- 		AND SearchValue.strItemNo = Total.strItemNo
+	-- 	GROUP BY Total.strLocation, Total.strSupplier, Total.strItemNo
+	-- ) Total
+	-- WHERE @SupplierName LIKE '%' + SearchValue.strSearchValue + '%'
+	-- 		OR @SupplyPoint LIKE '%' + SearchValue.strSearchValue + '%'
+	-- 		OR @Item LIKE '%' + SearchValue.strSearchValue + '%'
+	-- GROUP BY SearchValue.strLocation, SearchValue.strSupplier, SearchValue.strItemNo, Total.dblTotal
+	-- HAVING COUNT(*) = Total.dblTotal
+	-- ORDER BY Total.dblTotal DESC
+
+	-- SELECT @Id = intKeyId FROM vyuTRGetSupplyPointSearchValue
+	-- WHERE @Location = strLocation
+	-- 	AND @Supplier = strSupplier
+	-- 	AND @ItemNo = strItemNo
 
 	RETURN @Id
 END
