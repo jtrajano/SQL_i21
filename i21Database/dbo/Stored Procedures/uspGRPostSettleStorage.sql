8b26021a6811ef000b94762f1ab1e5f31a2cd89c
Leslie Guevarra <leslie.arat@irely.com>
2020-07-11 20:10:33 +0800

    GRN-2258 (Call uspAPUpdateCost after creating taxes.)

--- i21Database/dbo/Stored Procedures/uspGRPostSettleStorage.sql
+++ i21Database/dbo/Stored Procedures/uspGRPostSettleStorage.sql
@@ -2526,7 +2526,7 @@ BEGIN TRY
 							update @avqty set ysnApplied = 1, intBillDetailId = @cur_bid where id = @cur_id
 						end
 
-						EXEC [uspAPUpdateVoucherDetailTax] @detailCreated
+						--EXEC [uspAPUpdateVoucherDetailTax] @detailCreated
 
 					end
 
