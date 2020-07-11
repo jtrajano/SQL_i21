d812d2d594dc694d9bc5798e9400346cd0241e40
Erick Dela Cruz <erick.delacruz@irely.com>
2020-07-11 11:49:04 +0800

    AP-8172
    # Conflicts:
    #	i21Database/Scripts/Post-Deployment/PostDeployment.sql
    #	i21Database/i21Database.sqlproj

--- i21Database/dbo/Stored Procedures/uspAPAddVoucherDetail.sql
+++ i21Database/dbo/Stored Procedures/uspAPAddVoucherDetail.sql
@@ -236,6 +236,7 @@ SELECT TOP 100 PERCENT
 	,dblBundleTotal						=	0
 	,dblQtyBundleReceived				=	0
 	,dblBundleUnitQty					=	0
+	,intFreightTermId					=	A.intFreightTermId
 	,ysnStage							=	A.ysnStage
 INTO #tmpVoucherPayableData
 FROM @voucherDetails A
@@ -438,7 +439,8 @@ INSERT
 	,intItemBundleId					
 	,dblBundleTotal						
 	,dblQtyBundleReceived				
-	,dblBundleUnitQty			
+	,dblBundleUnitQty		
+	,intFreightTermId	
 	,ysnStage		
 )
 VALUES
@@ -528,6 +530,7 @@ VALUES
 	,dblBundleTotal						
 	,dblQtyBundleReceived				
 	,dblBundleUnitQty		
+	,intFreightTermId
 	,ysnStage
 )
 OUTPUT inserted.intBillDetailId, SourceData.intVoucherPayableId INTO @voucherDetailsInfo;
