93eae20205df69e8e5efd3a707f83d9dc3976625
Leslie Guevarra <leslie.arat@irely.com>
2020-07-13 15:58:43 +0800

    GRN-2257 (CLONE - HDTN-247851 - John Winter contract 3608)

--- i21Database/dbo/Views/vyuGRGetStorageTickets.sql
+++ i21Database/dbo/Views/vyuGRGetStorageTickets.sql
@@ -20,9 +20,9 @@ SELECT DISTINCT
     ,strDPARecieptNumber				= ISNULL(CS.strDPARecieptNumber,'')
     ,dblOpenBalance						= ROUND(dbo.fnCTConvertQtyToTargetItemUOM(CS.intItemUOMId, ItemUOM.intItemUOMId, CS.dblOpenBalance), 6)
     ,ysnDPOwnedType						= ST.ysnDPOwnedType
-    ,intContractHeaderId                = case when (ST.ysnDPOwnedType = 1 and GHistory.intContractHeaderId is not null) then GHistory.intContractHeaderId end
-    ,intContractDetailId				= case when (ST.ysnDPOwnedType = 1 and GHistory.intContractDetailId is not null) then GHistory.intContractDetailId  end
-    ,strContractNumber					= case when (ST.ysnDPOwnedType = 1 and GHistory.intContractHeaderId is not null) then GHistory.strContractNumber end
+    ,intContractHeaderId                = case when (ST.ysnDPOwnedType = 1 and GHistory.intContractHeaderId is not null) then GHistory.intContractHeaderId else CASE WHEN CS.ysnTransferStorage = 0 THEN CH_Ticket.intContractHeaderId ELSE CH_Transfer.intContractHeaderId END end
+    ,intContractDetailId				= case when (ST.ysnDPOwnedType = 1 and GHistory.intContractDetailId is not null) then GHistory.intContractDetailId else CASE WHEN CS.ysnTransferStorage = 0 THEN CD_Ticket.intContractDetailId ELSE CD_Transfer.intContractDetailId END end
+    ,strContractNumber					= case when (ST.ysnDPOwnedType = 1 and GHistory.intContractHeaderId is not null) then GHistory.strContractNumber else CASE WHEN CS.ysnTransferStorage = 0 THEN CH_Ticket.strContractNumber ELSE CH_Transfer.strContractNumber END end
     ,intTicketId						= ISNULL(SC.intTicketId,0)
     ,dblDiscountUnPaid					= ISNULL(dblDiscountsDue,0) - ISNULL(dblDiscountsPaid,0)
     ,dblStorageUnPaid					= ISNULL(dblStorageDue,0) - ISNULL(dblStoragePaid,0)
@@ -71,16 +71,23 @@ LEFT JOIN
 		ON GCH.intContractHeaderId = GCD.intContractHeaderId
 	WHERE GSH.strType IN ('From Transfer','From Delivery Sheet','From Scale')
 )GHistory
-    on GHistory.intCustomerStorageId = CS.intCustomerStorageId 
-        --and CS.intStorageTypeId = 2
+    on GHistory.intCustomerStorageId = CS.intCustomerStorageId
 		AND ST.ysnDPOwnedType = 1 
-LEFT JOIN tblSCTicket SC
+LEFT JOIN (
+	tblSCTicket SC 		
+	LEFT JOIN tblGRStorageHistory SH
+		ON SH.intTicketId = SC.intTicketId
+	LEFT JOIN tblICInventoryReceiptItem IRI
+		ON IRI.intInventoryReceiptId = SH.intInventoryReceiptId
+	) 
 	ON SC.intTicketId = CS.intTicketId
---LEFT JOIN tblCTContractDetail CD_Ticket
---    ON CD_Ticket.intContractDetailId = SC.intContractId
---		AND CS.ysnTransferStorage = 0
---LEFT JOIN tblCTContractHeader CH_Ticket 
---    ON CH_Ticket.intContractHeaderId = CD_Ticket.intContractHeaderId
+		AND SH.intCustomerStorageId = CS.intCustomerStorageId
+LEFT JOIN tblCTContractDetail CD_Ticket
+    --ON CD_Ticket.intContractDetailId = SC.intContractId
+	ON CD_Ticket.intContractDetailId = IRI.intContractDetailId
+		AND CS.ysnTransferStorage = 0
+LEFT JOIN tblCTContractHeader CH_Ticket 
+    ON CH_Ticket.intContractHeaderId = CD_Ticket.intContractHeaderId
 LEFT JOIN tblGRTransferStorageSplit TSS
 	ON TSS.intTransferToCustomerStorageId = CS.intCustomerStorageId
 LEFT JOIN tblCTContractDetail CD_Transfer
