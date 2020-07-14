8e04c55429cad7228059ad66c6f584a7dbab573d
Kevin Estrada <kevin.estrada@irely.com>
2020-07-14 17:56:37 +0800

    SC-3721

--- i21Database/dbo/Views/vyuSCDiscountScheduleAuditRecords.sql
+++ i21Database/dbo/Views/vyuSCDiscountScheduleAuditRecords.sql
@@ -6,17 +6,31 @@ AS (
 SELECT DISTINCT    A.intLogId, B.intRecordId, A.dtmDate from tblSMLog A  
 INNER JOIN tblSMAudit E ON E.intLogId  = A.intLogId  
 INNER JOIN tblSMTransaction B ON B.intTransactionId = A.intTransactionId 
-INNER JOIN tblGRDiscountId F on F.intDiscountId = B.intRecordId
+INNER JOIN tblGRDiscountSchedule F on F.intDiscountScheduleId = B.intRecordId
 INNER JOIN tblSMScreen C ON C.intScreenId = B.intScreenId  
-WHERE C.strNamespace = 'Grain.view.DiscountTable' 
+WHERE C.strNamespace = 'Grain.view.DiscountSchedule' 
+
 AND F.ysnSent = 0 
 AND E.intParentAuditId IS NULL  
 AND E.strAction IN ('Created','Updated')   
 
 --order by A.dtmDate asc  
-)
-   
+), secondLayer AS
+(
 SELECT intRecordId, intLogId, dtmDate  FROM cte 
+UNION
+SELECT DISTINCT  F.intDiscountScheduleId as 'intRecordId' ,A.intLogId, A.dtmDate from tblSMLog A  
+INNER JOIN tblSMAudit E ON E.intLogId  = A.intLogId  
+INNER JOIN tblSMTransaction B ON B.intTransactionId = A.intTransactionId
+INNER JOIN tblGRDiscountScheduleCode F ON F.intDiscountScheduleCodeId = B.intRecordId
+INNER JOIN tblGRDiscountSchedule G ON G.intDiscountScheduleId = F.intDiscountScheduleId
+INNER JOIN tblSMScreen C ON C.intScreenId = B.intScreenId  
+WHERE C.strNamespace = 'Grain.view.DiscountCode' 
+AND G.ysnSent = 0
+)
+
+   
+SELECT intRecordId, intLogId, dtmDate  FROM secondLayer 
 WHERE   
 intRecordId  
 NOT IN  
@@ -25,10 +39,14 @@ NOT IN
   
 SELECT DISTINCT B.intRecordId FROM tblSMLog A  
 INNER JOIN tblSMTransaction B ON B.intTransactionId = A.intTransactionId
-inner join tblGRDiscountId F on F.intDiscountId = B.intRecordId  
+inner join tblGRDiscountSchedule F on F.intDiscountScheduleId = B.intRecordId  
 INNER JOIN tblSMScreen C ON C.intScreenId = B.intScreenId  
 INNER JOIN tblSMAudit E ON E.intLogId = A.intLogId  
-WHERE C.strNamespace = 'Grain.view.DiscountTable'    
+WHERE C.strNamespace = 'Grain.view.DiscountSchedule'    
 AND E.strAction = 'Deleted' AND E.intParentAuditId IS NULL  
   
-) 
+)
+
+
+  
+ 
