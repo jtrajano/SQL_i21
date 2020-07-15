ca306b80ae68fd90e1a5a18662866e4d7abbed03
lawrence_zabala <lawrence.zabala@irely.com>
2020-07-15 16:37:19 +0800

    CT-4930

--- i21Database/dbo/Stored Procedures/uspCTSaveContract.sql
+++ i21Database/dbo/Stored Procedures/uspCTSaveContract.sql
@@ -163,13 +163,13 @@ BEGIN TRY
 	WHERE	CD.intPricingTypeId	IN (1,6)
 	AND		intContractHeaderId =	@intContractHeaderId
 
-	SELECT	@ErrMsg = COALESCE(@ErrMsg,'') + '#'+ LTRIM(CC.strItemNo)  + '@' + LTRIM(CD.intContractSeq) +'^'+ CD.strItemUOM + '.' +CHAR(13) + CHAR(10) 
+	SELECT	@ErrMsg = COALESCE(@ErrMsg, '') + '#' + LTRIM(CC.strItemNo)  + '@' + LTRIM(CD.intContractSeq) + '^' + CD.strItemUOM + '.' +CHAR(13) + CHAR(10) 
 	FROM	vyuCTContractCostView	CC
 	JOIN	vyuCTContractSequence	CD	ON CD.intContractDetailId	=	CC.intContractDetailId
 	WHERE	NOT EXISTS(SELECT * FROM tblICItemUOM WHERE intItemId = CC.intItemId AND intUnitMeasureId = CD.intUnitMeasureId)
 	AND		CD.intContractHeaderId	=	@intContractHeaderId AND strCostMethod NOT IN ('Amount','Percentage')
 
-	SELECT @ErrMsg = REPLACE(REPLACE(REPLACE(REPLACE(@ErrMsg,'#','Cost item '),'@',' of sequence '),'^',' don''t have '),'.',' UOM configured for it. Configure the UOM to the cost item to proceed with save.')
+	SELECT @ErrMsg = REPLACE(REPLACE(REPLACE(REPLACE(@ErrMsg, '#', 'Cost item '), '@', ' of sequence '), '^', ' is not configured for sequence UOM "'), '.', '". Configure the Cost Item to have the Sequence UOM and try again.')
 
 	IF	@ErrMsg IS NOT NULL
 		RAISERROR (@ErrMsg,18,1,'WITH NOWAIT')
