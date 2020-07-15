2ff3214c11210b0404f8aca8e0df19a40bf70d3d
axel.casauran <axel.casauran@irely.com>
2020-07-15 11:10:40 +0800

    FRD-1585
    (cherry picked from commit 193a51bab5258381fb243c5088f136bb285270c0)

--- i21Database/dbo/Views/vyuFRUserReportRestriction.sql
+++ i21Database/dbo/Views/vyuFRUserReportRestriction.sql
@@ -18,7 +18,7 @@ ROW_NUMBER() OVER (ORDER BY A.intReportId asc) rowId,
         WHEN R.ysnRestriction = 1 THEN 'No Access' 
         WHEN R.ysnRestriction = 0 THEN 'Read-Only' 
         ELSE '' 
-    END strRestriction,
+    END COLLATE Latin1_General_CI_AS as strRestriction,
 	ISNULL(R.intConcurrencyId,0) intConcurrencyId,
 	CASE WHEN intRestrictionId IS NOT null THEN CAST( 1 AS BIT) ELSE CAST( 0 AS BIT) END
 	ysnRestricted
