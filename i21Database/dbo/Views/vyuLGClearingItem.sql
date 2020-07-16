6620b9704e8ca2f6d0a68e26fdb99711360e122e
Arman Arenas <arman.arenas@irely.com>
2020-07-16 22:04:45 +0800

    LG-2629 Added vyuLGClearingItem and vyuLGClearingItemGL - 19.2

---
+++ i21Database/dbo/Views/vyuLGClearingItem.sql
@@ -0,0 +1,70 @@
+﻿CREATE VIEW [dbo].[vyuLGClearingItem]
+AS
+SELECT
+	[strLoadNumber] = L.strLoadNumber
+	,[intLoadId] = L.intLoadId
+	,[dtmDate] = DATEADD(dd, DATEDIFF(dd, 0, L.dtmScheduledDate), 0)
+	,[intLoadDetailId] = LD.intLoadDetailId
+	,[intUnitMeasureId] = ISNULL(L.intWeightUnitMeasureId, LDUM.intUnitMeasureId)
+	,[strUnitMeasure] = ISNULL(UM.strUnitMeasure, LDUM.strUnitMeasure)
+	,[intItemId] = LD.intItemId
+	,[dblQty] = LD.dblNet
+	,[dblAmount] = ROUND((CASE WHEN intPurchaseSale = 3 THEN COALESCE(AD.dblSeqPrice, dbo.fnCTGetSequencePrice(AD.intContractDetailId, NULL), 0) 
+						ELSE COALESCE(LD.dblUnitPrice, AD.dblSeqPrice, dbo.fnCTGetSequencePrice(AD.intContractDetailId, NULL), 0) END)
+					/ CASE WHEN (AD.ysnSeqSubCurrency = 1) THEN 100 ELSE 1 END
+					* dbo.fnCalculateQtyBetweenUOM(ISNULL(LD.intWeightItemUOMId, LD.intItemUOMId), ISNULL(LD.intPriceUOMId, AD.intSeqPriceUOMId), LD.dblNet), 2)
+	,[dblAmountForeign] = CASE WHEN AD.ysnValidFX = 1 THEN 
+							ROUND(dbo.fnMultiply(
+								(CASE WHEN intPurchaseSale = 3 THEN COALESCE(AD.dblSeqPrice, dbo.fnCTGetSequencePrice(AD.intContractDetailId, NULL), 0) 
+									ELSE COALESCE(LD.dblUnitPrice, AD.dblSeqPrice, dbo.fnCTGetSequencePrice(AD.intContractDetailId, NULL), 0) END)
+								/ CASE WHEN (AD.ysnSeqSubCurrency = 1) THEN 100 ELSE 1 END
+								* dbo.fnCalculateQtyBetweenUOM(ISNULL(LD.intWeightItemUOMId, LD.intItemUOMId), ISNULL(LD.intPriceUOMId, AD.intSeqPriceUOMId), LD.dblNet)
+								,CASE 
+									WHEN AD.ysnValidFX = 1 THEN 
+										CASE WHEN (ISNULL(SC.intMainCurrencyId, SC.intCurrencyID) <> DC.intDefaultCurrencyId AND CD.intInvoiceCurrencyId <> DC.intDefaultCurrencyId)
+												THEN ISNULL(FX.dblFXRate, 1)
+											ELSE 1 END
+									ELSE
+										CASE WHEN (DC.intDefaultCurrencyId <> ISNULL(SC.intMainCurrencyId, SC.intCurrencyID)) 
+											THEN ISNULL(FX.dblFXRate, 1)
+											ELSE 1 END
+									END), 2)
+							ELSE 
+								CONVERT(NUMERIC(18, 6), 0)
+							END
+	,[dblTaxForeign] = CONVERT(NUMERIC(18, 6), 0)
+	,[intLocationId] = CL.intCompanyLocationId
+	,[strLocationName] = CL.strLocationName 
+	,[intAccountId] = apClearing.intAccountId
+	,[strAccountId] = apClearing.strAccountId
+FROM 
+	tblLGLoad L
+	LEFT JOIN tblLGLoadDetail LD ON LD.intLoadId = L.intLoadId
+	LEFT JOIN tblCTContractDetail CD ON CD.intContractDetailId = LD.intPContractDetailId
+	LEFT JOIN vyuLGAdditionalColumnForContractDetailView AD ON AD.intContractDetailId = LD.intPContractDetailId
+	LEFT JOIN tblSMCurrency SC ON SC.intCurrencyID = CD.intCurrencyId
+	LEFT JOIN tblICItemUOM LDUOM ON LDUOM.intItemUOMId = ISNULL(LD.intWeightItemUOMId, LD.intItemUOMId)
+	LEFT JOIN tblICUnitMeasure LDUM ON LDUM.intUnitMeasureId = LDUOM.intUnitMeasureId
+	LEFT JOIN tblICItem I ON I.intItemId = LD.intItemId
+	LEFT JOIN tblICUnitMeasure UM ON UM.intUnitMeasureId = L.intWeightUnitMeasureId
+	LEFT JOIN tblSMCompanyLocation CL ON CL.intCompanyLocationId = LD.intPCompanyLocationId
+	LEFT JOIN tblICItemLocation ItemLoc ON ItemLoc.intItemId = LD.intItemId and ItemLoc.intLocationId = LD.intPCompanyLocationId
+	OUTER APPLY dbo.fnGetItemGLAccountAsTable(LD.intItemId, ItemLoc.intItemLocationId, 'AP Clearing') itemAccnt
+	LEFT JOIN dbo.tblGLAccount apClearing ON apClearing.intAccountId = itemAccnt.intAccountId
+	OUTER APPLY (SELECT TOP 1 intDefaultCurrencyId FROM tblSMCompanyPreference) DC
+	OUTER APPLY (SELECT	TOP 1  
+						intForexRateTypeId = RD.intRateTypeId
+						,dblFXRate = CASE WHEN ER.intFromCurrencyId = DC.intDefaultCurrencyId
+									THEN 1/RD.[dblRate] 
+									ELSE RD.[dblRate] END 
+						FROM tblSMCurrencyExchangeRate ER
+						JOIN tblSMCurrencyExchangeRateDetail RD ON RD.intCurrencyExchangeRateId = ER.intCurrencyExchangeRateId
+						WHERE DC.intDefaultCurrencyId <> ISNULL(SC.intMainCurrencyId, SC.intCurrencyID)
+							AND ((ER.intFromCurrencyId = ISNULL(SC.intMainCurrencyId, SC.intCurrencyID) AND ER.intToCurrencyId = DC.intDefaultCurrencyId) 
+								OR (ER.intFromCurrencyId = DC.intDefaultCurrencyId AND ER.intToCurrencyId = ISNULL(SC.intMainCurrencyId, SC.intCurrencyID)))
+						ORDER BY RD.dtmValidFromDate DESC) FX
+WHERE L.ysnPosted = 1 AND ISNULL(L.ysnCancelled, 0) = 0
+	AND (L.intFreightTermId IS NULL OR L.intFreightTermId IN (SELECT intFreightTermId FROM tblSMFreightTerms WHERE strFobPoint = 'Origin'))
+	AND L.strBatchId IS NOT NULL
+
+GO
---
+++ i21Database/dbo/Views/vyuLGClearingItemGL.sql
@@ -0,0 +1,56 @@
+﻿CREATE VIEW [dbo].[vyuLGClearingItemGL]
+AS
+SELECT
+	[strLoadNumber] = L.strLoadNumber
+	,[intLoadId] = L.intLoadId
+	,[dtmDate] = GLD.dtmDate
+	,[intLoadDetailId] = LD.intLoadDetailId
+	,[intUnitMeasureId] = ISNULL(L.intWeightUnitMeasureId, LDUM.intUnitMeasureId)
+	,[strUnitMeasure] = ISNULL(UM.strUnitMeasure, LDUM.strUnitMeasure)
+	,[intItemId] = LD.intItemId
+	,[dblQty] = LD.dblQty
+	,[dblAmount] = GLD.dblClearingAmount
+	,[dblAmountForeign] = GLD.dblClearingAmountForeign
+	,[dblTaxForeign] = CONVERT(NUMERIC(18, 6), 0)
+	,[intLocationId] = CL.intCompanyLocationId
+	,[strLocationName] = CL.strLocationName 
+	,[intAccountId] = GLD.intAccountId
+	,[strAccountId] = GLA.strAccountId
+FROM 
+	(SELECT 
+		intTransactionId
+		,intAccountId
+		,dtmDate = dtmTransactionDate
+		,dblClearingAmount = dblDebit - dblCredit
+		,dblClearingAmountForeign = dblDebitForeign - dblCreditForeign
+		,intLineNo = DENSE_RANK() OVER (PARTITION BY intTransactionId, intAccountId ORDER BY intJournalLineNo)
+		FROM tblGLDetail
+		WHERE strModuleName = 'Logistics'
+		 AND intAccountId IN 
+			(SELECT intAccountId FROM tblGLAccountSegmentMapping WHERE intAccountSegmentId IN 
+				(SELECT intAccountSegmentId FROM tblGLAccountSegment WHERE intAccountCategoryId IN (
+					SELECT intAccountCategoryId FROM tblGLAccountCategory WHERE strAccountCategory = 'AP Clearing')))
+					AND ysnIsUnposted = 0) GLD
+	INNER JOIN tblGLAccount GLA ON GLA.intAccountId = GLD.intAccountId
+	INNER JOIN tblLGLoad L ON L.intLoadId = GLD.intTransactionId
+	OUTER APPLY (SELECT * FROM
+					(SELECT 
+						intLoadDetailId
+						,intLoadCostId = NULL
+						,intVendorId = intVendorEntityId
+						,intItemId 
+						,dblQty = dblNet
+						,intWeightUOMId = ISNULL(LD1.intWeightItemUOMId, LD1.intItemUOMId)
+						,intCompanyLocationId = intPCompanyLocationId
+						,intLineNo = DENSE_RANK() OVER (PARTITION BY intLoadId ORDER BY intLoadDetailId)
+					 FROM tblLGLoadDetail LD1
+					 WHERE LD1.intLoadId = L.intLoadId) LD2
+				 WHERE GLD.intLineNo  = LD2.intLineNo) LD
+	LEFT JOIN tblICItem I ON I.intItemId = LD.intItemId
+	LEFT JOIN tblICUnitMeasure UM ON UM.intUnitMeasureId = L.intWeightUnitMeasureId
+	LEFT JOIN tblICItemUOM LDUOM ON LDUOM.intItemUOMId = LD.intWeightUOMId
+	LEFT JOIN tblICUnitMeasure LDUM ON LDUM.intUnitMeasureId = LDUOM.intUnitMeasureId
+	LEFT JOIN tblSMCompanyLocation CL ON CL.intCompanyLocationId = LD.intCompanyLocationId
+WHERE ISNULL(L.ysnCancelled, 0) <> 1
+
+GO
