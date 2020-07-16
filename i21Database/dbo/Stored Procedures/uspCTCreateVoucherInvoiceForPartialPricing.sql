119bd3430e4f39a4bc178bb15983aa8e53615078
Dan Jalimeo <dan.jalimao@irely.com>
2020-07-16 14:26:29 +0800

    CT-4977

--- i21Database/dbo/Stored Procedures/uspCTCreateVoucherInvoiceForPartialPricing.sql
+++ i21Database/dbo/Stored Procedures/uspCTCreateVoucherInvoiceForPartialPricing.sql
@@ -596,11 +596,6 @@ BEGIN TRY
 
 							--UPDATE	tblAPBillDetail SET  dblQtyOrdered = @dblQtyToBill, dblQtyReceived = @dblQtyToBill,dblNetWeight = dbo.fnCTConvertQtyToTargetItemUOM(intUnitOfMeasureId, intWeightUOMId, @dblQtyToBill) WHERE intBillDetailId = @intBillDetailId
 
-							IF (ISNULL(@intBillDetailId, 0) <> 0)
-							BEGIN
-								EXEC uspAPUpdateCost @intBillDetailId, @dblFinalPrice, 1
-							END
-
 							-- CT-3983
 							DELETE @detailCreated
 
@@ -617,6 +612,11 @@ BEGIN TRY
 							WHERE APD.intTaxGroupId IS NULL AND intInventoryReceiptChargeId IS NULL
 							
 							EXEC [uspAPUpdateVoucherDetailTax] @detailCreated
+
+							IF (ISNULL(@intBillDetailId, 0) <> 0)
+							BEGIN
+								EXEC uspAPUpdateCost @intBillDetailId, @dblFinalPrice, 1
+							END
 							--
 
 							IF ISNULL(@ysnBillPosted,0) = 1
@@ -878,11 +878,6 @@ BEGIN TRY
 					    
 								--UPDATE	tblAPBillDetail SET  dblQtyOrdered = @dblQtyToBill, dblQtyReceived = @dblQtyToBill,dblNetWeight = dbo.fnCTConvertQtyToTargetItemUOM(intUnitOfMeasureId, intWeightUOMId, @dblQtyToBill) WHERE intBillDetailId = @intBillDetailId
 
-								IF (ISNULL(@intBillDetailId, 0) <> 0)
-								BEGIN
-									EXEC uspAPUpdateCost @intBillDetailId, @dblFinalPrice, 1
-								END
-
 								-- CT-3983
 								DELETE FROM @detailCreated
 							
@@ -899,6 +894,11 @@ BEGIN TRY
 								WHERE APD.intTaxGroupId IS NULL AND intInventoryReceiptChargeId IS NULL
 							
 								EXEC [uspAPUpdateVoucherDetailTax] @detailCreated
+
+								IF (ISNULL(@intBillDetailId, 0) <> 0)
+								BEGIN
+									EXEC uspAPUpdateCost @intBillDetailId, @dblFinalPrice, 1
+								END
 								--
 
 								IF ISNULL(@ysnBillPosted,0) = 1
