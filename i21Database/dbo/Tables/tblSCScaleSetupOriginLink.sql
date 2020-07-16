de197ac7fa1a57df1059d7a8dbb11bb3a09c7463
Mon Gonzales <Mon.Gonzales@irely.com>
2020-07-16 08:43:35 +0800

    SC-3730 - SC - Catch Collation Issues in database (tbl)

--- i21Database/dbo/Tables/tblSCScaleSetupOriginLink.sql
+++ i21Database/dbo/Tables/tblSCScaleSetupOriginLink.sql
@@ -2,8 +2,8 @@
 (
 	intScaleSetupOriginLinkId int not null identity(1,1) primary key,
 	intScaleSetupId int not null,
-	strStationShortDescription nvarchar(5) not null,
-	strLinkStationShortDescription nvarchar(5) not null,
+	strStationShortDescription nvarchar(5)  COLLATE Latin1_General_CI_AS not null,
+	strLinkStationShortDescription nvarchar(5)  COLLATE Latin1_General_CI_AS not null,
 	intConcurrencyId int default(1) not null,
 
 	CONSTRAINT [FK_ScaleSetupOriginLink_ScaleSetup] FOREIGN KEY ([intScaleSetupId]) REFERENCES [tblSCScaleSetup](intScaleSetupId),	
