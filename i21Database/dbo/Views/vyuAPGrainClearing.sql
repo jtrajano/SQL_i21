8e7f9df363314d8abaa9738f1157db5e27768fb9
Leslie Guevarra <leslie.arat@irely.com>
2020-07-18 20:24:49 +0800

    GRN-2266 (CLONE - HDTN-247499 - A/P Clearing)

--- i21Database/dbo/Views/vyuAPGrainClearing.sql
+++ i21Database/dbo/Views/vyuAPGrainClearing.sql
@@ -31,6 +31,9 @@ SELECT
 	,GD.intAccountId
 	,AD.strAccountId
 FROM tblGRCustomerStorage CS
+INNER JOIN tblGRStorageType STY
+	ON STY.intStorageScheduleTypeId = CS.intStorageTypeId
+		AND STY.ysnDPOwnedType = 0
 INNER JOIN tblICItem IM
 	ON IM.intItemId = CS.intItemId
 INNER JOIN tblICCommodity CO
@@ -170,6 +173,9 @@ SELECT
 	,GLDetail.intAccountId
 	,GLDetail.strAccountId
 FROM tblGRCustomerStorage CS
+INNER JOIN tblGRStorageType STY
+	ON STY.intStorageScheduleTypeId = CS.intStorageTypeId
+		AND STY.ysnDPOwnedType = 0
 INNER JOIN tblICCommodity CO
 	ON CO.intCommodityId = CS.intCommodityId
 INNER JOIN tblICItem IM
@@ -336,6 +342,9 @@ INNER JOIN tblGRDiscountScheduleCode DSC
 	ON DSC.intDiscountScheduleCodeId = QM.intDiscountScheduleCodeId
 INNER JOIN tblGRCustomerStorage CS
 	ON CS.intCustomerStorageId = QM.intTicketFileId
+INNER JOIN tblGRStorageType STY
+	ON STY.intStorageScheduleTypeId = CS.intStorageTypeId
+		AND STY.ysnDPOwnedType = 0
 INNER JOIN tblICItem IM
 	ON DSC.intItemId = IM.intItemId
 INNER JOIN tblGRDiscountSchedule DS
