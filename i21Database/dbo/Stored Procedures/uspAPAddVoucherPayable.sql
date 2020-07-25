d86b6e683ed23fae4e833bc71a13bd8b10159dde
Erick Dela Cruz <erick.delacruz@irely.com>
2020-07-25 08:14:47 +0800

    AP-8172

--- i21Database/dbo/Stored Procedures/uspAPAddVoucherPayable.sql
+++ i21Database/dbo/Stored Procedures/uspAPAddVoucherPayable.sql
@@ -232,6 +232,7 @@ BEGIN
 														ELSE A.intTermId END
 			,[strTerm]							=	CASE WHEN contractTerm.intTermID IS NOT NULL THEN contractTerm.strTerm
 														ELSE term.strTerm END
+			,[intFreightTermId]					=	A.intFreightTermId
 			,[strBillOfLading]					=	A.strBillOfLading
 			,[int1099Form]						=	CASE 	WHEN patron.intEntityId IS NOT NULL 
 																AND A.intItemId > 0
@@ -377,7 +378,8 @@ BEGIN
 		,[intShipViaId]					
 		,[strShipVia]					
 		,[intTermId]						
-		,[strTerm]						
+		,[strTerm]	
+		,[intFreightTermId]					
 		,[strBillOfLading]
 		,[int1099Form]
 		,[int1099Category]				
@@ -459,7 +461,8 @@ BEGIN
 		,[intShipViaId]					
 		,[strShipVia]					
 		,[intTermId]						
-		,[strTerm]						
+		,[strTerm]		
+		,[intFreightTermId]				
 		,[strBillOfLading]
 		,[int1099Form]
 		,[int1099Category]				
@@ -570,7 +573,7 @@ BEGIN
 		,intCompanyLocationId		= A.intShipToId
 		,intVendorLocationId		= A.intShipFromId
 		,ysnIncludeExemptedCodes	= 1
-		,intFreightTermId			= EL.intFreightTermId
+		,intFreightTermId			= ISNULL(A.intFreightTermId,EL.intFreightTermId)
 		,ysnExcludeCheckOff			= 0
 		,intItemUOMId				= CASE WHEN A.intWeightUOMId > 0 AND A.dblNetWeight > 0
 										THEN A.intWeightUOMId
