b587f92ec1f3a90662bf4613a9a0364b1b3709d4
Von Pasion <von.pasion@irely.com>
2020-07-28 17:55:03 +0800

    FRM-9069

--- i21Database/dbo/Tables/tblSMUserSecurityBookPermission.sql
+++ i21Database/dbo/Tables/tblSMUserSecurityBookPermission.sql
@@ -6,7 +6,8 @@
     [intSubBookId]		INT NULL,    
     [intConcurrencyId]	INT NOT NULL DEFAULT (1), 
     CONSTRAINT [FK_tblSMUserSecurityBookPermission_tblSMUserSecurity_intEntityId] FOREIGN KEY ([intEntityId]) REFERENCES [tblSMUserSecurity]([intEntityId]) ON DELETE CASCADE,
-	CONSTRAINT [FK_tblSMUserSecurityBookPermission_tblCTBook_intBookId] FOREIGN KEY ([intBookId]) REFERENCES [tblCTBook]([intBookId])
+	CONSTRAINT [FK_tblSMUserSecurityBookPermission_tblCTBook_intBookId] FOREIGN KEY ([intBookId]) REFERENCES [tblCTBook]([intBookId]),
+	CONSTRAINT [FK_tblSMUserSecurityBookPermission_tblCTSubBook_intSubBookId] FOREIGN KEY ([intSubBookId]) REFERENCES [tblCTSubBook]([intSubBookId])
 )
 
 GO
