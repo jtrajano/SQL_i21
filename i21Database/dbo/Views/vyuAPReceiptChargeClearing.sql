fdab635b99cfdec22a60b186102bea483b4157cd
Erick Dela Cruz <erick.delacruz@irely.com>
2020-07-28 19:36:49 +0800

    AP-8950

--- i21Database/dbo/Views/vyuAPReceiptChargeClearing.sql
+++ i21Database/dbo/Views/vyuAPReceiptChargeClearing.sql
@@ -23,7 +23,7 @@ SELECT
     ,0 AS dblVoucherQty      
     ,CAST((ISNULL(dblAmount * -1,0) --multiple the amount to reverse if ysnPrice = 1      
         + ISNULL(dblTax,0)) AS DECIMAL (18,2)) AS dblReceiptChargeTotal      
-    ,ISNULL(ReceiptCharge.dblQuantity,0) * -1 AS dblReceiptChargeQty      
+    ,ROUND(ISNULL(ReceiptCharge.dblQuantity,0),2) * -1 AS dblReceiptChargeQty      
     ,Receipt.intLocationId      
     ,compLoc.strLocationName      
     ,CAST(1 AS BIT) ysnAllowVoucher      
@@ -74,7 +74,7 @@ SELECT
     ,0 AS dblVoucherTotal      
     ,0 AS dblVoucherQty      
     ,CAST((ISNULL(dblAmount,0) + ISNULL(dblTax,0)) AS DECIMAL (18,2)) AS dblReceiptChargeTotal      
-    ,ISNULL(ReceiptCharge.dblQuantity,0) AS dblReceiptChargeQty      
+    ,ROUND(ISNULL(ReceiptCharge.dblQuantity,0),2) AS dblReceiptChargeQty      
     ,Receipt.intLocationId      
     ,compLoc.strLocationName      
     ,CAST(1 AS BIT) ysnAllowVoucher     
@@ -127,7 +127,7 @@ SELECT
     ,0 AS dblVoucherTotal      
     ,0 AS dblVoucherQty      
     ,CAST((ISNULL(dblAmount,0) + ISNULL(dblTax,0)) AS DECIMAL (18,2)) AS dblReceiptChargeTotal      
-    ,ISNULL(ReceiptCharge.dblQuantity,0) AS dblReceiptChargeQty      
+    ,ROUND(ISNULL(ReceiptCharge.dblQuantity,0),2) AS dblReceiptChargeQty      
     ,Receipt.intLocationId      
     ,compLoc.strLocationName      
     ,CAST(1 AS BIT) ysnAllowVoucher     
