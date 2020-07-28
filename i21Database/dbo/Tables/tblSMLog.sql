c5aa5bdead02c8de1298582906f9ed17828edd75
IRELY\JMendones <jesmar.mendones@irely.com>
2020-07-28 10:05:13 +0800

    Revert "FRM-7918"
    This reverts commit e6fedea7d59ccf3822313696ce2625eb70a425b5.

--- i21Database/dbo/Tables/tblSMLog.sql
+++ i21Database/dbo/Tables/tblSMLog.sql
@@ -2,13 +2,11 @@
     [intLogId]			 INT              IDENTITY (1, 1) NOT NULL,
     [strType]		     NVARCHAR(100)    COLLATE Latin1_General_CI_AS NULL,
     [dtmDate]			 DATETIME         NOT NULL,
-    [intEntityId]        INT              NOT NULL,
-	[strEntityName]	     NVARCHAR(500)    COLLATE Latin1_General_CI_AS NULL,
-	[strCustomerName]    NVARCHAR(500)    COLLATE Latin1_General_CI_AS NULL,
+    [intEntityId]        INT              NULL,
 	[intTransactionId]	 INT			  NULL,
 	[strRoute]			 NVARCHAR(MAX)	  NULL,
     [intConcurrencyId]   INT              NOT NULL,
 	CONSTRAINT [FK_dbo.tblSMLog_tblSMTransaction] FOREIGN KEY ([intTransactionId]) REFERENCES [tblSMTransaction]([intTransactionId]) ON DELETE CASCADE,
-    --CONSTRAINT [FK_dbo.tblSMLog_dbo.tblEMEntity_intEntityId] FOREIGN KEY ([intEntityId]) REFERENCES [dbo].tblEMEntity ([intEntityId]),
+    CONSTRAINT [FK_dbo.tblSMLog_dbo.tblEMEntity_intEntityId] FOREIGN KEY ([intEntityId]) REFERENCES [dbo].tblEMEntity ([intEntityId]),
     CONSTRAINT [PK_dbo.tblSMLog] PRIMARY KEY CLUSTERED ([intLogId] ASC)
 );
