8c9bd7d7fef1b9277f25f22c7bf6da2fdeb76861
bonn ponce <bonn.ponce@irely.com>
2020-08-04 11:00:35 +0800

    ST-1765

--- i21Database/dbo/Stored Procedures/uspSTGetLotteryCountData.sql
+++ i21Database/dbo/Stored Procedures/uspSTGetLotteryCountData.sql
@@ -4,8 +4,97 @@
 	@checkoutId INT
 AS
 
+DECLARE @tblSTOuputTable TABLE
+(
+	 intBeginCount			INT
+	,dtmSoldDate			DATETIME
+	,strStatus				NVARCHAR(MAX)
+	,ysnBookSoldOut			BIT
+	,intStoreId				INT
+	,intLotteryBookId		INT
+	,intBinNumber			INT
+	,strGame				NVARCHAR(MAX)
+	,intLotteryGameId		INT
+	,strBookNumber			NVARCHAR(MAX)
+	,strCountDirection		NVARCHAR(MAX)
+	,intStartingNumber		INT
+	,dblTicketValue			NUMERIC(18,6)
+	,intTicketPerPack		INT
+	,intEndingNumber		INT
+	,intConcurrencyId		INT
+	,intItemId				INT
+	,strItemDescription		NVARCHAR(MAX)
+	,strItemNo				NVARCHAR(MAX)
+	,strLongUPCCode			NVARCHAR(MAX)
+	,intCategoryId			INT
+	,strCategoryCode		NVARCHAR(MAX)
+	,strCategoryDescription	NVARCHAR(MAX)
+	,intItemUOMId			INT
+	,strSoldOut				NVARCHAR(MAX)
+)
+
+
+SELECT tblSTCheckoutLotteryCount.* INTO #tempLotteryCount
+FROM tblSTCheckoutLotteryCount 
+INNER JOIN tblSTReturnLottery ON tblSTCheckoutLotteryCount.intLotteryBookId = tblSTReturnLottery.intLotteryBookId
+INNER JOIN tblSTLotteryBook ON tblSTCheckoutLotteryCount.intLotteryBookId = tblSTLotteryBook.intLotteryBookId
+WHERE tblSTCheckoutLotteryCount.intCheckoutId = @checkoutId 
+AND LOWER(tblSTLotteryBook.strStatus) = 'returned' AND tblSTReturnLottery.dtmReturnDate != @date
+
+
+INSERT INTO @tblSTOuputTable
+(
+	 intBeginCount			
+	,dtmSoldDate			
+	,strStatus				
+	,ysnBookSoldOut			
+	,intStoreId				
+	,intLotteryBookId		
+	,intBinNumber			
+	,strGame				
+	,intLotteryGameId		
+	,strBookNumber			
+	,strCountDirection		
+	,intStartingNumber		
+	,dblTicketValue			
+	,intTicketPerPack		
+	,intEndingNumber		
+	,intConcurrencyId		
+	,intItemId				
+	,strItemDescription		
+	,strItemNo				
+	,strLongUPCCode			
+	,intCategoryId			
+	,strCategoryCode		
+	,strCategoryDescription	
+	,intItemUOMId			
+	,strSoldOut				
+)
 SELECT 
-*
+ intBeginCount			
+,dtmSoldDate			
+,strStatus				
+,ysnBookSoldOut			
+,intStoreId				
+,intLotteryBookId		
+,intBinNumber			
+,strGame				
+,intLotteryGameId		
+,strBookNumber			
+,strCountDirection		
+,intStartingNumber		
+,dblTicketValue			
+,intTicketPerPack		
+,intEndingNumber		
+,intConcurrencyId		
+,intItemId				
+,strItemDescription		
+,strItemNo				
+,strLongUPCCode			
+,intCategoryId			
+,strCategoryCode		
+,strCategoryDescription	
+,intItemUOMId			
 ,strSoldOut = 
 CASE WHEN LOWER(strStatus) = 'sold' 
 	THEN 'Yes'
@@ -113,13 +202,28 @@ SELECT
 	ON vyuSTItemPricingOnFirstLocation.intItemId = tblSTLotteryGame.intItemId
 	LEFT JOIN tblSTReturnLottery
 	ON tblSTReturnLottery.intLotteryBookId = tblSTLotteryBook.intLotteryBookId
-	WHERE ('active' = LOWER(strStatus)
-	OR ( ('sold' = LOWER(strStatus)) AND (dtmSoldDate = @date)) 
-	OR 'returned' = LOWER(strStatus) AND ( tblSTReturnLottery.dtmReturnDate = @date)) 
-	AND tblSTLotteryBook.intStoreId = @storeId
+	WHERE 
+	tblSTLotteryBook.intStoreId = @storeId 
+	AND ( 
+		'active' = LOWER(strStatus) 
+		OR ( 'sold' = LOWER(strStatus) AND (dtmSoldDate = @date)) 
+		OR ('returned' = LOWER(strStatus) AND  tblSTReturnLottery.dtmReturnDate = @date) 
+		)
+	OR tblSTLotteryBook.intLotteryBookId IN (SELECT intLotteryBookId FROM #tempLotteryCount)
 	
 
 ) as tblSTCompileData
 ) as tblSTCompileData1
 ORDER BY 
 intBinNumber ASC
+
+
+UPDATE @tblSTOuputTable 
+SET 
+	 intEndingNumber = [#tempLotteryCount].intLotteryBookId
+,strSoldOut = [#tempLotteryCount].strSoldOut
+FROM #tempLotteryCount
+WHERE [#tempLotteryCount].intLotteryBookId = [@tblSTOuputTable].intLotteryBookId
+
+
+SELECT * FROM @tblSTOuputTable
