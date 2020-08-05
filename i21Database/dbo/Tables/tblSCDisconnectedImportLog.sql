0ee63932123e615c189ba34527f85c45df5c3560
Kevin Estrada <kevin.estrada@irely.com>
2020-08-05 16:30:44 +0800

    SC-3765
    (cherry picked from commit 5a03e630f06fd1664ab936b02f0f8c773a1b6f26)
    (cherry picked from commit bb00606a2cd666e7e5c15bf6f13e3e38f539e2ef)

--- i21Database/dbo/Tables/tblSCDisconnectedImportLog.sql
+++ i21Database/dbo/Tables/tblSCDisconnectedImportLog.sql
@@ -4,6 +4,9 @@
     [strFileName] NVARCHAR(MAX)  COLLATE Latin1_General_CI_AS  NULL ,
     [intRecordId] INT NULL,
     [dtmDate] DATETIME NOT NULL,
+    [ysnSuccess] BIT NOT NULL DEFAULT(0),
+    [strBatchNo] NVARCHAR(200) NULL,
+    [strMessage] NVARCHAR(MAX) NULL,
     [intConcurrencyId] INT NOT NULL DEFAULT(0)
     CONSTRAINT [PK_dbo.tblSCDisconnectedImportLog] PRIMARY KEY CLUSTERED ([intDisconnectedImportLogId] ASC)
   
