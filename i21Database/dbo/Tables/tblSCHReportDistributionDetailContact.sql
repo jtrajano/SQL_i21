4e0a2af03214e19bea01e67154b0dcf2fbb58341
IRELY\JMendones <jesmar.mendones@irely.com>
2020-08-07 17:54:36 +0800

    SCH-1

---
+++ i21Database/dbo/Tables/tblSCHReportDistributionDetailContact.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[tblSCHReportDistributionDetailContact]
+(
+	[intReportDistributionDetailContactId]	INT				IDENTITY (1, 1) NOT NULL,
+	[intReportDistributionId]				INT				NOT NULL,
+	[intEntityId]							INT				NOT NULL,
+	[strEmail]								[nvarchar](500) COLLATE Latin1_General_CI_AS NULL,
+	[intFilterId]							INT				NULL,
+	[intConcurrencyId]						INT				NOT NULL,
+
+    CONSTRAINT [PK_tblSCHReportDistributionDetailContact] PRIMARY KEY CLUSTERED ([intReportDistributionDetailContactId] ASC),
+    CONSTRAINT [FK_tblSCHReportDistributionDetailContact_tblSCHReportDistribution] FOREIGN KEY (intReportDistributionId) REFERENCES [dbo].[tblSCHReportDistribution] ([intReportDistributionId]) ON DELETE CASCADE
+)
---
+++ i21Database/dbo/Tables/tblSCHReportDistributionGroupContact.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tblSCHReportDistributionGroupContact]
+(
+	[intReportDistributionGroupContactId]				INT IDENTITY (1, 1) NOT NULL,
+	[intReportDistributionGroupId]						[int] NOT NULL,
+	[intEntityId]										[int] NOT NULL,
+	[intFilterId]										[int] NULL,
+	[intConcurrencyId]									[int] DEFAULT 1,
+
+	CONSTRAINT [PK_tblSCHReportDistributionGroupContact] PRIMARY KEY CLUSTERED ([intReportDistributionGroupContactId] ASC),
+    CONSTRAINT [FK_tblSCHReportDistributionGroupContact_tblSCHReportDistributionGroup] FOREIGN KEY ([intReportDistributionGroupId]) REFERENCES [dbo].[tblSCHReportDistributionGroup] ([intReportDistributionGroupId]) ON DELETE CASCADE
+)
