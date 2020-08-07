8daa9110ef0dbfa138bea1ac82973e753ba95861
Jeremiah Villanueva <jeremiah.villanueva@irely.com>
2020-08-07 16:24:03 +0800

    AR-11546

--- i21Database/dbo/Views/vyuARTransactionSummary.sql
+++ i21Database/dbo/Views/vyuARTransactionSummary.sql
@@ -32,4 +32,16 @@ SELECT  intYear 					= YEAR(SAR.dtmDate)
 	  , strAccountStatusCode		= SAR.strAccountStatusCode
 	  , strSource					= SAR.strType
 	  , strLocationName				= strLocationName
+	  , strCompanyName				= COMPANY.strCompanyName
+	  , strCompanyAddress			= COMPANY.strCompanyAddress
+
 FROM vyuARSalesAnalysisReport SAR
+
+ OUTER APPLY (SELECT
+       TOP 1 strCompanyName,
+       strCompanyAddress = dbo.[fnARFormatCustomerAddress]
+       (NULL, NULL, NULL, strAddress, strCity,
+       strState, strZip, strCountry, NULL, 0)
+       COLLATE Latin1_General_CI_AS FROM
+       dbo.tblSMCompanySetup WITH (NOLOCK))
+       COMPANY
