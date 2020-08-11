e9b53392233b71c3f845ce645bd5fa0144445a5f
Jeff Trajano <jeffrey.trajano@irely.com>
2020-08-11 13:22:48 +0800

    GL-7739 CLONE - BI - Gross Margin Changes

--- i21Database/Scripts/Post-Deployment/PostDeployment.sql
+++ i21Database/Scripts/Post-Deployment/PostDeployment.sql
@@ -269,6 +269,7 @@ print 'BEGIN POST DEPLOYMENT'
 :r .\AR\DefaultData\36_UpdateTaxDetailInvalidSetup.sql
 :r .\AR\DefaultData\37_UpdatePOSNewFields.sql
 :r .\AR\DefaultData\99_ReCreateTriggers.sql
+:r .\AR\DefaultData\98_UpdateInvoiceGrossMarginSummary.sql
 
 --Accounts Payable
 --:r .\AP\RestoreVendorId.sql
