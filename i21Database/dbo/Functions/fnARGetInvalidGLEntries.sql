aba2144d102837d41b4f9faa49f5c8211b16fcb4
kelvin_quinto <kelvin.quinto@irely.com>
2020-08-11 20:28:51 +0800

    AR-12224

--- i21Database/dbo/Functions/fnARGetInvalidGLEntries.sql
+++ i21Database/dbo/Functions/fnARGetInvalidGLEntries.sql
@@ -43,7 +43,40 @@ BEGIN
 	) GL ON GL.intTransactionId = ENTRIES.intTransactionId AND GL.strTransactionId = ENTRIES.strTransactionId
 	WHERE ISNULL(I.dblBaseInvoiceTotal, 0) - (ISNULL(ENTRIES.dblAmount, 0) + ISNULL(GL.dblAmount, 0)) <> 0.000000
 	  AND @ysnPost = 1
-	ORDER BY strTransactionId
+	  AND I.strTransactionType <> 'Cash'
+	--ORDER BY strTransactionId
+
+	UNION ALL
+
+	SELECT strTransactionId	= I.strInvoiceNumber
+		 , strText			= I.strInvoiceNumber + ' has discrepancy on Undeposited Funds of ' + LTRIM(STR(ISNULL(I.dblBaseInvoiceTotal, 0) - (ISNULL(ENTRIES.dblAmount, 0) + ISNULL(GL.dblAmount, 0)), 16, 2))
+		 , intErrorCode		= 1
+		 , strModuleName	= ENTRIES.strModuleName
+	FROM dbo.tblARInvoice I
+	INNER JOIN (
+		SELECT intTransactionId		= GLE.intTransactionId
+			 , strTransactionId		= GLE.strTransactionId
+			 , strModuleName		= GLE.strModuleName
+			 , dblAmount			= SUM(dblDebit - dblCredit)
+		FROM @GLEntries GLE
+		INNER JOIN vyuGLAccountDetail GLAD ON GLE.intAccountId = GLAD.intAccountId	
+		WHERE GLAD.strAccountCategory = 'Undeposited Funds'
+		GROUP BY GLE.intTransactionId, GLE.strTransactionId, GLE.strModuleName
+	) ENTRIES ON I.intInvoiceId = ENTRIES.intTransactionId AND I.strInvoiceNumber = ENTRIES.strTransactionId
+	INNER JOIN (
+		SELECT intTransactionId		= GL.intTransactionId
+			 , strTransactionId		= GL.strTransactionId
+			 , dblAmount			= SUM(dblDebit - dblCredit)
+		FROM tblGLDetail GL
+		INNER JOIN vyuGLAccountDetail GLAD ON GL.intAccountId = GLAD.intAccountId
+		WHERE GL.ysnIsUnposted = 0
+		  AND GLAD.strAccountCategory = 'Undeposited Funds'
+		GROUP BY GL.intTransactionId, GL.strTransactionId, GL.strModuleName
+	) GL ON GL.intTransactionId = ENTRIES.intTransactionId AND GL.strTransactionId = ENTRIES.strTransactionId
+	WHERE ISNULL(I.dblBaseInvoiceTotal, 0) - (ISNULL(ENTRIES.dblAmount, 0) + ISNULL(GL.dblAmount, 0)) <> 0.000000
+	  AND @ysnPost = 1
+	  AND I.strTransactionType = 'Cash'
+	--ORDER BY strTransactionId
 
 	RETURN
 		
