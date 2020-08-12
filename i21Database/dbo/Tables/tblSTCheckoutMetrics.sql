b7e91a40734a070a09640311a52295fb4ed829ab
nelsonreyes19 <nelson.reyes@irely.com>
2020-08-12 09:01:09 +0800

    ST-1763

--- i21Database/dbo/Tables/tblSTCheckoutMetrics.sql
+++ i21Database/dbo/Tables/tblSTCheckoutMetrics.sql
@@ -6,10 +6,12 @@
 	[intMetricItemId]											INT												NOT NULL, 
 	[intOffsetItemId]											INT												NOT NULL,  
     [intRegisterImportFieldId]									INT												NOT NULL,  
+	[intDepartmentId] 											INT 												NULL,
     [intConcurrencyId]											INT												NOT NULL,             
     CONSTRAINT [PK_tblSTCheckoutMetrics_intCheckoutMetricsId]						PRIMARY KEY CLUSTERED		([intCheckoutMetricsId]),
 	CONSTRAINT [AK_tblSTCheckoutMetrics_intCheckoutId_strMetricsDescription]		UNIQUE	NONCLUSTERED		([intCheckoutId],[strMetricsDescription]),
     CONSTRAINT [FK_tblSTCheckoutMetrics_tblSTCheckoutHeader]						FOREIGN KEY					([intCheckoutId])			REFERENCES [dbo].[tblSTCheckoutHeader] ([intCheckoutId]) ON DELETE CASCADE,
+	CONSTRAINT [FK_tblSTCheckoutMetrics_tblICCategory]								FOREIGN KEY					([intDepartmentId])			REFERENCES [tblICCategory] ([intCategoryId]),
 	CONSTRAINT [FK_tblSTCheckoutMetrics_tblICItemMetrics]							FOREIGN KEY					([intMetricItemId])			REFERENCES [tblICItem]([intItemId]),
 	CONSTRAINT [FK_tblSTCheckoutMetrics_tblICItemOffset]							FOREIGN KEY					([intOffsetItemId])			REFERENCES [tblICItem]([intItemId])
 );
