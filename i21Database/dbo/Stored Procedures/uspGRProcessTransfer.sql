78874c44c6db959d29e0c6fb5af445372fd2a23c
Leslie Guevarra <leslie.arat@irely.com>
2020-08-13 09:31:02 +0800

    Revert "GRN--2300 (HDTN-249417 - Transfer From PLA (19.1))"
    This reverts commit ac2006cc2c5ffe59438674b9644e217b74dcb75a.

--- i21Database/dbo/Stored Procedures/uspGRProcessTransfer.sql
+++ i21Database/dbo/Stored Procedures/uspGRProcessTransfer.sql
@@ -89,7 +89,7 @@ BEGIN
 			,intCustomerStorageId
 		) AS (
 			SELECT intTransferContractDetailId	= SourceSplit.intContractDetailId,
-				dblTransferUnits				= -CASE WHEN (SourceSplit.dblOriginalUnits - SourceSplit.dblDeductedUnits) = 0 THEN SourceSplit.dblOriginalUnits ELSE SourceSplit.dblDeductedUnits END ,
+				dblTransferUnits				= -CASE WHEN (SourceSplit.dblOriginalUnits - SourceSplit.dblDeductedUnits) = 0 THEN SourceSplit.dblOriginalUnits ELSE (SourceSplit.dblOriginalUnits - SourceSplit.dblDeductedUnits) END ,
 				intSourceItemUOMId				= TransferStorage.intItemUOMId,
 				intCustomerStorageId			= SourceSplit.intSourceCustomerStorageId
 			FROM tblGRTransferStorageSourceSplit SourceSplit
