e37922a233878e4cb5c70617630951fb0a387c57
bonn ponce <bonn.ponce@irely.com>
2020-08-14 09:42:38 +0800

    ST-1713 [20.1RC-5]

--- i21Database/dbo/Tables/tblSTReturnLottery.sql
+++ i21Database/dbo/Tables/tblSTReturnLottery.sql
@@ -12,8 +12,8 @@ CREATE TABLE [dbo].[tblSTReturnLottery] (
     [intConcurrencyId]      INT             CONSTRAINT [DF_tblSTReturnLottery_intConcurrencyId] DEFAULT ((1)) NULL,
     CONSTRAINT [PK_tblSTReturnLottery] PRIMARY KEY CLUSTERED ([intReturnLotteryId] ASC),
     CONSTRAINT [FK_tblSTReturnLottery_tblICInventoryReceipt] FOREIGN KEY ([intInventoryReceiptId]) REFERENCES [dbo].[tblICInventoryReceipt] ([intInventoryReceiptId]) ON DELETE CASCADE,
-    CONSTRAINT [FK_tblSTReturnLottery_tblSTLotteryBook] FOREIGN KEY ([intLotteryBookId]) REFERENCES [dbo].[tblSTLotteryBook] ([intLotteryBookId]) ON DELETE CASCADE
-    CONSTRAINT [FK_tblSTReceiveLottery_tblSTCheckoutHeader] FOREIGN KEY ([intCheckoutId]) REFERENCES [dbo].[tblSTCheckoutHeader] ([intCheckoutId]) ON DELETE CASCADE,
+    CONSTRAINT [FK_tblSTReturnLottery_tblSTLotteryBook] FOREIGN KEY ([intLotteryBookId]) REFERENCES [dbo].[tblSTLotteryBook] ([intLotteryBookId]) ON DELETE CASCADE,
+    CONSTRAINT [FK_tblSTReturnLottery_tblSTCheckoutHeader] FOREIGN KEY ([intCheckoutId]) REFERENCES [dbo].[tblSTCheckoutHeader] ([intCheckoutId]) ON DELETE CASCADE
    
 );
 
