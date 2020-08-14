fa1645ff161bb98b2d33426acf7e82398030fb7d
Jeff Trajano <jeffrey.trajano@irely.com>
2020-08-14 16:10:12 +0800

    GL-7705 HDTN-233962 - SOW for Origin Check Stub Format - Adjustments

--- i21Database/dbo/Stored Procedures/uspCMVoucherCheckMiddleSubReportAPPayment.sql
+++ i21Database/dbo/Stored Procedures/uspCMVoucherCheckMiddleSubReportAPPayment.sql
@@ -32,8 +32,18 @@ DECLARE @BANK_DEPOSIT INT = 1
   ,@VOID_CHECK AS INT = 19
   ,@AP_ECHECK AS INT = 20
   ,@PAYCHECK AS INT = 21;
-
-WITH Invoices AS(
+WITH InvoiceType As(
+	select 0 intTransactionType, 'Invoice', strTransactionType UNION ALL
+	select 1 intTransactionType , 'Voucher' strTransactionType UNION ALL
+	select 2 intTransactionType , 'Prepayment' strTransactionType UNION ALL
+	select 3 intTransactionType , 'Debit Memo' strTransactionType UNION ALL
+	select 9 intTransactionType , '1099 Adjustment' strTransactionType UNION ALL
+	select 11 intTransactionType , 'Claim' strTransactionType UNION ALL
+	select 12 intTransactionType , 'Prepayment Reversal' strTransactionType UNION ALL
+	select 13 intTransactionType , 'Basis Advance' strTransactionType UNION ALL
+	select 14 intTransactionType , 'Deferred Interest' strTransactionType
+),
+Invoices AS(
 	SELECT  
 			intTransactionId = F.intTransactionId
 			,strBillId = BILL.strBillId
@@ -119,7 +129,7 @@ WITH Invoices AS(
 						ELSE  PYMTDetail.dblInterest 
 						END
 			,dblNet = PYMTDetail.dblPayment
-			,'' AS intTransactionType
+			,0 AS intTransactionType
 			,PYMTDetail.intPaymentDetailId
 			,F.intCurrencyId
 			,F.strReferenceNo strCheckNumber
@@ -135,10 +145,12 @@ WITH Invoices AS(
 			AND F.intBankTransactionTypeId IN (@AP_PAYMENT, @AP_ECHECK)
 ) 
 SELECT
-a.*
-,b.strTermCode
-,c.strCurrency
-FROM Invoices a
-LEFT JOIN tblSMTerm b on a.intTermsId = b.intTermID
-LEFT JOIN tblSMCurrency c ON a.intCurrencyId = c.intCurrencyID
+Invoice.*
+,InvoiceType.strTransactionType
+,Term.strTermCode
+,Curr.strCurrency
+FROM Invoices Invoice
+LEFT JOIN tblSMTerm Term on Invoice.intTermsId = Term.intTermID
+LEFT JOIN tblSMCurrency Curr ON Invoice.intCurrencyId = Curr.intCurrencyID
+LEFT JOIN InvoiceType ON InvoiceType.intTransactionType = Invoice.intTransactionType
 order by strInvoice, intPaymentDetailId
