ddf948e064bb78682447841ea12cdeb9ac69d915
kelvin_quinto <kelvin.quinto@irely.com>
2020-08-18 17:54:15 +0800

    AR-12197
    (cherry picked from commit 8eb4b739ce6afc15d70671a52a755fa10476ebce)

--- i21Database/dbo/Stored Procedures/uspARInvoiceMCPReport.sql
+++ i21Database/dbo/Stored Procedures/uspARInvoiceMCPReport.sql
@@ -706,7 +706,7 @@ IF EXISTS (SELECT TOP 1 NULL FROM tblARInvoiceReportStagingTable WHERE intEntity
 		  , intBillToLocationId		= ORIG.intBillToLocationId
 		  , intShipToLocationId		= ORIG.intShipToLocationId
 		  , intTermId				= ORIG.intTermId
-		  , intItemId				= NULL
+		  , intItemId				= CASE WHEN STAGING.strItemNo = 'State Sales Tax' THEN NULL ELSE ORIG.intItemId END
 		  , intShipViaId			= ORIG.intShipViaId
 		  , intTicketId				= ORIG.intTicketId
 		  , strCompanyName			= ORIG.strCompanyName
@@ -735,6 +735,7 @@ IF EXISTS (SELECT TOP 1 NULL FROM tblARInvoiceReportStagingTable WHERE intEntity
 		  , dtmDueDate				= ORIG.dtmDueDate
 		  , blbLogo					= ORIG.blbLogo
 		  , strUnitMeasure			= ORIG.strUnitMeasure
+		  , strSalesOrderNumber		= ORIG.strSalesOrderNumber
 		FROM tblARInvoiceReportStagingTable STAGING
 		INNER JOIN (
 			SELECT *
