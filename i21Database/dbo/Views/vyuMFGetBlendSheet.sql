85d3aafe798778b3df2c70628ebdf45cb74c5d5c
Vinoth Kumar T <vinoth.t@irely.com>
2020-08-25 22:30:32 +0530

    MFG-4239

--- i21Database/dbo/Views/vyuMFGetBlendSheet.sql
+++ i21Database/dbo/Views/vyuMFGetBlendSheet.sql
@@ -1,7 +1,7 @@
 ﻿CREATE VIEW [dbo].[vyuMFGetBlendSheet]
 AS 
 Select w.intWorkOrderId,w.strWorkOrderNo,i.strItemNo,i.strDescription,
-w.dblQuantity,um.strUnitMeasure AS strUOM,w.dtmExpectedDate,w.intStatusId
+w.dblQuantity,um.strUnitMeasure AS strUOM,w.dtmExpectedDate,w.intStatusId,w.ysnUseTemplate
 from tblMFWorkOrder w 
 Join tblICItem i on w.intItemId=i.intItemId 
 Join tblICItemUOM iu on w.intItemUOMId=iu.intItemUOMId 
