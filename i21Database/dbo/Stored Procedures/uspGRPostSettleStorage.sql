17205fd2c6a20667bc9c7318a2df0c90190cd613
Leslie Guevarra <leslie.arat@irely.com>
2020-08-26 00:48:34 +0800

    Revert "GRN-2258 (Call uspAPUpdateCost after creating taxes.)"
    This reverts commit 8566b2358105530474adb248efa02e484a6e00e9.

--- i21Database/dbo/Stored Procedures/uspGRPostSettleStorage.sql
+++ i21Database/dbo/Stored Procedures/uspGRPostSettleStorage.sql
@@ -3210,7 +3210,7 @@ BEGIN TRY
 							update @avqty set ysnApplied = 1, intBillDetailId = @cur_bid where id = @cur_id
 						end
 
-						--EXEC [uspAPUpdateVoucherDetailTax] @detailCreated
+						EXEC [uspAPUpdateVoucherDetailTax] @detailCreated
 
 					end
 
