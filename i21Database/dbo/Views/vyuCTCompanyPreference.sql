7f559f7c5235e1666dce83deb0292630e3ccedcf
jayson_ramos <jayson.ramos@irely.com>
2020-08-26 18:23:06 +0800

    CT-5099
    (cherry picked from commit 53568d7c200b9059d39ae814438542a091007e22)

--- i21Database/dbo/Views/vyuCTCompanyPreference.sql
+++ i21Database/dbo/Views/vyuCTCompanyPreference.sql
@@ -94,6 +94,7 @@ AS
 			,CP.ysnForexRatePriceOptionalOnContract
 			,CP.ysnAllowSignedWhenContractHasAmendment
 			,CP.intQuantityDecimals
+			,CP.intPricingDecimals
 	FROM	tblCTCompanyPreference		CP LEFT
 	JOIN	tblICUnitMeasure			U1	ON	U1.intUnitMeasureId			=	CP.intCleanCostUOMId		LEFT
 	JOIN	tblSMCurrency				C1	ON	C1.intCurrencyID			=	CP.intCleanCostCurrencyId	LEFT
