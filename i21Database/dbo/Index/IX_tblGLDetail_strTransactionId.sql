696990180a1382a3b47b0a44b6838cd31292a5ec
Jeff Trajano <jeffrey.trajano@irely.com>
2020-09-02 14:07:15 +0800

    GL-7761 Undeposited Funds - UnMatched Records

---
+++ i21Database/dbo/Index/IX_tblGLDetail_strTransactionId.sql
@@ -0,0 +1,19 @@
+/****** Object:  Index [tblGLDetail_strTransactionId]    Script Date: 02/09/2020 1:53:33 PM ******/
+CREATE NONCLUSTERED INDEX [IX_tblGLDetail_strTransactionId] ON [dbo].[tblGLDetail]
+(
+	[strTransactionId] ASC
+)
+INCLUDE ( 	
+    [ysnIsUnposted],
+	[intAccountId]) 
+    WITH (PAD_INDEX = OFF, 
+    STATISTICS_NORECOMPUTE = OFF, 
+    SORT_IN_TEMPDB = OFF, 
+    DROP_EXISTING = OFF, 
+    ONLINE = OFF, 
+    ALLOW_ROW_LOCKS = ON, 
+    ALLOW_PAGE_LOCKS = ON) 
+    ON [PRIMARY]
+GO
+
+
