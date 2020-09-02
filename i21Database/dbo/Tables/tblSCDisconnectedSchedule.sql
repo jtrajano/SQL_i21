874e22350aa6e948f99f8bb59a736ceaf7d31827
Kevin Estrada <kevin.estrada071993@gmail.com>
2020-09-02 18:59:22 +0800

    Merge branch 'SC-3769' into 20.1Dev
    (cherry picked from commit 2250e1c239eb2f29632a6016723796c99c26c31c)

---
+++ i21Database/dbo/Tables/tblSCDisconnectedSchedule.sql
@@ -0,0 +1,12 @@
+CREATE TABLE [dbo].[tblSCDisconnectedSchedule]
+(
+	[intScheduleId]				INT IDENTITY (1, 1) NOT NULL,
+	[strFrequency]				[nvarchar](500) COLLATE Latin1_General_CI_AS NULL,
+    [intDay]					[int] NULL,
+	[intDayOfMonth]				[int] NULL,
+	[dtmRunTime]				DATETIME NULL,
+	[intConcurrencyId]			[int] DEFAULT 1,
+	CONSTRAINT [PK_tblSCDisconnectedSchedule] PRIMARY KEY CLUSTERED ([intScheduleId] ASC),
+	
+)
+
