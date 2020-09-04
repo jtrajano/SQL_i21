98f60c1e25cef71193a9aaccc760887a85d0773e
Jeff Trajano <jeffrey.trajano@irely.com>
2020-09-04 08:13:14 +0800

    GL-7705 HDTN-233962 - SOW for Origin Check Stub Format - Adjustments

--- i21Database/dbo/Stored Procedures/uspCMPostBankTransaction.sql
+++ i21Database/dbo/Stored Procedures/uspCMPostBankTransaction.sql
@@ -357,6 +357,7 @@ BEGIN
 			,[strTransactionForm]
 			,[strModuleName]
 			,[intEntityId]
+			,[intJournalLineNo]
 	)
 	SELECT	[strTransactionId]		= @strTransactionId
 			,[intTransactionId]		= @intTransactionId
@@ -385,6 +386,7 @@ BEGIN
 			,[strTransactionForm]	= @TRANSACTION_FORM
 			,[strModuleName]		= @MODULE_NAME
 			,[intEntityId]			= A.intEntityId
+			,[intJournalLineNo]		= NULL
 	FROM	[dbo].tblCMBankTransaction A INNER JOIN [dbo].tblCMBankAccount BankAccnt
 				ON A.intBankAccountId = BankAccnt.intBankAccountId
 			INNER JOIN vyuGLAccountDetail GLAccnt
@@ -420,6 +422,7 @@ BEGIN
 			,[strTransactionForm]	= @TRANSACTION_FORM
 			,[strModuleName]		= @MODULE_NAME
 			,[intEntityId]			= A.intEntityId
+			,[intJournalLineNo]		= B.intTransactionDetailId
 	FROM	[dbo].tblCMBankTransaction A INNER JOIN [dbo].tblCMBankTransactionDetail B
 				ON A.intTransactionId = B.intTransactionId
 			INNER JOIN vyuGLAccountDetail GLAccnt
