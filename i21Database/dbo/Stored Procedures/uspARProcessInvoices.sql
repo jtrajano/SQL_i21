b9b974668e2106a64e309282c2cdf250001002a5
kelvin_quinto <kelvin.quinto@irely.com>
2020-09-08 12:07:24 +0800

    AR-12311
    (cherry picked from commit 211e0308f6c295941ea71e78ceafd26f8e382f1e)
    (cherry picked from commit d74ef26cd785fea8d51c7d42d5bd0dc11896ca7e)
    (cherry picked from commit 70278d999ad42f0d04fd987e6580aaef5e6075b5)

--- i21Database/dbo/Stored Procedures/uspARProcessInvoices.sql
+++ i21Database/dbo/Stored Procedures/uspARProcessInvoices.sql
@@ -424,10 +424,7 @@ BEGIN
 		,@ItemPrepayTypeId 				= (CASE WHEN @GroupingOption = 0 THEN [intPrepayTypeId] ELSE NULL END) 
 		,@ItemPrepayRate 				= (CASE WHEN @GroupingOption = 0 THEN [dblPrepayRate] ELSE NULL END) 
 		,@Inventory						= (CASE WHEN @GroupingOption = 0 THEN [ysnInventory] ELSE NULL END)
-		,@ItemDocumentNumber			= (CASE WHEN @GroupingOption = 0 THEN CASE WHEN [strTransactionType] = 'Transport Delivery' 
-																				THEN ISNULL(ISNULL([strDocumentNumber], @SourceNumber),[strSourceId])
-																				ELSE [strDocumentNumber] END
-																		 ELSE NULL END)
+		,@ItemDocumentNumber			= (CASE WHEN @GroupingOption = 0 THEN ISNULL(ISNULL([strDocumentNumber], @SourceNumber),[strSourceId]) ELSE NULL END)
 		,@ItemDescription				= (CASE WHEN @GroupingOption = 0 THEN [strItemDescription] ELSE NULL END)
 		,@ItemOrderUOMId				= (CASE WHEN @GroupingOption = 0 THEN [intOrderUOMId] ELSE NULL END)
 		,@ItemQtyOrdered				= (CASE WHEN @GroupingOption = 0 THEN [dblQtyOrdered] ELSE NULL END)
@@ -853,7 +850,7 @@ BEGIN
 					,@ItemPrepayTypeId				= [intPrepayTypeId]
 					,@ItemPrepayRate 				= [dblPrepayRate]
 					,@Inventory						= [ysnInventory]
-					,@ItemDocumentNumber			= CASE WHEN strTransactionType = 'Transport Delivery' THEN ISNULL([strDocumentNumber], @SourceNumber) ELSE [strDocumentNumber] END
+					,@ItemDocumentNumber			= ISNULL([strDocumentNumber], @SourceNumber)
 					,@ItemDescription				= [strItemDescription]
 					,@ItemOrderUOMId				= [intOrderUOMId]					
 					,@ItemQtyOrdered				= [dblQtyOrdered]
@@ -1627,7 +1624,7 @@ BEGIN TRY
 						,@ItemPrepayTypeId				= [intPrepayTypeId]
 						,@ItemPrepayRate				= [dblPrepayRate]
 						,@Inventory						= [ysnInventory]
-						,@ItemDocumentNumber			= CASE WHEN strTransactionType = 'Transport Delivery' THEN ISNULL([strDocumentNumber], @SourceNumber) ELSE [strDocumentNumber] END
+						,@ItemDocumentNumber			= ISNULL([strDocumentNumber], @SourceNumber)
 						,@ItemDescription				= [strItemDescription]
 						,@ItemOrderUOMId				= [intOrderUOMId]
 						,@ItemQtyOrdered				= [dblQtyOrdered]
