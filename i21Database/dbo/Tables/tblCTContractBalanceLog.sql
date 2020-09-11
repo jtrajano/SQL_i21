57dccd1aa6cbee48213e36726e302bf2f1ebeed5
Dan Jalimeo <dan.jalimao@irely.com>
2020-09-11 11:48:09 +0800

    Revert "CT-5135"
    This reverts commit c57c02c3a35e1cb6318baa35f48b4d3aadaa2164.

--- i21Database/dbo/Tables/tblCTContractBalanceLog.sql
+++ i21Database/dbo/Tables/tblCTContractBalanceLog.sql
@@ -44,6 +44,5 @@
 	[strProcess] NVARCHAR (100) COLLATE Latin1_General_CI_AS NULL,
 	[strPricingStatus] NVARCHAR (100) COLLATE Latin1_General_CI_AS NULL,
 	[ysnDeleted] BIT DEFAULT((0)) NULL,
-	[strPricingStatus] NVARCHAR(30) COLLATE Latin1_General_CI_AS NULL,
     CONSTRAINT [PK_tblCTContractBalanceLog] PRIMARY KEY ([intContractBalanceLogId]) 
 )
