b2521471e997c70f565c1509c1d6a3bb27ccd8ea
axelcasauran <axelcasauran@irely.com>
2020-09-11 11:08:51 +0800

    MBIL-303

---
+++ i21Database/dbo/Views/vyuMBILExportCustomerTaxExemption.sql
@@ -0,0 +1,31 @@
+﻿CREATE VIEW [dbo].[vyuMBILExportCustomerTaxExemption]  
+AS  
+
+
+SELECT * FROM 
+	(
+		SELECT TE.*							
+		FROM
+			tblARCustomerTaxingTaxException TE
+		LEFT OUTER JOIN
+			tblSMTaxCode TC
+				ON TE.[intTaxCodeId] = TC.[intTaxCodeId]
+		LEFT OUTER JOIN
+			[tblEMEntityLocation] EL	
+				ON TE.[intEntityCustomerLocationId] = EL.[intEntityLocationId]
+		LEFT OUTER JOIN
+			tblSMTaxClass TCL
+				ON TE.[intTaxClassId] = TCL.[intTaxClassId]
+		LEFT OUTER JOIN
+			tblICItem  IC
+				ON TE.[intItemId] = IC.[intItemId]
+		LEFT OUTER JOIN
+			tblICCategory ICC
+				ON TE.[intCategoryId] = ICC.[intCategoryId]
+		LEFT OUTER JOIN
+			tblCFCard CFC
+				ON TE.[intCardId] = CFC.[intCardId]
+		LEFT OUTER JOIN
+			tblCFVehicle CFV
+				ON TE.[intVehicleId] = CFV.[intVehicleId] 		
+	) tblCustomer
