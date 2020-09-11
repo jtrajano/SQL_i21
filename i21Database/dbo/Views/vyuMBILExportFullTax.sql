ef6f83afa9930535ef27aa08ca635ddd90ecd4bd
axelcasauran <axelcasauran@irely.com>
2020-09-11 11:30:56 +0800

    MBIL-319

---
+++ i21Database/dbo/Views/vyuMBILExportFullTax.sql
@@ -0,0 +1,24 @@
+﻿CREATE VIEW [dbo].[vyuMBILExportFullTax]
+AS 
+
+SELECT
+		  ROW_NUMBER() OVER(ORDER BY TG.intTaxGroupId) AS intFulltaxId
+		, TG.intTaxGroupId
+		, TG.strTaxGroup
+		, TC.intTaxCodeId
+		, TC.strTaxCode
+		, TCL.intTaxClassId
+		, TCL.strTaxClass
+		, strCalculationMethod = (SELECT TOP 1 strCalculationMethod from tblSMTaxCodeRate where intTaxCodeId = TC.intTaxCodeId and CAST(dtmEffectiveDate  AS DATE) <= CAST(GETDATE() AS DATE) ORDER BY dtmEffectiveDate DESC) 
+		, dblRate = (SELECT TOP 1 dblRate from tblSMTaxCodeRate where intTaxCodeId = TC.intTaxCodeId and CAST(dtmEffectiveDate  AS DATE) <= CAST(GETDATE() AS DATE) ORDER BY dtmEffectiveDate DESC) 
+		, dblQuantity = 0.00
+		, dblPrice = 0.00
+		, dblTotal = 0.00
+		, ysnCheckoffTax
+		, ysnTaxOnly
+		, strTaxableByOtherTaxes
+		from tblSMTaxGroup TG
+	inner join tblSMTaxGroupCode TGC on TGC.intTaxGroupId = TG.intTaxGroupId
+	inner join tblSMTaxCode TC on TC.intTaxCodeId = TGC.intTaxCodeId
+	inner join tblSMTaxClass TCL on TCL.intTaxClassId = TC.intTaxClassId
+
