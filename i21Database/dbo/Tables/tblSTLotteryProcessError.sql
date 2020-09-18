0547df0b7dc8437f65eb8dbdcab00e917e2dcf95
bonn ponce <bonn.ponce@irely.com>
2020-09-18 15:04:55 +0800

    ST-1802

--- i21Database/dbo/Tables/tblSTLotteryProcessError.sql
+++ i21Database/dbo/Tables/tblSTLotteryProcessError.sql
@@ -5,5 +5,6 @@
     [strGame]					 NVARCHAR(MAX)  NULL,
     [strError]					 NVARCHAR(MAX)  NULL,
     [strProcess]				 NVARCHAR(MAX)  NULL,
+    [intConcurrencyId]           INT NULL CONSTRAINT [DF_tblSTLotteryProcessError_intConcurrencyId]  DEFAULT ((1)),
     CONSTRAINT [PK_tblSTLotteryProcessError] PRIMARY KEY CLUSTERED ([intLotteryProcessErrorId] ASC) WITH (FILLFACTOR = 70),
 );
