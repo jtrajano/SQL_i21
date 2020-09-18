19248e40a17fc0a6735e471753492682374d17d1
Jeff Trajano <jeffrey.trajano@irely.com>
2020-09-18 10:52:10 +0800

    GL-7804 Unposting didn't refresh Trial balance

--- i21Database/dbo/Stored Procedures/uspGLUpdateTrialBalance.sql
+++ i21Database/dbo/Stored Procedures/uspGLUpdateTrialBalance.sql
@@ -25,4 +25,25 @@ FROM  @GLEntries,  tblGLFiscalYearPeriod F
 JOIN tblGLFiscalYear FY ON FY.intFiscalYearId = F.intFiscalYearId
 where dtmDate BETWEEN F.dtmStartDate AND F.dtmEndDate
 GROUP BY intAccountId, intGLFiscalYearPeriodId, strTransactionId
-GO
+
+INSERT INTO tblGLTrialBalance
+(
+    intAccountId,
+    strTransactionId,
+    YTDBalance,
+    intGLFiscalYearPeriodId,
+    intConcurrencyId,
+    dtmDateModified
+)
+SELECT 
+A.intAccountId,
+strTransactionId,
+sum(dblDebit-dblCredit) dblAmount,
+F1.intGLFiscalYearPeriodId,
+1,
+@dtmDate
+FROM @GLEntries A JOIN tblGLFiscalYearPeriod F ON dtmDate BETWEEN F.dtmStartDate AND F.dtmEndDate,
+tblGLFiscalYearPeriod F1 
+where F1.dtmEndDate > F.dtmEndDate
+GROUP BY intAccountId ,  F1.intGLFiscalYearPeriodId,strTransactionId
+			
