8a72fcddbfd8fd6c2c7cbb482ca5c5499b39bad7
Jeff Trajano <jeffrey.trajano@irely.com>
2020-09-18 14:43:58 +0800

    GL-7804 Unposting didn't refresh Trial balance

--- i21Database/dbo/Stored Procedures/uspGLUpdateTrialBalance.sql
+++ i21Database/dbo/Stored Procedures/uspGLUpdateTrialBalance.sql
@@ -26,6 +26,8 @@ JOIN tblGLFiscalYear FY ON FY.intFiscalYearId = F.intFiscalYearId
 where dtmDate BETWEEN F.dtmStartDate AND F.dtmEndDate
 GROUP BY intAccountId, intGLFiscalYearPeriodId, strTransactionId
 
+-- NON -RETAINED EARNING 
+-- ADD YTD BALANCE WHERE PERIOD IS GREATER THAN POSTING DATE
 INSERT INTO tblGLTrialBalance
 (
     intAccountId,
@@ -44,6 +46,31 @@ F1.intGLFiscalYearPeriodId,
 @dtmDate
 FROM @GLEntries A JOIN tblGLFiscalYearPeriod F ON dtmDate BETWEEN F.dtmStartDate AND F.dtmEndDate,
 tblGLFiscalYearPeriod F1 
-where F1.dtmEndDate > F.dtmEndDate
+WHERE F1.dtmEndDate > F.dtmEndDate
+AND A.intAccountId NOT IN ( SELECT intRetainAccount FROM tblGLFiscalYear)
 GROUP BY intAccountId ,  F1.intGLFiscalYearPeriodId,strTransactionId
-			
+
+-- RETAINED EARNINGS --DEV ONLY
+-- ADD YTD BALANCE WHERE PERIOD IS GREATER THAN POSTING DATE UP TO FISCAL PERIOD END
+INSERT INTO tblGLTrialBalance
+(
+    intAccountId,
+    strTransactionId,
+    YTDBalance,
+    intGLFiscalYearPeriodId,
+    intConcurrencyId,
+    dtmDateModified
+)
+SELECT 
+A.intAccountId,
+strTransactionId,
+sum(dblDebit-dblCredit) dblAmount,
+F1.intGLFiscalYearPeriodId,
+1,
+@dtmDate
+FROM @GLEntries A JOIN tblGLFiscalYearPeriod F ON dtmDate BETWEEN F.dtmStartDate AND F.dtmEndDate
+JOIN tblGLFiscalYear FY on FY.intFiscalYearId = F.intFiscalYearId,
+tblGLFiscalYearPeriod F1 
+WHERE F1.dtmEndDate > F.dtmEndDate and F1.dtmStartDate < FY.dtmDateTo
+AND A.intAccountId IN (SELECT  intRetainAccount FROM tblGLFiscalYear)
+GROUP BY intAccountId ,  F1.intGLFiscalYearPeriodId, strTransactionId
