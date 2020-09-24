3a5da2c8008cd2803752ff1561db8347c99ade34
Jeff Trajano <jeffrey.trajano@irely.com>
2020-09-24 23:41:53 +0800

    GL-7739 CLONE - BI - Gross Margin Changes

--- i21Database/Scripts/Post-Deployment/AR/DefaultData/98_UpdateInvoiceGrossMarginSummary.sql
+++ i21Database/Scripts/Post-Deployment/AR/DefaultData/98_UpdateInvoiceGrossMarginSummary.sql
@@ -8,8 +8,8 @@ GO
 IF NOT EXISTS (SELECT TOP 1 1 FROM tblARInvoiceGrossMarginSummary )
 BEGIN
 	PRINT('Started initializing summary data for invoice gross margin')
-	INSERT INTO tblARInvoiceGrossMarginSummary ( strType, dtmDate, dblAmount, intConcurrencyId)
-	SELECT strType, dtmDate, SUM(dblAmount),1 FROM vyuARInvoiceGrossMargin
+	INSERT INTO tblARInvoiceGrossMarginSummary (intInvoiceId, strType, dtmDate, dblAmount, intConcurrencyId)
+	SELECT intInvoiceId, strType, dtmDate, SUM(dblAmount),1 FROM vyuARInvoiceGrossMargin
 	group by strType, dtmDate
 	PRINT('Finished initializing summary data for invoice gross margin')
 END
