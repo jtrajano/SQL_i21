4b259726ed016b63995aea5d818f4509ceabea16
Jeff Trajano <jeffrey.trajano@irely.com>
2020-09-29 23:55:49 +0800

    GL-7829 GL-7739

--- i21Database/Scripts/Post-Deployment/AR/DefaultData/98_UpdateInvoiceGrossMarginSummary.sql
+++ i21Database/Scripts/Post-Deployment/AR/DefaultData/98_UpdateInvoiceGrossMarginSummary.sql
@@ -8,9 +8,9 @@ GO
 IF NOT EXISTS (SELECT TOP 1 1 FROM tblARInvoiceGrossMarginSummary )
 BEGIN
 	PRINT('Started initializing summary data for invoice gross margin')
-	INSERT INTO tblARInvoiceGrossMarginSummary ( strType, dtmDate, dblAmount, intConcurrencyId)
-	SELECT strType, dtmDate, SUM(dblAmount),1 FROM vyuARInvoiceGrossMargin
-	group by strType, dtmDate
+	INSERT INTO tblARInvoiceGrossMarginSummary (intInvoiceId, strType, dtmDate, dblAmount, intConcurrencyId)
+	SELECT intInvoiceId, strType, dtmDate, SUM(dblAmount),1 FROM vyuARInvoiceGrossMargin
+	group by strType, dtmDate,intInvoiceId
 	PRINT('Finished initializing summary data for invoice gross margin')
 END
 GO
