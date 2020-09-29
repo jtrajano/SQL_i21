5eec658039b535a4db4d2ec7d82f51f47ea8ba8f
Jopet Lambinicio <jopet.lambinicio@irely.com>
2020-09-29 16:10:41 +0800

    AP-9196

---
+++ i21Database/dbo/Views/vyuAPVoucherPayableTaxStaging.sql
@@ -0,0 +1,22 @@
+﻿CREATE VIEW [dbo].[vyuAPVoucherPayableTaxStaging]
+AS
+SELECT          TS.intVoucherPayableId,
+				TS.intTaxGroupId,
+				TS.intTaxCodeId,
+				TS.intTaxClassId,
+				TS.strTaxableByOtherTaxes,
+				TS.strCalculationMethod,
+				TS.dblRate,
+				TS.intAccountId,
+				TS.dblTax,
+				TS.dblAdjustedTax,
+				TS.ysnTaxAdjusted,
+				TS.ysnSeparateOnBill,
+				TS.ysnCheckOffTax,
+				TS.ysnTaxOnly,
+				TS.ysnTaxExempt,
+				TS.dtmDateEntered,
+				TC.strTaxCode
+FROM tblAPVoucherPayableTaxStaging TS
+INNER JOIN tblSMTaxCode TC ON  TC.intTaxCodeId  = TS.intTaxCodeId
+GO
