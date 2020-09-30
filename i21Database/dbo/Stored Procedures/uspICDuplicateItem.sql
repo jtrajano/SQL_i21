11daea0abbfd0668215f039af3d7ae42a9d1280a
Wendell Wayne Estrada <wendell.estrada@irely.com>
2020-09-30 13:52:42 +0800

    [B] IC-8942 CLONE - Make UOM required field for Bundles

--- i21Database/dbo/Stored Procedures/uspICDuplicateItem.sql
+++ i21Database/dbo/Stored Procedures/uspICDuplicateItem.sql
@@ -671,7 +671,7 @@ BEGIN
 		intBundleItemId,
 		strDescription,
 		dblQuantity,
-		dbo.fnICGetItemUOMIdFromDuplicateItem(intItemUnitMeasureId, @NewItemId),
+		dbo.fnICGetItemUOMIdFromDuplicateItem(intItemUnitMeasureId, intBundleItemId),
 		ysnAddOn,
 		dblMarkUpOrDown,
 		dtmBeginDate,
