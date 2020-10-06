c1ab953c890f9fdb8ce532304be197121ef93a88
Jopet Lambinicio <jopet.lambinicio@irely.com>
2020-10-06 12:55:47 +0800

    AP-9211

--- i21Database/dbo/Stored Procedures/uspAPPostVoucherPrepay.sql
+++ i21Database/dbo/Stored Procedures/uspAPPostVoucherPrepay.sql
@@ -228,16 +228,6 @@ BEGIN
 		WHERE B.intBillId IN (SELECT intId FROM @validVoucherPrepay)
 		AND B.strBillId = A.strTransactionId
 	) AND A.ysnIsUnposted = 0
-
-	--CLEAN ysnInPayment, dblPaymentTemp, & ysnPrepayHasPayment
-	--WE CAN ASSUME NEWLY POSTED VOUCHERS DOES NOT HAVE PAYMENT YET
-	UPDATE B
-	SET B.ysnInPayment = 0,
-		B.dblPaymentTemp = 0,
-		B.ysnPrepayHasPayment = 0
-	FROM tblAPBill B
-	INNER JOIN @validVoucherPrepay VP ON VP.intId = B.intBillId
-	WHERE B.ysnPrepayHasPayment = 0 AND @post = 1
 END
 
 -- Get the vendor id to intSourceEntityId
@@ -316,6 +306,16 @@ BEGIN
 		SET prepay.ysnPosted = @post, prepay.intConcurrencyId = ISNULL(prepay.intConcurrencyId,0) + 1
 	FROM tblAPBill prepay
 	INNER JOIN @validVoucherPrepay prepayIds ON prepay.intBillId = prepayIds.intId
+
+	--CLEAN ysnInPayment, dblPaymentTemp, & ysnPrepayHasPayment
+	--WE CAN ASSUME NEWLY POSTED VOUCHERS DOES NOT HAVE PAYMENT YET
+	UPDATE B
+	SET B.ysnInPayment = 0,
+		B.dblPaymentTemp = 0,
+		B.ysnPrepayHasPayment = 0
+	FROM tblAPBill B
+	INNER JOIN @validVoucherPrepay VP ON VP.intId = B.intBillId
+	WHERE B.ysnPrepayHasPayment = 0 AND @post = 1
 
 	DECLARE @totalPosted INT = 0
 	DECLARE @postedCtr INT = 0
