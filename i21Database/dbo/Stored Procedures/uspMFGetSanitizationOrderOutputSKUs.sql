81dd889affd11ef9f0495c37d7f11ff9e8d9f892
Vinoth Kumar T <vinoth.t@irely.com>
2020-10-06 23:59:45 +0530

    MFG-4277

---
+++ i21Database/dbo/Stored Procedures/uspMFGetSanitizationOrderOutputSKUs.sql
@@ -0,0 +1,35 @@
+﻿CREATE PROCEDURE uspMFGetSanitizationOrderOutputSKUs @intLocationId INT
+	,@intBatchId INT
+AS
+SET QUOTED_IDENTIFIER OFF
+SET ANSI_NULLS ON
+SET NOCOUNT ON
+SET XACT_ABORT ON
+SET ANSI_WARNINGS OFF
+
+SELECT WP.intLotId
+	,C.intContainerId
+	,C.strContainerNo
+	,S.intSKUId
+	,S.strSKUNo
+	,S.dblQty
+	,S.dtmExpiryDate
+	,U.intUnitMeasureId
+	,U.strUnitMeasure
+	,WP.intWorkOrderProducedSKUId
+	,S.intSKUStatusId
+	,SS.strSKUStatus
+	,SL.intStorageLocationId
+	,SL.strName
+	,SL.intSubLocationId
+	,CSL.strSubLocationName
+	,WP.intBatchId
+FROM dbo.tblMFWorkOrderProducedSKU WP
+JOIN dbo.tblWHSKU S ON WP.intSKUId = S.intSKUId
+	AND WP.intBatchId = @intBatchId
+JOIN dbo.tblWHContainer C ON C.intContainerId = S.intContainerId
+JOIN dbo.tblICStorageLocation SL ON C.intStorageLocationId = SL.intStorageLocationId
+JOIN dbo.tblSMCompanyLocationSubLocation CSL ON CSL.intCompanyLocationSubLocationId = SL.intSubLocationId
+JOIN dbo.tblICUnitMeasure U ON U.intUnitMeasureId = S.intUOMId
+JOIN tblWHSKUStatus SS ON SS.intSKUStatusId = S.intSKUStatusId
+ORDER BY WP.intBatchId
