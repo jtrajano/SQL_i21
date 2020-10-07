1d2d94fbdfc39114fea0d9ea2c3e66996719d29a
Feb Montefrio <feb.montefrio@irely.com>
2020-10-07 11:16:42 +0800

    [B] IC-8966

--- i21Database/dbo/Stored Procedures/uspICPostCosting.sql
+++ i21Database/dbo/Stored Procedures/uspICPostCosting.sql
@@ -126,10 +126,14 @@ SELECT
 	,[intItemLocationId] = p.intItemLocationId
 	,[intItemUOMId] = CASE WHEN ISNULL(i.ysnSeparateStockForUOMs, 0) = 0 AND ISNULL(i.strLotTracking, 'No') = 'No' THEN iu.intItemUOMId ELSE p.intItemUOMId END 
 	,[dtmDate] = 
+		p.dtmDate
+		-- Revert the code below. It is causing problems at Pri Mar. See IC-8966. 
+		/**
 		CASE 			
 			WHEN lastTransaction.dtmDate IS NOT NULL THEN @dtmSytemGeneratedPostDate
 			ELSE p.dtmDate
 		END 
+		**/
     ,[dblQty] = CASE WHEN ISNULL(i.ysnSeparateStockForUOMs, 0) = 0 AND ISNULL(i.strLotTracking, 'No') = 'No' THEN dbo.fnCalculateQtyBetweenUOM(p.intItemUOMId, iu.intItemUOMId, p.dblQty) ELSE p.dblQty END 
 	,[dblUOMQty] = CASE WHEN ISNULL(i.ysnSeparateStockForUOMs, 0) = 0 AND ISNULL(i.strLotTracking, 'No') = 'No' THEN iu.dblUnitQty ELSE p.dblUOMQty END 
     ,[dblCost] = CASE WHEN ISNULL(i.ysnSeparateStockForUOMs, 0) = 0 AND ISNULL(i.strLotTracking, 'No') = 'No' THEN dbo.fnCalculateCostBetweenUOM(p.intItemUOMId, iu.intItemUOMId, p.dblCost) ELSE p.dblCost END 
@@ -166,17 +170,17 @@ FROM
 	LEFT JOIN tblICItemUOM iu
 		ON iu.intItemId = p.intItemId
 		AND iu.ysnStockUnit = 1
-	OUTER APPLY (
-		SELECT TOP 1 
-			t.dtmDate
-		FROM 
-			tblICInventoryTransaction t
-		WHERE
-			t.strTransactionId = p.strTransactionId	
-			AND t.strBatchId <> REPLACE(@strBatchId, '-P', '') 
-		ORDER BY 
-			t.intInventoryTransactionId DESC 
-	) lastTransaction
+	--OUTER APPLY (
+	--	SELECT TOP 1 
+	--		t.dtmDate
+	--	FROM 
+	--		tblICInventoryTransaction t
+	--	WHERE
+	--		t.strTransactionId = p.strTransactionId	
+	--		AND t.strBatchId <> REPLACE(@strBatchId, '-P', '') 
+	--	ORDER BY 
+	--		t.intInventoryTransactionId DESC 
+	--) lastTransaction
 
 ORDER BY 
 	p.intId
