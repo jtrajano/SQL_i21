7556747a9d57afdcb3d5dff4c5bbd4bab1b764d2
Feb Montefrio <feb.montefrio@irely.com>
2020-10-07 12:08:24 +0800

    [B] IC-8966

--- i21Database/dbo/Stored Procedures/uspICPostInTransitCosting.sql
+++ i21Database/dbo/Stored Procedures/uspICPostInTransitCosting.sql
@@ -115,11 +115,11 @@ SELECT
 	[intItemId] = p.intItemId 
 	,[intItemLocationId] = p.intItemLocationId
 	,[intItemUOMId] = CASE WHEN ISNULL(i.ysnSeparateStockForUOMs, 0) = 0 AND ISNULL(i.strLotTracking, 'No') = 'No' THEN iu.intItemUOMId ELSE p.intItemUOMId END 
-	,[dtmDate] = 
-		CASE 
-			WHEN lastTransaction.dtmDate IS NOT NULL THEN @dtmSytemGeneratedPostDate
-			ELSE p.dtmDate
-		END 
+	,[dtmDate] = p.dtmDate
+		--CASE 
+		--	WHEN lastTransaction.dtmDate IS NOT NULL THEN @dtmSytemGeneratedPostDate
+		--	ELSE p.dtmDate
+		--END 
     ,[dblQty] = CASE WHEN ISNULL(i.ysnSeparateStockForUOMs, 0) = 0 AND ISNULL(i.strLotTracking, 'No') = 'No' THEN dbo.fnCalculateQtyBetweenUOM(p.intItemUOMId, iu.intItemUOMId, p.dblQty) ELSE p.dblQty END 
 	,[dblUOMQty] = CASE WHEN ISNULL(i.ysnSeparateStockForUOMs, 0) = 0 AND ISNULL(i.strLotTracking, 'No') = 'No' THEN iu.dblUnitQty ELSE p.dblUOMQty END 
     ,[dblCost] = CASE WHEN ISNULL(i.ysnSeparateStockForUOMs, 0) = 0 AND ISNULL(i.strLotTracking, 'No') = 'No' THEN dbo.fnCalculateCostBetweenUOM(p.intItemUOMId, iu.intItemUOMId, p.dblCost) ELSE p.dblCost END 
@@ -147,17 +147,17 @@ FROM
 	LEFT JOIN tblICItemUOM iu
 		ON iu.intItemId = p.intItemId
 		AND iu.ysnStockUnit = 1
-	OUTER APPLY (
-		SELECT TOP 1 
-			t.dtmDate
-		FROM 
-			tblICInventoryTransaction t
-		WHERE
-			t.strTransactionId = p.strTransactionId	
-			AND t.strBatchId <> @strBatchId 
-		ORDER BY 
-			t.intInventoryTransactionId DESC 
-	) lastTransaction
+	--OUTER APPLY (
+	--	SELECT TOP 1 
+	--		t.dtmDate
+	--	FROM 
+	--		tblICInventoryTransaction t
+	--	WHERE
+	--		t.strTransactionId = p.strTransactionId	
+	--		AND t.strBatchId <> @strBatchId 
+	--	ORDER BY 
+	--		t.intInventoryTransactionId DESC 
+	--) lastTransaction
 ORDER BY 
 	p.intId
 
