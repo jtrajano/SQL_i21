cf803704d5b5c0c0e803ff55da18cd622f98d280
Jopet Lambinicio <jopet.lambinicio@irely.com>
2020-10-07 15:04:13 +0800

    AP-9210

--- i21Database/dbo/Views/vyuAPBillForPayment.sql
+++ i21Database/dbo/Views/vyuAPBillForPayment.sql
@@ -70,6 +70,7 @@ FROM (
 			WHEN voucher.intTransactionType IN (2, 13) AND voucher.ysnPrepayHasPayment = 1 THEN voucher.dblPaymentTemp * -1
 			ELSE voucher.dblPaymentTemp END AS dblPaymentTemp
 		,voucher.ysnInPayment
+		,0 AS ysnInPaymentSched
 	FROM tblAPBill voucher
 	INNER JOIN (tblAPVendor vendor INNER JOIN tblEMEntity entity ON vendor.intEntityId = entity.intEntityId)
 		ON vendor.intEntityId = voucher.intEntityVendorId
@@ -92,7 +93,6 @@ FROM (
 	AND voucher.intTransactionType NOT IN (11, 12)
 	AND voucher.intTransactionReversed IS NULL
 	AND voucher.ysnIsPaymentScheduled = 0
-	AND ((voucher.ysnInPayment IS NULL OR voucher.ysnInPayment = 0) OR voucher.ysnPrepayHasPayment <> 0)
 	UNION ALL
 	SELECT 
 		voucher.intBillId
@@ -148,6 +148,7 @@ FROM (
 					AS BIT)
 		,voucher.dblPaymentTemp
 		,voucher.ysnInPayment
+		,paySched.ysnInPayment AS ysnInPaymentSched
 	FROM tblAPBill voucher
 	INNER JOIN (tblAPVendor vendor INNER JOIN tblEMEntity entity ON vendor.intEntityId = entity.intEntityId)
 		ON vendor.intEntityId = voucher.intEntityVendorId
@@ -164,6 +165,4 @@ FROM (
 	AND voucher.intTransactionReversed IS NULL
 	AND voucher.ysnIsPaymentScheduled = 1 --AP-7092
 	AND paySched.ysnPaid = 0
-	AND paySched.ysnInPayment = 0
-	AND ((voucher.ysnInPayment IS NULL OR voucher.ysnInPayment = 0) OR voucher.ysnPrepayHasPayment <> 0)
 ) forPayment
