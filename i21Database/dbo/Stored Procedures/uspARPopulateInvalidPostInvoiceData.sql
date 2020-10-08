e9afc3f235de2a35cd4cc2babf387f11990b9a59
Roudinee Panteria <roudinee.panteria@irely.com>
2020-10-08 16:31:30 +0800

    AR-12412
    (cherry picked from commit ca05f72871bb62e7e987a9f46d8aa3e13ac06e6a)

--- i21Database/dbo/Stored Procedures/uspARPopulateInvalidPostInvoiceData.sql
+++ i21Database/dbo/Stored Procedures/uspARPopulateInvalidPostInvoiceData.sql
@@ -1948,7 +1948,7 @@ BEGIN
 		,[intItemLocationId]
 		,[intItemUOMId]
 		,[dtmDate]
-		,[dblQty]
+		,CASE WHEN [strType] = 'CF Tran' THEN ABS([dblQty]) ELSE [dblQty] END
 		,[dblUOMQty]
 		,[dblCost]
 		,[dblValue]
@@ -1971,9 +1971,6 @@ BEGIN
 		,[dblForexRate]
 	FROM
 		#ARItemsForCosting
-	WHERE
-		[ysnForValidation] IS NULL
-		OR [ysnForValidation] = @OneBit
 
 	INSERT INTO #ARInvalidInvoiceData
 		([intInvoiceId]
