82619f65041f31573497b3215d21234ca657db42
Dan Jalimeo <dan.jalimao@irely.com>
2020-10-10 21:44:30 +0800

    CT-5256

--- i21Database/dbo/Tables/tblCTSequenceHistory.sql
+++ i21Database/dbo/Tables/tblCTSequenceHistory.sql
@@ -79,3 +79,7 @@
     CONSTRAINT [PK_tblCTSequenceHistory_intSequenceHistoryId] PRIMARY KEY CLUSTERED (intSequenceHistoryId ASC),
     CONSTRAINT [FK_tblCTSequenceHistory_tblCTContractDetail_intContractDetailId] FOREIGN KEY (intContractDetailId) REFERENCES [tblCTContractDetail](intContractDetailId) ON DELETE CASCADE
 )
+GO
+CREATE NONCLUSTERED INDEX [IX_tblCTSequenceHistory_intContractHeaderId]
+    ON [dbo].[tblCTSequenceHistory] ([intContractHeaderId] ASC)
+GO
