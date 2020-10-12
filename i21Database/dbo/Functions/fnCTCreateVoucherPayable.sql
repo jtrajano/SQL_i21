9e3d0f46e26a08215b357cbb8ed6b1d209cbbec7
Arman Arenas <arman.arenas@irely.com>
2020-10-12 22:35:40 +0800

    LG-2742 optimized fnCTCreateVoucherPayable; include intContractCostId on IX_tblAPVoucherPayable_deleteIX index

--- i21Database/dbo/Functions/fnCTCreateVoucherPayable.sql
+++ i21Database/dbo/Functions/fnCTCreateVoucherPayable.sql
@@ -7,7 +7,7 @@
 )
 RETURNS TABLE AS RETURN
 (
-	SELECT	DISTINCT
+	SELECT	
 		[intEntityVendorId]							=	ISNULL(entity.intEntityId, payable.intEntityVendorId)
 		,[intTransactionType]						=	CASE WHEN RT.Item = 0 THEN 1 ELSE 3 END --voucher
 		,[intLocationId]							=	NULL --Contract doesn't have location
@@ -35,23 +35,32 @@ RETURNS TABLE AS RETURN
 		,[intInventoryShipmentChargeId]				=	NULL
 		,[intLoadShipmentId]						=	NULL --?
 		,[intLoadShipmentDetailId]					=	NULL --?
-		,[intItemId]								=	CC.intItemId
+		,[intItemId]								=	item.intItemId
 		,[intPurchaseTaxGroupId]					=	NULL
-		,[strMiscDescription]						=	CC.strItemDescription
+		,[strMiscDescription]						=	item.strDescription
 		,[dblOrderQty]								=	CASE WHEN CC.strCostMethod = 'Per Unit' THEN ISNULL(CD.dblQuantity,0) ELSE 1 END
 		,[dblOrderUnitQty]							=	1
 		,[intOrderUOMId]							=	ISNULL(ItemUOM.intItemUOMId,CD.intItemUOMId)
 		,[dblQuantityToBill]						=	CASE WHEN CC.strCostMethod = 'Per Unit' THEN ISNULL(CD.dblQuantity,0) ELSE 1 END
 		,[dblQtyToBillUnitQty]						=	1
 		,[intQtyToBillUOMId]						=	ISNULL(ItemUOM.intItemUOMId,CD.intItemUOMId)
-		,[dblCost]									=	CASE WHEN CC.strCostMethod = 'Per Unit' THEN ISNULL(CC.dblRate,0) ELSE ISNULL(CC.dblAmount,0) END
+		,[dblCost]									=	CASE	WHEN	CC.strCostMethod IN ('Per Unit', 'Amount', 'Per Container') THEN
+																	ISNULL(CC.dblRate, 0)
+																WHEN	CC.strCostMethod = 'Percentage' THEN 
+																	dbo.fnCTConvertQuantityToTargetItemUOM(CD.intItemId,CD.intUnitMeasureId,PriceUOM.intUnitMeasureId,CD.dblQuantity)*CD.dblCashPrice*CC.dblRate/100
+														END
 		,[dblCostUnitQty]							=	1
 		,[intCostUOMId]								=	ISNULL(CostUOM.intItemUOMId,CD.intItemUOMId)
 		,[dblNetWeight]								=	CASE 
 															WHEN CC.strCostMethod = 'Per Unit' THEN CAST(dbo.fnMFConvertCostToTargetItemUOM(CostUOM.intItemUOMId,WeightUOM.intItemUOMId,CD.dblNetWeight) AS DECIMAL(38,20)) 
 															ELSE 
 																CASE WHEN CostUOM.intItemUOMId IS NULL THEN 0
-																ELSE ISNULL(CC.dblAmount,0) 
+																ELSE 
+																	CASE WHEN CC.strCostMethod = 'Amount' OR CC.strCostMethod = 'Per Container' THEN
+																		CC.dblRate
+																	WHEN	CC.strCostMethod = 'Percentage' THEN 
+																		dbo.fnCTConvertQuantityToTargetItemUOM(CD.intItemId,CD.intUnitMeasureId,PriceUOM.intUnitMeasureId,CD.dblQuantity)*CD.dblCashPrice*CC.dblRate/100
+																	END
 															END
 														END
 		,[dblWeightUnitQty]							=	CAST(1 AS DECIMAL(38,20))
@@ -65,45 +74,40 @@ RETURNS TABLE AS RETURN
 		,[intSubCurrencyCents]						=	CASE WHEN CY.ysnSubCurrency > 0 THEN CY.intCent ELSE 1 END
 		,[intAccountId]								=	apClearing.intAccountId
 		,[intShipViaId]								=	0
-		,[intTermId]								=	CC.intTermId	
+		,[intTermId]								=	term.intTermID
 		,[strBillOfLading]							=	NULL
 		,[ysnReturn]								=	CAST(RT.Item AS BIT)	
-	FROM vyuCTContractCostView CC	
+	FROM tblCTContractCost CC	
 	CROSS APPLY ( select ysnMultiplePriceFixation from tblCTCompanyPreference ) CPT
-	JOIN tblCTContractDetail CD	ON CD.intContractDetailId = CC.intContractDetailId AND (CC.ysnPrice = 1 AND CD.intPricingTypeId IN (1,6) 
-			OR CC.ysnAccrue = CASE 
-				WHEN CPT.ysnMultiplePriceFixation = 0 AND @accrue = 1 THEN 1 
-				ELSE CC.ysnAccrue 
-			END
+	INNER JOIN tblCTContractDetail CD ON CD.intContractDetailId = CC.intContractDetailId AND (CC.ysnPrice = 1 AND CD.intPricingTypeId IN (1,6) 
+			OR (CPT.ysnMultiplePriceFixation = 0 AND @accrue = 1)
 		) 
-		AND (CASE WHEN @remove = 0 AND CC.intConcurrencyId <> CC.intPrevConcurrencyId THEN 1 ELSE @remove END = 1)
-	JOIN tblCTContractHeader CH	ON	CH.intContractHeaderId = CD.intContractHeaderId
-	LEFT JOIN (tblAPVendor D1 INNER JOIN tblEMEntity D2 ON D1.[intEntityId] = D2.intEntityId) ON CC.intVendorId = D1.[intEntityId] 
-
+		AND (@remove = 0 AND CC.intConcurrencyId <> CC.intPrevConcurrencyId OR @remove = 1)
+	INNER JOIN tblCTContractHeader CH ON CH.intContractHeaderId = CD.intContractHeaderId
 	INNER JOIN tblICItem item ON item.intItemId = CC.intItemId 
 	CROSS APPLY tblSMCompanyPreference compPref
 	LEFT JOIN tblSMCurrency CU ON CU.intCurrencyID = CD.intCurrencyId
 	LEFT JOIN tblICItemLocation ItemLoc ON ItemLoc.intItemId = CC.intItemId AND ItemLoc.intLocationId = CD.intCompanyLocationId
-	LEFT JOIN tblICInventoryReceiptCharge RC ON	RC.intContractId = CC.intContractHeaderId AND RC.intChargeId = CC.intItemId
+	LEFT JOIN tblICInventoryReceiptCharge RC ON	RC.intContractId = CH.intContractHeaderId AND RC.intChargeId = CC.intItemId
+	LEFT JOIN tblICItemUOM CCUOM ON CCUOM.intItemUOMId = CC.intItemUOMId
 	LEFT JOIN tblICItemUOM ItemUOM ON ItemUOM.intItemUOMId = CD.intItemUOMId
-	LEFT JOIN tblICItemUOM CostUOM ON CostUOM.intItemId = CD.intItemId AND CostUOM.intUnitMeasureId = CC.intUnitMeasureId
+	LEFT JOIN tblICItemUOM CostUOM ON CostUOM.intItemId = CD.intItemId AND CostUOM.intUnitMeasureId = CCUOM.intUnitMeasureId
 	LEFT JOIN tblICItemUOM WeightUOM ON WeightUOM.intItemId = CD.intItemId AND WeightUOM.intItemUOMId = CD.intNetWeightUOMId
+	LEFT JOIN tblICItemUOM PriceUOM ON PriceUOM.intItemUOMId = CD.intPriceItemUOMId
 	LEFT JOIN tblICUnitMeasure UOM ON UOM.intUnitMeasureId = ItemUOM.intUnitMeasureId
-	LEFT JOIN tblICStorageLocation SLOC ON SLOC.intStorageLocationId = CD.intStorageLocationId
 	LEFT JOIN tblSMCompanyLocationSubLocation subLoc ON	CD.intSubLocationId = subLoc.intCompanyLocationSubLocationId
 	LEFT JOIN tblSMCurrencyExchangeRate F ON  (F.intFromCurrencyId = compPref.intDefaultCurrencyId AND F.intToCurrencyId = CC.intCurrencyId) 
 	LEFT JOIN tblSMCurrencyExchangeRateDetail G1 ON F.intCurrencyExchangeRateId = G1.intCurrencyExchangeRateId AND G1.dtmValidFromDate = (SELECT CONVERT(char(10), GETDATE(),126))
 	LEFT JOIN tblSMCurrency CY ON CY.intCurrencyID = CC.intCurrencyId
 	LEFT JOIN tblSMCurrencyExchangeRate Rate ON Rate.intFromCurrencyId = compPref.intDefaultCurrencyId AND Rate.intToCurrencyId = CU.intMainCurrencyId
 	LEFT JOIN tblSMCurrencyExchangeRateDetail RateDetail ON Rate.intCurrencyExchangeRateId = RateDetail.intCurrencyExchangeRateId
-	LEFT JOIN vyuPATEntityPatron patron ON patron.intEntityId = CC.intItemId
 	LEFT JOIN tblSMCurrencyExchangeRateType rtype ON rtype.intCurrencyExchangeRateTypeId = CC.intRateTypeId
-	LEFT JOIN tblSMTerm term ON term.intTermID =  CC.intTermId
+	LEFT JOIN tblSMTerm term ON term.intTermID =  CH.intTermId
 	OUTER APPLY dbo.fnGetItemGLAccountAsTable(CC.intItemId, ItemLoc.intItemLocationId,  case
-																						when CC.strCostType = 'Other Charges'
-																						or (
+																						when item.strCostType = 'Other Charges'
+																						or exists (
 																								select
-																									count(1)
+																									top 1 1
 																								from
 																									tblICInventoryReceiptItem a
 																									,tblICInventoryReceipt b
@@ -111,18 +115,12 @@ RETURNS TABLE AS RETURN
 																									a.intOrderId = CD.intContractHeaderId
 																									and a.intLineNo = CD.intContractDetailId
 																									and b.intInventoryReceiptId = a.intInventoryReceiptId
-																							) = 0
+																							)
 																						then 'Other Charge Expense'
 																						else 'AP Clearing' 
 																						end
 											) itemAccnt
 	LEFT JOIN dbo.tblGLAccount apClearing ON apClearing.intAccountId = itemAccnt.intAccountId
-	OUTER APPLY 
-	(
-		SELECT TOP 1 dblRate as forexRate from tblSMCurrencyExchangeRateDetail G1
-		WHERE F.intCurrencyExchangeRateId = G1.intCurrencyExchangeRateId AND G1.dtmValidFromDate < (SELECT CONVERT(char(10), GETDATE(),126))
-		ORDER BY G1.dtmValidFromDate DESC
-	) rate
 	CROSS JOIN  dbo.fnSplitString('0,1',',') RT
 	-- FOR REVIEW
 	OUTER APPLY 
@@ -134,11 +132,13 @@ RETURNS TABLE AS RETURN
 			ELSE 0
 		END = 1
 	) payable
-	LEFT JOIN
+	OUTER APPLY
 	(
-		SELECT intEntityId 
+		SELECT TOP 1 intEntityId 
 		FROM tblEMEntity
-	) entity ON entity.intEntityId = CC.intVendorId OR entity.intEntityId = (CASE WHEN CC.ysnPrice = 1 AND CH.intContractTypeId = 1 THEN CH.intEntityId ELSE CC.intVendorId END)
+		WHERE intEntityId = CC.intVendorId OR (CC.ysnPrice = 1 AND CH.intContractTypeId = 1 AND intEntityId = CH.intEntityId)
+	) entity 
+
 	WHERE RC.intInventoryReceiptChargeId IS NULL AND CC.ysnAccrue = @accrue AND --CC.ysnBasis = 0 AND
 	NOT EXISTS(SELECT 1 FROM tblICInventoryShipmentCharge WHERE intContractDetailId = CD.intContractDetailId AND intChargeId = CC.intItemId) AND
 	CASE 
