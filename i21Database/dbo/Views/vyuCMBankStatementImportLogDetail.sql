1696e184c316563f9a8586309de10211b7efb82d
Jeff Trajano <jeffrey.trajano@irely.com>
2020-10-12 13:20:09 +0800

    GL-7678 Import Bank Statement

--- i21Database/dbo/Views/vyuCMBankStatementImportLogDetail.sql
+++ i21Database/dbo/Views/vyuCMBankStatementImportLogDetail.sql
@@ -1,15 +1,19 @@
-
-CREATE view [dbo].[vyuCMBankStatementImportLogDetail]
-as
-select
-A.intId,
-B.intImportBankStatementLogId,
-A.intLineNo,
-A.strTaskId,
-ysnError = case when strError is null then cast(0 as bit) else cast(1 as bit) end ,
-strError =  isnull(strError, 'No error'),
-BS.strBankDescription
-from tblCMBankStatementImportLogDetail A join tblCMBankStatementImportLog B on B.intImportBankStatementLogId = A.intImportBankStatementLogId
-left join
+  
+CREATE view [dbo].[vyuCMBankStatementImportLogDetail]  
+as  
+select  
+A.intId,  
+B.intImportBankStatementLogId,  
+A.intLineNo,  
+A.strTaskId,  
+ysnError = case when strError is null then cast(0 as bit) else cast(1 as bit) end ,  
+strError =  
+case when strError is null and  BS.intBankStatementImportId is not null and T.intBankStatementImportId is null then  'Imported' 
+when strError is null and  BS.intBankStatementImportId is not null and T.intBankStatementImportId is not null then  'Task Created'
+else A.strError
+end,
+BS.strBankDescription  
+from tblCMBankStatementImportLogDetail A join tblCMBankStatementImportLog B on B.intImportBankStatementLogId = A.intImportBankStatementLogId  
+left join  
 tblCMBankStatementImport BS on BS.intBankStatementImportId = A.intBankStatementImportId
-
+left join tblCMResponsiblePartyTask T on T.intBankStatementImportId = A.intBankStatementImportId
