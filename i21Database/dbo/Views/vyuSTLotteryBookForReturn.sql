20e756eff8a6d5f27c9ff95e31d7ecb50b7b5141
bonn ponce <bonn.ponce@irely.com>
2020-10-14 13:45:14 +0800

    ST-1830
    (cherry picked from commit 7aa9f0b4d796134bba5724cdd061dfefe3284697)

---
+++ i21Database/dbo/Views/vyuSTLotteryBookForReturn.sql
@@ -0,0 +1,33 @@
+﻿
+CREATE VIEW vyuSTLotteryBookForReturn
+AS
+SELECT 
+dblQuantityRemaining,
+dtmActivateDate,
+dtmReceiptDate,
+tblSTLotteryBook.intConcurrencyId,
+intLotteryBookId,
+tblSTLotteryGame.intLotteryGameId,
+tblSTStore.intStoreId,
+intBinNumber,
+strBookNumber,
+tblSTLotteryBook.strStatus,
+strCountDirection,
+strGame = tblSTLotteryGame.strGame,
+intStoreNo = tblSTStore.intStoreNo,
+strStoreDescription = tblSTStore.strDescription,
+strItemDescription = tblICItem.strDescription,
+dblTicketCost = tblSTLotteryGame.dblInventoryCost,
+dblTicketValue = tblSTLotteryGame.dblTicketValue,
+strState = tblSTStore.strState 
+FROM tblSTLotteryBook
+INNER JOIN tblSTLotteryGame
+ON tblSTLotteryBook.intLotteryGameId = tblSTLotteryGame.intLotteryGameId
+INNER JOIN tblSTStore 
+ON tblSTLotteryBook.intStoreId = tblSTStore.intStoreId
+LEFT JOIN tblICItem 
+ON tblSTLotteryGame.intItemId = tblICItem.intItemId 
+WHERE LOWER(tblSTLotteryBook.strStatus) != 'returned' 
+AND LOWER(tblSTLotteryBook.strStatus) != 'sold'
+
+
