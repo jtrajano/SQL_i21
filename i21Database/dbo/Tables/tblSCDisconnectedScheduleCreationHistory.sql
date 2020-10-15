572d6745e7eb84162bcd2fcab355eb3c501961ef
Kevin Estrada <kevin.estrada071993@gmail.com>
2020-10-15 18:14:35 +0800

    SC-3769
    (cherry picked from commit 92fbcfdd6d1e73baa4d151cdfa2012c50ac3b1f8)

---
+++ i21Database/dbo/Tables/tblSCDisconnectedScheduleCreationHistory.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [tblSCDisconnectedScheduleCreationHistory]
+(
+[intHistoryId]		INT IDENTITY (1, 1) ,
+[strJobId]			NVARCHAR(MAX) NULL,
+[strStatus]			NVARCHAR(200) NULL,
+[strRemarks]		NVARCHAR(MAX) NULL,
+[strTempJobId]		NVARCHAR(MAX) NULL,
+[dtmDateOfExecution] DATETIME NULL,
+[dtmDateCreated]	DATETIME NULL,
+[dtmDateStarted]	DATETIME NULL,
+[dtmDateCompleted]	DATETIME NULL,
+[intConcurrencyId]	INT NOT NULL DEFAULT(1),
+CONSTRAINT [PK_tblSCDisconnectedScheduleCreationHistory_intHistoryId] primary key clustered ([intHistoryId] ASC)
+
+)
