7298736a2cab048770707f82a7426567fd9921b4
Erick Dela Cruz <erick.delacruz@irely.com>
2020-10-16 13:57:17 +0800

    AP-9177

--- i21Database/dbo/Views/vyuAPREceiptClearing.sql
+++ i21Database/dbo/Views/vyuAPREceiptClearing.sql
@@ -51,7 +51,8 @@ SELECT
         END
     )
     +
-    CASE WHEN ISNULL(voucherTax.intCount,0) = 0 THEN 0 ELSE receiptItem.dblTax END
+    --CASE WHEN ISNULL(voucherTax.intCount,0) = 0 THEN 0 ELSE receiptItem.dblTax END
+    ISNULL(clearingTax.dblTax,0)
     AS dblReceiptTotal
     ,CASE	
         WHEN receiptItem.intWeightUOMId IS NULL THEN 
