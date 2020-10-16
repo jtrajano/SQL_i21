b8dc7c93be690a5c6eeebe19f37dcb2e9cd0e644
cris.lazaga <cris.lazaga@irely.com>
2020-10-16 16:03:16 +0800

    MFT-1504

---
+++ i21Database/dbo/User Defined Types/TFLocality.sql
@@ -0,0 +1,8 @@
+﻿CREATE TYPE [dbo].[TFLocality] AS TABLE
+(
+	[intLocalityId] INT NOT NULL,
+	[strLocalityCode] NVARCHAR(10) COLLATE Latin1_General_CI_AS NOT NULL,
+	[strLocalityZipCode] NVARCHAR(10) COLLATE Latin1_General_CI_AS NOT NULL,
+	[strLocalityName] NVARCHAR(100) COLLATE Latin1_General_CI_AS NOT NULL,
+	[intMasterId] INT NULL
+)
