c86742fc038055711c8dd42ca6892c0a9ea03cd2
Leslie Guevarra <leslie.arat@irely.com>
2020-10-16 13:46:14 +0800

    GRN-2377 (Settle Storage: posting incorrect qty in inventory valuation)

--- i21Database/dbo/Stored Procedures/uspGRPostSettleStorage.sql
+++ i21Database/dbo/Stored Procedures/uspGRPostSettleStorage.sql
@@ -622,6 +622,7 @@ BEGIN TRY
 			FROM @avqty A
 			INNER JOIN tblGRSettleContract B
 				ON B.intSettleStorageId = @intSettleStorageId
+					AND B.intContractDetailId = A.intContractDetailId
 			WHERE A.dblAvailableQuantity > 0
 				AND A.intPriceFixationDetailId IS NOT NULL
 				AND A.intPricingTypeId = 1 AND A.intPricingTypeIdHeader = 2
@@ -1249,7 +1250,7 @@ BEGIN TRY
 					,dblCashPriceUsed		
 				FROM @SettleVoucherCreate SVC
 				OUTER APPLY (
-					SELECT * FROM tblGRSettleContractPriceFixationDetail WHERE intSettleStorageId = @intSettleStorageId
+					SELECT * FROM tblGRSettleContractPriceFixationDetail WHERE intSettleStorageId = @intSettleStorageId AND intContractDetailId = SVC.intContractDetailId
 				) A
 
 				--SELECT 'TESTTTT', SS.* ,dblNetSettlement.*,dblDiscountDue.*
