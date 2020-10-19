9ae37833107dfde9087f6f32b6eddd829efff6ef
Feb Montefrio <feb.montefrio@irely.com>
2020-10-19 22:23:44 +0800

    [B] IC-8992 IC-8724

--- i21Database/dbo/Stored Procedures/uspICConvertReceiptToVoucher.sql
+++ i21Database/dbo/Stored Procedures/uspICConvertReceiptToVoucher.sql
@@ -313,7 +313,7 @@ BEGIN
 					vyuICGetInventoryReceiptVoucher
 				WHERE 
 					intInventoryReceiptId = @intReceiptId
-					AND dblQtyToVoucher = dblQtyToReceive
+					AND dblQtyToReceive = dblQtyVouchered
 			)
 			BEGIN
 				IF @billTypeToUse = @type_Voucher
