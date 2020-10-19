bfcaf8567cf3263962a7d56cf2650c49dfa6c76e
Vinoth Kumar T <vinoth.t@irely.com>
2020-10-19 17:22:13 +0530

    IN-652

---
+++ i21Database/dbo/Tables/tblIPLoadDocumentsError.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE tblIPLoadDocumentsError
+(
+	intStageLoadDocumentsId		INT IDENTITY(1,1),
+	intStageLoadId				INT NOT NULL,
+	strCustomerReference		NVARCHAR(100) COLLATE Latin1_General_CI_AS,
+	strTypeCode					NVARCHAR(50) COLLATE Latin1_General_CI_AS,
+	strName						NVARCHAR(50) COLLATE Latin1_General_CI_AS,
+	intOriginal					INT,
+	intCopies					INT,
+
+	CONSTRAINT [PK_tblIPLoadDocumentsError_intStageLoadDocumentsId] PRIMARY KEY (intStageLoadDocumentsId),
+	CONSTRAINT [FK_tblIPLoadDocumentsError_tblIPLoadError_intStageLoadId] FOREIGN KEY ([intStageLoadId]) REFERENCES [tblIPLoadError]([intStageLoadId]) ON DELETE CASCADE
+)
