24cb85af9cfe82b4725c4b19b645de275fbddea5
bonn ponce <bonn.ponce@irely.com>
2020-10-20 15:56:16 +0800

    ST-1825
    (cherry picked from commit 09ff7cb6afe022b66fa3b6691adf0f6957536832)

---
+++ i21Database/dbo/Tables/tblSTLotteryBookProcessError.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[tblSTLotteryBookProcessError] (
+    [intLotteryBookProcessErrorId]   INT            IDENTITY (1, 1) NOT NULL,
+    [intUserId]						 INT            NULL,
+    [strBookNumber]					 NVARCHAR(MAX)  NULL,
+    [strGame]						 NVARCHAR(MAX)  NULL,
+    [strError]						 NVARCHAR(MAX)  NULL,
+    [strProcess]					 NVARCHAR(MAX)  NULL,
+    [intConcurrencyId]				 INT NULL CONSTRAINT [DF_tblSTLotteryBookProcessError_intConcurrencyId]  DEFAULT ((1)),
+    CONSTRAINT [PK_tblSTLotteryBookProcessError] PRIMARY KEY CLUSTERED ([intLotteryBookProcessErrorId] ASC) WITH (FILLFACTOR = 70),
+);
