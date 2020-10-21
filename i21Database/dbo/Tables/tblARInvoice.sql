d62456d19b334aff5c1f07e13194908501985988
Jeremiah Villanueva <jeremiah.villanueva@irely.com>
2020-10-22 00:31:24 +0800

    AR-12420

--- i21Database/dbo/Tables/tblARInvoice.sql
+++ i21Database/dbo/Tables/tblARInvoice.sql
@@ -129,6 +129,7 @@
 	[strBillingMethod]					NVARCHAR(100)   COLLATE Latin1_General_CI_AS 	NULL,
 	[strApplicatorLicense]				NVARCHAR(50)    COLLATE Latin1_General_CI_AS 	NULL,
 	[intPeriodId]						INT												NULL,
+    [intUserIdforDelete]				INT												NULL,
     CONSTRAINT [PK_tblARInvoice_intInvoiceId] PRIMARY KEY CLUSTERED ([intInvoiceId] ASC),
 	CONSTRAINT [UK_tblARInvoice_strInvoiceNumber] UNIQUE ([strInvoiceNumber]),
     CONSTRAINT [FK_tblARInvoice_tblARCustomer_intEntityCustomerId] FOREIGN KEY ([intEntityCustomerId]) REFERENCES [dbo].[tblARCustomer] ([intEntityId]),
@@ -294,4 +295,38 @@ BEGIN
 		DELETE A
 		FROM tblARInvoice A
 		INNER JOIN DELETED B ON A.intInvoiceId = B.intInvoiceId
+END
+
+GO
+CREATE TRIGGER trgAfterDeleteARInvoice
+    ON dbo.tblARInvoice
+    FOR DELETE
+AS
+BEGIN
+	IF EXISTS (SELECT * FROM DELETED)
+		BEGIN
+			--iNSERT
+		   INSERT INTO  tblARAuditLog
+		   SELECT 'Deleted','Invoice',strInvoiceNumber,GETDATE(),intUserIdforDelete,0 FROM DELETED
+
+		END
+END
+
+GO
+
+CREATE TRIGGER trgForUpdateARInvoice 
+ON dbo.tblARInvoice
+	FOR UPDATE 
+AS
+BEGIN
+	
+		DECLARE @strInvoiceNumber NVARCHAR (50) 
+		SELECT @strInvoiceNumber=i.strInvoiceNumber from deleted i; IF UPDATE(strInvoiceNumber)
+
+		IF @strInvoiceNumber IS NOT  NULL
+		BEGIN
+		IF UPDATE (strInvoiceNumber)
+		INSERT INTO  tblARAuditLog
+		   SELECT 'Updated','Invoice',strInvoiceNumber,GETDATE(),NULL,0 FROM deleted
+		END
 END
