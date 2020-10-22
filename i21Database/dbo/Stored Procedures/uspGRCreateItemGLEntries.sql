4826b535e9bb6007c9cb35a4d9b8e84c0c194fa9
Feb Montefrio <feb.montefrio@irely.com>
2020-10-22 21:47:40 +0800

    [B] IC-9001
    # Conflicts:
    #	i21Database/dbo/Stored Procedures/uspGRCreateItemGLEntries.sql

--- i21Database/dbo/Stored Procedures/uspGRCreateItemGLEntries.sql
+++ i21Database/dbo/Stored Procedures/uspGRCreateItemGLEntries.sql
@@ -368,7 +368,7 @@ SELECT
 	,dblDebitUnit				= DebitUnit.Value
 	,dblCreditUnit				= CreditUnit.Value 
 	,strDescription				= ISNULL(@strGLDescription, ISNULL(tblGLAccount.strDescription, '')) + ' ' + dbo.[fnICDescribeSoldStock](strItemNo, dblQty, dblItemCost) 
-	,strCode					= 'STR' 
+	,strCode					= 'IC' 
 	,strReference				= '' 
 	,intCurrencyId				= ForGLEntries_CTE.intCurrencyId
 	,dblExchangeRate			= ForGLEntries_CTE.dblExchangeRate
@@ -432,7 +432,7 @@ SELECT
 	,dblDebitUnit				= CreditUnit.Value
 	,dblCreditUnit				= DebitUnit.Value
 	,strDescription				= ISNULL(@strGLDescription, ISNULL(tblGLAccount.strDescription, '')) + ' ' + dbo.[fnICDescribeSoldStock](strItemNo, dblQty, dblItemCost) 
-	,strCode					= 'STR' 
+	,strCode					= 'IC' 
 	,strReference				= '' 
 	,intCurrencyId				= ForGLEntries_CTE.intCurrencyId
 	,dblExchangeRate			= ForGLEntries_CTE.dblExchangeRate
