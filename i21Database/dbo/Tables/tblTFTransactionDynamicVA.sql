c4ff9e485c6493821e7fabe02de691d319487773
cris.lazaga <cris.lazaga@irely.com>
2020-10-23 10:12:25 +0800

    MFT-1506

--- i21Database/dbo/Tables/tblTFTransactionDynamicVA.sql
+++ i21Database/dbo/Tables/tblTFTransactionDynamicVA.sql
@@ -2,11 +2,10 @@
 (
 	[intTransactionDynamicId] [int] IDENTITY(1,1) NOT NULL,	
 	[intTransactionId] [int] NOT NULL,
-	[strVALocalityCode] [nvarchar](10) NULL,
-	[strVALocalityName] [nvarchar](150) NULL,
-	[strVADestinationAddress] [nvarchar](250) NULL,
-	[strVADestinationZipCode] [nvarchar](10) NULL,
-	[strVAReturnType] [nvarchar](5) NULL,
+	[strVALocalityCode] [nvarchar](10) COLLATE Latin1_General_CI_AS NULL,
+	[strVALocalityName] [nvarchar](150) COLLATE Latin1_General_CI_AS NULL,
+	[strVADestinationAddress] [nvarchar](250) COLLATE Latin1_General_CI_AS NULL,
+	[strVADestinationZipCode] [nvarchar](10) COLLATE Latin1_General_CI_AS NULL,
 	[intConcurrencyId] [int] NULL,
 	CONSTRAINT [FK_tblTFTransactionDynamicVA_tblTFTransaction] FOREIGN KEY ([intTransactionId]) REFERENCES [tblTFTransaction]([intTransactionId]) ON DELETE CASCADE,
 	CONSTRAINT [PK_tblTFTransactionDynamicVA] PRIMARY KEY ([intTransactionDynamicId])
