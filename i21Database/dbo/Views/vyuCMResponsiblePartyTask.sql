20094496cf07fa0486b3c27ec3de11a2bf3e2ccf
Jeff Trajano <jeffrey.trajano@irely.com>
2020-10-26 14:13:59 +0800

    GL-7870 [Search Task Created] Filtering and sorting is not working properly

--- i21Database/dbo/Views/vyuCMResponsiblePartyTask.sql
+++ i21Database/dbo/Views/vyuCMResponsiblePartyTask.sql
@@ -1,39 +1,41 @@
-	CREATE VIEW vyuCMResponsiblePartyTask            
-	AS            
-	SELECT             
-	   S.intBankStatementImportId,            
-	   S.strBankStatementImportId,            
-	   S.strBankDescription,            
-	   S.dblAmount,            
-	   S.dtmDate,            
-	   S.strPayee,            
-	   S.strReferenceNo,            
-	   S.dtmCreated,            
-	   T.intResponsibleBankAccountId,            
-	   E.strName strResponsibleEntity,            
-	   T.strTransactionId strRelatedTransaction,            
-	   S.intBankAccountId,            
-	   S.strRTN,            
-	   S.dblDepositAmount,            
-	   S.dblWithdrawalAmount,            
-	   S.intImportStatus,            
-	   S.strDebitCredit,            
-	   T.intEntityId,            
-	   T.intTaskId,            
-	   T.strTaskId,        
-	   T.intConcurrencyId,          
-	   BFr.strBankAccountNo strBankAccountFrom,         
-	   BFr.intBankAccountId intBankAccountFrom,      
-	   BT.ysnPosted,   
-	   CASE WHEN isnull(PostedDetail.Amount,0) = T.dblAmount AND BT.ysnPosted = 1 THEN CAST(1 AS BIT) ELSE CAST(0 AS BIT) END  ysnStatus,  
-	   BTo.strBankAccountNo strResponsibleBankAccount,            
-	   BTo.intGLAccountId intGLAccountIdTo 
-	FROM tblCMResponsiblePartyTask T            
-	JOIN tblCMBankStatementImport S ON S.intBankStatementImportId = T.intBankStatementImportId            
-	JOIN tblEMEntity E on E.intEntityId = T.intEntityId            
-	JOIN tblCMBankTransfer BT on BT.strTransactionId = T.strTransactionId          
-	JOIN vyuCMBankAccount BFr on BT.intBankAccountIdFrom = BFr.intBankAccountId    
-	JOIN vyuCMBankAccount BTo on BT.intBankAccountIdTo = BTo.intBankAccountId   
-	OUTER APPLY(    
-	 SELECT sum(dblAmount) Amount  FROM vyuCMResponsiblePartyTaskDetail WHERE ysnPosted = 1 AND intTaskId = T.intTaskId    
-	)PostedDetail
+ CREATE VIEW dbo.vyuCMResponsiblePartyTask
+ AS
+  WITH BT AS(
+ SELECT ysnPosted, dblAmount, dtmDate, intTransactionId, strTransactionId FROM tblCMBankTransaction union
+ select ysnPosted, dblAmount, dtmDate, intTransactionId, strTransactionId from tblCMBankTransfer
+ )
+ SELECT                 
+    S.intBankStatementImportId,
+    S.strBankStatementImportId,
+    T.strNotes,
+    ISNULL(BT.dblAmount, T.dblAmount) dblAmount,
+	BT.dtmDate dtmTransactionDate,
+    T.dtmDateCreated,
+    E.strName strResponsibleEntity,
+	T.strTransactionId strRelatedTransaction,
+    T.intEntityId,
+    T.intTaskId,
+    T.strTaskId,
+    T.intConcurrencyId,
+    T.ysnStatus,
+    T.intResponsibleBankAccountId,
+    BA.strBankAccountNo strResponsibleBankAccountNo,
+    BT.ysnPosted,
+    T.intActionId,
+    BT.intTransactionId,
+    CASE WHEN T.intActionId = 0  then 'Ignore'
+    when T.intActionId = 1 then 'Notify Only' 
+    when T.intActionId = 2 then 'Clear Check' 
+    when T.intActionId = 3 then 'Bank Transfer'
+    when T.intActionId = 4 then 'Bank Deposit' 
+    else ''END strActionType
+
+ FROM tblCMResponsiblePartyTask T
+ JOIN tblCMBankStatementImport S ON S.intBankStatementImportId = T.intBankStatementImportId
+ JOIN tblEMEntity E on E.intEntityId = T.intEntityId
+ LEFT JOIN BT on BT.strTransactionId = T.strTransactionId
+ LEFT JOIN vyuCMBankAccount BA on BA.intBankAccountId = T.intResponsibleBankAccountId
+
+ 
+
+   
