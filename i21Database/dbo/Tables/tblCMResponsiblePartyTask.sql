936b5865d0aca31de16a1a682b6f3ce11e753fee
Jeff Trajano <jeffrey.trajano@irely.com>
2020-10-27 09:27:48 +0800

    GL-7870 [Search Task Created] Filtering and sorting is not working properly

--- i21Database/dbo/Tables/tblCMResponsiblePartyTask.sql
+++ i21Database/dbo/Tables/tblCMResponsiblePartyTask.sql
@@ -1,23 +1,21 @@
 CREATE TABLE [dbo].[tblCMResponsiblePartyTask](
-	[intTaskId] [int] NOT NULL,
+	[intTaskId] [int] IDENTITY(1,1) NOT NULL,
 	[strTaskId] [nvarchar](20) COLLATE Latin1_General_CI_AS NULL,
-	[intResponsibleBankAccountId] [int] NOT NULL,
+	[intResponsibleBankAccountId] [int] NULL,
+	[intActionId] INT NULL,
 	[intEntityId] [int] NULL,
 	[strTransactionId] [nvarchar](20) COLLATE Latin1_General_CI_AS NULL,
 	[dtmDateCreated] [datetime] NOT NULL,
 	[intConcurrencyId] [int] NOT NULL,
 	[ysnStatus] [bit] NULL,
-	[dblAmount] [decimal](18, 6) NULL,
-	[strBankStatementImportId] [nvarchar](20) COLLATE Latin1_General_CI_AS NULL,
+	[strNotes] [nvarchar](500) COLLATE Latin1_General_CI_AS NULL,
 	[intBankStatementImportId] [int] NULL,
- CONSTRAINT [PK_tblCMResponsiblePartyTask] PRIMARY KEY CLUSTERED 
+	[dblAmount] DECIMAL(18,6) NULL
+ CONSTRAINT [PK_tblCMResponsiblePartyTask] PRIMARY KEY CLUSTERED
 (
 	[intTaskId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
-GO
-
-ALTER TABLE [dbo].[tblCMResponsiblePartyTask] ADD  CONSTRAINT [DF_tblCMResponsiblePartyTask_dtmDateCreated]  DEFAULT (getdate()) FOR [dtmDateCreated]
 GO
 
 ALTER TABLE [dbo].[tblCMResponsiblePartyTask] ADD  CONSTRAINT [DF_tblCMResponsiblePartyTask_intConcurrencyId]  DEFAULT ((1)) FOR [intConcurrencyId]
