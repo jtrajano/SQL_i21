25b622c71f7c6e15255c50e7adcb7a31d90aef1e
Jeff Trajano <jeffrey.trajano@irely.com>
2020-10-30 10:31:10 +0800

    GL-7880 Warn if Errors to Continue or Cancel

--- i21Database/dbo/Views/vyuCMBankStatementImportLog.sql
+++ i21Database/dbo/Views/vyuCMBankStatementImportLog.sql
@@ -1,7 +1,12 @@
 CREATE VIEW vyuCMBankStatementImportLog
 AS
 SELECT A.*,
+Detail.intErrorCount,
 B.strName
 FROM tblCMBankStatementImportLog A
 LEFT JOIN
 tblEMEntity B on A.intEntityId = B.intEntityId
+OUTER APPLY(
+    SELECT intErrorCount = COUNT(*) FROM tblCMBankStatementImportLogDetail WHERE ISNULL(strError,'') <> ''
+
+)Detail
