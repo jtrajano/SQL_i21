9891d14f94c1477f06d1a7f536f6f9b5ed3ab12d
Christian Joel Martinez <Christian.Martinez@iRely.com>
2020-10-30 10:22:46 +0800

    SC-3902

--- i21Database/dbo/Views/vyuGRGetStorageTickets.sql
+++ i21Database/dbo/Views/vyuGRGetStorageTickets.sql
@@ -23,7 +23,7 @@ SELECT DISTINCT
     ,intContractHeaderId                = case when (ST.ysnDPOwnedType = 1 and GHistory.intContractHeaderId is not null) then GHistory.intContractHeaderId else CASE WHEN CS.ysnTransferStorage = 0 THEN CH_Ticket.intContractHeaderId ELSE CH_Transfer.intContractHeaderId END end
     ,intContractDetailId				= case when (ST.ysnDPOwnedType = 1 and GHistory.intContractDetailId is not null) then GHistory.intContractDetailId else CASE WHEN CS.ysnTransferStorage = 0 THEN CD_Ticket.intContractDetailId ELSE CD_Transfer.intContractDetailId END end
     ,strContractNumber					= case when (ST.ysnDPOwnedType = 1 and GHistory.intContractHeaderId is not null) then GHistory.strContractNumber else CASE WHEN CS.ysnTransferStorage = 0 THEN CH_Ticket.strContractNumber ELSE CH_Transfer.strContractNumber END end
-    ,intTicketId						= ISNULL(SC.intTicketId,0)
+    ,intTicketId						= SC.intTicketId
     ,dblDiscountUnPaid					= ISNULL(dblDiscountsDue,0) - ISNULL(dblDiscountsPaid,0)
     ,dblStorageUnPaid					= ISNULL(dblStorageDue,0) - ISNULL(dblStoragePaid,0)
     ,strReceiptNumber					= IR.strReceiptNumber
