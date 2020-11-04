d1a6dfd8a5bc1aafd3d644023bc583ecf7b15212
Jeff Trajano <jeffrey.trajano@irely.com>
2020-11-04 18:06:21 +0800

    GL-7884 Issue in Conversion of Historical Journal

--- i21Database/dbo/Stored Procedures/uspGLImportOriginHistoricalJournal.sql
+++ i21Database/dbo/Stored Procedures/uspGLImportOriginHistoricalJournal.sql
@@ -1,5 +1,6 @@
 ﻿CREATE PROCEDURE  [dbo].[uspGLImportOriginHistoricalJournal]
-@intEntityId		INT
+@intEntityId INT,
+@result	NVARCHAR(MAX) OUTPUT
 AS
 IF EXISTS (SELECT TOP 1 1 FROM tblSMCompanyPreference WHERE ysnLegacyIntegration = 1)
 	RAISERROR('Import Historical Procedure is not available', 16, 1);
