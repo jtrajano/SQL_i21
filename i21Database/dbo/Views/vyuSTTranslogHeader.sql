fbfd8f4ee5f5ef335c555e075702341bf49e412e
nelsonreyes19 <nelson.reyes@irely.com>
2020-11-13 09:00:02 +0800

    ST-1798
    Added required fields for new columns in report

--- i21Database/dbo/Views/vyuSTTranslogHeader.sql
+++ i21Database/dbo/Views/vyuSTTranslogHeader.sql
@@ -2,19 +2,21 @@
 AS
 SELECT ROW_NUMBER() OVER (ORDER BY TRR.intTermMsgSN ASC) AS intId 
       , CAST(TRR.intTermMsgSN AS NVARCHAR(MAX)) + '0' +  CAST(TRR.intTermMsgSNterm AS NVARCHAR(MAX)) + '0' + CAST(TRR.intStoreId AS NVARCHAR(MAX)) 
-		--+ CAST(TRR.intEntityId AS NVARCHAR(MAX)) 
-		COLLATE Latin1_General_CI_AS AS strUniqueId
+		  --+ CAST(TRR.intEntityId AS NVARCHAR(MAX)) 
+		  COLLATE Latin1_General_CI_AS AS strUniqueId
       , TRR.intTermMsgSN
 	  , TRR.intStoreId
 	  , TRR.intCheckoutId
 	  , TRR.dtmCheckoutDate
+	  , TRR.dtmClosedTime
 	  , CAST(TRR.intStoreNo AS NVARCHAR(100)) + ' ' + TRR.strStoreDescription AS strStoreDescription
 	  , TRR.strTransType
 	  , TRR.dtmDate
 	  , TRR.strCashier
 	  , TRR.dblTrpAmt
+	  , TRR.dblTrlQty AS dblTotalTrlQty
 	  , TRR.intCompanyLocationId
-	  -- , TRR.intEntityId
+	  --, TRR.intEntityId
 FROM
 (   
 	SELECT DISTINCT
@@ -29,12 +31,10 @@ FROM
 		 , ST.strDescription AS strStoreDescription
 		 , ST.intCompanyLocationId
 		 , CH.dtmCheckoutDate
+		 , TR.dtmClosedTime
 		 , TR.dblTrpAmt
-		 -- , RolePerm.intCompanyLocationId AS intUserCompanyLocationId
-		 -- , USec.ysnStoreManager AS ysnIsUserStoreManager
-		 -- , USec.ysnAdmin AS ysnIsUserAdmin
-		 -- , USec.strDashboardRole
-		 -- , USec.intEntityId
+		 , SUM(TR.dblTrlQty) AS dblTrlQty
+		 --, USec.intEntityId
 	FROM tblSTTranslogRebates TR
 	--JOIN tblSTCheckoutHeader CH 
 	LEFT JOIN tblSTCheckoutHeader CH 
@@ -49,6 +49,20 @@ FROM
 		-- AND ST.intCompanyLocationId = RolePerm.intCompanyLocationId
 	WHERE TR.strTrpPaycode = 'CASH'
 	AND TR.dblTrpAmt > 0
-	--ORDER BY TR.dtmDate OFFSET 0 ROWS
+	GROUP BY 
+		TR.intTermMsgSN
+		, TR.intTermMsgSNterm
+		, TR.intStoreId
+		, TR.intCheckoutId 
+		, TR.strTransType
+		, TR.dtmDate
+		, TR.strCashier
+		, ST.intStoreNo
+		, ST.strDescription
+		, ST.intCompanyLocationId
+		, CH.dtmCheckoutDate
+		, TR.dtmClosedTime
+		 , TR.dblTrpAmt
+	--OR, TR.dblTrpAmtDER BY TR.dtmDate OFFSET 0 ROWS
 ) TRR
 GO
