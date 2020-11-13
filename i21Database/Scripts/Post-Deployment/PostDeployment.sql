44653c195d9c290b58330ed0a4d08e40321ec001
Jeff Trajano <jeffrey.trajano@irely.com>
2020-11-13 15:37:20 +0800

    GL-7905 Deployment error on proddev19.1

--- i21Database/Scripts/Post-Deployment/PostDeployment.sql
+++ i21Database/Scripts/Post-Deployment/PostDeployment.sql
@@ -612,6 +612,6 @@ print 'BEGIN POST DEPLOYMENT'
 :r .\CM\3_PopulateSourceSystemData.sql
 :r .\CM\4_DataFix.sql
 :r .\CM\5_UndepositedFund.sql
-:R .\CM\6_EnableTriggers.sql
+:r .\CM\6_EnableTriggers.sql
 
 print 'END POST DEPLOYMENT'
