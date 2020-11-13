49d71b32c9ca7709d1d54f6a163352e84beab969
Jeff Trajano <jeffrey.trajano@irely.com>
2020-11-13 15:14:42 +0800

    GL-7905 Deployment error on proddev19.1

--- i21PreDeployment/Scripts/Pre-Deployment/2_CM/1_CM.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/2_CM/1_CM.sql
@@ -22,3 +22,10 @@ BEGIN
 END
 GO
 
+IF EXISTS(SELECT TOP 1 1 FROM sys.objects WHERE [name] = N'trgCMBankTransaction' AND [type] = 'TR')
+	EXEC('DISABLE TRIGGER dbo.trgCMBankTransaction ON tblCMBankTransaction')
+IF EXISTS(SELECT TOP 1 1 FROM sys.objects WHERE [name] = N'trgCMBankTransactionDetail' AND [type] = 'TR')
+	EXEC('DISABLE TRIGGER dbo.trgCMBankTransactionDetail ON tblCMBankTransactionDetail')
+
+
+
