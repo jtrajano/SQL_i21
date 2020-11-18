91237ecddb81558c1b87b12354d4eb3ea266327e
kelvin_quinto <kelvin.quinto@irely.com>
2020-11-18 09:25:37 +0800

    Revert "AR-12571"
    This reverts commit 1becff5cdb462d4878979116f03b4c4cba9fb907.

--- i21Database/dbo/Stored Procedures/uspARCreateInvoiceFromShipment.sql
+++ i21Database/dbo/Stored Procedures/uspARCreateInvoiceFromShipment.sql
@@ -919,7 +919,8 @@ IF EXISTS (SELECT TOP 1 NULL FROM #CONTRACTSPRICING)
 									END
 
 								UPDATE @EntriesForInvoice
-								SET dblPrice		= @dblFinalPrice
+								SET dblQtyOrdered	= @dblOriginalQtyShipped
+								  , dblPrice		= @dblFinalPrice
 								  , dblUnitPrice	= @dblFinalPrice
 								WHERE intId = @intInvoiceEntriesId
 
@@ -1014,7 +1015,7 @@ IF EXISTS (SELECT TOP 1 NULL FROM #CONTRACTSPRICING)
 									, strDocumentNumber
 									, strItemDescription
 									, intOrderUOMId
-									, dblQtyOrdered
+									, dblQtyOrdered				= @dblOriginalQtyShipped
 									, intItemUOMId
 									, intPriceUOMId
 									, dblContractPriceUOMQty
