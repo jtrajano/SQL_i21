2736347d7b51c7502ee023f57c29e59930a788ae
Jeff Trajano <jeffrey.trajano@irely.com>
2020-11-19 16:39:18 +0800

    Revert GL-7901 Prevent Edits to Posted Bank Transactions

--- i21Database/Scripts/Post-Deployment/CM/EnableTriggers.sql
+++
@@ -1,5 +0,0 @@
-
-IF EXISTS(SELECT TOP 1 1 FROM sys.objects WHERE [name] = N'trgCMBankTransaction' AND [type] = 'TR')
-	EXEC ('ENABLE TRIGGER dbo.trgCMBankTransaction ON tblCMBankTransaction')
-IF EXISTS(SELECT TOP 1 1 FROM sys.objects WHERE [name] = N'trgCMBankTransactionDetail' AND [type] = 'TR')
-	EXEC('ENABLE TRIGGER dbo.trgCMBankTransactionDetail ON tblCMBankTransactionDetail')
