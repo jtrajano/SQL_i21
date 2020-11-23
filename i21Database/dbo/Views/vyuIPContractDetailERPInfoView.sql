5c3ed2606ffdc03ba8620f91ee9954d1f0667fb8
Premkumar S <prem.selladurai@irely.com>
2020-11-23 22:44:05 +0530

    IN-674

---
+++ i21Database/dbo/Views/vyuIPContractDetailERPInfoView.sql
@@ -0,0 +1,7 @@
+﻿CREATE VIEW vyuIPContractDetailERPInfoView
+AS
+SELECT CD.intContractHeaderId 
+		,CD.intContractDetailRefId
+		,CD.strERPPONumber
+		,CD.strERPItemNumber
+FROM tblCTContractDetail CD
