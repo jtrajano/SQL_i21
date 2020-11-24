779e8981dea8bf023c0ee31bf595c92548eccbaa
Roudinee Panteria <roudinee.panteria@irely.com>
2020-11-24 08:25:22 +0800

    AR-12170

--- i21Database/dbo/Stored Procedures/uspARPopulateItemsForCosting.sql
+++ i21Database/dbo/Stored Procedures/uspARPopulateItemsForCosting.sql
@@ -265,8 +265,8 @@ WHERE
 	AND (ARID.[intStorageScheduleTypeId] IS NULL OR ISNULL(ARID.[intStorageScheduleTypeId],0) = 0)
 	AND (ARID.intLoadDetailId IS NULL OR (ARID.intLoadDetailId IS NOT NULL AND ISNULL(LGL.[intPurchaseSale], 0) NOT IN (2, 3)))
 	AND (((ISNULL(T.[intTicketTypeId], 0) <> 9 AND (ISNULL(T.[intTicketType], 0) <> 6 OR ISNULL(T.[strInOutFlag], '') <> 'O')) AND ISNULL(ARID.[intTicketId], 0) <> 0) OR ISNULL(ARID.[intTicketId], 0) = 0)
-	AND (ARID.[ysnFromProvisional] = 0 OR (ARID.[ysnFromProvisional] = 1 AND (ARID.[intOriginalInvoiceDetailId] IS NULL OR (ARID.[intSourceId] = 2 AND ARID.[dblQtyShipped] <> ARIDP.[dblQtyShipped]))))
 
+-- Final Invoice
 INSERT INTO #ARItemsForCosting
 	([intItemId]
 	,[intItemLocationId]
@@ -299,163 +299,6 @@ SELECT
 	,ARIC.[intItemLocationId]
 	,ARIC.[intItemUOMId]
 	,ARIC.[dtmDate]
-	,[dblQty] = ARIDP.[dblQtyShipped]-- * -1
-	,ARIC.[dblUOMQty]
-	,ARIC.[dblCost]
-	,ARIC.[dblSalesPrice]
-	,ARIC.[intCurrencyId]
-	,ARIC.[dblExchangeRate]
-	,ARIC.[intTransactionId]
-	,ARIC.[intTransactionDetailId]
-	,ARIC.[strTransactionId]
-	,ARIC.[intTransactionTypeId]
-	,ARIC.[intLotId]
-	,ARIC.[intSubLocationId]
-	,ARIC.[intStorageLocationId]
-	,ARIC.[strActualCostId]
-	,ARIC.[intForexRateTypeId]
-	,ARIC.[dblForexRate]
-	,ARIC.[intStorageScheduleTypeId]
-    ,ARIC.[dblUnitRetail]
-	,ARIC.[intCategoryId]
-	,ARIC.[dblAdjustRetailValue]
-	,ARIC.[ysnForValidation] 
-FROM #ARItemsForCosting ARIC
-INNER JOIN #ARPostInvoiceDetail ARID
-ON ARIC.intTransactionDetailId = ARID.intInvoiceDetailId
-INNER JOIN tblARInvoiceDetail ARIDP
-ON ARID.intOriginalInvoiceDetailId = ARIDP.intInvoiceDetailId
-WHERE ARID.[intSourceId] = 2
-
--- INSERT INTO #ARItemsForCosting
--- 	([intItemId]
--- 	,[intItemLocationId]
--- 	,[intItemUOMId]
--- 	,[dtmDate]
--- 	,[dblQty]
--- 	,[dblUOMQty]
--- 	,[dblCost]
--- 	,[dblSalesPrice]
--- 	,[intCurrencyId]
--- 	,[dblExchangeRate]
--- 	,[intTransactionId]
--- 	,[intTransactionDetailId]
--- 	,[strTransactionId]
--- 	,[intTransactionTypeId]
--- 	,[intLotId]
--- 	,[intSubLocationId]
--- 	,[intStorageLocationId]
--- 	,[strActualCostId]
--- 	,[intForexRateTypeId]
--- 	,[dblForexRate]
--- 	,[intStorageScheduleTypeId]
---     ,[dblUnitRetail]
--- 	,[intCategoryId]
--- 	,[dblAdjustRetailValue]
--- 	,[ysnForValidation]
--- )
--- SELECT
--- 	 [intItemId]				= ARIC.[intComponentItemId]
--- 	,[intItemLocationId]		= IST.intItemLocationId
--- 	,[intItemUOMId]				= ARIC.[intItemUnitMeasureId] 
--- 	,[dtmDate]					= ISNULL(ARID.[dtmPostDate], ARID.[dtmShipDate])
--- 	,[dblQty]					= ((ARID.[dblQtyShipped] * ARIC.[dblQuantity]) * (CASE WHEN ARID.[strTransactionType] IN ('Invoice', 'Cash') THEN -1 ELSE 1 END)) * CASE WHEN ARID.[ysnPost] = @ZeroBit THEN -1 ELSE 1 END
--- 	,[dblUOMQty]				= ICIUOM.[dblUnitQty]
--- 	-- If item is using average costing, it must use the average cost. 
--- 	-- Otherwise, it must use the last cost value of the item. 
--- 	,[dblCost]					= ISNULL(dbo.fnMultiply (dbo.fnMultiply (	CASE	WHEN dbo.fnGetCostingMethod(ARIC.[intComponentItemId], IST.[intItemLocationId]) = @AVERAGECOST THEN 
--- 																							dbo.fnGetItemAverageCost(ARIC.[intComponentItemId], IST.[intItemLocationId], ARIC.[intItemUnitMeasureId]) 
--- 																						ELSE 
--- 																							IST.[dblLastCost]  
--- 																				END 
--- 																				,ARID.[dblSplitPercent]
--- 																		), ICIUOM.[dblUnitQty]
--- 													),@ZeroDecimal)
--- 	,[dblSalesPrice]			= ARID.[dblPrice]
--- 	,[intCurrencyId]			= ARID.[intCurrencyId]
--- 	,[dblExchangeRate]			= ARID.[dblCurrencyExchangeRate]
--- 	,[intTransactionId]			= ARID.[intInvoiceId]
--- 	,[intTransactionDetailId]	= ARID.[intInvoiceDetailId]
--- 	,[strTransactionId]			= ARID.[strInvoiceNumber]
--- 	,[intTransactionTypeId]		= CASE WHEN ARID.strTransactionType = 'Credit Memo' THEN @CREDIT_MEMO_INVOICE_TYPE ELSE @INVENTORY_INVOICE_TYPE END
--- 	,[intLotId]					= NULL 
--- 	,[intSubLocationId]			= ARID.[intSubLocationId]
--- 	,[intStorageLocationId]		= ARID.[intStorageLocationId]
--- 	,[strActualCostId]			= CASE WHEN (ISNULL(ARID.[intDistributionHeaderId],0) <> 0 OR ISNULL(ARID.[intLoadDistributionHeaderId],0) <> 0) THEN ARID.[strActualCostId] ELSE NULL END
--- 	,[intForexRateTypeId]		= ARID.[intCurrencyExchangeRateTypeId]
--- 	,[dblForexRate]				= ARID.[dblCurrencyExchangeRate]
--- 	,[intStorageScheduleTypeId] = ARID.[intStorageScheduleTypeId]
---     ,[dblUnitRetail]			= CASE WHEN IST.ysnRetailValuation = @OneBit THEN ARID.dblPrice ELSE NULL END
--- 	,[intCategoryId]			= IST.[intCategoryId]
--- 	,[dblAdjustRetailValue]		= CASE WHEN dbo.fnGetCostingMethod(ARIC.[intComponentItemId], IST.[intItemLocationId]) = @CATEGORYCOST THEN (ARID.[dblQtyShipped] * ARIC.[dblQuantity]) * ARID.[dblPrice] ELSE NULL END
--- 	,[ysnForValidation]         = NULL
--- FROM
--- 	(SELECT [intComponentItemId], [intItemUnitMeasureId], [intCompanyLocationId],[dblQuantity], [intItemId], [strType] FROM vyuARGetItemComponents WITH (NOLOCK)) ARIC
--- INNER JOIN
--- 	#ARPostInvoiceDetail ARID
--- 		ON ARIC.[intItemId] = ARID.[intItemId]
--- 		AND ARIC.[intCompanyLocationId] = ARID.[intCompanyLocationId]	
--- INNER JOIN
--- 	(SELECT [intItemId], [ysnAutoBlend] FROM tblICItem WITH (NOLOCK)) ICI
--- 		ON ARIC.[intComponentItemId] = ICI.[intItemId]
--- LEFT OUTER JOIN
--- 	(SELECT [intItemUOMId], [dblUnitQty] FROM tblICItemUOM WITH (NOLOCK)) ICIUOM
--- 		ON ARIC.[intItemUnitMeasureId] = ICIUOM.[intItemUOMId]
--- LEFT OUTER JOIN
--- 	(SELECT [intItemId], [intItemLocationId], intLocationId, dblLastCost, [intCategoryId], ysnRetailValuation FROM vyuICGetItemStock WITH (NOLOCK)) IST
--- 		ON ARIC.[intComponentItemId] = IST.[intItemId]
--- 		AND ARID.[intCompanyLocationId] = IST.[intLocationId]	
--- LEFT OUTER JOIN
---     (SELECT [intLoadId], [intPurchaseSale] FROM tblLGLoad WITH (NOLOCK)) LGL
--- 		ON LGL.[intLoadId] = ARID.[intLoadId]				 				 
--- WHERE
--- 	((ISNULL(ARID.[strImportFormat], '') <> 'CarQuest' AND (ARID.[dblTotal] <> 0 OR dbo.fnGetItemAverageCost(ARIC.[intComponentItemId], IST.[intItemLocationId], ARIC.[intItemUnitMeasureId]) <> 0)) OR ISNULL(ARID.[strImportFormat], '') = 'CarQuest') 
--- 	AND (
--- 		((ARID.[intInventoryShipmentItemId] IS NULL OR ARID.[intInventoryShipmentItemId] = 0) AND (ARID.[intLoadDetailId] IS NULL OR ARID.[intLoadDetailId] = 0) AND  ARID.[strTransactionType] <> 'Credit Memo')
--- 		OR
--- 		(((ARID.[intInventoryShipmentItemId] IS NULL OR ARID.[intInventoryShipmentItemId] = 0) OR (ARID.[intLoadDetailId] IS NULL OR ARID.[intLoadDetailId] = 0)) AND ARID.[strTransactionType] = 'Credit Memo')
--- 		)
---     AND ARID.[ysnImpactInventory] = @OneBit
--- 	AND ARID.[intItemId] IS NOT NULL
--- 	AND ISNULL(ARIC.[intComponentItemId],0) <> 0
--- 	AND ARID.[strTransactionType] <> 'Debit Memo'
--- 	AND ISNULL(ARIC.[strType],'') NOT IN ('Finished Goods','Comment')
--- 	AND (ARID.[intStorageScheduleTypeId] IS NULL OR ISNULL(ARID.[intStorageScheduleTypeId],0) = 0)	
--- 	AND (ARID.intLoadDetailId IS NULL OR (ARID.intLoadDetailId IS NOT NULL AND ISNULL(LGL.[intPurchaseSale], 0) NOT IN (2, 3)))
-
--- Final Invoice
-INSERT INTO #ARItemsForCosting
-	([intItemId]
-	,[intItemLocationId]
-	,[intItemUOMId]
-	,[dtmDate]
-	,[dblQty]
-	,[dblUOMQty]
-	,[dblCost]
-	,[dblSalesPrice]
-	,[intCurrencyId]
-	,[dblExchangeRate]
-	,[intTransactionId]
-	,[intTransactionDetailId]
-	,[strTransactionId]
-	,[intTransactionTypeId]
-	,[intLotId]
-	,[intSubLocationId]
-	,[intStorageLocationId]
-	,[strActualCostId]
-	,[intForexRateTypeId]
-	,[dblForexRate]
-	,[intStorageScheduleTypeId]
-    ,[dblUnitRetail]
-	,[intCategoryId]
-	,[dblAdjustRetailValue]
-	,[strType]
-) 
-SELECT
-	ARIC.[intItemId]
-	,ARIC.[intItemLocationId]
-	,ARIC.[intItemUOMId]
-	,ARIC.[dtmDate]
 	,[dblQty] = ARIDP.[dblQtyShipped]
 	,ARIC.[dblUOMQty]
 	,ARIC.[dblCost]
@@ -476,7 +319,7 @@ SELECT
     ,ARIC.[dblUnitRetail]
 	,ARIC.[intCategoryId]
 	,ARIC.[dblAdjustRetailValue]
-	,ARID.[strType]
+	,@ZeroBit
 FROM #ARItemsForCosting ARIC
 INNER JOIN #ARPostInvoiceDetail ARID
 ON ARIC.intTransactionDetailId = ARID.intInvoiceDetailId
