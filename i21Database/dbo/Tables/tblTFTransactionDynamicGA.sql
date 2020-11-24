6256c136617e0d5e3e276f67bbc26a0d85fbf33a
cris.lazaga <cris.lazaga@irely.com>
2020-11-24 15:38:29 +0800

    MFT-1522

---
+++ i21Database/dbo/Tables/tblTFTransactionDynamicGA.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tblTFTransactionDynamicGA]
+(
+	[intTransactionDynamicId] [int] IDENTITY(1,1) NOT NULL,
+	[intTransactionId] [int] NOT NULL,
+	[strGAOriginFacilityNumber] [nvarchar](50) NULL,
+	[strGAOriginAddress] [nvarchar](250) NULL,
+	[strGADestinationFacilityNumber] [nvarchar](50) NULL,
+	[strGADestinationAddress] [nvarchar](250) NULL,
+	CONSTRAINT [FK_tblTFTransactionDynamicGA_tblTFTransaction] FOREIGN KEY ([intTransactionId]) REFERENCES [tblTFTransaction]([intTransactionId]) ON DELETE CASCADE,
+	CONSTRAINT [PK_tblTFTransactionDynamicGA] PRIMARY KEY ([intTransactionDynamicId])
+)
