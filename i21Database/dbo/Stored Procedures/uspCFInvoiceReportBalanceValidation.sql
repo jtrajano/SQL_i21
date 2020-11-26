f778e313ac1b6274def80c9547866a1750021078
bonn.ponce <bonn.ponce@irely.com>
2020-11-26 09:57:33 +0800

    CF-2583
    (cherry picked from commit 38fb14ab03d00fcae2ec36c8dfd026bb8d0a7902)
    (cherry picked from commit a9651e3cbbd67888919ad6996a7910d1fd914084)
    (cherry picked from commit 58252056f811eefae37f125f85ea29f856e1d9b2)

---
+++ i21Database/dbo/Stored Procedures/uspCFInvoiceReportBalanceValidation.sql
@@ -0,0 +1,92 @@
+﻿CREATE PROCEDURE [dbo].[uspCFInvoiceReportBalanceValidation](
+	 @UserId NVARCHAR(MAX)
+)
+AS
+BEGIN
+
+	DECLARE @intUserId INT
+	SELECT TOP 1 @intUserId = intEntityId FROM tblSMUserSecurity WHERE strUserName = @UserId
+
+	
+	SET NOCOUNT ON;
+	
+	DECLARE @tblMainQuery TABLE 
+	(
+		 intEntityCustomerId INT NULL
+		,strCustomerNumber	 NVARCHAR (MAX)  COLLATE Latin1_General_CI_AS NULL
+		,strCustomerName	 NVARCHAR (MAX)  COLLATE Latin1_General_CI_AS NULL
+		,dtmDate DATETIME NULL
+		,dblCalcRunningBalance NUMERIC(18,6) NULL
+		,dblTotalAR NUMERIC(18,6) NULL
+	)
+
+
+	INSERT INTO @tblMainQuery
+	(
+		 intEntityCustomerId 
+		,strCustomerNumber	
+		,strCustomerName	
+		,dtmDate
+		,dblCalcRunningBalance
+		,dblTotalAR
+	)
+	SELECT  
+	 intEntityCustomerId 
+	,strCustomerNumber	
+	,strCustomerName	
+	,dtmDate
+	,dblCalcRunningBalance = SUM (CASE WHEN strTransactionType = 'Balance Forward' 
+											  THEN dblBalance - dblPayment 
+											  ELSE ISNULL (dblInvoiceTotal, 0) - dblPayment END)
+											  OVER (PARTITION BY intEntityCustomerId ORDER BY dtmDate ROWS
+											 BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) 
+	,dblTotalAR
+	FROM (SELECT *
+	FROM tblARCustomerStatementStagingTable 
+	WHERE strStatementFormat = 'Balance Forward' AND intEntityUserId = @intUserId) 
+	as innerquery 
+
+
+	
+	INSERT INTO tblCFInvoiceReportBalanceValidation
+	(
+		 intEntityCustomerId
+		,strCustomerNumber	
+		,strCustomerName	
+		,dblRunningBalance
+		,dblAREndingBalance
+		,dblDiff
+		,dtmDate
+		,strUserId
+		,strStatementType
+	)
+	SELECT 
+		 mainQuery.intEntityCustomerId
+		 ,strCustomerNumber	
+		 ,strCustomerName	
+		,dblCalcRunningBalance	
+		,dblTotalAR	
+		,ABS(ISNULL(dblCalcRunningBalance,0) - ISNULL(dblTotalAR,0))
+		,mainQuery.dtmDate
+		,@UserId
+		,'invoice'	
+	FROM (
+			SELECT intEntityCustomerId , MAX(dtmDate) as dtmDate 
+			FROM @tblMainQuery
+			GROUP BY intEntityCustomerId
+		) as innerQuery
+	INNER JOIN @tblMainQuery as mainQuery
+	ON mainQuery.intEntityCustomerId = innerQuery.intEntityCustomerId
+	AND mainQuery.dtmDate = innerQuery.dtmDate
+	AND dblCalcRunningBalance != dblTotalAR
+
+
+	
+
+
+
+
+
+END
+
+
