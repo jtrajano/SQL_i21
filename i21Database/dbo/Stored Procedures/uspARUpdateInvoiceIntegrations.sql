c57748027ca86f6df624e358c64eb9f39220f61c
Roudinee Panteria <roudinee.panteria@irely.com>
2020-11-27 17:44:19 +0800

    AR-12574

--- i21Database/dbo/Stored Procedures/uspARUpdateInvoiceIntegrations.sql
+++ i21Database/dbo/Stored Procedures/uspARUpdateInvoiceIntegrations.sql
@@ -102,9 +102,9 @@ BEGIN TRY
 	UPDATE tblARInvoice SET intUserIdforDelete =@UserId  WHERE intInvoiceId = @InvoiceId
 
 	--UPDATE PREPAID ITEM CONTRACT
-	IF ISNULL(@ysnFromItemContract, 0) <> 0
+	IF(ISNULL(@intTransactionDetailId, 0) <> 0 AND ISNULL(@ysnFromItemContract, 0) = 0)
 	BEGIN
-		EXEC uspCTItemContractUpdateRemainingDollarValue @intItemContractHeaderId,  @dblValueToUpdate, @intUserId, @intTransactionDetailId , @strScreenName,  @strRowState, @intInvoiceId
+		EXEC uspCTItemContractUpdateRemainingDollarValue @intItemContractHeaderId, @dblValueToUpdate, @intUserId, @intTransactionDetailId , @strScreenName,  @strRowState, @intInvoiceId
 	END	
 	EXEC dbo.[uspARUpdatePricingHistory] 2, @intInvoiceId, @intUserId
 	EXEC dbo.[uspSOUpdateOrderShipmentStatus] @intInvoiceId, 'Invoice', @ForDelete
