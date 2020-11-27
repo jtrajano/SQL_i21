0421875dbc2e9f2dbec25c9b7e54a0fea733a712
bonn.ponce <bonn.ponce@irely.com>
2020-11-27 16:42:50 +0800

    CF-2583

--- i21Database/dbo/Stored Procedures/uspCFInvoiceToStagingTable.sql
+++ i21Database/dbo/Stored Procedures/uspCFInvoiceToStagingTable.sql
@@ -116,6 +116,17 @@ BEGIN TRY
 	-----------------------------------------------------------
 	DELETE FROM tblCFInvoiceReportTempTable	WHERE strUserId = @UserId AND LOWER(strStatementType) =  LOWER(@StatementType)
 	EXEC "dbo"."uspCFInvoiceReport"			@xmlParam	=	@xmlParam , @UserId = @UserId, @StatementType = @StatementType
+
+
+	DELETE FROM tblCFInvoiceReportTotalValidation WHERE strUserId = @UserId 
+	EXEC "dbo"."uspCFInvoiceReportValidation" @UserId = @UserId 
+
+	DECLARE @intInvalidTransaction INT 
+	SELECT @intInvalidTransaction = COUNT(1) FROM tblCFInvoiceReportTotalValidation	WHERE strUserId = @UserId 
+
+	IF (@intInvalidTransaction >= 1) 
+	GOTO EXITWITHERROR
+	
 	
 	DELETE FROM tblCFInvoiceSummaryTempTable WHERE strUserId = @UserId AND LOWER(strStatementType) =  LOWER(@StatementType)
 	EXEC "dbo"."uspCFInvoiceReportSummary" @UserId = @UserId , @StatementType = @StatementType
@@ -1194,8 +1205,11 @@ BEGIN CATCH
 
 	RAISERROR (@CatchErrorMessage,@CatchErrorSeverity,@CatchErrorState)
 
-	
-	
 
 END CATCH
+
+
+	EXITWITHERROR:
+	RAISERROR ('Total Validation Error',16,1)
+
 END
