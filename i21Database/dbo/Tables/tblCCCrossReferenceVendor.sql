3ecfe8a78722d891436ffe0960cd7ce20bbe24a5
cris.lazaga <cris.lazaga@irely.com>
2020-12-04 17:00:55 +0800

    CCR-438

--- i21Database/dbo/Tables/tblCCCrossReferenceVendor.sql
+++ i21Database/dbo/Tables/tblCCCrossReferenceVendor.sql
@@ -1,9 +1,11 @@
 ﻿CREATE TABLE [dbo].[tblCCCrossReferenceVendor]
 (
 	[intCrossReferenceVendorId] INT NOT NULL IDENTITY,
-	[intVendorId] INT NOT NULL,
+	[intCrossReferenceId] INT NOT NULL,
+	[intVendorDefaultId] INT NOT NULL,
 	[strImportValue] NVARCHAR(100) COLLATE Latin1_General_CI_AS NOT NULL,
 	[intConcurrencyId] INT NOT NULL DEFAULT ((1)),
 	CONSTRAINT [PK_tblCCCrossReferenceVendor_intCrossReferenceVendorId] PRIMARY KEY CLUSTERED ([intCrossReferenceVendorId] ASC),
-	CONSTRAINT [FK_tblCCCrossReferenceVendor_tblAPVendor_intVendorId] FOREIGN KEY ([intVendorId]) REFERENCES [tblAPVendor]([intEntityId])
+	CONSTRAINT [FK_tblCCCrossReferenceVendor_tblCCCrossReference_intCrossReferenceId] FOREIGN KEY ([intCrossReferenceId]) REFERENCES [dbo].[tblCCCrossReference] ([intCrossReferenceId]) ON DELETE CASCADE,
+	CONSTRAINT [FK_tblCCCrossReferenceVendor_tblCCVendorDefault_intVendorDefaultId] FOREIGN KEY ([intVendorDefaultId]) REFERENCES [tblCCVendorDefault]([intVendorDefaultId])
 )
