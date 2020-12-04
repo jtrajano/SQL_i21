2913c94343aa2869d2571cf708f6d1e15f12618e
cris.lazaga <cris.lazaga@irely.com>
2020-12-04 14:20:28 +0800

    CCR-438

---
+++ i21Database/dbo/Tables/tblCCCrossReferenceVendor.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[tblCCCrossReferenceVendor]
+(
+	[intCrossReferenceVendorId] INT NOT NULL IDENTITY,
+	[intVendorId] INT NOT NULL,
+	[strImportValue] NVARCHAR(100) COLLATE Latin1_General_CI_AS NOT NULL,
+	[intConcurrencyId] INT NOT NULL DEFAULT ((1)),
+	CONSTRAINT [PK_tblCCCrossReferenceVendor_intCrossReferenceVendorId] PRIMARY KEY CLUSTERED ([intCrossReferenceVendorId] ASC),
+	CONSTRAINT [FK_tblCCCrossReferenceVendor_tblAPVendor_intVendorId] FOREIGN KEY ([intVendorId]) REFERENCES [tblAPVendor]([intEntityId])
+)
