d7fed984ba14493c755f4f85fb1238616d231c1c
Arman Arenas <arman.arenas@irely.com>
2020-12-04 19:58:27 +0800

    LG-2782 revisions on displaying Reserves values

--- i21Database/dbo/Stored Procedures/uspLGPurchaseWisePnLReportReserves.sql
+++ i21Database/dbo/Stored Procedures/uspLGPurchaseWisePnLReportReserves.sql
@@ -7,43 +7,46 @@ AS
 DECLARE @intDefaultCurrencyId AS INT = dbo.fnSMGetDefaultCurrency('FUNCTIONAL')
 		,@strUnitMeasure AS NVARCHAR(200)
 		,@strCurrency AS NVARCHAR(200)
+		,@intFutureMarketCurrency AS NVARCHAR(200)
 
-IF (@intUnitMeasureId IS NULL)
-	SELECT @intUnitMeasureId = intSUnitMeasureId 
-	FROM tblLGAllocationDetail WHERE intAllocationDetailId = @intAllocationDetailId
+SELECT @intUnitMeasureId = FM.intUnitMeasureId, @intFutureMarketCurrency = FM.intCurrencyId
+FROM tblLGAllocationDetail ALD
+	INNER JOIN tblCTContractDetail PCD ON PCD.intContractDetailId = ALD.intPContractDetailId
+	INNER JOIN tblRKFutureMarket FM ON FM.intFutureMarketId = PCD.intFutureMarketId
+WHERE intAllocationDetailId = @intAllocationDetailId
 
 SELECT @strUnitMeasure = CASE WHEN ISNULL(strSymbol, '') <> '' THEN strSymbol ELSE strUnitMeasure END
 FROM tblICUnitMeasure WHERE intUnitMeasureId = @intUnitMeasureId
 
 SELECT @strCurrency = CASE WHEN ISNULL(strSymbol, '') <> '' THEN strSymbol ELSE strCurrency END
-FROM tblSMCurrency WHERE intCurrencyID = @intDefaultCurrencyId
+FROM tblSMCurrency WHERE intCurrencyID = @intFutureMarketCurrency
 
 SELECT
 	I.strItemNo
 	,dblRate = ISNULL(CC.dblRate, 0)
-	,dblEstimatedAmount = ISNULL(CC.dblRate, 0) * -1
+	,dblEstimatedAmount = ISNULL(CC.dblAmount, 0) * -1
 	,dblInvoicedAmount = CASE WHEN @intReserves = 1 THEN ISNULL(VCHR.dblTotal, 0) + ISNULL(INVC.dblTotal, 0) ELSE 0 END * -1
 	,dblVariance = CASE WHEN @intReserves = 1 THEN 
 						CASE WHEN (ISNULL(VCHR.dblTotal, 0) + ISNULL(INVC.dblTotal, 0)) <> 0 
 							THEN ((ISNULL(VCHR.dblTotal, 0) + ISNULL(INVC.dblTotal, 0)) * -1) 
-							- (ISNULL(CC.dblRate, 0) * -1)
+							- (ISNULL(CC.dblAmount, 0) * -1)
 							ELSE 0 END
 					ELSE 0 END
 	,dblRecalcValue = CASE WHEN @intReserves = 1 THEN 
 						CASE WHEN (ISNULL(VCHR.dblTotal, 0) + ISNULL(INVC.dblTotal, 0)) <> 0 
 							THEN (ISNULL(VCHR.dblTotal, 0) + ISNULL(INVC.dblTotal, 0)) 
 							ELSE ISNULL(CC.dblAmount, 0) END
-					ELSE ISNULL(CC.dblRate, 0) END * -1 
+					ELSE ISNULL(CC.dblAmount, 0) END * -1 
 	,strUnitCurrency = @strCurrency + '/' + @strUnitMeasure
 FROM tblICItem I
 	OUTER APPLY (SELECT intPContractDetailId, intSContractDetailId, dblPAllocatedQty, dblSAllocatedQty
 					FROM tblLGAllocationDetail WHERE intAllocationDetailId = @intAllocationDetailId) ALD
 	OUTER APPLY (SELECT CC.intContractCostId
 						,dblRate = CASE WHEN CC.strCostMethod = 'Per Unit' THEN 
-											dbo.fnCalculateCostBetweenUOM(CC.intItemUOMId,CToUOM.intItemUOMId,CC.dblRate)
+											CC.dblRate
 										WHEN CC.strCostMethod = 'Amount' THEN
 											CC.dblRate / dbo.fnCalculateQtyBetweenUOM(CD.intItemUOMId,ToUOM.intItemUOMId,ALD.dblSAllocatedQty)
-										ELSE CC.dblRate END / ISNULL(CCUR.intCent, 1) * COALESCE(CC.dblFX, FX.dblFXRate, 1)
+										ELSE CC.dblRate END * COALESCE(CC.dblFX, FX.dblFXRate, 1)
 						,dblAmount = CASE WHEN CC.strCostMethod = 'Per Unit' THEN 
 											dbo.fnCalculateQtyBetweenUOM(CD.intItemUOMId,ToUOM.intItemUOMId,CD.dblQuantity) 
 											* dbo.fnCalculateCostBetweenUOM(CC.intItemUOMId,CToUOM.intItemUOMId,CC.dblRate) / ISNULL(CCUR.intCent, 1)
