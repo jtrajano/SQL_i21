100edebf28f27a8b412478e0559a260bc30da5f2
Feb Montefrio <feb.montefrio@irely.com>
2020-12-07 14:50:16 +0800

    [B] IC-9089
    # Conflicts:
    #	i21Database/dbo/Stored Procedures/uspICRebuildInventoryValuation.sql

--- i21Database/dbo/Stored Procedures/uspICRebuildInventoryValuation.sql
+++ i21Database/dbo/Stored Procedures/uspICRebuildInventoryValuation.sql
@@ -6217,9 +6217,10 @@ BEGIN
 		@strPeriod = strPeriod
 	FROM 
 		tblGLFiscalYearPeriod fyp
-		CROSS APPLY dbo.udfDateGreaterThanEquals(fyp.dtmStartDate, @dtmStartDate) dtmStart	
+	WHERE	
+		fyp.dtmStartDate <= @dtmStartDate
 	ORDER BY 
-		fyp.dtmStartDate ASC 
+		fyp.dtmStartDate DESC 
 
 	EXEC dbo.[uspICSearchInventoryValuationSummary] 
 		@strPeriod
