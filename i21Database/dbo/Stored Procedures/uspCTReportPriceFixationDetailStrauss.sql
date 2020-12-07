2bfd0c8cf4575e8341ac1a218c06f9e7819074ac
jayson_ramos <jayson.ramos@irely.com>
2020-12-07 15:54:48 +0800

    CT-5324
    (cherry picked from commit 85dfc96cd8d279336a59bb6cfb4c83214a95e790)
    (cherry picked from commit 8dff183dd8e272354a0853b486c41b3174528b5b)

--- i21Database/dbo/Stored Procedures/uspCTReportPriceFixationDetailStrauss.sql
+++ i21Database/dbo/Stored Procedures/uspCTReportPriceFixationDetailStrauss.sql
@@ -10,7 +10,9 @@ BEGIN TRY
 
 	DECLARE
 		@intPriceFixationId		INT,
-		@xmlDocumentId			INT;
+		@xmlDocumentId			INT,
+		@intContractHeaderId	int,
+		@intContractDetailId	int;
 
 	IF	LTRIM(RTRIM(@xmlParam)) = ''   
 		SET @xmlParam = NULL   
@@ -47,6 +49,8 @@ BEGIN TRY
 	SELECT	@intPriceFixationId = [from]
 	FROM	@temp_xml_table   
 	WHERE	[fieldname] = 'intPriceFixationId'
+
+	select @intContractHeaderId = intContractHeaderId, @intContractDetailId = intContractDetailId from tblCTPriceFixation where intPriceFixationId = @intPriceFixationId;
 			
 	SELECT	DISTINCT 
 			PF.intPriceFixationId,
@@ -58,7 +62,22 @@ BEGIN TRY
 			PD.strNotes
 	FROM	tblCTPriceFixation			PF
 	JOIN	tblCTPriceFixationDetail	PD	ON	PD.intPriceFixationId			=	PF.intPriceFixationId
-	CROSS APPLY dbo.fnCTGetTopOneSequence(PF.intContractHeaderId,PF.intContractDetailId) CD
+	cross apply	(	select top 1 cd1.*
+					from tblCTContractDetail cd1
+					where cd1.intContractHeaderId = 
+													case
+													when isnull(@intContractDetailId,0) = 0
+													then @intContractHeaderId
+													else cd1.intContractHeaderId
+													end
+						  and cd1.intContractDetailId =
+						  							case
+						  							when isnull(@intContractDetailId,0) = 0
+						  							then cd1.intContractDetailId
+						  							else @intContractDetailId 
+						  							end
+				) CD
+	--CROSS APPLY dbo.fnCTGetTopOneSequence(PF.intContractHeaderId,PF.intContractDetailId) CD
 	LEFT	JOIN	tblRKFutureMarket			MA	ON	MA.intFutureMarketId			=	PD.intFutureMarketId	
 	LEFT	JOIN	tblRKFuturesMonth			MO	ON	MO.intFutureMonthId				=	PD.intFutureMonthId		
 	LEFT	JOIN	tblSMCurrency				CY	ON	CY.intCurrencyID				=	CD.intCurrencyId		
