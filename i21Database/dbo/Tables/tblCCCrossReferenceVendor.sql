ec88a082a8c61d25e7c3320e5dfe3fec3de84b08
cris.lazaga <cris.lazaga@irely.com>
2020-12-09 09:23:58 +0800

    CCR-438

--- i21Database/dbo/Tables/tblCCCrossReferenceVendor.sql
+++ i21Database/dbo/Tables/tblCCCrossReferenceVendor.sql
@@ -8,5 +8,5 @@
 	CONSTRAINT [PK_tblCCCrossReferenceVendor_intCrossReferenceVendorId] PRIMARY KEY CLUSTERED ([intCrossReferenceVendorId] ASC),
 	CONSTRAINT [FK_tblCCCrossReferenceVendor_tblCCCrossReference_intCrossReferenceId] FOREIGN KEY ([intCrossReferenceId]) REFERENCES [dbo].[tblCCCrossReference] ([intCrossReferenceId]) ON DELETE CASCADE,
 	CONSTRAINT [FK_tblCCCrossReferenceVendor_tblCCVendorDefault_intVendorDefaultId] FOREIGN KEY ([intVendorDefaultId]) REFERENCES [tblCCVendorDefault]([intVendorDefaultId]),
-	CONSTRAINT [UK_tblCCCrossReferenceVendor_intVendorDefaultId_strImportValue] UNIQUE ([intVendorDefaultId], [strImportValue]) 
+	CONSTRAINT [UK_tblCCCrossReferenceVendor_strImportValue] UNIQUE ([strImportValue])
 )
