837f9f380dbce820741d87bf1ef1a11b2cac105f
Premkumar S <prem.selladurai@irely.com>
2020-12-11 00:05:41 +0530

    MFG-4314

--- i21Database/dbo/Stored Procedures/uspMFProcessRecipes.sql
+++ i21Database/dbo/Stored Procedures/uspMFProcessRecipes.sql
@@ -14,11 +14,19 @@ BEGIN
 	INSERT INTO @tblMFSession
 	SELECT DISTINCT strSessionId
 	FROM tblMFRecipeStage
-	WHERE IsNULL(strMessage, '') = ''
+	WHERE IsNULL(strMessage, '') = '' and intStatusId is null
 	UNION
 	SELECT DISTINCT strSessionId
 	FROM tblMFRecipeItemStage
-	WHERE IsNULL(strMessage, '') = ''
+	WHERE IsNULL(strMessage, '') = '' and intStatusId is null
+
+	Update tblMFRecipeStage 
+	Set intStatusId=3 
+	Where strSessionId in (Select strSessionId from @tblMFSession)
+
+	Update tblMFRecipeItemStage 
+	Set intStatusId=3 
+	Where strSessionId in (Select strSessionId from @tblMFSession)
 
 	SELECT @intRecordId = MIN(intRecordId)
 	FROM @tblMFSession
@@ -52,5 +60,13 @@ BEGIN
 		FROM @tblMFSession
 		WHERE intRecordId > @intRecordId
 	END
+
+	Update tblMFRecipeStage 
+	Set intStatusId=NULL
+	Where strSessionId in (Select strSessionId from @tblMFSession) and intStatusId=3 
+
+	Update tblMFRecipeItemStage 
+	Set intStatusId=NULL
+	Where strSessionId in (Select strSessionId from @tblMFSession)and intStatusId=3 
 END
 
