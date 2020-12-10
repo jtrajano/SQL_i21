3ee2ae85d2fcecc77869e211003748aaaba1fe3f
Roudinee Panteria <roudinee.panteria@irely.com>
2020-12-10 11:40:53 +0800

    AR-12646

--- i21Database/dbo/Stored Procedures/uspARCustomerAgingDetailAsOfDateReport.sql
+++ i21Database/dbo/Stored Procedures/uspARCustomerAgingDetailAsOfDateReport.sql
@@ -236,8 +236,8 @@ WHERE ysnPosted = 1
 	AND (@strSourceTransactionLocal IS NULL OR strType LIKE '%'+@strSourceTransactionLocal+'%')
 
 --#CASHREFUNDS
-SELECT intOriginalInvoiceId	= I.intOriginalInvoiceId
-     , dblRefundTotal		= I.dblInvoiceTotal
+SELECT strDocumentNumber	= ID.strDocumentNumber
+     , dblRefundTotal		= SUM(ID.dblTotal) 
 INTO #CASHREFUNDS
 FROM tblARInvoice I
 INNER JOIN @tblCustomers C ON I.intEntityCustomerId = C.intEntityCustomerId
