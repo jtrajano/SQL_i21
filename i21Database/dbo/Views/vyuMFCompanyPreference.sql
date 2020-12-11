92ea5cf191126f0e304c92135265c29ff0895093
Vinoth Kumar T <vinoth.t@irely.com>
2020-12-11 22:29:10 +0530

    MFG-4316

--- i21Database/dbo/Views/vyuMFCompanyPreference.sql
+++ i21Database/dbo/Views/vyuMFCompanyPreference.sql
@@ -88,6 +88,7 @@ SELECT CP.intCompanyPreferenceId
 	,CP.ysnDisplayDemandWithItemNoAndDescription
 	,CP.ysnDisplayRestrictedBookInDemandView
 	,CP.intNoofdaystoconsiderforDemandBatches
+	,CP.intDemandAnalysisMonthlyCutOffDay
 	,SL.strName AS strShipmentStagingLocation
 	,SL1.strName AS strShipmentDockDoorLocation
 	,LS.strSecondaryStatus AS strBondLotStatus
