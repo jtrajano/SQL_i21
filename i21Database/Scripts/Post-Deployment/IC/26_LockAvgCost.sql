31bcf883c045e24945211850c0efefa17b08c018
Wendell Wayne Estrada <wendell.estrada@irely.com>
2020-12-15 15:14:38 +0800

    [F] IC-9086 HDTN-253957 - Make Avg Cost field in item setup non editable

--- i21Database/Scripts/Post-Deployment/IC/26_LockAvgCost.sql
+++ i21Database/Scripts/Post-Deployment/IC/26_LockAvgCost.sql
@@ -1,11 +1,12 @@
-PRINT 'Locking AVG cost for items with existing transactions...'
+PRINT 'Locking AVG cost for items with existing transactions in specific locations...'
 
 UPDATE i
 SET i.ysnAvgLocked = 1
 FROM (
-	SELECT DISTINCT intItemId
+	SELECT DISTINCT intItemId, intItemLocationId
 	FROM tblICInventoryTransaction t
 ) xt
-INNER JOIN tblICItem i ON i.intItemId = xt.intItemId
+INNER JOIN tblICItemPricing i ON i.intItemId = xt.intItemId
+	AND i.intItemLocationId = xt.intItemLocationId
 
-PRINT 'End of locking AVG cost for items with existing transactions'
+PRINT 'End of locking AVG cost for items with existing transactions in specific locations'
