ec49072006ef9c1d76c474cb5c2b6ab5edb8ba58
manny_armas <manny.armas@iRely.com>
2020-12-17 22:06:23 +0800

    IET-33, IET-34 - Haz Material Messages

---
+++ i21Database/dbo/Views/vyuETExportHazMat.sql
@@ -0,0 +1,5 @@
+﻿CREATE VIEW [dbo].[vyuETExportHazMat]    
+AS    
+SELECT intTagId epa_group, strMessage msg1, msg2 ='' 
+FROM tblICTag
+WHERE strType = 'Hazmat Message'
---
+++ i21Database/dbo/Views/vyuETExportHazMatXRef.sql
@@ -0,0 +1,8 @@
+﻿CREATE VIEW [dbo].[vyuETExportHazMatXRef]    
+AS
+SELECT ICT.intTagId epa_group
+           ,strItemNo item_no
+FROM dbo.tblICItem ICC WITH (NOLOCK)
+INNER JOIN dbo.tblICTag ICT ON ICC.intHazmatTag = ICT.intTagId AND ICT.strType = 'Hazmat Message'
+WHERE ISNULL(ICC.intHazmatTag, 0) <> 0
+GROUP BY intTagId, strItemNo
