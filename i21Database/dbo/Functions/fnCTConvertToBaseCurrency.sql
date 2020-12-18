eb400e4e17de28f8b5c0aa4472354238bba926f5
jayson_ramos <jayson.ramos@irely.com>
2020-12-19 00:00:54 +0800

    CT-5363
    (cherry picked from commit 486067a72d46015270c44617adb1773dfd3ed612)

---
+++ i21Database/dbo/Functions/fnCTConvertToBaseCurrency.sql
@@ -0,0 +1,32 @@
+﻿CREATE FUNCTION [dbo].[fnCTConvertToBaseCurrency](  
+ @MainCurrencyId INT  
+ ,@Amount NUMERIC(38, 20)  
+)  
+RETURNS NUMERIC(38,20)  
+AS   
+BEGIN   
+ DECLARE @CurrencyId INT  
+  ,@SubCurrency BIT  
+  ,@Cent   INT  
+  
+ SELECT  
+   @CurrencyId = [intCurrencyID]  
+  ,@SubCurrency  = [ysnSubCurrency]  
+  ,@Cent    = [intCent]  
+ FROM  
+  tblSMCurrency  
+ WHERE  
+  [intMainCurrencyId] = @MainCurrencyId  
+    
+  
+ IF ISNULL(@SubCurrency,0) = 0  
+  RETURN @Amount  
+    
+ IF @CurrencyId = @MainCurrencyId  
+  RETURN @Amount  
+    
+ IF @CurrencyId <> @MainCurrencyId AND ISNULL(@Cent,0) <> 0  
+  RETURN @Amount/@Cent  
+    
+ RETURN @Amount  
+END
