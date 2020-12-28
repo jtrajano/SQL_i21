db2890c3aef11a8acb438703083c8cf9232c0ba6
manny_armas <manny.armas@iRely.com>
2020-12-28 22:45:39 +0800

    IET-526 - HDTN-255018 - Item Message Export (Filter Out empty message)

--- i21Database/dbo/Views/vyuETExportItemMessages.sql
+++ i21Database/dbo/Views/vyuETExportItemMessages.sql
@@ -1,7 +1,25 @@
 ﻿CREATE VIEW [dbo].[vyuETExportItemMessages]
-	AS SELECT 	 imsgno = strItemNo
-				,imsg1 = SUBSTRING(strInvoiceComments,1,60)
-				,imsg2 = ''
-				,imsg3 = ''
-				,iamsgno = 0
-	FROM tblICItem
+AS 
+
+WITH ItemCrossMessages (intItemId, strItemNo, ItemMessageLineNo, ItemMessage)  
+AS  
+(  
+	SELECT 
+	I.intItemId
+	,I.strItemNo 
+	,ROW_NUMBER() OVER ( PARTITION BY intItemId ORDER BY intItemId) AS ItemMessageLineNo
+	,LTRIM(RTRIM(ISNULL(ItemMsgs.Item,''))) ItemMessage
+	FROM tblICItem I
+	CROSS APPLY dbo.[fnSplitString](I.strInvoiceComments,CHAR(10)) ItemMsgs
+	WHERE LTRIM(RTRIM(ISNULL(ItemMsgs.Item,'')))  <> ''
+)  
+
+SELECT 
+	tblICItem.intItemId
+	,imsgno = tblICItem.strItemNo
+	,imsg1 = (SELECT TOP 1 SUBSTRING(ItemMessage,1, 60) FROM ItemCrossMessages WHERE ItemMessageLineNo = 1)
+	,imsg2 = (SELECT TOP 1 SUBSTRING(ItemMessage,1, 60) FROM ItemCrossMessages WHERE ItemMessageLineNo = 2)
+	,imsg3 = (SELECT TOP 1 SUBSTRING(ItemMessage,1, 60) FROM ItemCrossMessages WHERE ItemMessageLineNo = 3)
+	,iamsgno = 0
+FROM tblICItem
+WHERE REPLACE(LTRIM(RTRIM(ISNULL(strInvoiceComments,''))),CHAR(10),'') <> ''
