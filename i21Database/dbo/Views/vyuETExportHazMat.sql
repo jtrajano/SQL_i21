dde728a266ee98648d610ac0cf944f039345fb6d
manny_armas <manny.armas@iRely.com>
2020-12-29 21:37:06 +0800

    IET-33, IET-34 - Haz Material Messages, IET-527 - HDTN-254496 - ET - hazardous Material Class not printing on BOL form

--- i21Database/dbo/Views/vyuETExportHazMat.sql
+++ i21Database/dbo/Views/vyuETExportHazMat.sql
@@ -1,5 +1,25 @@
 ﻿CREATE VIEW [dbo].[vyuETExportHazMat]    
 AS    
-SELECT intTagId epa_group, strMessage msg1, msg2 ='' 
+
+WITH HazMatMessages (intTagId, strTagNumber, ItemMessageLineNo, ItemMessage)  
+AS  
+(  
+	SELECT 
+	I.intTagId
+	,I.strTagNumber 
+	,ROW_NUMBER() OVER ( PARTITION BY strTagNumber ORDER BY strTagNumber) AS ItemMessageLineNo
+	,LTRIM(RTRIM(ISNULL(HazMatMsgs.Item,''))) ItemMessage
+	FROM tblICTag I
+	CROSS APPLY dbo.[fnSplitString](I.strMessage,CHAR(10)) HazMatMsgs
+	WHERE LTRIM(RTRIM(ISNULL(HazMatMsgs.Item,'')))  <> ''
+	AND strType = 'Hazmat Message'
+)  
+
+SELECT 
+	tblICTag.intTagId
+	,epa_group = tblICTag.strTagNumber
+	,msg1 = ISNULL((SELECT TOP 1 SUBSTRING(ItemMessage,1, 60) FROM HazMatMessages WHERE ItemMessageLineNo = 1 AND strTagNumber = tblICTag.strTagNumber),'')
+	,msg2 = ISNULL((SELECT TOP 1 SUBSTRING(ItemMessage,1, 60) FROM HazMatMessages WHERE ItemMessageLineNo = 2 AND strTagNumber = tblICTag.strTagNumber),'')
 FROM tblICTag
-WHERE strType = 'Hazmat Message'
+WHERE REPLACE(LTRIM(RTRIM(ISNULL(strMessage,''))),CHAR(10),'') <> ''
+AND strType = 'Hazmat Message'
