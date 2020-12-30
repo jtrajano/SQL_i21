24f86d979af3a2b421ce977362a19617dd231f8c
Roudinee Panteria <roudinee.panteria@irely.com>
2020-12-30 18:02:13 +0800

    AR-12718

--- i21Database/dbo/Tables/tblARInvoiceReportStagingTable.sql
+++ i21Database/dbo/Tables/tblARInvoiceReportStagingTable.sql
@@ -125,5 +125,6 @@
 	[dtmLoadedDate]						DATETIME		NULL,
 	[dtmScaleDate]						DATETIME		NULL,
 	[blbLogo]							VARBINARY (MAX) NULL,
-	[blbSignature]						VARBINARY (MAX) NULL
+	[blbSignature]						VARBINARY (MAX) NULL,
+	[dtmCreated]						DATETIME		NULL
 );
