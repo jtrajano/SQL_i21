c82365fd5fc4f811fc4c1f9eaa81278d27673675
jayson_ramos <jayson.ramos@irely.com>
2021-01-04 17:06:30 +0800

    CT-5426
    (cherry picked from commit 506da27197dc491d666eae8d1440f8ead216ccd9)
    (cherry picked from commit a36ca60844375e905cc9f4ffa39b0e5aff77b7eb)

--- i21Database/dbo/Tables/tblCTMiscellaneous.sql
+++ i21Database/dbo/Tables/tblCTMiscellaneous.sql
@@ -4,5 +4,6 @@
 	[ysnContractBalanceInProgress] BIT NOT NULL DEFAULT 0,
 	[ysnDestinationWeightsAndGradesFixed] BIT NOT NULL DEFAULT 0,
 	[ysnFixedSMTransactionWithWrongPricingScreenId] BIT NOT NULL DEFAULT 0,
-	[ysnContractPriceContractInProgress] BIT NOT NULL DEFAULT 0
+	[ysnContractPriceContractInProgress] BIT NOT NULL DEFAULT 0,
+	[ysnFixedPriceAndInvoiceLink] BIT NOT NULL DEFAULT 0
 )
---
+++ i21PreDeployment/Scripts/Pre-Deployment/14_CT/CreatePricingInvoiceLink.sql
@@ -0,0 +1,163 @@
+﻿
+declare @InvoiceDetail table (
+	intInvoiceDetailId int
+	,intInvoiceId int
+	,intContractDetailId int
+	,dblPrice numeric(18,6)
+	,dblQtyShipped numeric(18,6)
+)
+
+declare
+	@intInvoiceDetailId int = 0
+	,@intActiveInvoiceDetailId int = 0
+	,@intInvoiceId int
+	,@intContractDetailId int
+	,@dblPrice numeric(18,6)
+	,@dblQtyShipped numeric(18,6)
+	,@intPriceFixationDetailId int
+	;
+
+insert into @InvoiceDetail
+select distinct
+	intInvoiceDetailId = tbl.intInvoiceDetailId
+	,intInvoiceId = tbl.intInvoiceId
+	,intContractDetailId = tbl.intContractDetailId
+	,dblPrice = tbl.dblPrice
+	,dblQtyShipped = tbl.dblQtyShipped from
+(
+select distinct di.intInvoiceDetailId, di.intInvoiceId, di.intContractDetailId, di.dblPrice, di.dblQtyShipped, pfd.intPriceFixationDetailId, ar.intPriceFixationDetailAPARId
+from
+	tblCTContractDetail cd
+	join tblARInvoiceDetail di on isnull(di.intContractHeaderId,0) = cd.intContractHeaderId and isnull(di.intContractDetailId,0) = cd.intContractDetailId
+	join tblCTPriceFixation pf on pf.intContractHeaderId = cd.intContractHeaderId and pf.intContractDetailId = cd.intContractDetailId
+	join tblCTPriceFixationDetail pfd on pfd.intPriceFixationId = pf.intPriceFixationId
+	left join tblCTPriceFixationDetailAPAR ar on ar.intInvoiceDetailId = di.intInvoiceDetailId and ar.intPriceFixationDetailId = pfd.intPriceFixationDetailId
+where
+	isnull(di.ysnReturned,0) = 0
+	and pfd.dblCashPrice = di.dblPrice
+)tbl
+where tbl.intPriceFixationDetailId is not null and tbl.intPriceFixationDetailAPARId is null
+
+exec ('disable trigger trgCTPriceFixationDetailAPARDelete on tblCTPriceFixationDetailAPAR');
+
+IF not EXISTS
+	(
+		SELECT
+			1
+		FROM
+			sys.columns 
+		WHERE
+			Name = N'ysnFixedPriceAndInvoiceLink'
+			AND Object_ID = Object_ID(N'tblCTMiscellaneous')
+	)
+	BEGIN
+		exec('alter table tblCTMiscellaneous add ysnFixedPriceAndInvoiceLink bit not null default 0');
+	END
+
+IF OBJECT_ID(N'tempdb..#tmpB') IS NOT NULL
+BEGIN
+	exec('drop table #tmpB');
+END	
+create table #tmpB (intPriceFixationDetailAPARId int);
+
+IF OBJECT_ID(N'tempdb..#tmpA') IS NOT NULL
+BEGIN
+	exec('drop table #tmpA');
+END
+create table #tmpA (intValue int);
+exec('insert into #tmpA select top 1 1 from tblCTMiscellaneous where ysnFixedPriceAndInvoiceLink = 1')
+
+if exists (select top 1 1 from #tmpA)
+begin
+	goto _Exit;
+end
+
+IF not EXISTS
+	(
+		SELECT
+			1
+		FROM
+			sys.columns 
+		WHERE
+			Name = N'ysnPreDeploymentFix'
+			AND Object_ID = Object_ID(N'tblCTPriceFixationDetailAPAR')
+	)
+	BEGIN
+		exec('alter table tblCTPriceFixationDetailAPAR add ysnPreDeploymentFix bit null default 0');
+	END
+
+select @intInvoiceDetailId = min(intInvoiceDetailId) from @InvoiceDetail where intInvoiceDetailId > @intInvoiceDetailId;
+while (@intInvoiceDetailId is not null)
+begin
+	select @intInvoiceId = intInvoiceId, @intContractDetailId = intContractDetailId, @dblPrice = dblPrice, @dblQtyShipped = dblQtyShipped from @InvoiceDetail where intInvoiceDetailId = @intInvoiceDetailId;
+
+	select @intPriceFixationDetailId = tbl.intPriceFixationDetailId
+	from
+	(
+	select top 1
+		pfd.intPriceFixationDetailId
+		,pfd.dblCashPrice
+		,pfd.dblQuantity
+		,dblQtyShipped = isnull(sum(di.dblQtyShipped),0)
+	from
+		tblCTPriceFixation pf 
+		join tblCTPriceFixationDetail pfd on pfd.intPriceFixationId = pf.intPriceFixationId
+		left join tblCTPriceFixationDetailAPAR ar on ar.intPriceFixationDetailId = pfd.intPriceFixationDetailId
+		left join tblARInvoiceDetail di on di.intInvoiceDetailId = ar.intInvoiceDetailId and di.intInventoryShipmentChargeId is null
+	where
+		pf.intContractDetailId = @intContractDetailId
+	group by
+		pfd.intPriceFixationDetailId
+		,pfd.dblCashPrice
+		,pfd.dblQuantity
+	)tbl
+	where
+		tbl.dblCashPrice = @dblPrice		
+		and @dblQtyShipped <= (tbl.dblQuantity - tbl.dblQtyShipped)
+
+	if (isnull(@intPriceFixationDetailId,0) > 0)
+	begin
+
+		insert into tblCTPriceFixationDetailAPAR
+		(
+			intPriceFixationDetailId
+			,intBillId
+			,intBillDetailId
+			,intInvoiceId
+			,intInvoiceDetailId
+			,ysnMarkDelete
+			,intConcurrencyId
+		)
+		select
+			intPriceFixationDetailId = @intPriceFixationDetailId
+			,intBillId = null
+			,intBillDetailId = null
+			,intInvoiceId = @intInvoiceId
+			,intInvoiceDetailId = @intInvoiceDetailId
+			,ysnMarkDelete = null
+			,intConcurrencyId = 1
+			
+		insert into #tmpB select intPriceFixationDetailAPARId = SCOPE_IDENTITY();
+
+	end
+
+	set @intActiveInvoiceDetailId = @intInvoiceDetailId;
+	
+	select
+		@intInvoiceDetailId = null
+		,@intContractDetailId = null
+		,@dblPrice = null
+		,@dblQtyShipped = null
+		,@intPriceFixationDetailId = null;
+
+	select @intInvoiceDetailId = min(intInvoiceDetailId) from @InvoiceDetail where intInvoiceDetailId > @intActiveInvoiceDetailId;
+end
+
+exec('enable trigger trgCTPriceFixationDetailAPARDelete on tblCTPriceFixationDetailAPAR');
+
+exec('update tblCTMiscellaneous set ysnFixedPriceAndInvoiceLink = 1');
+
+exec('Update tblCTPriceFixationDetailAPAR set ysnPreDeploymentFix = 1 where intPriceFixationDetailAPARId in (select intPriceFixationDetailAPARId from #tmpB)');
+
+
+_Exit:
