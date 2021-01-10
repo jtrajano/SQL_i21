482c3c2b8504fa2086fbbc6fe8be1211c6641af8
Premkumar S <prem.selladurai@irely.com>
2021-01-10 21:53:54 +0530

    IN-700

--- i21Database/dbo/Stored Procedures/uspCTContractProcessStgXML.sql
+++ i21Database/dbo/Stored Procedures/uspCTContractProcessStgXML.sql
@@ -1697,11 +1697,18 @@ BEGIN TRY
 						END
 					END
 
+					SELECT @intFutureMarketId = NULL
+
+					SELECT @intFutureMarketId = intFutureMarketId
+					FROM tblRKFutureMarket FM
+					WHERE FM.strFutMarketName = @strFutMarketName
+
 					IF @strFutureMonth IS NOT NULL
 						AND NOT EXISTS (
 							SELECT 1
 							FROM tblRKFuturesMonth MO
 							WHERE MO.strFutureMonth = @strFutureMonth
+							AND intFutureMarketId=@intFutureMarketId
 							)
 					BEGIN
 						IF @strErrorMessage <> ''
@@ -2098,8 +2105,6 @@ BEGIN TRY
 
 					SELECT @intPriceItemUOMId = NULL
 
-					SELECT @intFutureMarketId = NULL
-
 					SELECT @intFutureMonthId = NULL
 
 					SELECT @intPricingTypeId = NULL
@@ -2208,13 +2213,10 @@ BEGIN TRY
 					WHERE PU.intItemId = @intItemId
 						AND PU.intUnitMeasureId = @intPriceUnitMeasureId
 
-					SELECT @intFutureMarketId = intFutureMarketId
-					FROM tblRKFutureMarket FM
-					WHERE FM.strFutMarketName = @strFutMarketName
-
 					SELECT @intFutureMonthId = intFutureMonthId
 					FROM tblRKFuturesMonth MO
 					WHERE MO.strFutureMonth = @strFutureMonth
+					AND intFutureMarketId=@intFutureMarketId
 
 					SELECT @intPricingTypeId = intPricingTypeId
 					FROM tblCTPricingType PT
