cb2bef2aa501138e57a3d5f242a19ee1294b90a9
Jopet Lambinicio <jopet.lambinicio@irely.com>
2021-01-13 12:41:11 +0800

    AP-9228

---
+++ i21Database/Scripts/Post-Deployment/AP/NonEmployeeCompensationTo1099NEC.sql
@@ -0,0 +1,21 @@
+﻿--VENDOR
+UPDATE E
+SET E.str1099Form = '1099-NEC',
+	E.str1099Type = 'Nonemployee Compensation'
+FROM tblEMEntity E
+WHERE E.str1099Form = '1099-MISC' AND E.str1099Type = 'Nonemployee Compensation'
+
+
+--VOUCHERS
+UPDATE BD
+SET BD.int1099Form = 7,
+	BD.int1099Category = 8
+FROM tblAPBillDetail BD
+INNER JOIN tblAPBill B ON B.intBillId = BD.intBillId
+OUTER APPLY (
+	SELECT Y.strFiscalYear
+	FROM tblGLFiscalYearPeriod YP
+	INNER JOIN tblGLFiscalYear Y ON Y.intFiscalYearId = YP.intFiscalYearId
+	WHERE B.dtmDate BETWEEN YP.dtmStartDate AND YP.dtmEndDate OR B.dtmDate = YP.dtmStartDate OR B.dtmDate = YP.dtmEndDate
+) FP
+WHERE CONVERT(INT, strFiscalYear) >= 2020 AND BD.int1099Form = 1 AND BD.int1099Category = 8
