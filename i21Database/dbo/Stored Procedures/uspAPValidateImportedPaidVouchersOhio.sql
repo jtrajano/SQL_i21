c7abf9cb588a7d106716dc16e4e9a00a86a76add
Erick Dela Cruz <erick.delacruz@irely.com>
2021-01-13 17:00:22 +0800

    AP-9429

--- i21Database/dbo/Stored Procedures/uspAPValidateImportedPaidVouchersOhio.sql
+++ i21Database/dbo/Stored Procedures/uspAPValidateImportedPaidVouchersOhio.sql
@@ -41,9 +41,10 @@ UPDATE A
 						AND B.ysnPaid = 0
 						AND B.ysnPosted = 1
 						AND B.intBillId > 0
-						AND 1 = (CASE WHEN B.intTransactionType = 1 AND A.dblPayment > 0 AND A.dblPayment <= B.dblAmountDue THEN 1
-									WHEN B.intTransactionType = 3 AND A.dblPayment < 0 AND ABS(A.dblPayment) <= B.dblAmountDue THEN 1
-									ELSE 0 END)
+						-- AND 1 = (CASE WHEN B.intTransactionType = 1 AND A.dblPayment > 0 AND A.dblPayment <= B.dblAmountDue THEN 1
+						-- 			WHEN B.intTransactionType = 3 AND A.dblPayment < 0 AND ABS(A.dblPayment) <= B.dblAmountDue THEN 1
+						-- 			ELSE 0 END)
+						AND A.dblPayment = B.dblAmountDue
 						THEN NULL
 					WHEN 
 						A.intCurrencyId != B.intCurrencyId
