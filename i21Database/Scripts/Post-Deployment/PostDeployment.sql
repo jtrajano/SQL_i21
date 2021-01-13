d1a750597f190b841869fdff1e1af50d2035496a
Jopet Lambinicio <jopet.lambinicio@irely.com>
2021-01-13 12:41:11 +0800

    AP-9228

--- i21Database/Scripts/Post-Deployment/PostDeployment.sql
+++ i21Database/Scripts/Post-Deployment/PostDeployment.sql
@@ -321,6 +321,7 @@ print 'BEGIN POST DEPLOYMENT'
 :r .\AP\PurgePayableCompleted.sql
 :r .\AP\UpdatePaymentDetailOffset.sql
 :r .\AP\UpdatePayableFreightTerm.sql
+:r .\AP\NonEmployeeCompensationTo1099NEC.sql
 
 -- Inventory 
 :r .\IC\01_InventoryTransactionTypes.sql 
