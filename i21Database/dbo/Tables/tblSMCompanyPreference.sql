a720741c951282e29ceaea85146b07b544f76df5
IRELY\JMendones <jesmar.mendones@irely.com>
2021-01-14 17:45:05 +0800

    FRM-9309

--- i21Database/dbo/Tables/tblSMCompanyPreference.sql
+++ i21Database/dbo/Tables/tblSMCompanyPreference.sql
@@ -70,5 +70,7 @@
     [strADUserName]									NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NULL,
     [strADPassword]									NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NULL,
 	[ysnEnableUserActivityLog]						BIT NOT NULL DEFAULT 0,
+	[strPowerBIAdminUsername]						NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NULL,
+	[strPowerBIAdminPassword]						NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NULL,
     [intConcurrencyId]								INT NOT NULL DEFAULT 1
 )
