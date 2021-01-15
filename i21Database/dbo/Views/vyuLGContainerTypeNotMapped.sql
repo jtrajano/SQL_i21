40cfff5fe2af6c2b6eea13d8fa404fdadae0a2de
Arman Arenas <arman.arenas@irely.com>
2021-01-15 20:57:08 +0800

    LG-2796 added vyuLGContainerTypeNotMapped.dblConversionFactor

--- i21Database/dbo/Views/vyuLGContainerTypeNotMapped.sql
+++ i21Database/dbo/Views/vyuLGContainerTypeNotMapped.sql
@@ -20,8 +20,13 @@ SELECT CT.intContainerTypeId
 	  ,C.strCommodityCode 
 	  ,C.strDescription AS strCommodityDescription
 	  ,UM.strUnitMeasure
+	  ,dblConversionFactor = CASE WHEN (UM.intUnitMeasureId = WUM.intUnitMeasureId) THEN 1 ELSE ISNULL(UMC.dblConversionToStock, 0) END
 FROM tblLGContainerType CT
 LEFT JOIN tblLGContainerTypeCommodityQty CTCQ ON CT.intContainerTypeId = CTCQ.intContainerTypeId
 LEFT JOIN tblICCommodity C ON C.intCommodityId = CTCQ.intCommodityId
+LEFT JOIN tblICUnitMeasure WUM ON WUM.intUnitMeasureId = CT.intWeightUnitMeasureId
 LEFT JOIN tblICUnitMeasure UM ON UM.intUnitMeasureId = CTCQ.intUnitMeasureId
+OUTER APPLY 
+	(SELECT TOP 1 dblConversionToStock FROM tblICUnitMeasureConversion 
+	WHERE intUnitMeasureId = WUM.intUnitMeasureId AND intStockUnitMeasureId = UM.intUnitMeasureId) UMC
 LEFT JOIN tblICCommodityAttribute CA ON CA.intCommodityAttributeId = CTCQ.intCommodityAttributeId
