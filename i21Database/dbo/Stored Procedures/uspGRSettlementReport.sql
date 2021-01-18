a6ca0fea14bf9686fc3e2e51029b64fe5169b275
Mon Gonzales <mon.gonzales@irely.com>
2021-01-18 13:37:25 +0800

    GRN-2503 - Each ticket shows up twice on settlement

--- i21Database/dbo/Stored Procedures/uspGRSettlementReport.sql
+++ i21Database/dbo/Stored Procedures/uspGRSettlementReport.sql
@@ -777,6 +777,13 @@ BEGIN
 			JOIN tblSCTicket SC 
 				ON SC.intTicketId = BillDtl.intScaleTicketId
 					AND BillDtl.intCustomerStorageId IS NULL
+
+			LEFT JOIN tblICInventoryReceiptItem INVRCPTITEM 
+				on SC.intTicketId = INVRCPTITEM.intSourceId
+			
+			LEFT JOIN tblICInventoryReceipt InventoryReceipt
+				on INVRCPTITEM.intInventoryReceiptId = InventoryReceipt.intInventoryReceiptId
+
 			LEFT JOIN tblEMEntitySplit EM 
 				ON EM.intSplitId = SC.intSplitId 
 					AND SC.intSplitId <> 0
@@ -852,6 +859,8 @@ BEGIN
 					ON PartialPayment.intPaymentId = PYMT.intPaymentId
 			WHERE BNKTRN.intBankAccountId = @intBankAccountId
 				AND BNKTRN.strTransactionId = @strPaymentNo
+				and INVRCPTITEM.intInventoryReceiptItemId is null
+				and InventoryReceipt.intSourceType = 1
 			/*--------------------------------------------------------
 			*******************FROM SETTLE STORAGE********************
 			--------------------------------------------------------*/			
