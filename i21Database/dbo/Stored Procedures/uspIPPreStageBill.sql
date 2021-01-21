18f0c0ec6c4b965b8daf1b82c4a65141eb2120e6
Premkumar S <prem.selladurai@irely.com>
2021-01-21 15:03:21 +0530

    MFG-4336

---
+++ i21Database/dbo/Stored Procedures/uspIPPreStageBill.sql
@@ -0,0 +1,32 @@
+﻿CREATE PROCEDURE uspIPPreStageBill (
+	@strBillId NVARCHAR(MAX)
+	,@strRowState NVARCHAR(50) = NULL
+	,@intUserId INT = NULL
+	)
+AS
+BEGIN TRY
+	SET NOCOUNT ON
+
+	DECLARE @ErrMsg NVARCHAR(MAX)
+
+	INSERT INTO dbo.tblAPBillPreStage (
+		intBillId
+		,strRowState
+		,intUserId
+		)
+	SELECT Item Collate Latin1_General_CI_AS
+		,@strRowState
+		,@intUserId
+	FROM [dbo].[fnSplitString](@strBillId, ',')
+END TRY
+
+BEGIN CATCH
+	SET @ErrMsg = ERROR_MESSAGE()
+
+	RAISERROR (
+			@ErrMsg
+			,16
+			,1
+			,'WITH NOWAIT'
+			)
+END CATCH
---
+++ i21Database/dbo/Tables/tblAPBillPreStage.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE tblAPBillPreStage
+(
+	intBillPreStageId				INT IDENTITY(1,1) PRIMARY KEY, 
+	intBillId						INT,
+	strRowState						NVARCHAR(50) COLLATE Latin1_General_CI_AS,
+	intUserId						INT,
+	strFeedStatus					NVARCHAR(50) COLLATE Latin1_General_CI_AS,
+	dtmFeedDate						DATETIME CONSTRAINT DF_tblAPBillPreStage_dtmFeedDate DEFAULT GETDATE(),
+	strMessage						NVARCHAR(MAX) COLLATE Latin1_General_CI_AS
+)
