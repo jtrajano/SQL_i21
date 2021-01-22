1a6494a2f2653ec6653372765fda7228ddf8217b
IRELY\JMendones <jesmar.mendones@irely.com>
2021-01-22 09:35:17 +0800

    SM-5081

--- i21Database/dbo/Stored Procedures/uspGLFixStartingNumbers.sql
+++ i21Database/dbo/Stored Procedures/uspGLFixStartingNumbers.sql
@@ -188,40 +188,40 @@ BEGIN
 	SET @intNumber = NULL 
 END 
 
--- 6 of 6: Fix the Batch Posting.  -> moved to uspSMFixStartingNumbers
---BEGIN 
---	SET @strTransactionType = 'Batch Post'
---	SELECT	@strTransactionId = strPrefix + CAST(intNumber AS NVARCHAR(20)) 
---			,@intNumber = intNumber
---			,@strPrefix = strPrefix
---	FROM	dbo.tblSMStartingNumber
---	WHERE	strTransactionType = @strTransactionType
+-- 6 of 6: Fix the Batch Posting. 
+BEGIN 
+	SET @strTransactionType = 'Batch Post'
+	SELECT	@strTransactionId = strPrefix + CAST(intNumber AS NVARCHAR(20)) 
+			,@intNumber = intNumber
+			,@strPrefix = strPrefix
+	FROM	dbo.tblSMStartingNumber
+	WHERE	strTransactionType = @strTransactionType
 
---	IF EXISTS (SELECT TOP 1 1 FROM dbo.tblGLDetail)
---	BEGIN 
---		-- Retrieve the Max transaction id. 
---		SET @intNumber = NULL
+	IF EXISTS (SELECT TOP 1 1 FROM dbo.tblGLDetail)
+	BEGIN 
+		-- Retrieve the Max transaction id. 
+		SET @intNumber = NULL
 		
---		-- Retrieve the highest number part in the transaction id. 
---		SELECT	@intNumber = MAX(CAST(REPLACE(strBatchId, @strPrefix, '') AS INT))	
---		FROM	dbo.tblGLDetail  WHERE strBatchId LIKE @strPrefix + '%'
+		-- Retrieve the highest number part in the transaction id. 
+		SELECT	@intNumber = MAX(CAST(REPLACE(strBatchId, @strPrefix, '') AS INT))	
+		FROM	dbo.tblGLDetail  WHERE strBatchId LIKE @strPrefix + '%'
 		
---		DECLARE @intSMNumber int = 0;
---		SELECT	@intSMNumber = MAX(CAST(REPLACE(strBatchNo, 'BATCH-', '') AS INT))	
---		FROM	dbo.tblSMBatchPostingLog
+		DECLARE @intSMNumber int = 0;
+		SELECT	@intSMNumber = MAX(CAST(REPLACE(strBatchNo, 'BATCH-', '') AS INT))	
+		FROM	dbo.tblSMBatchPostingLog
 
---		IF (@intNumber IS NOT NULL AND ISNULL(@intSMNumber, 0) < @intNumber)	
---		BEGIN 	
---			-- Update the next transaction id. 
---			UPDATE	dbo.tblSMStartingNumber
---			SET		intNumber = @intNumber + 1 
---			WHERE	strTransactionType = @strTransactionType	
---		END 
---	END 
+		IF (@intNumber IS NOT NULL AND ISNULL(@intSMNumber, 0) < @intNumber)	
+		BEGIN 	
+			-- Update the next transaction id. 
+			UPDATE	dbo.tblSMStartingNumber
+			SET		intNumber = @intNumber + 1 
+			WHERE	strTransactionType = @strTransactionType	
+		END 
+	END 
 
---	-- Clean-up 
---	SET @strTransactionId = NULL
---	SET @strTransactionType = NULL 
---	SET @strPrefix = NULL 
---	SET @intNumber = NULL 
---END 
+	-- Clean-up 
+	SET @strTransactionId = NULL
+	SET @strTransactionType = NULL 
+	SET @strPrefix = NULL 
+	SET @intNumber = NULL 
+END 
