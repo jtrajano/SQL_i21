90420f3d5703fa77f0b12d619c981374ed81c9cf
Smith Wilson G. de Jesus <smith.dejesus@irely.com>
2021-01-23 12:43:03 +0800

    RM-4054

--- i21Database/dbo/Views/vyuRKGetDPRContractHedgeSummary.sql
+++ i21Database/dbo/Views/vyuRKGetDPRContractHedgeSummary.sql
@@ -2,14 +2,34 @@
 
 AS
 
-SELECT intRowId = CAST(ROW_NUMBER() OVER (ORDER BY intSeqNo DESC) AS INT)
-	, intDPRHeaderId
-	, strCommodityCode
-	, intSeqNo = CAST(intSeqNo AS INT)
-	, strType
-	, dblTotal = CAST(ISNULL(SUM(ISNULL(dblTotal, 0.00)), 0.00) AS DECIMAL(24, 10))
-FROM tblRKDPRContractHedge
-GROUP BY intDPRHeaderId
-	, strCommodityCode
-	, intSeqNo
-	, strType
+SELECT 
+intRowId = CAST(ROW_NUMBER() OVER (ORDER BY intSeqNo DESC) AS INT)
+,* 
+FROM (
+
+	SELECT  intDPRHeaderId
+		, strCommodityCode
+		, intSeqNo = CAST(intSeqNo AS INT)
+		, strType
+		, dblTotal = CAST(ISNULL(SUM(ISNULL(dblTotal, 0.00)), 0.00) AS DECIMAL(24, 10))
+	FROM tblRKDPRContractHedge
+	WHERE strType NOT IN ('Basis Risk','Price Risk')
+	GROUP BY intDPRHeaderId
+		, strCommodityCode
+		, intSeqNo
+		, strType
+	HAVING CAST(ISNULL(SUM(ISNULL(dblTotal, 0.00)), 0.00) AS DECIMAL(24, 10)) <> 0
+
+	UNION ALL
+	SELECT intDPRHeaderId
+		, strCommodityCode
+		, intSeqNo = CAST(intSeqNo AS INT)
+		, strType
+		, dblTotal = CAST(ISNULL(SUM(ISNULL(dblTotal, 0.00)), 0.00) AS DECIMAL(24, 10))
+	FROM tblRKDPRContractHedge
+	WHERE strType IN ('Basis Risk','Price Risk')
+	GROUP BY intDPRHeaderId
+		, strCommodityCode
+		, intSeqNo
+		, strType
+) t 
