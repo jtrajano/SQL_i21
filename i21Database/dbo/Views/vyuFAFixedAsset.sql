7ecdb77ae118f984c45b0971581d85870dd02043
Jeff Trajano <jeffrey.trajano@irely.com>
2021-01-23 11:20:20 +0800

    FA-160 Fixed asset - add fields to search screen

---
+++ i21Database/dbo/Views/vyuFAFixedAsset.sql
@@ -0,0 +1,29 @@
+CREATE VIEW vyuFAFixedAsset
+AS
+SELECT 
+FA.*,
+DM.strDepreciationMethodId,
+DM.strConvention,
+DM.strDepreciationType,
+GLAsset.strAccountId strAssetAccountId,
+GLExpense.strAccountId strExpenseAccountId,
+GLDepreciation.strAccountId strDepreciationAccountId,
+GLAccumulation.strAccountId strAccumulatedAccountId,
+GLGainLoss.strAccountId strGainLossAccountId,
+D.dblDepreciationToDate,
+Company.strLocationName strCompanyLocation,
+Currency.strCurrency
+from tblFAFixedAsset FA
+JOIN tblFADepreciationMethod DM ON FA.intDepreciationMethodId = DM.intDepreciationMethodId
+LEFT JOIN tblGLAccount GLAsset ON GLAsset.intAccountId = FA.intAssetAccountId
+LEFT JOIN tblGLAccount GLExpense ON GLExpense.intAccountId = FA.intExpenseAccountId
+LEFT JOIN tblGLAccount GLDepreciation ON GLDepreciation.intAccountId = FA.intDepreciationAccountId
+LEFT JOIN tblGLAccount GLAccumulation ON GLAccumulation.intAccountId = FA.intAccumulatedAccountId
+LEFT JOIN tblGLAccount GLGainLoss ON GLGainLoss.intAccountId = FA.intGainLossAccountId
+LEFT JOIN tblSMCurrency Currency ON Currency.intCurrencyID=FA.intCurrencyId
+LEFT JOIN tblSMCompanyLocation Company ON Company.intCompanyLocationId = FA.intCompanyLocationId
+OUTER APPLY(
+	SELECT TOP 1 dblDepreciationToDate FROM tblFAFixedAssetDepreciation WHERE intDepreciationMethodId = FA.intDepreciationMethodId ORDER BY intAssetDepreciationId DESC
+)D
+
+
