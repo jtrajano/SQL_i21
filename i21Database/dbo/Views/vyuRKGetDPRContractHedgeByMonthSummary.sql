64f6cafd18c30b25fc5a7b0dfc6ff9f11a4e4195
Smith Wilson G. de Jesus <smith.dejesus@irely.com>
2021-01-23 12:43:03 +0800

    RM-4054

--- i21Database/dbo/Views/vyuRKGetDPRContractHedgeByMonthSummary.sql
+++ i21Database/dbo/Views/vyuRKGetDPRContractHedgeByMonthSummary.sql
@@ -1,17 +1,38 @@
 ﻿CREATE VIEW [dbo].[vyuRKGetDPRContractHedgeByMonthSummary]
 
 AS
+SELECT 
+	intRowId = CAST(ROW_NUMBER() OVER (ORDER BY intSeqNo DESC) AS INT)
+	,* 
 
-SELECT intRowId = CAST(ROW_NUMBER() OVER (ORDER BY intSeqNo DESC) AS INT)
-	, intDPRHeaderId
-	, strCommodityCode
-	, intSeqNo = CAST(intSeqNo AS INT)
-	, strContractEndMonth
-	, strType
-	, dblTotal = CAST(ISNULL(SUM(ISNULL(dblTotal, 0.00)), 0.00) AS DECIMAL(24, 10))
-FROM tblRKDPRContractHedgeByMonth
-GROUP BY intDPRHeaderId
-	, strCommodityCode
-	, intSeqNo
-	, strContractEndMonth
-	, strType
+FROM (
+	SELECT intDPRHeaderId
+		, strCommodityCode
+		, intSeqNo = CAST(intSeqNo AS INT)
+		, strContractEndMonth
+		, strType
+		, dblTotal = CAST(ISNULL(SUM(ISNULL(dblTotal, 0.00)), 0.00) AS DECIMAL(24, 10))
+	FROM tblRKDPRContractHedgeByMonth
+	WHERE strType NOT IN ('Basis Risk','Price Risk')
+	GROUP BY intDPRHeaderId
+		, strCommodityCode
+		, intSeqNo
+		, strContractEndMonth
+		, strType
+	HAVING CAST(ISNULL(SUM(ISNULL(dblTotal, 0.00)), 0.00) AS DECIMAL(24, 10)) <> 0
+
+	UNION ALL
+	SELECT intDPRHeaderId
+		, strCommodityCode
+		, intSeqNo = CAST(intSeqNo AS INT)
+		, strContractEndMonth
+		, strType
+		, dblTotal = CAST(ISNULL(SUM(ISNULL(dblTotal, 0.00)), 0.00) AS DECIMAL(24, 10))
+	FROM tblRKDPRContractHedgeByMonth
+	WHERE strType IN ('Basis Risk','Price Risk')
+	GROUP BY intDPRHeaderId
+		, strCommodityCode
+		, intSeqNo
+		, strContractEndMonth
+		, strType
+) t 
