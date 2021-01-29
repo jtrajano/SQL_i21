d8fd009208c6a039463b30011da6d0198fe60b5a
Roudinee Panteria <roudinee.panteria@irely.com>
2021-01-29 18:25:49 +0800

    AR-12864

--- i21Database/dbo/Views/vyuARTaxReport.sql
+++ i21Database/dbo/Views/vyuARTaxReport.sql
@@ -128,7 +128,7 @@ INNER JOIN (
 		 , strItemNo				= ITEM.strItemNo
 		 , strCategoryCode			= CATEGORY.strCategoryCode
 		 , intTaxClassId			= IDT.intTaxClassId
-		 , intSalesTaxAccountId		= TAXCODE.intSalesTaxAccountId
+		 , intSalesTaxAccountId		= IDT.intSalesTaxAccountId
 		 , intPurchaseTaxAccountId	= TAXCODE.intPurchaseTaxAccountId
 		 , intCategoryId			= ITEM.intCategoryId
 		 , intTaxCodeCount			= ISNULL(TAXTOTAL.intTaxCodeCount, TAXCLASSTOTAL.intTaxClassCount)
@@ -148,6 +148,7 @@ INNER JOIN (
 			 , ysnTaxExempt
 			 , ysnTaxAdjusted
 			 , ysnInvalidSetup
+			 , intSalesTaxAccountId
 		FROM dbo.tblARInvoiceDetailTax WITH (NOLOCK)
 	) IDT ON IDT.intInvoiceDetailId = ID.intInvoiceDetailId
 	LEFT JOIN (
