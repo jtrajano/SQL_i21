b94891ffd43fb7b281665a168764f68536b2a5a9
Jeff Trajano <jeffrey.trajano@irely.com>
2021-01-30 19:27:05 +0800

    FA-154 FA-155

--- i21Database/dbo/Functions/fnFAComputeDepreciation.sql
+++ i21Database/dbo/Functions/fnFAComputeDepreciation.sql
@@ -1,17 +1,14 @@
 
 CREATE FUNCTION [dbo].[fnFAComputeDepreciation]
 (
- @intAssetId INT,
- @intYear INT,
- @intMonth INT,
- @totalMonths INT
- 
- )
+    @intAssetId INT
+)
 RETURNS @tbl TABLE (
 	dblBasis NUMERIC(18,6) NULL,
 	dblMonth NUMERIC(18,6) NULL,
 	dblDepre NUMERIC(18,6) NULL,
-	ysnFullyDepreciated BIT NULL
+	ysnFullyDepreciated BIT NULL,
+	strError NVARCHAR(100) NULL
 )
 AS
 BEGIN
@@ -26,78 +23,71 @@ SELECT
 FROM tblFAFixedAsset A join tblFADepreciationMethod B on A.intAssetId = B.intAssetId
 WHERE A.intAssetId = @intAssetId
 
--- Service Year Percentage
-DECLARE @dblPercentage	INT = (SELECT ISNULL(dblPercentage,1) as dblPercentage FROM tblFADepreciationMethodDetail A 
-										WHERE A.[intDepreciationMethodId] = @intDepreciationMethodId and intYear =  @intYear)
-
 -- Running Balance
 DECLARE @dblYear NUMERIC (18,6) = (SELECT TOP 1 dblDepreciationToDate FROM tblFAFixedAssetDepreciation A WHERE A.[intAssetId] =@intAssetId
 ORDER BY intAssetDepreciationId DESC)
 
--- Computation
+IF ROUND(@dblYear,2) >=  ROUND(@dblBasis,2)
+BEGIN
+	INSERT INTO @tbl(strError,ysnFullyDepreciated) select 'Fixed asset should be disposed' , 1
+	RETURN  
+END  
+
+
+DECLARE @intYear INT, @intMonth INT, @totalMonths INT ,@intExcessMonth INT,@intServiceYear INT, @intMonthDivisor INT = 12
+SELECT @intMonth = COUNT(1) FROM tblFAFixedAssetDepreciation WHERE intAssetId = @intAssetId 
+select @intYear = ceiling(@intMonth/12.0)
+
+
+SELECT 
+@totalMonths=ISNULL(intServiceYear,0)* 12 + ISNULL(intMonth ,0),
+@intExcessMonth = isnull(intMonth,0), 
+@intServiceYear = ISNULL(intServiceYear,0)
+FROM tblFADepreciationMethod where  @intAssetId = intAssetId	
+		
+IF (@intMonth > (@intServiceYear * 12) AND @intExcessMonth > 0)
+    SET @intMonthDivisor = @intExcessMonth
+
+
+-- Service Year Percentage
+DECLARE @dblPercentage	INT = (SELECT ISNULL(dblPercentage,1) as dblPercentage FROM tblFADepreciationMethodDetail A 
+										WHERE A.[intDepreciationMethodId] = @intDepreciationMethodId and intYear =  @intYear)
+
+
 
 	DECLARE @dblAnnualDep	NUMERIC (18,6)	= (@dblBasis * (@dblPercentage * .01))
+	DECLARE @dblMonth		NUMERIC (18,6)	= (@dblAnnualDep / @intMonthDivisor)
+	DECLARE @intDaysInFirstMonth INT = DAY(EOMONTH(@dtmPlacedInService)) --31
+ 
+    IF @intMonth = 1 
+    BEGIN
+        IF @strConvention = 'Actual Days'
+	    BEGIN
+            DECLARE @intDaysRemainingFirstMonth INT =  @intDaysInFirstMonth - DAY(@dtmPlacedInService)
+            SET @dblMonth =  @dblMonth * (@intDaysRemainingFirstMonth/ CAST(@intDaysInFirstMonth AS FLOAT))
+        END
+        IF @strConvention = 'Mid Month'
+	    BEGIN
+            SET @dblMonth =  @dblMonth * .50
+        END
 
-	DECLARE @intExcessMonthBegin INT =  @totalMonths  - (@totalMonths % 12)
-	DECLARE @intMonthDivisor INT =12
-	
+    END
+    IF @intMonth > @totalMonths
+    BEGIN
+		SELECT @dblPercentage = dblPercentage FROM tblFADepreciationMethodDetail A WHERE A.[intDepreciationMethodId] = @intDepreciationMethodId and intYear =  1
+		SELECT @dblAnnualDep = (@dblBasis * (@dblPercentage * .01))
+		SELECT @dblMonth =@dblAnnualDep  / case when ISNULL( @intServiceYear,0) > 0 THEN 12 ELSE @intExcessMonth END
+        IF @strConvention = 'Actual Days'
+	    BEGIN
+            DECLARE @intDaysRemainingLastMonth INT =  DAY(@dtmPlacedInService)
+            SELECT @dblMonth =  @dblMonth * (@intDaysRemainingLastMonth/CAST(@intDaysInFirstMonth AS FLOAT)) , @ysnFullyDepreciated = 1
+        END
+        IF @strConvention = 'Mid Month'
+	    BEGIN
+            SELECT @dblMonth =  @dblMonth * .50
+        END
 
-	IF @intExcessMonthBegin = 0 OR @intMonth > @intExcessMonthBegin
-		SET @intMonthDivisor = @totalMonths % 12
-	
-	DECLARE @dblMonth		NUMERIC (18,6)	= (@dblAnnualDep / CASE WHEN @intMonthDivisor = 0 THEN 1 ELSE @intMonthDivisor END)								--NEED TO VERIFY IF ITS REALLY 12
-
-	IF @strConvention = 'Mid Month'
-	BEGIN
-		IF @intMonth = 1 
-			SELECT @dblMonth = @dblMonth /2
-
-		IF @intMonth = @totalMonths + 1
-		BEGIN
-			SELECT @dblMonth= @dblAnnualDep / 24
-			SET @ysnFullyDepreciated = 1
-		END
-			
-			
-	END
-	IF @strConvention = 'Actual Days'
-	BEGIN
-		DECLARE @intDaysInFirstMonth INT = DAY(EOMONTH(@dtmPlacedInService))
-		DECLARE @intDaysRemainingFirstMonth INT =  @intDaysInFirstMonth - DAY(@dtmPlacedInService)
-
-		IF @intMonth = 1 
-		BEGIN
-			SET @dblMonth =  @dblMonth * (@intDaysRemainingFirstMonth/ CAST(@intDaysInFirstMonth AS FLOAT))
-		END
-		IF @intMonth > @totalMonths
-		BEGIN
-			DECLARE @intDaysRemainingLastMonth INT = @intDaysInFirstMonth - @intDaysRemainingFirstMonth
-			SELECT @dblMonth =  @dblMonth * (@intDaysRemainingLastMonth/CAST(@intDaysInFirstMonth AS FLOAT)) , @ysnFullyDepreciated = 1
-			
-		END
-	END
-	ELSE
-	BEGIN -- Full Month
-		IF @intMonth = @totalMonths + 1
-			SET @ysnFullyDepreciated = 1
-	END
-	-- ELSE Full Month would be in effect
-	
-	-- IF @strConvention = 'Half Year'
-	-- BEGIN
-	-- 	IF @intMonth BETWEEN 1 AND 12
-	-- 		OR @intMonth BETWEEN    @totalMonths - 11 AND @totalMonths
-	-- 		SELECT @dblMonth = @dblMonth / 2
-		
-	-- END
-
-	-- IF @strConvention = 'Mid Quarter'
-	-- BEGIN
-	-- 	IF @intMonth BETWEEN 1 AND 3
-	-- 		OR @intMonth BETWEEN    @totalMonths -1 AND @totalMonths+1 -- 12 -2 == 10 October , November , December
-	-- 		SELECT @dblMonth = @dblMonth /2  --- half of quarter
-		
-	-- END
+    END
 	DECLARE @dblDepre		NUMERIC (18,6)	= (@dblMonth ) + ISNULL(@dblYear,0)	
 
 	INSERT INTO @tbl(
@@ -112,6 +102,4 @@ ORDER BY intAssetDepreciationId DESC)
 	RETURN
 
 END
-
-GO
 
