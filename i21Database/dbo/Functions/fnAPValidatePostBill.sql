b82f032c7779c199edf5bfedb7a23905b99b1c60
Erick Dela Cruz <erick.delacruz@irely.com>
2021-02-01 16:05:40 +0800

    AP-9465

--- i21Database/dbo/Functions/fnAPValidatePostBill.sql
+++ i21Database/dbo/Functions/fnAPValidatePostBill.sql
@@ -506,6 +506,19 @@ BEGIN
 			)
 		)--Prepay and Debit Memo transactions
 		
+		INSERT INTO @returntable(strError, strTransactionType, strTransactionId, intTransactionId, intErrorKey)
+		SELECT
+			ISNULL(C.strItemNo, A2.strMiscDescription) + ' has incorrect cost. Cost cannot be negative.',
+			'Bill',
+			A.strBillId,
+			A.intBillId,
+			37
+		FROM tblAPBill A
+		INNER JOIN tblAPBillDetail A2 ON A.intBillId = A2.intBillId
+		LEFT JOIN tblICItem C ON A2.intItemId = C.intItemId  
+		WHERE 
+			A.intBillId IN (SELECT intBillId FROM @tmpBills) 
+		AND A2.dblCost < 0
 
 	END
 	ELSE
