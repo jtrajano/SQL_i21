0a0de30fdf6d28cbd52dec6e72f4b78ede889f8d
Premkumar S <prem.selladurai@irely.com>
2021-02-05 23:15:48 +0530

    IN-741

---
+++ i21Database/dbo/Views/vyuIPAuditView.sql
@@ -0,0 +1,14 @@
+﻿CREATE VIEW vyuIPAuditView
+AS
+SELECT 
+	A.intAuditId
+	,A.intLogId
+	,A.strAction
+	,A.strChange
+	,A.strFrom
+	,A.strTo
+	,A.strAlias
+	,A.ysnField
+	,A.ysnHidden
+	,A.intParentAuditId
+FROM tblSMAudit A 
---
+++ i21Database/dbo/Views/vyuIPLogView.sql
@@ -0,0 +1,8 @@
+﻿CREATE VIEW vyuIPLogView
+AS
+SELECT L.intLogId
+	,L.dtmDate
+	,L.intEntityId
+	,L.strRoute
+FROM tblSMLog L
+WHERE L.strType = 'Audit' 
