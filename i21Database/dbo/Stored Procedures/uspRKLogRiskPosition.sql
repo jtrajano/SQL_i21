3928d81fc8f63bc8834f8b0d91c7d51df65a881b
Smith Wilson G. de Jesus <smith.dejesus@irely.com>
2021-02-09 08:44:39 +0800

    RM-4083

--- i21Database/dbo/Stored Procedures/uspRKLogRiskPosition.sql
+++ i21Database/dbo/Stored Procedures/uspRKLogRiskPosition.sql
@@ -166,6 +166,7 @@ BEGIN
 			, @intUserId = intUserId
 			, @intEntityId = intEntityId
 			, @strNotes = strNotes
+			, @strInOut = strInOut
 			, @ysnDelete = ysnDelete
 			, @strMiscFields = strMiscFields
 		FROM #tmpSummaryLogs
@@ -511,7 +512,7 @@ BEGIN
 				, @intBookId
 				, @intSubBookId
 				, @intLocationId
-				, strInOut = CASE WHEN UPPER(@strDistributionType) = 'BUY' THEN 'IN' ELSE 'OUT' END
+				, strInOut = @strInOut
 				, dblOrigNoOfLots = @dblNoOfLots 
 				, @dblContractSize
 				, dblOrigQty = ISNULL(@dblQty, @dblNoOfLots * @dblContractSize)
