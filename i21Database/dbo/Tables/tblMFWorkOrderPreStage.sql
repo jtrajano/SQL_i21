832411c7ef2110195d3615b1edb7d672c0db390b
Premkumar S <prem.selladurai@irely.com>
2021-02-11 21:53:49 +0530

    MFG-4259

---
+++ i21Database/dbo/Tables/tblMFWorkOrderPreStage.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE tblMFWorkOrderPreStage (
+	intWorkOrderPreStageId INT IDENTITY(1, 1) PRIMARY KEY
+	,intWorkOrderId INT
+	,intWorkOrderStatusId INT
+	,intUserId INT
+	,intStatusId INT
+	,strRowState NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL
+	,dtmFeedDate DATETIME CONSTRAINT DF_tblMFWorkOrderPreStage_dtmFeedDate DEFAULT GETDATE()
+	,strMessage NVARCHAR(MAX) COLLATE Latin1_General_CI_AS
+	,ysnMailSent BIT
+	)
