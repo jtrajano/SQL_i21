06fae4d7a884728ad1fb75bef9e4ed3408894724
Feb Montefrio <feb.montefrio@irely.com>
2021-02-16 12:25:40 +0800

    [F] IC-8781

--- i21Database/dbo/Functions/fnICGetErrorMessage.sql
+++ i21Database/dbo/Functions/fnICGetErrorMessage.sql
@@ -275,7 +275,6 @@ BEGIN
 		WHEN @msgId = 80259 THEN 'Bill Amount as charge (or discount) for %s is already %f. You cannot over bill the transaction'
 		WHEN @msgId = 80260 THEN 'Bill Quantity as charge (or discount) for %s is already %f. You cannot over bill the transaction'
 		WHEN @msgId = 80261 THEN 'The Storage Location invalid in %s for %s.'
-		WHEN @msgId = 80262 THEN 'Unable to Post. The following contract(s) needs to be Priced:<p><br><div style="list-style-position: inside;;"><ul>%s</ul></div></p>'
 	END 
 
 	RETURN @msg
