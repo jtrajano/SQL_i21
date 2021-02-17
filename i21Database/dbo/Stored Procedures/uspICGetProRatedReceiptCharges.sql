47ad7a86b4f93f7d15da80bc742d21caaa8378ca
Feb Montefrio <feb.montefrio@irely.com>
2021-02-17 14:55:39 +0800

    [B] IC-9246

--- i21Database/dbo/Stored Procedures/uspICGetProRatedReceiptCharges.sql
+++ i21Database/dbo/Stored Procedures/uspICGetProRatedReceiptCharges.sql
@@ -353,7 +353,8 @@ BEGIN
 		)  IRCT
 
 	WHERE
-		A.intInventoryReceiptId = @intInventoryReceiptId	
+		A.intInventoryReceiptId = @intInventoryReceiptId
+		AND A.intEntityVendorId = r.intEntityId
 		AND ISNULL(A.intContractHeaderId, 0) = ISNULL(@intContractHeaderId, 0)
 		AND ISNULL(A.intContractDetailId, 0) = ISNULL(@intContractDetailId, 0)
 		AND ISNULL(rc.strChargesLink, '') = ISNULL(@strChargesLink, '')
