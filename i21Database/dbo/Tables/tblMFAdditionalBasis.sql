56abf7edb216b6641625f4753b8411f31ff961fb
Vinoth Kumar T <vinoth.t@irely.com>
2021-02-17 23:36:46 +0530

    MFG-4258

---
+++ i21Database/dbo/Tables/tblMFAdditionalBasis.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE tblMFAdditionalBasis
+(
+	intAdditionalBasisId INT IDENTITY(1,1) NOT NULL,
+	dtmAdditionalBasisDate DATETIME NOT NULL,
+	strComment NVARCHAR(MAX) COLLATE Latin1_General_CI_AS,
+	intConcurrencyId INT CONSTRAINT [DF_tblMFAdditionalBasis_intConcurrencyId] DEFAULT 0,
+
+	CONSTRAINT [PK_tblMFAdditionalBasis] PRIMARY KEY (intAdditionalBasisId),
+	CONSTRAINT [UK_tblMFAdditionalBasis_dtmAdditionalBasisDate] UNIQUE (dtmAdditionalBasisDate)
+)
