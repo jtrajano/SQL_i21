392af7f5bf5dffccbf769e8c6df6424d09e80404
Premkumar S <prem.selladurai@irely.com>
2021-02-19 19:28:10 +0530

    MFG-4258

--- i21Database/dbo/Tables/tblMFAdditionalBasis.sql
+++ i21Database/dbo/Tables/tblMFAdditionalBasis.sql
@@ -4,7 +4,12 @@
 	dtmAdditionalBasisDate DATETIME NOT NULL,
 	strComment NVARCHAR(MAX) COLLATE Latin1_General_CI_AS,
 	intConcurrencyId INT CONSTRAINT [DF_tblMFAdditionalBasis_intConcurrencyId] DEFAULT 0,
-
+	[dtmCreated] DATETIME, 
+    [intCreatedUserId] INT,
+	[dtmLastModified] DATETIME, 
+    [intLastModifiedUserId] INT,
+	[intLocationId] INT,
+	ysnImported BIT CONSTRAINT [DF_tblMFAdditionalBasis_ysnImported] DEFAULT 0,
 	CONSTRAINT [PK_tblMFAdditionalBasis] PRIMARY KEY (intAdditionalBasisId),
 	CONSTRAINT [UK_tblMFAdditionalBasis_dtmAdditionalBasisDate] UNIQUE (dtmAdditionalBasisDate)
 )
