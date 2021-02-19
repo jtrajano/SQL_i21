70c55a984065fab17c9745440f5d98919877e121
Premkumar S <prem.selladurai@irely.com>
2021-02-19 19:55:32 +0530

    MFG-4258

--- i21Database/dbo/Tables/tblMFAdditionalBasisImportError.sql
+++ i21Database/dbo/Tables/tblMFAdditionalBasisImportError.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE tblMFAdditionalBasisImportError (
 	intAdditionalBasisImportErrorId INT NOT NULL IDENTITY
 	,intAdditionalBasisImportId INT 
-	,intConcurrencyId INT NULL CONSTRAINT DF_tblMFAdditionalBasisImport_intConcurrencyId DEFAULT 0
+	,intConcurrencyId INT NULL CONSTRAINT DF_tblMFAdditionalBasisImportError_intConcurrencyId DEFAULT 0
 	,dtmAdditionalBasisDate DATETIME
 	,strComment NVARCHAR(MAX) COLLATE Latin1_General_CI_AS
 	,strItemNo NVARCHAR(50) COLLATE Latin1_General_CI_AS 
@@ -10,6 +10,6 @@
 	,strCurrency NVARCHAR(50) COLLATE Latin1_General_CI_AS 
 	,strUnitMeasure NVARCHAR(50) COLLATE Latin1_General_CI_AS 
 	,intCreatedUserId int
-	,dtmCreated DATETIME NULL CONSTRAINT DF_tblMFAdditionalBasisImport_dtmCreated DEFAULT GETDATE()
+	,dtmCreated DATETIME NULL CONSTRAINT DF_tblMFAdditionalBasisImportError_dtmCreated DEFAULT GETDATE()
 	,strErrorMessage NVARCHAR(MAX) COLLATE Latin1_General_CI_AS 
 	)
