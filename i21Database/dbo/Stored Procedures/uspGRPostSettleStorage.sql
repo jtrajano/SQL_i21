fe7ae97b3dca3f34997aca26e165be360a60f7fe
Leslie Guevarra <leslie.arat@irely.com>
2021-02-23 19:34:08 +0800

    GRN-2552 (CLONE - HDTN-256433 - Rebuild failed - STR out of balance)

--- i21Database/dbo/Stored Procedures/uspGRPostSettleStorage.sql
+++ i21Database/dbo/Stored Procedures/uspGRPostSettleStorage.sql
@@ -1566,6 +1566,7 @@ BEGIN TRY
 					INNER JOIN tblICItem I
 						ON I.intItemId = SV2.intItemId
 							AND I.ysnInventoryCost = 1
+							AND SV2.intItemType = 3
 				) DiscountCost				
 
 				INSERT INTO @ItemsToPost 
