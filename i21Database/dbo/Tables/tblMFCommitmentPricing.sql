528aede1f4d09d584d34e7b9f24966ab3f571351
Vinoth Kumar T <vinoth.t@irely.com>
2021-02-28 21:11:39 +0530

    MFG-4258

--- i21Database/dbo/Tables/tblMFCommitmentPricing.sql
+++ i21Database/dbo/Tables/tblMFCommitmentPricing.sql
@@ -9,8 +9,8 @@
 	intUnitMeasureId INT,
 	intCurrencyId INT,
 	dtmDate DATETIME NOT NULL,
-	dtmM2MBasisDate DATETIME,
-	dtmAdditionalBasisDate DATETIME,
+	intM2MBasisId INT,
+	intAdditionalBasisId INT,
 	strERPNo NVARCHAR(100) COLLATE Latin1_General_CI_AS,
 	dblBalanceQty NUMERIC(18, 6),
 	strComment NVARCHAR(MAX) COLLATE Latin1_General_CI_AS,
@@ -24,5 +24,7 @@
 	CONSTRAINT [PK_tblMFCommitmentPricing] PRIMARY KEY (intCommitmentPricingId),
 	CONSTRAINT [FK_tblMFCommitmentPricing_tblEMEntity] FOREIGN KEY (intEntityId) REFERENCES tblEMEntity(intEntityId), 
 	CONSTRAINT [FK_tblMFCommitmentPricing_tblICUnitMeasure] FOREIGN KEY (intUnitMeasureId) REFERENCES tblICUnitMeasure(intUnitMeasureId),
-	CONSTRAINT [FK_tblMFCommitmentPricing_tblSMCurrency] FOREIGN KEY (intCurrencyId) REFERENCES tblSMCurrency(intCurrencyID)
+	CONSTRAINT [FK_tblMFCommitmentPricing_tblSMCurrency] FOREIGN KEY (intCurrencyId) REFERENCES tblSMCurrency(intCurrencyID),
+	CONSTRAINT [FK_tblMFCommitmentPricing_tblRKM2MBasis] FOREIGN KEY (intM2MBasisId) REFERENCES tblRKM2MBasis(intM2MBasisId),
+	CONSTRAINT [FK_tblMFCommitmentPricing_tblMFAdditionalBasis] FOREIGN KEY (intAdditionalBasisId) REFERENCES tblMFAdditionalBasis(intAdditionalBasisId)
 )
---
+++ i21Database/dbo/Views/vyuMFGetCommitmentPricing.sql
@@ -0,0 +1,23 @@
+﻿CREATE VIEW vyuMFGetCommitmentPricing
+AS
+SELECT CP.intCommitmentPricingId
+	,CP.strPricingNumber
+	,C.strName
+	,C.strName AS strAliasName
+	,CP.dtmDeliveryFrom
+	,CP.dtmDeliveryTo
+	,UOM.strUnitMeasure
+	,CUR.strCurrency
+	,CP.dtmDate
+	,MB.dtmM2MBasisDate
+	,AB.dtmAdditionalBasisDate
+	,CP.strERPNo
+	,CP.dblBalanceQty
+	,CP.strComment
+	,CP.dblMarketArbitrage
+FROM tblMFCommitmentPricing CP
+LEFT JOIN tblEMEntity C on C.intEntityId = CP.intEntityId
+LEFT JOIN tblICUnitMeasure UOM ON UOM.intUnitMeasureId = CP.intUnitMeasureId
+LEFT JOIN tblSMCurrency CUR ON CUR.intCurrencyID = CP.intCurrencyId
+LEFT JOIN tblRKM2MBasis MB ON MB.intM2MBasisId = CP.intM2MBasisId
+LEFT JOIN tblMFAdditionalBasis AB ON AB.intAdditionalBasisId = CP.intAdditionalBasisId
