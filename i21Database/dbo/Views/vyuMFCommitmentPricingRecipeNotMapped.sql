737ff91e890eed17103de4185428a546808e35b0
Vinoth Kumar T <vinoth.t@irely.com>
2021-03-02 00:50:06 +0530

    MFG-4258

---
+++ i21Database/dbo/Views/vyuMFCommitmentPricingRecipeNotMapped.sql
@@ -0,0 +1,14 @@
+﻿CREATE VIEW vyuMFCommitmentPricingRecipeNotMapped
+AS
+SELECT CPR.intCommitmentPricingRecipeId
+	,VR.strName AS strVirtualOutputItemNo
+	,AR.strName AS strActualOutputItemNo
+	,VI.strItemNo AS strVirtualInputItemNo
+	,AI.strItemNo AS strActualInputItemNo
+FROM tblMFCommitmentPricingRecipe CPR
+LEFT JOIN tblMFRecipe VR ON VR.intRecipeId = CPR.intVirtualRecipeId
+LEFT JOIN tblMFRecipe AR ON AR.intRecipeId = CPR.intActualRecipeId
+LEFT JOIN tblMFRecipeItem VRI ON VRI.intRecipeItemId = CPR.intVirtualRecipeItemId
+LEFT JOIN tblMFRecipeItem ARI ON ARI.intRecipeItemId = CPR.intActualRecipeItemId
+LEFT JOIN tblICItem VI ON VI.intItemId = VRI.intItemId
+LEFT JOIN tblICItem AI ON AI.intItemId = ARI.intItemId
---
+++ i21Database/dbo/Views/vyuMFCommitmentPricingSalesNotMapped.sql
@@ -0,0 +1,14 @@
+﻿CREATE VIEW vyuMFCommitmentPricingSalesNotMapped
+AS
+SELECT CPS.intCommitmentPricingSalesId
+	,CH.strContractNumber
+	,CD.intContractSeq
+	,FM.strFutMarketName
+	,B.strBook
+	,SB.strSubBook
+FROM tblMFCommitmentPricingSales CPS
+JOIN tblCTContractDetail CD ON CD.intContractDetailId = CPS.intContractDetailId
+JOIN tblCTContractHeader CH ON CH.intContractHeaderId = CD.intContractHeaderId
+LEFT JOIN tblRKFutureMarket FM ON FM.intFutureMarketId = CPS.intFutureMarketId
+LEFT JOIN tblCTBook B ON B.intBookId = CPS.intBookId
+LEFT JOIN tblCTSubBook SB ON SB.intSubBookId = CPS.intSubBookId
