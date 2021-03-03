73c2cf36265f4eda9d72dfc8ace9d182d5bbe3d3
Arman Arenas <arman.arenas@irely.com>
2021-03-03 22:53:05 +0800

    LG-2841, LG-2685, LG-2845 revert shipped fields

--- i21Database/dbo/Views/vyuICLoadContainersSearch.sql
+++ i21Database/dbo/Views/vyuICLoadContainersSearch.sql
@@ -9,22 +9,14 @@ SELECT
 	, dblItemUOMCF = ItemUOM.dblUnitQty
 	, dblQuantity =
 		CASE
-			WHEN ISNULL(ContainerLink.dblQuantity,0) = 0 THEN 
-				CASE WHEN ([Load].intShipmentStatus = 4 AND [Load].ysnAllowReweighs = 1) 
-					THEN ISNULL(LoadDetail.dblShippedQuantity, LoadDetail.dblQuantity) 
-					ELSE LoadDetail.dblQuantity END
-			ELSE 
-				CASE WHEN ([Load].intShipmentStatus = 4 AND [Load].ysnAllowReweighs = 1) 
-					THEN ISNULL(LoadContainer.dblShippedQuantity, ContainerLink.dblQuantity) 
-					ELSE ContainerLink.dblQuantity END
+			WHEN ISNULL(ContainerLink.dblQuantity,0) = 0 THEN LoadDetail.dblQuantity 
+			ELSE ContainerLink.dblQuantity 
 		END
 	, intWeightUOMId = LoadDetail.intWeightItemUOMId
 	, dblContainerWeightPerQty = NULL
 	, dblFranchise = NULL
 	, strContainerNumber = LoadContainer.strContainerNumber
-	, dblNetWt = CASE WHEN ([Load].intShipmentStatus = 4 AND [Load].ysnAllowReweighs = 1) 
-					THEN ISNULL(LoadContainer.dblShippedNetWt, LoadContainer.dblNetWt) 
-					ELSE LoadContainer.dblNetWt END
+	, LoadContainer.dblNetWt
 FROM tblLGLoad [Load]
 	INNER JOIN tblLGLoadDetail LoadDetail ON [Load].intLoadId = LoadDetail.intLoadId
 	LEFT OUTER JOIN tblLGLoadDetailContainerLink ContainerLink ON ContainerLink.intLoadDetailId = LoadDetail.intLoadDetailId
