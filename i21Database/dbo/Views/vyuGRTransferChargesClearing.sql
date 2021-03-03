01bb06e57c25f1fc57b430bb9531f9ec524bae26
Leslie Guevarra <leslie.arat@irely.com>
2021-03-03 12:57:36 +0800

    GRN-2451 (Transfers from DP to OS Excluded from AP Clearing report)

--- i21Database/dbo/Views/vyuGRTransferChargesClearing.sql
+++ i21Database/dbo/Views/vyuGRTransferChargesClearing.sql
@@ -314,6 +314,76 @@ LEFT JOIN
 WHERE GL.strDescription LIKE '%Charges from %'
 --and TS.dtmTransferStorageDate between '2021-03-02' and '2021-03-03'
 AND (CASE WHEN GL.dblDebitUnit = 0 THEN GL.dblCreditUnit ELSE GL.dblDebitUnit END = SR.dblUnitQty)
+UNION ALL      
+--Transfer for Receipt Charge Taxes
+SELECT DISTINCT --'5',
+    CS.intEntityId AS intEntityVendorId
+    ,TS.dtmTransferStorageDate AS dtmDate      
+    ,TS.strTransferStorageTicket
+	,IR.intInventoryReceiptId
+    ,TS.intTransferStorageId
+    ,TS.strTransferStorageTicket
+    ,SR.intTransferStorageReferenceId
+    ,IRC.intInventoryReceiptChargeId      
+    ,IC.intItemId      
+    ,CS.intItemUOMId
+    ,unitMeasure.strUnitMeasure AS strUOM  
+    ,0 AS dblTransferTotal      
+    ,0 AS dblTransferQty  
+    ,ROUND((CASE WHEN GL.dblDebit <> 0 THEN GL.dblDebit ELSE GL.dblCredit END), 2) dblReceiptChargeTotal  
+    ,0 AS dblReceiptChargeQty 
+    ,CS.intCompanyLocationId      
+    ,CL.strLocationName      
+    ,0
+FROM vyuGLDetail GL
+INNER JOIN vyuGLAccountDetail APClearing
+    ON APClearing.intAccountId = GL.intAccountId 
+		AND APClearing.intAccountCategoryId = 45
+INNER JOIN tblGRTransferStorage TS
+	ON TS.intTransferStorageId = GL.intTransactionId
+		AND TS.strTransferStorageTicket = GL.strTransactionId
+INNER JOIN tblGRTransferStorageReference SR
+	ON SR.intTransferStorageId = TS.intTransferStorageId
+INNER JOIN tblGRCustomerStorage CS
+	ON CS.intCustomerStorageId = SR.intSourceCustomerStorageId
+INNER JOIN tblSMCompanyLocation CL      
+    ON CL.intCompanyLocationId = CS.intCompanyLocationId
+INNER JOIN (
+	SELECT CS.intCustomerStorageId
+		,intInventoryReceiptId
+	FROM tblGRStorageHistory SH
+	INNER JOIN tblGRTransferStorageReference TSR
+		ON TSR.intSourceCustomerStorageId = SH.intCustomerStorageId
+	INNER JOIN tblGRCustomerStorage CS
+		ON CS.intCustomerStorageId = TSR.intSourceCustomerStorageId
+			AND CS.ysnTransferStorage = 0
+			AND CS.intTicketId IS NOT NULL
+    INNER JOIN tblGRStorageType ST_FROM
+        ON ST_FROM.intStorageScheduleTypeId = CS.intStorageTypeId
+    INNER JOIN tblGRCustomerStorage CS_TO
+        ON CS_TO.intCustomerStorageId = TSR.intToCustomerStorageId
+    INNER JOIN tblGRStorageType ST_TO
+        ON ST_TO.intStorageScheduleTypeId = CS_TO.intStorageTypeId
+    WHERE ST_FROM.ysnDPOwnedType = 1 AND ST_TO.ysnDPOwnedType = 1
+) IR_SOURCE	
+	ON IR_SOURCE.intCustomerStorageId = CS.intCustomerStorageId
+INNER JOIN tblICInventoryReceipt IR
+	ON IR.intInventoryReceiptId = IR_SOURCE.intInventoryReceiptId
+INNER JOIN tblICInventoryReceiptCharge IRC
+	ON IRC.intInventoryReceiptId = IR.intInventoryReceiptId
+INNER JOIN tblICItem IC
+	ON IC.intItemId = IRC.intChargeId
+	AND GL.strDescription LIKE CONCAT('%', IC.strItemNo, '%')
+INNER JOIN tblICInventoryReceiptChargeTax IRCT
+	ON IRCT.intInventoryReceiptChargeId = IRC.intInventoryReceiptChargeId
+LEFT JOIN   
+(  
+    tblICItemUOM itemUOM INNER JOIN tblICUnitMeasure unitMeasure  
+        ON itemUOM.intUnitMeasureId = unitMeasure.intUnitMeasureId  
+)  
+    ON itemUOM.intItemUOMId = CS.intItemUOMId
+WHERE GL.strDescription NOT LIKE '%Charges from %'
+
 ) charges  
 OUTER APPLY (
 SELECT TOP 1 intAccountId, strAccountId FROM vyuAPReceiptClearingGL gl
