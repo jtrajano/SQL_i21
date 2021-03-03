88e0c3852deb47c75fd07692c99637d8e9239a97
Feb Montefrio <feb.montefrio@irely.com>
2021-03-03 14:21:41 +0800

    [F] IC-9005
    [B] IC-9280

---
+++ i21Database/dbo/Views/vyuICSearchBundleDetails.sql
@@ -0,0 +1,28 @@
+﻿CREATE VIEW vyuICSearchBundleDetails
+AS
+
+SELECT 
+	i.strItemNo
+	,i.strBundleType
+	,i.strShortName
+	,i.strDescription
+	,i.strStatus
+	,i.intCommodityId
+	,com.strCommodityCode
+	,i.intCategoryId
+	,cat.strCategoryCode
+	,i.intBrandId
+	,b.strBrandCode
+	,i.intManufacturerId
+	,m.strManufacturer
+	,i.ysnListBundleSeparately
+FROM 
+	tblICItem i 
+	LEFT JOIN tblICCommodity com
+		ON i.intCommodityId = com.intCommodityId
+	LEFT JOIN tblICCategory cat
+		ON cat.intCategoryId = i.intCategoryId
+	LEFT JOIN tblICBrand b
+		ON b.intBrandId = i.intBrandId
+	LEFT JOIN tblICManufacturer m
+		ON m.intManufacturerId = i.intManufacturerId
