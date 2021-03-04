52feee3ba629a30e2518ac7dba57e5bfde62f3b9
Jopet Lambinicio <jopet.lambinicio@irely.com>
2021-03-04 18:35:52 +0800

    AP-9486

--- i21Database/dbo/Stored Procedures/uspAPRptOpenClearingSummary.sql
+++ i21Database/dbo/Stored Procedures/uspAPRptOpenClearingSummary.sql
@@ -10,6 +10,7 @@ SET ANSI_WARNINGS OFF
 
 DECLARE @query NVARCHAR(MAX) = ''
 DECLARE @filter NVARCHAR(MAX) = ''
+DECLARE @filterPerItem NVARCHAR(MAX) = ''
 
 DECLARE @fieldname NVARCHAR(50)
 DECLARE @condition NVARCHAR(20)     
@@ -24,12 +25,12 @@ DECLARE @datatype NVARCHAR(50)
 DECLARE @dateFrom DATETIME
 DECLARE @dateTo DATETIME
 DECLARE @dateCondition NVARCHAR(50)
-DECLARE @filterCount INT = 0;
+DECLARE @filterCount INT = 0
 
 --SANITIZE AND CHECK IF @xmlParam IS EMPTY 
 IF LTRIM(RTRIM(@xmlParam)) = '' 
 BEGIN
-  --ADD THIS TO INITIALIZE XtraReports FIELD
+  	--ADD THIS TO INITIALIZE XtraReports FIELD
 	SELECT 
 		0 AS dbl1,
 		0 AS dbl30,
@@ -44,7 +45,7 @@ BEGIN
 		NULL AS dtmEndDate
 END
 
-DECLARE @xmlDocumentId AS INT;	
+DECLARE @xmlDocumentId AS INT
 --CREATE TABLE TO HOLD XML DATA FILTERS
 DECLARE @temp_xml_table TABLE (
 	id INT IDENTITY(1,1)
@@ -85,8 +86,27 @@ BEGIN
 	SET @dateTo = GETDATE()
 END
 
---FILTERS
-DELETE FROM @temp_xml_table  where [condition] = 'Dummy'
+--DELETE DUMMY FILTERS
+DELETE FROM @temp_xml_table WHERE [condition] = 'Dummy'
+
+--CREATE PER ITEM FILTERS
+WHILE EXISTS(SELECT 1 FROM @temp_xml_table WHERE [fieldname] IN ('dblClearingQuantity', 'dblClearingAmount'))
+BEGIN
+	SELECT @id = id, @fieldname = [fieldname], @condition = [condition], @from = [from], @to = [to], @join = [join], @datatype = [datatype] FROM @temp_xml_table WHERE [fieldname] IN ('dblClearingQuantity', 'dblClearingAmount')
+	SET @filterPerItem = @filterPerItem + ' ' + dbo.fnAPCreateFilter(@fieldname, @condition, @from, @to, @join, null, null, @datatype)
+	DELETE FROM @temp_xml_table WHERE id = @id
+	IF EXISTS(SELECT 1 FROM @temp_xml_table WHERE [fieldname] IN ('dblClearingQuantity', 'dblClearingAmount'))
+	BEGIN
+		SET @filterPerItem = @filterPerItem + ' AND '
+	END
+END
+
+IF NULLIF(@filterPerItem, '') IS NOT NULL
+BEGIN
+	SET @filterPerItem = ' WHERE ' + @filterPerItem
+END
+
+--CREATE FILTERS
 WHILE EXISTS(SELECT 1 FROM @temp_xml_table)
 BEGIN
 	SELECT @id = id, @fieldname = [fieldname], @condition = [condition], @from = [from], @to = [to], @join = [join], @datatype = [datatype] FROM @temp_xml_table
@@ -102,65 +122,110 @@ END
 SET @query = '
 	SELECT * FROM (
 		SELECT
-			CASE WHEN DATEDIFF(dayofyear, C.dtmDate, GETDATE()) >= 0 AND DATEDIFF(dayofyear, C.dtmDate, GETDATE()) <= 30 
-				THEN C.dblAmount
-				ELSE 0 
-			END AS dbl1,
-			CASE WHEN DATEDIFF(dayofyear, C.dtmDate, GETDATE()) > 30 AND DATEDIFF(dayofyear, C.dtmDate, GETDATE()) <= 60
-				THEN C.dblAmount
-				ELSE 0 
-			END AS dbl30, 
-			CASE WHEN DATEDIFF(dayofyear, C.dtmDate, GETDATE()) > 60 AND DATEDIFF(dayofyear, C.dtmDate, GETDATE()) <= 90 
-				THEN C.dblAmount
-				ELSE 0 
-			END AS dbl60,
-			CASE WHEN DATEDIFF(dayofyear, C.dtmDate, GETDATE()) > 90
-				THEN C.dblAmount
-				ELSE 0
-			END AS dbl90,
-			C.dblAmount AS dblTotal,
+			C.intTransactionType,
+			C.intTransactionDetailId,
 			dbo.fnTrim(ISNULL(V.strVendorId, E.strEntityNo) + '' - '' + ISNULL(E.strName, '''')) AS strVendorIdName,
+			C.strTransactionId,
 			C.dtmDate,
+			CL.strLocationName,
+			C.strReferenceNumber,
+			CASE WHEN C.intOffsetId > 0 THEN 0 ELSE C.dblQuantity END AS dblQuantity,
+			CASE WHEN C.intOffsetId > 0 THEN 0 ELSE C.dblAmount END AS dblAmount,
+			CASE WHEN C.intOffsetId > 0 THEN C.dblQuantity * -1 ELSE 0 END AS dblOffsetQuantity,
+			CASE WHEN C.intOffsetId > 0 THEN C.dblAmount * -1 ELSE 0 END AS dblOffsetAmount,
 			A.strAccountId,
 			FP.strPeriod
 		FROM tblAPClearing C
-		INNER JOIN (tblAPVendor V INNER JOIN tblEMEntity E ON V.intEntityId = E.intEntityId) ON C.intEntityVendorId = V.intEntityId  
+		INNER JOIN (tblAPVendor V INNER JOIN tblEMEntity E ON V.intEntityId = E.intEntityId) ON C.intEntityVendorId = V.intEntityId
+		INNER JOIN tblSMCompanyLocation CL ON CL.intCompanyLocationId = C.intLocationId
 		INNER JOIN tblGLAccount A ON A.intAccountId = C.intAccountId
 		LEFT JOIN tblGLFiscalYearPeriod FP ON C.dtmDate BETWEEN FP.dtmStartDate AND FP.dtmEndDate OR C.dtmDate = FP.dtmStartDate OR C.dtmDate = FP.dtmEndDate
 	) rawClearing
 '
 
 --CONCATENATE FILTER TO THE RAW QUERY
-IF NULLIF(@filter,'') IS NOT NULL
+IF NULLIF(@filter, '') IS NOT NULL
 BEGIN
 	SET @query = @query + ' WHERE ' + @filter
 END
 
---GROUP BY QUERY AND HEADER INFORMATIONS
+--PER ITEM CLEARING AND CONCATENATE ITEM FILTER
 SET @query = '
 	SELECT 
-		groupedClearing.*,
+		*
+	FROM (
+		SELECT
+			intTransactionType,
+			strVendorIdName,
+			strTransactionId,
+			dtmDate = MIN(dtmDate),
+			strLocationName,
+			strReferenceNumber,
+			SUM(dblQuantity) AS dblQuantity,
+			SUM(dblAmount) AS dblAmount,
+			SUM(dblOffsetQuantity) AS dblOffsetQuantity,
+			SUM(dblOffsetAmount) AS dblOffsetAmount,
+			SUM(dblQuantity - dblOffsetQuantity) AS dblClearingQuantity,
+			SUM(dblAmount - dblOffsetAmount) AS dblClearingAmount
+		FROM (
+			'+ @query +'
+		) filteredClearing
+		'+ @filterPerItem +'
+		GROUP BY intTransactionType, intTransactionDetailId, strVendorIdName, strTransactionId, strLocationName, strReferenceNumber
+	) detailedClearing
+	WHERE detailedClearing.dblClearingAmount <> 0
+'
+
+--GROUP BY VENDOR
+SET @query = '
+	SELECT
+		strVendorIdName,
+		SUM(dbl1) AS dbl1,
+		SUM(dbl30) AS dbl30,
+		SUM(dbl60) AS dbl60,
+		SUM(dbl90) AS dbl90,
+		SUM(dblTotal) AS dblTotal
+	FROM (
+		SELECT
+			strVendorIdName,
+			CASE WHEN DATEDIFF(dayofyear, dtmDate, GETDATE()) >= 0 AND DATEDIFF(dayofyear, dtmDate, GETDATE()) <= 30 
+				THEN dblClearingAmount
+				ELSE 0 
+			END AS dbl1,
+			CASE WHEN DATEDIFF(dayofyear, dtmDate, GETDATE()) > 30 AND DATEDIFF(dayofyear, dtmDate, GETDATE()) <= 60
+				THEN dblClearingAmount
+				ELSE 0 
+			END AS dbl30, 
+			CASE WHEN DATEDIFF(dayofyear, dtmDate, GETDATE()) > 60 AND DATEDIFF(dayofyear, dtmDate, GETDATE()) <= 90 
+				THEN dblClearingAmount
+				ELSE 0 
+			END AS dbl60,
+			CASE WHEN DATEDIFF(dayofyear, dtmDate, GETDATE()) > 90
+				THEN dblClearingAmount
+				ELSE 0
+			END AS dbl90,
+			dblClearingAmount AS dblTotal
+		FROM (
+			'+ @query +'
+		) agingClearing
+	) groupedClearing
+	GROUP BY strVendorIdName
+'
+
+--FINALIZE AND ADD HEADER INFORMATIONS
+SET @query = '
+	SELECT
+		finalClearing.*,
 		GETDATE() AS dtmCurrentDate,
 		dbo.fnAPFormatAddress(NULL, NULL, NULL, CS.strAddress, CS.strCity, CS.strState, CS.strZip, CS.strCountry, NULL) AS strCompanyAddress,
 		CS.strCompanyName,
 		dtmStartDate = '''+ CASE WHEN @filterCount > 0 THEN '0' ELSE CONVERT(NVARCHAR(10), ISNULL(@dateFrom, '1/1/1900'), 101) END +''',
 		dtmEndDate = '''+ CASE WHEN @filterCount > 0 THEN '0' ELSE CONVERT(NVARCHAR(10), CASE WHEN @dateCondition = 'Equal To' THEN @dateFrom ELSE ISNULL(@dateTo, GETDATE()) END, 101) END +'''
 	FROM (
-		SELECT
-		SUM(dbl1) AS dbl1,
-		SUM(dbl30) AS dbl30,
-		SUM(dbl60) AS dbl60,
-		SUM(dbl90) AS dbl90,
-		SUM(dblTotal) AS dblTotal,
-		strVendorIdName
-		FROM (
-			'+ @query +'
-		) filteredClearing
-		GROUP BY strVendorIdName
-	) groupedClearing
+		'+ @query +'
+	) finalClearing
 	CROSS APPLY tblSMCompanySetup CS
-	WHERE groupedClearing.dblTotal <> 0
-	ORDER BY groupedClearing.strVendorIdName
+	ORDER BY finalClearing.strVendorIdName
 '
 
 --EXECUTE QUERY
