b6733c66b9dc8d227578a2a7d13746e8756af2ef
Wendell Wayne Estrada <wendell.estrada@irely.com>
2021-03-04 20:42:58 +0800

    [F] IN-764 Barchart - Add Fields to Contracts endpoint

--- i21Database/dbo/Views/vyuCTRestApiContractHeader.sql
+++ i21Database/dbo/Views/vyuCTRestApiContractHeader.sql
@@ -5,6 +5,9 @@ SELECT ch.*
 	, pt.strPricingType
 	, tp.strContractType
 	, dbo.fnCTGetContractStatuses(ch.intContractHeaderId) COLLATE Latin1_General_CI_AS AS strStatuses
+	, c.strCommodityCode
+	, c.strDescription strCommodityDescription
 FROM tblCTContractHeader ch
 LEFT JOIN tblCTPricingType pt ON pt.intPricingTypeId = ch.intPricingTypeId
 LEFT JOIN tblCTContractType tp ON tp.intContractTypeId = ch.intContractTypeId
+LEFT JOIN tblICCommodity c ON c.intCommodityId = ch.intCommodityId
