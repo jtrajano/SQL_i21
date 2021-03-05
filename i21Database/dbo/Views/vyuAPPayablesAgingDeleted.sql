cd010587a52ddc916ada66a1d654284707e69e90
Erick Dela Cruz <erick.delacruz@irely.com>
2021-03-05 17:04:09 +0800

    AP-9322

--- i21Database/dbo/Views/vyuAPPayablesAgingDeleted.sql
+++ i21Database/dbo/Views/vyuAPPayablesAgingDeleted.sql
@@ -104,18 +104,17 @@ SELECT  A.dtmDatePaid AS dtmDate,
 	, C.dtmDueDate 
 	, C.ysnPosted 
 	, C.ysnPaid
-	, B.intAccountId
+	, C.intAccountId
 	, F.strAccountId
 	, EC.strClass
 	, 3 AS intCount
 	-- ,'Payment' AS [Info]
 FROM dbo.tblAPPayment  A
- INNER JOIN dbo.tblAPPaymentDetail B ON A.intPaymentId = B.intPaymentId
- INNER JOIN dbo.tblAPBillArchive C ON ISNULL(B.intBillId,B.intOrigBillId) = C.intBillId
- LEFT JOIN dbo.fnAPGetVoucherAverageRate() avgRate ON C.intBillId = avgRate.intBillId --handled payment for origin old payment import
- LEFT JOIN (dbo.tblAPVendor D INNER JOIN dbo.tblEMEntity D2 ON D.[intEntityId] = D2.intEntityId)
- 	ON A.[intEntityVendorId] = D.[intEntityId]
-LEFT JOIN dbo.tblGLAccount F ON  B.intAccountId = F.intAccountId		
+LEFT JOIN dbo.tblAPPaymentDetail B ON A.intPaymentId = B.intPaymentId
+LEFT JOIN dbo.tblAPBillArchive C ON ISNULL(B.intBillId,B.intOrigBillId) = C.intBillId
+LEFT JOIN (dbo.tblAPVendor D INNER JOIN dbo.tblEMEntity D2 ON D.[intEntityId] = D2.intEntityId)
+	ON A.[intEntityVendorId] = D.[intEntityId]
+LEFT JOIN dbo.tblGLAccount F ON  C.intAccountId = F.intAccountId		
 LEFT JOIN dbo.tblCMBankTransaction E
 	ON A.strPaymentRecordNum = E.strTransactionId
 LEFT JOIN dbo.tblEMEntityClass EC ON EC.intEntityClassId = D2.intEntityClassId		
