c3bb6ccc1ebcd97fed162305e0454bf145afe868
Christian Joel Martinez <Christian.Martinez@iRely.com>
2021-03-08 13:45:22 +0800

    Revert "GRN-2448 - Transfer storage AP Clearing issues."
    This reverts commit d5813f7aa39160bf28b0a89f0d9587622af6531e.

--- i21Database/dbo/Views/vyuGRTransferClearing.sql
+++ i21Database/dbo/Views/vyuGRTransferClearing.sql
@@ -422,7 +422,7 @@ INNER JOIN tblGRTransferStorage TS
 INNER JOIN tblSMCompanyLocation CL
     ON CL.intCompanyLocationId = CS.intCompanyLocationId
 join tblAPBillDetail BillDetail
-	on BillDetail.intCustomerStorageId =  CS.intCustomerStorageId
+	on BillDetail.intCustomerStorageId =  BillDetail.intCustomerStorageId
 		and BillDetail.intItemId = TS.intItemId
 join tblAPBill Bill
     on Bill.intBillId = BillDetail.intBillId
