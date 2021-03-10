317c2656e0f1ef14228a3db6395e7c89433ecdde
Premkumar S <prem.selladurai@irely.com>
2021-03-10 22:05:23 +0530

    IN-770

--- i21Database/dbo/Views/vyuCTContractApproverView.sql
+++ i21Database/dbo/Views/vyuCTContractApproverView.sql
@@ -1,14 +1,28 @@
 ﻿CREATE VIEW [dbo].[vyuCTContractApproverView]
 AS
-SELECT Distinct A.intTransactionId AS intContractHeaderId
-	,E.strName
-	,US.strUserName AS strUserName
-	,S.strScreenName
-	,B.strTransactionNo As strContractNumber
-FROM tblSMApproval A
-JOIN tblSMTransaction B ON A.intTransactionId = B.intTransactionId
-JOIN tblSMScreen S ON S.intScreenId = A.intScreenId
-JOIN tblEMEntity E ON E.intEntityId = A.intApproverId
-JOIN tblSMUserSecurity US on US.intEntityId =E.intEntityId 
-WHERE A.strStatus = 'Approved'
-	AND S.strNamespace = 'ContractManagement.view.Contract'
+SELECT intContractHeaderId
+	,strName
+	,strUserName
+	,strScreenName
+	,strContractNumber
+FROM (
+	SELECT A.intTransactionId AS intContractHeaderId
+		,E.strName
+		,US.strUserName AS strUserName
+		,S.strScreenName
+		,B.strTransactionNo AS strContractNumber
+		,Rank() OVER (
+			PARTITION BY A.intTransactionId ORDER BY intApprovalId DESC
+			) intRowId
+	FROM tblSMApproval A
+	JOIN tblSMTransaction B ON A.intTransactionId = B.intTransactionId
+	JOIN tblSMScreen S ON S.intScreenId = A.intScreenId
+	JOIN tblEMEntity E ON E.intEntityId = A.intApproverId
+	JOIN tblSMUserSecurity US ON US.intEntityId = E.intEntityId
+	WHERE A.strStatus = 'Approved'
+		AND S.strNamespace IN (
+			'ContractManagement.view.Contract'
+			,'ContractManagement.view.Amendments'
+			)
+	) AS DT
+WHERE DT.intRowId = 1
