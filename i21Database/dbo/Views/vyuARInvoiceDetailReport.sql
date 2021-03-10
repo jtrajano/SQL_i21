4f24049f0b772926bbdb49b05eb7edd1b5d88ec1
Jeremiah Villanueva <jeremiah.villanueva@irely.com>
2021-03-10 15:25:12 +0800

    AR-12989

--- i21Database/dbo/Views/vyuARInvoiceDetailReport.sql
+++ i21Database/dbo/Views/vyuARInvoiceDetailReport.sql
@@ -22,7 +22,7 @@ SELECT intInvoiceId				= I.intInvoiceId
 	 , strComments				= I.strComments
 	 , dblQtyShipped			= CASE WHEN (I.strTransactionType  IN ('Invoice', 'Debit Memo', 'Cash', 'Proforma Invoice')) THEN ISNULL(ID.dblQtyShipped, 0) ELSE ISNULL(ID.dblQtyShipped, 0) * -1 END
 	 , strUnitMeasure 			= ID.[strUnitMeasure]
-	 , dblItemWeight			= CASE WHEN (I.strTransactionType  IN ('Invoice', 'Debit Memo', 'Cash', 'Proforma Invoice')) THEN ISNULL(ID.dblItemWeight, 0) ELSE ISNULL(ID.dblItemWeight, 0) * -1 END
+	 , dblItemWeight			= CASE WHEN (I.strTransactionType  IN ('Invoice', 'Debit Memo', 'Cash', 'Proforma Invoice')) THEN ISNULL(ID.dblShipmentNetWt, 0) ELSE ISNULL(ID.dblShipmentNetWt, 0) * -1 END
 	 , dblUnitCost				= CASE WHEN CT.intContractHeaderId IS NOT NULL THEN ISNUlL(ID.dblUnitPrice,0) ELSE ISNULL( ID.dblPrice,0) END
 	 , dblCostPerUOM			= ISNULL(ID.dblPrice, 0)
 	 , dblUnitCostCurrency		= ISNULL(ID.dblPrice, 0)
@@ -59,6 +59,7 @@ INNER JOIN (
 		 , strBinNumber
 		 , strGroupNumber
 		 , strFeedDiet
+		 , dblShipmentNetWt
 	FROM dbo.tblARInvoiceDetail ID WITH (NOLOCK)
 	LEFT JOIN (
 		SELECT intCurrencyID
