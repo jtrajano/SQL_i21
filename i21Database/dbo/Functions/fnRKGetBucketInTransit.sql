51672c65e32b15d013c6827088203a5ecf41d43a
lawrence_zabala <lawrence.zabala@irely.com>
2021-03-11 15:08:06 +0800

    RM-4149
    (cherry picked from commit 1c6555772c594c0acd7fd9cca7e79af1771322e9)

--- i21Database/dbo/Functions/fnRKGetBucketInTransit.sql
+++ i21Database/dbo/Functions/fnRKGetBucketInTransit.sql
@@ -111,7 +111,7 @@ BEGIN
 			, strCurrency
 		FROM vyuRKGetSummaryLog c
 		WHERE strBucketType IN ('Sales In-Transit', 'Purchase In-Transit') 
-			AND CONVERT(DATETIME, CONVERT(VARCHAR(10), c.dtmCreatedDate, 110), 110) <= CONVERT(DATETIME, @dtmDate)
+			--AND CONVERT(DATETIME, CONVERT(VARCHAR(10), c.dtmCreatedDate, 110), 110) <= CONVERT(DATETIME, @dtmDate)
 			AND CONVERT(DATETIME, CONVERT(VARCHAR(10), c.dtmTransactionDate, 110), 110) <= CONVERT(DATETIME, @dtmDate)
 			AND ISNULL(c.intCommodityId,0) = ISNULL(@intCommodityId, ISNULL(c.intCommodityId, 0)) 
 			AND ISNULL(intEntityId, 0) = ISNULL(@intVendorId, ISNULL(intEntityId, 0))
