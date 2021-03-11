d5bb750ecb304596040b9e935cafe6b0c1b306d2
Jopet Lambinicio <jopet.lambinicio@irely.com>
2021-03-11 18:35:41 +0800

    Revert "AP-7464"
    This reverts commit 3380860c8bca2a829984a737802c30485154f04e.

--- i21Database/dbo/Tables/tblAPClearing.sql
+++ i21Database/dbo/Tables/tblAPClearing.sql
@@ -17,10 +17,10 @@
 	[dblQuantity]				NUMERIC(18, 6) DEFAULT 0 NOT NULL,
 	[dblAmount]					NUMERIC(18, 6) DEFAULT 0 NOT NULL,
 	--VOUCHER
-	[intOffsetId]				INT NULL,
-	[strOffsetId]				NVARCHAR(55) COLLATE Latin1_General_CI_AS NULL,
-	[intOffsetDetailId]			INT NULL,
-	[intOffsetDetailTaxId]		INT NULL,
+	[intBillId]					INT NULL,
+	[strBillId]					NVARCHAR(55) COLLATE Latin1_General_CI_AS NULL,
+	[intBillDetailId]			INT NULL,
+	[intBillDetailTaxId]		INT NULL,
 	--OTHER INFORMATION
 	[strCode]					NVARCHAR(55) COLLATE Latin1_General_CI_AS NOT NULL,
 	[ysnPostAction]				BIT DEFAULT 1 NOT NULL,
