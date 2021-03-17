699fd3a61ac29466d2412fabcccd9383bcce9ffc
Roudinee Panteria <roudinee.panteria@irely.com>
2021-03-17 10:20:21 +0800

    AR-12984

--- i21Database/dbo/Tables/tblARInventoryReceipt.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[tblARInventoryReceipt]
-(
-	[intInventoryReceiptId] [int] IDENTITY(1,1) NOT NULL,
-	[intInvoiceId] int NOT NULL,
-	[strInvoiceNumber] [nvarchar](15) NOT NULL,
-	[strReceiptNumber] [nvarchar](15) NOT NULL,
-	[ysnDeleted] [bit] NOT NULL default 0, 
-    CONSTRAINT [PK_tblARInventoryReceipt] PRIMARY KEY ([intInventoryReceiptId])
-)
