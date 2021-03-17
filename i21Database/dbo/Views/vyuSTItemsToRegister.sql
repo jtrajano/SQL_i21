5959f1cacb07634bc4eb974928062fd791b17db2
nelsonreyes19 <nelson.reyes@irely.com>
2021-03-17 08:09:41 +0800

    Revert "ST-1737"
    This reverts commit 735d3509b740b8df03d99d92331cdc0143138a28.

--- i21Database/dbo/Views/vyuSTItemsToRegister.sql
+++ i21Database/dbo/Views/vyuSTItemsToRegister.sql
@@ -9,7 +9,6 @@ AS
 -- 4. Register (based on Store)
 -- 5. Pricing
 -- 6. Special Pricing
--- 7. Product Code - As required in C-Store defined in ST-1737
 
 SELECT DISTINCT 
     x.dtmDateModified
@@ -24,8 +23,6 @@ JOIN tblICCategory Cat
 	ON Cat.intCategoryId = I.intCategoryId
 JOIN tblICItemLocation IL 
 	ON IL.intItemId = I.intItemId
-JOIN tblSTSubcategoryRegProd sr
-	ON IL.intProductCodeId = sr.intRegProdId
 LEFT JOIN tblSTSubcategoryRegProd SubCat 
 	ON SubCat.intRegProdId = IL.intProductCodeId
 JOIN tblSTStore ST 
