9bf29c75695660601660a71ec38692c849b409d6
Feb Montefrio <feb.montefrio@irely.com>
2021-03-19 10:31:56 +0800

    [B] IC-9286

---
+++ i21Database/dbo/Stored Procedures/uspICLogCategoryChange.sql
@@ -0,0 +1,31 @@
+﻿CREATE PROCEDURE [dbo].[uspICLogCategoryChange]
+	@intItemId AS INT
+	,@intCategoryId AS INT 
+	,@intEntitySecurityUserId AS INT 
+AS
+
+SET QUOTED_IDENTIFIER OFF
+SET ANSI_NULLS ON
+SET NOCOUNT ON
+SET XACT_ABORT ON
+SET ANSI_WARNINGS OFF
+
+INSERT INTO tblICItemCategoryChangeLog (
+	[intItemId]
+	,[intOriginalCategoryId]
+	,[intNewCategoryId]
+	,[dtmDateChanged]
+	,[intCreatedByUserId]
+)
+SELECT 
+	[intItemId]
+	,[intOriginalCategoryId] = i.intCategoryId
+	,[intNewCategoryId] = @intCategoryId
+	,[dtmDateChanged] = GETDATE() 
+	,[intCreatedByUserId] = @intEntitySecurityUserId
+FROM 
+	tblICItem i 
+WHERE
+	i.intItemId = @intItemId
+	AND ISNULL(i.intCategoryId, 0) <> ISNULL(@intCategoryId, 0)
+	
