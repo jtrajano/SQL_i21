f293ee2af78f95cadff9f4cf4eb794aa3389cfc1
Feb Montefrio <feb.montefrio@irely.com>
2021-03-19 10:31:56 +0800

    [B] IC-9286
    # Conflicts:
    #	i21Database/i21Database.sqlproj
    
    # Conflicts:
    #	i21Database/dbo/Stored Procedures/uspICRebuildInventoryValuation.sql
    #	i21Database/i21Database.sqlproj
    
    # Conflicts:
    #	i21Database/i21Database.sqlproj

---
+++ i21Database/Scripts/Post-Deployment/IC/Data_Fix_For_19.1/02_PopulateCategoryChangeLog.sql
@@ -0,0 +1,41 @@
+﻿PRINT N'BEGIN - IC Data Fix for 19.1. Populate the Item Category Change Log'
+GO
+
+--IF	EXISTS (SELECT 1 FROM (SELECT TOP 1 dblVersion = CAST(LEFT(strVersionNo, 4) AS NUMERIC(18,1)) FROM tblSMBuildNumber ORDER BY intVersionID DESC) v WHERE v.dblVersion <= 19.1)
+--	AND NOT EXISTS (SELECT TOP 1 1 FROM tblICItemStockUsagePerPeriod)
+IF NOT EXISTS (SELECT TOP 1 1 FROM tblICItemCategoryChangeLog)
+BEGIN 
+	INSERT INTO tblICItemCategoryChangeLog (
+		intItemId
+		,intOriginalCategoryId
+		,intNewCategoryId
+		,dtmDateChanged
+		,intCreatedByUserId	
+	)
+	SELECT 
+		intItemId = i.intItemId 
+		,intOriginalCategoryId = t.intCategoryId
+		,intNewCategoryId = i.intCategoryId 
+		,dtmDateChanged = t.dtmDate
+		,intCreatedByUserId	= NULL 
+	FROM
+		tblICItem i 
+		OUTER APPLY (
+			SELECT TOP 1 
+				t.intCategoryId
+				,t.dtmDate
+				
+			FROM 
+				tblICInventoryTransaction t
+			WHERE
+				t.intItemId = i.intItemId
+				AND t.intCategoryId <> i.intCategoryId
+			ORDER BY
+				t.dtmDate DESC
+		) t
+	WHERE	
+		t.intCategoryId IS NOT NULL 
+END 
+GO
+
+PRINT N'END - IC Data Fix for 19.1. Populate the Item Category Change Log'
