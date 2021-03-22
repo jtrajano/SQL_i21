e76a8cea15c5114ca4e42ad99fcfc74cbb861ef0
Premkumar S <prem.selladurai@irely.com>
2021-03-22 18:19:03 +0530

    IN-795

---
+++ i21Database/dbo/Stored Procedures/uspIPGenerateFeedXML_ERP.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROCEDURE uspIPGenerateFeedXML_ERP @strCompanyLocation NVARCHAR(6)
+	,@strType NVARCHAR(50)
+AS
+BEGIN TRY
+	DECLARE @ErrMsg NVARCHAR(MAX)
+
+	IF @strType = 'Production Order'
+	BEGIN
+		EXEC dbo.uspMFGenerateERPProductionOrder @strCompanyLocation = @strCompanyLocation
+	END
+END TRY
+
+BEGIN CATCH
+	SET @ErrMsg = ERROR_MESSAGE()
+
+	RAISERROR (
+			@ErrMsg
+			,16
+			,1
+			,'WITH NOWAIT'
+			)
+END CATCH
