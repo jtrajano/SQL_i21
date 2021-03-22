0d8c94e40037396fe0a248ab060bfab0d46ee42b
jorey <jorey@XPS-JOREY>
2021-03-22 21:24:18 +0800

    details validation, next is to catch incoming from header

--- i21Database/dbo/Stored Procedures/uspCTValidateContractBeforeSave.sql
+++ i21Database/dbo/Stored Procedures/uspCTValidateContractBeforeSave.sql
@@ -1,5 +1,7 @@
 ﻿CREATE PROCEDURE [dbo].[uspCTValidateContractBeforeSave]
 	@ysnLoad bit
+	,@ysnUnlimitedQuantity bit
+	,@userId int
 	,@strXML NVARCHAR(MAX)
 	,@error NVARCHAR(1000) = NULL OUTPUT
 AS
@@ -15,7 +17,24 @@ BEGIN TRY
 	,@dblQuantityPerLoad numeric(18,10)
 	,@dblQuantity numeric(18,10)
 	,@dblBalance numeric(18,10)
+	,@strContractStatus numeric(18,10)
 	,@dblBalanceLoad numeric(18,10)
+	,@RowState varchar(50)
+	,@dtmEndDate datetime
+	,@intLastModifiedById int
+	,@dtmLastModified datetime
+	,@intCreatedById int
+	,@dtmCreated datetime
+
+	DECLARE 
+		@_intNoOfLoad int,
+		@_dblQuantity numeric(18,10),
+		@_dblScheduleQty numeric(18,10),
+		@_dblBalanceLoad numeric(18,10),
+		@_dblScheduleLoad numeric(18,10),
+		@_dblBalance numeric(18,10),
+		@_intContractStatusId int,
+		@_intPricingTypeId int
 
 	EXEC @PrepareXmlStatus = sp_xml_preparedocument @idoc OUTPUT, @strXML  
 
@@ -31,6 +50,8 @@ BEGIN TRY
 		[intContractDetailId] int
 		,[intContractHeaderId] int
 		,[strRowState] varchar(50)
+		,[RowState] varchar(50)
+		,[dtmEndDate] datetime
 		,[intNoOfLoad] int
 		,[dblQuantityPerLoad] numeric(18,10)
 		,[dblQuantity] numeric(18,10)
@@ -50,6 +71,11 @@ BEGIN TRY
 				,@dblQuantityPerLoad = dblQuantityPerLoad
 				,@dblQuantity =	dblQuantity
 				,@strRowState =	strRowState
+				,@dblBalance = dblBalance
+				--,@strContractStatus = strContractStatus
+				,@RowState = RowState
+				,@dtmEndDate = dtmEndDate
+				--,@intCreatedById = intCreatedById
 		FROM	#tblContractDetails 
 		WHERE	intUniqueId = @intUniqueId
 		
@@ -58,59 +84,143 @@ BEGIN TRY
 		BEGIN
 
 			UPDATE #tblContractDetails
-			SET dblBalance = IIF(@ysnLoad = 1, @intNoOfLoad * @dblQuantityPerLoad, @dblQuantity)
-			,dblBalanceLoad = IIF(@ysnLoad = 1, @intNoOfLoad, dblBalanceLoad)
+			SET dblBalance = CASE WHEN @ysnLoad = 1 THEN @intNoOfLoad * @dblQuantityPerLoad ELSE @dblQuantity END
+			,dblBalanceLoad = CASE WHEN @ysnLoad = 1 THEN @intNoOfLoad ELSE dblBalanceLoad END
 			WHERE intUniqueId = @intUniqueId
 
 		END
 		ELSE IF(@strRowState = 'Modified')
 		BEGIN
-			
-			IF OBJECT_ID('tempdb..#tblContractDetailsOld') IS NOT NULL  	
-			DROP TABLE #tblContractDetailsOld	
-			
-			
-            --_intNoOfLoad = detail.intNoOfLoad.GetValueOrDefault();
-            --_dblQuantity = detail.dblQuantity;
-            --_dblBalance = detail.dblBalance.GetValueOrDefault();
-            --_dblScheduleQty = detail.dblScheduleQty.GetValueOrDefault();
-            --_dblBalanceLoad = detail.dblBalanceLoad.GetValueOrDefault();
-            --_dblScheduleLoad = detail.dblScheduleLoad.GetValueOrDefault();
 
-			DECLARE 
-				@_intNoOfLoad int,
-				@_dblQuantity int,
-				@_dblBalance int,
-				@_dblScheduleQty int,
-				@_dblBalanceLoad int,
-				@_dblScheduleLoad int
-			
-			--SELECT	@intContractDetailIdOld = intContractDetailId
-			--		,@intNoOfLoadOld = [intNoOfLoad]
-			--		,@dblQuantityOld =	dblQuantity
-			--		,@dblQuantityPerLoadOld = dblQuantityPerLoad
-			--INTO #tblContractDetailsOld
-			--FROM	tblCTContractDetail 
-			--WHERE intContractDetailId = @intContractDetailId
-
-			SELECT top 1 *
-			INTO #tblContractDetailsOld
-			FROM tblCTContractDetail
+			SELECT	@_intNoOfLoad = [intNoOfLoad]
+					,@_dblQuantity = dblQuantity
+					,@_dblBalance = dblQuantityPerLoad
+					,@_dblScheduleQty = dblScheduleQty
+					,@_dblBalanceLoad = dblBalanceLoad
+					,@_dblScheduleLoad = dblScheduleLoad
+			FROM tblCTContractDetail 
 			WHERE intContractDetailId = @intContractDetailId
 
 			IF(@ysnLoad = 1)
 			BEGIN
 
-				DECLARE @sample varchar(50) = ''
+				IF(@intNoOfLoad != @_intNoOfLoad)
+				BEGIN
+					
+					IF(@_intNoOfLoad = @_dblBalanceLoad)
+					BEGIN
+
+						UPDATE #tblContractDetails
+						SET dblBalanceLoad = @intNoOfLoad
+					
+					END
+					ELSE IF (@_intNoOfLoad - @_dblBalanceLoad > 0)
+					BEGIN
+						
+						UPDATE #tblContractDetails
+						SET dblBalanceLoad = @intNoOfLoad - (@_intNoOfLoad - @_dblBalanceLoad)
+
+					END
+					ELSE
+					BEGIN
+
+						UPDATE #tblContractDetails
+						SET dblBalanceLoad += @intNoOfLoad - @_intNoOfLoad
+
+					END
+
+					UPDATE #tblContractDetails
+					SET dblBalance = dblBalance * dblQuantityPerLoad
+
+				END
 				
 			END
+			ELSE
+			BEGIN
+				
+				IF(@dblQuantity != @_dblQuantity)
+				BEGIN
+					
+					IF(@_dblQuantity = @_dblBalance)
+					BEGIN
+						UPDATE #tblContractDetails
+						SET dblBalance = @dblQuantity
+					END
+					ELSE IF(@_dblQuantity - @_dblBalance > 0)
+					BEGIN
+						UPDATE #tblContractDetails
+						SET dblBalance = @dblQuantity - (@_dblQuantity - @_dblBalance)
+					END
+					ELSE
+					BEGIN
+						UPDATE #tblContractDetails
+						SET dblBalance += @dblQuantity - @_dblQuantity
+					END
+
+				END
+			END
+		END
+
+		/* Change Status */
+		IF(@strRowState = 'Modified')
+		BEGIN
+
+			SELECT	@_intContractStatusId = intContractStatusId
+					,@_intPricingTypeId = intPricingTypeId
+					,@_dblBalance = dblBalance
+			FROM tblCTContractDetail 
+			WHERE intContractDetailId = @intContractDetailId
+
+			IF (@dblBalance = 0)
+			BEGIN
+				IF(@ysnUnlimitedQuantity = 1
+				and @_intPricingTypeId in (1,6,7)
+				and @_intContractStatusId in (1,4)
+				and @intContractDetailId in (1,4))
+				BEGIN
+					UPDATE #tblContractDetails
+					SET @_intContractStatusId = 5,
+					strContractStatus = 'Complete'
+				END
+			END
+			ELSE IF (@_dblBalance = 0)
+			BEGIN
+				IF(@_intContractStatusId = 5 and @intContractDetailId = 5)
+				BEGIN
+					UPDATE #tblContractDetails
+					SET @_intContractStatusId = 4,
+					strContractStatus = 'Re-Open'
+				END
+			END
+
 
 		END
 
-		
-		SELECT	*
-		FROM	#tblContractDetails 
-		WHERE	intUniqueId = @intUniqueId
+		/* Set Creation and Modification Info */
+		UPDATE #tblContractDetails
+		set RowState = @strRowState
+		,dtmEndDate = DATEADD(SECOND, 86399, DATEDIFF(dd, 0, @dtmEndDate))
+
+		IF(@strRowState = 'Modified')
+		BEGIN
+			UPDATE #tblContractDetails
+			SET intLastModifiedById = @userId
+			,dtmLastModified = GETUTCDATE()
+		END
+		ELSE IF(@strRowState = 'Modified')
+		BEGIN
+			UPDATE #tblContractDetails
+			SET intCreateById = @userId
+			,@dtmCreated = GETUTCDATE()
+		END
+
+		/* preview results on debug */
+		DECLARE @preview nvarchar(max) = (SELECT * FROM #tblContractDetails FOR JSON AUTO)
+
+		/* preview results on query execution */
+		--SELECT *
+		--FROM	#tblContractDetails 
+		--WHERE	intUniqueId = @intUniqueId
 
 		SET @intUniqueId = (SELECT MIN(intUniqueId) FROM #tblContractDetails WHERE intUniqueId > @intUniqueId)
 
