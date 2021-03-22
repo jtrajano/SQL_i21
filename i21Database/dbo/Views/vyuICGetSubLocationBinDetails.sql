f3fa142f5f05476e1e122324a6fa9dd0660f13ef
Feb Montefrio <feb.montefrio@irely.com>
2021-03-22 15:11:40 +0800

    [B] IC-9331
    # Conflicts:
    #	i21Database/dbo/Views/vyuICGetSubLocationBinDetails.sql

--- i21Database/dbo/Views/vyuICGetSubLocationBinDetails.sql
+++ i21Database/dbo/Views/vyuICGetSubLocationBinDetails.sql
@@ -6,22 +6,20 @@ SELECT
 	, intItemLocationId			= il.intItemLocationId
 	, intSubLocationId			= CASE WHEN (i.ysnSeparateStockForUOMs = 1 AND i.strLotTracking = 'No') THEN stockUOM1.intSubLocationId ELSE stockUOM2.intSubLocationId END 								
 	, strSubLocationName		= CASE WHEN (i.ysnSeparateStockForUOMs = 1 AND i.strLotTracking = 'No') THEN stockUOM1.strSubLocationName ELSE stockUOM2.strSubLocationName END 
-	, intStorageLocationId		= sl.intStorageLocationId
+	, intStorageLocationId		= NULL 
 	, strLocation				= c.strLocationName
-	, strStorageLocation		= sl.strName
+	, strStorageLocation		= NULL 
 	, strItemNo					= i.strItemNo
 	, strItemDescription		= i.strDescription
 	, strItemUOM				= um.strUnitMeasure
 	, dblStock					= 
-		CAST(
 			CASE 
 				WHEN i.ysnSeparateStockForUOMs = 1 AND i.strLotTracking = 'No' THEN 
 					ISNULL(stockUOM1.dblOnHand, 0) + ISNULL(stockUOM1.dblUnitStorage, 0) 
 				ELSE 
 					ISNULL(stockUOM2.dblOnHand, 0) + ISNULL(stockUOM2.dblUnitStorage, 0) 
 			END 								
-		 AS NUMERIC(28, 6))
-	, dblCapacity				= CAST(sl.dblCapacity AS NUMERIC(28, 6)) 
+	, dblCapacity				= ISNULL(sl.dblCapacity, 0)
 	, dblAvailable				= 
 		CAST(
 			CASE 
@@ -71,7 +69,7 @@ FROM
 		WHERE		
 			sm.intItemLocationId = il.intItemLocationId
 			AND sm.intItemId = il.intItemId
-			AND sm.intSubLocationId IS NOT NULL 
+			--AND sm.intSubLocationId IS NOT NULL 
 			AND i.ysnSeparateStockForUOMs = 1
 			AND (i.strLotTracking = 'No' OR i.strLotTracking IS NULL) 
 		GROUP BY
@@ -86,13 +84,12 @@ FROM
 				,sm.intSubLocationId
 				,sc.strSubLocationName
 		FROM 
-			tblICItemStockUOM sm 
-			LEFT JOIN tblSMCompanyLocationSubLocation sc ON 
-				sc.intCompanyLocationSubLocationId = sm.intSubLocationId
+			tblICItemStockUOM sm LEFT JOIN tblSMCompanyLocationSubLocation sc 
+				ON sc.intCompanyLocationSubLocationId = sm.intSubLocationId
 		WHERE		
 			sm.intItemLocationId = il.intItemLocationId
 			AND sm.intItemId = il.intItemId
-			AND sm.intSubLocationId IS NOT NULL 
+			--AND sm.intSubLocationId IS NOT NULL 
 			AND sm.intItemUOMId = stockUOM.intItemUOMId
 			AND (
 				ISNULL(i.ysnSeparateStockForUOMs, 1) = 0 
@@ -104,20 +101,17 @@ FROM
 			,sc.strSubLocationName
 	) stockUOM2
 
+	-- Get the total "storage" capacity of a company-location and storage-location (formerly known as sub-location). 
 	OUTER APPLY (
 		SELECT 
 			dblCapacity = SUM(ISNULL(sl.dblEffectiveDepth,0) *  ISNULL(sl.dblUnitPerFoot, 0))
-			,sl.intStorageLocationId
-			,sl.strName 
 		FROM 
 			tblICStorageLocation sl inner join tblICItemStockUOM sm
 				ON sl.intStorageLocationId = sm.intStorageLocationId
 		WHERE
 			sm.intItemId = i.intItemId	
 			AND sm.intItemLocationId = il.intItemLocationId
-		GROUP BY 
-			sl.intStorageLocationId
-			,sl.strName 	
+			AND sl.intSubLocationId = COALESCE(stockUOM1.intSubLocationId, stockUOM2.intSubLocationId)
 	) sl
 
 	LEFT OUTER JOIN tblICCommodity com ON com.intCommodityId = i.intCommodityId
