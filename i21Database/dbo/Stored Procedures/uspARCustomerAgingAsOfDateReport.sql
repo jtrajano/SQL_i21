db71777a031252776b0e4e9ff7120364b198ef16
Roudinee Panteria <roudinee.panteria@irely.com>
2021-03-23 17:26:07 +0800

    Revert "AR-13056"
    This reverts commit c3116009a79cf8714f3c4ea9eb9fd5fca6275a29.

--- i21Database/dbo/Stored Procedures/uspARCustomerAgingAsOfDateReport.sql
+++ i21Database/dbo/Stored Procedures/uspARCustomerAgingAsOfDateReport.sql
@@ -241,8 +241,8 @@ INNER JOIN (
 WHERE intInvoiceId IS NOT NULL
 
 --#FORGIVENSERVICECHARGE
-SELECT SC.intInvoiceId
-	 , SC.strInvoiceNumber
+SELECT I.intInvoiceId
+	 , I.strInvoiceNumber
 INTO #FORGIVENSERVICECHARGE 
 FROM tblARInvoice I
 INNER JOIN @tblCustomers C ON I.intEntityCustomerId = C.intEntityCustomerId
