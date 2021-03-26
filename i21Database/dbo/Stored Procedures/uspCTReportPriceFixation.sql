b3098475c738bd352878f0cdd71753405412d368
jorey <jorey@XPS-JOREY>
2021-03-26 10:25:46 +0800

    CT-5744

--- i21Database/dbo/Stored Procedures/uspCTReportPriceFixation.sql
+++ i21Database/dbo/Stored Procedures/uspCTReportPriceFixation.sql
@@ -200,7 +200,12 @@ BEGIN TRY
 			lblAdditionalCostColon = CASE WHEN ISNULL(PF.dblAdditionalCost,0) <> 0 THEN ':' ELSE NULL END,
 			strEQTAdditionalCost = CASE WHEN ISNULL(PF.dblAdditionalCost,0) <> 0 THEN dbo.fnRemoveTrailingZeroes(PF.dblAdditionalCost) + ' ' + CY.strCurrency + ' ' + @per + ' ' + isnull(rtrt3.strTranslation,CM.strUnitMeasure) ELSE NULL END, 
 			strAdditionalCost = dbo.fnRemoveTrailingZeroes(PF.dblAdditionalCost) + ' ' + CY.strCurrency + ' ' + @per + ' ' + isnull(rtrt3.strTranslation,CM.strUnitMeasure) ,
-			strFinalPrice = dbo.fnCTChangeNumericScale(dbo.fnCTConvertToBaseCurrency(CD.intCurrencyId,dbo.fnCTConvertQtyToTargetCommodityUOM(CH.intCommodityId,CU.intUnitMeasureId, FU.intUnitMeasureId,PF.dblFinalPrice)),2) + ' ' + CY.strDescription + ' ' + @per + ' ' + isnull(rtrt3.strTranslation,CM.strUnitMeasure) ,  
+
+			
+			/* CT-5744 */
+			--strFinalPrice = dbo.fnCTChangeNumericScale(dbo.fnCTConvertToBaseCurrency(CD.intCurrencyId,dbo.fnCTConvertQtyToTargetCommodityUOM(CH.intCommodityId,CU.intUnitMeasureId, FU.intUnitMeasureId,PF.dblFinalPrice)),2) + ' ' + CY.strDescription + ' ' + @per + ' ' + isnull(rtrt3.strTranslation,CM.strUnitMeasure) ,  
+			strFinalPrice = CAST(dbo.fnCTChangeNumericScale(ROUND(dbo.fnCTConvertToBaseCurrency(CD.intCurrencyId,dbo.fnCTConvertQtyToTargetCommodityUOM(CH.intCommodityId,CU.intUnitMeasureId, FU.intUnitMeasureId,PF.dblFinalPrice)),2),2) as varchar(50) ) + ' ' + CY.strDescription + ' ' + @per + ' ' + isnull(rtrt3.strTranslation,CM.strUnitMeasure) ,  
+
 			strFinalPrice2 =	'=    ' + dbo.fnRemoveTrailingZeroes(ROUND(
 								CASE	WHEN	CD.intCurrencyId = CD.intInvoiceCurrencyId 
 										THEN	NULL
