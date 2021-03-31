3a65907338e8a564256657d06e0d4d5880f60901
Jeff Trajano <jeffrey.trajano@irely.com>
2021-03-31 11:05:11 +0800

    GL-8083 Stuck in 21.1 DB Upgrade

--- i21Database/Scripts/Post-Deployment/GL/DefaultData/1u_InsertDefaultDataSync.sql
+++ i21Database/Scripts/Post-Deployment/GL/DefaultData/1u_InsertDefaultDataSync.sql
@@ -1,13 +1,15 @@
+
 GO
     DECLARE @rowUpdated  NVARCHAR(20)
 	DECLARE @updateCount INT = 0
 	DECLARE @tCount INT
+	DECLARE @interval INT = 1000
 	SELECT @tCount = COUNT(1) FROM tblGLDetail WHERE intFiscalPeriodId IS NULL
 
 
     WHILE  @updateCount < @tCount
 	  BEGIN
-	  	SET @updateCount = @updateCount + 1000
+	  	SET @updateCount = @updateCount + @interval
 
 		;WITH cte as(
 		    SELECT intGLDetailId,
@@ -15,13 +17,14 @@ GO
 			from tblGLDetail WHERE intFiscalPeriodId IS NULL
 		),
 		cte1 AS(
-			SELECT intGLDetailId FROM cte WHERE rowId <= @updateCount
+			SELECT intGLDetailId FROM cte WHERE rowId <= @interval
 		)
         UPDATE T SET intFiscalPeriodId = F.intGLFiscalYearPeriodId FROM tblGLDetail T 
 		JOIN cte1 C ON C.intGLDetailId = T.intGLDetailId
         CROSS APPLY dbo.fnGLGetFiscalPeriod(T.dtmDate) F
         SELECT  @rowUpdated = CONVERT( NVARCHAR(20) , @@ROWCOUNT )
 		PRINT ('Updated fiscal period of ' +  @rowUpdated +  ' records in tblGLDetail')
+
+		
 	 END
 GO
-
