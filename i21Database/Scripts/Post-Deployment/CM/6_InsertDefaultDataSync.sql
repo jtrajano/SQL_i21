e7c3ddf9b1af6b278840638ce14c2d910f44b45d
Jeff Trajano <jeffrey.trajano@irely.com>
2021-03-31 11:05:11 +0800

    GL-8083 Stuck in 21.1 DB Upgrade

--- i21Database/Scripts/Post-Deployment/CM/6_InsertDefaultDataSync.sql
+++ i21Database/Scripts/Post-Deployment/CM/6_InsertDefaultDataSync.sql
@@ -2,12 +2,13 @@ GO
     DECLARE @rowUpdated  NVARCHAR(20)
 	DECLARE @btCount INT
 	DECLARE @updateCount INT = 0
+	DECLARE @interval INT = 1000
 	SELECT @btCount = COUNT(1) FROM tblCMBankTransaction WHERE intFiscalPeriodId IS NULL
 
 
     WHILE @updateCount < @btCount
 	  BEGIN
-	  	SET @updateCount = @updateCount + 1000
+	  	SET @updateCount = @updateCount + @interval
 		;WITH cte as(
 		    SELECT intTransactionId ,
 			ROW_NUMBER() over(order by intTransactionId) rowId
@@ -16,7 +17,7 @@ GO
 		),
 		cte1 as(
 
-			SELECT intTransactionId from cte WHERE rowId <=@updateCount
+			SELECT intTransactionId from cte WHERE rowId <= @interval
 		)
 
         UPDATE T SET intFiscalPeriodId = F.intGLFiscalYearPeriodId FROM tblCMBankTransaction T 
@@ -32,7 +33,7 @@ GO
 
     WHILE @updateCount < @btCount
 	  BEGIN
-	  	SET @updateCount = @updateCount + 1000
+	  	SET @updateCount = @updateCount + @interval
 
 		;WITH cte as(
 		    SELECT intTransactionId,
@@ -41,7 +42,7 @@ GO
 		),
 		cte1 as(
 
-			SELECT intTransactionId from cte WHERE rowId <=@updateCount
+			SELECT intTransactionId from cte WHERE rowId <= @interval
 		)
         UPDATE T SET intFiscalPeriodId = F.intGLFiscalYearPeriodId FROM tblCMBankTransfer T 
 		JOIN cte1 C ON C.intTransactionId = T.intTransactionId
