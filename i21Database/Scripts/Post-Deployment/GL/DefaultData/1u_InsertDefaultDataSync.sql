0c8c31dbc055e26c03fb5990f924d83205548de3
Jeff Trajano <jeffrey.trajano@irely.com>
2021-03-31 10:17:43 +0800

    GL-8083 Stuck in 21.1 DB Upgrade

--- i21Database/Scripts/Post-Deployment/GL/DefaultData/1u_InsertDefaultDataSync.sql
+++ i21Database/Scripts/Post-Deployment/GL/DefaultData/1u_InsertDefaultDataSync.sql
@@ -1,15 +1,27 @@
-
 GO
     DECLARE @rowUpdated  NVARCHAR(20)
-    WHILE EXISTS(SELECT TOP 1 1 FROM tblGLDetail WHERE intFiscalPeriodId IS NULL)
+	DECLARE @updateCount INT = 0
+	DECLARE @tCount INT
+	SELECT @tCount = COUNT(1) FROM tblGLDetail WHERE intFiscalPeriodId IS NULL
+
+
+    WHILE  @updateCount < @tCount
 	  BEGIN
+	  	SET @updateCount = @updateCount + 1000
+
 		;WITH cte as(
-		    SELECT TOP 1000000 intGLDetailId from tblGLDetail WHERE intFiscalPeriodId IS NULL
+		    SELECT intGLDetailId,
+			ROW_NUMBER() OVER (ORDER BY intGLDetailId) rowId
+			from tblGLDetail WHERE intFiscalPeriodId IS NULL
+		),
+		cte1 AS(
+			SELECT intGLDetailId FROM cte WHERE rowId <= @updateCount
 		)
         UPDATE T SET intFiscalPeriodId = F.intGLFiscalYearPeriodId FROM tblGLDetail T 
-		JOIN cte C ON C.intGLDetailId = T.intGLDetailId
+		JOIN cte1 C ON C.intGLDetailId = T.intGLDetailId
         CROSS APPLY dbo.fnGLGetFiscalPeriod(T.dtmDate) F
         SELECT  @rowUpdated = CONVERT( NVARCHAR(20) , @@ROWCOUNT )
 		PRINT ('Updated fiscal period of ' +  @rowUpdated +  ' records in tblGLDetail')
 	 END
 GO
+
