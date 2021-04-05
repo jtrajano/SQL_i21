6af511445e893e66eb7766c8f54d5da9990641cb
Erick Dela Cruz <erick.delacruz@irely.com>
2021-04-05 21:09:09 +0800

    AP-9563

--- i21Database/dbo/Stored Procedures/uspAPValidateImportedPaidVouchersDTN.sql
+++ i21Database/dbo/Stored Procedures/uspAPValidateImportedPaidVouchersDTN.sql
@@ -22,19 +22,19 @@ IF @transCount = 0 BEGIN TRANSACTION;
 
 UPDATE A
 	SET A.strNotes = CASE
-					WHEN 
-							A.intCurrencyId = B.intCurrencyId
-						AND B.ysnPaid = 0
-						AND B.ysnPosted = 1
-						AND B.intBillId > 0
-						AND (A.dblPayment + A.dblDiscount) = B.dblAmountDue
-						THEN 
-							(
-								CASE 
-								WHEN A.dblPayment < 0 AND B.intTransactionType = 1
-								THEN 'Invalid amount.'
-								ELSE NULL END
-							)
+					-- WHEN 
+					-- 		A.intCurrencyId = B.intCurrencyId
+					-- 	AND B.ysnPaid = 0
+					-- 	AND B.ysnPosted = 1
+					-- 	AND B.intBillId > 0
+					-- 	AND (A.dblPayment + A.dblDiscount) = B.dblAmountDue
+					-- 	THEN 
+					-- 		(
+					-- 			CASE 
+					-- 			WHEN A.dblPayment < 0 AND B.intTransactionType = 1
+					-- 			THEN 'Invalid amount.'
+					-- 			ELSE NULL END
+					-- 		)
 					WHEN 
 						A.intCurrencyId != B.intCurrencyId
 					THEN 'Currency is different on current selected currency.'
