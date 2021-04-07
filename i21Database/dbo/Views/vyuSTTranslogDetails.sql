e2bf861731fc8d1fb3e96903ccd6b603adc1f44d
nelsonreyes19 <nelson.reyes@irely.com>
2021-04-07 10:47:46 +0800

    ST-1883
    Fix issue

--- i21Database/dbo/Views/vyuSTTranslogDetails.sql
+++ i21Database/dbo/Views/vyuSTTranslogDetails.sql
@@ -5,26 +5,24 @@ FROM
 (
 	SELECT DISTINCT 
 		CAST(TR.intTermMsgSN AS NVARCHAR(MAX)) + '0' +  CAST(TR.intTermMsgSNterm AS NVARCHAR(MAX)) + '0' + CAST(TR.intStoreId AS NVARCHAR(MAX)) 
-	   --+ CAST(USec.intEntityId AS NVARCHAR(MAX)) 
-	   COLLATE Latin1_General_CI_AS AS strUniqueId
+			--+ CAST(USec.intEntityId AS NVARCHAR(MAX)) 
+			COLLATE Latin1_General_CI_AS AS strUniqueId
        , TR.intTrlDeptNumber
 	   , TR.strTrlDept
 	   , TR.strTrlNetwCode
 	   , TR.strTrlUPC
 	   , TR.strTrlDesc
-	   , TR. dblTrlQty
+	   , TR.dblTrlQty
 	   , TR.dblTrlUnitPrice
 	   , TR.dblTrlLineTot
 	   , TR.intTermMsgSN
 	   , TR.dtmDate
 	   , TR.intCashierPosNum
 	   , RIGHT('0' + CONVERT(VARCHAR(2), DATEPART(HOUR, TR.dtmDate)), 2) as Hr
-	   -- , USec.intEntityId
+	   --, USec.intEntityId
 	FROM tblSTTranslogRebates TR
-	JOIN tblSTCheckoutHeader CH 
-		ON TR.intCheckoutId = CH.intCheckoutId
 	JOIN tblSTStore ST 
-		ON CH.intStoreId = ST.intStoreId 
+		ON TR.intStoreId = ST.intStoreId 
 	-- OUTER APPLY tblSMUserSecurity USec
 	-- INNER JOIN tblSMUserSecurityCompanyLocationRolePermission RolePerm
 		-- ON USec.intEntityId = RolePerm.intEntityId
