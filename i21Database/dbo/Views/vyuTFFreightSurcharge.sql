fa1e947c58aa31e06e540b1a58c78cf3312a4f84
cris.lazaga <cris.lazaga@irely.com>
2021-04-08 13:08:30 +0800

    TR-1592

---
+++ i21Database/dbo/Views/vyuTFFreightSurcharge.sql
@@ -0,0 +1,25 @@
+﻿CREATE VIEW [dbo].[vyuTFFreightSurcharge]
+	AS 
+SELECT CF.intFreightXRefId
+	,C.intShipViaId
+	,ShipVia.strName AS strShipViaName
+	,CF.intCategoryId
+	,CAT.strCategoryCode AS strCategoryCode
+	,AR.intEntityTariffTypeId
+	,CF.intEntityCustomerId
+	,Customer.strName AS strCustomerName
+	,C.dtmEffectiveDateTime
+	,CONVERT(int,CF.dblFreightMiles) intFreightMiles
+	,C.dblReceiptFreightRate
+	,C.dblInvoiceFreightRate
+	,C.dtmSurchargeEffectiveDateTime
+	,C.dblInvoiceSurchargePercentage
+	,C.dblReceiptSurchargePercentage
+FROM tblARCustomerFreightXRef CF 
+	INNER JOIN tblARCustomer AR on AR.intEntityId = CF.intEntityCustomerId
+	INNER JOIN tblICCategory CAT ON CAT.intCategoryId = CF.intCategoryId
+	INNER JOIN tblEMEntity Customer ON Customer.intEntityId = CF.intEntityCustomerId
+CROSS APPLY (
+	SELECT  * FROM dbo.fnTRGetFreightSurcharge(CF.strFreightType, CF.intCategoryId, AR.intEntityTariffTypeId, CF.dblFreightRate, CONVERT(int,CF.dblFreightMiles))
+) C
+	INNER JOIN tblEMEntity ShipVia ON ShipVia.intEntityId = C.intShipViaId
