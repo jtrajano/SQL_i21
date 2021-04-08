a608b72bb2b6444aed2ed7fb9eefd108b53a0fbd
IRELY\JMendones <jesmar.mendones@irely.com>
2021-04-08 13:34:36 +0800

    SM-5124

--- i21Database/Scripts/Post-Deployment/SM/DefaultData/1_MasterMenu.sql
+++ i21Database/Scripts/Post-Deployment/SM/DefaultData/1_MasterMenu.sql
@@ -11,8 +11,7 @@ GO
 	/* UPDATE ENTITY CREDENTIAL CONCURRENCY */
 
 
-
-	IF EXISTS(SELECT TOP 1 1 FROM tblSMMasterMenu WHERE strMenuName = 'Amendments' AND strModuleName = 'Contract Management')
+	IF EXISTS(SELECT TOP 1 1 FROM tblSMMasterMenu WHERE strMenuName = 'Lottery Inventory Report' AND strCommand = ' Store.view.LotteryInventoryReport')
 	BEGIN
 		EXEC uspSMIncreaseECConcurrency 0
 		
@@ -5321,9 +5320,9 @@ ELSE
 
 IF NOT EXISTS(SELECT TOP 1 1 FROM tblSMMasterMenu WHERE strMenuName = 'Lottery Inventory Report' AND strModuleName = 'Store' AND intParentMenuID = @StoreLotteryParentMenuId)	
 	INSERT [dbo].[tblSMMasterMenu] ([strMenuName], [strModuleName], [intParentMenuID], [strDescription], [strCategory], [strType], [strCommand], [strIcon], [ysnVisible], [ysnExpanded], [ysnIsLegacy], [ysnLeaf], [intSort], [intConcurrencyId]) 
-	VALUES (N'Lottery Inventory Report', N'Store', @StoreLotteryParentMenuId, N'Lottery Inventory Report', N'Lottery', N'Screen', N' Store.view.LotteryInventoryReport', N'small-menu-lottery', 0, 0, 0, 1, 1, 3)	
+	VALUES (N'Lottery Inventory Report', N'Store', @StoreLotteryParentMenuId, N'Lottery Inventory Report', N'Lottery', N'Screen', N'Store.view.LotteryInventoryReport', N'small-menu-lottery', 0, 0, 0, 1, 1, 3)	
 ELSE
-	UPDATE tblSMMasterMenu SET intSort = 0, strCommand = N' Store.view.LotteryInventoryReport' WHERE strMenuName = 'Lottery Inventory Report' AND strModuleName = 'Store' AND intParentMenuID = @StoreLotteryParentMenuId
+	UPDATE tblSMMasterMenu SET intSort = 0, strCommand = N'Store.view.LotteryInventoryReport' WHERE strMenuName = 'Lottery Inventory Report' AND strModuleName = 'Store' AND intParentMenuID = @StoreLotteryParentMenuId
 
 --END LOTTERY
 
