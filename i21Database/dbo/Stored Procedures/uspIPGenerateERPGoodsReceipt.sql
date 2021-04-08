293187be36bc95a5a142bdcc8be046cc4b534e48
Vinoth Kumar T <vinoth.t@irely.com>
2021-04-09 00:14:28 +0530

    IN-786

--- i21Database/dbo/Stored Procedures/uspIPGenerateERPGoodsReceipt.sql
+++ i21Database/dbo/Stored Procedures/uspIPGenerateERPGoodsReceipt.sql
@@ -10,10 +10,12 @@ BEGIN TRY
 		,@strRowState NVARCHAR(50)
 		,@strUserName NVARCHAR(50)
 		,@strError NVARCHAR(MAX) = ''
+		,@strFinalXML NVARCHAR(MAX) = ''
 		,@strXML NVARCHAR(MAX) = ''
 		,@strItemXML NVARCHAR(MAX) = ''
-		,@strContainerXML NVARCHAR(MAX) = ''
-		,@strFinalXML NVARCHAR(MAX) = ''
+		,@strLotXML NVARCHAR(MAX) = ''
+		,@strLineXML NVARCHAR(MAX) = ''
+		,@strHeaderXML NVARCHAR(MAX) = ''
 	DECLARE @strQuantityUOM NVARCHAR(50)
 		,@strDefaultCurrency NVARCHAR(40)
 		,@intCurrencyId INT
@@ -49,6 +51,14 @@ BEGIN TRY
 	DECLARE @tblICInventoryReceipt TABLE (intInventoryReceiptId INT)
 	DECLARE @tblICInventoryReceiptItem TABLE (intInventoryReceiptItemId INT)
 	DECLARE @tblICInventoryReceiptItemLot TABLE (intInventoryReceiptItemLotId INT)
+	DECLARE @tblOutput AS TABLE (
+		intRowNo INT IDENTITY(1, 1)
+		,intInventoryReceiptId INT
+		,strRowState NVARCHAR(50)
+		,strXML NVARCHAR(MAX)
+		,strReceiptNumber NVARCHAR(50)
+		,strTransferNo NVARCHAR(50)
+		)
 
 	DELETE
 	FROM @tblICInventoryReceipt
@@ -60,20 +70,23 @@ BEGIN TRY
 	JOIN tblICInventoryReceipt R ON R.intInventoryReceiptId = RI.intInventoryReceiptId
 	JOIN tblCTContractDetail CD ON CD.intContractDetailId = RI.intContractDetailId
 	JOIN tblSMCompanyLocation CL ON CL.intCompanyLocationId = CD.intCompanyLocationId
-	AND CL.strLotOrigin = @strCompanyLocation
+		AND CL.strLotOrigin = @strCompanyLocation
 	WHERE R.strReceiptType = 'Purchase Contract'
 		AND R.ysnPosted = 1
 		AND RI.ysnExported IS NULL
 		AND ISNULL(CD.strERPPONumber, '') <> ''
 
-	DECLARE @tblOutput AS TABLE (
-		intRowNo INT IDENTITY(1, 1)
-		,intInventoryReceiptId INT
-		,strRowState NVARCHAR(50)
-		,strXML NVARCHAR(MAX)
-		,strReceiptNumber NVARCHAR(50)
-		,strTransferNo NVARCHAR(50)
-		)
+	INSERT INTO @tblICInventoryReceipt (intInventoryReceiptId)
+	SELECT DISTINCT R.intInventoryReceiptId
+	FROM tblICInventoryReceiptItemLot RIL
+	JOIN tblICInventoryReceiptItem RI ON RI.intInventoryReceiptItemId = RIL.intInventoryReceiptItemId
+	JOIN tblICInventoryReceipt R ON R.intInventoryReceiptId = RI.intInventoryReceiptId
+	JOIN tblICInventoryTransfer IT ON IT.intInventoryTransferId = RI.intInventoryTransferId
+	JOIN tblSMCompanyLocation CL ON CL.intCompanyLocationId = IT.intToLocationId
+		AND CL.strLotOrigin = @strCompanyLocation
+	WHERE R.strReceiptType = 'Transfer Order'
+		AND R.ysnPosted = 1
+		AND RI.ysnExported IS NULL
 
 	IF NOT EXISTS (
 			SELECT 1
@@ -91,7 +104,6 @@ BEGIN TRY
 		SELECT @strRowState = NULL
 			,@strUserName = NULL
 			,@strError = ''
-			,@strXML = ''
 
 		SELECT @intActionId = NULL
 			,@strVendorAccountNum = NULL
@@ -120,8 +132,8 @@ BEGIN TRY
 			,@dtmReceiptDate = R.dtmReceiptDate
 			,@strBillOfLading = R.strBillOfLading
 			,@strWarehouseRefNo = R.strWarehouseRefNo
-			,@strTransferNo = ''
-			,@strERPTransferOrderNo = ''
+			--,@strTransferNo = ''
+			--,@strERPTransferOrderNo = ''
 			,@strCurrency = C.strCurrency
 		FROM dbo.tblICInventoryReceipt R
 		JOIN dbo.tblSMUserSecurity US ON US.intEntityId = ISNULL(R.intModifiedByUserId, R.intCreatedByUserId)
@@ -129,11 +141,21 @@ BEGIN TRY
 		LEFT JOIN tblSMCurrency C ON C.intCurrencyID = R.intCurrencyId
 		WHERE intInventoryReceiptId = @intInventoryReceiptId
 
+		IF @intActionId = 2
+		BEGIN
+			SELECT TOP 1 @strTransferNo = IT.strTransferNo
+				,@strERPTransferOrderNo = ''
+			FROM tblICInventoryReceiptItem RI
+			JOIN tblICInventoryTransfer IT ON IT.intInventoryTransferId = RI.intInventoryTransferId
+				AND RI.intInventoryReceiptId = @intInventoryReceiptId
+		END
+
 		IF NOT EXISTS (
 				SELECT 1
 				FROM tblIPInvReceiptFeed
 				WHERE intInventoryReceiptId = @intInventoryReceiptId
 				)
+		BEGIN
 			INSERT INTO tblIPInvReceiptFeed (
 				strCompanyLocation
 				,intInventoryReceiptId
@@ -156,12 +178,36 @@ BEGIN TRY
 				,NULL
 				,NULL
 				,NULL
+		END
+		ELSE
+		BEGIN
+			UPDATE tblIPInvReceiptFeed
+			SET strTransferNo = @strTransferNo
+				,strERPTransferOrderNo = @strERPTransferOrderNo
+				,strCreatedBy = @strUserName
+				,strMessage = NULL
+				,intStatusId = NULL
+				,strFeedStatus = NULL
+			WHERE intInventoryReceiptId = @intInventoryReceiptId
+		END
 
 		IF @intActionId = 1
 		BEGIN
 			IF ISNULL(@strVendorAccountNum, '') = ''
 			BEGIN
 				SELECT @strError = @strError + 'Vendor Account Number cannot be blank. '
+			END
+		END
+		ELSE IF @intActionId = 2
+		BEGIN
+			IF ISNULL(@strTransferNo, '') = ''
+			BEGIN
+				SELECT @strError = @strError + 'Transfer No. cannot be blank. '
+			END
+
+			IF ISNULL(@strERPTransferOrderNo, '') = ''
+			BEGIN
+				SELECT @strError = @strError + 'ERP Transfer No. cannot be blank. '
 			END
 		END
 
@@ -175,11 +221,43 @@ BEGIN TRY
 			GOTO NextRec
 		END
 
+		SELECT @strXML = ''
+
+		SELECT @strXML += '<header id="' + LTRIM(@intInventoryReceiptId) + '">'
+
+		SELECT @strXML += '<TrxSequenceNo>' + LTRIM(@intInventoryReceiptId) + '</TrxSequenceNo>'
+
+		SELECT @strXML += '<CompanyLocation>' + LTRIM(@strCompanyLocation) + '</CompanyLocation>'
+
+		SELECT @strXML += '<ActionId>' + LTRIM(@intActionId) + '</ActionId>'
+
+		SELECT @strXML += '<CreatedDate>' + CONVERT(VARCHAR(33), GetDate(), 126) + '</CreatedDate>'
+
+		SELECT @strXML += '<CreatedByUser>' + @strUserName + '</CreatedByUser>'
+
+		SELECT @strXML += '<VendorAccountNo>' + ISNULL(@strVendorAccountNum, '') + '</VendorAccountNo>'
+
+		SELECT @strXML += '<VendorRefNo>' + ISNULL(@strVendorRefNo, '') + '</VendorRefNo>'
+
+		SELECT @strXML += '<ReceiptNo>' + ISNULL(@strReceiptNumber, '') + '</ReceiptNo>'
+
+		SELECT @strXML += '<ReceiptDate>' + ISNULL(CONVERT(VARCHAR, @dtmReceiptDate, 112), '') + '</ReceiptDate>'
+
+		SELECT @strXML += '<BOLNo>' + ISNULL(@strBillOfLading, '') + '</BOLNo>'
+
+		SELECT @strXML += '<WarehouseRefNo>' + ISNULL(@strWarehouseRefNo, '') + '</WarehouseRefNo>'
+
+		SELECT @strXML += '<TransferOrderNo>' + ISNULL(@strTransferNo, '') + '</TransferOrderNo>'
+
+		SELECT @strXML += '<ERPTransferOrderNo>' + ISNULL(@strERPTransferOrderNo, '') + '</ERPTransferOrderNo>'
+
 		DELETE
 		FROM @tblICInventoryReceiptItemLot
 
 		DELETE
 		FROM @tblICInventoryReceiptItem
+
+		SELECT @strLineXML = ''
 
 		INSERT INTO @tblICInventoryReceiptItem (intInventoryReceiptItemId)
 		SELECT RI.intInventoryReceiptItemId
@@ -251,10 +329,10 @@ BEGIN TRY
 					AND IUOM.ysnStockUnit = 1
 			END
 
-			SELECT @strContractNumber = CH.strContractNumber
-				,@strSequenceNo = LTRIM(CD.intContractSeq)
+			SELECT @strContractNumber = ISNULL(CH.strContractNumber, '')
+				,@strSequenceNo = ISNULL(LTRIM(CD.intContractSeq), '')
 				,@strItemNo = I.strItemNo
-				,@dblReceiptQty = RI.dblOrderQty
+				,@dblReceiptQty = CONVERT(NUMERIC(18, 6), RI.dblOrderQty)
 				,@strReceiptQtyUOM = UOM.strUnitMeasure
 				,@dblGross = CONVERT(NUMERIC(18, 6), ISNULL(dbo.fnCTConvertQtyToTargetItemUOM(RI.intWeightUOMId, @intItemUOMId, RI.dblGross), 0))
 				,@dblNet = CONVERT(NUMERIC(18, 6), ISNULL(dbo.fnCTConvertQtyToTargetItemUOM(RI.intWeightUOMId, @intItemUOMId, RI.dblNet), 0))
@@ -265,12 +343,12 @@ BEGIN TRY
 				,@strERPPONumber = ISNULL(CD.strERPPONumber, '')
 				,@strERPItemNumber = ISNULL(CD.strERPItemNumber, '')
 			FROM tblICInventoryReceiptItem RI
-			JOIN tblCTContractDetail CD ON CD.intContractDetailId = RI.intContractDetailId
-				AND RI.intInventoryReceiptItemId = @intInventoryReceiptItemId
-			JOIN tblCTContractHeader CH ON CH.intContractHeaderId = CD.intContractHeaderId
 			JOIN tblICItem I ON I.intItemId = RI.intItemId
 			JOIN tblICItemUOM IUOM ON IUOM.intItemUOMId = RI.intUnitMeasureId
 			JOIN tblICUnitMeasure UOM ON UOM.intUnitMeasureId = IUOM.intUnitMeasureId
+			LEFT JOIN tblCTContractDetail CD ON CD.intContractDetailId = RI.intContractDetailId
+				AND RI.intInventoryReceiptItemId = @intInventoryReceiptItemId
+			LEFT JOIN tblCTContractHeader CH ON CH.intContractHeaderId = CD.intContractHeaderId
 			LEFT JOIN tblSMCompanyLocationSubLocation CSL ON CSL.intCompanyLocationSubLocationId = RI.intSubLocationId
 			LEFT JOIN tblICStorageLocation SL ON SL.intStorageLocationId = RI.intStorageLocationId
 			LEFT JOIN tblLGLoadContainer LC ON LC.intLoadContainerId = RI.intContainerId
@@ -316,25 +394,79 @@ BEGIN TRY
 				GOTO NextRec
 			END
 
-			SELECT @strContainerXML = ''
+			SELECT @strItemXML = ''
 
-			SELECT RIL.strParentLotNumber
-				,RIL.strLotNumber
-				,RIL.dblQuantity
-				,UOM.strUnitMeasure
-				,CONVERT(NUMERIC(18, 6), ISNULL(dbo.fnCTConvertQtyToTargetItemUOM(RI.intWeightUOMId, @intItemUOMId, RIL.dblGrossWeight), 0))
-				,CONVERT(NUMERIC(18, 6), ISNULL(dbo.fnCTConvertQtyToTargetItemUOM(RI.intWeightUOMId, @intItemUOMId, RIL.dblTareWeight), 0))
-				,(CONVERT(NUMERIC(18, 6), ISNULL(dbo.fnCTConvertQtyToTargetItemUOM(RI.intWeightUOMId, @intItemUOMId, RIL.dblGrossWeight), 0)) - CONVERT(NUMERIC(18, 6), ISNULL(dbo.fnCTConvertQtyToTargetItemUOM(RI.intWeightUOMId, @intItemUOMId, RIL.dblTareWeight), 0)))
-				,@strQuantityUOM
-				,ISNULL(RIL.strVendorLotId, '')
-				,RIL.dtmExpiryDate
-				,SL.strName
-				,LS.strSecondaryStatus
-				,ISNULL(RIL.strCondition, '')
-				,ISNULL(RIL.strMarkings, '')
-				,ISNULL(O.strCountry, '')
-				,ISNULL(RIL.strLotAlias, '')
-				,ISNULL(RIL.strGarden, '')
+			SELECT @strItemXML += '<line id="' + LTRIM(@intInventoryReceiptItemId) + '" parentId="' + LTRIM(@intInventoryReceiptId) + '">'
+
+			SELECT @strItemXML += '<TrxSequenceNo>' + LTRIM(@intInventoryReceiptItemId) + '</TrxSequenceNo>'
+
+			SELECT @strItemXML += '<ContractNo>' + ISNULL(@strContractNumber, '') + '</ContractNo>'
+
+			SELECT @strItemXML += '<SequenceNo>' + ISNULL(@strSequenceNo, '') + '</SequenceNo>'
+
+			SELECT @strItemXML += '<ItemNo>' + ISNULL(@strItemNo, '') + '</ItemNo>'
+
+			SELECT @strItemXML += '<ReceiptQty>' + LTRIM(@dblReceiptQty) + '</ReceiptQty>'
+
+			SELECT @strItemXML += '<ReceiptQtyUOM>' + ISNULL(@strReceiptQtyUOM, '') + '</ReceiptQtyUOM>'
+
+			SELECT @strItemXML += '<GrossWeight>' + LTRIM(@dblGross) + '</GrossWeight>'
+
+			SELECT @strItemXML += '<TareWeight>' + LTRIM(@dblTare) + '</TareWeight>'
+
+			SELECT @strItemXML += '<NetWeight>' + LTRIM(@dblNet) + '</NetWeight>'
+
+			SELECT @strItemXML += '<WeightUOM>' + ISNULL(@strQuantityUOM, '') + '</WeightUOM>'
+
+			SELECT @strItemXML += '<Cost>' + LTRIM(@dblUnitCost) + '</Cost>'
+
+			SELECT @strItemXML += '<CostUOM>' + ISNULL(@strQuantityUOM, '') + '</CostUOM>'
+
+			SELECT @strItemXML += '<Currency>' + ISNULL(@strCurrency, '') + '</Currency>'
+
+			SELECT @strItemXML += '<StorageLocation>' + ISNULL(@strSubLocationName, '') + '</StorageLocation>'
+
+			SELECT @strItemXML += '<StorageUnit>' + ISNULL(@strStorageLocation, '') + '</StorageUnit>'
+
+			SELECT @strItemXML += '<ContainerNo>' + ISNULL(@strContainerNumber, '') + '</ContainerNo>'
+
+			SELECT @strItemXML += '<ERPPONumber>' + ISNULL(@strERPPONumber, '') + '</ERPPONumber>'
+
+			SELECT @strItemXML += '<ERPPOlineNo>' + ISNULL(@strERPItemNumber, '') + '</ERPPOlineNo>'
+
+			IF ISNULL(@strItemXML, '') = ''
+			BEGIN
+				UPDATE tblIPInvReceiptFeed
+				SET strMessage = 'Receipt Item XML not available. '
+					,intStatusId = 1
+				WHERE intInventoryReceiptId = @intInventoryReceiptId
+
+				GOTO NextRec
+			END
+
+			SELECT @strLotXML = ''
+
+			SELECT @strLotXML = @strLotXML
+				+ '<detail id="' + LTRIM(RIL.intInventoryReceiptItemLotId) + '" parentId="' + LTRIM(RIL.intInventoryReceiptItemId) + '">'
+				+ '<TrxSequenceNo>' + LTRIM(RIL.intInventoryReceiptItemLotId) + '</TrxSequenceNo>'
+				+ '<MotherLotNo>' + RIL.strParentLotNumber + '</MotherLotNo>'
+				+ '<LotNo>' + RIL.strLotNumber + '</LotNo>'
+				+ '<Quantity>' + LTRIM(CONVERT(NUMERIC(18, 6), RIL.dblQuantity)) + '</Quantity>'
+				+ '<QuantityUOM>' + UOM.strUnitMeasure + '</QuantityUOM>'
+				+ '<GrossWeight>' + LTRIM(CONVERT(NUMERIC(18, 6), ISNULL(dbo.fnCTConvertQtyToTargetItemUOM(RI.intWeightUOMId, @intItemUOMId, RIL.dblGrossWeight), 0))) + '</GrossWeight>'
+				+ '<TareWeight>' + LTRIM(CONVERT(NUMERIC(18, 6), ISNULL(dbo.fnCTConvertQtyToTargetItemUOM(RI.intWeightUOMId, @intItemUOMId, RIL.dblTareWeight), 0))) + '</TareWeight>'
+				+ '<NetWeight>' + LTRIM((CONVERT(NUMERIC(18, 6), ISNULL(dbo.fnCTConvertQtyToTargetItemUOM(RI.intWeightUOMId, @intItemUOMId, RIL.dblGrossWeight), 0)) - CONVERT(NUMERIC(18, 6), ISNULL(dbo.fnCTConvertQtyToTargetItemUOM(RI.intWeightUOMId, @intItemUOMId, RIL.dblTareWeight), 0)))) + '</NetWeight>'
+				+ '<WeightUOM>' + @strQuantityUOM + '</WeightUOM>'
+				+ '<VendorLotNo>' + ISNULL(RIL.strVendorLotId, '') + '</VendorLotNo>'
+				+ '<ExpiryDate>' + ISNULL(CONVERT(VARCHAR, RIL.dtmExpiryDate, 112), '') + '</ExpiryDate>'
+				+ '<StorageUnit>' + SL.strName + '</StorageUnit>'
+				+ '<LotStatus>' + LS.strSecondaryStatus + '</LotStatus>'
+				+ '<Condition>' + ISNULL(RIL.strCondition, '') + '</Condition>'
+				+ '<Markings>' + ISNULL(RIL.strMarkings, '') + '</Markings>'
+				+ '<Origin>' + ISNULL(O.strCountry, '') + '</Origin>'
+				+ '<LotAlias>' + ISNULL(RIL.strLotAlias, '') + '</LotAlias>'
+				+ '<Garden>' + ISNULL(RIL.strGarden, '') + '</Garden>'
+				+ '</detail>'
 			FROM tblICInventoryReceiptItemLot RIL
 			JOIN tblICInventoryReceiptItem RI ON RI.intInventoryReceiptItemId = RIL.intInventoryReceiptItemId
 				AND RIL.intInventoryReceiptItemId = @intInventoryReceiptItemId
@@ -346,10 +478,24 @@ BEGIN TRY
 			LEFT JOIN tblSMCountry O ON O.intCountryID = RIL.intOriginId
 			WHERE RIL.intInventoryReceiptItemId = @intInventoryReceiptItemId
 
+			IF ISNULL(@strLotXML, '') = ''
+			BEGIN
+				UPDATE tblIPInvReceiptFeed
+				SET strMessage = 'Receipt Lot XML not available. '
+					,intStatusId = 1
+				WHERE intInventoryReceiptId = @intInventoryReceiptId
+
+				GOTO NextRec
+			END
+
+			SELECT @strLineXML += @strItemXML + @strLotXML + '</line>'
+
 			SELECT @intInventoryReceiptItemId = MIN(intInventoryReceiptItemId)
 			FROM @tblICInventoryReceiptItem
 			WHERE intInventoryReceiptItemId > @intInventoryReceiptItemId
 		END
+
+		SELECT @strHeaderXML += @strXML + @strLineXML + '</header>'
 
 		IF @ysnUpdateFeedStatus = 1
 		BEGIN
@@ -370,9 +516,9 @@ BEGIN TRY
 		WHERE intInventoryReceiptId > @intInventoryReceiptId
 	END
 
-	IF @strFinalXML <> ''
+	IF @strHeaderXML <> ''
 	BEGIN
-		SELECT @strFinalXML = '<root><data>' + @strFinalXML + '</data></root>'
+		SELECT @strFinalXML = '<root><data>' + @strHeaderXML + '</data></root>'
 
 		DELETE
 		FROM @tblOutput
