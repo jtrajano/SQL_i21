67409e758d54250a925da82cade4c3f6abc204a0
IRELY\JMendones <jesmar.mendones@irely.com>
2021-04-08 13:34:36 +0800

    SM-5124

--- i21Database/Scripts/Post-Deployment/SM/DefaultData/1_MasterMenu.sql
+++ i21Database/Scripts/Post-Deployment/SM/DefaultData/1_MasterMenu.sql
@@ -10,10 +10,8 @@
 GO
 	/* UPDATE ENTITY CREDENTIAL CONCURRENCY */
 
-	
-	IF EXISTS(SELECT TOP 1 1 FROM tblSMMasterMenu WHERE strMenuName = 'Email Log' AND strModuleName = 'System Manager' AND intParentMenuID = (
-		SELECT intMenuID FROM tblSMMasterMenu WHERE strModuleName = 'System Manager' AND strMenuName = 'Maintenance' AND intParentMenuID = (SELECT intMenuID FROM tblSMMasterMenu WHERE strMenuName = 'System Manager' AND strModuleName = 'System Manager' AND intParentMenuID = 0)
-	))
+
+	IF EXISTS(SELECT TOP 1 1 FROM tblSMMasterMenu WHERE strMenuName = 'Lottery Inventory Report' AND strCommand = ' Store.view.LotteryInventoryReport')
 	BEGIN
 		EXEC uspSMIncreaseECConcurrency 0
 		
@@ -5184,6 +5182,12 @@ IF NOT EXISTS(SELECT TOP 1 1 FROM tblSMMasterMenu WHERE strMenuName = 'Lottery S
 	VALUES (N'Lottery Sales Report', N'Store', @StoreLotteryParentMenuId, N'Lottery Sales Report', N'Lottery', N'Screen', N'Store.view.LotterySalesReport', N'small-menu-lottery', 0, 0, 0, 1, 1, 3)	
 ELSE
 	UPDATE tblSMMasterMenu SET intSort = 0, strCommand = N'Store.view.LotterySalesReport' WHERE strMenuName = 'Lottery Sales Report' AND strModuleName = 'Store' AND intParentMenuID = @StoreLotteryParentMenuId
+
+IF NOT EXISTS(SELECT TOP 1 1 FROM tblSMMasterMenu WHERE strMenuName = 'Lottery Inventory Report' AND strModuleName = 'Store' AND intParentMenuID = @StoreLotteryParentMenuId)	
+	INSERT [dbo].[tblSMMasterMenu] ([strMenuName], [strModuleName], [intParentMenuID], [strDescription], [strCategory], [strType], [strCommand], [strIcon], [ysnVisible], [ysnExpanded], [ysnIsLegacy], [ysnLeaf], [intSort], [intConcurrencyId]) 
+	VALUES (N'Lottery Inventory Report', N'Store', @StoreLotteryParentMenuId, N'Lottery Inventory Report', N'Lottery', N'Screen', N'Store.view.LotteryInventoryReport', N'small-menu-lottery', 0, 0, 0, 1, 1, 3)	
+ELSE
+	UPDATE tblSMMasterMenu SET intSort = 0, strCommand = N'Store.view.LotteryInventoryReport' WHERE strMenuName = 'Lottery Inventory Report' AND strModuleName = 'Store' AND intParentMenuID = @StoreLotteryParentMenuId
 
 --END LOTTERY
 
