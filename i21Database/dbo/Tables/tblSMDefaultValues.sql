734e45d9c8f9a89a61ffd72a069673449604487e
Ian Ocampo <ian.ocampo@irely.com>
2021-04-14 06:53:16 +0800

    FRM-9411

---
+++ i21Database/dbo/Tables/tblSMDefaultValues.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[tblSMDefaultValues] (
+    [intDefaultValueId] INT IDENTITY (1, 1) NOT NULL,
+    [strModule] NVARCHAR (50) COLLATE Latin1_General_CI_AS NOT NULL,
+    [strScreen] NVARCHAR (50) COLLATE Latin1_General_CI_AS NOT NULL,
+    [strField] NVARCHAR (50) COLLATE Latin1_General_CI_AS NOT NULL,
+    [strValue] NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NULL,
+    [intConcurrencyId] INT DEFAULT (1) NOT NULL,
+    CONSTRAINT [PK_tblSMDefaultValues] PRIMARY KEY CLUSTERED ([intDefaultValueId] ASC)
+);
+
