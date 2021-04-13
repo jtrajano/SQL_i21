4e044d45a7080122b7d036ecad3708fa40c82724
jayson_ramos <jayson.ramos@irely.com>
2021-04-13 15:39:16 +0800

    Revert "CT-5801"
    This reverts commit 9e1c46da1bbb1768ececd4a66b1e98b0fd029b31.

--- i21Database/dbo/Stored Procedures/uspCTLogSummary.sql
+++ i21Database/dbo/Stored Procedures/uspCTLogSummary.sql
@@ -3893,7 +3893,7 @@ BEGIN TRY
 					-- Priced 1000 | BD 0 /
 					-- Priced 500 | BD 0 /
 					-- Priced 400 | BD 100 /
-					IF (@_basis > 0 OR @_actual > 0) AND @ysnDirect <> 1 AND isnull(@ysnLoadBased,0) = 0
+					IF (@_basis > 0 OR @_actual > 0) AND @ysnDirect <> 1
 					BEGIN				
 						-- Basis Deliveries  
 						UPDATE @cbLogSpecific SET dblQty = CASE WHEN @ysnLoadBased = 1 THEN @_actual ELSE @_basis END,
