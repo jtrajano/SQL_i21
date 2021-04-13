9a5341459180b7659f3d9b3c8b90b47e38757375
kelvin_quinto <kelvin.quinto@irely.com>
2021-04-13 12:14:50 +0800

    AR-13155
    (cherry picked from commit 67191f96ad20740b722deab5812a585cfca1ebc2)

--- i21Database/Scripts/Post-Deployment/AR/DefaultData/13_FixOnOrderCommittedQuantity.sql
+++ i21Database/Scripts/Post-Deployment/AR/DefaultData/13_FixOnOrderCommittedQuantity.sql
@@ -13,42 +13,53 @@ FROM (
 		SELECT intItemId
 			 , intCompanyLocationId
 			 , dblCommitted = SUM(ISNULL(dblCommitted, 0))
-		FROM (SELECT SOD.intItemId
-					, SO.intCompanyLocationId			 
-					, dblCommitted = SUM(dbo.fnICConvertUOMtoStockUnit(SOD.intItemId, SOD.intItemUOMId, SOD.dblQtyOrdered)) 
-									- ISNULL(SUM(dbo.fnICConvertUOMtoStockUnit(ID.intItemId, ID.intItemUOMId, ID.dblQtyShipped)), 0)
-									- ISNULL(SUM(dbo.fnICConvertUOMtoStockUnit(ISHI.intItemId, ISHI.intItemUOMId, ISHI.dblQuantity)), 0)
-			FROM tblSOSalesOrderDetail SOD INNER JOIN tblSOSalesOrder SO 
-					ON SOD.intSalesOrderId = SO.intSalesOrderId
-				LEFT JOIN (tblARInvoiceDetail ID INNER JOIN tblARInvoice I 
-								ON ID.intInvoiceId = I.intInvoiceId 
-								AND I.ysnPosted = 1
-								AND ID.intSalesOrderDetailId NOT IN (SELECT ISHI.intLineNo FROM tblICInventoryShipmentItem ISHI
-												INNER JOIN tblICInventoryShipment ISH ON ISHI.intInventoryShipmentId = ISH.intInventoryShipmentId
-											WHERE dbo.fnIsStockTrackingItem(ISHI.intItemId) = 1
-												AND ISH.ysnPosted = 1
-												AND ISNULL(ISHI.intLineNo, 0) > 0))
-					ON SOD.intSalesOrderDetailId = ID.intSalesOrderDetailId
-				LEFT JOIN (tblICInventoryShipmentItem ISHI INNER JOIN tblICInventoryShipment ISH 
-								ON ISHI.intInventoryShipmentId = ISH.intInventoryShipmentId AND ISH.ysnPosted = 1) 
-					ON SOD.intSalesOrderDetailId = ISHI.intLineNo
+		FROM (
+			SELECT SOD.intItemId
+				 , SO.intCompanyLocationId		
+				 , dblCommitted = SUM(dbo.fnICConvertUOMtoStockUnit(SOD.intItemId, SOD.intItemUOMId, SOD.dblQtyOrdered) - ISNULL(ID.dblQtyShipped, 0) - ISNULL(ISHI.dblQuantity, 0))
+			FROM tblSOSalesOrderDetail SOD 
+			INNER JOIN tblSOSalesOrder SO ON SOD.intSalesOrderId = SO.intSalesOrderId
+			LEFT JOIN (
+				SELECT ID.intSalesOrderDetailId
+					 , dblQtyShipped = dbo.fnICConvertUOMtoStockUnit(ID.intItemId, ID.intItemUOMId, ID.dblQtyShipped)
+				FROM tblARInvoiceDetail ID 
+				INNER JOIN tblARInvoice I ON ID.intInvoiceId = I.intInvoiceId
+				LEFT JOIN (
+					SELECT ISHI.intLineNo 
+					FROM tblICInventoryShipmentItem ISHI
+					INNER JOIN tblICInventoryShipment ISH ON ISHI.intInventoryShipmentId = ISH.intInventoryShipmentId
+					WHERE dbo.fnIsStockTrackingItem(ISHI.intItemId) = 1
+					  AND ISH.ysnPosted = 1
+					  AND ISNULL(ISHI.intLineNo, 0) > 0
+				) SHIP ON ID.intSalesOrderDetailId = SHIP.intLineNo
+				WHERE I.ysnPosted = 1
+					AND ISNULL(SHIP.intLineNo, 0) = 0
+			) ID ON SOD.intSalesOrderDetailId = ID.intSalesOrderDetailId
+			LEFT JOIN (
+				SELECT intLineNo
+					 , dblQuantity = dbo.fnICConvertUOMtoStockUnit(ISHI.intItemId, ISHI.intItemUOMId, ISHI.dblQuantity)
+				FROM tblICInventoryShipmentItem ISHI 
+				INNER JOIN tblICInventoryShipment ISH ON ISHI.intInventoryShipmentId = ISH.intInventoryShipmentId 
+				WHERE ISH.ysnPosted = 1
+			) ISHI ON SOD.intSalesOrderDetailId = ISHI.intLineNo
 			WHERE dbo.fnIsStockTrackingItem(SOD.intItemId) = 1		  
-				AND SO.strTransactionType = 'Order'
+			  AND SO.strTransactionType = 'Order'
 			GROUP BY SOD.intItemId, SO.intCompanyLocationId
 
 			UNION ALL
 
-			SELECT ID.intItemId
-					, I.intCompanyLocationId
-					, dblCommitted = SUM(dbo.fnICConvertUOMtoStockUnit(ID.intItemId, ID.intItemUOMId, ID.dblQtyShipped))
-			FROM tblARInvoiceDetail ID
-				INNER JOIN tblARInvoice I ON ID.intInvoiceId = I.intInvoiceId
-			WHERE dbo.fnIsStockTrackingItem(ID.intItemId) = 1
-				AND I.ysnPosted = 0
-				AND I.strTransactionType NOT IN ('Service Charge', 'Credit Memo')
-				AND ISNULL(ID.intInventoryShipmentItemId, 0) = 0
-				AND ISNULL(ID.intSalesOrderDetailId, 0) = 0
-			GROUP BY ID.intItemId, I.intCompanyLocationId) TBL
+            SELECT ID.intItemId
+                    , I.intCompanyLocationId
+                    , dblCommitted = SUM(dbo.fnICConvertUOMtoStockUnit(ID.intItemId, ID.intItemUOMId, ID.dblQtyShipped))
+            FROM tblARInvoiceDetail ID
+                INNER JOIN tblARInvoice I ON ID.intInvoiceId = I.intInvoiceId
+            WHERE dbo.fnIsStockTrackingItem(ID.intItemId) = 1
+                AND I.ysnPosted = 0
+                AND I.strTransactionType NOT IN ('Service Charge', 'Credit Memo')
+                AND ISNULL(ID.intInventoryShipmentItemId, 0) = 0
+                AND ISNULL(ID.intSalesOrderDetailId, 0) = 0
+            GROUP BY ID.intItemId, I.intCompanyLocationId
+		) TBL
 		GROUP BY intItemId, intCompanyLocationId
 	) AS SO
 	LEFT JOIN tblICItemLocation IIL ON SO.intItemId = IIL.intItemId AND SO.intCompanyLocationId = IIL.intLocationId
@@ -76,42 +87,53 @@ FROM (
 		SELECT intItemId
 			 , intCompanyLocationId
 			 , dblCommitted = SUM(ISNULL(dblCommitted, 0))
-		FROM (SELECT SOD.intItemId
-					, SO.intCompanyLocationId			 
-					, dblCommitted = SUM(dbo.fnICConvertUOMtoStockUnit(SOD.intItemId, SOD.intItemUOMId, SOD.dblQtyOrdered)) 
-									- ISNULL(SUM(dbo.fnICConvertUOMtoStockUnit(ID.intItemId, ID.intItemUOMId, ID.dblQtyShipped)), 0)
-									- ISNULL(SUM(dbo.fnICConvertUOMtoStockUnit(ISHI.intItemId, ISHI.intItemUOMId, ISHI.dblQuantity)), 0)
-			FROM tblSOSalesOrderDetail SOD INNER JOIN tblSOSalesOrder SO 
-					ON SOD.intSalesOrderId = SO.intSalesOrderId
-				LEFT JOIN (tblARInvoiceDetail ID INNER JOIN tblARInvoice I 
-								ON ID.intInvoiceId = I.intInvoiceId 
-								AND I.ysnPosted = 1
-								AND ID.intSalesOrderDetailId NOT IN (SELECT ISHI.intLineNo FROM tblICInventoryShipmentItem ISHI
-												INNER JOIN tblICInventoryShipment ISH ON ISHI.intInventoryShipmentId = ISH.intInventoryShipmentId
-											WHERE dbo.fnIsStockTrackingItem(ISHI.intItemId) = 1
-												AND ISH.ysnPosted = 1
-												AND ISNULL(ISHI.intLineNo, 0) > 0))
-					ON SOD.intSalesOrderDetailId = ID.intSalesOrderDetailId
-				LEFT JOIN (tblICInventoryShipmentItem ISHI INNER JOIN tblICInventoryShipment ISH 
-								ON ISHI.intInventoryShipmentId = ISH.intInventoryShipmentId AND ISH.ysnPosted = 1) 
-					ON SOD.intSalesOrderDetailId = ISHI.intLineNo
+		FROM (
+			SELECT SOD.intItemId
+				 , SO.intCompanyLocationId		
+				 , dblCommitted = SUM(dbo.fnICConvertUOMtoStockUnit(SOD.intItemId, SOD.intItemUOMId, SOD.dblQtyOrdered) - ISNULL(ID.dblQtyShipped, 0) - ISNULL(ISHI.dblQuantity, 0))
+			FROM tblSOSalesOrderDetail SOD 
+			INNER JOIN tblSOSalesOrder SO ON SOD.intSalesOrderId = SO.intSalesOrderId
+			LEFT JOIN (
+				SELECT ID.intSalesOrderDetailId
+					 , dblQtyShipped = dbo.fnICConvertUOMtoStockUnit(ID.intItemId, ID.intItemUOMId, ID.dblQtyShipped)
+				FROM tblARInvoiceDetail ID 
+				INNER JOIN tblARInvoice I ON ID.intInvoiceId = I.intInvoiceId
+				LEFT JOIN (
+					SELECT ISHI.intLineNo 
+					FROM tblICInventoryShipmentItem ISHI
+					INNER JOIN tblICInventoryShipment ISH ON ISHI.intInventoryShipmentId = ISH.intInventoryShipmentId
+					WHERE dbo.fnIsStockTrackingItem(ISHI.intItemId) = 1
+					  AND ISH.ysnPosted = 1
+					  AND ISNULL(ISHI.intLineNo, 0) > 0
+				) SHIP ON ID.intSalesOrderDetailId = SHIP.intLineNo
+				WHERE I.ysnPosted = 1
+					AND ISNULL(SHIP.intLineNo, 0) = 0
+			) ID ON SOD.intSalesOrderDetailId = ID.intSalesOrderDetailId
+			LEFT JOIN (
+				SELECT intLineNo
+					 , dblQuantity = dbo.fnICConvertUOMtoStockUnit(ISHI.intItemId, ISHI.intItemUOMId, ISHI.dblQuantity)
+				FROM tblICInventoryShipmentItem ISHI 
+				INNER JOIN tblICInventoryShipment ISH ON ISHI.intInventoryShipmentId = ISH.intInventoryShipmentId 
+				WHERE ISH.ysnPosted = 1
+			) ISHI ON SOD.intSalesOrderDetailId = ISHI.intLineNo
 			WHERE dbo.fnIsStockTrackingItem(SOD.intItemId) = 1		  
-				AND SO.strTransactionType = 'Order'
+			  AND SO.strTransactionType = 'Order'
 			GROUP BY SOD.intItemId, SO.intCompanyLocationId
 
 			UNION ALL
 
-			SELECT ID.intItemId
-					, I.intCompanyLocationId
-					, dblCommitted = SUM(dbo.fnICConvertUOMtoStockUnit(ID.intItemId, ID.intItemUOMId, ID.dblQtyShipped))
-			FROM tblARInvoiceDetail ID
-				INNER JOIN tblARInvoice I ON ID.intInvoiceId = I.intInvoiceId
-			WHERE dbo.fnIsStockTrackingItem(ID.intItemId) = 1
-				AND I.ysnPosted = 0
-				AND I.strTransactionType NOT IN ('Service Charge', 'Credit Memo')
-				AND ISNULL(ID.intInventoryShipmentItemId, 0) = 0
-				AND ISNULL(ID.intSalesOrderDetailId, 0) = 0
-			GROUP BY ID.intItemId, I.intCompanyLocationId) TBL
+            SELECT ID.intItemId
+                    , I.intCompanyLocationId
+                    , dblCommitted = SUM(dbo.fnICConvertUOMtoStockUnit(ID.intItemId, ID.intItemUOMId, ID.dblQtyShipped))
+            FROM tblARInvoiceDetail ID
+                INNER JOIN tblARInvoice I ON ID.intInvoiceId = I.intInvoiceId
+            WHERE dbo.fnIsStockTrackingItem(ID.intItemId) = 1
+                AND I.ysnPosted = 0
+                AND I.strTransactionType NOT IN ('Service Charge', 'Credit Memo')
+                AND ISNULL(ID.intInventoryShipmentItemId, 0) = 0
+                AND ISNULL(ID.intSalesOrderDetailId, 0) = 0
+            GROUP BY ID.intItemId, I.intCompanyLocationId
+		) TBL
 		GROUP BY intItemId, intCompanyLocationId
 	) AS SO
 	LEFT JOIN tblICItemLocation IIL ON SO.intItemId = IIL.intItemId AND SO.intCompanyLocationId = IIL.intLocationId
