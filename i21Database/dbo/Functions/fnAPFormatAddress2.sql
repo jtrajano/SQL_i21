67c0f82678a0772cb4e686cf9da71d85c60485f0
Jopet Lambinicio <jopet.lambinicio@irely.com>
2021-04-15 13:19:28 +0800

    Revert "AP-9446"
    This reverts commit e33dbe9329d0fb7f64d9486ba36953c885d71e84.

--- i21Database/dbo/Functions/fnAPFormatAddress2.sql
+++ i21Database/dbo/Functions/fnAPFormatAddress2.sql
@@ -5,7 +5,6 @@
 	@strShipToAttn  NVARCHAR(100),
 	@strAddress NVARCHAR(100),
 	@strCity NVARCHAR(30),
-	@strState NVARCHAR(50),
 	@strZipCode NVARCHAR(12),
 	@strCountry NVARCHAR(25),
 	@strPhone NVARCHAR(25)
@@ -20,7 +19,6 @@ BEGIN
 	SET @strShipToAttn = CASE WHEN @strShipToAttn = '' THEN NULL ELSE @strShipToAttn END	
 	SET @strAddress = CASE WHEN @strAddress = '' THEN NULL ELSE @strAddress END	
 	SET @strCity = CASE WHEN @strCity = '' THEN NULL ELSE @strCity END
-	SET @strState = CASE WHEN @strState = '' THEN NULL ELSE @strState END
 	SET @strZipCode = CASE WHEN @strZipCode = '' THEN NULL ELSE @strZipCode END
 	SET @strCountry = CASE WHEN @strCountry = '' THEN NULL ELSE @strCountry END
 	SET @strPhone = CASE WHEN @strPhone = '' THEN NULL ELSE @strPhone END  
@@ -32,7 +30,6 @@ BEGIN
 				 + ISNULL(RTRIM(@strAddress) + CHAR(13) + char(10), '')
 				 + ISNULL('' + RTRIM(@strZipCode) + ' ', '')
 				 + ISNULL(RTRIM(@strCity) + CHAR(10), '')
-				 + ISNULL(RTRIM(@strState) + CHAR(13) + char(10), '')
 				 + ISNULL('' + RTRIM(@strCountry) + CHAR(13) + char(10), '')
 				 + ISNULL(RTRIM(@strPhone)+ CHAR(13) + char(10), '')
 	RETURN @fullAddress	
