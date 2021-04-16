d2806dce69a973811d9f7fc240929cf8b3a959f4
Feb Montefrio <feb.montefrio@irely.com>
2021-04-16 16:31:58 +0800

    [B] IC-9367

--- i21Database/dbo/Stored Procedures/uspICRebuildInventoryValuation.sql
+++ i21Database/dbo/Stored Procedures/uspICRebuildInventoryValuation.sql
@@ -1738,11 +1738,13 @@ BEGIN
 				--PRINT 'Reposting Bill Cost Adjustments: ' + @strTransactionId
 				
 				-- uspICRepostBillCostAdjustment creates and posts it own g/l entries 
-				EXEC uspICRepostBillCostAdjustment
+				EXEC @intReturnValue = uspICRepostBillCostAdjustment
 					@strTransactionId
 					,@strBatchId
 					,@intEntityUserSecurityId
 					,@ysnRegenerateBillGLEntries
+
+				IF @intReturnValue <> 0 GOTO _EXIT_WITH_ERROR
 			END
 
 			ELSE IF EXISTS (SELECT 1 WHERE @strTransactionType IN ('Cost Adjustment') AND @strTransactionForm IN ('Settle Storage'))
@@ -1750,10 +1752,12 @@ BEGIN
 				--PRINT 'Reposting Settle Storage Cost Adjustments: ' + @strTransactionId
 				
 				-- uspICRepostSettleStorageCostAdjustment creates and posts it own g/l entries 
-				EXEC uspICRepostSettleStorageCostAdjustment
+				EXEC @intReturnValue = uspICRepostSettleStorageCostAdjustment
 					@strTransactionId
 					,@strBatchId
 					,@intEntityUserSecurityId
+
+				IF @intReturnValue <> 0 GOTO _EXIT_WITH_ERROR
 			END
 
 			ELSE IF EXISTS (SELECT 1 WHERE @strTransactionType IN ('Cost Adjustment') AND @strTransactionForm IN ('Produce', 'Consume'))
@@ -1761,10 +1765,12 @@ BEGIN
 				--PRINT 'Reposting MFG Cost Adjustments: ' + @strTransactionId
 				
 				-- uspICRepostSettleStorageCostAdjustment creates and posts it own g/l entries 
-				EXEC uspMFRepostCostAdjustment
+				EXEC @intReturnValue = uspMFRepostCostAdjustment
 					@strBatchId
 					,@intEntityUserSecurityId
 					,@dtmDate
+
+				IF @intReturnValue <> 0 GOTO _EXIT_WITH_ERROR
 			END
 
 			-- Repost 'Consume' and 'Produce'
