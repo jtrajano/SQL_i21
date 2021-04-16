fadf72035426e62da28b9e670bc9670f69031ceb
Feb Montefrio <feb.montefrio@irely.com>
2021-04-16 16:31:58 +0800

    [B] IC-9367
    # Conflicts:
    #	i21Database/dbo/Functions/fnICGetErrorMessage.sql

--- i21Database/dbo/Stored Procedures/uspICRebuildInventoryValuation.sql
+++ i21Database/dbo/Stored Procedures/uspICRebuildInventoryValuation.sql
@@ -1740,11 +1740,13 @@ BEGIN
 				--PRINT 'Reposting Bill Cost Adjustments: ' + @strTransactionId
 				
 				-- uspICRepostBillCostAdjustment creates and posts it own g/l entries 
-				EXEC uspICRepostBillCostAdjustment
+				EXEC @intReturnValue = uspICRepostBillCostAdjustment
 					@strTransactionId
 					,@strBatchId
 					,@intEntityUserSecurityId
 					,@ysnRegenerateBillGLEntries
+
+				IF @intReturnValue <> 0 GOTO _EXIT_WITH_ERROR
 			END
 
 			ELSE IF EXISTS (SELECT 1 WHERE @strTransactionType IN ('Cost Adjustment') AND @strTransactionForm IN ('Settle Storage'))
@@ -1752,10 +1754,12 @@ BEGIN
 				--PRINT 'Reposting Settle Storage Cost Adjustments: ' + @strTransactionId
 				
 				-- uspICRepostSettleStorageCostAdjustment creates and posts it own g/l entries 
-				EXEC uspICRepostSettleStorageCostAdjustment
+				EXEC @intReturnValue = uspICRepostSettleStorageCostAdjustment
 					@strTransactionId
 					,@strBatchId
 					,@intEntityUserSecurityId
+
+				IF @intReturnValue <> 0 GOTO _EXIT_WITH_ERROR
 			END
 
 			ELSE IF EXISTS (SELECT 1 WHERE @strTransactionType IN ('Cost Adjustment') AND @strTransactionForm IN ('Produce', 'Consume'))
@@ -1763,10 +1767,12 @@ BEGIN
 				--PRINT 'Reposting MFG Cost Adjustments: ' + @strTransactionId
 				
 				-- uspICRepostSettleStorageCostAdjustment creates and posts it own g/l entries 
-				EXEC uspMFRepostCostAdjustment
+				EXEC @intReturnValue = uspMFRepostCostAdjustment
 					@strBatchId
 					,@intEntityUserSecurityId
 					,@dtmDate
+
+				IF @intReturnValue <> 0 GOTO _EXIT_WITH_ERROR
 			END
 
 			-- Repost 'Consume' and 'Produce'
