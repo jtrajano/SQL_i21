aa29a89ee2a13d6bf45bd2a09853719b6af471c0
kelvin_quinto <kelvin.quinto@irely.com>
2021-04-16 20:46:04 +0800

    AR-13176
    (cherry picked from commit be7a20968189f87be297a5d74d68f661f4872a8c)

--- i21Database/dbo/Stored Procedures/uspARPopulateItemsForCosting.sql
+++ i21Database/dbo/Stored Procedures/uspARPopulateItemsForCosting.sql
@@ -356,6 +356,7 @@ WHERE
 	AND ISNULL(ARIC.[intComponentItemId],0) <> 0
 	AND ARID.[strTransactionType] <> 'Debit Memo'
 	AND ISNULL(ARIC.[strType],'') NOT IN ('Finished Good','Comment')
+	AND ARID.[strItemType] NOT IN ('Non-Inventory','Service','Other Charge','Software','Bundle','Comment')
 	AND (ARID.[intStorageScheduleTypeId] IS NULL OR ISNULL(ARID.[intStorageScheduleTypeId],0) = 0)	
 	AND (ARID.intLoadDetailId IS NULL OR (ARID.intLoadDetailId IS NOT NULL AND ISNULL(LGL.[intPurchaseSale], 0) NOT IN (2, 3)))
 
