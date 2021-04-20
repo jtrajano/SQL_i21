1c467735f23b6a78367fd56c6df85a65f244e1f5
jayson_ramos <jayson.ramos@irely.com>
2021-04-20 11:46:09 +0800

    CT-5822

--- i21Database/dbo/Stored Procedures/uspCTLoadContractDetails.sql
+++ i21Database/dbo/Stored Procedures/uspCTLoadContractDetails.sql
@@ -366,17 +366,27 @@ BEGIN TRY
 									ELSE CASE WHEN CT.ysnLoad = 1 THEN ISNULL(CD.intNoOfLoad, 0) - ISNULL(CD.dblBalanceLoad, 0)
 											ELSE ISNULL(CD.dblQuantity, 0) - ISNULL(CD.dblBalance, 0) END * CD.dblQuantityPerLoad END
 		*/
-		  , dblAppliedLoadQty =  	CASE
-									WHEN Shipment.dblQuantity > 0 THEN Shipment.dblDestinationQuantity + ISNULL(Invoice.dblQuantity,0)
-									WHEN Bill.dblQuantity > 0 THEN Bill.dblQuantity
+	    , dblAppliedLoadQty =   case
+								when CT.ysnLoad = 1
+								then
+									case
+									when Bill.dblQuantity > 0 THEN Bill.dblQuantity
 									WHEN Invoice.dblQuantity > 0  THEN Invoice.dblQuantity
-									ELSE
-										CASE
-										WHEN CT.ysnLoad = 1 THEN ISNULL(CD.intNoOfLoad, 0) - ISNULL(CD.dblBalanceLoad, 0)
-										ELSE ISNULL(CD.dblQuantity, 0) - ISNULL(CD.dblBalance, 0)
-										END
-										* CD.dblQuantityPerLoad
-									END
+									else (ISNULL(CD.intNoOfLoad, 0) - ISNULL(CD.dblBalanceLoad, 0) ) * CD.dblQuantityPerLoad 
+									end
+								else
+									CASE  
+									WHEN Shipment.dblQuantity > 0 THEN Shipment.dblDestinationQuantity + ISNULL(Invoice.dblQuantity,0)  
+									WHEN Bill.dblQuantity > 0 THEN Bill.dblQuantity  
+									WHEN Invoice.dblQuantity > 0  THEN Invoice.dblQuantity  
+									ELSE  
+										CASE  
+										WHEN CT.ysnLoad = 1 THEN ISNULL(CD.intNoOfLoad, 0) - ISNULL(CD.dblBalanceLoad, 0)  
+										ELSE ISNULL(CD.dblQuantity, 0) - ISNULL(CD.dblBalance, 0)  
+										END  
+										* CD.dblQuantityPerLoad  
+									END 
+								end
 		, dblExchangeRate = dbo.fnCTGetCurrencyExchangeRate(CD.intContractDetailId, 0)
 		, IM.intProductTypeId
 		, ysnItemUOMIdExist = CAST(1 AS BIT)
