f9015be625f6737d9fa7f82fd5475df90acbcaa6
Jeff Trajano <jeffrey.trajano@irely.com>
2021-04-20 15:07:57 +0800

    GL-8119 Remove Procedure uspGLErrorMessages

--- i21Database/Scripts/Post-Deployment/Common/ErrorMessages.sql
+++ i21Database/Scripts/Post-Deployment/Common/ErrorMessages.sql
@@ -1,6 +1,5 @@
 ﻿-- Add the SQL Server custom messages
 EXEC dbo.uspSMErrorMessages
-EXEC dbo.uspGLErrorMessages
 EXEC dbo.uspCMErrorMessages
 EXEC dbo.uspICErrorMessages
 EXEC dbo.uspMFGErrorMessages
