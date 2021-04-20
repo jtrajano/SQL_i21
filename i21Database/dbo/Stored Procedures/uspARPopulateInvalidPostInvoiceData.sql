1e5ea8589e94e44baae07a3ffa7a407255263575
Jeremiah Villanueva <jeremiah.villanueva@irely.com>
2021-04-20 15:43:42 +0800

    AR-13132

--- i21Database/dbo/Stored Procedures/uspARPopulateInvalidPostInvoiceData.sql
+++ i21Database/dbo/Stored Procedures/uspARPopulateInvalidPostInvoiceData.sql
@@ -1866,7 +1866,7 @@ BEGIN
 		,[intItemLocationId]
 		,[intItemUOMId]
 		,[dtmDate]
-		,CASE WHEN [strType] IN ('CF Tran','POS') THEN ABS([dblQty]) ELSE [dblQty] END
+		,CASE WHEN [strType] IN ('CF Tran') THEN ABS([dblQty]) ELSE [dblQty] END
 		,[dblUOMQty]
 		,[dblCost]
 		,[dblValue]
