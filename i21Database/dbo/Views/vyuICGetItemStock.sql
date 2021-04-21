691d54ccf73c0f6fb155c01ce4d3d12a0fc2bb23
Samuel Marvin Aguilos <samuel.aguilos@irely.com>
2021-04-21 11:49:56 +0800

    IC-9249

--- i21Database/dbo/Views/vyuICGetItemStock.sql
+++ i21Database/dbo/Views/vyuICGetItemStock.sql
@@ -156,7 +156,8 @@ SELECT
 	Item.ysnLotWeightsRequired,
 	ysnHasAddOn = CAST(ISNULL(ItemAddOn.ysnHasAddOn, 0) AS BIT),
 	ysnHasSubstitute = CAST(ISNULL(ItemSubstitute.ysnHasSubstitute, 0) AS BIT),
-	ysnHasAddOnOtherCharge = CAST(ISNULL(AddOnOtherCharge.ysnHasAddOnOtherCharge, 0) AS BIT)
+	ysnHasAddOnOtherCharge = CAST(ISNULL(AddOnOtherCharge.ysnHasAddOnOtherCharge, 0) AS BIT),
+	dblStandardWeight = StockUOM.dblStandardWeight
 FROM	
 	tblICItem Item 
 	LEFT JOIN (
