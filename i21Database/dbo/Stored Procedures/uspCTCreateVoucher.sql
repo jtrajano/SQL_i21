f309e42b894b929b52ee7356c842d7fa5dad5291
jayson_ramos <jayson.ramos@irely.com>
2021-04-23 12:39:53 +0800

    CT-5824
    (cherry picked from commit 2f34ff619195ee4ca28c576b538786da323f473c)

--- i21Database/dbo/Stored Procedures/uspCTCreateVoucher.sql
+++ i21Database/dbo/Stored Procedures/uspCTCreateVoucher.sql
@@ -1094,10 +1094,13 @@ begin try
 				while (@intCreatedBillId is not null and @intCreatedBillId > 0)
 				begin
 
-					select top 1 @intInventoryReceiptItemId = intInventoryReceiptItemId, @ysnPostBill = 1 from @CreatedVoucher where intBillId = @intCreatedBillId ;
-					if exists (select top 1 1 from tblICInventoryReceiptItem ri, tblICInventoryReceipt ir where ri.intInventoryReceiptItemId = @intInventoryReceiptItemId and ir.intInventoryReceiptId = ri.intInventoryReceiptId and ir.intSourceType = 1)
+					select top 1 @intInventoryReceiptItemId = intInventoryReceiptItemId, @ysnPostBill = 1, @intCreatedBillDetailId = intBillDetailId from @CreatedVoucher where intBillId = @intCreatedBillId ;
+					if exists (select top 1 1 from tblICInventoryReceiptItem ri join tblICInventoryReceipt ir on ir.intInventoryReceiptId = ri.intInventoryReceiptId where ri.intInventoryReceiptItemId = @intInventoryReceiptItemId and ir.intSourceType = 1)
 					begin
-						select top 1 @ysnPostBill = isnull(v.ysnPostVoucher,0)from tblAPBill b, tblAPVendor v where b.intBillId = @intCreatedBillId and v.intEntityId = b.intEntityId
+						if exists (select top 1 1 from tblAPBillDetail bd join tblCTContractDetail cd on cd.intContractDetailId = bd.intContractDetailId where bd.intBillDetailId = @intCreatedBillDetailId and cd.intPricingTypeId in (1,6))
+						begin
+							select top 1 @ysnPostBill = isnull(v.ysnPostVoucher,0)from tblAPBill b join tblAPVendor v on v.intEntityId = b.intEntityVendorId where b.intBillId = @intCreatedBillId
+						end
 					end
 					if (@ysnPostBill = 1)
 					begin
