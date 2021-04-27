fa9400afa438a94c1c63a46ad15ad16d61f969d8
Jopet Lambinicio <jopet.lambinicio@irely.com>
2021-04-27 23:04:57 +0800

    BB-125

--- i21Database/dbo/Views/vyuBBBuybackExport.sql
+++ i21Database/dbo/Views/vyuBBBuybackExport.sql
@@ -18,8 +18,6 @@ AS
 		,P.strProgramDescription
 		,P.strVendorProgramId
 		,E.strVendorCustomerLocation
-		,E.strVendorShipTo
-		,E.strVendorSoldTo
 		,M.intVendorSetupId
 	FROM tblARInvoice A
 	INNER JOIN tblARInvoiceDetail B
