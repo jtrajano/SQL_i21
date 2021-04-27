6c9d83d0a47eda55bc7bd14ca7332a8d43e8d1db
jayson_ramos <jayson.ramos@irely.com>
2021-04-27 16:34:35 +0800

    CT-5838
    (cherry picked from commit f9ace35e328647637a3fee8098319ff42d5b08f9)

--- i21Database/dbo/Stored Procedures/uspCTLogSummary.sql
+++ i21Database/dbo/Stored Procedures/uspCTLogSummary.sql
@@ -2946,6 +2946,7 @@ BEGIN TRY
 					and cter.intTransactionReferenceDetailId = curr.intTransactionReferenceDetailId
 					and (cter.dblQty * -1) = curr.dblQty
 					and cter.intId <> curr.intId
+					and cter.strTransactionType = curr.strTransactionType
 			where
 				curr.intId = @intId
 		)
