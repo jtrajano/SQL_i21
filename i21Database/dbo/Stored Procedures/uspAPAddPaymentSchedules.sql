9ed386db71af98b75aa069009edd3e2b26facfee
Darwin Estrada <darwin.estrada@irely.com>
2021-04-29 10:21:17 +0800

    AP-9609, AP-9618 resolved syntax errors and misspelled fields.

--- i21Database/dbo/Stored Procedures/uspAPAddPaymentSchedules.sql
+++ i21Database/dbo/Stored Procedures/uspAPAddPaymentSchedules.sql
@@ -9,15 +9,15 @@ DECLARE @invalidPaySchedules AS TABLE(intBillId INT, strError NVARCHAR(500))
 INSERT INTO @invalidPaySchedules
 --INSERT THOSE PAYMENT NOT EQUAL WITH AMOUNT DUE
 SELECT
-	A.inBillId,
+	A.intBillId,
 	'Payment Schedule not equal with the voucher (' + B.strBillId + ') amount due.'
 FROM @paySchedules A
 INNER JOIN tblAPBill B ON A.intBillId = B.intBillId
-GROUP BY A.intBillId
-HAVING SUM(A.dblPayment) != B.dblTotal
+GROUP BY A.intBillId, B.strBillId
+HAVING SUM(A.dblPayment) != max(B.dblTotal)
 UNION ALL
 SELECT --EXCLUDE PAID
-	A.inBillId,
+	A.intBillId,
 	'Voucher (' + B.strBillId + ') is already paid.'
 FROM @paySchedules A
 INNER JOIN tblAPBill B ON A.intBillId = B.intBillId
