79ca7f7b2746191eda43d9c66903c5e2be49fe78
Premkumar S <prem.selladurai@irely.com>
2021-05-02 21:55:29 +0530

    IN-825

---
+++ i21Database/dbo/Tables/tblIPInventoryAdjustment.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE tblIPInventoryAdjustment (
+	intInventoryAdjustmentId INT identity(1, 1)
+	,dtmCreatedDate DATETIME
+	,intContractHeaderId INT
+	,intContractDetailId INT
+	,intLoadId INT
+	,intLoadDetailId INT
+	,intLoadContainerId INT
+	,intInventoryReceiptId INT
+	,strLotNumber NVARCHAR(50) COLLATE Latin1_General_CI_AS
+	,dblNet NUMERIC(18, 6)
+	,dblGross NUMERIC(18, 6)
+	,CONSTRAINT PK_tblIPInventoryAdjustment PRIMARY KEY (intInventoryAdjustmentId )
+	)
