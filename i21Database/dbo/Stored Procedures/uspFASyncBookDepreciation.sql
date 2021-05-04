5c83150e85ba550a13556a2f5b14317913d326a2
Jeff Trajano <jeffrey.trajano@irely.com>
2021-05-04 14:42:06 +0800

    FA-187

--- i21Database/dbo/Stored Procedures/uspFASyncBookDepreciation.sql
+++ i21Database/dbo/Stored Procedures/uspFASyncBookDepreciation.sql
@@ -1,36 +1,23 @@
-CREATE PROCEDURE uspFASyncBookDepreciation
-(
-	@intAssetId INT
-
-)
-AS
-
-DECLARE @intBookDepreciationId INT = NULL
-SELECT TOP 1 @intBookDepreciationId = intBookDepreciationId FROM tblFABookDepreciation WHERE intAssetId = @intAssetId AND intBookId =1
-
-IF @intBookDepreciationId IS NOT NULL
-BEGIN
-	UPDATE A SET
-	dblCost = B.dblCost
-	, dblSalvageValue= B.dblSalvageValue
-	, dtmPlacedInService= B.dtmDateInService
-	FROM
-	tblFABookDepreciation A JOIN tblFAFixedAsset B on A.intDepreciationMethodId = B.intDepreciationMethodId
-	WHERE intBookDepreciationId = @intBookDepreciationId
-END
-ELSE
-BEGIN
-	DECLARE @intDepreciationMethodId INT
-	SELECT TOP 1 @intDepreciationMethodId =A.intDepreciationMethodId FROM 
-	tblFADepreciationMethod A
-	JOIN tblFAFixedAsset FA ON FA.intDepreciationMethodId = A.intDepreciationMethodId
-	LEFT JOIN tblFABookDepreciation B ON B.intDepreciationMethodId= A.intDepreciationMethodId
-	WHERE FA.intAssetId = @intAssetId AND ISNULL(B.intBookId,1) = 1
-
-
-	IF @intDepreciationMethodId IS NOT NULL
-	INSERT INTO tblFABookDepreciation(intAssetId, intBookId, intDepreciationMethodId, dblCost, dblSalvageValue, dtmPlacedInService, intConcurrencyId)
-	SELECT A.intAssetId, 1, @intDepreciationMethodId, A.dblCost, A.dblSalvageValue, A.dtmDateInService,1 FROM tblFAFixedAsset A
-	WHERE @intAssetId = A.intAssetId
-
+CREATE PROCEDURE uspFASyncBookDepreciation (  @intAssetId INT )  
+AS  
+DECLARE @intBookDepreciationId INT = NULL , @intDepreciationMethodId INT
+SELECT TOP 1 @intBookDepreciationId = intBookDepreciationId FROM tblFABookDepreciation WHERE intAssetId = @intAssetId AND intBookId =1  
+SELECT TOP 1 @intDepreciationMethodId = intDepreciationMethodId FROM tblFAFixedAsset where intAssetId = @intAssetId
+  
+IF EXISTS(SELECT 1 FROM tblFABookDepreciation WHERE intAssetId = @intAssetId AND intBookId =1 )
+BEGIN  
+	UPDATE tblFABookDepreciation SET intDepreciationMethodId = @intDepreciationMethodId WHERE intBookDepreciationId = @intBookDepreciationId
+	UPDATE A SET   
+	dblCost = B.dblCost  
+	, dblSalvageValue= B.dblSalvageValue  
+	, dtmPlacedInService= B.dtmDateInService  
+	FROM  
+	tblFABookDepreciation A JOIN tblFAFixedAsset B on A.intDepreciationMethodId = B.intDepreciationMethodId  
+	WHERE intBookDepreciationId = @intBookDepreciationId  
+END  
+ELSE  
+BEGIN  
+	INSERT INTO tblFABookDepreciation(intAssetId, intBookId, intDepreciationMethodId, dblCost, dblSalvageValue, dtmPlacedInService, intConcurrencyId)  
+	SELECT A.intAssetId, 1, @intDepreciationMethodId, A.dblCost, A.dblSalvageValue, A.dtmDateInService,1 FROM tblFAFixedAsset A  
+	WHERE @intAssetId = A.intAssetId  
 END
