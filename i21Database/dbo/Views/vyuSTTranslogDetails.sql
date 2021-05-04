649c789bee9cfa081cb9271ed824e1d6bdbc202b
nelsonreyes19 <nelson.reyes@irely.com>
2021-05-04 15:31:09 +0800

    ST-1902
    Add new fields and update this view

--- i21Database/dbo/Views/vyuSTTranslogDetails.sql
+++ i21Database/dbo/Views/vyuSTTranslogDetails.sql
@@ -5,26 +5,34 @@ FROM
 (
 	SELECT DISTINCT 
 		CAST(TR.intTermMsgSN AS NVARCHAR(MAX)) + '0' +  CAST(TR.intTermMsgSNterm AS NVARCHAR(MAX)) + '0' + CAST(TR.intStoreId AS NVARCHAR(MAX)) 
-	   --+ CAST(USec.intEntityId AS NVARCHAR(MAX)) 
-	   COLLATE Latin1_General_CI_AS AS strUniqueId
+			--+ CAST(USec.intEntityId AS NVARCHAR(MAX)) 
+			COLLATE Latin1_General_CI_AS AS strUniqueId
        , TR.intTrlDeptNumber
 	   , TR.strTrlDept
 	   , TR.strTrlNetwCode
 	   , TR.strTrlUPC
+	   , TR.strTrpPaycode
+	   , TR.strTrpCardInfoTrpcCCName
 	   , TR.strTrlDesc
-	   , TR. dblTrlQty
+	   , TR.dblTrlQty
 	   , TR.dblTrlUnitPrice
 	   , TR.dblTrlLineTot
 	   , TR.intTermMsgSN
-	   , TR.dtmDate
-	   , TR.intCashierPosNum
+	   , TR.dtmDate AS dtmDateTime
+	   , CONVERT(VARCHAR, TR.dtmDate, 23) AS dtmDate
+	   , CAST(TR.intCashierPosNum AS INT) AS intCashierPosNum
+	   , CAST(ST.intStoreId AS INT) AS intStoreId
+	   , CAST(ST.intStoreNo AS INT) AS intStoreNo
+	   , CAST(TR.intTrTickNumPosNum AS INT) AS intTicketPosNum -- Ticket Number
+	   , CAST(TR.intTrTickNumTrSeq AS INT) AS intTicketTrSeq -- Ticket Number
+	   , TR.strTransType 
+	   , CAST(TR.intCashierSysId AS INT) AS intCashierSysId
+	   , TR.strCashier
 	   , RIGHT('0' + CONVERT(VARCHAR(2), DATEPART(HOUR, TR.dtmDate)), 2) as Hr
-	   -- , USec.intEntityId
+	   --, USec.intEntityId
 	FROM tblSTTranslogRebates TR
-	JOIN tblSTCheckoutHeader CH 
-		ON TR.intCheckoutId = CH.intCheckoutId
 	JOIN tblSTStore ST 
-		ON CH.intStoreId = ST.intStoreId 
+		ON TR.intStoreId = ST.intStoreId 
 	-- OUTER APPLY tblSMUserSecurity USec
 	-- INNER JOIN tblSMUserSecurityCompanyLocationRolePermission RolePerm
 		-- ON USec.intEntityId = RolePerm.intEntityId
