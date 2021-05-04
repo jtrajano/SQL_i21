52bfba269a042f1fa6746d7820caf7b1a606033c
Jeff Trajano <jeffrey.trajano@irely.com>
2021-05-04 14:42:06 +0800

    FA-187

--- i21Database/dbo/Views/vyuFADepreciationMethod.sql
+++ i21Database/dbo/Views/vyuFADepreciationMethod.sql
@@ -7,6 +7,9 @@ tblFADepreciationMethod A
 -- SHOWS IF DM WAS ALREADY USED TO DEPRECIATE ASSET
 OUTER APPLY(
     SELECT TOP 1 1 ysnHasDepreciatedAsset FROM 
-    tblFAFixedAsset WHERE intDepreciationMethodId = A.intDepreciationMethodId 
-    AND (ysnDepreciated = 1 OR ysnTaxDepreciated = 1)
+    tblFAFixedAsset  FA 
+    JOIN tblFABookDepreciation BD ON BD.intAssetId = FA.intAssetId
+    JOIN tblFADepreciationMethod DM ON DM.intDepreciationMethodId = BD.intDepreciationMethodId
+    WHERE BD.intDepreciationMethodId = A.intDepreciationMethodId 
+    AND (ISNULL(FA.ysnDepreciated,0) = 1 OR ISNULL(ysnTaxDepreciated,0) = 1)
 )U
