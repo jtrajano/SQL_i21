9dacca9481a7b11c59e1527613a4a8f4ac2e8fc5
Wendell Wayne Estrada <wendell.estrada@irely.com>
2021-05-05 15:56:55 +0800

    API-55 [Email Import] Item Contract

--- i21Database/dbo/Stored Procedures/uspRestApiSchemaTransformItemContract.sql
+++ i21Database/dbo/Stored Procedures/uspRestApiSchemaTransformItemContract.sql
@@ -1,4 +1,4 @@
-CREATE PROCEDURE [dbo].[uspRestApiSchemaTransformDollarContract] (@guiApiUniqueId UNIQUEIDENTIFIER)
+CREATE PROCEDURE [dbo].[uspRestApiSchemaTransformItemContract] (@guiApiUniqueId UNIQUEIDENTIFIER)
 AS
 
 DECLARE @Logs TABLE (strError NVARCHAR(500), strField NVARCHAR(100), strValue NVARCHAR(500), intLineNumber INT NULL, dblTotalAmount NUMERIC(18, 6), intLinePosition INT NULL, strLogLevel NVARCHAR(50))
@@ -6,28 +6,28 @@ DECLARE @Logs TABLE (strError NVARCHAR(500), strField NVARCHAR(100), strValue NV
 -- Validations
 INSERT INTO @Logs (strError, strField, strLogLevel, strValue)
 SELECT 'Cannot find the location: ''' + sc.strLocation + '''', 'Location', 'Error', sc.strLocation
-FROM tblRestApiSchemaDollarContract sc
+FROM tblRestApiSchemaItemContract sc
 LEFT JOIN tblSMCompanyLocation loc ON loc.strLocationName = sc.strLocation OR loc.strLocationNumber = sc.strLocation
 WHERE loc.intCompanyLocationId IS NULL
 	AND sc.guiApiUniqueId = @guiApiUniqueId
 
 INSERT INTO @Logs (strError, strField, strLogLevel, strValue)
 SELECT 'Cannot find the customer with Customer No.: ''' + sc.strCustomerNo + '''', 'Customer No', 'Error',  sc.strCustomerNo
-FROM tblRestApiSchemaDollarContract sc
+FROM tblRestApiSchemaItemContract sc
 LEFT JOIN vyuARCustomer customer ON customer.strCustomerNumber = sc.strCustomerNo OR customer.strName = sc.strCustomerNo
 WHERE customer.intEntityId IS NULL
 	AND sc.guiApiUniqueId = @guiApiUniqueId
 
 INSERT INTO @Logs (strError, strField, strLogLevel, strValue)
 SELECT 'Cannot find the salesperson: ''' + sc.strSalesperson + '''', 'Salesperson', 'Error',  sc.strSalesperson
-FROM tblRestApiSchemaDollarContract sc
+FROM tblRestApiSchemaItemContract sc
 LEFT JOIN vyuCTEntity sp ON sp.strEntityName = sc.strSalesperson OR sp.strEntityNumber = sc.strSalesperson
 WHERE sp.intEntityId IS NULL
 	AND sc.guiApiUniqueId = @guiApiUniqueId
 
 INSERT INTO @Logs (strError, strField, strLogLevel, strValue)
 SELECT 'Cannot find the currency: ''' + sc.strCurrency + '''', 'Currency', 'Error',  sc.strCurrency
-FROM tblRestApiSchemaDollarContract sc
+FROM tblRestApiSchemaItemContract sc
 LEFT JOIN tblSMCurrency currency ON currency.strCurrency = sc.strCurrency OR currency.strDescription = sc.strCurrency
 WHERE currency.intCurrencyID IS NULL
 	AND sc.guiApiUniqueId = @guiApiUniqueId
@@ -44,7 +44,6 @@ SELECT DISTINCT
     , sc.ysnIsSigned
     , sc.ysnIsPrinted
     , sc.dtmDueDate
-    , sc.dblContractValue
     , loc.intCompanyLocationId
     , currency.intCurrencyID
     , sp.intEntityId AS intSalespersonId
@@ -62,7 +61,7 @@ SELECT DISTINCT
     , sc.strSalesperson
     , sc.strContractText
     , sc.strLineOfBusiness
-FROM tblRestApiSchemaDollarContract sc
+FROM tblRestApiSchemaItemContract sc
 INNER JOIN vyuARCustomer customer ON customer.strCustomerNumber = sc.strCustomerNo OR customer.strName = sc.strCustomerNo
 INNER JOIN tblSMCompanyLocation loc ON loc.strLocationName = sc.strLocation OR loc.strLocationNumber = sc.strLocation
 INNER JOIN tblSMCurrency currency ON currency.strCurrency = sc.strCurrency OR currency.strDescription = sc.strCurrency
@@ -82,7 +81,6 @@ GROUP BY
     , sc.ysnIsSigned
     , sc.ysnIsPrinted
     , sc.dtmDueDate
-    , sc.dblContractValue
     , loc.intCompanyLocationId
     , currency.intCurrencyID
     , sp.intEntityId
@@ -141,7 +139,6 @@ FETCH NEXT FROM cur INTO
     , @ysnIsSigned
     , @ysnIsPrinted
     , @dtmDueDate
-    , @dblContractValue
     , @intCompanyLocationId
     , @intCurrencyID
     , @intSalespersonId
@@ -183,7 +180,6 @@ BEGIN
         , intContractTextId
         , intTermId
         , intLineOfBusinessId
-        , dblDollarValue
     )
     SELECT
           guiApiUniqueId = @guiApiUniqueId
@@ -204,20 +200,18 @@ BEGIN
         , intContractTextId = @intContractTextId
         , intTermId = @intTermID
         , intLineOfBusinessId = @intLineOfBusinessId
-        , dblDollarValue = @dblContractValue
 
     SET @intItemContractStagingId = SCOPE_IDENTITY()
 
     INSERT INTO @Logs (strError, strField, strLogLevel, strValue)
-    SELECT 'Cannot find the category: ''' + sc.strCategory + '''', 'Category', 'Error',  sc.strCategory
-    FROM tblRestApiSchemaDollarContract sc
-    LEFT JOIN tblICCategory c ON c.strCategoryCode = sc.strCategory OR c.strDescription = sc.strCategory
+    SELECT 'Cannot find the Item No.: ''' + sc.strItemNo + '''', 'Item No', 'Error',  sc.strItemNo
+    FROM tblRestApiSchemaItemContract sc
+    LEFT JOIN tblICItem i ON i.strItemNo = sc.strItemNo
     WHERE sc.guiApiUniqueId = @guiApiUniqueId
-        AND c.intCategoryId IS NULL
+        AND i.intItemId IS NULL
         AND ISNULL(sc.dtmContractDate, @Date) = ISNULL(@dtmContractDate, @Date)
         AND ISNULL(sc.dtmExpirationDate, @Date) = ISNULL(@dtmExpirationDate, @Date)
         AND ISNULL(sc.dtmDueDate, @Date) = ISNULL(@dtmDueDate, @Date)
-        AND sc.dblContractValue = @dblContractValue
         AND ISNULL(sc.ysnIsSigned, 0) = ISNULL(@ysnIsSigned, 0)
         AND ISNULL(sc.ysnIsPrinted, 0) = ISNULL(@ysnIsPrinted, 0)
         AND ISNULL(sc.strContract, '') = ISNULL(@strContract, '')
@@ -232,15 +226,105 @@ BEGIN
         AND ISNULL(sc.strContractText, '') = ISNULL(@strContractText, '')
         AND ISNULL(sc.strLineOfBusiness, '') = ISNULL(@strLineOfBusiness, '')
 
-    INSERT INTO tblCTApiDollarContractDetailStaging (intApiItemContractStagingId, intCategoryId)
-    SELECT @intItemContractStagingId, c.intCategoryId
-    FROM tblRestApiSchemaDollarContract sc
-    INNER JOIN tblICCategory c ON c.strCategoryCode = sc.strCategory OR c.strDescription = sc.strCategory
+    INSERT INTO @Logs (strError, strField, strLogLevel, strValue)
+    SELECT 'In valid UOM.: ''' + sc.strUnitMeasure + '''', 'UOM', 'Error',  sc.strUnitMeasure
+    FROM tblRestApiSchemaItemContract sc
+    LEFT JOIN tblICItem i ON i.strItemNo = sc.strItemNo
+    LEFT JOIN tblICUnitMeasure uom ON uom.strUnitMeasure = sc.strUnitMeasure
+    LEFT JOIN tblICItemUOM u ON u.intItemId = i.intItemId AND uom.intUnitMeasureId = uom.intUnitMeasureId
+    WHERE sc.guiApiUniqueId = @guiApiUniqueId
+        AND u.intItemUOMId IS NULL
+        AND ISNULL(sc.dtmContractDate, @Date) = ISNULL(@dtmContractDate, @Date)
+        AND ISNULL(sc.dtmExpirationDate, @Date) = ISNULL(@dtmExpirationDate, @Date)
+        AND ISNULL(sc.dtmDueDate, @Date) = ISNULL(@dtmDueDate, @Date)
+        AND ISNULL(sc.ysnIsSigned, 0) = ISNULL(@ysnIsSigned, 0)
+        AND ISNULL(sc.ysnIsPrinted, 0) = ISNULL(@ysnIsPrinted, 0)
+        AND ISNULL(sc.strContract, '') = ISNULL(@strContract, '')
+        AND ISNULL(sc.strEntryContract, '') = ISNULL(@strEntryContract, '')
+        AND ISNULL(sc.strCustomerNo, '') = ISNULL(@strCustomerNo, '')
+        AND ISNULL(sc.strCurrency, '') = ISNULL(@strCurrency, '')
+        AND ISNULL(sc.strLocation, '') = ISNULL(@strLocation, '')
+        AND ISNULL(sc.strFreightTerm, '') = ISNULL(@strFreightTerm, '')
+        AND ISNULL(sc.strCountry, '') = ISNULL(@strCountry, '')
+        AND ISNULL(sc.strTerms, '') = ISNULL(@strTerms, '')
+        AND ISNULL(sc.strSalesperson, '') = ISNULL(@strSalesperson, '')
+        AND ISNULL(sc.strContractText, '') = ISNULL(@strContractText, '')
+        AND ISNULL(sc.strLineOfBusiness, '') = ISNULL(@strLineOfBusiness, '')
+
+    INSERT INTO @Logs (strError, strField, strLogLevel, strValue)
+    SELECT 'Invalid tax group: ''' + sc.strTaxGroup + '''', 'Tax Group', 'Error',  sc.strTaxGroup
+    FROM tblRestApiSchemaItemContract sc
+    LEFT JOIN tblSMTaxGroup taxGroup ON taxGroup.strTaxGroup = sc.strTaxGroup
+    WHERE sc.guiApiUniqueId = @guiApiUniqueId
+        AND taxGroup.intTaxGroupId IS NULL
+        AND ISNULL(sc.dtmContractDate, @Date) = ISNULL(@dtmContractDate, @Date)
+        AND ISNULL(sc.dtmExpirationDate, @Date) = ISNULL(@dtmExpirationDate, @Date)
+        AND ISNULL(sc.dtmDueDate, @Date) = ISNULL(@dtmDueDate, @Date)
+        AND ISNULL(sc.ysnIsSigned, 0) = ISNULL(@ysnIsSigned, 0)
+        AND ISNULL(sc.ysnIsPrinted, 0) = ISNULL(@ysnIsPrinted, 0)
+        AND ISNULL(sc.strContract, '') = ISNULL(@strContract, '')
+        AND ISNULL(sc.strEntryContract, '') = ISNULL(@strEntryContract, '')
+        AND ISNULL(sc.strCustomerNo, '') = ISNULL(@strCustomerNo, '')
+        AND ISNULL(sc.strCurrency, '') = ISNULL(@strCurrency, '')
+        AND ISNULL(sc.strLocation, '') = ISNULL(@strLocation, '')
+        AND ISNULL(sc.strFreightTerm, '') = ISNULL(@strFreightTerm, '')
+        AND ISNULL(sc.strCountry, '') = ISNULL(@strCountry, '')
+        AND ISNULL(sc.strTerms, '') = ISNULL(@strTerms, '')
+        AND ISNULL(sc.strSalesperson, '') = ISNULL(@strSalesperson, '')
+        AND ISNULL(sc.strContractText, '') = ISNULL(@strContractText, '')
+        AND ISNULL(sc.strLineOfBusiness, '') = ISNULL(@strLineOfBusiness, '')
+
+    INSERT INTO @Logs (strError, strField, strLogLevel, strValue)
+    SELECT 'Invalid contract status: ''' + sc.strStatus + '''', 'Status', 'Error',  sc.strStatus
+    FROM tblRestApiSchemaItemContract sc
+    LEFT JOIN tblCTContractStatus s ON s.strContractStatus = sc.strStatus
+    WHERE sc.guiApiUniqueId = @guiApiUniqueId
+        AND s.intContractStatusId IS NULL
+        AND ISNULL(sc.dtmContractDate, @Date) = ISNULL(@dtmContractDate, @Date)
+        AND ISNULL(sc.dtmExpirationDate, @Date) = ISNULL(@dtmExpirationDate, @Date)
+        AND ISNULL(sc.dtmDueDate, @Date) = ISNULL(@dtmDueDate, @Date)
+        AND ISNULL(sc.ysnIsSigned, 0) = ISNULL(@ysnIsSigned, 0)
+        AND ISNULL(sc.ysnIsPrinted, 0) = ISNULL(@ysnIsPrinted, 0)
+        AND ISNULL(sc.strContract, '') = ISNULL(@strContract, '')
+        AND ISNULL(sc.strEntryContract, '') = ISNULL(@strEntryContract, '')
+        AND ISNULL(sc.strCustomerNo, '') = ISNULL(@strCustomerNo, '')
+        AND ISNULL(sc.strCurrency, '') = ISNULL(@strCurrency, '')
+        AND ISNULL(sc.strLocation, '') = ISNULL(@strLocation, '')
+        AND ISNULL(sc.strFreightTerm, '') = ISNULL(@strFreightTerm, '')
+        AND ISNULL(sc.strCountry, '') = ISNULL(@strCountry, '')
+        AND ISNULL(sc.strTerms, '') = ISNULL(@strTerms, '')
+        AND ISNULL(sc.strSalesperson, '') = ISNULL(@strSalesperson, '')
+        AND ISNULL(sc.strContractText, '') = ISNULL(@strContractText, '')
+        AND ISNULL(sc.strLineOfBusiness, '') = ISNULL(@strLineOfBusiness, '')
+
+    INSERT INTO tblCTApiItemContractDetailStaging (
+          intApiItemContractStagingId
+        , intItemId
+        , intItemUOMId
+        , dblContracted
+        , dblPrice
+        , dtmDeliveryDate
+        , intTaxGroupId
+        , strContractStatus)
+    SELECT
+          @intItemContractStagingId
+        , i.intItemId
+        , u.intItemUOMId
+        , sc.dblContractedQty
+        , sc.dblPrice
+        , sc.dtmDeliveryDate
+        , taxGroup.intTaxGroupId
+        , s.intContractStatusId
+    FROM tblRestApiSchemaItemContract sc
+    INNER JOIN tblCTContractStatus s ON s.strContractStatus = sc.strStatus
+    INNER JOIN tblICItem i ON i.strItemNo = sc.strItemNo
+    INNER JOIN tblICUnitMeasure uom ON uom.strUnitMeasure = sc.strUnitMeasure
+    INNER JOIN tblICItemUOM u ON u.intItemId = i.intItemId AND u.intUnitMeasureId = u.intUnitMeasureId
+    INNER JOIN tblSMTaxGroup taxGroup ON taxGroup.strTaxGroup = sc.strTaxGroup OR taxGroup.strDescription =  sc.strTaxGroup
     WHERE sc.guiApiUniqueId = @guiApiUniqueId
         AND ISNULL(sc.dtmContractDate, @Date) = ISNULL(@dtmContractDate, @Date)
         AND ISNULL(sc.dtmExpirationDate, @Date) = ISNULL(@dtmExpirationDate, @Date)
         AND ISNULL(sc.dtmDueDate, @Date) = ISNULL(@dtmDueDate, @Date)
-        AND sc.dblContractValue = @dblContractValue
         AND ISNULL(sc.ysnIsSigned, 0) = ISNULL(@ysnIsSigned, 0)
         AND ISNULL(sc.ysnIsPrinted, 0) = ISNULL(@ysnIsPrinted, 0)
         AND ISNULL(sc.strContract, '') = ISNULL(@strContract, '')
@@ -257,39 +341,38 @@ BEGIN
     
     FETCH NEXT FROM cur INTO 
           @intCustomerId
-        , @dtmContractDate
-        , @dtmExpirationDate
-        , @strEntryContract
-        , @strContract
-        , @ysnIsSigned
-        , @ysnIsPrinted
-        , @dtmDueDate
-        , @dblContractValue
-        , @intCompanyLocationId
-        , @intCurrencyID
-        , @intSalespersonId
-        , @intFreightTermId
-        , @intTermID
-        , @intCountryID
-        , @intContractTextId
-        , @intLineOfBusinessId
-        , @strCustomerNo
-        , @strCurrency
-        , @strLocation
-        , @strFreightTerm
-        , @strCountry
-        , @strTerms
-        , @strSalesperson
-        , @strContractText
-        , @strLineOfBusiness
+		, @dtmContractDate
+		, @dtmExpirationDate
+		, @strEntryContract
+		, @strContract
+		, @ysnIsSigned
+		, @ysnIsPrinted
+		, @dtmDueDate
+		, @intCompanyLocationId
+		, @intCurrencyID
+		, @intSalespersonId
+		, @intFreightTermId
+		, @intTermID
+		, @intCountryID
+		, @intContractTextId
+		, @intLineOfBusinessId
+		, @strCustomerNo
+		, @strCurrency
+		, @strLocation
+		, @strFreightTerm
+		, @strCountry
+		, @strTerms
+		, @strSalesperson
+		, @strContractText
+		, @strLineOfBusiness
 END
 
 CLOSE cur;
 DEALLOCATE cur;
 
 INSERT INTO @Logs
-EXEC dbo.uspApiImportDollarContractsFromStaging @guiApiUniqueId
+EXEC dbo.uspApiImportItemContractsFromStaging @guiApiUniqueId
 
-DELETE FROM tblRestApiSchemaDollarContract WHERE guiApiUniqueId = @guiApiUniqueId
+DELETE FROM tblRestApiSchemaItemContract WHERE guiApiUniqueId = @guiApiUniqueId
 
 SELECT * FROM @Logs
