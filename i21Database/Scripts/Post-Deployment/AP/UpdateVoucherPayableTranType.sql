8fd016a506e9c9a996f82523870b3c7486d4d7d6
Jopet Lambinicio <jopet.lambinicio@irely.com>
2021-05-06 22:16:18 +0800

    AP-9630

--- i21Database/Scripts/Post-Deployment/AP/UpdateVoucherPayableTranType.sql
+++ i21Database/Scripts/Post-Deployment/AP/UpdateVoucherPayableTranType.sql
@@ -1,4 +1,5 @@
-﻿UPDATE A
+﻿PRINT N'START: UPDATING VOUCHER PAYABLE TRANSACTION TYPE'
+UPDATE A
 	SET A.intTransactionType = CASE WHEN A.ysnReturn = 1 THEN 3 ELSE 1 END
 FROM tblAPVoucherPayable A
 WHERE A.intTransactionType = 0
@@ -7,3 +8,4 @@ UPDATE A
 	SET A.intTransactionType = CASE WHEN A.ysnReturn = 1 THEN 3 ELSE 1 END
 FROM tblAPVoucherPayableCompleted A
 WHERE A.intTransactionType = 0
+PRINT N'SUCCESS: UPDATING VOUCHER PAYABLE TRANSACTION TYPE'
