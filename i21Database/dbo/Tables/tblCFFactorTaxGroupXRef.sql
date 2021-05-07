1ad0c942720d30763ed89becd2c3b329c6acd93c
bponce <bonn.ponce@irely.com>
2021-05-07 13:49:32 +0800

    CF-2676
    (cherry picked from commit 36d0e9173844ba36521beb5e71ce8d6b787732bc)
    (cherry picked from commit 0775b36a15cfd0b7c4b8658e744e37469a101698)
    (cherry picked from commit 52c24ae9addfa24858ef0d3fb214eb5c342bc0f2)

--- i21Database/dbo/Tables/tblCFFactorTaxGroupXRef.sql
+++ i21Database/dbo/Tables/tblCFFactorTaxGroupXRef.sql
@@ -2,7 +2,9 @@
     [intFactorTaxGroupXRefId]  INT            IDENTITY (1, 1) NOT NULL,
 	[intCustomerId]		INT NULL,
 	[intCategoryId]		INT NULL,
+	[intSiteId]		    INT NULL,
     [strFactorTaxGroup]   NVARCHAR (250) COLLATE Latin1_General_CI_AS NULL,
+    [strAltAccount]   NVARCHAR (250) COLLATE Latin1_General_CI_AS NULL,
     [strState]   NVARCHAR (250) COLLATE Latin1_General_CI_AS NULL,
     [intConcurrencyId] INT            CONSTRAINT [DF_tblCFFactorTaxGroupXRef_intConcurrencyId] DEFAULT ((1)) NULL,
     CONSTRAINT [PK_tblCFFactorTaxGroupXRef] PRIMARY KEY CLUSTERED ([intFactorTaxGroupXRefId] ASC)
@@ -10,7 +12,7 @@
 GO
 
 
-CREATE UNIQUE NONCLUSTERED INDEX [tblCFFactorTaxGroupXRef_UniqueCustomerStateCategory]
-    ON [dbo].[tblCFFactorTaxGroupXRef]([intCustomerId] ASC, [strState] ASC,  [intCategoryId] ASC);
+CREATE UNIQUE NONCLUSTERED INDEX [tblCFFactorTaxGroupXRef_UniqueSiteCustomerStateCategory]
+    ON [dbo].[tblCFFactorTaxGroupXRef]([intSiteId] ASC, [intCustomerId] ASC, [strState] ASC,  [intCategoryId] ASC);
 GO
 
