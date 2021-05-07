d570fbf1635a2773e65d88760f84b2f827ba1e4d
Jeff Trajano <jeffrey.trajano@irely.com>
2021-05-07 11:55:06 +0800

    GL-8147 Trial Balance Date field filtering

--- i21Database/dbo/Views/vyuGLFiscalYearPeriod.sql
+++ i21Database/dbo/Views/vyuGLFiscalYearPeriod.sql
@@ -6,6 +6,7 @@ SELECT A.[intGLFiscalYearPeriodId]
 	,A.[strPeriod] COLLATE Latin1_General_CI_AS strPeriod
 	,A.[dtmStartDate]
 	,A.[dtmEndDate]
+	,dtmEndDateMDY = CONVERT(NVARCHAR(20), A.[dtmEndDate], 101)
 	,A.[ysnOpen]
 	,A.[intConcurrencyId]
 	,B.[strFiscalYear] COLLATE Latin1_General_CI_AS strFiscalYear
