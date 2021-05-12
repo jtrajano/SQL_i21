b2b03bf033cb01207af3efe46b140814f99a537f
Darwin Estrada <darwin.estrada@irely.com>
2021-05-12 22:17:42 +0800

    FRM-9508

--- i21Database/Scripts/Post-Deployment/RM/03_CleanupObjects.sql
+++ i21Database/Scripts/Post-Deployment/RM/03_CleanupObjects.sql
@@ -5,5 +5,9 @@ IF OBJECT_ID('vyuRKGetMoneyMarket','v') IS NOT NULL
 	DROP VIEW vyuRKGetMoneyMarket;
 GO
 
+IF OBJECT_ID('vyuRKGetBankBalance','v') IS NOT NULL
+	DROP VIEW vyuRKGetBankBalance;
+GO
+
 PRINT 'End Risk Clean up Objects - Drop obsolete objects'
 GO
