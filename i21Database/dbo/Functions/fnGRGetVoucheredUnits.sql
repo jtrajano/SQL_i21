7daba6d43a1a5ed98855e5a873396c7d551f66e5
Mon Gonzales <mon.gonzales@irely.com>
2021-05-12 16:08:36 +0800

    GRN-2634 HDTN-240944 - Show ticket numbers instead of STR numbers

--- i21Database/dbo/Functions/fnGRGetVoucheredUnits.sql
+++ i21Database/dbo/Functions/fnGRGetVoucheredUnits.sql
@@ -6,7 +6,10 @@
 RETURNS DECIMAL(24,10)
 AS
 BEGIN
-	
-	return (select sum(dblQtyReceived) from tblAPBillDetail where intBillId in (select intBillId from tblAPBill where strVendorOrderNumber = (select strStorageTicket from tblGRSettleStorage where intSettleStorageId = @intSettleStorageId)) and intContractSeq is not null)
+	--Mon
+	return (select sum(dblQtyReceived) 
+				from tblAPBillDetail 
+					where intSettleStorageId = @intSettleStorageId
+						and intContractSeq is not null)
 END
 
