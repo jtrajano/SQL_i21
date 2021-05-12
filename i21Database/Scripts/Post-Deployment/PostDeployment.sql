ccc8249b5e2b2ba4e6d10ca5f34039485fb2cf3e
Christian Joel Martinez <Christian.Martinez@iRely.com>
2021-05-12 23:41:24 +0800

    GRN-2674

--- i21Database/Scripts/Post-Deployment/PostDeployment.sql
+++ i21Database/Scripts/Post-Deployment/PostDeployment.sql
@@ -402,6 +402,7 @@ print 'BEGIN POST DEPLOYMENT'
 :R .\GR\1920_Dev_Linking_Voucher_Discount.sql
 :R .\GR\StorageInventoryReceipt.sql
 :R .\GR\Fix_NULL_SSId_in_voucher_details.sql
+:R .\GR\UpdateStrCodeInTransferStorageGLEntries.sql
 
 --Manufacturing
 :R .\MF\1_MasterTables.sql
