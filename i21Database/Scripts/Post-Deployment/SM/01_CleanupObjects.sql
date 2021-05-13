4bc8af8a1dddca09949298eac9bd354f31fce989
Darwin Estrada <darwin.estrada@irely.com>
2021-05-13 20:09:54 +0800

    FRM-9496

---
+++ i21Database/Scripts/Post-Deployment/SM/01_CleanupObjects.sql
@@ -0,0 +1,12 @@
+﻿PRINT 'Begin SM Clean up Objects - Drop obsolete objects'
+GO
+
+IF EXISTS(SELECT top 1 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.uspSMReplicatonDropPublication'))
+       DROP PROCEDURE uspSMReplicatonDropPublication;
+GO
+
+
+
+
+PRINT 'End SM Clean up Objects - Drop obsolete objects'
+GO
