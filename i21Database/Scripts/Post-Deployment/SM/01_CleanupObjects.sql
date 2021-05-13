9b307bf0c91d8878b7c30f84c5c0695195f5a5d5
Darwin Estrada <darwin.estrada@irely.com>
2021-05-13 20:27:55 +0800

    FRM-9479

--- i21Database/Scripts/Post-Deployment/SM/01_CleanupObjects.sql
+++ i21Database/Scripts/Post-Deployment/SM/01_CleanupObjects.sql
@@ -13,8 +13,9 @@ GO
 IF EXISTS(SELECT top 1 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.uspIUAuditLogs'))
        DROP PROCEDURE uspIUAuditLogs;
 GO
-
-
+IF EXISTS(SELECT top 1 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.uspSMCreateReIndexMaintenancePlan'))
+       DROP PROCEDURE uspSMCreateReIndexMaintenancePlan;
+GO
 
 PRINT 'End SM Clean up Objects - Drop obsolete objects'
 GO
