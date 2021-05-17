4d8748da99599babaa34184148afde386237beba
IRELY\JMendones <jesmar.mendones@irely.com>
2021-05-17 17:30:59 +0800

    EM-2807

---
+++ i21Database/dbo/Tables/tblEMRelatedEntity.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[tblEMRelatedEntity]
+(
+	[intRelatedEntityId]				INT NOT NULL PRIMARY KEY IDENTITY,
+    [intEntityId]						INT NOT NULL,
+    [strEntityName]						NVARCHAR(500) COLLATE Latin1_General_CI_AS  NOT NULL,
+    [intConcurrencyId]					INT NOT NULL DEFAULT 1, 
+
+    CONSTRAINT [FK_tblEMRelatedEntity_tblEMEntity] FOREIGN KEY ([intEntityId]) REFERENCES [tblEMEntity]([intEntityId]) ON DELETE CASCADE,
+	CONSTRAINT [UK_tblEMRelatedEntity_Column] UNIQUE ([strEntityName])
+)
