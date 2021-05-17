d1cb49251c03468804951bfe5dfe4f0c5996c75d
Arman Arenas <arman.arenas@irely.com>
2021-05-17 21:29:44 +0800

    LG-2903 additional columns for sales/transfer order routing

--- i21Database/dbo/Views/vyuLGRouteOpenOrders.sql
+++ i21Database/dbo/Views/vyuLGRouteOpenOrders.sql
@@ -3,7 +3,7 @@ AS
 
 SELECT Top 100 percent Convert(int, ROW_NUMBER() OVER (ORDER BY intSourceType)) as intKeyColumn,*  FROM (
 SELECT 
-	intSourceType = 2
+	intSourceType = 2 /* TM Orders */
 	,intOrderId = TMO.intDispatchId
 	,intEntityId = NULL
 	,intEntityLocationId = NULL
@@ -19,6 +19,7 @@ SELECT
 	,strOrderNumber = TMO.strOrderNumber
 	,strLocationName = TMO.strCompanyLocationName
 	,intLocationId = TMO.intCompanyLocationId
+	,strFromWarehouse = NULL
 	,strLocationAddress = CompLoc.strAddress
 	,strLocationCity = CompLoc.strCity
 	,strLocationZipCode = CompLoc.strZipPostalCode
@@ -27,7 +28,10 @@ SELECT
 	,dblFromLongitude = CompLoc.dblLongitude
 	,dblFromLatitude = CompLoc.dblLatitude
 	,dtmScheduledDate = TMO.dtmRequestedDate
+	,dtmHoursFrom = CAST(NULL AS DATETIME)
+	,dtmHoursTo = CAST(NULL AS DATETIME)
 	,strEntityName = TMO.strCustomerName
+	,strToWarehouse = NULL
 	,strToAddress = TMO.strSiteAddress
 	,strToCity = TMO.strSiteCity
 	,strToZipCode = TMO.strSiteZipCode
@@ -39,7 +43,10 @@ SELECT
 	,strOrderStatus = TMO.strOrderStatus
 	,strDriver = TMO.strDriverName
 	,strItemNo = TMO.strProduct
+	,dblOnHand = NULL
+	,dblOrderedQty = NULL
 	,dblQuantity = TMO.dblQuantity
+	,dblStandardWeight = NULL
 	,strCustomerReference = ''
 	,strOrderComments = TMO.strComments
 	,strLocationType = 'Delivery' COLLATE Latin1_General_CI_AS
@@ -52,7 +59,7 @@ SELECT
 	,strFillMethod = TMO.strFillMethod
 	,ysnHold = TMO.ysnHold
 	,ysnRoutingAlert = TMO.ysnRoutingAlert
-
+	,strRoute = CAST(NULL AS NVARCHAR(500))
 FROM vyuTMGeneratedCallEntry TMO 
 LEFT JOIN tblSMCompanyLocation CompLoc ON CompLoc.intCompanyLocationId = TMO.intCompanyLocationId
 WHERE TMO.strOrderStatus <> 'Delivered' AND TMO.strOrderStatus <> 'Routed'
@@ -60,7 +67,7 @@ WHERE TMO.strOrderStatus <> 'Delivered' AND TMO.strOrderStatus <> 'Routed'
 UNION ALL
 
 SELECT
-	intSourceType = 1
+	intSourceType = 1  /* LG Loads - Outbound */
 	,intOrderId = LGLD.intLoadDetailId
 	,intEntityId = LGLD.intCustomerEntityId
 	,intEntityLocationId = LGLD.intCustomerEntityLocationId
@@ -76,6 +83,7 @@ SELECT
 	,strOrderNumber = LGLD.strLoadNumber
 	,strLocationName = LGLD.strSLocationName
 	,intLocationId = LGLD.intSCompanyLocationId
+	,strFromWarehouse = LGLD.strSSubLocationName
 	,strLocationAddress = LGLD.strSLocationAddress
 	,strLocationCity = LGLD.strSLocationCity
 	,strLocationZipCode = LGLD.strSLocationZipCode
@@ -84,7 +92,10 @@ SELECT
 	,dblFromLongitude = CompLoc.dblLongitude
 	,dblFromLatitude = CompLoc.dblLatitude
 	,dtmScheduledDate = LGL.dtmScheduledDate
+	,dtmHoursFrom = CAST(NULL AS DATETIME)
+	,dtmHoursTo = CAST(NULL AS DATETIME)
 	,strEntityName = LGLD.strCustomer
+	,strToWarehouse = LGLD.strPSubLocationName
 	,strToAddress = LGLD.strShipToAddress
 	,strToCity = LGLD.strShipToCity
 	,strToZipCode = LGLD.strShipToZipCode
@@ -96,7 +107,10 @@ SELECT
 	,strOrderStatus = LGL.strShipmentStatus
 	,strDriver = LGL.strDriver
 	,strItemNo = LGLD.strItemNo
+	,dblOnHand = OH.dblUnitOnHand
+	,dblOrderedQty = NULL
 	,dblQuantity = LGLD.dblQuantity
+	,dblStandardWeight = 0.0
 	,strCustomerReference = LGLD.strCustomerReference
 	,strOrderComments = LGLD.strComments
 	,strLocationType = 'Delivery' COLLATE Latin1_General_CI_AS
@@ -109,17 +123,18 @@ SELECT
 	,strFillMethod = ''
 	,ysnHold = Cast(0 as Bit)
 	,ysnRoutingAlert = Cast(0 as Bit)
-
+	,strRoute = CAST(NULL AS NVARCHAR(500))
 FROM vyuLGLoadDetailView LGLD
 JOIN vyuLGLoadView LGL ON LGL.intLoadId = LGLD.intLoadId 
 JOIN tblSMCompanyLocation CompLoc ON CompLoc.intCompanyLocationId = LGLD.intSCompanyLocationId
 JOIN tblEMEntityLocation EML ON EML.intEntityLocationId = LGLD.intCustomerEntityLocationId
+LEFT JOIN vyuICGetItemStock OH ON OH.intItemId = LGLD.intItemId AND OH.intLocationId = LGLD.intSCompanyLocationId
 WHERE LGL.intPurchaseSale = 2 AND LGL.intShipmentStatus = 1 AND IsNull(LGLD.intLoadDetailId, 0) NOT IN (SELECT IsNull(intLoadDetailId, 0) FROM tblLGRouteOrder)
 
 UNION ALL
 
 SELECT
-	intSourceType = 3
+	intSourceType = 3  /* LG Loads - Inbound */
 	,intOrderId = LGLD.intLoadDetailId
 	,intEntityId = LGLD.intVendorEntityId
 	,intEntityLocationId = LGLD.intVendorEntityLocationId
@@ -135,6 +150,7 @@ SELECT
 	,strOrderNumber = LGLD.strLoadNumber
 	,strLocationName = LGLD.strPLocationName
 	,intLocationId = LGLD.intPCompanyLocationId
+	,strFromWarehouse = LGLD.strSSubLocationName
 	,strLocationAddress = LGLD.strPLocationAddress
 	,strLocationCity = LGLD.strPLocationCity
 	,strLocationZipCode = LGLD.strPLocationZipCode
@@ -143,7 +159,10 @@ SELECT
 	,dblFromLongitude = CompLoc.dblLongitude
 	,dblFromLatitude = CompLoc.dblLatitude
 	,dtmScheduledDate = LGL.dtmScheduledDate
+	,dtmHoursFrom = CAST(NULL AS DATETIME)
+	,dtmHoursTo = CAST(NULL AS DATETIME)
 	,strEntityName = LGLD.strVendor
+	,strToWarehouse = LGLD.strPSubLocationName
 	,strToAddress = LGLD.strShipFromAddress
 	,strToCity = LGLD.strShipFromCity
 	,strToZipCode = LGLD.strShipFromZipCode
@@ -155,7 +174,10 @@ SELECT
 	,strOrderStatus = LGL.strShipmentStatus
 	,strDriver = LGL.strDriver
 	,strItemNo = LGLD.strItemNo
+	,dblOnHand = OH.dblUnitOnHand
+	,dblOrderedQty = NULL
 	,dblQuantity = LGLD.dblQuantity
+	,dblStandardWeight = 0.0
 	,strCustomerReference = LGLD.strCustomerReference
 	,strOrderComments = LGLD.strComments
 	,strLocationType = 'Delivery' COLLATE Latin1_General_CI_AS
@@ -168,17 +190,18 @@ SELECT
 	,strFillMethod = ''
 	,ysnHold = Cast(0 as Bit)
 	,ysnRoutingAlert = Cast(0 as Bit)
-
+	,strRoute = CAST(NULL AS NVARCHAR(500))
 FROM vyuLGLoadDetailView LGLD
 JOIN vyuLGLoadView LGL ON LGL.intLoadId = LGLD.intLoadId 
 JOIN tblSMCompanyLocation CompLoc ON CompLoc.intCompanyLocationId = LGLD.intPCompanyLocationId
 JOIN tblEMEntityLocation EML ON EML.intEntityLocationId = LGLD.intVendorEntityLocationId
+LEFT JOIN vyuICGetItemStock OH ON OH.intItemId = LGLD.intItemId AND OH.intLocationId = LGLD.intPCompanyLocationId
 WHERE LGL.intPurchaseSale = 1 AND LGL.intShipmentStatus = 1 AND IsNull(LGLD.intLoadDetailId, 0) NOT IN (SELECT IsNull(intLoadDetailId, 0) FROM tblLGRouteOrder)
 
 UNION ALL
 
 SELECT 
-	intSourceType = 4
+	intSourceType = 4  /* TM Sites */
 	,intOrderId = TMO.intSiteId
 	,intEntityId = NULL
 	,intEntityLocationId = NULL
@@ -194,6 +217,7 @@ SELECT
 	,strOrderNumber = NULL
 	,strLocationName = TMO.strCompanyLocationName
 	,intLocationId = TMO.intCompanyLocationId
+	,strFromWarehouse = NULL
 	,strLocationAddress = ''
 	,strLocationCity = ''
 	,strLocationZipCode = ''
@@ -202,7 +226,10 @@ SELECT
 	,dblFromLongitude = 0.0
 	,dblFromLatitude = 0.0
 	,dtmScheduledDate = NULL
+	,dtmHoursFrom = CAST(NULL AS DATETIME)
+	,dtmHoursTo = CAST(NULL AS DATETIME)
 	,strEntityName = TMO.strCustomerName
+	,strToWarehouse = NULL
 	,strToAddress = TMO.strSiteAddress
 	,strToCity = TMO.strSiteCity
 	,strToZipCode = TMO.strSiteZip
@@ -214,7 +241,10 @@ SELECT
 	,strOrderStatus = NULL
 	,strDriver = TMO.strDriverName COLLATE Latin1_General_CI_AS
 	,strItemNo = NULL
+	,dblOnHand = NULL
+	,dblOrderedQty = NULL
 	,dblQuantity = NULL
+	,dblStandardWeight = NULL
 	,strCustomerReference = ''
 	,strOrderComments = ''
 	,strLocationType = 'Delivery' COLLATE Latin1_General_CI_AS
@@ -227,13 +257,13 @@ SELECT
 	,strFillMethod = ''
 	,ysnHold = Cast(0 as Bit)
 	,ysnRoutingAlert = Cast(0 as Bit)
-
+	,strRoute = CAST(NULL AS NVARCHAR(500))
 FROM vyuTMCustomerConsumptionSiteInfo TMO WHERE TMO.ysnActive = 1
 
 UNION ALL
 
 SELECT  
-	intSourceType = 5
+	intSourceType = 5  /* Entities */
 	,intOrderId = EN.intEntityId
 	,intEntityId = EN.intEntityId
 	,intEntityLocationId = EL.intEntityLocationId
@@ -249,6 +279,7 @@ SELECT
 	,strOrderNumber = NULL
 	,strLocationName = NULL
 	,intLocationId = NULL
+	,strFromWarehouse = NULL
 	,strLocationAddress = NULL
 	,strLocationCity = NULL
 	,strLocationZipCode = NULL
@@ -257,7 +288,10 @@ SELECT
 	,dblFromLongitude = NULL
 	,dblFromLatitude = NULL
 	,dtmScheduledDate = NULL
+	,dtmHoursFrom = CAST(NULL AS DATETIME)
+	,dtmHoursTo = CAST(NULL AS DATETIME)
 	,strEntityName = EN.strName
+	,strToWarehouse = NULL
 	,strToAddress = EL.strAddress
 	,strToCity = EL.strCity
 	,strToZipCode = EL.strZipCode
@@ -269,7 +303,10 @@ SELECT
 	,strOrderStatus = NULL
 	,strDriver = NULL
 	,strItemNo = NULL
+	,dblOnHand = NULL
+	,dblOrderedQty = NULL
 	,dblQuantity = NULL
+	,dblStandardWeight = NULL
 	,strCustomerReference = ''
 	,strOrderComments = ''
 	,strLocationType = 'Delivery' COLLATE Latin1_General_CI_AS
@@ -282,7 +319,7 @@ SELECT
 	,strFillMethod = ''
 	,ysnHold = Cast(0 as Bit)
 	,ysnRoutingAlert = Cast(0 as Bit)
-
+	,strRoute = CAST(NULL AS NVARCHAR(500))
 FROM tblEMEntityLocation EL
 JOIN vyuEMEntity EN ON EN.intEntityId = EL.intEntityId
 JOIN tblEMEntityType ET ON ET.intEntityId = EN.intEntityId
@@ -307,6 +344,7 @@ SELECT
 	,strOrderNumber = SO.strSalesOrderNumber
 	,strLocationName = CompLoc.strLocationName
 	,intLocationId = CompLoc.intCompanyLocationId
+	,strFromWarehouse = FromStrg.strSubLocationName
 	,strLocationAddress = CompLoc.strAddress
 	,strLocationCity = CompLoc.strCity
 	,strLocationZipCode = CompLoc.strZipPostalCode
@@ -315,7 +353,10 @@ SELECT
 	,dblFromLongitude = CompLoc.dblLongitude
 	,dblFromLatitude = CompLoc.dblLatitude
 	,dtmScheduledDate = SO.dtmDate
+	,dtmHoursFrom = CAST(NULL AS DATETIME)
+	,dtmHoursTo = CAST(NULL AS DATETIME)
 	,strEntityName = E.strName
+	,strToWarehouse = ToStrg.strSubLocationName
 	,strToAddress = EL.strAddress
 	,strToCity = EL.strCity
 	,strToZipCode = EL.strZipCode
@@ -327,7 +368,10 @@ SELECT
 	,strOrderStatus = SO.strOrderStatus
 	,strDriver = NULL
 	,strItemNo = I.strItemNo
+	,dblOnHand = OH.dblUnitOnHand
+	,dblOrderedQty = SOD.dblQtyOrdered
 	,dblQuantity = SOD.dblQtyOrdered
+	,dblStandardWeight = SOD.dblStandardWeight
 	,strCustomerReference = SO.strPONumber
 	,strOrderComments = SOD.strComments
 	,strLocationType = 'Delivery' COLLATE Latin1_General_CI_AS
@@ -340,14 +384,17 @@ SELECT
 	,strFillMethod = ''
 	,ysnHold = Cast(0 as Bit)
 	,ysnRoutingAlert = Cast(0 as Bit)
-
+	,strRoute = CAST(NULL AS NVARCHAR(500))
 FROM tblSOSalesOrderDetail SOD
 JOIN tblSOSalesOrder SO ON SO.intSalesOrderId = SOD.intSalesOrderId 
 JOIN tblSMCompanyLocation CompLoc ON CompLoc.intCompanyLocationId = SO.intCompanyLocationId
 JOIN tblEMEntity E ON E.intEntityId = SO.intEntityCustomerId
 JOIN tblEMEntityLocation EL ON EL.intEntityId = E.intEntityId AND SO.intShipToLocationId = EL.intEntityLocationId
 JOIN tblICItem I ON I.intItemId = SOD.intItemId
+LEFT JOIN tblSMCompanyLocationSubLocation FromStrg ON FromStrg.intCompanyLocationSubLocationId = SOD.intSubLocationId
+LEFT JOIN tblSMCompanyLocationSubLocation ToStrg ON ToStrg.intCompanyLocationSubLocationId = SOD.intCustomerStorageId
 LEFT JOIN vyuARCustomerInquiryReport CB ON CB.intEntityCustomerId = E.intEntityId
+LEFT JOIN vyuICGetItemStock OH ON OH.intItemId = I.intItemId AND OH.intLocationId = CompLoc.intCompanyLocationId
 WHERE SO.strTransactionType = 'Order' AND SO.strOrderStatus NOT IN ('Closed')
 
 UNION ALL
@@ -369,6 +416,7 @@ SELECT
 	,strOrderNumber = IT.strTransferNo
 	,strLocationName = FromLoc.strLocationName
 	,intLocationId = FromLoc.intCompanyLocationId
+	,strFromWarehouse = FromStrg.strSubLocationName
 	,strLocationAddress = CASE WHEN ITD.intFromSubLocationId IS NOT NULL AND ISNULL(FromStrg.strAddress, '') <> '' THEN FromStrg.strAddress ELSE FromLoc.strAddress END
 	,strLocationCity = CASE WHEN ITD.intFromSubLocationId IS NOT NULL AND ISNULL(FromStrg.strAddress, '') <> '' THEN FromStrg.strCity ELSE FromLoc.strCity END
 	,strLocationZipCode = CASE WHEN ITD.intFromSubLocationId IS NOT NULL AND ISNULL(FromStrg.strAddress, '') <> '' THEN FromStrg.strZipCode ELSE FromLoc.strZipPostalCode END
@@ -377,7 +425,10 @@ SELECT
 	,dblFromLongitude = CASE WHEN ITD.intFromSubLocationId IS NOT NULL AND ISNULL(FromStrg.strAddress, '') <> '' THEN FromStrg.dblLongitude ELSE FromLoc.dblLongitude END
 	,dblFromLatitude = CASE WHEN ITD.intFromSubLocationId IS NOT NULL AND ISNULL(FromStrg.strAddress, '') <> '' THEN FromStrg.dblLatitude ELSE FromLoc.dblLatitude END
 	,dtmScheduledDate = IT.dtmTransferDate
+	,dtmHoursFrom = CAST(NULL AS DATETIME)
+	,dtmHoursTo = CAST(NULL AS DATETIME)
 	,strEntityName = E.strName
+	,strToWarehouse = ToStrg.strSubLocationName
 	,strToAddress = CASE WHEN ITD.intToSubLocationId IS NOT NULL AND ISNULL(ToStrg.strAddress, '') <> '' THEN ToStrg.strAddress ELSE ToLoc.strAddress END
 	,strToCity = CASE WHEN ITD.intToSubLocationId IS NOT NULL AND ISNULL(ToStrg.strAddress, '') <> '' THEN ToStrg.strCity ELSE ToLoc.strCity END
 	,strToZipCode = CASE WHEN ITD.intToSubLocationId IS NOT NULL AND ISNULL(ToStrg.strAddress, '') <> '' THEN ToStrg.strZipCode ELSE ToLoc.strZipPostalCode END
@@ -391,7 +442,10 @@ SELECT
 	,strOrderStatus = ITS.strStatus
 	,strDriver = NULL
 	,strItemNo = I.strItemNo
+	,dblOnHand = OH.dblUnitOnHand
+	,dblOrderedQty = 0.0
 	,dblQuantity = ITD.dblQuantity
+	,dblStandardWeight = ITD.dblStandardWeight
 	,strCustomerReference = NULL
 	,strOrderComments = ITD.strComment
 	,strLocationType = 'Delivery' COLLATE Latin1_General_CI_AS
@@ -404,7 +458,7 @@ SELECT
 	,strFillMethod = ''
 	,ysnHold = Cast(0 as Bit)
 	,ysnRoutingAlert = Cast(0 as Bit)
-
+	,strRoute = CAST(NULL AS NVARCHAR(500))
 FROM tblICInventoryTransferDetail ITD
 JOIN tblICInventoryTransfer IT ON IT.intInventoryTransferId = ITD.intInventoryTransferId 
 JOIN tblSMCompanyLocation FromLoc ON FromLoc.intCompanyLocationId = IT.intFromLocationId
@@ -414,6 +468,7 @@ JOIN tblICItem I ON I.intItemId = ITD.intItemId
 LEFT JOIN tblICStatus ITS ON ITS.intStatusId = IT.intStatusId
 LEFT JOIN tblSMCompanyLocationSubLocation FromStrg ON FromStrg.intCompanyLocationSubLocationId = ITD.intFromSubLocationId
 LEFT JOIN tblSMCompanyLocationSubLocation ToStrg ON ToStrg.intCompanyLocationSubLocationId = ITD.intToSubLocationId
+LEFT JOIN vyuICGetItemStock OH ON OH.intItemId = I.intItemId AND OH.intLocationId = FromLoc.intCompanyLocationId
 WHERE IT.ysnPosted = 1 AND IT.intStatusId IN (1, 2)
 
 ) t1
