45d1010462bc865ebda276063846053b6f6cd6da
Premkumar S <prem.selladurai@irely.com>
2021-05-24 18:44:31 +0530

    MFG-4355

--- i21Database/dbo/Views/vyuMFGetContractDetail.sql
+++ i21Database/dbo/Views/vyuMFGetContractDetail.sql
@@ -20,6 +20,7 @@ SELECT CD.intContractDetailId
 	,CH.intEntityId
 	,CD.intContractStatusId
 	,CH.ysnEnableFutures
+	,dbo.fnCTConvertQtyToTargetItemUOM(CD.intItemUOMId,CD.intRefFuturesItemUOMId,CD.dblRefFuturesQty)*100 dblRefPriceInPriceUOM
 FROM tblCTContractDetail CD
 JOIN tblCTContractHeader CH ON CH.intContractHeaderId = CD.intContractHeaderId
 JOIN tblRKFutureMarket FM ON FM.intFutureMarketId = CD.intFutureMarketId
