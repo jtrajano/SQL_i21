8654457927faa74cc0f3e0d881621f739d4b9832
Mon Gonzales <mon.gonzales@irely.com>
2021-05-28 15:08:43 +0800

    Revert "SC-4124 - APC issue - Delivery sheet shrinkage is not creating A/P Clearing GL entries for the shrink unit"
    This reverts commit 7b45e929283a8df854d1010fcd6ca0cfee8ece81.

--- i21Database/dbo/Tables/tblSCDeliverySheet.sql
+++ i21Database/dbo/Tables/tblSCDeliverySheet.sql
@@ -23,7 +23,6 @@
 	[strCountyProducer] NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NULL,
 	[intConcurrencyId] INT NOT NULL DEFAULT ((1)),  
 	[dtmImportedDate] DATETIME NULL, 
-    [ysnInvolvedWithShrinkage] bit null,
     CONSTRAINT [PK_tblSCDeliverySheet_intDeliverySheetId] PRIMARY KEY ([intDeliverySheetId]),
 	CONSTRAINT [FK_tblSCDeliverySheet_tblEMEntity_intEntityId] FOREIGN KEY ([intEntityId]) REFERENCES tblEMEntity([intEntityId]),
 	CONSTRAINT [FK_tblSCDeliverySheet_tblSMCompanyLocation_intCompanyLocationId] FOREIGN KEY ([intCompanyLocationId]) REFERENCES [tblSMCompanyLocation]([intCompanyLocationId]),
