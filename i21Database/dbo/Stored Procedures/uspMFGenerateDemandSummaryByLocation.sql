5d9c038ade3714366d17903ec3bd59837e2cc5cf
Premkumar S <prem.selladurai@irely.com>
2021-05-31 00:52:59 +0530

    MFG-4262

--- i21Database/dbo/Stored Procedures/uspMFGenerateDemandSummaryByLocation.sql
+++ i21Database/dbo/Stored Procedures/uspMFGenerateDemandSummaryByLocation.sql
@@ -12,6 +12,19 @@ BEGIN
 		,@strItemNoList VARCHAR(MAX)
 		,@ysnAllItem BIT
 		,@intCategoryId INT
+		,@intMonthsToView INT = 12
+		,@intMonthId INT
+		,@intLocationId INT
+		,@dblSupplyTarget INT
+		,@dblDecimalPart NUMERIC(18, 6)
+		,@intIntegerPart INT
+		,@dblTotalConsumptionQty NUMERIC(18, 6)
+		,@intConsumptionAvlMonth INT
+		,@dblEndInventory NUMERIC(18, 6)
+		,@dblWeeksOfSsupply NUMERIC(18, 6)
+		,@intConsumptionMonth INT
+		,@dblRemainingConsumptionQty NUMERIC(18, 6)
+		,@dblConsumptionQty NUMERIC(18, 6)
 
 	IF ISNULL((
 				SELECT 1
@@ -95,6 +108,413 @@ BEGIN
 	LEFT JOIN tblCTSubBook SB ON SB.intSubBookId = RM.intSubBookId
 	WHERE RM.intInvPlngReportMasterID = @intInvPlngReportMasterID
 
+	DECLARE @tblCTInvPlngReportAttributeValue TABLE (
+		intInvPlngReportMasterID INT
+		,intReportAttributeID INT
+		,strFieldName NVARCHAR(50)
+		,strValue NVARCHAR(50)
+		,intLocationId INT
+		,intMonthId INT
+		,dblQty NUMERIC(18, 6)
+		)
+	DECLARE @tblSMCompanyLocation TABLE (intLocationId INT)
+
+	INSERT INTO @tblSMCompanyLocation
+	SELECT DISTINCT intLocationId
+	FROM @tblCTInvPlngReportAttributeValue
+
+	INSERT INTO @tblCTInvPlngReportAttributeValue (
+		intInvPlngReportMasterID
+		,intReportAttributeID
+		,strFieldName
+		,strValue
+		,intLocationId
+		,intMonthId
+		,dblQty
+		)
+	SELECT intInvPlngReportMasterID
+		,intReportAttributeID
+		,strFieldName
+		,Sum(CASE 
+				WHEN IsNumeric(strValue) = 1
+					THEN Convert(NUMERIC(18, 6), strValue)
+				ELSE 0
+				END)
+		,intLocationId
+		,Replace(Replace(Replace(strFieldName, 'strMonth', ''), 'OpeningInv', ''), 'PastDue', '')
+		,Sum(CASE 
+				WHEN IsNumeric(strValue) = 1
+					THEN Convert(NUMERIC(18, 6), strValue)
+				ELSE 0
+				END)
+	FROM dbo.tblCTInvPlngReportAttributeValue s
+	WHERE s.intInvPlngReportMasterID = @intInvPlngReportMasterID
+		AND s.intReportAttributeID IN (
+			2
+			,5
+			,6
+			,8
+			,9
+			,13
+			,14
+			,15
+			,16
+			)
+		AND intLocationId IS NOT NULL
+	GROUP BY intInvPlngReportMasterID
+		,intReportAttributeID
+		,strFieldName
+		,intLocationId
+
+	IF (
+			SELECT Count(*)
+			FROM @tblSMCompanyLocation
+			) > 1
+	BEGIN
+		INSERT INTO @tblCTInvPlngReportAttributeValue (
+			intInvPlngReportMasterID
+			,intReportAttributeID
+			,strFieldName
+			,strValue
+			,intLocationId
+			,intMonthId
+			,dblQty
+			)
+		SELECT intInvPlngReportMasterID
+			,intReportAttributeID
+			,strFieldName
+			,Sum(CASE 
+					WHEN IsNumeric(strValue) = 1
+						THEN Convert(NUMERIC(18, 6), strValue)
+					ELSE 0
+					END)
+			,9999
+			,Replace(Replace(Replace(strFieldName, 'strMonth', ''), 'OpeningInv', ''), 'PastDue', '')
+			,Sum(CASE 
+					WHEN IsNumeric(strValue) = 1
+						THEN Convert(NUMERIC(18, 6), strValue)
+					ELSE 0
+					END)
+		FROM dbo.tblCTInvPlngReportAttributeValue s
+		WHERE s.intInvPlngReportMasterID = @intInvPlngReportMasterID
+			AND s.intReportAttributeID IN (
+				2
+				,5
+				,6
+				,8
+				,9
+				,13
+				,14
+				,15
+				,16
+				)
+			AND intLocationId IS NOT NULL
+			AND IsNumeric(strValue) = 1
+		GROUP BY intInvPlngReportMasterID
+			,intReportAttributeID
+			,strFieldName
+	END
+
+	INSERT INTO @tblCTInvPlngReportAttributeValue (
+		intInvPlngReportMasterID
+		,intReportAttributeID
+		,strFieldName
+		,strValue
+		,intLocationId
+		)
+	SELECT intInvPlngReportMasterID
+		,intReportAttributeID
+		,strFieldName
+		,strValue
+		,intLocationId
+	FROM dbo.tblCTInvPlngReportAttributeValue s
+	WHERE s.intInvPlngReportMasterID = @intInvPlngReportMasterID
+		AND s.intReportAttributeID = 1
+
+	WHILE @intMonthId <= @intMonthsToView
+	BEGIN
+		SELECT @intLocationId = NULL
+
+		SELECT @intLocationId = intLocationId
+		FROM @tblSMCompanyLocation
+
+		WHILE @intLocationId IS NOT NULL
+		BEGIN
+			BEGIN
+				SELECT @dblEndInventory = 0
+					,@dblWeeksOfSsupply = 0
+					,@dblSupplyTarget = NULL
+					,@dblDecimalPart = NULL
+					,@intIntegerPart = NULL
+					,@dblTotalConsumptionQty = NULL
+					,@intLocationId = NULL
+
+				SELECT @dblSupplyTarget = MAX(dblQty)
+				FROM @tblCTInvPlngReportAttributeValue
+				WHERE intReportAttributeID = 11 --Weeks of Supply Target
+					AND intMonthId = @intMonthId
+					AND intLocationId = @intLocationId
+
+				SELECT @dblDecimalPart = @dblSupplyTarget % 1
+
+				SELECT @intIntegerPart = @dblSupplyTarget - @dblDecimalPart
+
+				IF @intIntegerPart = 0
+				BEGIN
+					IF @dblDecimalPart > 0
+					BEGIN
+						SELECT @dblTotalConsumptionQty = ABS(SUM(dblQty)) * @dblDecimalPart
+						FROM @tblCTInvPlngReportAttributeValue
+						WHERE intMonthId = @intMonthId + 1
+							AND intReportAttributeID IN (
+								8
+								,15
+								)
+							AND intLocationId = @intLocationId
+					END
+				END
+				ELSE
+				BEGIN
+					SELECT @dblTotalConsumptionQty = ABS(SUM(CASE 
+									WHEN intReportAttributeID = 16
+										AND dblQty > 0
+										THEN 0
+									ELSE dblQty
+									END))
+					FROM @tblCTInvPlngReportAttributeValue
+					WHERE intMonthId BETWEEN @intMonthId + 1
+							AND @intMonthId + @intIntegerPart
+						AND intReportAttributeID IN (
+							8
+							,15
+							,16
+							)
+						AND intLocationId = @intLocationId
+
+					IF @dblDecimalPart > 0
+					BEGIN
+						SELECT @dblTotalConsumptionQty = isNULL(@dblTotalConsumptionQty, 0) + (
+								ABS(SUM(CASE 
+											WHEN intReportAttributeID = 16
+												AND dblQty > 0
+												THEN 0
+											ELSE dblQty
+											END)) * @dblDecimalPart
+								)
+						FROM @tblCTInvPlngReportAttributeValue
+						WHERE intMonthId = @intMonthId + @intIntegerPart + 1
+							AND intReportAttributeID IN (
+								8
+								,15
+								,16
+								)
+							AND intLocationId = @intLocationId
+					END
+				END
+
+				SELECT @dblEndInventory = dblQty
+				FROM @tblCTInvPlngReportAttributeValue D
+				WHERE intReportAttributeID = 9 --Ending Inventory
+					AND intMonthId = @intMonthId
+					AND intLocationId = @intLocationId
+
+				IF @dblEndInventory IS NULL
+					SELECT @dblEndInventory = 0
+
+				SELECT @intConsumptionMonth = @intMonthId + 1
+
+				INSERT INTO @tblCTInvPlngReportAttributeValue (
+					intInvPlngReportMasterID
+					,intReportAttributeID
+					,strFieldName
+					,strValue
+					,intLocationId
+					)
+				SELECT @intInvPlngReportMasterID
+					,12 --Short/Excess Inventory
+					,'strMonth' + ltrim(@intMonthId)
+					,@dblEndInventory - IsNULL(@dblTotalConsumptionQty, 0)
+					,@intLocationId
+
+				SELECT @intConsumptionAvlMonth = 0
+
+				SELECT @intConsumptionAvlMonth = Count(*)
+				FROM @tblCTInvPlngReportAttributeValue
+				WHERE (
+						intReportAttributeID IN (
+							8
+							,15
+							)
+						OR (
+							intReportAttributeID = 16
+							AND dblQty < 0
+							)
+						)
+					AND intLocationId = @intLocationId
+
+				IF @intConsumptionAvlMonth IS NULL
+					SELECT @intConsumptionAvlMonth = @intMonthsToView
+
+				WHILE @intConsumptionMonth <= 12
+					AND @dblEndInventory > 0
+				BEGIN
+					SELECT @dblRemainingConsumptionQty = NULL
+
+					SELECT @dblRemainingConsumptionQty = ABS(SUM(CASE 
+									WHEN intReportAttributeID = 16
+										AND dblQty > 0
+										THEN 0
+									ELSE dblQty
+									END))
+					FROM @tblCTInvPlngReportAttributeValue
+					WHERE intMonthId >= @intConsumptionMonth
+						AND intReportAttributeID IN (
+							8
+							,15
+							,16
+							)
+						AND intLocationId = @intLocationId
+
+					IF @dblRemainingConsumptionQty IS NULL
+						SELECT @dblRemainingConsumptionQty = 0
+
+					IF (@dblRemainingConsumptionQty = 0)
+						AND @intConsumptionMonth = @intMonthId + 1
+					BEGIN
+						IF NOT EXISTS (
+								SELECT *
+								FROM @tblCTInvPlngReportAttributeValue
+								WHERE intReportAttributeID = 10
+									AND dblQty = 999
+									AND intLocationId = @intLocationId
+								)
+						BEGIN
+							INSERT INTO @tblCTInvPlngReportAttributeValue (
+								intInvPlngReportMasterID
+								,intReportAttributeID
+								,strFieldName
+								,strValue
+								,intLocationId
+								)
+							SELECT @intInvPlngReportMasterID
+								,10 --Weeks of Supply
+								,'strMonth' + ltrim(@intMonthId)
+								,999
+								,@intLocationId
+						END
+						ELSE
+						BEGIN
+							INSERT INTO @tblCTInvPlngReportAttributeValue (
+								intInvPlngReportMasterID
+								,intReportAttributeID
+								,strFieldName
+								,strValue
+								,intLocationId
+								)
+							SELECT @intInvPlngReportMasterID
+								,10 --Weeks of Supply
+								,'strMonth' + ltrim(@intMonthId)
+								,0
+								,@intLocationId
+						END
+
+						GOTO NextMonth
+					END
+
+					SELECT @dblConsumptionQty = 0
+
+					SELECT @dblConsumptionQty = ABS(SUM(CASE 
+									WHEN intReportAttributeID = 16
+										AND dblQty > 0
+										THEN 0
+									ELSE dblQty
+									END))
+					FROM @tblCTInvPlngReportAttributeValue
+					WHERE intMonthId = @intConsumptionMonth
+						AND intReportAttributeID IN (
+							8
+							,15
+							,16
+							)
+						AND intLocationId = @intLocationId
+
+					IF @dblConsumptionQty IS NULL
+						SELECT @dblConsumptionQty = 0
+
+					IF @dblEndInventory > @dblConsumptionQty
+					BEGIN
+						SELECT @dblEndInventory = @dblEndInventory - @dblConsumptionQty
+
+						SELECT @dblWeeksOfSsupply = @dblWeeksOfSsupply + 1
+
+						IF NOT EXISTS (
+								SELECT 1
+								FROM @tblCTInvPlngReportAttributeValue
+								WHERE intMonthId > @intConsumptionMonth
+									AND intReportAttributeID IN (
+										8
+										,15
+										,16
+										)
+									AND intLocationId = @intLocationId
+								HAVING ABS(SUM(CASE 
+												WHEN intReportAttributeID = 16
+													AND dblQty > 0
+													THEN 0
+												ELSE dblQty
+												END)) > 0
+								)
+						BEGIN
+							INSERT INTO @tblCTInvPlngReportAttributeValue (
+								intInvPlngReportMasterID
+								,intReportAttributeID
+								,strFieldName
+								,strValue
+								,intLocationId
+								)
+							SELECT @intInvPlngReportMasterID
+								,10 --Weeks of Supply
+								,'strMonth' + ltrim(@intMonthId)
+								,@dblWeeksOfSsupply
+								,@intLocationId
+
+							SELECT @dblEndInventory = 0
+						END
+					END
+					ELSE
+					BEGIN
+						SELECT @dblWeeksOfSsupply = @dblWeeksOfSsupply + (@dblEndInventory / @dblConsumptionQty)
+
+						SELECT @dblEndInventory = 0
+
+						INSERT INTO @tblCTInvPlngReportAttributeValue (
+							intInvPlngReportMasterID
+							,intReportAttributeID
+							,strFieldName
+							,strValue
+							,intLocationId
+							)
+						SELECT @intInvPlngReportMasterID
+							,10 --Weeks of Supply
+							,'strMonth' + ltrim(@intMonthId)
+							,@dblWeeksOfSsupply
+							,@intLocationId
+					END
+
+					SELECT @intConsumptionMonth = @intConsumptionMonth + 1
+				END
+
+				NextMonth:
+			END
+
+			SELECT @intLocationId = intLocationId
+			FROM @tblSMCompanyLocation
+			WHERE intLocationId > @intLocationId
+		END
+
+		SELECT @intMonthId = @intMonthId + 1
+	END
+
 	SELECT Ext.intReportAttributeID [AttributeId]
 		,CASE 
 			WHEN RA.intReportAttributeID = 10
@@ -128,6 +548,7 @@ BEGIN
 		,Ext.strMonth10
 		,Ext.strMonth11
 		,Ext.strMonth12
+		,RA.ysnEditable 
 	FROM (
 		SELECT *
 		FROM (
@@ -136,8 +557,7 @@ BEGIN
 				,strFieldName
 				,strValue
 				,intLocationId
-			FROM tblCTInvPlngReportAttributeValue s
-			WHERE s.intInvPlngReportMasterID = @intInvPlngReportMasterID
+			FROM @tblCTInvPlngReportAttributeValue
 			) AS st
 		pivot(MAX(strValue) FOR strFieldName IN (
 					OpeningInv
