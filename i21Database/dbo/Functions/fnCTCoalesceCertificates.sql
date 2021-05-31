ed274353540947783e983bfb88589f6730bca323
LexPeregrino <lex.peregrino@irely.com>
2021-06-01 03:11:36 +0800

    CT-5894 1.0

---
+++ i21Database/dbo/Functions/fnCTCoalesceCertificates.sql
@@ -0,0 +1,19 @@
+﻿CREATE FUNCTION [dbo].[fnCTCoalesceCertificates](@intContractDetailId int)
+RETURNS nvarchar(max)
+AS
+BEGIN
+
+	DECLARE @strCertificates nvarchar(max);
+
+	select
+		@strCertificates = coalesce(@strCertificates + ',', '') +  b.strCertificationName
+	from
+		tblCTContractCertification a
+		inner join tblICCertification b on a.intCertificationId = b.intCertificationId
+	where
+		a.intContractDetailId = @intContractDetailId
+		and b.strCertificationName is not null
+
+	return @strCertificates
+
+END
