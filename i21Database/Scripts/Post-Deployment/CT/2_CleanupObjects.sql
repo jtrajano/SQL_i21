b310b584f792ba1c602075d50b27a368a553b993
Darwin Estrada <darwin.estrada@irely.com>
2021-06-02 17:28:28 +0800

    FRM-9719

--- i21Database/Scripts/Post-Deployment/CT/2_CleanupObjects.sql
+++ i21Database/Scripts/Post-Deployment/CT/2_CleanupObjects.sql
@@ -5,6 +5,11 @@ IF EXISTS(SELECT top 1 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.uspC
        DROP PROCEDURE uspCTAllowDPContractClosure;
 GO
 
+IF EXISTS(SELECT top 1 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.uspCTProcessInvoiceDelete'))
+       DROP PROCEDURE uspCTProcessInvoiceDelete;
+GO
+
+
 
 PRINT 'End Contract Management Clean up Objects - Drop obsolete objects'
 GO
