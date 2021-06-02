e200304e30adc1c1a49e2841344a63d908126aaf
Darwin Estrada <darwin.estrada@irely.com>
2021-06-02 17:49:06 +0800

    FRM-9719

--- i21Database/Scripts/Post-Deployment/SM/01_CleanupObjects.sql
+++ i21Database/Scripts/Post-Deployment/SM/01_CleanupObjects.sql
@@ -1,6 +1,40 @@
 ﻿PRINT 'Begin SM Clean up Objects - Drop obsolete objects'
 GO
 
+
+
+IF EXISTS(SELECT top 1 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.uspDMMergeTables'))
+       DROP PROCEDURE uspDMMergeTables;
+GO
+
+IF EXISTS(SELECT top 1 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.uspSMSyncTables'))
+       DROP PROCEDURE uspSMSyncTables;
+GO
+
+IF EXISTS(SELECT top 1 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.uspSMReplicationRemoteConfiguration'))
+       DROP PROCEDURE uspSMReplicationRemoteConfiguration;
+GO
+
+IF EXISTS(SELECT top 1 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.uspSMReplicationPullSubscription'))
+       DROP PROCEDURE uspSMReplicationPullSubscription;
+GO
+
+IF EXISTS(SELECT top 1 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.uspSMReplicationMainConfiguration'))
+       DROP PROCEDURE uspSMReplicationMainConfiguration;
+GO
+
+IF EXISTS(SELECT top 1 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.uspSMReplicationDropPullSubscription'))
+       DROP PROCEDURE uspSMReplicationDropPullSubscription;
+GO
+
+IF EXISTS(SELECT top 1 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.uspSMReplicationDropPublication'))
+       DROP PROCEDURE uspSMReplicationDropPublication;
+GO
+
+IF EXISTS(SELECT top 1 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.uspSMReplicationAddSubscriptionForRemote'))
+       DROP PROCEDURE uspSMReplicationAddSubscriptionForRemote;
+GO
+
 IF EXISTS(SELECT top 1 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.uspSMReplicatonDropPublication'))
        DROP PROCEDURE uspSMReplicatonDropPublication;
 GO
