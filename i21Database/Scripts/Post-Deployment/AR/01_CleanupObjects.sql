2789c294e3cadcc3e8c3a06404ab781a7abee900
Darwin Estrada <darwin.estrada@irely.com>
2021-06-06 11:40:07 +0800

    FRM-9705

--- i21Database/Scripts/Post-Deployment/AR/01_CleanupObjects.sql
+++ i21Database/Scripts/Post-Deployment/AR/01_CleanupObjects.sql
@@ -10,6 +10,10 @@ IF OBJECT_ID('vyuARSearchPOSEndOfDay','v') IS NOT NULL
 	DROP VIEW vyuARSearchPOSEndOfDay;
 GO
 
+IF OBJECT_ID('vyuARPOSAvailableDrawer','v') IS NOT NULL
+	DROP VIEW vyuARPOSAvailableDrawer;
+GO
+
 IF EXISTS(SELECT top 1 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.uspARErrorMessages'))
        DROP PROCEDURE uspARErrorMessages;
 GO
