47b117fca7bc834f59ef4cacc72fcadd6ee17103
Darwin Estrada <darwin.estrada@irely.com>
2021-06-06 13:49:39 +0800

    FRM-9701

--- i21Database/Scripts/Post-Deployment/TR/01_CleanupObjects.sql
+++ i21Database/Scripts/Post-Deployment/TR/01_CleanupObjects.sql
@@ -6,5 +6,12 @@ IF OBJECT_ID('vyuTRQuoteView','v') IS NOT NULL
 	DROP VIEW vyuTRQuoteView;
 GO
 
+
+IF OBJECT_ID('vyuTRRackPriceView','v') IS NOT NULL
+	DROP VIEW vyuTRRackPriceView;
+GO
+
+
+
 PRINT 'End Transports Clean up Objects - Drop obsolete objects'
 GO
