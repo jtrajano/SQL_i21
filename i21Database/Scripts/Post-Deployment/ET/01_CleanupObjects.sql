3ea8fdaa7f61ed31125fc85ce6de04485553240c
Darwin Estrada <darwin.estrada@irely.com>
2021-06-06 14:07:25 +0800

    FRM-9680

--- i21Database/Scripts/Post-Deployment/ET/01_CleanupObjects.sql
+++ i21Database/Scripts/Post-Deployment/ET/01_CleanupObjects.sql
@@ -4,6 +4,10 @@ GO
 IF OBJECT_ID('vyuETCustomer','v') IS NOT NULL
 	DROP VIEW vyuETCustomer;
 GO
+IF OBJECT_ID('vyuARGetItemPricingLevelDetail','v') IS NOT NULL
+	DROP VIEW vyuARGetItemPricingLevelDetail;
+GO
+
 
 PRINT 'End AP Clean up Objects - Drop obsolete objects'
 GO
