81f87b392cf7cef03646b94b512394f4d197ea94
Darwin Estrada <darwin.estrada@irely.com>
2021-06-06 11:34:10 +0800

    FRM-9710

---
+++ i21Database/Scripts/Post-Deployment/AP/01_CleanupObjects.sql
@@ -0,0 +1,10 @@
+﻿PRINT 'Begin AP Clean up Objects - Drop obsolete objects'
+GO
+
+
+IF OBJECT_ID('vyuAPChargesForBilling','v') IS NOT NULL
+	DROP VIEW vyuAPChargesForBilling;
+GO
+
+PRINT 'End AP Clean up Objects - Drop obsolete objects'
+GO
