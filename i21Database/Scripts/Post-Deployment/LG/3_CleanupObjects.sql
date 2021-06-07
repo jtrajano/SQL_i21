340ef0d4f0010357a02edce71e189a173f42be87
Darwin Estrada <darwin.estrada@irely.com>
2021-06-07 12:29:15 +0800

    FRM-9707

--- i21Database/Scripts/Post-Deployment/LG/3_CleanupObjects.sql
+++ i21Database/Scripts/Post-Deployment/LG/3_CleanupObjects.sql
@@ -8,6 +8,10 @@ GO
 IF OBJECT_ID('vyuLGWeightLoss','v') IS NOT NULL
 	DROP VIEW vyuLGWeightLoss;
 GO
+IF OBJECT_ID('vyuLGAllocationOpenSContracts','v') IS NOT NULL
+	DROP VIEW vyuLGAllocationOpenSContracts;
+GO
+
 
 
 PRINT 'End LG Clean up Objects - Drop obsolete objects'
