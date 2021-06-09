7e738d431498fd99734bf6588e19de8df1711aad
LexPeregrino <lex.peregrino@irely.com>
2021-06-10 01:21:37 +0800

    CT-5892 1.0
    (cherry picked from commit b1efcbbe66d214e94252f8ee2048c1739dd605d8)
    (cherry picked from commit 51e1c7f22f37e6a33c81f00eb4592d2fd63d979f)

--- i21Database/dbo/Views/vyuCTContStsGoodsShipped.sql
+++ i21Database/dbo/Views/vyuCTContStsGoodsShipped.sql
@@ -5,20 +5,21 @@ AS
 	SELECT	CAST(ROW_NUMBER() OVER (ORDER BY intContractDetailId ASC) AS INT) intUniqueId,*
 	FROM
 	(			
-			SELECT	CQ.intPContractDetailId intContractDetailId,
+			SELECT	intContractDetailId = CASE WHEN (SH.intPurchaseSale = 2) THEN CQ.intSContractDetailId ELSE CQ.intPContractDetailId END,
 					SH.strLoadNumber,
-					IU.intItemUOMId,
-					dbo.fnCTConvertQuantityToTargetItemUOM(CQ.intItemId,IU.intUnitMeasureId,LP.intWeightUOMId,CQ.dblNet)	AS dblNetWeight,
-					IM.strUnitMeasure,
+					ISNULL(IU.intItemUOMId,ISNULL(CQ.intItemUOMId, LP.intWeightUOMId) ) AS intItemUOMId,
+					ISNULL(dbo.fnCTConvertQuantityToTargetItemUOM(CQ.intItemId,IU.intUnitMeasureId,LP.intWeightUOMId,CQ.dblNet),0)	AS dblNetWeight,
+					ISNULL(IM.strUnitMeasure,LPM.strUnitMeasure) AS strUnitMeasure,
 					SH.intShipmentType,
 					SH.intLoadId,
 					'intLoadId' COLLATE Latin1_General_CI_AS AS strIdColumn
 			FROM	tblLGLoadDetail	CQ
-			JOIN	tblLGLoad					SH	ON	SH.intLoadId			=	CQ.intLoadId 
-			JOIN	tblICItemUOM				IU	ON	IU.intItemId			=	CQ.intItemId	
-													AND IU.intUnitMeasureId		=	SH.intWeightUnitMeasureId
-			JOIN	tblICUnitMeasure			IM	ON	IM.intUnitMeasureId		=	IU.intUnitMeasureId		CROSS	
+			LEFT JOIN	tblLGLoad					SH	ON	SH.intLoadId			=	CQ.intLoadId 
+			LEFT JOIN	tblICItemUOM				IU	ON	IU.intItemId			=	CQ.intItemId	
+													AND IU.intUnitMeasureId		=	ISNULL(SH.intWeightUnitMeasureId, CQ.intItemUOMId)
+			LEFT JOIN	tblICUnitMeasure			IM	ON	IM.intUnitMeasureId		=	IU.intUnitMeasureId		CROSS	
 			APPLY	tblLGCompanyPreference		LP 	
+			INNER JOIN tblICUnitMeasure LPM ON  LPM.intUnitMeasureId = LP.intWeightUOMId
 
 			UNION ALL
 
