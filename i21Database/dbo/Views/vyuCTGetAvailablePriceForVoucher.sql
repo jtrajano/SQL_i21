38d765c8b934e0dcf8f8ab402a203323d231b307
jayson_ramos <jayson.ramos@irely.com>
2021-06-10 13:03:53 +0800

    CT-5917
    (cherry picked from commit 088112ae1561049d0c9f3e0db32fb579abab4bbf)

--- i21Database/dbo/Views/vyuCTGetAvailablePriceForVoucher.sql
+++ i21Database/dbo/Views/vyuCTGetAvailablePriceForVoucher.sql
@@ -74,11 +74,14 @@
 					
 				from        
 					tblCTContractDetail cd
-					left join tblAPBillDetail bd on bd.intContractDetailId = cd.intContractDetailId and isnull(bd.intSettleStorageId,0) = 0        
+					left join tblAPBillDetail bd1 on bd1.intContractDetailId = cd.intContractDetailId and isnull(bd1.intSettleStorageId,0) = 0 and bd1.intInventoryReceiptChargeId is null       
 					left join tblICCommodityUnitMeasure co on co.intCommodityUnitMeasureId = cd.intPriceItemUOMId
 					left join tblICItemUOM iu on iu.intItemId = cd.intItemId and iu.intUnitMeasureId = co.intUnitMeasureId
+					left join tblAPBill b on b.intBillId = bd1.intBillId and b.intTransactionType = 1
+                    left join tblAPBillDetail bd on bd.intContractDetailId = cd.intContractDetailId and isnull(bd.intSettleStorageId,0) = 0 and bd.intBillId = b.intBillId and bd.intInventoryReceiptChargeId is null       
 				where
-					not exists (select top 1 1 from tblCTPriceFixation pf, tblCTPriceContract pc where pc.intPriceContractId = pf.intPriceContractId and pf.intContractDetailId = cd.intContractDetailId)
+					cd.dblCashPrice is not null
+					and not exists (select top 1 1 from tblCTPriceFixation pf, tblCTPriceContract pc where pc.intPriceContractId = pf.intPriceContractId and pf.intContractDetailId = cd.intContractDetailId)
 				group by        
 					cd.intContractDetailId        
 					,cd.dblQuantity        
