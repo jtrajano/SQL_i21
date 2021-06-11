dc4710fa5f76fa2b898f00b84494044d7ebb1233
jayson_ramos <jayson.ramos@irely.com>
2021-06-11 14:26:24 +0800

    CT-5874

--- i21Database/dbo/Views/vyuCTCompanyPreference.sql
+++ i21Database/dbo/Views/vyuCTCompanyPreference.sql
@@ -98,6 +98,7 @@ AS
 			,CP.ysnAutoCompleteDPDeliveryDate
 			,CP.intPricingDecimals
 			,CP.ysnEnableHTAMultiplePricing
+			,CP.ysnAllowBasisSequencePriceChangeWhenPartiallyPriced
 	FROM	tblCTCompanyPreference		CP LEFT
 	JOIN	tblICUnitMeasure			U1	ON	U1.intUnitMeasureId			=	CP.intCleanCostUOMId		LEFT
 	JOIN	tblSMCurrency				C1	ON	C1.intCurrencyID			=	CP.intCleanCostCurrencyId	LEFT
