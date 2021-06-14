3898be065fc396e5de53c32bd298ef7627255aa9
Jett Michael Marcella <jett.marcella@irely.com>
2021-06-14 17:07:23 +0800

    Revert "FRM-9745"
    This reverts commit b361ba1a9321ce688510dfe646d5da299ecb55cb.

--- i21PreDeployment/Scripts/Pre-Deployment/PreDeployment.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/PreDeployment.sql
@@ -10,8 +10,7 @@
 --------------------------------------------------------------------------------------
 */
 
--- Fixes the Kill State Error. Applicable only for SQL Server 2019 or higher
-:r .\4_SM\2010_Kill_State_Error_Fix.sql
+
 
 --EM
 :r .\12_EM\17_DropDependencies_RenameTable.sql
