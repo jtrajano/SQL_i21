0d4d438835a1b19b11024b70709b7432afbb9fb9
cris.lazaga <cris.lazaga@irely.com>
2021-06-15 14:16:15 +0800

    TR-1606

--- i21Database/dbo/Tables/tblTRImportAttachment.sql
+++ i21Database/dbo/Tables/tblTRImportAttachment.sql
@@ -6,6 +6,7 @@
 	[intUserId] INT NOT NULL,
 	[intConcurrencyId] INT NOT NULL DEFAULT ((1)),
 	[strSource] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL,
+	[strMessage] NVARCHAR(300) COLLATE Latin1_General_CI_AS NULL,
 	CONSTRAINT [PK_tblTRImportAttachment] PRIMARY KEY (intImportAttachmentId)
 )
 GO
