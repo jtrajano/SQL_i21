e44e813590a79745f2792fe7e1b9cba4e6061000
Erick Dela Cruz <erick.delacruz@irely.com>
2021-06-18 19:19:33 +0800

    AP-9742

--- i21Database/dbo/Stored Procedures/uspICRepostBillCostAdjustment.sql
+++ i21Database/dbo/Stored Procedures/uspICRepostBillCostAdjustment.sql
@@ -423,7 +423,7 @@ BEGIN
 		AND B.intInventoryReceiptChargeId IS NOT NULL 
 		AND rc.ysnInventoryCost = 1 --create cost adjustment entries for Inventory only for inventory cost yes
 		AND (
-			(B.dblCost <> (CASE WHEN rc.strCostMethod = 'Amount' THEN rc.dblAmount ELSE rc.dblRate END))
+			(B.dblCost <> (CASE WHEN rc.strCostMethod IN ('Amount','Percentage') THEN rc.dblAmount ELSE rc.dblRate END))
 			OR ISNULL(NULLIF(rc.dblForexRate,0),1) <> B.dblRate
 		)
 		AND A.intTransactionReversed IS NULL
