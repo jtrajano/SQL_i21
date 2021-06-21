502ee5c2353d8b104413dc363a226c620dca7321
Jeff Trajano <jeffrey.trajano@irely.com>
2021-06-21 15:12:46 +0800

    GL-8235

--- i21Database/dbo/Stored Procedures/uspCMUndepositedFundReport.sql
+++ i21Database/dbo/Stored Procedures/uspCMUndepositedFundReport.sql
@@ -11,7 +11,7 @@ AS
 DECLARE @temp_xml_table TABLE (
 	id INT IDENTITY(1,1)
 	,[fieldname] NVARCHAR(50)
-	,condition NVARCHAR(20)
+	,condition NVARCHAR(20)      
 	,[from] NVARCHAR(50)
 	,[to] NVARCHAR(50)
 	,[join] NVARCHAR(10)
@@ -36,13 +36,13 @@ DECLARE @dtmDateFrom DATETIME,@dtmDateTo DATETIME,@dtmCMDate DATETIME, @dtmTemp 
 
 IF EXISTS(SELECT 1 FROM @temp_xml_table)
 BEGIN
+	
 
-
-	SELECT
+	SELECT 
 		@dtmDateFrom = [from],
 		@dtmTemp = [from],
 		@dtmDateTo =  [to],
-		@condition  = condition
+		@condition  = condition 
 	FROM @temp_xml_table WHERE [fieldname] = 'dtmDate' --and @condition <> 'Equal To'
 
 	IF @condition IS NULL RETURN;
@@ -50,7 +50,7 @@ BEGIN
 
 	IF @condition <> 'Equal To'
 	BEGIN
-
+		
 
 		DECLARE @dtmDateCurrent DATETIME
 		select @dtmDateCurrent = CAST(CONVERT(nvarchar(20), GETDATE(), 101) AS DATETIME)
@@ -63,15 +63,26 @@ BEGIN
 				ELSE ISNULL(@dtmDateTo,@dtmDateFrom)
 			END
 
-		SELECT @dtmCMDate = DATEADD( DAY, 1, @dtmDateTo)
+		SELECT @dtmCMDate = DATEADD( DAY, 1, @dtmDateTo) 
 		SELECT @dtmDateTo = DATEADD( SECOND,-1, DATEADD(DAY, 1 ,@dtmDateTo))
 	END
 	ELSE
 
 	BEGIN
 		SELECT @dtmDateTo = DATEADD( SECOND,-1, DATEADD(DAY, 1 ,@dtmDateFrom))
-		SELECT @dtmCMDate = DATEADD( SECOND, 1, @dtmDateTo)
+		SELECT @dtmCMDate = DATEADD( SECOND, 1, @dtmDateTo) 
 	END
+
+	DECLARE @intUserId INT, @intBankAccountId INT
+	select TOP 1 @intUserId = intEntityId from tblSMConnectedUser order by dtmConnectDate desc
+	SELECT TOP 1 @intBankAccountId = intBankAccountId FROM tblCMBankAccount
+	IF @intBankAccountId IS NULL
+	BEGIN
+		RAISERROR('There are no bank accounts', 16, 1);
+		RETURN;
+	END
+		
+	EXEC [dbo].[uspCMRefreshUndepositedFundsFromOrigin]	@intBankAccountId = @intBankAccountId,@intUserId = @intUserId
 
 
 	DECLARE @strLocation NVARCHAR(50)
@@ -101,7 +112,7 @@ BEGIN
 	'' AS strBatchId
 	UNION
 
-	SELECT
+	SELECT 
 	a.rowId,
 	@dtmDateFrom as dtmDateFrom,
 	@dtmDateTo as dtmDateTo,
@@ -125,8 +136,8 @@ BEGIN
 	GL.strBatchId
 	FROM dbo.fnCMUndepositedFundReport(@dtmDateFrom,@dtmDateTo,@dtmCMDate) a
 	OUTER APPLY(
-		SELECT TOP 1 strBatchId FROM tblGLDetail
-		WHERE strTransactionId = a.strSourceTransactionId
+		SELECT TOP 1 strBatchId FROM tblGLDetail 
+		WHERE strTransactionId = a.strSourceTransactionId 
 		AND strTransactionType = 'Receive Payments'
 		AND ysnIsUnposted = 0
 	)GL
