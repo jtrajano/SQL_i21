458a788d2db2af3edae729cecff5c08f224feb57
Kenneth Lew Lagunero <kenneth.lagunero@irely.com>
2021-06-21 15:10:33 +0800

    SC-4170
    # Conflicts:
    #	i21Database/Scripts/Post-Deployment/PostDeployment.sql
    #	i21Database/dbo/Tables/tblSCTicket.sql
    #	i21Database/i21Database.sqlproj

--- i21Database/dbo/Tables/tblSCTicket.sql
+++ i21Database/dbo/Tables/tblSCTicket.sql
@@ -157,6 +157,7 @@
     [ysnExportRailXML] BIT NOT NULL DEFAULT 0,
     [strTrailerId] NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL,
     [intTransferWeight] INT NOT NULL DEFAULT 1, 
+    [dblDWGOriginalNetUnits] NUMERIC(38, 20) NULL,
     CONSTRAINT [PK_tblSCTicket_intTicketId] PRIMARY KEY CLUSTERED ([intTicketId] ASC),
     CONSTRAINT [UK_tblSCTicket_intTicketPoolId_strTicketNumber] UNIQUE ([intTicketPoolId], [intTicketType], [strInOutFlag], [strTicketNumber],[intEntityId],[intProcessingLocationId]),
 	CONSTRAINT [FK_tblSCScaleSetup_tblSMCompanyLocation_intTicketLocationId] FOREIGN KEY ([intTicketLocationId]) REFERENCES [tblSMCompanyLocation]([intCompanyLocationId]),
