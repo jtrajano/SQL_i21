9a33edf133e8b77b7c010b2b0b23a03f486bf951
jayson.ramos <jayson.ramos@irely.com>
2021-06-22 14:34:29 +0800

    CT-5958
    (cherry picked from commit ca824fc5ffa50906f547a7529cce61fcb6a62baa)
    (cherry picked from commit 4c21cf1529d21af355a8261b2d942a798baedd5f)

--- i21Database/dbo/Stored Procedures/uspCTCreateVoucher.sql
+++ i21Database/dbo/Stored Procedures/uspCTCreateVoucher.sql
@@ -50,7 +50,7 @@ begin try
 		,@intPayToAddressId				INT
 		,@intCurrencyId					INT
 		,@dtmDate						DATETIME
-		,@dtmVoucherDate				DATETIME
+		,@dtmVoucherDate				DATETIME = getdate()
 		,@dtmDueDate					DATETIME
 		,@strVendorOrderNumber			NVARCHAR (MAX)
 		,@strReference					NVARCHAR(400)
@@ -420,8 +420,8 @@ begin try
 					,intShipFromEntityId = vp.intShipFromEntityId
 					,intPayToAddressId = vp.intPayToAddressId
 					,intCurrencyId = vp.intCurrencyId
-					,dtmDate = getdate()
-					,dtmVoucherDate = getdate()
+					,dtmDate = (case when @dtmVoucherDate > vp.dtmDate then @dtmVoucherDate else vp.dtmDate end)
+					,dtmVoucherDate = (case when @dtmVoucherDate > vp.dtmVoucherDate then @dtmVoucherDate else vp.dtmVoucherDate end)
 					,dtmDueDate = vp.dtmDueDate
 					,strVendorOrderNumber = vp.strVendorOrderNumber
 					,strReference = vp.strReference
@@ -836,8 +836,8 @@ begin try
 			,intShipFromEntityId = vp.intShipFromEntityId
 			,intPayToAddressId = vp.intPayToAddressId
 			,intCurrencyId = vp.intCurrencyId
-			,dtmDate = getdate()
-			,dtmVoucherDate = getdate()
+			,dtmDate = (case when @dtmVoucherDate > vp.dtmDate then @dtmVoucherDate else vp.dtmDate end)
+			,dtmVoucherDate = (case when @dtmVoucherDate > vp.dtmVoucherDate then @dtmVoucherDate else vp.dtmVoucherDate end)
 			,dtmDueDate = vp.dtmDueDate
 			,strVendorOrderNumber = vp.strVendorOrderNumber
 			,strReference = vp.strReference
