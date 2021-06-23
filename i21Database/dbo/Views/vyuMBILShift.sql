4f5a6b33288362359d79f8a00c35e930072935e1
axelcasauran <axel.casauran@irely.com>
2021-06-23 15:55:08 +0800

    MBIL-491
    (cherry picked from commit 53ed0a772ed441db50048b7caccdcba98f9b7630)
    (cherry picked from commit 59b0cf5861a3e51aa37ffc5f8a9a447dbdab4b5b)
    (cherry picked from commit e37e669c99060c298783254d4eaecfffe8f053ea)

--- i21Database/dbo/Views/vyuMBILShift.sql
+++ i21Database/dbo/Views/vyuMBILShift.sql
@@ -28,12 +28,12 @@ LEFT JOIN (
 				intShiftId, SUM(dblFuelGallonsDelievered) as dblFuelGallonsDelievered, SUM(dblFuelSales) as dblFuelSales
 			FROM (
 					SELECT 
-						intShiftId, SUM(dblQuantity) as dblFuelGallonsDelievered, SUM(dblQuantity) * dblPrice as dblFuelSales
+						intShiftId, SUM(dblQuantity) as dblFuelGallonsDelievered,  (ISNULL(dblItemTotal,0) + ISNULL(dblTaxTotal,0)) as dblFuelSales
 					FROM 
 						vyuMBILInvoiceItem InvoiceItem
 						INNER JOIN tblICItem Item ON Item.intItemId = InvoiceItem.intItemId
 						WHERE Item.strType = 'Inventory' and Item.ysnAvailableTM = 1
-						GROUP BY intShiftId, dblQuantity, dblPrice
+						GROUP BY intShiftId, dblQuantity, dblItemTotal, dblTaxTotal
 				) tblTotalItem
 				GROUP BY intShiftId
 		) Items ON Items.intShiftId = Shift.intShiftId
