1c2b7ff97edd1d45af0a995cdcd65df9ae4ca572
nelsonreyes19 <nelson.reyes@irely.com>
2021-06-23 08:42:54 +0800

    ST-1930
    Add changes from customer

--- i21Database/dbo/Views/vyuSTTranslogDetails.sql
+++ i21Database/dbo/Views/vyuSTTranslogDetails.sql
@@ -29,6 +29,11 @@ FROM
 	   , CAST(TR.intCashierSysId AS INT) AS intCashierSysId
 	   , TR.strCashier
 	   , RIGHT('0' + CONVERT(VARCHAR(2), DATEPART(HOUR, TR.dtmDate)), 2) as Hr
+	   , CAST(Right(intCashierPosNum , 1) AS INT) as intRegister
+	   , CAST((CASE WHEN LEN(strTrlFuelBasePrice) > 0
+				THEN 1
+				ELSE 0
+				END) AS BIT)  as ysnFuel
 	   --, USec.intEntityId
 	FROM tblSTTranslogRebates TR
 	JOIN tblSTStore ST 
@@ -37,4 +42,5 @@ FROM
 	-- INNER JOIN tblSMUserSecurityCompanyLocationRolePermission RolePerm
 		-- ON USec.intEntityId = RolePerm.intEntityId
 		-- AND ST.intCompanyLocationId = RolePerm.intCompanyLocationId
+	 WHERE (strTransRollback IS NULL) AND (strTransFuelPrepayCompletion IS NULL)
 ) x
