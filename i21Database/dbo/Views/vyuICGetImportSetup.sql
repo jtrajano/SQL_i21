832f4ae7541ac7af44078a02e81d96e94594b43f
Feb Montefrio <feb.montefrio@irely.com>
2021-06-24 14:43:41 +0800

    [F] IC-9093

---
+++ i21Database/dbo/Views/vyuICGetImportSetup.sql
@@ -0,0 +1,17 @@
+﻿CREATE VIEW [dbo].[vyuICGetImportSetup]
+AS 
+
+SELECT 
+	s.intImportSetupId
+	,s.strName
+	,s.strFolder
+	,s.strArchiveFolder
+	,strSchedule = schedule.strDescription
+	,strTemplate = template.strName
+FROM
+	tblICImportSetup s LEFT JOIN tblSCHSchedule schedule 
+		ON s.intScheduleId = schedule.intScheduleId
+	LEFT JOIN tblICEdiMapTemplate template
+		ON s.intEdiMapTemplateId = template.intEdiMapTemplateId
+
+GO 
