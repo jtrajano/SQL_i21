96015f6af782dab438e91487dc1c7d4eb16276d8
von.pasion <von.pasion@irely.com>
2021-06-25 09:21:52 +0800

    FRM-9782

---
+++ i21Database/dbo/Tables/tblSMLocalization.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tblSMLocalization]
+(
+	[intLocalizationId]	INT IDENTITY (1, 1) NOT NULL,
+	[intScreenLabelId]	INT NOT NULL,
+	[intLanguageId]		INT NOT NULL,
+    [strTranslation]	NVARCHAR (100) COLLATE Latin1_General_CI_AS NOT NULL,
+	[intConcurrencyId]  INT NOT NULL DEFAULT 0,
+	CONSTRAINT [FK_tblSMLocalization_tblSMLanguage] FOREIGN KEY ([intLanguageId]) REFERENCES [dbo].[tblSMLanguage] ([intLanguageId]),
+	CONSTRAINT [FK_tblSMLocalization_tblSMScreenLabel] FOREIGN KEY ([intScreenLabelId]) REFERENCES [dbo].[tblSMScreenLabel] ([intScreenLabelId]),
+	CONSTRAINT [PK_tblSMLocalization] PRIMARY KEY CLUSTERED ([intLocalizationId] ASC)
+)
