1e3bf7ad09246515e6d119ffecb6bcb51c50d704
Vinoth Kumar T <vinoth.t@irely.com>
2021-06-27 20:15:05 +0530

    IN-886

--- i21Database/dbo/Views/vyuQMGetSample.sql
+++ i21Database/dbo/Views/vyuQMGetSample.sql
@@ -1,7 +1,16 @@
 ﻿CREATE VIEW vyuQMGetSample
 AS
 SELECT SPS.intSamplePreStageId AS TrxSequenceNo
-	,0 AS RecordStatus
+	,intRecordStatus AS RecordStatus
+	,CASE SPS.strRowState
+		WHEN 'Added'
+			THEN 1
+		WHEN 'Modified'
+			THEN 2
+		WHEN 'Delete'
+			THEN 4
+		ELSE 2
+		END AS ActionId
 	,S.strSampleNumber AS SampleNo
 	,ST.strSampleTypeName AS SampleType
 	,CH.strContractNumber + ' - ' + LTRIM(CD.intContractSeq) AS ContractNo
@@ -15,18 +24,19 @@ SELECT SPS.intSamplePreStageId AS TrxSequenceNo
 	,CONVERT(NUMERIC(18, 0), ISNULL(dblRepresentingQty, 0)) AS Qty
 	,CONVERT(VARCHAR, CD.dtmStartDate, 112) AS ContractDeliveryMonth
 	,CONVERT(VARCHAR, S.dtmSampleReceivedDate, 112) AS SampleReceivedDate
-	,NULL AS ActionId -- 1 / 2 / 4
 	,S.dtmCreated AS CreatedDateTime
 	,CE.strName AS CreatedBy
 	,C.strCountry AS Origin
 	,CD.strGrade AS Grade
 	,CLSL.strSubLocationName AS StorageLocation
 FROM dbo.tblQMSamplePreStage SPS
-JOIN dbo.tblQMSample S ON S.intSampleId = SPS.intSampleId
-JOIN dbo.tblQMSampleType ST ON ST.intSampleTypeId = S.intSampleTypeId
-JOIN dbo.tblICItem I ON I.intItemId = S.intItemId
-JOIN dbo.tblSMCountry C ON C.intCountryID = S.intCountryID
-JOIN dbo.tblSMCompanyLocationSubLocation CLSL ON CLSL.intCompanyLocationSubLocationId = S.intCompanyLocationSubLocationId
+JOIN dbo.tblQMSampleType ST ON ST.intSampleTypeId = SPS.intSampleTypeId
+JOIN dbo.tblICItem I ON I.intItemId = SPS.intItemId
+JOIN tblICCommodity COM ON COM.intCommodityId = I.intCommodityId
+	AND COM.strCommodityCode = 'Coffee'
+JOIN dbo.tblSMCountry C ON C.intCountryID = SPS.intCountryID
+JOIN dbo.tblSMCompanyLocationSubLocation CLSL ON CLSL.intCompanyLocationSubLocationId = SPS.intCompanyLocationSubLocationId
+LEFT JOIN dbo.tblQMSample S ON S.intSampleId = SPS.intSampleId
 LEFT JOIN dbo.tblCTContractDetail AS CD ON CD.intContractDetailId = S.intContractDetailId
 LEFT JOIN dbo.tblCTContractHeader CH ON CH.intContractHeaderId = CD.intContractHeaderId
 LEFT JOIN dbo.tblEMEntity E ON E.intEntityId = S.intEntityId
