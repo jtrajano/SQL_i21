32cb6426b638a5c81b21847c6bdc233a427535c0
Arman Arenas <arman.arenas@irely.com>
2021-06-28 21:31:17 +0800

    LG-2989 use Scheduled Date when posting to GL
    # Conflicts:
    #	i21Database/dbo/Stored Procedures/uspLGPostInventoryShipment.sql

--- i21Database/dbo/Stored Procedures/uspLGPostInTransitCosting.sql
+++ i21Database/dbo/Stored Procedures/uspLGPostInTransitCosting.sql
@@ -123,7 +123,7 @@ BEGIN TRY
 			intItemId = LD.intItemId
 			,intItemLocationId = IL.intItemLocationId
 			,intItemUOMId = ISNULL(LD.intWeightItemUOMId, LD.intItemUOMId) 
-			,dtmDate = GETDATE()
+			,dtmDate = L.dtmScheduledDate
 			,dblQty = LD.dblQuantity
 			,dblUOMQty = IU.dblUnitQty
 			,dblCost = dbo.fnDivide(
@@ -419,7 +419,7 @@ BEGIN
 		,[strInventoryReceiptId]  = L.strLoadNumber
 		,[strReceiptType] = 'Purchase Contract'
 		,[intSourceType] = -1
-		,[dtmDate] = GETDATE()
+		,[dtmDate] = L.dtmScheduledDate
 		,[intCurrencyId] = NULL
 		,[dblExchangeRate] = 1
 		-- Detail 
