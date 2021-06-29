82f2a34e8682b5f7dca1b1eb9566dbefda1b1fd0
bponce <bonn.ponce@irely.com>
2021-06-29 16:18:00 +0800

    CF-2748
    (cherry picked from commit e05d527286282422ad8c3ca145d83c1c9ce2c66b)
    (cherry picked from commit 07fcd559ef89e8efbae1cafdd59fc9aa88286327)
    (cherry picked from commit af9e4a4f75419c74a0b3666ad8350849de529f9b)
    (cherry picked from commit 4909651e3da7682592fe1bb9aad1dc378b968969)
    (cherry picked from commit 5de4716ed74e0da376d7d9111b59baa98420c1f6)

--- i21Database/dbo/Stored Procedures/uspCFRecalculateTransaciton.sql
+++ i21Database/dbo/Stored Procedures/uspCFRecalculateTransaciton.sql
@@ -6891,6 +6891,8 @@ BEGIN
 	DECLARE @ysnDuplicate BIT = 0
 	DECLARE @intParentId INT = 0
 
+	
+
 	IF (@strTransactionType != 'Foreign Sale')
 	BEGIN
 		SELECT @intDupTransCount = COUNT(*)
@@ -6914,10 +6916,12 @@ BEGIN
 
 		SELECT @intDupTransCount = COUNT(*)
 		FROM tblCFTransaction
+		LEFT JOIN tblCFCard 
+		ON tblCFCard.intCardId = tblCFTransaction.intCardId
 		WHERE intNetworkId = @intNetworkId
 		AND intSiteId = @intSiteId
 		AND dtmTransactionDate = @dtmTransactionDate
-		AND ISNULL(strForeignCardId,'') = ISNULL(@ForeignCardId,'')
+		AND (ISNULL(strForeignCardId,'') = ISNULL(@ForeignCardId,'') OR ISNULL(tblCFCard.strCardId,'') = ISNULL(@ForeignCardId,''))
 		AND intProductId = @ProductId
 		AND intPumpNumber = @PumpId
 		AND intTransactionId != @intTransactionId
