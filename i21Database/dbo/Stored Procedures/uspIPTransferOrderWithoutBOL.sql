b94abc3d3a99aabac2a5b3df67dfcdf9aa7b7c01
Premkumar S <prem.selladurai@irely.com>
2021-06-30 01:40:05 +0530

    IN-890

--- i21Database/dbo/Stored Procedures/uspIPTransferOrderWithoutBOL.sql
+++ i21Database/dbo/Stored Procedures/uspIPTransferOrderWithoutBOL.sql
@@ -5,6 +5,9 @@ DECLARE @strStyle NVARCHAR(MAX)
 	,@strHeader NVARCHAR(MAX)
 	,@strDetail NVARCHAR(MAX) = ''
 	,@strMessage NVARCHAR(MAX)
+	,@dtmCurrentDate datetime
+
+SELECT @dtmCurrentDate = CONVERT(DATETIME, CONVERT(CHAR, Getdate(), 101))
 
 SET @strStyle = '<style type="text/css" scoped>
 					table.GeneratedTable {
@@ -71,8 +74,9 @@ SELECT @strDetail = @strDetail + '<tr>
 	JOIN dbo.tblSMCompanyLocation DL on DL.intCompanyLocationId =IT.intToLocationId 
 	JOIN dbo.tblEMEntity E on E.intEntityId =IT.intShipViaId 
 	JOIN dbo.tblICStatus S on S.intStatusId =IT.intStatusId 
-	Where IsNULL(IT.strBolNumber,'')=''
-	and IT.ysnShipmentRequired =1
+	WHERE IsNULL(IT.strBolNumber,'')=''
+	AND IT.ysnShipmentRequired =1
+	AND IT.dtmTransferDate =@dtmCurrentDate
 
 SET @strHtml = REPLACE(@strHtml, '@header', @strHeader)
 SET @strHtml = REPLACE(@strHtml, '@detail', @strDetail)
