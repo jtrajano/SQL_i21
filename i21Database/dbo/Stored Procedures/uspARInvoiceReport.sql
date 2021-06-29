8dcf4fda668f7d25d7897f18c14a5c7bd3ba7d5c
Jeremiah Villanueva <jeremiah.villanueva@irely.com>
2021-06-29 11:13:51 +0800

    AR-13473

--- i21Database/dbo/Stored Procedures/uspARInvoiceReport.sql
+++ i21Database/dbo/Stored Procedures/uspARInvoiceReport.sql
@@ -276,7 +276,7 @@ SELECT intInvoiceId				= INV.intInvoiceId
 	 , dtmCreated				= GETDATE()
 	 , strServiceChargeItem		= CASE WHEN SELECTEDINV.strInvoiceFormat 
 										IN ('By Customer Balance', 'By Invoice') 
-										THEN 'Service Charge on Past Due ' + CHAR(13) + 'Balance as of: ' +  CONVERT(VARCHAR(10), INV.dtmDate, 101)
+										THEN 'Service Charge on Past Due' + CHAR(13) + 'Balance as of: ' +  CAST(CAST(INV.dtmDate AS DATE) AS VARCHAR)
 										ELSE
 										''
 										END
