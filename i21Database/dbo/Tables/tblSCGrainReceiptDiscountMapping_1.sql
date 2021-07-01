a4927bfb0a2d4259d686948f96075202abd774fc
Mon Gonzales <mon.gonzales@irely.com>
2021-07-01 12:52:41 +0800

    GRN-2668 - Create a New Grain Receipt Template

---
+++ i21Database/dbo/Tables/tblSCGrainReceiptDiscountMapping_1.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[tblSCGrainReceiptDiscountMapping]
+(
+	[intGrainReceiptDiscountMapping200Id]	int not null identity(1,1)
+	,[intCommodityId]						int not null	   
+    ,[intGradeId]							int not null    
+	,[intConcurrencyId]						int default(0) not null 
+	,CONSTRAINT [PK_tblSCGrainReceiptDiscountMapping200_intGrainReceiptDiscountMapping200Id] PRIMARY KEY CLUSTERED ([intGrainReceiptDiscountMapping200Id] ASC)
+	,CONSTRAINT [UK_tblSCGrainReceiptDiscountMapping200_intCommodityId] UNIQUE ([intCommodityId] ASC)			
+	,CONSTRAINT [FK_tblSCGrainReceiptDiscountMapping200_tblICItem_GradeId] FOREIGN KEY ([intGradeId]) REFERENCES [tblICItem]([intItemId])
+)
