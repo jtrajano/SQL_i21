dd25a136684e7ee8529adf7818c0872681637847
jayson.ramos <jayson.ramos@irely.com>
2021-07-02 10:45:58 +0800

    CT-5974
    (cherry picked from commit e7b446963ea793bfc3bfaecb8089e1f61199f2f7)
    (cherry picked from commit 503f48732681131ecf252420622c0b51cd36ca16)

--- i21Database/dbo/Stored Procedures/uspCTPriceFixationDetailDelete.sql
+++ i21Database/dbo/Stored Procedures/uspCTPriceFixationDetailDelete.sql
@@ -92,7 +92,7 @@ BEGIN TRY
 				FROM vyuAPBillPayment a
 				JOIN tblAPPayment b on b.intPaymentId = a.intPaymentId
 				JOIN tblSMPaymentMethod c on c.intPaymentMethodID = b.intPaymentMethodId
-				WHERE a.intBillId = @Id and c.strPaymentMethod in ('Cash','Check','eCheck')
+				WHERE a.intBillId = @Id and c.strPaymentMethod in ('Cash','Check','eCheck','ACH')
 			)
 		begin
 			EXEC uspAPDeletePayment @Id, @intUserId
