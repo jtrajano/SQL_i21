10869e427a58cc05badc7e7b5c29b9e7b51f3611
von.pasion <von.pasion@irely.com>
2021-07-07 16:55:29 +0800

    FRM-9782

--- i21Database/dbo/Stored Procedures/uspSMImportLocalization.sql
+++ i21Database/dbo/Stored Procedures/uspSMImportLocalization.sql
@@ -14,11 +14,23 @@ BEGIN TRANSACTION
 	USING tblSMLocalizationStaging B ON (A.strLabel = B.strLabel AND A.intLanguageId = @intLanguageId)
 	--When records are matched, update the records if there is any change
 	WHEN MATCHED AND A.strTranslation <> B.strTranslation
-	THEN UPDATE SET A.strTranslation = B.strTranslation, A.ysnEdited = 1
+	THEN UPDATE SET A.strTranslation = B.strTranslation
 	--When no records are matched, insert the incoming records from source table to target table
 	WHEN NOT MATCHED BY TARGET 
 	THEN INSERT (strLabel, strTranslation, intLanguageId) 
 	VALUES (B.strLabel, B.strTranslation, @intLanguageId);
+
+	IF NOT EXISTS (SELECT 1 FROM tblSMLocalizationHistory WHERE intLanguageId = @intLanguageId)
+		BEGIN
+			INSERT INTO tblSMLocalizationHistory (intLanguageId, strUnique, dtmUpdated, intConcurrencyId)
+			SELECT @intLanguageId, NEWID(), GETUTCDATE(), 1
+		END
+	ELSE 
+		BEGIN
+			UPDATE tblSMLocalizationHistory
+			SET strUnique = NEWID(), dtmUpdated = GETUTCDATE()
+			WHERE intLanguageId = @intLanguageId
+		END
 
 	-- Delete screen(s) staging that doesn't have conflicts
 	DELETE FROM tblSMLocalizationStaging
