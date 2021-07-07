5c104410bc58d9fc218669c68b90bf004f9f0fc2
Feb Montefrio <feb.montefrio@irely.com>
2021-07-07 17:12:53 +0800

    [F] IC-9093

---
+++ i21Database/dbo/Tables/tblICImportSetup.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE [dbo].[tblICImportSetup]
+(
+	[intImportSetupId] INT IDENTITY(1, 1) NOT NULL,
+	[strName] NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL,
+	[strFolder] NVARCHAR(500) COLLATE Latin1_General_CI_AS NULL,
+	[strArchiveFolder] NVARCHAR(500) COLLATE Latin1_General_CI_AS NULL,
+	[intEdiMapTemplateId] INT NULL,	
+	[intScheduleId] INT NULL,	
+	[dtmDateCreated] DATETIME NULL,
+	[dtmDateModified] DATETIME NULL,
+	[intCreatedByUserId] INT NULL,
+	[intModifiedByUserId] INT NULL,
+	[intConcurrencyId] INT NULL, 
+    CONSTRAINT [PK_tblICImportSetup] PRIMARY KEY ([intImportSetupId]), 
+	CONSTRAINT [AK_tblICImportSetup_strName] UNIQUE ([strName]), 
+	CONSTRAINT [FK_tblICImportSetup_tblSCHSchedule] FOREIGN KEY ([intScheduleId]) REFERENCES [tblSCHSchedule]([intScheduleId]), 
+	CONSTRAINT [FK_tblICImportSetup_tblICEdiMapTemplate] FOREIGN KEY ([intEdiMapTemplateId]) REFERENCES [tblICEdiMapTemplate]([intEdiMapTemplateId])
+)
+GO
+
+CREATE NONCLUSTERED INDEX [IX_tblICImportSetup_strName]
+	ON [dbo].[tblICImportSetup]([strName] ASC)
+GO
---
+++ i21Database/dbo/Views/vyuICGetImportSetup.sql
@@ -0,0 +1,53 @@
+﻿CREATE VIEW [dbo].[vyuICGetImportSetup]
+AS 
+
+SELECT 
+	s.intImportSetupId
+	,s.strName
+	,s.strFolder
+	,s.strArchiveFolder
+	,strSchedule = schedule.strDescription
+	,strTemplate = template.strName
+	,s.intEdiMapTemplateId
+	,s.intScheduleId 
+	,strCronExpression = 
+		/*
+			CRON Expression
+			-------------------------------------------------------------
+			Field			Description
+			------------	-----------------------------------------------------
+			Seconds			0-59
+			Minutes			0-59
+			Hours			0-23
+			Day of Month	1-31 
+			Month			1-12 or JAN-DEC
+			Day of Week		0-6 or SUN-SAT
+		*/
+		dbo.fnFormatMessage(
+			'0 %s %s %s * %s'
+			,ISNULL(CAST(DATEPART(MINUTE, schedule.dtmRunTime) AS NVARCHAR(3)), '*') -- Minute
+			,ISNULL(CAST(DATEPART(HOUR, schedule.dtmRunTime) AS NVARCHAR(3)), '*') -- Hour
+			,ISNULL(CAST(NULLIF(schedule.intDayOfMonth, 0) AS NVARCHAR(3)), '?') -- Day of Month
+			,dbo.fnCronDayOfWeek(
+				schedule.ysnSunday
+				,schedule.ysnMonday
+				,schedule.ysnTuesday
+				,schedule.ysnWednesday
+				,schedule.ysnThursday
+				,schedule.ysnFriday
+				,schedule.ysnSaturday
+			) -- Day of Week
+			,DEFAULT 
+			,DEFAULT 
+			,DEFAULT 
+			,DEFAULT 
+			,DEFAULT 
+			,DEFAULT 
+		) 
+FROM
+	tblICImportSetup s LEFT JOIN tblSCHSchedule schedule 
+		ON s.intScheduleId = schedule.intScheduleId
+	LEFT JOIN tblICEdiMapTemplate template
+		ON s.intEdiMapTemplateId = template.intEdiMapTemplateId
+
+GO 
