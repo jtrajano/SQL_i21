fa936ab9e9a80bdbef7b783fb242f3b5ec82067b
von.pasion <von.pasion@irely.com>
2021-07-09 07:43:09 +0800

    FRM-9782

--- i21Database/Scripts/Post-Deployment/SM/2120_InsertLanguages.sql
+++ i21Database/Scripts/Post-Deployment/SM/2120_InsertLanguages.sql
@@ -4,8 +4,8 @@ print('/*******************  BEGIN INSERTING LANGUAGES *******************/')
 
 IF NOT EXISTS (SELECT 1 FROM tblSMLanguage WHERE strLanguage = 'Spanish')
 BEGIN
-	INSERT INTO tblSMLanguage (strLanguage, intConcurrencyId)
-	VALUES ('Spanish', 1)
+	INSERT INTO tblSMLanguage (strLanguage, ysnDefault, intConcurrencyId)
+	VALUES ('Spanish', 0, 1)
 END
 
 print('/*******************  END INSERTING LANGUAGES  *******************/')
