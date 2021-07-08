9f2e8bcbb57740c4259c7c53ade67a923e65e4cf
von.pasion <von.pasion@irely.com>
2021-07-08 14:13:43 +0800

    FRM-9782

--- i21Database/Scripts/Post-Deployment/PostDeployment.sql
+++ i21Database/Scripts/Post-Deployment/PostDeployment.sql
@@ -99,6 +99,7 @@ print 'BEGIN POST DEPLOYMENT'
 :r .\SM\2110_CreateIDPGridLayout.sql
 :r .\SM\2110_UpdateAuditLogs.sql
 :r .\SM\2120_AddUserPortalSettingsToContactImport.sql
+:r .\SM\2120_InsertLanguages.sql
 
 
 -- Canned Report
---
+++ i21Database/Scripts/Post-Deployment/SM/2120_InsertLanguages.sql
@@ -0,0 +1,13 @@
+﻿GO
+print('/*******************  BEGIN INSERTING LANGUAGES *******************/')
+
+
+IF NOT EXISTS (SELECT 1 FROM tblSMLanguage WHERE strLanguage = 'Spanish')
+BEGIN
+	INSERT INTO tblSMLanguage (strLanguage, intConcurrencyId)
+	VALUES ('Spanish', 1)
+END
+
+print('/*******************  END INSERTING LANGUAGES  *******************/')
+
+GO
