b738a127b891ae0de7ef5884d2a829d1f6c60b1a
Samuel Marvin Aguilos <samuel.aguilos@irely.com>
2021-07-09 00:43:55 +0800

    IC-9603

--- i21Database/dbo/Stored Procedures/uspICAddTransactionLinks.sql
+++ i21Database/dbo/Stored Procedures/uspICAddTransactionLinks.sql
@@ -5,24 +5,62 @@ BEGIN
 DECLARE @GraphId UNIQUEIDENTIFIER = NEWID()
 DECLARE @LinkDate DATE = GETUTCDATE()
 
-INSERT INTO tblICTransactionLinks(
-	guiTransactionGraphId, dtmLinkUtcDate, strOperation,
-	intSrcId, strSrcTransactionNo, strSrcModuleName, strSrcTransactionType,
-	intDestId, strDestTransactionNo, strDestModuleName, strDestTransactionType)
-SELECT COALESCE(related.guiTransactionGraphId, @GraphId), @LinkDate, link.strOperation,
-	link.intSrcId, link.strSrcTransactionNo, link.strSrcModuleName, link.strSrcTransactionType,
-	link.intDestId, link.strDestTransactionNo, link.strDestModuleName, link.strDestTransactionType
-FROM @TransactionLinks link
-OUTER APPLY (
-	SELECT TOP 1 nodes.guiTransactionGraphId
-	FROM tblICTransactionNodes nodes
-	WHERE nodes.strTransactionNo = link.strSrcTransactionNo
-) related
-WHERE NOT EXISTS(
-	SELECT TOP 1 1 FROM tblICTransactionLinks
-	WHERE (strSrcTransactionNo = link.strSrcTransactionNo AND strSrcTransactionNo = link.strDestTransactionNo) OR
-		(strDestTransactionNo = link.strSrcTransactionNo AND strDestTransactionNo = link.strDestTransactionNo)
-)
+
+;MERGE INTO tblICTransactionLinks AS TARGET
+USING (
+	SELECT 
+		COALESCE(related.guiTransactionGraphId, @GraphId) AS guiTransactionGraphId, 
+		@LinkDate AS dtmLinkUtcDate, 
+		link.strOperation AS strOperation,
+		link.intSrcId AS intSrcId, 
+		link.strSrcTransactionNo AS strSrcTransactionNo, 
+		link.strSrcModuleName AS strSrcModuleName, 
+		link.strSrcTransactionType AS strSrcTransactionType,
+		link.intDestId AS intDestId, 
+		link.strDestTransactionNo AS strDestTransactionNo, 
+		link.strDestModuleName AS strDestModuleName, 
+		link.strDestTransactionType AS strDestTransactionType
+	FROM @TransactionLinks link
+	OUTER APPLY (
+		SELECT TOP 1 nodes.guiTransactionGraphId
+		FROM tblICTransactionNodes nodes
+		WHERE nodes.strTransactionNo = link.strSrcTransactionNo
+	) related
+	WHERE NOT EXISTS(
+		SELECT TOP 1 1 FROM tblICTransactionLinks
+		WHERE (strSrcTransactionNo = link.strSrcTransactionNo AND strSrcTransactionNo = link.strDestTransactionNo) OR
+			(strDestTransactionNo = link.strSrcTransactionNo AND strDestTransactionNo = link.strDestTransactionNo)
+	)
+) AS SOURCE 
+ON 
+(
+	TARGET.intDestId = SOURCE.intDestId 
+	AND 
+	TARGET.strDestTransactionNo = SOURCE.strDestTransactionNo 
+	AND 
+	TARGET.strDestModuleName = SOURCE.strDestModuleName 
+	AND 
+	TARGET.strDestTransactionType = SOURCE.strDestTransactionType
+) 
+WHEN MATCHED AND TARGET.intSrcId IS NULL THEN
+UPDATE SET 
+	TARGET.intSrcId = SOURCE.intSrcId, 
+	TARGET.strSrcTransactionNo = SOURCE.strSrcTransactionNo, 
+	TARGET.strSrcModuleName = SOURCE.strSrcModuleName,
+	TARGET.strSrcTransactionType = SOURCE.strSrcTransactionType
+WHEN NOT MATCHED THEN
+	INSERT
+	(
+		guiTransactionGraphId, dtmLinkUtcDate, strOperation,
+		intSrcId, strSrcTransactionNo, strSrcModuleName, strSrcTransactionType,
+		intDestId, strDestTransactionNo, strDestModuleName, strDestTransactionType
+	)
+	VALUES 
+	(
+		SOURCE.guiTransactionGraphId, SOURCE.dtmLinkUtcDate, SOURCE.strOperation,
+		SOURCE.intSrcId, SOURCE.strSrcTransactionNo, SOURCE.strSrcModuleName, SOURCE.strSrcTransactionType,
+		SOURCE.intDestId, SOURCE.strDestTransactionNo, SOURCE.strDestModuleName, SOURCE.strDestTransactionType
+	);
 
 ;WITH CTE 
 AS (
