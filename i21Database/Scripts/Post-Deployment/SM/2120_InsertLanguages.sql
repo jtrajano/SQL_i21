3b6f298fc2ed4cfae8bca8705294f4ec34511541
von.pasion <von.pasion@irely.com>
2021-07-09 09:37:55 +0800

    FRM-9782 - Added languages

--- i21Database/Scripts/Post-Deployment/SM/2120_InsertLanguages.sql
+++ i21Database/Scripts/Post-Deployment/SM/2120_InsertLanguages.sql
@@ -8,6 +8,55 @@ BEGIN
 	VALUES ('Spanish', 0, 1)
 END
 
+IF NOT EXISTS (SELECT 1 FROM tblSMLanguage WHERE strLanguage = 'German')
+BEGIN
+	INSERT INTO tblSMLanguage (strLanguage, ysnDefault, intConcurrencyId)
+	VALUES ('German', 0, 1)
+END
+
+IF NOT EXISTS (SELECT 1 FROM tblSMLanguage WHERE strLanguage = 'Portuguese')
+BEGIN
+	INSERT INTO tblSMLanguage (strLanguage, ysnDefault, intConcurrencyId)
+	VALUES ('Portuguese', 0, 1)
+END
+
+
+IF NOT EXISTS (SELECT 1 FROM tblSMLanguage WHERE strLanguage = 'French')
+BEGIN
+	INSERT INTO tblSMLanguage (strLanguage, ysnDefault, intConcurrencyId)
+	VALUES ('French', 0, 1)
+END
+
+IF NOT EXISTS (SELECT 1 FROM tblSMLanguage WHERE strLanguage = 'Russian')
+BEGIN
+	INSERT INTO tblSMLanguage (strLanguage, ysnDefault, intConcurrencyId)
+	VALUES ('Russian', 0, 1)
+END
+
+IF NOT EXISTS (SELECT 1 FROM tblSMLanguage WHERE strLanguage = 'Vietnamese')
+BEGIN
+	INSERT INTO tblSMLanguage (strLanguage, ysnDefault, intConcurrencyId)
+	VALUES ('Vietnamese', 0, 1)
+END
+
+IF NOT EXISTS (SELECT 1 FROM tblSMLanguage WHERE strLanguage = 'Indonesian')
+BEGIN
+	INSERT INTO tblSMLanguage (strLanguage, ysnDefault, intConcurrencyId)
+	VALUES ('Indonesian', 0, 1)
+END
+
+IF NOT EXISTS (SELECT 1 FROM tblSMLanguage WHERE strLanguage = 'Polish')
+BEGIN
+	INSERT INTO tblSMLanguage (strLanguage, ysnDefault, intConcurrencyId)
+	VALUES ('Polish', 0, 1)
+END
+
+IF NOT EXISTS (SELECT 1 FROM tblSMLanguage WHERE strLanguage = 'Italian')
+BEGIN
+	INSERT INTO tblSMLanguage (strLanguage, ysnDefault, intConcurrencyId)
+	VALUES ('Italian', 0, 1)
+END
+
 print('/*******************  END INSERTING LANGUAGES  *******************/')
 
 GO
