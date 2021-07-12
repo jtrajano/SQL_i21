cf3cef2f9377bc02729ccf3ec1b90eae3f1625a6
Ed Ibe <ed.ibe@irely.com>
2021-07-12 13:15:00 +0800

    SM-5163
    (cherry picked from commit 365004f045a1abb7b528a4d56fd91dee25b1f434)

---
+++ i21Database/Scripts/Post-Deployment/EM/DataFix/2110_RemoveDuplicateUsers.sql
@@ -0,0 +1,35 @@
+﻿PRINT N'Start removing user entity to contact with duplicates'
+
+GO
+
+DECLARE @tblEMEntityWithDuplicate TABLE (
+	intEntityId INT
+)
+
+DECLARE @tblEMEntityContactDuplicateToDelete TABLE (
+	intEntityId INT
+)
+
+/* Get user entities to contact with duplicates */
+
+INSERT INTO @tblEMEntityWithDuplicate
+SELECT A.intEntityId FROM tblEMEntityToContact A
+	INNER JOIN tblSMUserSecurity B ON A.intEntityId = B.intEntityId
+	GROUP BY A.intEntityId
+	HAVING COUNT(*) > 1
+
+IF (SELECT COUNT(*) FROM @tblEMEntityWithDuplicate) > 0
+BEGIN
+	INSERT INTO @tblEMEntityContactDuplicateToDelete
+	SELECT intEntityContactId FROM tblEMEntityToContact A
+		INNER JOIN tblEMEntity B ON A.intEntityContactId = B.intEntityId
+		WHERE A.intEntityId IN (SELECT intEntityId FROM @tblEMEntityWithDuplicate) AND (ISNULL(strEmail, '') = '')
+	
+	/* delete duplicates with entity's contact email is null or empty string, since creating a user requires email */
+
+	DELETE FROM tblEMEntityToContact WHERE intEntityContactId IN (SELECT * FROM @tblEMEntityContactDuplicateToDelete)
+END
+
+GO
+
+PRINT N'End removing user entity to contact with duplicates'
