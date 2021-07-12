772c43804d98e8e09f079296c76973672edbd35b
von.pasion <von.pasion@irely.com>
2021-07-12 18:27:31 +0800

    FRM-9814

---
+++ i21Database/dbo/Tables/tblSMLanguageTranslationStaging.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[tblSMLanguageTranslationStaging]
+(
+	[intLanguageTranslationStagingId]	INT IDENTITY (1, 1) NOT NULL,
+	[strLabel]					NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NOT NULL,
+    [strTranslation]			NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NOT NULL,
+	CONSTRAINT [PK_tblSMLanguageTranslationStaging] PRIMARY KEY CLUSTERED ([intLanguageTranslationStagingId] ASC)
+)
--- i21Database/dbo/Tables/tblSMLocalizationStaging.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[tblSMLocalizationStaging]
-(
-	[intLocalizationStagingId]	INT IDENTITY (1, 1) NOT NULL,
-	[strLabel]					NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NOT NULL,
-    [strTranslation]			NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NOT NULL,
-	CONSTRAINT [PK_tblSMLocalizationStaging] PRIMARY KEY CLUSTERED ([intLocalizationStagingId] ASC)
-)
