9277ef30c600a8481ff58a31b002e3585a1678f9
von.pasion <von.pasion@irely.com>
2021-07-13 18:33:39 +0800

    FRM-9841

--- i21Database/Scripts/Post-Deployment/SM/2120_InsertLanguages.sql
+++ i21Database/Scripts/Post-Deployment/SM/2120_InsertLanguages.sql
@@ -2,59 +2,59 @@
 print('/*******************  BEGIN INSERTING LANGUAGES *******************/')
 
 
-IF NOT EXISTS (SELECT 1 FROM tblSMLanguage WHERE strLanguage = 'Spanish')
+IF NOT EXISTS (SELECT 1 FROM tblSMLanguage WHERE strLanguage = 'Español')
 BEGIN
 	INSERT INTO tblSMLanguage (strLanguage, ysnDefault, intConcurrencyId)
-	VALUES ('Spanish', 0, 1)
+	VALUES ('Español', 0, 1)
 END
 
-IF NOT EXISTS (SELECT 1 FROM tblSMLanguage WHERE strLanguage = 'German')
+IF NOT EXISTS (SELECT 1 FROM tblSMLanguage WHERE strLanguage = 'Deutsch')
 BEGIN
 	INSERT INTO tblSMLanguage (strLanguage, ysnDefault, intConcurrencyId)
-	VALUES ('German', 0, 1)
+	VALUES ('Deutsch', 0, 1)
 END
 
-IF NOT EXISTS (SELECT 1 FROM tblSMLanguage WHERE strLanguage = 'Portuguese')
+IF NOT EXISTS (SELECT 1 FROM tblSMLanguage WHERE strLanguage = 'Português')
 BEGIN
 	INSERT INTO tblSMLanguage (strLanguage, ysnDefault, intConcurrencyId)
-	VALUES ('Portuguese', 0, 1)
+	VALUES ('Português', 0, 1)
 END
 
 
-IF NOT EXISTS (SELECT 1 FROM tblSMLanguage WHERE strLanguage = 'French')
+IF NOT EXISTS (SELECT 1 FROM tblSMLanguage WHERE strLanguage = 'Français')
 BEGIN
 	INSERT INTO tblSMLanguage (strLanguage, ysnDefault, intConcurrencyId)
-	VALUES ('French', 0, 1)
+	VALUES ('Français', 0, 1)
 END
 
-IF NOT EXISTS (SELECT 1 FROM tblSMLanguage WHERE strLanguage = 'Russian')
+IF NOT EXISTS (SELECT 1 FROM tblSMLanguage WHERE strLanguage = 'Русский')
 BEGIN
 	INSERT INTO tblSMLanguage (strLanguage, ysnDefault, intConcurrencyId)
-	VALUES ('Russian', 0, 1)
+	VALUES ('Русский', 0, 1)
 END
 
-IF NOT EXISTS (SELECT 1 FROM tblSMLanguage WHERE strLanguage = 'Vietnamese')
+IF NOT EXISTS (SELECT 1 FROM tblSMLanguage WHERE strLanguage = '㗂越')
 BEGIN
 	INSERT INTO tblSMLanguage (strLanguage, ysnDefault, intConcurrencyId)
-	VALUES ('Vietnamese', 0, 1)
+	VALUES ('㗂越', 0, 1)
 END
 
-IF NOT EXISTS (SELECT 1 FROM tblSMLanguage WHERE strLanguage = 'Indonesian')
+IF NOT EXISTS (SELECT 1 FROM tblSMLanguage WHERE strLanguage = 'Bahasa Indonesia')
 BEGIN
 	INSERT INTO tblSMLanguage (strLanguage, ysnDefault, intConcurrencyId)
-	VALUES ('Indonesian', 0, 1)
+	VALUES ('Bahasa Indonesia', 0, 1)
 END
 
-IF NOT EXISTS (SELECT 1 FROM tblSMLanguage WHERE strLanguage = 'Polish')
+IF NOT EXISTS (SELECT 1 FROM tblSMLanguage WHERE strLanguage = 'Polski')
 BEGIN
 	INSERT INTO tblSMLanguage (strLanguage, ysnDefault, intConcurrencyId)
-	VALUES ('Polish', 0, 1)
+	VALUES ('Polski', 0, 1)
 END
 
-IF NOT EXISTS (SELECT 1 FROM tblSMLanguage WHERE strLanguage = 'Italian')
+IF NOT EXISTS (SELECT 1 FROM tblSMLanguage WHERE strLanguage = 'Italiano')
 BEGIN
 	INSERT INTO tblSMLanguage (strLanguage, ysnDefault, intConcurrencyId)
-	VALUES ('Italian', 0, 1)
+	VALUES ('Italiano', 0, 1)
 END
 
 print('/*******************  END INSERTING LANGUAGES  *******************/')
