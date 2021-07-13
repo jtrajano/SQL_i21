db635fe8759e6c886bc3882ed3a45eaaa2e95bcd
Feb Montefrio <feb.montefrio@irely.com>
2021-07-13 17:48:34 +0800

    [F] IC-9611

--- i21Database/dbo/Stored Procedures/uspICEdiImportPricebook.sql
+++ i21Database/dbo/Stored Procedures/uspICEdiImportPricebook.sql
@@ -8,6 +8,8 @@
 	, @TotalRows INT OUTPUT
 AS
 
+SET @intVendorId = NULLIF(@intVendorId, '0') 
+
 DECLARE @LogId INT
 SELECT @LogId = intImportLogId FROM tblICImportLog WHERE strUniqueId = (SELECT TOP 1 strUniqueId FROM tblICEdiPricebook)
 
@@ -17,6 +19,15 @@ BEGIN
 	SELECT 'Import Pricebook successful', 'EDI', @strFileType, @strFileName, GETDATE(), @intUserId, 1
 	SET @LogId = @@IDENTITY
 END
+
+DECLARE 
+	@updatedItems AS INT = 0
+	,@updatedItemUOM AS INT = 0
+	,@updatedItemPricing AS INT = 0
+	,@updatedItemLocation AS INT = 0 
+	,@updatedSpecialItemPricing AS INT = 0
+	,@updatedVendorXRef AS INT = 0
+	,@insertedVendorXRef AS INT = 0 
 
 --Update Item
 UPDATE i
@@ -32,6 +43,8 @@ FROM
 		ON i.intItemId = u.intItemId
 	LEFT OUTER JOIN tblICBrand b 
 		ON b.strBrandName = p.strManufacturersBrandName
+
+SET @updatedItems = @@ROWCOUNT; 
 
 -------------------------------------------------
 -- BEGIN Validation 
@@ -120,8 +133,7 @@ IF OBJECT_ID('tempdb..#tmpICEdiImportPricebook_tblICItemVendorXref') IS NULL
 DECLARE 
 	@TotalRowsUpdated AS INT = 0 
 	,@TotalRowsInserted AS INT = 0 
-
-	
+	   	
 -- Update UOM
 UPDATE u
 SET u.intUnitMeasureId = ISNULL(ISNULL(m.intUnitMeasureId, s.intUnitMeasureId), u.intUnitMeasureId)
@@ -136,7 +148,7 @@ FROM
 	LEFT OUTER JOIN tblICUnitMeasure s 
 		ON s.strSymbol = NULLIF(p.strItemUnitOfMeasure, '')
 
-SET @TotalRowsUpdated = ISNULL(@TotalRowsUpdated, 0) + @@ROWCOUNT; 
+SET @updatedItemUOM = @@ROWCOUNT;
 	
 -- Update Item Location
 UPDATE l
@@ -161,6 +173,7 @@ SET   l.intClassId = ISNULL(ISNULL(sc.intSubcategoryId, catLoc.intClassId), l.in
 	, l.ysnIdRequiredCigarette = ISNULL(l.ysnIdRequiredCigarette, catLoc.ysnIdRequiredCigarette)
 	, l.ysnIdRequiredLiquor = ISNULL(l.ysnIdRequiredLiquor, catLoc.ysnIdRequiredLiquor)
 	, l.intMinimumAge = ISNULL(l.intMinimumAge, catLoc.intMinimumAge)
+	, l.intCountGroupId = cg.intCountGroupId
 FROM tblICEdiPricebook p
 	INNER JOIN tblICItemUOM u 
 		ON ISNULL(NULLIF(u.strLongUPCCode, ''), u.strUpcCode) = p.strSellingUpcNumber
@@ -168,19 +181,23 @@ FROM tblICEdiPricebook p
 		ON i.intItemId = u.intItemId
 	INNER JOIN tblICItemLocation l 
 		ON l.intItemId = i.intItemId
-	LEFT OUTER JOIN tblSTSubcategory sc 
+	INNER JOIN @Locations loc 
+		ON loc.intCompanyLocationId = l.intLocationId
+	LEFT JOIN tblSTSubcategory sc 
 		ON sc.strSubcategoryId = NULLIF(p.strProductClass, '')
 		AND sc.strSubcategoryType = 'C'
-	LEFT OUTER JOIN tblSTSubcategory sf 
+	LEFT JOIN tblSTSubcategory sf 
 		ON sf.strSubcategoryId = NULLIF(p.strProductFamily, '')
 		AND sf.strSubcategoryType = 'F'
-	LEFT OUTER JOIN tblICCategory cat 
+	LEFT JOIN tblICCategory cat 
 		ON cat.intCategoryId = i.intCategoryId
-	LEFT OUTER JOIN tblICCategoryLocation catLoc 
+	LEFT JOIN tblICCategoryLocation catLoc 
 		ON catLoc.intCategoryId = cat.intCategoryId
 		AND catLoc.intLocationId = l.intLocationId
+	LEFT JOIN tblICCountGroup cg
+		ON cg.strCountGroup = p.strInventoryGroup
 
-SET @TotalRowsUpdated = ISNULL(@TotalRowsUpdated, 0) + @@ROWCOUNT; 
+SET @updatedItemLocation = @@ROWCOUNT;
 
 -- Update Standard Pricing
 UPDATE price
@@ -195,20 +212,32 @@ SET   price.dblSalePrice = CAST(CASE WHEN ISNUMERIC(p.strRetailPrice) = 1 THEN p
 	, price.dtmDateModified = GETDATE()
 	, price.intModifiedByUserId = @intUserId
 FROM tblICEdiPricebook p
-	INNER JOIN tblICItemUOM u ON ISNULL(NULLIF(u.strLongUPCCode, ''), u.strUpcCode) = p.strSellingUpcNumber
-	INNER JOIN tblICItem i ON i.intItemId = u.intItemId
-	INNER JOIN tblICItemLocation il ON il.intItemId = i.intItemId
-	INNER JOIN @Locations loc ON loc.intCompanyLocationId = il.intLocationId
-	INNER JOIN tblICItemPricing price ON price.intItemId = i.intItemId
+	INNER JOIN tblICItemUOM u 
+		ON ISNULL(NULLIF(u.strLongUPCCode, ''), u.strUpcCode) = p.strSellingUpcNumber
+	INNER JOIN tblICItem i 
+		ON i.intItemId = u.intItemId
+	INNER JOIN tblICItemLocation il 
+		ON il.intItemId = i.intItemId
+	INNER JOIN @Locations loc 
+		ON loc.intCompanyLocationId = il.intLocationId
+	INNER JOIN tblICItemPricing price 
+		ON price.intItemId = i.intItemId
 		AND il.intItemLocationId = price.intItemLocationId
-	LEFT OUTER JOIN tblICCategory cat ON cat.intCategoryId = i.intCategoryId
-	LEFT OUTER JOIN tblICCategoryLocation catLoc ON catLoc.intCategoryId = cat.intCategoryId
+	INNER JOIN vyuAPVendor v
+		ON (v.strVendorId = p.strVendorId AND @intVendorId IS NULL) 
+		OR (v.intEntityId = @intVendorId AND @intVendorId IS NOT NULL)
+	INNER JOIN tblICCategory cat 
+		ON cat.intCategoryId = i.intCategoryId
+	INNER JOIN tblICCategoryLocation catLoc 
+		ON catLoc.intCategoryId = cat.intCategoryId
 		AND catLoc.intLocationId = il.intLocationId
-	LEFT OUTER JOIN tblICCategoryVendor catV ON catV.intCategoryLocationId = catLoc.intCategoryLocationId
-		AND catV.intVendorId = @intVendorId
-WHERE catV.ysnUpdatePrice = 1
-
-SET @TotalRowsUpdated = ISNULL(@TotalRowsUpdated, 0) + @@ROWCOUNT; 
+	INNER JOIN tblICCategoryVendor catV 
+		ON catV.intCategoryLocationId = catLoc.intCategoryLocationId
+		AND catV.intVendorId = v.intEntityId
+WHERE 
+	catV.ysnUpdatePrice = 1
+	
+SET @updatedItemPricing = @@ROWCOUNT;
 
 --SELECT cat.strCategoryCode, catV.intVendorId
 --FROM tblICEdiPricebook p
@@ -241,12 +270,16 @@ FROM tblICEdiPricebook p
 	INNER JOIN tblICCategory cat ON cat.intCategoryId = i.intCategoryId
 	INNER JOIN tblICCategoryLocation catLoc ON catLoc.intCategoryId = cat.intCategoryId
 		AND catLoc.intLocationId = il.intLocationId
-	INNER JOIN tblICCategoryVendor catV ON catV.intCategoryLocationId = catLoc.intCategoryLocationId
-		AND catV.intVendorId = @intVendorId
-WHERE catV.intVendorId = @intVendorId
-	AND catV.ysnUpdatePrice = 1
+	INNER JOIN vyuAPVendor v
+		ON (v.strVendorId = p.strVendorId AND @intVendorId IS NULL) 
+		OR (v.intEntityId = @intVendorId AND @intVendorId IS NOT NULL)
+	INNER JOIN tblICCategoryVendor catV ON 
+		catV.intCategoryLocationId = catLoc.intCategoryLocationId
+		AND catV.intVendorId = v.intEntityId
+WHERE 
+	catV.ysnUpdatePrice = 1
 
-SET @TotalRowsUpdated = ISNULL(@TotalRowsUpdated, 0) + @@ROWCOUNT; 
+SET @updatedSpecialItemPricing = @@ROWCOUNT;
 
 -- Upsert the Vendor XRef (Cross Reference)
 INSERT INTO #tmpICEdiImportPricebook_tblICItemVendorXref (
@@ -367,11 +400,14 @@ FROM (
 	, intVendorId_New
 );
 
-SELECT @TotalRowsUpdated = ISNULL(@TotalRowsUpdated, 0) + COUNT(1) FROM #tmpICEdiImportPricebook_tblICItemVendorXref WHERE strAction = 'UPDATE'
-SELECT @TotalRowsInserted = ISNULL(@TotalRowsInserted, 0) + COUNT(1) FROM #tmpICEdiImportPricebook_tblICItemVendorXref WHERE strAction = 'INSERTED'
+SELECT @updatedVendorXRef = COUNT(1) FROM #tmpICEdiImportPricebook_tblICItemVendorXref WHERE strAction = 'UPDATE'
+SELECT @insertedVendorXRef = COUNT(1) FROM #tmpICEdiImportPricebook_tblICItemVendorXref WHERE strAction = 'INSERTED'
 
 SELECT @ErrorCount = COUNT(*) FROM tblICImportLogDetail WHERE intImportLogId = @LogId AND strType = 'Error'
 SELECT @TotalRows = COUNT(*) FROM tblICEdiPricebook WHERE strUniqueId = (SELECT TOP 1 strUniqueId FROM tblICImportLogDetail WHERE intImportLogId = @LogId)
+
+SET @TotalRowsUpdated = @TotalRowsUpdated + @updatedItems + @updatedItemUOM + @updatedItemPricing + @updatedItemLocation + @updatedSpecialItemPricing + @updatedVendorXRef
+SET @TotalRowsInserted = @TotalRowsInserted + @insertedVendorXRef 
 
 IF @ErrorCount > 0
 BEGIN
@@ -395,6 +431,203 @@ BEGIN
 		intRowsImported = @TotalRowsInserted
 	WHERE 
 		intImportLogId = @LogId
+
+
+	-- Log the updated items. 
+	IF @updatedItems <> 0 
+	BEGIN 
+		INSERT INTO tblICImportLogDetail(
+			intImportLogId
+			, strType
+			, intRecordNo
+			, strField
+			, strValue
+			, strMessage
+			, strStatus
+			, strAction
+			, intConcurrencyId
+		)
+		SELECT 
+			@LogId
+			, 'Info'
+			, NULL
+			, NULL 
+			, NULL 
+			, dbo.fnFormatMessage('%i item(s) are updated.', @updatedItems,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT)
+			, 'Success'
+			, 'Updated'
+			, 1
+		WHERE 
+			@updatedItems <> 0 
+	END 
+
+	-- Log the updated item uom. 
+	IF @updatedItemUOM <> 0 
+	BEGIN 
+		INSERT INTO tblICImportLogDetail(
+			intImportLogId
+			, strType
+			, intRecordNo
+			, strField
+			, strValue
+			, strMessage
+			, strStatus
+			, strAction
+			, intConcurrencyId
+		)
+		SELECT 
+			@LogId
+			, 'Info'
+			, NULL
+			, NULL 
+			, NULL 
+			, dbo.fnFormatMessage('%i item UOM(s) are updated.', @updatedItemUOM,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT)
+			, 'Success'
+			, 'Updated'
+			, 1
+		WHERE 
+			@updatedItemUOM <> 0 
+	END 
+
+	-- Log the updated item pricing. 
+	IF @updatedItemPricing <> 0 
+	BEGIN 
+		INSERT INTO tblICImportLogDetail(
+			intImportLogId
+			, strType
+			, intRecordNo
+			, strField
+			, strValue
+			, strMessage
+			, strStatus
+			, strAction
+			, intConcurrencyId
+		)
+		SELECT 
+			@LogId
+			, 'Info'
+			, NULL
+			, NULL 
+			, NULL 
+			, dbo.fnFormatMessage('%i item pricing record(s) are updated.', @updatedItemPricing,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT)
+			, 'Success'
+			, 'Updated'
+			, 1
+		WHERE 
+			@updatedItemPricing <> 0 
+	END
+
+	-- Log the updated item location. 
+	IF @updatedItemLocation <> 0 
+	BEGIN 
+		INSERT INTO tblICImportLogDetail(
+			intImportLogId
+			, strType
+			, intRecordNo
+			, strField
+			, strValue
+			, strMessage
+			, strStatus
+			, strAction
+			, intConcurrencyId
+		)
+		SELECT 
+			@LogId
+			, 'Info'
+			, NULL
+			, NULL 
+			, NULL 
+			, dbo.fnFormatMessage('%i item location record(s) are updated.', @updatedItemLocation,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT)
+			, 'Success'
+			, 'Updated'
+			, 1
+		WHERE 
+			@updatedItemLocation <> 0 
+	END
+
+	-- Log the updated item special pricing. 
+	IF @updatedSpecialItemPricing <> 0 
+	BEGIN 
+		INSERT INTO tblICImportLogDetail(
+			intImportLogId
+			, strType
+			, intRecordNo
+			, strField
+			, strValue
+			, strMessage
+			, strStatus
+			, strAction
+			, intConcurrencyId
+		)
+		SELECT 
+			@LogId
+			, 'Info'
+			, NULL
+			, NULL 
+			, NULL 
+			, dbo.fnFormatMessage('%i item special pricing record(s) are updated.', @updatedSpecialItemPricing,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT)
+			, 'Success'
+			, 'Updated'
+			, 1
+		WHERE 
+			@updatedSpecialItemPricing <> 0 
+	END
+
+	-- Log the updated vendor xref
+	IF @updatedVendorXRef <> 0 
+	BEGIN 
+		INSERT INTO tblICImportLogDetail(
+			intImportLogId
+			, strType
+			, intRecordNo
+			, strField
+			, strValue
+			, strMessage
+			, strStatus
+			, strAction
+			, intConcurrencyId
+		)
+		SELECT 
+			@LogId
+			, 'Info'
+			, NULL
+			, NULL 
+			, NULL 
+			, dbo.fnFormatMessage('%i vendor xref record(s) are updated.', @updatedVendorXRef,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT)
+			, 'Success'
+			, 'Updated'
+			, 1
+		WHERE 
+			@updatedVendorXRef <> 0 
+	END
+
+	-- Log the inserted vendor xref
+	IF @insertedVendorXRef <> 0 
+	BEGIN 
+		INSERT INTO tblICImportLogDetail(
+			intImportLogId
+			, strType
+			, intRecordNo
+			, strField
+			, strValue
+			, strMessage
+			, strStatus
+			, strAction
+			, intConcurrencyId
+		)
+		SELECT 
+			@LogId
+			, 'Info'
+			, NULL
+			, NULL 
+			, NULL 
+			, dbo.fnFormatMessage('%i vendor xref record(s) are created.', @insertedVendorXRef,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT)
+			, 'Success'
+			, 'Created'
+			, 1
+		WHERE 
+			@insertedVendorXRef <> 0 
+	END
 END 
 
 DELETE FROM tblICEdiPricebook
