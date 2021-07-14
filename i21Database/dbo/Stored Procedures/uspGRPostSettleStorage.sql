becc0e77c712a095d25d69c492e65774fcad74ab
Leslie Guevarra <leslie.arat@irely.com>
2021-07-14 16:44:35 +0800

    GRN-2761 (Contract pricing: error on pricing contract after settlement)

--- i21Database/dbo/Stored Procedures/uspGRPostSettleStorage.sql
+++ i21Database/dbo/Stored Procedures/uspGRPostSettleStorage.sql
@@ -2224,19 +2224,18 @@ BEGIN TRY
 																END
 														ELSE
 															CASE 
-																WHEN (a.intPricingTypeId = 2 OR a.intPricingTypeId in (1, 6) ) AND availableQtyForVoucher.intContractDetailId IS NOT NULL AND availableQtyForVoucher.dblAvailableQuantity > 0
+																WHEN (a.intPricingTypeId = 2 OR a.intPricingTypeId IN (1, 6) ) AND availableQtyForVoucher.intContractDetailId IS NOT NULL AND availableQtyForVoucher.dblAvailableQuantity > 0
 																	THEN 
 																		CASE 
-																			WHEN intItemType = 1 
-																				THEN availableQtyForVoucher.dblAvailableQuantity 
-																			ELSE ROUND((availableQtyForVoucher.dblAvailableQuantity / a.dblSettleContractUnits) * a.dblUnits,6)
+																			WHEN intItemType = 1 THEN availableQtyForVoucher.dblAvailableQuantity 
+																			ELSE ROUND((availableQtyForVoucher.dblAvailableQuantity / a.dblSettleContractUnits) * a.dblUnits, 15)
 																		END
 																WHEN @origdblSpotUnits > 0 
 																	THEN ROUND(dbo.fnCalculateQtyBetweenUOM(b.intItemUOMId,@intCashPriceUOMId,a.dblUnits),6) 
 																WHEN a.intPricingTypeId in (1, 6) AND @ysnFromPriceBasisContract = 1 
 																	THEN a.dblUnits -- @dblTotalVoucheredQuantity
 																WHEN @ysnFromPriceBasisContract = 1 AND (intItemType = 2 OR intItemType = 3)
-																	THEN ROUND((availableQtyForVoucher.dblAvailableQuantity / a.dblSettleContractUnits) * a.dblUnits, 15)
+																	THEN a.dblUnits
 																ELSE 
 																	CASE 
 																		WHEN @ysnFromPriceBasisContract = 1 AND  availableQtyForVoucher.intContractDetailId IS NULL  AND c.strType = 'Inventory' 
@@ -2245,8 +2244,7 @@ BEGIN TRY
 																			THEN a.dblUnits - @dblTotalVoucheredQuantity
 																		WHEN @ysnFromPriceBasisContract = 1  AND (@dblQtyFromCt + @dblTotalVoucheredQuantity) < a.dblUnits 
 																			THEN @dblQtyFromCt - a.dblUnits
-																		ELSE 
-																			a.dblUnits
+																		ELSE a.dblUnits
 																	END
 															END
 														END																							
