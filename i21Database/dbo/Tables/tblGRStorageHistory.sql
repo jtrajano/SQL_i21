22fd3f75b18f18774ed88ec43e144ed684b083cb
Christian Joel Martinez <Christian.Martinez@iRely.com>
2021-07-16 16:02:31 +0800

    GRN-2765

--- i21Database/dbo/Tables/tblGRStorageHistory.sql
+++ i21Database/dbo/Tables/tblGRStorageHistory.sql
@@ -64,3 +64,19 @@ INCLUDE (
 	,intContractHeaderId
 ) 
 GO 
+
+CREATE NONCLUSTERED INDEX [IX_tblGRStorageHistory_intInventoryReceiptId] ON [dbo].[tblGRStorageHistory]
+(
+	[intInventoryReceiptId] DESC
+)
+INCLUDE (
+	intCustomerStorageId
+	,intTicketId
+	,intContractHeaderId
+)
+GO
+CREATE NONCLUSTERED INDEX [IX_tblGRStorageHistory_intTicketId] ON [dbo].[tblGRStorageHistory]
+(
+	[intTicketId] ASC
+)
+GO 
