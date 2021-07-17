8dc3b8aad73cad9bf0e7aeca7c153e4b604daf97
Kenneth Lew Lagunero <kenneth.lagunero@irely.com>
2021-07-17 13:17:46 +0800

    SM-5176

--- i21Database/dbo/Stored Procedures/uspSMFixStartingNumbers.sql
+++ i21Database/dbo/Stored Procedures/uspSMFixStartingNumbers.sql
@@ -14,43 +14,45 @@ DECLARE @strPrefix AS NVARCHAR(50)
 DECLARE @intNumber AS INT 
 
 
--- 1: Fix the Batch Posting. -> from uspGLFixStartingNumbers
-BEGIN 
-	SET @strTransactionType = 'Batch Post'
-	SELECT	@strTransactionId = strPrefix + CAST(intNumber AS NVARCHAR(20)) 
-			,@intNumber = intNumber
-			,@strPrefix = strPrefix
-	FROM	dbo.tblSMStartingNumber
-	WHERE	strTransactionType = @strTransactionType
+-- Commented this since Batch ID generation is base on the Server DateTime float value no need to update the starting Number table.
+-- Starting number for batch posting is not utilizing the starting number table
+-- -- 1: Fix the Batch Posting. -> from uspGLFixStartingNumbers
+-- BEGIN 
+-- 	SET @strTransactionType = 'Batch Post'
+-- 	SELECT	@strTransactionId = strPrefix + CAST(intNumber AS NVARCHAR(20)) 
+-- 			,@intNumber = intNumber
+-- 			,@strPrefix = strPrefix
+-- 	FROM	dbo.tblSMStartingNumber
+-- 	WHERE	strTransactionType = @strTransactionType
 
-	IF EXISTS (SELECT TOP 1 1 FROM dbo.tblGLDetail)
-	BEGIN 
-		-- Retrieve the Max transaction id. 
-		SET @intNumber = NULL
+-- 	IF EXISTS (SELECT TOP 1 1 FROM dbo.tblGLDetail)
+-- 	BEGIN 
+-- 		-- Retrieve the Max transaction id. 
+-- 		SET @intNumber = NULL
 		
-		-- Retrieve the highest number part in the transaction id. 
-		SELECT	@intNumber = MAX(CAST(REPLACE(strBatchId, @strPrefix, '') AS INT))	
-		FROM	dbo.tblGLDetail  WHERE strBatchId LIKE @strPrefix + '%'
+-- 		-- Retrieve the highest number part in the transaction id. 
+-- 		SELECT	@intNumber = MAX(CAST(REPLACE(strBatchId, @strPrefix, '') AS INT))	
+-- 		FROM	dbo.tblGLDetail  WHERE strBatchId LIKE @strPrefix + '%'
 		
-		DECLARE @intSMNumber int = 0;
-		SELECT	@intSMNumber = MAX(CAST(REPLACE(strBatchNo, 'BATCH-', '') AS INT))	
-		FROM	dbo.tblSMBatchPostingLog
+-- 		DECLARE @intSMNumber int = 0;
+-- 		SELECT	@intSMNumber = MAX(CAST(REPLACE(strBatchNo, 'BATCH-', '') AS INT))	
+-- 		FROM	dbo.tblSMBatchPostingLog
 
-		IF (@intNumber IS NOT NULL AND ISNULL(@intSMNumber, 0) < @intNumber)	
-		BEGIN 	
-			-- Update the next transaction id. 
-			UPDATE	dbo.tblSMStartingNumber
-			SET		intNumber = @intNumber + 1 
-			WHERE	strTransactionType = @strTransactionType	
-		END 
-	END 
+-- 		IF (@intNumber IS NOT NULL AND ISNULL(@intSMNumber, 0) < @intNumber)	
+-- 		BEGIN 	
+-- 			-- Update the next transaction id. 
+-- 			UPDATE	dbo.tblSMStartingNumber
+-- 			SET		intNumber = @intNumber + 1 
+-- 			WHERE	strTransactionType = @strTransactionType	
+-- 		END 
+-- 	END 
 
-	-- Clean-up 
-	SET @strTransactionId = NULL
-	SET @strTransactionType = NULL 
-	SET @strPrefix = NULL 
-	SET @intNumber = NULL 
-END 
+-- 	-- Clean-up 
+-- 	SET @strTransactionId = NULL
+-- 	SET @strTransactionType = NULL 
+-- 	SET @strPrefix = NULL 
+-- 	SET @intNumber = NULL 
+-- END 
 
 -- 2: Fix the Debit Memo (Voucher/Ticket)
 BEGIN 
