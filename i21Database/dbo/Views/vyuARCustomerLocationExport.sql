8c163fffc455b982549645b30bcb6625703be2a3
Roudinee Panteria <roudinee.panteria@irely.com>
2021-07-20 10:13:42 +0800

    AR-13305

---
+++ i21Database/dbo/Views/vyuARCustomerLocationExport.sql
@@ -0,0 +1,24 @@
+CREATE VIEW [dbo].[vyuARCustomerLocationExport]
+AS
+SELECT intEntity				= ARC.intEntityId
+	 , strCustomerName			= EME.strName
+     , intEntityLocationId		= EMEL.intEntityLocationId
+     , strLocationName			= EMEL.strLocationName
+     , strAddress				= ISNULL(EMEL.strAddress, '')
+     , strCity					= ISNULL(EMEL.strCity, '')
+     , strCountry				= ISNULL(EMEL.strCountry, '')
+     , strCounty				= ISNULL(EMEL.strCounty, '')
+     , strState					= ISNULL(EMEL.strState, '')
+     , strZipCode				= ISNULL(EMEL.strZipCode, '')
+     , strPhone					= ISNULL(EMEL.strPhone, '')
+     , strFax					= ISNULL(EMEL.strFax, '')
+     , strNotes					= ISNULL(EMEL.strNotes, '')
+     , ysnActive				= EMEL.ysnActive
+     , dblLongitude				= EMEL.dblLongitude
+     , dblLatitude				= EMEL.dblLatitude
+     , strTimezone				= ISNULL(EMEL.strTimezone, '')
+     , strShipVia				= ISNULL(SMSV.strShipVia, '')
+FROM tblARCustomer ARC
+INNER JOIN tblEMEntityLocation EMEL ON ARC.intEntityId = EMEL.intEntityId
+INNER JOIN tblEMEntity EME ON ARC.intEntityId = EME.intEntityId
+LEFT JOIN tblSMShipVia SMSV ON EMEL.intShipViaId = SMSV.intEntityId
