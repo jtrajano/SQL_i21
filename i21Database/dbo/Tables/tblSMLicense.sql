1ebee7c1b8b934175ca6d3837a122dced535dd26
Chris Salvante <chris.salvante@irely.com>
2021-07-22 16:55:38 +0800

    FRM-9660

---
+++ i21Database/dbo/Tables/tblSMLicense.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[tblSMLicense]
+(
+	[intLicenseId]			INT NOT NULL IDENTITY(1,1),
+	[intEntityCustomerId]	INT NOT NULL,
+	[dtmDateRegistered]			DATETIME NOT NULL DEFAULT(GETDATE()),
+	[dtmDateExpiration]		DATETIME NOT NULL DEFAULT(GETDATE()),
+	[dtmSupportExpiration]	DATETIME NOT NULL DEFAULT(GETDATE()),
+	[intNumberOfAdmin]		INT NOT NULL DEFAULT(1),
+	[intNumberOfUser]		INT NOT NULL,
+	[strCompanyId]			NVARCHAR(100) COLLATE Latin1_General_CI_AS NOT NULL,
+	[strDescription]		NVARCHAR(150) COLLATE Latin1_General_CI_AS NOT NULL,
+	[strType]				NVARCHAR(300) COLLATE Latin1_General_CI_AS NOT NULL DEFAULT '',
+	[strURL]				NVARCHAR(300) COLLATE Latin1_General_CI_AS NOT NULL DEFAULT '',
+	[strUniqueId]			NVARCHAR(40) COLLATE Latin1_General_CI_AS NOT NULL,
+	[ysnExternalAccess]		BIT NOT NULL DEFAULT 0,
+    [intConcurrencyId]		INT CONSTRAINT [DF_tblSMLicense_intConcurrencyId] DEFAULT ((0)) NOT NULL,
+
+    CONSTRAINT [PK_tblSMLicense] PRIMARY KEY CLUSTERED ([intLicenseId] ASC),
+	CONSTRAINT [FK_tblSMLicense_tblARCustomer] FOREIGN KEY ([intEntityCustomerId]) REFERENCES [dbo].[tblARCustomer] ([intEntityId]) ON DELETE CASCADE
+)
