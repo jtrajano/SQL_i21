fefae7ad09103c7a0defd1f1d6e782fe0f0bb027
cris.lazaga <cris.lazaga@irely.com>
2021-07-22 16:15:58 +0800

    CCR-467

--- i21Database/dbo/Stored Procedures/uspCCTransactionLink.sql
+++ i21Database/dbo/Stored Procedures/uspCCTransactionLink.sql
@@ -11,32 +11,46 @@ BEGIN
     -- START TR-1611 - Sub ledger Transaction traceability
 	IF (ISNULL(@ForDelete, 0) = 0)
     BEGIN
-        DECLARE @tblTransactionLinks udtICTransactionLinks
-        
-        INSERT INTO @tblTransactionLinks (
-            strOperation
-            , intSrcId
-            , strSrcTransactionNo
-            , strSrcTransactionType
-            , strSrcModuleName
-            , intDestId
-            , strDestTransactionNo
-            , strDestTransactionType
-            , strDestModuleName
-        )    
-        SELECT strOperation	= 'Create'
-            , intSrcId = SH.intSiteHeaderId
-            , strSrcTransactionNo = SH.strCcdReference
-            , strSrcTransactionType = 'Dealer Credit Card'
-            , strSrcModuleName  = 'Credit Card Reconciliation'
-            , intDestId	= SH.intSiteHeaderId
-            , strDestTransactionNo = SH.strCcdReference
-            , strDestTransactionType = 'Dealer Credit Card'
-            , strDestModuleName = 'Credit Card Reconciliation'
-        FROM tblCCSiteHeader SH
+        DECLARE @intCreateLinkTransactionId INT, 
+			@strCreateLinkTransactionNo NVARCHAR(50),
+			@strCreateLinkTransactionType NVARCHAR(100),
+			@strCreateLinkModuleName NVARCHAR(100)
+
+		SELECT @intCreateLinkTransactionId = SH.intSiteHeaderId
+			, @strCreateLinkTransactionNo = SH.strCcdReference
+			, @strCreateLinkTransactionType  = 'Dealer Credit Card'
+			, @strCreateLinkModuleName = 'Credit Card Reconciliation'
+		FROM tblCCSiteHeader SH
 		WHERE SH.intSiteHeaderId = @intTransactionId
 
-        EXEC dbo.uspICAddTransactionLinks @tblTransactionLinks
+		EXEC dbo.uspICAddTransactionLinkOrigin @intCreateLinkTransactionId, @strCreateLinkTransactionNo, @strCreateLinkTransactionType, @strCreateLinkModuleName
+
+        -- DECLARE @tblTransactionLinks udtICTransactionLinks
+        
+        -- INSERT INTO @tblTransactionLinks (
+        --     strOperation
+        --     , intSrcId
+        --     , strSrcTransactionNo
+        --     , strSrcTransactionType
+        --     , strSrcModuleName
+        --     , intDestId
+        --     , strDestTransactionNo
+        --     , strDestTransactionType
+        --     , strDestModuleName
+        -- )    
+        -- SELECT strOperation	= 'Create'
+        --     , intSrcId = SH.intSiteHeaderId
+        --     , strSrcTransactionNo = SH.strCcdReference
+        --     , strSrcTransactionType = 'Dealer Credit Card'
+        --     , strSrcModuleName  = 'Credit Card Reconciliation'
+        --     , intDestId	= SH.intSiteHeaderId
+        --     , strDestTransactionNo = SH.strCcdReference
+        --     , strDestTransactionType = 'Dealer Credit Card'
+        --     , strDestModuleName = 'Credit Card Reconciliation'
+        -- FROM tblCCSiteHeader SH
+		-- WHERE SH.intSiteHeaderId = @intTransactionId
+
+        -- EXEC dbo.uspICAddTransactionLinks @tblTransactionLinks
     END
 	ELSE
 	BEGIN	
