c9a68fadd16f5a455b4fd1ab788fd5fe45e761d7
Chris Salvante <chris.salvante@irely.com>
2021-07-23 11:26:15 +0800

    FRM-9660

--- i21Database/Scripts/Post-Deployment/SM/2120_UpdateAddonComponents.sql
+++ i21Database/Scripts/Post-Deployment/SM/2120_UpdateAddonComponents.sql
@@ -8,6 +8,20 @@ BEGIN
 	WHERE		strModule = 'Language Translation'
 END
 
+IF EXISTS (SELECT 1 FROM tblSMModule WHERE strModule = 'Sub Ledger Traceability')
+BEGIN
+	UPDATE		tblSMModule
+	SET			ysnAddonComponent = 1
+	WHERE		strModule = 'Sub Ledger Traceability'
+END
+
+IF EXISTS (SELECT 1 FROM tblSMModule WHERE strModule = 'Report Hierarchy')
+BEGIN
+	UPDATE		tblSMModule
+	SET			ysnAddonComponent = 1
+	WHERE		strModule = 'Report Hierarchy'
+END
+
 PRINT('/*******************  END UPDATING ADDON COMPONENTS  *******************/')
 
 GO
--- i21Database/Scripts/Post-Deployment/SM/DefaultData/15_Module.sql
+++ i21Database/Scripts/Post-Deployment/SM/DefaultData/15_Module.sql
@@ -1297,6 +1297,28 @@ GO
 		   [strPrefix]						=		N'LT',
 		   [ysnAddonComponent]              =       1
 
+	IF NOT EXISTS(SELECT TOP 1 1 FROM tblSMModule WHERE strApplicationName = 'i21' AND strModule = 'Sub Ledger Traceability')
+	INSERT INTO [dbo].[tblSMModule] ([intModuleId],[strApplicationName], [strModule], [strAppCode], [ysnSupported], [intSort], [strPrefix], [ysnAddonComponent] )
+	SELECT [intModuleId]					=		127,
+		   [strApplicationName]				=		N'i21',
+		   [strModule]						=		N'Sub Ledger Traceability',
+		   [strAppCode]						=		N'',
+		   [ysnSupported]					=		1,
+		   [intSort]						=		125,
+		   [strPrefix]						=		N'SLT',
+		   [ysnAddonComponent]              =       1
+
+	IF NOT EXISTS(SELECT TOP 1 1 FROM tblSMModule WHERE strApplicationName = 'i21' AND strModule = 'Report Hierarchy')
+	INSERT INTO [dbo].[tblSMModule] ([intModuleId],[strApplicationName], [strModule], [strAppCode], [ysnSupported], [intSort], [strPrefix], [ysnAddonComponent] )
+	SELECT [intModuleId]					=		128,
+		   [strApplicationName]				=		N'i21',
+		   [strModule]						=		N'Report Hierarchy',
+		   [strAppCode]						=		N'',
+		   [ysnSupported]					=		1,
+		   [intSort]						=		126,
+		   [strPrefix]						=		N'RH',
+		   [ysnAddonComponent]              =       1
+
 	SET IDENTITY_INSERT [dbo].[tblSMModule] OFF
 
 GO
