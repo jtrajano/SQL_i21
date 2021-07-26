390bf8cc01c2465331ed6de04f964490f26c0c00
Feb Montefrio <feb.montefrio@irely.com>
2021-07-26 13:41:48 +0800

    [B] IC-9663 IC-9670
    # Conflicts:
    #	i21Database/dbo/Stored Procedures/uspICAdjustmentGLPostPreview.sql
    #	i21Database/dbo/Stored Procedures/uspICInventoryTransferDetailReportMPCT.sql
    #	i21Database/dbo/Stored Procedures/uspICPostCostAdjustmentSimplifiedAvg.sql

--- i21Database/dbo/Stored Procedures/uspICPostLotInTransit.sql
+++ i21Database/dbo/Stored Procedures/uspICPostLotInTransit.sql
@@ -17,7 +17,7 @@ CREATE PROCEDURE [dbo].[uspICPostLotInTransit]
 	--,@dblExchangeRate AS NUMERIC(38,20)
 	,@intTransactionId AS INT
 	,@intTransactionDetailId AS INT 
-	,@strTransactionId AS NVARCHAR(20)
+	,@strTransactionId AS NVARCHAR(40)
 	,@strBatchId AS NVARCHAR(40)
 	,@intTransactionTypeId AS INT
 	,@strTransactionForm AS NVARCHAR(255)
