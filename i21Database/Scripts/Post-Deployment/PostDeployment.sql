4f859592d94604a6fe2d87e59395b7d4c1000a4b
Ed Ibe <ed.ibe@irely.com>
2021-07-28 09:42:41 +0800

    SM-5186
    (cherry picked from commit 1cb3cb04190b96e239155903394214a70a1e2a16)

--- i21Database/Scripts/Post-Deployment/PostDeployment.sql
+++ i21Database/Scripts/Post-Deployment/PostDeployment.sql
@@ -96,6 +96,7 @@ print 'BEGIN POST DEPLOYMENT'
 :r .\SM\2010_RemoveF9Shortcut.sql
 :r .\SM\1920_DeleteDuplicateEntityPhoneNumber.sql
 :r .\SM\2010_UpdateEntityInquiryPermissionFrom1920.sql
+:r .\SM\2010_UpdateLicenseAgreement.sql
 :r .\SM\2110_CreateIDPGridLayout.sql
 :r .\SM\2110_UpdateAuditLogs.sql
 :r .\SM\2110_AddIDPToCustomerLicenseModule.sql
