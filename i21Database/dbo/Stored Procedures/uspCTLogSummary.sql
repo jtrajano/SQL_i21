85e793f700365628603da0df90e5b14ae4ae503b
JAYSON\Administrator <jayson.ramos@iRely.com>
2021-07-31 18:11:40 +0800

    CT-6072
    (cherry picked from commit dd6406995b410e1d5d60790afe09b833554e6bb1)
    (cherry picked from commit e2a3dbb1a339a50c7af24da2776ee26863eeef93)

--- i21Database/dbo/Stored Procedures/uspCTLogSummary.sql
+++ i21Database/dbo/Stored Procedures/uspCTLogSummary.sql
@@ -34,7 +34,8 @@ BEGIN TRY
 			@ysnDWGPriceOnly		BIT = 0,
 			@ysnReassign			BIT = 0,
 			@intCurrStatusId		INT = 0,
-   			@ysnWithPriceFix BIT;
+   			@ysnWithPriceFix 		BIT,
+   			@intPricingTypeId       int;
 
 	-------------------------------------------
 	--- Uncomment line below when debugging ---
@@ -540,6 +541,26 @@ BEGIN TRY
 			WHERE intSequenceUsageHistoryId IS NULL
 		) tbl
 		WHERE Row_Num = 1
+
+		select
+			@intPricingTypeId = intPricingTypeId
+		from (
+			SELECT
+				Row_Num = ROW_NUMBER() OVER (PARTITION BY sh.intContractDetailId ORDER BY sh.intSequenceHistoryId DESC)
+				,sh.intPricingTypeId
+			FROM
+				tblCTSequenceHistory sh
+				INNER JOIN @tmpContractDetail cd ON cd.intContractDetailId = sh.intContractDetailId
+			WHERE
+				intSequenceUsageHistoryId IS NULL
+		) tbl
+		where
+			Row_Num = 2
+
+		if not (@intPricingTypeId is null)
+		begin
+			update @cbLogTemp set intPricingTypeId = @intPricingTypeId;
+		end
 
 		IF (SELECT COUNT(*) FROM @cbLogPrev WHERE intContractDetailId = @intContractDetailId) >= 1
 		BEGIN
@@ -3558,13 +3579,13 @@ BEGIN TRY
 
 				SET @dblRunningQty += @dblOrigQty
 
-				SELECT TOP 1 @prevOrigQty = ISNULL(dblOrigQty, 0)
-					, @qtyDiff = @dblOrigQty - ISNULL(dblOrigQty, 0)
-				FROM @cbLogPrev
-				WHERE strProcess = 'Price Fixation'
-					AND strTransactionType = 'Contract Balance'
-					AND intTransactionReferenceDetailId = @intPriceFixationDetailId
-				ORDER BY intId DESC
+				SELECT TOP 1
+					@prevOrigQty = ISNULL(dblPreviousQty,dblQuantity)    
+					,@qtyDiff = dblQuantity - ISNULL(dblPreviousQty,dblQuantity)  
+				FROM
+					tblCTPriceFixationDetail    
+				WHERE
+					intPriceFixationDetailId = @intPriceFixationDetailId
 
 				IF (@intContractTypeId = 1)
 				BEGIN
