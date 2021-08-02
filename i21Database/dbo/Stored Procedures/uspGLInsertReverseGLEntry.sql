ac96ecd7260689802c416ff2c7e84f3f6c6aaba4
Jeff Trajano <jeffrey.trajano@irely.com>
2021-08-02 14:28:42 +0800

    GL-8327

--- i21Database/dbo/Stored Procedures/uspGLInsertReverseGLEntry.sql
+++ i21Database/dbo/Stored Procedures/uspGLInsertReverseGLEntry.sql
@@ -3,13 +3,178 @@ CREATE PROCEDURE [dbo].[uspGLInsertReverseGLEntry]
 	@strTransactionId NVARCHAR(100),
 	@intEntityId INT,
 	@dtmDateReverse DATETIME = NULL,
-	@strBatchId NVARCHAR(100) = ''
+	@strBatchId NVARCHAR(100) = '',
+	@strCode NVARCHAR(50) = NULL,
+	@ysnUseIntegerTransactionId BIT = 0
 )
 AS
 BEGIN
 	DECLARE @GLEntries RecapTableType
-		INSERT INTO @GLEntries (
-				 [strTransactionId]
+	DECLARE @intTransactionId INT
+
+	SET @strCode = ISNULL(@strCode,'')
+	IF(@ysnUseIntegerTransactionId = 0)
+	BEGIN
+		IF(@strCode = '') 
+		BEGIN
+			INSERT INTO @GLEntries (
+					[strTransactionId]
+					,[intTransactionId]
+					,[dtmDate]
+					,[strBatchId]
+					,[intAccountId]
+					,[dblDebit]
+					,[dblCredit]
+					,[dblDebitUnit]
+					,[dblCreditUnit]
+					,[dblDebitForeign]
+					,[dblCreditForeign]
+					,[dblForeignRate]
+					,dblReportingRate
+					,[strDescription]
+					,[strCode]
+					,[strReference]
+					,[intCurrencyId]
+					,[intCurrencyExchangeRateTypeId]
+					,[dblExchangeRate]
+					,[dtmDateEntered]
+					,[dtmTransactionDate]
+					,[strJournalLineDescription]
+					,[intJournalLineNo]
+					,[ysnIsUnposted]
+					,[intConcurrencyId]
+					,[intUserId]
+					,[intEntityId]
+					,[strTransactionType]
+					,[strTransactionForm]
+					,[strModuleName]
+			)
+			SELECT	 [strTransactionId]
+					,[intTransactionId]
+					,dtmDate			= ISNULL(@dtmDateReverse, [dtmDate]) -- If date is provided, use date reverse as the date for unposting the transaction.
+					,[strBatchId]		= @strBatchId
+					,[intAccountId]
+					,[dblCredit]
+					,[dblDebit]
+					,[dblCreditUnit]
+					,[dblDebitUnit]
+					,[dblCreditForeign]
+					,[dblDebitForeign]
+					,dblForeignRate
+					,dblReportingRate
+					,[strDescription]
+					,[strCode]
+					,[strReference]
+					,[intCurrencyId]
+					,[intCurrencyExchangeRateTypeId]
+					,[dblExchangeRate]
+					,dtmDateEntered		= GETDATE()
+					,[dtmTransactionDate]
+					,[strJournalLineDescription]
+					,[intJournalLineNo]
+					,ysnIsUnposted		= 1
+					,[intConcurrencyId]
+					,[intUserId]		= NULL
+					,[intEntityId]		= @intEntityId
+					,[strTransactionType]
+					,[strTransactionForm]
+					,[strModuleName]
+			FROM	tblGLDetail
+			WHERE	strTransactionId = @strTransactionId and ysnIsUnposted = 0
+			ORDER BY intGLDetailId
+
+			EXEC uspGLBookEntries @GLEntries, 0
+
+			UPDATE	tblGLDetail
+			SET		ysnIsUnposted = 1
+			WHERE	strTransactionId = @strTransactionId
+		END
+		ELSE
+		BEGIN
+			INSERT INTO @GLEntries (
+					[strTransactionId]
+					,[intTransactionId]
+					,[dtmDate]
+					,[strBatchId]
+					,[intAccountId]
+					,[dblDebit]
+					,[dblCredit]
+					,[dblDebitUnit]
+					,[dblCreditUnit]
+					,[dblDebitForeign]
+					,[dblCreditForeign]
+					,[dblForeignRate]
+					,dblReportingRate
+					,[strDescription]
+					,[strCode]
+					,[strReference]
+					,[intCurrencyId]
+					,[intCurrencyExchangeRateTypeId]
+					,[dblExchangeRate]
+					,[dtmDateEntered]
+					,[dtmTransactionDate]
+					,[strJournalLineDescription]
+					,[intJournalLineNo]
+					,[ysnIsUnposted]
+					,[intConcurrencyId]
+					,[intUserId]
+					,[intEntityId]
+					,[strTransactionType]
+					,[strTransactionForm]
+					,[strModuleName]
+			)
+			SELECT	 [strTransactionId]
+					,[intTransactionId]
+					,dtmDate			= ISNULL(@dtmDateReverse, [dtmDate]) -- If date is provided, use date reverse as the date for unposting the transaction.
+					,[strBatchId]		= @strBatchId
+					,[intAccountId]
+					,[dblCredit]
+					,[dblDebit]
+					,[dblCreditUnit]
+					,[dblDebitUnit]
+					,[dblCreditForeign]
+					,[dblDebitForeign]
+					,dblForeignRate
+					,dblReportingRate
+					,[strDescription]
+					,[strCode]
+					,[strReference]
+					,[intCurrencyId]
+					,[intCurrencyExchangeRateTypeId]
+					,[dblExchangeRate]
+					,dtmDateEntered		= GETDATE()
+					,[dtmTransactionDate]
+					,[strJournalLineDescription]
+					,[intJournalLineNo]
+					,ysnIsUnposted		= 1
+					,[intConcurrencyId]
+					,[intUserId]		= NULL
+					,[intEntityId]		= @intEntityId
+					,[strTransactionType]
+					,[strTransactionForm]
+					,[strModuleName]
+			FROM	tblGLDetail
+			WHERE	strTransactionId = @strTransactionId 
+				AND ysnIsUnposted = 0
+				AND strCode = @strCode
+			ORDER BY intGLDetailId
+
+			EXEC uspGLBookEntries @GLEntries, 0
+
+			UPDATE	tblGLDetail
+			SET		ysnIsUnposted = 1
+			WHERE	strTransactionId = @strTransactionId
+				AND strCode = @strCode
+		END
+	END
+	ELSE 
+	BEGIN
+		SET @intTransactionId = CAST(@strTransactionId AS INT)
+
+		IF(@strCode = '')
+		BEGIN
+			INSERT INTO @GLEntries (
+				[strTransactionId]
 				,[intTransactionId]
 				,[dtmDate]
 				,[strBatchId]
@@ -39,8 +204,9 @@ BEGIN
 				,[strTransactionType]
 				,[strTransactionForm]
 				,[strModuleName]
-		)
-		SELECT	 [strTransactionId]
+			)
+			SELECT	 
+				[strTransactionId]
 				,[intTransactionId]
 				,dtmDate			= ISNULL(@dtmDateReverse, [dtmDate]) -- If date is provided, use date reverse as the date for unposting the transaction.
 				,[strBatchId]		= @strBatchId
@@ -70,13 +236,96 @@ BEGIN
 				,[strTransactionType]
 				,[strTransactionForm]
 				,[strModuleName]
-		FROM	tblGLDetail
-		WHERE	strTransactionId = @strTransactionId and ysnIsUnposted = 0
-		ORDER BY intGLDetailId
+			FROM	tblGLDetail
+			WHERE	intTransactionId = @intTransactionId
+				AND ysnIsUnposted = 0
+			ORDER BY intGLDetailId
 
-		EXEC uspGLBookEntries @GLEntries, 0
+			EXEC uspGLBookEntries @GLEntries, 0
 
-		UPDATE	tblGLDetail
-		SET		ysnIsUnposted = 1
-		WHERE	strTransactionId = @strTransactionId
+			UPDATE	tblGLDetail
+			SET		ysnIsUnposted = 1
+			WHERE	intTransactionId = @intTransactionId
+		END
+		ELSE
+		BEGIN
+			INSERT INTO @GLEntries (
+				[strTransactionId]
+				,[intTransactionId]
+				,[dtmDate]
+				,[strBatchId]
+				,[intAccountId]
+				,[dblDebit]
+				,[dblCredit]
+				,[dblDebitUnit]
+				,[dblCreditUnit]
+				,[dblDebitForeign]
+				,[dblCreditForeign]
+				,[dblForeignRate]
+				,dblReportingRate
+				,[strDescription]
+				,[strCode]
+				,[strReference]
+				,[intCurrencyId]
+				,[intCurrencyExchangeRateTypeId]
+				,[dblExchangeRate]
+				,[dtmDateEntered]
+				,[dtmTransactionDate]
+				,[strJournalLineDescription]
+				,[intJournalLineNo]
+				,[ysnIsUnposted]
+				,[intConcurrencyId]
+				,[intUserId]
+				,[intEntityId]
+				,[strTransactionType]
+				,[strTransactionForm]
+				,[strModuleName]
+			)
+			SELECT	 
+				[strTransactionId]
+				,[intTransactionId]
+				,dtmDate			= ISNULL(@dtmDateReverse, [dtmDate]) -- If date is provided, use date reverse as the date for unposting the transaction.
+				,[strBatchId]		= @strBatchId
+				,[intAccountId]
+				,[dblCredit]
+				,[dblDebit]
+				,[dblCreditUnit]
+				,[dblDebitUnit]
+				,[dblCreditForeign]
+				,[dblDebitForeign]
+				,dblForeignRate
+				,dblReportingRate
+				,[strDescription]
+				,[strCode]
+				,[strReference]
+				,[intCurrencyId]
+				,[intCurrencyExchangeRateTypeId]
+				,[dblExchangeRate]
+				,dtmDateEntered		= GETDATE()
+				,[dtmTransactionDate]
+				,[strJournalLineDescription]
+				,[intJournalLineNo]
+				,ysnIsUnposted		= 1
+				,[intConcurrencyId]
+				,[intUserId]		= NULL
+				,[intEntityId]		= @intEntityId
+				,[strTransactionType]
+				,[strTransactionForm]
+				,[strModuleName]
+			FROM	tblGLDetail
+			WHERE	intTransactionId = @intTransactionId
+				AND ysnIsUnposted = 0
+				AND strCode = @strCode
+			ORDER BY intGLDetailId
+
+			EXEC uspGLBookEntries @GLEntries, 0
+
+			UPDATE	tblGLDetail
+			SET		ysnIsUnposted = 1
+			WHERE	strTransactionId = @strTransactionId
+				AND strCode = @strCode
+		END
+	END
+
+	
 END
