502a2ef110fcc49ebea8334fecb8af4474d20ebc
lawrence_zabala <lawrence.zabala@irely.com>
2021-08-04 12:34:35 +0800

    CT-6076
    (cherry picked from commit f349e3cd092b7ad94b9ddfad8c73fc7d212ab7cc)
    (cherry picked from commit 00e56b6b16297749610313a7485e26e81e4b5fee)
    (cherry picked from commit 0a1a81e777c73016e266f18c968de4f75d5b0b48)
    (cherry picked from commit f8332c207c3da9eb6915b30838c53dfcef2b5468)

--- i21Database/dbo/Stored Procedures/uspCTReportContractDetail.sql
+++ i21Database/dbo/Stored Procedures/uspCTReportContractDetail.sql
@@ -130,64 +130,16 @@ BEGIN TRY
 			strStaussItemDescription = (case when @ysnExternal = convert(bit,1) then '(' + IBM.strItemNo + ') ' else '' end) + IM.strDescription,
 			strItemBundleNoLabel	= (case when @ysnExternal = convert(bit,1) then 'GROUP QUALITY CODE:' else null end),
 			strStraussItemBundleNo	= IBM.strItemNo,
-			-- strStraussPrice			= 	CASE
-			-- 								WHEN
-			-- 									CD.intPricingTypeId = 2
-			-- 								THEN
-			-- 									'PTBF basis '
-			-- 									+ MA.strFutMarketName
-			-- 									+ ' '
-			-- 									+ DATENAME(mm,MO.dtmFutureMonthsDate)
-			-- 									+ ' '
-			-- 									+ DATENAME(yyyy,MO.dtmFutureMonthsDate)
-			-- 									+ CASE
-			-- 											WHEN CD.dblBasis < 0
-			-- 											THEN ' minus '
-			-- 											ELSE ' plus '
-			-- 											END
-			-- 									+ BCU.strCurrency
-			-- 									+ ' '
-			-- 									+ dbo.fnCTChangeNumericScale(abs(CD.dblBasis),2)
-			-- 									+ '/'
-			-- 									+ BUM.strUnitMeasure
-			-- 									+ ' at '
-			-- 									+ CD.strFixationBy
-			-- 									+ '''s option prior to FND of '
-			-- 									+ DATENAME(mm,MO.dtmFutureMonthsDate)
-			-- 									+ ' '
-			-- 									+ DATENAME(yyyy,MO.dtmFutureMonthsDate)
-			-- 									+ ' or on presentation of documents,whichever is earlier.'
-			-- 								ELSE
-			-- 									''
-			-- 									+ dbo.fnCTChangeNumericScale(CD.dblCashPrice,2)
-			-- 									+ ' '
-			-- 									+ BCU.strCurrency
-			-- 									+ ' per '
-			-- 									+ PU.strUnitMeasure
-			-- 						   END,
-			strStraussPrice			=	'PTBF basis '
-										+ MA.strFutMarketName
-										+ ' '
-										+ DATENAME(mm,MO.dtmFutureMonthsDate)
-										+ ' '
-										+ DATENAME(yyyy,MO.dtmFutureMonthsDate)
-										+ CASE
-												WHEN CD.dblBasis < 0
-												THEN ' minus '
-												ELSE ' plus '
-												END
-										+ BCU.strCurrency
-										+ ' '
-										+ dbo.fnCTChangeNumericScale(abs(CD.dblBasis),2)
-										+ '/'
-										+ BUM.strUnitMeasure
-										+ ' at '
-										+ CD.strFixationBy
-										+ '''s option prior to FND of '
-										+ DATENAME(mm,MO.dtmFutureMonthsDate)
-										+ ' '
-										+ DATENAME(yyyy,MO.dtmFutureMonthsDate)
-										+ ' or on presentation of documents,whichever is earlier.',
+			strStraussPrice			= CASE WHEN CH.intPricingTypeId = 2
+											THEN 'PTBF basis ' + MA.strFutMarketName + ' ' + DATENAME(mm,MO.dtmFutureMonthsDate) + ' ' + DATENAME(yyyy,MO.dtmFutureMonthsDate)
+			 									+ CASE WHEN CD.dblBasis < 0 THEN ' minus ' ELSE ' plus ' END
+			 									+ BCU.strCurrency + ' '
+			 									+ dbo.fnCTChangeNumericScale(abs(CD.dblBasis),2) + '/' + BUM.strUnitMeasure
+			 									+ ' at ' + CD.strFixationBy + '''s option prior to FND of '
+			 									+ DATENAME(mm,MO.dtmFutureMonthsDate) + ' ' + DATENAME(yyyy,MO.dtmFutureMonthsDate)
+			 									+ ' or on presentation of documents,whichever is earlier.'
+			 								ELSE '' + dbo.fnCTChangeNumericScale(CD.dblCashPrice,2) + ' ' + BCU.strCurrency + ' per ' + PU.strUnitMeasure
+			 						   END,
 			strStraussShipmentLabel	= (case when PO.strPositionType = 'Spot' then 'DELIVERY' else 'SHIPMENT' end),
 			strStraussShipment		= CASE WHEN SM.strReportDateFormat = 'M/d/yyyy'		THEN dbo.fnConvertDateToReportDateFormat(CD.dtmStartDate, 0) + ' - ' + dbo.fnConvertDateToReportDateFormat( CD.dtmEndDate, 0)
 										   WHEN SM.strReportDateFormat = 'M/d/yy'		THEN dbo.fnConvertDateToReportDateFormat(CD.dtmStartDate, 0) + ' - ' + dbo.fnConvertDateToReportDateFormat( CD.dtmEndDate, 0)
