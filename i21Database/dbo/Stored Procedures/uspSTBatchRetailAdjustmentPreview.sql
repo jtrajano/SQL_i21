5f01e7501a6c1da7fb7cf8f1aa310eec5321fb37
nelsonreyes19 <nelson.reyes@irely.com>
2021-08-05 16:31:19 +0800

    ST-1936
    Added additional fix

--- i21Database/dbo/Stored Procedures/uspSTBatchRetailAdjustmentPreview.sql
+++ i21Database/dbo/Stored Procedures/uspSTBatchRetailAdjustmentPreview.sql
@@ -124,7 +124,7 @@ BEGIN
 				)
 				SELECT
 					pad.intRetailPriceAdjustmentDetailId,
-					pad.intRetailPriceAdjustmentId,
+					rpa.intRetailPriceAdjustmentId,
 					ysnOneTimeUse,
 					intCompanyLocationId,
 					intStoreGroupId,
@@ -465,7 +465,7 @@ BEGIN
 							, trp.strRetailPriceAdjustmentNumber		AS strRetailPriceAdjustmentNumber
 					FROM @tblPreview tp
 						LEFT JOIN #tmpbatchpostingretailadjustmentId trpa
-							ON tp.intItemId = trpa.intItemId AND CAST(tp.strPreviewNewData AS NUMERIC) = CAST(trpa.dblPrice AS NUMERIC)
+							ON tp.intItemId = trpa.intItemId 
 						LEFT JOIN tblSTRetailPriceAdjustment trp
 							ON trp.intRetailPriceAdjustmentId = trpa.intRetailPriceAdjustmentId
 					WHERE strPreviewOldData != strPreviewNewData
