197be30854fca9d3feed785108fa0e30e01317ca
Samuel Marvin Aguilos <samuel.aguilos@irely.com>
2021-08-09 17:52:23 +0800

    IC-9550

---
+++ i21Database/dbo/Views/vyuICSearchCountByCategoryDetail.sql
@@ -0,0 +1,31 @@
+﻿CREATE VIEW [dbo].[vyuICSearchCountByCategoryDetail]
+	AS 
+
+SELECT 
+	CountByCategory.intInventoryCountByCategoryId,
+	CountByCategoryDetail.intInventoryCountByCategoryDetailId, 
+	CountByCategory.strCountNo, 
+	Location.strLocationName, 
+	CountByCategory.dtmCountDate, 
+	CountByCategory.ysnPosted,
+	Category.strCategoryCode,
+	Category.strDescription,
+	CountByCategoryDetail.dblTargetMargin,
+	CountByCategoryDetail.dblCurrentMargin,
+	CountByCategoryDetail.dblCurrentRetail,
+	CountByCategoryDetail.dblCurrentCost,
+	CountByCategoryDetail.dblNewRetail,
+	CountByCategoryDetail.dblNewCost,
+	CountByCategoryDetail.dblNewMargin
+FROM tblICInventoryCountByCategory CountByCategory
+INNER JOIN (
+	tblICInventoryCountByCategoryDetail CountByCategoryDetail 
+	LEFT JOIN tblICCategory Category 
+	ON 
+	CountByCategoryDetail.intCategoryId = Category.intCategoryId
+)
+ON
+CountByCategory.intInventoryCountByCategoryId = CountByCategoryDetail.intInventoryCountByCategoryId 
+LEFT JOIN tblSMCompanyLocation Location 
+ON 
+Location.intCompanyLocationId = CountByCategory.intLocationId
