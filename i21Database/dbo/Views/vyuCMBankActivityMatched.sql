3658720c20d0a4aeb0aa99e8fa82850b1f80c05c
Jeff Trajano <jeffrey.trajano@irely.com>
2021-08-11 12:41:58 +0800

    GL-8341

--- i21Database/dbo/Views/vyuCMBankActivityMatched.sql
+++ i21Database/dbo/Views/vyuCMBankActivityMatched.sql
@@ -1,46 +1,47 @@
-CREATE VIEW vyuCMBankActivityMatched
-AS
-SELECT 
-A.strMatchingId,
-A.intBankAccountId, --  FOR FILTERING
-A.intABRActivityMatchedId intMatchedId,
-A.intABRActivityId,
-A.intTransactionId,
-Trans.strTransactionId,
-Trans.strReferenceNo,
-Trans.dtmDate,
-ABS(Trans.dblAmount) * CASE WHEN strDebitCredit = 'D' THEN -1 ELSE 1 END  dblTransAmount,
-Trans.strBankTransactionTypeName,
-Activity.strABRActivityId,
-Activity.dtmClear,
-Activity.strBankDescription,
-Activity.dblAmount * CASE WHEN strDebitCredit = 'D' THEN -1 ELSE 1 END dblActivityAmount,
-Activity.strDebitCredit,
-A.dtmDateReconciled,
-Trans.intBankTransactionTypeId,
-A.intConcurrencyId
-FROM 
-tblCMABRActivityMatched A
-OUTER APPLY(
-	SELECT
-	strABRActivityId,
-	dtmClear, 
-    strBankDescription,
-    dblAmount , 
-    strDebitCredit,
-	intImportStatus
-    FROM tblCMABRActivity 
-    WHERE intABRActivityId = A.intABRActivityId
-) Activity
-OUTER APPLY(
-	SELECT 
-	intBankAccountId,
-	strTransactionId, 
-	strReferenceNo,
-	dtmDate,
-	dblAmount,
-	strBankTransactionTypeName,
-	intBankTransactionTypeId
-    FROM vyuCMBankTransaction 
-    WHERE intTransactionId = A.intTransactionId
+CREATE VIEW vyuCMBankActivityMatched  
+AS  
+SELECT   
+A.strMatchingId,  
+A.intBankAccountId, --  FOR FILTERING  
+A.intABRActivityMatchedId intMatchedId,  
+A.intABRActivityId,  
+A.intTransactionId,  
+Trans.strTransactionId,  
+Trans.strReferenceNo,  
+Trans.dtmDate,  
+dblTransAmount,  
+Trans.strBankTransactionTypeName,  
+Activity.strABRActivityId,  
+Activity.dtmClear,  
+Activity.strBankDescription,  
+dblActivityAmount,  
+Activity.strDebitCredit,  
+A.dtmDateReconciled,  
+Trans.intBankTransactionTypeId,  
+A.intConcurrencyId  
+FROM   
+tblCMABRActivityMatched A  
+OUTER APPLY(  
+ 	SELECT  
+	strABRActivityId,  
+	dtmClear,   
+	strBankDescription,  
+	ABS(dblAmount) * CASE WHEN B.strDebitCredit = 'D' THEN -1 ELSE 1 END  dblActivityAmount ,   
+	strDebitCredit,  
+	intImportStatus  
+	FROM tblCMABRActivity B  
+	WHERE intABRActivityId = A.intABRActivityId  
+) Activity  
+OUTER APPLY(  
+	SELECT   
+	intBankAccountId,  
+	strTransactionId,   
+	strReferenceNo,  
+	dtmDate,  
+	ABS(dblAmount) * CASE WHEN BTY.strDebitCredit = 'D' THEN -1 ELSE 1 END dblTransAmount,
+	strBankTransactionTypeName,  
+	BT.intBankTransactionTypeId  
+    FROM tblCMBankTransaction   BT join tblCMBankTransactionType BTY on BT.intBankTransactionTypeId = BTY.intBankTransactionTypeId
+    WHERE intTransactionId = A.intTransactionId  
 ) Trans
+
