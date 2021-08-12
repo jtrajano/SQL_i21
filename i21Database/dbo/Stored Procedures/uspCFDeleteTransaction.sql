2606fcee4ddd4a410dd241c2ffc09da761b2a4fd
bponce <bonn.ponce@irely.com>
2021-08-12 16:04:29 +0800

    CF-2778
    (cherry picked from commit e90dab0262d714ce43f0f88b73ee922267f1c303)

--- i21Database/dbo/Stored Procedures/uspCFDeleteTransaction.sql
+++ i21Database/dbo/Stored Procedures/uspCFDeleteTransaction.sql
@@ -32,8 +32,10 @@ DECLARE @IVTable TABLE
 
 BEGIN TRY
 
-
+DECLARE @ysnInvoiced AS BIT = 0
 DECLARE @dblQuantity AS NUMERIC(18,6)
+
+SELECT @ysnInvoiced = 1 FROM tblCFTransaction WHERE intTransactionId = @intTransactionId AND ISNULL(intInvoiceId,0) != 0
 
 IF(ISNULL(@intContractId,0) != 0)
 BEGIN
@@ -71,6 +73,10 @@ BEGIN
 
 	RETURN 1
 
+END
+ELSE IF(@ysnInvoiced != 1)
+BEGIN
+	DELETE FROM tblCFTransaction WHERE intTransactionId = @intTransactionId
 END
 ELSE
 BEGIN
