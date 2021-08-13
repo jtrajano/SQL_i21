6d6b9b3b6356a09b1cdd553fc4fda4279e5deca1
Girome Andaya <girome.andaya@iRely.com>
2021-08-13 14:46:14 +0800

    TR-1675
    (cherry picked from commit 9fe3121f053fcc87e1cb10bbb361c5c31d9497b0)
    (cherry picked from commit 3dc73c0de597e8c51c4d16b828c821f9adb1f4f7)
    (cherry picked from commit 70cca04e48d0e340157c093ff780989ddd81e698)

--- i21Database/Scripts/Post-Deployment/TR/ImportDtnDefaultSetup.sql
+++ i21Database/Scripts/Post-Deployment/TR/ImportDtnDefaultSetup.sql
@@ -79,11 +79,19 @@ GO
 
 DECLARE @strImportSetupName NVARCHAR(100) = NULL
 DECLARE @intImportFileHeaderId_Header INT = NULL
-
+DECLARE @intImportFileHeaderId_Item INT = NULL
+DECLARE @intImportFileHeaderId_DefTax INT = NULL
+	
 SET @strImportSetupName = 'iRely Enterprise Vendor Invoice Import Format'
 
 SELECT @intImportFileHeaderId_Header = intImportFileHeaderId FROM tblSMImportFileHeader 
-WHERE strLayoutTitle = 'iRely Enterprise Vendor Invoice Import Format'
+WHERE strLayoutTitle = 'iRely Enterprise Vendor Invoice Header'
+
+SELECT @intImportFileHeaderId_Item = intImportFileHeaderId FROM tblSMImportFileHeader 
+WHERE strLayoutTitle = 'iRely Enterprise Vendor Invoice Item'
+
+SELECT @intImportFileHeaderId_DefTax = intImportFileHeaderId FROM tblSMImportFileHeader 
+WHERE strLayoutTitle = 'iRely Enterprise Vendor Invoice Deferred Tax'
 
 DECLARE @intDtnImportSetupId INT = 2
 
@@ -97,16 +105,53 @@ BEGIN
 	VALUES (@intDtnImportSetupId, @strImportSetupName, 1)
 
 	SET IDENTITY_INSERT tblTRDtnImportSetup OFF
-END
 
-IF(@intImportFileHeaderId_Header IS NOT NULL)
-BEGIN
-	IF NOT EXISTS (SELECT TOP 1 1 FROM tblTRDtnImportSetupDetail WHERE intImportFileHeaderId = @intImportFileHeaderId_Header)
+	IF(@intImportFileHeaderId_Header IS NOT NULL)
 	BEGIN
 		INSERT INTO tblTRDtnImportSetupDetail (intDtnImportSetupId, strType, intImportFileHeaderId, intConcurrencyId) 
 		VALUES (@intDtnImportSetupId, 'Header', @intImportFileHeaderId_Header, 1)
 	END
+
+	IF(@intImportFileHeaderId_Item IS NOT NULL)
+	BEGIN
+		INSERT INTO tblTRDtnImportSetupDetail (intDtnImportSetupId, strType, intImportFileHeaderId, intConcurrencyId) 
+		VALUES (@intDtnImportSetupId, 'Item', @intImportFileHeaderId_Item, 1)
+	END
+
+	IF(@intImportFileHeaderId_DefTax IS NOT NULL)
+	BEGIN
+		INSERT INTO tblTRDtnImportSetupDetail (intDtnImportSetupId, strType, intImportFileHeaderId, intConcurrencyId) 
+		VALUES (@intDtnImportSetupId, 'DefTax', @intImportFileHeaderId_DefTax, 1)
+	END
+
+END
+BEGIN
+
+	IF(@intImportFileHeaderId_Header IS NOT NULL)
+	BEGIN
+		IF NOT EXISTS (SELECT TOP 1 1 FROM tblTRDtnImportSetupDetail WHERE intImportFileHeaderId = @intImportFileHeaderId_Header)
+		BEGIN
+			INSERT INTO tblTRDtnImportSetupDetail (intDtnImportSetupId, strType, intImportFileHeaderId, intConcurrencyId) 
+			VALUES (@intDtnImportSetupId, 'Header', @intImportFileHeaderId_Header, 1)
+		END
+	END
+
+	IF(@intImportFileHeaderId_Item IS NOT NULL)
+	BEGIN
+		IF NOT EXISTS (SELECT TOP 1 1 FROM tblTRDtnImportSetupDetail WHERE intImportFileHeaderId = @intImportFileHeaderId_Item)
+		BEGIN
+			INSERT INTO tblTRDtnImportSetupDetail (intDtnImportSetupId, strType, intImportFileHeaderId, intConcurrencyId) 
+			VALUES (@intDtnImportSetupId, 'Item', @intImportFileHeaderId_Item, 1)
+		END
+	END
+
+	IF(@intImportFileHeaderId_DefTax IS NOT NULL)
+	BEGIN
+		IF NOT EXISTS (SELECT TOP 1 1 FROM tblTRDtnImportSetupDetail WHERE intImportFileHeaderId = @intImportFileHeaderId_DefTax)
+		BEGIN
+			INSERT INTO tblTRDtnImportSetupDetail (intDtnImportSetupId, strType, intImportFileHeaderId, intConcurrencyId) 
+			VALUES (@intDtnImportSetupId, 'DefTax', @intImportFileHeaderId_DefTax, 1)
+		END
+	END
 END
 GO
-
-
