79a952648001061fc80381a964f45cde23c101b0
Premkumar S <prem.selladurai@irely.com>
2021-08-16 00:12:32 +0530

    MFG-4294

---
+++ i21Database/dbo/Tables/tblMFWODetail.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE dbo.tblMFWODetail (
+	intDetailId INT IDENTITY(1, 1) NOT NULL
+	,intProducedItemId INT
+	,intItemId INT
+	,dblQuantity NUMERIC(18, 6)
+	,dblLowerToleranceQty NUMERIC(18, 6)
+	,dblUpperToleranceQty NUMERIC(18, 6)
+	,intItemUOMId INT
+	,intStorageLocationId INT
+	,intSubLocationId INT
+	,intTransactionTypeId INT
+	,intUserId INT NULL
+	,dtmDate DATETIME
+	,ysnProcessed BIT
+	,intWorkOrderId INT
+	,intLocationId INT
+	)
