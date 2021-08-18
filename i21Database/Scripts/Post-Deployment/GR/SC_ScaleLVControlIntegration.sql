4a5011e82ba91020d958533c5c117bf1f542f238
Mon Gonzales <mon.gonzales@irely.com>
2021-08-18 08:54:40 +0800

    SC-4279 - [Jenkins] Ticket Management: ZEI_T611 - SC-3806 HDTN-250106 - One Weigh Import: Storage Unit Default Not saving (FAILURE)

--- i21Database/Scripts/Post-Deployment/GR/SC_ScaleLVControlIntegration.sql
+++ i21Database/Scripts/Post-Deployment/GR/SC_ScaleLVControlIntegration.sql
@@ -824,8 +824,26 @@ IF (SELECT TOP 1 1 TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 
 						LEFT JOIN tblSCListTicketTypes SCL ON SCL.strInOutIndicator = SC.strInOutFlag AND SCL.intTicketType = SC.intTicketType
 						--LEFT JOIN tblGRDiscountSchedule GRDS ON GRDS.strDiscountDescription =  (IC.strDescription  + '' Discount'' COLLATE Latin1_General_CI_AS) 
 						--left join tblGRDiscountCrossReference GRD_CROSS_REF on GRDI.intDiscountId = GRD_CROSS_REF.intDiscountId				
-						left join tblGRDiscountCrossReference GRD_CROSS_REF on ISNULL(GRDI.intDiscountId, ICC.intScheduleDiscountId) = GRD_CROSS_REF.intDiscountId
-						left join tblGRDiscountCrossReference ICC_GRD_CROSS_REF on ICC.intScheduleDiscountId = ICC_GRD_CROSS_REF.intDiscountId
+						left join (
+							select 
+								DCrossRef.intDiscountId 
+								,DCrossRef.intDiscountScheduleId
+								,DSchedule.intCommodityId
+							from tblGRDiscountCrossReference DCrossRef
+							join tblGRDiscountSchedule DSchedule
+								on DCrossRef.intDiscountScheduleId = DSchedule.intDiscountScheduleId
+						) GRD_CROSS_REF on ISNULL(GRDI.intDiscountId, ICC.intScheduleDiscountId) = GRD_CROSS_REF.intDiscountId
+							and ICC.intCommodityId = GRD_CROSS_REF.intCommodityId
+						left join (
+							select 
+								DCrossRef.intDiscountId 
+								,DCrossRef.intDiscountScheduleId
+								,DSchedule.intCommodityId
+							from tblGRDiscountCrossReference DCrossRef
+							join tblGRDiscountSchedule DSchedule
+								on DCrossRef.intDiscountScheduleId = DSchedule.intDiscountScheduleId
+						)ICC_GRD_CROSS_REF on ICC.intScheduleDiscountId = ICC_GRD_CROSS_REF.intDiscountId
+							and ICC.intCommodityId = ICC_GRD_CROSS_REF.intCommodityId
 						OUTER APPLY (
 							SELECT TOP 1 intLoadId, intLoadDetailId,
 								intContractDetailId = case 
