d6a12be1af13c255ade4eb302e79ef8751adb536
Smith Wilson G. de Jesus <smith.dejesus@irely.com>
2021-08-23 15:20:16 +0800

    Revert "CT-6094"

--- i21Database/dbo/Stored Procedures/uspRKRebuildSummaryLog.sql
+++ i21Database/dbo/Stored Procedures/uspRKRebuildSummaryLog.sql
@@ -2456,11 +2456,6 @@ BEGIN TRY
         INSERT INTO tblRKRebuildRTSLog(strLogMessage) VALUES ('End Populate RK Summary Log - On Hold')
         DELETE FROM @ExistingHistory
 
-		----------------------------------------------------
-		-- Run Integration scripts required after rebuild --
-		----------------------------------------------------
-		EXEC uspRKRunIntegrationAfterRebuild
-
 		UPDATE tblRKRebuildSummaryLog
 		SET ysnSuccess = 1
 		WHERE intRebuildSummaryLogId = @RebuildLogId
