a82cf6e71485317ff9514d347b154b1edc9e8cf3
Jeff Trajano <jeffrey.trajano@irely.com>
2021-08-26 17:39:10 +0800

    GL-8365

--- i21Database/dbo/Views/vyuCMBankFileGenerationLog.sql
+++ i21Database/dbo/Views/vyuCMBankFileGenerationLog.sql
@@ -12,7 +12,7 @@ strFileName,
 ysnSent,
 dtmSent,
 intEntityId,
-intBatchId = CASE WHEN f.intBatchId = 0 THEN 1000 ELSE f.intBatchId END,
+intBatchId = CASE WHEN f.intBatchId = 0 THEN 1e9 ELSE f.intBatchId END,
 strGroupName =CASE WHEN f.intBatchId = 0 THEN f.strFileName ELSE 
   'Batch Log ID : ' + CAST(f.intBatchId AS NVARCHAR(10)) +  ' - ' + CONVERT(VARCHAR(11), dtmGenerated, 101)  + ' - ' + strFileName
 END,
