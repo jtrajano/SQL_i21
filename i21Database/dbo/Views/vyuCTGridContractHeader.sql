f0ff9e715492b20207c053535a9be69b70bd67f8
JAYSON\Administrator <jayson.ramos@iRely.com>
2021-08-26 16:22:40 +0800

    Revert "CT-6133 1.1"
    This reverts commit 650a35271285172ac988070e17cca33af3dd23ba.

--- i21Database/dbo/Views/vyuCTGridContractHeader.sql
+++ i21Database/dbo/Views/vyuCTGridContractHeader.sql
@@ -146,11 +146,10 @@ AS
 			AP.strApprovalStatus,
 			P.strPositionType,
 			CH.ysnReadOnlyInterCoContract,
-			intCommodityFutureMarketId = NM.intCommodityFutureMarketId,
-			ysnContractRequiresApproval = (case when te.countValue > 0 or ue.countValue > 0 then convert(bit,1) else convert(bit,0) end)
+			intCommodityFutureMarketId = NM.intCommodityFutureMarketId
 	FROM		tblCTContractHeader				CH
 	JOIN		vyuCTContractHeaderNotMapped	NM	ON	NM.intContractHeaderId	=	CH.intContractHeaderId
-	OUTER APPLY
+	OUTER APPLY --dbo.[fnCTGetLastApprovalStatus](CH.intContractHeaderId) strApprovalStatus
 	(
 		SELECT	TOP 1 AP.strStatus AS strApprovalStatus 
 		FROM	tblSMApproval		AP
@@ -160,10 +159,4 @@ AS
 		WHERE	SC.strNamespace IN ('ContractManagement.view.Contract','ContractManagement.view.Amendments')
 		AND		AP.ysnCurrent = 1
 	) AP
-	cross apply (
-		select countValue=count(*) from tblEMEntityRequireApprovalFor em where em.intEntityId = CH.intEntityId
-	)te
-	cross apply (
-		select countValue=count(*) from tblSMUserSecurityRequireApprovalFor smUser where smUser.intEntityUserSecurityId = isnull(CH.intLastModifiedById,CH.intCreatedById)
-	)ue
 	LEFT JOIN tblCTPosition P ON CH.intPositionId = P.intPositionId
