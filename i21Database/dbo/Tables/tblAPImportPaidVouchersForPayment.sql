167e8fe60bae1f6c70d43f3a7440bda836838fda
Jopet Lambinicio <jopet.lambinicio@irely.com>
2021-08-27 20:24:49 +0800

    AP-9591

--- i21Database/dbo/Tables/tblAPImportPaidVouchersForPayment.sql
+++ i21Database/dbo/Tables/tblAPImportPaidVouchersForPayment.sql
@@ -13,5 +13,5 @@
 	[dblDiscount] 			DECIMAL(18, 2) NULL DEFAULT 0, 
 	[dblInterest] 			DECIMAL(18, 2) NULL DEFAULT 0,
 	[strNotes] 				NVARCHAR (1000) COLLATE Latin1_General_CI_AS NULL,
-	[intCustomPartition]	INT NOT NULL DEFAULT 0
+	[intCustomPartition]	INT NULL DEFAULT 0
 )
