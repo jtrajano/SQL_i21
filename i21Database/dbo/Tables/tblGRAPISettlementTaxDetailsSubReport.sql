15eb41265861003ea603484471539240cda5e36f
Mon Gonzales <mon.gonzales@irely.com>
2021-08-27 11:36:10 +0800

    SC-4297 - Bushel - Create a new SP that will Export a Settlement Sheet for REST API

---
+++ i21Database/dbo/Tables/tblGRAPISettlementTaxDetailsSubReport.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[tblGRAPISettlementTaxDetailsSubReport](
+	intSettlementTaxDetailsSubReportId	INT NOT NULL IDENTITY(1,1) PRIMARY KEY
+	,guiApiUniqueId						uniqueidentifier
+	,[strBillId]						[nvarchar](50) NULL,
+	[strTaxClass]						[nvarchar](50) NULL,
+	[dblTax]							[numeric](38, 6) NULL,
+	[intInventoryReceiptItemId]			[int] NOT NULL,
+	[intContractDetailId]				[int] NOT NULL,
+	[strItemNo]							[nvarchar](50) NULL,
+	[intBillDetailId]					[int] NOT NULL,
+	[intId]								[int] NOT NULL
+)
