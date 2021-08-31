780ce34eed9433cb1fa6a8fd2be87b0da69be99b
Jeff Trajano <jeffrey.trajano@irely.com>
2021-08-31 19:54:18 +0800

    GL-8326

--- i21Database/dbo/Tables/tblCMCompanyPreferenceOption.sql
+++ i21Database/dbo/Tables/tblCMCompanyPreferenceOption.sql
@@ -1,6 +1,11 @@
-CREATE TABLE [dbo].[tblCMCompanyPreferenceOption]
+CREATE TABLE [dbo].[tblCMCompanyPreferenceOption](
+	[intCompanyPreferenceOptionId] [int] IDENTITY(1,1) NOT NULL,
+	[intUTCOffset] [int] NULL,
+	[intConcurrencyId] [int] NULL,
+ CONSTRAINT [PK_tblCMCompanyPreferenceOption] PRIMARY KEY CLUSTERED 
 (
-	[intCompanyPreferenceOptionId] INT IDENTITY(1,1) NOT NULL,
-	[intConcurrencyId] INT NULL,
-	[ysnImportBankTransFromBankStmnt] BIT NULL
-)
+	[intCompanyPreferenceOptionId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
