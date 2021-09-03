a389e3b51ec046e20a8b0a1ec6ff23e35afad4af
JAYSON\Administrator <jayson.ramos@iRely.com>
2021-09-03 17:21:25 +0800

    CT-6150
    (cherry picked from commit 5d41c404c3a4758f88e42aab82d687cb7d147da9)

--- i21Database/dbo/Views/vyuCTGridContractHeader.sql
+++ i21Database/dbo/Views/vyuCTGridContractHeader.sql
@@ -151,7 +151,7 @@ AS
 			CH.ysnStrategic, -- CT-5315
 			CH.intEntitySelectedLocationId, -- CT-5315
 			NM.strEntitySelectedLocation, -- CT-5315
-			ysnContractRequiresApproval = (case when te.countValue > 0 or ue.countValue > 0 then convert(bit,1) else convert(bit,0) end)
+			ysnContractRequiresApproval = (case when ue.countValue > 0 then convert(bit,1) else convert(bit,0) end)
 	FROM		tblCTContractHeader				CH
 	JOIN		vyuCTContractHeaderNotMapped	NM	ON	NM.intContractHeaderId	=	CH.intContractHeaderId
 	OUTER APPLY
