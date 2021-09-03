2b4f53e30ea4e5ff32ccb3228f178751c31acc50
Vinoth Kumar T <vinoth.t@irely.com>
2021-09-03 22:34:43 +0530

    MFG-4294

---
+++ i21Database/dbo/Stored Procedures/uspMFRouteGetTODetail.sql
@@ -0,0 +1,26 @@
+﻿CREATE PROCEDURE uspMFRouteGetTODetail (
+	@intRouteOrderId INT
+	,@intInventoryTransferDetailId INT
+	)
+AS
+SELECT TD.intInventoryTransferDetailId
+	,T.intInventoryTransferId
+	,T.strTransferNo
+	,sl.strName strStorageUnit
+	,subl.strSubLocationName strStorageLocation
+	,TD.intItemId
+	,i.strItemNo
+	,i.strDescription
+	,CONVERT(NUMERIC(38, 3), TD.dblQuantity) AS dblQuantity
+	,um.strUnitMeasure
+	,iu.intItemUOMId
+	,iu.strLongUPCCode
+	,iu.strUpcCode AS strShortUpcCode
+FROM dbo.tblICInventoryTransfer T
+JOIN dbo.tblICInventoryTransferDetail TD ON TD.intInventoryTransferId = T.intInventoryTransferId
+	AND TD.intInventoryTransferDetailId = @intInventoryTransferDetailId
+JOIN dbo.tblICItem i ON i.intItemId = TD.intItemId
+JOIN dbo.tblICItemUOM iu ON iu.intItemUOMId = TD.intItemUOMId
+JOIN dbo.tblICUnitMeasure um ON um.intUnitMeasureId = iu.intUnitMeasureId
+LEFT JOIN dbo.tblSMCompanyLocationSubLocation subl ON subl.intCompanyLocationSubLocationId = TD.intFromSubLocationId
+LEFT JOIN dbo.tblICStorageLocation sl ON sl.intStorageLocationId = TD.intFromStorageLocationId
