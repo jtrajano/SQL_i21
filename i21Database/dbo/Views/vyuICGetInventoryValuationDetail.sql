e136d4720cc9d5afa4997bdd954761f38b524165
Samuel Marvin Aguilos <samuel.aguilos@irely.com>
2021-09-08 22:24:41 +0800

    IC-8970

--- i21Database/dbo/Views/vyuICGetInventoryValuationDetail.sql
+++
@@ -1,8 +0,0 @@
-CREATE VIEW [dbo].[vyuICGetInventoryValuationDetail]
-AS
-
-SELECT 
-	Valuation.*
-	strSummaryLocationName = COALESCE(Valuation.strInTransitSourceLocationName, Valuation.strLocationName) 
-FROM 
-vyuICGetInventoryValuation Valuation
