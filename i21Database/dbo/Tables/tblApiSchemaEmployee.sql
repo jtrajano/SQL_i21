b018dbd315941cea282194d09599e8da296ba490
Rey Anthony Crisostomo <anthonycr@cloudstaff.com>
2021-09-08 16:30:49 +0800

    PR-2248 - update

--- i21Database/dbo/Tables/tblApiSchemaEmployee.sql
+++ i21Database/dbo/Tables/tblApiSchemaEmployee.sql
@@ -2,11 +2,11 @@ CREATE TABLE tblApiSchemaEmployee(
     guiApiUniqueId UNIQUEIDENTIFIER NOT NULL,
     intRowNumber INT NULL,
     intKey INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
-    strEmployeeId NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
+    strEmployeeId NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL, --tblPREmployee
     strName NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
     strContactNumber NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
     strSuffix NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
-    strPhone NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
+    strEMPhone NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
     strClass NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
     strLocationName NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
     strPrintedName NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
@@ -28,36 +28,36 @@ CREATE TABLE tblApiSchemaEmployee(
     strLineOfBusiness3 NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
     strLineOfBusiness4 NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
     strLineOfBusiness5 NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
-    strFirstName NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
-    strMiddleName NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
-	strLastName NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
-	strNameSuffix NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,	
-    strTitle NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
-    strType NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
-    strPayPeriod NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
-    strRank NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
-    dtmReviewDate [datetime] NULL,
-    dtmNextReview [datetime] NULL,
-    strTimeEntryPassword NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
-    strEmergencyContact NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
-    strEmergencyRelation NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
-    strEmergencyPhone NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
-    strEmergencyPhone2 NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
-    dtmBirthDate [datetime] NULL,
-    ysnActive NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
-    dtmOriginalDateHired [datetime] NULL,
-    strGender NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
-    dtmDateHired [datetime] NULL,
-    strSpouse NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
-    strMaritalStatus NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
-    strWorkPhone NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
-    intWorkersCompensationId,
-    strEthnicity NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
-    strEEOCCode NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
-    strSocialSecurity NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
-    dtmTerminated [datetime] NULL,
-    strTerminatedReason NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
-    ysn1099Employee NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
+    strFirstName NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,          --tblPREmployee
+    strMiddleName NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,         --tblPREmployee
+	strLastName NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,           --tblPREmployee
+	strNameSuffix NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,	        --tblPREmployee
+    strTitle NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,              --tblPREmployee
+    strType NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,               --tblPREmployee
+    strPayPeriod NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,          --tblPREmployee
+    strRank NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,               --tblPREmployee
+    dtmReviewDate [datetime] NULL,                                          --tblPREmployee
+    dtmNextReview [datetime] NULL,                                          --tblPREmployee
+    strTimeEntryPassword NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,  --tblPREmployee
+    strEmergencyContact NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,   --tblPREmployee
+    strEmergencyRelation NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,  --tblPREmployee
+    strEmergencyPhone NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,     --tblPREmployee
+    strEmergencyPhone2 NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,    --tblPREmployee
+    dtmBirthDate [datetime] NULL,                                           --tblPREmployee
+    ysnActive NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,             --tblPREmployee
+    dtmOriginalDateHired [datetime] NULL,                                   --tblPREmployee
+    strGender NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,             --tblPREmployee
+    dtmDateHired [datetime] NULL,                                           --tblPREmployee
+    strSpouse NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,--tblPREmployee
+    strMaritalStatus NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,--tblPREmployee
+    strWorkPhone NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,--tblPREmployee
+    intWorkersCompensationId,--tblPREmployee
+    strEthnicity NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,--tblPREmployee
+    strEEOCCode NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,--tblPREmployee
+    strSocialSecurity NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,--tblPREmployee
+    dtmTerminated [datetime] NULL,--tblPREmployee
+    strTerminatedReason NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,--tblPREmployee
+    ysn1099Employee NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,--tblPREmployee
     strSupervisorId1 NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL, 
     strSupervisorName1 NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL, 
     strSupervisorId2 NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL, 
