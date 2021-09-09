e33cdbd68223a76857330c01f65761b8f5a5f26a
Cris <cris.lazaga@irely.com>
2021-09-09 14:21:26 +0800

    CCR-479

--- i21Database/dbo/Stored Procedures/uspCCTransactionToAPBill.sql
+++ i21Database/dbo/Stored Procedures/uspCCTransactionToAPBill.sql
@@ -49,6 +49,7 @@ BEGIN
 
 		INSERT INTO @Voucher (intTransactionType
 			, dtmDate
+			, dtmVoucherDate
 			, intEntityVendorId
 			, intShipToId
 			, intLocationId
@@ -61,6 +62,7 @@ BEGIN
 			, ysnStage)
 		SELECT intTransactionType = 3 
 			,dtmDate = @dtmDate
+			,dtmVoucherDate = @dtmDate
 			,intEntityVendorId = @intVendorId
 			,intShipToId = @intShipTo
 			,intLocationId = @intShipTo
@@ -87,7 +89,7 @@ BEGIN
 					WHEN ccItem.strItem = 'Company Owned Fees' AND ccSite.strSiteType = 'Company Owned' THEN ccSiteDetail.dblFees
 					WHEN ccItem.strItem = 'Dealer Site Gross' AND ccSite.strSiteType = 'Dealer Site Shared Fees' THEN ccSiteDetail.dblGross
 					WHEN ccItem.strItem = 'Dealer Site Fees' AND ccSite.strSiteType = 'Dealer Site Shared Fees' THEN ccSiteDetail.dblFees * (ccSite.dblSharedFeePercentage / 100)
-					WHEN ccItem.strItem = 'Dealer Site Shared Fees' AND ccSite.strSiteType = 'Dealer Site Shared Fees' THEN ccSiteDetail.dblFees - (ccSiteDetail.dblFees * (ccSite.dblSharedFeePercentage / 100))
+					WHEN ccItem.strItem = 'Dealer Site Shared Fees' AND ccSite.strSiteType = 'Dealer Site Shared Fees' THEN ccSiteDetail.dblFees - ROUND(ccSiteDetail.dblFees * (ccSite.dblSharedFeePercentage / 100), 2)
 					WHEN ccItem.strItem = 'Company Owned Gross' AND ccSite.strSiteType = 'Company Owned Pass Thru' THEN ccSiteDetail.dblGross 
 					WHEN ccItem.strItem = 'Company Owned Fees' AND ccSite.strSiteType = 'Company Owned Pass Thru' THEN ccSiteDetail.dblFees
 					ELSE null END) AS dblCost
@@ -100,32 +102,94 @@ BEGIN
 		GROUP BY  intAccountId, intSiteDetailId, strItem 
 
 		-- 1099K Adjustment
-		INSERT INTO @Voucher1099K (intTransactionType
-			, dtmDate
-			, intEntityVendorId
-			, intShipToId
-			, strVendorOrderNumber
-			, intAccountId
-			, intLocationId
-			, intCCSiteDetailId
-			, strMiscDescription
-			, dblCost
-			, dblQuantityToBill
-			, ysnStage)
-		SELECT 9
-			, dtmDate
-			, intEntityVendorId
-			, intShipToId
-			, strVendorOrderNumber
-			, intAccountId
-			, intLocationId
-			, intCCSiteDetailId
-			, strMiscDescription
-			, dblCost
-			, dblQuantityToBill
-			, 0 
-		FROM @Voucher WHERE strMiscDescription IN ('Dealer Site Gross', 'Dealer Site Net')
+		DECLARE @strInvalidCustomer NVARCHAR(2000)
 
+		DECLARE @dtmDate1099K DATETIME = NULL, @dtmVoucherDate1099K DATETIME = NULL, @intCustomerId1099K INT = NULL, @intShipToId1099K INT = NULL
+		, @strVendorOrderNumber1099K NVARCHAR(100) = NULL, @intAccountId INT = NULL, @intLocationId1099K INT = NULL
+		, @intCCSiteDetailId1099K INT = NULL, @strMiscDescription1099K NVARCHAR(500) = NULL, @dblCost1099K NUMERIC(18,6) = NULL
+		, @dblQuantityToBill1099K NUMERIC(18,6) = NULL, @Vendor1099K INT = NULL
+		, @strCustomerName NVARCHAR(200) = NULL
+
+		DECLARE @Cursor1099KTran AS CURSOR
+		SET @Cursor1099KTran = CURSOR FOR 
+		SELECT V.dtmDate
+			, V.dtmVoucherDate
+			, S.intCustomerId	
+			, V.intShipToId
+			, V.strVendorOrderNumber
+			, V.intAccountId
+			, V.intLocationId
+			, V.intCCSiteDetailId
+			, V.strMiscDescription
+			, V.dblCost
+			, V.dblQuantityToBill
+			, ET.[Vendor]
+			, ET.strEntityName
+		FROM @Voucher V INNER JOIN tblCCSiteDetail SD ON SD.intSiteDetailId = V.intCCSiteDetailId
+		INNER JOIN tblCCSite S ON S.intSiteId = SD.intSiteId
+		LEFT JOIN vyuEMEntityBasicWithType ET ON ET.intEntityId = S.intCustomerId
+		WHERE strMiscDescription IN ('Dealer Site Gross', 'Dealer Site Net')
+		OPEN @Cursor1099KTran
+		FETCH NEXT FROM @Cursor1099KTran INTO @dtmDate1099K, @dtmVoucherDate1099K, @intCustomerId1099K, @intShipToId1099K, @strVendorOrderNumber1099K, @intAccountId, @intLocationId1099K
+			,@intCCSiteDetailId1099K, @strMiscDescription1099K, @dblCost1099K, @dblQuantityToBill1099K, @Vendor1099K, @strCustomerName
+		WHILE @@FETCH_STATUS = 0
+		BEGIN	
+			-- VALIDATE THE CUSTOMER
+			IF(@Vendor1099K != 1) 
+			BEGIN
+				IF(ISNULL(@strInvalidCustomer,'') = '')
+				BEGIN
+					SET @strInvalidCustomer = @strCustomerName
+				END
+				ELSE
+				BEGIN
+					SET @strInvalidCustomer = @strInvalidCustomer + ', ' + @strCustomerName 
+				END
+			END
+			ELSE
+			BEGIN
+				INSERT INTO @Voucher1099K (intTransactionType
+					, dtmDate
+					, dtmVoucherDate
+					, intEntityVendorId
+					, intShipToId
+					, strVendorOrderNumber
+					, intAccountId
+					, intLocationId
+					, intCCSiteDetailId
+					, strMiscDescription
+					, dblCost
+					, dblQuantityToBill
+					, ysnStage)
+				VALUES(
+					9
+					, @dtmDate1099K
+					, @dtmVoucherDate1099K
+					, @intCustomerId1099K
+					, @intShipToId1099K
+					, @strVendorOrderNumber1099K
+					, @intAccountId
+					, @intLocationId1099K
+					, @intCCSiteDetailId1099K
+					, @strMiscDescription1099K
+					, @dblCost1099K
+					, @dblQuantityToBill1099K
+					, 0
+				)
+			END
+			
+			FETCH NEXT FROM @Cursor1099KTran INTO @dtmDate1099K, @dtmVoucherDate1099K, @intCustomerId1099K, @intShipToId1099K, @strVendorOrderNumber1099K, @intAccountId, @intLocationId1099K
+			,@intCCSiteDetailId1099K, @strMiscDescription1099K, @dblCost1099K, @dblQuantityToBill1099K, @Vendor1099K, @strCustomerName
+		END
+		CLOSE @Cursor1099KTran
+		DEALLOCATE @Cursor1099KTran
+
+		IF(ISNULL(@strInvalidCustomer, '') != '')
+		BEGIN
+			DECLARE @strMessage1099 NVARCHAR(4000) = 'Dealer Site ' + @strInvalidCustomer + ' must be setup as a Vendor in order for i21 to create the 1099 Adjustment entry.'
+			RAISERROR(@strMessage1099, 16, 1)
+		END
+		
 		IF EXISTS(SELECT TOP 1 1 FROM @Voucher)
 		BEGIN
 			EXEC [dbo].[uspAPCreateVoucher] 
@@ -295,6 +359,7 @@ BEGIN
 				EXEC [dbo].[uspAPDeleteVoucher]
 					@intBillId = @intBillId
 					,@UserId = @userId
+					,@callerModule = 9
 			END
 			ELSE
 			BEGIN
