b66e01d3674fb1d8948ee85873bd639186ab6bf4
Smith Wilson G. de Jesus <smith.dejesus@irely.com>
2021-09-10 16:25:37 +0800

    RM-4341

--- i21Database/Scripts/Post-Deployment/RM/01_MasterScript.sql
+++ i21Database/Scripts/Post-Deployment/RM/01_MasterScript.sql
@@ -196,7 +196,8 @@ BEGIN
 		UNION ALL SELECT 64, 'Created Credit Memo', 'Created Credit Memo.'
 		UNION ALL SELECT 65, 'Deleted Credit Memo', 'Deleted Credit Memo.'
 		UNION ALL SELECT 66, 'Posted Credit Memo', 'Posted Credit Memo.'
-		UNION ALL SELECT 67, 'Delete Match Derivatives', 'Delete Match Derivatives.'
+		UNION ALL SELECT 67, 'Price Updated', 'Price Updated.'
+		UNION ALL SELECT 68, 'Delete Match Derivatives', 'Delete Match Derivatives.'
 	) tbl
 	WHERE intActionId NOT IN (SELECT intLogActionId FROM tblRKLogAction)
 
