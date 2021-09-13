63ca9c265d85a2c0ab069c66da98dbcf3723b68a
Girome Andaya <girome.andaya@iRely.com>
2021-09-13 20:34:31 +0800

    TR-1704

--- i21Database/dbo/Stored Procedures/uspTRImportDtnVoucherPayment.sql
+++ i21Database/dbo/Stored Procedures/uspTRImportDtnVoucherPayment.sql
@@ -29,45 +29,65 @@ BEGIN
 			DECLARE @dblDeferredAmt NUMERIC(18,6) = NULL		
 			DECLARE @dblDeferredAmt1 NUMERIC(18,6) = NULL
 			DECLARE @dtmDeferredDate1 DATETIME = NULL
+			DECLARE @strDeferredInvoiceNo1 NVARCHAR(200) = NULL
 			DECLARE @dblDeferredAmt2 NUMERIC(18,6) = NULL
 			DECLARE @dtmDeferredDate2 DATETIME = NULL
+			DECLARE @strDeferredInvoiceNo2 NVARCHAR(200) = NULL
 			DECLARE @dblDeferredAmt3 NUMERIC(18,6) = NULL
 			DECLARE @dtmDeferredDate3 DATETIME = NULL
+			DECLARE @strDeferredInvoiceNo3 NVARCHAR(200) = NULL
 			DECLARE @dblDeferredAmt4 NUMERIC(18,6) = NULL
 			DECLARE @dtmDeferredDate4 DATETIME = NULL
+			DECLARE @strDeferredInvoiceNo4 NVARCHAR(200) = NULL
 			DECLARE @dblDeferredAmt5 NUMERIC(18,6) = NULL
 			DECLARE @dtmDeferredDate5 DATETIME = NULL
+			DECLARE @strDeferredInvoiceNo5 NVARCHAR(200) = NULL
 			DECLARE @dblDeferredAmt6 NUMERIC(18,6) = NULL
 			DECLARE @dtmDeferredDate6 DATETIME = NULL
+			DECLARE @strDeferredInvoiceNo6 NVARCHAR(200) = NULL
 			DECLARE @dblDeferredAmt7 NUMERIC(18,6) = NULL
 			DECLARE @dtmDeferredDate7 DATETIME = NULL
+			DECLARE @strDeferredInvoiceNo7 NVARCHAR(200) = NULL
 			DECLARE @dblDeferredAmt8 NUMERIC(18,6) = NULL
 			DECLARE @dtmDeferredDate8 DATETIME = NULL
+			DECLARE @strDeferredInvoiceNo8 NVARCHAR(200) = NULL
 			DECLARE @dblDeferredAmt9 NUMERIC(18,6) = NULL
 			DECLARE @dtmDeferredDate9 DATETIME = NULL
+			DECLARE @strDeferredInvoiceNo9 NVARCHAR(200) = NULL
 			DECLARE @dblDeferredAmt10 NUMERIC(18,6) = NULL
 			DECLARE @dtmDeferredDate10 DATETIME = NULL
+			DECLARE @strDeferredInvoiceNo10 NVARCHAR(200) = NULL
 
 			SELECT @dblDeferredAmt1 = dblDeferredAmt1
 				,@dtmDeferredDate1 = dtmDeferredDate1
+				,@strDeferredInvoiceNo1 = strDeferredInvoiceNo1
 				,@dblDeferredAmt2 = dblDeferredAmt2
 				,@dtmDeferredDate2 = dtmDeferredDate2
+				,@strDeferredInvoiceNo2 = strDeferredInvoiceNo2
 				,@dblDeferredAmt3 = dblDeferredAmt3
 				,@dtmDeferredDate3 = dtmDeferredDate3
+				,@strDeferredInvoiceNo3 = strDeferredInvoiceNo3
 				,@dblDeferredAmt4 = dblDeferredAmt4
 				,@dtmDeferredDate4 = dtmDeferredDate4
+				,@strDeferredInvoiceNo4 = strDeferredInvoiceNo4
 				,@dblDeferredAmt5 = dblDeferredAmt5
 				,@dtmDeferredDate5 = dtmDeferredDate5
+				,@strDeferredInvoiceNo5 = strDeferredInvoiceNo5
 				,@dblDeferredAmt6 = dblDeferredAmt6
 				,@dtmDeferredDate6 = dtmDeferredDate6
+				,@strDeferredInvoiceNo6 = strDeferredInvoiceNo6
 				,@dblDeferredAmt7 = dblDeferredAmt7
 				,@dtmDeferredDate7 = dtmDeferredDate7
+				,@strDeferredInvoiceNo7 = strDeferredInvoiceNo7
 				,@dblDeferredAmt8 = dblDeferredAmt8
 				,@dtmDeferredDate8 = dtmDeferredDate8
+				,@strDeferredInvoiceNo8 = strDeferredInvoiceNo8
 				,@dblDeferredAmt9 = dblDeferredAmt9
 				,@dtmDeferredDate9 = dtmDeferredDate9
+				,@strDeferredInvoiceNo9 = strDeferredInvoiceNo9
 				,@dblDeferredAmt10 = dblDeferredAmt10
 				,@dtmDeferredDate10 = dtmDeferredDate10
+				,@strDeferredInvoiceNo10 = strDeferredInvoiceNo10
 				,@dtmDueDate = dtmDueDate
 			FROM tblTRImportDtnDetail DD WHERE DD.ysnValid = 1 AND DD.intImportDtnId = @intImportLoadId
 						
@@ -88,6 +108,7 @@ BEGIN
 						
 			INSERT INTO @PaymentSchedule ([intBillId],
 				[intTermsId],
+				[strPaymentScheduleNumber],
 				[dtmDueDate],
 				[dblPayment],
 				[ysnPaid],
@@ -95,6 +116,7 @@ BEGIN
 				[dblDiscount])
 			SELECT intBillId = @intBillId,
 				intTermsId = @intTermId,
+				strPaymentScheduleNumber = NULL,
 				dtmDueDate = @dtmDueDate,
 				dblPayment = @dblDeferredAmt,
 				ysnPaid = 0,
@@ -103,6 +125,7 @@ BEGIN
 			UNION ALL
 			SELECT intBillId = @intBillId,
 				intTermsId = @intTermId,
+				strPaymentScheduleNumber = @strDeferredInvoiceNo1,
 				dtmDueDate = @dtmDeferredDate1,
 				dblPayment = @dblDeferredAmt1,
 				ysnPaid = 0,
@@ -113,6 +136,7 @@ BEGIN
 			BEGIN
 				INSERT INTO @PaymentSchedule ([intBillId],
 					[intTermsId],
+					[strPaymentScheduleNumber],
 					[dtmDueDate],
 					[dblPayment],
 					[ysnPaid],
@@ -120,6 +144,7 @@ BEGIN
 					[dblDiscount])
 				SELECT intBillId = @intBillId,
 					intTermsId = @intTermId,
+					strPaymentScheduleNumber = @strDeferredInvoiceNo2,
 					dtmDueDate = @dtmDeferredDate2,
 					dblPayment = @dblDeferredAmt2,
 					ysnPaid = 0,
@@ -131,6 +156,7 @@ BEGIN
 			BEGIN
 				INSERT INTO @PaymentSchedule ([intBillId],
 					[intTermsId],
+					[strPaymentScheduleNumber],
 					[dtmDueDate],
 					[dblPayment],
 					[ysnPaid],
@@ -138,6 +164,7 @@ BEGIN
 					[dblDiscount])
 				SELECT intBillId = @intBillId,
 					intTermsId = @intTermId,
+					strPaymentScheduleNumber = @strDeferredInvoiceNo3,
 					dtmDueDate = @dtmDeferredDate3,
 					dblPayment = @dblDeferredAmt3,
 					ysnPaid = 0,
@@ -149,6 +176,7 @@ BEGIN
 			BEGIN
 				INSERT INTO @PaymentSchedule ([intBillId],
 					[intTermsId],
+					[strPaymentScheduleNumber],
 					[dtmDueDate],
 					[dblPayment],
 					[ysnPaid],
@@ -156,6 +184,7 @@ BEGIN
 					[dblDiscount])
 				SELECT intBillId = @intBillId,
 					intTermsId = @intTermId,
+					strPaymentScheduleNumber = @strDeferredInvoiceNo4,
 					dtmDueDate = @dtmDeferredDate4,
 					dblPayment = @dblDeferredAmt4,
 					ysnPaid = 0,
@@ -168,6 +197,7 @@ BEGIN
 			BEGIN
 				INSERT INTO @PaymentSchedule ([intBillId],
 					[intTermsId],
+					[strPaymentScheduleNumber],
 					[dtmDueDate],
 					[dblPayment],
 					[ysnPaid],
@@ -175,6 +205,7 @@ BEGIN
 					[dblDiscount])
 				SELECT intBillId = @intBillId,
 					intTermsId = @intTermId,
+					strPaymentScheduleNumber = @strDeferredInvoiceNo5,
 					dtmDueDate = @dtmDeferredDate5,
 					dblPayment = @dblDeferredAmt5,
 					ysnPaid = 0,
@@ -188,6 +219,7 @@ BEGIN
 			BEGIN
 				INSERT INTO @PaymentSchedule ([intBillId],
 					[intTermsId],
+					[strPaymentScheduleNumber],
 					[dtmDueDate],
 					[dblPayment],
 					[ysnPaid],
@@ -195,6 +227,7 @@ BEGIN
 					[dblDiscount])
 				SELECT intBillId = @intBillId,
 					intTermsId = @intTermId,
+					strPaymentScheduleNumber = @strDeferredInvoiceNo6,
 					dtmDueDate = @dtmDeferredDate6,
 					dblPayment = @dblDeferredAmt6,
 					ysnPaid = 0,
@@ -208,6 +241,7 @@ BEGIN
 			BEGIN
 				INSERT INTO @PaymentSchedule ([intBillId],
 					[intTermsId],
+					[strPaymentScheduleNumber],
 					[dtmDueDate],
 					[dblPayment],
 					[ysnPaid],
@@ -215,6 +249,7 @@ BEGIN
 					[dblDiscount])
 				SELECT intBillId = @intBillId,
 					intTermsId = @intTermId,
+					strPaymentScheduleNumber = @strDeferredInvoiceNo7,
 					dtmDueDate = @dtmDeferredDate7,
 					dblPayment = @dblDeferredAmt7,
 					ysnPaid = 0,
@@ -229,6 +264,7 @@ BEGIN
 			BEGIN
 				INSERT INTO @PaymentSchedule ([intBillId],
 					[intTermsId],
+					[strPaymentScheduleNumber],
 					[dtmDueDate],
 					[dblPayment],
 					[ysnPaid],
@@ -236,6 +272,7 @@ BEGIN
 					[dblDiscount])
 				SELECT intBillId = @intBillId,
 					intTermsId = @intTermId,
+					strPaymentScheduleNumber = @strDeferredInvoiceNo8,
 					dtmDueDate = @dtmDeferredDate8,
 					dblPayment = @dblDeferredAmt8,
 					ysnPaid = 0,
@@ -250,6 +287,7 @@ BEGIN
 			BEGIN
 				INSERT INTO @PaymentSchedule ([intBillId],
 					[intTermsId],
+					[strPaymentScheduleNumber],
 					[dtmDueDate],
 					[dblPayment],
 					[ysnPaid],
@@ -257,6 +295,7 @@ BEGIN
 					[dblDiscount])
 				SELECT intBillId = @intBillId,
 					intTermsId = @intTermId,
+					strPaymentScheduleNumber = @strDeferredInvoiceNo9,
 					dtmDueDate = @dtmDeferredDate9,
 					dblPayment = @dblDeferredAmt9,
 					ysnPaid = 0,
@@ -272,6 +311,7 @@ BEGIN
 			BEGIN
 				INSERT INTO @PaymentSchedule ([intBillId],
 					[intTermsId],
+					[strPaymentScheduleNumber],
 					[dtmDueDate],
 					[dblPayment],
 					[ysnPaid],
@@ -279,6 +319,7 @@ BEGIN
 					[dblDiscount])
 				SELECT intBillId = @intBillId,
 					intTermsId = @intTermId,
+					strPaymentScheduleNumber = @strDeferredInvoiceNo10,
 					dtmDueDate = @dtmDeferredDate10,
 					dblPayment = @dblDeferredAmt10,
 					ysnPaid = 0,
