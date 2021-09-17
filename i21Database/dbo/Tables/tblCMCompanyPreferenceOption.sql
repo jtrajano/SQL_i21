7e5b7c46c5d408fb6f9847fbef0621b7a570360c
Jeff Trajano <jeffrey.trajano@irely.com>
2021-09-17 15:25:20 +0800

    GL-8409

--- i21Database/dbo/Tables/tblCMCompanyPreferenceOption.sql
+++ i21Database/dbo/Tables/tblCMCompanyPreferenceOption.sql
@@ -1,6 +1,12 @@
+ 
 CREATE TABLE [dbo].[tblCMCompanyPreferenceOption](
 	[intCompanyPreferenceOptionId] [int] IDENTITY(1,1) NOT NULL,
-	[intUTCOffset] [int] NULL,
+	[intBTFromFXGLAccountId] [int] NULL,
+	[intBTToFXGLAccountId] [int] NULL,
+	[intBTFeesAccountId] [int] NULL,
+	[intBTForwardAccountId] [int] NULL,
+	[intBTForwardAccrualAccountId] [int] NULL,
+	[intBTBankFeesAccountId] [int] NULL,
 	[intConcurrencyId] [int] NULL,
  CONSTRAINT [PK_tblCMCompanyPreferenceOption] PRIMARY KEY CLUSTERED 
 (
---
+++ i21Database/dbo/Views/vyuCMCompanyPreferenceOption.sql
@@ -0,0 +1,24 @@
+CREATE VIEW vyuCMCompanyPreferenceOption
+AS
+SELECT A.*,
+strBTFromFXGLAccountId =  B.strAccountId,
+strBTToFXGLAccountId = C.strAccountId,
+strBTForwardAccountId = D.strAccountId,
+strBTForwardAccrualAccountId = E.strAccountId,
+strBTBankFeesAccountId = F.strAccountId
+FROM tblCMCompanyPreferenceOption A
+OUTER APPLY(
+    SELECT TOP 1 strAccountId FROM tblGLAccount WHERE intAccountId = A.intBTFromFXGLAccountId
+)B
+OUTER APPLY(
+    SELECT TOP 1 strAccountId FROM tblGLAccount WHERE intAccountId = A.intBTToFXGLAccountId
+)C
+OUTER APPLY(
+    SELECT TOP 1 strAccountId FROM tblGLAccount WHERE intAccountId = A.intBTForwardAccountId
+)D
+OUTER APPLY(
+    SELECT TOP 1 strAccountId FROM tblGLAccount WHERE intAccountId = A.intBTForwardAccrualAccountId
+)E
+OUTER APPLY(
+    SELECT strAccountId FROM tblGLAccount WHERE intAccountId = A.intBTFeesAccountId
+)F
