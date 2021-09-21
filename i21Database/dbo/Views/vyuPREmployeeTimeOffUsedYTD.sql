7df0c50c6679cf1f060e750ae0bae93179edf716
manny_armas <manny.armas@iRely.com>
2021-09-21 14:29:38 +0800

    PR-2373 - Nagel: Time Off Hours Error

--- i21Database/dbo/Views/vyuPREmployeeTimeOffUsedYTD.sql
+++ i21Database/dbo/Views/vyuPREmployeeTimeOffUsedYTD.sql
@@ -4,34 +4,19 @@ SELECT
 	intYear
 	,intEntityEmployeeId
 	,intTypeTimeOffId
-	,dblHoursUsed = SUM(dblHours)
+	,dblHoursUsed = SUM(dblHoursUsed)
 FROM
-	(SELECT
-		intYear  = YEAR(dtmDateFrom)
-		,intEntityEmployeeId
-		,intTypeTimeOffId
-		,dblHours = dblRequest
-	FROM tblPRTimeOffRequest TOR
-	WHERE NOT EXISTS (SELECT 1 FROM tblPREmployeeEarning 
-		WHERE intEntityEmployeeId = TOR.intEntityEmployeeId 
-			AND intEmployeeTimeOffId = TOR.intTypeTimeOffId)
-		AND ysnPostedToCalendar = 1
-	UNION ALL
-	SELECT 
-		intYear = YEAR(dtmPayDate)
-		,intEntityEmployeeId
-		,intTypeTimeoffId = intEmployeeTimeOffId
-		,dblHours
-	FROM 
-		tblPRPaycheckEarning PCE 
-		INNER JOIN tblPRPaycheck PC
-			ON PCE.intPaycheckId = PC.intPaycheckId
-	WHERE 
-		intEmployeeTimeOffId IS NOT NULL
-		AND ysnPosted = 1
-		AND ysnVoid = 0
-		AND dblHours <> 0
-	) TimeOffHours
+		(
+		
+		SELECT 
+			intYear = YEAR(GETDATE())
+			,intEntityEmployeeId
+			,intTypeTimeOffId
+			,dblHoursUsed
+		FROM 
+			vyuPREmployeeTimeOff			
+
+		) TimeOffHours
 GROUP BY 
 	intYear
 	,intEntityEmployeeId
