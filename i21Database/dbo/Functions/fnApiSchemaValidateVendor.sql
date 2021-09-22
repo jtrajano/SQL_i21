6868f33aa210e74501bf5e2a603b138642d424ff
Jopet Lambinicio <jopet.lambinicio@irely.com>
2021-09-22 23:18:54 +0800

    AP-9830

---
+++ i21Database/dbo/Functions/fnApiSchemaValidateVendor.sql
@@ -0,0 +1,33 @@
+﻿CREATE FUNCTION [dbo].[fnApiSchemaValidateVendor]
+(
+	@guiApiUniqueId UNIQUEIDENTIFIER,
+	@guiLogId UNIQUEIDENTIFIER
+)
+RETURNS @returntable TABLE
+(
+	guiApiImportLogDetailId UNIQUEIDENTIFIER NOT NULL,
+	guiApiImportLogId UNIQUEIDENTIFIER NOT NULL,
+	strLogLevel NVARCHAR(100) NOT NULL,
+	strStatus NVARCHAR(150) NOT NULL,
+	strAction NVARCHAR(150) NULL,
+	intRowNo INT NULL,
+	strField NVARCHAR(100) NULL,
+	strValue NVARCHAR(4000) NULL,
+	strMessage NVARCHAR(4000) NULL
+)
+AS
+BEGIN
+	-- --
+	-- INSERT @returntable
+	-- SELECT NEWID(), @guiLogId, 'Error', 'Failed', NULL, A.intRowNo, 'Item No', A.strValue, 'Item Number is not valid'
+
+	-- --
+	-- INSERT @returntable
+	-- SELECT NEWID(), @guiLogId, 'Error', 'Failed', NULL, A.intRowNo, 'Item No', A.strValue, 'Item Number is not valid'
+
+	-- --
+	-- INSERT @returntable
+	-- SELECT NEWID(), @guiLogId, 'Error', 'Failed', NULL, A.intRowNo, 'Item No', A.strValue, 'Item Number is not valid'
+	
+	RETURN
+END
