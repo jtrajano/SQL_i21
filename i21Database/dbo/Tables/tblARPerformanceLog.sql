e1e02498e85611f60c26c2a5eb29cc50ef8ac9ac
kelvin_quinto <kelvin.quinto@irely.com>
2021-09-24 14:17:32 +0800

    AR-13759

---
+++ i21Database/dbo/Tables/tblARPerformanceLog.sql
@@ -0,0 +1,12 @@
+
+CREATE TABLE [dbo].[tblARPerformanceLog](
+	[intPerformanceLogId]       INT IDENTITY(1,1) NOT NULL,
+	[strScreenName]             NVARCHAR(200) NULL,
+	[strProcedureName]          NVARCHAR(200) NULL,
+    [strBuildNumber]            NVARCHAR(50) NULL,
+	[dtmStartDateTime]          DATETIME NULL,
+    [dtmEndDateTime]            DATETIME NULL,
+	[intUserId]                 INT NULL,
+	CONSTRAINT [PK_tblARPerformanceLog_intPerformanceLogId] PRIMARY KEY CLUSTERED ([intPerformanceLogId] ASC)
+);
+CREATE INDEX [idx_tblARPerformanceLog] ON [dbo].[tblARPerformanceLog] (strScreenName, dtmStartDateTime, dtmEndDateTime, intUserId)
