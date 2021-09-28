98bbe1cf9c3d7841c64532e677ebd30b0a6e51a8
kelvin_quinto <kelvin.quinto@irely.com>
2021-09-28 12:45:34 +0800

    AR-13759
    (cherry picked from commit e743519e21268b942e41ca18308339230fa65387)

--- i21Database/dbo/Views/vyuARPerformanceLog.sql
+++ i21Database/dbo/Views/vyuARPerformanceLog.sql
@@ -1,15 +1,16 @@
 CREATE VIEW dbo.vyuARPerformanceLog
 AS
-SELECT intPerformanceLogId	= PL.intPerformanceLogId
+SELECT intPerformanceLogId	= MIN(PL.intPerformanceLogId)
 	 , strScreenName		= PL.strScreenName
 	 , strProcedureName		= PL.strProcedureName
 	 , strBuildNumber		= PL.strBuildNumber
-	 , dtmStartDateTime		= PL.dtmStartDateTime
-	 , dtmEndDateTime		= PL.dtmEndDateTime 
-	 , strTimeElapse		= CONVERT(NVARCHAR(200), CAST((PL.dtmEndDateTime-PL.dtmStartDateTime) AS TIME(0)))
+	 , dtmStartDateTime		= MIN(dtmStartDateTime)
+	 , dtmEndDateTime		= MAX(dtmEndDateTime)
+	 , strTimeElapse		= CONVERT(NVARCHAR(200), CAST((MAX(dtmEndDateTime)-MIN(dtmStartDateTime)) AS TIME(0)))
 	 , intUserId			= PL.intUserId
 	 , strUserName			= LTRIM(RTRIM(E.strName))
 FROM tblARPerformanceLog PL
 LEFT JOIN tblEMEntity E ON PL.intUserId = E.intEntityId
 WHERE PL.dtmEndDateTime IS NOT NULL
   AND PL.dtmStartDateTime IS NOT NULL
+GROUP BY PL.strScreenName, PL.strProcedureName, PL.strBuildNumber, PL.intUserId, CAST(CONVERT(CHAR(16), PL.dtmStartDateTime,20) AS DATETIME), CAST(CONVERT(CHAR(16), PL.dtmEndDateTime,20) AS DATETIME), E.strName
