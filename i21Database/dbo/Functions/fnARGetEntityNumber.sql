9dd2779afc6c723498fe586296422ec4328131cd
Roudinee Panteria <roudinee.panteria@irely.com>
2021-09-30 18:25:32 +0800

    AR-13651

--- i21Database/dbo/Functions/fnARGetEntityNumber.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE FUNCTION [dbo].[fnARGetEntityNumber]()
-RETURNS NVARCHAR(50)
-AS
-BEGIN
-	DECLARE @strEntityNumber NVARCHAR(50)
-
-	EXEC uspSMGetStartingNumber 43, @strEntityNumber OUT
-	RETURN @strEntityNumber
-END
