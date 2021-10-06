9467d7c478a5cbac72611815cd8c4b826edaa58c
Feb Montefrio <feb.montefrio@irely.com>
2021-10-06 15:23:33 +0800

    [B] IC-9821 IC-9824 IC-9820
    # Conflicts:
    #	i21Database/dbo/Stored Procedures/uspICAdjustmentGLPostPreview.sql
    #	i21Database/dbo/Stored Procedures/uspICAdjustmentJournalGLPostPreview.sql
    #	i21Database/dbo/Stored Procedures/uspICAdjustmentSubLedgerGLPostPreview.sql
    #	i21Database/dbo/Stored Procedures/uspICAdjustmentSummaryGLPostPreview.sql
    #	i21Database/dbo/Stored Procedures/uspICAfterPostInventoryTransferIntegration.sql
    #	i21Database/dbo/Stored Procedures/uspICBeforePostInventoryTransferIntegration.sql
    #	i21Database/dbo/Stored Procedures/uspICBeforePostValidateInventoryReceipt.sql
    #	i21Database/dbo/Stored Procedures/uspICImportReceipt.sql
    #	i21Database/dbo/Stored Procedures/uspICInventoryReceiptUpdateInternalComments.sql
    #	i21Database/dbo/Stored Procedures/uspICInventoryTransferAfterSave.sql
    #	i21Database/dbo/Stored Procedures/uspICInventoryTransferDetailReportMPCT.sql
    #	i21Database/dbo/Stored Procedures/uspICLinkInventoryAdjustmentTransaction.sql
    #	i21Database/dbo/Stored Procedures/uspICLinkInventoryReceiptTransaction.sql
    #	i21Database/dbo/Stored Procedures/uspICLinkInventoryShipmentTransaction.sql
    #	i21Database/dbo/Stored Procedures/uspICLinkInventoryTransferTransaction.sql
    #	i21Database/dbo/Stored Procedures/uspICLinkShipmentToInvoice.sql
    #	i21Database/dbo/Stored Procedures/uspICMassAddPromotionForCStore.sql
    #	i21Database/dbo/Stored Procedures/uspICPostCostAdjustmentSimplifiedAvg.sql
    #	i21Database/dbo/Stored Procedures/uspICReserveStockForInventoryTransfer.sql
    #	i21Database/dbo/Stored Procedures/uspICSubLedgerAddReportEntries.sql
    #	i21Database/dbo/Stored Procedures/uspICSubLedgerRemoveReportEntries.sql
    #	i21Database/dbo/Stored Procedures/uspICTransactionLinkPatcher.sql
    #	i21Database/dbo/Stored Procedures/uspICUpdateEffectivePricingForCStore.sql
    #	i21Database/dbo/Stored Procedures/uspICUpdateInventoryValuationSummary.sql
    #	i21Database/dbo/Stored Procedures/uspICUpdateRevertEffectivePricingForCStore.sql
    #	i21Database/dbo/Stored Procedures/uspIPProcessERPInventoryAdjustment.sql

--- i21Database/dbo/Stored Procedures/uspICInventoryReceiptCalculateTotals.sql
+++ i21Database/dbo/Stored Procedures/uspICInventoryReceiptCalculateTotals.sql
@@ -4,22 +4,75 @@ CREATE PROCEDURE [dbo].[uspICInventoryReceiptCalculateTotals] (
 )
 AS
 
-DECLARE @intMaxId INT = ISNULL(@ReceiptId, 2147483647)
-DECLARE @intMinId INT = ISNULL(@ReceiptId, 1) -- if allows negative seeds -2147483647
 DECLARE @Date DATETIME = GETUTCDATE()
+
+--UPDATE r
+--SET
+--	  r.dtmLastCalculateTotals = @Date
+--	, r.dblSubTotal = ISNULL(dbo.fnICGetReceiptTotals(r.intInventoryReceiptId, 1),0)
+--	, r.dblTotalTax = ISNULL(dbo.fnICGetReceiptTotals(r.intInventoryReceiptId, 2),0)
+--	, r.dblTotalCharges = ISNULL(dbo.fnICGetReceiptTotals(r.intInventoryReceiptId, 3),0)
+--	, r.dblTotalGross = ISNULL(dbo.fnICGetReceiptTotals(r.intInventoryReceiptId, 4),0)
+--	, r.dblTotalNet =  ISNULL(dbo.fnICGetReceiptTotals(r.intInventoryReceiptId, 5),0)
+--	, r.dblGrandTotal =  ISNULL(dbo.fnICGetReceiptTotals(r.intInventoryReceiptId, 6),0)
+--FROM tblICInventoryReceipt r
+--WHERE (@ForceRecalc = 1 OR (r.dtmLastCalculateTotals IS NULL OR r.dtmDateModified > r.dtmLastCalculateTotals))
+--	AND (r.intInventoryReceiptId BETWEEN @intMinId AND @intMaxId)
+
+-- Temporarily comment this out to resolve IC-7927 but will temporarily remove optimizations in IC-7893
+--EXEC dbo.uspICUpdateInventoryReceiptDetail @ReceiptId, @ForceRecalc
 
 UPDATE r
 SET
 	  r.dtmLastCalculateTotals = @Date
-	, r.dblSubTotal = ISNULL(dbo.fnICGetReceiptTotals(r.intInventoryReceiptId, 1),0)
-	, r.dblTotalTax = ISNULL(dbo.fnICGetReceiptTotals(r.intInventoryReceiptId, 2),0)
-	, r.dblTotalCharges = ISNULL(dbo.fnICGetReceiptTotals(r.intInventoryReceiptId, 3),0)
-	, r.dblTotalGross = ISNULL(dbo.fnICGetReceiptTotals(r.intInventoryReceiptId, 4),0)
-	, r.dblTotalNet =  ISNULL(dbo.fnICGetReceiptTotals(r.intInventoryReceiptId, 5),0)
-	, r.dblGrandTotal =  ISNULL(dbo.fnICGetReceiptTotals(r.intInventoryReceiptId, 6),0)
-FROM tblICInventoryReceipt r
-WHERE (@ForceRecalc = 1 OR (r.dtmLastCalculateTotals IS NULL OR r.dtmDateModified > r.dtmLastCalculateTotals))
-	AND (r.intInventoryReceiptId BETWEEN @intMinId AND @intMaxId)
-
--- Temporarily comment this out to resolve IC-7927 but will temporarily remove optimizations in IC-7893
---EXEC dbo.uspICUpdateInventoryReceiptDetail @ReceiptId, @ForceRecalc
+	, r.dblSubTotal = ISNULL(items.subTotal, 0)
+	, r.dblTotalTax = ISNULL(items.totalTax, 0) + ISNULL(charges.totalChargesTax,0)
+	, r.dblTotalCharges = ISNULL(charges.totalCharges,0)
+	, r.dblTotalGross = ISNULL(items.totalGross,0)
+	, r.dblTotalNet =  ISNULL(items.totalNet,0)
+	, r.dblGrandTotal =  ISNULL(items.subTotal,0) + ISNULL(charges.totalCharges, 0) + ISNULL(items.totalTax, 0) + ISNULL(charges.totalChargesTax,0)
+FROM 
+	tblICInventoryReceipt r 
+	OUTER APPLY (
+		SELECT subTotal = SUM(ISNULL(ReceiptItem.dblLineTotal, 0))
+			   ,totalTax = SUM(ISNULL(ReceiptItem.dblTax, 0))
+			   ,totalGross = SUM(ISNULL(ReceiptItem.dblGross, 0))
+			   ,totalNet = SUM(ISNULL(ReceiptItem.dblNet, 0))
+		FROM	tblICInventoryReceiptItem ReceiptItem
+		WHERE	ReceiptItem.intInventoryReceiptId = r.intInventoryReceiptId
+	) items
+	OUTER APPLY (
+		SELECT totalCharges = SUM(
+					CASE 
+						WHEN Receipt.intCurrencyId = ISNULL(ReceiptCharge.intCurrencyId, Receipt.intCurrencyId) THEN 
+							CASE 
+								WHEN ReceiptCharge.ysnPrice = 1 THEN -ReceiptCharge.dblAmount 
+								WHEN Receipt.intEntityVendorId = ISNULL(ReceiptCharge.intEntityVendorId, Receipt.intEntityVendorId) THEN ReceiptCharge.dblAmount 
+								ELSE 0.00
+							END 
+						ELSE
+							0.00
+					END
+				)
+				,totalChargesTax = 
+				SUM (
+					CASE 
+						WHEN Receipt.intCurrencyId = ISNULL(ReceiptCharge.intCurrencyId, Receipt.intCurrencyId) THEN 
+							CASE 
+								WHEN ReceiptCharge.ysnPrice = 1 THEN -ReceiptCharge.dblTax 
+								WHEN ReceiptCharge.ysnAccrue = 1 AND Receipt.intEntityVendorId = ISNULL(ReceiptCharge.intEntityVendorId, Receipt.intEntityVendorId) THEN ReceiptCharge.dblTax 
+								ELSE 0.00
+							END 
+						ELSE
+							0.00
+					END
+				)
+		FROM	tblICInventoryReceipt Receipt INNER JOIN tblICInventoryReceiptCharge ReceiptCharge
+					ON Receipt.intInventoryReceiptId = ReceiptCharge.intInventoryReceiptId
+		WHERE	ReceiptCharge.intInventoryReceiptId = r.intInventoryReceiptId
+				AND ISNULL(Receipt.intCurrencyId, 1) = ISNULL(ReceiptCharge.intCurrencyId, ISNULL(Receipt.intCurrencyId, 1)) 	
+	) charges
+WHERE 
+	(r.intInventoryReceiptId = @ReceiptId OR @ReceiptId IS NULL) 
+	AND (@ForceRecalc = 1 OR (r.dtmLastCalculateTotals IS NULL OR r.dtmDateModified > r.dtmLastCalculateTotals))
+	
