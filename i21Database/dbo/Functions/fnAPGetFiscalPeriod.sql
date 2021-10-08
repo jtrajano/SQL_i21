bbc1adacf1c4f631b68b536d4997903557fb8e14
Jopet Lambinicio <jopet.lambinicio@irely.com>
2021-10-08 21:21:58 +0800

    AP-9956

--- i21Database/dbo/Functions/fnAPGetFiscalPeriod.sql
+++ i21Database/dbo/Functions/fnAPGetFiscalPeriod.sql
@@ -2,9 +2,9 @@
 RETURNS NVARCHAR(255)
 AS
 BEGIN
-
-DECLARE @fiscalPeriod AS NVARCHAR(255)
+	DECLARE @fiscalPeriod AS NVARCHAR(255)
 
 	SELECT @fiscalPeriod = strPeriod FROM tblGLFiscalYearPeriod FP WHERE @date BETWEEN FP.dtmStartDate AND FP.dtmEndDate OR @date = FP.dtmStartDate OR @date = FP.dtmEndDate
+	
 	RETURN @fiscalPeriod
 END
