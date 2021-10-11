12640885864b94d4e001ec33d4094698715be833
Girome Andaya <girome.andaya@iRely.com>
2021-10-11 11:36:04 +0800

    TR-1716

--- i21Database/dbo/Tables/tblEMEntityTariffMileage.sql
+++ i21Database/dbo/Tables/tblEMEntityTariffMileage.sql
@@ -5,8 +5,9 @@
 	[intFromMiles]						INT,
 	[intToMiles]						INT,
 	[dblCostRatePerUnit]				NUMERIC(18, 6) NULL DEFAULT ((0)), 
-	[dblInvoiceRatePerUnit]				NUMERIC(18, 6) NULL DEFAULT ((0)), 
-	[intConcurrencyId]						INT            CONSTRAINT [DF_tblEMEntityTariffMileage_intConcurrencyId] DEFAULT ((0)) NOT NULL,
+	[dblInvoiceRatePerUnit]				NUMERIC(18, 6) NULL DEFAULT ((0)),
+	[guiApiUniqueId]					UNIQUEIDENTIFIER NULL,
+	[intConcurrencyId]					INT            CONSTRAINT [DF_tblEMEntityTariffMileage_intConcurrencyId] DEFAULT ((0)) NOT NULL,
     CONSTRAINT [PK_tblEMEntityTariffMileage] PRIMARY KEY CLUSTERED ([intEntityTarifffMileageId] ASC),     
 	CONSTRAINT [FK_dbo_tblEMEntityTariffMileage_tblEMEntityTariff_intEntityTariffId] FOREIGN KEY ([intEntityTariffId]) REFERENCES [dbo].[tblEMEntityTariff] ([intEntityTariffId]) ON DELETE CASCADE
 )
