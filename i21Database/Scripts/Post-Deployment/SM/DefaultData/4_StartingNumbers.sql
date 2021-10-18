99d9a39c9a017e67d9636430c761bee6d717bda5
Vinoth Kumar T <vinoth.t@irely.com>
2021-10-18 23:41:13 +0530

    IN-948

--- i21Database/Scripts/Post-Deployment/SM/DefaultData/4_StartingNumbers.sql
+++ i21Database/Scripts/Post-Deployment/SM/DefaultData/4_StartingNumbers.sql
@@ -1596,7 +1596,15 @@ GO
 			,[ysnEnable]			= 1
 			,[intConcurrencyId]		= 1
 	WHERE NOT EXISTS (SELECT TOP 1 1 FROM tblSMStartingNumber WHERE strTransactionType = N'Bank Matching' AND strModule = 'Cash Management')
-
+	UNION ALL
+	SELECT	[intStartingNumberId]	= 165
+			,[strTransactionType]	= N'Receipt Feed Id'
+			,[strPrefix]			= N''
+			,[intNumber]			= 1
+			,[strModule]			= 'Manufacturing'
+			,[ysnEnable]			= 1
+			,[intConcurrencyId]		= 1
+	WHERE NOT EXISTS (SELECT TOP 1 1 FROM tblSMStartingNumber WHERE strTransactionType = N'Receipt Feed Id')
 
 
 	SET IDENTITY_INSERT [dbo].[tblSMStartingNumber] OFF
