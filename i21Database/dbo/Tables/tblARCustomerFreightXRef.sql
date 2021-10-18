ce4b58000bb4a1ceaa231a0cd064b85a62a409d0
Cris <cris.lazaga@irely.com>
2021-10-18 16:27:49 +0800

    TR-1720
    (cherry picked from commit a938689d0a0999e0b4c29e0bf51b1763610daaef)

--- i21Database/dbo/Tables/tblARCustomerFreightXRef.sql
+++ i21Database/dbo/Tables/tblARCustomerFreightXRef.sql
@@ -16,12 +16,13 @@
 	[intCompanyId]			INT             NULL,
     [intConcurrencyId]		INT             NOT NULL,
 	[guiApiUniqueId] UNIQUEIDENTIFIER NULL,
+    [intEntityTariffTypeId] INT NULL,
     CONSTRAINT [PK_tblARCustomerFreightXRef] PRIMARY KEY CLUSTERED ([intFreightXRefId] ASC),
 	CONSTRAINT [FK_tblARCustomerFreightXRef_tblICCategory] FOREIGN KEY ([intCategoryId]) REFERENCES [tblICCategory]([intCategoryId]),
 	CONSTRAINT [FK_tblARCustomerFreightXRef_tblSMShipVia] FOREIGN KEY ([intShipViaId]) REFERENCES [tblSMShipVia]([intEntityId]),	
 	CONSTRAINT [FK_tblARCustomerFreightXRef_tblARCustomer] FOREIGN KEY ([intEntityCustomerId]) REFERENCES [dbo].[tblARCustomer] ([intEntityId]),
 	CONSTRAINT [FK_tblARCustomerFreightXRef_tblEMEntityLocation] FOREIGN KEY ([intEntityLocationId]) REFERENCES [dbo].[tblEMEntityLocation] ([intEntityLocationId]),
-	CONSTRAINT [UK_tblARCustomerFreightXRef_reference_columns] UNIQUE NONCLUSTERED ([strZipCode] ASC, [intCategoryId] ASC,[intEntityLocationId] ASC),	--THE NAME IS USED IN THE FRONT END, IF THERE ARE CHANGES PLEASE INFORM MON.GONZALES	
-
+	CONSTRAINT [UK_tblARCustomerFreightXRef_reference_columns] UNIQUE NONCLUSTERED ([intEntityTariffTypeId] ASC, [strZipCode] ASC, [intCategoryId] ASC,[intEntityLocationId] ASC),	--THE NAME IS USED IN THE FRONT END, IF THERE ARE CHANGES PLEASE INFORM MON.GONZALES	
+    CONSTRAINT [FK_tblARCustomerFreightXRef_tblEMEntityTariffType_intEntityTariffTypeId] FOREIGN KEY ([intEntityTariffTypeId]) REFERENCES [dbo].[tblEMEntityTariffType] ([intEntityTariffTypeId])
 );
 
