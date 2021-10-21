a7ff28821bdaeec17808629dda9077c16c96d538
Roudinee Panteria <roudinee.panteria@irely.com>
2021-10-21 15:52:20 +0800

    AR-13772

--- i21Database/dbo/Views/vyuARShippedItems.sql
+++ i21Database/dbo/Views/vyuARShippedItems.sql
@@ -107,7 +107,6 @@ SELECT id							= NEWID()
 	 , ysnShowForShipment			= isnull(SCALETICKET.ysnShowForShipment, 1)
 	 , intItemContractHeaderId		= SHIPPEDITEMS.intItemContractHeaderId
 	 , intItemContractDetailId		= SHIPPEDITEMS.intItemContractDetailId
-	 , dblStandardWeight			= SHIPPEDITEMS.dblStandardWeight
 	 , ysnDestinationWeightsAndGrades   = SHIPPEDITEMS.ysnDestinationWeightsAndGrades
 	 , dblDestinationQuantity		= SHIPPEDITEMS.dblDestinationQuantity
 FROM (
@@ -192,7 +191,6 @@ FROM (
 		 , intSubBookId						= NULL
 		 , intItemContractHeaderId			= NULL
 		 , intItemContractDetailId			= NULL
-		 , dblStandardWeight				= SOD.dblStandardWeight
 		 , ysnDestinationWeightsAndGrades   = NULL
 		 , dblDestinationQuantity			= NULL
 	FROM dbo.tblSOSalesOrder SO WITH (NOLOCK)
@@ -379,7 +377,6 @@ FROM (
 		 , intSubBookId						= NULL
 		 , intItemContractHeaderId			= ITEMCONTRACT.intItemContractHeaderId
 		 , intItemContractDetailId			= ITEMCONTRACT.intItemContractDetailId
-		 , dblStandardWeight				= 0.000000
 		 , ysnDestinationWeightsAndGrades   = ICISI.ysnDestinationWeightsAndGrades
 		 , dblDestinationQuantity			= ICISI.dblDestinationQuantity
 	FROM (
@@ -603,7 +600,6 @@ FROM (
 		 , intSubBookId						= NULL
 		 , intItemContractHeaderId			= NULL
 		 , intItemContractDetailId			= NULL
-		 , dblStandardWeight				= 0.000000
 		 , ysnDestinationWeightsAndGrades   = NULL
 		 , dblDestinationQuantity			= NULL
 	FROM dbo.tblICInventoryShipmentCharge ICISC WITH (NOLOCK)
@@ -737,7 +733,6 @@ FROM (
 		 , intSubBookId						= NULL
 		 , intItemContractHeaderId			= NULL
 		 , intItemContractDetailId			= NULL
-		 , dblStandardWeight				= 0.000000
 		 , ysnDestinationWeightsAndGrades   = NULL
 		 , dblDestinationQuantity			= NULL
 	FROM dbo.tblSOSalesOrder SO WITH (NOLOCK)
@@ -854,7 +849,6 @@ FROM (
 		 , intSubBookId						= NULL
 		 , intItemContractHeaderId			= NULL
 		 , intItemContractDetailId			= NULL
-		 , dblStandardWeight				= 0.000000
 		 , ysnDestinationWeightsAndGrades   = NULL
 		 , dblDestinationQuantity			= NULL
 	FROM dbo.tblICInventoryShipmentItem ICISI WITH (NOLOCK)
@@ -967,7 +961,6 @@ FROM (
 		 , intSubBookId						= intSubBookId
 		 , intItemContractHeaderId			= NULL
 		 , intItemContractDetailId			= NULL
-		 , dblStandardWeight				= 0.000000
 		 , ysnDestinationWeightsAndGrades   = NULL
 		 , dblDestinationQuantity			= NULL
 	FROM 
@@ -1055,7 +1048,6 @@ FROM (
 		 , intSubBookId						= NULL
 		 , intItemContractHeaderId			= NULL
 		 , intItemContractDetailId			= NULL
-		 , dblStandardWeight				= 0.000000
 		 , ysnDestinationWeightsAndGrades   = NULL
 		 , dblDestinationQuantity			= NULL
 	FROM (
@@ -1179,7 +1171,6 @@ FROM (
 		 , intSubBookId						= NULL
 		 , intItemContractHeaderId			= NULL
 		 , intItemContractDetailId			= NULL
-		 , dblStandardWeight				= 0.000000
 		 , ysnDestinationWeightsAndGrades   = NULL
 		 , dblDestinationQuantity			= NULL
 	FROM (
@@ -1300,7 +1291,6 @@ FROM (
 		 , intSubBookId						= NULL
 		 , intItemContractHeaderId			= NULL
 		 , intItemContractDetailId			= NULL
-		 , dblStandardWeight				= 0.000000
 		 , ysnDestinationWeightsAndGrades   = NULL
 		 , dblDestinationQuantity			= NULL
 	FROM (
