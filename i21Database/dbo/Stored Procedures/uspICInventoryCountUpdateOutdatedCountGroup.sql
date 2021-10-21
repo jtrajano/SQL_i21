3c115ad443961682a34fbc74d43a46be4b23c482
Feb Montefrio <feb.montefrio@irely.com>
2021-10-21 15:42:27 +0800

    [B] IC-9864
    # Conflicts:
    #	i21Database/i21Database.sqlproj
    
    # Conflicts:
    #	i21Database/i21Database.sqlproj

---
+++ i21Database/dbo/Stored Procedures/uspICInventoryCountUpdateOutdatedCountGroup.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE dbo.uspICInventoryCountUpdateOutdatedCountGroup 
+(
+	@intInventoryCountId INT
+)
+AS
+
+UPDATE cd
+SET 
+	cd.dblSystemCount = os.dblNewOnHand,
+	cd.dblWeightQty = os.dblNewWeightQty,
+	cd.dblNetQty = os.dblNewWeightQty
+FROM 
+	tblICInventoryCountDetail cd
+	INNER JOIN vyuICGetInventoryCountOutdatedCountGroup os 
+		ON os.intInventoryCountDetailId = cd.intInventoryCountDetailId
+WHERE 
+	cd.intInventoryCountId = @intInventoryCountId
+
