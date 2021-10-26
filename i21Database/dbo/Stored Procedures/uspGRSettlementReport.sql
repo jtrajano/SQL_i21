d24de7cdc616c47ec86c9713f6ea3214ce7e4fe2
Christian Joel Martinez <Christian.Martinez@iRely.com>
2021-10-26 11:10:25 +0800

    GRN-2838

--- i21Database/dbo/Stored Procedures/uspGRSettlementReport.sql
+++ i21Database/dbo/Stored Procedures/uspGRSettlementReport.sql
@@ -459,6 +459,7 @@ BEGIN
 			JOIN tblAPBillDetail BillDtl 
 				ON Bill.intBillId = BillDtl.intBillId 
 					AND BillDtl.intInventoryReceiptChargeId IS NULL
+					AND BillDtl.intSettleStorageId IS NULL
 			JOIN tblICItem Item 
 				ON BillDtl.intItemId = Item.intItemId
 			LEFT JOIN tblICCommodity Commodity 
@@ -1350,6 +1351,7 @@ BEGIN
 			JOIN tblAPBillDetail BillDtl 
 				ON Bill.intBillId = BillDtl.intBillId 
 					AND BillDtl.intInventoryReceiptChargeId IS NULL
+					AND BillDtl.intSettleStorageId IS NULL
 			JOIN tblICItem Item 
 				ON BillDtl.intItemId = Item.intItemId
 			LEFT JOIN tblICCommodity Commodity 
