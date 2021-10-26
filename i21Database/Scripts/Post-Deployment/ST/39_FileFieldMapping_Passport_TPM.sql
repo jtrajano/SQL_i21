c1088dc99c702ba5d5d3a90b377b9e532ee5ed9f
nelsonreyes19 <nelson.reyes@irely.com>
2021-10-26 09:21:49 +0800

    ST-2006

--- i21Database/Scripts/Post-Deployment/ST/39_FileFieldMapping_Passport_TPM.sql
+++ i21Database/Scripts/Post-Deployment/ST/39_FileFieldMapping_Passport_TPM.sql
@@ -5,6 +5,10 @@ DECLARE @intTagAttributeId INT
 DECLARE @strLayoutTitle NVARCHAR(MAX)
 SET @strLayoutTitle = 'Passport - TPM 3.4'
 
+
+PRINT N'BEGIN - Store Register TPM setup entries'
+
+
 --START CHECK HEADER
 IF EXISTS(SELECT 1 FROM dbo.tblSMImportFileHeader WHERE strLayoutTitle = @strLayoutTitle)
 BEGIN
@@ -21,11 +25,15 @@ SELECT @intImportFileHeaderId = intImportFileHeaderId FROM dbo.tblSMImportFileHe
 END
 ELSE IF NOT EXISTS(SELECT 1 FROM dbo.tblSMImportFileHeader WHERE strLayoutTitle = @strLayoutTitle)
 BEGIN
+
+    PRINT N'BEGIN - Insert TPM setup entries'
   INSERT INTO [dbo].[tblSMImportFileHeader]
       ([strLayoutTitle],[strFileType],[strFieldDelimiter],[strXMLType],[strXMLInitiater],[ysnActive],[intConcurrencyId])
   VALUES 
       ('Passport - TPM 3.4','XML',NULL,'Inbound','<?xml version="1.0" encoding="ISO-8859-1"?>',1,2)
+    PRINT N'END - Insert TPM setup entries'
 END
 --END CHECK HEADER
 
+PRINT N'END - Store Register TPM setup entries'
 GO
