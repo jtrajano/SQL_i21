cf76e252b3d790e6b2b170b4632b82e6bcc86ae4
nelsonreyes19 <nelson.reyes@irely.com>
2021-10-27 15:24:01 +0800

    ST-1849

--- i21Database/dbo/Stored Procedures/uspICUpdateItemPromotionalPricingForCStore.sql
+++ i21Database/dbo/Stored Procedures/uspICUpdateItemPromotionalPricingForCStore.sql
@@ -7,6 +7,8 @@ CREATE PROCEDURE [dbo].[uspICUpdateItemPromotionalPricingForCStore]
 	,@dtmBeginDate AS DATETIME = NULL 
 	,@dtmEndDate AS DATETIME = NULL 
 	,@strUpcCode AS VARCHAR(30) = NULL 
+	,@intItemId AS INT = NULL 
+	,@intLocationId AS INT = NULL 
 	,@intItemSpecialPricingId AS INT = NULL 
 	,@intEntityUserSecurityId AS INT 
 AS
@@ -56,6 +58,7 @@ IF OBJECT_ID('tempdb..#tmpUpdateItemPricingForCStore_ItemSpecialPricingAuditLog'
 		,dblNewCost NUMERIC(38, 20) NULL 
 		,dtmNewBeginDate DATETIME NULL
 		,dtmNewEndDate DATETIME NULL 		
+		,strAction VARCHAR(20) NULL 	
 	)
 ;
 
@@ -72,6 +75,7 @@ BEGIN
 		,dblNewCost
 		,dtmNewBeginDate
 		,dtmNewEndDate
+		,strAction
 	)
 	SELECT	[Changes].intItemId
 			,[Changes].intItemSpecialPricingId
@@ -83,6 +87,7 @@ BEGIN
 			,[Changes].dblNewCost
 			,[Changes].dtmNewBeginDate
 			,[Changes].dtmNewEndDate
+			,[Changes].strAction
 	FROM	(
 				-- Merge will help us build the audit log and update the records at the same time. 
 				MERGE	
@@ -90,13 +95,22 @@ BEGIN
 					WITH	(HOLDLOCK) 
 					AS		itemSpecialPricing	
 					USING (
-						SELECT	itemSpecialPricing.intItemSpecialPricingId
-						FROM	tblICItemSpecialPricing itemSpecialPricing INNER JOIN tblICItemLocation il
-									ON itemSpecialPricing.intItemLocationId = il.intItemLocationId 
-								INNER JOIN tblICItem i
+						SELECT	itemSpecialPricing.intItemSpecialPricingId,
+								i.intItemId,
+								il.intItemLocationId,
+								uom.intItemUOMId,
+								itemSpecialPricing.dtmBeginDate,
+								itemSpecialPricing.dtmEndDate
+						FROM	tblICItem i 
+								LEFT JOIN  tblICItemSpecialPricing itemSpecialPricing 
 									ON i.intItemId = itemSpecialPricing.intItemId 
+								LEFT JOIN tblICItemLocation il
+									ON i.intItemId = il.intItemId 
+								INNER JOIN tblICItemUOM uom
+									ON i.intItemId = uom.intItemId
+									AND ysnStockUnit = 1
 						WHERE	
-								itemSpecialPricing.intItemSpecialPricingId = ISNULL(@intItemSpecialPricingId, itemSpecialPricing.intItemSpecialPricingId)
+								ISNULL(itemSpecialPricing.intItemSpecialPricingId,0) = ISNULL(@intItemSpecialPricingId, ISNULL(itemSpecialPricing.intItemSpecialPricingId, 0))
 								AND (
 									NOT EXISTS (SELECT TOP 1 1 FROM #tmpUpdateItemPricingForCStore_Location)
 									OR EXISTS (SELECT TOP 1 1 FROM #tmpUpdateItemPricingForCStore_Location WHERE intLocationId = il.intLocationId) 			
@@ -126,21 +140,54 @@ BEGIN
 												AND (uom.strUpcCode = @strUpcCode OR uom.strLongUPCCode = @strUpcCode)
 									)
 								)
+								AND i.intItemId = ISNULL(@intItemId, i.intItemId)
 					) AS Source_Query  
-						ON itemSpecialPricing.intItemSpecialPricingId = Source_Query.intItemSpecialPricingId					
+						ON itemSpecialPricing.intItemSpecialPricingId = Source_Query.intItemSpecialPricingId	
+						AND itemSpecialPricing.dtmBeginDate = @dtmBeginDate
+						AND itemSpecialPricing.dtmEndDate = @dtmEndDate
 					
 					-- If matched, update the Promotional Retail Price. 
 					WHEN MATCHED THEN 
 						UPDATE 
 						SET		dblUnitAfterDiscount = ISNULL(@dblPromotionalSalesPrice, itemSpecialPricing.dblUnitAfterDiscount)
 								,dblCost = ISNULL(@dblPromotionalCost, itemSpecialPricing.dblCost)
-								,dtmBeginDate = ISNULL(@dtmBeginDate, itemSpecialPricing.dtmBeginDate)
-								,dtmEndDate = ISNULL(@dtmEndDate, itemSpecialPricing.dtmEndDate)
 								,dtmDateModified = GETUTCDATE()
 								,intModifiedByUserId = @intEntityUserSecurityId
+					-- If not matched, insert the Promotional Retail Price. 
+					WHEN NOT MATCHED
+							AND @dtmBeginDate NOT BETWEEN ISNULL(Source_Query.dtmBeginDate, '') AND  ISNULL(Source_Query.dtmEndDate, '')
+							AND @dtmEndDate NOT BETWEEN ISNULL(Source_Query.dtmBeginDate, '') AND  ISNULL(Source_Query.dtmEndDate, '')
+							AND ISNULL(Source_Query.dtmBeginDate, '') NOT BETWEEN @dtmBeginDate AND  @dtmEndDate
+							AND ISNULL(Source_Query.dtmEndDate, '') NOT BETWEEN @dtmBeginDate AND  @dtmEndDate
+						THEN 
+						INSERT (
+							intItemId
+							, intItemLocationId
+							, strPromotionType
+							, dtmBeginDate
+							, dtmEndDate
+							, intItemUnitMeasureId
+							, dblUnit
+							, dblDiscount
+							, dblUnitAfterDiscount --Retail Price
+							, dblCost
+							, intCreatedByUserId
+						)
+						VALUES (
+							Source_Query.intItemId
+							, @intLocationId
+							, 'Vendor Discount'
+							, @dtmBeginDate
+							, @dtmEndDate
+							, Source_Query.intItemUOMId
+							, 1
+							, 0
+							, @dblPromotionalSalesPrice
+							, @dblPromotionalCost
+							, @intEntityUserSecurityId
+						)
 					OUTPUT 
-						$action
-						, inserted.intItemId
+						inserted.intItemId
 						, inserted.intItemSpecialPricingId
 						, deleted.dblUnitAfterDiscount
 						, deleted.dblCost
@@ -150,9 +197,9 @@ BEGIN
 						, inserted.dblCost
 						, inserted.dtmBeginDate
 						, inserted.dtmEndDate
+						, $action AS strAction
 			) AS [Changes] (
-				Action
-				, intItemId
+				intItemId
 				, intItemSpecialPricingId
 				, dblOldUnitAfterDiscount
 				, dblOldCost
@@ -162,8 +209,8 @@ BEGIN
 				, dblNewCost
 				, dtmNewBeginDate
 				, dtmNewEndDate
+				, strAction
 			)
-	WHERE	[Changes].Action = 'UPDATE'
 	;
 END
 
@@ -171,7 +218,7 @@ IF EXISTS (SELECT TOP 1 1 FROM #tmpUpdateItemPricingForCStore_ItemSpecialPricing
 BEGIN 
 
 	DECLARE @auditLog_strDescription AS NVARCHAR(255) 
-			,@auditLog_actionType AS NVARCHAR(50) = 'Updated'
+			,@auditLog_actionType AS NVARCHAR(50) 
 			,@auditLog_id AS INT 
 			,@auditLog_Old AS NVARCHAR(255)
 			,@auditLog_New AS NVARCHAR(255)
@@ -183,28 +230,35 @@ BEGIN
 			,strOld = dblOldUnitAfterDiscount
 			,strNew = dblNewUnitAfterDiscount
 	FROM	#tmpUpdateItemPricingForCStore_ItemSpecialPricingAuditLog auditLog
-	WHERE	ISNULL(dblOldUnitAfterDiscount, 0) <> ISNULL(dblNewUnitAfterDiscount, 0)
+	WHERE	ISNULL(dblOldUnitAfterDiscount, 0) <> ISNULL(dblNewUnitAfterDiscount, 0) AND auditLog.strAction = 'UPDATE'
 	UNION ALL 
 	SELECT	intItemId
 			,strDescription = 'C-Store updates the Promotional Cost'
 			,strOld = dblOldCost
 			,strNew = dblNewCost
 	FROM	#tmpUpdateItemPricingForCStore_ItemSpecialPricingAuditLog auditLog
-	WHERE	ISNULL(dblOldCost, 0) <> ISNULL(dblNewCost, 0)
+	WHERE	ISNULL(dblOldCost, 0) <> ISNULL(dblNewCost, 0) AND auditLog.strAction = 'UPDATE'
 	UNION ALL 
 	SELECT	intItemId
 			,strDescription = 'C-Store updates the Promotional Begin Date'
 			,strOld = dtmOldBeginDate
 			,strNew = dtmNewBeginDate
 	FROM	#tmpUpdateItemPricingForCStore_ItemSpecialPricingAuditLog auditLog
-	WHERE	ISNULL(dtmOldBeginDate, 0) <> ISNULL(dtmNewBeginDate, 0)
+	WHERE	ISNULL(dtmOldBeginDate, 0) <> ISNULL(dtmNewBeginDate, 0) AND auditLog.strAction = 'UPDATE'
 	UNION ALL 
 	SELECT	intItemId
 			,strDescription = 'C-Store updates the Promotional End Date'
 			,strOld = dtmOldEndDate
 			,strNew = dtmNewEndDate
 	FROM	#tmpUpdateItemPricingForCStore_ItemSpecialPricingAuditLog auditLog
-	WHERE	ISNULL(dtmOldEndDate, 0) <> ISNULL(dtmNewEndDate, 0)
+	WHERE	ISNULL(dtmOldEndDate, 0) <> ISNULL(dtmNewEndDate, 0) AND auditLog.strAction = 'UPDATE'
+	UNION ALL 
+	SELECT	intItemId
+			,strDescription = 'C-Store inserts a new Promotion'
+			,strOld = ''
+			,strNew = ''
+	FROM	#tmpUpdateItemPricingForCStore_ItemSpecialPricingAuditLog auditLog
+	WHERE	auditLog.strAction = 'INSERT'
 
 	OPEN loopAuditLog;
 
