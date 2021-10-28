84c30521519cdc511a215f75597117e9d8318a15
Samuel Marvin Aguilos <samuel.aguilos@irely.com>
2021-10-28 16:37:55 +0800

    VR-202

--- i21Database/dbo/Tables/tblApiSchemaTransformVendorSetup.sql
+++ i21Database/dbo/Tables/tblApiSchemaTransformVendorSetup.sql
@@ -8,8 +8,12 @@ CREATE TABLE [dbo].[tblApiSchemaTransformVendorSetup] (
 	strCompany1Id NVARCHAR(200) COLLATE Latin1_General_CI_AS NULL, -- The vendor setup company 1 ID.
 	strCompany2Id NVARCHAR(200) COLLATE Latin1_General_CI_AS NULL, -- The vendor setup company 2 ID.
 	strCustomer NVARCHAR(200) COLLATE Latin1_General_CI_AS NULL, -- The vendor customer cross reference.
+	strVendorCustomer NVARCHAR(200) COLLATE Latin1_General_CI_AS NULL, -- The vendor customer custom name.
 	strItemNo NVARCHAR(200) COLLATE Latin1_General_CI_AS NULL, -- The vendor item cross reference.
+	strVendorItemNo NVARCHAR(200) COLLATE Latin1_General_CI_AS NULL, -- The vendor item custom name.
 	strUnitMeasure NVARCHAR(200) COLLATE Latin1_General_CI_AS NULL, -- The vendor unit measure cross reference.
+	strVendorUnitMeasure NVARCHAR(200) COLLATE Latin1_General_CI_AS NULL, -- The vendor unit measure custom name.
 	strEquipmentType NVARCHAR(200) COLLATE Latin1_General_CI_AS NULL, -- The vendor unit measure cross reference equipment type.
-	strCategory NVARCHAR(200) COLLATE Latin1_General_CI_AS NULL -- The vendor category cross reference.
+	strCategory NVARCHAR(200) COLLATE Latin1_General_CI_AS NULL, -- The vendor category cross reference.
+	strVendorCategory NVARCHAR(200) COLLATE Latin1_General_CI_AS NULL -- The vendor category custom name.
 )
