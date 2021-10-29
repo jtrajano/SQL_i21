61d13588e462605848bef0c6d4539b9b4b364fa4
Arman Arenas <arman.arenas@irely.com>
2021-10-29 17:27:37 +0800

    LG-3113 fixed reserves display of estimated and recalculation amount

--- i21Database/dbo/Stored Procedures/uspLGPurchaseWisePnLReportReserves.sql
+++ i21Database/dbo/Stored Procedures/uspLGPurchaseWisePnLReportReserves.sql
@@ -82,26 +82,23 @@ FROM tblICItem I
 											CC.dblRate
 										WHEN CC.strCostMethod = 'Amount' THEN
 											CC.dblRate 
-											/ dbo.fnCalculateQtyBetweenUOM(CD.intItemUOMId,ToUOM.intItemUOMId,
-												CASE WHEN ISNULL(PTON.dblNetShippedWt, 0) <> 0 THEN PTON.dblNetShippedWt + ISNULL(PTONAdj.dblWtAdjustment, 0)
-													WHEN ISNULL(LS.dblShippedNetQty, 0) <> 0 THEN LS.dblShippedNetQty
-													ELSE ALD.dblSAllocatedQty END)
+											/ CASE WHEN ISNULL(PTON.dblNetShippedWt, 0) <> 0 THEN PTON.dblNetShippedWt + ISNULL(PTONAdj.dblWtAdjustment, 0)
+													WHEN ISNULL(LS.dblShippedNetQty, 0) <> 0 THEN dbo.fnCalculateQtyBetweenUOM(CD.intItemUOMId,ToUOM.intItemUOMId,LS.dblShippedNetQty)
+													ELSE dbo.fnCalculateQtyBetweenUOM(CD.intItemUOMId,ToUOM.intItemUOMId,ALD.dblSAllocatedQty) END
 										ELSE CC.dblRate END * COALESCE(CC.dblFX, FX.dblFXRate, 1)
 						,dblAmount = CASE WHEN CC.strCostMethod = 'Per Unit' THEN
-											dbo.fnCalculateQtyBetweenUOM(CD.intItemUOMId,ToUOM.intItemUOMId,
-												CASE WHEN ISNULL(PTON.dblNetShippedWt, 0) <> 0 THEN PTON.dblNetShippedWt + ISNULL(PTONAdj.dblWtAdjustment, 0)
-													WHEN ISNULL(LS.dblShippedNetQty, 0) <> 0 THEN LS.dblShippedNetQty
-													ELSE ALD.dblSAllocatedQty END)
+											CASE WHEN ISNULL(PTON.dblNetShippedWt, 0) <> 0 THEN PTON.dblNetShippedWt + ISNULL(PTONAdj.dblWtAdjustment, 0)
+													WHEN ISNULL(LS.dblShippedNetQty, 0) <> 0 THEN dbo.fnCalculateQtyBetweenUOM(CD.intItemUOMId,ToUOM.intItemUOMId,LS.dblShippedNetQty)
+													ELSE dbo.fnCalculateQtyBetweenUOM(CD.intItemUOMId,ToUOM.intItemUOMId,ALD.dblSAllocatedQty) END
 											* dbo.fnCalculateCostBetweenUOM(CC.intItemUOMId,CToUOM.intItemUOMId,CC.dblRate) / ISNULL(CCUR.intCent, 1)
 										WHEN CC.strCostMethod = 'Amount' THEN
 											CC.dblRate
 										WHEN CC.strCostMethod = 'Per Container'	THEN
 											CC.dblRate * (CASE WHEN ISNULL(CD.intNumberOfContainers,1) = 0 THEN 1 ELSE ISNULL(CD.intNumberOfContainers,1) END)
 										WHEN CC.strCostMethod = 'Percentage' THEN 
-											dbo.fnCalculateQtyBetweenUOM(CD.intItemUOMId,ToUOM.intItemUOMId,
-												CASE WHEN ISNULL(PTON.dblNetShippedWt, 0) <> 0 THEN PTON.dblNetShippedWt + ISNULL(PTONAdj.dblWtAdjustment, 0)
-													WHEN ISNULL(LS.dblShippedNetQty, 0) <> 0 THEN LS.dblShippedNetQty
-													ELSE ALD.dblSAllocatedQty END)
+											CASE WHEN ISNULL(PTON.dblNetShippedWt, 0) <> 0 THEN PTON.dblNetShippedWt + ISNULL(PTONAdj.dblWtAdjustment, 0)
+													WHEN ISNULL(LS.dblShippedNetQty, 0) <> 0 THEN dbo.fnCalculateQtyBetweenUOM(CD.intItemUOMId,ToUOM.intItemUOMId,LS.dblShippedNetQty)
+													ELSE dbo.fnCalculateQtyBetweenUOM(CD.intItemUOMId,ToUOM.intItemUOMId,ALD.dblSAllocatedQty) END
 											* CD.dblCashPrice * CC.dblRate/100
 										END 
 									* COALESCE(CC.dblFX, FX.dblFXRate, 1)
