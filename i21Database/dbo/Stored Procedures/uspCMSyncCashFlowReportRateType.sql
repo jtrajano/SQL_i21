8424c90377063e606cae65c0e87e08afd670f175
Mark Daniel Marmeto <mark.marmeto@irely.com>
2021-11-02 09:59:18 +0800

    GL-8435

--- i21Database/dbo/Stored Procedures/uspCMSyncCashFlowReportRateType.sql
+++ i21Database/dbo/Stored Procedures/uspCMSyncCashFlowReportRateType.sql
@@ -1,4 +1,4 @@
-﻿CREATE PROCEDURE [dbo].[uspCMSyncCashFlowReportRateType]
+﻿ALTER PROCEDURE [dbo].[uspCMSyncCashFlowReportRateType]
 (
 	@intCashFlowReportId INT,
 	@intReportingCurrencyId INT = NULL,
@@ -8,6 +8,8 @@ AS
 
 IF (@intReportingCurrencyId IS NULL)
 	SELECT @intReportingCurrencyId = intReportingCurrencyId FROM tblCMCashFlowReport WHERE intCashFlowReportId = @intCashFlowReportId
+IF (@intFilterCurrencyId IS NULL)
+	SELECT @intFilterCurrencyId = intFilterCurrencyId FROM tblCMCashFlowReport WHERE intCashFlowReportId = @intCashFlowReportId
 
 IF NOT EXISTS(SELECT TOP 1 1 FROM tblCMCashFlowReportRateType WHERE intCashFlowReportId = @intCashFlowReportId) -- create new rates
 BEGIN
@@ -38,42 +40,26 @@ ELSE   -- Update existing rates
 BEGIN
 	IF (@intFilterCurrencyId IS NOT NULL)
 	BEGIN
+		DELETE tblCMCashFlowReportRateType WHERE intCashFlowReportId = @intCashFlowReportId
+		DELETE tblCMCashFlowReportRate WHERE intCashFlowReportId = @intCashFlowReportId
 
-		IF ((SELECT COUNT(1) intCount FROM tblCMCashFlowReportRateType WHERE intFilterCurrencyId = @intFilterCurrencyId) = 1)
-		BEGIN
-			UPDATE tblCMCashFlowReportRateType
-			SET intFilterCurrencyId = @intFilterCurrencyId
-
-			WHERE intCashFlowReportId = @intCashFlowReportId
-
-			UPDATE tblCMCashFlowReportRate
-			SET intFilterCurrencyId = @intFilterCurrencyId
-			WHERE intCashFlowReportId = @intCashFlowReportId
-			
-			GOTO EXIT_PROCESS
-		END
-		ELSE
-		BEGIN
-			DELETE tblCMCashFlowReportRateType WHERE intCashFlowReportId = @intCashFlowReportId
-			DELETE tblCMCashFlowReportRate WHERE intCashFlowReportId = @intCashFlowReportId
-
-			INSERT INTO tblCMCashFlowReportRateType (intCashFlowReportId, intFilterCurrencyId) VALUES (@intCashFlowReportId, @intFilterCurrencyId)
-			INSERT INTO tblCMCashFlowReportRate (
-				intCashFlowReportId, 
-				intFilterCurrencyId,
-				dblRateBucket1,
-				dblRateBucket2,
-				dblRateBucket3,
-				dblRateBucket4,
-				dblRateBucket5,
-				dblRateBucket6,
-				dblRateBucket7,
-				dblRateBucket8,
-				dblRateBucket9
-			) 
-			VALUES (@intCashFlowReportId, @intFilterCurrencyId, 1, 1, 1, 1, 1, 1, 1, 1, 1)
-			GOTO EXIT_PROCESS
-		END
+		INSERT INTO tblCMCashFlowReportRateType (intCashFlowReportId, intFilterCurrencyId) VALUES (@intCashFlowReportId, @intFilterCurrencyId)
+		INSERT INTO tblCMCashFlowReportRate (
+			intCashFlowReportId, 
+			intFilterCurrencyId,
+			dblRateBucket1,
+			dblRateBucket2,
+			dblRateBucket3,
+			dblRateBucket4,
+			dblRateBucket5,
+			dblRateBucket6,
+			dblRateBucket7,
+			dblRateBucket8,
+			dblRateBucket9
+		) 
+		VALUES (@intCashFlowReportId, @intFilterCurrencyId, 1, 1, 1, 1, 1, 1, 1, 1, 1)
+		
+		GOTO EXIT_PROCESS
 	END
 	
 	GOTO GET_FROM_CURRENCY_EXCHANGE
@@ -81,12 +67,15 @@ END
 
 GET_FROM_CURRENCY_EXCHANGE:
 
+DELETE tblCMCashFlowReportRateType WHERE intCashFlowReportId = @intCashFlowReportId
+DELETE tblCMCashFlowReportRate WHERE intCashFlowReportId = @intCashFlowReportId
+
 DECLARE @tblCurrency TABLE (
 	intFromCurrencyId INT
 )
 
 INSERT INTO @tblCurrency
-SELECT intFromCurrencyId
+SELECT DISTINCT intFromCurrencyId
 FROM tblSMCurrencyExchangeRate 
 WHERE intToCurrencyId = @intReportingCurrencyId
 
@@ -98,10 +87,13 @@ END
 ELSE
 BEGIN
 	IF (@intFilterCurrencyId IS NULL)
-		INSERT INTO @tblCurrency SELECT intCurrencyID FROM tblSMCurrency WHERE intCurrencyID <> @intReportingCurrencyId
+		INSERT INTO @tblCurrency SELECT DISTINCT intCurrencyID FROM tblSMCurrency
 	ELSE
 	BEGIN
-		INSERT INTO tblCMCashFlowReportRateType (intCashFlowReportId, intFilterCurrencyId) VALUES (@intCashFlowReportId, @intFilterCurrencyId)
+		INSERT INTO tblCMCashFlowReportRateType (intCashFlowReportId, intFilterCurrencyId) 
+		VALUES 
+			(@intCashFlowReportId, @intFilterCurrencyId)
+
 		INSERT INTO tblCMCashFlowReportRate (
 				intCashFlowReportId, 
 				intFilterCurrencyId,
@@ -115,17 +107,25 @@ BEGIN
 				dblRateBucket8,
 				dblRateBucket9
 			) 
-		VALUES (@intCashFlowReportId, @intFilterCurrencyId, 1, 1, 1, 1, 1, 1, 1, 1, 1)
+		VALUES 
+			(@intCashFlowReportId, @intReportingCurrencyId, 1, 1, 1, 1, 1, 1, 1, 1, 1)
 	END
 END
 
 INSERT INTO tblCMCashFlowReportRateType (intCashFlowReportId, intFilterCurrencyId, intConcurrencyId)
-SELECT @intCashFlowReportId, intFromCurrencyId, 1
+SELECT DISTINCT @intCashFlowReportId, intFromCurrencyId, 1
 FROM @tblCurrency
+
 
 INSERT INTO tblCMCashFlowReportRate
-SELECT @intCashFlowReportId, intFromCurrencyId, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+SELECT DISTINCT @intCashFlowReportId, intFromCurrencyId, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
 FROM @tblCurrency
 
-
 EXIT_PROCESS:
+
+-- Add reporting currency
+DELETE tblCMCashFlowReportRateType WHERE intFilterCurrencyId = @intReportingCurrencyId AND intCashFlowReportId = @intCashFlowReportId
+DELETE tblCMCashFlowReportRate WHERE intFilterCurrencyId = @intReportingCurrencyId AND intCashFlowReportId = @intCashFlowReportId
+
+INSERT INTO tblCMCashFlowReportRateType (intCashFlowReportId, intFilterCurrencyId, intConcurrencyId) VALUES (@intCashFlowReportId, @intReportingCurrencyId, 1)
+INSERT INTO tblCMCashFlowReportRate VALUES (@intCashFlowReportId, @intReportingCurrencyId, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
