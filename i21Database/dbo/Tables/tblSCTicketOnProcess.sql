a4869463dfdbd0193d5b69e545338e7d3ae56607
Kenneth Lew Lagunero <kenneth.lagunero@irely.com>
2021-11-02 21:05:54 +0800

    SC-4355
    # Conflicts:
    #	i21Database/i21Database.sqlproj
    
    # Conflicts:
    #	i21Database/i21Database.sqlproj

---
+++ i21Database/dbo/Tables/tblSCTicketOnProcess.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[tblSCTicketOnProcess]
+(
+	[intTicketOnProcessId] INT NOT NULL IDENTITY, 
+    [intTicketId] INT NOT NULL, 
+    [intUserId] INT NOT NULL, 
+    [ysnInProgress] BIT NOT NULL DEFAULT 0,
+    [intConcurrencyId] INT NOT NULL DEFAULT 0
+	CONSTRAINT [PK_tblSCTicketOnProcess_intTicketOnProcessId] PRIMARY KEY ([intTicketOnProcessId]) 
+)
+GO
+
+CREATE NONCLUSTERED INDEX [IX_tblSCTicketLoadUsed_intLoadDetailId]
+ON [dbo].[tblSCTicketOnProcess] ([intTicketId],[intUserId],[ysnInProgress])
+GO
+
+CREATE UNIQUE NONCLUSTERED INDEX [UK_tblSCTicketOnProcess_intTicketid] ON [dbo].[tblSCTicketOnProcess]
+(
+	[intTicketId] ASC
+)
+GO
