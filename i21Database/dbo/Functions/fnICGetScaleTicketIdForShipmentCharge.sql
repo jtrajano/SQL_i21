5502800f6a7f95607ba314610a6dde8464b7aeec
Feb Montefrio <feb.montefrio@irely.com>
2021-11-03 15:39:25 +0800

    [F] IC-9888

--- i21Database/dbo/Functions/fnICGetScaleTicketIdForShipmentCharge.sql
+++ i21Database/dbo/Functions/fnICGetScaleTicketIdForShipmentCharge.sql
@@ -6,15 +6,23 @@ CREATE FUNCTION [dbo].[fnICGetScaleTicketIdForShipmentCharge](
 )
 RETURNS TABLE
 RETURN (
-	-- Assuming that there is only one IR per Scale Ticket id, then pick the top ticket id we can retrieve 
-	-- when joining the IR and Scale Ticket tables. 
+	-- Assuming that there is only one Shipment per Scale Ticket id, then pick the top ticket id we can retrieve 
+	-- when joining the Shipment and Scale Ticket tables. 
 	SELECT	TOP 1 
 			intScaleTicketId = st.intTicketId
 			,strScaleTicketNumber = st.strTicketNumber
+			,l.strLoadNumber
+			,l.intLoadId
+			,ld.intLoadDetailId
 	FROM	tblICInventoryShipment r INNER JOIN tblICInventoryShipmentItem ri
 				ON r.intInventoryShipmentId = ri.intInventoryShipmentId
 			INNER JOIN tblSCTicket st
 				ON st.intTicketId = ri.intSourceId
+			LEFT JOIN tblLGLoadDetail ld
+				ON ld.intLoadDetailId = st.intLoadDetailId 
+			LEFT JOIN tblLGLoad l
+				ON l.intLoadId = ld.intLoadId
+
 	WHERE	r.intInventoryShipmentId = @intInventoryShipmentId
 			AND r.strShipmentNumber = @strShipmentNumber 
 			AND r.intSourceType = 1 -- Scale
