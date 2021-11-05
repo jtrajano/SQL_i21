fafdc6d7f3e033bc01aa3852722d96b0d43624a3
Rey Anthony Crisostomo <anthony.crisostomo@iRely.com>
2021-11-05 17:48:15 +0800

    PR-2440 - data type fix

--- i21Database/dbo/Tables/tblApiSchemaEmployeeDirectDeposit.sql
+++ i21Database/dbo/Tables/tblApiSchemaEmployeeDirectDeposit.sql
@@ -2,7 +2,7 @@ CREATE TABLE tblApiSchemaEmployeeDirectDeposit(
     guiApiUniqueId UNIQUEIDENTIFIER NOT NULL
     ,intRowNumber INT NULL
     ,intKey INT IDENTITY(1,1) NOT NULL PRIMARY KEY
-    ,intEntityNo NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL NOT NULL                                               --Required
+    ,intEntityNo NVARCHAR(100) COLLATE Latin1_General_CI_AS NOT NULL                                               --Required
     ,strBankName NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL
     ,strAccountNumber NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL
     ,strAccountType NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL        --Values are Checking and Savings only
