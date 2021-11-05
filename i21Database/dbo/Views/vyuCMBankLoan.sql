19abc14a68a8bde4a004637d662230e60b1db209
Jeff Trajano <jeffrey.trajano@irely.com>
2021-11-05 15:34:09 +0800

    GL-8331 Bank Loan changes

--- i21Database/dbo/Views/vyuCMBankLoan.sql
+++ i21Database/dbo/Views/vyuCMBankLoan.sql
@@ -2,11 +2,7 @@
 AS
 SELECT   
 L.*,
-V.strTransactionId,  
 BA.strBankAccountNo,  
-U.dblBalance,  
-intGLLoanAccountId = BA.intGLAccountId,  
-strGLLoanAccountId = BA.strGLAccountId,  
 BA.strBankName,  
 BA.strCurrency,  
 T.intCurrencyId,  
@@ -28,27 +24,6 @@ END,
 strBorrowingFacilityId
 FROM tblCMBankLoan L   
 LEFT JOIN tblCMBankTransaction T  
-ON L.intBankLoanId = T.intBankLoanId  
-AND intBankTransactionTypeId = 52  
+ON L.intBankLoanId = T.intBankLoanId
 LEFT JOIN vyuCMBankAccount BA ON BA.intBankAccountId = L.intBankAccountId
 LEFT JOIN tblCMBorrowingFacility BF ON BF.intBorrowingFacilityId = L.intBorrowingFacilityId
-OUTER APPLY  
-(  
- SELECT SUM(BB.dblCredit - BB.dblDebit) dblBalance   
- FROM tblCMBankTransaction AA JOIN tblCMBankTransactionDetail BB  
- on AA.intTransactionId = BB.intTransactionId  
- WHERE AA.intBankLoanId = L.intBankLoanId   
-)U  
-OUTER APPLY  
-(  
- SELECT SUM(DD.dblCredit - DD.dblDebit) dblBalance, FF.strAccountId, 
- CC.strTransactionId, DD.intGLAccountId  
- FROM tblCMBankTransaction CC   
- JOIN tblCMBankTransactionDetail DD on CC.intTransactionId = DD.intTransactionId  
- JOIN vyuCMBankAccount EE on EE.intBankAccountId = CC.intBankAccountId  
- JOIN tblGLAccount FF on DD.intGLAccountId = FF.intAccountId  
-   
-  WHERE CC.intBankLoanId = L.intBankLoanId   
-  AND CC.intBankTransactionTypeId = 52  
-  GROUP BY DD.intGLAccountId, FF.strAccountId, CC.strTransactionId
-)V
--- i21Database/dbo/Views/vyuCMBankLoanDetail.sql
+++ i21Database/dbo/Views/vyuCMBankLoanDetail.sql
@@ -1,30 +1,22 @@
 ﻿CREATE VIEW [dbo].[vyuCMBankLoanDetail] AS
 SELECT 
-D.intTransactionDetailId,
-D.intTransactionId,
+T.intTransactionId,
 T.intBankTransactionTypeId,
-strGLAccountIdDetail = A.strAccountId,
 P.strBankTransactionTypeName,
-intGLAccountIdDetail = D.intGLAccountId,
 T.strTransactionId,
 L.intBankLoanId,
 T.dtmDate,
-detail.dblCredit,
+T.dblAmount,
 T.strMemo,
-T.ysnPosted,
 T.intConcurrencyId
 FROM tblCMBankLoan L
 JOIN tblCMBankTransaction T
 ON L.intBankLoanId = T.intBankLoanId
-LEFT JOIN tblCMBankTransactionDetail D
-ON D.intTransactionId = T.intTransactionId
-LEFT JOIN tblGLAccount A
-on A.intAccountId = D.intGLAccountId
+JOIN tblCMBankAccount BA 
+ON BA.intBankAccountId=T.intBankAccountId
 LEFT JOIN
 tblCMBankTransactionType P
 ON P.intBankTransactionTypeId = T.intBankTransactionTypeId
-OUTER APPLY(
-SELECT sum(dblCredit - dblDebit) dblCredit FROM tblCMBankTransactionDetail WHERE intTransactionId = T.intTransactionId
-) detail
+WHERE T.ysnPosted = 1
 GO
 
