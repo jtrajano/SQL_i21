8aaeb6af9b6c356be3fdc76124ad6dfef700d232
Girome Andaya <girome.andaya@iRely.com>
2021-11-09 17:42:45 +0800

    CCR-483
    (cherry picked from commit 2f309e8a5eda01d82adc9e9b998090b0b15e2daa)
    (cherry picked from commit 73657a5edba5757edd5ba648f1479c5fac069652)

--- i21Database/dbo/Tables/tblApiSchemaCCVendorSite.sql
+++ i21Database/dbo/Tables/tblApiSchemaCCVendorSite.sql
@@ -8,10 +8,9 @@
 	strCustomerNumber			NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL,		-- Customer	Number			| Required
 	strVendorNumber				NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL,		-- Vendor Number			| Required
 	strSite						NVARCHAR(100) COLLATE Latin1_General_CI_AS NOT NULL,	-- Site						| Required
+	strDescription				NVARCHAR(250) COLLATE Latin1_General_CI_AS NOT NULL,	-- Description				| Required
 	strSiteType					NVARCHAR(10) COLLATE Latin1_General_CI_AS NOT NULL,		-- Site Type Flag			| Required
-	strPayType					NVARCHAR(100) COLLATE Latin1_General_CI_AS NOT NULL,	-- AR Pay Type				| Required
+	strPayType					NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL,		-- AR Pay Type				| Optional - Defaults to 'Debit Memos and Payments'
 	strFeeExpenseGL				NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL,			-- Fee Expense GL			| Required Only If strSiteType = 'I'
 	dblSharedFee				NUMERIC(18, 6) NULL,									-- Shared Fee				| Optional
-	ysnPostNet					BIT NULL DEFAULT ((1))									-- Post Net					| Optional
 )
-
