0b941bb918caabe8b6fc2f63ee063090261189ae
Girome Andaya <girome.andaya@iRely.com>
2021-11-10 20:21:57 +0800

    TR-1733

---
+++ i21Database/dbo/Triggers/trgAfterDeleteTRSupplyPoint.sql
@@ -0,0 +1,12 @@
+﻿CREATE TRIGGER [dbo].[trgAfterDeleteTRSupplyPoint] 
+	ON [dbo].[tblTRSupplyPoint]
+	FOR DELETE
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	IF EXISTS (SELECT TOP 1 1 FROM tblTRSupplyPoint WHERE intRackPriceSupplyPointId IN (SELECT intSupplyPointId FROM deleted))
+	BEGIN
+		UPDATE tblTRSupplyPoint SET intRackPriceSupplyPointId = NULL WHERE intRackPriceSupplyPointId IN (SELECT intSupplyPointId FROM deleted) 
+	END
+END
