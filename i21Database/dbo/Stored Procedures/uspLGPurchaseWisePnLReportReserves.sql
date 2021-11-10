26a13eec7899797656fb0aee2f3bfc2d41e0d635
Arman Arenas <arman.arenas@irely.com>
2021-11-10 19:27:01 +0800

    LG-3128 fixed tonnage calculation if claim is present

--- i21Database/dbo/Stored Procedures/uspLGPurchaseWisePnLReportReserves.sql
+++ i21Database/dbo/Stored Procedures/uspLGPurchaseWisePnLReportReserves.sql
@@ -54,7 +54,10 @@ FROM tblICItem I
 		FROM 
 			(SELECT bl.intBillId, bld.intItemId, bld.intContractDetailId, bl.ysnPosted, bl.intTransactionType
 				,dblQtyReceived = dbo.fnCalculateQtyBetweenUOM (bld.intWeightUOMId, PCD.intItemUOMId, 
-										CASE WHEN (bld.intItemId <> PCD.intItemId) THEN 0 ELSE bld.dblNetWeight END) 
+										CASE WHEN (bld.intItemId <> PCD.intItemId) THEN 0 
+											ELSE 
+												CASE WHEN bl.intTransactionType IN (11) THEN bld.dblQtyOrdered ELSE bld.dblNetWeight  END
+											END) 
 									* CASE WHEN bl.intTransactionType IN (3, 11) THEN -1 ELSE 1 END
 				FROM tblAPBillDetail bld
 				INNER JOIN tblAPBill bl on bl.intBillId = bld.intBillId) BLD
