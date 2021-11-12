61583240d2db1392aad52706cac9950bf44eb26c
kelvin_quinto <kelvin.quinto@irely.com>
2021-11-12 13:13:41 +0800

    AR-13996

--- i21Database/dbo/Stored Procedures/uspARCreateInvoiceFromShipment.sql
+++ i21Database/dbo/Stored Procedures/uspARCreateInvoiceFromShipment.sql
@@ -882,6 +882,7 @@ IF EXISTS (SELECT TOP 1 NULL FROM #CONTRACTSPRICING)
 					  , @intPriceFixationId				INT = NULL
 					  , @intContractDetailToDeleteId    INT = NULL
 					  , @ysnLoad						BIT = 0
+					  , @intContractDetailId			INT = NULL
 
 				SELECT TOP 1 @intInvoiceEntriesId			= intInvoiceEntriesId 
 						   , @dblQtyShipped					= dblQtyShipped
@@ -889,6 +890,7 @@ IF EXISTS (SELECT TOP 1 NULL FROM #CONTRACTSPRICING)
 						   , @intPriceFixationId			= intPriceFixationId
 						   , @intContractDetailToDeleteId	= intContractDetailId
 						   , @ysnLoad						= ysnLoad
+						   , @intContractDetailId			= intContractDetailId
 				FROM #CONTRACTSPRICING 
 				ORDER BY intInvoiceEntriesId
 
@@ -922,10 +924,13 @@ IF EXISTS (SELECT TOP 1 NULL FROM #CONTRACTSPRICING)
 									END
 
 								UPDATE @EntriesForInvoice
-								SET dblQtyOrdered	= CASE WHEN @ysnLoad = 0 THEN dblQtyOrdered ELSE @dblOriginalQtyShipped END
-								  , dblPrice		= @dblFinalPrice
+								SET dblPrice		= @dblFinalPrice
 								  , dblUnitPrice	= @dblFinalPrice
-								  ,  intPriceFixationDetailId	= @intPriceFixationDetailId
+								  , intPriceFixationDetailId	= @intPriceFixationDetailId
+								WHERE intId = @intInvoiceEntriesId OR (intContractDetailId = @intContractDetailId AND ISNULL(intOrderUOMId, 0) <> 0)
+
+								UPDATE @EntriesForInvoice
+								SET dblQtyOrdered	= CASE WHEN @ysnLoad = 0 THEN dblQtyOrdered ELSE @dblOriginalQtyShipped END
 								WHERE intId = @intInvoiceEntriesId
 
 								SET @dblQtyShipped = @dblQtyShipped - @dblQuantity
