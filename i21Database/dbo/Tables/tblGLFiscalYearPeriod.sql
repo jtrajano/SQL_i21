eb819c8ac0a14d8bc375f23985d7bc71cfe70605
Jeff Trajano <jeffrey.trajano@irely.com>
2021-11-15 10:53:52 +0800

    GL-8490 Fiscal Period should be unique

--- i21Database/dbo/Tables/tblGLFiscalYearPeriod.sql
+++ i21Database/dbo/Tables/tblGLFiscalYearPeriod.sql
@@ -19,9 +19,16 @@
     [ysnCMRevalued]				BIT				DEFAULT 0 NULL,
 	[ysnConsolidated]			BIT				DEFAULT 0 NULL,
     [intConcurrencyId]			INT				DEFAULT 1 NOT NULL,
-    CONSTRAINT [PK_tblGLPeriod] PRIMARY KEY CLUSTERED ([intGLFiscalYearPeriodId] ASC, [intFiscalYearId] ASC),
-    CONSTRAINT [FK_tblGLPeriod_tblGLFiscalYearPeriod] FOREIGN KEY ([intFiscalYearId]) REFERENCES [dbo].[tblGLFiscalYear] ([intFiscalYearId]) ON DELETE CASCADE
-);
+   CONSTRAINT [PK_tblGLPeriod] PRIMARY KEY CLUSTERED 
+(
+	[intGLFiscalYearPeriodId] ASC,
+	[intFiscalYearId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 70) ON [PRIMARY],
+ CONSTRAINT [IX_tblGLFiscalYearPeriod] UNIQUE NONCLUSTERED 
+(
+	[strPeriod] ASC --GL-8520 Make Fiscal Period unique
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
 GO
 
 CREATE NONCLUSTERED INDEX [IX_tblGLFiscalYearPeriod_intFiscalYearId]
--- i21PreDeployment/Scripts/Pre-Deployment/6_GL/7_2110.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/6_GL/7_2110.sql
@@ -2,3 +2,26 @@ GO
 IF object_id('tblGLAccountImportDataStaging2') IS NOT NULL   
     DROP TABLE dbo.tblGLAccountImportDataStaging2  
 GO
+
+/* BEGIN 21.2  */
+--GL-8520 Make Fiscal Period unique
+IF NOT EXISTS (SELECT TOP 1 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[tblGLDataFixLog]') AND type in (N'U')) 
+    CREATE TABLE [dbo].[tblGLDataFixLog](
+	[intLogId] [int] IDENTITY(1,1) NOT NULL,
+	[dtmDate] [datetime] NULL,
+	[strDescription] [nvarchar](100) COLLATE Latin1_General_CI_AS NULL,
+    CONSTRAINT [PK_tblGLDataFixLog] PRIMARY KEY CLUSTERED 
+    (
+        [intLogId] ASC
+    )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+    ) ON [PRIMARY]
+    GO
+
+
+IF NOT EXISTS(SELECT TOP 1 1 FROM tblGLDataFixLog WHERE strDescription= 'Update fiscal period name')
+BEGIN
+    UPDATE tblGLFiscalYearPeriod SET strPeriod = DATENAME(MONTH, dtmStartDate) + ' ' + DATENAME(YEAR, dtmStartDate)
+    INSERT INTO tblGLDataFixLog(dtmDate, strDescription) VALUES(GETDATE(), 'Update fiscal period name')
+END
+GO
+/* END 21.2  */
