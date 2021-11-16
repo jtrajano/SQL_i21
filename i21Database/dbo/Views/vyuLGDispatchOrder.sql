77e6c3430443a77b00631a455791e4379c49360e
Arman Arenas <arman.arenas@irely.com>
2021-11-16 13:47:38 +0800

    LG-3034 added vyuLGDispatchOrder

---
+++ i21Database/dbo/Views/vyuLGDispatchOrder.sql
@@ -0,0 +1,26 @@
+﻿CREATE VIEW [dbo].[vyuLGDispatchOrder]
+AS
+SELECT
+	DO.intDispatchOrderId
+	,DO.strDispatchOrderNumber
+	,DO.dtmDispatchDate
+	,DO.dtmDeliveryTimeStart
+	,DO.dtmDeliveryTimeEnd
+	,DO.intEntityShipViaId
+	,DO.intEntityShipViaTruckId
+	,DO.intDriverEntityId
+	,DO.intDeliveryStatus
+	,DO.intFromCompanyLocationId
+	,DO.intFromCompanyLocationSubLocationId
+	,DO.strComments
+	,DO.intConcurrencyId
+	,strShipVia = SV.strName
+	,strTruckNumber = SVT.strTruckNumber
+	,strLocation = CL.strLocationName
+	,strSubLocation = CLSL.strSubLocationName
+FROM tblLGDispatchOrder DO
+	LEFT JOIN tblEMEntity SV ON SV.intEntityId = DO.intEntityShipViaId
+	LEFT JOIN tblSMShipViaTruck SVT ON SVT.intEntityShipViaTruckId = DO.intEntityShipViaTruckId
+	LEFT JOIN tblSMCompanyLocation CL ON CL.intCompanyLocationId = DO.intFromCompanyLocationId
+	LEFT JOIN tblSMCompanyLocationSubLocation CLSL ON CLSL.intCompanyLocationSubLocationId = DO.intFromCompanyLocationSubLocationId
+GO
