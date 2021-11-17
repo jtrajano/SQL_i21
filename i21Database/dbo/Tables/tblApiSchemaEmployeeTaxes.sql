8a0cdfc4f7cd4c7c95bfcf2f739201d9ec5a031b
Rey Anthony Crisostomo <anthony.crisostomo@iRely.com>
2021-11-17 11:06:31 +0800

    PR-2258 - applying conversions

--- i21Database/dbo/Tables/tblApiSchemaEmployeeTaxes.sql
+++ i21Database/dbo/Tables/tblApiSchemaEmployeeTaxes.sql
@@ -2,14 +2,14 @@ CREATE TABLE tblApiSchemaEmployeeTaxes(
     guiApiUniqueId UNIQUEIDENTIFIER NOT NULL,
     intRowNumber INT NULL,
     intKey INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
-    intEntityNo INT NOT NULL,
+    intEntityNo NVARCHAR(100) COLLATE Latin1_General_CI_AS NOT NULL,                                                   --Required. Employee Entity ID
     strTaxId NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
     strTaxDescription NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
     strCalculationType NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
     strPaidBy NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
-    ysnDefault NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL, 
-    strFilingStatus NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
-    strState NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
+    ysnDefault BIT  NULL,                --Default Values should be "Y" and "N" only
+    strFilingStatus NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,           --Default Values should be Single and Married
+    strState NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,                  --Default Values should be state names only
     strCountry NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
     dblAmount FLOAT(50) NULL,
     dblExtraWithholding FLOAT(50) NULL,
@@ -17,14 +17,14 @@ CREATE TABLE tblApiSchemaEmployeeTaxes(
     dblFederalAllowance FLOAT(50) NULL,
     strSupplimentalCalc NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
     strLiabilityAccount NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
-    ysnLiabilityGlSplit NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL, 
-    strExpenseAccount NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,
-    ysnExpenseAccountGlSplit NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL, 
-    ysn2020W4 NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL, 
-    ysnStep2c NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL, 
+    ysnLiabilityGlSplit BIT  NULL,       --Default Values should be "Y" and "N" only
+    strExpenseAccount NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL,         --Default Values should be "Y" and "N" only
+    ysnExpenseAccountGlSplit BIT NULL,  --Default Values should be "Y" and "N" only
+    ysn2020W4 BIT NULL,                 --Default Values should be "Y" and "N" only
+    ysnStep2c BIT NULL,                 --Default Values should be "Y" and "N" only
     dblClaimDependents FLOAT(50) NULL,
     dblotherIncome FLOAT(50) NULL,
-    ysnResident NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL, 
+    ysnResident BIT NULL,               --Default Values should be "Y" and "N" only
     intDependents INT NULL,
     dblPercentGross FLOAT(50) NULL,
     strAccountId NVARCHAR(100) COLLATE Latin1_General_CI_AS  NULL, 
