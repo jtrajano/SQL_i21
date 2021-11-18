f915824ae7bdc33a7b67bbeb9f91fccfc4d6a89d
Jeff Trajano <jeffrey.trajano@irely.com>
2021-11-18 08:50:57 +0800

    GL-8533

--- i21Database/dbo/Tables/tblGLTempCOASegment.sql
+++ i21Database/dbo/Tables/tblGLTempCOASegment.sql
@@ -1,9 +1,9 @@
 
 CREATE TABLE [dbo].[tblGLTempCOASegment](
 	[intAccountId] [int] NOT NULL,
-	[strAccountId] [nvarchar](40) NOT NULL,
-	[Location] [nvarchar](20) NULL,
-	[Primary Account] [nvarchar](20) NULL,
+	[strAccountId] [nvarchar](40) COLLATE Latin1_General_CI_AS NOT NULL,
+	[Location] [nvarchar](20) COLLATE Latin1_General_CI_AS NULL,
+	[Primary Account] [nvarchar](20) COLLATE Latin1_General_CI_AS NULL,
 	[LOB] [nvarchar](20) NULL,
  CONSTRAINT [PK_tblGLTempCOASegment] PRIMARY KEY CLUSTERED 
 (
