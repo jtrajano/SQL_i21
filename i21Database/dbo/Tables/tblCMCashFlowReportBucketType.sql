26caa18f5607af52abb9857239b1e4da467f9a43
Mark Daniel Marmeto <mark.marmeto@irely.com>
2021-11-19 11:18:50 +0800

    GL-8433

--- i21Database/dbo/Tables/tblCMCashFlowReportBucketType.sql
+++ i21Database/dbo/Tables/tblCMCashFlowReportBucketType.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[tblCMCashFlowReportBucketType]
 (
 	[intCashFlowReportBucketTypeId] INT IDENTITY(1, 1) NOT NULL,
-	[strCashFlowReportBucketType]	NVARCHAR(100) NOT NULL,
+	[strCashFlowReportBucketType]	NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL,
 	[intConcurrencyId]				INT DEFAULT 1 NOT NULL,
 
 	CONSTRAINT [PK_tblCMCashFlowReportBucketType] PRIMARY KEY CLUSTERED ([intCashFlowReportBucketTypeId] ASC)
