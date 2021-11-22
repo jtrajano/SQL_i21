018c9f2ba82f658d64391a87e5e1aae41fa367e4
Jeff Trajano <jeffrey.trajano@irely.com>
2021-11-22 13:49:31 +0800

    GL-8548

--- i21PreDeployment/Scripts/Pre-Deployment/6_GL/7_2110.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/6_GL/7_2110.sql
@@ -16,13 +16,18 @@ IF NOT EXISTS (SELECT TOP 1 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[db
     GO
 
 /* add data fixes before the constraint is applied*/
-IF NOT EXISTS(SELECT TOP 1 1 FROM tblGLDataFixLog WHERE strDescription= 'Update fiscal period name.')
+IF EXISTS (SELECT TOP 1 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[tblGLFiscalYearPeriod]') AND type in (N'U')) 
 BEGIN
-    UPDATE tblGLFiscalYearPeriod SET strPeriod = DATENAME(MONTH, dtmStartDate) + ' ' + DATENAME(YEAR, dtmStartDate)
-    INSERT INTO tblGLDataFixLog(dtmDate, strDescription) VALUES(GETDATE(), 'Update fiscal period name.')
-END
-IF NOT EXISTS(SELECT TOP 1 1 FROM tblGLDataFixLog WHERE strDescription= 'Remove orphaned fiscal period.')
-BEGIN
-	DELETE FROM tblGLFiscalYearPeriod WHERE intFiscalYearId NOT IN(SELECT intFiscalYearId FROM tblGLFiscalYear)
-	INSERT INTO tblGLDataFixLog(dtmDate, strDescription) VALUES(GETDATE(), 'Remove orphaned fiscal period.')
+    EXECUTE(
+    'IF NOT EXISTS(SELECT TOP 1 1 FROM tblGLDataFixLog WHERE strDescription= ''Update fiscal period name.'')
+    BEGIN
+        UPDATE tblGLFiscalYearPeriod SET strPeriod = DATENAME(MONTH, dtmStartDate) + '' '' + DATENAME(YEAR, dtmStartDate)
+        INSERT INTO tblGLDataFixLog(dtmDate, strDescription) VALUES(GETDATE(), ''Update fiscal period name.'')
+    END
+    IF NOT EXISTS(SELECT TOP 1 1 FROM tblGLDataFixLog WHERE strDescription= ''Remove orphaned fiscal period.'')
+    BEGIN
+    	DELETE FROM tblGLFiscalYearPeriod WHERE intFiscalYearId NOT IN(SELECT intFiscalYearId FROM tblGLFiscalYear)
+	    INSERT INTO tblGLDataFixLog(dtmDate, strDescription) VALUES(GETDATE(), ''Remove orphaned fiscal period.'')
+    END'
+    )
 END
