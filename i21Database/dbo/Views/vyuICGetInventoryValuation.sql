23734056b24cbcf02d2eb41782c64cc255d97bfe
Feb Montefrio <feb.montefrio@irely.com>
2021-11-23 18:56:15 +0800

    [B] IC-9931
    # Conflicts:
    #	i21Database/dbo/Stored Procedures/uspICLogRiskPositionFromOnhand.sql

--- i21Database/dbo/Views/vyuICGetInventoryValuation.sql
+++ i21Database/dbo/Views/vyuICGetInventoryValuation.sql
@@ -58,6 +58,8 @@ SELECT	intInventoryValuationKeyId  = ISNULL(t.intInventoryTransactionId, 0)
 		,t.intTicketId				
 		,strTransactionUOM			= transactionUnitMeasure.strUnitMeasure 
 		,strDescription				= t.strDescription 
+		,intItemStockUOM			= stockUOM.intItemUOMId 
+		,intUnitMeasureStockUOM		= stockUnitMeasure.intUnitMeasureId
 FROM 	tblICInventoryTransaction t 
 		INNER JOIN tblICItem i 
 			ON t.intItemId = i.intItemId
