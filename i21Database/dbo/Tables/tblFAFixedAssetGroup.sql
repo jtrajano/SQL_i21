eee664dc1303f7fb0cf9bdfe70557c3659979235
Mark Daniel Marmeto <mark.marmeto@irely.com>
2021-11-23 12:24:34 +0800

    FA-285

--- i21Database/dbo/Tables/tblFAFixedAssetGroup.sql
+++ i21Database/dbo/Tables/tblFAFixedAssetGroup.sql
@@ -2,6 +2,8 @@ CREATE TABLE [dbo].[tblFAFixedAssetGroup] (
     [intAssetGroupId]			INT IDENTITY (1, 1) NOT NULL,
 	[strGroupCode]	            NVARCHAR (20) COLLATE Latin1_General_CI_AS NOT NULL,
 	[strGroupDescription]		NVARCHAR (100) COLLATE Latin1_General_CI_AS NOT NULL,
+    [intDepreciationMethodId]   INT NULL,
     [intConcurrencyId]          INT DEFAULT 1 NOT NULL,
-    CONSTRAINT [PK_tblFAFixedAssetGroup] PRIMARY KEY CLUSTERED ([intAssetGroupId] ASC)
+    CONSTRAINT [PK_tblFAFixedAssetGroup] PRIMARY KEY CLUSTERED ([intAssetGroupId] ASC),
+    CONSTRAINT [FK_tblFAFixedAssetGroup_tblFADepreciationMethod] FOREIGN KEY([intDepreciationMethodId]) REFERENCES [dbo].[tblFADepreciationMethod]([intDepreciationMethodId])
 );
