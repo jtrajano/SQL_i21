7d019e5105ea965762ae3b3c05bba90244c58305
IRELY\JMendones <jesmar.mendones@irely.com>
2021-11-24 13:57:32 +0800

    SCH-6
    (cherry picked from commit 0c7d447e364bc1dd2e9634924dc670be8e978174)

---
+++ i21PreDeployment/Scripts/Pre-Deployment/27_SCH/1_FixOrphanDistribution.sql
@@ -0,0 +1,21 @@
+﻿
+--=====================================================================================================================================
+-- 	UPDATE FIELDS OF REPORT CRITERIA TABLES
+---------------------------------------------------------------------------------------------------------------------------------------
+
+IF EXISTS (SELECT TOP 1 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[tblSCHReportDistribution]') AND type in (N'U'))
+BEGIN
+	IF EXISTS (SELECT TOP 1 1 FROM tblSCHSchedule)
+	BEGIN
+		UPDATE tblSCHReportDistribution 
+		SET intScheduleId = (SELECT TOP 1 intScheduleId FROM tblSCHSchedule)
+		WHERE intScheduleId NOT IN (SELECT intScheduleId FROM tblSCHSchedule)
+	END
+	ELSE
+	BEGIN
+		DELETE FROM tblSCHReportDistribution WHERE intScheduleId NOT IN (SELECT intScheduleId FROM tblSCHSchedule)
+	END
+END
+GO
+
+
