8409fe3b43b2f489e33d04280132984f063ba23b
Wendell Wayne Estrada <wendell.estrada@irely.com>
2021-11-24 19:54:31 +0800

    API-264 Enhancements to Adjustment endpoint

--- i21Database/dbo/Tables/tblApiRESTErrorLog.sql
+++ i21Database/dbo/Tables/tblApiRESTErrorLog.sql
@@ -2,11 +2,11 @@ CREATE TABLE tblApiRESTErrorLog
 (
     intId INT IDENTITY(1, 1) PRIMARY KEY,
     guiApiUniqueId UNIQUEIDENTIFIER,
-    strError NVARCHAR(500),
-    strField NVARCHAR(100),
-    strValue NVARCHAR(500),
+    strError NVARCHAR(500) COLLATE Latin1_General_CI_AS,
+    strField NVARCHAR(100) COLLATE Latin1_General_CI_AS,
+    strValue NVARCHAR(500) COLLATE Latin1_General_CI_AS,
     intLineNumber INT NULL,
     dblTotalAmount NUMERIC(18, 6),
     intLinePosition INT NULL,
-    strLogLevel NVARCHAR(50)
+    strLogLevel NVARCHAR(50) COLLATE Latin1_General_CI_AS
 )
