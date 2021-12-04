004d5e1d43e2af0f1fb2c8a32c525339f1ba0c86
JAYSON\Administrator <jayson.ramos@iRely.com>
2021-12-04 17:12:45 +0800

    CT-6518
    (cherry picked from commit dac76377401af21273b2a4f7fb4391fa42de14b8)

--- i21Database/dbo/Stored Procedures/uspCTLogSummary.sql
+++ i21Database/dbo/Stored Procedures/uspCTLogSummary.sql
@@ -4536,6 +4536,13 @@ BEGIN TRY
 
 						END 
 
+						-- Increase SBD upon creation of IR
+						IF (@strTransactionReference = 'Inventory Receipt')
+						BEGIN
+							UPDATE @cbLogSpecific SET dblQty = @_dblActual, intPricingTypeId = 1, strTransactionType = 'Purchase Basis Deliveries'
+							EXEC uspCTLogContractBalance @cbLogSpecific, 0 
+						END
+
 					END				
 					ELSE IF ISNULL(@dblBasis, 0) > 0
 					BEGIN
