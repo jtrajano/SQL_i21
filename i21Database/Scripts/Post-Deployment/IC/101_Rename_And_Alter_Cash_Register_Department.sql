6e28e3a5a842b7e3264de77acf8931f088ed328a
Samuel Marvin Aguilos <samuel.aguilos@irely.com>
2021-12-07 00:24:53 +0800

    IC-9935

--- i21Database/Scripts/Post-Deployment/IC/101_Rename_And_Alter_Cash_Register_Department.sql
+++ i21Database/Scripts/Post-Deployment/IC/101_Rename_And_Alter_Cash_Register_Department.sql
@@ -1,19 +1,13 @@
 PRINT 'Checking tblICCategoryLocation for intRegisterDepartmentId'
-GO
 IF(EXISTS(SELECT TOP 1 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE [TABLE_NAME] = 'tblICCategoryLocation'  and [COLUMN_NAME] = 'intRegisterDepartmentId' ))
 BEGIN
 	PRINT 'EXECUTE'
-	GO
 	
 	UPDATE tblICCategoryLocation SET strCashRegisterDepartment = CAST(intRegisterDepartmentId AS NVARCHAR)
-	GO
 
 	-- EXEC('
 	-- 	ALTER TABLE tblICCategoryLocation
 	-- 	DROP COLUMN intRegisterDepartmentId
 	-- ')
-	-- GO
 END
-GO
 PRINT 'Done checking tblICCategoryLocation for intRegisterDepartmentId'
-GO
