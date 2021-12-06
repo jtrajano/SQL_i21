1d34cf5f1d2310682a56c97cfc72ecfa3bca459b
Mark Daniel Marmeto <mark.marmeto@irely.com>
2021-12-06 13:14:26 +0800

    GL-8513

--- i21Database/Scripts/Post-Deployment/CM/15_InsertDefaultBankAccountType.sql
+++ i21Database/Scripts/Post-Deployment/CM/15_InsertDefaultBankAccountType.sql
@@ -1,13 +1,4 @@
 ﻿GO
-PRINT 'Start updating zero-valued CM Bank Account Types to 1'
-UPDATE tblCMBankAccount
-SET
-	intBankAccountTypeId = 1
-WHERE intBankAccountTypeId = 0
-GO
-PRINT 'Finished updating zero-valued CM Bank Account Types to 1'
-
-GO
 PRINT 'Start generating default CM Bank Account Types'
 GO
 	
