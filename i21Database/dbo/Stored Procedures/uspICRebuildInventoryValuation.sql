7881288cbac039af36bd5e9ee963c5e69491de7a
Feb Montefrio <feb.montefrio@irely.com>
2021-12-13 10:20:17 +0800

    [B] IC-9958

--- i21Database/dbo/Stored Procedures/uspICRebuildInventoryValuation.sql
+++ i21Database/dbo/Stored Procedures/uspICRebuildInventoryValuation.sql
@@ -1050,6 +1050,7 @@ BEGIN
 							20	Inventory Adjustment - Lot Move	Inventory Adjustment
 						*/
 						WHEN t.intTransactionTypeId IN (12, 13, 14, 15, 17, 19, 20) THEN 4 
+						WHEN dblQty > 0 AND t.strTransactionForm IN ('Produce') THEN 4
 						WHEN dblQty > 0 AND t.strTransactionForm NOT IN ('Invoice', 'Inventory Shipment', 'Inventory Count', 'Credit Memo') THEN 3 
 						WHEN dblQty < 0 AND t.strTransactionForm = 'Inventory Shipment' THEN 5
 						WHEN dblQty > 0 AND t.strTransactionForm = 'Inventory Shipment' THEN 6
@@ -1125,6 +1126,7 @@ BEGIN
 					20	Inventory Adjustment - Lot Move	Inventory Adjustment
 				*/
 				WHEN t.intTransactionTypeId IN (12, 13, 14, 15, 17, 19, 20) THEN 4 
+				WHEN dblQty > 0 AND t.strTransactionForm IN ('Produce') THEN 4
 				WHEN dblQty > 0 AND t.strTransactionForm NOT IN ('Invoice','Inventory Shipment','Inventory Count','Credit Memo') THEN 3 
 				WHEN dblQty < 0 AND t.strTransactionForm = 'Inventory Shipment' THEN 5
 				WHEN dblQty > 0 AND t.strTransactionForm = 'Inventory Shipment' THEN 6
