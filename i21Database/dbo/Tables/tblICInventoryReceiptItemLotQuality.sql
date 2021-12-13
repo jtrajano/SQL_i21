6e341d799f41ef120656b0822f2b6bc2a6dfe8e7
Samuel Marvin Aguilos <samuel.aguilos@irely.com>
2021-12-13 21:06:07 +0800

    IC-9944

---
+++ i21Database/dbo/Tables/tblICInventoryReceiptItemLotQuality.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE [dbo].[tblICInventoryReceiptItemLotQuality]
+(
+	[intInventoryReceiptItemLotQualityId] INT NOT NULL IDENTITY, 
+	[intInventoryReceiptItemLotId] INT NOT NULL,
+	[intComponentMapId] INT NULL,
+	[strValue] NVARCHAR(200) NULL,
+	[intConcurrencyId] INT NULL DEFAULT ((0)),
+	[dtmDateCreated] DATETIME NULL,
+	[dtmDateModified] DATETIME NULL,
+	[intCreatedByUserId] INT NULL,
+	[intModifiedByUserId] INT NULL, 		
+	CONSTRAINT [PK_tblICInventoryReceiptItemLotQuality] PRIMARY KEY ([intInventoryReceiptItemLotQualityId]), 
+	CONSTRAINT [FK_tblICInventoryReceiptItemLotQuality_tblICInventoryReceiptItemLot] FOREIGN KEY ([intInventoryReceiptItemLotId]) REFERENCES [tblICInventoryReceiptItemLot]([intInventoryReceiptItemLotId]) ON DELETE CASCADE,
+	CONSTRAINT [FK_tblICInventoryReceiptItemLotQuality_tblQMComponentMap] FOREIGN KEY ([intComponentMapId]) REFERENCES tblQMComponentMap([intComponentMapId]), 
+)
+GO
+
+CREATE NONCLUSTERED INDEX [IX_tblICInventoryReceiptItemLotQuality_intInventoryReceiptItemLotId]
+	ON [dbo].[tblICInventoryReceiptItemLotQuality]([intInventoryReceiptItemLotId] ASC)
+
+GO
