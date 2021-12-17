bfa95b1ea608490aaf76fc1bb6378b9954708695
kelvin_quinto <kelvin.quinto@irely.com>
2021-12-17 12:35:13 +0800

    Revert "AR-14101"
    This reverts commit 50650e90d651136c94e356822020eca910b676c1.

--- i21Database/dbo/Stored Procedures/uspARInitializeTempTableForAging.sql
+++ i21Database/dbo/Stored Procedures/uspARInitializeTempTableForAging.sql
@@ -109,7 +109,7 @@ CREATE TABLE ##GLACCOUNTS (
 	, strAccountCategory			NVARCHAR (100)   COLLATE Latin1_General_CI_AS	NULL
 )
 CREATE TABLE ##INVOICETOTALPREPAYMENTS (
-	  intInvoiceId					INT												NULL
+	  intInvoiceId					INT												NOT NULL PRIMARY KEY
 	, dblPayment					NUMERIC(18, 6)									NULL DEFAULT 0
 )
 CREATE TABLE ##CASHREFUNDS (
