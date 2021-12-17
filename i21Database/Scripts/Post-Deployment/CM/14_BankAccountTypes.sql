94c90c01d52b8b18e4ff36fc4c0755671b2f3e2d
Mark Daniel Marmeto <mark.marmeto@irely.com>
2021-12-17 15:34:34 +0800

    GL-8589

--- i21Database/Scripts/Post-Deployment/CM/14_BankAccountTypes.sql
+++ i21Database/Scripts/Post-Deployment/CM/14_BankAccountTypes.sql
@@ -1,15 +1,22 @@
 ﻿GO
 PRINT 'Start generating default Bank Account Types'
-
 GO
+
 SET  IDENTITY_INSERT tblCMBankAccountType ON
 	MERGE 
 	INTO	dbo.tblCMBankAccountType
 	WITH	(HOLDLOCK) 
 	AS		BankAccountTypeTable
 	USING	(
-			SELECT id = 1,  bankAccountType = 'Bank' UNION ALL 
-			SELECT id = 2,  bankAccountType = 'Brokerage'
+			SELECT id = 1,  bankAccountType = 'Payroll' UNION ALL 
+			SELECT id = 2,  bankAccountType = 'Bank' UNION ALL 
+			SELECT id = 3,  bankAccountType = 'Brokerage' UNION ALL 
+			SELECT id = 4,  bankAccountType = 'Operating (AR/AP)' UNION ALL 
+			SELECT id = 5,  bankAccountType = 'Investing' UNION ALL 
+			SELECT id = 6,  bankAccountType = 'Disbursement (AP Only)' UNION ALL 
+			SELECT id = 7,  bankAccountType = 'Collection (AR Only)' UNION ALL 
+			SELECT id = 8,  bankAccountType = 'Sweep' UNION ALL 
+			SELECT id = 9,  bankAccountType = 'Loan'
 	) AS BankAccountTypeHardCodedValues
 		ON  BankAccountTypeTable.intBankAccountTypeId = BankAccountTypeHardCodedValues.id
 	WHEN MATCHED THEN 
@@ -28,19 +35,27 @@ SET  IDENTITY_INSERT tblCMBankAccountType ON
 			,1
 		);
 	SET  IDENTITY_INSERT tblCMBankAccountType OFF
-	
 GO
+
 PRINT 'Finished generating default Bank Account Types'
-
 GO
+
 PRINT 'Start updating old zero-valued bank account type'
-
 GO
+
 UPDATE tblCMBankAccount
 SET
-	intBankAccountTypeId = 1
+	intBankAccountTypeId = 3
 WHERE
-	intBankAccountTypeId = 0 OR intBankAccountTypeId IS NULL
-
+	intBankAccountTypeId = 2 OR intBankAccountTypeId IS NULL
 GO
+
+UPDATE tblCMBankAccount
+SET
+	intBankAccountTypeId = 2
+WHERE
+	intBankAccountTypeId IN (0, 1) OR intBankAccountTypeId IS NULL
+GO
+
+
 PRINT 'Finish updating old zero-valued bank account type'
