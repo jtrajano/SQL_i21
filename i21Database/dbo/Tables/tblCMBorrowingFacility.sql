893acfdded7460b1709aea3c9e9b2e9e5eaf5fe8
Jeff Trajano <jeffrey.trajano@irely.com>
2021-12-20 20:24:07 +0800

    GL-8588

--- i21Database/dbo/Tables/tblCMBorrowingFacility.sql
+++ i21Database/dbo/Tables/tblCMBorrowingFacility.sql
@@ -3,11 +3,11 @@ CREATE TABLE tblCMBorrowingFacility(
   strBorrowingFacilityId              NVARCHAR(20)    COLLATE Latin1_General_CI_AS NOT NULL,
   intBankId                           INT             NOT NULL,
   strBankReferenceNo                  NVARCHAR(30)    COLLATE Latin1_General_CI_AS NOT NULL,
-  intBankValuationRuleId              INT             NOT NULL,
   intPositionCurrencyId               INT             NOT NULL,
   dblGlobalLineCredit                 DECIMAL (18, 2) NOT NULL,
   dtmExpiration                       DATETIME        NOT NULL,
   strComment                          NVARCHAR(1000)  COLLATE Latin1_General_CI_AS NULL,
+  ysnActive                           BIT             NULL,
   intConcurrencyId                    INT             NOT NULL,
   CONSTRAINT [PK_BorrowingFacilityId] PRIMARY KEY CLUSTERED ([intBorrowingFacilityId] ASC)
   WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) 
