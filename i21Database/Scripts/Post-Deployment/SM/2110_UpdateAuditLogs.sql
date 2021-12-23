bb02701732c0e991d6e228a47b2485613c756a98
Feb Montefrio <feb.montefrio@irely.com>
2021-12-23 17:18:00 +0800

    [F] IC-9557

--- i21Database/Scripts/Post-Deployment/SM/2110_UpdateAuditLogs.sql
+++ i21Database/Scripts/Post-Deployment/SM/2110_UpdateAuditLogs.sql
@@ -59,3 +59,255 @@ WHERE ISNULL(strTransactionNo, '') = '' AND B.strNamespace = 'Payroll.view.Entit
 print('/*******************  END UPDATING AUDIT LOG FOR 21.1  *******************/')
 
 GO
+
+print('/*******************  BEGIN UPDATING INVENTORY AUDIT LOG FOR 21.2 *******************/')
+GO
+IF EXISTS (SELECT * FROM [tblICCompanyPreference] WHERE ISNULL(ysnUpdateSMTransaction, 0) = 0)
+BEGIN 
+	--1. Update the item 
+	UPDATE tblSMTransaction
+	SET 
+		strTransactionNo = NULL 
+		,strName = i.strItemNo
+		,strDescription = i.strDescription
+		,dtmDate = NULL 
+		,intEntityId = NULL 
+	FROM 
+		tblSMTransaction A INNER JOIN tblSMScreen B 
+			ON A.intScreenId = B.intScreenId
+		LEFT JOIN tblICItem i 
+			ON i.intItemId = A.intRecordId
+	WHERE 
+		B.strNamespace = 'Inventory.view.Item' 
+
+	--2. Update the item location
+	UPDATE tblSMTransaction
+	SET 
+		strTransactionNo = NULL 
+		,strName = i.intItemLocationId 
+		,strDescription = i.strDescription
+		,dtmDate = NULL 
+		,intEntityId = NULL 
+	FROM 
+		tblSMTransaction A INNER JOIN tblSMScreen B 
+			ON A.intScreenId = B.intScreenId
+		LEFT JOIN tblICItemLocation i 
+			ON i.intItemLocationId = A.intRecordId
+	WHERE 
+		B.strNamespace = 'Inventory.view.ItemLocation' 
+
+	--3. Update bundle
+	UPDATE tblSMTransaction
+	SET 
+		strTransactionNo = NULL 
+		,strName = i.strItemNo 
+		,strDescription = i.strDescription
+		,dtmDate = NULL 
+		,intEntityId = NULL 
+	FROM 
+		tblSMTransaction A INNER JOIN tblSMScreen B 
+			ON A.intScreenId = B.intScreenId
+		LEFT JOIN tblICItem i 
+			ON i.intItemId = A.intRecordId
+	WHERE 
+		B.strNamespace = 'Inventory.view.Bundle' 
+
+	--4. Update category
+	UPDATE tblSMTransaction
+	SET 
+		strTransactionNo = NULL 
+		,strName = i.strCategoryCode 
+		,strDescription = i.strDescription
+		,dtmDate = NULL 
+		,intEntityId = NULL 
+	FROM 
+		tblSMTransaction A INNER JOIN tblSMScreen B 
+			ON A.intScreenId = B.intScreenId
+		LEFT JOIN tblICCategory i 
+			ON i.intCategoryId = A.intRecordId
+	WHERE 
+		B.strNamespace = 'Inventory.view.Category' 
+
+	--5. Update commodity
+	UPDATE tblSMTransaction
+	SET 
+		strTransactionNo = NULL 
+		,strName = i.strCommodityCode 
+		,strDescription = i.strDescription
+		,dtmDate = NULL 
+		,intEntityId = NULL 
+	FROM 
+		tblSMTransaction A INNER JOIN tblSMScreen B 
+			ON A.intScreenId = B.intScreenId
+		LEFT JOIN tblICCommodity i 
+			ON i.intCommodityId = A.intRecordId
+	WHERE 
+		B.strNamespace = 'Inventory.view.Commodity' 
+
+	--6. Update fuel type
+	UPDATE tblSMTransaction
+	SET 
+		strTransactionNo = NULL 
+		,strName = i.intFuelTypeId 
+		,strDescription = i.strEquivalenceValue
+		,dtmDate = NULL 
+		,intEntityId = NULL 
+	FROM 
+		tblSMTransaction A INNER JOIN tblSMScreen B 
+			ON A.intScreenId = B.intScreenId
+		LEFT JOIN tblICFuelType i 
+			ON i.intFuelTypeId = A.intRecordId
+	WHERE 
+		B.strNamespace = 'Inventory.view.FuelType' 
+
+	--7. Update Inventory Adjustment 
+	UPDATE tblSMTransaction
+	SET 
+		strTransactionNo = i.strAdjustmentNo
+		,strName = NULL
+		,strDescription = i.strDescription
+		,dtmDate = i.dtmAdjustmentDate
+		,intEntityId = NULL 
+	FROM 
+		tblSMTransaction A INNER JOIN tblSMScreen B 
+			ON A.intScreenId = B.intScreenId
+		LEFT JOIN tblICInventoryAdjustment i 
+			ON i.intInventoryAdjustmentId = A.intRecordId
+	WHERE 
+		B.strNamespace = 'Inventory.view.InventoryAdjustment' 
+
+	--8. Update Inventory Count By Category
+	UPDATE tblSMTransaction
+	SET 
+		strTransactionNo = i.strCountNo
+		,strName = NULL
+		,strDescription = NULL 
+		,dtmDate = i.dtmCountDate
+		,intEntityId = NULL 
+	FROM 
+		tblSMTransaction A INNER JOIN tblSMScreen B 
+			ON A.intScreenId = B.intScreenId
+		LEFT JOIN tblICInventoryCountByCategory i 
+			ON i.intInventoryCountByCategoryId = A.intRecordId
+	WHERE 
+		B.strNamespace = 'Inventory.view.InventoryCountByCategory' 
+
+	--9. Update Inventory Count
+	UPDATE tblSMTransaction
+	SET 
+		strTransactionNo = i.strCountNo
+		,strName = NULL
+		,strDescription = i.strDescription
+		,dtmDate = i.dtmCountDate
+		,intEntityId = NULL 
+	FROM 
+		tblSMTransaction A INNER JOIN tblSMScreen B 
+			ON A.intScreenId = B.intScreenId
+		LEFT JOIN tblICInventoryCount i 
+			ON i.intInventoryCountId = A.intRecordId
+	WHERE 
+		B.strNamespace = 'Inventory.view.InventoryCount' 
+
+	--10. Update Inventory Receipt
+	UPDATE tblSMTransaction
+	SET 
+		strTransactionNo = i.strReceiptNumber
+		,strName = NULL
+		,strDescription = NULL 
+		,dtmDate = i.dtmReceiptDate
+		,intEntityId = i.intEntityVendorId
+	FROM 
+		tblSMTransaction A INNER JOIN tblSMScreen B 
+			ON A.intScreenId = B.intScreenId
+		LEFT JOIN tblICInventoryReceipt i 
+			ON i.intInventoryReceiptId = A.intRecordId
+	WHERE
+		B.strNamespace = 'Inventory.view.InventoryReceipt' 
+
+	--11. Update Inventory Shipment
+	UPDATE tblSMTransaction
+	SET 
+		strTransactionNo = i.strShipmentNumber
+		,strName = NULL
+		,strDescription = NULL 
+		,dtmDate = i.dtmShipDate
+		,intEntityId = i.intEntityCustomerId
+	FROM 
+		tblSMTransaction A INNER JOIN tblSMScreen B 
+			ON A.intScreenId = B.intScreenId
+		LEFT JOIN tblICInventoryShipment i 
+			ON i.intInventoryShipmentId = A.intRecordId
+	WHERE
+		B.strNamespace = 'Inventory.view.InventoryShipment' 
+
+	--12. Update Transfer
+	UPDATE tblSMTransaction
+	SET 
+		strTransactionNo = i.strTransferNo
+		,strName = NULL
+		,strDescription = i.strDescription 
+		,dtmDate = i.dtmTransferDate
+		,intEntityId = NULL 
+	FROM 
+		tblSMTransaction A INNER JOIN tblSMScreen B 
+			ON A.intScreenId = B.intScreenId
+		LEFT JOIN tblICInventoryTransfer i 
+			ON i.intInventoryTransferId = A.intRecordId
+	WHERE
+		B.strNamespace = 'Inventory.view.InventoryTransfer' 
+
+	--13. Update Storage Measurement Reading
+	UPDATE tblSMTransaction
+	SET 
+		strTransactionNo = i.strReadingNo
+		,strName = NULL
+		,strDescription = i.strDescription 
+		,dtmDate = i.dtmDate
+		,intEntityId = NULL 
+	FROM 
+		tblSMTransaction A INNER JOIN tblSMScreen B 
+			ON A.intScreenId = B.intScreenId
+		LEFT JOIN tblICStorageMeasurementReading i 
+			ON i.intStorageMeasurementReadingId = A.intRecordId
+	WHERE
+		B.strNamespace = 'Inventory.view.StorageMeasurementReading' 
+
+	--14. Update Storage Unit
+	UPDATE tblSMTransaction
+	SET 
+		strTransactionNo = NULL 
+		,strName = i.strName
+		,strDescription = i.strDescription 
+		,dtmDate = NULL 
+		,intEntityId = NULL 
+	FROM 
+		tblSMTransaction A INNER JOIN tblSMScreen B 
+			ON A.intScreenId = B.intScreenId
+		LEFT JOIN tblICStorageLocation i 
+			ON i.intStorageLocationId = A.intRecordId
+	WHERE
+		B.strNamespace = 'Inventory.view.StorageUnit' 
+
+	--15. Update Unit of Measure
+	UPDATE tblSMTransaction
+	SET 
+		strTransactionNo = NULL 
+		,strName = i.strUnitMeasure
+		,strDescription = i.strUnitType
+		,dtmDate = NULL 
+		,intEntityId = NULL 
+	FROM 
+		tblSMTransaction A INNER JOIN tblSMScreen B 
+			ON A.intScreenId = B.intScreenId
+		LEFT JOIN tblICUnitMeasure i 
+			ON i.intUnitMeasureId = A.intRecordId
+	WHERE
+		B.strNamespace = 'Inventory.view.InventoryUOM' 
+
+
+	UPDATE [tblICCompanyPreference] SET ysnUpdateSMTransaction = 1 
+END 
+GO
+
+print('/*******************  END UPDATING INVENTORY AUDIT LOG FOR 21.2 *******************/')
+GO
