35b0ba3d695be45d3843955ae3a76e58a27547e0
Samuel Marvin Aguilos <samuel.aguilos@irely.com>
2022-01-13 15:56:31 +0800

    IC-9937
    # Conflicts:
    #	i21PreDeployment/Scripts/Pre-Deployment/PreDeployment.sql
    #	i21PreDeployment/i21PreDeployment.sqlproj

--- i21PreDeployment/Scripts/Pre-Deployment/PreDeployment.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/PreDeployment.sql
@@ -150,6 +150,8 @@
 :r .\11_IC\FixFKItemPricingLevel.sql 
 :r .\11_IC\ImproveValuationReport.sql
 :r .\11_IC\DropIndexes.sql
+:r .\11_IC\AddEffectiveItemPriceUOMColumn.sql 
+:r .\11_IC\UpdateEDIPricebookWithCaseRetailPrice.sql
 
 -- EM
 :r .\12_EM\01_EntitySchemaUpdate.sql
