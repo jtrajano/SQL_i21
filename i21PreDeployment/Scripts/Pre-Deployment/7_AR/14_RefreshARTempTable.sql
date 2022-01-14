c92e999418781723ae8bc351f5698fd20f39cd6d
Roudinee Panteria <roudinee.panteria@irely.com>
2022-01-14 11:18:32 +0800

    AR-14182

--- i21PreDeployment/Scripts/Pre-Deployment/7_AR/14_RefreshARTempTable.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/7_AR/14_RefreshARTempTable.sql
@@ -1,52 +1,10 @@
-﻿PRINT '********************** BEGIN - DROP AR TEMPORARY TABLES **********************'
+﻿PRINT '********************** BEGIN - Refresh AR TEMPORARY TABLES **********************'
 GO
 
 
-IF(OBJECT_ID('tempdb..##ARPostInvoiceHeader') IS NOT NULL)
-BEGIN
-    DROP TABLE ##ARPostInvoiceHeader
-END
-
-IF(OBJECT_ID('tempdb..##ARPostInvoiceDetail') IS NOT NULL)
-BEGIN
-    DROP TABLE ##ARPostInvoiceDetail
-END
-
-IF(OBJECT_ID('tempdb..##ARInvoiceItemAccount') IS NOT NULL)
-BEGIN
-    DROP TABLE ##ARInvoiceItemAccount
-END
-
-IF(OBJECT_ID('tempdb..##ARInvalidInvoiceData') IS NOT NULL)
-BEGIN
-    DROP TABLE ##ARInvalidInvoiceData
-END
-
-IF(OBJECT_ID('tempdb..##ARItemsForCosting') IS NOT NULL)
-BEGIN
-    DROP TABLE ##ARItemsForCosting
-END
-
-IF(OBJECT_ID('tempdb..##ARItemsForInTransitCosting') IS NOT NULL)
-BEGIN
-    DROP TABLE ##ARItemsForInTransitCosting
-END
-
-IF(OBJECT_ID('tempdb..##ARItemsForStorageCosting') IS NOT NULL)
-BEGIN
-    DROP TABLE ##ARItemsForStorageCosting
-END
-
-IF(OBJECT_ID('tempdb..##ARItemsForContracts') IS NOT NULL)
-BEGIN
-    DROP TABLE ##ARItemsForContracts
-END
-
-IF(OBJECT_ID('tempdb..##ARInvoiceGLEntries') IS NOT NULL)
-BEGIN
-    DROP TABLE ##ARInvoiceGLEntries
-END
+EXEC [dbo].[uspARInitializeTempTableForPosting]
+EXEC [dbo].[uspARInitializeTempTableForAging]
 
 
-PRINT ' ********************** END - DROP AR TEMPORARY TABLES  **********************'
+PRINT ' ********************** END - Refresh AR TEMPORARY TABLES  **********************'
 GO
