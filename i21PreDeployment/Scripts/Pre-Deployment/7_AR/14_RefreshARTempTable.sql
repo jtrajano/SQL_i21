aea1133dd0634bb5f545fcf0677087e2e8edd8ed
Roudinee Panteria <roudinee.panteria@irely.com>
2022-01-14 12:50:41 +0800

    AR-14182

--- i21PreDeployment/Scripts/Pre-Deployment/7_AR/14_RefreshARTempTable.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/7_AR/14_RefreshARTempTable.sql
@@ -1,10 +1,7 @@
 ﻿PRINT '********************** BEGIN - Refresh AR TEMPORARY TABLES **********************'
 GO
 
-
 EXEC [dbo].[uspARInitializeTempTableForPosting]
-EXEC [dbo].[uspARInitializeTempTableForAging]
-
 
 PRINT ' ********************** END - Refresh AR TEMPORARY TABLES  **********************'
 GO
