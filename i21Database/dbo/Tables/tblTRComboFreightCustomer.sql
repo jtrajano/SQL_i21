ce1b077f2a0be34231e2d5d271dc1203cc990112
Cris <cris.lazaga@irely.com>
2022-01-14 09:29:35 +0800

    TR-1784

---
+++ i21Database/dbo/Tables/tblTRComboFreightCustomer.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[tblTRComboFreightCustomer]
+(
+	[intComboFreightCustomerId] INT NOT NULL IDENTITY,
+	[intCustomerEntityId] INT NULL,
+	[intCustomerLocationId] INT NULL,
+	[dblMinimumUnit] DECIMAL(18, 6) NOT NULL,
+	[strFreightRateType] NVARCHAR(100) COLLATE Latin1_General_CI_AS NOT NULL,
+	[strGallonType]  NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL,
+	[intCategoryId] INT NULL,
+	[dtmEffectiveDateTime] DATETIME NULL,
+	[intConcurrencyId] INT NOT NULL DEFAULT ((1)),
+	CONSTRAINT [PK_tblTRComboFreightCustomer] PRIMARY KEY ([intComboFreightCustomerId]),
+	CONSTRAINT [FK_tblTRComboFreightCustomer_tblARCustomer_intCustomerEntityId] FOREIGN KEY ([intCustomerEntityId]) REFERENCES [dbo].[tblARCustomer] (intEntityId),
+	CONSTRAINT [FK_tblTRComboFreightCustomer_tblEMEntityLocation_intCustomerLocationId] FOREIGN KEY ([intCustomerLocationId]) REFERENCES [dbo].[tblEMEntityLocation] (intEntityLocationId),
+	CONSTRAINT [FK_tblTRComboFreightCustomer_tblICCategory] FOREIGN KEY ([intCategoryId]) REFERENCES [tblICCategory]([intCategoryId]),
+	CONSTRAINT [AK_tblTRComboFreightCustomer_UniqueCombo] UNIQUE ([intCustomerEntityId],[intCustomerLocationId],[strFreightRateType],[strGallonType],[intCategoryId],[dtmEffectiveDateTime])
+)
---
+++ i21Database/dbo/Views/vyuTRComboFreightShipVia.sql
@@ -0,0 +1,17 @@
+﻿CREATE VIEW [dbo].[vyuTRComboFreightShipVia]
+AS
+SELECT FSV.intComboFreightShipViaId,
+	FSV.intShipViaEntityId,
+	E.strName AS strShipViaName,
+	E.strEntityNo AS strShipViaEntityNo,
+	FSV.dblMinimumUnit,
+	FSV.strFreightRateType,
+	FSV.strGallonType,
+	FSV.intCategoryId,
+	CA.strCategoryCode AS strCategoryCode,
+	FSV.dtmEffectiveDateTime,
+	FSV.intConcurrencyId
+FROM tblTRComboFreightShipVia FSV
+LEFT JOIN tblSMShipVia SV ON SV.intEntityId = FSV.intShipViaEntityId
+LEFT JOIN tblEMEntity E ON E.intEntityId = SV.intEntityId
+LEFT JOIN tblICCategory CA ON CA.intCategoryId = FSV.intCategoryId
