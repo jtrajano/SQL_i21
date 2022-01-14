0b23e63b5dc14ed384177c66630772a651fd068a
Roudinee Panteria <roudinee.panteria@irely.com>
2022-01-14 18:56:37 +0800

    AR-14186

--- i21PreDeployment/Scripts/Pre-Deployment/7_AR/14_RefreshARTempTable.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/7_AR/14_RefreshARTempTable.sql
@@ -1,8 +1,15 @@
 ﻿PRINT '********************** BEGIN - Refresh AR TEMPORARY TABLES **********************'
 GO
 
-IF EXISTS(SELECT * FROM sys.objects WHERE [type] = 'P' AND [name] = 'uspARInitializeTempTableForPosting')
-    EXEC [dbo].[uspARInitializeTempTableForPosting]
+IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'uspARInitializeTempTableForPosting')
+BEGIN
+	EXEC [dbo].[uspARInitializeTempTableForPosting]
+END
+
+IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'uspARInitializeTempTableForAging')
+BEGIN
+	EXEC [dbo].[uspARInitializeTempTableForAging]
+END
 
 PRINT ' ********************** END - Refresh AR TEMPORARY TABLES  **********************'
 GO
