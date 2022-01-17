f8c744dd34eb9dffda8faf9c3641ec2ae46eb06e
Mark Daniel Marmeto <mark.marmeto@irely.com>
2022-01-17 17:46:52 +0800

    GL-8578

--- i21Database/dbo/Stored Procedures/uspCMGetBankTransferGLRevalueAccount.sql
+++ i21Database/dbo/Stored Procedures/uspCMGetBankTransferGLRevalueAccount.sql
@@ -108,7 +108,7 @@ BEGIN
 		IF(ISNULL(@ysnHasLocationSegment, 0) = 1)
 		BEGIN
 			BEGIN TRY
-				EXEC dbo.uspGLGetOverrideGLAccount @intCurrentAccountId, @intCurrentUnrealizedId, 3, @intBankTransferTypeId, @intAccountOverridden
+				EXEC dbo.uspGLGetOverrideGLAccount @intCurrentAccountId, @intCurrentUnrealizedId, 3, @intBankTransferTypeId, @intAccountOverridden OUTPUT
 			END TRY
 			BEGIN CATCH
 				SELECT  @strErrorMessage = ERROR_MESSAGE();
@@ -116,13 +116,13 @@ BEGIN
 			END CATCH
 		END
 
-		IF(ISNULL(@ysnHasLocationSegment, 0) = 1)
+		IF(ISNULL(@ysnHasLOBSegment, 0) = 1)
 		BEGIN
 			BEGIN TRY
 				IF(@intAccountOverridden IS NULL)
 					SET @intAccountOverridden = @intCurrentUnrealizedId
 
-				EXEC dbo.uspGLGetOverrideGLAccount @intCurrentAccountId, @intCurrentUnrealizedId, 6, @intBankTransferTypeId, @intAccountOverridden
+				EXEC dbo.uspGLGetOverrideGLAccount @intCurrentAccountId, @intCurrentUnrealizedId, 6, @intBankTransferTypeId, @intAccountOverridden OUTPUT
 			END TRY
 			BEGIN CATCH
 				SELECT  @strErrorMessage = ERROR_MESSAGE();
