9066a7726510b96265bd3427fd83c4945b2c9fc5
Leslie Guevarra <leslie.arat@irely.com>
2022-01-18 17:08:26 +0800

    GRN-2893 (HDTN-268919 - Error When Buying out of Storage)

--- i21Database/dbo/Tables/tblGRSettleVoucherCreateReferenceTable.sql
+++ i21Database/dbo/Tables/tblGRSettleVoucherCreateReferenceTable.sql
@@ -19,5 +19,6 @@
 	,dblCostUnitQty DECIMAL(24, 10) NULL
 	,dblSettleContractUnits DECIMAL(24,10) NULL
 	,ysnDiscountFromGrossWeight BIT NULL
-	,ysnItemInventoryCost BIT NULL
+	,ysnItemInventoryCost BIT NULL,
+	,intTransactionId INT NULL
 )
