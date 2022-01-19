2889a10f59e1810dc15c23cbfb6c4c270699d36c
Samuel Marvin Aguilos <samuel.aguilos@irely.com>
2022-01-19 12:00:17 +0800

    IC-10017

--- i21PreDeployment/Scripts/Pre-Deployment/11_IC/RenameAndAlterCashRegisterDepartment.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/11_IC/RenameAndAlterCashRegisterDepartment.sql
@@ -16,4 +16,33 @@ BEGIN
 			')
 		END
     END
+	IF EXISTS (SELECT TOP 1 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[vyuICCategoryLocation]') AND type in (N'U')) 
+	BEGIN
+		EXEC('DROP VIEW vyuICCategoryLocation')
+
+		EXEC('
+			CREATE VIEW [dbo].[vyuICCategoryLocation]
+			AS 
+
+			SELECT	
+				categoryLocation.* 
+				,companyLocation.strLocationName
+				,companyLocation.intCompanyLocationId
+				,strProductCodeId = defaultProductCode.strRegProdCode
+				,strFamilyId = defaultFamily.strSubcategoryId
+				,strClassId = defaultClass.strSubcategoryId
+				,strGeneralItemNo = Item.strItemNo
+			FROM	
+				tblICCategoryLocation categoryLocation
+				LEFT JOIN tblSMCompanyLocation companyLocation
+					ON categoryLocation.intLocationId = companyLocation.intCompanyLocationId
+				LEFT JOIN tblSTSubcategoryRegProd defaultProductCode
+					ON categoryLocation.intProductCodeId = defaultProductCode.intRegProdId
+				LEFT JOIN tblSTSubcategory defaultFamily
+					ON categoryLocation.intFamilyId = defaultFamily.intSubcategoryId
+				LEFT JOIN tblSTSubcategory defaultClass
+					ON categoryLocation.intClassId = defaultClass.intSubcategoryId
+				LEFT JOIN tblICItem Item ON Item.intItemId = categoryLocation.intGeneralItemId
+		')
+	END
 END
