f0f4935dd0ed118a81bd04982a9743ab30153e51
Bryan-PC <013.bryan@gmail.com>
2022-01-20 09:47:22 +0800

    CT-6042

---
+++ i21Database/dbo/Stored Procedures/uspCTLoadForwardCurrency.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROCEDURE [dbo].[uspCTLoadForwardCurrency]
+	@intContractDetailId	INT
+AS
+	SELECT	 FC.*
+	FROM tblCTContractDetail CD
+	INNER JOIN (
+		SELECT	  intFutOptTransactionHeaderId
+				, intFutOptTransactionId
+				, strInternalTradeNo
+				, strFromCurrency = strBuyCurrency
+				, strToCurrency = strSellCurrency
+				, dtmMaturityDate
+				, dtmTransactionDate = dtmTradeDate
+				, dblBuyAmount
+				, dblSellAmount
+				, strApprovalStatus
+				, intContractDetailId
+				, dblContractRate
+				,intConcurrencyId = 1
+		FROM vyuRKGetOtcForwards
+		WHERE intContractDetailId = @intContractDetailId
+	) FC on FC.intContractDetailId = CD.intContractDetailId
