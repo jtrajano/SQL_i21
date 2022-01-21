deaba052f32c816471fe9dae5598805fdd9474ff
Bryan-PC <013.bryan@gmail.com>
2022-01-21 20:14:47 +0800

    CT-6641

--- i21Database/dbo/Functions/fnCTGetSeqPriceFixationInfo.sql
+++ i21Database/dbo/Functions/fnCTGetSeqPriceFixationInfo.sql
@@ -28,7 +28,8 @@ BEGIN
 			@dblPFQuantityUOMId			NUMERIC(18,6),
 			@ysnSpreadAvailable			BIT = 0,
 			@ysnFixationDetailAvailable	BIT = 0,
-			@ysnMultiPricingDetail		BIT = 0
+			@ysnMultiPricingDetail		BIT = 0,
+			@intMaxFixationDetailId		INT
 
     IF EXISTS(
 		SELECT
@@ -59,11 +60,32 @@ BEGIN
 		WHERE
 			cd.intContractDetailId = @intContractDetailId
 
+		IF EXISTS(
+		
+		SELECT top 1 1
+		FROM
+			tblCTContractDetail cd
+			join tblCTContractHeader ch
+				on ch.intContractHeaderId = cd.intContractHeaderId
+			join tblCTPriceFixation pf
+				on pf.intContractHeaderId = ch.intContractHeaderId
+				and isnull(pf.intContractDetailId,0) = (case when ch.ysnMultiplePriceFixation = 1 then isnull(pf.intContractDetailId,0) else cd.intContractDetailId end)
+			join  tblSMTransaction t on t.intRecordId = pf.intPriceContractId and t.intScreenId = 119 and t.strApprovalStatus in 	('Waiting for Approval', 'Waiting for Submit')
+		WHERE
+			cd.intContractDetailId = @intContractDetailId
+		)BEGIN
+
+			SELECT @intMaxFixationDetailId = MAX(intPriceFixationDetailId) 
+			FROM tblCTPriceFixationDetail
+			WHERE	 intPriceFixationId		=   @intPriceFixationId
+
+		END
+
 		SELECT	 @intPFDCount			=   COUNT(intPriceFixationDetailId) 
 				,@dblQuantityPriceFixed =   SUM(dblQuantity) 
 				,@dblPFQuantityUOMId	=   MAX(intQtyItemUOMId)   
 		FROM	 tblCTPriceFixationDetail
-		WHERE	 intPriceFixationId		=   @intPriceFixationId
+		WHERE	 intPriceFixationId		=   @intPriceFixationId AND intPriceFixationDetailId NOT IN (ISNULL(@intMaxFixationDetailId,0))
 		GROUP BY intPriceFixationId
 
 		SELECT	@ysnSpreadAvailable = CASE WHEN SA.intSpreadArbitrageId > 0 THEN 1 ELSE 0 END FROM tblCTSpreadArbitrage SA  WHERE SA.intPriceFixationId = @intPriceFixationId	  
