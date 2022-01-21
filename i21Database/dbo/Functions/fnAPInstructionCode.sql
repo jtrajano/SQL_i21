a40f011785ff04b53a7e3f82dc2a055aeeb87e26
Jopet Lambinicio <jopet.lambinicio@irely.com>
2022-01-21 16:40:56 +0800

    AP-10114

---
+++ i21Database/dbo/Functions/fnAPInstructionCode.sql
@@ -0,0 +1,25 @@
+﻿CREATE FUNCTION [dbo].[fnAPInstructionCode]
+(
+	@instructionCode INT
+)
+RETURNS NVARCHAR(10)
+AS
+BEGIN
+	DECLARE @strInstructionCode NVARCHAR(10);
+
+	SELECT @strInstructionCode = CASE WHEN @instructionCode = 2 THEN 'CHQB'
+									  WHEN @instructionCode = 3 THEN 'CMSW'
+									  WHEN @instructionCode = 4 THEN 'CMTO'
+									  WHEN @instructionCode = 5 THEN 'CMZB'
+									  WHEN @instructionCode = 6 THEN 'CORT'
+									  WHEN @instructionCode = 7 THEN 'EQUI'
+									  WHEN @instructionCode = 8 THEN 'INTC'
+									  WHEN @instructionCode = 9 THEN 'NETS'
+									  WHEN @instructionCode = 10 THEN 'OTHR'
+									  WHEN @instructionCode = 11 THEN 'PHON'
+									  WHEN @instructionCode = 12 THEN 'REPA'
+									  WHEN @instructionCode = 13 THEN 'RTGS'
+									  ELSE 'URGP' END
+
+	RETURN @strInstructionCode
+END
