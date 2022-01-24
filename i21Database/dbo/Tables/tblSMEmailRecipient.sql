85492a760d068ebb609f023c16c1184458eae3a0
IRELY\JMendones <jesmar.mendones@irely.com>
2022-01-24 18:19:48 +0800

    Revert "SM-5325"
    This reverts commit a34255d932392497e16c7dbd113b75382bbe36fe.

--- i21Database/dbo/Tables/tblSMEmailRecipient.sql
+++ i21Database/dbo/Tables/tblSMEmailRecipient.sql
@@ -5,14 +5,7 @@
     [intEntityContactId] INT NULL, 
     [strEmailAddress] NVARCHAR(150) COLLATE Latin1_General_CI_AS NOT NULL, 
     [strRecipientType] NVARCHAR(5) COLLATE Latin1_General_CI_AS NOT NULL, 
-	[intConcurrencyId] INT NOT NULL DEFAULT 1	
+	[intConcurrencyId] INT NOT NULL DEFAULT 1,
+
+	CONSTRAINT [FK_tblSMEmailRecipien_tblSMActivity] FOREIGN KEY ([intEmailId]) REFERENCES [tblSMActivity]([intActivityId]) ON DELETE CASCADE
 )
-
-GO
-
-IF NOT EXISTS(SELECT top 1 1 FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE CONSTRAINT_NAME = 'FK_tblSMEmailRecipien_tblSMActivity')
-BEGIN
-ALTER TABLE [tblSMEmailRecipient]	WITH NOCHECK ADD CONSTRAINT [FK_tblSMEmailRecipien_tblSMActivity] FOREIGN KEY ([intEmailId]) REFERENCES [tblSMActivity]([intActivityId]) ON DELETE CASCADE
-END
-
-GO
