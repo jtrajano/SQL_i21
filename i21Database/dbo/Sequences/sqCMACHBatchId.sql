d1c8a09806d779e4d112f3e5eba2d067a67fe363
Jeff Trajano <jeffrey.trajano@irely.com>
2022-01-24 13:58:03 +0800

    GL-8649

---
+++ i21Database/dbo/Sequences/sqCMACHBatchId.sql
@@ -0,0 +1,10 @@
+
+/****** Object:  Sequence [dbo].[ACHBatchId_Seq]    Script Date: 24/01/2022 1:32:20 PM ******/
+CREATE SEQUENCE [dbo].[sqCMACHBatchId] 
+ AS [int]
+ START WITH 1
+ INCREMENT BY 1
+ MINVALUE 1
+ MAXVALUE 999999999
+ CACHE 
+GO
