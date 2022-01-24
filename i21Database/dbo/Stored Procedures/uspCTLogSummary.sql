b40cfa0b0a608988d1414e7037fff486665c7cee
lawrence_zabala <lawrence.zabala@irely.com>
2022-01-24 15:59:48 +0800

    CT-6583
    (cherry picked from commit ea84a2c6788c08e52fefd4d00f7291050d56ff8c)

--- i21Database/dbo/Stored Procedures/uspCTLogSummary.sql
+++ i21Database/dbo/Stored Procedures/uspCTLogSummary.sql
@@ -4847,7 +4847,7 @@ BEGIN TRY
 								IF (ISNULL(@TotalOrigPriced, 0) = 0) OR (@TotalOrigPriced - (@TotalConsumed + @dblQty) <= 0)
 								BEGIN
 									UPDATE @cbLogSpecific SET intPricingTypeId = CASE WHEN @currPricingTypeId = 3 THEN 1
-																						WHEN @intHeaderPricingTypeId = 1 THEN 1
+																						WHEN @intHeaderPricingTypeId IN (1, 3) THEN 1
 																						ELSE 2 END, intActionId = (CASE WHEN intActionId = 46 AND @currPricingTypeId <> 3 THEN 18 ELSE intActionId END)
 								END
 							END
@@ -4863,7 +4863,7 @@ BEGIN TRY
 							IF (ISNULL(@TotalOrigPriced, 0) = 0) OR (@TotalOrigPriced - (@TotalConsumed + @dblQty) <= 0)
 							BEGIN
 								UPDATE @cbLogSpecific SET intPricingTypeId = CASE WHEN @currPricingTypeId = 3 THEN 1 
-																					WHEN @intHeaderPricingTypeId = 1 THEN 1
+																					WHEN @intHeaderPricingTypeId IN (1, 3) THEN 1
 																					ELSE 2 END, intActionId = (CASE WHEN intActionId = 46 AND @currPricingTypeId <> 3 THEN 18 ELSE intActionId END)
 							END
 						END
