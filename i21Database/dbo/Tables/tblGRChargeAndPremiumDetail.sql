b0304a2bc4c06cadea84e435b8f1a46521f3f74a
Christian Joel Martinez <Christian.Martinez@irely.com>
2022-01-25 23:18:54 +0800

    GRN-2898 - Add Rate Type column in charge and premium detail table

--- i21Database/dbo/Tables/tblGRChargeAndPremiumDetail.sql
+++ i21Database/dbo/Tables/tblGRChargeAndPremiumDetail.sql
@@ -7,6 +7,7 @@ CREATE TABLE [dbo].[tblGRChargeAndPremiumDetail]
 	[intInventoryItemId] INT NULL,
 	[intOtherChargeItemId] INT NULL,
 	[dblRate] DECIMAL(38,20) NULL DEFAULT ((0)),
+	[strRateType] NVARCHAR(30) NULL,
 	[dtmDateCreated] DATETIME NULL DEFAULT(GETDATE()),
 	[intConcurrencyId] INT NULL DEFAULT ((1)),
 	CONSTRAINT [PK_tblGRChargeAndPremiumDetail_intChargeAndPremiumDetailId] PRIMARY KEY ([intChargeAndPremiumDetailId]),
