63424640bec1293aa954132b46fe1978eefe9e4f
Erick Dela Cruz <erick.delacruz@irely.com>
2022-01-26 21:20:27 +0800

    AP-10133

---
+++ i21Database/dbo/Views/vyuAPVendorWIthPayables.sql
@@ -0,0 +1,24 @@
+﻿CREATE VIEW [dbo].[vyuAPVendorWIthPayables]
+AS
+
+SELECT DISTINCT intEntityVendorId
+FROM (
+	SELECT G.intEntityVendorId
+	FROM dbo.tblAPBill G 
+	WHERE G.ysnPosted = 1 AND G.ysnPaid = 0
+	UNION 
+	SELECT AA.intEntityCustomerId
+	FROM tblARInvoice AA
+	WHERE 
+		NOT EXISTS( SELECT TOP 1 1 
+					FROM tblAPBillDetail apbilldetail
+					INNER JOIN tblAPBill apbill 
+						ON apbilldetail.intBillId = apbill.intBillId 
+					WHERE apbill.ysnPosted = 1
+						AND intInvoiceId = AA.intInvoiceId
+					)
+		AND AA.ysnPosted = 1
+		AND AA.dblAmountDue != 0
+		AND AA.strTransactionType IN ('Cash Refund','Invoice','Debit Memo', 'Cash')
+		AND AA.strType != 'CT Tran'
+) tmpVendos
