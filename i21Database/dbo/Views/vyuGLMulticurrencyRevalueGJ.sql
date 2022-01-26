ce123770f735b4880dabe0baa7c64cf1fd6c0f3f
Jeff Trajano <jeffrey.trajano@irely.com>
2022-01-26 14:52:47 +0800

    GL-8686

---
+++ i21Database/dbo/Views/vyuGLMulticurrencyRevalueGJ.sql
@@ -0,0 +1,30 @@
+CREATE VIEW vyuGLMulticurrencyRevalueGJ
+AS
+SELECT   strTransactionType		=	GJ.strTransactionType
+			,strTransactionId		=	GJ.strJournalId
+			,strTransactionDate		=	GJ.dtmDate
+			,strTransactionDueDate	=	NULL
+			,strVendorName			=	'' COLLATE Latin1_General_CI_AS 
+			,strCommodity			=	'' COLLATE Latin1_General_CI_AS 
+			,strLineOfBusiness		=	'' COLLATE Latin1_General_CI_AS 
+			,strLocation			=	'' COLLATE Latin1_General_CI_AS 
+			,strTicket				=	'' COLLATE Latin1_General_CI_AS 
+			,strContractNumber		=	'' COLLATE Latin1_General_CI_AS 
+			,strItemId				=	'' COLLATE Latin1_General_CI_AS 
+			,dblQuantity			=	NULL 
+			,dblUnitPrice			=	NULL
+			,dblAmount				=	GJD.dblDebit - GJD.dblCredit
+			,intCurrencyId			=	GJD.intCurrencyId
+			,intForexRateType		=	GJD.intCurrencyExchangeRateTypeId
+			,strForexRateType		=	RT.strCurrencyExchangeRateType
+			,dblForexRate			=	ISNULL(GJD.dblDebitRate,GJD.dblCreditRate)
+			,dblHistoricAmount		=	GJD.dblDebit - GJD.dblCredit
+			,dblNewForexRate		=	0
+			,dblNewAmount			=	0
+			,dblUnrealizedDebitGain =	0
+			,dblUnrealizedCreditGain=	0
+			,dblDebit				=	0
+			,dblCredit				=	0
+FROM tblGLJournal GJ JOIN tblGLJournalDetail GJD ON GJ.intJournalId = GJD.intJournalId
+LEFT JOIN tblSMCurrencyExchangeRateType RT ON RT.intCurrencyExchangeRateTypeId = GJD.intCurrencyExchangeRateTypeId
+WHERE ysnPosted = 1
