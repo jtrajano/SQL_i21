7b5bebcf06cfcf9d5aa4f6e20b913f9b1b78ba63
Erick Dela Cruz <erick.delacruz@irely.com>
2022-01-28 14:16:18 +0800

    AP-10133

--- i21Database/dbo/Views/vyuAPVendorWIthPayables.sql
+++ i21Database/dbo/Views/vyuAPVendorWIthPayables.sql
@@ -9,15 +9,18 @@ FROM (
 	UNION 
 	SELECT AA.intEntityCustomerId
 	FROM tblARInvoice AA
+	LEFT JOIN (tblAPBillDetail A2 INNER JOIN tblAPBill A3 ON A2.intBillId = A3.intBillId AND A3.ysnPosted = 1) ON AA.intInvoiceId = A2.intInvoiceId
 	WHERE 
-		NOT EXISTS( SELECT TOP 1 1 
-					FROM tblAPBillDetail apbilldetail
-					INNER JOIN tblAPBill apbill 
-						ON apbilldetail.intBillId = apbill.intBillId 
-					WHERE apbill.ysnPosted = 1
-						AND intInvoiceId = AA.intInvoiceId
-					)
-		AND AA.ysnPosted = 1
+		-- NOT EXISTS( SELECT TOP 1 1 
+		-- 			FROM tblAPBillDetail apbilldetail
+		-- 			INNER JOIN tblAPBill apbill 
+		-- 				ON apbilldetail.intBillId = apbill.intBillId 
+		-- 			WHERE apbill.ysnPosted = 1
+		-- 				AND intInvoiceId = AA.intInvoiceId
+		-- 			)
+		-- AND 
+			AA.ysnPosted = 1
+		AND A2.intBillId IS NULL
 		AND AA.dblAmountDue != 0
 		AND AA.strTransactionType IN ('Cash Refund','Invoice','Debit Memo', 'Cash')
 		AND AA.strType != 'CT Tran'
