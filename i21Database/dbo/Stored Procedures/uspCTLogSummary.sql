58465efe6190ab8ec4b342955dd6caf5fd22da39
lawrence_zabala <lawrence.zabala@irely.com>
2022-01-28 10:09:49 +0800

    CT-6583
    (cherry picked from commit fb5c40ccc36114e767042998f25bc8932ca20324)
    (cherry picked from commit b63f195f5ebc85a1f3255a62c1147c2e0d1a6360)

--- i21Database/dbo/Stored Procedures/uspCTLogSummary.sql
+++ i21Database/dbo/Stored Procedures/uspCTLogSummary.sql
@@ -4833,9 +4833,12 @@ BEGIN TRY
 
 								IF (ISNULL(@TotalOrigPriced, 0) = 0) OR (@TotalOrigPriced - (@TotalConsumed + @dblQty) <= 0)
 								BEGIN
-									UPDATE @cbLogSpecific SET intPricingTypeId = CASE WHEN @currPricingTypeId = 3 THEN 1
-																						WHEN @intHeaderPricingTypeId IN (1, 3) THEN 1
-																						ELSE 2 END, intActionId = (CASE WHEN intActionId = 46 AND @currPricingTypeId <> 3 THEN 18 ELSE intActionId END)
+									UPDATE @cbLogSpecific
+									SET intPricingTypeId = CASE WHEN @currPricingTypeId = 3 THEN 1
+																WHEN @intHeaderPricingTypeId IN (1, 3) THEN 1
+																ELSE 2 END
+										, intActionId = CASE WHEN @currPricingTypeId = 3 OR @intHeaderPricingTypeId IN (1, 3) THEN 46
+															ELSE intActionId END
 								END
 							END
 							ELSE
@@ -4849,9 +4852,12 @@ BEGIN TRY
 
 							IF (ISNULL(@TotalOrigPriced, 0) = 0) OR (@TotalOrigPriced - (@TotalConsumed + @dblQty) <= 0)
 							BEGIN
-								UPDATE @cbLogSpecific SET intPricingTypeId = CASE WHEN @currPricingTypeId = 3 THEN 1 
-																					WHEN @intHeaderPricingTypeId IN (1, 3) THEN 1
-																					ELSE 2 END, intActionId = (CASE WHEN intActionId = 46 AND @currPricingTypeId <> 3 THEN 18 ELSE intActionId END)
+								UPDATE @cbLogSpecific
+								SET intPricingTypeId = CASE WHEN @currPricingTypeId = 3 THEN 1
+															WHEN @intHeaderPricingTypeId IN (1, 3) THEN 1
+															ELSE 2 END
+									, intActionId = CASE WHEN @currPricingTypeId = 3 OR @intHeaderPricingTypeId IN (1, 3) THEN 46
+															ELSE intActionId END
 							END
 						END
 					END
