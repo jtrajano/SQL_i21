d06d6a82e04bed646513e6f7c7fbf4a6fb5ca3cc
Erick Dela Cruz <erick.delacruz@irely.com>
2022-02-01 22:03:42 +0800

    AP-10119

--- i21Database/dbo/Stored Procedures/uspAPUpdateVoucherPayment.sql
+++ i21Database/dbo/Stored Procedures/uspAPUpdateVoucherPayment.sql
@@ -49,6 +49,7 @@ BEGIN TRY
 			WHERE PD2.intPayScheduleId = PS.intId AND P2.ysnNewFlag = 1
 		) paySched
 		WHERE PD.intPaymentId IN (SELECT intId FROM @ids)
+		AND PD.dblPayment != 0
 
 		UPDATE tblAPBill 
 		SET
@@ -100,6 +101,7 @@ BEGIN TRY
 			WHERE APD.intBillId = B.intBillId AND APD.ysnApplied = 1
 		) appliedPrepays
 		WHERE P.intPaymentId IN (SELECT intId FROM @ids) AND (B.ysnPrepayHasPayment = 0 OR B.intTransactionType NOT IN (2, 13))
+		AND PD.dblPayment != 0
 	END
 	ELSE IF @post = 0
 	BEGIN
@@ -117,6 +119,7 @@ BEGIN TRY
 			WHERE PD2.intPayScheduleId = PS.intId AND P2.ysnNewFlag = 1 AND P2.intPaymentId <> PD.intPaymentId
 		) paySched
 		WHERE PD.intPaymentId IN (SELECT intId FROM @ids)
+		AND PD.dblPayment != 0
 
 		UPDATE tblAPBill 
 		SET
@@ -168,6 +171,7 @@ BEGIN TRY
 			WHERE APD.intBillId = B.intBillId AND APD.ysnApplied = 1
 		) appliedPrepays
 		WHERE P.intPaymentId IN (SELECT intId FROM @ids) AND (B.ysnPrepayHasPayment = 0 OR B.intTransactionType NOT IN (2, 13))
+		AND PD.dblPayment != 0
 	END
 
 	--SELECT NULLED BILLS
