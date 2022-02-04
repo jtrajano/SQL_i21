669f251ecb9de7ab591296c8687f66b8b26f0c5a
nelsonreyes19 <nelson.reyes@irely.com>
2022-02-04 13:32:05 +0800

    ST-2085

--- i21Database/dbo/Stored Procedures/uspSTGenerateVendorRebate.sql
+++ i21Database/dbo/Stored Procedures/uspSTGenerateVendorRebate.sql
@@ -311,10 +311,10 @@ BEGIN TRY
 			, CASE WHEN DEPT.ysnTobacco = 1   
 					AND TR.strTrlMatchLineTrlMatchName IS NOT NULL 
 					AND TR.strTrlMatchLineTrlPromotionIDPromoType = 'mixAndMatchOffer'
-					AND (TR.dblTrlQty >= 2 OR tblSumQty.dblSumTrlQty >= 2) -- 2 Can Deal
+					AND (TR.dblTrlQty >= 2 OR (SELECT SUM(dblTrlQty) FROM tblSTTranslogRebates where intTermMsgSN = TR.intTermMsgSN and dtmDate = TR.dtmDate and intStoreId = TR.intStoreId and strTrlMatchLineTrlPromotionID = TR.strTrlMatchLineTrlPromotionID GROUP BY intTermMsgSN, dtmDate ,intStoreId , strTrlMatchLineTrlPromotionID) >= 2) -- 2 Can Deal
 				THEN (TR.dblTrlUnitPrice - (TR.dblTrlMatchLineTrlPromoAmount / TR.dblTrlQty)) * TR.dblTrlQty
 					WHEN TR.strTrlMatchLineTrlPromotionIDPromoType IN ('mixAndMatchOffer', 'combinationOffer') 
-					AND (TR.dblTrlQty >= 2	   OR tblSumQty.dblSumTrlQty >= 2) -- 2 Can Deal
+					AND (TR.dblTrlQty >= 2 OR (SELECT SUM(dblTrlQty) FROM tblSTTranslogRebates where intTermMsgSN = TR.intTermMsgSN and dtmDate = TR.dtmDate and intStoreId = TR.intStoreId and strTrlMatchLineTrlPromotionID = TR.strTrlMatchLineTrlPromotionID GROUP BY intTermMsgSN, dtmDate ,intStoreId , strTrlMatchLineTrlPromotionID) >= 2) -- 2 Can Deal
 				THEN (TR.dblTrlUnitPrice - (TR.dblTrlMatchLineTrlPromoAmount / TR.dblTrlQty)) * TR.dblTrlQty
 					WHEN TR.strTrpPaycode IN ('COUPONS')
 				THEN (TR.dblTrlUnitPrice - (TR.dblTrpAmt))
