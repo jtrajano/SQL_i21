ead6581ccca297a6e79a69e941a382fc62458943
Cris <cris.lazaga@irely.com>
2022-02-07 10:04:32 +0800

    TR-1796

--- i21Database/Scripts/Post-Deployment/TR/CleanUp_TR_Data.sql
+++ i21Database/Scripts/Post-Deployment/TR/CleanUp_TR_Data.sql
@@ -7,3 +7,22 @@ SET strRackPriceToUse = 'Vendor'
 WHERE ISNULL(strRackPriceToUse, '') = ''
 
 GO
+
+
+PRINT('Sync Freight-in with Freight-out')
+GO
+
+IF EXISTS(SELECT * FROM  INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'tblSMCleanupLog') 
+BEGIN
+	IF NOT EXISTS(SELECT * FROM  tblSMCleanupLog WHERE strModuleName = 'TR' AND strDesription = 'Sync Freight-In and Freight-Out' AND ysnActive = 1)
+	BEGIN
+		UPDATE tblARCustomerFreightXRef SET dblFreightRateIn = dblFreightRate WHERE dblFreightRate > 0 AND dblFreightRateIn IS NULL
+
+		INSERT INTO tblSMCleanupLog VALUES('TR', 'Sync Freight-In and Freight-Out', GETDATE(), GETUTCDATE(), 1)
+	END
+END
+GO
+
+
+
+
