14524e7675079f512720620e44e22f9c9d134b06
Jopet Lambinicio <jopet.lambinicio@irely.com>
2022-02-09 16:57:05 +0800

    AP-10169

---
+++ i21Database/dbo/Views/vyuAPEntityEFTInformation.sql
@@ -0,0 +1,13 @@
+﻿CREATE VIEW [dbo].[vyuAPEntityEFTInformation]
+AS
+SELECT EFT.intEntityEFTInfoId,
+	   EFT.intEntityId,
+	   EFT.intBankId,
+	   EFT.strBankName COLLATE Latin1_General_CI_AS strBankName,
+	   ISNULL(dbo.fnAESDecryptASym(EFT.strAccountNumber), EFT.strAccountNumber) COLLATE Latin1_General_CI_AS strAccountNumber,
+	   EFT.intCurrencyId,
+	   C.strCurrency COLLATE Latin1_General_CI_AS strCurrency,
+	   ISNULL(EFT.ysnDefaultAccount, 0) ysnDefaultAccount
+FROM tblEMEntityEFTInformation EFT
+LEFT JOIN tblSMCurrency C ON C.intCurrencyID = EFT.intCurrencyId
+WHERE EFT.ysnActive = 1
