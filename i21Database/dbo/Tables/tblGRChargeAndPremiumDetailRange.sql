e7652d35af6adf2f36d3923070b30e3a226ec2c3
Leslie Guevarra <leslie.arat@irely.com>
2022-02-10 16:37:30 +0800

    GRN-2898 - Charge and Premium Detail

--- i21Database/dbo/Tables/tblGRChargeAndPremiumDetailRange.sql
+++ i21Database/dbo/Tables/tblGRChargeAndPremiumDetailRange.sql
@@ -2,12 +2,12 @@ CREATE TABLE [dbo].[tblGRChargeAndPremiumDetailRange]
 (
 	[intChargeAndPremiumDetailRangeId] INT NOT NULL IDENTITY,
 	[intChargeAndPremiumDetailId] INT NOT NULL,
-	[dblFrom] DECIMAL(38,20) NULL DEFAULT ((0)),
-	[dblTo] DECIMAL(38,20) NULL DEFAULT ((0)),
-	[dblRate] DECIMAL(38,20) NULL DEFAULT ((0)),
+	[dblFrom] DECIMAL(18,6) NULL DEFAULT ((0)),
+	[dblTo] DECIMAL(18,6) NULL DEFAULT ((0)),
+	[dblRangeRate] DECIMAL(18,6) NULL DEFAULT ((0)),
 	[intConcurrencyId] INT NULL DEFAULT ((1)),
 	CONSTRAINT [PK_tblGRChargeAndPremiumDetailRange_intChargeAndPremiumDetailRangeId] PRIMARY KEY ([intChargeAndPremiumDetailRangeId]),
-	CONSTRAINT [FK_tblGRChargeAndPremiumDetailRange_intChargeAndPremiumDetailId] FOREIGN KEY ([intChargeAndPremiumDetailId]) REFERENCES [tblGRChargeAndPremiumDetail]([intChargeAndPremiumDetailId])
+	CONSTRAINT [FK_tblGRChargeAndPremiumDetailRange_intChargeAndPremiumDetailId] FOREIGN KEY ([intChargeAndPremiumDetailId]) REFERENCES [tblGRChargeAndPremiumDetail]([intChargeAndPremiumDetailId]) ON DELETE CASCADE
 )
 GO
 
