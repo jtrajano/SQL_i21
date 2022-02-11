f96960c3e06067a0a30b7b66600d940a2f43a60c
Chris Salvante <chris.salvante@irely.com>
2022-02-11 14:04:07 +0800

    SM-5355

--- i21Database/Scripts/Post-Deployment/VersionUpdate.sql
+++ i21Database/Scripts/Post-Deployment/VersionUpdate.sql
@@ -1,6 +1,9 @@
 ﻿GO
 	PRINT N'BEGIN INSERT VERSION UPDATE'
 GO
+	DELETE FROM tblSMBuildNumber
+	WHERE strVersionNo = '' OR strVersionNo = NULL
+
 	INSERT INTO tblSMBuildNumber (strVersionNo, dtmLastUpdate)
 	SELECT '22.1', getdate()
 GO
