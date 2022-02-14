aa793b7e64e6a278e51ae7a435b500962a4343a2
Arman Arenas <arman.arenas@irely.com>
2022-02-14 18:34:48 +0800

    LG-3208 added email distribution check for in-store letter

--- i21Database/dbo/Stored Procedures/uspLGGetEmailInfo.sql
+++ i21Database/dbo/Stored Procedures/uspLGGetEmailInfo.sql
@@ -256,15 +256,28 @@ BEGIN
 		WHERE intEntityId = @intEntityId
 
 
-		SELECT @strIds = STUFF((
-					SELECT DISTINCT '|^|' + LTRIM(intEntityContactId)
-					FROM vyuCTEntityToContact
-					WHERE intEntityId = @intEntityId
-					AND           ISNULL(strEmail,'') <> ''
-					FOR XML PATH('')
-					), 1, 3, '')
-		FROM vyuCTEntityToContact CH
-		WHERE intEntityId = @intEntityId
+		IF EXISTS(SELECT 1 FROM vyuCTEntityToContact WHERE intEntityId = @intEntityId AND LOWER(strEmailDistributionOption) = 'warehouse in-store letter')
+			SELECT @strIds = STUFF((
+						SELECT DISTINCT '|^|' + LTRIM(intEntityContactId)
+						FROM vyuCTEntityToContact
+						WHERE intEntityId = @intEntityId
+						AND LOWER(strEmailDistributionOption) = 'warehouse in-store letter'
+						AND ISNULL(strEmail,'') <> ''
+						FOR XML PATH('')
+						), 1, 3, '')
+			FROM vyuCTEntityToContact CH
+			WHERE intEntityId = @intEntityId
+			AND LOWER(strEmailDistributionOption) = 'warehouse in-store letter'
+		ELSE
+			SELECT @strIds = STUFF((
+						SELECT DISTINCT '|^|' + LTRIM(intEntityContactId)
+						FROM vyuCTEntityToContact
+						WHERE intEntityId = @intEntityId
+						AND ISNULL(strEmail,'') <> ''
+						FOR XML PATH('')
+						), 1, 3, '')
+			FROM vyuCTEntityToContact CH
+			WHERE intEntityId = @intEntityId
 
         SET @Subject = 'Load/Shipment Schedule - ' + CASE WHEN @strInstoreTo = 'Warehouse' THEN 'In Store Letter' ELSE 'Release Order' END + ' - ' + @strLoadNumber
         SET @body += '<!DOCTYPE html>'
