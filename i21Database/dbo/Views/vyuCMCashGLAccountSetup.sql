a212c97bcfd728dee42605bea117623881c137ad
Jeff Trajano <jeffrey.trajano@irely.com>
2022-02-14 08:12:10 +0800

    GL-8727

--- i21Database/dbo/Views/vyuCMCashGLAccountSetup.sql
+++ i21Database/dbo/Views/vyuCMCashGLAccountSetup.sql
@@ -2,8 +2,7 @@
 AS 
 
 SELECT 
-* 
+*
 FROM vyuGLAccountDetail
-WHERE 
-strAccountCategory = 'Cash Account' 
-AND intAccountId NOT IN (select distinct intAccountId from tblGLDetail where intAccountId not in (select intGLAccountId from tblCMBankAccount))
+WHERE
+strAccountCategory = 'Cash Account'
