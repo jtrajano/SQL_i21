71c5f7d83e03f63e3236d6f2551428361edf7061
Christian Joel Martinez <Christian.Martinez@irely.com>
2022-02-17 23:29:19 +0800

    GRN-2898 - Implement calculation date, Charge and Premium override in Settlement, Deduct from vendor calculation

--- i21Database/dbo/Tables/tblGRSettleStorageTicket.sql
+++ i21Database/dbo/Tables/tblGRSettleStorageTicket.sql
@@ -5,9 +5,11 @@
 	[intSettleStorageId] INT NOT NULL,
     [intCustomerStorageId] INT NULL,
 	[dblUnits] DECIMAL(24, 10) NULL,
+	[intChargeAndPremiumId] INT NULL,
 	CONSTRAINT [PK_tblGRSettleStorageTicket_intSettleStorageTicketId] PRIMARY KEY ([intSettleStorageTicketId]),
 	CONSTRAINT [FK_tblGRSettleStorageTicket_tblGRSettleStorage_intSettleStorageId] FOREIGN KEY ([intSettleStorageId]) REFERENCES [dbo].[tblGRSettleStorage] ([intSettleStorageId]) ON DELETE CASCADE,	
 	CONSTRAINT [FK_tblGRSettleStorageTicket_tblGRCustomerStorage_intCustomerStorageId] FOREIGN KEY ([intCustomerStorageId]) REFERENCES [tblGRCustomerStorage]([intCustomerStorageId]),	
+	CONSTRAINT [FK_tblGRSettleStorageTicket_tblGRChargeAndPremiumId_intChargeAndPremiumId] FOREIGN KEY ([intChargeAndPremiumId]) REFERENCES [tblGRChargeAndPremiumId]([intChargeAndPremiumId])
 )
 
 GO
