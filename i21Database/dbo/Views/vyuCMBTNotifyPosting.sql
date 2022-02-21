be448702e5b8455aca0d2eaa6ef633ef5ff02617
Jeff Trajano <jeffrey.trajano@irely.com>
2022-02-21 14:39:09 +0800

    GL-8376

--- i21Database/dbo/Views/vyuCMBTNotifyPosting.sql
+++ i21Database/dbo/Views/vyuCMBTNotifyPosting.sql
@@ -1,11 +1,15 @@
-CREATE VIEW vyuCMBTNotifyPosting  
-AS  
- SELECT BT.intEntityId, BT.intTransactionId, BT.strTransactionId
-FROM tblCMCompanyPreferenceOption o  
-CROSS APPLY(
-SELECT intTransactionId, intEntityId, strTransactionId  from tblCMBankTransfer     
-WHERE ysnPostedInTransit = 1 AND ysnPosted = 0
-and intBankTransferTypeId in(1,2,3)
-AND datediff( DAY,dbo.fnRemoveTimeOnDate(GETDATE()), dtmDate  ) BETWEEN 0 AND isnull(intBTPostReminderDays,0)
-
+CREATE VIEW vyuCMBTNotifyPosting    
+AS    
+SELECT 
+BT.intEntityId, 
+BT.intTransactionId, 
+BT.strTransactionId 
+FROM tblCMCompanyPreferenceOption o    
+CROSS APPLY(  
+    SELECT intTransactionId, intEntityId, strTransactionId  
+    FROM tblCMBankTransfer       
+    WHERE ysnPostedInTransit = 1 AND ysnPosted = 0  
+    and intBankTransferTypeId in(2,3) 
+    AND DATEDIFF( DAY, dbo.fnRemoveTimeOnDate(GETDATE()), dtmInTransit  ) 
+    BETWEEN 0 AND ISNULL(intBTPostReminderDays,0)  
 ) BT
