3f6c670bb8cfe923b16ef4af8102c3b79d30186a
Ed Ibe <ed.ibe@irely.com>
2022-02-22 15:47:11 +0800

    CRM-907

---
+++ i21Database/dbo/Tables/tblCRMIndustrySegment.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[tblCRMIndustrySegment]
+(
+	[intIndustrySegmentId] INT IDENTITY(1,1) NOT NULL,
+	[strIndustrySegment] NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NOT NULL,
+	[intConcurrencyId] [int] NOT NULL DEFAULT 1,
+	CONSTRAINT [PK_tblCRMIndustrySegment] PRIMARY KEY CLUSTERED ([intIndustrySegmentId] ASC),
+	CONSTRAINT [UQ_tblCRMIndustrySegment_strIndustrySegment] UNIQUE ([strIndustrySegment])
+)
---
+++ i21Database/dbo/Tables/tblCRMOpportunityType.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[tblCRMOpportunityType]
+(
+	[intOpportunityTypeId] INT IDENTITY(1,1) NOT NULL,
+	[strOpportunityType] NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NOT NULL,
+	[intConcurrencyId] [int] NOT NULL DEFAULT 1,
+	CONSTRAINT [PK_tblCRMOpportunityType] PRIMARY KEY CLUSTERED ([intOpportunityTypeId] ASC),
+	CONSTRAINT [UQ_tblCRMOpportunityType_strOpportunityType] UNIQUE ([strOpportunityType])
+)
