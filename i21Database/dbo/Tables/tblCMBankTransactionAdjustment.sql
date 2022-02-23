bf20b4446fd7cd4ccc3995ca7f6119114cdb772d
Jeff Trajano <jeffrey.trajano@irely.com>
2022-02-23 11:26:40 +0800

    GL-8723

--- i21Database/dbo/Tables/tblCMBankTransactionAdjustment.sql
+++ i21Database/dbo/Tables/tblCMBankTransactionAdjustment.sql
@@ -3,7 +3,7 @@
 
 CREATE TABLE [dbo].[tblCMBankTransactionAdjustment](
 	[intAdjustmentId] [int] IDENTITY(1,1) NOT NULL,
-	[intBankFeeId] [int] NULL,
+	[intRelatedId] [int] NULL,
 	[intTransactionId] [int] NULL,
     [strType] NVARCHAR(10) COLLATE Latin1_General_CI_AS NOT NULL,
  CONSTRAINT [PK_tblCMBankTransactionAdjustment] PRIMARY KEY CLUSTERED (	[intAdjustmentId] ASC)
