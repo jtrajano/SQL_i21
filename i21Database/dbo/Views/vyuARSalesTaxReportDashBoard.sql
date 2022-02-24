1d5dbe374042ce18ed343be696b78cd7a4aee773
Edsel Roldan Magura <edsel.magura@iRely.com>
2022-02-24 17:26:03 +0800

    AR-14333

---
+++ i21Database/dbo/Views/vyuARSalesTaxReportDashBoard.sql
@@ -0,0 +1,13 @@
+﻿CREATE VIEW [dbo].[vyuARSalesTaxReportDashBoard]
+AS
+SELECT  
+  strInvoiceNumber
+, strItemNo
+, strTaxGroup
+, dtmDate
+, SUM(dblTaxable) dblTaxable
+, SUM(dblNonTaxable) dblNonTaxable
+, SUM(dblTotalSales) dblTotalSales  
+, SUM(dblTax) dblTax
+FROM vyuARSalesTaxReport
+GROUP BY strInvoiceNumber, strItemNo, strTaxGroup, dtmDate
