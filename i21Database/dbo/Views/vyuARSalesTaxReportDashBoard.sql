d96adfbd4bb53019da207a32a0baa5998951e79d
Edsel Roldan Magura <edsel.magura@iRely.com>
2022-02-25 18:50:07 +0800

    AR-14333

--- i21Database/dbo/Views/vyuARSalesTaxReportDashBoard.sql
+++ i21Database/dbo/Views/vyuARSalesTaxReportDashBoard.sql
@@ -2,12 +2,15 @@
 AS
 SELECT  
   strInvoiceNumber
+, COUNT(strInvoiceNumber) intInvoiceCount
 , strItemNo
 , strTaxGroup
 , dtmDate
 , SUM(dblTaxable) dblTaxable
 , SUM(dblNonTaxable) dblNonTaxable
 , SUM(dblTotalSales) dblTotalSales  
-, SUM(dblTax) dblTax
+, SUM(dblTotalTax) dblTotalTax
+, SUM(dblTotalAdjustedTax) dblTotalAdjustedTax
+, dblTotalAdjustment = SUM(dblTotalTax) - SUM(dblTotalAdjustedTax) 
 FROM vyuARSalesTaxReport
 GROUP BY strInvoiceNumber, strItemNo, strTaxGroup, dtmDate
