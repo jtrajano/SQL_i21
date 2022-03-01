0135c879b58152fe7fe28900ffdabacbdb31628b
Jeff Trajano <jeffrey.trajano@irely.com>
2022-03-02 07:57:42 +0800

    GL-8784

--- i21Database/Scripts/Post-Deployment/GL/DefaultData/1h_AccountRange.sql
+++ i21Database/Scripts/Post-Deployment/GL/DefaultData/1h_AccountRange.sql
@@ -1,11 +1,8 @@
 ﻿GO
 	PRINT 'Start generating default account range'
 GO
-	IF NOT EXISTS (SELECT TOP 1 1 FROM tblGLAccountRange)
-	BEGIN
-		DECLARE @result nvarchar(20)
-		EXEC dbo.uspGLGenerateAccountRange @result out
-	END
+	DECLARE @result nvarchar(20)
+	EXEC dbo.uspGLGenerateAccountRange @result out
 GO
 	PRINT 'Fnished generating default account range'
 GO
