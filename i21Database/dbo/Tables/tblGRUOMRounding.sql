af6b43d957080a451ab0c2a0375ec8dd5c47225e
Kenneth Lew Lagunero <kenneth.lagunero@irely.com>
2022-03-02 14:47:59 +0800

    GRN-2915

---
+++ i21Database/dbo/Tables/tblGRUOMRounding.sql
@@ -0,0 +1,27 @@
+﻿CREATE TABLE [dbo].[tblGRUOMRounding](
+	[intUOMRoundingId] [int] IDENTITY(1,1) NOT NULL,
+	[intUnitOfMeasureFromId] [int] NOT NULL,
+	[intUnitOfMeasureToId] [int] NOT NULL,
+	[intDecimalAdjustment] [int] NOT NULL  DEFAULT 0,
+	[intConcurrencyId] [int] NOT NULL  DEFAULT 0
+) ON [PRIMARY]
+GO
+
+CREATE UNIQUE NONCLUSTERED INDEX [tblGRUOMRounding_UniqueKey] ON [dbo].[tblGRUOMRounding]
+(
+	[intUnitOfMeasureFromId] ASC,
+	[intUnitOfMeasureToId] ASC
+)
+GO
+
+CREATE NONCLUSTERED INDEX [tblGRUOMRounding_intUnitOfMeasureToId] ON [dbo].[tblGRUOMRounding]
+(
+	[intUnitOfMeasureToId] ASC
+)
+GO
+
+CREATE NONCLUSTERED INDEX [tblGRUOMRounding_intUnitOfMeasureFromId] ON [dbo].[tblGRUOMRounding]
+(
+	[intUnitOfMeasureFromId] ASC
+)
+GO
