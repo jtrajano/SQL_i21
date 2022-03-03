1fc91aed7f8c51683c6c6e48576091a6b1807c4f
Feb Montefrio <feb.montefrio@irely.com>
2022-03-03 19:51:27 +0800

    [F] IC-9512

---
+++ i21Database/dbo/Views/vyuICGetReceiptTradeFinanceHistory.sql
@@ -0,0 +1,9 @@
+﻿CREATE VIEW vyuICGetReceiptTradeFinanceHistory
+AS
+	SELECT 
+		r.intInventoryReceiptId
+		,tfh.* 
+	FROM 
+		tblICInventoryReceipt r INNER JOIN tblTRFTradeFinanceHistory tfh
+			ON r.strTradeFinanceNumber = tfh.strTradeFinanceNumber
+GO 
