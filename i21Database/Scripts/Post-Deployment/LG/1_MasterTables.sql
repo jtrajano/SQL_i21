4cf71229ba90dfbc9205f84937d658a0766b77a7
Arman Arenas <arman.arenas@irely.com>
2022-03-05 00:03:04 +0800

    LG-3231 polished the formula for claimable wt; fixed issue on pending claim entries with 0 claimable wt

--- i21Database/Scripts/Post-Deployment/LG/1_MasterTables.sql
+++ i21Database/Scripts/Post-Deployment/LG/1_MasterTables.sql
@@ -207,7 +207,6 @@ GO
 /* 
 * Populate Pending Claims table
 */
-
 IF EXISTS(SELECT * FROM sys.columns WHERE object_id = object_id('tblLGPendingClaim'))
 BEGIN
 	EXEC('
@@ -284,16 +283,20 @@ BEGIN
 				,dblReceivedGrossWt = (RI.dblGross - ISNULL(IRN.dblIRGross, 0))
 				,dblFranchisePercent = WG.dblFranchise
 				,dblFranchise = WG.dblFranchise / 100
-				,dblFranchiseWt = CASE WHEN (CASE WHEN (CLNW.dblLinkNetWt IS NOT NULL) THEN (CLNW.dblLinkNetWt) ELSE LD.dblNet END * WG.dblFranchise / 100) <> 0.0
-									THEN ((CASE WHEN (CLNW.dblLinkNetWt IS NOT NULL) THEN (CLNW.dblLinkNetWt) ELSE LD.dblNet END - ISNULL(IRN.dblIRNet, 0)) * WG.dblFranchise / 100)
-								ELSE 0.0 END
+				,dblFranchiseWt = CASE WHEN ((CASE WHEN (CLNW.dblLinkNetWt IS NOT NULL) THEN (CLNW.dblLinkNetWt) ELSE LD.dblNet END) * WG.dblFranchise / 100) <> 0.0
+										THEN ((CASE WHEN (CLNW.dblLinkNetWt IS NOT NULL) THEN (CLNW.dblLinkNetWt) ELSE LD.dblNet END) * WG.dblFranchise / 100)
+									ELSE 0.0 END
 				,dblWeightLoss = CASE WHEN (RI.dblNet - CASE WHEN (CLNW.dblLinkNetWt IS NOT NULL) THEN (CLNW.dblLinkNetWt) ELSE LD.dblNet END) < 0.0
 									THEN (RI.dblNet - CASE WHEN (CLNW.dblLinkNetWt IS NOT NULL) THEN (CLNW.dblLinkNetWt) ELSE LD.dblNet END)
-								ELSE (RI.dblNet - CASE WHEN (CLNW.dblLinkNetWt IS NOT NULL) THEN (CLNW.dblLinkNetWt) ELSE LD.dblNet END) END
-				,dblClaimableWt = CASE WHEN ((RI.dblNet - CASE WHEN (CLNW.dblLinkNetWt IS NOT NULL) THEN (CLNW.dblLinkNetWt) ELSE LD.dblNet END) + (LD.dblNet * WG.dblFranchise / 100)) < 0.0
-									THEN ((RI.dblNet - CASE WHEN (CLNW.dblLinkNetWt IS NOT NULL) THEN (CLNW.dblLinkNetWt) ELSE LD.dblNet END) + (LD.dblNet * WG.dblFranchise / 100))
-									ELSE (RI.dblNet - CASE WHEN (CLNW.dblLinkNetWt IS NOT NULL) THEN (CLNW.dblLinkNetWt) ELSE LD.dblNet END)
-								END
+								ELSE (CASE WHEN (CLNW.dblLinkNetWt IS NOT NULL) THEN (CLNW.dblLinkNetWt) ELSE LD.dblNet END - RI.dblNet) END
+				,dblClaimableWt = CASE WHEN (ABS((RI.dblNet - CASE WHEN (CLNW.dblLinkNetWt IS NOT NULL) THEN (CLNW.dblLinkNetWt) ELSE LD.dblNet END)) > (LD.dblNet * WG.dblFranchise / 100))
+									THEN
+										CASE WHEN (RI.dblNet - CASE WHEN (CLNW.dblLinkNetWt IS NOT NULL) THEN (CLNW.dblLinkNetWt) ELSE LD.dblNet END) < 0.0
+											THEN (RI.dblNet - CASE WHEN (CLNW.dblLinkNetWt IS NOT NULL) THEN (CLNW.dblLinkNetWt) ELSE LD.dblNet END) + (LD.dblNet * WG.dblFranchise / 100)
+										ELSE 
+											(CASE WHEN (CLNW.dblLinkNetWt IS NOT NULL) THEN (CLNW.dblLinkNetWt) ELSE LD.dblNet END - RI.dblNet) - (LD.dblNet * WG.dblFranchise / 100)
+										END
+									ELSE 0.0 END
 				,dblSeqPrice = AD.dblSeqPrice
 				,strSeqCurrency = AD.strSeqCurrency
 				,strSeqPriceUOM = AD.strSeqPriceUOM
@@ -306,6 +309,7 @@ BEGIN
 				,intItemId = LD.intItemId
 				,intContractDetailId = CD.intContractDetailId
 				,dblSeqPriceConversionFactoryWeightUOM = (WUI.dblUnitQty / PUI.dblUnitQty)
+				,ysnDropShip = CAST(CASE WHEN (L.intPurchaseSale = 3) THEN 1 ELSE 0 END AS BIT)
 			FROM tblLGLoad L
 				JOIN tblLGLoadDetail LD ON LD.intLoadId = L.intLoadId
 				JOIN tblCTContractDetail CD ON CD.intContractDetailId = intPContractDetailId
@@ -375,6 +379,7 @@ BEGIN
 				,intItemId = CD.intItemId
 				,intContractDetailId = CD.intContractDetailId
 				,dblSeqPriceConversionFactoryWeightUOM = (WUI.dblUnitQty / PUI.dblUnitQty)
+				,ysnDropShip = CAST(CASE WHEN (L.intPurchaseSale = 3) THEN 1 ELSE 0 END AS BIT)
 			FROM tblLGLoad L
 				JOIN tblICUnitMeasure WUOM ON WUOM.intUnitMeasureId = L.intWeightUnitMeasureId
 				JOIN tblLGLoadDetail LD ON LD.intLoadId = L.intLoadId
@@ -402,6 +407,7 @@ BEGIN
 					AND (LD.ysnNoClaim IS NULL OR LD.ysnNoClaim = 0)
 					AND NOT EXISTS (SELECT TOP 1 1 FROM tblLGPendingClaim WHERE intLoadId = L.intLoadId AND intPurchaseSale = 2)
 				) LI
+			WHERE ([ysnDropShip] = 0 AND [dblClaimableWt] <> 0) OR [ysnDropShip] = 1
 	')
 END
 GO
