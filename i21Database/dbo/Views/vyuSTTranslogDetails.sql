9be81ee0d6bd01ef8b4e445460468b8678565e30
nelsonreyes19 <nelson.reyes@irely.com>
2022-03-04 10:18:32 +0800

    ST-2090

--- i21Database/dbo/Views/vyuSTTranslogDetails.sql
+++ i21Database/dbo/Views/vyuSTTranslogDetails.sql
@@ -33,7 +33,11 @@ SELECT intTranslogId AS intId
 	, strTrLineType
 	, ysnFuel
 	, dblTrlPrcOvrd
-	, intMixMatchDeals
+	--ST-2090 - removed this field since this is generating wrong value on some scenarios
+	--, intMixMatchDeals
+	, strTrLoyaltyProgramProgramID	
+	, strTrLoyaltyProgramTrloAccount	
+	, strTrLoyaltyProgramTrloEntryMeth
 	, strItemType
 	, strTrlMatchLineTrlMatchName
 	, CAST((CASE WHEN dblTrlQty<>0 
@@ -48,15 +52,23 @@ SELECT intTranslogId AS intId
 				END AS dblTrlQty  
 		    -- Added 12/21/2021
 	, dblTrlSign
-    ,cast((CASE WHEN strTrlMatchLineTrlMatchName IS NOT NULL THEN dblTrlLineTot - intMixMatchDeals * dblTrlMatchLineTrlPromoAmount   
-		            --Line total - the number of Mix Match Deals * dblTrlMatchLineTrlPromoAmount
-					  ELSE dblTrlLineTot END) AS DECIMAL(18,2)) AS dblTrlLineTot
-	    
+	--,cast((CASE WHEN strTrlMatchLineTrlMatchName IS NOT NULL THEN dblTrlLineTot - intMixMatchDeals * dblTrlMatchLineTrlPromoAmount   
+	--	            --Line total - the number of Mix Match Deals * dblTrlMatchLineTrlPromoAmount
+	--				  ELSE dblTrlLineTot END) AS DECIMAL(18,2)) AS dblTrlLineTot
+	--ST-2090 - Added group by to get the correct mix and match line total on all scenarios -- 02/10/2022
+    ,CAST(
+		CASE WHEN strTrlMatchLineTrlMatchName IS NOT NULL 
+			THEN dblTrlLineTot - dblTrlMatchLineTrlPromoAmount
+			ELSE dblTrlLineTot 
+		END 
+	AS DECIMAL(18,2)) AS dblTrlLineTot
+	, dblTrlMatchLineTrlMatchQuantity
+	, dblTrlMatchLineTrlPromoAmount
 	--, dblTrlUnitPrice * dblTrlQty * dblTrlSign AS dblTrlLineTot  -- Added 10/12/2021
 	--, ROW_NUMBER() OVER (ORDER BY intTermMsgSN ASC) AS intId
 FROM
 (   SELECT --DISTINCT  -- update 09/08
-        intTranslogId
+        MIN(intTranslogId) AS intTranslogId
 		, CAST(TR.intTermMsgSN AS NVARCHAR(MAX)) + '0' +  CAST(TR.intTermMsgSNterm AS NVARCHAR(MAX)) + '0' + CAST(TR.intStoreId AS NVARCHAR(MAX)) 
                 --+ CAST(USec.intEntityId AS NVARCHAR(MAX)) 
                 COLLATE Latin1_General_CI_AS AS strUniqueId
@@ -68,8 +80,9 @@ FROM
        , TR.strTrpPaycode
        , TR.strTrpCardInfoTrpcCCName
        , TR.strTrlDesc
-	   , TR.dblTrlLineTot
-	   , TR.dblTrlMatchLineTrlPromoAmount
+	   , SUM(TR.dblTrlLineTot) AS dblTrlLineTot
+	   , SUM(TR.dblTrlMatchLineTrlPromoAmount) AS dblTrlMatchLineTrlPromoAmount
+	   , SUM(TR.dblTrlMatchLineTrlMatchQuantity) AS dblTrlMatchLineTrlMatchQuantity
        --, TR.dblTrlUnitPrice removed 09/08/2021
        , TR.strTrlDeptType
 	   , TR.strTrLineType
@@ -79,6 +92,9 @@ FROM
 	   , TR.dblTrExNetProdTrENPAmount
 	   , TR.strCustDOB
 	   , TR.strTrlModifier
+	   , TR.strTrLoyaltyProgramProgramID	
+	   , TR.strTrLoyaltyProgramTrloAccount	
+	   , TR.strTrLoyaltyProgramTrloEntryMeth
        , CONVERT(VARCHAR, TR.dtmDate, 23) AS dtmDate
        , CAST(TR.intCashierPosNum AS INT) AS intCashierPosNum
        , CAST(TR.intStoreId AS INT) AS intStoreId
@@ -112,14 +128,14 @@ FROM
                     THEN 'I' --Normal Item 
             END 
         AS strItemType
-		,CASE WHEN strTransType like '%refund%' or strTrLineType like '%void%' --  Added 11/17/2021
+		,SUM(CASE WHEN strTransType like '%refund%' or strTrLineType like '%void%' --  Added 11/17/2021
 		        then  TR.dblTrlQty * -1
 				Else TR.dblTrlQty 
-				END as dblTrlQty 
+				END) as dblTrlQty 
 	     --Flips sign for quantity. If both "refund" and "void" are on the same line, sign does not need flipped.
 													 
-
-		 ,Cast(CASE WHEN dblTrlMatchLineTrlMatchQuantity=1 THEN 1 ELSE ROUND((dblTrlQty-.5)/dblTrlMatchLineTrlMatchQuantity,0) END AS INT) AS intMixMatchDeals
+		 -- Removed since this is not being used
+		 --,Cast(CASE WHEN dblTrlMatchLineTrlMatchQuantity=1 THEN 1 ELSE ROUND((dblTrlQty-.5)/dblTrlMatchLineTrlMatchQuantity,0) END AS INT) AS intMixMatchDeals
 		
 		
 					 
@@ -129,8 +145,85 @@ FROM
 		,TR.strTrlMatchLineTrlMatchName  -- Added 09/26/2021
 
        FROM tblSTTranslogRebates TR 
-        WHERE  (strTransRollback IS NULL AND strTransFuelPrepay IS NULL AND strTransType NOT LIKE '%suspended%' AND strTransType NOT LIKE '%void%' AND strTrLineType<>'preFuel' AND strTransFuelPrepayCompletion IS NULL) 
-		OR     (strTransRollback IS NULL AND strTransFuelPrepay IS NULL AND strTransType NOT LIKE '%suspended%' AND strTransType NOT LIKE '%void%' AND strTrLineType='postFuel' AND strTransFuelPrepayCompletion IS NOT NULL)
-       	   
+	    -- Change to improve query performance - 02/11/2022
+		--Where (strTransRollback IS NULL 
+		--AND strTransFuelPrepay IS NULL 
+		--AND strTransType NOT LIKE '%suspended%' 
+		--AND strTransType NOT LIKE '%void%' 
+		--AND strTrLineType<>'preFuel')
+		WHERE (strTransRollback IS NULL
+		AND strTransFuelPrepay IS NULL
+		AND strTransType <> 'suspended sale'
+		AND strTransType <> 'refund void'
+		AND strTransType <> 'void'
+		AND strTrLineType <>'preFuel')
+
+		--ST-2090 - Added group by to get the correct mix and match line total on all scenarios -- 02/10/2022
+		GROUP BY
+		CAST(TR.intTermMsgSN AS NVARCHAR(MAX)) + '0' +  CAST(TR.intTermMsgSNterm AS NVARCHAR(MAX)) + '0' + CAST(TR.intStoreId AS NVARCHAR(MAX)) 
+                --+ CAST(USec.intEntityId AS NVARCHAR(MAX)) 
+                COLLATE Latin1_General_CI_AS 
+       , TR.intTrlDeptNumber
+       , TR.strTrlDept
+       , TR.strTrlNetwCode
+       , TR.strTrlUPC
+       , dbo.fnSTUPCRemoveLeadingZero(strTrlUPC) 
+       , TR.strTrpPaycode
+       , TR.strTrpCardInfoTrpcCCName
+       , TR.strTrlDesc
+       --, TR.dblTrlUnitPrice removed 09/08/2021
+       , TR.strTrlDeptType
+	   , TR.strTrLineType
+       , TR.intTermMsgSN
+       , TR.dtmDate 
+	   , TR.strTransRollback
+	   , TR.dblTrExNetProdTrENPAmount
+	   , TR.strCustDOB
+	   , TR.strTrlModifier
+	   , TR.strTrLoyaltyProgramProgramID	
+	   , TR.strTrLoyaltyProgramTrloAccount	
+	   , TR.strTrLoyaltyProgramTrloEntryMeth
+       , CONVERT(VARCHAR, TR.dtmDate, 23) 
+       , CAST(TR.intCashierPosNum AS INT) 
+       , CAST(TR.intStoreId AS INT) 
+       , CAST(TR.intStoreNumber AS INT)
+	
+       , CAST(TR.intTrTickNumPosNum AS INT)
+       , CAST(TR.intTrTickNumTrSeq AS INT) 
+       , TR.strTransType 
+       , CAST(TR.intCashierSysId AS INT) 
+       , TR.strCashier
+       , TR.intTrTickNumTrSeq -- Added 09/08/2021
+	   , TR.dblTrlPrcOvrd 
+       , RIGHT('0' + CONVERT(VARCHAR(2), DATEPART(HOUR, TR.dtmDate)), 2)
+       , CAST(RIGHT(intCashierPosNum , 1) AS INT)
+       , CAST((CASE WHEN LEN(strTrlFuelBasePrice) > 0
+                           THEN 1
+                           ELSE 0
+                           END) AS BIT)  
+        ,CASE WHEN dblTrlPrcOvrd IS NOT NULL
+                    THEN 'O' --Price Override
+            WHEN strTrlMatchLineTrlMatchName IS NOT NULL
+                    THEN 'M' --Mix/Match Item
+           -- WHEN strTrlUPC IS NULL AND (dblTrlSign = -1 OR dblTrlLineTot < 0 OR strTrlDeptType = 'neg') 
+		   WHEN strTrlUPC IS NULL AND strTrLineType = 'void dept'
+                    THEN 'N' --Department Return
+            WHEN strTrlUPC IS NOT NULL AND strTrLineType = 'void plu'
+                    THEN 'R' --Item Return
+            WHEN strTrlUPC IS NULL
+                    THEN 'D' --Department 
+            WHEN strTrlUPC IS NOT NULL
+                    THEN 'I' --Normal Item 
+            END 
+	     --Flips sign for quantity. If both "refund" and "void" are on the same line, sign does not need flipped.
+													 
+
+		 ,Cast(CASE WHEN dblTrlMatchLineTrlMatchQuantity=1 THEN 1 ELSE ROUND((dblTrlQty-.5)/dblTrlMatchLineTrlMatchQuantity,0) END AS INT) 
+		
+		
+					 
+
+	    --,dblTrlUnitPrice * dblTrlQty * dblTrlSign as dblTrlLineTot
+		,dblTrlSign -- Added 10/12/2021		
+		,TR.strTrlMatchLineTrlMatchName  -- Added 09/26/2021
 ) x
-GO
