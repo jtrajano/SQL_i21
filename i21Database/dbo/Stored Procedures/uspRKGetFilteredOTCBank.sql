dde4d70f15d0e3b07daddffbcccd0124176d547d
Michael Mirho Villaluz <mirho.villaluz@irely.com>
2022-03-07 17:05:22 +0800

    RM-4558

--- i21Database/dbo/Stored Procedures/uspRKGetFilteredOTCBank.sql
+++ i21Database/dbo/Stored Procedures/uspRKGetFilteredOTCBank.sql
@@ -1,13 +1,13 @@
 CREATE PROCEDURE [dbo].[uspRKGetFilteredOTCBank]
 	  @intBuyCurrencyId INT
 	, @intSellCurrencyId INT
+	, @intLocationId INT
+	, @strInstrumentType NVARCHAR(100)
 AS
 BEGIN
 	SELECT intBankId
 		, strBankName
 	FROM tblCMBank bank
-	WHERE EXISTS	(SELECT TOP 1 '' FROM vyuCMBankAccount bankAcct 
-						WHERE bankAcct.intBankId = bank.intBankId AND bankAcct.intCurrencyId = @intBuyCurrencyId)
-		AND EXISTS	(SELECT TOP 1 '' FROM vyuCMBankAccount bankAcct 
-						WHERE bankAcct.intBankId = bank.intBankId AND bankAcct.intCurrencyId = @intSellCurrencyId)
+	WHERE EXISTS	(SELECT TOP 1 '' FROM dbo.fnRKGetOTCAllowedBankAccounts(@intLocationId, @strInstrumentType, @intBuyCurrencyId, bank.intBankId) bankAcct)
+		AND EXISTS	(SELECT TOP 1 '' FROM dbo.fnRKGetOTCAllowedBankAccounts(@intLocationId, @strInstrumentType, @intSellCurrencyId, bank.intBankId) bankAcct)
 END
