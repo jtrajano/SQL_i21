6be822958e970ec266cad91b27c12332f37d4e0f
Feb Montefrio <feb.montefrio@irely.com>
2022-03-08 16:01:26 +0800

    [F] IC-9589

---
+++ i21Database/dbo/Functions/fnGetItemCompanySegment.sql
@@ -0,0 +1,17 @@
+﻿CREATE FUNCTION [dbo].[fnGetItemCompanySegment]
+(
+	@intLocationId INT
+)
+RETURNS INT
+AS 
+BEGIN 
+	DECLARE @intProfitCenter AS INT
+
+	-- Get the COMPANY segment from the company location
+	SELECT	@intProfitCenter = intCompanySegment
+	FROM	dbo.tblSMCompanyLocation 
+	WHERE	intCompanyLocationId = @intLocationId
+
+	RETURN @intProfitCenter 
+END
+GO
