32723edcd99ad07e593f5f990eb71790a411f59b
IRELY\JMendones <jesmar.mendones@irely.com>
2022-03-10 13:36:17 +0800

    EM-2903

--- i21Database/Scripts/Post-Deployment/EM/2210_AddDefaultEFTBank.sql
+++ i21Database/Scripts/Post-Deployment/EM/2210_AddDefaultEFTBank.sql
@@ -45,4 +45,61 @@
 	DEALLOCATE temp_cursor
 
 	PRINT ('*****End EFT Default Bank*****')
+-------------------------------------------------------------------------------------------------------------
+	PRINT ('*****Begin ADD EFT Header table*****')
+
+	IF OBJECT_ID('tempdb..#TempUpdateEFTHeader') IS NOT NULL
+		DROP TABLE #TempUpdateEFTHeader
+
+	CREATE TABLE #TempUpdateEFTHeader
+	(
+		[intEntityId]			INT	NOT NULL,
+		[intEntityEFTInfoId]	INT	NOT NULL
+	)
+
+	INSERT INTO #TempUpdateEFTHeader ([intEntityId], [intEntityEFTInfoId])
+	SELECT intEntityId, intEntityEFTInfoId
+	FROM tblEMEntityEFTInformation 
+	WHERE ISNULL([intEntityEFTHeaderId], 0) = 0
+	ORDER BY intEntityId
+
+	DECLARE temp_cursor_header CURSOR FOR
+	SELECT [intEntityId], [intEntityEFTInfoId]
+	FROM #TempUpdateEFTHeader
+
+	DECLARE @intNewHeaderId			INT
+
+	OPEN temp_cursor_header
+	FETCH NEXT FROM temp_cursor_header into @intEntityId, @intEntityEFTInfoId
+	WHILE @@FETCH_STATUS = 0
+	BEGIN
+
+		IF NOT EXISTS (SELECT TOP 1 1 FROM tblEMEntityEFTHeader WHERE intEntityId = @intEntityId)
+		BEGIN
+			INSERT INTO [tblEMEntityEFTHeader] ([intEntityId], [intConcurrencyId])
+			VALUES (@intEntityId, 1)
+			
+			SET @intNewHeaderId = SCOPE_IDENTITY();
+
+			IF ISNULL(@intNewHeaderId, 0) <> 0
+			BEGIN
+				UPDATE tblEMEntityEFTInformation 
+				SET [intEntityEFTHeaderId] = @intNewHeaderId WHERE intEntityEFTInfoId = @intEntityEFTInfoId
+			END
+		END
+		ELSE
+		BEGIN
+			UPDATE tblEMEntityEFTInformation 
+			SET 
+				intEntityEFTHeaderId = (SELECT TOP 1 intEntityEFTHeaderId FROM tblEMEntityEFTHeader WHERE intEntityId = @intEntityId)
+			WHERE intEntityEFTInfoId = @intEntityEFTInfoId
+		END
+		
+		FETCH NEXT FROM temp_cursor_header into @intEntityId, @intEntityEFTInfoId
+	END
+
+	CLOSE temp_cursor_header
+	DEALLOCATE temp_cursor_header
+
+	PRINT ('*****End ADD EFT Header table*****')
 GO
