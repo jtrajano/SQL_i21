d725bf021bc18ab960a940383d0025b2dd17ecd9
Leslie Guevarra <leslie.arat@irely.com>
2022-03-10 11:16:00 +0800

    GRN-2898 (HDTN-269162 - Charges and Premiums Enhancement) - Override Rates in Settle Storage screen

---
+++ i21Database/dbo/User Defined Types/SettleStorageChargeAndPremium.sql
@@ -0,0 +1,10 @@
+﻿CREATE TYPE [dbo].[SettleStorageChargeAndPremium] AS TABLE
+(
+	[intParentSettleStorageId]     INT NULL
+    ,[intCustomerStorageId] 		INT
+    ,[intChargeAndPremiumDetailId]	INT
+    ,[dblRate]						DECIMAL(18,6)
+    ,[dblUnits]						DECIMAL(38, 20)
+    ,[dblCost]						DECIMAL(38, 20)
+	,[ysnOverride]					BIT DEFAULT((0))
+)
