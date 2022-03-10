2f3e1eda63732f85b540a54157711054d562cc93
Leslie Guevarra <leslie.arat@irely.com>
2022-03-10 11:16:00 +0800

    GRN-2898 (HDTN-269162 - Charges and Premiums Enhancement) - Override Rates in Settle Storage screen

--- i21Database/dbo/Functions/fnGRCalculateChargeAndPremium.sql
+++ i21Database/dbo/Functions/fnGRCalculateChargeAndPremium.sql
@@ -8,6 +8,8 @@ CREATE FUNCTION [dbo].[fnGRCalculateChargeAndPremium](
     ,@tblQMDiscountIds Id READONLY
     ,@dblCost DECIMAL(18, 6)
     ,@dtmCalculateOn DATETIME
+	,@settleStorageChargeAndPremium SettleStorageChargeAndPremium READONLY
+	,@intCustomerStorageId INT
 )
 RETURNS @tblChargeAndPremium TABLE
 (
@@ -44,13 +46,13 @@ BEGIN
         ,[intCalculationTypeId]             =   CT.intCalculationTypeId
         ,[strCalculationType]               =   CT.strCalculationType
         ,[dblRate]                          =   CASE CAPD.intCalculationTypeId
-                                                    WHEN 5 THEN ISNULL(FIXED_RATE.dblRate, 0)
-                                                    WHEN 4 THEN ISNULL(PERCENTAGE_BY_DISCOUNT.dblRate, 0)
-                                                    WHEN 3 THEN ISNULL(PERCENTAGE_BY_ITEM.dblRate, 0)
-                                                    WHEN 2 THEN ISNULL(RANGE_BY_GRADEREADING.dblRate, 0)
-                                                    WHEN 1 THEN ISNULL(RANGE_BY_UNITS.dblRate, 0)
-                                                    ELSE 0
-                                                END
+													WHEN 5 THEN ISNULL(FIXED_RATE.dblRate, 0)
+													WHEN 4 THEN ISNULL(PERCENTAGE_BY_DISCOUNT.dblRate, 0)
+													WHEN 3 THEN ISNULL(PERCENTAGE_BY_ITEM.dblRate, 0)
+													WHEN 2 THEN ISNULL(RANGE_BY_GRADEREADING.dblRate, 0)
+													WHEN 1 THEN ISNULL(RANGE_BY_UNITS.dblRate, 0)
+													ELSE 0
+												END
         ,[strRateType]                      =   CAPD.strRateType
         ,[dblQty]                           =   (CASE CAPD.intCalculationTypeId
                                                     WHEN 5 THEN ISNULL(FIXED_RATE.dblQty, 0)
@@ -100,20 +102,34 @@ BEGIN
         ON OC_ITEM.intItemId = CAPD.intOtherChargeItemId
     LEFT JOIN tblICItem INV_ITEM
         ON INV_ITEM.intItemId = CAPD.intInventoryItemId
-
-    -- Range By Grade Reading Calculation
+	--OUTER APPLY (
+	--	SELECT * 
+	--	FROM @settleStorageChargeAndPremium
+	--	WHERE ysnOverride = 1
+	--		AND intChargeAndPremiumDetailId = CAPD.intChargeAndPremiumDetailId
+	--) _OVERRIDE2
+    -- Range By Grade Reading Calculation --OK
     OUTER APPLY (
         SELECT
             [dblQty]    =   CASE CAPD.strRateType
                                 WHEN 'Per Unit' THEN @dblNetUnits
                                 WHEN 'Flat' THEN 1
                             END
-            ,[dblCost]  =   CASE CAPD.strRateType
-                                WHEN 'Per Unit' THEN ISNULL(CAPD_RANGE.dblRangeRate, 0)
-                                -- Get prorated cost when rate type is "Flat"
-                                WHEN 'Flat' THEN (@dblNetUnits * ISNULL(CAPD_RANGE.dblRangeRate, 0)) / @dblTransactionUnits
-                            END
-            ,[dblRate]  =   ISNULL(CAPD_RANGE.dblRangeRate, 0)
+            ,[dblCost]  =   CASE 
+								WHEN _OVERRIDE.ysnOverride = 1 THEN 
+									CASE CAPD.strRateType
+										WHEN 'Per Unit' THEN ISNULL(_OVERRIDE.dblRate, 0)
+										-- Get prorated cost when rate type is "Flat"
+										WHEN 'Flat' THEN (@dblNetUnits * ISNULL(_OVERRIDE.dblRate, 0)) / @dblTransactionUnits
+									END
+								ELSE
+									CASE CAPD.strRateType
+										WHEN 'Per Unit' THEN ISNULL(CAPD_RANGE.dblRangeRate, 0)
+										-- Get prorated cost when rate type is "Flat"
+										WHEN 'Flat' THEN (@dblNetUnits * ISNULL(CAPD_RANGE.dblRangeRate, 0)) / @dblTransactionUnits
+									END
+							END
+            ,[dblRate]  =  CASE WHEN _OVERRIDE.ysnOverride = 1 THEN _OVERRIDE.dblRate ELSE ISNULL(CAPD_RANGE.dblRangeRate, 0) END
         FROM @tblQMDiscountIds QM_ID
         INNER JOIN tblQMTicketDiscount QM
             ON QM.intTicketDiscountId = QM_ID.intId
@@ -121,6 +137,13 @@ BEGIN
             ON DSC.intDiscountScheduleCodeId = QM.intDiscountScheduleCodeId
         INNER JOIN tblGRChargeAndPremiumDetailRange CAPD_RANGE
             ON CAPD_RANGE.intChargeAndPremiumDetailId = CAPD.intChargeAndPremiumDetailId
+		OUTER APPLY (
+			SELECT * 
+			FROM @settleStorageChargeAndPremium
+			WHERE ysnOverride = 1
+				AND intChargeAndPremiumDetailId = CAPD.intChargeAndPremiumDetailId
+				AND intCustomerStorageId = QM.intTicketFileId
+		) _OVERRIDE
         WHERE DSC.intItemId = CAPD.intOtherChargeItemId
         AND QM.dblGradeReading BETWEEN CAPD_RANGE.dblFrom AND CAPD_RANGE.dblTo
         AND CAPD.intCalculationTypeId = 2 --Range By Grade Reading
@@ -133,16 +156,33 @@ BEGIN
                                 WHEN 'Per Unit' THEN @dblNetUnits
                                 WHEN 'Flat' THEN 1
                             END
-            ,[dblCost]  =   CASE CAPD.strRateType
-                                WHEN 'Per Unit' THEN ISNULL(CAPD_RANGE.dblRangeRate, 0)
-                                -- Get prorated cost when rate type is "Flat"
-                                WHEN 'Flat' THEN (@dblNetUnits * ISNULL(CAPD_RANGE.dblRangeRate, 0)) / @dblTransactionUnits
-                            END
-            ,[dblRate]  =   ISNULL(CAPD_RANGE.dblRangeRate, 0)
+            ,[dblCost]  =   CASE 
+								WHEN _OVERRIDE.ysnOverride = 1 THEN 
+									CASE CAPD.strRateType
+										WHEN 'Per Unit' THEN ISNULL(_OVERRIDE.dblRate, 0)
+										-- Get prorated cost when rate type is "Flat"
+										WHEN 'Flat' THEN (@dblNetUnits * ISNULL(_OVERRIDE.dblRate, 0)) / @dblTransactionUnits
+									END
+								ELSE
+									CASE CAPD.strRateType
+										WHEN 'Per Unit' THEN ISNULL(CAPD_RANGE.dblRangeRate, 0)
+										-- Get prorated cost when rate type is "Flat"
+										WHEN 'Flat' THEN (@dblNetUnits * ISNULL(CAPD_RANGE.dblRangeRate, 0)) / @dblTransactionUnits
+									END
+							END
+            ,[dblRate]  =   CASE WHEN _OVERRIDE.ysnOverride = 1 THEN _OVERRIDE.dblRate ELSE ISNULL(CAPD_RANGE.dblRangeRate, 0) END
         FROM tblGRChargeAndPremiumDetailRange CAPD_RANGE
+		OUTER APPLY (
+			SELECT * 
+			FROM @settleStorageChargeAndPremium
+			WHERE ysnOverride = 1
+				AND intChargeAndPremiumDetailId = CAPD.intChargeAndPremiumDetailId
+				AND intCustomerStorageId = ISNULL(@intCustomerStorageId,intCustomerStorageId)
+		) _OVERRIDE
         WHERE CAPD_RANGE.intChargeAndPremiumDetailId = CAPD.intChargeAndPremiumDetailId
-        AND @dblTransactionUnits BETWEEN CAPD_RANGE.dblFrom AND CAPD_RANGE.dblTo
-        AND CAPD.intCalculationTypeId = 1 --Range By Units
+			--AND CAPD_RANGE.intChargeAndPremiumDetailId = _OVERRIDE.intChargeAndPremiumDetailId
+			AND @dblTransactionUnits BETWEEN CAPD_RANGE.dblFrom AND CAPD_RANGE.dblTo
+			AND CAPD.intCalculationTypeId = 1 --Range By Units
     ) RANGE_BY_UNITS
 
     -- Fixed Rate Calculation
@@ -153,17 +193,34 @@ BEGIN
                                 WHEN 'Flat' THEN 1
                             END
                             --The "Rate From Location" takes precedence over the default rate.
-            ,[dblCost]  =   CASE CAPD.strRateType
-                                WHEN 'Per Unit' THEN ISNULL(CAPD_LOC.dblLocationRate, ISNULL(CAPD2.dblRate, 0))
-                                -- Get prorated cost when rate type is "Flat"
-                                WHEN 'Flat' THEN (@dblNetUnits * ISNULL(CAPD_LOC.dblLocationRate, ISNULL(CAPD2.dblRate, 0))) / @dblTransactionUnits
-                            END
-            ,[dblRate]  =   ISNULL(CAPD_LOC.dblLocationRate, ISNULL(CAPD2.dblRate, 0))
+            ,[dblCost]  =   CASE 
+								WHEN _OVERRIDE.ysnOverride = 1 THEN 
+									CASE CAPD.strRateType
+										WHEN 'Per Unit' THEN _OVERRIDE.dblRate
+										-- Get prorated cost when rate type is "Flat"
+										WHEN 'Flat' THEN (@dblNetUnits * _OVERRIDE.dblRate) / @dblTransactionUnits
+									END
+								ELSE
+									CASE CAPD.strRateType
+										WHEN 'Per Unit' THEN ISNULL(CAPD_LOC.dblLocationRate, ISNULL(CAPD2.dblRate, 0))
+										-- Get prorated cost when rate type is "Flat"
+										WHEN 'Flat' THEN (@dblNetUnits * ISNULL(CAPD_LOC.dblLocationRate, ISNULL(CAPD2.dblRate, 0))) / @dblTransactionUnits
+									END
+							END							
+            ,[dblRate]  =   CASE WHEN _OVERRIDE.ysnOverride = 1 THEN _OVERRIDE.dblRate ELSE ISNULL(CAPD_LOC.dblLocationRate, ISNULL(CAPD2.dblRate, 0)) END
         FROM tblGRChargeAndPremiumDetail CAPD2
         LEFT JOIN tblGRChargeAndPremiumDetailLocation CAPD_LOC
             ON CAPD_LOC.intChargeAndPremiumDetailId = CAPD2.intChargeAndPremiumDetailId
             AND CAPD_LOC.intCompanyLocationId = @intCompanyLocationId
+		OUTER APPLY (
+			SELECT * 
+			FROM @settleStorageChargeAndPremium
+			WHERE ysnOverride = 1
+				AND intChargeAndPremiumDetailId = CAPD2.intChargeAndPremiumDetailId
+				AND intCustomerStorageId = ISNULL(@intCustomerStorageId,intCustomerStorageId)
+		) _OVERRIDE
         WHERE CAPD2.intChargeAndPremiumDetailId = CAPD.intChargeAndPremiumDetailId
+		--AND CAPD2.intChargeAndPremiumDetailId = _OVERRIDE.intChargeAndPremiumDetailId
         AND CAPD.intCalculationTypeId = 5 --Fixed Rate
     ) FIXED_RATE
 
@@ -186,8 +243,8 @@ BEGIN
                             )
                             -- Multiply by Charge/Premium Percentage Rate
                             -- The "Rate From Location" takes precedence over the default rate.
-                            * (ISNULL(CAPD_LOC.dblLocationRate, ISNULL(CAPD.dblRate, 0)) / 100)
-            ,[dblRate]  =   ISNULL(CAPD_LOC.dblLocationRate, ISNULL(CAPD.dblRate, 0))
+                            * (CASE WHEN _OVERRIDE.ysnOverride = 1 THEN _OVERRIDE.dblRate ELSE ISNULL(CAPD_LOC.dblLocationRate, ISNULL(CAPD.dblRate, 0)) END / 100)
+            ,[dblRate]  =   CASE WHEN _OVERRIDE.ysnOverride = 1 THEN _OVERRIDE.dblRate ELSE ISNULL(CAPD_LOC.dblLocationRate, ISNULL(CAPD.dblRate, 0)) END
         FROM @tblQMDiscountIds QM_ID
         INNER JOIN tblQMTicketDiscount QM
             ON QM.intTicketDiscountId = QM_ID.intId
@@ -196,6 +253,13 @@ BEGIN
         LEFT JOIN tblGRChargeAndPremiumDetailLocation CAPD_LOC
             ON CAPD_LOC.intChargeAndPremiumDetailId = CAPD.intChargeAndPremiumDetailId
             AND CAPD_LOC.intCompanyLocationId = @intCompanyLocationId
+		OUTER APPLY (
+			SELECT * 
+			FROM @settleStorageChargeAndPremium
+			WHERE ysnOverride = 1
+				AND intChargeAndPremiumDetailId = CAPD.intChargeAndPremiumDetailId
+				AND intCustomerStorageId = QM.intTicketFileId
+		) _OVERRIDE
         WHERE DSC.intItemId = CAPD.intOtherChargeItemId
         AND CAPD.intCalculationTypeId = 4 --Percentage By Discount
     ) PERCENTAGE_BY_DISCOUNT
@@ -207,12 +271,19 @@ BEGIN
             ,[dblCost]  =   (@dblNetUnits * @dblCost)
                             -- Multiply by Charge/Premium Percentage Rate
                             -- The "Rate From Location" takes precedence over the default rate.
-                            * (ISNULL(CAPD_LOC.dblLocationRate, ISNULL(CAPD.dblRate, 0)) / 100)
-            ,[dblRate]  =   ISNULL(CAPD_LOC.dblLocationRate, ISNULL(CAPD.dblRate, 0))
+                            * (CASE WHEN _OVERRIDE.ysnOverride = 1 THEN _OVERRIDE.dblRate ELSE ISNULL(CAPD_LOC.dblLocationRate, ISNULL(CAPD.dblRate, 0)) END / 100)
+            ,[dblRate]  =   CASE WHEN _OVERRIDE.ysnOverride = 1 THEN _OVERRIDE.dblRate ELSE ISNULL(CAPD_LOC.dblLocationRate, ISNULL(CAPD.dblRate, 0)) END
         FROM tblGRChargeAndPremiumDetail CAPD2
         LEFT JOIN tblGRChargeAndPremiumDetailLocation CAPD_LOC
             ON CAPD_LOC.intChargeAndPremiumDetailId = CAPD2.intChargeAndPremiumDetailId
             AND CAPD_LOC.intCompanyLocationId = @intCompanyLocationId
+		OUTER APPLY (
+			SELECT * 
+			FROM @settleStorageChargeAndPremium
+			WHERE ysnOverride = 1
+				AND intChargeAndPremiumDetailId = CAPD.intChargeAndPremiumDetailId
+				AND intCustomerStorageId = ISNULL(@intCustomerStorageId,intCustomerStorageId)
+		) _OVERRIDE
         WHERE CAPD2.intChargeAndPremiumDetailId = CAPD.intChargeAndPremiumDetailId
         AND CAPD2.intInventoryItemId = @intInventoryItemId
         AND CAPD.intCalculationTypeId = 3 --Percentage By Item
@@ -226,6 +297,7 @@ BEGIN
             AND @dtmCalculateOn BETWEEN CAPD.dtmEffectiveDate AND CAPD.dtmTerminationDate
         )
     )
+	--AND _OVERRIDE.intChargeAndPremiumDetailId = CAPD.intChargeAndPremiumDetailId
 
     RETURN
 END
---
+++ i21Database/dbo/Tables/tblGRSettleStorageChargeAndPremium.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[tblGRSettleStorageChargeAndPremium]
+(
+	[intSettleStorageChargeAndPremiumId] INT NOT NULL IDENTITY
+    ,[intParentSettleStorageId]     INT NULL
+    ,[intCustomerStorageId] 		INT NOT NULL
+    ,[intChargeAndPremiumDetailId]	INT NOT NULL
+    ,[dblRate]						DECIMAL(18,6)
+    ,[dblUnits]						DECIMAL(38, 20) NULL
+    ,[dblCost]						DECIMAL(38, 20) NULL
+	,[ysnOverride]					BIT DEFAULT((0))
+	,[intConcurrencyId] INT NULL DEFAULT ((1))
+	,CONSTRAINT [PK_tblGRSettleStorageChargeAndPremium_intSettleStorageChargeAndPremiumId] PRIMARY KEY ([intSettleStorageChargeAndPremiumId]),
+    CONSTRAINT [FK_tblGRSettleStorageChargeAndPremium_tblGRCustomerStorage_intCustomerStorageId] FOREIGN KEY ([intCustomerStorageId]) REFERENCES [tblGRCustomerStorage]([intCustomerStorageId]),
+	CONSTRAINT [PK_tblGRSettleStorageChargeAndPremium_intChargeAndPremiumDetailId] FOREIGN KEY ([intChargeAndPremiumDetailId]) REFERENCES [tblGRChargeAndPremiumDetail]([intChargeAndPremiumDetailId]),
+	CONSTRAINT [FK_tblGRSettleStorageChargeAndPremium_tblGRSettleStorage_intParentSettleStorageId] FOREIGN KEY ([intParentSettleStorageId]) REFERENCES [tblGRSettleStorage]([intSettleStorageId]) ON DELETE CASCADE
+)
