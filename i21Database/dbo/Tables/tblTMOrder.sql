8accad1cb78a5782894cd4799e046cfd00b9e3ff
Cris <cris.lazaga@irely.com>
2022-03-11 08:49:08 +0800

    TM-3320

---
+++ i21Database/dbo/Tables/tblTMOrder.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[tblTMOrder]
+(
+	[intOrderId] INT IDENTITY(1,1) NOT NULL,
+	[intDispatchId] INT NOT NULL,
+	[intSiteId] INT NOT NULL,
+	[intItemId] INT NOT NULL,
+	[strOrderNumber] NVARCHAR(100) NOT NULL,
+	[strPricingMethod] NVARCHAR(100) NOT NULL,
+	[intContractDetailId] INT NULL,
+	[dblQuantity] NUMERIC(18,6) NULL,
+	[dblPrice] NUMERIC(18,6) NULL,
+	[dblTotal] NUMERIC(18,6) NULL,
+	[strSource] NVARCHAR(100) NULL,
+	[dtmTransactionDate] DATETIME NOT NULL,
+	CONSTRAINT [PK_tblTMOrder] PRIMARY KEY CLUSTERED ([intOrderId] ASC),
+	CONSTRAINT [FK_tblTMOrder_tblTMSite] FOREIGN KEY ([intSiteId]) REFERENCES [dbo].[tblTMSite] ([intSiteID])
+)
+
