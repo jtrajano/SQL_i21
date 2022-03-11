988e342e4401076e60ed9838b5bddbe9920b1832
lawrence_zabala <lawrence.zabala@irely.com>
2022-03-11 11:34:03 +0800

    LG-2950

--- i21Database/dbo/Tables/tblLGInsurancePremiumFactorDetail.sql
+++ i21Database/dbo/Tables/tblLGInsurancePremiumFactorDetail.sql
@@ -1,14 +1,17 @@
 ﻿CREATE TABLE [dbo].[tblLGInsurancePremiumFactorDetail]
 (
-	intInsurancePremiumFactorDetailId INT NOT NULL PRIMARY KEY IDENTITY (1, 1),
+	intInsurancePremiumFactorDetailId INT NOT NULL IDENTITY (1, 1),
 	intInsurancePremiumFactorId INT NOT NULL,
-	dtmValidFrom DATETIME NOT NULL,
-	dtmValidTo DATETIME NOT NULL,
+	intLoadingZoneId INT NULL,
+	intLoadingPortId INT NULL,
+	intDestinationZoneId INT NULL,
+	intDestinationPortId INT NULL,
 	dblCost NUMERIC(18,6) NOT NULL,
 	intCostUOM INT NOT NULL, 
 	dblInsurancePremiumFactor NUMERIC(18,6),
 	dblProfitMarkup NUMERIC(18,6),
 	intConcurrencyId INT NOT NULL, 
 
-	CONSTRAINT [FK_tblLGInsurancePremiumFactorDetail_tblLGInsurancePremiumFactor_intInsurancePremiumFactorId] FOREIGN KEY ([intInsurancePremiumFactorId]) REFERENCES [tblLGInsurancePremiumFactor]([intInsurancePremiumFactorId]) ON DELETE CASCADE
+	CONSTRAINT [FK_tblLGInsurancePremiumFactorDetail_tblLGInsurancePremiumFactor_intInsurancePremiumFactorId] FOREIGN KEY ([intInsurancePremiumFactorId]) REFERENCES [tblLGInsurancePremiumFactor]([intInsurancePremiumFactorId]) ON DELETE CASCADE, 
+    CONSTRAINT [PK_tblLGInsurancePremiumFactorDetail] PRIMARY KEY ([intInsurancePremiumFactorDetailId])
 )
