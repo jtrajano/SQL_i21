0c1fac8a08f441f4bfde2d61be7c731576d28f6c
Wendell Wayne Estrada <wendell.estrada@irely.com>
2022-03-11 23:44:56 +0800

    ICM-2

--- i21Database/dbo/Views/vyuApiAttachment.sql
+++ i21Database/dbo/Views/vyuApiAttachment.sql
@@ -10,4 +10,7 @@ SELECT
     , a.strName
     , a.strComment
     , CAST(a.strRecordNo AS INT) intTransactionId
+    , a.intEntityId
+    , e.strName strEntityName
 FROM tblSMAttachment a
+LEFT JOIN tblEMEntity e ON e.intEntityId = a.intEntityId
