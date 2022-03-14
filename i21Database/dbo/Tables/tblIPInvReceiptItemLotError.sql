0f005731eee304f87f99a1d58c8645ad18c9b66b
Vinoth Kumar T <vinoth.t@irely.com>
2022-03-14 22:33:34 +0530

    IN-1003

--- i21Database/dbo/Tables/tblIPInvReceiptItemLotError.sql
+++ i21Database/dbo/Tables/tblIPInvReceiptItemLotError.sql
@@ -13,6 +13,7 @@
 	dblTareWeight				NUMERIC(18, 6),
 	dblNetWeight				NUMERIC(18, 6),
 	strWeightUOM				NVARCHAR(50) COLLATE Latin1_General_CI_AS,
+	strLotPrimaryStatus			NVARCHAR(50) COLLATE Latin1_General_CI_AS,
 
 	CONSTRAINT [PK_tblIPInvReceiptItemLotError_intStageReceiptItemLotId] PRIMARY KEY (intStageReceiptItemLotId),
 	CONSTRAINT [FK_tblIPInvReceiptItemLotError_tblIPInvReceiptError_intStageReceiptId] FOREIGN KEY (intStageReceiptId) REFERENCES [tblIPInvReceiptError](intStageReceiptId) ON DELETE CASCADE
