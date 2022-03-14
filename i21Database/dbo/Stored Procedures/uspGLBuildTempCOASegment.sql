3d1bc51a5bfa6946e059387c3f5dccba0abca6e1
DESKTOP-H1MTHE6\iRelyNUC4 <dom.caoile.02@gmail.com>
2022-03-14 23:29:35 +0800

    FRD-1663 Fixed

--- i21Database/dbo/Stored Procedures/uspGLBuildTempCOASegment.sql
+++ i21Database/dbo/Stored Procedures/uspGLBuildTempCOASegment.sql
@@ -130,23 +130,22 @@ BEGIN
 	IF EXISTS (SELECT top 1 1  FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'tblGLTempCOASegment')
 	BEGIN
 		EXEC ('CREATE VIEW [dbo].[vyuGLSummary]
-		AS
-	SELECT * FROM (
-	  SELECT  A.dtmDate, A.dblDebit, A.dblCredit, A.dblDebitUnit, A.dblCreditUnit,A.dblDebitForeign,A.dblCreditForeign, ISNULL(A.strCode,'') strCode, B.strDescription, C.strAccountGroup, C.strAccountType, D.*, E.strUOMCode, E.dblLbsPerUnit, A.intCurrencyId, F.strCurrency,B.intUnnaturalAccountId     
-		  FROM         dbo.tblGLPosted AS A   
-			  INNER JOIN dbo.tblGLAccount AS B ON B.intAccountId = A.intAccountId   
-			  INNER JOIN dbo.tblGLAccountGroup AS C ON C.intAccountGroupId = B.intAccountGroupId  
-			  LEFT JOIN dbo.tblGLTempCOASegment AS D ON D.intAccountId = B.intAccountId  
-			  LEFT JOIN dbo.tblGLAccountUnit AS E ON E.intAccountUnitId = B.intAccountUnitId  
-			  INNER JOIN dbo.tblSMCurrency AS F ON F.intCurrencyID= A.intCurrencyId 
-		)T0 LEFT JOIN 
-		(	
-			SELECT B.intAccountId intUnAccountId,B.strAccountId strUnAccountId,B.strDescription strUnDescription, C.strAccountGroup strUnAccountGroup, C.strAccountType strUnAccountType,Location UnLocation, [Primary Account] [UnPrimary Account] FROM
-			dbo.tblGLAccount B
-			INNER JOIN dbo.tblGLAccountGroup AS C ON C.intAccountGroupId = B.intAccountGroupId  
-			LEFT JOIN dbo.tblGLTempCOASegment AS D ON D.intAccountId = B.intAccountId  
-			LEFT JOIN dbo.tblGLAccountUnit AS E ON E.intAccountUnitId = B.intAccountUnitId  
-		)T1 ON T0.intUnnaturalAccountId = T1.intUnAccountId	')
+			AS
+			SELECT * FROM (
+			  SELECT  A.dtmDate, A.dblDebit, A.dblCredit, A.dblDebitUnit, A.dblCreditUnit,A.dblDebitForeign,A.dblCreditForeign, ISNULL(A.strCode,'''') strCode, B.strDescription, C.strAccountGroup, C.strAccountType, D.*, E.strUOMCode, E.dblLbsPerUnit, A.intCurrencyId, F.strCurrency,B.intUnnaturalAccountId     
+				  FROM  dbo.tblGLPosted AS A   
+					  INNER JOIN dbo.tblGLAccount AS B ON B.intAccountId = A.intAccountId   
+					  INNER JOIN dbo.tblGLAccountGroup AS C ON C.intAccountGroupId = B.intAccountGroupId  
+					  LEFT JOIN dbo.tblGLTempCOASegment AS D ON D.intAccountId = B.intAccountId  
+					  LEFT JOIN dbo.tblGLAccountUnit AS E ON E.intAccountUnitId = B.intAccountUnitId  
+					  INNER JOIN dbo.tblSMCurrency AS F ON F.intCurrencyID= A.intCurrencyId 
+				)T0 LEFT JOIN (	
+					SELECT B.intAccountId intUnAccountId,B.strAccountId strUnAccountId,B.strDescription strUnDescription, C.strAccountGroup strUnAccountGroup, C.strAccountType strUnAccountType,Location UnLocation, [Primary Account] [UnPrimary Account] FROM
+					dbo.tblGLAccount B
+					INNER JOIN dbo.tblGLAccountGroup AS C ON C.intAccountGroupId = B.intAccountGroupId  
+					LEFT JOIN dbo.tblGLTempCOASegment AS D ON D.intAccountId = B.intAccountId  
+					LEFT JOIN dbo.tblGLAccountUnit AS E ON E.intAccountUnitId = B.intAccountUnitId  
+				)T1 ON T0.intUnnaturalAccountId = T1.intUnAccountId')
 	END
 
 	
