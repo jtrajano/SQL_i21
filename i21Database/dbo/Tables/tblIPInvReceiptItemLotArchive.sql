be1b06a436912ef12d0449b52aa1984a75371fcd
Vinoth Kumar T <vinoth.t@irely.com>
2022-03-14 22:33:34 +0530

    IN-1003

--- i21Database/dbo/Tables/tblIPInvReceiptItemLotArchive.sql
+++ i21Database/dbo/Tables/tblIPInvReceiptItemLotArchive.sql
@@ -13,6 +13,7 @@
 	dblTareWeight				NUMERIC(18, 6),
 	dblNetWeight				NUMERIC(18, 6),
 	strWeightUOM				NVARCHAR(50) COLLATE Latin1_General_CI_AS,
+	strLotPrimaryStatus			NVARCHAR(50) COLLATE Latin1_General_CI_AS,
 
 	CONSTRAINT [PK_tblIPInvReceiptItemLotArchive_intStageReceiptItemLotId] PRIMARY KEY (intStageReceiptItemLotId),
 	CONSTRAINT [FK_tblIPInvReceiptItemLotArchive_tblIPInvReceiptArchive_intStageReceiptId] FOREIGN KEY (intStageReceiptId) REFERENCES [tblIPInvReceiptArchive](intStageReceiptId) ON DELETE CASCADE
--- i21Database/dbo/Tables/tblIPInvReceiptItemLotStage.sql
+++ i21Database/dbo/Tables/tblIPInvReceiptItemLotStage.sql
@@ -13,6 +13,7 @@
 	dblTareWeight				NUMERIC(18, 6),
 	dblNetWeight				NUMERIC(18, 6),
 	strWeightUOM				NVARCHAR(50) COLLATE Latin1_General_CI_AS,
+	strLotPrimaryStatus			NVARCHAR(50) COLLATE Latin1_General_CI_AS,
 
 	CONSTRAINT [PK_tblIPInvReceiptItemLotStage_intStageReceiptItemLotId] PRIMARY KEY (intStageReceiptItemLotId),
 	CONSTRAINT [FK_tblIPInvReceiptItemLotStage_tblIPInvReceiptStage_intStageReceiptId] FOREIGN KEY (intStageReceiptId) REFERENCES [tblIPInvReceiptStage](intStageReceiptId) ON DELETE CASCADE
