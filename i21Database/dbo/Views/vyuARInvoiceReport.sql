983df032857afb4ab225370dc19ebfde6bb7b600
DESKTOP-0Q1GJ6F\iRelyNUC14 <kelvin.quinto@irely.com>
2022-03-14 14:20:48 +0800

    AR-14402
    (cherry picked from commit aae69abe077dae46d7c59af1dd8958005eae51af)
    (cherry picked from commit 575eb3b66af194171a9e9d5f22e8cdd4758c33e4)

--- i21Database/dbo/Views/vyuARInvoiceReport.sql
+++ i21Database/dbo/Views/vyuARInvoiceReport.sql
@@ -315,21 +315,19 @@ OUTER APPLY (
 	  AND ysnProcessed = 1
 	  AND intInvoiceId = INV.intOriginalInvoiceId
 ) PROVISIONAL
-OUTER APPLY (
-	SELECT strTicketNumbers = LEFT(strTicketNumber, LEN(strTicketNumber) - 1) COLLATE Latin1_General_CI_AS
+LEFT JOIN (
+	SELECT intInvoiceId		= ID.intInvoiceId
+		 , strTicketNumbers = STRING_AGG(T.strTicketNumber, ', ')
 	FROM (
-		SELECT CAST(T.strTicketNumber AS VARCHAR(200))  + ', '
-		FROM dbo.tblARInvoiceDetail ID WITH(NOLOCK)		
-		INNER JOIN (
-			SELECT intTicketId
-				 , strTicketNumber 
-			FROM dbo.tblSCTicket WITH(NOLOCK)
-		) T ON ID.intTicketId = T.intTicketId
-		WHERE ID.intInvoiceId = INV.intInvoiceId
-		GROUP BY ID.intInvoiceId, ID.intTicketId, T.strTicketNumber
-		FOR XML PATH ('')
-	) INV (strTicketNumber)
-) SCALETICKETS
+		SELECT DISTINCT ID.intInvoiceId
+			 , ID.intTicketId 
+		FROM tblARInvoiceDetail ID
+		WHERE ID.intTicketId IS NOT NULL
+		GROUP BY ID.intInvoiceId, ID.intTicketId
+	) ID
+	INNER JOIN tblSCTicket T ON ID.intTicketId = T.intTicketId
+	GROUP BY ID.intInvoiceId
+) SCALETICKETS ON SCALETICKETS.intInvoiceId = INV.intInvoiceId
 LEFT JOIN(
 	SELECT intInvoiceId, strAddonDetailKey 
 	FROM dbo.tblARInvoiceDetail WITH(NOLOCK)
