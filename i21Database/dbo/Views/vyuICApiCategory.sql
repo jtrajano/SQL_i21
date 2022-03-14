f2ff49c4e89674f00fd12691334061cf8021dd26
Wendell Wayne Estrada <wendell.estrada@irely.com>
2022-03-14 15:10:27 +0800

    ICM-2

--- i21Database/dbo/Views/vyuICApiCategory.sql
+++ i21Database/dbo/Views/vyuICApiCategory.sql
@@ -2,5 +2,20 @@ CREATE VIEW [dbo].[vyuICApiCategory]
 AS
 
 SELECT c.intCategoryId, c.strCategoryCode, c.strDescription, c.ysnRetailValuation, lob.strLineOfBusiness, c.dtmDateCreated, c.dtmDateModified
+, COALESCE(updated.dtmDate, created.dtmDate, c.dtmDateModified, c.dtmDateCreated) dtmDateLastUpdated
 FROM tblICCategory c
 LEFT OUTER JOIN tblSMLineOfBusiness lob ON lob.intLineOfBusinessId = c.intLineOfBusinessId
+OUTER APPLY (
+	SELECT TOP 1 au.dtmDate
+	FROM vyuApiRecordAudit au
+	WHERE au.intRecordId = c.intCategoryId
+		AND au.strAction = 'Created'
+		AND au.strNamespace = 'Inventory.view.Category'
+) created
+OUTER APPLY (
+	SELECT TOP 1 au.dtmDate
+	FROM vyuApiRecordAudit au
+	WHERE au.intRecordId = c.intCategoryId
+		AND au.strAction = 'Updated'
+		AND au.strNamespace = 'Inventory.view.Category'
+) updated
