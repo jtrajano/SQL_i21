b4cb22f2e83c27c2ba96cc2e96b9cf7e90575e82
Jeff Trajano <jeffrey.trajano@irely.com>
2022-03-14 17:34:00 +0800

    GL-8808

--- i21Database/Scripts/Post-Deployment/AP/PopulateVoucherPayToBankAccount.sql
+++ i21Database/Scripts/Post-Deployment/AP/PopulateVoucherPayToBankAccount.sql
@@ -1,3 +1,5 @@
+GO
+
 PRINT N'START: POPULATING VOUCHER PAY TO BANK ACCOUNT'
 --POPULATE EMPTY VOUCHER PAY TO ACCOUNT TO VENDOR'S DEFAULT
 UPDATE B
@@ -7,3 +9,23 @@ INNER JOIN tblEMEntityEFTInformation EFT ON EFT.intEntityId = B.intEntityVendorI
 WHERE B.intPayToBankAccountId IS NULL
 
 PRINT N'SUCCESS: POPULATING VOUCHER PAY TO BANK ACCOUNT'
+
+GO
+PRINT N'START: POPULATING BANK TRANSACTION EFT ACCOUNT'
+
+IF NOT EXISTS (SELECT 1 FROM tblCMDataFixLog WHERE strDescription = 'Update NULL tblCMBankTransaction.intEFTInfo')
+BEGIN
+    UPDATE C SET intEFTInfoId = B.intPayToBankAccountId 
+    FROM tblCMBankTransaction C 
+    CROSS APPLY(
+        SELECT TOP 1 intPayToBankAccountId FROM
+        tblAPPayment P JOIN tblAPBill B ON B.intPaymentId = P.intPaymentId
+        WHERE P.strPaymentRecordNum = C.strTransactionId 
+        AND B.intPayToBankAccountId IS NOT NULL 
+        AND C.intEFTInfoId IS NULL
+    )BP
+    INSERT INTO tblCMDataFixLog(dtmDate,strDescription,intRowsAffected) SELECT GETDATE(), 'Update NULL tblCMBankTransaction.intEFTInfo', @@ROWCOUNT
+END
+
+PRINT N'SUCCESS: POPULATING BANK TRANSACTION EFT ACCOUNT'
+GO
