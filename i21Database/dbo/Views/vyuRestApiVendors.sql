35cf7ea8e7c27b20c35192845daa6464f8ce30a7
Wendell Wayne Estrada <wendell.estrada@irely.com>
2022-03-14 15:10:27 +0800

    ICM-2

--- i21Database/dbo/Views/vyuRestApiVendors.sql
+++ i21Database/dbo/Views/vyuRestApiVendors.sql
@@ -34,7 +34,8 @@ SELECT
 	-- CAST(CASE WHEN ISNULL(terminal.ysnTransportTerminal, 0) = 0 AND vendor.ysnTransportTerminal = 1 THEN 1 ELSE 0 END AS BIT) ysnSupplier,
 	vendor.ysnTransportTerminal ysnSupplier,
 	entityLocation.intShipViaId,
-	shipVia.strShipVia
+	shipVia.strShipVia,
+	COALESCE(updated.dtmDate, vendor.dtmLastModified, created.dtmDate, vendor.dtmCreated) dtmDateLastUpdated
 FROM tblAPVendor vendor
 JOIN tblEMEntity entity ON entity.intEntityId = vendor.intEntityId
 JOIN tblEMEntityType entityType ON entityType.intEntityId = entity.intEntityId
@@ -53,6 +54,21 @@ OUTER APPLY (
 	FROM vyuApiTransportTerminals t
 	WHERE t.intEntityId = vendor.intEntityId
 ) terminal
+OUTER APPLY (
+	SELECT TOP 1 au.dtmDate
+	FROM vyuApiRecordAudit au
+	WHERE au.intRecordId = vendor.intEntityId
+		AND au.strAction = 'Created'
+		AND au.strNamespace = 'EntityManagement.view.Entity'
+) created
+OUTER APPLY (
+	SELECT TOP 1 au.dtmDate
+	FROM vyuApiRecordAudit au
+	WHERE au.intRecordId = vendor.intEntityId
+		AND au.strAction = 'Updated'
+		AND au.strNamespace = 'EntityManagement.view.Entity'
+) updated
+
 --ORDER BY v.strName ASC, v.ysnActive DESC, v.dtmOriginationDate ASC
 
 GO
