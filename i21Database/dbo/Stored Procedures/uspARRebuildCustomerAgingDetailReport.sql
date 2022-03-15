c0bc8f758851ce1fed6d43f4bf9f4c406153d237
Edsel Roldan Magura <edsel.magura@iRely.com>
2022-03-15 11:27:21 +0800

    AR-13720

---
+++ i21Database/dbo/Stored Procedures/uspARRebuildCustomerAgingDetailReport.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[uspARRebuildCustomerAgingDetailReport]
+       @intEntityUserId INT = 0
+AS
+
+	EXEC uspARCustomerAgingDetailAsOfDateReport @intEntityUserId = @intEntityUserId
+
+GO
