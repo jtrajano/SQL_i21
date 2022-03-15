317fe21fe0a1b3c302fc299dac92a572272c0ad9
Girome Andaya <girome.andaya@iRely.com>
2022-03-15 13:27:54 +0800

    TR-1818
    (cherry picked from commit a10ceacff7e94d5bcfcc15e53d4b34aef7004c54)

--- i21Database/dbo/Views/vyuTRShipViaTariffMileage.sql
+++ i21Database/dbo/Views/vyuTRShipViaTariffMileage.sql
@@ -12,7 +12,11 @@ SELECT TA.intEntityTariffId
 , FM.intToMiles
 , FM.dblCostRatePerUnit
 , FM.dblInvoiceRatePerUnit
+, FC.intEntityTariffCategoryId
+, CC.strCategoryCode
 FROM tblEMEntityTariff TA 
 INNER JOIN tblSMShipVia SV ON SV.intEntityId = TA.intEntityId
 LEFT JOIN tblEMEntityTariffType TT ON TT.intEntityTariffTypeId = TA.intEntityTariffTypeId
 LEFT JOIN tblEMEntityTariffMileage FM ON FM.intEntityTariffId = TA.intEntityTariffId
+LEFT JOIN tblEMEntityTariffCategory FC ON FC.intEntityTariffId = TA.intEntityTariffId
+LEFT JOIN tblICCategory CC ON FC.intCategoryId = CC.intCategoryId
