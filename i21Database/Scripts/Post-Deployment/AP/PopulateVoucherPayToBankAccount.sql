e6863ad1c0604411d20dbaa0708febd3d76b0449
Mon Gonzales <mon.gonzales@irely.com>
2022-03-15 10:36:11 +0800

    Fix build issue

--- i21Database/Scripts/Post-Deployment/AP/PopulateVoucherPayToBankAccount.sql
+++ i21Database/Scripts/Post-Deployment/AP/PopulateVoucherPayToBankAccount.sql
@@ -11,21 +11,21 @@ WHERE B.intPayToBankAccountId IS NULL
 PRINT N'SUCCESS: POPULATING VOUCHER PAY TO BANK ACCOUNT'
 
 GO
-PRINT N'START: POPULATING BANK TRANSACTION EFT ACCOUNT'
+--PRINT N'START: POPULATING BANK TRANSACTION EFT ACCOUNT'
 
-IF NOT EXISTS (SELECT 1 FROM tblCMDataFixLog WHERE strDescription = 'Update NULL tblCMBankTransaction.intEFTInfo')
-BEGIN
-    UPDATE C SET intEFTInfoId = B.intPayToBankAccountId 
-    FROM tblCMBankTransaction C 
-    CROSS APPLY(
-        SELECT TOP 1 intPayToBankAccountId FROM
-        tblAPPayment P JOIN tblAPBill B ON B.intPaymentId = P.intPaymentId
-        WHERE P.strPaymentRecordNum = C.strTransactionId 
-        AND B.intPayToBankAccountId IS NOT NULL 
-        AND C.intEFTInfoId IS NULL
-    )BP
-    INSERT INTO tblCMDataFixLog(dtmDate,strDescription,intRowsAffected) SELECT GETDATE(), 'Update NULL tblCMBankTransaction.intEFTInfo', @@ROWCOUNT
-END
+--IF NOT EXISTS (SELECT 1 FROM tblCMDataFixLog WHERE strDescription = 'Update NULL tblCMBankTransaction.intEFTInfo')
+--BEGIN
+--    UPDATE C SET intEFTInfoId = B.intPayToBankAccountId 
+--    FROM tblCMBankTransaction C 
+--    CROSS APPLY(
+--        SELECT TOP 1 intPayToBankAccountId FROM
+--        tblAPPayment P JOIN tblAPBill B ON B.intPaymentId = P.intPaymentId
+--        WHERE P.strPaymentRecordNum = C.strTransactionId 
+--        AND B.intPayToBankAccountId IS NOT NULL 
+--        AND C.intEFTInfoId IS NULL
+--    )BP
+--    INSERT INTO tblCMDataFixLog(dtmDate,strDescription,intRowsAffected) SELECT GETDATE(), 'Update NULL tblCMBankTransaction.intEFTInfo', @@ROWCOUNT
+--END
 
-PRINT N'SUCCESS: POPULATING BANK TRANSACTION EFT ACCOUNT'
+--PRINT N'SUCCESS: POPULATING BANK TRANSACTION EFT ACCOUNT'
 GO
