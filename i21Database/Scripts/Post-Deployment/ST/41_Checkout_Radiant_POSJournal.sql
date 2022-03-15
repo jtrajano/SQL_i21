3384732997df320c4c031a5aec2b7787dcf5dc0e
nelsonreyes19 <nelson.reyes@irely.com>
2022-03-15 08:46:57 +0800

    ST-1229

--- i21Database/Scripts/Post-Deployment/ST/41_Checkout_Radiant_POSJournal.sql
+++ i21Database/Scripts/Post-Deployment/ST/41_Checkout_Radiant_POSJournal.sql
@@ -1,9 +1,11 @@
--GO
+GO
 DECLARE @intImportFileHeaderId INT
 DECLARE @intImportFileColumnDetailId INT
 DECLARE @intTagAttributeId INT
 DECLARE @strLayoutTitle NVARCHAR(MAX)
 SET @strLayoutTitle = 'Radiant - POSJournal'
+
+PRINT N'BEGIN - Store Register Radiant - POS Journal setup entries'
 
 --START CHECK HEADER
 IF EXISTS(SELECT 1 FROM dbo.tblSMImportFileHeader WHERE strLayoutTitle = @strLayoutTitle)
@@ -38,66 +40,67 @@ BEGIN
 END
 --END CHECK HEADER
 
-SELECT @intImportFileHeaderId = intImportFileHeaderId FROM dbo.tblSMImportFileHeader WHERE strLayoutTitle = @strLayoutTitle
+--SELECT @intImportFileHeaderId = intImportFileHeaderId FROM dbo.tblSMImportFileHeader WHERE strLayoutTitle = @strLayoutTitle
 
---LEVEL 1
-IF NOT EXISTS(SELECT 1 FROM dbo.tblSMImportFileColumnDetail WHERE intLevel = 1 AND intImportFileHeaderId = @intImportFileHeaderId)
-BEGIN
-  INSERT INTO [dbo].[tblSMImportFileColumnDetail]
-      ([intImportFileHeaderId],[intImportFileRecordMarkerId],[intLevel],[intPosition],[strXMLTag],[strTable],[strColumnName],[strDataType],[intLength],[strDefaultValue],[ysnActive],[intConcurrencyId])
-  VALUES 
-      (@intImportFileHeaderId,NULL,1,0,'NAXML-POSJournal',NULL,NULL,NULL,0,NULL,1,1)
-END
-ELSE IF EXISTS(SELECT 1 FROM dbo.tblSMImportFileColumnDetail WHERE intLevel = 1 AND intImportFileHeaderId = @intImportFileHeaderId)
-BEGIN
-SELECT @intImportFileColumnDetailId = intImportFileColumnDetailId FROM dbo.tblSMImportFileColumnDetail WHERE intImportFileHeaderId = @intImportFileHeaderId AND intLevel = 1
-  UPDATE [dbo].[tblSMImportFileColumnDetail]
-  SET [intImportFileHeaderId] = @intImportFileHeaderId
-       ,[intImportFileRecordMarkerId] = NULL
-       ,[intLevel] = 1
-       ,[intPosition] = 0
-       ,[strXMLTag] = 'NAXML-POSJournal'
-       ,[strTable] = NULL
-       ,[strColumnName] = NULL
-       ,[strDataType] = NULL
-       ,[intLength] = 0
-       ,[strDefaultValue] = NULL
-       ,[ysnActive] = 1
-       ,[intConcurrencyId] = 1
-  WHERE intImportFileColumnDetailId = @intImportFileColumnDetailId
-END
+----LEVEL 1
+--IF NOT EXISTS(SELECT 1 FROM dbo.tblSMImportFileColumnDetail WHERE intLevel = 1 AND intImportFileHeaderId = @intImportFileHeaderId)
+--BEGIN
+--  INSERT INTO [dbo].[tblSMImportFileColumnDetail]
+--      ([intImportFileHeaderId],[intImportFileRecordMarkerId],[intLevel],[intPosition],[strXMLTag],[strTable],[strColumnName],[strDataType],[intLength],[strDefaultValue],[ysnActive],[intConcurrencyId])
+--  VALUES 
+--      (@intImportFileHeaderId,NULL,1,0,'NAXML-POSJournal',NULL,NULL,NULL,0,NULL,1,1)
+--END
+--ELSE IF EXISTS(SELECT 1 FROM dbo.tblSMImportFileColumnDetail WHERE intLevel = 1 AND intImportFileHeaderId = @intImportFileHeaderId)
+--BEGIN
+--SELECT @intImportFileColumnDetailId = intImportFileColumnDetailId FROM dbo.tblSMImportFileColumnDetail WHERE intImportFileHeaderId = @intImportFileHeaderId AND intLevel = 1
+--  UPDATE [dbo].[tblSMImportFileColumnDetail]
+--  SET [intImportFileHeaderId] = @intImportFileHeaderId
+--       ,[intImportFileRecordMarkerId] = NULL
+--       ,[intLevel] = 1
+--       ,[intPosition] = 0
+--       ,[strXMLTag] = 'NAXML-POSJournal'
+--       ,[strTable] = NULL
+--       ,[strColumnName] = NULL
+--       ,[strDataType] = NULL
+--       ,[intLength] = 0
+--       ,[strDefaultValue] = NULL
+--       ,[ysnActive] = 1
+--       ,[intConcurrencyId] = 1
+--  WHERE intImportFileColumnDetailId = @intImportFileColumnDetailId
+--END
 
---Delete Rows Not Included
-DELETE FROM tblSMImportFileColumnDetail WHERE intImportFileHeaderId = @intImportFileHeaderId AND intLevel NOT IN (1)
+----Delete Rows Not Included
+--DELETE FROM tblSMImportFileColumnDetail WHERE intImportFileHeaderId = @intImportFileHeaderId AND intLevel NOT IN (1)
 
 
 
---LEVEL 1   Attributes(1x)
-SELECT @intImportFileColumnDetailId = intImportFileColumnDetailId FROM dbo.tblSMImportFileColumnDetail WHERE intImportFileHeaderId = @intImportFileHeaderId AND intLevel = 1 AND strXMLTag = 'NAXML-POSJournal'
-IF NOT EXISTS(SELECT 1 FROM dbo.tblSMXMLTagAttribute WHERE intSequence = 1 AND intImportFileColumnDetailId = @intImportFileColumnDetailId)
-BEGIN
-  INSERT INTO [dbo].[tblSMXMLTagAttribute]
-      ([intImportFileColumnDetailId],[intSequence],[strTagAttribute],[strTable],[strColumnName],[strDefaultValue],[ysnActive],[intConcurrencyId])
-  VALUES 
-      (@intImportFileColumnDetailId,1,'version',NULL,NULL,'3.4',1,1)
-END
-ELSE IF EXISTS(SELECT 1 FROM dbo.tblSMXMLTagAttribute WHERE intSequence = 1 AND intImportFileColumnDetailId = @intImportFileColumnDetailId)
-BEGIN
-  SELECT @intTagAttributeId = intTagAttributeId FROM dbo.tblSMXMLTagAttribute WHERE intSequence = 1 AND intImportFileColumnDetailId = @intImportFileColumnDetailId
-  UPDATE[dbo].[tblSMXMLTagAttribute]
-  SET [intImportFileColumnDetailId] = @intImportFileColumnDetailId
-       ,[intSequence] = 1
-       ,[strTagAttribute] = 'version'
-       ,[strTable] = NULL
-       ,[strColumnName] = NULL
-       ,[strDefaultValue] = '3.4'
-       ,[ysnActive] = 1
-       ,[intConcurrencyId] = 1
-  WHERE intTagAttributeId = @intTagAttributeId
-END
+----LEVEL 1   Attributes(1x)
+--SELECT @intImportFileColumnDetailId = intImportFileColumnDetailId FROM dbo.tblSMImportFileColumnDetail WHERE intImportFileHeaderId = @intImportFileHeaderId AND intLevel = 1 AND strXMLTag = 'NAXML-POSJournal'
+--IF NOT EXISTS(SELECT 1 FROM dbo.tblSMXMLTagAttribute WHERE intSequence = 1 AND intImportFileColumnDetailId = @intImportFileColumnDetailId)
+--BEGIN
+--  INSERT INTO [dbo].[tblSMXMLTagAttribute]
+--      ([intImportFileColumnDetailId],[intSequence],[strTagAttribute],[strTable],[strColumnName],[strDefaultValue],[ysnActive],[intConcurrencyId])
+--  VALUES 
+--      (@intImportFileColumnDetailId,1,'version',NULL,NULL,'3.4',1,1)
+--END
+--ELSE IF EXISTS(SELECT 1 FROM dbo.tblSMXMLTagAttribute WHERE intSequence = 1 AND intImportFileColumnDetailId = @intImportFileColumnDetailId)
+--BEGIN
+--  SELECT @intTagAttributeId = intTagAttributeId FROM dbo.tblSMXMLTagAttribute WHERE intSequence = 1 AND intImportFileColumnDetailId = @intImportFileColumnDetailId
+--  UPDATE[dbo].[tblSMXMLTagAttribute]
+--  SET [intImportFileColumnDetailId] = @intImportFileColumnDetailId
+--       ,[intSequence] = 1
+--       ,[strTagAttribute] = 'version'
+--       ,[strTable] = NULL
+--       ,[strColumnName] = NULL
+--       ,[strDefaultValue] = '3.4'
+--       ,[ysnActive] = 1
+--       ,[intConcurrencyId] = 1
+--  WHERE intTagAttributeId = @intTagAttributeId
+--END
 
---Delete Rows Not Included in LEVEL 1   Attributes(1x)
-DELETE FROM tblSMXMLTagAttribute WHERE intImportFileColumnDetailId = @intImportFileColumnDetailId AND intSequence NOT IN (1)
+----Delete Rows Not Included in LEVEL 1   Attributes(1x)
+--DELETE FROM tblSMXMLTagAttribute WHERE intImportFileColumnDetailId = @intImportFileColumnDetailId AND intSequence NOT IN (1)
 
+PRINT N'END - Store Register Radiant - POS Journal setup entries'
 
 GO
