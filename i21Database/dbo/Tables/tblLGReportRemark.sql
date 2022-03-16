a1451be250c0c361c3350fa7ec1954ff951f6f1b
lawrence_zabala <lawrence.zabala@irely.com>
2022-03-16 11:28:54 +0800

    LG-3205

---
+++ i21Database/dbo/Tables/tblLGReportRemark.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[tblLGReportRemark]
+(
+	intReportRemarkId INT IDENTITY NOT NULL,
+	strType NVARCHAR(50) NOT NULL,
+	intValueId INT NOT NULL,
+	intLocationId INT NULL,
+	strRemarks NVARCHAR(MAX) NULL,
+	intConcurrencyId INT NULL DEFAULT((0)), 
+    CONSTRAINT [PK_tblLGReportRemark] PRIMARY KEY ([intReportRemarkId]), 
+    CONSTRAINT [AK_tblLGReportRemark] UNIQUE (strType, intValueId, intLocationId)
+
+)
