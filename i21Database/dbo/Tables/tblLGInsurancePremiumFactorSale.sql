435f74d68341853526152288f4b0aac8b7f41d2f
lawrence_zabala <lawrence.zabala@irely.com>
2022-03-17 09:59:41 +0800

    LG-2950

---
+++ i21Database/dbo/Tables/tblLGInsurancePremiumFactorSale.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tblLGInsurancePremiumFactorSale]
+(
+	[intInsurancePremiumFactorSaleId] INT IDENTITY NOT NULL , 
+    [intInsurancePremiumFactorId] INT NOT NULL, 
+    [intFreightTermId] INT NOT NULL, 
+    [dblInsurancePercent] NUMERIC(18, 6) NULL DEFAULT ((0)), 
+    [intConcurrencyId] INT NULL DEFAULT ((0)), 
+    CONSTRAINT [PK_tblLGInsurancePremiumFactorSale] PRIMARY KEY ([intInsurancePremiumFactorSaleId]), 
+    CONSTRAINT [FK_tblLGInsurancePremiumFactorSale_tblLGInsurancePremiumFactor] FOREIGN KEY ([intInsurancePremiumFactorId]) REFERENCES [tblLGInsurancePremiumFactor]([intInsurancePremiumFactorId]) ON DELETE CASCADE, 
+    CONSTRAINT [FK_tblLGInsurancePremiumFactorSale_tblSMFreightTerms] FOREIGN KEY ([intFreightTermId]) REFERENCES [tblSMFreightTerms]([intFreightTermId])
+)
