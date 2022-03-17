969cb8df59beaf3a78a8c51cfdb7fa8951d880f7
lawrence_zabala <lawrence.zabala@irely.com>
2022-03-17 10:14:54 +0800

    LG-3205

---
+++ i21Database/dbo/Views/vyuLGGetReportRemarkLocation.sql
@@ -0,0 +1,19 @@
+﻿CREATE VIEW [dbo].[vyuLGGetReportRemarkLocation]
+
+AS
+
+SELECT intLocationId = IL.intItemLocationId
+	, strLocation = CL.strLocationName
+	, intReferenceId = IL.intItemId
+	, strType = 'Item'
+FROM tblICItemLocation IL
+JOIN tblSMCompanyLocation CL ON CL.intCompanyLocationId = IL.intLocationId 
+
+UNION ALL SELECT intLocationId = EL.intEntityLocationId
+	, strLocation = EL.strLocationName
+	, intReferenceId = EL.intEntityId
+	, strType = 'Entity'
+FROM tblEMEntityLocation EL
+WHERE EL.intEntityId IN (
+	SELECT intEntityId FROM tblEMEntityType WHERE strType IN ('Vendor', 'Customer')
+)
---
+++ i21Database/dbo/Views/vyuLGGetReportRemarkValue.sql
@@ -0,0 +1,16 @@
+﻿CREATE VIEW [dbo].[vyuLGGetReportRemarkValue]
+
+AS
+
+SELECT intValueId = intItemId
+	, strValue = strItemNo
+	, strType = 'Item'
+FROM tblICItem I
+
+UNION ALL SELECT intValueId = EL.intEntityId
+	, strValue = EL.strName
+	, strType = 'Entity'
+FROM tblEMEntity EL
+WHERE EL.intEntityId IN (
+	SELECT intEntityId FROM tblEMEntityType WHERE strType IN ('Vendor', 'Customer')
+)
