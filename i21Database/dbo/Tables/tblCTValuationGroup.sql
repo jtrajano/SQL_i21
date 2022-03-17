6735783dca2840af7534cee56f29cf5843a0e131
lawrence_zabala <lawrence.zabala@irely.com>
2022-03-17 09:16:34 +0800

    CT-5996

---
+++ i21Database/dbo/Tables/tblCTValuationGroup.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[tblCTValuationGroup]
+(
+	[intValuationGroupId] INT IDENTITY NOT NULL, 
+    [strName] NVARCHAR(50) NOT NULL, 
+    [strDescription] NVARCHAR(100) NULL, 
+    [intConcurrencyId] INT NULL DEFAULT((0)), 
+    CONSTRAINT [PK_tblCTValuationGroup] PRIMARY KEY ([intValuationGroupId]) 
+)
---
+++ i21Database/dbo/Tables/tblCTValuationGroupDetail.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[tblCTValuationGroupDetail]
+(
+	[intValuationGroupDetailId] INT IDENTITY NOT NULL , 
+    [intValuationGroupId] INT NOT NULL, 
+    [dblPremium] NUMERIC(18, 6) NULL, 
+    [intCurrencyId] INT NULL, 
+    [intUOMId] INT NULL, 
+    [dtmStartDate] DATETIME NULL, 
+    [dtmEndDate] DATETIME NULL, 
+    [intConcurrencyId] INT NULL DEFAULT ((0)), 
+    CONSTRAINT [PK_tblCTValuationGroupDetail] PRIMARY KEY ([intValuationGroupDetailId]), 
+    CONSTRAINT [FK_tblCTValuationGroupDetail_tblCTValuationGroup] FOREIGN KEY ([intValuationGroupDetailId]) REFERENCES [tblCTValuationGroup]([intValuationGroupId]) ON DELETE CASCADE, 
+    CONSTRAINT [FK_tblCTValuationGroupDetail_tblSMCurrency] FOREIGN KEY ([intCurrencyId]) REFERENCES [tblSMCurrency]([intCurrencyID]), 
+    CONSTRAINT [FK_tblCTValuationGroupDetail_tblICUnitMeasure] FOREIGN KEY ([intUOMId]) REFERENCES [tblICUnitMeasure]([intUnitMeasureId])
+)
