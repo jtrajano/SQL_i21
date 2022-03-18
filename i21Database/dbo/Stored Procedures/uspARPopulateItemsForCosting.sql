0a84455ad60ac8b343e4c0959c28eff868077691
Jeremiah Villanueva <jeremiah.villanueva@irely.com>
2022-03-18 18:52:31 +0800

    AR-14432

--- i21Database/dbo/Stored Procedures/uspARPopulateItemsForCosting.sql
+++ i21Database/dbo/Stored Procedures/uspARPopulateItemsForCosting.sql
@@ -147,7 +147,7 @@ WHERE ARID.[strTransactionType] IN ('Invoice', 'Credit Memo', 'Credit Note', 'Ca
 	AND ARID.[intStorageScheduleTypeId] IS NULL
 	AND (ARID.intLoadId IS NULL OR (ARID.intLoadId IS NOT NULL AND LGL.[intPurchaseSale] NOT IN (2, 3)))
 	AND (ARID.[ysnFromProvisional] = 0 OR (ARID.[ysnFromProvisional] = 1 AND ((ARID.[dblQtyShipped] <> ARIDP.[dblQtyShipped] AND ARID.[intInventoryShipmentItemId] IS NULL)) OR ((ARID.[dblQtyShipped] > ARIDP.[dblQtyShipped] AND ARID.[intInventoryShipmentItemId] IS NOT NULL))))
-	AND ISNULL(T.[intTicketTypeId], 0) <> 
+	AND ISNULL(T.[intTicketTypeId], 0) <> 9 
 
 --Bundle Items
 INSERT INTO ##ARItemsForCosting
