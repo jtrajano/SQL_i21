46678175411a1299daba0e6f7b0ac76144a7daec
LexPeregrino <lex.peregrino@irely.com>
2022-03-19 17:13:49 +0800

    CT-5924 SQL v2

--- i21Database/dbo/Tables/tblCTCompanyPreference.sql
+++ i21Database/dbo/Tables/tblCTCompanyPreference.sql
@@ -117,6 +117,7 @@
 	ysnEnableItemQualityFields BIT NOT NULL DEFAULT 0,
     [intFinanceCostId] INT NULL, 
 	ysnEnableFXForwardRequestOnSequence bit not null default 0,
+	ysnEnableVendorCertificationProgram BIT NOT NULL DEFAULT 0,
     CONSTRAINT [PK_tblCTCompanyPreference_intCompanyPreferenceId] PRIMARY KEY CLUSTERED ([intCompanyPreferenceId] ASC),
 	CONSTRAINT [FK_tblCTCompanyPreference_tblSMCurrency_intCleanCostCurrencyId_intCurrencyId] FOREIGN KEY ([intCleanCostCurrencyId]) REFERENCES [tblSMCurrency]([intCurrencyID]),
 	CONSTRAINT [FK_tblCTCompanyPreference_tblICUnitMeasure_intCleanCostUOMId_intUnitMeasureId] FOREIGN KEY ([intCleanCostUOMId]) REFERENCES [tblICUnitMeasure]([intUnitMeasureId]),
