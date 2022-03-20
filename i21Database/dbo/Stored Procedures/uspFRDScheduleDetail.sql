d6c3960d862b867766c737d33877c91c777e919b
Dom Caoile <dom.caoile.02@gmail.com>
2022-03-20 23:44:33 +0800

    FRD-1718

--- i21Database/dbo/Stored Procedures/uspFRDScheduleDetail.sql
+++ i21Database/dbo/Stored Procedures/uspFRDScheduleDetail.sql
@@ -1,5 +1,5 @@
 CREATE PROCEDURE  [dbo].[uspFRDScheduleDetail]
-	@intHierarchyScheduleId INT
+    @intHierarchyScheduleId INT  
            
 AS          
 BEGIN          
@@ -8,14 +8,14 @@ SET QUOTED_IDENTIFIER OFF
 SET ANSI_NULLS ON          
 SET NOCOUNT ON           
         
- --MAIN DATA SOURCE        
- 
+ --MAIN DATA SOURCE          
+ DELETE tblFRReportHierarchyScheduleDetails WHERE intHierarchyScheduleId = @intHierarchyScheduleId  
  ---------------------------------------------------------------------------------------------------------------------------------------          
- INSERT INTO tblFRReportHierarchyScheduleDetails (intReportId,intHierarchyScheduleId,intReportHierarchyId,intReportHierarchyDetailId,dtmAsOfDate,dtmSchedDate)         
- SELECT intReportId,intHierarchyScheduleId,intReportHierarchyDetailId,T0.intReportHierarchyDetailId,dtmAsOfDate,dtmScheduleDate FROM tblFRReportHierarchyDetail T0
- INNER JOIN  tblFRReportHierarchySchedule T1
- ON T0.intReportHierarchyId = T1.intReportHierarchyId
- WHERE T1.intHierarchyScheduleId = @intHierarchyScheduleId
+ INSERT INTO tblFRReportHierarchyScheduleDetails (intReportId,intHierarchyScheduleId,intReportHierarchyId,intReportHierarchyDetailId,dtmAsOfDate,dtmSchedDate)           
+ SELECT intReportId,intHierarchyScheduleId,T0.intReportHierarchyId,T0.intReportHierarchyDetailId,dtmAsOfDate,dtmScheduleDate FROM tblFRReportHierarchyDetail T0  
+ INNER JOIN  tblFRReportHierarchySchedule T1  
+ ON T0.intReportHierarchyId = T1.intReportHierarchyId  
+ WHERE T1.intHierarchyScheduleId = @intHierarchyScheduleId  
         
  SELECT @@ROWCOUNT        
  END      
