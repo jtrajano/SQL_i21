4d2b9853a07d89be8a285c4b863888a3b3154af4
Vinoth Kumar T <vinoth.t@irely.com>
2022-03-20 15:42:09 +0530

    MFG-4503

--- i21Database/dbo/Tables/tblMFLotInventory.sql
+++ i21Database/dbo/Tables/tblMFLotInventory.sql
@@ -17,6 +17,9 @@
 	,intWorkOrderId int
 	,intManufacturingProcessId int
 	,intLoadId int
+	,ysnPrinted BIT NOT NULL CONSTRAINT [DF_tblMFLotInventory_ysnPrinted] DEFAULT 0
+	,dtmLastPrinted DATETIME
+	,intPrintedById INT
 	,CONSTRAINT [PK_tblMFLotInventory] PRIMARY KEY ([intLotInventoryId]), 
 	CONSTRAINT [AK_tblMFLotInventory_intLotId] UNIQUE ([intLotId]), 
 	CONSTRAINT [FK_tblMFLotInventory_tblICLot] FOREIGN KEY ([intLotId]) REFERENCES [tblICLot]([intLotId]) ON DELETE CASCADE,
