bc5d7550f1750ddd2787b03161216b0247545e57
Roudinee Panteria <roudinee.panteria@irely.com>
2022-03-21 19:38:36 +0800

    QC-578

--- i21Database/dbo/Stored Procedures/uspQMGetEmailInfo.sql
+++ i21Database/dbo/Stored Procedures/uspQMGetEmailInfo.sql
@@ -5,20 +5,14 @@
 		@intCurrentUserEntityId int
 AS 
 BEGIN
-	DECLARE @strNumber					NVARCHAR(100),
-			@intEntityId				INT,
+	DECLARE @intEntityId				INT,
 			@strEntityName				NVARCHAR(200),
 			@body						NVARCHAR(MAX) = '',
 			@Subject					NVARCHAR(MAX) = '',
 			@Filter						NVARCHAR(MAX) = '',
 			@strIds						NVARCHAR(MAX),
-			@intUniqueId				INT,
-			@Id							INT,
-			@routeScreen				NVARCHAR(50),
 			@intSalespersonId			INT,
-			@strCustomerContract		NVARCHAR(50),
-			@strThanks					NVARCHAR(MAX) = 'Thank you for your business.',
-			@ysnContractSlspnOnEmail	BIT = 0
+			@strThanks					NVARCHAR(MAX) = 'Thank you for your business.'
 
 	DECLARE @loop TABLE
 	(
@@ -29,105 +23,55 @@ BEGIN
 		intSalespersonId INT
 	)
 
-	IF @strMailType = 'SampleInstruction'
+	IF @strMailType = 'Sample Instruction'
 	BEGIN
-		SET @routeScreen = 'Contract'
 		INSERT INTO @loop
 		SELECT CD.intContractHeaderId,CH.intEntityId,CH.strContractNumber +'-'+ CAST(CD.intContractSeq AS NVARCHAR(10)) ,CH.intSalespersonId 
 		FROM tblCTContractDetail CD
 		INNER JOIN tblCTContractHeader CH ON CD.intContractHeaderId = CH.intContractHeaderId
 		WHERE CD.intContractDetailId IN (SELECT * FROM  dbo.fnSplitString(@strId,','))
 	END
-
-	IF @strMailType = 'ArrivalForm'
+	ELSE IF @strMailType = 'Sample Print'
 	BEGIN
-		SET @routeScreen = 'Quality'
 		INSERT INTO @loop
 		SELECT intSampleId,intPartyName,strContractNumber,intSalespersonId FROM vyuQMSampleList WHERE intSampleId IN (SELECT * FROM  dbo.fnSplitString(@strId,','))
 	END
 
 	SELECT @intEntityId = intEntityId FROM @loop
 	SELECT @strEntityName = strName FROM tblEMEntity WHERE intEntityId = @intEntityId
-
 	SELECT @intSalespersonId = intSalespersonId FROM @loop
 
-	IF @strMailType = 'Contract'
-	BEGIN
-		SELECT	@strIds	=	STUFF(															
-										(
-											SELECT	DISTINCT												
-													'|^|' +	LTRIM(intEntityContactId)
-											FROM	vyuCTEntityToContact 
-											WHERE	intEntityId = @intEntityId
-											AND		ISNULL(strEmail,'') <> ''
-											AND		strEmailDistributionOption LIKE '%Contracts%'
-											FOR XML PATH('')
-										),1,3, ''
-									)
+	SELECT	@strIds	=	STUFF(															
+								(
+									SELECT	DISTINCT												
+											'|^|' +	LTRIM(intEntityContactId)
+									FROM	vyuCTEntityToContact 
+									WHERE	intEntityId = @intEntityId
+									AND		ISNULL(strEmail,'') <> ''
+									FOR XML PATH('')
+								),1,3, ''
+							)
 				
-		FROM	vyuCTEntityToContact CH
-		WHERE	intEntityId = @intEntityId
-	END
-	ELSE
-	BEGIN
-		SELECT	@strIds	=	STUFF(															
-										(
-											SELECT	DISTINCT												
-													'|^|' +	LTRIM(intEntityContactId)
-											FROM	vyuCTEntityToContact 
-											WHERE	intEntityId = @intEntityId
-											AND		ISNULL(strEmail,'') <> ''
-											FOR XML PATH('')
-										),1,3, ''
-									)
-				
-		FROM	vyuCTEntityToContact CH
-		WHERE	intEntityId = @intEntityId
-	END
+	FROM	vyuCTEntityToContact CH
+	WHERE	intEntityId = @intEntityId
+
 	IF EXISTS ( 
 	SELECT	DISTINCT	1 
 	FROM	vyuCTEntityToContact 
 	WHERE	intEntityId = @intEntityId
 	AND		ISNULL(strEmail,'') <> '' 
 	AND     strEmail NOT LIKE  '_%@__%.__%' )
-
 	BEGIN 
 		RAISERROR('Entity has invalid Email Address.', 16, 1);
 		RETURN;
 	END
 
-	SELECT	@strNumber	=	STUFF(															
-									(
-										SELECT	DISTINCT												
-												', ' +	LTRIM(strNumber)
-										FROM	@loop
-										FOR XML PATH('')
-									),1,2, ''
-								)
-				
-	FROM	@loop CH
-
-	SET @Subject = @strMailType + ' - ' + @strNumber
-
-	IF @strMailType = 'SampleInstruction'
-		SELECT @strCustomerContract = isnull(strCustomerContract, '') FROM tblCTContractHeader WHERE intContractHeaderId IN (SELECT TOP 1 Id FROM @loop)
-	ELSE IF @strMailType = 'ArrivalForm'
-		SELECT @strCustomerContract = isnull(strCustomerContract, '') FROM tblCTContractHeader WHERE intContractHeaderId IN (SELECT intLinkContractHeaderId FROM vyuQMSampleList WHERE intSampleId = (SELECT TOP 1 Id FROM @loop))
-
-	IF(@strCustomerContract = '')
-		SET @Subject = 'Contract' + ' - ' + @strNumber + ' - Sample Instruction'
-	ELSE
-		SET @Subject = 'Contract' + ' - ' + @strNumber + ' - Sample Instruction - Your ref. no. ' + @strCustomerContract
+	SET @Subject = @strMailType
 
 	SET @body +='<!DOCTYPE html>'
 	SET @body +='<html>'
 	SET @body +='<body>Dear <strong>'+@strEntityName+'</strong>, <br><br>'
-
-	IF @strCustomerContract = ''
-		SELECT  @body += 'Please find attached the sample instructions for contract.'
-	ELSE
-		SELECT  @body += 'Please find attached the sample instructions for contract - ' + @strNumber + '(Your ref. no. '+ @strCustomerContract +')'
-
+	SET  @body += 'Please find the attached ' + LOWER(@strMailType) + '.'
 	SET @body += '<br>'
 	SET @body +=@strThanks+'<br><br>'
 	SET @body +='Sincerely, <br>'
