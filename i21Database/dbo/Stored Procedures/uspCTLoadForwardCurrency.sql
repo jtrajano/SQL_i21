b2360fbecf0d18b96d47e33b4e06af4a3885f515
Bryan-PC <013.bryan@gmail.com>
2022-03-22 19:59:54 +0800

    CT-6945

--- i21Database/dbo/Stored Procedures/uspCTLoadForwardCurrency.sql
+++ i21Database/dbo/Stored Procedures/uspCTLoadForwardCurrency.sql
@@ -4,19 +4,25 @@ AS
 	SELECT	 FC.*
 	FROM tblCTContractDetail CD
 	INNER JOIN (
-		SELECT	  intFutOptTransactionHeaderId
-				, intFutOptTransactionId
-				, strInternalTradeNo
-				, strFromCurrency = strBuyCurrency
-				, strToCurrency = strSellCurrency
-				, dtmMaturityDate
-				, dtmTransactionDate = dtmTradeDate
-				, dblBuyAmount
-				, dblSellAmount
-				, strApprovalStatus
-				, intContractDetailId
-				, dblContractRate
-				,intConcurrencyId = 1
-		FROM vyuRKGetOtcForwards
-		WHERE intContractDetailId = @intContractDetailId
+		SELECT	  FD.intFutOptTransactionHeaderId
+				, FD.intFutOptTransactionId
+				, FD.strInternalTradeNo
+				, strFromCurrency = FD.strBuyCurrency
+				, strToCurrency = FD.strSellCurrency
+				, FD.dtmMaturityDate
+				, dtmTransactionDate = FD.dtmTradeDate
+				, FD.dblBuyAmount
+				, FD.dblSellAmount
+				, FD.strApprovalStatus
+				, FD.intContractDetailId
+				, FD.dblContractRate
+				, O.strOrderType
+				, der.dblLimitRate
+				, der.dtmMarketDate
+				, der.ysnGTC
+				, intConcurrencyId = 1
+		FROM vyuRKGetOtcForwards FD
+		INNER JOIN tblRKFutOptTransaction der on der.intFutOptTransactionId = FD.intFutOptTransactionId
+		LEFT JOIN tblCTOrderTypeFX O on O.intOrderTypeId = der.intOrderTypeId
+		WHERE FD.intContractDetailId = @intContractDetailId
 	) FC on FC.intContractDetailId = CD.intContractDetailId
