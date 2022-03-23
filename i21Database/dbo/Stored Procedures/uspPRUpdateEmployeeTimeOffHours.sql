9af1818eda9fa7f9115d78dd45884d013752c349
Rey Anthony Crisostomo <anthony.crisostomo@iRely.com>
2022-03-23 10:49:27 +0800

    PR-2624 - fixing accrued reset on award

--- i21Database/dbo/Stored Procedures/uspPRUpdateEmployeeTimeOffHours.sql
+++ i21Database/dbo/Stored Procedures/uspPRUpdateEmployeeTimeOffHours.sql
@@ -52,13 +52,9 @@ BEGIN
  --Calculate Next Award Date    
  UPDATE #tmpEmployees     
   SET dtmNextAward = CASE WHEN (strAwardPeriod = 'Start of Week') THEN    
-                        CASE WHEN CAST(DATEADD(WK, DATEDIFF(WK, 0, GETDATE()), 0) AS DATE) >= GETDATE() THEN
-							CAST(DATEADD(WK, DATEDIFF(WK,6 , GETDATE()), 0) AS DATE)
-						ELSE
-							CAST(DATEADD(WK, DATEDIFF(WK, 0, GETDATE()), 0) AS DATE) 
-						END
+                            CAST(DATEADD(WK, DATEDIFF(WK, 6, GETDATE()), 0) AS DATE)    
                     WHEN (strAwardPeriod = 'End of Week') THEN    
-                        CASE WHEN (dtmLastAward) <= CAST(DATEADD(DD, 7-(DATEPART(DW, GETDATE())), GETDATE()) AS DATE) THEN    
+                        CASE WHEN (dtmLastAward) < CAST(DATEADD(DD, 7-(DATEPART(DW, GETDATE())), GETDATE()) AS DATE) THEN    
                          DATEADD(DD, -7, CAST(DATEADD(DD, 7-(DATEPART(DW, GETDATE())), GETDATE()) AS DATE))    
                         ELSE     
                          CAST(DATEADD(DD, 7-(DATEPART(DW, GETDATE())) + 7, GETDATE()) AS DATE)    
