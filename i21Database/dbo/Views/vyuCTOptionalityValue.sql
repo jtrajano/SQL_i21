f0b9b77ae01c2bbaa33915d9758ce69f075f814b
LexPeregrino <lex.peregrino@irely.com>
2022-03-24 20:33:00 +0800

    CT-6947 v2

--- i21Database/dbo/Views/vyuCTOptionalityValue.sql
+++ i21Database/dbo/Views/vyuCTOptionalityValue.sql
@@ -10,7 +10,7 @@ FROM
 			  ,strCropYear strValue
 			  ,'cropyearcombo' strControlName
 			  ,b.strCommodityCode strDescription
-		from tblCTCropYear a
+		FROM tblCTCropYear a
 		INNER JOIN tblICCommodity b on a.intCommodityId = b.intCommodityId
 
 		UNION ALL
@@ -20,8 +20,48 @@ FROM
 			  ,strCity,
 			  'citycombo' strControlName
 			  ,strCountry
-		from tblSMCity a
+		FROM tblSMCity a
 		Inner join tblSMCountry b on a.intCountryId = b.intCountryID
+
+		UNION ALL 
+
+		SELECT intUnitMeasureId
+		      ,intUnitMeasureId
+			  ,strUnitMeasure,
+			  'packagecombo' strControlName
+			  ,strSymbol
+		FROM tblICUnitMeasure a
+
+		UNION ALL 
+
+		SELECT intFreightTermId
+		      ,intFreightTermId
+			  ,strFreightTerm,
+			  'incotermcombo' strControlName
+			  ,strDescription
+		FROM vyuSMFreightTerms a
+		WHERE ysnActive = 1
+
+		UNION ALL 
+
+		SELECT intAssociationId
+		      ,intAssociationId
+			  ,strName,
+			  'assoccombo' strControlName
+			  ,strComment
+		FROM tblCTAssociation a 
+		WHERE ysnActive = 1
+
+		UNION ALL 
+
+		SELECT intCityId
+		      ,intCountryId
+			  ,strCity,
+			  'arbitrationcombo' strControlName
+			  ,strCountry
+		FROM tblSMCity a
+		Inner join tblSMCountry b on a.intCountryId = b.intCountryID
+		where ysnArbitration = 1
 		
 	) a
 Inner join tblCTOption b on a.strControlName = b.strControlName
