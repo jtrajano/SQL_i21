758f264420d506ac34d54f5a875b2558609318f0
Wendell Wayne Estrada <wendell.estrada@irely.com>
2022-03-24 11:19:54 +0800

    ICM-2

--- i21Database/dbo/Views/vyuApiActivityAttendee.sql
+++ i21Database/dbo/Views/vyuApiActivityAttendee.sql
@@ -13,4 +13,5 @@ FROM tblSMActivityAttendee a
 JOIN vyuEMEntityCredentialContact ve ON ve.intEntityId = a.intEntityId
 JOIN tblEMEntity e ON e.intEntityId = a.intEntityId
 JOIN tblSMActivity ac ON ac.intActivityId = a.intActivityId
-WHERE ac.strType != 'Comment'
+WHERE ac.strType != 'Comment' 
+	AND (([ve].[ysnDisabled] = CAST(0 AS bit)) AND ([ve].[User] = 1)) AND ([ve].[strName] IS NOT NULL AND NOT ([ve].[strName] LIKE N''))
