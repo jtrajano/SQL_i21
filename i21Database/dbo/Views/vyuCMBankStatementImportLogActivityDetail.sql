f073bbf1973e4272112b40ecd2f15e6bfbdfd385
Jeff Trajano <jeffrey.trajano@irely.com>
2022-03-25 14:50:22 +0800

    GL-8837

--- i21Database/dbo/Views/vyuCMBankStatementImportLogActivityDetail.sql
+++ i21Database/dbo/Views/vyuCMBankStatementImportLogActivityDetail.sql
@@ -1,20 +1,25 @@
-CREATE  VIEW vyuCMBankStatementImportLogActivityDetail
-AS
-select 
-C.intId,
-A.intImportBankStatementLogId,  
-C.intLineNo,  
-B.strABRActivityId strTaskId,
-ysnError = case when strError is null then cast(0 as bit) else cast(1 as bit) end ,  
-C.strError,
-B.strBankDescription,
-strTransactionMatched = Match.strTransactionId
-from 
-tblCMBankStatementImportLog A 
-join tblCMBankStatementImportLogDetail C on C.intImportBankStatementLogId = A.intImportBankStatementLogId
-left join tblCMABRActivity B on B.strABRActivityId = C.strABRActivityId
-OUTER APPLY(
-    SELECT TOP 1 strTransactionId FROM tblCMABRActivityMatched M
-    JOIN tblCMBankTransaction CM ON CM.intTransactionId = M.intTransactionId
-    WHERE intABRActivityId = B.intABRActivityId
+CREATE  VIEW vyuCMBankStatementImportLogActivityDetail  
+AS  
+select   
+C.intId,  
+A.intImportBankStatementLogId,    
+C.intLineNo,    
+ISNULL(C.strTaskId, B.strABRActivityId) strTaskId,  
+ysnError = case when strError is null then cast(0 as bit) else cast(1 as bit) end ,    
+C.strError,  
+ISNULL(B.strBankDescription, Task.strNotes) strBankDescription,
+strTransactionMatched = Match.strTransactionId  
+FROM   
+tblCMBankStatementImportLog A   
+join tblCMBankStatementImportLogDetail C on C.intImportBankStatementLogId = A.intImportBankStatementLogId  
+left join tblCMABRActivity B on B.strABRActivityId = C.strABRActivityId  
+OUTER APPLY(  
+    SELECT TOP 1 strTransactionId FROM tblCMABRActivityMatched M  
+    JOIN tblCMBankTransaction CM ON CM.intTransactionId = M.intTransactionId  
+    WHERE intABRActivityId = B.intABRActivityId  
 )Match
+OUTER APPLY(  
+    SELECT strNotes FROM tblCMResponsiblePartyTask
+	WHERE strTaskId = C.strTaskId
+
+)Task
