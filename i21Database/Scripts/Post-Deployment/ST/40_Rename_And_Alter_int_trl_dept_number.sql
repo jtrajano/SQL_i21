94ce32439f539f62461d95aab7edf12f8be0b7bf
nelsonreyes19 <nelson.reyes@irely.com>
2022-03-29 08:16:13 +0800

    ST-2140

--- i21Database/Scripts/Post-Deployment/ST/40_Rename_And_Alter_int_trl_dept_number.sql
+++ i21Database/Scripts/Post-Deployment/ST/40_Rename_And_Alter_int_trl_dept_number.sql
@@ -1,10 +1,14 @@
-﻿﻿PRINT 'Checking tblSTTranslogRebates for intTrlDeptNumber'
+﻿PRINT 'Checking tblSTTranslogRebates for intTrlDeptNumber'
+
 IF(EXISTS(SELECT TOP 1 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE [TABLE_NAME] = 'tblSTTranslogRebates'  and [COLUMN_NAME] = 'intTrlDeptNumber' ))
 BEGIN
 	PRINT 'EXECUTE'
-	
-	UPDATE tblSTTranslogRebates SET strTrlDeptNumber = CAST(intTrlDeptNumber AS NVARCHAR)
-	WHERE strTrlDeptNumber IS NULL
+	IF EXISTS (SELECT TOP 1 1 FROM tblSTTranslogRebates WHERE strTrlDeptNumber IS NULL AND intTrlDeptNumber IS NOT NULL)
+	BEGIN
+		UPDATE tblSTTranslogRebates SET strTrlDeptNumber = CAST(intTrlDeptNumber AS NVARCHAR)
+		WHERE strTrlDeptNumber IS NULL AND intTrlDeptNumber IS NOT NULL
+	END
 
 END
+
 PRINT 'Done tblSTTranslogRebates for intTrlDeptNumber'
