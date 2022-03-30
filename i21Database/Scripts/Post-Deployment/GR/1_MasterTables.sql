3c9a77c7cbde49809d700f5ca024acb797bc2bfe
Leslie Guevarra <leslie.arat@irely.com>
2022-03-30 14:53:42 +0800

    GRN-2944 (Calculation Types not loading in Charge and Premium Details screen)

--- i21Database/Scripts/Post-Deployment/GR/1_MasterTables.sql
+++ i21Database/Scripts/Post-Deployment/GR/1_MasterTables.sql
@@ -428,3 +428,12 @@ BEGIN
 END
 GO
 
+IF NOT EXISTS(SELECT 1 FROM tblGRCalculationType)
+BEGIN
+	INSERT INTO [tblGRCalculationType]
+	(
+		[strCalculationType]
+	) 
+	VALUES ('Range by Unit'), ('Range by Grade Reading'), ('Percentage by Item'), ('Percentage by Discount'), ('Fixed Rate')
+END
+GO
