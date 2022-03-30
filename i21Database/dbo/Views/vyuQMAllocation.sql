0bd2d856aa37b6742fcde68ecccaa4d3a3e2cf26
Jeff Trajano <jeffrey.trajano@irely.com>
2022-03-30 11:12:05 +0800

    QC-645

--- i21Database/dbo/Views/vyuQMAllocation.sql
+++ i21Database/dbo/Views/vyuQMAllocation.sql
@@ -1,10 +1,18 @@
+/*
+FOR VIEWING OF ALLOCATION IN THE QUALITY SAMPLE SCREEN
+IF THE SALES CONTRACT SELECTED ON A NEW SAMPLE HAS PURCHASE CONTRACT, GET THE SAMPLE ID
+- IF MORE THAN ONE, USER WILL MANUALLY CREATE THE SAMPLE
+*/
+
 CREATE VIEW vyuQMAllocation
 AS
 
 WITH QUERY AS(
 SELECT 
-
-A.intPContractDetailId 
+strDescription = 'Purchase Contract'
+,A.intAllocationDetailId
+,A.strAllocationNumber
+,A.intPContractDetailId 
 ,A.intSContractDetailId
 ,B.intSampleId
 ,C.strContractNumber
@@ -15,13 +23,10 @@ A.intPContractDetailId
 ,C.strEntityName strVendor
 ,B.dblSampleQty
 ,B.strSampleUOM
-
 ,D.dblAllocatedQty
 ,D.strItemUOM
 ,D.dblNetWeight
 ,D.strNetWeightUOM
-
-
 ,B.dblRepresentingQty
 ,B.strRepresentingUOM
 ,B.strItemNo
@@ -38,20 +43,23 @@ A.intPContractDetailId
 ,B.strCourier
 --,C.strGrade 
 ,B.strSampleTypeName
-
 ,B.strStatus
 ,B.dtmSampleReceivedDate
-FROM vyuLGLoadDetailView A
+from vyuLGAllocationDetails A
 join vyuQMSampleList  B
 on B.intContractDetailId = intPContractDetailId
 join vyuCTGridContractHeader C on C.intContractHeaderId = intPContractHeaderId
 join vyuCTContractDetailView D on D.intContractDetailId = B.intContractDetailId
+join tblLGAllocationDetail E on E.intPContractDetailId = B.intContractDetailId
 
 UNION
 
 SELECT 
 distinct
-A.intPContractDetailId 
+strDescription = 'Sales Contract'
+,A.intAllocationDetailId
+,A.strAllocationNumber
+,A.intPContractDetailId 
 ,A.intSContractDetailId
 ,B.intSampleId
 ,C.strContractNumber
@@ -62,13 +70,10 @@ A.intPContractDetailId
 ,C.strEntityName strVendor
 ,B.dblSampleQty 
 ,B.strSampleUOM
-
 ,D.dblAllocatedQty
 ,D.strItemUOM
 ,D.dblNetWeight
 ,D.strNetWeightUOM
-
-
 ,B.dblRepresentingQty
 ,B.strRepresentingUOM
 ,B.strItemNo
@@ -85,14 +90,17 @@ A.intPContractDetailId
 ,B.strCourier 
 --,C.strGrade 
 ,B.strSampleTypeName
-
 ,B.strStatus
 ,B.dtmSampleReceivedDate
-FROM vyuLGLoadDetailView A
+
+FROM --vyuLGLoadDetailView A
+vyuLGAllocationDetails A
 join vyuQMSampleList  B
-on B.intContractDetailId = intSContractDetailId
+on B.intContractDetailId = A.intSContractDetailId
 join vyuCTGridContractHeader C on C.intContractHeaderId = intSContractDetailId
 join vyuCTContractDetailView D on D.intContractDetailId = B.intContractDetailId
+join tblLGAllocationDetail E on E.intSContractDetailId = B.intContractDetailId
 
 )
 SELECT DISTINCT * FROM QUERY
+
