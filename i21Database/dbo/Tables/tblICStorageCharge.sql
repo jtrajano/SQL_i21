4dc11a8b18109c0b9013e62e1d75b1e3dbb1f0ff
Kenneth Lew Lagunero <kenneth.lagunero@irely.com>
2022-03-30 20:47:39 +0800

    IC-9542

--- i21Database/dbo/Tables/tblICStorageCharge.sql
+++ i21Database/dbo/Tables/tblICStorageCharge.sql
@@ -8,7 +8,7 @@
 	[strStorageChargeNumber] [nvarchar](50) COLLATE Latin1_General_CI_AS NOT NULL,
 	[intCurrencyId] [int] NOT NULL,
 	[strDescription] [nvarchar](200) COLLATE Latin1_General_CI_AS NULL,
-	[ysnPosted] NCHAR(10) NOT NULL DEFAULT 0, 
+	[ysnPosted] BIT NOT NULL DEFAULT 0, 
 	[intConcurrencyId] [int] NOT NULL DEFAULT ((0)) ,
     CONSTRAINT [PK_tblICStorageCharge] PRIMARY KEY CLUSTERED ([intStorageChargeId] ASC)
 ) 
