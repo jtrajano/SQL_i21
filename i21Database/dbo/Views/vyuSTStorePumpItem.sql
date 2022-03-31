a5807ae1aa9f4c3bba95d92361760bc6cd720006
nelsonreyes19 <nelson.reyes@irely.com>
2022-03-31 09:01:09 +0800

    ST-2142, ST-2084

--- i21Database/dbo/Views/vyuSTStorePumpItem.sql
+++ i21Database/dbo/Views/vyuSTStorePumpItem.sql
@@ -9,6 +9,7 @@ SELECT
 	, PU.intTaxGroupId
 	, PU.intCategoryId
 	, CAT.strCategoryCode
+	, UM.strUnitMeasure
 	, I.strItemNo
 	, UOM.strLongUPCCode
 	, I.strDescription AS strPumpItemDescription
@@ -17,6 +18,8 @@ SELECT
 FROM tblSTPumpItem PU
 JOIN tblICItemUOM UOM 
 	ON PU.intItemUOMId = UOM.intItemUOMId
+JOIN tblICUnitMeasure UM 
+	ON UOM.intUnitMeasureId = UM.intUnitMeasureId
 JOIN tblICCategory CAT
 	ON PU.intCategoryId = CAT.intCategoryId
 JOIN tblICItem I
