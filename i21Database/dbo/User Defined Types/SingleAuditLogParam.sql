8cf7fd1f5936d2a22cac230fa56fcb1ea3316c0a
Chris Salvante <chris.salvante@irely.com>
2022-04-01 18:48:02 +0800

    FRM-9976

---
+++ i21Database/dbo/User Defined Types/SingleAuditLogParam.sql
@@ -0,0 +1,12 @@
+﻿CREATE TYPE SingleAuditLogParam 
+AS TABLE ( 
+  [Id]			INT, 
+  [Action]		NVARCHAR(MAX),
+  [Change]		NVARCHAR(MAX),
+  [From]		NVARCHAR(MAX),
+  [To]			NVARCHAR(MAX),
+  [Alias]		NVARCHAR(MAX),
+  [Field]		BIT,
+  [Hidden]		BIT,
+  [ParentId]	INT
+)
