5610dcc6c0018866fd24492e66eb9f2bd809deb0
Jeff Trajano <jeffrey.trajano@irely.com>
2022-04-05 19:57:22 +0800

    QC-644

--- i21Database/dbo/Views/vyuQMAllocation.sql
+++ i21Database/dbo/Views/vyuQMAllocation.sql
@@ -1,106 +1,82 @@
 /*
-FOR VIEWING OF ALLOCATION IN THE QUALITY SAMPLE SCREEN
-IF THE SALES CONTRACT SELECTED ON A NEW SAMPLE HAS PURCHASE CONTRACT, GET THE SAMPLE ID
-- IF MORE THAN ONE, USER WILL MANUALLY CREATE THE SAMPLE
+- FOR VIEWING RELATED SAMPLE
+- EXCLUDES CONTRACT WITH STATUS SHORT CLOSE, COMPLETED, CANCELLED
 */
-
 CREATE VIEW vyuQMAllocation
 AS
-
-WITH QUERY AS(
-SELECT 
-strDescription = 'Purchase Contract'
-,A.intAllocationDetailId
-,A.strAllocationNumber
-,A.intPContractDetailId 
-,A.intSContractDetailId
-,B.intSampleId
-,C.strContractNumber
-, intPContractSeq intSequence
-, B.strSampleNumber
-, B.intItemId
-, intPContractHeaderId 
-,C.strEntityName strVendor
-,B.dblSampleQty
-,B.strSampleUOM
-,D.dblAllocatedQty
-,D.strItemUOM
-,D.dblNetWeight
-,D.strNetWeightUOM
-,B.dblRepresentingQty
-,B.strRepresentingUOM
-,B.strItemNo
-,B.strSamplingMethod
-,B.dtmSamplingEndDate
-,strSamplingCriteria = ''
-,C.strLocationName
-,C.strFreightTerm
-,C.strINCOLocation
-,D.dtmStartDate
-,D.dtmEndDate
-,dtmRequestedDate
-,dtmSampleSentDate
-,B.strCourier
---,C.strGrade 
-,B.strSampleTypeName
-,B.strStatus
-,B.dtmSampleReceivedDate
-from vyuLGAllocationDetails A
-join vyuQMSampleList  B
-on B.intContractDetailId = intPContractDetailId
-join vyuCTGridContractHeader C on C.intContractHeaderId = intPContractHeaderId
-join vyuCTContractDetailView D on D.intContractDetailId = B.intContractDetailId
-join tblLGAllocationDetail E on E.intPContractDetailId = B.intContractDetailId
-
-UNION
-
-SELECT 
-distinct
-strDescription = 'Sales Contract'
-,A.intAllocationDetailId
-,A.strAllocationNumber
-,A.intPContractDetailId 
-,A.intSContractDetailId
-,B.intSampleId
-,C.strContractNumber
-, intPContractSeq intSequence
-, B.strSampleNumber
-, B.intItemId
-, intPContractHeaderId 
-,C.strEntityName strVendor
-,B.dblSampleQty 
-,B.strSampleUOM
-,D.dblAllocatedQty
-,D.strItemUOM
-,D.dblNetWeight
-,D.strNetWeightUOM
-,B.dblRepresentingQty
-,B.strRepresentingUOM
-,B.strItemNo
-,B.strSamplingMethod
-,B.dtmSamplingEndDate
-,strSamplingCriteria = ''
-,C.strLocationName
-,C.strFreightTerm
-,C.strINCOLocation
-,D.dtmStartDate
-,D.dtmEndDate
-,dtmRequestedDate
-,dtmSampleSentDate
-,B.strCourier 
---,C.strGrade 
-,B.strSampleTypeName
-,B.strStatus
-,B.dtmSampleReceivedDate
-
-FROM --vyuLGLoadDetailView A
-vyuLGAllocationDetails A
-join vyuQMSampleList  B
-on B.intContractDetailId = A.intSContractDetailId
-join vyuCTGridContractHeader C on C.intContractHeaderId = intSContractHeaderId
-join vyuCTContractDetailView D on D.intContractDetailId = B.intContractDetailId
-join tblLGAllocationDetail E on E.intSContractDetailId = B.intContractDetailId
-
-)
-SELECT DISTINCT * FROM QUERY
-
+SELECT DISTINCT --A.intPContractDetailId,
+		  D.intContractDetailId intPContractDetailId,
+                B.intSampleId,
+                D.intContractHeaderId intContractHeaderIdP,
+                D.strContractNumber strContractNumberP,
+		  D.intContractSeq intSequenceP,
+                D.strEntityName strEntityNameP,
+                B.dblSampleQty dblSampleQtyP,
+                B.strSampleUOM strSampleUOMP,
+                D.dblNetWeight dblNetWeightP,
+                D.strNetWeightUOM strNetWeightUOMP,
+                B.dblRepresentingQty dblRepresentingQtyP,
+                B.strItemNo strItemNoP,
+                B.strSampleNumber strSampleNumberP,
+                B.strGrade strSampleTypeP,
+                B.strSamplingCriteria strSamplingCriteriaP,
+                B.dtmSamplingEndDate dtmSamplingEndDateP,
+                B.dtmRequestedDate dtmRequestedDateP,
+                B.dtmSampleSentDate dtmSampleSentDateP,
+                B.strCourier strCourierP,
+                B.dtmSampleReceivedDate dtmSampleReceivedDateP,
+                B.strStatus strStatusP,
+                B.strLotNumber strLotNumberP,
+                C.strFreightTerm strFreightTermP,
+                C.strINCOLocation strINCOLocationP,
+                D.dtmStartDate dtmStartDateP,
+                D.dtmEndDate dtmEndDateP,
+                SalesContract.*
+FROM   vyuQMSampleList B
+       LEFT JOIN vyuLGAllocationDetails A
+              ON B.intContractDetailId = intPContractDetailId
+       LEFT JOIN vyuCTGridContractHeader C
+              ON C.intContractHeaderId = intPContractHeaderId
+       LEFT JOIN vyuCTContractDetailView D
+              ON D.intContractDetailId = B.intContractDetailId
+       LEFT JOIN tblLGAllocationDetail E
+              ON E.intPContractDetailId = B.intContractDetailId
+       OUTER APPLY(SELECT
+                  DD.intContractHeaderId intContractHeaderIdS,
+				  DD.strContractNumber strContractNumberS,
+                  DD.intContractSeq  intSequenceS,
+                  DD.strEntityName strEntityNameS,
+                  BB.dblSampleQty dblSampleQtyS,
+                  BB.strSampleUOM strSampleUOMS,
+                  DD.dblNetWeight dblNetWeightS,
+                  DD.strNetWeightUOM strNetWeightUOMS,
+                  BB.dblRepresentingQty dblRepresentingQtyS,
+                  BB.strSampleNumber strSampleNumberS,
+                  BB.strGrade strSampleTypeS,
+                  CC.strFreightTerm strFreightTermS,
+                  CC.strINCOLocation strINCOLocationS,
+                  DD.dtmStartDate dtmStartDateS,
+                  DD.dtmEndDate dtmEndDateS,
+                  BB.strSamplingCriteria strSamplingCriteriaS,
+                  BB.dtmSamplingEndDate dtmSamplingEndDateS, 
+                  BB.dtmSampleSentDate dtmSampleSentDateS,
+                  BB.strCourier strCourierS,
+                  BB.dtmSampleReceivedDate dtmSampleReceivedDateS,
+                  BB.strStatus strStatusS,
+                  BB.strLotNumber strLotNumberS
+				  
+                   FROM   --vyuLGLoadDetailView A
+                  vyuQMSampleList BB
+                  LEFT JOIN vyuLGAllocationDetails AA
+                         ON BB.intContractDetailId = AA.intSContractDetailId
+                  LEFT JOIN vyuCTGridContractHeader CC
+                         ON CC.intContractHeaderId = intSContractHeaderId
+                  LEFT JOIN vyuCTContractDetailView DD
+                         ON DD.intContractDetailId = BB.intContractDetailId
+                  LEFT JOIN tblLGAllocationDetail EE
+                         ON EE.intSContractDetailId = BB.intContractDetailId
+                   WHERE   AA.intPContractDetailId = D.intContractDetailId
+                          AND intContractStatusId NOT IN( 3, 5, 6 ))
+                  SalesContract
+WHERE  intContractStatusId NOT IN( 3, 5, 6 ) 
+AND intRelatedSampleId IS  NULL
