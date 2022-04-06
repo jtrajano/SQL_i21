afd20575b368532910f5620c9e38ff3095f97598
Jopet Lambinicio <jopet.lambinicio@irely.com>
2022-04-06 15:14:53 +0800

    AP-10325

--- i21Database/dbo/Stored Procedures/uspAPProvisionalFinalize.sql
+++ i21Database/dbo/Stored Procedures/uspAPProvisionalFinalize.sql
@@ -23,7 +23,7 @@ BEGIN TRY
 
 --PERCENTAGE LOGIC AND VALIDATION
 
---MAKE SURE BILL TYPE ONLY
+--MAKE SURE PROVISIONAL VOUCHER TYPE ONLY
 IF (SELECT intTransactionType FROM tblAPBill WHERE intBillId = @billId) <> 16
 BEGIN
 	RAISERROR('Invalid transaction type for provisional finalize.', 16, 1);
@@ -33,160 +33,166 @@ END
 IF @transCount = 0 BEGIN TRANSACTION
 ELSE SAVE TRAN @SavePoint
 
-DECLARE @generatedBillRecordId NVARCHAR(50);
-DECLARE @vendorId INT;
-DECLARE @locationId INT;
-DECLARE @requireApproval BIT = 0;
-DECLARE @currencyId INT;
-DECLARE @Total NUMERIC (18, 6);
-DECLARE @DueDate DATE;
+	--CREATE FINAL VOUCHER
+	DECLARE @generatedBillRecordId NVARCHAR(50);
+	DECLARE @vendorId INT;
+	DECLARE @locationId INT;
+	DECLARE @requireApproval BIT = 0;
+	DECLARE @currencyId INT;
+	DECLARE @Total NUMERIC (18, 6);
+	DECLARE @DueDate DATE;
 
---DUPLICATE tblAPBill
-IF OBJECT_ID('tempdb..#tmpDuplicateBill') IS NOT NULL DROP TABLE #tmpDuplicateBill
+	--DUPLICATE tblAPBill
+	IF OBJECT_ID('tempdb..#tmpDuplicateBill') IS NOT NULL DROP TABLE #tmpDuplicateBill
 
---SELECT * INTO #tmpDuplicateBill FROM tblAPProvisional WHERE intBillId = @billId
-SELECT * INTO #tmpDuplicateBill FROM tblAPBill WHERE intBillId = @billId
-ALTER TABLE #tmpDuplicateBill DROP COLUMN intBillId
+	--SELECT * INTO #tmpDuplicateBill FROM tblAPProvisional WHERE intBillId = @billId
+	SELECT * INTO #tmpDuplicateBill FROM tblAPBill WHERE intBillId = @billId
+	ALTER TABLE #tmpDuplicateBill DROP COLUMN intBillId
 
-EXEC uspSMGetStartingNumber 9, @generatedBillRecordId OUT
+	EXEC uspSMGetStartingNumber 9, @generatedBillRecordId OUT
 
-UPDATE A
-	SET A.strBillId = @generatedBillRecordId
-	,A.intTransactionType = 1
-	,A.intEntityId = @userId
-FROM #tmpDuplicateBill A
+	UPDATE A
+		SET A.strBillId = @generatedBillRecordId
+		,A.intTransactionType = 1
+		,A.intEntityId = @userId
+	FROM #tmpDuplicateBill A
 
-INSERT INTO tblAPBill SELECT * FROM #tmpDuplicateBill
+	INSERT INTO tblAPBill SELECT * FROM #tmpDuplicateBill
 
-SET @createdVoucher = SCOPE_IDENTITY();
+	SET @createdVoucher = SCOPE_IDENTITY();
 
---DUPLICATE tblAPBillDetail
-IF OBJECT_ID('tempdb..#tmpDuplicateBillDetail') IS NOT NULL DROP TABLE #tmpDuplicateBillDetail
+	--DUPLICATE tblAPBillDetail
+	IF OBJECT_ID('tempdb..#tmpDuplicateBillDetail') IS NOT NULL DROP TABLE #tmpDuplicateBillDetail
 
-DECLARE @billDetailTaxes TABLE(intCreatedBillDetailId INT, originalBillDetailId INT)
+	DECLARE @billDetailTaxes TABLE(intCreatedBillDetailId INT, originalBillDetailId INT)
 
---SELECT * INTO #tmpDuplicateBillDetail FROM tblAPProvisionalDetail WHERE intBillId = @billId ORDER BY intBillDetailId
-SELECT * INTO #tmpDuplicateBillDetail FROM tblAPBillDetail WHERE intBillId = @billId ORDER BY intBillDetailId
+	--SELECT * INTO #tmpDuplicateBillDetail FROM tblAPProvisionalDetail WHERE intBillId = @billId ORDER BY intBillDetailId
+	SELECT * INTO #tmpDuplicateBillDetail FROM tblAPBillDetail WHERE intBillId = @billId ORDER BY intBillDetailId
 
-UPDATE A
-	SET A.intBillId = @createdVoucher
-FROM #tmpDuplicateBillDetail A
+	UPDATE A
+		SET A.intBillId = @createdVoucher
+	FROM #tmpDuplicateBillDetail A
 
-MERGE INTO tblAPBillDetail
-USING #tmpDuplicateBillDetail A
-ON 1 = 0
-	WHEN NOT MATCHED THEN
-		INSERT(
-			[intBillId],
-			[strMiscDescription],
-			[strComment], 
-			[intAccountId],
-			[intItemId],
-			[intInventoryReceiptItemId],
-			[intInventoryReceiptChargeId],
-			[intPurchaseDetailId],
-			[intContractHeaderId],
-			[intContractDetailId],
-			[intPrepayTypeId],
-			[intTaxGroupId],
-			[dblTotal],
-			[intConcurrencyId], 
-			[dblQtyOrdered], 
-			[dblQtyReceived], 
-			[dblDiscount], 
-			[dblCost], 
-			[dblLandedCost], 
-			[dblTax], 
-			[dblPrepayPercentage], 
-			[dblWeight], 
-			[dblVolume], 
-			[dtmExpectedDate], 
-			[int1099Form], 
-			[int1099Category], 
-			[intLineNo]
-		)
-		VALUES
-		(
-			[intBillId],
-			[strMiscDescription],
-			[strComment], 
-			[intAccountId],
-			[intItemId],
-			[intInventoryReceiptItemId],
-			[intInventoryReceiptChargeId],
-			[intPurchaseDetailId],
-			[intContractHeaderId],
-			[intContractDetailId],
-			[intPrepayTypeId],
-			[intTaxGroupId],
-			[dblTotal],
-			[intConcurrencyId], 
-			[dblQtyOrdered], 
-			[dblQtyReceived], 
-			[dblDiscount], 
-			[dblCost], 
-			[dblLandedCost], 
-			[dblTax], 
-			[dblPrepayPercentage], 
-			[dblWeight], 
-			[dblVolume], 
-			[dtmExpectedDate], 
-			[int1099Form], 
-			[int1099Category], 
-			[intLineNo]
-		)
-		OUTPUT inserted.intBillDetailId, A.intBillDetailId INTO @billDetailTaxes(intCreatedBillDetailId, originalBillDetailId);
+	MERGE INTO tblAPBillDetail
+	USING #tmpDuplicateBillDetail A
+	ON 1 = 0
+		WHEN NOT MATCHED THEN
+			INSERT(
+				[intBillId],
+				[strMiscDescription],
+				[strComment], 
+				[intAccountId],
+				[intItemId],
+				[intInventoryReceiptItemId],
+				[intInventoryReceiptChargeId],
+				[intPurchaseDetailId],
+				[intContractHeaderId],
+				[intContractDetailId],
+				[intPrepayTypeId],
+				[intTaxGroupId],
+				[dblTotal],
+				[intConcurrencyId], 
+				[dblQtyOrdered], 
+				[dblQtyReceived], 
+				[dblDiscount], 
+				[dblCost], 
+				[dblLandedCost], 
+				[dblTax], 
+				[dblPrepayPercentage], 
+				[dblWeight], 
+				[dblVolume], 
+				[dtmExpectedDate], 
+				[int1099Form], 
+				[int1099Category], 
+				[intLineNo]
+			)
+			VALUES
+			(
+				[intBillId],
+				[strMiscDescription],
+				[strComment], 
+				[intAccountId],
+				[intItemId],
+				[intInventoryReceiptItemId],
+				[intInventoryReceiptChargeId],
+				[intPurchaseDetailId],
+				[intContractHeaderId],
+				[intContractDetailId],
+				[intPrepayTypeId],
+				[intTaxGroupId],
+				[dblTotal],
+				[intConcurrencyId], 
+				[dblQtyOrdered], 
+				[dblQtyReceived], 
+				[dblDiscount], 
+				[dblCost], 
+				[dblLandedCost], 
+				[dblTax], 
+				[dblPrepayPercentage], 
+				[dblWeight], 
+				[dblVolume], 
+				[dtmExpectedDate], 
+				[int1099Form], 
+				[int1099Category], 
+				[intLineNo]
+			)
+			OUTPUT inserted.intBillDetailId, A.intBillDetailId INTO @billDetailTaxes(intCreatedBillDetailId, originalBillDetailId);
 
---DUPLICATE tblAPBillDetailTax
-IF OBJECT_ID('tempdb..#tmpDuplicateBillDetailTaxes') IS NOT NULL DROP TABLE #tmpDuplicateBillDetailTaxes
+	--DUPLICATE tblAPBillDetailTax
+	IF OBJECT_ID('tempdb..#tmpDuplicateBillDetailTaxes') IS NOT NULL DROP TABLE #tmpDuplicateBillDetailTaxes
 
--- SELECT A.* INTO #tmpDuplicateBillDetailTaxes 
--- FROM tblAPProvisionalDetailTax A
--- INNER JOIN tblAPProvisionalDetail B ON A.intBillDetailId = B.intBillDetailId
--- WHERE B.intBillId = @billId
-SELECT A.* INTO #tmpDuplicateBillDetailTaxes 
-FROM tblAPBillDetailTax A
-INNER JOIN tblAPBillDetail B ON A.intBillDetailId = B.intBillDetailId
-WHERE B.intBillId = @billId
+	-- SELECT A.* INTO #tmpDuplicateBillDetailTaxes 
+	-- FROM tblAPProvisionalDetailTax A
+	-- INNER JOIN tblAPProvisionalDetail B ON A.intBillDetailId = B.intBillDetailId
+	-- WHERE B.intBillId = @billId
+	SELECT A.* INTO #tmpDuplicateBillDetailTaxes 
+	FROM tblAPBillDetailTax A
+	INNER JOIN tblAPBillDetail B ON A.intBillDetailId = B.intBillDetailId
+	WHERE B.intBillId = @billId
 
-ALTER TABLE #tmpDuplicateBillDetailTaxes DROP COLUMN intBillDetailTaxId
+	ALTER TABLE #tmpDuplicateBillDetailTaxes DROP COLUMN intBillDetailTaxId
 
-UPDATE A
-SET A.intBillDetailId = B.intCreatedBillDetailId
-FROM #tmpDuplicateBillDetailTaxes A
-INNER JOIN @billDetailTaxes B ON A.intBillDetailId = B.originalBillDetailId
+	UPDATE A
+	SET A.intBillDetailId = B.intCreatedBillDetailId
+	FROM #tmpDuplicateBillDetailTaxes A
+	INNER JOIN @billDetailTaxes B ON A.intBillDetailId = B.originalBillDetailId
 
-INSERT INTO tblAPBillDetailTax
-SELECT * FROM #tmpDuplicateBillDetailTaxes
+	INSERT INTO tblAPBillDetailTax
+	SELECT * FROM #tmpDuplicateBillDetailTaxes
 
-SELECT
-	@vendorId = A.intEntityVendorId
-	,@locationId = A.intShipToId
-	,@currencyId = A.intCurrencyId
-	,@Total = A.dblTotal
-	,@DueDate = A.dtmDueDate
-FROM tblAPBill A
-WHERE A.intBillId = @createdVoucher
+	SELECT
+		@vendorId = A.intEntityVendorId
+		,@locationId = A.intShipToId
+		,@currencyId = A.intCurrencyId
+		,@Total = A.dblTotal
+		,@DueDate = A.dtmDueDate
+	FROM tblAPBill A
+	WHERE A.intBillId = @createdVoucher
 
-EXEC [dbo].[uspSMTransactionCheckIfRequiredApproval]
-	@type = N'AccountsPayable.view.Voucher',
-	@transactionEntityId = @vendorId,
-	@currentUserEntityId = @userId,
-	@locationId = @locationId,
-	@amount = @Total,
-	@requireApproval = @requireApproval OUTPUT
-
-IF @requireApproval = 1
-BEGIN
-	EXEC uspSMUnSubmitTransaction
-		@type = 'AccountsPayable.view.Voucher',
-		@recordId = @createdVoucher,
-		@transactionNo = @generatedBillRecordId,
+	EXEC [dbo].[uspSMTransactionCheckIfRequiredApproval]
+		@type = N'AccountsPayable.view.Voucher',
 		@transactionEntityId = @vendorId,
 		@currentUserEntityId = @userId,
-		@amount = @Total, 
-		@dueDate = @DueDate,
-		@currencyId = @currencyId
+		@locationId = @locationId,
+		@amount = @Total,
+		@requireApproval = @requireApproval OUTPUT
+
+	IF @requireApproval = 1
+	BEGIN
+		EXEC uspSMUnSubmitTransaction
+			@type = 'AccountsPayable.view.Voucher',
+			@recordId = @createdVoucher,
+			@transactionNo = @generatedBillRecordId,
+			@transactionEntityId = @vendorId,
+			@currentUserEntityId = @userId,
+			@amount = @Total, 
+			@dueDate = @DueDate,
+			@currencyId = @currencyId
+
+	--UPDATE FINAL VOUCHER BASED ON THE PERCENTAGE
+
+	--UPDATE OR DELETE PROVISIONAL VOUCHER BASED ON THE PERCENTAGE
+	EXEC uspAPDeleteVoucher @billId, @userId
 END
 
 IF @transCount = 0
