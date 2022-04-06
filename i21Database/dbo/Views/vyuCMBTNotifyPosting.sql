82599f779d7922509cfa6331432f448edad2aef4
Jeff Trajano <jeffrey.trajano@irely.com>
2022-04-06 15:42:00 +0800

    GL-8917

--- i21Database/dbo/Views/vyuCMBTNotifyPosting.sql
+++ i21Database/dbo/Views/vyuCMBTNotifyPosting.sql
@@ -1,15 +1,26 @@
-CREATE VIEW vyuCMBTNotifyPosting    
-AS    
-SELECT 
-BT.intEntityId, 
-BT.intTransactionId, 
-BT.strTransactionId 
-FROM tblCMCompanyPreferenceOption o    
-CROSS APPLY(  
-    SELECT intTransactionId, intEntityId, strTransactionId  
-    FROM tblCMBankTransfer       
-    WHERE ysnPostedInTransit = 1 AND ysnPosted = 0  
-    and intBankTransferTypeId in(2,3) 
-    AND DATEDIFF( DAY, dbo.fnRemoveTimeOnDate(GETDATE()), dtmInTransit  ) 
-    BETWEEN 0 AND ISNULL(intBTPostReminderDays,0)  
-) BT
+CREATE VIEW vyuCMBTNotifyPosting
+AS
+  SELECT BT.intEntityId,
+         BT.intTransactionId,
+         BT.strTransactionId
+  FROM   tblCMCompanyPreferenceOption o
+         CROSS APPLY(SELECT intTransactionId,
+                            intEntityId,
+                            strTransactionId,
+                            dtmInTransit
+                     FROM   tblCMBankTransfer
+                     WHERE  ysnPostedInTransit = 1
+                            AND ysnPosted = 0
+                            AND intBankTransferTypeId IN( 2, 3 )
+                            AND Datediff(DAY, dbo.fnRemoveTimeOnDate(Getdate()),
+                                dtmInTransit)
+                                BETWEEN CASE
+                                WHEN
+                                    ISNULL(intBTPostReminderDays, 0) < 0 THEN
+                                    ISNULL(intBTPostReminderDays, 0) ELSE 0 END
+                                AND
+                                    CASE
+                                      WHEN 0 > ISNULL(intBTPostReminderDays, 0) THEN 
+                                      0 ELSE ISNULL(intBTPostReminderDays, 0)
+                                    END
+                    ) BT 
--- i21Database/dbo/Views/vyuCMBTNotifySwapPosting.sql
+++ i21Database/dbo/Views/vyuCMBTNotifySwapPosting.sql
@@ -1,19 +1,26 @@
 CREATE VIEW vyuCMBTNotifySwapPosting
 AS
-SELECT BTS.intEntityId, intBankSwapId
-FROM tblCMCompanyPreferenceOption o  
-CROSS APPLY(
-SELECT 
-S.intBankSwapId,intEntityId,dtmInTransit
-FROM 
-tblCMBankSwap S  
-	CROSS APPLY(
-		SELECT intEntityId, dtmInTransit FROM tblCMBankTransfer B 
-		WHERE (S.intSwapLongId = B.intTransactionId OR S.intSwapShortId = B.intTransactionId)
-		AND ysnPostedInTransit = 1 AND ysnPosted = 0
-	)BT
-	
-
-) BTS
-WHERE datediff( DAY,dbo.fnRemoveTimeOnDate(GETDATE()), BTS.dtmInTransit  ) BETWEEN 0 AND isnull(intBTPostReminderDays,0)
-
+  SELECT BTS.intEntityId,
+         intBankSwapId
+  FROM   tblCMCompanyPreferenceOption o
+         CROSS APPLY(SELECT S.intBankSwapId,
+                            intEntityId,
+                            dtmInTransit
+                     FROM   tblCMBankSwap S
+                            CROSS APPLY(SELECT intEntityId,
+                                               dtmInTransit
+                                        FROM   tblCMBankTransfer B
+                                        WHERE  ( S.intSwapLongId =
+                                                 B.intTransactionId
+                                                  OR S.intSwapShortId =
+                                                     B.intTransactionId )
+                                               AND ysnPostedInTransit = 1
+                                               AND ysnPosted = 0)BT) BTS
+  WHERE  DATEDIFF(DAY, dbo.fnRemoveTimeOnDate(GETDATE()), BTS.dtmInTransit)
+         BETWEEN CASE
+                WHEN ISNULL(intBTPostReminderDays, 0) < 0 THEN
+                	 ISNULL(intBTPostReminderDays, 0) ELSE 0 END
+                AND 
+				CASE
+                WHEN 0 > ISNULL(intBTPostReminderDays, 0) THEN 0
+                    ELSE ISNULL(intBTPostReminderDays, 0) END
