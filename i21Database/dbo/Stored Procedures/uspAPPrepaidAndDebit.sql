77f548ae8f27a6ee22e1718fdd7b92e0f0a81ba2
Erick Dela Cruz <erick.delacruz@irely.com>
2022-04-07 13:15:33 +0800

    AP-10325

--- i21Database/dbo/Stored Procedures/uspAPPrepaidAndDebit.sql
+++ i21Database/dbo/Stored Procedures/uspAPPrepaidAndDebit.sql
@@ -859,6 +859,28 @@ AND EXISTS
 -- AND A.dblAmountDue != 0 --EXCLUDE THOSE FULLY APPLIED
 -- AND 1 = CASE WHEN A.intTransactionType = 13 AND A.ysnPosted = 0 THEN 0 --EXCLUDE UNPOSTED 
 -- 		ELSE 1 END
+UNION ALL
+--PROVISIONAL
+SELECT
+	[intBillId]				=	@billId, 
+	[intBillDetailApplied]	=	NULL, 
+	[intLineApplied]		=	NULL, 
+	[intTransactionId]		=	A.intBillId,
+	[strTransactionNumber]	=	A.strBillId,
+	[intItemId]				=	NULL,
+	[strItemDescription]	=	NULL,
+	[strItemNo]				=	NULL,
+	[intContractHeaderId]	=	NULL,	
+	[strContractNumber]		=	NULL,
+	[intPrepayType]			=	NULL,
+	[dblTotal]				=	A.dblTotal,
+	[dblBillAmount]			=	(SELECT dblTotal FROM tblAPBill WHERE intBillId = @billId),
+	[dblBalance]			=	A.dblAmountDue,
+	[dblAmountApplied]		=	0,
+	[ysnApplied]			=	0,
+	[intConcurrencyId]		=	0
+FROM tblAPBill A
+WHERE A.intTransactionType = 16
 
 SELECT * FROM tblAPAppliedPrepaidAndDebit WHERE intBillId = @billId
 
