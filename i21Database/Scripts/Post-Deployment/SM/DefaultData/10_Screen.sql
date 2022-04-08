b50a0335db30dee09c0e58e70ee72a26be731797
Kenneth Lew Lagunero <kenneth.lagunero@irely.com>
2022-04-08 11:23:09 +0800

    AR-14539

--- i21Database/Scripts/Post-Deployment/SM/DefaultData/10_Screen.sql
+++ i21Database/Scripts/Post-Deployment/SM/DefaultData/10_Screen.sql
@@ -961,12 +961,12 @@ GO
 
 	IF NOT EXISTS (SELECT TOP 1 1 FROM tblSMScreen WHERE strNamespace = 'AccountsReceivable.view.EntityCustomer') 
 	BEGIN
-		INSERT [dbo].[tblSMScreen] ([strScreenId], [strScreenName], [strPortalName], [strNamespace], [strModule], [strTableName], [intConcurrencyId], [strGroupName]) 
-		VALUES (N'Customers', N'Customers', N'My Company', N'AccountsReceivable.view.EntityCustomer', N'Accounts Receivable', N'tblARCustomer', 1, N'Accounts Receivable')
+		INSERT [dbo].[tblSMScreen] ([strScreenId], [strScreenName], [strPortalName], [strNamespace], [strModule], [strTableName], [intConcurrencyId], [strGroupName], [ysnApproval]) 
+		VALUES (N'Customers', N'Customers', N'My Company', N'AccountsReceivable.view.EntityCustomer', N'Accounts Receivable', N'tblARCustomer', 1, N'Accounts Receivable',1)
 	END
 	ELSE
 	BEGIN
-		UPDATE [tblSMScreen] SET  strScreenId = 'Customers', strScreenName = 'Customers', strPortalName = N'My Company', strModule = 'Accounts Receivable' WHERE strNamespace = 'AccountsReceivable.view.EntityCustomer'
+		UPDATE [tblSMScreen] SET  strScreenId = 'Customers', strScreenName = 'Customers', strPortalName = N'My Company', strModule = 'Accounts Receivable', [ysnApproval] = 1 WHERE strNamespace = 'AccountsReceivable.view.EntityCustomer'
 	END
 	
 	IF NOT EXISTS (SELECT TOP 1 1 FROM tblSMScreen WHERE strNamespace = 'AccountsReceivable.view.EntitySalesperson') 
