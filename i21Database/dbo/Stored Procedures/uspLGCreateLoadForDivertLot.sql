df1ae2fa64e29227012ee9d9628c888fab7ddf5a
Arman Arenas <arman.arenas@irely.com>
2022-04-14 11:33:29 +0800

    LG-3341 fixed issue when diverting multiple lots from 1 container

--- i21Database/dbo/Stored Procedures/uspLGCreateLoadForDivertLot.sql
+++ i21Database/dbo/Stored Procedures/uspLGCreateLoadForDivertLot.sql
@@ -3,6 +3,7 @@
 	,@intEntityUserSecurityId INT
 	,@intNewLoadId INT OUTPUT
 AS
+DECLARE @outputId AS Id
 
 /* Check if there are any Lot Quantity to Divert */
 IF NOT EXISTS (SELECT 1 FROM tblLGLoadDetailLot WHERE ISNULL(dblDivertQuantity, 0) > 0
@@ -137,6 +138,7 @@ INSERT INTO tblLGLoad (
 	,ysnLoadBased
 	,intConcurrencyId
 	,intLoadRefId)
+OUTPUT inserted.intLoadId INTO @outputId(intId)
 SELECT @strNewLoadNumber
 	,intCompanyLocationId
 	,intPurchaseSale = 2
@@ -258,7 +260,8 @@ SELECT @strNewLoadNumber
 FROM tblLGLoad
 WHERE intLoadId = @intLoadId
 
-SELECT @intNewLoadId = SCOPE_IDENTITY()
+IF EXISTS(SELECT TOP 1 1 FROM @outputId)
+	SELECT TOP 1 @intNewLoadId = intId FROM @outputId
 
 /* Get all Load Details where Divert Qty is specified */
 SELECT LD.intLoadDetailId 
@@ -281,6 +284,7 @@ BEGIN
 	SELECT TOP 1 @intLoadDetailId = intLoadDetailId FROM #tmpLoadDetails
 
 	/* Create New Load Details */
+	DELETE FROM @outputId
 	INSERT INTO tblLGLoadDetail (
 		intLoadId
 		,intVendorEntityId
@@ -326,6 +330,7 @@ BEGIN
 		,strExternalBatchNo
 		,intLoadDetailRefId
 		,intConcurrencyId)
+	OUTPUT inserted.intLoadDetailId INTO @outputId(intId)
 	SELECT 
 		intLoadId = @intNewLoadId
 		,intVendorEntityId = NULL
@@ -380,229 +385,256 @@ BEGIN
 		AND ISNULL(dblDivertQuantity, 0) > 0) LDL
 	WHERE LD.intLoadDetailId = @intLoadDetailId
 
-	SELECT @intNewLoadDetailId = SCOPE_IDENTITY()
+	IF EXISTS(SELECT TOP 1 1 FROM @outputId)
+		SELECT TOP 1 @intNewLoadDetailId = intId FROM @outputId
 
-	/* Create New Load Detail Lots */
-	INSERT INTO tblLGLoadDetailLot (
-		intLoadDetailId
-		,intLotId
-		,dblLotQuantity
-		,intItemUOMId
-		,dblGross
-		,dblTare
-		,dblNet
-		,intWeightUOMId
-		,strWarehouseCargoNumber
-		,intSort
-		,intLoadDetailLotRefId
-		,intNewLotId
-		,strNewLotNumber
-		,strID1
-		,strID2
-		,strID3
-		,dblDivertQuantity
-		,intConcurrencyId)
-	SELECT
-		intLoadDetailId = @intNewLoadDetailId
-		,LDL.intLotId
-		,dblLotQuantity = LDL.dblDivertQuantity
-		,LDL.intItemUOMId
-		,dblGross = LDL.dblDivertQuantity * (ISNULL(LDLV.dblWeightPerUnit, 1) + ISNULL(LDLV.dblTarePerQty, 1))
-		,dblTare = LDL.dblDivertQuantity * ISNULL(LDLV.dblTarePerQty, 1)
-		,dblNet = LDL.dblDivertQuantity * ISNULL(LDLV.dblWeightPerUnit, 1)
-		,LDL.intWeightUOMId
-		,LDL.strWarehouseCargoNumber
-		,LDL.intSort
-		,intLoadDetailLotRefId = LDL.intLoadDetailLotId
-		,intNewLotId = NULL
-		,strNewLotNumber = NULL
-		,strID1 = LDL.strID1
-		,strID2 = LDL.strID2
-		,strID3 = LDL.strID3
-		,dblDivertQuantity = NULL
-		,intConcurrencyId = 1
-	FROM tblLGLoadDetailLot LDL
-	INNER JOIN vyuLGLoadDetailLotsView LDLV ON LDLV.intLoadDetailLotId = LDL.intLoadDetailLotId
-	WHERE LDL.intLoadDetailId = @intLoadDetailId
-
-	/* Create Load Containers */
-	IF (EXISTS (SELECT TOP 1 intLoadContainerId FROM tblLGLoadContainer WHERE intLoadId = @intLoadId)
-		AND EXISTS (SELECT TOP 1 intLoadContainerId FROM tblLGLoadDetailContainerLink WHERE intLoadDetailId = @intLoadDetailId))
+	IF (@intNewLoadDetailId IS NOT NULL)
 	BEGIN
-	IF NOT EXISTS (SELECT 1 FROM tblLGLoadContainer NLC INNER JOIN tblLGLoadContainer LC ON LC.intLoadContainerId = NLC.intLoadContainerRefId 
-		WHERE LC.intLoadId = @intNewLoadId AND LC.intLoadContainerId IN (SELECT TOP 1 intLoadContainerId FROM tblLGLoadDetailContainerLink WHERE intLoadDetailId = @intLoadDetailId))
-		BEGIN
-			INSERT INTO tblLGLoadContainer (
-				intConcurrencyId
-				,intLoadId
-				,strContainerId
-				,strContainerNumber
-				,dblQuantity
-				,intUnitMeasureId
-				,dblGrossWt
-				,dblTareWt
-				,dblNetWt
-				,intWeightUnitMeasureId
-				,strComments
-				,strSealNumber
-				,strLotNumber
-				,strMarks
-				,strOtherMarks
-				,ysnRejected
-				,dtmUnloading
-				,dtmCustoms
-				,ysnCustomsHold
-				,strCustomsComments
-				,dtmFDA
-				,ysnFDAHold
-				,strFDAComments
-				,dtmFreight
-				,ysnDutyPaid
-				,strFreightComments
-				,dtmUSDA
-				,ysnUSDAHold
-				,strUSDAComments
-				,dblUnitCost
-				,intCostUOMId
-				,intCurrencyId
-				,dblTotalCost
-				,ysnNewContainer
-				,dblCustomsClearedQty
-				,dblIntransitQty
-				,strDocumentNumber
-				,dtmClearanceDate
-				,strClearanceMonth
-				,dblDeclaredWeight
-				,dblStaticValue
-				,intStaticValueCurrencyId
-				,dblAmount
-				,intAmountCurrencyId
-				,strRemarks
-				,intLoadContainerRefId
-				,intSort)
-			SELECT
-				intConcurrencyId = 1
-				,intLoadId = @intNewLoadId
-				,strContainerId
-				,strContainerNumber
-				,dblQuantity = LDL.dblDivertQuantity
-				,intUnitMeasureId
-				,dblGrossWt = LDL.dblDivertQuantity * (ISNULL(LDL.dblWeightPerUnit, 1) + ISNULL(LDL.dblTarePerQty, 1))
-				,dblTareWt = LDL.dblDivertQuantity * ISNULL(LDL.dblTarePerQty, 1)
-				,dblNetWt = LDL.dblDivertQuantity * ISNULL(LDL.dblWeightPerUnit, 1)
-				,intWeightUnitMeasureId
-				,strComments
-				,strSealNumber
-				,strLotNumber
-				,strMarks
-				,strOtherMarks
-				,ysnRejected
-				,dtmUnloading
-				,dtmCustoms
-				,ysnCustomsHold
-				,strCustomsComments
-				,dtmFDA
-				,ysnFDAHold
-				,strFDAComments
-				,dtmFreight
-				,ysnDutyPaid
-				,strFreightComments
-				,dtmUSDA
-				,ysnUSDAHold
-				,strUSDAComments
-				,LC.dblUnitCost
-				,LC.intCostUOMId
-				,LC.intCurrencyId
-				,LC.dblTotalCost
-				,ysnNewContainer
-				,dblCustomsClearedQty
-				,dblIntransitQty
-				,strDocumentNumber
-				,dtmClearanceDate
-				,strClearanceMonth
-				,dblDeclaredWeight
-				,dblStaticValue
-				,intStaticValueCurrencyId
-				,LC.dblAmount
-				,intAmountCurrencyId
-				,strRemarks
-				,intLoadContainerRefId = LC.intLoadContainerId
-				,intSort
-			FROM tblLGLoadContainer LC
-			CROSS APPLY 
-				(SELECT dblDivertQuantity = SUM(dblDivertQuantity)
-					,dblWeightPerUnit = SUM(dblWeightPerUnit)
-					,dblTarePerQty = SUM(dblTarePerQty)
-				FROM vyuLGLoadDetailLotsView 
-				WHERE intLoadDetailId = @intLoadDetailId
-				AND ISNULL(dblDivertQuantity, 0) > 0
-				GROUP BY intLoadDetailId) LDL
-			WHERE intLoadContainerId IN (SELECT TOP 1 intLoadContainerId FROM tblLGLoadDetailContainerLink WHERE intLoadDetailId = @intLoadDetailId)
-				AND NOT EXISTS (SELECT 1 FROM tblLGLoadContainer WHERE strContainerNumber = LC.strContainerNumber AND intLoadId = @intNewLoadId)
-
-			SELECT @intNewLoadContainerId = SCOPE_IDENTITY()
-		END
-		ELSE
-		BEGIN
-			SELECT @intNewLoadContainerId = NLC.intLoadContainerId 
-			FROM tblLGLoadContainer NLC INNER JOIN tblLGLoadContainer LC ON LC.intLoadContainerId = NLC.intLoadContainerRefId 
-			WHERE LC.intLoadId = @intNewLoadId 
-			AND LC.intLoadContainerId IN (SELECT TOP 1 intLoadContainerId FROM tblLGLoadDetailContainerLink WHERE intLoadDetailId = @intLoadDetailId)
-		END
-	
-		/* Create Load Container Links */
-		INSERT INTO tblLGLoadDetailContainerLink (
-			intConcurrencyId
-			,intLoadId
-			,intLoadContainerId
-			,intLoadDetailId
-			,dblQuantity
+		/* Create New Load Detail Lots */
+		INSERT INTO tblLGLoadDetailLot (
+			intLoadDetailId
+			,intLotId
+			,dblLotQuantity
 			,intItemUOMId
-			,dblReceivedQty
-			,dblLinkGrossWt
-			,dblLinkTareWt
-			,dblLinkNetWt
-			,dblUnitCost
-			,intCostUOMId
-			,intCurrencyId
-			,dblTotalCost
-			,strIntegrationNumber
-			,dtmIntegrationRequested
-			,strIntegrationOrderNumber
-			,dblIntegrationOrderPrice
-			,strExternalContainerId
-			,ysnExported
-			,dtmExportedDate
-			,dtmIntegrationOrderDate
-			,intLoadDetailContainerLinkRefId)
+			,dblGross
+			,dblTare
+			,dblNet
+			,intWeightUOMId
+			,strWarehouseCargoNumber
+			,intSort
+			,intLoadDetailLotRefId
+			,intNewLotId
+			,strNewLotNumber
+			,strID1
+			,strID2
+			,strID3
+			,dblDivertQuantity
+			,intConcurrencyId)
 		SELECT
-			intConcurrencyId = 1
-			,intLoadId = @intNewLoadId
-			,intLoadContainerId = @intNewLoadContainerId
-			,intLoadDetailId = @intNewLoadDetailId
-			,LD.dblQuantity
-			,LD.intItemUOMId
-			,dblReceivedQty = NULL
-			,dblLinkGrossWt = LD.dblGross
-			,dblLinkTareWt = LD.dblTare
-			,dblLinkNetWt = LD.dblNet
-			,dblUnitCost = LC.dblUnitCost
-			,intCostUOMId = LC.intCostUOMId
-			,intCurrencyId = LC.intCurrencyId
-			,dblTotalCost = LC.dblTotalCost 
-			,strIntegrationNumber = NULL
-			,dtmIntegrationRequested = NULL
-			,strIntegrationOrderNumber = NULL
-			,dblIntegrationOrderPrice = NULL
-			,strExternalContainerId = NULL
-			,ysnExported = NULL
-			,dtmExportedDate = NULL
-			,dtmIntegrationOrderDate = NULL
-			,intLoadDetailContainerLinkRefId = NULL
-		FROM tblLGLoadDetail LD
-		OUTER APPLY (SELECT TOP 1 * FROM tblLGLoadContainer WHERE intLoadContainerId = @intNewLoadContainerId) LC
-		WHERE LD.intLoadDetailId = @intNewLoadDetailId
+			intLoadDetailId = @intNewLoadDetailId
+			,LDL.intLotId
+			,dblLotQuantity = LDL.dblDivertQuantity
+			,LDL.intItemUOMId
+			,dblGross = LDL.dblDivertQuantity * (ISNULL(LDLV.dblWeightPerUnit, 1) + ISNULL(LDLV.dblTarePerQty, 1))
+			,dblTare = LDL.dblDivertQuantity * ISNULL(LDLV.dblTarePerQty, 1)
+			,dblNet = LDL.dblDivertQuantity * ISNULL(LDLV.dblWeightPerUnit, 1)
+			,LDL.intWeightUOMId
+			,LDL.strWarehouseCargoNumber
+			,LDL.intSort
+			,intLoadDetailLotRefId = LDL.intLoadDetailLotId
+			,intNewLotId = NULL
+			,strNewLotNumber = NULL
+			,strID1 = LDL.strID1
+			,strID2 = LDL.strID2
+			,strID3 = LDL.strID3
+			,dblDivertQuantity = NULL
+			,intConcurrencyId = 1
+		FROM tblLGLoadDetailLot LDL
+		INNER JOIN vyuLGLoadDetailLotsView LDLV ON LDLV.intLoadDetailLotId = LDL.intLoadDetailLotId
+		WHERE LDL.intLoadDetailId = @intLoadDetailId
+
+		/* Check if Load contains Containers */
+		IF (EXISTS (SELECT TOP 1 intLoadContainerId FROM tblLGLoadContainer WHERE intLoadId = @intLoadId)
+			AND EXISTS (SELECT TOP 1 intLoadContainerId FROM tblLGLoadDetailContainerLink WHERE intLoadDetailId = @intLoadDetailId))
+		BEGIN
+			/* Create Load Containers - Check if container to insert is the same as the first one */
+			DELETE FROM @outputId
+			IF NOT EXISTS (SELECT 1 FROM tblLGLoadContainer NLC INNER JOIN tblLGLoadContainer LC ON LC.intLoadContainerId = NLC.intLoadContainerRefId 
+				WHERE NLC.intLoadId = @intNewLoadId AND LC.intLoadContainerId IN (SELECT TOP 1 intLoadContainerId FROM tblLGLoadDetailContainerLink WHERE intLoadDetailId = @intLoadDetailId))
+			BEGIN
+				INSERT INTO tblLGLoadContainer (
+					intConcurrencyId
+					,intLoadId
+					,strContainerId
+					,strContainerNumber
+					,dblQuantity
+					,intUnitMeasureId
+					,dblGrossWt
+					,dblTareWt
+					,dblNetWt
+					,intWeightUnitMeasureId
+					,strComments
+					,strSealNumber
+					,strLotNumber
+					,strMarks
+					,strOtherMarks
+					,ysnRejected
+					,dtmUnloading
+					,dtmCustoms
+					,ysnCustomsHold
+					,strCustomsComments
+					,dtmFDA
+					,ysnFDAHold
+					,strFDAComments
+					,dtmFreight
+					,ysnDutyPaid
+					,strFreightComments
+					,dtmUSDA
+					,ysnUSDAHold
+					,strUSDAComments
+					,dblUnitCost
+					,intCostUOMId
+					,intCurrencyId
+					,dblTotalCost
+					,ysnNewContainer
+					,dblCustomsClearedQty
+					,dblIntransitQty
+					,strDocumentNumber
+					,dtmClearanceDate
+					,strClearanceMonth
+					,dblDeclaredWeight
+					,dblStaticValue
+					,intStaticValueCurrencyId
+					,dblAmount
+					,intAmountCurrencyId
+					,strRemarks
+					,intLoadContainerRefId
+					,intSort)
+				OUTPUT inserted.intLoadContainerId INTO @outputId(intId)
+				SELECT
+					intConcurrencyId = 1
+					,intLoadId = @intNewLoadId
+					,strContainerId
+					,strContainerNumber
+					,dblQuantity = LDL.dblDivertQuantity
+					,intUnitMeasureId
+					,dblGrossWt = LDL.dblDivertQuantity * (ISNULL(LDL.dblWeightPerUnit, 1) + ISNULL(LDL.dblTarePerQty, 1))
+					,dblTareWt = LDL.dblDivertQuantity * ISNULL(LDL.dblTarePerQty, 1)
+					,dblNetWt = LDL.dblDivertQuantity * ISNULL(LDL.dblWeightPerUnit, 1)
+					,intWeightUnitMeasureId
+					,strComments
+					,strSealNumber
+					,strLotNumber
+					,strMarks
+					,strOtherMarks
+					,ysnRejected
+					,dtmUnloading
+					,dtmCustoms
+					,ysnCustomsHold
+					,strCustomsComments
+					,dtmFDA
+					,ysnFDAHold
+					,strFDAComments
+					,dtmFreight
+					,ysnDutyPaid
+					,strFreightComments
+					,dtmUSDA
+					,ysnUSDAHold
+					,strUSDAComments
+					,LC.dblUnitCost
+					,LC.intCostUOMId
+					,LC.intCurrencyId
+					,LC.dblTotalCost
+					,ysnNewContainer
+					,dblCustomsClearedQty
+					,dblIntransitQty
+					,strDocumentNumber
+					,dtmClearanceDate
+					,strClearanceMonth
+					,dblDeclaredWeight
+					,dblStaticValue
+					,intStaticValueCurrencyId
+					,LC.dblAmount
+					,intAmountCurrencyId
+					,strRemarks
+					,intLoadContainerRefId = LC.intLoadContainerId
+					,intSort
+				FROM tblLGLoadContainer LC
+				CROSS APPLY 
+					(SELECT dblDivertQuantity = SUM(dblDivertQuantity)
+						,dblWeightPerUnit = SUM(dblWeightPerUnit)
+						,dblTarePerQty = SUM(dblTarePerQty)
+					FROM vyuLGLoadDetailLotsView 
+					WHERE intLoadDetailId = @intLoadDetailId
+					AND ISNULL(dblDivertQuantity, 0) > 0
+					GROUP BY intLoadDetailId) LDL
+				WHERE intLoadContainerId IN (SELECT TOP 1 intLoadContainerId FROM tblLGLoadDetailContainerLink WHERE intLoadDetailId = @intLoadDetailId)
+					AND NOT EXISTS (SELECT 1 FROM tblLGLoadContainer WHERE strContainerNumber = LC.strContainerNumber AND intLoadId = @intNewLoadId)
+
+				IF EXISTS(SELECT TOP 1 1 FROM @outputId)
+					SELECT TOP 1 @intNewLoadContainerId = intId FROM @outputId
+			END
+			ELSE
+			BEGIN
+				SELECT @intNewLoadContainerId = NLC.intLoadContainerId 
+				FROM tblLGLoadContainer NLC INNER JOIN tblLGLoadContainer LC ON LC.intLoadContainerId = NLC.intLoadContainerRefId 
+				WHERE NLC.intLoadId = @intNewLoadId AND LC.intLoadContainerId IN (SELECT TOP 1 intLoadContainerId FROM tblLGLoadDetailContainerLink WHERE intLoadDetailId = @intLoadDetailId)
+
+				UPDATE LC
+				SET dblQuantity = dblQuantity + LDL.dblDivertQuantity
+					,dblGrossWt = dblGrossWt + (LDL.dblDivertQuantity * (ISNULL(LDL.dblWeightPerUnit, 1) + ISNULL(LDL.dblTarePerQty, 1)))
+					,dblTareWt = dblTareWt + (LDL.dblDivertQuantity * ISNULL(LDL.dblTarePerQty, 1))
+					,dblNetWt = dblNetWt + (LDL.dblDivertQuantity * ISNULL(LDL.dblWeightPerUnit, 1))
+				FROM tblLGLoadContainer LC
+				CROSS APPLY 
+					(SELECT dblDivertQuantity = SUM(dblDivertQuantity)
+						,dblWeightPerUnit = SUM(dblWeightPerUnit)
+						,dblTarePerQty = SUM(dblTarePerQty)
+					FROM vyuLGLoadDetailLotsView 
+					WHERE intLoadDetailId = @intLoadDetailId
+					AND ISNULL(dblDivertQuantity, 0) > 0
+					GROUP BY intLoadDetailId) LDL
+				WHERE intLoadContainerId = @intNewLoadContainerId
+			END
+	
+			IF (@intNewLoadContainerId IS NOT NULL)
+			BEGIN
+				/* Create Load Container Links */
+				INSERT INTO tblLGLoadDetailContainerLink (
+					intConcurrencyId
+					,intLoadId
+					,intLoadContainerId
+					,intLoadDetailId
+					,dblQuantity
+					,intItemUOMId
+					,dblReceivedQty
+					,dblLinkGrossWt
+					,dblLinkTareWt
+					,dblLinkNetWt
+					,dblUnitCost
+					,intCostUOMId
+					,intCurrencyId
+					,dblTotalCost
+					,strIntegrationNumber
+					,dtmIntegrationRequested
+					,strIntegrationOrderNumber
+					,dblIntegrationOrderPrice
+					,strExternalContainerId
+					,ysnExported
+					,dtmExportedDate
+					,dtmIntegrationOrderDate
+					,intLoadDetailContainerLinkRefId)
+				SELECT
+					intConcurrencyId = 1
+					,intLoadId = @intNewLoadId
+					,intLoadContainerId = @intNewLoadContainerId
+					,intLoadDetailId = @intNewLoadDetailId
+					,LD.dblQuantity
+					,LD.intItemUOMId
+					,dblReceivedQty = NULL
+					,dblLinkGrossWt = LD.dblGross
+					,dblLinkTareWt = LD.dblTare
+					,dblLinkNetWt = LD.dblNet
+					,dblUnitCost = LC.dblUnitCost
+					,intCostUOMId = LC.intCostUOMId
+					,intCurrencyId = LC.intCurrencyId
+					,dblTotalCost = LC.dblTotalCost 
+					,strIntegrationNumber = NULL
+					,dtmIntegrationRequested = NULL
+					,strIntegrationOrderNumber = NULL
+					,dblIntegrationOrderPrice = NULL
+					,strExternalContainerId = NULL
+					,ysnExported = NULL
+					,dtmExportedDate = NULL
+					,dtmIntegrationOrderDate = NULL
+					,intLoadDetailContainerLinkRefId = NULL
+				FROM tblLGLoadDetail LD
+				OUTER APPLY (SELECT TOP 1 * FROM tblLGLoadContainer WHERE intLoadContainerId = @intNewLoadContainerId) LC
+				WHERE LD.intLoadDetailId = @intNewLoadDetailId
+			END
+		END
 	END
+	
 	
 	--Loop control
 	DELETE FROM #tmpLoadDetails WHERE intLoadDetailId = @intLoadDetailId
