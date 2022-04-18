0a3fce18302f73644a4fc157c6bb8ec253fd2654
Kenneth Lew Lagunero <kenneth.lagunero@irely.com>
2022-04-18 10:27:51 +0800

    IC-10205

--- i21Database/dbo/Tables/tblICInsuranceRateDetail.sql
+++ i21Database/dbo/Tables/tblICInsuranceRateDetail.sql
@@ -7,7 +7,7 @@
 	[strAppliedTo] [nvarchar](15) COLLATE Latin1_General_CI_AS NOT NULL DEFAULT 'Blank',
 	[dblRate] NUMERIC(18, 6) NOT NULL DEFAULT 0, 
 	[intCurrencyId] [int] NOT NULL,
-	[intItemUOMId] [int] NULL,
+	[intUnitMeasureId] [int] NULL,
 	[intConcurrencyId] [int] NOT NULL DEFAULT 0,
     CONSTRAINT [PK_tblICInsuranceRateDetail] PRIMARY KEY CLUSTERED ([intInsuranceRateDetailId] ASC)
 ) 
