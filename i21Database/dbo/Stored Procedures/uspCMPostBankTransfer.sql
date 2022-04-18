360b3880b5f5f9b614e9f5a678d1b7fe25fbe8ae
Jeff Trajano <jeffrey.trajano@irely.com>
2022-04-18 11:08:25 +0800

    GL-8947

--- i21Database/dbo/Stored Procedures/uspCMPostBankTransfer.sql
+++ i21Database/dbo/Stored Procedures/uspCMPostBankTransfer.sql
@@ -718,6 +718,14 @@ IF @ysnPost = 1
     BEGIN    
       IF  @ysnPostedInTransit = 1 OR @intBankTransferTypeId =2 OR @intBankTransferTypeId = 4
       BEGIN  
+            DELETE A FROM tblCMBankTransaction A
+            WHERE 
+            A.strTransactionId IN( 
+              SELECT strTransactionId +@BANK_TRANSFER_WD_PREFIX strTransactionId FROM #tmpGLDetail UNION 
+              SELECT strTransactionId +@BANK_TRANSFER_DEP_PREFIX strTransactionId FROM #tmpGLDetail
+            )
+
+
             INSERT INTO tblCMBankTransaction (    
             strTransactionId    
             ,intBankTransactionTypeId    
