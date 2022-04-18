cbed51da0578a4eae1c9fb8abd872feeac2e5168
Mark Daniel Marmeto <mark.marmeto@irely.com>
2022-04-18 10:33:05 +0800

    GL-8871

---
+++ i21Database/dbo/Tables/tblSMRapidDeployment.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[tblSMRapidDeployment]
+(
+	[intRapidDeploymentId]  INT IDENTITY (1, 1) NOT NULL,
+    [intModuleId]           INT NOT NULL,
+    [intConcurrencyId]      INT DEFAULT (1) NOT NULL,
+    CONSTRAINT [PK_tblSMRapidDeployment] PRIMARY KEY CLUSTERED ([intRapidDeploymentId] ASC)
+)
---
+++ i21Database/vyuSMRapidDeploymentDetail.sql
@@ -0,0 +1,11 @@
+﻿CREATE VIEW [dbo].[vyuSMRapidDeploymentDetail]
+AS 
+SELECT
+	  A.*
+	, strUserAssigned = E.strName
+	, strUserCompleted = E1.strName
+FROM tblSMRapidDeploymentDetail A
+LEFT JOIN tblEMEntity E
+	ON E.intEntityId = A.intUserAssignedId
+LEFT JOIN tblEMEntity E1
+	ON E1.intEntityId = A.intUserCompletedId
