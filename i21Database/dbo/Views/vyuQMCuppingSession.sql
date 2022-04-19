c9c80a5785784e95c26358e0f3e3f6e780aeca71
Jeff Trajano <jeffrey.trajano@irely.com>
2022-04-19 08:27:19 +0800

    QC-632

--- i21Database/dbo/Views/vyuQMCuppingSession.sql
+++ i21Database/dbo/Views/vyuQMCuppingSession.sql
@@ -2,6 +2,6 @@ CREATE VIEW vyuQMCuppingSession
 AS
 SELECT intCuppingSessionId			= CS.intCuppingSessionId
 	 , strCuppingSessionNumber		= CS.strCuppingSessionNumber
-     , dtmCuppingDate               = CS.dtmCuppingDate + CAST(CS.dtmCuppingTime AS TIME)
+     , dtmCuppingDate               =  cast(CS.dtmCuppingDate as datetime) + cast(CAST(CS.dtmCuppingTime AS TIME) as datetime) --  CS.dtmCuppingDate + CAST(CS.dtmCuppingTime AS TIME)
      , dtmCuppingTime               = CS.dtmCuppingTime
 FROM tblQMCuppingSession CS
