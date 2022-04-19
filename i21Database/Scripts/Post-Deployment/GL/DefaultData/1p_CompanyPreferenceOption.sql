960bc325438817e01ea3305d61df9bda4d877ed3
Jeff Trajano <jeffrey.trajano@irely.com>
2022-04-20 07:53:26 +0800

    GL-8226

--- i21Database/Scripts/Post-Deployment/GL/DefaultData/1p_CompanyPreferenceOption.sql
+++ i21Database/Scripts/Post-Deployment/GL/DefaultData/1p_CompanyPreferenceOption.sql
@@ -7,68 +7,68 @@ ELSE
 GO
 
 -- update what segment to override in retained earnings during closing period.
--- IF EXISTS(SELECT 1 FROM tblGLCompanyPreferenceOption WHERE ISNULL(ysnREOverride,0) =1)
--- BEGIN
+IF EXISTS(SELECT 1 FROM tblGLCompanyPreferenceOption WHERE ISNULL(ysnREOverride,0) =1)
+BEGIN
     
---         DECLARE @ysnREOverrideLocation BIT,
---                 @ysnREOverrideLOB      BIT,
---                 @ysnREOverrideCompany  BIT
+        DECLARE @ysnREOverrideLocation BIT,
+                @ysnREOverrideLOB      BIT,
+                @ysnREOverrideCompany  BIT
 
---         SELECT TOP 1 @ysnREOverrideLocation = ysnREOverrideLocation,
---                     @ysnREOverrideLOB = ysnREOverrideLOB,
---                     @ysnREOverrideCompany = ysnREOverrideCompany
---         FROM   tblGLCompanyPreferenceOption
+        SELECT TOP 1 @ysnREOverrideLocation = ysnREOverrideLocation,
+                    @ysnREOverrideLOB = ysnREOverrideLOB,
+                    @ysnREOverrideCompany = ysnREOverrideCompany
+        FROM   tblGLCompanyPreferenceOption
 
---         DECLARE @tbl TABLE
---         (
---             rowId            INT,
---             intStructureType INT
---         )
+        DECLARE @tbl TABLE
+        (
+            rowId            INT,
+            intStructureType INT
+        )
 
---         INSERT INTO @tbl
---         SELECT ROW_NUMBER() OVER (ORDER BY intSort ASC) AS rowId, intStructureType
---         FROM   tblGLAccountStructure
---         WHERE  strType != 'Divider'
+        INSERT INTO @tbl
+        SELECT ROW_NUMBER() OVER (ORDER BY intSort ASC) AS rowId, intStructureType
+        FROM   tblGLAccountStructure
+        WHERE  strType != 'Divider'
 
---         DECLARE @i          INT,
---                 @intSType   INT,
---                 @str        NVARCHAR(10)='',
---                 @ysnStarted BIT = 0
+        DECLARE @i          INT,
+                @intSType   INT,
+                @str        NVARCHAR(10)='',
+                @ysnStarted BIT = 0
 
---         WHILE EXISTS(SELECT 1 FROM   @tbl)
---         BEGIN
---             IF @ysnStarted = 1
---                 SET @str += ','
+        WHILE EXISTS(SELECT 1 FROM   @tbl)
+        BEGIN
+            IF @ysnStarted = 1
+                SET @str += ','
 
---             SELECT TOP 1 @i = rowId - 1,
---                         @intSType = intStructureType
---             FROM   @tbl
+            SELECT TOP 1 @i = rowId,
+                        @intSType = intStructureType
+            FROM   @tbl
 
---             IF ( ISNULL(@ysnREOverrideLocation, 0) = 1
---                 AND @intSType = 3 )
---                 BEGIN
---                     SET @str += Cast(@i AS NVARCHAR(1))
---                     SET @ysnStarted = 1
---                 END
+            IF ( ISNULL(@ysnREOverrideLocation, 0) = 1
+                AND @intSType = 3 )
+                BEGIN
+                    SET @str += Cast(@i-1 AS NVARCHAR(1))
+                    SET @ysnStarted = 1
+                END
 
---             IF ( ISNULL(@ysnREOverrideLOB, 0) = 1
---                 AND @intSType = 5 )
---                 BEGIN
---                     SET @str += Cast(@i AS NVARCHAR(1))
---                     SET @ysnStarted = 1
---                 END
+            IF ( ISNULL(@ysnREOverrideLOB, 0) = 1
+                AND @intSType = 5 )
+                BEGIN
+                    SET @str += Cast(@i-1 AS NVARCHAR(1))
+                    SET @ysnStarted = 1
+                END
 
---             IF ( ISNULL(@ysnREOverrideCompany, 0) = 1
---                 AND @intSType = 6 )
---                 BEGIN
---                     SET @str += Cast(@i AS NVARCHAR(1))
---                     SET @ysnStarted = 1
---                 END
+            IF ( ISNULL(@ysnREOverrideCompany, 0) = 1
+                AND @intSType = 6 )
+                BEGIN
+                    SET @str += Cast(@i-1 AS NVARCHAR(1))
+                    SET @ysnStarted = 1
+                END
 
---             DELETE FROM @tbl WHERE  rowId = @i
---         END
+            DELETE FROM @tbl WHERE  rowId = @i
+        END
 
---         UPDATE tblGLCompanyPreferenceOption set strOverrideREArray = @str 
+        UPDATE tblGLCompanyPreferenceOption set strOverrideREArray = @str 
 
--- END
+END
 GO
