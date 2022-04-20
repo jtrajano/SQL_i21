668ce8dce72a6399eef8dfab60d49a53f1ad8290
Roudinee Panteria <roudinee.panteria@irely.com>
2022-04-20 09:52:30 +0800

    QC-633

--- i21Database/dbo/Views/vyuQMTestingSessionSummaryReport.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE VIEW vyuQMTestingSessionSummaryReport
-AS
-SELECT 
-	 strPropertyName
-	,strPropertyValue
-	,strResult
-FROM tblQMTestResult QMTR
-JOIN tblQMProperty QMP ON QMP.intPropertyId = QMTR.intPropertyId
