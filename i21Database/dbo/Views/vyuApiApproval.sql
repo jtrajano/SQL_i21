473ccaf4d99d9e5ace56af27511020f574de4067
Wendell Wayne Estrada <wendell.estrada@irely.com>
2022-04-20 11:09:46 +0800

    ICM-2

--- i21Database/dbo/Views/vyuApiApproval.sql
+++ i21Database/dbo/Views/vyuApiApproval.sql
@@ -9,6 +9,11 @@ SELECT
 	, a.strComment
 	, g.strApproverGroup
 	, e.strName strApprover
+	, a.ysnVisible
+	, a.intOrder
+	, s.strNamespace
 FROM tblSMApproval a
+INNER JOIN tblSMTransaction t ON t.intTransactionId = a.intTransactionId
+INNER JOIN tblSMScreen s ON s.intScreenId = t.intScreenId
 LEFT JOIN tblSMApproverGroup g ON g.intApproverGroupId = a.intApproverGroupId
 LEFT JOIN tblEMEntity e ON e.intEntityId = a.intApproverId
