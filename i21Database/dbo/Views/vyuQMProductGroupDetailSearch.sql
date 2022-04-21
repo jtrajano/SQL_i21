407e031d37bc135efd5a79b605a144cbc8221918
DESKTOP-0Q1GJ6F\iRelyNUC14 <kelvin.quinto@irely.com>
2022-04-21 21:31:47 +0800

    QC-631

--- i21Database/dbo/Views/vyuQMProductGroupDetailSearch.sql
+++ i21Database/dbo/Views/vyuQMProductGroupDetailSearch.sql
@@ -3,6 +3,7 @@ AS
 SELECT intPriorityGroupId		= PG.intPriorityGroupId
 	 , intPriorityGroupDetailId	= PGD.intPriorityGroupDetailId
 	 , intSortId				= PG.intSortId
+	 , intConcurrencyId			= PGD.intConcurrencyId
 	 , intSortDetailId			= PGD.intSortId
 	 , intProductTypeId			= PGD.intProductTypeId
 	 , intOriginId				= PGD.intOriginId
@@ -10,11 +11,11 @@ SELECT intPriorityGroupId		= PG.intPriorityGroupId
 	 , intItemId				= PGD.intItemId
 	 , strProductType			= PT.strDescription
 	 , strOrigin				= O.strDescription
-	 , strExtension				= E.strDescription
-	 , strItemDescription		= I.strDescription
-FROM tblQMPriorityGroup PG
-INNER JOIN tblQMPriorityGroupDetail PGD ON PG.intPriorityGroupId = PGD.intPriorityGroupId
+	 , strExtension				= PL.strDescription
+	 , strItemDescription		= ITEM.strItemNo
+FROM tblQMPriorityGroupDetail PGD
+LEFT JOIN tblQMPriorityGroup PG ON PGD.intPriorityGroupId = PG.intPriorityGroupId
 LEFT JOIN tblICCommodityAttribute PT ON PGD.intProductTypeId = PT.intCommodityAttributeId AND PT.strType = 'ProductType'
 LEFT JOIN tblICCommodityAttribute O ON PGD.intOriginId = O.intCommodityAttributeId AND O.strType = 'Origin'
-LEFT JOIN tblICCommodityAttribute E ON PGD.intExtensionId = E.intCommodityAttributeId AND E.strType = 'Season'
-LEFT JOIN tblICItem I ON PGD.intItemId = I.intItemId
+LEFT JOIN tblICCommodityProductLine PL ON PGD.intExtensionId = PL.intCommodityProductLineId
+LEFT JOIN tblICItem ITEM ON PGD.intItemId = ITEM.intItemId
