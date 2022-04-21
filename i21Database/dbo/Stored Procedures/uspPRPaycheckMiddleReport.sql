dfdab2f86c0ca72c3a1f7dc810454459f576c463
manny_armas <manny.armas@iRely.com>
2022-04-21 19:26:20 +0800

    PR-2603 - HDTN-270203 - Unable to print more than 5 pay checks at a time

--- i21Database/dbo/Stored Procedures/uspPRPaycheckMiddleReport.sql
+++ i21Database/dbo/Stored Procedures/uspPRPaycheckMiddleReport.sql
@@ -84,6 +84,9 @@ WHERE [fieldname] = 'strBatchId'
 -- Sanitize the parameters  
 SET @strTransactionId = CASE WHEN LTRIM(RTRIM(ISNULL(@strTransactionId, ''))) = '' THEN NULL ELSE @strTransactionId END  
 SET @strBatchId = CASE WHEN LTRIM(RTRIM(ISNULL(@strBatchId, ''))) = '' THEN NULL ELSE @strBatchId END  
+
+SELECT strValues into #tmpChecks FROM dbo.fnARGetRowsFromDelimitedValues(@strTransactionId)    
+
 -- Report Query:  
 SELECT DISTINCT 
 	tblCMBankTransaction.dtmDate,
@@ -135,8 +138,8 @@ SELECT DISTINCT
 	dblDeductionYTD = Sum(ISNULL(tblPaycheckYTD.dblDeductionTotalYTD, 0)),
 	dblNetYTD = Sum(ISNULL(tblPaycheckYTD.dblNetPayTotalYTD, 0))
 FROM 
-	tblSMCompanySetup, 
-	(SELECT
+	tblSMCompanySetup
+	inner join (SELECT
 		intPaycheckId, 
 		strPaycheckId,
 		intEntityEmployeeId,
@@ -151,7 +154,8 @@ FROM
 	FROM 
 		tblPRPaycheck
 	WHERE 
-		ysnPosted = 1 AND ysnVoid = 0) [tblPRPaycheck] 
+		ysnPosted = 1 AND tblPRPaycheck.strPaycheckId IN (select strValues COLLATE Latin1_General_CI_AS from #tmpChecks)
+	) [tblPRPaycheck] on 1=1
 	LEFT JOIN 
 		(SELECT 
 			tblPREmployee.*,
@@ -218,7 +222,7 @@ FROM
 WHERE 
 	tblCMBankTransaction.intBankTransactionTypeId = 21
 	AND tblCMBankTransaction.intBankAccountId = @intBankAccountId 
-	AND tblCMBankTransaction.strTransactionId IN (SELECT strValues COLLATE Latin1_General_CI_AS FROM dbo.fnARGetRowsFromDelimitedValues(@strTransactionId))
+	AND tblCMBankTransaction.strTransactionId IN  (select strValues COLLATE Latin1_General_CI_AS from #tmpChecks)
 GROUP BY 
 	tblSMCompanySetup.strCompanyName,
 	tblSMCompanySetup.strAddress, tblSMCompanySetup.strCity,
