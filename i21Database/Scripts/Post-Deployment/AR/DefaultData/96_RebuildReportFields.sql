426a3c4bd47a53c68bf5f6f55614b9e461c08a11
Jeremiah Villanueva <jeremiah.villanueva@irely.com>
2022-04-21 15:33:11 +0800

    AR-14556

--- i21Database/Scripts/Post-Deployment/AR/DefaultData/96_RebuildReportFields.sql
+++ i21Database/Scripts/Post-Deployment/AR/DefaultData/96_RebuildReportFields.sql
@@ -6,5 +6,10 @@ BEGIN
 	EXEC [dbo].[uspARUpdateInvoiceReportFields] DEFAULT, 1
 END
 
+IF EXISTS ( SELECT * FROM sys.objects WHERE type = 'P' AND name = 'uspARUpdatePaymentInvoiceField')
+BEGIN
+	EXEC [dbo].[uspARUpdatePaymentInvoiceField] DEFAULT, 1
+END
+
 PRINT ' ********************** END - Rebuild AR Report Fields  **********************'
 GO
