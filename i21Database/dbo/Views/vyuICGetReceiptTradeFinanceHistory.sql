e3a5bbc618c01aadd08e678fc31a71fffed888db
Feb Montefrio <feb.montefrio@irely.com>
2022-04-22 18:29:52 +0800

    [F] IC-10279

--- i21Database/dbo/Views/vyuICGetReceiptTradeFinanceHistory.sql
+++ i21Database/dbo/Views/vyuICGetReceiptTradeFinanceHistory.sql
@@ -5,21 +5,32 @@ AS
 		,tfh.* 
 	FROM 
 		tblICInventoryReceipt r 
+		-- Get the TF related to the IR. 
+		CROSS APPLY (
+			SELECT DISTINCT 
+				tfh.intTradeFinanceId
+			FROM 
+				tblTRFTradeFinanceHistory tfh
+			WHERE
+				tfh.strTransactionNumber = r.strReceiptNumber						
+		) tf
+		-- Get the first log in the history. 
 		CROSS APPLY (
 			SELECT 
 				intTradeFinanceHistoryId = MIN (tfh.intTradeFinanceHistoryId)  
 			FROM 
 				tblTRFTradeFinanceHistory tfh
 			WHERE
-				tfh.strTradeFinanceNumber = r.strTradeFinanceNumber 		
+				tfh.intTradeFinanceId = tf.intTradeFinanceId
 		) firstLog 
+		-- Get all the related history records. 
 		CROSS APPLY (
 			SELECT 
 				* 
 			FROM 
 				tblTRFTradeFinanceHistory tfh
 			WHERE
-				tfh.strTradeFinanceNumber = r.strTradeFinanceNumber 
+				tfh.intTradeFinanceId = tf.intTradeFinanceId
 				AND (
 					tfh.strTransactionNumber = r.strReceiptNumber
 					OR tfh.intTradeFinanceHistoryId <= firstLog.intTradeFinanceHistoryId
