ccb4aa28bd34da55cbc2c88ebe2aa914de4062e8
Roudinee Panteria <roudinee.panteria@irely.com>
2022-04-25 17:12:43 +0800

    AR-14545

--- i21Database/dbo/Stored Procedures/uspARInvoiceWalterMatterReport.sql
+++ i21Database/dbo/Stored Procedures/uspARInvoiceWalterMatterReport.sql
@@ -60,6 +60,10 @@ INSERT INTO tblARInvoiceReportStagingTable (
 	,strMVessel
 	,strPaymentComments
 	,blbLogo
+	,strBankName
+	,strIBAN
+	,strSWIFT
+	,strBICCode
 )
 SELECT
 	 intInvoiceId			= ARI.intInvoiceId
@@ -95,6 +99,10 @@ SELECT
 	,strMVessel				= LGL.strMVessel
 	,strPaymentComments		= ARI.strTradeFinanceComments
 	,blbLogo                = @blbLogo
+	,strBankName			= CMB.strBankName
+	,strIBAN				= CMBA.strIBAN
+	,strSWIFT				= CMBA.strSWIFT
+	,strBICCode				= CMBA.strBICCode
 FROM dbo.tblARInvoice ARI WITH (NOLOCK)
 INNER JOIN #WALTERMATTERINVOICES SELECTEDINV ON ARI.intInvoiceId = SELECTEDINV.intInvoiceId
 INNER JOIN vyuARCustomerSearch ARCS WITH (NOLOCK) ON ARI.intEntityCustomerId = ARCS.intEntityId
@@ -104,3 +112,5 @@ LEFT JOIN vyuCTContractDetailView CTCDV WITH (NOLOCK) ON ARGID.intContractDetail
 LEFT JOIN tblICCommodity ICC WITH (NOLOCK) ON CTCDV.intCommodityId = ICC.intCommodityId
 LEFT JOIN tblLGLoad LGL WITH (NOLOCK) ON ARGID.strDocumentNumber = LGL.strLoadNumber AND ISNULL(ARGID.intLoadDetailId, 0) <> 0 AND ISNULL(LGL.strBLNumber,'') <> ''
 LEFT JOIN tblSMTerm SMT WITH (NOLOCK) ON ARI.intTermId = SMT.intTermID
+LEFT JOIN tblCMBankAccount CMBA WITH (NOLOCK) ON ISNULL(ISNULL(ARI.intPayToCashBankAccountId, ARI.intDefaultPayToBankAccountId), 0) = CMBA.intBankAccountId
+LEFT JOIN tblCMBank CMB WITH (NOLOCK) ON CMBA.intBankId = CMB.intBankId
