0132c6eb19fade2d424b14913972f65279b86514
Mark Daniel Marmeto <mark.marmeto@irely.com>
2022-04-26 16:41:28 +0800

    GL-8892

--- i21Database/dbo/Functions/fnCMCashFlowTransactions.sql
+++ i21Database/dbo/Functions/fnCMCashFlowTransactions.sql
@@ -117,3 +117,26 @@ RETURN
             ELSE CASE WHEN (BTR.dtmDate <= @dtmTo) THEN 1 ELSE 0 END
             END
         ) = 1
+    UNION ALL
+    SELECT 
+        intTransactionId = UF.intSourceTransactionId,
+        strTransactionId = UF.strSourceTransactionId,
+        intBankAccountId = UF.intBankAccountId,
+        intCurrencyId = UF.intCurrencyId,
+        dtmDate = UF.dtmDate,
+        dblAmount = UF.dblAmount,
+        intGLAccountId = BA.intGLAccountId,
+		intCompanyLocationId = UF.intLocationId,
+        strTransactionType = UF.strPaymentMethod
+    FROM [dbo].[tblCMUndepositedFund] UF
+    JOIN [dbo].[tblCMBankAccount] BA
+        ON BA.intBankAccountId = UF.intBankAccountId
+    WHERE 
+        UF.intBankAccountId = @intBankAccountId
+        AND intBankDepositId IS NULL
+        AND 
+        (CASE WHEN @dtmFrom IS NOT NULL
+            THEN CASE WHEN (UF.dtmDate BETWEEN @dtmFrom AND @dtmTo) THEN 1 ELSE 0 END
+            ELSE CASE WHEN (UF.dtmDate <= @dtmTo) THEN 1 ELSE 0 END
+            END
+        ) = 1
