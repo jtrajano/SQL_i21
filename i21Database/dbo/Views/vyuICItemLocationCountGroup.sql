bf45e7fabf3d23acd7468e05e3edb83c2cb13f12
stunktoy <valenzuela.jonathan@outlook.com>
2022-04-26 15:46:22 +0800

    IC-10099

---
+++ i21Database/dbo/Views/vyuICItemLocationCountGroup.sql
@@ -0,0 +1,23 @@
+﻿CREATE VIEW [dbo].[vyuICItemLocationCountGroup]
+AS 
+SELECT ItemLocation.intItemLocationId
+	  ,ItemLocation.intLocationId
+	  ,Item.intItemId
+	  ,Item.intCategoryId	  
+	  ,ItemLocation.intCountGroupId
+	  ,Item.strItemNo
+	  ,Item.strDescription AS strItemDescription
+	  ,ItemCategory.strDescription AS strCategoryDescription
+	  ,ItemCategory.strCategoryCode
+	  ,CompanyLocation.strLocationName
+	  ,CountGroup.strCountGroup
+	  ,ItemLocation.dtmDateCreated
+	  ,ItemLocation.dtmDateModified
+	  ,ItemLocation.intModifiedByUserId
+	  ,ItemLocation.intCreatedByUserId
+	  ,ItemLocation.intConcurrencyId
+FROM tblICItemLocation AS ItemLocation
+LEFT JOIN tblSMCompanyLocation AS CompanyLocation ON ItemLocation.intLocationId = CompanyLocation.intCompanyLocationId
+LEFT JOIN tblICItem AS Item ON ItemLocation.intItemId = Item.intItemId
+LEFT JOIN tblICCategory AS ItemCategory ON Item.intCategoryId = ItemCategory.intCategoryId
+LEFT JOIN tblICCountGroup AS CountGroup ON ItemLocation.intCountGroupId = CountGroup.intCountGroupId;
---
+++ i21Database/dbo/Views/vyuICSearchCountGroupItem.sql
@@ -0,0 +1,23 @@
+﻿CREATE VIEW [dbo].[vyuICSearchCountGroupItem]
+	AS 
+
+SELECT Item.intItemId
+	  ,Item.strItemNo
+	  ,Item.strDescription AS strItemDescription
+	  ,CountGroup.strCountGroup
+	  ,CountGroup.intCountGroupId
+	  ,CountGroup.intCountsPerYear
+	  ,CountGroup.ysnIncludeOnHand
+	  ,CountGroup.ysnScannedCountEntry
+	  ,CountGroup.ysnCountByLots
+	  ,CountGroup.ysnCountByPallets
+	  ,CountGroup.ysnRecountMismatch
+	  ,CountGroup.ysnExternal
+	  ,ItemCategory.strDescription AS strCategoryDescription
+	  ,CompanyLocation.strLocationName
+FROM tblICItem AS Item 
+LEFT JOIN tblICItemLocation AS ItemLocation ON Item.intItemId = ItemLocation.intItemId 
+LEFT JOIN tblSMCompanyLocation AS CompanyLocation ON ItemLocation.intLocationId = CompanyLocation.intCompanyLocationId
+LEFT JOIN tblICCategory AS ItemCategory ON Item.intCategoryId = ItemCategory.intCategoryId
+LEFT JOIN tblICCountGroup AS CountGroup ON ItemLocation.intCountGroupId = CountGroup.intCountGroupId
+WHERE ItemLocation.intCountGroupId IS NOT NULL;
