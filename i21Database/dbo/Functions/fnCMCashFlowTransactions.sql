e7b1ec5654848bfb89900be5016e4aac8a383a11
Mark Daniel Marmeto <mark.marmeto@irely.com>
2022-04-26 18:24:51 +0800

    GL-8892

--- i21Database/dbo/Functions/fnCMCashFlowTransactions.sql
+++ i21Database/dbo/Functions/fnCMCashFlowTransactions.sql
@@ -5,8 +5,6 @@
     @dtmTo DATETIME
 )
 RETURNS TABLE
-WITH
-    SCHEMABINDING
 AS
 RETURN
     SELECT
@@ -119,24 +117,13 @@ RETURN
         ) = 1
     UNION ALL
     SELECT 
-        intTransactionId = UF.intSourceTransactionId,
-        strTransactionId = UF.strSourceTransactionId,
-        intBankAccountId = UF.intBankAccountId,
-        intCurrencyId = UF.intCurrencyId,
-        dtmDate = UF.dtmDate,
-        dblAmount = UF.dblAmount,
-        intGLAccountId = BA.intGLAccountId,
-		intCompanyLocationId = UF.intLocationId,
-        strTransactionType = UF.strPaymentMethod
-    FROM [dbo].[tblCMUndepositedFund] UF
-    JOIN [dbo].[tblCMBankAccount] BA
-        ON BA.intBankAccountId = UF.intBankAccountId
-    WHERE 
-        UF.intBankAccountId = @intBankAccountId
-        AND intBankDepositId IS NULL
-        AND 
-        (CASE WHEN @dtmFrom IS NOT NULL
-            THEN CASE WHEN (UF.dtmDate BETWEEN @dtmFrom AND @dtmTo) THEN 1 ELSE 0 END
-            ELSE CASE WHEN (UF.dtmDate <= @dtmTo) THEN 1 ELSE 0 END
-            END
-        ) = 1
+        intTransactionId,
+        strTransactionId,
+        intBankAccountId,
+        intCurrencyId,
+        dtmDate,
+        dblAmount,
+        intGLAccountId,
+		intCompanyLocationId,
+        strTransactionType
+    FROM [dbo].[fnCMGetUndepositedFundsForCashFlowReport](@intBankAccountId, @dtmFrom, @dtmTo)
---
+++ i21Database/dbo/Functions/fnCMGetUndepositedFundsForCashFlowReport.sql
@@ -0,0 +1,30 @@
+﻿CREATE FUNCTION [dbo].[fnCMGetUndepositedFundsForCashFlowReport]
+(
+	@intBankAccountId INT,
+    @dtmFrom DATETIME,
+    @dtmTo DATETIME
+)
+RETURNS TABLE
+AS
+RETURN SELECT 
+        intTransactionId = UF.intSourceTransactionId,
+        strTransactionId = UF.strSourceTransactionId,
+        intBankAccountId = UF.intBankAccountId,
+        intCurrencyId = UF.intCurrencyId,
+        dtmDate = UF.dtmDate,
+        dblAmount = ISNULL(UF.dblAmount, 0),
+        intGLAccountId = BA.intGLAccountId,
+		intCompanyLocationId = UF.intLocationId,
+        strTransactionType = UF.strPaymentMethod
+    FROM [dbo].[tblCMUndepositedFund] UF
+    JOIN [dbo].[tblCMBankAccount] BA
+        ON BA.intBankAccountId = UF.intBankAccountId
+    WHERE 
+        UF.intBankAccountId = @intBankAccountId
+        AND intBankDepositId IS NULL
+        AND 
+        (CASE WHEN @dtmFrom IS NOT NULL
+            THEN CASE WHEN (UF.dtmDate BETWEEN @dtmFrom AND @dtmTo) THEN 1 ELSE 0 END
+            ELSE CASE WHEN (UF.dtmDate <= @dtmTo) THEN 1 ELSE 0 END
+            END
+        ) = 1
