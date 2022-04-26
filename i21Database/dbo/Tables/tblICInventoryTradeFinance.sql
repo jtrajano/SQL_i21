727935d640beb449a39f7f008aceaf7d127cd6ba
Feb Montefrio <feb.montefrio@irely.com>
2022-04-26 16:57:33 +0800

    [F] IC-10126

---
+++ i21Database/dbo/Tables/tblICInventoryTradeFinance.sql
@@ -0,0 +1,39 @@
+﻿/*
+## Overview
+Type the overview for the table here. 
+
+## Fields, description, and mapping. 
+*	Type the field name here
+	Type the description of the field here
+	Maps: Type the mapping to origin. Type None if not applicable.
+
+
+## Source Code:
+*/
+CREATE TABLE [dbo].[tblICInventoryTradeFinance]
+(
+	[intInventoryTradeFinanceId] [int] IDENTITY NOT NULL,
+	[strTradeFinanceNumber] NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL,
+	[intBankId] INT NULL,
+	[intBankAccountId] INT NULL,
+	[intBorrowingFacilityId] INT NULL,
+	[strBankReferenceNo] NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL,	
+	[intLimitTypeId] INT NULL,
+	[intSublimitTypeId] INT NULL,
+	[ysnSubmittedToBank] BIT NULL, 
+	[dtmDateSubmitted] DATETIME NULL,
+	[strApprovalStatus] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL,	
+	[dtmDateApproved] DATETIME NULL,
+	[strWarrantNo] NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL,	
+	[intWarrantStatus] INT NULL,
+	[strReferenceNo] NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL,	
+	[intOverrideFacilityValuation] INT NULL,
+	[strComments] NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NULL,	
+
+	CONSTRAINT [PK_tblICInventoryTradeFinance] PRIMARY KEY ([intInventoryTradeFinanceId]), 
+	CONSTRAINT [AK_tblICInventoryTradeFinance_strTradeFinanceNumber] UNIQUE ([strTradeFinanceNumber]), 
+	CONSTRAINT [FK_tblICInventoryTradeFinance_tblCMBank_intBankId] FOREIGN KEY ([intBankId]) REFERENCES [tblCMBank] ([intBankId]),
+	CONSTRAINT [FK_tblICInventoryTradeFinance_tblCMBankAccount_intBankAccountId] FOREIGN KEY ([intBankAccountId]) REFERENCES [tblCMBankAccount]([intBankAccountId]),
+	CONSTRAINT [FK_tblICInventoryTradeFinance_tblCMBorrowingFacility_intBorrowingFacilityId] FOREIGN KEY ([intBorrowingFacilityId]) REFERENCES [tblCMBorrowingFacility] ([intBorrowingFacilityId])
+)
+GO
---
+++ i21Database/dbo/Tables/tblICInventoryTradeFinanceLot.sql
@@ -0,0 +1,26 @@
+﻿/*
+## Overview
+Type the overview for the table here. 
+
+## Fields, description, and mapping. 
+*	Type the field name here
+	Type the description of the field here
+	Maps: Type the mapping to origin. Type None if not applicable.
+
+
+## Source Code:
+*/
+CREATE TABLE [dbo].[tblICInventoryTradeFinanceLot]
+(
+	[intInventoryTradeFinanceLotId] [int] IDENTITY NOT NULL,
+	[intInventoryTradeFinanceId] [int] NOT NULL,
+	[intItemId] [int] NOT NULL,
+	[intLotId] [int] NOT NULL,
+	[strWarrantNo] NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL,	
+	[intWarrantStatus] INT NULL,
+
+	CONSTRAINT [PK_tblICInventoryTradeFinanceLot] PRIMARY KEY ([intInventoryTradeFinanceLotId]), 
+	CONSTRAINT [FK_tblICInventoryTradeFinanceLot_intItemId] FOREIGN KEY ([intItemId]) REFERENCES [tblICItem] ([intItemId]),
+	CONSTRAINT [FK_tblICInventoryTradeFinanceLot_intLotId] FOREIGN KEY ([intLotId]) REFERENCES [tblICLot] ([intLotId]),
+)
+GO
