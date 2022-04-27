4f3d9d54f02b2749ba456a049b395979fde357fe
Feb Montefrio <feb.montefrio@irely.com>
2022-04-27 11:35:01 +0800

    [F] IC-10121

--- i21Database/dbo/Views/vyuICItemLot.sql
+++ i21Database/dbo/Views/vyuICItemLot.sql
@@ -49,6 +49,8 @@ SELECT
 	, lot.ysnInsuranceClaimed
 	, lot.ysnRejected
 	, lot.strRejectedBy
+	, tf.strTradeFinanceNumber
+	, tf.intTradeFinanceId
 FROM tblICLot lot
 	INNER JOIN tblICItem item ON item.intItemId = lot.intItemId
 	LEFT JOIN tblSMCompanyLocation loc ON loc.intCompanyLocationId = lot.intLocationId
@@ -76,3 +78,4 @@ FROM tblICLot lot
 	LEFT JOIN tblICCommodityAttribute Class ON Class.intCommodityAttributeId = item.intClassVarietyId
 	LEFT JOIN tblICCommodityProductLine ProductLine ON ProductLine.intCommodityProductLineId = item.intProductLineId
 	LEFT JOIN tblICWarrantStatus WarrantStatus ON WarrantStatus.intWarrantStatus = lot.intWarrantStatus
+	LEFT JOIN tblTRFTradeFinance tf ON tf.intTradeFinanceId = lot.intTradeFinanceId
