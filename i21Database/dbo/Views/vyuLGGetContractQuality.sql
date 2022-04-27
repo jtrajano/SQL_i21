bcf28a1a9824e80dc3e2a069632a02988dc8cf38
Bryan-PC <013.bryan@gmail.com>
2022-04-27 12:46:20 +0800

    CT-6940

---
+++ i21Database/dbo/Views/vyuLGGetContractQuality.sql
@@ -0,0 +1,38 @@
+﻿CREATE VIEW [dbo].[vyuLGGetContractQuality]
+AS
+
+Select
+	CQ.intQualityId
+	,CQ.intContractDetailId
+	,CQ.intItemId
+	,IC.strItemNo
+	,CQ.intPropertyId
+	,CQ.strPropertyName
+	,CQ.dblTargetValue
+	,CQ.dblMinValue
+	,CQ.dblMaxValue
+	,CQ.dblFactorOverTarget
+	,CQ.dblPremium
+	,CQ.dblFactorUnderTarget
+	,CQ.dblDiscount
+	,CQ.strCostMethod
+	,CQ.intCurrencyId
+	,CQ.strCurrency
+	,CQ.intUnitMeasureId
+	,CQ.strUnitMeasure
+	,CQ.strEscalatedBy
+	,CQ.dblActualValue
+	,CQ.dblResult
+	,CQ.intSequenceCurrencyId
+	,CQ.strSequenceCurrency
+	,CQ.intSequenceUnitMeasureId
+	,CQ.strSequenceUnitMeasure
+	,CQ.dblFXRate
+	,CQ.dblAmount
+	,CQ.intConcurrencyId
+FROM tblCTContractQuality CQ
+INNER JOIN tblCTContractDetail CD on CD.intContractDetailId = CQ.intContractDetailId
+INNER JOIN tblICItem IC on CQ.intItemId = IC.intItemId
+
+GO
+
