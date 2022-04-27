93f99b73069ad1fbbaa2b4c0ec557db48f302a07
Jayson Ramos <jayson.ramos@iRely.com>
2022-04-27 16:40:50 +0800

    CT-7161
    (cherry picked from commit 6107c0300b331c82ff3d482a06c700f9cbe99f24)
    (cherry picked from commit 014c531fe5bbe5afe2a34f4c4c0c44efb066b4cc)
    (cherry picked from commit d45a67ae218a806156a3bad1f65936c13c37164c)
    (cherry picked from commit 984de9178f95fc1a2bb62c0ccf93b164aa16cbc3)
    (cherry picked from commit b807fa93d65e3fc80e71f2140f1c53852d572a53)

--- i21Database/dbo/Stored Procedures/uspCTSaveContract.sql
+++ i21Database/dbo/Stored Procedures/uspCTSaveContract.sql
@@ -248,6 +248,27 @@ BEGIN TRY
 	
 	WHILE ISNULL(@intContractDetailId,0) > 0
 	BEGIN
+
+		if (isnull(@ysnMultiplePriceFixation,0) <> 1)
+		begin
+			update cd
+			set
+				cd.intPricingTypeId = 2
+				,cd.dblFutures = NULL
+				,cd.dblCashPrice = NULL
+				,cd.dblTotalCost = NULL
+			from @CDTableUpdate cd
+			left join (
+				select pf.intContractDetailId, dblPricedQuantity = sum(fd.dblQuantity)
+				from tblCTPriceFixation pf
+				join tblCTPriceFixationDetail fd on fd.intPriceFixationId = pf.intPriceFixationId
+				where pf.intContractDetailId = @intContractDetailId
+				group by pf.intContractDetailId
+			)p on p.intContractDetailId = cd.intContractDetailId
+			where cd.intContractDetailId = @intContractDetailId
+			and isnull(p.dblPricedQuantity,0) < cd.dblQuantity
+		end
+
 		SELECT	@intPricingTypeId	=	NULL,
 				@dblCashPrice		=	NULL,
 				@dblBasis			=	NULL,
