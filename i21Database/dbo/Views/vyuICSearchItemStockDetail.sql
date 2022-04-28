42eb9bbcaf09c2a709d5ddd804b46c8053112c67
Jonathan Valenzuela <valenzuela.jonathan@outlook.com>
2022-04-28 15:45:19 +0800

    IC-8944

---
+++ i21Database/dbo/Views/vyuICSearchItemStockDetail.sql
@@ -0,0 +1,25 @@
+﻿CREATE VIEW [dbo].[vyuICSearchItemStockDetail]
+AS 
+SELECT ItemStockDetail.intItemStockDetailId
+	   ,ItemStockDetail.intItemStockTypeId
+	   ,Item.strItemNo
+	   ,Item.strDescription
+	   ,Item.strType
+	   ,ItemCommodity.strDescription AS strCommodityDescription
+       ,ItemCategory.strDescription AS strCategoryDescription
+	   ,CompanyLocation.strLocationName
+	   ,ItemStockDetail.strTransactionId
+	   ,ItemStockType.strName AS strStockTypeName
+	   ,UnitMeasure.strUnitMeasure
+	   ,Item.strLotTracking
+	   ,ItemStockDetail.dblQty
+FROM tblICItemStockDetail AS ItemStockDetail
+LEFT JOIN tblICItemStockType AS ItemStockType ON ItemStockDetail.intItemStockTypeId = ItemStockType.intItemStockTypeId
+LEFT JOIN tblICItem AS Item ON ItemStockDetail.intItemId = Item.intItemId
+LEFT JOIN tblICCommodity AS ItemCommodity ON Item.intCommodityId = ItemCommodity.intCommodityId
+LEFT JOIN tblICCategory AS ItemCategory ON Item.intCategoryId = ItemCategory.intCategoryId
+LEFT JOIN tblICItemLocation AS ItemLocation ON ItemStockDetail.intItemLocationId = ItemLocation.intItemLocationId
+LEFT JOIN tblSMCompanyLocation AS CompanyLocation ON ItemLocation.intLocationId = CompanyLocation.intCompanyLocationId
+LEFT JOIN tblICItemUOM AS ItemUnitOfMeasure ON ItemStockDetail.intItemUOMId = ItemUnitOfMeasure.intItemUOMId
+LEFT JOIN tblICUnitMeasure AS UnitMeasure ON ItemUnitOfMeasure.intUnitMeasureId = UnitMeasure.intUnitMeasureId
+
---
+++ i21Database/dbo/Views/vyuICSearchItemStockDetailOnHand.sql
@@ -0,0 +1,22 @@
+﻿CREATE VIEW [dbo].[vyuICSearchItemStockDetailOnHand]
+AS 
+SELECT  InventoryTransaction.intInventoryTransactionId
+	   ,Item.strItemNo
+	   ,Item.strDescription
+	   ,Item.strType
+	   ,ItemCommodity.strDescription AS strCommodityDescription
+       ,ItemCategory.strDescription AS strCategoryDescription
+	   ,CompanyLocation.strLocationName
+	   ,InventoryTransaction.strTransactionId
+	   ,UnitMeasure.strUnitMeasure
+	   ,Item.strLotTracking
+	   ,InventoryTransaction.dblQty
+FROM tblICInventoryTransaction AS InventoryTransaction
+LEFT JOIN tblICItem AS Item ON InventoryTransaction.intItemId = Item.intItemId
+LEFT JOIN tblICCommodity AS ItemCommodity ON Item.intCommodityId = ItemCommodity.intCommodityId
+LEFT JOIN tblICCategory AS ItemCategory ON Item.intCategoryId = ItemCategory.intCategoryId
+LEFT JOIN tblICItemLocation AS ItemLocation ON InventoryTransaction.intItemLocationId = ItemLocation.intItemLocationId
+LEFT JOIN tblSMCompanyLocation AS CompanyLocation ON ItemLocation.intLocationId = CompanyLocation.intCompanyLocationId
+LEFT JOIN tblICItemUOM AS ItemUnitOfMeasure ON InventoryTransaction.intItemUOMId = ItemUnitOfMeasure.intItemUOMId
+LEFT JOIN tblICUnitMeasure AS UnitMeasure ON ItemUnitOfMeasure.intUnitMeasureId = UnitMeasure.intUnitMeasureId
+WHERE strUnitMeasure IS NOT NULL
