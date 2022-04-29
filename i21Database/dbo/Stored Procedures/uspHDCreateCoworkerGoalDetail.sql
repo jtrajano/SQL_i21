887cdbdaa54cccdf5400c0fa944f82943f2731be
Edsel Roldan Magura <edsel.magura@iRely.com>
2022-04-29 18:35:23 +0800

    HD-3871

---
+++ i21Database/dbo/Stored Procedures/uspHDCreateCoworkerGoalDetail.sql
@@ -0,0 +1,45 @@
+﻿CREATE PROCEDURE [dbo].[uspHDCreateCoworkerGoalDetail]
+(
+	 @CoworkerGoalId INT
+)
+AS
+BEGIN
+	DECLARE  @intTimeEntryPeriodDetail INT = NULL,
+			  @strFiscalYear NVARCHAR(10)
+
+	SELECT TOP 1 @strFiscalYear = strFiscalYear
+	FROM tblHDCoworkerGoal
+	WHERE intCoworkerGoalId = @CoworkerGoalId
+
+	SELECT TOP 1 @intTimeEntryPeriodDetail = intTimeEntryPeriodId
+	FROM tblHDTimeEntryPeriod
+	WHERE strFiscalYear = @strFiscalYear
+
+	IF @intTimeEntryPeriodDetail IS NULL OR
+	EXISTS(
+			SELECT TOP 1 ''
+			FROM tblHDCoworkerGoalDetail
+			WHERE intCoworkerGoalId = @CoworkerGoalId
+	)
+	BEGIN
+		RETURN
+	END
+
+	INSERT INTO tblHDCoworkerGoalDetail
+	(
+		 [intCoworkerGoalId]
+		,[intBillingPeriod]
+		,[strBillingPeriodName]
+		,[dblBudget]
+		,[intUtilization]
+	)
+	SELECT  [intCoworkerGoalId]			= @CoworkerGoalId
+		   ,[intBillingPeriod]			= TimeEntryPeriodDetail.intBillingPeriod
+		   ,[strBillingPeriodName]		= TimeEntryPeriodDetail.strBillingPeriodName
+		   ,[dblBudget]					= 0
+		   ,[intUtilization]			= 0
+    FROM tblHDTimeEntryPeriodDetail TimeEntryPeriodDetail
+	WHERE intTimeEntryPeriodId = @intTimeEntryPeriodDetail
+END
+
+GO
---
+++ i21Database/dbo/Views/vyuHDCoworkerGoal.sql
@@ -0,0 +1,32 @@
+﻿CREATE VIEW [dbo].[vyuHDCoworkerGoal]
+AS
+SELECT   [intCoworkerGoalId]				= CoworkerGoal.[intCoworkerGoalId]			
+		,[intEntityId]						= CoworkerGoal.[intEntityId]	
+		,[strFiscalYear]					= CoworkerGoal.[strFiscalYear]	
+		,[intCurrencyId]					= CoworkerGoal.[intCurrencyId]		
+		,[intUtilizationTargetAnnual]		= CoworkerGoal.[intUtilizationTargetAnnual]
+		,[intUtilizationTargetWeekly]		= CoworkerGoal.[intUtilizationTargetWeekly]
+		,[intUtilizationTargetMonthly]		= CoworkerGoal.[intUtilizationTargetMonthly]
+		,[dblAnnualHurdle]					= CoworkerGoal.[dblAnnualHurdle]
+		,[dblIncentiveRate]					= CoworkerGoal.[dblIncentiveRate]
+		,[dblAnnualBudget]					= CoworkerGoal.[dblAnnualBudget]
+		,[intCommissionAccountId]			= CoworkerGoal.[intCommissionAccountId]
+		,[intRevenueAccountId]				= CoworkerGoal.[intRevenueAccountId]
+		,[strGoal]							= CoworkerGoal.[strGoal]
+		,[intConcurrencyId] 				= CoworkerGoal.[intConcurrencyId]
+		,[strFullName]						= Entity.[strName]
+		,[strCurrency]						= Currency.[strCurrency]
+		,[strCommissionAccount]				= GLAccountCommission.strDescription
+		,[strRevenueAccount]				= GLAccountRevenue.strDescription
+FROM tblHDCoworkerGoal CoworkerGoal
+		INNER JOIN tblEMEntity Entity
+ON Entity.intEntityId = CoworkerGoal.intEntityId
+		INNER JOIN tblSMCurrency Currency
+ON Currency.intCurrencyID = CoworkerGoal.intCurrencyId
+		LEFT JOIN tblGLAccount GLAccountCommission
+ON GLAccountCommission.intAccountId = CoworkerGoal.intCommissionAccountId
+		LEFT JOIN tblGLAccount GLAccountRevenue
+ON GLAccountRevenue.intAccountId = CoworkerGoal.intRevenueAccountId
+
+		
+GO 
