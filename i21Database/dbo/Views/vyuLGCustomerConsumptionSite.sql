0ad630c8172213fa1a6c14a0238c7094c5d17c26
Arman Arenas <arman.arenas@irely.com>
2022-05-05 20:05:02 +0800

    LG-3362 added vyuLGCustomerConsumptionSite

---
+++ i21Database/dbo/Views/vyuLGCustomerConsumptionSite.sql
@@ -0,0 +1,25 @@
+﻿CREATE VIEW [dbo].[vyuLGCustomerConsumptionSite]
+AS  
+	SELECT 
+		A.intSiteID
+		,strSiteID = RIGHT('000'+ CAST(A.intSiteNumber AS NVARCHAR(4)),4)  COLLATE Latin1_General_CI_AS
+		,A.strDescription
+		,A.intCustomerID
+		,C.strName
+		,C.strEntityNo
+		,D.intEntityLocationId
+		,D.strLocationName
+		,B.intCurrentSiteNumber
+		,ysnLocationActive = D.ysnActive
+		,ysnSiteActive = A.ysnActive 
+		,A.intConcurrencyId
+	FROM tblTMSite A
+	INNER JOIN tblTMCustomer B
+		ON A.intCustomerID = B.intCustomerID
+	INNER JOIN tblEMEntity C
+		ON B.intCustomerNumber = C.intEntityId
+	INNER JOIN tblEMEntityLocation D
+		ON C.intEntityId = D.intEntityId 
+	INNER JOIN tblEMEntityLocationConsumptionSite E
+		ON D.intEntityLocationId = E.intEntityLocationId
+GO
