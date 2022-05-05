555c69d651e79ea4c8162e251ebaba9efe9f17f7
Feb Montefrio <feb.montefrio@irely.com>
2022-05-05 16:14:40 +0800

    [B] IC-10307

--- Integration/Scripts/IC/uspICDCUomMigrationPt.sql
+++ Integration/Scripts/IC/uspICDCUomMigrationPt.sql
@@ -17,21 +17,61 @@ SET ANSI_WARNINGS OFF
 -- UnitMeasure data migration from ptuommst origin table to tblICUnitMeasure i21 table 
 -- This does not insert duplicates
 --------------------------------------------------------------------------------------------------------------------------------------------
+DECLARE @UOMS TABLE (
+	strUnitMeasure NVARCHAR(100) COLLATE Latin1_General_CI_AS
+	, strSymbol NVARCHAR(100) COLLATE Latin1_General_CI_AS
+)
+
+INSERT INTO @UOMS
+SELECT DISTINCT
+	  strUnitMeasure	= LTRIM(RTRIM(ptuom_desc)) COLLATE Latin1_General_CI_AS 
+	, strSymbol			= LTRIM(RTRIM(ptuom_code)) COLLATE Latin1_General_CI_AS
+FROM ptuommst
+WHERE RTRIM(LTRIM(ptuom_code)) <> ''
+
+UNION ALL
+SELECT DISTINCT
+	  strUnitMeasure	= LTRIM(RTRIM(ptpkg_desc)) COLLATE Latin1_General_CI_AS 
+	, strSymbol			= LTRIM(RTRIM(ptpkg_code)) COLLATE Latin1_General_CI_AS
+FROM ptpkgmst
+WHERE RTRIM(LTRIM(ptpkg_code)) <> ''
+
+UNION ALL 
+SELECT DISTINCT
+	  strUnitMeasure	= LTRIM(RTRIM(ptitm_unit)) COLLATE Latin1_General_CI_AS 
+	, strSymbol			= LTRIM(RTRIM(ptitm_unit)) COLLATE Latin1_General_CI_AS
+FROM ptitmmst
+WHERE LTRIM(RTRIM(ptitm_unit)) <> ''
+
+UNION ALL 
+SELECT DISTINCT
+	  strUnitMeasure	= LTRIM(RTRIM(ptitm_pak_desc)) COLLATE Latin1_General_CI_AS 
+	, strSymbol			= LTRIM(RTRIM(ptitm_pak_desc)) COLLATE Latin1_General_CI_AS
+FROM ptitmmst
+WHERE LTRIM(RTRIM(ptitm_pak_desc)) <> ''
+
+-- Delete the duplicate UOMs, relative to strUnitMeasure
+;WITH CTE AS 
+(
+    SELECT strUnitMeasure, strSymbol, ROW_NUMBER() OVER 
+    (
+        PARTITION BY strUnitMeasure ORDER BY strUnitMeasure
+    ) RowNumber
+    FROM  @UOMS
+)
+DELETE
+FROM CTE 
+WHERE RowNumber > 1;
 
 MERGE tblICUnitMeasure AS [Target]
 USING 
 (
-	SELECT
-		  strUnitMeasure	= RTRIM(ptuom_desc) COLLATE Latin1_General_CI_AS 
-		, strSymbol			= RTRIM(ptuom_code) COLLATE Latin1_General_CI_AS
-		, intConcurrencyId	= 1
-	FROM ptuommst
-	WHERE ptuom_code != ' '	
-) AS [Source] (strUnitMeasure, strSymbol, intConcurrencyId)
+	SELECT DISTINCT strUnitMeasure, strSymbol FROM @UOMS
+) AS [Source] (strUnitMeasure, strSymbol)
 ON [Target].strUnitMeasure = [Source].strUnitMeasure
 WHEN NOT MATCHED THEN
 INSERT (strUnitMeasure, strSymbol, intConcurrencyId)
-VALUES ([Source].strUnitMeasure, [Source].strSymbol, [Source].intConcurrencyId);
+VALUES ([Source].strUnitMeasure, [Source].strSymbol, 1);
 
 --update the unit type for the imported uoms
 UPDATE tblICUnitMeasure
@@ -44,8 +84,6 @@ CASE WHEN UPPER(strSymbol) IN ('BUSHEL','BUSHELS','BU', 'GAL', 'OZ', 'GA', 'QT')
 	WHEN UPPER(strSymbol) IN ('HOUR', 'HR') THEN 'Time'
 	ELSE 'Quantity'
 END 
-
-
 
 GO
 
