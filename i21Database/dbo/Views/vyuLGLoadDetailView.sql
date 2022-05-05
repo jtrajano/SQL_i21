aaed1583cf908eb289228145ea6dfc887dd843ea
Arman Arenas <arman.arenas@irely.com>
2022-05-05 16:30:18 +0800

    LG-3362, LG-3370 added vyuLGLoadDetailView TM order number and site no.

--- i21Database/dbo/Views/vyuLGLoadDetailView.sql
+++ i21Database/dbo/Views/vyuLGLoadDetailView.sql
@@ -234,7 +234,10 @@ SELECT LD.intLoadDetailId
 	, strDetailTerminalReference = LD.strTerminalReference
 	, LD.intCropYearId
 	, strCropYear = CPY.strCropYear
-
+	, LD.intTMDispatchId 
+	, strTMOrderNumber = TMD.strOrderNumber
+	, LD.intTMSiteId
+	, strSiteID = RIGHT('000'+ CAST(TMS.intSiteNumber AS NVARCHAR(4)),4)  COLLATE Latin1_General_CI_AS
 FROM tblLGLoadDetail LD
 JOIN tblLGLoad L ON L.intLoadId = LD.intLoadId
 LEFT JOIN tblICItem Item On Item.intItemId = LD.intItemId
@@ -276,6 +279,8 @@ LEFT JOIN tblCTIndex SIndex ON SIndex.intIndexId = SDetail.intIndexId
 LEFT JOIN tblSMCity SLPort ON SLPort.intCityId = SDetail.intLoadingPortId
 LEFT JOIN tblSMCity SDPort ON SDPort.intCityId = SDetail.intDestinationPortId
 LEFT JOIN tblTRSupplyPoint SSP ON SSP.intEntityVendorId = SIndex.intVendorId AND SSP.intEntityLocationId = SIndex.intVendorLocationId
+LEFT JOIN tblTMDispatch TMD ON TMD.intDispatchID = LD.intTMDispatchId
+LEFT JOIN tblTMSite TMS ON TMS.intSiteID = LD.intTMSiteId
 LEFT JOIN tblICItem	IMS ON IMS.intItemId = SDetail.intItemId
 LEFT JOIN vyuLGAdditionalColumnForContractDetailView ADS ON AD.intContractDetailId = SDetail.intContractDetailId
 LEFT JOIN tblSMCurrency	CUS ON CUS.intCurrencyID = SDetail.intCurrencyId			
