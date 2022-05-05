642585d19b91a59cb95c9e4b404789c3b206811a
Arman Arenas <arman.arenas@irely.com>
2022-05-05 22:21:42 +0800

    LG-3362 fixed joins on vyuLGCustomerConsumptionSite

--- i21Database/dbo/Views/vyuLGCustomerConsumptionSite.sql
+++ i21Database/dbo/Views/vyuLGCustomerConsumptionSite.sql
@@ -1,25 +1,22 @@
 ﻿CREATE VIEW [dbo].[vyuLGCustomerConsumptionSite]
 AS  
 	SELECT 
-		A.intSiteID
-		,strSiteID = RIGHT('000'+ CAST(A.intSiteNumber AS NVARCHAR(4)),4)  COLLATE Latin1_General_CI_AS
-		,A.strDescription
-		,A.intCustomerID
-		,C.strName
-		,C.strEntityNo
-		,D.intEntityLocationId
-		,D.strLocationName
-		,B.intCurrentSiteNumber
-		,ysnLocationActive = D.ysnActive
-		,ysnSiteActive = A.ysnActive 
-		,A.intConcurrencyId
-	FROM tblTMSite A
-	INNER JOIN tblTMCustomer B
-		ON A.intCustomerID = B.intCustomerID
-	INNER JOIN tblEMEntity C
-		ON B.intCustomerNumber = C.intEntityId
-	INNER JOIN tblEMEntityLocation D
-		ON C.intEntityId = D.intEntityId 
-	INNER JOIN tblEMEntityLocationConsumptionSite E
-		ON D.intEntityLocationId = E.intEntityLocationId
+		TMS.intSiteID
+		,strSiteID = RIGHT('000'+ CAST(TMS.intSiteNumber AS NVARCHAR(4)),4)  COLLATE Latin1_General_CI_AS
+		,TMS.strDescription
+		,intCustomerID = E.intEntityId
+		,E.strName
+		,E.strEntityNo
+		,ELCS.intEntityLocationId
+		,EL.strLocationName
+		,TMC.intCurrentSiteNumber
+		,ysnLocationActive = EL.ysnActive
+		,ysnSiteActive = TMS.ysnActive 
+		,TMS.intConcurrencyId
+	FROM 
+		tblEMEntityLocationConsumptionSite ELCS
+		INNER JOIN tblEMEntityLocation EL ON EL.intEntityLocationId = ELCS.intEntityLocationId
+		INNER JOIN tblEMEntity E ON E.intEntityId = EL.intEntityId 
+		LEFT JOIN tblTMCustomer TMC ON TMC.intCustomerNumber = E.intEntityId 
+		INNER JOIN tblTMSite TMS ON TMS.intSiteID = ELCS.intSiteID
 GO
