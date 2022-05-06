3bc3f7ebf2e504b54ce1cf79f552763bc30acd2f
Vinoth Kumar T <vinoth.t@irely.com>
2022-05-07 00:11:46 +0530

    MFG-4508

--- i21Database/dbo/Stored Procedures/uspMFGetProductionLine.sql
+++ i21Database/dbo/Stored Procedures/uspMFGetProductionLine.sql
@@ -9,10 +9,12 @@ BEGIN
 		,C.strDescription
 		,ISNULL(MC.ysnDefault, 0) AS ysnDefault
 		,C.dblStdCapacity AS dblBatchSize
+		,MCUOM.strUnitMeasure AS strBatchSizeUOM
 	FROM dbo.tblICItem I
 	JOIN dbo.tblICItemFactory IL ON IL.intItemId = I.intItemId
 	JOIN dbo.tblICItemFactoryManufacturingCell MC ON MC.intItemFactoryId = IL.intItemFactoryId
 	JOIN dbo.[tblMFManufacturingCell] C ON C.intManufacturingCellId = MC.intManufacturingCellId
+	LEFT JOIN tblICUnitMeasure MCUOM ON MCUOM.intUnitMeasureId = C.intStdUnitMeasureId
 	WHERE C.intLocationId = @intLocationId
 		AND I.intItemId = @intItemId
 		ORDER BY C.strCellName
