4780066572dda3cdd875b90dd9a0a2487528230d
Jopet Lambinicio <jopet.lambinicio@irely.com>
2022-05-06 19:05:44 +0800

    AP-10126

---
+++ i21Database/dbo/Tables/tblAPVendorAccountNumLocation.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[tblAPVendorAccountNumLocation]
+(
+	[intVendorAccountNumLocationId] INT NOT NULL IDENTITY,
+    [intEntityVendorId] INT NOT NULL, 
+    [intCompanyLocationId] INT NOT NULL, 
+	[strVendorAccountNum] NVARCHAR (50) COLLATE Latin1_General_CI_AS NOT NULL,
+    [intConcurrencyId] INT NOT NULL DEFAULT 1,
+	CONSTRAINT [PK_tblAPVendorAccountNumLocation] PRIMARY KEY ([intVendorAccountNumLocationId])
+)
