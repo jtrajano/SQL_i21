f4750f6a8d819852b0d02cae31997e614243a98d
Cris <cris.lazaga@irely.com>
2022-05-06 11:27:01 +0800

    TM-3355

---
+++ i21Database/dbo/Views/vyuTMSiteCustomerLocation.sql
@@ -0,0 +1,9 @@
+﻿CREATE VIEW [dbo].[vyuTMSiteCustomerLocation]
+AS 
+SELECT LCS.intEntityLocationConsumptionSiteId
+, LCS.intEntityLocationId
+, EL.strLocationName
+, LCS.intSiteID
+FROM tblEMEntityLocationConsumptionSite LCS
+INNER JOIN tblTMSite S ON S.intSiteID = LCS.intSiteID
+INNER JOIN tblEMEntityLocation EL ON EL.intEntityLocationId = LCS.intEntityLocationId
