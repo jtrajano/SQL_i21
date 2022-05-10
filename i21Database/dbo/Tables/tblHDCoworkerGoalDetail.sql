f45f9ae73f062c7a673ea68fdce69548a6a4e630
Edsel Roldan Magura <edsel.magura@iRely.com>
2022-05-10 09:10:56 +0800

    HD-3871

--- i21Database/dbo/Tables/tblHDCoworkerGoalDetail.sql
+++ i21Database/dbo/Tables/tblHDCoworkerGoalDetail.sql
@@ -1,14 +1,16 @@
 ﻿CREATE TABLE [dbo].[tblHDCoworkerGoalDetail]
 (
-	[intCoworkerGoalDetailId]	INT IDENTITY(1,1) NOT NULL,
-	[intCoworkerGoalId]		INT			   NOT NULL,
-	[intBillingPeriod]			INT			   NOT NULL,
-	[strBillingPeriodName]		NVARCHAR(250) COLLATE Latin1_General_CI_AS NOT NULL,
-	[dblBudget]					NUMERIC(18, 6) NULL DEFAULT 0,
-	[intUtilization]			INT			   NULL,
+	[intCoworkerGoalDetailId]	 INT IDENTITY(1,1) NOT NULL,
+	[intCoworkerGoalId]			 INT			   NOT NULL,
+	[intBillingPeriod]			 INT			   NOT NULL,
+	[strBillingPeriodName]		 NVARCHAR(250) COLLATE Latin1_General_CI_AS NOT NULL,
+	[dblBudget]					 NUMERIC(18, 6) NULL DEFAULT 0,
+	[intUtilization]			 INT			   NULL,
+	[intTimeEntryPeriodDetailId] INT			   NULL,
 	[intConcurrencyId] [int]	NOT NULL DEFAULT 1,
 	CONSTRAINT [PK_tblHDCoworkerGoalDetail_intCoworkerGoalDetailId] PRIMARY KEY CLUSTERED ([intCoworkerGoalDetailId] ASC),
 	CONSTRAINT [FK_tblHDCoworkerGoalDetail_tblHDCoworkerGoal_intCoworkerGoalId] FOREIGN KEY ([intCoworkerGoalId]) REFERENCES [dbo].[tblHDCoworkerGoal] ([intCoworkerGoalId]) ON DELETE CASCADE,
+	CONSTRAINT [FK_tblHDCoworkerGoalDetail_tblHDTimeEntryPeriodDetail_intTimeEntryPeriodDetailId] FOREIGN KEY ([intTimeEntryPeriodDetailId]) REFERENCES [dbo].[tblHDTimeEntryPeriodDetail] ([intTimeEntryPeriodDetailId]),
     CONSTRAINT [UQ_tblHDCoworkerGoalDetail_intCoworkerGoalId_intBillingPeriod] UNIQUE ([intCoworkerGoalId],[intBillingPeriod])
 )
 
