b82b8d72370187c8a871c18f61eb22a4de556d98
Dom Caoile <dom.caoile.02@gmail.com>
2022-05-12 07:52:20 +0800

    FRD-1786

--- i21Database/dbo/Tables/tblFRLedger.sql
+++ i21Database/dbo/Tables/tblFRLedger.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[tblFRLedger](
 	[intFRLedgerId]				INT				IDENTITY (1, 1) NOT NULL,
-	[intReportId]				INT				NULL,
-	[intLedgerId]				INT				NULL,
+	[intReportId]				INT				NOT NULL,
+	[intLedgerId]				INT				NOT NULL Default 0,
 	[strLedgerName]				NVARCHAR (255)	COLLATE Latin1_General_CI_AS NULL,
 	[intConcurrencyId]			INT,
 	CONSTRAINT [PK_tblFRLedger] PRIMARY KEY (intFRLedgerId,intLedgerId),
