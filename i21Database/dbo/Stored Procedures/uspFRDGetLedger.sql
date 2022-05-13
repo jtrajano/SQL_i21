f56e20e1ba32434c114f541c90bc7ae2eb535180
Dom Caoile <dom.caoile.02@gmail.com>
2022-05-13 12:15:50 +0800

    FRD-1786

---
+++ i21Database/dbo/Stored Procedures/uspFRDGetLedger.sql
@@ -0,0 +1,28 @@
+CREATE PROCEDURE  [dbo].[uspFRDGetLedger]          
+ @strFilterString AS NVARCHAR(MAX),  
+ @strLedger AS NVARCHAR(MAX)= '' OUTPUT      
+AS          
+        
+SET QUOTED_IDENTIFIER OFF                  
+SET ANSI_NULLS ON                  
+SET NOCOUNT ON                  
+SET XACT_ABORT ON                     
+        
+BEGIN        
+	DECLARE @queryString NVARCHAR(MAX)          
+       
+	CREATE TABLE #tempLedger(              
+		[intLedgerId]  INT,                        
+	);     
+
+	SET @strFilterString = REPLACE(REPLACE(REPLACE(REPLACE(@strFilterString,'[ID]','strAccountId'),'[Group]','strAccountGroup'),'[Type]','strAccountType'),'[Description]','strDescription')
+	SET @queryString = 'INSERT INTO #tempLedger SELECT TOP 1 intLedgerId FROM vyuGLSummary WHERE  '+@strFilterString+''         
+	EXEC(@queryString)    
+
+	WHILE EXISTS(SELECT 1 FROM #tempLedger)          
+	BEGIN    	   
+		SET @strLedger = (SELECT strLedgerName FROM tblGLLedger WHERE intLedgerId = (SELECT TOP 1 intLedgerId FROM #tempLedger))
+		DELETE #tempLedger WHERE intLedgerId = (SELECT TOP 1 intLedgerId FROM #tempLedger)
+	END 
+	DROP TABLE #tempLedger
+END
