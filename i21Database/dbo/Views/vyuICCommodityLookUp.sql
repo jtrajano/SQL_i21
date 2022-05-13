6fb5d42ec12f5a03f21db66981d0b10a811c43de
Jonathan Valenzuela <valenzuela.jonathan@outlook.com>
2022-05-13 16:22:06 +0800

    IC-10326

--- i21Database/dbo/Views/vyuICCommodityLookUp.sql
+++ i21Database/dbo/Views/vyuICCommodityLookUp.sql
@@ -10,7 +10,7 @@ SELECT
 	,strAdjustInventorySales = AdjustInventorySales.strTerms
 	,strAdjustInventoryTransfer = AdjustInventoryTransfer.strTerms
 	,strLineOfBusiness = LineOfBusiness.strLineOfBusiness
-	,CAST(CASE WHEN TransactionCount > 0 THEN 1 ELSE 0 END AS BIT) AS ysnCommodityTransaction
+	,ysnCommodityTransaction =  CAST(CASE WHEN InventoryValuation.intInventoryTransactionId IS NULL THEN 0 ELSE 1 END AS BIT)
 FROM tblICCommodity Commodity
 	LEFT JOIN tblRKFutureMarket FutureMarket ON FutureMarket.intFutureMarketId = Commodity.intFutureMarketId
 	LEFT JOIN tblGRStorageScheduleRule StorageSchedule ON StorageSchedule.intStorageScheduleRuleId = Commodity.intScheduleStoreId
@@ -19,6 +19,6 @@ FROM tblICCommodity Commodity
 	LEFT JOIN tblICAdjustInventoryTerms AdjustInventorySales ON AdjustInventorySales.intAdjustInventoryTermsId = Commodity.intAdjustInventorySales
 	LEFT JOIN tblICAdjustInventoryTerms AdjustInventoryTransfer ON AdjustInventoryTransfer.intAdjustInventoryTermsId = Commodity.intAdjustInventoryTransfer
 	LEFT JOIN tblSMLineOfBusiness LineOfBusiness ON LineOfBusiness.intLineOfBusinessId = Commodity.intLineOfBusinessId
-	LEFT JOIN (SELECT intCommodityId,COUNT(intCommodityId) AS TransactionCount
-			   FROM vyuICGetInventoryValuation
-			   GROUP BY intCommodityId) AS InventoryValuation ON Commodity.intCommodityId = InventoryValuation.intCommodityId
+	OUTER APPLY (SELECT TOP 1 *
+				 FROM vyuICGetInventoryValuation v
+				 WHERE v.intCommodityId = Commodity.intCommodityId) InventoryValuation
