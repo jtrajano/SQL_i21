2db6fa83ab94684f8f0285e9afa19550b12406ff
Dom Caoile <dom.caoile.02@gmail.com>
2022-05-14 15:53:40 +0800

    FRD-1792

--- i21Database/dbo/Tables/tblFRRowDesign.sql
+++ i21Database/dbo/Tables/tblFRRowDesign.sql
@@ -33,6 +33,7 @@
     [intCurrencyID]				INT             DEFAULT 0 NULL,
     [strCurrency]		        NVARCHAR (100)  COLLATE Latin1_General_CI_AS NULL,   
     [ysnUnnaturalAccount]	    BIT             DEFAULT 0 NULL,
+    [strLedger]		            NVARCHAR (MAX)  COLLATE Latin1_General_CI_AS NULL,   
     CONSTRAINT [PK_tblFRRowDesign] PRIMARY KEY CLUSTERED ([intRowDetailId] ASC),
     CONSTRAINT [FK_tblFRRowDesign_tblFRRow] FOREIGN KEY([intRowId]) REFERENCES [dbo].[tblFRRow] ([intRowId]) ON DELETE CASCADE
 );
