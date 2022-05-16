a23746cfa5e6b9a4d1a46bbe90c804f434353017
Wendell Wayne Estrada <wendell.estrada@irely.com>
2022-05-16 11:18:45 +0800

    ICM-2

---
+++ i21Database/dbo/Views/vyuApiTopCustomersBySalesPerUom.sql
@@ -0,0 +1,20 @@
+CREATE VIEW [dbo].[vyuApiTopCustomersBySalesPerUom]
+AS
+SELECT 
+	  r.intEntityCustomerId
+	, r.strCustomerName
+	, r.strCustomerNumber
+	, c.Email strCustomerEmail
+	, c.Phone strCustomerPhone
+	, r.intUnitMeasureId
+	, r.strUOM
+	, SUM(dbo.fnCMGetForexRateFromCurrency(r.intCurrencyId, dbo.fnSMGetDefaultCurrency('REPORTING'), ad.intCurrencyExchangeRateTypeId, r.dtmDate) * r.dblLineTotal) dblTotalSales
+	, SUM(ad.dblQtyOrdered) dblTotalOrdered
+	, MIN(r.dtmDate) dtmLastOrderDate
+	, MIN(ad.dblQtyOrdered) dblLastOrderQty
+FROM tblARSalesAnalysisStagingReport r
+JOIN tblARInvoiceDetail ad ON ad.intInvoiceDetailId = r.intInvoiceDetailId
+JOIN tblICItemUOM iu ON iu.intItemUOMId = ad.intItemUOMId
+JOIN vyuApiCustomer c ON c.EntityId = r.intEntityCustomerId
+WHERE r.strTransactionType != 'Order'
+GROUP BY r.intEntityCustomerId, r.strCustomerName, r.strCustomerNumber, r.intUnitMeasureId, r.strUOM, c.Email, c.Phone
