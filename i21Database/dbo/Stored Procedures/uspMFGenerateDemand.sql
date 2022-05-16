24cc0cf4d89625ed20fa77b82d7eb8234127f08a
Prem Selladurai <prem.selladurai@irely.com>
2022-05-16 18:59:02 +0530

    MFG-4540
    MFG-4540

--- i21Database/dbo/Stored Procedures/uspMFGenerateDemand.sql
+++ i21Database/dbo/Stored Procedures/uspMFGenerateDemand.sql
@@ -308,6 +308,7 @@ BEGIN TRY
 				SELECT I.intItemId
 				FROM tblICItem I
 				WHERE I.intCategoryId = @intCategoryId
+					AND I.strStatus='Active'
 					AND NOT EXISTS (
 						SELECT *
 						FROM tblMFItemExclude IE
