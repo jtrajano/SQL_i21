b2f7cc2443dcb7bbba933855bc7037f37aaa46c4
Roudinee Panteria <roudinee.panteria@irely.com>
2022-05-16 12:40:23 +0800

    AP-10514

--- i21Database/dbo/Views/vyuAPReserve.sql
+++ i21Database/dbo/Views/vyuAPReserve.sql
@@ -6,6 +6,10 @@ SELECT
 	,GLFY.[strFiscalYear]
 	,APR.[intGLFiscalYearPeriodId]
 	,GLFYP.[strPeriod]
+	,APR.[intReserveAccountId]
+	,strReserveAccountId			= GLAR.[strAccountId]
+	,APR.[intExpenseAccountId]
+	,strExpenseAccountId			= GLAE.[strAccountId]
 	,[dtmPostDate]
 	,[dblReserveBucket30Percentage]
 	,[dblReserveBucket60Percentage]
@@ -15,3 +19,5 @@ SELECT
 FROM tblAPReserve APR
 LEFT JOIN tblGLFiscalYearPeriod GLFYP ON APR.intGLFiscalYearPeriodId = GLFYP.intGLFiscalYearPeriodId
 LEFT JOIN tblGLFiscalYear GLFY ON APR.intFiscalYearId = GLFY.intFiscalYearId
+LEFT JOIN tblGLAccount GLAR ON APR.intReserveAccountId = GLAR.intAccountId
+LEFT JOIN tblGLAccount GLAE ON APR.intExpenseAccountId = GLAE.intAccountId
