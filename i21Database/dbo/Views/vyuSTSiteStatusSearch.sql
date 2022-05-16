e4f477c0dfaefd932e7a46bc739e5905953b5142
Jase Rueda <jase.rueda@iRely.com>
2022-05-16 13:54:27 +0800

    CS-30

---
+++ i21Database/dbo/Views/vyuSTSiteStatusSearch.sql
@@ -0,0 +1,11 @@
+﻿CREATE VIEW [dbo].[vyuSTSiteStatusSearch]
+AS
+SELECT		a.intStoreId,
+			a.intStoreNo,
+			a.strDescription,
+			ISNULL(b.ysnInternetConnectivity, 0) as ysnInternetConnectivity,
+			ISNULL(b.ysnRegisterConnectivity, 0) as ysnRegisterConnectivity
+FROM		tblSTStore a
+LEFT JOIN	tblSTSiteStatus b
+ON			a.intStoreId = b.intStoreId AND
+			DATEDIFF(MINUTE, b.dtmStatusDate, GETDATE()) <= 6
