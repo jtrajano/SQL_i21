70c2f83aebc6b5a8dff0af02096baaacf011aab7
Jase Rueda <jase.rueda@iRely.com>
2022-05-16 13:54:27 +0800

    CS-30

---
+++ i21Database/dbo/Tables/tblSTSiteStatus.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tblSTSiteStatus]
+(
+    [intSiteStatusId] INT NOT NULL IDENTITY,
+    [intStoreId] INT NOT NULL,
+    [dtmStatusDate] DATETIME NOT NULL,
+    [ysnInternetConnectivity] BIT NOT NULL,
+    [ysnRegisterConnectivity] BIT NOT NULL,
+    [intConcurrencyId] INT NULL,
+    CONSTRAINT [PK_tblSTSiteStatus] PRIMARY KEY CLUSTERED ([intSiteStatusId] ASC),
+    CONSTRAINT [FK_tblSTSiteStatus_tblSTStore] FOREIGN KEY ([intStoreId]) REFERENCES [tblSTStore]([intStoreId]),    
+)
