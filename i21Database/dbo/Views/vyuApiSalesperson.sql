be99d29e1f6608f21a6e047da802d553c0ce34ce
Wendell Wayne Estrada <wendell.estrada@irely.com>
2022-05-17 16:07:16 +0800

    ICM-2

--- i21Database/dbo/Views/vyuApiSalesperson.sql
+++ i21Database/dbo/Views/vyuApiSalesperson.sql
@@ -3,19 +3,20 @@ AS
 SELECT
       e.ysnActive
     , e.intEntityId
-    , e.strEntityNumber
-    , e.strEntityName
-    , e.strEntityType
-    , e.strEntityAddress
-    , e.strEntityCity
-    , e.strEntityState
-    , e.strEntityZipCode
-    , e.strEntityCountry
-    , e.strEntityPhone
+    , e.strSalespersonId as strEntityNumber
+    , e.strSalespersonName as strEntityName
+    --, CASE WHEN e.strType = 'Sales Representative' THEN 'Salesperson' ELSE e.strType END as strEntityType
+	, 'Salesperson' as strEntityType
+    , e.strAddress as strEntityAddress
+    , e.strCity as strEntityCity
+    , e.strState as strEntityState
+    , e.strZipCode as strEntityZipCode
+    , e.strCountry as strEntityCountry
+    , e.strPhone as strEntityPhone
+    , e.strEmail
     , created.dtmDate dtmDateCreated
 	, COALESCE(updated.dtmDate, created.dtmDate) dtmDateLastUpdated
-    , e.strEntityEmail AS strEmail
-FROM vyuCTEntity e
+FROM vyuEMSalesperson e
 OUTER APPLY (
 	SELECT TOP 1 au.dtmDate
 	FROM vyuApiRecordAudit au
@@ -30,3 +31,4 @@ OUTER APPLY (
 		AND au.strAction = 'Updated'
 		AND au.strNamespace = 'EntityManagement.view.Entity'
 ) updated
+WHERE  e.ysnActive = 1
