814aba82111c653f4a301555e21e57935783dcda
Jett Michael Marcella <jett.marcella@irely.com>
2022-05-19 17:13:11 +0800

    FRM-10277
    (cherry picked from commit 6af3e2bc27e2482a5e8a4661310dd95219f0a4d3)

--- Integration/dbo/Stored Procedures/uspARSyncTerms.sql
+++ Integration/dbo/Stored Procedures/uspARSyncTerms.sql
@@ -5,7 +5,7 @@ IF EXISTS(select top 1 1 from sys.procedures where name = 'uspARSyncTerms')
 GO
 
 
-IF (SELECT TOP 1 ysnUsed FROM ##tblOriginMod WHERE strPrefix = 'AG' and strDBName = db_name()) = 1
+IF (SELECT TOP 1 ysnUsed FROM #tblOriginMod WHERE strPrefix = 'AG') = 1
 BEGIN
 
 	EXEC('CREATE PROCEDURE uspARSyncTerms  
@@ -312,7 +312,7 @@ BEGIN
 		  END')
 END
 
-IF (SELECT TOP 1 ysnUsed FROM ##tblOriginMod WHERE strPrefix = 'PT' and strDBName = db_name()) = 1
+IF (SELECT TOP 1 ysnUsed FROM #tblOriginMod WHERE strPrefix = 'PT') = 1
 BEGIN
 	EXEC('CREATE PROCEDURE uspARSyncTerms  
 		   @ToOrigin   bit    = 0  
--- Integration/dbo/Views/vyuCMBankAccount.sql
+++ Integration/dbo/Views/vyuCMBankAccount.sql
@@ -1,5 +1,5 @@
 ﻿GO
-IF  (SELECT TOP 1 ysnUsed FROM ##tblOriginMod WHERE strPrefix = 'AP') = 1
+IF  (SELECT TOP 1 ysnUsed FROM #tblOriginMod WHERE strPrefix = 'AP') = 1
 BEGIN
 	IF EXISTS (SELECT 1 FROM sys.triggers WHERE Name = 'trg_insert_vyuCMBankAccount') DROP TRIGGER dbo.trg_insert_vyuCMBankAccount;
 	IF EXISTS (SELECT 1 FROM sys.triggers WHERE Name = 'trg_delete_vyuCMBankAccount') DROP TRIGGER dbo.trg_delete_vyuCMBankAccount;
