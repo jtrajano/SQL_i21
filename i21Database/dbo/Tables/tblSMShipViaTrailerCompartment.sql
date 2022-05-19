c92b3bc9b053980bacc6b2ac8f85b5165740d25d
IRELY\JMendones <jesmar.mendones@irely.com>
2022-05-19 14:43:46 +0800

    EM-2920

---
+++ i21Database/dbo/Tables/tblSMShipViaTrailerCompartment.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[tblSMShipViaTrailerCompartment]
+(
+	[intEntityShipViaTrailerCompartmentId]		INT				IDENTITY(1,1) NOT NULL,
+	[intEntityShipViaTrailerId]					INT				NOT NULL,
+	[strTrailerNumber]							NVARCHAR(100)	COLLATE Latin1_General_CI_AS NULL,
+	[strCompartmentNumber]						NVARCHAR(100)	COLLATE Latin1_General_CI_AS NULL,
+	[strCategory]								NVARCHAR(500)	COLLATE Latin1_General_CI_AS NULL,
+	[strCapacity]								NVARCHAR(500)	COLLATE Latin1_General_CI_AS NULL,
+	
+
+	[intConcurrencyId]							INT				NOT NULL DEFAULT (1), 
+    CONSTRAINT [PK_tblSMShipViaTrailerCompartment] PRIMARY KEY CLUSTERED ([intEntityShipViaTrailerCompartmentId] ASC), 
+	CONSTRAINT [FK_dbo_tblSMShipViaTrailerCompartment_tblSMShipViaTrailer] FOREIGN KEY ([intEntityShipViaTrailerId]) REFERENCES [dbo].tblSMShipViaTrailer ([intEntityShipViaTrailerId]) ON DELETE CASCADE
+)
