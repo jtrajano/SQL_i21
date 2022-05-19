b73f43b0790c98c41babdcb1cffa5c7bf2ef858d
lawrence_zabala <lawrence.zabala@irely.com>
2022-05-19 14:28:43 +0800

    CT-7251 CT-7136
    (cherry picked from commit e25a60fc51f7f3abfda4fc04edca834559bc6a35)

--- i21Database/dbo/Stored Procedures/uspCTLoadContractCost.sql
+++ i21Database/dbo/Stored Procedures/uspCTLoadContractCost.sql
@@ -1,77 +1,76 @@
 ﻿CREATE PROCEDURE [dbo].[uspCTLoadContractCost]
-	@intContractDetailId	INT
+	@intContractDetailId INT
+
 AS
 
-	SELECT		--CC.*,
-				CC.intContractCostId
-				,CC.intConcurrencyId
-				,CC.intPrevConcurrencyId
-				,CC.intContractDetailId
-				,CC.intItemId
-				,CC.intVendorId
-				,CC.strCostMethod
-				,CC.intCurrencyId
-				,CC.dblRate
-				,CC.intItemUOMId
-				,CC.intRateTypeId
-				,CC.dblFX
-				,CC.ysnAccrue
-				,CC.ysnMTM
-				,CC.ysnPrice
-				,CC.ysnAdditionalCost
-				,CC.ysnBasis
-				,CC.ysnReceivable
-				,CC.strParty
-				,CC.strPaidBy
-				,CC.dtmDueDate
-				,CC.strReference
-				,CC.ysn15DaysFromShipment
-				,CC.strRemarks
-				,CC.strStatus
-				,CC.strCostStatus
-				,CC.dblReqstdAmount
-				,CC.dblRcvdPaidAmount
-				,dblActualAmount = isnull(CC.dblActualAmount,0.00)
-				,CC.dblAccruedAmount
-				,CC.dblRemainingPercent
-				,CC.dtmAccrualDate
-				,CC.strAPAR
-				,CC.strPayToReceiveFrom
-				,CC.strReferenceNo
-				,CC.intContractCostRefId
-    			,ysnFromBasisComponent = CC.ysnBasis,
-
-				IM.strItemNo, 
-				IM.strDescription strItemDescription,
-				UM.strUnitMeasure strUOM, 
-				EY.strName strVendorName, 
-				CD.intContractHeaderId, 
-				IU.intUnitMeasureId, 
-				CD.intContractSeq, 
-				CY.strCurrency,
-				CH.strContractNumber + ' - ' + LTRIM(CD.intContractSeq) strContractSeq,
-				CAST(ISNULL((SELECT TOP 1 intBillDetailId FROM tblAPBillDetail WHERE intContractCostId = CC.intContractCostId),0) AS BIT) ysnBilled,
-				CH.intTermId,					
-				IM.strCostType,
-				IM.ysnInventoryCost,
-				CH.strContractNumber,
-				CH.dtmContractDate,
-				MY.strCurrency	AS	strMainCurrency,
-				(CASE	WHEN	CC.strCostMethod = 'Per Unit'	THEN 
-							dbo.fnCTConvertQuantityToTargetItemUOM(CD.intItemId,QU.intUnitMeasureId,CM.intUnitMeasureId,CD.dblQuantity)*CC.dblRate
-						WHEN	CC.strCostMethod = 'Amount' THEN
-							CC.dblRate * isnull(CC.dblFX,1)
-						WHEN	CC.strCostMethod = 'Per Container'	THEN
-							(CC.dblRate * (case when isnull(CD.intNumberOfContainers,1) = 0 then 1 else isnull(CD.intNumberOfContainers,1) end))
-						WHEN	CC.strCostMethod = 'Percentage' THEN 
-							dbo.fnCTConvertQuantityToTargetItemUOM(CD.intItemId,QU.intUnitMeasureId,PU.intUnitMeasureId,CD.dblQuantity)*CD.dblCashPrice*CC.dblRate/100
-				END)  / (case when isnull(CY.ysnSubCurrency,convert(bit,0)) = convert(bit,1) then isnull(CY.intCent,1) else 1 end)  dblAmount,
-				RT.strCurrencyExchangeRateType,
-				CH.intBookId AS intHeaderBookId,
-				CH.intSubBookId AS intHeaderSubBookId,
-				CD.intBookId AS intDetailBookId,
-				CD.intSubBookId AS intDetailSubBookId	
-
+	SELECT CC.intContractCostId
+		, CC.intConcurrencyId
+		, CC.intPrevConcurrencyId
+		, CC.intContractDetailId
+		, CC.intItemId
+		, CC.intVendorId
+		, CC.strCostMethod
+		, CC.intCurrencyId
+		, CC.dblRate
+		, CC.intItemUOMId
+		, CC.intRateTypeId
+		, CC.dblFX
+		, CC.ysnAccrue
+		, CC.ysnMTM
+		, CC.ysnPrice
+		, CC.ysnAdditionalCost
+		, CC.ysnBasis
+		, CC.ysnReceivable
+		, CC.strParty
+		, CC.strPaidBy
+		, CC.dtmDueDate
+		, CC.strReference
+		, CC.ysn15DaysFromShipment
+		, CC.strRemarks
+		, CC.strStatus
+		, CC.strCostStatus
+		, CC.dblReqstdAmount
+		, CC.dblRcvdPaidAmount
+		, dblActualAmount = ISNULL(CC.dblActualAmount, 0.00)
+		, CC.dblAccruedAmount
+		, CC.dblRemainingPercent
+		, CC.dtmAccrualDate
+		, CC.strAPAR
+		, CC.strPayToReceiveFrom
+		, CC.strReferenceNo
+		, CC.intContractCostRefId
+		, ysnFromBasisComponent = CC.ysnBasis
+		, IM.strItemNo
+		, strItemDescription = IM.strDescription
+		, strUOM = UM.strUnitMeasure
+		, strVendorName = EY.strName
+		, CD.intContractHeaderId
+		, IU.intUnitMeasureId
+		, CD.intContractSeq
+		, CY.strCurrency
+		, strContractSeq = CH.strContractNumber + ' - ' + LTRIM(CD.intContractSeq)
+		, ysnBilled = CAST(ISNULL((SELECT TOP 1 intBillDetailId FROM tblAPBillDetail WHERE intContractCostId = CC.intContractCostId),0) AS BIT)
+		, CH.intTermId
+		, IM.strCostType
+		, IM.ysnInventoryCost
+		, CH.strContractNumber
+		, CH.dtmContractDate
+		, strMainCurrency = MY.strCurrency
+		, dblAmount = (CASE	WHEN CC.strCostMethod = 'Per Unit'
+								THEN dbo.fnCTConvertQuantityToTargetItemUOM(CD.intItemId, QU.intUnitMeasureId, CM.intUnitMeasureId, CD.dblQuantity) * CC.dblRate * ISNULL(CC.dblFX, 1)
+							WHEN CC.strCostMethod = 'Amount'
+								THEN CC.dblRate * ISNULL(CC.dblFX, 1)
+							WHEN CC.strCostMethod = 'Per Container'
+								THEN (CC.dblRate * (CASE WHEN ISNULL(CD.intNumberOfContainers, 1) = 0 THEN 1 ELSE ISNULL(CD.intNumberOfContainers, 1) END)) * ISNULL(CC.dblFX, 1)
+							WHEN CC.strCostMethod = 'Percentage'
+								THEN dbo.fnCTConvertQuantityToTargetItemUOM(CD.intItemId, QU.intUnitMeasureId, PU.intUnitMeasureId, CD.dblQuantity) * CD.dblCashPrice * CC.dblRate/100 * ISNULL(CC.dblFX, 1)
+							END)
+					/ (CASE WHEN ISNULL(CY.ysnSubCurrency, CONVERT(BIT, 0)) = CONVERT(BIT, 1) THEN ISNULL(CY.intCent, 1) ELSE 1 END)
+		, RT.strCurrencyExchangeRateType
+		, intHeaderBookId = CH.intBookId
+		, intHeaderSubBookId = CH.intSubBookId
+		, intDetailBookId = CD.intBookId
+		, intDetailSubBookId = CD.intSubBookId
 	FROM		tblCTContractCost	CC
 	JOIN		tblCTContractDetail	CD	ON	CD.intContractDetailId	=	CC.intContractDetailId
 										AND CC.intContractDetailId  =	@intContractDetailId	
