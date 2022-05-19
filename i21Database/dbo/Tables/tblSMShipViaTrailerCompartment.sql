cfa955eb36f90387b4ba25b5d6016255031c979f
IRELY\JMendones <jesmar.mendones@irely.com>
2022-05-19 14:43:46 +0800

    EM-2920

--- i21Database/dbo/Tables/tblSMShipViaTrailerCompartment.sql
+++ i21Database/dbo/Tables/tblSMShipViaTrailerCompartment.sql
@@ -2,13 +2,13 @@
 (
 	[intEntityShipViaTrailerCompartmentId]		INT				IDENTITY(1,1) NOT NULL,
 	[intEntityShipViaTrailerId]					INT				NOT NULL,
+	[strTrailerNumber]							NVARCHAR(100)	COLLATE Latin1_General_CI_AS NULL,
 	[strCompartmentNumber]						NVARCHAR(100)	COLLATE Latin1_General_CI_AS NULL,
-	[intCategoryId]								INT				NULL,
-	[dblCapacity]								NUMERIC (18, 6) NULL,
+	[strCategory]								NVARCHAR(500)	COLLATE Latin1_General_CI_AS NULL,
+	[strCapacity]								NVARCHAR(500)	COLLATE Latin1_General_CI_AS NULL,
+	
 
 	[intConcurrencyId]							INT				NOT NULL DEFAULT (1), 
     CONSTRAINT [PK_tblSMShipViaTrailerCompartment] PRIMARY KEY CLUSTERED ([intEntityShipViaTrailerCompartmentId] ASC), 
-	CONSTRAINT [FK_dbo_tblSMShipViaTrailerCompartment_tblSMShipViaTrailer] FOREIGN KEY ([intEntityShipViaTrailerId]) REFERENCES [dbo].tblSMShipViaTrailer ([intEntityShipViaTrailerId]) ON DELETE CASCADE,
-	CONSTRAINT [FK_dbo_tblSMShipViaTrailerCompartment_tblICCategory_intCategoryId] FOREIGN KEY ([intCategoryId]) REFERENCES [dbo].[tblICCategory] ([intCategoryId]),
-	CONSTRAINT [UK_tblSMShipViaTrailerCompartment_intEntityShipViaTrailer_strCompartmentNumber] UNIQUE NONCLUSTERED ([strCompartmentNumber] ASC,[intEntityShipViaTrailerId] ASC),
+	CONSTRAINT [FK_dbo_tblSMShipViaTrailerCompartment_tblSMShipViaTrailer] FOREIGN KEY ([intEntityShipViaTrailerId]) REFERENCES [dbo].tblSMShipViaTrailer ([intEntityShipViaTrailerId]) ON DELETE CASCADE
 )
