df38e7debeb0cdde3345c14b25e53f008fdde6bb
Roudinee Panteria <roudinee.panteria@irely.com>
2022-05-21 11:16:27 +0800

    AR-14731

--- i21Database/dbo/Stored Procedures/uspARPostInvoiceIntegrations.sql
+++ i21Database/dbo/Stored Procedures/uspARPostInvoiceIntegrations.sql
@@ -500,7 +500,7 @@ SET dblHighestAR		= CUSTOMER.dblARBalance
 FROM dbo.tblARCustomer CUSTOMER
 INNER JOIN (
 	SELECT DISTINCT intEntityCustomerId
-	FROM ##ARPostInvoiceHeader
+	FROM tblARPostInvoiceHeader
 ) INVOICE ON CUSTOMER.intEntityId = INVOICE.intEntityCustomerId
 WHERE CUSTOMER.dblARBalance > ISNULL(CUSTOMER.dblHighestAR, 0)
 
@@ -522,7 +522,7 @@ INNER JOIN (
     FROM tblARInvoice I
     INNER JOIN (
         SELECT DISTINCT intEntityCustomerId
-        FROM ##ARPostInvoiceHeader
+        FROM tblARPostInvoiceHeader
     ) INVOICE ON I.intEntityCustomerId = INVOICE.intEntityCustomerId
     WHERE I.ysnPosted = 1
       AND I.ysnForgiven = 0
