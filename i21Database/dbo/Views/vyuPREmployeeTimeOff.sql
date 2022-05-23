5e7adebbe9e2599ccf9880cb911027d072a71d02
Rey Anthony Crisostomo <anthony.crisostomo@iRely.com>
2022-05-23 18:23:56 +0800

    PR-2685 - Time off keeps changing on its own to random hours

--- i21Database/dbo/Views/vyuPREmployeeTimeOff.sql
+++ i21Database/dbo/Views/vyuPREmployeeTimeOff.sql
@@ -1,156 +1,183 @@
 ﻿CREATE VIEW [dbo].[vyuPREmployeeTimeOff]  
 AS   
   
-SELECT ETO.intEntityEmployeeId
-	,ET.intEntityId
-	,ETO.intEmployeeTimeOffId
-	,ETO.intTypeTimeOffId
-	,ET.strEntityNo
-	,strEntityName = ET.strName
-	,strTimeOffId = TTO.strTimeOff
-	,strTimeOffDescription = TTO.strDescription
-	,dblHoursLeft = ISNULL(dblHoursEarned,0) - ISNULL(dblHoursUsed,0)
-	,ETO.dtmEligible
-	,ETO.dblRate
-	,ETO.strAwardPeriod
-	,ETO.dblRateFactor
-	,ETO.dblMaxCarryover
-	,ETO.dblMaxBalance
-	,ETO.dblMaxEarned
-	,ETO.dtmLastAward
-	,ETO.dblHoursEarned
-	,ETO.dblHoursAccrued
-	,ETO.intSort
-	,ETO.dblPerPeriod
-	,ETO.strPeriod
-	,EMP.intRank
-	,dblHoursUsed = ISNULL(TOYTD.dblHoursUsedYTD,0)
-	,dblBalance = (ETO.dblHoursCarryover + ETO.dblHoursEarned) - ETO.dblHoursUsed - ISNULL(TOYTD.dblHoursUsedYTD,0)
-	,ETO.dblHoursCarryover 
-	,dblAdjustments =  ETO.dblHoursUsed 
-FROM tblPREmployeeTimeOff ETO
-LEFT JOIN(
-	SELECT intEntityId
-		,strEntityNo
-		,strName
-	FROM tblEMEntity
-) ET ON ETO.intEntityEmployeeId = ET.intEntityId
-LEFT JOIN(
-	SELECT intTypeTimeOffId
-		,strTimeOff
-		,strDescription
-	FROM tblPRTypeTimeOff
-) TTO ON ETO.intTypeTimeOffId = TTO.intTypeTimeOffId
-LEFT JOIN(
-	SELECT intEntityId 
-		,intRank
-	FROM tblPREmployee
-) EMP ON ETO.intEntityEmployeeId = EMP.intEntityId
+SELECT ETO.intEntityEmployeeId      
+ ,ET.intEntityId      
+ ,ETO.intEmployeeTimeOffId      
+ ,ETO.intTypeTimeOffId      
+ ,ET.strEntityNo      
+ ,strEntityName = ET.strName      
+ ,strTimeOffId = TTO.strTimeOff      
+ ,strTimeOffDescription = TTO.strDescription      
+ ,dblHoursLeft = ISNULL(dblHoursEarned,0) - ISNULL(dblHoursUsed,0)      
+ ,ETO.dtmEligible      
+ ,ETO.dblRate      
+ ,ETO.strAwardPeriod      
+ ,ETO.dblRateFactor      
+ ,ETO.dblMaxCarryover      
+ ,ETO.dblMaxBalance      
+ ,ETO.dblMaxEarned      
+ ,ETO.dtmLastAward      
+ ,ETO.dblHoursEarned      
+ ,ETO.dblHoursAccrued      
+ ,ETO.intSort      
+ ,ETO.dblPerPeriod      
+ ,ETO.strPeriod      
+ ,EMP.intRank      
+ ,dblHoursUsed = ISNULL(TOYTD.dblHoursUsedYTD,0)      
+ ,dblBalance = (ETO.dblHoursCarryover + ETO.dblHoursEarned) - ETO.dblHoursUsed - ISNULL(TOYTD.dblHoursUsedYTD,0)      
+ ,ETO.dblHoursCarryover       
+ ,dblAdjustments =  ETO.dblHoursUsed     
+ ,TOYTD.intYear  
+FROM tblPREmployeeTimeOff ETO      
+LEFT JOIN(      
+ SELECT intEntityId      
+  ,strEntityNo      
+  ,strName      
+ FROM tblEMEntity      
+) ET ON ETO.intEntityEmployeeId = ET.intEntityId      
+LEFT JOIN(      
+ SELECT intTypeTimeOffId      
+  ,strTimeOff      
+  ,strDescription      
+ FROM tblPRTypeTimeOff      
+) TTO ON ETO.intTypeTimeOffId = TTO.intTypeTimeOffId      
+LEFT JOIN(      
+ SELECT intEntityId       
+  ,intRank      
+ FROM tblPREmployee      
+) EMP ON ETO.intEntityEmployeeId = EMP.intEntityId      
+      
+      
+INNER JOIN(      
+       
+  SELECT E.intEntityId    
+  ,intYear  
+     ,dblHoursUsedYTD = SUM(CASE WHEN U.intYear IS NOT NULL THEN ISNULL(U.dblHours, 0) ELSE 0 END)      
+     ,T.intTypeTimeOffId      
+   FROM tblPREmployee E INNER JOIN tblPREmployeeTimeOff T  ON E.intEntityId = T.intEntityEmployeeId        
+         
+   LEFT JOIN     
+   (  
+  SELECT * FROM   
+  (  
+   SELECT       
+    intYear = YEAR(dtmPayDate)  
+    ,PC.intEntityEmployeeId      
+    ,intTypeTimeOffId = PCE.intEmployeeTimeOffId      
+    ,ET.strAwardPeriod  
+    ,ET.dtmLastAward  
+    ,ISNULL(ET.dtmLastAward, EMP.dtmDateHired) AS dtmLastAwardPC  
+    ,dblHours =  
+		CASE   
+			WHEN YEAR(dtmPayDate) IS NOT NULL AND (ET.strAwardPeriod = 'Anniversary Date') THEN   
+				CASE WHEN (  
+				(dtmDateFrom < DATEADD(YY, YEAR(GETDATE()) - YEAR(EMP.dtmDateHired), EMP.dtmDateHired)  OR YEAR(dtmDateFrom) =  YEAR(GETDATE())))   
+					THEN dblHours  
+			ELSE   
+				CASE WHEN DATEADD(YY, YEAR(GETDATE()) - YEAR(dtmDateHired), dtmDateHired) > ET.dtmLastAward THEN 0 ELSE dblHours END   
+			END  
+			WHEN YEAR(dtmPayDate) IS NOT NULL AND (ET.strAwardPeriod = 'End of Year') THEN   
+				CASE WHEN (dtmDateFrom < DATEADD(yy, DATEDIFF(yy, 0, GETDATE()) + 1, -1))   
+					THEN dblHours  
+			ELSE 
+				CASE WHEN ET.dtmLastAward = DATEADD(yy, DATEDIFF(yy, 0, GETDATE()) + 1, -1) THEN 0 ELSE dblHours END 
+			END  
+			WHEN YEAR(dtmPayDate) IS NOT NULL  AND (ET.strAwardPeriod = 'Start of Year') THEN  
+				CASE WHEN YEAR(dtmPayDate) = YEAR(GETDATE())  
+					THEN dblHours  
+			ELSE 
+				CASE WHEN YEAR(ET.dtmLastAward) = YEAR(GETDATE()) THEN 0 ELSE dblHours END 
+			END  
+		ELSE   
+			CASE WHEN YEAR(dtmPayDate) IS NOT NULL AND (YEAR(dtmPayDate) = YEAR(GETDATE())) THEN  
+				dblHours  
+			ELSE 
+				CASE WHEN YEAR(ET.dtmLastAward) = YEAR(GETDATE()) THEN dblHours ELSE 0 END
+			END  
+		END  
+    ,PCE.intTimeOffRequestId    
+    ,PCE.intPaycheckId  
+   FROM       
+    tblPRPaycheckEarning PCE       
+   INNER JOIN tblPRPaycheck PC      
+    ON PCE.intPaycheckId = PC.intPaycheckId  
+   LEFT JOIN tblPREmployee EMP  
+    ON PC.intEntityEmployeeId = EMP.intEntityId  
+   LEFT JOIN tblPREmployeeTimeOff ET  
+    ON ET.intEntityEmployeeId = PC.intEntityEmployeeId  
+    AND ET.intTypeTimeOffId = PCE.intEmployeeTimeOffId  
+   WHERE       
+    PCE.intEmployeeTimeOffId IS NOT NULL      
+    AND ysnPosted = 1      
+    AND ysnVoid = 0      
+    AND dblHours <> 0   
+    AND PC.dtmDateFrom >= ISNULL(ET.dtmLastAward,EMP.dtmDateHired)  
 
-
-INNER JOIN(
-	
-		SELECT E.intEntityId  
-			  ,dblHoursUsedYTD = SUM(
-										CASE WHEN PCTimeOff.intYear IS NOT NULL AND (T.strAwardPeriod = 'Anniversary Date') THEN 
-													CASE WHEN (
-														PCTimeOff.dtmDateFrom >= ISNULL(T.dtmLastAward, E.dtmDateHired)  
-														AND 
-														(PCTimeOff.dtmDateFrom < DATEADD(YY, YEAR(GETDATE()) - YEAR(dtmDateHired), dtmDateHired)  OR YEAR(PCTimeOff.dtmDateFrom) =  YEAR(GETDATE()) )
-														) THEN dblHours
-													ELSE 
-														0
-													END
-											When TOR.intYear IS NOT NULL AND  (T.strAwardPeriod = 'Anniversary Date') THEN 
-													CASE WHEN (
-														TOR.dtmDateFrom >= ISNULL(T.dtmLastAward, E.dtmDateHired)  
-														AND 
-														(TOR.dtmDateFrom < DATEADD(YY, YEAR(GETDATE()) - YEAR(dtmDateHired), dtmDateHired)  OR YEAR(TOR.dtmDateFrom) =  YEAR(GETDATE()) )
-														) THEN TOR.dblHoursTOR
-													ELSE 
-														0
-													END
-											WHEN PCTimeOff.intYear IS NOT NULL AND (T.strAwardPeriod = 'End of Year') THEN 
-													CASE WHEN (PCTimeOff.dtmDateFrom < DATEADD(yy, DATEDIFF(yy, 0, GETDATE()) + 1, -1) 
-															AND PCTimeOff.dtmDateFrom >= ISNULL(T.dtmLastAward, E.dtmDateHired)  
-															) THEN dblHours
-													ELSE 0
-
-													END 
-                                            WHEN TOR.intYear IS NOT NULL AND (T.strAwardPeriod = 'End of Year') THEN   
-                                                CASE WHEN (  TOR.dtmDateFrom >= ISNULL(T.dtmLastAward, E.dtmDateHired)    
-														AND (TOR.dtmDateFrom < DATEADD(yy, DATEDIFF(yy, 0, GETDATE()) + 1, -1))  
-														) THEN TOR.dblHoursTOR  
-												ELSE   
-													0	
-												END  
-											WHEN TOR.intYear IS NOT NULL  AND (T.strAwardPeriod = 'Start of Year') THEN
-												CASE WHEN TOR.intYear = YEAR(GETDATE())
-												THEN TOR.dblHoursTOR
-												ELSE 0
-												END
-											ELSE 
-												CASE WHEN PCTimeOff.intYear IS NOT NULL AND (PCTimeOff.intYear = YEAR(GETDATE())) THEN
-														dblHours
-													WHEN TOR.intYear IS NOT NULL AND (T.strAwardPeriod = 'Anniversary Date') THEN 
-															CASE WHEN (
-																		 TOR.dtmDateFrom >= ISNULL(T.dtmLastAward, E.dtmDateHired)  
-																		 AND 
-																		 (TOR.dtmDateFrom < DATEADD(YY, YEAR(GETDATE()) - YEAR(dtmDateHired), dtmDateHired)  OR YEAR(TOR.dtmDateFrom) =  YEAR(GETDATE()) )
-																		) THEN TOR.dblHoursTOR
-															ELSE 
-															  0
-															END
-													WHEN TOR.intYear IS NOT NULL AND (TOR.intYear =  YEAR(dtmLastAward))
-															THEN TOR.dblHoursTOR
-													ELSE
-														0
-													END
-											END
-										)
- 
-			,T.intTypeTimeOffId
-			FROM tblPREmployee E INNER JOIN tblPREmployeeTimeOff T  ON E.intEntityId = T.intEntityEmployeeId  
-			
-			LEFT JOIN 
-				(SELECT 
-					intYear = YEAR(dtmPayDate)
-					,dtmDateFrom
-					,intEntityEmployeeId
-					,intTypeTimeoffId = intEmployeeTimeOffId
-					,dblHours
-					,intPaycheckEarningId
-				FROM 
-					tblPRPaycheckEarning PCE 
-					INNER JOIN tblPRPaycheck PC
-						ON PCE.intPaycheckId = PC.intPaycheckId
-				WHERE 
-					intEmployeeTimeOffId IS NOT NULL
-					AND ysnPosted = 1
-					AND ysnVoid = 0
-					AND dblHours <> 0
-				)PCTimeOff
-				ON PCTimeOff.intEntityEmployeeId = E.intEntityId 
-				AND PCTimeOff.intTypeTimeoffId = T.intTypeTimeOffId
-   
-   			LEFT JOIN (	SELECT
-						intYear  = YEAR(dtmDateFrom)
-						,dtmDateFrom
-						,dtmDateTo
-						,intEntityEmployeeId
-						,intTypeTimeOffId
-						,dblHoursTOR = dblRequest
-						FROM tblPRTimeOffRequest TOR
-						WHERE ysnPostedToCalendar = 1
-				)TOR
-				ON E.intEntityId = TOR.intEntityEmployeeId
-				AND T.intTypeTimeOffId = TOR.intTypeTimeOffId
-				AND PCTimeOff.intYear = YEAR(TOR.dtmDateTo) 
-   			GROUP BY 
-			E.intEntityId
-			,T.intTypeTimeOffId   
-) TOYTD 
-	ON ETO.intEntityEmployeeId = TOYTD.intEntityId 
-	AND ETO.intTypeTimeOffId = TOYTD.intTypeTimeOffId
+  UNION  
+  
+   SELECT      
+    intYear  = YEAR(dtmDateFrom)     
+    ,TOR.intEntityEmployeeId      
+    ,TOR.intTypeTimeOffId    
+    ,ET.strAwardPeriod  
+    ,ET.dtmLastAward  
+    ,ISNULL(ET.dtmLastAward, EMP.dtmDateHired) AS dtmLastAwardTOR  
+    ,dblHoursTOR =   
+		CASE   
+			WHEN YEAR(dtmDateFrom) IS NOT NULL AND  (ET.strAwardPeriod = 'Anniversary Date') THEN   
+				CASE WHEN (  
+				(TOR.dtmDateFrom < DATEADD(YY, YEAR(GETDATE()) - YEAR(dtmDateHired), dtmDateHired)  OR YEAR(TOR.dtmDateFrom) =  YEAR(GETDATE()) )  
+				) THEN dblRequest  
+			ELSE   
+				CASE WHEN DATEADD(YY, YEAR(GETDATE()) - YEAR(dtmDateHired), dtmDateHired) > ET.dtmLastAward THEN 0 ELSE dblRequest END 
+			END  
+  
+			WHEN YEAR(dtmDateFrom) IS NOT NULL AND (ET.strAwardPeriod = 'End of Year') THEN     
+				CASE WHEN ((TOR.dtmDateFrom < DATEADD(yy, DATEDIFF(yy, 0, GETDATE()) + 1, -1)))   
+					THEN dblRequest   
+			ELSE    
+				CASE WHEN ET.dtmLastAward = DATEADD(yy, DATEDIFF(yy, 0, GETDATE()) + 1, -1) THEN 0 ELSE dblRequest END
+			END   
+			WHEN YEAR(dtmDateFrom) IS NOT NULL  AND (ET.strAwardPeriod = 'Start of Year') THEN  
+				CASE WHEN YEAR(dtmDateFrom) = YEAR(GETDATE())  
+					THEN dblRequest  
+			ELSE   
+				CASE WHEN YEAR(ET.dtmLastAward) = YEAR(GETDATE()) THEN 0 ELSE dblRequest END
+			END  
+		ELSE   
+			CASE WHEN YEAR(dtmDateFrom) IS NOT NULL AND (YEAR(dtmDateFrom) =  YEAR(dtmLastAward))  
+				THEN dblRequest  
+			ELSE  
+				CASE WHEN YEAR(ET.dtmLastAward) = YEAR(GETDATE()) THEN dblRequest ELSE 0 END  
+			END  
+		END  
+    ,TOR.intTimeOffRequestId  
+    ,TOR.intPaycheckId    
+   FROM   
+    tblPRTimeOffRequest TOR    
+   LEFT JOIN tblPREmployee EMP  
+    on EMP.intEntityId = TOR.intEntityEmployeeId  
+   LEFT JOIN tblPREmployeeTimeOff ET  
+    on ET.intEntityEmployeeId = TOR.intEntityEmployeeId  
+    and ET.intTypeTimeOffId = TOR.intTypeTimeOffId  
+   LEFT JOIN tblPRPaycheckEarning PCE  
+    on PCE.intTimeOffRequestId = TOR.intTypeTimeOffId  
+   WHERE   
+     ysnPostedToCalendar = 1    
+    AND TOR.dtmDateFrom >= ISNULL(ET.dtmLastAward,EMP.dtmDateHired)  
+  ) U  
+   ) U  
+     
+ ON U.intEntityEmployeeId = E.intEntityId  
+ and U.intTypeTimeOffId = T.intTypeTimeOffId  
+  
+ GROUP BY       
+  E.intEntityId      
+    ,T.intTypeTimeOffId       
+    ,U.intYear  
+) TOYTD       
+ ON ETO.intEntityEmployeeId = TOYTD.intEntityId       
+ AND ETO.intTypeTimeOffId = TOYTD.intTypeTimeOffId 
   
 GO
