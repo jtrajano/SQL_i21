1ece5e377a230b4a57f4a62fce59a9ae4944e4fc
Arman Arenas <arman.arenas@irely.com>
2022-05-25 13:25:37 +0800

    LG-3303 added vyuLGGetDispatchOrderCompartmentData

---
+++ i21Database/dbo/Views/vyuLGGetDispatchOrderCompartmentData.sql
@@ -0,0 +1,21 @@
+﻿CREATE VIEW [dbo].[vyuLGGetDispatchOrderCompartmentData]
+AS
+SELECT 
+	DO.intDispatchOrderId
+	,DO.intEntityShipViaTrailerId
+	,SVTC.intEntityShipViaTrailerCompartmentId
+	,SVTC.strCompartmentNumber
+	,SVTC.intCategoryId
+	,CAT.strCategoryCode
+	,SVTC.dblCapacity 
+	,dblLoadWeight = DOD.dblLoadWeight
+	,dblPercentageFull = (DOD.dblLoadWeight / ISNULL(SVTC.dblCapacity, 1)) * 100
+FROM tblLGDispatchOrder DO
+LEFT JOIN tblSMShipViaTrailerCompartment SVTC ON SVTC.intEntityShipViaTrailerId = DO.intEntityShipViaTrailerId
+LEFT JOIN tblICCategory CAT ON CAT.intCategoryId = SVTC.intCategoryId
+OUTER APPLY (SELECT dblLoadWeight = SUM(ISNULL(dblStandardWeight, 0)) 
+			FROM tblLGDispatchOrderDetail 
+			WHERE intDispatchOrderId = DO.intDispatchOrderId
+				AND intEntityShipViaCompartmentId = SVTC.intEntityShipViaTrailerCompartmentId) DOD
+GO
+
