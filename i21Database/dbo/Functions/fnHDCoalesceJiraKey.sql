3b474ebbb4d54ef2e1bfec48277ad0dff71e19e1
Edsel Roldan Magura <edsel.magura@iRely.com>
2022-05-25 10:14:58 +0800

    HD-3912

--- i21Database/dbo/Functions/fnHDCoalesceJiraKey.sql
+++ i21Database/dbo/Functions/fnHDCoalesceJiraKey.sql
@@ -8,12 +8,13 @@ BEGIN
 		@strJiraKeys = (case when @ysnDisplay = convert(bit,1) then COALESCE(@strJiraKeys + ',<br>', '') + '<a target="_blank" title="Click to go to ' + Ticket.strKey + '." href="http://jira.irelyserver.com/browse/' + Ticket.strKey + '">' + Ticket.strKey + '</a>' else COALESCE(@strJiraKeys + ',', '') + Ticket.strKey end)
 	FROM
 		( 
-			SELECT strKey
+			SELECT strKey = CASE WHEN ISNULL(strJiraKey, '') <> '' AND strKey != strJiraKey  
+									THEN strJiraKey
+								ELSE strKey
+							END
 			FROM tblHDTicketJIRAIssue	
 			WHERE LTRIM(RTRIM(tblHDTicketJIRAIssue.strKey)) <> ''
 				  AND tblHDTicketJIRAIssue.intTicketId = @intTicketId
-				  AND (strKey != strJiraKey OR ISNULL(strJiraKey, '') = '')
-			GROUP BY strKey
 		) Ticket
 
 	RETURN @strJiraKeys
