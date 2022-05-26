17a3bd0553c46286f32de10273c39a30b31661db
IRELY\JMendones <jesmar.mendones@irely.com>
2022-05-26 15:06:36 +0800

    EM-2920
    (cherry picked from commit f9ccfdb17748a84a5edcf24ee1d977d858e7b66c)

--- i21Database/dbo/Tables/tblSMShipViaTrailer.sql
+++ i21Database/dbo/Tables/tblSMShipViaTrailer.sql
@@ -17,5 +17,5 @@
     CONSTRAINT [PK_tblSMShipViaTrailer] PRIMARY KEY CLUSTERED ([intEntityShipViaTrailerId] ASC), 
 	CONSTRAINT [FK_dbo_tblSMShipViaTrailer_tblSMShipVia_] FOREIGN KEY ([intEntityShipViaId]) REFERENCES [dbo].tblSMShipVia ([intEntityId]) ON DELETE CASCADE,
 	CONSTRAINT [UK_tblSMShipViaTrailer_intEntityShipViaId_strTrailerLicenseNumber] UNIQUE NONCLUSTERED ([strTrailerLicenseNumber] ASC,[intEntityShipViaId] ASC),
-	CONSTRAINT [FK_tblSMShipViaTrailer_tblSMShipViaTruck_] FOREIGN KEY ([intTruckAssigned]) REFERENCES [dbo].tblSMShipViaTruck ([intEntityShipViaTruckId])
+	CONSTRAINT [FK_tblSMShipViaTrailer_tblSMShipViaTruck_] FOREIGN KEY ([intTruckAssignedId]) REFERENCES [dbo].tblSMShipViaTruck ([intEntityShipViaTruckId])
  )
