8979f0da48ecc8f1ecd0f47fd668bd478b3c42da
Feb Montefrio <feb.montefrio@irely.com>
2022-05-27 16:22:19 +0800

    [F] IC-10322

--- i21Database/dbo/Tables/tblICRecostFormulationDetail.sql
+++ i21Database/dbo/Tables/tblICRecostFormulationDetail.sql
@@ -3,6 +3,7 @@
 	[intRecostFormulationDetailId] INT NOT NULL PRIMARY KEY
 	,[intRecostFormulationId] INT NOT NULL 
 	,[intItemId] INT NOT NULL 
+	,[intLocationId] INT NOT NULL
 	,[dblOldStandardCost] NUMERIC(38, 20) NULL 
 	,[dblNewStandardCost] NUMERIC(38, 20) NULL
 	,[dblDifference] NUMERIC(38, 20) NULL
