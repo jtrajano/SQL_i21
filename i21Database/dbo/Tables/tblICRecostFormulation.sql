d26a12dfc3952afe610528918bd2b9271b51ede4
Feb Montefrio <feb.montefrio@irely.com>
2022-05-28 12:33:59 +0800

    [F] IC-10322

--- i21Database/dbo/Tables/tblICRecostFormulation.sql
+++ i21Database/dbo/Tables/tblICRecostFormulation.sql
@@ -15,4 +15,8 @@
     ,[dtmDateModified] DATETIME NULL
     ,[intCreatedByUserId] INT NULL
     ,[intModifiedByUserId] INT NULL 
+	,CONSTRAINT [FK_tblICRecostFormulationDetail_tblSMCompanyLocationFrom] FOREIGN KEY ([intLocationFromId]) REFERENCES [tblSMCompanyLocation]([intCompanyLocationId])
+	,CONSTRAINT [FK_tblICRecostFormulationDetail_tblSMCompanyLocationTo] FOREIGN KEY ([intLocationToId]) REFERENCES [tblSMCompanyLocation]([intCompanyLocationId])
+	,CONSTRAINT [FK_tblICRecostFormulationDetail_tblICCategoryFrom] FOREIGN KEY ([intCategoryFromId]) REFERENCES [tblICCategory]([intCategoryId])
+	,CONSTRAINT [FK_tblICRecostFormulationDetail_tblICCategoryTo] FOREIGN KEY ([intCategoryToId]) REFERENCES [tblICCategory]([intCategoryId])
 )
--- i21Database/dbo/Tables/tblICRecostFormulationDetail.sql
+++ i21Database/dbo/Tables/tblICRecostFormulationDetail.sql
@@ -4,6 +4,7 @@
 	,[intRecostFormulationId] INT NOT NULL 
 	,[intItemId] INT NOT NULL 
 	,[intLocationId] INT NOT NULL
+	,[intRecipeId] INT NOT NULL
 	,[dblOldStandardCost] NUMERIC(38, 20) NULL 
 	,[dblNewStandardCost] NUMERIC(38, 20) NULL
 	,[dblDifference] NUMERIC(38, 20) NULL
@@ -14,4 +15,7 @@
     ,[dtmDateModified] DATETIME NULL
     ,[intCreatedByUserId] INT NULL
     ,[intModifiedByUserId] INT NULL 
+	,CONSTRAINT [FK_tblICRecostFormulationDetail_tblICItem] FOREIGN KEY ([intItemId]) REFERENCES [tblICItem]([intItemId]) 
+	,CONSTRAINT [FK_tblICRecostFormulationDetail_tblSMCompanyLocation] FOREIGN KEY ([intLocationId]) REFERENCES [tblSMCompanyLocation]([intCompanyLocationId])
+	,CONSTRAINT [FK_tblICRecostFormulationDetail_tblMFRecipe] FOREIGN KEY ([intRecipeId]) REFERENCES [tblMFRecipe]([intRecipeId])
 )
