830c6651c1c7196b478c3bbed9ce396080a65cd5
Roudinee Panteria <roudinee.panteria@irely.com>
2022-05-30 16:49:09 +0800

    AR-14735

---
+++ i21Database/dbo/Views/vyuARTaxLocation.sql
@@ -0,0 +1,24 @@
+﻿CREATE VIEW [dbo].[vyuARTaxLocation]
+AS 
+SELECT 
+	 intTaxLocationId	= intCompanyLocationId
+	,strLocationName
+	,strAddress
+	,strFobPoint		= 'Origin'
+	,intEntityId		= 0
+	,strType			= 'Company'
+FROM vyuSMGetCompanyLocationSearchList
+WHERE ysnLocationActive = 1
+
+UNION ALL
+
+SELECT
+	 intTaxLocationId	= intEntityLocationId
+	,strLocationName
+	,strAddress
+	,strFobPoint		= 'Destination'
+	,intEntityId
+	,strType			= 'Entity'
+FROM vyuEMEntityLocationSearch EMELS
+LEFT JOIN tblSMFreightTerms SMFT ON EMELS.intFreightTermId = SMFT.intFreightTermId
+WHERE EMELS.ysnActive = 1
