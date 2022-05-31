60132685ace0d73a8c9387b9f102763dc2d6f8d3
DESKTOP-0Q1GJ6F\iRelyNUC14 <kelvin.quinto@irely.com>
2022-05-31 11:11:16 +0800

    AR-14646

--- i21Database/Scripts/Post-Deployment/PostDeployment.sql
+++ i21Database/Scripts/Post-Deployment/PostDeployment.sql
@@ -302,6 +302,7 @@ print 'BEGIN POST DEPLOYMENT'
 :r .\AR\DefaultData\36_UpdateTaxDetailInvalidSetup.sql
 :r .\AR\DefaultData\37_UpdatePOSNewFields.sql
 :r .\AR\DefaultData\39_UpdateCreditCardCompanyPreference.sql
+:r .\AR\DefaultData\40_UpdateSchedulePaymentMethod.sql
 :r .\AR\DefaultData\99_ReCreateTriggers.sql
 :r .\AR\DefaultData\98_UpdateInvoiceGrossMarginSummary.sql
 :r .\AR\DefaultData\96_RebuildReportFields.sql
