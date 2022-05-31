1e85bba926b76c125e5824efd53e81a6e8e93077
Nelson Reyes <nelson.reyes@irely.com>
2022-05-31 09:47:40 +0800

    ST-2200

---
+++ i21Database/dbo/Tables/tblSTTranslogMixMatch.sql
@@ -0,0 +1,19 @@
+CREATE TABLE [tblSTTranslogMixMatch]
+(
+    [intTranslogMixMatchId] INT IDENTITY (1, 1) NOT NULL,
+	-- trHeader
+    [intTermMsgSN] bigint NULL,
+    [intScanTransactionId] INT NULL,
+	-- trlMatchLine
+	[strTrlMatchLineTrlMatchName] nvarchar(50) COLLATE Latin1_General_CI_AS NULL,
+    [dblTrlMatchLineTrlMatchQuantity] decimal(18, 3) NULL,
+    [dblTrlMatchLineTrlMatchPrice] decimal(18, 3) NULL,
+    [intTrlMatchLineTrlMatchMixes] int NULL,
+    [dblTrlMatchLineTrlPromoAmount] decimal(18, 3) NULL,
+    [strTrlMatchLineTrlPromotionID] nvarchar(50) COLLATE Latin1_General_CI_AS NULL,
+    [strTrlMatchLineTrlPromotionIDPromoType] nvarchar(50) COLLATE Latin1_General_CI_AS NULL,
+	[intTrlMatchLineTrlMatchNumber] int NULL,
+	[dtmDateAdded] DATETIME NULL,
+	[intConcurrencyId] int NULL
+)
+
