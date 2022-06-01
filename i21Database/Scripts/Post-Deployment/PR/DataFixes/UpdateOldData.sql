90a778bcee9ca5d41659dc0537394ccba3cbb56b
manny_armas <manny.armas@iRely.com>
2022-06-01 17:42:35 +0800

    PR-2635 - Timecard hours does not display in Process Pay Groups screen

--- i21Database/Scripts/Post-Deployment/PR/DataFixes/UpdateOldData.sql
+++ i21Database/Scripts/Post-Deployment/PR/DataFixes/UpdateOldData.sql
@@ -440,3 +440,12 @@ BEGIN
 	AND strSocialSecurity IS NOT NULL
 	AND LTRIM(RTRIM(strSocialSecurity)) <> ''
 END
+
+/*
+* PR Companyconfiguration
+* SET Default value for newly added OT configurations
+*/
+IF EXISTS(SELECT TOP 1 1 FROM sys.columns WHERE NAME  = N'strOvertimeCalculation' AND OBJECT_ID = OBJECT_ID(N'tblPRCompanyPreference'))
+BEGIN
+	EXEC ('IF EXISTS(SELECT TOP 1 1 FROM tblPRCompanyPreference WHERE strOvertimeCalculation IS NULL) UPDATE tblPRCompanyPreference SET strOvertimeCalculation = ''Weekly'',dblRegularHoursThreshold = 40 ')
+END
