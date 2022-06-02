3bed136bcd35bc0e2788ac27ea752bf4d82f2f18
Wendell Wayne Estrada <wendell.estrada@irely.com>
2022-06-02 14:59:09 +0800

    ICm-2

--- i21Database/dbo/Views/vyuApiGetTaxDetail.sql
+++ i21Database/dbo/Views/vyuApiGetTaxDetail.sql
@@ -16,9 +16,13 @@ SELECT
     , ysnSpecialTax = CAST(1 AS BIT)
     , st.ysnTaxExempt
     , st. ysnInvalidSetup
-    , dblTax = st.dblAdjustedTax
+    , dblTax = st.dblTax
     , st.dblRate
     , sod.dblStandardWeight
+    , st.strCalculationMethod
+    , st.ysnTaxAdjusted
+    , st.dblAdjustedTax
+    , st.intSalesOrderDetailId
 FROM tblSOSalesOrderDetail sod
 JOIN tblSOSalesOrderDetailTax st ON st.intSalesOrderDetailId = sod.intSalesOrderDetailId
 JOIN tblSMTaxClass tc ON tc.intTaxClassId = st.intTaxClassId
