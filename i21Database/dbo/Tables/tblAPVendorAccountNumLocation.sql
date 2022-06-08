b1a3506c70405830fce803b2cd070f8aeb9bdb30
Jopet Lambinicio <jopet.lambinicio@irely.com>
2022-06-08 12:10:50 +0800

    AP-10544

--- i21Database/dbo/Tables/tblAPVendorAccountNumLocation.sql
+++ i21Database/dbo/Tables/tblAPVendorAccountNumLocation.sql
@@ -3,7 +3,8 @@
 	[intVendorAccountNumLocationId] INT NOT NULL IDENTITY,
     [intEntityVendorId] INT NOT NULL, 
     [intCompanyLocationId] INT NOT NULL, 
-	[strVendorAccountNum] NVARCHAR (50) COLLATE Latin1_General_CI_AS NOT NULL,
+	[strVendorAccountNum] NVARCHAR (50) COLLATE Latin1_General_CI_AS NULL,
+    [intPayFromBankAccountId] INT NULL, 
     [intConcurrencyId] INT NOT NULL DEFAULT 1,
 	CONSTRAINT [PK_tblAPVendorAccountNumLocation] PRIMARY KEY ([intVendorAccountNumLocationId])
 )
