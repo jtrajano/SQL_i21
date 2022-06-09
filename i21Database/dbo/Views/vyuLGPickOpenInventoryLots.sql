b99a30a7d59557949e7bb81b90fc95fda37db376
Allen Marasigan <54519457+jaumarasigan@users.noreply.github.com>
2022-06-09 11:53:50 +0800

    LG-3283 Added ysnPosted filter

--- i21Database/dbo/Views/vyuLGPickOpenInventoryLots.sql
+++ i21Database/dbo/Views/vyuLGPickOpenInventoryLots.sql
@@ -136,7 +136,7 @@ FROM (
 	FROM tblICLot Lot
 		LEFT JOIN tblICInventoryReceiptItemLot ReceiptLot ON ReceiptLot.intLotId = ISNULL(Lot.intSplitFromLotId, Lot.intLotId)
 		LEFT JOIN tblICInventoryReceiptItem ReceiptItem ON ReceiptItem.intInventoryReceiptItemId = ReceiptLot.intInventoryReceiptItemId
-		LEFT JOIN tblICInventoryReceipt Receipt ON Receipt.intInventoryReceiptId = ReceiptItem.intInventoryReceiptId
+		LEFT JOIN tblICInventoryReceipt Receipt ON Receipt.intInventoryReceiptId = ReceiptItem.intInventoryReceiptId AND Receipt.ysnPosted = 1
 		LEFT JOIN tblCTContractDetail CTDetail ON CTDetail.intContractDetailId = ReceiptItem.intLineNo 
 		LEFT JOIN tblCTContractHeader CTHeader ON CTHeader.intContractHeaderId = ReceiptItem.intOrderId
 		LEFT JOIN vyuLGAdditionalColumnForContractDetailView AD ON AD.intContractDetailId = CTDetail.intContractDetailId
