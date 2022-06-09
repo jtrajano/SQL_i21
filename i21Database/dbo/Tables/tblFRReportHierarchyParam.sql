32053608b3ef4bfca974d4c4f69cad4c0a207ab4
Dom Caoile <dom.caoile.02@gmail.com>
2022-06-09 22:11:31 +0800

    FRD-1791

---
+++ i21Database/dbo/Tables/tblFRReportHierarchyParam.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[tblFRReportHierarchyParam](
+	[intParamId]					INT             IDENTITY (1, 1) NOT NULL,
+	[strParm]						NVARCHAR (MAX)	COLLATE Latin1_General_CI_AS NULL,
+	CONSTRAINT [PK_tblFRReportHierarchyParam] PRIMARY KEY CLUSTERED ([intParamId] ASC)
+);
