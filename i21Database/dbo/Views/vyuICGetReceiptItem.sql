3c79b5282f8ecc7b6032e7745a4bc30c6a1bafa5
Jonathan Valenzuela <valenzuela.jonathan@outlook.com>
2022-06-09 15:53:04 +0800

    Revert "IC-10385"
    This reverts commit a8ee3ba8d8cd02b26775eacf25bddd73ee7919be.

--- i21Database/dbo/Views/vyuICGetReceiptItem.sql
+++ i21Database/dbo/Views/vyuICGetReceiptItem.sql
@@ -14,7 +14,7 @@ SELECT
 	Item.strLotTracking,
 	Item.ysnLotWeightsRequired,
 	ItemPricing.strPricingMethod,
-	dblLastCost = COALESCE(dbo.fnICGetPromotionalCostByEffectiveDate(Item.intItemId, ItemLocation.intItemLocationId, COALESCE(ReceiveUOM.intItemUOMId, ItemUOM.intItemUOMId, GrossUOM.intItemUOMId), GETDATE()), EffectiveCost.dblCost, ItemPricing.dblLastCost, 0),
+	dblLastCost = COALESCE(dbo.fnICGetPromotionalCostByEffectiveDate(Item.intItemId, ItemLocation.intItemLocationId, COALESCE(ReceiveUOM.intItemUOMId, ItemUOM.intItemUOMId, GrossUOM.intItemUOMId), TranSession.dtmTransactionDate), EffectiveCost.dblCost, ItemPricing.dblLastCost, 0),
 	dblStandardCost = COALESCE(ItemPricing.dblStandardCost, 0),
 	dblSalePrice = COALESCE(EffectivePrice.dblRetailPrice, ItemPricing.dblSalePrice, 0),
 	dblReceiveUOMConvFactor = COALESCE(ReceiveUOM.dblUnitQty, ItemUOM.dblUnitQty, 0),
@@ -48,6 +48,8 @@ SELECT
 			END,
 	ysnHasAddOn = CAST(ISNULL(ItemAddOn.ysnHasAddOn, 0) AS BIT),
 	ysnHasAddOnOtherCharge = CAST(ISNULL(AddOnOtherCharge.ysnHasAddOnOtherCharge, 0) AS BIT),
+	TranSession.guiSessionId,
+	dtmSessionDate = TranSession.dtmTransactionDate,
 	Item.intComputeItemTotalOption
 FROM tblICItem Item
 LEFT JOIN (
@@ -74,8 +76,9 @@ LEFT JOIN (
 		ON ItemUOM.intUnitMeasureId = iUOM.intUnitMeasureId
 )
 	ON ItemUOM.intItemId = Item.intItemId
-OUTER APPLY dbo.fnICGetItemCostByEffectiveDate(GETDATE(), Item.intItemId, ItemLocation.intItemLocationId, DEFAULT) EffectiveCost
-OUTER APPLY dbo.fnICGetItemPriceByEffectiveDate(GETDATE(), Item.intItemId, ItemLocation.intItemLocationId, COALESCE(ReceiveUOM.intItemUOMId, ItemUOM.intItemUOMId, GrossUOM.intItemUOMId), DEFAULT) EffectivePrice
+OUTER APPLY tblICTransactionSession TranSession
+OUTER APPLY dbo.fnICGetItemCostByEffectiveDate(TranSession.dtmTransactionDate, Item.intItemId, ItemLocation.intItemLocationId, DEFAULT) EffectiveCost
+OUTER APPLY dbo.fnICGetItemPriceByEffectiveDate(TranSession.dtmTransactionDate, Item.intItemId, ItemLocation.intItemLocationId, COALESCE(ReceiveUOM.intItemUOMId, ItemUOM.intItemUOMId, GrossUOM.intItemUOMId), DEFAULT) EffectivePrice
 LEFT JOIN tblICItemPricing ItemPricing 
 	ON ItemLocation.intItemId = ItemPricing.intItemId 
 	AND ItemLocation.intItemLocationId = ItemPricing.intItemLocationId
