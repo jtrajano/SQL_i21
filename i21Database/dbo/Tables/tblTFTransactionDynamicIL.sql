373791c7bc47d2f6cb549a5bde63faeacf670f53
Cris <cris.lazaga@irely.com>
2022-06-15 15:35:47 +0800

    MFT-1664

---
+++ i21Database/dbo/Tables/tblTFTransactionDynamicIL.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tblTFTransactionDynamicIL]
+(
+	[intTransactionDynamicId] INT IDENTITY NOT NULL,
+	[intTransactionId] INT NOT NULL,
+	[strCustomerBillToAddress] NVARCHAR(200) COLLATE Latin1_General_CI_AS NULL,
+	[strCustomerBillToCity] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL,
+	[strCustomerBillToState] NVARCHAR(20) COLLATE Latin1_General_CI_AS NULL,
+	[strCustomerBillToZipCode] NVARCHAR(30) COLLATE Latin1_General_CI_AS NULL,
+	CONSTRAINT [FK_tblTFTransactionDynamicIL_tblTFTransaction] FOREIGN KEY ([intTransactionId]) REFERENCES [tblTFTransaction]([intTransactionId]) ON DELETE CASCADE, 
+    CONSTRAINT [PK_tblTFTransactionDynamicIL] PRIMARY KEY ([intTransactionDynamicId])
+)
