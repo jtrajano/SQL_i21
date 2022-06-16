23e61124141c4f3461e64748c33c94952176c124
lawrence_zabala <lawrence.zabala@irely.com>
2022-06-16 21:33:27 +0800

    GL-9112 (with conflict)

--- i21Database/dbo/Tables/tblGLDetail.sql
+++ i21Database/dbo/Tables/tblGLDetail.sql
@@ -3,7 +3,6 @@
     [intSubsidiaryCompanyId] [int] NULL,
     [intMultiCompanyId] [int]   NULL,
     [dtmDate]                   DATETIME         NOT NULL,
-    --[dtmDateNoTime]             AS CAST(dtmDate AS DATE) PERSISTED,
     [strBatchId]                NVARCHAR (40)    COLLATE Latin1_General_CI_AS NULL,
     [intAccountId]              INT              NOT NULL,
     [dblDebit] [numeric](18, 6) NULL CONSTRAINT [DF_tblGLDetail_dblDebit]  DEFAULT ((0)),
