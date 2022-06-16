96e2b490d77f7e5f689bcf857642939e5759f6a1
lawrence_zabala <lawrence.zabala@irely.com>
2022-06-16 21:30:28 +0800

    GL-9112

--- i21Database/dbo/Views/vyuGLDetail.sql
+++ i21Database/dbo/Views/vyuGLDetail.sql
@@ -3,7 +3,7 @@ AS
         SELECT 
 		A.intGLDetailId,
 		A.intAccountId,
-		dtmDate = A.dtmDateNoTime,
+		A.dtmDate,
         A.strBatchId COLLATE Latin1_General_CI_AS strBatchId,
         B.strAccountId COLLATE Latin1_General_CI_AS strAccountId,
         RTRIM(LTRIM(ISNULL(B.strDescription,''))) COLLATE Latin1_General_CI_AS strAccountDescription,
