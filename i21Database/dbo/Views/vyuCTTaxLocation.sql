d4eef134eb2cbd410378ca33aebf7c50e36d7a49
Bryan-PC <013.bryan@gmail.com>
2022-06-19 04:24:41 +0800

    CT-7285 v4

--- i21Database/dbo/Views/vyuCTTaxLocation.sql
+++ i21Database/dbo/Views/vyuCTTaxLocation.sql
@@ -1,37 +1,37 @@
 ﻿CREATE VIEW [dbo].[vyuCTTaxLocation]
 AS
-		SELECT intCompanyLocationId intTaxLocationId
-		  , 1 intContractTypeId
-		  , 'Origin' strTaxPoint
-		  , strLocationName strTaxLocation
-		  , 0 intEntityId 
+	SELECT  intTaxLocationId = intEntityLocationId
+			, 1 intContractTypeId
+			, 'Origin' strTaxPoint
+			, strLocationName strTaxLocation
+			, intEntityId 
+	FROM tblEMEntityLocation
+
+	UNION ALL
+
+	SELECT  intTaxLocationId = intCompanyLocationId
+			, 1 intContractTypeId
+			, 'Destination' strTaxPoint
+			, strLocationName strTaxLocation
+			, 0 intEntityId 
 	FROM tblSMCompanyLocation
 
 	UNION ALL
 
-	SELECT intCompanyLocationId intTaxLocationId
-		 , 2 intContractTypeId
-		 , 'Destination' strTaxPoint
-		 , strLocationName strTaxLocation
-		 , 0 intEntityId 
+	SELECT  intTaxLocationId = intCompanyLocationId
+			,2 intContractTypeId
+			, 'Origin' strTaxPoint
+			, strLocationName strTaxLocation
+			, 0 intEntityId 
 	FROM tblSMCompanyLocation
 
 	UNION ALL
 
-	SELECT  intTaxLocationId	= intEntityLocationId
-		, 1 intContractTypeId
-		, 'Destination' strTaxPoint
-		, strLocationName strTaxLocation
-		, intEntityId 
-		FROM tblEMEntityLocation
-
-	UNION ALL
-
-	SELECT  intTaxLocationId	= intEntityLocationId
-		, 2 intContractTypeId
-		, 'Origin' strTaxPoint
-		, strLocationName strTaxLocation
-		, intEntityId 
+	SELECT  intTaxLocationId = intEntityLocationId
+			, 2 intContractTypeId
+			, 'Destination' strTaxPoint
+			, strLocationName strTaxLocation
+			, intEntityId 
 	FROM tblEMEntityLocation
 GO
 
