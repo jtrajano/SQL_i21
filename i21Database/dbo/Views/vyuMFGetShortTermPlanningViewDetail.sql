7b1a152582a2238997201a406146001d4154d6d9
Prem Selladurai <prem.selladurai@irely.com>
2022-06-20 19:35:00 +0530

    MFG-4530
    MFG-4530

--- i21Database/dbo/Views/vyuMFGetShortTermPlanningViewDetail.sql
+++ i21Database/dbo/Views/vyuMFGetShortTermPlanningViewDetail.sql
@@ -120,12 +120,12 @@ FROM tblMFShortTermPlanningViewDetail D
 LEFT JOIN tblLGLoadContainer LC ON LC.intLoadContainerId = D.intLoadContainerId
 LEFT JOIN tblLGLoad L ON L.intLoadId = LC.intLoadId
 JOIN tblSMCompanyLocation CL ON CL.intCompanyLocationId = D.intLocationId
-JOIN tblCTContractDetail CD ON CD.intContractDetailId = D.intContractDetailId
-JOIN tblCTContractHeader CH ON CH.intContractHeaderId = CD.intContractHeaderId
+LEFT JOIN tblCTContractDetail CD ON CD.intContractDetailId = D.intContractDetailId
+LEFT JOIN tblCTContractHeader CH ON CH.intContractHeaderId = CD.intContractHeaderId
 JOIN tblICItem I ON I.intItemId = D.intItemId
 LEFT JOIN tblICCommodityAttribute CA1 ON CA1.intCommodityAttributeId = I.intOriginId
 JOIN tblEMEntity E ON E.intEntityId = CH.intEntityId
-JOIN tblICItemUOM IU ON IU.intItemUOMId = CD.intNetWeightUOMId
+LEFT JOIN tblICItemUOM IU ON IU.intItemUOMId = CD.intNetWeightUOMId
 LEFT JOIN tblICUnitMeasure LCWU ON LCWU.intUnitMeasureId = IU.intUnitMeasureId
 LEFT JOIN tblICUnitMeasure LCIU ON LCIU.intUnitMeasureId = CD.intUnitMeasureId
 JOIN tblICCommodity Comm ON Comm.intCommodityId = I.intCommodityId
