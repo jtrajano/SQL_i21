4a06f31b4de5acb6353e1f0985dc913a9606bcda
Maverick Linget <maverick.linget@iRely.com>
2022-06-21 09:50:21 +0800

    RM-4676

---
+++ i21Database/dbo/Stored Procedures/uspRKGetCreditLineCurrencyRate.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[uspRKGetCreditLineCurrencyRate]
+	@intFromCurrencyId INT = NULL
+	, @intToCurrencyId INT = NULL
+AS
+BEGIN
+
+	SELECT TOP 1 dblRate
+	FROM tblSMCurrencyExchangeRate er
+	JOIN tblSMCurrencyExchangeRateDetail rd ON er.intCurrencyExchangeRateId = rd.intCurrencyExchangeRateId
+	WHERE intFromCurrencyId = @intFromCurrencyId
+	AND intToCurrencyId = @intToCurrencyId
+	AND dtmValidFromDate <= GETDATE()
+	ORDER BY dtmValidFromDate DESC
+
+END
