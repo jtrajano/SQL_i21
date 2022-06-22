88d70e3650d15227a545fdd4bf81fb587c2533ea
LexPeregrino <lex.peregrino@irely.com>
2022-06-22 12:29:02 +0800

    CT-7425 v1
    (cherry picked from commit 23e40a2bcfd1d909fdb05478e1537beede30ad7a)
    (cherry picked from commit f615ad3a1a45fb6053bbe2d8cc4434f68006b0b2)

--- i21Database/dbo/Stored Procedures/uspCTReportPriceFixationDetail.sql
+++ i21Database/dbo/Stored Procedures/uspCTReportPriceFixationDetail.sql
@@ -88,8 +88,10 @@ BEGIN TRY
 			dbo.fnRemoveTrailingZeroes(PD.dblFutures) + CY.strCurrency + '-' + isnull(rtrt2.strTranslation,CM.strUnitMeasure)	AS strGABPrice,
 			CD.dblRatio,
 			dbo.fnRemoveTrailingZeroes(PD.dblQuantity) + ' ' + CD.strItemUOM AS strQtyWithUOM,
-			PD.dblFX
-		   ,ysnEnableFXFieldInContractPricing = @ysnEnableFXFieldInContractPricing
+			lblFX = CASE WHEN PD.dblFX != 1  AND PD.dblFX IS NOT NULL THEN 'FX' ELSE NULL  END,
+			strFX = CASE WHEN PD.dblFX != 1  AND PD.dblFX IS NOT NULL THEN LTRIM(CAST( PD.dblFX AS NUMERIC(18, 6)))  ELSE NULL END,
+			dblFX = PD.dblFX,
+		    ysnEnableFXFieldInContractPricing = @ysnEnableFXFieldInContractPricing
 
 	FROM	tblCTPriceFixation			PF
 	JOIN	tblCTPriceFixationDetail	PD	ON	PD.intPriceFixationId			=	PF.intPriceFixationId
