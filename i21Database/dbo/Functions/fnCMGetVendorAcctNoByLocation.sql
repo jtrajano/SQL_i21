464f99113f6de4af464d5bbd030e38b8722d3b14
Jeffrey Trajano <jeffrey.trajano@irely.com>
2022-06-22 10:29:51 +0800

    GL-9018 Vendor AccountNumber in Checks

--- i21Database/dbo/Functions/fnCMGetVendorAcctNoByLocation.sql
+++ i21Database/dbo/Functions/fnCMGetVendorAcctNoByLocation.sql
@@ -3,30 +3,36 @@ CREATE FUNCTION  dbo.fnCMGetVendorAcctNoByLocation
 	@intBankAccountId INT,
 	@intEntityVendorId INT
 )
-
-RETURNS  NVARCHAR(30)
+RETURNS  NVARCHAR(50)
 AS
 BEGIN
 
-declare @intLocationSegmentId int , @intCompanySegmentId INT, @intCompanyLocationId INT, @strVendorAccountNum NVARCHAR(50) = ''
-select  @intLocationSegmentId = L.intAccountSegmentId, @intCompanySegmentId = C.intAccountSegmentId from tblCMBankAccount A  
-
-outer apply(
+DECLARE @intLocationSegmentId int , @intCompanySegmentId INT, @intCompanyLocationId INT, @strVendorAccountNum NVARCHAR(50) = ''
+SELECT  @intLocationSegmentId = L.intAccountSegmentId, @intCompanySegmentId = C.intAccountSegmentId from tblCMBankAccount A  
+OUTER apply(
 	select  intAccountSegmentId from vyuGLLocationAccountId where intGLAccountId = intAccountId
 )L
-outer apply(
+OUTER apply(
 	select  intAccountSegmentId from vyuGLCompanyAccountId where intGLAccountId = intAccountId
 )C
 WHERE @intBankAccountId = intBankAccountId
 
-if @intCompanySegmentId is not null
-	select @intCompanyLocationId =intCompanyLocationId from tblSMCompanyLocation where intProfitCenter = @intLocationSegmentId
-ELSE
-	if @intLocationSegmentId is not null
-	select TOP 1 @intCompanyLocationId =intCompanyLocationId from tblSMCompanyLocation where intProfitCenter = @intLocationSegmentId
+IF @intLocationSegmentId IS NOT NULL AND @intCompanySegmentId IS NULL
+SELECT TOP 1 @strVendorAccountNum = strVendorAccountNum from tblSMCompanyLocation A JOIN
+tblAPVendorAccountNumLocation B ON A.intCompanyLocationId = B.intCompanyLocationId
+WHERE intProfitCenter = @intLocationSegmentId
 
-IF @intCompanyLocationId IS NOT NULL
-	SELECT TOP 1 @strVendorAccountNum = strVendorAccountNum FROM [tblAPVendorAccountNumLocation] where @intCompanyLocationId = intCompanyLocationId AND intEntityVendorId = @intEntityVendorId
+
+IF @intLocationSegmentId IS NULL AND @intCompanySegmentId IS NOT NULL
+SELECT TOP 1 @strVendorAccountNum = strVendorAccountNum from tblSMCompanyLocation A JOIN
+tblAPVendorAccountNumLocation B ON A.intCompanyLocationId = B.intCompanyLocationId
+WHERE intCompanySegment = @intCompanySegmentId
+
+
+IF @intLocationSegmentId IS NOT NULL AND @intCompanySegmentId IS NOT NULL
+SELECT TOP 1 @strVendorAccountNum = strVendorAccountNum from tblSMCompanyLocation A JOIN
+tblAPVendorAccountNumLocation B ON A.intCompanyLocationId = B.intCompanyLocationId
+WHERE intCompanySegment = @intCompanySegmentId AND intProfitCenter = @intLocationSegmentId
 
 IF ISNULL(@strVendorAccountNum,'') = ''
 	SELECT TOP 1 @strVendorAccountNum = strVendorAccountNum FROM tblAPVendor  WHERE  intEntityId = @intEntityVendorId
