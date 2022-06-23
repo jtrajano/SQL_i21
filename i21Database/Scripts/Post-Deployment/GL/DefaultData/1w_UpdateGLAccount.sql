73fa55459785cc52845393bc677ca76a58816aa7
Jeffrey Trajano <jeffrey.trajano@irely.com>
2022-06-23 19:50:19 +0800

    GL-9161

--- i21Database/Scripts/Post-Deployment/GL/DefaultData/1w_UpdateGLAccount.sql
+++ i21Database/Scripts/Post-Deployment/GL/DefaultData/1w_UpdateGLAccount.sql
@@ -5,7 +5,7 @@ GO
     UPDATE A SET ysnRevalue = 1
     FROM tblGLAccount A 
     JOIN vyuGLAccountDetail B ON A.intAccountId = B.intAccountId
-    WHERE strAccountType IN ('Asset','Liability', 'Equity') AND ysnRevalue IS NULL
+    WHERE strAccountType IN ('Asset','Liability', 'Equity') AND A.ysnRevalue IS NULL
 GO
 
 
