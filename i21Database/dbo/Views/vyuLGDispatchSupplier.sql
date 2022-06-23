35463b49c7a44b43b01b66a7e8c7211b3b60a4d7
Arman Arenas <arman.arenas@irely.com>
2022-06-23 23:28:30 +0800

    LG-3303 updated vyuLGDispatchSupplier and vyuLGDispatchSupplyPoint to only display vendors data

--- i21Database/dbo/Views/vyuLGDispatchSupplier.sql
+++ i21Database/dbo/Views/vyuLGDispatchSupplier.sql
@@ -1,20 +1,10 @@
 ﻿CREATE VIEW [dbo].[vyuLGDispatchSupplier]
 AS
-SELECT 
-	intKeyColumn = CONVERT(INT, ROW_NUMBER() OVER (ORDER BY strEntityName ASC))
-	,SUP.* 
-FROM
-   (SELECT 
-		intEntityId = CL.intCompanyLocationId
-		,strEntityName = CL.strLocationName
-		,strLocationType = 'Bulk Location' COLLATE Latin1_General_CI_AS
-	FROM tblSMCompanyLocation CL 
-	UNION ALL
-	SELECT 
-		intEntityId = E.intEntityId
-		,strEntityName = E.strName
-		,strLocationType = 'Supplier Terminal' COLLATE Latin1_General_CI_AS
-	FROM tblEMEntity E 
-	INNER JOIN tblAPVendor V ON V.intEntityId = E.intEntityId AND V.ysnTransportTerminal = 1
-	) SUP
+SELECT E.intEntityId
+	,strEntityName = E.strName
+	,strEntityNo
+	,ysnTransportTerminal = ISNULL(ysnTransportTerminal, 0)
+FROM tblEMEntity E 
+INNER JOIN tblAPVendor V ON V.intEntityId = E.intEntityId
+
 GO
