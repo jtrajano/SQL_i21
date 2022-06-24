7444d6547703b0c870018b20121a0b09aeb58253
Jopet Lambinicio <jopet.lambinicio@irely.com>
2022-06-24 15:43:36 +0800

    AP-10534

--- i21Database/dbo/User Defined Types/VoucherPayable.sql
+++ i21Database/dbo/User Defined Types/VoucherPayable.sql
@@ -113,6 +113,7 @@ CREATE TYPE [dbo].[VoucherPayable] AS TABLE
 	/*Tax info*/
 	[intFreightTermId]				INT NULL,
 	[intPurchaseTaxGroupId]			INT NULL,
+	[ysnOverrideTaxGroup] 			BIT NULL,
 	[dblTax]						DECIMAL(18,2) NOT NULL DEFAULT(0), --IF THIS IS NOT 0, PLEASE PROVIDE DATA FOR VoucherDetailTax
 	/*Discount Info*/
 	[dblDiscount]					DECIMAL(18,2) NOT NULL DEFAULT(0),
@@ -161,6 +162,5 @@ CREATE TYPE [dbo].[VoucherPayable] AS TABLE
  	[dblOptionalityPremium] DECIMAL(18, 6) DEFAULT 0,
 	 /*Tax Override*/
 	[strTaxPoint] NVARCHAR (50) COLLATE Latin1_General_CI_AS NULL,
-	[intTaxLocationId] INT NULL,
-	[intOriginalTaxGroupId] INT NULL
+	[intTaxLocationId] INT NULL
 )
