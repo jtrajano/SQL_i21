4d04a3b4d73f15628fd861468cddeb89bf8fdefa
Mon Gonzales <mon.gonzales@irely.com>
2022-06-28 12:19:41 +0800

    SC-4570 - Adding Rail Menu and Functions Needed

---
+++ i21Database/dbo/Tables/tblSCRailSheetLog.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tblSCRailSheetLog]
+(
+	intRailSheetLogId int not null identity(1,1) primary key
+	,intRailSheetId int not null
+	,strLog nvarchar(100)
+	,ysnError bit not null default(0)
+	,dtmDate datetime default(getdate())
+	,intConcurrencyId int default(1)
+	
+	,constraint fk_tblSCRailSheetLog_tblSCRailSheet_intRailSheetId foreign key (intRailSheetId) references tblSCRailSheet(intRailSheetId)
+)
