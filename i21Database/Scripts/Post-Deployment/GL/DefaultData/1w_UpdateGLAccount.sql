383c9e10849c198abbf5677cfb3a9399c379b17a
Jeffrey Trajano <jeffrey.trajano@irely.com>
2022-06-29 11:37:42 +0800

    GL-9186

--- i21Database/Scripts/Post-Deployment/GL/DefaultData/1w_UpdateGLAccount.sql
+++ i21Database/Scripts/Post-Deployment/GL/DefaultData/1w_UpdateGLAccount.sql
@@ -1,14 +1,11 @@
 GO
     EXEC dbo.uspGLUpdateAccountLocationId
 GO
-    IF NOT EXISTS ( SELECT 1 FROM tblGLDataFixLog WHERE strDescription ='Set GL Account should be revalued.')
-    BEGIN
-        UPDATE A SET ysnRevalue = 1
-        FROM tblGLAccount A 
-        JOIN vyuGLAccountDetail B ON A.intAccountId = B.intAccountId
-        WHERE strAccountType IN ('Asset','Liability', 'Equity')
-        INSERT INTO tblGLDataFixLog(dtmDate, strDescription) VALUES(GETDATE(), 'Set GL Account should be revalued.')
-    END
+    UPDATE A SET ysnRevalue = 1
+    FROM tblGLAccount A 
+    JOIN vyuGLAccountDetail B ON A.intAccountId = B.intAccountId
+    WHERE strAccountType IN ('Asset','Liability', 'Equity')
+    AND A.ysnRevalue IS NULL
 GO
     EXEC uspGLRefreshOverrideAccountTable 1
 GO
