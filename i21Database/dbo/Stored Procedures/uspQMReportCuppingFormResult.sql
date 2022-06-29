d38137f5021de84e773a9e8a7437089f4b01b81f
Roudinee Panteria <roudinee.panteria@irely.com>
2022-06-29 18:08:57 +0800

    QC-700

--- i21Database/dbo/Stored Procedures/uspQMReportCuppingFormResult.sql
+++ i21Database/dbo/Stored Procedures/uspQMReportCuppingFormResult.sql
@@ -8,15 +8,14 @@ BEGIN TRY
 	SELECT 
 		 QMCS.intCuppingSessionId
 		,QMCS.strCuppingSessionNumber
-		,QMP.strPropertyName
+		,QMTRNM.strPropertyName
 		,QMTR.strPropertyValue
 		,QMTR.strResult
 	FROM tblQMCuppingSession QMCS
 	INNER JOIN tblQMCuppingSessionDetail QMCSD ON QMCS.intCuppingSessionId = QMCSD.intCuppingSessionId
 	INNER JOIN tblQMSample QMS ON QMCSD.intCuppingSessionDetailId = QMS.intCuppingSessionDetailId AND QMS.intParentSampleId = @intSampleId
-	INNER JOIN  tblQMTestResult QMTR ON QMS.intSampleId = QMTR.intSampleId
-	INNER JOIN tblQMProductProperty QMPP ON QMPP.intPropertyId = QMTR.intPropertyId AND QMPP.ysnPrintInCuppingForm = 1 AND QMTR.intProductId = QMPP.intProductId
-	INNER JOIN tblQMProperty QMP ON QMPP.intPropertyId = QMP.intPropertyId AND QMP.strPropertyName NOT IN ('Visual Aspect', 'Humidity', 'Roasting')
+	INNER JOIN tblQMTestResult QMTR ON QMS.intSampleId = QMTR.intSampleId
+	LEFT JOIN vyuQMTestResultNotMapped QMTRNM ON QMTR.intTestResultId = QMTRNM.intTestResultId AND QMTRNM.strPropertyName NOT IN ('Visual Aspect', 'Humidity', 'Roasting')
 
 END TRY
 
