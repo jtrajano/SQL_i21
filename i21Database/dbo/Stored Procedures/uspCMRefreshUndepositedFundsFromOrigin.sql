86b2cafa4645c5ea9321631fa10c5132d1eb0040
Jeffrey Trajano <jeffrey.trajano@irely.com>
2022-06-29 16:43:13 +0800

    GL-9057

--- i21Database/dbo/Stored Procedures/uspCMRefreshUndepositedFundsFromOrigin.sql
+++ i21Database/dbo/Stored Procedures/uspCMRefreshUndepositedFundsFromOrigin.sql
@@ -49,6 +49,18 @@ BEGIN
 	IF @@ERROR <> 0	GOTO uspCMRefreshUndepositedFundsFromOrigin_Rollback
 
 END
+
+-- remove duplicates
+;WITH dup AS(
+	SELECT  ROW_NUMBER() OVER ( PARTITION BY d.strSourceTransactionId ORDER BY d.intUndepositedFundId ) rowId, d.intUndepositedFundId
+	FROM	dbo.tblCMUndepositedFund d 
+	LEFT JOIN tblCMBankTransactionDetail CM 
+	ON CM.intUndepositedFundId = d.intUndepositedFundId
+	WHERE CM.intUndepositedFundId is null
+)
+DELETE A FROM tblCMUndepositedFund A JOIN dup B ON A.intUndepositedFundId = B.intUndepositedFundId
+WHERE B.rowId > 1
+
 -- Insert records from the Deposit Entry
 ;WITH CTE AS (
 SELECT	
@@ -76,20 +88,8 @@ SELECT
 		,ysnEODComplete = NULL
 		,strPaymentInfo = '' COLLATE Latin1_General_CI_AS
 		,b.intCurrencyId
-		
 FROM	vyuCMOriginDepositEntry v INNER JOIN tblCMBankAccount b
-			ON b.strCbkNo = v.aptrx_cbk_no COLLATE Latin1_General_CI_AS 
-WHERE	NOT EXISTS (
-			SELECT TOP 1 1
-			FROM	tblCMUndepositedFund f
-			WHERE	f.strSourceTransactionId = ( 
-							CAST(v.aptrx_vnd_no AS NVARCHAR(10)) 
-							+ CAST(v.aptrx_ivc_no AS NVARCHAR(18)) 
-							+ CAST(v.aptrx_cbk_no AS NVARCHAR(2)) 
-							+ CAST(v.aptrx_chk_no AS NVARCHAR(8))
-						) COLLATE Latin1_General_CI_AS
-		)
-
+			ON b.strCbkNo = v.aptrx_cbk_no COLLATE Latin1_General_CI_AS
 UNION SELECT DISTINCT
 	ISNULL(v.intBankAccountId,@intBankAccountId)  intBankAccountId,
 	strSourceTransactionId,
@@ -111,13 +111,8 @@ UNION SELECT DISTINCT
 	v.strPaymentInfo,
 	v.intCurrencyId
 FROM vyuARUndepositedPayment v
-
 LEFT JOIN tblARPayment p on p.strRecordNumber = v.strSourceTransactionId
-WHERE	NOT EXISTS (
-			SELECT TOP 1 1
-			FROM	tblCMUndepositedFund f
-			WHERE	f.strSourceTransactionId = v.strSourceTransactionId)
-		AND isnull(p.intPaymentMethodId,0) <> 9 -- EXEMPT CF INVOICE
+WHERE isnull(p.intPaymentMethodId,0) <> 9 -- EXEMPT CF INVOICE
 )
 
 INSERT INTO tblCMUndepositedFund (
@@ -163,7 +158,9 @@ SELECT
 		,intCurrencyId
 		
 FROM CTE
-WHERE dblAmount <> 0
+WHERE 
+strSourceTransactionId NOT IN (SELECT strSourceTransactionId FROM tblCMundepositedFund)
+AND dblAmount <> 0
 
 
 IF @@ERROR <> 0	GOTO uspCMRefreshUndepositedFundsFromOrigin_Rollback
