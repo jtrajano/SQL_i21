4cb19bcd3b2fd0d16e326076536031bb97d268d2
Jeffrey Trajano <jeffrey.trajano@irely.com>
2022-06-29 16:36:12 +0800

    GL-9057

--- i21Database/dbo/Stored Procedures/uspCMRefreshUndepositedFundsFromOrigin.sql
+++ i21Database/dbo/Stored Procedures/uspCMRefreshUndepositedFundsFromOrigin.sql
@@ -88,20 +88,8 @@ SELECT
 		,ysnEODComplete = NULL
 		,strPaymentInfo = '' COLLATE Latin1_General_CI_AS
 		,b.intCurrencyId
-		
 FROM	vyuCMOriginDepositEntry v INNER JOIN tblCMBankAccount b
-			ON b.strCbkNo = v.aptrx_cbk_no COLLATE Latin1_General_CI_AS 
---WHERE	NOT EXISTS (
---			SELECT TOP 1 1
---			FROM	tblCMUndepositedFund f
---			WHERE	f.strSourceTransactionId = ( 
---							CAST(v.aptrx_vnd_no AS NVARCHAR(10)) 
---							+ CAST(v.aptrx_ivc_no AS NVARCHAR(18)) 
---							+ CAST(v.aptrx_cbk_no AS NVARCHAR(2)) 
---							+ CAST(v.aptrx_chk_no AS NVARCHAR(8))
---						) COLLATE Latin1_General_CI_AS
---		)
-
+			ON b.strCbkNo = v.aptrx_cbk_no COLLATE Latin1_General_CI_AS
 UNION SELECT DISTINCT
 	ISNULL(v.intBankAccountId,@intBankAccountId)  intBankAccountId,
 	strSourceTransactionId,
