0e39e79d9aefc65fce5b5fb1b316667aa2ed1f4e
Bryan-PC <013.bryan@gmail.com>
2022-06-30 14:29:07 +0800

    CT-7416

---
+++ i21Database/dbo/Stored Procedures/uspCTContractToFeed.sql
@@ -0,0 +1,60 @@
+﻿
+Create PROCEDURE [dbo].[uspCTContractToFeed]
+	@intContractDetailId	INT
+AS
+
+BEGIN TRY
+
+
+	DECLARE @ErrMsg						NVARCHAR(MAX),
+			@strRowState				NVARCHAR(10)
+
+			
+			SELECT CAST(@intContractDetailId as varchar)
+
+				SELECT @strRowState= 'Added'
+				DELETE FROM tblCTContractFeed WHERE intContractDetailId = @intContractDetailId
+				INSERT INTO tblCTContractFeed
+				(
+						intContractHeaderId,		intContractDetailId,		strCommodityCode,		strCommodityDesc,
+						strContractBasis,			strContractBasisDesc,		strSubLocation,			strCreatedBy,
+						strCreatedByNo,				strEntityNo,				strTerm,				strPurchasingGroup,
+						strContractNumber,			strERPPONumber,				intContractSeq,			strItemNo,
+						strStorageLocation,			dblQuantity,				dblCashPrice,			strQuantityUOM,
+						dtmPlannedAvailabilityDate,	dblBasis,					strCurrency,			dblUnitCashPrice,
+						strPriceUOM,				strRowState,				dtmContractDate,		dtmStartDate,	
+						dtmEndDate,					dtmFeedCreated,				strSubmittedBy,			strSubmittedByNo,
+						strOrigin,					dblNetWeight,				strNetWeightUOM,		strVendorAccountNum,
+						strTermCode,				strContractItemNo,			strContractItemName,	strERPItemNumber,			
+						strERPBatchNumber,			strLoadingPoint,			strPackingDescription,	ysnMaxPrice,
+						ysnSubstituteItem,			strLocationName,			strSalesperson,			strSalespersonExternalERPId,	
+						strProducer,				intItemId
+
+				)
+				SELECT	intContractHeaderId,		intContractDetailId,		strCommodityCode,		strCommodityDesc,
+						strContractBasis,			strContractBasisDesc,		strSubLocation,			strCreatedBy,
+						strCreatedByNo,				strEntityNo,				strTerm,				strPurchasingGroup,
+						strContractNumber,			strERPPONumber,				intContractSeq,			strItemNo,
+						strStorageLocation,			dblQuantity,				dblCashPrice,			strQuantityUOM,
+						dtmPlannedAvailabilityDate,	dblBasis,					strCurrency,			dblUnitCashPrice,	
+						strPriceUOM,				
+						CASE WHEN intContractStatusId = 3 THEN 'Delete' ELSE @strRowState END,				
+						dtmContractDate,			dtmStartDate,	
+						dtmEndDate,					GETDATE(),					strSubmittedBy,			strSubmittedByNo,
+						strOrigin,					dblNetWeight,				strNetWeightUOM,		strVendorAccountNum,
+						strTermCode,				strContractItemNo,			strContractItemName,	strERPItemNumber,			
+						strERPBatchNumber,			strLoadingPoint,			strPackingDescription,	ysnMaxPrice,
+						ysnSubstituteItem,			strLocationName,			strSalesperson,			strSalespersonExternalERPId,  
+						strProducer,				intItemId
+
+				FROM	vyuCTContractFeed
+				WHERE	intContractDetailId = @intContractDetailId
+END TRY
+
+BEGIN CATCH
+
+	SET @ErrMsg = ERROR_MESSAGE()  
+	RAISERROR (@ErrMsg,18,1,'WITH NOWAIT')  
+	
+END CATCH
+
