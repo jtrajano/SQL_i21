bce6d851006ca8d2414e7ca524ec523a19663041
Mon Gonzales <mon.gonzales@irely.com>
2022-06-30 12:38:30 +0800

    Fix merge conflicts

--- i21Database/dbo/Tables/tblQMSampleLabel.sql
+++ i21Database/dbo/Tables/tblQMSampleLabel.sql
@@ -7,5 +7,6 @@
 	intConcurrencyId INT NULL CONSTRAINT [DF_tblQMSampleLabel_intConcurrencyId] DEFAULT 1,
 
 	CONSTRAINT [PK_tblQMSampleLabel] PRIMARY KEY (intSampleLabelId),
-	CONSTRAINT [AK_tblQMSampleLabel_strSampleLabelName] UNIQUE (strSampleLabelName)
+	-- MON
+	-- CONSTRAINT [AK_tblQMSampleLabel_strSampleLabelName] UNIQUE (strSampleLabelName)
 )
