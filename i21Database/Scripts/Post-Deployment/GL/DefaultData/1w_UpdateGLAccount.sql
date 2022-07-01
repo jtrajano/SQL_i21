8f0b025c16058ab04b6dc45a7c5e87de2a66db8c
Jeffrey Trajano <jeffrey.trajano@irely.com>
2022-07-01 10:37:15 +0800

    GL-9186

--- i21Database/Scripts/Post-Deployment/GL/DefaultData/1w_UpdateGLAccount.sql
+++ i21Database/Scripts/Post-Deployment/GL/DefaultData/1w_UpdateGLAccount.sql
@@ -1,7 +1,7 @@
 GO
     EXEC dbo.uspGLUpdateAccountLocationId
 GO
-    UPDATE A SET ysnRevalue = 1
+    UPDATE A SET A.ysnRevalue = 1
     FROM tblGLAccount A 
     JOIN vyuGLAccountDetail B ON A.intAccountId = B.intAccountId
     WHERE strAccountType IN ('Asset','Liability', 'Equity')
