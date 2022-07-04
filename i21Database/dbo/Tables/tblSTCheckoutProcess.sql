e85953efc13dbac557e63c147bbc956825c563f8
Jase Rueda <jase.rueda@iRely.com>
2022-07-04 17:58:51 +0800

    CS-31

--- i21Database/dbo/Tables/tblSTCheckoutProcess.sql
+++ i21Database/dbo/Tables/tblSTCheckoutProcess.sql
@@ -7,5 +7,6 @@ CREATE TABLE [dbo].[tblSTCheckoutProcess]
     [strGuid]					NVARCHAR(50) NOT NULL,
     [intConcurrencyId]			INT NOT NULL, 
     CONSTRAINT [PK_tblSTCheckoutProcess_intCheckoutProcessId] PRIMARY KEY ([intCheckoutProcessId]), 
+    CONSTRAINT [FK_tblSTCheckoutProcess_tblSTCheckoutHeader] FOREIGN KEY ([intCheckoutId]) REFERENCES [tblSTCheckoutHeader]([intCheckoutId]) ON DELETE CASCADE,
     CONSTRAINT [FK_tblSTCheckoutProcess_tblSTStore] FOREIGN KEY ([intStoreId]) REFERENCES [tblSTStore]([intStoreId]) ON DELETE CASCADE,
 )
