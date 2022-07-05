7bd9c9b902c72d35546e959aff890683e0d1442c
Roudinee Panteria <roudinee.panteria@irely.com>
2022-07-05 18:44:25 +0800

    AR-14914

---
+++ i21Database/dbo/Functions/fnARGetCustomerDefaultPayToBankAccount.sql
@@ -0,0 +1,27 @@
+﻿CREATE FUNCTION [dbo].[fnARGetCustomerDefaultPayToBankAccount]
+(
+	 @EntityCustomerId	INT
+	,@CurrencyId		INT
+	,@CompanyLocationId	INT
+)
+RETURNS INT
+AS
+BEGIN
+	RETURN	ISNULL((	SELECT TOP 1 
+							intBankAccountId
+						FROM
+							tblARCustomerDefaultPayToBankAccount
+						WHERE
+							intEntityCustomerId = @EntityCustomerId
+						AND intCurrencyId = @CurrencyId
+						AND intCompanyLocationId = @CompanyLocationId
+					), (SELECT TOP 1
+							intBankAccountId
+						FROM
+							tblARCompanyDefaultPayToBankAccount
+						WHERE
+							intCurrencyId = @CurrencyId
+						AND intCompanyLocationId = @CompanyLocationId
+						)
+				    )		
+END
