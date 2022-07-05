e8a5d1f56f870bb86894866254b8acf083aa31cc
Cris <cris.lazaga@irely.com>
2022-07-05 08:41:31 +0800

    TR-1957
    (cherry picked from commit 47f71b746cbe2bdef44bdf5fd822ac27f6196f19)

--- i21Database/dbo/Views/vyuTRGetCompanyPreference.sql
+++ i21Database/dbo/Views/vyuTRGetCompanyPreference.sql
@@ -37,6 +37,8 @@ SELECT CP.intCompanyPreferenceId
 	, CP.intConcurrencyId
 	, CP.ysnFreightInRequired
 	, CP.ysnComboFreight
+	, CP.strDtnImportProcessFolder
+	, CP.strDtnImportArchiveFolder
 FROM tblTRCompanyPreference CP
 LEFT JOIN tblSMImportFileHeader Import on Import.intImportFileHeaderId = CP.intRackPriceImportMappingId
 LEFT JOIN tblSMImportFileHeader ImportBol ON ImportBol.intImportFileHeaderId = CP.intBolImportFormatId 
