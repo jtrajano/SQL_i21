ff5285d8095ea2d02efe3810e9668d2b9880e87c
Edsel Roldan Magura <edsel.magura@iRely.com>
2022-07-05 16:14:28 +0800

    HD-4078 (w/ conflict)

--- i21Database/dbo/Views/vyuHDTimeEntryTicket.sql
+++ i21Database/dbo/Views/vyuHDTimeEntryTicket.sql
@@ -19,8 +19,20 @@
 			,f.strModule
 			,i.strProjectName
 			,i.intProjectId
+			,j.strStatus
+			,dtmCloseDate = CONVERT(VARCHAR(30),a.dtmCompleted , 101) 
+			,intDefaultCurrencyExchangeRateTypeId = DefaultHelpDeskRateType.intHelpdeskRateTypeId
+			,strDefaultCurrencyExchangeRateType = DefaultHelpDeskRateType.strCurrencyExchangeRateType
 		from
 			tblHDTicket a
+			outer apply (
+					select top 1  a.intHelpdeskRateTypeId
+								 ,b.strCurrencyExchangeRateType
+					from tblSMMultiCurrency a
+							inner join
+						 tblSMCurrencyExchangeRateType b 
+					on b.intCurrencyExchangeRateTypeId = a.intHelpdeskRateTypeId
+			) DefaultHelpDeskRateType
 			left join tblSMCurrency b on b.intCurrencyID = a.intCurrencyId
 			left join tblSMCurrencyExchangeRateType c on c.intCurrencyExchangeRateTypeId = a.intCurrencyExchangeRateTypeId
 			left join tblEMEntity d on d.intEntityId = a.intAssignedToEntity
@@ -29,5 +41,8 @@
 			left join tblEMEntity g on g.intEntityId = a.intCustomerId
 			left join tblHDProjectTask h on h.intTicketId = a.intTicketId
 			left join tblHDProject i on i.intProjectId = h.intProjectId
+			left join tblHDTicketStatus j on a.intTicketStatusId = j.intTicketStatusId
+			
 		where
 			a.strType <> 'CRM'
+GO
