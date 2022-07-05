7404c3f9a4afa0d5f54812a1e45dc35093293aee
Jopet Lambinicio <jopet.lambinicio@irely.com>
2022-07-05 13:59:43 +0800

    AP-10659

--- i21Database/dbo/Tables/tblAPDefaultPayFromBankAccount.sql
+++ i21Database/dbo/Tables/tblAPDefaultPayFromBankAccount.sql
@@ -1,7 +1,8 @@
 CREATE TABLE [dbo].[tblAPDefaultPayFromBankAccount]
 (
 	[intDefaultPayFromBankAccountId] INT NOT NULL IDENTITY,
-    [intCurrencyId] INT NOT NULL, 
+    [intCompanyLocationId] INT NOT NULL DEFAULT 0,
+    [intCurrencyId] INT NOT NULL DEFAULT 0, 
     [intBankAccountId] INT NOT NULL, 
     [intConcurrencyId] INT NOT NULL DEFAULT 1,
 	CONSTRAINT [PK_tblAPDefaultPayFromBankAccount] PRIMARY KEY ([intDefaultPayFromBankAccountId])
