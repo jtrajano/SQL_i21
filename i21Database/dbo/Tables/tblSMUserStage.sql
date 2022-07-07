3a93e8fd55294cac0e2a7d171d90c7cadae03d3b
Chris Salvante <chris.salvante@irely.com>
2022-07-07 16:03:22 +0800

    FRM-9260

---
+++ i21Database/dbo/Tables/tblSMUserStage.sql
@@ -0,0 +1,28 @@
+﻿CREATE TABLE tblSMUserStage
+(
+	intUserStageId				INT IDENTITY(1,1),
+	strUserName					NVARCHAR(100) COLLATE Latin1_General_CI_AS,
+	strUserId					NVARCHAR(100) COLLATE Latin1_General_CI_AS,
+	strExtErpId					NVARCHAR(100) COLLATE Latin1_General_CI_AS,
+	strEmail					NVARCHAR(100) COLLATE Latin1_General_CI_AS,
+	strPhone					NVARCHAR(100) COLLATE Latin1_General_CI_AS,
+	strMobile					NVARCHAR(100) COLLATE Latin1_General_CI_AS,
+	strContactName				NVARCHAR(100) COLLATE Latin1_General_CI_AS,
+	strLocationName				NVARCHAR(100) COLLATE Latin1_General_CI_AS,
+	strAddress					NVARCHAR(100) COLLATE Latin1_General_CI_AS,
+	strCity						NVARCHAR(100) COLLATE Latin1_General_CI_AS,
+	strState					NVARCHAR(100) COLLATE Latin1_General_CI_AS,
+	strZip						NVARCHAR(100) COLLATE Latin1_General_CI_AS,
+	strCountry					NVARCHAR(100) COLLATE Latin1_General_CI_AS,
+	strUserRole					NVARCHAR(100) COLLATE Latin1_General_CI_AS,
+	ysnActive					BIT DEFAULT 0,
+
+	strTransactionType			NVARCHAR(50) COLLATE Latin1_General_CI_AS,
+	strErrorMessage				NVARCHAR(MAX) COLLATE Latin1_General_CI_AS,
+	strImportStatus				NVARCHAR(50) COLLATE Latin1_General_CI_AS,
+	strSessionId				NVARCHAR(50) COLLATE Latin1_General_CI_AS,
+	dtmTransactionDate			DATETIME DEFAULT(GETDATE()),
+	ysnMailSent					BIT DEFAULT 0,
+
+	CONSTRAINT [PK_tblSMUserStage_intUserStageId] PRIMARY KEY ([intUserStageId])
+)
