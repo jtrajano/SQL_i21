f9885f88de8f50eec365e161fdb94d873f47cfbf
Arman Arenas <arman.arenas@irely.com>
2022-07-07 19:14:27 +0800

    22.1Feature changes restoration

--- i21Database/dbo/Views/vyuLGDispatchOrder.sql
+++ i21Database/dbo/Views/vyuLGDispatchOrder.sql
@@ -12,19 +12,57 @@ SELECT
 	,DO.strComments
 	,DO.intConcurrencyId
 	,DO.intSourceType
+	,DO.intOriginType
+	,DO.intVendorId
+	,DO.intVendorLocationId
+	,DO.intCompanyLocationId
+	,DO.intCompanyLocationSubLocationId
+	,strOriginType = CASE (DO.intOriginType) 
+		WHEN 1 THEN 'Location' 
+		ELSE 'Terminal' END
+	,strVendor = V.strName
+	,strVendorLocation = VL.strLocationName
+	,strCompanyLocation = CL.strLocationName
+	,strSubLocation = CLSL.strSubLocationName
+	,strFromAddress = CASE WHEN (DO.intVendorId IS NOT NULL) THEN VL.strAddress 
+		WHEN (CLSL.intCompanyLocationSubLocationId IS NOT NULL) THEN CLSL.strAddress 
+		ELSE CL.strAddress END
+	,strFromCity = CASE WHEN (DO.intVendorId IS NOT NULL) THEN VL.strCity 
+		WHEN (CLSL.intCompanyLocationSubLocationId IS NOT NULL) THEN CLSL.strCity 
+		ELSE CL.strCity END
+	,strFromState = CASE WHEN (DO.intVendorId IS NOT NULL) THEN VL.strState 
+		WHEN (CLSL.intCompanyLocationSubLocationId IS NOT NULL) THEN CLSL.strState 
+		ELSE CL.strStateProvince END
+	,strFromCountry = CASE WHEN (DO.intVendorId IS NOT NULL) THEN VL.strCountry  
+		ELSE CL.strCountry END
+	,strFromZipCode = CASE WHEN (DO.intVendorId IS NOT NULL) THEN VL.strZipCode 
+		WHEN (CLSL.intCompanyLocationSubLocationId IS NOT NULL) THEN CLSL.strZipCode 
+		ELSE CL.strZipPostalCode END
+	,dblFromLongitude = CASE WHEN (DO.intVendorId IS NOT NULL) THEN VL.dblLongitude 
+		WHEN (CLSL.intCompanyLocationSubLocationId IS NOT NULL) THEN CLSL.dblLongitude 
+		ELSE CL.dblLongitude END
+	,dblFromLatitude = CASE WHEN (DO.intVendorId IS NOT NULL) THEN VL.dblLatitude 
+		WHEN (CLSL.intCompanyLocationSubLocationId IS NOT NULL) THEN CLSL.dblLatitude 
+		ELSE CL.dblLatitude END
 	,strShipVia = SV.strName
 	,strDriver = DV.strName
 	,strTruckNumber = SVT.strTruckNumber
 	,strTrailerNumber = SVTL.strTrailerNumber
+	,strTrailerType = SVTL.strType
+	,strTrailerStatus = SVTL.strTrailerStatus
+	,dblMaxWeight = DO.dblMaxWeight
+	,dblLoadWeight = DO.dblLoadWeight
 	,strDispatchStatus = CASE (DO.intDispatchStatus) 
-		WHEN 1 THEN 'Scheduled'
-		WHEN 2 THEN 'TM Orders'
-		WHEN 3 THEN 'Complete'
-		WHEN 4 THEN 'Cancelled'
+		WHEN 0 THEN 'Created'
+		WHEN 1 THEN 'Routed'
+		WHEN 2 THEN 'Scheduled'
+		WHEN 3 THEN 'In Progress'
+		WHEN 4 THEN 'Complete'
+		WHEN 5 THEN 'Cancelled'
 		ELSE '' END COLLATE Latin1_General_CI_AS
 	,strSourceType = CASE (DO.intSourceType)
 		WHEN 1 THEN 'LG Loads - Outbound'
-		WHEN 2 THEN 'In Progress'
+		WHEN 2 THEN 'TM Orders'
 		WHEN 3 THEN 'LG Loads - Inbound'
 		WHEN 4 THEN 'TM Sites'
 		WHEN 5 THEN 'Entities'
@@ -35,4 +73,8 @@ FROM tblLGDispatchOrder DO
 	LEFT JOIN tblEMEntity DV ON DV.intEntityId = DO.intDriverEntityId
 	LEFT JOIN tblSMShipViaTruck SVT ON SVT.intEntityShipViaTruckId = DO.intEntityShipViaTruckId
 	LEFT JOIN tblSMShipViaTrailer SVTL ON SVTL.intEntityShipViaTrailerId = DO.intEntityShipViaTrailerId
+	LEFT JOIN tblEMEntity V ON V.intEntityId = DO.intVendorId
+	LEFT JOIN tblEMEntityLocation VL ON VL.intEntityLocationId = DO.intVendorLocationId
+	LEFT JOIN tblSMCompanyLocation CL ON CL.intCompanyLocationId = DO.intCompanyLocationId
+	LEFT JOIN tblSMCompanyLocationSubLocation CLSL ON CLSL.intCompanyLocationSubLocationId = DO.intCompanyLocationSubLocationId
 GO
