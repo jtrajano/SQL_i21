69aff9380f1d4c180d7fb80f82c326bd8737488d
Roudinee Panteria <roudinee.panteria@irely.com>
2022-07-08 12:11:22 +0800

    AR-14629

--- i21Database/dbo/Views/vyuARGetInvoice.sql
+++ i21Database/dbo/Views/vyuARGetInvoice.sql
@@ -183,6 +183,7 @@ SELECT
 	,intBorrowingFacilityLimitId		= INV.intBorrowingFacilityLimitId
 	,strBorrowingFacilityLimit			= BFL.strBorrowingFacilityLimit
 	,intBorrowingFacilityLimitDetailId	= INV.intBorrowingFacilityLimitDetailId
+	,strBorrowingFacilityLimitDetail	= BFLD.strLimitDescription
 	,strBankReferenceNo					= INV.strBankReferenceNo
 	,strBankTransactionId				= INV.strBankTransactionId
 	,dblLoanAmount						= INV.dblLoanAmount
@@ -412,6 +413,7 @@ LEFT JOIN tblCMBank B ON B.intBankId = ISNULL(INV.intBankId,0)
 LEFT JOIN vyuCMBankAccount BA ON BA.intBankAccountId = ISNULL(INV.intBankAccountId,0)
 LEFT JOIN tblCMBorrowingFacility BF ON BF.intBorrowingFacilityId = ISNULL(INV.intBorrowingFacilityId,0)
 LEFT JOIN tblCMBorrowingFacilityLimit BFL ON BFL.intBorrowingFacilityLimitId = ISNULL(INV.intBorrowingFacilityLimitId,0)
+LEFT JOIN tblCMBorrowingFacilityLimitDetail BFLD ON BFLD.intBorrowingFacilityLimitDetailId = ISNULL(INV.intBorrowingFacilityLimitDetailId,0)
 LEFT JOIN tblCMBankValuationRule BVR ON BVR.intBankValuationRuleId = ISNULL(INV.intBankValuationRuleId,0)
 LEFT JOIN vyuARTaxLocation TAXLOCATION ON TAXLOCATION.intTaxLocationId = ISNULL(INV.intTaxLocationId,0) AND TAXLOCATION.strType = CASE WHEN INV.strTaxPoint = 'Destination' THEN 'Entity' ELSE 'Company' END
 OUTER APPLY(
