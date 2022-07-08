8c67027ba68f38cb175e796584f28062c2b9619b
Edsel Roldan Magura <edsel.magura@iRely.com>
2022-07-08 09:14:00 +0800

    Fixed Merge Conflict

--- i21Database/dbo/Views/vyuHDCoworkerGoal.sql
+++ i21Database/dbo/Views/vyuHDCoworkerGoal.sql
@@ -2,29 +2,37 @@
 AS
 SELECT   [intCoworkerGoalId]				= CoworkerGoal.[intCoworkerGoalId]			
 		,[intEntityId]						= CoworkerGoal.[intEntityId]	
+		,[intReportsToId]					= CoworkerGoal.[intReportsToId]
 		,[strFiscalYear]					= CoworkerGoal.[strFiscalYear]	
 		,[intCurrencyId]					= CoworkerGoal.[intCurrencyId]		
 		,[intUtilizationTargetAnnual]		= CoworkerGoal.[intUtilizationTargetAnnual]
 		,[intUtilizationTargetWeekly]		= CoworkerGoal.[intUtilizationTargetWeekly]
 		,[intUtilizationTargetMonthly]		= CoworkerGoal.[intUtilizationTargetMonthly]
-		,[dblAnnualHurdle]					= CoworkerGoal.[dblAnnualHurdle]
-		,[dblIncentiveRate]					= CoworkerGoal.[dblIncentiveRate]
-		,[dblAnnualBudget]					= CoworkerGoal.[dblAnnualBudget]
+		,[dblAnnualHurdle]					= ISNULL(CoworkerGoal.[dblAnnualHurdle], 0)
+		,[strIncentiveType]					= CoworkerGoal.[strIncentiveType]
+		,[strIncentiveRateType]				= CoworkerGoal.[strIncentiveRateType]
+		,[dblIncentiveRate]					= ISNULL(CoworkerGoal.[dblIncentiveRate], 0)
+		,[dblAnnualBudget]					= ISNULL(CoworkerGoal.[dblAnnualBudget], 0)
 		,[intCommissionAccountId]			= CoworkerGoal.[intCommissionAccountId]
 		,[intRevenueAccountId]				= CoworkerGoal.[intRevenueAccountId]
 		,[strGoal]							= CoworkerGoal.[strGoal]
 		,[strCommissionType]				= CoworkerGoal.[strCommissionType]
-		-- Von removed this cause - developer needs to fix this
-		--,[strRateType]					= CoworkerGoal.[strRateType]
-		--,[intCommissionRate]				= CoworkerGoal.[intCommissionRate]
+		,[strCommissionRateType]			= CoworkerGoal.[strCommissionRateType]
+		,[dblCommissionRate]				= ISNULL(CoworkerGoal.[dblCommissionRate], 0)
+		,[dblUnderAllocated]				= ISNULL(CoworkerGoal.[dblUnderAllocated], 0)
+		,[ysnActive]						= CoworkerGoal.[ysnActive]
+		,[ysnCanViewOtherCoworker]			= CoworkerGoal.[ysnCanViewOtherCoworker]
 		,[intConcurrencyId] 				= CoworkerGoal.[intConcurrencyId]
 		,[strFullName]						= Entity.[strName]
+		,[strReportsTo]						= ReportsTo.[strName]
 		,[strCurrency]						= Currency.[strCurrency]
-		,[strCommissionAccount]				= GLAccountCommission.strDescription
-		,[strRevenueAccount]				= GLAccountRevenue.strDescription
+		,[strCommissionAccount]				= GLAccountCommission.strAccountId
+		,[strRevenueAccount]				= GLAccountRevenue.strAccountId
 FROM tblHDCoworkerGoal CoworkerGoal
 		INNER JOIN tblEMEntity Entity
 ON Entity.intEntityId = CoworkerGoal.intEntityId
+		LEFT JOIN tblEMEntity ReportsTo
+ON ReportsTo.intEntityId = CoworkerGoal.intReportsToId
 		INNER JOIN tblSMCurrency Currency
 ON Currency.intCurrencyID = CoworkerGoal.intCurrencyId
 		LEFT JOIN tblGLAccount GLAccountCommission
