3fb22fa4476be03e14e01a9f8afc1c9ce616138d
Dom Caoile <dom.caoile.02@gmail.com>
2022-07-14 11:05:51 +0800

    Revert "FRD-1819"
    This reverts commit 832b866e594262f7a8e5f08e592a1f63d5a05c6d.

--- i21Database/dbo/Functions/fnFRDGetGroupIntSort.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE FUNCTION dbo.fnFRDGetGroupIntSort ( @Type VARCHAR(MAX),@intParentGroupId INT)    
-RETURNS INT    
-AS    
-BEGIN    
-DECLARE @intSort INT          
- set @intSort = ( select top 1 intSort from tblGLAccountGroup  where strAccountType = @Type and intParentGroupId = @intParentGroupId  and isnull(intSort,0) <> 0 group by intParentGroupId,intSort )  
- return @intSort  
-END
