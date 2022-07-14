832b866e594262f7a8e5f08e592a1f63d5a05c6d
Dom Caoile <dom.caoile.02@gmail.com>
2022-07-14 11:04:41 +0800

    FRD-1819

---
+++ i21Database/dbo/Functions/fnFRDGetGroupIntSort.sql
@@ -0,0 +1,8 @@
+﻿CREATE FUNCTION dbo.fnFRDGetGroupIntSort ( @Type VARCHAR(MAX),@intParentGroupId INT)    
+RETURNS INT    
+AS    
+BEGIN    
+DECLARE @intSort INT          
+ set @intSort = ( select top 1 intSort from tblGLAccountGroup  where strAccountType = @Type and intParentGroupId = @intParentGroupId  and isnull(intSort,0) <> 0 group by intParentGroupId,intSort )  
+ return @intSort  
+END
