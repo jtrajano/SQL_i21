f3ab8c336e1025fa41a8b1c26cdce3dded432739
Wendell Wayne Estrada <wendell.estrada@irely.com>
2022-07-16 00:26:27 +0800

    API-293 HDTN-271715 - Item API import - Brand did not get import when template was edited.

--- i21Database/dbo/Tables/tblICItem.sql
+++ i21Database/dbo/Tables/tblICItem.sql
@@ -193,6 +193,8 @@ Type the overview for the table here.
 		[intComputeItemTotalOption] TINYINT NOT NULL DEFAULT(0),
 		[ysnProducePartialPacking] BIT NOT NULL DEFAULT(0),
 		[intSubcategoryId] [int] NULL,
+		[intSubcategoriesId] [int] NULL,
+
 		CONSTRAINT [AK_tblICItem_strItemNo] UNIQUE ([strItemNo]), 
 		CONSTRAINT [PK_tblICItem] PRIMARY KEY ([intItemId]), 
 		CONSTRAINT [FK_tblICItem_tblICManufacturer] FOREIGN KEY ([intManufacturerId]) REFERENCES [tblICManufacturer]([intManufacturerId]), 
