eff39653bd2329749fb5346b33ffc78e195b699c
Arman Arenas <arman.arenas@irely.com>
2022-07-16 15:57:04 +0800

    LG-3451 added vyuLGNotifyPartiesAddresses.strTerminalControlNumber

--- i21Database/dbo/Views/vyuLGNotifyPartiesAddresses.sql
+++ i21Database/dbo/Views/vyuLGNotifyPartiesAddresses.sql
@@ -12,9 +12,12 @@ AS
 			ENLOC.strFax,
 			ENTYPE.strType,
 			ENLOC.strNotes,
+			TCN.strTerminalControlNumber,
 			CONVERT(BIT,ENLOC.ysnActive)									AS ysnActive
 	FROM	[tblEMEntityLocation] ENLOC
 	JOIN	[tblEMEntityType] ENTYPE ON ENTYPE.intEntityId = ENLOC.intEntityId  
+	LEFT JOIN tblTRSupplyPoint SP ON SP.intEntityLocationId = ENLOC.intEntityLocationId
+	LEFT JOIN tblTFTerminalControlNumber TCN ON TCN.intTerminalControlNumberId = SP.intTerminalControlNumberId
 
 	UNION ALL
 
@@ -30,6 +33,7 @@ AS
 			L.strFax,
 			'Company'														AS strType,
 			''																AS strNotes,
+			''																AS strTerminalControlNumber,
 			CONVERT(BIT,L.ysnActive) 										AS ysnActive
 	FROM	tblSMCompanyLocation L
 
@@ -47,5 +51,6 @@ AS
 			strFax,
 			'Bank'															AS strType,
 			''																AS strNotes,
+			''																AS strTerminalControlNumber,
 			CONVERT(BIT,1)													AS ysnActive
 	FROM	tblCMBank
