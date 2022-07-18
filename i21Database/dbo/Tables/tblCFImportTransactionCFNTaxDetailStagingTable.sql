c8bd5059cfb8f2f7d009ee0d27e146a70f7530bf
bponce <bonn.ponce@irely.com>
2022-07-18 09:45:54 +0800

    Reverse commit CF-2736

--- i21Database/dbo/Tables/tblCFImportTransactionCFNTaxDetailStagingTable.sql
+++
@@ -1,8 +0,0 @@
-
-CREATE TABLE [dbo].[tblCFImportTransactionCFNTaxDetailStagingTable](
-	[intRowId] int IDENTITY(1,1) NOT NULL,
-	[intRecordId] int NULL,
-	[strTaxCode] nvarchar(max) COLLATE Latin1_General_CI_AS NULL,
-	[dblTaxValue] numeric(18, 6) NULL,
-	[strGUID] nvarchar(max) COLLATE Latin1_General_CI_AS NULL
-) 
