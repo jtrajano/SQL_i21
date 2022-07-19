4aae6e6d4f44bad4ed6f60bb6acf352cdf79f135
Jase Rueda <jase.rueda@iRely.com>
2022-07-19 10:15:57 +0800

    CS-16

---
+++ i21Database/dbo/Stored Procedures/uspSTCheckoutCommanderTranslogFuelComputation.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[uspSTCheckoutCommanderTranslogFuelComputation]
+	@intCheckoutId											INT,
+	@UDT_Translog StagingTransactionLogFuelComputation		READONLY
+AS
+BEGIN
+
+INSERT INTO tblSTCheckoutFuelSalesByGradeAndPricePoint (intCheckoutId, intProductNumber, dblPrice, dblDollarsSold, dblGallonsSold, intConcurrencyId)
+SELECT	@intCheckoutId,
+		a.intProductNumber,
+		a.dblPrice,
+		a.dblDollarsSold,
+		a.dblGallonsSold,
+		1
+FROM	@UDT_Translog a
+
+END
---
+++ i21Database/dbo/User Defined Types/StagingTransactionLogFuelComputation.sql
@@ -0,0 +1,8 @@
+﻿CREATE TYPE StagingTransactionLogFuelComputation AS TABLE
+(
+	[intRowCount] INT NULL,
+	[intProductNumber] INT NOT NULL,
+	[dblPrice] DECIMAL(18,2) NOT NULL,
+	[dblDollarsSold] DECIMAL(18, 2) NOT NULL,
+	[dblGallonsSold] DECIMAL(18, 3) NOT NULL
+)
