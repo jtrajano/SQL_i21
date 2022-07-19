6369f4f5b15506fa6c7d9beb86ef767292f9de8f
Roudinee Panteria <roudinee.panteria@irely.com>
2022-07-19 16:08:24 +0800

    AR-14994

--- i21Database/dbo/Functions/fnARGetCustomerPricingDetails.sql
+++ i21Database/dbo/Functions/fnARGetCustomerPricingDetails.sql
@@ -122,7 +122,7 @@ BEGIN
 		,SP.strPriceBasis
 		,SP.strCustomerGroup
 		,SP.strCostToUse
-		,SP.dblDeviation + SP.dblDeviation2 + SP.dblDeviation3 + SP.dblDeviation4
+		,ISNULL(SP.dblDeviation, 0) + ISNULL(SP.dblDeviation2, 0) + ISNULL(SP.dblDeviation3, 0) + ISNULL(SP.dblDeviation4, 0)
 		,SP.strLineNote
 		,SP.intRackVendorId
 		,SP.intRackItemId
