78787bbb4d2d200e88a2e659f0ec44a660659364
Arman Arenas <arman.arenas@irely.com>
2022-07-19 17:51:58 +0800

    LG-3545 adjust uspLGFixStartingNumbers to handle non-standard transaction starting numbers

--- i21Database/dbo/Stored Procedures/uspLGFixStartingNumbers.sql
+++ i21Database/dbo/Stored Procedures/uspLGFixStartingNumbers.sql
@@ -11,29 +11,29 @@ BEGIN
 
 	/* Allocation */
 	UPDATE A
-	SET A.intNumber = CAST(LEFT(SUBSTRING(strTransactionId, PATINDEX('%[0-9]%', strTransactionId), 8000), 8000) AS INT) + 1
+	SET A.intNumber = CAST(RIGHT(strTransactionId, COALESCE(NULLIF(PATINDEX('%[^0-9]%', REVERSE(strTransactionId)), 0) - 1, LEN(strTransactionId))) AS INT) + 1
 	FROM tblSMStartingNumber A
 	OUTER APPLY (
 		SELECT strTransactionId = ISNULL(strAllocationNumber, '')
 		FROM tblLGAllocationHeader 
-		WHERE intAllocationHeaderId = (SELECT MAX(intAllocationHeaderId) FROM tblLGAllocationHeader)
+		WHERE intAllocationHeaderId = (SELECT MAX(intAllocationHeaderId) FROM tblLGAllocationHeader WHERE CHARINDEX(A.strPrefix, strAllocationNumber) > 0)
 		) B
 	WHERE A.strModule = 'Logistics' 
 		AND A.strTransactionType = 'Allocations' 
-		AND A.intNumber <> CAST(LEFT(SUBSTRING(strTransactionId, PATINDEX('%[0-9]%', strTransactionId), 8000), 8000) AS INT) + 1
+		AND A.intNumber < CAST(RIGHT(strTransactionId, COALESCE(NULLIF(PATINDEX('%[^0-9]%', REVERSE(strTransactionId)), 0) - 1, LEN(strTransactionId))) AS INT) + 1
 
 	/* Load Schedule */
 	UPDATE A
-	SET A.intNumber = CAST(LEFT(SUBSTRING(strTransactionId, PATINDEX('%[0-9]%', strTransactionId), 8000), 8000) AS INT) + 1
+	SET A.intNumber = CAST(RIGHT(strTransactionId, COALESCE(NULLIF(PATINDEX('%[^0-9]%', REVERSE(strTransactionId)), 0) - 1, LEN(strTransactionId))) AS INT) + 1
 	FROM tblSMStartingNumber A
 	OUTER APPLY (
 		SELECT strTransactionId = ISNULL(strLoadNumber, '')
 		FROM tblLGLoad 
-		WHERE intLoadId = (SELECT MAX(intLoadId) FROM tblLGLoad WHERE intShipmentType = 1)
+		WHERE intLoadId = (SELECT MAX(intLoadId) FROM tblLGLoad WHERE intShipmentType = 1 AND CHARINDEX(A.strPrefix, strLoadNumber) > 0)
 		) B
 	WHERE A.strModule = 'Logistics' 
 		AND A.strTransactionType = 'Load Schedule' 
-		AND A.intNumber <> CAST(LEFT(SUBSTRING(strTransactionId, PATINDEX('%[0-9]%', strTransactionId), 8000), 8000) AS INT) + 1
+		AND A.intNumber < CAST(RIGHT(strTransactionId, COALESCE(NULLIF(PATINDEX('%[^0-9]%', REVERSE(strTransactionId)), 0) - 1, LEN(strTransactionId))) AS INT) + 1
 
 	/* Generate Load/Batch Load */
 	UPDATE A
@@ -46,110 +46,110 @@ BEGIN
 		) B
 	WHERE A.strModule = 'Logistics' 
 		AND A.strTransactionType IN ('Batch Load', 'Generate Load')
-		AND A.intNumber <> intTransactionId + 1
+		AND A.intNumber < intTransactionId + 1
 
 	/* Pick Lots */
 	UPDATE A
-	SET A.intNumber = CAST(LEFT(SUBSTRING(strTransactionId, PATINDEX('%[0-9]%', strTransactionId), 8000), 8000) AS INT) + 1
+	SET A.intNumber = CAST(RIGHT(strTransactionId, COALESCE(NULLIF(PATINDEX('%[^0-9]%', REVERSE(strTransactionId)), 0) - 1, LEN(strTransactionId))) AS INT) + 1
 	FROM tblSMStartingNumber A
 	OUTER APPLY (
 		SELECT strTransactionId = ISNULL(strPickLotNumber, '')
 		FROM tblLGPickLotHeader 
-		WHERE intPickLotHeaderId = (SELECT MAX(intPickLotHeaderId) FROM tblLGPickLotHeader WHERE intType = 1)
+		WHERE intPickLotHeaderId = (SELECT MAX(intPickLotHeaderId) FROM tblLGPickLotHeader WHERE intType = 1 AND CHARINDEX(A.strPrefix, strPickLotNumber) > 0)
 		) B
 	WHERE A.strModule = 'Logistics' 
 		AND A.strTransactionType = 'Pick Lots' 
-		AND A.intNumber <> CAST(LEFT(SUBSTRING(strTransactionId, PATINDEX('%[0-9]%', strTransactionId), 8000), 8000) AS INT) + 1
+		AND A.intNumber < CAST(RIGHT(strTransactionId, COALESCE(NULLIF(PATINDEX('%[^0-9]%', REVERSE(strTransactionId)), 0) - 1, LEN(strTransactionId))) AS INT) + 1
 
 	/* Stock Sales */
 	UPDATE A
-	SET A.intNumber = CAST(LEFT(SUBSTRING(strTransactionId, PATINDEX('%[0-9]%', strTransactionId), 8000), 8000) AS INT) + 1
+	SET A.intNumber = CAST(RIGHT(strTransactionId, COALESCE(NULLIF(PATINDEX('%[^0-9]%', REVERSE(strTransactionId)), 0) - 1, LEN(strTransactionId))) AS INT) + 1
 	FROM tblSMStartingNumber A
 	OUTER APPLY (
 		SELECT strTransactionId = ISNULL(strStockSalesNumber, '')
 		FROM tblLGStockSalesHeader 
-		WHERE intStockSalesHeaderId = (SELECT MAX(intStockSalesHeaderId) FROM tblLGStockSalesHeader)
+		WHERE intStockSalesHeaderId = (SELECT MAX(intStockSalesHeaderId) FROM tblLGStockSalesHeader WHERE CHARINDEX(A.strPrefix, strStockSalesNumber) > 0)
 		) B
 	WHERE A.strModule = 'Logistics' 
 		AND A.strTransactionType = 'Stock Sales' 
-		AND A.intNumber <> CAST(LEFT(SUBSTRING(strTransactionId, PATINDEX('%[0-9]%', strTransactionId), 8000), 8000) AS INT) + 1
+		AND A.intNumber < CAST(RIGHT(strTransactionId, COALESCE(NULLIF(PATINDEX('%[^0-9]%', REVERSE(strTransactionId)), 0) - 1, LEN(strTransactionId))) AS INT) + 1
 
 	/* Delivery Notice */
 	UPDATE A
-	SET A.intNumber = CAST(LEFT(SUBSTRING(strTransactionId, PATINDEX('%[0-9]%', strTransactionId), 8000), 8000) AS INT) + 1
+	SET A.intNumber = CAST(RIGHT(strTransactionId, COALESCE(NULLIF(PATINDEX('%[^0-9]%', REVERSE(strTransactionId)), 0) - 1, LEN(strTransactionId))) AS INT) + 1
 	FROM tblSMStartingNumber A
 	OUTER APPLY (
 		SELECT strTransactionId = ISNULL(strDeliveryNoticeNumber, '')
 		FROM tblLGLoadWarehouse 
-		WHERE intLoadWarehouseId = (SELECT MAX(intLoadWarehouseId) FROM tblLGLoadWarehouse)
+		WHERE intLoadWarehouseId = (SELECT MAX(intLoadWarehouseId) FROM tblLGLoadWarehouse WHERE CHARINDEX(A.strPrefix, strDeliveryNoticeNumber) > 0)
 		) B
 	WHERE A.strModule = 'Logistics' 
 		AND A.strTransactionType = 'Delivery Notice' 
-		AND A.intNumber <> CAST(LEFT(SUBSTRING(strTransactionId, PATINDEX('%[0-9]%', strTransactionId), 8000), 8000) AS INT) + 1
+		AND A.intNumber < CAST(RIGHT(strTransactionId, COALESCE(NULLIF(PATINDEX('%[^0-9]%', REVERSE(strTransactionId)), 0) - 1, LEN(strTransactionId))) AS INT) + 1
 
 	/* Least Cost Routing */
 	UPDATE A
-	SET A.intNumber = CAST(LEFT(SUBSTRING(strTransactionId, PATINDEX('%[0-9]%', strTransactionId), 8000), 8000) AS INT) + 1
+	SET A.intNumber = CAST(RIGHT(strTransactionId, COALESCE(NULLIF(PATINDEX('%[^0-9]%', REVERSE(strTransactionId)), 0) - 1, LEN(strTransactionId))) AS INT) + 1
 	FROM tblSMStartingNumber A
 	OUTER APPLY (
 		SELECT strTransactionId = ISNULL(strRouteNumber, '')
 		FROM tblLGRoute 
-		WHERE intRouteId = (SELECT MAX(intRouteId) FROM tblLGRoute)
+		WHERE intRouteId = (SELECT MAX(intRouteId) FROM tblLGRoute WHERE CHARINDEX(A.strPrefix, strRouteNumber) > 0)
 		) B
 	WHERE A.strModule = 'Logistics' 
 		AND A.strTransactionType = 'Least Cost Routing' 
-		AND A.intNumber <> CAST(LEFT(SUBSTRING(strTransactionId, PATINDEX('%[0-9]%', strTransactionId), 8000), 8000) AS INT) + 1
+		AND A.intNumber < CAST(RIGHT(strTransactionId, COALESCE(NULLIF(PATINDEX('%[^0-9]%', REVERSE(strTransactionId)), 0) - 1, LEN(strTransactionId))) AS INT) + 1
 
 	/* Load Shipping Instruction */
 	UPDATE A
-	SET A.intNumber = CAST(LEFT(SUBSTRING(strTransactionId, PATINDEX('%[0-9]%', strTransactionId), 8000), 8000) AS INT) + 1
+	SET A.intNumber = CAST(RIGHT(strTransactionId, COALESCE(NULLIF(PATINDEX('%[^0-9]%', REVERSE(strTransactionId)), 0) - 1, LEN(strTransactionId))) AS INT) + 1
 	FROM tblSMStartingNumber A
 	OUTER APPLY (
 		SELECT strTransactionId = ISNULL(strLoadNumber, '')
 		FROM tblLGLoad 
-		WHERE intLoadId = (SELECT MAX(intLoadId) FROM tblLGLoad WHERE intShipmentType = 2)
+		WHERE intLoadId = (SELECT MAX(intLoadId) FROM tblLGLoad WHERE intShipmentType = 2 AND CHARINDEX(A.strPrefix, strLoadNumber) > 0)
 		) B
 	WHERE A.strModule = 'Logistics' 
 		AND A.strTransactionType = 'Load Shipping Instruction' 
-		AND A.intNumber <> CAST(LEFT(SUBSTRING(strTransactionId, PATINDEX('%[0-9]%', strTransactionId), 8000), 8000) AS INT) + 1
+		AND A.intNumber < CAST(RIGHT(strTransactionId, COALESCE(NULLIF(PATINDEX('%[^0-9]%', REVERSE(strTransactionId)), 0) - 1, LEN(strTransactionId))) AS INT) + 1
 
 	/* Weight Claims */
 	UPDATE A
-	SET A.intNumber = CAST(LEFT(SUBSTRING(strTransactionId, PATINDEX('%[0-9]%', strTransactionId), 8000), 8000) AS INT) + 1
+	SET A.intNumber = CAST(RIGHT(strTransactionId, COALESCE(NULLIF(PATINDEX('%[^0-9]%', REVERSE(strTransactionId)), 0) - 1, LEN(strTransactionId))) AS INT) + 1
 	FROM tblSMStartingNumber A
 	OUTER APPLY (
 		SELECT strTransactionId = ISNULL(strReferenceNumber, '')
 		FROM tblLGWeightClaim 
-		WHERE intWeightClaimId = (SELECT MAX(intWeightClaimId) FROM tblLGWeightClaim)
+		WHERE intWeightClaimId = (SELECT MAX(intWeightClaimId) FROM tblLGWeightClaim WHERE CHARINDEX(A.strPrefix, strReferenceNumber) > 0)
 		) B
 	WHERE A.strModule = 'Logistics' 
 		AND A.strTransactionType = 'Weight Claims' 
-		AND A.intNumber <> CAST(LEFT(SUBSTRING(strTransactionId, PATINDEX('%[0-9]%', strTransactionId), 8000), 8000) AS INT) + 1
+		AND A.intNumber < CAST(RIGHT(strTransactionId, COALESCE(NULLIF(PATINDEX('%[^0-9]%', REVERSE(strTransactionId)), 0) - 1, LEN(strTransactionId))) AS INT) + 1
 
-	/* Weight Claims */
+	/* Allocation Details */
 	UPDATE A
-	SET A.intNumber = CAST(LEFT(SUBSTRING(strTransactionId, PATINDEX('%[0-9]%', strTransactionId), 8000), 8000) AS INT) + 1
+	SET A.intNumber = CAST(RIGHT(strTransactionId, COALESCE(NULLIF(PATINDEX('%[^0-9]%', REVERSE(strTransactionId)), 0) - 1, LEN(strTransactionId))) AS INT) + 1
 	FROM tblSMStartingNumber A
 	OUTER APPLY (
 		SELECT strTransactionId = ISNULL(strAllocationDetailRefNo, '')
 		FROM tblLGAllocationDetail 
-		WHERE intAllocationDetailId = (SELECT MAX(intAllocationDetailId) FROM tblLGAllocationDetail)
+		WHERE intAllocationDetailId = (SELECT MAX(intAllocationDetailId) FROM tblLGAllocationDetail WHERE CHARINDEX(A.strPrefix, strAllocationDetailRefNo) > 0)
 		) B
 	WHERE A.strModule = 'Logistics' 
 		AND A.strTransactionType = 'Allocation Detail' 
-		AND A.intNumber <> CAST(LEFT(SUBSTRING(strTransactionId, PATINDEX('%[0-9]%', strTransactionId), 8000), 8000) AS INT) + 1
+		AND A.intNumber < CAST(RIGHT(strTransactionId, COALESCE(NULLIF(PATINDEX('%[^0-9]%', REVERSE(strTransactionId)), 0) - 1, LEN(strTransactionId))) AS INT) + 1
 
 	/* Pick Containers */
 	UPDATE A
-	SET A.intNumber = CAST(LEFT(SUBSTRING(strTransactionId, PATINDEX('%[0-9]%', strTransactionId), 8000), 8000) AS INT) + 1
+	SET A.intNumber = CAST(RIGHT(strTransactionId, COALESCE(NULLIF(PATINDEX('%[^0-9]%', REVERSE(strTransactionId)), 0) - 1, LEN(strTransactionId))) AS INT) + 1
 	FROM tblSMStartingNumber A
 	OUTER APPLY (
 		SELECT strTransactionId = ISNULL(strPickLotNumber, '')
 		FROM tblLGPickLotHeader 
-		WHERE intPickLotHeaderId = (SELECT MAX(intPickLotHeaderId) FROM tblLGPickLotHeader WHERE intType = 2)
+		WHERE intPickLotHeaderId = (SELECT MAX(intPickLotHeaderId) FROM tblLGPickLotHeader WHERE intType = 2 AND CHARINDEX(A.strPrefix, strPickLotNumber) > 0)
 		) B
 	WHERE A.strModule = 'Logistics' 
 		AND A.strTransactionType = 'Pick Containers' 
-		AND A.intNumber <> CAST(LEFT(SUBSTRING(strTransactionId, PATINDEX('%[0-9]%', strTransactionId), 8000), 8000) AS INT) + 1
+		AND A.intNumber < CAST(RIGHT(strTransactionId, COALESCE(NULLIF(PATINDEX('%[^0-9]%', REVERSE(strTransactionId)), 0) - 1, LEN(strTransactionId))) AS INT) + 1
 
 END
