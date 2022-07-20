a59f677177a6c93c783813fca707140c940a5240
LexPeregrino <lex.peregrino@irely.com>
2022-07-20 08:38:45 +0800

    CT-7529 v1

--- i21Database/dbo/Stored Procedures/uspCTGetContractBalance.sql
+++ i21Database/dbo/Stored Procedures/uspCTGetContractBalance.sql
@@ -389,8 +389,8 @@ BEGIN
 				INNER JOIN tblICUnitMeasure			UOM			ON UOM.intUnitMeasureId = C1.intUnitMeasureId
 				INNER JOIN tblICItem				IM			ON IM.intItemId = CBL.intItemId
 				INNER JOIN tblSMCompanyLocation		L			ON L.intCompanyLocationId = CBL.intLocationId
-				INNER JOIN vyuCTEntity				EY			ON EY.intEntityId = CBL.intEntityId AND EY.strEntityType = (CASE WHEN CBL.intContractTypeId = 1 THEN 'Vendor' ELSE 'Customer' END)
 				INNER JOIN tblCTContractHeader		CH			ON CH.intContractHeaderId = CBL.intContractHeaderId
+				INNER JOIN vyuCTEntity				EY			ON EY.intEntityId = CBL.intEntityId AND EY.strEntityType = (CASE WHEN CBL.intContractTypeId = 1 THEN 'Vendor' ELSE 'Customer' END) AND ISNULL(CH.intEntitySelectedLocationId,EY.intDefaultLocationId) = EY.intDefaultLocationId				
 				INNER JOIN tblCTContractDetail		CD			ON CD.intContractDetailId = CBL.intContractDetailId
 				INNER JOIN tblICItemUOM				ItemUOM		ON ItemUOM.intItemUOMId = CD.intItemUOMId
 				INNER JOIN tblICUnitMeasure			IUM			ON IUM.intUnitMeasureId = ItemUOM.intUnitMeasureId
