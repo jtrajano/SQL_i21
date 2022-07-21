8237888aa0b788dad6cb0ea1381233267dc3140a
Jeffrey Trajano <jeffrey.trajano@irely.com>
2022-07-21 21:14:54 +0800

    GL-9306

--- i21Database/dbo/Tables/tblCMBankStatementImportLogDetail.sql
+++ i21Database/dbo/Tables/tblCMBankStatementImportLogDetail.sql
@@ -4,6 +4,7 @@ CREATE TABLE [dbo].[tblCMBankStatementImportLogDetail](
 	[strTransactionId] [nvarchar](40) COLLATE Latin1_General_CI_AS NULL,
 	[intBankStatementImportId] [int] NULL,
 	[strError] [nvarchar](500) COLLATE Latin1_General_CI_AS NULL,
+	[strComment] [nvarchar](500) COLLATE Latin1_General_CI_AS NULL,
 	[strBankStatementImportId] [nvarchar](20) COLLATE Latin1_General_CI_AS NULL,
 	[strCategory] [nvarchar](50) COLLATE Latin1_General_CI_AS NULL,
 	[intLineNo] INT NULL,
