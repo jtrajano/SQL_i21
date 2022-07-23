952b4e8f063efb5040b5f988f1952695d934e1af
Roudinee Panteria <roudinee.panteria@irely.com>
2022-07-23 15:14:19 +0800

    AR-15013

--- i21Database/dbo/Stored Procedures/uspSMGetItemTaxes.sql
+++ i21Database/dbo/Stored Procedures/uspSMGetItemTaxes.sql
@@ -46,10 +46,17 @@ BEGIN
 				SELECT @NewTaxGroupId = ISNULL([dbo].[fnGetTaxGroupIdForVendor](@EntityId, @TaxLocationId, @ItemId, @TaxLocationId, @FreightTermId, @FOB), 0)
 		END
 
-		IF @NewTaxGroupId <> 0
-			SET @TaxGroupId = @NewTaxGroupId
+		IF(ISNULL(@TaxLocationId, 0) <> 0)
+		BEGIN
+			IF @NewTaxGroupId <> 0
+				SET @TaxGroupId = @NewTaxGroupId
+		END
+		ELSE
+		BEGIN
+			SET @TaxGroupId = CASE WHEN @NewTaxGroupId = 0 THEN @OriginalTaxGroupId ELSE @NewTaxGroupId END
+		END
 
-		SET @IsOverrideTaxGroup = CASE WHEN @OriginalTaxGroupId <> @NewTaxGroupId THEN 1 ELSE 0 END
+		SET @IsOverrideTaxGroup = CASE WHEN @OriginalTaxGroupId <> @NewTaxGroupId AND ISNULL(@TaxLocationId, 0) <> 0 THEN 1 ELSE 0 END
 	END
 	ELSE
 	BEGIN
