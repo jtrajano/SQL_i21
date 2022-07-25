6d9a3597f5c09e62cfe13a8cd170f1d0f06f1c75
Jase Rueda <jase.rueda@iRely.com>
2022-07-25 13:56:18 +0800

    CS-24

---
+++ i21Database/dbo/Views/vyuSTCheckoutAggregateMeterReadingsByFuelGrade.sql
@@ -0,0 +1,18 @@
+﻿CREATE VIEW [dbo].[vyuSTCheckoutAggregateMeterReadingsByFuelGrade]
+AS
+SELECT		a.intFuelTotalSoldId,
+			a.intCheckoutId,
+			a.intProductNumber,
+			CASE 
+				WHEN a.intProductNumber = 1
+				THEN '87 Unl'
+				WHEN a.intProductNumber = 2
+				THEN '89 Mid'
+				WHEN a.intProductNumber = 3
+				THEN '91 Premium'
+				WHEN a.intProductNumber = 4
+				THEN 'Diesel'
+				END as 'strDescription',
+			a.dblDollarsSold,
+			a.dblGallonsSold
+FROM		tblSTCheckoutFuelTotalSold a
