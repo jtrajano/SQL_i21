6a722e01bf37094692d1c6f807d532799030d314
Arman Arenas <arman.arenas@irely.com>
2022-07-25 23:06:26 +0800

    LG-3290 handle blank output for fnLGGetSupplyPointContractData; added avail qty field on load detail

--- i21Database/dbo/Functions/fnLGGetSupplyPointContractData.sql
+++ i21Database/dbo/Functions/fnLGGetSupplyPointContractData.sql
@@ -4,29 +4,57 @@
 	,@dtmEffectiveDate DATETIME = NULL
 	,@intContractDetailId INT = NULL
 )
-RETURNS TABLE
+RETURNS @returntable TABLE (
+	intEntityVendorId INT
+	,intVendorLocationId INT
+	,intContractHeaderId INT NULL
+	,intContractDetailId INT NULL
+	,strContractNumber NVARCHAR(50) COLLATE Latin1_General_CI_AS
+	,intContractSeq INT NULL
+	,dblBalance NUMERIC(18, 6) NULL
+	,dblScheduleQty NUMERIC(18, 6) NULL
+	,dblAvailableQty NUMERIC(18, 6) NULL
+	,dblAppliedQty NUMERIC(18, 6) NULL
+	,dtmStartDate DATETIME NULL
+	,dtmEndDate DATETIME NULL)
 AS
-RETURN 
+BEGIN
+	INSERT INTO @returntable 
+	SELECT TOP 1
+		intEntityVendorId = CH.intEntityId
+		,CD.intVendorLocationId
+		,CH.intContractHeaderId
+		,CD.intContractDetailId
+		,CH.strContractNumber
+		,CD.intContractSeq
+		,CD.dblBalance
+		,dblScheduleQty = ISNULL(CD.dblScheduleQty, 0)
+		,dblAvailableQty = CD.dblBalance - ISNULL(CD.dblScheduleQty, 0)
+		,dblAppliedQty = CASE WHEN CH.ysnLoad = 1 THEN ISNULL(CD.intNoOfLoad, 0) - ISNULL(CD.dblBalanceLoad, 0) ELSE ISNULL(CD.dblQuantity, 0) - ISNULL(CD.dblBalance, 0) END
+		,CD.dtmStartDate 
+		,CD.dtmEndDate
+	FROM tblCTContractDetail CD
+	INNER JOIN tblCTContractHeader CH ON CH.intContractHeaderId = CD.intContractHeaderId 
+	WHERE CH.ysnSupplyPointContract = 1 
+		AND ((@intContractDetailId IS NOT NULL AND @intContractDetailId = CD.intContractDetailId)
+			OR (@intContractDetailId IS NULL AND (@dtmEffectiveDate IS NULL OR @dtmEffectiveDate BETWEEN CD.dtmStartDate AND CD.dtmEndDate)))
 
-SELECT TOP 1
-	intEntityVendorId = CH.intEntityId
-	,CD.intVendorLocationId
-	,CH.intContractHeaderId
-	,CD.intContractDetailId
-	,CH.strContractNumber
-	,CD.intContractSeq
-	,CD.dblBalance
-	,dblScheduleQty = ISNULL(CD.dblScheduleQty, 0)
-	,dblAvailableQty = CD.dblBalance - ISNULL(CD.dblScheduleQty, 0)
-	,dblAppliedQty = CASE WHEN CH.ysnLoad = 1 THEN ISNULL(CD.intNoOfLoad, 0) - ISNULL(CD.dblBalanceLoad, 0) ELSE ISNULL(CD.dblQuantity, 0) - ISNULL(CD.dblBalance, 0) END
-	,CD.dtmStartDate 
-	,CD.dtmEndDate
-FROM tblCTContractDetail CD
-INNER JOIN tblCTContractHeader CH ON CH.intContractHeaderId = CD.intContractHeaderId 
-WHERE CH.ysnSupplyPointContract = 1 
-	AND ((@intContractDetailId IS NOT NULL AND @intContractDetailId = CD.intContractDetailId)
-		OR (@intContractDetailId IS NULL AND (@dtmEffectiveDate IS NULL OR @dtmEffectiveDate BETWEEN CD.dtmStartDate AND CD.dtmEndDate)))
-
+	IF NOT EXISTS(SELECT 1 FROM @returntable) 
+		INSERT INTO @returntable 
+		SELECT
+			intEntityVendorId = @intEntityVendorId
+			,intVendorLocationId = @intEntityLocationId
+			,intContractHeaderId = NULL
+			,intContractDetailId = NULL
+			,strContractNumber = NULL
+			,intContractSeq = NULL
+			,dblBalance = 0
+			,dblScheduleQty = 0
+			,dblAvailableQty = 0
+			,dblAppliedQty = 0
+			,dtmStartDate = NULL
+			,dtmEndDate = NULL
+	
+	RETURN;
+	END
 GO
-
-
