f2edb479894dd529f65a72f9189de31b6802c220
Erick Dela Cruz <erick.delacruz@irely.com>
2022-07-26 22:45:32 +0800

    AP-10657

--- i21Database/dbo/Stored Procedures/uspAPPostBill.sql
+++ i21Database/dbo/Stored Procedures/uspAPPostBill.sql
@@ -1200,6 +1200,27 @@ BEGIN
 				GOTO Post_Rollback
 			END CATCH
 		END
+
+		--LOG TO tblAPClearing
+		BEGIN TRY
+			DECLARE @clearingIds AS Id
+			DECLARE @APClearing AS APClearing
+
+			INSERT INTO @clearingIds
+			SELECT intBillId FROM #tmpPostBillData
+
+			INSERT INTO @APClearing
+			SELECT * FROM fnAPClearing(@clearingIds)
+
+			EXEC uspAPClearing @APClearing = @APClearing, @post = @post
+		END TRY
+		BEGIN CATCH
+				DECLARE @errorClearing NVARCHAR(200) = ERROR_MESSAGE()
+				SET @invalidCount = @invalidCount + 1;
+				SET @totalRecords = @totalRecords - 1;
+				RAISERROR(@errorClearing, 16, 1);
+				GOTO Post_Rollback
+		END CATCH
 	END
 	ELSE
 	BEGIN
