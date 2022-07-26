b2792f29bf8f5c38da5e674471b46ac6420e9396
Renze Cuadra <renze.cuadra@iRely.com>
2022-07-26 18:36:54 +0800

    CS-52

---
+++ i21Database/dbo/Views/vyuSTCheckoutDeposit.sql
@@ -0,0 +1,6 @@
+﻿CREATE VIEW [dbo].[vyuSTCheckoutDeposit]
+AS
+SELECT cd.*, bt.intTransactionId, bt.strTransactionId, bt.dtmDate, bt.dblAmount
+	FROM tblSTCheckoutDeposits cd
+	LEFT JOIN tblCMBankTransaction bt
+		ON cd.intBDepId = bt.intTransactionId
