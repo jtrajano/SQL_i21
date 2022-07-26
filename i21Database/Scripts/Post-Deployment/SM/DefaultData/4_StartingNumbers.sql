6a6435bbaa3707c9c6fb5b53dd4d1b57f06de674
IRELY\JMendones <jesmar.mendones@irely.com>
2022-07-26 16:18:27 +0800

    SM-5615

--- i21Database/Scripts/Post-Deployment/SM/DefaultData/4_StartingNumbers.sql
+++ i21Database/Scripts/Post-Deployment/SM/DefaultData/4_StartingNumbers.sql
@@ -1682,6 +1682,15 @@ GO
 				,[ysnEnable]			= 1
 				,[intConcurrencyId]		= 1
 	WHERE NOT EXISTS (SELECT TOP 1 1 FROM tblSMStartingNumber WHERE strTransactionType = N'Recost Formulation' AND [strModule] = 'Inventory')
+	UNION ALL
+	SELECT	[intStartingNumberId]	= 177
+				,[strTransactionType]	= N'Adjust Settlements'
+				,[strPrefix]			= N'ASTR-'
+				,[intNumber]			= 1
+				,[strModule]			= 'Ticket Management'
+				,[ysnEnable]			= 1
+				,[intConcurrencyId]		= 1
+	WHERE NOT EXISTS (SELECT TOP 1 1 FROM tblSMStartingNumber WHERE strTransactionType = N'Recost Formulation' AND [strModule] = 'Inventory')
 
 
 	--Make sure to check with 19.1 and lower version. 142 is the last number
