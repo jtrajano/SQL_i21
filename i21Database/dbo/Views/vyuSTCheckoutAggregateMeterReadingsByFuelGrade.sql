683b834d57e677172008e0475101ba5c2cc32ef8
Jase Rueda <jase.rueda@iRely.com>
2022-07-26 16:15:51 +0800

    CS-53

--- i21Database/dbo/Views/vyuSTCheckoutAggregateMeterReadingsByFuelGrade.sql
+++ i21Database/dbo/Views/vyuSTCheckoutAggregateMeterReadingsByFuelGrade.sql
@@ -1,18 +1,20 @@
 ﻿CREATE VIEW [dbo].[vyuSTCheckoutAggregateMeterReadingsByFuelGrade]
 AS
-SELECT		a.intFuelTotalSoldId,
+SELECT		a.intPumpTotalsId as intFuelTotalSoldId,
 			a.intCheckoutId,
-			a.intProductNumber,
-			CASE 
-				WHEN a.intProductNumber = 1
-				THEN '87 Unl'
-				WHEN a.intProductNumber = 2
-				THEN '89 Mid'
-				WHEN a.intProductNumber = 3
-				THEN '91 Premium'
-				WHEN a.intProductNumber = 4
-				THEN 'Diesel'
-				END as 'strDescription',
-			a.dblDollarsSold,
-			a.dblGallonsSold
-FROM		tblSTCheckoutFuelTotalSold a
+			IL.strPassportFuelId1 as intProductNumber,
+			a.strDescription,
+			a.dblAmount as dblDollarsSold,
+			a.dblQuantity as dblGallonsSold
+FROM		tblSTCheckoutPumpTotals  a
+INNER JOIN	tblSTCheckoutHeader CH
+ON			a.intCheckoutId = CH.intCheckoutId
+INNER JOIN tblSTStore ST
+ON			CH.intStoreId = ST.intStoreId
+INNER JOIN tblICItemUOM UOM
+ON			a.intPumpCardCouponId = UOM.intItemUOMId
+INNER JOIN tblICItem Item
+ON			UOM.intItemId = Item.intItemId
+INNER JOIN dbo.tblICItemLocation IL 
+ON			Item.intItemId = IL.intItemId AND 
+			ST.intCompanyLocationId = IL.intLocationId
