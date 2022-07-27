279aa08acccaf6e8ee89959a55187d7df8a886f0
Cris <cris.lazaga@irely.com>
2022-07-27 09:47:41 +0800

    TM-3414
    (cherry picked from commit 5abc772fce9b758d59af7e1f86986ecc97b99286)
    (cherry picked from commit f2402f337d875e361c6b8a453f2c8303b5424522)
    (cherry picked from commit b6500f10d74cfff1fca05629060441ca79777f04)
    (cherry picked from commit b691d0cb2668c3ba745c48062caa1b1886fe7d24)
    (cherry picked from commit fb436b01d8d8e92de42c6039402ef4952c24e0a5)

--- Integration/dbo/Stored Procedures/uspTMRecreateGeneratedCallEntryView.sql
+++ Integration/dbo/Stored Procedures/uspTMRecreateGeneratedCallEntryView.sql
@@ -66,7 +66,7 @@ BEGIN
 				,strSiteCountry = B.strCountry
 				,dblLongitude = B.dblLongitude
 				,dblLatitude = B.dblLatitude
-				,intCustomerID = B.intCustomerID
+				,intCustomerId = B.intCustomerID
 				,ysnLeakCheckRequired = A.ysnLeakCheckRequired
 				,dblCustomerBalance = ISNULL(D.vwcus_high_past_due, 0.0) -- TM-2695
 				,dblSiteEstimatedPercentLeft = B.dblEstimatedPercentLeft
@@ -138,7 +138,7 @@ BEGIN
 				,strSiteCountry = B.strCountry
 				,dblLongitude = B.dblLongitude
 				,dblLatitude = B.dblLatitude
-				,intCustomerID = B.intCustomerID
+				,intCustomerId = B.intCustomerID
 				,ysnLeakCheckRequired = A.ysnLeakCheckRequired
 				,dblCustomerBalance = ISNULL(K.dbl30Days,0.0) + ISNULL(K.dbl60Days,0.0) + ISNULL(K.dbl90Days,0.0) + ISNULL(K.dbl91Days,0.0) --TM-2695
 				,dblSiteEstimatedPercentLeft = B.dblEstimatedPercentLeft
