22dc2d56fe34d2000657cf48e24468d14ead8600
DESKTOP-0Q1GJ6F\iRelyNUC14 <kelvin.quinto@irely.com>
2022-07-27 14:47:59 +0800

    AR-15018

--- i21Database/dbo/Stored Procedures/uspARCreateCustomerInvoice.sql
+++ i21Database/dbo/Stored Procedures/uspARCreateCustomerInvoice.sql
@@ -184,6 +184,7 @@ DECLARE @ZeroDecimal						NUMERIC(18, 6)
 		,@InitTranCount						INT
 		,@Savepoint							NVARCHAR(32)
 		,@ImpactForProvisional				BIT = 0
+		,@intARTermId						INT = NULL
 
 SET @InitTranCount = @@TRANCOUNT
 SET @Savepoint = SUBSTRING(('ARCreateCustomerInvoice' + CONVERT(VARCHAR, @InitTranCount)), 1, 32)
@@ -424,8 +425,14 @@ BEGIN
 		SAVE TRANSACTION @Savepoint
 END
 
-SELECT TOP 1 @ImpactForProvisional = CASE WHEN @Type = 'Provisional' THEN ISNULL([ysnImpactForProvisional], 0) ELSE 0 END
+SELECT TOP 1 @ImpactForProvisional 	= CASE WHEN @Type = 'Provisional' THEN ISNULL([ysnImpactForProvisional], 0) ELSE 0 END	       
 FROM tblARCompanyPreference
+ORDER BY intCompanyPreferenceId DESC
+
+SELECT TOP 1 @intARTermId = CASE WHEN strPullPoint = 'Company Location' THEN intTermId ELSE NULL END
+FROM tblSMTermPullPoint P 
+WHERE P.strPullPoint = 'Company Location'
+ORDER BY P.intTermPullPointId ASC
 
 DECLARE @BorrowingFacilityLimitDetailId INT = NULL
 
@@ -555,11 +562,11 @@ BEGIN TRY
 		,[intCompanyLocationId]				= @CompanyLocationId
 		,[intAccountId]						= @ARAccountId
 		,[intCurrencyId]					= @DefaultCurrency
-		,[intTermId]						= ISNULL(ISNULL(@TermId, C.[intTermsId]), EL.[intTermsId])
+		,[intTermId]						= ISNULL(ISNULL(ISNULL(@TermId, EL.[intTermsId]), @intARTermId), C.[intTermsId])
 		,[intSourceId]						= @SourceId
 		,[intPeriodsToAccrue]				= ISNULL(@PeriodsToAccrue, 1)
 		,[dtmDate]							= CASE WHEN @Type = 'Transport Delivery' THEN ISNULL(@InvoiceDate, @DateOnly) ELSE ISNULL(CAST(@InvoiceDate AS DATE), @DateOnly) END
-		,[dtmDueDate]						= ISNULL(@DueDate, (CAST(dbo.fnGetDueDateBasedOnTerm(ISNULL(CAST(@InvoiceDate AS DATE),@DateOnly), ISNULL(ISNULL(@TermId, C.[intTermsId]),0)) AS DATE)))
+		,[dtmDueDate]						= ISNULL(@DueDate, (CAST(dbo.fnGetDueDateBasedOnTerm(ISNULL(CAST(@InvoiceDate AS DATE),@DateOnly), ISNULL(ISNULL(ISNULL(@TermId, EL.[intTermsId]), @intARTermId), C.[intTermsId])) AS DATE)))
 		,[dtmShipDate]						= ISNULL(@ShipDate, ISNULL(CAST(@PostDate AS DATE),@DateOnly))
 		,[dtmCalculated]					= CAST(@CalculatedDate AS DATE)
 		,[dtmPostDate]						= ISNULL(CAST(@PostDate AS DATE),ISNULL(CAST(@InvoiceDate AS DATE),@DateOnly))
@@ -651,40 +658,12 @@ BEGIN TRY
 		,[strSourcedFrom]					= @SourcedFrom
 		,[intTaxLocationId]					= @TaxLocationId
 		,[strTaxPoint]						= @TaxPoint
-	FROM	
-		tblARCustomer C
-	LEFT OUTER JOIN
-					(	SELECT 
-							 [intEntityLocationId]
-							,[strLocationName]
-							,[strAddress]
-							,[intEntityId] 
-							,[strCountry]
-							,[strState]
-							,[strCity]
-							,[strZipCode]
-							,[intTermsId]
-							,[intShipViaId]
-						FROM 
-							[tblEMEntityLocation]
-						WHERE
-							ysnDefaultLocation = 1
-					) EL
-						ON C.[intEntityId] = EL.[intEntityId]
-	LEFT OUTER JOIN
-		[tblEMEntityLocation] SL
-			ON ISNULL(@ShipToLocationId, 0) <> 0
-			AND @ShipToLocationId = SL.intEntityLocationId
-	LEFT OUTER JOIN
-		[tblEMEntityLocation] SL1
-			ON C.intShipToId = SL1.intEntityLocationId
-	LEFT OUTER JOIN
-		[tblEMEntityLocation] BL
-			ON ISNULL(@BillToLocationId, 0) <> 0
-			AND @BillToLocationId = BL.intEntityLocationId		
-	LEFT OUTER JOIN
-		[tblEMEntityLocation] BL1
-			ON C.intBillToId = BL1.intEntityLocationId	
+	FROM tblARCustomer C
+	LEFT OUTER JOIN [tblEMEntityLocation] EL ON C.[intEntityId] = EL.[intEntityId] AND EL.ysnDefaultLocation = 1
+	LEFT OUTER JOIN [tblEMEntityLocation] SL ON ISNULL(@ShipToLocationId, 0) <> 0 AND @ShipToLocationId = SL.intEntityLocationId
+	LEFT OUTER JOIN [tblEMEntityLocation] SL1 ON C.intShipToId = SL1.intEntityLocationId
+	LEFT OUTER JOIN [tblEMEntityLocation] BL ON ISNULL(@BillToLocationId, 0) <> 0 AND @BillToLocationId = BL.intEntityLocationId		
+	LEFT OUTER JOIN [tblEMEntityLocation] BL1 ON C.intBillToId = BL1.intEntityLocationId	
 	WHERE C.[intEntityId] = @EntityCustomerId
 	
 	SET @NewId = SCOPE_IDENTITY()
