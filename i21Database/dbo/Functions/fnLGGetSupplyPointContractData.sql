24b205d7ee3b8dc63f9778b9e47fada3fc8c5f8a
Arman Arenas <arman.arenas@irely.com>
2022-07-28 20:31:57 +0800

    LG-3290 add itemId parameter to fnLGGetSupplyPointContractData

--- i21Database/dbo/Functions/fnLGGetSupplyPointContractData.sql
+++ i21Database/dbo/Functions/fnLGGetSupplyPointContractData.sql
@@ -1,12 +1,14 @@
 ﻿CREATE FUNCTION [dbo].[fnLGGetSupplyPointContractData](
 	@intEntityVendorId INT
 	,@intEntityLocationId INT
+	,@intItemId INT
 	,@dtmEffectiveDate DATETIME = NULL
 	,@intContractDetailId INT = NULL
 )
 RETURNS @returntable TABLE (
 	intEntityVendorId INT
 	,intVendorLocationId INT
+	,intCategoryId INT NULL
 	,intContractHeaderId INT NULL
 	,intContractDetailId INT NULL
 	,strContractNumber NVARCHAR(50) COLLATE Latin1_General_CI_AS
@@ -19,10 +21,15 @@ RETURNS @returntable TABLE (
 	,dtmEndDate DATETIME NULL)
 AS
 BEGIN
+
+	DECLARE @intCategoryId INT
+	SELECT @intCategoryId = intCategoryId FROM tblICItem WHERE intItemId = @intItemId
+
 	INSERT INTO @returntable 
 	SELECT TOP 1
 		intEntityVendorId = CH.intEntityId
 		,CD.intVendorLocationId
+		,CD.intCategoryId
 		,CH.intContractHeaderId
 		,CD.intContractDetailId
 		,CH.strContractNumber
@@ -36,14 +43,18 @@ BEGIN
 	FROM tblCTContractDetail CD
 	INNER JOIN tblCTContractHeader CH ON CH.intContractHeaderId = CD.intContractHeaderId 
 	WHERE CH.ysnSupplyPointContract = 1 
+		AND CD.intCategoryId = @intCategoryId
 		AND ((@intContractDetailId IS NOT NULL AND @intContractDetailId = CD.intContractDetailId)
 			OR (@intContractDetailId IS NULL AND (@dtmEffectiveDate IS NULL OR @dtmEffectiveDate BETWEEN CD.dtmStartDate AND CD.dtmEndDate)))
+		AND ((@intCategoryId IS NOT NULL AND @intCategoryId = CD.intCategoryId))
+	ORDER BY CD.dtmStartDate DESC
 
 	IF NOT EXISTS(SELECT 1 FROM @returntable) 
 		INSERT INTO @returntable 
 		SELECT
 			intEntityVendorId = @intEntityVendorId
 			,intVendorLocationId = @intEntityLocationId
+			,intCategoryId = @intCategoryId
 			,intContractHeaderId = NULL
 			,intContractDetailId = NULL
 			,strContractNumber = NULL
