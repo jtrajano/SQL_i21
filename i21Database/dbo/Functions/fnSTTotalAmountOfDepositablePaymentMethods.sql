2bd106b0298e355decef5c33c250a4478a68b17b
Jase Rueda <jase.rueda@iRely.com>
2022-07-28 18:37:38 +0800

    CS-22

---
+++ i21Database/dbo/Functions/fnSTTotalAmountOfDepositablePaymentMethods.sql
@@ -0,0 +1,18 @@
+﻿CREATE FUNCTION [dbo].[fnSTTotalAmountOfDepositablePaymentMethods] 
+(
+	@intCheckoutId AS INT
+)
+RETURNS DECIMAL(18,2)
+AS BEGIN
+
+    DECLARE @dblTotalAmountOfDepositablePaymentMethods DECIMAL(18,2) = 0
+
+    SELECT          @dblTotalAmountOfDepositablePaymentMethods = ISNULL(SUM(a.dblAmount),0)
+    FROM            tblSTCheckoutPaymentOptions a
+    INNER JOIN      tblSTPaymentOption b
+    ON              a.intPaymentOptionId = b.intPaymentOptionId
+    WHERE           b.ysnDepositable = 1 AND
+                    a.intCheckoutId = @intCheckoutId
+
+    RETURN @dblTotalAmountOfDepositablePaymentMethods
+END
