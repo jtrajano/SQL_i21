161ec3f63d305dc1551c0f44e4d4a7f31d9e1811
Edsel Roldan Magura <edsel.magura@iRely.com>
2022-07-29 11:59:36 +0800

    HD-3894

---
+++ i21Database/dbo/Views/vyuHDCustomerCompany.sql
@@ -0,0 +1,11 @@
+﻿CREATE VIEW [dbo].[vyuHDCustomerCompany]
+AS
+	SELECT  intCustomerProductVersionId	 = CONVERT(INT,ROW_NUMBER() OVER ( ORDER BY CustomerProductVersion.strCompany ))
+		   ,strCompany					 = CustomerProductVersion.strCompany
+		   ,intCustomerId				 = CustomerProductVersion.intCustomerId
+		   ,strName						 = CustomerProductVersion.strName
+	FROM tblARCustomerProductVersion CustomerProductVersion
+	GROUP BY CustomerProductVersion.intCustomerId
+			,CustomerProductVersion.strCompany
+			,CustomerProductVersion.strName
+GO
