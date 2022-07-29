d2c4137023e41bbd12d1550089095bff7d5ca02e
Allen Marasigan <54519457+jaumarasigan@users.noreply.github.com>
2022-07-29 16:06:07 +0800

    LG-3388 Added Allocation screen in tblSMScreen on PostDeploy

--- i21Database/Scripts/Post-Deployment/SM/DefaultData/10_Screen.sql
+++ i21Database/Scripts/Post-Deployment/SM/DefaultData/10_Screen.sql
@@ -1308,4 +1308,10 @@ GO
     --     SET  ysnApproval = 1
     --     WHERE strNamespace = 'Inventory.view.InventoryReceipt.TransferOrders'
 	-- END
+
+	IF NOT EXISTS (SELECT TOP 1 1 FROM tblSMScreen WHERE strNamespace = 'Logistics.view.Allocation') 
+	BEGIN
+		INSERT [dbo].[tblSMScreen] ([strScreenId], [strScreenName], [strNamespace], [strModule], [strTableName], [intConcurrencyId], [strGroupName]) 
+		VALUES (N'', N'Allocation', N'Logistics.view.Allocation', N'Logistics', NULL, 1, N'Logistics')
+	END
 GO
