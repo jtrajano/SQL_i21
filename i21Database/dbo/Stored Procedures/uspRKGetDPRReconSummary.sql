4fbe883ffe8d65ad8721bcb582e15759c9f1017e
Smith Wilson G. de Jesus <smith.dejesus@irely.com>
2022-07-29 14:14:04 +0800

    RM-4863

--- i21Database/dbo/Stored Procedures/uspRKGetDPRReconSummary.sql
+++ i21Database/dbo/Stored Procedures/uspRKGetDPRReconSummary.sql
@@ -14,9 +14,17 @@ BEGIN TRY
 
 DECLARE @ErrMsg NVARCHAR(MAX) 
 
-DECLARE @strCommodityCode NVARCHAR(100)
+DECLARE @strCommodityCode NVARCHAR(100),
+		@dtmFromDate DATETIME,
+		@dtmToDate DATETIME
 
 SELECT @strCommodityCode = strCommodityCode  FROM tblICCommodity where intCommodityId = @intCommodityId
+
+SELECT 
+	@dtmFromDate = DATEADD(mi, DATEDIFF(mi, GETUTCDATE(), GETDATE()), dtmFromDate) 
+	,@dtmToDate = DATEADD(mi, DATEDIFF(mi, GETUTCDATE(), GETDATE()), dtmToDate) 
+FROM tblRKDPRReconHeader WHERe intDPRReconHeaderId = @intDPRReconHeaderId
+
 
 select * into #tmpDPRReconSummary from (
 
@@ -26,6 +34,7 @@ select * into #tmpDPRReconSummary from (
 		,strCommodityCode
 		,intSort
 		,strGroup = '+ Priced Purchase Contract'
+		,intGroupSort = 1
 	from tblRKDPRReconContracts
 	where strContractType = 'Purchase'
 	and intDPRReconHeaderId  = @intDPRReconHeaderId
@@ -38,6 +47,7 @@ select * into #tmpDPRReconSummary from (
 		,strCommodityCode
 		,intSort
 		,strGroup = '- Priced Sales Contract'
+		,intGroupSort = 2
 	from tblRKDPRReconContracts
 	where strContractType = 'Sales'
 	and intDPRReconHeaderId  = @intDPRReconHeaderId
@@ -51,6 +61,7 @@ select * into #tmpDPRReconSummary from (
 		,strCommodityCode
 		,intSort
 		,strGroup = '+ Futures'
+		,intGroupSort = 3
 	from tblRKDPRReconDerivatives
 	where intDPRReconHeaderId  = @intDPRReconHeaderId
 	group by strBucketName, strCommodityCode,intSort,intDPRReconHeaderId
@@ -61,10 +72,15 @@ select * into #tmpDPRReconSummary from (
 select 
 	intSort
 	,strGroup
-	,strBucketName
+	,intGroupSort
+	,strBucketName = strBucketName +  CASE WHEN sum(dblTotal) is null THEN ' w/o drill down' Else '' END
+	,strOrigBucketName = strBucketName
 	,dblTotal = sum(dblTotal) 
 	,strCommodityCode
 	,intDPRReconHeaderId = @intDPRReconHeaderId
+	,ysnIsTotalNull = CAST( CASE WHEN sum(dblTotal) is null THEN 1 Else 0 END AS BIT) 
+	,strFromDate = REPLACE(REPLACE(REPLACE(CONVERT(VARCHAR(10),@dtmFromDate,101) + ' ' + RIGHT(CONVERT(VARCHAR, @dtmFromDate, 100), 7),'  ',' '),'AM',' AM'),'PM',' PM')
+	,strToDate = REPLACE(REPLACE(REPLACE(CONVERT(VARCHAR(10),@dtmToDate,101) + ' ' + RIGHT(CONVERT(VARCHAR, @dtmToDate, 100), 7),'  ',' '),'AM',' AM'),'PM',' PM')
 from (
 	--with data
 	select * from #tmpDPRReconSummary
@@ -76,10 +92,11 @@ from (
 	union all
 	select 
 		strBucketName = '+ New Priced Purchase Contract'
-		,dblTotal = NULL
+		,dblTotal = null
 		,strCommodityCode = @strCommodityCode
 		,intSort = 1
 		,strGroup = '+ Priced Purchase Contract'
+		,intGroupSort = 1
 
 	union all
 	select 
@@ -88,6 +105,7 @@ from (
 		,strCommodityCode = @strCommodityCode
 		,intSort = 2
 		,strGroup = '+ Priced Purchase Contract'
+		,intGroupSort = 1
 
 	union all
 	select 
@@ -96,46 +114,52 @@ from (
 		,strCommodityCode = @strCommodityCode
 		,intSort = 3
 		,strGroup = '+ Priced Purchase Contract'
+		,intGroupSort = 1
 
-		union all
+	union all
 	select 
 		strBucketName = '+ Purchase Basis Pricing'
 		,dblTotal = NULL
 		,strCommodityCode = @strCommodityCode
 		,intSort = 4
 		,strGroup = '+ Priced Purchase Contract'
+		,intGroupSort = 1
 
-		union all
+	union all
 	select 
 		strBucketName = '+ Purchase Qty Adjustment'
 		,dblTotal = NULL
 		,strCommodityCode = @strCommodityCode
 		,intSort = 5
 		,strGroup = '+ Priced Purchase Contract'
+		,intGroupSort = 1
 
-		union all
+	union all
 	select 
 		strBucketName = '+/- Purchase Load Variance'
 		,dblTotal = NULL
 		,strCommodityCode = @strCommodityCode
 		,intSort = 6
 		,strGroup = '+ Priced Purchase Contract'
+		,intGroupSort = 1
 
-		union all
+	union all
 	select 
 		strBucketName = '- Purchase Short Closed'
 		,dblTotal = NULL
 		,strCommodityCode = @strCommodityCode
 		,intSort =7
 		,strGroup = '+ Priced Purchase Contract'
+		,intGroupSort = 1
 
-		union all
+	union all
 	select 
 		strBucketName = '- Purchase Cancelled'
 		,dblTotal = NULL
 		,strCommodityCode = @strCommodityCode
 		,intSort = 8
 		,strGroup = '+ Priced Purchase Contract'
+		,intGroupSort = 1
 
 	--=========================================================
 	--				SALES
@@ -147,6 +171,7 @@ from (
 		,strCommodityCode = @strCommodityCode
 		,intSort = 11
 		,strGroup = '- Priced Sales Contract'
+		,intGroupSort = 2
 
 	union all
 	select 
@@ -155,6 +180,7 @@ from (
 		,strCommodityCode = @strCommodityCode
 		,intSort = 12
 		,strGroup = '- Priced Sales Contract'
+		,intGroupSort = 2
 
 	union all
 	select 
@@ -163,46 +189,52 @@ from (
 		,strCommodityCode = @strCommodityCode
 		,intSort = 13
 		,strGroup = '- Priced Sales Contract'
+		,intGroupSort = 2
 
-		union all
+	union all
 	select 
 		strBucketName = '+ Sales Basis Pricing'
 		,dblTotal = NULL
 		,strCommodityCode = @strCommodityCode
 		,intSort = 14
 		,strGroup = '- Priced Sales Contract'
+		,intGroupSort = 2
 
-		union all
+	union all
 	select 
 		strBucketName = '+ Sales Qty Adjustment'
 		,dblTotal = NULL
 		,strCommodityCode = @strCommodityCode
 		,intSort = 15
 		,strGroup = '- Priced Sales Contract'
+		,intGroupSort = 2
 
-		union all
+	union all
 	select 
 		strBucketName = '+/- Sales Load Variance'
 		,dblTotal = NULL
 		,strCommodityCode = @strCommodityCode
 		,intSort = 16
 		,strGroup = '- Priced Sales Contract'
+		,intGroupSort = 2
 
-		union all
+	union all
 	select 
 		strBucketName = '- Sales Short Closed'
 		,dblTotal = NULL
 		,strCommodityCode = @strCommodityCode
 		,intSort =17
 		,strGroup = '- Priced Sales Contract'
+		,intGroupSort = 2
 
-		union all
+	union all
 	select 
 		strBucketName = '- Sales Cancelled'
 		,dblTotal = NULL
 		,strCommodityCode = @strCommodityCode
 		,intSort = 18
 		,strGroup = '- Priced Sales Contract'
+		,intGroupSort = 2
 
 	--=========================================================
 	--				FUTURES
@@ -214,6 +246,7 @@ from (
 		,strCommodityCode = @strCommodityCode
 		,intSort = 21
 		,strGroup = '+ Futures'
+		,intGroupSort = 3
 
 	union all
 	select 
@@ -222,6 +255,7 @@ from (
 		,strCommodityCode = @strCommodityCode
 		,intSort = 22
 		,strGroup = '+ Futures'
+		,intGroupSort = 3
 
 ) t
 
@@ -230,6 +264,7 @@ group by
 	,strCommodityCode
 	,intSort
 	,strGroup
+	,intGroupSort
 order by intSort
 	
 
---
+++ i21Database/dbo/Stored Procedures/uspRKRptDPRReconSummary.sql
@@ -0,0 +1,51 @@
+﻿CREATE PROCEDURE [dbo].[uspRKRptDPRReconSummary] 
+	@xmlParam NVARCHAR(MAX) = NULL
+
+as
+BEGIN
+
+	DECLARE @idoc INT
+		,@intCommodityId int
+		,@intDPRReconHeaderId int
+	IF LTRIM(RTRIM(@xmlParam)) = ''
+		SET @xmlParam = NULL
+
+	DECLARE @temp_xml_table TABLE (
+		fieldname NVARCHAR(50)
+		,condition NVARCHAR(20)
+		,[from] NVARCHAR(50)
+		,[to] NVARCHAR(50)
+		,[join] NVARCHAR(10)
+		,[begingroup] NVARCHAR(50)
+		,[endgroup] NVARCHAR(50)
+		,[datatype] NVARCHAR(50)
+		)
+
+	EXEC sp_xml_preparedocument @idoc OUTPUT
+		,@xmlParam
+
+	INSERT INTO @temp_xml_table
+	SELECT *
+	FROM OPENXML(@idoc, 'xmlparam/filters/filter', 2) WITH (
+			 fieldname NVARCHAR(50)
+			,condition NVARCHAR(20)
+			,[from] NVARCHAR(50)
+			,[to] NVARCHAR(50)
+			,[join] NVARCHAR(10)
+			,[begingroup] NVARCHAR(50)
+			,[endgroup] NVARCHAR(50)
+			,[datatype] NVARCHAR(50)
+			)
+
+	SELECT @intDPRReconHeaderId = [from]
+	FROM @temp_xml_table	
+	WHERE [fieldname] = 'intDPRReconHeaderId'
+	
+	SELECT @intCommodityId = [from]
+	FROM @temp_xml_table	
+	WHERE [fieldname] = 'intCommodityId'
+	
+	
+	EXEC uspRKGetDPRReconSummary @intDPRReconHeaderId, @intCommodityId
+
+END
