59d05c1ed9b0a662a6eda74e6fae7d5d62cbd3a3
Roudinee Panteria <roudinee.panteria@irely.com>
2022-07-30 17:27:56 +0800

    AR-15056

---
+++ i21Database/dbo/Stored Procedures/uspSMGetOriginalTax.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE [dbo].[uspSMGetOriginalTax]
+	 @ItemId						INT			= NULL
+	,@LocationId					INT			= NULL
+	,@TransactionType				NVARCHAR(20)='Purchase'
+	,@EntityId						INT			= NULL
+	,@ShipLocationId				INT			= NULL
+	,@SiteId						INT			= NULL
+	,@FreightTermId					INT			= NULL
+	,@OriginalTaxGroupId			INT			= 0		OUTPUT
+AS
+
+BEGIN
+	IF (@TransactionType = 'Sale')
+	BEGIN
+		SELECT @OriginalTaxGroupId = ISNULL([dbo].[fnGetTaxGroupIdForCustomer](@EntityId, @LocationId, @ItemId, @BillShipToLocationId, @SiteId, @FreightTermId, NULL), 0)
+	END
+	ELSE
+	BEGIN
+		SELECT @OriginalTaxGroupId = ISNULL([dbo].[fnGetTaxGroupIdForVendor](@EntityId, @LocationId, @ItemId, @BillShipToLocationId, @FreightTermId, NULL), 0)
+	END
+
+	RETURN @OriginalTaxGroupId
+END
