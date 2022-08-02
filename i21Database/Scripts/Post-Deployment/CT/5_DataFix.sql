188531a807172eff13c1318d3a94369ee8aad80b
lawrence_zabala <lawrence.zabala@irely.com>
2022-08-02 09:24:27 +0800

    CT-7277

--- i21Database/Scripts/Post-Deployment/CT/5_DataFix.sql
+++ i21Database/Scripts/Post-Deployment/CT/5_DataFix.sql
@@ -187,7 +187,7 @@ GO
 		,tblCTContractCondition b
 		,tblCTCondition c
 	where
-		TRIM(ISNULL(b.strConditionDescription, '')) = ''
+		LTRIM(RTRIM(ISNULL(b.strConditionDescription, ''))) = ''
 		and b.intContractHeaderId = a.intContractHeaderId
 		and c.intConditionId = b.intConditionId
 
