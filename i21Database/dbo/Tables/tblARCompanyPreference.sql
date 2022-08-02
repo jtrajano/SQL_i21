c1cdd5abb16537361f9fbe18d970c58cfe56cafe
DESKTOP-0Q1GJ6F\iRelyNUC14 <kelvin.quinto@irely.com>
2022-08-02 12:38:37 +0800

    AR-14001

--- i21Database/dbo/Tables/tblARCompanyPreference.sql
+++ i21Database/dbo/Tables/tblARCompanyPreference.sql
@@ -80,6 +80,7 @@
 	[ysnOverrideLineOfBusinessSegment]		BIT NOT NULL CONSTRAINT [DF_tblARCompanyPreference_ysnOverrideLineOfBusinessSegment] DEFAULT((0)),
 	[intSurchargeRevenueAccount]			INT NULL, 
     [intSurchargeExpenseAccount]			INT NULL,
+	[ysnEnableCustomStatement]              BIT NOT NULL CONSTRAINT [DF_tblARCompanyPreference_ysnEnableCustomStatement] DEFAULT ((0)),
 	CONSTRAINT [FK_tblARCompanyPreference_tblGLAccount_intARAccountId] FOREIGN KEY ([intARAccountId]) REFERENCES [dbo].[tblGLAccount] ([intAccountId]),
 	CONSTRAINT [FK_tblARCompanyPreference_tblGLAccount_intDiscountAccountId] FOREIGN KEY ([intDiscountAccountId]) REFERENCES [dbo].[tblGLAccount] ([intAccountId]),
 	CONSTRAINT [FK_tblARCompanyPreference_tblGLAccount_intWriteOffAccountId] FOREIGN KEY ([intWriteOffAccountId]) REFERENCES [dbo].[tblGLAccount] ([intAccountId]),
