01f733330a165690c6ac4ee973bac451a4a15b5f
DESKTOP-0Q1GJ6F\iRelyNUC14 <kelvin.quinto@irely.com>
2022-08-02 14:10:17 +0800

    AR-14956

--- i21Database/dbo/Views/vyuARGetCustomer.sql
+++ i21Database/dbo/Views/vyuARGetCustomer.sql
@@ -178,6 +178,7 @@ SELECT intEntityId							= C.intEntityId
 	, intDefaultPayToBankAccountId		    = C.intDefaultPayToBankAccountId
 	, strDefaultPayToBankAccountNo		    = C.strDefaultPayToBankAccountNo
      , ysnLoadToLoad		                = C.ysnLoadToLoad
+  , strDeliveryDocument					= C.strDeliveryDocument
 FROM tblARCustomer C
 INNER JOIN tblEMEntity E ON C.intEntityId = E.intEntityId
 LEFT JOIN tblSMLanguage LANG ON E.intLanguageId = LANG.intLanguageId
