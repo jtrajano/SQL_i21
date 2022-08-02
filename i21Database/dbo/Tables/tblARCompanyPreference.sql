911c7a21fee37bc8843d36ce395d84f553140ae6
DESKTOP-0Q1GJ6F\iRelyNUC14 <kelvin.quinto@irely.com>
2022-08-02 12:38:37 +0800

    AR-14001

--- i21Database/dbo/Tables/tblARCompanyPreference.sql
+++ i21Database/dbo/Tables/tblARCompanyPreference.sql
@@ -45,6 +45,7 @@
 	[strSOCSMessage]						NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NOT NULL DEFAULT '',
 	[intAttachmentRetention]				INT NOT NULL DEFAULT 7,
 	[strCustomerAgingBy]					NVARCHAR(250) COLLATE Latin1_General_CI_AS NOT NULL DEFAULT 'Invoice Due Date', 
+	[ysnEnableCustomStatement]				BIT	NOT NULL CONSTRAINT [DF_tblARCompanyPreference_ysnEnableCustomStatement] DEFAULT ((0)),
 	CONSTRAINT [FK_tblARCompanyPreference_tblGLAccount_intARAccountId] FOREIGN KEY ([intARAccountId]) REFERENCES [dbo].[tblGLAccount] ([intAccountId]),
 	CONSTRAINT [FK_tblARCompanyPreference_tblGLAccount_intDiscountAccountId] FOREIGN KEY ([intDiscountAccountId]) REFERENCES [dbo].[tblGLAccount] ([intAccountId]),
 	CONSTRAINT [FK_tblARCompanyPreference_tblGLAccount_intWriteOffAccountId] FOREIGN KEY ([intWriteOffAccountId]) REFERENCES [dbo].[tblGLAccount] ([intAccountId]),
