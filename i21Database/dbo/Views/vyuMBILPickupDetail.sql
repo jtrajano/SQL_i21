86eda2d8a3b884b70daa763b1f69823d85796f73
Kenneth Sam Perez <kenneth.sam.perez@iRely.com>
2022-08-03 19:53:35 +0800

    MBIL-849

--- i21Database/dbo/Views/vyuMBILPickupDetail.sql
+++ i21Database/dbo/Views/vyuMBILPickupDetail.sql
@@ -25,6 +25,8 @@ SELECT  detail.intPickupDetailId
        ,detail.strLoadRefNo
 	   ,detail.intShiftId
        ,item.intCategoryId
+       ,detail.dblGross
+       ,detail.dblNet
 FROM tblMBILPickupDetail detail              
 INNER JOIN tblMBILLoadHeader load on detail.intLoadHeaderId = load.intLoadHeaderId        
 INNER JOIN tblICItem item on detail.intItemId = item.intItemId        
