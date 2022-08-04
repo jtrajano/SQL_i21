4bb21b76100193c16c20a839e0d23dc30b0b765a
Nelson Reyes <nelson.reyes@irely.com>
2022-08-04 10:24:51 +0800

    ST-2197

--- i21Database/dbo/Tables/tblSTCheckoutShiftPhysicalPreview.sql
+++ i21Database/dbo/Tables/tblSTCheckoutShiftPhysicalPreview.sql
@@ -8,10 +8,12 @@ CREATE TABLE [dbo].[tblSTCheckoutShiftPhysicalPreview]
     [intItemUOMId] INT NULL, 
     [dblSystemCount] NUMERIC(38, 20) NOT NULL DEFAULT ((0)), --Begin Qty
     [dblQtyReceived] NUMERIC(38, 20) NULL DEFAULT((0)),
+    [dblQtyTransferred] NUMERIC(38, 20) NULL DEFAULT((0)),
     [dblQtySold] NUMERIC(38, 20) NULL DEFAULT((0)),
     [dblPhysicalCount] NUMERIC(38, 20) NOT NULL DEFAULT ((0)),
     [intEntityUserSecurityId] INT NOT NULL,
     [dtmCheckoutDate] DATETIME NULL,
     [intLocationId] INT NOT NULL,
-    [intConcurrencyId] INT NULL DEFAULT ((0))
+    [intConcurrencyId] INT NULL DEFAULT ((0)),
+    CONSTRAINT [FK_tblSTCheckoutShiftPhysicalPreview_tblSTCheckoutHeader] FOREIGN KEY ([intCheckoutId]) REFERENCES [tblSTCheckoutHeader]([intCheckoutId]) ON DELETE CASCADE,
 )
