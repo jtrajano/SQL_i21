1f09479adb8ff95c25083d987028c42ed86975b5
Roudinee Panteria <roudinee.panteria@irely.com>
2022-08-06 17:04:40 +0800

    AR-15046

--- i21Database/dbo/Stored Procedures/uspARCreateInvoiceFromShipment.sql
+++ i21Database/dbo/Stored Procedures/uspARCreateInvoiceFromShipment.sql
@@ -27,7 +27,6 @@ DECLARE @ZeroDecimal					DECIMAL(18,6) = 0
 	  , @ysnHasPriceFixation			BIT = 0
 
 SELECT TOP 1 @strReferenceNumber = strSalesOrderNumber FROM tblSOSalesOrder ORDER BY intSalesOrderId DESC
---SET @dtmShipmentDate			 = ISNULL(CAST(@dtmShipmentDate AS DATE), @DateOnly)
 
 DECLARE
 	 @TransactionType			NVARCHAR(25)
@@ -229,6 +228,8 @@ INSERT INTO @UnsortedEntriesForInvoice
 	,[intSubCurrencyId] 
 	,[dblSubCurrencyRate] 
 	,[dblStandardWeight]
+	,[strTaxPoint]
+	,[intTaxLocationId]
 	)
 SELECT
 	 [strSourceTransaction]					= 'Inventory Shipment'
@@ -268,7 +269,7 @@ SELECT
 	,[ysnResetDetails]						= 0
 	,[ysnRecap]								= 0
 	,[ysnPost]								= 0
-																																																		
+
 	,[intInvoiceDetailId]					= NULL
 	,[intItemId]							= ARSI.[intItemId]
 	,[ysnInventory]							= 1
@@ -350,6 +351,8 @@ SELECT
 	,[intSubCurrencyId]						= ARSI.[intSubCurrencyId]
 	,[dblSubCurrencyRate]					= ARSI.[dblSubCurrencyRate]
 	,[dblStandardWeight]					= ARSI.dblStandardWeight
+	,[strTaxPoint]							= ARSI.strTaxPoint
+	,[intTaxLocationId]						= ARSI.intTaxLocationId
 FROM vyuARShippedItems ARSI
 LEFT JOIN(
  SELECT H.intPricingTypeId,D.intContractDetailId,D.dblQuantity  from tblCTContractHeader H
@@ -477,6 +480,8 @@ SELECT
 	,[intSubCurrencyId]						= SOD.[intSubCurrencyId]
 	,[dblSubCurrencyRate]					= SOD.[dblSubCurrencyRate]
 	,[dblStandardWeight]					= SOD.dblStandardWeight
+	,[strTaxPoint]							= NULL
+	,[intTaxLocationId]						= NULL
 FROM tblICInventoryShipment ICIS
 INNER JOIN tblSOSalesOrder SO ON SO.strSalesOrderNumber = @strReferenceNumber
 							 AND ICIS.intEntityCustomerId = SO.intEntityCustomerId 
@@ -603,6 +608,8 @@ SELECT
 	,[intSubCurrencyId]						= NULL
 	,[dblSubCurrencyRate]					= @ZeroDecimal
 	,[dblStandardWeight]					= @ZeroDecimal
+	,[strTaxPoint]							= NULL
+	,[intTaxLocationId]						= NULL
 FROM 
 	tblICInventoryShipment ICIS
 INNER JOIN
