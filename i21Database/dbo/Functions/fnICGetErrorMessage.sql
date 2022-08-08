241af3d1a91dcb35593411b1568ec066e37e0f1a
Feb Montefrio <feb.montefrio@irely.com>
2022-08-08 11:12:49 +0800

    [F] IC-10600
    # Conflicts:
    #	i21Database/dbo/Functions/fnICGetErrorMessage.sql
    #	i21Database/dbo/Stored Procedures/uspICPostDestinationInventoryShipment.sql

--- i21Database/dbo/Functions/fnICGetErrorMessage.sql
+++ i21Database/dbo/Functions/fnICGetErrorMessage.sql
@@ -281,8 +281,11 @@ BEGIN
 		WHEN @msgId = 80265 THEN 'Cost adjustment for %s is missing. Stock rebuild will abort.'
 		WHEN @msgId = 80266 THEN 'Invoice Control Total and Receipt Total does not match. Unable to post.'
 		WHEN @msgId = 80267 THEN 'The Receipt Qty for %s is %f. Total Lot Quantity is %f. The difference is %f.'
-		WHEN @msgId = 80268 THEN '%s is currently in a Voucher. Please remove it in %s first before you can unpost the Destination Weight/Grade.'
+		WHEN @msgId = 80268 THEN '%s is a missing lot it should not have a negative quantity.'
+		WHEN @msgId = 80269 THEN 'Multiple lots are not allowed for %s. See the company configuration &#8594; Receipts &#8594; Single or Multiple Lots option.'
+		WHEN @msgId = 80270 THEN 'Available Qty in %s is %f. Releasing %f is not allowed.'
+		WHEN @msgId = 80271 THEN '%s is currently in a Voucher. Please remove it in %s first before you can unpost the Destination Weight/Grade.'
 	END 
 
-	RETURN @msg
+	RETURN @msg COLLATE Latin1_General_CI_AS
 END 
