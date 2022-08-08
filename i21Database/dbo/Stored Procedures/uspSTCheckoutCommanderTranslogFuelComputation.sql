e4db6ee1debdeb02d7c6da357e0282905806b88e
Jase Rueda <jase.rueda@iRely.com>
2022-08-08 10:28:28 +0800

    CS-24

--- i21Database/dbo/Stored Procedures/uspSTCheckoutCommanderTranslogFuelComputation.sql
+++ i21Database/dbo/Stored Procedures/uspSTCheckoutCommanderTranslogFuelComputation.sql
@@ -4,13 +4,32 @@
 AS
 BEGIN
 
-INSERT INTO tblSTCheckoutFuelSalesByGradeAndPricePoint (intCheckoutId, intProductNumber, dblPrice, dblDollarsSold, dblGallonsSold, intConcurrencyId)
+DECLARE @intStoreId INT
+
+SELECT	@intStoreId = intStoreId
+FROM	dbo.tblSTCheckoutHeader 
+WHERE	intCheckoutId = @intCheckoutId
+
+INSERT INTO tblSTCheckoutFuelSalesByGradeAndPricePoint (intCheckoutId, intProductNumber, dblPrice, dblDollarsSold, dblGallonsSold, intItemUOMId, intConcurrencyId)
 SELECT	@intCheckoutId,
 		a.intProductNumber,
 		a.dblPrice,
 		a.dblDollarsSold,
 		a.dblGallonsSold,
+		UOM.intItemUOMId,
 		1
 FROM	@UDT_Translog a
+JOIN dbo.tblICItemLocation IL 
+	ON ISNULL(CAST(a.intProductNumber as NVARCHAR(10)), '') COLLATE Latin1_General_CI_AS IN (ISNULL(IL.strPassportFuelId1, ''), ISNULL(IL.strPassportFuelId2, ''), ISNULL(IL.strPassportFuelId3, ''))
+JOIN dbo.tblICItem I 
+	ON I.intItemId = IL.intItemId
+JOIN dbo.tblICItemUOM UOM 
+	ON UOM.intItemId = I.intItemId
+JOIN dbo.tblSMCompanyLocation CL 
+	ON CL.intCompanyLocationId = IL.intLocationId
+JOIN dbo.tblSTStore S 
+	ON S.intCompanyLocationId = CL.intCompanyLocationId
+WHERE S.intStoreId = @intStoreId
+
 
 END
