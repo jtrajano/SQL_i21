9f5dd42e883fc74696b340235016ac811da52358
Maverick Linget <maverick.linget@iRely.com>
2022-08-09 14:22:05 +0800

    RM-4884

--- i21Database/dbo/Stored Procedures/uspRKGetCreditLineCurrencyRate.sql
+++ i21Database/dbo/Stored Procedures/uspRKGetCreditLineCurrencyRate.sql
@@ -2,14 +2,30 @@
 	@intFromCurrencyId INT = NULL
 	, @intToCurrencyId INT = NULL
 AS
-BEGIN
+BEGIN	
+	DECLARE @tempResult AS TABLE (
+		dblRate NUMERIC(18,6)
+	)
 
-	SELECT TOP 1 dblRate
-	FROM tblSMCurrencyExchangeRate er
-	JOIN tblSMCurrencyExchangeRateDetail rd ON er.intCurrencyExchangeRateId = rd.intCurrencyExchangeRateId
-	WHERE intFromCurrencyId = @intFromCurrencyId
-	AND intToCurrencyId = @intToCurrencyId
-	AND dtmValidFromDate <= GETDATE()
-	ORDER BY dtmValidFromDate DESC
+	IF @intFromCurrencyId <> @intToCurrencyId
+	BEGIN
+		SELECT TOP 1 dblRate
+		FROM tblSMCurrencyExchangeRate er
+		JOIN tblSMCurrencyExchangeRateDetail rd ON er.intCurrencyExchangeRateId = rd.intCurrencyExchangeRateId
+		WHERE intFromCurrencyId = @intFromCurrencyId
+		AND intToCurrencyId = @intToCurrencyId
+		AND dtmValidFromDate <= GETDATE()
+		ORDER BY dtmValidFromDate DESC
+	END
+	ELSE 
+	BEGIN  
+		INSERT INTO @tempResult
+		SELECT 1
+
+		IF EXISTS (SELECT dblRate FROM @tempResult)
+		BEGIN
+			SELECT dblRate FROM @tempResult
+		END
+	END	
 
 END
