333a87402e5f36c682f242b96ac58c46c1c99ed8
Mark Daniel Marmeto <mark.marmeto@irely.com>
2022-08-09 11:15:32 +0800

    GL-8783

---
+++ i21Database/dbo/Tables/tblGLChangeAccountCategory.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[tblGLChangeAccountCategory]
+(
+	[intTransactionId]			INT IDENTITY(1, 1)	NOT NULL,
+	[strTransactionId]			NVARCHAR (20)		COLLATE Latin1_General_CI_AS NULL,
+	[intAccountId]				INT					NOT NULL,
+	[intAccountCategoryId]		INT					NOT NULL,
+	[intNewAccountCategoryId]	INT					NULL,
+	[ysnChanged]				BIT DEFAULT 0		NOT NULL,
+	[intEntityId]				INT					NULL,
+	[intConcurrencyId]			INT DEFAULT 1		NOT NULL,
+
+	CONSTRAINT [PK_tblGLChangeAccountCategory] PRIMARY KEY CLUSTERED ([intTransactionId] ASC),
+)
