86b7356819332006fcf6f34248c0caf2c9c2d300
Feb Montefrio <feb.montefrio@irely.com>
2022-08-10 17:51:48 +0800

    [B] IC-10602

--- i21Database/dbo/Stored Procedures/uspICPostInventoryReceipt.sql
+++ i21Database/dbo/Stored Procedures/uspICPostInventoryReceipt.sql
@@ -2760,6 +2760,7 @@ BEGIN
 
 	IF @ysnAllowBlankGLEntries = 0 OR EXISTS (SELECT TOP 1 1 FROM @GLEntries)
 	BEGIN 
+		UPDATE @GLEntries SET dtmDate = dbo.fnRemoveTimeOnDate(dtmDate) 
 		EXEC dbo.uspGLBookEntries @GLEntries, @ysnPost 
 	END 	
 
