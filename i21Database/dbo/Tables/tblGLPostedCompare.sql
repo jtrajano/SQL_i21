c3513d71bb8bd6450578fd9f23b0702162d6c623
Dom Caoile <dom.caoile.02@gmail.com>
2022-08-10 15:56:53 +0800

    FRD-1820

--- i21Database/dbo/Tables/tblGLPostedCompare.sql
+++ i21Database/dbo/Tables/tblGLPostedCompare.sql
@@ -13,7 +13,7 @@ CREATE TABLE [dbo].[tblGLPostedCompare]
 	[intCurrencyId]			INT             NOT NULL,
 	[intConcurrencyId]		INT             NOT NULL,
 	[intLedgerId]			INT             NOT NULL,
-	[dtmDateEntered]				DATETIME		NOT NULL,
+	[dtmDateEntered]		DATETIME		NOT NULL,
 	[strReport]				NVARCHAR (100)  COLLATE Latin1_General_CI_AS NOT NULL,
-	CONSTRAINT [PK_tblGLPostedCompare] PRIMARY KEY CLUSTERED ([intAccountId] ASC,[dtmDate] ASC,[intCurrencyId] ASC,[strCode] ASC,[intLedgerId] ASC),
+	CONSTRAINT [PK_tblGLPostedCompare] PRIMARY KEY CLUSTERED ([intAccountId] ASC,[dtmDate] ASC,[intCurrencyId] ASC,[strCode] ASC,[intLedgerId] ASC,[dtmDateEntered] ASC,[strReport] ASC),
 )
