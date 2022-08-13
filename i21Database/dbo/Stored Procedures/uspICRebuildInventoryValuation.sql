26fd14a994c8ac3ffb2bebed892953de85970d02
Feb Montefrio <feb.montefrio@irely.com>
2022-08-13 22:12:13 +0800

    [B] IC-10574

--- i21Database/dbo/Stored Procedures/uspICRebuildInventoryValuation.sql
+++ i21Database/dbo/Stored Procedures/uspICRebuildInventoryValuation.sql
@@ -1120,7 +1120,7 @@ BEGIN
 				,dblCost
 				,dblValue
 				,dblSalesPrice
-				,intCurrencyId
+				,t.intCurrencyId
 				,dblExchangeRate
 				,intTransactionId
 				,t.strTransactionId
@@ -1134,14 +1134,14 @@ BEGIN
 				,strRelatedTransactionId
 				,t.strTransactionForm
 				,intCostingMethod
-				,dtmCreated
+				,t.dtmCreated
 				,strDescription
-				,intCreatedUserId
+				,t.intCreatedUserId
 				,intCreatedEntityId
-				,intConcurrencyId  
+				,t.intConcurrencyId  
 				,intForexRateTypeId
 				,dblForexRate 
-				,strActualCostId
+				,t.strActualCostId
 				,intCategoryId
 				,dblUnitRetail
 				,dblCategoryCostValue
@@ -3591,7 +3591,7 @@ BEGIN
 								ON s.intFreightTermId = ft.intFreightTermId
 							LEFT JOIN tblICFobPoint fp
 								ON fp.strFobPoint = ft.strFobPoint
-					WHERE	s.strShipmentNumber = @strTransactionId  		
+					WHERE	s.strShipmentNumber = @strTransactionId 		
 
 					SELECT	@strAccountToCounterInventory = NULL 
 					WHERE	ISNULL(@intFobPointId, @FOB_ORIGIN) = @FOB_DESTINATION 
@@ -6779,5 +6779,4 @@ BEGIN
 		DROP TABLE #tmpLogRiskPosition		
 END 
 
-RETURN @intReturnValue; 
-
+RETURN @intReturnValue;
