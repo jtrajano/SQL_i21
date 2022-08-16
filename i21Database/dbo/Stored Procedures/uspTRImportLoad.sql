7d1a811b47ed194d91a93b95ee300ac4038eddc6
GRM-NUC\iRelyNUC23 <girome.andaya@iRely.com>
2022-08-16 20:51:48 +0800

    TR-2043
    (cherry picked from commit 42e9a7fafec7c3bb1d684ee06f67bc3c6c8c066c)
    (cherry picked from commit 81627e90a00975c1095a12dbc5f82469b7a8f022)

--- i21Database/dbo/Stored Procedures/uspTRImportLoad.sql
+++ i21Database/dbo/Stored Procedures/uspTRImportLoad.sql
@@ -160,9 +160,12 @@ BEGIN
 
 				IF(@strSource = 'API')
 				BEGIN
-					SELECT TOP 1 @intTruckId = E.intEntityId FROM tblSCTruckDriverReference D 
-					INNER JOIN tblEMEntity E ON E.intEntityId = D.intEntityId 
-					WHERE E.strName = @strTruck
+					--SELECT TOP 1 @intTruckId = E.intEntityId FROM tblSCTruckDriverReference D 
+					--INNER JOIN tblEMEntity E ON E.intEntityId = D.intEntityId 
+					--WHERE E.strName = @strTruck
+					SELECT TOP 1 @intTruckId = T.intEntityShipViaTruckId FROM tblSMShipViaTruck T 
+					INNER JOIN tblSMShipVia SV ON SV.intEntityId = T.intEntityShipViaId 
+					WHERE T.strTruckNumber = @strTruck
 				END
 				ELSE
 				BEGIN
