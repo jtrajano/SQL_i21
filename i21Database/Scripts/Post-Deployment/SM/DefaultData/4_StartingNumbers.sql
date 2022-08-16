8d01320ec77aa3b2dc5c8a98528863d9d5a66a35
Chris Salvante <chris.salvante@irely.com>
2022-08-16 09:16:18 +0800

    SM-5801
    (cherry picked from commit d08348bbd513e3a220ccb1faa856927f0beced99)

--- i21Database/Scripts/Post-Deployment/SM/DefaultData/4_StartingNumbers.sql
+++ i21Database/Scripts/Post-Deployment/SM/DefaultData/4_StartingNumbers.sql
@@ -1713,7 +1713,15 @@ GO
 				,[ysnEnable]			= 1
 				,[intConcurrencyId]		= 1
 	WHERE NOT EXISTS (SELECT TOP 1 1 FROM tblSMStartingNumber WHERE strTransactionType = N'Insurance Charge' AND [strModule] = 'Inventory')
-
+	UNION ALL
+	SELECT	[intStartingNumberId]	= 177
+			,[strTransactionType]	= N'Change Account Category'
+			,[strPrefix]			= N'CAC-'
+			,[intNumber]			= 1
+			,[strModule]			= 'General Ledger'
+			,[ysnEnable]			= 1
+			,[intConcurrencyId]		= 1
+	WHERE NOT EXISTS (SELECT TOP 1 1 FROM tblSMStartingNumber WHERE strTransactionType = N'Change Account Category' AND [strModule] = 'General Ledger')
 
 	SET IDENTITY_INSERT [dbo].[tblSMStartingNumber] OFF
 GO
