7720887a883d1e414481c8c485668ab67f6ea84a
Mon Gonzales <mon.gonzales@irely.com>
2022-08-16 21:18:42 +0800

    TR-2041 - SkyBitz - New Screen "Fuel Terminals"

---
+++ i21Database/dbo/Tables/tblTRFuelTerminal.sql
@@ -0,0 +1,25 @@
+﻿CREATE TABLE [dbo].[tblTRFuelTerminal] (
+    [intFuelTerminalId]			        INT            IDENTITY (1, 1) NOT NULL,
+    
+    [strFuelTerminalNo]				    NVARCHAR (50) COLLATE Latin1_General_CI_AS NOT NULL,
+    [strFuelTerminalName]				NVARCHAR (100) COLLATE Latin1_General_CI_AS NOT NULL,
+
+    [strAddress]					NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NULL,
+    [strCity]						NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NULL,
+    [strState]						NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NULL,
+    [strZipCode]					NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NULL,    
+    [dblLongitude]					NUMERIC (18, 6) DEFAULT ((0)) NULL,
+    [dblLatitude]					NUMERIC (18, 6) DEFAULT ((0)) NULL,	
+	
+    [intAverageLoadTime]			INT            NULL,
+
+	
+    [guiApiUniqueId] UNIQUEIDENTIFIER NULL,
+    [intConcurrencyId] 		        INT CONSTRAINT [DF_tblTRTransportTerminal_ConcurrencyId] DEFAULT ((0)) NOT NULL,	
+
+    CONSTRAINT [PK_dbo.tblTRFuelTerminal] PRIMARY KEY CLUSTERED ([intFuelTerminalId] ASC),    
+	CONSTRAINT [AK_tblTRFuelTerminal_Fuel_Terminal_No] UNIQUE ([strFuelTerminalNo]),
+);
+
+GO
+
