0cd93c9de37af269ef5010fda6f0071e2e9b611f
Mon Gonzales <mon.gonzales@irely.com>
2022-08-17 11:58:08 +0800

    AR-15136 - SkyBitz - Change Sales Rep screen

--- i21Database/dbo/Tables/tblARSalespersonFuelTerminal.sql
+++ i21Database/dbo/Tables/tblARSalespersonFuelTerminal.sql
@@ -1,14 +1,15 @@
 ﻿CREATE TABLE [dbo].[tblARSalespersonFuelTerminal] (
-    [intSalesoersonFuelTerminalId]		INT                 IDENTITY (1, 1) NOT NULL,
+    [intSalespersonFuelTerminalId]		INT                 IDENTITY (1, 1) NOT NULL,
     [intEntitySalespersonId]            INT                 NOT NULL,
     [intFuelTerminalId]			        INT				    NULL,
     
     [intConcurrencyId]                  INT                 DEFAULT(1) NOT NULL,
     [guiApiUniqueId]                    UNIQUEIDENTIFIER    NULL,
 
-    CONSTRAINT [PK_tblARSalespersonFuelTerminal] PRIMARY KEY CLUSTERED ([intSalesoersonFuelTerminalId] ASC),
+    CONSTRAINT [PK_tblARSalespersonFuelTerminal] PRIMARY KEY CLUSTERED ([intSalespersonFuelTerminalId] ASC),
 	CONSTRAINT [FK_tblARSalespersonFuelTerminal_tblARSalesperson] FOREIGN KEY ([intEntitySalespersonId]) REFERENCES [tblARSalesperson]([intEntityId]),
 	CONSTRAINT [FK_tblARSalespersonFuelTerminal_tblTRFuelTerminal] FOREIGN KEY ([intFuelTerminalId]) REFERENCES [tblTRFuelTerminal]([intFuelTerminalId]),
+	CONSTRAINT [UQ_tblARSalespersonFuelTerminal_entitySalespersonId_fuelTerminalId] UNIQUE NONCLUSTERED ([intEntitySalespersonId] ASC, intFuelTerminalId)
 	
 );
 
