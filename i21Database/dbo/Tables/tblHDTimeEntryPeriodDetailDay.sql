05afd4c3e1c1fd1cc3f5c094c7a6912a81608514
Edsel Roldan Magura <edsel.magura@iRely.com>
2022-08-18 11:06:28 +0800

    HD-4139

---
+++ i21Database/dbo/Tables/tblHDTimeEntryPeriodDetailDay.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[tblHDTimeEntryPeriodDetailDay]
+(
+	[intTimeEntryPeriodDetailDaysId]		INT IDENTITY(1,1) NOT NULL,
+	[intTimeEntryPeriodDetailId]			INT			   NOT NULL,
+	[strDaysDisplay]						NVARCHAR(250) COLLATE Latin1_General_CI_AS NOT NULL,
+	[dtmBillingPeriodStart]					DATETIME NOT NULL,
+	[dtmBillingPeriodEnd]					DATETIME NOT NULL,
+	[intConcurrencyId] [int]				NOT NULL DEFAULT 1,
+	CONSTRAINT [PK_tblHDTimeEntryPeriodDetailDay_intTimeEntryPeriodDetailDaysId] PRIMARY KEY CLUSTERED ([intTimeEntryPeriodDetailDaysId] ASC),
+	CONSTRAINT [FK_tblHDTimeEntryPeriodDetailDay_tblHDTimeEntryPeriodDetail_intTimeEntryPeriodDetailId] FOREIGN KEY ([intTimeEntryPeriodDetailId]) REFERENCES [dbo].[tblHDTimeEntryPeriodDetail] ([intTimeEntryPeriodDetailId]) ON DELETE CASCADE,
+    CONSTRAINT [UQ_tblHDTimeEntryPeriodDetailDay_intTimeEntryPeriodDetailId_strDaysDisplay] UNIQUE ([intTimeEntryPeriodDetailId],[strDaysDisplay])
+)
+
+GO
