dc164182b6fcb720c276f55531b6566fab231e38
Chris Salvante <chris.salvante@irely.com>
2022-08-19 15:39:22 +0800

    FRM-10440

--- i21Database/Scripts/Post-Deployment/PostDeployment.sql
+++ i21Database/Scripts/Post-Deployment/PostDeployment.sql
@@ -94,9 +94,9 @@ print 'BEGIN POST DEPLOYMENT'
 :r .\SM\1910_DeleteDuplicateUserPreference.sql
 :r .\SM\2010_RemoveF9Shortcut.sql
 :r .\SM\1920_DeleteDuplicateEntityPhoneNumber.sql
-:r .\SM\2010_UpdateEntityInquiryPermissionFrom1920.sql
-:r .\SM\2010_UpdateLicenseAgreement.sql
-:r .\SM\2010_AddConstrainWithNoCheck_tblSMEmailRecipient.sql
+--:r .\SM\2010_UpdateEntityInquiryPermissionFrom1920.sql
+--:r .\SM\2010_UpdateLicenseAgreement.sql
+--:r .\SM\2010_AddConstrainWithNoCheck_tblSMEmailRecipient.sql
 :r .\SM\2210_EncryptPasswords.sql
 
 
