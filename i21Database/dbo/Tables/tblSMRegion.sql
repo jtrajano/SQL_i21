68b92bcce65b02cf5dbcf700ce0ead24223ff9e0
Mon Gonzales <mon.gonzales@irely.com>
2022-08-23 07:18:12 +0800

    SM-5396 - New screen: Region

---
+++ i21Database/dbo/Tables/tblSMRegion.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[tblSMRegion] (
+    [intRegionId]			        INT IDENTITY (1, 1) NOT NULL,    
+    [strRegion]				        NVARCHAR (50) COLLATE Latin1_General_CI_AS NOT NULL,
+   
+    [guiApiUniqueId]                UNIQUEIDENTIFIER NULL,
+    [intConcurrencyId] 		        INT CONSTRAINT [DF_tblSMRegion_ConcurrencyId] DEFAULT ((0)) NOT NULL,	
+
+    CONSTRAINT [PK_tblSMRegion] PRIMARY KEY CLUSTERED ([intRegionId] ASC),    
+	CONSTRAINT [UQ_tblSMRegion_Region] UNIQUE ([strRegion]),
+);
+
+GO
+
