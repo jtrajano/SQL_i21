966d273b90b64be2effcdacf257a199bd6b0db63
Mon Gonzales <mon.gonzales@irely.com>
2022-08-23 13:31:58 +0800

    SM-5397 - New screen: Territory

---
+++ i21Database/dbo/Tables/tblSMTerritory.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[tblSMTerritory] (
+    [intTerritoryId]			    INT IDENTITY (1, 1) NOT NULL,    
+    [intRegionId]                   INT NOT NULL,
+    [strTerritory]			        NVARCHAR (50) COLLATE Latin1_General_CI_AS NOT NULL,
+   
+    [guiApiUniqueId]                UNIQUEIDENTIFIER NULL,
+    [intConcurrencyId] 		        INT CONSTRAINT [DF_tblSMTerritory_ConcurrencyId] DEFAULT ((0)) NOT NULL,	
+
+    CONSTRAINT [PK_tblSMTerritory] PRIMARY KEY CLUSTERED ([intTerritoryId] ASC),    
+	CONSTRAINT [FK_tblSMTerritory_tblSMRegion] FOREIGN KEY ([intRegionId]) REFERENCES [tblSMRegion]([intRegionId]),
+	CONSTRAINT [UQ_tblSMTerritory_Region_Territory] UNIQUE ([intRegionId], [strTerritory]),
+);
+
+GO
+
