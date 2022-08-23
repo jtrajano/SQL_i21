ac8b8f0e7eeeda371df7894b704c788fca3463bc
Mon Gonzales <mon.gonzales@irely.com>
2022-08-23 10:50:47 +0800

    AR-15136 - SkyBitz - Change Sales Rep screen

---
+++ i21Database/dbo/Tables/tblARSalespersonRegion.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[tblARSalespersonRegion] (
+    [intSalespersonRegionId]		INT                 IDENTITY (1, 1) NOT NULL,
+    [intEntitySalespersonId]        INT                 NOT NULL,
+    [intRegionId]			        INT				    NULL,
+    
+    [intConcurrencyId]              INT                 DEFAULT(1) NOT NULL,
+    [guiApiUniqueId]                UNIQUEIDENTIFIER    NULL,
+
+    CONSTRAINT [PK_tblARSalespersonRegion] PRIMARY KEY CLUSTERED ([intSalespersonRegionId] ASC),
+	CONSTRAINT [FK_tblARSalespersonRegion_tblARSalesperson] FOREIGN KEY ([intEntitySalespersonId]) REFERENCES [tblARSalesperson]([intEntityId]),
+	CONSTRAINT [FK_tblARSalespersonRegion_tblSMRegion] FOREIGN KEY ([intRegionId]) REFERENCES [tblSMRegion]([intRegionId]),
+	CONSTRAINT [UQ_tblARSalespersonRegion_entitySalespersonId_regionId] UNIQUE NONCLUSTERED ([intEntitySalespersonId] ASC, intRegionId)
+	
+);
+
+
