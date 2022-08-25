25c6f501f5f6b2b083ae990f3e432f631ac597da
Mon Gonzales <mon.gonzales@irely.com>
2022-08-26 07:09:16 +0800

    AR-15110 - SkyBitz - Change Customer screen

---
+++ i21Database/dbo/Tables/tblARCustomerRecurringPO.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[tblARCustomerRecurringPO] (
+    [intCustomerRecurringPOId]	INT                 IDENTITY (1, 1) NOT NULL,
+    [intEntityCustomerId]       INT                 NOT NULL,    
+    [strRecurringPONumber]      NVARCHAR(50)        COLLATE Latin1_General_CI_AS NULL,    
+    [dtmFrom]				    DATETIME            NOT NULL,    
+    [dtmTo]				    DATETIME            NOT NULL,    
+    [intConcurrencyId]			INT                 NOT NULL,
+    [guiApiUniqueId]            UNIQUEIDENTIFIER    NULL,
+    CONSTRAINT [PK_tblARCustomerRecurringPO] PRIMARY KEY CLUSTERED ([intCustomerRecurringPOId] ASC),
+	CONSTRAINT [FK_tblARCustomerRecurringPO_tblARCustomer] FOREIGN KEY ([intEntityCustomerId]) REFERENCES [dbo].[tblARCustomer] ([intEntityId]) ON DELETE CASCADE,
+);
+
+
