598582ebc61f97665b93a410e6f93e4158e39dc8
Jeffrey Trajano <jeffrey.trajano@irely.com>
2022-08-31 21:07:29 +0800

    GL-9465

--- i21Database/Scripts/Post-Deployment/CM/18_BankFileFormat.sql
+++ i21Database/Scripts/Post-Deployment/CM/18_BankFileFormat.sql
@@ -1,7 +1,6 @@
 
 
 --REPLACE ALL Transaction Date to Cleared Date in bank file format detail with bank statement type
-GO
 
 UPDATE A SET strFieldName = 'Cleared Date' from tblCMBankFileFormatDetail A join tblCMBankFileFormat B on
 A.intBankFileFormatId = B.intBankFileFormatId 
