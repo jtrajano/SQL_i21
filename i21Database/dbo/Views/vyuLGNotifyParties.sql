21a9f1242a12c9752c9f83dd7fea30f8c57bf676
Arman Arenas <arman.arenas@irely.com>
2022-08-31 22:35:16 +0800

    LG-3617 additional fields on vyuLGNotifyParties for transports load

--- i21Database/dbo/Views/vyuLGNotifyParties.sql
+++ i21Database/dbo/Views/vyuLGNotifyParties.sql
@@ -10,15 +10,19 @@ SELECT	E.intEntityId,
 		strZipCode = EL.strZipCode,
 		strCountry = EL.strCountry,
 		strPhone = EC.strPhone,
+		ysnActive = E.ysnActive,
 		intDefaultLocationId = EL.intEntityLocationId,
 		strDefaultLocation = EL.strLocationName,
-		ysnTransportTerminal = ISNULL(V.ysnTransportTerminal, 0)
+		ysnTransportTerminal = ISNULL(V.ysnTransportTerminal, 0),
+		strTerminalControlNumber = TCN.strTerminalControlNumber
 FROM vyuEMEntity E
 LEFT JOIN tblEMEntityLocation EL ON EL.intEntityId = E.intEntityId AND EL.ysnDefaultLocation = 1
 LEFT JOIN tblEMEntityToContact ETC ON ETC.intEntityId = E.intEntityId AND ETC.ysnDefaultContact = 1
 LEFT JOIN tblEMEntity EC ON EC.intEntityId = ETC.intEntityId
 LEFT JOIN tblAPVendor V ON V.intEntityId = E.intEntityId AND ISNULL(V.ysnPymtCtrlActive,0) = 1
-WHERE strType IN ('Vendor', 'Customer','Forwarding Agent','Shipping Line','Terminal', 'Broker')
+LEFT JOIN tblTRSupplyPoint SP ON SP.intEntityLocationId = EL.intEntityLocationId
+LEFT JOIN tblTFTerminalControlNumber TCN ON TCN.intTerminalControlNumberId = SP.intTerminalControlNumberId
+WHERE E.strType IN ('Vendor', 'Customer','Forwarding Agent','Shipping Line','Terminal', 'Broker')
 
 UNION ALL
 
@@ -32,9 +36,11 @@ SELECT 	intEntityId = B.intBankId,
 		strZipCode = B.strZipCode,
 		strCountry = B.strCountry,
 		strPhone = B.strPhone,
+		ysnActive = CAST(1 AS BIT),
 		intDefaultLocationId = -1,
 		strDefaultLocation = NULL,
-		ysnTransportTerminal = CAST(0 AS BIT)
+		ysnTransportTerminal = CAST(0 AS BIT),
+		strTerminalControlNumber = NULL
 FROM 	tblCMBank B
 
 UNION ALL
@@ -49,7 +55,9 @@ SELECT 	intEntityId = C.intCompanySetupID,
 		strZipCode = C.strZip,
 		strCountry = C.strCountry,
 		strPhone = C.strPhone,
+		ysnActive = CAST(1 AS BIT),
 		intDefaultLocationId = -1,
 		strDefaultLocation = NULL,
-		ysnTransportTerminal = CAST(0 AS BIT)
+		ysnTransportTerminal = CAST(0 AS BIT),
+		strTerminalControlNumber = NULL
 FROM tblSMCompanySetup C
