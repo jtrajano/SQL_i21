0b98c59faee8edbe1990ca0814bfa0cbff7fce24
Jeffrey Trajano <jeffrey.trajano@irely.com>
2022-08-31 19:38:04 +0800

    GL-9465

---
+++ i21Database/dbo/Views/vyuCMBSBankFileFormatStatus.sql
@@ -0,0 +1,37 @@
+
+CREATE VIEW vyuCMBSBankFileFormatStatus
+AS
+SELECT intBankFileFormatId 'BankFileFormatId'
+	, FileFormatName
+	,[tblCMBankTransaction.dblAmount] 'Amount'
+	,[tblCMBankTransaction.dblWithdrawalAmount] 'Debit'
+	,[tblCMBankTransaction.dblDepositAmount] 'Credit'
+	,[Cleared Date] 'ClearDate'
+	,[tblCMBankTransaction.strReferenceNo] 'CheckNumber'
+	,[tblCMBankAccount.strBankAccountNo] 'BankAccountNo'
+	,[Bank Description] 'BankDescription'
+FROM
+(
+SELECT A.intBankFileFormatId,A.strName FileFormatName ,  A.strName, B.strFieldName from tblCMBankFileFormat A
+LEFT JOIN
+tblCMBankFileFormatDetail B ON A.intBankFileFormatId = B.intBankFileFormatId
+WHERE A.intBankFileType = 3
+) AS sourceTable
+PIVOT 
+(
+	count( strName ) 
+	FOR strFieldName IN (
+	[tblCMBankTransaction.dblAmount]
+	,[tblCMBankTransaction.dblWithdrawalAmount]
+	,[tblCMBankTransaction.dblDepositAmount]
+	,[Cleared Date]
+	,[tblCMBankTransaction.strReferenceNo]
+	,[tblCMBankAccount.strBankAccountNo]
+	,[Bank Description]
+	)	
+) AS PivotTable
+
+
+
+
+
