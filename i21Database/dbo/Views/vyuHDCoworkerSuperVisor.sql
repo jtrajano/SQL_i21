439b55eeb3064a08435d7836bad4d591f1cc3f74
Edsel Roldan Magura <edsel.magura@iRely.com>
2022-08-31 15:24:22 +0800

    HD-4160

---
+++ i21Database/dbo/Views/vyuHDCoworkerSuperVisor.sql
@@ -0,0 +1,13 @@
+﻿CREATE VIEW [dbo].[vyuHDCoworkerSuperVisor]
+AS
+SELECT   [intCoworkerSuperVisorId]			= CoworkerSuperVisor.[intCoworkerSuperVisorId]			
+		,[intEntityId]						= CoworkerSuperVisor.[intEntityId]	
+		,[ysnAutoAdded]						= CoworkerSuperVisor.[ysnAutoAdded]
+		,[intConcurrencyId] 				= CoworkerSuperVisor.[intConcurrencyId]
+		,[strFullName]						= Entity.[strName]
+FROM tblHDCoworkerSuperVisor CoworkerSuperVisor
+		INNER JOIN tblEMEntity Entity
+ON Entity.intEntityId = CoworkerSuperVisor.intEntityId
+
+
+GO
