cf8390a633aff1e9c05326b4e33662f748d57a02
Arman Arenas <arman.arenas@irely.com>
2022-08-31 22:12:12 +0800

    LG-3617 additional fields on vyuLGNotifyParties to reuse it for entity dropdown

--- i21Database/dbo/Views/vyuLGNotifyParties.sql
+++ i21Database/dbo/Views/vyuLGNotifyParties.sql
@@ -2,21 +2,54 @@ CREATE VIEW vyuLGNotifyParties
 AS
 SELECT	E.intEntityId,
 		E.strName,
-		E.strType 				AS strEntity
-FROM	vyuEMEntity				E
+		strEntity = E.strType,
+		strEntityNo = E.strEntityNo,
+		strAddress = EL.strAddress,
+		strCity = EL.strCity,
+		strState = EL.strState,
+		strZipCode = EL.strZipCode,
+		strCountry = EL.strCountry,
+		strPhone = EC.strPhone,
+		intDefaultLocationId = EL.intEntityLocationId,
+		strDefaultLocation = EL.strLocationName,
+		ysnTransportTerminal = ISNULL(V.ysnTransportTerminal, 0)
+FROM vyuEMEntity E
+LEFT JOIN tblEMEntityLocation EL ON EL.intEntityId = E.intEntityId AND EL.ysnDefaultLocation = 1
+LEFT JOIN tblEMEntityToContact ETC ON ETC.intEntityId = E.intEntityId AND ETC.ysnDefaultContact = 1
+LEFT JOIN tblEMEntity EC ON EC.intEntityId = ETC.intEntityId
 LEFT JOIN tblAPVendor V ON V.intEntityId = E.intEntityId AND ISNULL(V.ysnPymtCtrlActive,0) = 1
 WHERE strType IN ('Vendor', 'Customer','Forwarding Agent','Shipping Line','Terminal', 'Broker')
 
 UNION ALL
 
-SELECT 	B.intBankId 			AS intEntityId,
-		B.strBankName 			AS strName,
-		'Bank' 					AS strEntity
-FROM 	tblCMBank 				B
+SELECT 	intEntityId = B.intBankId,
+		strName = B.strBankName,
+		strEntity = 'Bank',
+		strEntityNo = B.strRTN,
+		strAddress = B.strAddress,
+		strCity = B.strCity,
+		strState = B.strState,
+		strZipCode = B.strZipCode,
+		strCountry = B.strCountry,
+		strPhone = B.strPhone,
+		intDefaultLocationId = -1,
+		strDefaultLocation = NULL,
+		ysnTransportTerminal = CAST(0 AS BIT)
+FROM 	tblCMBank B
 
 UNION ALL
 
-SELECT 	C.intCompanySetupID 	AS intEntityId,
-		C.strCompanyName 		AS strName,
-		'Company' 				AS strEntity
+SELECT 	intEntityId = C.intCompanySetupID,
+		strName = C.strCompanyName,
+		strEntity = 'Company',
+		strEntityNo = C.strEin,
+		strAddress = C.strAddress,
+		strCity = C.strCity,
+		strState = C.strState,
+		strZipCode = C.strZip,
+		strCountry = C.strCountry,
+		strPhone = C.strPhone,
+		intDefaultLocationId = -1,
+		strDefaultLocation = NULL,
+		ysnTransportTerminal = CAST(0 AS BIT)
 FROM tblSMCompanySetup C
