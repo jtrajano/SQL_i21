1bbccb2bd8ef4216e9f2df9ccf439540e0892cab
Jeffrey Trajano <jeffrey.trajano@irely.com>
2022-09-01 13:06:17 +0800

    Revert "GL-9441"
    This reverts commit e81dce873659bad28e0a39426145a7ea2be08279.

--- i21Database/Scripts/Post-Deployment/GL/DefaultData/1x_RecurringTransaction.sql
+++
@@ -1,8 +0,0 @@
-GO
-
-DELETE A FROM tblSMRecurringTransaction A LEFT JOIN 
-tblGLJournal B ON A.intTransactionId = B.intJournalId
-WHERE A.strTransactionType = 'General Journal' 
-AND B.intJournalId IS NULL
-
-GO
