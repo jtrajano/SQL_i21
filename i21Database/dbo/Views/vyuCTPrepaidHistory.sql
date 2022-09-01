94467632e9bb1c680c09d83e553cf91eba760368
Bryan-PC <013.bryan@gmail.com>
2022-09-01 22:00:06 +0800

    CT-7357

---
+++ i21Database/dbo/Views/vyuCTPrepaidHistory.sql
@@ -0,0 +1,20 @@
+﻿
+
+Create VIEW [dbo].vyuCTPrepaidHistory
+
+AS
+
+SELECT ID.intInvoiceDetailId
+	 , I.intInvoiceId
+	 , CH.intItemContractHeaderId
+	 , I.strInvoiceNumber 
+FROM tblCTItemContractHeader CH
+INNER JOIN tblCTItemContractDetail CD on CH.intItemContractHeaderId = CD.intItemContractHeaderId
+INNER JOIN tblARInvoiceDetail ID on ID.intItemContractHeaderId = CD.intItemContractHeaderId AND ID.intItemContractDetailId = CD.intItemContractDetailId
+INNER JOIN tblARInvoice I on I.intInvoiceId = ID.intInvoiceId
+
+
+
+
+GO
+
