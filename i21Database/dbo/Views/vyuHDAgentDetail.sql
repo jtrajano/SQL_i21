b0890df97856904d0304000ad8952c11bcd9b664
Edsel Roldan Magura <edsel.magura@iRely.com>
2022-09-02 15:36:21 +0800

    HD-4162

--- i21Database/dbo/Views/vyuHDAgentDetail.sql
+++ i21Database/dbo/Views/vyuHDAgentDetail.sql
@@ -10,12 +10,17 @@
 		strPhone = en.strPhone,
 		strMobile = en.strMobile,
 		us.strJIRAUserName,
-		ysnVendor = (select case when count(*) < 1 then convert(bit,0) else convert(bit,1) end from tblEMEntityType where intEntityId = us.intEntityId and strType = 'Vendor')
+		ysnVendor = (select case when count(*) < 1 then convert(bit,0) else convert(bit,1) end from tblEMEntityType where intEntityId = us.intEntityId and strType = 'Vendor'),
+		ysnTimeEntryExempt = case when ExemptedAgent.intEntityId is not null
+									then convert(bit,1)
+								   else convert(bit,0)
+							 end
 	from
 		tblSMUserSecurity us
 		inner join vyuEMEntityContact en on en.intEntityId = us.[intEntityId] and en.ysnDefaultContact = 1
 		inner join [tblEMEntityCredential] ec on ec.intEntityId = us.[intEntityId]
 		inner join tblEMEntity ue on ue.intEntityId = us.intEntityId
+		left join vyuHDExemptedAgent ExemptedAgent on ExemptedAgent.intEntityId = us.intEntityId
 	where
 		us.[intEntityId] is not null
 
@@ -31,11 +36,18 @@
 		strPhone = en.strPhone,
 		strMobile = en.strMobile,
 		strJIRAUserName = (select strJIRAUserName from tblSMUserSecurity where intEntityId = us.intEntityId),
-		ysnVendor = (select case when count(*) < 1 then convert(bit,0) else convert(bit,1) end from tblEMEntityType where intEntityId = us.intEntityId and strType = 'Vendor')
+		ysnVendor = (select case when count(*) < 1 then convert(bit,0) else convert(bit,1) end from tblEMEntityType where intEntityId = us.intEntityId and strType = 'Vendor'),
+		ysnTimeEntryExempt = case when ExemptedAgent.intEntityId is not null
+									then convert(bit,1)
+								   else convert(bit,0)
+							 end
 	from
 		tblARSalesperson us
 		inner join tblEMEntityToContact etc on etc.intEntityId = us.intEntityId
 		inner join tblEMEntity en on en.intEntityId = etc.intEntityContactId
 		inner join tblEMEntityCredential ec on ec.intEntityId = etc.intEntityContactId
+		left join vyuHDExemptedAgent ExemptedAgent on ExemptedAgent.intEntityId = us.intEntityId
 	where
 		us.[intEntityId] is not null
+
+GO
