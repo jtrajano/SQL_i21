b264c885cf6a67efb9931b929a0240e942365410
Chris Salvante <chris.salvante@irely.com>
2022-09-03 00:51:31 +0800

    SM-5693

--- i21Database/Scripts/Post-Deployment/SM/2210_EncryptPasswords.sql
+++ i21Database/Scripts/Post-Deployment/SM/2210_EncryptPasswords.sql
@@ -15,6 +15,10 @@ UPDATE		tblRMConnection
 SET			strPassword = dbo.fnAESEncryptASym(ISNULL(strPassword, ''))
 WHERE		dbo.fnAESDecryptASym(strPassword) IS NULL
 
+UPDATE		tblSMCompanyPreference
+SET			strSMTPPassword = dbo.fnAESEncryptASym(ISNULL(strSMTPPassword, ''))
+WHERE		dbo.fnAESDecryptASym(strSMTPPassword) IS NULL
+
 PRINT('/*******************  END PASSWORD ENCRYPTION *******************/')
 
 GO
