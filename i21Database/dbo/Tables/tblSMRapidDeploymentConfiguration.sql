5b1b68659fc65c053cd8f09a8ebe74716374a2d4
Mark Daniel Marmeto <mark.marmeto@irely.com>
2022-09-02 11:25:40 +0800

    GL-9373

---
+++ i21Database/dbo/Tables/tblSMRapidDeploymentConfiguration.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[tblSMRapidDeploymentConfiguration]
+(
+	[intRapidDeploymentConfigurationId] INT IDENTITY (1, 1) NOT NULL,
+    [intModuleId]                       INT NOT NULL,
+    [intEntityId]                       INT NOT NULL,
+    [intConcurrencyId]                  INT DEFAULT (1) NOT NULL,
+    CONSTRAINT [PK_tblSMRapidDeploymentConfiguration] PRIMARY KEY CLUSTERED ([intRapidDeploymentConfigurationId] ASC)
+)
