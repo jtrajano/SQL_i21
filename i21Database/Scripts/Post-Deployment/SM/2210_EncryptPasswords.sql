8f71c1edf5d1179d3e28258817d53e6348a0f79d
Chris Salvante <chris.salvante@irely.com>
2022-09-03 00:51:31 +0800

    SM-5693

---
+++ i21Database/Scripts/Post-Deployment/SM/2210_EncryptPasswords.sql
@@ -0,0 +1,24 @@
+﻿GO
+PRINT('/*******************  BEGIN PASSWORD ENCRYPTION *******************/')
+
+UPDATE		tblEMEntitySMTPInformation
+SET			strPassword = dbo.fnAESEncryptASym(strPassword),
+			ysnIsPasswordEncrypted = 1
+WHERE		ysnIsPasswordEncrypted = 0
+
+UPDATE		tblSMInterCompany
+SET			strPassword = dbo.fnAESEncryptASym(strPassword),
+			ysnIsPasswordEncrypted = 1
+WHERE		ysnIsPasswordEncrypted = 0
+
+UPDATE		tblRMConnection
+SET			strPassword = dbo.fnAESEncryptASym(ISNULL(strPassword, ''))
+WHERE		dbo.fnAESDecryptASym(strPassword) IS NULL
+
+UPDATE		tblSMCompanyPreference
+SET			strSMTPPassword = dbo.fnAESEncryptASym(ISNULL(strSMTPPassword, ''))
+WHERE		dbo.fnAESDecryptASym(strSMTPPassword) IS NULL
+
+PRINT('/*******************  END PASSWORD ENCRYPTION *******************/')
+
+GO
