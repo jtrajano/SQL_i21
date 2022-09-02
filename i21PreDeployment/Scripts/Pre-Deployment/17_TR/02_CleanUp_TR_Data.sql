b75e1c693c16121d9e9b2f0075bca35ef2124615
GRM-NUC\iRelyNUC23 <girome.andaya@iRely.com>
2022-09-02 16:38:21 +0800

    TR-2073

---
+++ i21PreDeployment/Scripts/Pre-Deployment/17_TR/02_CleanUp_TR_Data.sql
@@ -0,0 +1,20 @@
+﻿GO
+PRINT 'BEGIN Pre-deployment TR Data Clean-up'
+GO
+
+
+PRINT('Update TR Cross Reference BOL TruckId')
+GO
+
+IF EXISTS(SELECT * FROM  INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'tblTRCrossReferenceBol') 
+BEGIN
+	UPDATE 
+		tblTRCrossReferenceBol
+	SET 
+		intTruckId = NULL
+	WHERE
+		strType = 'Truck' AND
+		intTruckId NOT IN
+			(SELECT intEntityShipViaTruckId FROM tblSMShipViaTruck)
+END
+GO
