9753873861d117462ecafb67a5cb20a4900cc554
Erick F Dela Cruz <erick.delacruz@irely.com>
2022-09-05 14:11:49 +0800

    AP-10859

--- i21Database/Scripts/Post-Deployment/AP/PopulateVoucherAverageExchangeRate.sql
+++ i21Database/Scripts/Post-Deployment/AP/PopulateVoucherAverageExchangeRate.sql
@@ -1,10 +1,12 @@
 ﻿PRINT N'START: POPULATING VOUCHER AVERAGE EXCHANGE RATE'
 --POPULATE EMPTY VOUCHER AVERAGE EXCHANGE RATE
 UPDATE B
-SET B.dblAverageExchangeRate = CASE WHEN B.ysnOrigin = 1 THEN 1 ELSE (BD.dblTotalUSD / B.dblTotal) END
+--SET B.dblAverageExchangeRate = CASE WHEN B.ysnOrigin = 1 THEN 1 ELSE (BD.dblTotalUSD / B.dblTotal) END
+SET B.dblAverageExchangeRate = CASE WHEN B.ysnOrigin = 1 THEN 1 ELSE BD.dblAverageExchangeRate END
 FROM tblAPBill B
 OUTER APPLY (
-	SELECT COUNT(*) intDetailCount, SUM((dblTotal + dblTax)) dblTotal, SUM((dblTotal + dblTax) * dblRate) dblTotalUSD
+	SELECT COUNT(*) intDetailCount, SUM((dblTotal + dblTax) * dblRate) / SUM((dblTotal + dblTax))  AS dblAverageExchangeRate
+	--SUM((dblTotal + dblTax)) dblTotal, SUM((dblTotal + dblTax) * dblRate) dblTotalUSD
 	FROM tblAPBillDetail
 	WHERE intBillId = B.intBillId
 	
@@ -13,10 +15,12 @@ WHERE B.dblTotal <> 0 AND BD.intDetailCount > 0
 
 --POPULATE EMPTY VOUCHER AVERAGE EXCHANGE RATE (ARCHIVED)
 UPDATE B
-SET B.dblAverageExchangeRate = CASE WHEN B.ysnOrigin = 1 THEN 1 ELSE (BD.dblTotalUSD / B.dblTotal) END
+--SET B.dblAverageExchangeRate = CASE WHEN B.ysnOrigin = 1 THEN 1 ELSE (BD.dblTotalUSD / B.dblTotal) END
+SET B.dblAverageExchangeRate = CASE WHEN B.ysnOrigin = 1 THEN 1 ELSE BD.dblAverageExchangeRate END
 FROM tblAPBillArchive B
 OUTER APPLY (
-	SELECT COUNT(*) intDetailCount, SUM((dblTotal + dblTax)) dblTotal, SUM((dblTotal + dblTax) * dblRate) dblTotalUSD
+	SELECT COUNT(*) intDetailCount, SUM((dblTotal + dblTax) * dblRate) / SUM((dblTotal + dblTax))  AS dblAverageExchangeRate
+	--SUM((dblTotal + dblTax)) dblTotal, SUM((dblTotal + dblTax) * dblRate) dblTotalUSD
 	FROM tblAPBillDetailArchive
 	WHERE intBillId = B.intBillId
 	
