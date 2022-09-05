18f0c40d4fb4af9da397c865b7c2f786d843e783
Edsel Roldan Magura <edsel.magura@iRely.com>
2022-09-05 17:58:54 +0800

    HD-4159

--- i21Database/dbo/Views/vyuHDExemptedAgent.sql
+++ i21Database/dbo/Views/vyuHDExemptedAgent.sql
@@ -6,11 +6,11 @@ AS
 		SELECT   intEntityId   = CoworkerSuperVisor.intEntityId
 		FROM tblHDCoworkerSuperVisor CoworkerSuperVisor
 
-		UNION ALL
+		--UNION ALL
 
-		SELECT intEntityId   = CoworkerGoal.intEntityId
-		FROM tblHDCoworkerGoal CoworkerGoal
-		WHERE ysnActive = CONVERT(bit, 0) AND strFiscalYear = DATEPART(YEAR, GETDATE())
+		--SELECT intEntityId   = CoworkerGoal.intEntityId
+		--FROM tblHDCoworkerGoal CoworkerGoal
+		--WHERE ysnActive = CONVERT(bit, 0) AND strFiscalYear = DATEPART(YEAR, GETDATE())
 	) Agent
 	GROUP BY Agent.intEntityId
 GO
