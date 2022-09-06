0966abdc5e8dbe302d6fd8e833f171ce8b076928
Erick F Dela Cruz <erick.delacruz@irely.com>
2022-09-06 15:19:37 +0800

    AP-10838

---
+++ i21Database/Scripts/Post-Deployment/AP/UpdateVendorStorePassword.sql
@@ -0,0 +1,11 @@
+﻿GO
+
+UPDATE A
+SET
+	A.strStoreFTPPassword = dbo.fnAESEncryptASym(A.strStoreFTPPassword)
+FROM tblAPVendor A
+WHERE
+	LEN(LTRIM(RTRIM(strStoreFTPPassword))) != 344
+AND NULLIF(A.strStoreFTPPassword,'') IS NOT NULL
+
+GO
--- i21Database/Scripts/Post-Deployment/PostDeployment.sql
+++ i21Database/Scripts/Post-Deployment/PostDeployment.sql
@@ -364,6 +364,7 @@ print 'BEGIN POST DEPLOYMENT'
 :r .\AP\InsertClearingTransactionsToClearingTable.sql
 :r .\AP\PopulateVoucherPayToBankAccount.sql
 :r .\AP\PopulateVoucherAverageExchangeRate.sql
+:r .\AP\UpdateVendorStorePassword.sql
 
 -- Inventory 
 :r .\IC\01_InventoryTransactionTypes.sql 
