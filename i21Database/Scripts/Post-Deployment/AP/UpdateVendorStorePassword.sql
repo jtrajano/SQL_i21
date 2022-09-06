8578f757b64b74073de325859912038897925414
Erick F Dela Cruz <erick.delacruz@irely.com>
2022-09-06 17:37:47 +0800

    AP-10838

--- i21Database/Scripts/Post-Deployment/AP/UpdateVendorStorePassword.sql
+++ i21Database/Scripts/Post-Deployment/AP/UpdateVendorStorePassword.sql
@@ -1,11 +1,7 @@
-﻿GO
-
-UPDATE A
+﻿UPDATE A
 SET
 	A.strStoreFTPPassword = dbo.fnAESEncryptASym(A.strStoreFTPPassword)
 FROM tblAPVendor A
 WHERE
-	LEN(LTRIM(RTRIM(strStoreFTPPassword))) != 344
+	LEN(LTRIM(RTRIM(A.strStoreFTPPassword))) != 344
 AND NULLIF(A.strStoreFTPPassword,'') IS NOT NULL
-
-GO
