dbe3aa72612a5677b89555671e95d2b219a2a100
Jeffrey Trajano <jeffrey.trajano@irely.com>
2022-09-07 19:50:09 +0800

    GL-9311 Show All Bank Accounts Config

--- i21Database/dbo/Tables/tblCMCompanyPreferenceOption.sql
+++ i21Database/dbo/Tables/tblCMCompanyPreferenceOption.sql
@@ -27,6 +27,7 @@ CREATE TABLE [dbo].[tblCMCompanyPreferenceOption](
 	ysnOverrideLocationSegment_Forward BIT NULL,
 	ysnOverrideCompanySegment_Forward BIT NULL,
 	ysnShowFeeColumns_BankRecon BIT NULL,
+	ysnProcessMultipleBankAccount BIT NULL,
 	intConcurrencyId int NULL,
  CONSTRAINT [PK_tblCMCompanyPreferenceOption] PRIMARY KEY CLUSTERED 
 (
