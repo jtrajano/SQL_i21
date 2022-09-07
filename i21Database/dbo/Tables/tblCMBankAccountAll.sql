37aabd0b6b473dc2cad62438dd2a559998552ad7
Jeffrey Trajano <jeffrey.trajano@irely.com>
2022-09-08 02:08:14 +0800

    Revert "GL-9311 Process Payments: Show All Bank Accounts"

--- i21Database/dbo/Tables/tblCMBankAccountAll.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE [dbo].[tblCMBankAccountAll](
-	[intBankAccountId] [int] NULL,
-	[strBankAccountNo] [nvarchar](30) COLLATE Latin1_General_CI_AS NULL,
-	[strBankName] [nvarchar](30) NULL
-) ON [PRIMARY]
-GO
-
