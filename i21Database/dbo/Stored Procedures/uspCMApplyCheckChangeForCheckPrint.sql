4b2e02c3ade89ad76feef8d9c556b0ab4577ebc1
Jeffrey Trajano <jeffrey.trajano@irely.com>
2022-09-07 19:11:19 +0800

    GL-9311 Flag all Bank Trans by  TransactionId

--- i21Database/dbo/Stored Procedures/uspCMApplyCheckChangeForCheckPrint.sql
+++ i21Database/dbo/Stored Procedures/uspCMApplyCheckChangeForCheckPrint.sql
@@ -1,5 +1,4 @@
 ﻿CREATE PROCEDURE [dbo].[uspCMApplyCheckChangeForCheckPrint]
-	@intBankAccountId INT = NULL,
 	@intBankTransactionTypeId INT = NULL,
 	@strTransactionId NVARCHAR(MAX) = NULL,
 	@strProcessType NVARCHAR(100),
@@ -53,10 +52,9 @@ BEGIN
 	FROM tblCMUndepositedFund U
 	INNER JOIN tblCMBankTransaction B ON U.intBankDepositId = B.intTransactionId
 	CROSS apply (
-		SELECT Item  from dbo.fnSplitString(@strTransactionId,',') where U.intUndepositedFundId = Item ) S
-	WHERE 
-	U.intBankAccountId = @intBankAccountId
-	AND U.ysnCommitted is null
+		SELECT Item  from dbo.fnSplitString(@strTransactionId,',') where U.intUndepositedFundId = Item ) b
+	WHERE
+	U.ysnCommitted is null
 	AND U.intBankDepositId IS NOT NULL
 	
 END
@@ -71,8 +69,8 @@ BEGIN
 			,intConcurrencyId = intConcurrencyId + 1
 	FROM [dbo].[tblCMBankTransaction] B
 	CROSS apply (SELECT Item  from dbo.fnSplitString(@strTransactionId,',') where intTransactionId = Item ) S
-	WHERE	intBankAccountId = @intBankAccountId
-			AND ( intBankTransactionTypeId = @intBankTransactionTypeId OR intBankTransactionTypeId = @intBankTransactionTypeId + 100)
+	WHERE	
+			( intBankTransactionTypeId = @intBankTransactionTypeId OR intBankTransactionTypeId = @intBankTransactionTypeId + 100)
 			AND ysnPosted = 1
 			AND dtmCheckPrinted IS NULL
 			AND dblAmount <> 0
