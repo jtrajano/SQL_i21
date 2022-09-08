b779844f637940b5c81e551d696ab7bbe6724d58
Nelson Reyes <nelson.reyes@irely.com>
2022-09-08 13:12:28 +0800

    ST-2120

--- i21Database/dbo/User Defined Types/StagingPassportMSM.sql
+++ i21Database/dbo/User Defined Types/StagingPassportMSM.sql
@@ -1,9 +1,10 @@
 CREATE TYPE StagingPassportMSM AS TABLE
 (
 	[intRowCount] 										  INT				NULL,
+	[strCashierId]										  NVARCHAR(MAX)		NULL,
 	[strMiscellaneousSummaryCode]                         NVARCHAR(MAX)		NULL,
 	[strMiscellaneousSummarySubCode]					  NVARCHAR(MAX)		NULL,
 	[strMiscellaneousSummarySubCodeModifier] 			  NVARCHAR(MAX)		NULL,
 	[dblMiscellaneousSummaryAmount]                       NUMERIC(18,6)     NULL,
-	[intMiscellaneousSummaryCount]                        INT				NULL
+	[dblMiscellaneousSummaryCount]                        NUMERIC(18,6)				NULL
 )
