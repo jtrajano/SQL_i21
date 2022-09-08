617680cc7c3c5c60e0ada09a91a8a7a9ffbb364b
Jonathan Valenzuela <valenzuela.jonathan@outlook.com>
2022-09-08 19:23:20 +0800

    QC-719

--- i21Database/dbo/Stored Procedures/uspQMGetEmailInfo.sql
+++ i21Database/dbo/Stored Procedures/uspQMGetEmailInfo.sql
@@ -1,85 +1,113 @@
 ﻿CREATE PROCEDURE [dbo].[uspQMGetEmailInfo]
-		@strId			NVARCHAR(MAX),
-		@strMailType	NVARCHAR(100),
-		@strURL			NVARCHAR(MAX),
-		@intCurrentUserEntityId int,
-		@strNumber		NVARCHAR(MAX)
+	@strId					NVARCHAR(MAX)
+  , @strMailType			NVARCHAR(100)
+  , @strURL					NVARCHAR(MAX)
+  , @intCurrentUserEntityId INT
+  , @strNumber				NVARCHAR(MAX)
+  , @intEmailTemplate		INT = 0
 AS 
 BEGIN
-	DECLARE @intEntityId				INT,
-			@strEntityName				NVARCHAR(200),
-			@body						NVARCHAR(MAX) = '',
-			@Subject					NVARCHAR(MAX) = '',
-			@Filter						NVARCHAR(MAX) = '',
-			@strIds						NVARCHAR(MAX),
-			@strThanks					NVARCHAR(MAX) = 'Thank you for your business.'
 
-	DECLARE @loop TABLE
-	(
-		intUniqueId INT IDENTITY(1,1),
-		Id INT,
-		strNumber NVARCHAR(50)
-	)
+DECLARE @intEntityId		INT
+	  , @strEntityName		NVARCHAR(200)
+	  , @body				NVARCHAR(MAX) = ''
+	  , @Subject			NVARCHAR(MAX) = ''
+	  , @Filter				NVARCHAR(MAX) = ''
+	  , @strIds				NVARCHAR(MAX)
+	  , @strThanks			NVARCHAR(MAX) = 'Thank you for your business.'
+	  , @strReferenceNo		NVARCHAR(MAX) = ''
+	  , @strSampleStatus	NVARCHAR(MAX) = ''
+	  , @strContractNumber	NVARCHAR(MAX) = ''
+	  , @strVendorRefNumber NVARCHAR(MAX) = ''
 
-	IF @strMailType = 'Sample Instruction'
+DECLARE @loop TABLE
+(
+	intUniqueId INT IDENTITY(1,1)
+  , Id			INT
+  , strNumber	NVARCHAR(50)
+);
+
+IF @strMailType = 'Sample Instruction'
 	BEGIN
 		INSERT INTO @loop
 		SELECT CD.intContractHeaderId,SL.strSampleNumber
 		FROM tblCTContractDetail CD
 		INNER JOIN tblCTContractHeader CH ON CD.intContractHeaderId = CH.intContractHeaderId
-		INNER JOIN vyuQMSampleList SL ON CD.intContractDetailId = SL.intContractDetailId AND SL.strSampleNumber COLLATE SQL_Latin1_General_CP1_CS_AS IN (SELECT * FROM  dbo.fnSplitString(@strNumber,','))
+		INNER JOIN vyuQMSampleList SL ON CD.intContractDetailId = SL.intContractDetailId AND SL.strSampleNumber COLLATE SQL_Latin1_General_CP1_CS_AS IN (SELECT * 
+																																						 FROM  dbo.fnSplitString(@strNumber, ','))
 	END
-	ELSE IF @strMailType = 'Sample Print'
+ELSE IF @strMailType = 'Sample Print'
 	BEGIN
 		INSERT INTO @loop
 		SELECT intSampleId,strSampleNumber FROM vyuQMSampleList WHERE intSampleId IN (SELECT * FROM  dbo.fnSplitString(@strId,','))
 	END
 
-	SELECT @intEntityId = intPartyName
-	FROM vyuQMSampleList
-	WHERE intSampleId IN (SELECT * FROM  dbo.fnSplitString(@strId,','))
-	OR intContractDetailId IN (SELECT * FROM  dbo.fnSplitString(@strId,','))
 	
-	SELECT @strEntityName = strName FROM tblEMEntity WHERE intEntityId = @intEntityId
 
-	SELECT	@strIds	=	STUFF(															
-								(
-									SELECT	DISTINCT												
-											'|^|' +	LTRIM(intEntityContactId)
-									FROM	vyuCTEntityToContact 
-									WHERE	intEntityId = @intEntityId
-									AND		ISNULL(strEmail,'') <> ''
-									FOR XML PATH('')
-								),1,3, ''
-							)
-				
-	FROM	vyuCTEntityToContact CH
-	WHERE	intEntityId = @intEntityId
+SELECT @intEntityId		   = QualitySample.intPartyName
+	 , @strReferenceNo	   = ISNULL(QualitySample.strRefNo, '')
+	 , @strContractNumber  = QualitySample.strContractNumber
+	 , @strSampleStatus    = QualitySample.strStatus
+	 , @strVendorRefNumber = ISNULL(ContractHeader.strCustomerContract, '')
+FROM vyuQMSampleList AS QualitySample
+OUTER APPLY (SELECT TOP 1 CH.strCustomerContract
+			 FROM tblCTContractHeader CH
+			 WHERE CH.intContractHeaderId = ISNULL(QualitySample.intContractHeaderId, QualitySample.intLinkContractHeaderId)) AS ContractHeader
+WHERE intSampleId IN (SELECT * 
+					  FROM dbo.fnSplitString(@strId,',')) OR intContractDetailId IN (SELECT * 
+																					 FROM dbo.fnSplitString(@strId, ','))
+	
+SELECT @strEntityName = strName 
+FROM tblEMEntity 
+WHERE intEntityId = @intEntityId
 
-	IF EXISTS ( 
-	SELECT	DISTINCT	1 
-	FROM	vyuCTEntityToContact 
-	WHERE	intEntityId = @intEntityId
-	AND		ISNULL(strEmail,'') <> '' 
-	AND     strEmail NOT LIKE  '_%@__%.__%' )
-	BEGIN 
-		RAISERROR('Entity has invalid Email Address.', 16, 1);
-		RETURN;
+SELECT	@strIds	= STUFF((SELECT	DISTINCT '|^|' + LTRIM(intEntityContactId)
+						 FROM	vyuCTEntityToContact 
+						 WHERE	intEntityId = @intEntityId AND ISNULL(strEmail,'') <> '' FOR XML PATH('')), 1, 3, '')
+FROM vyuCTEntityToContact CH
+WHERE intEntityId = @intEntityId
+
+IF EXISTS (SELECT DISTINCT 1 
+		   FROM	vyuCTEntityToContact 
+		   WHERE intEntityId = @intEntityId AND ISNULL(strEmail,'') <> '' AND strEmail NOT LIKE  '_%@__%.__%' )
+BEGIN 
+	RAISERROR('Entity has invalid Email Address.', 16, 1);
+	RETURN;
+END
+
+/* Strauss Template Sample Print Template */
+IF @intEmailTemplate = 1
+	BEGIN
+		SET @Subject = 'Sample evaluation Strauss '+ @strContractNumber +' Counterparty Ref '+ @strVendorRefNumber
+		SET @body +='<!DOCTYPE html>'
+		SET @body +='<html>'
+		SET @body +='<body>Dear All, <br>'
+		SET @body += 'Please be informed sample ref #<strong>' + @strReferenceNo + '</strong> was <strong>'+ @strSampleStatus +'</strong>.'
+		SET @body += '<br><br>'
+		SET @body +='Sincerely, <br>'
+		SET @body +=(select top 1 strName from tblEMEntity where intEntityId = @intCurrentUserEntityId)
+		SET @body +='</html>'
+		SET @Filter = '[{"column":"intEntityContactId","value":"' + @strIds + '","condition":"eq","conjunction":"and"}]'
+	
+		SELECT @Subject AS strSubject,@Filter AS strFilters,@body AS strMessage
+	END
+/* Default Sample Print Template */
+ELSE  
+	BEGIN
+		SET @Subject = 'Quality ' + @strMailType + ' - ' + @strNumber
+		SET @body +='<!DOCTYPE html>'
+		SET @body +='<html>'
+		SET @body +='<body>Dear <strong>'+@strEntityName+'</strong>, <br><br>'
+		SET @body += 'Please find the attached ' + LOWER(@strMailType) + '.'
+		SET @body += '<br>'
+		SET @body +=@strThanks+'<br><br>'
+		SET @body +='Sincerely, <br>'
+		SET @body +=(select top 1 strName from tblEMEntity where intEntityId = @intCurrentUserEntityId)
+		SET @body +='</html>'
+		SET @Filter = '[{"column":"intEntityContactId","value":"' + @strIds + '","condition":"eq","conjunction":"and"}]'
+	
+		SELECT @Subject AS strSubject,@Filter AS strFilters,@body AS strMessage
 	END
 
-	SET @Subject = 'Quality ' + @strMailType + ' - ' + @strNumber
 
-	SET @body +='<!DOCTYPE html>'
-	SET @body +='<html>'
-	SET @body +='<body>Dear <strong>'+@strEntityName+'</strong>, <br><br>'
-	SET @body += 'Please find the attached ' + LOWER(@strMailType) + '.'
-	SET @body += '<br>'
-	SET @body +=@strThanks+'<br><br>'
-	SET @body +='Sincerely, <br>'
-	SET @body +=(select top 1 strName from tblEMEntity where intEntityId = @intCurrentUserEntityId)
-	SET @body +='</html>'
-
-	SET @Filter = '[{"column":"intEntityContactId","value":"' + @strIds + '","condition":"eq","conjunction":"and"}]'
-	
-	SELECT @Subject AS strSubject,@Filter AS strFilters,@body AS strMessage
 END
--- i21Database/dbo/Views/vyuQMCompanyPreference.sql
+++ i21Database/dbo/Views/vyuQMCompanyPreference.sql
@@ -1,40 +1,44 @@
 ﻿CREATE VIEW vyuQMCompanyPreference
 AS
 SELECT CP.intCompanyPreferenceId
-	,CP.intConcurrencyId
-	,CP.intNumberofDecimalPlaces
-	,CP.ysnEnableParentLot
-	,CP.ysnIsSamplePrintEnable
-	,CP.intApproveLotStatus
-	,CP.intRejectLotStatus
-	,CP.ysnAllowReversalSampleEntry
-	,CP.ysnChangeLotStatusOnApproveforPreSanitizeLot
-	,CP.ysnRejectLGContainer
-	,CP.intUserSampleApproval
-	,CP.ysnFilterContractByERPPONumber
-	,CP.ysnEnableSampleTypeByUserRole
-	,CP.ysnShowSampleFromAllLocation
-	,CP.ysnValidateMultipleValuesInTestResult
-	,CP.strTestReportComments
-	,CP.strSampleImportDateTimeFormat
-	,CP.ysnCaptureItemInProperty
-	,CP.ysnEnableAssignContractsInSample
-	,LS.strSecondaryStatus AS strApprovalLotStatus
-	,LS1.strSecondaryStatus AS strRejectionLotStatus
-	,CP.ysnEnableContractSequencesTabInSampleSearchScreen
-	,CP.strSampleInstructionReport
-	,CP.intDefaultSampleStatusId
-	,QMSS.strStatus AS strDefaultSampleStatus
-	,ISNULL(CP.intDefaultSampleStatusId, QMSS1.intSampleStatusId) AS intBlankDefaultSampleStatusId
-	,ISNULL(QMSS.strStatus, QMSS1.strStatus) AS strBlankDefaultSampleStatus
-	,CP.ysnSetDefaultReceivedDateInSampleScreen
-	,CP.intCuppingSessionLimit
+	 , CP.intConcurrencyId
+	 , CP.intNumberofDecimalPlaces
+	 , CP.ysnEnableParentLot
+	 , CP.ysnIsSamplePrintEnable
+	 , CP.intApproveLotStatus
+	 , CP.intRejectLotStatus
+	 , CP.ysnAllowReversalSampleEntry
+	 , CP.ysnChangeLotStatusOnApproveforPreSanitizeLot
+	 , CP.ysnRejectLGContainer
+	 , CP.intUserSampleApproval
+	 , CP.ysnFilterContractByERPPONumber
+	 , CP.ysnEnableSampleTypeByUserRole
+	 , CP.ysnShowSampleFromAllLocation
+	 , CP.ysnValidateMultipleValuesInTestResult
+	 , CP.strTestReportComments
+	 , CP.strSampleImportDateTimeFormat
+	 , CP.ysnCaptureItemInProperty
+	 , CP.ysnEnableAssignContractsInSample
+	 , LS.strSecondaryStatus AS strApprovalLotStatus
+	 , LS1.strSecondaryStatus AS strRejectionLotStatus
+	 , CP.ysnEnableContractSequencesTabInSampleSearchScreen
+	 , CP.strSampleInstructionReport
+	 , CP.intDefaultSampleStatusId
+	 , QMSS.strStatus AS strDefaultSampleStatus
+	 , ISNULL(CP.intDefaultSampleStatusId, QMSS1.intSampleStatusId) AS intBlankDefaultSampleStatusId
+	 , ISNULL(QMSS.strStatus, QMSS1.strStatus) AS strBlankDefaultSampleStatus
+	 , CP.ysnSetDefaultReceivedDateInSampleScreen
+	 , CP.intCuppingSessionLimit
+	 , ISNULL(CP.intSamplePrintEmailTemplate, 0) AS intSamplePrintEmailTemplate
+	 , CASE WHEN ISNULL(CP.intSamplePrintEmailTemplate, 0) = 0 THEN 
+				'Default'
+			ELSE
+				'Strauss Template 01'
+	   END AS strSamplePrintEmailTemplate
 FROM tblQMCompanyPreference CP
 LEFT JOIN tblICLotStatus LS ON LS.intLotStatusId = CP.intApproveLotStatus
 LEFT JOIN tblICLotStatus LS1 ON LS1.intLotStatusId = CP.intRejectLotStatus
 LEFT JOIN tblQMSampleStatus QMSS ON CP.intDefaultSampleStatusId = QMSS.intSampleStatusId
-OUTER APPLY (
-	SELECT TOP 1 intSampleStatusId, strStatus
-	FROM tblQMSampleStatus
-	WHERE strStatus = 'Received'
-) QMSS1
+OUTER APPLY (SELECT TOP 1 intSampleStatusId, strStatus
+			 FROM tblQMSampleStatus
+			 WHERE strStatus = 'Received') QMSS1
