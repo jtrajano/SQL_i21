67296a0f83fb92e8b129009eb8aa28d7299eddbb
Jeffrey Trajano <jeffrey.trajano@irely.com>
2022-09-08 22:49:13 +0800

    GL-9311 Process Multiple Bank Account Config

--- i21Database/dbo/Tables/tblCMBankAccountAll.sql
+++ i21Database/dbo/Tables/tblCMBankAccountAll.sql
@@ -1,7 +1,7 @@
 CREATE TABLE [dbo].[tblCMBankAccountAll](
 	[intBankAccountId] [int] NULL,
 	[strBankAccountNo] [nvarchar](30) COLLATE Latin1_General_CI_AS NULL,
-	[strBankName] [nvarchar](30) NULL
+	[strBankName] [nvarchar](30) COLLATE Latin1_General_CI_AS NULL
 ) ON [PRIMARY]
 GO
 
