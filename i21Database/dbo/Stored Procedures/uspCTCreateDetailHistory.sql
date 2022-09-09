eafe3c5d2099fd1bc68d08884c7d5a299631d083
LexPeregrino <lex.peregrino@irely.com>
2022-09-09 15:55:02 +0800

    CT-7484 v2

--- i21Database/dbo/Stored Procedures/uspCTCreateDetailHistory.sql
+++ i21Database/dbo/Stored Procedures/uspCTCreateDetailHistory.sql
@@ -1,4 +1,4 @@
-﻿ALTER PROCEDURE [dbo].[uspCTCreateDetailHistory]
+﻿CREATE PROCEDURE [dbo].[uspCTCreateDetailHistory]
 	@intContractHeaderId INT
 	, @intContractDetailId INT = NULL
 	, @strComment NVARCHAR(100) = NULL
