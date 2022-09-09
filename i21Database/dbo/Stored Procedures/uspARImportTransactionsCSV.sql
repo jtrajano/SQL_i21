3d564ef8eac9a83a3c065347c62255e2cb4fa684
DESKTOP-0Q1GJ6F\iRelyNUC14 <kelvin.quinto@irely.com>
2022-09-09 22:05:29 +0800

    AR-15279
    (cherry picked from commit ca151457489d9b7ad90ea3ea529f538d400cdbd5)

--- i21Database/dbo/Stored Procedures/uspARImportTransactionsCSV.sql
+++ i21Database/dbo/Stored Procedures/uspARImportTransactionsCSV.sql
@@ -767,7 +767,7 @@ BEGIN
 			,[dtmDueDate]				= CAST(dbo.fnGetDueDateBasedOnTerm(D.dtmDate, S.intDeliveryTermID) AS DATE)
 			,[dtmShipDate]				= D.dtmDate
 			,[dtmCalculated]			= NULL
-			,[dtmPostDate]				= NULL
+			,[dtmPostDate]				= D.dtmPostDate
 			,[intEntitySalespersonId]	= CASE WHEN ISNULL(D.strSalespersonNumber, '') <> '' AND @IsFromOldVersion = 1 THEN SP.intEntityId END
 			,[intFreightTermId]			= NULL
 			,[intShipViaId]				= NULL
@@ -898,7 +898,7 @@ BEGIN
 				,[dtmDueDate]				= CAST(dbo.fnGetDueDateBasedOnTerm(ILD.dtmDate, C.intTermsId) AS DATE)
 				,[dtmShipDate]				= ILD.dtmDate
 				,[dtmCalculated]			= NULL
-				,[dtmPostDate]				= NULL
+				,[dtmPostDate]				= ILD.dtmPostDate
 				,[intEntitySalespersonId]	= CASE WHEN ISNULL(ILD.strSalespersonNumber, '') <> '' AND @IsFromOldVersion = 1 THEN SP.intEntityId END
 				,[intFreightTermId]			= NULL
 				,[intShipViaId]				= NULL
@@ -1055,7 +1055,7 @@ BEGIN
 				,[dtmDueDate]				= CAST(dbo.fnGetDueDateBasedOnTerm(D.dtmDate, C.intTermsId) AS DATE)
 				,[dtmShipDate]				= D.dtmDate
 				,[dtmCalculated]			= NULL
-				,[dtmPostDate]				= NULL
+				,[dtmPostDate]				= D.dtmPostDate
 				,[intEntitySalespersonId]	= CASE WHEN ISNULL(D.strSalespersonNumber, '') <> '' AND @IsFromOldVersion = 1 THEN SP.intEntityId END
 				,[intFreightTermId]			= NULL
 				,[intShipViaId]				= NULL
