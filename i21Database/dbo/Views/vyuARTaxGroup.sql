89bdc562181c1cc8f2f24b9532a86137b74659bc
Roudinee Panteria <roudinee.panteria@irely.com>
2022-09-10 16:45:45 +0800

    AR-15237

---
+++ i21Database/dbo/Views/vyuARTaxGroup.sql
@@ -0,0 +1,18 @@
+﻿CREATE VIEW [dbo].[vyuARTaxGroup]
+AS 
+
+SELECT 
+	 SMTG.intTaxGroupId
+	,SMTG.strDescription
+	,SMTG.strTaxGroup
+	,ICI.intItemId
+FROM tblSMTaxGroup SMTG 
+INNER JOIN tblSMTaxGroupCode SMTGC ON SMTG.intTaxGroupId = SMTGC.intTaxGroupId
+INNER JOIN tblSMTaxCode SMTC ON SMTGC.intTaxCodeId = SMTC.intTaxCodeId
+INNER JOIN tblICCategoryTax ICCT ON SMTC.intTaxClassId = ICCT.intTaxClassId
+INNER JOIN tblICItem ICI ON ICCT.intCategoryId = ICI.intCategoryId
+GROUP BY
+	 SMTG.intTaxGroupId
+	,SMTG.strDescription
+	,SMTG.strTaxGroup
+	,ICI.intItemId
