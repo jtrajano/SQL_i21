bcc7c365399ff3f6ffb3404fb2464a7af35d4999
Bryan-PC <013.bryan@gmail.com>
2022-09-12 07:35:51 +0800

    CT-7741

--- i21Database/dbo/Tables/tblSMUpload.sql
+++ i21Database/dbo/Tables/tblSMUpload.sql
@@ -8,6 +8,10 @@
     CONSTRAINT [PK_tblUpload] PRIMARY KEY CLUSTERED ([intUploadId] ASC),
     CONSTRAINT [FK_tblSMUpload_tblSMAttachment] FOREIGN KEY ([intAttachmentId]) REFERENCES [dbo].[tblSMAttachment] ([intAttachmentId]) ON DELETE CASCADE
 );
+GO
 
+CREATE INDEX [IX_tblSMUpload_intAttachmentId] ON [dbo].[tblSMUpload] ([intAttachmentId]) INCLUDE([blbFile])
+
+GO
 
 
