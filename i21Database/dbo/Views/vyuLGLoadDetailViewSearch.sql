c285f1b25d86785449b63b594ef44159dfdda846
Arman Arenas <arman.arenas@irely.com>
2022-09-12 20:49:32 +0800

    LG-3621 modify related views to use vyuLGShipmentStatus

--- i21Database/dbo/Views/vyuLGLoadDetailViewSearch.sql
+++ i21Database/dbo/Views/vyuLGLoadDetailViewSearch.sql
@@ -192,50 +192,7 @@ SELECT   L.intLoadId
 		,CA.intCommodityAttributeId
 		,strOrigin = ISNULL(CO.strCountry, CA.strDescription)
 		,LD.intNumberOfContainers
-		,strShipmentStatus = CASE L.intShipmentStatus
-			WHEN 1 THEN 
-				CASE WHEN (L.dtmLoadExpiration IS NOT NULL AND GETDATE() > L.dtmLoadExpiration AND L.intShipmentType = 1
-						AND L.intTicketId IS NULL AND L.intLoadHeaderId IS NULL)
-				THEN 'Expired'
-				ELSE 'Scheduled' END
-			WHEN 2 THEN 'Dispatched'
-			WHEN 3 THEN 
-				CASE WHEN (L.ysnDocumentsApproved = 1 
-						AND L.dtmDocumentsApproved IS NOT NULL
-						AND ((L.dtmDocumentsApproved > L.dtmArrivedInPort OR L.dtmArrivedInPort IS NULL)
-						AND (L.dtmDocumentsApproved > L.dtmCustomsReleased OR L.dtmCustomsReleased IS NULL))) 
-						THEN 'Documents Approved'
-					WHEN (L.ysnCustomsReleased = 1) THEN 'Customs Released'
-					WHEN (L.ysnArrivedInPort = 1) THEN 'Arrived in Port'
-					ELSE 'Inbound Transit' END
-			WHEN 4 THEN 'Received'
-			WHEN 5 THEN 
-				CASE WHEN (L.ysnDocumentsApproved = 1 
-						AND L.dtmDocumentsApproved IS NOT NULL
-						AND ((L.dtmDocumentsApproved > L.dtmArrivedInPort OR L.dtmArrivedInPort IS NULL)
-						AND (L.dtmDocumentsApproved > L.dtmCustomsReleased OR L.dtmCustomsReleased IS NULL))) 
-						THEN 'Documents Approved'
-					WHEN (L.ysnCustomsReleased = 1) THEN 'Customs Released'
-					WHEN (L.ysnArrivedInPort = 1) THEN 'Arrived in Port'
-					ELSE 'Outbound Transit' END
-			WHEN 6 THEN 
-				CASE WHEN (L.ysnDocumentsApproved = 1 
-						AND L.dtmDocumentsApproved IS NOT NULL
-						AND ((L.dtmDocumentsApproved > L.dtmArrivedInPort OR L.dtmArrivedInPort IS NULL)
-						AND (L.dtmDocumentsApproved > L.dtmCustomsReleased OR L.dtmCustomsReleased IS NULL))) 
-						THEN 'Documents Approved'
-					WHEN (L.ysnCustomsReleased = 1) THEN 'Customs Released'
-					WHEN (L.ysnArrivedInPort = 1) THEN 'Arrived in Port'
-					ELSE 'Delivered' END
-			WHEN 7 THEN 
-				CASE WHEN (ISNULL(L.strBookingReference, '') <> '') THEN 'Booked'
-						ELSE 'Shipping Instruction Created' END
-			WHEN 8 THEN 'Partial Shipment Created'
-			WHEN 9 THEN 'Full Shipment Created'
-			WHEN 10 THEN 'Cancelled'
-			WHEN 11 THEN 'Invoiced'
-			WHEN 12 THEN 'Rejected'
-			ELSE '' END COLLATE Latin1_General_CI_AS
+		,LGSS.strShipmentStatus
 		,strTransportationMode = CASE L.intTransportationMode
 			WHEN 1 THEN 'Truck'
 			WHEN 2 THEN 'Ocean Vessel'
@@ -270,6 +227,7 @@ SELECT   L.intLoadId
 		,Item.strMarketValuation
 FROM tblLGLoadDetail LD
 JOIN tblLGLoad L ON L.intLoadId = LD.intLoadId
+JOIN vyuLGShipmentStatus LGSS ON LGSS.intLoadId = L.intLoadId
 LEFT JOIN tblLGGenerateLoad GLoad ON GLoad.intGenerateLoadId = L.intGenerateLoadId
 LEFT JOIN tblSMCompanyLocation PCL ON PCL.intCompanyLocationId = LD.intPCompanyLocationId
 LEFT JOIN tblSMCompanyLocation SCL ON SCL.intCompanyLocationId = LD.intSCompanyLocationId
