09227e4d5185ede413c4bf383ac745076f7e2591
Jonathan Valenzuela <valenzuela.jonathan@outlook.com>
2022-09-12 15:02:16 +0800

    MFG-4637

--- i21Database/dbo/Views/vyuMFGetSalesOrder.sql
+++ i21Database/dbo/Views/vyuMFGetSalesOrder.sql
@@ -1,12 +1,22 @@
 ﻿CREATE VIEW [dbo].[vyuMFGetSalesOrder]
 AS 
-Select sh.intSalesOrderId,sh.strSalesOrderNumber,sh.dtmDueDate,c.strName AS strCustomerName,cl.strLocationName,
-sd.intSalesOrderDetailId,
-i.intItemId,i.strItemNo,i.strDescription,sd.dblQtyOrdered AS dblOrderedQty,sd.intItemUOMId,um.strUnitMeasure AS strUOM
-From tblSOSalesOrder sh Join tblSOSalesOrderDetail sd on sh.intSalesOrderId=sd.intSalesOrderId 
-Join tblICItem i on sd.intItemId=i.intItemId
-Join tblICItemUOM iu on sd.intItemUOMId=iu.intItemUOMId
-Join tblICUnitMeasure um on iu.intUnitMeasureId=um.intUnitMeasureId
-Join tblSMCompanyLocation cl on sh.intCompanyLocationId=cl.intCompanyLocationId
-Left Join vyuARCustomer c on sh.intEntityCustomerId=c.[intEntityId] 
-Where sh.strOrderStatus NOT IN ('Closed','Cancelled','Short Closed')
+SELECT sh.intSalesOrderId
+	 , sh.strSalesOrderNumber
+	 , sh.dtmDueDate
+	 , c.strName AS strCustomerName
+	 , cl.strLocationName
+	 , sd.intSalesOrderDetailId
+	 , i.intItemId
+	 , i.strItemNo
+	 , i.strDescription
+	 , ISNULL(sd.dblQtyOrdered, 0) AS dblOrderedQty
+	 , sd.intItemUOMId
+	 , um.strUnitMeasure AS strUOM
+FROM tblSOSalesOrder sh 
+JOIN tblSOSalesOrderDetail sd ON sh.intSalesOrderId = sd.intSalesOrderId 
+JOIN tblICItem i ON sd.intItemId = i.intItemId
+JOIN tblICItemUOM iu ON sd.intItemUOMId = iu.intItemUOMId
+JOIN tblICUnitMeasure um ON iu.intUnitMeasureId = um.intUnitMeasureId
+JOIN tblSMCompanyLocation cl ON sh.intCompanyLocationId = cl.intCompanyLocationId
+LEFT JOIN vyuARCustomer c ON sh.intEntityCustomerId = c.[intEntityId] 
+WHERE sh.strOrderStatus NOT IN ('Closed', 'Cancelled', 'Short Closed')
