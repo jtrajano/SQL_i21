6e83614c5854778d3935954b5b00216cc54e3568
Chris Salvante <chris.salvante@irely.com>
2022-09-13 00:33:04 +0800

    SM-5702

--- i21Database/Scripts/Post-Deployment/SM/DefaultData/4_StartingNumbers.sql
+++ i21Database/Scripts/Post-Deployment/SM/DefaultData/4_StartingNumbers.sql
@@ -1721,6 +1721,15 @@ GO
 			,[ysnEnable]			= 1
 			,[intConcurrencyId]		= 1
 	WHERE NOT EXISTS (SELECT TOP 1 1 FROM tblSMStartingNumber WHERE strTransactionType = N'Change Account Category' AND [strModule] = 'General Ledger')
+	UNION ALL
+	SELECT	[intStartingNumberId]	= 179
+				,[strTransactionType]	= N'Transfer Settlements'
+				,[strPrefix]			= N'TSTR-'
+				,[intNumber]			= 1
+				,[strModule]			= 'Ticket Management'
+				,[ysnEnable]			= 1
+				,[intConcurrencyId]		= 1
+	WHERE NOT EXISTS (SELECT TOP 1 1 FROM tblSMStartingNumber WHERE strTransactionType = N'Adjust Settlements' AND [strModule] = 'Ticket Management')
 	--Make sure to check with 19.1 and lower version. 142 is the last number
 
 
