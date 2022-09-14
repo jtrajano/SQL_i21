9ebe09d02a010cb5905bad549351d69f84ee05e6
Chris Salvante <chris.salvante@irely.com>
2022-09-14 15:44:24 +0800

    Revert "FRM-10440"

--- i21PreDeployment/Scripts/Pre-Deployment/PreDeployment.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/PreDeployment.sql
@@ -62,6 +62,7 @@
 :r .\4_SM\1810_Fix_GL_Account.sql
 :r .\4_SM\11_RemoveDuplicateTaxCodeUnderTaxGroup.sql
 :r .\4_SM\12_MigrateRecentlyViewed.sql
+:r .\4_SM\2010_TemporaryDecryptPasswords.sql
 
 -- GL
 :r .\6_GL\1_1410_to_1420.sql
