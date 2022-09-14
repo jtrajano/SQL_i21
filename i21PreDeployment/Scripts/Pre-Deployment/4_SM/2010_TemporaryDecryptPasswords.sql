af9236612a074b16fd75a71e7cbceac6725c1669
Chris Salvante <chris.salvante@irely.com>
2022-09-14 15:44:24 +0800

    Revert "FRM-10440"

---
+++ i21PreDeployment/Scripts/Pre-Deployment/4_SM/2010_TemporaryDecryptPasswords.sql
@@ -0,0 +1,19 @@
+﻿GO
+	PRINT N'START TEMPORARY DECRYPT PASSWORDS'
+	BEGIN
+		
+		UPDATE	tblEMEntitySMTPInformation
+		SET		strPassword = dbo.fnAESDecryptASym(strPassword)
+		WHERE	dbo.fnAESDecryptASym(strPassword) IS NOT NULL
+
+		UPDATE	tblSMInterCompany
+		SET		strPassword = dbo.fnAESDecryptASym(strPassword)
+		WHERE	dbo.fnAESDecryptASym(strPassword) IS NOT NULL
+
+		UPDATE	tblRMConnection
+		SET		strPassword = dbo.fnAESDecryptASym(strPassword)
+		WHERE	dbo.fnAESDecryptASym(strPassword) IS NOT NULL
+
+	END
+	PRINT N'END TEMPORARY DECRYPT PASSWORDS'
+GO
