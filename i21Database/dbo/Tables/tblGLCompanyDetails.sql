790e4a78087265a3d4541dd7fc6f1a34794d259f
Jeffrey Trajano <jeffrey.trajano@irely.com>
2022-09-15 01:41:20 +0800

    GL-9516

--- i21Database/dbo/Tables/tblGLCompanyDetails.sql
+++ i21Database/dbo/Tables/tblGLCompanyDetails.sql
@@ -1,9 +1,11 @@
 
 CREATE TABLE [dbo].[tblGLCompanyDetails](
-	[intAccountSegmentId] [int],
+	[intCompanyDetailsId] [int] IDENTITY(1,1),
+	[intAccountSegmentId][int],
 	[strCompanyName] [nvarchar](50) NULL,
 	[strFEIN] [nvarchar](10) NULL,
 	[strAddress] [nvarchar](300) NULL,
-	CONSTRAINT [PK_tblGLCompanyDetails] PRIMARY KEY CLUSTERED ([intAccountSegmentId] ASC)
+	[intConcurrencyId] INT NULL,
+	CONSTRAINT [PK_tblGLCompanyDetails] PRIMARY KEY CLUSTERED ([intCompanyDetailsId] ASC)
 ) 
 GO
