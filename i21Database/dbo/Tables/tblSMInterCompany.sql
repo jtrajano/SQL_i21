09f4039fa7d34cf0476e35edcc5a6e3f7cad4156
Chris Salvante <chris.salvante@irely.com>
2022-09-14 15:44:24 +0800

    Revert "FRM-10440"

--- i21Database/dbo/Tables/tblSMInterCompany.sql
+++ i21Database/dbo/Tables/tblSMInterCompany.sql
@@ -6,7 +6,6 @@
 	[strDatabaseName] nvarchar(max),
 	[strUserName] nvarchar(max),
 	[strPassword] nvarchar(max),
-	[ysnIsPasswordEncrypted] BIT DEFAULT 0 NOT NULL,
 	[intConcurrencyId] int default(0),
 	constraint [PK_dbo.tblSMInterCompany] PRIMARY KEY CLUSTERED ([intInterCompanyId] ASC)
   )
