3d0c32683b9c3a265141f1e3c4c75e2155a4d72a
Allen Marasigan <54519457+jaumarasigan@users.noreply.github.com>
2022-09-15 16:28:41 +0800

    LG-3627 Added allocation status view

---
+++ i21Database/dbo/Views/vyuLGAllocationStatus.sql
@@ -0,0 +1,36 @@
+﻿CREATE VIEW [dbo].[vyuLGAllocationStatus]
+AS
+	SELECT 
+		intAllocationHeaderId,
+		strAllocationStatus = CASE 
+								WHEN dblPContractAllocatedQty = dblPDetailQuantity THEN 'Allocated'
+								WHEN dblPContractAllocatedQty < dblPDetailQuantity THEN 'Partially Allocated' END,
+		strAllocationNumber,
+		dblAllocatedQuantity = dblPContractAllocatedQty,
+		strPurchaseContractNumber,
+		strSalesContractNumber
+	FROM vyuLGAllocatedContracts
+
+	UNION ALL
+
+	SELECT 
+		intAllocationHeaderId = NULL,
+		strAllocationStatus = 'Unallocated',
+		strAllocationNumber = NULL,
+		dblAllocatedQuantity,
+		strPurchaseContractNumber = CASE WHEN intPurchaseSale = 1 THEN strContractNumber ELSE NULL END,
+		strSalesContractNumber = CASE WHEN intPurchaseSale = 2 THEN strContractNumber ELSE NULL END 
+	FROM vyuLGAllocationOpenContracts WHERE dblAllocatedQuantity = 0
+
+	UNION ALL
+
+	SELECT
+		intAllocationHeaderId = NULL,
+		strAllocationStatus = 'Reserved',
+		strAllocationNumber = NULL,
+		dblAllocatedQuantity = 0,
+		strPurchaseContractNumber = CASE WHEN intPurchaseSale = 1 THEN strContractNumber ELSE NULL END,
+		strSalesContractNumber = CASE WHEN intPurchaseSale = 2 THEN strContractNumber ELSE NULL END 
+	FROM tblLGReservation R
+	LEFT JOIN tblCTContractDetail CD ON R.intContractDetailId = CD.intContractDetailId
+	LEFT JOIN tblCTContractHeader CH ON CD.intContractHeaderId = CH.intContractHeaderId
