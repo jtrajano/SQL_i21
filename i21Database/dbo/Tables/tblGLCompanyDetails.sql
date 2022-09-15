5803ec95f0f92fb674727080d86c072d9446d24b
Jeffrey Trajano <jeffrey.trajano@irely.com>
2022-09-15 18:18:02 +0800

    GL-9508 Check Printing by Company

--- i21Database/dbo/Tables/tblGLCompanyDetails.sql
+++ i21Database/dbo/Tables/tblGLCompanyDetails.sql
@@ -2,9 +2,9 @@
 CREATE TABLE [dbo].[tblGLCompanyDetails](
 	[intCompanyDetailsId] [int] IDENTITY(1,1),
 	[intAccountSegmentId][int],
-	[strCompanyName] [nvarchar](50) NULL,
-	[strFEIN] [nvarchar](10) NULL,
-	[strAddress] [nvarchar](300) NULL,
+	[strCompanyName] [nvarchar](50) COLLATE Latin1_General_CI_AS NULL,
+	[strFEIN] [nvarchar](10) COLLATE Latin1_General_CI_AS NULL,
+	[strAddress] [nvarchar](300) COLLATE Latin1_General_CI_AS NULL,
 	[intConcurrencyId] INT NULL,
 	CONSTRAINT [PK_tblGLCompanyDetails] PRIMARY KEY CLUSTERED ([intCompanyDetailsId] ASC)
 ) 
