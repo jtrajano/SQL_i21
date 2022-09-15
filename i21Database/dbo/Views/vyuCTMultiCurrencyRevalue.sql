a26add30d58c53cb973d3571b7be7394030b3ea6
Bryan-PC <013.bryan@gmail.com>
2022-09-15 23:49:31 +0800

    CT-7799 v2

--- i21Database/dbo/Views/vyuCTMultiCurrencyRevalue.sql
+++ i21Database/dbo/Views/vyuCTMultiCurrencyRevalue.sql
@@ -1,5 +1,4 @@
-﻿
-CREATE VIEW [dbo].[vyuCTMultiCurrencyRevalue]
+﻿Create VIEW [dbo].[vyuCTMultiCurrencyRevalue]
 
 AS 
 
@@ -14,14 +13,14 @@ AS
 			,strTicket				=	'' COLLATE Latin1_General_CI_AS 
 			,strContractNumber		=	CH.strContractNumber
 			,strItemId				=	IM.strItemNo
-			,dblQuantity			=	CD.dblQuantity
+			,dblQuantity			=	CASE WHEN t.strStatus = 'Partially Priced' THEN t.dblQuantity ELSE CD.dblBalance END
 			,dblUnitPrice			=	CD.dblCashPrice
 			,dblAmount				=	CD.dblTotalCost
 			,intCurrencyId			=	CD.intCurrencyId
-			,intForexRateType		=	CD.intHistoricalRateTypeId
+			,intForexRateType		=	CD.intRateTypeId
 			,strForexRateType		=	RT.strCurrencyExchangeRateType
-			,dblForexRate			=	CD.dblHistoricalRate
-			,dblHistoricAmount		=	CD.dblTotalCost * CD.dblHistoricalRate
+			,dblForexRate			=	CD.dblRate
+			,dblHistoricAmount		=	CASE WHEN t.strStatus = 'Partially Priced' THEN t.dblFinalPrice ELSE CD.dblTotalCost END * CD.dblRate
 			,dblNewForexRate		=	0
 			,dblNewAmount			=	0
 			,dblUnrealizedDebitGain =	0
@@ -37,11 +36,11 @@ AS
 	JOIN	tblCTContractType				CT	ON	CT.intContractTypeId				=	CH.intContractTypeId
 	JOIN	tblEMEntity						EY	ON	EY.intEntityId						=	CH.intEntityId
 	JOIN	tblICCommodity					CY	ON	CY.intCommodityId					=	CH.intCommodityId
-	JOIN	tblSMCompanyLocation			CL	ON	CL.intCompanyLocationId				=	CD.intCompanyLocationId			
-	LEFT	JOIN	tblICItem						IM	ON	IM.intItemId						=	CD.intItemId					
-	LEFT	JOIN	tblICCategory					CG	ON	CG.intCategoryId					=	IM.intCategoryId				
-	LEFT	JOIN	tblSMCurrencyExchangeRateType	RT	ON	RT.intCurrencyExchangeRateTypeId	=	CD.intHistoricalRateTypeId				
-	LEFT	JOIN	tblSMLineOfBusiness				LB	ON 	LB.intLineOfBusinessId				=	CG.intLineOfBusinessId
+	JOIN	tblSMCompanyLocation			CL	ON	CL.intCompanyLocationId				=	CD.intCompanyLocationId			LEFT
+	JOIN	tblICItem						IM	ON	IM.intItemId						=	CD.intItemId					LEFT
+	JOIN	tblICCategory					CG	ON	CG.intCategoryId					=	IM.intCategoryId				LEFT 
+	JOIN	tblSMCurrencyExchangeRateType	RT	ON	RT.intCurrencyExchangeRateTypeId	=	CD.intRateTypeId				LEFT
+	JOIN	tblSMLineOfBusiness				LB	ON 	LB.intLineOfBusinessId				=	CG.intLineOfBusinessId
 	LEFT JOIN (
 		select 
 		fspm.dblLastSettle, cmm.intCommodityId, sp.intFutureMarketId , fspm.intFutureMonthId, sp.dtmPriceDate 
@@ -57,7 +56,42 @@ AS
 		where
 		sp.strPricingType = 'Mark to Market' COLLATE Latin1_General_CS_AS
 		and sp.intCommodityMarketId = cmm.intCommodityMarketId
-
-
 	) LS on LS.intCommodityId = CH.intCommodityId and LS.intFutureMarketId = CD.intFutureMarketId and LS.intFutureMonthId = CD.intFutureMonthId
+
+	OUTER APPLY  (
+		SELECT 		intContractDetailId		=	PF.intContractDetailId
+			,strStatus				=	CASE WHEN ISNULL(PF.[dblTotalLots],0)-ISNULL(SUM(CASE WHEN (T.intPriceFixationId) IS NOT  NULL THEN 0 ELSE PFD.dblNoOfLots END),0) = 0 
+												THEN 'Fully Priced' 
+												WHEN ISNULL(SUM(CASE WHEN (T.intPriceFixationId) IS NOT  NULL THEN 0 ELSE PFD.dblNoOfLots END),0) = 0 THEN 'Unpriced'
+												ELSE 'Partially Priced' 
+										END		COLLATE Latin1_General_CI_AS
+			,dblFinalPrice			=	PF.dblFinalPrice
+			,dblQuantity			=	SUM(ISNULL(PFD.dblQuantity,0))
+				
+		FROM		tblCTPriceFixation			PF 	WITH (NOLOCK)
+		LEFT JOIN tblCTPriceFixationDetail PFD on PFD.intPriceFixationId = PF.intPriceFixationId
+		LEFT JOIN (
+			
+									SELECT PFD.intPriceFixationId, MAX(PFD.intPriceFixationDetailId) intPriceFixationDetailId
+												FROM
+									tblCTContractDetail cd
+									join tblCTContractHeader ch
+										on ch.intContractHeaderId = cd.intContractHeaderId
+									join tblCTPriceFixation pf
+										on pf.intContractHeaderId = ch.intContractHeaderId
+										and isnull(pf.intContractDetailId,0) = (case when ch.ysnMultiplePriceFixation = 1 then isnull(pf.intContractDetailId,0) else cd.intContractDetailId end)
+									left join tblCTPriceFixationDetail PFD on PFD.intPriceFixationId = pf.intPriceFixationId
+									join  tblSMTransaction t on t.intRecordId = pf.intPriceContractId and t.intScreenId = 119 and t.strApprovalStatus in 	('Waiting for Approval', 'Waiting for Submit')
+									group By PFD.intPriceFixationId
+		) T on T.intPriceFixationId = PF.intPriceFixationId  and T.intPriceFixationDetailId = PFD.intPriceFixationDetailId
+		where PF.intContractDetailId = CD.intContractDetailId
+		GROUP BY
+					PF.intContractDetailId, 
+					PF.[dblTotalLots] ,
+					PF.dblFinalPrice
+
+	) t
 GO
+
+
+
