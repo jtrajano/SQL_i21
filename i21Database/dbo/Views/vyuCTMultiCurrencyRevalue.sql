07aed2ec4b637224ee486b8f8552d3c7b7103318
Bryan-PC <013.bryan@gmail.com>
2022-09-15 14:33:49 +0800

    CT-7799

--- i21Database/dbo/Views/vyuCTMultiCurrencyRevalue.sql
+++ i21Database/dbo/Views/vyuCTMultiCurrencyRevalue.sql
@@ -1,4 +1,5 @@
-﻿CREATE VIEW [dbo].[vyuCTMultiCurrencyRevalue]
+﻿
+Create VIEW [dbo].[vyuCTMultiCurrencyRevalue]
 
 AS 
 
@@ -13,14 +14,14 @@ AS
 			,strTicket				=	'' COLLATE Latin1_General_CI_AS 
 			,strContractNumber		=	CH.strContractNumber
 			,strItemId				=	IM.strItemNo
-			,dblQuantity			=	CD.dblQuantity
+			,dblQuantity			=	CASE WHEN t.strStatus = 'Partially Priced' THEN t.dblQuantity ELSE CD.dblQuantity END
 			,dblUnitPrice			=	CD.dblCashPrice
 			,dblAmount				=	CD.dblTotalCost
 			,intCurrencyId			=	CD.intCurrencyId
 			,intForexRateType		=	CD.intRateTypeId
 			,strForexRateType		=	RT.strCurrencyExchangeRateType
 			,dblForexRate			=	CD.dblRate
-			,dblHistoricAmount		=	CD.dblTotalCost * CD.dblRate
+			,dblHistoricAmount		=	CASE WHEN t.strStatus = 'Partially Priced' THEN t.dblFinalPrice ELSE CD.dblTotalCost END * CD.dblRate
 			,dblNewForexRate		=	0
 			,dblNewAmount			=	0
 			,dblUnrealizedDebitGain =	0
@@ -34,8 +35,27 @@ AS
 	JOIN	tblCTContractType				CT	ON	CT.intContractTypeId				=	CH.intContractTypeId
 	JOIN	tblEMEntity						EY	ON	EY.intEntityId						=	CH.intEntityId
 	JOIN	tblICCommodity					CY	ON	CY.intCommodityId					=	CH.intCommodityId
-	JOIN	tblSMCompanyLocation			CL	ON	CL.intCompanyLocationId				=	CD.intCompanyLocationId			LEFT
-	JOIN	tblICItem						IM	ON	IM.intItemId						=	CD.intItemId					LEFT
-	JOIN	tblICCategory					CG	ON	CG.intCategoryId					=	IM.intCategoryId				LEFT 
-	JOIN	tblSMCurrencyExchangeRateType	RT	ON	RT.intCurrencyExchangeRateTypeId	=	CD.intRateTypeId				LEFT
-	JOIN	tblSMLineOfBusiness				LB	ON 	LB.intLineOfBusinessId				=	CG.intLineOfBusinessId
+	JOIN	tblSMCompanyLocation			CL	ON	CL.intCompanyLocationId				=	CD.intCompanyLocationId			
+	LEFT	JOIN	tblICItem						IM	ON	IM.intItemId						=	CD.intItemId					
+	LEFT	JOIN	tblICCategory					CG	ON	CG.intCategoryId					=	IM.intCategoryId				
+	LEFT	JOIN	tblSMCurrencyExchangeRateType	RT	ON	RT.intCurrencyExchangeRateTypeId	=	CD.intHistoricalRateTypeId				
+	LEFT	JOIN	tblSMLineOfBusiness				LB	ON 	LB.intLineOfBusinessId				=	CG.intLineOfBusinessId
+	LEFT JOIN (
+		select 
+		fspm.dblLastSettle, cmm.intCommodityId, sp.intFutureMarketId , fspm.intFutureMonthId, sp.dtmPriceDate 
+		from
+		tblRKFuturesSettlementPrice sp
+		join tblRKCommodityMarketMapping cmm on cmm.intFutureMarketId = sp.intFutureMarketId
+		join tblRKFutSettlementPriceMarketMap fspm on fspm.intFutureSettlementPriceId = sp.intFutureSettlementPriceId
+		join (
+			select max(dtmPriceDate) dtmPriceDate, intFutureMarketId
+			from tblRKFuturesSettlementPrice
+			GROUP BY intFutureMarketId
+		) mx on mx.intFutureMarketId = sp.intFutureMarketId and mx.dtmPriceDate = sp.dtmPriceDate
+		where
+		sp.strPricingType = 'Mark to Market' COLLATE Latin1_General_CS_AS
+		and sp.intCommodityMarketId = cmm.intCommodityMarketId
+
+
+	) LS on LS.intCommodityId = CH.intCommodityId and LS.intFutureMarketId = CD.intFutureMarketId and LS.intFutureMonthId = CD.intFutureMonthId
+GO
