34c156aaa10eafb61187eb070324c017f1c0e149
Mark Daniel Marmeto <mark.marmeto@irely.com>
2022-09-15 14:06:59 +0800

    GL-9509
    (cherry picked from commit 99992c912d720e9d1b75e210ebd27c22f74f3e11)

--- i21Database/dbo/Tables/tblGLLedgerAccount.sql
+++ i21Database/dbo/Tables/tblGLLedgerAccount.sql
@@ -3,6 +3,7 @@
 	[intLedgerAccountId]	INT IDENTITY(1, 1) NOT NULL,
 	[intLedgerId]			INT NOT NULL,
 	[intAccountId]			INT NOT NULL,
+	[ysnRequireSubledger]	BIT DEFAULT(0) NOT NULL,
     [intConcurrencyId]		INT DEFAULT(1) NOT NULL,
 
     CONSTRAINT [PK_tblGLLedgerAccount] PRIMARY KEY CLUSTERED ([intLedgerAccountId] ASC),
