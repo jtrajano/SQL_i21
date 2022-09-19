cd02854fcef465162b1734d156544b52bffdc463
Feb Montefrio <feb.montefrio@irely.com>
2022-09-19 18:25:37 +0800

    [B] IC-10721
    # Conflicts:
    #	i21Database/dbo/Stored Procedures/uspICPostCostAdjustmentRetroactiveLot.sql
    
    # Conflicts:
    #	i21Database/dbo/Stored Procedures/uspICPostCostAdjustmentRetroactiveLot.sql

--- i21Database/dbo/Stored Procedures/uspICPostCostAdjustmentRetroactiveLot.sql
+++ i21Database/dbo/Stored Procedures/uspICPostCostAdjustmentRetroactiveLot.sql
@@ -296,7 +296,8 @@ BEGIN
 	
 	-- Negative stocks 
 	UNION ALL 
-	SELECT	t.intInventoryTransactionId
+	SELECT	DISTINCT 
+			t.intInventoryTransactionId
 			,2 
 	FROM	tblICInventoryLotOut cbOut INNER JOIN tblICInventoryLot cb 
 				ON cbOut.intInventoryLotId = cb.intInventoryLotId
@@ -307,7 +308,17 @@ BEGIN
 				AND t.intTransactionId = cbSource.intTransactionId
 				AND t.intTransactionDetailId = cbSource.intTransactionDetailId
 				AND t.intLotId = cbSource.intLotId
+				AND ABS(t.dblQty) = ABS(cbOut.dblQty)
 				AND t.intInTransitSourceLocationId IS NULL 
+	 UNION ALL 
+	SELECT	t.intInventoryTransactionId 
+			,2	
+	FROM	tblICInventoryLot cb INNER JOIN (
+				tblICInventoryLotOut cbOut INNER JOIN tblICInventoryTransaction t 
+					ON cbOut.intInventoryTransactionId = t.intInventoryTransactionId
+					AND cbOut.intRevalueLotId IS NOT NULL 					
+			)
+				ON cb.intInventoryLotId = cbOut.intRevalueLotId		
 	WHERE	cb.intItemId = @intItemId
 			AND cb.intItemLocationId = @intItemLocationId
 			AND cb.intTransactionId = @intSourceTransactionId
@@ -315,7 +326,6 @@ BEGIN
 			AND cb.strTransactionId = @strSourceTransactionId
 			AND ISNULL(cb.ysnIsUnposted, 0) = 0 
 			AND cb.intLotId = ISNULL(@intLotId, cb.intLotId) 
-			AND cbOut.intRevalueLotId IS NOT NULL 	
 END 
 
 -- Remember the original cost from the cost bucket
