f49bd02c5ac0864b1cb3279ff1dcbd393fc5de97
Jeffrey Trajano <jeffrey.trajano@irely.com>
2022-09-19 16:54:45 +0800

    GL-9536 Restrict GJ to General

--- i21Database/Scripts/Post-Deployment/GL/DefaultData/1e_AccountCategory.sql
+++ i21Database/Scripts/Post-Deployment/GL/DefaultData/1e_AccountCategory.sql
@@ -455,7 +455,7 @@ GO
 		
 		--FOR GL ACCOUNT COMBO BOX FILTERING
 		UPDATE tblGLAccountCategory SET ysnGLRestricted  = 
-			case when strAccountCategory IN('Cash Account','AP Account','AR Account','Inventory','Vendor Prepayments') THEN 1 ELSE 0 END
+			CASE WHEN strAccountCategory 'General' THEN 0 ELSE 1 END
 		
 		-- FOR AP ACCOUNT COMBO BOX FILTERING
 		UPDATE tblGLAccountCategory SET ysnAPRestricted = CASE WHEN strAccountCategory IN('AP Account','AR Account', 'Cash Account', 'Inventory', 'AP Clearing', ' Inventory In-Transit', 'Inventory Adjustment', 'Vendor Prepayments')
