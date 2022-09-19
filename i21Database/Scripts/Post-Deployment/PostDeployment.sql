fcc0319197ae8da480116e78e0e9b71c94614e9d
Chris Salvante <chris.salvante@irely.com>
2022-09-19 17:01:24 +0800

    SM-5709

--- i21Database/Scripts/Post-Deployment/PostDeployment.sql
+++ i21Database/Scripts/Post-Deployment/PostDeployment.sql
@@ -109,6 +109,7 @@ print 'BEGIN POST DEPLOYMENT'
 :r .\SM\2210_RemoveOldPowerBIData.sql
 :r .\SM\2210_EncryptPasswords.sql
 :r .\SM\2210_EncryptPasswords2.sql
+:r .\SM\2210_AttachmentsFileTypesFix.sql
 
 
 -- Canned Report
