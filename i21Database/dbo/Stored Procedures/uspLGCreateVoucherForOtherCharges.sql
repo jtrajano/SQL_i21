ab89cff5afef5d2d800f271be615c234ad07b5ab
Arman Arenas <arman.arenas@irely.com>
2022-09-20 22:32:13 +0800

    LG-3641 fixed account Id used for uspLGCreateVoucherForOtherCharges

--- i21Database/dbo/Stored Procedures/uspLGCreateVoucherForOtherCharges.sql
+++ i21Database/dbo/Stored Procedures/uspLGCreateVoucherForOtherCharges.sql
@@ -344,8 +344,8 @@ BEGIN TRY
 				,[ysnSubCurrency] = CUR.ysnSubCurrency
 				,[intSubCurrencyCents] = CUR.intCent
 				,[intAccountId] = CASE WHEN (CP.ysnEnableAccrualsForOutbound = 1 AND L.intPurchaseSale = 2 AND VDD.ysnAccrue = 1 AND VDD.intVendorEntityId IS NOT NULL) 
-					THEN dbo.fnGetItemGLAccount(VDD.intItemId, CD.intCompanyLocationId, 'AP Clearing') 
-					ELSE dbo.fnGetItemGLAccount(VDD.intItemId, CD.intCompanyLocationId, 'Other Charge Expense') END
+					THEN dbo.fnGetItemGLAccount(VDD.intItemId, ItemLoc.intItemLocationId, 'AP Clearing') 
+					ELSE dbo.fnGetItemGLAccount(VDD.intItemId, ItemLoc.intItemLocationId, 'Other Charge Expense') END
 				,[strBillOfLading] = L.strBLNumber
 				,[ysnReturn] = CAST(0 AS BIT)
 				,[ysnStage] = CAST(CASE WHEN (COC.ysnCreateOtherCostPayable = 1 AND CTC.intContractCostId IS NOT NULL) THEN 0 ELSE 1 END AS BIT)
@@ -363,6 +363,7 @@ BEGIN TRY
 							AND VDD.intItemId = CTC.intItemId
 							AND VDD.intVendorEntityId = CTC.intVendorId
 						) CTC
+				OUTER APPLY (SELECT TOP 1 intItemLocationId FROM tblICItemLocation IL WHERE intItemId = VDD.intItemId AND IL.intLocationId = CD.intCompanyLocationId) ItemLoc
 			WHERE VDD.intVendorEntityId = @intVendorEntityId
 
 			EXEC uspAPCreateVoucher 
@@ -506,7 +507,7 @@ BEGIN TRY
 					,[intCurrencyExchangeRateTypeId] = VDD.intCurrencyExchangeRateTypeId
 					,[ysnSubCurrency] = CUR.ysnSubCurrency
 					,[intSubCurrencyCents] = CUR.intCent
-					,[intAccountId] = CASE WHEN (CTC.intContractCostId IS NULL) THEN dbo.fnGetItemGLAccount(VDD.intItemId, CD.intCompanyLocationId, 'Other Charge Expense') ELSE VDD.intAccountId END
+					,[intAccountId] = CASE WHEN (CTC.intContractCostId IS NULL) THEN dbo.fnGetItemGLAccount(VDD.intItemId, ItemLoc.intItemLocationId, 'Other Charge Expense') ELSE VDD.intAccountId END
 					,[strBillOfLading] = L.strBLNumber
 					,[ysnReturn] = CAST(0 AS BIT)
 					,[ysnStage] = CAST(CASE WHEN (COC.ysnCreateOtherCostPayable = 1 AND CTC.intContractCostId IS NOT NULL) THEN 0 ELSE 1 END AS BIT)
@@ -523,6 +524,7 @@ BEGIN TRY
 							AND VDD.intItemId = CTC.intItemId
 							AND VDD.intVendorEntityId = CTC.intVendorId
 						) CTC
+					OUTER APPLY (SELECT TOP 1 intItemLocationId FROM tblICItemLocation IL WHERE intItemId = VDD.intItemId AND IL.intLocationId = CD.intCompanyLocationId) ItemLoc
 				WHERE VDD.intVendorEntityId = @intVendorEntityId AND VDD.ysnInventoryCost = 0
 			
 				EXEC uspAPCreateVoucher 
