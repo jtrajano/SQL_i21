559cf1003ca84ea1926fea360de05a570f725ad0
Kenneth Lew Lagunero <kenneth.lagunero@irely.com>
2022-09-21 19:19:33 +0800

    SC-4717

--- i21Database/dbo/Stored Procedures/uspSCProcessTicketWithBasisContract.sql
+++ i21Database/dbo/Stored Procedures/uspSCProcessTicketWithBasisContract.sql
@@ -43,7 +43,7 @@ BEGIN TRY
 					,@intLocationId = @intLocationId
 					,@dtmScaleDate = @dtmScaleDate
 					,@intUserId = @intUserId
-					,@intBillId = @intCreatedTransactionId
+					,@intBillId = @intCreatedTransactionId OUTPUT
 		END
 		ELSE
 		BEGIN
