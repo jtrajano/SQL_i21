f2a8ab98a9397aa4abaa34f6aa969dd1ca169330
Dom Caoile <dom.caoile.02@gmail.com>
2022-09-21 22:16:43 +0800

    FRD-1863

---
+++ i21Database/dbo/Tables/tblFRSubLedger.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[tblFRSubLedger](
+	[intFRSubLedgerId]			INT				IDENTITY (1, 1) NOT NULL,
+	[intReportId]				INT				NOT NULL,
+	[intLedgerDetailId]			INT				NOT NULL Default 0,
+	[intConcurrencyId]			INT,
+	CONSTRAINT [PK_tblFRSubLedger] PRIMARY KEY (intReportId,intLedgerDetailId),
+	CONSTRAINT [FK_tblFRSubLedger] FOREIGN KEY (intReportId) REFERENCES tblFRReport(intReportId)
+);
