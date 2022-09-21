dcb6c2cd97b8231bddb80e35ea8abe5292e8ac27
Feb Montefrio <feb.montefrio@irely.com>
2022-09-21 06:43:37 +0000

    [B] IC-10747

--- i21Database/dbo/Stored Procedures/uspICDeleteTransactionLink.sql
+++ i21Database/dbo/Stored Procedures/uspICDeleteTransactionLink.sql
@@ -1,100 +1,134 @@
-CREATE PROCEDURE [dbo].uspICDeleteTransactionLink(@TransactionLinks udtICTransactionLinks READONLY)
+CREATE PROCEDURE [dbo].uspICDeleteTransactionLink(
+	@TransactionLinks udtICTransactionLinks READONLY
+)
 AS
 BEGIN
 
-DELETE Links
-FROM tblICTransactionLinks Links
-INNER JOIN @TransactionLinks Link
-ON 
-Links.intSrcId = Link.intSrcId 
-AND 
-Links.strSrcTransactionNo = Link.strSrcTransactionNo 
-AND 
-Links.strSrcModuleName = Link.strSrcModuleName 
-AND 
-Links.strSrcTransactionType = Link.strSrcTransactionType 
-AND
-Links.intDestId = Link.intDestId 
-AND 
-Links.strDestTransactionNo = Link.strDestTransactionNo 
-AND 
-Links.strDestModuleName = Link.strDestModuleName 
-AND 
-Links.strDestTransactionType = Link.strDestTransactionType
+	-- Delete the specific transaction link. 
+	DELETE d 
+	FROM 
+	(
+		SELECT Links.intTransactionLinkId
+		FROM 
+			tblICTransactionLinks Links INNER JOIN @TransactionLinks Link
+				ON Links.intSrcId = Link.intSrcId 
+				AND Links.strSrcTransactionNo = Link.strSrcTransactionNo 
+				AND Links.strSrcModuleName = Link.strSrcModuleName 
+				AND Links.strSrcTransactionType = Link.strSrcTransactionType 	
+				AND Links.intDestId = Link.intDestId 
+				AND Links.strDestTransactionNo = Link.strDestTransactionNo 
+				AND Links.strDestModuleName = Link.strDestModuleName 
+				AND Links.strDestTransactionType = Link.strDestTransactionType
+	) list 
+	INNER JOIN tblICTransactionLinks d
+		ON list.intTransactionLinkId = d.intTransactionLinkId			   
 
+	-- Delete the source node
+	DELETE d
+	FROM (
+			SELECT 
+				SourceNodes.intTransactionNodeId
+			FROM 
+				tblICTransactionNodes SourceNodes INNER JOIN @TransactionLinks Link
+					ON SourceNodes.intTransactionId = Link.intSrcId 
+					AND SourceNodes.strTransactionNo = Link.strSrcTransactionNo 
+					AND SourceNodes.strModuleName = Link.strSrcModuleName 
+					AND SourceNodes.strTransactionType = Link.strSrcTransactionType
+				OUTER APPLY (
+					SELECT TOP 1 
+						Links.intTransactionLinkId 
+					FROM tblICTransactionLinks Links
+					WHERE
+						Links.intSrcId = Link.intSrcId 
+						AND Links.strSrcTransactionNo = Link.strSrcTransactionNo 
+						AND Links.strSrcModuleName = Link.strSrcModuleName 
+						AND Links.strSrcTransactionType = Link.strSrcTransactionType 
+				) Links 
+			WHERE 
+				Links.intTransactionLinkId IS NULL
+		) list 
+		INNER JOIN tblICTransactionNodes d
+			ON list.intTransactionNodeId = d.intTransactionNodeId
+	
+	-- Delete the source node
+	DELETE d
+	FROM (
+			SELECT 
+				SourceNodes.intTransactionNodeId
+			FROM 
+				tblICTransactionNodes SourceNodes INNER JOIN @TransactionLinks Link
+					ON SourceNodes.intTransactionId = Link.intSrcId 
+					AND SourceNodes.strTransactionNo = Link.strSrcTransactionNo 
+					AND SourceNodes.strModuleName = Link.strSrcModuleName 
+					AND SourceNodes.strTransactionType = Link.strSrcTransactionType
+				OUTER APPLY (
+					SELECT TOP 1 
+						Links.intTransactionLinkId 
+					FROM tblICTransactionLinks Links
+					WHERE
+						Links.intDestId = Link.intSrcId 
+						AND Links.strDestTransactionNo = Link.strSrcTransactionNo 
+						AND Links.strDestModuleName = Link.strSrcModuleName 
+						AND Links.strDestTransactionType = Link.strSrcTransactionType 
+				) Links 
+			WHERE 
+				Links.intTransactionLinkId IS NULL
+		) list 
+		INNER JOIN tblICTransactionNodes d
+			ON list.intTransactionNodeId = d.intTransactionNodeId
 
+	-- Delete the destination node
+	DELETE d
+	FROM (
+			SELECT  
+				DestinationNode.intTransactionNodeId
+			FROM 
+				tblICTransactionNodes DestinationNode INNER JOIN @TransactionLinks Link
+				ON DestinationNode.intTransactionId = Link.intDestId 
+				AND DestinationNode.strTransactionNo = Link.strDestTransactionNo 
+				AND DestinationNode.strModuleName = Link.strDestModuleName 
+				AND DestinationNode.strTransactionType = Link.strDestTransactionType
+			OUTER APPLY (			
+				SELECT TOP 1 
+					Links.intTransactionLinkId
+				FROM tblICTransactionLinks Links
+				WHERE 
+					Links.intDestId = Link.intDestId 
+					AND Links.strDestTransactionNo = Link.strDestTransactionNo 
+					AND Links.strDestModuleName = Link.strDestModuleName 
+					AND Links.strDestTransactionType = Link.strDestTransactionType
+			) Links
+			WHERE 
+				Links.intTransactionLinkId IS NULL
+		) list 		
+		INNER JOIN tblICTransactionNodes d
+			ON list.intTransactionNodeId = d.intTransactionNodeId
 
-DELETE SourceNodes
-FROM tblICTransactionNodes SourceNodes
-INNER JOIN @TransactionLinks Link
-ON 
-SourceNodes.intTransactionId = Link.intSrcId 
-AND 
-SourceNodes.strTransactionNo = Link.strSrcTransactionNo 
-AND 
-SourceNodes.strModuleName = Link.strSrcModuleName 
-AND 
-SourceNodes.strTransactionType = Link.strSrcTransactionType
-LEFT JOIN tblICTransactionLinks Links
-ON 
-(
-Links.intSrcId = Link.intSrcId 
-AND 
-Links.strSrcTransactionNo = Link.strSrcTransactionNo 
-AND 
-Links.strSrcModuleName = Link.strSrcModuleName 
-AND 
-Links.strSrcTransactionType = Link.strSrcTransactionType 
-)
-OR
-(
-Links.intDestId = Link.intSrcId 
-AND 
-Links.strDestTransactionNo = Link.strSrcTransactionNo 
-AND 
-Links.strDestModuleName = Link.strSrcModuleName 
-AND 
-Links.strDestTransactionType = Link.strSrcTransactionType
-)
-WHERE 
-Links.intTransactionLinkId IS NULL
-
-
-DELETE DestinationNode
-FROM tblICTransactionNodes DestinationNode
-INNER JOIN @TransactionLinks Link
-ON 
-DestinationNode.intTransactionId = Link.intDestId 
-AND 
-DestinationNode.strTransactionNo = Link.strDestTransactionNo 
-AND 
-DestinationNode.strModuleName = Link.strDestModuleName 
-AND 
-DestinationNode.strTransactionType = Link.strDestTransactionType
-LEFT JOIN tblICTransactionLinks Links
-ON 
-(
-Links.intDestId = Link.intDestId 
-AND 
-Links.strDestTransactionNo = Link.strDestTransactionNo 
-AND 
-Links.strDestModuleName = Link.strDestModuleName 
-AND 
-Links.strDestTransactionType = Link.strDestTransactionType
-)
-OR
-(
-Links.intSrcId = Link.intDestId 
-AND 
-Links.strSrcTransactionNo = Link.strDestTransactionNo 
-AND 
-Links.strSrcModuleName = Link.strDestModuleName 
-AND 
-Links.strSrcTransactionType = Link.strDestTransactionType 
-)
-WHERE 
-Links.intTransactionLinkId IS NULL
+	-- Delete the destination node
+	DELETE d
+	FROM (
+			SELECT  
+				DestinationNode.intTransactionNodeId
+			FROM 
+				tblICTransactionNodes DestinationNode INNER JOIN @TransactionLinks Link
+					ON DestinationNode.intTransactionId = Link.intDestId 
+					AND DestinationNode.strTransactionNo = Link.strDestTransactionNo 
+					AND DestinationNode.strModuleName = Link.strDestModuleName 
+					AND DestinationNode.strTransactionType = Link.strDestTransactionType
+				OUTER APPLY(
+					SELECT TOP 1 
+						Links.intTransactionLinkId
+					FROM tblICTransactionLinks Links
+					WHERE
+						Links.intSrcId = Link.intDestId 
+						AND Links.strSrcTransactionNo = Link.strDestTransactionNo 
+						AND Links.strSrcModuleName = Link.strDestModuleName 
+						AND Links.strSrcTransactionType = Link.strDestTransactionType 
+				) Links
+			WHERE 
+				Links.intTransactionLinkId IS NULL
+		) list 
+		INNER JOIN tblICTransactionNodes d
+			ON list.intTransactionNodeId = d.intTransactionNodeId
 
 END
-
-GO
