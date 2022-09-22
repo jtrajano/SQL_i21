e496e1576b01430b438c5f05c6ee0a90d36413bf
Jayson Ramos <jayson.ramos@iRely.com>
2022-09-22 10:27:31 +0800

    CT-7841

--- i21Database/dbo/Stored Procedures/uspCTLogSummary.sql
+++ i21Database/dbo/Stored Procedures/uspCTLogSummary.sql
@@ -4032,7 +4032,6 @@ BEGIN TRY
 						SET dblQty = CASE WHEN @strProcess = 'Price Fixation' THEN (SELECT dblQty * - 1 FROM @cbLogPrev)
 										ELSE @TotalPriced END
 							, intPricingTypeId = CASE WHEN ISNULL(@truePreviousPricingType, 0) = 1 AND ISNULL(@truePricingTypeId, 0) <> 1 THEN @truePricingTypeId ELSE 1 END
-							, dblFutures = @dblPreviousFutures
 						EXEC uspCTLogContractBalance @cbLogSpecific, 0
 					END
 					IF (ISNULL(@TotalHTA, 0) <> 0)
@@ -4373,7 +4372,6 @@ BEGIN TRY
 						UPDATE @cbLogSpecific
 						SET dblQty = 1
 							, intPricingTypeId = 1
-							, dblFutures = @dblPreviousFutures
 						EXEC uspCTLogContractBalance @cbLogSpecific, 0
 					END
 				END
