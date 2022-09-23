6d0df13c50203680fc7301044e14ce62aa9c8772
Mark Daniel Marmeto <mark.marmeto@irely.com>
2022-09-23 11:19:18 +0800

    GL-9531

--- i21Database/dbo/Stored Procedures/uspGLBookEntries.sql
+++ i21Database/dbo/Stored Procedures/uspGLBookEntries.sql
@@ -240,4 +240,91 @@ BEGIN
 		);
 END;
 
+
+
+
+--=====================================================================================================================================
+-- 	UPSERT DATA TO THE FRD SUMMARY TABLE
+---------------------------------------------------------------------------------------------------------------------------------------
+
+BEGIN
+	MERGE
+	INTO	dbo.tblGLPosted
+	WITH	(HOLDLOCK)
+	AS		gl_summary
+	USING (
+				SELECT	intAccountId
+						,dtmDate = dbo.fnRemoveTimeOnDate(GLEntries.dtmDate)
+						,strCode
+						,dblDebit = SUM(CASE WHEN @ysnPost = 1 THEN Debit.Value ELSE Credit.Value * -1 END)
+						,dblCredit = SUM(CASE WHEN @ysnPost = 1 THEN Credit.Value ELSE Debit.Value * -1 END)
+						,dblDebitForeign = SUM(CASE WHEN @ysnPost = 1 THEN DebitForeign.Value ELSE CreditForeign.Value * -1 END)
+						,dblCreditForeign = SUM(CASE WHEN @ysnPost = 1 THEN CreditForeign.Value ELSE DebitForeign.Value * -1 END)
+						,dblDebitUnit = SUM(CASE WHEN @ysnPost = 1 THEN DebitUnit.Value ELSE CreditUnit.Value * -1 END)
+						,dblCreditUnit = SUM(CASE WHEN @ysnPost = 1 THEN CreditUnit.Value ELSE DebitUnit.Value * -1 END)	
+						,intCurrencyId					
+						,intLedgerId = ISNULL(intLedgerId,0)
+				FROM	@GLEntries GLEntries
+						CROSS APPLY dbo.fnGetDebit(ISNULL(GLEntries.dblDebit, 0) - ISNULL(GLEntries.dblCredit, 0)) Debit
+						CROSS APPLY dbo.fnGetCredit(ISNULL(GLEntries.dblDebit, 0) - ISNULL(GLEntries.dblCredit, 0))  Credit
+						CROSS APPLY dbo.fnGetDebit(ISNULL(CASE WHEN GLEntries.[dblExchangeRate] = 1 THEN GLEntries.dblDebit ELSE GLEntries.dblDebitForeign END, 0)
+												- ISNULL(CASE WHEN GLEntries.[dblExchangeRate] = 1 THEN GLEntries.dblCredit ELSE GLEntries.dblCreditForeign END, 0)) DebitForeign
+						CROSS APPLY dbo.fnGetCredit(ISNULL(CASE WHEN GLEntries.[dblExchangeRate] = 1 THEN GLEntries.dblDebit ELSE GLEntries.dblDebitForeign END, 0)
+												- ISNULL(CASE WHEN GLEntries.[dblExchangeRate] = 1 THEN GLEntries.dblCredit ELSE GLEntries.dblCreditForeign END, 0))  CreditForeign
+						CROSS APPLY dbo.fnGetDebit(ISNULL(GLEntries.dblDebitUnit, 0) - ISNULL(GLEntries.dblCreditUnit, 0)) DebitUnit
+						CROSS APPLY dbo.fnGetCredit(ISNULL(GLEntries.dblDebitUnit, 0) - ISNULL(GLEntries.dblCreditUnit, 0))  CreditUnit
+				GROUP BY intAccountId, dbo.fnRemoveTimeOnDate(GLEntries.dtmDate), strCode, intCurrencyId, intLedgerId
+	) AS Source_Query  
+		ON gl_summary.intAccountId = Source_Query.intAccountId
+		AND gl_summary.strCode = Source_Query.strCode 
+		AND gl_summary.intCurrencyId = Source_Query.intCurrencyId
+		AND FLOOR(CAST(gl_summary.dtmDate AS FLOAT)) = FLOOR(CAST(Source_Query.dtmDate AS FLOAT))
+
+	
+
+	-- Update an existing gl summary record
+	WHEN MATCHED THEN 
+		UPDATE 
+		SET		dblDebit = gl_summary.dblDebit + Source_Query.dblDebit 
+				,dblCredit = gl_summary.dblCredit + Source_Query.dblCredit 
+				,dblDebitForeign = gl_summary.dblDebitForeign + Source_Query.dblDebitForeign
+				,dblCreditForeign = gl_summary.dblCreditForeign + Source_Query.dblCreditForeign
+				,dblCreditUnit = gl_summary.dblCreditUnit + Source_Query.dblCreditUnit
+				,dblDebitUnit = gl_summary.dblDebitUnit + Source_Query.dblDebitUnit
+				,intCurrencyId = gl_summary.intCurrencyId 
+				,intLedgerId = gl_summary.intLedgerId
+				,intConcurrencyId = ISNULL(intConcurrencyId, 0) + 1
+
+	-- Insert a new gl summary record 
+	WHEN NOT MATCHED  THEN 
+		INSERT (
+			intAccountId
+			,dtmDate
+			,dblDebit
+			,dblCredit
+			,dblDebitForeign
+			,dblCreditForeign
+			,dblDebitUnit
+			,dblCreditUnit
+			,strCode
+			,intCurrencyId
+			,intLedgerId
+			,intConcurrencyId
+		)
+		VALUES (
+			Source_Query.intAccountId
+			,Source_Query.dtmDate
+			,Source_Query.dblDebit
+			,Source_Query.dblCredit
+			,Source_Query.dblDebitForeign
+			,Source_Query.dblCreditForeign
+			,Source_Query.dblDebitUnit
+			,Source_Query.dblCreditUnit
+			,Source_Query.strCode
+			,Source_Query.intCurrencyId
+			,Source_Query.intLedgerId
+			,1
+		);
+END;
+
 RETURN 0
