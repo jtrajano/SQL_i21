e42f10ba415fa991b21e7e151a9dbd5aff8a9ea7
Jase Rueda <jase.rueda@iRely.com>
2022-09-26 16:00:53 +0800

    CS-73

---
+++ i21Database/dbo/Functions/fnSTGetCurrentBusinessDay.sql
@@ -0,0 +1,16 @@
+﻿CREATE FUNCTION [dbo].[fnSTGetCurrentBusinessDay] 
+(
+	@intStoreId AS INT
+)
+RETURNS DATETIME
+AS BEGIN
+
+    DECLARE			@dtmCurrentBusinessDay DATETIME
+
+    SELECT			TOP 1 @dtmCurrentBusinessDay =  ISNULL(DATEADD(DAY, 1, dtmCheckoutDate), GETDATE()) 
+	FROM			tblSTCheckoutHeader
+	WHERE			intStoreId = @intStoreId AND strCheckoutStatus = 'Posted'
+	ORDER BY		dtmCheckoutDate DESC
+
+    RETURN			@dtmCurrentBusinessDay
+END
