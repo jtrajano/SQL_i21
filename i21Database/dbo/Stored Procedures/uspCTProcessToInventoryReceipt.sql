2e4a4025d4a251d230a99af03c4ec72a1dbff333
Jayson Ramos <jayson.ramos@iRely.com>
2022-09-26 16:46:02 +0800

    CT-7854

--- i21Database/dbo/Stored Procedures/uspCTProcessToInventoryReceipt.sql
+++ i21Database/dbo/Stored Procedures/uspCTProcessToInventoryReceipt.sql
@@ -136,8 +136,10 @@ AS
 				intBookId					=	CD.intBookId,
 				intSubBookId				=	CD.intSubBookId,
 				intLoadReceive				=	ISNULL(CD.dblBalanceLoad,0)		-	ISNULL(CD.dblScheduleLoad,0),
-				dblUnitRetail				=	CD.dblCashPrice
-
+				dblUnitRetail				=	CD.dblCashPrice,
+				strTaxPoint					=	case when isnull(CD.ysnTaxOverride,0) = 0 then null else CD.strTaxPoint end,
+				intTaxLocationId			=	case when isnull(CD.ysnTaxOverride,0) = 0 then null else CD.intTaxLocationId end,
+				intTaxGroupId				=	case when isnull(CD.ysnTaxOverride,0) = 0 then null else CD.intTaxGroupId end
 		FROM	tblCTContractDetail			CD	
 		JOIN	tblCTContractHeader			CH	ON	CH.intContractHeaderId	=	CD.intContractHeaderId
 		--CT-7100 (ECOM commented this line for ECOM) --JOIN tblICItemUOM IU ON IU.intItemId = CD.intItemId AND IU.ysnStockUnit = 1		
