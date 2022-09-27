b1c9ee71084834b6947f36662706258892eb8f08
Edsel Roldan Magura <edsel.magura@iRely.com>
2022-09-27 17:18:00 +0800

    HD-4229

---
+++ i21Database/dbo/Stored Procedures/uspHDSyncTimeOffRequest.sql
@@ -0,0 +1,56 @@
+﻿CREATE PROCEDURE [dbo].[uspHDSyncTimeOffRequest]
+(
+	@TimeEntryPeriodDetailId INT
+)
+AS
+
+SET QUOTED_IDENTIFIER OFF  
+SET ANSI_NULLS ON  
+SET NOCOUNT ON  
+SET XACT_ABORT ON  
+SET ANSI_WARNINGS ON  
+
+BEGIN
+			--Start Sync Time Off Request
+
+		    DECLARE @intTimeEntryPeriodDetail INT = @TimeEntryPeriodDetailId,
+					@dtmDateFrom DATE = NULL,
+					@dtmDateTo DATE = NULL,
+					@strFiscalYear NVARCHAR(10) = NULL
+
+			SELECT TOP 1 @dtmDateFrom		 = a.dtmBillingPeriodStart 
+						,@dtmDateTo			 = a.dtmBillingPeriodEnd
+						,@strFiscalYear		 = b.strFiscalYear
+			FROM tblHDTimeEntryPeriodDetail a
+					INNER JOIN tblHDTimeEntryPeriod b
+			ON a.intTimeEntryPeriodId = b.intTimeEntryPeriodId
+			WHERE intTimeEntryPeriodDetailId = @intTimeEntryPeriodDetail
+
+			DECLARE @intEntityEmployeeId INT
+
+			DECLARE EmployeeLoop CURSOR 
+			  LOCAL STATIC READ_ONLY FORWARD_ONLY
+			FOR 
+
+			SELECT intEntityEmployeeId 
+			FROM tblPRTimeOffRequest TimeOffRequest
+			WHERE TimeOffRequest.dtmDateFrom <= @dtmDateTo AND 
+				  TimeOffRequest.dtmDateFrom >= @dtmDateFrom
+			GROUP BY intEntityEmployeeId
+
+			OPEN EmployeeLoop
+			FETCH NEXT FROM EmployeeLoop INTO @intEntityEmployeeId
+			WHILE @@FETCH_STATUS = 0
+			BEGIN 
+  
+				EXEC [dbo].[uspHDGenerateTimeOffRequest] @intEntityEmployeeId
+
+				FETCH NEXT FROM EmployeeLoop INTO @intEntityEmployeeId
+			END
+			CLOSE EmployeeLoop
+			DEALLOCATE EmployeeLoop
+
+			--End Sync Time Off Request
+		
+END
+GO
