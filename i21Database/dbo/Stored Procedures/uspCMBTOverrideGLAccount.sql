5ac8d8b7adfa4aa818ab4e8e3e142d65e7907b0e
Jeffrey Trajano <jeffrey.trajano@irely.com>
2022-09-28 13:13:06 +0800

    GL-9008

--- i21Database/dbo/Stored Procedures/uspCMBTOverrideGLAccount.sql
+++ i21Database/dbo/Stored Procedures/uspCMBTOverrideGLAccount.sql
@@ -101,13 +101,8 @@ BEGIN
     WHERE GL.intAccountId <> @intAccountId
     GROUP BY GL.intAccountId, strAccountId
 
-    IF @ysnOverrideLocation = 1 
-        SELECT @newStrAccountId= dbo.fnGLGetOverrideAccount(3,@strAccountId,@strAccountId1)
-    
-    IF @ysnOverrideCompany = 1
-        SELECT @newStrAccountId= dbo.fnGLGetOverrideAccount(6,@strAccountId,@newStrAccountId)
+    SELECT @newStrAccountId = dbo.fnGLGetOverrideAccountByAccount( @intAccountId,@intAccountIdLoop, @ysnOverrideLocation,0, @ysnOverrideCompany)
         
-
     IF @newStrAccountId = ''
     BEGIN
 	    SET @msg += '<li>Overriding ' + @strAccountId1 + ' encountered an unknow error.</li>'
