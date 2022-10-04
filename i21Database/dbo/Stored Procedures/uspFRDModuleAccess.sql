15915f83b7ce25552d3f1df819c107c286bdafa9
Dom Caoile <dom.caoile.02@gmail.com>
2022-10-04 09:11:22 +0800

    FRD-1861

---
+++ i21Database/dbo/Stored Procedures/uspFRDModuleAccess.sql
@@ -0,0 +1,37 @@
+﻿CREATE PROCEDURE  [dbo].[uspFRDModuleAccess]          
+ @intUserRoleId AS INT,        
+ @intEntityId AS INT,        
+ @intCompanyLocationId AS INT,    
+ @strMenuName AS NVARCHAR(MAX),                                
+ @hasAccess AS INT OUTPUT  
+AS          
+        
+SET QUOTED_IDENTIFIER OFF                  
+SET ANSI_NULLS ON                  
+SET NOCOUNT ON                  
+SET XACT_ABORT ON               
+        
+        
+BEGIN        
+ DECLARE @intCompanyUserRole INT        
+ DECLARE @intHasAccess INT  = 0       
+  
+ set @intCompanyUserRole   = (  
+  SELECT COUNT(0) FROM tblSMUserSecurityCompanyLocationRolePermission WHERE intEntityId = @intEntityId and intCompanyLocationId = intCompanyLocationId  
+ )  
+ IF @intCompanyUserRole = 0  
+  BEGIN   
+   SET @intHasAccess = (SELECT ysnVisible FROM tblSMUserRoleMenu WHERE intUserRoleId = @intUserRoleId AND intMenuId = (select TOP 1 intMenuID FROM tblSMMasterMenu WHERE strMenuName = @strMenuName))  
+     
+  END  
+ ELSE  
+  BEGIN   
+   SET @intHasAccess = (SELECT ysnVisible FROM tblSMUserRoleMenu WHERE intUserRoleId = @intCompanyUserRole AND intMenuId = (select TOP 1 intMenuID FROM tblSMMasterMenu WHERE strMenuName = @strMenuName))  
+  END  
+  
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------   
+  
+--RETURN      
+ SET @hasAccess = @intHasAccess  
+END 
+
