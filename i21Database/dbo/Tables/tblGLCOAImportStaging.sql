52c1d5ea52b5061cf0ba34a01467fb92e16e12c1
Mark Daniel Marmeto <mark.marmeto@irely.com>
2022-10-05 16:07:47 +0800

    GL-9566

---
+++ i21Database/dbo/Tables/tblGLCOAImportStaging.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[tblGLCOAImportStaging]
+(
+	[intImportStagingId]	INT IDENTITY(1, 1),
+	[strAccountPartition]	NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NULL,  
+	[intPartitionType]		INT NOT NULL,  
+	[strPartitionGroup]		NVARCHAR(40) COLLATE Latin1_General_CI_AS NULL,
+	[strGUID]				NVARCHAR(40) COLLATE Latin1_General_CI_AS NULL,
+	[strRawString]			NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NULL,
+	[intLineNumber]			INT DEFAULT (1) NOT NULL
+)
