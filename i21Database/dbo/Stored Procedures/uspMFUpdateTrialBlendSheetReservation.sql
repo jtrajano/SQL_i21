0619804257ae3662379a2e300be06f4bd41d5e5c
Prem Selladurai <prem.selladurai@irely.com>
2022-10-05 23:42:21 +0530

    MFG-4570
    MFG-4570

---
+++ i21Database/dbo/Stored Procedures/uspMFUpdateTrialBlendSheetReservation.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE uspMFUpdateTrialBlendSheetReservation (@intWorkOrderId INT)
+AS
+BEGIN
+	UPDATE LI
+	SET dblReservedQtyInTBS = IsNULL(LI.dblReservedQtyInTBS, 0) + WI.dblQuantity
+	FROM dbo.tblMFLotInventory LI
+	JOIN dbo.tblMFWorkOrderInputLot WI ON WI.intLotId = LI.intLotId
+	WHERE WI.intWorkOrderId = @intWorkOrderId
+END
