ba19ecc7b36a5e8e3e05b5003da10486b928077d
Kenneth Lew Lagunero <kenneth.lagunero@irely.com>
2022-10-07 10:27:12 +0800

    TM-3532
    # Conflicts:
    #	i21Database/dbo/Stored Procedures/uspTMCreateOrder.sql
    
    # Conflicts:
    #	i21Database/dbo/Stored Procedures/uspTMContractScheduleQty.sql

--- i21Database/dbo/Stored Procedures/uspTMCreateOrder.sql
+++ i21Database/dbo/Stored Procedures/uspTMCreateOrder.sql
@@ -40,99 +40,261 @@ BEGIN
 	-- DELETE IF THERE IS EXISTING DISPATCH RECORD
 	DELETE tblTMOrder WHERE intDispatchId = @intDispatchId
 
-	IF(@intContractDetailId IS NOT NULL) 
+	IF(@strSource = 'TM - Generate Order')
 	BEGIN
-		DECLARE @dblBalance DECIMAL(18,6) = NULL,
-			@dblCashPrice DECIMAL(18,6) = NULL
-
-		SELECT @dblBalance = B.dblBalance, @dblCashPrice = B.dblCashPrice
-		FROM tblCTContractHeader A
-		INNER JOIN vyuCTContractHeaderNotMapped H
-			ON A.intContractHeaderId = H.intContractHeaderId
-		INNER JOIN tblCTContractDetail B
-			ON A.intContractHeaderId = B.intContractHeaderId
-		WHERE B.intContractDetailId = @intContractDetailId
-
-		IF(@dblQuantity > @dblBalance)
+		IF(ISNULL(@intContractDetailId,0) > 0)
 		BEGIN
-
-			DECLARE @dblRemainingQty NUMERIC(18, 6) = NULL,
-				@dblItemPrice NUMERIC(18, 6) = NULL,
-				@strPricing NVARCHAR(200) = NULL
-
-			SET @dblRemainingQty = @dblQuantity - @dblBalance
-
-			SET @dblTotal = @dblBalance * @dblPrice
-
 			INSERT INTO tblTMOrder (
-				intDispatchId,
-				intSiteId,
-				intItemId,
-				strOrderNumber,
-				strPricingMethod,
-				intContractDetailId,
-				dblQuantity,
-				dblPrice,
-				dblTotal,
-				strSource,
-				dtmTransactionDate
-			)
-			VALUES(
-				@intDispatchId,
-				@intSiteId,
-				@intItemId,
-				@strOrderNumber,
-				@strPricingMethod,
-				@intContractDetailId,
-				@dblBalance,
-				@dblPrice,
-				@dblTotal,
-				@strSource,
-				GETDATE()
-			)
+					intDispatchId,
+					intSiteId,
+					intItemId,
+					strOrderNumber,
+					strPricingMethod,
+					intContractDetailId,
+					dblQuantity,
+					dblPrice,
+					dblTotal,
+					strSource,
+					dtmTransactionDate
+				)
+			SELECT 
+					A.intDispatchID
+					,A.intSiteID
+					,A.intProductID
+					,A.strOrderNumber
+					,A.strPricingMethod
+					,A.intContractId
+					,dblQuantity = (CASE WHEN ISNULL(A.dblMinimumQuantity, 0) = 0 THEN A.dblQuantity ELSE A.dblMinimumQuantity END) - A.dblOverageQty
+					,A.dblPrice
+					,A.dblTotal
+					,@strSource
+					,GETDATE()
+			FROM tblTMDispatch A
+			INNER JOIN tblCTContractDetail B
+				ON A.intContractId = B.intContractDetailId
+			WHERE intDispatchID = @intDispatchId
 
-			IF(@dblRemainingQty > 0)
+			---Overrage
+			INSERT INTO tblTMOrder (
+					intDispatchId,
+					intSiteId,
+					intItemId,
+					strOrderNumber,
+					strPricingMethod,
+					intContractDetailId,
+					dblQuantity,
+					dblPrice,
+					dblTotal,
+					strSource,
+					dtmTransactionDate
+					,ysnOverage
+				)
+			SELECT 
+					A.intDispatchID
+					,A.intSiteID
+					,A.intProductID
+					,A.strOrderNumber
+					,A.strPricingMethod
+					,A.intContractId
+					,dblQuantity = A.dblOverageQty
+					,dblPrice = A.dblOveragePrice
+					,A.dblTotal
+					,@strSource
+					,GETDATE()
+					,ysnOverage = 1
+			FROM tblTMDispatch A
+			WHERE intDispatchID = @intDispatchId
+				AND A.dblOverageQty > 0
+				AND A.dblOverageQty IS NOT NULL
+		END
+		ELSE
+		BEGIN
+			INSERT INTO tblTMOrder (
+					intDispatchId,
+					intSiteId,
+					intItemId,
+					strOrderNumber,
+					strPricingMethod,
+					intContractDetailId,
+					dblQuantity,
+					dblPrice,
+					dblTotal,
+					strSource,
+					dtmTransactionDate
+				)
+			SELECT 
+					A.intDispatchID
+					,A.intSiteID
+					,A.intProductID
+					,A.strOrderNumber
+					,A.strPricingMethod
+					,A.intContractId
+					,dblQuantity = CASE WHEN ISNULL(A.dblMinimumQuantity, 0) = 0 THEN A.dblQuantity ELSE A.dblMinimumQuantity END
+					,A.dblPrice
+					,A.dblTotal
+					,@strSource
+					,GETDATE()
+			FROM tblTMDispatch A
+			WHERE intDispatchID = @intDispatchId
+		END
+	END
+	ELSE
+	BEGIN
+		IF(@intContractDetailId IS NOT NULL) 
+		BEGIN
+			DECLARE @dblBalance DECIMAL(18,6) = NULL,
+				@dblCashPrice DECIMAL(18,6) = NULL
+
+			SELECT @dblBalance = B.dblBalance, @dblCashPrice = B.dblCashPrice
+			FROM tblCTContractHeader A
+			INNER JOIN vyuCTContractHeaderNotMapped H
+				ON A.intContractHeaderId = H.intContractHeaderId
+			INNER JOIN tblCTContractDetail B
+				ON A.intContractHeaderId = B.intContractHeaderId
+			WHERE B.intContractDetailId = @intContractDetailId
+
+			IF(@dblQuantity > @dblBalance)
 			BEGIN
-				DECLARE @intUOMId INT = NULL
 
-				--SELECT @dblItemPrice = dblRegularPrice FROM tblTMDispatch WHERE intDispatchID = @intDispatchId
-				
-				-- SELECT @intUOMId = intIssueUOMId 
-                -- FROM tblICItemLocation WHERE intItemId = @intItemId
-                -- AND intLocationId = @intLocationId
+				DECLARE @dblRemainingQty NUMERIC(18, 6) = NULL,
+					@dblItemPrice NUMERIC(18, 6) = NULL,
+					@strPricing NVARCHAR(200) = NULL
 
-				SELECT TOP 1 @intUOMId = intItemUOMId
-                                          FROM vyuARItemUOM
-                                          WHERE intItemId = @intItemId
-										  
-				EXEC dbo.uspARGetItemPrice @ItemUOMId = @intUOMId
-					,@ItemId =  @intItemId
-					,@CustomerId = @intCustomerId
-					,@LocationId = @intLocationId
-					,@Quantity = @dblRemainingQty
-					,@PricingLevelId = @intCompanyLocationPricingLevelId
-					,@InvoiceType = 'Tank Delivery'
-					,@ExcludeContractPricing = 1
-					,@Price = @dblItemPrice OUT
-					,@Pricing = @strPricing OUT
+				SET @dblRemainingQty = @dblQuantity - @dblBalance
 
-				-- IF(@dblItemPrice IS NULL)
-				-- BEGIN
-				-- 	SET @dblItemPrice = 0
-				-- 	SET @strPricingMethod = 'Regular'
-				-- END
-				-- ELSE IF(@strPricing = 'Standard Pricing')
-				-- BEGIN
-				-- 	SET @strPricingMethod = 'Regular'
-				-- END
-				-- ELSE
-				-- BEGIN
-				-- 	SET @strPricingMethod = 'Special'
-				-- END
+				SET @dblTotal = @dblBalance * @dblPrice
 
-				SET @dblTotal = @dblRemainingQty * @dblItemPrice
-						
 				INSERT INTO tblTMOrder (
+					intDispatchId,
+					intSiteId,
+					intItemId,
+					strOrderNumber,
+					strPricingMethod,
+					intContractDetailId,
+					dblQuantity,
+					dblPrice,
+					dblTotal,
+					strSource,
+					dtmTransactionDate
+				)
+				VALUES(
+					@intDispatchId,
+					@intSiteId,
+					@intItemId,
+					@strOrderNumber,
+					@strPricingMethod,
+					@intContractDetailId,
+					@dblBalance,
+					@dblPrice,
+					@dblTotal,
+					@strSource,
+					GETDATE()
+				)
+
+				IF(@dblRemainingQty > 0)
+				BEGIN
+					DECLARE @intUOMId INT = NULL
+
+					SELECT @dblItemPrice = dblRegularPrice FROM tblTMDispatch WHERE intDispatchID = @intDispatchId
+					
+					-- SELECT @intUOMId = intIssueUOMId 
+					-- FROM tblICItemLocation WHERE intItemId = @intItemId
+					-- AND intLocationId = @intLocationId
+
+					-- EXEC dbo.uspARGetItemPrice
+					--  @ItemUOMId = @intUOMId
+					-- ,@ItemId =  @intItemId
+					-- ,@CustomerId = @intCustomerId
+					-- ,@LocationId = @intLocationId
+					-- ,@Quantity = @dblRemainingQty
+					-- ,@PricingLevelId = @intCompanyLocationPricingLevelId
+					-- ,@InvoiceType = 'Tank Delivery'
+					-- ,@ExcludeContractPricing = 1
+					-- ,@Price = @dblItemPrice OUT
+					-- ,@Pricing = @strPricing OUT
+
+					-- IF(@dblItemPrice IS NULL)
+					-- BEGIN
+					-- 	SET @dblItemPrice = 0
+					-- 	SET @strPricingMethod = 'Regular'
+					-- END
+					-- ELSE IF(@strPricing = 'Standard Pricing')
+					-- BEGIN
+					-- 	SET @strPricingMethod = 'Regular'
+					-- END
+					-- ELSE
+					-- BEGIN
+					-- 	SET @strPricingMethod = 'Special'
+					-- END
+
+					SET @dblTotal = @dblRemainingQty * @dblItemPrice
+							
+					INSERT INTO tblTMOrder (
+						intDispatchId,
+						intSiteId,
+						intItemId,
+						strOrderNumber,
+						strPricingMethod,
+						dblQuantity,
+						dblPrice,
+						dblTotal,
+						strSource,
+						dtmTransactionDate,
+						ysnOverage
+					)
+					VALUES(
+						@intDispatchId,
+						@intSiteId,
+						@intItemId,
+						@strOrderNumber,
+						@strOriginalPricingMethod,
+						@dblRemainingQty,
+						@dblItemPrice,
+						@dblTotal,
+						@strSource,
+						GETDATE(),
+						1
+					)
+
+					UPDATE tblTMDispatch SET dblOverageQty = @dblRemainingQty, dblOveragePrice = @dblItemPrice
+					WHERE intDispatchID = @intDispatchId
+					
+				END
+			END
+			ELSE
+			BEGIN
+				INSERT INTO tblTMOrder (
+					intDispatchId,
+					intSiteId,
+					intItemId,
+					strOrderNumber,
+					strPricingMethod,
+					intContractDetailId,
+					dblQuantity,
+					dblPrice,
+					dblTotal,
+					strSource,
+					dtmTransactionDate
+				)
+				VALUES(
+					@intDispatchId,
+					@intSiteId,
+					@intItemId,
+					@strOrderNumber,
+					@strPricingMethod,
+					@intContractDetailId,
+					@dblQuantity,
+					@dblPrice,
+					@dblTotal,
+					@strSource,
+					GETDATE()
+				)
+			END
+
+		END
+		ELSE
+		BEGIN
+			INSERT INTO tblTMOrder (
 					intDispatchId,
 					intSiteId,
 					intItemId,
@@ -142,86 +304,22 @@ BEGIN
 					dblPrice,
 					dblTotal,
 					strSource,
-					dtmTransactionDate,
-					ysnOverage
+					dtmTransactionDate
 				)
 				VALUES(
 					@intDispatchId,
 					@intSiteId,
 					@intItemId,
 					@strOrderNumber,
-					@strOriginalPricingMethod,
-					@dblRemainingQty,
-					@dblItemPrice,
+					@strPricingMethod,
+					@dblQuantity,
+					@dblPrice,
 					@dblTotal,
 					@strSource,
-					GETDATE(),
-					1
+					GETDATE()
 				)
-
-				UPDATE tblTMDispatch SET dblOverageQty = @dblRemainingQty, dblOveragePrice = @dblItemPrice
-				WHERE intDispatchID = @intDispatchId
-				
-			END
 		END
-		ELSE
-		BEGIN
-			INSERT INTO tblTMOrder (
-				intDispatchId,
-				intSiteId,
-				intItemId,
-				strOrderNumber,
-				strPricingMethod,
-				intContractDetailId,
-				dblQuantity,
-				dblPrice,
-				dblTotal,
-				strSource,
-				dtmTransactionDate
-			)
-			VALUES(
-				@intDispatchId,
-				@intSiteId,
-				@intItemId,
-				@strOrderNumber,
-				@strPricingMethod,
-				@intContractDetailId,
-				@dblQuantity,
-				@dblPrice,
-				@dblTotal,
-				@strSource,
-				GETDATE()
-			)
-		END
-
 	END
-	ELSE
-	BEGIN
-		INSERT INTO tblTMOrder (
-				intDispatchId,
-				intSiteId,
-				intItemId,
-				strOrderNumber,
-				strPricingMethod,
-				dblQuantity,
-				dblPrice,
-				dblTotal,
-				strSource,
-				dtmTransactionDate
-			)
-			VALUES(
-				@intDispatchId,
-				@intSiteId,
-				@intItemId,
-				@strOrderNumber,
-				@strPricingMethod,
-				@dblQuantity,
-				@dblPrice,
-				@dblTotal,
-				@strSource,
-				GETDATE()
-			)
-	END
-
-
 END
+
+GO
