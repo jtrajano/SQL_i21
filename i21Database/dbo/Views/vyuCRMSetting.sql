fa6467efaea3875b82b3417d398d97886d29647c
DESKTOP-V0PRVO9\iRelyNUC30 <angelo.regalado@irely.com>
2022-10-07 14:51:33 +0800

    CRM-995

---
+++ i21Database/dbo/Views/vyuCRMSetting.sql
@@ -0,0 +1,15 @@
+﻿CREATE VIEW [dbo].[vyuCRMSetting]
+AS 
+SELECT
+	S.intCrmSettingId
+	,S.strSignatureFormat
+	,HC.intHubspotConfigId
+	,CF.ysnHubspotIntegration
+	,HC.strHsClientId
+	,HC.strHsClientSecret
+	,HC.strHsRedirectUrl
+	,HC.strHsRefreshToken
+FROM            
+	[dbo].[tblCRMSetting] AS S,
+    [dbo].[tblCRMHubspotConfig] AS HC,
+	[dbo].[tblSMCompanyPreference] AS CF
