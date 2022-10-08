9313a8f7b9112920b9a4d7953a5c176df76095c7
lawrence_zabala <lawrence.zabala@irely.com>
2022-10-08 16:05:55 +0800

    CT-7421

---
+++ i21Database/dbo/Tables/tblCTContractDetailImportHeader.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[tblCTContractDetailImportHeader]
+(
+	[intContractDetailImportHeaderId] INT IDENTITY NOT NULL, 
+	[guiUniqueId] UNIQUEIDENTIFIER NULL,
+    [dtmImportDate] DATETIME NULL, 
+    [strFileName] NVARCHAR(100) NULL, 
+    [intUserId] INT NULL, 
+    [intConcurrencyId] INT NOT NULL DEFAULT ((1)),     
+    CONSTRAINT [PK_tblCTContractDetailImportHeader] PRIMARY KEY ([intContractDetailImportHeaderId]) 
+)
