f78832b1b05354cb63a5e6ddd65007ff6b662382
Edsel Roldan Magura <edsel.magura@iRely.com>
2022-10-10 17:34:00 +0800

    HD-4245

---
+++ i21Database/dbo/Views/vyuHDProjectModuleSearch.sql
@@ -0,0 +1,55 @@
+﻿CREATE VIEW [dbo].[vyuHDProjectModuleSearch]
+AS 
+
+SELECT  intProjectModuleId			= ProjectModule.intProjectModuleId
+	   ,intModuleId				    = ProjectModule.intModuleId
+	   ,strModule				    = SMModule.strModule
+	   ,intProjectId				= ProjectModule.intProjectId
+	   ,strProjectName				= Project.strProjectName
+	   ,intProjectManagerId			= ProjectModule.intProjectManagerId
+	   ,strProjectManager			= ImplementationLead.strName
+	   ,strDataConversionExpert		= DataConversionExpert.strName
+	   ,strTrainer					= Trainer.strName
+	   ,strContact					= Contact.strName
+	   ,dblQuotedHours				= ISNULL(ProjectModule.dblQuotedHours, 0)
+	   ,intCustomerId				= Project.intCustomerId
+	   ,dblActualBillableHours		= ISNULL(ProjectTickets.dblActualHours, 0)
+	   ,dblHoursOverShort			= ISNULL(ProjectModule.dblQuotedHours,0) - ISNULL(ProjectTickets.dblActualHours,0)
+	   ,strPercentComplete			= CASE WHEN ISNULL(ProjectTickets.dblQuotedHours, 0) = 0
+											THEN '0%'
+										ELSE CONVERT(NVARCHAR(10), CONVERT(INT,ROUND(ProjectTickets.dblBillablehours / ProjectTickets.dblQuotedHours, 2) * 100)) + '%'
+									END
+
+FROM tblHDProjectModule ProjectModule
+		INNER JOIN tblHDProject Project
+ON ProjectModule.intProjectId = Project.intProjectId
+		INNER JOIN tblHDModule Module
+ON Module.intModuleId = ProjectModule.intModuleId
+		INNER JOIN tblSMModule SMModule
+ON SMModule.intModuleId = Module.intSMModuleId
+		LEFT JOIN tblEMEntity ImplementationLead
+ON ImplementationLead.intEntityId = ProjectModule.intProjectManagerId
+		LEFT JOIN tblEMEntity DataConversionExpert
+ON DataConversionExpert.intEntityId = ProjectModule.intDataConversionExpertId
+		LEFT JOIN tblEMEntity Trainer
+ON Trainer.intEntityId = ProjectModule.intTrainerId
+		LEFT JOIN vyuHDProjectCustomerContact Contact
+ON Contact.intEntityId = ProjectModule.intContactId
+		OUTER APPLY(
+		
+		SELECT dblActualHours		= SUM(ISNULL(ProjectTickets.dblActualHours, 0))
+			  ,dblQuotedHours		= SUM(ISNULL(ProjectTickets.dblQuotedHours, 0))
+			  ,dblBillablehours     = CASE WHEN ProjectTickets.intTicketStatusId = 2
+											THEN SUM(ISNULL(ProjectTickets.dblQuotedHours, 0))
+											ELSE SUM(ISNULL(ProjectTickets.dblActualHours, 0))
+									  END
+		FROM tblHDProjectTask ProjectTask
+				INNER JOIN vyuHDProjectTickets ProjectTickets
+		ON ProjectTickets.intTicketId = ProjectTask.intTicketId
+		WHERE ProjectTask.intProjectId = Project.intProjectId AND
+			  ProjectTickets.strModule = SMModule.strModule	
+		GROUP BY ProjectTickets.intTicketStatusId
+		) ProjectTickets
+WHERE Project.strType = 'HD' 
+
+GO
