7ecf5483d6e34e199afc9c37e6cb17c9be64a556
Kelvin Quinto <kelvin.quinto@irely.com>
2022-10-10 14:26:19 +0800

    AR-15489 AR-15492 AR-15498
    (cherry picked from commit 3a8d0e15b6a3fd5a21b9991a6eaa4cb55aa1a77b)
    (cherry picked from commit eb47c6b68f47674072aeafab4b2b0d7fce4452c5)

--- i21Database/dbo/Stored Procedures/uspARPopulateContractDetails.sql
+++ i21Database/dbo/Stored Procedures/uspARPopulateContractDetails.sql
@@ -149,7 +149,7 @@ WHERE ID.[intInventoryShipmentChargeId] IS NULL
 	AND (RI.[intInvoiceId] IS NULL OR (RI.[intInvoiceId] IS NOT NULL AND (ID.intLoadDetailId IS NULL OR ID.[intTicketId] IS NOT NULL)))
 	AND ((ID.ysnFromProvisional = 1 AND PI.ysnPosted = 0) OR ID.ysnFromProvisional = 0)
 	AND (ISNULL(W.strWhereFinalized, '') <> 'Destination' AND ISNULL(G.strWhereFinalized, '') <> 'Destination')
-	AND (ISNULL(T.intTicketType, 0) <> 6 AND ISNULL(T.intTicketTypeId, 0 ) <> 9 AND ISNULL(T.strInOutFlag,'') <> 'O')
+	AND (ISNULL(T.intTicketType, 0) <> 6 AND ISNULL(T.intTicketTypeId, 0 ) <> 9)
 	AND ID.intContractDetailId IS NOT NULL
 	AND ID.strSessionId = @strSessionId
 	AND ID.strType <> 'Service Charge'
@@ -244,7 +244,7 @@ IF NOT EXISTS(SELECT TOP 1 NULL FROM #TBLTOPROCESS)
 		  AND ID.intShipmentPurchaseSalesContractId IS NULL
 		  AND (I.intLoadDetailId IS NULL OR (I.intLoadDetailId IS NOT NULL AND ID.strPricing = 'Subsystem - Ticket Management'))
 		  AND (I.[strItemType] IS NOT NULL AND I.[strItemType] <> 'Other Charge')
-		  AND (ISNULL(T.intTicketType, 0) <> 6 AND ISNULL(T.intTicketTypeId, 0 ) <> 9 AND ISNULL(T.strInOutFlag,'') <> 'O')
+		  AND (ISNULL(T.intTicketType, 0) <> 6 AND ISNULL(T.intTicketTypeId, 0 ) <> 9)
 		  AND I.strSessionId = @strSessionId
 		GROUP BY I.[intInvoiceId], I.[intContractDetailId], I.[intContractHeaderId], I.[intItemUOMId], I.[intTicketId], ISNULL(S.intItemUOMId, ID.intItemUOMId), ID.[strPricing], ID.intInventoryShipmentItemId, I.strBatchId, I.strInvoiceNumber, I.strTransactionType, I.strItemNo,  I.dtmDate, RI.intInvoiceId, ID.intOriginalInvoiceDetailId, CD.intItemId, CD.intItemUOMId, CH.intEntityId, T.intTicketTypeId, T.intTicketType, T.strInOutFlag, CH.ysnLoad, CD.dblScheduleQty
 	END
