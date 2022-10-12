a18e73bd3f5441b66fab69f7841f40ce40a1254f
Dom Caoile <dom.caoile.02@gmail.com>
2022-10-12 16:38:27 +0800

    FRD-1861

--- i21Database/dbo/Stored Procedures/uspFRDModuleAccess.sql
+++ i21Database/dbo/Stored Procedures/uspFRDModuleAccess.sql
@@ -1,35 +1,50 @@
-﻿CREATE PROCEDURE  [dbo].[uspFRDModuleAccess]            
- @intUserRoleId AS INT,          
- @intEntityId AS INT,          
- @intCompanyLocationId AS INT,      
- @strMenuName AS NVARCHAR(MAX),                                  
- @hasAccess AS INT OUTPUT    
-AS            
-          
-SET QUOTED_IDENTIFIER OFF                    
-SET ANSI_NULLS ON                    
-SET NOCOUNT ON                    
-SET XACT_ABORT ON                 
-          
-          
-BEGIN          
- DECLARE @intCnt INT        
- DECLARE @intCompanyUserRole INT          
- DECLARE @intHasAccess INT  = 0         
-    
-	SET @intCnt   = (    
-		SELECT COUNT(0) FROM tblSMUserSecurityCompanyLocationRolePermission WHERE intEntityId = @intEntityId and intCompanyLocationId = @intCompanyLocationId    
-	)     
-	IF @intCompanyUserRole = 0    
-		BEGIN     
-			SET @intHasAccess = (SELECT ISNULL(ysnVisible,0) FROM tblSMUserRoleMenu WHERE intUserRoleId = @intUserRoleId AND intMenuId = (select TOP 1 intMenuID FROM tblSMMasterMenu WHERE strMenuName = @strMenuName))    
-		END    
-	ELSE    
-		BEGIN     
-			SET @intCompanyUserRole = (SELECT ISNULL(intUserRoleId,0) FROM tblSMUserSecurityCompanyLocationRolePermission WHERE intEntityId = @intEntityId and intCompanyLocationId = @intCompanyLocationId )
-			SET @intHasAccess = (SELECT ISNULL(ysnVisible,0) FROM tblSMUserRoleMenu WHERE intUserRoleId = @intCompanyUserRole AND intMenuId = (select TOP 1 intMenuID FROM tblSMMasterMenu WHERE strMenuName = @strMenuName))    
-		END    
-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------    
---RETURN        
- SET @hasAccess = @intHasAccess    
-END
+﻿
+CREATE PROCEDURE  [dbo].[uspFRDModuleAccess]                
+ @intUserRoleId AS INT,              
+ @intEntityId AS INT,              
+ @intCompanyLocationId AS INT,          
+ @strMenuName AS NVARCHAR(MAX),                                      
+ @hasAccess AS INT OUTPUT        
+AS                
+              
+SET QUOTED_IDENTIFIER OFF                        
+SET ANSI_NULLS ON                        
+SET NOCOUNT ON                        
+SET XACT_ABORT ON                     
+              
+              
+BEGIN              
+ DECLARE @intCnt INT            
+ DECLARE @intCompanyUserRole INT      
+ DECLARE @intControlId INT          
+ DECLARE @strPermission NVARCHAR(120) --COLLATE Latin1_General_CI_AS        
+ DECLARE @intHasAccess INT  = 0             
+        
+ SET @intCnt  = (        
+  SELECT COUNT(0) FROM tblSMUserSecurityCompanyLocationRolePermission WHERE intEntityId = @intEntityId and intCompanyLocationId = @intCompanyLocationId        
+ )    
+ SET @intControlId = (SELECT TOP 1 intControlId FROM tblSMControl WHERE strControlName LIKE '%Close Year%' AND intScreenId = (SELECT intScreenId FROM tblSMScreen WHERE strScreenName like '%FiscalYear%'))    
+         
+ IF @intCnt = 0        
+  BEGIN         
+   SET @strPermission = (SELECT strPermission FROM tblSMUserRoleControlPermission WHERE intControlId = @intControlId AND intUserRoleId = @intUserRoleId)  
+   IF @strPermission = 'Editable'  
+    BEGIN   
+     SET @intHasAccess = 1   
+    END   
+  END        
+ ELSE        
+  BEGIN         
+   SET @intCompanyUserRole = (SELECT ISNULL(intUserRoleId,0) FROM tblSMUserSecurityCompanyLocationRolePermission WHERE intEntityId = @intEntityId and intCompanyLocationId = @intCompanyLocationId )    
+   SET @strPermission = (SELECT strPermission FROM tblSMUserRoleControlPermission WHERE intControlId = @intControlId AND intUserRoleId = @intCompanyUserRole)  
+   IF @strPermission = 'Editable'  
+    BEGIN   
+     SET @intHasAccess = 1   
+    END   
+  END        
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------   
+  
+--RETURN            
+SET @hasAccess = @intHasAccess        
+END  
+  
