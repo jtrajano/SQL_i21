ded7ca74695e555d64f1a2df92d098b490c75252
Edsel Roldan Magura <edsel.magura@iRely.com>
2022-10-12 10:31:08 +0800

    HD-4269

---
+++ i21Database/dbo/Tables/tblHDDeploymentType.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[tblHDDeploymentType]
+(
+	[intDeploymentTypeId]					INT IDENTITY(1,1) NOT NULL,
+    [strDeploymentType]						NVARCHAR(100) COLLATE Latin1_General_CI_AS NOT NULL,
+	[intConcurrencyId]						[int] NOT NULL DEFAULT 1,
+	CONSTRAINT [PK_tblHDDeploymentType_intDeploymentTypeId] PRIMARY KEY CLUSTERED ([intDeploymentTypeId] ASC),
+    CONSTRAINT [UQ_tblHDDeploymentType_strDeploymentType] UNIQUE ([strDeploymentType])
+)
+
+GO
