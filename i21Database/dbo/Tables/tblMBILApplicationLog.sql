c730792ece720aef562b6e061a36419b201493e5
axelcasauran <axel.casauran@irely.com>
2022-10-13 18:35:59 +0800

    MBIL-999

---
+++ i21Database/dbo/Tables/tblMBILApplicationLog.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tblMBILApplicationLog]
+(
+	[intLogId] [int] IDENTITY NOT NULL,
+	[strEmailAddress] [nvarchar](150) COLLATE Latin1_General_CI_AS NULL, 	
+	[strAppVersion] [nvarchar](50) COLLATE Latin1_General_CI_AS NULL, 	
+	[strServerConnection] [nvarchar](250) COLLATE Latin1_General_CI_AS NULL, 
+	[strCompany] [nvarchar](50) COLLATE Latin1_General_CI_AS NULL,	
+	[dtmCreated] [datetime] NULL,	
+	[intConcurrencyId] [int] NULL,
+	CONSTRAINT [PK_tblMBILApplicationLog] PRIMARY KEY ([intLogId])
+)
