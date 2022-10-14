996d6eb935c5ad945d06d2dadc1e9e7a12b6c2ef
Jonathan Valenzuela <valenzuela.jonathan@outlook.com>
2022-10-14 13:35:50 +0800

    MFG-4664

--- i21Database/dbo/Stored Procedures/uspMFPostWorkOrder.sql
+++ i21Database/dbo/Stored Procedures/uspMFPostWorkOrder.sql
@@ -1313,6 +1313,7 @@ BEGIN TRY
 
 			UPDATE PL
 			SET PL.dblItemValue = CostAdj.dblNewValue
+			  , PL.dblFirstItemValue = CostAdj.dblNewValue
 			FROM @adjustedEntries CostAdj
 			JOIN dbo.tblMFWorkOrderProducedLot PL ON PL.intWorkOrderProducedLotId = CostAdj.intTransactionDetailId
 
