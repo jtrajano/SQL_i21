7e9c4a820b92dc24a6273039bc7f5aa62701b120
Maverick Linget <maverick.linget@iRely.com>
2022-10-18 23:14:44 +0800

    QC-685

---
+++ i21Database/dbo/Tables/tblQMCatalogueType.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[tblQMCatalogueType]
+(
+	[intCatalogueTypeId] INT NOT NULL IDENTITY, 
+	[strCatalogueType] NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL,
+	[strDescription] NVARCHAR(50) COLLATE Latin1_General_CI_AS NOT NULL,
+	[intConcurrencyId] INT NULL DEFAULT 0, 
+	CONSTRAINT [PK_tblQMCatalogueType] PRIMARY KEY ([intCatalogueTypeId]), 
+)
