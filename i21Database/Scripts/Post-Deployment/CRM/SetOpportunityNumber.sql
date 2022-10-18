0518cdeb603a9a794cd6b9e33206a23ea6810cb1
DESKTOP-V0PRVO9\iRelyNUC30 <angelo.regalado@irely.com>
2022-10-18 11:32:41 +0800

    CRM-1010

--- i21Database/Scripts/Post-Deployment/CRM/SetOpportunityNumber.sql
+++ i21Database/Scripts/Post-Deployment/CRM/SetOpportunityNumber.sql
@@ -8,7 +8,7 @@ GO
 		BEGIN
 			DECLARE @output NVARCHAR(40)
 
-			EXEC uspSMGetStartingNumber 179, @output OUTPUT
+			EXEC uspSMGetStartingNumber 180, @output OUTPUT
 			UPDATE tblCRMOpportunity SET strOpportunityNumber = @output WHERE intOpportunityId = (SELECT TOP 1 intOpportunityId FROM tblCRMOpportunity WHERE strOpportunityNumber IS NULL ORDER BY intOpportunityId)
 		END
 	END
