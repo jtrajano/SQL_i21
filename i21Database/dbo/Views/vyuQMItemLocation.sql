a38aa034975c07125464c082d25f36ee4412b4e7
Jonathan Valenzuela <valenzuela.jonathan@outlook.com>
2022-10-20 22:40:55 +0800

    QC-733

---
+++ i21Database/dbo/Views/vyuQMItemLocation.sql
@@ -0,0 +1,14 @@
+﻿CREATE VIEW [dbo].[vyuQMItemLocation]
+AS 
+SELECT Item.intItemId
+	 , strItemNo
+	 , strLocationName
+	 , Item.strDescription
+	 , strType
+	 , intOriginId
+	 , strOriginName
+	 , CompanyLocation.intCompanyLocationId
+FROM vyuICGetCompactItem AS Item
+LEFT JOIN tblICItemLocation AS ItemLocation ON Item.intItemId = ItemLocation.intItemId
+LEFT JOIN tblSMCompanyLocation AS CompanyLocation ON ItemLocation.intLocationId = CompanyLocation.intCompanyLocationId
+
