e43ee106ba0da46299ef32660193bf70796779a6
Feb Montefrio <feb.montefrio@irely.com>
2022-10-20 09:35:23 +0800

    [B] IC-10837
    # Conflicts:
    #	i21Database/dbo/Stored Procedures/uspICRepostCosting.sql

--- i21Database/dbo/Stored Procedures/uspICPostCosting.sql
+++ i21Database/dbo/Stored Procedures/uspICPostCosting.sql
@@ -980,7 +980,7 @@ BEGIN
 				,@intSubLocationId = NULL
 				,@intStorageLocationId = NULL 
 				,@dtmDate = @dtmDate
-				,@dblQty  = @dblQty
+				,@dblQty  = 0
 				,@dblUOMQty = 0
 				,@dblCost = 0
 				,@dblValue = @dblAutoVariance
@@ -1103,7 +1103,7 @@ BEGIN
 				,@intSubLocationId = NULL
 				,@intStorageLocationId = NULL 
 				,@dtmDate = @dtmDate
-				,@dblQty  = @dblQty
+				,@dblQty  = 0
 				,@dblUOMQty = 0
 				,@dblCost = 0
 				,@dblValue = @dblAutoVariance
