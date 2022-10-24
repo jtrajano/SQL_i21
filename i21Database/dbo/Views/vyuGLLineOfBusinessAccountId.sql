ea1fbd145d016cc77a527b901756cf8a1add9c3a
Jeffrey Trajano <jeffrey.trajano@irely.com>
2022-10-24 10:18:12 +0800

    AP-11000

---
+++ i21Database/dbo/Views/vyuGLLineOfBusinessAccountId.sql
@@ -0,0 +1,13 @@
+CREATE VIEW dbo.vyuGLLineOfBusinessAccountId
+AS
+SELECT
+A.intAccountSegmentId,
+S.intAccountId,
+A.strCode,
+A.strDescription,
+S.strAccountId COLLATE Latin1_General_CI_AS strAccountId
+FROM tblGLTempCOASegment S 
+JOIN tblGLAccountSegment A ON S.[Location] COLLATE Latin1_General_CI_AS = A.strCode
+JOIN tblGLAccountStructure B ON B.intAccountStructureId = A.intAccountStructureId
+WHERE B.intStructureType = 5
+
