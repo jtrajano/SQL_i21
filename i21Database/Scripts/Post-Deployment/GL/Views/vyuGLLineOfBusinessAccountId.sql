afd6c50a09d698b7f537c1f6bdcff2b7e6281e8a
Jeffrey Trajano <jeffrey.trajano@irely.com>
2022-10-24 11:02:38 +0800

    AP-11000

--- i21Database/Scripts/Post-Deployment/GL/Views/vyuGLLineOfBusinessAccountId.sql
+++ i21Database/Scripts/Post-Deployment/GL/Views/vyuGLLineOfBusinessAccountId.sql
@@ -1,28 +1,30 @@
-
+GO
 IF EXISTS(SELECT 1 FROM tblGLAccountStructure WHERE intStructureType = 5 )
 BEGIN
     DECLARE @strStructureName NVARCHAR(20)
     SELECT TOP 1 @strStructureName = strStructureName FROM tblGLAccountStructure WHERE intStructureType = 5 
 
 
-IF OBJECT_ID('vyuGLLineOfBusinessAccountId', 'V') IS NOT NULL
-    DROP VIEW vyuGLLineOfBusinessAccountId;
-    EXEC('
-    CREATE VIEW dbo.vyuGLLineOfBusinessAccountId
-    AS
-    SELECT
-    A.intAccountSegmentId,
-    S.intAccountId,
-    A.strCode,
-    A.strDescription,
-    S.strAccountId COLLATE Latin1_General_CI_AS strAccountId
-    FROM tblGLTempCOASegment S 
-    JOIN tblGLAccountSegment A ON S.['+ @strStructureName +'] COLLATE Latin1_General_CI_AS = A.strCode
-    JOIN tblGLAccountStructure B ON B.intAccountStructureId = A.intAccountStructureId
-    WHERE B.intStructureType = 5')
+	IF OBJECT_ID('vyuGLLineOfBusinessAccountId', 'V') IS NOT NULL
+	BEGIN
+		DROP VIEW vyuGLLineOfBusinessAccountId;
+		EXEC('
+		CREATE VIEW dbo.vyuGLLineOfBusinessAccountId
+		AS
+		SELECT
+		A.intAccountSegmentId,
+		S.intAccountId,
+		A.strCode,
+		A.strDescription,
+		S.strAccountId COLLATE Latin1_General_CI_AS strAccountId
+		FROM tblGLTempCOASegment S 
+		JOIN tblGLAccountSegment A ON S.['+ @strStructureName +'] COLLATE Latin1_General_CI_AS = A.strCode
+		JOIN tblGLAccountStructure B ON B.intAccountStructureId = A.intAccountStructureId
+		WHERE B.intStructureType = 5')
 
 
-/* Column does not exist or caller does not have permission to view the object */
+	/* Column does not exist or caller does not have permission to view the object */
+	END
 END
 ELSE
 BEGIN
@@ -39,3 +41,4 @@ BEGIN
     strAccountId = ''''
     ')
 END
+GO
