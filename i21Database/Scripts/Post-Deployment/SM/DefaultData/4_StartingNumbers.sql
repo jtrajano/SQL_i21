130ec0869005ed6d6b3d131a20338b641abfd5a0
IRELY\JMendones <jesmar.mendones@irely.com>
2022-10-24 16:57:20 +0800

    SM-5781

--- i21Database/Scripts/Post-Deployment/SM/DefaultData/4_StartingNumbers.sql
+++ i21Database/Scripts/Post-Deployment/SM/DefaultData/4_StartingNumbers.sql
@@ -1751,7 +1751,15 @@ GO
 			,[ysnEnable]			= 1
 			,[intConcurrencyId]		= 1
 	WHERE NOT EXISTS (SELECT TOP 1 1 FROM tblSMStartingNumber WHERE strTransactionType = N'Batch' AND [strModule] = 'Manufacturing')
-
+	UNION ALL
+	SELECT	[intStartingNumberId]	= 182
+			,[strTransactionType]	= N'Catalogue Reconciliation'
+			,[strPrefix]			= N'CR-'
+			,[intNumber]			= 0
+			,[strModule]			= 'Quality'
+			,[ysnEnable]			= 1
+			,[intConcurrencyId]		= 1
+	WHERE NOT EXISTS (SELECT TOP 1 1 FROM tblSMStartingNumber WHERE strTransactionType = N'Catalogue Reconciliation' AND [strModule] = 'Quality')
 	--Make sure to check with 19.1 and lower version. 142 is the last number
 
 
