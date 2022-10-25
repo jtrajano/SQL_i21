45ea22385e653cecbc3fb198d0e117a38029f39a
Jase Rueda <jase.rueda@iRely.com>
2022-10-25 17:32:28 +0800

    CS-135

--- i21Database/dbo/Views/vyuSTFuelTanks.sql
+++ i21Database/dbo/Views/vyuSTFuelTanks.sql
@@ -3,7 +3,17 @@ AS
 SELECT DISTINCT 
 	device.strSerialNumber, 
 	item.strItemNo, 
-	item.strDescription
+	item.strDescription,
+	site.intCompanyLocationId,
+	CASE 
+		WHEN LEN(site.intSiteNumber) = 1
+		THEN '000' + CAST(site.intSiteNumber as NVARCHAR(1))
+		WHEN LEN(site.intSiteNumber) = 2
+		THEN '00' + CAST(site.intSiteNumber as NVARCHAR(2))
+		WHEN LEN(site.intSiteNumber) = 3
+		THEN '0' + CAST(site.intSiteNumber as NVARCHAR(3))
+		ELSE CAST(site.intSiteNumber as NVARCHAR(1))
+		END as strSiteNumber
 FROM tblTMDevice device
 JOIN tblTMCompanySiteDevice sitedevice
 	ON device.intDeviceId = sitedevice.intDeviceId
@@ -11,4 +21,5 @@ INNER JOIN tblTMCompanyConsumptionSite site
 	ON site.intCompanyConsumptionSiteId = sitedevice.intCompanyConsumptionSiteId
 INNER JOIN tblICItem item
 	ON site.intItemId = item.intItemId
-WHERE strSerialNumber != ''
+WHERE device.strSerialNumber != '' AND 
+		device.intDeviceTypeId = 1 --intDeviceTypeId = 1 means Tank
--- i21Database/dbo/Views/vyuSTStoreFuelTanks.sql
+++ i21Database/dbo/Views/vyuSTStoreFuelTanks.sql
@@ -7,13 +7,26 @@ SELECT
 	item.strItemNo, 
 	item.strDescription,
 	FT.intRegisterTankNumber,
-	FT.intConcurrencyId
+	FT.intConcurrencyId,
+	site.intCompanyLocationId,
+	CASE 
+		WHEN LEN(site.intSiteNumber) = 1
+		THEN '000' + CAST(site.intSiteNumber as NVARCHAR(1))
+		WHEN LEN(site.intSiteNumber) = 2
+		THEN '00' + CAST(site.intSiteNumber as NVARCHAR(2))
+		WHEN LEN(site.intSiteNumber) = 3
+		THEN '0' + CAST(site.intSiteNumber as NVARCHAR(3))
+		ELSE CAST(site.intSiteNumber as NVARCHAR(1))
+		END as strSiteNumber
 FROM tblSTStoreFuelTanks FT
+JOIN tblSTStore ST
+	ON FT.intStoreId = ST.intStoreId
 JOIN tblTMDevice device
 	ON FT.strSerialNumber = device.strSerialNumber
 JOIN tblTMCompanySiteDevice sitedevice
 	ON device.intDeviceId = sitedevice.intDeviceId
 INNER JOIN tblTMCompanyConsumptionSite site
-	ON site.intCompanyConsumptionSiteId = sitedevice.intCompanyConsumptionSiteId
+	ON site.intCompanyConsumptionSiteId = sitedevice.intCompanyConsumptionSiteId AND
+		ST.intCompanyLocationId = site.intCompanyLocationId
 INNER JOIN tblICItem item
 	ON site.intItemId = item.intItemId
