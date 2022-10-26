bab58ad27b6c805763661c21848647b1efbc986f
Jase Rueda <jase.rueda@iRely.com>
2022-10-26 19:25:39 +0800

    CS-135

--- i21Database/dbo/Views/vyuSTFuelTanks.sql
+++ i21Database/dbo/Views/vyuSTFuelTanks.sql
@@ -13,7 +13,9 @@ SELECT DISTINCT
 		WHEN LEN(site.intSiteNumber) = 3
 		THEN '0' + CAST(site.intSiteNumber as NVARCHAR(3))
 		ELSE CAST(site.intSiteNumber as NVARCHAR(1))
-		END as strSiteNumber
+		END as strSiteNumber,
+	device.intDeviceId,
+	site.intCompanyConsumptionSiteId
 FROM tblTMDevice device
 JOIN tblTMCompanySiteDevice sitedevice
 	ON device.intDeviceId = sitedevice.intDeviceId
