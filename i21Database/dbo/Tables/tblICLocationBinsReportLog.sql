e4c4fded0ee9a6eea5b13d070a6a51b4373e60df
Jonathan Valenzuela <valenzuela.jonathan@outlook.com>
2022-10-27 12:03:29 +0800

    IC-10874

--- i21Database/dbo/Tables/tblICLocationBinsReportLog.sql
+++ i21Database/dbo/Tables/tblICLocationBinsReportLog.sql
@@ -1,16 +1,10 @@
-﻿CREATE TABLE [dbo].[tblICLocationBinsReportLog](
-	[intLogId] [int] IDENTITY(1,1) NOT NULL,
-	[dtmLastRun] [datetime] NULL,
-	[ysnRebuilding] [bit] NULL,
-	[dtmStart] [datetime] NULL,
-	[dtmEnd] [datetime] NULL,
-	[intEntityUserSecurityId] [int] NULL,
- CONSTRAINT [PK_tblICLocationBinsReportLog] PRIMARY KEY CLUSTERED 
-(
-	[intLogId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-
-ALTER TABLE [dbo].[tblICLocationBinsReportLog] ADD  DEFAULT ((0)) FOR [ysnRebuilding]
+﻿CREATE TABLE dbo.tblICLocationBinsReportLog (
+	[intLogId] INT NOT NULL IDENTITY(1,1),
+	[dtmLastRun] DATETIME NULL,
+	[ysnRebuilding] BIT NULL DEFAULT(0), 
+	[dtmStart] DATETIME NULL,
+	[dtmEnd] DATETIME NULL,	
+	[intEntityUserSecurityId] INT NULL,
+    CONSTRAINT [PK_tblICLocationBinsReportLog] PRIMARY KEY CLUSTERED ([intLogId])
+)
 GO
