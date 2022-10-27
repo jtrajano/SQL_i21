cbd902992c5779769ae8f7ee9c9bcb6b44dac640
Jeffrey Trajano <jeffrey.trajano@irely.com>
2022-10-27 15:44:02 +0800

    QC-692

--- i21Database/dbo/Tables/tblMFBatch.sql
+++ i21Database/dbo/Tables/tblMFBatch.sql
@@ -93,7 +93,7 @@ CREATE  TABLE tblMFBatch
 	strReserveMU NVARCHAR(50) COLLATE Latin1_General_CI_AS  NULL,
 	strQualityComments NVARCHAR(2048) COLLATE Latin1_General_CI_AS  NULL,
 	strRareEarth NVARCHAR(50) COLLATE Latin1_General_CI_AS  NULL,
-
+	strERPPONumber NVARCHAR(50) COLLATE  Latin1_General_CI_AS  NULL,
 	strFreightAgent NVARCHAR(50) COLLATE Latin1_General_CI_AS  NULL,
 	strSealNumber NVARCHAR(50) COLLATE Latin1_General_CI_AS  NULL,
 	strContainerType NVARCHAR(50) COLLATE Latin1_General_CI_AS  NULL,
