3d492570d31226fdf24bda2be592954868394001
Prem Selladurai <prem.selladurai@irely.com>
2022-11-01 21:34:12 +0530

    IN-1066
    IN-1066

---
+++ i21Database/dbo/Tables/tblIPContractedStockPreStage.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE tblIPContractedStockPreStage (
+	intContractedStockPreStageId INT
+	,intContractDetailId INT
+	,dblBalanceQty NUMERIC(18, 6)
+	,intStatusId INT
+	,dtmProcessedDate DATETIME
+	)
