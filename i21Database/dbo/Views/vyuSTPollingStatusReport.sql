fc44054afbd74815b45383c059f084b1cdcdc945
Renze Cuadra <renze.cuadra@iRely.com>
2022-11-03 10:17:03 +0800

    CS-95

--- i21Database/dbo/Views/vyuSTPollingStatusReport.sql
+++ i21Database/dbo/Views/vyuSTPollingStatusReport.sql
@@ -27,9 +27,14 @@ sts.strDescription,
 stcpew.strMessageType, 
 stcpew.strMessage,
 0 AS rn, 
-'' AS strReportFilter
+'' COLLATE Latin1_General_CI_AS AS strReportFilter
 FROM dbo.tblSTCheckoutProcessErrorWarning AS stcpew 
-INNER JOIN dbo.tblSTCheckoutProcess AS stcp ON stcpew.intCheckoutProcessId = stcp.intCheckoutProcessId 
-INNER JOIN dbo.tblSTStore AS sts ON stcp.intStoreId = sts.intStoreId
+INNER JOIN dbo.tblSTCheckoutProcess AS stcp 
+	ON stcpew.intCheckoutProcessId = stcp.intCheckoutProcessId 
+INNER JOIN dbo.tblSTStore AS sts 
+	ON stcp.intStoreId = sts.intStoreId
+LEFT OUTER JOIN dbo.tblSTCheckoutHeader CH
+	ON stcp.intCheckoutId = CH.intCheckoutId
 GROUP BY stcp.intStoreId, stcpew.intCheckoutProcessId, stcpew.intCheckoutProcessErrorWarningId, stcp.intCheckoutId, 
-stcp.strGuid, stcp.dtmCheckoutProcessDate, sts.intStoreNo, sts.strDescription, stcpew.strMessageType, stcpew.strMessage
+stcp.strGuid, stcp.dtmCheckoutProcessDate, CH.dtmCheckoutDate, CH.dtmCountDate, sts.intStoreNo, sts.strDescription, stcpew.strMessageType, stcpew.strMessage, CH.strCheckoutCloseDate
+HAVING CH.dtmCheckoutDate IS NOT NULL
