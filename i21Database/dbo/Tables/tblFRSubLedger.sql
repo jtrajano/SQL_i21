51a8e6f2a6148122522e5428d6a9dc5c67794897
Dom Caoile <dom.caoile.02@gmail.com>
2022-11-03 19:16:12 +0800

    Revert "FRD-1863"
    This reverts commit f2a8ab98a9397aa4abaa34f6aa969dd1ca169330.

--- i21Database/dbo/Tables/tblFRSubLedger.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[tblFRSubLedger](
-	[intFRSubLedgerId]			INT				IDENTITY (1, 1) NOT NULL,
-	[intReportId]				INT				NOT NULL,
-	[intLedgerDetailId]			INT				NOT NULL Default 0,
-	[intConcurrencyId]			INT,
-	CONSTRAINT [PK_tblFRSubLedger] PRIMARY KEY (intReportId,intLedgerDetailId),
-	CONSTRAINT [FK_tblFRSubLedger] FOREIGN KEY (intReportId) REFERENCES tblFRReport(intReportId)
-);
