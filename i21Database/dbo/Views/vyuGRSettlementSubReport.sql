44881ec009afa6495b1af32c397b4325b2337c98
Leslie Arat <leslie.arat@irely.com>
2022-11-03 22:47:06 +0800

    GRN-3115 (HDTN-278958 - Irely Mis-Calculating)

--- i21Database/dbo/Views/vyuGRSettlementSubReport.sql
+++ i21Database/dbo/Views/vyuGRSettlementSubReport.sql
@@ -278,6 +278,44 @@ FROM
 			AND (MC.intBillDetailItemId = BillDtl.intBillDetailId AND MC.intBillDetailOtherChargeItemId = BillDtl2.intBillDetailId AND MC.ysnShow = 1)
    )t3 
 	WHERE t3.intItemId IS NOT NULL 
+
+	UNION ALL
+	--manually added item in the voucher but as a misc field only
+	SELECT
+		intBillDetailIdItem				= B.intBillDetailId
+		,strId							= AP.strBillId
+		,intBillId						= AP.intBillId
+		,intItemId						= NULL
+		,strDiscountCode				= NULL
+		,strDiscountCodeDescription		= BD.strMiscDescription
+		,dblDiscountAmount				= BD.dblCost
+		,dblShrinkPercent				= 0
+		,dblGradeReading				= NULL
+		,dblAmount						= BD.dblTotal
+		,intContractDetailId			= B.intContractDetailId
+		,dblTax							= BD.dblTax
+		,dblNetTotal					= BD.dblTotal + ISNULL(BD.dblTax,0)
+		,strTaxClass					= TaxClass.strTaxClass
+	FROM tblAPBillDetail BD
+	OUTER APPLY (
+		SELECT TOP 1 
+			BD_ITEM.intBillDetailId
+			,BD_ITEM.intContractDetailId
+		FROM tblAPBillDetail BD_ITEM
+		INNER JOIN tblICItem IC
+			ON IC.intItemId = BD_ITEM.intItemId
+				AND IC.strType = 'Inventory'
+		WHERE (BD_ITEM.intInventoryReceiptItemId IS NOT NULL OR BD_ITEM.intCustomerStorageId IS NOT NULL) 
+			AND BD_ITEM.intBillId = BD.intBillId
+	) B
+	--) BON BD.intBillId = B.intBillId
+	INNER JOIN tblAPBill AP
+		ON AP.intBillId = BD.intBillId
+	LEFT JOIN vyuAPBillDetailTax Tax 
+			ON BD.intBillDetailId = Tax.intBillDetailId
+	LEFT JOIN tblSMTaxClass TaxClass 
+		ON Tax.intTaxClassId = TaxClass.intTaxClassId
+	WHERE BD.intItemId IS NULL AND BD.ysnStage = 0	
 )t
 GO
 
