be26201e0707831de880d290c331ef0fa4479834
Jase Rueda <jase.rueda@iRely.com>
2022-11-03 14:49:38 +0800

    CS-7

--- i21Database/dbo/Tables/tblSTCheckoutTankReadings.sql
+++ i21Database/dbo/Tables/tblSTCheckoutTankReadings.sql
@@ -10,7 +10,7 @@
     [dblWaterLevel] DECIMAL(18, 6) NULL,
 	[dblSumOfDeliveriesPerTank] DECIMAL(18, 6) NULL,
 	[dblTankVolume] DECIMAL(18, 6) NULL,
-	[dblCalculatedVariance] DECIMAL(18, 6) NULL,
+	[dblCalculatedVariance] AS dbo.fnSTGetCalculatedVariance(intCheckoutId,intDeviceId,dblFuelVolume,dblSumOfDeliveriesPerTank,dblTankVolume),
     [intConcurrencyId] INT NULL, 
     CONSTRAINT [PK_tblSTCheckoutTankReadings_intTankReadingsId] PRIMARY KEY ([intTankReadingsId]), 
     CONSTRAINT [FK_tblSTCheckoutTankReadings_tblSTCheckoutHeader] FOREIGN KEY ([intCheckoutId]) REFERENCES [tblSTCheckoutHeader]([intCheckoutId]) ON DELETE CASCADE, 
