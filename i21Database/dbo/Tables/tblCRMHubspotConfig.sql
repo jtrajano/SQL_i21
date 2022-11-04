187305ce6980ee10c2b43896ec00333b5626ab6c
DESKTOP-V0PRVO9\iRelyNUC30 <angelo.regalado@irely.com>
2022-11-04 16:15:11 +0800

    CRM-1017

--- i21Database/dbo/Tables/tblCRMHubspotConfig.sql
+++ i21Database/dbo/Tables/tblCRMHubspotConfig.sql
@@ -9,6 +9,7 @@
     [strHsi21RedirectUrl] NVARCHAR(250) COLLATE Latin1_General_CI_AS NULL, 
     [strHsi21AuthorizeUrl] NVARCHAR(250) COLLATE Latin1_General_CI_AS NULL, 
     [strHsRefreshToken] NVARCHAR(250) COLLATE Latin1_General_CI_AS NULL, 
+    [strScopesId] NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL, 
     [intConcurrencyId] INT NOT NULL DEFAULT 1
     CONSTRAINT [PK_tblCRMHubspotConfig] PRIMARY KEY CLUSTERED ([intHubspotConfigId] ASC)
 )
