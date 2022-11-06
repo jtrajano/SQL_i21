ce01f4e9d14b46fc4f681d0c1b9fb097dc2a9d91
Allen Marasigan <54519457+jaumarasigan@users.noreply.github.com>
2022-11-07 04:40:05 +0800

    LG-3729 Added TF logs on outbound pick lots

--- i21Database/dbo/Stored Procedures/uspLGProcessTradeFinance.sql
+++ i21Database/dbo/Stored Procedures/uspLGProcessTradeFinance.sql
@@ -17,11 +17,17 @@ BEGIN
 			,@strLastTradeFinanceLogStatus INT = NULL
 			,@intApprovalStatusId INT = NULL
 			,@ysnDelete BIT = 0
+			,@intSourceType INT = NULL
+			,@intPurchaseSale INT = NULL
 
 	IF @strAction = 'DELETE' BEGIN SET @ysnDelete = 1 END
 
 	/* Get current approval status */
-	SELECT @intApprovalStatusId = intApprovalStatusId FROM tblLGLoad WHERE intLoadId = @intLoadId
+	SELECT 
+		@intApprovalStatusId = intApprovalStatusId,
+		@intSourceType = intSourceType,
+		@intPurchaseSale = intPurchaseSale
+	FROM tblLGLoad WHERE intLoadId = @intLoadId
 	
 	/* Get Last Trade Finance Number associated to this LS */
 	SELECT TOP 1 
@@ -172,221 +178,325 @@ BEGIN
 		END
 	END
 
-	/* Construct Trade Finance Log SP parameter */
-	SELECT TOP 1 @intTradeFinanceId = intTradeFinanceId 
-	FROM tblTRFTradeFinance WHERE strTransactionType = 'Logistics' AND intTransactionHeaderId = @intLoadId
-	ORDER BY intTradeFinanceId DESC
-
-	INSERT INTO @TRFLog
-		(strAction
-		,strTransactionType
-		,intTradeFinanceTransactionId
-		,strTradeFinanceTransaction
-		,intTransactionHeaderId
-		,intTransactionDetailId
-		,strTransactionNumber
-		,dtmTransactionDate
-		,intBankTransactionId
-		,strBankTransactionId
-		,intBankId
-		,strBank
-		,intBankAccountId
-		,strBankAccount
-		,intBorrowingFacilityId
-		,strBorrowingFacility
-		,strBorrowingFacilityBankRefNo
-		,dblTransactionAmountAllocated
-		,dblTransactionAmountActual
-		,intLoanLimitId
-		,strLoanLimitNumber
-		,strLoanLimitType
-		,intLimitId
-		,strLimit
-		,dblLimit
-		,intSublimitId
-		,strSublimit
-		,dblSublimit
-		,strBankTradeReference
-		,dblFinanceQty
-		,dblFinancedAmount
-		,strBankApprovalStatus
-		,dtmAppliedToTransactionDate
-		,intStatusId
-		,intWarrantId
-		,strWarrantId
-		,intUserId
-		,intConcurrencyId
-		,intContractHeaderId
-		,intContractDetailId
-		,intOverrideBankValuationId
-		,ysnDeleted)
-	SELECT
-		strAction = CASE WHEN (@strAction = 'ADD') THEN 'Created ' ELSE CASE WHEN (@ysnDelete = 1) THEN 'Deleted ' ELSE 'Updated ' END END
-					+ CASE WHEN (L.intShipmentType = 2) THEN 'Shipping Instruction' ELSE 'Shipment' END
-		,strTransactionType = 'Logistics'
-		,intTradeFinanceTransactionId = @intTradeFinanceId
-		,strTradeFinanceTransaction = L.strTradeFinanceNo
-		,intTransactionHeaderId = L.intLoadId
-		,intTransactionDetailId = LD.intLoadDetailId
-		,strTransactionNumber = L.strLoadNumber
-		,dtmTransactionDate = GETDATE()
-		,intBankTransactionId = null
-		,strBankTransactionId = null
-		,intBankId = BA.intBankId
-		,strBank = BA.strBankName
-		,intBankAccountId = L.intBankAccountId
-		,strBankAccount = BA.strBankAccountNo
-		,intBorrowingFacilityId = L.intBorrowingFacilityId
-		,strBorrowingFacility = FA.strBorrowingFacilityId
-		,strBorrowingFacilityBankRefNo = FA.strBankReferenceNo
-		,dblTransactionAmountAllocated = CD.dblLoanAmount
-		,dblTransactionAmountActual = CD.dblLoanAmount
-		,intLoanLimitId = L.intLoanLimitId
-		,strLoanLimitNumber = BL.strBankLoanId
-		,strLoanLimitType = BL.strLimitDescription
-		,intLimitId = L.intBorrowingFacilityLimitId
-		,strLimit = FL.strBorrowingFacilityLimit
-		,dblLimit = FL.dblLimit 
-		,intSublimitId = L.intBorrowingFacilityLimitDetailId
-		,strSublimit = FLD.strLimitDescription
-		,dblSublimit = FLD.dblLimit
-		,strBankTradeReference = L.strTradeFinanceReferenceNo
-		,dblFinanceQty = LD.dblQuantity
-		,dblFinancedAmount = LD.dblAmount
-		,strBankApprovalStatus = ASTF.strApprovalStatus
-		,dtmAppliedToTransactionDate = GETDATE()
-		,intStatusId = CASE WHEN L.intShipmentStatus = 4 THEN 2 ELSE 1 END
-		,intWarrantId = null
-		,strWarrantId = L.strWarrantNo
-		,intUserId = @intUserId
-		,intConcurrencyId = 1
-		,intContractHeaderId = CD.intContractHeaderId
-		,intContractDetailId = L.intContractDetailId
-		,intOverrideBankValuationId = L.intBankValuationRuleId
-		,ysnDeleted = @ysnDelete
-	FROM tblLGLoad L
-		CROSS APPLY (SELECT TOP 1 intLoadDetailId, dblQuantity, dblAmount FROM tblLGLoadDetail WHERE intLoadId = L.intLoadId AND intPContractDetailId = L.intContractDetailId) LD
-		INNER JOIN tblCTContractDetail CD on CD.intContractDetailId = L.intContractDetailId
-		INNER JOIN tblCTContractHeader CH on CH.intContractHeaderId = CD.intContractHeaderId
-		LEFT JOIN vyuCMBankAccount BA ON BA.intBankAccountId = L.intBankAccountId
-		LEFT JOIN tblCMBorrowingFacility FA ON FA.intBorrowingFacilityId = L.intBorrowingFacilityId
-		LEFT JOIN tblCMBorrowingFacilityLimit FL ON FL.intBorrowingFacilityLimitId = L.intBorrowingFacilityLimitId
-		LEFT JOIN tblCMBorrowingFacilityLimitDetail FLD ON FLD.intBorrowingFacilityLimitDetailId = L.intBorrowingFacilityLimitDetailId
-		LEFT JOIN tblCMBankValuationRule BVR ON BVR.intBankValuationRuleId = L.intBankValuationRuleId
-		LEFT JOIN tblCTApprovalStatusTF ASTF on ASTF.intApprovalStatusId = L.intApprovalStatusId
-		LEFT JOIN tblCMBankLoan BL on BL.intBankLoanId = L.intLoanLimitId
-	WHERE intLoadId = @intLoadId
-
-	IF EXISTS (SELECT 1 FROM @TRFLog)
+	IF (@intPurchaseSale = 1)
 	BEGIN
-		/* If Last Trade Finance Number does not match the current Trade Finance Number, Cancel/Reject the previous */
-		IF (@strLastTradeFinanceNo IS NOT NULL AND ISNULL(@strLastApprovalStatus, '') NOT IN ('Canceled', 'Cancelled', 'Rejected')
-			AND ISNULL(@strLastTradeFinanceNo, '') <> ISNULL(@strTradeFinanceNumber, ''))
+		/* Construct Trade Finance Log SP parameter */
+		SELECT TOP 1 @intTradeFinanceId = intTradeFinanceId 
+		FROM tblTRFTradeFinance WHERE strTransactionType = 'Logistics' AND intTransactionHeaderId = @intLoadId
+		ORDER BY intTradeFinanceId DESC
+
+		INSERT INTO @TRFLog
+			(strAction
+			,strTransactionType
+			,intTradeFinanceTransactionId
+			,strTradeFinanceTransaction
+			,intTransactionHeaderId
+			,intTransactionDetailId
+			,strTransactionNumber
+			,dtmTransactionDate
+			,intBankTransactionId
+			,strBankTransactionId
+			,intBankId
+			,strBank
+			,intBankAccountId
+			,strBankAccount
+			,intBorrowingFacilityId
+			,strBorrowingFacility
+			,strBorrowingFacilityBankRefNo
+			,dblTransactionAmountAllocated
+			,dblTransactionAmountActual
+			,intLoanLimitId
+			,strLoanLimitNumber
+			,strLoanLimitType
+			,intLimitId
+			,strLimit
+			,dblLimit
+			,intSublimitId
+			,strSublimit
+			,dblSublimit
+			,strBankTradeReference
+			,dblFinanceQty
+			,dblFinancedAmount
+			,strBankApprovalStatus
+			,dtmAppliedToTransactionDate
+			,intStatusId
+			,intWarrantId
+			,strWarrantId
+			,intUserId
+			,intConcurrencyId
+			,intContractHeaderId
+			,intContractDetailId
+			,intOverrideBankValuationId
+			,ysnDeleted)
+		SELECT
+			strAction = CASE WHEN (@strAction = 'ADD') THEN 'Created ' ELSE CASE WHEN (@ysnDelete = 1) THEN 'Deleted ' ELSE 'Updated ' END END
+						+ CASE WHEN (L.intShipmentType = 2) THEN 'Shipping Instruction' ELSE 'Shipment 1' END
+			,strTransactionType = 'Logistics'
+			,intTradeFinanceTransactionId = @intTradeFinanceId
+			,strTradeFinanceTransaction = L.strTradeFinanceNo
+			,intTransactionHeaderId = L.intLoadId
+			,intTransactionDetailId = LD.intLoadDetailId
+			,strTransactionNumber = L.strLoadNumber
+			,dtmTransactionDate = GETDATE()
+			,intBankTransactionId = null
+			,strBankTransactionId = null
+			,intBankId = BA.intBankId
+			,strBank = BA.strBankName
+			,intBankAccountId = L.intBankAccountId
+			,strBankAccount = BA.strBankAccountNo
+			,intBorrowingFacilityId = L.intBorrowingFacilityId
+			,strBorrowingFacility = FA.strBorrowingFacilityId
+			,strBorrowingFacilityBankRefNo = FA.strBankReferenceNo
+			,dblTransactionAmountAllocated = CD.dblLoanAmount
+			,dblTransactionAmountActual = CD.dblLoanAmount
+			,intLoanLimitId = L.intLoanLimitId
+			,strLoanLimitNumber = BL.strBankLoanId
+			,strLoanLimitType = BL.strLimitDescription
+			,intLimitId = L.intBorrowingFacilityLimitId
+			,strLimit = FL.strBorrowingFacilityLimit
+			,dblLimit = FL.dblLimit 
+			,intSublimitId = L.intBorrowingFacilityLimitDetailId
+			,strSublimit = FLD.strLimitDescription
+			,dblSublimit = FLD.dblLimit
+			,strBankTradeReference = L.strTradeFinanceReferenceNo
+			,dblFinanceQty = LD.dblQuantity
+			,dblFinancedAmount = LD.dblAmount
+			,strBankApprovalStatus = ASTF.strApprovalStatus
+			,dtmAppliedToTransactionDate = GETDATE()
+			,intStatusId = CASE WHEN L.intShipmentStatus = 4 THEN 2 ELSE 1 END
+			,intWarrantId = null
+			,strWarrantId = L.strWarrantNo
+			,intUserId = @intUserId
+			,intConcurrencyId = 1
+			,intContractHeaderId = CD.intContractHeaderId
+			,intContractDetailId = L.intContractDetailId
+			,intOverrideBankValuationId = L.intBankValuationRuleId
+			,ysnDeleted = @ysnDelete
+		FROM tblLGLoad L
+			CROSS APPLY (SELECT TOP 1 intLoadDetailId, dblQuantity, dblAmount FROM tblLGLoadDetail WHERE intLoadId = L.intLoadId AND intPContractDetailId = L.intContractDetailId) LD
+			INNER JOIN tblCTContractDetail CD on CD.intContractDetailId = L.intContractDetailId
+			INNER JOIN tblCTContractHeader CH on CH.intContractHeaderId = CD.intContractHeaderId
+			LEFT JOIN vyuCMBankAccount BA ON BA.intBankAccountId = L.intBankAccountId
+			LEFT JOIN tblCMBorrowingFacility FA ON FA.intBorrowingFacilityId = L.intBorrowingFacilityId
+			LEFT JOIN tblCMBorrowingFacilityLimit FL ON FL.intBorrowingFacilityLimitId = L.intBorrowingFacilityLimitId
+			LEFT JOIN tblCMBorrowingFacilityLimitDetail FLD ON FLD.intBorrowingFacilityLimitDetailId = L.intBorrowingFacilityLimitDetailId
+			LEFT JOIN tblCMBankValuationRule BVR ON BVR.intBankValuationRuleId = L.intBankValuationRuleId
+			LEFT JOIN tblCTApprovalStatusTF ASTF on ASTF.intApprovalStatusId = L.intApprovalStatusId
+			LEFT JOIN tblCMBankLoan BL on BL.intBankLoanId = L.intLoanLimitId
+		WHERE intLoadId = @intLoadId
+
+		IF EXISTS (SELECT 1 FROM @TRFLog)
 		BEGIN
+			/* If Last Trade Finance Number does not match the current Trade Finance Number, Cancel/Reject the previous */
+			IF (@strLastTradeFinanceNo IS NOT NULL AND ISNULL(@strLastApprovalStatus, '') NOT IN ('Canceled', 'Cancelled', 'Rejected')
+				AND ISNULL(@strLastTradeFinanceNo, '') <> ISNULL(@strTradeFinanceNumber, ''))
+			BEGIN
 
-			INSERT INTO @TRFLogCancel
-				(strAction
-				,strTransactionType
-				,intTradeFinanceTransactionId
-				,strTradeFinanceTransaction
-				,intTransactionHeaderId
-				,intTransactionDetailId
-				,strTransactionNumber
-				,dtmTransactionDate
-				,intBankTransactionId
-				,strBankTransactionId
-				,intBankId
-				,strBank
-				,intBankAccountId
-				,strBankAccount
-				,intBorrowingFacilityId
-				,strBorrowingFacility
-				,strBorrowingFacilityBankRefNo
-				,dblTransactionAmountAllocated
-				,dblTransactionAmountActual
-				,intLoanLimitId
-				,strLoanLimitNumber
-				,strLoanLimitType
-				,intLimitId
-				,strLimit
-				,dblLimit
-				,intSublimitId
-				,strSublimit
-				,dblSublimit
-				,strBankTradeReference
-				,dblFinanceQty
-				,dblFinancedAmount
-				,strBankApprovalStatus
-				,dtmAppliedToTransactionDate
-				,intStatusId
-				,intWarrantId
-				,strWarrantId
-				,intUserId
-				,intConcurrencyId
-				,intContractHeaderId
-				,intContractDetailId)
-			SELECT TOP 1
-				strAction = REPLACE(strAction, 'Created', 'Updated')
-				,strTransactionType
-				,intTradeFinanceTransactionId = @intLastTradeFinanceId
-				,strTradeFinanceTransaction = @strLastTradeFinanceNo
-				,intTransactionHeaderId
-				,intTransactionDetailId
-				,strTransactionNumber
-				,dtmTransactionDate = GETDATE()
-				,intBankTransactionId
-				,strBankTransactionId
-				,intBankId
-				,strBank
-				,intBankAccountId
-				,strBankAccount
-				,intBorrowingFacilityId
-				,strBorrowingFacility
-				,strBorrowingFacilityBankRefNo
-				,dblTransactionAmountAllocated
-				,dblTransactionAmountActual
-				,intLoanLimitId
-				,strLoanLimitNumber
-				,strLoanLimitType
-				,intLimitId
-				,strLimit
-				,dblLimit
-				,intSublimitId
-				,strSublimit
-				,dblSublimit
-				,strBankTradeReference
-				,dblFinanceQty = 0
-				,dblFinancedAmount
-				,strBankApprovalStatus = CASE WHEN ISNULL(@intApprovalStatusId, '') = 3 THEN 'Rejected' ELSE 'Cancelled' END
-				,dtmAppliedToTransactionDate = GETDATE()
-				,intStatusId
-				,intWarrantId
-				,strWarrantId
-				,intUserId
-				,intConcurrencyId
-				,intContractHeaderId
-				,intContractDetailId
-			FROM tblTRFTradeFinanceLog
-			WHERE strTransactionType = 'Logistics'
-			AND intTransactionHeaderId = @intLoadId
-			ORDER BY intTradeFinanceLogId DESC
+				INSERT INTO @TRFLogCancel
+					(strAction
+					,strTransactionType
+					,intTradeFinanceTransactionId
+					,strTradeFinanceTransaction
+					,intTransactionHeaderId
+					,intTransactionDetailId
+					,strTransactionNumber
+					,dtmTransactionDate
+					,intBankTransactionId
+					,strBankTransactionId
+					,intBankId
+					,strBank
+					,intBankAccountId
+					,strBankAccount
+					,intBorrowingFacilityId
+					,strBorrowingFacility
+					,strBorrowingFacilityBankRefNo
+					,dblTransactionAmountAllocated
+					,dblTransactionAmountActual
+					,intLoanLimitId
+					,strLoanLimitNumber
+					,strLoanLimitType
+					,intLimitId
+					,strLimit
+					,dblLimit
+					,intSublimitId
+					,strSublimit
+					,dblSublimit
+					,strBankTradeReference
+					,dblFinanceQty
+					,dblFinancedAmount
+					,strBankApprovalStatus
+					,dtmAppliedToTransactionDate
+					,intStatusId
+					,intWarrantId
+					,strWarrantId
+					,intUserId
+					,intConcurrencyId
+					,intContractHeaderId
+					,intContractDetailId)
+				SELECT TOP 1
+					strAction = REPLACE(strAction, 'Created', 'Updated')
+					,strTransactionType
+					,intTradeFinanceTransactionId = @intLastTradeFinanceId
+					,strTradeFinanceTransaction = @strLastTradeFinanceNo
+					,intTransactionHeaderId
+					,intTransactionDetailId
+					,strTransactionNumber
+					,dtmTransactionDate = GETDATE()
+					,intBankTransactionId
+					,strBankTransactionId
+					,intBankId
+					,strBank
+					,intBankAccountId
+					,strBankAccount
+					,intBorrowingFacilityId
+					,strBorrowingFacility
+					,strBorrowingFacilityBankRefNo
+					,dblTransactionAmountAllocated
+					,dblTransactionAmountActual
+					,intLoanLimitId
+					,strLoanLimitNumber
+					,strLoanLimitType
+					,intLimitId
+					,strLimit
+					,dblLimit
+					,intSublimitId
+					,strSublimit
+					,dblSublimit
+					,strBankTradeReference
+					,dblFinanceQty = 0
+					,dblFinancedAmount
+					,strBankApprovalStatus = CASE WHEN ISNULL(@intApprovalStatusId, '') = 3 THEN 'Rejected' ELSE 'Cancelled' END
+					,dtmAppliedToTransactionDate = GETDATE()
+					,intStatusId
+					,intWarrantId
+					,strWarrantId
+					,intUserId
+					,intConcurrencyId
+					,intContractHeaderId
+					,intContractDetailId
+				FROM tblTRFTradeFinanceLog
+				WHERE strTransactionType = 'Logistics'
+				AND intTransactionHeaderId = @intLoadId
+				ORDER BY intTradeFinanceLogId DESC
 
-			EXEC uspTRFLogTradeFinance @TradeFinanceLogs = @TRFLogCancel;
+				EXEC uspTRFLogTradeFinance @TradeFinanceLogs = @TRFLogCancel;
+			END
+
+			IF EXISTS (SELECT 1 FROM @TRFLog WHERE ISNULL(strTradeFinanceTransaction, '') <> '') OR @ysnDelete = 1
+				EXEC uspTRFLogTradeFinance @TradeFinanceLogs = @TRFLog;
 		END
 
-		IF EXISTS (SELECT 1 FROM @TRFLog WHERE ISNULL(strTradeFinanceTransaction, '') <> '') OR @ysnDelete = 1
-			EXEC uspTRFLogTradeFinance @TradeFinanceLogs = @TRFLog;
+		/* Clear trade finance approval status after updating tfrecord and logs if rejected/cancelled */
+		IF (@intApprovalStatusId = 3 OR @intApprovalStatusId = 4)
+		BEGIN
+			UPDATE tblLGLoad
+			SET intApprovalStatusId = NULL
+			WHERE intLoadId = @intLoadId 
+		END
 	END
-
-	/* Clear trade finance approval status after updating tfrecord and logs if rejected/cancelled */
-	IF (@intApprovalStatusId = 3 OR @intApprovalStatusId = 4)
+	/* Log Outbound Shipment TF logs upon creation and deletion */
+	ELSE IF (@intPurchaseSale = 2 AND @intSourceType IN (5,6) AND
+			(@ysnDelete = 1 OR NOT EXISTS (SELECT * FROM tblTRFTradeFinanceLog WHERE intTransactionHeaderId = @intLoadId)))
 	BEGIN
-		UPDATE tblLGLoad
-		SET intApprovalStatusId = NULL
-		WHERE intLoadId = @intLoadId 
+
+		INSERT INTO @TRFLog (
+			strAction
+			, strTransactionType
+			, intTradeFinanceTransactionId
+			, strTradeFinanceTransaction
+			, intTransactionHeaderId
+			, intTransactionDetailId
+			, strTransactionNumber
+			, dtmTransactionDate
+			, intBankTransactionId
+			, strBankTransactionId
+			, dblTransactionAmountAllocated
+			, dblTransactionAmountActual
+			, dtmAppliedToTransactionDate
+			, intStatusId
+			, intWarrantId
+			, strWarrantId
+			, intUserId
+			, intConcurrencyId
+			, intContractHeaderId
+			, intContractDetailId
+			, intBankId
+			, intBankAccountId
+			, intBorrowingFacilityId
+			, intLimitId
+			, intSublimitId
+			, strBankTradeReference
+			, strBankApprovalStatus
+			, dblLimit
+			, dblSublimit
+			, dblFinanceQty
+			, dblFinancedAmount
+			, strBorrowingFacilityBankRefNo
+			, ysnDeleted
+			, intOverrideBankValuationId
+		)
+		SELECT
+			strAction = 'Created Shipment'
+			, strTransactionType = 'Logistics'
+			, intTradeFinanceTransactionId = TF.intTradeFinanceId
+			, strTradeFinanceTransaction = TF.strTradeFinanceNumber
+			, intTransactionHeaderId = L.intLoadId
+			, intTransactionDetailId = LD.intLoadDetailId
+			, strTransactionNumber = L.strLoadNumber
+			, dtmTransactionDate = getdate()
+			, intBankTransactionId = null
+			, strBankTransactionId = null
+			, dblTransactionAmountAllocated = IC.dblGrandTotal 
+			, dblTransactionAmountActual = IC.dblGrandTotal
+			, dtmAppliedToTransactionDate = getdate()
+			, intStatusId = 1
+			, intWarrantId = null
+			, strWarrantId = null
+			, intUserId = @intUserId
+			, intConcurrencyId = 1
+			, intContractHeaderId = CR.intContractHeaderId
+			, intContractDetailId = CR.intContractDetailId
+			, intBankId = IC.intBankId
+			, intBankAccountId = IC.intBankAccountId
+			, intBorrowingFacilityId = IC.intBorrowingFacilityId
+			, intLimitId = IC.intLimitTypeId
+			, intSublimitId = IC.intSublimitTypeId
+			, strBankTradeReference = IC.strReferenceNo
+			, strBankApprovalStatus = IC.strApprovalStatus
+			, dblLimit = FL.dblLimit
+			, dblSublimit = FLD.dblLimit
+			, dblFinanceQty = LT.dblLotQuantity
+			, dblFinancedAmount = (IC.dblSubTotal  / LT.dblLotQuantity) * (LT.dblLotQuantity)
+			, strBorrowingFacilityBankRefNo = IC.strBankReferenceNo
+			, ysnDeleted = @ysnDelete
+			, intOverrideBankValuationId = TF.intOverrideFacilityValuation
+		FROM
+			tblLGLoad L
+			INNER JOIN tblLGLoadDetail LD ON LD.intLoadId = L.intLoadId
+			INNER JOIN tblLGLoadDetailLot LT ON LT.intLoadDetailId = LD.intLoadDetailId
+			INNER JOIN tblICInventoryReceiptItemLot IL ON IL.intLotId = LT.intLotId
+			INNER JOIN tblICInventoryReceiptItem II ON II.intInventoryReceiptItemId = IL.intInventoryReceiptItemId
+			INNER JOIN tblICInventoryReceipt IC ON IC.intInventoryReceiptId = II.intInventoryReceiptId
+			INNER JOIN tblTRFTradeFinance TF ON TF.strTradeFinanceNumber = (IC.strTradeFinanceNumber COLLATE Latin1_General_CI_AS) and TF.strTransactionType = 'Inventory'
+			LEFT JOIN tblCMBorrowingFacilityLimit FL ON FL.intBorrowingFacilityLimitId = IC.intLimitTypeId
+			LEFT JOIN tblCMBorrowingFacilityLimitDetail FLD ON FLD.intBorrowingFacilityLimitDetailId = IC.intSublimitTypeId
+			OUTER APPLY (
+						SELECT TOP 1 
+							RI.intContractHeaderId
+							,RI.intContractDetailId
+						FROM 
+							tblICInventoryReceiptItem RI 
+						WHERE
+							RI.intInventoryReceiptId = IC.intInventoryReceiptId
+					) CR
+		WHERE L.intLoadId = @intLoadId
+
+		-- Log Trade Finance
+		IF EXISTS(SELECT 1 FROM @TRFLog)
+		BEGIN
+			EXEC uspTRFLogTradeFinance @TradeFinanceLogs = @TRFLog
+		END
 	END
-
-
 END
 
 GO
