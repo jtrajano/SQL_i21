fc1c19fa9749644accf32987bf7ad8b8da819d9b
Jeremiah Villanueva <jeremiah.villanueva@irely.com>
2022-11-07 19:16:33 +0800

    AR-15703

--- i21Database/dbo/Functions/fnARCustomerAgingDetail.sql
+++ i21Database/dbo/Functions/fnARCustomerAgingDetail.sql
@@ -142,6 +142,16 @@ BEGIN
 		,strInvoiceNumber		NVARCHAR(25)	COLLATE Latin1_General_CI_AS	NULL
 		,strDocumentNumber	NVARCHAR(25)	COLLATE Latin1_General_CI_AS	NULL
 	)
+	DECLARE @CANCELLEDINVOICE TABLE (
+		intInvoiceId			INT												NOT NULL PRIMARY KEY
+		,strInvoiceNumber		NVARCHAR(25)	COLLATE Latin1_General_CI_AS	NULL
+		,ysnPaid				BIT												NULL
+	)
+	DECLARE @CANCELLEDCMINVOICE TABLE (
+		intInvoiceId			INT												NOT NULL PRIMARY KEY
+		,strInvoiceNumber		NVARCHAR(25)	COLLATE Latin1_General_CI_AS	NULL
+	)
+
 	DECLARE @POSTEDINVOICES TABLE (
 	     intInvoiceId				INT												NOT NULL PRIMARY KEY
 		,intEntityCustomerId		INT												NOT NULL
@@ -359,6 +369,27 @@ BEGIN
 		AND I.strTransactionType = 'Credit Memo'
 		AND I.dtmPostDate BETWEEN @dtmDateFromLocal AND @dtmDateToLocal	
 		AND (@strSourceTransactionLocal IS NULL OR strType LIKE '%'+@strSourceTransactionLocal+'%')
+
+	--@CANCELLEDINVOICE
+	INSERT INTO @CANCELLEDINVOICE (
+		 intInvoiceId
+		,strInvoiceNumber
+		,ysnPaid
+	)
+	SELECT  INVCANCELLED.intInvoiceId,INVCANCELLED.strInvoiceNumber,INVCANCELLED.ysnPaid from tblARInvoice INVCANCELLED
+	WHERE ysnCancelled =1 and ysnPosted =1
+	AND INVCANCELLED.dtmPostDate BETWEEN @dtmDateFromLocal AND @dtmDateToLocal	
+
+	--@CANCELLEDINVOICE
+	INSERT INTO @CANCELLEDCMINVOICE (
+		 intInvoiceId
+		,strInvoiceNumber
+	)
+	SELECT CM.intInvoiceId,CM.strInvoiceNumber from tblARInvoice CM
+	where CM.intOriginalInvoiceId IN (SELECT intInvoiceId FROM @CANCELLEDINVOICE WHERE ISNULL(ysnPaid, 0) = 0)
+	AND CM.ysnPosted =1
+	AND CM.strTransactionType = 'Credit Memo'
+	AND CM.dtmPostDate BETWEEN @dtmDateFromLocal AND @dtmDateToLocal
 
 	--@POSTEDINVOICES
 	INSERT INTO @POSTEDINVOICES (
