a8305da1377b204ee8dfd1e33712a228e2337ee5
DESKTOP-V0PRVO9\iRelyNUC30 <angelo.regalado@irely.com>
2022-11-07 12:40:30 +0800

    CRM-894

--- i21Database/dbo/Views/vyuCRMSetting.sql
+++ i21Database/dbo/Views/vyuCRMSetting.sql
@@ -9,7 +9,10 @@ SELECT
 	,HC.strHsClientSecret
 	,HC.strHsRedirectUrl
 	,HC.strHsRefreshToken
+	,S.intFreightTermId
+	,FT.strFreightTerm
 FROM            
-	[dbo].[tblCRMSetting] AS S,
+	[dbo].[tblCRMSetting] AS S
+		LEFT JOIN tblSMFreightTerms FT ON FT.intFreightTermId = S.intFreightTermId,
     [dbo].[tblCRMHubspotConfig] AS HC,
 	[dbo].[tblSMCompanyPreference] AS CF
