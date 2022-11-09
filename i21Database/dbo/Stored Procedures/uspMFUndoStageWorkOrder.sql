e9891aeb914ac35f5f2f1b0ff8fcc0464c6124eb
Jonathan Valenzuela <valenzuela.jonathan@outlook.com>
2022-11-09 21:40:34 +0800

    Revert "MFG-4691"
    This reverts commit 0e7e1ce36460f9a38fcb321fd158ea54c567681f.

--- i21Database/dbo/Stored Procedures/uspMFUndoStageWorkOrder.sql
+++ i21Database/dbo/Stored Procedures/uspMFUndoStageWorkOrder.sql
@@ -265,7 +265,7 @@ BEGIN TRY
 			EXEC uspICInventoryAdjustment_CreatePostLotMerge
 				-- Parameters for filtering:
 				@intItemId = @intInputItemId
-				,@dtmDate = NULL
+				,@dtmDate = @dtmProductionDate
 				,@intLocationId = @intLocationId
 				,@intSubLocationId = @intSubLocationId
 				,@intStorageLocationId = @intStorageLocationId
