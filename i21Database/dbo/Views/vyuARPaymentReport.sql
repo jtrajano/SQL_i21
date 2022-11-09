6bf50c97a06304efca92196fec0d1c8fe190dba1
Roudinee Panteria <roudinee.panteria@irely.com>
2022-11-09 17:00:53 +0800

    AR-15750

--- i21Database/dbo/Views/vyuARPaymentReport.sql
+++ i21Database/dbo/Views/vyuARPaymentReport.sql
@@ -61,7 +61,7 @@ FROM (
 		 , dblDiscount          = ISNULL(ARPD.dblDiscount, 0.00)
 		 , dblWriteOffAmount	= ISNULL(ARPD.dblWriteOffAmount, 0.00)
 		 , dblCreditCardFee		= ISNULL(ARPD.dblCreditCardFee, 0.00)
-		 , dblPayment           = ISNULL(ARPD.dblPayment, 0.00) + ISNULL(ARPD.dblCreditCardFee, 0.00)
+		 , dblPayment           = ISNULL(ARPD.dblPayment, 0.00)
 	FROM tblARPayment ARP
 	LEFT OUTER JOIN (
 		SELECT PD.intPaymentId
