d4d288df81bffc0cd7d115f4cc80cc566f7e8b08
Leslie Arat <leslie.arat@irely.com>
2022-11-10 10:41:04 +0800

    GRN-3135 (GII Report - Total Company Owned Increase/Decrease (Inc DP), In Transit issues)

---
+++ i21Database/dbo/Stored Procedures/uspGRGetInTransitPerLocation.sql
@@ -0,0 +1,184 @@
+﻿CREATE PROCEDURE [dbo].[uspGRGetInTransitPerLocation] 
+	@dtmDate DATE = NULL
+	,@intCommodityId INT = NULL
+	,@intItemId INT = NULL
+	,@intLocationId INT = NULL
+	,@Locations Id READONLY
+AS
+
+BEGIN
+	
+	DECLARE @intCommodityUnitMeasureId AS INT
+		,@intCommodityStockUOMId INT
+	
+	SELECT @intCommodityUnitMeasureId = intCommodityUnitMeasureId
+		,@intCommodityStockUOMId = intUnitMeasureId
+	FROM tblICCommodityUnitMeasure
+	WHERE intCommodityId = @intCommodityId AND ysnStockUnit = 1
+
+	DECLARE @tblResultInventory TABLE 
+	(
+		Id INT IDENTITY
+		,dtmDate DATETIME
+		,dblInvIn NUMERIC(18,6)
+		,dblInvOut NUMERIC(18,6)
+		,dblAdjustments NUMERIC(18,6)
+		,dblInventoryCount NUMERIC(18,6)
+		,dblSalesInTransit NUMERIC(18,6)
+		,dblBeginningSalesInTransit NUMERIC(18,6)
+		,strDistribution NVARCHAR(50) COLLATE Latin1_General_CI_AS
+		,strTransactionType NVARCHAR(100) COLLATE Latin1_General_CI_AS
+		,strOwnership NVARCHAR(20) COLLATE Latin1_General_CI_AS
+		,intCompanyLocationId INT
+		,strLocationName NVARCHAR(200) COLLATE Latin1_General_CI_AS
+	)
+			
+	;WITH N1 (N) AS (SELECT 1 FROM (VALUES (1), (1), (1), (1), (1), (1), (1), (1), (1), (1)) n (N)),
+	N2 (N) AS (SELECT 1 FROM N1 AS N1 CROSS JOIN N1 AS N2),
+	N3 (N) AS (SELECT 1 FROM N2 AS N1 CROSS JOIN N2 AS N2),
+	Dates AS
+	(	SELECT TOP (DATEDIFF(DAY,  DATEADD(day,-2,@dtmDate), @dtmDate))
+				Date = DATEADD(DAY, ROW_NUMBER() OVER(ORDER BY N), DATEADD(day,-2,@dtmDate))
+		FROM N3
+	)
+	SELECT Date
+	INTO #tempDateRange
+	FROM Dates AS d
+
+	--select '1',* from #tempDateRange
+
+	SELECT *
+	INTO #InTransitDateRange
+	FROM (
+		SELECT
+			dtmDate = InTran.dtmTransactionDate
+			,strTransactionNumber
+			,dblInTransitQty = SUM(CASE WHEN ISNULL(uomFrom.dblUnitQty, 0) <> uomTo.dblUnitQty 
+								THEN CASE WHEN uomTo.dblUnitQty <> 0 
+									THEN CAST((ISNULL((InTran.dblTotal), 0) * uomFrom.dblUnitQty) AS NUMERIC(38,20)) / uomTo.dblUnitQty
+									ELSE NULL
+									END
+								ELSE ISNULL((InTran.dblTotal), 0)
+								END)
+		
+		FROM dbo.fnRKGetBucketInTransit(@dtmDate,@intCommodityId,NULL) InTran
+		LEFT JOIN tblICCommodityUnitMeasure uomFrom
+			ON uomFrom.intCommodityUnitMeasureId = intOrigUOMId
+		LEFT JOIN tblICCommodityUnitMeasure uomTo
+			ON uomTo.intCommodityUnitMeasureId = @intCommodityUnitMeasureId
+		WHERE InTran.strBucketType = 'Sales In-Transit'
+			AND InTran.intItemId = ISNULL(@intItemId, InTran.intItemId)
+			AND InTran.intLocationId = ISNULL(@intLocationId, InTran.intLocationId ) 
+		GROUP BY InTran.dtmTransactionDate,strTransactionNumber--,uomFrom.dblUnitQty,uomTo.dblUnitQty 
+	) A
+	WHERE dblInTransitQty <> 0
+
+		--select '#InTransitDateRange',* from #InTransitDateRange order by dtmDate
+			
+	DECLARE @tblBalanceInvByDate AS TABLE (dtmDate DATE NULL
+		,dblBeginningSalesInTransit NUMERIC(18,6)
+		,dblSalesInTransit NUMERIC(18,6))
+
+	DECLARE @date DATE
+		, @dblSalesInTransitAsOf NUMERIC(18,6)
+	
+	--BEGINNING
+	WHILE EXISTS(SELECT TOP 1 1 FROM #tempDateRange)
+	BEGIN
+		SELECT TOP 1 @date = Date FROM #tempDateRange
+		
+		INSERT INTO @tblBalanceInvByDate(dtmDate
+			, dblBeginningSalesInTransit)
+		SELECT @date
+			, SUM(dblInTransitQty)
+		FROM #InTransitDateRange WHERE dtmDate <= @date --CONVERT(DATETIME, CONVERT(NVARCHAR(10), dtmDate, 110), 110) <= CONVERT(DATETIME, @date)
+
+		DELETE #tempDateRange WHERE Date = @date
+	END
+
+	--CURRENT
+	;WITH N1 (N) AS (SELECT 1 FROM (VALUES (1), (1), (1), (1), (1), (1), (1), (1), (1), (1)) n (N)),
+	N2 (N) AS (SELECT 1 FROM N1 AS N1 CROSS JOIN N1 AS N2),
+	N3 (N) AS (SELECT 1 FROM N2 AS N1 CROSS JOIN N2 AS N2),
+	Dates AS
+	(	SELECT TOP (DATEDIFF(DAY,  DATEADD(day,-1,@dtmDate), @dtmDate))
+				Date = DATEADD(DAY, ROW_NUMBER() OVER(ORDER BY N), DATEADD(day,-1,@dtmDate))
+		FROM N3
+	)
+	SELECT Date
+	INTO #tempDateRange2
+	FROM Dates AS d
+
+	--select '2',* from #tempDateRange
+			
+	WHILE EXISTS(SELECT TOP 1 1 FROM #tempDateRange2)
+	BEGIN
+		SELECT TOP 1 @date = Date FROM #tempDateRange2
+
+		--TOTAL INVENTORY SHIPMENT FOR THE DAY
+		SELECT @dblSalesInTransitAsOf = SUM(ISNULL(InTran.dblInTransitQty, 0))
+		FROM #InTransitDateRange InTran
+		OUTER APPLY (
+			SELECT TOP 1 ysnIsUnposted
+			FROM tblICInventoryTransaction 
+			WHERE strTransactionId = InTran.strTransactionNumber
+				AND ysnIsUnposted = 0
+		) IT
+		WHERE InTran.dtmDate = @date
+			AND InTran.dblInTransitQty > 0
+			AND IT.ysnIsUnposted IS NOT NULL
+
+		--	SELECT *
+		--FROM #InTransitDateRange InTran
+		--OUTER APPLY (
+		--	SELECT TOP 1 ysnIsUnposted
+		--	FROM tblICInventoryTransaction 
+		--	WHERE strTransactionId = InTran.strTransactionNumber
+		--		AND ysnIsUnposted = 0
+		--) IT
+		--WHERE InTran.dtmDate = @date
+		--	AND InTran.dblInTransitQty > 0
+		--	AND IT.ysnIsUnposted IS NOT NULL
+
+		UPDATE @tblBalanceInvByDate
+		SET dblBeginningSalesInTransit = @dblSalesInTransitAsOf
+		WHERE dtmDate = @date
+
+		--SELECT '@tblBalanceInvByDate1',* FROM @tblBalanceInvByDate
+
+		--TOTAL INVOICE FOR THEDAY
+		SELECT @dblSalesInTransitAsOf = SUM(ISNULL(InTran.dblInTransitQty, 0))
+		FROM #InTransitDateRange InTran
+		OUTER APPLY (
+			SELECT TOP 1 ysnIsUnposted
+			FROM tblICInventoryTransaction 
+			WHERE strTransactionId = InTran.strTransactionNumber
+				AND ysnIsUnposted = 0
+		) IT
+		WHERE InTran.dtmDate = @date
+			AND InTran.dblInTransitQty < 0
+			AND IT.ysnIsUnposted IS NOT NULL
+
+		--	SELECT *
+		--FROM #InTransitDateRange InTran
+		--OUTER APPLY (
+		--	SELECT TOP 1 ysnIsUnposted
+		--	FROM tblICInventoryTransaction 
+		--	WHERE strTransactionId = InTran.strTransactionNumber
+		--		AND ysnIsUnposted = 0
+		--) IT
+		--WHERE InTran.dtmDate = @date
+		--	AND InTran.dblInTransitQty < 0
+		--	AND IT.ysnIsUnposted IS NOT NULL
+
+		--SELECT '@tblBalanceInvByDate2',* FROM @tblBalanceInvByDate
+
+		UPDATE @tblBalanceInvByDate
+		SET dblSalesInTransit = @dblSalesInTransitAsOf
+		WHERE dtmDate = @date
+		
+		DELETE #tempDateRange2 WHERE Date = @date
+	END
+
+	SELECT * FROM @tblBalanceInvByDate
+END
