baedc7592f5ee86273154adac24c5dddc648d987
Jayson Ramos <jayson.ramos@iRely.com>
2022-11-10 15:47:07 +0800

    CT-8049
    (cherry picked from commit 132e3827ed3663f10d5b5ef3e2828b447a63cffe)
    (cherry picked from commit e9f6cb17aaed96135bbebfe1502cf58a2b7a9cac)

--- i21Database/dbo/Stored Procedures/uspCTCreateVoucherInvoiceForPartialPricing.sql
+++ i21Database/dbo/Stored Procedures/uspCTCreateVoucherInvoiceForPartialPricing.sql
@@ -415,7 +415,7 @@ BEGIN TRY
 							WHERE	intPriceFixationId = @intPriceFixationId 
 							AND		intPriceFixationDetailId < @intPriceFixationDetailId
 
-							IF @dblRemainingQty <= @dblReceived
+							IF @dblRemainingQty <= @dblReceived and isnull(@dblTotalIVForSHQty,0) = 0
 							BEGIN						
 								INSERT	INTO @tblToProcess
 								SELECT	@intInventoryReceiptId,@intInventoryReceiptItemId,@dblRemainingQty,@intPriceFixationDetailId
