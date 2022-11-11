9f596f46bbe469a6c88fcb587f1f1e9b0469aedc
Vinoth Kumar T <vinoth.t@irely.com>
2022-11-12 02:28:04 +0530

    IN-1059

---
+++ i21Database/dbo/Tables/tblIPContractHeaderError.sql
@@ -0,0 +1,25 @@
+﻿CREATE TABLE tblIPContractHeaderError (
+	intContractHeaderStageId INT IDENTITY(1, 1)
+	,strSender NVARCHAR(50) COLLATE Latin1_General_CI_AS
+	,strContractNo NVARCHAR(50) COLLATE Latin1_General_CI_AS
+	,dtmContractDate DATETIME
+	,strVendorAccountNo NVARCHAR(100) COLLATE Latin1_General_CI_AS
+	,strLocation NVARCHAR(50) COLLATE Latin1_General_CI_AS
+	,strCommodity NVARCHAR(50) COLLATE Latin1_General_CI_AS
+	,strTermsCode NVARCHAR(50) COLLATE Latin1_General_CI_AS
+	,strIncoTerm NVARCHAR(100) COLLATE Latin1_General_CI_AS
+	,strIncoTermLocation NVARCHAR(100) COLLATE Latin1_General_CI_AS
+	,strSalesPerson NVARCHAR(100) COLLATE Latin1_General_CI_AS
+	,dblContractValue NUMERIC(18, 6)
+	,strCurrency NVARCHAR(40) COLLATE Latin1_General_CI_AS
+	,dtmPeriodFrom DATETIME
+	,dtmPeriodTo DATETIME
+	,strStatus NVARCHAR(50) COLLATE Latin1_General_CI_AS
+	,strBuyingOrderNo NVARCHAR(50) COLLATE Latin1_General_CI_AS
+	
+	,strErrorMessage NVARCHAR(MAX) COLLATE Latin1_General_CI_AS
+	,dtmTransactionDate DATETIME DEFAULT(GETDATE())
+	,ysnMailSent BIT DEFAULT 0
+	
+	,CONSTRAINT PK_tblIPContractHeaderError PRIMARY KEY (intContractHeaderStageId)
+	)
