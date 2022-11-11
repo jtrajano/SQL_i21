5884e858763fb7b802bd55eaf6cb4951983097d3
Nelson Reyes <nelson.reyes@irely.com>
2022-11-11 11:40:24 +0800

    ST-2312

--- i21Database/dbo/Stored Procedures/uspSTCheckoutRadiantFGM.sql
+++ i21Database/dbo/Stored Procedures/uspSTCheckoutRadiantFGM.sql
@@ -175,6 +175,27 @@ BEGIN
 					WHERE CPT.intCheckoutId = @intCheckoutId
 			END
 
+		
+			UPDATE
+				dept
+			SET
+				dept.dblTotalSalesAmountComputed = pump.dblAmount,
+				dept.intItemsSold = pump.dblQuantity
+			FROM
+				tblSTCheckoutDepartmetTotals AS dept
+				INNER JOIN (
+					SELECT 
+						intCategoryId,
+						SUM(dblQuantity) AS dblQuantity, 
+						SUM(dblAmount) AS dblAmount
+					FROM tblSTCheckoutPumpTotals
+					WHERE intCheckoutId = @intCheckoutId
+					GROUP BY intCategoryId
+				) AS pump
+					ON pump.intCategoryId = dept.intCategoryId
+			WHERE
+				dept.intCheckoutId = @intCheckoutId
+
 
 
 		SET @intCountRows = 1
