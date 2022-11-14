b4bc5091cd997db380a2b94064d9ead0c115c3be
Erick F Dela Cruz <erick.delacruz@irely.com>
2022-11-14 21:00:27 +0800

    AP-11070

--- i21Database/dbo/Stored Procedures/uspAPValidateImportedPaidVouchersDTN.sql
+++ i21Database/dbo/Stored Procedures/uspAPValidateImportedPaidVouchersDTN.sql
@@ -22,11 +22,21 @@ FROM tblAPImportPaidVouchersForPayment I
 LEFT JOIN tblGLVendorMapping VM ON VM.intVendorMappingId = I.intEntityVendorId
 LEFT JOIN tblGLVendorMappingDetail MD ON MD.intVendorMappingId = VM.intVendorMappingId AND MD.strMapVendorName = I.strEntityVendorName
 
-;WITH cte AS (
+DECLARE @cteTbl AS TABLE(
+	intRow INT,
+	intId INT
+)
+
+INSERT INTO @cteTbl
 	SELECT ROW_NUMBER() OVER(PARTITION BY A.strVendorOrderNumber ORDER BY A.intId) intRow,
 	A.intId
 	FROM tblAPImportPaidVouchersForPayment A
-)
+
+-- ;WITH cte AS (
+-- 	SELECT ROW_NUMBER() OVER(PARTITION BY A.strVendorOrderNumber ORDER BY A.intId) intRow,
+-- 	A.intId
+-- 	FROM tblAPImportPaidVouchersForPayment A
+-- )
 
 UPDATE A
 	SET A.strNotes = CASE
@@ -68,7 +78,7 @@ UPDATE A
 		A.strBillId = B.strBillId,
 		A.strVendorOrderNumber = A.strVendorOrderNumber
 FROM tblAPImportPaidVouchersForPayment A
-INNER JOIN cte cte ON cte.intId = A.intId
+INNER JOIN @cteTbl cte ON cte.intId = A.intId
 OUTER APPLY	(
 	SELECT *
 	FROM (
@@ -92,6 +102,74 @@ OUTER APPLY (
 	) AS strPaymentRecordNum
 ) P
 WHERE A.strNotes IS NULL
+AND A.dblPayment > 0
+
+UPDATE A
+	SET A.strNotes = CASE
+					WHEN 
+						A.intCurrencyId != B.intCurrencyId
+					THEN 'Currency is different on current selected currency.'
+					WHEN 
+						B.ysnPaid = 1
+					THEN 'Voucher already paid.'
+					WHEN 
+						B.ysnPosted = 0
+					THEN 'Voucher is not yet posted'
+					WHEN 
+						B.intBillId IS NULL
+					THEN 'Voucher not found.'
+					WHEN 
+						A.dblPayment > 0 AND B.intTransactionType != 1
+					THEN 'Amount is positive. Voucher type is expected.'
+					WHEN 
+						A.dblPayment < 0 AND B.intTransactionType != 3
+					THEN 'Amount is negative. Debit Memo type is expected.'
+					WHEN 
+						A.dblPayment > B.dblAmountDue AND B.intTransactionType = 3
+					THEN 'Overpayment'
+					WHEN 
+						((A.dblPayment + A.dblDiscount) - A.dblInterest) > B.dblAmountDue  AND B.intTransactionType = 1
+					THEN 'Overpayment'
+					WHEN
+						A.dblPayment < B.dblAmountDue AND B.intTransactionType = 3
+					THEN 'Underpayment'
+					WHEN 
+						((A.dblPayment + A.dblDiscount) - A.dblInterest) < B.dblAmountDue  AND B.intTransactionType = 1
+					THEN 'Underpayment'
+					WHEN
+						ABS((A.dblPayment + A.dblDiscount) - A.dblInterest) > ABS((B.dblTotal - B.dblPaymentTemp))
+					THEN 'Already included in payment' + P.strPaymentRecordNum
+					ELSE NULL
+					END,
+		A.strBillId = B.strBillId,
+		A.strVendorOrderNumber = A.strVendorOrderNumber
+FROM tblAPImportPaidVouchersForPayment A
+INNER JOIN @cteTbl cte ON cte.intId = A.intId
+OUTER APPLY	(
+	SELECT *
+	FROM (
+		SELECT *, ROW_NUMBER() OVER (ORDER BY intBillId ASC) intRow
+		FROM vyuAPBillForImport forImport
+		WHERE forImport.intEntityVendorId = A.intEntityVendorId
+			--AND ISNULL(forImport.strPaymentScheduleNumber, forImport.strVendorOrderNumber) = A.strVendorOrderNumber
+			AND ISNULL(forImport.dblTotal - forImport.dblTempDiscount, forImport.dblAmountDue) = (A.dblPayment + A.dblDiscount)--Amount of Payment Schedule should be match as well
+			AND forImport.intTransactionType = 3
+	) voucher
+	WHERE voucher.intRow = cte.intRow
+) B
+OUTER APPLY (
+	SELECT STUFF(
+		(
+			SELECT ', ' + P.strPaymentRecordNum 
+			FROM tblAPPaymentDetail PD 
+			INNER JOIN tblAPPayment P ON P.intPaymentId = PD.intPaymentId 
+			WHERE PD.intBillId = B.intBillId AND ISNULL(PD.intPayScheduleId, 0) = ISNULL(B.intPayScheduleId, 0) AND PD.dblPayment <> 0
+			ORDER BY P.intPaymentId FOR XML PATH('')
+		), 1, 1, ''
+	) AS strPaymentRecordNum
+) P
+WHERE A.strNotes IS NULL
+AND A.dblPayment < 0
 	
 IF @transCount = 0 COMMIT TRANSACTION;  
   
