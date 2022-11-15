8ba978647a99072b0e8b8709476ab9f68d0528e9
Kelvin Quinto <kelvin.quinto@irely.com>
2022-11-15 14:33:07 +0800

    AR-15814
    (cherry picked from commit a3f661690b706c0873ff098024d2c38f1a29601d)

--- i21Database/dbo/Stored Procedures/uspARPopulateInvalidPostInvoiceData.sql
+++ i21Database/dbo/Stored Procedures/uspARPopulateInvalidPostInvoiceData.sql
@@ -101,6 +101,7 @@ BEGIN
 			SELECT ICT.strTransactionId
 				 , ICT.intTransactionId
 				 , ICT.intLotId
+				 , ICT.intItemId
 				 , dblAvailableQty	= SUM(CASE WHEN ICT.intLotId IS NULL THEN ISNULL(IAC.dblStockIn, 0) - ISNULL(IAC.dblStockOut, 0) ELSE ISNULL(IL.dblStockIn, 0) - ISNULL(IL.dblStockOut, 0) END)
 			FROM tblICInventoryTransaction ICT WITH (NOLOCK)
 			LEFT JOIN tblICInventoryActualCost IAC WITH (NOLOCK) ON ICT.strTransactionId = IAC.strTransactionId AND ICT.intTransactionId = IAC.intTransactionId AND ICT.intTransactionDetailId = IAC.intTransactionDetailId
@@ -109,11 +110,12 @@ BEGIN
 			  AND ISNULL(IL.ysnIsUnposted, 0) = 0
   			  AND ISNULL(IAC.ysnIsUnposted, 0) = 0  
 			  AND ICT.intInTransitSourceLocationId IS NOT NULL
-			GROUP BY ICT.strTransactionId, ICT.intTransactionId, ICT.intLotId
+			GROUP BY ICT.strTransactionId, ICT.intTransactionId, ICT.intLotId, ICT.intItemId
 		) ICT ON ICT.strTransactionId = COSTING.strSourceTransactionId	
 		     AND ICT.intTransactionId = COSTING.intSourceTransactionId
 			 AND (ICT.intLotId IS NULL OR (ICT.intLotId IS NOT NULL AND ICT.intLotId = COSTING.intLotId))
 			 AND ABS(COSTING.dblQty) > ICT.dblAvailableQty
+			 AND ICT.intItemId = COSTING.intItemId
 		WHERE COSTING.strSessionId = @strSessionId
 	) INTRANSIT ON I.intInvoiceId = INTRANSIT.intTransactionId AND I.strInvoiceNumber = INTRANSIT.strTransactionId
 	OUTER APPLY (
