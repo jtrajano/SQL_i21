e6c3568934d257a014f6cd851c54fe4d99f7fd77
Nelson Reyes <nelson.reyes@irely.com>
2022-11-16 22:06:09 +0800

    ST-2360

--- i21Database/dbo/Stored Procedures/uspSTstgInsertPromotionItemListSend.sql
+++ i21Database/dbo/Stored Procedures/uspSTstgInsertPromotionItemListSend.sql
@@ -302,79 +302,166 @@ BEGIN
 			END
 		ELSE IF(@strRegisterClass = 'RADIANT')
 			BEGIN
-				INSERT INTO [tblSTstgPromotionItemListSend]
-				SELECT DISTINCT
-				 ST.intStoreNo [StoreLocationID]
-					, 'iRely' [VendorName]  	
-					, 'Rel. 13.2.0' [VendorModelVersion]
-					, 'update' [TableActionType]
-					, 'addchange' [RecordActionType] 
-					, CASE PIL.ysnDeleteFromRegister 
-						WHEN 0 
-							THEN 'addchange' 
-						WHEN 1 
-							THEN 'delete' 
-						ELSE 'addchange' 
-					END as [ILTDetailRecordActionType] 
-					, PIL.intPromoItemListNo [ItemListID]
-					, PIL.strPromoItemListDescription [ItemListDescription]
-					, CASE 
-						WHEN ISNULL(ST.intMaxPlu,0) > ISNULL(CAST(IUOM.strUpcCode as bigint),0) 
-							THEN 'PLU' 
-						ELSE 'upcA' 
-					END [POSCodeFormat]
-					, CASE	
-						WHEN ISNULL(ST.intMaxPlu,0) > ISNULL(CAST(IUOM.strUpcCode as bigint),0) 
-							THEN RIGHT('0000'+ISNULL(IUOM.strUpcCode,''),4) 
-						ELSE RIGHT('00000000000'+ISNULL(IUOM.strLongUPCCode,''),11) 
-					END [POSCode]
-					, IUM.strUnitMeasure [PosCodeModifierName] 
-					, '0' [PosCodeModifierValue] 
-					, Cat.strCategoryCode [MerchandiseCode]	
-				FROM tblICItem I
-				--JOIN @Tab_UpdatedItems tmpItem 
-				--	ON tmpItem.intItemId = I.intItemId 
-				JOIN tblICItemLocation IL 
+
+			
+				DECLARE @tblTempRadiantItemLists TABLE 
+				(
+					[intItemLocationId] INT,
+					[strStoreNo] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
+					[strVendorName] NVARCHAR(150) COLLATE Latin1_General_CI_AS NULL, 
+					[strVendorModelVersion] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL,
+
+					[strRecordActionType] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL, 
+					[strItemListID] NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL,
+					[strItemListDescription] NVARCHAR(250) COLLATE Latin1_General_CI_AS NULL,
+
+					[strPOSCodeFormatFormat] NVARCHAR(10) COLLATE Latin1_General_CI_AS NULL,
+					[strPOSCode] NVARCHAR(20) COLLATE Latin1_General_CI_AS NULL,
+					[strPOSCodeModifier] NVARCHAR(10) COLLATE Latin1_General_CI_AS NULL
+				)
+
+
+
+				INSERT INTO @tblTempRadiantItemLists
+				(
+					[intItemLocationId],
+					[strStoreNo], 
+					[strVendorName], 
+					[strVendorModelVersion],
+
+					[strRecordActionType], 
+					[strItemListID],
+					[strItemListDescription],
+
+					[strPOSCodeFormatFormat],
+					[strPOSCode],
+					[strPOSCodeModifier]
+				)
+				SELECT
+					[intItemLocationId]					=	IL.intItemLocationId, 
+					[strStoreNo]						=	CAST(ST.intStoreNo AS NVARCHAR(50)), 
+					[strVendorName]						=	'iRely', 
+					[strVendorModelVersion]				=	(SELECT TOP (1) strVersionNo FROM tblSMBuildNumber ORDER BY intVersionID DESC),
+
+					[strRecordActionType]				=	CASE
+																WHEN (PIL.ysnDeleteFromRegister = 1)
+																	THEN 'delete'
+																ELSE 'addchange'
+														    END, 
+					[strItemListID]						=	PIL.strPromoItemListId,
+					[strItemListDescription]			=	PIL.strPromoItemListDescription,
+
+					[strPOSCodeFormatFormat]			=	PCF.strPosCodeFormat,
+					[strPOSCode]						=	PCF.strUPCwthOrwthOutCheckDigit,
+					[strPOSCodeModifier]				=	CAST(ISNULL(IUOM.intModifier, '000') AS VARCHAR(100))
+				FROM tblSTPromotionItemListDetail ILT
+				INNER JOIN tblSTPromotionItemList PIL
+					ON ILT.intPromoItemListId = PIL.intPromoItemListId
+				INNER JOIN tblICItemUOM IUOM 
+					ON IUOM.intItemUOMId = ILT.intItemUOMId
+				INNER JOIN tblICItem I
+					ON IUOM.intItemId = I.intItemId	
+				INNER JOIN tblICItemLocation IL 
 					ON IL.intItemId = I.intItemId
-				JOIN tblSMCompanyLocation L 
-					ON L.intCompanyLocationId = IL.intLocationId
-				JOIN tblICItemUOM IUOM 
-					ON IUOM.intItemId = I.intItemId 
-				JOIN tblICUnitMeasure IUM 
-					ON IUM.intUnitMeasureId = IUOM.intUnitMeasureId 
-				JOIN tblSTStore ST 
-					ON ST.intCompanyLocationId = L.intCompanyLocationId 
-				JOIN tblICCategory Cat 
-					ON Cat.intCategoryId = I.intCategoryId
-				--LEFT JOIN tblSTSubcategoryRegProd SubCat 
-				--	ON SubCat.intStoreId = ST.intStoreId
-				JOIN tblSTRegister R 
-					ON R.intStoreId = ST.intStoreId
-				JOIN tblSTPromotionItemList PIL 
-					ON PIL.intStoreId = ST.intStoreId 
-				INNER JOIN tblSTPromotionItemListDetail ILT
-					ON ILT.intItemUOMId = IUOM.intItemUOMId
-					AND PIL.intPromoItemListId = ILT.intPromoItemListId
+				INNER JOIN tblSMCompanyLocation CL 
+					ON CL.intCompanyLocationId = IL.intLocationId
+				INNER JOIN tblSTStore ST 
+					ON  CL.intCompanyLocationId =	ST.intCompanyLocationId
+					AND CL.intCompanyLocationId =	IL.intLocationId
+					AND PIL.intStoreId			=	ST.intStoreId 
+				INNER JOIN vyuSTItemUOMPosCodeFormat PCF
+					ON I.intItemId = PCF.intItemId
+					AND IL.intLocationId = PCF.intLocationId
+					AND IUOM.intItemUOMId = PCF.intItemUOMId
 				WHERE I.ysnFuelItem = CAST(0 AS BIT) 
-				AND R.intRegisterId = @intRegisterId 
-				AND ST.intStoreId = @intStoreId 
-				-- AND SaleList.strPromoType = 'M'
-				-- AND PIL.intPromoItemListId BETWEEN @BeginningItemListId AND @EndingItemListId
-	
+					AND ST.intStoreId = @intStoreId
+					AND IUOM.strLongUPCCode IS NOT NULL
+					AND IUOM.strLongUPCCode <> ''
+					AND IUOM.strLongUPCCode <> '0'
+					AND IUOM.strLongUPCCode NOT LIKE '%[^0-9]%'
+				ORDER BY PIL.intPromoItemListNo ASC
 
 
-				IF EXISTS(SELECT StoreLocationID FROM tblSTstgPromotionItemListSend)
+				IF EXISTS(SELECT TOP 1 1 FROM @tblTempRadiantItemLists)
 					BEGIN
-						--Generate XML for the pricebook data availavle in staging table
-						Exec dbo.uspSMGenerateDynamicXML @intImportFileHeaderId, 'tblSTstgPromotionItemListSend~intPromotionItemListSend > 0', 0, @strGeneratedXML OUTPUT
+						
+						DECLARE @xmlRadiant XML = N''
+				
+						SELECT @xmlRadiant =
+						(
+							SELECT
+								trans.strStoreNo				AS 'TransmissionHeader/StoreLocationID',
+								trans.strVendorName 			AS 'TransmissionHeader/VendorName',
+								trans.strVendorModelVersion 	AS 'TransmissionHeader/VendorModelVersion'
+								,(	
+									SELECT
+										'update' AS [TableAction/@type],
+										'addchange' AS [RecordAction/@type],
+										(	
+											SELECT
+												ILM.strRecordActionType		AS [RecordAction/@type],
+												ILM.strItemListID			AS [ItemListID],
+												ILM.strItemListDescription	AS [ItemListDescription],
+												(
+													SELECT
+														IL.strPOSCodeFormatFormat	AS [ItemCode/POSCodeFormat/@format],
+														IL.strPOSCode				AS [ItemCode/POSCode],
+														IL.strPOSCodeModifier		AS [ItemCode/POSCodeModifier]
+													FROM @tblTempRadiantItemLists IL
+													WHERE ILM.strItemListID = IL.strItemListID
+													ORDER BY IL.strPOSCode ASC
+													FOR XML PATH('ItemListEntry'), TYPE
+												)
+											FROM 
+											(
+												SELECT DISTINCT
+													strItemListID
+													, strRecordActionType
+													, strItemListDescription
+												FROM @tblTempRadiantItemLists
+											) ILM
+											ORDER BY ILM.strItemListID ASC
+											FOR XML PATH('ILTDetail'), TYPE
+										)
+									FOR XML PATH('ItemListMaintenance'), TYPE		
+								)
+							FROM 
+							(
+								SELECT DISTINCT
+									[strStoreNo], 
+									[strVendorName], 
+									[strVendorModelVersion]
+								FROM @tblTempRadiantItemLists
+							) trans
+							FOR XML PATH('NAXML-MaintenanceRequest'), TYPE
+							--FOR XML PATH('TransmissionHeader'), 
+							--ROOT('NAXML-MaintenanceRequest'), TYPE
+						);
 
-						--Once XML is generated delete the data from pricebook  staging table.
-						DELETE FROM [tblSTstgPromotionItemListSend]	
+
+						DECLARE @strXmlnsRadiant AS NVARCHAR(50) = 'http://www.naxml.org/POSBO/Vocabulary/2003-10-16'
+								, @strVersionRadiant NVARCHAR(50) = '3.4'
+
+						DECLARE @strNamesSpaceRadiant AS NVARCHAR(150) = 'domain:PLU xmlns:domain="urn:vfi-sapphire:np.domain.2001-07-01" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"'
+								, @strDomainPluRadiant AS NVARCHAR(50) = 'domain:PLU'
+
+						-- INSERT Attributes 'page' and 'ofpages' to Root header
+						SET @xmlRadiant.modify('insert 
+									   (
+											attribute version { 
+																	sql:variable("@strVersion")
+															  }		   
+										) into (/*:NAXML-MaintenanceRequest)[1]');
+
+						DECLARE @strXMLRadiant AS NVARCHAR(MAX) = CAST(@xmlRadiant AS NVARCHAR(MAX))
+						SET @strXMLRadiant = REPLACE(@strXMLRadiant, '<NAXML-MaintenanceRequest', '<NAXML-MaintenanceRequest release="3.4.0" xmlns="http://www.naxml.org/POSBO/Vocabulary/2003-10-16" xmlns:radiant="http://www.radiantsystems.com/NAXML-Extension" xsi:schemaLocation="NAXML-PBIDictionaryRadiantExtension.xsd" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"')
+						
+						SET @strGeneratedXML = REPLACE(@strXMLRadiant, '><', '>' + CHAR(13) + '<')
 					END
 				ELSE 
 					BEGIN
 						SET @ysnSuccessResult = CAST(0 AS BIT)
-						SET @strMessageResult = @strMessageResult + 'No result found to generate Item List - ' + @strFilePrefix + ' Outbound file. '
+						SET @strMessageResult = 'No result found to generate Item List - ' + @strFilePrefix + ' Outbound file'
 					END
 			END
 		ELSE IF(@strRegisterClass = 'SAPPHIRE/COMMANDER')
