2cf9a95837e481024293991f66e69843e4630aa4
Roudinee Panteria <roudinee.panteria@irely.com>
2022-11-16 08:49:51 +0800

    AR-14915

--- i21Database/dbo/Stored Procedures/uspARPopulateInvoiceDetailForPosting.sql
+++ i21Database/dbo/Stored Procedures/uspARPopulateInvoiceDetailForPosting.sql
@@ -511,6 +511,7 @@ INSERT tblARPostInvoiceDetail WITH (TABLOCK)
     ,[dblPercentage]
     ,[dblProvisionalTotal]
     ,[dblBaseProvisionalTotal]
+    ,dblBaseProvisionalTotalTax
 )
 SELECT 
      [intInvoiceId]                     = ARI.[intInvoiceId]
@@ -666,6 +667,7 @@ SELECT
     ,[dblPercentage]                    = ARID.[dblPercentage]
     ,[dblProvisionalTotal]              = ARID.[dblProvisionalTotal]
     ,[dblBaseProvisionalTotal]          = ROUND(ARID.[dblProvisionalTotal] * ARID.[dblCurrencyExchangeRate], [dbo].[fnARGetDefaultDecimal]())
+    ,dblBaseProvisionalTotalTax         = ARID.dblBaseProvisionalTotalTax
 FROM tblARPostInvoiceHeader ARI
 INNER JOIN tblARInvoiceDetail ARID ON ARI.[intInvoiceId] = ARID.[intInvoiceId]
 INNER JOIN tblSMCompanyLocation SMCL ON ARI.[intCompanyLocationId] = SMCL.[intCompanyLocationId]
@@ -1391,12 +1393,18 @@ INNER JOIN (
 WHERE ID.strSessionId = @strSessionId
 
 UPDATE ARPIH
-SET dblBaseInvoiceTotal = ARPID.dblBaseTotal
+SET dblBaseInvoiceTotal = CASE WHEN ARPIH.dblPercentage <> 100 
+                            THEN ARPID.dblBaseProvisionalTotal + ARPID.dblBaseProvisionalTotalTax 
+                            ELSE ARPID.dblBaseTotal + ARPID.dblBaseTax
+                          END
 FROM tblARPostInvoiceHeader ARPIH
 INNER JOIN (
     SELECT
-         intInvoiceId = intInvoiceId
-        ,dblBaseTotal = SUM(dblBaseTotal) + SUM(dblBaseTax)
+         intInvoiceId               = intInvoiceId
+        ,dblBaseTotal               = SUM(dblBaseTotal) 
+        ,dblBaseTax                 = SUM(dblBaseTax)
+        ,dblBaseProvisionalTotal    = SUM(dblBaseProvisionalTotal)
+        ,dblBaseProvisionalTotalTax = SUM(dblBaseProvisionalTotalTax)
     FROM tblARPostInvoiceDetail
     WHERE strSessionId = @strSessionId
     GROUP BY intInvoiceId
