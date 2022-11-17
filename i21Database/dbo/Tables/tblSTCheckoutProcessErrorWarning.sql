2093ccb0789ce65bf7c7f9d33621298f4ffaceeb
Renze Cuadra <renze.cuadra@iRely.com>
2022-11-17 21:42:09 +0800

    CS-201

--- i21Database/dbo/Tables/tblSTCheckoutProcessErrorWarning.sql
+++ i21Database/dbo/Tables/tblSTCheckoutProcessErrorWarning.sql
@@ -6,7 +6,6 @@
 	[strMessageType]					NVARCHAR(1) COLLATE Latin1_General_CI_AS NOT NULL,
     [strMessage]						NVARCHAR(500) COLLATE Latin1_General_CI_AS NOT NULL,
     [intConcurrencyId] INT NULL, 
-    CONSTRAINT [PK_tblSTCheckoutProcessErrorWarning_intCheckoutProcessErrorWarningId] PRIMARY KEY ([intCheckoutProcessErrorWarningId]), 
-    CONSTRAINT [FK_tblSTCheckoutProcessErrorWarning_tblSTCheckoutHeader] FOREIGN KEY ([intCheckoutId]) REFERENCES [tblSTCheckoutHeader]([intCheckoutId]) ON DELETE CASCADE,
+    CONSTRAINT [PK_tblSTCheckoutProcessErrorWarning_intCheckoutProcessErrorWarningId] PRIMARY KEY ([intCheckoutProcessErrorWarningId]),
     CONSTRAINT [FK_tblSTCheckoutProcessErrorWarning_tblSTCheckoutProcess] FOREIGN KEY ([intCheckoutProcessId]) REFERENCES [tblSTCheckoutProcess]([intCheckoutProcessId]) ON DELETE CASCADE
 )
