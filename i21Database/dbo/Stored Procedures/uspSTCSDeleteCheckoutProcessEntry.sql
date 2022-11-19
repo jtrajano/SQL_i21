2ac1c41a1419664e1070fd6351498057a67d519f
Renze Cuadra <renze.cuadra@iRely.com>
2022-11-19 16:51:41 +0800

    CS-201

---
+++ i21Database/dbo/Stored Procedures/uspSTCSDeleteCheckoutProcessEntry.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[uspSTCSDeleteCheckoutProcessEntry] (
+	@intCheckoutId INT
+	--@intStoreId INT,
+	--@dtmCheckoutProcessDate DATETIME,
+	--@strGuid NVARCHAR(100),
+	--@intCheckoutProcessId INT OUT
+)
+AS
+SET NOCOUNT ON;
+
+DECLARE @intCheckoutProcessId INT
+SELECT @intCheckoutProcessId = intCheckoutProcessId FROM tblSTCheckoutProcessErrorWarning WHERE intCheckoutId = @intCheckoutId
+DELETE FROM tblSTCheckoutProcessErrorWarning WHERE intCheckoutId = @intCheckoutId
+IF NOT EXISTS (SELECT TOP 1 1 FROM tblSTCheckoutProcessErrorWarning WHERE @intCheckoutProcessId = @intCheckoutProcessId)
+BEGIN
+	DELETE FROM tblSTCheckoutProcess WHERE intCheckoutProcessId = @intCheckoutProcessId
+END
