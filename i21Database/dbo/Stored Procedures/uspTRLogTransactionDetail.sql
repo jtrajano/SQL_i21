68b3024ce78c374c1b38232ab12a04ad7a5b1c10
lawrence_zabala <lawrence.zabala@irely.com>
2022-11-21 12:01:32 +0800

    TR-2209 TR-2211
    (cherry picked from commit ebcb6fc2fbd82ff3b1f1d8f7ad9cc944bab638c7)

--- i21Database/dbo/Stored Procedures/uspTRLogTransactionDetail.sql
+++ i21Database/dbo/Stored Procedures/uspTRLogTransactionDetail.sql
@@ -2,7 +2,8 @@
 	@TransactionType NVARCHAR(50),
 	@TransactionId INT,
 	@UserId INT,
-	@ForDelete BIT = 0
+	@ForDelete BIT = 0,
+	@TransactionLog NVARCHAR(MAX) OUTPUT
 AS
 	SET QUOTED_IDENTIFIER OFF  
 	SET ANSI_NULLS ON  
@@ -31,67 +32,70 @@ BEGIN
 	IF (@TransactionType = @TransactionType_TransportLoad)
 	BEGIN
 		
-		INSERT INTO tblTRTransactionDetailLog(
-			strTransactionType
-			, intTransactionId
-			, intTransactionDetailId
-			, strSourceType
-			, intSourceId
-			, dblQuantity
-			, intItemId
-			, intItemUOMId
-			, intContractDetailId)
-		SELECT strTransactionType = @TransactionType_TransportLoad
-			, intTransactionId = LR.intLoadHeaderId
-			, intTransactionDetailId = LR.intLoadReceiptId
-			, strSourceType = @SourceType_InventoryReceipt
-			, intSourceId = LR.intInventoryReceiptId
-			, dblQuantity = CASE WHEN (SP.strGrossOrNet = 'Gross') THEN LR.dblGross ELSE LR.dblNet END
-			, intItemId = LR.intItemId
-			, intItemUOMId = NULL
-			, LR.intContractDetailId
-		FROM tblTRLoadReceipt LR WITH (NOLOCK)
-			LEFT JOIN tblTRLoadHeader LH WITH (NOLOCK) ON LH.intLoadHeaderId = LR.intLoadHeaderId
-			LEFT JOIN tblICInventoryReceipt IR ON IR.intInventoryReceiptId = LR.intInventoryReceiptId
-			LEFT JOIN tblTRSupplyPoint SP ON SP.intSupplyPointId = LR.intSupplyPointId
-		WHERE LH.intLoadHeaderId = @TransactionId
-			AND LR.strOrigin = 'Terminal'
-		UNION ALL
-		SELECT strTransactionType = @TransactionType_TransportLoad
-			, intTransactionId = LR.intLoadHeaderId
-			, intTransactionDetailId = LR.intLoadReceiptId
-			, strSourceType = @SourceType_InventoryTransfer
-			, intSourceId = LR.intInventoryTransferId
-			, dblQuantity = CASE WHEN (SP.strGrossOrNet = 'Gross') THEN LR.dblGross ELSE LR.dblNet END
-			, intItemId = LR.intItemId
-			, intItemUOMId = NULL
-			, LR.intContractDetailId
-		FROM tblTRLoadReceipt LR WITH (NOLOCK)
-			LEFT JOIN tblTRLoadHeader LH WITH (NOLOCK) ON LH.intLoadHeaderId = LR.intLoadHeaderId
-			LEFT JOIN tblICInventoryTransfer IT ON IT.intInventoryTransferId = LR.intInventoryTransferId
-			LEFT JOIN tblTRSupplyPoint SP ON SP.intSupplyPointId = LR.intSupplyPointId
-			LEFT JOIN tblTRLoadDistributionHeader DH WITH (NOLOCK) ON LH.intLoadHeaderId = DH.intLoadHeaderId		
-			LEFT JOIN tblTRLoadDistributionDetail DD WITH (NOLOCK) ON DH.intLoadDistributionHeaderId = DD.intLoadDistributionHeaderId
-		WHERE LH.intLoadHeaderId = @TransactionId
-			AND ((LR.strOrigin = 'Location' AND DH.strDestination = 'Location') 
-				OR (LR.strOrigin = 'Terminal' AND DH.strDestination = 'Location' AND LR.intCompanyLocationId != DH.intCompanyLocationId)
-				OR (LR.strOrigin = 'Location' AND DH.strDestination = 'Customer' AND LR.intCompanyLocationId != DH.intCompanyLocationId)
-				OR (LR.strOrigin = 'Terminal' AND DH.strDestination = 'Customer' AND LR.intCompanyLocationId != DH.intCompanyLocationId AND (ISNULL(LR.dblUnitCost, 0) <> 0 OR ISNULL(LR.dblFreightRate, 0) <> 0 OR ISNULL(LR.dblPurSurcharge, 0) <> 0)))
-		UNION ALL
-		SELECT strTransactionType = @TransactionType_TransportLoad
-			, intTransactionId = DH.intLoadHeaderId
-			, intTransactionDetailId = DD.intLoadDistributionDetailId
-			, strSourceType = @SourceType_Invoice
-			, intSourceId = DH.intInvoiceId
-			, dblQuantity = DD.dblUnits
-			, intItemId = NULL
-			, intItemUOMId = NULL
-			, DD.intContractDetailId
-		FROM tblTRLoadDistributionHeader DH WITH (NOLOCK)
-			LEFT JOIN tblTRLoadDistributionDetail DD WITH (NOLOCK) ON DD.intLoadDistributionHeaderId = DH.intLoadDistributionHeaderId
-		WHERE DH.intLoadHeaderId = @TransactionId
-			AND DH.strDestination = 'Customer'
-			AND ISNULL(DD.intLoadDistributionDetailId, '') <> ''
+		--INSERT INTO tblTRTransactionDetailLog(
+		--	strTransactionType
+		--	, intTransactionId
+		--	, intTransactionDetailId
+		--	, strSourceType
+		--	, intSourceId
+		--	, dblQuantity
+		--	, intItemId
+		--	, intItemUOMId
+		--	, intContractDetailId)
+		SET @TransactionLog =
+		(SELECT * FROM (
+			SELECT strTransactionType = @TransactionType_TransportLoad
+				, intTransactionId = LR.intLoadHeaderId
+				, intTransactionDetailId = LR.intLoadReceiptId
+				, strSourceType = @SourceType_InventoryReceipt
+				, intSourceId = LR.intInventoryReceiptId
+				, dblQuantity = CASE WHEN (SP.strGrossOrNet = 'Gross') THEN LR.dblGross ELSE LR.dblNet END
+				, intItemId = LR.intItemId
+				, intItemUOMId = NULL
+				, LR.intContractDetailId
+			FROM tblTRLoadReceipt LR WITH (NOLOCK)
+				LEFT JOIN tblTRLoadHeader LH WITH (NOLOCK) ON LH.intLoadHeaderId = LR.intLoadHeaderId
+				LEFT JOIN tblICInventoryReceipt IR ON IR.intInventoryReceiptId = LR.intInventoryReceiptId
+				LEFT JOIN tblTRSupplyPoint SP ON SP.intSupplyPointId = LR.intSupplyPointId
+			WHERE LH.intLoadHeaderId = @TransactionId
+				AND LR.strOrigin = 'Terminal'
+			UNION ALL
+			SELECT strTransactionType = @TransactionType_TransportLoad
+				, intTransactionId = LR.intLoadHeaderId
+				, intTransactionDetailId = LR.intLoadReceiptId
+				, strSourceType = @SourceType_InventoryTransfer
+				, intSourceId = LR.intInventoryTransferId
+				, dblQuantity = CASE WHEN (SP.strGrossOrNet = 'Gross') THEN LR.dblGross ELSE LR.dblNet END
+				, intItemId = LR.intItemId
+				, intItemUOMId = NULL
+				, LR.intContractDetailId
+			FROM tblTRLoadReceipt LR WITH (NOLOCK)
+				LEFT JOIN tblTRLoadHeader LH WITH (NOLOCK) ON LH.intLoadHeaderId = LR.intLoadHeaderId
+				LEFT JOIN tblICInventoryTransfer IT ON IT.intInventoryTransferId = LR.intInventoryTransferId
+				LEFT JOIN tblTRSupplyPoint SP ON SP.intSupplyPointId = LR.intSupplyPointId
+				LEFT JOIN tblTRLoadDistributionHeader DH WITH (NOLOCK) ON LH.intLoadHeaderId = DH.intLoadHeaderId		
+				LEFT JOIN tblTRLoadDistributionDetail DD WITH (NOLOCK) ON DH.intLoadDistributionHeaderId = DD.intLoadDistributionHeaderId
+			WHERE LH.intLoadHeaderId = @TransactionId
+				AND ((LR.strOrigin = 'Location' AND DH.strDestination = 'Location') 
+					OR (LR.strOrigin = 'Terminal' AND DH.strDestination = 'Location' AND LR.intCompanyLocationId != DH.intCompanyLocationId)
+					OR (LR.strOrigin = 'Location' AND DH.strDestination = 'Customer' AND LR.intCompanyLocationId != DH.intCompanyLocationId)
+					OR (LR.strOrigin = 'Terminal' AND DH.strDestination = 'Customer' AND LR.intCompanyLocationId != DH.intCompanyLocationId AND (ISNULL(LR.dblUnitCost, 0) <> 0 OR ISNULL(LR.dblFreightRate, 0) <> 0 OR ISNULL(LR.dblPurSurcharge, 0) <> 0)))
+			UNION ALL
+			SELECT strTransactionType = @TransactionType_TransportLoad
+				, intTransactionId = DH.intLoadHeaderId
+				, intTransactionDetailId = DD.intLoadDistributionDetailId
+				, strSourceType = @SourceType_Invoice
+				, intSourceId = DH.intInvoiceId
+				, dblQuantity = DD.dblUnits
+				, intItemId = NULL
+				, intItemUOMId = NULL
+				, DD.intContractDetailId
+			FROM tblTRLoadDistributionHeader DH WITH (NOLOCK)
+				LEFT JOIN tblTRLoadDistributionDetail DD WITH (NOLOCK) ON DD.intLoadDistributionHeaderId = DH.intLoadDistributionHeaderId
+			WHERE DH.intLoadHeaderId = @TransactionId
+				AND DH.strDestination = 'Customer'
+				AND ISNULL(DD.intLoadDistributionDetailId, '') <> ''
+		) tbl FOR XML RAW ('Log'), ROOT ('Logs'))
 
 		IF (@ForDelete = 1)
 		BEGIN
