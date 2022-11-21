92420f75aa2f1ff36ee7b53a34aeaf4563df4152
Roudinee Panteria <roudinee.panteria@irely.com>
2022-11-21 13:17:52 +0800

    AR-15232

--- i21Database/dbo/Stored Procedures/uspARTaxReport.sql
+++ i21Database/dbo/Stored Procedures/uspARTaxReport.sql
@@ -323,6 +323,7 @@ IF ISNULL(@strTaxReportType, 'Tax Detail') <> 'Tax By State'
 			, strStateTaxId
 			, blbCompanyLogo
 			, strLogoType
+			,strInvoiceOriginId
 		)
 		SELECT intEntityCustomerId			= TAX.intEntityCustomerId
 			, intEntitySalespersonId		= TAX.intEntitySalespersonId
@@ -383,6 +384,7 @@ IF ISNULL(@strTaxReportType, 'Tax Detail') <> 'Tax By State'
 			, strStateTaxId					= TAX.strStateTaxId
 			, blbCompanyLogo				= ISNULL(SMLP.imgLogo, @blbLogo)
 			, strLogoType					= CASE WHEN SMLP.imgLogo IS NOT NULL THEN 'Logo' ELSE 'Attachment' END
+			,strInvoiceOriginId				= TAX.strInvoiceOriginId
 		FROM dbo.vyuARSalesTaxReport TAX WITH (NOLOCK)
 		INNER JOIN #CUSTOMERS C ON TAX.intEntityCustomerId = C.intEntityCustomerId
 		INNER JOIN #COMPANYLOCATIONS CL ON TAX.intCompanyLocationId = CL.intCompanyLocationId
--- i21Database/dbo/Views/vyuARGetInvoice.sql
+++ i21Database/dbo/Views/vyuARGetInvoice.sql
@@ -205,7 +205,7 @@ SELECT
 	,ysnOverrideTaxLocation             = CAST(CASE WHEN ISNULL(INV.intTaxLocationId,0) > 0 THEN 1 ELSE 0 END AS BIT)
 	,strSourcedFrom						= CASE WHEN ISNULL(INV.intDefaultPayToBankAccountId,0) <> 0 THEN INV.strSourcedFrom ELSE '' END
 	,intProfitCenter					= CLOC.intProfitCenter
-	,ysnTaxAdjusted						= CAST(CASE WHEN RELATEDINVOICE.strType = 'Tax Adjustment' AND RELATEDINVOICE.ysnPosted = 1 THEN 1 ELSE 0 END AS BIT)
+	,ysnTaxAdjusted						= CAST(CASE WHEN RELATEDINVOICE2.strType = 'Tax Adjustment' AND RELATEDINVOICE2.ysnPosted = 1 THEN 1 ELSE 0 END AS BIT)
 	,strPrintFormat						= INV.strPrintFormat
 	,dblPercentage						= INV.dblPercentage
 	,dblProvisionalTotal				= CASE WHEN INV.dblPercentage <> 100 THEN INV.dblProvisionalTotal ELSE INV.dblInvoiceTotal END
@@ -413,6 +413,14 @@ LEFT JOIN
 		,ysnPosted
 	FROM tblARInvoice  WITH (NOLOCK) 
 ) RELATEDINVOICE ON RELATEDINVOICE.intInvoiceId = INV.intOriginalInvoiceId
+LEFT JOIN
+(
+	SELECT  
+		 intOriginalInvoiceId
+		,strType
+		,ysnPosted
+	FROM tblARInvoice  WITH (NOLOCK) 
+) RELATEDINVOICE2 ON RELATEDINVOICE2.intOriginalInvoiceId = INV.intInvoiceId
 LEFT JOIN vyuCMBankAccount DBA ON DBA.intBankAccountId = ISNULL(INV.intDefaultPayToBankAccountId,0)
 LEFT JOIN vyuCMBankAccount PFCBA ON PFCBA.intBankAccountId = ISNULL(INV.intPayToCashBankAccountId,0)
 LEFT JOIN tblCMBank B ON B.intBankId = ISNULL(INV.intBankId,0)
