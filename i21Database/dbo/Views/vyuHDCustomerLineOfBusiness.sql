94fa7c08d98f4771eee9311c861d9fa18817bb8e
Edsel Roldan Magura <edsel.magura@iRely.com>
2022-11-22 16:22:47 +0800

    HD-4333

--- i21Database/dbo/Views/vyuHDCustomerLineOfBusiness.sql
+++ i21Database/dbo/Views/vyuHDCustomerLineOfBusiness.sql
@@ -1,13 +1,22 @@
 ﻿CREATE VIEW [dbo].[vyuHDCustomerLineOfBusiness]
 	AS
-		SELECT      intId				= intLineOfBusinessId
-				   ,intEntityId         = LineOfBusiness.intEntityId
-				   ,strEntityName       = Entity.strName
-				   ,intSalespersonId    = LineOfBusiness.intEntityId
-				   ,strSalespersonName  = Entity.strName
-				   ,intLineOfBusinessId = LineOfBusiness.intLineOfBusinessId
-				   ,strLineOfBusiness   = LineOfBusiness.strLineOfBusiness
-	FROM tblSMLineOfBusiness LineOfBusiness
-		INNER JOIN tblEMEntity Entity
-	ON Entity.intEntityId = LineOfBusiness.intEntityId
+	SELECT		    intId				 = CONVERT(INT,ROW_NUMBER() OVER (ORDER BY Project.intProjectId))  
+				   ,intEntityId          = SMLineOfBusiness.intEntityId
+				   ,strEntityName        = Entity.strName
+				   ,intSalespersonId     = SMLineOfBusiness.intEntityId
+				   ,strSalespersonName   = Entity.strName
+				   ,intLineOfBusinessId  = SMLineOfBusiness.intLineOfBusinessId
+				   ,strLineOfBusiness    = SMLineOfBusiness.strLineOfBusiness
+				   ,intProjectId		 = Project.intProjectId
+	FROM tblHDProject Project
+	OUTER APPLY
+	(
+		SELECT * 
+		FROM [dbo].fnSplitStringWithTrim(Project.strLinesOfBusinessId, ',')
+		WHERE ISNULL(Item, '') != ''
+	) LineOfBusiness
+	INNER JOIN tblSMLineOfBusiness SMLineOfBusiness
+		ON SMLineOfBusiness.intLineOfBusinessId = convert(int,LineOfBusiness.Item)
+	INNER JOIN tblEMEntity Entity
+		ON Entity.intEntityId = SMLineOfBusiness.intEntityId
 GO
