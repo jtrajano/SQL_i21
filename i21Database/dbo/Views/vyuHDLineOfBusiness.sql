4aa2768381a0f9bb2eb9a65956474de8ed5ae109
Edsel Roldan Magura <edsel.magura@iRely.com>
2022-11-22 16:22:47 +0800

    HD-4333

---
+++ i21Database/dbo/Views/vyuHDLineOfBusiness.sql
@@ -0,0 +1,13 @@
+﻿CREATE VIEW [dbo].[vyuHDLineOfBusiness]
+	AS
+		SELECT      intId				= intLineOfBusinessId
+				   ,intEntityId         = LineOfBusiness.intEntityId
+				   ,strEntityName       = Entity.strName
+				   ,intSalespersonId    = LineOfBusiness.intEntityId
+				   ,strSalespersonName  = Entity.strName
+				   ,intLineOfBusinessId = LineOfBusiness.intLineOfBusinessId
+				   ,strLineOfBusiness   = LineOfBusiness.strLineOfBusiness
+	FROM tblSMLineOfBusiness LineOfBusiness
+		INNER JOIN tblEMEntity Entity
+	ON Entity.intEntityId = LineOfBusiness.intEntityId
+GO
