c9e2e35821a659e563d7c0e55b1adc3a6a15f6bb
Leslie Arat <leslie.arat@irely.com>
2022-11-24 18:22:07 +0800

    GRN-3151 (HDTN-281294 - Settled Storage Not Pulling in Tax Group on Voucher)

--- i21Database/dbo/Stored Procedures/uspSCStorageUpdate.sql
+++ i21Database/dbo/Stored Procedures/uspSCStorageUpdate.sql
@@ -328,6 +328,7 @@ BEGIN TRY
 		,[intCurrencyId]
 		,[intUserId]
 		,[dblGrossQuantity]
+		,[intShipFromLocationId]
 	)
 	SELECT 
 		[strTransactionNumber]					= CASE WHEN ISNULL(SC.intDeliverySheetId,0) > 0 THEN SC.strDeliverySheetNumber ELSE SC.strTicketNumber END
@@ -353,6 +354,7 @@ BEGIN TRY
 		,[intCurrencyId]						= SC.intCurrencyId
 		,[intUserId]							= @intUserId
 		,[dblGrossQuantity]						= (@dblNetUnits / SC.dblNetUnits) * SC.dblGrossUnits
+		,[intShipFromLocationId]				= SC.intFarmFieldId
 	FROM vyuSCTicketScreenView SC
 	INNER JOIN tblSCScaleSetup SCSetup ON SCSetup.intScaleSetupId = SC.intScaleSetupId
 	LEFT JOIN tblICItem ICFees ON ICFees.intItemId = SCSetup.intDefaultFeeItemId
