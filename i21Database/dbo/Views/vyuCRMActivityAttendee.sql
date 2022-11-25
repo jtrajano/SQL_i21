c35ee5002b631d02b12ed6aa743cd994bda242f9
DESKTOP-V0PRVO9\iRelyNUC30 <angelo.regalado@irely.com>
2022-11-25 11:00:49 +0800

    CRM-1018

---
+++ i21Database/dbo/Views/vyuCRMActivityAttendee.sql
@@ -0,0 +1,9 @@
+﻿CREATE VIEW vyuCRMActivityAttendee
+AS
+SELECT AA.intActivityAttendeeId, 
+	AA.intActivityId, 
+	ECC.intEntityId, 
+	ECC.strName, 
+	ECC.strEmail
+FROM tblSMActivityAttendee AS AA LEFT JOIN
+    vyuEMEntityCredentialContact AS ECC ON ECC.intEntityId = AA.intEntityId
