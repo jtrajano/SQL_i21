57c321868d6bbdab831edc1cf9dc19996ce9a427
Khristine Arante <khristine.arante@iRely.com>
2022-11-25 12:29:53 +0800

    AR-15845

--- i21Database/dbo/Stored Procedures/uspARUpdateCustomerInvoices.sql
+++ i21Database/dbo/Stored Procedures/uspARUpdateCustomerInvoices.sql
@@ -359,7 +359,7 @@ SELECT
 FROM
 	@InvoicesToUpdate ITG --WITH (NOLOCK)
 WHERE
-	ISNULL(ITG.[strType],'') NOT IN ('Meter Billing', 'Standard', 'Software', 'Tank Delivery', 'Provisional', 'Service Charge', 'Transport Delivery', 'Store', 'Card Fueling', 'CF Tran', 'CF Invoice', 'Store Checkout')
+	ISNULL(ITG.[strType],'') NOT IN ('Meter Billing', 'Standard', 'Software', 'Tank Delivery', 'Provisional', 'Service Charge', 'Transport Delivery', 'Store', 'Card Fueling', 'CF Tran', 'CF Invoice', 'Store End of Day')
 
 UNION ALL
 
