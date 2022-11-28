afe7bf8bf09bf7f76ad1d737266948be764e5294
Jonathan Valenzuela <jonathan.valenzuela@irely.com>
2022-11-28 09:30:42 +0800

    IC-10941

--- i21Database/dbo/Stored Procedures/uspMFGetItemStockStorageUnitDetail.sql
+++ i21Database/dbo/Stored Procedures/uspMFGetItemStockStorageUnitDetail.sql
@@ -1,21 +1,22 @@
-﻿CREATE PROCEDURE uspMFGetItemStockStorageUnitDetail @intItemId INT
-	,@intItemUOMId INT
-	,@intLocationId INT
-	,@strStorageLocationName NVARCHAR(50) = ''
-	,@strSubLocationName NVARCHAR(50) = ''
+﻿CREATE PROCEDURE uspMFGetItemStockStorageUnitDetail 
+(
+	@intItemId				INT
+  , @intItemUOMId			INT
+  , @intLocationId			INT
+  , @strStorageLocationName NVARCHAR(50) = ''
+  , @strSubLocationName		NVARCHAR(50) = ''
+)
 AS
 BEGIN
 	SELECT Item.intItemId
 	     , StockUOM.intStorageLocationId
-		 , ItemLocation.intLocationId
 	FROM tblICItem AS Item
-	JOIN tblICItemLocation AS ItemLocation ON Item.intItemId = ItemLocation.intItemId AND ItemLocation.intLocationId = @intLocationId
-	JOIN tblICItemStockUOM AS StockUOM ON ItemLocation.intItemLocationId = StockUOM.intItemLocationId 
+	LEFT JOIN tblICItemLocation AS ItemLocation ON Item.intItemId = ItemLocation.intItemId AND ItemLocation.intLocationId = @intLocationId
+	LEFT JOIN tblICItemStockUOM AS StockUOM ON ItemLocation.intItemLocationId = StockUOM.intItemLocationId 
 	LEFT JOIN tblICItemUOM AS ItemUOM ON  Item.intItemId = ItemUOM.intItemId
 	LEFT JOIN tblICStorageLocation AS StorageLocation ON StockUOM.intStorageLocationId = StorageLocation.intStorageLocationId
 	LEFT JOIN tblSMCompanyLocationSubLocation AS SubLocation ON StockUOM.intSubLocationId = SubLocation.intCompanyLocationSubLocationId
-	WHERE Item.intItemId = @intItemId  AND ItemUOM.intItemUOMId = @intItemUOMId
+	WHERE Item.intItemId = @intItemId AND ItemLocation.intLocationId = @intLocationId AND ItemUOM.intItemUOMId = @intItemUOMId
 	  AND ISNULL(StorageLocation.strName, '') = (CASE WHEN @strStorageLocationName = '' THEN ISNULL(StorageLocation.strName, '') ELSE @strStorageLocationName END)
 	  AND ISNULL(SubLocation.strSubLocationName, '') = (CASE WHEN @strSubLocationName = '' THEN ISNULL(SubLocation.strSubLocationName, '') ELSE @strSubLocationName END)
-
 END
