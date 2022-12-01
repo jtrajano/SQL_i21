ae2c6eb257c3ba7b41da5de1b47286f82693cb28
Mark Daniel Marmeto <mark.marmeto@irely.com>
2022-12-02 07:55:58 +0800

    GL-9794

--- i21Database/dbo/Views/vyuCMBankAccountRegisterRunningBalanceCount.sql
+++ i21Database/dbo/Views/vyuCMBankAccountRegisterRunningBalanceCount.sql
@@ -18,9 +18,9 @@ strPayee =
 	ELSE ISNULL(strPayee, '') END,
 dblPayment = 
 	CASE WHEN intBankTransactionTypeId IN ( 3, 9, 12, 13, 14, 15, 16, 20, 21, 22, 23 ) THEN dblAmount 
-	WHEN intBankTransactionTypeId IN ( 2, 5, 51 ) AND ISNULL(dblAmount,0) < 0 THEN dblAmount * -1 ELSE 0 END                        , 
+	WHEN intBankTransactionTypeId IN ( 2, 5, 25, 26, 51 ) AND ISNULL(dblAmount,0) < 0 THEN dblAmount * -1 ELSE 0 END                        , 
 dblDeposit = CASE WHEN intBankTransactionTypeId IN ( 1, 10, 11, 18, 19, 103, 116, 121, 122, 123 ) THEN dblAmount 
-	WHEN intBankTransactionTypeId = 5 AND ISNULL(dblAmount,0) > 0 THEN dblAmount ELSE 0 END, 
+	WHEN intBankTransactionTypeId IN (5, 25) AND ISNULL(dblAmount,0) > 0 THEN dblAmount ELSE 0 END, 
 strReferenceNo,
 strTransactionId,
 ysnCheckVoid,
