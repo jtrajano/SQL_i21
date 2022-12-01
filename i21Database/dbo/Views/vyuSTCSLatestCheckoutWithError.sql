1676731818a119f7c2217fe4ef153e44e4d688b2
Renze Cuadra <renze.cuadra@iRely.com>
2022-12-01 19:25:24 +0800

    CS-73

--- i21Database/dbo/Views/vyuSTCSLatestCheckoutWithError.sql
+++ i21Database/dbo/Views/vyuSTCSLatestCheckoutWithError.sql
@@ -1,9 +1,8 @@
 ﻿CREATE VIEW [dbo].[vyuSTCSLatestCheckoutWithError]
 AS
-SELECT TOP 1 ch.intStoreId, cpew.intCheckoutId
+SELECT ch.intStoreId, cpew.intCheckoutId
 FROM tblSTCheckoutProcessErrorWarning cpew
 JOIN tblSTCheckoutHeader ch
 	ON cpew.intCheckoutId = ch.intCheckoutId
 WHERE  ch.strCheckoutStatus <> 'Posted'
 AND cpew.strMessageType IN ('S', 'F')
-ORDER BY intCheckoutProcessErrorWarningId DESC
