dff8203e7057c8ed5784086932097edd75e1a1bd
Edsel Roldan Magura <edsel.magura@iRely.com>
2022-12-01 11:38:42 +0800

    HD-4378

---
+++ i21Database/dbo/Views/vyuHDEntityLineOfBusiness.sql
@@ -0,0 +1,13 @@
+﻿CREATE VIEW [dbo].[vyuHDEntityLineOfBusiness]
+	AS
+	SELECT intEntityLineOfBusinessId	= a.intEntityLineOfBusinessId
+	      ,intCustomerId				= a.intEntityId
+		  ,intLineOfBusinessId			= a.intLineOfBusinessId
+		  ,strLineOfBusiness			= b.strLineOfBusiness
+		  ,intSalespersonId				= a.intEntitySalespersonId
+		  ,strSalesperson				= c.strName
+		  ,intConcurrencyId				= 1
+	FROM tblEMEntityLineOfBusiness a
+		INNER JOIN tblSMLineOfBusiness b on b.intLineOfBusinessId = a.intLineOfBusinessId
+		LEFT JOIN tblEMEntity c on c.intEntityId = a.intEntitySalespersonId
+GO
