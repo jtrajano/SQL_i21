547617567baf68d39239bba58544228e307ea02f
Roudinee Panteria <roudinee.panteria@irely.com>
2022-12-01 17:40:14 +0800

    AR-15925

--- i21Database/dbo/Stored Procedures/uspARPopulateInvalidPostInvoiceData.sql
+++ i21Database/dbo/Stored Procedures/uspARPopulateInvalidPostInvoiceData.sql
@@ -1447,6 +1447,34 @@ BEGIN
 	WHERE I.strTransactionType = 'Cash Refund'
 	  AND I.dblInvoiceTotal <> ISNULL(PREPAIDS.dblAppliedInvoiceAmount, 0)
 
+	DECLARE @strItemBlankStorageLocation NVARCHAR(MAX) = NULL;
+ 
+	SELECT @strItemBlankStorageLocation = COALESCE(@strItemBlankStorageLocation + ', ' + I.strItemNo, I.strItemNo)
+	FROM ##ARPostInvoiceDetail I
+	WHERE ISNULL(I.intStorageLocationId, 0) > 0
+	AND ISNULL(I.intSubLocationId, 0) = 0
+ 
+	IF (@strItemBlankStorageLocation IS NOT NULL)
+	BEGIN
+		INSERT INTO ##ARInvalidInvoiceData
+			([intInvoiceId]
+			,[strInvoiceNumber]
+			,[strTransactionType]
+			,[intInvoiceDetailId]
+			,[intItemId]
+			,[strBatchId]
+			,[strPostingError])
+		SELECT
+			 [intInvoiceId]   = I.[intInvoiceId]
+			,[strInvoiceNumber]  = I.[strInvoiceNumber]  
+			,[strTransactionType] = I.[strTransactionType]
+			,[intInvoiceDetailId] = I.[intInvoiceDetailId]
+			,[intItemId]   = I.[intItemId]
+			,[strBatchId]   = I.[strBatchId]
+			,[strPostingError]  = 'The Storage Location field is required if the Storage Unit field is populated.  Please review these fields for Item(s) (' + @strItemBlankStorageLocation + ') and make the appropriate edits.'
+		FROM ##ARPostInvoiceDetail I
+	END
+
 	INSERT INTO ##ARInvalidInvoiceData
 		([intInvoiceId]
 		,[strInvoiceNumber]
