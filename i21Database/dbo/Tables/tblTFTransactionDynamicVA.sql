a022869526e45c6a82797d387cb0c64c6d6ffb89
manny_armas <manny.armas@iRely.com>
2022-12-02 14:45:40 +0800

    MFT-1746 - HDTN-282136 - E Files for State VA need to amended as per State Requirements (WH return)

--- i21Database/dbo/Tables/tblTFTransactionDynamicVA.sql
+++ i21Database/dbo/Tables/tblTFTransactionDynamicVA.sql
@@ -6,6 +6,7 @@
 	[strVALocalityName] [nvarchar](150) COLLATE Latin1_General_CI_AS NULL,
 	[strVADestinationAddress] [nvarchar](250) COLLATE Latin1_General_CI_AS NULL,
 	[strVADestinationZipCode] [nvarchar](10) COLLATE Latin1_General_CI_AS NULL,
+	[dblVADollarValue] [numeric](18, 6) NULL,
 	[intConcurrencyId] [int] NULL,
 	CONSTRAINT [FK_tblTFTransactionDynamicVA_tblTFTransaction] FOREIGN KEY ([intTransactionId]) REFERENCES [tblTFTransaction]([intTransactionId]) ON DELETE CASCADE,
 	CONSTRAINT [PK_tblTFTransactionDynamicVA] PRIMARY KEY ([intTransactionDynamicId])
