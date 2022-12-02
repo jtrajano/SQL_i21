3d4630f54f6805b59fea23119eeb42a33f3db4cd
Edsel Roldan Magura <edsel.magura@iRely.com>
2022-12-02 10:01:11 +0800

    HD-4378

--- i21Database/dbo/Views/vyuHDEntityLineOfBusiness.sql
+++ i21Database/dbo/Views/vyuHDEntityLineOfBusiness.sql
@@ -4,10 +4,12 @@
 	      ,intCustomerId				= a.intEntityId
 		  ,intLineOfBusinessId			= a.intLineOfBusinessId
 		  ,strLineOfBusiness			= b.strLineOfBusiness
-		  ,intSalespersonId				= a.intEntitySalespersonId
-		  ,strSalesperson				= c.strName
+		  ,intSalespersonId				= COALESCE(a.intEntitySalespersonId, d.intSalespersonId)
+		  ,strSalesperson				= COALESCE(c.strName, e.strName)
 		  ,intConcurrencyId				= 1
 	FROM tblEMEntityLineOfBusiness a
 		INNER JOIN tblSMLineOfBusiness b on b.intLineOfBusinessId = a.intLineOfBusinessId
 		LEFT JOIN tblEMEntity c on c.intEntityId = a.intEntitySalespersonId
+		LEFT JOIN tblARCustomer d on d.intEntityId = a.intEntityId
+		LEFT JOIN tblEMEntity e on e.intEntityId = d.intSalespersonId
 GO
