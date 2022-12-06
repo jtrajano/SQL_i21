3793bed3d0597dd6646cfa19179c5bc77cd32d45
Jopet Lambinicio <jopet.lambinicio@irely.com>
2022-12-07 00:12:23 +0800

    AP-11174

--- i21Database/dbo/Functions/fnAPGetPayVoucherForPaymentPayVoucher.sql
+++ i21Database/dbo/Functions/fnAPGetPayVoucherForPaymentPayVoucher.sql
@@ -115,7 +115,7 @@ RETURNS TABLE AS RETURN
 		FROM tblAPAppliedPrepaidAndDebit APD
 		WHERE APD.intBillId = voucher.intBillId AND APD.ysnApplied = 1
 	) appliedPrepays
-	WHERE (forPay.intPaymentMethodId = @paymentMethodId OR forPay.intPaymentMethodId IS NULL)
+	WHERE (forPay.intPaymentMethodId = @paymentMethodId OR (forPay.intPaymentMethodId IS NULL AND @paymentMethodId = 0))
 	AND forPay.ysnDeferredPay = @showDeferred
 	AND 1 = (CASE WHEN @currencyId > 0
 					THEN (CASE WHEN forPay.intCurrencyId = @currencyId THEN 1 ELSE 0 END)
