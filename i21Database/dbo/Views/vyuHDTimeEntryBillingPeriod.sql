6f0986126666a81b6d085b5ced6af91232f7b312
Edsel Roldan Magura <edsel.magura@iRely.com>
2022-12-06 15:49:18 +0800

    HD-4382

--- i21Database/dbo/Views/vyuHDTimeEntryBillingPeriod.sql
+++ i21Database/dbo/Views/vyuHDTimeEntryBillingPeriod.sql
@@ -1,16 +1,16 @@
 ﻿CREATE VIEW [dbo].[vyuHDTimeEntryBillingPeriod]
 AS 
-	SELECT [intTimeEntryPeriodDetailId]		= TimeEntryPeriodDetail.[intTimeEntryPeriodDetailId]
+		SELECT [intTimeEntryPeriodDetailId]		= TimeEntryPeriodDetail.[intTimeEntryPeriodDetailId]
 		  ,[strFiscalYear]					= TimeEntryPeriod.[strFiscalYear]
 		  ,[strBillingPeriodName]			= TimeEntryPeriodDetail.[strBillingPeriodName]
 		  ,[dtmBillingPeriodStart]			= TimeEntryPeriodDetail.[dtmBillingPeriodStart]
 		  ,[dtmBillingPeriodEnd]			= TimeEntryPeriodDetail.[dtmBillingPeriodEnd]
 		  ,[intRequiredHours]				= TimeEntryPeriodDetail.[intRequiredHours]
-		  ,[strPeriodDisplay]				= TimeEntryPeriod.[strFiscalYear] + ' - ' + TimeEntryPeriodDetail.[strBillingPeriodName]
+		  ,[strPeriodDisplay]				= TimeEntryPeriod.[strFiscalYear] + ' - ' + TimeEntryPeriodDetail.[strBillingPeriodName] + ' ( ' + CONVERT(NVARCHAR(100),TimeEntryPeriodDetail.[dtmBillingPeriodStart],101) + ' - ' + CONVERT(NVARCHAR(100),TimeEntryPeriodDetail.[dtmBillingPeriodEnd],101)  + ' ) ' 
 		  ,[intConcurrencyId]				= TimeEntryPeriodDetail.[intConcurrencyId]
 	FROM tblHDTimeEntryPeriodDetail TimeEntryPeriodDetail 
 		INNER JOIN tblHDTimeEntryPeriod TimeEntryPeriod
-	ON TimeEntryPeriod.intTimeEntryPeriodId = TimeEntryPeriodDetail.intTimeEntryPeriodId	
+	ON TimeEntryPeriod.intTimeEntryPeriodId = TimeEntryPeriodDetail.intTimeEntryPeriodId
 
 
 GO
