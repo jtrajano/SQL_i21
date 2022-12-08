2058e6567dea99f7b464a71cf68fab98d52b2129
Roudinee Panteria <roudinee.panteria@irely.com>
2022-12-08 14:33:54 +0800

    Revert "AR-15032"

--- i21Database/dbo/Stored Procedures/uspARInvoiceWalterMatterReport.sql
+++ i21Database/dbo/Stored Procedures/uspARInvoiceWalterMatterReport.sql
@@ -107,83 +107,45 @@ FROM dbo.tblSMCompanySetup WITH (NOLOCK)
 ORDER BY intCompanySetupID DESC
 
 SELECT
-	 intInvoiceId				= ARI.intInvoiceId
-	,intInvoiceDetailId			= ISNULL(ARGID.intInvoiceDetailId, 0)
-	,strCompanyName				= @strCompanyName
-	,strCompanyAddress			= @strCompanyFullAddress
-	,strInvoiceNumber			= ARI.strInvoiceNumber
-	,strCustomerName			= ARCS.strName
-	,strLocationName			= SMCL.strLocationName + ',' +  + [dbo].[fnConvertDateToReportDateFormat](ARI.dtmDate, 0)
-	,strContractNumber			= CTCDV.strContractNumber + '-' + CAST(CTCDV.intContractSeq AS NVARCHAR(100)) + ' dated ' + [dbo].[fnConvertDateToReportDateFormat](CTCDV.dtmContractDate, 0)
-	,strOrigin					= CTCDV.strItemOrigin
-	,strFreightTerm				= CTCDV.strFreightTerm
-	,strWeight					= CTCDV.strWeight
-	,strCustomerReference		= CTCDV.strCustomerContract
-	,strFLOId					= ARCS.strFLOId
-	,strGrade					= CTCDV.strGrade
-	,dtmDueDate					= CAST(ARI.dtmDueDate AS DATE)
-	,strTerm					= SMT.strTerm
-	,strItemDescription			= ARGID.strItemDescription
-	,strQtyShipped				= CONVERT(VARCHAR,CAST(ARGID.dblQtyShipped AS MONEY),1) + ' ' + ARGID.strUnitMeasure
-	,strShipmentGrossWt			= CONVERT(VARCHAR,CAST(ARGID.dblShipmentGrossWt AS MONEY),1) + ' ' + ARGID.strWeightUnitMeasure
-	,strShipmentTareWt			= CONVERT(VARCHAR,CAST(ARGID.dblShipmentTareWt AS MONEY),1) + ' ' + ARGID.strWeightUnitMeasure
-	,strShipmentNetWt			= CONVERT(VARCHAR,CAST(ARGID.dblShipmentNetWt AS MONEY),1) + ' ' + ARGID.strWeightUnitMeasure
-	,strCurrenyPriceUOM			= ARGID.strCurrency + ' ' + REPLACE(CONVERT(VARCHAR,CAST(ARGID.dblPrice AS MONEY),1), '.00','') + ' ' + ARGID.strPriceUnitMeasure
-	,strEDICode					= ICC.strEDICode
-	,ysnCustomsReleased			= ISNULL(LGL.ysnCustomsReleased, 0)
-	,strBOLNumber				= LGL.strBLNumber + ' dd ' + [dbo].[fnConvertDateToReportDateFormat](LGL.dtmBLDate, 0)
-	,strDestinationCity			= LGL.strDestinationCity
-	,strMVessel					= LGL.strMVessel
-	,strPaymentComments			= dbo.fnEliminateHTMLTags(ISNULL(ARI.strPaymentInstructions, ''), 0)
-	,blbLogo					= ISNULL(SMLP.imgLogo, @blbLogo)
-	,strLogoType				= CASE WHEN SMLP.imgLogo IS NOT NULL THEN 'Logo' ELSE 'Attachment' END
-	,strBankName				= CMB.strBankName
-	,strIBAN					= CMBA.strIBAN
-	,strSWIFT					= CMBA.strSWIFT
-	,strBICCode					= CMBA.strBICCode
-	,blbFooterLogo				= SMLPF.imgLogo
-	,dblInvoiceSubtotal			= ARI.dblInvoiceSubtotal
-	,dblTax						= ARI.dblTax
-	,dblInvoiceTotal			= CASE WHEN ARI.strTransactionType = 'Customer Prepayment' AND ARI.ysnPosted = 0 THEN 0 ELSE ARI.dblInvoiceTotal END
-	,strVATNo					= ISNULL(EMELS.strVATNo, '')
-	,strOurFiscalRepName		= EMELS.strOurFiscalRepName
-	,strOurFiscalRepAddress		= EMELS.strOurFiscalRepAddress
-	,strEntityLocationRemarks	= EMELS.strRemarks
-	,strFooterComments			= dbo.fnEliminateHTMLTags(ISNULL(ARI.strFooterComments, ''), 0)
-	,dblTotal					= ARGID.dblTotal
-	,strReportIdentifier		= CASE 
-									WHEN ARI.strType = 'Provisional' THEN 'Provisional ' 
-									WHEN ARIR.strType = 'Provisional' THEN 'Commercial ' 
-									ELSE ''
-								  END + 'Invoice No: ' + ARI.strInvoiceNumber
-	,strRelatedInvoiceRemarks	= CASE 
-									WHEN ARIR.strType = 'Provisional' THEN 'Replaces Provisional Invoice: ' + + ARIR.strInvoiceNumber
-									WHEN ARI.strTransactionType = 'Credit Memo' AND ISNULL(ARI.intOriginalInvoiceId, 0) <> 0 THEN 'Cancels Invoice: ' + ARIR.strInvoiceNumber
-									ELSE ''
-								  END
-	,dblAmountDue				= ARI.dblAmountDue
-	,dblShipmentNetWt			= ARGID.dblShipmentNetWt
-	,dblQtyShipped				= ARGID.dblQtyShipped
-	,strUnitMeasure				= ARGID.strUnitMeasure
-	,dblUnitPrice				= ARGID.dblUnitPrice
-	,strProvisionalInvoiceNumber= ISNULL(ARIR.strInvoiceNumber, '')
-	,dtmProvisionalDate			= ARIR.dtmDate
-	,dblProvisionalPayment		= ARIR.dblPayment
-	,dblProvisionalShipmentNetWt= ARGIDP.dblShipmentNetWt
-	,dblProvisionalQtyShipped	= ARGIDP.dblQtyShipped
-	,strProvisionalUnitMeasure	= ARGIDP.strUnitMeasure
-	,dblProvisionalUnitPrice	= ARGIDP.dblUnitPrice
-	,strRelatedType				= ARIR.strType
-	,strTransactionType			= ARI.strTransactionType
-	,strBuyer					= ISNULL(RTRIM(EMELB.strCheckPayeeName) + CHAR(13) + char(10), '') + ISNULL(RTRIM(ARI.strBillToAddress) + CHAR(13) + CHAR(10), '')	+ ISNULL(NULLIF(ARI.strBillToCity, ''), '') + ISNULL(', ' + NULLIF(ARI.strBillToState, ''), '') + ISNULL(', ' + NULLIF(ARI.strBillToZipCode, ''), '') + ISNULL(', ' + NULLIF(ARI.strBillToCountry, ''), '')
-FROM tblARInvoice ARI WITH (NOLOCK)
+	 intInvoiceId			= ARI.intInvoiceId
+	,intInvoiceDetailId		= ISNULL(ARGID.intInvoiceDetailId, 0)
+	,strCompanyName			= @strCompanyName
+	,strCompanyAddress		= @strCompanyFullAddress
+	,strInvoiceNumber		= ARI.strInvoiceNumber
+	,strCustomerName		= ARCS.strName
+	,strLocationName		= SMCL.strLocationName + ',' +  + [dbo].[fnConvertDateToReportDateFormat](ARI.dtmDate, 0)
+	,strContractNumber		= CTCDV.strContractNumber + '-' + CAST(CTCDV.intContractSeq AS NVARCHAR(100)) + ' dated ' + [dbo].[fnConvertDateToReportDateFormat](CTCDV.dtmContractDate, 0)
+	,strOrigin				= CTCDV.strItemOrigin
+	,strFreightTerm			= CTCDV.strFreightTerm
+	,strWeight				= CTCDV.strWeight
+	,strCustomerReference	= CTCDV.strCustomerContract
+	,strFLOId				= ARCS.strFLOId
+	,strGrade				= CTCDV.strGrade
+	,dtmDueDate				= CAST(ARI.dtmDueDate AS DATE)
+	,strTerm				= SMT.strTerm
+	,strItemDescription		= ARGID.strItemDescription
+	,strQtyShipped			= CONVERT(VARCHAR,CAST(ARGID.dblQtyShipped AS MONEY),1) + ' ' + ARGID.strUnitMeasure
+	,strShipmentGrossWt		= CONVERT(VARCHAR,CAST(ARGID.dblShipmentGrossWt AS MONEY),1) + ' ' + ARGID.strWeightUnitMeasure
+	,strShipmentTareWt		= CONVERT(VARCHAR,CAST(ARGID.dblShipmentTareWt AS MONEY),1) + ' ' + ARGID.strWeightUnitMeasure
+	,strShipmentNetWt		= CONVERT(VARCHAR,CAST(ARGID.dblShipmentNetWt AS MONEY),1) + ' ' + ARGID.strWeightUnitMeasure
+	,strCurrenyPriceUOM		= ARGID.strCurrency + ' ' + REPLACE(CONVERT(VARCHAR,CAST(ARGID.dblPrice AS MONEY),1), '.00','') + ' ' + ARGID.strPriceUnitMeasure
+	,dblTotal				= ARGID.dblTotal
+	,strEDICode				= ICC.strEDICode
+	,ysnCustomsReleased		= ISNULL(LGL.ysnCustomsReleased, 0)
+	,strBOLNumber			= LGL.strBLNumber + ' dd ' + [dbo].[fnConvertDateToReportDateFormat](LGL.dtmBLDate, 0)
+	,strDestinationCity		= LGL.strDestinationCity
+	,strMVessel				= LGL.strMVessel
+	,strPaymentComments		= ARI.strTradeFinanceComments
+	,blbLogo                = ISNULL(SMLP.imgLogo, @blbLogo)
+	,strLogoType			= CASE WHEN SMLP.imgLogo IS NOT NULL THEN 'Logo' ELSE 'Attachment' END
+	,strBankName			= CMB.strBankName
+	,strIBAN				= CMBA.strIBAN
+	,strSWIFT				= CMBA.strSWIFT
+	,strBICCode				= CMBA.strBICCode
+FROM dbo.tblARInvoice ARI WITH (NOLOCK)
 INNER JOIN vyuARCustomerSearch ARCS WITH (NOLOCK) ON ARI.intEntityCustomerId = ARCS.intEntityId 
 INNER JOIN tblSMCompanyLocation SMCL WITH (NOLOCK) ON ARI.intCompanyLocationId = SMCL.intCompanyLocationId
-INNER JOIN tblEMEntityLocation EMELS WITH (NOLOCK) ON ARI.intShipToLocationId = EMELS.intEntityLocationId
-INNER JOIN tblEMEntityLocation EMELB ON ARI.intBillToLocationId = EMELB.intEntityLocationId
 LEFT JOIN vyuARGetInvoiceDetail ARGID WITH (NOLOCK) ON ARI.intInvoiceId = ARGID.intInvoiceId
-LEFT JOIN tblARInvoice ARIR WITH (NOLOCK) ON ARI.intOriginalInvoiceId = ARIR.intInvoiceId
-LEFT JOIN vyuARGetInvoiceDetail ARGIDP WITH (NOLOCK) ON ARGID.intOriginalInvoiceDetailId = ARGIDP.intInvoiceDetailId
 LEFT JOIN vyuCTContractDetailView CTCDV WITH (NOLOCK) ON ARGID.intContractDetailId = CTCDV.intContractDetailId
 LEFT JOIN tblICCommodity ICC WITH (NOLOCK) ON CTCDV.intCommodityId = ICC.intCommodityId
 LEFT JOIN tblLGLoad LGL WITH (NOLOCK) ON ARGID.strDocumentNumber = LGL.strLoadNumber AND ISNULL(ARGID.intLoadDetailId, 0) <> 0 AND ISNULL(LGL.strBLNumber,'') <> ''
@@ -191,7 +153,6 @@ LEFT JOIN tblSMTerm SMT WITH (NOLOCK) ON ARI.intTermId = SMT.intTermID
 LEFT JOIN tblCMBankAccount CMBA WITH (NOLOCK) ON ISNULL(ISNULL(ARI.intPayToCashBankAccountId, ARI.intDefaultPayToBankAccountId), 0) = CMBA.intBankAccountId
 LEFT JOIN tblCMBank CMB WITH (NOLOCK) ON CMBA.intBankId = CMB.intBankId
 LEFT JOIN tblSMLogoPreference SMLP ON SMLP.intCompanyLocationId = ARI.intCompanyLocationId AND (ysnARInvoice = 1 OR ysnDefault = 1)
-LEFT JOIN tblSMLogoPreferenceFooter SMLPF ON SMLPF.intCompanyLocationId = ARI.intCompanyLocationId AND (SMLPF.ysnARInvoice = 1 OR SMLPF.ysnDefault = 1)
 WHERE ARI.intInvoiceId BETWEEN @intInvoiceIdFrom AND @intInvoiceIdTo 
 OR ARI.intInvoiceId IN (SELECT intID FROM fnGetRowsFromDelimitedValues(@strInvoiceIds))
 OR ARI.dtmDate BETWEEN @dtmDateFrom AND @dtmDateTo
