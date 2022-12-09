23a9f6d3a2b47c1e1b4bed0b2d5191c5527314a7
Jeff Trajano <jeffrey.trajano@irely.com>
2022-12-09 12:19:13 +0800

    GL-9829

--- i21Database/dbo/Stored Procedures/uspGLPostRecap.sql
+++ i21Database/dbo/Stored Procedures/uspGLPostRecap.sql
@@ -63,6 +63,7 @@ INSERT INTO tblGLPostRecap (
 		,[strRateType]
 		,strOverrideAccountError
 		,strNewAccountIdOverride
+		,intLOBSegmentOverrideId
 		,[intConcurrencyId]
 )
 -- RETRIEVE THE DATA FROM THE TABLE VARIABLE. 
@@ -107,6 +108,7 @@ SELECT	[dtmDate]
 		,[strRateType] = ISNULL(Rate.strCurrencyExchangeRateType, forexRateType.strCurrencyExchangeRateType) 
 		,strOverrideAccountError
 		,strNewAccountIdOverride
+		,intLOBSegmentOverrideId
 		,[intConcurrencyId] = 1
 FROM	@RecapTable udtRecap LEFT JOIN tblGLAccount gl
 			ON udtRecap.intAccountId = gl.intAccountId
