660d6124d2ebb8b4abcb9c3261acaa13ab81e37a
Mon Gonzales <mon.gonzales@irely.com>
2022-12-09 14:46:57 +0800

    SC-4822 - HDTN-280442 - add the "on hold" for the inbound side just like we did intransit on the outbound side.
    Database object

---
+++ i21Database/dbo/Tables/tblSCTicketApplyContractAllocation.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[tblSCTicketApplyContractAllocation]
+(
+	intTicketApplyContractAllocationId INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	intTicketApplyTicketId INT NOT NULL,
+	intTicketApplyContractId INT NOT NULL,
+	dblUnit NUMERIC(38, 20) NULL,
+
+	intConcurrencyId INT NOT NULL DEFAULT(1),
+
+	CONSTRAINT [FK_TicketApplyContractAllocation_TicketApplyTicket_TicketApplyId]
+		FOREIGN KEY (intTicketApplyTicketId)
+		REFERENCES dbo.tblSCTicketApplyTicket(intTicketApplyTicketId),
+
+	CONSTRAINT [FK_TicketApplyContractAllocation_TicketApplyContract_TicketApplyContractId]
+		FOREIGN KEY (intTicketApplyContractId)
+		REFERENCES dbo.tblSCTicketApplyContract(intTicketApplyContractId),
+
+)
