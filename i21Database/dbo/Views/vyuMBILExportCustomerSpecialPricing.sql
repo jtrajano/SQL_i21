9e2ed2adc3b2fc5bc94a7f2abbc55e335a1ab9bf
Kenneth Sam Perez <kenneth.sam.perez@iRely.com>
2022-12-12 09:09:46 +0800

    MBIL-1082

--- i21Database/dbo/Views/vyuMBILExportCustomerSpecialPricing.sql
+++ i21Database/dbo/Views/vyuMBILExportCustomerSpecialPricing.sql
@@ -1,64 +1,93 @@
 ﻿CREATE VIEW [dbo].[vyuMBILExportCustomerSpecialPricing]
  AS   
-	SELECT   
-	ROW_NUMBER() OVER(ORDER BY strCustomerNumber) AS intCustomerPricingId  
-	,b.strCustomerNumber
-	,ICItems.strItemNo
-	,ISNULL(c.dblPrice ,0) as dblPrice
-	,c.strPricing  COLLATE Latin1_General_CI_AS AS strPricing
-	,z.intEntityId
-	,ICItems.intLocationId
-	  
-	from tblARCustomerSpecialPrice a  
-	join tblARCustomer b  
-	on a.intEntityCustomerId = b.[intEntityId]  
-	join tblEMEntity z  
-	on a.intEntityCustomerId = z.intEntityId  
-	join tblEMEntityLocation d  
-	on d.intEntityId = b.[intEntityId] --and d.ysnDefaultLocation = 1  
-	  
-	INNER JOIN  vyuICGetItemStock ICItems 
-			ON (a.intCategoryId = ICItems.intCategoryId
-				AND ICItems.intLocationId = d.intWarehouseId 
-				AND a.intItemId IS NULL)   
-			OR (a.intItemId = ICItems.intItemId
-				AND ICItems.intLocationId = d.intWarehouseId
-				AND a.intItemId IS NOT NULL)   
-	INNER JOIN (SELECT DISTINCT intItemId FROM [vyuETExportItem]) ETItems ON ICItems.intItemId = ETItems.intItemId
+	WITH
+		Items
+		as
+		(
+			Select I.intCategoryId, I.intItemId, IL.intLocationId, I.strItemNo, StockUOM.intItemUOMId
+			From tblICItem I
+				INNER JOIN tblICItemLocation IL on I.intItemId = IL.intItemId
+				LEFT JOIN tblICItemUOM StockUOM on StockUOM.intItemId = I.intItemId AND StockUOM.ysnStockUnit = 1
 
-	CROSS APPLY dbo.[fnARGetItemPricingDetails](  
-			ICItems.intItemId,  
-			b.[intEntityId],  
-			ICItems.intLocationId,  
-			ICItems.intStockUOMId,  
-			(SELECT TOP 1 intDefaultCurrencyId FROM tblSMCompanyPreference),  
-			cast(GetDate() as date),  
-			1,  
-			null,    
-			null,    
-			null,
-			null,
-			null,
-			null,
-			null,    
-			null,     
-			null,    
-			null,       
-			null,    
-			1,
-			null,  
-			null,   
-			null,      
-			null,     
-			null,    
-			b.intShipToId,  
-			null,    
-			null,    
-			null,   
-			NULL,  
-			null,  
-			0,  
-			1.000000,  
-			null,
-			0  
-			) c  
+		),
+		ETItems
+		AS
+		(
+			SELECT DISTINCT intItemId
+			FROM [vyuETExportItem]
+		),
+		MainItems
+		AS
+		(
+			SELECT a.*
+			FROM Items a
+				JOIN ETItems b ON b.intItemId = a.intItemId
+		)
+,
+		CompPref
+		AS
+		(
+			SELECT TOP 1
+				intDefaultCurrencyId
+			FROM tblSMCompanyPreference
+		),
+		Main
+		as
+		(
+			SELECT
+				DISTINCT
+				b.strCustomerNumber  
+	, ICItems.strItemNo  
+	--,ISNULL(c.dblPrice ,0) as dblPrice  
+	--,c.strPricing  COLLATE Latin1_General_CI_AS AS strPricing  
+	, z.intEntityId  
+	, ICItems.intLocationId  
+	, intShipToId
+	, ICItems.intItemId
+	, ICItems.intItemUOMId
+    , (SELECT TOP 1
+					intDefaultCurrencyId
+				FROM tblSMCompanyPreference) AS intDefaultCurrencyId
+			from tblARCustomerSpecialPrice a
+				join tblARCustomer b
+				on a.intEntityCustomerId = b.[intEntityId]
+				join tblEMEntity z
+				on a.intEntityCustomerId = z.intEntityId
+				join tblEMEntityLocation d
+				on d.intEntityId = b.[intEntityId] --and d.ysnDefaultLocation = 1    
+
+				INNER JOIN MainItems ICItems
+				ON (a.intCategoryId = ICItems.intCategoryId
+					AND ICItems.intLocationId = d.intWarehouseId
+					AND a.intItemId IS NULL)
+					OR (a.intItemId = ICItems.intItemId
+					AND ICItems.intLocationId = d.intWarehouseId
+					AND a.intItemId IS NOT NULL)
+			Where strPriceBasis <> 'O'
+		)
+
+	SELECT
+		ROW_NUMBER() OVER(ORDER BY strCustomerNumber) AS intCustomerPricingId   
+	, strCustomerNumber  
+	, strItemNo
+	, ISNULL(c.dblPrice ,0) as dblPrice  
+	, c.strPricing  COLLATE Latin1_General_CI_AS AS strPricing  
+	, intEntityId  
+	, intLocationId
+	From Main
+	OUTER APPLY dbo.[fnARGetCustomerPricingDetails](    
+		intItemId
+		,intEntityId
+		,intLocationId
+		,intItemUOMId
+		,cast(getdate() as date)
+		,null
+		,null
+		,null
+		,null
+		,intShipToId
+		,null
+		,null
+		,0
+		,intDefaultCurrencyId
+		) c
