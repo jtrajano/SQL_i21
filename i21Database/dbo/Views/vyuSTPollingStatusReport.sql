bf4e95d5cb962176be15898959e7bde48c95b2bb
Renze Cuadra <renze.cuadra@iRely.com>
2022-12-12 14:55:43 +0800

    CS-255

--- i21Database/dbo/Views/vyuSTPollingStatusReport.sql
+++ i21Database/dbo/Views/vyuSTPollingStatusReport.sql
@@ -8,99 +8,6 @@ stcpew.intCheckoutProcessErrorWarningId,
 stcp.intCheckoutId, 
 stcp.strGuid, 
 FORMAT(stcp.dtmCheckoutProcessDate, 'd','us') AS strReportDate,
-(
-	SELECT COUNT('') FROM (SELECT DISTINCT  intStoreId 
-	FROM tblSTCheckoutProcess 
-	WHERE FORMAT(dtmCheckoutProcessDate, 'd','us') = FORMAT(stcp.dtmCheckoutProcessDate, 'd','us') 
-	GROUP BY intStoreId) a
-) AS intProcessedStore,
-
-(
-	SELECT COUNT('') FROM (SELECT DISTINCT chIn.dtmCheckoutDate  
-	FROM tblSTCheckoutProcess cpIn
-	JOIN tblSTCheckoutHeader chIn
-		ON cpIn.intCheckoutId = chIn.intCheckoutId
-	WHERE FORMAT(chIn.dtmCheckoutDate, 'd','us') = FORMAT(CH.dtmCheckoutDate, 'd','us') 
-	GROUP BY chIn.dtmCheckoutDate) a
-) AS intProcessedStorePerCheckoutDate,
-
-(
-	SELECT COUNT('') FROM (SELECT DISTINCT cpIn.intCheckoutId 
-	FROM tblSTCheckoutProcess cpIn
-	JOIN tblSTCheckoutHeader chIn
-		ON cpIn.intCheckoutId = chIn.intCheckoutId
-	WHERE cpIn.intCheckoutId = stcp.intCheckoutId
-	GROUP BY cpIn.intCheckoutId) a
-) AS intProcessedStorePerCheckout,
-
-(
-	SELECT COUNT('') FROM (SELECT DISTINCT cpIn.intStoreId 
-	FROM tblSTCheckoutProcess cpIn
-	JOIN tblSTCheckoutHeader chIn
-		ON cpIn.intCheckoutId = chIn.intCheckoutId
-	WHERE FORMAT(chIn.dtmCheckoutDate, 'd','us') = FORMAT(CH.dtmCheckoutDate, 'd','us') 
-	AND cpIn.intStoreId = stcp.intStoreId
-	GROUP BY cpIn.intStoreId) a
-) AS intProcessedStorePerCheckoutDateAndStore,
-
-(
-	SELECT COUNT('') FROM (SELECT DISTINCT cpIn.intStoreId 
-	FROM tblSTCheckoutProcess cpIn
-	JOIN tblSTCheckoutHeader chIn
-		ON cpIn.intCheckoutId = chIn.intCheckoutId
-	WHERE FORMAT(cpIn.dtmCheckoutProcessDate, 'd','us') = FORMAT(stcp.dtmCheckoutProcessDate, 'd','us') 
-	AND cpIn.intStoreId = stcp.intStoreId
-	GROUP BY cpIn.intStoreId) a
-) AS intProcessedStorePerProcessedDatePerStore,
-
-(
-	SELECT COUNT('') FROM (SELECT DISTINCT  intStoreId 
-	FROM tblSTCheckoutProcess cp
-	JOIN tblSTCheckoutProcessErrorWarning spew
-		ON cp.intCheckoutProcessId = spew.intCheckoutProcessId
-	WHERE FORMAT(dtmCheckoutProcessDate, 'd','us') = FORMAT(stcp.dtmCheckoutProcessDate, 'd','us') 
-	AND (spew.strMessageType = 'F' OR spew.strMessageType = 'S')
-	GROUP BY intStoreId) a
-) AS intStoreWithError,
-
-(
-	SELECT COUNT('') FROM (SELECT DISTINCT chIn.dtmCheckoutDate, cp.intStoreId 
-	FROM tblSTCheckoutProcess cp
-	JOIN tblSTCheckoutProcessErrorWarning spew
-		ON cp.intCheckoutProcessId = spew.intCheckoutProcessId
-	JOIN tblSTCheckoutHeader chIn
-		ON cp.intCheckoutId = chIn.intCheckoutId
-	WHERE FORMAT(chIn.dtmCheckoutDate, 'd','us') = FORMAT(CH.dtmCheckoutDate, 'd','us') 
-	AND (spew.strMessageType = 'F' OR spew.strMessageType = 'S')
-	GROUP BY chIn.dtmCheckoutDate, cp.intStoreId) a
-) AS intStoreWithErrorPerCheckoutDate,
-
-(
-	SELECT COUNT('') FROM (SELECT DISTINCT cp.intStoreId 
-	FROM tblSTCheckoutProcess cp
-	JOIN tblSTCheckoutProcessErrorWarning spew
-		ON cp.intCheckoutProcessId = spew.intCheckoutProcessId
-	JOIN tblSTCheckoutHeader chIn
-		ON cp.intCheckoutId = chIn.intCheckoutId
-	WHERE FORMAT(chIn.dtmCheckoutDate, 'd','us') = FORMAT(CH.dtmCheckoutDate, 'd','us') 
-	AND cp.intStoreId = stcp.intStoreId
-	AND (spew.strMessageType = 'F' OR spew.strMessageType = 'S')
-	GROUP BY cp.intStoreId) a
-) AS intStoreWithErrorPerCheckoutDatePerStore,
-
-(
-	SELECT COUNT('') FROM (SELECT DISTINCT cp.intStoreId 
-	FROM tblSTCheckoutProcess cp
-	JOIN tblSTCheckoutProcessErrorWarning spew
-		ON cp.intCheckoutProcessId = spew.intCheckoutProcessId
-	JOIN tblSTCheckoutHeader chIn
-		ON cp.intCheckoutId = chIn.intCheckoutId
-	WHERE FORMAT(cp.dtmCheckoutProcessDate, 'd','us') = FORMAT(stcp.dtmCheckoutProcessDate, 'd','us') 
-	AND cp.intStoreId = stcp.intStoreId
-	AND (spew.strMessageType = 'F' OR spew.strMessageType = 'S')
-	GROUP BY cp.intStoreId) a
-) AS intStoreWithErrorPerProcessDatePerStore,
-
 stcp.dtmCheckoutProcessDate, 
 CAST((CASE WHEN CH.strCheckoutCloseDate IS NULL THEN DATEADD(DAY, 1, stcp.dtmCheckoutProcessDate) ELSE DATEADD(DAY, 1, CH.strCheckoutCloseDate) END) AS DATETIME) AS dtmCurrentBusinessDay, 
 CAST((CASE WHEN CH.strCheckoutCloseDate IS NULL THEN stcp.dtmCheckoutProcessDate ELSE CAST(CH.strCheckoutCloseDate AS VARCHAR(50)) END) AS VARCHAR(50)) AS strCheckoutCloseDate, 
@@ -108,9 +15,7 @@ ISNULL(CH.dtmCheckoutDate, CH.dtmCountDate) AS dtmCheckoutDate,
 sts.intStoreNo, 
 sts.strDescription, 
 stcpew.strMessageType, 
-stcpew.strMessage,
-0 AS rn, 
-'' COLLATE Latin1_General_CI_AS AS strReportFilter
+stcpew.strMessage
 FROM dbo.tblSTCheckoutProcessErrorWarning AS stcpew 
 INNER JOIN dbo.tblSTCheckoutProcess AS stcp 
 	ON stcpew.intCheckoutProcessId = stcp.intCheckoutProcessId 
