85014ae294f551602446db0e4e2fb295cdc106cb
Mon Gonzales <mon.gonzales@irely.com>
2022-12-14 15:27:41 +0800

    SC-4822 - HDTN-280442 - add the "on hold" for the inbound side just like we did intransit on the outbound side.
    Unique No for hold ticket

--- i21Database/dbo/Tables/tblSCTicketApply.sql
+++ i21Database/dbo/Tables/tblSCTicketApply.sql
@@ -3,6 +3,7 @@
 	[intTicketApplyId] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
 	[intEntityId] INT NOT NULL,	
 	[intItemId] INT NOT NULL,			   
+	[strHoldNo] NVARCHAR (50)   COLLATE Latin1_General_CI_AS NULL,
 	intConcurrencyId INT NOT NULL DEFAULT(1)
 
 		
