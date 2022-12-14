728599d38773c8e26a584b82e5ad77b9e3202118
LexPeregrino <lex.peregrino@irely.com>
2022-12-14 16:17:57 +0800

    CT-8208 v1
    (cherry picked from commit 9f21ebed57423ea464ffe7325d96a522dd052cdf)

--- i21Database/Scripts/Post-Deployment/CT/DefaultData.sql
+++ i21Database/Scripts/Post-Deployment/CT/DefaultData.sql
@@ -15,6 +15,7 @@ MERGE INTO tblCTBasisCost AS destination
 			 SELECT 
 				 ICI.strItemNo ,
 				 ICI.intItemId,
+				 CASE WHEN ICI.strCostMethod = '' THEN 'Amount' ELSE ICI.strCostMethod END strCostMethod,
 				 ROW_NUMBER() OVER (ORDER BY CC.intPriority DESC) as [intPriority] ,
 				 ROW_NUMBER() OVER (ORDER BY CC.intPriority DESC) as [intSort],
 				 ISNULL(CC.[intConcurrencyId],1) as [intConcurrencyId]
@@ -29,6 +30,7 @@ MERGE INTO tblCTBasisCost AS destination
 		(
 				[strItemNo]
 				,[intItemId]
+				,[strCostMethod]
 				,[intPriority]
 				,[intSort]
 				,[intConcurrencyId]
@@ -37,6 +39,7 @@ MERGE INTO tblCTBasisCost AS destination
 		(
 			SourceData.strItemNo
 			,SourceData.intItemId
+			,SourceData.strCostMethod
 			,SourceData.[intPriority]
 			,SourceData.[intSort]
 			,SourceData.[intConcurrencyId]
