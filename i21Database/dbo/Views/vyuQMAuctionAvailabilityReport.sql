a2110966a065899a4a53e8df4cfd1a64f34ad976
Christian Joel Martinez <Christian.Martinez@irely.com>
2022-12-15 21:03:50 +0800

    QC-740

--- i21Database/dbo/Views/vyuQMAuctionAvailabilityReport.sql
+++ i21Database/dbo/Views/vyuQMAuctionAvailabilityReport.sql
@@ -12,9 +12,11 @@ FROM tblQMSample S
 INNER JOIN vyuEMSearchEntityBroker EB ON EB.intEntityId = S.intBrokerId
 INNER JOIN tblICCommodityAttribute TEA_TYPE ON TEA_TYPE.intCommodityAttributeId = S.intManufacturingLeafTypeId
 INNER JOIN tblICItem I ON I.intItemId = S.intItemId
+INNER JOIN tblARMarketZone MZ ON MZ.intMarketZoneId = S.intMarketZoneId
 LEFT JOIN tblMFBatch B ON B.intSampleId = S.intSampleId
 WHERE B.intBatchId IS NULL
 AND (S.strSaleNumber IS NOT NULL AND S.strSaleNumber <> '')
+AND MZ.strMarketZoneCode = 'AUC' --Auction only
 GROUP BY
     S.strSaleNumber
     ,TEA_TYPE.strDescription
