92e8b4323b68c1b1c7532e8e86ceb9422eefb9d1
Roudinee Panteria <roudinee.panteria@irely.com>
2022-12-15 19:09:44 +0800

    AR-15909

--- i21Database/dbo/Tables/tblARInvoiceReportStagingTable.sql
+++ i21Database/dbo/Tables/tblARInvoiceReportStagingTable.sql
@@ -149,5 +149,6 @@
 	[strSWIFT]							NVARCHAR (MAX)  COLLATE Latin1_General_CI_AS NULL,
 	[strBICCode]						NVARCHAR (MAX)  COLLATE Latin1_General_CI_AS NULL,
 	[dblServiceChargeAPR]				NUMERIC (18, 6)	NULL,
-	[strLogoType]						NVARCHAR (10)	COLLATE Latin1_General_CI_AS NULL
+	[strLogoType]						NVARCHAR (10)	COLLATE Latin1_General_CI_AS NULL,
+	[dtmDueDateSC]						DATETIME		NULL
 );
