9be6250b1a24de0ccb57f407a6d29eb307806df6
Kenneth Sam Perez <kenneth.sam.perez@iRely.com>
2022-12-16 17:14:12 +0800

    MBIL-968

---
+++ i21Database/dbo/Views/vyuMBILGetLoads.sql
@@ -0,0 +1,61 @@
+﻿CREATE VIEW [dbo].[vyuMBILGetLoads]
+AS 
+SELECT 
+  lh.intLoadHeaderId, 
+  lh.strLoadNumber, 
+  lh.strType, 
+  lh.intDispatchOrderId,
+  lh.intLoadId, 
+  lh.intDriverId, 
+  lh.intHaulerId as intShipViaId, 
+  lh.intTruckId, 
+  lh.intTrailerId, 
+  lh.strTrailerNo, 
+  pd.intPickupDetailId, 
+  pd.intEntityId as intVendorId, 
+  pd.intEntityLocationId as intVendorLocationId, 
+  sp.intSupplyPointId, 
+  pd.intLoadDetailId, 
+  pd.intSalespersonId, 
+  pd.intSellerId, 
+  pd.intTaxGroupId, 
+  pd.intContractDetailId, 
+  pd.intItemId, 
+  pd.intCompanyLocationId as intReceiptCompanyLocationId, 
+  pd.dblPickupQuantity, 
+  pd.dblGross, 
+  pd.dblNet, 
+  pd.dtmPickupFrom, 
+  pd.dtmPickupTo, 
+  pd.dtmActualPickupFrom, 
+  pd.dtmActualPickupTo, 
+  pd.strBOL, 
+  pd.strItemUOM, 
+  pd.strLoadRefNo, 
+  pd.strNote, 
+  pd.strPONumber, 
+  pd.strRack, 
+  pd.strTerminalRefNo, 
+  dh.intDeliveryHeaderId, 
+  dh.intEntityId as intCustomerId, 
+  dh.intEntityLocationId as intCustomerLocationId, 
+  dh.intCompanyLocationId as intDistributionCompanyLocationId, 
+  dh.intSalesPersonId, 
+  dh.dtmActualDelivery, 
+  dd.intDeliveryDetailId, 
+  dd.intTMDispatchId, 
+  dd.intTMSiteId, 
+  dd.strTank, 
+  dd.dblStickStartReading, 
+  dd.dblStickEndReading, 
+  dd.dblWaterInches, 
+  dd.dblPrice,  
+  dd.dblDeliveredQty, 
+  dd.dblPercentFull 
+FROM 
+  tblMBILPickupDetail pd 
+  JOIN tblMBILDeliveryDetail dd ON dd.intPickupDetailId = pd.intPickupDetailId 
+  LEFT JOIN tblTRSupplyPoint sp ON sp.intEntityVendorId = pd.intEntityId  AND 
+							  sp.intEntityLocationId = pd.intEntityLocationId 
+  JOIN tblMBILDeliveryHeader dh ON dh.intDeliveryHeaderId = dd.intDeliveryHeaderId 
+  JOIN tblMBILLoadHeader lh ON lh.intLoadHeaderId = dh.intLoadHeaderId
