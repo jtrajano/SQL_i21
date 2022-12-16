8e738edb3d6a65a8b0781479068b0e271a792db8
Christian Joel Martinez <Christian.Martinez@irely.com>
2022-12-16 14:28:22 +0800

    QC-890

--- i21Database/dbo/Views/vyuQMAuctionAvailabilityReport.sql
+++ i21Database/dbo/Views/vyuQMAuctionAvailabilityReport.sql
@@ -2,7 +2,7 @@ CREATE VIEW [dbo].[vyuQMAuctionAvailabilityReport]
 AS
 SELECT
     S.strSaleNumber
-    ,[strTeaType] = TEA_TYPE.strDescription
+    ,[strTeaType] = CT.strCatalogueType
     ,I.intItemId
     ,I.strItemNo
     ,EB.intEntityId
@@ -10,7 +10,7 @@ SELECT
     ,[dblQty] = SUM(ISNULL(S.dblRepresentingQty, 0))
 FROM tblQMSample S
 INNER JOIN vyuEMSearchEntityBroker EB ON EB.intEntityId = S.intBrokerId
-INNER JOIN tblICCommodityAttribute TEA_TYPE ON TEA_TYPE.intCommodityAttributeId = S.intManufacturingLeafTypeId
+INNER JOIN tblQMCatalogueType CT ON CT.intCatalogueTypeId = S.intCatalogueTypeId
 INNER JOIN tblICItem I ON I.intItemId = S.intItemId
 INNER JOIN tblARMarketZone MZ ON MZ.intMarketZoneId = S.intMarketZoneId
 LEFT JOIN tblMFBatch B ON B.intSampleId = S.intSampleId
@@ -19,7 +19,7 @@ AND (S.strSaleNumber IS NOT NULL AND S.strSaleNumber <> '')
 AND MZ.strMarketZoneCode = 'AUC' --Auction only
 GROUP BY
     S.strSaleNumber
-    ,TEA_TYPE.strDescription
+    ,CT.strCatalogueType
     ,I.intItemId
     ,I.strItemNo
     ,EB.intEntityId
