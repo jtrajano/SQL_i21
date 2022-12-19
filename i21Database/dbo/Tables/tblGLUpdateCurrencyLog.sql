bff64918867291b31f9746146f36057fd7c3bd8d
Mark Daniel Marmeto <mark.marmeto@irely.com>
2022-12-19 13:37:05 +0800

    GL-9773

---
+++ i21Database/dbo/Tables/tblGLUpdateCurrencyLog.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[tblGLUpdateCurrencyLog]
+(
+	[intUpdateCurrencyLogId] INT IDENTITY(1, 1) NOT NULL,
+	[intEntityId] INT NULL,
+	[intAccountId] INT NULL,
+	[intOldCurrencyId] INT NULL,
+	[intNewCurrencyId] INT NULL,
+	[strDescription] NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NULL,
+	[dtmDate] DATETIME,
+	[intConcurrencyId] INT DEFAULT 1 NOT NULL
+
+	CONSTRAINT [PK_tblGLUpdateCurrencyLog] PRIMARY KEY CLUSTERED ([intUpdateCurrencyLogId] ASC)
+)
