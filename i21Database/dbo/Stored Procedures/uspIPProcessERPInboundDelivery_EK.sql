585a7dac86ef3ff9a215a2cac97eee106a3b7808
Prem Selladurai <prem.selladurai@irely.com>
2022-12-19 10:16:48 +0530

    IN-1081
    IN-1081

---
+++ i21Database/dbo/Stored Procedures/uspIPProcessERPInboundDelivery_EK.sql
@@ -0,0 +1,276 @@
+﻿CREATE PROCEDURE [dbo].[uspIPProcessERPInboundDelivery_EK] @strInfo1 NVARCHAR(MAX) = '' OUT
+	,@strInfo2 NVARCHAR(MAX) = '' OUT
+	,@intNoOfRowsAffected INT = 0 OUT
+AS
+BEGIN TRY
+	SET QUOTED_IDENTIFIER OFF
+	SET ANSI_NULLS ON
+	SET NOCOUNT ON
+	SET XACT_ABORT ON
+	SET ANSI_WARNINGS ON
+
+	DECLARE @intInboundDeliveryStageId INT
+	DECLARE @ErrMsg NVARCHAR(max)
+	DECLARE @dtmDate DATETIME
+	DECLARE @intUserId INT
+	DECLARE @strUserName NVARCHAR(100)
+	DECLARE @strFinalErrMsg NVARCHAR(MAX) = ''
+		,@strCompanyLocation NVARCHAR(50)
+		,@dtmCreatedDate DATETIME
+		,@strCreatedBy NVARCHAR(50)
+		,@strPONumber NVARCHAR(50)
+		,@strPOLineItemNo NVARCHAR(50)
+		,@strPOStatus NVARCHAR(50)
+		,@strBatchId NVARCHAR(50)
+		,@strContainerNo NVARCHAR(50)
+		,@strBOLNo NVARCHAR(50)
+		,@dtmStockDate DATETIME
+		,@strFreightAgent NVARCHAR(50)
+		,@strSealNo NVARCHAR(50)
+		,@strContainerType NVARCHAR(50)
+		,@strVoyage NVARCHAR(50)
+		,@strVessel NVARCHAR(50)
+		,@intCompanyLocationId INT
+		,@strError NVARCHAR(MAX)
+
+	SELECT @dtmDate = GETDATE()
+
+	SELECT @intUserId = intEntityId
+	FROM tblSMUserSecurity WITH (NOLOCK)
+	WHERE strUserName = 'IRELYADMIN'
+
+	DECLARE @tblIPInboundDeliveryStage TABLE (intInboundDeliveryStageId INT)
+
+	INSERT INTO @tblIPInboundDeliveryStage
+	SELECT intInboundDeliveryStageId
+	FROM tblIPInboundDeliveryStage
+	WHERE intStatusId IS NULL
+
+	UPDATE tblIPInboundDeliveryStage
+	SET intStatusId = - 1
+	WHERE intInboundDeliveryStageId IN (
+			SELECT intInboundDeliveryStageId
+			FROM @tblIPInboundDeliveryStage
+			)
+
+	SELECT @intInboundDeliveryStageId = MIN(intInboundDeliveryStageId)
+	FROM @tblIPInboundDeliveryStage
+
+	SELECT @strInfo1 = ''
+
+	SELECT @strInfo1 = @strInfo1 + ISNULL(b.strBatchId, '') + ', '
+	FROM @tblIPInboundDeliveryStage a
+	JOIN tblIPInboundDeliveryStage b ON a.intInboundDeliveryStageId = b.intInboundDeliveryStageId
+
+	IF Len(@strInfo1) > 0
+	BEGIN
+		SELECT @strInfo1 = Left(@strInfo1, Len(@strInfo1) - 1)
+	END
+
+	WHILE @intInboundDeliveryStageId IS NOT NULL
+	BEGIN
+		BEGIN TRY
+			SELECT @strCompanyLocation = NULL
+				,@dtmCreatedDate = NULL
+				,@strCreatedBy = NULL
+				,@strPONumber = NULL
+				,@strPOLineItemNo = NULL
+				,@strPOStatus = NULL
+				,@strBatchId = NULL
+				,@strContainerNo = NULL
+				,@strBOLNo = NULL
+				,@dtmStockDate = NULL
+				,@strFreightAgent = NULL
+				,@strSealNo = NULL
+				,@strContainerType = NULL
+				,@strVoyage = NULL
+				,@strVessel = NULL
+
+			SELECT @strCompanyLocation = strCompanyLocation
+				,@dtmCreatedDate = dtmCreatedDate
+				,@strPONumber = strPONumber
+				,@strPOLineItemNo = strPOLineItemNo
+				,@strPOStatus = strPOStatus
+				,@strBatchId = strBatchId
+				,@strContainerNo = strContainerNo
+				,@strBOLNo = strBOLNo
+				,@dtmStockDate = dtmStockDate
+				,@strFreightAgent = strFreightAgent
+				,@strSealNo = strSealNo
+				,@strContainerType = strContainerType
+				,@strVoyage = strVoyage
+				,@strVessel = strVessel
+			FROM tblIPInboundDeliveryStage
+			WHERE intInboundDeliveryStageId = @intInboundDeliveryStageId
+
+			SELECT @intCompanyLocationId = NULL
+
+			SELECT @intCompanyLocationId = intCompanyLocationId
+			FROM dbo.tblSMCompanyLocation
+			WHERE strLocationNumber = @strCompanyLocation
+
+			IF @intCompanyLocationId IS NULL
+			BEGIN
+				SELECT @strError = 'Company Location ' + @strCompanyLocation + 'is not available.'
+
+				RAISERROR (
+						@strError
+						,16
+						,1
+						)
+			END
+
+			BEGIN TRAN
+
+			UPDATE tblMFBatch
+			SET strERPPONumber = @strPONumber
+				,strERPPOLineNo = @strPOLineItemNo
+				,strPOStatus = (
+					CASE 
+						WHEN @strPOStatus = 'A'
+							THEN 'PO Open'
+						ELSE 'Completed'
+						END
+					)
+				,strContainerNumber = @strContainerNo
+				,strBOLNo = @strBOLNo
+				,dtmStock = @dtmStockDate
+				,strFreightAgent = @strFreightAgent
+				,strSealNumber = @strSealNo
+				,strContainerType = @strContainerType
+				,strVoyage = @strVoyage
+				,strVessel = @strVessel
+			WHERE strBatchId = @strBatchId
+
+			MOVE_TO_ARCHIVE:
+
+			--Move to Ack
+			INSERT INTO tblIPInboundDeliveryArchive (
+				intInboundDeliveryArchiveId
+				,intTrxSequenceNo
+				,strCompanyLocation
+				,dtmCreatedDate
+				,strPONumber
+				,strPOLineItemNo
+				,strPOStatus
+				,strBatchId
+				,strContainerNo
+				,strBOLNo
+				,dtmStockDate
+				,strFreightAgent
+				,strSealNo
+				,strContainerType
+				,strVoyage
+				,strVessel
+				)
+			SELECT intInboundDeliveryStageId
+				,intTrxSequenceNo
+				,strCompanyLocation
+				,dtmCreatedDate
+				,strPONumber
+				,strPOLineItemNo
+				,strPOStatus
+				,strBatchId
+				,strContainerNo
+				,strBOLNo
+				,dtmStockDate
+				,strFreightAgent
+				,strSealNo
+				,strContainerType
+				,strVoyage
+				,strVessel
+			FROM tblIPInboundDeliveryStage
+			WHERE intInboundDeliveryStageId = @intInboundDeliveryStageId
+
+			DELETE
+			FROM tblIPInboundDeliveryStage
+			WHERE intInboundDeliveryStageId = @intInboundDeliveryStageId
+
+			COMMIT TRAN
+		END TRY
+
+		BEGIN CATCH
+			IF XACT_STATE() != 0
+				AND @@TRANCOUNT > 0
+				ROLLBACK TRANSACTION
+
+			SET @ErrMsg = NULL
+			SET @ErrMsg = ERROR_MESSAGE()
+			SET @strFinalErrMsg = @strFinalErrMsg + @ErrMsg
+
+			--Move to Error
+			INSERT INTO tblIPInboundDeliveryError (
+				intInboundDeliveryErrorId
+				,intTrxSequenceNo
+				,strCompanyLocation
+				,dtmCreatedDate
+				,strPONumber
+				,strPOLineItemNo
+				,strPOStatus
+				,strBatchId
+				,strContainerNo
+				,strBOLNo
+				,dtmStockDate
+				,strFreightAgent
+				,strSealNo
+				,strContainerType
+				,strVoyage
+				,strVessel
+				,strMessage
+				)
+			SELECT intInboundDeliveryStageId
+				,intTrxSequenceNo
+				,strCompanyLocation
+				,dtmCreatedDate
+				,strPONumber
+				,strPOLineItemNo
+				,strPOStatus
+				,strBatchId
+				,strContainerNo
+				,strBOLNo
+				,dtmStockDate
+				,strFreightAgent
+				,strSealNo
+				,strContainerType
+				,strVoyage
+				,strVessel
+				,@ErrMsg AS strStatusText
+			FROM tblIPInboundDeliveryStage
+			WHERE intInboundDeliveryStageId = @intInboundDeliveryStageId
+
+			DELETE
+			FROM tblIPInboundDeliveryStage
+			WHERE intInboundDeliveryStageId = @intInboundDeliveryStageId
+		END CATCH
+
+		SELECT @intInboundDeliveryStageId = MIN(intInboundDeliveryStageId)
+		FROM @tblIPInboundDeliveryStage
+		WHERE intInboundDeliveryStageId > @intInboundDeliveryStageId
+	END
+
+	UPDATE tblIPInboundDeliveryStage
+	SET intStatusId = NULL
+	WHERE intInboundDeliveryStageId IN (
+			SELECT intInboundDeliveryStageId
+			FROM @tblIPInboundDeliveryStage
+			)
+		AND intStatusId = - 1
+
+	IF ISNULL(@strFinalErrMsg, '') <> ''
+		RAISERROR (
+				@strFinalErrMsg
+				,16
+				,1
+				)
+END TRY
+
+BEGIN CATCH
+	SET @ErrMsg = ERROR_MESSAGE()
+
+	RAISERROR (
+			@ErrMsg
+			,16
+			,1
+			,'WITH NOWAIT'
+			)
+END CATCH
---
+++ i21Database/dbo/Stored Procedures/uspIPStageERPInboundDelivery_EK.sql
@@ -0,0 +1,194 @@
+﻿CREATE PROCEDURE [dbo].[uspIPStageERPInboundDelivery_EK] @strInfo1 NVARCHAR(MAX) = '' OUTPUT
+	,@strInfo2 NVARCHAR(MAX) = '' OUTPUT
+	,@intNoOfRowsAffected INT = 0 OUTPUT
+AS
+BEGIN TRY
+	SET QUOTED_IDENTIFIER OFF
+	SET ANSI_NULLS ON
+	SET NOCOUNT ON
+	SET XACT_ABORT ON
+	SET ANSI_WARNINGS OFF
+
+	DECLARE @tblIPBatch TABLE (strBatchId NVARCHAR(MAX))
+	DECLARE @idoc INT
+	DECLARE @ErrMsg NVARCHAR(MAX) = ''
+		,@intRowNo INT
+		,@strXml NVARCHAR(MAX)
+		,@strFinalErrMsg NVARCHAR(MAX) = ''
+	DECLARE @tblIPIDOCXMLStage TABLE (intIDOCXMLStageId INT)
+
+	INSERT INTO @tblIPIDOCXMLStage (intIDOCXMLStageId)
+	SELECT intIDOCXMLStageId
+	FROM tblIPIDOCXMLStage
+	WHERE strType = 'IBD'
+		AND intStatusId IS NULL
+
+	SELECT @intRowNo = MIN(intIDOCXMLStageId)
+	FROM @tblIPIDOCXMLStage
+
+	IF @intRowNo IS NULL
+	BEGIN
+		RETURN
+	END
+
+	UPDATE S
+	SET S.intStatusId = - 1
+	FROM tblIPIDOCXMLStage S
+	JOIN @tblIPIDOCXMLStage TS ON TS.intIDOCXMLStageId = S.intIDOCXMLStageId
+
+	WHILE (ISNULL(@intRowNo, 0) > 0)
+	BEGIN
+		BEGIN TRY
+			BEGIN TRANSACTION
+
+			SELECT @strXml = NULL
+				,@idoc = NULL
+				,@intNoOfRowsAffected = 1
+
+			SELECT @strXml = strXml
+			FROM tblIPIDOCXMLStage
+			WHERE intIDOCXMLStageId = @intRowNo
+
+			SET @strXml = REPLACE(@strXml, 'utf-8' COLLATE Latin1_General_CI_AS, 'utf-16' COLLATE Latin1_General_CI_AS)
+
+			EXEC sp_xml_preparedocument @idoc OUTPUT
+				,@strXml
+
+			DELETE
+			FROM @tblIPBatch
+
+			INSERT INTO dbo.tblIPInboundDeliveryStage (
+				intTrxSequenceNo
+				,strCompanyLocation
+				,dtmCreatedDate
+				,strPONumber
+				,strPOLineItemNo
+				,strPOStatus
+				,strBatchId
+				,strContainerNo
+				,strBOLNo
+				,dtmStockDate
+				,strFreightAgent
+				,strSealNo
+				,strContainerType
+				,strVoyage
+				,strVessel
+				)
+			OUTPUT INSERTED.strBatchId
+			INTO @tblIPBatch
+			SELECT DocNo
+				,PurchGroup
+				,GETDATE()
+				,PONumber
+				,POLineItemNo
+				,POStatus
+				,BatchId
+				,ContainerNo
+				,BOLNo
+				,StockDate
+				,FreightAgent
+				,SealNo
+				,ContainerType
+				,Voyage
+				,Vessel
+			FROM OPENXML(@idoc, 'root/Header', 2) WITH (
+					DocNo BIGINT '../CtrlPoint/DocNo'
+					,PurchGroup NVARCHAR(50)
+					,PONumber NVARCHAR(50)
+					,POLineItemNo NVARCHAR(50)
+					,POStatus NVARCHAR(50)
+					,BatchId NVARCHAR(50)
+					,ContainerNo NVARCHAR(50)
+					,BOLNo NVARCHAR(50)
+					,StockDate DATETIME
+					,FreightAgent NVARCHAR(50)
+					,SealNo NVARCHAR(50)
+					,ContainerType NVARCHAR(50)
+					,Voyage NVARCHAR(50)
+					,Vessel NVARCHAR(50)
+					)
+
+			SELECT @strInfo1 = @strInfo1 + ISNULL(strBatchId, '') + ','
+			FROM @tblIPBatch
+
+			--Move to Archive
+			INSERT INTO tblIPIDOCXMLArchive (
+				strXml
+				,strType
+				,dtmCreatedDate
+				)
+			SELECT strXml
+				,strType
+				,dtmCreatedDate
+			FROM tblIPIDOCXMLStage
+			WHERE intIDOCXMLStageId = @intRowNo
+
+			DELETE
+			FROM tblIPIDOCXMLStage
+			WHERE intIDOCXMLStageId = @intRowNo
+
+			COMMIT TRANSACTION
+		END TRY
+
+		BEGIN CATCH
+			IF XACT_STATE() != 0
+				AND @@TRANCOUNT > 0
+				ROLLBACK TRANSACTION
+
+			SET @ErrMsg = ERROR_MESSAGE()
+			SET @strFinalErrMsg = @strFinalErrMsg + @ErrMsg
+
+			--Move to Error
+			INSERT INTO tblIPIDOCXMLError (
+				strXml
+				,strType
+				,strMsg
+				,dtmCreatedDate
+				)
+			SELECT strXml
+				,strType
+				,@ErrMsg
+				,dtmCreatedDate
+			FROM tblIPIDOCXMLStage
+			WHERE intIDOCXMLStageId = @intRowNo
+
+			DELETE
+			FROM tblIPIDOCXMLStage
+			WHERE intIDOCXMLStageId = @intRowNo
+		END CATCH
+
+		SELECT @intRowNo = MIN(intIDOCXMLStageId)
+		FROM @tblIPIDOCXMLStage
+		WHERE intIDOCXMLStageId > @intRowNo
+	END
+
+	UPDATE S
+	SET S.intStatusId = NULL
+	FROM tblIPIDOCXMLStage S
+	JOIN @tblIPIDOCXMLStage TS ON TS.intIDOCXMLStageId = S.intIDOCXMLStageId
+	WHERE S.intStatusId = - 1
+
+	IF (ISNULL(@strInfo1, '')) <> ''
+		SELECT @strInfo1 = LEFT(@strInfo1, LEN(@strInfo1) - 1)
+
+	IF @strFinalErrMsg <> ''
+		RAISERROR (
+				@strFinalErrMsg
+				,16
+				,1
+				)
+END TRY
+
+BEGIN CATCH
+	SET @ErrMsg = ERROR_MESSAGE()
+
+	IF @idoc <> 0
+		EXEC sp_xml_removedocument @idoc
+
+	RAISERROR (
+			@ErrMsg
+			,16
+			,1
+			,'WITH NOWAIT'
+			)
+END CATCH
