3f71a14efee4d94184ab9ed60b918750a5010a77
Feb Montefrio <feb.montefrio@irely.com>
2022-12-19 11:56:55 +0800

    [B] IC-11043 IC-11052

--- i21Database/dbo/Stored Procedures/uspICPostCostAdjustmentRetroactiveAvg.sql
+++ i21Database/dbo/Stored Procedures/uspICPostCostAdjustmentRetroactiveAvg.sql
@@ -442,7 +442,11 @@ BEGIN
 			AND t.intInventoryTransactionId >= @InventoryTransactionStartId
 			--AND t.intTransactionTypeId <> @INV_TRANS_TYPE_Cost_Adjustment
 			AND (c.strCostingMethod <> 'ACTUAL COST' OR t.strActualCostId IS NULL)
-			AND (t.dblQty <> 0 OR negativeStock.dblQty <> 0) 
+			AND (
+				t.dblQty <> 0 
+				OR negativeStock.dblQty <> 0
+				OR (t.intTransactionTypeId <> @INV_TRANS_TYPE_Cost_Adjustment)
+			) 
 
 	ORDER BY t.intInventoryTransactionId ASC 
 
@@ -516,7 +520,7 @@ BEGIN
 						@t_dblQty < 0 
 						AND @t_intTransactionTypeId = @INV_TRANS_TYPE_NegativeStock THEN 							
 							@t_dblQty * @NewAverageCost
-							+ (-@t_dblQty * @t_NegativeStockQty) 
+							+ (-@t_dblQty * @t_NegativeStockCost) 
 
 					WHEN @t_dblQty < 0 THEN 
 						@t_dblQty * @NewAverageCost 
