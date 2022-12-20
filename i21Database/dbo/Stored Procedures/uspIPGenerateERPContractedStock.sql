93a602ce4633df3cad635dc38a51787422f67078
Prem Selladurai <prem.selladurai@irely.com>
2022-12-20 20:21:05 +0530

    IN-1066
    IN-1066

--- i21Database/dbo/Stored Procedures/uspIPGenerateERPContractedStock.sql
+++ i21Database/dbo/Stored Procedures/uspIPGenerateERPContractedStock.sql
@@ -9,6 +9,7 @@ BEGIN TRY
 	SET NOCOUNT ON
 
 	DECLARE @strXML NVARCHAR(MAX)
+		,@strDetailXML NVARCHAR(MAX)
 		,@intContractedStockPreStageId INT
 		,@dtmCurrentDate DATETIME
 		,@ErrMsg   NVARCHAR(MAX)
@@ -42,9 +43,7 @@ BEGIN TRY
 			,dtmProcessedDate
 			)
 		SELECT intContractedStockPreStageId = ROW_NUMBER() OVER (
-				ORDER BY (
-						SELECT NULL
-						)
+				ORDER BY CH.intContractHeaderId
 				)
 			,intContractDetailId = CD.intContractDetailId
 			,dblBalanceQty = CD.dblQuantity - IsNULL(LD2.dblQuantity, 0)
@@ -58,6 +57,7 @@ BEGIN TRY
 			WHERE LD.intPContractDetailId = CD.intContractDetailId
 			) LD2
 		WHERE CD.intContractStatusId = 1 --Open
+		AND CD.dblQuantity - IsNULL(LD2.dblQuantity, 0)>0
 		ORDER BY CH.intContractHeaderId
 	END
 
@@ -82,9 +82,9 @@ BEGIN TRY
 	IF ISNULL(@tmp, 0) = 0
 		SELECT @tmp = 50
 
-	IF @offset > @tmp
+	IF @limit > @tmp
 	BEGIN
-		SELECT @offset = @tmp
+		SELECT @limit = @tmp
 	END
 
 	INSERT INTO @tblIPContractedStockPreStage (intContractedStockPreStageId)
@@ -107,18 +107,18 @@ BEGIN TRY
 	+ '<Receiver>ICRON</Receiver>' 
 	FROM @tblIPContractedStockPreStage CS
 
-	SELECT @strXML = IsNULL(@strXML,'') 
+	SELECT @strDetailXML = IsNULL(@strDetailXML,'') 
 	+ '<Header><StockCode></StockCode>' 
 	+ '<BatchNumber>' + Case When B.strBatchId is not null Then B.strBatchId Else CH.strContractNumber + '/' + ltrim(CD.intContractSeq) End + '</BatchNumber>' 
-	+ '<Plant>' + IsNULL(ltrim(MU.strLocationNumber),'')  + '</Plant>' 
+	+ '<Plant>' + IsNULL(ltrim(MU.strVendorRefNoPrefix),'')  + '</Plant>' 
 	+ '<ItemCode>' + I.strItemNo + '</ItemCode>' 
 	+ '<StockDate>' + IsNULL(CONVERT(VARCHAR(33), CD.dtmUpdatedAvailabilityDate, 126),'') + '</StockDate>' 
 	+ '<Quantity>' + [dbo].[fnRemoveTrailingZeroes](CD.dblQuantity) + '</Quantity>' 
-	+ '<MixingUnit>' + IsNULL(ltrim(MU.strLocationName) , '') + '</MixingUnit>' 
-	+ '<Taste>' + IsNULL(ltrim(B.dblTeaTaste),'') + '</Taste>' 
-	+ '<Hue>' + IsNULL(ltrim(B.dblTeaHue),'') + '</Hue>' 
-	+ '<Intensity>' + IsNULL(ltrim(B.dblTeaIntensity),'') + '</Intensity>' 
-	+ '<MouthFeel>' + IsNULL(ltrim(B.dblTeaMouthFeel),'') + '</MouthFeel>'
+	+ '<MixingUnit>' + IsNULL(ltrim(MU.strLocationNumber) , '') + '</MixingUnit>' 
+	+ '<Taste>' + IsNULL([dbo].[fnRemoveTrailingZeroes](B.dblTeaTaste),'') + '</Taste>' 
+	+ '<Hue>' + IsNULL([dbo].[fnRemoveTrailingZeroes](B.dblTeaHue),'') + '</Hue>' 
+	+ '<Intensity>' + IsNULL([dbo].[fnRemoveTrailingZeroes](B.dblTeaIntensity),'') + '</Intensity>' 
+	+ '<MouthFeel>' + IsNULL([dbo].[fnRemoveTrailingZeroes](B.dblTeaMouthFeel),'') + '</MouthFeel>'
 	+ '<ContainerNo></ContainerNo>' 
 	+ '<FromLocationCode>' + IsNULL(C1.strCity, '') + '</FromLocationCode>' 
 	+ '<PONumber>' +  Case When B.strBatchId is not null Then B.strBatchId Else CH.strContractNumber + '/' + ltrim(CD.intContractSeq) End + '</PONumber>' 
@@ -127,19 +127,19 @@ BEGIN TRY
 	+ '<UnitCost>' +  IsNULL([dbo].[fnRemoveTrailingZeroes](CD.dblCashPrice),'') + '</UnitCost>' 
 	+ '<Vessel></Vessel>' 
 	+ '<StockType>C</StockType>' 
-	+ '<Channel>' + IsNULL(SB.strSubBook, '') + '</Channel>' 
-	+ '<StorageLocation>' + IsNULL(MUSL.strSubLocationName , '') + '</StorageLocation>' 
+	+ '<Channel>' + IsNULL(MZ.strMarketZoneCode, '') + '</Channel>' 
+	+ '<StorageLocation>' + IsNULL(LTRIM(SUBSTRING(ISNULL(MUSL.strSubLocationName, ''), CHARINDEX('/', MUSL.strSubLocationName) + 1, LEN(MUSL.strSubLocationName))) , '') + '</StorageLocation>' 
 	+ '<Size>' + IsNULL(B.strLeafSize, '') + '</Size>' 
-	+ '<Volume>' + IsNULL(ltrim(B.dblTeaVolume), '') + '</Volume>' 
-	+ '<Appearance>' + IsNULL(ltrim(B.dblTeaAppearance), '') + '</Appearance>' 
+	+ '<Volume>' + IsNULL([dbo].[fnRemoveTrailingZeroes](B.dblTeaVolume), '') + '</Volume>' 
+	+ '<Appearance>' + IsNULL([dbo].[fnRemoveTrailingZeroes](B.dblTeaAppearance), '') + '</Appearance>' 
 	+ '<ExpiryDate>' + IsNULL(CONVERT(VARCHAR(33), DateAdd(mm, I.intLifeTime, B.dtmProductionBatch), 126),'') + '</ExpiryDate>' 
 	+ '<NoOfBags>' + IsNULL([dbo].[fnRemoveTrailingZeroes](CS1.dblBalanceQty) ,'') + '</NoOfBags>' 
 	+ '<UnitWeight>' + IsNULL([dbo].[fnRemoveTrailingZeroes](CS1.dblBalanceQty*IU.dblUnitQty),'') + '</UnitWeight>' 
 	+ '<NoOfBagsPerPallet>' + IsNULL(ltrim(I.intUnitPerLayer * I.intLayerPerPallet), '') + '</NoOfBagsPerPallet>' 
 	+ '<ReceiptDate></ReceiptDate>' 
 	+ '<BuyingOrderNo>' + IsNULL(B.strBuyingOrderNumber, '') + '</BuyingOrderNo>' 
-	+ '<StrategicFlag>' + IsNULL(ltrim(B.ysnStrategic), '') + '</StrategicFlag>' 
-	+ '<Origin>' + IsNULL(B.strTeaOrigin, '') + '</Origin>' 
+	+ '<StrategicFlag>' + IsNULL(ltrim(SB.strSubBook ), '') + '</StrategicFlag>' 
+	+ '<Origin>' + IsNULL(Country.strISOCode, '') + '</Origin>' 
 	+ '<IncoTerms>' + IsNULL(FT.strFreightTerm, '') + '</IncoTerms>' 
 	+ '<IncoTermsDesc>' + IsNULL(FT.strDescription, '') + '</IncoTermsDesc>' 
 	+ '<Currency>' + IsNULL(C.strCurrency, '') + '</Currency>' 
@@ -153,37 +153,40 @@ BEGIN TRY
 	+ '<SaleYear>' + IsNULL(ltrim(B.intSalesYear), '') + '</SaleYear>' 
 	+ '<SaleNumber>' + IsNULL(ltrim(B.intSales), '') + '</SaleNumber>' 
 	+ '<BrokerCode>' + IsNULL(BK.strName, '') + '</BrokerCode>' 
-	+ '<AuctionCenter>' + IsNULL(SM.strLocationName, '') + '</AuctionCenter>' 
-	+ '<BoughtPrice>' + IsNULL(ltrim(B.dblBoughtPrice), '') + '</BoughtPrice>' 
-	+ '<LandedPrice>' + IsNULL(ltrim(B.dblLandedPrice), '') + '</LandedPrice>' 
+	+ '<AuctionCenter>' + IsNULL(SM.strLocationNumber , '') + '</AuctionCenter>' 
+	+ '<BoughtPrice>' + IsNULL([dbo].[fnRemoveTrailingZeroes](CD.dblCashPrice), '') + '</BoughtPrice>' 
+	+ '<LandedPrice>' + IsNULL([dbo].[fnRemoveTrailingZeroes](B.dblLandedPrice), '') + '</LandedPrice>' 
 	+ '<TinNo>' + IsNULL(T.strTINNumber , '') + '</TinNo>' 
-	+ '<TBO>' + IsNULL(SM.strLocationName, '') + '</TBO>' 
+	+ '<TBO>' + IsNULL(SM.strLocationNumber, '') + '</TBO>' 
 	+ '<AvailableFrom>' + IsNULL(CONVERT(VARCHAR(33), B.dtmTeaAvailableFrom, 126),'') + '</AvailableFrom>' 
 	+ '<TeaType>' + IsNULL(B.strTeaType, '') + '</TeaType>' 
 	+ '<SaleDate>' + IsNULL(CONVERT(VARCHAR (33), B.dtmSalesDate , 126),'') + '</SaleDate>' 
 	+ '<InitialBuyDate>' + IsNULL(CONVERT(VARCHAR(33), B.dtmInitialBuy, 126),'') + '</InitialBuyDate>' 
 	+ '<BulkDensity>' + IsNULL(ltrim(B.dblBulkDensity), '') + '</BulkDensity>' 
-	+ '<BasePrice>' + IsNULL([dbo].[fnRemoveTrailingZeroes](B.dblBasePrice), '') + '</BasePrice></Header>'
+	+ '<BasePrice>' + IsNULL([dbo].[fnRemoveTrailingZeroes](CD.dblCashPrice), '') + '</BasePrice></Header>'
 	FROM @tblIPContractedStockPreStage CS
 	JOIN tblIPContractedStockPreStage CS1 on CS1.intContractedStockPreStageId =CS.intContractedStockPreStageId 
 	JOIN tblCTContractDetail CD ON CD.intContractDetailId = CS1.intContractDetailId
 	JOIN tblCTContractHeader CH ON CH.intContractHeaderId = CD.intContractHeaderId
 	JOIN tblICItem I ON I.intItemId = CD.intItemId
-	JOIN tblICItemUOM IU ON IU.intItemId  = CD.intItemId and IU.ysnStockUnit =1
-	JOIN tblCTSubBook SB ON SB.intSubBookId = CD.intSubBookId
+	Left JOIN tblICCommodityAttribute CA on CA.intCommodityAttributeId =I.intOriginId 
+	Left JOIN tblSMCountry Country on Country.intCountryID =CA.intCountryID 
+	JOIN tblICItemUOM IU ON IU.intItemUOMId  = CD.intItemUOMId 
+	Left JOIN tblCTSubBook SB ON SB.intSubBookId = CD.intSubBookId
 	JOIN tblSMCurrency C ON C.intCurrencyID = CD.intCurrencyId
 	LEFT JOIN tblSMCity C1 ON C1.intCityId = CD.intLoadingPortId
 	LEFT JOIN tblSMFreightTerms FT ON FT.intFreightTermId = CD.intFreightTermId
 	LEFT JOIN tblMFBatch B ON B.intContractDetailId = CD.intContractDetailId --It should be based on PO
 	LEFT JOIN tblEMEntity BK ON BK.intEntityId = B.intBrokerId
 	LEFT JOIN tblSMCompanyLocation SM ON SM.intCompanyLocationId = B.intBuyingCenterLocationId
-	LEFT JOIN tblSMCompanyLocation MU ON MU.intCompanyLocationId = CD.intCompanyLocationId  
+	LEFT JOIN tblSMCompanyLocation MU ON MU.intCompanyLocationId = B.intMixingUnitLocationId  
 	LEFT JOIN tblSMCompanyLocationSubLocation  MUSL ON MUSL.intCompanyLocationSubLocationId  = CD.intSubLocationId 
 	LEFT JOIN tblQMGardenMark  GM ON GM.intGardenMarkId = B.intGardenMarkId  
 	LEFT JOIN tblICItem OI on OI.intItemId=B.intOriginalItemId
 	LEFT JOIN tblQMTINClearance T on T.intBatchId =B.intBatchId 
+	Left JOIN tblARMarketZone MZ on MZ.intMarketZoneId =CD.intMarketZoneId
+	Where B.intLocationId<>B.intMixingUnitLocationId  
 	
-
 	UPDATE dbo.tblIPContractedStockPreStage
 	SET intStatusId = NULL
 	WHERE intContractedStockPreStageId IN (
@@ -192,13 +195,16 @@ BEGIN TRY
 			)
 		AND intStatusId = - 1
 
-	Select @strXML=@strXML+'</root>'
+	IF LEN(@strDetailXML)>0
+	BEGIN
+		SELECT @strXML=@strXML+@strDetailXML+'</root>'
 
-	SELECT IsNULL(1, '0') AS id
-		,IsNULL(@strXML, '') AS strXml
-		,'' AS strInfo1
-		,'' AS strInfo2
-		,'' AS strOnFailureCallbackSql
+		SELECT IsNULL(1, '0') AS id
+			,IsNULL(@strXML, '') AS strXml
+			,'' AS strInfo1
+			,'' AS strInfo2
+			,'' AS strOnFailureCallbackSql
+	END
 END TRY
 
 BEGIN CATCH
