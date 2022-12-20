c784c6f1c6fcc833884b559adbe11f2d5a765ae8
Mark Daniel Marmeto <mark.marmeto@irely.com>
2022-12-20 10:39:15 +0800

    GL-9773

--- i21Database/Scripts/Post-Deployment/GL/DefaultData/1k_AccountCurrency.sql
+++ i21Database/Scripts/Post-Deployment/GL/DefaultData/1k_AccountCurrency.sql
@@ -1,4 +1,3 @@
 ﻿DECLARE @intDefaultCurrencyID INT 
 SELECT TOP 1  @intDefaultCurrencyID=intDefaultCurrencyId  FROM tblSMCompanyPreference
-UPDATE tblGLAccount SET  intCurrencyID=@intDefaultCurrencyID WHERE intCurrencyID IS NULL
 UPDATE tblGLAccountReallocation  set intCurrencyId = @intDefaultCurrencyID where intCurrencyId IS NULL
