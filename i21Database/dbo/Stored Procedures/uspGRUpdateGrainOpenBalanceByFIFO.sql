2b6e102e5c8523da822b1c24772de60f03a089b4
Leslie Arat <leslie.arat@irely.com>
2022-12-22 13:06:04 +0800

    GRN-3176 (HDTN-283793 - Grain Bank tickets hitting risk management on wrong date)

--- i21Database/dbo/Stored Procedures/uspGRUpdateGrainOpenBalanceByFIFO.sql
+++ i21Database/dbo/Stored Procedures/uspGRUpdateGrainOpenBalanceByFIFO.sql
@@ -354,7 +354,7 @@ BEGIN TRY
 			,[intInvoiceId] 			= CASE WHEN @strSourceType = 'Invoice' THEN @IntSourceKey ELSE NULL END
 			,[intInventoryShipmentId] 	= CASE WHEN @strSourceType = 'InventoryShipment' THEN @IntSourceKey ELSE NULL END
 			,[dblUnits] 				= dblOpenBalance
-			,[dtmHistoryDate] 			= dbo.fnRemoveTimeOnDate(dtmDeliveryDate)
+			,[dtmHistoryDate] 			= A.dtmShipDate--dbo.fnRemoveTimeOnDate(dtmDeliveryDate)
 			,[dblPaidAmount] 			= [dblCharge] * dblOpenBalance + ISNULL(dblFlatFee,0)
 			,[intTransactionTypeId] 	= CASE
 											WHEN @strSourceType = 'Invoice' THEN 6
