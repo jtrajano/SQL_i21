83a8d4100eaabc359e90eabb227ce85a1bf48bf4
Renze Cuadra <renze.cuadra@iRely.com>
2022-12-27 14:40:59 +0800

    CS-261

--- i21Database/dbo/Tables/tblSTCompanyPreference.sql
+++ i21Database/dbo/Tables/tblSTCompanyPreference.sql
@@ -1,15 +1,17 @@
 ﻿CREATE TABLE [dbo].[tblSTCompanyPreference] (
-    [intCompanyPreferenceId]                INT            IDENTITY (1, 1) NOT NULL,
-    [strDailySecurityCode]                  NVARCHAR (MAX) COLLATE Latin1_General_CI_AS NULL,
-    [dtmDateEntered]                        DATETIME       NULL,
-    [ysnUnlockedLotteryModule]              BIT            NULL,
-    [ysnEnableLotteryManagement]            BIT            NULL,
-	[strStoreBasePath]			            NVARCHAR (250) COLLATE Latin1_General_CI_AS NULL,    
-    [intConcurrencyId]                      INT            CONSTRAINT [DF_tblSTCompanyPreference_intConcurrencyId] DEFAULT ((1)) NULL,
-    [strPollingStatusReportTime]			NVARCHAR (30)  COLLATE Latin1_General_CI_AS NULL,
-    [strPollingStatusReportEmailAddress]	NVARCHAR (250) COLLATE Latin1_General_CI_AS NULL,
-    [strPollingStatusReportEmailAddressSecond] NVARCHAR(250) COLLATE Latin1_General_CI_AS NULL, 
-    [strPollingStatusReportEmailAddressThird] NVARCHAR(250) COLLATE Latin1_General_CI_AS NULL, 
+    [intCompanyPreferenceId]                    INT             IDENTITY (1, 1) NOT NULL,
+    [strDailySecurityCode]                      NVARCHAR (MAX)  COLLATE Latin1_General_CI_AS NULL,
+    [dtmDateEntered]                            DATETIME        NULL,
+    [ysnUnlockedLotteryModule]                  BIT             NULL,
+    [ysnEnableLotteryManagement]                BIT             NULL,
+	[strStoreBasePath]			                NVARCHAR (250)  NULL,    
+    [intConcurrencyId]                          INT             CONSTRAINT [DF_tblSTCompanyPreference_intConcurrencyId] DEFAULT ((1)) NULL,
+    [strPollingStatusReportTime]			    NVARCHAR (30)   NULL,
+    [strPollingStatusReportEmailAddress]	    NVARCHAR (250)  NULL,
+    [strPollingStatusReportEmailAddressSecond]  NVARCHAR(250)   NULL, 
+    [strPollingStatusReportEmailAddressThird]   NVARCHAR(250)   NULL, 
+    [strCompanyName]                            NVARCHAR(20)    NULL, 
+    [strApplicationRootPath]                    NVARCHAR(250)   NULL, 
     CONSTRAINT [PK_tblSTCompanyPreference]  PRIMARY KEY CLUSTERED ([intCompanyPreferenceId] ASC) WITH (FILLFACTOR = 70)
 );
 
