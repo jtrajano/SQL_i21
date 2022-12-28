0ee1439837509afde5586de2679e39352c19d1c2
Jeff Trajano <jeffrey.trajano@irely.com>
2022-12-28 11:11:12 +0800

    QC-913

--- i21Database/dbo/User Defined Types/MFBatchSplitTableType.sql
+++ i21Database/dbo/User Defined Types/MFBatchSplitTableType.sql
@@ -2,6 +2,7 @@ CREATE TYPE MFBatchSplitTableType AS TABLE
 (
 	intBatchId INT,
     intParentBatchId INT,
+    intLocationId INT,
     intSplitStorageLocationId INT,
     intSplitStorageUnitId INT,
     dblSplitQuantity DECIMAL(18,6),
@@ -12,4 +13,3 @@ CREATE TYPE MFBatchSplitTableType AS TABLE
 	dtmSplit DATETIME NULL,
 	ysnSplit BIT
 )
-
