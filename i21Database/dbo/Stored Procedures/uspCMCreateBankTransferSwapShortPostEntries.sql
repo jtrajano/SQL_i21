94c5551424e31bd7dea356add07ec69695fe870e
Jeff Trajano <jeffrey.trajano@irely.com>
2022-12-29 10:33:17 +0800

    GL-9697

--- i21Database/dbo/Stored Procedures/uspCMCreateBankTransferSwapShortPostEntries.sql
+++ i21Database/dbo/Stored Procedures/uspCMCreateBankTransferSwapShortPostEntries.sql
@@ -299,7 +299,7 @@ END
         ,[strReference]          = A.strReferenceTo  
         ,[intCurrencyId]         = intCurrencyIdAmountTo
         ,[intCurrencyExchangeRateTypeId] =  intRateTypeIdAmountTo
-        ,[dblExchangeRate]       = dblRateAmountTo
+        ,[dblExchangeRate]       = ROUND(dblPayableFn/dblPayableFx, 6)
         ,[dtmDateEntered]        = GETDATE()      
         ,[dtmTransactionDate]    = A.dtmDate      
         ,[strJournalLineDescription]  = 'Currency Payable'
@@ -329,7 +329,7 @@ END
         ,[strReference]          = A.strReferenceTo  
         ,[intCurrencyId]         = intCurrencyIdAmountFrom
         ,[intCurrencyExchangeRateTypeId] =  intRateTypeIdAmountFrom
-        ,[dblExchangeRate]       = dblRateAmountFrom
+        ,[dblExchangeRate]       = ROUND(dblReceivableFn/dblReceivableFx,6)
         ,[dtmDateEntered]        = GETDATE()      
         ,[dtmTransactionDate]    = A.dtmDate      
         ,[strJournalLineDescription]  = 'Currency Receivable'
