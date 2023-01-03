aad89c384650a4bf1bce7c76a2d7bc1327fc548f
Jonathan Valenzuela <jonathan.valenzuela@irely.com>
2023-01-03 10:39:07 +0800

    QC-824

--- i21Database/dbo/Views/vyuQMCompanyPreference.sql
+++ i21Database/dbo/Views/vyuQMCompanyPreference.sql
@@ -1,26 +1,27 @@
 ﻿CREATE VIEW vyuQMCompanyPreference
 AS
 SELECT CP.intCompanyPreferenceId
-	,CP.intConcurrencyId
-	,CP.intNumberofDecimalPlaces
-	,CP.ysnEnableParentLot
-	,CP.ysnIsSamplePrintEnable
-	,CP.intApproveLotStatus
-	,CP.intRejectLotStatus
-	,CP.ysnAllowReversalSampleEntry
-	,CP.ysnChangeLotStatusOnApproveforPreSanitizeLot
-	,CP.ysnRejectLGContainer
-	,CP.intUserSampleApproval
-	,CP.ysnFilterContractByERPPONumber
-	,CP.ysnEnableSampleTypeByUserRole
-	,CP.ysnShowSampleFromAllLocation
-	,CP.ysnValidateMultipleValuesInTestResult
-	,CP.strTestReportComments
-	,CP.strSampleImportDateTimeFormat
-	,CP.ysnCaptureItemInProperty
-	,CP.ysnEnableAssignContractsInSample
-	,LS.strSecondaryStatus AS strApprovalLotStatus
-	,LS1.strSecondaryStatus AS strRejectionLotStatus
+	 , CP.intConcurrencyId
+	 , CP.intNumberofDecimalPlaces
+	 , CP.ysnEnableParentLot
+	 , CP.ysnIsSamplePrintEnable
+	 , CP.intApproveLotStatus
+	 , CP.intRejectLotStatus
+	 , CP.ysnAllowReversalSampleEntry
+	 , CP.ysnChangeLotStatusOnApproveforPreSanitizeLot
+	 , CP.ysnRejectLGContainer
+	 , CP.intUserSampleApproval
+	 , CP.ysnFilterContractByERPPONumber
+	 , CP.ysnEnableSampleTypeByUserRole
+	 , CP.ysnShowSampleFromAllLocation
+	 , CP.ysnValidateMultipleValuesInTestResult
+	 , CP.strTestReportComments
+	 , CP.strSampleImportDateTimeFormat
+	 , CP.ysnCaptureItemInProperty
+	 , CP.ysnEnableAssignContractsInSample
+	 , LS.strSecondaryStatus AS strApprovalLotStatus
+	 , LS1.strSecondaryStatus AS strRejectionLotStatus
+	 , CP.ysnValidateLotNo
 FROM tblQMCompanyPreference CP
 LEFT JOIN tblICLotStatus LS ON LS.intLotStatusId = CP.intApproveLotStatus
 LEFT JOIN tblICLotStatus LS1 ON LS1.intLotStatusId = CP.intRejectLotStatus
