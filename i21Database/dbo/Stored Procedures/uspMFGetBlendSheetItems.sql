28c2adf1e1f71a9d46f04647919e2ce5189f61c6
Jonathan Valenzuela <jonathan.valenzuela@irely.com>
2023-01-04 17:20:59 +0800

    MFG-4774

--- i21Database/dbo/Stored Procedures/uspMFGetBlendSheetItems.sql
+++ i21Database/dbo/Stored Procedures/uspMFGetBlendSheetItems.sql
@@ -349,8 +349,10 @@ SELECT Item.intItemId
 	 , RequiredQty.dblRequiredQty						AS dblOrgRequiredQty
 	 , ISNULL(ReservedQtyInTBS.dblReservedQtyInTBS,0)	AS dblReservedQtyInTBS
 	 , CAST(ISNULL(ISNULL(ROUND((ISNULL((ISNULL(PhysicalQty.dblPhysicalQty, 0) - ISNULL(ReservedQty.dblReservedQty, 0) - ISNULL(ReservedQtyInTBS.dblReservedQtyInTBS, 0)), 0)) / CASE WHEN ISNULL(PhysicalQty.dblWeightPerUnit, 1) = 0 THEN 1 ELSE ISNULL(PhysicalQty.dblWeightPerUnit, 1) END, 0), 0.0), 0) / (Item.intLayerPerPallet * Item.intUnitPerLayer) AS NUMERIC(18, 2)) AS dblNoOfPallet
-	 ,MT.strDescription AS strProductType
-	,B.strBrandCode
+	 , MT.strDescription AS strProductType
+	 , B.strBrandCode
+	 , Item.intUnitPerLayer
+	 , Item.intLayerPerPallet
 FROM @tblRequiredQty AS RequiredQty 
 JOIN tblICItem AS Item ON RequiredQty.intItemId = Item.intItemId
 LEFT JOIN @tblPhysicalQty AS PhysicalQty ON RequiredQty.intItemId = PhysicalQty.intItemId
