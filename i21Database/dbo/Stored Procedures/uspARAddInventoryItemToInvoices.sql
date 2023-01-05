1b8ae29b0828cfb1951407fa674549e36db99c55
Patrick Matthew Maniego <pat.maniego@iRely.com>
2023-01-05 15:08:34 +0800

    AR-16129

--- i21Database/dbo/Stored Procedures/uspARAddInventoryItemToInvoices.sql
+++ i21Database/dbo/Stored Procedures/uspARAddInventoryItemToInvoices.sql
@@ -1350,6 +1350,7 @@ BEGIN TRY
 	 WHERE
 		[ysnSuccess] = 1
 		AND ISNULL([ysnRecomputeTax], 0) = 1
+		AND [strType] = 'POS'
 
 	EXEC [dbo].[uspARReComputeInvoicesTaxes] @InvoiceIds = @RecomputeTaxIds, @SkipRecompute = @SkipRecompute
 
@@ -1371,6 +1372,7 @@ BEGIN TRY
 		 WHERE
 			[ysnSuccess] = 1
 			AND ISNULL([ysnRecomputeTax], 0) = 0
+			AND [strType] = 'POS'
 
 		EXEC [dbo].[uspARReComputeInvoicesAmounts] @InvoiceIds = @RecomputeAmountIds
 	END
