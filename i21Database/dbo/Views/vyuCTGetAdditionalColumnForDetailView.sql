4023914d49881c7a46796ae09e3f84cf0af0388f
Jayson Ramos <jayson.ramos@iRely.com>
2023-01-05 12:31:23 +0800

    CT-8218
    (cherry picked from commit afb9266cd3870bdc8b43c54e0de534099f73edd4)
    (cherry picked from commit 1ed5d89ec359f660d6b2dbae62da0f6822cae47a)

--- i21Database/dbo/Views/vyuCTGetAdditionalColumnForDetailView.sql
+++ i21Database/dbo/Views/vyuCTGetAdditionalColumnForDetailView.sql
@@ -9,10 +9,10 @@ SELECT
                                             END
                                             ELSE CD.intCurrencyId
                                         END
-    ,ysnSeqSubCurrency              =   CAST(CASE WHEN FX.ysnUseFXPrice = 1
-                                            THEN 0
+    ,ysnSeqSubCurrency              =   CASE WHEN FX.ysnUseFXPrice = 1
+                                            THEN convert(bit,0)
                                             ELSE CY.ysnSubCurrency
-                                        END AS BIT)
+                                            END
     ,intSeqPriceUOMId               =   CASE WHEN FX.ysnUseFXPrice = 1
                                             THEN CD.intFXPriceUOMId
                                             ELSE ISNULL(CD.intPriceItemUOMId,CD.intAdjItemUOMId)
@@ -64,8 +64,8 @@ SELECT
                                             ELSE CD.intBasisUOMId
                                         END
     ,ysnValidFX                     =   CASE WHEN FX.ysnUseFXPrice = 1
-                                            THEN 1
-                                            ELSE 0
+                                            THEN convert(bit,1)
+                                            ELSE convert(bit,0)
                                         END
     ,dblSeqFutures                  =   CAST(CASE WHEN FX.ysnUseFXPrice = 1
                                             THEN dbo.fnCTConvertQtyToTargetItemUOM(CD.intFXPriceUOMId,ISNULL(CD.intPriceItemUOMId,CD.intAdjItemUOMId), CD.dblFutures / CASE WHEN CY.ysnSubCurrency = 1 THEN CASE WHEN ISNULL(CY.intCent,0) = 0 THEN 1 ELSE CY.intCent END ELSE 1 END) * CD.dblRate
