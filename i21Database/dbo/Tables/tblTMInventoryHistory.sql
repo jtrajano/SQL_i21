c79489f98fe1e87756a620b6ef34b80c2cf5bab5
DESKTOP-VFHGDG0\iRelyNUC015 <denny.divinagracia@irely.com>
2023-01-06 17:31:02 +0800

    TM-3614

--- i21Database/dbo/Tables/tblTMInventoryHistory.sql
+++ i21Database/dbo/Tables/tblTMInventoryHistory.sql
@@ -1,14 +1,14 @@
 CREATE TABLE [dbo].[tblTMInventoryHistory]
 (
     [intConcurrencyId] INT DEFAULT 1 NOT NULL,
-    [intInventoryId] INT IDENTITY(1,1) NOT NULL,
+    [intInventoryHistoryId] INT IDENTITY(1,1) NOT NULL,
     [intSiteID]      INT DEFAULT 0 NULL,
     [dblLastInventoryReading] NUMERIC (18, 6) DEFAULT 0 NULL,
     [dtmLastInventoryTime] DATETIME NULL, 
     [dblCurrentInventoryReading] NUMERIC (18, 6) DEFAULT 0 NULL,
     [dtmInventoryTime]  DATETIME NULL
    
-    CONSTRAINT [PK_tblTMInventoryHistory] PRIMARY KEY CLUSTERED ([intInventoryId] ASC),
+    CONSTRAINT [PK_tblTMInventoryHistory] PRIMARY KEY CLUSTERED ([intInventoryHistoryId] ASC),
     CONSTRAINT [FK_tblTMInventoryHistory_tblTMSite] FOREIGN KEY ([intSiteID]) REFERENCES [dbo].[tblTMSite] ([intSiteID]),
 )
 
