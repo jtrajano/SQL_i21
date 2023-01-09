63cd13657cbaae79d4ed3eaf2eb831c8e3d0882d
Kenneth Sam Perez <kenneth.sam.perez@iRely.com>
2023-01-09 11:27:00 +0800

    MBIL-1080

--- i21Database/dbo/Views/vyuMBILDeliveryDetail.sql
+++ i21Database/dbo/Views/vyuMBILDeliveryDetail.sql
@@ -16,6 +16,5 @@ SELECT detail.intDeliveryDetailId
       ,detail.intTMSiteId
 	  ,detail.intShiftId
       ,detail.dblWaterInches
-      ,detail.intContractDetailId
 FROM tblMBILDeliveryDetail detail            
 INNER JOIN tblICItem item ON detail.intItemId = item.intItemId 
