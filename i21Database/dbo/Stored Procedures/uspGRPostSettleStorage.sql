e8163dd15e258b2207176c34399cd4618dc37816
Mon Gonzales <mon.gonzales@irely.com>
2023-01-10 16:10:37 +0800

    GRN-3189 - [Grain]- HDTN-281990 - Delayed price contract 4758

--- i21Database/dbo/Stored Procedures/uspGRPostSettleStorage.sql
+++ i21Database/dbo/Stored Procedures/uspGRPostSettleStorage.sql
@@ -2779,7 +2779,7 @@ BEGIN TRY
 				LEFT JOIN tblGRCustomerStorage CS
 					ON CS.intCustomerStorageId = a.intCustomerStorageId
 				left join tblSCTicket SC on SC.intTicketId = CS.intTicketId
-				left join tblICInventoryReceiptItem IRI on SC.intTicketId = IRI.intSourceId
+				left join tblICInventoryReceiptItem IRI on SC.intTicketId = IRI.intSourceId AND IRI.intContractHeaderId IS NOT NULL
 				LEFT JOIN tblGRDiscountScheduleCode DSC
 					ON DSC.intDiscountScheduleId = CS.intDiscountScheduleId 
 						AND DSC.intItemId = a.intItemId
