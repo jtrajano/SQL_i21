829254a69a78af4262b02a461a625e0f649d504d
Mark Daniel Marmeto <mark.marmeto@irely.com>
2023-01-10 16:48:02 +0800

    GL-9709

---
+++ i21Database/dbo/Tables/tblGLAuditorTransactionsByAccountId.sql
@@ -0,0 +1,35 @@
+﻿CREATE TABLE [dbo].[tblGLAuditorTransactionsByAccountId]
+(
+	[intAuditorTransactionId] INT IDENTITY (1, 1) NOT NULL,
+	[intGeneratedBy] INT NULL,
+	[dtmDateGenerated] DATETIME,
+	[intEntityId] INT NULL,
+	[strGroupTitle] NVARCHAR(MAX) COLLATE Latin1_General_CI_AS  NULL,
+	[strTotalTitle] NVARCHAR(MAX) COLLATE Latin1_General_CI_AS  NULL,
+	[intAccountId] INT NULL,
+	[strTransactionId] NVARCHAR(40) COLLATE Latin1_General_CI_AS  NULL,
+	[dtmDate] DATETIME NULL,
+	[dblDebit] NUMERIC(18, 6) NULL,
+	[dblCredit] NUMERIC(18, 6) NULL,
+	[dblDebitForeign] NUMERIC(18, 6) NULL,
+	[dblCreditForeign] NUMERIC(18, 6) NULL,
+	[dblBeginningBalance] NUMERIC(18, 6) NULL,
+	[dblEndingBalance] NUMERIC(18, 6) NULL,
+	[dblBeginningBalanceForeign] NUMERIC(18, 6) NULL,
+	[dblEndingBalanceForeign] NUMERIC(18, 6) NULL,
+	[intCurrencyId] INT NULL,
+	[strBatchId] NVARCHAR (40) COLLATE Latin1_General_CI_AS NULL,
+	[dtmDateEntered] DATETIME NULL,
+	[intCreatedBy] INT NULL,
+	[strCode] NVARCHAR (40) COLLATE Latin1_General_CI_AS NULL, -- Source System
+    [strTransactionType]        NVARCHAR (255)   COLLATE Latin1_General_CI_AS NULL,
+    [strTransactionForm]        NVARCHAR (255)   COLLATE Latin1_General_CI_AS NULL,
+    [strModuleName]             NVARCHAR (255)   COLLATE Latin1_General_CI_AS NULL,
+	[strReference] NVARCHAR(255) COLLATE Latin1_General_CI_AS NULL,
+	[strDocument] NVARCHAR(255) COLLATE Latin1_General_CI_AS NULL,
+	[strComments] NVARCHAR(255) COLLATE Latin1_General_CI_AS NULL,
+
+	[intConcurrencyId] INT DEFAULT 1 NOT NULL,
+	
+	CONSTRAINT [PK_tblGLAuditorTransactionsByAccountId] PRIMARY KEY CLUSTERED ([intAuditorTransactionId] ASC)
+)
---
+++ i21Database/dbo/Views/vyuGLAuditorTransactionsByAccountId.sql
@@ -0,0 +1,10 @@
+﻿CREATE VIEW [dbo].[vyuGLAuditorTransactionsByAccountId]
+AS
+	SELECT 
+		A.*
+		,ACC.strAccountId
+		,Currency.strCurrency
+	FROM tblGLAuditorTransactionsByAccountId A
+	LEFT JOIN tblGLAccount ACC ON ACC.intAccountId = A.intAccountId
+	LEFT JOIN tblSMCurrency Currency ON Currency.intCurrencyID = A.intCurrencyId
+
