2fe589820e2b1b96475bd7effce78fe350e1a2a6
Mark Daniel Marmeto <mark.marmeto@irely.com>
2023-01-11 22:51:40 +0800

    GL-9709

--- i21Database/dbo/Views/vyuGLAuditorTransactionsByAccountId.sql
+++ i21Database/dbo/Views/vyuGLAuditorTransactionsByAccountId.sql
@@ -1,10 +1,15 @@
 ﻿CREATE VIEW [dbo].[vyuGLAuditorTransactionsByAccountId]
 AS
-	SELECT 
-		A.*
-		,ACC.strAccountId
-		,Currency.strCurrency
-	FROM tblGLAuditorTransactionsByAccountId A
-	LEFT JOIN tblGLAccount ACC ON ACC.intAccountId = A.intAccountId
-	LEFT JOIN tblSMCurrency Currency ON Currency.intCurrencyID = A.intCurrencyId
+SELECT 
+	A.*
+	,ACC.strAccountId
+	,ACC.strDescription strAccountDescription
+	,AG.strAccountGroup
+	,Currency.strCurrency
+	,CASE WHEN (A.strTotalTitle = 'Total') THEN '' ELSE EM.strName END COLLATE Latin1_General_CI_AS strUserName
+FROM tblGLAuditorTransactionsByAccountId A
+LEFT JOIN tblGLAccount ACC ON ACC.intAccountId = A.intAccountId
+LEFT JOIN tblGLAccountGroup AG ON AG.intAccountGroupId = ACC.intAccountGroupId
+LEFT JOIN tblSMCurrency Currency ON Currency.intCurrencyID = A.intCurrencyId
+LEFT JOIN tblEMEntity EM ON EM.intEntityId = A.intEntityId
 
