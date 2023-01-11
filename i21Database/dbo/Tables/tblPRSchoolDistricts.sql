22a1bc961f51d0f8d5df6dbe93128d7b1bb4deaf
Rey Anthony Crisostomo <anthony.crisostomo@iRely.com>
2023-01-11 19:28:59 +0800

    Revert "PR-2926 - add school district table"
    This reverts commit 17eed02b5464c12e22e03520209f489bab8b0d83.

--- i21Database/dbo/Tables/tblPRSchoolDistricts.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE tblPRSchoolDistricts(
-	[intSchoolDistrictId] [int] IDENTITY(1,1) NOT NULL,
-	[strSchoolDistrict] [nvarchar](50) COLLATE Latin1_General_CI_AS NULL,
-	[strSchoolDistrictCode] [nvarchar](50) COLLATE Latin1_General_CI_AS NULL,
-	intConcurrencyId INT NULL DEFAULT ((1)), 
-)
