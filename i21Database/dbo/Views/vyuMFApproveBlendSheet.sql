3a07d22c29f2c760c3c6e4a70a3f208905c0bef9
Jonathan Valenzuela <jonathan.valenzuela@irely.com>
2023-01-11 14:51:39 +0800

    MFG-4805

--- i21Database/dbo/Views/vyuMFApproveBlendSheet.sql
+++ i21Database/dbo/Views/vyuMFApproveBlendSheet.sql
@@ -1,65 +1,72 @@
 CREATE VIEW [dbo].[vyuMFApproveBlendSheet]  
-AS  
-  
-SELECT 
-	   wo.intWorkOrderId
-	  ,woil.intWorkOrderInputLotId
-	  ,wo.intTrialBlendSheetStatusId
-	  ,YEAR(wo.dtmApprovedDate) [intApproveYear]
-	  ,MONTH(wo.dtmApprovedDate) [intApproveYearMonth]
-	  ,CAST(YEAR(wo.dtmApprovedDate) AS VARCHAR(10)) + CAST(MONTH(wo.dtmApprovedDate) AS VARCHAR(10))[strMonthYear]
-	  ,CAST(wo.dtmApprovedDate AS DATE)[dtmApprovedDate]
-	  ,wo.strWorkOrderNo [strOrder]
-	  ,b.strBatchId [strBatchId]
-	  ,itemwork.strItemNo [strBlendCode]
-	  ,CAST(br.dblEstNoOfBlendSheet AS NUMERIC(18,6)) [dblNoOfMixes]
-	  ,item.strItemNo [strTeaItem]
-	  ,CAST(woil.dblIssuedQuantity AS NUMERIC(18,6)) [dblNoOfPackages]
-	  ,CAST(woil.dblQuantity AS NUMERIC(18,6)) [dblAllocQty]
-	  ,CAST(b.dblSellingPrice AS NUMERIC(18,6)) [dblSellingPrice]
-	  ,CAST((woil.dblQuantity * 1) AS NUMERIC(18,6)) [dblAllocQtyInUsd]
-	  ,CAST(ISNULL((item.intUnitPerLayer * item.intLayerPerPallet), 0.000000) AS NUMERIC(18,6)) [dblPackagesPerPaller]
-	  ,loc.strLocationName [strAuction]
-	  ,b.strTeaOrigin [strOrigin]
-	  ,plantloc.strLocationName[strPlant]
-	  ,loc.strLocationName [strTBO]
-	  ,item.strModelNo [strMaterial]
-	  ,'' [strPurchasingDocument]
-	  ,'' [strProducer]
-	  ,b.strSustainability [strSustainability]
-	  ,mark.strGardenMark [strTeaMark]
-	  ,b.strLeafGrade [strGrade]
-	  ,b.strTeaGardenChopInvoiceNumber [strChop]
-	  ,b.strLeafSize [strLeafSize] 
-	  ,CAST(0.000000 AS NUMERIC(18,6)) [dblPurchasePrice]
-	  ,CAST(0.000000 AS NUMERIC(18,6)) [dblBasePrice]
-	  ,CAST(b.dblLandedPrice  AS NUMERIC(18,6)) [dblLandedPrice]
-	  ,itemwork.strDescription [strMaterialDescription]
-	  ,'' strUTK
-	  ,CAST(0.000000  AS NUMERIC(18,6)) dblActualQty
-	  ,CAST(0.000000  AS NUMERIC(18,6)) dblActualQtyInUSD
-	  ,em.strName [strApprovedBy]
-	  ,b.strTeaColour [strColor]
-	  ,'' [strDustContent]
-	  ,'' [strFactoryConsumptionDate]
-	  ,'' [strLowCostCountry]
-	  ,b.strLeafManufacturingType [strManufacturingType]
-	  ,b.strProductionSite [strProductionSite]
-	  ,b.strSupplierReference [strReference]
-	  ,b.strTeaColour [strTeaColour]
-	  ,b.strTeaLingoSubCluster [strTeaLingoSubCluster]
-	  ,CAST(b.dblTeaVolume  AS NUMERIC(18,6)) [dblTeaVolume]
-FROM tblMFWorkOrder wo
-INNER JOIN tblMFWorkOrderInputLot woil ON wo.intWorkOrderId = woil.intWorkOrderId
-INNER JOIN tblICLot lot ON lot.intLotId = woil.intLotId
-INNER JOIN tblMFBlendRequirement br ON br.intBlendRequirementId = wo.intBlendRequirementId
-INNER JOIN tblMFLotInventory loti ON loti.intLotId = woil.intLotId
-INNER JOIN tblMFBatch b ON b.intBatchId = loti.intBatchId
-LEFT JOIN tblQMGardenMark mark ON mark.intGardenMarkId = b.intGardenMarkId
-LEFT JOIN tblICItem item ON item.intItemId = woil.intItemId
-LEFT JOIN tblICItem itemwork ON item.intItemId = wo.intItemId
-LEFT JOIN tblQMTINClearance TC ON TC.intBatchId = b.intBatchId
-LEFT JOIN tblSMCompanyLocation loc ON b.intBuyingCenterLocationId = loc.intCompanyLocationId
-LEFT JOIN tblSMCompanyLocation plantloc ON b.intLocationId= loc.intCompanyLocationId
-LEFT JOIN tblEMEntity em on em.intEntityId=wo.intApprovedBy
-  
+AS    
+SELECT WorkOrder.intWorkOrderId
+	 , InputLot.intWorkOrderInputLotId
+	 , WorkOrder.intTrialBlendSheetStatusId
+	 , YEAR(WorkOrder.dtmApprovedDate)						AS intApproveYear
+	 , MONTH(WorkOrder.dtmApprovedDate)						AS intApproveYearMonth
+	 , CAST(YEAR(WorkOrder.dtmApprovedDate) AS VARCHAR(10)) + CAST(MONTH(WorkOrder.dtmApprovedDate) AS VARCHAR(10)) AS strMonthYear
+	 , CAST(WorkOrder.dtmApprovedDate AS DATE)				AS dtmApprovedDate
+	 , WorkOrder.strWorkOrderNo								AS strOrder
+	 , Batch.strBatchId										AS strBatchId
+	 , WorkOrderItem.strItemNo								AS strBlendCode
+	 , CAST(BlendRequirement.dblEstNoOfBlendSheet AS NUMERIC(18, 6)) AS dblNoOfMixes
+	 , InputLotItem.strItemNo								AS strTeaItem
+	 , CAST(InputLot.dblIssuedQuantity AS NUMERIC(18, 6))	AS dblNoOfPackages
+	 , CAST(InputLot.dblQuantity AS NUMERIC(18, 6))			AS dblAllocQty
+	 , CAST(Batch.dblSellingPrice AS NUMERIC(18, 6))		AS dblSellingPrice
+	 , CAST((InputLot.dblQuantity * 1) AS NUMERIC(18, 6))	AS dblAllocQtyInUsd
+	 , CAST(ISNULL((InputLotItem.intUnitPerLayer * InputLotItem.intLayerPerPallet), 0.000000) AS NUMERIC(18,6)) AS dblPackagesPerPaller
+	 , BuyingCenterLocation.strLocationName			 AS strAuction
+	 , Batch.strTeaOrigin							 AS strOrigin
+	 , BatchLocation.strLocationName				 AS strPlant
+	 , BuyingCenterLocation.strLocationName			 AS strTBO
+	 , InputLotItem.strModelNo						 AS	strMaterial
+	 , ''											 AS strPurchasingDocument
+	 , ''											 AS strProducer
+	 , Batch.strSustainability						 AS strSustainability
+	 , GardenMark.strGardenMark						 AS strTeaMark
+	 , Batch.strLeafGrade							 AS strGrade
+	 , Batch.strTeaGardenChopInvoiceNumber			 AS strChop
+	 , Batch.strLeafSize							 AS strLeafSize
+	 , CAST(0.000000 AS NUMERIC(18, 6))				 AS dblPurchasePrice
+	 , CAST(0.000000 AS NUMERIC(18, 6))				 AS dblBasePrice
+	 , CAST(Batch.dblLandedPrice  AS NUMERIC(18, 6)) AS dblLandedPrice
+	 , WorkOrderItem.strDescription					 AS strMaterialDescription
+	 , ''											 AS strUTK
+	 , CAST(0.000000  AS NUMERIC(18, 6))			 AS dblActualQty
+	 , CAST(0.000000  AS NUMERIC(18, 6))			 AS dblActualQtyInUSD
+	 , Entity.strName								 AS strApprovedBy
+	 , Batch.strTeaColour							 AS strColor
+	 , ''											 AS strDustContent
+	 , ''											 AS strFactoryConsumptionDate
+	 , ''											 AS strLowCostCountry
+	 , Batch.strLeafManufacturingType				 AS strManufacturingType
+	 , Batch.strProductionSite						 AS strProductionSite
+	 , Batch.strSupplierReference					 AS strReference
+	 , Batch.strTeaColour							 AS strTeaColour
+	 , Batch.strTeaLingoSubCluster					 AS strTeaLingoSubCluster
+	 , CAST(Batch.dblTeaVolume  AS NUMERIC(18, 6))	 AS dblTeaVolume
+	 , BlendRequirement.strDemandNo					 AS strComment
+	 , WorkOrder.strERPOrderNo						 AS strERPOrderNo
+	 , WorkOrderStatus.strName						 AS strWorkOrderStatus
+	 , InputLot.strFW								 
+	 , CASE WHEN MarketZone.strMarketZoneCode = 'AUC' THEN 'Yes'
+			ELSE 'No'
+	   END AS strChannel
+FROM tblMFWorkOrder AS WorkOrder
+INNER JOIN tblMFWorkOrderInputLot AS InputLot ON WorkOrder.intWorkOrderId = InputLot.intWorkOrderId
+INNER JOIN tblICLot AS Lot ON Lot.intLotId = InputLot.intLotId
+INNER JOIN tblMFBlendRequirement AS BlendRequirement ON BlendRequirement.intBlendRequirementId = WorkOrder.intBlendRequirementId
+INNER JOIN tblMFLotInventory AS LotInventory ON LotInventory.intLotId = InputLot.intLotId
+INNER JOIN tblMFBatch AS Batch ON Batch.intBatchId = LotInventory.intBatchId
+INNER JOIN tblMFWorkOrderStatus AS WorkOrderStatus ON WorkOrder.intStatusId = WorkOrderStatus.intStatusId
+LEFT JOIN tblARMarketZone AS MarketZone ON Batch.intMarketZoneId = MarketZone.intMarketZoneId
+LEFT JOIN tblQMGardenMark AS GardenMark ON GardenMark.intGardenMarkId = Batch.intGardenMarkId
+LEFT JOIN tblICItem AS InputLotItem ON InputLotItem.intItemId = InputLot.intItemId
+LEFT JOIN tblICItem AS WorkOrderItem ON InputLotItem.intItemId = WorkOrder.intItemId
+LEFT JOIN tblQMTINClearance AS TinClearance ON TinClearance.intBatchId = Batch.intBatchId
+LEFT JOIN tblSMCompanyLocation AS BuyingCenterLocation ON Batch.intBuyingCenterLocationId = BuyingCenterLocation.intCompanyLocationId
+LEFT JOIN tblSMCompanyLocation AS BatchLocation ON Batch.intLocationId = BuyingCenterLocation.intCompanyLocationId
+LEFT JOIN tblEMEntity AS Entity on Entity.intEntityId = WorkOrder.intApprovedBy
+GO
