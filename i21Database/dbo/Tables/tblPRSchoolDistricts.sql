fe4f7a19bdbf963865980176672372c4c039377f
Rey Anthony Crisostomo <anthony.crisostomo@iRely.com>
2023-01-13 13:17:18 +0800

    PR-2926 - Added School District

--- i21Database/dbo/Tables/tblPRSchoolDistricts.sql
+++ i21Database/dbo/Tables/tblPRSchoolDistricts.sql
@@ -2,5 +2,5 @@ CREATE TABLE tblPRSchoolDistricts(
 	[intSchoolDistrictId] [int] IDENTITY(1,1) NOT NULL,
 	[strSchoolDistrict] [nvarchar](50) COLLATE Latin1_General_CI_AS NULL,
 	[strSchoolDistrictCode] [nvarchar](50) COLLATE Latin1_General_CI_AS NULL,
-	intConcurrencyId INT NULL DEFAULT ((1)), 
+	intConcurrencyId INT NULL DEFAULT ((1))
 )
