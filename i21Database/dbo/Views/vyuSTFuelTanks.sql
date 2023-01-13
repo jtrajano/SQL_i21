5b84d4d2b4d4d6debaa66963f1494cfea3a34351
Jase Rueda <jase.rueda@iRely.com>
2023-01-13 11:15:05 +0800

    CS-275

--- i21Database/dbo/Views/vyuSTFuelTanks.sql
+++ i21Database/dbo/Views/vyuSTFuelTanks.sql
@@ -1,10 +1,21 @@
 CREATE VIEW [dbo].[vyuSTFuelTanks]
 AS
-
 SELECT DISTINCT 
 	device.strSerialNumber, 
 	item.strItemNo, 
-	item.strDescription
+	item.strDescription,
+	site.intLocationId as intCompanyLocationId,
+	CASE 
+		WHEN LEN(site.intSiteNumber) = 1
+		THEN '000' + CAST(site.intSiteNumber as NVARCHAR(1))
+		WHEN LEN(site.intSiteNumber) = 2
+		THEN '00' + CAST(site.intSiteNumber as NVARCHAR(2))
+		WHEN LEN(site.intSiteNumber) = 3
+		THEN '0' + CAST(site.intSiteNumber as NVARCHAR(3))
+		ELSE CAST(site.intSiteNumber as NVARCHAR(1))
+		END as strSiteNumber,
+	device.intDeviceId,
+	site.intSiteID as intCompanyConsumptionSiteId
 FROM tblTMDevice device
 JOIN tblTMSiteDevice sitedevice
 	ON device.intDeviceId = sitedevice.intDeviceId
@@ -12,4 +23,5 @@ INNER JOIN tblTMSite site
 	ON site.intSiteID = sitedevice.intSiteID
 INNER JOIN tblICItem item
 	ON site.intProduct = item.intItemId
-WHERE strSerialNumber != ''
+WHERE device.strSerialNumber != '' AND 
+		device.intDeviceTypeId = 1 --intDeviceTypeId = 1 means Tank
--- i21Database/dbo/Views/vyuSTStoreFuelTanks.sql
+++ i21Database/dbo/Views/vyuSTStoreFuelTanks.sql
@@ -7,13 +7,28 @@ SELECT
 	item.strItemNo, 
 	item.strDescription,
 	FT.intRegisterTankNumber,
-	FT.intConcurrencyId
+	FT.intConcurrencyId,
+	site.intLocationId as intCompanyLocationId,
+	CASE 
+		WHEN LEN(site.intSiteNumber) = 1
+		THEN '000' + CAST(site.intSiteNumber as NVARCHAR(1))
+		WHEN LEN(site.intSiteNumber) = 2
+		THEN '00' + CAST(site.intSiteNumber as NVARCHAR(2))
+		WHEN LEN(site.intSiteNumber) = 3
+		THEN '0' + CAST(site.intSiteNumber as NVARCHAR(3))
+		ELSE CAST(site.intSiteNumber as NVARCHAR(1))
+		END as strSiteNumber,
+	device.intDeviceId,
+	site.intSiteID as intCompanyConsumptionSiteId
 FROM tblSTStoreFuelTanks FT
+JOIN tblSTStore ST
+	ON FT.intStoreId = ST.intStoreId
 JOIN tblTMDevice device
 	ON FT.strSerialNumber = device.strSerialNumber
 JOIN tblTMSiteDevice sitedevice
 	ON device.intDeviceId = sitedevice.intDeviceId
 INNER JOIN tblTMSite site
-	ON site.intSiteID = sitedevice.intSiteID
+	ON site.intSiteID = sitedevice.intSiteID AND
+		ST.intCompanyLocationId = site.intLocationId
 INNER JOIN tblICItem item
 	ON site.intProduct = item.intItemId
