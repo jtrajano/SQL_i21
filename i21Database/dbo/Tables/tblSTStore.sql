acbf0f27bd204b9196c4082f60b123e8f549c4df
Renze Cuadra <renze.cuadra@iRely.com>
2023-01-17 16:42:59 +0800

    CS-274

--- i21Database/dbo/Tables/tblSTStore.sql
+++ i21Database/dbo/Tables/tblSTStore.sql
@@ -147,6 +147,8 @@
     [intConsDealerCommissionItemId] INT NULL,
     [dblConsMatchTolerance] DECIMAL(18,6) NOT NULL DEFAULT 0,
     [intConsFuelDiscountItemId] INT NULL,
+    [ysnConsIncludeTaxesInCostBasis] BIT NOT NULL DEFAULT 0,
+    [ysnConsIncludeFreightChargesInCostBasis] BIT NOT NULL DEFAULT 0,
 
     [intConcurrencyId] INT NOT NULL, 
     CONSTRAINT [PK_tblSTStore] PRIMARY KEY CLUSTERED ([intStoreId] ASC),
