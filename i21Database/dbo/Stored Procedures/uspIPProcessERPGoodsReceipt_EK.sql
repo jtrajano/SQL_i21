9098ce86222c48f1d906adde359e2276940e0ac7
Vinoth Kumar T <vinoth.t@irely.com>
2023-01-18 17:16:51 +0530

    IN-1128

--- i21Database/dbo/Stored Procedures/uspIPProcessERPGoodsReceipt_EK.sql
+++ i21Database/dbo/Stored Procedures/uspIPProcessERPGoodsReceipt_EK.sql
@@ -92,7 +92,6 @@ BEGIN TRY
 	SELECT intStageReceiptId
 	FROM tblIPInvReceiptStage
 	WHERE intStatusId IS NULL
-		AND ISNULL(strOrderType, '') = 'PO'
 
 	SELECT @intStageReceiptId = MIN(intStageReceiptId)
 	FROM @tblIPInvReceiptStage
