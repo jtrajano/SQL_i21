d7d7307fa095580d7db855236c9df80f7e0560be
Kenneth Sam Perez <kenneth.sam.perez@iRely.com>
2023-01-23 23:30:39 +0800

    MBIL-1088

--- i21Database/dbo/Views/vyuMBILGetLoads.sql
+++ i21Database/dbo/Views/vyuMBILGetLoads.sql
@@ -58,7 +58,10 @@ SELECT lh.intLoadHeaderId,
  dd.dblPrice,      
  dd.dblDeliveredQty,     
  dd.dblPercentFull,    
- dd.ysnDelivered    
+ dd.ysnDelivered,
+ lh.ysnDiversion,
+ lh.strDiversionNumber,
+ lh.intStateId
 FROM     
 tblMBILPickupDetail pd     
 JOIN tblMBILDeliveryDetail dd ON dd.intPickupDetailId = pd.intPickupDetailId     
