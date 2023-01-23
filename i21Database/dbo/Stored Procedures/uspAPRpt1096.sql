47b4d7420e4dc4f0bc8d920ef8d751ee63694660
Erick F Dela Cruz <erick.delacruz@irely.com>
2023-01-23 19:52:51 +0800

    AP-11419

--- i21Database/dbo/Stored Procedures/uspAPRpt1096.sql
+++ i21Database/dbo/Stored Procedures/uspAPRpt1096.sql
@@ -231,6 +231,76 @@ AS
 					ELSE 0 END)
 		END)
 	GROUP BY A.intYear,A.strEIN,A.strCompanyName,A.strAddress,A.strCity,A.strZipState
+),
+K1099 (
+	intTotalForm
+	,intYear
+	,dblTotal
+	,strYear
+)
+AS
+( 
+	SELECT
+		COUNT(*) AS intTotalForm
+		,A.intYear
+		,SUM(dblTotalPayment) dblTotal
+		,(SELECT RIGHT(@yearParam,2)) AS strYear
+	FROM dbo.vyuAP1099K A
+	OUTER APPLY
+	(
+		SELECT TOP 1 * FROM tblAP1099History B
+		WHERE A.intYear = B.intYear AND B.int1099Form = 4
+		AND B.intEntityVendorId = A.intEntityVendorId
+		ORDER BY B.dtmDatePrinted DESC
+	) History
+	WHERE 1 = (CASE WHEN @vendorFromParam IS NOT NULL THEN
+					(CASE WHEN A.strVendorId BETWEEN @vendorFromParam AND @vendorToParam THEN 1 ELSE 0 END)
+				ELSE 1 END)
+	AND A.intYear = @yearParam
+	AND 1 = (
+		CASE WHEN  ISNULL(@correctedParam,0) = 1 THEN 1 
+				ELSE 
+					(CASE WHEN History.ysnPrinted IS NOT NULL AND History.ysnPrinted = 1 AND @reprintParam = 1 THEN 1 
+						WHEN History.ysnPrinted IS NULL THEN 1
+						WHEN History.ysnPrinted IS NOT NULL AND History.ysnPrinted = 0 THEN 1
+					ELSE 0 END)
+		END)
+	GROUP BY A.intYear,A.strEIN,A.strCompanyName,A.strAddress,A.strCity,A.strZipState
+),
+NEC1099 (
+	intTotalForm
+	,intYear
+	,dblTotal
+	,strYear
+)
+AS
+( 
+	SELECT
+		COUNT(*) AS intTotalForm
+		,A.intYear
+		,SUM(dblTotalPayment) dblTotal
+		,(SELECT RIGHT(@yearParam,2)) AS strYear
+	FROM dbo.vyuAP1099NEC A
+	OUTER APPLY
+	(
+		SELECT TOP 1 * FROM tblAP1099History B
+		WHERE A.intYear = B.intYear AND B.int1099Form = 4
+		AND B.intEntityVendorId = A.intEntityVendorId
+		ORDER BY B.dtmDatePrinted DESC
+	) History
+	WHERE 1 = (CASE WHEN @vendorFromParam IS NOT NULL THEN
+					(CASE WHEN A.strVendorId BETWEEN @vendorFromParam AND @vendorToParam THEN 1 ELSE 0 END)
+				ELSE 1 END)
+	AND A.intYear = @yearParam
+	AND 1 = (
+		CASE WHEN  ISNULL(@correctedParam,0) = 1 THEN 1 
+				ELSE 
+					(CASE WHEN History.ysnPrinted IS NOT NULL AND History.ysnPrinted = 1 AND @reprintParam = 1 THEN 1 
+						WHEN History.ysnPrinted IS NULL THEN 1
+						WHEN History.ysnPrinted IS NOT NULL AND History.ysnPrinted = 0 THEN 1
+					ELSE 0 END)
+		END)
+	GROUP BY A.intYear,A.strEIN,A.strCompanyName,A.strAddress,A.strCity,A.strZipState
 )
 
 SELECT
@@ -242,12 +312,13 @@ SELECT
 	,SUM(intTotalForm) intTotalForm
 	,SUM(dblTotal) dblTotal
 	,(SELECT RIGHT(@yearParam,2)) AS strYear
-	,str1099MISC = CASE WHEN EXISTS(SELECT 1 FROM MISC1099 WHERE intYear = @yearParam) THEN 'X' ELSE NULL END
-	,str1099INT = CASE WHEN EXISTS(SELECT 1 FROM INT1099 WHERE intYear = @yearParam) THEN 'X' ELSE NULL END
-	,str1099B = CASE WHEN EXISTS(SELECT 1 FROM B1099 WHERE intYear = @yearParam) THEN 'X' ELSE NULL END
-	,str1099PATR = CASE WHEN EXISTS(SELECT 1 FROM PATR1099 WHERE intYear = @yearParam) THEN 'X' ELSE NULL END   
-	,str1099DIV = CASE WHEN EXISTS(SELECT 1 FROM DIV1099 WHERE intYear = @yearParam) THEN 'X' ELSE NULL END   
-	,str1099K = CASE WHEN EXISTS(SELECT 1 FROM K1099 WHERE intYear = @yearParam) THEN 'X' ELSE NULL END   
+	,str1099MISC = CASE WHEN EXISTS(SELECT 1 FROM MISC1099 WHERE 1 = (CASE WHEN @form1099Param = 0 OR @form1099Param = 1 THEN 1 ELSE 0 END)) THEN 'X' ELSE NULL END
+	,str1099INT = CASE WHEN EXISTS(SELECT 1 FROM INT1099 WHERE 1 = (CASE WHEN @form1099Param = 0 OR @form1099Param = 2 THEN 1 ELSE 0 END)) THEN 'X' ELSE NULL END
+	,str1099B = CASE WHEN EXISTS(SELECT 1 FROM B1099 WHERE 1 = (CASE WHEN @form1099Param = 0 OR @form1099Param = 3 THEN 1 ELSE 0 END)) THEN 'X' ELSE NULL END
+	,str1099PATR = CASE WHEN EXISTS(SELECT 1 FROM PATR1099 WHERE 1 = (CASE WHEN @form1099Param = 0 OR @form1099Param = 4 THEN 1 ELSE 0 END)) THEN 'X' ELSE NULL END   
+	,str1099DIV = CASE WHEN EXISTS(SELECT 1 FROM DIV1099 WHERE 1 = (CASE WHEN @form1099Param = 0 OR @form1099Param = 5 THEN 1 ELSE 0 END)) THEN 'X' ELSE NULL END   
+	,str1099K = CASE WHEN EXISTS(SELECT 1 FROM K1099 WHERE 1 = (CASE WHEN @form1099Param = 0 OR @form1099Param = 6 THEN 1 ELSE 0 END)) THEN 'X' ELSE NULL END   
+	,str1099NEC = CASE WHEN EXISTS(SELECT 1 FROM NEC1099 WHERE 1 = (CASE WHEN @form1099Param = 0 OR @form1099Param = 7 THEN 1 ELSE 0 END)) THEN 'X' ELSE NULL END   
 	,B.strContactName
 	,B.strContactPhone
 	,B.strContactEmail
@@ -276,7 +347,19 @@ inner join
 	UNION ALL
 	SELECT 
 		*
+<<<<<<< HEAD
 	FROM K1099 WHERE 1 = (CASE WHEN EXISTS(SELECT 1 FROM K1099 WHERE intYear = @yearParam) THEN 1 ELSE 0 END)
+=======
+	FROM DIV1099 WHERE 1 = (CASE WHEN @form1099Param = 0 OR @form1099Param = 5 THEN 1 ELSE 0 END)
+	UNION ALL
+	SELECT 
+		*
+	FROM NEC1099 WHERE 1 = (CASE WHEN @form1099Param = 0 OR @form1099Param = 7 THEN 1 ELSE 0 END)
+	UNION ALL
+	SELECT 
+		*
+	FROM K1099 WHERE 1 = (CASE WHEN @form1099Param = 0 OR @form1099Param = 6 THEN 1 ELSE 0 END)
+>>>>>>> dde495cceb (AP-11419)
 ) Data1099 on 1=1
 inner join tblAP1099Threshold B on 1=1
 GROUP BY intYear
