93c851ff81cc6681c50450ef0c8f203659a5adfd
Allen Marasigan <54519457+jaumarasigan@users.noreply.github.com>
2023-01-24 20:49:58 +0800

    LG-3489 Added Borrowing Facility type

--- i21Database/dbo/Views/vyuLGGetReportRemark.sql
+++ i21Database/dbo/Views/vyuLGGetReportRemark.sql
@@ -7,10 +7,12 @@ SELECT RR.intReportRemarkId
 	, RR.intValueId
 	, strValue = CASE WHEN RR.strType = 'Item' THEN Item.strItemNo
 					WHEN RR.strType = 'Entity' THEN Entity.strName
+					WHEN RR.strType = 'Borrowing Facility' THEN VRR.strValue
 					ELSE '' END
 	, RR.intLocationId
 	, strLocation = CASE WHEN RR.strType = 'Item' THEN ItemLoc.strLocationName
 						WHEN RR.strType = 'Entity' THEN EL.strLocationName
+						WHEN RR.strType = 'Borrowing Facility' THEN SM.strLocationName
 						ELSE '' END
 	, RR.strRemarks
 	, RR.intConcurrencyId
@@ -20,3 +22,5 @@ LEFT JOIN tblEMEntity Entity ON Entity.intEntityId = RR.intValueId AND RR.strTyp
 LEFT JOIN tblICItemLocation IL ON IL.intLocationId = RR.intLocationId AND RR.strType = 'Item' AND IL.intItemId = RR.intValueId
 LEFT JOIN tblSMCompanyLocation ItemLoc ON ItemLoc.intCompanyLocationId = IL.intLocationId
 LEFT JOIN tblEMEntityLocation EL ON EL.intEntityLocationId = RR.intLocationId AND RR.strType = 'Entity'
+LEFT JOIN vyuLGGetReportRemarkValue VRR ON VRR.intValueId = RR.intValueId AND VRR.strType = 'Borrowing Facility'
+LEFT JOIN tblSMCompanyLocation SM ON SM.intCompanyLocationId = RR.intLocationId
