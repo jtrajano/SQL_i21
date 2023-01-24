8d80ae5e44089f517adfd6cd97c968d1e4f117aa
Jonathan Valenzuela <jonathan.valenzuela@irely.com>
2023-01-24 17:30:08 +0800

    QC-941

---
+++ i21Database/dbo/Views/vyuQMUserLocation.sql
@@ -0,0 +1,16 @@
+﻿CREATE VIEW [dbo].[vyuQMUserLocation]
+AS
+/*
+ * Title: User Location
+ * Description: Returns list of company location that user have permissions.
+ * Created By: Jonathan Valenzuela
+ * Created Date: 01/24/2023
+ * JIRA: QC-941 
+*/
+SELECT intEntityUserSecurityId
+	 , intEntityId
+	 , intUserRoleId
+	 , CompanyLocation.intCompanyLocationId
+	 , CompanyLocation.strLocationName AS strCompanyLocationName
+FROM tblSMUserSecurityCompanyLocationRolePermission AS UserLocationRole
+JOIN tblSMCompanyLocation AS CompanyLocation ON UserLocationRole.intCompanyLocationId = CompanyLocation.intCompanyLocationId
