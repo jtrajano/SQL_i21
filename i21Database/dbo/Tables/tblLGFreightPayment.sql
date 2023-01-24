2fd375f1e0c32f7b73b10e1ab6dedd95a83ae675
Allen Marasigan <54519457+jaumarasigan@users.noreply.github.com>
2023-01-24 20:54:27 +0800

    LG-3489 Added Freight Payment table

---
+++ i21Database/dbo/Tables/tblLGFreightPayment.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[tblLGFreightPayment]
+(
+ [intFreightPaymentId] INT NOT NULL IDENTITY,
+ [intConcurrencyId] INT NOT NULL,
+ [strFreightPayment] NVARCHAR(100) COLLATE Latin1_General_CI_AS NOT NULL,
+
+ CONSTRAINT [PK_tblLGFreightPayment_intFreightPaymentId] PRIMARY KEY ([intFreightPaymentId]),
+ CONSTRAINT [UK_tblLGFreightPayment_strFreightPayment] UNIQUE ([strFreightPayment])
+ )
