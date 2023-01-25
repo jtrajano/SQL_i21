fdf8d200ae0ba85e96a8754b8de68b96323baa11
Kelvin Quinto <kelvin.quinto@irely.com>
2023-01-25 09:55:58 +0800

    AR-16248
    (cherry picked from commit d04a17665253950994741e7870601dff5b799f3e)

--- i21Database/dbo/Views/vyuARSpecialPricingEndpoint.sql
+++ i21Database/dbo/Views/vyuARSpecialPricingEndpoint.sql
@@ -1,26 +1,16 @@
 ﻿CREATE VIEW [dbo].[vyuARSpecialPricingEndpoint]
-	AS 
-SELECT
-	SP.intSpecialPriceId
-	,SP.intEntityCustomerId
-	,Customer.strEntityNo strCustomerEntityNo
-	,Customer.strName strCustomerName
-	,SP.intCustomerLocationId
-	,Loc.strLocationName strCustomerLocation
-	,SP.intItemId
-	,I.strItemNo
-	,dblDeviation = SP.dblDeviation + SP.dblDeviation2 + SP.dblDeviation3 + SP.dblDeviation4
-FROM
-	tblARCustomerSpecialPrice SP
-INNER JOIN
-	tblARCustomer C
-		ON SP.intEntityCustomerId = C.[intEntityId]
-INNER JOIN
-	tblEMEntity Customer 
-		ON Customer.intEntityId = C.intEntityId
-LEFT JOIN 
-	tblEMEntityLocation Loc 
-		ON Loc.intEntityLocationId = SP.intCustomerLocationId
-LEFT JOIN
-	tblICItem I
-		ON I.intItemId = SP.intItemId
+AS 
+SELECT intSpecialPriceId		= SP.intSpecialPriceId
+	 , intEntityCustomerId		= SP.intEntityCustomerId
+	 , strCustomerEntityNo		= Customer.strEntityNo 
+	 , strCustomerName			= Customer.strName 
+	 , intCustomerLocationId	= SP.intCustomerLocationId
+	 , strCustomerLocation		= Loc.strLocationName 
+	 , intItemId				= SP.intItemId
+	 , strItemNo				= I.strItemNo
+	 , dblDeviation 			= ISNULL(SP.dblDeviation, 0) + ISNULL(SP.dblDeviation2, 0) + ISNULL(SP.dblDeviation3, 0) + ISNULL(SP.dblDeviation4, 0)
+FROM tblARCustomerSpecialPrice SP
+INNER JOIN tblARCustomer C ON SP.intEntityCustomerId = C.[intEntityId]
+INNER JOIN tblEMEntity Customer ON Customer.intEntityId = C.intEntityId
+LEFT JOIN tblEMEntityLocation Loc ON Loc.intEntityLocationId = SP.intCustomerLocationId
+LEFT JOIN tblICItem I ON I.intItemId = SP.intItemId
