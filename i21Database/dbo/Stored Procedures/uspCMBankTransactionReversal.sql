da1c459ad7269dad4352efabef72dd90bb5c94ff
Jeff Trajano <jeffrey.trajano@irely.com>
2023-01-25 10:10:14 +0800

    GL-9866

--- i21Database/dbo/Stored Procedures/uspCMBankTransactionReversal.sql
+++ i21Database/dbo/Stored Procedures/uspCMBankTransactionReversal.sql
@@ -51,6 +51,8 @@ DECLARE @BANK_DEPOSIT INT = 1
 		,@PAYCHECK AS INT = 21
 		,@ACH AS INT = 22
 		,@DIRECT_DEPOSIT AS INT = 23
+		,@BANK_FEE AS INT = 27
+		,@NEGATIVE_AP_ECHECK AS INT = 120
 		
 -- Constant variables for Check number status. 
 DECLARE	@CHECK_NUMBER_STATUS_UNUSED AS INT = 1
@@ -369,6 +371,20 @@ WHERE	F.intBankTransactionTypeId IN (@AP_PAYMENT, @AR_PAYMENT, @AP_ECHECK, @ACH)
 				AND F.dtmCheckPrinted IS NULL 		
 			)
 		)
+
+DELETE GL FROM tblGLDetail GL INNER JOIN
+tblCMBankTransaction F ON GL.strTransactionId = F.strTransactionId
+JOIN #tmpCMBankTransaction TMP
+			ON F.strTransactionId  = TMP.strTransactionId + '-F'
+WHERE	F.intBankTransactionTypeId IN (@BANK_FEE)		
+		
+
+DELETE F FROM
+tblCMBankTransaction F INNER JOIN #tmpCMBankTransaction TMP
+			ON F.strTransactionId  = TMP.strTransactionId + '-F'
+WHERE	F.intBankTransactionTypeId IN (@BANK_FEE)		
+		
+
 IF @@ERROR <> 0	GOTO Exit_BankTransactionReversal_WithErrors
 
 Exit_Successfully:
