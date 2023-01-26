829bcd58c2ddd5626e7439f5eec8208e3422d25c
Jonathan Valenzuela <jonathan.valenzuela@irely.com>
2023-01-26 09:24:18 +0800

    MFG-4793

--- i21Database/dbo/Views/vyuMFLocationLeadTime.sql
+++ i21Database/dbo/Views/vyuMFLocationLeadTime.sql
@@ -1,34 +1,27 @@
 CREATE VIEW [dbo].[vyuMFLocationLeadTime]  
-
 AS  
-
-SELECT 
-
-[intLocationLeadTimeId],
-[intOriginId],
-[strOrigin],
-[intBuyingCenterId],
-[strBuyingCenter] = LOC.strLocationName,
-[intReceivingPlantId], 
-[strReceivingPlant],
-[intReceivingStorageLocation],
-[strReceivingStorageLocation] = SLOC.strSubLocationName,
-[intChannelId],
-[strChannel] = MZ.strMarketZoneCode,
-[intPortOfDispatchId],
-[strPortOfDispatch],
-[intPortOfArrivalId],
-[strPortOfArrival],
-[dblPurchaseToShipment],
-[dblPortToPort],
-[dblPortToMixingUnit],
-[dblMUToAvailableForBlending],
-[intEntityId] = LLT.intEntityId
-
-FROM 
-tblMFLocationLeadTime LLT
-
-INNER JOIN tblSMCompanyLocation LOC ON LLT.intBuyingCenterId = LOC.intCompanyLocationId
-INNER JOIN tblSMCompanyLocationSubLocation SLOC ON SLOC.intCompanyLocationSubLocationId=LLT.intReceivingStorageLocation
-INNER JOIN tblARMarketZone MZ ON MZ.intMarketZoneId=LLT.intChannelId
-LEFT JOIN tblSMCompanyLocation RLOC ON SLOC.intCompanyLocationId = RLOC.intCompanyLocationId
+SELECT intLocationLeadTimeId
+	 , intOriginId
+	 , strOrigin
+	 , intBuyingCenterId
+	 , strBuyingCenter = CompanyLocation.strLocationName
+	 , intReceivingPlantId
+	 , strReceivingPlant
+	 , intReceivingStorageLocation
+	 , strReceivingStorageLocation = SubLocation.strSubLocationName
+	 , intChannelId
+	 , strChannel = MarketZone.strMarketZoneCode
+	 , intPortOfDispatchId
+	 , strPortOfDispatch
+	 , intPortOfArrivalId
+	 , strPortOfArrival
+	 , dblPurchaseToShipment
+	 , dblPortToPort
+	 , dblPortToMixingUnit
+	 , dblMUToAvailableForBlending
+	 , intEntityId = LeadTime.intEntityId
+	 , strShippingLine
+FROM tblMFLocationLeadTime AS LeadTime
+INNER JOIN tblSMCompanyLocation AS CompanyLocation ON LeadTime.intBuyingCenterId = CompanyLocation.intCompanyLocationId
+INNER JOIN tblSMCompanyLocationSubLocation AS SubLocation ON SubLocation.intCompanyLocationSubLocationId=LeadTime.intReceivingStorageLocation
+INNER JOIN tblARMarketZone AS MarketZone ON MarketZone.intMarketZoneId=LeadTime.intChannelId
