57c82888b4e31eba4c48678f921b21eb8c2f1c87
Arman Arenas <arman.arenas@irely.com>
2023-01-26 20:48:11 +0800

    LG-3767 fixed weight claims GL accounts

--- i21Database/dbo/Stored Procedures/uspLGPostWeightClaims.sql
+++ i21Database/dbo/Stored Procedures/uspLGPostWeightClaims.sql
@@ -170,9 +170,9 @@ BEGIN /* Inbound Claims */
 			,strRateType = FX.strRateType
 			,strItemNo = I.strItemNo
 			,intEntityId = WCD.intPartyEntityId
-			,intAPAccountId = CASE WHEN WCD.dblClaimableWt < 0 THEN CL.intAPAccount ELSE NULL END
+			,intAPAccountId = CASE WHEN WCD.dblClaimableWt > 0 THEN CL.intAPAccount ELSE NULL END
 			,intAPClearingAccountId = dbo.fnGetItemGLAccount(I.intItemId, IL.intItemLocationId, 'AP Clearing')
-			,intExpenseAccountId = CASE WHEN WCD.dblClaimableWt < 0 THEN NULL ELSE V.intGLAccountExpenseId END
+			,intExpenseAccountId = CASE WHEN WCD.dblClaimableWt < 0 THEN V.intGLAccountExpenseId ELSE NULL END
 		FROM tblLGWeightClaimDetail WCD
 			INNER JOIN tblLGWeightClaim WC ON WC.intWeightClaimId = WCD.intWeightClaimId
 			INNER JOIN tblLGLoad L ON L.intLoadId = WC.intLoadId
@@ -237,8 +237,8 @@ BEGIN /* Inbound Claims */
 			,[strRateType]
 			)
 		-------------------------------------------------------------------------------------------
-		-- Inbound Claim Weight Loss Impact: Dr. AP, Cr. AP Clearing	
-		-- Inbound Claim Weight Gain Impact: Dr. AP Clearing, Cr. Expense
+		-- Inbound Claim Weight Loss Impact: Dr. AP Clearing, Cr. Expense	
+		-- Inbound Claim Weight Gain Impact: Dr. AP, Cr. AP Clearing
 		-------------------------------------------------------------------------------------------
 		SELECT dtmDate = ForGLEntries_CTE.dtmDate
 			,strBatchId = @strBatchId
@@ -277,7 +277,7 @@ BEGIN /* Inbound Claims */
 			,dblForeignRate = ForGLEntries_CTE.dblForexRate
 			,strRateType = ForGLEntries_CTE.strRateType
 		FROM ForGLEntries_CTE
-		INNER JOIN dbo.tblGLAccount GLAccount ON GLAccount.intAccountId = ISNULL(ForGLEntries_CTE.intAPAccountId, ForGLEntries_CTE.intAPClearingAccountId)
+		INNER JOIN dbo.tblGLAccount GLAccount ON GLAccount.intAccountId = ISNULL(ForGLEntries_CTE.intAPClearingAccountId, ForGLEntries_CTE.intAPAccountId)
 		CROSS APPLY dbo.fnGetDebitFunctional(ForGLEntries_CTE.dblCost, ForGLEntries_CTE.intCurrencyId, @intFunctionalCurrencyId, ForGLEntries_CTE.dblForexRate) Debit
 		CROSS APPLY dbo.fnGetDebit(ForGLEntries_CTE.dblCost) DebitForeign
 		WHERE ForGLEntries_CTE.intItemId IS NOT NULL
@@ -321,7 +321,7 @@ BEGIN /* Inbound Claims */
 			,dblForeignRate = ForGLEntries_CTE.dblForexRate
 			,strRateType = ForGLEntries_CTE.strRateType
 		FROM ForGLEntries_CTE
-		INNER JOIN dbo.tblGLAccount GLAccount ON GLAccount.intAccountId = ISNULL(ForGLEntries_CTE.intAPClearingAccountId, ForGLEntries_CTE.intExpenseAccountId)
+		INNER JOIN dbo.tblGLAccount GLAccount ON GLAccount.intAccountId = ISNULL(ForGLEntries_CTE.intExpenseAccountId, ForGLEntries_CTE.intAPClearingAccountId)
 		CROSS APPLY dbo.fnGetCreditFunctional(-ForGLEntries_CTE.dblCost, ForGLEntries_CTE.intCurrencyId, @intFunctionalCurrencyId, ForGLEntries_CTE.dblForexRate) Credit
 		CROSS APPLY dbo.fnGetCredit(-ForGLEntries_CTE.dblCost) CreditForeign
 		WHERE ForGLEntries_CTE.intItemId IS NOT NULL
