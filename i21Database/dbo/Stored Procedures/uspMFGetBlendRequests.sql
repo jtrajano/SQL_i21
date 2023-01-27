41a63f1265b594b5630e42f308c8ee5235e299ad
Jonathan Valenzuela <jonathan.valenzuela@irely.com>
2023-01-27 13:26:17 +0800

    MFG-4836

--- i21Database/dbo/Stored Procedures/uspMFGetBlendRequests.sql
+++ i21Database/dbo/Stored Procedures/uspMFGetBlendRequests.sql
@@ -7,63 +7,110 @@ SET NOCOUNT ON
 SET XACT_ABORT ON
 SET ANSI_WARNINGS OFF
 
-If @intWorkOrderId=0
-	Select a.intBlendRequirementId,a.strDemandNo,a.intItemId,b.strItemNo,b.strDescription,(a.dblQuantity - ISNULL(a.dblIssuedQty,0)) dblQuantity,
-	c.intItemUOMId,d.strUnitMeasure AS strUOM,a.dtmDueDate,a.intLocationId,
-	a.intManufacturingCellId AS intManufacturingCellId,
-	a.intMachineId,a.dblBlenderSize,g.dblStandardCost,mc.strCellName,r.intManufacturingProcessId,
-	CASE Month(GETDATE()) 
-			WHEN 1 THEN bg.dblJan WHEN 2 THEN bg.dblFeb WHEN 3 THEN bg.dblMar WHEN 4 THEN bg.dblApr WHEN 5 THEN bg.dblMay WHEN 6 THEN bg.dblJun 
-			WHEN 7 THEN bg.dblJul WHEN 8 THEN bg.dblAug WHEN 9 THEN bg.dblSep WHEN 10 THEN bg.dblOct WHEN 11 THEN bg.dblNov WHEN 12 THEN bg.dblDec 
-		END AS dblAffordabilityCost,
-	CompanyLocation.strLocationName AS strCompanyLocationName
-	,a.strReferenceNo AS strERPOrderNo
-	from tblMFBlendRequirement a 
-	Join tblICItem b on a.intItemId=b.intItemId 
-	Join tblICItemUOM c on b.intItemId=c.intItemId and a.intUOMId=c.intUnitMeasureId 
-	Join tblICUnitMeasure d on c.intUnitMeasureId=d.intUnitMeasureId 
-	Left Join tblMFRecipe e on a.intItemId=e.intItemId And a.intLocationId=e.intLocationId And e.ysnActive=1 
-	Left Join tblICItemLocation f on b.intItemId=f.intItemId and f.intLocationId=a.intLocationId
-	Left Join tblICItemPricing g on g.intItemId=b.intItemId And g.intItemLocationId=f.intItemLocationId
-	Left Join tblMFManufacturingCell mc on a.intManufacturingCellId=mc.intManufacturingCellId
-	Left Join tblMFRecipe r on a.intItemId=r.intItemId AND a.intLocationId=r.intLocationId AND r.ysnActive=1
-	Left Join tblMFBudget bg on a.intItemId=bg.intItemId AND a.intLocationId=bg.intLocationId AND bg.intYear=YEAR(GETDATE()) AND bg.intBudgetTypeId=2
-	left join tblSMCompanyLocation AS CompanyLocation ON a.intLocationId = CompanyLocation.intCompanyLocationId
-	Where a.intStatusId=1
+IF @intWorkOrderId = 0
+	SELECT a.intBlendRequirementId
+		 , a.strDemandNo
+		 , a.intItemId
+		 , b.strItemNo
+		 , b.strDescription
+		 , (a.dblQuantity - ISNULL(a.dblIssuedQty, 0)) AS dblQuantity
+		 , c.intItemUOMId
+		 , d.strUnitMeasure AS strUOM
+		 , a.dtmDueDate
+		 , a.intLocationId
+		 , a.intManufacturingCellId AS intManufacturingCellId
+		 , a.intMachineId
+		 , a.dblBlenderSize
+		 , g.dblStandardCost
+		 , mc.strCellName
+		 , r.intManufacturingProcessId
+		 , CASE MONTH(GETDATE()) WHEN 1 THEN bg.dblJan 
+								 WHEN 2 THEN bg.dblFeb 
+								 WHEN 3 THEN bg.dblMar 
+								 WHEN 4 THEN bg.dblApr 
+								 WHEN 5 THEN bg.dblMay 
+								 WHEN 6 THEN bg.dblJun 
+								 WHEN 7 THEN bg.dblJul 
+								 WHEN 8 THEN bg.dblAug 
+								 WHEN 9 THEN bg.dblSep 
+								 WHEN 10 THEN bg.dblOct 
+								 WHEN 11 THEN bg.dblNov 
+								 WHEN 12 THEN bg.dblDec 
+		   END AS dblAffordabilityCost
+		 , CompanyLocation.strLocationName AS strCompanyLocationName
+		 , a.strReferenceNo AS strERPOrderNo
+	FROM tblMFBlendRequirement a 
+	JOIN tblICItem b ON a.intItemId = b.intItemId 
+	JOIN tblICItemUOM c ON b.intItemId = c.intItemId AND a.intUOMId=c.intUnitMeasureId 
+	JOIN tblICUnitMeasure d ON c.intUnitMeasureId = d.intUnitMeasureId 
+	LEFT JOIN tblMFRecipe e ON a.intItemId = e.intItemId AND a.intLocationId = e.intLocationId AND e.ysnActive=1 
+	LEFT JOIN tblICItemLocation f ON b.intItemId = f.intItemId AND f.intLocationId=a.intLocationId
+	LEFT JOIN tblICItemPricing g ON g.intItemId = b.intItemId AND g.intItemLocationId=f.intItemLocationId
+	LEFT JOIN tblMFManufacturingCell mc ON a.intManufacturingCellId = mc.intManufacturingCellId
+	LEFT JOIN tblMFRecipe r ON a.intItemId = r.intItemId AND a.intLocationId=r.intLocationId AND r.ysnActive=1
+	LEFT JOIN tblMFBudget bg ON a.intItemId = bg.intItemId AND a.intLocationId = bg.intLocationId AND bg.intYear = YEAR(GETDATE()) AND bg.intBudgetTypeId = 2
+	LEFT JOIN tblSMCompanyLocation AS CompanyLocation ON a.intLocationId = CompanyLocation.intCompanyLocationId
+	WHERE a.intStatusId = 1
 
 --Positive means WorkOrderId
-If @intWorkOrderId>0
-	Select a.intBlendRequirementId,a.strDemandNo,a.intItemId,b.strItemNo,b.strDescription,
-	Case When (a.dblQuantity - ISNULL(a.dblIssuedQty,0))<=0 then 0 Else (a.dblQuantity - ISNULL(a.dblIssuedQty,0)) End AS dblQuantity,
-	c.intItemUOMId,d.strUnitMeasure AS strUOM,a.dtmDueDate,a.intLocationId,a.intManufacturingCellId,
-	h.dblStandardCost,f.intManufacturingProcessId,
-	CompanyLocation.strLocationName AS strCompanyLocationName
-	,f.strERPOrderNo AS strERPOrderNo
-	from tblMFBlendRequirement a 
-	Join tblICItem b on a.intItemId=b.intItemId 
-	Join tblICItemUOM c on b.intItemId=c.intItemId and a.intUOMId=c.intUnitMeasureId 
-	Join tblICUnitMeasure d on c.intUnitMeasureId=d.intUnitMeasureId 
-	Left Join tblMFRecipe e on a.intItemId=e.intItemId And a.intLocationId=e.intLocationId And e.ysnActive=1 
-	Join tblMFWorkOrder f on a.intBlendRequirementId=f.intBlendRequirementId
-	Left Join tblICItemLocation g on b.intItemId=g.intItemId and g.intLocationId=a.intLocationId
-	Left Join tblICItemPricing h on h.intItemId=b.intItemId And g.intItemLocationId=h.intItemLocationId
-	left join tblSMCompanyLocation AS CompanyLocation ON a.intLocationId = CompanyLocation.intCompanyLocationId
-	Where f.intWorkOrderId=@intWorkOrderId
+IF @intWorkOrderId > 0
+	SELECT a.intBlendRequirementId
+		 , a.strDemandNo
+		 , a.intItemId
+		 , b.strItemNo
+		 , b.strDescription
+		 , CASE WHEN (a.dblQuantity - ISNULL(a.dblIssuedQty, 0)) <= 0 THEN 0 
+				ELSE (a.dblQuantity - ISNULL(a.dblIssuedQty, 0)) 
+		   END AS dblQuantity
+		 , c.intItemUOMId
+		 , d.strUnitMeasure AS strUOM
+		 , a.dtmDueDate
+		 , a.intLocationId
+		 , a.intManufacturingCellId
+		 , h.dblStandardCost
+		 , f.intManufacturingProcessId
+		 , CompanyLocation.strLocationName AS strCompanyLocationName
+		 , f.strERPOrderNo AS strERPOrderNo
+		 , ISNULL(WorkOrderStatus.strName, 'Not Released')	AS strWorkOrderStatus
+	FROM tblMFBlendRequirement a 
+	JOIN tblICItem b ON a.intItemId = b.intItemId 
+	JOIN tblICItemUOM c ON b.intItemId = c.intItemId AND a.intUOMId = c.intUnitMeasureId 
+	JOIN tblICUnitMeasure d ON c.intUnitMeasureId = d.intUnitMeasureId 
+	LEFT JOIN tblMFRecipe e ON a.intItemId = e.intItemId AND a.intLocationId = e.intLocationId AND e.ysnActive=1 
+	JOIN tblMFWorkOrder f ON a.intBlendRequirementId = f.intBlendRequirementId
+	LEFT JOIN tblICItemLocation g ON b.intItemId = g.intItemId AND g.intLocationId = a.intLocationId
+	LEFT JOIN tblICItemPricing h ON h.intItemId = b.intItemId AND g.intItemLocationId = h.intItemLocationId
+	LEFT JOIN tblSMCompanyLocation AS CompanyLocation ON a.intLocationId = CompanyLocation.intCompanyLocationId
+	OUTER APPLY (SELECT WorkOrderStatus.strName
+				 FROM tblMFWorkOrder AS WorkOrder
+				 JOIN tblMFWorkOrderStatus AS WorkOrderStatus ON WorkOrder.intStatusId = WorkOrderStatus.intStatusId
+				 WHERE WorkOrder.intBlendRequirementId = a.intBlendRequirementId) AS WorkOrderStatus
+	WHERE f.intWorkOrderId = @intWorkOrderId
 
 --Negative means BlendRequirementId
-If @intWorkOrderId<0
-	Select a.intBlendRequirementId,a.strDemandNo,a.intItemId,b.strItemNo,b.strDescription,
-	Case When (a.dblQuantity - ISNULL(a.dblIssuedQty,0))<=0 then 0 Else (a.dblQuantity - ISNULL(a.dblIssuedQty,0)) End AS dblQuantity,
-	c.intItemUOMId,d.strUnitMeasure AS strUOM,a.dtmDueDate,a.intLocationId,a.intManufacturingCellId,
-	h.dblStandardCost,
-	CompanyLocation.strLocationName AS strCompanyLocationName
-	,a.strReferenceNo AS strERPOrderNo
-	from tblMFBlendRequirement a 
-	Join tblICItem b on a.intItemId=b.intItemId 
-	Join tblICItemUOM c on b.intItemId=c.intItemId and a.intUOMId=c.intUnitMeasureId 
-	Join tblICUnitMeasure d on c.intUnitMeasureId=d.intUnitMeasureId 
-	Left Join tblMFRecipe e on a.intItemId=e.intItemId And a.intLocationId=e.intLocationId And e.ysnActive=1 
-	Left Join tblICItemLocation g on b.intItemId=g.intItemId and g.intLocationId=a.intLocationId
-	Left Join tblICItemPricing h on h.intItemId=b.intItemId And g.intItemLocationId=h.intItemLocationId
-	left join tblSMCompanyLocation AS CompanyLocation ON a.intLocationId = CompanyLocation.intCompanyLocationId
-	Where a.intBlendRequirementId=ABS(@intWorkOrderId)
+IF @intWorkOrderId < 0
+	SELECT a.intBlendRequirementId
+		 , a.strDemandNo
+		 , a.intItemId
+		 , b.strItemNo
+		 , b.strDescription
+		 , CASE WHEN (a.dblQuantity - ISNULL(a.dblIssuedQty, 0)) <= 0 THEN 0 
+				ELSE (a.dblQuantity - ISNULL(a.dblIssuedQty, 0)) 
+		   END AS dblQuantity
+		 , c.intItemUOMId
+		 , d.strUnitMeasure AS strUOM
+		 , a.dtmDueDate
+		 , a.intLocationId
+		 , a.intManufacturingCellId
+		 , h.dblStandardCost
+		 , CompanyLocation.strLocationName AS strCompanyLocationName
+		 , a.strReferenceNo AS strERPOrderNo
+	FROM tblMFBlendRequirement a 
+	JOIN tblICItem b ON a.intItemId = b.intItemId 
+	JOIN tblICItemUOM c ON b.intItemId = c.intItemId AND a.intUOMId=c.intUnitMeasureId 
+	JOIN tblICUnitMeasure d ON c.intUnitMeasureId = d.intUnitMeasureId 
+	LEFT JOIN tblMFRecipe e ON a.intItemId = e.intItemId AND a.intLocationId = e.intLocationId AND e.ysnActive=1 
+	LEFT JOIN tblICItemLocation g ON b.intItemId = g.intItemId AND g.intLocationId = a.intLocationId
+	LEFT JOIN tblICItemPricing h ON h.intItemId = b.intItemId AND g.intItemLocationId = h.intItemLocationId
+	LEFT JOIN tblSMCompanyLocation AS CompanyLocation ON a.intLocationId = CompanyLocation.intCompanyLocationId
+	WHERE a.intBlendRequirementId = ABS(@intWorkOrderId)
--- i21Database/dbo/Stored Procedures/uspMFGetBlendSheet.sql
+++ i21Database/dbo/Stored Procedures/uspMFGetBlendSheet.sql
@@ -1,19 +1,52 @@
 ﻿CREATE PROCEDURE [dbo].[uspMFGetBlendSheet]
 	@intWorkOrderId int
 AS
-Select a.intWorkOrderId,a.strWorkOrderNo,a.intItemId,b.strItemNo,a.dblQuantity,
-a.dblPlannedQuantity,a.intItemUOMId,d.strUnitMeasure AS strUOM,a.intStatusId,a.intManufacturingCellId,a.intMachineId,
-a.dtmCreated,a.intCreatedUserId,a.dtmLastModified,a.intLastModifiedUserId,a.dtmExpectedDate,
-a.dblBinSize,a.intBlendRequirementId,a.ysnUseTemplate,
-a.ysnKittingEnabled,a.ysnDietarySupplements,a.strComment,a.intLocationId, 
-Case When (e.dblQuantity - ISNULL(e.dblIssuedQty ,0)) <=0 Then a.dblQuantity Else (e.dblQuantity - ISNULL(e.dblIssuedQty ,0)) End AS dblBalancedQtyToProduce,
-mc.strCellName,m.strName AS strMachineName,a.intManufacturingProcessId,a.intPlannedShiftId,s.strShiftName AS strPlannedShiftName,a.intTrialBlendSheetStatusId
-,a.strERPOrderNo AS strERPOrderNo
-From tblMFWorkOrder a Join tblICItem b on a.intItemId=b.intItemId
-Join tblICItemUOM c on a.intItemUOMId=c.intItemUOMId
-Join tblICUnitMeasure d on c.intUnitMeasureId=d.intUnitMeasureId
-Join tblMFBlendRequirement e on a.intBlendRequirementId=e.intBlendRequirementId
-Left Join tblMFManufacturingCell mc on a.intManufacturingCellId=mc.intManufacturingCellId
-Left Join tblMFMachine m on a.intMachineId=m.intMachineId
-Left Join tblMFShift s on a.intPlannedShiftId=s.intShiftId
-Where a.intWorkOrderId=@intWorkOrderId
+SELECT WorkOrder.intWorkOrderId
+	 , WorkOrder.strWorkOrderNo
+	 , WorkOrder.intItemId
+	 , Item.strItemNo
+	 , WorkOrder.dblQuantity
+	 , WorkOrder.dblPlannedQuantity
+	 , WorkOrder.intItemUOMId
+	 , UOM.strUnitMeasure AS strUOM
+	 , WorkOrder.intStatusId
+	 , WorkOrder.intManufacturingCellId
+	 , WorkOrder.intMachineId
+	 , WorkOrder.dtmCreated
+	 , WorkOrder.intCreatedUserId
+	 , WorkOrder.dtmLastModified
+	 , WorkOrder.intLastModifiedUserId
+	 , WorkOrder.dtmExpectedDate
+	 , WorkOrder.dblBinSize
+	 , WorkOrder.intBlendRequirementId
+	 , WorkOrder.ysnUseTemplate
+	 , WorkOrder.ysnKittingEnabled
+	 , WorkOrder.ysnDietarySupplements
+	 , WorkOrder.strComment
+	 , WorkOrder.intLocationId
+	 , CASE WHEN (BlendRequirement.dblQuantity - ISNULL(BlendRequirement.dblIssuedQty ,0)) <= 0 THEN WorkOrder.dblQuantity 
+			ELSE (BlendRequirement.dblQuantity - ISNULL(BlendRequirement.dblIssuedQty ,0)) 
+	   END AS dblBalancedQtyToProduce
+	 , ManufacturingCell.strCellName
+	 , Machine.strName					AS strMachineName
+	 , WorkOrder.intManufacturingProcessId
+	 , WorkOrder.intPlannedShiftId
+	 , ManufacturingShift.strShiftName	AS strPlannedShiftName
+	 , WorkOrder.intTrialBlendSheetStatusId
+	 , WorkOrder.strERPOrderNo			AS strERPOrderNo
+	 , WorkOrder.intIssuedUOMTypeId
+	 , IssuedUOMType.strName			AS strIssuedUOMType
+	 , ISNULL(WorkOrderStatus.strName, 'Not Released')	AS strWorkOrderStatus
+FROM tblMFWorkOrder AS WorkOrder 
+JOIN tblICItem AS Item ON WorkOrder.intItemId = Item.intItemId
+JOIN tblICItemUOM AS ItemUOM ON WorkOrder.intItemUOMId = ItemUOM.intItemUOMId
+JOIN tblICUnitMeasure AS UOM ON ItemUOM.intUnitMeasureId = UOM.intUnitMeasureId
+JOIN tblMFBlendRequirement AS BlendRequirement ON WorkOrder.intBlendRequirementId=  BlendRequirement.intBlendRequirementId
+LEFT JOIN tblMFManufacturingCell AS ManufacturingCell ON WorkOrder.intManufacturingCellId = ManufacturingCell.intManufacturingCellId
+LEFT JOIN tblMFMachine AS Machine ON WorkOrder.intMachineId = Machine.intMachineId
+LEFT JOIN tblMFShift AS ManufacturingShift ON WorkOrder.intPlannedShiftId = ManufacturingShift.intShiftId
+OUTER APPLY (SELECT strName
+			 FROM tblMFMachineIssuedUOMType 
+			 WHERE intIssuedUOMTypeId = WorkOrder.intIssuedUOMTypeId) AS IssuedUOMType
+LEFT JOIN tblMFWorkOrderStatus AS WorkOrderStatus ON WorkOrderStatus.intStatusId = WorkOrder.intStatusId
+WHERE WorkOrder.intWorkOrderId = @intWorkOrderId
