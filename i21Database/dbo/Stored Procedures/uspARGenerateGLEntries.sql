4985279d4d47c743d39ccfa325dbb08e56e34eba
Roudinee Panteria <roudinee.panteria@irely.com>
2023-01-30 15:53:02 +0800

    AR-14947

--- i21Database/dbo/Stored Procedures/uspARGenerateGLEntries.sql
+++ i21Database/dbo/Stored Procedures/uspARGenerateGLEntries.sql
@@ -37,7 +37,7 @@ DECLARE @TempGLEntries AS TABLE (
 	, [strCode]							NVARCHAR (40)    COLLATE Latin1_General_CI_AS NULL 
 	, [strReference]					NVARCHAR (255)   COLLATE Latin1_General_CI_AS NULL
 	, [intCurrencyId]					INT              NULL
-	, [dblExchangeRate]					NUMERIC (38, 20) DEFAULT 1 NULL
+	, [dblExchangeRate]					NUMERIC (38, 20) NULL DEFAULT 1
 	, [dtmDateEntered]					DATETIME         NOT NULL
 	, [dtmTransactionDate]				DATETIME         NULL
 	, [strJournalLineDescription]		NVARCHAR (250)   COLLATE Latin1_General_CI_AS NULL
