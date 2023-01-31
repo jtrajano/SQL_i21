aa86f52da18b3182438b7b251ea420edd61b2018
Jonathan Valenzuela <jonathan.valenzuela@irely.com>
2023-01-31 15:01:08 +0800

    QC-948

--- i21Database/dbo/Stored Procedures/uspQMReportCuppingFormSaleInfromation.sql
+++ i21Database/dbo/Stored Procedures/uspQMReportCuppingFormSaleInfromation.sql
@@ -1,24 +1,23 @@
-﻿CREATE PROCEDURE uspQMReportCuppingFormSaleInfromation
+﻿CREATE PROCEDURE [dbo].[uspQMReportCuppingFormSaleInfromation]
      @intContractDetailId INT
 AS
-
 DECLARE @ErrMsg NVARCHAR(MAX)
-
 BEGIN TRY
-	SELECT 
-		 strSContractNumber
-		,strBuyer
-		,dblSContractAllocatedQty
-		,strSItemUOM
-		,strShipmentPeriod = CONVERT(VARCHAR(10), dtmSStartDate, 104) + ' - ' + CONVERT(VARCHAR(10), dtmSEndDate, 104)
+	SELECT strSContractNumber
+		 , strBuyer
+		 , dblSContractAllocatedQty
+		 , strSItemUOM
+		 , strShipmentPeriod = CONVERT(VARCHAR(10), dtmSStartDate, 104) + ' - ' + CONVERT(VARCHAR(10), dtmSEndDate, 104)
 	FROM vyuLGAllocatedContracts
 	WHERE intPContractDetailId = @intContractDetailId
-
+	GROUP BY strSContractNumber
+		   , strBuyer
+		   , dblSContractAllocatedQty
+		   , strSItemUOM
+		   , CONVERT(VARCHAR(10), dtmSStartDate, 104) + ' - ' + CONVERT(VARCHAR(10), dtmSEndDate, 104)
 END TRY
-
 BEGIN CATCH
+	SET @ErrMsg = ERROR_MESSAGE();
 
-	SET @ErrMsg = ERROR_MESSAGE()  
-	RAISERROR (@ErrMsg,18,1,'WITH NOWAIT')  
-
+	RAISERROR (@ErrMsg, 18, 1, 'WITH NOWAIT'); 
 END CATCH
