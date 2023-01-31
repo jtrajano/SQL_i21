35f3b6da9928854a968633bee1a8d6162c0db331
Edsel Roldan Magura <edsel.magura@iRely.com>
2023-01-31 11:16:01 +0800

    HD-4225

---
+++ i21Database/dbo/Views/vyuHDTicketType.sql
@@ -0,0 +1,32 @@
+﻿CREATE VIEW [dbo].[vyuHDTicketType]
+AS
+SELECT    intTicketTypeId		= TicketType.intTicketTypeId
+         ,strType				= TicketType.strType
+         ,strDescription		= TicketType.strDescription
+         ,strJIRAType			= TicketType.strJIRAType
+         ,strIcon				= TicketType.strIcon
+         ,intSort				= TicketType.intSort
+         ,ysnTicket				= TicketType.ysnTicket
+         ,intTicketTypeTypeId	= TicketType.intTicketTypeTypeId
+         ,ysnActivity			= TicketType.ysnActivity
+         ,ysnOpportunity		= TicketType.ysnOpportunity
+         ,ysnProject			= TicketType.ysnProject
+         ,ysnCampaign			= TicketType.ysnCampaign
+         ,ysnDefaultProject		= TicketType.ysnDefaultProject
+         ,ysnDefaultTicket      = TicketType.ysnDefaultTicket
+         ,ysnSupported			= TicketType.ysnSupported
+         ,strTicketTypeType		= CASE WHEN TicketType.intTicketTypeTypeId = 1
+											THEN 'No'
+									   WHEN TicketType.intTicketTypeTypeId = 2
+											THEN 'Help Ticket'
+									   WHEN TicketType.intTicketTypeTypeId = 3
+											THEN 'Upgrade Ticket'
+									   WHEN TicketType.intTicketTypeTypeId = 4
+											THEN 'Statement of Work'
+									   ELSE ''
+								  END
+		  ,intConcurrencyId = TicketType.intConcurrencyId
+FROM tblHDTicketType TicketType
+
+		
+GO 
