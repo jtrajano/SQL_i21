a8ccce4f5cf92462c9cbf3c671cf93ee822a744e
Girome Andaya <girome.andaya@iRely.com>
2023-02-01 12:08:22 +0800

    TR-2331

--- i21Database/dbo/Views/vyuTRGetCompanyPreference.sql
+++ i21Database/dbo/Views/vyuTRGetCompanyPreference.sql
@@ -43,6 +43,7 @@ SELECT CP.intCompanyPreferenceId
 	, CP.intAdjustmentAccountId
 	, GLA.strAccountId strAdjustmentAccountId
 	, CP.dblAdjustmentTolerance
+	, CP.ysnIncludeSurchargeInQuote
 FROM tblTRCompanyPreference CP
 LEFT JOIN tblSMImportFileHeader Import on Import.intImportFileHeaderId = CP.intRackPriceImportMappingId
 LEFT JOIN tblSMImportFileHeader ImportBol ON ImportBol.intImportFileHeaderId = CP.intBolImportFormatId 
