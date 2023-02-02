49085a52f9942b1bf8be8dd822d269ec6e0a183e
Roudinee Panteria <roudinee.panteria@irely.com>
2023-02-02 16:09:47 +0800

    AR-16279

--- i21Database/dbo/Stored Procedures/uspARAddInventoryItemToInvoice.sql
+++ i21Database/dbo/Stored Procedures/uspARAddInventoryItemToInvoice.sql
@@ -188,6 +188,14 @@ IF NOT EXISTS(	SELECT NULL
 		end
 	END
 
+-- IF ISNULL(@ItemSiteId, 0) <> 0 AND @SourceType = 'Tank Delivery' AND @ItemLoadDistributionDetailId IS NOT NULL AND NOT EXISTS (SELECT TOP 1 1 FROM tblTMDispatch WHERE intSiteID = @ItemSiteId)
+-- 	BEGIN		
+-- 		IF ISNULL(@RaiseError,0) = 1
+-- 			RAISERROR('Tank Delivery has no available Order!', 16, 1);
+-- 		SET @ErrorMessage = 'Tank Delivery has no available Order!'
+-- 		RETURN 0;
+-- 	END
+	
 IF ISNULL(@RaiseError,0) = 0	
 BEGIN
 	IF @InitTranCount = 0
@@ -266,23 +274,23 @@ IF (ISNULL(@RefreshPrice,0) = 1)
 		END
 	END
 
-IF ISNULL(@ItemSiteId, 0) <> 0 AND @SourceType = 'Tank Delivery' AND @ItemLoadDistributionDetailId IS NOT NULL AND @ContractHeaderId IS NULL AND @ContractDetailId IS NULL
-BEGIN
-	SELECT TOP 1 @ContractDetailId 	= TMO.intContractDetailId
-				, @ContractHeaderId 	= CH.intContractHeaderId
-				, @ItemPrice			= ISNULL(CD.dblCashPrice, 0)
-	FROM tblTMSite S
-	INNER JOIN tblTMOrder TMO ON S.intSiteID = TMO.intSiteId
-	INNER JOIN tblTMDispatch TMD ON TMD.intSiteID = S.intSiteID
-	INNER JOIN tblCTSequenceUsageHistory CU ON TMO.intContractDetailId = CU.intContractDetailId AND TMO.intSiteId = CU.intExternalId
-	INNER JOIN tblCTContractDetail CD ON CD.intContractDetailId = CU.intContractDetailId
-	INNER JOIN tblCTContractHeader CH ON CD.intContractHeaderId = CH.intContractHeaderId
-	WHERE S.intSiteID = @ItemSiteId
-		AND CU.strFieldName = 'Scheduled Quantity'
-		AND CU.strScreenName = 'TM - Create Order'
-		AND CD.intItemId = @ItemId
-END
-		
+--IF ISNULL(@ItemSiteId, 0) <> 0 AND @SourceType = 'Tank Delivery' AND @ItemLoadDistributionDetailId IS NOT NULL AND @ContractHeaderId IS NULL AND @ContractDetailId IS NULL
+--BEGIN
+--	SELECT TOP 1 @ContractDetailId 	= TMO.intContractDetailId
+--				, @ContractHeaderId 	= CH.intContractHeaderId
+--				, @ItemPrice			= ISNULL(CD.dblCashPrice, 0)
+--	FROM tblTMSite S
+--	INNER JOIN tblTMOrder TMO ON S.intSiteID = TMO.intSiteId
+--	INNER JOIN tblTMDispatch TMD ON TMD.intSiteID = S.intSiteID
+--	INNER JOIN tblCTSequenceUsageHistory CU ON TMO.intContractDetailId = CU.intContractDetailId AND TMO.intSiteId = CU.intExternalId
+--	INNER JOIN tblCTContractDetail CD ON CD.intContractDetailId = CU.intContractDetailId
+--	INNER JOIN tblCTContractHeader CH ON CD.intContractHeaderId = CH.intContractHeaderId
+--	WHERE S.intSiteID = @ItemSiteId
+--		AND CU.strFieldName = 'Scheduled Quantity'
+--		AND CU.strScreenName = 'TM - Create Order'
+--		AND CD.intItemId = @ItemId
+--END
+	
 END TRY
 BEGIN CATCH
 	SET @ErrorMessage = ERROR_MESSAGE();
@@ -608,6 +616,29 @@ BEGIN
 	WHERE
 		intLoadDetailId = @ItemLoadDetailId 
 END
+
+----OVERAGE TRANSPORT AND TM ORDER
+--IF ISNULL(@ItemSiteId, 0) <> 0 AND @SourceType = 'Tank Delivery' AND @ItemLoadDistributionDetailId IS NOT NULL AND @ContractDetailId IS NOT NULL
+--BEGIN
+--	IF EXISTS (
+--		SELECT TOP 1 1 
+--		FROM tblARInvoiceDetail ID
+--		INNER JOIN tblCTContractDetail CTD ON ID.intContractDetailId = CTD.intContractDetailId
+--		WHERE ID.intInvoiceId = @InvoiceId
+--			AND ID.intLoadDistributionDetailId IS NOT NULL
+--			AND ID.intContractDetailId IS NOT NULL
+--			AND ID.intSiteId IS NOT NULL
+--			AND ISNULL(ID.[dblQtyShipped], 0) > ISNULL(CTD.dblBalance, 0) - ISNULL(CTD.dblScheduleQty, 0)
+--	)
+--	BEGIN
+--	 	EXEC dbo.uspARUpdateOverageContracts @intInvoiceId 			= @InvoiceId
+--	 										, @intScaleUOMId 		= NULL
+--	 										, @intUserId 			= 1
+--	 										, @dblNetWeight 			= 0
+--	 										, @ysnFromSalesOrder 	= 0
+--	 										, @ysnFromImport			= 1
+--	END
+--END
 		
 BEGIN TRY
  	IF @RecomputeTax = 1
