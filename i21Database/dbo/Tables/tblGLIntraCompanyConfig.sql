05b596c103634426aff75d161048fb3c63b2ff49
Jeff Trajano <jeffrey.trajano@irely.com>
2023-02-03 22:49:18 +0800

    GL-9940

--- i21Database/dbo/Tables/tblGLIntraCompanyConfig.sql
+++ i21Database/dbo/Tables/tblGLIntraCompanyConfig.sql
@@ -2,7 +2,7 @@ CREATE TABLE tblGLIntraCompanyConfig
 (
     intIntraCompanyConfigId INT IDENTITY (1,1),
     intParentCompanySegmentId INT NOT NULL,
-    intTargeCompanySegmentId INT NOT NULL,
+    intTargetCompanySegmentId INT NOT NULL,
     intDueFromAccountId INT NOT NULL,
     intDueToAccountId INT NOT NULL,
     intConcurrencyId INT NULL,
