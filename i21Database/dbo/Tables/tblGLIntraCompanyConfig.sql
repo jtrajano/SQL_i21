e3262bd409d4af5fdd6aa94c191a0f8152ddaf0c
Jeff Trajano <jeffrey.trajano@irely.com>
2023-02-03 22:49:18 +0800

    GL-9940

---
+++ i21Database/dbo/Tables/tblGLIntraCompanyConfig.sql
@@ -0,0 +1,10 @@
+CREATE TABLE tblGLIntraCompanyConfig
+(
+    intIntraCompanyConfigId INT IDENTITY (1,1),
+    intParentCompanySegmentId INT NOT NULL,
+    intTargetCompanySegmentId INT NOT NULL,
+    intDueFromAccountId INT NOT NULL,
+    intDueToAccountId INT NOT NULL,
+    intConcurrencyId INT NULL,
+	CONSTRAINT [PK_tblGLIntraCompanyConfig] PRIMARY KEY CLUSTERED (intIntraCompanyConfigId ASC)
+)
