035a30a60a69da9d60179b51aae875013020db58
Leslie Arat <leslie.arat@irely.com>
2023-02-03 19:55:27 +0800

    GRN-3092 (HDTN-274706 - Need Grain Flow Report - Purchasing)

--- i21Database/dbo/Views/vyuGRGrainFlowReport.sql
+++ i21Database/dbo/Views/vyuGRGrainFlowReport.sql
@@ -1,181 +1,195 @@
 ﻿CREATE VIEW [dbo].[vyuGRGrainFlowReport]
 AS
+--DELIVERED
+--CONTRACTS and SPOT
 SELECT 
-	strCommodityCode
-	,dtmReceiptDate
-	,dblDelivered = SUM(dblDelivered)
-	,dblDirect = SUM(dblDirect)
-	,dblFromStorage = SUM(dblFromStorage)
-	,dblUnpricedReceipts = SUM(dblUnpricedReceipts)
-	,dblAllSales = SUM(dblAllSales)
-	,dblBasis = AVG(dblBasis)
-	,dblFutures = AVG(dblFutures)
-	,strFuturesMonth
-FROM (
---delivered
-SELECT 
-	ICRI.strCommodityCode
-	,ICRI.dtmReceiptDate
-	--,ICRI.strLocationName
-	--,dblDelivered = SUM(SC.dblNetUnits)
-	,dblDelivered = SC.dblNetUnits
-	,dblDirect = 0
-	,dblFromStorage = 0
-	,dblUnpricedReceipts = 0
-	,dblAllSales = 0
-	,ICRI.dblBasis
-	,ICRI.dblFutures
-	,ICRI.strFuturesMonth
+	strCommodityCode		= ICRI.strCommodityCode
+	,dtmReceiptDate			= FORMAT(ICRI.dtmReceiptDate,'MM/yyyy')
+	,dblDelivered 			= dbo.fnCTConvertQuantityToTargetCommodityUOM(CO_UOM_FROM.intCommodityUnitMeasureId,CO_UOM_TO.intCommodityUnitMeasureId,SC.dblNetUnits)
+	,dblDirect 				= NULL
+	,dblFromStorage 		= NULL
+	,dblUnpricedReceipts 	= NULL
+	,dblAllSales 			= NULL
+	,dblBuyBasis			= ISNULL(ICRI.dblBasis,SC.dblUnitBasis)
+	,dblSellBasis			= NULL
+	,strFuturesMonth		= ISNULL(REPLACE(ICRI.strFuturesMonth, ' ',''),REPLACE(FM.strFutureMonth, ' ',''))
 FROM tblSCTicket SC 
 INNER JOIN tblICInventoryReceipt ICR ON SC.intInventoryReceiptId = ICR.intInventoryReceiptId
 INNER JOIN vyuICGetInventoryReceiptItem ICRI ON SC.strTicketNumber = ICRI.strSourceNumber 
 	AND SC.intInventoryReceiptId = ICRI.intInventoryReceiptId
-WHERE ICRI.strReceiptType = 'Purchase Contract'
---GROUP BY ICRI.strCommodityCode
---	,ICRI.dtmReceiptDate
---	--,ICRI.strLocationName
---	,ICRI.dblBasis
---	,ICRI.dblFutures
---	,ICRI.strFuturesMonth
+LEFT JOIN (
+	tblCTContractDetail CD
+	INNER JOIN tblRKFuturesMonth FM
+		ON FM.intFutureMonthId = CD.intFutureMonthId
+	)	
+	ON CD.intContractDetailId = ICRI.intOrderId
+		AND CD.intContractSeq = ICRI.intContractSeq
+INNER JOIN tblICItemUOM UOM 
+	ON UOM.intItemUOMId = SC.intItemUOMIdTo
+INNER JOIN tblICCommodityUnitMeasure CO_UOM_FROM
+	ON CO_UOM_FROM.intCommodityId = ICRI.intCommodityId
+		AND CO_UOM_FROM.intUnitMeasureId = UOM.intUnitMeasureId
+INNER JOIN tblICCommodityUnitMeasure CO_UOM_TO
+	ON CO_UOM_TO.intCommodityId = ICRI.intCommodityId
+		AND CO_UOM_TO.ysnStockUnit = 1
 UNION ALL
---direct
+--DIRECT
+--DIRECT IN
 SELECT 
-	CO.strCommodityCode
-	,dbo.fnRemoveTimeOnDate(SC.dtmDateCreatedUtc)
-	--,CL.strLocationName
-	,0
-	,SC.dblNetUnits
-	,0
-	,0
-	,0
-	,ISNULL(CD.dblBasis,0)
-	,ISNULL(CD.dblFutures,0)
-	,ISNULL(REPLACE(FM.strFutureMonth, ' ',''),'')
-FROM tblSCTicket SC
-INNER JOIN tblICCommodity CO
-	ON CO.intCommodityId = SC.intCommodityId
-INNER JOIN tblSMCompanyLocation CL
-	ON CL.intCompanyLocationId = SC.intCompanyId
-INNER JOIN tblCTContractDetail CD
-	ON CD.intContractDetailId = SC.intContractId
-INNER JOIN tblRKFuturesMonth FM
-	ON FM.intFutureMonthId = CD.intFutureMonthId
-WHERE SC.intTicketType = 6
---GROUP BY CO.strCommodityCode
---	,dbo.fnRemoveTimeOnDate(SC.dtmDateCreatedUtc)
---	--,CL.strLocationName
---	,CD.dblBasis
---	,CD.dblFutures
---	,REPLACE(FM.strFutureMonth, ' ','')
+	strCommodityCode		= IC.strCommodityCode
+	,dtmReceiptDate			= FORMAT(SL.dtmTransactionDate,'MM/yyyy')
+	,dblDelivered 			= NULL
+	,dblDirect 				= dbo.fnCTConvertQuantityToTargetCommodityUOM(CO_UOM_FROM.intCommodityUnitMeasureId,CO_UOM_TO.intCommodityUnitMeasureId,SL.dblOrigQty)
+	,dblFromStorage 		= NULL
+	,dblUnpricedReceipts 	= NULL
+	,dblAllSales 			= NULL
+	,dblBuyBasis			= ISNULL(CD.dblBasis,SC.dblUnitBasis)
+	,dblSellBasis			= NULL
+	,strFuturesMonth		= REPLACE(FM.strFutureMonth, ' ','')
+FROM tblRKSummaryLog SL
+INNER JOIN tblSCTicket SC
+	ON SC.intTicketId = SL.intTransactionRecordHeaderId
+INNER JOIN tblICCommodity IC
+	ON IC.intCommodityId = SL.intCommodityId
+LEFT JOIN (
+	tblCTContractDetail CD
+	INNER JOIN tblRKFuturesMonth FM
+		ON FM.intFutureMonthId = CD.intFutureMonthId
+	)	
+	ON CD.intContractDetailId = SL.intContractDetailId
+INNER JOIN tblICCommodityUnitMeasure CO_UOM_FROM
+	ON CO_UOM_FROM.intCommodityUnitMeasureId = SL.intOrigUOMId
+LEFT JOIN tblICCommodityUnitMeasure CO_UOM_TO
+	ON CO_UOM_TO.intCommodityId = SL.intCommodityId
+		AND CO_UOM_TO.ysnStockUnit = 1
+WHERE SL.strTransactionType = 'Direct In'
 UNION ALL
---from storage
+--FROM STORAGE
+--SETTLEMENTS
 SELECT 
-	CO.strCommodityCode
-	,dbo.fnRemoveTimeOnDate(SS.dtmCreated)
-	--,CL.strLocationName
-	,0
-	,0
-	,SC.dblUnits
-	,0
-	,0
-	,ISNULL(CD.dblBasis,0)
-	,ISNULL(CD.dblFutures,0)
-	,ISNULL(REPLACE(FM.strFutureMonth, ' ',''),'')
+	strCommodityCode		= CO.strCommodityCode
+	,dtmReceiptDate			= FORMAT(SS.dtmCreated,'MM/yyyy')
+	,dblDelivered 			= NULL
+	,dblDirect 				= NULL
+	,dblFromStorage 		= dbo.fnCTConvertQuantityToTargetCommodityUOM(CO_UOM_FROM.intCommodityUnitMeasureId,CO_UOM_TO.intCommodityUnitMeasureId,ISNULL(SC.dblUnits,SS.dblSpotUnits))
+	,dblUnpricedReceipts 	= NULL
+	,dblAllSales 			= NULL
+	,dblBuyBasis			= ISNULL(CD.dblBasis,SS.dblFuturesBasis)
+	,dblSellBasis			= NULL
+	,strFuturesMonth		= REPLACE(FM.strFutureMonth, ' ','')
 FROM tblGRSettleStorage SS
 INNER JOIN tblICCommodity CO
 	ON CO.intCommodityId = SS.intCommodityId
 INNER JOIN tblSMCompanyLocation CL
 	ON CL.intCompanyLocationId = SS.intCompanyLocationId
-INNER JOIN tblGRSettleContract SC
+LEFT JOIN (
+	tblGRSettleContract SC
+	INNER JOIN tblCTContractDetail CD
+		ON CD.intContractDetailId = SC.intContractDetailId
+	INNER JOIN tblRKFuturesMonth FM
+		ON FM.intFutureMonthId = CD.intFutureMonthId
+	)
 	ON SC.intSettleStorageId = SS.intSettleStorageId
-INNER JOIN tblCTContractDetail CD
-	ON CD.intContractDetailId = SC.intContractDetailId
-INNER JOIN tblRKFuturesMonth FM
-	ON FM.intFutureMonthId = CD.intFutureMonthId
+INNER JOIN tblICItemUOM UOM 
+	ON UOM.intItemUOMId = SS.intItemUOMId
+INNER JOIN tblICCommodityUnitMeasure CO_UOM_FROM
+	ON CO_UOM_FROM.intCommodityId = SS.intCommodityId
+		AND CO_UOM_FROM.intUnitMeasureId = UOM.intUnitMeasureId
+INNER JOIN tblICCommodityUnitMeasure CO_UOM_TO
+	ON CO_UOM_TO.intCommodityId = SS.intCommodityId
+		AND CO_UOM_TO.ysnStockUnit = 1
 WHERE SS.intParentSettleStorageId IS NOT NULL
---GROUP BY CO.strCommodityCode
---	,dbo.fnRemoveTimeOnDate(SS.dtmCreated)
---	--,CL.strLocationName
---	,CD.dblBasis
---	,CD.dblFutures
---	,REPLACE(FM.strFutureMonth, ' ','')
 UNION ALL
 --1.1 unpriced receipts, customer storage
 SELECT 
-	CO.strCommodityCode
-	,dbo.fnRemoveTimeOnDate(CS.dtmDeliveryDate)
-	--,CL.strLocationName
-	,0
-	,0
-	,0
-	,CS.dblOpenBalance
-	,0
-	,0
-	,0
-	,''
+	strCommodityCode		= CO.strCommodityCode
+	,dtmReceiptDate			= FORMAT(CS.dtmDeliveryDate,'MM/yyyy')
+	,dblDelivered 			= NULL
+	,dblDirect 				= NULL
+	,dblFromStorage 		= dbo.fnCTConvertQuantityToTargetCommodityUOM(CO_UOM_FROM.intCommodityUnitMeasureId,CO_UOM_TO.intCommodityUnitMeasureId,CS.dblOpenBalance)
+	,dblUnpricedReceipts 	= NULL
+	,dblAllSales 			= NULL
+	,dblBuyBasis			= NULL
+	,dblSellBasis			= NULL
+	,strFuturesMonth		= NULL
 FROM tblGRCustomerStorage CS
 INNER JOIN tblICCommodity CO
 	ON CO.intCommodityId = CS.intCommodityId
 INNER JOIN tblSMCompanyLocation CL
 	ON CL.intCompanyLocationId = CS.intCompanyLocationId
-WHERE dblOpenBalance > 0
---GROUP BY CO.strCommodityCode
---	,CS.dtmDeliveryDate
---	--,CL.strLocationName
+INNER JOIN tblICItemUOM UOM 
+	ON UOM.intItemUOMId = CS.intItemUOMId
+INNER JOIN tblICCommodityUnitMeasure CO_UOM_FROM
+	ON CO_UOM_FROM.intCommodityId = CS.intCommodityId
+		AND CO_UOM_FROM.intUnitMeasureId = UOM.intUnitMeasureId
+INNER JOIN tblICCommodityUnitMeasure CO_UOM_TO
+	ON CO_UOM_TO.intCommodityId = CS.intCommodityId
+		AND CO_UOM_TO.ysnStockUnit = 1
+WHERE CS.dblOpenBalance > 0
 UNION ALL
 --1.2 unpriced receipts, hold
 SELECT 
-	CO.strCommodityCode
-	,dbo.fnRemoveTimeOnDate(SC.dtmDateCreatedUtc)
-	--,CL.strLocationName
-	,0
-	,0
-	,0
-	,SC.dblNetUnits
-	,0
-	,0
-	,0
-	,''
-FROM tblSCTicket SC
-INNER JOIN tblICCommodity CO
-	ON CO.intCommodityId = SC.intCommodityId
-INNER JOIN tblSMCompanyLocation CL
-	ON CL.intCompanyLocationId = SC.intCompanyId
-INNER JOIN tblCTContractDetail CD
-	ON CD.intContractDetailId = SC.intContractId
-INNER JOIN tblRKFuturesMonth FM
-	ON FM.intFutureMonthId = CD.intFutureMonthId
-WHERE SC.strDistributionOption = 'HLD'
---GROUP BY CO.strCommodityCode
---	,SC.dtmDateCreatedUtc
---	--,CL.strLocationName
+	strCommodityCode		= IC.strCommodityCode
+	,dtmReceiptDate			= FORMAT(SL.dtmTransactionDate,'MM/yyyy')
+	,dblDelivered 			= NULL
+	,dblDirect 				= NULL
+	,dblFromStorage 		= NULL
+	,dblUnpricedReceipts 	= dbo.fnCTConvertQuantityToTargetCommodityUOM(CO_UOM_FROM.intCommodityUnitMeasureId,CO_UOM_TO.intCommodityUnitMeasureId,SL.dblOrigQty)
+	,dblAllSales 			= NULL
+	,dblBuyBasis			= ISNULL(CD.dblBasis,SC.dblUnitBasis)
+	,dblSellBasis			= NULL
+	,strFuturesMonth		= NULL
+FROM tblRKSummaryLog SL
+INNER JOIN tblSCTicket SC
+	ON SC.intTicketId = SL.intTransactionRecordHeaderId
+INNER JOIN tblICCommodity IC
+	ON IC.intCommodityId = SL.intCommodityId
+LEFT JOIN (
+	tblCTContractDetail CD
+	INNER JOIN tblRKFuturesMonth FM
+		ON FM.intFutureMonthId = CD.intFutureMonthId
+	)	
+	ON CD.intContractDetailId = SL.intContractDetailId
+INNER JOIN tblICCommodityUnitMeasure CO_UOM_FROM
+	ON CO_UOM_FROM.intCommodityUnitMeasureId = SL.intOrigUOMId
+LEFT JOIN tblICCommodityUnitMeasure CO_UOM_TO
+	ON CO_UOM_TO.intCommodityId = SL.intCommodityId
+		AND CO_UOM_TO.ysnStockUnit = 1
+WHERE SL.strDistributionType = 'Hold'
+AND SC.intCommodityId = 11
 UNION ALL
 --all sales
 SELECT
-	ICSI.strCommodityCode
-	,dbo.fnRemoveTimeOnDate(ICSI.dtmShipDate)
-	--,ICSI.strShipFromLocation
-	,0
-	,0
-	,0
-	,0
-	,SC.dblNetUnits
-	,ISNULL(ICSI.dblBasis,0)
-	,ISNULL(ICSI.dblFutures,0)
-	,ISNULL(ICSI.strFuturesMonth,'')
+	strCommodityCode		= ICSI.strCommodityCode
+	,dtmReceiptDate			= FORMAT(ICSI.dtmShipDate,'MM/yyyy')
+	--,ICRI.strLocationName
+	--,dblDelivered = SUM(SC.dblNetUnits)
+	,dblDelivered 			= NULL
+	,dblDirect 				= NULL
+	,dblFromStorage 		= NULL
+	,dblUnpricedReceipts 	= NULL
+	,dblAllSales 			= dbo.fnCTConvertQuantityToTargetCommodityUOM(CO_UOM_FROM.intCommodityUnitMeasureId,CO_UOM_TO.intCommodityUnitMeasureId,SC.dblNetUnits)
+	,dblBuyBasis			= NULL
+	,dblSellBasis			= CASE WHEN ISNULL(ICSI.dblBasis,0) = 0 THEN CD.dblBasis ELSE ICSI.dblBasis END
+	,strFuturesMonth		= ISNULL(REPLACE(ICSI.strFuturesMonth, ' ',''),REPLACE(FM.strFutureMonth, ' ',''))
 FROM tblSCTicket SC  
 INNER JOIN vyuICGetInventoryShipmentItem ICSI
 	ON SC.intTicketId = ICSI.intSourceId
 	AND SC.intItemId = ICSI.intItemId
 	AND ICSI.strSourceType = 'Scale'
+LEFT JOIN (
+	tblCTContractDetail CD
+	INNER JOIN tblRKFuturesMonth FM
+		ON FM.intFutureMonthId = CD.intFutureMonthId
+	)	
+	ON CD.intContractDetailId = ICSI.intOrderId
+		AND CD.intContractSeq = ICSI.intContractSeq
+INNER JOIN tblICItemUOM UOM 
+	ON UOM.intItemUOMId = SC.intItemUOMIdTo
+INNER JOIN tblICCommodityUnitMeasure CO_UOM_FROM
+	ON CO_UOM_FROM.intCommodityId = ICSI.intCommodityId
+		AND CO_UOM_FROM.intUnitMeasureId = UOM.intUnitMeasureId
+INNER JOIN tblICCommodityUnitMeasure CO_UOM_TO
+	ON CO_UOM_TO.intCommodityId = ICSI.intCommodityId
+		AND CO_UOM_TO.ysnStockUnit = 1
 WHERE SC.strDistributionOption = 'Contract'
---GROUP BY ICSI.strCommodityCode
---	,dbo.fnRemoveTimeOnDate(ICSI.dtmShipDate)
---	--,ICSI.strShipFromLocation
---	,ICSI.dblBasis
---	,ICSI.dblFutures
---	,ICSI.strFuturesMonth
-) A
-GROUP BY strCommodityCode
-	,dtmReceiptDate
-	,strFuturesMonth
