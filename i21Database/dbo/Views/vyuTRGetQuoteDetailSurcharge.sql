d87aea216a8730832c3ecbb721d208e185ecbc08
Lawrence Zabala <lawrence.zabala@irely.com>
2023-02-03 13:09:37 +0800

    TR-2331

--- i21Database/dbo/Views/vyuTRGetQuoteDetailSurcharge.sql
+++ i21Database/dbo/Views/vyuTRGetQuoteDetailSurcharge.sql
@@ -3,7 +3,7 @@
 	
 SELECT QD.intQuoteDetailId
 	, intItemId = Pref.intItemForFreightId
-	, Item.strItemNo
+	, strItemNo = Item.strItemNo + CASE WHEN ISNULL(QD.dblSurcharge, 0) <> 0 THEN ' - ' + CAST(FORMAT(((ISNULL(QD.dblSurcharge, 0) / ISNULL(QD.dblFreightRate, 0)) * 100), 'N2') AS NVARCHAR) + '%' END 
 	, dblSurcharge = ISNULL(QD.dblSurcharge, 0)
 FROM tblTRQuoteDetail QD
 JOIN tblTRCompanyPreference Pref ON ISNULL(Pref.intSurchargeItemId, '') <> ''
