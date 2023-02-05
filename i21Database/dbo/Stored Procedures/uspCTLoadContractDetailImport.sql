e5e958aebe9107505e55ce9c6391155334c75065
Bryan-PC <013.bryan@gmail.com>
2023-02-05 21:27:50 +0800

    CT-8282

---
+++ i21Database/dbo/Stored Procedures/uspCTLoadContractDetailImport.sql
@@ -0,0 +1,24 @@
+﻿Create PROCEDURE [dbo].[uspCTLoadContractDetailImport]
+	@intContractDetailImportHeaderId INT
+AS
+
+BEGIN TRY	
+
+	Declare @ErrMsg Nvarchar(MAX)
+
+	SELECT * 
+	into #tmpTable
+	FROM  vyuCTContractDetailImport
+	WHERE intContractDetailImportHeaderId = @intContractDetailImportHeaderId
+
+
+	DELETE FROM tblCTContractDetailImport WHERE intContractDetailImportHeaderId = @intContractDetailImportHeaderId
+
+	SELECT * FROM #tmpTable ORDER BY intContractDetailImportId
+
+
+END TRY
+BEGIN CATCH
+	SET @ErrMsg = ERROR_MESSAGE()  
+	RAISERROR (@ErrMsg,18,1,'WITH NOWAIT')  	
+END CATCH
