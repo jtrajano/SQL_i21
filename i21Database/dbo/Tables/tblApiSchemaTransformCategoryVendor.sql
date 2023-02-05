2193f87859fe6ec516927ab12b2513f0747552cd
Wendell Wayne Estrada <wendell.estrada@irely.com>
2023-02-06 06:52:57 +0800

    API-358 HDTN-284781 - P03 - Category POS & Category XRef Imports Not Functional

--- i21Database/dbo/Tables/tblApiSchemaTransformCategoryVendor.sql
+++ i21Database/dbo/Tables/tblApiSchemaTransformCategoryVendor.sql
@@ -1,5 +1,5 @@
 CREATE TABLE [dbo].[tblApiSchemaTransformCategoryVendor](
-	[intKey] [int] IDENTITY(1,1) NOT NULL,
+	[intKey] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
 	[guiApiUniqueId] [uniqueidentifier] NOT NULL,
 	[intRowNumber] [int] NULL,
 	[strCategory] [nvarchar](200) COLLATE Latin1_General_CI_AS NOT NULL,
@@ -13,10 +13,5 @@ CREATE TABLE [dbo].[tblApiSchemaTransformCategoryVendor](
 	[strDefaultFamily] [nvarchar](200) COLLATE Latin1_General_CI_AS NULL,
 	[strDefaultSellClass] [nvarchar](200) COLLATE Latin1_General_CI_AS NULL,
 	[strDefaultOrderClass] [nvarchar](200) COLLATE Latin1_General_CI_AS NULL,
-	[strComments] [nvarchar](200) COLLATE Latin1_General_CI_AS NULL,
-PRIMARY KEY CLUSTERED 
-(
-	[intKey] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
+	[strComments] [nvarchar](200) COLLATE Latin1_General_CI_AS NULL
+)
