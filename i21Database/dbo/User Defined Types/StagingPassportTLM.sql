36d5b8cd3af32b098b085121beec6754a3627087
Nelson Reyes <nelson.reyes@irely.com>
2023-02-07 10:57:26 +0800

    ST-2416

--- i21Database/dbo/User Defined Types/StagingPassportTLM.sql
+++ i21Database/dbo/User Defined Types/StagingPassportTLM.sql
@@ -3,7 +3,7 @@ CREATE TYPE StagingPassportTLM AS TABLE
 	[intRowCount] 								  INT				NULL,
 	[intTaxLevelID]						  		  INT			    NULL,
 	[dblMerchandiseCode]				  		  NUMERIC(18,6)     NULL,
-	[dblTaxableSalesAmount]						  NUMERIC(18,6)     NULL,
+	[dblTaxableSalesAmount]						  NUMERIC(24,6)     NULL,
 	[dblTaxableSalesRefundedAmount]				  NUMERIC(18,6)     NULL,
 	[dblTaxCollectedAmount]						  NUMERIC(18,6)     NULL,
 	[dblTaxExemptSalesAmount]					  NUMERIC(18,6)     NULL,
