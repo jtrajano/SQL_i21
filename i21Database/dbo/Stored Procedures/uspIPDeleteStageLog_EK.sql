97942b7bbbf44e6bdf711e1be7fdf88e6f9283e8
Vinoth Kumar T <vinoth.t@irely.com>
2023-02-07 23:23:20 +0530

    IN-1105

---
+++ i21Database/dbo/Stored Procedures/uspIPDeleteStageLog_EK.sql
@@ -0,0 +1,70 @@
+﻿CREATE PROCEDURE dbo.uspIPDeleteStageLog_EK (@intNoOfDay INT = 30)
+AS
+BEGIN
+	DECLARE @dtmDate DATETIME
+
+	SELECT @dtmDate = CONVERT(VARCHAR(10), GETDATE() - @intNoOfDay, 126) + ' 00:00:00'
+
+	DELETE
+	FROM tblIPIDOCXMLError
+	WHERE dtmCreatedDate < @dtmDate
+
+	DELETE
+	FROM tblIPIDOCXMLArchive
+	WHERE dtmCreatedDate < @dtmDate
+
+	--DELETE
+	--FROM tblIPContractHeaderArchive
+	--WHERE dtmTransactionDate < @dtmDate
+
+	--DELETE
+	--FROM tblIPContractHeaderError
+	--WHERE dtmTransactionDate < @dtmDate
+
+	--DELETE
+	--FROM tblIPInvReceiptArchive
+	--WHERE dtmTransactionDate < @dtmDate
+
+	--DELETE
+	--FROM tblIPInvReceiptError
+	--WHERE dtmTransactionDate < @dtmDate
+
+	--DELETE
+	--FROM tblIPEntityArchive
+	--WHERE dtmTransactionDate < @dtmDate
+
+	--DELETE
+	--FROM tblIPEntityTermArchive
+	--WHERE intStageEntityId NOT IN (
+	--		SELECT intStageEntityId
+	--		FROM tblIPEntityArchive
+	--		)
+
+	--DELETE
+	--FROM tblIPEntityError
+	--WHERE dtmTransactionDate < @dtmDate
+
+	--DELETE
+	--FROM tblIPEntityTermError
+	--WHERE intStageEntityId NOT IN (
+	--		SELECT intStageEntityId
+	--		FROM tblIPEntityError
+	--		)
+
+	--DELETE
+	--FROM tblIPLotArchive
+	--WHERE dtmTransactionDate < @dtmDate
+
+	--DELETE
+	--FROM tblIPLotError
+	--WHERE dtmTransactionDate < @dtmDate
+
+	--DELETE
+	--FROM tblIPPBBSArchive
+	--WHERE dtmTransactionDate < @dtmDate
+
+	--DELETE
+	--FROM tblIPPBBSError
+	--WHERE dtmTransactionDate < @dtmDate
+
+END
