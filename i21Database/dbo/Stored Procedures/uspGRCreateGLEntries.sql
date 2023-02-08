1f5ebf894ef8229c97e2e20485a02af485f61a26
Leslie Arat <leslie.arat@irely.com>
2023-02-08 09:26:31 +0800

    GRN-3227 (Settle Storage - Unable to Post)

--- i21Database/dbo/Stored Procedures/uspGRCreateGLEntries.sql
+++ i21Database/dbo/Stored Procedures/uspGRCreateGLEntries.sql
@@ -658,7 +658,7 @@ BEGIN
 			,dblDebitUnit				= 0
 			,dblCreditUnit				= InventoryCostCharges.dblUnits
 			,strDescription				= ISNULL(GLAccount.strDescription, '') + ', Charges from ' + InventoryCostCharges.strCharge + ' for ' + InventoryCostCharges.strItem
-			,strCode					= @strCode
+			,strCode					= 'IC'
 			,strReference				= @EntityNo
 			,intCurrencyId				= InventoryCostCharges.intCurrencyId
 			,dblExchangeRate			= InventoryCostCharges.dblForexRate
@@ -724,7 +724,7 @@ BEGIN
 			,dblDebitUnit				= InventoryCostCharges.dblUnits
 			,dblCreditUnit				= 0
 			,strDescription				= ISNULL(GLAccount.strDescription, '') + ', Charges from ' + InventoryCostCharges.strCharge
-			,strCode					= @strCode
+			,strCode					= 'IC'
 			,strReference				= @EntityNo
 			,intCurrencyId				= InventoryCostCharges.intCurrencyId
 			,dblExchangeRate			= InventoryCostCharges.dblForexRate
@@ -793,7 +793,7 @@ BEGIN
 				,dblDebitUnit				= NonInventoryCostCharges.dblUnits
 				,dblCreditUnit				= 0
 				,strDescription				= ISNULL(GLAccount.strDescription, '') + ', Charges from ' + NonInventoryCostCharges.strCharge
-				,strCode					= @strCode
+				,strCode					= 'IC'
 				,strReference				= @EntityNo
 				,intCurrencyId				= NonInventoryCostCharges.intCurrencyId
 				,dblExchangeRate			= NonInventoryCostCharges.dblForexRate
@@ -851,7 +851,7 @@ BEGIN
 			,dblDebitUnit				= 0
 			,dblCreditUnit				= NonInventoryCostCharges.dblUnits
 			,strDescription				= ISNULL(GLAccount.strDescription, '') + ', Charges from ' + NonInventoryCostCharges.strCharge
-			,strCode					= @strCode
+			,strCode					= 'IC'
 			,strReference				= @EntityNo
 			,intCurrencyId				= NonInventoryCostCharges.intCurrencyId
 			,dblExchangeRate			= NonInventoryCostCharges.dblForexRate
--- i21Database/dbo/Stored Procedures/uspGRCreateItemGLEntries.sql
+++ i21Database/dbo/Stored Procedures/uspGRCreateItemGLEntries.sql
@@ -267,7 +267,12 @@ AS
 		,t.strTransactionId
 		,t.dblQty
 		,t.dblUOMQty
-		,dblCost = t.dblCost - (dbo.fnDivide(DiscountCost.dblTotalDiscountCost, t.dblQty)) 
+		,dblCost = SUM(t.dblCost - 
+						ISNULL(dbo.fnDivide(DiscountCost.dblTotalDiscountCost, isnull(@dblSelectedUnits, t.dblQty)),0)
+							+ ISNULL(CASE WHEN DiscountCost.ysnDiscountFromGrossWeight = 1 THEN dbo.fnDivide(DiscountCost.dblTotalDiscountCost, isnull(@dblGrossUnits, t.dblQty) ) ELSE 0 END ,0)
+							
+					  
+					  )
 		,dblItemCost = t.dblCost
 		,t.dblValue
 		,t.intTransactionTypeId
@@ -305,6 +310,7 @@ AS
 					, 2) 
 				) 
 			,0) AS dblTotalDiscountCost
+			,SV2.ysnDiscountFromGrossWeight
 		FROM tblGRSettleVoucherCreateReferenceTable SV2
 		INNER JOIN tblICItem I
 			ON I.intItemId = SV2.intItemId				
@@ -312,6 +318,7 @@ AS
 		WHERE SV2.strBatchId = t.strBatchId
 			AND isnull(SV2.ysnItemInventoryCost,I.ysnInventoryCost) = 1
 			AND ISNULL(SV2.intTransactionId ,0) = ISNULL(@intSettleStorageId,0)
+		GROUP BY SV2.ysnDiscountFromGrossWeight
 	) DiscountCost
 	LEFT JOIN tblICLot lot
 		ON lot.intLotId = t.intLotId
@@ -320,6 +327,25 @@ AS
 		AND ISNULL(i.intCategoryId, 0) = COALESCE(@intRebuildCategoryId, i.intCategoryId, 0) 
 		AND t.intInTransitSourceLocationId IS NULL -- If there is a value in intInTransitSourceLocationId, then it is for In-Transit costing. Use uspICCreateGLEntriesForInTransitCosting instead of this sp.
 		AND t.intTransactionId = ISNULL(@intSettleStorageId, t.intTransactionId)
+	GROUP BY t.dtmDate
+		,t.intItemId
+		,t.intItemLocationId
+		,t.intTransactionId
+		,t.strTransactionId
+		,t.dblQty
+		,t.dblUOMQty
+		,t.dblCost
+		,t.dblValue
+		,t.intTransactionTypeId
+		,t.intCurrencyId
+		,t.dblExchangeRate
+		,t.intInventoryTransactionId
+		,TransType.strName
+		,t.strTransactionForm 
+		,t.strDescription
+		,t.dblForexRate
+		,i.strItemNo
+		,currencyRateType.strCurrencyExchangeRateType
 )
 -------------------------------------------------------------------------------------------
 -- This part is for the usual G/L entries for Inventory Account and its contra account 
