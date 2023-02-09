ba12b14137b15c35290be37b2ca9de3a7c3c608e
Jase Rueda <jase.rueda@iRely.com>
2023-02-09 10:10:57 +0800

    CS-305

--- i21Database/dbo/Views/vyuSTSiteStatusSearch.sql
+++ i21Database/dbo/Views/vyuSTSiteStatusSearch.sql
@@ -5,7 +5,7 @@ SELECT		a.intStoreId,
 			a.strDescription,
 			ISNULL(b.ysnInternetConnectivity, 0) as ysnInternetConnectivity,
 			ISNULL(b.ysnRegisterConnectivity, 0) as ysnRegisterConnectivity,
-			ISNULL(b.dblUploadSpeed,0) as dblUploadSpeed,
+			ISNULL(c.dblUploadSpeed,0) as dblUploadSpeed,
 			FORMAT(dbo.fnSTGetCurrentBusinessDay(a.intStoreId), 'd','us')  as dtmCurrentBusinessDay,
 			a.ysnConsignmentStore,
 			reg.strStoreAppFileVersion as strStoreAppVersion
@@ -21,4 +21,11 @@ LEFT JOIN	(	SELECT		intStoreId,
 													GROUP BY	intStoreId)) b
 ON			a.intStoreId = b.intStoreId AND
 			DATEDIFF(MINUTE, b.dtmStatusDate, GETDATE()) <= 2
+LEFT JOIN	(	SELECT		intStoreId,
+							dblUploadSpeed
+				FROM		tblSTSiteStatus
+				WHERE		intSiteStatusId IN (	SELECT		MAX(intSiteStatusId) 
+													FROM		tblSTSiteStatus
+													GROUP BY	intStoreId)) c
+ON			a.intStoreId = c.intStoreId
 INNER JOIN	vyuSTRegister reg ON a.intStoreId = reg.intStoreId
