fba054c626e7b4381e7c7cbfe57f30c78c34e44b
Kelvin Quinto <kelvin.quinto@irely.com>
2023-02-10 14:07:39 +0800

    AR-16339
    (cherry picked from commit bc3c5ba5c0f3f7b1c0c4d5d21a1c2ff37bd963dc)
    (cherry picked from commit 3297f01dccba9edd555c88216b61796c1b3e4725)
    (cherry picked from commit 87cb9f07eac6a5b137aa0f0f16db77cab5f9e35c)

--- Integration/dbo/Stored Procedures/uspARImportCustomer.sql
+++ Integration/dbo/Stored Procedures/uspARImportCustomer.sql
@@ -1488,7 +1488,7 @@ CREATE PROCEDURE [dbo].[uspARImportCustomer]
 				, intCompanyLocationPricingLevelId		= C.intCompanyLocationPricingLevelId
 			FROM #CUSTOMERS C
 			INNER JOIN tblEMEntity E ON C.strCustomerNumber = E.strEntityNo
-			LEFT JOIN tblEMEntityType ET ON E.intEntityId  = ET.intEntityId
+			LEFT JOIN tblEMEntityType ET ON E.intEntityId  = ET.intEntityId AND ET.strType = ''Customer''
             WHERE ET.intEntityTypeId IS NULL
 
 			--UPDATE TEMP TABLE ENTITY ID
