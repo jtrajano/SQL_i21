c6444bdf6b6d608d5bd20d2401666192e232e09a
Kenneth Lew Lagunero <kenneth.lagunero@irely.com>
2023-02-16 22:14:13 +0800

    TM-3705

--- i21Database/dbo/Tables/tblTMSite.sql
+++ i21Database/dbo/Tables/tblTMSite.sql
@@ -84,6 +84,8 @@
 	[strFacilityNumber]         NVARCHAR (30)   COLLATE Latin1_General_CI_AS NULL,
     [guiApiUniqueId] UNIQUEIDENTIFIER NULL,
     [intRowNumber] INT NULL,
+    [ysnCompanySite]			  BIT             DEFAULT 0 NOT NULL,
+    [intCompanyLocationSubLocationId] INT NULL,
     CONSTRAINT [PK_tblTMSite] PRIMARY KEY CLUSTERED ([intSiteID] ASC),
     CONSTRAINT [FK_tblTMSite_tblTMClock] FOREIGN KEY ([intClockID]) REFERENCES [dbo].[tblTMClock] ([intClockID]),
     CONSTRAINT [FK_tblTMSite_tblTMCustomer] FOREIGN KEY ([intCustomerID]) REFERENCES [dbo].[tblTMCustomer] ([intCustomerID]),
