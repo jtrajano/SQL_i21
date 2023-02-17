8c18aa6d4822b1eb60fbbba5dbb27659e3e25cdb
ICER-PC\icere <ice.exiomo@irely.com>
2023-02-17 18:25:28 +0800

    Revert "[F] IC-11188 - HDTN-285320 - P01 - Customized Part Number"
    This reverts commit d7cbeee9eb4c020b6cb690226129be68ceb9b4d6.

--- i21Database/dbo/Views/vyuICSearchCompanyPreference.sql
+++ i21Database/dbo/Views/vyuICSearchCompanyPreference.sql
@@ -6,7 +6,6 @@ SELECT
 	-- Get the company preference from the other modules that is related with IC. 
 	,ysnImposeReversalTransaction = CAST(0 AS BIT) --rk.ysnImposeReversalTransaction
 	,i.strItemNo
-	,(SELECT ysnEnable FROM tblSMStartingNumber WHERE intStartingNumberId = '183') AS ysnEnable
 FROM 
 	tblICCompanyPreference ic
 	OUTER APPLY (
