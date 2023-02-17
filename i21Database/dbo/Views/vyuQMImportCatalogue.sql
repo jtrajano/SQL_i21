f625816a6385074511cde3da826aa23ab11b4ecd
Jonathan Valenzuela <jonathan.valenzuela@iRely.com>
2023-02-17 13:53:47 +0800

    QC-847

--- i21Database/dbo/Views/vyuQMImportCatalogue.sql
+++ i21Database/dbo/Views/vyuQMImportCatalogue.sql
@@ -1,7 +1,14 @@
 ﻿CREATE VIEW [dbo].[vyuQMImportCatalogue]
-AS 
+AS
+
+/* Created By: Jonathan Valenzuela
+ * Created Date: 02/17/2023
+ * Title: Catalogue Import Log
+ * JIRA: QC-847
+ * Description: Return list of Catalogue Import Log
+ */
 SELECT ImportCatalogue.*
      , QMSample.strSampleNumber
 FROM tblQMImportCatalogue AS ImportCatalogue
-JOIN tblQMSample AS QMSample ON ImportCatalogue.intSampleId = QMSample.intSampleId
-
+LEFT JOIN tblQMSample AS QMSample ON ImportCatalogue.intSampleId = QMSample.intSampleId
+GO
