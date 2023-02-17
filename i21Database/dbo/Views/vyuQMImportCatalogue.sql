7d56fd882f21ffffd827c1de11794d8f7cc65d04
Jonathan Valenzuela <jonathan.valenzuela@iRely.com>
2023-02-17 10:54:04 +0800

    QC-847

---
+++ i21Database/dbo/Views/vyuQMImportCatalogue.sql
@@ -0,0 +1,7 @@
+﻿CREATE VIEW [dbo].[vyuQMImportCatalogue]
+AS 
+SELECT ImportCatalogue.*
+     , QMSample.strSampleNumber
+FROM tblQMImportCatalogue AS ImportCatalogue
+JOIN tblQMSample AS QMSample ON ImportCatalogue.intSampleId = QMSample.intSampleId
+
