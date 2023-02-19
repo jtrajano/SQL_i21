e68c985e9f1f0e97e4b2066570686cd450a3f39b
Feb Montefrio <feb.montefrio@irely.com>
2023-02-19 04:19:38 +0000

    [B] IC-11211

--- i21Database/dbo/Stored Procedures/uspICPostCostAdjustmentRetroactiveAvg.sql
+++ i21Database/dbo/Stored Procedures/uspICPostCostAdjustmentRetroactiveAvg.sql
@@ -1256,7 +1256,7 @@ BEGIN
 			,@intSubLocationId = NULL
 			,@intStorageLocationId = NULL 
 			,@dtmDate = @dtmDate
-			,@dblQty  = @dblQty
+			,@dblQty  = 0
 			,@dblUOMQty = 0
 			,@dblCost = 0
 			,@dblValue = @dblAutoVariance
