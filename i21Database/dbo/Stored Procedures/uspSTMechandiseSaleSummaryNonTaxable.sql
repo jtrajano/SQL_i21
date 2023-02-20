a02076c6caad77d3d2723c4d68bf4d95dec69e9f
Dom Caoile <dom.caoile.02@gmail.com>
2023-02-20 09:48:21 +0800

    ST-2271

---
+++ i21Database/dbo/Stored Procedures/uspSTMechandiseSaleSummaryNonTaxable.sql
@@ -0,0 +1,29 @@
+﻿CREATE PROCEDURE  [dbo].[uspSTMechandiseSaleSummaryNonTaxable]                
+ @dtmFromDate AS DATETIME,      
+ @dtmToDate AS DATETIME      
+AS                
+              
+SET QUOTED_IDENTIFIER OFF                        
+SET ANSI_NULLS ON                        
+SET NOCOUNT ON                        
+SET XACT_ABORT ON                     
+      
+BEGIN          
+        
+SELECT T0.intStoreId, T2.intStoreNo, T2.strDescription AS    
+strStoreName, 0 AS intCategoryId, 'Taxable Sales' AS strDescription, ISNULL (SUM    
+(T1.dblTotalSalesAmountRaw), 0) AS dblTotalSalesAmountRaw, SUM    
+(T1.intItemsSold) AS intItemsSold, T4.ysnUseTaxFlag2 FROM    
+tblSTCheckoutHeader T0     
+INNER JOIN tblSTCheckoutDepartmetTotals T1 ON T0.intCheckoutId = T1.intCheckoutId     
+INNER JOIN tblSTStore T2 ON T0.intStoreId = T2.intStoreId     
+INNER JOIN tblICItem T3 ON T1.intItemId = T3.intItemId     
+INNER JOIN tblICCategoryLocation T4 ON T3.intCategoryId = T4.intCategoryId     
+INNER JOIN tblICCategory T5 ON T4.intCategoryId = T5.intCategoryId     
+WHERE ISNULL(T4.ysnUseTaxFlag2, 0) = 0 AND ISNULL (T0.intInvoiceId,0) <> 0      
+AND T0.dtmCheckoutDate BETWEEN @dtmFromDate AND @dtmToDate      
+GROUP BY T0.intStoreId, T2.intStoreNo,    
+T2.strDescription,T4.ysnUseTaxFlag2     
+ORDER BY ysnUseTaxFlag2, intStoreId    
+      
+END
