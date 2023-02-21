440757cacff52df90edbd516bc35f050f5367ab7
Leslie Arat <leslie.arat@irely.com>
2023-02-21 17:29:01 +0800

    GRN-3173 (Unable to delete Adjust Settlement Record)

--- i21Database/dbo/Stored Procedures/uspGRUnpostAdjustSettlements.sql
+++ i21Database/dbo/Stored Procedures/uspGRUnpostAdjustSettlements.sql
@@ -6,135 +6,89 @@ AS
 BEGIN
 DECLARE @ErrMsg AS NVARCHAR(MAX)
 DECLARE @intTypeId INT
-DECLARE @intBillId INT
 DECLARE @intUserId INT
 DECLARE @success BIT
 DECLARE @strBillId NVARCHAR(40)
 DECLARE @ysnBillPosted BIT
+DECLARE @BillIds AS Id
+DECLARE @intBillId INT
+DECLARE @intAdjustmentTypeId INT
+DECLARE @batchIdUsed NVARCHAR(50)
 
 BEGIN TRANSACTION
 BEGIN TRY
 
 SELECT @intTypeId = intTypeId
-	,@intBillId = strBillNumbers
+	,@strBillId = strBillNumbers
 	,@intUserId = intCreatedUserId
 	,@ysnBillPosted = ysnBillPosted
+	,@intAdjustmentTypeId = intAdjustmentTypeId
 FROM vyuGRSearchAdjustSettlements
 WHERE intAdjustSettlementId = @intAdjustSettlementId
+
+INSERT INTO @BillIds
+SELECT COALESCE(A.intBillId,B.intBillId)
+FROM tblGRAdjustSettlements A
+LEFT JOIN tblGRAdjustSettlementsSplit B
+	ON B.intAdjustSettlementId = A.intAdjustSettlementId
 
 IF @intTypeId = 1
 BEGIN
 	IF @ysnBillPosted = 1
 	BEGIN
-		EXEC [dbo].[uspAPPostBill] 
+		IF @intAdjustmentTypeId <> 1 --NOT ADVANCE
+		BEGIN
+			EXEC [dbo].[uspAPPostBill] 
+					@post = 0
+					,@recap = 0
+					,@isBatch = 0
+					,@param = @strBillId
+					,@userId = @intUserId
+					,@transactionType = NULL
+					,@success = @success OUTPUT
+		END
+		ELSE
+		BEGIN
+			EXEC [dbo].[uspAPPostVoucherPrepay]
 				@post = 0
 				,@recap = 0
-				,@isBatch = 0
-				,@param = @intBillId
+				,@param = @strBillId
+				,@batchId = DEFAULT
 				,@userId = @intUserId
-				,@transactionType = NULL
 				,@success = @success OUTPUT
+				,@batchIdUsed = @batchIdUsed OUTPUT
+		END
 	END
-
-	IF @intBillId IS NOT NULL
+	
+	IF EXISTS(SELECT 1 FROM @BillIds)
 	BEGIN
+		SELECT TOP 1 @intBillId = intId FROM @BillIds
+
 		EXEC uspAPDeleteVoucher 
 			@intBillId
 			,@intUserId
 			,@callerModule = 1
+
+		DELETE FROM @BillIds WHERE intId = @intBillId
 	END	
 END
 ELSE
 BEGIN
 	IF @ysnBillPosted = 1
 	BEGIN
-		SET @strBillId = CAST(@intBillId AS NVARCHAR(40))
+		SET @strBillId = CAST(@strBillId AS NVARCHAR(40))
 		EXEC dbo.uspARPostInvoice @param = @strBillId, @post = 0, @recap = 0, @userId = @intUserId, @raiseError = 1
 	END
 	
-	IF @intBillId IS NOT NULL
+	IF EXISTS(SELECT 1 FROM @BillIds)
 	BEGIN
+		SELECT TOP 1 @intBillId = intId FROM @BillIds
+
 		EXEC dbo.uspARDeleteInvoice @intBillId, @intUserId
+
+		DELETE FROM @BillIds WHERE intId = @intBillId
 	END
 END
-
-INSERT INTO tblGRDeletedAdjustSettlements
-(
-	[intAdjustSettlementId]
-	,[strAdjustSettlementNumber]
-	,[intTypeId]
-	,[intEntityId]
-	,[intCompanyLocationId]
-	,[intItemId]
-	,[intTicketId]
-	,[strTicketNumber]
-	,[intAdjustmentTypeId]
-	,[intSplitId]
-	,[dtmAdjustmentDate]
-	,[dblAdjustmentAmount]
-	,[dblWithholdAmount]
-	,[dblTotalAdjustment]
-	,[dblCkoffAdjustment]
-	,[strRailReferenceNumber]
-	,[strCustomerReference]
-	,[strComments]
-	,[intGLAccountId]
-	,[ysnTransferSettlement]
-	,[intTransferEntityId]
-	,[strTransferComments]
-	,[intBillId]
-	,[strBillId]
-	--FREIGHT
-	,[dblFreightUnits]
-	,[dblFreightRate]
-	,[dblFreightSettlement]
-	--CONTRACT
-	,[intContractLocationId]
-	,[intContractDetailId]
-	,[intContractHeaderId]
-	,[dtmDateCreated]
-	,[intConcurrencyId]
-	,[intParentAdjustSettlementId]
-	,[intCreatedUserId]
-)
-SELECT [intAdjustSettlementId]
-	,[strAdjustSettlementNumber]
-	,[intTypeId]
-	,[intEntityId]
-	,[intCompanyLocationId]
-	,[intItemId]
-	,[intTicketId]
-	,[strTicketNumber]
-	,[intAdjustmentTypeId]
-	,[intSplitId]
-	,[dtmAdjustmentDate]
-	,[dblAdjustmentAmount]
-	,[dblWithholdAmount]
-	,[dblTotalAdjustment]
-	,[dblCkoffAdjustment]
-	,[strRailReferenceNumber]
-	,[strCustomerReference]
-	,[strComments]
-	,[intGLAccountId]
-	,[ysnTransferSettlement]
-	,[intTransferEntityId]
-	,[strTransferComments]
-	,[strBillId]
-	,[strBillNumbers]
-	--FREIGHT
-	,[dblFreightUnits]
-	,[dblFreightRate]
-	,[dblFreightSettlement]
-	--CONTRACT
-	,[intContractLocationId]
-	,[intContractDetailId]
-	,[intContractHeaderId]
-	,[dtmDateCreated]
-	,[intConcurrencyId]
-	,[intParentAdjustSettlementId]
-	,[intCreatedUserId]
-FROM vyuGRSearchAdjustSettlements
-WHERE intAdjustSettlementId = @intAdjustSettlementId
 
 DELETE FROM tblGRAdjustSettlements WHERE intAdjustSettlementId = @intAdjustSettlementId
 
