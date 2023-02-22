1df93018676d008aeba34fb6844f08c38e925bdb
Jase Rueda <jase.rueda@iRely.com>
2023-02-22 10:05:44 +0800

    CS-329

--- i21Database/dbo/Tables/tblSTCheckoutFuelSalesByGradeAndPricePoint.sql
+++ i21Database/dbo/Tables/tblSTCheckoutFuelSalesByGradeAndPricePoint.sql
@@ -3,6 +3,7 @@ CREATE TABLE [dbo].[tblSTCheckoutFuelSalesByGradeAndPricePoint]
 	[intFuelSalesByGradeAndPricePointId] [int] IDENTITY(1,1) NOT NULL,
 	[intCheckoutId] [int] NOT NULL,
 	[intProductNumber] [int] NOT NULL,
+	[dblRegularPrice] [decimal](18,2) NULL,
 	[dblPrice] [decimal](18,2) NOT NULL,
 	[dblDollarsSold] [decimal](18, 2) NOT NULL,
 	[dblGallonsSold] [decimal](18, 3) NOT NULL,
