808911cabf51829082aa0e7363e8b97af6e745e2
Christian Joel Martinez <Christian.Martinez@irely.com>
2023-02-23 14:27:05 +0800

    QC-999

--- i21Database/dbo/Views/vyuQMApprovedQualityOrders.sql
+++ i21Database/dbo/Views/vyuQMApprovedQualityOrders.sql
@@ -69,8 +69,6 @@ SELECT
     ,MZ.strMarketZoneCode
 FROM tblMFBatch B
 INNER JOIN tblQMSample S ON S.intSampleId = B.intSampleId -- Auction or Non-Auction Sample
-LEFT JOIN tblLGLoadDetail LD ON LD.intBatchId = B.intBatchId
-LEFT JOIN tblLGLoad L ON L.intLoadId = LD.intLoadId
 LEFT JOIN tblQMCatalogueType CT ON CT.intCatalogueTypeId = S.intCatalogueTypeId
 LEFT JOIN tblARMarketZone MZ ON MZ.intMarketZoneId = B.intMarketZoneId
 LEFT JOIN vyuEMSearchEntityBroker EB ON EB.intEntityId = B.intBrokerId
@@ -109,8 +107,10 @@ OUTER APPLY (
 ) WQTY
 OUTER Apply (Select Top 1 V1.intEntityId,V1.strEntityName,V1.intDefaultLocationId,V1.strDefaultLocation  from vyuQMGetSupplier V1 Where V1.intEntityId = CH.intEntityId) V
 OUTER Apply (Select Top 1 SV1.intEntityId,SV1.strEntityName,SV1.intDefaultLocationId,SV1.strDefaultLocation  from vyuQMGetSupplier SV1 Where SV1.intEntityId = S.intEntityId) SV
+LEFT JOIN (tblLGLoadDetail LD INNER JOIN tblLGLoad L ON L.intLoadId = LD.intLoadId AND ISNULL(L.ysnCancelled, 0) = 0)
+    ON LD.intBatchId = B.intBatchId
 
-WHERE LD.intLoadDetailId IS NULL OR L.ysnCancelled = 1
+WHERE LD.intLoadDetailId IS NULL
 
 GO
 
