1ae846aa69ba7c3c3aeca54b4b3fea95754499af
Bryan-PC <013.bryan@gmail.com>
2023-02-23 10:04:59 +0800

    Revert "CT-8447"
    This reverts commit 5d1fcdaa8462d63ea569ce377e50699692433929.

--- i21Database/dbo/Stored Procedures/uspCTGetDefaultData.sql
+++ i21Database/dbo/Stored Procedures/uspCTGetDefaultData.sql
@@ -252,20 +252,12 @@ BEGIN
 		SELECT @intCurrencyId = ISNULL(intMainCurrencyId,intCurrencyID) FROM tblSMCurrency WHERE intCurrencyID = @intCurrencyId
 		IF @intCurrencyId <> @intInvoiceCurrencyId
 		BEGIN
-			SELECT	ER.intCurrencyExchangeRateId ,
-					'From ' + FC.strCurrency +' To ' + TC.strCurrency strExchangeRate, 
-					ERD.dblRate, 
-					ERD.intRateTypeId,
-					ERD.strCurrencyExchangeRateType
+			SELECT	intCurrencyExchangeRateId ,
+					'From ' + FC.strCurrency +' To ' + TC.strCurrency strExchangeRate,
+					(SELECT TOP 1 dblRate FROM tblSMCurrencyExchangeRateDetail WHERE intCurrencyExchangeRateId = ER.intCurrencyExchangeRateId AND intRateTypeId = @intRateTypeId ORDER BY dtmValidFromDate DESC) dblRate
 			FROM	tblSMCurrencyExchangeRate ER
 			JOIN	tblSMCurrency FC ON FC.intCurrencyID = ER.intFromCurrencyId
 			JOIN	tblSMCurrency TC ON TC.intCurrencyID = ER.intToCurrencyId
-			CROSS APPLY (
-				SELECT TOP 1 ERT.strCurrencyExchangeRateType, CERD.* 
-				FROM tblSMCurrencyExchangeRateDetail  CERD
-				JOIN tblSMCurrencyExchangeRateType ERT on CERD.intRateTypeId = ERT.intCurrencyExchangeRateTypeId
-				WHERE CERD.intCurrencyExchangeRateId =   ER.intCurrencyExchangeRateId ORDER BY dtmValidFromDate DESC
-			)  ERD 
 			WHERE	intToCurrencyId = @intCurrencyId  AND intFromCurrencyId = @intInvoiceCurrencyId
 		END
 	END
