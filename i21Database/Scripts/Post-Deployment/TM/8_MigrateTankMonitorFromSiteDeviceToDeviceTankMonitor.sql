74bb215b5d43c9e2b8db4458b3e8aa19efba1b93
DESKTOP-VFHGDG0\iRelyNUC015 <denny.divinagracia@irely.com>
2023-02-24 12:01:35 +0800

    TM-3695

---
+++ i21Database/Scripts/Post-Deployment/TM/8_MigrateTankMonitorFromSiteDeviceToDeviceTankMonitor.sql
@@ -0,0 +1,19 @@
+﻿print N'BEGIN Migration of Tank Monitor from device table to Device Tank Monitor table'
+IF EXISTS(SELECT TOP 1 1 FROM sys.tables WHERE name = N'tblTMDeviceTankMonitor')
+BEGIN
+
+
+	EXEC('
+		insert into tblTMDeviceTankMonitor (intDeviceId)
+		(
+		select distinct intDeviceId 
+		from tblTMDevice 
+		where intDeviceTypeId = 4 
+		and intDeviceId not in (select intDeviceId from tblTMDeviceTankMonitor)
+		)
+
+		DELETE from tblTMSiteDevice where intDeviceId in (select intDeviceId from tblTMDeviceTankMonitor)
+	')
+
+END
+print N'END Migration of Tank Monitor from device table to Device Tank Monitor table'
