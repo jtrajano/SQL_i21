d0aed5957a24e446ccba9c6b522b12ba75cd35bc
Feb Montefrio <feb.montefrio@irely.com>
2023-02-25 05:22:59 +0000

    [B] IC-11278

--- i21Database/dbo/Stored Procedures/uspICRepostCosting.sql
+++ i21Database/dbo/Stored Procedures/uspICRepostCosting.sql
@@ -1317,7 +1317,7 @@ BEGIN
 			,@intSubLocationId = NULL
 			,@intStorageLocationId = NULL 
 			,@dtmDate = @dtmDate
-			,@dblQty  = @dblQty
+			,@dblQty  = 0
 			,@dblUOMQty = 0
 			,@dblCost = 0
 			,@dblValue = @dblAutoVariance
@@ -1542,7 +1542,7 @@ BEGIN
 			,@intSubLocationId = NULL
 			,@intStorageLocationId = NULL 
 			,@dtmDate = @dtmDate
-			,@dblQty  = @dblQty
+			,@dblQty  = 0
 			,@dblUOMQty = 0
 			,@dblCost = 0
 			,@dblValue = @dblAutoVariance
@@ -1771,7 +1771,7 @@ BEGIN
 			,@intSubLocationId = NULL
 			,@intStorageLocationId = NULL 
 			,@dtmDate = @dtmDate
-			,@dblQty  = @dblQty
+			,@dblQty  = 0
 			,@dblUOMQty = 0
 			,@dblCost = 0
 			,@dblValue = @dblAutoVariance
