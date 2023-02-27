02727c9e04d5d4aa5c36ef410be60cec2a68b18b
Jeff Trajano <jeffrey.trajano@irely.com>
2023-02-28 00:54:07 +0800

    FA-450

---
+++ i21Database/dbo/Tables/tblFAFixedAssetJournal.sql
@@ -0,0 +1,17 @@
+CREATE TABLE tblFAFixedAssetJournal
+(
+    intFixedAssetJournalId INT IDENTITY(1,1),
+    intJournalId INT NOT NULL,
+    intAssetId INT NOT NULL,
+    intConcurrencyId INT NOT NULL,
+    dtmDateEntered DATETIME NOT NULL,
+    intEntityId INT NOT NULL,
+    CONSTRAINT [PK_tblFAFixedAssetJournal] PRIMARY KEY CLUSTERED ([intFixedAssetJournalId] ASC),
+    CONSTRAINT [FK_tblFAFixedAssetJournal_tblEMEntity] FOREIGN KEY ([intEntityId]) REFERENCES [dbo].[tblEMEntity] ([intEntityId]),
+    CONSTRAINT [DF_tblFAFixedAssetJournal_dtmDateEntered]  DEFAULT (getdate()) FOR [dtmDateEntered]
+)
+GO
+
+ALTER TABLE tblFAFixedAssetJournal
+add  CONSTRAINT [DF_tblFAFixedAssetJournal_dtmDateEntered]  DEFAULT (getdate()) FOR [dtmDateEntered]
+GO
