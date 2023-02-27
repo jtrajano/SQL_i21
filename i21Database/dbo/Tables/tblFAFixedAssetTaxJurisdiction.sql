e8dc749af9c2e05c9c8ac98c742783f67096c8b6
Dom Caoile <dom.caoile.02@gmail.com>
2023-02-27 14:48:05 +0800

    FA-448

---
+++ i21Database/dbo/Tables/tblFAFixedAssetTaxJurisdiction.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[tblFAFixedAssetTaxJurisdiction]
+(
+	[intAssetTaxJurisdictionId]		INT IDENTITY (1, 1) NOT NULL,
+	[strTaxJurisdiction]			NVARCHAR (255) COLLATE Latin1_General_CI_AS NOT NULL,
+    [intConcurrencyId]				INT DEFAULT 1 NOT NULL,
+    CONSTRAINT [PK_tblFAFixedAssetTaxJurisdiction] PRIMARY KEY CLUSTERED ([intAssetTaxJurisdictionId] ASC)
+)
