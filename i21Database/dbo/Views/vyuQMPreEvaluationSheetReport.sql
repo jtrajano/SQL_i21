6a1bf6d0dea0321be1a9a154958a7376eaa2af1f
Kelvin Quinto <kelvin.quinto@irely.com>
2023-02-28 15:34:30 +0800

    QC-929

--- i21Database/dbo/Views/vyuQMPreEvaluationSheetReport.sql
+++ i21Database/dbo/Views/vyuQMPreEvaluationSheetReport.sql
@@ -3,9 +3,9 @@ AS
 SELECT intBatchId			= B.intBatchId 
 	 , strLocationCode1		= B.strPlant
 	 , strLocationCode2		= IL.strName
-	 , strL1PONo			= B.strERPPOLineNo
+	 , strL1PONo			= B.strERPPONumber
 	 , strSupplierCode		= E.strEntityNo
-	 , strShipmentNo		= B.strERPPONumber
+	 , strShipmentNo		= CAST('SHIP#123' AS NVARCHAR(100))
 	 , dtmDeliveryDate		= CAST(GETDATE() AS DATE)
 	 , dtmLandedDate		= CAST(GETDATE() AS DATE)	 
 	 , strContainerNumber	= B.strContainerNumber
@@ -13,28 +13,50 @@ SELECT intBatchId			= B.intBatchId
 	 , strTeaLingo			= I.strItemNo
 	 , strTeaLingoDesc		= I.strDescription
 	 , strBatchId			= B.strBatchId
-	 , strTasteRange		= CAST('0-100' AS NVARCHAR(100))
+	 , strTasteRange		= CAST(CAST(ISNULL(TASTE.dblMinValue, 0) AS DECIMAL(18,2)) AS NVARCHAR(100)) + ' - ' + CAST(CAST(ISNULL(TASTE.dblMaxValue, 0) AS DECIMAL(18,2)) AS NVARCHAR(100))
 	 , strLeafDescription	= B.strLeafStyle
 	 , strLeafSize			= B.strLeafSize
 	 , strGardenMark		= GM.strGardenMark
-	 , strChopNo			= CAST('CHOP#123' AS NVARCHAR(100))
-	 , dblPrice				= B.dblBoughtPrice
-	 , dblTasteScore		= CAST(1 AS NUMERIC(18, 6))
-	 , dblHueScore			= CAST(1 AS NUMERIC(18, 6))
-	 , dblIntensityScore	= CAST(1 AS NUMERIC(18, 6))
-	 , dblMouthFeelScore	= CAST(1 AS NUMERIC(18, 6))
+	 , strChopNo			= B.strTeaGardenChopInvoiceNumber
+	 , dblPrice				= ISNULL(B.dblLandedPrice, 0)
+	 , dblTasteScore		= ISNULL(B.dblTeaTaste, 0)
+	 , dblHueScore			= ISNULL(B.dblTeaHue, 0)
+	 , dblIntensityScore	= ISNULL(B.dblTeaIntensity, 0)
+	 , dblMouthFeelScore	= ISNULL(B.dblTeaMouthFeel, 0)
 	 , strComment			= B.strTasterComments
 	 , strCompanyName		= COMP.strCompanyName
 	 , strCompanyAddress	= COMP.strAddress
 	 , strCityStateZip		= COMP.strCity + ', ' + COMP.strState + ', ' + COMP.strZip
-	 , strCompanyCountry	= COMP.strCountry
+	 , strCompanyCountry	= COMP.strCountry	 
 FROM tblMFBatch B
 LEFT JOIN tblICItem I ON B.intTealingoItemId = I.intItemId
 LEFT JOIN tblQMGardenMark GM ON B.intGardenMarkId = GM.intGardenMarkId
 LEFT JOIN tblICStorageLocation IL ON B.intStorageLocationId = IL.intStorageLocationId
-LEFT JOIN tblEMEntity E ON B.intBrokerId = E.intEntityId
+LEFT JOIN tblLGLoadDetail LD ON B.intBatchId = LD.intBatchId
+LEFT JOIN tblEMEntity E ON E.intEntityId = LD.intVendorEntityId
+OUTER APPLY (
+	SELECT dblMinValue	= MIN(PPV.dblMinValue)
+		 , dblMaxValue 	= MAX(PPV.dblMaxValue)
+	FROM tblQMProduct PRD
+	INNER JOIN tblQMProductControlPoint PC ON PC.intProductId = PRD.intProductId
+	INNER JOIN tblQMProductProperty PP ON PP.intProductId = PRD.intProductId
+	INNER JOIN tblQMProductTest PT ON PT.intProductId = PP.intProductId AND PT.intProductId = PRD.intProductId
+	INNER JOIN tblQMTest T ON T.intTestId = PP.intTestId AND T.intTestId = PT.intTestId
+	INNER JOIN tblQMTestProperty TP ON TP.intPropertyId = PP.intPropertyId
+					   			   AND TP.intTestId = PP.intTestId
+								   AND TP.intTestId = T.intTestId
+								   AND TP.intTestId = PT.intTestId
+	INNER JOIN tblQMProperty AS PRT ON PRT.intPropertyId = PP.intPropertyId AND PRT.intPropertyId = TP.intPropertyId
+	INNER JOIN tblQMProductPropertyValidityPeriod AS PPV ON PPV.intProductPropertyId = PP.intProductPropertyId
+	WHERE PRD.intProductTypeId = 2
+	  AND PRD.intProductValueId = B.intTealingoItemId
+	  AND PRD.ysnActive = 1
+	  AND PRT.strPropertyName ='Taste'
+) TASTE
 OUTER APPLY (
 	SELECT TOP 1 *
 	FROM tblSMCompanySetup
 	ORDER BY intCompanySetupID ASC
 ) COMP
+WHERE B.strContainerNumber != ''
+  AND B.strContainerNumber IS NOT NULL
