10bf520c56323b852434851436745cf1b7552a5d
chenarrd delfin <chen.delfin@irely.com>
2023-02-28 16:10:55 +0800

    LG-3942- HDTN-287022 - Batch should be available in 'Add Orders' of LS creation screen only after Catalogue Approval

--- i21Database/dbo/Views/vyuQMApprovedQualityOrders.sql
+++ i21Database/dbo/Views/vyuQMApprovedQualityOrders.sql
@@ -107,7 +107,16 @@ OUTER APPLY (
 OUTER APPLY (
     SELECT [dblWeight] = dbo.fnCalculateQtyBetweenUOM(QIUOM.intItemUOMId, WIUOM.intItemUOMId, B.dblTotalQuantity)
 ) WQTY
-WHERE LD.intLoadDetailId IS NULL
+OUTER Apply (Select Top 1 V1.intEntityId,V1.strEntityName,V1.intDefaultLocationId,V1.strDefaultLocation  from vyuQMGetSupplier V1 Where V1.intEntityId = CH.intEntityId) V
+OUTER Apply (Select Top 1 SV1.intEntityId,SV1.strEntityName,SV1.intDefaultLocationId,SV1.strDefaultLocation  from vyuQMGetSupplier SV1 Where SV1.intEntityId = S.intEntityId) SV
+LEFT JOIN (tblLGLoadDetail LD INNER JOIN tblLGLoad L ON L.intLoadId = LD.intLoadId AND ISNULL(L.ysnCancelled, 0) = 0)
+    ON LD.intBatchId = B.intBatchId
+inner join 
+( select  intSampleId from  tblQMCatalogueReconciliationDetail CRD INNER JOIN
+tblQMCatalogueReconciliation CR ON CR.intCatalogueReconciliationId = CRD.intCatalogueReconciliationId inner join
+tblSMTransaction SMT ON SMT.intRecordId = CR.intCatalogueReconciliationId AND SMT.strTransactionNo = CR.strReconciliationNumber
+where SMT.strApprovalStatus IN ('Approved') ) Qlty on  Qlty.intSampleId = S.intSampleId
+--WHERE LD.intLoadDetailId IS NULL
 
 GO
 
