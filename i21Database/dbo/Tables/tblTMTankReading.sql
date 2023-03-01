d8d75139336851dc41d498fe5ac20f0d84fa1800
DESKTOP-VFHGDG0\iRelyNUC015 <denny.divinagracia@irely.com>
2023-03-01 18:02:56 +0800

    TM-3695

--- i21Database/dbo/Tables/tblTMTankReading.sql
+++ i21Database/dbo/Tables/tblTMTankReading.sql
@@ -3,7 +3,7 @@ CREATE TABLE [dbo].[tblTMTankReading] (
     [intTankReadingId]          INT             IDENTITY (1, 1) NOT NULL,
 	[dtmDateTime]				DATETIME 		NULL,
 	[intReadingSource]			INT NULL,
-	[intDeviceTankMonitorId]	INT NOT NULL,
+	[intDeviceTankMonitorId]	INT NULL,
 	[intTankNumber]				INT				NULL,
 	[strTankStatus]				NVARCHAR (100)   COLLATE Latin1_General_CI_AS DEFAULT ('') NULL,
 	[intFuelGrade]				INT				NULL,
@@ -23,7 +23,8 @@ CREATE TABLE [dbo].[tblTMTankReading] (
     [ysnManual]  bit NULL,
 	[intCheckoutId] int NULL
 	CONSTRAINT [PK_tblTMTankReading_intTankReadingId] PRIMARY KEY CLUSTERED ([intTankReadingId] ASC),
-	CONSTRAINT [FK_tblTMTankReading_tblTMReadingSourceType] FOREIGN KEY ([intReadingSource]) REFERENCES [dbo].[tblTMReadingSourceType] ([intReadingSourceTypeId])
+	CONSTRAINT [FK_tblTMTankReading_tblTMReadingSourceType] FOREIGN KEY ([intReadingSource]) REFERENCES [dbo].[tblTMReadingSourceType] ([intReadingSourceTypeId]),
+	CONSTRAINT [FK_tblTMTankReading_tblTMSite] FOREIGN KEY ([intSiteId]) REFERENCES [dbo].[tblTMSite] ([intSiteID])
 	)
 
 GO
