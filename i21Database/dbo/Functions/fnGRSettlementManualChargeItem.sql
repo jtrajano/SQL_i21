71107f97a829ba2caf5c87db543cffdfa88a2ba4
Leslie Arat <leslie.arat@irely.com>
2023-03-02 01:50:42 +0800

    GRN-3244 (HDTN-288923 - Settlement Report duplicating manual voucher entries on print)

--- i21Database/dbo/Functions/fnGRSettlementManualChargeItem.sql
+++ i21Database/dbo/Functions/fnGRSettlementManualChargeItem.sql
@@ -22,26 +22,24 @@ SELECT intBillDetailItemId = BD.intBillDetailId
 		ELSE 4
 	END
 	,ysnShow = CASE 
-		WHEN BD.intInventoryReceiptItemId IS NOT NULL AND BD.intContractDetailId IS NULL THEN 0 --SPOT
-		--WHEN BD.intInventoryReceiptItemId IS NOT NULL AND BD.intContractDetailId IS NOT NULL THEN 1 --CONTRACT
 		WHEN BD.intInventoryReceiptItemId IS NOT NULL AND BD.intContractDetailId IS NOT NULL THEN CASE WHEN BD2.ysnStage = 0 AND BD2.intSettleStorageId IS NULL THEN 0 ELSE 1 END --CONTRACT
-		WHEN BD.intCustomerStorageId IS NOT NULL THEN 1 --SETTLE STORAGE
-		ELSE 1
+		ELSE 0
 	END
 	,BD2.ysnStage
 FROM tblAPBillDetail BD
 INNER JOIN tblICItem IC
 	ON IC.intItemId = BD.intItemId
-		AND IC.strType <> 'Other Charge'
+		AND IC.strType = 'Inventory'
 LEFT JOIN (
 			tblAPBillDetail BD2
 			LEFT JOIN tblICItem IC2
 				ON IC2.intItemId = BD2.intItemId
-					AND IC2.strType <> 'Inventory'
+					AND IC2.strType = 'Other Charge'
 	)	
 	ON BD2.intBillId = BD.intBillId
 		AND isnull(BD2.intInventoryReceiptItemId, isnull(BD.intInventoryReceiptItemId, 0)) = isnull(BD.intInventoryReceiptItemId, 0)
 		AND (ISNULL(BD2.intLinkingId, 0) = ISNULL(BD.intLinkingId, 0) OR ISNULL(BD2.intLinkingId,-90) = -90)
+		AND ISNULL(BD2.intItemId,0) <> BD.intItemId
 WHERE BD.intBillId = @intBillId
 ) A
 
@@ -55,14 +53,14 @@ SET ysnShow = CASE
 --SELECT A.*,B.*,C.*
 FROM @tbl A
 INNER JOIN (
-	SELECT * FROM @tbl
+	SELECT DISTINCT * FROM @tbl
 ) B ON B.intBillDetailOtherChargeItemId = A.intBillDetailOtherChargeItemId
 LEFT JOIN (
 	SELECT intBillDetailOtherChargeItemId
 		,intBillDetailItemId
 		,rowNum = ROW_NUMBER() OVER (PARTITION BY intBillDetailOtherChargeItemId ORDER BY intBillDetailItemId ASC)
 	FROM @tbl
-	WHERE intTypeId = 2
+	WHERE intTypeId IN (2,3)
 		AND ysnStage = 0
 		AND NOT EXISTS(SELECT 1 FROM @tbl WHERE intTypeId = 1)
 ) C ON C.intBillDetailOtherChargeItemId = A.intBillDetailOtherChargeItemId
