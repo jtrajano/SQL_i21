fe43dc95ed6fdec182cf95d7b0c6c22c83bfb8e2
Jeff Trajano <jeffrey.trajano@irely.com>
2023-03-01 14:43:17 +0800

    GL-10050 Refresh Summary

--- i21Database/dbo/Stored Procedures/uspGLRefreshAuditorReportByAccountId.sql
+++ i21Database/dbo/Stored Procedures/uspGLRefreshAuditorReportByAccountId.sql
@@ -1,12 +1,13 @@
 CREATE PROCEDURE uspGLRefreshAuditorReportByAccountId
 AS
-
+DECLARE @intDefaultCurrencyId INT
+SELECT TOP 1 @intDefaultCurrencyId = intDefaultCurrencyId FROM tblSMCompanyPreference 
 TRUNCATE TABLE tblGLAuditorReportByAccountId;
 
 With CTE AS(
-select * from fnGLGetAccountCurrencyReportExpRev(6)
+select * from fnGLGetAccountCurrencyReportExpRev(@intDefaultCurrencyId)
 union all
-select * from fnGLGetAccountCurrencyReport(6)
+select * from fnGLGetAccountCurrencyReport(@intDefaultCurrencyId)
 )
 INSERT INTO tblGLAuditorReportByAccountId
 SELECT * FROM CTE
