c73569dab393d2cef54831b001fe8ca8b3bf2e8c
Peter John Casasola <peter.casasola@iRely.com>
2023-03-04 11:03:42 +0800

    AP-11499 Fixed column name not found
    (cherry picked from commit b47704941b080415cfd4a2a887c00f9111665c8a)

--- i21Database/dbo/Stored Procedures/uspAPUpdateVoucherDetailTax.sql
+++ i21Database/dbo/Stored Procedures/uspAPUpdateVoucherDetailTax.sql
@@ -70,7 +70,7 @@ IF @transCount = 0 BEGIN TRANSACTION
 										ELSE B.dblCost
 									END / ISNULL(B.dblCostUnitQty, 1)
 		,dblQuantity				= CASE 
-										WHEN B.intComputeTotalOption = 0 AND B.intWeightUOMId IS NOT NULL AND A.intWeightClaimDetailId IS NULL
+										WHEN B.intComputeTotalOption = 0 AND B.intWeightUOMId IS NOT NULL AND B.intWeightClaimDetailId IS NULL
 											THEN B.dblNetWeight * B.dblWeightUnitQty
 										ELSE B.dblQtyReceived * B.dblUnitQty 
 									END
