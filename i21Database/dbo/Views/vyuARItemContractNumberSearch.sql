068d612720f35b3e2a5518c163016f9cdab06c4d
Kelvin Quinto <kelvin.quinto@irely.com>
2023-03-06 20:46:40 +0800

    AR-15395 - Fix Item Contract Number search

--- i21Database/dbo/Views/vyuARItemContractNumberSearch.sql
+++ i21Database/dbo/Views/vyuARItemContractNumberSearch.sql
@@ -14,44 +14,47 @@ SELECT DISTINCT
 	 , intCategoryId			= intCategoryId
 	 , strCategory				= strCategory
 	 , strPrepayment			= CASE WHEN ISNULL(strPrepayment, '') = '' THEN 'Not Selectable without Prepayment' ELSE strPrepayment END
+	 , strContractType			= TP.strContractType
+	 , strContractStatus		= CTICD.strContractStatus
+	 , dblAvailableQty			= CTICD.dblAvailable
+	 , intCurrencyId			= CTICH.intCurrencyId
+	 , strCurrency				= CUR.strCurrency
 FROM dbo.tblCTItemContractHeader CTICH
 INNER JOIN (
-	SELECT 
-		  CTICD.intItemContractHeaderId
-		, intItemId		= CTICD.intItemId
-		, intCategoryId = ISNULL(ICC.intCategoryId, 0)
-		, strCategory	= ICC.strCategoryCode
-		, strPrepayment	= ARI.strInvoiceNumber
+	SELECT intItemContractHeaderId	= CTICD.intItemContractHeaderId
+		 , intItemId				= CTICD.intItemId
+		 , intCategoryId			= ISNULL(ICC.intCategoryId, 0)
+		 , strCategory				= ICC.strCategoryCode
+		 , strPrepayment			= ARI.strInvoiceNumber
+		 , strContractStatus		= CS.strContractStatus
+		 , dblAvailable				= CTICD.dblAvailable
 	FROM tblCTItemContractDetail CTICD
-	INNER JOIN tblICItem ICI
-	ON CTICD.intItemId = ICI.intItemId
-	INNER JOIN tblICCategory ICC
-	ON ICI.intCategoryId = ICC.intCategoryId
-	LEFT JOIN tblARInvoiceDetail ARID
-	ON CTICD.intItemContractHeaderId = ARID.intItemContractHeaderId
-	AND ARID.ysnRestricted = 1
-	AND ISNULL(ARID.intItemId, 0) <> 0
-	LEFT JOIN tblARInvoice ARI
-	ON ARID.intInvoiceId = ARI.intInvoiceId
+	INNER JOIN tblICItem ICI ON CTICD.intItemId = ICI.intItemId
+	INNER JOIN tblICCategory ICC ON ICI.intCategoryId = ICC.intCategoryId
+	LEFT JOIN tblARInvoiceDetail ARID ON CTICD.intItemContractHeaderId = ARID.intItemContractHeaderId 
+								     AND ARID.ysnRestricted = 1
+									 AND ARID.intItemId IS NOT NULL
+	LEFT JOIN tblARInvoice ARI ON ARID.intInvoiceId = ARI.intInvoiceId
+	LEFT JOIN tblCTContractStatus CS ON CTICD.intContractStatusId = CS.intContractStatusId
 	WHERE CTICD.intContractStatusId IN (1, 4, 5)
-	GROUP BY CTICD.intItemContractHeaderId, CTICD.intItemId, ICC.intCategoryId, ICC.strCategoryCode, ARI.strInvoiceNumber
+	GROUP BY CTICD.intItemContractHeaderId, CTICD.intItemId, ICC.intCategoryId, ICC.strCategoryCode, ARI.strInvoiceNumber, CS.strContractStatus, CTICD.dblAvailable
 
 	UNION ALL
 
-	SELECT 
-		  CTICHC.intItemContractHeaderId
-		, intItemId		= 0
-		, intCategoryId = CTICHC.intCategoryId
-		, strCategory	= ICC.strCategoryCode
-		, strPrepayment	= ARI.strInvoiceNumber
+	SELECT intItemContractHeaderId	= CTICHC.intItemContractHeaderId
+		, intItemId					= 0
+		, intCategoryId				= CTICHC.intCategoryId
+		, strCategory				= ICC.strCategoryCode
+		, strPrepayment				= ARI.strInvoiceNumber
+		, strContractStatus			= CAST('Open' AS NVARCHAR(10))
+		, dblAvailable				= CAST(1 AS NUMERIC(18, 6))
 	FROM tblCTItemContractHeaderCategory CTICHC
-	INNER JOIN tblICCategory ICC
-	ON CTICHC.intCategoryId = ICC.intCategoryId
-	LEFT JOIN tblARInvoiceDetail ARID
-	ON CTICHC.intItemContractHeaderId = ARID.intItemContractHeaderId
-	AND ARID.ysnRestricted = 1
-	AND ISNULL(ARID.intItemId, 0) = 0
-	LEFT JOIN tblARInvoice ARI
-	ON ARID.intInvoiceId = ARI.intInvoiceId
+	INNER JOIN tblICCategory ICC ON CTICHC.intCategoryId = ICC.intCategoryId
+	LEFT JOIN tblARInvoiceDetail ARID ON CTICHC.intItemContractHeaderId = ARID.intItemContractHeaderId
+									 AND ARID.ysnRestricted = 1
+									 AND ARID.intItemId IS NOT NULL
+	LEFT JOIN tblARInvoice ARI ON ARID.intInvoiceId = ARI.intInvoiceId
 	GROUP BY CTICHC.intItemContractHeaderId, CTICHC.intCategoryId, ICC.strCategoryCode, ARI.strInvoiceNumber
 ) CTICD ON CTICH.intItemContractHeaderId = CTICD.intItemContractHeaderId
+LEFT JOIN tblCTContractType	TP ON TP.intContractTypeId = CTICH.intContractTypeId
+LEFT JOIN tblSMCurrency CUR ON CTICH.intCurrencyId = CUR.intCurrencyID
