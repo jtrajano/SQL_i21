c4adcd783cbd4f3c6134a0b082b67f5644bcedb4
Jonathan Valenzuela <jonathan.valenzuela@iRely.com>
2023-03-07 13:11:00 +0800

    MFG-4861

---
+++ i21Database/dbo/Views/vyuMFUserLocation.sql
@@ -0,0 +1,10 @@
+﻿CREATE VIEW [dbo].[vyuMFUserLocation]
+AS 
+SELECT intEntityUserSecurityId
+	 , intEntityId
+	 , intUserRoleId
+	 , CompanyLocation.intCompanyLocationId
+	 , CompanyLocation.strLocationName AS strCompanyLocationName
+FROM tblSMUserSecurityCompanyLocationRolePermission AS UserLocationRole
+JOIN tblSMCompanyLocation AS CompanyLocation ON UserLocationRole.intCompanyLocationId = CompanyLocation.intCompanyLocationId
+GO
