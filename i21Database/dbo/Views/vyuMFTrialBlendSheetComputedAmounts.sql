44cb212d087d9999189daa8fb31beb0dc4daabff
Jonathan Valenzuela <jonathan.valenzuela@iRely.com>
2023-03-08 15:12:41 +0800

    MFG-4880

--- i21Database/dbo/Views/vyuMFTrialBlendSheetComputedAmounts.sql
+++ i21Database/dbo/Views/vyuMFTrialBlendSheetComputedAmounts.sql
@@ -1,17 +1,17 @@
 ﻿CREATE VIEW [dbo].[vyuMFTrialBlendSheetComputedAmounts]
 AS
-SELECT * FROM
-(SELECT 
- SUM(dblWeight)[dblTotalWeight]
-,SUM(dblBags)[dblTotalBags]
-,SUM(dblWeight*dblSellingPrice )/SUM(dblWeight) dblWAvgSellingPrice
-,SUM(dblWeight*dblLandedPrice )/SUM(dblWeight) dblWAvgLandedPrice
-,SUM(dblWeight*dblT )/SUM(dblWeight) dblWAvgT
-,SUM(dblWeight*dblH )/SUM(dblWeight) dblWAvgH
-,SUM(dblWeight*dblI )/SUM(dblWeight) dblWAvgI
-,SUM(dblWeight*dblM )/SUM(dblWeight) dblWAvgM
-,SUM(dblWeight*dblA )/SUM(dblWeight) dblWAvgA
-,SUM(dblWeight*dblV )/SUM(dblWeight) dblWAvgV
-,intWorkOrderId
-from vyuMFTrialBlendSheetDetail
-GROUP BY intWorkOrderId)x
+SELECT * 
+FROM
+(SELECT SUM(dblWeight)															AS dblTotalWeight
+	  , SUM(dblBags)															AS dblTotalBags
+	  , ISNULL(SUM(dblWeight * dblSellingPrice) / NULLIF(SUM(dblWeight), 0), 0)	AS dblWAvgSellingPrice
+	  , ISNULL(SUM(dblWeight * dblLandedPrice) / NULLIF(SUM(dblWeight), 0), 0)	AS dblWAvgLandedPrice
+	  , ISNULL(SUM(dblWeight * dblT) / NULLIF(SUM(dblWeight), 0), 0)			AS dblWAvgT
+	  , ISNULL(SUM(dblWeight * dblH) / NULLIF(SUM(dblWeight), 0), 0)			AS dblWAvgH
+	  , ISNULL(SUM(dblWeight * dblI) / NULLIF(SUM(dblWeight), 0), 0)			AS dblWAvgI
+	  , ISNULL(SUM(dblWeight * dblM) / NULLIF(SUM(dblWeight), 0), 0)			AS dblWAvgM
+	  , ISNULL(SUM(dblWeight * dblA) / NULLIF(SUM(dblWeight), 0), 0)			AS dblWAvgA
+	  , ISNULL(SUM(dblWeight * dblV) / NULLIF(SUM(dblWeight), 0), 0)			AS dblWAvgV
+	  , intWorkOrderId
+FROM vyuMFTrialBlendSheetDetail
+GROUP BY intWorkOrderId) x
