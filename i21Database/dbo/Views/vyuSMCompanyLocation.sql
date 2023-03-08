e4867cbf1836d91c5df7e5b64ed3fff51b601f8a
IRELY\JMendones <jesmar.mendones@irely.com>
2023-03-08 11:33:22 +0800

    EM-3084
    (cherry picked from commit 944ff0c310faa7c688a6da0901b39465677aa647)

--- i21Database/dbo/Views/vyuSMCompanyLocation.sql
+++ i21Database/dbo/Views/vyuSMCompanyLocation.sql
@@ -35,6 +35,8 @@ SELECT compLoc.intCompanyLocationId
 ,compLoc.strOregonFacilityNumber 
 ,compLoc.strVendorRefNoPrefix
 ,compLoc.strVatNo
+,compLoc.strFLOID
+,compLoc.strBioINSPECTANo
 ,compLoc.strOutOfStockWarning
 ,compLoc.strLotOverdrawnWarning
 ,compLoc.strDefaultCarrier
