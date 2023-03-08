3f625f8dd3aae2a1984f5d6fd92776aea50ea982
radatienza <rad.atienza@iRely.com>
2023-03-08 09:50:24 +0800

    EM-3081

---
+++ i21Database/dbo/Tables/tblApiSchemaEntitySplit.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[tblApiSchemaEntitySplit] (
+    [guiApiUniqueId]				UNIQUEIDENTIFIER NOT NULL,
+    [intRowNumber]				    INT NULL,
+    [intKey]						INT IDENTITY(1, 1) NOT NULL PRIMARY KEY,
+
+    [strEntityNo]				    NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL,
+
+	[strSplitNumber]			    NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL,
+	[strExceptionCategories]	    NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NULL,
+	[strFarm]						NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NULL,
+	[strDescription]			    NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NULL,
+	[strEntityType]				    NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL,
+	[strSplitEntityNo]			    NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL,
+	[strPercent]				    NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL,
+	[strOption]						NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL,
+	[strStorageTypeCode]			NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL
+
+)
