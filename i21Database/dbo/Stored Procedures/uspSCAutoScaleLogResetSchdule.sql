9c700f3374e429dfb3ccd3d458cc851bfab52893
Mon Gonzales <mon.gonzales@irely.com>
2023-03-09 16:33:38 +0800

    SC-4898 - HDTN-287146 - Unable to un-distribute ticket

---
+++ i21Database/dbo/Stored Procedures/uspSCAutoScaleLogResetSchdule.sql
@@ -0,0 +1,45 @@
+﻿CREATE PROCEDURE [dbo].[uspSCAutoScaleLogResetSchdule]
+	@TICKET_ID INT,
+	@USER_ID INT
+AS
+BEGIN
+	DECLARE @CURRENT_ID INT
+	
+
+	
+	DECLARE @UNIT NUMERIC(18,6)
+	DECLARE @CONTRACT_ID INT
+	
+
+	SELECT @CURRENT_ID = MIN(intTicketAutoScaleLogId) 
+	FROM tblSCTicketAutoScaleLog
+		WHERE intTicketId = @TICKET_ID
+			AND ysnHeader = 0
+		
+	WHILE @CURRENT_ID IS NOT NULL
+	BEGIN
+		
+		SELECT @UNIT = dblUnit * -1
+			,@CONTRACT_ID = intContractDetailId
+		FROM tblSCTicketAutoScaleLog 
+		WHERE intTicketAutoScaleLogId = @CURRENT_ID
+			AND ysnHeader = 0
+
+		PRINT 'CREATING A SCHEDULE FOR ' + LTRIM(@CONTRACT_ID) + ' FOR TICKET' + LTRIM(@TICKET_ID) + ' BY ' + LTRIM(@UNIT)
+		EXEC	uspCTUpdateScheduleQuantity 
+			@intContractDetailId	=	@CONTRACT_ID,
+			@dblQuantityToUpdate	=	@UNIT ,
+			@intUserId				=	@USER_ID,
+			@intExternalId			=	@TICKET_ID,
+			@strScreenName			=	'Auto - Scale'
+
+
+		SELECT @CURRENT_ID = MIN(intTicketAutoScaleLogId) 
+		FROM tblSCTicketAutoScaleLog		
+			WHERE intTicketId = @TICKET_ID
+			AND intTicketAutoScaleLogId > @CURRENT_ID			
+			AND ysnHeader = 0
+	END
+	
+	DELETE FROM tblSCTicketAutoScaleLog WHERE intTicketId = @TICKET_ID
+END
