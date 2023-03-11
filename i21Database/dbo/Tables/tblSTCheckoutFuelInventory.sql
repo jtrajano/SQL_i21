ffe49766c85c1a5c59d95049a92d2c91e9120a87
Jase Rueda <jase.rueda@iRely.com>
2023-03-11 14:41:31 +0800

    CS-359

---
+++ i21Database/dbo/Tables/tblSTCheckoutFuelInventory.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[tblSTCheckoutFuelInventory]
+(
+	[intFuelInventoryId] INT NOT NULL IDENTITY, 
+	[intCheckoutId] INT NOT NULL,
+    [intDeviceId] INT NOT NULL,
+    [dblGallons] DECIMAL(18, 6) NOT NULL, 
+    [dtmFuelInventoryDate] DATETIME NOT NULL,
+    [ysnIsManualEntry] BIT NOT NULL,
+    [intConcurrencyId] INT NULL, 
+    CONSTRAINT [PK_tblSTCheckoutFuelInventory_intFuelInventoryId] PRIMARY KEY ([intFuelInventoryId]), 
+    CONSTRAINT [FK_tblSTCheckoutFuelInventory_tblSTCheckoutHeader] FOREIGN KEY ([intCheckoutId]) REFERENCES [tblSTCheckoutHeader]([intCheckoutId]) ON DELETE CASCADE, 
+    CONSTRAINT [FK_tblSTCheckoutFuelInventory_tblTMDevice] FOREIGN KEY ([intDeviceId]) REFERENCES [dbo].[tblTMDevice] ([intDeviceId]),
+)
---
+++ i21Database/dbo/Views/vyuSTGetFuelInventory.sql
@@ -0,0 +1,19 @@
+CREATE VIEW [dbo].[vyuSTGetFuelInventory]
+AS
+SELECT		fuelInventory.intFuelInventoryId,
+			fuelInventory.intCheckoutId,
+			fuelInventory.intDeviceId,
+			fuelInventory.dblGallons,
+			fuelInventory.dtmFuelInventoryDate,
+			fuelInventory.intConcurrencyId,
+			item.strItemNo,
+			device.strSerialNumber
+FROM		tblSTCheckoutFuelInventory fuelInventory
+INNER JOIN	tblTMDevice device
+ON			fuelInventory.intDeviceId = device.intDeviceId
+INNER JOIN	tblTMSiteDevice sitedevice
+ON			device.intDeviceId = sitedevice.intDeviceId
+INNER JOIN	tblTMSite site
+ON			site.intSiteID = sitedevice.intSiteID
+INNER JOIN tblICItem item
+ON			site.intProduct = item.intItemId
