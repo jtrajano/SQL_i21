5b817f5889d9bed004e601ae56242895473aa4db
bponce <bonn.ponce@irely.com>
2023-03-13 11:02:30 +0800

    CF-3254

---
+++ i21Database/dbo/Views/vyuCFSearchSiteGroupAdjustment.sql
@@ -0,0 +1,32 @@
+﻿CREATE VIEW dbo.vyuCFSearchSiteGroupAdjustment  
+AS  
+SELECT    
+  tblCFSiteGroupPriceAdjustmentHeader.intSiteGroupId  
+ ,tblCFSiteGroupPriceAdjustmentHeader.dtmEffectiveDate  
+ ,tblCFSiteGroupPriceAdjustmentHeader.intSiteGroupPriceAdjustmentHeaderId  
+ ,tblCFSiteGroupPriceAdjustmentHeader.intConcurrencyId  
+ ,tblCFSiteGroupPriceAdjustment.dblRate  
+ ,tblCFSiteGroupPriceAdjustment.dtmEndEffectiveDate  
+ ,tblCFSiteGroupPriceAdjustment.dtmStartEffectiveDate  
+ ,tblCFSiteGroupPriceAdjustment.intARItemId    
+ ,tblCFSiteGroupPriceAdjustment.intPriceGroupId  
+ ,tblCFSiteGroupPriceAdjustment.intSiteGroupPriceAdjustmentId  
+ ,tblICItem.intItemId  
+ ,tblICItem.strItemNo  
+ ,strItemDescription = tblICItem.strDescription  
+ ,strSiteGroupDescription = tblCFSiteGroup.strDescription  
+ ,tblCFSiteGroup.strSiteGroup  
+ ,tblCFSiteGroup.strType  
+ ,tblCFPriceRuleGroup.intPriceRuleGroupId  
+ ,strPriceRuleGroup = tblCFPriceRuleGroup.strPriceGroup  
+ ,strPriceRuleGroupDescription = tblCFPriceRuleGroup.strPriceGroupDescription  
+FROM tblCFSiteGroupPriceAdjustmentHeader  
+LEFT JOIN tblCFSiteGroupPriceAdjustment  
+ON tblCFSiteGroupPriceAdjustmentHeader.intSiteGroupPriceAdjustmentHeaderId = tblCFSiteGroupPriceAdjustment.intSiteGroupPriceAdjustmentHeaderId  
+LEFT JOIN tblICItem  
+ON tblCFSiteGroupPriceAdjustment.intARItemId = tblICItem.intItemId  
+LEFT JOIN tblCFSiteGroup  
+ON tblCFSiteGroupPriceAdjustmentHeader.intSiteGroupId = tblCFSiteGroup.intSiteGroupId  
+LEFT JOIN tblCFPriceRuleGroup  
+ON tblCFSiteGroupPriceAdjustment.intPriceGroupId = tblCFPriceRuleGroup.intPriceRuleGroupId  
+ GO
