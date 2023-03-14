b3208649433677bbd07f49b72c1a84ea869ecb4b
Leslie Arat <leslie.arat@irely.com>
2023-03-14 17:45:06 +0800

    GRN-3261 (GII-Inventory Adjustment)

--- i21Database/dbo/Stored Procedures/uspGRGrainInventoryInquiryReport.sql
+++ i21Database/dbo/Stored Procedures/uspGRGrainInventoryInquiryReport.sql
@@ -1612,7 +1612,7 @@ OUTER APPLY (
 		,UM.strUnitMeasure
 ) D
 OUTER APPLY (
-	SELECT SUM(dblUnits) dblUnits
+	SELECT ABS(SUM(dblUnits)) dblUnits
 		,intCommodityId
 		,intCompanyLocationId
 		,strUOM
