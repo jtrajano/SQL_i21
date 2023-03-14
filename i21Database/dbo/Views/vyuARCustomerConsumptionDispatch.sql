a0a9b346c086249994324c9395722e9a82b25ae4
Roudinee Panteria <roudinee.panteria@irely.com>
2023-03-14 17:59:21 +0800

    AR-15794

---
+++ i21Database/dbo/Views/vyuARCustomerConsumptionDispatch.sql
@@ -0,0 +1,30 @@
+﻿CREATE VIEW dbo.vyuARCustomerConsumptionDispatch
+AS
+
+SELECT 
+	 TMO.intDispatchId
+	,TMO.strOrderNumber
+	,AC.intEntityId
+	,TMS.intSiteID
+	,TMS.intSiteNumber
+	,strSiteNumber			= REPLACE(STR(TMS.[intSiteNumber], 4), SPACE(1), '0') COLLATE Latin1_General_CI_AS
+	,TMS.strBillingBy
+	,TMS.dblLastMeterReading
+	,TMS.dblPriceAdjustment
+	,TANK.dblConversionFactor
+FROM tblTMOrder TMO
+INNER JOIN tblTMDispatch TMD ON TMO.intDispatchId = TMD.intDispatchID
+INNER JOIN tblTMSite TMS ON TMO.intSiteId = TMS.intSiteID
+INNER JOIN tblTMCustomer TMC ON TMS.intCustomerID = TMC.intCustomerID
+INNER JOIN tblARCustomer AC ON TMC.intCustomerNumber = AC.intEntityId
+OUTER APPLY (
+	SELECT TOP 1 
+		 MT.[strMeterType]
+		,MT.[dblConversionFactor]
+	FROM tblTMSiteDevice SD
+	INNER JOIN tblTMDevice D ON SD.[intDeviceId] = D.[intDeviceId]  
+	INNER JOIN tblTMDeviceType DT ON D.[intDeviceTypeId] = DT.[intDeviceTypeId] 
+	INNER JOIN tblTMMeterType MT ON D.[intMeterTypeId] = MT.[intMeterTypeId]
+	WHERE SD.[intSiteID] = TMS.[intSiteID] AND DT.strDeviceType = 'Flow Meter'
+	ORDER BY intSiteDeviceID ASC
+) TANK
