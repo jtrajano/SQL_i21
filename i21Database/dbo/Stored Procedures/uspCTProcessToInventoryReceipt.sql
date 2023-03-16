d9baf4fd8617aea5603a205a16139591c6c3182c
Jayson Ramos <jayson.ramos@iRely.com>
2023-03-16 19:12:40 +0800

    CT-8588
    (cherry picked from commit 66c9eced55d5b486c907c7f17709ece353c9eee3)

--- i21Database/dbo/Stored Procedures/uspCTProcessToInventoryReceipt.sql
+++ i21Database/dbo/Stored Procedures/uspCTProcessToInventoryReceipt.sql
@@ -112,8 +112,28 @@ AS
 				--CT-7100 (ECOM commented this line for ECOM) --								END,
 				--CT-7100 (ECOM commented this line for ECOM) --intCostUOMId				=	IU.intItemUOMId, -- If Seq-price-uom is null, then use the contract-detail-item-uom. 
 				dblCost						=	CASE	WHEN	CD.intPricingTypeId = 2 
-														THEN	((CASE WHEN @ysnEnableBudgetForBasisPricing = 0 THEN ISNULL(dbo.fnRKGetLatestClosingPrice(CD.intFutureMarketId,CD.intFutureMonthId,GETDATE()), 0) ELSE CD.dblBudgetPrice END + 
-																ISNULL(CD.dblBasis,0))  / CASE WHEN ISNULL(CU.ysnSubCurrency, 0) = CAST(1 AS BIT)   THEN 100 ELSE 1 END) * ISNULL(CD.dblRate, 1 )
+														THEN	isnull(
+																	(
+																		(
+																			(
+																				CASE
+																				WHEN @ysnEnableBudgetForBasisPricing = 0
+																				THEN ISNULL(dbo.fnRKGetLatestClosingPrice(CD.intFutureMarketId,CD.intFutureMonthId,GETDATE()), 0)
+																				ELSE CD.dblBudgetPrice
+																				END
+																				+ 
+																				ISNULL(CD.dblBasis,0)
+																			)
+																			/
+																			CASE
+																			WHEN ISNULL(CU.ysnSubCurrency, 0) = CAST(1 AS BIT)
+																			THEN 100
+																			ELSE 1
+																			END
+																		) * ISNULL(CD.dblRate, 1)
+																	)
+																	,0
+																)
 														ELSE	ISNULL(AD.dblSeqPrice,0)
 												END,
 				intCostUOMId				=	CASE WHEN CD.intPricingTypeId = 2 OR ISNULL(CD.intCurrencyExchangeRateId, 0) = 0 THEN 
