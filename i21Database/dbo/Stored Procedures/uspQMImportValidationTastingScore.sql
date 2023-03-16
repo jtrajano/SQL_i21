d94f78da0511a7e877349ddb169d674267f20608
Jonathan Valenzuela <jonathan.valenzuela@iRely.com>
2023-03-16 09:40:37 +0800

    QC-1006

--- i21Database/dbo/Stored Procedures/uspQMImportValidationTastingScore.sql
+++ i21Database/dbo/Stored Procedures/uspQMImportValidationTastingScore.sql
@@ -22,7 +22,7 @@ BEGIN TRY
 
 	/* Catalogue Validation. */
 	UPDATE Catalogue 
-	SET Catalogue.strLogResult	= 'Tasting Score information is not available for the Tealingo Item and Imported Catalogue.'
+	SET Catalogue.strLogResult	= 'Tasting Score record not found or Tealingo Item was not provided.'
 	  , Catalogue.ysnSuccess	= 0
 	  , Catalogue.ysnProcessed	= 1
 	FROM tblQMImportCatalogue AS Catalogue
