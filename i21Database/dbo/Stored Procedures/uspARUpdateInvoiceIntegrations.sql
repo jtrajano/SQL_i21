5edb0c030cc4567c8a6993c09e124228dd60fb05
Patrick Matthew Maniego <pat.maniego@iRely.com>
2023-03-17 17:09:38 +0800

    AR-13737

--- i21Database/dbo/Stored Procedures/uspARUpdateInvoiceIntegrations.sql
+++ i21Database/dbo/Stored Procedures/uspARUpdateInvoiceIntegrations.sql
@@ -175,7 +175,7 @@ BEGIN TRY
 	EXEC dbo.[uspARUpdateInboundShipmentOnInvoice] @intInvoiceId, @ForDelete, @intUserId	
 	EXEC dbo.[uspARUpdateGrainOpenBalance] @intInvoiceId, @ForDelete, @intUserId
 	IF @FromPosting = 0 EXEC dbo.[uspARUpdateContractOnInvoice] @intInvoiceId, @ForDelete, @intUserId, @InvoiceIds
-	EXEC dbo.[uspARUpdateItemContractOnInvoice] @intInvoiceId, @ForDelete, @intUserId
+	IF @FromPosting = 0 EXEC dbo.[uspARUpdateItemContractOnInvoice] @intInvoiceId, @ForDelete, @intUserId
 	IF @ForDelete = 1 AND @InvoiceDetailId IS NULL EXEC dbo.[uspCTBeforeInvoiceDelete] @intInvoiceId, @intUserId
 	EXEC dbo.[uspARUpdateReturnedInvoice] @intInvoiceId, @ForDelete, @intUserId 
 	EXEC dbo.[uspARUpdateInvoiceAccruals] @intInvoiceId, @strSessionId	
