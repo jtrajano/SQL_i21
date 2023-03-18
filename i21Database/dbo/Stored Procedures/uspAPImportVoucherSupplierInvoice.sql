8a178b13153e3096fbcab4e6496755bebf0b4789
Erick Dela Cruz <erick.delacruz@irely.com>
2023-03-18 13:37:29 +0800

    AP-11625

--- i21Database/dbo/Stored Procedures/uspAPImportVoucherSupplierInvoice.sql
+++ i21Database/dbo/Stored Procedures/uspAPImportVoucherSupplierInvoice.sql
@@ -18,6 +18,8 @@ DECLARE @lastIndex INT;
 DECLARE @vouchers TABLE(intBillId INT);
 DECLARE @totalVoucherCreated INT = 0;
 DECLARE @totalIssues INT;
+DECLARE @detailsUpdated INT = 0;
+DECLARE @detailsAdded INT = 0;
 
 -- SET @lastIndex = (LEN(@file)) -  CHARINDEX('/', REVERSE(@file)) 
 -- SET @path = SUBSTRING(@file, 0, @lastindex + 1)
@@ -53,15 +55,15 @@ SELECT
 	ysnActive							=	C.ysnPymtCtrlActive,
 	dtmDate								=	CASE WHEN A.dtmDate LIKE '%.%' THEN CONVERT(DATETIME,A.dtmDate, 103) ELSE CONVERT(DATETIME,A.dtmDate, 101) END,
 	dtmBillDate							=	CASE WHEN A.dtmDate LIKE '%.%' THEN CONVERT(DATETIME,A.dtmDate, 103) ELSE CONVERT(DATETIME,A.dtmDate, 101) END,
-	strVendorOrderNumber				=	A.strInvoiceNumber,
+	strVendorOrderNumber				=	NULLIF(A.strInvoiceNumber,''),
 	dtmExpectedDate						=	CASE WHEN A.dtmExpectedDate LIKE '%.%' THEN CONVERT(DATETIME,A.dtmExpectedDate, 103) ELSE CONVERT(DATETIME,A.dtmExpectedDate, 101) END,
     dblQuantityToBill					=	CAST(A.dblQtyReceived AS DECIMAL(38,20)),
     dblCost								=	CAST(A.dblCost AS DECIMAL(38,15)),
 	/*Supplier Invoice*/				
 	intSaleYear							=	CAST(A.intSaleYear AS INT),
 	intSaleYearId						=	J.intSaleYearId,
-	strSaleNumber						=	CAST(A.strSaleNo AS NVARCHAR(50)),
-	strVendorLotNumber					=	CAST(A.strLotNumber AS NVARCHAR(50)),
+	strSaleNumber						=	NULLIF(CAST(A.strSaleNo AS NVARCHAR(50)),''),
+	strVendorLotNumber					=	NULLIF(CAST(A.strLotNumber AS NVARCHAR(50)),''),
 	dtmSaleDate							=	CASE WHEN A.dtmSaleDate LIKE '%.%' THEN CONVERT(DATETIME,A.dtmSaleDate, 103) ELSE CONVERT(DATETIME,A.dtmSaleDate, 101) END,
 	strPreInvoiceGarden					=	CAST(A.strPreInvoiceGarden AS NVARCHAR(50)),
 	intGardenMarkId						=	G.intGardenMarkId,
@@ -78,7 +80,7 @@ SELECT
 	/*Not exists in i21*/		
 	strCatalogueType					=	A.strCatalogueType,
 	intCatalogueTypeId					=	I.intCatalogueTypeId,
-	strChannel							=	A.strChannel,
+	strChannel							=	NULLIF(A.strChannel,''),
 	dblPackageBreakups					=	CAST(A.strPackageBreakups AS DECIMAL(18,6)),
 	strPurchaseType						=	A.strPurchaseType,
 	intTransactionType					=	1, --CASE WHEN A.strPurchaseType = 'I' THEN 1 ELSE 3 END,
@@ -126,13 +128,13 @@ INSERT INTO @voucherPayables
     ,dblCost
 	,strVendorOrderNumber
 	,intSubLocationId	
-	,intLotId
 	,intSaleYear						
 	,strSaleNumber						
 	,dtmSaleDate						
 	,strVendorLotNumber		
 	,intGardenMarkId				
 	,strPreInvoiceGardenNumber			
+	,strPreInvoicePurchaseType
 	,strBook							
 	,strSubBook		
 	,strComments
@@ -163,13 +165,13 @@ SELECT
     ,dblCost
 	,strVendorOrderNumber
 	,intSubLocationId
-	,intLotId
 	,intSaleYear						
 	,strSaleNumber						
 	,dtmSaleDate						
 	,strVendorLotNumber					
 	,intGardenMarkId				
-	,strPreInvoiceGardenNumber			
+	,strPreInvoiceGardenNumber		
+	,strPurchaseType	
 	,strBook							
 	,strSubBook	
 	,A.strPreInvoiceGrade
@@ -187,6 +189,14 @@ SELECT
 FROM #tmpConvertedSupplierInvoiceData A
 WHERE
 	A.intEntityVendorId > 0
+AND A.intMarketZoneId > 0
+AND A.intCatalogueTypeId > 0
+AND A.intSaleYearId > 0
+AND A.intLocationId > 0
+AND A.intSubLocationId > 0
+AND A.strSaleNumber IS NOT NULL
+-- AND A.strVendorLotNumber IS NOT NULL
+AND A.strVendorOrderNumber IS NOT NULL
 
 IF NOT EXISTS(SELECT 1 FROM @voucherPayables)
 BEGIN
@@ -195,16 +205,251 @@ BEGIN
 	GOTO IMPORTFAILED
 END
 
+--GET ALL VOUCHERS INFO
+IF OBJECT_ID(N'tempdb..#tmpVoucherHeaderInfo') IS NOT NULL DROP TABLE #tmpVoucherHeaderInfo
+
+SELECT DISTINCT
+	A.intEntityVendorId, A.strVendorOrderNumber, A.dtmDate
+INTO #tmpVoucherHeaderInfo
+FROM @voucherPayables A
+
+IF OBJECT_ID(N'tempdb..#tmpVoucherHeaderMatch') IS NOT NULL DROP TABLE #tmpVoucherHeaderMatch
+
+SELECT 
+	A.intEntityVendorId,
+	A.strVendorOrderNumber,
+	A.dtmDate,
+	B.strBillId,
+	B.intBillId
+INTO #tmpVoucherHeaderMatch
+FROM #tmpVoucherHeaderInfo A
+INNER JOIN tblAPBill B 
+	ON A.intEntityVendorId = B.intEntityVendorId
+AND A.strVendorOrderNumber = B.strVendorOrderNumber
+AND A.dtmDate = DATEADD(dd, DATEDIFF(dd, 0,B.dtmDate), 0)
+WHERE
+	B.ysnPosted = 0
+
+IF EXISTS(SELECT 1 FROM #tmpVoucherHeaderMatch)
+BEGIN 
+
+	--GET ALL ITEMS THAT MATCHES THE EXISTING VOUCHER BASE ON VENDOR, INVOICE NUMBER AND DATE
+	IF OBJECT_ID(N'tempdb..#tmpupdateDetailsPayable') IS NOT NULL DROP TABLE #tmpupdateDetailsPayable
+
+	SELECT A.* 
+	INTO #tmpupdateDetailsPayable
+	FROM @voucherPayables A
+	INNER JOIN #tmpVoucherHeaderMatch B
+		ON A.intEntityVendorId = B.intEntityVendorId
+	AND A.strVendorOrderNumber = B.strVendorOrderNumber
+	AND A.dtmDate = B.dtmDate
+	
+	--REMOVE FROM VOUCHER PAYABLES TO AVOID CREATION
+	DELETE A
+	FROM @voucherPayables A
+	INNER JOIN #tmpVoucherHeaderMatch B
+	ON 
+		A.intEntityVendorId = B.intEntityVendorId
+	AND A.strVendorOrderNumber = B.strVendorOrderNumber
+	AND A.dtmDate = B.dtmDate
+
+	--VALIDATE MULTIPLE MATCHES
+	DECLARE @multipleMatchesInvoice AS TABLE(strVendorOrderNumber NVARCHAR(50) COLLATE Latin1_General_CI_AS,
+	 strName NVARCHAR(50), dtmDate DATETIME, intEntityVendorId INT)
+
+	INSERT INTO @multipleMatchesInvoice
+	SELECT
+		A.strVendorOrderNumber,
+		B.strName,
+		A.dtmDate,
+		B.intEntityId
+	FROM #tmpVoucherHeaderMatch A
+	INNER JOIN tblEMEntity B ON A.intEntityVendorId = B.intEntityId
+	GROUP BY 
+		A.strVendorOrderNumber,
+		B.strName,
+		A.dtmDate,
+		B.intEntityId
+	HAVING COUNT(*) > 1
+
+	--REMOVE FROM #tmpVoucherHeaderMatch the multiple matches, we do not change the voucher for multiple matches
+	DELETE A
+	FROM #tmpVoucherHeaderMatch A
+	INNER JOIN @multipleMatchesInvoice B
+	ON
+		A.strVendorOrderNumber = B.strVendorOrderNumber
+	AND	A.dtmDate = B.dtmdate
+	AND A.intEntityVendorId = B.intEntityVendorId
+
+	--CHECK IF MATCHES ON DETAILS
+	IF OBJECT_ID(N'tempdb..#tmpupdateDetailsPayableMatch') IS NOT NULL DROP TABLE #tmpupdateDetailsPayableMatch
+
+	UPDATE A
+	SET
+		A.intBillId = B.intBillId
+	FROM #tmpupdateDetailsPayable A
+	INNER JOIN #tmpVoucherHeaderMatch B
+	ON
+		A.strVendorOrderNumber = B.strVendorOrderNumber
+	AND	A.dtmDate = B.dtmDate
+	AND A.intEntityVendorId = B.intEntityVendorId
+
+	-- -- --CREATE THE EMPTY TEMP TABLE SO WE COULD USE THE OUTPUT CLAUSE
+	-- -- SELECT A.*
+	-- -- INTO #tmpupdateDetailsPayableMatch
+	-- -- FROM #tmpupdateDetailsPayable A
+	-- -- WHERE 1 = 0
+	-- DECLARE @headerFound AS TABLE(intBillId INT, intVoucherPayableId INT)
+
+	-- INSERT INTO @headerFound
+	-- SELECT
+	-- 	B.intBillId, A.intVoucherPayableId
+	-- FROM #tmpupdateDetailsPayable A
+	-- INNER JOIN tblAPBill B
+	-- 	ON A.intEntityVendorId = B.intEntityVendorId
+	-- AND A.strVendorOrderNumber = B.strVendorOrderNumber
+	-- AND A.dtmDate = DATEADD(dd, DATEDIFF(dd, 0,B.dtmDate), 0)
+	-- WHERE
+	-- 	B.ysnPosted = 0
+
+	-- --UPDATE THE intBillId of the payables, for insert or update
+	-- UPDATE A
+	-- SET
+	-- 	A.intBillId = B.intBillId
+	-- FROM #tmpupdateDetailsPayable A
+	-- INNER JOIN @headerFound B ON A.intVoucherPayableId = B.intVoucherPayableId
+	
+	DECLARE @detailsFound AS TABLE(intBillId INT, intBillDetailId INT, intVoucherPayableId INT)
+	INSERT INTO @detailsFound
+	SELECT
+		C.intBillId, C.intBillDetailId, A.intVoucherPayableId
+	FROM #tmpupdateDetailsPayable A
+	INNER JOIN (
+		tblAPBill B INNER JOIN tblAPBillDetail C ON B.intBillId = C.intBillId
+	)
+	ON
+		A.intEntityVendorId = B.intEntityVendorId
+	AND A.strVendorOrderNumber = B.strVendorOrderNumber
+	AND A.intSaleYear = C.intSaleYear
+	AND A.strSaleNumber = C.strSaleNumber
+	AND A.intCatalogueTypeId = C.intCatalogueTypeId
+	AND A.intLocationId = B.intStoreLocationId
+	AND A.intSubLocationId = C.intSubLocationId
+	AND A.intMarketZoneId = C.intMarketZoneId
+	AND A.strVendorLotNumber = C.strVendorLotNumber
+	AND A.dtmDate = DATEADD(dd, DATEDIFF(dd, 0,B.dtmDate), 0)
+	WHERE
+		B.ysnPosted = 0
+		
+	SELECT A.* 
+	INTO #tmpupdateDetailsPayableMatch
+	FROM #tmpupdateDetailsPayable A
+	INNER JOIN (
+		tblAPBill B INNER JOIN tblAPBillDetail C ON B.intBillId = C.intBillId
+	)
+		ON A.intEntityVendorId = B.intEntityVendorId
+	AND A.strVendorOrderNumber = B.strVendorOrderNumber
+	AND A.intSaleYear = C.intSaleYear
+	AND A.strSaleNumber = C.strSaleNumber
+	AND A.intCatalogueTypeId = C.intCatalogueTypeId
+	AND A.intLocationId = B.intStoreLocationId
+	AND A.intSubLocationId = C.intSubLocationId
+	AND A.intMarketZoneId = C.intMarketZoneId
+	AND A.strVendorLotNumber = C.strVendorLotNumber
+	AND A.dtmDate = DATEADD(dd, DATEDIFF(dd, 0,B.dtmDate), 0)
+	WHERE
+		B.ysnPosted = 0
+
+	--WE DO NOT UPDATE HERE THE EXISTING DETAIL RECORDS, INSTEAD, WE REMOVE AND RE-ADDED FOR SIMPLICITY OF CODES
+	--VALIDATE MULTIPLE MATCHES
+	DECLARE @multipleMatchesInvoiceDetails AS TABLE(strVendorOrderNumber NVARCHAR(50), strName NVARCHAR(50), dtmDate DATETIME, intVoucherPayableId INT)
+
+	INSERT INTO @multipleMatchesInvoiceDetails
+	SELECT strVendorOrderNumber, strName, dtmDate, intVoucherPayableId
+	FROM (
+		SELECT
+			A.strVendorOrderNumber,
+			B.strName,
+			A.dtmDate,
+			A.intVoucherPayableId,
+			A.intSaleYear,
+			A.strSaleNumber,
+			A.intCatalogueTypeId,
+			A.intLocationId,
+			A.intMarketZoneId,
+			A.strVendorLotNumber
+		FROM #tmpupdateDetailsPayable A
+		INNER JOIN tblEMEntity B ON A.intEntityVendorId = B.intEntityId
+		GROUP BY 
+			A.intEntityVendorId
+			,A.strVendorOrderNumber
+			,B.strName
+			,A.intSaleYear
+			,A.strSaleNumber
+			,A.intCatalogueTypeId
+			,A.intLocationId
+			,A.intMarketZoneId
+			,A.strVendorLotNumber
+			,A.dtmDate
+			,A.intVoucherPayableId
+		HAVING COUNT(*) > 1
+	) tmp
+
+	--REMOVE FROM MATCH THE DUPLICATES
+	DELETE A
+	FROM #tmpupdateDetailsPayableMatch A
+	INNER JOIN @multipleMatchesInvoiceDetails B ON A.intVoucherPayableId = B.intVoucherPayableId
+
+	DELETE A
+	FROM #tmpupdateDetailsPayable A
+	INNER JOIN @multipleMatchesInvoiceDetails B ON A.intVoucherPayableId = B.intVoucherPayableId
+
+	--IF DETAIL MATCHES
+	IF EXISTS(SELECT 1 FROM #tmpupdateDetailsPayableMatch)
+	BEGIN
+		--IF MATCHES ON DETAILS, REMOVE IT FROM EXISTING VOUCHERS
+		DELETE A
+		FROM tblAPBillDetail A
+		INNER JOIN tblAPBill B ON A.intBillId = B.intBillId
+		INNER JOIN @detailsFound C ON A.intBillDetailId = C.intBillDetailId
+		WHERE
+			B.ysnPosted = 0
+
+		SET @detailsUpdated = (SELECT COUNT(*) FROM #tmpupdateDetailsPayableMatch)
+	END
+	ELSE
+	BEGIN
+	
+		SET @detailsAdded = (SELECT COUNT(*) FROM #tmpupdateDetailsPayable A WHERE A.intVoucherPayableId NOT IN (SELECT intVoucherPayableId FROM #tmpupdateDetailsPayableMatch))
+	END
+
+	ALTER TABLE #tmpupdateDetailsPayable DROP COLUMN intVoucherPayableId
+	DECLARE @insertUpdateVoucherDetail AS VoucherPayable
+	
+	INSERT INTO @insertUpdateVoucherDetail
+	SELECT A.* FROM #tmpupdateDetailsPayable A --USE ALIAS TO WORK WITH IDENTITY
+
+	EXEC uspAPAddVoucherDetail @voucherDetails = @insertUpdateVoucherDetail
+	
+	DECLARE @voucherIds AS Id
+	INSERT INTO @voucherIds
+	SELECT DISTINCT
+		intBillId
+	FROM @insertUpdateVoucherDetail
+	EXEC uspAPUpdateVoucherTotal @voucherIds
+END
+
 --vendor do not have default expense account
 --vendor do not exists
-
 DECLARE @createdVoucher NVARCHAR(MAX);
-EXEC uspAPCreateVoucher @voucherPayables = @voucherPayables, @userId = @userId, @throwError = 1, @createdVouchersId = @createdVoucher OUT
-
-IF @createdVoucher IS NULL 
-BEGIN 
-	RAISERROR('No valid record to create the voucher.', 16, 1);
-	RETURN;
+IF EXISTS(SELECT 1 FROM @voucherPayables)
+BEGIN
+	EXEC uspAPCreateVoucher @voucherPayables = @voucherPayables, @userId = @userId, @throwError = 1, @createdVouchersId = @createdVoucher OUT
+	IF @createdVoucher IS NULL 
+	BEGIN 
+		RAISERROR('Failed to create the voucher.', 16, 1);
+		RETURN;
+	END
 END
 
 IMPORTFAILED:
@@ -226,7 +471,11 @@ OR A.intCatalogueTypeId IS NULL
 OR A.intPurchasingGroupId IS NULL
 OR A.intSubLocationId IS NULL
 OR A.intMarketZoneId IS NULL
+-- OR A.strVendorLotNumber IS NULL
+OR A.strVendorOrderNumber IS NULL
 
+SET @totalIssues = @totalIssues + (SELECT COUNT(*) FROM @multipleMatchesInvoice)
+SET @totalIssues = @totalIssues + (SELECT COUNT(*) FROM @multipleMatchesInvoiceDetails)
 
 DECLARE @invalidPayables AS TABLE (strVendorOrderNumber NVARCHAR(MAX) COLLATE Latin1_General_CI_AS, strError NVARCHAR(MAX) COLLATE Latin1_General_CI_AS)
 INSERT INTO @invalidPayables
@@ -258,11 +507,29 @@ FROM (
 	UNION ALL
 	SELECT intPartitionId, strVendorOrderNumber, 'Line with Invoice No. ' + strVendorOrderNumber + ': Invalid Purchase Group ' + strPurchasingGroup FROM #tmpConvertedSupplierInvoiceData WHERE intPurchasingGroupId IS NULL
 	UNION ALL
-	SELECT intPartitionId, strVendorOrderNumber, 'Line with Invoice No. ' + strVendorOrderNumber + ': Invalid Market Zone ' + strChannel FROM #tmpConvertedSupplierInvoiceData WHERE intMarketZoneId IS NULL
+	SELECT intPartitionId, strVendorOrderNumber, 'Line with Invoice No. ' + strVendorOrderNumber + ': Invalid Market Zone ' + ISNULL(strChannel,'') FROM #tmpConvertedSupplierInvoiceData WHERE intMarketZoneId IS NULL
 	UNION ALL
 	SELECT intPartitionId, strVendorOrderNumber, 'Line with Invoice No. ' + strVendorOrderNumber + ': Invalid Storage Location ' + strSubLocationName FROM #tmpConvertedSupplierInvoiceData WHERE intSubLocationId IS NULL
+	-- UNION ALL
+	-- SELECT intPartitionId, strVendorOrderNumber, 'Line with Invoice No. ' + strVendorOrderNumber + ': Invalid Lot Number ' + ISNULL(strVendorLotNumber,'') FROM #tmpConvertedSupplierInvoiceData WHERE strVendorLotNumber IS NULL
+	-- UNION ALL
+	-- SELECT DISTINCT NULL, strVendorOrderNumber, 'Line with Invoice No. ' + strVendorOrderNumber + ': multiple match for voucher(s) found.'  FROM @multipleMatchesInvoice
+	-- UNION ALL
+	-- SELECT DISTINCT NULL, strVendorOrderNumber, 'Line with Invoice No. ' + strVendorOrderNumber + ': multiple match for voucher detail(s) found.'  FROM @multipleMatchesInvoiceDetails
 ) tblErrors
 ORDER BY intPartitionId
+
+IF EXISTS(SELECT 1 FROM @multipleMatchesInvoice)
+BEGIN
+	INSERT INTO @invalidPayables
+	SELECT DISTINCT strVendorOrderNumber, 'Line with Invoice No. ' + strVendorOrderNumber + ': multiple match for voucher(s) found.'  FROM @multipleMatchesInvoice
+END
+
+IF EXISTS(SELECT 1 FROM @multipleMatchesInvoiceDetails)
+BEGIN
+	INSERT INTO @invalidPayables
+	SELECT DISTINCT strVendorOrderNumber, 'Line with Invoice No. ' + strVendorOrderNumber + ': multiple match for voucher detail(s) found.'  FROM @multipleMatchesInvoiceDetails
+END
 
 --LOG ALL
 DECLARE @logId INT;
@@ -303,6 +570,61 @@ BEGIN
 		'Voucher ' + B.strBillId + ' successfully created'
 	FROM @vouchers A
 	INNER JOIN tblAPBill B ON A.intBillId = B.intBillId
+	UNION ALL
+	SELECT
+		@logId,
+		'There were ' + CAST(@detailsUpdated AS NVARCHAR) + ' details updated.'
+	UNION ALL
+	SELECT
+		@logId,
+		'There were ' + CAST(@detailsAdded AS NVARCHAR) + ' details inserted.'
+
+	--FAILED
+	INSERT INTO tblAPImportLogDetail
+	(
+		intImportLogId,
+		strEventDescription
+	)
+	SELECT
+		@logId,
+		strError
+	FROM @invalidPayables A
+END
+ELSE IF(@detailsAdded > 0 OR @detailsUpdated > 0)
+BEGIN
+	INSERT INTO tblAPImportLog
+	(
+		strEvent,
+		strIrelySuiteVersion,
+		intEntityId,
+		dtmDate,
+		intSuccessCount,
+		intErrorCount
+	)
+	SELECT
+		'Some voucher(s) successfully updated from CSV',
+		(SELECT TOP 1 strVersionNo FROM tblSMBuildNumber ORDER BY intVersionID DESC),
+		@userId,
+		GETDATE(),
+		@totalVoucherCreated,
+		@totalIssues
+
+	SET @logId = SCOPE_IDENTITY();
+	SET @importLogId = @logId;
+
+	--SUCCESS
+	INSERT INTO tblAPImportLogDetail
+	(
+		intImportLogId,
+		strEventDescription
+	)
+	SELECT
+		@logId,
+		'There were ' + CAST(@detailsUpdated AS NVARCHAR) + ' details updated.'
+	UNION ALL
+	SELECT
+		@logId,
+		'There were ' + CAST(@detailsAdded AS NVARCHAR) + ' details inserted.'
 
 	--FAILED
 	INSERT INTO tblAPImportLogDetail
@@ -321,7 +643,7 @@ END
 --INSERT ERROR LOG
 IF @totalIssues > 0 
 BEGIN
-	IF @totalVoucherCreated <= 0
+	IF @totalVoucherCreated <= 0 AND @detailsAdded = 0 AND @detailsUpdated = 0
 	BEGIN
 		INSERT INTO tblAPImportLog
 		(
@@ -351,9 +673,22 @@ BEGIN
 		OR A.intPurchasingGroupId IS NULL
 		OR A.intSubLocationId IS NULL
 		OR A.intMarketZoneId IS NULL
-
+		-- OR A.strVendorLotNumber IS NULL
+		OR A.strVendorOrderNumber IS NULL
 		
 		SET @logId = SCOPE_IDENTITY();
+
+		IF NOT (@logId > 0)
+		BEGIN
+			SELECT TOP 1
+			'Importing voucher Failed',
+			(SELECT TOP 1 strVersionNo FROM tblSMBuildNumber ORDER BY intVersionID DESC),
+			@userId,
+			GETDATE(),
+			@totalVoucherCreated,
+			@totalIssues
+			FROM @multipleMatchesInvoice
+		END
 
 		SET @importLogId = @logId;
 
