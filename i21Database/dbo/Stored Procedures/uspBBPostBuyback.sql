850f68a8fa1a76fb044621c3188f93dc6959a9ea
Wendell Wayne Estrada <wendell.estrada@irely.com>
2023-03-19 12:32:39 +0800

    BB-191 HDTN-260507 - should only update inventory cost in sales invoice buyback amount field

--- i21Database/dbo/Stored Procedures/uspBBPostBuyback.sql
+++ i21Database/dbo/Stored Procedures/uspBBPostBuyback.sql
@@ -350,6 +350,7 @@ AS
 					,intInvoiceDetailId
 					FROM tblBBBuybackDetail
 					WHERE intBuybackId = @intBuyBackId
+						AND strCharge = 'Inventory'
 					GROUP BY intInvoiceDetailId
 					) A
 			WHERE tblARInvoiceDetail.intInvoiceDetailId = A.intInvoiceDetailId
