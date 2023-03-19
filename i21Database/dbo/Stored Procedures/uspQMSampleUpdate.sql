6e30966dca109eab2fc71281cfbbf62c3aac2bbc
Christian Joel Martinez <Christian.Martinez@irely.com>
2023-03-19 15:52:07 +0800

    QC-782

--- i21Database/dbo/Stored Procedures/uspQMSampleUpdate.sql
+++ i21Database/dbo/Stored Procedures/uspQMSampleUpdate.sql
@@ -57,6 +57,8 @@ BEGIN TRY
   ,@intOrgCompanyLocationSubLocationId INT  
   ,@intRelatedSampleId INT  
   ,@intCurrentRelatedSampleId INT  
+
+ DECLARE @intCountryID INT
   
 DECLARE @ysnSuccess BIT
 
@@ -571,6 +573,34 @@ DECLARE @ysnSuccess BIT
    ) x  
  WHERE dbo.tblQMSample.intSampleId = @intSampleId  
   AND x.strRowState = 'MODIFIED'  
+
+  --  Origin Check
+  SELECT @intCountryID = CA.intCommodityAttributeId
+  FROM tblICCommodityAttribute CA
+  INNER JOIN tblSMCountry C ON C.intCountryID = CA.intCountryID
+  INNER JOIN tblQMSample S ON S.intSampleId = @intSampleId
+  WHERE CA.strType = 'Origin'
+  AND C.strCountry = S.strCountry
+
+  IF @intCountryID IS NULL AND EXISTS (SELECT 1 FROM tblQMCompanyPreference WHERE ysnAllowEditingTheOrigin =1)
+  BEGIN
+    RAISERROR (  
+     'Origin does not exist.'  
+     ,16  
+     ,1  
+     )  
+  END
+  ELSE
+  BEGIN
+    UPDATE S
+    SET
+      intCountryID = CA.intCommodityAttributeId
+      ,strCountry = C.strCountry
+    FROM tblQMSample S
+    INNER JOIN tblICCommodityAttribute CA ON CA.intCommodityAttributeId = @intCountryID
+    INNER JOIN tblSMCountry C ON C.intCountryID = CA.intCountryID
+    WHERE S.intSampleId = @intSampleId
+  END
   
  IF ISNULL(@intRelatedSampleId ,0) <> 0  
  BEGIN
