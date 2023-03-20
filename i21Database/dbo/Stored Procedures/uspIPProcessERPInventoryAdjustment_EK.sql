a2d6d9666a2decdb8f15075e75ceddd669774f83
Prem Selladurai <prem.selladurai@irely.com>
2023-03-20 10:11:48 +0530

    MFG-4924
    MFG-4924

--- i21Database/dbo/Stored Procedures/uspIPProcessERPInventoryAdjustment_EK.sql
+++ i21Database/dbo/Stored Procedures/uspIPProcessERPInventoryAdjustment_EK.sql
@@ -1039,10 +1039,10 @@ BEGIN TRY
 
 				SELECT @intItemUOMId = @intLotItemUOMId
 
-				Update tblICItemLocation
-				Set intAllowNegativeInventory=1
-				WHERE intItemId =@intItemId
-				AND intLocationId =@intCompanyLocationId 
+				--Update tblICItemLocation
+				--Set intAllowNegativeInventory=1
+				--WHERE intItemId =@intItemId
+				--AND intLocationId =@intCompanyLocationId 
 
 				EXEC dbo.uspMFLotMove @intLotId = @intLotId
 					,@intNewSubLocationId = @intCompanyLocationNewSubLocationId
@@ -1063,10 +1063,10 @@ BEGIN TRY
 					,@intAdjustmentId = @intAdjustmentId OUTPUT
 					,@ysnExternalSystemMove = 1
 
-				Update tblICItemLocation
-				Set intAllowNegativeInventory=3
-				WHERE intItemId =@intItemId
-				AND intLocationId =@intCompanyLocationId 
+				--Update tblICItemLocation
+				--Set intAllowNegativeInventory=3
+				--WHERE intItemId =@intItemId
+				--AND intLocationId =@intCompanyLocationId 
 
 				SELECT @strAdjustmentNo = NULL
 
