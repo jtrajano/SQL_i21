604be1de126212f682b6a6cc529d9a7627e18b1f
Jase Rueda <jase.rueda@iRely.com>
2023-03-20 14:38:34 +0800

    CS-367

---
+++ i21Database/Scripts/Post-Deployment/ST/52_Populate_JobTypes.sql
@@ -0,0 +1,15 @@
+﻿GO
+	PRINT N'BEGIN INSERT STORE JOB TYPES'
+GO
+	IF NOT EXISTS(SELECT '' FROM tblSTJobTypes where  strJobType = 'Reboot')
+	BEGIN
+		INSERT [dbo].[tblSTJobTypes] ([strJobType], [intConcurrencyId]) VALUES ( 'Reboot', 1)
+	END
+	
+	IF NOT EXISTS(SELECT '' FROM tblSTJobTypes where strJobType = 'Poll Files')
+	BEGIN
+		INSERT [dbo].[tblSTJobTypes] ([strJobType], [intConcurrencyId]) VALUES ( 'Poll Files', 1)
+	END
+GO
+	PRINT N'END INSERT STORE JOB TYPES'
+GO
---
+++ i21Database/dbo/Views/vyuSTJobs.sql
@@ -0,0 +1,18 @@
+CREATE VIEW [dbo].[vyuSTJobs]
+AS
+SELECT		Job.intJobId,
+			Job.intStoreId,
+			JobType.strJobType,
+			Job.strParameter1,
+			Job.strParameter2,
+			register.strRegisterClass,
+			register.strSAPPHIREUserName,
+			register.strSAPPHIREPassword
+FROM		tblSTJobs Job
+INNER JOIN	tblSTJobTypes JobType
+ON			Job.intJobTypeId = JobType.intJobTypeId
+INNER JOIN	tblSTStore store
+ON			Job.intStoreId = store.intStoreId
+LEFT JOIN	tblSTRegister register
+ON			store.intRegisterId = register.intRegisterId
+WHERE		Job.ysnJobReceived = 0
