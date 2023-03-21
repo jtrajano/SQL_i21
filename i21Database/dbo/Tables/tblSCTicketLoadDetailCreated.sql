aeb1619fe99b761638660208d41675a5de5d00e4
Mon Gonzales <mon.gonzales@irely.com>
2023-03-21 14:16:37 +0800

    SC-4821 - HDTN-279841 - SubSow 14- The ability to create loads without a contract and assign contract number at the scale

---
+++ i21Database/dbo/Tables/tblSCTicketLoadDetailCreated.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[tblSCTicketLoadDetailCreated]
+(
+	[intTicketLoadDetailCreatedId] INT NOT NULL IDENTITY, 
+    [intTicketId] INT NULL, 
+    [intLoadDetailId] INT NULL, 
+    [dblQty] DECIMAL(18, 6) NULL,
+	CONSTRAINT [PK_tblSCTicketLoadDetailCreated_intTicketId] PRIMARY KEY ([intTicketLoadDetailCreatedId]), 
+    CONSTRAINT [FK_tblSCTicketLoadDetailCreated_tblSCTicket_intTicketId] FOREIGN KEY (intTicketId) REFERENCES [tblSCTicket](intTicketId),
+    CONSTRAINT [FK_tblSCTicketLoadDetailCreated_tblLGLoadDetail_intLoadDetailId] FOREIGN KEY (intLoadDetailId) REFERENCES [tblLGLoadDetail](intLoadDetailId),
+)
