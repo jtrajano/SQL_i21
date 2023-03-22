38e19187e9ff8568fa251909afc217af84128605
radatienza <rad.atienza@iRely.com>
2023-03-22 10:53:25 +0800

    FRM-10831

---
+++ i21Database/dbo/User Defined Types/BatchAuditLogParamNested.sql
@@ -0,0 +1,8 @@
+﻿CREATE TYPE BatchAuditLogParamNested AS TABLE ( 
+  [Id]			INT, 
+  [Action]		NVARCHAR(MAX),
+  [Description]	NVARCHAR(MAX),
+  [From]		NVARCHAR(MAX),
+  [To]			NVARCHAR(MAX),
+  [ParentId]	INT
+)
