ae37e5bc3da54ebcb15439315cee48e1d29379df
Vinoth Kumar T <vinoth.t@irely.com>
2023-03-23 23:44:01 +0530

    IN-1168 - Merging issues from 22.2Dev

--- i21Database/dbo/Stored Procedures/uspIPProcessOrdersToFeed.sql
+++ i21Database/dbo/Stored Procedures/uspIPProcessOrdersToFeed.sql
@@ -25,10 +25,7 @@ BEGIN TRY
 	JOIN dbo.tblARMarketZone MZ WITH (NOLOCK) ON MZ.intMarketZoneId = L.intMarketZoneId
 	WHERE intLoadId = @intLoadId
 
-	IF ISNULL(@strMarketZoneCode, '') IN (
-			'AUC'
-			,'SPT'
-			)
+	IF EXISTS (SELECT *FROM tblLGLoadDetail WHERE intLoadDetailId=@intLoadDetailId AND intPContractDetailId IS NULL)
 	BEGIN
 		INSERT INTO tblIPContractFeed
 		(
