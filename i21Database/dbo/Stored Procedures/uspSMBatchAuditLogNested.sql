d618f011918d40a28cce65567cba6d9eb023ecf3
radatienza <rad.atienza@iRely.com>
2023-03-24 21:34:55 +0800

    FRM-10831

--- i21Database/dbo/Stored Procedures/uspSMBatchAuditLogNested.sql
+++ i21Database/dbo/Stored Procedures/uspSMBatchAuditLogNested.sql
@@ -3,17 +3,16 @@
 ---------------------------------------------------------------------------------------------------------------------------------------
 CREATE PROCEDURE [dbo].[uspSMBatchAuditLogNested]
 	@strScreenName		NVARCHAR(255),
-	@intRecordId		INT,
 	@intEntityId		INT,
 	@tblAuditLogParam	BatchAuditLogParamNested READONLY
 AS 
 
-	SET QUOTED_IDENTIFIER OFF
-	SET ANSI_NULLS ON
-	SET NOCOUNT ON
-	SET XACT_ABORT ON
+SET QUOTED_IDENTIFIER OFF
+SET ANSI_NULLS ON
+SET NOCOUNT ON
+SET XACT_ABORT ON
 
-	BEGIN TRY
+BEGIN TRY
 	--=====================================================================================================================================
 	-- 	VARIABLE DECLARATION
 	---------------------------------------------------------------------------------------------------------------------------------------
@@ -21,9 +20,12 @@ AS
 	DECLARE @error NVARCHAR(255)
 	DECLARE @intTransactionId	INT  
 	DECLARE @intLogId			INT  
-	DECLARE @ysnWithDuplicateId	TINYINT = 0
-	DECLARE @ysnParentIdNotExists	TINYINT = 0
-	DECLARE @insertedPKTable TABLE (intId INT,	tempKey INT)
+	DECLARE @ysnWithDuplicateId	INT = 0
+	DECLARE @ysnParentIdNotExists	INT = 0
+	DECLARE @insertedPKTable TABLE (intId INT,	intLogId INT, tempKey INT)
+	DECLARE @insertedSMLogHeader TABLE (intLogId INT,	intTransactionId INT)
+	DECLARE @intRecordId INT
+
 	--=====================================================================================================================================
 
 
@@ -31,80 +33,116 @@ AS
 	--=====================================================================================================================================
 	-- 	INSERT TO TRANSACTION TABLES THAT ARE NOT YET EXISTING
 	---------------------------------------------------------------------------------------------------------------------------------------
-		EXEC uspSMInsertTransaction @screenNamespace = @strScreenName, @intKeyValue = @intRecordId, @output = @intTransactionId OUTPUT  
-  
-		INSERT INTO tblSMLog (strType, dtmDate, intEntityId, intTransactionId, intConcurrencyId)   
-		VALUES('Audit', GETUTCDATE(), @intEntityId, @intTransactionId, 1)  
-
-		SET @intLogId = SCOPE_IDENTITY()  
-		---------------------------------------------------------------------------------------------------------------------------------------
-		-- Validations
-		---------------------------------------------------------------------------------------------------------------------------------------
-		SELECT	TOP 1 @ysnWithDuplicateId = COUNT(Id)
-		FROM	@tblAuditLogParam 
-		group by Id
-		having count(Id) > 1
-
-		select	@ysnParentIdNotExists = COUNT(Id)
-		from	@tblAuditLogParam
-		WHERE	ParentId NOT IN (SELECT Id FROM @tblAuditLogParam)
-
-
-		IF ISNULL(@ysnWithDuplicateId, 0) = 0 AND @ysnParentIdNotExists = 0
-		BEGIN
-
-			--=====================================================================================================================================
-			-- INSERT tblSMAudit data
-			---------------------------------------------------------------------------------------------------------------------------------------
-
-			INSERT INTO dbo.tblSMAudit (intLogId, intKeyValue, strAction, strChange, strFrom, strTo,  
-										intParentAuditId, intConcurrencyId)
-			OUTPUT		INSERTED.intAuditId, INSERTED.intKeyValue INTO @insertedPKTable
-			SELECT		@intLogId, [Id], [Action], [Description], [From], [To], [ParentId], 1
-			FROM		@tblAuditLogParam
-
-			--=====================================================================================================================================
-			-- Update Parent Id values with new audit Id inserted
-			---------------------------------------------------------------------------------------------------------------------------------------
-			UPDATE		a
-			SET			a.intParentAuditId = b.intId,
-						a.intKeyValue = @intRecordId
-			FROM		tblSMAudit AS a
-						INNER JOIN @insertedPKTable AS b ON
-						a.intParentAuditId = b.tempKey AND
-						a.intAuditId IN(select intId FROM @insertedPKTable)
-
-		END
-		ELSE
-		BEGIN
-
-			SET @error =  'Error encountered. Please make sure Ids are unique and parent Id provided exists';
-			RAISERROR(@error, 16, 1);
-			RETURN;
 		
-		END
-		IF @transCount = 0 COMMIT TRANSACTION
+	INSERT INTO tblSMTransaction (intScreenId, intRecordId, intConcurrencyId)
+	SELECT	b.intScreenId, a.RecordId, 1 
+	FROM	@tblAuditLogParam			AS a
+			LEFT JOIN tblSMTransaction	AS b 
+			ON b.intRecordId = a.RecordId
+			LEFT JOIN tblSMScreen		AS c ON
+			b.intScreenId = c.intScreenId
+	WHERE	ISNULL(b.intTransactionId, 0) = 0 AND
+			c.strScreenName = @strScreenName
+  
+	---------------------------------------------------------------------------------------------------------------------------------------
+	-- Validations
+	---------------------------------------------------------------------------------------------------------------------------------------
+	SELECT	TOP 1 @ysnWithDuplicateId = COUNT(Id)
+	FROM	@tblAuditLogParam
+	group by RecordId, Id
+	having count(Id) > 1
 
-	END TRY
-	BEGIN CATCH
-		DECLARE @ErrorSeverity INT,
-				@ErrorNumber   INT,
-				@ErrorMessage nvarchar(4000),
-				@ErrorState INT,
-				@ErrorLine  INT,
-				@ErrorProc nvarchar(200);
+		
+	select	@ysnParentIdNotExists = COUNT(*)
+	from	@tblAuditLogParam AS childTable
+			LEFT JOIN @tblAuditLogParam AS rootTable ON
+			-- child.intParentAuditId = root.intAuditId
+			childTable.ParentId = rootTable.Id
+	WHERE	childTable.ParentId IS NOT NULL AND
+			rootTable.Id is null
 
-		-- Grab error information from SQL functions
-		SET @ErrorSeverity = ERROR_SEVERITY()
-		SET @ErrorNumber   = ERROR_NUMBER()
-		SET @ErrorMessage  = ERROR_MESSAGE()
-		SET @ErrorState    = ERROR_STATE()
-		SET @ErrorLine     = ERROR_LINE()
 
-		IF @transCount = 0 AND XACT_STATE() <> 0 ROLLBACK TRANSACTION
 
-		SET @error = @ErrorMessage;
-		RAISERROR (@ErrorMessage , @ErrorSeverity, @ErrorState, @ErrorNumber)
-	END CATCH
+	IF ISNULL(@ysnWithDuplicateId, 0) = 0 AND @ysnParentIdNotExists = 0
+	BEGIN
+
+		----=====================================================================================================================================
+		---- INSERT tblSMLog data
+		-----------------------------------------------------------------------------------------------------------------------------------------
+		INSERT INTO tblSMLog (strType, dtmDate, intEntityId, intTransactionId, intConcurrencyId)   
+		OUTPUT		INSERTED.intLogId, INSERTED.intTransactionId INTO @insertedSMLogHeader
+		SELECT	'Audit', GETUTCDATE(), @intEntityId, b.intTransactionId, 1
+		FROM	(select distinct RecordId from @tblAuditLogParam) AS a
+				INNER JOIN tblSMTransaction AS b ON
+				a.RecordId = b.intRecordId
+				INNER JOIN tblSMScreen AS c ON
+				b.intScreenId = c.intScreenId
+		WHERE	c.strNamespace = @strScreenName
+			
+		----=====================================================================================================================================
+		---- INSERT tblSMAudit data
+		-----------------------------------------------------------------------------------------------------------------------------------------
+
+		INSERT INTO tblSMAudit (intLogId, intKeyValue, strAction, strFrom, strTo, intParentAuditId, intConcurrencyId)
+		OUTPUT		INSERTED.intAuditId, INSERTED.intLogId, INSERTED.intKeyValue INTO @insertedPKTable
+		SELECT		c.intLogId,
+					a.[Id],
+					a.[Action],
+					a.[From],
+					a.[To],
+					a.[ParentId],
+					1
+			FROM	@tblAuditLogParam AS a
+					inner join tblSMTransaction AS b ON
+					a.RecordId = b.intRecordId
+					INNER JOIN @insertedSMLogHeader AS c ON
+					b.intTransactionId = c.intTransactionId
+
+		--=====================================================================================================================================
+		-- Update Parent Id values with new audit Id inserted
+		---------------------------------------------------------------------------------------------------------------------------------------
+		UPDATE		a
+		SET			a.intParentAuditId = b.intId,
+					a.intKeyValue = NULL
+		FROM		tblSMAudit AS a
+						
+					INNER JOIN @insertedPKTable as b ON
+					a.intLogId = b.intLogId
+
+		WHERE		a.intAuditId IN(select intId FROM @insertedPKTable)  AND
+					a.intParentAuditId = b.tempKey
+
+	END
+	ELSE
+	BEGIN
+
+		SET @error =  'Error encountered. Please make sure Ids are unique and parent Id provided exists';
+		RAISERROR(@error, 16, 1);
+		RETURN;
+		
+	END
+	IF @transCount = 0 COMMIT TRANSACTION
+
+END TRY
+BEGIN CATCH
+	DECLARE @ErrorSeverity INT,
+			@ErrorNumber   INT,
+			@ErrorMessage nvarchar(4000),
+			@ErrorState INT,
+			@ErrorLine  INT,
+			@ErrorProc nvarchar(200);
+
+	-- Grab error information from SQL functions
+	SET @ErrorSeverity = ERROR_SEVERITY()
+	SET @ErrorNumber   = ERROR_NUMBER()
+	SET @ErrorMessage  = ERROR_MESSAGE()
+	SET @ErrorState    = ERROR_STATE()
+	SET @ErrorLine     = ERROR_LINE()
+
+	IF @transCount = 0 AND XACT_STATE() <> 0 ROLLBACK TRANSACTION
+
+	SET @error = @ErrorMessage;
+	RAISERROR (@ErrorMessage , @ErrorSeverity, @ErrorState, @ErrorNumber)
+END CATCH
 
 GO
--- i21Database/dbo/User Defined Types/BatchAuditLogParamNested.sql
+++ i21Database/dbo/User Defined Types/BatchAuditLogParamNested.sql
@@ -1,5 +1,6 @@
 ﻿CREATE TYPE BatchAuditLogParamNested AS TABLE ( 
   [Id]			INT, 
+  [RecordId]	INT,
   [Action]		NVARCHAR(MAX),
   [Description]	NVARCHAR(MAX),
   [From]		NVARCHAR(MAX),
