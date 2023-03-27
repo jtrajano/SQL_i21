a1d3a6b2de0ffa9c390f2c1b7035b1fc4260c05a
Kenneth Lew Lagunero <kenneth.lagunero@irely.com>
2023-03-27 10:42:00 +0800

    TM-3747
    # Conflicts:
    #	i21Database/i21Database.sqlproj

---
+++ i21Database/dbo/Tables/tblTMDispatchHistorySource.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[tblTMDispatchHistorySource] (
+    [intDispatchHistorySourceId]	INT				IDENTITY (1, 1) NOT NULL,
+    [intDispatchSyncId]				INT				NOT NULL,
+    [intSourceType]					INT				NOT NULL,		--	Valid Values
+																	--	1-"Invoice"
+																	--	2-"Transport Load"
+	[intConcurrencyId]				INT				DEFAULT 1 NOT NULL,
+    CONSTRAINT [PK_tblTMDispatchHistorySource] PRIMARY KEY CLUSTERED ([intDispatchHistorySourceId] ASC),
+    CONSTRAINT [FK_tblTMDispatchHistorySource_tblTMDispatchHistory_intDispatchSyncId] FOREIGN KEY ([intDispatchSyncId]) REFERENCES [tblTMDispatchHistory]([intDispatchSyncId]) ON DELETE CASCADE
+);
+GO
+
+CREATE INDEX [IX_tblTMDispatchHistorySource_intDispatchSyncId] ON [dbo].[tblTMDispatchHistorySource] ([intDispatchSyncId]) 
+
+GO
