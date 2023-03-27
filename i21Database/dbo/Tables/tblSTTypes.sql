b6b55496d052d2a2a2fdbe5b5f8bec5c46cd6d02
Renze Cuadra <renze.cuadra@iRely.com>
2023-03-27 12:04:16 +0800

    CS-348

--- i21Database/dbo/Tables/tblSTTypes.sql
+++ i21Database/dbo/Tables/tblSTTypes.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[tblSTJobTypes]
 (
     [intJobTypeId] INT NOT NULL IDENTITY,
-    [strJobType] NVARCHAR(20) COLLATE Latin1_General_CI_AS NOT NULL,
+    [strJobType] NVARCHAR(150) COLLATE Latin1_General_CI_AS NOT NULL,
     [intConcurrencyId] INT NULL,
     CONSTRAINT [PK_tblSTJobTypes] PRIMARY KEY CLUSTERED ([intJobTypeId] ASC)
 )
