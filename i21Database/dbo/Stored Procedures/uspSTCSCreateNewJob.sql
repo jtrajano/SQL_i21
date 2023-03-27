9b08b9ba5168a2668fd913dfebe7a52a1daf7e29
Renze Cuadra <renze.cuadra@iRely.com>
2023-03-28 07:59:04 +0800

    CS-348

---
+++ i21Database/dbo/Stored Procedures/uspSTCSCreateNewJob.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[uspSTCSCreateNewJob] (
+	@intStoreId INT,
+	@intJobTypeId INT,
+	@strParameter1 NVARCHAR(150),
+	@strParameter2 NVARCHAR(150),
+	@strParameter3 NVARCHAR(150),
+	@dtmJobCreated DATETIME,
+	@intEntityId INT,
+	@intJobId INT OUT
+)
+AS
+SET NOCOUNT ON;
+
+INSERT INTO tblSTJobs (intStoreId, intJobTypeId, dtmJobCreated, strParameter1, strParameter2, strParameter3, ysnJobReceived, intConcurrencyId, intEntityId) 
+			   VALUES (@intStoreId, @intJobTypeId, @dtmJobCreated, @strParameter1, @strParameter2, @strParameter3, 0, 1, @intEntityId)
+SET @intJobId = (SELECT TOP 1 intJobId FROM tblSTJobs WHERE intStoreId = @intStoreId AND intJobTypeId = @intJobTypeId AND strParameter2 = @strParameter2 ORDER BY intJobId DESC)
