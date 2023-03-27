464cb93ba0ee1ac7acf32dca05cbddcafe9ba3f1
Kenneth Lew Lagunero <kenneth.lagunero@irely.com>
2023-03-27 10:42:00 +0800

    TM-3747
    # Conflicts:
    #	i21Database/i21Database.sqlproj
    
    # Conflicts:
    #	i21Database/i21Database.sqlproj

---
+++ i21Database/dbo/User Defined Types/TMOrderHistoryStagingTable.sql
@@ -0,0 +1,9 @@
+﻿CREATE TYPE [dbo].[TMOrderHistoryStagingTable] AS TABLE
+(
+	 [intDispatchId]					INT	NOT NULL				-- Order/Dispatch Id
+	,[ysnDelete]						BIT	DEFAULT 1 NOT NULL		-- Delete/Restore Record
+	,[intSourceType]					INT NULL					--	Valid Values
+																	--	1-"Invoice"
+																	--	2-"Transport Load"
+	,[intDeliveryHistoryId]				INT NULL	
+)
