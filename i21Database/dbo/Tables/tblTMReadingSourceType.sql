88741b92179c368193f5d5ffc2f87db2e62040db
DESKTOP-VFHGDG0\iRelyNUC015 <denny.divinagracia@irely.com>
2023-03-28 17:33:29 +0800

    Fixes for conflict after cherry pick

---
+++ i21Database/dbo/Tables/tblTMReadingSourceType.sql
@@ -0,0 +1,13 @@
+CREATE TABLE [dbo].[tblTMReadingSourceType]
+(
+    [intReadingSourceTypeId] INT IDENTITY(1,1) NOT NULL,
+    [strReadingSourceType] NVARCHAR(70) COLLATE Latin1_General_CI_AS NULL,
+    [intConcurrencyId] INT NOT NULL DEFAULT(0),
+    [ysnDefault] BIT DEFAULT 0 NOT NULL,
+    CONSTRAINT [PK_dbo.tblTMReadingSourceType_intReadingSourceTypeId] PRIMARY KEY CLUSTERED ([intReadingSourceTypeId] ASC)
+)
+
+
+
+
+
