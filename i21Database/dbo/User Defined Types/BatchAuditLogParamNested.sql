23639ab2cf2f1daa3156e30b71c4aee19060211c
radatienza <rad.atienza@iRely.com>
2023-03-28 18:10:08 +0800

    Revert "FRM-10831"
    This reverts commit 44364bb1165c6687c890344ed859e32ab29d77a3.

--- i21Database/dbo/User Defined Types/BatchAuditLogParamNested.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TYPE BatchAuditLogParamNested AS TABLE ( 
-  [Id]			INT, 
-  [RecordId]	INT,
-  [Action]		NVARCHAR(MAX),
-  [Description]	NVARCHAR(MAX),
-  [From]		NVARCHAR(MAX),
-  [To]			NVARCHAR(MAX),
-  [ParentId]	INT
-)
