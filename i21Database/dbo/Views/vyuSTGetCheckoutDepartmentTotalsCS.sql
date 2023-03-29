7e766646a1911ddcb04f246da36b5746ff75f149
Renze Cuadra <renze.cuadra@iRely.com>
2023-03-29 16:34:31 +0800

    CS-341

---
+++ i21Database/dbo/Views/vyuSTGetCheckoutDepartmentTotalsCS.sql
@@ -0,0 +1,27 @@
+﻿CREATE VIEW [dbo].[vyuSTGetCheckoutDepartmentTotalsCS]
+AS
+SELECT CDT.*
+	, StoreDepartments.strRegisterCode AS strCashRegisterDepartment
+	, Cat.strCategoryCode
+	, Cat.strDescription AS strCategoryDesc
+	, CASE
+		WHEN StoreDepartments.strCategoriesOrSubcategories = 'C'
+		THEN StoreDepartments.strGeneralItemDescription
+		ELSE StoreDepartments.strSubcategoryItemDescription
+		END strItemDescription
+	, CASE
+		WHEN StoreDepartments.strCategoriesOrSubcategories = 'C'
+		THEN StoreDepartments.strGeneralItemNo
+		ELSE StoreDepartments.strSubcategoryItemNo
+		END AS strItemNo
+FROM tblSTCheckoutDepartmetTotals CDT
+INNER JOIN dbo.tblSTCheckoutHeader CH
+	ON CDT.intCheckoutId = CH.intCheckoutId
+INNER JOIN dbo.tblSTStore AS ST 
+	ON CH.intStoreId = ST.intStoreId
+INNER JOIN tblICCategory Cat
+	ON CDT.intCategoryId = Cat.intCategoryId
+INNER JOIN dbo.vyuSTStoreDepartments AS StoreDepartments 
+	ON StoreDepartments.intCategoryId = Cat.intCategoryId
+	AND StoreDepartments.strRegisterCode = CDT.strRegisterCode
+	AND StoreDepartments.intStoreId = ST.intStoreId
