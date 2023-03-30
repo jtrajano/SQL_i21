239d8665892fee6c8b6d9fdee2d1603afd1ced31
Jeff Trajano <jeffrey.trajano@irely.com>
2023-03-30 12:01:40 +0800

    GL-10210

--- i21Database/dbo/Stored Procedures/uspGLGenerateAuditorTransactionsByAccountId.sql
+++ i21Database/dbo/Stored Procedures/uspGLGenerateAuditorTransactionsByAccountId.sql
@@ -190,12 +190,12 @@ BEGIN
                             , strTotalTitle
                             , strGroupTitle
                             , intEntityId
-                            , dblDebit
-                            , dblCredit
-                            , dblEndingBalance
-                            , dblDebitForeign
-                            , dblCreditForeign
-                            , dblEndingBalanceForeign
+                            --, dblDebit
+                            --, dblCredit
+                            , dblBeginningBalance
+                            --, dblDebitForeign
+                            --, dblCreditForeign
+                            , dblBeginningBalanceForeign
                             , strCurrency
                             , strAccountId
                             , strLocation
@@ -212,11 +212,11 @@ BEGIN
                                 , 'Beginning Balance'
                                 , 'Account ID: ' + strAccountId + ', Currency: ' + strCurrency
                                 , @intEntityId
-                                , @beginBalanceDebit
-                                , @beginBalanceCredit
+                                --, @beginBalanceDebit
+                                --, @beginBalanceCredit
                                 , @beginBalance
-                                , @beginBalanceDebitForeign
-                                , @beginBalanceCreditForeign     
+                                --, @beginBalanceDebitForeign
+                                --, @beginBalanceCreditForeign     
                                 , @beginBalanceForeign            
                                 , strCurrency
                                 , strAccountId
