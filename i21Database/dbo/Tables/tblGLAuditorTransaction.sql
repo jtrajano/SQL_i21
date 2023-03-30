ce66b149be7350a56a5a21fffcec51f39a450ee6
Jeff Trajano <jeffrey.trajano@irely.com>
2023-03-30 19:22:04 +0800

    GL-10199

--- i21Database/dbo/Tables/tblGLAuditorTransaction.sql
+++ i21Database/dbo/Tables/tblGLAuditorTransaction.sql
@@ -61,5 +61,8 @@
 	[strAccountDescription]  NVARCHAR (100) COLLATE Latin1_General_CI_AS NULL,
 	[ysnGroupHeader] BIT NULL,
 	[ysnGroupFooter] BIT NULL,
+	[ysnSummary] BIT NULL,
+	[ysnSummaryFooter] BIT NULL,
+	[ysnSpace] BIT NULL,
 	CONSTRAINT [PK_tblGLAuditorTransaction] PRIMARY KEY CLUSTERED ([intAuditorTransactionId] ASC)
 )
