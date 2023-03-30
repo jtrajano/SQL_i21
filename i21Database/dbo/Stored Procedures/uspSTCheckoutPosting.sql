16d5e6ca09602db85cb013f4e33b553c9d2dcc40
Jase Rueda <jase.rueda@iRely.com>
2023-03-30 11:11:43 +0800

    CS-388

--- i21Database/dbo/Stored Procedures/uspSTCheckoutPosting.sql
+++ i21Database/dbo/Stored Procedures/uspSTCheckoutPosting.sql
@@ -6235,7 +6235,7 @@ BEGIN
 									END as ysnManual,
 								siteDevice.intSiteID,
 								fuelInventory.intDeviceId,
-								tankMonitorDevice.intDeviceId as intTankMonitorDeviceId,
+								siteDeviceTankMonitor.intDeviceTankMonitorId,
 								tankMonitorDevice.strSerialNumber                
 				FROM			tblSTCheckoutFuelInventory fuelInventory
 				INNER JOIN		tblTMDevice device
@@ -6246,8 +6246,10 @@ BEGIN
 				ON				fuelInventory.intDeviceId = siteDevice.intDeviceId
 				LEFT JOIN		tblTMSiteDeviceTankMonitor siteDeviceTankMonitor
 				ON				siteDevice.intSiteID = siteDeviceTankMonitor.intSiteId
+				LEFT JOIN		tblTMDeviceTankMonitor deviceTankMonitor
+				ON				siteDeviceTankMonitor.intDeviceTankMonitorId = deviceTankMonitor.intDeviceTankMonitorId
 				LEFT JOIN		tblTMDevice tankMonitorDevice
-				ON				tankMonitorDevice.intDeviceId = siteDeviceTankMonitor.intDeviceTankMonitorId
+				ON				tankMonitorDevice.intDeviceId = deviceTankMonitor.intDeviceId
                 WHERE			fuelInventory.intCheckoutId = @intCheckoutId
 				------------------------------------------------------
 				---------- TRANSFER TANK READINGS TO TM  -------------
