ce70c82523245165524eb44c2d7ca75fa296d6d3
Renze Cuadra <renze.cuadra@iRely.com>
2023-03-30 17:45:38 +0800

    CS-341

--- i21Database/dbo/Functions/fnSTGetDepartmentTotalsForFuel.sql
+++ i21Database/dbo/Functions/fnSTGetDepartmentTotalsForFuel.sql
@@ -7,19 +7,51 @@ AS BEGIN
 
     DECLARE     @dblDepartmentTotalsForFuel DECIMAL(18,2) = 0
 
-    SELECT      @dblDepartmentTotalsForFuel = ISNULL(SUM(dblTotalSalesAmountComputed),0)
-    FROM        tblSTCheckoutDepartmetTotals a
-    WHERE       a.intCheckoutId = @intCheckoutId AND
-				(
-				a.intCategoryId IN (    SELECT      intCategoryId 
-                                        FROM        tblSTPumpItem
-									    WHERE       intStoreId IN (SELECT intStoreId FROM tblSTCheckoutHeader WHERE intCheckoutId = @intCheckoutId))
-				OR
-				a.intSubcategoriesId IN (
-										SELECT		intSubcategoriesId
-										FROM		tblSTStoreDepartments
-										WHERE		intStoreId IN (SELECT intStoreId FROM tblSTCheckoutHeader WHERE intCheckoutId = @intCheckoutId))
-				)
+	SELECT      @dblDepartmentTotalsForFuel = ISNULL(SUM(dblTotalSalesAmountComputed),0)
+	FROM        tblSTCheckoutDepartmetTotals a
+	LEFT JOIN	tblICItem b
+	ON          a.intItemId = b.intItemId
+	INNER JOIN  tblSTCheckoutHeader ch
+	ON			a.intCheckoutId = ch.intCheckoutId
+	INNER JOIN  tblSTStore st
+	ON			ch.intStoreId = st.intStoreId
+	WHERE       a.intCheckoutId = @intCheckoutId 
+	AND
+	a.intCategoryId IN
+	(CASE WHEN st.strDepartmentOrCategory = 'C' AND st.strCategoriesOrSubcategories = 'C' 
+		THEN 
+			(
+				SELECT		intCategoryId
+				FROM		tblSTStoreDepartments
+				WHERE		intStoreId IN (SELECT ISNULL(intStoreId,0) FROM tblSTCheckoutHeader WHERE intCheckoutId = @intCheckoutId)
+			
+			)
+	WHEN st.strDepartmentOrCategory = 'D' AND st.strCategoriesOrSubcategories = 'C' 
+		THEN
+			(
+				SELECT      intCategoryId 
+				FROM        tblSTPumpItem
+				WHERE       intStoreId IN (SELECT intStoreId FROM tblSTCheckoutHeader WHERE intCheckoutId = @intCheckoutId)
+			)
+	END)
+	OR
+	a.intSubcategoriesId IN
+	(CASE WHEN st.strDepartmentOrCategory = 'C' AND st.strCategoriesOrSubcategories = 'S' 
+		THEN 
+			(
+				SELECT		intSubcategoriesId
+				FROM		tblSTStoreDepartments
+				WHERE		intStoreId IN (SELECT ISNULL(intStoreId,0) FROM tblSTCheckoutHeader WHERE intCheckoutId = @intCheckoutId)
+			
+			)
+	WHEN st.strDepartmentOrCategory = 'D' AND st.strCategoriesOrSubcategories = 'S' 
+		THEN
+			(
+				SELECT      intCategoryId 
+				FROM        tblSTPumpItem
+				WHERE       intStoreId IN (SELECT intStoreId FROM tblSTCheckoutHeader WHERE intCheckoutId = @intCheckoutId)
+			)
+	END)
 
     RETURN      @dblDepartmentTotalsForFuel
 END
