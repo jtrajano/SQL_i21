c7709f805acdd7f3f0550a2c9ad7550db70b9dca
ranelle.vicente@irely.com <ranelle.vicente@irely.com>
2023-03-30 13:56:35 +0800

    AR-16687

--- i21PreDeployment/Scripts/Pre-Deployment/7_AR/6_PaymentDetailInvoiceCleanUp.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/7_AR/6_PaymentDetailInvoiceCleanUp.sql
@@ -51,3 +51,49 @@ END
 
 PRINT '********************** END - FIX INVALID PAYMENT LOCATION **********************'
 GO
+
+PRINT '********************** BEGIN - FIX INVALID PAYMENT METHOD **********************'
+GO
+ 
+IF EXISTS (SELECT TOP 1 1 FROM sys.columns WHERE [name] = N'intPaymentMethodId' AND [object_id] = OBJECT_ID(N'tblARPayment'))
+  AND EXISTS (SELECT TOP 1 1 FROM sys.columns WHERE [name] = N'intPaymentMethodID' AND [object_id] = OBJECT_ID(N'tblSMPaymentMethod'))
+  AND EXISTS (SELECT TOP 1 1 FROM sys.columns WHERE [name] = N'strPaymentMethod' AND [object_id] = OBJECT_ID(N'tblCMUndepositedFund'))
+BEGIN
+ IF OBJECT_ID('tempdb..#PAYMENTS') IS NOT NULL DROP TABLE #PAYMENTS
+ 
+ DECLARE @intPaymentMethodId INT    = NULL
+    , @strPaymentMethod NVARCHAR(100) = NULL
+ 
+ SELECT TOP 1 @intPaymentMethodId = intPaymentMethodID
+      , @strPaymentMethod  = strPaymentMethod
+ FROM tblSMPaymentMethod
+ ORDER BY intPaymentMethodID ASC
+ 
+ IF @intPaymentMethodId IS NOT NULL
+  BEGIN
+   SELECT intPaymentId
+     , strRecordNumber
+     , strPaymentMethod
+   INTO #PAYMENTS
+   FROM tblARPayment P
+   WHERE intPaymentMethodId = 0
+ 
+   UPDATE P
+   SET intPaymentMethodId = CASE WHEN PM.intPaymentMethodID IS NULL THEN @intPaymentMethodId ELSE PM.intPaymentMethodID END
+     , strPaymentMethod = CASE WHEN PM.intPaymentMethodID IS NULL THEN @strPaymentMethod ELSE PM.strPaymentMethod END
+   FROM tblARPayment P
+   INNER JOIN #PAYMENTS PP ON P.intPaymentId = PP.intPaymentId
+   LEFT JOIN tblSMPaymentMethod PM ON P.strPaymentMethod = PM.strPaymentMethod 
+ 
+   UPDATE UF
+   SET strPaymentMethod = P.strPaymentMethod
+   FROM tblCMUndepositedFund UF
+   INNER JOIN #PAYMENTS PP ON UF.intSourceTransactionId = PP.intPaymentId AND UF.strSourceTransactionId = PP.strRecordNumber
+   INNER JOIN tblARPayment P ON P.intPaymentId = PP.intPaymentId
+  END
+ 
+ IF OBJECT_ID('tempdb..#PAYMENTS') IS NOT NULL DROP TABLE #PAYMENTS
+END
+ 
+PRINT '********************** END - FIX INVALID PAYMENT METHOD **********************'
+GO
