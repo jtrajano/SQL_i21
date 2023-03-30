fe4a39bad99554290a640613d344b6b3c5e5fd64
ranelle.vicente@irely.com <ranelle.vicente@irely.com>
2023-03-30 11:59:26 +0800

    AR-16687

--- i21Database/dbo/Tables/tblARPayment.sql
+++ i21Database/dbo/Tables/tblARPayment.sql
@@ -48,7 +48,7 @@
 	CONSTRAINT [FK_tblARPayment_tblEMEntityCardInformation_intEntityCardInfoId] FOREIGN KEY ([intEntityCardInfoId]) REFERENCES [dbo].[tblEMEntityCardInformation] ([intEntityCardInfoId]),
 	CONSTRAINT [FK_tblARPayment_tblSMCurrencyExchangeRateType_intCurrencyExchangeRateTypeId] FOREIGN KEY ([intCurrencyExchangeRateTypeId]) REFERENCES [dbo].[tblSMCurrencyExchangeRateType] ([intCurrencyExchangeRateTypeId]),
 	CONSTRAINT [FK_tblARPayment_tblSMCompanyLocation_intLocationId] FOREIGN KEY ([intLocationId]) REFERENCES [dbo].[tblSMCompanyLocation] ([intCompanyLocationId]),
-	CONSTRAINT [FK_tblARPayment_tblSMCurrency_intCurrencyId] FOREIGN KEY ([intCurrencyId]) REFERENCES [dbo].[tblSMCurrency]([intCurrencyID])
+	CONSTRAINT [FK_tblARPayment_tblSMCurrency_intCurrencyId] FOREIGN KEY ([intCurrencyId]) REFERENCES [dbo].[tblSMCurrency]([intCurrencyID]),
 	CONSTRAINT [FK_tblARPayment_tblEMEntityCardInformation_intPaymentMethodId] FOREIGN KEY ([intPaymentMethodId]) REFERENCES [dbo].[tblSMPaymentMethod] ([intPaymentMethodID])
 );
 --INDEXES
