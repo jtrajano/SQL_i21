f9af9bd0db98e2e3b9f2b4776fca2f7d666b47ff
Renze Cuadra <renze.cuadra@iRely.com>
2023-03-31 13:38:28 +0800

    CS-386

---
+++ i21Database/dbo/Views/vyuSTStoreMaintenanceDepartments.sql
@@ -0,0 +1,18 @@
+﻿CREATE VIEW [dbo].[vyuSTStoreMaintenanceDepartments]
+AS
+SELECT 
+intStoreDepartmentId
+,sd.intStoreId
+,sd.intCategoryId
+,strCategoryCode
+,sd.intSubcategoriesId
+,sc.strSubCategory
+,strSubCategoryCode
+,strRegisterCode
+,sd.intConcurrencyId
+FROM tblSTStoreDepartments sd
+LEFT JOIN vyuSTDepartmentTotalsPreloadCS plcs
+ON sd.intStoreId = plcs.intStoreId
+AND sd.strRegisterCode = plcs.strCashRegisterDepartment
+LEFT JOIN tblSTSubCategories sc
+ON sc.intSubcategoriesId = plcs.intSubcategoriesId
