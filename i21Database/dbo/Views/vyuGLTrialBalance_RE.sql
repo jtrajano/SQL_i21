73ec58f100d7a0a90ea162cdf933186764bf6aa4
Jeff Trajano <jeffrey.trajano@irely.com>
2023-04-03 10:23:07 +0800

    GL-10113

--- i21Database/dbo/Views/vyuGLTrialBalance_RE.sql
+++ i21Database/dbo/Views/vyuGLTrialBalance_RE.sql
@@ -14,36 +14,17 @@ WITH DETAIL as (
 		tblGLFiscalYearPeriod P on F.intFiscalYearId = P.intFiscalYearId
 )
 SELECT 
-	Fiscal.intAccountId, 
-	Fiscal.intFiscalYearId, 
-	Fiscal.intGLFiscalYearPeriodId,
-	Fiscal.PeriodStart,
-	Fiscal.FiscalStart,
-	ISNULL(ExpRev_Prior.beginningBalance,0) + ISNULL(ExpRev_Current.beginningBalance,0) YTD,
-	PERIODACTIVITY.beginningBalance MTD,
-	SM.intCurrencyId
+	Fiscal.*,
+	ISNULL(ExpRev_Prior.beginningBalance,0) YTD,
+	0 MTD,
+	ExpRev_Prior.intCurrencyId
 	--,Fiscal.strPeriod
 FROM DETAIL Fiscal
 OUTER APPLY ( 
-	SELECT  SUM(ISNULL(dblDebit,0) - ISNULL(dblCredit,0)) beginningBalance
+	SELECT  D.intCurrencyId, SUM(ISNULL(dblDebit,0) - ISNULL(dblCredit,0)) beginningBalance
 	FROM tblGLAccount A LEFT JOIN  tblGLDetail D on D.intAccountId = A.intAccountId
 	LEFT JOIN tblGLAccountGroup G ON G.intAccountGroupId = A.intAccountGroupId
-	WHERE D.dtmDate BETWEEN Fiscal.FiscalStart and PeriodEnd and D.ysnIsUnposted = 0
+	WHERE D.dtmDate < Fiscal.FiscalStart and D.ysnIsUnposted = 0
 	AND G.strAccountType in ('Expense','Revenue')  
-)ExpRev_Current
-OUTER APPLY ( 
-	SELECT  SUM(ISNULL(dblDebit,0) - ISNULL(dblCredit,0)) beginningBalance
-	FROM tblGLAccount A LEFT JOIN  tblGLDetail D on D.intAccountId = A.intAccountId
-	LEFT JOIN tblGLAccountGroup G ON G.intAccountGroupId = A.intAccountGroupId
-	WHERE D.dtmDate BETWEEN  DATEADD( DAY, FiscalDays,Fiscal.FiscalStart) AND  DATEADD(SECOND, -1, Fiscal.FiscalStart) and D.ysnIsUnposted = 0
-	AND G.strAccountType in ('Expense','Revenue')  
+	GROUP BY D.intCurrencyId
 )ExpRev_Prior
-OUTER APPLY(
-	SELECT  SUM(ISNULL(dblDebit,0) - isnull(dblCredit,0)) beginningBalance
-	FROM tblGLAccount A LEFT JOIN  tblGLDetail D on D.intAccountId = A.intAccountId
-	WHERE D.dtmDate Between Fiscal.PeriodStart and Fiscal.PeriodEnd and D.ysnIsUnposted = 0
-	AND Fiscal.intAccountId = A.intAccountId
-)PERIODACTIVITY
-outer apply(
-	select top 1 intDefaultCurrencyId intCurrencyId from tblSMCompanyPreference
-)SM
