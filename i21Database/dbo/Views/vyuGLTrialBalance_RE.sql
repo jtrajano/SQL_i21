455d11a6fc1875a7b393200696ff39adbb7e04d7
Jeff Trajano <jeffrey.trajano@irely.com>
2023-04-03 08:59:11 +0800

    GL-10113

--- i21Database/dbo/Views/vyuGLTrialBalance_RE.sql
+++ i21Database/dbo/Views/vyuGLTrialBalance_RE.sql
@@ -16,17 +16,10 @@ WITH DETAIL as (
 SELECT 
 	Fiscal.*,
 	ISNULL(ExpRev_Prior.beginningBalance,0) YTD,
-	PERIODACTIVITY.beginningBalance MTD,
+	0 MTD,
 	ExpRev_Prior.intCurrencyId
 	--,Fiscal.strPeriod
 FROM DETAIL Fiscal
--- OUTER APPLY ( 
--- 	SELECT  SUM(ISNULL(dblDebit,0) - ISNULL(dblCredit,0)) beginningBalance
--- 	FROM tblGLAccount A LEFT JOIN  tblGLDetail D on D.intAccountId = A.intAccountId
--- 	LEFT JOIN tblGLAccountGroup G ON G.intAccountGroupId = A.intAccountGroupId
--- 	WHERE D.dtmDate BETWEEN Fiscal.FiscalStart and PeriodEnd and D.ysnIsUnposted = 0
--- 	AND G.strAccountType in ('Expense','Revenue')  
--- )ExpRev_Current
 OUTER APPLY ( 
 	SELECT  D.intCurrencyId, SUM(ISNULL(dblDebit,0) - ISNULL(dblCredit,0)) beginningBalance
 	FROM tblGLAccount A LEFT JOIN  tblGLDetail D on D.intAccountId = A.intAccountId
@@ -35,11 +28,3 @@ OUTER APPLY (
 	AND G.strAccountType in ('Expense','Revenue')  
 	GROUP BY D.intCurrencyId
 )ExpRev_Prior
-OUTER APPLY(
-	SELECT  SUM(ISNULL(dblDebit,0) - isnull(dblCredit,0)) beginningBalance
-	FROM tblGLAccount A LEFT JOIN  tblGLDetail D on D.intAccountId = A.intAccountId
-	WHERE D.dtmDate Between Fiscal.PeriodStart and Fiscal.PeriodEnd and D.ysnIsUnposted = 0
-	AND Fiscal.intAccountId = A.intAccountId
-	AND ExpRev_Prior.intCurrencyId = D.intCurrencyId
-	GROUP BY A.intAccountId, D.intCurrencyId
-)PERIODACTIVITY
