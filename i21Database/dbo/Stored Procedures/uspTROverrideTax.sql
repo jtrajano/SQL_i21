a4e83f0e8978d1a9f279c83fc41b3bb616f391d7
Girome Andaya <girome.andaya@iRely.com>
2023-04-04 18:22:25 +0800

    TR-2466
    (cherry picked from commit 423b7f2d133b6d705b4308f5d3632577ea3aa88c)

--- i21Database/dbo/Stored Procedures/uspTROverrideTax.sql
+++ i21Database/dbo/Stored Procedures/uspTROverrideTax.sql
@@ -1,198 +1,415 @@
-CREATE PROCEDURE [dbo].[uspTROverrideTax]
-	@intSupplierId INT,
-	 @intSupplyPointId INT,
-	 @strReceiptState NVARCHAR(5),
-	 @intCustomerId INT,
-	 @intCustomerShipToId INT,
-	 @intShipViaId INT,
-	 @strDistributionState NVARCHAR(5),
-	 @intDistributionBulkLocationId INT,
-	 @intReceiptTaxGroupId INT OUTPUT,
-	 @strReceiptTaxGroup NVARCHAR(100) OUTPUT,
-	 @intDistributionTaxGroupId INT OUTPUT,
-	 @strDistributionTaxGroup NVARCHAR(100) OUTPUT
-AS
+CREATE PROCEDURE [dbo].[uspTROverrideTax]  
+ @intSupplierId INT,  
+  @intSupplyPointId INT,  
+  @strReceiptState NVARCHAR(5),  
+  @intCustomerId INT,  
+  @intCustomerShipToId INT,  
+  @intShipViaId INT,  
+  @strDistributionState NVARCHAR(5),  
+  @intDistributionBulkLocationId INT,  
+  @intReceiptTaxGroupId INT OUTPUT,  
+  @strReceiptTaxGroup NVARCHAR(100) OUTPUT,  
+  @intDistributionTaxGroupId INT OUTPUT,  
+  @strDistributionTaxGroup NVARCHAR(100) OUTPUT  
+AS  
+  
+SET QUOTED_IDENTIFIER OFF  
+SET ANSI_NULLS ON  
+SET NOCOUNT ON  
+SET XACT_ABORT ON  
+SET ANSI_WARNINGS ON  
+  
+BEGIN TRY  
+  
+	DECLARE @intSetupReceiptTaxGroupId INT = NULL,  
+	@intSetupDistributionTaxGroupId INT = NULL,  
+	@intSetupSupplierId INT = NULL,  
+	@intSetupSupplyPointId INT = NULL,  
+	@strSetupReceiptState NVARCHAR(5) = NULL,  
+	@intSetupCustomerId INT = NULL,  
+	@intSetupCustomerShipToId INT = NULL,  
+	@strSetupDistributionState NVARCHAR(5) = NULL,  
+	@intSetupBulkLocationId INT = NULL,  
+	@intSetupShipViaId INT = NULL,
+    @intSetupId INT = NULL
 
-SET QUOTED_IDENTIFIER OFF
-SET ANSI_NULLS ON
-SET NOCOUNT ON
-SET XACT_ABORT ON
-SET ANSI_WARNINGS ON
-
-BEGIN TRY
-
-	DECLARE @CursorOverrideSetup AS CURSOR,
-		@intSetupReceiptTaxGroupId INT = NULL,
-		@intSetupDistributionTaxGroupId INT = NULL,
-		@intSetupSupplierId INT = NULL,
-		@intSetupSupplyPointId INT = NULL,
-		@strSetupReceiptState NVARCHAR(5) = NULL,
-		@intSetupCustomerId INT = NULL,
-		@intSetupCustomerShipToId INT = NULL,
-		@strSetupDistributionState NVARCHAR(5) = NULL,
-		@intSetupBulkLocationId INT = NULL,
-		@intSetupShipViaId INT = NULL
-
-	SET @CursorOverrideSetup = CURSOR FOR
-	SELECT OTGD.intReceiptTaxGroupId, 
-		OTGD.intDistributionTaxGroupId,
-		OTGD.intSupplierId,
-		OTGD.intSupplyPointId,
-		OTGD.strReceiptState,
-		OTGD.intCustomerId,
-		OTGD.intCustomerShipToId,
-		OTGD.strDistributionState,
-		OTGD.intBulkLocationId,
-		OTGD.intShipViaId
-	FROM tblTROverrideTaxGroupDetail OTGD
-	ORDER BY intOverrideTaxGroupDetailId
-
-	DECLARE @ynsMatch BIT = 0
-
-	OPEN @CursorOverrideSetup
-	FETCH NEXT FROM @CursorOverrideSetup INTO @intSetupReceiptTaxGroupId, @intSetupDistributionTaxGroupId, @intSetupSupplierId, @intSetupSupplyPointId, @strSetupReceiptState, @intSetupCustomerId, @intSetupCustomerShipToId, @strSetupDistributionState, @intSetupBulkLocationId, @intSetupShipViaId
-	WHILE @@FETCH_STATUS = 0
+	-- All Setup
+	IF (@intSetupId IS NULL
+	AND @strReceiptState IS NOT NULL 
+	AND @strDistributionState IS NOT NULL 
+	AND @intSupplierId IS NOT NULL
+	AND @intSupplyPointId IS NOT NULL
+	AND @intCustomerId IS NOT NULL
+	AND @intCustomerShipToId IS NOT NULL
+	AND @intShipViaId IS NOT NULL
+	AND @intDistributionBulkLocationId IS NOT NULL
+	)
 	BEGIN
-
-		-- RECEIPT SIDE
-		DECLARE @ysnReceipt BIT = 0
-		IF(@intSupplierId IS NOT NULL AND @intSupplyPointId IS NOT NULL)
-		BEGIN
-			IF(@intSetupSupplierId = @intSupplierId AND @intSetupSupplyPointId = @intSupplyPointId)
-			BEGIN
-				SET @ysnReceipt = 1
-			END
-		END	
-
-		IF (ISNULL(@strReceiptState, '') != '' AND @ysnReceipt = 0)
-		BEGIN
-			IF(ISNULL(@strSetupReceiptState, '') = ISNULL(@strReceiptState, ''))
-			BEGIN
-				SET @ysnReceipt = 1
-			END
-		END
-
-
-		--DECLARE @ysnReceipt BIT = 0
-		--IF(@intSupplierId IS NOT NULL AND @intSupplyPointId IS NOT NULL)
-		--BEGIN
-		--	IF(@intSetupSupplierId = @intSupplierId AND @intSetupSupplyPointId = @intSupplyPointId)
-		--	BEGIN
-		--		SET @ysnReceipt = 1
-		--	END
-		--END	
-		--ELSE IF (ISNULL(@strReceiptState, '') != '' AND @ysnReceipt = 0)
-		--BEGIN
-		--	IF(ISNULL(@strSetupReceiptState, '') = ISNULL(@strReceiptState, ''))
-		--	BEGIN
-		--		SET @ysnReceipt = 1
-		--	END
-		--END
-		
-
-		-- DISTRIBUTION SIDE
-		DECLARE @ysnDistribution BIT = 0
-
-		IF(@intCustomerId IS NOT NULL AND @intCustomerShipToId IS NOT NULL)
-		BEGIN
-			IF(@intSetupCustomerId = @intCustomerId AND @intSetupCustomerShipToId = @intCustomerShipToId)
-			BEGIN
-				SET @ysnDistribution = 1
-			END
-			ELSE IF (ISNULL(@strSetupDistributionState, '') = ISNULL(@strDistributionState, ''))
-			BEGIN
-				SET @ysnDistribution = 1
-			END
-		END
-
-		--IF (ISNULL(@strDistributionState, '') != '' AND @ysnDistribution = 0)
-		--BEGIN
-		--	IF(ISNULL(@strSetupDistributionState, '') = ISNULL(@strDistributionState, ''))
-		--	BEGIN
-		--		SET @ysnDistribution = 1
-		--	END
-		--END
-
-		ELSE IF (@intDistributionBulkLocationId IS NOT NULL)
-		BEGIN
-			IF(@intSetupBulkLocationId = @intDistributionBulkLocationId)
-			BEGIN
-				SET @ysnDistribution = 1
-			END
-			ELSE IF (ISNULL(@strSetupDistributionState, '') = ISNULL(@strDistributionState, ''))
-			BEGIN
-				SET @ysnDistribution = 1
-			END
-		END
-
-
-
-		--ELSE IF (ISNULL(@strDistributionState, '') != '')
-		--BEGIN
-		--	IF (@intDistributionBulkLocationId IS NOT NULL)
-		--	BEGIN
-		--		IF(@intSetupBulkLocationId = @intDistributionBulkLocationId)
-		--		BEGIN
-		--			SET @ysnDistribution = 1
-		--		END
-		--	END
-		--	ELSE IF(ISNULL(@strSetupDistributionState, '') = ISNULL(@strDistributionState, ''))
-		--	BEGIN
-		--		SET @ysnDistribution = 1
-		--	END
-		--END
-
-
-		--IF(@intSetupCustomerId IS NOT NULL AND @intSetupCustomerShipToId IS NOT NULL)
-		--BEGIN
-		--	IF(@intSetupCustomerId = @intCustomerId AND @intSetupCustomerShipToId = @intCustomerShipToId)
-		--	BEGIN
-		--		SET @ysnDistribution = 1
-		--	END
-		--END
-		--ELSE IF (ISNULL(@strSetupDistributionState, '') != '' AND @ysnDistribution = 0)
-		--BEGIN
-		--	IF(ISNULL(@strSetupDistributionState, '') = ISNULL(@strDistributionState, ''))
-		--	BEGIN
-		--		SET @ysnDistribution = 1
-		--	END
-		--END
-
-		
-		DECLARE @ysnShipVia BIT = 1
-		IF(@intShipViaId IS NOT NULL)
-		BEGIN
-			IF(@intSetupShipViaId != @intShipViaId)
-			BEGIN
-				SET @ysnShipVia = 0
-			END
-		END
-
-		IF(@ysnReceipt = 1 AND @ysnDistribution = 1 AND @ysnShipVia = 1)
-		BEGIN
-			SELECT @strReceiptTaxGroup = strTaxGroup FROM tblSMTaxGroup WHERE intTaxGroupId = @intSetupReceiptTaxGroupId
-			SELECT @strDistributionTaxGroup = strTaxGroup FROM tblSMTaxGroup WHERE intTaxGroupId = @intSetupDistributionTaxGroupId
-			SET @intReceiptTaxGroupId = @intSetupReceiptTaxGroupId
-			SET @intDistributionTaxGroupId = @intSetupDistributionTaxGroupId
-			BREAK 
-		END
-
-		FETCH NEXT FROM @CursorOverrideSetup INTO @intSetupReceiptTaxGroupId, @intSetupDistributionTaxGroupId, @intSetupSupplierId, @intSetupSupplyPointId, @strSetupReceiptState, @intSetupCustomerId, @intSetupCustomerShipToId, @strSetupDistributionState, @intSetupBulkLocationId, @intSetupShipViaId
+		SELECT @intSetupId = intOverrideTaxGroupDetailId
+		FROM tblTROverrideTaxGroupDetail 
+		WHERE
+			-- Receipt
+			strReceiptState = @strReceiptState 
+			AND intSupplierId = @intSupplierId
+			AND intSupplyPointId = @intSupplyPointId
+			-- Distribution
+			AND strDistributionState = @strDistributionState
+			AND intCustomerId = @intCustomerId
+			AND intCustomerShipToId = @intCustomerShipToId
+			AND intBulkLocationId = @intDistributionBulkLocationId
+			-- Ship Via
+			AND intShipViaId = @intShipViaId
 	END
 
-	CLOSE @CursorOverrideSetup
-	DEALLOCATE @CursorOverrideSetup
+	-- No Ship Via
+	IF (@intSetupId IS NULL
+	AND @strReceiptState IS NOT NULL 
+	AND @strDistributionState IS NOT NULL
+	AND @intSupplierId IS NOT NULL
+	AND @intSupplyPointId IS NOT NULL
+	AND @intCustomerId IS NOT NULL
+	AND @intCustomerShipToId IS NOT NULL
+	AND @intDistributionBulkLocationId IS NOT NULL
+	)
+	BEGIN
+		SELECT @intSetupId = intOverrideTaxGroupDetailId
+		FROM tblTROverrideTaxGroupDetail 
+		WHERE
+			-- Receipt
+			strReceiptState = @strReceiptState 
+			AND intSupplierId = @intSupplierId
+			AND intSupplyPointId = @intSupplyPointId
+			-- Distribution
+			AND strDistributionState = @strDistributionState
+			AND intCustomerId = @intCustomerId
+			AND intCustomerShipToId = @intCustomerShipToId
+			AND intBulkLocationId = @intDistributionBulkLocationId
+	END
 
-END TRY
-BEGIN CATCH
-	DECLARE @ErrorSeverity INT,
-			@ErrorNumber   INT,
-			@ErrorState INT,
-			@errorMessage NVARCHAR(MAX)
+	-- No Bulk Location
+	IF (@intSetupId IS NULL
+	AND @strReceiptState IS NOT NULL 
+	AND @strDistributionState IS NOT NULL
+	AND @intSupplierId IS NOT NULL
+	AND @intSupplyPointId IS NOT NULL
+	AND @intCustomerId IS NOT NULL
+	AND @intCustomerShipToId IS NOT NULL
+	AND @intShipViaId IS NOT NULL
+	)
+	BEGIN
+		SELECT @intSetupId = intOverrideTaxGroupDetailId
+		FROM tblTROverrideTaxGroupDetail 
+		WHERE
+			-- Receipt
+			strReceiptState = @strReceiptState 
+			AND intSupplierId = @intSupplierId
+			AND intSupplyPointId = @intSupplyPointId
+			-- Distribution
+			AND strDistributionState = @strDistributionState
+			AND intCustomerId = @intCustomerId
+			AND intCustomerShipToId = @intCustomerShipToId
+			-- Ship Via
+			AND intShipViaId = @intShipViaId
+	END
 
-	SET @ErrorSeverity = ERROR_SEVERITY()
-	SET @ErrorNumber   = ERROR_NUMBER()
-	SET @errorMessage  = ERROR_MESSAGE()
-	SET @ErrorState    = ERROR_STATE()
-	RAISERROR (@errorMessage , @ErrorSeverity, @ErrorState, @ErrorNumber)
+
+	-- No Bulk, No Shipvia
+	IF (@intSetupId IS NULL
+	AND @strReceiptState IS NOT NULL 
+	AND @strDistributionState IS NOT NULL 
+	AND @intSupplierId IS NOT NULL
+	AND @intSupplyPointId IS NOT NULL
+	AND @intCustomerId IS NOT NULL
+	AND @intCustomerShipToId IS NOT NULL
+	)
+	BEGIN
+		SELECT @intSetupId = intOverrideTaxGroupDetailId
+		FROM tblTROverrideTaxGroupDetail 
+		WHERE
+			-- Receipt
+			strReceiptState = @strReceiptState 
+			AND intSupplierId = @intSupplierId
+			AND intSupplyPointId = @intSupplyPointId
+			-- Distribution
+			AND strDistributionState = @strDistributionState
+			AND intCustomerId = @intCustomerId
+			AND intCustomerShipToId = @intCustomerShipToId
+	END
+
+
+	-- No Customer, 
+	IF (@intSetupId IS NULL
+	AND @strReceiptState IS NOT NULL 
+	AND @strDistributionState IS NOT NULL 
+	AND @intSupplierId IS NOT NULL
+	AND @intSupplyPointId IS NOT NULL
+	AND @intShipViaId IS NOT NULL
+	AND @intDistributionBulkLocationId IS NOT NULL
+	)
+	BEGIN
+		SELECT @intSetupId = intOverrideTaxGroupDetailId
+		FROM tblTROverrideTaxGroupDetail 
+		WHERE
+			-- Receipt
+			strReceiptState = @strReceiptState 
+			AND intSupplierId = @intSupplierId
+			AND intSupplyPointId = @intSupplyPointId
+			-- Distribution
+			AND strDistributionState = @strDistributionState
+			AND intBulkLocationId = @intDistributionBulkLocationId
+			-- Ship Via
+			AND intShipViaId = @intShipViaId
+	END
+
+	-- No Customer, No Bulk
+	IF (@intSetupId IS NULL
+	AND @strReceiptState IS NOT NULL 
+	AND @strDistributionState IS NOT NULL 
+	AND @intSupplierId IS NOT NULL
+	AND @intSupplyPointId IS NOT NULL
+	AND @intShipViaId IS NOT NULL
+	)
+	BEGIN
+		SELECT @intSetupId = intOverrideTaxGroupDetailId
+		FROM tblTROverrideTaxGroupDetail 
+		WHERE
+			-- Receipt
+			strReceiptState = @strReceiptState 
+			AND intSupplierId = @intSupplierId
+			AND intSupplyPointId = @intSupplyPointId
+			-- Distribution
+			AND strDistributionState = @strDistributionState
+			-- Ship Via
+			AND intShipViaId = @intShipViaId
+	END
+
+	-- No Customer, No Ship Via
+	IF (@intSetupId IS NULL
+	AND @strReceiptState IS NOT NULL 
+	AND @strDistributionState IS NOT NULL 
+	AND @intSupplierId IS NOT NULL
+	AND @intSupplyPointId IS NOT NULL
+	AND @intDistributionBulkLocationId IS NOT NULL
+	)
+	BEGIN
+		SELECT @intSetupId = intOverrideTaxGroupDetailId
+		FROM tblTROverrideTaxGroupDetail 
+		WHERE
+			-- Receipt
+			strReceiptState = @strReceiptState 
+			AND intSupplierId = @intSupplierId
+			AND intSupplyPointId = @intSupplyPointId
+			-- Distribution
+			AND strDistributionState = @strDistributionState
+			AND intBulkLocationId = @intDistributionBulkLocationId
+	END
+
+
+
+	-- No Customer, No Bulk, No Ship Via
+	IF (@intSetupId IS NULL
+	AND @strReceiptState IS NOT NULL 
+	AND @strDistributionState IS NOT NULL 
+	AND @intSupplierId IS NOT NULL
+	AND @intSupplyPointId IS NOT NULL
+	)
+	BEGIN
+		SELECT @intSetupId = intOverrideTaxGroupDetailId
+		FROM tblTROverrideTaxGroupDetail 
+		WHERE
+			-- Receipt
+			strReceiptState = @strReceiptState 
+			AND intSupplierId = @intSupplierId
+			AND intSupplyPointId = @intSupplyPointId
+			-- Distribution
+			AND strDistributionState = @strDistributionState
+	END
+
+
+	-- No Supplier
+	IF (@intSetupId IS NULL
+	AND @strReceiptState IS NOT NULL 
+	AND @strDistributionState IS NOT NULL 
+	AND @intCustomerId IS NOT NULL
+	AND @intCustomerShipToId IS NOT NULL
+	AND @intShipViaId IS NOT NULL
+	AND @intDistributionBulkLocationId IS NOT NULL
+	)
+	BEGIN
+		SELECT @intSetupId = intOverrideTaxGroupDetailId
+		FROM tblTROverrideTaxGroupDetail 
+		WHERE
+			-- Receipt
+			strReceiptState = @strReceiptState 
+			-- Distribution
+			AND strDistributionState = @strDistributionState
+			AND intCustomerId = @intCustomerId
+			AND intCustomerShipToId = @intCustomerShipToId
+			AND intBulkLocationId = @intDistributionBulkLocationId
+			-- Ship Via
+			AND intShipViaId = @intShipViaId
+	END
+
+
+	-- No Supplier, No Bulk
+	IF (@intSetupId IS NULL
+	AND @strReceiptState IS NOT NULL 
+	AND @strDistributionState IS NOT NULL 
+	AND @intCustomerId IS NOT NULL
+	AND @intCustomerShipToId IS NOT NULL
+	AND @intShipViaId IS NOT NULL
+	)
+	BEGIN
+		SELECT @intSetupId = intOverrideTaxGroupDetailId
+		FROM tblTROverrideTaxGroupDetail 
+		WHERE
+			-- Receipt
+			strReceiptState = @strReceiptState 
+			-- Distribution
+			AND strDistributionState = @strDistributionState
+			AND intCustomerId = @intCustomerId
+			AND intCustomerShipToId = @intCustomerShipToId
+			-- Ship Via
+			AND intShipViaId = @intShipViaId
+	END
+
+	-- No Supplier, No Ship Via
+	IF (@intSetupId IS NULL
+	AND @strReceiptState IS NOT NULL 
+	AND @strDistributionState IS NOT NULL 
+	AND @intCustomerId IS NOT NULL
+	AND @intCustomerShipToId IS NOT NULL
+	AND @intDistributionBulkLocationId IS NOT NULL
+	)
+	BEGIN
+		SELECT @intSetupId = intOverrideTaxGroupDetailId
+		FROM tblTROverrideTaxGroupDetail 
+		WHERE
+			-- Receipt
+			strReceiptState = @strReceiptState 
+			-- Distribution
+			AND strDistributionState = @strDistributionState
+			AND intCustomerId = @intCustomerId
+			AND intCustomerShipToId = @intCustomerShipToId
+			AND intBulkLocationId = @intDistributionBulkLocationId
+	END
+
+
+	-- No Supplier, No Bulk, No Ship Via
+	IF (@intSetupId IS NULL
+	AND @strReceiptState IS NOT NULL 
+	AND @strDistributionState IS NOT NULL 
+	AND @intCustomerId IS NOT NULL
+	AND @intCustomerShipToId IS NOT NULL
+	)
+	BEGIN
+		SELECT @intSetupId = intOverrideTaxGroupDetailId
+		FROM tblTROverrideTaxGroupDetail 
+		WHERE
+			-- Receipt
+			strReceiptState = @strReceiptState 
+			-- Distribution
+			AND strDistributionState = @strDistributionState
+			AND intCustomerId = @intCustomerId
+			AND intCustomerShipToId = @intCustomerShipToId
+	END
+
+
+	-- No Customer, No Supplier
+	IF (@intSetupId IS NULL
+	AND @strReceiptState IS NOT NULL 
+	AND @strDistributionState IS NOT NULL 
+	AND @intShipViaId IS NOT NULL
+	AND @intDistributionBulkLocationId IS NOT NULL
+	)
+	BEGIN
+		SELECT @intSetupId = intOverrideTaxGroupDetailId
+		FROM tblTROverrideTaxGroupDetail 
+		WHERE
+			-- Receipt
+			strReceiptState = @strReceiptState 
+			-- Distribution
+			AND strDistributionState = @strDistributionState
+			AND intBulkLocationId = @intDistributionBulkLocationId
+			-- Ship Via
+			AND intShipViaId = @intShipViaId
+	END
+
+
+	-- No Customer, No Supplier, No Ship Via
+	IF (@intSetupId IS NULL
+	AND @strReceiptState IS NOT NULL 
+	AND @strDistributionState IS NOT NULL 
+	AND @intDistributionBulkLocationId IS NOT NULL
+	)
+	BEGIN
+		SELECT @intSetupId = intOverrideTaxGroupDetailId
+		FROM tblTROverrideTaxGroupDetail 
+		WHERE
+			-- Receipt
+			strReceiptState = @strReceiptState 
+			-- Distribution
+			AND strDistributionState = @strDistributionState
+			AND intBulkLocationId = @intDistributionBulkLocationId
+	END
+	
+
+	-- No Customer, No Supplier, No Bulk
+	IF (@intSetupId IS NULL
+	AND @strReceiptState IS NOT NULL 
+	AND @strDistributionState IS NOT NULL 
+	AND @intShipViaId IS NOT NULL
+	)
+	BEGIN
+		SELECT @intSetupId = intOverrideTaxGroupDetailId
+		FROM tblTROverrideTaxGroupDetail 
+		WHERE
+			-- Receipt
+			strReceiptState = @strReceiptState 
+			-- Distribution
+			AND strDistributionState = @strDistributionState
+			-- Ship Via
+			AND intShipViaId = @intShipViaId
+	END
+
+
+	-- No Customer, No Supplier, No Bulk, No Ship Via
+	IF (@intSetupId IS NULL
+	AND @strReceiptState IS NOT NULL 
+	AND @strDistributionState IS NOT NULL 
+	)
+	BEGIN
+		SELECT @intSetupId = intOverrideTaxGroupDetailId
+		FROM tblTROverrideTaxGroupDetail 
+		WHERE
+			-- Receipt
+			strReceiptState = @strReceiptState 
+			-- Distribution
+			AND strDistributionState = @strDistributionState
+	END
+
+	IF (@intSetupId IS NOT NULL)
+	BEGIN
+		SELECT @intSetupReceiptTaxGroupId = intReceiptTaxGroupId, 
+			@intSetupDistributionTaxGroupId = intDistributionTaxGroupId
+		FROM tblTROverrideTaxGroupDetail
+		WHERE intOverrideTaxGroupDetailId = @intSetupId
+
+		SELECT @strReceiptTaxGroup = strTaxGroup FROM tblSMTaxGroup WHERE intTaxGroupId = @intSetupReceiptTaxGroupId  
+		SELECT @strDistributionTaxGroup = strTaxGroup FROM tblSMTaxGroup WHERE intTaxGroupId = @intSetupDistributionTaxGroupId  
+		SET @intReceiptTaxGroupId = @intSetupReceiptTaxGroupId  
+		SET @intDistributionTaxGroupId = @intSetupDistributionTaxGroupId  
+	END
+END TRY  
+BEGIN CATCH  
+ DECLARE @ErrorSeverity INT,  
+   @ErrorNumber   INT,  
+   @ErrorState INT,  
+   @errorMessage NVARCHAR(MAX)  
+  
+ SET @ErrorSeverity = ERROR_SEVERITY()  
+ SET @ErrorNumber   = ERROR_NUMBER()  
+ SET @errorMessage  = ERROR_MESSAGE()  
+ SET @ErrorState    = ERROR_STATE()  
+ RAISERROR (@errorMessage , @ErrorSeverity, @ErrorState, @ErrorNumber)  
 END CATCH
