d060c4e0863abac985222618dee8a4ae653d9b1e
Jeff Trajano <jeffrey.trajano@irely.com>
2023-04-05 02:54:43 +0800

    GL-10234 Fix Audit/GJ Posting

--- i21Database/dbo/Stored Procedures/uspGLPostJournal.sql
+++ i21Database/dbo/Stored Procedures/uspGLPostJournal.sql
@@ -63,7 +63,10 @@ IF ISNULL(@ysnPost, 0) = 0
 			BEGIN
 				SET @Param = (SELECT strJournalId FROM tblGLJournal WHERE intJournalId IN (SELECT intJournalId FROM @tmpPostJournals))
 				EXEC [dbo].[uspGLReverseGLEntries] @strBatchId,@Param, @ysnRecap, 'GJ', NULL, @intEntityId, @intCount	OUT
-				SET @successfulCount = @intCount
+				
+				IF @@ERROR <> 0	GOTO Post_Rollback;
+				
+				SET @successfulCount = count(*) FROM tblGLDetail a
 				
 				IF(@intCount > 0)
 				BEGIN
@@ -285,8 +288,9 @@ IF ISNULL(@ysnRecap, 0) = 0
 
 		DECLARE @PostResult INT
 		EXEC @PostResult = uspGLBookEntries @GLEntries = @GLEntries, @ysnPost = @ysnPost, @SkipICValidation = 1
-		
+
 		IF @@ERROR <> 0	OR @PostResult <> 0 GOTO Post_Rollback;
+		SET @successfulCount = 1
 	END
 ELSE
 	BEGIN
@@ -374,10 +378,13 @@ ELSE
 		IF @@ERROR <>  0 
 		GOTO Post_Rollback;
 		
-
-		EXEC dbo.uspGLPostRecap 
-			@GLEntries
-			,@intEntityId
+		IF EXISTS(SELECT 1 FROM @GLEntries)
+		BEGIN
+			EXEC uspGLValidateGLEntries @GLEntries, @ysnPost
+			IF @@ERROR <> 0	GOTO Post_Rollback;
+			EXEC uspGLPostRecap @GLEntries , @intEntityId , 0
+			SET @successfulCount = 1
+		END
 
 	
 		IF @@ERROR <> 0	GOTO Post_Rollback;
