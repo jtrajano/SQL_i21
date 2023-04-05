56350f531f2f4b5c1ae8b35f6d557585d56897fb
Nelson Reyes <nelson.reyes@irely.com>
2023-04-05 15:25:47 +0800

    ST-2510

---
+++ i21Database/dbo/Views/vyuSTStoreMaintenanceDepartments.sql
@@ -0,0 +1,18 @@
+﻿CREATE VIEW [dbo].[vyuSTStoreMaintenanceDepartments]
+AS
+SELECT 
+intStoreDepartmentId
+,sd.intStoreId
+,sd.intCategoryId
+,strCategoryCode
+,sd.intSubcategoriesId
+,sc.strSubCategory
+,strSubCategoryCode
+,sd.strRegisterCode
+,sd.intConcurrencyId
+FROM tblSTStoreDepartments sd
+LEFT JOIN vyuSTDepartmentTotalsPreloadCS plcs
+ON sd.intStoreId = plcs.intStoreId
+AND ISNULL(sd.strRegisterCode, '') = ISNULL(plcs.strCashRegisterDepartment, '')
+LEFT JOIN tblSTSubCategories sc
+ON ISNULL(sc.intSubcategoriesId, 0) = ISNULL(plcs.intSubcategoriesId, 0)
