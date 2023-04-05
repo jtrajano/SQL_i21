25442655b7472867ab09bc8a618fa66b842b2599
Kenneth Lew Lagunero <kenneth.lagunero@irely.com>
2023-04-05 11:29:54 +0800

    Revert "TM-3528"
    This reverts commit c7f2eb892f37fd55a8cd283c20dcfd7c77e1bccc.

--- i21Database/dbo/Tables/tblEMEntityLocationConsumptionSite.sql
+++ i21Database/dbo/Tables/tblEMEntityLocationConsumptionSite.sql
@@ -5,7 +5,6 @@
     [intConcurrencyId]                      INT DEFAULT 1 NOT NULL,
     CONSTRAINT [PK_tblEMEntityLocationConsumptionSite] PRIMARY KEY CLUSTERED ([intEntityLocationConsumptionSiteId] ASC),
     CONSTRAINT [FK_tblEMEntityLocationConsumptionSite_tblTMSite] FOREIGN KEY ([intSiteID]) REFERENCES [dbo].[tblTMSite] ([intSiteID]),
-    CONSTRAINT [FK_tblEMEntityLocationConsumptionSite_tblEMEntityLocation] FOREIGN KEY ([intEntityLocationId]) REFERENCES [dbo].[tblEMEntityLocation] ([intEntityLocationId]),
-    CONSTRAINT [UK_tblEMEntityLocationConsumptionSite_intSiteID] UNIQUE ([intSiteID])
+    CONSTRAINT [FK_tblEMEntityLocationConsumptionSite_tblEMEntityLocation] FOREIGN KEY ([intEntityLocationId]) REFERENCES [dbo].[tblEMEntityLocation] ([intEntityLocationId])
 );
 GO
