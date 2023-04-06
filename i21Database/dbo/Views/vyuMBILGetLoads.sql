c0a248c96fb718db8f23a1e9983976836aeae9b3
Lawrence Zabala <lawrence.zabala@irely.com>
2023-04-06 15:19:47 +0800

    TR-2489
    (cherry picked from commit 9e83b5075d03256a9424dfc164feb3147bfb7cda)
    (cherry picked from commit f42388ce52ab9bd4211fabf5a5a10bb21e767f9f)

--- i21Database/dbo/Views/vyuMBILGetLoads.sql
+++ i21Database/dbo/Views/vyuMBILGetLoads.sql
@@ -65,6 +65,7 @@ SELECT lh.intLoadHeaderId
 	, lh.intStateId
 	, pd.intDispatchOrderRouteId
 	, dd.intDispatchOrderDetailId
+	, ysnLockPrice = ISNULL(tm.ysnLockPrice, CAST(0 AS BIT))
 FROM tblMBILPickupDetail pd
 JOIN tblMBILDeliveryDetail dd ON dd.intPickupDetailId = pd.intPickupDetailId
 LEFT JOIN tblTRSupplyPoint sp ON sp.intEntityVendorId = pd.intEntityId AND sp.intEntityLocationId = pd.intEntityLocationId
