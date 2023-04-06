4f0fbb5291143fb596e6e18380f9e3ff35dda724
IRELY\JMendones <jesmar.mendones@irely.com>
2023-04-06 17:42:18 +0800

    FRM-10923

--- i21Database/dbo/Tables/tblSMPowerBIUserRoleReport.sql
+++ i21Database/dbo/Tables/tblSMPowerBIUserRoleReport.sql
@@ -1,7 +1,11 @@
 ﻿CREATE TABLE [dbo].[tblSMPowerBIUserRoleReport]
 (
-	[intPowerBIUserRoleReportId]	INT		NOT NULL	PRIMARY KEY IDENTITY, 
+	[intPowerBIUserRoleReportId]	INT IDENTITY (1, 1) NOT NULL,
 	[intUserRoleId]					INT		NOT NULL,
 	[intPowerBIReportId]			INT		NOT NULL,
-	[intConcurrencyId]				INT		NOT NULL	DEFAULT 1
+	[strPermission]					NVARCHAR(100),
+	[intConcurrencyId]				INT		NOT NULL	DEFAULT 1,
+
+	CONSTRAINT [PK_tblSMPowerBIUserRoleReport] PRIMARY KEY CLUSTERED ([intPowerBIUserRoleReportId] ASC),
+	CONSTRAINT [FK_dbo.tblSMPowerBIReport_tblSMPowerBIUserRoleReport] FOREIGN KEY ([intPowerBIReportId]) REFERENCES [tblSMPowerBIReport]([intPowerBIReportId]) ON DELETE CASCADE,
 )
