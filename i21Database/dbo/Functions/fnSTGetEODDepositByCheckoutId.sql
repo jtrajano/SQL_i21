ed661b5fc95345fb00405057d4eaf721e852274b
Renze Cuadra <renze.cuadra@iRely.com>
2023-04-10 20:03:57 +0800

    CS-395

---
+++ i21Database/dbo/Functions/fnSTGetEODDepositByCheckoutId.sql
@@ -0,0 +1,19 @@
+﻿CREATE FUNCTION [dbo].[fnSTGetEODDepositByCheckoutId] 
+(
+	@intCheckoutId AS INT
+)
+RETURNS INT
+AS BEGIN
+
+    DECLARE @intEODDepositId INT
+	IF EXISTS (SELECT TOP 1 1 FROM tblSTCheckoutDeposits WHERE intCheckoutId = @intCheckoutId)
+	BEGIN
+		SET @intEODDepositId = (SELECT TOP 1 intDepositId FROM tblSTCheckoutDeposits WHERE intCheckoutId = @intCheckoutId);
+	END
+	ELSE
+	BEGIN
+		SET @intEODDepositId = 0;
+	END
+
+	RETURN @intEODDepositId
+END
