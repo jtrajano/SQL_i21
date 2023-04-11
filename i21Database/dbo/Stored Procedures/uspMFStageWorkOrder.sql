7fc336e51022169a1d4fe9fa3015f783842e4db0
Jonathan Valenzuela <jonathan.valenzuela@iRely.com>
2023-04-11 14:26:05 +0800

    MFG-4983

--- i21Database/dbo/Stored Procedures/uspMFStageWorkOrder.sql
+++ i21Database/dbo/Stored Procedures/uspMFStageWorkOrder.sql
@@ -1,61 +1,62 @@
-﻿CREATE PROCEDURE uspMFStageWorkOrder (
-	@strXML NVARCHAR(MAX)
-	,@intWorkOrderInputLotId INT = NULL OUTPUT
-	)
+﻿CREATE PROCEDURE uspMFStageWorkOrder 
+(
+	@strXML					NVARCHAR(MAX)
+  , @intWorkOrderInputLotId INT = NULL OUTPUT
+)
 AS
 BEGIN TRY
 	DECLARE @ErrMsg NVARCHAR(MAX)
-		,@intManufacturingProcessId INT
-		,@idoc INT
-		,@intLocationId INT
-		,@strAttributeValue NVARCHAR(50)
+		  , @intManufacturingProcessId INT
+		  , @idoc INT
+		  , @intLocationId INT
+		  , @strAttributeValue NVARCHAR(50)
 
 	EXEC sp_xml_preparedocument @idoc OUTPUT
-		,@strXML
+							  , @strXML
 
-	SELECT @intManufacturingProcessId = intManufacturingProcessId
-		,@intLocationId = intLocationId
-	FROM OPENXML(@idoc, 'root', 2) WITH (
-			intManufacturingProcessId INT
-			,intLocationId INT
-			)
+	SELECT @intManufacturingProcessId	= intManufacturingProcessId
+		 , @intLocationId				= intLocationId
+	FROM OPENXML(@idoc, 'root', 2) 
+	WITH 
+	(
+		intManufacturingProcessId	INT
+	  , intLocationId				INT
+	)
 
-	SELECT @strAttributeValue = strAttributeValue
-	FROM tblMFManufacturingProcessAttribute
-	WHERE intManufacturingProcessId = @intManufacturingProcessId
-		AND intLocationId = @intLocationId
-		AND intAttributeId = 116
-
-	IF @strAttributeValue IS NULL
-		OR @strAttributeValue = ''
-	BEGIN
-		SELECT @strAttributeValue = 'False'
-	END
+	/* Retrieve if attribute of stage work order is Storage Location or Lot (Stage WorkOrder By Storage Location). */
+	SELECT @strAttributeValue = CASE WHEN intAttributeId = 116 THEN ISNULL(NULLIF(strAttributeValue, ''), 'False')
+									 ELSE @strAttributeValue
+								END
+	FROM vyuMFProcessAttributeDetail 
+	WHERE intManufacturingProcessId = @intManufacturingProcessId AND intLocationId = @intLocationId
 
 	IF @strAttributeValue = 'True'
-	BEGIN
-		EXEC [dbo].[uspMFStageWorkOrderByStorageLocation] @strXML = @strXML
-			,@intWorkOrderInputLotId = @intWorkOrderInputLotId OUTPUT
-	END
+		BEGIN
+			EXEC [dbo].[uspMFStageWorkOrderByStorageLocation] @strXML					= @strXML
+														    , @intWorkOrderInputLotId	= @intWorkOrderInputLotId OUTPUT
+		END
 	ELSE
-	BEGIN
-		EXEC [dbo].[uspMFStageWorkOrderByLot] @strXML = @strXML
-			,@intWorkOrderInputLotId = @intWorkOrderInputLotId OUTPUT
-	END
+		BEGIN
+			EXEC [dbo].[uspMFStageWorkOrderByLot] @strXML					= @strXML
+												, @intWorkOrderInputLotId	= @intWorkOrderInputLotId OUTPUT
+		END
 
 	EXEC sp_xml_removedocument @idoc
 END TRY
 
 BEGIN CATCH
-	SET @ErrMsg = ERROR_MESSAGE()
+	SET @ErrMsg = ERROR_MESSAGE();
 
 	IF @idoc <> 0
-		EXEC sp_xml_removedocument @idoc
+		BEGIN
+			EXEC sp_xml_removedocument @idoc;
+		END
 
-	RAISERROR (
-			@ErrMsg
-			,16
-			,1
-			,'WITH NOWAIT'
-			)
+	RAISERROR 
+	(
+		@ErrMsg
+	  , 16
+	  , 1
+	  , 'WITH NOWAIT'
+	);
 END CATCH
