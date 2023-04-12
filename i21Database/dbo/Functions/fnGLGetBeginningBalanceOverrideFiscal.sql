52802754e6169472335638310fe98bf99b9ad25e
Jeff Trajano <jeffrey.trajano@irely.com>
2023-04-12 13:42:58 +0800

    GL-10029 Add RE support

--- i21Database/dbo/Functions/fnGLGetBeginningBalanceOverrideFiscal.sql
+++ i21Database/dbo/Functions/fnGLGetBeginningBalanceOverrideFiscal.sql
@@ -1,7 +1,8 @@
 CREATE FUNCTION [dbo].[fnGLGetBeginningBalanceOverrideFiscal] (
     @strAccountId NVARCHAR(100),
     @dtmDate DATETIME = NULL,
-    @dtmOverrideFiscalEndDate DATETIME = NULL -- end date should be a day after the last fiscal end date or have 23:59:59  time
+    @dtmOverrideFiscalEndDate DATETIME = NULL, -- end date should be a day after the last fiscal end date or have 23:59:59  time
+    @ysnRE BIT = 0
     )
 RETURNS @tbl TABLE (
   strAccountId     NVARCHAR(100),
@@ -15,22 +16,57 @@ AS
        DECLARE @intAccountId INT
        SELECT TOP 1 @strAccountType = strAccountType, @intAccountId = intAccountId from vyuGLAccountDetail WHERE strAccountId = @strAccountId
 
+       DECLARE @dtmOverrideFiscalStartDate DATETIME
+             
+       IF @dtmOverrideFiscalEndDate  IS NOT NULL
+              SELECT @dtmOverrideFiscalStartDate = CAST ( DATEADD(YEAR, -1, @dtmOverrideFiscalEndDate) AS DATE)
+              IF @dtmDate < @dtmOverrideFiscalStartDate
+              BEGIN
+                     INSERT INTO @tbl(strError) SELECT 'Date parameter should be later than ' + CONVERT( NVARCHAR(10), @dtmOverrideFiscalStartDate , 101)
+                     RETURN
+              END
+       ELSE
+       BEGIN
+              SELECT TOP 1 @dtmOverrideFiscalStartDate = dtmDateFrom,@dtmOverrideFiscalEndDate = dtmDateTo 
+              FROM tblGLFiscalYear where @dtmDate >= dtmDateFrom AND @dtmDate <= dtmDateTo
+       END
+
+       IF ( @ysnRE = 1)
+       BEGIN
+              ;WITH cte AS(
+              SELECT 
+                      dblDebit - dblCredit   dblBalance,
+                      dblDebitUnit - dblCreditUnit dblBalanceUnit
+              FROM   tblGLDetail C
+              WHERE @intAccountId =C.intAccountId 
+              AND dtmDate < @dtmDate
+              AND ysnIsUnposted = 0
+              AND ISNULL(strCode, '') <> ''
+              UNION ALL
+              SELECT 
+                      dblDebit - dblCredit   dblBalance,
+                      dblDebitUnit -dblCreditUnit dblBalanceUnit
+              FROM   vyuGLDetail C
+              WHERE 
+              dtmDate < @dtmOverrideFiscalStartDate
+              AND ysnIsUnposted = 0
+              AND ISNULL(strCode, '') <> ''
+              AND strAccountType IN ('Expense', 'Revenue')
+              )
+              INSERT INTO @tbl
+              SELECT 
+              @strAccountId                     AS strAccountId,
+              Sum(dblBalance)         AS beginbalance,
+              Sum(dblBalanceUnit) AS beginbalanceunit,
+              NULL
+              FROM cte
+
+       END
+       ELSE
        IF @strAccountType IN ('Expense', 'Revenue')
        BEGIN
-              DECLARE @dtmOverrideFiscalStartDate DATETIME
-              IF @dtmOverrideFiscalEndDate  IS NOT NULL
-                     SELECT @dtmOverrideFiscalStartDate = CAST ( DATEADD(YEAR, -1, @dtmOverrideFiscalEndDate) AS DATE)
-
-                     IF @dtmDate < @dtmOverrideFiscalStartDate
-                      BEGIN
-                        INSERT INTO @tbl(strError) SELECT 'Date parameter should be later than ' + CONVERT( NVARCHAR(10), @dtmOverrideFiscalStartDate , 101)
-                        RETURN
-                      END
-              ELSE
-              BEGIN
-                     SELECT TOP 1 @dtmOverrideFiscalStartDate = dtmDateFrom,@dtmOverrideFiscalEndDate = dtmDateTo 
-                     FROM tblGLFiscalYear where @dtmDate >= dtmDateFrom AND @dtmDate <= dtmDateTo
-              END
+             
+             
               INSERT INTO @tbl
               SELECT @strAccountId                     AS strAccountId,
                       Sum(dblDebit - dblCredit)         AS beginbalance,
@@ -57,7 +93,10 @@ AS
                AND ysnIsUnposted = 0
                AND ISNULL(strCode, '') <> ''
                GROUP BY C.intAccountId
+       
        END
+
+
        
       RETURN
   END 
