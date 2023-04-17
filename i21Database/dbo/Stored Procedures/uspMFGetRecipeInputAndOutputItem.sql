5419b7142e3c38c2a8023d679f27d06b44b7049c
Jonathan Valenzuela <jonathan.valenzuela@iRely.com>
2023-04-17 12:29:23 +0800

    MFG-5005

--- i21Database/dbo/Stored Procedures/uspMFGetRecipeInputAndOutputItem.sql
+++ i21Database/dbo/Stored Procedures/uspMFGetRecipeInputAndOutputItem.sql
@@ -1,4 +1,7 @@
-﻿CREATE PROCEDURE uspMFGetRecipeInputAndOutputItem (@strXML NVARCHAR(MAX) = '')
+﻿CREATE PROCEDURE [dbo].[uspMFGetRecipeInputAndOutputItem] 
+(
+	@strXML NVARCHAR(MAX) = ''
+)
 AS
 BEGIN TRY
 	DECLARE @strPackagingCategory NVARCHAR(50)
@@ -475,7 +478,7 @@ BEGIN TRY
 		,Prod.strLotAlias
 		,C.intCategoryId
 		,I.strLotTracking
-		,I.dblWeight
+		, ISNULL(ISNULL(NULLIF(I.dblWeight, 0), IU1.dblWeight), ItemGrossUOM.dblUnitPerQty) AS dblWeight
 		,Prod.ysnFillPartialPallet
 		,Prod.intParentLotId
 		,Prod.strThirdPartyLotNumber AS strLotNumber
@@ -502,6 +505,11 @@ BEGIN TRY
 	LEFT JOIN dbo.tblICItemUOM IU2 ON IU2.intItemId = I.intItemId
 		AND IU2.intUnitMeasureId = UM1.intUnitMeasureId
 	LEFT JOIN tblICContainer Cont ON Cont.intContainerId = Prod.intContainerId
+	OUTER APPLY (SELECT TOP 1 ISNULL(ICIUOM.dblWeight, ICIUOM.dblUnitQty) AS dblUnitPerQty
+				 FROM tblICItemLocation AS ICILocation
+				 LEFT JOIN tblICItemUOM AS ICIUOM ON ICILocation.intGrossUOMId = ICIUOM.intItemUOMId
+				 LEFT JOIN tblICUnitMeasure AS ICUnitMeasure ON ICIUOM.intUnitMeasureId = ICIUOM.intUnitMeasureId
+				 WHERE ICILocation.intItemId = ri.intItemId AND ICILocation.intLocationId = @intLocationId) as ItemGrossUOM
 	WHERE r.intWorkOrderId = @intWorkOrderId
 		AND ri.intRecipeItemTypeId = 2
 
