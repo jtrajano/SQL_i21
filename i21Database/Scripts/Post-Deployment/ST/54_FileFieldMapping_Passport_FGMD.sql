4fb6a96cada37d7c3de5980639a20dd372bf22e4
Jase Rueda <jase.rueda@iRely.com>
2023-04-18 19:50:03 +0800

    CS-401

---
+++ i21Database/Scripts/Post-Deployment/ST/54_FileFieldMapping_Passport_FGMD.sql
@@ -0,0 +1,29 @@
+﻿GO
+DECLARE @intImportFileHeaderId INT
+DECLARE @intImportFileColumnDetailId INT
+DECLARE @intTagAttributeId INT
+DECLARE @strLayoutTitle NVARCHAR(MAX)
+SET @strLayoutTitle = 'Passport - FGMD 3.4'
+
+--START CHECK HEADER
+IF EXISTS(SELECT 1 FROM dbo.tblSMImportFileHeader WHERE strLayoutTitle = @strLayoutTitle)
+BEGIN
+SELECT @intImportFileHeaderId = intImportFileHeaderId FROM dbo.tblSMImportFileHeader WHERE strLayoutTitle = @strLayoutTitle
+  UPDATE [dbo].[tblSMImportFileHeader]
+  SET [strLayoutTitle] = 'Passport - FGMD 3.4'
+       ,[strFileType] = 'XML'
+       ,[strFieldDelimiter] = NULL
+       ,[strXMLType] = 'Inbound'
+       ,[strXMLInitiater] = NULL
+       ,[ysnActive] = 1
+       ,[intConcurrencyId] = 5
+  WHERE intImportFileHeaderId = @intImportFileHeaderId
+END
+ELSE IF NOT EXISTS(SELECT 1 FROM dbo.tblSMImportFileHeader WHERE strLayoutTitle = @strLayoutTitle)
+BEGIN
+  INSERT INTO [dbo].[tblSMImportFileHeader]
+      ([strLayoutTitle],[strFileType],[strFieldDelimiter],[strXMLType],[strXMLInitiater],[ysnActive],[intConcurrencyId])
+  VALUES 
+      ('Passport - FGMD 3.4','XML',NULL,'Inbound',NULL,1,5)
+END
+GO
