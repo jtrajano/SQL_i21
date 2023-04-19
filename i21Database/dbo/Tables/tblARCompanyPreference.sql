7d6846667f717d586eb81b72b928e397b78a559b
Patrick Matthew Maniego <pat.maniego@iRely.com>
2023-04-19 17:51:08 +0800

    AR-16046

--- i21Database/dbo/Tables/tblARCompanyPreference.sql
+++ i21Database/dbo/Tables/tblARCompanyPreference.sql
@@ -89,6 +89,7 @@
 	[strExpressApplicationID]					NVARCHAR(40) COLLATE Latin1_General_CI_AS NULL,
 	[strExpressAcceptorID]						NVARCHAR(50) COLLATE Latin1_General_CI_AS NULL,
 	[strExpressAccountToken]					NVARCHAR(140) COLLATE Latin1_General_CI_AS NULL,
+	[ysnIncludeHazmatMessage]					BIT NOT NULL CONSTRAINT [DF_tblARCompanyPreference_ysnIncludeHazmatMessage] DEFAULT((0)),
 	[ysnOverrideARAccountLineOfBusinessSegment]	BIT NOT NULL CONSTRAINT [DF_tblARCompanyPreference_ysnOverrideARAccountLineOfBusinessSegment] DEFAULT((0)),
 	[ysnAllowContractWithoutPrepayment]			BIT NOT NULL DEFAULT 0,
 	CONSTRAINT [FK_tblARCompanyPreference_tblGLAccount_intARAccountId] FOREIGN KEY ([intARAccountId]) REFERENCES [dbo].[tblGLAccount] ([intAccountId]),
