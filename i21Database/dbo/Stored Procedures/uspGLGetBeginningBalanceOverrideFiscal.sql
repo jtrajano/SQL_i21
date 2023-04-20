62743dbd398a49aacb1028ed942a5bf951d3ece2
Jeff Trajano <jeffrey.trajano@irely.com>
2023-04-20 11:04:07 +0800

    GL-10029

--- i21Database/dbo/Stored Procedures/uspGLGetBeginningBalanceOverrideFiscal.sql
+++ i21Database/dbo/Stored Procedures/uspGLGetBeginningBalanceOverrideFiscal.sql
@@ -26,16 +26,15 @@ AS
               FROM tblGLFiscalYear where @dtmDate >= dtmDateFrom AND @dtmDate <= dtmDateTo
        END
 
-
-
-
        IF ( @strType= 'RE')-- get balance before fiscal start date
        BEGIN
               ;WITH cte AS(
               SELECT 
                      strAccountId,
                      ISNULL(dblDebit,0) dblDebit,
-                     ISNULL(dblCredit,0) dblCredit,          
+                     ISNULL(dblCredit,0) dblCredit,
+                     ISNULL(dblDebitForeign,0) dblDebitForeign,
+                     ISNULL(dblCreditForeign,0) dblCreditForeign,          
                      ISNULL(dblDebitUnit,0) dblDebitUnit,
                      ISNULL(dblCreditUnit,0) dblCreditUnit
               FROM   tblGLDetail C JOIN @tblAccount A ON A.intAccountId = C.intAccountId
@@ -47,6 +46,8 @@ AS
                      strAccountId,
                      ISNULL(dblDebit,0) dblDebit,
                      ISNULL(dblCredit,0) dblCredit,
+                     ISNULL(dblDebitForeign,0) dblDebitForeign,
+                     ISNULL(dblCreditForeign,0) dblCreditForeign,
                      ISNULL(dblDebitUnit,0) dblDebitUnit,
                      ISNULL(dblCreditUnit,0) dblCreditUnit
               FROM   vyuGLDetail C 
@@ -59,9 +60,12 @@ AS
               SELECT strAccountId,
                     SUM(dblDebit) beginBalanceDebit,
                     SUM(dblCredit) beginBalanceCredit,
+                    SUM(dblDebitForeign) beginBalanceDebitForeign,
+                    SUM(dblCreditForeign) beginBalanceCreditForeign,
                     SUM(dblDebitUnit) beginBalanceDebitUnit,
                     SUM(dblCreditUnit) beginBalanceCreditUnit,
                     Sum(dblDebit - dblCredit) beginBalance,
+                    Sum(dblDebitForeign - dblCreditForeign) beginBalanceForeign,
                     Sum(dblDebitUnit- dblCreditUnit ) beginBalanceUnit
               FROM cte
               GROUP BY strAccountId
@@ -71,12 +75,15 @@ AS
        IF ( @strType= 'CY')-- get balance before fiscal start date
        BEGIN          
               SELECT  strAccountId,
-                      SUM(dblDebit) beginBalanceDebit,
-                      SUM(dblCredit) beginBalanceCredit,
-                      SUM(dblDebitUnit) beginBalanceDebitUnit,
-                      SUM(dblCreditUnit) beginBalanceCreditUnit,
-                      Sum(dblDebit - dblCredit) beginBalance,
-                      Sum(dblDebitUnit- dblCreditUnit ) beginBalanceUnit
+                    SUM(dblDebit) beginBalanceDebit,
+                    SUM(dblCredit) beginBalanceCredit,
+                    SUM(dblDebitForeign) beginBalanceDebitForeign,
+                    SUM(dblCreditForeign) beginBalanceCreditForeign,
+                    SUM(dblDebitUnit) beginBalanceDebitUnit,
+                    SUM(dblCreditUnit) beginBalanceCreditUnit,
+                    Sum(dblDebit - dblCredit) beginBalance,
+                    Sum(dblDebitForeign - dblCreditForeign) beginBalanceForeign,
+                    Sum(dblDebitUnit- dblCreditUnit ) beginBalanceUnit
                FROM   tblGLDetail C JOIN @tblAccount A ON A.intAccountId = C.intAccountId
                AND dtmDate BETWEEN @dtmOverrideFiscalStartDate AND @dtmDate
                AND ysnIsUnposted = 0
@@ -87,12 +94,15 @@ AS
        ELSE
        BEGIN
                 SELECT  strAccountId,
-                        SUM(dblDebit) beginBalanceDebit,
-                        SUM(dblCredit) beginBalanceCredit,
-                        SUM(dblDebitUnit) beginBalanceDebitUnit,
-                        SUM(dblCreditUnit) beginBalanceCreditUnit,
-                        Sum(dblDebit - dblCredit) beginBalance,
-                        Sum(dblDebitUnit- dblCreditUnit ) beginBalanceUnit
+                    SUM(dblDebit) beginBalanceDebit,
+                    SUM(dblCredit) beginBalanceCredit,
+                    SUM(dblDebitForeign) beginBalanceDebitForeign,
+                    SUM(dblCreditForeign) beginBalanceCreditForeign,
+                    SUM(dblDebitUnit) beginBalanceDebitUnit,
+                    SUM(dblCreditUnit) beginBalanceCreditUnit,
+                    Sum(dblDebit - dblCredit) beginBalance,
+                    Sum(dblDebitForeign - dblCreditForeign) beginBalanceForeign,
+                    Sum(dblDebitUnit- dblCreditUnit ) beginBalanceUnit
                FROM   tblGLDetail C JOIN @tblAccount A ON A.intAccountId = C.intAccountId
                AND dtmDate < @dtmDate
                AND ysnIsUnposted = 0
