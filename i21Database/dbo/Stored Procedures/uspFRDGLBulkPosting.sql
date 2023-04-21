83a8b50b8d10fef5dec0006e328bce2d01edcaba
Dom Caoile <dom.caoile.02@gmail.com>
2023-04-21 10:26:05 +0800

    FRD-1966

--- i21Database/dbo/Stored Procedures/uspFRDGLBulkPosting.sql
+++ i21Database/dbo/Stored Procedures/uspFRDGLBulkPosting.sql
@@ -12,30 +12,30 @@ SET NOCOUNT ON
  ---------------------------------------------------------------------------------------------------------------------------------------         
  --WITH DATA IN tblGLDetail
 
-    INSERT INTO tblGLPosted (intAccountId,dtmDate,dblDebit,dblCredit,dblDebitForeign,dblCreditForeign,dblDebitUnit,dblCreditUnit,strCode,intConcurrencyId,intCurrencyId)         
-    SELECT  T2.intAccountId,T2.dtmDate,  
-    SUM(dblDebit)dblDebit,SUM(dblCredit)dblCredit,            
-    SUM(dblDebitForeign)dblDebitForeign,SUM(dblCreditForeign)dblCreditForeign,            
-    SUM(dblDebitUnit)dblDebitUnit,SUM(dblCreditUnit)dblCreditUnit,            
-    T2.strCode,1,T2.intCurrencyID  
-    FROM (  
-    SELECT T0.intAccountId,T0.dtmDate,            
-    SUM(dblDebit)dblDebit,SUM(dblCredit)dblCredit,            
-    SUM(dblDebitForeign)dblDebitForeign,SUM(dblCreditForeign)dblCreditForeign,            
-    SUM(dblDebitUnit)dblDebitUnit,SUM(dblCreditUnit)dblCreditUnit,            
-    T0.strCode,ISNULL(T0.intCurrencyId,T1.intCurrencyID)intCurrencyID FROM              
-    tblGLDetail T0    
-    LEFT JOIN tblGLAccount T1    
-    ON T0.intAccountId = T1.intAccountId    
-    WHERE T0.ysnIsUnposted = 0
-    GROUP BY T0.intAccountId,T0.dtmDate,T0.strCode,T0.intCurrencyId,T1.intCurrencyID     
-    )T2  
-    GROUP BY T2.intAccountId,T2.dtmDate,T2.strCode,T2.intCurrencyID          
+    INSERT INTO tblGLPosted (intAccountId,dtmDate,dblDebit,dblCredit,dblDebitForeign,dblCreditForeign,dblDebitUnit,dblCreditUnit,strCode,intConcurrencyId,intCurrencyId,intLedgerId,intSubledgerId)             
+    SELECT  T2.intAccountId,T2.dtmDate,      
+    SUM(dblDebit)dblDebit,SUM(dblCredit)dblCredit,                
+    SUM(dblDebitForeign)dblDebitForeign,SUM(dblCreditForeign)dblCreditForeign,                
+    SUM(dblDebitUnit)dblDebitUnit,SUM(dblCreditUnit)dblCreditUnit,                
+    T2.strCode,1,T2.intCurrencyID,T2.intLedgerId,T2.intSubledgerId              
+    FROM (      
+    SELECT T0.intAccountId,T0.dtmDate,                
+    SUM(dblDebit)dblDebit,SUM(dblCredit)dblCredit,                
+    SUM(dblDebitForeign)dblDebitForeign,SUM(dblCreditForeign)dblCreditForeign,                
+    SUM(dblDebitUnit)dblDebitUnit,SUM(dblCreditUnit)dblCreditUnit,                
+    T0.strCode,ISNULL(T0.intCurrencyId,T1.intCurrencyID)intCurrencyID,ISNULL(T0.intLedgerId,0)intLedgerId,ISNULL(T0.intSubledgerId,0)intSubledgerId FROM                  
+    tblGLDetail T0        
+    LEFT JOIN tblGLAccount T1        
+    ON T0.intAccountId = T1.intAccountId        
+    WHERE T0.ysnIsUnposted = 0    
+    GROUP BY T0.intAccountId,T0.dtmDate,T0.strCode,T0.intCurrencyId,T1.intCurrencyID,T0.intLedgerId,T0.intSubledgerId                    
+    )T2      
+    GROUP BY T2.intAccountId,T2.dtmDate,T2.strCode,T2.intCurrencyID,T2.intLedgerId,T2.intSubledgerId     
 
 --OTHER CURRENCIES DUMMY 
 
-    INSERT INTO tblGLPosted 
-    SELECT T0.intAccountId,'01/01/1900',0,0,0,0,0,0,'GJ',T1.intCurrencyID,0 FROM tblGLAccount T0
+    INSERT INTO tblGLPosted (intAccountId,dtmDate,dblDebit,dblCredit,dblDebitForeign,dblCreditForeign,dblDebitUnit,dblCreditUnit,strCode,intConcurrencyId,intCurrencyId,intLedgerId,intSubledgerId)             
+    SELECT T0.intAccountId,'01/01/1900',0,0,0,0,0,0,'GJ',0,T1.intCurrencyID,0,0 FROM tblGLAccount T0
     LEFT JOIN tblSMCurrency T1 ON 1 = 1 
     LEFT JOIN (
     SELECT intAccountId,intCurrencyId FROM tblGLPosted GROUP BY intAccountId,intCurrencyId 
