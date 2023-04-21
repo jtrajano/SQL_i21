57c893fff08fbed50a1e409f954b605b35f9e763
Kenneth Lew Lagunero <kenneth.lagunero@irely.com>
2023-04-21 21:00:58 +0800

    GL-10336

--- i21Database/dbo/Views/vyuCMGetBankTransaction.sql
+++ i21Database/dbo/Views/vyuCMGetBankTransaction.sql
@@ -43,10 +43,11 @@ OUTER APPLY(
 	B.intTransactionId
 
 )Related
+LEFT JOIN dbo.vyuCMBankAccount BA on BA.intBankAccountId = BT.intBankAccountId
 LEFT JOIN tblCMBankTransactionType BTT ON BTT.intBankTransactionTypeId = BT.intBankTransactionTypeId
 LEFT JOIN tblSMCompanyLocation CL on CL.intCompanyLocationId = BT.intCompanyLocationId
 LEFT JOIN tblSMCurrency CU on CU.intCurrencyID = BT.intCurrencyId
 LEFT JOIN tblSMCurrencyExchangeRateType RT ON RT.intCurrencyExchangeRateTypeId = BT.intCurrencyExchangeRateTypeId
 LEFT JOIN tblCMBankLoan BL ON BL.intBankLoanId = BT.intBankLoanId
 LEFT JOIN tblGLFiscalYearPeriod FP on FP.intGLFiscalYearPeriodId = BT.intFiscalPeriodId
-LEFT JOIN vyuCMBankAccount BA on BA.intBankAccountId = BT.intBankAccountId
+GO
