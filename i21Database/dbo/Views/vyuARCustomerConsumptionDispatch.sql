f972213c1ded0e8527eec23cba44e99060166eef
Kenneth Lew Lagunero <kenneth.lagunero@irely.com>
2023-04-27 12:52:42 +0800

    AR-16923
    # Conflicts:
    #	i21Database/dbo/Views/vyuARCustomerConsumptionDispatch.sql

--- i21Database/dbo/Views/vyuARCustomerConsumptionDispatch.sql
+++ i21Database/dbo/Views/vyuARCustomerConsumptionDispatch.sql
@@ -28,3 +28,32 @@ OUTER APPLY (
 	WHERE SD.[intSiteID] = TMS.[intSiteID] AND DT.strDeviceType = 'Flow Meter'
 	ORDER BY intSiteDeviceID ASC
 ) TANK
+
+-- UNION ALL 
+-- SELECT 
+-- 	 TMO.intDispatchId
+-- 	,TMO.strOrderNumber
+-- 	,AC.intEntityId
+-- 	,TMS.intSiteID
+-- 	,TMS.intSiteNumber
+-- 	,strSiteNumber			= REPLACE(STR(TMS.[intSiteNumber], 4), SPACE(1), '0') COLLATE Latin1_General_CI_AS
+-- 	,TMS.strBillingBy
+-- 	,TMS.dblLastMeterReading
+-- 	,TMS.dblPriceAdjustment
+-- 	,TANK.dblConversionFactor
+-- FROM tblTMOrder TMO
+-- INNER JOIN tblTMDispatchHistory TMD ON TMO.intDispatchId = TMD.intDispatchId
+-- INNER JOIN tblTMSite TMS ON TMO.intSiteId = TMS.intSiteID
+-- INNER JOIN tblTMCustomer TMC ON TMS.intCustomerID = TMC.intCustomerID
+-- INNER JOIN tblARCustomer AC ON TMC.intCustomerNumber = AC.intEntityId
+-- OUTER APPLY (
+-- 	SELECT TOP 1 
+-- 		 MT.[strMeterType]
+-- 		,MT.[dblConversionFactor]
+-- 	FROM tblTMSiteDevice SD
+-- 	INNER JOIN tblTMDevice D ON SD.[intDeviceId] = D.[intDeviceId]  
+-- 	INNER JOIN tblTMDeviceType DT ON D.[intDeviceTypeId] = DT.[intDeviceTypeId] 
+-- 	INNER JOIN tblTMMeterType MT ON D.[intMeterTypeId] = MT.[intMeterTypeId]
+-- 	WHERE SD.[intSiteID] = TMS.[intSiteID] AND DT.strDeviceType = 'Flow Meter'
+-- 	ORDER BY intSiteDeviceID ASC
+-- ) TANK
