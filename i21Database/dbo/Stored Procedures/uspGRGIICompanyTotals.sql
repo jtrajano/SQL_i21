36bcb12d99852abfb07b477318d319f4f6944b0f
Leslie Arat <leslie.arat@irely.com>
2023-04-27 11:10:10 +0800

    GRN-3328

--- i21Database/dbo/Stored Procedures/uspGRGIICompanyTotals.sql
+++ i21Database/dbo/Stored Procedures/uspGRGIICompanyTotals.sql
@@ -516,7 +516,7 @@ END
 TRUNCATE TABLE tblGRGIIPhysicalInventory
 TRUNCATE TABLE tblGRGIICustomerStorage
 
-UPDATE @CompanyOwnedData SET dblTotalEnding = ISNULL(dblTotalBeginning,0) + ISNULL(dblTotalIncrease,0) - ISNULL(dblTotalIncrease,0)
+UPDATE @CompanyOwnedData SET dblTotalEnding = ISNULL(dblTotalBeginning,0) + ISNULL(dblTotalIncrease,0) - ISNULL(dblTotalDecrease,0) WHERE strLabel <> ''
 
 SELECT * FROM @CompanyOwnedData ORDER BY intOrderNo
 
