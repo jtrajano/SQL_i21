93cfad2db618463de1325ec63e8b0963a62922e2
Leslie Arat <leslie.arat@irely.com>
2023-05-03 10:53:59 +0800

    AP-11921 (Percent Discounts in Open Clearing Detail report are showing)

--- i21Database/dbo/Functions/fnGRGetPercentDiscounts.sql
+++ i21Database/dbo/Functions/fnGRGetPercentDiscounts.sql
@@ -17,8 +17,6 @@ BEGIN
 	FROM dbo.tblAPBillDetail R
 	INNER JOIN tblAPBill AP
 		ON AP.intBillId = R.intBillId
-	INNER JOIN tblICItem item 
-		ON item.intItemId = R.intItemId
 	INNER JOIN tblGRSettleStorageBillDetail SBD
 		ON SBD.intBillId = R.intBillId
 	INNER JOIN tblGRSettleStorageTicket SST
@@ -29,29 +27,8 @@ BEGIN
 		ON TD.intTicketFileId = CS.intCustomerStorageId
 			AND TD.strSourceType = 'Storage'
 			AND TD.strDiscountChargeType = 'Percent'
-	INNER JOIN tblGRDiscountScheduleCode DSC
-		ON DSC.intDiscountScheduleCodeId = TD.intDiscountScheduleCodeId
-			AND DSC.intItemId = item.intItemId
 	INNER JOIN tblICInventoryReceiptCharge IRC
 		ON IRC.intInventoryReceiptChargeId = R.intInventoryReceiptChargeId
-	OUTER APPLY (
-		SELECT TOP 1 IL.intItemLocationId
-		FROM tblAPBillDetail A
-		INNER JOIN tblICItem IC
-			ON IC.intItemId = A.intItemId
-				AND IC.strType = 'Inventory'
-		INNER JOIN tblICItemLocation IL
-			ON IL.intItemId = IC.intItemId
-				AND IL.intLocationId = A.intLocationId
-		WHERE intBillId = R.intBillId			
-	) ITEM
-	OUTER APPLY (
-		SELECT TOP 1 stockUnit.*
-		FROM tblICItemUOM stockUnit 
-		WHERE 
-			item.intItemId = stockUnit.intItemId 
-		AND stockUnit.ysnStockUnit = 1
-	) itemUOM
 	WHERE IRC.intInventoryReceiptChargeId = @intInventoryReceiptChargeId
 
 	RETURN;
