283b22628ea99b6600a5832e892f0d83eb295e56
Jeff Trajano <jeffrey.trajano@irely.com>
2023-05-05 13:30:54 +0800

    GL-10381

---
+++ i21Database/dbo/Views/vyuGLAccountDetailCombo.sql
@@ -0,0 +1,59 @@
+CREATE VIEW [dbo].[vyuGLAccountDetailCombo]
+AS
+
+	SELECT      TOP 1000000 
+				account.intConcurrencyId,
+				account.strAccountId COLLATE Latin1_General_CI_AS strAccountId,
+				CAST(replace(account.strAccountId,'-','') AS NVARCHAR(40)) COLLATE Latin1_General_CI_AS strAccountId1,
+				account.strOldAccountId COLLATE Latin1_General_CI_AS strOldAccountId,
+				cast(replace(account.strOldAccountId,'-','') AS NVARCHAR(50)) COLLATE Latin1_General_CI_AS strOldAccountId1,
+				account.strDescription COLLATE Latin1_General_CI_AS strDescription, 
+				grp.strAccountGroup COLLATE Latin1_General_CI_AS strAccountGroup, 
+				grp.strAccountType COLLATE Latin1_General_CI_AS strAccountType, 
+				sg.strAccountCategory COLLATE Latin1_General_CI_AS strAccountCategory, 
+                account.strComments COLLATE Latin1_General_CI_AS strComments, 
+				account.strCashFlow COLLATE Latin1_General_CI_AS strCashFlow, 
+				account.ysnActive, account.ysnSystem, account.ysnRevalue, u.intAccountUnitId, 
+                u.strUOMCode COLLATE Latin1_General_CI_AS strUOMCode, 
+				account.intAccountId, account.intCurrencyID, account.intCurrencyExchangeRateTypeId, 
+				cast (account.strNote as nvarchar(255)) COLLATE Latin1_General_CI_AS strNote, 
+				curr.strCurrency COLLATE Latin1_General_CI_AS strCurrency, 
+				rtype.strCurrencyExchangeRateType COLLATE Latin1_General_CI_AS strCurrencyExchangeRateType, 
+				account.intAccountGroupId, sg.intAccountCategoryId,
+				coa.strExternalId COLLATE Latin1_General_CI_AS strExternalId, 
+				coa.strCurrentExternalId COLLATE Latin1_General_CI_AS strCurrentExternalId, 
+				sg.strCode COLLATE Latin1_General_CI_AS strCode, 
+				cast(0.00 as numeric(18,2)) as dblBalance,
+				sg.ysnGLRestricted, 
+				sg.ysnAPRestricted,
+				account.intUnnaturalAccountId,
+				unnaturalAccount.strAccountId COLLATE Latin1_General_CI_AS strUnnaturalAccountId,
+				account.intLocationSegmentId,
+				account.intCompanySegmentId,
+				locationSegment.strCode COLLATE Latin1_General_CI_AS strLocationSegmentId,
+				companySegment.strCode COLLATE Latin1_General_CI_AS strCompanySegmentId
+FROM            dbo.tblGLAccount account 
+				CROSS APPLY (
+					SELECT 
+					strCode, 
+					cat.intAccountCategoryId, 
+					cat.strAccountCategory, 
+					cat.ysnGLRestricted, 
+					cat.ysnAPRestricted
+					FROM dbo.tblGLAccountSegmentMapping mapping 
+					LEFT JOIN dbo.tblGLAccountSegment segment ON segment.intAccountSegmentId = mapping.intAccountSegmentId
+					LEFT JOIN dbo.tblGLAccountCategory cat ON segment.intAccountCategoryId = cat.intAccountCategoryId
+					JOIN dbo.tblGLAccountStructure struc ON segment.intAccountStructureId = struc.intAccountStructureId 
+					AND struc.strType = 'Primary'
+					where intAccountId = account.intAccountId
+				)sg
+				
+                LEFT JOIN dbo.tblSMCurrencyExchangeRateType rtype ON account.intCurrencyExchangeRateTypeId = rtype.intCurrencyExchangeRateTypeId 
+				LEFT JOIN dbo.tblSMCurrency curr ON account.intCurrencyID = curr.intCurrencyID 
+				LEFT JOIN dbo.tblGLAccountUnit u ON account.intAccountUnitId = u.intAccountUnitId
+				LEFT JOIN dbo.tblGLCOACrossReference coa  on account.intAccountId =inti21Id
+				LEFT JOIN dbo.tblGLAccountGroup grp ON account.intAccountGroupId = grp.intAccountGroupId
+				LEFT JOIN dbo.tblGLAccount unnaturalAccount on unnaturalAccount.intAccountId = account.intUnnaturalAccountId
+				LEFT JOIN dbo.tblGLAccountSegment locationSegment ON locationSegment.intAccountSegmentId = account.intLocationSegmentId
+				LEFT JOIN dbo.tblGLAccountSegment companySegment ON companySegment.intAccountSegmentId = account.intCompanySegmentId
+GO
