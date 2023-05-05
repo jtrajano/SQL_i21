882f6eed32e84e0a7358c4c1c77a246316dea1b2
Jeff Trajano <jeffrey.trajano@irely.com>
2023-05-05 13:30:54 +0800

    GL-10381

--- i21Database/dbo/Stored Procedures/uspGLUpdateAccountLocationId.sql
+++ i21Database/dbo/Stored Procedures/uspGLUpdateAccountLocationId.sql
@@ -4,12 +4,10 @@ UPDATE A set intLocationSegmentId = S.intAccountSegmentId FROM tblGLAccount A JO
 JOIN tblGLAccountSegment S ON S.intAccountSegmentId = M.intAccountSegmentId
 JOIN tblGLAccountStructure ST ON ST.intAccountStructureId =  S.intAccountStructureId
 WHERE intStructureType = 3
-AND intLocationSegmentId IS NULL
-
 
 UPDATE A set intCompanySegmentId = S.intAccountSegmentId FROM tblGLAccount A JOIN tblGLAccountSegmentMapping M on A.intAccountId = M.intAccountId
 JOIN tblGLAccountSegment S ON S.intAccountSegmentId = M.intAccountSegmentId
 JOIN tblGLAccountStructure ST ON ST.intAccountStructureId =  S.intAccountStructureId
 WHERE intStructureType = 6
-AND intCompanySegmentId IS NULL
+
 
