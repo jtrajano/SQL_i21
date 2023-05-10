cc0c32d2c9b7dfca07cbc9e2a4acfd8f75eb7918
Dom Caoile <dom.caoile.02@gmail.com>
2023-05-10 13:44:49 +0800

    Revert "FA-430"
    This reverts commit a82c984e01dbe30b862d04424db2ed699fd15c85.

--- i21Database/dbo/Views/vyuGLFiscalYear.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE VIEW [dbo].[vyuGLFiscalYear]
-AS 
-
-SELECT (SELECT TOP 1 intGLFiscalYearPeriodId FROM tblGLFiscalYearPeriod WHERE intFiscalYearId = T0.intFiscalYearId  ORDER BY intGLFiscalYearPeriodId DESC ) AS intGLFiscalYearPeriodId,strFiscalYear,ysnCurrent 
-FROM  tblGLFiscalYear T0
