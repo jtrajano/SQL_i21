774e0a6c10de5952bb4ed6393cedbb030a9af055
Jonathan Valenzuela <jonathan.valenzuela@iRely.com>
2023-05-15 16:09:46 +0800

    MFG-5045

--- i21Database/dbo/Views/vyuMFDemandDetailContract.sql
+++ i21Database/dbo/Views/vyuMFDemandDetailContract.sql
@@ -30,10 +30,12 @@ SELECT intContractDetailId
 	 , 0.00 AS dblDemandQty
 	 , dblDetailQuantity
 	 , dblAppliedQty
-	 , dbo.fnCTConvertQtyToTargetItemUOM(intItemUOMId, intStockUOMId, dblDetailQuantity - ISNULL(NULLIF(dblAppliedQty, 0), ISNULL(dblScheduleQty,0))) AS dblContractDemandBalanceQty
+	 , ISNULL(dbo.fnICConvertUOMtoStockUnit(intItemId, intItemUOMId, dblAvailableQty), 0) AS dblContractDemandBalanceQty
 	 , strStockItemUOM
 	 , intStockUOMId
 	 , intItemUOMId
+	 , 'Open'							AS strPurchaseType
+	 , ''								AS strLoadNumber		
 FROM vyuCTContractDetailView
 OUTER APPLY (SELECT TOP 1 intItemBundleId
 			 FROM tblCTContractDetail AS CDContractDetail
@@ -42,9 +44,11 @@ OUTER APPLY (SELECT TOP 1 strItemNo
 			 FROM tblICItem AS ICItem
 			 WHERE ICItem.intItemId = BundleItem.intItemBundleId) AS BundleItemNo
 WHERE intContractStatusId IN (1, 4) 
-  AND (dblDetailQuantity - ISNULL(NULLIF(dblAppliedQty, 0), ISNULL(dblScheduleQty,0)) > 0) 
+--  AND (dblDetailQuantity - ISNULL(NULLIF(dblAppliedQty, 0), ISNULL(dblScheduleQty,0)) > 0) 
+    AND dblAvailableQty > 0
 UNION 
-/* In-Transit */
+
+/* Sceheduled */
 SELECT CTDetailView.intContractDetailId
 	 , strSequenceNumber
 	 , CTDetailView.strLocationName 
@@ -53,7 +57,7 @@ SELECT CTDetailView.intContractDetailId
 	 , CTDetailView.dtmStartDate
 	 , CTDetailView.dtmEndDate
 	 , CTDetailView.dtmPlannedAvailabilityDate
-	 , CASE WHEN (SELECT TOP 1 intPositionByETADemandReport FROM tblMFCompanyPreference) = 1 THEN dtmUpdatedAvailabilityDate
+	 , CASE WHEN DemandPosition.intPositionByETADemandReport = 1 THEN dtmUpdatedAvailabilityDate
 			WHEN LGLoad.intTransportationMode = 2 THEN ISNULL(DATEADD(DAY, ISNULL(City.intLeadTime, 0), LGLoad.dtmETAPOD), dtmUpdatedAvailabilityDate)
 		    ELSE LGLoad.dtmETAPOD
 	   END AS dtmUpdatedAvailabilityDate
@@ -71,12 +75,14 @@ SELECT CTDetailView.intContractDetailId
 	 , 0.00 AS dblDemandQty
 	 , dblDetailQuantity
 	 , dblAppliedQty
-	 , CASE WHEN dblAppliedQty < 1 THEN   dbo.fnCTConvertQtyToTargetItemUOM(CTDetailView.intItemUOMId, intStockUOMId, ISNULL(dblScheduleQty, 0))
-			ELSE  dbo.fnCTConvertQtyToTargetItemUOM(CTDetailView.intItemUOMId, intStockUOMId, dblDetailQuantity - dblAppliedQty)
-	   END AS dblContractDemandBalanceQty
-	 , strStockItemUOM
+	 , LGTransit.dblLoadScheduleQty		AS dblContractDemandBalanceQty
+	 , strStockItemUOM	
 	 , intStockUOMId
 	 , CTDetailView.intItemUOMId
+	 , CASE WHEN DemandPosition.intPositionByETADemandReport = 3  THEN 'Open - Schedule'
+			ELSE 'In-Transit' 
+	   END AS strPurchaseType
+	 , LGTransit.strLoadNumber			AS strLoadNumber
 FROM vyuCTContractDetailView AS CTDetailView
 LEFT JOIN  tblLGLoadDetail AS LoadDetail ON CTDetailView.intContractDetailId = LoadDetail.intPContractDetailId
 LEFT JOIN tblLGLoad AS LGLoad ON LGLoad.intLoadId = LoadDetail.intLoadId
@@ -88,11 +94,163 @@ OUTER APPLY (SELECT TOP 1 intItemBundleId
 OUTER APPLY (SELECT TOP 1 strItemNo
 			 FROM tblICItem AS ICItem
 			 WHERE ICItem.intItemId = BundleItem.intItemBundleId) AS BundleItemNo
+OUTER APPLY (SELECT SUM(dbo.fnICConvertUOMtoStockUnit(OALoadDetail.intItemId, OALoadDetail.intItemUOMId, ISNULL(OAContainerLink.dblQuantity, OALoadDetail.dblQuantity) - (CASE WHEN (OAContainerLink.intLoadDetailContainerLinkId IS NOT NULL) THEN ISNULL(OAContainerLink.dblReceivedQty, 0)
+																																											   ELSE OALoadDetail.dblDeliveredQuantity
+			      																																							END))) AS dblLoadScheduleQty
+				  , OALoad.strLoadNumber			
+			 FROM tblLGLoadDetail AS OALoadDetail 
+			 LEFT JOIN tblLGLoad AS OALoad ON OALoad.intLoadId = OALoadDetail.intLoadId 
+			 LEFT JOIN tblLGLoadDetailContainerLink AS OAContainerLink ON OALoadDetail.intLoadDetailId = OAContainerLink.intLoadDetailId
+			 WHERE OALoadDetail.intPContractDetailId = CTDetailView.intContractDetailId 
+			   AND OALoadDetail.intLoadDetailId = LoadDetail.intLoadDetailId
+			   AND OALoad.intPurchaseSale = 1 
+			   AND OALoad.intShipmentType = 1 
+			   AND (
+					(SELECT intPositionByETADemandReport FROM tblMFCompanyPreference) = 3 AND OALoad.ysnPosted = 0
+				 OR (SELECT intPositionByETADemandReport FROM tblMFCompanyPreference) != 3 AND 1 = 1
+				   )
+			   AND ISNULL(OAContainerLink.dblQuantity, OALoadDetail.dblQuantity) - (CASE WHEN (OAContainerLink.intLoadDetailContainerLinkId IS NOT NULL) THEN ISNULL(OAContainerLink.dblReceivedQty, 0)
+																						 ELSE OALoadDetail.dblDeliveredQuantity
+																					END) > 0
+			GROUP BY OALoad.strLoadNumber) AS LGTransit
 LEFT JOIN tblSMCity AS City ON City.strCity = LGLoad.strDestinationPort
+OUTER APPLY (SELECT TOP 1 intPositionByETADemandReport 
+			 FROM tblMFCompanyPreference) AS DemandPosition
 WHERE intContractStatusId IN (1, 4) 
-  AND (CASE WHEN dblAppliedQty < 1 THEN   dbo.fnCTConvertQtyToTargetItemUOM(CTDetailView.intItemUOMId, intStockUOMId, ISNULL(dblScheduleQty, 0))
-			ELSE  dbo.fnCTConvertQtyToTargetItemUOM(CTDetailView.intItemUOMId, intStockUOMId, dblDetailQuantity - dblAppliedQty)
-	   END) > 0
+  AND LGTransit.dblLoadScheduleQty > 0
+GROUP BY CTDetailView.intContractDetailId
+	   , strSequenceNumber
+	   , CTDetailView.strLocationName 
+	   , strEntityName
+	   , dblAvailableQty
+	   , CTDetailView.dtmStartDate
+	   , CTDetailView.dtmEndDate
+	   , CTDetailView.dtmPlannedAvailabilityDate
+	   , CASE WHEN DemandPosition.intPositionByETADemandReport = 1 THEN dtmUpdatedAvailabilityDate
+	  		WHEN LGLoad.intTransportationMode = 2 THEN ISNULL(DATEADD(DAY, ISNULL(City.intLeadTime, 0), LGLoad.dtmETAPOD), dtmUpdatedAvailabilityDate)
+	  	    ELSE LGLoad.dtmETAPOD
+	     END 
+	   , CTDetailView.intBookId
+	   , CTDetailView.intSubBookId
+	   , CTDetailView.intItemId
+	   , CTDetailView.intCompanyLocationId
+	   , dblQtyInStockUOM
+	   , intContractStatusId
+	   , DAY(dtmUpdatedAvailabilityDate)	
+	   , strBook
+	   , strSubBook
+	   , BundleItem.intItemBundleId
+	   , BundleItemNo.strItemNo			
+	   , dblDetailQuantity
+	   , dblAppliedQty
+	   , LGTransit.dblLoadScheduleQty			
+	   , strStockItemUOM	
+	   , intStockUOMId
+	   , CTDetailView.intItemUOMId
+	   , LGTransit.strLoadNumber	
+	   , CASE WHEN DemandPosition.intPositionByETADemandReport = 3  THEN 'Open - Schedule'
+			ELSE 'In-Transit' 
+	     END
+
+UNION
+/* In-Transit */
+SELECT CTDetailView.intContractDetailId
+	 , strSequenceNumber
+	 , CTDetailView.strLocationName 
+	 , strEntityName
+	 , dblAvailableQty
+	 , CTDetailView.dtmStartDate
+	 , CTDetailView.dtmEndDate
+	 , CTDetailView.dtmPlannedAvailabilityDate
+	 , CASE WHEN DemandPosition.intPositionByETADemandReport = 1 THEN dtmUpdatedAvailabilityDate
+			WHEN LGLoad.intTransportationMode = 2 THEN ISNULL(DATEADD(DAY, ISNULL(City.intLeadTime, 0), LGLoad.dtmETAPOD), dtmUpdatedAvailabilityDate)
+		    ELSE LGLoad.dtmETAPOD
+	   END AS dtmUpdatedAvailabilityDate
+	 , CTDetailView.intBookId
+	 , CTDetailView.intSubBookId
+	 , CTDetailView.intItemId
+	 , CTDetailView.intCompanyLocationId
+	 , dblQtyInStockUOM
+	 , intContractStatusId
+	 , DAY(dtmUpdatedAvailabilityDate)	AS intUADDay
+	 , strBook
+	 , strSubBook
+	 , BundleItem.intItemBundleId
+	 , BundleItemNo.strItemNo			AS strBundleItemNo
+	 , 0.00 AS dblDemandQty
+	 , dblDetailQuantity
+	 , dblAppliedQty
+	 , InTransit.dblIntrasitQty			AS dblContractDemandBalanceQty
+	 , strStockItemUOM	
+	 , intStockUOMId
+	 , CTDetailView.intItemUOMId
+	 , 'In-Transit'						AS strPurchaseType
+	 , InTransit.strLoadNumber			AS strLoadNumber
+FROM vyuCTContractDetailView AS CTDetailView
+LEFT JOIN tblLGLoadDetail AS LoadDetail ON CTDetailView.intContractDetailId = LoadDetail.intPContractDetailId
+LEFT JOIN tblLGLoad AS LGLoad ON LGLoad.intLoadId = LoadDetail.intLoadId
+						AND LGLoad.intPurchaseSale = 1
+						AND LGLoad.intShipmentType = 1
+OUTER APPLY (SELECT TOP 1 intItemBundleId
+			 FROM tblCTContractDetail AS CDContractDetail
+			 WHERE CDContractDetail.intContractDetailId = CTDetailView.intContractDetailId) AS BundleItem
+OUTER APPLY (SELECT TOP 1 strItemNo
+			 FROM tblICItem AS ICItem
+			 WHERE ICItem.intItemId = BundleItem.intItemBundleId) AS BundleItemNo
+OUTER APPLY (SELECT SUM(dbo.fnICConvertUOMtoStockUnit(OALoadDetail.intItemId, OALoadDetail.intItemUOMId, ISNULL(OAContainerLink.dblQuantity, OALoadDetail.dblQuantity) - (CASE WHEN (OAContainerLink.intLoadDetailContainerLinkId IS NOT NULL) THEN ISNULL(OAContainerLink.dblReceivedQty, 0)
+																																											   ELSE OALoadDetail.dblDeliveredQuantity
+			      																																			  END))) AS dblIntrasitQty
+				  , OALoad.strLoadNumber			
+			 FROM tblLGLoadDetail AS OALoadDetail 
+			 LEFT JOIN tblLGLoad AS OALoad ON OALoad.intLoadId = OALoadDetail.intLoadId 
+			 LEFT JOIN tblLGLoadDetailContainerLink AS OAContainerLink ON OALoadDetail.intLoadDetailId = OAContainerLink.intLoadDetailId
+			 WHERE OALoadDetail.intPContractDetailId = CTDetailView.intContractDetailId 
+			   AND OALoadDetail.intLoadDetailId = LoadDetail.intLoadDetailId
+			   AND OALoad.intPurchaseSale = 1 
+			   AND OALoad.intShipmentType = 1 
+			   AND (
+					(SELECT intPositionByETADemandReport FROM tblMFCompanyPreference) = 3 AND OALoad.ysnPosted = 1
+				 OR (SELECT intPositionByETADemandReport FROM tblMFCompanyPreference) != 3 AND 1 = 1
+				   )
+			   AND ISNULL(OAContainerLink.dblQuantity, OALoadDetail.dblQuantity) - (CASE WHEN (OAContainerLink.intLoadDetailContainerLinkId IS NOT NULL) THEN ISNULL(OAContainerLink.dblReceivedQty, 0)
+																						 ELSE OALoadDetail.dblDeliveredQuantity
+																					END) > 0
+			GROUP BY OALoad.strLoadNumber) AS InTransit
+LEFT JOIN tblSMCity AS City ON City.strCity = LGLoad.strDestinationPort
+OUTER APPLY (SELECT TOP 1 intPositionByETADemandReport 
+			 FROM tblMFCompanyPreference) AS DemandPosition
+WHERE intContractStatusId IN (1, 4) 
+  AND InTransit.dblIntrasitQty > 0
+GROUP BY CTDetailView.intContractDetailId
+	   , strSequenceNumber
+	   , CTDetailView.strLocationName 
+	   , strEntityName
+	   , dblAvailableQty
+	   , CTDetailView.dtmStartDate
+	   , CTDetailView.dtmEndDate
+	   , CTDetailView.dtmPlannedAvailabilityDate
+	   , CASE WHEN DemandPosition.intPositionByETADemandReport = 1 THEN dtmUpdatedAvailabilityDate
+	  		WHEN LGLoad.intTransportationMode = 2 THEN ISNULL(DATEADD(DAY, ISNULL(City.intLeadTime, 0), LGLoad.dtmETAPOD), dtmUpdatedAvailabilityDate)
+	  	    ELSE LGLoad.dtmETAPOD
+	     END 
+	   , CTDetailView.intBookId
+	   , CTDetailView.intSubBookId
+	   , CTDetailView.intItemId
+	   , CTDetailView.intCompanyLocationId
+	   , dblQtyInStockUOM
+	   , intContractStatusId
+	   , DAY(dtmUpdatedAvailabilityDate)	
+	   , strBook
+	   , strSubBook
+	   , BundleItem.intItemBundleId
+	   , BundleItemNo.strItemNo			
+	   , dblDetailQuantity
+	   , dblAppliedQty
+	   , InTransit.dblIntrasitQty			
+	   , strStockItemUOM	
+	   , intStockUOMId
+	   , CTDetailView.intItemUOMId
+	   , InTransit.strLoadNumber			
 GO
 
 
