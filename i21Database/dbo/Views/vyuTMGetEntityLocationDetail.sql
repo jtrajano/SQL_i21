624bd1dc83d9e1e5734d24a324023bddb1baa5a8
Kenneth Lew Lagunero <kenneth.lagunero@irely.com>
2023-05-16 18:02:02 +0800

    TM-3803

---
+++ i21Database/dbo/Views/vyuTMGetEntityLocationDetail.sql
@@ -0,0 +1,26 @@
+﻿CREATE VIEW [dbo].[vyuTMGetEntityLocationDetail]
+AS 
+
+SELECT 
+    A.intEntityLocationId
+    ,A.strZipCode
+	,A.strCity
+	,A.strState
+	,A.strCounty
+	,A.strCountry
+	,A.dblLatitude
+	,A.dblLongitude
+	,A.intTaxGroupId
+	,B.strTaxGroup
+	,A.strAddress
+	,A.strLocationRoute
+	,A.strPricingLevel
+	,A.intTermsId
+	,C.strTerm
+FROM tblEMEntityLocation A
+INNER JOIN tblSMTaxGroup B
+	ON A.intTaxGroupId = B.intTaxGroupId
+INNER JOIN tblSMTerm C
+	ON A.intTermsId = C.intTermID
+
+GO
