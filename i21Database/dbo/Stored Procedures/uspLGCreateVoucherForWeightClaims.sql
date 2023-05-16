5d14ea729fca2b3423b8406b05c8079d6d0dbf14
Christian Joel Martinez <Christian.Martinez@irely.com>
2023-05-16 15:33:42 +0800

    LG-4032

--- i21Database/dbo/Stored Procedures/uspLGCreateVoucherForWeightClaims.sql
+++ i21Database/dbo/Stored Procedures/uspLGCreateVoucherForWeightClaims.sql
@@ -121,7 +121,7 @@ BEGIN TRY
 		,intPartyEntityId = WCD.intPartyEntityId
 		,dblNetShippedWeight = WCD.dblFromNet
 		,dblWeightLoss = ABS(WCD.dblWeightLoss)
-		,dblNetWeight = ABS(WCD.dblWeightLoss) --CASE WHEN (strCondition = 'Missing') THEN WCD.dblFromNet ELSE WCD.dblToNet END
+		,dblNetWeight = ABS(WCD.dblClaimableWt) --CASE WHEN (strCondition = 'Missing') THEN WCD.dblFromNet ELSE WCD.dblToNet END
 		,dblFranchiseWeight = CASE WHEN WCD.dblWeightLoss > 0 THEN 0 ELSE WCD.dblFranchiseWt END
 		,dblQtyReceived = (ABS(WCD.dblWeightLoss) - CASE WHEN WCD.dblWeightLoss > 0 THEN 0 ELSE WCD.dblFranchiseWt END)
 		,dblCost = WCD.dblUnitPrice
