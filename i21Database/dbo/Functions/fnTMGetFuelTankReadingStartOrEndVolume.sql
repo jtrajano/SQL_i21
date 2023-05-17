f675bc5e2cb57b009bdbcaa6c398466f5fca4fd6
Jase Rueda <jase.rueda@iRely.com>
2023-05-18 07:11:05 +0800

    CS-532

---
+++ i21Database/dbo/Functions/fnTMGetFuelTankReadingStartOrEndVolume.sql
@@ -0,0 +1,57 @@
+﻿CREATE FUNCTION [dbo].[fnTMGetFuelTankReadingStartOrEndVolume]
+(
+	@dtmDate				DATETIME,
+	@intSiteId				INT,
+	@ysnStore				BIT,
+	@ysnGetStartVolume		BIT
+)
+RETURNS DECIMAL(18,6)
+BEGIN
+	DECLARE		@dblReturnValue DECIMAL(18,6) = 0
+
+	IF @ysnGetStartVolume = 1
+	BEGIN
+		IF @ysnStore = 1
+		BEGIN
+			SELECT		TOP 1 @dblReturnValue = dblFuelVolume
+			FROM		tblTMTankReading 
+			WHERE		dtmDateTime < @dtmDate AND
+						intSiteId = @intSiteId AND
+						intReadingSource IN (2)
+			ORDER BY	dtmDateTime DESC
+		END
+		ELSE
+		BEGIN
+			SELECT		TOP 1 @dblReturnValue = dblFuelVolume
+			FROM		tblTMTankReading 
+			WHERE		dtmDateTime < @dtmDate AND
+						intSiteId = @intSiteId AND
+						intReadingSource IN (1,3)
+			ORDER BY	dtmDateTime DESC
+		END
+	END
+	ELSE
+	BEGIN
+		IF @ysnStore = 1
+		BEGIN
+			SELECT		TOP 1 @dblReturnValue = dblFuelVolume
+			FROM		tblTMTankReading 
+			WHERE		dtmDateTime >= @dtmDate AND dtmDateTime < DATEADD(DAY,1,@dtmDate) AND
+						intSiteId = @intSiteId AND
+						intReadingSource IN (2)
+			ORDER BY	dtmDateTime DESC
+		END
+		ELSE
+		BEGIN
+			SELECT		TOP 1 @dblReturnValue = dblFuelVolume
+			FROM		tblTMTankReading 
+			WHERE		dtmDateTime >= @dtmDate AND dtmDateTime < DATEADD(DAY,1,@dtmDate) AND
+						intSiteId = @intSiteId AND
+						intReadingSource IN (1,3)
+			ORDER BY	dtmDateTime DESC
+		END
+	END
+	
+	RETURN @dblReturnValue
+END
+GO
---
+++ i21Database/dbo/Functions/fnTMGetSalesFromIC.sql
@@ -0,0 +1,26 @@
+﻿CREATE FUNCTION [dbo].[fnTMGetSalesFromIC]
+(
+	@dtmDate				DATETIME,
+	@intSiteId				INT
+)
+RETURNS DECIMAL(18,6)
+BEGIN
+	DECLARE		@dblReturnValue DECIMAL(18,6) = 0
+	DECLARE		@intItemId INT = 0
+	DECLARE		@intLocationId INT = 0
+
+	SELECT		@intItemId = intProduct,
+				@intLocationId = intLocationId
+	FROM		tblTMSite
+	WHERE		intSiteID = @intSiteId
+
+	SELECT		@dblReturnValue = SUM(dblQuantity) 
+	FROM		vyuICGetInventoryValuation
+	WHERE		intItemId = @intItemId AND
+				intLocationId = @intLocationId AND
+				dblQuantity < 0 AND --meaning outgoing
+				(dtmCreated >= @dtmDate AND dtmCreated < DATEADD(DAY, 1, @dtmDate))
+
+	RETURN @dblReturnValue
+END
+GO
