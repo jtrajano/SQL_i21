6f834d557cccb4d8f29ec234a40eb6aee7ff03a0
IRELY\JMendones <jesmar.mendones@irely.com>
2023-05-17 17:30:45 +0800

    FRM-11054
    (cherry picked from commit 0a95f514c2418259e2acb04b5bb958c0aa514a00)
    (cherry picked from commit 25bded6743c65499bbeadfc93f98ed0b1aea74d3)

--- i21Database/dbo/Tables/tblARCustomerLicenseInformation.sql
+++ i21Database/dbo/Tables/tblARCustomerLicenseInformation.sql
@@ -20,6 +20,7 @@
 	[strLicenseKey]						NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NOT NULL,
 	[ysnNew]							BIT NOT NULL DEFAULT 0,
 	[intCompanyId]						INT NULL,
+	[intPowerBIRefreshes]				INT NULL,
     [intConcurrencyId]					INT CONSTRAINT [DF_tblARCustomerLicenseInformation_intConcurrencyId] DEFAULT ((0)) NOT NULL,
 
     CONSTRAINT [PK_tblARCustomerLicenseInformation] PRIMARY KEY CLUSTERED ([intCustomerLicenseInformationId] ASC),
