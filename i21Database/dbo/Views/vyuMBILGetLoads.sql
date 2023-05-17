6e08df817e2e9f6eb41e9daf7bf0ca7ca2c767d5
Kenneth Sam Perez <kenneth.sam.perez@iRely.com>
2023-05-17 11:46:17 +0800

    MBIL-1318

--- i21Database/dbo/Views/vyuMBILGetLoads.sql
+++ i21Database/dbo/Views/vyuMBILGetLoads.sql
@@ -67,8 +67,8 @@ SELECT lh.intLoadHeaderId
 	, pd.intDispatchOrderRouteId
 	, dd.intDispatchOrderDetailId
 	, ysnLockPrice = ISNULL(tm.ysnLockPrice, CAST(0 AS BIT))
-	, dd.dblGross as dblDistributionDetailGross
-	, dd.dblNet as dblDistributionDetailNet
+	, ISNULL(dd.dblGross,dd.dblDeliveredQty) as dblDistributionDetailGross
+	, ISNULL(dd.dblNet,dd.dblDeliveredQty) as dblDistributionDetailNet
 FROM tblMBILPickupDetail pd
 JOIN tblMBILDeliveryDetail dd ON dd.intPickupDetailId = pd.intPickupDetailId
 LEFT JOIN tblTRSupplyPoint sp ON sp.intEntityVendorId = pd.intEntityId AND sp.intEntityLocationId = pd.intEntityLocationId
