c3d53681d20faec6539cbac66e8de7f0fe0a33c9
Jeff Trajano <jeffrey.trajano@irely.com>
2023-05-19 09:10:36 +0800

    AP-117277

--- i21Database/dbo/Stored Procedures/uspAPVendorToIPayablesJob.sql
+++ i21Database/dbo/Stored Procedures/uspAPVendorToIPayablesJob.sql
@@ -1,103 +1,76 @@
-CREATE VIEW vyuAPVendorToIPayables
-as
-WITH cte as(
-select 
-A.intEntityId,
-A.strName VendorName,
-ISNULL(strVendorId,'') COLLATE Latin1_General_CI_AS  VendorNbr ,
-ISNULL(Address1.Street1, '') COLLATE Latin1_General_CI_AS  Street1,
---ISNULL(Address2.Street2, '') COLLATE Latin1_General_CI_AS  Street2,
-ISNULL(B.strCity,'') COLLATE Latin1_General_CI_AS  City,
-ISNULL(B.strState,'') COLLATE Latin1_General_CI_AS  CountryCode,
-ISNULL(B.strZipCode,'') COLLATE Latin1_General_CI_AS strZipCode ,
-ISNULL(B.strCountry,'') COLLATE Latin1_General_CI_AS strCountry,
-ISNULL(P.strPhone,'') COLLATE Latin1_General_CI_AS strPhone,
-ISNULL(ContactDetails.strFax,'') COLLATE Latin1_General_CI_AS strFax,
-ISNULL(A.strEmail,'') COLLATE Latin1_General_CI_AS  strEmail,
-ISNULL(A.strTerm, '') COLLATE Latin1_General_CI_AS  strTerm,
-ISNULL(EM.ysnActive, CAST (0 AS BIT) ) ysnActive,
-E.strName strContactName
-from vyuAPVendor A
-outer apply (select top 1 strAddress, strCity,strState, strZipCode, strCountry from tblEMEntityLocation where A.intEntityId = intEntityId ) B
-outer apply (select top 1 strName,intEntityContactId from tblEMEntityToContact C JOIN tblEMEntity B ON C.intEntityContactId=B.intEntityId where C.intEntityId = A.intEntityId) E
-left join tblEMEntityPhoneNumber P on P.intEntityId = E.intEntityContactId
-OUTER APPLY(
-	SELECT TOP 1 strValue strFax FROM tblEMContactDetail CD 
-	WHERE intEntityId = E.intEntityContactId AND CD.intContactDetailTypeId = 3
-)ContactDetails
-left join tblEMEntity EM on A.intEntityId = EM.intEntityId
-outer apply (select Item Street1 from  dbo.fnSplitStringWithRowId(B.strAddress, char(10))  where RowId = 1 ) Address1
---outer apply (select Item Street2 from  dbo.fnSplitStringWithRowId(B.strAddress, char(10))  where RowId = 2 ) Address2
-) ,
-EML AS ( 
-	select A.intEntityId,
-	B.strName VendorName,
-	ISNULL(B.strVendorId,'') COLLATE Latin1_General_CI_AS  VendorNbr ,
-	ISNULL(A.strLocationName,'')  COLLATE Latin1_General_CI_AS AddressType,  
-	ISNULL(A.strAddress,'')  COLLATE Latin1_General_CI_AS strAddress,
-	ISNULL(A.strCity,'')  COLLATE Latin1_General_CI_AS strCity,
-	ISNULL(A.strState,'')  COLLATE Latin1_General_CI_AS strState,
-	ISNULL(A.strZipCode,'')  COLLATE Latin1_General_CI_AS strZipCode,
-	ISNULL(A.strCountry,'')  COLLATE Latin1_General_CI_AS strCountry,
-	ISNULL(Address1.Street1 ,'') COLLATE Latin1_General_CI_AS Street1,
-	ISNULL(Address2.Street2,'') COLLATE Latin1_General_CI_AS Street2
-	from  tblEMEntityLocation A join vyuAPVendor B ON A.intEntityId = B.intEntityId
-	outer apply (select Item Street1 from  dbo.fnSplitStringWithRowId(A.strAddress, char(10))  where RowId = 1 ) Address1
-	outer apply (select Item Street2 from  dbo.fnSplitStringWithRowId(A.strAddress, char(10))  where RowId = 2 ) Address2
-),
-tag01 as(
-select '01' strTag, intEntityId,
-'01|C0000549|' + VendorNbr + '|' + VendorName + '|' + Street1 + '||' + City+ '|' +CountryCode+ '|' +strZipCode+ '|' +strCountry+ '|' + strContactName + '|' +strPhone+ '|' +strFax+ '|' +strEmail  strData
-from cte 
-),
-tag02 as(
-select '02' strTag, intEntityId,
-'02|C0000549|' + VendorNbr + '|0090|' + strTerm   strData
-from cte 
-),
-tag03 as(
-	select '03' strTag,intEntityId,'03|C0000549|'+ VendorNbr + '|0090|Corrigan Administration Services LLC' strData from cte
-),
-tag05 as(
-	select '05' strTag, intEntityId,
-	'05|C0000549|' + VendorNbr + '|0090|' + CASE WHEN ysnActive = 1 THEN 'ACTV' ELSE 'IACTV' END  strData
-	from cte 
-),
-tag06 as(
-	select '06' strTag, intEntityId,
-	'06|C0000549|' + VendorNbr + '|0090|1|' + AddressType + '|' 
-	+ VendorName + ',' + Street1 + ',' 
-	+ Street2 + ',' + strCity + ','
-	+ strState + ',' 
-	+ strZipCode + ',' + strCountry   
-	strData
-	from EML
-),
-tag07 as(
-	select '07' strTag, intEntityId,
-	'07|C0000549|' + VendorNbr + '|0090|CHCK'
-	strData
-	from cte
-),
 
-cteOrder as(
+--EXEC uspAPVendorToIpayablesJob  'D:\SQL Backup'
 
-select strTag, intEntityId, strData from tag01
-UNION ALL
-select strTag, intEntityId, strData from tag02
-UNION ALL
-select strTag, intEntityId, strData from tag03
-UNION ALL
-select strTag, intEntityId, strData from tag05
-UNION ALL
-select strTag, intEntityId, strData from tag06
-UNION ALL
-select strTag, intEntityId, strData from tag07
-),
+CREATE PROCEDURE uspAPVendorToIPayablesJob
+@folderPath NVARCHAR(MAX)
 
-cteAllRecords AS (
-	SELECT  strTag, intEntityId, strData from cteOrder
-	UNION ALL
-	select  '99', 999999, '99|C0000549|'+ cast( count(*) as nvarchar(10)) from cteOrder
-)
-select strTag, intEntityId,strData from cteAllRecords
+AS
+BEGIN
+DECLARE @_JobID UNIQUEIDENTIFIER
+
+SELECT @_JobID = job_id   
+FROM  msdb.dbo.sysjobs  
+WHERE (name = N'Vendor to IPayables')   
+IF (@_JobID IS NOT NULL)  
+/****** Object:  Job [Vendor to IPayables]    Script Date: 4/18/2023 6:24:07 PM ******/
+EXEC msdb.dbo.sp_delete_job @job_id=@_JobID, @delete_unused_schedule=1
+
+
+/****** Object:  Job [Vendor to IPayables]    Script Date: 4/18/2023 6:24:07 PM ******/
+BEGIN TRANSACTION
+DECLARE @ReturnCode INT
+SELECT @ReturnCode = 0
+/****** Object:  JobCategory [Data Collector]    Script Date: 4/18/2023 6:24:07 PM ******/
+IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'Data Collector' AND category_class=1)
+BEGIN
+EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'Data Collector'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+END
+
+DECLARE @jobId BINARY(16)
+EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'Vendor to IPayables', 
+		@enabled=1, 
+		@notify_level_eventlog=0, 
+		@notify_level_email=0, 
+		@notify_level_netsend=0, 
+		@notify_level_page=0, 
+		@delete_level=0, 
+		@description=N'No description available.', 
+		@category_name=N'Data Collector', 
+		@owner_login_name=N'sa', @job_id = @jobId OUTPUT
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [Generate CSV File]    Script Date: 4/18/2023 6:24:07 PM ******/
+DECLARE @strDate  NVARCHAR(20) =  REPLACE(CONVERT(nvarchar(20), GETDATE(), 102),'.','')
+DECLARE @command NVARCHAR(MAX) =
+N'sqlcmd -S '+ @@SERVERNAME +' -d ' + DB_NAME() + '  -Q "SET NOCOUNT ON; select strData from vyuAPVendorToIPayables order by intEntityId, strTag;" -o "' + @folderPath +'\C0000549_VENDOR_'+ @strDate +'.csv" -W -w 1024 -h-1'
+--N'sqlcmd -S .\MSSQL2019 -d ' + DB_NAME() + ' -U irely -P iRely486  -Q "SET NOCOUNT ON; select strData from vyuAPVendorToIPayables order by intEntityId, strTag;" -o "' + @folderPath +'\C0000549_VENDOR_'+ @strDate +'.csv" -W -w 1024 -h-1'
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Generate CSV File', 
+		@step_id=1, 
+		@cmdexec_success_code=0, 
+		@on_success_action=1, 
+		@on_success_step_id=0, 
+		@on_fail_action=2, 
+		@on_fail_step_id=0, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'CmdExec', 
+		@command= @command,
+		@flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+COMMIT TRANSACTION
+GOTO EndSave
+QuitWithRollback:
+    IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
+EndSave:
+
+EXEC msdb.dbo.sp_start_job N'Vendor to IPayables' ;  
+
+
+END
+
+
