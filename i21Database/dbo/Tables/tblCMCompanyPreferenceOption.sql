99ac2c5e8a4cee3594bcf98258fe7a8de309db33
Jeff Trajano <jeffrey.trajano@irely.com>
2023-05-22 18:51:41 +0800

    GL-9982

--- i21Database/dbo/Tables/tblCMCompanyPreferenceOption.sql
+++ i21Database/dbo/Tables/tblCMCompanyPreferenceOption.sql
@@ -36,7 +36,7 @@ CREATE TABLE [dbo].[tblCMCompanyPreferenceOption](
 	ysnRemittanceAdvice_AttachSettlement BIT NULL,
 	intConcurrencyId int NULL,
 	intBankFileFormatPrenoteAPId INT NULL,
-	intBankFileFormatPrenoteARId INT NULL
+	intBankFileFormatPrenoteARId INT NULL,
  CONSTRAINT [PK_tblCMCompanyPreferenceOption] PRIMARY KEY CLUSTERED 
 (
 	[intCompanyPreferenceOptionId] ASC
