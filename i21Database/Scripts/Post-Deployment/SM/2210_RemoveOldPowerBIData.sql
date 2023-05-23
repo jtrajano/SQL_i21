0da463fde0cbc1a9bc5889cdfe080cfa2664a657
IRELY\JMendones <jesmar.mendones@irely.com>
2023-05-23 14:50:36 +0800

    FRM-11046_p1

--- i21Database/Scripts/Post-Deployment/SM/2210_RemoveOldPowerBIData.sql
+++ i21Database/Scripts/Post-Deployment/SM/2210_RemoveOldPowerBIData.sql
@@ -1,5 +1,5 @@
 ﻿GO
-PRINT('/*******************  BEGIN DELETING OLD POWER BI DATA *******************/')
+PRINT('/*******************  BEGIN DELETING POWER BI DATA USER CREDENTIAL *******************/')
 
 IF EXISTS(SELECT TOP 1 1 FROM tblSMCompanyPreference WHERE ISNULL(strPowerBIAdminUsername, '') != '' AND ISNULL(strPowerBIAdminPassword, '') != '' AND
 			ISNULL(strPowerBIClientId, '') = '' AND ISNULL(strPowerBIWorkspaceId, '') = '' AND ISNULL(strPowerBISecretId, '') = '' AND ISNULL(strPowerBITenantId, '') = '')
@@ -13,18 +13,35 @@ BEGIN
 	UPDATE tblSMCompanyPreference set strPowerBIAdminUsername = NULL, strPowerBIAdminPassword = NULL
 END
 
-PRINT('/*******************  END DELETING OLD POWER BI DATA *******************/')
+PRINT('/*******************  END DELETING POWER BI DATA USER CREDENTIAL  *******************/')
 
 
 
-PRINT('/*******************  BEGIN UPDATING POWER BI EMPTY SCHEDULE *******************/')
+--PRINT('/*******************  BEGIN UPDATING POWER BI EMPTY SCHEDULE *******************/')
+--IF EXISTS(SELECT TOP 1 1 FROM tblSMPowerBIDataset WHERE ISNULL(dtmNextRefresh, '') = '')
+--BEGIN
+--	UPDATE tblSMPowerBIDataset SET strFrequency = 'None'
+--	WHERE ISNULL(dtmNextRefresh, '') = ''
+--END
+--PRINT('/*******************  END UPDATING POWER BI EMPTY SCHEDULE *******************/')
+
+
+PRINT('/*******************  BEGIN UPDATING POWER BI DEFAULT DATA *******************/')
 IF EXISTS(SELECT TOP 1 1 FROM tblSMPowerBIDataset WHERE ISNULL(dtmNextRefresh, '') = '')
 BEGIN
 	UPDATE tblSMPowerBIDataset SET strFrequency = 'None'
 	WHERE ISNULL(dtmNextRefresh, '') = ''
 END
 
+IF EXISTS(SELECT TOP 1 1 FROM tblSMCompanyPreference WHERE ysnPowerBIProfilePipeline = NULL)
+BEGIN
+	UPDATE tblSMCompanyPreference SET ysnPowerBIProfilePipeline = 1
+END
 
-PRINT('/*******************  END UPDATING POWER BI EMPTY SCHEDULE *******************/')
+
+--INSERT NEW LINE HERE
+
+
+PRINT('/*******************  END UPDATING POWER BI DEFAULT DATA *******************/')
 
 GO
