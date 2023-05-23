5370c21e2cab38a5dcbc118b95de695abc0c2ec2
Jayson Ramos <jayson.ramos@iRely.com>
2023-05-23 12:09:56 +0800

    CT-8826
    (cherry picked from commit 09b5ff72b425923b13ba76006ade51e55d73f85b)

--- i21Database/dbo/Views/vyuCTInventoryItemBundle.sql
+++ i21Database/dbo/Views/vyuCTInventoryItemBundle.sql
@@ -33,7 +33,8 @@ AS
 			ICC.strSeason,
 			ICC.strClass,
 			ICC.strProductLine,
-		   strXrefVendorProduct = x.strVendorProduct
+		   strXrefVendorProduct = x.strVendorProduct,
+		   strXrefCustomerProduct = xc.strCustomerProduct
 	FROM	tblICItem				IM
 	JOIN	tblICItemLocation		IL	ON	IL.intItemId				=	IM.intItemId		LEFT
 	JOIN	tblICCategory			CR	ON	CR.intCategoryId			=	IM.intCategoryId	LEFT
@@ -44,6 +45,7 @@ AS
 	JOIN	tblSMPurchasingGroup	PG	ON	PG.intPurchasingGroupId		=	CA.intPurchasingGroupId LEFT 
 	JOIN	 vyuICGetCompactItem ICC ON ICC.intItemId = IM.intItemId
 	left join tblICItemVendorXref x on x.intItemId = IM.intItemId
+	left join tblICItemCustomerXref xc on xc.intItemId = IM.intItemId
 			
 	WHERE	IM.strType IN ('Inventory','Finished Good','Raw Material')
 
@@ -79,7 +81,8 @@ AS
 			ICC.strSeason,
 			ICC.strClass,
 			ICC.strProductLine,
-		   strXrefVendorProduct = x.strVendorProduct
+		   strXrefVendorProduct = x.strVendorProduct,
+		   strXrefCustomerProduct = xc.strCustomerProduct
 	FROM	tblICItem				IM
 	JOIN	tblICItemLocation		IL	ON	IL.intItemId				=	IM.intItemId		
 	LEFT JOIN	tblICCategory			CR	ON	CR.intCategoryId			=	IM.intCategoryId	
@@ -90,6 +93,7 @@ AS
 	LEFT JOIN	tblSMPurchasingGroup	PG	ON	PG.intPurchasingGroupId		=	CA.intPurchasingGroupId		
 	LEFT JOIN	 vyuICGetCompactItem ICC ON ICC.intItemId = IM.intItemId	
 	left join tblICItemVendorXref x on x.intItemId = IM.intItemId
+	left join tblICItemCustomerXref xc on xc.intItemId = IM.intItemId
 	WHERE	IM.strType IN ('Bundle')
 
 	UNION ALL
@@ -123,7 +127,8 @@ AS
 			ICC.strSeason,
 			ICC.strClass,
 			ICC.strProductLine,
-		   strXrefVendorProduct = x.strVendorProduct
+		   strXrefVendorProduct = x.strVendorProduct,
+		   strXrefCustomerProduct = xc.strCustomerProduct
 	FROM	tblICItem				IM
 	JOIN	tblICItemLocation		IL	ON	IL.intItemId				=	IM.intItemId		
 	LEFT JOIN	tblICCategory			CR	ON	CR.intCategoryId			=	IM.intCategoryId	
@@ -135,6 +140,7 @@ AS
 	JOIN	vyuICGetBundleItem		BI	ON	BI.intBundleItemId			=	IM.intItemId 
 	LEFT  JOIN	 vyuICGetCompactItem ICC ON ICC.intItemId = IM.intItemId
 	left join tblICItemVendorXref x on x.intItemId = IM.intItemId
+	left join tblICItemCustomerXref xc on xc.intItemId = IM.intItemId
 	--LEFT JOIN	vyuICGetBundleItem		B2	ON	B2.intItemId				=	IM.intItemId
 	WHERE	IM.strType IN ('Inventory','Finished Good','Raw Material','Bundle')
 	
@@ -169,7 +175,8 @@ AS
 			ICC.strSeason,
 			ICC.strClass,
 			ICC.strProductLine,
-		   strXrefVendorProduct = x.strVendorProduct
+		   strXrefVendorProduct = x.strVendorProduct,
+		   strXrefCustomerProduct = xc.strCustomerProduct
 	FROM	tblICItem				IM
 	JOIN	tblICItemLocation		IL	ON	IL.intItemId				=	IM.intItemId		
 	LEFT JOIN	tblICCategory			CR	ON	CR.intCategoryId			=	IM.intCategoryId	
@@ -181,9 +188,8 @@ AS
 	JOIN	vyuICGetBundleItem		B2	ON	B2.intItemId				=	IM.intItemId
 	LEFT  JOIN	 vyuICGetCompactItem ICC ON ICC.intItemId = IM.intItemId
 	left join tblICItemVendorXref x on x.intItemId = IM.intItemId
+	left join tblICItemCustomerXref xc on xc.intItemId = IM.intItemId
 	WHERE	IM.strType IN ('Inventory','Finished Good','Raw Material','Bundle')
 
 
 	) fq
-
-
