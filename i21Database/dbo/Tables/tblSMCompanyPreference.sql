32b75fca06451809c8472b7db7a20f1892746741
IRELY\JMendones <jesmar.mendones@irely.com>
2023-05-23 14:50:36 +0800

    FRM-11046_p1
    (cherry picked from commit 0da463fde0cbc1a9bc5889cdfe080cfa2664a657)

--- i21Database/dbo/Tables/tblSMCompanyPreference.sql
+++ i21Database/dbo/Tables/tblSMCompanyPreference.sql
@@ -90,6 +90,7 @@
 	[strPowerBIClientId]							NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL,
 	[strPowerBISecretId]							NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL,
 	[strPowerBIWorkspaceId]							NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL,
+	[ysnPowerBIProfilePipeline]						BIT NULL,
 	[strPowerBITenantId]							NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL,
 	[strPowerBICapacityId]							NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL,
 	[strPowerBIProfileId]							NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL,
