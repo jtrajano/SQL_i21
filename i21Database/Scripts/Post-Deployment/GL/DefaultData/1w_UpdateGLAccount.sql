fea54b3f123ea33465a7589343ea6d4ae068cea4
JES-NUC-LAPTOP\Jesmar-iRely <jesmar.mendones@irely.com>
2023-05-25 10:10:56 +0800

    Revert "GL-10205"
    This reverts commit dc45a621af8b44e7ae77548513190ca7b1c3ccfc.

--- i21Database/Scripts/Post-Deployment/GL/DefaultData/1w_UpdateGLAccount.sql
+++ i21Database/Scripts/Post-Deployment/GL/DefaultData/1w_UpdateGLAccount.sql
@@ -6,13 +6,4 @@ GO
     JOIN vyuGLAccountDetail B ON A.intAccountId = B.intAccountId
     WHERE strAccountType IN ('Asset','Liability', 'Equity')
     AND A.ysnRevalue IS NULL
-
-    -- FOR primary segment range query
-    ;WITH cte AS (
-
-        SELECT ROW_NUMBER() OVER(ORDER BY strCode ASC) rowId, intAccountSegmentId FROM tblGLAccountSegment A JOIN 
-        tblGLAccountStructure B ON A.intAccountStructureId = A.intAccountStructureId WHERE strType= 'Primary'
-    )
-    UPDATE A SET intPrimaryOrderId = rowId FROM tblGLAccountSegment A JOIN cte B ON A.intAccountSegmentId = B.intAccountSegmentId
-
 GO
