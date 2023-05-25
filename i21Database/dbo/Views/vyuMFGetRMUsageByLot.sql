e94be85978a67894a91865782b20441f69c6afba
Jonathan Valenzuela <jonathan.valenzuela@iRely.com>
2023-05-25 14:06:12 +0800

    MFG-5049

--- i21Database/dbo/Views/vyuMFGetRMUsageByLot.sql
+++ i21Database/dbo/Views/vyuMFGetRMUsageByLot.sql
@@ -1,58 +1,58 @@
 ﻿CREATE VIEW vyuMFGetRMUsageByLot
 AS
 SELECT [Dump Date]
-	,[Product]
-	,[Product Description]
-	,[Production Lot]
-	,Line
-	,[Work Order #]
-	,[Job #]
-	,[WSI Item]
-	,[WSI Item Description]
-	,[Lot #]
-	,Ceiling(SUM([Quantity] / dblWeightPerQty)) AS Quantity
-	,ISNULL(UM1.strUnitMeasure, [Weight UOM]) [Quantity UOM]
-	,SUM([Weight]) AS Weight
-	,[Weight UOM]
-	,intWorkOrderId
-	,dtmPlannedDate
-FROM (
-	SELECT Rtrim(Convert(CHAR, W.dtmPlannedDate, 101)) COLLATE Latin1_General_CI_AS AS [Dump Date]
-		,I.strItemNo [Product]
-		,I.strDescription [Product Description]
-		,(
-			SELECT TOP 1 strParentLotNumber
+	 , [Product]
+	 , [Product Description]
+	 , [Production Lot]	
+	 , Line
+	 , [Work Order #]
+	 , [Job #]
+	 , [WSI Item]
+	 , [WSI Item Description]
+	 , [Lot #]
+	 , CEILING(SUM([Quantity] / dblWeightPerQty)) AS Quantity
+	 , ISNULL(UM1.strUnitMeasure, [Weight UOM]) [Quantity UOM]
+	 , SUM([Weight]) AS Weight
+	 , [Weight UOM]
+	 , intWorkOrderId
+	 , dtmPlannedDate
+	 , intLocationId			
+	 , strLocationName
+	 , strCategoryCode
+	 , strDescription
+FROM 
+(
+	SELECT RTRIM(CONVERT(CHAR, W.dtmPlannedDate, 101)) COLLATE Latin1_General_CI_AS AS [Dump Date]
+		 , I.strItemNo				AS [Product]
+		 , I.strDescription			AS [Product Description]
+		 , (SELECT TOP 1 strParentLotNumber
 			FROM dbo.tblMFWorkOrderProducedLot WP
-			WHERE WP.intWorkOrderId = W.intWorkOrderId
-				AND WP.intShiftId = WI.intShiftId
-			) AS [Production Lot]
-		,MC.strCellName AS Line
-		,W.strWorkOrderNo AS [Work Order #]
-		,W.strReferenceNo AS [Job #]
-		,I1.strItemNo AS [WSI Item]
-		,I1.strDescription [WSI Item Description]
-		,IPL.strParentLotNumber AS [Lot #]
-		,(WI.dblIssuedQuantity) AS [Quantity]
-		,(WI.dblQuantity) AS [Weight]
-		,UM.strUnitMeasure [Weight UOM]
-		,IsNULL((
-				SELECT TOP 1 L1.dblWeightPerQty
-				FROM tblICLot L1
-				WHERE L1.strLotNumber = IL.strLotNumber
-					AND L1.dblWeightPerQty > 1
-				), 1) AS dblWeightPerQty
-		,(
-			SELECT TOP 1 L1.intItemUOMId
+			WHERE WP.intWorkOrderId = W.intWorkOrderId AND WP.intShiftId = WI.intShiftId
+		    )						AS [Production Lot]
+		 , MC.strCellName			AS Line
+		 , W.strWorkOrderNo			AS [Work Order #]
+		 , W.strReferenceNo			AS [Job #]
+		 , I1.strItemNo				AS [WSI Item]
+		 , I1.strDescription		AS [WSI Item Description]
+		 , IPL.strParentLotNumber	AS [Lot #]
+		 , (WI.dblIssuedQuantity)	AS [Quantity]
+		 , (WI.dblQuantity)			AS [Weight]
+		 , UM.strUnitMeasure		AS [Weight UOM]
+		 , ISNULL((SELECT TOP 1 L1.dblWeightPerQty
+				   FROM tblICLot L1
+				   WHERE L1.strLotNumber = IL.strLotNumber AND L1.dblWeightPerQty > 1), 1) AS dblWeightPerQty
+		 , (SELECT TOP 1 L1.intItemUOMId
 			FROM tblICLot L1
-			WHERE L1.strLotNumber = IL.strLotNumber
-				AND L1.dblWeightPerQty > 1
-			) AS intItemUOMId
-		,W.intWorkOrderId
-		,W.dtmPlannedDate
+			WHERE L1.strLotNumber = IL.strLotNumber AND L1.dblWeightPerQty > 1
+		   ) AS intItemUOMId
+		 , W.intWorkOrderId
+		 , W.dtmPlannedDate
+		 , W.intLocationId			
+		 , CompanyLocation.strLocationName
+		 , Category.strCategoryCode
+		 , Category.strDescription
 	FROM dbo.tblMFWorkOrder W
-	JOIN dbo.tblMFWorkOrderInputLot WI ON WI.intWorkOrderId = W.intWorkOrderId
-		AND WI.ysnConsumptionReversed = 0
-		AND W.intStatusId = 13
+	JOIN dbo.tblMFWorkOrderInputLot WI ON WI.intWorkOrderId = W.intWorkOrderId AND WI.ysnConsumptionReversed = 0 AND W.intStatusId = 13
 	JOIN dbo.tblICItem I ON I.intItemId = W.intItemId
 	JOIN dbo.tblICItemUOM IU ON IU.intItemUOMId = WI.intItemUOMId
 	JOIN dbo.tblICUnitMeasure UM ON UM.intUnitMeasureId = IU.intUnitMeasureId
@@ -60,27 +60,32 @@ FROM (
 	JOIN dbo.tblICLot IL ON IL.intLotId = WI.intLotId
 	JOIN dbo.tblICParentLot IPL ON IPL.intParentLotId = IL.intParentLotId
 	JOIN dbo.tblICItem I1 ON I1.intItemId = IL.intItemId
-	WHERE I1.intCategoryId NOT IN (
-			SELECT PA.strAttributeValue
-			FROM tblMFManufacturingProcessAttribute PA
-			WHERE PA.intManufacturingProcessId = W.intManufacturingProcessId
-				AND PA.intLocationId = W.intLocationId
-				AND PA.intAttributeId = 46
-			)
-	) AS DT
+	JOIN dbo.tblSMCompanyLocation AS CompanyLocation ON W.intLocationId = CompanyLocation.intCompanyLocationId
+	JOIN dbo.tblICCategory AS Category ON I1.intCategoryId = Category.intCategoryId
+	WHERE I1.intCategoryId NOT IN 
+	(
+		SELECT PA.strAttributeValue
+		FROM tblMFManufacturingProcessAttribute PA
+		WHERE PA.intManufacturingProcessId = W.intManufacturingProcessId AND PA.intLocationId = W.intLocationId AND PA.intAttributeId = 46
+	)
+) AS DT
 LEFT JOIN dbo.tblICItemUOM IU1 ON IU1.intItemUOMId = DT.intItemUOMId
 LEFT JOIN dbo.tblICUnitMeasure UM1 ON UM1.intUnitMeasureId = IU1.intUnitMeasureId
 GROUP BY [Dump Date]
-	,[Product]
-	,[Product Description]
-	,[Production Lot]
-	,Line
-	,[Work Order #]
-	,[Job #]
-	,[WSI Item]
-	,[WSI Item Description]
-	,[Lot #]
-	,UM1.strUnitMeasure
-	,[Weight UOM]
-	,intWorkOrderId
-	,dtmPlannedDate
+	   , [Product]
+	   , [Product Description]
+	   , [Production Lot]
+	   , Line
+	   , [Work Order #]
+	   , [Job #]
+	   , [WSI Item]
+	   , [WSI Item Description]
+	   , [Lot #]
+	   , UM1.strUnitMeasure
+	   , [Weight UOM]
+	   , intWorkOrderId
+	   , dtmPlannedDate
+	   , intLocationId			
+	   , strLocationName
+	   , strCategoryCode
+	   , strDescription
