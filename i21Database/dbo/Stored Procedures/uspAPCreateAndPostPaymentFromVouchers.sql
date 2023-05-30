df2f4542b634f8e2938af4f6294719c951b54b32
Gary Malonzo <gary.malonzo@iRely.com>
2023-05-30 10:52:55 +0800

    AP-11782

--- i21Database/dbo/Stored Procedures/uspAPCreateAndPostPaymentFromVouchers.sql
+++ i21Database/dbo/Stored Procedures/uspAPCreateAndPostPaymentFromVouchers.sql
@@ -244,7 +244,7 @@ BEGIN
 			* 
 		FROM 
 		#tmpPartitionedVouchers
-	) sortedVouchers ' --ORDER BY ' +  CASE WHEN @clientSort = 'undefined' THEN  'strBillId ASC' ELSE @clientSort END
+	) sortedVouchers ORDER BY ' +  CASE WHEN @clientSort = 'undefined' THEN  'strBillId ASC' ELSE @clientSort END
 	
 	EXECUTE sp_executesql @script
 	
