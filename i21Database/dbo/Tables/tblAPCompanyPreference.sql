3a0d763854510d21153f7f8e57a490cfbc655198
Jeff Trajano <jeffrey.trajano@irely.com>
2023-06-01 08:44:34 +0800

    GL-10226

--- i21Database/dbo/Tables/tblAPCompanyPreference.sql
+++ i21Database/dbo/Tables/tblAPCompanyPreference.sql
@@ -19,7 +19,7 @@
 	[intDebitMemoInvoiceNoOption]   TINYINT NULL,
     [strVoucherImportTemplate]      NVARCHAR (50) COLLATE Latin1_General_CI_AS NOT NULL DEFAULT 'Standard', 
     [intImportTypeId]               INT NOT NULL DEFAULT 5,
-    [intConcurrencyId]              INT NOT NULL DEFAULT 0
+    [intConcurrencyId]              INT NOT NULL DEFAULT 0,
+    [ysnRemittanceAdvice_DisplayVendorAccountNumber] BIT NULL DEFAULT(1),
     PRIMARY KEY CLUSTERED (intCompanyPreferenceId ASC)
 );
-
