b7ca0b7db34d58c2fdb6c3a83de5fdd36a047a97
LexPeregrino <lex.peregrino@irely.com>
2023-06-01 15:35:36 +0800

    Revert "CT-8929 v1"
    This reverts commit b9a65c2598cbd341fd7ddfa690de962a0d4ee9da.

--- i21Database/dbo/Views/vyuCTFWCOverviewCompleted.sql
+++ i21Database/dbo/Views/vyuCTFWCOverviewCompleted.sql
@@ -90,7 +90,6 @@ AS
 			LEFT JOIN tblSMCity					LP	 WITH (NOLOCK) ON LP.intCityId			  =	CTD.intLoadingPortId
 			LEFT JOIN tblSMCity					DP	 WITH (NOLOCK) ON DP.intCityId			  =	CTD.intDestinationPortId
 			LEFT JOIN tblMFLocationLeadTime		MFL	 WITH (NOLOCK) ON CTD.intLoadingPortId    = MFL.intPortOfDispatchId AND  CTD.intDestinationPortId = MFL.intPortOfArrivalId
-			AND (SELECT  dbo.[fnCTGetSeqDisplayField](CTD.intContractDetailId, 'Origin')) = MFL.strOrigin
 			LEFT JOIN tblQMSaleYear				SY	 WITH (NOLOCK) ON SY.intSaleYearId		  = MFB.intSalesYear
 			LEFT JOIN tblSMPurchasingGroup		PG	 WITH (NOLOCK) ON PG.intPurchasingGroupId = CTD.intPurchasingGroupId
 			LEFT JOIN tblARMarketZone			MZ   WITH (NOLOCK) ON MZ.intMarketZoneId	  = CTD.intMarketZoneId
