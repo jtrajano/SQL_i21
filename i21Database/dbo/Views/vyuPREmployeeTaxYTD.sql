a7f106039a9a57798e5ca645724eba0737f2f2cc
Edsel Roldan Magura <edsel.magura@iRely.com>
2023-06-01 16:34:45 +0800

    PR-3237

---
+++ i21Database/dbo/Views/vyuPREmployeeTaxYTD.sql
@@ -0,0 +1,15 @@
+﻿CREATE VIEW [dbo].[vyuPREmployeeTaxYTD]
+AS
+	SELECT 
+		 intEntityEmployeeId = PaycheckTax.intEntityEmployeeId
+		,strTaxId			 = PaycheckTax.strTaxId
+		,strDescription		 = PaycheckTax.strDescription
+		,intYear			 = DATEPART(YEAR, PaycheckTax.dtmPayDate)
+		,dblTotal			 = SUM(PaycheckTax.dblTotal)
+		,dblTaxableAmount	 = SUM(PaycheckTax.dblTaxableAmount)
+	 FROM vyuPRPaycheckTax PaycheckTax 
+	 GROUP BY PaycheckTax.intEntityEmployeeId,
+			  PaycheckTax.strTaxId,
+			  PaycheckTax.strDescription,
+			  DATEPART(YEAR, PaycheckTax.dtmPayDate)
+ GO
