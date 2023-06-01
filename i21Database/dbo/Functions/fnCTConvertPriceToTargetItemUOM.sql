3a3fb7388289c6e42294b08e646a7803853a98e7
Jayson Ramos <jayson.ramos@iRely.com>
2023-06-01 20:06:43 +0800

    CT-8959

---
+++ i21Database/dbo/Functions/fnCTConvertPriceToTargetItemUOM.sql
@@ -0,0 +1,22 @@
+﻿CREATE FUNCTION [dbo].[fnCTConvertPriceToTargetItemUOM]
+(
+	@intFromItemUOMId	INT,
+	@intToItemUOMId	INT,
+	@dblPrice				NUMERIC(26,12)
+)
+RETURNS NUMERIC(26,12)
+AS 
+BEGIN 
+	declare
+		@dblFromUnitQuantity numeric(18,6),
+		@dblToUnitQuantity numeric(18,6),
+		@dblRet numeric(18,6);
+
+	select @dblFromUnitQuantity = dblUnitQty from tblICItemUOM where intItemUOMId = @intFromItemUOMId;
+	select @dblToUnitQuantity = dblUnitQty from tblICItemUOM where intItemUOMId = @intToItemUOMId;
+
+	select @dblRet = (@dblPrice/@dblFromUnitQuantity) * @dblToUnitQuantity
+
+	RETURN @dblRet
+END
+GO
