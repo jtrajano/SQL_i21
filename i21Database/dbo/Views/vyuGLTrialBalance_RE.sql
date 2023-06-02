21bf178f62c08d014ea530348e761f3abbbd8180
Jeff Trajano <jeffrey.trajano@irely.com>
2023-06-02 13:23:01 +0800

    GL-10483

--- i21Database/dbo/Views/vyuGLTrialBalance_RE.sql
+++ i21Database/dbo/Views/vyuGLTrialBalance_RE.sql
@@ -1,32 +1,30 @@
 ﻿CREATE VIEW [dbo].[vyuGLTrialBalance_RE]
 AS
 WITH DETAIL as (
-
 	SELECT F.intRetainAccount intAccountId,
 		F.intFiscalYearId, 
 		P.intGLFiscalYearPeriodId,
 		P.dtmEndDate PeriodEnd, 
 		P.dtmStartDate PeriodStart, 
 		F.dtmDateFrom FiscalStart,
-		B.strAccountId
+		P.strPeriod,
+		DATEDIFF(DAY,F.dtmDateTo, F.dtmDateFrom) FiscalDays
 	FROM 
-		tblGLFiscalYear F 
-		JOIN tblGLFiscalYearPeriod P on F.intFiscalYearId = P.intFiscalYearId
-		JOIN tblGLAccount B on B.intAccountId = F.intRetainAccount
+		tblGLFiscalYear F JOIN
+		tblGLFiscalYearPeriod P on F.intFiscalYearId = P.intFiscalYearId
 )
 SELECT 
-	Fiscal.intAccountId, 
-	Fiscal.intFiscalYearId, 
-	Fiscal.intGLFiscalYearPeriodId,
-	Fiscal.PeriodStart,
-	PeriodEnd.beginBalance YTD,
-	PERIODACTIVITY.beginningBalance MTD
+	Fiscal.*,
+	ISNULL(ExpRev_Prior.beginningBalance,0) YTD,
+	0 MTD,
+	ExpRev_Prior.intCurrencyId
+	--,Fiscal.strPeriod
 FROM DETAIL Fiscal
-OUTER APPLY dbo.fnGLGetBeginningBalanceAndUnitRETB(Fiscal.strAccountId,DATEADD(DAY, 1, Fiscal.PeriodEnd)) PeriodEnd
-OUTER APPLY(
-	SELECT  SUM(isnull(dblDebit,0) - isnull(dblCredit,0)) beginningBalance
+OUTER APPLY ( 
+	SELECT  D.intCurrencyId, SUM(ISNULL(dblDebit,0) - ISNULL(dblCredit,0)) beginningBalance
 	FROM tblGLAccount A LEFT JOIN  tblGLDetail D on D.intAccountId = A.intAccountId
-	WHERE D.dtmDate Between Fiscal.PeriodStart and Fiscal.PeriodEnd and D.ysnIsUnposted = 0
-	AND Fiscal.intAccountId = A.intAccountId
-)PERIODACTIVITY
-
+	LEFT JOIN tblGLAccountGroup G ON G.intAccountGroupId = A.intAccountGroupId
+	WHERE D.dtmDate < Fiscal.FiscalStart and D.ysnIsUnposted = 0
+	AND G.strAccountType in ('Expense','Revenue')  
+	GROUP BY D.intCurrencyId
+)ExpRev_Prior
