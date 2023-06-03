fc6bc6e4db1301478e2235b7d7dbf66f6f998614
Jase Rueda <jase.rueda@iRely.com>
2023-06-03 15:02:30 +0800

    CS-577

---
+++ i21Database/dbo/Tables/tblTMTankMonitorStatus.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tblTMTankMonitorStatus]
+(
+    [intTankMonitorStatusId] INT NOT NULL IDENTITY,
+    [intDeviceId] INT NOT NULL,
+    [dtmDate] DATETIME NOT NULL,
+    [ysnInternetConnectivity] BIT NOT NULL,
+    [ysnATGConnectivity] BIT NOT NULL,
+    [intConcurrencyId] INT NULL,
+    CONSTRAINT [PK_tblTMTankMonitorStatus] PRIMARY KEY CLUSTERED ([intTankMonitorStatusId] ASC),
+    CONSTRAINT [FK_tblTMTankMonitorStatus_tblTMDevice] FOREIGN KEY ([intDeviceId]) REFERENCES [tblTMDevice]([intDeviceId]) ON DELETE CASCADE,
+)
