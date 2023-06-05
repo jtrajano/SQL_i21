b73359c65e51217aeeb4832bc65ee64c21b0e274
DESKTOP-VFHGDG0\iRelyNUC015 <denny.divinagracia@irely.com>
2023-06-05 14:07:27 +0800

    TM-3797

---
+++ i21Database/dbo/Tables/tblTMConsumptionSiteFee.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[tblTMConsumptionSiteFee] (
+[intConcurrencyId]   INT  DEFAULT 1 NOT NULL,
+[intConsumptionSiteFeeId] INT IDENTITY (1, 1) NOT NULL,
+[DateTime] DATETIME        DEFAULT 0 NULL,
+[strType]			NVARCHAR (100)  COLLATE Latin1_General_CI_AS DEFAULT ('') NULL,
+[strDecription]			NVARCHAR (100)  COLLATE Latin1_General_CI_AS DEFAULT ('') NULL,
+[dblFee]			NUMERIC (18, 6) DEFAULT 0 NULL,
+[intSiteId]			INT NOT	NULL,
+CONSTRAINT [PK_tblTMConsumptionSiteFee] PRIMARY KEY ([intConsumptionSiteFeeId]),
+CONSTRAINT [FK_tblTMConsumptionSiteFee_tblTMSite] FOREIGN KEY ([intSiteId]) REFERENCES [dbo].[tblTMSite] ([intSiteID])
+)
+GO
+CREATE INDEX [IX_tblTMConsumptionSiteFee_intSiteId] ON [dbo].[tblTMConsumptionSiteFee] ([intSiteId])
+GO
