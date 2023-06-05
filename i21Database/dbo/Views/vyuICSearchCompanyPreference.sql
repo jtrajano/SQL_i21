66285e7edb4414304bae2cabf09d52d978d455bc
Feb Montefrio <feb.montefrio@irely.com>
2023-06-05 20:02:45 +0800

    [F] IC-11556 IC-11657
    (cherry picked from commit cbd0b150f845dffbb023a03bcffebb14fb79d34d)

--- i21Database/dbo/Views/vyuICSearchCompanyPreference.sql
+++ i21Database/dbo/Views/vyuICSearchCompanyPreference.sql
@@ -1,14 +1,16 @@
-﻿CREATE VIEW [dbo].[vyuICSearchCompanyPreference]
---Fixing to right view name vyuICGetCompanyPreference(not use) to vyuICSearchCompanyPreference
+﻿--Fixing to right view name vyuICGetCompanyPreference (do not use) to vyuICSearchCompanyPreference
+CREATE VIEW [dbo].[vyuICSearchCompanyPreference]
 AS
-
 SELECT 
 	ic.* 
 	-- Get the company preference from the other modules that is related with IC. 
 	,ysnImposeReversalTransaction = CAST(0 AS BIT) --rk.ysnImposeReversalTransaction
 	,i.strItemNo
-	,(SELECT ysnEnable FROM tblSMStartingNumber WHERE intStartingNumberId = '185') AS ysnEnable
-	,strEnableIntraCompanyTransfer = CASE WHEN ic.ysnEnableIntraCompanyTransfer = 1 THEN 'Yes' ELSE 'No' END 
+	,startingNumber.ysnEnable
+	,strEnableIntraCompanyTransfer = CASE WHEN ic.ysnEnableIntraCompanyTransfer = 1 THEN 'Yes' ELSE 'No' END 	
+	,strOverrideCompanySegment = CASE WHEN ic.ysnOverrideCompanySegment = 1 OR ic.ysnOverrideCompanySegment IS NULL THEN 'Yes' ELSE 'No' END 
+	,strOverrideLocationSegment = CASE WHEN ic.ysnOverrideLocationSegment = 1 OR ic.ysnOverrideLocationSegment IS NULL THEN 'Yes' ELSE 'No' END 
+	,strOverrideLOBSegment = CASE WHEN ic.ysnOverrideLOBSegment = 1 OR ic.ysnOverrideLOBSegment IS NULL THEN 'Yes' ELSE 'No' END 
 FROM 
 	tblICCompanyPreference ic
 	OUTER APPLY (
@@ -19,4 +21,9 @@ FROM
 	) rk 
 	LEFT JOIN tblICItem i 
 		ON i.intItemId = ic.intItemIdHolderForReceiptImport
-	 
+	OUTER APPLY (
+		SELECT	TOP 1 
+				ysnEnable 
+		FROM	tblSMStartingNumber 
+		WHERE	intStartingNumberId = '185'
+	) startingNumber
