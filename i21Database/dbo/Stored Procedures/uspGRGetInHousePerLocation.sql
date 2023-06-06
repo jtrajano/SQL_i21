a1ed7daec921569542817e437c334a3a5cd81eb5
Leslie Guevarra <leslie.arat@irely.com>
2023-06-06 10:28:33 +0800

    Revert "GRN-3379 (SI-224636(SCALE TICKET 240013)is not showing on the GII report)"

--- i21Database/dbo/Stored Procedures/uspGRGetInHousePerLocation.sql
+++ i21Database/dbo/Stored Procedures/uspGRGetInHousePerLocation.sql
@@ -128,7 +128,7 @@ BEGIN
 		,strLocationName
 	)
 	SELECT
-		dtmDate = CONVERT(DATETIME,CONVERT(VARCHAR(10),AR.dtmPostDate,110),110)
+		dtmDate = CONVERT(DATETIME,CONVERT(VARCHAR(10),SC.dtmTicketDateTime,110),110)
 		,dblTotal = QTY.dblResultQty
 		,AR.strInvoiceNumber
 		,'Invoice'
