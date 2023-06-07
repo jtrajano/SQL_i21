b5f61650d891ab044672c1346ee58f55ad5d7fb7
Feb Montefrio <feb.montefrio@irely.com>
2023-06-07 19:29:50 +0800

    [F] IC-11556 IC-11657

--- i21Database/dbo/Functions/fnGetItemGLAccountAsTable.sql
+++ i21Database/dbo/Functions/fnGetItemGLAccountAsTable.sql
@@ -6,7 +6,7 @@
 RETURNS TABLE 
 RETURN (
 
-	SELECT intAccountId	= ISNULL(CompanySegment.intAccountId, LocationSegment.intAccountId) 
+	SELECT intAccountId	= COALESCE(LOBSegment.intAccountId, CompanySegment.intAccountId, LocationSegment.intAccountId) 
 	FROM	(
 				-- Re-create the strAccountId (Original Account + Account Structure to modify)
 				SELECT strAccountId = STUFF(
@@ -36,7 +36,12 @@ RETURN (
 																
 															-- Join in this sub-query will get the base-account id 
 															INNER JOIN (
-																SELECT	intAccountId = ISNULL(ItemLevel.intAccountId, ISNULL(CommodityLevel.intAccountId, ISNULL(CategoryLevel.intAccountId, CompanyLocationLevel.intAccountId)))
+																SELECT	intAccountId = COALESCE(
+																			ItemLevel.intAccountId
+																			, CommodityLevel.intAccountId
+																			, CategoryLevel.intAccountId
+																			, CompanyLocationLevel.intAccountId
+																		)
 																FROM	(
 																			-- Get the base acccount at the item-level
 																			SELECT	TOP 1 
@@ -169,4 +174,65 @@ RETURN (
 			) RecreatedAccountUsingCompanySegment LEFT JOIN tblGLAccount CompanySegment 
 				-- To be sure, cross reference the re-created account id with the tblGLAccount table
 				ON RecreatedAccountUsingCompanySegment.strAccountId = CompanySegment.strAccountId COLLATE Latin1_General_CI_AS
+
+			OUTER APPLY (
+
+				-- Re-create the strAccountId (Original Account + Account Structure to modify)
+				SELECT strAccountId = STUFF(
+									(	
+										SELECT	Divider.strMask + RecreateStructure.strCode
+										FROM	tblGLAccountSegment RecreateStructure INNER JOIN (
+													SELECT	intAccountSegmentId = 
+																CASE	WHEN EXISTS (
+																			-- Get the structure id for tblSMLineOfBusiness.intSegmentCodeId. if it matches, use it as the override. 
+																			SELECT	B.intAccountStructureId
+																			FROM	tblGLAccountSegment A INNER JOIN tblGLAccountStructure B
+																						ON A.intAccountStructureId = B.intAccountStructureId
+																			WHERE	A.intAccountSegmentId = tblSMLineOfBusiness.intSegmentCodeId
+																					AND B.intAccountStructureId = Structure.intAccountStructureId
+																		) 
+																		THEN 
+																			tblSMLineOfBusiness.intSegmentCodeId
+																		ELSE 
+																			SegmentMap.intAccountSegmentId 
+																END 
+															,Structure.intSort 
+													FROM	tblGLAccountStructure Structure INNER JOIN tblGLAccountSegment Segment
+																ON Structure.intAccountStructureId = Segment.intAccountStructureId
+															INNER JOIN tblGLAccountSegmentMapping SegmentMap
+																ON Segment.intAccountSegmentId = SegmentMap.intAccountSegmentId
+																AND SegmentMap.intAccountId = ISNULL(CompanySegment.intAccountId, LocationSegment.intAccountId) 
+															
+															-- Join in this table will get the Company Segment (value of intAccountSegmentId as stored in tblSMCompanyLocation.intCompanySegment)
+															INNER JOIN tblICItem 
+																ON tblICItem.intItemId = @intItemId
+															INNER JOIN tblICItemLocation 
+																ON tblICItemLocation.intItemLocationId = @intItemLocationId
+																AND tblICItemLocation.intItemId = @intItemId 
+															INNER JOIN tblICCommodity 
+																ON tblICCommodity.intCommodityId = tblICItem.intCommodityId
+															INNER JOIN tblSMLineOfBusiness 
+																ON tblSMLineOfBusiness.intLineOfBusinessId = tblICCommodity.intLineOfBusinessId
+																
+													WHERE	Structure.strType <> 'Divider'
+												) AS TemplateStructure 
+													ON RecreateStructure.intAccountSegmentId = TemplateStructure.intAccountSegmentId
+												,(
+													SELECT TOP 1 
+															strMask = ISNULL(strMask, '')
+													FROM	tblGLAccountStructure
+													WHERE	strType = 'Divider'
+												) AS Divider							
+										ORDER BY TemplateStructure.intSort
+										FOR XML PATH('')
+									)
+									, 1
+									, 1 -- We expect the divider used in COA setup is always one character. 
+									, '' 
+							)				
+			
+			) RecreatedAccountUsingLOBSegment LEFT JOIN tblGLAccount LOBSegment 
+				-- To be sure, cross reference the re-created account id with the tblGLAccount table
+				ON RecreatedAccountUsingLOBSegment.strAccountId = LOBSegment.strAccountId COLLATE Latin1_General_CI_AS
+
 )
--- i21Database/dbo/Views/vyuICSearchCompanyPreference.sql
+++ i21Database/dbo/Views/vyuICSearchCompanyPreference.sql
@@ -10,7 +10,7 @@ SELECT
 	,strEnableIntraCompanyTransfer = CASE WHEN ic.ysnEnableIntraCompanyTransfer = 1 THEN 'Yes' ELSE 'No' END 	
 	,strOverrideCompanySegment = CASE WHEN ic.ysnOverrideCompanySegment = 1 OR ic.ysnOverrideCompanySegment IS NULL THEN 'Yes' ELSE 'No' END 
 	,strOverrideLocationSegment = CASE WHEN ic.ysnOverrideLocationSegment = 1 OR ic.ysnOverrideLocationSegment IS NULL THEN 'Yes' ELSE 'No' END 
-	,strOverrideLOBSegment = CASE WHEN ic.ysnOverrideLOBSegment = 1 OR ic.ysnOverrideLOBSegment IS NULL THEN 'Yes' ELSE 'No' END 
+	,strOverrideLOBSegment = CASE WHEN ic.ysnOverrideLOBSegment = 1 THEN 'Yes' ELSE 'No' END 
 FROM 
 	tblICCompanyPreference ic
 	OUTER APPLY (
