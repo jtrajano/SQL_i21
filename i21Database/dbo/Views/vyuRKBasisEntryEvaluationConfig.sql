9429e943a34b5ad147427a75de47ab763e8c2ad6
Michael Mirho Villaluz <mirho.villaluz@irely.com>
2023-06-09 13:27:46 +0800

    RM-5406

---
+++ i21Database/dbo/Views/vyuRKBasisEntryEvaluationConfig.sql
@@ -0,0 +1,22 @@
+CREATE VIEW [dbo].[vyuRKBasisEntryEvaluationConfig]
+AS
+SELECT TOP 1 
+	  intCompanyPreferenceId = rk.intCompanyPreferenceId
+	--, ysnIncludeProductInformation = rk.ysnIncludeProductInformation
+	, ysnEnterSeparateMarketBasisDifferentialsForBuyVsSell = rk.ysnEnterSeparateMarketBasisDifferentialsForBuyVsSell
+	, ysnEnterForwardCurveForMarketBasisDifferential = rk.ysnEnterForwardCurveForMarketBasisDifferential
+	, strEvaluationBy = rk.strEvaluationBy
+	, ysnEvaluationByLocation = rk.ysnEvaluationByLocation
+	, ysnEvaluationByMarketZone = rk.ysnEvaluationByMarketZone
+	, ysnEvaluationByOriginPort = rk.ysnEvaluationByOriginPort
+	, ysnEvaluationByDestinationPort = rk.ysnEvaluationByDestinationPort
+	, ysnEvaluationByCropYear = rk.ysnEvaluationByCropYear
+	, ysnEvaluationByStorageLocation = rk.ysnEvaluationByStorageLocation
+	, ysnEvaluationByStorageUnit = rk.ysnEvaluationByStorageUnit
+	--, ysnEnableMTMPoint = ct.ysnEnableMTMPoint 
+FROM tblRKCompanyPreference rk
+--OUTER APPLY (
+--	SELECT TOP 1 
+--		ysnEnableMTMPoint
+--	FROM tblCTCompanyPreference
+--) ct
