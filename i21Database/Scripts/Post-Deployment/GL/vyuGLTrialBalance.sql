1d09b1bc93ef7176bc1d8cb2d482a59c59fdc479
Jeff Trajano <jeffrey.trajano@irely.com>
2023-06-09 16:32:32 +0800

    GL-10464

--- i21Database/Scripts/Post-Deployment/GL/vyuGLTrialBalance.sql
+++ i21Database/Scripts/Post-Deployment/GL/vyuGLTrialBalance.sql
@@ -1,6 +1,20 @@
 --ADD LOB IF COLUMN / SEGMENT IS EXISTING IN THE STRUCTURE
 
+DECLARE @Lob NVARCHAR(10) ='', @Company  NVARCHAR(10) = '',
+@LobQuery NVARCHAR(100), @CompanyQuery NVARCHAR(100)
+
 IF COL_LENGTH('tblGLTempCOASegment','LOB') IS NOT NULL 
+BEGIN
+	SET @Lob = ',[LOB]'
+	SET @LobQuery = 'Segment.[LOB] COLLATE Latin1_General_CI_AS strLOBCode ,'
+END
+
+IF COL_LENGTH('tblGLTempCOASegment','Company') IS NOT NULL 
+BEGIN
+	SET @Company = ',[Company]'
+	SET @CompanyQuery = 'Segment.[Company] COLLATE Latin1_General_CI_AS strCompanySegment ,'
+END
+
 EXEC(
 'ALTER VIEW [dbo].[vyuGLTrialBalance] AS
 SELECT 
@@ -13,9 +27,9 @@ ISNULL( A.strAccountId,'''')  COLLATE Latin1_General_CI_AS strAccountId,
 ISNULL(strAccountType,'''') COLLATE Latin1_General_CI_AS strAccountType ,
 ISNULL(A.strCashFlow,'''') COLLATE Latin1_General_CI_AS strCashFlow,
 Segment.[Primary Account] COLLATE Latin1_General_CI_AS strPrimaryCode ,
-Segment.[Location] COLLATE Latin1_General_CI_AS strLocationCode ,
-Segment.[LOB] COLLATE Latin1_General_CI_AS strLOBCode ,
-ISNULL(A.strComments,'''') COLLATE Latin1_General_CI_AS strComments,
+Segment.[Location] COLLATE Latin1_General_CI_AS strLocationCode ,' +
+@LobQuery + @CompanyQuery +
+'ISNULL(A.strComments,'''') COLLATE Latin1_General_CI_AS strComments,
 ISNULL(strCurrency,'''') COLLATE Latin1_General_CI_AS strCurrency,
 ISNULL(coa.strCurrentExternalId,'''') COLLATE Latin1_General_CI_AS  strCurrentExternalId,
 ISNULL(A.strDescription,'''') COLLATE Latin1_General_CI_AS strDescription,
@@ -23,14 +37,14 @@ ISNULL(coa.strExternalId,'''') COLLATE Latin1_General_CI_AS strExternalId,
 ISNULL(A.strNote,'''') COLLATE Latin1_General_CI_AS strNote,
 ISNULL(strUOMCode,'''') COLLATE Latin1_General_CI_AS strUOMCode,
 ISNULL(A.ysnActive,0) ysnActive,
-TBSum.dtmDateFrom,
+TBSum.dtmDateFrom,	
 TBSum.dtmDateTo,
 TBSum.intGLFiscalYearPeriodId,
 TBSum.strPeriod
 FROM vyuGLAccountDetail A
 LEFT JOIN tblGLCOACrossReference coa ON A.intAccountId =coa.inti21Id 
 OUTER APPLY(
-	SELECT TOP 1 [Primary Account],[Location],[LOB] FROM tblGLTempCOASegment WHERE intAccountId = A.intAccountId
+	SELECT TOP 1 [Primary Account],[Location]' + @Lob +  @Company +'FROM tblGLTempCOASegment WHERE intAccountId = A.intAccountId
 )Segment
 OUTER APPLY (
 
@@ -45,4 +59,5 @@ OUTER APPLY (
 	WHERE intAccountId = A.intAccountId
 	GROUP BY intAccountId, TB.intGLFiscalYearPeriodId,dtmStartDate,dtmEndDate, FYP.strPeriod
 )TBSum')
+
 GO
