38c3756ba99e7da0ca6969ab74e61d6724300a2e
Dom Caoile <dom.caoile.02@gmail.com>
2023-06-12 01:07:12 +0800

    FRD-1994

--- i21Database/dbo/Tables/tblGLPosted.sql
+++ i21Database/dbo/Tables/tblGLPosted.sql
@@ -9,15 +9,15 @@
 	[dblCreditForeign]		NUMERIC(20, 6)	NULL,
 	[dblDebitUnit]			NUMERIC(20, 6)	NULL,
 	[dblCreditUnit]			NUMERIC(20, 6)	NULL,
-	[strCode]				NVARCHAR (100)  COLLATE Latin1_General_CI_AS NOT NULL,
+	[strCode]				NVARCHAR (100)  COLLATE Latin1_General_CI_AS NULL,
 	[intCurrencyId]			INT             NOT NULL DEFAULT 0,
 	[intConcurrencyId]		INT             NOT NULL,
 	[intLedgerId]			INT             NOT NULL DEFAULT 0,
 	[intSubledgerId]		INT             NOT NULL DEFAULT 0,
-	CONSTRAINT [PK_tblGLPosted] PRIMARY KEY CLUSTERED ([intAccountId] ASC,[dtmDate] ASC,[intCurrencyId] ASC,[strCode] ASC,[intLedgerId] ASC,[intSubledgerId] ASC),
+	CONSTRAINT [PK_tblGLPosted] PRIMARY KEY CLUSTERED ([intAccountId] ASC,[dtmDate] ASC,[intCurrencyId] ASC,[intLedgerId] ASC,[intSubledgerId] ASC),
 )
 GO
 CREATE NONCLUSTERED INDEX [IX_tblGLPosted_intAccountId_strCode_intCurrencyId_dtmDate]
-    ON [dbo].[tblGLPosted]([intAccountId] ASC, [strCode] ASC, [intCurrencyId] ASC, [dtmDate] ASC)
+    ON [dbo].[tblGLPosted]([intAccountId] ASC, [intCurrencyId] ASC, [dtmDate] ASC)
     INCLUDE (dblDebit, dblCredit, dblDebitUnit, dblCreditUnit, dblDebitForeign, dblCreditForeign);
 GO
