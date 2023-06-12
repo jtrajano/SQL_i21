d063b19cb69d50a848ed3d126d9bb0d6fc411a7b
thomasjasonpamilar <thomasjason.pamilar@irely.com>
2023-06-12 14:51:33 +0800

    QC-1154

--- i21Database/dbo/Stored Procedures/uspQMImportTastingScore.sql
+++ i21Database/dbo/Stored Procedures/uspQMImportTastingScore.sql
@@ -1280,7 +1280,7 @@ BEGIN TRY
 				,intItemUOMId = S.intSampleUOMId
 				,intWeightUOMId = S.intSampleUOMId
 				,strTeaOrigin = S.strCountry
-				,intOriginalItemId = S.intItemId
+				,intOriginalItemId = BT.intOriginalItemId
 				,dblPackagesPerPallet = IsNULL(I.intUnitPerLayer *I.intLayerPerPallet,20)
 				,strPlant = MU.strVendorRefNoPrefix 
 				,dblTotalQuantity = S.dblSampleQty 
