5cc2c69973ad797965546c711003a7f04f31332c
Gary Malonzo <gary.malonzo@iRely.com>
2023-06-13 17:42:58 +0800

    AP-11782

--- i21Database/dbo/Stored Procedures/uspAPCreateAndPostPaymentFromVouchers.sql
+++ i21Database/dbo/Stored Procedures/uspAPCreateAndPostPaymentFromVouchers.sql
@@ -168,7 +168,7 @@ BEGIN
 		,vendor.strVendorId
 		,entity.strName
 		,payTo.strCheckPayeeName
-		,voucher.dtmBillDate
+		,ISNULL(voucher.dtmBillDate, invoice.dtmDate) AS dtmBillDate
 		,ISNULL(voucher.dtmDueDate, invoice.dtmDueDate) AS dtmDueDate
 		,ISNULL(voucher.strBillId, invoice.strInvoiceNumber) AS strBillId
 		,ISNULL(voucher.strVendorOrderNumber, invoice.strInvoiceNumber) AS strVendorOrderNumber
