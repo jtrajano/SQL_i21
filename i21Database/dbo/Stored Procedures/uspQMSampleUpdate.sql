8957dc8fbd429e200640af145349ade95edac4ad
Ardrian Jhay Pineda <ardrianjhay.pineda@iRely.com>
2023-06-14 10:04:19 +0800

    QC-1144

--- i21Database/dbo/Stored Procedures/uspQMSampleUpdate.sql
+++ i21Database/dbo/Stored Procedures/uspQMSampleUpdate.sql
@@ -130,7 +130,8 @@ DECLARE @ysnSuccess BIT
  BEGIN  
   SELECT @dblConvertedSampleQty = dbo.fnCTConvertQuantityToTargetItemUOM(@intItemId, @intSampleUOMId, @intRepresentingUOMId, @dblSampleQty)  
   
-  IF @dblConvertedSampleQty > @dblRepresentingQty  
+  IF (@dblConvertedSampleQty > @dblRepresentingQty)
+    AND (EXISTS (SELECT 1 FROM dbo.tblQMCompanyPreference WHERE ysnValidateSampleQty = 1))  
   BEGIN  
    RAISERROR (  
      'Sample Qty cannot be greater than Representing Qty. '  
