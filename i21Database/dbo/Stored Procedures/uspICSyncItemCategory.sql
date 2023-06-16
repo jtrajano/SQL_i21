e4bb8ea59b2ba8c891750eeea44ecd0a90f610c6
Rhen Solis <rhen.solis@iRely.com>
2023-06-16 09:50:15 +0800

    ST-2264

---
+++ i21Database/dbo/Stored Procedures/uspICSyncItemCategory.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[uspICSyncItemCategory]
+    @intItemNo INT = NULL,
+	@intCategoryId INT = NULL
+AS
+BEGIN
+    UPDATE tblSTPumpItem
+    SET tblSTPumpItem.intCategoryId = @intCategoryId
+    FROM tblSTPumpItem PU
+	JOIN tblICItemUOM UOM 
+	ON PU.intItemUOMId = UOM.intItemUOMId
+	JOIN tblICItem I
+	ON UOM.intItemId = I.intItemId
+	Where I.intItemId = @intItemNo
+END
