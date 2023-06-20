ec47f325188f0c47b6d0e9fe8c13f622de63114e
Prem Selladurai <prem.selladurai@irely.com>
2023-06-20 11:20:40 +0530

    IN-1238
    IN-1238

--- i21Database/dbo/Stored Procedures/uspIPGenerateERPAuctionStock.sql
+++ i21Database/dbo/Stored Procedures/uspIPGenerateERPAuctionStock.sql
@@ -97,6 +97,7 @@ BEGIN TRY
 				AND S.intMarketZoneId = 1
 				and S.intLocationId IS NOT NULL
 				and S.intCurrencyId IS NOT NULL
+				AND isNULL(S.dblB1QtyBought , 0) + isNULL(S.dblB2QtyBought , 0) + isNULL(S.dblB3QtyBought , 0) + isNULL(S.dblB4QtyBought , 0) + isNULL(S.dblB5QtyBought , 0)>0
 			) AS DT
 		ORDER BY intItemId
 	END
@@ -190,6 +191,7 @@ BEGIN TRY
 	WHERE S.dtmSaleDate BETWEEN @dtmStartDayOfLast7Days
 			AND @dtmStartDayOfWeek
 		AND S.intMarketZoneId = 1
+		AND isNULL(S.dblB1QtyBought , 0) + isNULL(S.dblB2QtyBought , 0) + isNULL(S.dblB3QtyBought , 0) + isNULL(S.dblB4QtyBought , 0) + isNULL(S.dblB5QtyBought , 0)>0
 	GROUP BY S.intItemId
 		,S.intLocationId
 		,S.intCurrencyId
@@ -217,6 +219,7 @@ BEGIN TRY
 	WHERE S.dtmSaleDate BETWEEN @dtmStartDayOfLast28Days
 			AND @dtmStartDayOfWeek
 		AND S.intMarketZoneId = 1
+		AND isNULL(S.dblB1QtyBought , 0) + isNULL(S.dblB2QtyBought , 0) + isNULL(S.dblB3QtyBought , 0) + isNULL(S.dblB4QtyBought , 0) + isNULL(S.dblB5QtyBought , 0)>0
 	GROUP BY S.intItemId
 		,S.intLocationId
 		,S.intCurrencyId
