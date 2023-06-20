3ac096d4b14b02af1ce833e511577a35e1b4ad57
thomasjasonpamilar <thomasjason.pamilar@irely.com>
2023-06-20 12:07:18 +0800

    QC-1172

--- i21Database/dbo/Views/vyuQMTINLabelReport.sql
+++ i21Database/dbo/Views/vyuQMTINLabelReport.sql
@@ -6,10 +6,10 @@ SELECT intBatchId					= B.intBatchId
 	, strGardenMark					= GM.strGardenMark
 	, strTeaGardenChopInvoiceNumber	= B.strTeaGardenChopInvoiceNumber
 	, intMixingUnitLocationId		= B.intMixingUnitLocationId
-	, strMixingUnit					= CL.strLocationNumber
+	, strMixingUnit					= CL.strLocationName
 	, intTealingoItemId				= B.intTealingoItemId
 	, strTealingoItem				= I.strItemNo
-	, dtmExpiryDate					= B.dtmExpiration
+	, dtmExpiryDate					= Item.dtmExpiration
 	, strERPPONumber				= SUBSTRING(ISNULL(B.strERPPONumber, ''), LEN(ISNULL(B.strERPPONumber, '')) - 4, LEN(ISNULL(B.strERPPONumber, '')))
 	, strContainerNumber			= B.strContainerNumber
 	, strBOLNo						= B.strBOLNo
@@ -17,5 +17,21 @@ FROM tblMFBatch B
 LEFT JOIN tblQMGardenMark GM ON B.intGardenMarkId = GM.intGardenMarkId
 LEFT JOIN tblSMCompanyLocation CL ON B.intMixingUnitLocationId = CL.intCompanyLocationId
 LEFT JOIN tblICItem I ON B.intTealingoItemId = I.intItemId
+OUTER APPLY(
+    SELECT TOP 1 strItemNo,strDescription, strShortName,
+    CASE WHEN ISNULL(intLifeTime,0) > 0 AND B.dtmProductionBatch IS NOT NULL
+    THEN
+    CASE 
+        WHEN  strLifeTimeType = 'Years' THEN DATEADD( YEAR, intLifeTime, B.dtmProductionBatch)
+        WHEN  strLifeTimeType = 'Months' THEN DATEADD( MONTH, intLifeTime, B.dtmProductionBatch)
+        WHEN  strLifeTimeType = 'Days' THEN DATEADD( DAY, intLifeTime, B.dtmProductionBatch)
+        WHEN  strLifeTimeType = 'Hours' THEN DATEADD( HOUR, intLifeTime, B.dtmProductionBatch)
+        WHEN  strLifeTimeType = 'Minutes' THEN DATEADD( MINUTE, intLifeTime, B.dtmProductionBatch)
+        ELSE NULL END
+    ELSE 
+    NULL 
+    END dtmExpiration
+    FROM tblICItem WHERE intItemId = B.intTealingoItemId
+)Item
 WHERE B.strContainerNumber IS NOT NULL
  AND B.strContainerNumber <> ''
