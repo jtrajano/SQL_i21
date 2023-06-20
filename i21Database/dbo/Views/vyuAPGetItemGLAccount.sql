5a7cf931a337d1c804d6c4dd08b27691c1f9b434
ascaileen_ <aileengrace.asico@irely.com>
2023-06-20 11:17:15 +0800

    AP-12072

---
+++ i21Database/dbo/Views/vyuAPGetItemGLAccount.sql
@@ -0,0 +1,34 @@
+﻿CREATE VIEW [dbo].[vyuAPGetItemGLAccount]
+AS 
+SELECT 
+	ISNULL(ItemLevel.intItemId, CategoryLevel.intItemId) AS intItemId,
+    ISNULL(ItemLevel.intAccountId, CategoryLevel.intAccountId) AS intAccountId,
+	ISNULL(ItemLevel.strAccountId, CategoryLevel.strAccountId) AS strAccountId,
+	ISNULL(ItemLevel.strDescription, CategoryLevel.strDescription) AS strDescription,
+	ISNULL(ItemLevel.strAccountCategory, CategoryLevel.strAccountCategory) AS strAccountCategory
+FROM(
+		SELECT * FROM [dbo].[vyuICGetItemAccount] 
+		WHERE 
+			strAccountCategory = 'Other Charge Expense'
+	) AS ItemLevel
+	FULL JOIN 
+	(
+		SELECT	 
+			Item.intItemId,
+			CategoryAccounts.intAccountId,
+			GLAccnt.strAccountId,
+			GLAccnt.strDescription,
+			AccntCategory.strAccountCategory
+		FROM	
+			dbo.tblICItem Item INNER JOIN dbo.tblICCategory Category
+				ON Item.intCategoryId = Category.intCategoryId
+			INNER JOIN tblICCategoryAccount CategoryAccounts
+				ON Category.intCategoryId = CategoryAccounts.intCategoryId
+			INNER JOIN dbo.tblGLAccountCategory AccntCategory
+				ON CategoryAccounts.intAccountCategoryId = AccntCategory.intAccountCategoryId
+			INNER JOIN dbo.tblGLAccount GLAccnt
+				ON CategoryAccounts.intAccountId = GLAccnt.intAccountId
+		WHERE	
+			AccntCategory.strAccountCategory = 'Other Charge Expense'	
+	) AS CategoryLevel
+	ON ItemLevel.intItemId = CategoryLevel.intItemId 
