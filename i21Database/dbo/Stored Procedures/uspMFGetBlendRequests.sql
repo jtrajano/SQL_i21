a106e8261accf50ab4342231ccb596e09991d90d
Jonathan Valenzuela <jonathan.valenzuela@iRely.com>
2023-06-20 17:27:40 +0800

    MFG-5149

--- i21Database/dbo/Stored Procedures/uspMFGetBlendRequests.sql
+++ i21Database/dbo/Stored Procedures/uspMFGetBlendRequests.sql
@@ -43,6 +43,10 @@ IF @intWorkOrderId = 0
 		 , ri.dblLowerTolerance
 		 , (ri.dblCalculatedUpperTolerance * (a.dblQuantity / e.dblQuantity)) dblUpperToleranceQty
 		 , (ri.dblCalculatedLowerTolerance * (a.dblQuantity / e.dblQuantity)) dblLowerToleranceQty 
+		 , (a.dblQuantity - ISNULL(a.dblIssuedQty, 0)) + ((a.dblQuantity - ISNULL(a.dblIssuedQty, 0)) / ri.dblUpperTolerance) AS dblCalculatedUpperTolerance
+		 , (a.dblQuantity - ISNULL(a.dblIssuedQty, 0)) - ((a.dblQuantity - ISNULL(a.dblIssuedQty, 0)) / ri.dblLowerTolerance) AS dblCalculatedLowerTolerance 
+		 , Machine.strName AS strMachine
+		 , a.dblEstNoOfBlendSheet
 	FROM tblMFBlendRequirement a 
 	JOIN tblICItem b ON a.intItemId = b.intItemId 
 	JOIN tblICItemUOM c ON b.intItemId = c.intItemId AND a.intUOMId=c.intUnitMeasureId 
@@ -55,6 +59,9 @@ IF @intWorkOrderId = 0
 	LEFT JOIN tblMFRecipeItem ri ON r.intRecipeId =ri.intRecipeId and a.intItemId = ri.intItemId and intRecipeItemTypeId =2 
 	LEFT JOIN tblMFBudget bg ON a.intItemId = bg.intItemId AND a.intLocationId = bg.intLocationId AND bg.intYear = YEAR(GETDATE()) AND bg.intBudgetTypeId = 2
 	LEFT JOIN tblSMCompanyLocation AS CompanyLocation ON a.intLocationId = CompanyLocation.intCompanyLocationId
+	OUTER APPLY (SELECT TOP 1 MFMachine.strName
+				 FROM tblMFMachine AS MFMachine
+				 WHERE MFMachine.intMachineId = a.intMachineId) AS Machine
 	WHERE a.intStatusId = 1
 
 --Positive means WorkOrderId
@@ -83,6 +90,9 @@ IF @intWorkOrderId > 0
 		 , ISNULL(f.dblCalculatedUpperTolerance, (ri.dblCalculatedUpperTolerance * (a.dblQuantity / e.dblQuantity))) AS dblCalculatedUpperTolerance
 		 , ISNULL(f.dblCalculatedLowerTolerance, (ri.dblCalculatedLowerTolerance * (a.dblQuantity / e.dblQuantity))) AS dblCalculatedLowerTolerance 
 		 , f.ysnOverrideRecipe
+		 , Machine.strName AS strMachine
+		 , a.dblEstNoOfBlendSheet
+		 , a.dblBlenderSize
 	FROM tblMFBlendRequirement a 
 	JOIN tblICItem b ON a.intItemId = b.intItemId 
 	JOIN tblICItemUOM c ON b.intItemId = c.intItemId AND a.intUOMId = c.intUnitMeasureId 
@@ -97,6 +107,9 @@ IF @intWorkOrderId > 0
 				 FROM tblMFWorkOrder AS WorkOrder
 				 JOIN tblMFWorkOrderStatus AS WorkOrderStatus ON WorkOrder.intStatusId = WorkOrderStatus.intStatusId
 				 WHERE WorkOrder.intBlendRequirementId = a.intBlendRequirementId) AS WorkOrderStatus
+	OUTER APPLY (SELECT TOP 1 MFMachine.strName
+				 FROM tblMFMachine AS MFMachine
+				 WHERE MFMachine.intMachineId = a.intMachineId) AS Machine
 	WHERE f.intWorkOrderId = @intWorkOrderId
 	GROUP BY a.intBlendRequirementId
 		 , a.strDemandNo
@@ -119,6 +132,9 @@ IF @intWorkOrderId > 0
 		 , ISNULL(f.dblCalculatedUpperTolerance, (ri.dblCalculatedUpperTolerance * (a.dblQuantity / e.dblQuantity)))
 		 , ISNULL(f.dblCalculatedLowerTolerance, (ri.dblCalculatedLowerTolerance * (a.dblQuantity / e.dblQuantity))) 
 		 , f.ysnOverrideRecipe
+		 , Machine.strName
+		 , a.dblEstNoOfBlendSheet
+		 , a.dblBlenderSize
 
 
 --Negative means BlendRequirementId
@@ -145,6 +161,9 @@ IF @intWorkOrderId < 0
 		--  , (ri.dblCalculatedLowerTolerance * (a.dblQuantity / e.dblQuantity)) AS dblCalculatedLowerTolerance 
 		 , (a.dblQuantity + (a.dblQuantity * (ri.dblUpperTolerance / 100))) AS dblCalculatedUpperTolerance
 		 , (a.dblQuantity - (a.dblQuantity * (ri.dblLowerTolerance / 100))) AS dblCalculatedLowerTolerance  
+		 , Machine.strName AS strMachine
+		 , a.dblEstNoOfBlendSheet
+		 , a.dblBlenderSize
 	FROM tblMFBlendRequirement a 
 	JOIN tblICItem b ON a.intItemId = b.intItemId 
 	JOIN tblICItemUOM c ON b.intItemId = c.intItemId AND a.intUOMId=c.intUnitMeasureId 
@@ -154,6 +173,9 @@ IF @intWorkOrderId < 0
 	LEFT JOIN tblICItemLocation g ON b.intItemId = g.intItemId AND g.intLocationId = a.intLocationId
 	LEFT JOIN tblICItemPricing h ON h.intItemId = b.intItemId AND g.intItemLocationId = h.intItemLocationId
 	LEFT JOIN tblSMCompanyLocation AS CompanyLocation ON a.intLocationId = CompanyLocation.intCompanyLocationId
+	OUTER APPLY (SELECT TOP 1 MFMachine.strName
+				 FROM tblMFMachine AS MFMachine
+				 WHERE MFMachine.intMachineId = a.intMachineId) AS Machine
 	WHERE a.intBlendRequirementId = ABS(@intWorkOrderId)
 	GROUP BY a.intBlendRequirementId
 		 , a.strDemandNo
@@ -176,3 +198,6 @@ IF @intWorkOrderId < 0
 		 , ri.dblLowerTolerance				
 		 , (ri.dblCalculatedUpperTolerance * (a.dblQuantity / e.dblQuantity)) 
 		 , (ri.dblCalculatedLowerTolerance * (a.dblQuantity / e.dblQuantity))
+		 , Machine.strName
+		 , a.dblEstNoOfBlendSheet
+		 , a.dblBlenderSize
