ae82dec462dad2a873d28ce1b8935cb793a864f3
Jeff Trajano <jeffrey.trajano@irely.com>
2023-06-21 15:24:49 +0800

    GL-10575

--- i21Database/dbo/Tables/tblGLIntraCompanyConfig.sql
+++ i21Database/dbo/Tables/tblGLIntraCompanyConfig.sql
@@ -9,3 +9,33 @@ CREATE TABLE tblGLIntraCompanyConfig
 	CONSTRAINT [PK_tblGLIntraCompanyConfig] PRIMARY KEY CLUSTERED (intIntraCompanyConfigId ASC),
     CONSTRAINT UNIQUE_PARENT_TARGET_tblGLIntraCompanyConfig UNIQUE (intParentCompanySegmentId,intTargetCompanySegmentId)
 )
+GO
+
+ALTER TABLE [dbo].[tblGLIntraCompanyConfig]  WITH CHECK ADD  CONSTRAINT [FK_tblGLIntraCompanyConfig_tblGLAccount_DueFrom] FOREIGN KEY([intDueFromAccountId])
+REFERENCES [dbo].[tblGLAccount] ([intAccountId])
+GO
+
+ALTER TABLE [dbo].[tblGLIntraCompanyConfig] CHECK CONSTRAINT [FK_tblGLIntraCompanyConfig_tblGLAccount_DueFrom]
+GO
+
+ALTER TABLE [dbo].[tblGLIntraCompanyConfig]  WITH CHECK ADD  CONSTRAINT [FK_tblGLIntraCompanyConfig_tblGLAccount_DueTo] FOREIGN KEY([intDueToAccountId])
+REFERENCES [dbo].[tblGLAccount] ([intAccountId])
+GO
+
+ALTER TABLE [dbo].[tblGLIntraCompanyConfig] CHECK CONSTRAINT [FK_tblGLIntraCompanyConfig_tblGLAccount_DueTo]
+GO
+
+ALTER TABLE [dbo].[tblGLIntraCompanyConfig]  WITH CHECK ADD  CONSTRAINT [FK_tblGLIntraCompanyConfig_tblGLAccountSegment_ParentCompany] FOREIGN KEY([intParentCompanySegmentId])
+REFERENCES [dbo].[tblGLAccountSegment] ([intAccountSegmentId])
+GO
+
+ALTER TABLE [dbo].[tblGLIntraCompanyConfig] CHECK CONSTRAINT [FK_tblGLIntraCompanyConfig_tblGLAccountSegment_ParentCompany]
+GO
+
+ALTER TABLE [dbo].[tblGLIntraCompanyConfig]  WITH CHECK ADD  CONSTRAINT [FK_tblGLIntraCompanyConfig_tblGLAccountSegment_TargetCompany] FOREIGN KEY([intTargetCompanySegmentId])
+REFERENCES [dbo].[tblGLAccountSegment] ([intAccountSegmentId])
+GO
+
+ALTER TABLE [dbo].[tblGLIntraCompanyConfig] CHECK CONSTRAINT [FK_tblGLIntraCompanyConfig_tblGLAccountSegment_TargetCompany]
+GO
+
