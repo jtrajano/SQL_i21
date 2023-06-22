01885e7d51ae33355b26a7ee92e51fa1c7b9fe1d
Smith Wilson G. de Jesus <smith.dejesus@irely.com>
2023-06-22 19:42:47 +0800

    RM-5384

--- i21Database/dbo/Functions/fnRKGetBucketOpenInTransit.sql
+++ i21Database/dbo/Functions/fnRKGetBucketOpenInTransit.sql
@@ -4,7 +4,7 @@
 	@intCommodityId INT,
 	@intVendorId INT
 )
-RETURNS @returntable TABLE
+RETURNS  @returntable TABLE
 (
 	  strBucketType NVARCHAR(50) COLLATE Latin1_General_CI_AS
 	, dtmCreateDate DATETIME
@@ -46,6 +46,7 @@ DECLARE  @tmpOpenInTransit TABLE
 (
 	dblTotal NUMERIC(18,6)
 	, intTransactionRecordHeaderId INT
+	, intContractDetailId INT
 	, strTransactionType NVARCHAR(100) COLLATE Latin1_General_CI_AS
 )
 
@@ -53,26 +54,29 @@ DECLARE  @tmpOpenInTransit TABLE
 ;WITH OutTransit (
 	  dblQty
 	, intTransactionReferenceId
+	, intContractDetailId
 	, strTransactionType
 ) AS (
 
 	SELECT 
 		dblQty = ROUND(SUM(dblQty),2)
 		, intTransactionReferenceId
+		, intContractDetailId
 		, strTransactionType = strTransactionType COLLATE Latin1_General_CI_AS
 	FROM tblRKDPRInTransitHelperLog
 	WHERE dtmDate < = @dtmDate
 	AND intCommodityId = @intCommodityId
-	GROUP BY intTransactionReferenceId, strTransactionType
+	GROUP BY intTransactionReferenceId, intContractDetailId, strTransactionType
 )
 
 
 
 insert into @tmpOpenInTransit
-select dblTotal = (t.dblTotal + ISNULL(OT.dblQty, 0)), intTransactionRecordHeaderId, t.strTransactionType from (
+select dblTotal = (t.dblTotal + ISNULL(OT.dblQty, 0)), intTransactionRecordHeaderId, t.intContractDetailId, t.strTransactionType from (
 	select 
 		dblTotal = ROUND(SUM(SL.dblOrigQty),2)
 		, intTransactionRecordHeaderId
+		, intContractDetailId
 		, strTransactionType
 	from vyuRKGetSummaryLog SL
 	WHERE strBucketType = 'Sales In-Transit'
@@ -80,20 +84,20 @@ select dblTotal = (t.dblTotal + ISNULL(OT.dblQty, 0)), intTransactionRecordHeade
 		AND CONVERT(DATETIME, CONVERT(VARCHAR(10), SL.dtmTransactionDate, 110), 110) <= CONVERT(DATETIME, @dtmDate)
 		AND ISNULL(SL.intCommodityId,0) = ISNULL(@intCommodityId, ISNULL(SL.intCommodityId, 0)) 
 		AND ISNULL(intEntityId, 0) = ISNULL(@intVendorId, ISNULL(intEntityId, 0))
-	GROUP BY intTransactionRecordHeaderId, strTransactionNumber, strTransactionType
+	GROUP BY intTransactionRecordHeaderId, strTransactionNumber, intContractDetailId, strTransactionType
 ) t
 LEFT JOIN OutTransit OT 
-	ON OT.intTransactionReferenceId = t.intTransactionRecordHeaderId and OT.strTransactionType = t.strTransactionType
+	ON OT.intTransactionReferenceId = t.intTransactionRecordHeaderId and OT.intContractDetailId = t.intContractDetailId AND OT.strTransactionType = t.strTransactionType
 WHERE (t.dblTotal + ISNULL(OT.dblQty, 0)) <> 0
 
 
 
 INSERT INTO @returntable
-	SELECT
+	SELECT  
 		  c.strBucketType
-		, c.dtmCreatedDate
-		, c.dtmTransactionDate
-		, dblTotal = c.dblOrigQty
+		, dtmCreatedDate = MAX(c.dtmCreatedDate)
+		, dtmTransactionDate = MAX(c.dtmTransactionDate)
+		, dblTotal = SUM(o.dblTotal)
 		, c.intEntityId
 		, c.strEntityName
 		, c.intLocationId
@@ -118,17 +122,45 @@ INSERT INTO @returntable
 		, c.strFutureMarket
 		, c.intFutureMonthId
 		, c.strFutureMonth
-		, strDeliveryDate = dbo.fnRKFormatDate(c.dtmEndDate, 'MMM yyyy')
+		, strDeliveryDate = dbo.fnRKFormatDate(MAX(c.dtmEndDate), 'MMM yyyy')
 		, strType = CASE WHEN strBucketType = 'Sales In-Transit' THEN 'Sales' ELSE 'Purchase' END
 		, intCurrencyId
 		, strCurrency
 	from vyuRKGetSummaryLog c
-	inner join @tmpOpenInTransit o on o.intTransactionRecordHeaderId = c.intTransactionRecordHeaderId and  o.strTransactionType = c.strTransactionType
+	inner join @tmpOpenInTransit o on o.intTransactionRecordHeaderId = c.intTransactionRecordHeaderId and  o.strTransactionType = c.strTransactionType and o.intContractDetailId = c.intContractDetailId
 	where strBucketType = 'Sales In-Transit'
 	AND c.strTransactionType IN('Inventory Shipment','Outbound Shipment')
 	AND CONVERT(DATETIME, CONVERT(VARCHAR(10), c.dtmTransactionDate, 110), 110) <= CONVERT(DATETIME, @dtmDate)
 	AND ISNULL(c.intCommodityId,0) = ISNULL(@intCommodityId, ISNULL(c.intCommodityId, 0)) 
 	AND ISNULL(intEntityId, 0) = ISNULL(@intVendorId, ISNULL(intEntityId, 0))
+	group by  c.strBucketType
+		, c.intEntityId
+		, c.strEntityName
+		, c.intLocationId
+		, c.strLocationName
+		, c.intItemId
+		, c.strItemNo
+		, c.intCategoryId
+		, c.strCategoryCode
+		, c.intCommodityId
+		, c.strCommodityCode
+		, c.strTransactionNumber
+		, c.strTransactionType
+		, c.intTransactionRecordHeaderId
+		, c.intOrigUOMId
+		, c.intContractDetailId
+		, c.intContractHeaderId
+		, c.strContractNumber
+		, c.strContractSeq
+		, c.intTicketId
+		, c.strTicketNumber
+		, c.intFutureMarketId
+		, c.strFutureMarket
+		, c.intFutureMonthId
+		, c.strFutureMonth
+		, intCurrencyId
+		, strCurrency
+
 
 RETURN
 
