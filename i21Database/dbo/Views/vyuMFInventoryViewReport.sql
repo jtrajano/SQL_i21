bfe31950643cdbc9e436b36958e852950eedfb05
Jonathan Valenzuela <jonathan.valenzuela@iRely.com>
2023-06-22 15:53:18 +0800

    MFG-4596

---
+++ i21Database/dbo/Views/vyuMFInventoryViewReport.sql
@@ -0,0 +1,46 @@
+﻿CREATE VIEW [dbo].[vyuMFInventoryViewReport]
+AS 
+/****************************************************************
+	Title: Inventory View Report
+	Description: Inventory View Report intended for Strauss 
+	JIRA: MFG-4596 
+	HD: HDTN-275494  
+	Created By: Jonathan Valenzuela
+	Date: 06/22/2023
+*****************************************************************/
+SELECT strLotNumber				AS [Item]
+	 , strParentLotNumber		AS [Parent Lot No]
+	 , strPrimaryStatus			AS [Status]
+	 , strItemNo				AS [Item Number]
+	 , strItemDescription		AS [Item Desc.]
+	 , strItemCategory			AS [Category]
+	 , strOwnershipType			AS [Ownership Type]
+	 , strCompanyLocationName	AS [Location]
+	 , strSubLocationName		AS [Storage Location]
+	 , strStorageLocationName	AS [Storage Unit]
+	 , dblWeight				AS [Qty]
+	 , strWeightUOM				AS [UOM]
+	 , dblWeightPerQty			AS [Weight Per Unit]
+	 , dblQty					AS [No of Packs]
+	 , strQtyUOM				AS [Packing UOM]
+	 , dblAvailableQty			AS [Available Qty]
+	 , dblAvailableNoOfPacks	AS [Available No of Packs]
+	 , intUnitPallet			AS [Units per Pallet]
+	 , dtmLastMoveDate			AS [Last Transaction Date]
+	 , dtmDateCreated			AS [Create Date]
+	 , dtmManufacturedDate		AS [Manufacturing Date]
+	 , dtmExpiryDate			AS [Expiry Date]
+	 , dtmDueDate				AS [Due Date]
+	 , strVendor				AS [Vendor]
+	 , dblLastCost				AS [Unit Cost]
+	 , strRestrictionType		AS [Restriction Type]
+	 , strBondStatus			AS [Bonded Status]
+	 , strContainerNo			AS [Container No]
+	 , strVendorRefNo			AS [Vendor Ref No]
+	 , strWarehouseRefNo		AS [WH Ref No / Order Entry No]
+	 , strReceiptNo				AS [Receipt No]
+	 , ysnPartialPallet			AS [Partial Pallet]
+	 , strNotes					AS [Remarks]
+	 , intAge					AS [Age(Days)]
+	 , intRemainingLife			AS [Remaining Life(Days)]
+FROM vyuMFInventoryView
---
+++ i21Database/dbo/Views/vyuMFRecipeReport.sql
@@ -0,0 +1,24 @@
+﻿CREATE VIEW [dbo].[vyuMFRecipeReport]
+AS 
+/****************************************************************
+	Title: Recipe View Report
+	Description: Recipe View Report intended for Strauss 
+	JIRA: MFG-4596 
+	HD: HDTN-275494  
+	Created By: Jonathan Valenzuela
+	Date: 06/22/2023
+*****************************************************************/
+SELECT strName				AS [Recipe Name]
+	 , strCustomer			AS [Customer]
+	 , strItemNo			AS [Item No]
+	 , strDescription		AS [Description]
+	 , dblQuantity			AS [Quantity]
+	 , strUOM				AS [UOM]
+	 , intVersionNo			AS [Version No]
+	 , ysnActive			AS [Active]
+	 , strLocationName		AS [Location]
+	 , dtmValidFrom			AS [Valid From]
+	 , dtmValidTo			AS [Valid To]
+	 , strComment			AS [Comment]
+	 , strProcessName		AS [Process Name]
+FROM vyuMFGetRecipe
