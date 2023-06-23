f7edfa26d47c4a4e4ab108794e5276c7bef94a30
Kenneth Sam Perez <kenneth.sam.perez@iRely.com>
2023-06-23 13:49:58 +0800

    MBIL-1365

--- i21Database/dbo/Views/vyuMBILGetLoads.sql
+++ i21Database/dbo/Views/vyuMBILGetLoads.sql
@@ -1,73 +1,81 @@
-﻿CREATE VIEW [dbo].[vyuMBILGetLoads]    
-    
-AS     
-    
-SELECT lh.intLoadHeaderId,     
- lh.strLoadNumber,     
- lh.strType,     
- lh.intDispatchOrderId,    
- lh.intLoadId,     
- lh.intDriverId,     
- lh.intHaulerId as intShipViaId,     
- lh.intTruckId,     
- lh.intTrailerId,     
- lh.strTrailerNo,     
- lh.dtmScheduledDate,    
- pd.intPickupDetailId,     
- pd.intEntityId as intVendorId,     
- pd.intEntityLocationId as intVendorLocationId,     
- sp.intSupplyPointId,     
- sp.strFreightSalesUnit,    
- el.intTaxGroupId as intOutboundTaxGroupId,     
- pd.intLoadDetailId,     
- isnull(pd.intSalespersonId,isnull(cel.intSalespersonId,arc.intSalespersonId)) AS intSalespersonId,     
- isnull(pd.intSellerId,trc.intSellerId)intSellerId,     
- pd.intTaxGroupId,     
- pd.intContractDetailId,     
- pd.intItemId,     
- pd.intCompanyLocationId as intReceiptCompanyLocationId,     
- RCL.strZipPostalCode,    
- pd.dblPickupQuantity,     
- pd.dblGross,     
- pd.dblNet,     
- pd.dtmPickupFrom,     
- pd.dtmPickupTo,     
- pd.dtmActualPickupFrom,     
- pd.dtmActualPickupTo,     
- pd.strBOL,     
- pd.strItemUOM,     
- pd.strLoadRefNo,     
- pd.strNote,     
- pd.strPONumber,     
- pd.strRack,     
- pd.strTerminalRefNo,     
- pd.ysnPickup,    
- dh.intDeliveryHeaderId,     
- dh.intEntityId as intCustomerId,     
- dh.intEntityLocationId as intCustomerLocationId,     
- dh.intCompanyLocationId as intDistributionCompanyLocationId,     
- isnull(dh.intSalesPersonId,isnull(cel.intSalespersonId,arc.intSalespersonId)) AS intSalesPersonId,  
- dh.dtmActualDelivery,     
- dd.intDeliveryDetailId,     
- dd.intTMDispatchId,     
- dd.intTMSiteId,     
- dd.strTank,     
- dd.dblStickStartReading,     
- dd.dblStickEndReading,     
- dd.dblWaterInches,     
- dd.dblPrice,      
- dd.dblDeliveredQty,     
- dd.dblPercentFull,    
- dd.ysnDelivered    
-FROM     
-tblMBILPickupDetail pd     
-JOIN tblMBILDeliveryDetail dd ON dd.intPickupDetailId = pd.intPickupDetailId     
-LEFT JOIN tblTRSupplyPoint sp ON sp.intEntityVendorId = pd.intEntityId  AND     
-       sp.intEntityLocationId = pd.intEntityLocationId     
-LEFT JOIN tblEMEntityLocation el ON el.intEntityLocationId = pd.intEntityLocationId    
-LEFT JOIN tblSMCompanyLocation RCL ON RCL.intCompanyLocationId = pd.intCompanyLocationId    
-JOIN tblMBILDeliveryHeader dh ON dh.intDeliveryHeaderId = dd.intDeliveryHeaderId     
-OUTER APPLY tblTRCompanyPreference trc  
+﻿CREATE VIEW [dbo].[vyuMBILGetLoads]
+
+AS
+
+SELECT lh.intLoadHeaderId
+	, lh.strLoadNumber
+	, strType = CASE WHEN pd.intEntityId IS NOT NULL  THEN 'Drop Ship' ELSE 'OutBound' END  
+	, lh.intDispatchOrderId
+	, lh.intLoadId
+	, lh.intDriverId
+	, lh.intHaulerId as intShipViaId
+	, lh.intTruckId
+	, lh.intTrailerId
+	, lh.strTrailerNo
+	, lh.dtmScheduledDate
+	, pd.intPickupDetailId
+	, pd.intEntityId as intVendorId
+	, pd.intEntityLocationId as intVendorLocationId
+	, sp.intSupplyPointId
+	, sp.strFreightSalesUnit
+	, intOutboundTaxGroupId = cel.intTaxGroupId
+	, pd.intLoadDetailId
+	, ISNULL(pd.intSalespersonId, ISNULL(cel.intSalespersonId, arc.intSalespersonId)) AS intSalespersonId
+	, ISNULL(pd.intSellerId, trc.intSellerId) intSellerId
+	, intTaxGroupId = el.intTaxGroupId
+	, pd.intContractDetailId as intReceiptContractDetailId
+	, dd.intContractDetailId
+	, pd.intItemId
+	, pd.intCompanyLocationId as intReceiptCompanyLocationId
+	, el.strZipCode as strZipPostalCode
+	, pd.dblPickupQuantity
+	, dblGross = case when tms.ysnCompanySite = 1 then isnull(dd.dblDeliveredQty,0) else pd.dblGross end
+	, dblNet = case when tms.ysnCompanySite = 1 then isnull(dd.dblDeliveredQty,0) else pd.dblNet end
+	, pd.dtmPickupFrom
+	, pd.dtmPickupTo
+	, pd.dtmActualPickupFrom
+	, pd.dtmActualPickupTo
+	, pd.strBOL
+	, LGLoadDetail.intItemUOMId
+	, pd.strItemUOM
+	, pd.strLoadRefNo
+	, pd.strNote
+	, pd.strPONumber
+	, pd.strRack
+	, pd.strTerminalRefNo
+	, pd.ysnPickup
+	, dh.intDeliveryHeaderId
+	, dh.intEntityId as intCustomerId
+	, dh.intEntityLocationId as intCustomerLocationId
+	, dh.intCompanyLocationId as intDistributionCompanyLocationId
+	, intCustomerSalesPersonId = ISNULL(dh.intSalesPersonId, ISNULL(cel.intSalespersonId, arc.intSalespersonId))
+	, dh.dtmActualDelivery
+	, dd.intDeliveryDetailId
+	, dd.intTMDispatchId
+	, dd.intTMSiteId
+	, dd.strTank
+	, dd.dblStickStartReading
+	, dd.dblStickEndReading
+	, dd.dblWaterInches
+	, dd.dblPrice
+	, dd.dblDeliveredQty
+	, dd.dblPercentFull
+	, dd.ysnDelivered
+	, lh.ysnDiversion
+	, lh.strDiversionNumber
+	, lh.intStateId
+	, pd.intDispatchOrderRouteId
+	, dd.intDispatchOrderDetailId
+	, ysnLockPrice = ISNULL(tm.ysnLockPrice, CAST(0 AS BIT))
+	, ISNULL(dd.dblGross,dd.dblDeliveredQty) as dblDistributionDetailGross
+	, ISNULL(dd.dblNet,dd.dblDeliveredQty) as dblDistributionDetailNet
+FROM tblMBILPickupDetail pd
+JOIN tblMBILDeliveryDetail dd ON dd.intPickupDetailId = pd.intPickupDetailId
+LEFT JOIN tblTRSupplyPoint sp ON sp.intEntityVendorId = pd.intEntityId AND sp.intEntityLocationId = pd.intEntityLocationId
+LEFT JOIN tblEMEntityLocation el ON el.intEntityLocationId = pd.intEntityLocationId
+LEFT JOIN tblSMCompanyLocation RCL ON RCL.intCompanyLocationId = pd.intCompanyLocationId
+JOIN tblMBILDeliveryHeader dh ON dh.intDeliveryHeaderId = dd.intDeliveryHeaderId
+OUTER APPLY tblTRCompanyPreference trc
 JOIN tblMBILLoadHeader lh ON lh.intLoadHeaderId = dh.intLoadHeaderId
 LEFT JOIN tblARCustomer arc ON arc.intEntityId = dh.intEntityId
 LEFT JOIN tblEMEntityLocation cel on cel.intEntityLocationId = dh.intEntityLocationId
