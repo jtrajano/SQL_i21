9ad005a950edfdea7db643118eaa593821e00b55
Jeff Trajano <jeffrey.trajano@irely.com>
2023-06-23 09:26:45 +0800

    GL-10275

--- i21Database/dbo/Stored Procedures/uspCMGridSelect.sql
+++ i21Database/dbo/Stored Procedures/uspCMGridSelect.sql
@@ -11,13 +11,15 @@ BEGIN
 	IF ISNULL(@intRelatedId,0) = 0
 	BEGIN
 		DELETE FROM tblCMGridSelectedRow WHERE strType = @strType
+
 		INSERT INTO tblCMGridSelectedRow (guidId, strType, intRelatedId)
 		SELECT @guidId, @strType, intEntityEFTInfoId FROM vyuEntityEFTInformation
 		WHERE
 		(ISNULL(dtmEffectiveDate, '2030-01-01') <= GETUTCDATE())
 		AND ISNULL(ysnActive,0) = 1
 		AND ISNULL(ysnPrenoteSent,0) = 0
-		AND ISNULL(Vendor, 0) = 1
+		AND ((ISNULL(Vendor, 0) = 1 AND @strType='toprenotevendor')
+		OR (ISNULL(Customer, 0) = 1 AND @strType='toprenotecustomer'))
 	END
 	ELSE
 	BEGIN
