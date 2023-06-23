00413be5c0558f4554bbef4d4aa2ec0effa012ae
Jeff Trajano <jeffrey.trajano@irely.com>
2023-06-23 15:26:22 +0800

    GL-10517

--- i21Database/dbo/Tables/tblCMCompanyPreferenceOption.sql
+++ i21Database/dbo/Tables/tblCMCompanyPreferenceOption.sql
@@ -2,5 +2,6 @@ CREATE TABLE [dbo].[tblCMCompanyPreferenceOption]
 (
 	[intCompanyPreferenceOptionId] INT IDENTITY(1,1) NOT NULL,
 	[intConcurrencyId] INT NULL,
-	[ysnRemittanceAdvice_DisplayVendorAccountNumber] BIT NULL
+	ysnRemittanceAdvice_DisplayVendorAccountNumber BIT NULL,
+	ysnRemittanceAdvice_AttachSettlement BIT NULL
 )
