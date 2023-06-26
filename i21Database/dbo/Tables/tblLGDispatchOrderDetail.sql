ec7d216006056f0e12e2637ddfccf0785d7e017d
rjayruta <127469625+rjayruta@users.noreply.github.com>
2023-06-26 14:12:27 +0800

    LG-4188 HDTN-291932 added Item description in dispatch order grid details

--- i21Database/dbo/Tables/tblLGDispatchOrderDetail.sql
+++ i21Database/dbo/Tables/tblLGDispatchOrderDetail.sql
@@ -30,6 +30,7 @@
 	[dblLatitude] NUMERIC(18, 6) NULL,
 	[intItemId] INT NULL,
 	[strItemNo] NVARCHAR(400) COLLATE Latin1_General_CI_AS NULL,
+	[strItemDescription] NVARCHAR(400) COLLATE Latin1_General_CI_AS NULL,
 	[dblQuantity] NUMERIC(18, 6) NULL,
 	[dblStandardWeight] NUMERIC(18, 6) NULL,
 	[strOrderComments] NVARCHAR(MAX) COLLATE Latin1_General_CI_AS NULL,
