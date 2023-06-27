4744a50b6384280c412bf2216bb7d88380fcd647
Dom Caoile <dom.caoile.02@gmail.com>
2023-06-27 10:47:16 +0800

    FRD-2004

--- i21PreDeployment/Scripts/Pre-Deployment/9_FRD/4_2110_to_2120.sql
+++ i21PreDeployment/Scripts/Pre-Deployment/9_FRD/4_2110_to_2120.sql
@@ -23,12 +23,6 @@ BEGIN
 END
 GO
 
-IF EXISTS (SELECT TOP 1 1 FROM sys.columns WHERE NAME  = N'intAccountGroupClusterId' AND OBJECT_ID = OBJECT_ID(N'tblFRReport')) 
-BEGIN
-    UPDATE tblFRReport SET intAccountGroupClusterId = 1 WHERE strReportType = 'Group'
-END
-GO
-
 
 GO
 	PRINT 'END FRD 2110'
