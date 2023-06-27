82085ad6a4b7834d736ab3267f506692e61a8795
Girome Andaya <girome.andaya@iRely.com>
2023-06-27 09:58:43 +0800

    TR-2521

--- i21Database/dbo/Views/vyuTRLoadDistributionDetailInfo.sql
+++ i21Database/dbo/Views/vyuTRLoadDistributionDetailInfo.sql
@@ -2,25 +2,28 @@
 
 AS
 
-SELECT DD.intLoadDistributionDetailId
-	, DD.intLoadDistributionHeaderId
-	, DD.intItemId
-	, IL.strItemNo
-	, intStockUOMId = IL.intIssueUOMId
-	, strStockUOM = IL.strIssueUOM
-	, DD.intContractDetailId
-	, CD.strContractNumber
-	, DD.intTaxGroupId
-	, TG.strTaxGroup
-	, DD.intSiteId
-	, strSiteNumber = RIGHT('0000'+CAST(S.intSiteNumber AS NVARCHAR(4)),4) COLLATE Latin1_General_CI_AS
-	, DD.intTMOId
-	, strTMOrder = ISNULL(TMD.strOrderNumber, TMH.strOrderNumber)
-FROM tblTRLoadDistributionDetail DD
-LEFT JOIN tblTRLoadDistributionHeader DH ON DH.intLoadDistributionHeaderId = DD.intLoadDistributionHeaderId
-LEFT JOIN vyuICGetItemLocation IL ON IL.intItemId = DD.intItemId AND IL.intLocationId = DH.intCompanyLocationId
-LEFT JOIN vyuCTContractDetailView CD ON CD.intContractDetailId = DD.intContractDetailId
-LEFT JOIN tblSMTaxGroup TG ON TG.intTaxGroupId = DD.intTaxGroupId
-LEFT JOIN tblTMSite S ON S.intSiteID = DD.intSiteId
-LEFT JOIN tblTMDispatch TMD ON TMD.intDispatchID = DD.intTMOId
+SELECT DD.intLoadDistributionDetailId  
+ , DD.intLoadDistributionHeaderId  
+ , DD.intItemId  
+ , IL.strItemNo  
+ , intStockUOMId = IL.intIssueUOMId  
+ , strStockUOM = IL.strIssueUOM  
+ , DD.intContractDetailId  
+ , CD.strContractNumber  
+ , DD.intTaxGroupId  
+ , TG.strTaxGroup  
+ , DD.intSiteId  
+ , strSiteNumber = RIGHT('0000'+CAST(S.intSiteNumber AS NVARCHAR(4)),4) COLLATE Latin1_General_CI_AS  
+ , DD.intTMOId  
+ , strTMOrder = ISNULL(TMD.strOrderNumber, TMH.strOrderNumber)
+ , DD.intItemCustomerXrefId
+ , CX.strCustomerProduct
+FROM tblTRLoadDistributionDetail DD  
+LEFT JOIN tblTRLoadDistributionHeader DH ON DH.intLoadDistributionHeaderId = DD.intLoadDistributionHeaderId  
+LEFT JOIN vyuICGetItemLocation IL ON IL.intItemId = DD.intItemId AND IL.intLocationId = DH.intCompanyLocationId  
+LEFT JOIN vyuCTContractDetailView CD ON CD.intContractDetailId = DD.intContractDetailId  
+LEFT JOIN tblSMTaxGroup TG ON TG.intTaxGroupId = DD.intTaxGroupId  
+LEFT JOIN tblTMSite S ON S.intSiteID = DD.intSiteId  
+LEFT JOIN tblTMDispatch TMD ON TMD.intDispatchID = DD.intTMOId  
 LEFT JOIN tblTMDispatchHistory TMH ON TMH.intDispatchId = DD.intTMOId
+LEFT JOIN tblICItemCustomerXref CX ON DD.intItemCustomerXrefId = CX.intItemCustomerXrefId
