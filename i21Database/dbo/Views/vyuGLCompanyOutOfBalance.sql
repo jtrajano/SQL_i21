8aefade482e831d03c0c656c5131b87d39a88a26
Jeff Trajano <jeffrey.trajano@irely.com>
2023-06-27 14:29:20 +0800

    GL-10587

--- i21Database/dbo/Views/vyuGLCompanyOutOfBalance.sql
+++ i21Database/dbo/Views/vyuGLCompanyOutOfBalance.sql
@@ -14,29 +14,10 @@ with cte as(
 	join tblGLAccountStructure S on C.intAccountStructureId = C.intAccountStructureId
 	where ysnIsUnposted = 0  and intStructureType = 6
 	group by C.strCode, strTransactionId having sum(isnull(dblDebit,0) - isnull(dblCredit,0)) <> 0
-),
-cteOrder as(
-	select *,
-	row_number() over(partition by strTransactionId order by strCompanySegment) rowId
-	from cte 
-),
-cteFirst as(
-	select strTransactionId, a.dblTotal from cteOrder a 
-	where rowId = 1
-),
-cteSecond as(
-	select strTransactionId, a.dblTotal from cteOrder a 
-	where rowId = 2
-),
-cteUnbalanced as(
-	select a.strTransactionId from 
-	cteFirst a left join cteSecond b on a.strTransactionId = b.strTransactionId
-	where (a.dblTotal + b.dblTotal) <> 0
 )
 select 
 intTransactionId,
 strModuleName,
 strTransactionForm,
-B.strTransactionId, A.strCompanySegment, dblTotal 
-from cte A join 
-cteUnbalanced B on A.strTransactionId = B.strTransactionId
+strTransactionId, strCompanySegment, dblTotal 
+from cte
