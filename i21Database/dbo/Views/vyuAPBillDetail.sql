087f71c7b94b2f07701700fa5a4c2e657ab4f51c
Gary Malonzo <gary.malonzo@iRely.com>
2023-07-04 07:43:31 +0800

    AP-12188

--- i21Database/dbo/Views/vyuAPBillDetail.sql
+++ i21Database/dbo/Views/vyuAPBillDetail.sql
@@ -127,7 +127,8 @@ SELECT
 	cgt.strCatalogueType,
 	pgrp.strName AS strPurchasingGroup,
 	mz.intMarketZoneId,
-	strMarketZone = mz.strMarketZoneCode
+	strMarketZone = mz.strMarketZoneCode,
+	A.dtmBillDate
 FROM dbo.tblAPBill A
 INNER JOIN (dbo.tblAPVendor G INNER JOIN dbo.tblEMEntity G2 ON G.[intEntityId] = G2.intEntityId) ON G.[intEntityId] = A.intEntityVendorId
 INNER JOIN dbo.tblAPBillDetail B 
