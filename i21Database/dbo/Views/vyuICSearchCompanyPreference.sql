fe02ef08faec8e5571324910157adc8835bccbc5
johnryanvillar <johnryan.villar@irely.com>
2023-07-04 17:28:29 +0800

    HDTN-301114

--- i21Database/dbo/Views/vyuICSearchCompanyPreference.sql
+++ i21Database/dbo/Views/vyuICSearchCompanyPreference.sql
@@ -3,7 +3,41 @@
 AS
 
 SELECT 
-	ic.* 
+	ic.intCompanyPreferenceId,
+ic.intInheritSetup,
+ic.intSort,
+ic.intConcurrencyId,
+ic.strLotCondition,
+ic.strReceiptType,
+ic.intReceiptSourceType,
+ic.intShipmentOrderType,
+ic.intShipmentSourceType,
+ic.strOriginLineOfBusiness,
+ic.strOriginLastTask,
+ic.strIRUnpostMode,
+ic.strReturnPostMode,
+ic.strReceiptReportFormat,
+ic.strPickListReportFormat,
+ic.strBOLReportFormat,
+ic.strTransferReportFormat,
+ic.strCountSheetReportFormat,
+ic.ysnMigrateNewInventoryTransaction,
+ic.dtmDateCreated,
+ic.dtmDateModified,
+ic.intCreatedByUserId,
+ic.intModifiedByUserId,
+ic.ysnIsCountSheetMultiFilter,
+ic.ysnPriceFixWarningInReceipt,
+ic.ysnValidateReceiptTotal,
+ic.intItemIdHolderForReceiptImport,
+ic.ysnUpdateSMTransaction,
+ic.ysnUpdateInventoryTransactionAccountId,
+ic.strSingleOrMultipleLots,
+ic.ysnInitialFamilyClassAdjustment,
+ic.ysnUpdateJournalLineDescription,
+ic.ysnEnableIntraCompanyTransfer,
+ic.ysnSkipICGLValidation
+
 	-- Get the company preference from the other modules that is related with IC. 
 	,ysnImposeReversalTransaction = CAST(0 AS BIT) --rk.ysnImposeReversalTransaction
 	,i.strItemNo
@@ -19,4 +53,5 @@ FROM
 	) rk 
 	LEFT JOIN tblICItem i 
 		ON i.intItemId = ic.intItemIdHolderForReceiptImport
+	 
 	 
