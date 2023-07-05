b4ce9942c01eed6918b5b5231e5c5e7af3b0bb35
Renze Cuadra <renze.cuadra@iRely.com>
2023-07-05 15:23:39 +0800

    CS-659

--- i21Database/Scripts/Post-Deployment/ST/52_Populate_JobTypes.sql
+++ i21Database/Scripts/Post-Deployment/ST/52_Populate_JobTypes.sql
@@ -3,17 +3,22 @@
 GO
 	IF NOT EXISTS(SELECT '' FROM tblSTJobTypes where  strJobType = 'Reboot')
 	BEGIN
-		INSERT [dbo].[tblSTJobTypes] ([strJobType], [intConcurrencyId]) VALUES ( 'Reboot', 1)
+		INSERT [dbo].[tblSTJobTypes] ([intJobTypeId], [strJobType], [intConcurrencyId]) VALUES (1, 'Reboot', 1)
 	END
 	
 	IF NOT EXISTS(SELECT '' FROM tblSTJobTypes where strJobType = 'Poll Files')
 	BEGIN
-		INSERT [dbo].[tblSTJobTypes] ([strJobType], [intConcurrencyId]) VALUES ( 'Poll Files', 1)
+		INSERT [dbo].[tblSTJobTypes] ([intJobTypeId], [strJobType], [intConcurrencyId]) VALUES (2, 'Poll Files', 1)
 	END
 
 	IF NOT EXISTS(SELECT '' FROM tblSTJobTypes where strJobType = 'Change Register Password')
 	BEGIN
-		INSERT [dbo].[tblSTJobTypes] ([strJobType], [intConcurrencyId]) VALUES ( 'Change Register Password', 1)
+		INSERT [dbo].[tblSTJobTypes] ([intJobTypeId], [strJobType], [intConcurrencyId]) VALUES (3, 'Change Register Password', 1)
+	END
+
+	IF NOT EXISTS(SELECT '' FROM tblSTJobTypes where strJobType = 'Passport_TPI_FSTab')
+	BEGIN
+		INSERT [dbo].[tblSTJobTypes] ([intJobTypeId], [strJobType], [intConcurrencyId]) VALUES (4, 'Passport_TPI_FSTab', 1)
 	END
 GO
 	PRINT N'END INSERT STORE JOB TYPES'
