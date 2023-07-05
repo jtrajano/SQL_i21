a4005adfd1788e888edf9cee113a8c88e0a07d35
LexPeregrino <lex.peregrino@irely.com>
2023-07-05 11:08:03 +0800

    CT-9054 v1

--- i21Database/dbo/Views/vyuCTCompanyPreference.sql
+++ i21Database/dbo/Views/vyuCTCompanyPreference.sql
@@ -147,6 +147,7 @@ AS
 			,CP.ysnForceReasonCodeForAmendments
 			,CP.ysnEnableNetWeightAdjustment
 			,CP.ysnSequenceImportToCreateCBLogs
+			,CP.ysnEnableUserProfileSignature
 	FROM	tblCTCompanyPreference		CP
 	LEFT JOIN	tblICUnitMeasure			U1	ON	U1.intUnitMeasureId			=	CP.intCleanCostUOMId
 	LEFT JOIN	tblSMCurrency				C1	ON	C1.intCurrencyID			=	CP.intCleanCostCurrencyId
