22e1f9e3a8d4abe58376530fe7b4ef85667e9b7a
Gary Malonzo <gary.malonzo@iRely.com>
2023-07-11 16:43:44 +0800

    AP-11039

--- i21Database/dbo/Functions/fnAPValidatePostPayment.sql
+++ i21Database/dbo/Functions/fnAPValidatePostPayment.sql
@@ -597,6 +597,7 @@ BEGIN
 		AND PD.dblPayment <> 0
 		AND PD.dblDiscount <> 0
 		AND CL.intDiscountAccountId IS NULL
+		
 		--DO NOT ALLOW PAYMENT EARLIER THAN THE INVOICE DATE
 		INSERT INTO @returntable(strError, strTransactionType, strTransactionId, intTransactionId)
 		SELECT
@@ -605,11 +606,10 @@ BEGIN
 			A.strPaymentRecordNum,
 			A.intPaymentId
 		FROM tblAPPayment A 
-		INNER JOIN tblAPPaymentDetail B
-			ON A.intPaymentId = B.intPaymentId
-		INNER JOIN tblAPBill C
-			ON B.intBillId = C.intBillId
-		WHERE  A.[dtmDatePaid] < C.[dtmBillDate]
+		INNER JOIN tblAPPaymentDetail B ON A.intPaymentId = B.intPaymentId
+		INNER JOIN tblAPBill C ON B.intBillId = C.intBillId
+		WHERE A.[intPaymentId] IN (SELECT intId FROM @paymentIds)
+		AND CAST(A.[dtmDatePaid] AS DATE) < CAST(C.[dtmBillDate] AS DATE)
 
 	END
 	ELSE
