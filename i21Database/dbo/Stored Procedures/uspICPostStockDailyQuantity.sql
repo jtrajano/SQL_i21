b2a6b84f75fbacda4c23a56ff68c9e9630ab3822
Jerwell <Jerwell.cruz@irely.com>
2023-07-12 17:13:12 +0800

    IC-11830

--- i21Database/dbo/Stored Procedures/uspICPostStockDailyQuantity.sql
+++ i21Database/dbo/Stored Procedures/uspICPostStockDailyQuantity.sql
@@ -439,6 +439,8 @@ BEGIN
 				,[dblQty] = @dblQty 
 				,[dblValue] = ROUND(dbo.fnMultiply(@dblQty, @dblCost) + @dblValue, 2)
 				,[dblValueRounded] = ROUND(dbo.fnMultiply(@dblQty, @dblCost) + @dblValue, 2)
+				,[dblValue] = ROUND(dbo.fnMultiply(ISNULL(@dblQty, 0), ISNULL(@dblCost, 0)) + ISNULL(@dblValue, 0), 2)
+				,[dblValueRounded] = ROUND(dbo.fnMultiply(ISNULL(@dblQty, 0), ISNULL(@dblCost, 0)) + ISNULL(@dblValue, 0), 2)
 			FROM
 				[dbo].[fnICGetCompanyLocation](@intItemLocationId, @intInTransitSourceLocationId) [location]
 			--SELECT	
