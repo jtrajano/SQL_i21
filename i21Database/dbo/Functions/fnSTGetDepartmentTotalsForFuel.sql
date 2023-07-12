edfa7eb5c80c0d32c50e6870e7cff778d7bfcdb8
Renze Cuadra <renze.cuadra@iRely.com>
2023-07-12 13:00:31 +0800

    CS-659

--- i21Database/dbo/Functions/fnSTGetDepartmentTotalsForFuel.sql
+++ i21Database/dbo/Functions/fnSTGetDepartmentTotalsForFuel.sql
@@ -5,14 +5,20 @@
 RETURNS DECIMAL(18,2)
 AS BEGIN
 
-   DECLARE     @dblDepartmentTotalsForFuel DECIMAL(18,2) = 0
+	DECLARE     @dblDepartmentTotalsForFuel DECIMAL(18,2) = 0
 
-    SELECT      @dblDepartmentTotalsForFuel = ISNULL(SUM(dblTotalSalesAmountComputed),0)
-    FROM        tblSTCheckoutDepartmetTotals a
-    LEFT JOIN  tblICItem b
-    ON          a.intItemId = b.intItemId
-    WHERE       a.intCheckoutId = @intCheckoutId 
-    AND         a.ysnFuelCategory = 1
+	SELECT      @dblDepartmentTotalsForFuel = ISNULL(SUM(dblTotalSalesAmountComputed),0)
+	FROM        tblSTCheckoutDepartmetTotals a
+	JOIN		tblSTCheckoutHeader ch
+	ON			a.intCheckoutId = ch.intCheckoutId
+	LEFT JOIN	tblICItem b
+	ON          a.intItemId = b.intItemId
+	JOIN		tblSTStoreDepartments sdc
+	ON			(a.intCategoryId = sdc.intCategoryId OR a.intSubcategoriesId = sdc.intSubcategoriesId)
+	AND			ch.intStoreId = sdc.intStoreId
+	AND			a.strRegisterCode = sdc.strRegisterCode
+	WHERE       a.intCheckoutId = @intCheckoutId 
+	AND         sdc.ysnFuelCategory = 1
 
     RETURN      @dblDepartmentTotalsForFuel
 
