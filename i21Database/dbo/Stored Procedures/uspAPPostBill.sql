61ba18733ffebec56934a2f22e1723779546ae4c
Jopet Lambinicio <jopet.lambinicio@irely.com>
2023-07-13 20:02:29 +0800

    AP-12190

--- i21Database/dbo/Stored Procedures/uspAPPostBill.sql
+++ i21Database/dbo/Stored Procedures/uspAPPostBill.sql
@@ -2000,50 +2000,6 @@ BEGIN
 					,@userId
 					,NULL
 					,@ValueToPost
-
-			INSERT INTO @GLEntries (
-					[dtmDate] 
-					,[strBatchId]
-					,[intAccountId]
-					,[dblDebit]
-					,[dblCredit]
-					,[dblDebitUnit]
-					,[dblCreditUnit]
-					,[strDescription]
-					,[strCode]
-					,[strReference]
-					,[intCurrencyId]
-					,[dblExchangeRate]
-					,[dtmDateEntered]
-					,[dtmTransactionDate]
-					,[strJournalLineDescription]
-					,[intJournalLineNo]
-					,[ysnIsUnposted]
-					,[intUserId]
-					,[intEntityId]
-					,[strTransactionId]					
-					,[intTransactionId]
-					,[strTransactionType]
-					,[strTransactionForm] 
-					,[strModuleName]
-					,[intConcurrencyId]
-					,[dblDebitForeign]
-					,[dblDebitReport]
-					,[dblCreditForeign]
-					,[dblCreditReport]
-					,[dblReportingRate]
-					,[dblForeignRate]
-					,[intSourceEntityId]
-					,[intCommodityId]
-					,[intCurrencyExchangeRateTypeId]
-					,[strRateType]					
-			)			
-			EXEC @intReturnValue = dbo.uspICCreateGLEntriesOnInTransitValueAdjustment
-				@strBatchId = @batchId
-				,@strTransactionId = NULL
-				,@intEntityUserSecurityId = @userId
-				,@strGLDescription = NULL
-				,@AccountCategory_Cost_Adjustment = DEFAULT 
 		END TRY
 		BEGIN CATCH
 			SET @errorAdjustment = ERROR_MESSAGE()
