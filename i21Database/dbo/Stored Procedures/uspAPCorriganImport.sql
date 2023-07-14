003023dfd309a9318997aa3979c6d9aa1775c9ad
Erick Dela Cruz <erick.delacruz@irely.com>
2023-07-14 14:58:44 +0800

    AP-12288

--- i21Database/dbo/Stored Procedures/uspAPCorriganImport.sql
+++ i21Database/dbo/Stored Procedures/uspAPCorriganImport.sql
@@ -377,13 +377,16 @@ END
 	DECLARE @defaultAPAccount NVARCHAR(50) = '200000-0000-0000-0090'
 	DECLARE @apAccountId INT = (SELECT TOP 1 intAccountId FROM tblGLAccount WHERE strAccountId = @defaultAPAccount)
 
+	DECLARE @currentTime DATETIME = GETDATE()
+	DECLARE @timeStamp NVARCHAR(100) = CONVERT(CHAR(19), @currentTime, 120);
+
 	INSERT INTO @voucherPayables(
 		intPartitionId
 		,strVendorOrderNumber
 		,intTransactionType
 		,intAPAccount
 		,intEntityVendorId
-		,strReferenceNo
+		,strReference
 		,intLocationId
 		,intShipToId
 		,intShipFromId
@@ -410,7 +413,7 @@ END
 		intTransactionType 		=	intTransactionType,
 		intAPAccount			=	@apAccountId,
 		intEntityVendorId		=	intEntityVendorId,
-		strReferenceNo			=	CAST(GETDATE() AS NVARCHAR),
+		strReference			=	@timeStamp,
 		intLocationId			=	intShipToId,
 		intShipToId				=	intShipToId,
 		intShipFromId			=	intShipFromId,
