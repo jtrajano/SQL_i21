a79711b6c4a6317e6144b18ab319496115646d39
Feb Montefrio <feb.montefrio@irely.com>
2023-07-14 17:47:35 +0800

    [B] IC-11938

--- i21Database/dbo/Stored Procedures/uspICPostCostAdjustmentRetroactiveAvg.sql
+++ i21Database/dbo/Stored Procedures/uspICPostCostAdjustmentRetroactiveAvg.sql
@@ -144,8 +144,12 @@ BEGIN
 
 
 	DECLARE @strReceiptType AS NVARCHAR(50)
-			,@costAdjustmentType_DETAILED AS TINYINT = 1
-			,@costAdjustmentType_SUMMARIZED AS TINYINT = 2
+	
+	DECLARE @costAdjustmentType_DETAILED AS TINYINT = 1
+		,@costAdjustmentType_SUMMARIZED AS TINYINT = 2
+		,@costAdjustmentType_RETROACTIVE_DETAILED AS TINYINT = 3
+		,@costAdjustmentType_RETROACTIVE_SUMMARIZED AS TINYINT = 4
+		,@costAdjustmentType_CURRENT_AVG AS TINYINT = 5
 
 	DECLARE @costAdjustmentType AS TINYINT 
 	SET @costAdjustmentType = dbo.fnICGetCostAdjustmentSetup(@intItemId, @intItemLocationId) 
@@ -637,7 +641,7 @@ BEGIN
 		SET @RunningQty += dbo.fnCalculateQtyBetweenUOM(@t_intItemUOMId, @StockItemUOMId, @t_dblQty)
 
 		-- Get accumulated cost adjustment for negative stocks
-		IF	@costAdjustmentType = @costAdjustmentType_SUMMARIZED 
+		IF	@costAdjustmentType IN (@costAdjustmentType_SUMMARIZED, @costAdjustmentType_RETROACTIVE_SUMMARIZED)
 			AND @t_NegativeStockQty <> 0 
 			AND @t_NegativeStockQty IS NOT NULL 
 		BEGIN 
@@ -687,7 +691,7 @@ BEGIN
 		END
 
 		-- Check if there is a transaction where the cost change needs escalation. 
-		IF @costAdjustmentType = @costAdjustmentType_DETAILED
+		IF @costAdjustmentType IN (@costAdjustmentType_DETAILED, @costAdjustmentType_RETROACTIVE_DETAILED)
 		BEGIN 
 			SET @EscalateCostAdjustment = 0 
 			SET @EscalateCostAdjustment = (@t_dblQty * @NewAverageCost) - (@t_dblQty * @OriginalAverageCost)
@@ -772,7 +776,7 @@ BEGIN
 				,[intInventoryCostAdjustmentTypeId] = 
 						CASE	WHEN @t_dblQty > 0 THEN 
 									CASE	
-											WHEN @costAdjustmentType = @costAdjustmentType_SUMMARIZED THEN 
+											WHEN @costAdjustmentType IN (@costAdjustmentType_SUMMARIZED, @costAdjustmentType_RETROACTIVE_SUMMARIZED) THEN 
 												@COST_ADJ_TYPE_Adjust_Value
 											WHEN @t_intTransactionTypeId = @INV_TRANS_TYPE_Produce THEN 
 												@COST_ADJ_TYPE_Adjust_WIP
@@ -803,7 +807,7 @@ BEGIN
 									END 
 								WHEN @t_dblQty < 0 THEN 
 									CASE	
-											WHEN @costAdjustmentType = @costAdjustmentType_SUMMARIZED THEN 
+											WHEN @costAdjustmentType IN (@costAdjustmentType_SUMMARIZED, @costAdjustmentType_RETROACTIVE_SUMMARIZED) THEN 
 												@COST_ADJ_TYPE_Adjust_Sold									
 											WHEN @t_intTransactionTypeId = @INV_TRANS_TYPE_Consume THEN 
 												@COST_ADJ_TYPE_Adjust_WIP
@@ -832,7 +836,7 @@ BEGIN
 									END 
 							WHEN @t_NegativeStockQty <> 0 AND @t_NegativeStockQty IS NOT NULL THEN 
 								CASE 
-									WHEN @costAdjustmentType = @costAdjustmentType_SUMMARIZED THEN 
+									WHEN @costAdjustmentType IN (@costAdjustmentType_SUMMARIZED, @costAdjustmentType_RETROACTIVE_SUMMARIZED) THEN 
 										@COST_ADJ_TYPE_Adjust_Sold									
 									WHEN @t_intTransactionTypeId = @INV_TRANS_TYPE_Consume THEN 
 										@COST_ADJ_TYPE_Adjust_WIP
@@ -943,7 +947,7 @@ BEGIN
 END 
 
 -- Book the cost adjustment. 
-IF @costAdjustmentType = @costAdjustmentType_SUMMARIZED
+IF @costAdjustmentType IN (@costAdjustmentType_SUMMARIZED, @costAdjustmentType_RETROACTIVE_SUMMARIZED)
 BEGIN 
 	SET @strNewCost = CONVERT(NVARCHAR, CAST(@CostAdjustment AS MONEY), 1)
 
@@ -1008,7 +1012,7 @@ BEGIN
 		WHERE	intInventoryTransactionId = @DummyInventoryTransactionId
 	END 
 END 
-ELSE IF @costAdjustmentType = @costAdjustmentType_DETAILED
+ELSE IF @costAdjustmentType IN (@costAdjustmentType_DETAILED, @costAdjustmentType_RETROACTIVE_DETAILED)
 BEGIN 
 
 	DECLARE @strTransactionIdCostAdjLog AS NVARCHAR(50)
