576c9b121eb3d01e517d6012dc9b912d4de66bd8
Girome Andaya <girome.andaya@iRely.com>
2023-07-18 07:20:49 +0800

    TR-2657
    (cherry picked from commit 083418025a2af09802d08263ecf7e01af6ad5447)
    (cherry picked from commit 7b82fe759ffee26077662cfcdb214a680d12727f)

---
+++ i21Database/dbo/Stored Procedures/uspTRFreightCommissionFreightParams.sql
@@ -0,0 +1,46 @@
+﻿create procedure uspTRFreightCommissionFreightParams  
+@intItemId INT,  
+@intInvoiceId INT,
+@intFreightItemId INT,
+@intSurchargeItemId INT,
+@intFreightCategoryId INT,
+@dblFreightUnitCommissionPct INT,--@dblFreightUnitCommissionPct DECIMAL(18,6),
+@dblOtherUnitCommissionPct INT,--@dblOtherUnitCommissionPct DECIMAL(18,6),
+@intLoadDistributionDetailId INT
+--@dtmFrom NVARCHAR(50),
+--@dtmTo NVARCHAR(50)
+  
+as
+
+SET QUOTED_IDENTIFIER OFF    
+SET ANSI_NULLS ON    
+SET NOCOUNT ON    
+SET XACT_ABORT ON    
+SET ANSI_WARNINGS OFF    
+
+
+--DECLARE @dto DATETIME = CONVERT(NVARCHAR(255),CONVERT(SMALLDATETIME, @dtmTo,105))
+--DECLARE @dfrom DATETIME = CONVERT(NVARCHAR(255),CONVERT(SMALLDATETIME, @dtmFrom,105))
+
+select   
+  intItemId  = @intItemId
+, intTrueItemId = intItemId
+, strItemDescription  = CASE 
+							WHEN intItemId = @intFreightItemId THEN 'Unit Freight Charge'
+							--WHEN intItemId = @intSurchargeItemId THEN 'Surcharge'
+							ELSE strItemDescription END
+, intInvoiceId  
+, dblFreightRate  
+, dblFreight = dblTotal
+, dblCommissionPct = CASE WHEN intItemId = @intFreightItemId THEN @dblFreightUnitCommissionPct ELSE @dblOtherUnitCommissionPct END
+, dblTotalCommission = CASE WHEN intItemId = @intFreightItemId THEN ((@dblFreightUnitCommissionPct/CONVERT(DECIMAL(18,6),100)) * dblTotal) ELSE ((@dblOtherUnitCommissionPct/CONVERT(DECIMAL(18,6),100)) * dblTotal) END
+
+  
+from vyuTRGetFreightCommissionFreight fcf
+where intInvoiceId = @intInvoiceId
+	AND (
+		(intItemId = @intFreightItemId AND intCategoryId = @intFreightCategoryId AND intLoadDistributionDetailId = @intLoadDistributionDetailId)
+		--OR (intItemId = @intSurchargeItemId AND intCategoryId = @intFreightCategoryId AND intLoadDistributionDetailId = @intLoadDistributionDetailId)
+		OR (intCategoryId = @intFreightCategoryId AND intItemId != @intItemId AND strBOLNumberDetail IS NULL AND intLoadDistributionDetailId = @intLoadDistributionDetailId)
+		OR (intCategoryId = @intFreightCategoryId AND intItemId != @intItemId AND intLoadDistributionDetailId = @intLoadDistributionDetailId)
+	)
---
+++ i21Database/dbo/Views/vyuTRGetFreightCommissionFreight.sql
@@ -0,0 +1,23 @@
+﻿create view vyuTRGetFreightCommissionFreight  
+as  
+  
+select   
+	aid.intItemId,
+	ai.intInvoiceId,
+	ii.intCategoryId,
+	aid.intLoadDistributionDetailId,
+	aid.strItemDescription,
+	aid.strBOLNumberDetail,
+	aid.dblQtyShipped,
+	dblFreightRate = CASE WHEN ldd.intLoadDistributionDetailId IS NULL THEN ISNULL(aid.dblBasePrice, 0) ELSE ISNULL(ldd.dblFreightRate, 0) END,
+	aid.dblTotal,
+	lh.dtmLoadDateTime
+
+from tblARInvoiceDetail aid  
+ left join tblARInvoice ai on ai.intInvoiceId = aid.intInvoiceId
+ left join tblICItem ii on ii.intItemId = aid.intItemId
+ left join tblTRLoadDistributionDetail ldd on ldd.intLoadDistributionDetailId = aid.intLoadDistributionDetailId
+ left join tblTRLoadHeader lh on lh.strTransaction = ai.strActualCostId
+ 
+where ai.strType = 'Transport Delivery'
+and strDocumentNumber like 'TR%'  
