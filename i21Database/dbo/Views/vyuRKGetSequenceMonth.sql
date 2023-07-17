1eed09bb274f6d8ece66ea05686854436e3b8d11
Michael Mirho Villaluz <mirho.villaluz@irely.com>
2023-07-17 16:14:56 +0800

    RM-5413

--- i21Database/dbo/Views/vyuRKGetSequenceMonth.sql
+++ i21Database/dbo/Views/vyuRKGetSequenceMonth.sql
@@ -2,9 +2,9 @@ CREATE VIEW [dbo].[vyuRKGetSequenceMonth]
 AS
 SELECT *
 FROM (
-	SELECT intDeliveryMonthId = n.n 
-		, strDeliveryMonth = RIGHT(CONVERT(VARCHAR(11),DATEADD(dd, -1, DATEADD(MONTH, n.n + DATEDIFF(MONTH, 0, GETDATE()), 0)), 6), 6)
+	SELECT intDeliveryMonthId = n.intDeliveryMonthId
+		, strDeliveryMonth = RIGHT(CONVERT(VARCHAR(11),DATEADD(dd, -1, DATEADD(MONTH, n.intDeliveryMonthId + DATEDIFF(MONTH, 0, GETDATE()), 0)), 6), 6)
 	FROM (
-		SELECT n FROM (VALUES (1),(2),(3),(4),(5),(6),(7),(8),(9),(10),(11),(12)) t (n)
+		SELECT intDeliveryMonthId FROM (VALUES (1),(2),(3),(4),(5),(6),(7),(8),(9),(10),(11),(12)) t (intDeliveryMonthId)
 	) n
 )t
