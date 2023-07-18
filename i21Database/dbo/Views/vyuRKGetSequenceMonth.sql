f99857005a500a4325ddc4b410776a4a5fd1e0a2
Michael Mirho Villaluz <mirho.villaluz@irely.com>
2023-07-18 10:31:09 +0800

    RM-5413

--- i21Database/dbo/Views/vyuRKGetSequenceMonth.sql
+++ i21Database/dbo/Views/vyuRKGetSequenceMonth.sql
@@ -3,7 +3,7 @@ AS
 SELECT *
 FROM (
 	SELECT intDeliveryMonthId = n.intDeliveryMonthId
-		, strDeliveryMonth = RIGHT(CONVERT(VARCHAR(11),DATEADD(dd, -1, DATEADD(MONTH, n.intDeliveryMonthId + DATEDIFF(MONTH, 0, GETDATE()), 0)), 6), 6)
+		, strDeliveryMonth = RIGHT(CONVERT(VARCHAR(11),DATEADD(dd, -1, DATEADD(MONTH, n.intDeliveryMonthId + DATEDIFF(MONTH, 0, GETDATE()), 0)), 6), 6) COLLATE Latin1_General_CI_AS
 	FROM (
 		SELECT intDeliveryMonthId FROM (VALUES (1),(2),(3),(4),(5),(6),(7),(8),(9),(10),(11),(12)) t (intDeliveryMonthId)
 	) n
