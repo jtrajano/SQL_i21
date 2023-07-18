311f8008608c1ba280cef7552efa802c46837cfc
Jeff Trajano <jeffrey.trajano@irely.com>
2023-07-18 10:00:50 +0800

    Revert "GL-10554"
    This reverts commit b7d67785422b9568bd10f90ba63297f6e9c5f24c.

--- i21Database/dbo/Functions/fnCMUndepositedFundReport.sql
+++ i21Database/dbo/Functions/fnCMUndepositedFundReport.sql
@@ -14,36 +14,117 @@ AS
 RETURN 
 (
 with mainTable as(	
-	select 
-	A.dtmDate,
-	A.strName,
-	C.strSourceTransactionId,
-	A.strPaymentMethod,
-	A.strSourceSystem,
-	C.strEODNumber,
-	C.strEODDrawer ,
-	C.ysnEODComplete ,
-	A.strCardType,
-	A.strLocationName strLocationName,
-	A.strUserName,
-	B.strTransactionId,
-	ysnPosted,
-	B.dtmDate dtmCMDate,
-	A.dblAmount,
-	A.strCompanySegment,
-	A.strAccountId
-	from vyuCMUndepositedFund  A join 
-	tblCMUndepositedFund C on C.intUndepositedFundId = A.intUndepositedFundId
-	left join
-	(
-		select intUndepositedFundId, BT.dtmDate, strTransactionId, ysnPosted from tblCMBankTransactionDetail BTD 
-		join tblCMBankTransaction BT on BT.intTransactionId = BTD.intTransactionId 
-	) B on
-	A.intUndepositedFundId = B.intUndepositedFundId 
-	where 
-	B.dtmDate BETWEEN @dtmDateFrom AND @dtmDateTo AND ysnPosted =0 --UNPOSTED BANK DEPOSIT
-	OR B.intUndepositedFundId is null -- NOT PICKED UP BY BANK DEPOSIT
+select
+a.dtmDate,
+a.strName,
+a.strSourceTransactionId,
+a.strPaymentMethod,
+a.strSourceSystem,
+d.strEODNumber,
+d.strEODDrawer ,
+d.ysnEODComplete ,
+a.strCardType,
+a.strLocationName strLocationName,
+a.strUserName,
+c.strTransactionId,
+ysnPosted,
+c.dtmDate dtmCMDate,
+a.dblAmount,
+a.strCompanySegment,
+a.strAccountId
+from vyuCMUndepositedFund a
+inner join tblCMUndepositedFund d on d.intUndepositedFundId = a.intUndepositedFundId
+left outer join tblCMBankTransactionDetail b on a.intUndepositedFundId = b.intUndepositedFundId
+left outer join tblCMBankTransaction c on c.intTransactionId = b.intTransactionId
+where a.dtmDate BETWEEN @dtmDateFrom AND @dtmDateTo --- Sales Payment Date
+and c.dtmDate is Null
+
+union
+
+--All undeposited Funds that have been imported into bank transation BUT NOT POSTED
+select
+a.dtmDate,
+a.strName,
+a.strSourceTransactionId,
+a.strPaymentMethod,
+a.strSourceSystem,
+d.strEODNumber,
+d.strEODDrawer ,
+d.ysnEODComplete ,
+a.strCardType,
+a.strLocationName strLocationName,
+a.strUserName,
+c.strTransactionId,
+ysnPosted,
+c.dtmDate dtmCMDate,
+a.dblAmount,
+a.strCompanySegment,
+a.strAccountId
+from vyuCMUndepositedFund a
+inner join tblCMUndepositedFund d on d.intUndepositedFundId = a.intUndepositedFundId
+left outer join tblCMBankTransactionDetail b on a.intUndepositedFundId = b.intUndepositedFundId
+left outer join tblCMBankTransaction c on c.intTransactionId = b.intTransactionId
+where a.dtmDate BETWEEN @dtmDateFrom AND @dtmDateTo
+--- Sales Payment Date
+and ysnPosted = 0
+
+
+union
+
+---All undeposited funds that have been imported into bank transactions AND POSTED BUT AFTER as of date
+select
+a.dtmDate,
+a.strName,
+a.strSourceTransactionId,
+a.strPaymentMethod,
+a.strSourceSystem,
+d.strEODNumber,
+d.strEODDrawer ,
+d.ysnEODComplete ,
+a.strCardType,
+a.strLocationName strLocationName,
+a.strUserName,
+c.strTransactionId,
+ysnPosted,
+c.dtmDate dtmCMDate,
+(case when c.strTransactionId like 'BWD%' and c.dtmDate >= @dtmCMDate -- DATEADD( SECOND, 1, @dtmDateTo)
+--DATEADD(DAY,1, a.dtmDate) 
+then a.dblAmount*0
+else a.dblAmount end) as dblAmount,
+a.strCompanySegment,
+a.strAccountId
+from vyuCMUndepositedFund a
+inner join tblCMUndepositedFund d on d.intUndepositedFundId = a.intUndepositedFundId
+left outer join tblCMBankTransactionDetail b on a.intUndepositedFundId = b.intUndepositedFundId
+left outer join tblCMBankTransaction c on c.intTransactionId = b.intTransactionId
+where a.dtmDate BETWEEN @dtmDateFrom AND @dtmDateTo ---Sales Payment Date
+and ysnPosted = 1 --- bank dep
+and c.dtmDate >= @dtmCMDate --  DATEADD( SECOND, 1, @dtmDateTo)---bank Post Date
+--DATEADD(DAY,1, a.dtmDate)
+group by
+a.dtmDate,
+a.strName,
+a.strSourceTransactionId,
+a.strPaymentMethod,
+a.strSourceSystem,
+d.strEODNumber,
+d.strEODDrawer,
+d.ysnEODComplete,
+a.strCardType,
+a.strLocationName,
+a.strUserName,
+c.strTransactionId,
+ysnPosted,
+c.dtmDate,
+a.dblAmount,
+a.strCompanySegment,
+a.strAccountId
+having (case when c.strTransactionId like 'BWD%' or c.strTransactionId like 'BTRN%' and
+c.dtmDate >= @dtmCMDate -- DATEADD( SECOND, 1, @dtmDateTo)
+then a.dblAmount*0
+else a.dblAmount end) <>0
 )
+
 select ROW_NUMBER() OVER( ORDER BY dtmDate) rowId, * from mainTable
 where ISNULL(strPaymentMethod,'') <> 'CF Invoice'
 )
