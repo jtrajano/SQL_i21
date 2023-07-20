acb279886cf98ede85ef363896cfbfc9a0ff245b
Nelson Reyes <nelson.reyes@irely.com>
2023-07-20 16:12:19 +0800

    ST-2748

--- i21Database/dbo/Stored Procedures/uspSTCheckoutPosting.sql
+++ i21Database/dbo/Stored Procedures/uspSTCheckoutPosting.sql
@@ -5911,6 +5911,7 @@ BEGIN
 									ON CH.intStoreId = ST.intStoreId
 								INNER JOIN tblICItemUOM STUOM
 									ON ST.intCustomerChargesItemId = STUOM.intItemId
+									AND STUOM.ysnStockUnit = 1
 								INNER JOIN vyuEMEntityCustomerSearch vC 
 									ON CC.intCustomerId = vC.intEntityId					
 								INNER JOIN tblICItemUOM UOM 
@@ -5956,7 +5957,6 @@ BEGIN
 				------------------------------------------------------------------------
 				-- END CREATE SEPARATE INVOICE for Customer Charges
 
-				
 				------------------------------------------------------
 				----------------- TEMP TABLE FOR LOTTERY  ------------
 				------------------------------------------------------
