0b0d01af79c51a6b567089d334b6402c3e73d514
Girome Andaya <girome.andaya@iRely.com>
2023-07-21 09:20:29 +0800

    TR-2657 | Additional 2

---
+++ i21Database/dbo/Stored Procedures/uspTRFreightCommissionDriverTotal.sql
@@ -0,0 +1,105 @@
+﻿CREATE PROCEDURE uspTRFreightCommissionDriverTotal
+(
+  @intDriverId INT,  
+  @strDeliveryType NVARCHAR(100),
+  @dtmRealDateFrom DATE,
+  @dtmRealDateTo DATE,
+  @dblFreightUnitCommissionPct INT,
+  @dblOtherUnitCommissionPct INT,
+  @intFreightCategoryId INT,
+  @intFreightItemId INT,
+  @strDriverName NVARCHAR(250),
+  @intShipViaId INT
+)
+
+AS 
+SET QUOTED_IDENTIFIER OFF    
+SET ANSI_NULLS ON    
+SET NOCOUNT ON    
+SET XACT_ABORT ON    
+SET ANSI_WARNINGS OFF    
+
+BEGIN
+
+DECLARE @dblTotalBilledUnits DECIMAL(18, 6) = 0 
+DECLARE @dblTotalCommission DECIMAL(18, 6) = 0
+DECLARE @tmpItemList TABLE
+(intItemId INT, 
+ intInvoiceId INT NULL,
+ intLoadDistributionDetailId INT,
+ dblUnits INT
+)
+
+
+-- Get list of items for driver
+insert INTO @tmpItemList
+SELECT intItemId, intInvoiceId, intLoadDistributionDetailId, dblUnits
+from vyuTRGetFreightCommissionLine cl  
+where ((cl.intDriverId =  @intDriverId))  
+and (cl.strDeliveryType = @strDeliveryType   
+  OR @strDeliveryType = 'All'   
+  OR cl.strDeliveryType = 'Other Charge'
+  OR (RTRIM(LTRIM(ISNULL(cl.strReceiptLink, ''))) = '' AND cl.intItemCategoryId = @intFreightCategoryId))  
+AND (cl.dtmLoadDateTime >= @dtmRealDateFrom AND cl.dtmLoadDateTime <= @dtmRealDateTo)
+AND (cl.intShipViaId = @intShipViaId OR @intShipViaId = 0)
+
+
+DECLARE @MyCursor CURSOR;
+DECLARE @curItemId INT;
+DECLARE @curInvoiceId INT;
+DECLARE @curLoadDistributionDetailId INT;
+DECLARE @curUnits INT;
+
+BEGIN
+    SET @MyCursor = CURSOR FOR
+    select * from @tmpItemList
+
+
+    OPEN @MyCursor 
+    FETCH NEXT FROM @MyCursor 
+    INTO @curItemId, @curInvoiceId, @curLoadDistributionDetailId, @curUnits
+
+    WHILE @@FETCH_STATUS = 0
+    BEGIN
+
+	print @curItemId
+	print @curInvoiceId
+	print @curLoadDistributionDetailId
+	print '------'
+
+	SET @dblTotalBilledUnits += @curUnits
+
+
+	select
+		@dblTotalCommission += CONVERT(DECIMAL(18,6),CASE WHEN intItemId = @intFreightItemId THEN ((@dblFreightUnitCommissionPct/CONVERT(DECIMAL(18,6),100)) * dblTotal) ELSE ((@dblOtherUnitCommissionPct/CONVERT(DECIMAL(18,6),100)) * dblTotal) END)
+
+  
+	FROM vyuTRGetFreightCommissionFreight fcf
+	WHERE intInvoiceId = @curInvoiceId
+		AND (
+			(intItemId = @intFreightItemId AND intCategoryId = @intFreightCategoryId AND intLoadDistributionDetailId = @curLoadDistributionDetailId)
+			OR (intCategoryId = @intFreightCategoryId AND intItemId != @curItemId AND strBOLNumberDetail IS NULL AND intLoadDistributionDetailId = @curLoadDistributionDetailId)
+			OR (intCategoryId = @intFreightCategoryId AND intItemId != @curItemId AND intLoadDistributionDetailId = @curLoadDistributionDetailId)
+		)
+
+
+
+
+      FETCH NEXT FROM @MyCursor 
+      INTO @curItemId, @curInvoiceId, @curLoadDistributionDetailId, @curUnits
+    END; 
+
+    CLOSE @MyCursor ;
+    DEALLOCATE @MyCursor;
+END;
+
+	print 'before end'
+	print @dblTotalBilledUnits
+	print @dblTotalCommission
+	print @strDriverName
+
+	SELECT 
+		dblTotalBilledUnits = @dblTotalBilledUnits,
+		dblTotalCommission = @dblTotalCommission,
+		strDriverName = @strDriverName
+END
--- i21Database/dbo/Stored Procedures/uspTRFreightCommissionFreightParams.sql
+++ i21Database/dbo/Stored Procedures/uspTRFreightCommissionFreightParams.sql
@@ -1,43 +1,82 @@
-﻿CREATE PROCEDURE uspTRFreightCommissionFreightParams  
-@intItemId INT,  
-@intInvoiceId INT,
-@intFreightItemId INT,
-@intSurchargeItemId INT,
-@intFreightCategoryId INT,
-@dblFreightUnitCommissionPct INT,--@dblFreightUnitCommissionPct DECIMAL(18,6),
-@dblOtherUnitCommissionPct INT,--@dblOtherUnitCommissionPct DECIMAL(18,6),
-@intLoadDistributionDetailId INT
---@dtmFrom NVARCHAR(50),
---@dtmTo NVARCHAR(50)
+﻿
+CREATE PROCEDURE uspTRFreightCommissionFreightParams    
+@intItemId INT,    
+@intInvoiceId INT NULL,  
+@intFreightItemId INT,  
+@intSurchargeItemId INT,  
+@intFreightCategoryId INT,  
+@dblFreightUnitCommissionPct INT,
+@dblOtherUnitCommissionPct INT,
+@intLoadDistributionDetailId INT  
+    
+AS  
   
-AS
+SET QUOTED_IDENTIFIER OFF      
+SET ANSI_NULLS ON      
+SET NOCOUNT ON      
+SET XACT_ABORT ON      
+SET ANSI_WARNINGS OFF      
+ 
+ 
 
-SET QUOTED_IDENTIFIER OFF    
-SET ANSI_NULLS ON    
-SET NOCOUNT ON    
-SET XACT_ABORT ON    
-SET ANSI_WARNINGS OFF    
+ IF(@intInvoiceId > 0)
+	 BEGIN
+		 -- Distribution Detail with Invoice (Delivered to Customer)
+		SELECT     
+		  intItemId  = @intItemId  
+		, intTrueItemId = intItemId  
+		, strItemDescription  = CASE WHEN intItemId = @intFreightItemId THEN 'Unit Freight Charge' ELSE 'Surcharge' END  
+		, intInvoiceId    
+		, dblFreightRate    
+		, dblFreight = dblTotal  
+		, dblCommissionPct = CASE WHEN intItemId = @intFreightItemId THEN @dblFreightUnitCommissionPct ELSE @dblOtherUnitCommissionPct END  
+		, dblTotalCommission = CASE WHEN intItemId = @intFreightItemId THEN ((@dblFreightUnitCommissionPct/CONVERT(DECIMAL(18,6),100)) * dblTotal) ELSE ((@dblOtherUnitCommissionPct/CONVERT(DECIMAL(18,6),100)) * dblTotal) END  
+		FROM vyuTRGetFreightCommissionFreight fcf  
+		WHERE intInvoiceId = @intInvoiceId  
+		 AND (  
+		  (intItemId = @intFreightItemId AND intCategoryId = @intFreightCategoryId AND intLoadDistributionDetailId = @intLoadDistributionDetailId)  
+		  --OR (intItemId = @intSurchargeItemId AND intCategoryId = @intFreightCategoryId AND intLoadDistributionDetailId = @intLoadDistributionDetailId)  
+		  OR (intCategoryId = @intFreightCategoryId AND intItemId != @intItemId AND strBOLNumberDetail IS NULL AND intLoadDistributionDetailId = @intLoadDistributionDetailId)  
+		  OR (intCategoryId = @intFreightCategoryId AND intItemId != @intItemId AND intLoadDistributionDetailId = @intLoadDistributionDetailId)  
+		 )
+		 AND @intInvoiceId IS NOT NULL
+
+	 END
+ ELSE
+	BEGIN
+	 ---- Distribution Detail without Invoice (Delivered to Location but has freight rate and surcharge)
+		-- FOR LDD Freight
+		SELECT     
+		  intItemId  = @intItemId  
+		, intTrueItemId = @intItemId  
+		, strItemDescription  = 'Unit Freight Charge'
+		, intInvoiceId = 0
+		, dblFreightRate    
+		, dblFreight = (dblFreightUnit * dblFreightRate)
+		, dblCommissionPct = @dblFreightUnitCommissionPct
+		, dblTotalCommission = ((@dblFreightUnitCommissionPct/CONVERT(DECIMAL(18,6),100)) * (dblFreightUnit * dblFreightRate))  
+		FROM tblTRLoadDistributionDetail ldd
+			left join tblICItem ii ON ii.intItemId = ldd.intItemId
+		WHERE intLoadDistributionDetailId = @intLoadDistributionDetailId
+			--AND @intInvoiceId IS NULL
+
+		UNION ALL
+
+		-- FOR LDD Surcharge
+		SELECT     
+		  intItemId  = @intItemId  
+		, intTrueItemId = @intItemId  
+		, strItemDescription  = 'Surcharge'
+		, intInvoiceId = 0
+		, dblFreightRate    
+		, dblFreight = (dblFreightUnit * dblFreightRate) * dblDistSurcharge
+		, dblCommissionPct = @dblOtherUnitCommissionPct  
+		, dblTotalCommission =  ((@dblOtherUnitCommissionPct/CONVERT(DECIMAL(18,6),100)) * ((dblFreightUnit * dblFreightRate) * dblDistSurcharge))  
+		FROM tblTRLoadDistributionDetail ldd
+			left join tblICItem ii ON ii.intItemId = ldd.intItemId
+		WHERE intLoadDistributionDetailId = @intLoadDistributionDetailId
+			--AND @intInvoiceId IS NULL
+	
+	END
 
 
---DECLARE @dto DATETIME = CONVERT(NVARCHAR(255),CONVERT(SMALLDATETIME, @dtmTo,105))
---DECLARE @dfrom DATETIME = CONVERT(NVARCHAR(255),CONVERT(SMALLDATETIME, @dtmFrom,105))
-
-SELECT   
-  intItemId  = @intItemId
-, intTrueItemId = intItemId
-, strItemDescription  = CASE WHEN intItemId = @intFreightItemId THEN 'Unit Freight Charge' ELSE 'Surcharge' END
-, intInvoiceId  
-, dblFreightRate  
-, dblFreight = dblTotal
-, dblCommissionPct = CASE WHEN intItemId = @intFreightItemId THEN @dblFreightUnitCommissionPct ELSE @dblOtherUnitCommissionPct END
-, dblTotalCommission = CASE WHEN intItemId = @intFreightItemId THEN ((@dblFreightUnitCommissionPct/CONVERT(DECIMAL(18,6),100)) * dblTotal) ELSE ((@dblOtherUnitCommissionPct/CONVERT(DECIMAL(18,6),100)) * dblTotal) END
-
-  
-FROM vyuTRGetFreightCommissionFreight fcf
-WHERE intInvoiceId = @intInvoiceId
-	AND (
-		(intItemId = @intFreightItemId AND intCategoryId = @intFreightCategoryId AND intLoadDistributionDetailId = @intLoadDistributionDetailId)
-		--OR (intItemId = @intSurchargeItemId AND intCategoryId = @intFreightCategoryId AND intLoadDistributionDetailId = @intLoadDistributionDetailId)
-		OR (intCategoryId = @intFreightCategoryId AND intItemId != @intItemId AND strBOLNumberDetail IS NULL AND intLoadDistributionDetailId = @intLoadDistributionDetailId)
-		OR (intCategoryId = @intFreightCategoryId AND intItemId != @intItemId AND intLoadDistributionDetailId = @intLoadDistributionDetailId)
-	)
