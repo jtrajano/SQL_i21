45c29d4431e89d516a3a477c3d74d2f8526c49e1
Girome Andaya <girome.andaya@iRely.com>
2023-07-21 09:20:29 +0800

    TR-2657 | Additional 2
    (cherry picked from commit 0b0d01af79c51a6b567089d334b6402c3e73d514)

--- i21Database/dbo/Stored Procedures/uspTRFreightCommissionParams.sql
+++ i21Database/dbo/Stored Procedures/uspTRFreightCommissionParams.sql
@@ -1,67 +1,65 @@
-﻿create procedure uspTRFreightCommissionParams
-@dtmDateFrom nvarchar(50),
-@dtmDateTo nvarchar(50),
-@dtmRealDateFrom DATE,
-@dtmRealDateTo DATE,
+﻿create procedure uspTRFreightCommissionParams  
+@dtmDateFrom nvarchar(50),  
+@dtmDateTo nvarchar(50),  
+@dtmRealDateFrom DATE,  
+@dtmRealDateTo DATE,  
 @intDriverId INT,
-@strDeliveryType nvarchar(100),
-@intFreightItemId INT,
-@intSurchargeItemId INT,
-@intFreightCategoryId INT,
-@dblFreightUnitCommissionPct INT,--@dblFreightUnitCommissionPct DECIMAL(18,6),
-@dblOtherUnitCommissionPct INT--@dblOtherUnitCommissionPct DECIMAL(18,6)
+@intShipViaId INT,
+@strDeliveryType nvarchar(100),  
+@intFreightItemId INT,  
+@intSurchargeItemId INT,  
+@intFreightCategoryId INT,  
+@dblFreightUnitCommissionPct INT,
+@dblOtherUnitCommissionPct INT
+    
+as  
+SET QUOTED_IDENTIFIER OFF    
+SET ANSI_NULLS ON    
+SET NOCOUNT ON    
+SET XACT_ABORT ON    
+SET ANSI_WARNINGS OFF    
   
-as
-SET QUOTED_IDENTIFIER OFF  
-SET ANSI_NULLS ON  
-SET NOCOUNT ON  
-SET XACT_ABORT ON  
-SET ANSI_WARNINGS OFF  
+select dtmFrom = @dtmDateFrom  
+,dtmTo = @dtmDateTo  
+,dtmRealDateFrom = @dtmRealDateFrom  
+,dtmRealDateTo = @dtmRealDateTo  
+,strDeliveryType = @strDeliveryType
+,intDriverId  
+,intFreightItemId = @intFreightItemId  
+,intSurchargeItemId = @intSurchargeItemId  
+,intFreightCategoryId = @intFreightCategoryId  
+,dblFreightUnitCommissionPct = @dblFreightUnitCommissionPct  
+,dblOtherUnitCommissionPct = @dblOtherUnitCommissionPct  
+,intShipViaId = @intShipViaId
 
-
---DECLARE @dto DATETIME = CONVERT(NVARCHAR(255),CONVERT(SMALLDATETIME, @dtmDateTo,105))
---DECLARE @dfrom DATETIME = CONVERT(NVARCHAR(255),CONVERT(SMALLDATETIME, @dtmDateFrom,105))
-
---print @dto
---print @dfrom
-
-select dtmFrom = @dtmDateFrom
-,dtmTo = @dtmDateTo
-,strDeliveryType
-,intDriverId
-,intFreightItemId = @intFreightItemId
-,intSurchargeItemId = @intSurchargeItemId
-,intFreightCategoryId = @intFreightCategoryId
-,dblFreightUnitCommissionPct = @dblFreightUnitCommissionPct
-,dblOtherUnitCommissionPct = @dblOtherUnitCommissionPct
-
-,strDriverName
-,dtmLoadDateTime
-,strMovement
-,strVendor = ISNULL(strVendor, '')
-,strSupplyPoint = ISNULL(strSupplyPoint, '')
-,strCustomerNumber = ISNULL(strCustomerNumber, '')
-,strCustomerName = ISNULL(strCustomerName, '')
-,intItemId
-,strItemNo
-,intItemCategoryId
-,strItemCategory
-,strItemDescription
-,dblUnits
-,dblPrice
-,intInvoiceId
-,strCompanyAddress 
-,strCompanyName
-,intLoadHeaderId
-,intLoadDistributionHeaderId
-,intLoadDistributionDetailId
-,dblTotalBillUnit = NULL
-,dblTotalCommision = NULL
-
-from vyuTRGetFreightCommissionLine cl
-where ((cl.intDriverId =  @intDriverId OR @intDriverId = 0) OR (RTRIM(LTRIM(ISNULL(cl.strReceiptLink, ''))) = '' AND cl.intItemCategoryId = @intFreightCategoryId))
-and (cl.strDeliveryType = @strDeliveryType 
-		OR @strDeliveryType = 'All' 
-		OR cl.strDeliveryType = 'Other Charge')
-AND (cl.dtmLoadDateTime >= @dtmRealDateFrom AND cl.dtmLoadDateTime <= @dtmRealDateTo)
-order by cl.dtmLoadDateTime, cl.strMovement desc
+,strDriverName  
+,dtmLoadDateTime  
+,strMovement  
+,strVendor = ISNULL(strVendor, '')  
+,strSupplyPoint = ISNULL(strSupplyPoint, '')  
+,strCustomerNumber = ISNULL(strCustomerNumber, '')  
+,strCustomerName = ISNULL(strCustomerName, '')  
+,intItemId  
+,strItemNo  
+,intItemCategoryId  
+,strItemCategory  
+,strItemDescription  
+,dblUnits  
+,dblPrice  
+,intInvoiceId = ISNULL(intInvoiceId, 0)
+,strCompanyAddress   
+,strCompanyName  
+,intLoadHeaderId  
+,intLoadDistributionHeaderId  
+,intLoadDistributionDetailId  
+  
+from vyuTRGetFreightCommissionLine cl  
+  
+where ((cl.intDriverId =  @intDriverId OR @intDriverId = 0))  
+and (cl.strDeliveryType = @strDeliveryType   
+  OR @strDeliveryType = 'All'   
+  OR cl.strDeliveryType = 'Other Charge'
+  OR (RTRIM(LTRIM(ISNULL(cl.strReceiptLink, ''))) = '' AND cl.intItemCategoryId = @intFreightCategoryId))  
+AND (cl.dtmLoadDateTime >= @dtmRealDateFrom AND cl.dtmLoadDateTime <= @dtmRealDateTo) 
+AND (cl.intShipViaId = @intShipViaId OR @intShipViaId = 0)
+order by cl.dtmLoadDateTime, cl.strMovement desc  
--- i21Database/dbo/Views/vyuTRGetFreightCommissionFreight.sql
+++ i21Database/dbo/Views/vyuTRGetFreightCommissionFreight.sql
@@ -1,23 +1,25 @@
-﻿create view vyuTRGetFreightCommissionFreight  
-as  
+﻿create  view vyuTRGetFreightCommissionFreight    
+as    
+    
+select     
+ aid.intItemId,  
+ ai.intInvoiceId,  
+ ii.intCategoryId,  
+ aid.intLoadDistributionDetailId,  
+ aid.strItemDescription,  
+ aid.strBOLNumberDetail,  
+ aid.dblQtyShipped,  
+ dblFreightRate = CASE WHEN ldd.intLoadDistributionDetailId IS NULL THEN ISNULL(aid.dblBasePrice, 0) ELSE ISNULL(ldd.dblFreightRate, 0) END,  
+ aid.dblTotal,  
+ lh.dtmLoadDateTime  
   
-select   
-	aid.intItemId,
-	ai.intInvoiceId,
-	ii.intCategoryId,
-	aid.intLoadDistributionDetailId,
-	aid.strItemDescription,
-	aid.strBOLNumberDetail,
-	aid.dblQtyShipped,
-	dblFreightRate = CASE WHEN ldd.intLoadDistributionDetailId IS NULL THEN ISNULL(aid.dblBasePrice, 0) ELSE ISNULL(ldd.dblFreightRate, 0) END,
-	aid.dblTotal,
-	lh.dtmLoadDateTime
-
-from tblARInvoiceDetail aid  
- left join tblARInvoice ai on ai.intInvoiceId = aid.intInvoiceId
- left join tblICItem ii on ii.intItemId = aid.intItemId
- left join tblTRLoadDistributionDetail ldd on ldd.intLoadDistributionDetailId = aid.intLoadDistributionDetailId
- left join tblTRLoadHeader lh on lh.strTransaction = ai.strActualCostId
- 
-where ai.strType = 'Transport Delivery'
-and strDocumentNumber like 'TR%'  
+from tblARInvoiceDetail aid    
+ left join tblARInvoice ai on ai.intInvoiceId = aid.intInvoiceId  
+ left join tblICItem ii on ii.intItemId = aid.intItemId  
+ left join tblTRLoadDistributionDetail ldd on ldd.intLoadDistributionDetailId = aid.intLoadDistributionDetailId  
+ left join tblTRLoadHeader lh on lh.strTransaction = ai.strActualCostId  
+   
+where ai.strType = 'Transport Delivery'  
+and strDocumentNumber like 'TR%'    
+  
+  
