3abd7daec4d1573221b3bef7220e7f448f24691c
Arman Arenas <arman.arenas@irely.com>
2023-07-21 21:37:47 +0800

    LG-4427 implement audit log entry on dispatch and orders status changes

--- i21Database/dbo/Stored Procedures/uspLGDispatchUpdateOrders.sql
+++ i21Database/dbo/Stored Procedures/uspLGDispatchUpdateOrders.sql
@@ -8,6 +8,9 @@ BEGIN
 			,@intLoadHeaderId INT = NULL
 			,@ysnTRPosted BIT
 
+	IF @intEntityUserSecurityId IS NULL
+	SELECT TOP 1 @intEntityUserSecurityId = intEntityId FROM tblSMUserSecurity WHERE ysnAdmin = 1
+
 	/* Get Dispatch Schedule Status */
 	SELECT @intSourceType = intSourceType 
 		,@intDispatchStatus = intDispatchStatus
@@ -26,11 +29,21 @@ BEGIN
 		IF (ISNULL(@ysnTRPosted, 0) = 1)
 		BEGIN
 			UPDATE tblLGDispatchOrder
-			SET intDispatchStatus = CASE WHEN (ISNULL(@ysnTRPosted, 0) = 1) 
-				THEN 5 /* Complete */ 
-				ELSE 4 /* In Progress */
-				END
+			SET intDispatchStatus = 5 /* Complete */
 			WHERE intDispatchOrderId = @intDispatchOrderId
+			AND intDispatchStatus <> 5
+
+			/* Log changes on dispatch status */
+			IF ((SELECT TOP 1 intDispatchStatus FROM tblLGDispatchOrder WHERE intDispatchOrderId = @intDispatchOrderId) <> @intDispatchStatus)
+			BEGIN
+				EXEC uspSMAuditLog	
+					@keyValue	=	@intDispatchOrderId,
+					@screenName =	'Logistics.view.DispatchOrder',
+					@entityId	=	@intEntityUserSecurityId,
+					@actionType =	'Completed',
+					@actionIcon =	'small-tree-modified',
+					@details	=	''
+			END
 
 			/* Check for deleted orders and mark them as 'Cancelled' on Route */
 			UPDATE tblLGDispatchOrderRoute
@@ -44,6 +57,23 @@ BEGIN
 		END
 		ELSE /* If TR is Saved or Unposted */
 		BEGIN
+			UPDATE tblLGDispatchOrder
+			SET intDispatchStatus = 3 /* Dispatched */
+			WHERE intDispatchOrderId = @intDispatchOrderId
+			AND intDispatchStatus <> 3
+
+			/* Log changes on dispatch status */
+			IF ((SELECT TOP 1 intDispatchStatus FROM tblLGDispatchOrder WHERE intDispatchOrderId = @intDispatchOrderId) <> @intDispatchStatus)
+			BEGIN
+				EXEC uspSMAuditLog	
+					@keyValue	=	@intDispatchOrderId,
+					@screenName =	'Logistics.view.DispatchOrder',
+					@entityId	=	@intEntityUserSecurityId,
+					@actionType =	'Dispatched',
+					@actionIcon =	'small-tree-modified',
+					@details	=	''
+			END
+
 			/* If Receipt Header with intDispatchOrderRouteId exists, update the route Id status to 'Loaded' */
 			UPDATE tblLGDispatchOrderRoute
 			SET intOrderStatus = 4 /* Loaded */ 
@@ -65,17 +95,28 @@ BEGIN
 														WHERE intLoadHeaderId = @intLoadHeaderId))
 			AND intOrderStatus NOT IN (4, 6)
 		END
-
 	END
 	ELSE
 	BEGIN
 		/* If deleting associated TR, set DS back to 'Dispatched' status */
-		IF (@intDispatchStatus = 4)
+		IF (@intDispatchStatus = 3)
 		BEGIN
 			UPDATE tblLGDispatchOrder
 				SET intDispatchStatus = 3 /* Dispatched */ 
 			WHERE intDispatchOrderId = @intDispatchOrderId
-			AND intDispatchStatus = 4
+			AND intDispatchStatus <> 3
+
+			/* Log changes on dispatch status */
+			IF ((SELECT TOP 1 intDispatchStatus FROM tblLGDispatchOrder WHERE intDispatchOrderId = @intDispatchOrderId) <> @intDispatchStatus)
+			BEGIN
+				EXEC uspSMAuditLog	
+					@keyValue	=	@intDispatchOrderId,
+					@screenName =	'Logistics.view.DispatchOrder',
+					@entityId	=	@intEntityUserSecurityId,
+					@actionType =	'Dispatched',
+					@actionIcon =	'small-tree-modified',
+					@details	=	''
+			END
 		END
 	END
 
@@ -90,22 +131,52 @@ BEGIN
 	SELECT @intDispatchStatus = intDispatchStatus
 	FROM tblLGDispatchOrder WHERE intDispatchOrderId = @intDispatchOrderId 
 
-	IF (@intSourceType = 2) /* Update TM Orders Status */
+	/* Update TM Orders Status */
+	IF (@intSourceType = 2)
 	BEGIN
-		IF (@intDispatchStatus IN (4))
+		DECLARE @ordersToUpdate TABLE ( intDispatchId INT, strWillCallStatus NVARCHAR(200))
+
+		IF (@intDispatchStatus IN (5))
 		BEGIN
 			/* Load Delivered */
+			INSERT INTO @ordersToUpdate (
+				intDispatchID
+				,strWillCallStatus )
+			SELECT 
+				TMD.intDispatchID
+				,TMD.strWillCallStatus
+			FROM tblTMDispatch TMD
+				INNER JOIN tblLGDispatchOrderDetail DOD ON DOD.intTMDispatchId = TMD.intDispatchID
+				INNER JOIN tblLGDispatchOrder DO ON DO.intDispatchOrderId = DOD.intDispatchOrderId
+			WHERE DO.intDispatchOrderId = @intDispatchOrderId
+				AND DOD.intOrderStatus = 4
+				AND TMD.strWillCallStatus NOT IN ('Delivered')
+
 			UPDATE TMD
 				SET strWillCallStatus = 'Delivered'
 			FROM tblTMDispatch TMD
 				INNER JOIN tblLGDispatchOrderDetail DOD ON DOD.intTMDispatchId = TMD.intDispatchID
 				INNER JOIN tblLGDispatchOrder DO ON DO.intDispatchOrderId = DOD.intDispatchOrderId
 			WHERE DO.intDispatchOrderId = @intDispatchOrderId
+				AND DOD.intOrderStatus = 4
 				AND TMD.strWillCallStatus NOT IN ('Delivered')
 		END
 		ELSE IF (@intDispatchStatus IN (3)) 
 		BEGIN
 			/* Load Dispatched */
+			INSERT INTO @ordersToUpdate (
+				intDispatchID
+				,strWillCallStatus )
+			SELECT 
+				TMD.intDispatchID
+				,TMD.strWillCallStatus
+			FROM tblTMDispatch TMD
+				INNER JOIN tblLGDispatchOrderDetail DOD ON DOD.intTMDispatchId = TMD.intDispatchID
+				INNER JOIN tblLGDispatchOrder DO ON DO.intDispatchOrderId = DOD.intDispatchOrderId
+				LEFT JOIN tblLGDispatchOrderRoute DOR ON DOR.intDispatchOrderDetailId = DOD.intDispatchOrderDetailId
+			WHERE DO.intDispatchOrderId = @intDispatchOrderId
+				AND TMD.strWillCallStatus NOT IN ('Delivered', 'Routed')
+
 			UPDATE TMD
 				SET ysnDispatched = 1
 					,strWillCallStatus = 'Routed'
@@ -117,24 +188,71 @@ BEGIN
 				INNER JOIN tblLGDispatchOrder DO ON DO.intDispatchOrderId = DOD.intDispatchOrderId
 				LEFT JOIN tblLGDispatchOrderRoute DOR ON DOR.intDispatchOrderDetailId = DOD.intDispatchOrderDetailId
 			WHERE DO.intDispatchOrderId = @intDispatchOrderId
-				AND TMD.strWillCallStatus NOT IN ('Delivered')
+				AND TMD.strWillCallStatus NOT IN ('Delivered', 'Routed')
 		END
 		ELSE
 		BEGIN
 			/* Load Cancelled */
+			INSERT INTO @ordersToUpdate (
+				intDispatchID
+				,strWillCallStatus )
+			SELECT 
+				TMD.intDispatchID
+				,TMD.strWillCallStatus
+			FROM tblTMDispatch TMD
+				INNER JOIN tblLGDispatchOrderDetail DOD ON DOD.intTMDispatchId = TMD.intDispatchID
+				INNER JOIN tblLGDispatchOrder DO ON DO.intDispatchOrderId = DOD.intDispatchOrderId
+				LEFT JOIN tblLGDispatchOrderRoute DOR ON DOR.intDispatchOrderDetailId = DOD.intDispatchOrderDetailId
+				LEFT JOIN tblTMSite TMS ON TMS.intSiteID = TMD.intSiteID
+			WHERE DO.intDispatchOrderId = @intDispatchOrderId
+				AND TMD.strWillCallStatus NOT IN ('Delivered', 'Generated')
+
 			UPDATE TMD
 				SET ysnDispatched = 0
 					,strWillCallStatus = 'Generated'
 					,dtmDispatchingDate = NULL
 					,intDriverID = COALESCE(DOR.intDriverEntityId, DO.intDriverEntityId, TMD.intDriverID)
 					,intDispatchOrderId = NULL
-				FROM tblTMDispatch TMD
+			FROM tblTMDispatch TMD
 				INNER JOIN tblLGDispatchOrderDetail DOD ON DOD.intTMDispatchId = TMD.intDispatchID
 				INNER JOIN tblLGDispatchOrder DO ON DO.intDispatchOrderId = DOD.intDispatchOrderId
 				LEFT JOIN tblLGDispatchOrderRoute DOR ON DOR.intDispatchOrderDetailId = DOD.intDispatchOrderDetailId
 				LEFT JOIN tblTMSite TMS ON TMS.intSiteID = TMD.intSiteID
 			WHERE DO.intDispatchOrderId = @intDispatchOrderId
-				AND TMD.strWillCallStatus NOT IN ('Delivered')
+				AND TMD.strWillCallStatus NOT IN ('Delivered', 'Generated')
+		END
+
+		/* Log TM Order status changes */
+		DECLARE @intDispatchId INT
+		DECLARE @intSiteId INT
+		DECLARE @strOrderNumber NVARCHAR(200)
+		DECLARE @strWillCallStatus NVARCHAR(200)
+		DECLARE @strCurrentWillCallStatus NVARCHAR(200)
+		DECLARE @strAuditDescription NVARCHAR(MAX)
+
+		WHILE EXISTS(SELECT 1 FROM @ordersToUpdate)
+		BEGIN
+			SELECT TOP 1 
+				@intDispatchId = OTU.intDispatchId
+				,@strOrderNumber = TMD.strOrderNumber
+				,@strWillCallStatus = OTU.strWillCallStatus
+				,@strCurrentWillCallStatus = TMD.strWillCallStatus
+				,@intSiteId = TMD.intSiteID
+			FROM @ordersToUpdate OTU
+			INNER JOIN tblTMDispatch TMD ON TMD.intDispatchID = OTU.intDispatchId
+
+			SELECT @strAuditDescription = 'Updated - ' + @strOrderNumber + ' Order Status'
+			PRINT @strAuditDescription
+			EXEC dbo.uspSMAuditLog @keyValue = @intDispatchOrderId 
+				,@screenName = 'Logistics.view.DispatchOrder'
+				,@entityId = @intEntityUserSecurityId
+				,@actionType = 'Updated Orders'
+				,@actionIcon = 'small-tree-modified'
+				,@changeDescription = @strAuditDescription
+				,@fromValue = @strWillCallStatus
+				,@toValue = @strCurrentWillCallStatus
+
+			DELETE FROM @ordersToUpdate WHERE intDispatchId = @intDispatchId
 		END
 	END
 END
