39a836b16d3fc66d197667a920039d19ac850146
Bryan <013.bryan@gmail.com>
2023-07-21 10:27:26 +0800

    CT-9183

--- i21Database/dbo/Functions/fnCTGetShipmentStatus.sql
+++ i21Database/dbo/Functions/fnCTGetShipmentStatus.sql
@@ -6,10 +6,10 @@ RETURNS table as return
 	SELECT TOP 1 strShipmentStatus = strShipmentStatus COLLATE Latin1_General_CI_AS
 	FROM
 	(
-		SELECT ROW_NUMBER() OVER(PARTITION BY intLoadDetailId ORDER BY dtmScheduledDate) AS intNumberId, strShipmentStatus, intPriorityId = CASE WHEN strShipmentStatus = 'Cancelled' THEN 2 ELSE 1 END
+		SELECT ROW_NUMBER() OVER(PARTITION BY intLoadDetailId ORDER BY dtmScheduledDate) AS intNumberId, strShipmentStatus, intPriorityId = CASE WHEN strShipmentStatus = 'Cancelled' THEN 2 ELSE 1 END, dtmScheduledDate
 		FROM vyuCTShipmentStatus
 		WHERE (intPContractDetailId = @intContractDetailId or intSContractDetailId = @intContractDetailId)
 		AND ((intShipmentType = 2 AND strShipmentStatus <> 'Scheduled') OR intShipmentType = 1)
 	) tbl
 	WHERE intNumberId = 1
-	ORDER BY intPriorityId ASC
+	ORDER BY dtmScheduledDate desc
