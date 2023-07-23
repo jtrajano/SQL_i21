c9326e63495e70c10ebf1e86569cd8a70159ea54
Kevin Magnifico <kevin.magnifico@iRely.com>
2023-07-23 11:52:32 +0800

    ST-2743

---
+++ i21Database/dbo/Views/vyuSTPromotionItemListDetail.sql
@@ -0,0 +1,33 @@
+
+CREATE VIEW [dbo].[vyuSTPromotionItemListDetail]
+AS
+
+SELECT intPromoItemListDetailId,
+	tblSTPromotionItemListDetail.intPromoItemListId,
+	tblSTPromotionItemListDetail.intItemUOMId,
+	strUpcDescription,
+	intUpcModifier,
+	strUpcCode,
+    	strLongUPCCode,
+    	strUnitMeasure,
+	dblSalePrice AS dblRetailPrice,
+	tblSTPromotionItemListDetail.intConcurrencyId
+	--vyuSTItemHierarchyPricing.intItemLocationId
+	FROM tblSTPromotionItemListDetail
+		
+	JOIN tblSTPromotionItemList ON 
+	tblSTPromotionItemListDetail.intPromoItemListId = tblSTPromotionItemList.intPromoItemListId
+		
+	LEFT JOIN tblSTStore ON tblSTPromotionItemList.intStoreId = tblSTStore.intStoreId
+		
+	LEFT JOIN tblICItemUOM ON tblSTPromotionItemListDetail.intItemUOMId = tblICItemUOM.intItemUOMId
+		
+	LEFT JOIN tblICItemLocation ON tblSTStore.intCompanyLocationId = tblICItemLocation.intLocationId 
+	AND tblICItemLocation.intItemId = tblICItemUOM.intItemId
+		
+	LEFT JOIN vyuSTItemHierarchyPricing ON tblSTPromotionItemListDetail.intItemUOMId = vyuSTItemHierarchyPricing.intItemUOMId
+	AND vyuSTItemHierarchyPricing.intItemLocationId = tblICItemLocation.intItemLocationId
+
+	LEFT JOIN tblICUnitMeasure ON tblICItemUOM.intUnitMeasureId = tblICUnitMeasure.intUnitMeasureId;
+
+GO
