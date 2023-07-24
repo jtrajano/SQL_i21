d65e33872e17447db778402ea8b0eedcab290043
LexPeregrino <lex.peregrino@irely.com>
2023-07-24 16:04:38 +0800

    CT-9195 v1
    (cherry picked from commit e8b88658512d904fcfdfb5f0ba17f7c96546bd4f)
    (cherry picked from commit e40216085c852ff8c4822e38a357888bc749ba75)
    (cherry picked from commit d48f70bed1884eff83aa45a887ef012f4af776ce)

--- i21Database/dbo/Views/vyuCTFWCOverview.sql
+++ i21Database/dbo/Views/vyuCTFWCOverview.sql
@@ -94,6 +94,8 @@ AS
 			LEFT JOIN tblSMCity					DP	 WITH (NOLOCK) ON DP.intCityId			  =	CTD.intDestinationPortId
 			LEFT JOIN tblMFLocationLeadTime		MFL	 WITH (NOLOCK) ON CTD.intLoadingPortId    = MFL.intPortOfDispatchId AND  CTD.intDestinationPortId = MFL.intPortOfArrivalId		
 			AND (SELECT  dbo.[fnCTGetSeqDisplayField](CTD.intContractDetailId, 'Origin')) = MFL.strOrigin
+			AND MFL.intChannelId = CTD.intMarketZoneId
+			AND MFL.strReceivingStorageLocation = dbo.[fnCTGetSeqDisplayField](CTD.intSubLocationId, 'tblSMCompanyLocationSubLocation')
 			LEFT JOIN tblSMPurchasingGroup		PG	 WITH (NOLOCK) ON PG.intPurchasingGroupId = CTD.intPurchasingGroupId
 			LEFT JOIN tblARMarketZone			MZ   WITH (NOLOCK) ON MZ.intMarketZoneId	  = CTD.intMarketZoneId
 			--OUTER APPLY dbo.fnCTGetSampleDetailAllocation(CTD.intContractDetailId) QA
