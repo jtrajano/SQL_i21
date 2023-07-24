313a89e9fa5a0d976b16ca2566898a645c1e410b
AzureAD\AlexRelano <alex.relano@irely.com>
2023-07-24 16:59:44 +0800

    IC-11609
    (cherry picked from commit a1e2036657da368d6f951de261bd68a4d2e4bfc1)
    (cherry picked from commit 82469c6b5a65d0e9ae2191b8217ba4b11794dba6)

--- i21Database/dbo/Functions/fnAllowCommodityToChange.sql
+++ i21Database/dbo/Functions/fnAllowCommodityToChange.sql
@@ -1,36 +1,32 @@
 ﻿CREATE FUNCTION fnAllowCommodityToChange (
-	@intItemId AS INT 
-	,@intCommodityId AS INT 
+    @intItemId AS INT,
+    @intCommodityId AS INT
 )
 RETURNS BIT
 AS
 BEGIN
-	IF (
-		@intItemId IS NOT NULL 
-		AND (
-			EXISTS (SELECT TOP 1 1 FROM tblICInventoryTransaction t WHERE t.intItemId = @intItemId AND ISNULL(t.ysnIsUnposted,0) = 0) 
-			OR EXISTS (
-				SELECT	TOP 1 1 
-				FROM	tblICLot l 
-				WHERE	l.dblQty <> 0
-						AND l.intItemId = @intItemId
-			)
-			OR EXISTS (
-				SELECT TOP 1 
-					ch.intCommodityId
-	
-				FROM 
-					tblCTContractHeader ch INNER JOIN tblCTContractDetail cd
-						ON ch.intContractHeaderId = cd.intContractHeaderId
-				WHERE 
-					--(ch.intCommodityId = @intCommodityId AND @intCommodityId IS NOT NULL)
-					(cd.intItemId = @intItemId AND @intItemId IS NOT NULL) 
-			)
-		)
-	)
-	BEGIN 
-		RETURN 0
-	END 
+    IF (@intItemId IS NOT NULL AND @intCommodityId IS NOT NULL)
+    BEGIN
+        IF (
+            EXISTS (
+                SELECT 1 FROM tblICInventoryTransaction t
+                WHERE t.intItemId = @intItemId AND ISNULL(t.ysnIsUnposted, 0) = 0
+            )
+            OR EXISTS (
+                SELECT 1 FROM tblICLot l
+                WHERE l.dblQty <> 0 AND l.intItemId = @intItemId
+            )
+            OR EXISTS (
+                SELECT 1
+                FROM tblCTContractHeader ch
+                INNER JOIN tblCTContractDetail cd ON ch.intContractHeaderId = cd.intContractHeaderId
+                WHERE cd.intItemId = @intItemId
+            )
+        )
+        BEGIN 
+            RETURN 0
+        END
+    END
 
-	RETURN 1
+    RETURN 1
 END
