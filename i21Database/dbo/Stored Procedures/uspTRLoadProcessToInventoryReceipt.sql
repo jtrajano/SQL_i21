a0b20f61cce02b37377dc6ccaa2418bdaa72e497
manny_armas <manny.armas@iRely.com>
2023-07-25 14:08:33 +0800

    TR-2705 - [Receipt Detail] No Receipt Information Error

--- i21Database/dbo/Stored Procedures/uspTRLoadProcessToInventoryReceipt.sql
+++ i21Database/dbo/Stored Procedures/uspTRLoadProcessToInventoryReceipt.sql
@@ -134,6 +134,7 @@ END
 						ELSE
 							ISNULL(SUM(DD.dblUnits) ,0)
 						END
+						 + SUM(BID.dblQuantity)
 		,dblCost					= min(TR.dblUnitCost)  
 		,intCurrencyId				= @defaultCurrency 
 		,dblExchangeRate   = 1 -- Need to check this  
@@ -156,6 +157,7 @@ END
 											ELSE
 												ISNULL(SUM(DD.dblUnits) ,0)
 											END
+											 + SUM(BID.dblQuantity)
 		,dblNet						= CASE WHEN ISNULL(@ysnAllowDifferentUnits,1) = 1 THEN 
 												ISNULL(
 													CASE WHEN min(SP.strGrossOrNet) = 'Gross' THEN SUM(DD.dblDistributionGrossSalesUnits)
@@ -165,6 +167,7 @@ END
 											ELSE
 												ISNULL(SUM(DD.dblUnits) ,0)
 											END
+											 + SUM(BID.dblQuantity)
 		,intInventoryReceiptId		= min(TR.intInventoryReceiptId)
 		,dblSurcharge				= min(TR.dblPurSurcharge)
 		,ysnFreightInPrice			= CAST(MIN(CAST(TR.ysnFreightInPrice AS INT)) AS BIT)
@@ -199,7 +202,7 @@ END
 			AND TR.strOrigin = 'Terminal'
 			AND IC.strType != 'Non-Inventory'
 			AND (TR.dblUnitCost != 0 or TR.dblFreightRate != 0 or TR.dblPurSurcharge != 0)
-			AND DD.intLoadDistributionDetailId IS NOT NULL
+			AND (DD.intLoadDistributionDetailId IS NOT NULL OR (intLoadDistributionDetailId IS NULL AND BID.intLoadHeaderId IS NOT NULL))
     group by TR.intLoadReceiptId,DH.strDestination,  DH.intCompanyLocationId,DD.intLoadDistributionDetailId
 	ORDER BY intEntityVendorId
 		,strBillOfLadding
