2c79835ad7b4834e9be9502c9a5d1c40f02f9d95
MjArat <marcjeromearat@yahoo.com>
2023-07-26 16:16:46 +0800

    MFG-5301

--- i21Database/dbo/Stored Procedures/uspMFGetBlendSheetInputLots.sql
+++ i21Database/dbo/Stored Procedures/uspMFGetBlendSheetInputLots.sql
@@ -1,4 +1,4 @@
-﻿CREATE PROCEDURE uspMFGetBlendSheetInputLots
+﻿CREATE PROCEDURE [dbo].[uspMFGetBlendSheetInputLots]
 	@intWorkOrderId INT
 AS
 
@@ -26,28 +26,28 @@ BEGIN
 			 , wi.intItemUOMId
 			 , wi.dblIssuedQuantity
 			 , wi.intItemIssuedUOMId
-			 , l.dblWeightPerQty AS dblWeightPerUnit
+			 , Lot.dblWeightPerQty AS dblWeightPerUnit
 			 , wi.intSequenceNo
 			 , wi.dtmCreated
 			 , wi.intCreatedUserId
 			 , wi.dtmLastModified
 			 , wi.intLastModifiedUserId
 			 , CAST(0 AS BIT) AS ysnParentLot
-			 , l.strLotNumber
+			 , Lot.strLotNumber
 			 , i.intItemId
 			 , i.strItemNo
 			 , i.strDescription
 			 , um.strUnitMeasure AS strUOM
 			 , um1.strUnitMeasure AS strIssuedUOM
 			 , wi.intRecipeItemId
-			 , Case When @ysnDisplayLandedPriceInBlendManagement=1 Then IsNULL(Batch.dblLandedPrice,0) Else l.dblLastCost End AS dblUnitCost
-			 , ISNULL(l.strLotAlias,'') AS strLotAlias
-			 , l.strGarden AS strGarden
-			 , l.intLocationId
+			 , Case When @ysnDisplayLandedPriceInBlendManagement=1 Then IsNULL(Batch.dblLandedPrice,0) Else Lot.dblLastCost End AS dblUnitCost
+			 , ISNULL(Lot.strLotAlias,'') AS strLotAlias
+			 , Lot.strGarden AS strGarden
+			 , Lot.intLocationId
 			 , cl.strLocationName AS strLocationName
 			 , sbl.strSubLocationName
 			 , sl.strName AS strStorageLocationName
-			 , l.strNotes AS strRemarks
+			 , Lot.strNotes AS strRemarks
 			 , i.dblRiskScore
 			 , ri.dblQuantity/@dblRecipeQty AS dblConfigRatio
 			 , CAST(ISNULL(q.Density,0) AS DECIMAL) AS dblDensity
@@ -68,10 +68,10 @@ BEGIN
 			 , SubCluster.strDescription	AS strSubCluster
 			 , Batch.dblTeaAppearance
 			 , ISNULL(Batch.dblTeaVolume, 0) AS dblTeaVolume
-			 , DATEDIFF(DAY, l.dtmDateCreated, GETDATE()) AS intAge
+			 , DATEDIFF(DAY, IsNULL(Lot.dtmManufacturedDate ,Lot.dtmDateCreated), GETDATE()) AS intAge
 			 , CASE WHEN (NULLIF(i.intUnitPerLayer,'') IS NULL OR i.intUnitPerLayer = 0) AND (NULLIF(i.intLayerPerPallet,'') IS NULL OR i.intLayerPerPallet = 0) THEN 0
 					WHEN (CASE WHEN ISNULL(wi.dblIssuedQuantity,0) > 0 THEN wi.dblIssuedQuantity 
-							   ELSE dbo.fnMFConvertQuantityToTargetItemUOM(wi.intItemUOMId, l.intItemUOMId, wi.dblIssuedQuantity) 
+							   ELSE dbo.fnMFConvertQuantityToTargetItemUOM(wi.intItemUOMId, Lot.intItemUOMId, wi.dblIssuedQuantity) 
 						  END) = 0 THEN 0
 					ELSE CAST(wi.dblIssuedQuantity / (i.intUnitPerLayer * i.intLayerPerPallet) AS NUMERIC(18, 0))
 			   END AS dblNoOfPallet
@@ -84,19 +84,19 @@ BEGIN
 		JOIN tblMFWorkOrder w ON wi.intWorkOrderId = w.intWorkOrderId
 		JOIN tblICItemUOM iu ON wi.intItemUOMId = iu.intItemUOMId
 		JOIN tblICUnitMeasure um ON iu.intUnitMeasureId = um.intUnitMeasureId
-		JOIN tblICLot l ON wi.intLotId = l.intLotId
-		JOIN tblICLotStatus LS ON l.intLotStatusId = LS.intLotStatusId
-		JOIN tblICItem i ON l.intItemId = i.intItemId
+		JOIN tblICLot Lot ON wi.intLotId = Lot.intLotId
+		JOIN tblICLotStatus LS ON Lot.intLotStatusId = LS.intLotStatusId
+		JOIN tblICItem i ON Lot.intItemId = i.intItemId
 		JOIN tblICItemUOM iu1 ON wi.intItemIssuedUOMId = iu1.intItemUOMId
 		JOIN tblICUnitMeasure um1 ON iu1.intUnitMeasureId = um1.intUnitMeasureId
-		JOIN tblSMCompanyLocation cl ON cl.intCompanyLocationId = l.intLocationId
-		LEFT JOIN tblSMCompanyLocationSubLocation sbl ON sbl.intCompanyLocationSubLocationId = l.intSubLocationId
-		LEFT JOIN tblICStorageLocation sl ON sl.intStorageLocationId = l.intStorageLocationId
-		LEFT JOIN vyuQMGetLotQuality q ON l.intLotId = q.intLotId
+		JOIN tblSMCompanyLocation cl ON cl.intCompanyLocationId = Lot.intLocationId
+		LEFT JOIN tblSMCompanyLocationSubLocation sbl ON sbl.intCompanyLocationSubLocationId = Lot.intSubLocationId
+		LEFT JOIN tblICStorageLocation sl ON sl.intStorageLocationId = Lot.intStorageLocationId
+		LEFT JOIN vyuQMGetLotQuality q ON Lot.intLotId = q.intLotId
 		LEFT JOIN tblMFRecipeItem ri ON wi.intRecipeItemId = ri.intRecipeItemId
 		LEFT JOIN tblICCommodityAttribute MT on MT.intCommodityAttributeId=i.intProductTypeId
 		LEFT JOIN tblICBrand B on B.intBrandId=i.intBrandId
-		LEFT JOIN tblMFLotInventory AS LotInventory ON LotInventory.intLotId = l.intLotId
+		LEFT JOIN tblMFLotInventory AS LotInventory ON LotInventory.intLotId = Lot.intLotId
 		LEFT JOIN tblMFBatch AS Batch ON LotInventory.intBatchId = Batch.intBatchId
 		LEFT JOIN tblSMCompanyLocation AS AuctionCenter ON Batch.intBuyingCenterLocationId = AuctionCenter.intCompanyLocationId
 		LEFT JOIN tblICCommodityAttribute AS SubCluster ON i.intRegionId = SubCluster.intCommodityAttributeId
@@ -111,28 +111,28 @@ BEGIN
 			 , wi.intItemUOMId
 			 , wi.dblIssuedQuantity
 			 , wi.intItemIssuedUOMId
-			 , l.dblWeightPerQty AS dblWeightPerUnit
+			 , Lot.dblWeightPerQty AS dblWeightPerUnit
 			 , wi.intSequenceNo
 			 , wi.dtmCreated
 			 , wi.intCreatedUserId
 			 , wi.dtmLastModified
 			 , wi.intLastModifiedUserId
 			 , CAST(0 AS BIT) AS ysnParentLot
-			 , l.strLotNumber
+			 , Lot.strLotNumber
 			 , i.intItemId
 			 , i.strItemNo
 			 , i.strDescription
 			 , um.strUnitMeasure AS strUOM
 			 , um1.strUnitMeasure AS strIssuedUOM
 			 , wi.intRecipeItemId
-			 , l.dblLastCost AS dblUnitCost
-			 , ISNULL(l.strLotAlias, '') AS strLotAlias
-			 , l.strGarden AS strGarden
-			 , l.intLocationId
+			 , Lot.dblLastCost AS dblUnitCost
+			 , ISNULL(Lot.strLotAlias, '') AS strLotAlias
+			 , Lot.strGarden AS strGarden
+			 , Lot.intLocationId
 			 , cl.strLocationName AS strLocationName
 			 , sbl.strSubLocationName
 			 , sl.strName AS strStorageLocationName
-			 , l.strNotes AS strRemarks
+			 , Lot.strNotes AS strRemarks
 			 , i.dblRiskScore
 			 , ri.dblQuantity / @dblRecipeQty AS dblConfigRatio
 			 , CAST(ISNULL(q.Density,0) AS DECIMAL) AS dblDensity
@@ -153,40 +153,38 @@ BEGIN
 			 , SubCluster.strDescription	AS strSubCluster
 			 , Batch.dblTeaAppearance
 			 , ISNULL(Batch.dblTeaVolume, 0) AS dblTeaVolume
-			 , DATEDIFF(DAY, l.dtmDateCreated, GETDATE()) AS intAge
+			 , DATEDIFF(DAY, IsNULL(Lot.dtmManufacturedDate ,Lot.dtmDateCreated), GETDATE()) AS intAge
 			 , CASE WHEN (NULLIF(i.intUnitPerLayer,'') IS NULL OR i.intUnitPerLayer = 0) AND (NULLIF(i.intLayerPerPallet,'') IS NULL OR i.intLayerPerPallet = 0) THEN 0
 					WHEN (CASE WHEN ISNULL(wi.dblIssuedQuantity,0) > 0 THEN wi.dblIssuedQuantity 
-							   ELSE dbo.fnMFConvertQuantityToTargetItemUOM(wi.intItemUOMId, l.intItemUOMId, wi.dblIssuedQuantity) 
+							   ELSE dbo.fnMFConvertQuantityToTargetItemUOM(wi.intItemUOMId, Lot.intItemUOMId, wi.dblIssuedQuantity) 
 						  END) = 0 THEN 0
 					ELSE CAST(wi.dblIssuedQuantity / (i.intUnitPerLayer * i.intLayerPerPallet) AS NUMERIC(18, 0))
 			   END AS dblNoOfPallet
 			 , Batch.strLeafGrade
 			 , Garden.strGardenMark
 			 , wi.dblQuantity  AS dblRequiredQtyPerSheet
-			 , 1 AS intConcurrencyId
-			 , '' AS strGrade
-		FROM tblMFWorkOrderConsumedLot wi 
-		JOIN tblMFWorkOrder w ON wi.intWorkOrderId = w.intWorkOrderId
-		JOIN tblICItemUOM iu ON wi.intItemUOMId = iu.intItemUOMId
-		JOIN tblICUnitMeasure um ON iu.intUnitMeasureId = um.intUnitMeasureId
-		JOIN tblICLot l ON wi.intLotId = l.intLotId
-		JOIN tblICLotStatus LS ON l.intLotStatusId = LS.intLotStatusId
-		JOIN tblICItem i ON l.intItemId = i.intItemId
-		JOIN tblICItemUOM iu1 ON wi.intItemIssuedUOMId = iu1.intItemUOMId
-		JOIN tblICUnitMeasure um1 ON iu1.intUnitMeasureId = um1.intUnitMeasureId
-		JOIN tblSMCompanyLocation cl ON cl.intCompanyLocationId = l.intLocationId
-		LEFT JOIN tblSMCompanyLocationSubLocation sbl ON sbl.intCompanyLocationSubLocationId = l.intSubLocationId
-		LEFT JOIN tblICStorageLocation sl ON sl.intStorageLocationId = l.intStorageLocationId
-		LEFT JOIN vyuQMGetLotQuality q ON l.intLotId = q.intLotId
-		LEFT JOIN tblMFRecipeItem ri ON wi.intRecipeItemId = ri.intRecipeItemId
-		LEFT JOIN tblICCommodityAttribute MT on MT.intCommodityAttributeId=i.intProductTypeId
-		LEFT JOIN tblICBrand B on B.intBrandId=i.intBrandId
-		LEFT JOIN tblMFLotInventory AS LotInventory ON LotInventory.intLotId = l.intLotId
-		LEFT JOIN tblMFBatch AS Batch ON LotInventory.intBatchId = Batch.intBatchId
-		LEFT JOIN tblSMCompanyLocation AS AuctionCenter ON Batch.intBuyingCenterLocationId = AuctionCenter.intCompanyLocationId
-		LEFT JOIN tblICCommodityAttribute AS SubCluster ON i.intRegionId = SubCluster.intCommodityAttributeId
-		LEFT JOIN tblQMGardenMark Garden ON Garden.intGardenMarkId = Batch.intGardenMarkId
-		WHERE wi.intWorkOrderId = @intWorkOrderId
+		 FROM tblMFWorkOrderConsumedLot wi 
+		 JOIN tblMFWorkOrder w ON wi.intWorkOrderId = w.intWorkOrderId
+		 JOIN tblICItemUOM iu ON wi.intItemUOMId = iu.intItemUOMId
+		 JOIN tblICUnitMeasure um ON iu.intUnitMeasureId = um.intUnitMeasureId
+		 JOIN tblICLot Lot ON wi.intLotId = Lot.intLotId
+		 JOIN tblICLotStatus LS ON Lot.intLotStatusId = LS.intLotStatusId
+		 JOIN tblICItem i ON Lot.intItemId = i.intItemId
+		 JOIN tblICItemUOM iu1 ON wi.intItemIssuedUOMId = iu1.intItemUOMId
+		 JOIN tblICUnitMeasure um1 ON iu1.intUnitMeasureId = um1.intUnitMeasureId
+		 JOIN tblSMCompanyLocation cl ON cl.intCompanyLocationId = Lot.intLocationId
+		 LEFT JOIN tblSMCompanyLocationSubLocation sbl ON sbl.intCompanyLocationSubLocationId = Lot.intSubLocationId
+		 LEFT JOIN tblICStorageLocation sl ON sl.intStorageLocationId = Lot.intStorageLocationId
+		 LEFT JOIN vyuQMGetLotQuality q ON Lot.intLotId = q.intLotId
+		 LEFT JOIN tblMFRecipeItem ri ON wi.intRecipeItemId = ri.intRecipeItemId
+		 LEFT JOIN tblICCommodityAttribute MT on MT.intCommodityAttributeId=i.intProductTypeId
+		 LEFT JOIN tblICBrand B on B.intBrandId=i.intBrandId
+		 LEFT JOIN tblMFLotInventory AS LotInventory ON LotInventory.intLotId = Lot.intLotId
+		 LEFT JOIN tblMFBatch AS Batch ON LotInventory.intBatchId = Batch.intBatchId
+		 LEFT JOIN tblSMCompanyLocation AS AuctionCenter ON Batch.intBuyingCenterLocationId = AuctionCenter.intCompanyLocationId
+		 LEFT JOIN tblICCommodityAttribute AS SubCluster ON i.intRegionId = SubCluster.intCommodityAttributeId
+		 LEFT JOIN tblQMGardenMark Garden ON Garden.intGardenMarkId = Batch.intGardenMarkId
+		 WHERE wi.intWorkOrderId = @intWorkOrderId
 END
 ELSE
 BEGIN
