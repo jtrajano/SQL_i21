9333cc650f84ccdb48c8591a056f0d29fd24f6fa
Allen Marasigan <allen.marasigan@irely.com>
2023-07-28 20:50:25 +0800

    LG-4132 Added view

---
+++ i21Database/dbo/Views/vyuLGNotifyPartyGroupView.sql
@@ -0,0 +1,26 @@
+﻿CREATE VIEW [dbo].[vyuLGNotifyPartyGroupView]
+AS
+SELECT 
+	N.[intNotifyPartyGroupId],
+	N.[intCountryId],
+	N.[strDestinationPort],
+	N.[strSubLocationName],
+	N.[intConcurrencyId],
+	NG.intNotifyPartyGroupDetailId,
+	NG.intBankId,
+	NG.intCompanyLocationId,
+	NG.intCompanySetupID,
+	NG.intEntityId,
+	NG.intEntityLocationId,
+	NG.strNotifyOrConsignee,
+	NG.strType,
+	NG.strParty,
+	NG.strPartyLocation,
+	NG.strAddress,
+	NG.strCity,
+	NG.strState,
+	NG.strCountry,
+	strOriginCountry = C.strCountry
+FROM tblLGNotifyPartyGroup N
+INNER JOIN vyuLGNotifyPartyGroup NG ON NG.intNotifyPartyGroupId = N.intNotifyPartyGroupId
+LEFT JOIN tblSMCountry C ON C.intCountryID = N.intCountryId
