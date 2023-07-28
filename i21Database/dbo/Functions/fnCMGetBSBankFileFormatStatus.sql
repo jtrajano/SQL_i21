83b15b474a1d9e6fdd2ffc111b400e03fba55b0e
Ceryl Joseph Garay <ceryljoseph.garay@iRely.com>
2023-07-28 17:32:06 +0800

    GL-10390

--- i21Database/dbo/Functions/fnCMGetBSBankFileFormatStatus.sql
+++ i21Database/dbo/Functions/fnCMGetBSBankFileFormatStatus.sql
@@ -18,7 +18,7 @@ RETURN (
     BankDescription = 0
     UNION
     SELECT BankFileFormatId , 'Bank File Statement  should have Cleared Date field.'  COLLATE Latin1_General_CI_AS  strError  FROM vyuCMBSBankFileFormatStatus WHERE 
-    BankDescription = 0
+    ClearDate = 0
 )
 
 
