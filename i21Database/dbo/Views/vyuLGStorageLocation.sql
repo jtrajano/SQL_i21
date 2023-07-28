1e767791d4f1b31f601e20f9bfab0aaa183548e2
Allen Marasigan <allen.marasigan@irely.com>
2023-07-28 15:35:13 +0800

    LG-4132 Added tables and views; Added Notify Party Groups to Menu

---
+++ i21Database/dbo/Views/vyuLGStorageLocation.sql
@@ -0,0 +1,8 @@
+﻿CREATE VIEW [dbo].[vyuLGStorageLocation]
+AS
+SELECT 
+	CAST(ROW_NUMBER() OVER(ORDER BY strSubLocationName ASC) AS int) AS intTempId,
+	strSubLocationName,
+	0 AS intConcurrencyId
+FROM tblSMCompanyLocationSubLocation 
+GROUP BY strSubLocationName
