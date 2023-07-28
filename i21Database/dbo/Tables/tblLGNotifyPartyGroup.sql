ba883845657d4b6d99449f7ff6fe4a57b8dde598
Allen Marasigan <allen.marasigan@irely.com>
2023-07-28 15:35:13 +0800

    LG-4132 Added tables and views; Added Notify Party Groups to Menu
    # Conflicts:
    #	i21Database/i21Database.sqlproj

---
+++ i21Database/dbo/Tables/tblLGNotifyPartyGroup.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tblLGNotifyPartyGroup]
+(
+	[intNotifyPartyGroupId] INT NOT NULL IDENTITY (1, 1),
+    [intCountryId] INT NOT NULL, 
+    [strDestinationPort] NVARCHAR(200) NOT NULL,
+    [strSubLocationName] NVARCHAR(200) NOT NULL,
+    [intConcurrencyId] INT NOT NULL, 
+
+    CONSTRAINT [PK_tblLGNotifyPartyGroup] PRIMARY KEY ([intNotifyPartyGroupId]), 
+    CONSTRAINT [FK_tblLGNotifyPartyGroup_tblSMCountry] FOREIGN KEY ([intCountryId]) REFERENCES [tblSMCountry]([intCountryID])
+)
---
+++ i21Database/dbo/Tables/tblLGNotifyPartyGroupDetail.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE [dbo].[tblLGNotifyPartyGroupDetail]
+(
+	[intNotifyPartyGroupDetailId] INT NOT NULL IDENTITY (1, 1),
+    [intNotifyPartyGroupId] INT NOT NULL, 
+    [intConcurrencyId] INT NOT NULL, 
+    [strNotifyOrConsignee] NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL,
+    [strType] NVARCHAR(100) COLLATE Latin1_General_CI_AS NULL,
+    [intEntityId] INT NULL,
+    [intCompanySetupID] INT NULL,
+    [intBankId] INT NULL,
+    [intEntityLocationId] INT NULL,
+    [intCompanyLocationId] INT NULL,
+    
+    CONSTRAINT [FK_tblLGNotifyPartyGroupDetail_tblLGNotifyPartyGroup] FOREIGN KEY ([intNotifyPartyGroupId]) REFERENCES [tblLGNotifyPartyGroup]([intNotifyPartyGroupId]) ON DELETE CASCADE,
+
+    CONSTRAINT [FK_tblLGNotifyPartyGroupDetail_tblEMEntity_intEntityId] FOREIGN KEY ([intEntityId]) REFERENCES tblEMEntity([intEntityId]),
+    CONSTRAINT [FK_tblLGNotifyPartyGroupDetail_tblSMCompanySetup_intCompanySetupID] FOREIGN KEY ([intCompanySetupID]) REFERENCES [tblSMCompanySetup]([intCompanySetupID]),
+    CONSTRAINT [FK_tblLGNotifyPartyGroupDetail_tblCMBank_intBankId] FOREIGN KEY ([intBankId]) REFERENCES [tblCMBank]([intBankId]),
+    CONSTRAINT [FK_tblLGNotifyPartyGroupDetail_tblEMEntityLocation_intEntityLocationId] FOREIGN KEY ([intEntityLocationId]) REFERENCES [tblEMEntityLocation]([intEntityLocationId]),
+    CONSTRAINT [FK_tblLGNotifyPartyGroupDetail_tblSMCompanyLocation_intCompanyLocationId] FOREIGN KEY ([intCompanyLocationId]) REFERENCES [tblSMCompanyLocation]([intCompanyLocationId]),
+)
