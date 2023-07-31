f165723b72ae93863ede09934fdca0e6b0bd6aba
Rad Atienza <rad.atienza@irely.com>
2023-07-31 11:50:39 +0800

    EM-3219

---
+++ i21Database/Scripts/Post-Deployment/EM/2310_FixCMBankTransactionNonExistentEFT.sql
@@ -0,0 +1,9 @@
+﻿PRINT N'EM tblEMEntityEFTInformationtblCMBankTransaction Data fix for CM with non existent EFT Info - START'
+
+UPDATE	tblCMBankTransaction
+SET		intEFTInfoId = NULL
+WHERE   intEFTInfoId NOT IN (SELECT intEntityEFTInfoId FROM tblEMEntityEFTInformation)
+
+PRINT N'EM tblEMEntityEFTInformationtblCMBankTransaction Data fix for CM with non existent EFT Info - END'
+
+
