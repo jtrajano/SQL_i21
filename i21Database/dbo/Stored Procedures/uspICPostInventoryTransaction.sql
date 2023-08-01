7010fbaf46e9aa2f38eca328ea3e22cca9e32808
Feb Montefrio <feb.montefrio@irely.com>
2023-08-01 13:53:20 +0800

    [B] IC-11960
    # Conflicts:
    #	i21Database/dbo/Stored Procedures/uspICPostInventoryTransaction.sql

--- i21Database/dbo/Stored Procedures/uspICPostInventoryTransaction.sql
+++ i21Database/dbo/Stored Procedures/uspICPostInventoryTransaction.sql
@@ -163,6 +163,7 @@ BEGIN
 			,[intTicketId]
 			,[strAccountIdInventory]
 			,[strAccountIdInTransit]
+			,[dblComputedValue]
 			,[dblForexValue]
 	)
 	SELECT	[intItemId]							= @intItemId
@@ -214,6 +215,7 @@ BEGIN
 			,[intTicketId]						= @intTicketId
 			,[strAccountIdInventory]			= glAccountIdInventory.strAccountId
 			,[strAccountIdInTransit]			= glAccountIdInTransit.strAccountId
+			,[dblComputedValue]                 = dbo.fnMultiply(ISNULL(@dblQty, 0), ISNULL(@dblCost, 0)) + ISNULL(@dblValue, 0) 
 			,[dblForexValue]					= ISNULL(@dblForexValue, 0)
 	FROM	tblICItem i 
 			CROSS APPLY [dbo].[fnICGetCompanyLocation](@intItemLocationId, @intInTransitSourceLocationId) [location]
